0f0f74851b194693b2607e7a732bd3d6acc934d8
Ramin Halavati <rhalavati@chromium.org>
2017-10-22 07:51:31 +0000

    Duplicate network traffic annotation id updated.
    'device_geolocation_request' annodation id was used before and was
    depricated, and cannot be used for a new purpose. It is changed to
    'network_location_request'.
    
    Bug: 656607
    Change-Id: I0be195170f39deaa145e8faa696cce0fa8227135
    Reviewed-on: https://chromium-review.googlesource.com/730725
    Reviewed-by: Renjie Liu <renjieliu@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510702}

--- device/geolocation/network_location_request.cc
+++ device/geolocation/network_location_request.cc
@@ -132,7 +132,7 @@ bool NetworkLocationRequest::MakeRequest(
   wifi_timestamp_ = wifi_timestamp;
 
   net::NetworkTrafficAnnotationTag traffic_annotation =
-      net::CompleteNetworkTrafficAnnotation("device_geolocation_request",
+      net::CompleteNetworkTrafficAnnotation("network_location_request",
                                             partial_traffic_annotation,
                                             R"(
         semantics {
