8e778e310ad29d735605f39361b1f231607abe10
Will Chen <chenwilliam@chromium.org>
2017-10-23 18:28:37 +0000

    DevTools: generate summary of test migration
    This prints a simple table summarizing the test migration
    on a per-directory basis in http/tests/devtools/.
    
    😞 sad face = html test
    🎉 party = js test
    
    Usage: npm run test-summary
    
    Bug: 667560
    Change-Id: I91abcf497a148a2f3ca2efdfe6415ae064b733b3
    Reviewed-on: https://chromium-review.googlesource.com/730492
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Will Chen <chenwilliam@chromium.org>
    Commit-Queue: Will Chen <chenwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510856}

---
+++ third_party/WebKit/Source/devtools/scripts/migrate_test/test_summary.js
@@ -0,0 +1,65 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+const fs = require('fs');
+const path = require('path');
+
+const utils = require('../utils');
+
+const TESTS_PATH = path.resolve(__dirname, '..', '..', '..', '..', 'LayoutTests', 'http', 'tests', 'devtools');
+
+function main() {
+  const groups = {};
+  groups['/'] = {html: 0, js: 0};
+  const total = {html: 0, js: 0};
+  const filenames = fs.readdirSync(TESTS_PATH);
+  for (const filename of filenames) {
+    const filePath = path.resolve(TESTS_PATH, filename);
+    if (utils.isDir(filePath)) {
+      groups[filename] = summarizeRecursive(filePath);
+    } else {
+      const extension = path.extname(filePath);
+      if (extension === '.js') {
+        groups['/'].js++;
+      }
+      if (extension === '.html') {
+        groups['/'].html++;
+      }
+    }
+  }
+
+  for (var key in groups) {
+    total.html += groups[key].html;
+    total.js += groups[key].js;
+  }
+
+  for (var key in groups) {
+    console.log(key, ' 😞', groups[key].html, ' 🎉', groups[key].js)
+  }
+
+  console.log('\nTotal: ', ' 😞', total.html, ' 🎉', total.js)
+}
+
+function summarizeRecursive(dirPath) {
+  const aggregatedStat = {html: 0, js: 0};
+  if (dirPath.endsWith('resources'))
+    return aggregatedStat;
+  const filenames = fs.readdirSync(dirPath);
+  for (const filename of filenames) {
+    const filePath = path.resolve(dirPath, filename);
+    if (utils.isDir(filePath)) {
+      const stat = summarizeRecursive(filePath);
+      aggregatedStat.html += stat.html;
+      aggregatedStat.js += stat.js;
+    } else {
+      const extension = path.extname(filePath);
+      if (extension === '.js') {
+        aggregatedStat.js++;
+      }
+      if (extension === '.html') {
+        aggregatedStat.html++;
+      }
+    }
+  }
+  return aggregatedStat;
+}
+
+main();
