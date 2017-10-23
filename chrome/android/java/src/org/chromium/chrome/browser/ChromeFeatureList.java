fa89da83a14432ec7646eb39af847febfef0c870
Michael van Ouwerkerk <mvanouwerkerk@google.com>
2017-10-23 17:48:53 +0000

    Delete small thumbnails variant for NTP suggestions.
    Bug: 734101
    Change-Id: Ib50580c7ce800d082382d9501ced6bbd52e4bb4a
    Reviewed-on: https://chromium-review.googlesource.com/732993
    Commit-Queue: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510839}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeFeatureList.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeFeatureList.java
@@ -171,8 +171,6 @@ public abstract class ChromeFeatureList {
             "ContentSuggestionsFaviconsFromNewServer";
     public static final String CONTENT_SUGGESTIONS_NOTIFICATIONS =
             "ContentSuggestionsNotifications";
-    public static final String CONTENT_SUGGESTIONS_LARGE_THUMBNAIL =
-            "ContentSuggestionsLargeThumbnail";
     public static final String CONTENT_SUGGESTIONS_SCROLL_TO_LOAD =
             "ContentSuggestionsScrollToLoad";
     public static final String CONTENT_SUGGESTIONS_SETTINGS = "ContentSuggestionsSettings";
--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -68,7 +68,6 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &kChromeHomeOptOutSnackbar,
     &kChromeHomeSwipeLogic,
     &kChromeMemexFeature,
-    &kContentSuggestionsLargeThumbnail,
     &kContentSuggestionsScrollToLoad,
     &kContentSuggestionsSettings,
     &kContentSuggestionsThumbnailDominantColor,
@@ -187,9 +186,6 @@ const base::Feature kChromeHomeSwipeLogic{"ChromeHomeSwipeLogic",
 
 const base::Feature kChromeMemexFeature{"ChromeMemex",
                                         base::FEATURE_DISABLED_BY_DEFAULT};
-
-const base::Feature kContentSuggestionsLargeThumbnail{
-    "ContentSuggestionsLargeThumbnail", base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kContentSuggestionsScrollToLoad{
     "ContentSuggestionsScrollToLoad", base::FEATURE_ENABLED_BY_DEFAULT};
