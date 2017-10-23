b6bee70406e703788bccf3b958c11e2765c51b33
Gauthier Ambard <gambard@chromium.org>
2017-10-23 15:22:06 +0000

    Revert "Save NTP tiles offline on the IO thread"
    This reverts commit 7f0472d85a62efebe0551537fe80f1f0794af6d4.
    
    Reason for revert: Introduce a crash on device on fresh install.
    
    Original change's description:
    > Save NTP tiles offline on the IO thread
    > 
    > Bug: 758560
    > Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    > Change-Id: Id40ef2e9be3cdc81da0e04f72c48d7f2e40e0213
    > Reviewed-on: https://chromium-review.googlesource.com/702308
    > Commit-Queue: Elodie Banel <lod@chromium.org>
    > Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#510578}
    
    TBR=rohitrao@chromium.org,sdefresne@chromium.org,lod@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 758560
    Change-Id: I0d0fec14287ef146d985c862165648aa27030cc2
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    Reviewed-on: https://chromium-review.googlesource.com/732819
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510799}

--- ios/chrome/browser/ui/ntp/ntp_tile_saver_unittest.mm
+++ ios/chrome/browser/ui/ntp/ntp_tile_saver_unittest.mm
@@ -4,10 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #import "ios/chrome/browser/ui/ntp/ntp_tile_saver.h"
-#include "base/run_loop.h"
 #import "base/strings/sys_string_conversions.h"
 #import "base/strings/utf_string_conversions.h"
-#include "base/test/scoped_task_environment.h"
 #import "components/ntp_tiles/ntp_tile.h"
 #import "ios/chrome/browser/ui/favicon/favicon_attributes.h"
 #import "ios/chrome/browser/ui/ntp/google_landing_data_source.h"
@@ -108,9 +106,6 @@ class NTPTileSaverControllerTest : public BlockCleanupTest {
     EXPECT_NSEQ(tile.fallbackBackgroundColor, UIColor.blueColor);
     EXPECT_EQ(tile.fallbackIsDefaultColor, NO);
   }
-
- protected:
-  base::test::ScopedTaskEnvironment scoped_task_evironment_;
 };
 
 TEST_F(NTPTileSaverControllerTest, SaveMostVisitedToDisk) {
@@ -132,8 +127,6 @@ TEST_F(NTPTileSaverControllerTest, SaveMostVisitedToDisk) {
 
   ntp_tile_saver::SaveMostVisitedToDisk(tiles, mockFaviconFetcher,
                                         testFaviconDirectory());
-  // Wait for all asynchronous tasks to complete.
-  scoped_task_evironment_.RunUntilIdle();
 
   // Read most visited from disk.
   NSDictionary<NSURL*, NTPTile*>* savedTiles =
@@ -174,8 +167,6 @@ TEST_F(NTPTileSaverControllerTest, UpdateSingleFaviconFallback) {
 
   ntp_tile_saver::SaveMostVisitedToDisk(tiles, mockFaviconFetcher,
                                         testFaviconDirectory());
-  // Wait for all asynchronous tasks to complete.
-  scoped_task_evironment_.RunUntilIdle();
 
   // Read most visited from disk.
   NSDictionary<NSURL*, NTPTile*>* savedTiles =
@@ -204,8 +195,6 @@ TEST_F(NTPTileSaverControllerTest, UpdateSingleFaviconFallback) {
                     {imageTile1.url, fallbackTile.url});
   ntp_tile_saver::UpdateSingleFavicon(imageTile1.url, mockFaviconFetcher2,
                                       testFaviconDirectory());
-  // Wait for all asynchronous tasks to complete.
-  scoped_task_evironment_.RunUntilIdle();
 
   // Read most visited from disk.
   NSDictionary<NSURL*, NTPTile*>* savedTilesAfterUpdate =
