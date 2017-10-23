2fdf121c3c9849cbf0e7026544d3ab74de526b95
Jamie Madill <jmadill@chromium.org>
2017-10-23 20:54:11 +0000

    Revert "Revert "Make HostFrameSinkManager handle GPU crash.""
    This reverts commit 71aadccf16627d5b2451756a551d5c2012f6f95f.
    
    Reason for revert: Did not seem to clear up the crash.
    
    Original change's description:
    > Revert "Make HostFrameSinkManager handle GPU crash."
    > 
    > This reverts commit c5cbe85f310704b191dd6b6ad61e75c45c337fee.
    > 
    > Reason for revert:
    > 
    > Seems to cause a reproducible crash on the GPU.FYI Windows bots:
    > 
    > https://build.chromium.org/p/chromium.gpu.fyi/builders/Win7%20Release%20%28NVIDIA%29/builds/31825
    > https://build.chromium.org/p/tryserver.chromium.angle/builders/win_angle_rel_ng/builds/7359
    > 
    > Failing test:
    > 
    > context_lost_tests on NVIDIA GPU
    > 
    > Crash text:
    > 
    > Received fatal exception EXCEPTION_ACCESS_VIOLATION
    > Backtrace:
    > 	viz::GpuServiceImpl::Crash [0x67B0E67F+223]
    > 	viz::mojom::GpuServiceStubDispatch::Accept [0x6614277A+1056]
    > 	viz::mojom::GpuServiceStub<mojo::RawPtrImplRefTraits<viz::mojom::GpuService> >::Accept [0x67B111D3+19]
    > 	mojo::InterfaceEndpointClient::HandleValidatedMessage [0x66DAAC98+608]
    > 	mojo::FilterChain::Accept [0x66DADFC3+129]
    > 	mojo::InterfaceEndpointClient::HandleIncomingMessage [0x66DAB9F6+104]
    > 	mojo::internal::MultiplexRouter::ProcessIncomingMessage [0x66DA27A8+694]
    > 	mojo::internal::MultiplexRouter::Accept [0x66DA22B9+295]
    > 	mojo::FilterChain::Accept [0x66DADFC3+129]
    > 	mojo::Connector::ReadSingleMessage [0x66DA85E6+376]
    > 	mojo::Connector::ReadAllAvailableMessages [0x66DA8C99+85]
    > 	mojo::Connector::OnHandleReadyInternal [0x66DA8B6F+135]
    > <snip>
    > 
    > Bug: 770833, 776050
    > 
    > Original change's description:
    > > Make HostFrameSinkManager handle GPU crash.
    > > 
    > > This CL adds code to HostFrameSinkManager to handle the case of the GPU
    > > crashing. The mojom::FrameSinkManager connection will be lost when this
    > > happens. CompositorFrameSinkImpl will be destroyed for all clients so
    > > change state internally.
    > > 
    > > Add code to reregister FrameSinkIds and FrameSink hierarchy when
    > > HostFrameSinkManager reconnects. VizProcessTransportFactory installs a
    > > callback so it can also handle GPU crash.
    > > 
    > > Add test for HostFrameSinkManager handling of GPU crash and
    > > reconnection.
    > > 
    > > Bug: 770833, 776050
    > > Change-Id: I400c658b62d09a6d469c37124a5c3aed5ce7f028
    > > Reviewed-on: https://chromium-review.googlesource.com/731266
    > > Commit-Queue: kylechar <kylechar@chromium.org>
    > > Reviewed-by: danakj <danakj@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#510823}
    > 
    > TBR=danakj@chromium.org,kylechar@chromium.org
    > 
    > Change-Id: I4f180251de60bf7bdaa4d729dc8257ba9b385b8d
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 770833, 776050
    > Reviewed-on: https://chromium-review.googlesource.com/734103
    > Reviewed-by: Jamie Madill <jmadill@chromium.org>
    > Commit-Queue: Jamie Madill <jmadill@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#510875}
    
    TBR=danakj@chromium.org,jmadill@chromium.org,kylechar@chromium.org
    
    Change-Id: I91c25fb0ee253606e3035b90bec3447924b26d7d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 770833, 776050
    Reviewed-on: https://chromium-review.googlesource.com/734083
    Reviewed-by: Jamie Madill <jmadill@chromium.org>
    Commit-Queue: Jamie Madill <jmadill@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510910}

--- content/browser/compositor/viz_process_transport_factory.cc
+++ content/browser/compositor/viz_process_transport_factory.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/gpu/gpu_process_host.h"
 #include "content/common/gpu_stream_constants.h"
 #include "content/public/common/content_client.h"
+#include "gpu/command_buffer/client/gles2_interface.h"
 #include "gpu/command_buffer/common/context_result.h"
 #include "gpu/ipc/client/gpu_channel_host.h"
 #include "services/ui/public/cpp/gpu/context_provider_command_buffer.h"
@@ -30,9 +31,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 namespace {
 
-// The client_id used here should not conflict with the client_id generated
-// from RenderWidgetHostImpl.
-constexpr uint32_t kDefaultClientId = 0u;
+// The client id for the browser process. It must not conflict with any
+// child process client id.
+constexpr uint32_t kBrowserClientId = 0u;
 
 scoped_refptr<ui::ContextProviderCommandBuffer> CreateContextProviderImpl(
     scoped_refptr<gpu::GpuChannelHost> gpu_channel_host,
@@ -59,6 +60,16 @@ scoped_refptr<ui::ContextProviderCommandBuffer> CreateContextProviderImpl(
       shared_context_provider, type);
 }
 
+bool LockAndCheckContextLost(
+    ui::ContextProviderCommandBuffer* context_provider) {
+  if (!context_provider)
+    return false;
+
+  viz::ContextProvider::ScopedContextLock lock(context_provider);
+  auto status = context_provider->ContextGL()->GetGraphicsResetStatusKHR();
+  return status != GL_NO_ERROR;
+}
+
 }  // namespace
 
 VizProcessTransportFactory::VizProcessTransportFactory(
@@ -66,7 +77,7 @@ VizProcessTransportFactory::VizProcessTransportFactory(
     scoped_refptr<base::SingleThreadTaskRunner> resize_task_runner)
     : gpu_channel_establish_factory_(gpu_channel_establish_factory),
       resize_task_runner_(std::move(resize_task_runner)),
-      frame_sink_id_allocator_(kDefaultClientId),
+      frame_sink_id_allocator_(kBrowserClientId),
       task_graph_runner_(std::make_unique<cc::SingleThreadTaskGraphRunner>()),
       renderer_settings_(
           viz::CreateRendererSettings(CreateBufferToTextureTargetMap())),
@@ -74,6 +85,9 @@ VizProcessTransportFactory::VizProcessTransportFactory(
   DCHECK(gpu_channel_establish_factory_);
   task_graph_runner_->Start("CompositorTileWorker1",
                             base::SimpleThread::Options());
+  GetHostFrameSinkManager()->SetConnectionLostCallback(
+      base::BindRepeating(&VizProcessTransportFactory::OnGpuProcessLost,
+                          weak_ptr_factory_.GetWeakPtr()));
 }
 
 VizProcessTransportFactory::~VizProcessTransportFactory() {
@@ -98,6 +112,8 @@ void VizProcessTransportFactory::ConnectHostFrameSinkManager() {
                           base::BindOnce(
                               [](viz::mojom::FrameSinkManagerRequest request,
                                  viz::mojom::FrameSinkManagerClientPtr client) {
+                                // TODO(kylechar): Check GpuProcessHost isn't
+                                // null but don't enter a restart loop.
                                 GpuProcessHost::Get()->ConnectFrameSinkManager(
                                     std::move(request), std::move(client));
                               },
@@ -256,6 +272,14 @@ void VizProcessTransportFactory::SetCompositorSuspendedForRecycle(
 }
 #endif
 
+void VizProcessTransportFactory::OnGpuProcessLost() {
+  // Reconnect HostFrameSinkManager to new GPU process.
+  ConnectHostFrameSinkManager();
+
+  for (auto& observer : observer_list_)
+    observer.OnLostResources();
+}
+
 void VizProcessTransportFactory::CreateLayerTreeFrameSinkForGpuChannel(
     base::WeakPtr<ui::Compositor> compositor_weak_ptr,
     scoped_refptr<gpu::GpuChannelHost> gpu_channel_host) {
@@ -272,7 +296,7 @@ void VizProcessTransportFactory::CreateLayerTreeFrameSinkForGpuChannel(
     return;
   }
 
-  // TODO(crbug.com/776050): Deal with context loss and GPU restartability.
+  // TODO(crbug.com/776050): Deal with context loss.
 
   // Create interfaces for a root CompositorFrameSink.
   viz::mojom::CompositorFrameSinkAssociatedPtrInfo sink_info;
@@ -317,7 +341,9 @@ void VizProcessTransportFactory::CreateLayerTreeFrameSinkForGpuChannel(
 scoped_refptr<ui::ContextProviderCommandBuffer>
 VizProcessTransportFactory::CreateContextProvider(
     scoped_refptr<gpu::GpuChannelHost> gpu_channel_host) {
-  // TODO(kylechar): Check if |shared_worker_context_provider_| is lost.
+  if (LockAndCheckContextLost(shared_worker_context_provider_.get()))
+    shared_worker_context_provider_ = nullptr;
+
   if (!shared_worker_context_provider_) {
     shared_worker_context_provider_ = CreateContextProviderImpl(
         gpu_channel_host, true /* support_locking */, nullptr,
