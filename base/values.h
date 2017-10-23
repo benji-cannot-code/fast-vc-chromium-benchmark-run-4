ae6059044a3e0f09b6643ad4c4517c03ca77a1e2
Lei Zhang <thestig@chromium.org>
2017-10-23 19:14:46 +0000

    Add base::Value::{From,To}UniquePtrValue().
    To help with converting base::Value usage from the old way to the new
    way, there needs to be adapters between code that use
    std::unique_ptr<base::Value> and just base::Value. Add them to
    base::Value so others do not have to re-invent it everywhere.
    
    Bug: 646113
    Change-Id: I328e426f3ac2079ecb813eccd98062e5bf941cc0
    Reviewed-on: https://chromium-review.googlesource.com/726939
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Brett Wilson <brettw@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510870}

--- base/values.h
+++ base/values.h
@@ -106,6 +106,10 @@ class BASE_EXPORT Value {
   static std::unique_ptr<Value> CreateWithCopiedBuffer(const char* buffer,
                                                        size_t size);
 
+  // Adaptors for converting from the old way to the new way and vice versa.
+  static Value FromUniquePtrValue(std::unique_ptr<Value> val);
+  static std::unique_ptr<Value> ToUniquePtrValue(Value val);
+
   Value(Value&& that) noexcept;
   Value() noexcept;  // A null value.
 
