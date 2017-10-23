745c8493fd3252763104c0b9adb606bfbcab3840
Philip Rogers <pdr@chromium.org>
2017-10-23 23:45:42 +0000

    Ensure scrollbars are laid out before hit testing
    This patch fixes a bug in hit testing where the frame was not
    laid out before hit testing scrollbars. Layout is required to
    ensure scrollbars are created/destroyed to account for content.
    
    Bug: 776607
    Change-Id: I4523d731d707de4ec3ffcf8721a86a32e68c464f
    Reviewed-on: https://chromium-review.googlesource.com/733620
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510961}

--- third_party/WebKit/Source/core/dom/TreeScope.cpp
+++ third_party/WebKit/Source/core/dom/TreeScope.cpp
@@ -196,7 +196,9 @@ HTMLMapElement* TreeScope::GetImageMap(const String& url) const {
       image_maps_by_name_->GetElementByMapName(AtomicString(name), *this));
 }
 
-static bool PointWithScrollAndZoomIfPossible(const Document& document,
+// If the point is not in the viewport, returns false. Otherwise, adjusts the
+// point to account for the frame's zoom and scroll.
+static bool PointWithScrollAndZoomIfPossible(Document& document,
                                              IntPoint& point) {
   LocalFrame* frame = document.GetFrame();
   if (!frame)
@@ -204,6 +206,9 @@ static bool PointWithScrollAndZoomIfPossible(const Document& document,
   LocalFrameView* frame_view = frame->View();
   if (!frame_view)
     return false;
+
+  // The visibleContentRect check below requires that scrollbars are up-to-date.
+  document.UpdateStyleAndLayoutIgnorePendingStylesheets();
 
   FloatPoint point_in_document(point);
   point_in_document.Scale(frame->PageZoomFactor(), frame->PageZoomFactor());
@@ -217,15 +222,15 @@ static bool PointWithScrollAndZoomIfPossible(const Document& document,
   return true;
 }
 
-HitTestResult HitTestInDocument(const Document* document,
+HitTestResult HitTestInDocument(Document* document,
                                 int x,
                                 int y,
                                 const HitTestRequest& request) {
-  IntPoint hit_point(x, y);
-  if (!PointWithScrollAndZoomIfPossible(*document, hit_point))
+  if (!document->IsActive())
     return HitTestResult();
 
-  if (!document->IsActive())
+  IntPoint hit_point(x, y);
+  if (!PointWithScrollAndZoomIfPossible(*document, hit_point))
     return HitTestResult();
 
   HitTestResult result(request, hit_point);
