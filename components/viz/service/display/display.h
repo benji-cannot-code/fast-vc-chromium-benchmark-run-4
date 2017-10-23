a30811f4bb0ca85f67a2c94cd9b641507353aef8
yiyix <yiyix@chromium.org>
2017-10-23 22:21:47 +0000

    Optimize overdraw on partially overlapped windows
    Overdraw means that one single pixel is drawn more than once, this can
    slow down our chrome browser performance; and this causes our browser
    to use more power to run. In this patch, a new draw occlusion tracker
    to remove DrawQuads that are not shown on the screen from the
    CompositorFrame is added.
    For detailed description:
    https://docs.google.com/document/d/1skSwTc7fDr_O_pYz1mH4hV7QtMkzAOhhKf5Qr4FQx2I/edit
    
    TEST=display_unittest.cc
    
    Bug: 672929
    Change-Id: I910597230c432450eabbef8166cab2cb9d3925ac
    Reviewed-on: https://chromium-review.googlesource.com/711391
    Commit-Queue: Yi Xu <yiyix@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510942}

--- components/viz/service/display/display.h
+++ components/viz/service/display/display.h
@@ -109,6 +109,7 @@ class VIZ_SERVICE_EXPORT Display : public DisplaySchedulerClient,
 
   void ForceImmediateDrawAndSwapIfPossible();
   void SetNeedsOneBeginFrame();
+  void RemoveOverdrawQuads(CompositorFrame* frame);
 
  private:
   void InitializeRenderer();
