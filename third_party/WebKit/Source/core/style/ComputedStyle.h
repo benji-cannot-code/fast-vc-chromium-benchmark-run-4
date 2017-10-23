cfac4fe275085dff59ff3ae91e504d9e98375597
Eddy Mead <meade@chromium.org>
2017-10-23 08:47:25 +0000

    Remove dead code from ComputedStyle.h
    This helper function was used for style sharing, which was removed
    recently.
    
    Change-Id: I050eb1ccf7358fa34f280e5264b7e54c3ff3a2be
    Reviewed-on: https://chromium-review.googlesource.com/732000
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: meade_UTC10 <meade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510738}

--- third_party/WebKit/Source/core/style/ComputedStyle.h
+++ third_party/WebKit/Source/core/style/ComputedStyle.h
@@ -2267,7 +2267,6 @@ class ComputedStyle : public ComputedStyleBase,
 
   // Other utility functions.
   bool IsStyleAvailable() const;
-  bool IsSharable() const;
 
   bool RequireTransformOrigin(ApplyTransformOrigin apply_origin,
                               ApplyMotionPath) const;
@@ -2614,14 +2613,6 @@ inline bool ComputedStyle::SetEffectiveZoom(float f) {
   if (EffectiveZoom() == clamped_effective_zoom)
     return false;
   SetEffectiveZoomInternal(clamped_effective_zoom);
-  return true;
-}
-
-inline bool ComputedStyle::IsSharable() const {
-  if (Unique())
-    return false;
-  if (HasUniquePseudoStyle())
-    return false;
   return true;
 }
 
