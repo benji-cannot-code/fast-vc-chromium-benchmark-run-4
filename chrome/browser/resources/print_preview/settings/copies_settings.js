435cedb128aa37af8577f702bb647a407660984f
rbpotter <rbpotter@chromium.org>
2017-10-23 21:22:24 +0000

    Print Preview cleanup: Convert settings to const and let
    Bug: None
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I11582cebd5073a206cf2ebe264b0e0e702cf9f5b
    Reviewed-on: https://chromium-review.googlesource.com/730858
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510923}

--- chrome/browser/resources/print_preview/settings/copies_settings.js
+++ chrome/browser/resources/print_preview/settings/copies_settings.js
@@ -140,7 +140,7 @@ cr.define('print_preview', function() {
      */
     onTextfieldTimeout_: function() {
       this.textfieldTimeout_ = null;
-      var newValue =
+      const newValue =
           (this.inputField_.validity.valid && this.inputField_.value != '') ?
           this.inputField_.valueAsNumber.toString() :
           '';
--- chrome/browser/resources/print_preview/settings/page_settings.js
+++ chrome/browser/resources/print_preview/settings/page_settings.js
@@ -110,7 +110,7 @@ cr.define('print_preview', function() {
     /** @override */
     enterDocument: function() {
       print_preview.SettingsSection.prototype.enterDocument.call(this);
-      var customInput = assert(this.customInput_);
+      const customInput = assert(this.customInput_);
       this.tracker.add(
           assert(this.allRadio_), 'click', this.onAllRadioClick_.bind(this));
       this.tracker.add(
@@ -165,7 +165,7 @@ cr.define('print_preview', function() {
         fadeOutElement(this.customHintEl_);
         return;
       }
-      var message;
+      let message;
       if (validity === PageRangeStatus.LIMIT_ERROR) {
         if (this.pageRangeTicketItem_.getDocumentNumPages()) {
           message = loadTimeData.getStringF(
@@ -278,7 +278,7 @@ cr.define('print_preview', function() {
      */
     onPageRangeTicketItemChange_: function() {
       if (this.isAvailable()) {
-        var pageRangeStr = this.pageRangeTicketItem_.getValue();
+        const pageRangeStr = this.pageRangeTicketItem_.getValue();
         if (pageRangeStr || this.customRadio_.checked) {
           if (!document.hasFocus() ||
               document.activeElement != this.customInput_) {
