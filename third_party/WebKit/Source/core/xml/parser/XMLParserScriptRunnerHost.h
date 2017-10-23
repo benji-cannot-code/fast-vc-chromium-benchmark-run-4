cc35a556f55963a79f1b39735bdda2cfbe325b38
Daniel Cheng <dcheng@chromium.org>
2017-10-23 10:29:05 +0000

    Remove helper macros for Oilpan and wrapper tracing.
    - cleans up a few stray references from code generation
      templates.
    - updates documentation references to the removed
      macros.
    - fixes a few quirks in the markdown, such as using ```
      or `` for inline code.
    
    Bug: 775741
    Change-Id: Ic739182611abe2e0ca960d4a0b98638c84b8071d
    Reviewed-on: https://chromium-review.googlesource.com/729546
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510757}

--- third_party/WebKit/Source/core/xml/parser/XMLParserScriptRunnerHost.h
+++ third_party/WebKit/Source/core/xml/parser/XMLParserScriptRunnerHost.h
@@ -14,7 +14,7 @@ namespace blink {
 class CORE_EXPORT XMLParserScriptRunnerHost : public GarbageCollectedMixin {
  public:
   virtual ~XMLParserScriptRunnerHost() {}
-  DEFINE_INLINE_VIRTUAL_TRACE() {}
+  virtual void Trace(Visitor*) {}
 
   virtual void NotifyScriptExecuted() = 0;
 };
