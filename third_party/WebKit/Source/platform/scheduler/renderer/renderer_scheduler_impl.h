99409fb24258b3e3f3899ec5c30a2f26812528a2
Nicolas Pena <npm@chromium.org>
2017-10-23 22:46:11 +0000

    Add UMA histograms for EQT by task queue type
    This CL allows reporting metrics for ExpectedQueueingTime by task queue
    type. A SplitCalculator class is added in the QueueingTimeEstimator.
    This class is in charge of splitting up the queueing times of each task
    into buckets and ask the client to report the split EQTs at the
    appropriate times.
    
    Since now the QueueingTimeEstimator knows about MainThreadTaskQueue and
    because this class is only used by renderer_scheduler_impl, the files
    were moved to scheduler/renderer.
    
    The current buckets are calculated by looking at the metric
    RendererScheduler.TaskDurationPerQueueType2.
    
    Bug: chromium:747504
    Change-Id: I8dc5fc980bb8a4095a51ba1d2c83edc34319c161
    Reviewed-on: https://chromium-review.googlesource.com/719607
    Commit-Queue: Nicolás Peña Moreno <npm@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510947}

--- third_party/WebKit/Source/platform/scheduler/renderer/renderer_scheduler_impl.h
+++ third_party/WebKit/Source/platform/scheduler/renderer/renderer_scheduler_impl.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "device/base/synchronization/shared_memory_seqlock_buffer.h"
 #include "platform/PlatformExport.h"
 #include "platform/scheduler/base/pollable_thread_safe_flag.h"
-#include "platform/scheduler/base/queueing_time_estimator.h"
 #include "platform/scheduler/base/task_time_observer.h"
 #include "platform/scheduler/child/idle_canceled_delayed_task_sweeper.h"
 #include "platform/scheduler/child/idle_helper.h"
@@ -26,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/scheduler/renderer/idle_time_estimator.h"
 #include "platform/scheduler/renderer/main_thread_scheduler_helper.h"
 #include "platform/scheduler/renderer/main_thread_task_queue.h"
+#include "platform/scheduler/renderer/queueing_time_estimator.h"
 #include "platform/scheduler/renderer/render_widget_signals.h"
 #include "platform/scheduler/renderer/renderer_metrics_helper.h"
 #include "platform/scheduler/renderer/task_cost_estimator.h"
@@ -151,6 +151,9 @@ class PLATFORM_EXPORT RendererSchedulerImpl
   // QueueingTimeEstimator::Client implementation:
   void OnQueueingTimeForWindowEstimated(base::TimeDelta queueing_time,
                                         bool is_disjoint_window) override;
+  void OnReportSplitExpectedQueueingTime(
+      const std::string& split_description,
+      base::TimeDelta queueing_time) override;
 
   scoped_refptr<MainThreadTaskQueue> DefaultTaskQueue();
   scoped_refptr<MainThreadTaskQueue> CompositorTaskQueue();
