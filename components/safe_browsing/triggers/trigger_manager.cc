96449c11596a832ec1f714a28e54c6774df75a75
Luke Zielinski <lpz@chromium.org>
2017-10-23 17:24:15 +0000

    Make the "collect-dont-send" finch feature also enable report generation for opted-out users.
    The reports are guaranteed to be discarded so no actual data collection
    is happening. This will, however, increase the size of population that
    are generating reports, which will give us more realistic performance
    numbers for this logic.
    
    Bug: 744869
    Change-Id: Ia06a4abb569ca4c8491f354b7e75bdae4bbed5a5
    Reviewed-on: https://chromium-review.googlesource.com/729079
    Reviewed-by: Nathan Parker <nparker@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Commit-Queue: Luke Z <lpz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510831}

--- components/safe_browsing/triggers/trigger_manager.cc
+++ components/safe_browsing/triggers/trigger_manager.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/safe_browsing/base_ui_manager.h"
 #include "components/safe_browsing/browser/threat_details.h"
 #include "components/safe_browsing/common/safe_browsing_prefs.h"
+#include "components/safe_browsing/features.h"
 #include "components/security_interstitials/content/unsafe_resource.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/browser_thread.h"
@@ -56,6 +57,15 @@ bool TriggerNeedsOptInForCollection(const TriggerType trigger_type) {
 
 bool CanSendReport(const SBErrorOptions& error_display_options,
                    const TriggerType trigger_type) {
+  // If the |kAdSamplerCollectButDontSendFeature| feature is enabled then we
+  // will overlook other checks to force the report to be created (which is safe
+  // because we ensure it will be discarded downstream).
+  // TODO(crbug.com/776893): Remote the feature and this logic.
+  if (trigger_type == TriggerType::AD_SAMPLE &&
+      base::FeatureList::IsEnabled(kAdSamplerCollectButDontSendFeature)) {
+    return true;
+  }
+
   // Some triggers require that users are eligible for elevated Scout data
   // collection in order to run.
   bool scout_check_ok = !TriggerNeedsScout(trigger_type) ||
@@ -100,6 +110,15 @@ SBErrorOptions TriggerManager::GetSBErrorDisplayOptions(
 bool TriggerManager::CanStartDataCollection(
     const SBErrorOptions& error_display_options,
     const TriggerType trigger_type) {
+  // If the |kAdSamplerCollectButDontSendFeature| feature is enabled then we
+  // will overlook other checks to force the report to be created (which is safe
+  // because we ensure it will be discarded downstream).
+  // TODO(crbug.com/776893): Remote the feature and this logic.
+  if (trigger_type == TriggerType::AD_SAMPLE &&
+      base::FeatureList::IsEnabled(kAdSamplerCollectButDontSendFeature)) {
+    return true;
+  }
+
   // Some triggers require that the user be opted-in to extended reporting in
   // order to run, while others can run without opt-in (eg: because users are
   // prompted for opt-in as part of the trigger).
