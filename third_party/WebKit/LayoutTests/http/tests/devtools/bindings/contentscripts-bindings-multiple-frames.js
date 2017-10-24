56ec4bf46fd4aac157358bdcefc1f118e5497955
Will Chen <chenwilliam@chromium.org>
2017-10-24 19:12:50 +0000

    DevTools: migrate tests in bindings/, audits/
    This converts a batch of tests from the old test framework (html file)
    into the new test framework (js file).
    
    Most of it was done through an automated transformation script, with
    a small amount of manual clean-up.
    
    Bug: 667560
    Change-Id: I29a616e9db34e23e1f5514661725813836975682
    Reviewed-on: https://chromium-review.googlesource.com/729240
    Reviewed-by: Andrey Lushnikov <lushnikov@chromium.org>
    Commit-Queue: Will Chen <chenwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511226}

--- third_party/WebKit/LayoutTests/http/tests/devtools/bindings/contentscripts-bindings-multiple-frames.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/bindings/contentscripts-bindings-multiple-frames.js
@@ -1,10 +1,12 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-<head>
-<script src="../../inspector/inspector-test.js"></script>
-<script src="../../inspector/bindings/bindings-test.js"></script>
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
-<script>
-async function test() {
+(async function() {
+  TestRunner.addResult(`Verify that SourceMap bindings are generating UISourceCodes properly.\n`);
+  await TestRunner.loadModule('bindings_test_runner');
+
   TestRunner.markStep('initialWorkspace');
   var snapshot = BindingsTestRunner.dumpWorkspace();
 
@@ -25,12 +27,4 @@ async function test() {
   snapshot = BindingsTestRunner.dumpWorkspace(snapshot);
 
   TestRunner.completeTest();
-}
-
-</script>
-</head>
-<body onload="runTest()">
-<p>
-Verify that SourceMap bindings are generating UISourceCodes properly.
-</p>
-</body>
+})();
