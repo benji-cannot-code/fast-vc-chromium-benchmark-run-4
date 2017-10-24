e4df240ba714a9c9bf4f431ded71a646b185ff62
Min Qin <qinmin@chromium.org>
2017-10-24 03:25:05 +0000

    Reenable 2 disabled download browsertests
    When a download is cancelled/removed, the embedded test server may still
    in the middle of sending responses.
    As a result, the tests failed as the URLRequest is not yet removed.
    We have to wait until server finishes sending all responses before terminating the test.
    The added new WaitForServerToFinishAllResponses() method also allow us to validate
    the number of requests sent to the server.
    
    BUG=777220
    
    Change-Id: I5792a83536425176d6b2047510b3b7573295053c
    Reviewed-on: https://chromium-review.googlesource.com/733812
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511029}

--- content/browser/download/download_browsertest.cc
+++ content/browser/download/download_browsertest.cc
@@ -773,6 +773,11 @@ class DownloadContentTest : public ContentBrowserTest {
     return CountingDownloadFile::GetNumberActiveFilesFromFileThread() == 0;
   }
 
+  void WaitForServerToFinishAllResponses(size_t request_num) {
+    while (test_response_handler_.completed_requests().size() != request_num)
+      base::RunLoop().RunUntilIdle();
+  }
+
   void SetupErrorInjectionDownloads() {
     auto factory = base::MakeUnique<ErrorInjectionDownloadFileFactory>();
     inject_error_callback_ = base::Bind(
@@ -1979,8 +1984,7 @@ IN_PROC_BROWSER_TEST_F(DownloadContentTest, CancelResumingDownload) {
   EXPECT_TRUE(EnsureNoPendingDownloads());
 }
 
-// Flaky. See http://crbug.com/777220.
-IN_PROC_BROWSER_TEST_F(DownloadContentTest, DISABLED_RemoveResumedDownload) {
+IN_PROC_BROWSER_TEST_F(DownloadContentTest, RemoveResumedDownload) {
   SetupErrorInjectionDownloads();
   TestDownloadHttpResponse::Parameters parameters =
       TestDownloadHttpResponse::Parameters::WithSingleInterruption(
@@ -2012,10 +2016,10 @@ IN_PROC_BROWSER_TEST_F(DownloadContentTest, DISABLED_RemoveResumedDownload) {
   EXPECT_FALSE(PathExists(intermediate_path));
   EXPECT_FALSE(PathExists(target_path));
   EXPECT_TRUE(EnsureNoPendingDownloads());
+  WaitForServerToFinishAllResponses(2);
 }
 
-// Flaky. See http://crbug.com/777220.
-IN_PROC_BROWSER_TEST_F(DownloadContentTest, DISABLED_CancelResumedDownload) {
+IN_PROC_BROWSER_TEST_F(DownloadContentTest, CancelResumedDownload) {
   SetupErrorInjectionDownloads();
   TestDownloadHttpResponse::Parameters parameters =
       TestDownloadHttpResponse::Parameters::WithSingleInterruption(
@@ -2047,6 +2051,7 @@ IN_PROC_BROWSER_TEST_F(DownloadContentTest, DISABLED_CancelResumedDownload) {
   EXPECT_FALSE(PathExists(intermediate_path));
   EXPECT_FALSE(PathExists(target_path));
   EXPECT_TRUE(EnsureNoPendingDownloads());
+  WaitForServerToFinishAllResponses(2);
 }
 
 IN_PROC_BROWSER_TEST_F(DownloadContentTest, ResumeRestoredDownload_NoFile) {
