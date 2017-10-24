2b19ac7a407b8517b580b710488baa8a50fa99e8
Ian Clelland <iclelland@google.com>
2017-10-24 20:10:21 +0000

    Count uses of navigator.getBattery which would be blocked by spec
    The most recent Battery Status API specification says that calls to getBattery
    from secure contexts, or from frames which are cross-origin with the top-level
    document, should return a promise which rejects. This doesn't match Chrome's
    implementation, so this CL adds use counters to evaluate the real-world effects
    that aligning with the spec would have.
    
    Bug: 747610
    Change-Id: I2b0a387b1e61510700191946e345d9cef9a10e69
    Reviewed-on: https://chromium-review.googlesource.com/733865
    Commit-Queue: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511243}

--- third_party/WebKit/Source/modules/battery/NavigatorBattery.cpp
+++ third_party/WebKit/Source/modules/battery/NavigatorBattery.cpp
@@ -5,8 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "modules/battery/NavigatorBattery.h"
 
+#include "core/dom/Document.h"
 #include "core/dom/ExecutionContext.h"
-#include "core/frame/LocalFrame.h"
+#include "core/frame/UseCounter.h"
 #include "modules/battery/BatteryManager.h"
 
 namespace blink {
@@ -20,10 +21,23 @@ ScriptPromise NavigatorBattery::getBattery(ScriptState* script_state,
 }
 
 ScriptPromise NavigatorBattery::getBattery(ScriptState* script_state) {
-  if (!battery_manager_) {
-    battery_manager_ =
-        BatteryManager::Create(ExecutionContext::From(script_state));
+  ExecutionContext* context = ExecutionContext::From(script_state);
+
+  // Check to see if this request would be blocked according to the Battery
+  // Status API specification.
+  if (context->IsDocument()) {
+    LocalFrame* frame = ToDocument(context)->GetFrame();
+    if (frame) {
+      if (!context->IsSecureContext())
+        UseCounter::Count(frame, WebFeature::kBatteryStatusInsecureOrigin);
+      UseCounter::CountIfFeatureWouldBeBlockedByFeaturePolicy(
+          *frame, WebFeature::kBatteryStatusCrossOrigin,
+          WebFeature::kBatteryStatusSameOriginABA);
+    }
   }
+
+  if (!battery_manager_)
+    battery_manager_ = BatteryManager::Create(context);
   return battery_manager_->StartRequest(script_state);
 }
 
