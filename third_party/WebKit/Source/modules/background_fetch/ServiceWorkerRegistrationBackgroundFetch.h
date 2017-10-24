efa6e85c54611a2fe13d45beb8367bbd781dd60d
Daniel Cheng <dcheng@chromium.org>
2017-10-24 12:16:31 +0000

    Automated fixes from the Chrome clang plugin in //third_party/WebKit/Source/modules/background_fetch
    - normalizes uses of virtual / override / final
    - ensures that auto is const / pointer / reference qualified as
      appropriate
    
    This CL was uploaded by git cl split.
    
    R=peter@chromium.org
    
    Bug: 706466
    Change-Id: I08b8f6b57fc9ddf006439f8760850f33812eef15
    Reviewed-on: https://chromium-review.googlesource.com/734843
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511112}

--- third_party/WebKit/Source/modules/background_fetch/ServiceWorkerRegistrationBackgroundFetch.h
+++ third_party/WebKit/Source/modules/background_fetch/ServiceWorkerRegistrationBackgroundFetch.h
@@ -31,7 +31,7 @@ class ServiceWorkerRegistrationBackgroundFetch final
   static BackgroundFetchManager* backgroundFetch(ServiceWorkerRegistration&);
   BackgroundFetchManager* backgroundFetch();
 
-  virtual void Trace(blink::Visitor*);
+  void Trace(blink::Visitor*) override;
 
  private:
   explicit ServiceWorkerRegistrationBackgroundFetch(ServiceWorkerRegistration*);
