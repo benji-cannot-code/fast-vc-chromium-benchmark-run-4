7313153d5aeb7ae8fe946899061c3ba53200e68a
Alex Newcomer <newcomer@chromium.org>
2017-10-24 18:51:30 +0000

    cros Removing bubble launcher
    Remove old bubble launcher code.
    
    Bug: 775279
    Change-Id: I4ae2da50a31ba15a076fea0412847eb9217b17e6
    Reviewed-on: https://chromium-review.googlesource.com/733844
    Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511219}

--- ui/app_list/views/all_apps_tile_item_view.cc
+++ ui/app_list/views/all_apps_tile_item_view.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/metrics/histogram_macros.h"
 #include "ui/app_list/app_list_constants.h"
-#include "ui/app_list/app_list_features.h"
 #include "ui/app_list/resources/grit/app_list_resources.h"
 #include "ui/app_list/views/app_list_view.h"
 #include "ui/app_list/views/contents_view.h"
@@ -71,8 +70,7 @@ void AllAppsTileItemView::ButtonPressed(views::Button* sender,
                             AppListModel::STATE_LAST);
 
   contents_view_->SetActiveState(AppListModel::STATE_APPS);
-  if (features::IsFullscreenAppListEnabled())
-    app_list_view_->SetState(AppListView::FULLSCREEN_ALL_APPS);
+  app_list_view_->SetState(AppListView::FULLSCREEN_ALL_APPS);
 }
 
 }  // namespace app_list
--- ui/app_list/views/folder_background_view.cc
+++ ui/app_list/views/folder_background_view.cc
@@ -21,7 +21,6 @@ namespace {
 
 const float kFolderInkBubbleScale = 1.2f;
 const int kBubbleTransitionDurationMs = 200;
-const int kBubbleRadiusFullScreen = 288;
 
 }  // namespace
 
@@ -73,14 +72,6 @@ void FolderBackgroundView::UpdateFolderContainerBubble(ShowState state) {
   SchedulePaint();
 }
 
-int FolderBackgroundView::GetFolderContainerBubbleRadius() const {
-  return is_fullscreen_app_list_enabled_
-             ? kBubbleRadiusFullScreen
-             : std::max(GetContentsBounds().width(),
-                        GetContentsBounds().height()) /
-                   2;
-}
-
 void FolderBackgroundView::OnPaint(gfx::Canvas* canvas) {
   if (show_state_ == NO_BUBBLE)
     return;
@@ -91,7 +82,7 @@ void FolderBackgroundView::OnPaint(gfx::Canvas* canvas) {
   flags.setAntiAlias(true);
   flags.setColor(kFolderBubbleColor);
   canvas->DrawCircle(GetContentsBounds().CenterPoint(),
-                     GetFolderContainerBubbleRadius(), flags);
+                     kFolderBackgroundBubbleRadius, flags);
 }
 
 void FolderBackgroundView::OnImplicitAnimationsCompleted() {
