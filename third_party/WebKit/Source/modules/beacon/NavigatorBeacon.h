971e7b9bbbd1b1e7249cae3dd08bea7d7a6a17fe
Daniel Cheng <dcheng@chromium.org>
2017-10-24 12:03:20 +0000

    Automated fixes from the Chrome clang plugin in //third_party/WebKit/Source/modules/beacon
    - normalizes uses of virtual / override / final
    - ensures that auto is const / pointer / reference qualified as
      appropriate
    
    This CL was uploaded by git cl split.
    
    R=tyoshino@chromium.org
    
    Bug: 706466
    Change-Id: Ie7f66ceab2dff76e6cc3b6617e2c6a32ab309a55
    Reviewed-on: https://chromium-review.googlesource.com/734765
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511101}

--- third_party/WebKit/Source/modules/beacon/NavigatorBeacon.h
+++ third_party/WebKit/Source/modules/beacon/NavigatorBeacon.h
@@ -31,7 +31,7 @@ class NavigatorBeacon final : public GarbageCollectedFinalized<NavigatorBeacon>,
                          const ArrayBufferViewOrBlobOrStringOrFormData&,
                          ExceptionState&);
 
-  virtual void Trace(blink::Visitor*);
+  void Trace(blink::Visitor*) override;
 
  private:
   explicit NavigatorBeacon(Navigator&);
