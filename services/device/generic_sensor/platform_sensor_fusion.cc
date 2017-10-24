5db3c4237ad6f27c00552677d8d1ca3f281ecc15
Jun Cai <juncai@chromium.org>
2017-10-24 04:50:15 +0000

    Fetch source sensors after PlatformSensorFusion object is fully constructed
    Currently the PlatformSensorFusion fetches its source sensors in its
    constructor. This can cause problems since when in constructor, the
    object is not fully constructed. This CL moves the fetching source sensor
    code out of the constructor and puts them into a function. After the
    PlatformSensorFusion object is fully constructed, the fetching source
    sensor function is called.
    
    Bug: 775874
    Change-Id: I8add6ede77c62583c26ff94b13a6c616743edd22
    Reviewed-on: https://chromium-review.googlesource.com/730646
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Jun Cai <juncai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511037}

--- services/device/generic_sensor/platform_sensor_fusion.cc
+++ services/device/generic_sensor/platform_sensor_fusion.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/device/generic_sensor/platform_sensor_fusion.h"
 
 #include "base/logging.h"
+#include "base/memory/scoped_refptr.h"
 #include "services/device/generic_sensor/platform_sensor_fusion_algorithm.h"
 #include "services/device/generic_sensor/platform_sensor_provider.h"
 
@@ -20,20 +21,7 @@ PlatformSensorFusion::PlatformSensorFusion(
                      std::move(mapping),
                      provider),
       callback_(callback),
-      fusion_algorithm_(std::move(fusion_algorithm)) {
-  if (!provider)
-    return;
-
-  for (mojom::SensorType type : fusion_algorithm_->source_types()) {
-    scoped_refptr<PlatformSensor> sensor = provider->GetSensor(type);
-    if (sensor) {
-      AddSourceSensor(std::move(sensor));
-    } else {
-      provider->CreateSensor(
-          type, base::Bind(&PlatformSensorFusion::CreateSensorCallback, this));
-    }
-  }
-}
+      fusion_algorithm_(std::move(fusion_algorithm)) {}
 
 // static
 void PlatformSensorFusion::Create(
@@ -41,10 +29,9 @@ void PlatformSensorFusion::Create(
     PlatformSensorProvider* provider,
     std::unique_ptr<PlatformSensorFusionAlgorithm> fusion_algorithm,
     const PlatformSensorProviderBase::CreateSensorCallback& callback) {
-  // TODO(Mikhail): Consider splitting PlatformSensorFusion construction and
-  // fetching source sensors.
-  scoped_refptr<PlatformSensor>(new PlatformSensorFusion(
-      std::move(mapping), provider, callback, std::move(fusion_algorithm)));
+  auto fusion_sensor = base::MakeRefCounted<PlatformSensorFusion>(
+      std::move(mapping), provider, callback, std::move(fusion_algorithm));
+  fusion_sensor->FetchSourceSensors(provider);
 }
 
 mojom::ReportingMode PlatformSensorFusion::GetReportingMode() {
@@ -132,6 +119,22 @@ PlatformSensorFusion::~PlatformSensorFusion() {
 
   for (const auto& pair : source_sensors_)
     pair.second->RemoveClient(this);
+}
+
+void PlatformSensorFusion::FetchSourceSensors(
+    PlatformSensorProvider* provider) {
+  if (!provider)
+    return;
+
+  for (mojom::SensorType type : fusion_algorithm_->source_types()) {
+    scoped_refptr<PlatformSensor> sensor = provider->GetSensor(type);
+    if (sensor) {
+      AddSourceSensor(std::move(sensor));
+    } else {
+      provider->CreateSensor(
+          type, base::Bind(&PlatformSensorFusion::CreateSensorCallback, this));
+    }
+  }
 }
 
 void PlatformSensorFusion::CreateSensorCallback(
