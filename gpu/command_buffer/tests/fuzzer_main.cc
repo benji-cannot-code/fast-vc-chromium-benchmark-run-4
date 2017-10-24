faef5db0db49749ceaa6a8f19e2b6e244a7232b4
Antoine Labour <piman@chromium.org>
2017-10-24 22:05:18 +0000

    Fix gpu_*_fuzzer
    They crash on startup because the ServiceDiscardableManager was initialized
    before the command line. Easy fix.
    
    Bug: 776267
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ie4e4c4cac8bfc342cd13bbb424e0fa6f05bdf90a
    Reviewed-on: https://chromium-review.googlesource.com/731376
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Commit-Queue: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511276}

--- gpu/command_buffer/tests/fuzzer_main.cc
+++ gpu/command_buffer/tests/fuzzer_main.cc
@@ -126,6 +126,7 @@ class CommandBufferSetup {
     CHECK(gl::init::InitializeGLOneOffImplementation(
         gl::kGLImplementationSwiftShaderGL, false, false, false, true));
 #endif
+    discardable_manager_ = std::make_unique<ServiceDiscardableManager>();
 
 #if !defined(GPU_FUZZER_USE_STUB)
     surface_ = new gl::PbufferGLSurfaceEGL(gfx::Size());
@@ -153,7 +154,7 @@ class CommandBufferSetup {
         &translator_cache_, &completeness_cache_, feature_info,
         true /* bind_generates_resource */, &image_manager_,
         nullptr /* image_factory */, nullptr /* progress_reporter */,
-        GpuFeatureInfo(), &discardable_manager_);
+        GpuFeatureInfo(), discardable_manager_.get());
     command_buffer_.reset(new CommandBufferDirect(
         context_group->transfer_buffer_manager(), &sync_point_manager_));
 
@@ -271,7 +272,7 @@ class CommandBufferSetup {
   scoped_refptr<gl::GLShareGroup> share_group_;
   SyncPointManager sync_point_manager_;
   gles2::ImageManager image_manager_;
-  ServiceDiscardableManager discardable_manager_;
+  std::unique_ptr<ServiceDiscardableManager> discardable_manager_;
 
   bool recreate_context_ = false;
   scoped_refptr<gl::GLSurface> surface_;
