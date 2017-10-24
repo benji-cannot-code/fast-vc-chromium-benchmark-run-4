d959752c3a0e3283ee69cdeecba3bb9f0df49e46
Vaclav Brozek <vabr@chromium.org>
2017-10-24 15:38:32 +0000

    Separate authentication logic from PasswordManagerPresenter
    Currently, PasswordManagerPresenter offers OS-level reauthentication to
    be used before revealing passwords in settings. This reauthentication
    has a platform independent logic concerning time spans of
    reauthentication attempt validity, and platform dependent OS calls to
    show appropriate UI. This is all inside one method within
    PasswordManagerPresenter. That makes it hard to test both the
    time-handling logic, and the fact that the rest of
    PasswordManagerPresenter invokes reauthentication as needed.
    
    Therefore, this CL separates that logic into a class,
    PasswordAccessAuthenticator, which contains precisely the
    platform-independent logic. An object of that class gets a callback to
    invoke the OS-dependent UI calls, which allows mocking it. Therefore the
    logic itself becomes easily testable, and also PasswordManagerPresenter
    can be tested for using reauthentication.
    
    Bug: 700003
    Change-Id: Ie694895c9e730be095712b95e64f320601fc0f8f
    Reviewed-on: https://chromium-review.googlesource.com/733131
    Commit-Queue: Vaclav Brozek <vabr@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511158}

--- chrome/browser/ui/passwords/password_manager_presenter.h
+++ chrome/browser/ui/passwords/password_manager_presenter.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "chrome/browser/ui/passwords/credential_provider_interface.h"
+#include "chrome/browser/ui/passwords/password_access_authenticator.h"
 #include "chrome/browser/ui/passwords/password_manager_porter.h"
 #include "components/password_manager/core/browser/password_store.h"
 #include "components/password_manager/core/browser/password_store_consumer.h"
@@ -79,9 +80,6 @@ class PasswordManagerPresenter
   // |index| The index of the entry.
   void RequestShowPassword(size_t index);
 
-  // Returns true if the user is authenticated.
-  virtual bool IsUserAuthenticated();
-
   // Trigger the password import procedure, allowing the user to load passwords
   // from a file.
   void ImportPasswords(content::WebContents* web_contents);
@@ -98,8 +96,16 @@ class PasswordManagerPresenter
   // undo action to |undo_manager_|.
   void RemoveLogin(const autofill::PasswordForm& form);
 
+  // Use this in tests to mock the OS-level reauthentication.
+  void SetOsReauthCallForTesting(
+      base::RepeatingCallback<bool()> os_reauth_call);
+
  private:
   friend class PasswordManagerPresenterTest;
+
+  // Triggers an OS-dependent UI to present OS account login challenge and
+  // returns true if the user is passed that challenge.
+  bool OsReauthCall();
 
   // Sets the password and exception list of the UI view.
   void SetPasswordList();
@@ -172,14 +178,12 @@ class PasswordManagerPresenter
   // Whether to show stored passwords or not.
   BooleanPrefMember show_passwords_;
 
-  // The last time the user was successfully authenticated.
-  // Used to determine whether or not to reveal plaintext passwords.
-  base::TimeTicks last_authentication_time_;
-
   // UI view that owns this presenter.
   PasswordUIView* password_view_;
 
   PasswordManagerPorter password_manager_porter_;
+
+  PasswordAccessAuthenticator password_access_authenticator_;
 
   DISALLOW_COPY_AND_ASSIGN(PasswordManagerPresenter);
 };
