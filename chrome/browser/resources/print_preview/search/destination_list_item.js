bb73d1951d0e5f4ec5d94d6540b8748f2ac1c8e6
rbpotter <rbpotter@chromium.org>
2017-10-24 01:37:23 +0000

    Print Preview cleanup: convert search to const and let
    Bug: 777621
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I2a72e81c072552dd6b18a6f328cdb494a0f20be5
    Reviewed-on: https://chromium-review.googlesource.com/731837
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510994}

--- chrome/browser/resources/print_preview/search/destination_list_item.js
+++ chrome/browser/resources/print_preview/search/destination_list_item.js
@@ -139,12 +139,12 @@ cr.define('print_preview', function() {
      * @private
      */
     updateUi_: function() {
-      var iconImg = this.getChildElement('.destination-list-item-icon');
+      const iconImg = this.getChildElement('.destination-list-item-icon');
       iconImg.src = this.destination_.iconUrl;
       iconImg.srcset = this.destination_.srcSet;
 
-      var nameEl = this.getChildElement('.destination-list-item-name');
-      var textContent = this.destination_.displayName;
+      const nameEl = this.getChildElement('.destination-list-item-name');
+      let textContent = this.destination_.displayName;
       if (this.query_) {
         nameEl.textContent = '';
         // When search query is specified, make it obvious why this particular
@@ -154,7 +154,7 @@ cr.define('print_preview', function() {
         // Show the first matching property.
         this.destination_.extraPropertiesToMatch.some(function(property) {
           if (property.match(this.query_)) {
-            var hintSpan = document.createElement('span');
+            const hintSpan = document.createElement('span');
             hintSpan.className = 'search-hint';
             nameEl.appendChild(hintSpan);
             this.addTextWithHighlight_(hintSpan, property);
@@ -170,8 +170,8 @@ cr.define('print_preview', function() {
       nameEl.title = textContent;
 
       if (this.destination_.isExtension) {
-        var extensionNameEl = this.getChildElement('.extension-name');
-        var extensionName = this.destination_.extensionName;
+        const extensionNameEl = this.getChildElement('.extension-name');
+        const extensionName = this.destination_.extensionName;
         extensionNameEl.title = this.destination_.extensionName;
         if (this.query_) {
           extensionNameEl.textContent = '';
@@ -180,7 +180,7 @@ cr.define('print_preview', function() {
           extensionNameEl.textContent = this.destination_.extensionName;
         }
 
-        var extensionIconEl = this.getChildElement('.extension-icon');
+        const extensionIconEl = this.getChildElement('.extension-icon');
         extensionIconEl.style.backgroundImage = '-webkit-image-set(' +
             'url(chrome://extension-icon/' + this.destination_.extensionId +
             '/24/1) 1x,' +
@@ -193,13 +193,13 @@ cr.define('print_preview', function() {
             this.onExtensionIconKeyDown_.bind(this));
       }
 
-      var extensionIndicatorEl =
+      const extensionIndicatorEl =
           this.getChildElement('.extension-controlled-indicator');
       setIsVisible(extensionIndicatorEl, this.destination_.isExtension);
 
       // Initialize the element which renders the destination's offline status.
       this.getElement().classList.toggle('stale', this.destination_.isOffline);
-      var offlineStatusEl = this.getChildElement('.offline-status');
+      const offlineStatusEl = this.getChildElement('.offline-status');
       offlineStatusEl.textContent = this.destination_.offlineStatusText;
       setIsVisible(offlineStatusEl, this.destination_.isOffline);
 
@@ -224,12 +224,12 @@ cr.define('print_preview', function() {
      * @private
      */
     addTextWithHighlight_: function(parent, text) {
-      var sections = text.split(this.query_);
-      for (var i = 0; i < sections.length; ++i) {
+      const sections = text.split(this.query_);
+      for (let i = 0; i < sections.length; ++i) {
         if (i % 2 == 0) {
           parent.appendChild(document.createTextNode(sections[i]));
         } else {
-          var span = document.createElement('span');
+          const span = document.createElement('span');
           span.className = 'destination-list-item-query-highlight';
           span.textContent = sections[i];
           parent.appendChild(span);
@@ -262,7 +262,7 @@ cr.define('print_preview', function() {
 
       // Check if the printer needs configuration before using. The user is only
       // allowed to set up one printer at one time.
-      var configureEvent = new CustomEvent(
+      const configureEvent = new CustomEvent(
           DestinationListItem.EventType.CONFIGURE_REQUEST,
           {detail: {destination: this.destination_}});
       this.eventTarget_.dispatchEvent(configureEvent);
@@ -276,7 +276,7 @@ cr.define('print_preview', function() {
     onDestinationActivated_: function() {
       if (this.destination_.connectionStatus !=
           print_preview.DestinationConnectionStatus.UNREGISTERED) {
-        var selectEvt = new Event(DestinationListItem.EventType.SELECT);
+        const selectEvt = new Event(DestinationListItem.EventType.SELECT);
         selectEvt.destination = this.destination_;
         this.eventTarget_.dispatchEvent(selectEvt);
       }
@@ -291,7 +291,7 @@ cr.define('print_preview', function() {
     onKeyDown_: function(e) {
       if (!hasKeyModifiers(e)) {
         if (e.keyCode == 13) {
-          var activeElementTag = document.activeElement ?
+          const activeElementTag = document.activeElement ?
               document.activeElement.tagName.toUpperCase() :
               '';
           if (activeElementTag == 'LI') {
@@ -308,7 +308,7 @@ cr.define('print_preview', function() {
      * @private
      */
     onRegisterPromoClicked_: function() {
-      var promoClickedEvent =
+      const promoClickedEvent =
           new Event(DestinationListItem.EventType.REGISTER_PROMO_CLICKED);
       promoClickedEvent.destination = this.destination_;
       this.eventTarget_.dispatchEvent(promoClickedEvent);
--- chrome/browser/resources/print_preview/search/destination_search.js
+++ chrome/browser/resources/print_preview/search/destination_search.js
@@ -302,7 +302,7 @@ cr.define('print_preview', function() {
      * @private
      */
     getAvailableListsHeight_: function() {
-      var elStyle = window.getComputedStyle(this.getElement());
+      const elStyle = window.getComputedStyle(this.getElement());
       return this.getElement().offsetHeight -
           parseInt(elStyle.getPropertyValue('padding-top'), 10) -
           parseInt(elStyle.getPropertyValue('padding-bottom'), 10) -
@@ -337,12 +337,12 @@ cr.define('print_preview', function() {
      * @private
      */
     renderDestinations_: function() {
-      var recentDestinations = [];
-      var localDestinations = [];
-      var cloudDestinations = [];
-      var unregisteredCloudDestinations = [];
+      const recentDestinations = [];
+      const localDestinations = [];
+      const cloudDestinations = [];
+      const unregisteredCloudDestinations = [];
 
-      var destinations =
+      const destinations =
           this.destinationStore_.destinations(this.userInfo_.activeUser);
       destinations.forEach(function(destination) {
         if (destination.isRecent) {
@@ -368,7 +368,7 @@ cr.define('print_preview', function() {
         this.registerPromoShownMetricRecorded_ = true;
       }
 
-      var finalCloudDestinations =
+      const finalCloudDestinations =
           unregisteredCloudDestinations
               .slice(0, DestinationSearch.MAX_PROMOTED_UNREGISTERED_PRINTERS_)
               .concat(
@@ -390,42 +390,44 @@ cr.define('print_preview', function() {
         return;
       }
 
-      var hasCloudList = getIsVisible(this.getChildElement('.cloud-list'));
-      var lists = [this.recentList_, this.localList_];
+      const hasCloudList = getIsVisible(this.getChildElement('.cloud-list'));
+      const lists = [this.recentList_, this.localList_];
       if (hasCloudList) {
         lists.push(this.cloudList_);
       }
 
-      var getListsTotalHeight = function(lists, counts) {
+      const getListsTotalHeight = function(lists, counts) {
         return lists.reduce(function(sum, list, index) {
-          var container = list.getContainerElement();
+          const container = list.getContainerElement();
           return sum + list.getEstimatedHeightInPixels(counts[index]) +
               parseInt(window.getComputedStyle(container).paddingBottom, 10);
         }, 0);
       };
-      var getCounts = function(lists, count) {
+      const getCounts = function(lists, count) {
         return lists.map(function(list) {
           return count;
         });
       };
 
-      var availableHeight = this.getAvailableListsHeight_();
-      var listsEl = this.getChildElement('.lists');
+      const availableHeight = this.getAvailableListsHeight_();
+      const listsEl = this.getChildElement('.lists');
       listsEl.style.maxHeight = availableHeight + 'px';
 
-      var maxListLength = lists.reduce(function(prevCount, list) {
+      const maxListLength = lists.reduce(function(prevCount, list) {
         return Math.max(prevCount, list.getDestinationsCount());
       }, 0);
-      for (var i = 1; i <= maxListLength; i++) {
+
+      let i = 1;
+      for (; i <= maxListLength; i++) {
         if (getListsTotalHeight(lists, getCounts(lists, i)) > availableHeight) {
           i--;
           break;
         }
       }
-      var counts = getCounts(lists, i);
+      const counts = getCounts(lists, i);
       // Fill up the possible n-1 free slots left by the previous loop.
       if (getListsTotalHeight(lists, counts) < availableHeight) {
-        for (var countIndex = 0; countIndex < counts.length; countIndex++) {
+        for (let countIndex = 0; countIndex < counts.length; countIndex++) {
           counts[countIndex]++;
           if (getListsTotalHeight(lists, counts) > availableHeight) {
             counts[countIndex]--;
@@ -440,10 +442,10 @@ cr.define('print_preview', function() {
 
       // Set height of the list manually so that search filter doesn't change
       // lists height.
-      var listsHeight = getListsTotalHeight(lists, counts) + 'px';
+      const listsHeight = getListsTotalHeight(lists, counts) + 'px';
       if (listsHeight != listsEl.style.height) {
         // Try to close account select if there's a possibility it's open now.
-        var accountSelectEl = this.getChildElement('.account-select');
+        const accountSelectEl = this.getChildElement('.account-select');
         if (!accountSelectEl.disabled) {
           accountSelectEl.disabled = true;
           accountSelectEl.disabled = false;
@@ -473,7 +475,7 @@ cr.define('print_preview', function() {
      * @private
      */
     updateInvitations_: function() {
-      var invitations = this.userInfo_.activeUser ?
+      const invitations = this.userInfo_.activeUser ?
           this.invitationStore_.invitations(this.userInfo_.activeUser) :
           [];
       if (invitations.length > 0) {
@@ -496,7 +498,7 @@ cr.define('print_preview', function() {
      * @private
      */
     showInvitation_: function(invitation) {
-      var invitationText = '';
+      let invitationText = '';
       if (invitation.asGroupManager) {
         invitationText = loadTimeData.getStringF(
             'groupPrinterSharingInviteText', HTMLEscape(invitation.sender),
@@ -509,7 +511,7 @@ cr.define('print_preview', function() {
       }
       this.getChildElement('.invitation-text').innerHTML = invitationText;
 
-      var acceptButton = this.getChildElement('.invitation-accept-button');
+      const acceptButton = this.getChildElement('.invitation-accept-button');
       acceptButton.textContent = loadTimeData.getString(
           invitation.asGroupManager ? 'acceptForGroup' : 'accept');
       acceptButton.disabled = !!this.invitationStore_.invitationInProgress;
@@ -525,17 +527,17 @@ cr.define('print_preview', function() {
      * @private
      */
     onUsersChanged_: function() {
-      var loggedIn = this.userInfo_.loggedIn;
+      const loggedIn = this.userInfo_.loggedIn;
       if (loggedIn) {
-        var accountSelectEl = this.getChildElement('.account-select');
+        const accountSelectEl = this.getChildElement('.account-select');
         accountSelectEl.innerHTML = '';
         this.userInfo_.users.forEach(function(account) {
-          var option = document.createElement('option');
+          const option = document.createElement('option');
           option.text = account;
           option.value = account;
           accountSelectEl.add(option);
         });
-        var option = document.createElement('option');
+        const option = document.createElement('option');
         option.text = loadTimeData.getString('addAccountTitle');
         option.value = '';
         accountSelectEl.add(option);
@@ -570,11 +572,11 @@ cr.define('print_preview', function() {
      * @private
      */
     onDestinationConfigureRequest_: function(event) {
-      var destination = event.detail.destination;
+      const destination = event.detail.destination;
       // Cloud Print Device printers are stored in the local list
       // crbug.com/713831.
       // TODO(crbug.com/416701): Upon resolution, update this.
-      var destinationItem =
+      const destinationItem =
           (destination.isLocal ||
            destination.origin == print_preview.DestinationOrigin.DEVICE) ?
           this.localList_.getDestinationItem(destination.id) :
@@ -651,7 +653,7 @@ cr.define('print_preview', function() {
             !!this.provisionalDestinationResolver_,
             'Unable to create provisional destination resolver');
 
-        var lastFocusedElement = document.activeElement;
+        const lastFocusedElement = document.activeElement;
         this.addChild(this.provisionalDestinationResolver_);
         this.provisionalDestinationResolver_.run(this.getElement())
             .then(resolvedDestination => {
@@ -689,9 +691,9 @@ cr.define('print_preview', function() {
      * @private
      */
     onDestinationStoreSelect_: function() {
-      var destinations =
+      const destinations =
           this.destinationStore_.destinations(this.userInfo_.activeUser);
-      var recentDestinations = [];
+      const recentDestinations = [];
       destinations.forEach(function(destination) {
         if (destination.isRecent) {
           recentDestinations.push(destination);
@@ -762,8 +764,8 @@ cr.define('print_preview', function() {
      * @private
      */
     onAccountChange_: function() {
-      var accountSelectEl = this.getChildElement('.account-select');
-      var account =
+      const accountSelectEl = this.getChildElement('.account-select');
+      const account =
           accountSelectEl.options[accountSelectEl.selectedIndex].value;
       if (account) {
         this.userInfo_.activeUser = account;
@@ -774,7 +776,7 @@ cr.define('print_preview', function() {
       } else {
         cr.dispatchSimpleEvent(this, DestinationSearch.EventType.ADD_ACCOUNT);
         // Set selection back to the active user.
-        for (var i = 0; i < accountSelectEl.options.length; i++) {
+        for (let i = 0; i < accountSelectEl.options.length; i++) {
           if (accountSelectEl.options[i].value == this.userInfo_.activeUser) {
             accountSelectEl.selectedIndex = i;
             break;
