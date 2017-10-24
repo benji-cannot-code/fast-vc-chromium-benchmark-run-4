c1d7dc80935e000cded4d99eb20fe3150a516dc9
Kyle Horimoto <khorimoto@google.com>
2017-10-24 17:39:36 +0000

    [CrOS Tether] Add metrics for stopping discovery sessions.
    Bug: 777027, 672263
    Change-Id: I2dc1447601b6c3c65e2d8390e917b20c8fff4bf5
    Reviewed-on: https://chromium-review.googlesource.com/734208
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511199}

--- chromeos/components/tether/ble_synchronizer.cc
+++ chromeos/components/tether/ble_synchronizer.cc
@@ -178,7 +178,7 @@ void BleSynchronizer::OnErrorUnregisteringAdvertisement(
 
 void BleSynchronizer::OnDiscoverySessionStarted(
     std::unique_ptr<device::BluetoothDiscoverySession> discovery_session) {
-  RecordDiscoverySessionStarted(true);
+  RecordDiscoverySessionStarted(true /* success */);
   ScheduleCommandCompletion();
   StartDiscoveryArgs* start_discovery_args =
       current_command_->start_discovery_args.get();
@@ -187,7 +187,7 @@ void BleSynchronizer::OnDiscoverySessionStarted(
 }
 
 void BleSynchronizer::OnErrorStartingDiscoverySession() {
-  RecordDiscoverySessionStarted(false);
+  RecordDiscoverySessionStarted(false /* success */);
   ScheduleCommandCompletion();
   StartDiscoveryArgs* start_discovery_args =
       current_command_->start_discovery_args.get();
@@ -196,6 +196,7 @@ void BleSynchronizer::OnErrorStartingDiscoverySession() {
 }
 
 void BleSynchronizer::OnDiscoverySessionStopped() {
+  RecordDiscoverySessionStopped(true /* success */);
   ScheduleCommandCompletion();
   StopDiscoveryArgs* stop_discovery_args =
       current_command_->stop_discovery_args.get();
@@ -204,6 +205,7 @@ void BleSynchronizer::OnDiscoverySessionStopped() {
 }
 
 void BleSynchronizer::OnErrorStoppingDiscoverySession() {
+  RecordDiscoverySessionStopped(false /* success */);
   ScheduleCommandCompletion();
   StopDiscoveryArgs* stop_discovery_args =
       current_command_->stop_discovery_args.get();
@@ -274,6 +276,11 @@ BleSynchronizer::BluetoothAdvertisementErrorCodeToResult(
 
 void BleSynchronizer::RecordDiscoverySessionStarted(bool success) {
   UMA_HISTOGRAM_BOOLEAN("InstantTethering.BluetoothDiscoverySessionStarted",
+                        success);
+}
+
+void BleSynchronizer::RecordDiscoverySessionStopped(bool success) {
+  UMA_HISTOGRAM_BOOLEAN("InstantTethering.BluetoothDiscoverySessionStopped",
                         success);
 }
 
