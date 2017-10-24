ca885d19d84880b05348464912eb90109be31978
Alex Newcomer <newcomer@chromium.org>
2017-10-24 20:43:27 +0000

    Revert "cros Removing bubble launcher"
    This reverts commit 51141ed46ad8bf54188d5def640cb25ae26203d5.
    
    Reason for revert: Reverting so we can remove the flag. Once that happens, I will re-land this change.
    
    Original change's description:
    > cros Removing bubble launcher
    > 
    > Remove old bubble launcher code.
    > 
    > Bug: 775279
    > Change-Id: I4ae2da50a31ba15a076fea0412847eb9217b17e6
    > Reviewed-on: https://chromium-review.googlesource.com/733844
    > Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#511219}
    
    TBR=xiyuan@chromium.org,newcomer@chromium.org
    
    Change-Id: I8a116157932b9ad9edf528e4c598d251a2ce2bfc
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 775279
    Reviewed-on: https://chromium-review.googlesource.com/736219
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511251}

--- ui/app_list/views/folder_background_view.cc
+++ ui/app_list/views/folder_background_view.cc
@@ -21,6 +21,7 @@ namespace {
 
 const float kFolderInkBubbleScale = 1.2f;
 const int kBubbleTransitionDurationMs = 200;
+const int kBubbleRadiusFullScreen = 288;
 
 }  // namespace
 
@@ -72,6 +73,14 @@ void FolderBackgroundView::UpdateFolderContainerBubble(ShowState state) {
   SchedulePaint();
 }
 
+int FolderBackgroundView::GetFolderContainerBubbleRadius() const {
+  return is_fullscreen_app_list_enabled_
+             ? kBubbleRadiusFullScreen
+             : std::max(GetContentsBounds().width(),
+                        GetContentsBounds().height()) /
+                   2;
+}
+
 void FolderBackgroundView::OnPaint(gfx::Canvas* canvas) {
   if (show_state_ == NO_BUBBLE)
     return;
@@ -82,7 +91,7 @@ void FolderBackgroundView::OnPaint(gfx::Canvas* canvas) {
   flags.setAntiAlias(true);
   flags.setColor(kFolderBubbleColor);
   canvas->DrawCircle(GetContentsBounds().CenterPoint(),
-                     kFolderBackgroundBubbleRadius, flags);
+                     GetFolderContainerBubbleRadius(), flags);
 }
 
 void FolderBackgroundView::OnImplicitAnimationsCompleted() {
