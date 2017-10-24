a6d71a77ff233510cfe1dd876c6c14dc0b02b90c
kylechar <kylechar@chromium.org>
2017-10-24 14:37:27 +0000

    Revert "Add confine bounds code for Ozone X11."
    This reverts commit 9e43f8812c7a4bcd02167f8f8cb162fd0df6f007.
    
    Reason for revert: This is still broken.
    
    Original change's description:
    > Add confine bounds code for Ozone X11.
    > 
    > This CL implements ConfineCursorToBounds() for Ozone X11. This is only
    > active if --ash-constrain-pointer-to-root is passed in.
    > 
    > Bug: 771212
    > Change-Id: I605c704846a34d61a77a87518f1872355e821773
    > Reviewed-on: https://chromium-review.googlesource.com/731028
    > Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    > Commit-Queue: kylechar <kylechar@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#510957}
    
    TBR=sadrul@chromium.org,kylechar@chromium.org
    
    Change-Id: I084728aefee51ba92c0df41413ea20afd66ac4d2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 771212
    Reviewed-on: https://chromium-review.googlesource.com/734954
    Reviewed-by: kylechar <kylechar@chromium.org>
    Commit-Queue: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511143}

--- ui/platform_window/x11/x11_window_base.h
+++ ui/platform_window/x11/x11_window_base.h
@@ -8,8 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
-#include <array>
-
 #include "base/callback.h"
 #include "base/macros.h"
 #include "ui/gfx/geometry/rect.h"
@@ -50,12 +48,11 @@ class X11_WINDOW_EXPORT X11WindowBase : public PlatformWindow {
   PlatformImeController* GetPlatformImeController() override;
 
  protected:
+  void Destroy();
+
   PlatformWindowDelegate* delegate() { return delegate_; }
   XDisplay* xdisplay() { return xdisplay_; }
   XID xwindow() const { return xwindow_; }
-
-  void Destroy();
-  void UnConfineCursor();
 
   // Checks if XEvent is for this XWindow.
   bool IsEventForXWindow(const XEvent& xev) const;
@@ -64,7 +61,7 @@ class X11_WINDOW_EXPORT X11WindowBase : public PlatformWindow {
   void ProcessXWindowEvent(XEvent* xev);
 
  private:
-  PlatformWindowDelegate* const delegate_;
+  PlatformWindowDelegate* delegate_;
 
   XDisplay* xdisplay_;
   XID xwindow_;
@@ -75,10 +72,6 @@ class X11_WINDOW_EXPORT X11WindowBase : public PlatformWindow {
 
   // The bounds of |xwindow_|.
   gfx::Rect bounds_;
-
-  // Keep track of barriers to confine cursor.
-  bool has_pointer_barriers_ = false;
-  std::array<XID, 4> pointer_barriers_;
 
   bool window_mapped_ = false;
 
