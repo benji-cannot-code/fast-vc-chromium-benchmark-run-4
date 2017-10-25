a0058a6d009fcf19c6613a93c08ba9237b4ba8bf
Rouslan Solomakhin <rouslan@chromium.org>
2017-10-25 21:58:07 +0000

    Revert "[Payments] Enable payment handler on waterfall."
    This reverts commit 4394930fd776570396ec207a98ff40c4ca072be0.
    
    Reason for revert: https://crbug.com/778401
    
    Original change's description:
    > [Payments] Enable payment handler on waterfall.
    > 
    > Before this patch, the waterfall was testing without payment handler
    > feature enabled by default.
    > 
    > This patch enables payment handler on the waterfall to thoroughly test
    > the payment handler feature before shipping it.
    > 
    > Note that, when the payment handler feature is enabled and the checkout
    > flow is aborted early (as in, before even showing any UI to the user),
    > then the journey logger does not log "user had a payment instrument" and
    > "user had all necessary information for payment" events, because
    > querying for installed payment handlers is asynchronous, whereas
    > autofill card query is synchronous.
    > 
    > After this patch, the waterfall is testing with the payment handler
    > feature enabled by default.
    > 
    > Bug: 773794
    > Change-Id: I69d032b4d3482185d8d099421c6fb7fd62f9a318
    > Reviewed-on: https://chromium-review.googlesource.com/713997
    > Commit-Queue: Mathieu Perreault <mathp@chromium.org>
    > Reviewed-by: Mathieu Perreault <mathp@chromium.org>
    > Reviewed-by: Dimitri Glazkov <dglazkov@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#511516}
    
    TBR=rouslan@chromium.org,mathp@chromium.org,dglazkov@chromium.org
    
    Change-Id: I2fb20f10b0cdaad34180f616bb6cde606eea05e4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 773794
    Reviewed-on: https://chromium-review.googlesource.com/737966
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511605}

--- third_party/WebKit/Source/platform/exported/WebRuntimeFeatures.cpp
+++ third_party/WebKit/Source/platform/exported/WebRuntimeFeatures.cpp
@@ -269,10 +269,6 @@ void WebRuntimeFeatures::EnablePassiveDocumentEventListeners(bool enable) {
   RuntimeEnabledFeatures::SetPassiveDocumentEventListenersEnabled(enable);
 }
 
-void WebRuntimeFeatures::EnablePaymentApp(bool enable) {
-  RuntimeEnabledFeatures::SetPaymentAppEnabled(enable);
-}
-
 void WebRuntimeFeatures::EnablePaymentRequest(bool enable) {
   RuntimeEnabledFeatures::SetPaymentRequestEnabled(enable);
 }
