44c7d56aab9440957b5274818ed7d56341c5d1de
Adrienne Walker <enne@chromium.org>
2017-10-25 17:16:13 +0000

    cc: Wrap alpha folding raster logic into an iterator
    The goal is to let this logic be reused for serialization.
    
    Bug: 758350
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I8ccb8a636a7ecce6231464a207f0878767d410a9
    Reviewed-on: https://chromium-review.googlesource.com/713439
    Commit-Queue: enne <enne@chromium.org>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511497}

--- cc/paint/paint_op_buffer.h
+++ cc/paint/paint_op_buffer.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <type_traits>
 
+#include "base/containers/stack_container.h"
 #include "base/debug/alias.h"
 #include "base/logging.h"
 #include "base/memory/aligned_memory.h"
@@ -841,7 +842,7 @@ class CC_PAINT_EXPORT PaintOpBuffer : public SkRefCnt {
     OffsetIterator(const PaintOpBuffer* buffer,
                    const std::vector<size_t>* offsets)
         : buffer_(buffer), ptr_(buffer_->data_.get()), offsets_(offsets) {
-      if (offsets->empty()) {
+      if (!offsets || offsets->empty()) {
         *this = end();
         return;
       }
@@ -940,6 +941,38 @@ class CC_PAINT_EXPORT PaintOpBuffer : public SkRefCnt {
     bool using_offsets_ = false;
     base::Optional<OffsetIterator> offset_iter_;
     base::Optional<Iterator> iter_;
+  };
+
+  class PlaybackFoldingIterator {
+   public:
+    PlaybackFoldingIterator(const PaintOpBuffer* buffer,
+                            const std::vector<size_t>* offsets);
+    ~PlaybackFoldingIterator();
+
+    const PaintOp* operator->() const { return current_op_; }
+    const PaintOp* operator*() const { return current_op_; }
+
+    PlaybackFoldingIterator& operator++() {
+      FindNextOp();
+      return *this;
+    }
+
+    operator bool() const { return !!current_op_; }
+
+    // Guaranteed to be 255 for all ops without flags.
+    uint8_t alpha() const { return current_alpha_; }
+
+   private:
+    void FindNextOp();
+    const PaintOp* NextUnfoldedOp();
+
+    PaintOpBuffer::CompositeIterator iter_;
+
+    // FIFO queue of paint ops that have been peeked at.
+    base::StackVector<const PaintOp*, 3> stack_;
+    DrawColorOp folded_draw_color_;
+    const PaintOp* current_op_ = nullptr;
+    uint8_t current_alpha_ = 255;
   };
 
  private:
