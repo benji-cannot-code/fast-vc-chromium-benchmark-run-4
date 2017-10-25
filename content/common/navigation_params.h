e22c153dbd93bd9b3957d129a74caa42698eccc9
David Jacobo <djacobo@chromium.org>
2017-10-25 02:28:06 +0000

    Add context menu flag to CommonNavigationParams
    Plumbing started_from_context_menu to CommonNavigationParams so it can
    be correctly seen by ArcNavigationThrottle even when PlzNavigate is
    enabled by default.
    
    Bug: 777990
    Test: Try.
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I969edbb39d0d12d2a4a6d7d26a2ef4e3d72a7b7a
    Reviewed-on: https://chromium-review.googlesource.com/736310
    Commit-Queue: David Jacobo <djacobo@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511332}

--- content/common/navigation_params.h
+++ content/common/navigation_params.h
@@ -71,7 +71,8 @@ struct CONTENT_EXPORT CommonNavigationParams {
                          std::string method,
                          const scoped_refptr<ResourceRequestBody>& post_data,
                          base::Optional<SourceLocation> source_location,
-                         CSPDisposition should_check_main_world_csp);
+                         CSPDisposition should_check_main_world_csp,
+                         bool started_from_context_menu);
   CommonNavigationParams(const CommonNavigationParams& other);
   ~CommonNavigationParams();
 
@@ -147,6 +148,9 @@ struct CONTENT_EXPORT CommonNavigationParams {
   // world which has initiated the navigation should be passed.
   // See https://crbug.com/702540
   CSPDisposition should_check_main_world_csp;
+
+  // Whether or not this navigation was started from a context menu.
+  bool started_from_context_menu;
 };
 
 // Provided by the renderer ----------------------------------------------------
