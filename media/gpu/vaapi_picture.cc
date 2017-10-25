e97883b5a93760105488a08fdc1874f61a2b6c5b
Miguel Casas-Sanchez <mcasas@chromium.org>
2017-10-25 15:23:21 +0000

    Move VAAPI static methods to impl classes
    This CL moves the factory method VaapiPicture::Create to the two impl
    classes vaapi_{drm,tfp}_picture, instead of having a single one with
    #ifdefs.
    
    Also in the process moved VaapiPicture::GetGLTextureTarget() which
    was in a similar situation, made a member const, inlined AllowOverlay()
    and changed LOG with DLOG()s in a file because:
     "prefer DVLOG(1) to other logging methods" [1]).
    
    [1] https://chromium.googlesource.com/chromium/src/+/master/styleguide/c++/c++.md#logging
    
    Bug: 778093
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I632acf8193ce052781b54ebed5d4957028635780
    Reviewed-on: https://chromium-review.googlesource.com/735743
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Kuang-che Wu <kcwu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511468}

--- media/gpu/vaapi_picture.cc
+++ media/gpu/vaapi_picture.cc
@@ -8,12 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/gl_bindings.h"
 #include "ui/gl/gl_implementation.h"
 
-#if defined(USE_X11)
-#include "media/gpu/vaapi_tfp_picture.h"
-#elif defined(USE_OZONE)
-#include "media/gpu/vaapi_drm_picture.h"
-#endif
-
 namespace media {
 
 VaapiPicture::VaapiPicture(
@@ -34,42 +28,6 @@ VaapiPicture::VaapiPicture(
 
 VaapiPicture::~VaapiPicture() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-}
-
-// static
-linked_ptr<VaapiPicture> VaapiPicture::CreatePicture(
-    const scoped_refptr<VaapiWrapper>& vaapi_wrapper,
-    const MakeGLContextCurrentCallback& make_context_current_cb,
-    const BindGLImageCallback& bind_image_cb,
-    int32_t picture_buffer_id,
-    const gfx::Size& size,
-    uint32_t texture_id,
-    uint32_t client_texture_id) {
-  linked_ptr<VaapiPicture> picture;
-#if defined(USE_X11)
-  picture.reset(new VaapiTFPPicture(vaapi_wrapper, make_context_current_cb,
-                                    bind_image_cb, picture_buffer_id, size,
-                                    texture_id, client_texture_id));
-#elif defined(USE_OZONE)
-  picture.reset(new VaapiDrmPicture(vaapi_wrapper, make_context_current_cb,
-                                    bind_image_cb, picture_buffer_id, size,
-                                    texture_id, client_texture_id));
-#endif  // USE_X11
-
-  return picture;
-}
-
-bool VaapiPicture::AllowOverlay() const {
-  return false;
-}
-
-// static
-uint32_t VaapiPicture::GetGLTextureTarget() {
-#if defined(USE_OZONE)
-  return GL_TEXTURE_EXTERNAL_OES;
-#else
-  return GL_TEXTURE_2D;
-#endif
 }
 
 }  // namespace media
