7a5a8dc4c0f8c68b06f489874bdc8994aa49ed06
Scott Graham <scottmg@chromium.org>
2017-10-25 04:01:02 +0000

    Enable chromium-style clang plugin on Fuchsia
    Bug: 777847
    Change-Id: Id237b07d559dc88605773607e681ab8fbb9991f2
    Reviewed-on: https://chromium-review.googlesource.com/734878
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Scott Graham <scottmg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511360}

--- base/message_loop/message_pump_fuchsia.h
+++ base/message_loop/message_pump_fuchsia.h
@@ -134,6 +134,7 @@ class BASE_EXPORT MessagePumpFuchsia : public MessagePump {
   };
 
   MessagePumpFuchsia();
+  ~MessagePumpFuchsia() override;
 
   bool WatchZxHandle(zx_handle_t handle,
                      bool persistent,
