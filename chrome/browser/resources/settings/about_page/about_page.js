ba3c76151fe4081c68054b818d0298c2a21be952
dpapad <dpapad@chromium.org>
2017-10-25 04:32:32 +0000

    Display help link when Chrome fails to update.
    Bug: 581085
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I676193a9f78591100cbb213f4f0012cebf1c1450
    Reviewed-on: https://chromium-review.googlesource.com/731305
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Dave Schuyler <dschuyler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511371}

--- chrome/browser/resources/settings/about_page/about_page.js
+++ chrome/browser/resources/settings/about_page/about_page.js
@@ -284,6 +284,14 @@ Polymer({
   },
 
   /**
+   * @return {boolean}
+   * @private
+   */
+  shouldShowLearnMoreLink_: function() {
+    return this.currentUpdateStatusEvent_.status == UpdateStatus.FAILED;
+  },
+
+  /**
    * @return {string}
    * @private
    */
