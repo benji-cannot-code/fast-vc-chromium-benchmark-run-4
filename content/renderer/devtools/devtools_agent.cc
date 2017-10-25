f61c7f35e6a3a50160867b72095c5b859502f007
Dmitry Gozman <dgozman@chromium.org>
2017-10-25 22:13:00 +0000

    Reland "[DevTools] Migrate DevToolsFrontendHost from IPC to Mojo"
    This is a reland of 10252c138c60cf614ecd7e3bfa2971dc46c8b5f8
    Original change's description:
    > [DevTools] Migrate DevToolsFrontendHost from IPC to Mojo
    > 
    > Bug: 776009
    > Change-Id: I700fbc3a999a3d1cad84572619a250c40ac9314a
    > Reviewed-on: https://chromium-review.googlesource.com/726369
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#511376}
    
    TBR=pfeldman@chromium.org
    
    Bug: 776009
    Change-Id: If68607acd73df7bdb9f264b117997283f945a5c8
    Reviewed-on: https://chromium-review.googlesource.com/738219
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511611}

--- content/renderer/devtools/devtools_agent.cc
+++ content/renderer/devtools/devtools_agent.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/devtools_messages.h"
 #include "content/common/frame_messages.h"
 #include "content/public/common/manifest.h"
-#include "content/renderer/devtools/devtools_client.h"
 #include "content/renderer/devtools/devtools_cpu_throttler.h"
 #include "content/renderer/manifest/manifest_manager.h"
 #include "content/renderer/render_frame_impl.h"
@@ -88,7 +87,6 @@ base::LazyInstance<IdToAgentMap>::Leaky
 
 DevToolsAgent::DevToolsAgent(RenderFrameImpl* frame)
     : RenderFrameObserver(frame),
-      is_devtools_client_(false),
       paused_(false),
       frame_(frame),
       weak_factory_(this) {
@@ -112,7 +110,6 @@ bool DevToolsAgent::OnMessageReceived(const IPC::Message& message) {
     IPC_MESSAGE_HANDLER(DevToolsAgentMsg_InspectElement, OnInspectElement)
     IPC_MESSAGE_HANDLER(DevToolsAgentMsg_RequestNewWindow_ACK,
                         OnRequestNewWindowACK)
-    IPC_MESSAGE_HANDLER(DevToolsMsg_SetupDevToolsClient, OnSetupDevToolsClient)
     IPC_MESSAGE_UNHANDLED(handled = false)
   IPC_END_MESSAGE_MAP()
 
@@ -282,14 +279,6 @@ void DevToolsAgent::OnRequestNewWindowACK(bool success) {
 
 void DevToolsAgent::ContinueProgram() {
   GetWebAgent()->ContinueProgram();
-}
-
-void DevToolsAgent::OnSetupDevToolsClient(const std::string& api_script) {
-  // We only want to register once; and only in main frame.
-  if (is_devtools_client_)
-    return;
-  is_devtools_client_ = true;
-  new DevToolsClient(frame_, api_script);
 }
 
 WebDevToolsAgent* DevToolsAgent::GetWebAgent() {
--- third_party/WebKit/Source/core/exported/WebDevToolsFrontendImpl.cpp
+++ third_party/WebKit/Source/core/exported/WebDevToolsFrontendImpl.cpp
@@ -60,6 +60,7 @@ WebDevToolsFrontendImpl::WebDevToolsFrontendImpl(
 }
 
 WebDevToolsFrontendImpl::~WebDevToolsFrontendImpl() {
+  web_frame_->SetDevToolsFrontend(nullptr);
   if (devtools_host_)
     devtools_host_->DisconnectClient();
 }
