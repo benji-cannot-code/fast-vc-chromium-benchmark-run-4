197f01246b2815229e8feaf36af0399aeff3ad7b
Jun Cai <juncai@chromium.org>
2017-10-25 19:32:40 +0000

    Reland: Refactor DeviceOrientationEventPump to use generic sensor as its backend
    The initial upload patch of this CL is the same as:
    https://chromium-review.googlesource.com/c/chromium/src/+/656139
    The above CL was reverted because of the issue 775874:
    https://bugs.chromium.org/p/chromium/issues/detail?id=775874
    Two CLs were landed to fix the above issue:
    https://chromium-review.googlesource.com/c/chromium/src/+/727239
    https://chromium-review.googlesource.com/c/chromium/src/+/730646
    
    And no code change has been made to the following patches of this CL and it is
    still the same as the above reverted CL.
    
    TBR=jam@chromium.org, reillyg@chromium.org, timvolodine@chromium.org
    
    Bug: 721427, 775874
    Change-Id: I9f64a97814644a467ec542b6293a6b44fc1b43c1
    Reviewed-on: https://chromium-review.googlesource.com/726261
    Reviewed-by: Jun Cai <juncai@chromium.org>
    Commit-Queue: Jun Cai <juncai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511553}

--- content/renderer/renderer_blink_platform_impl.cc
+++ content/renderer/renderer_blink_platform_impl.cc
@@ -81,6 +81,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/webpublicsuffixlist_impl.h"
 #include "content/renderer/worker_thread_registry.h"
 #include "device/gamepad/public/cpp/gamepads.h"
+#include "device/sensors/public/cpp/motion_data.h"
 #include "gpu/command_buffer/client/gles2_interface.h"
 #include "gpu/config/gpu_info.h"
 #include "gpu/ipc/client/gpu_channel_host.h"
@@ -1239,9 +1240,11 @@ RendererBlinkPlatformImpl::CreatePlatformEventObserverFromType(
     case blink::kWebPlatformEventTypeDeviceMotion:
       return base::MakeUnique<DeviceMotionEventPump>(thread);
     case blink::kWebPlatformEventTypeDeviceOrientation:
-      return base::MakeUnique<DeviceOrientationEventPump>(thread);
+      return base::MakeUnique<DeviceOrientationEventPump>(thread,
+                                                          false /* absolute */);
     case blink::kWebPlatformEventTypeDeviceOrientationAbsolute:
-      return base::MakeUnique<DeviceOrientationAbsoluteEventPump>(thread);
+      return base::MakeUnique<DeviceOrientationEventPump>(thread,
+                                                          true /* absolute */);
     case blink::kWebPlatformEventTypeGamepad:
       return base::MakeUnique<GamepadSharedMemoryReader>(thread);
     default:
--- services/device/device_service.cc
+++ services/device/device_service.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/single_thread_task_runner.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "build/build_config.h"
-#include "device/sensors/device_sensor_host.h"
 #include "mojo/public/cpp/system/message_pipe.h"
 #include "services/device/fingerprint/fingerprint.h"
 #include "services/device/generic_sensor/sensor_provider_impl.h"
@@ -86,11 +85,6 @@ DeviceService::~DeviceService() {
 void DeviceService::OnStart() {
   registry_.AddInterface<mojom::Fingerprint>(base::Bind(
       &DeviceService::BindFingerprintRequest, base::Unretained(this)));
-  registry_.AddInterface<mojom::OrientationSensor>(base::Bind(
-      &DeviceService::BindOrientationSensorRequest, base::Unretained(this)));
-  registry_.AddInterface<mojom::OrientationAbsoluteSensor>(
-      base::Bind(&DeviceService::BindOrientationAbsoluteSensorRequest,
-                 base::Unretained(this)));
   registry_.AddInterface<mojom::PowerMonitor>(base::Bind(
       &DeviceService::BindPowerMonitorRequest, base::Unretained(this)));
   registry_.AddInterface<mojom::ScreenOrientationListener>(
@@ -160,40 +154,6 @@ void DeviceService::BindVibrationManagerRequest(
 
 void DeviceService::BindFingerprintRequest(mojom::FingerprintRequest request) {
   Fingerprint::Create(std::move(request));
-}
-
-void DeviceService::BindOrientationSensorRequest(
-    mojom::OrientationSensorRequest request) {
-#if defined(OS_ANDROID)
-  // On Android the device sensors implementations need to run on the UI thread
-  // to communicate to Java.
-  DeviceOrientationHost::Create(std::move(request));
-#else
-  // On platforms other than Android the device sensors implementations run on
-  // the IO thread.
-  if (io_task_runner_) {
-    io_task_runner_->PostTask(
-        FROM_HERE,
-        base::Bind(&DeviceOrientationHost::Create, base::Passed(&request)));
-  }
-#endif  // defined(OS_ANDROID)
-}
-
-void DeviceService::BindOrientationAbsoluteSensorRequest(
-    mojom::OrientationAbsoluteSensorRequest request) {
-#if defined(OS_ANDROID)
-  // On Android the device sensors implementations need to run on the UI thread
-  // to communicate to Java.
-  DeviceOrientationAbsoluteHost::Create(std::move(request));
-#else
-  // On platforms other than Android the device sensors implementations run on
-  // the IO thread.
-  if (io_task_runner_) {
-    io_task_runner_->PostTask(FROM_HERE,
-                              base::Bind(&DeviceOrientationAbsoluteHost::Create,
-                                         base::Passed(&request)));
-  }
-#endif  // defined(OS_ANDROID)
 }
 
 void DeviceService::BindPowerMonitorRequest(
