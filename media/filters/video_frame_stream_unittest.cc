b03be22f9611c0f4841251450fc9b8a438a0a0d5
Dale Curtis <dalecurtis@chromium.org>
2017-10-25 04:16:02 +0000

    Add info MEDIA_LOG for decoder and decoder config changes.
    This adds a log for audio and video decoder and config changes. Samples:
    
    Selected FFmpegVideoDecoder for video decoding, config: codec: vp8 format: 2
    profile: unknown coded size: [332,249] visible rect: [0,0,320,240]
    natural size: [332,249] has extra data? false encrypted? true rotation: 0
    
    video decoder config changed midstream, new config: codec: vp8 format: 2
    profile: unknown coded size: [332,249] visible rect: [0,0,320,240]
    natural size: [332,249] has extra data? false encrypted? true rotation: 0
    
    BUG=516781
    TEST=new unittest.
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I6cc78993d3b47e0ce168cb5be1cfef0c403a1a9a
    Reviewed-on: https://chromium-review.googlesource.com/736064
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Matthew Wolenetz <wolenetz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511367}

--- media/filters/video_frame_stream_unittest.cc
+++ media/filters/video_frame_stream_unittest.cc
@@ -202,6 +202,9 @@ class VideoFrameStreamTest
       return;
     }
 
+    // Ensure there's a media log created whenever selecting a decoder.
+    EXPECT_MEDIA_LOG(HasSubstr("for video decoding, config"));
+
     std::string name = decoder->GetDisplayName();
     ASSERT_TRUE(GetDecoderName(0) == name || GetDecoderName(1) == name ||
                 GetDecoderName(2) == name);
@@ -365,8 +368,10 @@ class VideoFrameStreamTest
   void SatisfyPendingCallback(PendingState state) {
     DCHECK_NE(state, NOT_PENDING);
     switch (state) {
-      case DEMUXER_READ_NORMAL:
       case DEMUXER_READ_CONFIG_CHANGE:
+        EXPECT_MEDIA_LOG(HasSubstr("decoder config changed"))
+            .Times(testing::AtLeast(1));
+      case DEMUXER_READ_NORMAL:
         demuxer_stream_->SatisfyRead();
         break;
 
