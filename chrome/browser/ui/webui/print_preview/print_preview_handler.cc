2b1ea4258b3815661d9bb96eef876b4b04a94c1f
Lei Zhang <thestig@chromium.org>
2017-10-25 21:37:17 +0000

    Use recommended new base::Value APIs in print_preview code.
    Change-Id: I318844ec7b9e6683fa43c0fb862b58e6742f8819
    Reviewed-on: https://chromium-review.googlesource.com/737656
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511597}

--- chrome/browser/ui/webui/print_preview/print_preview_handler.cc
+++ chrome/browser/ui/webui/print_preview/print_preview_handler.cc
@@ -1021,7 +1021,7 @@ void PrintPreviewHandler::SendInitialSettings(
     initial_settings.SetString(kAppState,
                                *sticky_settings->printer_app_state());
   } else {
-    initial_settings.SetPath({kAppState}, base::Value());
+    initial_settings.SetKey(kAppState, base::Value());
   }
 
   base::CommandLine* cmdline = base::CommandLine::ForCurrentProcess();
@@ -1039,8 +1039,7 @@ void PrintPreviewHandler::SendInitialSettings(
     }
   }
   if (!set_rules) {
-    initial_settings.SetPath({kDefaultDestinationSelectionRules},
-                             base::Value());
+    initial_settings.SetKey(kDefaultDestinationSelectionRules, base::Value());
   }
 
   GetNumberFormatAndMeasurementSystem(&initial_settings);
