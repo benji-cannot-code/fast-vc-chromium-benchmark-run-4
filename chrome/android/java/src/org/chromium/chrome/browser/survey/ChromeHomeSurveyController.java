9611e8a70c918dc48fe14c3910914712b4e072b0
Daniel Park <danielpark@chromium.org>
2017-10-25 18:39:40 +0000

    [Chrome Home Survey] Remove advertising id parameter
    Bug: 774693
    Change-Id: I0799095393a63d06f9ad6eb7b262aaca7cab31f9
    Reviewed-on: https://chromium-review.googlesource.com/736831
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Daniel Park <danielpark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511534}

--- chrome/android/java/src/org/chromium/chrome/browser/survey/ChromeHomeSurveyController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/survey/ChromeHomeSurveyController.java
@@ -74,7 +74,7 @@ public class ChromeHomeSurveyController {
                 onSurveyAvailable(siteId);
             }
         };
-        surveyController.downloadSurvey(context, siteId, "", onSuccessRunnable);
+        surveyController.downloadSurvey(context, siteId, onSuccessRunnable);
     }
 
     private boolean doesUserQualifyForSurvey() {
