3ff4eec5171bc12c3262ace929ef029615915db2
Gauthier Ambard <gambard@chromium.org>
2017-10-25 12:26:07 +0000

    Store ClearCachedSuggestions calls while not Initialized
    If the RemoteSuggestionsProviderImpl receives a call to
    ClearCachedSuggestions while it is not initialized, it drops it
    silently.
    This CL makes it store the call to clear the suggestions once it is
    started.
    
    Bug: 776733
    Change-Id: Ic980d0ed7a6c7a57dde7017a55f17bdccc6afc91
    Reviewed-on: https://chromium-review.googlesource.com/730712
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511426}

--- components/ntp_snippets/remote/remote_suggestions_provider_impl.cc
+++ components/ntp_snippets/remote/remote_suggestions_provider_impl.cc
@@ -726,6 +726,7 @@ void RemoteSuggestionsProviderImpl::ClearHistory(
 
 void RemoteSuggestionsProviderImpl::ClearCachedSuggestions(Category category) {
   if (!initialized()) {
+    categories_clear_when_initialized_.insert(category);
     return;
   }
 
@@ -1515,6 +1516,13 @@ void RemoteSuggestionsProviderImpl::EnterState(State state) {
              category_contents_.end());
 
       UpdateAllCategoryStatus(CategoryStatus::AVAILABLE);
+
+      if (!categories_clear_when_initialized_.empty()) {
+        for (auto category : categories_clear_when_initialized_) {
+          ClearCachedSuggestions(category);
+        }
+        categories_clear_when_initialized_.clear();
+      }
       if (clear_history_dependent_state_when_initialized_) {
         clear_history_dependent_state_when_initialized_ = false;
         ClearHistoryDependentState();
@@ -1534,6 +1542,12 @@ void RemoteSuggestionsProviderImpl::EnterState(State state) {
       // suggestions below tells the scheduler to fetch them again if the
       // scheduler is not disabled. It is disabled; thus the calls are ignored.
       NotifyStateChanged();
+      if (!categories_clear_when_initialized_.empty()) {
+        for (auto category : categories_clear_when_initialized_) {
+          ClearCachedSuggestions(category);
+        }
+        categories_clear_when_initialized_.clear();
+      }
       if (clear_history_dependent_state_when_initialized_) {
         clear_history_dependent_state_when_initialized_ = false;
         ClearHistoryDependentState();
