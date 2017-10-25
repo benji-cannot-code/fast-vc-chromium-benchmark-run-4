4ad06d146579a7e5aa4919ef9017f01d09f2fb96
Kevin Cernekee <cernekee@chromium.org>
2017-10-25 17:40:18 +0000

    Hide ARC VPN behind a feature flag
    The UI work will be completed in the M64 timeframe, so for M63 we
    will require ARC VPN to be manually enabled via chrome://flags#arc-vpn
    
    BUG=696865
    TEST=manually test in both Enabled and Disabled state
    
    Change-Id: I5e362f2437629980c69dcfcef60a11786131e986
    Reviewed-on: https://chromium-review.googlesource.com/736817
    Commit-Queue: Kevin Cernekee <cernekee@chromium.org>
    Reviewed-by: Elijah Taylor <elijahtaylor@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511507}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1447,6 +1447,9 @@ extern const char kArcBootCompletedDescription[];
 extern const char kArcNativeBridgeExperimentName[];
 extern const char kArcNativeBridgeExperimentDescription[];
 
+extern const char kArcVpnName[];
+extern const char kArcVpnDescription[];
+
 extern const char kAshEnableUnifiedDesktopName[];
 extern const char kAshEnableUnifiedDesktopDescription[];
 
