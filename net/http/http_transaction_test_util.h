501023f4d5c1c0c246f44b6e7a4416f7ffc141f9
Shivani Sharma <shivanisha@chromium.org>
2017-10-25 21:14:55 +0000

    All truncate paths should check if entry should be truncated.
    The issue was coming because the checks whether to truncate or not
    were not getting checked in the following path:
    ASYNC_OP_COMPLETE_PRE_TRUNCATE => CACHE_WRITE_TRUNCATED_RESPONSE
    
    Bug: 778065
    Change-Id: I94d2c572bc823217bea02329f2007653cfb991bd
    Reviewed-on: https://chromium-review.googlesource.com/737576
    Commit-Queue: Shivani Sharma <shivanisha@chromium.org>
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511591}

--- net/http/http_transaction_test_util.h
+++ net/http/http_transaction_test_util.h
@@ -287,6 +287,7 @@ class MockNetworkTransaction
   unsigned int socket_log_id_;
 
   bool done_reading_called_;
+  bool reading_;
 
   CompletionCallback resume_start_callback_;  // used for pause and restart.
 
