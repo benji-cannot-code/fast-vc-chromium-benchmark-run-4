aa687ffd0106c33b199bc0ab3719bf193beee924
Xiaohan Wang <xhwang@chromium.org>
2017-10-25 03:19:33 +0000

    media: Delay MediaService destruction
    Currently we kill the MediaService right after the service becomes idle,
    i.e. all mojo media interface implementations are dropped. In cases
    where we reconnect to a mojo media interface immediately after all old
    interface connections are dropped (e.g. on cross-origin navigation), the
    MediaService instance will first be killed, and recreated immediately.
    If the service is running out-of-process (e.g. in a utility process),
    we'll kill the process first, and then relaunch the process, causing
    delays in interface connection time.
    
    This CL adds DelayedReleaseServiceContextRef to support delayed service
    destruction: the service will be destroyed only after a given delay (5
    seconds) since all references are dropped.
    
    Tested this on Mac Pro with a Debug build Chrome by switching between
    Youtube purchased movies and Shaka demo player, both needs to create
    Widevine CDM instances, hosted in CdmService (an alias of MediaService)
    in the utility process. The interface connection time is included in the
    Media.EME.Widevine.CreateCdmTime histogram. Here's the result:
    
    Histogram: Media.EME.Widevine.CreateCdmTime:
    - without this CL: recorded 20 samples, mean = 276.6 ms
    - with this CL: recorded 20 samples, mean = 100.8 ms
    
    BUG=510657
    TEST=Manually tested; see results above.
    
    Change-Id: Ie0b626d941ce21ce84bd4cd21f64b47d1438847c
    Reviewed-on: https://chromium-review.googlesource.com/736165
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511341}

--- media/mojo/services/interface_factory_impl.h
+++ media/mojo/services/interface_factory_impl.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace media {
 
 class CdmFactory;
+class DelayedReleaseServiceContextRef;
 class MediaLog;
 class MojoMediaClient;
 class RendererFactory;
@@ -71,7 +72,7 @@ class InterfaceFactoryImpl : public mojom::InterfaceFactory {
   mojo::StrongBindingSet<mojom::ContentDecryptionModule> cdm_bindings_;
 #endif  // BUILDFLAG(ENABLE_MOJO_CDM)
 
-  std::unique_ptr<service_manager::ServiceContextRef> connection_ref_;
+  std::unique_ptr<DelayedReleaseServiceContextRef> connection_ref_;
   MojoMediaClient* mojo_media_client_;
 
   DISALLOW_COPY_AND_ASSIGN(InterfaceFactoryImpl);
