f00e4fd8942afeb340432acb01cf34962a10ae7f
Dave Tapuska <dtapuska@chromium.org>
2017-10-25 13:14:55 +0000

    Fix flaky MouseLatencyBrowserTests
    Waiting on a synthetic gesture being completed is when the gesture has
    sent the events to the renderer not necessarily when it has processed
    them. Wait for MouseUp events before ending tracing.
    
    This test appears to be flaky when run with mojo input messages which
    has slightly different timing properties.
    
    BUG=722928
    
    Change-Id: I409c76b718051f75d4ff9e7309c146631fc546a8
    Reviewed-on: https://chromium-review.googlesource.com/733642
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511432}

--- content/browser/renderer_host/input/mouse_latency_browsertest.cc
+++ content/browser/renderer_host/input/mouse_latency_browsertest.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/render_widget_host_view.h"
 #include "content/public/browser/tracing_controller.h"
+#include "content/public/test/browser_test_utils.h"
 #include "content/public/test/content_browser_test.h"
 #include "content/public/test/content_browser_test_utils.h"
 #include "content/shell/browser/shell.h"
@@ -191,8 +192,12 @@ IN_PROC_BROWSER_TEST_F(MouseLatencyBrowserTest,
                        MAYBE_MouseDownAndUpRecordedWithoutSwap) {
   LoadURL();
 
+  auto filter = std::make_unique<InputMsgWatcher>(
+      GetWidgetHost(), blink::WebInputEvent::kMouseUp);
   StartTracing();
   DoSyncClick(gfx::PointF(100, 100));
+  EXPECT_EQ(INPUT_EVENT_ACK_STATE_CONSUMED,
+            filter->GetAckStateWaitIfNecessary());
   const base::Value& trace_data = StopTracing();
 
   const base::DictionaryValue* trace_data_dict;
@@ -238,9 +243,13 @@ IN_PROC_BROWSER_TEST_F(MouseLatencyBrowserTest,
                        MAYBE_CoalescedMouseMovesCorrectlyTerminated) {
   LoadURL();
 
+  auto filter = std::make_unique<InputMsgWatcher>(
+      GetWidgetHost(), blink::WebInputEvent::kMouseUp);
   StartTracing();
   DoSyncCoalescedMoves(gfx::PointF(100, 100), gfx::Vector2dF(150, 150),
                        gfx::Vector2dF(250, 250));
+  EXPECT_EQ(INPUT_EVENT_ACK_STATE_CONSUMED,
+            filter->GetAckStateWaitIfNecessary());
   const base::Value& trace_data = StopTracing();
 
   const base::DictionaryValue* trace_data_dict;
