9fb6f5896272444443bd17c6471f9ff6f0c17b4b
Philip Rogers <pdr@chromium.org>
2017-10-25 19:35:10 +0000

    [root layer scrolls] Make VisualViewport size RLS-aware
    This patch makes VisualViewport.{width, height} work with
    root layer scrolling (RLS) where the layout viewport
    scrollable area should be used to access the visible
    content rect. 
    
    This patch removes the scrollbar size codepath from
    LocalDomWindow so it is only used by inner{width, height}
    and can return the LocalFrameView's size without a layout
    in many cases. The scrollbar size codepaths in VisualViewport
    and DOMVisualViewport have been updated to use the correct
    visible content rect.
    
    With this patch, 11 more tests pass with RLS.
    
    Bug: 774711
    Change-Id: Idc2357fe12767bda6b3e0d94006141bf265203fd
    Reviewed-on: https://chromium-review.googlesource.com/729467
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511555}

--- third_party/WebKit/Source/core/frame/LocalDOMWindow.h
+++ third_party/WebKit/Source/core/frame/LocalDOMWindow.h
@@ -315,8 +315,6 @@ class CORE_EXPORT LocalDOMWindow final : public DOMWindow,
 
   void AcceptLanguagesChanged();
 
-  FloatSize GetViewportSize(IncludeScrollbarsInRect) const;
-
  protected:
   // EventTarget overrides.
   void AddedEventListener(const AtomicString& event_type,
@@ -341,6 +339,9 @@ class CORE_EXPORT LocalDOMWindow final : public DOMWindow,
 
   void DispatchLoadEvent();
   void ClearDocument();
+
+  // Return the viewport size including scrollbars.
+  IntSize GetViewportSize() const;
 
   TraceWrapperMember<Document> document_;
   Member<DOMVisualViewport> visualViewport_;
