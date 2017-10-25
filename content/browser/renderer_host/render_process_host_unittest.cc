c74a04c3491a6003364f74463f7fcd206ff32269
Sonny Rao <sonnyrao@chromium.org>
2017-10-25 04:38:36 +0000

    Raise renderer process limit on Chrome OS for all boards
    After running an experiment on Caroline with the renderer process
    limit raised to 100, we saw a decrease in the overall number of tab
    discards and no increase in the number of kernel OOM kills, so this
    seems like a better policy that makes the tab discarder more
    effective.
    
    Bug: 743170
    Change-Id: I83e1ee996e028608dc906896e15cc60afe512e5b
    Reviewed-on: https://chromium-review.googlesource.com/729495
    Commit-Queue: Sonny Rao <sonnyrao@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511372}

--- content/browser/renderer_host/render_process_host_unittest.cc
+++ content/browser/renderer_host/render_process_host_unittest.cc
@@ -49,7 +49,7 @@ TEST_F(RenderProcessHostUnitTest, GuestsAreNotSuitableHosts) {
       RenderProcessHost::GetExistingProcessHost(browser_context(), test_url));
 }
 
-#if !defined(OS_ANDROID)
+#if !defined(OS_ANDROID) && !defined(OS_CHROMEOS)
 TEST_F(RenderProcessHostUnitTest, RendererProcessLimit) {
   // This test shouldn't run with --site-per-process mode, which prohibits
   // the renderer process reuse this test explicitly exercises.
@@ -78,7 +78,7 @@ TEST_F(RenderProcessHostUnitTest, RendererProcessLimit) {
 }
 #endif
 
-#if defined(OS_ANDROID)
+#if defined(OS_ANDROID) || defined(OS_CHROMEOS)
 TEST_F(RenderProcessHostUnitTest, NoRendererProcessLimitOnAndroid) {
   // Disable any overrides.
   RenderProcessHostImpl::SetMaxRendererProcessCount(0);
