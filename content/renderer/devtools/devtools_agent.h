498784fad4d9cf2a238f9a6346255323b0dcc58e
Dmitry Gozman <dgozman@chromium.org>
2017-10-25 05:12:24 +0000

    [DevTools] Migrate DevToolsFrontendHost from IPC to Mojo
    Bug: 776009
    Change-Id: I700fbc3a999a3d1cad84572619a250c40ac9314a
    Reviewed-on: https://chromium-review.googlesource.com/726369
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511376}

--- content/renderer/devtools/devtools_agent.h
+++ content/renderer/devtools/devtools_agent.h
@@ -90,7 +90,6 @@ class CONTENT_EXPORT DevToolsAgent : public RenderFrameObserver,
   void OnInspectElement(int session_id, int x, int y);
   void OnRequestNewWindowACK(bool success);
   void ContinueProgram();
-  void OnSetupDevToolsClient(const std::string& api_script);
 
   void GotManifest(int session_id,
                    int command_id,
@@ -99,7 +98,6 @@ class CONTENT_EXPORT DevToolsAgent : public RenderFrameObserver,
                    const ManifestDebugInfo& debug_info);
 
   std::set<int> session_ids_;
-  bool is_devtools_client_;
   bool paused_;
   RenderFrameImpl* frame_;
   base::Callback<void(int, int, const std::string&, const std::string&)>
