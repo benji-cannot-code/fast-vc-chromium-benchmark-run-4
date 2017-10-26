614fbd2d55406b42306169e653f8ae50cb616004
Marc-Antoine Courteau <macourteau@chromium.org>
2017-10-26 15:32:09 +0000

    [Payment Request] Improve display of region names in address editor.
    Screenshot: https://goo.gl/mJ8P6D
    
    This CL creates an EditorViewOption protocol that can be implemented by
    objects provided as options to the PaymentRequestEditConsumer, which now
    accepts both NSString and EditorViewoption objects.
    
    We now also preserve the ordering of regions returned by the
    RegionDataLoader.
    
    This is a second attempt at landing this. The first one was reverted because it
    broke the PaymentRequestRegionDataLoaderTest.SourceSuccess test. The block for
    checking the passed NSArray<RegionData*>* was using == instead of
    isEqualToString: to compare the strings.
    
    R=mahmadi
    
    Bug: 
    Change-Id: If5a561c1795ee97d69feccd1d2f5fb3f4b63e3f2
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    Reviewed-on: https://chromium-review.googlesource.com/739641
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Marc-Antoine Courteau <macourteau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511826}

--- ios/chrome/browser/ui/payments/region_data_loader_unittest.mm
+++ ios/chrome/browser/ui/payments/region_data_loader_unittest.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/logging.h"
+#include "base/mac/foundation_util.h"
 #include "base/strings/sys_string_conversions.h"
 #include "components/autofill/core/browser/test_region_data_loader.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -41,10 +42,20 @@ TEST_F(PaymentRequestRegionDataLoaderTest, SourceSuccess) {
   // Mock the consumer.
   id consumer =
       [OCMockObject mockForProtocol:@protocol(RegionDataLoaderConsumer)];
-  [[consumer expect] regionDataLoaderDidSucceedWithRegions:@{
-    base::SysUTF8ToNSString(kQuebecCode) : base::SysUTF8ToNSString(kQuebec),
-    base::SysUTF8ToNSString(kOntarioCode) : base::SysUTF8ToNSString(kOntario)
-  }];
+  [[consumer expect]
+      regionDataLoaderDidSucceedWithRegions:[OCMArg checkWithBlock:^BOOL(
+                                                        id value) {
+        NSArray<RegionData*>* data =
+            base::mac::ObjCCastStrict<NSArray<RegionData*>>(value);
+        return [data[0].regionCode
+                   isEqualToString:base::SysUTF8ToNSString(kQuebecCode)] &&
+               [data[0].regionName
+                   isEqualToString:base::SysUTF8ToNSString(kQuebec)] &&
+               [data[1].regionCode
+                   isEqualToString:base::SysUTF8ToNSString(kOntarioCode)] &&
+               [data[1].regionName
+                   isEqualToString:base::SysUTF8ToNSString(kOntario)];
+      }]];
 
   RegionDataLoader region_data_loader(consumer);
   region_data_loader.LoadRegionData("some country",
@@ -63,7 +74,7 @@ TEST_F(PaymentRequestRegionDataLoaderTest, SourceFailure) {
   // Mock the consumer.
   id consumer =
       [OCMockObject mockForProtocol:@protocol(RegionDataLoaderConsumer)];
-  [[consumer expect] regionDataLoaderDidSucceedWithRegions:@{}];
+  [[consumer expect] regionDataLoaderDidSucceedWithRegions:@[]];
 
   RegionDataLoader region_data_loader(consumer);
   region_data_loader.LoadRegionData("some country",
