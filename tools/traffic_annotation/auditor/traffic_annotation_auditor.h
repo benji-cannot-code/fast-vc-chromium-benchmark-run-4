739d101a5f7433e16b9f0e4469ca8846d628e7e5
Ramin Halavati <rhalavati@chromium.org>
2017-10-26 09:39:59 +0000

    Annotation.xml update mechanism and errors updated.
    Traffic annotation auditor was updated as follows:
    1- If a new annotation is found, it is added to annotations.xml for all
       supported platforms, to minimize the need to run the auditor on all
       platforms to updated os_list.
    2- If annotation.xml requires updates and it is run in test mode, the
       list of updates are added to output errors.
    
    Bug: 656607
    Bug: 690323
    Change-Id: Icbdceddfc9a3e31cf9be397e19b002597119bb61
    Reviewed-on: https://chromium-review.googlesource.com/730687
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511773}

--- tools/traffic_annotation/auditor/traffic_annotation_auditor.h
+++ tools/traffic_annotation/auditor/traffic_annotation_auditor.h
@@ -130,7 +130,7 @@ class TrafficAnnotationAuditor {
     return extracted_calls_;
   }
 
-  const std::vector<AuditorResult>& errors() const { return errors_; }
+  std::vector<AuditorResult> errors() { return errors_; }
 
   void ClearErrorsForTesting() { errors_.clear(); }
 
