9e9f206d9a038b42939a7fd9dae9137a400ba033
Ella Ge <eirage@chromium.org>
2017-10-26 15:09:29 -0400

    Disable legacy touch event APIs on desktop
    In this cl, we make touch-events feature in chrome://flag
    default to be disabled.
    
    TouchEvent APIs:
    - ontouch* members on window, document, Element
    - document.createTouch, document.createTouchList
    - document.createEvent("TouchEvent")
    will be disabled by default on desktop.
    
    Intent to Deprecate and Remove:
    https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/KV6kqDJpYiE
    
    Bug: 392584
    Change-Id: I76716f3c4e529e5143bace95ae1b637b461c2ff5
    Reviewed-on: https://chromium-review.googlesource.com/729220
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Reviewed-by: Navid Zolghadr <nzolghadr@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526001}

--- chrome/browser/prefs/chrome_pref_service_unittest.cc
+++ chrome/browser/prefs/chrome_pref_service_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/memory/ptr_util.h"
 #include "base/strings/utf_string_conversions.h"
+#include "build/build_config.h"
 #include "chrome/browser/prefs/chrome_command_line_pref_store.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
@@ -100,4 +101,11 @@ TEST_F(ChromePrefServiceWebKitPrefs, PrefsCopied) {
   EXPECT_EQ(base::ASCIIToUTF16(kDefaultFont),
             webkit_prefs.standard_font_family_map[prefs::kWebKitCommonScript]);
   EXPECT_TRUE(webkit_prefs.javascript_enabled);
+
+#if defined(OS_ANDROID)
+  // Touch event enabled only on Android.
+  EXPECT_TRUE(webkit_prefs.touch_event_feature_detection_enabled);
+#else
+  EXPECT_FALSE(webkit_prefs.touch_event_feature_detection_enabled);
+#endif
 }
