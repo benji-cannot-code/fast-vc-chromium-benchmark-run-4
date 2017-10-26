3fabc9d1cc40742b307855a7d6ddd8cf70879b68
Peter Beverloo <peter@chromium.org>
2017-10-26 18:15:25 +0000

    Don't use -1 in Mac OS native notification action index responses
    The negative value used to represent that no action button was used, but
    that has since been changed to use base::Optional instead. This is
    causing code on Mac to unintentionally believe that a button was used
    anyway, which can lead to unexpected behaviour.
    
    BUG=774983
    
    Change-Id: I8c13aff77b77772077c9145b705d84904eb67a55
    Reviewed-on: https://chromium-review.googlesource.com/730753
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511882}

--- chrome/browser/notifications/notification_platform_bridge_mac.mm
+++ chrome/browser/notifications/notification_platform_bridge_mac.mm
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/scoped_mach_port.h"
 #include "base/mac/scoped_nsobject.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/optional.h"
 #include "base/strings/nullable_string16.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/sys_string_conversions.h"
@@ -374,6 +375,12 @@ void NotificationPlatformBridgeMac::ProcessNotificationResponse(
   NSNumber* notification_type =
       [response objectForKey:notification_constants::kNotificationType];
 
+  base::Optional<int> action_index;
+  if (button_index.intValue !=
+      notification_constants::kNotificationInvalidButtonIndex) {
+    action_index = button_index.intValue;
+  }
+
   content::BrowserThread::PostTask(
       content::BrowserThread::UI, FROM_HERE,
       base::Bind(DoProcessNotificationResponse,
@@ -382,8 +389,8 @@ void NotificationPlatformBridgeMac::ProcessNotificationResponse(
                  static_cast<NotificationCommon::Type>(
                      notification_type.unsignedIntValue),
                  profile_id, [is_incognito boolValue], notification_origin,
-                 notification_id, button_index.intValue /* action_index */,
-                 base::nullopt /* reply */, true /* by_user */));
+                 notification_id, action_index, base::nullopt /* reply */,
+                 true /* by_user */));
 }
 
 // static
@@ -411,7 +418,8 @@ bool NotificationPlatformBridgeMac::VerifyNotificationData(
   NSNumber* notification_type =
       [response objectForKey:notification_constants::kNotificationType];
 
-  if (button_index.intValue < -1 ||
+  if (button_index.intValue <
+          notification_constants::kNotificationInvalidButtonIndex ||
       button_index.intValue >=
           static_cast<int>(blink::kWebNotificationMaxActions)) {
     LOG(ERROR) << "Invalid number of buttons supplied "
