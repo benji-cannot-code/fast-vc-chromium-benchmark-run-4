f41cd269f681334a62c43349e7f6a4928aa46d5b
Wez <wez@chromium.org>
2017-10-26 20:57:53 +0000

    Add missing ScopedAllowBlocking to TabManagerDelegateChromeos.
    TabManagerDelegateChromeos reads the "low memory margin" and "available"
    memory values from kernel-provided entries in the filesystem, when
    determining how many processes need to be discarded to ease memory
    pressure. This causes AssertIOAllowed to fail if discards ever trigger
    in a build with DCHECKs enabled.
    
    In principle the reads from these kernel-provided files should not block
    so add a ScopedAllowBlocking to work-around the issue.
    
    Bug: 
    Change-Id: I472f7fca8a4780e6a6abceb0a3baff541c8e3a9b
    Reviewed-on: https://chromium-review.googlesource.com/738224
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Cheng-Yu Lee <cylee@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511951}

--- base/threading/thread_restrictions.h
+++ base/threading/thread_restrictions.h
@@ -93,6 +93,10 @@ namespace remoting {
 class AutoThread;
 }
 
+namespace resource_coordinator {
+class TabManagerDelegate;
+}
+
 namespace ui {
 class WindowResizeHelperMac;
 }
@@ -189,6 +193,7 @@ class BASE_EXPORT ScopedAllowBlocking {
   FRIEND_TEST_ALL_PREFIXES(ThreadRestrictionsTest, ScopedAllowBlocking);
   friend class cronet::CronetPrefsManager;
   friend class cronet::CronetURLRequestContextAdapter;
+  friend class resource_coordinator::TabManagerDelegate;  // crbug.com/778703
   friend class ScopedAllowBlockingForTesting;
 
   ScopedAllowBlocking() EMPTY_BODY_IF_DCHECK_IS_OFF;
