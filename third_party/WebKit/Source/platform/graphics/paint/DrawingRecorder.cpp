d43b1688fb6b6bd9ab943377177f6e993e14d413
Walter Korman <wkorman@chromium.org>
2017-10-26 23:24:53 +0000

    Remove last vestige of strict cull rect clipping REF.
    Bug: 771401
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I1e202776666de132d1dc13bb8a5df018e57bb0cb
    Reviewed-on: https://chromium-review.googlesource.com/740020
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512008}

--- third_party/WebKit/Source/platform/graphics/paint/DrawingRecorder.cpp
+++ third_party/WebKit/Source/platform/graphics/paint/DrawingRecorder.cpp
@@ -54,9 +54,6 @@ DrawingRecorder::~DrawingRecorder() {
     return;
 
 #if DCHECK_IS_ON()
-  if (RuntimeEnabledFeatures::SlimmingPaintStrictCullRectClippingEnabled())
-    context_.Restore();
-
   context_.SetInDrawingRecorder(false);
 
   if (!g_list_modification_check_disabled) {
@@ -68,8 +65,7 @@ DrawingRecorder::~DrawingRecorder() {
   sk_sp<const PaintRecord> picture = context_.EndRecording();
 
 #if DCHECK_IS_ON()
-  if (!RuntimeEnabledFeatures::SlimmingPaintStrictCullRectClippingEnabled() &&
-      !context_.GetPaintController().IsForPaintRecordBuilder() &&
+  if (!context_.GetPaintController().IsForPaintRecordBuilder() &&
       client_.PaintedOutputOfObjectHasNoEffectRegardlessOfSize()) {
     DCHECK_EQ(0u, picture->size()) << client_.DebugName();
   }
