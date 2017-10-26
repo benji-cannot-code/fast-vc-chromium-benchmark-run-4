ba65e6aed22db0354e4a18051e8c36536d72a8cf
Scott Graham <scottmg@chromium.org>
2017-10-26 02:00:13 +0000

    Don't gncheck #include of Linux header in cross-platform code
    scottmg@around:/work/cr/src$ gn gen out/fuchgncheck --args='target_os="fuchsia" is_chromecast=true' --check
    ERROR at //third_party/WebKit/Source/platform/wtf/typed_arrays/ArrayBufferContents.cpp:32:11: Include not allowed.
              ^---------------------------------------
    It is not in any dependency of
      //third_party/WebKit/Source/platform/wtf:wtf
    The include file is in the target(s):
      //sandbox/linux:sandbox_services
    which should somehow be reachable.
    
    Bug: 707031, 772488
    Change-Id: If109ffa0365a5ad676f1c580b4f0ea117c32eb23
    Reviewed-on: https://chromium-review.googlesource.com/738819
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Scott Graham <scottmg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511692}

--- third_party/WebKit/Source/platform/wtf/typed_arrays/ArrayBufferContents.cpp
+++ third_party/WebKit/Source/platform/wtf/typed_arrays/ArrayBufferContents.cpp
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string.h>
 #if defined(OS_LINUX)
-#include "sandbox/linux/services/resource_limits.h"
+#include "sandbox/linux/services/resource_limits.h"  // nogncheck
 #endif
 #include "base/allocator/partition_allocator/partition_alloc.h"
 #include "platform/wtf/Assertions.h"
