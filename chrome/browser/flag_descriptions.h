382118f00e26cd587ec1711040a6a78fe4d18379
Michael Thiessen <mthiesse@chromium.org>
2017-10-26 16:25:10 +0000

    VR: Skip redrawing the frame if nothing needs to be redrawn.
    This CL allows (behind a flag) VR Shell to skip rendering frames if it
    decides that nothing has been dirtied, and the head movement isn't
    significant enough to warrant re-rendering.
    
    The controller doesn't trigger re-rendering currently, so that we can
    see the performance difference ignoring the controller. Figuring out
    whether the controller needs redrawing will be done in followup CLs.
    
    Bug: 776521
    Change-Id: I81183127e0cc78aaa5d91146dd2118f5254cc9b5
    Reviewed-on: https://chromium-review.googlesource.com/738529
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511841}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1676,6 +1676,9 @@ extern const char kEnableVrShellDescription[];
 extern const char kVrCustomTabBrowsingName[];
 extern const char kVrCustomTabBrowsingDescription[];
 
+extern const char kVrShellExperimentalRenderingName[];
+extern const char kVrShellExperimentalRenderingDescription[];
+
 extern const char kWebVrAutopresentName[];
 extern const char kWebVrAutopresentDescription[];
 
--- chrome/common/chrome_features.h
+++ chrome/common/chrome_features.h
@@ -258,6 +258,7 @@ extern const base::Feature kVoiceSearchOnLocalNtp;
 #endif
 
 extern const base::Feature kVrShell;
+extern const base::Feature kVrShellExperimentalRendering;
 
 #if defined(OS_CHROMEOS)
 extern const base::Feature kOptInImeMenu;
