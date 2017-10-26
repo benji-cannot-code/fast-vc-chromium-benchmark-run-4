c14aba3b819c22757c2bec656611b0379d4140c1
Steve Kobes <skobes@chromium.org>
2017-10-26 03:34:38 +0000

    RLS: Mark main frame scrollbar layers opaque.
    This puts "contentsOpaque": true on the document scrollbar in the layer tree
    output of compositing/fixed-body-background-positioned.html, seen in pending
    rebaseline http://crrev.com/c/738925.
    
    Bug: 544140
    Change-Id: If7c0c9b66207a084d22846fa8bcc2bc4a1664dd9
    Reviewed-on: https://chromium-review.googlesource.com/738822
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511727}

--- third_party/WebKit/Source/core/page/scrolling/ScrollingCoordinator.cpp
+++ third_party/WebKit/Source/core/page/scrolling/ScrollingCoordinator.cpp
@@ -434,7 +434,6 @@ void ScrollingCoordinator::ScrollableAreaScrollbarLayerDidChange(
   if (!page_ || !page_->MainFrame())
     return;
 
-  bool is_main_frame = IsForMainFrame(scrollable_area);
   GraphicsLayer* scrollbar_graphics_layer =
       orientation == kHorizontalScrollbar
           ? scrollable_area->LayerForHorizontalScrollbar()
@@ -482,8 +481,8 @@ void ScrollingCoordinator::ScrollableAreaScrollbarLayerDidChange(
     // Root layer non-overlay scrollbars should be marked opaque to disable
     // blending.
     bool is_opaque_scrollbar = !scrollbar.IsOverlayScrollbar();
-    scrollbar_graphics_layer->SetContentsOpaque(is_main_frame &&
-                                                is_opaque_scrollbar);
+    scrollbar_graphics_layer->SetContentsOpaque(
+        IsForMainFrame(scrollable_area) && is_opaque_scrollbar);
   } else {
     RemoveWebScrollbarLayer(scrollable_area, orientation);
   }
@@ -1219,7 +1218,9 @@ bool ScrollingCoordinator::IsForMainFrame(
     return false;
 
   // FIXME(305811): Refactor for OOPI.
-  return scrollable_area == page_->DeprecatedLocalMainFrame()->View();
+  return scrollable_area == page_->DeprecatedLocalMainFrame()
+                                ->View()
+                                ->LayoutViewportScrollableArea();
 }
 
 void ScrollingCoordinator::FrameViewRootLayerDidChange(
