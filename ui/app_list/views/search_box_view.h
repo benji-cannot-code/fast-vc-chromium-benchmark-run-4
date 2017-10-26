1c6b7618d0b03fc7637daa145f2d0fea207b6d7c
Weidong Guo <weidongg@chromium.org>
2017-10-26 00:38:49 +0000

    applist-focus: Make search box and app list view crossable for ChromeVox
    Changes:
    Change the default role of search box's root view and native view and
    app list view's root view from AX_ROLE_WINDOW to AX_ROLE_GROUP, so that
    search box and app list view become crossable for ChromeVox focus.
    
    BUG=761124
    
    Change-Id: I8f0477c181d939d72a7d4e2960900ae336fd84c0
    Reviewed-on: https://chromium-review.googlesource.com/729341
    Commit-Queue: Weidong Guo <weidongg@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511665}

--- ui/app_list/views/search_box_view.h
+++ ui/app_list/views/search_box_view.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/button/image_button.h"
 #include "ui/views/controls/textfield/textfield_controller.h"
 #include "ui/views/view.h"
+#include "ui/views/widget/widget_delegate.h"
 
 namespace views {
 class BoxLayout;
@@ -49,7 +50,7 @@ class SearchBoxImageButton;
 // model that controls what icon to display, what placeholder text to use for
 // Textfield. The text and selection model part could be set to change the
 // contents and selection model of the Textfield.
-class APP_LIST_EXPORT SearchBoxView : public views::View,
+class APP_LIST_EXPORT SearchBoxView : public views::WidgetDelegateView,
                                       public views::TextfieldController,
                                       public views::ButtonListener,
                                       public SearchBoxModelObserver,
@@ -119,6 +120,10 @@ class APP_LIST_EXPORT SearchBoxView : public views::View,
   void OnGestureEvent(ui::GestureEvent* event) override;
   void OnMouseEvent(ui::MouseEvent* event) override;
   void OnKeyEvent(ui::KeyEvent* evetn) override;
+
+  // Overridden from views::WidgetDelegate:
+  ui::AXRole GetAccessibleWindowRole() const override;
+  bool ShouldAdvanceFocusToTopLevelWidget() const override;
 
   // Overridden from views::ButtonListener:
   void ButtonPressed(views::Button* sender, const ui::Event& event) override;
