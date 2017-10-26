06c9e40a52b655cadaae804492a8fa0af4bba10f
Steve Kobes <skobes@chromium.org>
2017-10-26 01:24:14 +0000

    Let callers of GitCL.trigger_try_jobs specify the master.
    Bug: 755401
    Change-Id: I4cfb6fcd4a124def9c9bd5861a627939b1c015a0
    Reviewed-on: https://chromium-review.googlesource.com/734190
    Reviewed-by: Quinten Yearsley <qyearsley@chromium.org>
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511682}

--- third_party/WebKit/Tools/Scripts/webkitpy/common/net/git_cl_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/net/git_cl_unittest.py
@@ -61,6 +61,7 @@ class GitCLTest(unittest.TestCase):
         self.assertEqual(host.executive.calls, [
             [
                 'git', 'cl', 'try',
+                '-m', 'tryserver.chromium.android',
                 '-b', 'android_blink_rel',
                 '--auth-refresh-token-json', 'token.json'
             ],
@@ -82,6 +83,7 @@ class GitCLTest(unittest.TestCase):
         self.assertEqual(host.executive.calls, [
             [
                 'git', 'cl', 'try',
+                '-m', 'tryserver.chromium.android',
                 '-b', 'android_blink_rel',
                 '--auth-refresh-token-json', 'token.json'
             ],
