79bc5740127426a63878750aed35da9d06109602
Will Chen <chenwilliam@chromium.org>
2017-10-26 03:33:35 +0000

    DevTools: automatically migrate http/tests/devtools/tracing/*.html
    This converts a batch of tests from the old test framework (html file)
    into the new test framework (js file).
    
    Most of it was done through an automated transformation script, with
    a small amount of manual clean-up.
    
    Bug: 667560
    Change-Id: I7d17b6da361a3a40d98783605e9685200f44522f
    Reviewed-on: https://chromium-review.googlesource.com/731794
    Commit-Queue: Will Chen <chenwilliam@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511726}

--- third_party/WebKit/LayoutTests/http/tests/devtools/tracing/buffer-usage.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/tracing/buffer-usage.js
@@ -1,11 +1,20 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-<html>
-<head>
-<script src="../../inspector/inspector-test.js"></script>
-<script src="../../inspector/timeline-test.js"></script>
-<script>
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
-function test() {
+(async function() {
+  TestRunner.addResult(
+      `Tests that buffer usage update are sent when recording trace events and TimelineLifecycleDelegate methods are properly invoked in the expected order.\n`);
+  await TestRunner.loadModule('performance_test_runner');
+  await TestRunner.showPanel('timeline');
+  await TestRunner.loadHTML(`
+      <p>
+      Tests that buffer usage update are sent when recording trace events and
+      TimelineLifecycleDelegate methods are properly invoked in the expected order.
+      </p>
+    `);
+
   TestTimelineControllerClient = function() {
     this._hadLoadingProgress = false;
   };
@@ -45,15 +54,4 @@ function test() {
   controller.startRecording({}, []).then(() => {
     TestRunner.addResult('TimelineControllerClient.recordingStarted');
   });
-}
-
-</script>
-</head>
-
-<body onload="runTest()">
-<p>
-Tests that buffer usage update are sent when recording trace events and
-TimelineLifecycleDelegate methods are properly invoked in the expected order.
-</p>
-</body>
-</html>
+})();
--- third_party/WebKit/LayoutTests/http/tests/devtools/tracing/frame-model.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/tracing/frame-model.js
@@ -1,11 +1,13 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-<html>
-<head>
-<script src="../../inspector/inspector-test.js"></script>
-<script src="../../inspector/timeline-test.js"></script>
-<script src="../tracing-test.js"></script>
-<script>
-function test() {
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(async function() {
+  TestRunner.addResult(`Test the frames are correctly built based on trace events\n`);
+  await TestRunner.loadModule('performance_test_runner');
+  await TestRunner.showPanel('timeline');
+
   var sessionId = '4.20';
   var mainThread = 1;
   var implThread = 2;
@@ -1850,15 +1852,4 @@ function test() {
     }
   }
   TestRunner.completeTest();
-}
-
-</script>
-</head>
-
-<body onload="runTest()">
-<p>
-Test the frames are correctly built based on trace events
-</p>
-
-</body>
-</html>
+})();
