c07c958a2a13e8bf47c6f15e2200d8d689e72b99
Daniele Castagna <dcastagna@chromium.org>
2017-10-26 19:23:49 +0000

    Add XR30 native pixmap GpuMemoryBuffer support.
    This adds a new BGRX_1010102 buffer format that uses 10 bits per
    component.
    
    On some boards (e.g: Kaby Lake) we can texture from it, use it as
    a render target, and scanout (with dithering).
    
    Mesa needs to be updated with some patches from upstream to support
    creating an image with XR30.
    
    Bug: 776093
    Test: ozone_gl_unittests --gtest_also_run_disabled_tests with patched mesa.
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Id21007b4c3bc13e83f09a2e9f55459c1dcd1811f
    Reviewed-on: https://chromium-review.googlesource.com/738555
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: David Reveman <reveman@chromium.org>
    Commit-Queue: Daniele Castagna <dcastagna@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511918}

--- ui/gfx/buffer_format_util.cc
+++ ui/gfx/buffer_format_util.cc
@@ -12,24 +12,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace gfx {
 namespace {
 
-const BufferFormat kBufferFormats[] = {BufferFormat::ATC,
-                                       BufferFormat::ATCIA,
-                                       BufferFormat::DXT1,
-                                       BufferFormat::DXT5,
-                                       BufferFormat::ETC1,
-                                       BufferFormat::R_8,
-                                       BufferFormat::R_16,
-                                       BufferFormat::RG_88,
-                                       BufferFormat::BGR_565,
-                                       BufferFormat::RGBA_4444,
-                                       BufferFormat::RGBX_8888,
-                                       BufferFormat::RGBA_8888,
-                                       BufferFormat::BGRX_8888,
-                                       BufferFormat::BGRA_8888,
-                                       BufferFormat::RGBA_F16,
-                                       BufferFormat::UYVY_422,
-                                       BufferFormat::YUV_420_BIPLANAR,
-                                       BufferFormat::YVU_420};
+const BufferFormat kBufferFormats[] = {
+    BufferFormat::ATC,       BufferFormat::ATCIA,
+    BufferFormat::DXT1,      BufferFormat::DXT5,
+    BufferFormat::ETC1,      BufferFormat::R_8,
+    BufferFormat::R_16,      BufferFormat::RG_88,
+    BufferFormat::BGR_565,   BufferFormat::RGBA_4444,
+    BufferFormat::RGBX_8888, BufferFormat::RGBA_8888,
+    BufferFormat::BGRX_8888, BufferFormat::BGRX_1010102,
+    BufferFormat::BGRA_8888, BufferFormat::RGBA_F16,
+    BufferFormat::UYVY_422,  BufferFormat::YUV_420_BIPLANAR,
+    BufferFormat::YVU_420};
 
 static_assert(arraysize(kBufferFormats) ==
                   (static_cast<int>(BufferFormat::LAST) + 1),
@@ -70,6 +63,7 @@ bool RowSizeForBufferFormatChecked(
       *size_in_bytes = (checked_size & ~0x3).ValueOrDie();
       return true;
     case BufferFormat::BGRX_8888:
+    case BufferFormat::BGRX_1010102:
     case BufferFormat::RGBX_8888:
     case BufferFormat::RGBA_8888:
     case BufferFormat::BGRA_8888:
@@ -119,6 +113,7 @@ size_t NumberOfPlanesForBufferFormat(BufferFormat format) {
     case BufferFormat::RGBX_8888:
     case BufferFormat::RGBA_8888:
     case BufferFormat::BGRX_8888:
+    case BufferFormat::BGRX_1010102:
     case BufferFormat::BGRA_8888:
     case BufferFormat::RGBA_F16:
     case BufferFormat::UYVY_422:
@@ -147,6 +142,7 @@ size_t SubsamplingFactorForBufferFormat(BufferFormat format, size_t plane) {
     case BufferFormat::RGBX_8888:
     case BufferFormat::RGBA_8888:
     case BufferFormat::BGRX_8888:
+    case BufferFormat::BGRX_1010102:
     case BufferFormat::BGRA_8888:
     case BufferFormat::RGBA_F16:
     case BufferFormat::UYVY_422:
@@ -220,6 +216,7 @@ size_t BufferOffsetForBufferFormat(const Size& size,
     case BufferFormat::RGBX_8888:
     case BufferFormat::RGBA_8888:
     case BufferFormat::BGRX_8888:
+    case BufferFormat::BGRX_1010102:
     case BufferFormat::BGRA_8888:
     case BufferFormat::RGBA_F16:
     case BufferFormat::UYVY_422:
--- ui/gl/gl_image_io_surface.mm
+++ ui/gl/gl_image_io_surface.mm
@@ -67,6 +67,7 @@ bool ValidFormat(gfx::BufferFormat format) {
     case gfx::BufferFormat::BGR_565:
     case gfx::BufferFormat::RGBA_4444:
     case gfx::BufferFormat::RGBX_8888:
+    case gfx::BufferFormat::BGRX_1010102:
     case gfx::BufferFormat::YVU_420:
       return false;
   }
@@ -100,6 +101,7 @@ GLenum TextureFormat(gfx::BufferFormat format) {
     case gfx::BufferFormat::BGR_565:
     case gfx::BufferFormat::RGBA_4444:
     case gfx::BufferFormat::RGBX_8888:
+    case gfx::BufferFormat::BGRX_1010102:
     case gfx::BufferFormat::YVU_420:
       NOTREACHED();
       return 0;
@@ -133,6 +135,7 @@ GLenum DataFormat(gfx::BufferFormat format) {
     case gfx::BufferFormat::BGR_565:
     case gfx::BufferFormat::RGBA_4444:
     case gfx::BufferFormat::RGBX_8888:
+    case gfx::BufferFormat::BGRX_1010102:
     case gfx::BufferFormat::YVU_420:
     case gfx::BufferFormat::YUV_420_BIPLANAR:
       NOTREACHED();
@@ -167,6 +170,7 @@ GLenum DataType(gfx::BufferFormat format) {
     case gfx::BufferFormat::BGR_565:
     case gfx::BufferFormat::RGBA_4444:
     case gfx::BufferFormat::RGBX_8888:
+    case gfx::BufferFormat::BGRX_1010102:
     case gfx::BufferFormat::YVU_420:
     case gfx::BufferFormat::YUV_420_BIPLANAR:
       NOTREACHED();
