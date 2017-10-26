037234e88eb56d6b0b5566096c19bcc0f4322702
Maria Khomenko <mariakhomenko@chromium.org>
2017-10-26 23:45:21 +0000

    Metric for Chrome custom logic vs isLowRamDevice.
    Detect how frequently the two definitions are identical. This will be
    used to decide if we should switch to system-based implementation.
    
    BUG=711079
    
    Change-Id: Ibbb12df3cbf1523fd0e2fe5bb63a9ac3d91ea7cd
    Reviewed-on: https://chromium-review.googlesource.com/738863
    Commit-Queue: Maria Khomenko <mariakhomenko@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512021}

--- base/android/java/src/org/chromium/base/metrics/RecordHistogram.java
+++ base/android/java/src/org/chromium/base/metrics/RecordHistogram.java
@@ -7,6 +7,7 @@ package org.chromium.base.metrics;
 
 import org.chromium.base.VisibleForTesting;
 import org.chromium.base.annotations.JNINamespace;
+import org.chromium.base.annotations.MainDex;
 
 import java.util.Collections;
 import java.util.HashMap;
@@ -25,6 +26,7 @@ import java.util.concurrent.TimeUnit;
  * code.
  */
 @JNINamespace("base::android")
+@MainDex
 public class RecordHistogram {
     private static Throwable sDisabledBy;
     private static Map<String, Long> sCache =
