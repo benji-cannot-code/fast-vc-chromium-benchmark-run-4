03eb3bd008b3077c868d8df76d12321d6493c114
Elodie Banel <lod@google.com>
2017-10-26 09:12:19 +0000

    Reland Use autolayout and safearealayoutguide for form input accessory view.
    Initialisation of view after adding to parent view resulted in
    the view not having accessibility labels correctly set on iOS 9.
    An alternative fix was to call "setNeedsLayout" after initialisation.
    
    Bug: 766672
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I1bdcfb13d628e383078dfb27b7a9404b6de08233
    Reviewed-on: https://chromium-review.googlesource.com/737880
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Elodie Banel <lod@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511769}

--- ios/chrome/browser/autofill/form_input_accessory_view_controller.mm
+++ ios/chrome/browser/autofill/form_input_accessory_view_controller.mm
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/autofill/form_suggestion_view.h"
 #import "ios/chrome/browser/passwords/password_generation_utils.h"
 #include "ios/chrome/browser/ui/ui_util.h"
+#import "ios/chrome/browser/ui/util/constraints_ui_util.h"
 #import "ios/web/public/url_scheme_util.h"
 #import "ios/web/public/web_state/js/crw_js_injection_receiver.h"
 #import "ios/web/public/web_state/ui/crw_web_view_proxy.h"
@@ -203,7 +204,7 @@ bool ComputeFramesOfKeyboardParts(UIView* inputAccessoryView,
   CGRect _keyboardFrame;
 
   // The custom view that should be shown in the input accessory view.
-  UIView* _customAccessoryView;
+  FormInputAccessoryView* _customAccessoryView;
 
   // The JS manager for interacting with the underlying form.
   JsSuggestionManager* _JSSuggestionManager;
@@ -368,12 +369,12 @@ bool ComputeFramesOfKeyboardParts(UIView* inputAccessoryView,
                                      &rightFrame)) {
       [self hideSubviewsInOriginalAccessoryView:inputAccessoryView];
       _customAccessoryView =
-          [[FormInputAccessoryView alloc] initWithFrame:inputAccessoryView.frame
-                                               delegate:self
-                                             customView:view
-                                              leftFrame:leftFrame
-                                             rightFrame:rightFrame];
+          [[FormInputAccessoryView alloc] initWithDelegate:self];
+      [_customAccessoryView initializeViewWithCustomView:view
+                                               leftFrame:leftFrame
+                                              rightFrame:rightFrame];
       [inputAccessoryView addSubview:_customAccessoryView];
+      AddSameConstraints(_customAccessoryView, inputAccessoryView);
     }
   }
 }
