f4f3dbde957e0b01e148a90ab70a472d83f9b744
Michael McGreevy <mcgreevy@chromium.org>
2017-10-26 04:59:35 +0000

    Stop passing params to LogErrorToConsole().
    LogErrorToConsole() ostensibly hides from callers the details of the error
    messages that it logs, requiring callers to simply supply an
    InstallableStatusCode. However, in some cases the error message is
    parameterized by an int value, which is supplied via the "param" parameter.
    This requires callers to have knowledge of the content of the error message
    format strings.
    
    This CL modifies LogErrorToConsole so that it knows how to obtain the data
    required to create the error messages for the two status codes that need it,
    removing the need for callers to know anything about the error message format
    strings.
    
    Bug: 
    Change-Id: I099caf34887bd877ba05c649f433661dfd9725df
    Reviewed-on: https://chromium-review.googlesource.com/737755
    Commit-Queue: Michael McGreevy <mcgreevy@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511741}

--- chrome/browser/banners/app_banner_manager.cc
+++ chrome/browser/banners/app_banner_manager.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/feature_list.h"
 #include "base/metrics/histogram_macros.h"
-#include "base/strings/string_number_conversions.h"
 #include "base/time/time.h"
 #include "chrome/browser/banners/app_banner_metrics.h"
 #include "chrome/browser/banners/app_banner_settings_helper.h"
@@ -73,15 +72,6 @@ class AppBannerManager::StatusReporter {
 
 namespace {
 
-// Returns a string parameter for a devtools console message corresponding to
-// |code|. Returns the empty string if |code| requires no parameter.
-std::string GetStatusParam(InstallableStatusCode code) {
-  if (code == NO_ACCEPTABLE_ICON || code == MANIFEST_MISSING_SUITABLE_ICON)
-    return base::IntToString(InstallableManager::GetMinimumIconSizeInPx());
-
-  return std::string();
-}
-
 // Logs installable status codes to the console.
 class ConsoleStatusReporter : public banners::AppBannerManager::StatusReporter {
  public:
@@ -89,7 +79,7 @@ class ConsoleStatusReporter : public banners::AppBannerManager::StatusReporter {
   // attached to |web_contents|.
   void ReportStatus(content::WebContents* web_contents,
                     InstallableStatusCode code) override {
-    LogErrorToConsole(web_contents, code, GetStatusParam(code));
+    LogErrorToConsole(web_contents, code);
   }
 };
 
