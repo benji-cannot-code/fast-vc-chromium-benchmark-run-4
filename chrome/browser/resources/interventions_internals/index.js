8322de664b3e3c245d1a720aef1ff8ee0c761f9a
Thanh Le <thanhdle@chromium.org>
2017-10-26 19:55:31 +0000

    Fix clear log button behavior, and onBlacklistCleared not removing all table rows.
    Added test to check the behavior of onBlacklistCleared.
    
    Bug: 778713
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I8f199df33434d38d7b5e82a5e0daa8dd8405fb75
    Reviewed-on: https://chromium-review.googlesource.com/739947
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Thanh Le <thanhdle@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511931}

--- chrome/browser/resources/interventions_internals/index.js
+++ chrome/browser/resources/interventions_internals/index.js
@@ -97,7 +97,7 @@ function setupLogClear() {
     // Remove hosts from table.
     let logsTable = $('message-logs-table');
     let rows = logsTable.querySelectorAll('.log-message');
-    for (let row = rows.length - 1; row > 0; row--) {
+    for (let row = rows.length; row > 0; row--) {
       logsTable.deleteRow(row);
     }
   });
@@ -180,8 +180,8 @@ InterventionsInternalPageImpl.prototype = {
    * Unix epoch.
    */
   onUserBlacklistedStatusChange: function(blacklisted) {
-    let userBlacklistedStatus = $('user-blacklisted-status');
-    userBlacklistedStatus.textContent = 'User blacklisted status: ' +
+    let userBlacklistedStatus = $('user-blacklisted-status-value');
+    userBlacklistedStatus.textContent =
         (blacklisted ? 'Blacklisted' : 'Not blacklisted');
   },
 
@@ -193,14 +193,13 @@ InterventionsInternalPageImpl.prototype = {
    * epoch.
    */
   onBlacklistCleared: function(time) {
-    let blacklistClearedStatus = $('blacklist-cleared-status');
-    blacklistClearedStatus.textContent =
-        'Last blacklist cleared: ' + getTimeFormat(time);
+    let blacklistClearedStatus = $('blacklist-last-cleared-time');
+    blacklistClearedStatus.textContent = getTimeFormat(time);
 
     // Remove hosts from table.
     let blacklistedHostsTable = $('blacklisted-hosts-table');
     let rows = blacklistedHostsTable.querySelectorAll('.blacklisted-host-row');
-    for (let row = rows.length - 1; row > 0; row--) {
+    for (let row = rows.length; row > 0; row--) {
       blacklistedHostsTable.deleteRow(row);
     }
   },
