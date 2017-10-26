45278605dde0f66222820a7c6c1ccd95fa5f466b
Xianzhu Wang <wangxianzhu@chromium.org>
2017-10-26 03:36:37 +0000

    [SPv175] Pass layer state to PaintArtifact::Replay() in GraphicsLayer::CaptureRecord()
    Previously the root property state was used causing extra clip,
    transform etc. applied around the actual record.
    
    This fixes the raster under-invalidation false-positives.
    
    Bug: 771643
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I00d31e9f633b06c3ab06719987631bd10cc0f199
    Reviewed-on: https://chromium-review.googlesource.com/736024
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511728}

--- third_party/WebKit/Source/platform/graphics/GraphicsLayer.h
+++ third_party/WebKit/Source/platform/graphics/GraphicsLayer.h
@@ -286,6 +286,9 @@ class PLATFORM_EXPORT GraphicsLayer : public cc::LayerClient,
 
   void SetLayerState(PropertyTreeState&&, const IntPoint& layer_offset);
 
+  // Capture the last painted result into a PaintRecord.
+  sk_sp<PaintRecord> CapturePaintRecord() const;
+
  protected:
   String DebugName(cc::Layer*) const;
   bool ShouldFlattenTransform() const { return should_flatten_transform_; }
@@ -339,8 +342,6 @@ class PLATFORM_EXPORT GraphicsLayer : public cc::LayerClient,
   void AddTransformJSONProperties(JSONObject&, RenderingContextMap&) const;
   void AddFlattenInheritedTransformJSON(JSONObject&) const;
   class LayersAsJSONArray;
-
-  sk_sp<PaintRecord> CaptureRecord();
 
   Vector<const PaintChunk*> AllChunkPointers() const;
   CompositedLayerRasterInvalidator& EnsureRasterInvalidator();
