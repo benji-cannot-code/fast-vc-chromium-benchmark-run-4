c8d106a61f7e8e0dda7c6caef47ac9bdc551502e
Dan Elphick <delphick@chromium.org>
2017-10-26 22:36:38 +0000

    [Background Fetch] Decouple storage lifetime from active jobs
    (Taken over from johnme@'s
    https://chromium-review.googlesource.com/c/chromium/src/+/723306).
    
    Previously, 1) marking a registration as no longer active (i.e.
    completed, failed or aborted), 2) deleting its JobController, and 3)
    deleting the registration from the DataManager, were all tightly coupled
    and happened simultaneously.
    
    This made it impossible to support the spec's requirement that
    registration data continue to be stored (and gettable) for as long as a
    reference is kept to one or more JavaScript BackgroundFetchRegistration
    objects, even if that registration has become inactive and another
    registration has been made in the meantime with the same |developer_id|.
    
    This patch decouples marking a registration as no longer active (aka.
    marking for deletion) from both deleting the JobController and deleting
    the registration data.
    
    Now marking the registration as no longer active is always the first
    step, and failure here is used to avoid race conditions between a) jobs
    completing/failing, b) jobs being aborted by the user, and c) jobs being
    aborted by the developer: now the first of these to mark it as inactive
    wins.
    
    Next, the JobController is deleted, but only once it's no longer
    necessary, so for the completed/failed events we have to wait until the
    event's waitUntil promise has been resolved.
    
    Finally, there are two ways the registration data can be deleted.
    
    1. Once JavaScript BackgroundFetchRegistration objects can no longer be
    obtained for a registration, and all existing ones have been garbage
    collected (we can count them since they each register a mojo observer
    with the RegistrationNotifier), the registration data will be deleted.
    (This doesn't work completely yet, but will be fixed in a follow-up
    change).
    
    2. After every browser restart, it is safe to delete registrations
    marked for deletion, since JavaScript can no longer have references to
    them. So the DataManager runs a Cleanup task with PostAfterStartupTask.
    
    Bug: 757760
    Change-Id: I6cfe1d32c9405c79afbd826839479548a9878514
    Reviewed-on: https://chromium-review.googlesource.com/735600
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Dan Elphick <delphick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511990}

--- content/browser/background_fetch/background_fetch_service_unittest.cc
+++ content/browser/background_fetch/background_fetch_service_unittest.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 namespace {
 
+const char kExampleUniqueId[] = "7e57ab1e-c0de-a150-ca75-1e75f005ba11";
 const char kExampleDeveloperId[] = "my-background-fetch";
 const char kAlternativeDeveloperId[] = "my-alternative-fetch";
 
@@ -87,12 +88,15 @@ class BackgroundFetchServiceTest : public BackgroundFetchTestBase {
   }
 
   // Synchronous wrapper for BackgroundFetchServiceImpl::Abort().
-  void Abort(const std::string& unique_id,
+  void Abort(int64_t service_worker_registration_id,
+             const std::string& developer_id,
+             const std::string& unique_id,
              blink::mojom::BackgroundFetchError* out_error) {
     DCHECK(out_error);
 
     base::RunLoop run_loop;
-    service_->Abort(unique_id,
+    service_->Abort(service_worker_registration_id, origin(), developer_id,
+                    unique_id,
                     base::BindOnce(&BackgroundFetchServiceTest::DidGetError,
                                    base::Unretained(this),
                                    run_loop.QuitClosure(), out_error));
@@ -592,7 +596,8 @@ TEST_F(BackgroundFetchServiceTest, Abort) {
   blink::mojom::BackgroundFetchError abort_error;
 
   // Immediately abort the registration. This also is expected to succeed.
-  Abort(registration_id.unique_id(), &abort_error);
+  Abort(service_worker_registration_id, kExampleDeveloperId,
+        registration_id.unique_id(), &abort_error);
   ASSERT_EQ(abort_error, blink::mojom::BackgroundFetchError::NONE);
   // Wait for the response of the Mojo IPC to dispatch
   // BackgroundFetchAbortEvent.
@@ -607,13 +612,25 @@ TEST_F(BackgroundFetchServiceTest, Abort) {
   ASSERT_EQ(second_error, blink::mojom::BackgroundFetchError::INVALID_ID);
 }
 
+TEST_F(BackgroundFetchServiceTest, AbortInvalidDeveloperIdArgument) {
+  // This test verifies that the Abort() function will kill the renderer and
+  // return INVALID_ARGUMENT when an invalid |developer_id| is sent over the
+  // Mojo channel.
+
+  blink::mojom::BackgroundFetchError error;
+  Abort(42 /* service_worker_registration_id */, "" /* developer_id */,
+        kExampleUniqueId, &error);
+  ASSERT_EQ(error, blink::mojom::BackgroundFetchError::INVALID_ARGUMENT);
+}
+
 TEST_F(BackgroundFetchServiceTest, AbortInvalidUniqueIdArgument) {
   // This test verifies that the Abort() function will kill the renderer and
   // return INVALID_ARGUMENT when an invalid |unique_id| is sent over the Mojo
   // channel.
 
   blink::mojom::BackgroundFetchError error;
-  Abort("not a GUID" /* unique_id */, &error);
+  Abort(42 /* service_worker_registration_id */, kExampleDeveloperId,
+        "not a GUID" /* unique_id */, &error);
   ASSERT_EQ(error, blink::mojom::BackgroundFetchError::INVALID_ARGUMENT);
 }
 
@@ -622,7 +639,8 @@ TEST_F(BackgroundFetchServiceTest, AbortUnknownUniqueId) {
   // |unique_id| that does not correspond to an active fetch kindly tells us so.
 
   blink::mojom::BackgroundFetchError error;
-  Abort("7e57ab1e-c0de-a150-ca75-1e75f005ba11" /* unique_id */, &error);
+  Abort(42 /* service_worker_registration_id */, kExampleDeveloperId,
+        kExampleUniqueId, &error);
   ASSERT_EQ(error, blink::mojom::BackgroundFetchError::INVALID_ID);
 }
 
@@ -668,7 +686,8 @@ TEST_F(BackgroundFetchServiceTest, AbortEventDispatch) {
   {
     blink::mojom::BackgroundFetchError error;
 
-    Abort(registration_id.unique_id(), &error);
+    Abort(service_worker_registration_id, kExampleDeveloperId,
+          registration_id.unique_id(), &error);
     ASSERT_EQ(error, blink::mojom::BackgroundFetchError::NONE);
   }
 
@@ -709,7 +728,8 @@ TEST_F(BackgroundFetchServiceTest, UniqueId) {
   // Immediately abort the registration so it is no longer active (everything
   // that follows should behave the same if the registration had completed
   // instead of being aborted).
-  Abort(aborted_registration_id.unique_id(), &error);
+  Abort(service_worker_registration_id, kExampleDeveloperId,
+        aborted_registration_id.unique_id(), &error);
   ASSERT_EQ(blink::mojom::BackgroundFetchError::NONE, error);
   // Wait for response of the Mojo IPC to dispatch BackgroundFetchAbortEvent.
   base::RunLoop().RunUntilIdle();
@@ -758,7 +778,8 @@ TEST_F(BackgroundFetchServiceTest, UniqueId) {
 
   // Aborting the previously aborted registration should fail with INVALID_ID
   // since it is no longer active.
-  Abort(aborted_registration_id.unique_id(), &error);
+  Abort(service_worker_registration_id, kExampleDeveloperId,
+        aborted_registration_id.unique_id(), &error);
   EXPECT_EQ(blink::mojom::BackgroundFetchError::INVALID_ID, error);
   // Wait for response of the Mojo IPC to dispatch BackgroundFetchAbortEvent.
   // (MockBackgroundFetchDelegate won't complete/fail second_registration in the
@@ -772,7 +793,8 @@ TEST_F(BackgroundFetchServiceTest, UniqueId) {
   EXPECT_EQ(second_registration.unique_id, gotten_registration.unique_id);
 
   // Aborting the second registration should succeed.
-  Abort(second_registration_id.unique_id(), &error);
+  Abort(service_worker_registration_id, kExampleDeveloperId,
+        second_registration_id.unique_id(), &error);
   EXPECT_EQ(blink::mojom::BackgroundFetchError::NONE, error);
   // Wait for response of the Mojo IPC to dispatch BackgroundFetchAbortEvent.
   // (MockBackgroundFetchDelegate won't complete/fail second_registration in the
