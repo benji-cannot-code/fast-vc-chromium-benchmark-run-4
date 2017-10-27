a584b225e7aae6e1488eafcfe77c0aa3b1349ff6
Nicholas Verne <nverne@chromium.org>
2017-10-27 10:20:44 +0000

    removed unused LocationSnapshot class
    Bug: 775817
    Change-Id: Iace54f25b3ac98c15055f9cd9b28806e5eb536a6
    Reviewed-on: https://chromium-review.googlesource.com/737739
    Commit-Queue: Nicholas Verne <nverne@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512143}

--- content/common/child_process_messages.h
+++ content/common/child_process_messages.h
@@ -33,12 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/platform_thread.h"
 #endif
 
-IPC_STRUCT_TRAITS_BEGIN(base::LocationSnapshot)
-  IPC_STRUCT_TRAITS_MEMBER(file_name)
-  IPC_STRUCT_TRAITS_MEMBER(function_name)
-  IPC_STRUCT_TRAITS_MEMBER(line_number)
-IPC_STRUCT_TRAITS_END()
-
 #if defined(OS_LINUX)
 IPC_ENUM_TRAITS_MAX_VALUE(base::ThreadPriority,
                           base::ThreadPriority::REALTIME_AUDIO)
