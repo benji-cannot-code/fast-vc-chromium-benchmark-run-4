ef350bf0fb4d7f6e45a10914ddf9af0a221fdee7
Eric Seckler <eseckler@chromium.org>
2017-10-27 13:35:13 +0000

    [headless] Fix use after free in HeadlessContentBrowserClient
    Bug: 778725
    Change-Id: Ica03ed97177357e76bb0ee12608fa7e34aa7862c
    Reviewed-on: https://chromium-review.googlesource.com/741585
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512168}

--- headless/lib/browser/headless_content_browser_client.h
+++ headless/lib/browser/headless_content_browser_client.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/public/browser/content_browser_client.h"
 #include "headless/lib/browser/headless_resource_dispatcher_host_delegate.h"
+#include "headless/public/headless_browser.h"
 
 namespace headless {
 
@@ -77,6 +78,10 @@ class HeadlessContentBrowserClient : public content::ContentBrowserClient {
   std::unique_ptr<base::Value> GetPackagedServicesServiceManifestOverlay();
 
   HeadlessBrowserImpl* browser_;  // Not owned.
+
+  // We store the callback here because we may call it from the I/O thread.
+  HeadlessBrowser::Options::AppendCommandLineFlagsCallback
+      append_command_line_flags_callback_;
 
   std::unique_ptr<HeadlessResourceDispatcherHostDelegate>
       resource_dispatcher_host_delegate_;
