0f77f34bf7c5643aff691ca2002f4a50a2ff1b6c
Emily Stark <estark@google.com>
2017-10-27 22:13:14 +0000

    Allow whitespace in Expect-CT preflight headers
    It's common for sites to serve
    
    Access-Control-Allow-Methods: GET, POST
    
    instead of "GET,POST". So we should allow whitespace when parsing
    CORS response headers in Expect-CT preflight responses.
    
    Bug: 
    Change-Id: I919ab51c30a11979a6f2c074da6e5efb018423f0
    Reviewed-on: https://chromium-review.googlesource.com/741464
    Commit-Queue: Emily Stark <estark@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512321}

--- chrome/browser/ssl/chrome_expect_ct_reporter.h
+++ chrome/browser/ssl/chrome_expect_ct_reporter.h
@@ -68,6 +68,8 @@ class ChromeExpectCTReporter
   FRIEND_TEST_ALL_PREFIXES(ChromeExpectCTReporterTest, EmptyReportURI);
   FRIEND_TEST_ALL_PREFIXES(ChromeExpectCTReporterTest, SendReport);
   FRIEND_TEST_ALL_PREFIXES(ChromeExpectCTReporterTest,
+                           PreflightContainsWhitespace);
+  FRIEND_TEST_ALL_PREFIXES(ChromeExpectCTReporterTest,
                            BadCORSPreflightResponseOrigin);
   FRIEND_TEST_ALL_PREFIXES(ChromeExpectCTReporterTest,
                            BadCORSPreflightResponseMethods);
