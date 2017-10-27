7266b9625cdd931d80f3a293f702a0de6105fd16
Xianzhu Wang <wangxianzhu@chromium.org>
2017-10-27 01:44:56 +0000

    [run-webkit-tests] Let --reset-results reset flag-specific baselines
    If --additional-driver-flag is specified, it was weird to still let
    --reset-results reset the existing baselines. There seems no use case
    for that.
    
    Now let --reset-results reset flag-specific baselines if --additional-
    driver-flag is specified.
    
    Bug: 660231
    Change-Id: I6ee5a5f563a7f972fcece763256d7f99f3e97cbe
    Reviewed-on: https://chromium-review.googlesource.com/740841
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512061}

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/run_webkit_tests_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/run_webkit_tests_unittest.py
@@ -1261,7 +1261,6 @@ class RebaselineTest(unittest.TestCase, StreamTestingMixin):
             'text-image-checksum_fail-txt')
         details, log_stream, _ = logging_run(
             ['--additional-driver-flag=--flag',
-             '--copy-baselines',
              '--reset-results',
              'failures/unexpected/text-image-checksum.html'],
             tests_included=True, host=host)
@@ -1320,7 +1319,6 @@ class RebaselineTest(unittest.TestCase, StreamTestingMixin):
 
         details, log_stream, _ = logging_run(
             ['--additional-driver-flag=--flag',
-             '--copy-baselines',
              '--reset-results',
              'failures/unexpected/text-image-checksum.html'],
             tests_included=True, host=host)
