fea686bdb944609f8ef2f22795ddb9454cf03692
Sonny Rao <sonnyrao@chromium.org>
2017-10-27 20:59:13 +0000

    Fix naming of RenderProcessHostUnitTest for Android and Chrome OS
    Previously we raised the limit of renderer processes on Chrome OS to
    be much higher and then had to change Chrome OS builds to use the same
    unit test as Android -- However, I neglected to properly rename that
    test, so this is fixing that.
    
    Bug: 743170
    Change-Id: I59b2ba5781ebb17362f0c8876722be109fe3d438
    Reviewed-on: https://chromium-review.googlesource.com/741069
    Reviewed-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Sonny Rao <sonnyrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512267}

--- content/browser/renderer_host/render_process_host_unittest.cc
+++ content/browser/renderer_host/render_process_host_unittest.cc
@@ -79,7 +79,7 @@ TEST_F(RenderProcessHostUnitTest, RendererProcessLimit) {
 #endif
 
 #if defined(OS_ANDROID) || defined(OS_CHROMEOS)
-TEST_F(RenderProcessHostUnitTest, NoRendererProcessLimitOnAndroid) {
+TEST_F(RenderProcessHostUnitTest, NoRendererProcessLimitOnAndroidOrChromeOS) {
   // Disable any overrides.
   RenderProcessHostImpl::SetMaxRendererProcessCount(0);
 
