56798a3e370691c1fd1c8fb8c8cb5d5cbdc62ad9
Robert Sesek <rsesek@chromium.org>
2017-10-27 22:16:21 +0000

    [Mac] Fix a use-after-free in -[TabController menu].
    This can occur if the accessibility system calls -[TabController menu]
    when the menu is already open. Because the TabController recreates
    the menu on each invocation, it was deleting the C++ model object that
    was still in use.
    
    Bug: 778776
    Test: unit_tests TabControllerTest.RecursiveMenu under ASan
    Change-Id: I030c3da740ce8696cee5a51aebe931dacf4456c6
    Reviewed-on: https://chromium-review.googlesource.com/740302
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512323}

--- chrome/browser/ui/cocoa/test/menu_test_observer.mm
+++ chrome/browser/ui/cocoa/test/menu_test_observer.mm
@@ -44,21 +44,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   isOpen_ = YES;
   didOpen_ = YES;
 
-  if (openCallback_)
-    openCallback_(self);
-
-  if (closeAfterOpening_) {
-    NSArray* modes = @[ NSEventTrackingRunLoopMode, NSDefaultRunLoopMode ];
-    [menu_ performSelector:@selector(cancelTracking)
-                withObject:nil
-                afterDelay:0
-                   inModes:modes];
-  }
+  // Post the callback to the runloop, since in this notification callback,
+  // the menu may not be fully in its tracking mode yet.
+  NSArray* modes = @[ NSEventTrackingRunLoopMode, NSDefaultRunLoopMode ];
+  [self performSelector:@selector(performOpenTasks)
+             withObject:nil
+             afterDelay:0
+                inModes:modes];
 }
 
 - (void)menuDidEndTracking:(NSNotification*)notif {
   DCHECK_EQ(menu_, [notif object]);
   isOpen_ = NO;
+}
+
+- (void)performOpenTasks {
+  DCHECK(isOpen_);
+
+  if (openCallback_)
+    openCallback_(self);
+
+  if (closeAfterOpening_)
+    [menu_ cancelTracking];
 }
 
 @end
