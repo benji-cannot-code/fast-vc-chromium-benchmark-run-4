49018f023abb88acc51b08b7bf02f4be8524e108
Sriram <srirama.m@samsung.com>
2017-10-27 15:27:13 +0000

    [MediaControls] Minor cleanup
    Remove unnecessary blink namespace prefix.
    Add proper comment for IsModern() function.
    
    Change-Id: Iff99b9563bcac62e58bf7c0107853f41e326d64b
    Reviewed-on: https://chromium-review.googlesource.com/739403
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512188}

--- third_party/WebKit/Source/modules/media_controls/MediaControlsResourceLoader.cpp
+++ third_party/WebKit/Source/modules/media_controls/MediaControlsResourceLoader.cpp
@@ -55,7 +55,7 @@ String MediaControlsResourceLoader::GetMediaControlsAndroidCSS() const {
       IDR_UASTYLE_LEGACY_MEDIA_CONTROLS_ANDROID_CSS);
 };
 
-// static.
+// static
 String MediaControlsResourceLoader::GetShadowTimelineStyleSheet() {
   return ResourceBundleHelper::UncompressResourceAsString(
       IDR_SHADOWSTYLE_MODERN_MEDIA_CONTROLS_TIMELINE_CSS);
