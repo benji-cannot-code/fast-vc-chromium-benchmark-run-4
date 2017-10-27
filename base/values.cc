be20032449e1ce7a033fd2ce0ded280a96f78bc1
jdoerrie <jdoerrie@chromium.org>
2017-10-27 13:39:48 +0000

    Add DCHECKs to base::Value Path API
    This change adds DCHECKs to the base::Value Path API to prevent mis-use.
    Ideally this checks should be static asserts, but this is currently not
    easily do-able.
    
    Bug: 646113
    Change-Id: I04700486d41a425a1b75c0fdd93f21da7009e05a
    Reviewed-on: https://chromium-review.googlesource.com/735860
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Brett Wilson <brettw@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512171}

--- base/values.cc
+++ base/values.cc
@@ -323,6 +323,7 @@ Value* Value::FindPath(span<const StringPiece> path) {
 }
 
 const Value* Value::FindPath(std::initializer_list<StringPiece> path) const {
+  DCHECK_GE(path.size(), 2u) << "Use FindKey() for a path of length 1.";
   return FindPath(make_span(path.begin(), path.size()));
 }
 
@@ -348,6 +349,7 @@ Value* Value::FindPathOfType(span<const StringPiece> path, Type type) {
 
 const Value* Value::FindPathOfType(std::initializer_list<StringPiece> path,
                                    Type type) const {
+  DCHECK_GE(path.size(), 2u) << "Use FindKeyOfType() for a path of length 1.";
   return FindPathOfType(make_span(path.begin(), path.size()), type);
 }
 
@@ -360,6 +362,7 @@ const Value* Value::FindPathOfType(span<const StringPiece> path,
 }
 
 Value* Value::SetPath(std::initializer_list<StringPiece> path, Value value) {
+  DCHECK_GE(path.size(), 2u) << "Use SetKey() for a path of length 1.";
   return SetPath(make_span(path.begin(), path.size()), std::move(value));
 }
 
@@ -394,6 +397,7 @@ Value* Value::SetPath(span<const StringPiece> path, Value value) {
 }
 
 bool Value::RemovePath(std::initializer_list<StringPiece> path) {
+  DCHECK_GE(path.size(), 2u) << "Use RemoveKey() for a path of length 1.";
   return RemovePath(make_span(path.begin(), path.size()));
 }
 
