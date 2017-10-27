45f41a41f66564de5a173aa10bea94fc042a2a81
Eddy Mead <meade@chromium.org>
2017-10-27 00:00:01 +0000

    Move processing extra CSSProperties.json5-formatted files up to css_properties.py
    This hopefully simplifies some of the processing around extra files, and 
    increases code sharing among the generators.
    
    - Further reduced direct usage of "private" members of generator classes 
    (denoted by a leading underscore as per usual style)
    - Added a check that properties that share a CSSPropertyAPI in
    CSSProperties.json5 also declare that they have the same methods.
    - Removed direct access to css_properties.CSSProperties private member 
    "_properties", and replaced it with "shorthands" and "longhands" getters. 
    Since _properties was a map, I had to adjust some of the templates also.
    
    This causes some changes to the generated code:
    Reordering of statements, e.g. case statements or generally the order in which
    functions are declared in.
    Old order was: 
    (mixed longhands and shorthands), aliases.
    New order is
    longhands, shorthands, aliases
    
    Diffs for files in each directory:
    core (13 files)
    https://gist.github.com/dac238646df18073995c5fef7750c7f4/revisions
    
    core/css/properties (2 files)
    https://gist.github.com/wilddamon/9d5578fb4f1c3faefe2f789301b4d5d9/revisions
    
    core/css (no changes)
    core/style (no changes)
    
    
    Bug: 
    Change-Id: I8b7c9b309e6910e758c5544f735d4d9f832cbe1b
    Reviewed-on: https://chromium-review.googlesource.com/737589
    Commit-Queue: meade_UTC10 <meade@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512029}

--- third_party/WebKit/Source/build/scripts/make_style_builder.py
+++ third_party/WebKit/Source/build/scripts/make_style_builder.py
@@ -72,13 +72,14 @@ class StyleBuilderWriter(json5_generator.Writer):
 
         self._json5_properties = css_properties.CSSProperties(json5_file_paths)
         self._input_files = json5_file_paths
-        self._properties = self._json5_properties.properties
-        for property_ in self._properties.values():
+        self._properties = self._json5_properties.longhands + \
+            self._json5_properties.shorthands
+        for property_ in self._properties:
             calculate_functions_to_declare(property_)
 
     @property
-    def properties(self):
-        return self._json5_properties.properties
+    def css_properties(self):
+        return self._json5_properties
 
     @property
     def default_parameters(self):
@@ -98,6 +99,7 @@ class StyleBuilderWriter(json5_generator.Writer):
         return {
             'input_files': self._input_files,
             'properties': self._properties,
+            'properties_by_id': self._json5_properties.properties_by_id,
         }
 
     @template_expander.use_jinja(
@@ -106,6 +108,7 @@ class StyleBuilderWriter(json5_generator.Writer):
         return {
             'input_files': self._input_files,
             'properties': self._properties,
+            'properties_by_id': self._json5_properties.properties_by_id,
         }
 
 
