13121fe99e3a6b3fe19ed9aabc484261f4f59a9a
Zhenyao Mo <zmo@chromium.org>
2017-10-27 21:05:32 +0000

    Attempt to fix SystemInfoHandler regression.
    There is a racing, that when we register an GPUInfo update observer, the GPU process already launched and sent back the GPUInfo and GpuFeatureInfo, so the observer is never called.
    
    I think this will likely fix the regression.
    
    Regardless, this is what we should do.
    
    BUG=778680,744658
    TEST=bots
    R=piman@chromium.org,nednguyen@chromium.org
    
    Change-Id: I32d0267fe4df96323b18619febb669c7fe2d5408
    Reviewed-on: https://chromium-review.googlesource.com/742281
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512277}

--- content/browser/devtools/protocol/system_info_handler.cc
+++ content/browser/devtools/protocol/system_info_handler.cc
@@ -155,6 +155,7 @@ class SystemInfoHandlerGpuObserver : public content::GpuDataManagerObserver {
         base::TimeDelta::FromMilliseconds(kGPUInfoWatchdogTimeoutMs));
 
     GpuDataManagerImpl::GetInstance()->AddObserver(this);
+    OnGpuInfoUpdate();
   }
 
   void OnGpuInfoUpdate() override {
