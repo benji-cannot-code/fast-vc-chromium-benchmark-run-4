f3f3fe92c229ab95a8a6fa284a0b120f7d5ef2df
Tim Schumann <tschumann@chromium.org>
2017-10-27 09:59:50 +0000

    Suspected to break  interactive_ui_tests  on chromium.mac/Mac10.10 Tests.
    Details: https://bugs.chromium.org/p/chromium/issues/detail?id=779019
    
    Revert "[Mac] Make the NSAssert family of macros uncontrollably fatal."
    
    This reverts commit 45dcae6e55efbec469089eb92a4d30832645def5.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Original change's description:
    > [Mac] Make the NSAssert family of macros uncontrollably fatal.
    > 
    > The default NSAssertionHandler implementation throws an
    > NSInternalInconsistencyException. Because several places in Cocoa catch,
    > swallow, or rethrow NSExceptions, this means NSAssert is not always
    > fatal at the point of assertion. This change installs a custom
    > NSAssertionHandler that does LOG(FATAL) instead, making assertions
    > immediately fatal. The current assertion handler is stored per-thread,
    > but since we primarily use Cocoa from the main thread, the custom
    > handler is only installed there.
    > 
    > Change-Id: I55f60ba2ae5fe17386eff795d1827e78ea36c4af
    > Reviewed-on: https://chromium-review.googlesource.com/735070
    > Commit-Queue: Robert Sesek <rsesek@chromium.org>
    > Reviewed-by: Rohit Rao (ping after 24h) <rohitrao@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#511999}
    
    TBR=rohitrao@chromium.org,rsesek@chromium.org
    
    Change-Id: Ie02cbbdbe2d7fe50a6ac3c42307bd31fc64a5f56
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/741601
    Reviewed-by: Tim Schumann <tschumann@chromium.org>
    Commit-Queue: Tim Schumann <tschumann@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512138}

--- chrome/browser/chrome_browser_application_mac.mm
+++ chrome/browser/chrome_browser_application_mac.mm
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/mac/exception_processor.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/crash_keys.h"
-#import "components/crash/core/common/assertion_handler.h"
 #import "components/crash/core/common/objc_zombie.h"
 #include "content/public/browser/browser_accessibility_state.h"
 
@@ -50,7 +49,6 @@ void CancelTerminate() {
   // the most recent 10,000 of them on the treadmill.
   ObjcEvilDoers::ZombieEnable(true, 10000);
 
-  crash_reporter::InstallNSAssertionHandlerOnCurrentThread();
   chrome::InstallObjcExceptionPreprocessor();
 }
 
