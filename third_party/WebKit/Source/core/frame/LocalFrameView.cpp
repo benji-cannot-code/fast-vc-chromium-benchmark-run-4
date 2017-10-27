4a1b96d9fbe1f8fa7af59f859aba0d5e8e3e7e8d
Stephen Chenney <schenney@chromium.org>
2017-10-27 21:07:34 +0000

    Force caret painting at low DPI
    The caret width must be forced non-zero when the DPI is lower than 1.
    Widths less than 1 do not paint.
    
    R=pdr@chromium.org
    BUG=731643
    
    Change-Id: I05953a3a029253644e801058d83988b4822418ba
    Reviewed-on: https://chromium-review.googlesource.com/740063
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Stephen Chenney <schenney@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512280}

--- third_party/WebKit/Source/core/frame/LocalFrameView.cpp
+++ third_party/WebKit/Source/core/frame/LocalFrameView.cpp
@@ -5662,7 +5662,8 @@ void LocalFrameView::SetAnimationHost(
 }
 
 LayoutUnit LocalFrameView::CaretWidth() const {
-  return LayoutUnit(GetChromeClient()->WindowToViewportScalar(1));
+  return LayoutUnit(
+      std::max<float>(1.0, GetChromeClient()->WindowToViewportScalar(1)));
 }
 
 ScrollbarTheme& LocalFrameView::GetPageScrollbarTheme() const {
