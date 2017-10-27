c42f931b903b9bd51e00e4e5c0098503d0c766c7
Liquan (Max) Gu <maxlg@chromium.org>
2017-10-27 21:08:03 +0000

    [PerformanceEntry] Resolve race condition in indexing
    ClusterFuzz has detected a data race in blink::PerformanceEntry::PerformanceEntry(crbug/776813).
    
    By observation, the original design has race condition when multiple threads create PerformanceEntry at the same time, with max_index being a shared variable.
    
    The new impl use atomic int which applies an atomic lock to the shared variable, to avoid the race condition.
    
    Bug: 776813
    Change-Id: Ibd251a020ddbb6c9e4496b0e5d197ed19b40658f
    Reviewed-on: https://chromium-review.googlesource.com/730773
    Commit-Queue: Liquan Gu <maxlg@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512281}

--- third_party/WebKit/Source/core/timing/PerformanceEntry.cpp
+++ third_party/WebKit/Source/core/timing/PerformanceEntry.cpp
@@ -31,13 +31,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/timing/PerformanceEntry.h"
 
+#include "base/atomic_sequence_num.h"
 #include "bindings/core/v8/ScriptValue.h"
 #include "bindings/core/v8/V8ObjectBuilder.h"
 
 namespace blink {
 
 namespace {
-static size_t max_index = 0;
+static base::AtomicSequenceNumber index_seq;
 }
 
 PerformanceEntry::PerformanceEntry(const String& name,
@@ -48,9 +49,8 @@ PerformanceEntry::PerformanceEntry(const String& name,
       entry_type_(entry_type),
       start_time_(start_time),
       duration_(finish_time - start_time),
-      entry_type_enum_(ToEntryTypeEnum(entry_type)) {
-  index_ = ++max_index;
-}
+      entry_type_enum_(ToEntryTypeEnum(entry_type)),
+      index_(index_seq.GetNext()) {}
 
 PerformanceEntry::~PerformanceEntry() {}
 
