b954cee290bfedc7d03d8838a9080a302c23113d
Dan Sanders <sandersd@chromium.org>
2017-10-27 23:35:43 +0000

    Move source_buffer_platform.h to //media/base/demuxer_memory_limit.h
    This allows the memory limit used by FFmpegDemuxer to be shared with the
    MP4 format parsers.
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I2aea47ebe5954d5f68223116411d5612a647bf8f
    Reviewed-on: https://chromium-review.googlesource.com/736048
    Commit-Queue: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: Matthew Wolenetz <wolenetz@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512348}

--- media/filters/ffmpeg_demuxer.cc
+++ media/filters/ffmpeg_demuxer.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "media/base/bind_to_current_loop.h"
 #include "media/base/decrypt_config.h"
+#include "media/base/demuxer_memory_limit.h"
 #include "media/base/limits.h"
 #include "media/base/media_log.h"
 #include "media/base/media_tracks.h"
@@ -1853,9 +1854,6 @@ bool FFmpegDemuxer::StreamsHaveAvailableCapacity() {
 
 bool FFmpegDemuxer::IsMaxMemoryUsageReached() const {
   DCHECK(task_runner_->BelongsToCurrentThread());
-
-  // Max allowed memory usage, all streams combined.
-  const size_t kDemuxerMemoryLimit = 150 * 1024 * 1024;
 
   size_t memory_left = kDemuxerMemoryLimit;
   for (const auto& stream : streams_) {
--- media/filters/source_buffer_stream.cc
+++ media/filters/source_buffer_stream.cc
@@ -14,9 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
 #include "base/trace_event/trace_event.h"
+#include "media/base/demuxer_memory_limit.h"
 #include "media/base/media_switches.h"
 #include "media/base/timestamp_constants.h"
-#include "media/filters/source_buffer_platform.h"
 #include "media/filters/source_buffer_range_by_dts.h"
 #include "media/filters/source_buffer_range_by_pts.h"
 
@@ -195,7 +195,7 @@ SourceBufferStream<RangeClass>::SourceBufferStream(
       range_for_next_append_(ranges_.end()),
       highest_output_buffer_timestamp_(kNoDecodeTimestamp()),
       max_interbuffer_distance_(kNoTimestamp),
-      memory_limit_(kSourceBufferAudioMemoryLimit) {
+      memory_limit_(kDemuxerStreamAudioMemoryLimit) {
   DCHECK(audio_config.IsValidConfig());
   audio_configs_.push_back(audio_config);
 }
@@ -210,7 +210,7 @@ SourceBufferStream<RangeClass>::SourceBufferStream(
       range_for_next_append_(ranges_.end()),
       highest_output_buffer_timestamp_(kNoDecodeTimestamp()),
       max_interbuffer_distance_(kNoTimestamp),
-      memory_limit_(kSourceBufferVideoMemoryLimit) {
+      memory_limit_(kDemuxerStreamVideoMemoryLimit) {
   DCHECK(video_config.IsValidConfig());
   video_configs_.push_back(video_config);
 }
@@ -226,7 +226,7 @@ SourceBufferStream<RangeClass>::SourceBufferStream(
       range_for_next_append_(ranges_.end()),
       highest_output_buffer_timestamp_(kNoDecodeTimestamp()),
       max_interbuffer_distance_(kNoTimestamp),
-      memory_limit_(kSourceBufferAudioMemoryLimit) {}
+      memory_limit_(kDemuxerStreamAudioMemoryLimit) {}
 
 template <typename RangeClass>
 SourceBufferStream<RangeClass>::~SourceBufferStream() {}
