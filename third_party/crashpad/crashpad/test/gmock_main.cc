adf6d77abb158fa801b1a9ada6f99dcdf3626bcb
Nico Weber <thakis@chromium.org>
2017-10-27 19:20:10 +0000

    Build and run crashpad_util_test on Windows bots.
    Ran
    
        python tools\mb\mb.py run out\gn crashpad_util_test
    
    to locally verify data and data_deps entries for swarming.
    
    Swarming apparently requires the use of base::TestLauncher, so
    use that in Crashpad-in-Chromium builds. Disable a bunch of tests
    that fail in that setup. 
    
    Bug: 777924,778672
    Change-Id: I4aa828040d488755582e5559d5f117e1ed8d1c86
    Reviewed-on: https://chromium-review.googlesource.com/737439
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512262}

--- third_party/crashpad/crashpad/test/gmock_main.cc
+++ third_party/crashpad/crashpad/test/gmock_main.cc
@@ -17,8 +17,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gtest/gtest.h"
 #include "test/main_arguments.h"
 
+#if defined(CRASHPAD_IN_CHROMIUM)
+#include "base/bind.h"
+#include "base/test/launcher/unit_test_launcher.h"
+#include "base/test/test_suite.h"
+#endif
+
 int main(int argc, char* argv[]) {
   crashpad::test::InitializeMainArguments(argc, argv);
+#if defined(CRASHPAD_IN_CHROMIUM)
+  // Writes a json file with test details which is needed by swarming.
+  base::TestSuite test_suite(argc, argv);
+  return base::LaunchUnitTests(
+      argc,
+      argv,
+      base::Bind(&base::TestSuite::Run, base::Unretained(&test_suite)));
+#else
   testing::InitGoogleMock(&argc, argv);
   return RUN_ALL_TESTS();
+#endif
 }
