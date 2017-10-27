e01dc0bbd11a4a23b1f7c35d13fcc7ab17284e08
Tom Sepez <tsepez@chromium.org>
2017-10-27 00:29:15 +0000

    Re-land "Move sandbox_win.cc to services/service_manager/sandbox/win""
    This reverts commit 2e41759f82c09a84dca4c3a3513b30a6f9e88dfc.
    
    TBR: jam@chromium.org
    Bug: 708738
    Change-Id: I0e229a1abe14acb41c7ff7b230eb393e1b93bb34
    Reviewed-on: https://chromium-review.googlesource.com/740281
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512040}

--- content/browser/gpu/gpu_process_host.cc
+++ content/browser/gpu/gpu_process_host.cc
@@ -81,8 +81,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if defined(OS_WIN)
-#include "content/common/sandbox_win.h"
 #include "sandbox/win/src/sandbox_policy.h"
+#include "services/service_manager/sandbox/win/sandbox_win.h"
+#include "ui/gfx/switches.h"
 #include "ui/gfx/win/rendering_window_manager.h"
 #endif
 
@@ -246,7 +247,8 @@ class GpuSandboxedProcessLauncherDelegate
       // Open GL path.
       policy->SetTokenLevel(sandbox::USER_RESTRICTED_SAME_ACCESS,
                             sandbox::USER_LIMITED);
-      SetJobLevel(cmd_line_, sandbox::JOB_UNPROTECTED, 0, policy);
+      service_manager::SandboxWin::SetJobLevel(
+          cmd_line_, sandbox::JOB_UNPROTECTED, 0, policy);
       policy->SetDelayedIntegrityLevel(sandbox::INTEGRITY_LEVEL_LOW);
     } else {
       policy->SetTokenLevel(sandbox::USER_RESTRICTED_SAME_ACCESS,
@@ -258,12 +260,12 @@ class GpuSandboxedProcessLauncherDelegate
       // turn blocks on the browser UI thread. So, instead we forgo a window
       // message pump entirely and just add job restrictions to prevent child
       // processes.
-      SetJobLevel(cmd_line_, sandbox::JOB_LIMITED_USER,
-                  JOB_OBJECT_UILIMIT_SYSTEMPARAMETERS |
-                      JOB_OBJECT_UILIMIT_DESKTOP |
-                      JOB_OBJECT_UILIMIT_EXITWINDOWS |
-                      JOB_OBJECT_UILIMIT_DISPLAYSETTINGS,
-                  policy);
+      service_manager::SandboxWin::SetJobLevel(
+          cmd_line_, sandbox::JOB_LIMITED_USER,
+          JOB_OBJECT_UILIMIT_SYSTEMPARAMETERS | JOB_OBJECT_UILIMIT_DESKTOP |
+              JOB_OBJECT_UILIMIT_EXITWINDOWS |
+              JOB_OBJECT_UILIMIT_DISPLAYSETTINGS,
+          policy);
 
       policy->SetIntegrityLevel(sandbox::INTEGRITY_LEVEL_LOW);
     }
--- services/service_manager/sandbox/sandbox.h
+++ services/service_manager/sandbox/sandbox.h
@@ -19,6 +19,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #endif  // defined(OS_MACOSX)
 
+namespace sandbox {
+struct SandboxInterfaceInfo;
+}  // namespace sandbox
+
 namespace service_manager {
 
 // Interface to the service manager sandboxes across the various platforms.
@@ -78,7 +82,8 @@ class SERVICE_MANAGER_SANDBOX_EXPORT Sandbox {
 #endif  // defined(OS_MACOSX)
 
 #if defined(OS_WIN)
-// TODO(tsepez): move win code here.
+  static bool Initialize(service_manager::SandboxType sandbox_type,
+                         sandbox::SandboxInterfaceInfo* sandbox_info);
 #endif  // defined(OS_WIN)
 };
 
