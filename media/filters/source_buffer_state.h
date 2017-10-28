1612db77644d9bd3f17c785eebd21727090e260f
Xiaohan Wang <xhwang@chromium.org>
2017-10-28 20:42:51 +0000

    media: Crash on SourceBufferState::Append() failure for bug investigation
    Based on the failure volume on Canary channel, choose to crash with a
    1/10 probablity on this particular error. We also differentiate between
    the case where encrypted media init data has been reported or not. This
    is not equivalent to clear/encrypted playback, but should be close enough
    for bug investigation purposes.
    
    This CL also does minor cleanup (reordering) in SourceBufferState.
    
    The crash CHECK will be reverted in a few days after we collect enough
    crash reports. The CL to revert the crash CHECK is prepared at
    https://chromium-review.googlesource.com/c/chromium/src/+/742229
    
    PSA: If this is causing too many crashes, feel free to CQ the above
    revert CL at any time!
    
    BUG=778363
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I1b5f1343a0e7ec18261b6e0ad1dac6740339ff83
    Reviewed-on: https://chromium-review.googlesource.com/739743
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Matthew Wolenetz <wolenetz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512408}

--- media/filters/source_buffer_state.h
+++ media/filters/source_buffer_state.h
@@ -172,6 +172,10 @@ class MEDIA_EXPORT SourceBufferState {
   // processing the buffers.
   bool OnNewBuffers(const StreamParser::BufferQueueMap& buffer_queue_map);
 
+  // Called when StreamParser encounters encrypted media init data.
+  void OnEncryptedMediaInitData(EmeInitDataType type,
+                                const std::vector<uint8_t>& init_data);
+
   void OnSourceInitDone(const StreamParser::InitParameters& params);
 
   // Sets memory limits for all demuxer streams.
@@ -180,9 +184,6 @@ class MEDIA_EXPORT SourceBufferState {
   // Tracks the number of MEDIA_LOGs emitted for segments missing expected audio
   // or video blocks. Useful to prevent log spam.
   int num_missing_track_logs_ = 0;
-
-  CreateDemuxerStreamCB create_demuxer_stream_cb_;
-  NewTextTrackCB new_text_track_cb_;
 
   // During Append(), if OnNewBuffers() coded frame processing updates the
   // timestamp offset then |*timestamp_offset_during_append_| is also updated
@@ -214,8 +215,12 @@ class MEDIA_EXPORT SourceBufferState {
   DemuxerStreamMap text_streams_;
 
   std::unique_ptr<FrameProcessor> frame_processor_;
+  CreateDemuxerStreamCB create_demuxer_stream_cb_;
   MediaLog* media_log_;
+
   StreamParser::InitCB init_cb_;
+  StreamParser::EncryptedMediaInitDataCB encrypted_media_init_data_cb_;
+  NewTextTrackCB new_text_track_cb_;
 
   State state_;
 
@@ -228,6 +233,7 @@ class MEDIA_EXPORT SourceBufferState {
   Demuxer::MediaTracksUpdatedCB init_segment_received_cb_;
   bool append_in_progress_ = false;
   bool first_init_segment_received_ = false;
+  bool encrypted_media_init_data_reported_ = false;
 
   std::vector<AudioCodec> expected_audio_codecs_;
   std::vector<VideoCodec> expected_video_codecs_;
