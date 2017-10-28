ad8199835dc5fc223712baba271cba96b3d8c7ee
Koji Ishii <kojii@chromium.org>
2017-10-28 09:55:12 +0000

    [LayoutNG] Change NGInlineNode::ComputeOffsetMappingIfNeeded() to return a pointer
    This patch changes NGInlineNode::ComputeOffsetMappingIfNeeded()
    to return a pointer instead of a ref, in preparation of an
    NGInlineNode may not always be laid out by LayoutNG.
    
    Also makes sure it gets NGInlineNode from NGBlockNode so that
    it uses the correct LayoutObject for multi-column case.
    
    Bug: 636993
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: Iaf16e39a30ca79c83db148bfdae40c5df7959e13
    Reviewed-on: https://chromium-review.googlesource.com/741102
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512395}

--- third_party/WebKit/Source/core/layout/ng/inline/ng_inline_node.cc
+++ third_party/WebKit/Source/core/layout/ng/inline/ng_inline_node.cc
@@ -423,7 +423,7 @@ const NGInlineNodeData& NGInlineNode::EnsureData() {
   return Data();
 }
 
-const NGOffsetMapping& NGInlineNode::ComputeOffsetMappingIfNeeded() {
+const NGOffsetMapping* NGInlineNode::ComputeOffsetMappingIfNeeded() {
   DCHECK(!GetLayoutBlockFlow()->GetDocument().NeedsLayoutTreeUpdate());
 
   if (!Data().offset_mapping_) {
@@ -444,7 +444,7 @@ const NGOffsetMapping& NGInlineNode::ComputeOffsetMappingIfNeeded() {
         WTF::MakeUnique<NGOffsetMapping>(mapping_builder.Build());
   }
 
-  return *Data().offset_mapping_;
+  return Data().offset_mapping_.get();
 }
 
 // Depth-first-scan of all LayoutInline and LayoutText nodes that make up this
