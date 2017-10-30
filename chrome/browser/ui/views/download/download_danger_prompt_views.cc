7115d14afc1b05b4e7fdec7b7b6436f6f96e38c8
Peter Boström <pbos@chromium.org>
2017-10-30 22:57:41 +0000

    Add interactive tests for DownloadDangerPrompt.
    Exposes all current window titles, message bodies and cancel-button
    text.
    
    Also fixing crash when DownloadDangerPromptViews is destructed without
    triggering an action. This happens at least when browser_tests are run
    without --interactive, though possibly also when a tab gets closed
    without closing the dialog first.
    
    Bug: chromium:654142
    Change-Id: I7339ffec27cb41d50ddc97aece2ca4fdf403cbb5
    Reviewed-on: https://chromium-review.googlesource.com/731617
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512660}

--- chrome/browser/ui/views/download/download_danger_prompt_views.cc
+++ chrome/browser/ui/views/download/download_danger_prompt_views.cc
@@ -47,6 +47,7 @@ class DownloadDangerPromptViews : public DownloadDangerPrompt,
   DownloadDangerPromptViews(content::DownloadItem* item,
                             bool show_context,
                             const OnDone& done);
+  ~DownloadDangerPromptViews() override;
 
   // DownloadDangerPrompt:
   void InvokeActionForTesting(Action action) override;
@@ -92,7 +93,6 @@ DownloadDangerPromptViews::DownloadDangerPromptViews(
       show_context_(show_context),
       done_(done),
       contents_view_(NULL) {
-  DCHECK(!done_.is_null());
   download_->AddObserver(this);
 
   contents_view_ = new views::View;
@@ -125,6 +125,11 @@ DownloadDangerPromptViews::DownloadDangerPromptViews(
       item->GetBrowserContext()));
   chrome::RecordDialogCreation(
       chrome::DialogIdentifier::DOWNLOAD_DANGER_PROMPT);
+}
+
+DownloadDangerPromptViews::~DownloadDangerPromptViews() {
+  if (download_)
+    download_->RemoveObserver(this);
 }
 
 // DownloadDangerPrompt methods:
