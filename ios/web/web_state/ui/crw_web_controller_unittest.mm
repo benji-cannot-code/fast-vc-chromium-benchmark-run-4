fe9761d5876e567cf7b5a94780715a41c8386f7b
Danyao Wang <danyao@chromium.org>
2017-10-30 15:24:12 +0000

    [Nav Experiment] Fix mock to correctly simulate a page load.
    CRWWebControllerTest.CurrentUrlWithTrustLevel simulates a page load
    using mock_web_view_ by calling WKNavigationDelegate methods directly.
    New WKBasedNavigationManager expects the WKBackForwardList to have been
    updated when |-webView:didCommitNavigation:| is called. Added this
    missing setup fixes the broken test.
    
    Bug: 776450
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I01a13fc4c21a0a64a067a4e542f27b0a9492d02c
    Reviewed-on: https://chromium-review.googlesource.com/742126
    Commit-Queue: Danyao Wang <danyao@chromium.org>
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512499}

--- ios/web/web_state/ui/crw_web_controller_unittest.mm
+++ ios/web/web_state/ui/crw_web_controller_unittest.mm
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/public/web_state/ui/crw_web_view_content_view.h"
 #include "ios/web/public/web_state/url_verification_constants.h"
 #include "ios/web/public/web_state/web_state_observer.h"
+#import "ios/web/test/fakes/crw_test_back_forward_list.h"
 #import "ios/web/test/web_test_with_web_controller.h"
 #import "ios/web/test/wk_web_view_crash_utils.h"
 #import "ios/web/web_state/ui/crw_web_controller_container_view.h"
@@ -166,7 +167,8 @@ class CRWWebControllerTest : public WebTestWithWebController {
     }
 #endif
 
-    [[result stub] backForwardList];
+    mock_wk_list_ = [[CRWTestBackForwardList alloc] init];
+    OCMStub([result backForwardList]).andReturn(mock_wk_list_);
     [[[result stub] andReturn:[NSURL URLWithString:@(kTestURLString)]] URL];
     [[result stub] setNavigationDelegate:[OCMArg checkWithBlock:^(id delegate) {
                      navigation_delegate_ = delegate;
@@ -186,6 +188,7 @@ class CRWWebControllerTest : public WebTestWithWebController {
   __weak id<WKNavigationDelegate> navigation_delegate_;
   UIScrollView* scroll_view_;
   id mock_web_view_;
+  CRWTestBackForwardList* mock_wk_list_;
 };
 
 // Tests that AllowCertificateError is called with correct arguments if
@@ -623,6 +626,7 @@ TEST_F(CRWWebControllerTest, CurrentUrlWithTrustLevel) {
   // Simulate a page load to trigger a URL update.
   [navigation_delegate_ webView:mock_web_view_
       didStartProvisionalNavigation:nil];
+  [mock_wk_list_ setCurrentURL:@"http://chromium.test"];
   [navigation_delegate_ webView:mock_web_view_ didCommitNavigation:nil];
 
   URLVerificationTrustLevel trust_level = kNone;
