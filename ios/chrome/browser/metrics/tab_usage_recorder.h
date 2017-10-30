bfb91b57339986a538c73e2cbf2824951afe3554
Sylvain Defresne <sdefresne@chromium.org>
2017-10-30 16:11:08 +0000

    Allow WebStateObserver to observe N WebStates [3/N].
    Convert TabUsageRecorder to observe to all WebStates inserted
    in the WebStateList it observes. Remove the helper class that
    was required when WebStateObserver could only observe a single
    WebState.
    
    Bug: 775684
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Icb42548d628465e16f91613c7c881f3ff269cb58
    Reviewed-on: https://chromium-review.googlesource.com/738872
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512506}

--- ios/chrome/browser/metrics/tab_usage_recorder.h
+++ ios/chrome/browser/metrics/tab_usage_recorder.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/time/time.h"
 #import "ios/chrome/browser/web_state_list/web_state_list_observer.h"
+#import "ios/web/public/web_state/web_state_observer.h"
 
 class PrerenderService;
 class WebStateList;
@@ -76,7 +77,8 @@ extern const char kRendererTerminationStateHistogram[];
 extern const int kSecondsBeforeRendererTermination;
 
 // Reports usage about the lifecycle of a single TabModel's tabs.
-class TabUsageRecorder : public WebStateListObserver {
+class TabUsageRecorder : public web::WebStateObserver,
+                         public WebStateListObserver {
  public:
   enum TabStateWhenSelected {
     IN_MEMORY = 0,
@@ -182,11 +184,6 @@ class TabUsageRecorder : public WebStateListObserver {
   // to depends on injecting values in |termination_timestamps_|.
   friend class TabUsageRecorderTest;
 
-  // Sub-class of web::WebStateObserver to allow track events from multiple
-  // web::WebState (needs to be forward-declared here because the destructor
-  // of web::WebStateObserver is protected).
-  class WebStateObserver;
-
   // Clear out all state regarding a current evicted tab.
   void ResetEvictedTab();
 
@@ -206,14 +203,19 @@ class TabUsageRecorder : public WebStateListObserver {
   // Returns the number of WebState that are still alive (in-memory).
   int GetLiveWebStatesCount() const;
 
-  // Called after a WebState is added to the WebStateList; will create the
-  // observer used to track the WebState's events.
-  void OnWebStateInserted(web::WebState* web_state);
-
   // Called before one of the tracked WebState is destroyed. The WebState is
   // still valid but will become invalid afterwards, so any reference to it
   // should be removed.
   void OnWebStateDestroyed(web::WebState* web_state);
+
+  // web::WebStateObserver implementation.
+  void DidStartNavigation(web::WebState* web_state,
+                          web::NavigationContext* navigation_context) override;
+  void PageLoaded(
+      web::WebState* web_state,
+      web::PageLoadCompletionStatus load_completion_status) override;
+  void RenderProcessGone(web::WebState* web_state) override;
+  void WebStateDestroyed(web::WebState* web_state) override;
 
   // WebStateListObserver implementation.
   void WebStateInsertedAt(WebStateList* web_state_list,
@@ -266,10 +268,6 @@ class TabUsageRecorder : public WebStateListObserver {
 
   // Keep track of the tabs that have a known eviction cause.
   std::map<web::WebState*, TabStateWhenSelected> evicted_web_states_;
-
-  // Maps WebStates to the WebStateObserver used to track its events.
-  std::map<web::WebState*, std::unique_ptr<WebStateObserver>>
-      web_state_observers_;
 
   // The WebStateList containing all the monitored tabs.
   WebStateList* web_state_list_;  // weak
