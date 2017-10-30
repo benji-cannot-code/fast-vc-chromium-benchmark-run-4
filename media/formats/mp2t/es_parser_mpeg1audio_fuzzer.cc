56b6dfffd1e632d2cb17454c9331519452ba2811
Matt Wolenetz <wolenetz@chromium.org>
2017-10-30 18:35:45 +0000

    MSE: Relocate fuzzers from //testing/libfuzzer/fuzzers to //media/...
    Many of the first fuzzers were implemented under //testing, including a
    few MSE-related fuzzers. This change moves those MSE fuzzers closer to
    the code they test under //media.
    
    BUG=779238
    
    Change-Id: Ied496bbaa78864e54d670cc549da46bdd09eeeb2
    Reviewed-on: https://chromium-review.googlesource.com/742452
    Commit-Queue: Max Moroz <mmoroz@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Max Moroz <mmoroz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512536}

--- media/formats/mp2t/es_parser_mpeg1audio_fuzzer.cc
+++ media/formats/mp2t/es_parser_mpeg1audio_fuzzer.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/bind.h"
+#include "media/base/media_log.h"
 #include "media/formats/mp2t/es_parser_mpeg1audio.h"
 
 class NullMediaLog : public media::MediaLog {
--- media/formats/mp4/mp4_box_reader_fuzzer.cc
+++ media/formats/mp4/mp4_box_reader_fuzzer.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/macros.h"
+#include "media/base/media_log.h"
 #include "media/formats/mp4/box_reader.h"
 
 class NullMediaLog : public media::MediaLog {
