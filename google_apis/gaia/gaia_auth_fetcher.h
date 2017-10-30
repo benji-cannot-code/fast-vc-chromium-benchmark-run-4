be0b1e34fde6f92b487324aced084bb4a5081207
Mihai Sardarescu <msarda@chromium.org>
2017-10-30 18:49:56 +0000

    Mark programmatic_auth URL as deprecated.
    This programmatic_auth endpoint is currently being deprecated on
    the Google infrastructure. This CL marks the URL and the corresponding
    methods as deprecated.
    
    This CL also adds an UMA count to record how many times the programmatic_auth
    endpoint is used in Chrome.
    
    Bug: 775009
    Change-Id: I8b9527f82c51641a52db42df9f8c3787077943ff
    Reviewed-on: https://chromium-review.googlesource.com/741582
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512557}

--- google_apis/gaia/gaia_auth_fetcher.h
+++ google_apis/gaia/gaia_auth_fetcher.h
@@ -82,7 +82,8 @@ class GaiaAuthFetcher : public net::URLFetcherDelegate {
   //
   // Either OnClientOAuthSuccess or OnClientOAuthFailure will be
   // called on the consumer on the original thread.
-  void StartCookieForOAuthLoginTokenExchange(const std::string& session_index);
+  void DeprecatedStartCookieForOAuthLoginTokenExchange(
+      const std::string& session_index);
 
   // Start a request to exchange the cookies of a signed-in user session
   // for an OAuthLogin-scoped oauth2 token. In the case of a session with
@@ -93,7 +94,7 @@ class GaiaAuthFetcher : public net::URLFetcherDelegate {
   //
   // Either OnClientOAuthSuccess or OnClientOAuthFailure will be
   // called on the consumer on the original thread.
-  void StartCookieForOAuthLoginTokenExchangeWithDeviceId(
+  void DeprecatedStartCookieForOAuthLoginTokenExchangeWithDeviceId(
       const std::string& session_index,
       const std::string& device_id);
 
@@ -109,7 +110,7 @@ class GaiaAuthFetcher : public net::URLFetcherDelegate {
   //
   // Either OnClientOAuthCode or ClientOAuthSuccess or OnClientOAuthFailure
   // will be called on the consumer on the original thread.
-  void StartCookieForOAuthLoginTokenExchange(
+  void DeprecatedStartCookieForOAuthLoginTokenExchange(
       bool fetch_token_from_auth_code,
       const std::string& session_index,
       const std::string& client_id,
@@ -430,7 +431,7 @@ class GaiaAuthFetcher : public net::URLFetcherDelegate {
 
   // While a fetch is going on:
   std::unique_ptr<net::URLFetcher> fetcher_;
-  GURL client_login_to_oauth2_gurl_;
+  GURL deprecated_client_login_to_oauth2_gurl_;
   std::string request_body_;
   std::string requested_service_;
   bool fetch_pending_ = false;
