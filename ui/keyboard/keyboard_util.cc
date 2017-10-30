3c43320cb251c683045cd17c8bcc4928a3d21046
Blake O'Hare <blakeo@chromium.org>
2017-10-30 20:21:36 +0000

    Use a new flag to relay floating VK status to the extension.
    This introduces a new flag to the extension called 'floatingkeyboard'
    as opposed to the old flag which was 'floatingvirtualkeyboard'. The old
    flag is still referenced by a helper method which is used to activate
    legacy floating keyboard code. Due to the semi-tandem nature of the
    extension, this is just the first step to relaying the flag. The
    extension needs to be updated to remove references to the old flag and
    the different name here will prevent accidental collisions or showing
    the old keyboard.
    
    Bug: 776260
    Change-Id: I38b5f5275b2b493e5eea5c87b7ad509a018741cc
    Reviewed-on: https://chromium-review.googlesource.com/741426
    Commit-Queue: Blake O'Hare <blakeo@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512599}

--- ui/keyboard/keyboard_util.cc
+++ ui/keyboard/keyboard_util.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/command_line.h"
+#include "base/feature_list.h"
 #include "base/lazy_instance.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_macros.h"
@@ -190,7 +191,7 @@ bool IsExperimentalInputViewEnabled() {
 }
 
 bool IsFloatingVirtualKeyboardEnabled() {
-  return false;
+  return base::FeatureList::IsEnabled(features::kEnableFloatingVirtualKeyboard);
 }
 
 bool IsGestureTypingEnabled() {
