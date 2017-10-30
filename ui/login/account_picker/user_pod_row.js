2d38b9c536f53a461f27ae92908d280d912900a1
Scott Chen <scottchen@chromium.org>
2017-10-30 11:02:24 +0000

    MD User Manager: fix tab handling for locked user.
    When a locked supervising user has no local credentials, the password
    input will be hidden. However, the code currently still tries to focus
    that input as the mainInput of the user pod, which puts tab navigation
    in a limbo state.
    
    This CL adds a check in DesktopUserPod to focus the correct element
    instead of the hidden input while tabbing, when there's no local creds.
    
    Bug: 617062
    Change-Id: Ia4e6edcecf8e25813debcdd89dcf72186f04be76
    Reviewed-on: https://chromium-review.googlesource.com/741749
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Commit-Queue: Scott Chen <scottchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512468}

--- ui/login/account_picker/user_pod_row.js
+++ ui/login/account_picker/user_pod_row.js
@@ -2470,7 +2470,7 @@ cr.define('login', function() {
 
     /** @override */
     get mainInput() {
-      if (this.user.needsSignin)
+      if (this.user.needsSignin && this.user.hasLocalCreds)
         return this.passwordElement;
       else
         return this.nameElement;
