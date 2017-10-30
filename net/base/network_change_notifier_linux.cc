2fc038eba43c304d826713f41137ec6aba2d4267
zhuoyu.qian <zhuoyu.qian@samsung.com>
2017-10-30 13:03:33 +0000

    Rename GetMaxBandwidthForConnectionSubtype.
    As the comment in network_change_notifier.h by jkarlin@,
    rename GetMaxBandwidthForConnectionSubtype to
    GetMaxBandwidthMbpsForConnectionSubtype.
    
    BUG=488585
    R=jkarlin@chromium.org
    
    Signed-off-by: zhuoyu.qian <zhuoyu.qian@samsung.com>
    Change-Id: Ic6d3cef9a66b0520581f1af388b1857578502323
    Reviewed-on: https://chromium-review.googlesource.com/734921
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Josh Karlin <jkarlin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512480}

--- net/base/network_change_notifier_linux.cc
+++ net/base/network_change_notifier_linux.cc
@@ -89,7 +89,7 @@ void NetworkChangeNotifierLinux::Thread::OnLinkChanged() {
     NetworkChangeNotifier::NotifyObserversOfConnectionTypeChange();
     last_type_ = GetCurrentConnectionType();
     double max_bandwidth_mbps =
-        NetworkChangeNotifier::GetMaxBandwidthForConnectionSubtype(
+        NetworkChangeNotifier::GetMaxBandwidthMbpsForConnectionSubtype(
             last_type_ == CONNECTION_NONE ? SUBTYPE_NONE : SUBTYPE_UNKNOWN);
     NetworkChangeNotifier::NotifyObserversOfMaxBandwidthChange(
         max_bandwidth_mbps, last_type_);
