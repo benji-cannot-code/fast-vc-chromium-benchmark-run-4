b550f2bdbf27705a3bb1bb7503aeb70b0907fb2f
David Benjamin <davidben@chromium.org>
2017-10-30 22:07:15 +0000

    Retain the password delegate with SSLPlatformKeyNSS.
    PK11_FindKeyByAnyCert retains wincx on the resulting SECKEYPrivateKey,
    which may be used on PK11_Sign. To keep the lifetimes correct, we must
    retain a reference to the password delegate in SSLPlatformKeyNSS.
    
    Bug: 779090
    Change-Id: Ia769ebe967582e83fbcbcaa5bca7ef788c1345a6
    Reviewed-on: https://chromium-review.googlesource.com/743847
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512641}

--- net/ssl/ssl_platform_key_nss.cc
+++ net/ssl/ssl_platform_key_nss.cc
@@ -46,8 +46,13 @@ void LogPRError(const char* message) {
 
 class SSLPlatformKeyNSS : public ThreadedSSLPrivateKey::Delegate {
  public:
-  SSLPlatformKeyNSS(int type, crypto::ScopedSECKEYPrivateKey key)
-      : type_(type), key_(std::move(key)) {}
+  SSLPlatformKeyNSS(int type,
+                    scoped_refptr<crypto::CryptoModuleBlockingPasswordDelegate>
+                        password_delegate,
+                    crypto::ScopedSECKEYPrivateKey key)
+      : type_(type),
+        password_delegate_(std::move(password_delegate)),
+        key_(std::move(key)) {}
   ~SSLPlatformKeyNSS() override {}
 
   std::vector<SSLPrivateKey::Hash> GetDigestPreferences() override {
@@ -148,6 +153,10 @@ class SSLPlatformKeyNSS : public ThreadedSSLPrivateKey::Delegate {
 
  private:
   int type_;
+  // NSS retains a pointer to the password delegate, so retain a reference here
+  // to ensure the lifetimes are correct.
+  scoped_refptr<crypto::CryptoModuleBlockingPasswordDelegate>
+      password_delegate_;
   crypto::ScopedSECKEYPrivateKey key_;
 
   DISALLOW_COPY_AND_ASSIGN(SSLPlatformKeyNSS);
@@ -158,7 +167,8 @@ class SSLPlatformKeyNSS : public ThreadedSSLPrivateKey::Delegate {
 scoped_refptr<SSLPrivateKey> FetchClientCertPrivateKey(
     const X509Certificate* certificate,
     CERTCertificate* cert_certificate,
-    crypto::CryptoModuleBlockingPasswordDelegate* password_delegate) {
+    scoped_refptr<crypto::CryptoModuleBlockingPasswordDelegate>
+        password_delegate) {
   // This function may acquire the NSS lock or reenter this code via extension
   // hooks (such as smart card UI). To ensure threads are not starved or
   // deadlocked, the base::ScopedBlockingCall below increments the thread pool
@@ -176,8 +186,13 @@ scoped_refptr<SSLPrivateKey> FetchClientCertPrivateKey(
   if (!GetClientCertInfo(certificate, &type, &max_length))
     return nullptr;
 
+  // Note that key contains a reference to password_delegate->wincx() and may
+  // use it in PK11_Sign. Thus password_delegate must outlive key. We pass it
+  // into SSLPlatformKeyNSS to tie the lifetimes together. See
+  // https://crbug.com/779090.
   return base::MakeRefCounted<ThreadedSSLPrivateKey>(
-      std::make_unique<SSLPlatformKeyNSS>(type, std::move(key)),
+      std::make_unique<SSLPlatformKeyNSS>(type, std::move(password_delegate),
+                                          std::move(key)),
       GetSSLPlatformKeyTaskRunner());
 }
 
--- net/ssl/ssl_platform_key_nss_unittest.cc
+++ net/ssl/ssl_platform_key_nss_unittest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/memory/ref_counted.h"
 #include "crypto/ec_private_key.h"
+#include "crypto/nss_crypto_module_delegate.h"
 #include "crypto/scoped_nss_types.h"
 #include "crypto/scoped_test_nss_db.h"
 #include "net/cert/x509_util_nss.h"
