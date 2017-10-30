0a8e574074762e31f2c22616e140398232c267ec
Allen Bauer <kylixrd@chromium.org>
2017-10-30 20:26:59 +0000

    Never show the close(x) icon on content setting bubbles.
    Bug: 652024
    Change-Id: I77bfbe29d728e3f3f8bdc0d7d2b1f728d8da969b
    Reviewed-on: https://chromium-review.googlesource.com/740085
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512603}

--- chrome/browser/ui/views/content_setting_bubble_contents.cc
+++ chrome/browser/ui/views/content_setting_bubble_contents.cc
@@ -358,7 +358,7 @@ base::string16 ContentSettingBubbleContents::GetWindowTitle() const {
 }
 
 bool ContentSettingBubbleContents::ShouldShowCloseButton() const {
-  return ChromeLayoutProvider::Get()->IsHarmonyMode();
+  return false;
 }
 
 void ContentSettingBubbleContents::Init() {
