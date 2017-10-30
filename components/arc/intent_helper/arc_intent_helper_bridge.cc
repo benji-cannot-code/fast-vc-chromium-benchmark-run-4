879dbfbfc6b7ca017a2302b1949f8cf74dcad60a
yusukes <yusukes@chromium.org>
2017-10-30 22:57:26 +0000

    arc: Reject about: URL in OnOpenUrl()
    Once ag/3121293 is submitted, we can safely drop the about: support.
    
    BUG=b:67758218
    TEST=try
    
    Change-Id: Ifc08fd3daac84ffc5923c642e3fa200c4efe9bd7
    Reviewed-on: https://chromium-review.googlesource.com/736314
    Commit-Queue: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: Brett Wilson <brettw@chromium.org>
    Reviewed-by: Luis Hector Chavez <lhchavez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512659}

--- components/arc/intent_helper/arc_intent_helper_bridge.cc
+++ components/arc/intent_helper/arc_intent_helper_bridge.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/arc/arc_browser_context_keyed_service_factory_base.h"
 #include "components/arc/arc_service_manager.h"
 #include "components/arc/audio/arc_audio_bridge.h"
+#include "components/url_formatter/url_fixer.h"
 #include "ui/base/layout.h"
 #include "url/gurl.h"
 
@@ -125,9 +126,11 @@ void ArcIntentHelperBridge::OnOpenDownloads() {
 
 void ArcIntentHelperBridge::OnOpenUrl(const std::string& url) {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
-  const GURL gurl(url);
+  // Converts |url| to a fixed-up one. This converts about: URIs to chrome://,
+  // for example.
+  const GURL gurl(url_formatter::FixupURL(url, std::string()));
   // Disallow opening chrome:// URLs.
-  if (gurl.SchemeIs(kChromeUIScheme))
+  if (!gurl.is_valid() || gurl.SchemeIs(kChromeUIScheme))
     return;
   open_url_delegate_->OpenUrl(gurl);
 }
