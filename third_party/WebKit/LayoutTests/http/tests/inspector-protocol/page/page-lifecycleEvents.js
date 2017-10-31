e6c5dffcebaeb1c2fd6afd6f7e116d33c811a882
Andrey Lushnikov <lushnikov@chromium.org>
2017-10-31 06:02:08 +0000

    DevTools: stop idleness detector when pending navigation commits
    This patch stops indleness detector when a new document gets committed.
    This makes sure that networkIdle events for the previous document don't
    come for the newly committed one.
    
    Drive-by: cleanup test.
    
    R=pfeldman
    
    Change-Id: Ia0cbcbbda31e07957867c53641649dd80091281e
    Reviewed-on: https://chromium-review.googlesource.com/745388
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Commit-Queue: Andrey Lushnikov <lushnikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512760}

--- third_party/WebKit/LayoutTests/http/tests/inspector-protocol/page/page-lifecycleEvents.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector-protocol/page/page-lifecycleEvents.js
@@ -8,15 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   var events = [];
   dp.Page.onLifecycleEvent(result => {
     events.push(result.params.name);
-    if (events.includes('networkIdle') && events.includes('networkAlmostIdle')) {
+    if (events.includes('networkIdle')) {
+      // 'load' can come before 'DOMContentLoaded'
       events.sort();
       testRunner.log(events);
       testRunner.completeTest();
     }
   });
 
-  // It's possible for Blink to finish the load and run out of tasks before
-  // network idle lifecycle events are generated.  Add a timeout
-  // greater than the network quiet window to guarantee that those events fire.
-  dp.Page.navigate({url: "data:text/html,Hello! <script>setTimeout(() => {}, 2000);</script>"});
+  dp.Page.navigate({url: "data:text/html,Hello!"});
 })
