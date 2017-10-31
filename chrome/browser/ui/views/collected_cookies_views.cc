d0eb05460c575fdc74943ff272d32a15fb62fed0
Elly Fong-Jones <ellyjones@chromium.org>
2017-10-31 19:40:10 +0000

    views: increase font size on collected cookies top labels
    These are meant to be 13pt, not 12pt.
    
    Bug: 610428
    Change-Id: I3718197e67e2eef26611572dfcbfdb40ffcce405
    Reviewed-on: https://chromium-review.googlesource.com/746367
    Commit-Queue: Bret Sepulveda <bsep@chromium.org>
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512908}

--- chrome/browser/ui/views/collected_cookies_views.cc
+++ chrome/browser/ui/views/collected_cookies_views.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/collected_cookies_infobar_delegate.h"
 #include "chrome/browser/ui/views/cookie_info_view.h"
 #include "chrome/browser/ui/views/harmony/chrome_layout_provider.h"
+#include "chrome/browser/ui/views/harmony/chrome_typography.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/constrained_window/constrained_window_views.h"
@@ -409,8 +410,9 @@ views::View* CollectedCookiesViews::CreateAllowedPane() {
       TabSpecificContentSettings::FromWebContents(web_contents_);
 
   // Create the controls that go into the pane.
-  allowed_label_ = new views::Label(l10n_util::GetStringUTF16(
-      IDS_COLLECTED_COOKIES_ALLOWED_COOKIES_LABEL));
+  allowed_label_ = new views::Label(
+      l10n_util::GetStringUTF16(IDS_COLLECTED_COOKIES_ALLOWED_COOKIES_LABEL),
+      CONTEXT_BODY_TEXT_LARGE);
   allowed_label_->SetHorizontalAlignment(gfx::ALIGN_LEFT);
 
   allowed_cookies_tree_model_ =
@@ -480,9 +482,10 @@ views::View* CollectedCookiesViews::CreateBlockedPane() {
   // Create the controls that go into the pane.
   blocked_label_ = new views::Label(
       l10n_util::GetStringUTF16(
-          prefs->GetBoolean(prefs::kBlockThirdPartyCookies) ?
-              IDS_COLLECTED_COOKIES_BLOCKED_THIRD_PARTY_BLOCKING_ENABLED :
-              IDS_COLLECTED_COOKIES_BLOCKED_COOKIES_LABEL));
+          prefs->GetBoolean(prefs::kBlockThirdPartyCookies)
+              ? IDS_COLLECTED_COOKIES_BLOCKED_THIRD_PARTY_BLOCKING_ENABLED
+              : IDS_COLLECTED_COOKIES_BLOCKED_COOKIES_LABEL),
+      CONTEXT_BODY_TEXT_LARGE);
   blocked_label_->SetMultiLine(true);
   blocked_label_->SetHorizontalAlignment(gfx::ALIGN_LEFT);
   blocked_label_->SizeToFit(kTreeViewWidth);
