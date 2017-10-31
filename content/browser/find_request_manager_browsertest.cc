03f84b2329311fbbe372b917cb031c218e8a522b
Makoto Shimazu <shimazu@chromium.org>
2017-10-31 04:30:07 +0000

    Disable FindRequestManagerTest.FindInPage_Issue627799 on Win dbg
    It's timeout frequently.
    
    Bug: 779912
    
    TBR=paulmeyer@chromium.org
    
    Change-Id: If0311d4f11119bfebeda1447dd8ac8003ba1ff16
    Reviewed-on: https://chromium-review.googlesource.com/745704
    Commit-Queue: Makoto Shimazu <shimazu@chromium.org>
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512740}

--- content/browser/find_request_manager_browsertest.cc
+++ content/browser/find_request_manager_browsertest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/strings/utf_string_conversions.h"
+#include "build/build_config.h"
 #include "content/browser/web_contents/web_contents_impl.h"
 #include "content/public/browser/notification_types.h"
 #include "content/public/common/content_switches.h"
@@ -446,7 +447,15 @@ IN_PROC_BROWSER_TEST_P(FindRequestManagerTest, MAYBE(FindNewMatches)) {
   EXPECT_EQ(4, results.active_match_ordinal);
 }
 
-IN_PROC_BROWSER_TEST_F(FindRequestManagerTest, MAYBE(FindInPage_Issue627799)) {
+// TODO(crbug.com/615291): These tests frequently fail on Android.
+// TODO(crbug.com/779912): Flaky timeout on Win7 (dbg).
+#if defined(OS_ANDROID) || (defined(OS_WIN) && !defined(NDEBUG))
+#define MAYBE_FindInPage_Issue627799 DISABLED_FindInPage_Issue627799
+#else
+#define MAYBE_FindInPage_Issue627799 FindInPage_Issue627799
+#endif
+
+IN_PROC_BROWSER_TEST_F(FindRequestManagerTest, MAYBE_FindInPage_Issue627799) {
   LoadAndWait("/find_in_long_page.html");
 
   blink::WebFindOptions options;
