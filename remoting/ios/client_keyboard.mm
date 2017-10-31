2dd1700eae253df74679102c0992438128234a39
Yuwei Huang <yuweih@chromium.org>
2017-10-31 18:51:53 +0000

    [CRD iOS] Fix typo in ClientKeyboard
    There are duplicated lines of _autocorrectionType but the second one is
    supposed to be _keyboardAppearance. UIKeyboardAppearanceDefault is the
    default value so we weren't seeing bugs before.
    
    Change-Id: I3c91a3496e62f778259c13fd6132ffbc28633ebd
    Reviewed-on: https://chromium-review.googlesource.com/744979
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512889}

--- remoting/ios/client_keyboard.mm
+++ remoting/ios/client_keyboard.mm
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   if (self) {
     _autocapitalizationType = UITextAutocapitalizationTypeNone;
     _autocorrectionType = UITextAutocorrectionTypeNo;
-    _autocorrectionType = UITextAutocorrectionTypeNo;
+    _keyboardAppearance = UIKeyboardAppearanceDefault;
     _keyboardType = UIKeyboardTypeDefault;
     _spellCheckingType = UITextSpellCheckingTypeNo;
 
