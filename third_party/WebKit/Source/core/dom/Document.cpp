6292ebcf82bac443c284f6eec8f760834c02bba2
Eric Willigers <ericwilligers@chromium.org>
2017-10-31 08:02:23 +0000

    Document: Use counters for stylesheetSet attributes
    We add use counters for the following attributes:
    attribute DOMString? selectedStylesheetSet;
    readonly attribute DOMString? preferredStylesheetSet;
    
    BUG=690600,690609
    
    Change-Id: I0edf7fc6ec5e3e9295d36f18ad5853ffabfa6c1e
    Reviewed-on: https://chromium-review.googlesource.com/745322
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Bugs Nash <bugsnash@chromium.org>
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512775}

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -4333,10 +4333,12 @@ String Document::preferredStylesheetSet() const {
 }
 
 String Document::selectedStylesheetSet() const {
+  UseCounter::Count(*this, WebFeature::kDocumentGetSelectedStylesheetSet);
   return style_engine_->SelectedStylesheetSetName();
 }
 
 void Document::setSelectedStylesheetSet(const String& a_string) {
+  UseCounter::Count(*this, WebFeature::kDocumentSetSelectedStylesheetSet);
   GetStyleEngine().SetSelectedStylesheetSetName(a_string);
 }
 
