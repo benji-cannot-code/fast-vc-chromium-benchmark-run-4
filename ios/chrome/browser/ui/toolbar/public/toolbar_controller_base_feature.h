b4c915f193194f11ed976177a363ffd215028483
sczs <sczs@chromium.org>
2017-10-31 23:34:06 +0000

    [ios] Creates UIViewPropertyAnimator for location bar expansion.
    Creates animations for expanding and contracting the location bar/omnibox
    using entirely UIViewPropertyAnimators.
    
    The feature is behind a new flag that is disabled by default.
    
    Real time video:
    https://drive.google.com/open?id=0Byo6-Nuda2jgVE1yTjBPRU9ybjg
    
    Slow Animations video:
    https://drive.google.com/open?id=0Byo6-Nuda2jgR21YM3k0amEwUEE
    
    Bug: 
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ib3a4f8f7aac9e72b674b881af2f463fabf76393a
    Reviewed-on: https://chromium-review.googlesource.com/729839
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512999}

--- ios/chrome/browser/ui/toolbar/public/toolbar_controller_base_feature.h
+++ ios/chrome/browser/ui/toolbar/public/toolbar_controller_base_feature.h
@@ -11,4 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Feature to choose whether the toolbar respects the safe area.
 extern const base::Feature kSafeAreaCompatibleToolbar;
 
+// Feature to choose whether the toolbar uses UIViewPropertyAnimators.
+extern const base::Feature kPropertyAnimationsToolbar;
+
 #endif  // IOS_CHROME_BROWSER_UI_TOOLBAR_TOOLBAR_CONTROLLER_BASE_FEATURE_H_
--- ios/chrome/browser/ui/toolbar/public/toolbar_controller_base_feature.mm
+++ ios/chrome/browser/ui/toolbar/public/toolbar_controller_base_feature.mm
@@ -7,3 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 const base::Feature kSafeAreaCompatibleToolbar{
     "SafeAreaCompatibleToolbar", base::FEATURE_DISABLED_BY_DEFAULT};
+
+extern const base::Feature kPropertyAnimationsToolbar{
+    "PropertyAnimationsToolbar", base::FEATURE_DISABLED_BY_DEFAULT};
