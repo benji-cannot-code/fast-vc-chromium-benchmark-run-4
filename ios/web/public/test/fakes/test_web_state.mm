0c0f45b7e63be4668c3475271af827548e60c375
John Z Wu <jzw@chromium.org>
2017-10-31 17:10:50 +0000

    Add js injection receiver methods to test web state.
    Bug: 
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ia5890c49825428177dbdc4c2a9429616bb92d747
    Reviewed-on: https://chromium-review.googlesource.com/744521
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Hiroshi Ichikawa <ichikawa@chromium.org>
    Commit-Queue: John Wu <jzw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512852}

--- ios/web/public/test/fakes/test_web_state.mm
+++ ios/web/public/test/fakes/test_web_state.mm
@@ -129,7 +129,7 @@ void TestWebState::SetIsEvicted(bool value) {
 }
 
 CRWJSInjectionReceiver* TestWebState::GetJSInjectionReceiver() const {
-  return nullptr;
+  return injection_receiver_;
 }
 
 void TestWebState::ExecuteJavaScript(const base::string16& javascript) {}
@@ -172,6 +172,11 @@ WebInterstitial* TestWebState::GetWebInterstitial() const {
 
 void TestWebState::SetBrowserState(BrowserState* browser_state) {
   browser_state_ = browser_state;
+}
+
+void TestWebState::SetJSInjectionReceiver(
+    CRWJSInjectionReceiver* injection_receiver) {
+  injection_receiver_ = injection_receiver;
 }
 
 void TestWebState::SetContentIsHTML(bool content_is_html) {
