f127508f27dea9a16ab55848e28714b2af7f2950
gogerald <gogerald@google.com>
2017-10-31 23:20:24 +0000

    [Payments] Filtering service worker payment apps based on capabilities
    This CL parses and stores 'basic-card' specific capabilities, and uses
    them to filter service worker payment apps.
    
    The capabilities will also be used to filter modifiers.
    
    Bug: 777470
    Change-Id: Ifab1054ec3bccd4f279cde5348cc68abf504a88d
    Reviewed-on: https://chromium-review.googlesource.com/730845
    Commit-Queue: Ganggui Tang <gogerald@chromium.org>
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Reviewed-by: Chris Palmer <palmer@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512994}

--- chrome/browser/android/payments/service_worker_payment_app_bridge.cc
+++ chrome/browser/android/payments/service_worker_payment_app_bridge.cc
@@ -38,6 +38,7 @@ using ::base::android::JavaRef;
 using ::base::android::ScopedJavaGlobalRef;
 using ::base::android::ScopedJavaLocalRef;
 using ::base::android::ToJavaArrayOfStrings;
+using ::base::android::ToJavaIntArray;
 using ::payments::mojom::CanMakePaymentEventData;
 using ::payments::mojom::CanMakePaymentEventDataPtr;
 using ::payments::mojom::PaymentCurrencyAmount;
@@ -106,6 +107,18 @@ void OnGotAllPaymentApps(const JavaRef<jobject>& jweb_contents,
       }
     }
 
+    base::android::ScopedJavaLocalRef<jobjectArray> jcapabilities =
+        Java_ServiceWorkerPaymentAppBridge_createCapabilities(
+            env, app_info.second->capabilities.size());
+    for (size_t i = 0; i < app_info.second->capabilities.size(); i++) {
+      Java_ServiceWorkerPaymentAppBridge_addCapabilities(
+          env, jcapabilities, base::checked_cast<int>(i),
+          ToJavaIntArray(
+              env, app_info.second->capabilities[i].supported_card_networks),
+          ToJavaIntArray(
+              env, app_info.second->capabilities[i].supported_card_types));
+    }
+
     Java_ServiceWorkerPaymentAppBridge_onPaymentAppCreated(
         env, app_info.second->registration_id,
         ConvertUTF8ToJavaString(env, app_info.second->scope.spec()),
@@ -123,6 +136,7 @@ void OnGotAllPaymentApps(const JavaRef<jobject>& jweb_contents,
             ? nullptr
             : gfx::ConvertToJavaBitmap(app_info.second->icon.get()),
         ToJavaArrayOfStrings(env, app_info.second->enabled_methods),
+        jcapabilities,
         ToJavaArrayOfStrings(env, preferred_related_application_ids),
         jweb_contents, jcallback);
   }
