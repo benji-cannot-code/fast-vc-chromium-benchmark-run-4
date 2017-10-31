730382c58d84d35c3abd26c897aeec291bac226e
Darren Shen <shend@chromium.org>
2017-10-31 05:59:04 +0000

    [css-typed-om] Add tests for the readonly part of StylePropertyMap.
    Spec: https://drafts.css-houdini.org/css-typed-om-1/#stylepropertymapreadonly
    
    We test get, getAll, has, iterable and getProperties through the inline
    StylePropertyMap.
    
    Currently failing:
    - Custom properties seem to be returned as CSSStyleValues and not
      CSSUnparsedValues
    - properties are not sorted correctly in iteration and getProperties.
    
    Bug: 774887
    Change-Id: I613329005030355e137d19063c5ad7e6247156ed
    Reviewed-on: https://chromium-review.googlesource.com/722400
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: Renée Wright <rjwright@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512757}

--- third_party/WebKit/LayoutTests/typedcssom/resources/testhelper.js
+++ third_party/WebKit/LayoutTests/typedcssom/resources/testhelper.js
@@ -1,5 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 const style_value_attributes = {
+  'CSSKeywordValue': ['value'],
   'CSSUnitValue': ['unit', 'value'],
 };
 
@@ -9,5 +10,13 @@ function assert_style_value_equals(a, b) {
   assert_equals(a.constructor.name, b.constructor.name);
   for (const attr of style_value_attributes[a.constructor.name]) {
     assert_equals(a[attr], b[attr], attr);
+  }
+}
+
+// Compares two arrays of CSSStyleValues to check if every element is equal
+function assert_style_value_array_equals(a, b) {
+  assert_equals(a.length, b.length);
+  for (let i = 0; i < a.length; i++) {
+    assert_style_value_equals(a[i], b[i]);
   }
 }
