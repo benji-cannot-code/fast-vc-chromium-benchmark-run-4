1e159a09b19c395af24516092e00972728f65770
Ken Rockot <rockot@chromium.org>
2017-10-31 01:50:59 +0000

    Reland "Always send subresource ULF on navigation"
    This is a reland of bba21f276796f75e0d427fe3a224088e48f375e6
    Original change's description:
    > Always send subresource ULF on navigation
    > 
    > This changes the Network Service-enabled browser-side navigation path
    > to always send a default subresource URLLoaderFactory to the render
    > frame on navigation commit.
    > 
    > In addition to sending a default ULF, we also send one for blobs.
    > Ultimately the idea here is to have the browser, at navigation time,
    > send a frame everything it will need to request any accessible
    > subresources, making it easier to reason about what subresource
    > capabilities any given frame has.
    > 
    > This set of capabilities is encapsulated by a new
    > URLLoaderFactoryBundle type, which can be serialized over mojom and is
    > now passed in new-document navigation commit messages.
    > 
    > Follow-up usage of URLLoaderFactoryBundle will support loading things
    > like file:// and chrome-extension:// subresources when appropriate.
    > 
    > Note that this does not yet change how workers acquire ULFs for their
    > own resource requests.
    > 
    > Bug: 721414,759230
    > Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    > Change-Id: I47b0897c7c012292633d5f08c874a18cc0fb1dcb
    > Reviewed-on: https://chromium-review.googlesource.com/740904
    > Commit-Queue: Ken Rockot <rockot@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#512459}
    
    Bug: 721414, 759230
    Change-Id: Ie13bcd97b7a5af8dbe4263a70239ba124205da15
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    
    TBR=dcheng@chromium.org
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: Ie13bcd97b7a5af8dbe4263a70239ba124205da15
    Reviewed-on: https://chromium-review.googlesource.com/744222
    Commit-Queue: Ken Rockot <rockot@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512706}

---
+++ content/common/url_loader_factory_bundle_struct_traits.h
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_COMMON_URL_LOADER_FACTORY_BUNDLE_STRUCT_TRAITS_H_
+#define CONTENT_COMMON_URL_LOADER_FACTORY_BUNDLE_STRUCT_TRAITS_H_
+
+#include "content/common/url_loader_factory_bundle.h"
+#include "content/common/url_loader_factory_bundle.mojom-shared.h"
+#include "mojo/public/cpp/bindings/struct_traits.h"
+
+namespace mojo {
+
+template <>
+struct StructTraits<content::mojom::URLLoaderFactoryBundleDataView,
+                    content::URLLoaderFactoryBundle> {
+  static content::mojom::URLLoaderFactoryPtr default_factory(
+      content::URLLoaderFactoryBundle& bundle);
+
+  static std::map<std::string, content::mojom::URLLoaderFactoryPtr> factories(
+      content::URLLoaderFactoryBundle& bundle);
+
+  static bool Read(content::mojom::URLLoaderFactoryBundleDataView data,
+                   content::URLLoaderFactoryBundle* out_bundle);
+};
+
+}  // namespace mojo
+
+#endif  // CONTENT_COMMON_URL_LOADER_FACTORY_BUNDLE_STRUCT_TRAITS_H_
