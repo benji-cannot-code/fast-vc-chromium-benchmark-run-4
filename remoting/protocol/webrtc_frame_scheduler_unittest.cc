9b9d282d323cc0394efce74245fb52086ac48993
Lambros Lambrou <lambroslambrou@chromium.org>
2017-10-31 19:50:19 +0000

    [remoting] Add unittests for WebrtcFrameSchedulerSimple
    This adds unittests for the changes made in
    crrev.com/a6a3f1e839e900f335350c8555a973cadce46386
    
    This requires a small refactoring to allow unittests to fake the
    current time for the WebrtcFrameSchedulerSimple class.
    
    Bug: 773894
    Change-Id: I00e1204b1eab446e18e5d8796770dbd64623abfc
    Reviewed-on: https://chromium-review.googlesource.com/729565
    Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    Reviewed-by: Zijie He <zijiehe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512914}

--- remoting/protocol/webrtc_frame_scheduler_unittest.cc
+++ remoting/protocol/webrtc_frame_scheduler_unittest.cc
@@ -10,6 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/protocol/webrtc_dummy_video_encoder.h"
 #include "remoting/protocol/webrtc_frame_scheduler_simple.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/webrtc/modules/desktop_capture/desktop_frame.h"
+
+using webrtc::BasicDesktopFrame;
+using webrtc::DesktopRect;
+using webrtc::DesktopSize;
 
 namespace remoting {
 namespace protocol {
@@ -18,9 +23,11 @@ class WebrtcFrameSchedulerTest : public ::testing::Test {
  public:
   WebrtcFrameSchedulerTest()
       : task_runner_(new base::TestSimpleTaskRunner()),
-        task_runner_handle_(task_runner_.get()) {
+        task_runner_handle_(task_runner_.get()),
+        now_(base::TimeTicks::Now()) {
     video_encoder_factory_.reset(new WebrtcDummyVideoEncoderFactory());
     scheduler_.reset(new WebrtcFrameSchedulerSimple());
+    scheduler_->SetCurrentTimeForTest(now_);
     scheduler_->Start(video_encoder_factory_.get(),
                       base::Bind(&WebrtcFrameSchedulerTest::CaptureCallback,
                                  base::Unretained(this)));
@@ -39,9 +46,11 @@ class WebrtcFrameSchedulerTest : public ::testing::Test {
   base::ThreadTaskRunnerHandle task_runner_handle_;
 
   std::unique_ptr<WebrtcDummyVideoEncoderFactory> video_encoder_factory_;
-  std::unique_ptr<WebrtcFrameScheduler> scheduler_;
+  std::unique_ptr<WebrtcFrameSchedulerSimple> scheduler_;
 
   bool capture_callback_called_ = false;
+
+  base::TimeTicks now_;
 };
 
 TEST_F(WebrtcFrameSchedulerTest, UpdateBitrateWhenPending) {
@@ -62,6 +71,48 @@ TEST_F(WebrtcFrameSchedulerTest, UpdateBitrateWhenPending) {
   // waiting for the previous capture request to complete.
   EXPECT_FALSE(task_runner_->HasPendingTask());
 }
+
+TEST_F(WebrtcFrameSchedulerTest, EmptyFrameUpdate_ShouldNotBeSentImmediately) {
+  auto video_channel_observer =
+      video_encoder_factory_->get_video_channel_state_observer_for_tests();
+  // Needed to avoid DCHECK in OnFrameCaptured().
+  video_channel_observer->OnTargetBitrateChanged(100);
+
+  WebrtcVideoEncoder::FrameParams outParams;
+  BasicDesktopFrame frame(DesktopSize(1, 1));
+  // Initial capture, full frame.
+  frame.mutable_updated_region()->SetRect(DesktopRect::MakeWH(1, 1));
+  scheduler_->OnFrameCaptured(&frame, &outParams);
+  // Empty frame.
+  frame.mutable_updated_region()->Clear();
+  bool result = scheduler_->OnFrameCaptured(&frame, &outParams);
+
+  // Should not be sent, because of throttling of empty frames.
+  EXPECT_FALSE(result);
+};
+
+TEST_F(WebrtcFrameSchedulerTest, EmptyFrameUpdate_ShouldBeSentAfter200ms) {
+  // Identical to the previous test, except it waits a short amount of time
+  // before the empty frame update.
+  auto video_channel_observer =
+      video_encoder_factory_->get_video_channel_state_observer_for_tests();
+  video_channel_observer->OnTargetBitrateChanged(100);
+
+  WebrtcVideoEncoder::FrameParams outParams;
+  BasicDesktopFrame frame(DesktopSize(1, 1));
+  // Initial capture, full frame.
+  frame.mutable_updated_region()->SetRect(DesktopRect::MakeWH(1, 1));
+  scheduler_->OnFrameCaptured(&frame, &outParams);
+  // Wait more than 200ms.
+  scheduler_->SetCurrentTimeForTest(now_ +
+                                    base::TimeDelta::FromMilliseconds(300));
+  // Empty frame.
+  frame.mutable_updated_region()->Clear();
+  bool result = scheduler_->OnFrameCaptured(&frame, &outParams);
+
+  // Empty frames should be sent at the throttled rate.
+  EXPECT_TRUE(result);
+};
 
 // TODO(sergeyu): Add more unittests.
 
