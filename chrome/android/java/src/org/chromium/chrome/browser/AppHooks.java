d332d928ea4ca484e4532d586d187ee0eae11505
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2017-11-01 01:05:56 +0000

    Extract partner customization for overriding
    Some of the partner customization logic is extracted to AppHooks,
    so that it can be overriden in downstream.
    
    Bug: 778805
    Change-Id: I6dcd4b0a14ed76f7a9c447452ce313f59ca98a65
    Reviewed-on: https://chromium-review.googlesource.com/741086
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513033}

--- chrome/android/java/src/org/chromium/chrome/browser/AppHooks.java
+++ chrome/android/java/src/org/chromium/chrome/browser/AppHooks.java
@@ -35,6 +35,7 @@ import org.chromium.chrome.browser.offlinepages.CCTRequestStatus;
 import org.chromium.chrome.browser.omaha.RequestGenerator;
 import org.chromium.chrome.browser.partnerbookmarks.PartnerBookmark;
 import org.chromium.chrome.browser.partnerbookmarks.PartnerBookmarksProviderIterator;
+import org.chromium.chrome.browser.partnercustomizations.PartnerBrowserCustomizations;
 import org.chromium.chrome.browser.physicalweb.PhysicalWebBleClient;
 import org.chromium.chrome.browser.policy.PolicyAuditor;
 import org.chromium.chrome.browser.preferences.LocationSettings;
@@ -322,5 +323,13 @@ public abstract class AppHooks {
     @Nullable
     public PartnerBookmark.BookmarkIterator getPartnerBookmarkIterator() {
         return PartnerBookmarksProviderIterator.createIfAvailable();
+    }
+
+    /**
+     * @return An instance of PartnerBrowserCustomizations.Provider that provides customizations
+     * specified by partners.
+     */
+    public PartnerBrowserCustomizations.Provider getCustomizationProvider() {
+        return new PartnerBrowserCustomizations.ProviderPackage();
     }
 }
