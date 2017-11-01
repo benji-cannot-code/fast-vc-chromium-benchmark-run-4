3bb18b38bd219a1dc6ebfe42c42cd7299c2f70de
Eugene But <eugenebut@google.com>
2017-11-01 21:32:00 +0000

    Added WARN_UNUSED_RESULT annotation to WaitUntilConditionOrTimeout.
    Ignoring the result of WaitUntilConditionOrTimeout call is incorrect in
    many cases. CL adds WARN_UNUSED_RESULT annotation to
    WaitUntilConditionOrTimeout to force callers to do the checks.
    
    Bug: 780062
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I96daa8dfc6ec280d7f622a91f474714fe48fe3bf
    Reviewed-on: https://chromium-review.googlesource.com/748108
    Reviewed-by: Mike Baxley <baxley@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513277}

--- ios/testing/wait_util.h
+++ ios/testing/wait_util.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 
+#include "base/compiler_specific.h"
 #import "base/ios/block_types.h"
 
 namespace testing {
@@ -30,7 +31,7 @@ extern const NSTimeInterval kWaitForPageLoadTimeout;
 // Returns true when condition() becomes true, otherwise returns false after
 // |timeout|.
 bool WaitUntilConditionOrTimeout(NSTimeInterval timeout,
-                                 ConditionBlock condition);
+                                 ConditionBlock condition) WARN_UNUSED_RESULT;
 
 }  // namespace testing
 
