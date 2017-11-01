7d33f74821f677b883d2611f108da1c22ff2f458
Hidehiko Abe <hidehiko@chromium.org>
2017-11-01 15:21:26 +0000

    Refactor test fixture for network_configuration_handler_unittest.cc
    This is preparation to remove gmock use in the file.
    This CL renames classes;
    - NetworkConfigurationHandlerTest -> NetworkConfigurationHandlerMockTest.
      now deprecated.
    - NetworkConfigurationHandlerStubTest -> NetworkConfigurationHandlerTest.
      This will be the main test fixture in this file. All MockTest TEST_F
      will be migrated into this.
    - TestObserver -> TestNetworkStateHandlerObserver. Ditto for var name.
      We have two Observer test implementation, so for more clarification.
    
    Also, moves classes to above.
    For migration, NetworkConfigurationHandlerMockTest TEST_F will be
    switched into NetworkConfigurationHandlerTest one-by-one, so that
    on code review, taking diff will become slightly easier.
    
    BUG=234463
    TEST=Ran bots.
    
    Change-Id: Iae1c82f814aa7a0a085e4d177e9193dea122e7d4
    Reviewed-on: https://chromium-review.googlesource.com/747341
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513132}

--- chromeos/network/network_configuration_handler.h
+++ chromeos/network/network_configuration_handler.h
@@ -146,7 +146,7 @@ class CHROMEOS_EXPORT NetworkConfigurationHandler
   friend class ClientCertResolverTest;
   friend class NetworkHandler;
   friend class NetworkConfigurationHandlerTest;
-  friend class NetworkConfigurationHandlerStubTest;
+  friend class NetworkConfigurationHandlerMockTest;
   class ProfileEntryDeleter;
 
   NetworkConfigurationHandler();
