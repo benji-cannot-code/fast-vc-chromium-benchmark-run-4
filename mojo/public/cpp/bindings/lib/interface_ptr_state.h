24926ef29a7d113aaf1655b97d4fa81e5b496721
Reilly Grant <reillyg@chromium.org>
2017-11-01 00:55:48 +0000

    Devirtualize calls made on a mojo::InterfacePtr<T>
    This patch devirtualizes calls to interface methods made through a
    mojo::InterfacePtr<T> and mojo::AssociatedInterfacePtr<T> by making
    get() and the -> and . operators return the proxy type directly and
    marking the interface methods on the proxy as final.
    
    The motivation of this change is performance and binary size. The non-
    virtual call sites are slightly smaller but not enough to effect the
    complete binary. Performance should be net-positive but as yet has not
    been accurately measured.
    
    Bug: None
    Change-Id: I463bc32c1989853606d118f57ded5c9dd9f34e2f
    Reviewed-on: https://chromium-review.googlesource.com/747884
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Yuzhu Shen <yzshen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513030}

--- mojo/public/cpp/bindings/lib/interface_ptr_state.h
+++ mojo/public/cpp/bindings/lib/interface_ptr_state.h
@@ -105,10 +105,12 @@ class MOJO_CPP_BINDINGS_EXPORT InterfacePtrStateBase {
 template <typename Interface>
 class InterfacePtrState : public InterfacePtrStateBase {
  public:
+  using Proxy = typename Interface::Proxy_;
+
   InterfacePtrState() = default;
   ~InterfacePtrState() = default;
 
-  Interface* instance() {
+  Proxy* instance() {
     ConfigureProxyIfNecessary();
 
     // This will be null if the object is not bound.
@@ -193,8 +195,6 @@ class InterfacePtrState : public InterfacePtrStateBase {
   }
 
  private:
-  using Proxy = typename Interface::Proxy_;
-
   void ConfigureProxyIfNecessary() {
     // The proxy has been configured.
     if (proxy_) {
