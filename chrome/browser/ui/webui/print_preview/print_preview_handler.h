383c1e16a7025fc64137dc7c2b2b2066fc82713b
rbpotter <rbpotter@chromium.org>
2017-11-01 00:25:54 +0000

    PrintPreviewHandler: Add second preview test
    Test that PrintPreviewHandler forwards renderer messages from the
    PrintPreviewUI to the Print Preview javascript correctly, and that it
    tries to kill the renderer if it receives unexpected messages.
    
    Bug: None
    Change-Id: Iec26fcd115270a167de6370aba2b90d5c68cf380
    Reviewed-on: https://chromium-review.googlesource.com/742649
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513021}

--- chrome/browser/ui/webui/print_preview/print_preview_handler.h
+++ chrome/browser/ui/webui/print_preview/print_preview_handler.h
@@ -102,10 +102,6 @@ class PrintPreviewHandler
     return regenerate_preview_request_count_;
   }
 
-  // Shuts down the initiator renderer. Called when a bad IPC message is
-  // received.
-  void BadMessageReceived();
-
   // Notifies PDF Printer Handler that |path| was selected. Used for tests.
   void FileSelectedForTesting(const base::FilePath& path,
                               int index,
@@ -123,6 +119,10 @@ class PrintPreviewHandler
  protected:
   // Protected so unit tests can override.
   virtual PrinterHandler* GetPrinterHandler(printing::PrinterType printer_type);
+
+  // Shuts down the initiator renderer. Called when a bad IPC message is
+  // received.
+  virtual void BadMessageReceived();
 
   // Gets the initiator for the print preview dialog.
   virtual content::WebContents* GetInitiator() const;
@@ -142,6 +142,7 @@ class PrintPreviewHandler
   FRIEND_TEST_ALL_PREFIXES(PrintPreviewHandlerTest, GetPrinterCapabilities);
   FRIEND_TEST_ALL_PREFIXES(PrintPreviewHandlerTest, Print);
   FRIEND_TEST_ALL_PREFIXES(PrintPreviewHandlerTest, GetPreview);
+  FRIEND_TEST_ALL_PREFIXES(PrintPreviewHandlerTest, SendPreviewUpdates);
   class AccessTokenService;
 
   content::WebContents* preview_web_contents() const;
