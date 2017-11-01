b856fba239b56f1a7c5096dc6322db535b629a24
Chris Hamilton <chrisha@chromium.org>
2017-11-01 23:04:29 +0000

    Create TabManager.TabLifetime UKM.
    BUG=776376
    
    Change-Id: I85295f70e14f216c11ecf6c334ee2a39990a9672
    Reviewed-on: https://chromium-review.googlesource.com/746532
    Commit-Queue: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513310}

--- chrome/browser/resource_coordinator/tab_manager_web_contents_data.h
+++ chrome/browser/resource_coordinator/tab_manager_web_contents_data.h
@@ -170,6 +170,9 @@ class TabManager::WebContentsData
     int discard_count;
     // Is the tab playing audio?
     bool is_recently_audible;
+    // The navigation time associated with this tab. Useful as a reference time
+    // from which to measure UKM event timings.
+    base::TimeTicks navigation_time;
     // Last time the tab started or stopped playing audio (we record the
     // transition time).
     base::TimeTicks last_audio_change_time;
@@ -196,6 +199,7 @@ class TabManager::WebContentsData
   // for more details.
   base::TimeTicks NowTicks() const;
 
+  void ReportUKMWhenTabIsClosed();
   void ReportUKMWhenBackgroundTabIsClosedOrForegrounded(bool is_foregrounded);
 
   // Contains all the needed data for the tab.
