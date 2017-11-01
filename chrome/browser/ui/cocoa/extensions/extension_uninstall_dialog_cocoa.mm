f6a4df0d75143386b520ad7af28d37579daebb26
Robert Liao <robliao@chromium.org>
2017-11-01 21:10:54 +0000

    Add a ScopedNestableTaskAllower to Fix Tab Navigation
    MacOS's alert dialog seems to need allowing nestable tasks for tab
    navigation to work.
    
    BUG=778562
    
    Change-Id: Ic8654defa4e6ec1c7089639108e8a937e923b486
    Reviewed-on: https://chromium-review.googlesource.com/747849
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513270}

--- chrome/browser/ui/cocoa/extensions/extension_uninstall_dialog_cocoa.mm
+++ chrome/browser/ui/cocoa/extensions/extension_uninstall_dialog_cocoa.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #import "base/mac/scoped_nsobject.h"
+#include "base/message_loop/message_loop.h"
 #import "base/strings/sys_string_conversions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/ui/cocoa/browser_dialogs_views_mac.h"
@@ -70,7 +71,13 @@ void ExtensionUninstallDialogCocoa::Show() {
     [alert setAccessoryView:reportAbuseCheckbox];
   }
 
-  if ([alert runModal] == NSAlertFirstButtonReturn) {
+  NSModalResponse response;
+  {
+    base::MessageLoop::ScopedNestableTaskAllower allow_nested(
+        base::MessageLoop::current());
+    response = [alert runModal];
+  }
+  if (response == NSAlertFirstButtonReturn) {
     bool report_abuse_checked =
         reportAbuseCheckbox.get() && [reportAbuseCheckbox state] == NSOnState;
     OnDialogClosed(report_abuse_checked ?
