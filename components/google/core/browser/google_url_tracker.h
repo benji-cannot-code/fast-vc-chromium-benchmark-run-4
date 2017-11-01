4e9c077b921b747f232e053b49c70197a4088e3a
Helen Li <xunjieli@chromium.org>
2017-11-01 20:37:05 +0000

    Fix documentation in components/google/core/browser/google_url_tracker.h
    The overridden method is from NetworkChangeObserver and not from
    IPAddressObserver.
    
    TBR=isherman@chromium.org
    
    Bug: 754695
    Change-Id: Ic37fccfce1abc1e89ad6300b652416cd73a6f39b
    Reviewed-on: https://chromium-review.googlesource.com/749302
    Reviewed-by: Helen Li <xunjieli@chromium.org>
    Commit-Queue: Helen Li <xunjieli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513253}

--- components/google/core/browser/google_url_tracker.h
+++ components/google/core/browser/google_url_tracker.h
@@ -82,7 +82,7 @@ class GoogleURLTracker
   // net::URLFetcherDelegate:
   void OnURLFetchComplete(const net::URLFetcher* source) override;
 
-  // NetworkChangeNotifier::IPAddressObserver:
+  // NetworkChangeNotifier::NetworkChangeObserver:
   void OnNetworkChanged(
       net::NetworkChangeNotifier::ConnectionType type) override;
 
