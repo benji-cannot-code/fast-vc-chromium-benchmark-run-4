14a50f939a9fa451080d81ba8f5ceffb33c47e1b
Ian Vollick <vollick@chromium.org>
2017-11-01 20:04:03 +0000

    [vr] Hide the voice search button in incognito mode
    With this change, the voice search button is disabled via binding when
    we are in incognito mode. Additionally, we store a bit on the model for
    when we've got experimental VR features enabled so that it may more
    easily be bound and toggled in tests. This also means that the ui scene
    manager no longer needs to retain a pointer to the voice search button.
    Further, we no longer add the button to control_elements_ since it will
    inherit the visibility of the URL bar due to being in its subtree.
    
    Bug: 779592
    Change-Id: I92832f46b4cdf33e7a9caf57076001a24ff9ab83
    Reviewed-on: https://chromium-review.googlesource.com/748774
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Reviewed-by: Biao She <bshe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513233}

--- chrome/browser/vr/ui.cc
+++ chrome/browser/vr/ui.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/vr/ui_scene.h"
 #include "chrome/browser/vr/ui_scene_manager.h"
 #include "chrome/browser/vr/vr_shell_renderer.h"
+#include "chrome/common/chrome_features.h"
 
 namespace vr {
 
@@ -34,6 +35,8 @@ Ui::Ui(UiBrowserInterface* browser,
       weak_ptr_factory_(this) {
   model_->started_for_autopresentation =
       ui_initial_state.web_vr_autopresentation_expected;
+  model_->experimental_features_enabled =
+      base::FeatureList::IsEnabled(features::kExperimentalVRFeatures);
 }
 
 Ui::~Ui() = default;
@@ -57,6 +60,7 @@ void Ui::SetToolbarState(const ToolbarState& state) {
 }
 
 void Ui::SetIncognito(bool enabled) {
+  model_->incognito = enabled;
   scene_manager_->SetIncognito(enabled);
 }
 
