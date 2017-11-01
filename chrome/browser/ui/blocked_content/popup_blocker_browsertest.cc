1d1ca06467f829f36d8dc28fecff3a43841510c3
Makoto Shimazu <shimazu@chromium.org>
2017-11-01 05:31:52 +0000

    Disabled PopupBlockerBrowserTest.Regress427477 on Mac
    This test seems flaky when disabling browser-side-navigation according to the
    flakiness dashboard.
    
    TEST=interactive_ui_tests --gtest_filter=PopupBlockerBrowserTest.Regress427477 --disable-browser-side-navigation
    TBR=avi@chromium.org
    
    Bug: 780371
    Change-Id: I2cd1027ebe8ce2d46919e8a492cda078417be935
    Reviewed-on: https://chromium-review.googlesource.com/748503
    Commit-Queue: Makoto Shimazu <shimazu@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513081}

--- chrome/browser/ui/blocked_content/popup_blocker_browsertest.cc
+++ chrome/browser/ui/blocked_content/popup_blocker_browsertest.cc
@@ -52,6 +52,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_widget_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_observer.h"
+#include "content/public/common/browser_side_navigation_policy.h"
 #include "content/public/common/url_constants.h"
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/test_navigation_observer.h"
@@ -609,6 +610,9 @@ IN_PROC_BROWSER_TEST_F(PopupBlockerBrowserTest, DenialOfService) {
 
 // Verify that an onunload popup does not show up for about:blank.
 IN_PROC_BROWSER_TEST_F(PopupBlockerBrowserTest, Regress427477) {
+  // Flaky in this case: crbug.com/780371
+  if (!content::IsBrowserSideNavigationEnabled())
+    return;
   ui_test_utils::NavigateToURL(browser(), GURL(chrome::kChromeUINewTabURL));
   ui_test_utils::NavigateToURL(browser(), GURL(url::kAboutBlankURL));
 
