1eec035b2e3cfc200d3e3f9b1d2c9b94e71b0cd9
Koji Ishii <kojii@chromium.org>
2017-11-01 16:50:21 +0000

    Fix line breaking with text-indent in an anonymous block
    This patch fixes line breaking of a block that meets all following
    conditions:
    1. Has the 'text-indent' property set.
    2. Is in an anonymous block.
    3. Is not the first child of its parent element.
    
    The spec[1] defines not to apply 'text-indent' in this case.
    LayoutBlockFlow honors this definition, but LineBreaker did not.
    
    In this patch, LayoutBlockFlow, LineBreaker, and NGLineBreaker
    shares the logic.
    
    [1] https://drafts.csswg.org/css-text-3/#text-indent-property
    
    Bug: 779921
    Change-Id: Iae81a7a96c1be1b787eb53b4b4a2001b8a68c526
    Reviewed-on: https://chromium-review.googlesource.com/748821
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513155}

--- third_party/WebKit/Source/core/layout/LayoutBlockFlow.h
+++ third_party/WebKit/Source/core/layout/LayoutBlockFlow.h
@@ -111,6 +111,8 @@ class CORE_EXPORT LayoutBlockFlow : public LayoutBlock {
 
   void DeleteLineBoxTree();
 
+  bool CanContainFirstFormattedLine() const;
+
   LayoutUnit AvailableLogicalWidthForLine(
       LayoutUnit position,
       IndentTextOrNot indent_text,
