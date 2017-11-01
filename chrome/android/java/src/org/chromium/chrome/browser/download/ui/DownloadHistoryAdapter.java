60988d339054df98245e1fad27153222c2b08e67
Shakti Sahu <shaktisahu@chromium.org>
2017-11-01 16:38:02 +0000

    Download Home : Fixed storage size after the download is finished
    We need to update the storage header on download home as the download
    progresses. However, we may not probably need to do this for every update,
    only checking this on completion should suffice.
    
    Bug: 778985
    Change-Id: I03ff2060fa05409f9d3bbce0ca41acd592e5c386
    Reviewed-on: https://chromium-review.googlesource.com/747816
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513149}

--- chrome/android/java/src/org/chromium/chrome/browser/download/ui/DownloadHistoryAdapter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/ui/DownloadHistoryAdapter.java
@@ -429,6 +429,9 @@ public class DownloadHistoryAdapter extends DateDividedAdapter
                     }
                     if (TextUtils.equals(item.getId(), wrapper.getId())) {
                         view.displayItem(mBackendProvider, existingWrapper);
+                        if (item.getDownloadInfo().state() == DownloadState.COMPLETE) {
+                            mSpaceDisplay.onChanged();
+                        }
                     }
                 }
 
@@ -830,6 +833,9 @@ public class DownloadHistoryAdapter extends DateDividedAdapter
                 for (DownloadItemView view : mViews) {
                     if (TextUtils.equals(item.id.id, view.getItem().getId())) {
                         view.displayItem(mBackendProvider, existingWrapper);
+                        if (item.state == OfflineItemState.COMPLETE) {
+                            mSpaceDisplay.onChanged();
+                        }
                     }
                 }
 
