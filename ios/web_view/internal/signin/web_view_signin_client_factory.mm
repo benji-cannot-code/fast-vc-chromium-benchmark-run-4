a40db94067580c438a1ed21b815e8b33c3ded00c
John Z Wu <jzw@chromium.org>
2017-11-01 19:28:15 +0000

    Change signin classes to objc files.
    This allows more flexibility when including header files that contain
    objc references.
    
    Bug: 
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I3bf28ef2c367348906fe7be8481aa348480720a0
    Reviewed-on: https://chromium-review.googlesource.com/747093
    Commit-Queue: John Wu <jzw@chromium.org>
    Reviewed-by: Hiroshi Ichikawa <ichikawa@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513212}

--- ios/web_view/internal/signin/web_view_signin_client_factory.mm
+++ ios/web_view/internal/signin/web_view_signin_client_factory.mm
@@ -16,6 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/web_view/internal/signin/web_view_signin_error_controller_factory.h"
 #include "ios/web_view/internal/web_view_browser_state.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace ios_web_view {
 
 // static
