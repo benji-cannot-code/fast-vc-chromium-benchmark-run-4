4f989b3410836162c3fc903f0f9b489d2eebce66
Ken Rockot <rockot@chromium.org>
2017-11-02 17:35:12 +0000

    Fix SSL browser test FeatureList usage
    Switches several SSL-related browser tests to use a more appropriate
    ScopedFeatureList API to avoid trampling unrelated feature flags. This
    is ultimately to allow for test coverage with the NetworkService
    feature enabled.
    
    Also disables all the affected tests on the network service bot, since
    they fail once we stop trampling the feature flag.
    
    Bug: None
    Change-Id: I2fc22f5e848db8e7d7030d66d30f1d71a69fa8c5
    Reviewed-on: https://chromium-review.googlesource.com/747808
    Commit-Queue: Ken Rockot <rockot@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513538}

--- chrome/browser/ssl/ssl_error_handler.cc
+++ chrome/browser/ssl/ssl_error_handler.cc
@@ -58,8 +58,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/policy/browser_policy_connector_chromeos.h"
 #endif  // #if defined(OS_WIN)
 
-namespace {
-
 const base::Feature kMITMSoftwareInterstitial{"MITMSoftwareInterstitial",
                                               base::FEATURE_ENABLED_BY_DEFAULT};
 
@@ -69,9 +67,6 @@ const base::Feature kCaptivePortalInterstitial{
 const base::Feature kCaptivePortalCertificateList{
     "CaptivePortalCertificateList", base::FEATURE_ENABLED_BY_DEFAULT};
 
-const base::Feature kSSLCommonNameMismatchHandling{
-    "SSLCommonNameMismatchHandling", base::FEATURE_ENABLED_BY_DEFAULT};
-
 #if defined(OS_WIN)
 const base::Feature kSuperfishInterstitial{"SuperfishInterstitial",
                                            base::FEATURE_ENABLED_BY_DEFAULT};
@@ -79,6 +74,11 @@ const base::Feature kSuperfishInterstitial{"SuperfishInterstitial",
 const base::Feature kSuperfishInterstitial{"SuperfishInterstitial",
                                            base::FEATURE_DISABLED_BY_DEFAULT};
 #endif
+
+namespace {
+
+const base::Feature kSSLCommonNameMismatchHandling{
+    "SSLCommonNameMismatchHandling", base::FEATURE_ENABLED_BY_DEFAULT};
 
 // Default delay in milliseconds before displaying the SSL interstitial.
 // This can be changed in tests.
--- chrome/browser/ssl/ssl_error_handler.h
+++ chrome/browser/ssl/ssl_error_handler.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/callback_forward.h"
+#include "base/feature_list.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "base/timer/timer.h"
@@ -41,6 +42,11 @@ class WebContents;
 namespace network_time {
 class NetworkTimeTracker;
 }
+
+extern const base::Feature kMITMSoftwareInterstitial;
+extern const base::Feature kCaptivePortalInterstitial;
+extern const base::Feature kCaptivePortalCertificateList;
+extern const base::Feature kSuperfishInterstitial;
 
 // This class is responsible for deciding what type of interstitial to display
 // for an SSL validation error and actually displaying it. The display of the
