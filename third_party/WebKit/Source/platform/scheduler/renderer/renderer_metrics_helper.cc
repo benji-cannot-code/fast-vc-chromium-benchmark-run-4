7b4e5b033bd8db960703886845ee8e1b9b7b2e01
Alexander Timin <altimin@chromium.org>
2017-11-02 12:21:48 +0000

    [scheduler] Expand granularity of frame type
    Distinguish more types of frames for reporting. Only NONE is used at the moment.
    
    - Add NONE to use with global task runners to compare per-renderer work to per-frame.
    - Add DETACHED to track work coming from a detached frame (will be used in the future).
    - Separate frames that are exempt due to themselves and due to other frames requesting a page-wide opt-out (to be used in the future).
    
    R=skyostil@chromium.org,asvitkine@chromium.org
    CC=​​npm@chromium.org
    
    Change-Id: I90c89288da6e57b1b64ac6b30a1925ea09a88f8b
    Reviewed-on: https://chromium-review.googlesource.com/749461
    Reviewed-by: Shubhie Panicker (slow to respond until Nov 3) <panicker@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513467}

--- third_party/WebKit/Source/platform/scheduler/renderer/renderer_metrics_helper.cc
+++ third_party/WebKit/Source/platform/scheduler/renderer/renderer_metrics_helper.cc
@@ -19,7 +19,7 @@ namespace scheduler {
 #define MAIN_THREAD_LOAD_METRIC_NAME "RendererScheduler.RendererMainThreadLoad5"
 #define EXTENSIONS_MAIN_THREAD_LOAD_METRIC_NAME \
   MAIN_THREAD_LOAD_METRIC_NAME ".Extension"
-#define PER_FRAME_TYPE_METRIC_NAME "RendererScheduler.TaskDurationPerFrameType"
+#define PER_FRAME_TYPE_METRIC_NAME "RendererScheduler.TaskDurationPerFrameType2"
 
 namespace {
 
@@ -35,11 +35,14 @@ constexpr base::TimeDelta kLongIdlePeriodDiscardingThreshold =
 
 enum class FrameThrottlingState {
   VISIBLE = 0,
-  HIDDEN = 1,
-  BACKGROUND = 2,
-  BACKGROUND_EXEMPT = 3,
+  VISIBLE_SERVICE = 1,
+  HIDDEN = 2,
+  HIDDEN_SERVICE = 3,
+  BACKGROUND = 4,
+  BACKGROUND_EXEMPT_SELF = 5,
+  BACKGROUND_EXEMPT_OTHER = 6,
 
-  COUNT = 4
+  COUNT = 7
 };
 
 enum class FrameOriginState {
@@ -59,7 +62,7 @@ FrameThrottlingState GetFrameThrottlingState(
   }
 
   if (frame_scheduler.IsExemptFromThrottling())
-    return FrameThrottlingState::BACKGROUND_EXEMPT;
+    return FrameThrottlingState::BACKGROUND_EXEMPT_SELF;
 
   return FrameThrottlingState::BACKGROUND;
 }
@@ -76,11 +79,14 @@ FrameOriginState GetFrameOriginState(const WebFrameScheduler& frame_scheduler) {
 
 }  // namespace
 
-FrameType GetFrameType(const WebFrameScheduler& frame_scheduler) {
+FrameType GetFrameType(WebFrameScheduler* frame_scheduler) {
+  if (!frame_scheduler)
+    return FrameType::NONE;
   FrameThrottlingState throttling_state =
-      GetFrameThrottlingState(frame_scheduler);
-  FrameOriginState origin_state = GetFrameOriginState(frame_scheduler);
+      GetFrameThrottlingState(*frame_scheduler);
+  FrameOriginState origin_state = GetFrameOriginState(*frame_scheduler);
   return static_cast<FrameType>(
+      static_cast<int>(FrameType::SPECIAL_CASES_COUNT) +
       static_cast<int>(origin_state) *
           static_cast<int>(FrameThrottlingState::COUNT) +
       static_cast<int>(throttling_state));
@@ -330,10 +336,8 @@ void RendererMetricsHelper::RecordTaskMetrics(MainThreadTaskQueue* queue,
     visible_task_duration_reporter.RecordTask(queue_type, duration);
   }
 
-  if (queue->GetFrameScheduler()) {
-    frame_type_duration_reporter.RecordTask(
-        GetFrameType(*queue->GetFrameScheduler()), duration);
-  }
+  frame_type_duration_reporter.RecordTask(
+      GetFrameType(queue->GetFrameScheduler()), duration);
 }
 
 void RendererMetricsHelper::RecordMainThreadTaskLoad(base::TimeTicks time,
