8b43a280f3ad5a2796cdb9f811848c631bd982a4
mrefaat <mrefaat@chromium.org>
2017-11-02 15:18:39 +0000

    Modify CallbackCounter so it can add more than one callback at once.
    Without that if the callback counter is incremented by one and then the
    callback executed before adding the rest of callbacks, The final
    callback will be called more than once.
    
    Bug: 
    Change-Id: I7f64ce37e92d2f9a263ed9779f4bf43d51fade97
    Reviewed-on: https://chromium-review.googlesource.com/749053
    Commit-Queue: Mohammad Refaat <mrefaat@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513493}

--- base/ios/callback_counter_unittest.mm
+++ base/ios/callback_counter_unittest.mm
@@ -13,8 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using CallbackCounterTest = PlatformTest;
 
-// Tests that CallbackCounter works with 2 callbacks.
-TEST_F(CallbackCounterTest, Basic) {
+// Tests that CallbackCounter works with adding callbacks one by one.
+TEST_F(CallbackCounterTest, BasicIncrementByOne) {
   __block BOOL block_was_called = NO;
   scoped_refptr<CallbackCounter> callback_counter =
       new CallbackCounter(base::BindBlock(^{
@@ -33,6 +33,26 @@ TEST_F(CallbackCounterTest, Basic) {
     callback_counter->DecrementCount();
   });
 
+  base::test::ios::WaitUntilCondition(^bool() {
+    return block_was_called;
+  });
+}
+
+// Tests that CallbackCounter works with adding all callbacks at once.
+TEST_F(CallbackCounterTest, BasicIncrementByMoreThanOne) {
+  __block BOOL block_was_called = NO;
+  scoped_refptr<CallbackCounter> callback_counter =
+      new CallbackCounter(base::BindBlock(^{
+        block_was_called = YES;
+      }));
+
+  // Enqueue the 5 callbacks.
+  callback_counter->IncrementCount(5);
+  for (int i = 0; i < 5; i++) {
+    dispatch_async(dispatch_get_main_queue(), ^{
+      callback_counter->DecrementCount();
+    });
+  }
   base::test::ios::WaitUntilCondition(^bool() {
     return block_was_called;
   });
