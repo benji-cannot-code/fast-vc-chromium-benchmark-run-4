25b385ca60137584f0fdf47bfa517d93924938a3
Tarun Bansal <tbansal@chromium.org>
2017-11-02 20:32:50 +0000

    Change min Chrome version for proxy connection tests to M63 since the Chromium fix was merged back to M63.
    Bug: 770789
    Change-Id: I529407a78d455e7b28f409f27421ddfe6aff54fa
    TBR: ryansturm@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/751925
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513594}

--- tools/chrome_proxy/webdriver/proxy_connection.py
+++ tools/chrome_proxy/webdriver/proxy_connection.py
@@ -11,7 +11,7 @@ from emulation_server import InvalidTLSHandler
 
 class ProxyConnection(IntegrationTest):
 
-  @ChromeVersionEqualOrAfterM(64)
+  @ChromeVersionEqualOrAfterM(63)
   def testTLSInjectionAfterHandshake(self):
     port = common.GetOpenPort()
     with TestDriver() as t:
