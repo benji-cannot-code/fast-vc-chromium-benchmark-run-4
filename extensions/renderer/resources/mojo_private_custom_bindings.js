e89ad486cb74bbba33c38cec369e6b9118306fde
Yuzhu Shen <yzshen@chromium.org>
2017-11-02 17:20:00 +0000

    Remove unused method define() in the private mojoPrivate extensions API.
    Bug: 699569,718047
    Change-Id: I2ebda3e66b6cb7f74cb6bc3f577f978022374a14
    Reviewed-on: https://chromium-review.googlesource.com/750191
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Yuzhu Shen <yzshen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513529}

--- extensions/renderer/resources/mojo_private_custom_bindings.js
+++ extensions/renderer/resources/mojo_private_custom_bindings.js
@@ -12,10 +12,6 @@ let binding = apiBridge || require('binding').Binding.create('mojoPrivate');
 binding.registerCustomHook(function(bindingsAPI) {
   let apiFunctions = bindingsAPI.apiFunctions;
 
-  apiFunctions.setHandleRequest('define', function(name, deps, factory) {
-    define(name, deps || [], factory);
-  });
-
   apiFunctions.setHandleRequest('requireAsync', function(moduleName) {
     return requireAsync(moduleName);
   });
