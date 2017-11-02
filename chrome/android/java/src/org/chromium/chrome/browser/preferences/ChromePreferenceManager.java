f74c0ac2e8e7092c67ea07a388a12a98139306d1
Peter Wen <wnwen@chromium.org>
2017-11-02 16:32:30 +0000

    Reland "Android: Add third_party/auto for errorprone""
    Original CL: https://crrev.com/c/739725
    
    Fix:
    - errorprone_plugin_java depends on errorprone_java, which is only
      available on $default_toolchain.
    
    BUG=777572
    TBR=fgorski@chromium.org,brettw@chromium.org,yfriedman@chromium.org,jbudorick@chromium.org,peter@chromium.org,torne@chromium.org,agrieve@chromium.org
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I2dceece332036a98014943cff4ec82b152075622
    Reviewed-on: https://chromium-review.googlesource.com/750645
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513513}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/ChromePreferenceManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/ChromePreferenceManager.java
@@ -9,7 +9,6 @@ import android.content.SharedPreferences;
 import android.os.StrictMode;
 
 import org.chromium.base.ContextUtils;
-import org.chromium.base.annotations.SuppressFBWarnings;
 import org.chromium.chrome.browser.ChromeSwitches;
 import org.chromium.chrome.browser.ChromeVersionInfo;
 import org.chromium.chrome.browser.crash.MinidumpUploadService.ProcessType;
@@ -71,7 +70,7 @@ public class ChromePreferenceManager {
 
     public static final String CHROME_HOME_SHARED_PREFERENCES_KEY = "chrome_home_enabled_date";
 
-    private static ChromePreferenceManager sPrefs;
+    private static ChromePreferenceManager sPrefs = new ChromePreferenceManager();
 
     private final SharedPreferences mSharedPreferences;
 
@@ -83,11 +82,7 @@ public class ChromePreferenceManager {
      * Get the static instance of ChromePreferenceManager if exists else create it.
      * @return the ChromePreferenceManager singleton
      */
-    @SuppressFBWarnings("CHROMIUM_SYNCHRONIZED_METHOD")
-    public static synchronized ChromePreferenceManager getInstance() {
-        if (sPrefs == null) {
-            sPrefs = new ChromePreferenceManager();
-        }
+    public static ChromePreferenceManager getInstance() {
         return sPrefs;
     }
 
