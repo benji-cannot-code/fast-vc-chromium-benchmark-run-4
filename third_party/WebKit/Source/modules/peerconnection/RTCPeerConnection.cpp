8a8c104d5243624baf805f7cf52458f32c3a024e
Hajime Hoshi <hajimehoshi@chromium.org>
2017-11-02 08:57:32 +0000

    Rename AsyncMethodRunner::Suspend/Resume -> Pause/Unpause
    This CL is part of the giant CL https://chromium-review.googlesource.com/c/chromium/src/+/748663
    
    Bug: 780378
    Change-Id: I622ca8aad24913ad5bf78f4f440b099620e89304
    Reviewed-on: https://chromium-review.googlesource.com/750423
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513429}

--- third_party/WebKit/Source/modules/peerconnection/RTCPeerConnection.cpp
+++ third_party/WebKit/Source/modules/peerconnection/RTCPeerConnection.cpp
@@ -1618,11 +1618,11 @@ ExecutionContext* RTCPeerConnection::GetExecutionContext() const {
 }
 
 void RTCPeerConnection::Suspend() {
-  dispatch_scheduled_event_runner_->Suspend();
+  dispatch_scheduled_event_runner_->Pause();
 }
 
 void RTCPeerConnection::Resume() {
-  dispatch_scheduled_event_runner_->Resume();
+  dispatch_scheduled_event_runner_->Unpause();
 }
 
 void RTCPeerConnection::ContextDestroyed(ExecutionContext*) {
