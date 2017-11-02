a794282e49f6440951812cb539da1b68d880f780
Antoine Labour <piman@chromium.org>
2017-11-02 02:07:19 +0000

    Fully support GL_ARB_map_buffer_range/GL_EXT_map_buffer_range bindings
    Skia expects the bindings for FlushMappedBufferRange to be present if
    either extension exists.
    
    Bug: 780320
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Iff7dbe4a40b01017c8b59b7e3e8b3e42610c1f5b
    Reviewed-on: https://chromium-review.googlesource.com/749992
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513370}

--- ui/gl/gl_bindings_autogen_mock.cc
+++ ui/gl/gl_bindings_autogen_mock.cc
@@ -1150,6 +1150,14 @@ MockGLInterface::Mock_glFlushMappedBufferRange(GLenum target,
 }
 
 void GL_BINDING_CALL
+MockGLInterface::Mock_glFlushMappedBufferRangeEXT(GLenum target,
+                                                  GLintptr offset,
+                                                  GLsizeiptr length) {
+  MakeFunctionUnique("glFlushMappedBufferRangeEXT");
+  interface_->FlushMappedBufferRange(target, offset, length);
+}
+
+void GL_BINDING_CALL
 MockGLInterface::Mock_glFramebufferRenderbuffer(GLenum target,
                                                 GLenum attachment,
                                                 GLenum renderbuffertarget,
@@ -4349,6 +4357,9 @@ MockGLInterface::GetGLProcAddress(const char* name) {
   if (strcmp(name, "glFlushMappedBufferRange") == 0)
     return reinterpret_cast<GLFunctionPointerType>(
         Mock_glFlushMappedBufferRange);
+  if (strcmp(name, "glFlushMappedBufferRangeEXT") == 0)
+    return reinterpret_cast<GLFunctionPointerType>(
+        Mock_glFlushMappedBufferRangeEXT);
   if (strcmp(name, "glFramebufferRenderbuffer") == 0)
     return reinterpret_cast<GLFunctionPointerType>(
         Mock_glFramebufferRenderbuffer);
