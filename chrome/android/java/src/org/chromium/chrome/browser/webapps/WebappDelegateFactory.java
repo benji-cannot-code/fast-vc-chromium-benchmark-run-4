d10d9a848f6a3167db45f800651a304a21f1d413
Yusuf Ozuysal <yusufo@google.com>
2017-11-02 20:54:05 +0000

    Add a way to launch TrustedWebActivities
    - Add controls within WebappActivity to support Sessions
    - Add a way to create WebAppInfo with a support library intent
    - Add plumbing for checking various requirement synchronously
    - Launch a WebAppActivity intent from LaunchDispatcher if all sync
    verifications are successful.
    
    Change-Id: I816ce64ec17be1ef34418230dff2f101ced7b72b
    Reviewed-on: https://chromium-review.googlesource.com/727372
    Commit-Queue: Yusuf Ozuysal <yusufo@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513600}

--- chrome/android/java/src/org/chromium/chrome/browser/webapps/WebappDelegateFactory.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/WebappDelegateFactory.java
@@ -44,7 +44,7 @@ public class WebappDelegateFactory extends TabDelegateFactory {
             // compatibility we relaunch it the hard way.
             String startUrl = mActivity.getWebappInfo().uri().toString();
 
-            String webApkPackageName = mActivity.getWebappInfo().webApkPackageName();
+            String webApkPackageName = mActivity.getWebappInfo().apkPackageName();
             if (!TextUtils.isEmpty(webApkPackageName)) {
                 Intent intent = WebApkNavigationClient.createLaunchWebApkIntent(
                         webApkPackageName, startUrl, false /* forceNavigation */);
@@ -92,5 +92,11 @@ public class WebappDelegateFactory extends TabDelegateFactory {
     @Override
     public InterceptNavigationDelegateImpl createInterceptNavigationDelegate(Tab tab) {
         return new WebappInterceptNavigationDelegate(mActivity, tab);
+    }
+
+    @Override
+    public boolean canShowAppBanners(Tab tab) {
+        // Do not show banners when we are in a standalone activity.
+        return false;
     }
 }
