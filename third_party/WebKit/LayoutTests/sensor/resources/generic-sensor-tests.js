8d13bb7f52c7dd468c1766604691c9bfb2e29708
Mikhail Pozdnyakov <mikhail.pozdnyakov@intel.com>
2017-11-02 10:54:43 +0000

    [OrientationSensor] Check that 'quaternion' attribute is updated
    Make sure that 'OrientationSensor.quaternion' is properly updated
    after repeating start()/stop() calls.
    
    Change-Id: I58892cf7880dcca143760ca935673ead0a4db9be
    Reviewed-on: https://chromium-review.googlesource.com/749385
    Reviewed-by: Alexander Shalamov <alexander.shalamov@intel.com>
    Commit-Queue: Mikhail Pozdnyakov <mikhail.pozdnyakov@intel.com>
    Cr-Commit-Position: refs/heads/master@{#513455}

--- third_party/WebKit/LayoutTests/sensor/resources/generic-sensor-tests.js
+++ third_party/WebKit/LayoutTests/sensor/resources/generic-sensor-tests.js
@@ -395,6 +395,7 @@ function runGenericSensorTests(sensorType, updateReading, verifyReading) {
     await new Promise((resolve, reject) => {
       let wrapper1 = new CallbackWrapper(() => {
         assert_true(sensorObject.hasReading);
+        assert_true(verifyReading(sensorObject));
         timestamp = sensorObject.timestamp;
         sensorObject.stop();
 
@@ -405,6 +406,7 @@ function runGenericSensorTests(sensorType, updateReading, verifyReading) {
 
       let wrapper2 = new CallbackWrapper(() => {
         assert_true(sensorObject.hasReading);
+        assert_true(verifyReading(sensorObject));
         // Make sure that 'timestamp' is already initialized.
         assert_greater_than(timestamp, 0);
         // Check that the reading is updated.
