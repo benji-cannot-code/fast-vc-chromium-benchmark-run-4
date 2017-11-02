27f57c99c547d5f7371871a02683a68c76510a67
Lucas Garron <lgarron@chromium.org>
2017-11-02 02:48:18 +0000

    Committed Interstitials: expand NavigationRequest::OnRequestFailed() path to allow optional error page html (2/2)
    Plumb failed request error page data URL from NavigationThrottle to RenderFrameImpl.
    As part of this, modify DidFailProvisionalLoad in WebFrameClient and all its subclasses
    to take an optional error_page_content string.
    
    Bug: 751949
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I43d7329a8dc6e479fb9c281f2efbe832f5341eb0
    Reviewed-on: https://chromium-review.googlesource.com/625337
    Commit-Queue: Lucas Garron <lgarron@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513385}

--- content/browser/frame_host/navigation_handle_impl_browsertest.cc
+++ content/browser/frame_host/navigation_handle_impl_browsertest.cc
@@ -6,9 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/memory/weak_ptr.h"
+#include "base/strings/stringprintf.h"
 #include "content/browser/frame_host/debug_urls.h"
 #include "content/browser/frame_host/navigation_handle_impl.h"
 #include "content/browser/web_contents/web_contents_impl.h"
+#include "content/public/browser/navigation_throttle.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/common/bindings_policy.h"
@@ -17,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_switches.h"
 #include "content/public/common/request_context_type.h"
 #include "content/public/common/url_constants.h"
-#include "content/public/test/browser_side_navigation_test_utils.h"
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/content_browser_test.h"
 #include "content/public/test/content_browser_test_utils.h"
@@ -32,6 +33,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/test/url_request/url_request_failed_job.h"
 #include "ui/base/page_transition_types.h"
 #include "url/url_constants.h"
+
+namespace {
+
+// Text to place in an HTML body. Should not contain any markup.
+const char kBodyTextContent[] = "some plain text content";
+
+}  // namespace
 
 namespace content {
 
@@ -2011,6 +2019,38 @@ IN_PROC_BROWSER_TEST_F(NavigationHandleImplDownloadBrowserTest,
   EXPECT_FALSE(observer.is_download());
   EXPECT_TRUE(observer.is_error());
   EXPECT_TRUE(observer.was_redirected());
+}
+
+IN_PROC_BROWSER_TEST_F(NavigationHandleImplBrowserTest,
+                       ThrottleFailureWithErrorPageContent) {
+  if (!IsBrowserSideNavigationEnabled())
+    return;
+
+  net::EmbeddedTestServer https_server(net::EmbeddedTestServer::TYPE_HTTPS);
+  https_server.SetSSLConfig(net::EmbeddedTestServer::CERT_MISMATCHED_NAME);
+  ASSERT_TRUE(https_server.Start());
+  GURL url(https_server.GetURL("/title1.html"));
+
+  NavigationThrottle::ThrottleCheckResult cancel_result = {
+      NavigationThrottle::CANCEL, net::ERR_CERT_COMMON_NAME_INVALID,
+      base::StringPrintf("<html><body>%s</body><html>", kBodyTextContent)};
+
+  NavigationHandleObserver observer(shell()->web_contents(), url);
+  TestNavigationThrottleInstaller installer(
+      shell()->web_contents(), NavigationThrottle::PROCEED,
+      NavigationThrottle::PROCEED, cancel_result, NavigationThrottle::PROCEED);
+
+  EXPECT_FALSE(NavigateToURL(shell(), url));
+
+  EXPECT_TRUE(observer.has_committed());
+  EXPECT_TRUE(observer.is_error());
+
+  std::string result;
+  const std::string javascript =
+      "domAutomationController.send(document.body.textContent)";
+  content::RenderFrameHost* rfh = shell()->web_contents()->GetMainFrame();
+  ASSERT_TRUE(content::ExecuteScriptAndExtractString(rfh, javascript, &result));
+  EXPECT_EQ(kBodyTextContent, result);
 }
 
 }  // namespace content
