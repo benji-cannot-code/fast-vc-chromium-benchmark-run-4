8670cce0ef32beabb6c7d988838bd20f2849bfca
Kevin McNee <mcnee@chromium.org>
2017-11-02 19:07:27 +0000

    Consolidate ZoomChangedWatchers and allow for custom predicates.
    There are duplicate versions of ZoomChangedWatcher used in tests, which
    we now combine.
    
    We also extend ZoomChangedWatcher to allow the caller to specify a
    predicate that ZoomChangedWatcher will use to determine when it has
    received the desired zoom change event, instead of just checking
    equality of events.
    
    Bug: None
    Change-Id: I2efead04ca575ccfa10e2143fc3048863863d2f8
    Reviewed-on: https://chromium-review.googlesource.com/742401
    Reviewed-by: James MacLean <wjmaclean@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513573}

--- chrome/browser/ui/zoom/zoom_controller_browsertest.cc
+++ chrome/browser/ui/zoom/zoom_controller_browsertest.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/prefs/pref_service.h"
 #include "components/signin/core/browser/profile_management_switches.h"
+#include "components/zoom/test/zoom_test_utils.h"
 #include "content/public/browser/host_zoom_map.h"
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/render_frame_host.h"
@@ -29,43 +30,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_test_utils.h"
 #include "testing/gmock/include/gmock/gmock.h"
 
+using zoom::ZoomChangedWatcher;
 using zoom::ZoomController;
 using zoom::ZoomObserver;
-
-bool operator==(const ZoomController::ZoomChangedEventData& lhs,
-                const ZoomController::ZoomChangedEventData& rhs) {
-  return lhs.web_contents == rhs.web_contents &&
-         lhs.old_zoom_level == rhs.old_zoom_level &&
-         lhs.new_zoom_level == rhs.new_zoom_level &&
-         lhs.zoom_mode == rhs.zoom_mode &&
-         lhs.can_show_bubble == rhs.can_show_bubble;
-}
-
-class ZoomChangedWatcher : public ZoomObserver {
- public:
-  ZoomChangedWatcher(
-      content::WebContents* web_contents,
-      const ZoomController::ZoomChangedEventData& expected_event_data)
-      : expected_event_data_(expected_event_data),
-        message_loop_runner_(new content::MessageLoopRunner) {
-    ZoomController::FromWebContents(web_contents)->AddObserver(this);
-  }
-  ~ZoomChangedWatcher() override {}
-
-  void Wait() { message_loop_runner_->Run(); }
-
-  void OnZoomChanged(
-      const ZoomController::ZoomChangedEventData& event_data) override {
-    if (event_data == expected_event_data_)
-      message_loop_runner_->Quit();
-  }
-
- private:
-  ZoomController::ZoomChangedEventData expected_event_data_;
-  scoped_refptr<content::MessageLoopRunner> message_loop_runner_;
-
-  DISALLOW_COPY_AND_ASSIGN(ZoomChangedWatcher);
-};
 
 class ZoomControllerBrowserTest : public InProcessBrowserTest {
  public:
--- components/zoom/test/zoom_test_utils.h
+++ components/zoom/test/zoom_test_utils.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_ZOOM_ZOOM_TEST_UTILS_H_
 #define COMPONENTS_ZOOM_ZOOM_TEST_UTILS_H_
 
+#include "base/callback_forward.h"
 #include "base/macros.h"
 #include "components/zoom/zoom_controller.h"
 #include "components/zoom/zoom_observer.h"
@@ -21,20 +22,37 @@ bool operator==(const ZoomController::ZoomChangedEventData& lhs,
 
 class ZoomChangedWatcher : public zoom::ZoomObserver {
  public:
+  using ZoomEventPred = base::RepeatingCallback<bool(
+      const ZoomController::ZoomChangedEventData&)>;
+
+  // Used to wait until we see a zoom changed event that satisfies the
+  // given |predicate|.
+  ZoomChangedWatcher(ZoomController* zoom_controller, ZoomEventPred predicate);
+  ZoomChangedWatcher(content::WebContents* web_contents,
+                     ZoomEventPred predicate);
+
+  // Used to wait until we see a zoom changed event equal to the given
+  // |expected_event_data|.
   ZoomChangedWatcher(
       ZoomController* zoom_controller,
       const ZoomController::ZoomChangedEventData& expected_event_data);
+  ZoomChangedWatcher(
+      content::WebContents* web_contents,
+      const ZoomController::ZoomChangedEventData& expected_event_data);
+
   ~ZoomChangedWatcher() override;
 
   void Wait();
 
+  // zoom::ZoomObserver:
   void OnZoomChanged(
       const ZoomController::ZoomChangedEventData& event_data) override;
 
  private:
   ZoomController* zoom_controller_;
-  ZoomController::ZoomChangedEventData expected_event_data_;
+  ZoomEventPred predicate_;
   scoped_refptr<content::MessageLoopRunner> message_loop_runner_;
+  bool change_received_ = false;
 
   DISALLOW_COPY_AND_ASSIGN(ZoomChangedWatcher);
 };
