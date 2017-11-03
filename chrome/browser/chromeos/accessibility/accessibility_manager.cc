956824cfc03b4ac8b43df04e384863ece39ae2d4
Łukasz Anforowicz <lukasza@chromium.org>
2017-11-03 18:35:18 +0000

    Revert "Extension URLs should only commit in an extension process."
    This reverts commit 3a7cc34f95c7a8bff01dede69940018cc1f4bdfa.
    
    Reason for revert: Caused https://crbug.com/780707
    
    Original change's description:
    > Extension URLs should only commit in an extension process.
    > 
    > Since --isolate-extensions has shipped in M56, we should be able to
    > enforce stricter CanCommitURL checks and disallow extension frames from
    > committing outside of an extension process.  This CL tightens the checks
    > done by ChromeContentBrowserClientExtensionsPart::CanCommitURL so
    > that extension URLs are only allowed to commit in an extension process.
    > 
    > Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    > Change-Id: I2c83659c5eba1a1a45c94876e1b254c52175f7b9
    > Bug: 770239
    > Reviewed-on: https://chromium-review.googlesource.com/683312
    > Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    > Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    > Reviewed-by: Nick Carter <nick@chromium.org>
    > Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    > Reviewed-by: Charlie Reis <creis@chromium.org>
    > Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#512959}
    
    TBR=dmazzoni@chromium.org,msw@chromium.org,creis@chromium.org,nick@chromium.org,rdevlin.cronin@chromium.org,alexmos@chromium.org,lukasza@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 770239
    Change-Id: I92393a58e9a3df77a321e4ef5d9c2cfbe2155640
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Reviewed-on: https://chromium-review.googlesource.com/752059
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513848}

--- chrome/browser/chromeos/accessibility/accessibility_manager.cc
+++ chrome/browser/chromeos/accessibility/accessibility_manager.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include <utility>
-#include <vector>
 
 #include "ash/accessibility/accessibility_controller.h"
 #include "ash/accessibility/accessibility_focus_ring_controller.h"
@@ -1118,11 +1117,6 @@ void AccessibilityManager::SetProfile(Profile* profile) {
 
   pref_change_registrar_.reset();
   local_state_pref_change_registrar_.reset();
-
-  // Cancel pending PostLoadChromeVox, PostSwitchChromeVoxProfile, and other
-  // in-flight callbacks - otherwise these callbacks would have operated on
-  // a different |profile_| than the one they were originally meant for.
-  weak_ptr_factory_.InvalidateWeakPtrs();
 
   if (profile) {
     // TODO(yoshiki): Move following code to PrefHandler.
