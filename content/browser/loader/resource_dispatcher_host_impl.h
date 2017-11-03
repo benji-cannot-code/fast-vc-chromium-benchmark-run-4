a01502143b66ca983920704b198711a598b13614
Alex Clarke <alexclarke@chromium.org>
2017-11-03 09:46:13 +0000

    Fix ResourceDispatcherHostImpl::delegate_ data race
    Bug: 776976, 766884
    Change-Id: Ia5aaf992a2a3f5d1b6e5c00cdb5ea10452b782f4
    Reviewed-on: https://chromium-review.googlesource.com/733094
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Commit-Queue: Alex Clarke <alexclarke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513730}

--- content/browser/loader/resource_dispatcher_host_impl.h
+++ content/browser/loader/resource_dispatcher_host_impl.h
@@ -99,7 +99,8 @@ class CONTENT_EXPORT ResourceDispatcherHostImpl
   // ResourceDispatcherHostImpl.
   ResourceDispatcherHostImpl(
       CreateDownloadHandlerIntercept download_handler_intercept,
-      const scoped_refptr<base::SingleThreadTaskRunner>& io_thread_runner);
+      const scoped_refptr<base::SingleThreadTaskRunner>& io_thread_runner,
+      bool enable_resource_scheduler);
   ResourceDispatcherHostImpl();
   ~ResourceDispatcherHostImpl() override;
 
@@ -754,6 +755,8 @@ class CONTENT_EXPORT ResourceDispatcherHostImpl
 
   // True if the resource dispatcher host has been shut down.
   bool is_shutdown_;
+
+  const bool enable_resource_scheduler_;
 
   using BlockedLoadersList = std::vector<std::unique_ptr<ResourceLoader>>;
   using BlockedLoadersMap =
