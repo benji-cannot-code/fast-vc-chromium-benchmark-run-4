ff6600a8d6b7f79c444fb0ef88c819bfff0f9a62
Zijie He <zijiehe@chromium.org>
2017-11-03 23:06:41 +0000

    [Chromoting] Implement EncoderBitrateFilter as a standalone component
    This change moves EncoderBitrateFilter into a standalone component rather than
    embedding in WebrtcFrameSchedulerSimple. The new implementation also provides a
    parameter to control the minimum-bitrate. So each encoder implementation can use
    a well-defined value to control the behavior of EncoderBitrateFilter.
    
    It solves two problems:
    1. Now, the minimum-bitrate is for VP8. The value is way to large for other
    encoders.
    2. Some encoder may not need "filter" at all, such as H264.
    
    Besides the structure change, this CL also uses DescendingSamples to replace the
    time-based average currently used in WebrtcFrameSchedulerSimple. It can help to
    smooth the bitrate we have received from WebRTC and avoid a sudden change. See
    the test cases for details.
    
    As noted in the comments of encoder_bitrate_filter.cc file, we are still using
    FPS to control the bandwidth usage, so smoothing the bitrate here is reasonable
    to avoid a sudden change to the image quality.
    
    Bug: chromium:779417, chromium:769391
    Change-Id: I2936c50e39a4ba72d61364b72a94587b6ad7a57c
    Reviewed-on: https://chromium-review.googlesource.com/750269
    Commit-Queue: Zijie He <zijiehe@chromium.org>
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513941}

---
+++ remoting/base/weighted_samples.h
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef REMOTING_BASE_WEIGHTED_SAMPLES_H_
+#define REMOTING_BASE_WEIGHTED_SAMPLES_H_
+
+#include <cstdint>
+
+namespace remoting {
+
+// Aggregates the samples and gives each of them a weight based on its age. This
+// class can help to smooth the input data.
+class WeightedSamples final {
+ public:
+  explicit WeightedSamples(double weight_factor);
+  ~WeightedSamples();
+
+  void Record(double value);
+  double WeightedAverage() const;
+
+ private:
+  const double weight_factor_;
+  double weighted_sum_ = 0;
+  double weight_ = 0;
+};
+
+}  // namespace remoting
+#endif  // REMOTING_BASE_WEIGHTED_SAMPLES_H_
