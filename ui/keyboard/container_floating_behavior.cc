19fc1425822696c65ff8c7ec47ee930e2f1eb895
Blake O'Hare <blakeo@chromium.org>
2017-11-03 23:06:33 +0000

    More specifically qualify some keyboard_locked() usages
    When keyboard_locked() is used for the purpose of determining whether
    the keyboard should go away when text focus is lost, use
    TextBlurHidesKeyboard() instead of keyboard_locked() directly.
    
    Bug: 
    Change-Id: Ia57cc4e073b0f5e87bf925d7fe38943025c4e2d4
    Reviewed-on: https://chromium-review.googlesource.com/747541
    Commit-Queue: Blake O'Hare <blakeo@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513940}

--- ui/keyboard/container_floating_behavior.cc
+++ ui/keyboard/container_floating_behavior.cc
@@ -235,4 +235,8 @@ void ContainerFloatingBehavior::SetCanonicalBounds(
   container->SetBounds(gfx::Rect(keyboard_location, keyboard_size));
 }
 
+bool ContainerFloatingBehavior::TextBlurHidesKeyboard() const {
+  return true;
+}
+
 }  //  namespace keyboard
