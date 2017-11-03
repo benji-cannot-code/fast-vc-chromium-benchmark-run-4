ed1c91c51198ebc5035ff5cd4fe7557d5cf0d81d
Rijubrata Bhaumik <rijubrata.bhaumik@intel.com>
2017-11-03 11:10:11 +0000

    [gpu] Don't suppress size_t to int warnings in gpu/
    BUG=167187
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ie7afdd824a307d02883c032064e29633bb6d057c
    Reviewed-on: https://chromium-review.googlesource.com/737832
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Rijubrata Bhaumik <rijubrata.bhaumik@intel.com>
    Cr-Commit-Position: refs/heads/master@{#513741}

--- gpu/command_buffer/client/mapped_memory.cc
+++ gpu/command_buffer/client/mapped_memory.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/atomic_sequence_num.h"
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
+#include "base/numerics/safe_conversions.h"
 #include "base/strings/stringprintf.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/trace_event/memory_dump_manager.h"
@@ -36,7 +37,9 @@ MemoryChunk::MemoryChunk(int32_t shm_id,
                          CommandBufferHelper* helper)
     : shm_id_(shm_id),
       shm_(shm),
-      allocator_(shm->size(), helper, shm->memory()) {}
+      allocator_(base::checked_cast<unsigned int>(shm->size()),
+                 helper,
+                 shm->memory()) {}
 
 MemoryChunk::~MemoryChunk() {}
 
--- gpu/command_buffer/client/ring_buffer.cc
+++ gpu/command_buffer/client/ring_buffer.cc
@@ -12,18 +12,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 
 #include "base/logging.h"
+#include "base/numerics/safe_conversions.h"
 #include "gpu/command_buffer/client/cmd_buffer_helper.h"
 
 namespace gpu {
 
 RingBuffer::RingBuffer(unsigned int alignment,
                        Offset base_offset,
-                       unsigned int size,
+                       size_t size,
                        CommandBufferHelper* helper,
                        void* base)
     : helper_(helper),
       base_offset_(base_offset),
-      size_(size),
+      size_(base::checked_cast<unsigned int>(size)),
       free_offset_(0),
       in_use_offset_(0),
       alignment_(alignment),
