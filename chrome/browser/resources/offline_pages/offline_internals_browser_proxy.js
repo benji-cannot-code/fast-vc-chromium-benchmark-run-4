099315a29eed63780a0c02ee6b049710b1c063dd
Cathy Li <chili@chromium.org>
2017-11-03 04:44:26 +0000

    [Offline Pages] Stringify javascript date numbers correctly so they would be human readable in the internals page dump.
    Bug: 775599
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I765da05d40b850dbb835b73a1f1300f7d717e053
    Reviewed-on: https://chromium-review.googlesource.com/750407
    Commit-Queue: Cathy Li <chili@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513687}

--- chrome/browser/resources/offline_pages/offline_internals_browser_proxy.js
+++ chrome/browser/resources/offline_pages/offline_internals_browser_proxy.js
@@ -26,7 +26,7 @@ var OfflinePage;
  *   creationTime: number,
  *   id: string,
  *   namespace: string,
- *   lastAttempt: number,
+ *   lastAttemptTime: number,
  *   requestOrigin: string
  * }}
  */
