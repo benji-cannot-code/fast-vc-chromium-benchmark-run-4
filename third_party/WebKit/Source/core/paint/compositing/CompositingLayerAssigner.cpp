a5c821c96b2e3fb040059209755b0cc85f5ba9d2
Stephen Chenney <schenney@chromium.org>
2017-11-03 19:25:48 +0000

    Use sibling HasInlineTransform to enable more squashing
    Traverse previous siblings of a layer looking for inline transforms
    as a way to enable more squashing when border radius is present.
    This targets the squashing restriction at the specific case that
    generated the original bug, and does not cause issues in other
    tests (that is, next siblings do not cause the original bug and
    only inline transforms cause the original bug).
    
    Telemetry tests on MacOS show a 3% or more improvement on time
    to first paint for load tests.
    
    TBR=chrishtr@chromium.org
    
    Bug: 776736,774544, 780244
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I1f9973e5c52d40a47c05dd2d8310bb66de3e0dfa
    Reviewed-on: https://chromium-review.googlesource.com/743694
    Commit-Queue: Stephen Chenney <schenney@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513869}

--- third_party/WebKit/Source/core/paint/compositing/CompositingLayerAssigner.cpp
+++ third_party/WebKit/Source/core/paint/compositing/CompositingLayerAssigner.cpp
@@ -123,6 +123,18 @@ CompositingLayerAssigner::ComputeCompositedLayerUpdate(PaintLayer* layer) {
   return update;
 }
 
+static bool SiblingSkippedOverlapTest(const PaintLayer* layer) {
+  PaintLayer* sibling = layer->PreviousSibling();
+  while (sibling) {
+    if (sibling->OverlapSkippedDueToInlineTransform(
+            sibling->GetCompositingReasons())) {
+      return true;
+    }
+    sibling = sibling->PreviousSibling();
+  }
+  return false;
+}
+
 SquashingDisallowedReasons
 CompositingLayerAssigner::GetReasonsPreventingSquashing(
     const PaintLayer* layer,
@@ -210,7 +222,8 @@ CompositingLayerAssigner::GetReasonsPreventingSquashing(
     return kSquashingDisallowedReasonFragmentedContent;
 
   if (layer->GetLayoutObject().Style()->HasBorderRadius() &&
-      layer->GetLayoutObject().HasOverflowClip())
+      layer->GetLayoutObject().HasOverflowClip() &&
+      SiblingSkippedOverlapTest(layer))
     return kSquashingDisallowedReasonBorderRadiusClipsDescendants;
 
   return kSquashingDisallowedReasonsNone;
