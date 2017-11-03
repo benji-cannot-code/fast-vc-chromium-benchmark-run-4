56b35d05ee74f2a74db2ac974cc3a74137f7b84b
Sylvain Defresne <sdefresne@chromium.org>
2017-11-03 13:43:19 +0000

    Allow WebStateObserver to observe N WebStates [7/N].
    Convert test WebStateObserver subclasses to directly manage
    the registration with WebState instead of dependending on
    the deprecated code from WebStateObserver.
    
    Bug: 775684
    Change-Id: I2c209d662794d2b1d7bbeb09e377f03f359e184f
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    Reviewed-on: https://chromium-review.googlesource.com/738675
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513761}

--- ios/web/public/test/fakes/test_web_state_observer.mm
+++ ios/web/public/test/fakes/test_web_state_observer.mm
@@ -19,15 +19,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace web {
 
 TestWebStateObserver::TestWebStateObserver(WebState* web_state)
-    : WebStateObserver(web_state) {}
-TestWebStateObserver::~TestWebStateObserver() = default;
+    : web_state_(web_state) {
+  web_state_->AddObserver(this);
+}
+
+TestWebStateObserver::~TestWebStateObserver() {
+  if (web_state_) {
+    web_state_->RemoveObserver(this);
+    web_state_ = nullptr;
+  }
+}
 
 void TestWebStateObserver::WasShown(WebState* web_state) {
+  ASSERT_EQ(web_state_, web_state);
   was_shown_info_ = base::MakeUnique<web::TestWasShownInfo>();
   was_shown_info_->web_state = web_state;
 }
 
 void TestWebStateObserver::WasHidden(WebState* web_state) {
+  ASSERT_EQ(web_state_, web_state);
   was_hidden_info_ = base::MakeUnique<web::TestWasHiddenInfo>();
   was_hidden_info_->web_state = web_state;
 }
@@ -35,6 +45,7 @@ void TestWebStateObserver::WasHidden(WebState* web_state) {
 void TestWebStateObserver::NavigationItemCommitted(
     WebState* web_state,
     const LoadCommittedDetails& load_details) {
+  ASSERT_EQ(web_state_, web_state);
   commit_navigation_info_ = base::MakeUnique<web::TestCommitNavigationInfo>();
   commit_navigation_info_->web_state = web_state;
   commit_navigation_info_->load_details = load_details;
@@ -43,6 +54,7 @@ void TestWebStateObserver::NavigationItemCommitted(
 void TestWebStateObserver::PageLoaded(
     WebState* web_state,
     PageLoadCompletionStatus load_completion_status) {
+  ASSERT_EQ(web_state_, web_state);
   load_page_info_ = base::MakeUnique<web::TestLoadPageInfo>();
   load_page_info_->web_state = web_state;
   load_page_info_->success =
@@ -50,6 +62,7 @@ void TestWebStateObserver::PageLoaded(
 }
 
 void TestWebStateObserver::InterstitialDismissed(WebState* web_state) {
+  ASSERT_EQ(web_state_, web_state);
   dismiss_interstitial_info_ =
       base::MakeUnique<web::TestDismissInterstitialInfo>();
   dismiss_interstitial_info_->web_state = web_state;
@@ -57,6 +70,7 @@ void TestWebStateObserver::InterstitialDismissed(WebState* web_state) {
 
 void TestWebStateObserver::LoadProgressChanged(WebState* web_state,
                                                double progress) {
+  ASSERT_EQ(web_state_, web_state);
   change_loading_progress_info_ =
       base::MakeUnique<web::TestChangeLoadingProgressInfo>();
   change_loading_progress_info_->web_state = web_state;
@@ -65,6 +79,7 @@ void TestWebStateObserver::LoadProgressChanged(WebState* web_state,
 
 void TestWebStateObserver::NavigationItemsPruned(WebState* web_state,
                                                  size_t pruned_item_count) {
+  ASSERT_EQ(web_state_, web_state);
   navigation_items_pruned_info_ =
       base::MakeUnique<web::TestNavigationItemsPrunedInfo>();
   navigation_items_pruned_info_->web_state = web_state;
@@ -72,6 +87,7 @@ void TestWebStateObserver::NavigationItemsPruned(WebState* web_state,
 }
 
 void TestWebStateObserver::NavigationItemChanged(WebState* web_state) {
+  ASSERT_EQ(web_state_, web_state);
   navigation_item_changed_info_ =
       base::MakeUnique<web::TestNavigationItemChangedInfo>();
   navigation_item_changed_info_->web_state = web_state;
@@ -79,6 +95,7 @@ void TestWebStateObserver::NavigationItemChanged(WebState* web_state) {
 
 void TestWebStateObserver::DidStartNavigation(WebState* web_state,
                                               NavigationContext* navigation) {
+  ASSERT_EQ(web_state_, web_state);
   ASSERT_TRUE(!navigation->GetError() || !navigation->IsSameDocument());
   did_start_navigation_info_ =
       base::MakeUnique<web::TestDidStartNavigationInfo>();
@@ -94,6 +111,7 @@ void TestWebStateObserver::DidStartNavigation(WebState* web_state,
 
 void TestWebStateObserver::DidFinishNavigation(WebState* web_state,
                                                NavigationContext* navigation) {
+  ASSERT_EQ(web_state_, web_state);
   ASSERT_TRUE(!navigation->GetError() || !navigation->IsSameDocument());
   did_finish_navigation_info_ =
       base::MakeUnique<web::TestDidFinishNavigationInfo>();
@@ -108,17 +126,20 @@ void TestWebStateObserver::DidFinishNavigation(WebState* web_state,
 }
 
 void TestWebStateObserver::TitleWasSet(WebState* web_state) {
+  ASSERT_EQ(web_state_, web_state);
   title_was_set_info_ = base::MakeUnique<web::TestTitleWasSetInfo>();
   title_was_set_info_->web_state = web_state;
 }
 
 void TestWebStateObserver::DidChangeVisibleSecurityState(WebState* web_state) {
+  ASSERT_EQ(web_state_, web_state);
   did_change_visible_security_state_info_ =
       base::MakeUnique<web::TestDidChangeVisibleSecurityStateInfo>();
   did_change_visible_security_state_info_->web_state = web_state;
 }
 
 void TestWebStateObserver::DidSuppressDialog(WebState* web_state) {
+  ASSERT_EQ(web_state_, web_state);
   did_suppress_dialog_info_ =
       base::MakeUnique<web::TestDidSuppressDialogInfo>();
   did_suppress_dialog_info_->web_state = web_state;
@@ -127,6 +148,7 @@ void TestWebStateObserver::DidSuppressDialog(WebState* web_state) {
 void TestWebStateObserver::DocumentSubmitted(WebState* web_state,
                                              const std::string& form_name,
                                              bool user_initiated) {
+  ASSERT_EQ(web_state_, web_state);
   submit_document_info_ = base::MakeUnique<web::TestSubmitDocumentInfo>();
   submit_document_info_->web_state = web_state;
   submit_document_info_->form_name = form_name;
@@ -139,6 +161,7 @@ void TestWebStateObserver::FormActivityRegistered(WebState* web_state,
                                                   const std::string& type,
                                                   const std::string& value,
                                                   bool input_missing) {
+  ASSERT_EQ(web_state_, web_state);
   form_activity_info_ = base::MakeUnique<web::TestFormActivityInfo>();
   form_activity_info_->web_state = web_state;
   form_activity_info_->form_name = form_name;
@@ -151,6 +174,7 @@ void TestWebStateObserver::FormActivityRegistered(WebState* web_state,
 void TestWebStateObserver::FaviconUrlUpdated(
     WebState* web_state,
     const std::vector<FaviconURL>& candidates) {
+  ASSERT_EQ(web_state_, web_state);
   update_favicon_url_candidates_info_ =
       base::MakeUnique<web::TestUpdateFaviconUrlCandidatesInfo>();
   update_favicon_url_candidates_info_->web_state = web_state;
@@ -158,25 +182,30 @@ void TestWebStateObserver::FaviconUrlUpdated(
 }
 
 void TestWebStateObserver::RenderProcessGone(WebState* web_state) {
+  ASSERT_EQ(web_state_, web_state);
   render_process_gone_info_ =
       base::MakeUnique<web::TestRenderProcessGoneInfo>();
   render_process_gone_info_->web_state = web_state;
 }
 
 void TestWebStateObserver::WebStateDestroyed(WebState* web_state) {
+  ASSERT_EQ(web_state_, web_state);
   EXPECT_TRUE(web_state->IsBeingDestroyed());
   web_state_destroyed_info_ =
       base::MakeUnique<web::TestWebStateDestroyedInfo>();
   web_state_destroyed_info_->web_state = web_state;
-  Observe(nullptr);
+  web_state_->RemoveObserver(this);
+  web_state_ = nullptr;
 }
 
 void TestWebStateObserver::DidStartLoading(WebState* web_state) {
+  ASSERT_EQ(web_state_, web_state);
   start_loading_info_ = base::MakeUnique<web::TestStartLoadingInfo>();
   start_loading_info_->web_state = web_state;
 }
 
 void TestWebStateObserver::DidStopLoading(WebState* web_state) {
+  ASSERT_EQ(web_state_, web_state);
   stop_loading_info_ = base::MakeUnique<web::TestStopLoadingInfo>();
   stop_loading_info_->web_state = web_state;
 }
