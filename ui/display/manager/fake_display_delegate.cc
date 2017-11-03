b267524dae19d4c2454d818fd0e205a2dac49ec5
kylechar <kylechar@chromium.org>
2017-11-03 20:13:06 +0000

    Change default fake display size.
    Change the default display size when using FakeDisplayDelegate. The size
    is now the same as the default display that classic ash creates in
    ManagedDisplayInfo.
    
    Bug: none
    Change-Id: I545c325e06903760ca1af8bbf81338c667e78afd
    Reviewed-on: https://chromium-review.googlesource.com/753925
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Commit-Queue: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513889}

--- ui/display/manager/fake_display_delegate.cc
+++ ui/display/manager/fake_display_delegate.cc
@@ -26,13 +26,14 @@ namespace display {
 namespace {
 
 // The EDID specification marks the top bit of the manufacturer id as reserved.
-const uint16_t kReservedManufacturerID = 1 << 15;
+constexpr uint16_t kReservedManufacturerID = 1 << 15;
 
 // A random product name hash.
 constexpr uint32_t kProductCodeHash = 3692486807;
 
-// Delay for Configure() in milliseconds.
-constexpr int64_t kConfigureDisplayDelayMs = 200;
+// Delay for Configure().
+constexpr base::TimeDelta kConfigureDisplayDelay =
+    base::TimeDelta::FromMilliseconds(200);
 
 }  // namespace
 
@@ -94,18 +95,14 @@ void FakeDisplayDelegate::Initialize() {
   DCHECK(!initialized_);
 
   // The default display will be an internal display with a native resolution
-  // of 1024x768 if --screen-config not specified on the command line.
-  std::string command_str = "1024x768/i";
+  // of 1366x768 if --screen-config not specified on the command line.
+  std::string command_str = "1366x768/i";
 
   base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
   if (command_line->HasSwitch(switches::kScreenConfig))
     command_str = command_line->GetSwitchValueASCII(switches::kScreenConfig);
 
-  if (!InitializeFromSpecString(command_str)) {
-    NOTREACHED() << "Bad --" << switches::kScreenConfig << " flag provided.";
-    return;
-  }
-
+  CreateDisplaysFromSpecString(command_str);
   initialized_ = true;
 }
 
@@ -151,9 +148,8 @@ void FakeDisplayDelegate::Configure(const DisplaySnapshot& output,
   // configuration requests then ConfigureDone() will handle starting the
   // next request.
   if (!configure_timer_.IsRunning()) {
-    configure_timer_.Start(
-        FROM_HERE, base::TimeDelta::FromMilliseconds(kConfigureDisplayDelayMs),
-        this, &FakeDisplayDelegate::ConfigureDone);
+    configure_timer_.Start(FROM_HERE, kConfigureDisplayDelay, this,
+                           &FakeDisplayDelegate::ConfigureDone);
   }
 }
 
@@ -188,10 +184,10 @@ FakeDisplayController* FakeDisplayDelegate::GetFakeDisplayController() {
   return this;
 }
 
-bool FakeDisplayDelegate::InitializeFromSpecString(const std::string& str) {
+void FakeDisplayDelegate::CreateDisplaysFromSpecString(const std::string& str) {
   // Start without any displays.
   if (str == "none")
-    return true;
+    return;
 
   // Split on commas and parse each display string.
   for (const std::string& part : base::SplitString(
@@ -204,12 +200,9 @@ bool FakeDisplayDelegate::InitializeFromSpecString(const std::string& str) {
       AddDisplay(std::move(snapshot));
       next_display_id_++;
     } else {
-      LOG(ERROR) << "Failed to parse display \"" << part << "\"";
-      return false;
+      LOG(FATAL) << "Bad --" << switches::kScreenConfig << " flag provided.";
     }
   }
-
-  return true;
 }
 
 void FakeDisplayDelegate::OnConfigurationChanged() {
@@ -227,9 +220,8 @@ void FakeDisplayDelegate::ConfigureDone() {
 
   // If there are more configuration requests waiting then restart the timer.
   if (!configure_callbacks_.empty()) {
-    configure_timer_.Start(
-        FROM_HERE, base::TimeDelta::FromMilliseconds(kConfigureDisplayDelayMs),
-        this, &FakeDisplayDelegate::ConfigureDone);
+    configure_timer_.Start(FROM_HERE, kConfigureDisplayDelay, this,
+                           &FakeDisplayDelegate::ConfigureDone);
   }
 }
 
