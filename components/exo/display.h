32791290937871eb720824666aa87f3f5595e381
David Reveman <reveman@chromium.org>
2017-11-03 01:49:21 +0000

    exo: Remove XDG shell v5
    Bug: 781086
    Test: exo_unittests
    Change-Id: I3345aacb6899d988215437bdfeeb66bb136e83cc
    Reviewed-on: https://chromium-review.googlesource.com/752364
    Commit-Queue: David Reveman <reveman@chromium.org>
    Commit-Queue: Daniele Castagna <dcastagna@chromium.org>
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513656}

--- components/exo/display.h
+++ components/exo/display.h
@@ -21,10 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/native_pixmap_handle.h"
 #endif
 
-namespace gfx {
-class Point;
-}
-
 namespace exo {
 class DataDevice;
 class DataDeviceDelegate;
@@ -70,13 +66,6 @@ class Display {
 
   // Creates a shell surface for an existing surface.
   std::unique_ptr<ShellSurface> CreateShellSurface(Surface* surface);
-
-  // Creates a popup shell surface for an existing surface at |position| and
-  // with |parent|. |position| is in |parent| surface local coordinates.
-  std::unique_ptr<ShellSurface> CreatePopupShellSurface(
-      Surface* surface,
-      ShellSurface* parent,
-      const gfx::Point& position);
 
   // Creates a remote shell surface for an existing surface using |container|.
   // The surface is scaled by 1 / |default_device_scale_factor|.
