a45fa96d0ae2e0e1aab21f87da7d9fb9dccce2a1
Brian White <bcwhite@chromium.org>
2017-11-03 18:45:43 +0000

    Handle bad files when creating tracker.
    Bug: 781043
    Change-Id: Ie10d85da6e7c69b54af14c330de64902320d76fa
    Reviewed-on: https://chromium-review.googlesource.com/753761
    Reviewed-by: Pierre-Antoine Manzagol (departed) <manzagop@chromium.org>
    Commit-Queue: Brian White <bcwhite@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513857}

--- base/debug/activity_tracker.cc
+++ base/debug/activity_tracker.cc
@@ -1271,7 +1271,7 @@ void GlobalActivityTracker::CreateWithAllocator(
 
 #if !defined(OS_NACL)
 // static
-void GlobalActivityTracker::CreateWithFile(const FilePath& file_path,
+bool GlobalActivityTracker::CreateWithFile(const FilePath& file_path,
                                            size_t size,
                                            uint64_t id,
                                            StringPiece name,
@@ -1287,15 +1287,19 @@ void GlobalActivityTracker::CreateWithFile(const FilePath& file_path,
                                    File::FLAG_WRITE | File::FLAG_SHARE_DELETE),
                               {0, static_cast<int64_t>(size)},
                               MemoryMappedFile::READ_WRITE_EXTEND);
-  DCHECK(success);
+  if (!success)
+    return false;
+  if (!FilePersistentMemoryAllocator::IsFileAcceptable(*mapped_file, false))
+    return false;
   CreateWithAllocator(std::make_unique<FilePersistentMemoryAllocator>(
                           std::move(mapped_file), size, id, name, false),
                       stack_depth, 0);
+  return true;
 }
 #endif  // !defined(OS_NACL)
 
 // static
-void GlobalActivityTracker::CreateWithLocalMemory(size_t size,
+bool GlobalActivityTracker::CreateWithLocalMemory(size_t size,
                                                   uint64_t id,
                                                   StringPiece name,
                                                   int stack_depth,
@@ -1303,6 +1307,7 @@ void GlobalActivityTracker::CreateWithLocalMemory(size_t size,
   CreateWithAllocator(
       std::make_unique<LocalPersistentMemoryAllocator>(size, id, name),
       stack_depth, process_id);
+  return true;
 }
 
 // static
