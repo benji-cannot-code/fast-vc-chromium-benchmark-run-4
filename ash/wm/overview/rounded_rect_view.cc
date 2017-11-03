4a7cf65937214ce92ab011d48ebb50624a5a2ed4
Sammie Quon <sammiequon@google.com>
2017-11-03 03:43:18 +0000

    splitview: Show app cannot snap in overview mode when an app is snapped.
    Copies android behaviour. Once an app is snapped, the remaining overview
    items which cannot be snapped show a message saying they cannot be
    snapped.
    
    Also show a toast when user tries to enter splitview while a cannot
    snap app is currently active. This can happen if users select a cannot
    snap app when there is a snapped app already, or by longpress on the
    overview button if the current app is not snappable.
    
    Pulls RoundedRectView out of window grid since it is the same as
    RoundedContainerView and a similar class is needed to display the cannot
    snap message.
    
    Test: manual
    Bug: 774238
    Change-Id: Ice993ae94ae5495292c179e0b5bf9ef97d3f8c1c
    Reviewed-on: https://chromium-review.googlesource.com/722028
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513668}

---
+++ ash/wm/overview/rounded_rect_view.cc
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/wm/overview/rounded_rect_view.h"
+
+#include "ui/gfx/canvas.h"
+#include "ui/gfx/skia_util.h"
+
+namespace ash {
+
+RoundedRectView::RoundedRectView(int corner_radius, SkColor background)
+    : corner_radius_(corner_radius), background_(background) {}
+
+RoundedRectView::~RoundedRectView() = default;
+
+void RoundedRectView::OnPaint(gfx::Canvas* canvas) {
+  views::View::OnPaint(canvas);
+
+  SkScalar radius = SkIntToScalar(corner_radius_);
+  const SkScalar kRadius[8] = {radius, radius, radius, radius,
+                               radius, radius, radius, radius};
+  SkPath path;
+  gfx::Rect bounds(size());
+  path.addRoundRect(gfx::RectToSkRect(bounds), kRadius);
+
+  canvas->ClipPath(path, true);
+  canvas->DrawColor(background_);
+}
+
+}  // namespace ash
--- ash/wm/overview/window_grid.cc
+++ ash/wm/overview/window_grid.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/screen_util.h"
 #include "ash/shelf/shelf.h"
 #include "ash/wm/overview/cleanup_animation_observer.h"
+#include "ash/wm/overview/rounded_rect_view.h"
 #include "ash/wm/overview/scoped_overview_animation_settings.h"
 #include "ash/wm/overview/window_selector.h"
 #include "ash/wm/overview/window_selector_delegate.h"
@@ -80,39 +81,6 @@ const float kOverviewInsetRatio = 0.05f;
 
 // Additional vertical inset reserved for windows in overview mode.
 const float kOverviewVerticalInset = 0.1f;
-
-// A View having rounded corners and a specified background color which is
-// only painted within the bounds defined by the rounded corners.
-// TODO(varkha): This duplicates code from RoundedImageView. Refactor these
-//               classes and move into ui/views.
-class RoundedRectView : public views::View {
- public:
-  RoundedRectView(int corner_radius, SkColor background)
-      : corner_radius_(corner_radius), background_(background) {}
-
-  ~RoundedRectView() override {}
-
-  void OnPaint(gfx::Canvas* canvas) override {
-    views::View::OnPaint(canvas);
-
-    SkScalar radius = SkIntToScalar(corner_radius_);
-    const SkScalar kRadius[8] = {radius, radius, radius, radius,
-                                 radius, radius, radius, radius};
-    SkPath path;
-    gfx::Rect bounds(size());
-    bounds.set_height(bounds.height() + radius);
-    path.addRoundRect(gfx::RectToSkRect(bounds), kRadius);
-
-    canvas->ClipPath(path, true);
-    canvas->DrawColor(background_);
-  }
-
- private:
-  int corner_radius_;
-  SkColor background_;
-
-  DISALLOW_COPY_AND_ASSIGN(RoundedRectView);
-};
 
 // BackgroundWith1PxBorder renders a solid background color, with a one pixel
 // border with rounded corners. This accounts for the scaling of the canvas, so
@@ -589,6 +557,11 @@ void WindowGrid::SetSelectionWidgetVisibility(bool visible) {
     selection_widget_->Show();
   else
     selection_widget_->Hide();
+}
+
+void WindowGrid::UpdateCannotSnapWarningVisibility() {
+  for (auto& window_selector_item : window_list_)
+    window_selector_item->UpdateCannotSnapWarningVisibility();
 }
 
 void WindowGrid::OnWindowDestroying(aura::Window* window) {
