007b9e30902d3b727cc0ca3ebf7c3df682763060
Charles Reis <creis@chromium.org>
2017-11-03 17:43:47 +0000

    Rewrite flaky CancelBeforeUnloadResetsURL test and update fix.
    We can invalidate the URL in the address bar without waiting for the
    beforeunload ACK from the renderer, making the test easier to write.
    We're keeping the invalidation in DidCancelLoading after the ACK as
    well, since it's not clear whether that's safe to remove.
    
    Also, the test can be implemented in content/ rather than chrome/.
    
    BUG=739773
    TEST=No behavior change, new test stays green.
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I66450965b5a8ac517211194590f47044d6ab50d3
    Reviewed-on: https://chromium-review.googlesource.com/750330
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Charlie Reis <creis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513826}

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -5697,6 +5697,9 @@ void WebContentsImpl::OnDialogClosed(int render_process_id,
       controller_.DiscardNonCommittedEntries();
     }
 
+    // Update the URL display either way, to avoid showing a stale URL.
+    NotifyNavigationStateChanged(INVALIDATE_TYPE_URL);
+
     for (auto& observer : observers_)
       observer.BeforeUnloadDialogCancelled();
   }
