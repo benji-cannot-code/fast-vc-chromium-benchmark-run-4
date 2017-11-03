458adac07793f9e55a332120a3c7a8d6dc75a7e7
Matthew Jones <mdjones@chromium.org>
2017-11-03 14:52:51 +0000

    Remove throttled-progress updates flag
    This change removes the flag for the throttled progress bar, turning
    it on by default for all users.
    
    BUG=759799
    
    Change-Id: Ia15d5db65f27bab222d4aac9dadea24bccca0e23
    Reviewed-on: https://chromium-review.googlesource.com/726844
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513770}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
@@ -1298,6 +1298,7 @@ public class ToolbarManager implements ToolbarTabController, UrlFocusChangeListe
      * Only start showing the progress bar if it is not already started.
      */
     private void startLoadProgress() {
+        android.util.Log.w("mdjones", "TOOLBARMANAGER starting");
         if (mToolbar.isProgressStarted()) return;
         mToolbar.startLoadProgress();
     }
--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -93,7 +93,6 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &kPayWithGoogleV1,
     &kPhysicalWebFeature,
     &kPhysicalWebSharing,
-    &kProgressBarThrottleFeature,
     &kPwaImprovedSplashScreen,
     &kPwaPersistentNotification,
     &kReaderModeInCCT,
@@ -270,9 +269,6 @@ const base::Feature kPhysicalWebFeature{"PhysicalWeb",
 
 const base::Feature kPhysicalWebSharing{"PhysicalWebSharing",
                                         base::FEATURE_DISABLED_BY_DEFAULT};
-
-const base::Feature kProgressBarThrottleFeature{
-    "ProgressBarThrottle", base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kPwaImprovedSplashScreen{"PwaImprovedSplashScreen",
                                              base::FEATURE_DISABLED_BY_DEFAULT};
