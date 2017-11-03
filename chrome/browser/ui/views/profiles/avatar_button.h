94be75353b5fb73a69993976e2f34547e87f7f09
Tom Anderson <thomasanderson@chromium.org>
2017-11-03 03:11:21 +0000

    Gtk: Ensure AvatarButton has pressed state when menu is showing
    When using the GTK theme with --enable-native-avatar-button, it's
    required that the avatar button have the pressed state when the
    profile chooser menu is showing so that it looks pressed.  Previously,
    the pressed state was handled by manually setting the ink drop
    animation state.  Now, AvatarButton is made a sublcass of MenuButton
    and reuses code from there to handle ink drop animation, and ensuring
    the state becomes pressed.
    
    Also, this incidentally fixes a bug where the ink drop would get stuck
    when right clicking on the avatar button.
    
    BUG=753067
    R=msw@chromium.org
    CC=sky@chromium.org
    
    Change-Id: I6aafb5499e230859ea1ebe1c5ca6e2375ea63994
    Reviewed-on: https://chromium-review.googlesource.com/744967
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513662}

--- chrome/browser/ui/views/profiles/avatar_button.h
+++ chrome/browser/ui/views/profiles/avatar_button.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/avatar_button_error_controller_delegate.h"
 #include "chrome/browser/ui/views/profiles/avatar_button_style.h"
 #include "components/keyed_service/core/keyed_service_shutdown_notifier.h"
-#include "ui/views/controls/button/label_button.h"
+#include "ui/views/controls/button/menu_button.h"
 #include "ui/views/widget/widget_observer.h"
 
 class AvatarButtonManager;
@@ -21,18 +21,22 @@ class Profile;
 
 // Base class for avatar buttons that display the active profile's name in the
 // caption area.
-class AvatarButton : public views::LabelButton,
+class AvatarButton : public views::MenuButton,
                      public AvatarButtonErrorControllerDelegate,
                      public ProfileAttributesStorage::Observer,
                      public views::WidgetObserver {
  public:
-  AvatarButton(views::ButtonListener* listener,
+  AvatarButton(views::MenuButtonListener* listener,
                AvatarButtonStyle button_style,
                Profile* profile,
                AvatarButtonManager* manager);
   ~AvatarButton() override;
 
   void SetupThemeColorButton();
+
+  // Called by AvatarButtonManager when the profile chooser menu is
+  // shown or hidden.
+  void OnAvatarButtonPressed(const ui::Event* event);
 
   // views::LabelButton:
   void AddedToWidget() override;
@@ -42,7 +46,6 @@ class AvatarButton : public views::LabelButton,
   std::unique_ptr<views::InkDropHighlight> CreateInkDropHighlight()
       const override;
   std::unique_ptr<views::InkDropMask> CreateInkDropMask() const override;
-  void NotifyClick(const ui::Event& event) override;
 
  protected:
   // views::LabelButton:
@@ -100,6 +103,9 @@ class AvatarButton : public views::LabelButton,
   // Set on desktop Linux to indicate if the avatar button should be
   // drawn using the system theme.
   bool render_native_nav_buttons_ = false;
+
+  // Shows the button in a pressed state while the bubble is open.
+  std::unique_ptr<PressedLock> pressed_lock_;
 
   ScopedObserver<views::Widget, views::WidgetObserver> widget_observer_;
 
