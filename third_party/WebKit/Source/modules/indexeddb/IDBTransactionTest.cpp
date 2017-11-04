4618f07828c6e8a4fbb7f9f716a2baabc4e8b994
Eric Willigers <ericwilligers@chromium.org>
2017-11-04 19:28:08 +0000

    indexeddb: Jumbo build fix for testing::_
    Use ::testing::_ instead of testing::_ to avoid clash with
    blink::testing in Jumbo builds.
    
    TBR=bratell@opera.com
    
    Change-Id: I7d58ae7281eaf6abdb4e2318677f80c02ebb0d90
    Reviewed-on: https://chromium-review.googlesource.com/754452
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514055}

--- third_party/WebKit/Source/modules/indexeddb/IDBTransactionTest.cpp
+++ third_party/WebKit/Source/modules/indexeddb/IDBTransactionTest.cpp
@@ -196,7 +196,7 @@ TEST_F(IDBTransactionTest, ContextDestroyedAfterDone) {
 TEST_F(IDBTransactionTest, ContextDestroyedWithQueuedResult) {
   V8TestingScope scope;
   std::unique_ptr<MockWebIDBDatabase> backend = MockWebIDBDatabase::Create();
-  EXPECT_CALL(*backend, AckReceivedBlobs(testing::_)).Times(1);
+  EXPECT_CALL(*backend, AckReceivedBlobs(::testing::_)).Times(1);
   EXPECT_CALL(*backend, Close()).Times(1);
   BuildTransaction(scope, std::move(backend));
 
@@ -233,7 +233,7 @@ TEST_F(IDBTransactionTest, ContextDestroyedWithQueuedResult) {
 TEST_F(IDBTransactionTest, ContextDestroyedWithTwoQueuedResults) {
   V8TestingScope scope;
   std::unique_ptr<MockWebIDBDatabase> backend = MockWebIDBDatabase::Create();
-  EXPECT_CALL(*backend, AckReceivedBlobs(testing::_)).Times(2);
+  EXPECT_CALL(*backend, AckReceivedBlobs(::testing::_)).Times(2);
   EXPECT_CALL(*backend, Close()).Times(1);
   BuildTransaction(scope, std::move(backend));
 
@@ -277,7 +277,7 @@ TEST_F(IDBTransactionTest, DocumentShutdownWithQueuedAndBlockedResults) {
 
   V8TestingScope scope;
   std::unique_ptr<MockWebIDBDatabase> backend = MockWebIDBDatabase::Create();
-  EXPECT_CALL(*backend, AckReceivedBlobs(testing::_)).Times(1);
+  EXPECT_CALL(*backend, AckReceivedBlobs(::testing::_)).Times(1);
   EXPECT_CALL(*backend, Close()).Times(1);
   BuildTransaction(scope, std::move(backend));
 
