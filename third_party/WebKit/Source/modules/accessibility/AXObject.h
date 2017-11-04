ae237f68c3dd1ba741786723160d90d0bc7010ba
Dominic Mazzoni <dmazzoni@chromium.org>
2017-11-04 00:31:33 +0000

    Remove some unused code in Blink accessibility
    Bug: none
    Change-Id: Ia1719fdb9c9c67de121792b7dd378098b77005da
    Reviewed-on: https://chromium-review.googlesource.com/747481
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513986}

--- third_party/WebKit/Source/modules/accessibility/AXObject.h
+++ third_party/WebKit/Source/modules/accessibility/AXObject.h
@@ -70,34 +70,6 @@ enum class AOMRelationListProperty;
 
 typedef unsigned AXID;
 
-enum AccessibilityTextSource {
-  kAlternativeText,
-  kChildrenText,
-  kSummaryText,
-  kHelpText,
-  kVisibleText,
-  kTitleTagText,
-  kPlaceholderText,
-  kLabelByElementText,
-};
-
-class AccessibilityText final
-    : public GarbageCollectedFinalized<AccessibilityText> {
-  WTF_MAKE_NONCOPYABLE(AccessibilityText);
-
- public:
-  void Trace(blink::Visitor* visitor) { visitor->Trace(text_element_); }
-
- private:
-  AccessibilityText(const String& text,
-                    const AccessibilityTextSource& source,
-                    AXObject* element)
-      : text_(text), text_element_(element) {}
-
-  String text_;
-  Member<AXObject> text_element_;
-};
-
 enum AXObjectInclusion {
   kIncludeObject,
   kIgnoreObject,
@@ -115,12 +87,6 @@ enum AccessibilityOptionalBool {
   kOptionalBoolUndefined = 0,
   kOptionalBoolTrue,
   kOptionalBoolFalse
-};
-
-enum TextUnderElementMode {
-  kTextUnderElementAll,
-  kTextUnderElementAny  // If the text is unimportant, just whether or not it's
-                        // present
 };
 
 class AXSparseAttributeClient {
