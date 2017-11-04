c3c010687bef8778062334e77f2169bd3a363719
Navid Zolghadr <nzolghadr@chromium.org>
2017-11-04 03:23:53 +0000

    Override click test API in the chromium repo
    We need to override click test API from wpt
    to go through our internal API to make it
    possible to run on the Chromium bots.
    
    Bug: 781231
    Change-Id: I325446bc8fb49fc3f42b61ad31d1fbf274450e6c
    Reviewed-on: https://chromium-review.googlesource.com/754063
    Commit-Queue: Navid Zolghadr <nzolghadr@chromium.org>
    Commit-Queue: Philip Jägenstedt <foolip@chromium.org>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514023}

---
+++ third_party/WebKit/LayoutTests/resources/testdriver-vendor.js
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(function() {
+  "use strict";
+  // Define functions one by one and do not override the whole
+  // test_driver_internal as it masks the new testing fucntions
+  // that will be added in the future.
+
+  window.test_driver_internal.click = function(element, coords) {
+    return new Promise(function(resolve, reject) {
+      if (window.chrome && chrome.gpuBenchmarking) {
+        chrome.gpuBenchmarking.pointerActionSequence(
+            [{
+              source: 'mouse',
+              actions: [
+              {name: 'pointerMove', x: coords.x, y: coords.y},
+              {name: 'pointerDown', x: coords.x, y: coords.y, button: 'left'},
+              {name: 'pointerUp', button: 'left'}
+              ]
+            }],
+            resolve);
+      } else {
+        reject(new Error("GPU benchmarnking is not enabled."));
+      }
+    });
+  };
+
+})();
