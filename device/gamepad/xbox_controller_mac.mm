69dde5f9104e86e1ba501d7c4d3cbdd6caa3cf8e
Matt Reynolds <mattreynolds@google.com>
2017-11-04 01:08:16 +0000

    Remove Objective-C calls from XboxDataFetcher
    After moving XboxControllerMac out of xbox_data_fetcher_mac.mm, there are
    only two Obj-C calls remaining, which are used for string conversions.
    This CL replaces the string conversions with base/strings methods
    and renames the file xbox_data_fetcher_mac.cc.
    
    BUG=749295
    
    Change-Id: I6456cc809f6d7ce365f397b2d1f9f22e57511c83
    Reviewed-on: https://chromium-review.googlesource.com/753533
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514001}

--- device/gamepad/xbox_controller_mac.mm
+++ device/gamepad/xbox_controller_mac.mm
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/foundation_util.h"
 #include "base/mac/scoped_ioobject.h"
 #include "base/sequenced_task_runner.h"
+#include "base/strings/stringprintf.h"
 #include "base/threading/thread_task_runner_handle.h"
 
 namespace device {
@@ -525,6 +526,26 @@ int XboxControllerMac::GetProductId() const {
 
 XboxControllerMac::ControllerType XboxControllerMac::GetControllerType() const {
   return controller_type_;
+}
+
+std::string XboxControllerMac::GetControllerTypeString() const {
+  switch (controller_type_) {
+    case XBOX_360_CONTROLLER:
+      return "Xbox 360 Controller";
+    case XBOX_ONE_CONTROLLER_2013:
+    case XBOX_ONE_CONTROLLER_2015:
+    case XBOX_ONE_ELITE_CONTROLLER:
+    case XBOX_ONE_S_CONTROLLER:
+      return "Xbox One Controller";
+    default:
+      return "Unrecognized Controller";
+  }
+}
+
+std::string XboxControllerMac::GetIdString() const {
+  return base::StringPrintf("%s (STANDARD GAMEPAD Vendor: %04x Product: %04x)",
+                            GetControllerTypeString().c_str(), GetVendorId(),
+                            GetProductId());
 }
 
 // static
