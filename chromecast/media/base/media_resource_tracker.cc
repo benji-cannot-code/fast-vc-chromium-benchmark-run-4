17db41c6a6eb05d1827ea18c29cb0f50accb45df
Sergey Ulanov <sergeyu@chromium.org>
2017-11-05 08:14:11 +0000

    Update MediaResourceTracker to initialize VolumeControl
    Currently public version of cast_shell doesn't initialize VolumeControl,
    as resilt volume is set to 0 in StreamMixer. This change updates
    MediaResourceTracker to initialize VolumeControl, which ensures that
    volume is set to a reasonable value in the mixer.
    
    Bug: 772488
    Merge-With: eureka-internal/115825
    Change-Id: Ia9b8af14998466c9603925cd0ede9978e2df8296
    Reviewed-on: https://chromium-review.googlesource.com/741029
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514064}

--- chromecast/media/base/media_resource_tracker.cc
+++ chromecast/media/base/media_resource_tracker.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "chromecast/base/bind_to_task_runner.h"
 #include "chromecast/public/cast_media_shlib.h"
+#include "chromecast/public/volume_control.h"
 
 namespace chromecast {
 namespace media {
@@ -149,10 +150,16 @@ void MediaResourceTracker::CallFinalizeOnMediaThread() {
 void MediaResourceTracker::DoInitializeMediaLib() {
   base::CommandLine* cmd_line = base::CommandLine::ForCurrentProcess();
   media::CastMediaShlib::Initialize(cmd_line->argv());
+  if (VolumeControl::Initialize) {
+    VolumeControl::Initialize(cmd_line->argv());
+  }
 }
 
 void MediaResourceTracker::DoFinalizeMediaLib() {
   CastMediaShlib::Finalize();
+  if (VolumeControl::Finalize) {
+    VolumeControl::Finalize();
+  }
 }
 
 }  // namespace media
