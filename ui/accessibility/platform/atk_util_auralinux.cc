6a1f02ba2efb094a5d26cc9eebaae91dd86622e9
Jose Dapena Paz <jose.dapena@lge.com>
2017-11-06 18:33:49 +0000

    Initialize ATK bridge without atk-bridge GTK module on GTK3.
    On GTK3, the atk-bridge.so GTK module is not expected to be available
    as the policy is that accessibility support is always loaded. So
    GTK initialization was already registering its own AtkMisc instance,
    and the GTK ATK root node. This was the node associated to ATK bridge.
    
    But Chromium needs to register its own ATK root node. So, for the
    case of GTK3, ATK bridge should happen immediately after the
    registration of Chromium AtkMisc instance, that will refer to
    the proper ATK root node.
    
    Bug: 780973
    Change-Id: Iaed33530eb6a801ab2bec0a299d8c9b7fa091f5f
    Reviewed-on: https://chromium-review.googlesource.com/753442
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Elliot Glaysher <erg@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Commit-Queue: José Dapena Paz <jose.dapena@lge.com>
    Cr-Commit-Position: refs/heads/master@{#514191}

--- ui/accessibility/platform/atk_util_auralinux.cc
+++ ui/accessibility/platform/atk_util_auralinux.cc
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include <atk/atk.h>
-#include <glib-2.0/gmodule.h>
 
 #include "base/bind.h"
 #include "base/environment.h"
@@ -19,11 +18,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/accessibility/platform/atk_util_auralinux.h"
 #include "ui/accessibility/platform/ax_platform_node_auralinux.h"
 
+#if defined(USE_ATK_BRIDGE)
+#include <atk-bridge.h>
+#else
+#include <glib-2.0/gmodule.h>
+#endif
+
 namespace {
 
 typedef void (*GnomeAccessibilityModuleInitFunc)();
 
 const char kAccessibilityEnabled[] = "ACCESSIBILITY_ENABLED";
+
+#if !defined(USE_ATK_BRIDGE)
 const char kAtkBridgeModule[] = "atk-bridge";
 const char kAtkBridgePath[] = "gtk-2.0/modules/libatk-bridge.so";
 const char kAtkBridgeSymbolName[] = "gnome_accessibility_module_init";
@@ -64,8 +71,12 @@ void FinishAccessibilityInitOnMainThread(
 
   init_func();
 }
+#endif  // !defined(USE_ATK_BRIDGE)
 
 bool PlatformShouldEnableAccessibility() {
+#if defined(USE_ATK_BRIDGE)
+  return false;
+#else
   std::unique_ptr<base::Environment> env(base::Environment::Create());
   std::string gtk_modules;
   if (!env->GetVar(kGtkModules, &gtk_modules))
@@ -78,6 +89,7 @@ bool PlatformShouldEnableAccessibility() {
       return true;
   }
   return false;
+#endif
 }
 
 bool ShouldEnableAccessibility() {
@@ -182,11 +194,19 @@ void AtkUtilAuraLinux::InitializeAsync() {
   if (!ShouldEnableAccessibility())
     return;
 
+#if defined(USE_ATK_BRIDGE)
+  // AT bridge enabling was disabled before loading GTK to avoid
+  // getting GTK implementation ATK root.
+  std::unique_ptr<base::Environment> env(base::Environment::Create());
+  env->SetVar("NO_AT_BRIDGE", "0");
+  atk_bridge_adaptor_init(nullptr, nullptr);
+#else
   base::PostTaskWithTraitsAndReplyWithResult(
       FROM_HERE,
       {base::MayBlock(), base::TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN},
       base::Bind(&GetAccessibilityModuleInitFunc),
       base::Bind(&FinishAccessibilityInitOnMainThread));
+#endif
 }
 
 }  // namespace ui
