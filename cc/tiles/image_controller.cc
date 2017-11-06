6248a6f8408349719875dc46021a831f3543b02e
Wez <wez@chromium.org>
2017-11-06 20:03:41 +0000

    Use WeakPtr member in ImageController, rather than calling GetWeakPtr.
    ImageController was previously calling GetWeakPtr() when Bind()ing,
    which is potentially racey with InvalidateWeakPtrs() on the thread on
    which the WeakPtrs are actually used.
    
    Bug: 729716
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I38ac1d5621703665d6b1c9deed915598fcd48ea9
    Reviewed-on: https://chromium-review.googlesource.com/751764
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514217}

--- cc/tiles/image_controller.cc
+++ cc/tiles/image_controller.cc
@@ -23,7 +23,9 @@ ImageController::ImageController(
     scoped_refptr<base::SequencedTaskRunner> worker_task_runner)
     : worker_task_runner_(std::move(worker_task_runner)),
       origin_task_runner_(origin_task_runner),
-      weak_ptr_factory_(this) {}
+      weak_ptr_factory_(this) {
+  weak_ptr_ = weak_ptr_factory_.GetWeakPtr();
+}
 
 ImageController::~ImageController() {
   StopWorkerTasks();
@@ -64,6 +66,7 @@ void ImageController::StopWorkerTasks() {
   // nothing can start running between wait and this invalidate, since it would
   // only run on the current (compositor) thread.
   weak_ptr_factory_.InvalidateWeakPtrs();
+  weak_ptr_ = weak_ptr_factory_.GetWeakPtr();
 
   // Now, begin cleanup.
 
@@ -270,7 +273,7 @@ void ImageController::ProcessNextImageDecodeOnWorkerThread() {
   }
   origin_task_runner_->PostTask(
       FROM_HERE, base::BindOnce(&ImageController::ImageDecodeCompleted,
-                                weak_ptr_factory_.GetWeakPtr(), decode.id));
+                                weak_ptr_, decode.id));
 }
 
 void ImageController::ImageDecodeCompleted(ImageDecodeRequestId id) {
