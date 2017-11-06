6176581846c230bf24cf6eaf7fa802c93a6877b2
Matt Falkenhagen <falken@chromium.org>
2017-11-06 23:12:51 +0000

    service worker: Remove unused resources/ files in http/tests/serviceworker
    These were probably resources for tests that have been upstreamed to WPT
    and removed.
    
    Bug: 688116
    Change-Id: Id9c08e2297b6c00c303f41734f9edeb9211c3749
    Reviewed-on: https://chromium-review.googlesource.com/754243
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514279}

--- third_party/WebKit/LayoutTests/http/tests/serviceworker/resources/invalid-chunked-encoding-with-flush.php
+++
@@ -1,8 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-<?php 
-header('Content-Type:application/javascript');
-header("Transfer-encoding: chunked");
-flush();
-sleep(1);
-echo "XX\r\n\r\n";
-?>
--- third_party/WebKit/LayoutTests/http/tests/serviceworker/resources/simple-intercept-worker.js
+++
@@ -1,6 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-self.onfetch = function(event) {
-  if (event.request.url.indexOf('simple') != -1)
-    event.respondWith(
-      new Response(new Blob(['intercepted by service worker'])));
-};
