38b31f35afb2cdda0164a6e285e4aaa3aec8aec1
meade_UTC10 <meade@chromium.org>
2017-11-06 02:04:03 +0000

    Revert "Added InfoBar event to chrome://interventions-internals."
    This reverts commit 7e92ed7022d5c96a608f686c72e57a9c6577a048.
    
    Reason for revert: Breaks PreviewsInfoBarDelegateUnitTest on win7 builder. First breakage on https://build.chromium.org/p/chromium.win/builders/Win7%20Tests%20%28dbg%29%281%29/builds/64447
    
    Original change's description:
    > Added InfoBar event to chrome://interventions-internals.
    > 
    > A new event log is added to the page, showing which type of previews
    > info bar is displayed on the page. This is an indication of which type
    > of preview is served on the page.
    > 
    > Bug: 780615
    > Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    > Change-Id: Ib1399f9a1979e6124a6f099f351dd63f9db740ac
    > Reviewed-on: https://chromium-review.googlesource.com/749671
    > Commit-Queue: Thanh Le <thanhdle@chromium.org>
    > Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    > Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#513800}
    
    TBR=tbansal@chromium.org,ryansturm@chromium.org,thanhdle@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 780615
    Change-Id: I2703cae91df6e86290e1a6d25ac3212e7e0c8777
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Reviewed-on: https://chromium-review.googlesource.com/754641
    Reviewed-by: meade_UTC10 <meade@chromium.org>
    Commit-Queue: meade_UTC10 <meade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514078}

--- chrome/browser/previews/previews_infobar_tab_helper.cc
+++ chrome/browser/previews/previews_infobar_tab_helper.cc
@@ -74,12 +74,6 @@ void PreviewsInfoBarTabHelper::DidFinishNavigation(
   displayed_preview_infobar_ = false;
   displayed_preview_timestamp_ = false;
 
-  // Retrieve PreviewsUIService* from |web_contents| if available.
-  PreviewsService* previews_service = PreviewsServiceFactory::GetForProfile(
-      Profile::FromBrowserContext(web_contents()->GetBrowserContext()));
-  previews::PreviewsUIService* previews_ui_service =
-      previews_service ? previews_service->previews_ui_service() : nullptr;
-
 #if BUILDFLAG(ENABLE_OFFLINE_PAGES)
   offline_pages::OfflinePageTabHelper* tab_helper =
       offline_pages::OfflinePageTabHelper::FromWebContents(web_contents());
@@ -93,7 +87,6 @@ void PreviewsInfoBarTabHelper::DidFinishNavigation(
         data_reduction_proxy_settings =
             DataReductionProxyChromeSettingsFactory::GetForBrowserContext(
                 web_contents()->GetBrowserContext());
-
     PreviewsInfoBarDelegate::Create(
         web_contents(), previews::PreviewsType::OFFLINE,
         base::Time() /* previews_freshness */, false /* is_reload */,
@@ -102,8 +95,7 @@ void PreviewsInfoBarTabHelper::DidFinishNavigation(
         base::Bind(&AddPreviewNavigationCallback,
                    web_contents()->GetBrowserContext(),
                    navigation_handle->GetRedirectChain()[0],
-                   previews::PreviewsType::OFFLINE),
-        previews_ui_service);
+                   previews::PreviewsType::OFFLINE));
     // Don't try to show other infobars if this is an offline preview.
     return;
   }
@@ -114,14 +106,12 @@ void PreviewsInfoBarTabHelper::DidFinishNavigation(
   if (headers && data_reduction_proxy::IsLitePagePreview(*headers)) {
     base::Time previews_freshness;
     headers->GetDateValue(&previews_freshness);
-
     PreviewsInfoBarDelegate::Create(
         web_contents(), previews::PreviewsType::LITE_PAGE, previews_freshness,
         true /* is_data_saver_user */, is_reload,
         base::Bind(&AddPreviewNavigationCallback,
                    web_contents()->GetBrowserContext(),
                    navigation_handle->GetRedirectChain()[0],
-                   previews::PreviewsType::LITE_PAGE),
-        previews_ui_service);
+                   previews::PreviewsType::LITE_PAGE));
   }
 }
