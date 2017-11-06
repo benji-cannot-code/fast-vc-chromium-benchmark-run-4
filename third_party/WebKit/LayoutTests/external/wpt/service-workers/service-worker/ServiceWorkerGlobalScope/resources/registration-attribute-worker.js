237d84ba160e3b2abaa6051600f2cbdfbaad7d04
Matt Falkenhagen <falken@chromium.org>
2017-11-06 19:59:14 +0000

    service worker: Upstream changes to registration-attribute.html
    This test diverged slightly from the WPT test. Merge the changes in and
    remove the chromium version.
    
    Bug: 437677, 557551, 688116
    Change-Id: Id3d06fef9e2d577987f268009c25619cff004966
    Reviewed-on: https://chromium-review.googlesource.com/754461
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514215}

--- third_party/WebKit/LayoutTests/external/wpt/service-workers/service-worker/ServiceWorkerGlobalScope/resources/registration-attribute-worker.js
+++ third_party/WebKit/LayoutTests/external/wpt/service-workers/service-worker/ServiceWorkerGlobalScope/resources/registration-attribute-worker.js
@@ -2,7 +2,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 importScripts('../../resources/test-helpers.sub.js');
 importScripts('../../resources/worker-testharness.js');
 
+// TODO(nhiroki): stop using global states because service workers can be killed
+// at any point. Instead, we could post a message to the page on each event via
+// Client object (http://crbug.com/558244).
 var events_seen = [];
+
+// TODO(nhiroki): Move these assertions to registration-attribute.html because
+// an assertion failure on the worker is not shown on the result page and
+// handled as timeout. See registration-attribute-newer-worker.js for example.
 
 assert_equals(
   self.registration.scope,
