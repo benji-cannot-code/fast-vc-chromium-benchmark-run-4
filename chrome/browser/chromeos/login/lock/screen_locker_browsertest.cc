fc25f802b9235267b7432116781501913a693b73
Steven Bennetts <stevenjb@chromium.org>
2017-11-06 20:43:47 +0000

    Revert "cros: Enable views-based lock by default."
    This reverts commit a309db51919b6e0159dd72214d6f5510a93a3283.
    
    Reason for revert: amd64-generic and betty VM Tests are failing (https://bugs.chromium.org/p/chromium/issues/detail?id=781845)
    
    Original change's description:
    > cros: Enable views-based lock by default.
    > 
    > Bug: 719015
    > Change-Id: I83b9207720d0166b69058c67a63e57d0ada69527
    > Reviewed-on: https://chromium-review.googlesource.com/726240
    > Commit-Queue: Jacob Dufault <jdufault@chromium.org>
    > Reviewed-by: James Cook <jamescook@chromium.org>
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#513987}
    
    TBR=jamescook@chromium.org,xiyuan@chromium.org,jdufault@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 719015
    Change-Id: I6be5ec041d823a02d87d29fdd4e6c7ba85799c4f
    Reviewed-on: https://chromium-review.googlesource.com/755333
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514236}

--- chrome/browser/chromeos/login/lock/screen_locker_browsertest.cc
+++ chrome/browser/chromeos/login/lock/screen_locker_browsertest.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
-#include "ash/public/cpp/ash_switches.h"
 #include "ash/wm/window_state.h"
 #include "base/command_line.h"
 #include "base/macros.h"
@@ -119,10 +118,6 @@ class ScreenLockerTest : public InProcessBrowserTest {
                     ->notify_lock_screen_dismissed_call_count();
   }
 
-  void SetUpCommandLine(base::CommandLine* command_line) override {
-    command_line->AppendSwitchASCII(switches::kLoginProfile, "user");
-  }
-
  private:
   void SetUpInProcessBrowserTestFixture() override {
     fake_session_manager_client_ = new FakeSessionManagerClient;
@@ -133,26 +128,16 @@ class ScreenLockerTest : public InProcessBrowserTest {
         ui::ScopedAnimationDurationScaleMode::ZERO_DURATION));
   }
 
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    command_line->AppendSwitchASCII(switches::kLoginProfile, "user");
+  }
+
   std::unique_ptr<ui::ScopedAnimationDurationScaleMode> zero_duration_mode_;
 
   DISALLOW_COPY_AND_ASSIGN(ScreenLockerTest);
 };
 
-class WebUiScreenLockerTest : public ScreenLockerTest {
- public:
-  WebUiScreenLockerTest() = default;
-  ~WebUiScreenLockerTest() override = default;
-
-  void SetUpCommandLine(base::CommandLine* command_line) override {
-    ScreenLockerTest::SetUpCommandLine(command_line);
-    command_line->AppendSwitch(ash::switches::kShowWebUiLock);
-  }
-
- private:
-  DISALLOW_COPY_AND_ASSIGN(WebUiScreenLockerTest);
-};
-
-IN_PROC_BROWSER_TEST_F(WebUiScreenLockerTest, TestBasic) {
+IN_PROC_BROWSER_TEST_F(ScreenLockerTest, TestBasic) {
   ScreenLocker::Show();
   std::unique_ptr<test::ScreenLockerTester> tester(ScreenLocker::GetTester());
   tester->EmulateWindowManagerReady();
@@ -200,7 +185,7 @@ IN_PROC_BROWSER_TEST_F(ScreenLockerTest, LockScreenWhileAddingUser) {
 }
 
 // Test how locking the screen affects an active fullscreen window.
-IN_PROC_BROWSER_TEST_F(WebUiScreenLockerTest, TestFullscreenExit) {
+IN_PROC_BROWSER_TEST_F(ScreenLockerTest, TestFullscreenExit) {
   // 1) If the active browser window is in fullscreen and the fullscreen window
   // does not have all the pixels (e.g. the shelf is auto hidden instead of
   // hidden), locking the screen should not exit fullscreen. The shelf is
