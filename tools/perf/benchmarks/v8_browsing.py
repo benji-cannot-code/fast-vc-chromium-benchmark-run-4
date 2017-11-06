6325288e23f5aadecd9252ec5cd849ea52a2848a
Mythri Alle <mythria@chromium.org>
2017-11-06 11:38:04 +0000

    Disable toi on v8.runtimestats.browsing_mobile
    This was already disabled on v8.browsing_mobile, so I think
    it should be disabled on v8.runtimestats as well.
    
    Also updated the bug id for cnn as well.
    
    Bug: chromium:728081
    Change-Id: Ifc152a4a5db870b78ec8d0f9709decafabf11d6a
    Reviewed-on: https://chromium-review.googlesource.com/753385
    Reviewed-by: Annie Sullivan <sullivan@chromium.org>
    Commit-Queue: Mythri Alle <mythria@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514110}

--- tools/perf/benchmarks/v8_browsing.py
+++ tools/perf/benchmarks/v8_browsing.py
@@ -312,7 +312,7 @@ class V8RuntimeStatsMobileBrowsingBenchmark(
         self.DisableStory(
             'browse:shopping:flipkart',
             [story.expectations.ALL_MOBILE],
-            'crbug.com/767970')
+            'crbug.com/767970, crbug.com/708300')
         self.DisableStory(
             'browse:news:cnn',
             [story.expectations.ALL_MOBILE],
@@ -321,6 +321,10 @@ class V8RuntimeStatsMobileBrowsingBenchmark(
              'browse:news:globo',
              [story.expectations.ALL_ANDROID],
              'crbug.com/714650')
+        self.DisableStory(
+             'browse:news:toi',
+             [story.expectations.ALL_ANDROID],
+             'crbug.com/728081')
         # TODO(rnephew): This disabling should move to CanRunOnBrowser.
         self.DisableStory(
              'browse:chrome:omnibox',
