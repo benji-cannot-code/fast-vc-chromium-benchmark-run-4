1cc2c521da6575b4a85e33ae6e278bb869887b93
Daniel Bratell <bratell@opera.com>
2017-11-06 11:44:46 +0000

    Avoid leaking mojo -blink.h headers outside blink
    mojo's -blink.h headers are for use inside blink. For the rest there
    is -shared.h headers.
    
    Change-Id: Iffb6bd56656545c35056ba49badfe06e8519ec29
    Reviewed-on: https://chromium-review.googlesource.com/753884
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Bratell <bratell@opera.com>
    Cr-Commit-Position: refs/heads/master@{#514111}

--- third_party/WebKit/public/platform/modules/presentation/WebPresentationAvailabilityObserver.h
+++ third_party/WebKit/public/platform/modules/presentation/WebPresentationAvailabilityObserver.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "public/platform/WebCommon.h"
 #include "public/platform/WebURL.h"
-#include "public/platform/modules/presentation/presentation.mojom-blink.h"
+#include "public/platform/modules/presentation/presentation.mojom-shared.h"
 
 namespace blink {
 
