b76df5418f4b5db8a1c1cfb9c5f280bb1b9d28c0
Dan Elphick <delphick@chromium.org>
2017-11-06 15:59:49 +0000

    Revert function names in BackgroundFetchContext and others
    Change the UpdateJobTitles back to UpdateUI back to match the names in
    the .mojom and Web API.
    
    The original CL is
    https://chromium-review.googlesource.com/c/chromium/src/+/753482
    
    Bug: 757760
    Change-Id: I1718c25747cf017c099907eb5ecf4cad1821b146
    Reviewed-on: https://chromium-review.googlesource.com/753387
    Commit-Queue: Dan Elphick <delphick@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514141}

--- content/browser/background_fetch/background_fetch_context.h
+++ content/browser/background_fetch/background_fetch_context.h
@@ -82,7 +82,7 @@ class CONTENT_EXPORT BackgroundFetchContext
   // Updates the title of the Background Fetch identified by |unique_id|. The
   // |callback| will be invoked when the title has been updated, or an error
   // occurred that prevents it from doing so.
-  void UpdateRegistrationUI(
+  void UpdateUI(
       const std::string& unique_id,
       const std::string& title,
       blink::mojom::BackgroundFetchService::UpdateUICallback callback);
@@ -119,7 +119,7 @@ class CONTENT_EXPORT BackgroundFetchContext
       std::unique_ptr<BackgroundFetchRegistration> registration);
 
   // Called when the new title has been updated in the data manager.
-  void DidUpdateStoredRegistrationUI(
+  void DidUpdateStoredUI(
       const std::string& unique_id,
       const std::string& title,
       blink::mojom::BackgroundFetchService::UpdateUICallback callback,
