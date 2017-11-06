86d4928c6186127c977358617d7894879e9e0f14
Daniel Erat <derat@chromium.org>
2017-11-06 23:15:14 +0000

    chromeos: Delete experimental spurious-power-button flags.
    Remove never-used flags that were added to attempt to ignore
    accidental power button presses:
    
    --spurious-power-button-window
    --spurious-power-button-accel-count
    --spurious-power-button-screen-accel
    --spurious-power-button-keyboard-accel
    --spurious-power-button-lid-angle-change
    
    Bug: 781561, 736936
    Change-Id: I389d6d2cc8d530bf1bef09d780fd52c1d6bd011c
    Reviewed-on: https://chromium-review.googlesource.com/754184
    Commit-Queue: Dan Erat <derat@chromium.org>
    Reviewed-by: Qiang(Joe) Xu <warx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514280}

--- ash/system/power/tablet_power_button_controller_test_api.h
+++ ash/system/power/tablet_power_button_controller_test_api.h
@@ -9,14 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/macros.h"
 
-namespace base {
-class CommandLine;
-}  // namespace base
-
-namespace chromeos {
-class AccelerometerReader;
-}  // namespace chromeos
-
 namespace ui {
 class KeyEvent;
 }  // namespace ui
@@ -39,16 +31,6 @@ class TabletPowerButtonControllerTestApi {
   // If |controller_->shutdown_timer_| is running, stops it, runs its task, and
   // returns true. Otherwise, returns false.
   bool TriggerShutdownTimeout() WARN_UNUSED_RESULT;
-
-  // Returns true if |controller_| is observing |reader|.
-  bool IsObservingAccelerometerReader(
-      chromeos::AccelerometerReader* reader) const;
-
-  // Calls |controller_|'s ParseSpuriousPowerButtonSwitches() method.
-  void ParseSpuriousPowerButtonSwitches(const base::CommandLine& command_line);
-
-  // Calls |controller_|'s IsSpuriousPowerButtonEvent() method.
-  bool IsSpuriousPowerButtonEvent() const;
 
   // Sends |event| to |controller_->display_controller_|.
   void SendKeyEvent(ui::KeyEvent* event);
