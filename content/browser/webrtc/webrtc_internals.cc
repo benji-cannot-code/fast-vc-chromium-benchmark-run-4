2b47ea4d2e4cb9b0a12b8f9788ee188205cce760
Elad Alon <eladalon@chromium.org>
2017-11-06 17:53:35 +0000

    Fix WebRTCInternals::SelectionType to comply with naming convention
    Comply with naming convention.
    
    Bug: None
    Change-Id: Iac94c2b488484f7b7acf25f8c83ea49bb7561dbe
    Reviewed-on: https://chromium-review.googlesource.com/753331
    Commit-Queue: Elad Alon <eladalon@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514168}

--- content/browser/webrtc/webrtc_internals.cc
+++ content/browser/webrtc/webrtc_internals.cc
@@ -88,7 +88,7 @@ WebRTCInternals::WebRTCInternals() : WebRTCInternals(500, true) {}
 
 WebRTCInternals::WebRTCInternals(int aggregate_updates_ms,
                                  bool should_block_power_saving)
-    : selection_type_(SelectionType::AudioDebugRecordings),
+    : selection_type_(SelectionType::kAudioDebugRecordings),
       audio_debug_recordings_(false),
       event_log_recordings_(false),
       num_open_connections_(0),
@@ -293,7 +293,7 @@ void WebRTCInternals::EnableAudioDebugRecordings(
 #if defined(OS_ANDROID)
   EnableAudioDebugRecordingsOnAllRenderProcessHosts();
 #else
-  selection_type_ = SelectionType::AudioDebugRecordings;
+  selection_type_ = SelectionType::kAudioDebugRecordings;
   DCHECK(!select_file_dialog_);
   select_file_dialog_ = ui::SelectFileDialog::Create(this, nullptr);
   select_file_dialog_->SelectFile(
@@ -353,7 +353,7 @@ void WebRTCInternals::EnableEventLogRecordings(
 #else
   DCHECK(web_contents);
   DCHECK(!select_file_dialog_);
-  selection_type_ = SelectionType::RtcEventLogs;
+  selection_type_ = SelectionType::kRtcEventLogs;
   select_file_dialog_ = ui::SelectFileDialog::Create(this, nullptr);
   select_file_dialog_->SelectFile(
       ui::SelectFileDialog::SELECT_SAVEAS_FILE, base::string16(),
@@ -417,11 +417,11 @@ void WebRTCInternals::FileSelected(const base::FilePath& path,
 #if BUILDFLAG(ENABLE_WEBRTC)
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
   switch (selection_type_) {
-    case SelectionType::RtcEventLogs:
+    case SelectionType::kRtcEventLogs:
       event_log_recordings_file_path_ = path;
       EnableEventLogRecordingsOnAllRenderProcessHosts();
       break;
-    case SelectionType::AudioDebugRecordings:
+    case SelectionType::kAudioDebugRecordings:
       audio_debug_recordings_file_path_ = path;
       EnableAudioDebugRecordingsOnAllRenderProcessHosts();
       break;
@@ -435,10 +435,10 @@ void WebRTCInternals::FileSelectionCanceled(void* params) {
 #if BUILDFLAG(ENABLE_WEBRTC)
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
   switch (selection_type_) {
-    case SelectionType::RtcEventLogs:
+    case SelectionType::kRtcEventLogs:
       SendUpdate("eventLogRecordingsFileSelectionCancelled", nullptr);
       break;
-    case SelectionType::AudioDebugRecordings:
+    case SelectionType::kAudioDebugRecordings:
       SendUpdate("audioDebugRecordingsFileSelectionCancelled", nullptr);
       break;
     default:
