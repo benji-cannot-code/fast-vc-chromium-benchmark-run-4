3836d1690b5354638727877d9042d0955a7fa4e9
Xiaochu Liu <xiaochu@chromium.org>
2017-11-07 19:17:47 +0000

    add OnCustomUninstall to ComponentInstallerPolicy
    Add OnCustomUninstall (similar to OnCustomInstall) that performs
    component specific operations after uninstall is complete.
    
    BUG=chromium:780172
    TEST=e2e test on DuT to remove comopnent.
    
    Change-Id: I1437b7bbd55387f52560326619e56f489eaab2d0
    Reviewed-on: https://chromium-review.googlesource.com/752001
    Commit-Queue: Xiaochu Liu <xiaochu@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514533}

--- chrome/browser/component_updater/optimization_hints_component_installer.cc
+++ chrome/browser/component_updater/optimization_hints_component_installer.cc
@@ -68,6 +68,8 @@ OptimizationHintsComponentInstallerPolicy::OnCustomInstall(
   return update_client::CrxInstaller::Result(0);  // Nothing custom here.
 }
 
+void OptimizationHintsComponentInstallerPolicy::OnCustomUninstall() {}
+
 void OptimizationHintsComponentInstallerPolicy::ComponentReady(
     const base::Version& version,
     const base::FilePath& install_dir,
--- components/component_updater/component_installer_unittest.cc
+++ components/component_updater/component_installer_unittest.cc
@@ -135,6 +135,8 @@ class FakeInstallerPolicy : public ComponentInstallerPolicy {
     return update_client::CrxInstaller::Result(0);
   }
 
+  void OnCustomUninstall() override {}
+
   void ComponentReady(
       const base::Version& version,
       const base::FilePath& install_dir,
