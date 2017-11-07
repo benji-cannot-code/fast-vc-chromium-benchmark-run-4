1360ff3dcf2788e1250b67a87d30d350e0309f30
Luna Lu <loonybear@chromium.org>
2017-11-07 18:06:39 +0000

    Feature Policy Onion Soup (3)
    STEP 6:
    Remove unnecessary layer in Platform.h (previously needed for type conversion)
    
    See Feature Policy Onion Soup (1) at:
    https://chromium-review.googlesource.com/c/chromium/src/+/726343/
    See Feature Policy Onion Soup (2) at:
    https://chromium-review.googlesource.com/c/chromium/src/+/742421/
    See Feature Policy Onion Soup (4) at:
    https://chromium-review.googlesource.com/c/chromium/src/+/749783
    
    Bug: 561879
    Change-Id: I25f6e71c3f780b418876dbf423689153af9171b7
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_chromium_dbg_ng;master.tryserver.chromium.linux:linux_site_isolation
    Reviewed-on: https://chromium-review.googlesource.com/742601
    Commit-Queue: Luna Lu <loonybear@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514506}

--- third_party/WebKit/Source/core/dom/SecurityContext.cpp
+++ third_party/WebKit/Source/core/dom/SecurityContext.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/SecurityContext.h"
 
 #include "core/frame/csp/ContentSecurityPolicy.h"
+#include "platform/feature_policy/FeaturePolicy.h"
 #include "platform/runtime_enabled_features.h"
 #include "platform/weborigin/SecurityOrigin.h"
 #include "public/platform/Platform.h"
@@ -107,16 +108,16 @@ void SecurityContext::InitializeFeaturePolicy(
     const ParsedFeaturePolicy& parsed_header,
     const ParsedFeaturePolicy& container_policy,
     const FeaturePolicy* parent_feature_policy) {
-  WebSecurityOrigin origin = WebSecurityOrigin(security_origin_);
-  feature_policy_ = Platform::Current()->CreateFeaturePolicy(
-      parent_feature_policy, container_policy, parsed_header, origin);
+  feature_policy_ = FeaturePolicy::CreateFromParentPolicy(
+      parent_feature_policy, container_policy, security_origin_->ToUrlOrigin());
+  feature_policy_->SetHeaderPolicy(parsed_header);
 }
 
 void SecurityContext::UpdateFeaturePolicyOrigin() {
   if (!feature_policy_)
     return;
-  feature_policy_ = Platform::Current()->DuplicateFeaturePolicyWithOrigin(
-      *feature_policy_, WebSecurityOrigin(security_origin_));
+  feature_policy_ = FeaturePolicy::CreateFromPolicyWithOrigin(
+      *feature_policy_, security_origin_->ToUrlOrigin());
 }
 
 }  // namespace blink
