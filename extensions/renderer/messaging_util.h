59e1b81d93aafe6885b4c5e9556f309c99671d11
Devlin Cronin <rdevlin.cronin@chromium.org>
2017-11-07 20:15:23 +0000

    [Extensions Bindings] Add native tabs hooks
    Add native hooks for the tabs API to use with sendRequest, sendMessage,
    and connect. Refactor a bit to pull out common code into
    messaging_util. Add tests for the new API hooks.
    
    Currently, the TabsHooksDelegate is only used in testing, and is not
    enabled (pending the rest of messaging being hooked up with native
    bindings).
    
    Bug: 653596
    Change-Id: I985ac774b2ad52ec08261ff31a90cf5e97e343a5
    Reviewed-on: https://chromium-review.googlesource.com/734720
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514566}

--- extensions/renderer/messaging_util.h
+++ extensions/renderer/messaging_util.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define EXTENSIONS_RENDERER_MESSAGING_UTIL_H_
 
 #include <memory>
+#include <string>
 
 #include "v8/include/v8.h"
 
@@ -14,6 +15,12 @@ namespace extensions {
 struct Message;
 
 namespace messaging_util {
+
+// The channel names for the sendMessage and sendRequest calls.
+extern const char kSendMessageChannel[];
+extern const char kSendRequestChannel[];
+
+extern const int kNoFrameId;
 
 // Parses the message from a v8 value, returning null on failure.
 std::unique_ptr<Message> MessageFromV8(v8::Local<v8::Context> context,
@@ -23,6 +30,43 @@ std::unique_ptr<Message> MessageFromV8(v8::Local<v8::Context> context,
 // should only be used for messages that have been validated.
 v8::Local<v8::Value> MessageToV8(v8::Local<v8::Context> context,
                                  const Message& message);
+
+// Extracts an integer id from |value|, including accounting for -0 (which is a
+// valid integer, but is stored in V8 as a number). This will DCHECK that
+// |value| is either an int32 or -0.
+int ExtractIntegerId(v8::Local<v8::Value> value);
+
+// The result of the call to ParseMessageOptions().
+enum ParseOptionsResult {
+  TYPE_ERROR,  // The arguments were invalid.
+  THROWN,      // The script threw an error during parsing.
+  SUCCESS,     // Parsing succeeded.
+};
+
+// Flags for ParseMessageOptions().
+enum ParseOptionsFlags {
+  NO_FLAGS = 0,
+  PARSE_CHANNEL_NAME = 1,
+  PARSE_FRAME_ID = 1 << 1,
+  PARSE_INCLUDE_TLS_CHANNEL_ID = 1 << 2,
+};
+
+struct MessageOptions {
+  std::string channel_name;
+  int frame_id = kNoFrameId;
+  bool include_tls_channel_id = false;
+};
+
+// Parses the parameters sent to sendMessage or connect, returning the result of
+// the attempted parse. If |check_for_channel_name| is true, also checks for a
+// provided channel name (this is only true for connect() calls). Populates the
+// result in |params_out| or |error_out| (depending on the success of the
+// parse).
+ParseOptionsResult ParseMessageOptions(v8::Local<v8::Context> context,
+                                       v8::Local<v8::Object> v8_options,
+                                       int flags,
+                                       MessageOptions* options_out,
+                                       std::string* error_out);
 
 }  // namespace messaging_util
 }  // namespace extensions
--- extensions/renderer/one_time_message_handler_unittest.cc
+++ extensions/renderer/one_time_message_handler_unittest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/renderer/bindings/api_bindings_system.h"
 #include "extensions/renderer/bindings/api_request_handler.h"
 #include "extensions/renderer/message_target.h"
+#include "extensions/renderer/messaging_util.h"
 #include "extensions/renderer/native_extension_bindings_system.h"
 #include "extensions/renderer/native_extension_bindings_system_test_base.h"
 #include "extensions/renderer/script_context.h"
@@ -24,8 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace extensions {
 
 namespace {
-
-constexpr char kSendMessageChannel[] = "chrome.runtime.sendMessage";
 
 constexpr char kEchoArgsAndError[] =
     "(function() {\n"
@@ -96,18 +95,18 @@ TEST_F(OneTimeMessageHandlerTest, SendMessageAndDontExpectReply) {
   // We should open a message port, send a message, and then close it
   // immediately.
   MessageTarget target(MessageTarget::ForExtension(extension()->id()));
-  EXPECT_CALL(
-      *ipc_message_sender(),
-      SendOpenMessageChannel(script_context(), port_id, target,
-                             kSendMessageChannel, include_tls_channel_id));
+  EXPECT_CALL(*ipc_message_sender(),
+              SendOpenMessageChannel(script_context(), port_id, target,
+                                     messaging_util::kSendMessageChannel,
+                                     include_tls_channel_id));
   EXPECT_CALL(*ipc_message_sender(),
               SendPostMessageToPort(MSG_ROUTING_NONE, port_id, message));
   EXPECT_CALL(*ipc_message_sender(),
               SendCloseMessagePort(MSG_ROUTING_NONE, port_id, true));
 
-  message_handler()->SendMessage(script_context(), port_id, target,
-                                 kSendMessageChannel, include_tls_channel_id,
-                                 message, v8::Local<v8::Function>());
+  message_handler()->SendMessage(
+      script_context(), port_id, target, messaging_util::kSendMessageChannel,
+      include_tls_channel_id, message, v8::Local<v8::Function>());
   ::testing::Mock::VerifyAndClearExpectations(ipc_message_sender());
 
   EXPECT_FALSE(message_handler()->HasPort(script_context(), port_id));
@@ -133,16 +132,16 @@ TEST_F(OneTimeMessageHandlerTest, SendMessageAndExpectReply) {
   // We should open a message port and send a message, and the message port
   // should remain open (to allow for a reply).
   MessageTarget target(MessageTarget::ForExtension(extension()->id()));
-  EXPECT_CALL(
-      *ipc_message_sender(),
-      SendOpenMessageChannel(script_context(), port_id, target,
-                             kSendMessageChannel, include_tls_channel_id));
+  EXPECT_CALL(*ipc_message_sender(),
+              SendOpenMessageChannel(script_context(), port_id, target,
+                                     messaging_util::kSendMessageChannel,
+                                     include_tls_channel_id));
   EXPECT_CALL(*ipc_message_sender(),
               SendPostMessageToPort(MSG_ROUTING_NONE, port_id, message));
 
   message_handler()->SendMessage(script_context(), port_id, target,
-                                 kSendMessageChannel, include_tls_channel_id,
-                                 message, callback);
+                                 messaging_util::kSendMessageChannel,
+                                 include_tls_channel_id, message, callback);
   ::testing::Mock::VerifyAndClearExpectations(ipc_message_sender());
 
   // We should have added a pending request to the APIRequestHandler, but
@@ -179,15 +178,15 @@ TEST_F(OneTimeMessageHandlerTest, DisconnectOpener) {
       FunctionFromString(context, kEchoArgsAndError);
 
   MessageTarget target(MessageTarget::ForExtension(extension()->id()));
-  EXPECT_CALL(
-      *ipc_message_sender(),
-      SendOpenMessageChannel(script_context(), port_id, target,
-                             kSendMessageChannel, include_tls_channel_id));
+  EXPECT_CALL(*ipc_message_sender(),
+              SendOpenMessageChannel(script_context(), port_id, target,
+                                     messaging_util::kSendMessageChannel,
+                                     include_tls_channel_id));
   EXPECT_CALL(*ipc_message_sender(),
               SendPostMessageToPort(MSG_ROUTING_NONE, port_id, message));
   message_handler()->SendMessage(script_context(), port_id, target,
-                                 kSendMessageChannel, include_tls_channel_id,
-                                 message, callback);
+                                 messaging_util::kSendMessageChannel,
+                                 include_tls_channel_id, message, callback);
   ::testing::Mock::VerifyAndClearExpectations(ipc_message_sender());
 
   EXPECT_EQ("undefined", GetGlobalProperty(context, "replyArgs"));
