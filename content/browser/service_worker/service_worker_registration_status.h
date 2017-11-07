0e7e45bee8be208bfbd306ce6763e64c7e49e505
xzhan96 <xiaofeng.zhang@intel.com>
2017-11-07 01:35:44 +0000

    [ServiceWorker] Eliminate unused function of sw registration status response
    After mojofing all methods related with service worker registrations,
    we use GetServiceWorkerErrorTypeForRegistration to replace with
    GetServiceWorkerRegistrationStatusResponse.
    
    BUG=755836
    
    Change-Id: I198c356841ca536d0c6fc6829b747ed8a26eebc6
    Reviewed-on: https://chromium-review.googlesource.com/753298
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Commit-Queue: Xiaofeng Zhang <xiaofeng.zhang@intel.com>
    Cr-Commit-Position: refs/heads/master@{#514333}

--- content/browser/service_worker/service_worker_registration_status.h
+++ content/browser/service_worker/service_worker_registration_status.h
@@ -12,17 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-// TODO(leonhsl): Eliminate this function once all its users changed to use
-// GetServiceWorkerErrorTypeForRegistration() which gets out a std::string
-// |message| rather than a base::string16. The legacy IPC used base::string16 as
-// an old WebKit convention. This should only be called for errors, where status
-// != OK.
-void GetServiceWorkerRegistrationStatusResponse(
-    ServiceWorkerStatusCode status,
-    const std::string& status_message,
-    blink::mojom::ServiceWorkerErrorType* error_type,
-    base::string16* message);
-
 // This should only be called for errors, where status != OK.
 void GetServiceWorkerErrorTypeForRegistration(
     ServiceWorkerStatusCode status,
