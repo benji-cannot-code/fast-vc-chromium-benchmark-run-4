0fdff95d282c118016f76565706213a393b9b1c3
Michael Spang <spang@chromium.org>
2017-11-07 00:25:40 +0000

    Enable more menu tests under ozone & Chrome OS
    The nested message loops breaking these tests have long been removed.
    Enable them under ozone.
    
    BUG=401304
    TEST=interactive_ui_tests
    
    Change-Id: Icf7487420176cc57f594a7f1f0d5bff8edd0286a
    Reviewed-on: https://chromium-review.googlesource.com/755921
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Michael Spang <spang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514316}

--- chrome/browser/ui/views/bookmarks/bookmark_bar_view_test.cc
+++ chrome/browser/ui/views/bookmarks/bookmark_bar_view_test.cc
@@ -1289,15 +1289,7 @@ class BookmarkBarViewTest11 : public BookmarkBarViewEventTestBase {
   BookmarkContextMenuNotificationObserver observer_;
 };
 
-#if defined(OS_LINUX) && !defined(OS_CHROMEOS)
-// TODO(erg): linux_aura bringup: http://crbug.com/163931
-#define MAYBE_CloseMenuAfterClosingContextMenu \
-  DISABLED_CloseMenuAfterClosingContextMenu
-#else
-#define MAYBE_CloseMenuAfterClosingContextMenu CloseMenuAfterClosingContextMenu
-#endif
-
-VIEW_TEST(BookmarkBarViewTest11, MAYBE_CloseMenuAfterClosingContextMenu)
+VIEW_TEST(BookmarkBarViewTest11, CloseMenuAfterClosingContextMenu)
 
 // Tests showing a modal dialog from a context menu.
 class BookmarkBarViewTest12 : public BookmarkBarViewEventTestBase {
@@ -1625,14 +1617,7 @@ class BookmarkBarViewTest16 : public BookmarkBarViewEventTestBase {
   }
 };
 
-#if defined(OS_LINUX) && !defined(OS_CHROMEOS)
-// TODO(erg): linux_aura bringup: http://crbug.com/163931
-#define MAYBE_DeleteMenu DISABLED_DeleteMenu
-#else
-#define MAYBE_DeleteMenu DeleteMenu
-#endif
-
-VIEW_TEST(BookmarkBarViewTest16, MAYBE_DeleteMenu)
+VIEW_TEST(BookmarkBarViewTest16, DeleteMenu)
 
 // Makes sure right clicking on an item while a context menu is already showing
 // doesn't crash and works.
@@ -1767,16 +1752,7 @@ class BookmarkBarViewTest18 : public BookmarkBarViewEventTestBase {
   }
 };
 
-#if defined(OS_LINUX) && !defined(OS_CHROMEOS)
-// TODO(erg): linux_aura bringup: http://crbug.com/163931
-#define MAYBE_BookmarkBarViewTest18_SiblingMenu \
-  DISABLED_BookmarkBarViewTest18_SiblingMenu
-#else
-#define MAYBE_BookmarkBarViewTest18_SiblingMenu \
-  BookmarkBarViewTest18_SiblingMenu
-#endif
-
-VIEW_TEST(BookmarkBarViewTest18, MAYBE_BookmarkBarViewTest18_SiblingMenu)
+VIEW_TEST(BookmarkBarViewTest18, BookmarkBarViewTest18_SiblingMenu)
 
 // Verifies mousing over an already open sibling menu doesn't prematurely cancel
 // the menu.
