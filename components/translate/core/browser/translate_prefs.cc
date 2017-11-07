54b035a93e57b754c1b50ae8065b3ecc63771757
Claudio Magni <claudiomagni@chromium.org>
2017-11-07 08:58:51 +0000

    Removed unused include
    This was suggested by Hajime in previous CL 732814, right before submit.
    
    Bug: 746771
    Change-Id: I056f53a3d04a26ae7636ba74f7c08d37c07fbc68
    Reviewed-on: https://chromium-review.googlesource.com/756294
    Reviewed-by: Hajime Hoshi <hajimehoshi@chromium.org>
    Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514422}

--- components/translate/core/browser/translate_prefs.cc
+++ components/translate/core/browser/translate_prefs.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/translate/core/browser/translate_prefs.h"
 
 #include <set>
-#include <unordered_set>
 #include <utility>
 
 #include "base/feature_list.h"
