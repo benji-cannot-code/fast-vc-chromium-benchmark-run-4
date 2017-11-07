c637df0c201ed0885859b4090119ac91d1cf609d
Miguel Casas-Sanchez <mcasas@chromium.org>
2017-11-07 05:03:34 +0000

    ui/display: add flag to enable use of monitor's color space info
    This CL adds a new flag to control if the monitor's provided color
    space info is actually propagated at the DisplayManager level, and
    also surfaces this flag in chrome://flags. The flag is disabled by
    default and the chrome://flags entry allows for enabling it. This
    flag will help debug the possibly related issue 779311.
    
    Bug: 771345, 779311
    Change-Id: I6df69c5fb9ab9954e9ff9f4fc04fe0616594315c
    Reviewed-on: https://chromium-review.googlesource.com/754593
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514398}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1623,6 +1623,9 @@ extern const char kUseMusDescription[];
 extern const char kEnableMashDescription[];
 extern const char kEnableMusDescription[];
 
+extern const char kUseMonitorColorSpaceName[];
+extern const char kUseMonitorColorSpaceDescription[];
+
 extern const char kVideoPlayerChromecastSupportName[];
 extern const char kVideoPlayerChromecastSupportDescription[];
 
