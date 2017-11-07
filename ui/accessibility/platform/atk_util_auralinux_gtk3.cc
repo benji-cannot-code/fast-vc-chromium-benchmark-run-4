b64bd6320a22c00dc47033c3be521020191ddb94
Jose Dapena Paz <jose.dapena@lge.com>
2017-11-07 19:43:13 +0000

    Move GTK2 and GTK3 ATK initialization to specific files.
    Instead of using build guards to isolate ATK bridge initialization
    code in GTK2 and GTK3, split it into different files.
    
    Bug: 780973
    Change-Id: I2877a1c0aea6252211d2d17e43a331c3b706a505
    Reviewed-on: https://chromium-review.googlesource.com/755134
    Commit-Queue: José Dapena Paz <jose.dapena@lge.com>
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514546}

---
+++ ui/accessibility/platform/atk_util_auralinux_gtk3.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <atk-bridge.h>
+
+#include "base/environment.h"
+#include "ui/accessibility/platform/atk_util_auralinux.h"
+
+namespace ui {
+
+bool AtkUtilAuraLinux::PlatformShouldEnableAccessibility() {
+  return false;
+}
+
+void AtkUtilAuraLinux::PlatformInitializeAsync() {
+  // AT bridge enabling was disabled before loading GTK to avoid
+  // getting GTK implementation ATK root.
+  std::unique_ptr<base::Environment> env(base::Environment::Create());
+  env->UnSetVar("NO_AT_BRIDGE");
+  atk_bridge_adaptor_init(nullptr, nullptr);
+}
+
+}  // namespace ui
