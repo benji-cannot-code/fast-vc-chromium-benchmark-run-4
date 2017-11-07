8d0e05fee0e44f889e95242c40ad76025b115a87
Nektarios Paisios <nektar@chromium.org>
2017-11-07 23:22:37 +0000

    Retrieves the parent widget of the current widget by always getting the top-level widget
    Surprisingly this method works quite well and doesn't require us to create a |AddedToWidgetObserver|.
    R=dmazzoni@chromium.org, aleventhal@chromium.org, tapted@chromium.org
    
    Bug: 730971
    Change-Id: Id84d70605c01705e259253a7ab33330b1177d496
    Tested: manually, existing unit tests
    Reviewed-on: https://chromium-review.googlesource.com/755950
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514644}

--- ui/views/accessibility/native_view_accessibility_mac.mm
+++ ui/views/accessibility/native_view_accessibility_mac.mm
@@ -29,9 +29,6 @@ gfx::NativeViewAccessible NativeViewAccessibilityMac::GetParent() {
   if (view_->GetWidget())
     return view_->GetWidget()->GetNativeView();
 
-  if (parent_widget_)
-    return parent_widget_->GetRootView()->GetNativeViewAccessible();
-
   return nullptr;
 }
 
