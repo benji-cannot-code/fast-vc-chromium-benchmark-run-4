40c2e7e102436a0e67f2be8419d836c82d863ace
Dan Elphick <delphick@chromium.org>
2017-11-07 14:26:37 +0000

    Factor out RequestManager from BackgroundFetchDataManager
    RequestManager is a small interface containing all the methods that
    JobController calls on DataManager. These methods are all to do with
    getting requests off the queue and updating their status. This means
    that BackgroundFetchJobController now has no dependency on DataManager.
    
    Also removes the final usage of DataManager in
    BackgroundFetchServiceImpl by adding the method to
    BackgroundFetchContext instead. As a result the data_manager() getter
    can be removed from BackgroundFetchContext.
    
    DataManager is now only used directly by the Context class and the
    Database tasks in storage.
    
    TBR=avi@chromium.org
    
    Bug: 757760
    Change-Id: I959ae0855fe2b9d8c5951f676252404f61aa123c
    Reviewed-on: https://chromium-review.googlesource.com/756746
    Commit-Queue: Dan Elphick <delphick@chromium.org>
    Reviewed-by: Anita Woodruff <awdf@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514464}

--- content/browser/background_fetch/background_fetch_job_controller.cc
+++ content/browser/background_fetch/background_fetch_job_controller.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/memory/ptr_util.h"
+#include "content/browser/background_fetch/background_fetch_request_manager.h"
 #include "content/public/browser/browser_thread.h"
 
 namespace content {
@@ -17,13 +18,13 @@ BackgroundFetchJobController::BackgroundFetchJobController(
     const BackgroundFetchRegistrationId& registration_id,
     const BackgroundFetchOptions& options,
     const BackgroundFetchRegistration& registration,
-    BackgroundFetchDataManager* data_manager,
+    BackgroundFetchRequestManager* request_manager,
     ProgressCallback progress_callback,
     FinishedCallback finished_callback)
     : registration_id_(registration_id),
       options_(options),
       complete_requests_downloaded_bytes_cache_(registration.downloaded),
-      data_manager_(data_manager),
+      request_manager_(request_manager),
       delegate_proxy_(delegate_proxy),
       progress_callback_(std::move(progress_callback)),
       finished_callback_(std::move(finished_callback)),
@@ -52,7 +53,7 @@ void BackgroundFetchJobController::Start() {
   // TODO(crbug.com/741609): Enforce kMaximumBackgroundFetchParallelRequests
   // globally and/or per origin rather than per fetch.
   for (size_t i = 0; i < kMaximumBackgroundFetchParallelRequests; i++) {
-    data_manager_->PopNextRequest(
+    request_manager_->PopNextRequest(
         registration_id_,
         base::BindOnce(&BackgroundFetchJobController::StartRequest,
                        weak_ptr_factory_.GetWeakPtr()));
@@ -78,8 +79,8 @@ void BackgroundFetchJobController::DidStartRequest(
     const scoped_refptr<BackgroundFetchRequestInfo>& request,
     const std::string& download_guid) {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
-  data_manager_->MarkRequestAsStarted(registration_id_, request.get(),
-                                      download_guid);
+  request_manager_->MarkRequestAsStarted(registration_id_, request.get(),
+                                         download_guid);
 }
 
 void BackgroundFetchJobController::DidUpdateRequest(
@@ -103,14 +104,13 @@ void BackgroundFetchJobController::DidCompleteRequest(
     const std::string& download_guid) {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
 
-  // This request is no longer in-progress, so the DataManager will take over
-  // responsibility for storing its downloaded bytes, though still need a cache.
   active_request_download_bytes_.erase(download_guid);
   complete_requests_downloaded_bytes_cache_ += request->GetFileSize();
 
-  // The DataManager must acknowledge that it stored the data and that there are
-  // no more pending requests to avoid marking this job as completed too early.
-  data_manager_->MarkRequestAsComplete(
+  // The RequestManager must acknowledge that it stored the data and that there
+  // are no more pending requests to avoid marking this job as completed too
+  // early.
+  request_manager_->MarkRequestAsComplete(
       registration_id_, request.get(),
       base::BindOnce(&BackgroundFetchJobController::DidMarkRequestCompleted,
                      weak_ptr_factory_.GetWeakPtr()));
@@ -123,7 +123,7 @@ void BackgroundFetchJobController::DidMarkRequestCompleted(
   // If not all requests have completed, start a pending request if there are
   // any left, and bail.
   if (has_pending_or_active_requests) {
-    data_manager_->PopNextRequest(
+    request_manager_->PopNextRequest(
         registration_id_,
         base::BindOnce(&BackgroundFetchJobController::StartRequest,
                        weak_ptr_factory_.GetWeakPtr()));
--- content/browser/background_fetch/background_fetch_service_unittest.cc
+++ content/browser/background_fetch/background_fetch_service_unittest.cc
@@ -124,6 +124,7 @@ class BackgroundFetchServiceTest : public BackgroundFetchTestBase {
 
   // Synchronous wrapper for BackgroundFetchServiceImpl::GetDeveloperIds().
   void GetDeveloperIds(int64_t service_worker_registration_id,
+                       const url::Origin& origin,
                        blink::mojom::BackgroundFetchError* out_error,
                        std::vector<std::string>* out_developer_ids) {
     DCHECK(out_error);
@@ -131,7 +132,7 @@ class BackgroundFetchServiceTest : public BackgroundFetchTestBase {
 
     base::RunLoop run_loop;
     service_->GetDeveloperIds(
-        service_worker_registration_id, origin(),
+        service_worker_registration_id, origin,
         base::BindOnce(&BackgroundFetchServiceTest::DidGetDeveloperIds,
                        base::Unretained(this), run_loop.QuitClosure(),
                        out_error, out_developer_ids));
@@ -826,7 +827,8 @@ TEST_F(BackgroundFetchServiceTest, GetDeveloperIds) {
     blink::mojom::BackgroundFetchError error;
     std::vector<std::string> developer_ids;
 
-    GetDeveloperIds(service_worker_registration_id, &error, &developer_ids);
+    GetDeveloperIds(service_worker_registration_id, origin(), &error,
+                    &developer_ids);
     ASSERT_EQ(error, blink::mojom::BackgroundFetchError::NONE);
 
     ASSERT_EQ(developer_ids.size(), 0u);
@@ -847,7 +849,8 @@ TEST_F(BackgroundFetchServiceTest, GetDeveloperIds) {
     blink::mojom::BackgroundFetchError error;
     std::vector<std::string> developer_ids;
 
-    GetDeveloperIds(service_worker_registration_id, &error, &developer_ids);
+    GetDeveloperIds(service_worker_registration_id, origin(), &error,
+                    &developer_ids);
     ASSERT_EQ(error, blink::mojom::BackgroundFetchError::NONE);
 
     ASSERT_EQ(developer_ids.size(), 1u);
@@ -869,7 +872,8 @@ TEST_F(BackgroundFetchServiceTest, GetDeveloperIds) {
     blink::mojom::BackgroundFetchError error;
     std::vector<std::string> developer_ids;
 
-    GetDeveloperIds(service_worker_registration_id, &error, &developer_ids);
+    GetDeveloperIds(service_worker_registration_id, origin(), &error,
+                    &developer_ids);
     ASSERT_EQ(error, blink::mojom::BackgroundFetchError::NONE);
 
     ASSERT_EQ(developer_ids.size(), 2u);
@@ -879,6 +883,36 @@ TEST_F(BackgroundFetchServiceTest, GetDeveloperIds) {
                 developer_ids[1] == kExampleDeveloperId);
     EXPECT_TRUE(developer_ids[0] == kAlternativeDeveloperId ||
                 developer_ids[1] == kAlternativeDeveloperId);
+  }
+
+  // Verify that using the wrong origin does not return developer ids even if
+  // the service worker registration is correct.
+  {
+    blink::mojom::BackgroundFetchError error;
+    std::vector<std::string> developer_ids;
+
+    GetDeveloperIds(service_worker_registration_id,
+                    url::Origin::Create(GURL("https://www.bogus-origin.com")),
+                    &error, &developer_ids);
+    ASSERT_EQ(error, blink::mojom::BackgroundFetchError::NONE);
+
+    ASSERT_EQ(developer_ids.size(), 0u);
+  }
+
+  // Verify that using the wrong service worker id does not return developer ids
+  // even if the origin is correct.
+  {
+    blink::mojom::BackgroundFetchError error;
+    std::vector<std::string> developer_ids;
+
+    int64_t bogus_service_worker_registration_id =
+        service_worker_registration_id + 1;
+
+    GetDeveloperIds(bogus_service_worker_registration_id, origin(), &error,
+                    &developer_ids);
+    ASSERT_EQ(error, blink::mojom::BackgroundFetchError::NONE);
+
+    ASSERT_EQ(developer_ids.size(), 0u);
   }
 }
 
