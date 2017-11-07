a66c685105d2d7e99f941080260bd259920ac876
David Reveman <reveman@chromium.org>
2017-11-07 05:22:59 +0000

    exo: Fix buffer release handling when window is changing size.
    Normally resources in last frame are not released by the frame
    sink but if the window size changes then that will cause the
    frame sink id to change and trigger a release of all old resources.
    This is incorrect behavior as the resource is still in use by
    the new frame sink.
    
    Fix this problem by keeping track of the last submitted resources
    and avoid releasing them.
    
    This also removes an unused parameter from SurfaceTreeHost ctor.
    
    Bug: 782098
    Test: exo_unittests --gtest_filter=BufferTest.SurfaceTreeHostLastFrame
    Change-Id: I5e08fe7a0b8a6c2cfb0c60f5eb30cf33fbe8fb42
    Reviewed-on: https://chromium-review.googlesource.com/756374
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Commit-Queue: David Reveman <reveman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514402}

--- components/exo/surface_tree_host.h
+++ components/exo/surface_tree_host.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace aura {
 class Window;
-class WindowDelegate;
 }  // namespace aura
 
 namespace gfx {
@@ -37,8 +36,7 @@ class SurfaceTreeHost : public SurfaceDelegate,
                         public viz::BeginFrameObserverBase,
                         public ui::ContextFactoryObserver {
  public:
-  SurfaceTreeHost(const std::string& window_name,
-                  aura::WindowDelegate* window_delegate);
+  explicit SurfaceTreeHost(const std::string& window_name);
   ~SurfaceTreeHost() override;
 
   // Sets a root surface of a surface tree. This surface tree will be hosted in
