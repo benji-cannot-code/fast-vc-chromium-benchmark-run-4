95cfcc7a151ea521ac79fbbebd0f7ec19b2d4797
Michael Martis <martis@chromium.org>
2017-11-07 01:57:35 +0000

    Optionally produce the heuristic language model in iOS.
    - This doesn't change any default behaviour, as the "use baseline" flag is
      enabled and the "use heuristic" flag is disabled by default.
    - We add a ULP preference to pref_names (to match e.g. the accept language
      preference) which duplicates definitions in translate_prefs. The existing
      definitions will go away shortly (crbug.com/777611, where we back out of the
      translate experiment that uses them), so I think this is fine.
    
    Bug: 762352
    Change-Id: Id0a3b57089365c930cc139061d9a36780c4f8f42
    Reviewed-on: https://chromium-review.googlesource.com/752965
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Jia Meng <jiameng@chromium.org>
    Commit-Queue: Michael Martis <martis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514349}

---
+++ components/language/core/browser/pref_names.h
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_LANGUAGE_CORE_BROWSER_PREF_NAMES_H_
+#define COMPONENTS_LANGUAGE_CORE_BROWSER_PREF_NAMES_H_
+
+namespace language {
+namespace prefs {
+
+// TODO(martis): Add accept language preference here.
+// TODO(martis): Use these preferences for Content too.
+extern const char kUserLanguageProfile[];
+
+}  // namespace prefs
+}  // namespace language
+
+#endif  // COMPONENTS_LANGUAGE_CORE_BROWSER_PREF_NAMES_H_
--- ios/chrome/browser/language/language_model_factory_unittest.cc
+++ ios/chrome/browser/language/language_model_factory_unittest.cc
@@ -5,43 +5,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ios/chrome/browser/language/language_model_factory.h"
 
-#include "base/test/scoped_feature_list.h"
-#include "components/language/core/browser/baseline_language_model.h"
 #include "ios/chrome/browser/browser_state/test_chrome_browser_state.h"
 #include "ios/web/public/test/test_web_thread_bundle.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/platform_test.h"
 
-using base::test::ScopedFeatureList;
 using testing::Eq;
 using testing::IsNull;
 using testing::Not;
 
 using LanguageModelFactoryTest = PlatformTest;
 
-// Check that the language model is not returned unless the experiment is
-// enabled.
-TEST_F(LanguageModelFactoryTest, Disabled) {
-  ScopedFeatureList disable_feature;
-  disable_feature.InitAndDisableFeature(language::kUseBaselineLanguageModel);
-  web::TestWebThreadBundle thread_bundle;
-
-  std::unique_ptr<TestChromeBrowserState> state(
-      TestChromeBrowserState::Builder().Build());
-  const language::LanguageModel* const model =
-      LanguageModelFactory::GetForBrowserState(state.get());
-  EXPECT_THAT(model, IsNull());
-
-  ios::ChromeBrowserState* const incognito =
-      state->GetOffTheRecordChromeBrowserState();
-  ASSERT_THAT(incognito, Not(IsNull()));
-  EXPECT_THAT(LanguageModelFactory::GetForBrowserState(incognito), IsNull());
-}
-
 // Check that Incognito language modeling is inherited from the user's profile.
 TEST_F(LanguageModelFactoryTest, SharedWithIncognito) {
-  ScopedFeatureList enable_feature;
-  enable_feature.InitAndEnableFeature(language::kUseBaselineLanguageModel);
   web::TestWebThreadBundle thread_bundle;
 
   std::unique_ptr<TestChromeBrowserState> state(
