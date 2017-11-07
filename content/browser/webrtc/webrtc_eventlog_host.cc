64f666d16701ccb06961a1b6c5568b08e1006553
Elad Alon <eladalon@chromium.org>
2017-11-07 16:14:27 +0000

    Fix limit on concurrent RTC event log files
    WebRTCEventLogHost::StopWebRTCEventLog() is not called when a 
    PeerConnection is torn down. This leads to number_active_log_files_ 
    never being decremented, which in turn leads us to have up to 5 logs 
    per Chrome session, rather than up to 5 ACTIVE logs per Chrome session.
    
    Bug: 780855
    Change-Id: I06e916136bc539d6783a91d8967f8a09f0f31c9e
    Reviewed-on: https://chromium-review.googlesource.com/751666
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Elad Alon <eladalon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514481}

--- content/browser/webrtc/webrtc_eventlog_host.cc
+++ content/browser/webrtc/webrtc_eventlog_host.cc
@@ -26,8 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-int WebRTCEventLogHost::number_active_log_files_ = 0;
-
 namespace {
 
 // In addition to the limit to the number of files given below, the size of the
@@ -78,6 +76,9 @@ WebRTCEventLogHost::WebRTCEventLogHost(int render_process_id)
 
 WebRTCEventLogHost::~WebRTCEventLogHost() {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
+  for (int lid : active_peer_connection_local_ids_) {
+    RtcEventLogRemoved(lid);
+  }
 }
 
 void WebRTCEventLogHost::PeerConnectionAdded(int peer_connection_local_id) {
@@ -88,7 +89,7 @@ void WebRTCEventLogHost::PeerConnectionAdded(int peer_connection_local_id) {
       active_peer_connection_local_ids_.end()) {
     active_peer_connection_local_ids_.push_back(peer_connection_local_id);
     if (rtc_event_logging_enabled_ &&
-        number_active_log_files_ < kMaxNumberLogFiles) {
+        ActivePeerConnectionsWithLogFiles().size() < kMaxNumberLogFiles) {
       StartEventLogForPeerConnection(peer_connection_local_id);
     }
   }
@@ -96,12 +97,13 @@ void WebRTCEventLogHost::PeerConnectionAdded(int peer_connection_local_id) {
 
 void WebRTCEventLogHost::PeerConnectionRemoved(int peer_connection_local_id) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
-  const auto found = std::find(active_peer_connection_local_ids_.begin(),
-                               active_peer_connection_local_ids_.end(),
-                               peer_connection_local_id);
-  if (found != active_peer_connection_local_ids_.end()) {
-    active_peer_connection_local_ids_.erase(found);
+  const auto lid = std::find(active_peer_connection_local_ids_.begin(),
+                             active_peer_connection_local_ids_.end(),
+                             peer_connection_local_id);
+  if (lid != active_peer_connection_local_ids_.end()) {
+    active_peer_connection_local_ids_.erase(lid);
   }
+  RtcEventLogRemoved(peer_connection_local_id);
 }
 
 bool WebRTCEventLogHost::StartWebRTCEventLog(const base::FilePath& file_path) {
@@ -117,15 +119,17 @@ bool WebRTCEventLogHost::StartWebRTCEventLog(const base::FilePath& file_path) {
 
 bool WebRTCEventLogHost::StopWebRTCEventLog() {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
-  if (!rtc_event_logging_enabled_)
+  if (!rtc_event_logging_enabled_) {
+    DCHECK(ActivePeerConnectionsWithLogFiles().empty());
     return false;
-  number_active_log_files_ = 0;
+  }
   rtc_event_logging_enabled_ = false;
   RenderProcessHost* host = RenderProcessHost::FromID(render_process_id_);
   if (host) {
     for (int local_id : active_peer_connection_local_ids_)
       host->Send(new PeerConnectionTracker_StopEventLog(local_id));
   }
+  ActivePeerConnectionsWithLogFiles().clear();
   return true;
 }
 
@@ -133,10 +137,18 @@ base::WeakPtr<WebRTCEventLogHost> WebRTCEventLogHost::GetWeakPtr() {
   return weak_ptr_factory_.GetWeakPtr();
 }
 
+std::vector<WebRTCEventLogHost::PeerConnectionKey>&
+WebRTCEventLogHost::ActivePeerConnectionsWithLogFiles() {
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
+  CR_DEFINE_STATIC_LOCAL(std::vector<WebRTCEventLogHost::PeerConnectionKey>,
+                         vector, ());
+  return vector;
+}
+
 void WebRTCEventLogHost::StartEventLogForPeerConnection(
     int peer_connection_local_id) {
-  if (number_active_log_files_ < kMaxNumberLogFiles) {
-    ++number_active_log_files_;
+  if (ActivePeerConnectionsWithLogFiles().size() < kMaxNumberLogFiles) {
+    RtcEventLogAdded(peer_connection_local_id);
     base::PostTaskWithTraitsAndReplyWithResult(
         FROM_HERE, {base::MayBlock(), base::TaskPriority::BACKGROUND},
         base::Bind(&CreateEventLogFileForChildProcess, base_file_path_,
@@ -149,8 +161,10 @@ void WebRTCEventLogHost::StartEventLogForPeerConnection(
 void WebRTCEventLogHost::SendEventLogFileToRenderer(
     int peer_connection_local_id,
     IPC::PlatformFileForTransit file_for_transit) {
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
   if (file_for_transit == IPC::InvalidPlatformFileForTransit()) {
-    --number_active_log_files_;
+    bool removed = RtcEventLogRemoved(peer_connection_local_id);
+    DCHECK(removed);
     return;
   }
   RenderProcessHost* rph = RenderProcessHost::FromID(render_process_id_);
@@ -158,9 +172,36 @@ void WebRTCEventLogHost::SendEventLogFileToRenderer(
     rph->Send(new PeerConnectionTracker_StartEventLog(peer_connection_local_id,
                                                       file_for_transit));
   } else {
-    --number_active_log_files_;
+    bool removed = RtcEventLogRemoved(peer_connection_local_id);
+    DCHECK(removed);
     IPC::PlatformFileForTransitToFile(file_for_transit).Close();
   }
+}
+
+void WebRTCEventLogHost::RtcEventLogAdded(int peer_connection_local_id) {
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
+  auto& active_peer_connections_with_log_files =
+      ActivePeerConnectionsWithLogFiles();
+  PeerConnectionKey pc_key = {render_process_id_, peer_connection_local_id};
+  DCHECK(std::find(active_peer_connections_with_log_files.begin(),
+                   active_peer_connections_with_log_files.end(),
+                   pc_key) == active_peer_connections_with_log_files.end());
+  active_peer_connections_with_log_files.push_back(pc_key);
+}
+
+bool WebRTCEventLogHost::RtcEventLogRemoved(int peer_connection_local_id) {
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
+  auto& active_peer_connections_with_log_files =
+      ActivePeerConnectionsWithLogFiles();
+  PeerConnectionKey pc_key = {render_process_id_, peer_connection_local_id};
+  const auto it =
+      std::find(active_peer_connections_with_log_files.begin(),
+                active_peer_connections_with_log_files.end(), pc_key);
+  if (it != active_peer_connections_with_log_files.end()) {
+    active_peer_connections_with_log_files.erase(it);
+    return true;
+  }
+  return false;
 }
 
 }  // namespace content
