7516a4bacef5337e26974c3147ebda14a687131a
John Abd-El-Malek <jam@chromium.org>
2017-11-07 21:15:34 +0000

    If --disable-in-process-stack-traces is specified don't print stack trace on browser tests error.
    This avoids noise when one is running tests known to fail.
    
    Change-Id: I69b33acff1c463cb55f728c74b9f239e908dd51e
    Reviewed-on: https://chromium-review.googlesource.com/757496
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514594}

--- content/public/test/browser_test_base.cc
+++ content/public/test/browser_test_base.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/sync_call_restrictions.h"
 #include "net/dns/mock_host_resolver.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
+#include "services/service_manager/embedder/switches.h"
 #include "services/service_manager/public/cpp/connector.h"
 #include "ui/base/platform_window_defaults.h"
 #include "ui/base/test/material_design_controller_test_api.h"
@@ -75,7 +76,9 @@ namespace {
 int g_browser_process_pid;
 
 void DumpStackTraceSignalHandler(int signal) {
-  if (g_browser_process_pid == base::GetCurrentProcId()) {
+  if (!base::CommandLine::ForCurrentProcess()->HasSwitch(
+          service_manager::switches::kDisableInProcessStackTraces) &&
+      g_browser_process_pid == base::GetCurrentProcId()) {
     std::string message("BrowserTestBase received signal: ");
     message += strsignal(signal);
     message += ". Backtrace:\n";
