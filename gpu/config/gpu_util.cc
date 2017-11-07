cb7df1c373b20114a9b70d03f33322408c752653
Antoine Labour <piman@chromium.org>
2017-11-07 00:04:04 +0000

    Use ExtensionSet in gpu_util.cc
    Shares a tiny bit of code
    
    Bug: None
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I9c8389b11904931b353b12c2cd3d5ca120e8ecf2
    Reviewed-on: https://chromium-review.googlesource.com/747806
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514304}

--- gpu/config/gpu_util.cc
+++ gpu/config/gpu_util.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_piece.h"
 #include "base/strings/string_split.h"
-#include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/sys_info.h"
 #include "gpu/config/gpu_blacklist.h"
@@ -25,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/config/gpu_finch_features.h"
 #include "gpu/config/gpu_info_collector.h"
 #include "gpu/config/gpu_switches.h"
+#include "ui/gl/extension_set.h"
 #include "ui/gl/gl_switches.h"
 
 namespace gpu {
@@ -329,7 +329,7 @@ GpuFeatureInfo ComputeGpuFeatureInfo(const GPUInfo& gpu_info,
       GetAcceleratedVideoDecodeFeatureStatus(
           blacklisted_features, use_swift_shader, use_swift_shader_for_webgl);
 
-  std::set<base::StringPiece> all_disabled_extensions;
+  gl::ExtensionSet all_disabled_extensions;
   std::string disabled_gl_extensions_value =
       command_line->GetSwitchValueASCII(switches::kDisableGLExtensions);
   if (!disabled_gl_extensions_value.empty()) {
@@ -363,9 +363,8 @@ GpuFeatureInfo ComputeGpuFeatureInfo(const GPUInfo& gpu_info,
       enabled_driver_bug_workarounds.end());
 
   if (all_disabled_extensions.size()) {
-    std::vector<base::StringPiece> v(all_disabled_extensions.begin(),
-                                     all_disabled_extensions.end());
-    gpu_feature_info.disabled_extensions = base::JoinString(v, " ");
+    gpu_feature_info.disabled_extensions =
+        gl::MakeExtensionString(all_disabled_extensions);
   }
 
   AdjustGpuFeatureStatusToWorkarounds(&gpu_feature_info);
