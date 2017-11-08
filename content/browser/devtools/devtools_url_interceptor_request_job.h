3679df8ff828a2be0082c2585eaa110df0998c0a
Andrey Kosyakov <caseq@chromium.org>
2017-11-08 01:04:13 +0000

    DevTools: extract a structure for intercepted request info sent to front-end
    This reduces all the parameters request interceptor sends to front-end to
    a single structure, which is then sent via a method of NetworkHandler.
    
    Bug: 758136
    Change-Id: I896d8467295d8130bfa9ab7b10fd47bdd096afc3
    Reviewed-on: https://chromium-review.googlesource.com/752199
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Reviewed-by: Alex Clarke <alexclarke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514691}

--- content/browser/devtools/devtools_url_interceptor_request_job.h
+++ content/browser/devtools/devtools_url_interceptor_request_job.h
@@ -72,6 +72,8 @@ class DevToolsURLInterceptorRequestJob : public net::URLRequestJob {
   const base::UnguessableToken& target_id() const { return target_id_; }
 
  private:
+  std::unique_ptr<InterceptedRequestInfo> BuildRequestInfo();
+
   class SubRequest;
   class MockResponseDetails;
 
--- content/browser/devtools/devtools_url_request_interceptor.cc
+++ content/browser/devtools/devtools_url_request_interceptor.cc
@@ -17,6 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
+InterceptedRequestInfo::InterceptedRequestInfo() = default;
+InterceptedRequestInfo::~InterceptedRequestInfo() = default;
+
 // static
 bool DevToolsURLRequestInterceptor::IsNavigationRequest(
     ResourceType resource_type) {
