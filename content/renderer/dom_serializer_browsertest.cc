e12b5ca62644b7654551ecd09f1c66fc90de6f4f
Xiaocheng Hu <xiaochengh@chromium.org>
2017-11-08 00:35:55 +0000

    Use a simplified version of Node.innerText for gtest text dump
    This patch changes WebFrameContentDumper to use a simplified version
    of Node.innerText instead of TextIterator for text dumping, so that
    Blink gtests no longer uses TextIterator for text dumping, so that we
    can avoid massive rebaselining when changing behavior of TextIterator.
    
    A follow-up patch will change layout tests to use WebFrameContentDumper
    for text dump, so that layout test text dumps are TextIterator-free.
    
    Bug: 781434
    Change-Id: If5bce13d0fa2c8685f2eddf146c7be1250845e28
    Reviewed-on: https://chromium-review.googlesource.com/756365
    Reviewed-by: Dimitri Glazkov <dglazkov@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514671}

--- content/renderer/dom_serializer_browsertest.cc
+++ content/renderer/dom_serializer_browsertest.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/web/WebDocument.h"
 #include "third_party/WebKit/public/web/WebElement.h"
 #include "third_party/WebKit/public/web/WebElementCollection.h"
+#include "third_party/WebKit/public/web/WebFrameContentDumper.h"
 #include "third_party/WebKit/public/web/WebFrameSerializer.h"
 #include "third_party/WebKit/public/web/WebFrameSerializerClient.h"
 #include "third_party/WebKit/public/web/WebLocalFrame.h"
@@ -450,7 +451,8 @@ class DomSerializerTests : public ContentBrowserTest,
       '%', 0x2285, 0x00b9, '\'', 0
     };
     WebString value = body_element.GetAttribute("title");
-    WebString content = doc.ContentAsTextForTesting();
+    WebString content = blink::WebFrameContentDumper::DumpWebViewAsText(
+        web_frame->View(), 1024);
     ASSERT_TRUE(base::UTF16ToWide(value.Utf16()) == parsed_value);
     ASSERT_TRUE(base::UTF16ToWide(content.Utf16()) == parsed_value);
 
