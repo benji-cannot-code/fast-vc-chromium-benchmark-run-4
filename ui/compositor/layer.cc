d8772c3bb88c1565f638ccd4846d9e9653f1fcf3
Francois Doray <fdoray@chromium.org>
2017-11-08 21:04:27 +0000

    Remove unused closure variable from ui::Layer::SetBoundsFromAnimation().
    Bug: 
    Change-Id: Ifcda2072622931ba0d3932091e2a4a7ac56cf9e6
    Reviewed-on: https://chromium-review.googlesource.com/758563
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514938}

--- ui/compositor/layer.cc
+++ ui/compositor/layer.cc
@@ -1130,7 +1130,6 @@ void Layer::SetBoundsFromAnimation(const gfx::Rect& bounds,
   if (bounds == bounds_)
     return;
 
-  base::Closure closure;
   const gfx::Rect old_bounds = bounds_;
   bounds_ = bounds;
 
