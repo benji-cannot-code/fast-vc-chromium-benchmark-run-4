cbbe1a003d50b6961318c09cd62fbaac4146e1dd
Zijie He <zijiehe@chromium.org>
2017-11-08 02:20:07 +0000

    [Chromoting] Avoid sending inconsistent codec with the video stream
    Unlike regular scenarios of WebRTC, we cannot decide the encoder before the
    first frame arrives. So we cannot use the codec from WebRTC, instead encoder
    itself should tell us the codec.
    
    Bug: chromium:769391
    Change-Id: Ibea685b79c059916e9ca3e6e94127bf57b7ab70e
    Reviewed-on: https://chromium-review.googlesource.com/752554
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Commit-Queue: Zijie He <zijiehe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514711}

--- remoting/codec/webrtc_video_encoder.h
+++ remoting/codec/webrtc_video_encoder.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/time/time.h"
+#include "third_party/webrtc/common_types.h"
 #include "third_party/webrtc/modules/desktop_capture/desktop_geometry.h"
 
 namespace webrtc {
@@ -51,6 +52,7 @@ class WebrtcVideoEncoder {
     std::string data;
     bool key_frame;
     int quantizer;
+    webrtc::VideoCodecType codec;
   };
 
   enum class EncodeResult {
