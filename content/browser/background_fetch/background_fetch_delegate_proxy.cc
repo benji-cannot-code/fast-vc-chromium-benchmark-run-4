9f1aeee375a75504fac911b7d4ebf08c4012aa10
Dan Elphick <delphick@chromium.org>
2017-11-08 10:26:26 +0000

    Simplify BackgroundFetchDelegateProxy::Controller
    Remove redundant guid parameter from the
    BackgroundFetchDelegateProxy::Controller interface since it already
    exists in the BackgroundFetchRequestInfo object.
    
    Also tighten up GUID check in BackgroundFetchServiceImpl to ensure that
    the GUID is a valid output string.
    
    R=peter@chromium.org
    
    Bug: 757760
    Change-Id: I8e07f9bfe2732e3ee0631f5a617dfd0084571e63
    Reviewed-on: https://chromium-review.googlesource.com/757142
    Commit-Queue: Dan Elphick <delphick@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514792}

--- content/browser/background_fetch/background_fetch_delegate_proxy.cc
+++ content/browser/background_fetch/background_fetch_delegate_proxy.cc
@@ -316,11 +316,12 @@ void BackgroundFetchDelegateProxy::DidStartRequest(
 
   const scoped_refptr<BackgroundFetchRequestInfo>& request_info =
       job_details.current_request_map[guid];
+  DCHECK_EQ(guid, request_info->download_guid());
 
   request_info->PopulateWithResponse(std::move(response));
 
   if (job_details.controller)
-    job_details.controller->DidStartRequest(request_info, guid);
+    job_details.controller->DidStartRequest(request_info);
 }
 
 void BackgroundFetchDelegateProxy::OnDownloadUpdated(
@@ -339,8 +340,10 @@ void BackgroundFetchDelegateProxy::OnDownloadUpdated(
 
   // TODO(peter): Should we update the |request_info| with the progress?
   if (job_details.controller) {
-    job_details.controller->DidUpdateRequest(
-        job_details.current_request_map[guid], guid, bytes_downloaded);
+    const scoped_refptr<BackgroundFetchRequestInfo>& request_info =
+        job_details.current_request_map[guid];
+    DCHECK_EQ(guid, request_info->download_guid());
+    job_details.controller->DidUpdateRequest(request_info, bytes_downloaded);
   }
 }
 
@@ -360,10 +363,11 @@ void BackgroundFetchDelegateProxy::OnDownloadComplete(
 
   const scoped_refptr<BackgroundFetchRequestInfo>& request_info =
       job_details.current_request_map[guid];
+  DCHECK_EQ(guid, request_info->download_guid());
   request_info->SetResult(std::move(result));
 
   if (job_details.controller)
-    job_details.controller->DidCompleteRequest(request_info, guid);
+    job_details.controller->DidCompleteRequest(request_info);
 }
 
 }  // namespace content
