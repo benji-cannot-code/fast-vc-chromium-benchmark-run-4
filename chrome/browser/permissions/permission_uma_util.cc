fd3e7f5c7aaa2e5be738f48ac2e68990986f20fd
Timothy Loh <timloh@chromium.org>
2017-11-08 00:45:10 +0000

    Remove histograms for grouped permission bubbles
    In M61 we restricted permissions to only group Mic+Camera, which made
    the histograms for grouped permission bubbles (which permissions were
    grouped together, how many were grouped) redundant. The same data can
    be readily obtained from Permissions.Prompt.(Accepted/Denied/Shown).
    
    Bug: 728483, 638076
    Change-Id: Iab49387d0b0d86e3e2a269cfa94f9072f1db95bb
    Reviewed-on: https://chromium-review.googlesource.com/756633
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Raymes Khoury <raymes@chromium.org>
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514680}

--- chrome/browser/permissions/permission_uma_util.cc
+++ chrome/browser/permissions/permission_uma_util.cc
@@ -251,14 +251,6 @@ const char PermissionUmaUtil::kPermissionsPromptDeniedGesture[] =
     "Permissions.Prompt.Denied.Gesture";
 const char PermissionUmaUtil::kPermissionsPromptDeniedNoGesture[] =
     "Permissions.Prompt.Denied.NoGesture";
-const char PermissionUmaUtil::kPermissionsPromptRequestsPerPrompt[] =
-    "Permissions.Prompt.RequestsPerPrompt";
-const char PermissionUmaUtil::kPermissionsPromptMergedBubbleTypes[] =
-    "Permissions.Prompt.MergedBubbleTypes";
-const char PermissionUmaUtil::kPermissionsPromptMergedBubbleAccepted[] =
-    "Permissions.Prompt.MergedBubbleAccepted";
-const char PermissionUmaUtil::kPermissionsPromptMergedBubbleDenied[] =
-    "Permissions.Prompt.MergedBubbleDenied";
 const char
     PermissionUmaUtil::kPermissionsPromptAcceptedPriorDismissCountPrefix[] =
         "Permissions.Prompt.Accepted.PriorDismissCount.";
@@ -440,16 +432,6 @@ void PermissionUmaUtil::PermissionPromptShown(
   }
 
   RecordPermissionPromptShown(permission_prompt_type, permission_gesture_type);
-
-  UMA_HISTOGRAM_ENUMERATION(kPermissionsPromptRequestsPerPrompt,
-                            requests.size(), 10);
-
-  if (requests.size() > 1) {
-    for (const auto* request : requests) {
-      PERMISSION_BUBBLE_TYPE_UMA(kPermissionsPromptMergedBubbleTypes,
-                                 request->GetPermissionRequestType());
-    }
-  }
 }
 
 void PermissionUmaUtil::PermissionPromptResolved(
@@ -780,16 +762,6 @@ void PermissionUmaUtil::RecordPromptDecided(
   if (requests.size() > 1) {
     permission_prompt_type = PermissionRequestType::MULTIPLE;
     permission_gesture_type = PermissionRequestGestureType::UNKNOWN;
-    for (size_t i = 0; i < requests.size(); ++i) {
-      const auto* request = requests[i];
-      if (accepted) {
-        PERMISSION_BUBBLE_TYPE_UMA(kPermissionsPromptMergedBubbleAccepted,
-                                   request->GetPermissionRequestType());
-      } else {
-        PERMISSION_BUBBLE_TYPE_UMA(kPermissionsPromptMergedBubbleDenied,
-                                   request->GetPermissionRequestType());
-      }
-    }
   }
 
   if (accepted) {
