ff5f19d957f2fb8563f59dbbb1c66d8d306047fd
Gabriel Charette <gab@chromium.org>
2017-11-08 23:35:53 +0000

    Migrate MessageLoop::ScopedNestableTaskAllower to RunLoop::Type::kNestableTasksAllowed in /ui/base/x
    This pass focuses on ML::SNTA in same scope as RunLoop variable
    Scripted via https://bugs.chromium.org/p/chromium/issues/detail?id=781352#c1
    
    The script should have replaced a MessageLoop::ScopedNestableTaskAllower
    in the same scope as a RunLoop with a RunLoop of
    Type::kNestableTasksAllowed. If there was a comment above the
    MessageLoop::ScopedNestableTaskAllower, it should have been moved
    alongside the augmented RunLoop.
    
    Please CQ if LGTY
    
    This CL was uploaded by git cl split.
    
    R=sadrul@chromium.org
    
    Bug: 781352
    Change-Id: I6516d971c26c54ca5dff3c64361d5ba692764896
    Reviewed-on: https://chromium-review.googlesource.com/758927
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515004}

--- ui/base/x/selection_requestor.cc
+++ ui/base/x/selection_requestor.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <X11/Xlib.h>
 
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
 #include "ui/base/x/selection_utils.h"
 #include "ui/base/x/x11_util.h"
@@ -268,9 +267,7 @@ void SelectionRequestor::BlockTillSelectionNotifyForRequest(Request* request) {
                          &SelectionRequestor::AbortStaleRequests);
     }
 
-    base::MessageLoop::ScopedNestableTaskAllower allow_nested(
-        base::MessageLoopForUI::current());
-    base::RunLoop run_loop;
+    base::RunLoop run_loop(base::RunLoop::Type::kNestableTasksAllowed);
     request->quit_closure = run_loop.QuitClosure();
     run_loop.Run();
 
