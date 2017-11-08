6c7b63d64b6640dd3c1770091acf0fd71484f7f0
Scott Violet <sky@chromium.org>
2017-11-08 00:55:02 +0000

    Revert "cros: Replace shelf STATUS_ACTIVE with STATUS_RUNNING"
    This reverts commit c11e7ba721840d965f6ae24e7f72ab260d166f5e.
    
    Reason for revert: Appears to have caused mash_browsertests to fail. https://chromium-swarm.appspot.com/task?id=39b1d0d34a750d10&refresh=10&show_raw=1&wide_logs=true output is:
    
    ../../chrome/browser/ui/ash/shelf_browsertest.cc:76: Failure
    Value of: has_overlapping_window
      Actual: false
    Expected: true
    [500:500:1107/162023.401861:INFO:chrome_cryptauth_service.cc(230)] Refresh token not yet available; waiting before starting CryptAuth managers.
    [534:534:1107/162023.407440:ERROR:shell_delegate_mus.cc(75)] Not implemented reached in virtual void ash::ShellDelegateMus::PreShutdown()
    [  FAILED  ] ShelfBrowserTest.StatusBubble, where TypeParam =  and GetParam() =  (962 ms)
    
    
    Original change's description:
    > cros: Replace shelf STATUS_ACTIVE with STATUS_RUNNING
    > 
    > Before the material design shelf we used to show light bars underneath
    > the shelf icons for running apps. An app with an active window had a
    > brighter bar. With the MD redesign we show dots and there is no
    > difference between running and active. Tracking the active state
    > complicates the go/mustash refactor, especially in tests.
    > 
    > * Eliminate the active state, which simplifies both code and tests
    > * Remove ShelfButton::STATE_ACTIVE and STATE_FOCUSED, neither of
    >   which are used anymore
    > * Eliminate browser tests that existed mostly to track the active
    >   state
    > 
    > Bug: 678687
    > Test: ash_unittests, browser_tests
    > Change-Id: I3dc1b045d44ea1ecc5a311afa4d877d0e908e35a
    > Reviewed-on: https://chromium-review.googlesource.com/757169
    > Reviewed-by: Tom Sepez <tsepez@chromium.org>
    > Reviewed-by: Michael Wasserman <msw@chromium.org>
    > Commit-Queue: James Cook <jamescook@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#514623}
    
    TBR=jamescook@chromium.org,msw@chromium.org,tsepez@chromium.org
    
    Change-Id: Ibf53d35870f583c86519b6b18b2036d64c82c1f7
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 678687
    Reviewed-on: https://chromium-review.googlesource.com/757937
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514685}

--- ash/public/cpp/shelf_types.h
+++ ash/public/cpp/shelf_types.h
@@ -130,6 +130,8 @@ enum ShelfItemStatus {
   STATUS_CLOSED,
   // A shelf item that has live instance.
   STATUS_RUNNING,
+  // An active shelf item that has focus.
+  STATUS_ACTIVE,
   // A shelf item that needs user's attention.
   STATUS_ATTENTION,
 };
