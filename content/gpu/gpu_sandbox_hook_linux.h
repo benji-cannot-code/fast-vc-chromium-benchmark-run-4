47dcf35efaa45feeda95b60d58e5adbfe6eee435
Tom Sepez <tsepez@chromium.org>
2017-11-08 19:39:58 +0000

    Consolidate GPU pre-sandbox hooks.
    Avoid having three very-similar functions.
    Add some missing consts along the way.
    
    Change-Id: I203e874cc20a2056bf74005cbc7f4a625c1094fe
    Reviewed-on: https://chromium-review.googlesource.com/757767
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514907}

--- content/gpu/gpu_sandbox_hook_linux.h
+++ content/gpu/gpu_sandbox_hook_linux.h
@@ -10,8 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-service_manager::SandboxSeccompBPF::PreSandboxHook GetGpuProcessPreSandboxHook(
-    bool use_amd_specific_policies);
+bool GpuProcessPreSandboxHook(
+    service_manager::BPFBasePolicy* policy,
+    service_manager::SandboxSeccompBPF::Options options);
 
 }  // namespace content
 
