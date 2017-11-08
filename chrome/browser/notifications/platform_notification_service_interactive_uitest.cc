933d70aa27cec39b11fe12f9224bea0a2fb5ed0d
Anita Woodruff <awdf@chromium.org>
2017-11-08 15:43:41 +0000

    [Notification Tests] Test UMA sent in interactive ui tests
    - Previously we did not test the UMA sent from
    PlatformNotificationServiceImpl.
    
    - Now all but one stat is tested (It's hard to test
    Notification.Persistent.ClickedWithoutPermission from a UI test).
    
    - This paves the way for refactoring the UMA-logging into a separate
    class.
    
    Bug: 780534
    Change-Id: I975679688f57608e22c107ab6f7944e7fc09ed3e
    Reviewed-on: https://chromium-review.googlesource.com/758643
    Commit-Queue: Anita Woodruff <awdf@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514841}

--- chrome/browser/notifications/platform_notification_service_interactive_uitest.cc
+++ chrome/browser/notifications/platform_notification_service_interactive_uitest.cc
@@ -14,7 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_piece.h"
 #include "base/strings/string_split.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
+#include "base/test/user_action_tester.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
 #include "chrome/browser/engagement/site_engagement_score.h"
@@ -186,6 +188,8 @@ class PlatformNotificationServiceBrowserTest : public InProcessBrowserTest {
 
   const base::FilePath server_root_;
   std::unique_ptr<NotificationDisplayServiceTester> display_service_tester_;
+  base::HistogramTester histogram_tester_;
+  base::UserActionTester user_action_tester_;
 
  private:
   std::unique_ptr<net::EmbeddedTestServer> https_server_;
@@ -212,6 +216,7 @@ IN_PROC_BROWSER_TEST_F(PlatformNotificationServiceBrowserTest,
 
 IN_PROC_BROWSER_TEST_F(PlatformNotificationServiceBrowserTest,
                        DisplayPersistentNotificationWithPermission) {
+  base::UserActionTester user_action_tester;
   RequestAndAcceptPermission();
 
   // Expect 5 engagement for notification permission and 0.5 for the navigation.
@@ -231,8 +236,9 @@ IN_PROC_BROWSER_TEST_F(PlatformNotificationServiceBrowserTest,
       KeepAliveOrigin::PENDING_NOTIFICATION_CLICK_EVENT));
 #endif
 
-  // We expect +1 engagement for the notification interaction.
   notifications[0].delegate()->Click();
+
+  // We expect +1 engagement for the notification interaction.
   EXPECT_DOUBLE_EQ(6.5, GetEngagementScore(GetLastCommittedURL()));
 
   // Clicking on the notification should not automatically close it.
@@ -257,6 +263,15 @@ IN_PROC_BROWSER_TEST_F(PlatformNotificationServiceBrowserTest,
 
   notifications = GetDisplayedNotifications(true /* is_persistent */);
   ASSERT_EQ(1u, notifications.size());
+
+  // Check UMA was recorded.
+  EXPECT_EQ(
+      1, user_action_tester_.GetActionCount("Notifications.Persistent.Shown"));
+  EXPECT_EQ(1, user_action_tester_.GetActionCount(
+                   "Notifications.Persistent.Clicked"));
+  histogram_tester_.ExpectUniqueSample(
+      "Notifications.PersistentWebNotificationClickResult",
+      0 /* SERVICE_WORKER_OK */, 1);
 }
 
 IN_PROC_BROWSER_TEST_F(PlatformNotificationServiceBrowserTest,
@@ -421,6 +436,7 @@ IN_PROC_BROWSER_TEST_F(PlatformNotificationServiceBrowserTest,
 
 IN_PROC_BROWSER_TEST_F(PlatformNotificationServiceBrowserTest,
                        UserClosesPersistentNotification) {
+  base::UserActionTester user_action_tester;
   ASSERT_NO_FATAL_FAILURE(GrantNotificationPermissionForTest());
 
   // Expect 5 engagement for notification permission and 0.5 for the navigation.
@@ -442,6 +458,12 @@ IN_PROC_BROWSER_TEST_F(PlatformNotificationServiceBrowserTest,
 
   ASSERT_TRUE(RunScript("GetMessageFromWorker()", &script_result));
   EXPECT_EQ("closing notification: close_test", script_result);
+
+  EXPECT_EQ(1, user_action_tester_.GetActionCount(
+                   "Notifications.Persistent.ClosedByUser"));
+  histogram_tester_.ExpectUniqueSample(
+      "Notifications.PersistentWebNotificationCloseResult",
+      0 /* SERVICE_WORKER_OK */, 1);
 }
 
 IN_PROC_BROWSER_TEST_F(PlatformNotificationServiceBrowserTest,
@@ -572,6 +594,7 @@ IN_PROC_BROWSER_TEST_F(PlatformNotificationServiceBrowserTest,
 
 IN_PROC_BROWSER_TEST_F(PlatformNotificationServiceBrowserTest,
                        DisplayPersistentNotificationWithActionButtons) {
+  base::UserActionTester user_action_tester;
   ASSERT_NO_FATAL_FAILURE(GrantNotificationPermissionForTest());
 
   // Expect 5 engagement for notification permission and 0.5 for the navigation.
@@ -592,18 +615,33 @@ IN_PROC_BROWSER_TEST_F(PlatformNotificationServiceBrowserTest,
   EXPECT_EQ("actionTitle2", base::UTF16ToUTF8(notification.buttons()[1].title));
 
   notification.delegate()->ButtonClick(0);
+
   ASSERT_TRUE(RunScript("GetMessageFromWorker()", &script_result));
   EXPECT_EQ("action_button_click actionId1", script_result);
   EXPECT_DOUBLE_EQ(6.5, GetEngagementScore(GetLastCommittedURL()));
 
+  EXPECT_EQ(1, user_action_tester_.GetActionCount(
+                   "Notifications.Persistent.ClickedActionButton"));
+  histogram_tester_.ExpectUniqueSample(
+      "Notifications.PersistentWebNotificationClickResult",
+      0 /* SERVICE_WORKER_OK */, 1);
+
   notification.delegate()->ButtonClick(1);
+
   ASSERT_TRUE(RunScript("GetMessageFromWorker()", &script_result));
   EXPECT_EQ("action_button_click actionId2", script_result);
   EXPECT_DOUBLE_EQ(7.5, GetEngagementScore(GetLastCommittedURL()));
+
+  EXPECT_EQ(2, user_action_tester_.GetActionCount(
+                   "Notifications.Persistent.ClickedActionButton"));
+  histogram_tester_.ExpectUniqueSample(
+      "Notifications.PersistentWebNotificationClickResult",
+      0 /* SERVICE_WORKER_OK */, 2);
 }
 
 IN_PROC_BROWSER_TEST_F(PlatformNotificationServiceBrowserTest,
                        DisplayPersistentNotificationWithReplyButton) {
+  base::UserActionTester user_action_tester;
   ASSERT_NO_FATAL_FAILURE(GrantNotificationPermissionForTest());
 
   // Expect 5 engagement for notification permission and 0.5 for the navigation.
@@ -623,9 +661,16 @@ IN_PROC_BROWSER_TEST_F(PlatformNotificationServiceBrowserTest,
   EXPECT_EQ("actionTitle1", base::UTF16ToUTF8(notification.buttons()[0].title));
 
   notification.delegate()->ButtonClickWithReply(0, base::ASCIIToUTF16("hello"));
+
   ASSERT_TRUE(RunScript("GetMessageFromWorker()", &script_result));
   EXPECT_EQ("action_button_click actionId1 hello", script_result);
   EXPECT_DOUBLE_EQ(6.5, GetEngagementScore(GetLastCommittedURL()));
+
+  EXPECT_EQ(1, user_action_tester_.GetActionCount(
+                   "Notifications.Persistent.ClickedActionButton"));
+  histogram_tester_.ExpectUniqueSample(
+      "Notifications.PersistentWebNotificationClickResult",
+      0 /* SERVICE_WORKER_OK */, 1);
 }
 
 IN_PROC_BROWSER_TEST_F(PlatformNotificationServiceBrowserTest,
