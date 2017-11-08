425e627567f423040b6c68ec0600722b93278fcb
Justin Novosad <junov@chromium.org>
2017-11-08 23:26:00 +0000

    Reland: Refactor of Canvas2DLayerBridge resource model
    Transition to polymorphic RAII model. The purpose of this change
    is to improve the maintainability and extendability of the code.
    The GpuMemoryBuffer code is also getting a drive-by optimization
    that will eliminate unnecessary gpu commandbuffer flushing and
    verified sync tokens, which result in synchronous IPCs.
    
    Original code review:
    https://chromium-review.googlesource.com/c/chromium/src/+/728340
    
    Fixed bad include of CanvasResourceProvider.cpp instead of .h
    
    BUG=773466,768049,735630
    TBR=fserb@chromium.org,xlai@chromium.org,mostynb@vewd.com,sky@chromium.org
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I143b7dc78d50ded8f1541868edecda97946e6292
    Reviewed-on: https://chromium-review.googlesource.com/758562
    Commit-Queue: Justin Novosad <junov@chromium.org>
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515001}

--- third_party/WebKit/Source/platform/graphics/ImageBufferSurface.cpp
+++ third_party/WebKit/Source/platform/graphics/ImageBufferSurface.cpp
@@ -83,8 +83,4 @@ void ImageBufferSurface::Draw(GraphicsContext& context,
                     op);
 }
 
-void ImageBufferSurface::Flush(FlushReason) {
-  Canvas()->flush();
-}
-
 }  // namespace blink
