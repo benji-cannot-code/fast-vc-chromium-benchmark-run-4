3fbbc86a90908d6c3dbb3d7a932a4b6425884464
Denis Kuznetsov <antrim@google.com>
2017-11-08 15:42:33 +0000

    Policy to disable obsolete Chrome versions on ChromeOS devices.
    Force users to sign out if policy requirements are not met.
    
    Bug: 774077
    Change-Id: Icd167afe4266835faba039c064ec6bafda0ba06a
    Reviewed-on: https://chromium-review.googlesource.com/733920
    Commit-Queue: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514840}

--- chrome/browser/chromeos/login/users/chrome_user_manager_impl.cc
+++ chrome/browser/chromeos/login/users/chrome_user_manager_impl.cc
@@ -173,6 +173,12 @@ void SetPublicAccountDelegates() {
       new extensions::ActiveTabPermissionGranterDelegateChromeOS);
 }
 
+policy::MinimumVersionPolicyHandler* GetMinimumVersionPolicyHandler() {
+  return g_browser_process->platform_part()
+      ->browser_policy_connector_chromeos()
+      ->GetMinimumVersionPolicyHandler();
+}
+
 }  // namespace
 
 // static
@@ -250,6 +256,11 @@ ChromeUserManagerImpl::ChromeUserManagerImpl()
       g_browser_process->platform_part()
           ->browser_policy_connector_chromeos()
           ->GetDeviceLocalAccountPolicyService();
+
+  if (GetMinimumVersionPolicyHandler()) {
+    GetMinimumVersionPolicyHandler()->AddObserver(this);
+  }
+
   if (!device_local_account_policy_service) {
     return;
   }
@@ -275,6 +286,10 @@ ChromeUserManagerImpl::~ChromeUserManagerImpl() {}
 void ChromeUserManagerImpl::Shutdown() {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
   ChromeUserManager::Shutdown();
+
+  if (GetMinimumVersionPolicyHandler()) {
+    GetMinimumVersionPolicyHandler()->RemoveObserver(this);
+  }
 
   local_accounts_subscription_.reset();
 
@@ -1187,6 +1202,17 @@ bool ChromeUserManagerImpl::IsGaiaUserAllowed(
                                      nullptr);
 }
 
+void ChromeUserManagerImpl::OnMinimumVersionStateChanged() {
+  NotifyUsersSignInConstraintsChanged();
+}
+
+bool ChromeUserManagerImpl::MinVersionConstraintsSatisfied() const {
+  return g_browser_process->platform_part()
+      ->browser_policy_connector_chromeos()
+      ->GetMinimumVersionPolicyHandler()
+      ->RequirementsAreSatisfied();
+}
+
 bool ChromeUserManagerImpl::IsUserAllowed(
     const user_manager::User& user) const {
   DCHECK(user.GetType() == user_manager::USER_TYPE_REGULAR ||
@@ -1201,6 +1227,9 @@ bool ChromeUserManagerImpl::IsUserAllowed(
       !AreSupervisedUsersAllowed())
     return false;
   if (user.HasGaiaAccount() && !IsGaiaUserAllowed(user))
+    return false;
+  if (!MinVersionConstraintsSatisfied() &&
+      user.GetType() != user_manager::USER_TYPE_GUEST)
     return false;
   return true;
 }
--- chrome/browser/chromeos/preferences.cc
+++ chrome/browser/chromeos/preferences.cc
@@ -132,6 +132,7 @@ void Preferences::RegisterPrefs(PrefRegistrySimple* registry) {
 
   registry->RegisterDictionaryPref(ash::prefs::kWallpaperColors,
                                    PrefRegistry::PUBLIC);
+  registry->RegisterStringPref(prefs::kMinimumAllowedChromeVersion, "");
 }
 
 // static
