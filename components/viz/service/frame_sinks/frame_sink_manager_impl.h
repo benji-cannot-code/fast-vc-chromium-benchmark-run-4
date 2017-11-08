9703e30d6da019f86fdea971b55a0276a0285bc9
Yuri Wiitala <miu@chromium.org>
2017-11-08 23:46:21 +0000

    Add CapturableFrameSink interface to support tab capture in VIZ.
    This is one of several patches that will introduce a new tab/desktop
    video capture implementation that runs in the VIZ component.
    
    CapturableFrameSink and CapturableFrameSink::Client interfaces are added
    so that the new capture pipeline can "hook into" events of interest:
    BeginFrame events, to get display timing data; and aggregated damage
    events spawned from the surface aggregation process. From these events,
    the rest of the implementation will make decisions as to whether to
    issue a CopyOutputRequest on a Surface.
    
    Bug: 754872
    Change-Id: Iec9e7f38b994664efb29d1bc49ceb46247aacc52
    Reviewed-on: https://chromium-review.googlesource.com/757793
    Reviewed-by: Fady Samuel <fsamuel@chromium.org>
    Commit-Queue: Yuri Wiitala <miu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515008}

--- components/viz/service/frame_sinks/frame_sink_manager_impl.h
+++ components/viz/service/frame_sinks/frame_sink_manager_impl.h
@@ -37,6 +37,8 @@ class SurfaceSynchronizationTest;
 
 namespace viz {
 
+class CapturableFrameSink;
+class CompositorFrameSinkSupport;
 class DisplayProvider;
 class FrameSinkManagerClient;
 
@@ -161,6 +163,11 @@ class VIZ_SERVICE_EXPORT FrameSinkManagerImpl : public SurfaceObserver,
   void RecursivelyDetachBeginFrameSource(const FrameSinkId& frame_sink_id,
                                          BeginFrameSource* source);
 
+  // TODO(crbug.com/754872): To be used by FrameSinkVideoCapturerImpl in
+  // an upcoming change.
+  CapturableFrameSink* FindCapturableFrameSink(
+      const FrameSinkId& frame_sink_id);
+
   // Returns true if |child framesink| is or has |search_frame_sink_id| as a
   // child.
   bool ChildContains(const FrameSinkId& child_frame_sink_id,
@@ -200,9 +207,13 @@ class VIZ_SERVICE_EXPORT FrameSinkManagerImpl : public SurfaceObserver,
 
   HitTestManager hit_test_manager_;
 
-  std::unordered_map<FrameSinkId,
-                     std::unique_ptr<mojom::CompositorFrameSink>,
-                     FrameSinkIdHash>
+  struct SinkAndSupport {
+    SinkAndSupport();
+    ~SinkAndSupport();
+    std::unique_ptr<mojom::CompositorFrameSink> sink;
+    CompositorFrameSinkSupport* support;  // Owned by |sink|.
+  };
+  std::unordered_map<FrameSinkId, SinkAndSupport, FrameSinkIdHash>
       compositor_frame_sinks_;
 
   THREAD_CHECKER(thread_checker_);
