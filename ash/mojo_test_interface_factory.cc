0b3513d8eb5e20495d5488760a88fdd53bffb1fe
James Cook <jamescook@chromium.org>
2017-11-08 02:56:57 +0000

    Revert "cros: Adapt ShelfBrowserTest to work with --mash"
    This reverts commit 14b4134077b54e0278bb651e008fd069a6c539a7.
    
    Reason for revert: Causes flaky failures in mash_browser_tests. I can repro the failures locally. Sample from bots:
    
    https://chromium-swarm.appspot.com/task?id=39b1d0d34a750d10&refresh=10&show_raw=1&wide_logs=true output is:
        
        ../../chrome/browser/ui/ash/shelf_browsertest.cc:76: Failure
        Value of: has_overlapping_window
          Actual: false
        Expected: true
        [500:500:1107/162023.401861:INFO:chrome_cryptauth_service.cc(230)] Refresh token not yet available; waiting before starting CryptAuth managers.
        [534:534:1107/162023.407440:ERROR:shell_delegate_mus.cc(75)] Not implemented reached in virtual void ash::ShellDelegateMus::PreShutdown()
        [  FAILED  ] ShelfBrowserTest.StatusBubble, where TypeParam =  and GetParam() =  (962 ms)
    
    Original change's description:
    > cros: Adapt ShelfBrowserTest to work with --mash
    > 
    > * Add a mojo shelf test API
    > * Convert 2 tests to use it
    > 
    > Bug: 781925, 678687
    > Test: browser_tests --mash
    > Change-Id: If75111a9795bc93eefb69bc2d80401d1fb2b6d65
    > Reviewed-on: https://chromium-review.googlesource.com/755344
    > Reviewed-by: Tom Sepez <tsepez@chromium.org>
    > Reviewed-by: Michael Wasserman <msw@chromium.org>
    > Commit-Queue: James Cook <jamescook@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#514582}
    
    TBR=jamescook@chromium.org,msw@chromium.org,tsepez@chromium.org
    
    Change-Id: I6e4bea721b44cacb2cf53d9c8861e7d4889fb4de
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 781925, 678687
    Reviewed-on: https://chromium-review.googlesource.com/757792
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514722}

--- ash/mojo_test_interface_factory.cc
+++ ash/mojo_test_interface_factory.cc
@@ -7,9 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
-#include "ash/public/interfaces/shelf_test_api.mojom.h"
 #include "ash/public/interfaces/system_tray_test_api.mojom.h"
-#include "ash/shelf/shelf_test_api.h"
 #include "ash/system/tray/system_tray_test_api.h"
 #include "base/bind.h"
 #include "base/single_thread_task_runner.h"
@@ -18,12 +16,8 @@ namespace ash {
 namespace mojo_test_interface_factory {
 namespace {
 
-// These functions aren't strictly necessary, but exist to make threading and
-// arguments clearer.
-void BindShelfTestApiOnMainThread(mojom::ShelfTestApiRequest request) {
-  ShelfTestApi::BindRequest(std::move(request));
-}
-
+// This isn't strictly necessary, but exists to make threading and arguments
+// clearer.
 void BindSystemTrayTestApiOnMainThread(
     mojom::SystemTrayTestApiRequest request) {
   SystemTrayTestApi::BindRequest(std::move(request));
@@ -34,8 +28,6 @@ void BindSystemTrayTestApiOnMainThread(
 void RegisterInterfaces(
     service_manager::BinderRegistry* registry,
     scoped_refptr<base::SingleThreadTaskRunner> main_thread_task_runner) {
-  registry->AddInterface(base::Bind(&BindShelfTestApiOnMainThread),
-                         main_thread_task_runner);
   registry->AddInterface(base::Bind(&BindSystemTrayTestApiOnMainThread),
                          main_thread_task_runner);
 }
