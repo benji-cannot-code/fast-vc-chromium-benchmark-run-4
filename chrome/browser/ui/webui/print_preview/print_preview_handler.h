8217fdf81f32855b90ec6f67953136b06508461e
Lei Zhang <thestig@chromium.org>
2017-11-08 20:04:41 +0000

    Inline PrintPreviewHandler::GetPreviewDataAndTitle().
    Also explain why Cloud Print does not need the title.
    
    Change-Id: I7758f02ea16a9dc18da121f4c796311f3f426cb2
    Reviewed-on: https://chromium-review.googlesource.com/758317
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514918}

--- chrome/browser/ui/webui/print_preview/print_preview_handler.h
+++ chrome/browser/ui/webui/print_preview/print_preview_handler.h
@@ -265,9 +265,6 @@ class PrintPreviewHandler
   // Populates |settings| according to the current locale.
   void GetNumberFormatAndMeasurementSystem(base::DictionaryValue* settings);
 
-  bool GetPreviewDataAndTitle(scoped_refptr<base::RefCountedBytes>* data,
-                              base::string16* title) const;
-
   PdfPrinterHandler* GetPdfPrinterHandler();
 
   // Called when printers are detected.
