1b41b655dd6a0f462a2fe504a7c5a06496238400
Sunny Sachanandani <sunnyps@chromium.org>
2017-11-08 00:52:03 +0000

    gpu: DCHECK for verified flush in sync token mojo serializer.
    The IPC serializer does this to prevent unverified sync tokens from
    being sent to another process.
    
    R=piman,kylechar
    BUG=NONE
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I9458a6bb5981b2cf7d0cc74657ea7226e587d99e
    Reviewed-on: https://chromium-review.googlesource.com/755948
    Reviewed-by: kylechar <kylechar@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514681}

--- gpu/ipc/common/sync_token_struct_traits.h
+++ gpu/ipc/common/sync_token_struct_traits.h
@@ -14,6 +14,7 @@ namespace mojo {
 template <>
 struct StructTraits<gpu::mojom::SyncTokenDataView, gpu::SyncToken> {
   static bool verified_flush(const gpu::SyncToken& token) {
+    DCHECK(!token.HasData() || token.verified_flush());
     return token.verified_flush();
   }
 
@@ -41,8 +42,11 @@ struct StructTraits<gpu::mojom::SyncTokenDataView, gpu::SyncToken> {
         data.extra_data_field(),
         gpu::CommandBufferId::FromUnsafeValue(data.command_buffer_id()),
         data.release_count());
-    if (data.verified_flush())
+    if (out->HasData()) {
+      if (!data.verified_flush())
+        return false;
       out->SetVerifyFlush();
+    }
     return true;
   }
 };
