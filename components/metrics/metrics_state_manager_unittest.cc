7580d7f1cc65bcf412fda69a124f6095178debab
Mark Pearson <mpearson@chromium.org>
2017-11-08 01:45:49 +0000

    Make PreviousSession UMA Logs Use Previous Client Id
    Also make them report IsClonedInstall too, if the previous session
    was detected as being a cloned install.
    
    Do so by remembering the previous client id seen upon startup
    and remembering whether IsClonedInstall was reported in the last
    session and using those values when responding to
    ProvidePreviousSessionData().
    
    The change in histogram macros was to make this a stability-tagged
    histogram, otherwise even if we record it in
    ProvidePreviousSessionData(), it would not be emitted for the
    previous session.
    
    Bug: 754758
    Change-Id: I9901c4e4de7efe1bb26bdda053a6ee734461db1a
    Reviewed-on: https://chromium-review.googlesource.com/638841
    Commit-Queue: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514703}

--- components/metrics/metrics_state_manager_unittest.cc
+++ components/metrics/metrics_state_manager_unittest.cc
@@ -15,7 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/macros.h"
 #include "base/strings/string16.h"
+#include "base/test/histogram_tester.h"
 #include "components/metrics/client_info.h"
+#include "components/metrics/metrics_log.h"
 #include "components/metrics/metrics_pref_names.h"
 #include "components/metrics/metrics_service.h"
 #include "components/metrics/metrics_switches.h"
@@ -219,6 +221,7 @@ TEST_F(MetricsStateManagerTest, ResetMetricsIDs) {
     std::unique_ptr<MetricsStateManager> state_manager(CreateStateManager());
     state_manager->ForceClientIdCreation();
     EXPECT_EQ(kInitialClientId, state_manager->client_id());
+    EXPECT_FALSE(state_manager->metrics_ids_were_reset_);
   }
 
   // Set the reset pref to cause the IDs to be reset.
@@ -229,6 +232,8 @@ TEST_F(MetricsStateManagerTest, ResetMetricsIDs) {
     std::unique_ptr<MetricsStateManager> state_manager(CreateStateManager());
     state_manager->ForceClientIdCreation();
     EXPECT_NE(kInitialClientId, state_manager->client_id());
+    EXPECT_TRUE(state_manager->metrics_ids_were_reset_);
+    EXPECT_EQ(kInitialClientId, state_manager->previous_client_id_);
 
     state_manager->GetLowEntropySource();
 
@@ -379,6 +384,8 @@ TEST_F(MetricsStateManagerTest, ResetBackup) {
     // A brand new client id should have been generated.
     EXPECT_NE(std::string(), state_manager->client_id());
     EXPECT_NE(client_info.client_id, state_manager->client_id());
+    EXPECT_TRUE(state_manager->metrics_ids_were_reset_);
+    EXPECT_EQ(client_info.client_id, state_manager->previous_client_id_);
     EXPECT_TRUE(stored_client_info_backup_);
 
     // The installation date should not have been affected.
@@ -411,6 +418,49 @@ TEST_F(MetricsStateManagerTest, CheckProvider) {
   provider->ProvideSystemProfileMetrics(&system_profile);
   EXPECT_EQ(system_profile.install_date(), kInstallDateExpected);
   EXPECT_EQ(system_profile.uma_enabled_date(), kEnabledDateExpected);
+
+  base::HistogramTester histogram_tester;
+  ChromeUserMetricsExtension uma_proto;
+  provider->ProvidePreviousSessionData(&uma_proto);
+  // The client_id field in the proto should not be overwritten.
+  EXPECT_FALSE(uma_proto.has_client_id());
+  // Nothing should have been emitted to the cloned install histogram.
+  histogram_tester.ExpectTotalCount("UMA.IsClonedInstall", 0);
+}
+
+TEST_F(MetricsStateManagerTest, CheckProviderResetIds) {
+  int64_t kInstallDate = 1373051956;
+  int64_t kInstallDateExpected = 1373050800;  // Computed from kInstallDate.
+  int64_t kEnabledDate = 1373001211;
+  int64_t kEnabledDateExpected = 1373000400;  // Computed from kEnabledDate.
+
+  ClientInfo client_info;
+  client_info.client_id = "AAAAAAAA-BBBB-CCCC-DDDD-EEEEEEEEEEEE";
+  client_info.installation_date = kInstallDate;
+  client_info.reporting_enabled_date = kEnabledDate;
+
+  SetFakeClientInfoBackup(client_info);
+  SetClientInfoPrefs(client_info);
+
+  // Set the reset pref to cause the IDs to be reset.
+  prefs_.SetBoolean(prefs::kMetricsResetIds, true);
+  std::unique_ptr<MetricsStateManager> state_manager(CreateStateManager());
+  EXPECT_NE(client_info.client_id, state_manager->client_id());
+  EXPECT_TRUE(state_manager->metrics_ids_were_reset_);
+  EXPECT_EQ(client_info.client_id, state_manager->previous_client_id_);
+
+  std::unique_ptr<MetricsProvider> provider = state_manager->GetProvider();
+  SystemProfileProto system_profile;
+  provider->ProvideSystemProfileMetrics(&system_profile);
+  EXPECT_EQ(system_profile.install_date(), kInstallDateExpected);
+  EXPECT_EQ(system_profile.uma_enabled_date(), kEnabledDateExpected);
+
+  base::HistogramTester histogram_tester;
+  ChromeUserMetricsExtension uma_proto;
+  provider->ProvidePreviousSessionData(&uma_proto);
+  EXPECT_EQ(MetricsLog::Hash(state_manager->previous_client_id_),
+            uma_proto.client_id());
+  histogram_tester.ExpectUniqueSample("UMA.IsClonedInstall", 1, 1);
 }
 
 }  // namespace metrics
