1a6ef842774cc5f194b05dbab4d2463482175874
Mehran Mahmoudi <mahmoudi@chromium.org>
2017-11-08 21:43:54 +0000

    [TTS] Clean up and refactor code related to Peek Promo
    Clean up and refactor unused code related to the Peek Promo feature, as it is refactored
    to Bar Banner and is used as an In-Product Help.
    
    Change-Id: I5803043980c0ed55645849e05d063f5a9111f64f
    Reviewed-on: https://chromium-review.googlesource.com/757220
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Mehran Mahmoudi <mahmoudi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514950}

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/MockContextualSearchPolicy.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/MockContextualSearchPolicy.java
@@ -25,11 +25,6 @@ public class MockContextualSearchPolicy extends ContextualSearchPolicy {
     }
 
     @Override
-    public boolean isPeekPromoConditionSatisfied() {
-        return false;
-    }
-
-    @Override
     public boolean shouldAnimateSearchProviderIcon() {
         return false;
     }
