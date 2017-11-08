65a5ceeb1238f8c41541b054f0366caaeb40a66d
Patrik Höglund <phoglund@chromium.org>
2017-11-08 08:12:11 +0000

    Point Chromium include sites to the new candidate.h.
    api/candidate.h is now a proper API include, so downstream users don't
    need to reach into WebRTC's guts to get it anymore.
    
    Bug: webrtc:7504
    Change-Id: I1bd258f8dd953537ae49292d3df03fd10d2a9560
    Reviewed-on: https://chromium-review.googlesource.com/757093
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Reviewed-by: Nicolas Zea <zea@chromium.org>
    Commit-Queue: Patrik Höglund <phoglund@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514778}

--- jingle/glue/utils.cc
+++ jingle/glue/utils.cc
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "net/base/ip_address.h"
 #include "net/base/ip_endpoint.h"
-#include "third_party/webrtc/p2p/base/candidate.h"
+#include "third_party/webrtc/api/candidate.h"
 #include "third_party/webrtc/rtc_base/byteorder.h"
 #include "third_party/webrtc/rtc_base/socketaddress.h"
 
--- remoting/protocol/jingle_messages.h
+++ remoting/protocol/jingle_messages.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/protocol/errors.h"
 #include "remoting/signaling/signaling_address.h"
 #include "third_party/libjingle_xmpp/xmllite/xmlelement.h"
-#include "third_party/webrtc/p2p/base/candidate.h"
+#include "third_party/webrtc/api/candidate.h"
 
 namespace remoting {
 namespace protocol {
