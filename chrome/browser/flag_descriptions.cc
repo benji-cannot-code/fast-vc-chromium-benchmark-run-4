3f3a28d633f55654ac9821537d5c3131a8713e9d
Takashi Toyoshima <toyoshim@chromium.org>
2017-11-09 14:02:48 +0000

    MidiManager Dynamic Instantiation Mode: Make the flag Android-only
    Now that the feature is enabled by default on platforms
    other than Android, I'd make the flag Android-only
    in order to avoid confusion.
    
    Bug: 694138
    Change-Id: Iffc79a0ae22b459e33f6b6c6e72a5a064517c193
    Reviewed-on: https://chromium-review.googlesource.com/760101
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515163}

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -430,11 +430,6 @@ const char kEnablePolicyToolDescription[] =
     "If enabled, the chrome://policy-tool URL loads a page for managing "
     "policies.";
 
-const char kEnableMidiManagerDynamicInstantiationName[] =
-    "MIDIManager dynamic instantiation for Web MIDI.";
-const char kEnableMidiManagerDynamicInstantiationDescription[] =
-    "Enable MIDIManager dynamic instantiation for Web MIDI.";
-
 const char kEnableMultiMirroringName[] =
     "Display mirroring across multiple displays.";
 const char kEnableMultiMirroringDescription[] =
@@ -1801,6 +1796,11 @@ const char kEnableFaviconsFromWebManifestName[] =
     "Load favicons from Web Manifests";
 const char kEnableFaviconsFromWebManifestDescription[] =
     "Fetch Web Manifests on page load to read favicons from them.";
+
+const char kEnableMidiManagerDynamicInstantiationName[] =
+    "MIDIManager dynamic instantiation for Web MIDI.";
+const char kEnableMidiManagerDynamicInstantiationDescription[] =
+    "Enable MIDIManager dynamic instantiation for Web MIDI.";
 
 const char kEnableNtpAssetDownloadSuggestionsName[] =
     "Show asset downloads on the New Tab page";
