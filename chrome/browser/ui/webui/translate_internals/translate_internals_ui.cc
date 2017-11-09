2ca3ba8598e1e452fd226affced4e8f01604c4ef
Charles Zhao <charleszhao@chromium.org>
2017-11-09 00:07:49 +0000

    Clean up cld2.
    (1) //third_party/cld2 is deleted since it contains all cld2 code.
    
    (2) //third_party/cld is deleted since it contains the flag to switch
    between cld2 and cld3.
    
    (3) //chrome/test/data/cld2_component/ is deleted since cld2 is gone.
    
    (4) chrome/browser/ui/webui/ records cld versions; and is now hard 
    coded to be cld3; The related dependencies in DEPS and BUILD.gn 
    are also deleted.  
    
    (5) components/translate/core/language_dection/ uses cld, which is now
    changed to cld3 for the head file and dependencies.
     
    (6) components/translate/cotent/render has the decencies to cld2,
    which I believe is unnecessary; So simply delete it.
    
    (7) extensions/render/ uses cld, which is now changed to cld3 for the
    head file and dependencies.
    
    (8) cld2 related enum and uma histogram are also obsoleted.
     
    (9) also changed other related dependencies.
    
    Bug: 771778
    Change-Id: Ice64df4d60087e41a97b152e88690402b501b99e
    Reviewed-on: https://chromium-review.googlesource.com/734680
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Commit-Queue: Charles . <charleszhao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515022}

--- chrome/browser/ui/webui/translate_internals/translate_internals_ui.cc
+++ chrome/browser/ui/webui/translate_internals/translate_internals_ui.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
 #include "content/public/browser/web_ui_data_source.h"
-#include "third_party/cld/cld_version.h"
 #include "ui/base/l10n/l10n_util.h"
 
 namespace {
@@ -63,17 +62,8 @@ content::WebUIDataSource* CreateTranslateInternalsHTMLSource() {
     source->AddString(key, value);
   }
 
-  std::string cld_version = "";
-  // The version string is hardcoded here to avoid linking with the CLD
-  // library, see http://crbug.com/297777.
-#if BUILDFLAG(CLD_VERSION) == 2
-  cld_version = "2";
-#elif BUILDFLAG(CLD_VERSION) == 3
-  cld_version = "3";
-#else
-# error "CLD_VERSION must be 2 or 3"
-#endif
-  source->AddString("cld-version", cld_version);
+  // Current cld-version is "3".
+  source->AddString("cld-version", "3");
 
   return source;
 }
