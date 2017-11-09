7109ab6ef3789a0132954272a6a7a46340da2af7
Patrick Monette <pmonette@chromium.org>
2017-11-09 22:23:54 +0000

    Remove an unused layout for the Win10 welcome page
    It turns out that it was never used so it can be safely deleted and the
    code simplified.
    
    Bug: 658918
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I8026f1ae05ca614e3ad0a70b210539ebbf6891ef
    Reviewed-on: https://chromium-review.googlesource.com/740781
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515320}

--- chrome/browser/resources/welcome/welcome_win10.js
+++ chrome/browser/resources/welcome/welcome_win10.js
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 Polymer({
-  is: 'welcome-win10-inline',
+  is: 'welcome-win10',
 
   properties: {
     // Determines if the combined variant should be displayed. The combined
