54a0d40db80f092a75070b74686cd18f24fdeee4
Olivier Robin <olivierrobin@chromium.org>
2017-11-09 13:31:12 +0000

    Synchronously send widget metrics.
    Posting task may cause crash if application has no longer access to
    shared sandbox on execution (suspended).
    
    Bug: 780832
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: If5998414728ef4d8d3c61da3f9f6d5e705744b33
    Reviewed-on: https://chromium-review.googlesource.com/758397
    Reviewed-by: Elodie Banel <lod@chromium.org>
    Reviewed-by: Eric Noyau <noyau@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515157}

--- ios/chrome/common/app_group/app_group_metrics_client.h
+++ ios/chrome/common/app_group/app_group_metrics_client.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/common/app_group/app_group_constants.h"
 
 namespace app_group {
+// TODO(crbug.com/782685): remove all functions.
 namespace client_app {
 
 // Adds a closed log to the pending log directory. The log will be processed the
