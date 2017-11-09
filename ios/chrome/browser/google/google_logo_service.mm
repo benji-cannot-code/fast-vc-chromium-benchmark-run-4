0489f67d27cfb8344323c0759f76ce539ab2bfc1
Nicolas Dossou-gbete <dgn@chromium.org>
2017-11-09 11:40:02 +0000

    🏡 Look up the logo type to request before each fetch
    Initialise LogoService with a callback to get the type of logo to
    request from the server rather than hardcoding it at construction
    time.
    This will allow showing the right type of Doodle in the NewTabPage
    or bottom sheet when ChromeHome is turned on or off at runtime.
    
    This patch also refactors the logo URL building logic, adding the
    parameters to the base URL in 2 steps: one for the ones that do not
    change across calls and are part of the endpoint API configuration
    (e.g. whether to use gray doodles), and the ones that can change
    across calls (e.g. logo fingerprint)
    
    Bug: 776725
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ibaba09431e2c24fdb518511c8e14b1c7a198944f
    Reviewed-on: https://chromium-review.googlesource.com/743964
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Eric Noyau <noyau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515140}

--- ios/chrome/browser/google/google_logo_service.mm
+++ ios/chrome/browser/google/google_logo_service.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 
+#include "base/bind.h"
 #include "base/strings/sys_string_conversions.h"
 #include "components/image_fetcher/ios/ios_image_decoder_impl.h"
 #include "net/url_request/url_request_context_getter.h"
@@ -36,11 +37,13 @@ base::FilePath DoodleDirectory() {
 GoogleLogoService::GoogleLogoService(
     TemplateURLService* template_url_service,
     scoped_refptr<net::URLRequestContextGetter> request_context_getter)
-    : LogoServiceImpl(DoodleDirectory(),
-                      template_url_service,
-                      image_fetcher::CreateIOSImageDecoder(),
-                      request_context_getter,
-                      /*use_gray_background=*/false) {}
+    : LogoServiceImpl(
+          DoodleDirectory(),
+          template_url_service,
+          image_fetcher::CreateIOSImageDecoder(),
+          request_context_getter,
+          /*want_gray_logo_getter=*/base::BindRepeating([] { return false; })) {
+}
 
 GoogleLogoService::~GoogleLogoService() {}
 
