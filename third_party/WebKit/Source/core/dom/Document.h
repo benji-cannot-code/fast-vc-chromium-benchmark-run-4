1293e014d128ce846bb45d63bc6efc0c00320472
John Budorick <jbudorick@chromium.org>
2017-11-09 17:14:27 +0000

    Revert "Re-Reland: Add UKM Document.OutliveTimeAfterShutdown"
    This reverts commit 3c722d4240550989f41e517912c974798c49c4ed.
    
    Reason for revert: Causing another log explosion: https://bugs.chromium.org/p/chromium/issues/detail?id=783221
    
    Original change's description:
    > Re-Reland: Add UKM Document.OutliveTimeAfterShutdown
    > 
    > This is basically same as https://chromium-review.googlesource.com/c/chromium/src/+/722301
    > This fix originally caused a log explosion (crbug/776334). Now UKM is
    > moved from a browser to a service, this problem has been fixed
    > (crbug/733995).
    > 
    > This CL adds a new UKM Document.OutliveTimeAfterShutdown, that is
    > recorded when a Document object survives 5, 10, 20 or 50 garbage
    > collections after detached. If a document outlives such long time, the
    > document might be leaked. The UKM would be very useful to know where such
    > leaky documents exist and to fix them.
    > 
    > Design doc: https://docs.google.com/document/d/1fbs5smdd-pBLLMpq7u8EkyddZILtI7CZPJlo_AA1kak/edit?usp=sharing
    > 
    > Bug: 757374
    > Change-Id: Ib8e3fdb0e8ed06842588d73a6e235208ccbcfbba
    > Reviewed-on: https://chromium-review.googlesource.com/722301
    > Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Reviewed-by: Chris Palmer <palmer@chromium.org>
    > Reviewed-by: Steven Holte <holte@chromium.org>
    > Cr-Original-Commit-Position: refs/heads/master@{#510029}
    > Reviewed-on: https://chromium-review.googlesource.com/757885
    > Cr-Commit-Position: refs/heads/master@{#515098}
    
    TBR=palmer@chromium.org,hajimehoshi@chromium.org,haraken@chromium.org,holte@chromium.org
    
    Change-Id: Ic0be7a1aac8f420ac37290682d5251b0a80b0de3
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 757374
    Reviewed-on: https://chromium-review.googlesource.com/760398
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Commit-Queue: John Budorick <jbudorick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515194}

--- third_party/WebKit/Source/core/dom/Document.h
+++ third_party/WebKit/Source/core/dom/Document.h
@@ -1395,8 +1395,6 @@ class CORE_EXPORT Document : public ContainerNode,
 
   scoped_refptr<WebTaskRunner> GetTaskRunner(TaskType) override;
 
-  void RecordUkmOutliveTimeAfterShutdown(int outlive_time_count);
-
  protected:
   Document(const DocumentInit&, DocumentClassFlags = kDefaultDocumentClass);
 
