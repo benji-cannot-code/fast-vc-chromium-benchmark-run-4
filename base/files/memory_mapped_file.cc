c8fed481f5fad15481f0eb0823aa968639acf24b
Yuzhu Shen <yzshen@chromium.org>
2017-11-09 20:36:05 +0000

    Revert "Change size in memory mapped region to size_t"
    This reverts commit bc2056c30e9d7245c3ef90664201eccca41418b7.
    
    Reason for revert: Causing compilation failure:
    https://uberchromegw.corp.google.com/i/chromium.mac/builders/ios-device-xcode-clang/builds/43902
    
    Original change's description:
    > Change size in memory mapped region to size_t
    > 
    > Subsequently, remove some static_casts that are no longer needed.
    > 
    > On Windows, while it is possible to create a file mapping of a file >
    > 32-bits long it is not possible to create the view to the file
    > mapping, so changing this to size_t should have no loss of functionality.
    > 
    > BUG=778316
    > 
    > Change-Id: I81cd2e0808794fdb83fc21c796367369ea2da220
    > Reviewed-on: https://chromium-review.googlesource.com/755114
    > Reviewed-by: Mark Mentovai <mark@chromium.org>
    > Commit-Queue: Will Harris <wfh@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#515257}
    
    TBR=wfh@chromium.org,mark@chromium.org
    
    Change-Id: I9a41a960cb7b3e457af41e330806285f5ab7879f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 778316
    Reviewed-on: https://chromium-review.googlesource.com/761597
    Reviewed-by: Yuzhu Shen <yzshen@chromium.org>
    Commit-Queue: Yuzhu Shen <yzshen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515273}

--- base/files/memory_mapped_file.cc
+++ base/files/memory_mapped_file.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_path.h"
 #include "base/logging.h"
-#include "base/numerics/safe_math.h"
 #include "base/sys_info.h"
 #include "build/build_config.h"
 
@@ -74,13 +73,10 @@ bool MemoryMappedFile::Initialize(File file,
   switch (access) {
     case READ_WRITE_EXTEND:
       DCHECK(Region::kWholeFile != region);
-      {
-        CheckedNumeric<int64_t> region_end(region.offset);
-        region_end += region.size;
-        if (!region_end.IsValid()) {
-          DLOG(ERROR) << "Region bounds exceed maximum for base::File.";
-          return false;
-        }
+      // Ensure that the extended size is within limits of File.
+      if (region.size > std::numeric_limits<int64_t>::max() - region.offset) {
+        DLOG(ERROR) << "Region bounds exceed maximum for base::File.";
+        return false;
       }
       // Fall through.
     case READ_ONLY:
@@ -96,8 +92,10 @@ bool MemoryMappedFile::Initialize(File file,
   if (IsValid())
     return false;
 
-  if (region != Region::kWholeFile)
+  if (region != Region::kWholeFile) {
     DCHECK_GE(region.offset, 0);
+    DCHECK_GT(region.size, 0);
+  }
 
   file_ = std::move(file);
 
@@ -115,17 +113,18 @@ bool MemoryMappedFile::IsValid() const {
 
 // static
 void MemoryMappedFile::CalculateVMAlignedBoundaries(int64_t start,
-                                                    size_t size,
+                                                    int64_t size,
                                                     int64_t* aligned_start,
-                                                    size_t* aligned_size,
+                                                    int64_t* aligned_size,
                                                     int32_t* offset) {
   // Sadly, on Windows, the mmap alignment is not just equal to the page size.
-  auto mask = SysInfo::VMAllocationGranularity() - 1;
-  DCHECK(IsValueInRangeForNumericType<int32_t>(mask));
+  const int64_t mask =
+      static_cast<int64_t>(SysInfo::VMAllocationGranularity()) - 1;
+  DCHECK_LT(mask, std::numeric_limits<int32_t>::max());
   *offset = start & mask;
   *aligned_start = start & ~mask;
   *aligned_size = (size + *offset + mask) & ~mask;
 }
-#endif  // !defined(OS_NACL)
+#endif
 
 }  // namespace base
