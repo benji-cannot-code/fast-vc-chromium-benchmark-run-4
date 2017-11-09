af81afa191aa6f71e53edc87ed20dac9a98dbad3
Jayson Adams <shrike@chromium.org>
2017-11-09 21:52:36 +0000

    [Mac] Improve appearance of Task Manager columns.
    Changes the Task Manager to use the monospace digit system font on
    macOS 10.11 so that the Task Manager's columns line up nicely. Also
    increases the CPU column's min width so that it's title is never tail-
    truncated when more columns are added.
    
    Bug: 782003
    Change-Id: I99470bc4935d058c2e113cf1937484a984a38179
    Reviewed-on: https://chromium-review.googlesource.com/756050
    Commit-Queue: Jayson Adams <shrike@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515306}

--- chrome/browser/ui/cocoa/task_manager_mac.mm
+++ chrome/browser/ui/cocoa/task_manager_mac.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "base/mac/bundle_locations.h"
+#import "base/mac/sdk_forward_declarations.h"
 #include "base/macros.h"
 #include "base/strings/sys_string_conversions.h"
 #include "chrome/browser/browser_process.h"
@@ -249,7 +250,14 @@ bool ShouldUseViewsTaskManager() {
   [[column.get() headerCell] setAlignment:textAlignment];
   [[column.get() dataCell] setAlignment:textAlignment];
 
-  NSFont* font = [NSFont systemFontOfSize:[NSFont smallSystemFontSize]];
+  const CGFloat smallSystemFontSize = [NSFont smallSystemFontSize];
+  NSFont* font = nil;
+  if (@available(macOS 10.11, *)) {
+    font = [NSFont monospacedDigitSystemFontOfSize:smallSystemFontSize
+                                            weight:NSFontWeightRegular];
+  } else {
+    font = [NSFont systemFontOfSize:smallSystemFontSize];
+  }
   [[column.get() dataCell] setFont:font];
 
   [column.get() setHidden:!columnData.default_visibility];
@@ -321,7 +329,7 @@ bool ShouldUseViewsTaskManager() {
 
 // Callback for the table header context menu. Toggles visibility of the table
 // column associated with the clicked menu item.
-- (void)toggleColumn:(id)item {
+- (void)toggleColumn:(NSMenuItem*)item {
   DCHECK([item isKindOfClass:[NSMenuItem class]]);
   if (![item isKindOfClass:[NSMenuItem class]])
     return;
