ffaa61026e59d79d1c6e08fcb58ec74ace421ab6
Nathan Bruer <allada@chromium.org>
2017-11-09 04:32:20 +0000

    [Devtools] Fixed bug where iframes were not editable with overrides
    R=pfeldman
    Bug=None
    
    Change-Id: I3b4f3b85e5629a3fdd8110c337494210fbf5f424
    Reviewed-on: https://chromium-review.googlesource.com/734300
    Commit-Queue: Blaise Bruer <allada@chromium.org>
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515099}

---
+++ third_party/WebKit/Source/devtools/front_end/bindings_test_runner/OverridesTestRunner.js
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @param {string} folderPath
+ * @return {!Workspace.Project}
+ */
+BindingsTestRunner.createOverrideProject = async function(folderPath) {
+  var testFilesystem = new BindingsTestRunner.TestFileSystem(folderPath);
+  await testFilesystem.reportCreatedPromise();
+  var isolatedFileSystem = Persistence.isolatedFileSystemManager.fileSystem(testFilesystem.fileSystemPath);
+  isolatedFileSystem._type = 'overrides';
+  var project =
+      Workspace.workspace.project(Persistence.FileSystemWorkspaceBinding.projectId(isolatedFileSystem.path()));
+  console.assert(project);
+  return project;
+};
+
+/**
+ * @param {boolean} enabled
+ */
+BindingsTestRunner.setOverridesEnabled = function(enabled) {
+  Common.settings.moduleSetting('persistenceNetworkOverridesEnabled').set(enabled);
+};
