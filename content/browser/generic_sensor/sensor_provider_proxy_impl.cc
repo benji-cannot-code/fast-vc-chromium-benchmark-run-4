38f55cda55323026440532afc6325106547c3e6a
Reilly Grant <reillyg@chromium.org>
2017-11-09 22:55:26 +0000

    Return Sensor pipe in SensorInitParams
    This patch modifies the GetSensor method so that the Sensor interface
    pointer is returned in the SensorInitParams struct instead of being
    passed to the GetSensor method as an interface request. This prevents
    a class of errors where an early connection error on the Sensor pipe
    can race with the reply to the GetSensor method as they are sent on
    different FIFOs.
    
    Bug: 781767
    Change-Id: I79e47417da4b0bda49ddb44070e123009e46b893
    Reviewed-on: https://chromium-review.googlesource.com/757283
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Tim Volodine <timvolodine@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515333}

--- content/browser/generic_sensor/sensor_provider_proxy_impl.cc
+++ content/browser/generic_sensor/sensor_provider_proxy_impl.cc
@@ -37,13 +37,12 @@ void SensorProviderProxyImpl::Bind(
 
 void SensorProviderProxyImpl::GetSensor(
     device::mojom::SensorType type,
-    device::mojom::SensorRequest sensor_request,
     GetSensorCallback callback) {
   ServiceManagerConnection* connection =
       ServiceManagerConnection::GetForProcess();
 
   if (!connection || !CheckPermission(type)) {
-    std::move(callback).Run(nullptr, nullptr);
+    std::move(callback).Run(nullptr);
     return;
   }
 
@@ -54,8 +53,7 @@ void SensorProviderProxyImpl::GetSensor(
         base::BindOnce(&device::mojom::SensorProviderPtr::reset,
                        base::Unretained(&sensor_provider_)));
   }
-  sensor_provider_->GetSensor(type, std::move(sensor_request),
-                              std::move(callback));
+  sensor_provider_->GetSensor(type, std::move(callback));
 }
 
 bool SensorProviderProxyImpl::CheckPermission(
--- third_party/WebKit/Source/modules/sensor/SensorProxy.cpp
+++ third_party/WebKit/Source/modules/sensor/SensorProxy.cpp
@@ -66,8 +66,7 @@ void SensorProxy::Initialize() {
   state_ = kInitializing;
   auto callback = ConvertToBaseCallback(
       WTF::Bind(&SensorProxy::OnSensorCreated, WrapWeakPersistent(this)));
-  provider_->GetSensorProvider()->GetSensor(type_, mojo::MakeRequest(&sensor_),
-                                            callback);
+  provider_->GetSensorProvider()->GetSensor(type_, std::move(callback));
 }
 
 void SensorProxy::AddConfiguration(SensorConfigurationPtr configuration,
@@ -166,8 +165,7 @@ void SensorProxy::HandleSensorError() {
   }
 }
 
-void SensorProxy::OnSensorCreated(SensorInitParamsPtr params,
-                                  SensorClientRequest client_request) {
+void SensorProxy::OnSensorCreated(SensorInitParamsPtr params) {
   DCHECK_EQ(kInitializing, state_);
   if (!params) {
     HandleSensorError();
@@ -184,8 +182,8 @@ void SensorProxy::OnSensorCreated(SensorInitParamsPtr params,
     return;
   }
 
-  DCHECK(sensor_.is_bound());
-  client_binding_.Bind(std::move(client_request));
+  sensor_ = std::move(params->sensor);
+  client_binding_.Bind(std::move(params->client_request));
 
   shared_buffer_handle_ = std::move(params->memory);
   DCHECK(!shared_buffer_);
