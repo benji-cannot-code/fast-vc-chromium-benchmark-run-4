9acc90d07eff2f78a20bb3f5408b00b3133477e0
Minh X. Nguyen <mxnguyen@chromium.org>
2017-11-09 17:11:14 +0000

    [Extension] Add IsExtensionEnabled to ExtensionsBrowserClient.
    UpdateDataProvider::GetData needs this function in order to determine
    the disable/enable state of an extension.
    
    Bug: 782765
    Change-Id: I99a199265b606a9aefcc660e85b341b71b4f3bd5
    Reviewed-on: https://chromium-review.googlesource.com/755155
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Minh Nguyen <mxnguyen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515192}

--- extensions/browser/extensions_browser_client.cc
+++ extensions/browser/extensions_browser_client.cc
@@ -53,6 +53,12 @@ ExtensionsBrowserClient::GetExtensionNavigationUIData(
   return nullptr;
 }
 
+bool ExtensionsBrowserClient::IsExtensionEnabled(
+    const std::string& extension_id,
+    content::BrowserContext* context) const {
+  return false;
+}
+
 ExtensionsBrowserClient* ExtensionsBrowserClient::Get() {
   return g_client;
 }
