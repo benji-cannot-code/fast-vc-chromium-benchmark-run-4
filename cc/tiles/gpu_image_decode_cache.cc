d155789572d1cb61c68e70a1e1defa599bc3a169
Vladimir Levin <vmpstr@chromium.org>
2017-11-09 01:54:12 +0000

    cc/metrics: Add CachedImagesCount histogram.
    This patch adds these histograms:
    
    Compositing.{Browser,Renderer>.CachedImagesCount.{Gpu,Software}
    
    To measure the maximum amount of images that we cache over the lifetime
    of the cache. This is meant to guide the maximum number of images we
    would keep in software and overall a data point to understand image
    usage on the web.
    
    R=ericrk@chromium.org, khushalsagar@chromium.org
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ice882e68f8b1d536bd8a4b072c55c0f0a0081a4f
    Reviewed-on: https://chromium-review.googlesource.com/758820
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515054}

--- cc/tiles/gpu_image_decode_cache.cc
+++ cc/tiles/gpu_image_decode_cache.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/trace_event/memory_dump_manager.h"
 #include "cc/base/devtools_instrumentation.h"
+#include "cc/base/histograms.h"
 #include "cc/raster/tile_task.h"
 #include "cc/tiles/mipmap_util.h"
 #include "components/viz/common/gpu/context_provider.h"
@@ -498,6 +499,11 @@ GpuImageDecodeCache::~GpuImageDecodeCache() {
       this);
   // Unregister this component with memory_coordinator::ClientRegistry.
   base::MemoryCoordinatorClientRegistry::GetInstance()->Unregister(this);
+
+  UMA_HISTOGRAM_CUSTOM_COUNTS(
+      base::StringPrintf("Compositing.%s.CachedImagesCount.Gpu",
+                         GetClientNameForMetrics()),
+      lifetime_max_items_in_cache_, 1, 1000, 20);
 }
 
 ImageDecodeCache::TaskResult GpuImageDecodeCache::GetTaskForImageAndRef(
@@ -1089,6 +1095,9 @@ bool GpuImageDecodeCache::EnsureCapacity(size_t required_size) {
   TRACE_EVENT0(TRACE_DISABLED_BY_DEFAULT("cc.debug"),
                "GpuImageDecodeCache::EnsureCapacity");
   lock_.AssertAcquired();
+
+  lifetime_max_items_in_cache_ =
+      std::max(lifetime_max_items_in_cache_, persistent_cache_.size());
 
   if (CanFitInWorkingSet(required_size) && !ExceedsPreferredCount())
     return true;
--- cc/tiles/software_image_decode_cache.cc
+++ cc/tiles/software_image_decode_cache.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/trace_event/memory_dump_manager.h"
 #include "cc/base/devtools_instrumentation.h"
+#include "cc/base/histograms.h"
 #include "cc/raster/tile_task.h"
 #include "cc/tiles/mipmap_util.h"
 #include "third_party/skia/include/core/SkCanvas.h"
@@ -38,8 +39,6 @@ namespace {
 // if more items are locked. That is, locked items ignore this limit.
 // Depending on the memory state of the system, we limit the amount of items
 // differently.
-// TODO(vmpstr): UMA how many image we should keep around. This number seems
-// high.
 const size_t kNormalMaxItemsInCache = 1000;
 const size_t kThrottledMaxItemsInCache = 100;
 const size_t kSuspendedMaxItemsInCache = 0;
@@ -222,6 +221,11 @@ SoftwareImageDecodeCache::~SoftwareImageDecodeCache() {
       this);
   // Unregister this component with memory_coordinator::ClientRegistry.
   base::MemoryCoordinatorClientRegistry::GetInstance()->Unregister(this);
+
+  UMA_HISTOGRAM_CUSTOM_COUNTS(
+      base::StringPrintf("Compositing.%s.CachedImagesCount.Software",
+                         GetClientNameForMetrics()),
+      lifetime_max_items_in_cache_, 1, 1000, 20);
 }
 
 ImageDecodeCache::TaskResult SoftwareImageDecodeCache::GetTaskForImageAndRef(
@@ -585,6 +589,8 @@ void SoftwareImageDecodeCache::DrawWithImageFinished(
 void SoftwareImageDecodeCache::ReduceCacheUsageUntilWithinLimit(size_t limit) {
   TRACE_EVENT0("cc",
                "SoftwareImageDecodeCache::ReduceCacheUsageUntilWithinLimit");
+  lifetime_max_items_in_cache_ =
+      std::max(lifetime_max_items_in_cache_, decoded_images_.size());
   for (auto it = decoded_images_.rbegin();
        decoded_images_.size() > limit && it != decoded_images_.rend();) {
     if (it->second->ref_count != 0) {
