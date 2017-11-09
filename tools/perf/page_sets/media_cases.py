0e68415c83e1e188e4dc23d001f0eec0e9c818c9
Caleb Rouleau <crouleau@chromium.org>
2017-11-09 03:14:17 +0000

    [Telemetry] Remove dead code from media_cases.py.
    I think this used to be used for back when we had a legacy benchmark,
    but our current benchmark is TBMv2, so we can't configure which metrics
    to generate in which cases anymore.
    
    Bug: 632021
    Change-Id: I5c9c3ce7c823c59984fd4989194d389be0569986
    Reviewed-on: https://chromium-review.googlesource.com/759318
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Commit-Queue: Caleb Rouleau <crouleau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515083}

--- tools/perf/page_sets/media_cases.py
+++ tools/perf/page_sets/media_cases.py
@@ -91,7 +91,6 @@ class _BeginningToEndPlayPage(_MediaPage):
                traffic_setting=traffic_setting_module.NONE):
     tags.append('beginning_to_end')
     tags.append('src')
-    self.add_browser_metrics = True
     super(_BeginningToEndPlayPage, self).__init__(
         url, page_set, tags, extra_browser_args,
         traffic_setting=traffic_setting)
@@ -113,7 +112,6 @@ class _SeekPage(_MediaPage):
                traffic_setting=traffic_setting_module.NONE):
     tags.append('seek')
     tags.append('src')
-    self.skip_basic_metrics = True
     self._action_timeout = action_timeout_in_seconds
     super(_SeekPage, self).__init__(
         url, page_set, tags, extra_browser_args,
@@ -147,7 +145,6 @@ class _BackgroundPlaybackPage(_MediaPage):
                background_time=10,
                traffic_setting=traffic_setting_module.NONE):
     self._background_time = background_time
-    self.skip_basic_metrics = True
     tags.append('background')
     tags.append('src')
     # disable-media-suspend is required since for Android background playback
