09e08885cfca54483f816cedd0d25e0b80349878
Qiang Xu <warx@chromium.org>
2017-11-09 03:01:37 +0000

    cros: keyboard shortcuts to move active window between displays
    design doc: go/cros-keyboard-window-movement
    
    changes:
    - Add keyboard shortcuts (Search + Alt + Arrow Keys) for moving active
      window between displays.
    - Candidate displays should be completely off the bounds of original
      display in the moving direction or cycled direction.
    - It will try to find the displays in the direction provided, if not
      found, then the displays in the cycled direction.
    - First, try to find the least apart distance in the given direction
      or cycled direction.
    - Second, for those that have the same least apart distance in the
      above, the one that has less vertical offset wins.
    - added test coverage
    
    Follow-up work:
    - Dealing with window resizing issue
    - UMA, a11y
    
    Bug: 778438
    Test: emulator tests and added test coverage
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I49be94635d5460e686f16b6e56fc2baf865ab794
    Reviewed-on: https://chromium-review.googlesource.com/738826
    Commit-Queue: Qiang(Joe) Xu <warx@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515078}

--- ash/accelerators/accelerator_controller.cc
+++ ash/accelerators/accelerator_controller.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/accessibility/accessibility_delegate.h"
 #include "ash/accessibility_types.h"
 #include "ash/display/display_configuration_controller.h"
+#include "ash/display/display_move_window_util.h"
 #include "ash/focus_cycler.h"
 #include "ash/ime/ime_controller.h"
 #include "ash/ime/ime_switch_type.h"
@@ -256,6 +257,22 @@ void HandleMediaPlayPause() {
 
 void HandleMediaPrevTrack() {
   Shell::Get()->media_controller()->HandleMediaPrevTrack();
+}
+
+void HandleMoveWindowBetweenDisplays(const ui::Accelerator& accelerator) {
+  ui::KeyboardCode key_code = accelerator.key_code();
+  DisplayMoveWindowDirection direction;
+  if (key_code == ui::VKEY_LEFT) {
+    direction = DisplayMoveWindowDirection::kLeft;
+  } else if (key_code == ui::VKEY_UP) {
+    direction = DisplayMoveWindowDirection::kAbove;
+  } else if (key_code == ui::VKEY_RIGHT) {
+    direction = DisplayMoveWindowDirection::kRight;
+  } else {
+    DCHECK(key_code == ui::VKEY_DOWN);
+    direction = DisplayMoveWindowDirection::kBelow;
+  }
+  HandleMoveWindowToDisplay(direction);
 }
 
 void HandleToggleMirrorMode() {
@@ -612,6 +629,14 @@ bool CanHandleLock() {
 void HandleLock() {
   base::RecordAction(UserMetricsAction("Accel_LockScreen_L"));
   Shell::Get()->session_controller()->LockScreen();
+}
+
+bool CanHandleMoveWindowBetweenDisplays() {
+  display::DisplayManager* display_manager = Shell::Get()->display_manager();
+  // Accelerators to move window between displays on unified desktop mode and
+  // mirror mode is disabled.
+  return !display_manager->IsInUnifiedMode() &&
+         !display_manager->IsInMirrorMode();
 }
 
 PaletteTray* GetPaletteTray() {
@@ -1074,6 +1099,11 @@ bool AcceleratorController::CanPerformAction(
       return CanHandleDisableCapsLock(previous_accelerator);
     case LOCK_SCREEN:
       return CanHandleLock();
+    case MOVE_WINDOW_TO_ABOVE_DISPLAY:
+    case MOVE_WINDOW_TO_BELOW_DISPLAY:
+    case MOVE_WINDOW_TO_LEFT_DISPLAY:
+    case MOVE_WINDOW_TO_RIGHT_DISPLAY:
+      return CanHandleMoveWindowBetweenDisplays();
     case NEW_INCOGNITO_WINDOW:
       return CanHandleNewIncognitoWindow();
     case NEXT_IME:
@@ -1287,6 +1317,12 @@ void AcceleratorController::PerformAction(AcceleratorAction action,
       break;
     case MEDIA_PREV_TRACK:
       HandleMediaPrevTrack();
+      break;
+    case MOVE_WINDOW_TO_ABOVE_DISPLAY:
+    case MOVE_WINDOW_TO_BELOW_DISPLAY:
+    case MOVE_WINDOW_TO_LEFT_DISPLAY:
+    case MOVE_WINDOW_TO_RIGHT_DISPLAY:
+      HandleMoveWindowBetweenDisplays(accelerator);
       break;
     case NEW_INCOGNITO_WINDOW:
       HandleNewIncognitoWindow();
