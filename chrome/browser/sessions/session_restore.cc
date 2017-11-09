aea02d3c02e61b5d0a92b12aa61d90a6bd8c0200
Gabriel Charette <gab@chromium.org>
2017-11-09 01:59:06 +0000

    Migrate MessageLoop::ScopedNestableTaskAllower to RunLoop::Type::kNestableTasksAllowed in /chrome/browser/sessions
    This pass focuses on ML::SNTA in same scope as RunLoop variable
    Scripted via https://bugs.chromium.org/p/chromium/issues/detail?id=781352#c1
    
    The script should have replaced a MessageLoop::ScopedNestableTaskAllower
    in the same scope as a RunLoop with a RunLoop of
    Type::kNestableTasksAllowed. If there was a comment above the
    MessageLoop::ScopedNestableTaskAllower, it should have been moved
    alongside the augmented RunLoop.
    
    Please CQ if LGTY
    
    This CL was uploaded by git cl split.
    
    R=sky@chromium.org
    
    Bug: 781352
    Change-Id: Ie28ff95630b68ff5dc11c920f652fd721920af63
    Reviewed-on: https://chromium-review.googlesource.com/758929
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515057}

--- chrome/browser/sessions/session_restore.cc
+++ chrome/browser/sessions/session_restore.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/debug/alias.h"
 #include "base/location.h"
 #include "base/macros.h"
-#include "base/message_loop/message_loop.h"
 #include "base/metrics/field_trial.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/run_loop.h"
@@ -146,9 +145,7 @@ class SessionRestoreImpl : public content::NotificationObserver {
 
     if (synchronous_) {
       {
-        base::MessageLoop::ScopedNestableTaskAllower allow(
-            base::MessageLoop::current());
-        base::RunLoop loop;
+        base::RunLoop loop(base::RunLoop::Type::kNestableTasksAllowed);
         quit_closure_for_sync_restore_ = loop.QuitClosure();
         loop.Run();
         quit_closure_for_sync_restore_ = base::Closure();
