50d4ad6ed73f2cc5a23644bcfd07d8c493a7ff2b
Sergei Datsenko <dats@chromium.org>
2017-11-09 23:23:24 +0000

    Add template for headless rendering tests
    Utility and an example for headless tests that need to compare rendering
    output to a golden reference.
    
    Change-Id: If3c4ae0e4f004a4e1a5d0be7377d231ac6aac1a1
    Reviewed-on: https://chromium-review.googlesource.com/759760
    Reviewed-by: Alex Clarke <alexclarke@chromium.org>
    Commit-Queue: Sergei Datsenko <dats@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515342}

--- headless/public/headless_devtools_client.h
+++ headless/public/headless_devtools_client.h
@@ -11,6 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "headless/public/headless_export.h"
 
+namespace base {
+class DictionaryValue;
+}  // namespace base
+
 namespace headless {
 
 namespace accessibility {
