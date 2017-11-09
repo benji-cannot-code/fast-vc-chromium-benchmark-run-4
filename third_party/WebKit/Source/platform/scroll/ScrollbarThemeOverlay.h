b4f0c1a75d08527796887ac14b514a48fc7f94c5
chaopeng <chaopeng@chromium.org>
2017-11-09 22:12:16 +0000

    Reland "Set proper settings for mobile emulator overlay scrollbar."
    This is a reland of e49403327faee603266393f4ca6ddbd07a6f5a50
    Original change's description:
    > Set proper settings for mobile emulator overlay scrollbar.
    >
    > This issue is caused by mobile emulator does not set proper settings.
    >
    > 1. solid_color_scrollbar need to be true to create animation controller.
    > 2. Need to use correct scrollbar theme for FrameView and PLSA.
    >
    > After this patch, when toggle mobile emulator, scrollbar will paint
    > animating as Android Scrollbar.
    >
    > Bug: 740879
    > Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    > Change-Id: I2cedba08609e9c0d2f06d26ec2ca92d508f83c48
    > Reviewed-on: https://chromium-review.googlesource.com/646727
    > Commit-Queue: Jianpeng Chao <chaopeng@chromium.org>
    > Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    > Reviewed-by: Philip Rogers <pdr@chromium.org>
    > Reviewed-by: David Bokan <bokan@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#514085}
    
    Bug: 740879
    Change-Id: Ifcf6685ab733fd2745c7b325480b1b3d381bec16
    TBR: pdr@chromium.org
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Reviewed-on: https://chromium-review.googlesource.com/760596
    Commit-Queue: Jianpeng Chao <chaopeng@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515313}

--- third_party/WebKit/Source/platform/scroll/ScrollbarThemeOverlay.h
+++ third_party/WebKit/Source/platform/scroll/ScrollbarThemeOverlay.h
@@ -85,6 +85,8 @@ class PLATFORM_EXPORT ScrollbarThemeOverlay : public ScrollbarTheme {
 
   int MinimumThumbLength(const ScrollbarThemeClient&) override;
 
+  bool IsMobileTheme() const;
+
   static ScrollbarThemeOverlay& MobileTheme();
 
  private:
@@ -92,6 +94,7 @@ class PLATFORM_EXPORT ScrollbarThemeOverlay : public ScrollbarTheme {
   int scrollbar_margin_;
   HitTestBehavior allow_hit_test_;
   Color color_;
+  bool is_mobile_theme_;
   const bool use_solid_color_;
 };
 
