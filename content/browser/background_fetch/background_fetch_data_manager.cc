5b9a8409d96758a55fddab7ac6338c3f5db7ec21
Dan Elphick <delphick@chromium.org>
2017-11-10 07:45:16 +0000

    Make GetRegistration work with persistent storage
    Add a new GetRegistrationTask to be used by BackgroundFetchDataManager
    when the persistent storage flag is enabled.
    
    Adds the origin to the proto stored in the database and check it
    matches when retrieving a registration, otherwise trigger a storage
    error.
    
    Make BackgroundFetchDataManagerTest parameterized by persistence, which
    turns on the command line flag. Now all tests run (and pass) with or
    without persistence although the Cleanup test doesn't test much yet
    since there's no way yet to interact with a non-active fetch.
    
    Bug: 757760
    Change-Id: I3aa29e17f9112f282f5672366e52522e549f32d0
    Reviewed-on: https://chromium-review.googlesource.com/758762
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Dan Elphick <delphick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515498}

--- content/browser/background_fetch/background_fetch_data_manager.cc
+++ content/browser/background_fetch/background_fetch_data_manager.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/background_fetch/storage/create_registration_task.h"
 #include "content/browser/background_fetch/storage/database_task.h"
 #include "content/browser/background_fetch/storage/delete_registration_task.h"
+#include "content/browser/background_fetch/storage/get_registration_task.h"
 #include "content/browser/background_fetch/storage/mark_registration_for_deletion_task.h"
 #include "content/browser/blob_storage/chrome_blob_storage_context.h"
 #include "content/browser/service_worker/service_worker_context_wrapper.h"
@@ -245,6 +246,14 @@ void BackgroundFetchDataManager::GetRegistration(
     const std::string& developer_id,
     GetRegistrationCallback callback) {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
+
+  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kEnableBackgroundFetchPersistence)) {
+    AddDatabaseTask(std::make_unique<background_fetch::GetRegistrationTask>(
+        this, service_worker_registration_id, origin, developer_id,
+        std::move(callback)));
+    return;
+  }
 
   auto developer_id_tuple =
       std::make_tuple(service_worker_registration_id, origin, developer_id);
