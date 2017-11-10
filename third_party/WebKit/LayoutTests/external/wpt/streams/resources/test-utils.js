d7e1c5077dd9a88ff8b2b416f76370cb6d8097ca
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2017-11-10 16:11:28 +0000

    Import wpt@17d52e4077cf4cdca2cee4f6d56396426722efc3
    Using wpt-import in Chromium e428ad32252e68b96b3aeece6a35f08a36c62130.
    With Chromium commits locally applied on WPT:
    2370563111 "service worker: Upstream sandboxed iframe test to WPT."
    
    
    Build: https://build.chromium.org/p/chromium.infra.cron/builders/wpt-importer/builds/4847
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    cbiesinger@chromium.org:
      external/wpt/css/css-flexbox
    domenic@chromium.org, ricea@chromium.org, tyoshino@chromium.org:
      external/wpt/streams
    jsbell@chromium.org:
      external/wpt/IndexedDB
      external/wpt/resources
    rego@igalia.com:
      external/wpt/css/css-grid
    rouslan@chromium.org, mathp@chromium.org:
      external/wpt/payment-request
    tyoshino@chromium.org, yhirano@chromium.org, mkwst@chromium.org, japhet@chromium.org:
      external/wpt/fetch
    
    TBR=kereliuk
    
    No-Export: true
    Change-Id: I2243eb8a8ead50c171e981c16c0ca321a9f7e6da
    Reviewed-on: https://chromium-review.googlesource.com/763134
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515563}

--- third_party/WebKit/LayoutTests/external/wpt/streams/resources/test-utils.js
+++ third_party/WebKit/LayoutTests/external/wpt/streams/resources/test-utils.js
@@ -8,7 +8,7 @@ self.getterRejects = (t, obj, getterName, target) => {
 };
 
 self.getterRejectsForAll = (t, obj, getterName, targets) => {
-  return Promise.all(targets.map(target => getterRejects(t, obj, getterName, target)));
+  return Promise.all(targets.map(target => self.getterRejects(t, obj, getterName, target)));
 };
 
 self.methodRejects = (t, obj, methodName, target, args) => {
@@ -18,8 +18,8 @@ self.methodRejects = (t, obj, methodName, target, args) => {
                          methodName + ' should reject with a TypeError');
 };
 
-self.methodRejectsForAll =  (t, obj, methodName, targets, args) => {
-  return Promise.all(targets.map(target => methodRejects(t, obj, methodName, target, args)));
+self.methodRejectsForAll = (t, obj, methodName, targets, args) => {
+  return Promise.all(targets.map(target => self.methodRejects(t, obj, methodName, target, args)));
 };
 
 self.getterThrows = (obj, getterName, target) => {
@@ -29,8 +29,8 @@ self.getterThrows = (obj, getterName, target) => {
 };
 
 self.getterThrowsForAll = (obj, getterName, targets) => {
-  targets.forEach(target => getterThrows(obj, getterName, target));
-}
+  targets.forEach(target => self.getterThrows(obj, getterName, target));
+};
 
 self.methodThrows = (obj, methodName, target, args) => {
   const method = obj[methodName];
@@ -40,8 +40,13 @@ self.methodThrows = (obj, methodName, target, args) => {
 };
 
 self.methodThrowsForAll = (obj, methodName, targets, args) => {
-  targets.forEach(target => methodThrows(obj, methodName, target, args));
-}
+  targets.forEach(target => self.methodThrows(obj, methodName, target, args));
+};
+
+self.constructorThrowsForAll = (constructor, firstArgs) => {
+  firstArgs.forEach(firstArg => assert_throws(new TypeError(), () => new constructor(firstArg),
+                                              'constructor should throw a TypeError'));
+};
 
 self.garbageCollect = () => {
   if (self.gc) {
