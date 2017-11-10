f64f667063c67677558703d7d70a1c1e94675437
Jinyoung Hur <hurims@gmail.com>
2017-11-10 01:41:59 +0000

    Add UMA for Firefox passwords import
    This CL add the following UMA:
    1. If password is selected by user when importing Firefox profiles.
    2. How many passwords are imported from Firefox.
    
    R=isherman@chromium.org, vabr@chromium.org
    
    Bug: 776664
    Change-Id: Ife7b1079502d4c4f0ebd2fb473bceb5feec7ddbc
    Reviewed-on: https://chromium-review.googlesource.com/744061
    Commit-Queue: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Vaclav Brozek <vabr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515407}

--- chrome/utility/importer/firefox_importer.cc
+++ chrome/utility/importer/firefox_importer.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
 #include "base/message_loop/message_loop.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
@@ -103,6 +104,9 @@ FirefoxImporter::~FirefoxImporter() {
 void FirefoxImporter::StartImport(const importer::SourceProfile& source_profile,
                                   uint16_t items,
                                   ImporterBridge* bridge) {
+  UMA_HISTOGRAM_BOOLEAN("Import.IncludesPasswords.Firefox",
+                        !!(items & importer::PASSWORDS));
+
   bridge_ = bridge;
   source_path_ = source_profile.source_path;
   app_path_ = source_profile.app_path;
@@ -375,6 +379,8 @@ void FirefoxImporter::ImportPasswords() {
   }
 
   if (!cancelled()) {
+    UMA_HISTOGRAM_COUNTS_10000("Import.NumberOfImportedPasswords.Firefox",
+                               forms.size());
     for (size_t i = 0; i < forms.size(); ++i) {
       if (!forms[i].username_value.empty() ||
           !forms[i].password_value.empty() ||
