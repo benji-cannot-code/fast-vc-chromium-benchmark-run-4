9b7ae35b2b054e6cfa6fcdc86f41e70c367931d2
Eric Lawrence <elawrence@chromium.org>
2017-11-10 19:36:31 +0000

    Remove unneeded parameter in DidInteractWithNonsecureCreditCardInput
    The DidInteractWithNonsecureCreditCardInput function currently expects
    a content::RenderFrameHost* pointer, but implementers do not use this
    parameter. Worse, that type should not be used on iOS (which does not
    use classes inside /content). This CL removes the unneeded parameter
    from the function.
    
    Bug: 782841
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I99edb4497397f8f82b1b712667eb0dc98b107982
    Reviewed-on: https://chromium-review.googlesource.com/759276
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Mathieu Perreault <mathp@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Eric Lawrence <elawrence@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515636}

--- components/autofill/ios/browser/autofill_driver_ios.mm
+++ components/autofill/ios/browser/autofill_driver_ios.mm
@@ -86,8 +86,7 @@ void AutofillDriverIOS::RendererShouldAcceptDataListSuggestion(
 
 void AutofillDriverIOS::DidInteractWithCreditCardForm() {
   if (!web::IsOriginSecure(web_state_->GetLastCommittedURL())) {
-    autofill_manager_.client()->DidInteractWithNonsecureCreditCardInput(
-        nullptr);
+    autofill_manager_.client()->DidInteractWithNonsecureCreditCardInput();
   }
 }
 
