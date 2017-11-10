8285b997561e508a0aa698ee6c3ceec3f6c108ba
Benjamin C. Wiley Sittler <bsittler@chromium.org>
2017-11-10 02:23:31 +0000

    TextCodecICU: respect encoder state when replacing unencodables
    This also fixes the IgnorableCodePoint test to use inputs which have
    nontrivial ISO-2022-JP shift states; previously this test did not
    actually ensure correct behavior when non-ASCII characters
    representable in ISO-2022-JP and non-ASCII characters not
    representable in ISO-2022-JP occur in sequence.
    
    This also adds WPT coverage.
    
    Prior to this fix, encoder state was not respected, leading to
    incorrect interpretation of the replacements and sometimes following
    bytes too, depending on whether the replacement lengths were even or
    odd, and on whether the active state of the ISO-2022-JP G0 character
    set was one-byte or two-byte. An example, with results transcribed in
    Unicode for readability:
    
    Input: ABC~¤•★星🌟星★•¤~XYZ
    
    Old output:
    Bytes: ABC~&#164;&#8226;␛$B!z@1&#127775;@1!z&#8226;&#164;␛(B~XYZ
    Meaning: ABC~&#164;&#8226;★星Γ渦祁卦酸院惕８臆胸Γ蔚柑~XYZ
    
    New output:
    Bytes: ABC~&#164;&#8226;␛$B!z@1␛(B&#127775;␛$B@1!z␛(B&#8226;&#164;~XYZ
    Meaning: ABC~&#164;&#8226;★星&#127775;星★&#8226;&#164;~XYZ
    
    Bug: 782565
    Change-Id: If2a7b76b99ce77cbec433af5384ed5c4d2e3c581
    Reviewed-on: https://chromium-review.googlesource.com/758405
    Commit-Queue: Benjamin Wiley Sittler <bsittler@chromium.org>
    Reviewed-by: Jungshik Shin <jungshik@google.com>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515425}

--- third_party/WebKit/Source/platform/wtf/text/TextCodecICUTest.cpp
+++ third_party/WebKit/Source/platform/wtf/text/TextCodecICUTest.cpp
@@ -20,5 +20,18 @@ TEST(TextCodecICUTest, IgnorableCodePoint) {
   CString encoded =
       codec->Encode(source.data(), source.size(), kEntitiesForUnencodables);
   EXPECT_STREQ("a&#8205;", encoded.data());
+  const String source2(u"ABC~¤•★星🌟星★•¤~XYZ");
+  const CString encoded2(codec->Encode(source2.GetCharacters<UChar>(),
+                                       source2.length(),
+                                       kEntitiesForUnencodables));
+  const String source3(u"ABC~&#164;&#8226;★星&#127775;星★&#8226;&#164;~XYZ");
+  const CString encoded3(codec->Encode(source3.GetCharacters<UChar>(),
+                                       source3.length(),
+                                       kEntitiesForUnencodables));
+  EXPECT_STREQ(encoded3.data(), encoded2.data());
+  EXPECT_STREQ(
+      "ABC~&#164;&#8226;\x1B$B!z@1\x1B(B&#127775;\x1B$B@1!z\x1B(B&#8226;&#164;~"
+      "XYZ",
+      encoded2.data());
 }
 }
