ca3408850227b86a3bc677eba16ff0687e86268f
Chris Mumford <cmumford@chromium.org>
2017-11-10 16:52:16 +0000

    IndexedDB: Added UMA value for destruction of corrupt leveldb.
    Added WebCore.IndexedDB.DestroyCorruptBackingStoreStatus to record the
    result of an attempt to destroy a corrupt (leveldb) backing store used
    by Indexed DB.
    
    Bug: 782972
    Change-Id: I13e9d64531c72d1d4f5193e2d682671d799f59a5
    Reviewed-on: https://chromium-review.googlesource.com/759616
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Commit-Queue: Chris Mumford <cmumford@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515574}

--- content/browser/indexed_db/indexed_db_factory_impl.cc
+++ content/browser/indexed_db/indexed_db_factory_impl.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/rand_util.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
@@ -540,8 +541,11 @@ void IndexedDBFactoryImpl::HandleBackingStoreCorruption(
   //       so our corruption info file will remain.
   leveldb::Status s =
       IndexedDBBackingStore::DestroyBackingStore(path_base, saved_origin);
-  if (!s.ok())
-    DLOG(ERROR) << "Unable to delete backing store: " << s.ToString();
+  DLOG_IF(ERROR, !s.ok()) << "Unable to delete backing store: " << s.ToString();
+  UMA_HISTOGRAM_ENUMERATION(
+      "WebCore.IndexedDB.DestroyCorruptBackingStoreStatus",
+      leveldb_env::GetLevelDBStatusUMAValue(s),
+      leveldb_env::LEVELDB_STATUS_MAX);
 }
 
 bool IndexedDBFactoryImpl::IsDatabaseOpen(const Origin& origin,
