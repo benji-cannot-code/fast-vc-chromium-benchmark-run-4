4fa1e10867f9e38bf17338ff7c6da939362966d6
Erik Chen <erikchen@chromium.org>
2017-11-10 18:15:53 +0000

    Omit malloc MDP metrics when OOP HP is enabled.
    When malloc is profiled by an external process, that process will be responsible
    for emitting metrics on behalf of this one. Thus, MallocDumpProvider should not
    do anything.
    
    Bug: 
    Change-Id: Ifc4c71467afe174404abeb03dd0f5e664352361b
    Reviewed-on: https://chromium-review.googlesource.com/760916
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Primiano Tucci <primiano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515601}

--- base/trace_event/malloc_dump_provider.h
+++ base/trace_event/malloc_dump_provider.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/singleton.h"
+#include "base/synchronization/lock.h"
 #include "base/threading/platform_thread.h"
 #include "base/trace_event/memory_dump_provider.h"
 #include "base/trace_event/sharded_allocation_register.h"
@@ -40,6 +41,12 @@ class BASE_EXPORT MallocDumpProvider : public MemoryDumpProvider {
   void InsertAllocation(void* address, size_t size);
   void RemoveAllocation(void* address);
 
+  // Used by out-of-process heap-profiling. When malloc is profiled by an
+  // external process, that process will be responsible for emitting metrics on
+  // behalf of this one. Thus, MallocDumpProvider should not do anything.
+  void EnableMetrics();
+  void DisableMetrics();
+
  private:
   friend struct DefaultSingletonTraits<MallocDumpProvider>;
 
@@ -53,6 +60,9 @@ class BASE_EXPORT MallocDumpProvider : public MemoryDumpProvider {
   // This is to prevent re-entrancy in the heap profiler when the heap dump
   // generation is malloc/new-ing for its own bookkeeping data structures.
   PlatformThreadId tid_dumping_heap_;
+
+  bool emit_metrics_on_memory_dump_ = true;
+  base::Lock emit_metrics_on_memory_dump_lock_;
 
   DISALLOW_COPY_AND_ASSIGN(MallocDumpProvider);
 };
