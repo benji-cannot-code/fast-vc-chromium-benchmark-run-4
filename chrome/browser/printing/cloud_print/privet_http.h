2352dc66f2023916d91cf3acb43b1b19a87213ba
rbpotter <rbpotter@chromium.org>
2017-11-10 18:14:12 +0000

    Fix cloud print device connection error
    This reverts changes made to chrome/browser/printing/cloud_print/* in
    https://chromium-review.googlesource.com/c/chromium/src/+/734700 and
    modifies PrivetPrinterHandler to work with the earlier version of the
    code.
    
    Bug: 782347
    Change-Id: Ib0389c5fb6203a6b3f72b61462a61fae8194e420
    Reviewed-on: https://chromium-review.googlesource.com/762376
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515600}

--- chrome/browser/printing/cloud_print/privet_http.h
+++ chrome/browser/printing/cloud_print/privet_http.h
@@ -33,8 +33,8 @@ class PrivetHTTPClient;
 class PrivetJSONOperation {
  public:
   // If value is null, the operation failed.
-  using ResultCallback =
-      base::OnceCallback<void(const base::DictionaryValue* /*value*/)>;
+  typedef base::RepeatingCallback<void(const base::DictionaryValue* /*value*/)>
+      ResultCallback;
 
   virtual ~PrivetJSONOperation() {}
 
@@ -53,7 +53,7 @@ class PrivetHTTPClient {
 
   // Creates operation to query basic information about local device.
   virtual std::unique_ptr<PrivetJSONOperation> CreateInfoOperation(
-      PrivetJSONOperation::ResultCallback callback) = 0;
+      const PrivetJSONOperation::ResultCallback& callback) = 0;
 
   // Creates a URL fetcher for PrivetV1.
   virtual std::unique_ptr<PrivetURLFetcher> CreateURLFetcher(
@@ -195,7 +195,7 @@ class PrivetV1HTTPClient {
 
   // Creates operation to query basic information about local device.
   virtual std::unique_ptr<PrivetJSONOperation> CreateInfoOperation(
-      PrivetJSONOperation::ResultCallback callback) = 0;
+      const PrivetJSONOperation::ResultCallback& callback) = 0;
 
   // Creates operation to register local device using Privet v1 protocol.
   virtual std::unique_ptr<PrivetRegisterOperation> CreateRegisterOperation(
@@ -204,7 +204,7 @@ class PrivetV1HTTPClient {
 
   // Creates operation to query capabilities of local printer.
   virtual std::unique_ptr<PrivetJSONOperation> CreateCapabilitiesOperation(
-      PrivetJSONOperation::ResultCallback callback) = 0;
+      const PrivetJSONOperation::ResultCallback& callback) = 0;
 
   // Creates operation to submit print job to local printer.
   virtual std::unique_ptr<PrivetLocalPrintOperation> CreateLocalPrintOperation(
