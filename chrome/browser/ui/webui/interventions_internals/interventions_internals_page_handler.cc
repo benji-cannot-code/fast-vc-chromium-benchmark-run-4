7f5f8ba8c146d3181973ea3ad7101ace8ffbc9f7
Doug Arnett <dougarnett@chromium.org>
2017-11-10 20:47:54 +0000

    Adds new NoScript previews type enabled status to internals page
    Bug: 782860
    Change-Id: I0f0fd341e4035ea0e1c777580ac263e39bbf61fc
    Reviewed-on: https://chromium-review.googlesource.com/764207
    Commit-Queue: Doug Arnett <dougarnett@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515670}

--- chrome/browser/ui/webui/interventions_internals/interventions_internals_page_handler.cc
+++ chrome/browser/ui/webui/interventions_internals/interventions_internals_page_handler.cc
@@ -14,11 +14,13 @@ namespace {
 // Key for status mapping.
 const char kAmpRedirectionPreviews[] = "ampPreviews";
 const char kClientLoFiPreviews[] = "clientLoFiPreviews";
+const char kNoScriptPreviews[] = "noScriptPreviews";
 const char kOfflinePreviews[] = "offlinePreviews";
 
 // Description for statuses.
 const char kAmpRedirectionDescription[] = "AMP Previews";
 const char kClientLoFiDescription[] = "Client LoFi Previews";
+const char kNoScriptDescription[] = "NoScript Previews";
 const char kOfflineDesciption[] = "Offline Previews";
 
 }  // namespace
@@ -112,6 +114,11 @@ void InterventionsInternalsPageHandler::GetPreviewsEnabled(
   client_lofi_status->description = kClientLoFiDescription;
   client_lofi_status->enabled = previews::params::IsClientLoFiEnabled();
   statuses[kClientLoFiPreviews] = std::move(client_lofi_status);
+
+  auto noscript_status = mojom::PreviewsStatus::New();
+  noscript_status->description = kNoScriptDescription;
+  noscript_status->enabled = previews::params::IsNoScriptPreviewsEnabled();
+  statuses[kNoScriptPreviews] = std::move(noscript_status);
 
   auto offline_status = mojom::PreviewsStatus::New();
   offline_status->description = kOfflineDesciption;
