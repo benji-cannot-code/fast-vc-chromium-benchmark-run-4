2220023d5aa3009eb04ce165fd1395c5f165665a
Emily Stark <estark@google.com>
2017-11-10 19:24:52 +0000

    Add a chrome://net-internals button for sending test Expect-CT reports
    Expect-CT reports can be difficult for server operators to trigger and
    debug. This CL adds dummy report functionality to the Expect-CT section of
    chrome://net-internals#hsts, to allow site operators to trigger test reports to
    a given report-uri.
    
    Bug: 781868
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I7adbd44c1cc7b4720522c02ccecc54a26ce0e30e
    Reviewed-on: https://chromium-review.googlesource.com/756274
    Commit-Queue: Emily Stark <estark@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515631}

--- chrome/browser/resources/net_internals/browser_bridge.js
+++ chrome/browser/resources/net_internals/browser_bridge.js
@@ -185,6 +185,10 @@ var BrowserBridge = (function() {
       this.send('expectCTAdd', [domain, report_uri, enforce]);
     },
 
+    sendExpectCTTestReport: function(report_uri) {
+      this.send('expectCTTestReport', [report_uri]);
+    },
+
     sendGetSessionNetworkStats: function() {
       this.send('getSessionNetworkStats');
     },
@@ -330,6 +334,11 @@ var BrowserBridge = (function() {
     receivedExpectCTResult: function(info) {
       for (var i = 0; i < this.expectCTObservers_.length; i++)
         this.expectCTObservers_[i].onExpectCTQueryResult(info);
+    },
+
+    receivedExpectCTTestReportResult: function(result) {
+      for (var i = 0; i < this.expectCTObservers_.length; i++)
+        this.expectCTObservers_[i].onExpectCTTestReportResult(result);
     },
 
     receivedONCFileParse: function(error) {
