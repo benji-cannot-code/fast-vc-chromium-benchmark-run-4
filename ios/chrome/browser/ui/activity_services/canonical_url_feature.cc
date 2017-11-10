537d4e414d2723eea21495a87db66ff7c67d644e
Gregory Chatzinoff <gchatz@chromium.org>
2017-11-10 01:04:13 +0000

    Enable Canonical URL Sharing by default.
    This CL enables canonical URL sharing by default. This way, a Finch
    configuration is not needed unless the kill switch is required.
    
    Bug: none
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I0cfd53eac3f62c006d55242d8a05fe5ee575ecb8
    Reviewed-on: https://chromium-review.googlesource.com/761766
    Reviewed-by: Peter Lee <pkl@chromium.org>
    Commit-Queue: Gregory Chatzinoff <gchatz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515396}

--- ios/chrome/browser/ui/activity_services/canonical_url_feature.cc
+++ ios/chrome/browser/ui/activity_services/canonical_url_feature.cc
@@ -8,6 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace activity_services {
 
 const base::Feature kShareCanonicalURL{"ShareCanonicalURLIOS",
-                                       base::FEATURE_DISABLED_BY_DEFAULT};
+                                       base::FEATURE_ENABLED_BY_DEFAULT};
 
 }  // namespace activity_services
