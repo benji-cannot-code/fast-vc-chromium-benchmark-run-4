fde729f73a2339cdab5b6d8bf85abd2752b4f5af
bsheedy <bsheedy@chromium.org>
2017-11-10 18:20:50 +0000

    Add missing @Overrides to TestVrShellDelegate
    Adds a few @Overrides to TestVrShellDelegate that were previously
    missing and causing compilation warnings.
    
    Bug: 
    Change-Id: I8d185eba1a10d6f0064d73709be26019fae4054b
    Reviewed-on: https://chromium-review.googlesource.com/761437
    Reviewed-by: Yash Malik <ymalik@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515605}

--- chrome/android/javatests/src/org/chromium/chrome/browser/vr_shell/TestVrShellDelegate.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/vr_shell/TestVrShellDelegate.java
@@ -50,6 +50,7 @@ public class TestVrShellDelegate extends VrShellDelegate {
         return VrShellDelegate.getVrShell();
     }
 
+    @Override
     public void shutdownVr(boolean disableVrMode, boolean stayingInChrome) {
         super.shutdownVr(disableVrMode, stayingInChrome);
     }
@@ -66,14 +67,17 @@ public class TestVrShellDelegate extends VrShellDelegate {
         super.setFeedbackFrequency(frequency);
     }
 
+    @Override
     public boolean isListeningForWebVrActivate() {
         return super.isListeningForWebVrActivate();
     }
 
+    @Override
     public boolean isClearActivatePending() {
         return super.isClearActivatePending();
     }
 
+    @Override
     public boolean isVrEntryComplete() {
         return super.isVrEntryComplete();
     }
