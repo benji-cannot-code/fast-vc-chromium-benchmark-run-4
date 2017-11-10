cabebcdf92669681eca4504f1cc574cef26cd388
Victor Costan <pwnall@chromium.org>
2017-11-10 00:57:10 +0000

    Async Cookies: Blink-side implementation of the document API.
    This CL implements a subset of cookie filtering in
    RestrictedCookieManager::GetAllForUrl. The implementation is the minimum
    needed to verify that CookieStore.{get,getAll,has} passes matchType
    correctly to the backend.
    
    Bug: 729800
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I02f9dcf624ef47e0f8e79d68fb51fae27b1c489d
    Reviewed-on: https://chromium-review.googlesource.com/756093
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Reviewed-by: Randy Smith <rdsmith@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515388}

--- content/network/restricted_cookie_manager.cc
+++ content/network/restricted_cookie_manager.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/memory/weak_ptr.h"
 #include "base/sequenced_task_runner.h"
+#include "base/strings/string_util.h"
 #include "base/threading/sequenced_task_runner_handle.h"
 #include "mojo/public/cpp/bindings/message.h"
 #include "net/base/registry_controlled_domains/registry_controlled_domain.h"
@@ -75,10 +76,23 @@ void RestrictedCookieManager::CookieListToGetAllForUrlCallback(
 
   std::vector<net::CanonicalCookie> result;
   result.reserve(cookie_list.size());
+  network::mojom::CookieMatchType match_type = options->match_type;
+  const std::string& match_name = options->name;
   for (size_t i = 0; i < cookie_list.size(); ++i) {
-    // TODO(pwnall): Parsing and canonicalization for net::CanonicalCookie.
     const net::CanonicalCookie& cookie = cookie_list[i];
-    // TODO(pwnall): Check cookie against options.
+    const std::string& cookie_name = cookie.Name();
+
+    if (match_type == network::mojom::CookieMatchType::EQUALS) {
+      if (cookie_name != match_name)
+        continue;
+    } else if (match_type == network::mojom::CookieMatchType::STARTS_WITH) {
+      if (!base::StartsWith(cookie_name, match_name,
+                            base::CompareCase::SENSITIVE)) {
+        continue;
+      }
+    } else {
+      NOTREACHED();
+    }
     result.emplace_back(cookie);
   }
   std::move(callback).Run(std::move(result));
