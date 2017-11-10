00800b6d6f4252b653c51406c8fe828c5311baea
Peter Boström <pbos@chromium.org>
2017-11-10 00:46:57 +0000

    Style views::MessageBoxView for Harmony.
    Introduces CONTEXT_MESSAGE_BOX_BODY_TEXT and styles it like
    CONTEXT_BODY_TEXT_LARGE and STYLE_SECONDARY under Harmony. This
    specifically styles the JavaScript dialogs.
    
    Bug: chromium:652015, chromium:652017, chromium:683346
    Change-Id: I846c33736cfffb368105ea1cd49c2a8079e20b14
    Reviewed-on: https://chromium-review.googlesource.com/759409
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515378}

--- chrome/browser/ui/views/harmony/harmony_typography_provider.cc
+++ chrome/browser/ui/views/harmony/harmony_typography_provider.cc
@@ -96,6 +96,7 @@ int HarmonyTypographyProvider::GetPlatformFontHeight(int font_context) {
     case views::style::CONTEXT_DIALOG_TITLE:
       return windows_10 || !direct_write_enabled ? 20 : 21;
     case CONTEXT_BODY_TEXT_LARGE:
+    case views::style::CONTEXT_MESSAGE_BOX_BODY_TEXT:
       return direct_write_enabled ? 18 : 17;
     case CONTEXT_BODY_TEXT_SMALL:
       return windows_10 && direct_write_enabled ? 16 : 15;
@@ -130,6 +131,7 @@ const gfx::FontList& HarmonyTypographyProvider::GetFont(int context,
       size_delta = kTitleSize - gfx::PlatformFont::kDefaultBaseFontSize;
       break;
     case CONTEXT_BODY_TEXT_LARGE:
+    case views::style::CONTEXT_MESSAGE_BOX_BODY_TEXT:
       size_delta = kBodyTextLargeSize - gfx::PlatformFont::kDefaultBaseFontSize;
       break;
     case CONTEXT_HEADLINE:
@@ -163,6 +165,12 @@ SkColor HarmonyTypographyProvider::GetColor(
     }
   }
 
+  // Use the secondary style instead of primary for message box body text.
+  if (context == views::style::CONTEXT_MESSAGE_BOX_BODY_TEXT &&
+      style == views::style::STYLE_PRIMARY) {
+    style = STYLE_SECONDARY;
+  }
+
   switch (style) {
     case views::style::STYLE_DIALOG_BUTTON_DEFAULT:
       return SK_ColorWHITE;
@@ -178,6 +186,7 @@ SkColor HarmonyTypographyProvider::GetColor(
     case STYLE_GREEN:
       return gfx::kGoogleGreen700;
   }
+
   return SkColorSetRGB(0x21, 0x21, 0x21);  // Primary for everything else.
 }
 
@@ -244,6 +253,7 @@ int HarmonyTypographyProvider::GetLineHeight(int context, int style) const {
     case views::style::CONTEXT_DIALOG_TITLE:
       return title_height;
     case CONTEXT_BODY_TEXT_LARGE:
+    case views::style::CONTEXT_MESSAGE_BOX_BODY_TEXT:
     case views::style::CONTEXT_TABLE_ROW:
       return body_large_height;
     case CONTEXT_HEADLINE:
