8497c6d582b1ced0353620c793ad2ad522713f44
Lily Houghton <lilyhoughton@chromium.org>
2017-11-10 03:40:33 +0000

    [Cronet] Define CronetMetrics class implementing the iOS NSURLSessionTaskTransactionMetrics interface
    This creates a class |CronetMetrics| which provides setters for all of the propeties defined in
    NSURLSessionTaskTransactionMetrics.
    
    Bug: 702796
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Icdcc917001c0ceb20107e2f128dba88259330f78
    Reviewed-on: https://chromium-review.googlesource.com/744401
    Commit-Queue: Lily Houghton <lilyhoughton@chromium.org>
    Reviewed-by: Misha Efimov <mef@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515444}

---
+++ components/cronet/ios/cronet_metrics.h
@@ -0,0 +1,39 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_CRONET_IOS_CRONET_METRICS_H_
+#define COMPONENTS_CRONET_IOS_CRONET_METRICS_H_
+
+#import <Foundation/Foundation.h>
+
+#include "components/grpc_support/include/bidirectional_stream_c.h"
+
+FOUNDATION_EXPORT GRPC_SUPPORT_EXPORT NS_AVAILABLE_IOS(10.0)
+@interface CronetMetrics : NSURLSessionTaskTransactionMetrics
+
+@property(copy, readwrite) NSURLRequest* request;
+@property(copy, readwrite) NSURLResponse* response;
+
+@property(copy, readwrite) NSDate* fetchStartDate;
+@property(copy, readwrite) NSDate* domainLookupStartDate;
+@property(copy, readwrite) NSDate* domainLookupEndDate;
+@property(copy, readwrite) NSDate* connectStartDate;
+@property(copy, readwrite) NSDate* secureConnectionStartDate;
+@property(copy, readwrite) NSDate* secureConnectionEndDate;
+@property(copy, readwrite) NSDate* connectEndDate;
+@property(copy, readwrite) NSDate* requestStartDate;
+@property(copy, readwrite) NSDate* requestEndDate;
+@property(copy, readwrite) NSDate* responseStartDate;
+@property(copy, readwrite) NSDate* responseEndDate;
+
+@property(copy, readwrite) NSString* networkProtocolName;
+@property(assign, readwrite, getter=isProxyConnection) BOOL proxyConnection;
+@property(assign, readwrite, getter=isReusedConnection) BOOL reusedConnection;
+@property(assign, readwrite)
+    NSURLSessionTaskMetricsResourceFetchType resourceFetchType;
+
+@end
+
+#endif // COMPONENTS_CRONET_IOS_CRONET_METRICS_H_
---
+++ components/cronet/ios/cronet_metrics.mm
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "components/cronet/ios/cronet_metrics.h"
+
+@implementation CronetMetrics
+
+@synthesize request = _request;
+@synthesize response = _response;
+
+@synthesize fetchStartDate = _fetchStartDate;
+@synthesize domainLookupStartDate = _domainLookupStartDate;
+@synthesize domainLookupEndDate = _domainLookupEndDate;
+@synthesize connectStartDate = _connectStartDate;
+@synthesize secureConnectionStartDate = _secureConnectionStartDate;
+@synthesize secureConnectionEndDate = _secureConnectionEndDate;
+@synthesize connectEndDate = _connectEndDate;
+@synthesize requestStartDate = _requestStartDate;
+@synthesize requestEndDate = _requestEndDate;
+@synthesize responseStartDate = _responseStartDate;
+@synthesize responseEndDate = _responseEndDate;
+
+@synthesize networkProtocolName = _networkProtocolName;
+@synthesize proxyConnection = _proxyConnection;
+@synthesize reusedConnection = _reusedConnection;
+@synthesize resourceFetchType = _resourceFetchType;
+
+@end
