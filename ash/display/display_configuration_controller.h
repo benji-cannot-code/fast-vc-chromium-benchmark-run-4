7306c70ec856f9cced6d14a9273a1103906deebd
Ahmed Fakhry <afakhry@google.com>
2017-11-10 22:43:28 +0000

    Unified Desktop: CL_3: Hookup the display APIs to the unified matrix support
    Make the chrome.system.display setDisplayLayout() and getDisplayLayout()
    work while in Unified Desktop mode, converting the given layout (if valid)
    to a display matrix and vice versa.
    
    BUG=752767
    TEST=Covered by tests
    
    
    Change-Id: I55d498ca0fd203c635036eb4bfb9eaa074723d77
    Reviewed-on: https://chromium-review.googlesource.com/749690
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515721}

--- ash/display/display_configuration_controller.h
+++ ash/display/display_configuration_controller.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "ui/display/display.h"
+#include "ui/display/unified_desktop_utils.h"
 
 namespace display {
 class DisplayLayout;
@@ -47,9 +48,14 @@ class ASH_EXPORT DisplayConfigurationController
   ~DisplayConfigurationController() override;
 
   // Sets the layout for the current displays with a fade in/out
-  // animation. Currently |display_id| is assumed to be the secondary
-  // display.  TODO(oshima/stevenjb): Support 3+ displays.
+  // animation.
   void SetDisplayLayout(std::unique_ptr<display::DisplayLayout> layout);
+
+  // This should be called instead of SetDisplayLayout() to set the display
+  // layout in the case of Unified Desktop mode. A fade in/out animation is
+  // used as well.
+  void SetUnifiedDesktopLayoutMatrix(
+      const display::UnifiedDesktopLayoutMatrix& matrix);
 
   // Sets the mirror mode with a fade-in/fade-out animation. Affects all
   // displays.
@@ -91,6 +97,8 @@ class ASH_EXPORT DisplayConfigurationController
   void SetDisplayLayoutImpl(std::unique_ptr<display::DisplayLayout> layout);
   void SetMirrorModeImpl(bool mirror);
   void SetPrimaryDisplayIdImpl(int64_t display_id);
+  void SetUnifiedDesktopLayoutMatrixImpl(
+      const display::UnifiedDesktopLayoutMatrix& matrix);
 
   // Returns the ScreenRotationAnimator associated with the |display_id|'s
   // |root_window|. If there is no existing ScreenRotationAnimator for
