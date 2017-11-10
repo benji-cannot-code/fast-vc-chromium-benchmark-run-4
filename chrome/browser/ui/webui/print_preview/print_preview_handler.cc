14301222f56c7caf059c0d188137f45532cd089c
Lei Zhang <thestig@chromium.org>
2017-11-10 02:00:36 +0000

    Simplify/constify code in PrintPreviewHandler and friends.
    Change-Id: Ie5526d13b4126f317e269b32f3aed06aa04b6428
    Reviewed-on: https://chromium-review.googlesource.com/758887
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515417}

--- chrome/browser/ui/webui/print_preview/print_preview_handler.cc
+++ chrome/browser/ui/webui/print_preview/print_preview_handler.cc
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/memory/ref_counted_memory.h"
 #include "base/metrics/histogram_macros.h"
-#include "base/threading/thread.h"
 #include "base/values.h"
 #include "build/build_config.h"
 #include "chrome/browser/app_mode/app_mode_utils.h"
@@ -63,9 +62,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/core/browser/profile_oauth2_token_service.h"
 #include "components/signin/core/browser/signin_manager.h"
 #include "content/public/browser/browser_context.h"
-#include "content/public/browser/browser_thread.h"
-#include "content/public/browser/navigation_controller.h"
-#include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/web_contents.h"
@@ -85,7 +81,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/printing/printer_configuration.h"
 #endif
 
-using content::BrowserThread;
 using content::RenderFrameHost;
 using content::WebContents;
 using printing::PrintViewManager;
@@ -579,7 +574,7 @@ void PrintPreviewHandler::HandleGetPreview(const base::ListValue* args) {
   ++regenerate_preview_request_count_;
 
   WebContents* initiator = GetInitiator();
-  content::RenderFrameHost* rfh =
+  RenderFrameHost* rfh =
       initiator
           ? PrintViewManager::FromWebContents(initiator)->print_preview_rfh()
           : nullptr;
@@ -598,17 +593,13 @@ void PrintPreviewHandler::HandleGetPreview(const base::ListValue* args) {
   if (display_header_footer) {
     settings->SetString(printing::kSettingHeaderFooterTitle,
                         initiator->GetTitle());
-    std::string url;
-    content::NavigationEntry* entry =
-        initiator->GetController().GetLastCommittedEntry();
-    if (entry) {
-      url::Replacements<char> url_sanitizer;
-      url_sanitizer.ClearUsername();
-      url_sanitizer.ClearPassword();
 
-      url = entry->GetVirtualURL().ReplaceComponents(url_sanitizer).spec();
-    }
-    settings->SetString(printing::kSettingHeaderFooterURL, url);
+    url::Replacements<char> url_sanitizer;
+    url_sanitizer.ClearUsername();
+    url_sanitizer.ClearPassword();
+    const GURL& initiator_url = initiator->GetLastCommittedURL();
+    settings->SetString(printing::kSettingHeaderFooterURL,
+                        initiator_url.ReplaceComponents(url_sanitizer).spec());
   }
 
   bool generate_draft_data = false;
