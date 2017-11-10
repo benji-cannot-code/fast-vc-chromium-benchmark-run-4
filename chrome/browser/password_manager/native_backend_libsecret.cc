1d288990b9e302f337cfea3b1c50fa8254312ee1
Christos Froussios <cfroussios@chromium.org>
2017-11-10 10:22:24 +0000

    🔐 Call g_object_unref on lists returned by secret_service_search
    We currently do not seem to be properly deleting SecretItems.
    
    The destruction of SecretItems is undocumented, but g_object_unref
    appears to have been used in other codebases for this purpose.
    
    Bug: 393395
    Change-Id: I046911136bac266c0e1a42b09a8a0cb79b678f4a
    Reviewed-on: https://chromium-review.googlesource.com/758851
    Reviewed-by: Vaclav Brozek <vabr@chromium.org>
    Commit-Queue: Christos Froussios <cfroussios@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515522}

--- chrome/browser/password_manager/native_backend_libsecret.cc
+++ chrome/browser/password_manager/native_backend_libsecret.cc
@@ -333,7 +333,7 @@ bool NativeBackendLibsecret::AddUpdateLoginSearch(
     LOG(ERROR) << "Unable to get logins " << error->message;
     g_error_free(error);
     if (found)
-      g_list_free(found);
+      g_list_free_full(found, &g_object_unref);
     return false;
   }
 
@@ -447,7 +447,7 @@ bool NativeBackendLibsecret::GetLoginsList(
     LOG(ERROR) << "Unable to get logins " << error->message;
     g_error_free(error);
     if (found)
-      g_list_free(found);
+      g_list_free_full(found, &g_object_unref);
     return false;
   }
 
@@ -583,6 +583,6 @@ NativeBackendLibsecret::ConvertFormList(
                                   : password_manager::PSL_DOMAIN_MATCH_NOT_USED,
                               password_manager::PSL_DOMAIN_MATCH_COUNT);
   }
-  g_list_free(found);
+  g_list_free_full(found, &g_object_unref);
   return forms;
 }
