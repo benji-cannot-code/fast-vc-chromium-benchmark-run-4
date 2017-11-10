4d25838fbcda70d48e4005239f656a4350299f20
Alex Vallée <avallee@chromium.org>
2017-11-10 21:47:48 +0000

    [OriginTrials] Remove junk comment in copyright comment.
    Accidentally added in http://crrev.com/508197/
    
    Bug: 
    Change-Id: I7c7e1e06b50ff381789682b9ea778085b46fb74f
    Reviewed-on: https://chromium-review.googlesource.com/764448
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Commit-Queue: Alex Vallee <avallee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515696}

--- third_party/WebKit/Source/core/origin_trials/OriginTrialContext.cpp
+++ third_party/WebKit/Source/core/origin_trials/OriginTrialContext.cpp
@@ -1,6 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2015 The Chromium Authors. All rights reserved.
-// // TODO(https://crbug.com/738505): Delete the validator.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
