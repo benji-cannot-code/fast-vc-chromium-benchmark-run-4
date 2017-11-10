7ec5cfe4fb1114fb1f7ae6bb2512d487d1cbbf25
Allen Bauer <kylixrd@chromium.org>
2017-11-10 15:47:03 +0000

    Make sure the auto signin prompt is snapped to at least 320. Fix browser dialog test to show auto signin prompt.
    Bug: 651682
    Change-Id: I9da1c20d416df5f12900b9f94acf962691ea93b8
    Reviewed-on: https://chromium-review.googlesource.com/761417
    Reviewed-by: Vaclav Brozek <vabr@chromium.org>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515559}

--- chrome/browser/ui/views/passwords/manage_passwords_bubble_view.h
+++ chrome/browser/ui/views/passwords/manage_passwords_bubble_view.h
@@ -75,6 +75,9 @@ class ManagePasswordsBubbleView : public LocationBarBubbleDelegateView,
 
   ~ManagePasswordsBubbleView() override;
 
+  // DialogDelegate:
+  bool ShouldSnapFrameWidth() const override;
+
   // LocationBarBubbleDelegateView:
   int GetDialogButtons() const override;
   views::View* GetInitiallyFocusedView() override;
