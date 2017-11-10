90930ff3fb0c0ce9f0c6013848211ada7a7d03a2
Francois Doray <fdoray@chromium.org>
2017-11-10 20:51:21 +0000

    Add ui::PropertyChangeReason argument to ui::LayerDelegate methods.
    With this CL,
    ui::LayerDelegate::OnLayer(BoundsChanged|OpacityChanged|Transformed)()
    have a new argument that indicates whether the property was set
    directly or by an animation.
    
    The goal is to pass this argument to aura::WindowObserver so that
    the upcoming aura::WindowOcclusionTracker can know when window
    animations start without calling
    ui::LayerAnimator::IsAnimatingOnePropertyOf().
    
    TBR=sadrul@chromium.org
    
    Bug: 738387
    Change-Id: I35bf33de5eb7dee50864ef8ad8258c18fe8945f5
    Reviewed-on: https://chromium-review.googlesource.com/753503
    Commit-Queue: François Doray <fdoray@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515674}

--- ui/compositor/layer_delegate.cc
+++ ui/compositor/layer_delegate.cc
@@ -7,11 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ui {
 
-void LayerDelegate::OnLayerBoundsChanged(const gfx::Rect& old_bounds) {}
+void LayerDelegate::OnLayerBoundsChanged(const gfx::Rect& old_bounds,
+                                         PropertyChangeReason reason) {}
 
-void LayerDelegate::OnLayerTransformed() {}
+void LayerDelegate::OnLayerTransformed(PropertyChangeReason reason) {}
 
-void LayerDelegate::OnLayerOpacityChanged(float old_opacity,
-                                          float new_opacity) {}
+void LayerDelegate::OnLayerOpacityChanged(PropertyChangeReason reason) {}
 
 }  // namespace ui
