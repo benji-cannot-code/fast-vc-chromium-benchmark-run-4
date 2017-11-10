f899692ed4174d07b633b7de718fcbc1fa7a9f12
Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
2017-11-10 06:44:58 +0000

    Enable Zip Archiver for unpacking by default.
    Zip Archiver is a new component extension that handles zipping and
    unzipping. After this change, Files app. will use Zip Archiver for
    unzipping zip files. It will also allow to unzip a zip file in the
    Google Drive volume or other non-local places.
    
    Bug: 783200
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I3851c8c801d58a7573632e664b4fc2fca92acb83
    Reviewed-on: https://chromium-review.googlesource.com/758696
    Reviewed-by: Naoki Fukino <fukino@chromium.org>
    Commit-Queue: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515487}

--- chromeos/chromeos_switches.cc
+++ chromeos/chromeos_switches.cc
@@ -625,9 +625,9 @@ bool IsVoiceInteractionEnabled() {
 }
 
 bool IsZipArchiverUnpackerEnabled() {
-  // Disabled by default.
-  return base::CommandLine::ForCurrentProcess()->HasSwitch(
-      kEnableZipArchiverUnpacker);
+  // Enabled by default.
+  return !base::CommandLine::ForCurrentProcess()->HasSwitch(
+      kDisableZipArchiverUnpacker);
 }
 
 }  // namespace switches
