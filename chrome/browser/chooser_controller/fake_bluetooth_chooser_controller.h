7e74a7982dd07f9f3f8b7d1177f95da283901e41
Bret Sepulveda <bsep@chromium.org>
2017-11-11 03:18:03 +0000

    Refactor DeviceChooserContentViewTest and ChooserDialogViewTest.
    Reduced redundant test expectations and generally made the tests more
    terse in preparation for the chooser dialogs' redesign.
    
    Bug: 654137
    Change-Id: Ifda8645378103d498bb869f8ab5589f6cb66e5db
    Reviewed-on: https://chromium-review.googlesource.com/731472
    Reviewed-by: Jun Cai <juncai@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515814}

--- chrome/browser/chooser_controller/fake_bluetooth_chooser_controller.h
+++ chrome/browser/chooser_controller/fake_bluetooth_chooser_controller.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_CHOOSER_CONTROLLER_FAKE_BLUETOOTH_CHOOSER_CONTROLLER_H_
 #define CHROME_BROWSER_CHOOSER_CONTROLLER_FAKE_BLUETOOTH_CHOOSER_CONTROLLER_H_
 
+#include <string>
+
 #include "base/macros.h"
 #include "base/strings/string16.h"
 #include "chrome/browser/chooser_controller/chooser_controller.h"
@@ -40,6 +42,7 @@ class FakeBluetoothChooserController : public ChooserController {
   static constexpr int kSignalStrengthLevel4 = 4;
 
   struct FakeDevice {
+    std::string name;
     bool connected;
     bool paired;
     int signal_strength;
