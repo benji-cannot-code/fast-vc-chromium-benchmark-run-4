20d7dfd30fa3df4665cc22a6ffad927e2cbe3b69
Yuzhu Shen <yzshen@chromium.org>
2017-11-11 02:01:09 +0000

    Move the mojo JS bindings resource into a separate grd file in mojo/pubilc/js.
    So that the resource could be used from both WebUI and extensions.
    
    BUG=699569, 718047
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation;master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: If2ce60b15e0351aba801746cd863e5a2b9e85485
    Reviewed-on: https://chromium-review.googlesource.com/762393
    Commit-Queue: Yuzhu Shen <yzshen@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515792}

--- content/browser/webui/shared_resources_data_source.cc
+++ content/browser/webui/shared_resources_data_source.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_thread.h"
 #include "content/public/common/content_client.h"
 #include "content/public/common/url_constants.h"
+#include "mojo/public/js/grit/mojo_bindings_resources.h"
 #include "ui/base/layout.h"
 #include "ui/base/webui/web_ui_util.h"
 #include "ui/resources/grit/webui_resources.h"
@@ -46,7 +47,7 @@ const struct {
   const char* const path;
   const int resource_id;
 } kAdditionalResourceMapEntries[] = {
-    {"js/mojo_bindings.js", IDR_WEBUI_MOJO_BINDINGS_JS},
+    {"js/mojo_bindings.js", IDR_MOJO_BINDINGS_JS},
 };
 
 void AddResource(const std::string& path,
