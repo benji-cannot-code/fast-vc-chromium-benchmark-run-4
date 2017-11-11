2a510443596bb11c410c26db59ba5b76fe7a6e90
Joel Einbinder <einbinder@chromium.org>
2017-11-11 01:26:52 +0000

    DevTools: Placeholder text for CodeMirrorTextEditor
    Bug: none
    Change-Id: If3ecb842a31ed70f2dbea29b910c286a60d67bf3
    Reviewed-on: https://chromium-review.googlesource.com/671202
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Commit-Queue: Joel Einbinder <einbinder@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515779}

--- third_party/WebKit/Source/devtools/front_end/externs.js
+++ third_party/WebKit/Source/devtools/front_end/externs.js
@@ -532,7 +532,7 @@ CodeMirror.prototype = {
   undo: function() {},
   unlinkDoc: function(other) {}
 };
-/** @type {!{cursorDiv: Element}} */
+/** @type {!{cursorDiv: Element, lineSpace: Element}} */
 CodeMirror.prototype.display;
 /** @type {!{mode: string}} */
 CodeMirror.prototype.options;
