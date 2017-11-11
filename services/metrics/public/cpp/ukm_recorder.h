1eb8b18451b9535c3eedea27e7286e2c99698edf
Nicholas Verne <nverne@chromium.org>
2017-11-11 11:36:25 +0000

    Revert "mojo IsInternalPluginAvailableForMimeType"
    This reverts commit 729f299a3e3ee1d7ac210411b092822e646c5b8c.
    
    Reason for revert: broken for buildflag WIDEVINE_CDM_AVAILABLE_NOT_COMPONENT
    
    Original change's description:
    > mojo IsInternalPluginAvailableForMimeType
    > 
    > We can now rename PluginInfoMessageFilter to PluginInfoHostImpl.
    > 
    > Bug: 577685
    > Change-Id: If95632401e048bbf7f756912dd59e6c399f433c3
    > Reviewed-on: https://chromium-review.googlesource.com/759621
    > Reviewed-by: Will Harris <wfh@chromium.org>
    > Reviewed-by: Sam McNally <sammc@chromium.org>
    > Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    > Commit-Queue: Nicholas Verne <nverne@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#515838}
    
    TBR=jam@chromium.org,sammc@chromium.org,wfh@chromium.org,nverne@chromium.org
    
    Change-Id: I3827db4165faa7d513de11f97235898768d9c629
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 577685
    Reviewed-on: https://chromium-review.googlesource.com/765387
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Commit-Queue: Nicholas Verne <nverne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515839}

--- services/metrics/public/cpp/ukm_recorder.h
+++ services/metrics/public/cpp/ukm_recorder.h
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class ContextualSearchRankerLoggerImpl;
 class DocumentWritePageLoadMetricsObserver;
 class FromGWSPageLoadMetricsLogger;
-class PluginInfoHostImpl;
+class PluginInfoMessageFilter;
 class ServiceWorkerPageLoadMetricsObserver;
 class SubresourceFilterMetricsObserver;
 class UkmPageLoadMetricsObserver;
@@ -91,7 +91,7 @@ class METRICS_EXPORT UkmRecorder {
   friend DocumentWritePageLoadMetricsObserver;
   friend FromGWSPageLoadMetricsLogger;
   friend LocalNetworkRequestsPageLoadMetricsObserver;
-  friend PluginInfoHostImpl;
+  friend PluginInfoMessageFilter;
   friend ServiceWorkerPageLoadMetricsObserver;
   friend SubresourceFilterMetricsObserver;
   friend TestRecordingHelper;
