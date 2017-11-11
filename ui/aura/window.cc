ff9ecdfa238cfc4bc78bb7f4a666ad13ee2fcdb2
Ria Jiang <riajiang@chromium.org>
2017-11-11 06:48:33 +0000

    Set hit-test data child surface flag based on embed_frame_sink_id_.
    This CL moves |embed_frame_sink_id_| from WindowPortMus to aura::Window.
    When a window is an embed window, |embed_frame_sink_id_| gets set from
    WindowPortMus(WindowServer) or RenderWidgetHostViewAura. It adds
    IsEmbeddingClient() in aura::Window to determine if a window should be
    a child surface based on that.
    
    Bug: 732395
    Test: aura_unittests
    Change-Id: I4f25491b55a031cd10a3440c9066abcdf607ff39
    Reviewed-on: https://chromium-review.googlesource.com/749502
    Commit-Queue: Ria Jiang <riajiang@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515832}

--- ui/aura/window.cc
+++ ui/aura/window.cc
@@ -1046,6 +1046,10 @@ viz::FrameSinkId Window::GetFrameSinkId() const {
   return port_->GetFrameSinkId();
 }
 
+bool Window::IsEmbeddingClient() const {
+  return embed_frame_sink_id_.is_valid();
+}
+
 void Window::OnPaintLayer(const ui::PaintContext& context) {
   Paint(context);
 }
