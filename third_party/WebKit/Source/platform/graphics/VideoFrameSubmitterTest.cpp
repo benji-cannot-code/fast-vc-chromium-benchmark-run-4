20ea3e165074f809045dbaaeb8e66331c0f11fe3
CJ DiMeglio <lethalantidote@chromium.org>
2017-11-11 02:22:34 +0000

    Allows VideoFrameSubmitter to return resources.
    The VideoFrameSubmitter will signal to the VideoFrameResourceProvider to
    PrepareSendToCompositor resources. When the compositor is done with
    these resources, they must be returned.
    
    Bug: 746182
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I90ac8fded8eb74b7435cd7763e4fa1cf8e4a3a50
    Reviewed-on: https://chromium-review.googlesource.com/723862
    Commit-Queue: CJ DiMeglio <lethalantidote@chromium.org>
    Reviewed-by: enne <enne@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515801}

--- third_party/WebKit/Source/platform/graphics/VideoFrameSubmitterTest.cpp
+++ third_party/WebKit/Source/platform/graphics/VideoFrameSubmitterTest.cpp
@@ -42,7 +42,6 @@ class MockVideoFrameProvider : public cc::VideoFrameProvider {
   MOCK_METHOD0(HasCurrentFrame, bool());
   MOCK_METHOD0(GetCurrentFrame, scoped_refptr<media::VideoFrame>());
   MOCK_METHOD0(PutCurrentFrame, void());
-  MOCK_METHOD0(task_runner, scoped_refptr<base::SingleThreadTaskRunner>());
 
  private:
   DISALLOW_COPY_AND_ASSIGN(MockVideoFrameProvider);
@@ -89,6 +88,12 @@ class MockVideoFrameResourceProvider
 
   MOCK_METHOD1(Initialize, void(viz::ContextProvider*));
   MOCK_METHOD1(AppendQuads, void(viz::RenderPass*));
+  MOCK_METHOD2(PrepareSendToParent,
+               void(const cc::LayerTreeResourceProvider::ResourceIdArray&,
+                    std::vector<viz::TransferableResource>*));
+  MOCK_METHOD1(
+      ReceiveReturnsFromParent,
+      void(const std::vector<viz::ReturnedResource>& transferable_resources));
   MOCK_METHOD0(ObtainContextProvider, void());
 
  private:
@@ -178,6 +183,7 @@ TEST_F(VideoFrameSubmitterTest,
   EXPECT_CALL(*provider_, PutCurrentFrame());
   EXPECT_CALL(*sink_, SetNeedsBeginFrame(false));
   EXPECT_CALL(*resource_provider_, AppendQuads(_));
+  EXPECT_CALL(*resource_provider_, PrepareSendToParent(_, _));
 
   submitter_->StopUsingProvider();
 
@@ -214,6 +220,7 @@ TEST_F(VideoFrameSubmitterTest, DidReceiveFrameSubmitsFrame) {
   EXPECT_CALL(*sink_, DoSubmitCompositorFrame(_, _));
   EXPECT_CALL(*provider_, PutCurrentFrame());
   EXPECT_CALL(*resource_provider_, AppendQuads(_));
+  EXPECT_CALL(*resource_provider_, PrepareSendToParent(_, _));
 
   submitter_->DidReceiveFrame();
   scoped_task_environment_.RunUntilIdle();
@@ -236,6 +243,7 @@ TEST_F(VideoFrameSubmitterTest, OnBeginFrameSubmitsFrame) {
   EXPECT_CALL(*sink_, DoSubmitCompositorFrame(_, _));
   EXPECT_CALL(*provider_, PutCurrentFrame());
   EXPECT_CALL(*resource_provider_, AppendQuads(_));
+  EXPECT_CALL(*resource_provider_, PrepareSendToParent(_, _));
 
   viz::BeginFrameArgs args = begin_frame_source_->CreateBeginFrameArgs(
       BEGINFRAME_FROM_HERE, now_src_.get());
@@ -293,6 +301,16 @@ TEST_F(VideoFrameSubmitterTest, NotRenderingDoesNotProduceFrame) {
   viz::BeginFrameArgs args = begin_frame_source_->CreateBeginFrameArgs(
       BEGINFRAME_FROM_HERE, now_src_.get());
   submitter_->OnBeginFrame(args);
+  scoped_task_environment_.RunUntilIdle();
+}
+
+TEST_F(VideoFrameSubmitterTest, ReturnsResourceOnCompositorAck) {
+  MakeSubmitter();
+  scoped_task_environment_.RunUntilIdle();
+
+  WTF::Vector<viz::ReturnedResource> resources;
+  EXPECT_CALL(*resource_provider_, ReceiveReturnsFromParent(_));
+  submitter_->DidReceiveCompositorFrameAck(resources);
   scoped_task_environment_.RunUntilIdle();
 }
 
