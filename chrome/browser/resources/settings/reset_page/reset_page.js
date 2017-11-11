419ff7ce6e2e5c11cf5ee208a2834875e2eabe83
Fabio Tirelo <ftirelo@chromium.org>
2017-11-11 02:55:42 +0000

    Chrome Cleanup: Move strings that are only used in the Settings page
    These strings were initially thought to be used by a separate page, but
    we ended up never moving them to the file where other strings for the
    Settings page are.
    
    Bug: 776538
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I01c8799d62564c56a8b0e4b59aaf948107afa7aa
    Reviewed-on: https://chromium-review.googlesource.com/762157
    Commit-Queue: Fabio Tirelo <ftirelo@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Chris Sharp <csharp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515810}

--- chrome/browser/resources/settings/reset_page/reset_page.js
+++ chrome/browser/resources/settings/reset_page/reset_page.js
@@ -39,7 +39,7 @@ Polymer({
       value: false,
     },
 
-    // <if expr="is_win">
+    // <if expr="_google_chrome and is_win">
     /** @private */
     userInitiatedCleanupsEnabled_: {
       type: Boolean,
@@ -90,7 +90,7 @@ Polymer({
   },
   // </if>
 
-  // <if expr="is_win">
+  // <if expr="_google_chrome and is_win">
   onChromeCleanupTap_: function() {
     settings.navigateTo(settings.routes.CHROME_CLEANUP);
   },
