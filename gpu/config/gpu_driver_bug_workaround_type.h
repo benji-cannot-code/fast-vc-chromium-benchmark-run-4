f4864290c06857fbe6466d9ec583076480d11072
Eric Karl <ericrk@chromium.org>
2017-11-11 01:34:31 +0000

    Flush after framebuffer changes on Intel 9th Gen Macs
    Certain sequences of drawing commands can lead to crashes on Intel 9th
    gen GPUs on MacOS. Adding a flush after framebuffer changes works
    around this.
    
    Bug: 783979
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I12ea134a592cf8eddd6e524d5d7d909d24640c40
    Reviewed-on: https://chromium-review.googlesource.com/764584
    Commit-Queue: Eric Karl <ericrk@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515781}

--- gpu/config/gpu_driver_bug_workaround_type.h
+++ gpu/config/gpu_driver_bug_workaround_type.h
@@ -116,6 +116,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
          emulate_isnan_on_float)                             \
   GPU_OP(EXIT_ON_CONTEXT_LOST,                               \
          exit_on_context_lost)                               \
+  GPU_OP(FLUSH_ON_FRAMEBUFFER_CHANGE,                        \
+         flush_on_framebuffer_change)                        \
   GPU_OP(FORCE_CUBE_COMPLETE,                                \
          force_cube_complete)                                \
   GPU_OP(FORCE_CUBE_MAP_POSITIVE_X_ALLOCATION,               \
