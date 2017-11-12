2c8a3610c862f146d0394f5bf23a8c34d7569045
Jochen Eisinger <jochen@chromium.org>
2017-11-12 06:23:44 +0000

    Remove hack for TreatNullAsString
    This was supposed to be a temporary workaround until issue 714866 is
    fixed, but never got removed.
    
    BUG=774176,783916
    R=mkwst@chromium.org,haraken@chromium.org
    
    Change-Id: I381d0bca04707d0a361d9ce12c96e189b626c389
    Reviewed-on: https://chromium-review.googlesource.com/762436
    Commit-Queue: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515861}

---
+++ third_party/WebKit/Source/bindings/core/v8/custom/V8ElementCustom.cpp
@@ -0,0 +1,81 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "bindings/core/v8/V8Element.h"
+
+#include "bindings/core/v8/ExceptionState.h"
+#include "bindings/core/v8/IDLTypes.h"
+#include "bindings/core/v8/V8TrustedHTML.h"
+#include "core/html/custom/V0CustomElementProcessingStack.h"
+
+namespace blink {
+
+// HTMLElement -----------------------------------------------------------------
+
+void V8Element::innerHTMLAttributeSetterCustom(
+    v8::Local<v8::Value> value,
+    const v8::FunctionCallbackInfo<v8::Value>& info) {
+  v8::Isolate* isolate = info.GetIsolate();
+
+  v8::Local<v8::Object> holder = info.Holder();
+
+  Element* impl = V8Element::ToImpl(holder);
+
+  V0CustomElementProcessingStack::CallbackDeliveryScope delivery_scope;
+
+  ExceptionState exception_state(isolate, ExceptionState::kSetterContext,
+                                 "Element", "innerHTML");
+
+  // Prepare the value to be set.
+  StringOrTrustedHTML cpp_value;
+  // This if statement is the only difference to the generated code and ensures
+  // that only null but not undefined is treated as the empty string.
+  // https://crbug.com/783916
+  if (value->IsNull()) {
+    cpp_value.SetString(String());
+  } else {
+    V8StringOrTrustedHTML::ToImpl(info.GetIsolate(), value, cpp_value,
+                                  UnionTypeConversionMode::kNotNullable,
+                                  exception_state);
+  }
+  if (exception_state.HadException())
+    return;
+
+  impl->setInnerHTML(cpp_value, exception_state);
+}
+
+void V8Element::outerHTMLAttributeSetterCustom(
+    v8::Local<v8::Value> value,
+    const v8::FunctionCallbackInfo<v8::Value>& info) {
+  v8::Isolate* isolate = info.GetIsolate();
+
+  v8::Local<v8::Object> holder = info.Holder();
+
+  Element* impl = V8Element::ToImpl(holder);
+
+  V0CustomElementProcessingStack::CallbackDeliveryScope delivery_scope;
+
+  ExceptionState exception_state(isolate, ExceptionState::kSetterContext,
+                                 "Element", "outerHTML");
+
+  // Prepare the value to be set.
+  StringOrTrustedHTML cpp_value;
+  // This if statement is the only difference to the generated code and ensures
+  // that only null but not undefined is treated as the empty string.
+  // https://crbug.com/783916
+  if (value->IsNull()) {
+    cpp_value.SetString(String());
+  } else {
+    V8StringOrTrustedHTML::ToImpl(info.GetIsolate(), value, cpp_value,
+                                  UnionTypeConversionMode::kNotNullable,
+                                  exception_state);
+  }
+  if (exception_state.HadException())
+    return;
+
+  impl->setOuterHTML(cpp_value, exception_state);
+}
+
+}  // namespace blink
