9a9776725c2382af9df2210f48c2d830c3502f47
Elly Fong-Jones <ellyjones@google.com>
2017-11-13 21:48:52 +0000

    views: normalize collected cookies infobar padding
    This change replaces the infobar's custom padding with the
    new INSETS_DIALOG padding constant. This causes the infobar's icon
    to be aligned with the other controls on the left margin.
    
    Bug: 610428
    Change-Id: Idb4c2faaa16004951eaf9988cd768b7dcc359167
    Reviewed-on: https://chromium-review.googlesource.com/758558
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516067}

--- chrome/browser/ui/views/collected_cookies_views.cc
+++ chrome/browser/ui/views/collected_cookies_views.cc
@@ -56,10 +56,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-// Spacing between the infobar frame and its contents.
-const int kInfobarVerticalPadding = 3;
-const int kInfobarHorizontalPadding = 8;
-
 // Dimensions of the tree views.
 const int kTreeViewWidth = 400;
 const int kTreeViewHeight = 125;
@@ -187,11 +183,22 @@ class InfobarView : public views::View {
   // Initialize contents and layout.
   void Init() {
     AddChildView(content_);
-    content_->SetLayoutManager(new views::BoxLayout(
-        views::BoxLayout::kHorizontal,
-        gfx::Insets(kInfobarVerticalPadding, kInfobarHorizontalPadding),
-        ChromeLayoutProvider::Get()->GetDistanceMetric(
-            DISTANCE_RELATED_CONTROL_HORIZONTAL_SMALL)));
+    // The containing dialog content view has no margins so that its
+    // TabbedPane can span the full width of the dialog, but because of
+    // that, InfobarView needs to impose its own horizontal margin.
+    gfx::Insets dialog_insets =
+        ChromeLayoutProvider::Get()->GetInsetsMetric(views::INSETS_DIALOG);
+    // No top inset is needed because the control above the infobar imposes one,
+    // but the button bar below the infobar has no margin, so a small bottom
+    // inset is needed.
+    gfx::Insets layout_insets(0, dialog_insets.left(),
+                              ChromeLayoutProvider::Get()->GetDistanceMetric(
+                                  DISTANCE_RELATED_CONTROL_VERTICAL_SMALL),
+                              dialog_insets.right());
+    content_->SetLayoutManager(
+        new views::BoxLayout(views::BoxLayout::kHorizontal, layout_insets,
+                             ChromeLayoutProvider::Get()->GetDistanceMetric(
+                                 DISTANCE_RELATED_CONTROL_HORIZONTAL_SMALL)));
     content_->AddChildView(info_image_);
     content_->AddChildView(label_);
     UpdateVisibility(false, CONTENT_SETTING_BLOCK, base::string16());
