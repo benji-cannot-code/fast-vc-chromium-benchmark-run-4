d8913364f15ed58e52bf1d73d267752fa192911b
Martin Sramek <msramek@chromium.org>
2017-11-13 12:09:20 +0000

    Consent auditor prototype
    This is a prototype of the consent auditor component.
    
    This CL only lays out the component and adds the functionality to
    record local consent plus tests.
    
    The functionality to record consents in the user's Google account,
    and any callsites, will come in subsequent CLs.
    
    See the design doc for background:
    https://docs.google.com/document/d/1Iu040VPbAPdulCpvbnOu1HLh0nXNJfuNMAEckdwmHWQ/
    
    Bug: 781765
    Change-Id: I1aef65249afe98c20db7aa692a7c3a007069e56b
    Reviewed-on: https://chromium-review.googlesource.com/731423
    Commit-Queue: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515929}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/bookmarks/bookmark_model_factory.h"
 #include "chrome/browser/browsing_data/chrome_browsing_data_remover_delegate_factory.h"
 #include "chrome/browser/chrome_browser_main.h"
+#include "chrome/browser/consent_auditor/consent_auditor_factory.h"
 #include "chrome/browser/content_settings/cookie_settings_factory.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/custom_handlers/protocol_handler_registry_factory.h"
@@ -225,6 +226,7 @@ EnsureBrowserContextKeyedServiceFactoriesBuilt() {
 #if BUILDFLAG(ENABLE_PRINT_PREVIEW)
   CloudPrintProxyServiceFactory::GetInstance();
 #endif
+  ConsentAuditorFactory::GetInstance();
   CookieSettingsFactory::GetInstance();
   NotifierStateTrackerFactory::GetInstance();
   data_use_measurement::ChromeDataUseAscriberServiceFactory::GetInstance();
---
+++ components/consent_auditor/pref_names.cc
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/consent_auditor/pref_names.h"
+
+namespace consent_auditor {
+namespace prefs {
+
+const char kLocalConsentsDictionary[] = "local_consents";
+
+}  // namespace prefs
+}  // namespace consent_auditor
