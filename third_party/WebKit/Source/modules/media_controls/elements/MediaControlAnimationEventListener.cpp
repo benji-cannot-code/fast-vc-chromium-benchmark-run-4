7c488d163942503e230e10e7b72756d79d58576a
Becca Hughes <beccahughes@chromium.org>
2017-11-13 14:57:07 +0000

    Media Controls: Refactor animation event listener
    The animation event listener that was part of loading panel
    will be useful for some other motion in the media controls so
    this refactors it into a reusable component.
    
    BUG=779989
    
    Change-Id: I30f3b3aebe20bbeb9886c1c7d4b4e2f5174e4042
    Reviewed-on: https://chromium-review.googlesource.com/763413
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515945}

---
+++ third_party/WebKit/Source/modules/media_controls/elements/MediaControlAnimationEventListener.cpp
@@ -0,0 +1,55 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "modules/media_controls/elements/MediaControlAnimationEventListener.h"
+
+#include "core/dom/Element.h"
+#include "core/dom/events/Event.h"
+
+namespace blink {
+
+MediaControlAnimationEventListener::MediaControlAnimationEventListener(
+    Observer* observer)
+    : EventListener(EventListener::kCPPEventListenerType), observer_(observer) {
+  observer_->WatchedAnimationElement().addEventListener(
+      EventTypeNames::animationend, this, false);
+  observer_->WatchedAnimationElement().addEventListener(
+      EventTypeNames::animationiteration, this, false);
+}
+
+void MediaControlAnimationEventListener::Detach() {
+  observer_->WatchedAnimationElement().removeEventListener(
+      EventTypeNames::animationend, this, false);
+  observer_->WatchedAnimationElement().removeEventListener(
+      EventTypeNames::animationiteration, this, false);
+}
+
+bool MediaControlAnimationEventListener::operator==(
+    const EventListener& other) const {
+  return this == &other;
+}
+
+void MediaControlAnimationEventListener::Trace(Visitor* visitor) {
+  visitor->Trace(observer_);
+  EventListener::Trace(visitor);
+}
+
+void MediaControlAnimationEventListener::handleEvent(ExecutionContext* context,
+                                                     Event* event) {
+  if (event->type() == EventTypeNames::animationend) {
+    observer_->OnAnimationEnd();
+    return;
+  }
+  if (event->type() == EventTypeNames::animationiteration) {
+    observer_->OnAnimationIteration();
+    return;
+  }
+
+  NOTREACHED();
+}
+
+void MediaControlAnimationEventListener::Observer::Trace(Visitor*) {}
+
+}  // namespace blink
