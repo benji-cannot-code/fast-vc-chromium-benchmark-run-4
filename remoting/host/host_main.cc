c1a9dbbe8fbf9ef3b54989bb263d602a5bb2512d
Jamie Walch <jamiewalch@chromium.org>
2017-11-13 22:17:48 +0000

    Add //mojo/edk/embedder:headers as a dependency for the multi-process host.
    Bug: 783173
    Change-Id: I2bcf961155fab34a9a58865ca17ebe28876bab10
    Reviewed-on: https://chromium-review.googlesource.com/764118
    Commit-Queue: Jamie Walch <jamiewalch@chromium.org>
    Reviewed-by: Zijie He <zijiehe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516078}

--- remoting/host/host_main.cc
+++ remoting/host/host_main.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
-#include "mojo/edk/embedder/embedder.h"
 #include "remoting/base/breakpad.h"
 #include "remoting/host/evaluate_capability.h"
 #include "remoting/host/host_exit_codes.h"
@@ -29,6 +28,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/host/setup/me2me_native_messaging_host.h"
 #include "remoting/host/switches.h"
 #include "remoting/host/usage_stats_consent.h"
+
+#if defined(REMOTING_MULTI_PROCESS)
+#include "mojo/edk/embedder/embedder.h"
+#endif  // defined(REMOTING_MULTI_PROCESS)
 
 #if defined(OS_MACOSX)
 #include "base/mac/scoped_nsautorelease_pool.h"
