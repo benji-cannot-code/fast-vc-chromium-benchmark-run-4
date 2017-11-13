4e9bcd71fc9156d1f1a2b90e571efd63561aebeb
Sylvain Defresne <sdefresne@chromium.org>
2017-11-13 16:26:01 +0000

    Allow WebStateObserver to observe N WebStates [11/N].
    Convert IOSTranslateDriver to directly track registration with
    the observed WebState instead of relying on the deprecated
    code in WebStateObserver.
    
    Bug: 775684
    Change-Id: Iad1d53fcab9448e088a19cd75767235f9816a0b3
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Reviewed-on: https://chromium-review.googlesource.com/757105
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515956}

--- components/translate/ios/browser/ios_translate_driver.h
+++ components/translate/ios/browser/ios_translate_driver.h
@@ -54,6 +54,7 @@ class IOSTranslateDriver : public TranslateDriver,
   void NavigationItemCommitted(
       web::WebState* web_state,
       const web::LoadCommittedDetails& load_details) override;
+  void WebStateDestroyed(web::WebState* web_state) override;
 
   // TranslateDriver methods.
   void OnIsPageTranslatedChanged() override;
@@ -100,6 +101,10 @@ class IOSTranslateDriver : public TranslateDriver,
   void OnTranslateComplete(bool success,
                            const std::string& original_language,
                            double translation_time) override;
+
+  // The WebState this instance is observing. Will be null after
+  // WebStateDestroyed has been called.
+  web::WebState* web_state_ = nullptr;
 
   // The navigation manager of the tab we are associated with.
   web::NavigationManager* navigation_manager_;
