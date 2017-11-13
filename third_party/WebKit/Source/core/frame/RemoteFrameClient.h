1d4239f8ec99ef6872fb57c41947e05a7790cfac
Ken Buchanan <kenrb@chromium.org>
2017-11-13 02:03:24 +0000

    Enable render throttling for OOPIFs
    Throttle OOPIFs when they are off screen or clipped. This adds a
    VisibilityObserver to RemoteFrames with LocalFrame parents, and
    sends an update across processes to the OOPIF when throttling status
    should change.
    
    This also fixes ScreenOrientation browser tests which has some
    flakiness in OOPIF tests that appears when this patch is applied.
    
    Bug: 761478
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I9bfb57058e534e796c23e238664a66a142be3c4a
    Reviewed-on: https://chromium-review.googlesource.com/719601
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515875}

--- third_party/WebKit/Source/core/frame/RemoteFrameClient.h
+++ third_party/WebKit/Source/core/frame/RemoteFrameClient.h
@@ -43,6 +43,9 @@ class RemoteFrameClient : public FrameClient {
   virtual void VisibilityChanged(bool visible) = 0;
 
   virtual void SetIsInert(bool) = 0;
+
+  virtual void UpdateRenderThrottlingStatus(bool isThrottled,
+                                            bool subtreeThrottled) = 0;
 };
 
 }  // namespace blink
