b99ece483319110ea4a3438e491906b9768c6a65
Wez <wez@chromium.org>
2017-11-13 19:05:07 +0000

    Derive blink::WebKeyboardEvent fields from ui::KeyEvent.
    Previously WebKeyboardEvents were built for Windows by ignoring the
    ui::KeyEvent field values, and populating the WebKeyboardEvent directly
    from the NativeEvent fields, if available.
    
    We now derive the WebKeyboardEvent fields from the supplied ui::Event,
    and update some fields from the NativeEvent, if available. To reduce
    the amount of patch-up code, handling of Windows' native auto-repeat
    flag will be used, for ui::KeyEvents generated from a NativeEvent,
    instead of being synthesized by ui::KeyEvent::IsRepeated.
    
    Bug: 25503
    Change-Id: I9d5b1468852d85392ae3a5f57d9457726612ddf1
    Reviewed-on: https://chromium-review.googlesource.com/754130
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515994}

--- ui/events/blink/web_input_event_builders_win.h
+++ ui/events/blink/web_input_event_builders_win.h
@@ -14,15 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ui {
 
-class WebKeyboardEventBuilder {
- public:
-  static blink::WebKeyboardEvent Build(HWND hwnd,
-                                       UINT message,
-                                       WPARAM wparam,
-                                       LPARAM lparam,
-                                       double time_stamp);
-};
-
 class WebMouseEventBuilder {
  public:
   static blink::WebMouseEvent Build(
