e05fae42e2cccf35de1ea221c9883b8c0a3c8a1b
Patrik Höglund <phoglund@chromium.org>
2017-11-13 10:38:51 +0000

    Rewrite getUseMedia calls to use promises.
    This removes many uses of waitForVideo() etc. and makes the
    control flow of the test clearer. Instead of having the
    all-events-occurred handler magically calling reportTestSuccess,
    it's laid out explicitly in each test.
    
    Bug: chromium:777857
    Change-Id: I034d8950f764df607069b3e4b5ce6baeb46eab9b
    Reviewed-on: https://chromium-review.googlesource.com/758848
    Commit-Queue: Patrik Höglund <phoglund@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515917}

--- content/browser/webrtc/webrtc_browsertest.cc
+++ content/browser/webrtc/webrtc_browsertest.cc
@@ -54,6 +54,15 @@ class MAYBE_WebRtcBrowserTest : public WebRtcContentBrowserTestBase {
   }
 };
 
+IN_PROC_BROWSER_TEST_F(MAYBE_WebRtcBrowserTest, CanSetupAudioAndVideoCall) {
+  MakeTypicalPeerConnectionCall("call({video: true, audio: true});");
+}
+
+IN_PROC_BROWSER_TEST_F(MAYBE_WebRtcBrowserTest,
+                       CanSetupDefaultVideoCallWithOldGetUserMedia) {
+  MakeTypicalPeerConnectionCall("oldStyleCall();");
+}
+
 // These tests will make a complete PeerConnection-based call and verify that
 // video is playing for the call.
 IN_PROC_BROWSER_TEST_F(MAYBE_WebRtcBrowserTest,
@@ -93,11 +102,6 @@ IN_PROC_BROWSER_TEST_F(MAYBE_WebRtcBrowserTest,
   const std::string javascript =
       "callAndDisableLocalVideo({video: true});";
   MakeTypicalPeerConnectionCall(javascript);
-}
-
-IN_PROC_BROWSER_TEST_F(MAYBE_WebRtcBrowserTest,
-                       CanSetupAudioAndVideoCall) {
-  MakeTypicalPeerConnectionCall("call({video: true, audio: true});");
 }
 
 
