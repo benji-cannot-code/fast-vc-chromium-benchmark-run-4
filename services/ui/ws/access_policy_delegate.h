a581773327c2f939001fd57a9535cda1b1b93ee9
Scott Violet <sky@chromium.org>
2017-11-13 21:19:10 +0000

    chromeos: expose child windows to client at embed point
    When a client embeds another client it can choose to intercept events
    by specifying kEmbedFlagEmbedderInterceptsEvents. When this happens
    events targetted at windows created by the embedded client go to the
    embedder. When this happens the embedder needs to know about the
    windows created by the embedded client, otherwise it has no way to
    know the real target.
    
    This patch makes it so that if kEmbedFlagEmbedderInterceptsEvents was
    specified (and the client was not created by way of Embed()) then the
    embedder client sees all windows created by any embedded client. The
    embedder can still not operate on the windows, but it sees the
    windows.
    
    In terms of chrome this makes it so that browser sees the windows
    created by renderers. Renderers can not see any windows created by
    other renderers (even ones embedded in renderers).
    
    The long term goal is to get rid of
    kEmbedFlagEmbedderInterceptsEvents, at which point this will be removed.
    
    BUG=781391
    TEST=covered by test
    
    Change-Id: I110d3205cd3b9c3bfa0e78760dd58b517fa43856
    Reviewed-on: https://chromium-review.googlesource.com/760797
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Elliot Glaysher <erg@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516050}

--- services/ui/ws/access_policy_delegate.h
+++ services/ui/ws/access_policy_delegate.h
@@ -35,6 +35,10 @@ class AccessPolicyDelegate {
   virtual bool IsWindowCreatedByWindowManager(
       const ServerWindow* window) const = 0;
 
+  // Returns true if the tree intercepts events targetted at |window|.
+  virtual bool ShouldInterceptEventsForAccessPolicy(
+      const ServerWindow* window) const = 0;
+
  protected:
   virtual ~AccessPolicyDelegate() {}
 };
