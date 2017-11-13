b3e44d90f1feeb9f11ae8f82a60cfcf97ae129ac
Lei Zhang <thestig@chromium.org>
2017-11-13 17:38:15 +0000

    Remove IS_IOS checks in chrome/
    Again.
    
    Change-Id: I82deeea0c31ac8f609a6a5d98113c65de1fd30b0
    Reviewed-on: https://chromium-review.googlesource.com/762873
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515975}

--- chrome/browser/content_settings/content_settings_default_provider_unittest.cc
+++ chrome/browser/content_settings/content_settings_default_provider_unittest.cc
@@ -176,7 +176,7 @@ TEST_F(ContentSettingsDefaultProviderTest, DiscardObsoletePreferences) {
   EXPECT_EQ(CONTENT_SETTING_BLOCK, prefs->GetInteger(kGeolocationPrefPath));
 }
 
-#if !defined(OS_IOS) && !defined(OS_ANDROID)
+#if !defined(OS_ANDROID)
 TEST_F(ContentSettingsDefaultProviderTest, DiscardObsoletePluginsAllow) {
   PrefService* prefs = profile_.GetPrefs();
   const std::string& plugins_pref_path =
@@ -209,7 +209,7 @@ TEST_F(ContentSettingsDefaultProviderTest, DiscardObsoletePluginsAllow) {
               prefs->GetInteger(plugins_pref_path));
   }
 }
-#endif  // !defined(OS_IOS) && !defined(OS_ANDROID)
+#endif  // !defined(OS_ANDROID)
 
 TEST_F(ContentSettingsDefaultProviderTest, OffTheRecord) {
   DefaultProvider otr_provider(profile_.GetPrefs(), true /* incognito */);
--- chrome/browser/metrics/chrome_metrics_service_client.cc
+++ chrome/browser/metrics/chrome_metrics_service_client.cc
@@ -148,7 +148,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-#if defined(OS_ANDROID) || defined(OS_IOS) || defined(OS_CHROMEOS)
+#if defined(OS_ANDROID) || defined(OS_CHROMEOS)
 const int kMaxHistogramStorageKiB = 100 << 10;  // 100 MiB
 #else
 const int kMaxHistogramStorageKiB = 500 << 10;  // 500 MiB
