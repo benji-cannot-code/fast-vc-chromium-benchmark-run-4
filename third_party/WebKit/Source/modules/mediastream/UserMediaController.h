7b2eea4f997dea21ea2d1c89c5b312e3f799eb2b
Guido Urdaneta <guidou@chromium.org>
2017-11-13 12:25:07 +0000

    Reland "Reland "Serialize MediaStreamTrack.stop() with getUserMedia()""
    This CL runs requests on the same task where they are issued if possible,
    which is more similar to the way MST.stop() used to run.
    If this is not enough to make the failing racy test pass, the only solution
    is to disable the test on the bots where it fails.
    
    Reusing haraken@ lgtm for the Blink part.
    
    TBR=haraken@chromium.org
    
    This is a reland of 7c8e8e13ce6cc2d04469a2503244151b8842775e
    Original change's description:
    > Reland "Serialize MediaStreamTrack.stop() with getUserMedia()"
    >
    > This is a reland of a58c8bd608d040102d374322b48aa9055d8fcdbe
    > Original change's description:
    > > Serialize MediaStreamTrack.stop() with getUserMedia()
    > >
    > > When multiple getUserMedia and stop() requests are active concurrently,
    > > getUserMedia() sometimes fails with TrackStartError.
    > > The reason is that the underlying video capture subsystem does not keep
    > > a count of how many sources have opened a video device.
    > > It can happen that a stop request stops video capture while a
    > > getUserMedia() request is being processed, with the result being that
    > > the video device is closed before getUserMedia() starts the new video
    > > track, resulting in an error.
    > >
    > > One solution to this problem is to make sure that stop requests and
    > > getUserMedia() requests cannot run concurrently, which is the approach
    > > followed by this CL.
    > >
    > > This CL basically moves handling of MediaStramTrack.stop() from
    > > MediaStreamCenter to UserMediaClient, and puts stop() requests in the
    > > same queue used for getUserMedia() and applyConstraints() requests.
    > >
    > >
    > > Bug: 778039
    > > Change-Id: Id7e0ee38e40265fd599dc6dd0712f0b667d2e1a5
    > > Reviewed-on: https://chromium-review.googlesource.com/751981
    > > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > > Reviewed-by: Henrik Boström <hbos@chromium.org>
    > > Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#515494}
    >
    > TBR=haraken@chromium.org
    >
    > Bug: 778039
    > Change-Id: I461e3c98f3d6471cc218d48501d22c6c0a8b03e9
    > Reviewed-on: https://chromium-review.googlesource.com/763636
    > Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    > Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Reviewed-by: Henrik Boström <hbos@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#515836}
    
    Bug: 778039
    Change-Id: Idd2e1c95952b9c924037beaca987344093751009
    Reviewed-on: https://chromium-review.googlesource.com/765657
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515932}

--- third_party/WebKit/Source/modules/mediastream/UserMediaController.h
+++ third_party/WebKit/Source/modules/mediastream/UserMediaController.h
@@ -36,6 +36,7 @@ namespace blink {
 class ApplyConstraintsRequest;
 class MediaDevices;
 class MediaDevicesRequest;
+class MediaStreamComponent;
 class UserMediaRequest;
 
 class UserMediaController final
@@ -54,6 +55,7 @@ class UserMediaController final
   void RequestMediaDevices(MediaDevicesRequest*);
   void SetMediaDeviceChangeObserver(MediaDevices*);
   void ApplyConstraints(ApplyConstraintsRequest*);
+  void StopTrack(MediaStreamComponent*);
 
   static const char* SupplementName();
   static UserMediaController* From(LocalFrame* frame) {
@@ -87,6 +89,10 @@ inline void UserMediaController::SetMediaDeviceChangeObserver(
 inline void UserMediaController::ApplyConstraints(
     ApplyConstraintsRequest* request) {
   client_->ApplyConstraints(request);
+}
+
+inline void UserMediaController::StopTrack(MediaStreamComponent* track) {
+  client_->StopTrack(track);
 }
 
 }  // namespace blink
