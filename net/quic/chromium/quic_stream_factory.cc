5c5ec366ecde5b02600ffb3e559c9a38c7367e7b
Helen Li <xunjieli@chromium.org>
2017-11-13 17:40:42 +0000

    Remove CHECKs added to investigate QuicStreamFactory::OnJobComplete crash
    The QuicStreamFactory::OnJobComplete crash is gone in M63. This CL removes
    the added CHECKs.
    
    Bug: 750271
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Iee46a69d151be4c5e8ceb91c33ec63e445ede2fd
    Reviewed-on: https://chromium-review.googlesource.com/766487
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Helen Li <xunjieli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515976}

--- net/quic/chromium/quic_stream_factory.cc
+++ net/quic/chromium/quic_stream_factory.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <tuple>
 #include <utility>
 
-#include "base/auto_reset.h"
 #include "base/callback_helpers.h"
 #include "base/location.h"
 #include "base/metrics/field_trial.h"
@@ -333,13 +332,12 @@ class QuicStreamFactory::Job {
   size_t EstimateMemoryUsage() const;
 
   void AddRequest(QuicStreamRequest* request) {
-    CHECK(request->server_id() == key_.server_id());
     stream_requests_.insert(request);
   }
 
   void RemoveRequest(QuicStreamRequest* request) {
     auto request_iter = stream_requests_.find(request);
-    CHECK(request_iter != stream_requests_.end());
+    DCHECK(request_iter != stream_requests_.end());
     stream_requests_.erase(request_iter);
   }
 
@@ -356,7 +354,6 @@ class QuicStreamFactory::Job {
     STATE_CONNECT_COMPLETE,
   };
 
-  bool in_loop_;  // Temporary to investigate crbug.com/750271.
   IoState io_state_;
   QuicStreamFactory* factory_;
   QuicTransportVersion quic_version_;
@@ -385,8 +382,7 @@ QuicStreamFactory::Job::Job(QuicStreamFactory* factory,
                             bool was_alternative_service_recently_broken,
                             int cert_verify_flags,
                             const NetLogWithSource& net_log)
-    : in_loop_(false),
-      io_state_(STATE_RESOLVE_HOST),
+    : io_state_(STATE_RESOLVE_HOST),
       factory_(factory),
       quic_version_(quic_version),
       host_resolver_(host_resolver),
@@ -414,7 +410,6 @@ QuicStreamFactory::Job::Job(QuicStreamFactory* factory,
 
 QuicStreamFactory::Job::~Job() {
   net_log_.EndEvent(NetLogEventType::QUIC_STREAM_FACTORY_JOB);
-  CHECK(!in_loop_);
   // If |this| is destroyed in QuicStreamFactory's destructor, |callback_| is
   // non-null.
 }
@@ -429,8 +424,6 @@ int QuicStreamFactory::Job::Run(const CompletionCallback& callback) {
 
 int QuicStreamFactory::Job::DoLoop(int rv) {
   TRACE_EVENT0(kNetTracingCategory, "QuicStreamFactory::Job::DoLoop");
-  CHECK(!in_loop_);
-  base::AutoReset<bool> auto_reset_in_loop(&in_loop_, true);
 
   do {
     IoState state = io_state_;
@@ -461,7 +454,6 @@ int QuicStreamFactory::Job::DoLoop(int rv) {
 void QuicStreamFactory::Job::OnIOComplete(int rv) {
   rv = DoLoop(rv);
   if (rv != ERR_IO_PENDING && !callback_.is_null()) {
-    CHECK(!in_loop_);
     base::ResetAndReturn(&callback_).Run(rv);
   }
 }
@@ -994,9 +986,7 @@ bool QuicStreamFactory::OnResolution(const QuicSessionKey& key,
 
 void QuicStreamFactory::OnJobComplete(Job* job, int rv) {
   auto iter = active_jobs_.find(job->key().server_id());
-  // TODO(xunjieli): Change following CHECKs back to DCHECKs after
-  // crbug.com/750271 is fixed.
-  CHECK(iter != active_jobs_.end());
+  DCHECK(iter != active_jobs_.end());
   if (rv == OK) {
     set_require_confirmation(false);
 
@@ -1005,7 +995,6 @@ void QuicStreamFactory::OnJobComplete(Job* job, int rv) {
     CHECK(session_it != active_sessions_.end());
     QuicChromiumClientSession* session = session_it->second;
     for (auto* request : iter->second->stream_requests()) {
-      CHECK(request->server_id() == job->key().server_id());
       // Do not notify |request| yet.
       request->SetSession(session->CreateHandle());
     }
