a1ae69771e65932f45cd2cb1ac62f434d4c4b6df
Dirk Pranke <dpranke@chromium.org>
2017-11-14 17:43:46 +0000

    Revert "Reuse kArcIntentHelperPackageName in c/b/"
    This reverts commit bf228a619a3edcb3321e1aee0ba23536427e0721.
    
    Reason for revert: Didn't compile properly on CrOS (which we missed due to a bad CQ configuration).
    
    Original change's description:
    > Reuse kArcIntentHelperPackageName in c/b/
    > 
    > Reusing kArcIntentHelperPackageName where needed. Also adding a method
    > to reuse such package name + a custom string appended to it, this is
    > AppendStringToIntentHelperPackageName.
    > 
    > Bug: None
    > Test: Build.
    > Change-Id: I1b9115b808533e1e5ed3339b7f3747496ca2f0c3
    > Reviewed-on: https://chromium-review.googlesource.com/764396
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    > Commit-Queue: David Jacobo <djacobo@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#516326}
    
    TBR=yusukes@chromium.org,sky@chromium.org,djacobo@chromium.org
    
    Change-Id: Idfef3e2b248c5a1339bb97c47be0f3a6cdf84ba3
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: None
    Reviewed-on: https://chromium-review.googlesource.com/769187
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Commit-Queue: Dirk Pranke <dpranke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516341}

--- chrome/browser/chromeos/arc/user_session/arc_user_session_service.cc
+++ chrome/browser/chromeos/arc/user_session/arc_user_session_service.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/singleton.h"
 #include "components/arc/arc_bridge_service.h"
 #include "components/arc/arc_browser_context_keyed_service_factory_base.h"
-#include "components/arc/intent_helper/arc_intent_helper_bridge.h"
 #include "components/session_manager/core/session_manager.h"
 
 namespace arc {
@@ -62,12 +61,9 @@ void ArcUserSessionService::OnSessionStateChanged() {
     return;
 
   instance->SendBroadcast(
-      ArcIntentHelperBridge::AppendStringToIntentHelperPackageName(
-          "USER_SESSION_ACTIVE"),
-      ArcIntentHelperBridge::kArcIntentHelperPackageName,
-      ArcIntentHelperBridge::AppendStringToIntentHelperPackageName(
-          "ArcIntentHelperService"),
-      "{}");
+      "org.chromium.arc.intent_helper.USER_SESSION_ACTIVE",
+      "org.chromium.arc.intent_helper",
+      "org.chromium.arc.intent_helper.ArcIntentHelperService", "{}");
 }
 
 void ArcUserSessionService::OnInstanceReady() {
--- chrome/browser/ui/app_list/arc/arc_app_utils.cc
+++ chrome/browser/ui/app_list/arc/arc_app_utils.cc
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/arc/arc_service_manager.h"
 #include "components/arc/arc_util.h"
 #include "components/arc/common/intent_helper.mojom.h"
-#include "components/arc/intent_helper/arc_intent_helper_bridge.h"
 #include "components/crx_file/id_util.h"
 #include "ui/aura/window.h"
 #include "ui/display/display.h"
@@ -57,11 +56,10 @@ namespace arc {
 
 namespace {
 
-// TODO(djacobo): Evaluate to build these strings by using
-// ArcIntentHelperBridge::AppendStringToIntentHelperPackageName.
 // Intent helper strings.
 constexpr char kIntentHelperClassName[] =
     "org.chromium.arc.intent_helper.SettingsReceiver";
+constexpr char kIntentHelperPackageName[] = "org.chromium.arc.intent_helper";
 constexpr char kSetInTouchModeIntent[] =
     "org.chromium.arc.intent_helper.SET_IN_TOUCH_MODE";
 constexpr char kShowTalkbackSettingsIntent[] =
@@ -121,9 +119,8 @@ bool Launch(content::BrowserContext* context,
     std::string extras_string;
     base::JSONWriter::Write(extras, &extras_string);
     intent_helper_instance->SendBroadcast(
-        kSetInTouchModeIntent,
-        ArcIntentHelperBridge::kArcIntentHelperPackageName,
-        kIntentHelperClassName, extras_string);
+        kSetInTouchModeIntent, kIntentHelperPackageName, kIntentHelperClassName,
+        extras_string);
   }
 
   if (app_info->shortcut || intent.has_value()) {
@@ -307,10 +304,9 @@ void ShowTalkBackSettings() {
   if (!intent_helper_instance)
     return;
 
-  intent_helper_instance->SendBroadcast(
-      kShowTalkbackSettingsIntent,
-      ArcIntentHelperBridge::kArcIntentHelperPackageName,
-      kIntentHelperClassName, "{}");
+  intent_helper_instance->SendBroadcast(kShowTalkbackSettingsIntent,
+                                        kIntentHelperPackageName,
+                                        kIntentHelperClassName, "{}");
 }
 
 void StartPaiFlow() {
