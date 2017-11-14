ef2b6cf0b4bdc1b17e1b83077884dcdfe8f002bb
Jacob Dufault <jdufault@google.com>
2017-11-14 19:22:24 +0000

    cros: Add delayed auth fail option in login/lock dev overlay.
    Bug: 719015
    Change-Id: I0b937317d18ed04bbc3faf28dbb7ebcc77093988
    Reviewed-on: https://chromium-review.googlesource.com/767023
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Jacob Dufault <jdufault@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516382}

--- ash/login/ui/lock_debug_view.h
+++ ash/login/ui/lock_debug_view.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <vector>
 
+#include "ash/login/lock_screen_controller.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/view.h"
 
@@ -67,7 +68,8 @@ class LockDebugView : public views::View, public views::ButtonListener {
   // Debug dispatcher and cached data for the UI.
   std::unique_ptr<DebugDataDispatcherTransformer> const debug_data_dispatcher_;
   size_t num_users_ = 1u;
-  bool force_fail_auth_ = false;
+  LockScreenController::ForceFailAuth force_fail_auth_ =
+      LockScreenController::ForceFailAuth::kOff;
 
   DISALLOW_COPY_AND_ASSIGN(LockDebugView);
 };
