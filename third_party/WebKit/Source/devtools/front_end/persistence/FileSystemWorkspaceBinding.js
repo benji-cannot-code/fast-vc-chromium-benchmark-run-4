49c31161f64e6180bebc02739204ca694a5566f4
Nathan Bruer <allada@chromium.org>
2017-11-14 06:20:51 +0000

    [Devtools] Content changed event should not emit when creating files
    If a file is being created using IsolatedFileSystem the UISourceCode should
    not receive checkContentUpdated triggers if the content has not fully been
    written yet.
    
    R=lushnikov
    Bug=None
    
    Change-Id: I1e7df5f8a187b507c5939d78b378be8938cf4d0b
    Reviewed-on: https://chromium-review.googlesource.com/759227
    Reviewed-by: Andrey Lushnikov <lushnikov@chromium.org>
    Commit-Queue: Blaise Bruer <allada@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516212}

--- third_party/WebKit/Source/devtools/front_end/persistence/FileSystemWorkspaceBinding.js
+++ third_party/WebKit/Source/devtools/front_end/persistence/FileSystemWorkspaceBinding.js
@@ -226,6 +226,8 @@ Persistence.FileSystemWorkspaceBinding.FileSystem = class extends Workspace.Proj
     this._fileSystemParentURL = this._fileSystemBaseURL.substr(0, fileSystemPath.lastIndexOf('/') + 1);
     this._fileSystemWorkspaceBinding = fileSystemWorkspaceBinding;
     this._fileSystemPath = fileSystemPath;
+    /** @type {!Set<string>} */
+    this._creatingFilesGuard = new Set();
 
     workspace.addProject(this);
     this.populate();
@@ -448,10 +450,7 @@ Persistence.FileSystemWorkspaceBinding.FileSystem = class extends Workspace.Proj
    * @return {string}
    */
   _extensionForPath(path) {
-    var extensionIndex = path.lastIndexOf('.');
-    if (extensionIndex === -1)
-      return '';
-    return path.substring(extensionIndex + 1).toLowerCase();
+    return Common.ParsedURL.extractExtension(path);
   }
 
   populate() {
@@ -501,11 +500,14 @@ Persistence.FileSystemWorkspaceBinding.FileSystem = class extends Workspace.Proj
    * @return {!Promise<?Workspace.UISourceCode>}
    */
   async createFile(path, name, content, isBase64) {
+    var guardFileName = this._fileSystemPath + path + (!path.endsWith('/') ? '/' : '') + name;
+    this._creatingFilesGuard.add(guardFileName);
     var filePath = await this._fileSystem.createFile(path, name);
     if (!filePath)
       return null;
     if (content)
       await new Promise(resolve => this._fileSystem.setFileContent(filePath, content, isBase64 || false, resolve));
+    this._creatingFilesGuard.delete(guardFileName);
     return this._addFile(filePath);
   }
 
@@ -543,6 +545,9 @@ Persistence.FileSystemWorkspaceBinding.FileSystem = class extends Workspace.Proj
    * @param {string} path
    */
   _fileChanged(path) {
+    // Ignore files that are being created but do not have content yet.
+    if (this._creatingFilesGuard.has(path))
+      return;
     var uiSourceCode = this.uiSourceCodeForURL(path);
     if (!uiSourceCode) {
       var contentType = Persistence.FileSystemWorkspaceBinding._contentTypeForExtension(this._extensionForPath(path));
