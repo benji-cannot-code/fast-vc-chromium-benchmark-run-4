f8152111ac02946af6e61497cc70c3a969004c78
Andrey Kraynov <iceman@yandex-team.ru>
2017-11-14 12:25:06 +0000

    Replace calls of deprecated functions FirstPositionInOrBeforeNode
    Use |const Node&| versions instead of deprecated ones.
    Explicitly check for nullptr node value if we can't be sure about it.
    
    No changes in behavior.
    
    Bug: 766448
    Change-Id: Id0a625da0d7d70f1d9f97a1fc1cf17ccddf1a2ec
    Reviewed-on: https://chromium-review.googlesource.com/763632
    Commit-Queue: Andrey Kraynov <iceman@yandex-team.ru>
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516275}

--- third_party/WebKit/Source/core/editing/commands/ApplyStyleCommand.cpp
+++ third_party/WebKit/Source/core/editing/commands/ApplyStyleCommand.cpp
@@ -1508,7 +1508,7 @@ void ApplyStyleCommand::RemoveInlineStyle(EditingStyle* style,
           DCHECK(s.IsBeforeAnchor() || s.IsBeforeChildren() ||
                  s.OffsetInContainerNode() <= 0)
               << s;
-          s = FirstPositionInOrBeforeNodeDeprecated(next);
+          s = next ? FirstPositionInOrBeforeNode(*next) : Position();
         }
         if (e.AnchorNode() == elem) {
           // Since elem must have been fully selected, and it is at the end
@@ -1518,7 +1518,7 @@ void ApplyStyleCommand::RemoveInlineStyle(EditingStyle* style,
                  !OffsetIsBeforeLastNodeOffset(s.OffsetInContainerNode(),
                                                s.ComputeContainerNode()))
               << s;
-          e = LastPositionInOrAfterNodeDeprecated(prev);
+          e = prev ? LastPositionInOrAfterNode(*prev) : Position();
         }
       }
 
