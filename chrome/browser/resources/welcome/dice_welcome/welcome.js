097bc0f62827e3e84428dcadd6d64249a602c035
Scott Chen <scottchen@chromium.org>
2017-11-14 13:32:08 +0000

    [signin] Fork welcome page base on DICE status.
    Setup work to change the welcome page visuals based on the DICE status
    of the profile. The new_welcome* files are exact copies of the original in this CL
    and there will be follow-up CLs to iterate on them.
    
    Bug: 782155
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: If620f91b4b30b70ca34ac9db5fd1f27c893bd189
    Reviewed-on: https://chromium-review.googlesource.com/751461
    Commit-Queue: Scott Chen <scottchen@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516288}

---
+++ chrome/browser/resources/welcome/dice_welcome/welcome.js
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+cr.define('welcome', function() {
+  'use strict';
+
+  function onAccept(e) {
+    chrome.send('handleActivateSignIn');
+  }
+
+  function onDecline(e) {
+    chrome.send('handleUserDecline');
+    e.preventDefault();
+  }
+
+  function initialize() {
+    $('accept-button').addEventListener('click', onAccept);
+    $('decline-button').addEventListener('click', onDecline);
+
+    var logo = document.querySelector('.logo-icon');
+    logo.onclick = function(e) {
+      logo.animate(
+          {
+            transform: ['none', 'rotate(-10turn)'],
+          },
+          /** @type {!KeyframeEffectOptions} */ ({
+            duration: 500,
+            easing: 'cubic-bezier(1, 0, 0, 1)',
+          }));
+    };
+  }
+
+  return {initialize: initialize};
+});
+
+document.addEventListener('DOMContentLoaded', welcome.initialize);
--- chrome/browser/ui/webui/welcome_ui.cc
+++ chrome/browser/ui/webui/welcome_ui.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/chromium_strings.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/prefs/pref_service.h"
+#include "components/signin/core/browser/profile_management_switches.h"
 #include "content/public/browser/web_ui_data_source.h"
 #include "net/base/url_util.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -61,12 +62,19 @@ WelcomeUI::WelcomeUI(content::WebUI* web_ui, const GURL& url)
   html_source->AddLocalizedString("acceptText", IDS_WELCOME_ACCEPT_BUTTON);
   html_source->AddLocalizedString("declineText", IDS_WELCOME_DECLINE_BUTTON);
 
-  html_source->AddResourcePath("welcome.js", IDR_WELCOME_JS);
-  html_source->AddResourcePath("welcome.css", IDR_WELCOME_CSS);
   html_source->AddResourcePath("logo.png", IDR_PRODUCT_LOGO_128);
   html_source->AddResourcePath("logo2x.png", IDR_PRODUCT_LOGO_256);
   html_source->AddResourcePath("watermark.svg", IDR_WEBUI_IMAGES_GOOGLE_LOGO);
-  html_source->SetDefaultResource(IDR_WELCOME_HTML);
+
+  if (signin::IsDiceEnabledForProfile(profile->GetPrefs())) {
+    html_source->AddResourcePath("welcome.js", IDR_DICE_WELCOME_JS);
+    html_source->AddResourcePath("welcome.css", IDR_DICE_WELCOME_CSS);
+    html_source->SetDefaultResource(IDR_DICE_WELCOME_HTML);
+  } else {
+    html_source->AddResourcePath("welcome.js", IDR_WELCOME_JS);
+    html_source->AddResourcePath("welcome.css", IDR_WELCOME_CSS);
+    html_source->SetDefaultResource(IDR_WELCOME_HTML);
+  }
 
   content::WebUIDataSource::Add(profile, html_source);
 }
