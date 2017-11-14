2e3aec982bae475a1701ae2de8cdb23fc9e1b27f
Min Qin <qinmin@chromium.org>
2017-11-14 21:57:19 +0000

    Network Service: Add a navigation throttle to intercept OMA DRM download
    OMA(Open Mobile Alliance) DRM download(https://en.wikipedia.org/wiki/OMA_DRM) requires
    the device to encrypt the downloaded media content with device keys.
    So that the downloaded media file cannot be copied and played on another device.
    Chrome doesn't have the capability to encrypt the content,
    so the content has to be downloaded through Android DownloadManager.
    This CL intercepts the download before it is intercepted by content::DownloadManagerImpl,
    and passes the download URL to Android DownloadManager.
    
    BUG=715630
    
    Change-Id: I93adff6d275cc2feeacc3b4e8d7f2cf93b7f216f
    Reviewed-on: https://chromium-review.googlesource.com/759326
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516444}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -286,6 +286,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/android/app_hooks.h"
 #include "chrome/browser/android/chrome_context_util.h"
 #include "chrome/browser/android/devtools_manager_delegate_android.h"
+#include "chrome/browser/android/download/intercept_oma_download_navigation_throttle.h"
 #include "chrome/browser/android/ntp/new_tab_page_url_handler.h"
 #include "chrome/browser/android/service_tab_launcher.h"
 #include "chrome/browser/android/tab_android.h"
@@ -3315,6 +3316,7 @@ ChromeContentBrowserClient::CreateThrottlesForNavigation(
         navigation_interception::InterceptNavigationDelegate::CreateThrottleFor(
             handle));
   }
+  throttles.push_back(InterceptOMADownloadNavigationThrottle::Create(handle));
 #elif BUILDFLAG(ENABLE_EXTENSIONS)
   if (handle->IsInMainFrame()) {
     // Redirect some navigations to apps that have registered matching URL
