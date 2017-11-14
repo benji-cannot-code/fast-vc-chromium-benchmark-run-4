2da5223b5964e25e62a1f4a78d6b738a6af4306a
Alexei Filippov <alph@chromium.org>
2017-11-14 03:25:29 +0000

    DevTools: Migrate remaining tests in http/tests/devtools/profiler
    Bug: 667560
    Change-Id: Ibcfac55e4d4542177ad573005e5dc549d95ef389
    Reviewed-on: https://chromium-review.googlesource.com/767789
    Commit-Queue: Alexei Filippov <alph@chromium.org>
    Reviewed-by: Will Chen <chenwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516168}

--- third_party/WebKit/Source/devtools/front_end/profiler_test_runner/ProfilerTestRunner.js
+++ third_party/WebKit/Source/devtools/front_end/profiler_test_runner/ProfilerTestRunner.js
@@ -70,11 +70,3 @@ ProfilerTestRunner._profileViewRefresh = function() {
     callback.callback(this);
   }
 };
-
-ProfilerTestRunner.startSamplingHeapProfiler = function() {
-  Profiler.SamplingHeapProfileType.instance.startRecordingProfile();
-};
-
-ProfilerTestRunner.stopSamplingHeapProfiler = function() {
-  Profiler.SamplingHeapProfileType.instance.stopRecordingProfile();
-};
