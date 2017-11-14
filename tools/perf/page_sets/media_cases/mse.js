ae08fbecebfa4e40361c291a95fb95463f70ed7c
Caleb Rouleau <crouleau@chromium.org>
2017-11-14 23:39:20 +0000

    [Media Cases] Reduce noise in MSE tests by waiting until xhr completes.
    This will mean that networking changes will not affect
    time_to_video_play and other metrics.
    
    Bug: 784664
    Change-Id: I73204bfcdf650ece2250361264970371544675a1
    Reviewed-on: https://chromium-review.googlesource.com/767889
    Commit-Queue: Caleb Rouleau <crouleau@chromium.org>
    Reviewed-by: Matthew Wolenetz <wolenetz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516500}

--- tools/perf/page_sets/media_cases/mse.js
+++ tools/perf/page_sets/media_cases/mse.js
@@ -80,9 +80,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       FailTest("Test timed out waiting for a mediaElement timeupdate event.");
     }, 10000);
 
-    for (const media_file of testParams.media) {
-      appenders.push(new BufferAppender(media_file, MEDIA_MIMES[media_file]));
-    }
     mediaSource.addEventListener('sourceopen', (open_event) => {
       let mediaSource = open_event.target;
       for (let i = 0; i < appenders.length; ++i) {
@@ -94,15 +91,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       mediaElement.play();
     });
 
-    // Fire off XHRs to get media data.
-    for (var i = 0; i < appenders.length; ++i) {
-      appenders[i].requestMediaBytes();
+    // Do not attach MediaSource object until all the buffer appenders have
+    // received the data from the network that they'll append. This removes
+    // the factor of network overhead from the attachment timing.
+    let number_of_appenders_with_data = 0;
+    for (const media_file of testParams.media) {
+      appender = new BufferAppender(media_file, MEDIA_MIMES[media_file]);
+      appender.requestMediaBytes(() => {
+        number_of_appenders_with_data++;
+        if (number_of_appenders_with_data === testParams.media.length) {
+          // This attaches the mediaSource object to the mediaElement. Once this
+          // operation has completed internally, the mediaSource object
+          // readyState will transition from closed to open, and the sourceopen
+          // event will fire.
+          mediaElement.src = URL.createObjectURL(mediaSource);
+        }
+      });
+      appenders.push(appender);
     }
-
-    // This attaches the mediaSource object to the mediaElement. Once this
-    // operation has completed internally, the mediaSource object readyState
-    // will transition from closed to open, and the sourceopen event will fire.
-    mediaElement.src = URL.createObjectURL(mediaSource);
   }
 
   class BufferAppender {
@@ -112,8 +118,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       this.xhr = new XMLHttpRequest();
       this.sourceBuffer = null;
     }
-    requestMediaBytes() {
-      this.xhr.addEventListener('loadend', this.attemptAppend.bind(this));
+    requestMediaBytes(callback) {
+      this.xhr.addEventListener('loadend', callback);
       this.xhr.open('GET', this.media_file);
       this.xhr.setRequestHeader(
           'Range', 'bytes=' + testParams.startOffset + '-' +
