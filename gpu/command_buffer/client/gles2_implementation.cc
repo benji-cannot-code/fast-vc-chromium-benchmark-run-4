6d0510401cf65390632e1d3ce0970beb8be9d190
Vladimir Levin <vmpstr@chromium.org>
2017-11-14 21:46:52 +0000

    oop: Adjust the display item list query rect by the inverse post scale.
    This patch ensures that if we have a post scale, then we adjust the
    playback rect by it to generate a query rect used to query the display
    item list rtree (since we're going to apply a scale before doing the
    playback).
    
    Added a pixel test that fails to rasterize anything except the background
    color without the patch, and passes with the patch.
    
    R=enne@chromium.org
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Iab82fff14ea8b073cb1a7a874857355526fc7dbc
    Reviewed-on: https://chromium-review.googlesource.com/767665
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516442}

--- gpu/command_buffer/client/gles2_implementation.cc
+++ gpu/command_buffer/client/gles2_implementation.cc
@@ -7214,9 +7214,14 @@ void GLES2Implementation::RasterCHROMIUM(const cc::DisplayItemList* list,
                      << ", " << post_translate_x << ", " << post_translate_y
                      << ", " << post_scale << ")");
 
+  if (std::abs(post_scale) < std::numeric_limits<float>::epsilon())
+    return;
+
   gfx::Rect playback_rect(clip_x, clip_y, clip_w, clip_h);
-  std::vector<size_t> indices = list->rtree_.Search(playback_rect);
-  if (indices.empty())
+  gfx::Rect query_rect =
+      gfx::ScaleToEnclosingRect(playback_rect, 1.f / post_scale);
+  std::vector<size_t> offsets = list->rtree_.Search(query_rect);
+  if (offsets.empty())
     return;
 
   // TODO(enne): tune these numbers
@@ -7239,7 +7244,7 @@ void GLES2Implementation::RasterCHROMIUM(const cc::DisplayItemList* list,
   cc::PaintOpBufferSerializer::SerializeCallback serialize_cb = base::Bind(
       &PaintOpSerializer::Serialize, base::Unretained(&op_serializer));
   cc::PaintOpBufferSerializer serializer(serialize_cb, nullptr);
-  serializer.Serialize(&list->paint_op_buffer_, &indices, preamble);
+  serializer.Serialize(&list->paint_op_buffer_, &offsets, preamble);
   DCHECK(serializer.valid());
   op_serializer.SendSerializedData();
 
