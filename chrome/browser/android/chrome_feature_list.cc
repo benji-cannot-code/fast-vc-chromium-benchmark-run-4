856866385b99b5c227873c103b6eaaecf8b3c51e
Donn Denman <donnd@google.com>
2017-11-14 21:13:07 +0000

    Default-disable Smart Selection in Chrome.
    Switches the default for Smart Selection in Chrome for Android.
    Replaces the CS disable param with an enable Feature.
    
    Update the fieldtrial_testing_config to enable the Feature.
    
    BUG=783932, 756241
    
    Change-Id: I4853943871f320c306fed99ec2d4883af7551ae3
    Reviewed-on: https://chromium-review.googlesource.com/706644
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Donn Denman <donnd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516426}

--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -69,6 +69,7 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &kChromeHomePromoOnStartup,
     &kChromeHomeOptOutSnackbar,
     &kChromeHomeSwipeLogic,
+    &kChromeSmartSelection,
     &kChromeMemexFeature,
     &kContentSuggestionsScrollToLoad,
     &kContentSuggestionsSettings,
@@ -196,6 +197,9 @@ const base::Feature kChromeHomeSwipeLogic{"ChromeHomeSwipeLogic",
 
 const base::Feature kChromeMemexFeature{"ChromeMemex",
                                         base::FEATURE_DISABLED_BY_DEFAULT};
+
+const base::Feature kChromeSmartSelection{"ChromeSmartSelection",
+                                          base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kContentSuggestionsScrollToLoad{
     "ContentSuggestionsScrollToLoad", base::FEATURE_ENABLED_BY_DEFAULT};
