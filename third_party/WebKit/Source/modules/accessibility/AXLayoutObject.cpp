56740d694ae41dcb6993fe277cd95306fe0c5e16
Nektarios Paisios <nektar@chromium.org>
2017-11-14 17:48:16 +0000

    Fixes NextOnLine and PreviousOnLine to work with inline block objects
    There were two bugs:
    1. The next or previous on line might be an anonymous block object that is ignored in the accessibility tree.
    Fixed by assigning the same next and previous on line object to any child, if the child doesn't have its own next or previous on line object.
    2. The object returned by next on line is not the same as the one returned from previous on line on the other object and vice versa.
    Fixed by always descending and returning a leaf object for next and previous on line.
    R=dmazzoni@chromium.org, aleventhal@chromium.org
    
    Bug: 646785
    Change-Id: Ibe6f1fd6b682b9b7345af22f9cbd3ca66f9db801
    Tested: Gmail compose window with a link, NVDA and Jaws with an anonymous block created by background-color: red, layout tests
    Reviewed-on: https://chromium-review.googlesource.com/766908
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516347}

--- third_party/WebKit/Source/modules/accessibility/AXLayoutObject.cpp
+++ third_party/WebKit/Source/modules/accessibility/AXLayoutObject.cpp
@@ -1153,10 +1153,11 @@ AXObject* AXLayoutObject::NextOnLine() const {
     result = next_sibling->Children()[0].Get();
   } else {
     InlineBox* inline_box = nullptr;
-    if (GetLayoutObject()->IsLayoutInline())
+    if (GetLayoutObject()->IsLayoutInline()) {
       inline_box = ToLayoutInline(GetLayoutObject())->LastLineBox();
-    else if (GetLayoutObject()->IsText())
+    } else if (GetLayoutObject()->IsText()) {
       inline_box = ToLayoutText(GetLayoutObject())->LastTextBox();
+    }
 
     if (!inline_box)
       return nullptr;
@@ -1169,12 +1170,14 @@ AXObject* AXLayoutObject::NextOnLine() const {
       if (result)
         break;
     }
+
+    if (!result && ParentObject())
+      result = ParentObject()->NextOnLine();
   }
 
-  // A static text node might span multiple lines. Try to return the first
-  // inline text box within that static text if possible.
-  if (result && result->RoleValue() == kStaticTextRole &&
-      result->Children().size())
+  // For consistency between the forward and backward directions, try to always
+  // return leaf nodes.
+  while (result && result->Children().size())
     result = result->Children()[0].Get();
 
   return result;
@@ -1185,10 +1188,11 @@ AXObject* AXLayoutObject::PreviousOnLine() const {
     return nullptr;
 
   InlineBox* inline_box = nullptr;
-  if (GetLayoutObject()->IsLayoutInline())
+  if (GetLayoutObject()->IsLayoutInline()) {
     inline_box = ToLayoutInline(GetLayoutObject())->FirstLineBox();
-  else if (GetLayoutObject()->IsText())
+  } else if (GetLayoutObject()->IsText()) {
     inline_box = ToLayoutText(GetLayoutObject())->FirstTextBox();
+  }
 
   if (!inline_box)
     return nullptr;
@@ -1203,10 +1207,12 @@ AXObject* AXLayoutObject::PreviousOnLine() const {
       break;
   }
 
-  // A static text node might span multiple lines. Try to return the last inline
-  // text box within that static text if possible.
-  if (result && result->RoleValue() == kStaticTextRole &&
-      result->Children().size())
+  if (!result && ParentObject())
+    result = ParentObject()->PreviousOnLine();
+
+  // For consistency between the forward and backward directions, try to always
+  // return leaf nodes.
+  while (result && result->Children().size())
     result = result->Children()[result->Children().size() - 1].Get();
 
   return result;
