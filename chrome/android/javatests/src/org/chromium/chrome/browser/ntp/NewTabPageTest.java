2d5962988f77629a1ab78fad9f4b1c44087ee0fd
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2017-11-14 22:35:41 +0000

    Disable NewTabPageTest#testRender
    TBR=bauerb@chromium.org
    
    Bug: 784598
    Change-Id: I8c8b2abe341f5ab25b9af7ff06d657943bcf0539
    Reviewed-on: https://chromium-review.googlesource.com/769790
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516469}

--- chrome/android/javatests/src/org/chromium/chrome/browser/ntp/NewTabPageTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ntp/NewTabPageTest.java
@@ -162,6 +162,7 @@ public class NewTabPageTest {
     @Test
     @MediumTest
     @Feature({"NewTabPage", "RenderTest"})
+    @DisabledTest(message = "crbug/784598")
     public void testRender() throws IOException {
         InstrumentationRegistry.getInstrumentation().waitForIdleSync();
         RenderTestRule.sanitize(mNtp.getView());
