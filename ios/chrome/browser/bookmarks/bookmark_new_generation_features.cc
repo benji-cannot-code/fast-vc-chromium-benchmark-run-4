557a80e32b322dc38f3d6e4b0e98e8ddbe61a233
Marti Wong <martiw@chromium.org>
2017-11-14 11:55:52 +0000

    Enable the new bookmarks UI feature flag by default on iOS.
    Bug: 782127
    Change-Id: Id683ed53061f2a34a73e5af96cb307c9b73d3069
    Reviewed-on: https://chromium-review.googlesource.com/768509
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Marti Wong <martiw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516270}

--- ios/chrome/browser/bookmarks/bookmark_new_generation_features.cc
+++ ios/chrome/browser/bookmarks/bookmark_new_generation_features.cc
@@ -7,4 +7,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Feature flag for the new bookmark UI
 const base::Feature kBookmarkNewGeneration{"BookmarkNewGeneration",
-                                           base::FEATURE_DISABLED_BY_DEFAULT};
+                                           base::FEATURE_ENABLED_BY_DEFAULT};
