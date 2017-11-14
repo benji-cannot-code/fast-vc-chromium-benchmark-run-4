e77960a8f7484be824427f4e1ca3dc175a0b8149
Keishi Hattori <keishi@chromium.org>
2017-11-14 07:32:00 +0000

    Validation bubble: Fix UnregisterPopupOpeningObserver from being called before registration
    UpdateLifecycleToCompositingCleanPlusScrolling may call UnregisterPopupOpeningObserver, so RegisterPopupOpeningObserver needs to be called before it.
    
    Bug: 782896
    Change-Id: I277e40edac248698486773436e8f38535b40864c
    Reviewed-on: https://chromium-review.googlesource.com/760436
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516224}

--- third_party/WebKit/Source/core/page/ValidationMessageClientImpl.cpp
+++ third_party/WebKit/Source/core/page/ValidationMessageClientImpl.cpp
@@ -71,6 +71,7 @@ void ValidationMessageClientImpl::ShowValidationMessage(
     HideValidationMessageImmediately(*current_anchor_);
   current_anchor_ = &anchor;
   message_ = message;
+  web_view_.GetChromeClient().RegisterPopupOpeningObserver(this);
   const double kMinimumSecondToShowValidationMessage = 5.0;
   const double kSecondPerCharacter = 0.05;
   finish_time_ =
@@ -89,7 +90,6 @@ void ValidationMessageClientImpl::ShowValidationMessage(
   overlay_ = PageOverlay::Create(target_frame, std::move(delegate));
   target_frame->GetFrameView()
       ->UpdateLifecycleToCompositingCleanPlusScrolling();
-  web_view_.GetChromeClient().RegisterPopupOpeningObserver(this);
   LayoutOverlay();
 }
 
