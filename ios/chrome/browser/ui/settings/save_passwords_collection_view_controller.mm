8c9deb08bcc3697bc8d7e673878ad91cb5f41fe3
Ioana Pandele <ioanap@chromium.org>
2017-11-14 13:09:30 +0000

    Add password sorting to the Save Passwords settings page on iOS
    The same logic used to sort passwords on other platforms will now be used on iOS too. This includes:
        - not displaying duplicates
        - removing all duplicates of an entry from the password store when this entry is selected for
    deletion.
    
    Bug: 764747
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I85070ed7563fcac482b13aa84bb126ff252a68b7
    Reviewed-on: https://chromium-review.googlesource.com/766791
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Vaclav Brozek <vabr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516282}

--- ios/chrome/browser/ui/settings/save_passwords_collection_view_controller.mm
+++ ios/chrome/browser/ui/settings/save_passwords_collection_view_controller.mm
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/common/password_form.h"
 #include "components/google/core/browser/google_util.h"
 #include "components/keyed_service/core/service_access_type.h"
+#include "components/password_manager/core/browser/password_list_sorter.h"
 #include "components/password_manager/core/browser/password_manager_constants.h"
 #include "components/password_manager/core/browser/password_store.h"
 #include "components/password_manager/core/browser/password_store_consumer.h"
@@ -133,6 +134,10 @@ void SavePasswordsConsumer::OnGetPasswordStoreResults(
   // to the PasswordForm, the PasswordForm must outlive the calls to
   // RemoveLogin. This vector will ensure this.
   std::vector<std::unique_ptr<autofill::PasswordForm>> deletedForms_;
+  // Map containing duplicates of saved passwords.
+  password_manager::DuplicatesMap savedPasswordDuplicates_;
+  // Map containing duplicates of blacklisted passwords.
+  password_manager::DuplicatesMap blacklistedPasswordDuplicates_;
   // The current Chrome browser state.
   ios::ChromeBrowserState* browserState_;
   // Object storing the time of the previous successful re-authentication.
@@ -420,6 +425,13 @@ void SavePasswordsConsumer::OnGetPasswordStoreResults(
       savedForms_.push_back(std::move(form));
   }
 
+  password_manager::SortEntriesAndHideDuplicates(
+      &savedForms_, &savedPasswordDuplicates_,
+      password_manager::PasswordEntryType::SAVED);
+  password_manager::SortEntriesAndHideDuplicates(
+      &blacklistedForms_, &blacklistedPasswordDuplicates_,
+      password_manager::PasswordEntryType::BLACKLISTED);
+
   [self updateEditButton];
   [self reloadData];
 }
@@ -512,9 +524,24 @@ void SavePasswordsConsumer::OnGetPasswordStoreResults(
     // Adjust index to account for deleted items.
     formIndex -= blacklisted ? blacklistedDeleted : passwordsDeleted;
     auto& forms = blacklisted ? blacklistedForms_ : savedForms_;
+    auto& duplicates =
+        blacklisted ? blacklistedPasswordDuplicates_ : savedPasswordDuplicates_;
+    password_manager::PasswordEntryType entryType =
+        blacklisted ? password_manager::PasswordEntryType::BLACKLISTED
+                    : password_manager::PasswordEntryType::SAVED;
+
     DCHECK_LT(formIndex, forms.size());
     auto formIterator = forms.begin() + formIndex;
+
     std::unique_ptr<autofill::PasswordForm> form = std::move(*formIterator);
+    std::string key = password_manager::CreateSortKey(*form, entryType);
+    auto duplicatesRange = duplicates.equal_range(key);
+    for (auto iterator = duplicatesRange.first;
+         iterator != duplicatesRange.second; ++iterator) {
+      passwordStore_->RemoveLogin(*(iterator->second));
+    }
+    duplicates.erase(key);
+
     forms.erase(formIterator);
     passwordStore_->RemoveLogin(*form);
     deletedForms_.push_back(std::move(form));
