7d8c4cac0112ff81633ae58ee1ef6571c370cc5e
Tom Anderson <thomasanderson@chromium.org>
2017-11-14 23:39:15 +0000

    Use regexps for license check exclusions
    One gotcha when adding/switching to a new sysroot is updating the
    license check script.  This CL changes the script to use a regexp that
    should handle future sysroots.
    
    R=thestig@chromium.org
    
    Change-Id: I7ed24ebb80faa1ac98e5a7a5751c6afc8f436555
    Reviewed-on: https://chromium-review.googlesource.com/768101
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516499}

--- tools/checklicenses/checklicenses.py
+++ tools/checklicenses/checklicenses.py
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import json
 import optparse
 import os.path
+import re
 import subprocess
 import sys
 
@@ -647,22 +648,13 @@ PATH_SPECIFIC_WHITELISTED_LICENSES = {
 
 EXCLUDED_PATHS = [
     # Don't check generated files
-    'out/',
+    re.compile('^out/'),
 
     # Don't check downloaded goma client binaries
-    'build/goma/client/',
+    re.compile('^build/goma/client/'),
 
     # Don't check sysroot directories
-    'build/linux/debian_jessie_arm64-sysroot/',
-    'build/linux/debian_jessie_amd64-sysroot/',
-    'build/linux/debian_jessie_arm-sysroot/',
-    'build/linux/debian_jessie_i386-sysroot/',
-    'build/linux/debian_jessie_mips-sysroot/',
-    'build/linux/debian_stretch_arm64-sysroot/',
-    'build/linux/debian_stretch_amd64-sysroot/',
-    'build/linux/debian_stretch_arm-sysroot/',
-    'build/linux/debian_stretch_i386-sysroot/',
-    'build/linux/debian_stretch_mips-sysroot/',
+    re.compile('^build/linux/.+-sysroot/'),
 ]
 
 
@@ -714,7 +706,7 @@ def check_licenses(options, args):
     filename = os.path.relpath(filename.strip(), options.base_directory)
 
     # Check if the file belongs to one of the excluded paths.
-    if any((filename.startswith(path) for path in EXCLUDED_PATHS)):
+    if any((pattern.match(filename) for pattern in EXCLUDED_PATHS)):
       continue
 
     # For now we're just interested in the license.
