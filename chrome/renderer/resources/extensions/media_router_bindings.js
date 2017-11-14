e2fe45b791f4f8761a8b18c03ec61dd18dfd5aec
Takumi Fujimoto <takumif@chromium.org>
2017-11-14 23:11:47 +0000

    [Media Router] Update safeguards in media_router_bindings
    Adding a default value for presentation ID in routeToMojo_().
    Also removing safeguards that are no longer necessary.
    
    Bug: 779155
    Change-Id: I4d39e3dcc1d5495d02c794b693f2f18824bcff4d
    Reviewed-on: https://chromium-review.googlesource.com/769570
    Reviewed-by: Derek Cheng <imcheng@chromium.org>
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516489}

--- chrome/renderer/resources/extensions/media_router_bindings.js
+++ chrome/renderer/resources/extensions/media_router_bindings.js
@@ -95,11 +95,11 @@ define('media_router_bindings', [
       'for_display': route.forDisplay,
       'is_incognito': route.offTheRecord,
       'is_local_presentation': route.isOffscreenPresentation,
-      'supports_media_route_controller': !!route.supportsMediaRouteController,
+      'supports_media_route_controller': route.supportsMediaRouteController,
       'controller_type': route.controllerType,
       // Begin newly added properties, followed by the milestone they were
       // added.  The guard should be safe to remove N+2 milestones later.
-      'presentation_id': route.presentationId
+      'presentation_id': route.presentationId || ''  // M64
     });
   }
 
@@ -165,35 +165,6 @@ define('media_router_bindings', [
             reason);
         return PresentationConnectionCloseReason.CONNECTION_ERROR;
     }
-  }
-
-  // TODO(crbug.com/688177): remove this conversion when M60 is in stable.
-  /**
-   * Converts string to Mojo origin.
-   * @param {string|!originMojom.Origin} origin
-   * @return {!originMojom.Origin}
-   */
-  function stringToMojoOrigin_(origin) {
-    if (origin instanceof originMojom.Origin) {
-      return origin;
-    }
-    const url = new URL(origin);
-    const mojoOrigin = {};
-    mojoOrigin.scheme = url.protocol.replace(':', '');
-    mojoOrigin.host = url.hostname;
-    var port = url.port ? Number.parseInt(url.port) : 0;
-    switch (mojoOrigin.scheme) {
-      case 'http':
-        mojoOrigin.port = port || 80;
-        break;
-      case 'https':
-        mojoOrigin.port = port || 443;
-        break;
-      default:
-        throw new Error('Scheme must be http or https');
-    }
-    mojoOrigin.suborigin = '';
-    return new originMojom.Origin(mojoOrigin);
   }
 
   /**
@@ -347,12 +318,12 @@ define('media_router_bindings', [
    * updated.
    * @param {!string} sourceUrn
    * @param {!Array<!MediaSink>} sinks
-   * @param {!Array<string>} origins
+   * @param {!Array<!originMojom.Origin>} origins
    */
   MediaRouter.prototype.onSinksReceived = function(sourceUrn, sinks, origins) {
     this.service_.onSinksReceived(
         mediaRouterMojom.MediaRouteProvider.Id.EXTENSION, sourceUrn,
-        sinks.map(sinkToMojo_), origins.map(stringToMojoOrigin_));
+        sinks.map(sinkToMojo_), origins);
   };
 
   /**
@@ -953,11 +924,6 @@ define('media_router_bindings', [
    */
   MediaRouteProvider.prototype.createMediaRouteController = function(
       routeId, controllerRequest, observer) {
-    // TODO(imcheng): Remove this check when M60 is in stable.
-    if (!this.handlers_.createMediaRouteController) {
-      return Promise.resolve({success: false});
-    }
-
     this.handlers_.onBeforeInvokeHandler();
     return this.handlers_
         .createMediaRouteController(routeId, controllerRequest, observer)
