412e55340e3817b51836e6946d024def0d9fd0d7
Pavel Yatsuk <pavely@chromium.org>
2017-11-14 17:10:43 +0000

    Reland: [Sync] Switch processor/worker to pull model
    This change makes worker use results of GetLocalChanges when preparing commit
    contribution. EnqueueForCommit is not used and therefore removed.
    
    With this change the expectation is that ModelTypeWorker::entities_ only
    accumulate entities in two scenarios:
    - Between calls to ProcessGetUpdatesResponse and ApplyUpdates remote updates are
      accumulated
    - Between calls to GetContribution and OnCommit response local changes are
      maintained
    
    Encryption is still done at ModelTypeWorker. There are a couple of important
    scenarios related to encryption:
    - After processor nudges worker about local changes worker should remember about
      that nudge in case cryptographer is not ready. When cryptographer becomes
      ready worker should nudge a NudgeHandler, processor is not going to reissue
      the nudge.
    - When worker receives corrupt encrypted update it should skip the update and
      continue with the rest of entities in the batch. It shouldn't block datatype.
    
    Another important scenario concerns commit failures. When commit fails on the
    server NonBlockingTypeCommitContribution would not include corresponding
    CommitResponseData in the response_list. Wth this change worker is not
    retaining/recommitting entities so processor should do that.
    
    This CL includes bunch of random small cleanups I stumbled upon while reviewing
    implementation.
    
    BUG=740757
    R=skym@chromium.org
    
    Reviewed-on: https://chromium-review.googlesource.com/729520
    Commit-Queue: Pavel Yatsuk <pavely@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#511258}
    Change-Id: I753173b6638d802c2057472f7a612e1b8f876d4c
    Reviewed-on: https://chromium-review.googlesource.com/753983
    Cr-Commit-Position: refs/heads/master@{#516333}

--- components/sync/test/engine/mock_model_type_processor.h
+++ components/sync/test/engine/mock_model_type_processor.h
@@ -97,6 +97,11 @@ class MockModelTypeProcessor : public ModelTypeProcessor {
 
   void SetDisconnectCallback(const DisconnectCallback& callback);
 
+  // Sets commit request that will be returned by GetLocalChanges().
+  void SetCommitRequest(const CommitRequestDataList& commit_request);
+
+  int GetLocalChangesCallCount() const;
+
  private:
   // Process a received commit response.
   //
@@ -150,6 +155,11 @@ class MockModelTypeProcessor : public ModelTypeProcessor {
 
   // Callback which will be call during disconnection
   DisconnectCallback disconnect_callback_;
+
+  // Commit request to return from GetLocalChanges().
+  CommitRequestDataList commit_request_;
+
+  int get_local_changes_call_count_ = 0;
 
   DISALLOW_COPY_AND_ASSIGN(MockModelTypeProcessor);
 };
