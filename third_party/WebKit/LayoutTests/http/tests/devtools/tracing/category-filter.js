d326dd1c94e29efb9c2f291ccee934bcf236fece
Will Chen <chenwilliam@chromium.org>
2017-11-14 20:35:06 +0000

    Reland of DevTools: automatically migrate http/tests/devtools/tracing/*.html
    This was originally reverted because it uncovered a flakiness with the new
    test framework that has since been fixed:
    * https://chromium-review.googlesource.com/c/chromium/src/+/761957
    
    Original CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/731794
    
    This converts a batch of tests from the old test framework (html file)
    into the new test framework (js file).
    
    Most of it was done through an automated transformation script, with
    a small amount of manual clean-up.
    
    TBR=caseq@chromium.org
    
    Bug: 667560
    Change-Id: I786a2ad181281cf84f8c30b4abe1904318e479c5
    Reviewed-on: https://chromium-review.googlesource.com/769331
    Reviewed-by: Will Chen <chenwilliam@chromium.org>
    Commit-Queue: Will Chen <chenwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516410}

--- third_party/WebKit/LayoutTests/http/tests/devtools/tracing/category-filter.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/tracing/category-filter.js
@@ -1,11 +1,13 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-<html>
-<head>
-<script src="../../inspector/inspector-test.js"></script>
-<script src="../../inspector/timeline-test.js"></script>
-<script>
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
-function test() {
+(async function() {
+  TestRunner.addResult(`Test the set of visible records is correctly update when category filter changes\n`);
+  await TestRunner.loadModule('performance_test_runner');
+  await TestRunner.showPanel('timeline');
+
   var sessionId = '4.20';
   var mainThread = 1;
   var pid = 100;
@@ -118,15 +120,4 @@ function test() {
   function dumpVisibleRecords() {
     PerformanceTestRunner.walkTimelineEventTreeUnderNode(event => TestRunner.addResult(event.name), view._currentTree);
   }
-}
-
-</script>
-</head>
-
-<body onload="runTest()">
-<p>
-Test the set of visible records is correctly update when category filter changes</a>
-</p>
-
-</body>
-</html>
+})();
