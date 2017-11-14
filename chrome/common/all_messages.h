8b02f386d859754d08320097708d89195b9369ef
Tom Sepez <tsepez@chromium.org>
2017-11-14 17:04:52 +0000

    Fix ipc_message_list and suppressions
    Re-enable listing/fuzzing of some messages lost by jumbo conversion.
    Suppress one batch that is obsolescent.
    
    Change-Id: I563b493732f6523901be450fbf88c667717ead76
    Reviewed-on: https://chromium-review.googlesource.com/767208
    Reviewed-by: Abhishek Arya <inferno@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516332}

--- chrome/common/all_messages.h
+++ chrome/common/all_messages.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(ENABLE_PRINTING)
 // TODO(dgn) remove from here when all the code using these messages is removed
 // from /chrome. (crbug.com/311308, crbug.com/450822)
+#undef COMPONENTS_PRINTING_COMMON_PRINT_MESSAGES_H_
 #include "components/printing/common/print_messages.h"  // nogncheck
 #endif
 
