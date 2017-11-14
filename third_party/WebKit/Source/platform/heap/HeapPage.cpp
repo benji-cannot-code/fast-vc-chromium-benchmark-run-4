2adc225405fe84012e3f7f20ed709bb2947dd5b4
Keishi Hattori <keishi@chromium.org>
2017-11-14 02:26:40 +0000

    Oilpan: Add magic number to BasePage
    Adds magic number to BasePage and use it to validate page in PageFromObject().
    
    Bug: 
    Change-Id: I298588e1c61ec49a878a3b02f29650156bbc02e3
    Reviewed-on: https://chromium-review.googlesource.com/756716
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516149}

--- third_party/WebKit/Source/platform/heap/HeapPage.cpp
+++ third_party/WebKit/Source/platform/heap/HeapPage.cpp
@@ -1267,7 +1267,8 @@ bool FreeList::TakeSnapshot(const String& dump_base_name) {
 }
 
 BasePage::BasePage(PageMemory* storage, BaseArena* arena)
-    : storage_(storage),
+    : magic_(GetMagic()),
+      storage_(storage),
       arena_(arena),
       next_(nullptr),
       swept_(true),
