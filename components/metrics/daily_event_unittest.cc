c7ef6fff1a913089e50b967716051f0f235c983a
Daniel Erat <derat@chromium.org>
2017-11-14 22:30:51 +0000

    metrics: Make DailyEvent pass reason to observers.
    Make metrics::DailyEvent pass observers the reason that it's
    notifying them. This allows observers to e.g. ignore
    first-run notifications if they only want to report metrics
    when a full day has passed.
    
    Bug: 782953
    Change-Id: I19b7bd627f56563d59dd102bb234c0e759eecb4a
    Reviewed-on: https://chromium-review.googlesource.com/766549
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Commit-Queue: Dan Erat <derat@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516467}

--- components/metrics/daily_event_unittest.cc
+++ components/metrics/daily_event_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
+#include "base/optional.h"
 #include "components/prefs/testing_pref_service.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -19,19 +20,18 @@ const char kTestMetricName[] = "TestMetric";
 
 class TestDailyObserver : public DailyEvent::Observer {
  public:
-  TestDailyObserver() : fired_(false) {}
+  TestDailyObserver() = default;
 
-  bool fired() const { return fired_; }
+  bool fired() const { return type_.has_value(); }
+  DailyEvent::IntervalType type() const { return type_.value(); }
 
-  void OnDailyEvent() override { fired_ = true; }
+  void OnDailyEvent(DailyEvent::IntervalType type) override { type_ = type; }
 
-  void Reset() {
-    fired_ = false;
-  }
+  void Reset() { type_ = {}; }
 
  private:
-  // True if this event has been observed.
-  bool fired_;
+  // Last-received type, or unset if OnDailyEvent() hasn't been called.
+  base::Optional<DailyEvent::IntervalType> type_;
 
   DISALLOW_COPY_AND_ASSIGN(TestDailyObserver);
 };
@@ -58,7 +58,8 @@ class DailyEventTest : public testing::Test {
 // The event should fire if the preference is not available.
 TEST_F(DailyEventTest, TestNewFires) {
   event_.CheckInterval();
-  EXPECT_TRUE(observer_->fired());
+  ASSERT_TRUE(observer_->fired());
+  EXPECT_EQ(DailyEvent::IntervalType::FIRST_RUN, observer_->type());
 }
 
 // The event should fire if the preference is more than a day old.
@@ -66,7 +67,8 @@ TEST_F(DailyEventTest, TestOldFires) {
   base::Time last_time = base::Time::Now() - base::TimeDelta::FromHours(25);
   prefs_.SetInt64(kTestPrefName, last_time.since_origin().InMicroseconds());
   event_.CheckInterval();
-  EXPECT_TRUE(observer_->fired());
+  ASSERT_TRUE(observer_->fired());
+  EXPECT_EQ(DailyEvent::IntervalType::DAY_ELAPSED, observer_->type());
 }
 
 // The event should fire if the preference is more than a day in the future.
@@ -74,7 +76,8 @@ TEST_F(DailyEventTest, TestFutureFires) {
   base::Time last_time = base::Time::Now() + base::TimeDelta::FromHours(25);
   prefs_.SetInt64(kTestPrefName, last_time.since_origin().InMicroseconds());
   event_.CheckInterval();
-  EXPECT_TRUE(observer_->fired());
+  ASSERT_TRUE(observer_->fired());
+  EXPECT_EQ(DailyEvent::IntervalType::CLOCK_CHANGED, observer_->type());
 }
 
 // The event should not fire if the preference is more recent than a day.
