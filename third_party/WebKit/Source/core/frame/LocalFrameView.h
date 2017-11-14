413a4e4e0b57a765f9dec31bafc6d834f3747b28
Ken Buchanan <kenrb@chromium.org>
2017-11-14 22:15:57 +0000

    Reland: Enable render throttling for OOPIFs
    Originally landed here:
    https://chromium-review.googlesource.com/c/chromium/src/+/719601
    
    The change caused a racy test to flake on Mac and was reverted. This
    patch restores the original CL and fixes the cause of the flakiness in
    the problematic test.
    
    TBR=dcheng@chromium.org
    
    Bug: 761478, 784236
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I0a4da143505b60ce21083413f741e4fe24b0db42
    Reviewed-on: https://chromium-review.googlesource.com/769489
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516454}

--- third_party/WebKit/Source/core/frame/LocalFrameView.h
+++ third_party/WebKit/Source/core/frame/LocalFrameView.h
@@ -903,6 +903,17 @@ class CORE_EXPORT LocalFrameView final
 
   ScrollbarTheme& GetPageScrollbarTheme() const override;
 
+  enum ForceThrottlingInvalidationBehavior {
+    kDontForceThrottlingInvalidation,
+    kForceThrottlingInvalidation
+  };
+  enum NotifyChildrenBehavior { kDontNotifyChildren, kNotifyChildren };
+  void UpdateRenderThrottlingStatus(
+      bool hidden,
+      bool subtree_throttled,
+      ForceThrottlingInvalidationBehavior = kDontForceThrottlingInvalidation,
+      NotifyChildrenBehavior = kNotifyChildren);
+
  protected:
   // Scroll the content via the compositor.
   bool ScrollContentsFastPath(const IntSize& scroll_delta);
@@ -1095,16 +1106,6 @@ class CORE_EXPORT LocalFrameView final
   void UpdateViewportIntersectionsForSubtree(
       DocumentLifecycle::LifecycleState) override;
 
-  enum ForceThrottlingInvalidationBehavior {
-    kDontForceThrottlingInvalidation,
-    kForceThrottlingInvalidation
-  };
-  enum NotifyChildrenBehavior { kDontNotifyChildren, kNotifyChildren };
-  void UpdateRenderThrottlingStatus(
-      bool hidden,
-      bool subtree_throttled,
-      ForceThrottlingInvalidationBehavior = kDontForceThrottlingInvalidation,
-      NotifyChildrenBehavior = kNotifyChildren);
   void NotifyResizeObservers();
 
   // PaintInvalidationCapableScrollableArea
