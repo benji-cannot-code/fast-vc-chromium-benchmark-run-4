c1c4521f43e4a88aebd6f6d7bb2856f37aa29e9c
Paul Meyer <paulmeyer@chromium.org>
2017-11-15 15:55:34 +0000

    Make Supplementable const-compatible.
    There is currently no way to get a Supplement off of a const
    Supplementable, simply because Supplementable::From() does not
    accept a const Supplementable. This patch allows this.
    
    Change-Id: If237f7473c1c0afb7428d0dfd97a61806835e91a
    Reviewed-on: https://chromium-review.googlesource.com/767667
    Commit-Queue: Paul Meyer <paulmeyer@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516706}

--- third_party/WebKit/Source/platform/Supplementable.h
+++ third_party/WebKit/Source/platform/Supplementable.h
@@ -124,12 +124,12 @@ class Supplement : public GarbageCollectedMixin,
     supplementable.ProvideSupplement(key, supplement);
   }
 
-  static Supplement<T>* From(Supplementable<T>& supplementable,
+  static Supplement<T>* From(const Supplementable<T>& supplementable,
                              const char* key) {
     return supplementable.RequireSupplement(key);
   }
 
-  static Supplement<T>* From(Supplementable<T>* supplementable,
+  static Supplement<T>* From(const Supplementable<T>* supplementable,
                              const char* key) {
     return supplementable ? supplementable->RequireSupplement(key) : 0;
   }
@@ -161,7 +161,7 @@ class Supplementable : public GarbageCollectedMixin {
     this->supplements_.erase(key);
   }
 
-  Supplement<T>* RequireSupplement(const char* key) {
+  Supplement<T>* RequireSupplement(const char* key) const {
 #if DCHECK_IS_ON()
     DCHECK_EQ(attached_thread_id_, CurrentThread());
 #endif
