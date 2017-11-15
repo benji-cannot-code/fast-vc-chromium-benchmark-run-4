cb9b88b6056f7379940293d8b3d22fbf70225d9f
Dominic Mazzoni <dmazzoni@chromium.org>
2017-11-15 19:05:42 +0000

    Refactor automation TreeCache into AutomationAXTreeWrapper
    This is a pure refactoring change. Turns the TreeCache struct into a
    class called AutomationAXTreeWrapper without any other side effects.
    
    The motivation behind this is that I'd like each
    AutomationAXTreeWrapper to extend AXEventGenerator, rather than
    AutomationInternalCustomBindings extending it since
    AutomationInternalCustomBindings is global to all trees.
    
    Over time I think it would make sense to move more of the code that's
    specific to one accessibility tree to AutomationAXTreeWrapper too.
    
    Bug: none
    Change-Id: Ic5f38261c9e0ad76de2e1f3fed8f047a7cc1bd9a
    Reviewed-on: https://chromium-review.googlesource.com/687988
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516773}

---
+++ chrome/renderer/extensions/automation_ax_tree_wrapper.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/renderer/extensions/automation_internal_custom_bindings.h"
+
+namespace extensions {
+
+AutomationAXTreeWrapper::AutomationAXTreeWrapper(
+    int32_t tree_id,
+    AutomationInternalCustomBindings* owner)
+    : tree_id_(tree_id), host_node_id_(-1), owner_(owner) {
+  tree_.SetDelegate(owner);
+}
+
+AutomationAXTreeWrapper::~AutomationAXTreeWrapper() {
+  // Explicitly clear the delegate so that we don't get any more
+  // callbacks as the tree is destroyed.
+  tree_.SetDelegate(nullptr);
+}
+
+}  // namespace extensions
