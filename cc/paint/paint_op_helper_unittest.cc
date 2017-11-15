d7d1fdce183d892a12ee09c47048043c181c9da9
Vladimir Levin <vmpstr@chromium.org>
2017-11-15 00:17:33 +0000

    cc/paint: Add type to paint op ctors.
    This patch adds PaintOp::type initialization to all paint op ctors.
    This ensures that users that create paint ops directly don't need to
    remember to set the type, since it's otherwise uninitialized.
    
    This also removes some unused default ctors from some paint ops.
    
    R=enne@chromium.org
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I4ae0a3e05480f12e7b8e8e526e6c20d5bb96aead
    Reviewed-on: https://chromium-review.googlesource.com/769165
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Reviewed-by: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516516}

--- cc/paint/paint_op_helper_unittest.cc
+++ cc/paint/paint_op_helper_unittest.cc
@@ -14,7 +14,6 @@ namespace {
 TEST(PaintOpHelper, AnnotateToString) {
   AnnotateOp op(PaintCanvas::AnnotationType::URL, SkRect::MakeXYWH(1, 2, 3, 4),
                 nullptr);
-  op.type = static_cast<uint32_t>(AnnotateOp::kType);
   std::string str = PaintOpHelper::ToString(&op);
   EXPECT_EQ(
       str,
@@ -23,7 +22,6 @@ TEST(PaintOpHelper, AnnotateToString) {
 
 TEST(PaintOpHelper, ClipPathToString) {
   ClipPathOp op(SkPath(), SkClipOp::kDifference, true);
-  op.type = static_cast<uint32_t>(ClipPathOp::kType);
   std::string str = PaintOpHelper::ToString(&op);
   EXPECT_EQ(str, "ClipPathOp(path=<SkPath>, op=kDifference, antialias=true)");
 }
@@ -31,7 +29,6 @@ TEST(PaintOpHelper, ClipPathToString) {
 TEST(PaintOpHelper, ClipRectToString) {
   ClipRectOp op(SkRect::MakeXYWH(10.1f, 20.2f, 30.3f, 40.4f),
                 SkClipOp::kIntersect, false);
-  op.type = static_cast<uint32_t>(ClipRectOp::kType);
   std::string str = PaintOpHelper::ToString(&op);
   EXPECT_EQ(str,
             "ClipRectOp(rect=[10.100,20.200 30.300x40.400], op=kIntersect, "
@@ -41,7 +38,6 @@ TEST(PaintOpHelper, ClipRectToString) {
 TEST(PaintOpHelper, ClipRRectToString) {
   ClipRRectOp op(SkRRect::MakeRect(SkRect::MakeXYWH(1, 2, 3, 4)),
                  SkClipOp::kDifference, false);
-  op.type = static_cast<uint32_t>(ClipRRectOp::kType);
   std::string str = PaintOpHelper::ToString(&op);
   EXPECT_EQ(str,
             "ClipRRectOp(rrect=[bounded by 1.000,2.000 3.000x4.000], "
@@ -50,7 +46,6 @@ TEST(PaintOpHelper, ClipRRectToString) {
 
 TEST(PaintOpHelper, ConcatToString) {
   ConcatOp op(SkMatrix::MakeAll(1, 2, 3, 4, 5, 6, 7, 8, 9));
-  op.type = static_cast<uint32_t>(ConcatOp::kType);
   std::string str = PaintOpHelper::ToString(&op);
   EXPECT_EQ(str,
             "ConcatOp(matrix=[  1.0000   2.0000   3.0000][  4.0000   5.0000   "
@@ -59,7 +54,6 @@ TEST(PaintOpHelper, ConcatToString) {
 
 TEST(PaintOpHelper, DrawColorToString) {
   DrawColorOp op(SkColorSetARGB(11, 22, 33, 44), SkBlendMode::kSrc);
-  op.type = static_cast<uint32_t>(DrawColorOp::kType);
   std::string str = PaintOpHelper::ToString(&op);
   EXPECT_EQ(str, "DrawColorOp(color=rgba(22, 33, 44, 11), mode=kSrc)");
 }
@@ -68,7 +62,6 @@ TEST(PaintOpHelper, DrawDRRectToString) {
   DrawDRRectOp op(SkRRect::MakeRect(SkRect::MakeXYWH(1, 2, 3, 4)),
                   SkRRect::MakeRect(SkRect::MakeXYWH(5, 6, 7, 8)),
                   PaintFlags());
-  op.type = static_cast<uint32_t>(DrawDRRectOp::kType);
   std::string str = PaintOpHelper::ToString(&op);
   EXPECT_EQ(str,
             "DrawDRRectOp(outer=[bounded by 1.000,2.000 3.000x4.000], "
@@ -77,7 +70,6 @@ TEST(PaintOpHelper, DrawDRRectToString) {
 
 TEST(PaintOpHelper, DrawImageToString) {
   DrawImageOp op(PaintImage(), 10.5f, 20.3f, nullptr);
-  op.type = static_cast<uint32_t>(DrawImageOp::kType);
   std::string str = PaintOpHelper::ToString(&op);
   EXPECT_EQ(str, "DrawImageOp(image=<paint image>, left=10.500, top=20.300)");
 }
@@ -86,7 +78,6 @@ TEST(PaintOpHelper, DrawImageRectToString) {
   DrawImageRectOp op(PaintImage(), SkRect::MakeXYWH(1, 2, 3, 4),
                      SkRect::MakeXYWH(5, 6, 7, 8), nullptr,
                      PaintCanvas::kStrict_SrcRectConstraint);
-  op.type = static_cast<uint32_t>(DrawImageRectOp::kType);
   std::string str = PaintOpHelper::ToString(&op);
   EXPECT_EQ(str,
             "DrawImageRectOp(image=<paint image>, src=[1.000,2.000 "
@@ -96,21 +87,18 @@ TEST(PaintOpHelper, DrawImageRectToString) {
 
 TEST(PaintOpHelper, DrawIRectToString) {
   DrawIRectOp op(SkIRect::MakeXYWH(1, 2, 3, 4), PaintFlags());
-  op.type = static_cast<uint32_t>(DrawIRectOp::kType);
   std::string str = PaintOpHelper::ToString(&op);
   EXPECT_EQ(str, "DrawIRectOp(rect=[1,2 3x4])");
 }
 
 TEST(PaintOpHelper, DrawLineToString) {
   DrawLineOp op(1.1f, 2.2f, 3.3f, 4.4f, PaintFlags());
-  op.type = static_cast<uint32_t>(DrawLineOp::kType);
   std::string str = PaintOpHelper::ToString(&op);
   EXPECT_EQ(str, "DrawLineOp(x0=1.100, y0=2.200, x1=3.300, y1=4.400)");
 }
 
 TEST(PaintOpHelper, DrawOvalToString) {
   DrawOvalOp op(SkRect::MakeXYWH(100, 200, 300, 400), PaintFlags());
-  op.type = static_cast<uint32_t>(DrawOvalOp::kType);
   std::string str = PaintOpHelper::ToString(&op);
   EXPECT_EQ(str, "DrawOvalOp(oval=[100.000,200.000 300.000x400.000])");
 }
@@ -118,21 +106,18 @@ TEST(PaintOpHelper, DrawOvalToString) {
 TEST(PaintOpHelper, DrawPathToString) {
   SkPath path;
   DrawPathOp op(path, PaintFlags());
-  op.type = static_cast<uint32_t>(DrawPathOp::kType);
   std::string str = PaintOpHelper::ToString(&op);
   EXPECT_EQ(str, "DrawPathOp(path=<SkPath>)");
 }
 
 TEST(PaintOpHelper, DrawRecordToString) {
   DrawRecordOp op(nullptr);
-  op.type = static_cast<uint32_t>(DrawRecordOp::kType);
   std::string str = PaintOpHelper::ToString(&op);
   EXPECT_EQ(str, "DrawRecordOp(record=<paint record>)");
 }
 
 TEST(PaintOpHelper, DrawRectToString) {
   DrawRectOp op(SkRect::MakeXYWH(-1, -2, -3, -4), PaintFlags());
-  op.type = static_cast<uint32_t>(DrawRectOp::kType);
   std::string str = PaintOpHelper::ToString(&op);
   EXPECT_EQ(str, "DrawRectOp(rect=[-1.000,-2.000 -3.000x-4.000])");
 }
@@ -140,14 +125,12 @@ TEST(PaintOpHelper, DrawRectToString) {
 TEST(PaintOpHelper, DrawRRectToString) {
   DrawRRectOp op(SkRRect::MakeRect(SkRect::MakeXYWH(-1, -2, 3, 4)),
                  PaintFlags());
-  op.type = static_cast<uint32_t>(DrawRRectOp::kType);
   std::string str = PaintOpHelper::ToString(&op);
   EXPECT_EQ(str, "DrawRRectOp(rrect=[bounded by -1.000,-2.000 3.000x4.000])");
 }
 
 TEST(PaintOpHelper, DrawTextBlobToString) {
   DrawTextBlobOp op(nullptr, 100, -222, PaintFlags());
-  op.type = static_cast<uint32_t>(DrawTextBlobOp::kType);
   std::string str = PaintOpHelper::ToString(&op);
   EXPECT_EQ(str,
             "DrawTextBlobOp(blob=<paint text blob>, x=100.000, y=-222.000)");
@@ -155,28 +138,24 @@ TEST(PaintOpHelper, DrawTextBlobToString) {
 
 TEST(PaintOpHelper, NoopToString) {
   NoopOp op;
-  op.type = static_cast<uint32_t>(NoopOp::kType);
   std::string str = PaintOpHelper::ToString(&op);
   EXPECT_EQ(str, "NoopOp()");
 }
 
 TEST(PaintOpHelper, RestoreToString) {
   RestoreOp op;
-  op.type = static_cast<uint32_t>(RestoreOp::kType);
   std::string str = PaintOpHelper::ToString(&op);
   EXPECT_EQ(str, "RestoreOp()");
 }
 
 TEST(PaintOpHelper, RotateToString) {
   RotateOp op(360);
-  op.type = static_cast<uint32_t>(RotateOp::kType);
   std::string str = PaintOpHelper::ToString(&op);
   EXPECT_EQ(str, "RotateOp(degrees=360.000)");
 }
 
 TEST(PaintOpHelper, SaveToString) {
   SaveOp op;
-  op.type = static_cast<uint32_t>(SaveOp::kType);
   std::string str = PaintOpHelper::ToString(&op);
   EXPECT_EQ(str, "SaveOp()");
 }
@@ -184,7 +163,6 @@ TEST(PaintOpHelper, SaveToString) {
 TEST(PaintOpHelper, SaveLayerToString) {
   SkRect bounds = SkRect::MakeXYWH(1, 2, 3, 4);
   SaveLayerOp op(&bounds, nullptr);
-  op.type = static_cast<uint32_t>(SaveLayerOp::kType);
   std::string str = PaintOpHelper::ToString(&op);
   EXPECT_EQ(str, "SaveLayerOp(bounds=[1.000,2.000 3.000x4.000])");
 }
@@ -192,7 +170,6 @@ TEST(PaintOpHelper, SaveLayerToString) {
 TEST(PaintOpHelper, SaveLayerAlphaToString) {
   SkRect bounds = SkRect::MakeXYWH(1, 2, 3, 4);
   SaveLayerAlphaOp op(&bounds, 255, false);
-  op.type = static_cast<uint32_t>(SaveLayerAlphaOp::kType);
   std::string str = PaintOpHelper::ToString(&op);
   EXPECT_EQ(str,
             "SaveLayerAlphaOp(bounds=[1.000,2.000 3.000x4.000], alpha=255, "
@@ -201,14 +178,12 @@ TEST(PaintOpHelper, SaveLayerAlphaToString) {
 
 TEST(PaintOpHelper, ScaleToString) {
   ScaleOp op(12, 13.9f);
-  op.type = static_cast<uint32_t>(ScaleOp::kType);
   std::string str = PaintOpHelper::ToString(&op);
   EXPECT_EQ(str, "ScaleOp(sx=12.000, sy=13.900)");
 }
 
 TEST(PaintOpHelper, SetMatrixToString) {
   SetMatrixOp op(SkMatrix::MakeAll(-1, 2, -3, 4, -5, 6, -7, 8, -9));
-  op.type = static_cast<uint32_t>(SetMatrixOp::kType);
   std::string str = PaintOpHelper::ToString(&op);
   EXPECT_EQ(str,
             "SetMatrixOp(matrix=[ -1.0000   2.0000  -3.0000][  4.0000  -5.0000 "
@@ -217,7 +192,6 @@ TEST(PaintOpHelper, SetMatrixToString) {
 
 TEST(PaintOpHelper, TranslateToString) {
   TranslateOp op(0, 0);
-  op.type = static_cast<uint32_t>(TranslateOp::kType);
   std::string str = PaintOpHelper::ToString(&op);
   EXPECT_EQ(str, "TranslateOp(dx=0.000, dy=0.000)");
 }
