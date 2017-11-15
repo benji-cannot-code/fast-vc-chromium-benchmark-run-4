b797a993c5f10dc59c15ce316572de5f355e07ce
Xing Liu <xingliu@chromium.org>
2017-11-15 06:16:13 +0000

    [Reland] Fix an issue in download with network service.
    This CL relands
    https://chromium-review.googlesource.com/c/chromium/src/+/761269,
    which is reverted in
    https://chromium-review.googlesource.com/c/chromium/src/+/765388.
    
    Also SBNavigationObserverBrowserTest.DownloadViaHTML5FileApiWithoutUserGesture
    and SBNavigationObserverBrowserTest.DownloadViaHTML5FileApiWithUserGesture is
    broken because of the reverted patch.
    See https://chromium-review.googlesource.com/c/chromium/src/+/765229.
    
    The code path from RenderFrameMessageFilter::DownloadUrl still needs
    the render_frame_id to find the WebContents. In this case, the download
    is not intercepted from navigation but from renderer, mostly triggered from javascript.
    
    Bug: 715630, 781565
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo;master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I414c87f54d3f03a3060a38a9de402e02197ca445
    Reviewed-on: https://chromium-review.googlesource.com/767852
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516595}

--- content/browser/download/resource_downloader.h
+++ content/browser/download/resource_downloader.h
@@ -39,6 +39,7 @@ class ResourceDownloader : public UrlDownloadHandler,
       const scoped_refptr<ResourceResponse>& response,
       mojo::ScopedDataPipeConsumerHandle consumer_handle,
       const SSLStatus& ssl_status,
+      int frame_tree_node_id,
       std::unique_ptr<ThrottlingURLLoader> url_loader,
       std::vector<GURL> url_chain,
       base::Optional<network::URLLoaderStatus> status);
@@ -51,8 +52,6 @@ class ResourceDownloader : public UrlDownloadHandler,
                      bool is_parallel_request,
                      bool is_transient,
                      bool fetch_error_body);
-  ResourceDownloader(base::WeakPtr<UrlDownloadHandler::Delegate> delegate,
-                     std::unique_ptr<ThrottlingURLLoader> url_loader);
   ~ResourceDownloader() override;
 
   // DownloadResponseHandler::Delegate
@@ -72,6 +71,7 @@ class ResourceDownloader : public UrlDownloadHandler,
                          const scoped_refptr<ResourceResponse>& response,
                          mojo::ScopedDataPipeConsumerHandle consumer_handle,
                          const SSLStatus& ssl_status,
+                         int frame_tree_node_id,
                          std::vector<GURL> url_chain,
                          base::Optional<network::URLLoaderStatus> status);
 
@@ -98,6 +98,9 @@ class ResourceDownloader : public UrlDownloadHandler,
 
   // Callback to run after download starts.
   DownloadUrlParameters::OnStartedCallback callback_;
+
+  // Used to get WebContents in browser process.
+  int frame_tree_node_id_ = -1;
 
   base::WeakPtrFactory<ResourceDownloader> weak_ptr_factory_;
 
--- content/browser/frame_host/navigation_request.cc
+++ content/browser/frame_host/navigation_request.cc
@@ -1125,7 +1125,8 @@ void NavigationRequest::OnWillProcessResponseChecksComplete(
           BrowserContext::GetDownloadManager(browser_context));
       loader_->InterceptNavigation(
           download_manager->GetNavigationInterceptionCB(
-              response_, std::move(handle_), ssl_status_));
+              response_, std::move(handle_), ssl_status_,
+              frame_tree_node_->frame_tree_node_id()));
       OnRequestFailed(false, net::ERR_ABORTED, base::nullopt, false);
       return;
     }
