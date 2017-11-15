406764612710b7d867905b4c8b571a6652329901
Gustav Sennton <gsennton@google.com>
2017-11-15 14:05:13 +0000

    [WebView] Add directory for interfaces between support library and glue
    We will build a set of interfaces to communicate between the WebView
    support library (statically built into apps), and the WebView support
    library glue layer (built into the WebView APK, loaded into app
    processes at run-time).
    
    To allow Chromium and the Android Support Library to share these
    interfaces we put them in their own directory, and mirror that directory
    publicly so that they can be referenced by the support library.
    
    This CL creates the interface directory, and adds some examples of
    interfaces.
    
    BUG=755506
    
    Change-Id: I3f7040c4ab88c7e0d26379aebc4b74fb53eee1de
    Reviewed-on: https://chromium-review.googlesource.com/768712
    Commit-Queue: Gustav Sennton <gsennton@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516688}

---
+++ android_webview/support_library/boundary_interfaces/src/org/chromium/support_lib_boundary/WebSettingsInterface.java
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.support_lib_boundary;
+
+// Technically this interface is not needed until we add a method to WebSettings with an
+// android.webkit parameter or android.webkit return value. But for forwards compatibility all
+// app-facing classes should have a boundary-interface that the WebView glue layer can build
+// against.
+/**
+ */
+public interface WebSettingsInterface {
+    void setSafeBrowsingEnabled(boolean enabled);
+    boolean getSafeBrowsingEnabled();
+}
---
+++ android_webview/support_library/boundary_interfaces/src/org/chromium/support_lib_boundary/WebViewProviderFactory.java
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.support_lib_boundary;
+
+import java.lang.reflect.InvocationHandler;
+
+/**
+ */
+public interface WebViewProviderFactory {
+    // android.webkit.WebViewProvider is in the SystemApi, not public, so we pass it as an Object
+    // here.
+    InvocationHandler /* org.chromium.sup_lib_boundary.WebViewProvider */ createWebView(
+            Object /* android.webkit.WebViewProvider */ webviewProvider);
+}
