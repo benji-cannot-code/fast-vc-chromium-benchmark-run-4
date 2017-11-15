989e60be607b73f96458baf0bcc486cc819ed926
Jacob Dufault <jdufault@google.com>
2017-11-15 22:24:46 +0000

    cros: Fix ScreenlockPrivateApiTest in mash.
    Bug: 784398
    Change-Id: Ic5cab7dab088645161ca7fa251faf7328b6aec5f
    Reviewed-on: https://chromium-review.googlesource.com/766987
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Jacob Dufault <jdufault@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516860}

--- ash/login/ui/lock_window.cc
+++ ash/login/ui/lock_window.cc
@@ -5,12 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/login/ui/lock_window.h"
 
-#include "ash/public/cpp/config.h"
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/shell.h"
-#include "services/ui/public/cpp/property_type_converters.h"
-#include "services/ui/public/interfaces/window_manager.mojom.h"
-#include "ui/aura/window.h"
 #include "ui/events/gestures/gesture_recognizer.h"
 
 namespace ash {
@@ -23,14 +19,10 @@ LockWindow::LockWindow(Config config) {
   params.delegate = this;
   params.show_state = ui::SHOW_STATE_FULLSCREEN;
   params.opacity = views::Widget::InitParams::TRANSLUCENT_WINDOW;
-  const int kLockContainer = kShellWindowId_LockScreenContainer;
-
-  if (config == Config::MASH) {
-    params.mus_properties[ui::mojom::WindowManager::kContainerId_InitProperty] =
-        mojo::ConvertTo<std::vector<uint8_t>>(kLockContainer);
-  } else {
-    params.parent =
-        Shell::GetContainer(Shell::GetPrimaryRootWindow(), kLockContainer);
+  // Shell may be null in tests.
+  if (Shell::HasInstance()) {
+    params.parent = Shell::GetContainer(Shell::GetPrimaryRootWindow(),
+                                        kShellWindowId_LockScreenContainer);
   }
   Init(params);
   SetVisibilityAnimationTransition(views::Widget::ANIMATE_NONE);
