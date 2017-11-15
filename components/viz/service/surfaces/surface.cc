c459ed5070fd85ce285688e0c85d1dc5a8853067
Peng Huang <penghuang@chromium.org>
2017-11-15 19:43:34 +0000

    viz: always run presented_callback for a compositor frame
    In some case, a frame can be replaced by a new frame before reaching the
    sreen. We need run presented_callback for those frames.
    
    Bug: 727775
    Change-Id: I5b5bc6614b233b8a1f8517fd662aff91455a6e51
    Reviewed-on: https://chromium-review.googlesource.com/762218
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Fady Samuel <fsamuel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516790}

--- components/viz/service/surfaces/surface.cc
+++ components/viz/service/surfaces/surface.cc
@@ -36,8 +36,8 @@ Surface::~Surface() {
   ClearCopyRequests();
   surface_manager_->SurfaceDiscarded(this);
 
-  UnrefFrameResourcesAndRunDrawCallback(std::move(pending_frame_data_));
-  UnrefFrameResourcesAndRunDrawCallback(std::move(active_frame_data_));
+  UnrefFrameResourcesAndRunCallbacks(std::move(pending_frame_data_));
+  UnrefFrameResourcesAndRunCallbacks(std::move(active_frame_data_));
 
   deadline_.Cancel();
 }
@@ -110,6 +110,10 @@ bool Surface::QueueFrame(
       frame.device_scale_factor() != surface_info_.device_scale_factor()) {
     TRACE_EVENT_INSTANT0("cc", "Surface invariants violation",
                          TRACE_EVENT_SCOPE_THREAD);
+    if (presented_callback) {
+      std::move(presented_callback)
+          .Run(base::TimeTicks(), base::TimeDelta(), 0);
+    }
     return false;
   }
 
@@ -118,9 +122,10 @@ bool Surface::QueueFrame(
         TransferableResource::ReturnResources(frame.resource_list);
     surface_client_->ReturnResources(resources);
     std::move(callback).Run();
-    if (presented_callback)
+    if (presented_callback) {
       std::move(presented_callback)
           .Run(base::TimeTicks(), base::TimeDelta(), 0);
+    }
     return true;
   }
 
@@ -156,7 +161,7 @@ bool Surface::QueueFrame(
   }
 
   // Returns resources for the previous pending frame.
-  UnrefFrameResourcesAndRunDrawCallback(std::move(previous_pending_frame_data));
+  UnrefFrameResourcesAndRunCallbacks(std::move(previous_pending_frame_data));
 
   return true;
 }
@@ -257,7 +262,7 @@ void Surface::ActivateFrame(FrameData frame_data) {
   for (auto& copy_request : old_copy_requests)
     RequestCopyOfOutput(std::move(copy_request));
 
-  UnrefFrameResourcesAndRunDrawCallback(std::move(previous_frame_data));
+  UnrefFrameResourcesAndRunCallbacks(std::move(previous_frame_data));
 
   if (!seen_first_frame_activation_) {
     seen_first_frame_activation_ = true;
@@ -387,7 +392,7 @@ void Surface::OnDeadline() {
   ActivatePendingFrameForDeadline();
 }
 
-void Surface::UnrefFrameResourcesAndRunDrawCallback(
+void Surface::UnrefFrameResourcesAndRunCallbacks(
     base::Optional<FrameData> frame_data) {
   if (!frame_data || !surface_client_)
     return;
@@ -399,8 +404,13 @@ void Surface::UnrefFrameResourcesAndRunDrawCallback(
     resource.sync_token.Clear();
   surface_client_->UnrefResources(resources);
 
-  if (!frame_data->draw_callback.is_null())
+  if (frame_data->draw_callback)
     std::move(frame_data->draw_callback).Run();
+
+  if (frame_data->presented_callback) {
+    std::move(frame_data->presented_callback)
+        .Run(base::TimeTicks(), base::TimeDelta(), 0);
+  }
 }
 
 void Surface::ClearCopyRequests() {
