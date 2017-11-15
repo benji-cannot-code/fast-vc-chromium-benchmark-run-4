5ad63c80f988a88324efdbbd122668692124f0a8
Miguel Casas-Sanchez <mcasas@chromium.org>
2017-11-15 19:26:20 +0000

    vaapi: add more testing to vaapi decoder unittests
    This Cl beefs up a vaapi_video_decode_accelerator_unittests
    test case. For that we need to mock VaapiWrapper, so it's
    made overrideable and two of its methods virtual.
    
    This CL also turns the static call to VaapiPicture's
    CreatePicture() into a Callback so it can be overriden
    by the test. (This is temporary: crbug.com/784507).
    MockVaapiPicture has to be MEDIA_GPU_EXPORTed to link
    against in the unittests (different link unit).
    
    This is important to test the usual decode startup sequence
    with allocation of PictureBuffers and VASurfaces:
    
    test                  VAVDA      mock_decoder_   mock_wrapper_
    QueueInputBuffer()--->
                          Decode()-->
                                    <I need
                                    PictureBuffers>
    ProvidePicture <----------------
    Buffers() ----------->
                          Assign
                          Picture
                          Buffers()---------------->
                                                    CreateSurfaces
                          Decode()-->
                                      <ok, done!>
    NotifyEndOf     <------
    BitstreamBuffer()
    
    
    Bug: 717265
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I716a54c98b44d28d6405f923f833a893c6d89a40
    Reviewed-on: https://chromium-review.googlesource.com/763920
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Reviewed-by: Fredrik Hubinette <hubbe@chromium.org>
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516780}

--- media/gpu/vaapi/vaapi_picture.h
+++ media/gpu/vaapi/vaapi_picture.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/sequence_checker.h"
 #include "media/gpu/gpu_video_decode_accelerator_helpers.h"
+#include "media/gpu/media_gpu_export.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/gfx/gpu_memory_buffer.h"
 
@@ -27,7 +28,7 @@ class VASurface;
 class VaapiWrapper;
 
 // Picture is native pixmap abstraction (X11/Ozone).
-class VaapiPicture {
+class MEDIA_GPU_EXPORT VaapiPicture {
  public:
   // Create a VaapiPicture of |size| to be associated with |picture_buffer_id|.
   // If provided, bind it to |texture_id|, as well as to |client_texture_id|
