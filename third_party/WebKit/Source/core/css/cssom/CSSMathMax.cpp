cc0a64edf1cee89a9826b297b143b8f2ee828cdc
Darren Shen <shend@chromium.org>
2017-11-15 00:53:01 +0000

    [css-typed-om] Implement addition of numeric types.
    When adding two CSSMathValues, we need to check if they are compatible
    in terms of their type. For example, it's fine to add 1px and 1cm,
    because they both have base type 'length'. However, we can't add 1px and
    1s, because one has type 'length' and the other 'time'. Furthermore, we
    can't add 1px with (1px * 1px), because one is 'length' and the other
    'length^2'. Hence, both the base types and their corresponding powers
    must match.
    
    The spec also has a special case for percentages: e.g. 1px + 1%. To make
    this addition work, the 1% must be a percentage of 'length'. Thus, in
    the presence of percentages, we can also convert them into another base
    type if that will make the addition work.
    
    Spec: https://drafts.css-houdini.org/css-typed-om-1/#add-two-types
    
    Bug: 776173
    Change-Id: I4fe0bbb920b3b1ccf94f1c9c49b4d31e09c095e5
    Reviewed-on: https://chromium-review.googlesource.com/765548
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: meade_UTC10 <meade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516529}

---
+++ third_party/WebKit/Source/core/css/cssom/CSSMathMax.cpp
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/css/cssom/CSSMathMax.h"
+
+namespace blink {
+
+CSSMathMax* CSSMathMax::Create(const HeapVector<CSSNumberish>& args,
+                               ExceptionState& exception_state) {
+  if (args.IsEmpty()) {
+    exception_state.ThrowDOMException(kSyntaxError, "Arguments can't be empty");
+    return nullptr;
+  }
+
+  CSSMathMax* result = Create(CSSNumberishesToNumericValues(args));
+  if (!result) {
+    exception_state.ThrowTypeError("Incompatible types");
+    return nullptr;
+  }
+
+  return result;
+}
+
+CSSMathMax* CSSMathMax::Create(CSSNumericValueVector values) {
+  bool error = false;
+  CSSNumericValueType final_type =
+      CSSMathVariadic::TypeCheck(values, CSSNumericValueType::Add, error);
+  return error ? nullptr
+               : new CSSMathMax(CSSNumericArray::Create(std::move(values)),
+                                final_type);
+}
+
+}  // namespace blink
