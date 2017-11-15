dc5f9c1eb47c92fed615d156534067ecaf1dc9ab
Luke Zielinski <lpz@chromium.org>
2017-11-15 19:12:24 +0000

    [Scout cleanup] Remove cmd-line switch for forcing scout group on/off.
    Bug: 662944
    Change-Id: I46835d9c6549f9544d71a86a2650d5b7e8162514
    Reviewed-on: https://chromium-review.googlesource.com/769350
    Reviewed-by: Jialiu Lin (Low reply Nov 15-16) <jialiul@chromium.org>
    Commit-Queue: Luke Z <lpz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516776}

--- components/safe_browsing/common/safe_browsing_prefs.cc
+++ components/safe_browsing/common/safe_browsing_prefs.cc
@@ -14,11 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-// Switch value which force the ScoutGroupSelected pref to true.
-const char kForceScoutGroupValueTrue[] = "true";
-// Switch value which force the ScoutGroupSelected pref to false.
-const char kForceScoutGroupValueFalse[] = "false";
-
 // Name of the Scout Transition UMA metric.
 const char kScoutTransitionMetricName[] = "SafeBrowsing.Pref.Scout.Transition";
 
@@ -164,8 +159,6 @@ const char kSafeBrowsingUnhandledSyncPasswordReuses[] =
 
 namespace safe_browsing {
 
-const char kSwitchForceScoutGroup[] = "force-scout-group";
-
 const base::Feature kCanShowScoutOptIn{"CanShowScoutOptIn",
                                        base::FEATURE_ENABLED_BY_DEFAULT};
 
@@ -212,26 +205,6 @@ const char* GetExtendedReportingPrefName(const PrefService& prefs) {
 }
 
 void InitializeSafeBrowsingPrefs(PrefService* prefs) {
-  // First handle forcing kSafeBrowsingScoutGroupSelected pref via cmd-line.
-  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
-          kSwitchForceScoutGroup)) {
-    std::string switch_value =
-        base::CommandLine::ForCurrentProcess()->GetSwitchValueASCII(
-            kSwitchForceScoutGroup);
-    if (switch_value == kForceScoutGroupValueTrue) {
-      prefs->SetBoolean(prefs::kSafeBrowsingScoutGroupSelected, true);
-      UMA_HISTOGRAM_ENUMERATION(kScoutTransitionMetricName,
-                                FORCE_SCOUT_FLAG_TRUE, MAX_REASONS);
-    } else if (switch_value == kForceScoutGroupValueFalse) {
-      prefs->SetBoolean(prefs::kSafeBrowsingScoutGroupSelected, false);
-      UMA_HISTOGRAM_ENUMERATION(kScoutTransitionMetricName,
-                                FORCE_SCOUT_FLAG_FALSE, MAX_REASONS);
-    }
-
-    // If the switch is used, don't process the experiment state.
-    return;
-  }
-
   // Handle the three possible experiment states.
   if (base::FeatureList::IsEnabled(kOnlyShowScoutOptIn)) {
     // OnlyShowScoutOptIn immediately turns on ScoutGroupSelected pref.
