d030df203d0b0fe8978e1ff0617a7df5b96e49b7
Bhagirathi Satpathy <bhagirathi.s@samsung.com>
2017-11-15 08:46:39 +0000

    document.open() should not invoke public window.open()
    window = document.open(url, name, features) should open new window
    with these three argements instead of calling overwrite open function.
    
    Spec : https://html.spec.whatwg.org/multipage/dynamic-markup-insertion.html#opening-the-input-stream
    
    Bug: 722737
    Change-Id: I466b3f586966498f6685aa18daef5f558caee6de
    Reviewed-on: https://chromium-review.googlesource.com/758698
    Commit-Queue: srirama chandra sekhar <srirama.m@samsung.com>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516641}

--- third_party/WebKit/LayoutTests/fast/js/script-tests/document-open-getter-throw-no-crash.js
+++
@@ -1,7 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-description("document.open() - propagate exception thrown when accessing window.open.");
-
-var frame = document.body.appendChild(document.createElement("iframe"));
-frame.contentWindow.__defineGetter__("open", function() { throw 'PASS (no crash)';});
-shouldThrow("frame.contentDocument.open(1, 1, 1, 1, 1);");
-
