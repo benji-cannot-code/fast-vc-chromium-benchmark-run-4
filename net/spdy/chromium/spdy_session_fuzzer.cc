5c6cc49a5ccea1a1f4f4abe5e91139bdecc127c3
Bence Béky <bnc@chromium.org>
2017-11-15 00:53:53 +0000

    Bundle test certificate in fuzzer at compile time.
    net_spdy_session_fuzzer is run on machines where source checkout is not
    available, therefore test certificates cannot be read from the usual
    location.  One possible way to solve this is to read in the certificate
    at compile time as a static resource.
    
    Bug: 781846
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I31d03bc88ac9652551f1e08f944e113eb63c7bd8
    Reviewed-on: https://chromium-review.googlesource.com/760488
    Commit-Queue: Bence Béky <bnc@chromium.org>
    Reviewed-by: Eric Roman <eroman@chromium.org>
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516530}

--- net/spdy/chromium/spdy_session_fuzzer.cc
+++ net/spdy/chromium/spdy_session_fuzzer.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/host_port_pair.h"
 #include "net/base/net_errors.h"
 #include "net/base/request_priority.h"
+#include "net/cert/x509_certificate.h"
 #include "net/log/net_log_source.h"
 #include "net/log/test_net_log.h"
 #include "net/socket/client_socket_handle.h"
@@ -18,10 +19,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/socket/ssl_client_socket.h"
 #include "net/spdy/chromium/spdy_test_util_common.h"
 #include "net/ssl/ssl_config.h"
-#include "net/test/cert_test_util.h"
-#include "net/test/test_data_directory.h"
 
 namespace {
+
+const char kCertData[] = {
+#include "net/data/ssl/certificates/spdy_pooling.inc"
+};
 
 class FuzzerDelegate : public net::SpdyStream::Delegate {
  public:
@@ -104,7 +107,8 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
 
   net::SSLSocketDataProvider ssl_provider(net::ASYNC, net::OK);
   ssl_provider.cert =
-      net::ImportCertFromFile(net::GetTestCertsDirectory(), "spdy_pooling.pem");
+      net::X509Certificate::CreateFromBytes(kCertData, arraysize(kCertData));
+  CHECK(ssl_provider.cert);
   socket_factory.AddSSLSocketDataProvider(&ssl_provider);
 
   net::SpdySessionDependencies deps;
