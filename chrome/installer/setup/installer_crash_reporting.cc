162a7384639d1e9c731239d4817ce0ff010531ec
Robert Sesek <rsesek@chromium.org>
2017-11-15 13:37:29 +0000

    Clean up targets in //components/crash/content/app.
    This merges the app_breakpad_mac_win_to_be_deleted and app_non_mac_win
    into the app target. The old Mac Breakpad integration code is removed,
    and on Windows it is relegated to the deprecated_breakpad_win target
    for exclusive use by nacl64. In addition, the lib target is shuffled
    around and made an internal detail of the component.
    
    Bug: 466890, 565771
    Change-Id: I7ea5ce854981aeeeaa651375e9367ec72dd412d5
    Reviewed-on: https://chromium-review.googlesource.com/768908
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Sergey Volk <servolk@chromium.org>
    Reviewed-by: Mark Seaborn <mseaborn@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516683}

--- chrome/installer/setup/installer_crash_reporting.cc
+++ chrome/installer/setup/installer_crash_reporting.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/installer/setup/installer_crash_reporter_client.h"
 #include "chrome/installer/setup/installer_state.h"
 #include "chrome/installer/util/google_update_settings.h"
-#include "components/crash/content/app/crash_keys_win.h"
 #include "components/crash/content/app/crashpad.h"
 #include "components/crash/core/common/crash_keys.h"
 
