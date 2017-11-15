2625d716042d4ea0d800fc77853e45cb14d8f93a
Hiroki Nakagawa <nhiroki@chromium.org>
2017-11-15 03:05:38 +0000

    Worklet: Add tests for CSP directives on addModule()
    Worklet module fetch should obey the script-src directives, not the worker-src
    directives. This CL adds WPT tests for that.
    
    Spec discussion: https://github.com/w3c/webappsec-csp/issues/203
    
    Bug: 773786
    Change-Id: I3b8ac7abfa0920e3b93a18b726e87871c62f8f3f
    Reviewed-on: https://chromium-review.googlesource.com/763169
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516576}

---
+++ third_party/WebKit/LayoutTests/external/wpt/worklets/resources/import-empty-worklet-script.js
@@ -0,0 +1,4 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// This script can be imported as a remote-origin script, so the
+// Access-Control-Allow-Origin is specified here.
+import './empty-worklet-script.js?pipe=header(Access-Control-Allow-Origin, *)';
