774275337ab68a5b219601d74cf6368b4bacb5b8
Lalit Maganti <lalitm@chromium.org>
2017-11-15 13:22:50 +0000

    memory-infra: Add UKM and UMA constants for shared memory footprint
    Now that we have the code for computing the shared memory footprint, we
    should add the means to report the data to UMA/UKM.
    
    See go/czhuu for context
    
    Bug: 781782
    Change-Id: I38a7b3e2e7794b09ef779e728ca998efba7659be
    Reviewed-on: https://chromium-review.googlesource.com/756750
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Primiano Tucci <primiano@chromium.org>
    Commit-Queue: Lalit Maganti <lalitm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516680}

--- chrome/browser/metrics/process_memory_metrics_emitter.cc
+++ chrome/browser/metrics/process_memory_metrics_emitter.cc
@@ -65,7 +65,10 @@ void EmitBrowserMemoryMetrics(const ProcessMemoryDumpPtr& pmd,
       pmd->os_dump->private_footprint_kb / 1024);
   UMA_HISTOGRAM_MEMORY_LARGE_MB("Memory.Browser.PrivateMemoryFootprint",
                                 pmd->os_dump->private_footprint_kb / 1024);
+  UMA_HISTOGRAM_MEMORY_LARGE_MB("Memory.Browser.SharedMemoryFootprint",
+                                pmd->os_dump->shared_footprint_kb / 1024);
   builder.SetPrivateMemoryFootprint(pmd->os_dump->private_footprint_kb / 1024);
+  builder.SetSharedMemoryFootprint(pmd->os_dump->shared_footprint_kb / 1024);
 
   // It is possible to run without a separate GPU process.
   // When that happens, we should log common GPU metrics from the browser proc.
@@ -88,6 +91,8 @@ void EmitBrowserMemoryMetrics(const ProcessMemoryDumpPtr& pmd,
                                   pmd->os_dump->private_footprint_kb / 1024);  \
     UMA_HISTOGRAM_MEMORY_LARGE_MB("Memory." type ".PrivateMemoryFootprint",    \
                                   pmd->os_dump->private_footprint_kb / 1024);  \
+    UMA_HISTOGRAM_MEMORY_LARGE_MB("Memory." type ".SharedMemoryFootprint",     \
+                                  pmd->os_dump->shared_footprint_kb / 1024);   \
     UMA_HISTOGRAM_MEMORY_LARGE_MB(                                             \
         "Memory.Experimental." type "2.PartitionAlloc",                        \
         pmd->chrome_dump->partition_alloc_total_kb / 1024);                    \
@@ -129,6 +134,7 @@ void EmitRendererMemoryMetrics(
   builder.SetMalloc(pmd->chrome_dump->malloc_total_kb / 1024);
 #endif
   builder.SetPrivateMemoryFootprint(pmd->os_dump->private_footprint_kb / 1024);
+  builder.SetSharedMemoryFootprint(pmd->os_dump->shared_footprint_kb / 1024);
   builder.SetPartitionAlloc(pmd->chrome_dump->partition_alloc_total_kb / 1024);
   builder.SetBlinkGC(pmd->chrome_dump->blink_gc_total_kb / 1024);
   builder.SetV8(pmd->chrome_dump->v8_total_kb / 1024);
@@ -173,7 +179,10 @@ void EmitGpuMemoryMetrics(const ProcessMemoryDumpPtr& pmd,
       pmd->os_dump->private_footprint_kb / 1024);
   UMA_HISTOGRAM_MEMORY_LARGE_MB("Memory.Gpu.PrivateMemoryFootprint",
                                 pmd->os_dump->private_footprint_kb / 1024);
+  UMA_HISTOGRAM_MEMORY_LARGE_MB("Memory.Gpu.SharedMemoryFootprint",
+                                pmd->os_dump->shared_footprint_kb / 1024);
   builder.SetPrivateMemoryFootprint(pmd->os_dump->private_footprint_kb / 1024);
+  builder.SetSharedMemoryFootprint(pmd->os_dump->shared_footprint_kb / 1024);
   if (uptime)
     builder.SetUptime(uptime.value().InSeconds());
   builder.Record(ukm_recorder);
@@ -306,9 +315,11 @@ void ProcessMemoryMetricsEmitter::CollateResults() {
     return;
 
   uint32_t private_footprint_total_kb = 0;
+  uint32_t shared_footprint_total_kb = 0;
   base::Time now = base::Time::Now();
   for (const ProcessMemoryDumpPtr& pmd : global_dump_->process_dumps) {
     private_footprint_total_kb += pmd->os_dump->private_footprint_kb;
+    shared_footprint_total_kb += pmd->os_dump->shared_footprint_kb;
     switch (pmd->process_type) {
       case memory_instrumentation::mojom::ProcessType::BROWSER: {
         EmitBrowserMemoryMetrics(pmd, ukm::UkmRecorder::GetNewSourceID(),
@@ -351,8 +362,11 @@ void ProcessMemoryMetricsEmitter::CollateResults() {
       private_footprint_total_kb / 1024);
   UMA_HISTOGRAM_MEMORY_LARGE_MB("Memory.Total.PrivateMemoryFootprint",
                                 private_footprint_total_kb / 1024);
+  UMA_HISTOGRAM_MEMORY_LARGE_MB("Memory.Total.SharedMemoryFootprint",
+                                shared_footprint_total_kb / 1024);
 
   ukm::builders::Memory_Experimental(ukm::UkmRecorder::GetNewSourceID())
       .SetTotal2_PrivateMemoryFootprint(private_footprint_total_kb / 1024)
+      .SetTotal2_SharedMemoryFootprint(shared_footprint_total_kb / 1024)
       .Record(GetUkmRecorder());
 }
