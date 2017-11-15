4c762a223566a848b3428958b0318dba1b81d8fc
Jochen Eisinger <jochen@chromium.org>
2017-11-15 06:20:53 +0000

    Treat invalid tokens in referrer policy headers as errors
    This brings us in sync with
    https://github.com/w3c/webappsec-referrer-policy/commit/380d752a0194aaa95be1784c3cf27e4a54233822
    
    R=estark@chromium.org
    BUG=705950
    
    Change-Id: I607c1a8c1d3c68c6016d115bd8a0a225e7602840
    Reviewed-on: https://chromium-review.googlesource.com/769807
    Reviewed-by: Emily Stark <estark@chromium.org>
    Commit-Queue: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516604}

--- third_party/WebKit/Source/platform/weborigin/SecurityPolicy.cpp
+++ third_party/WebKit/Source/platform/weborigin/SecurityPolicy.cpp
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/wtf/HashSet.h"
 #include "platform/wtf/PtrUtil.h"
 #include "platform/wtf/Threading.h"
+#include "platform/wtf/text/ParsingUtilities.h"
 #include "platform/wtf/text/StringHash.h"
 #include "public/platform/WebReferrerPolicy.h"
 
@@ -323,6 +324,15 @@ bool SecurityPolicy::ReferrerPolicyFromString(
   return false;
 }
 
+namespace {
+
+template <typename CharType>
+inline bool IsASCIIAlphaOrHyphen(CharType c) {
+  return IsASCIIAlpha(c) || c == '-';
+}
+
+}  // namespace
+
 bool SecurityPolicy::ReferrerPolicyFromHeaderValue(
     const String& header_value,
     ReferrerPolicyLegacyKeywordsSupport legacy_keywords_support,
@@ -333,10 +343,19 @@ bool SecurityPolicy::ReferrerPolicyFromHeaderValue(
   header_value.Split(',', true, tokens);
   for (const auto& token : tokens) {
     ReferrerPolicy current_result;
+    auto stripped_token = token.StripWhiteSpace();
     if (SecurityPolicy::ReferrerPolicyFromString(token.StripWhiteSpace(),
                                                  legacy_keywords_support,
                                                  &current_result)) {
       referrer_policy = current_result;
+    } else {
+      Vector<UChar> characters;
+      stripped_token.AppendTo(characters);
+      const UChar* position = characters.data();
+      UChar* end = characters.data() + characters.size();
+      SkipWhile<UChar, IsASCIIAlphaOrHyphen>(position, end);
+      if (position != end)
+        return false;
     }
   }
 
