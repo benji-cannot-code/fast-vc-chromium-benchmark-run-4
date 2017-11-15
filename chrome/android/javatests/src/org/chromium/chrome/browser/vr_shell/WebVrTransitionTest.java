6958bfbb4a7dafa69886546543a926d990953101
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2017-11-15 01:31:50 +0000

    Revert "Clean up VR-related runtime flags"
    This reverts commit 307a16244fb1f049a7a5d6ede37c0fc6866a3511.
    
    Reason for revert: broke compilation https://luci-milo.appspot.com/buildbot/chromium.android/Android%20x86%20Builder%20(dbg)/24639
    
    Original change's description:
    > Clean up VR-related runtime flags
    > 
    > * Consolidate VR-related features and use consistent naming.
    > * Clarify flag names and descriptions.
    > * Avoid the "enable" pattern.
    > 
    > Some flag and feature names are changed, but users shouldn't be using these.
    > 
    > Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    > Change-Id: I56de992d246a13ff911cb8c88938a0690731c18e
    > Reviewed-on: https://chromium-review.googlesource.com/762392
    > Reviewed-by: Nick Carter <nick@chromium.org>
    > Reviewed-by: Yash Malik <ymalik@chromium.org>
    > Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    > Reviewed-by: Klaus Weidner <klausw@chromium.org>
    > Reviewed-by: Ian Vollick <vollick@chromium.org>
    > Reviewed-by: Bill Orr <billorr@chromium.org>
    > Reviewed-by: Brandon Jones <bajones@chromium.org>
    > Commit-Queue: David Dorwin <ddorwin@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#516518}
    
    TBR=ddorwin@chromium.org,vollick@chromium.org,nick@chromium.org,bajones@chromium.org,ymalik@chromium.org,bsheedy@chromium.org,klausw@chromium.org,billorr@chromium.org
    
    Change-Id: Ie219c04ba221d068e3227ab9cc77132663b52aa9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Reviewed-on: https://chromium-review.googlesource.com/770370
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516547}

--- chrome/android/javatests/src/org/chromium/chrome/browser/vr_shell/WebVrTransitionTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/vr_shell/WebVrTransitionTest.java
@@ -152,7 +152,7 @@ public class WebVrTransitionTest {
      */
     @Test
     @MediumTest
-    @CommandLineFlags.Add("enable-features=WebVrAutopresentFromIntent")
+    @CommandLineFlags.Add("enable-features=WebVrAutopresent")
     @Restriction(RESTRICTION_TYPE_VIEWER_DAYDREAM)
     public void testTrustedIntentAllowsAutoPresent() throws InterruptedException {
         VrIntentUtils.setHandlerInstanceForTesting(new MockVrIntentHandler(
