0ee0c6f2ebc64d4616695e967173cc6548ba43ca
Darren Shen <shend@chromium.org>
2017-11-15 07:26:45 +0000

    [css-typed-om] Implement multiplication of numeric types.
    When multiplying two CSSMathValues, we need to check if they are
    compatible in terms of their type. Unlike addition, there's only one
    case where two types are incompatible: if both types are different non-
    null percent hints e.g. (1px + 1%) * (1s + 1%). If the types are
    compatible, we just need to add the corresponding exponents for each
    base type.
    
    Spec: https://drafts.css-houdini.org/css-typed-om-1/#cssnumericvalue-multiply-two-types
    
    Bug: 776173
    Change-Id: I818b258cac93bcc8b5d6105113bff9e6117d955a
    Reviewed-on: https://chromium-review.googlesource.com/767467
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: meade_UTC10 <meade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516625}

---
+++ third_party/WebKit/Source/core/css/cssom/CSSMathProduct.cpp
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/css/cssom/CSSMathProduct.h"
+
+namespace blink {
+
+CSSMathProduct* CSSMathProduct::Create(const HeapVector<CSSNumberish>& args,
+                                       ExceptionState& exception_state) {
+  if (args.IsEmpty()) {
+    exception_state.ThrowDOMException(kSyntaxError, "Arguments can't be empty");
+    return nullptr;
+  }
+
+  CSSMathProduct* result = Create(CSSNumberishesToNumericValues(args));
+  if (!result) {
+    exception_state.ThrowTypeError("Incompatible types");
+    return nullptr;
+  }
+
+  return result;
+}
+
+CSSMathProduct* CSSMathProduct::Create(CSSNumericValueVector values) {
+  bool error = false;
+  CSSNumericValueType final_type =
+      CSSMathVariadic::TypeCheck(values, CSSNumericValueType::Multiply, error);
+  return error ? nullptr
+               : new CSSMathProduct(CSSNumericArray::Create(std::move(values)),
+                                    final_type);
+}
+
+}  // namespace blink
