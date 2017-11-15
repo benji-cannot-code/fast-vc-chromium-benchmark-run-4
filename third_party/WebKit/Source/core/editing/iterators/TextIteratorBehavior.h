e26df66cf31c0f63d18b3d802750e1ccdb9441c9
tanvir.rizvi <tanvir.rizvi@samsung.com>
2017-11-15 06:23:08 +0000

    Clipboard should respect user-select style attribute
    Clipboard 'copy/paste'data also contains the elements on which
    user-select:none property has been applied.
    But the visible selection do not highlights those portions.
    The copy data should respect the user-select attribute.
    Currently the clipboard copy paste data only depends upon
    the selection done in the DOM tree.
    This CL makes the clipbaord text to work with the
    text property
    
    Bug: 147490
    Change-Id: I5ccc6ebce67e630d0eb9949e514da77f52a6ed42
    Reviewed-on: https://chromium-review.googlesource.com/763032
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Tanvir Rizvi <tanvir.rizvi@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#516608}

--- third_party/WebKit/Source/core/editing/iterators/TextIteratorBehavior.h
+++ third_party/WebKit/Source/core/editing/iterators/TextIteratorBehavior.h
@@ -62,6 +62,10 @@ class CORE_EXPORT TextIteratorBehavior final {
     return values_.bits.does_not_emit_space_beyond_range_end;
   }
 
+  bool SkipsUnselectableContent() const {
+    return values_.bits.skips_unselectable_content;
+  }
+
   static TextIteratorBehavior EmitsObjectReplacementCharacterBehavior();
   static TextIteratorBehavior IgnoresStyleVisibilityBehavior();
   static TextIteratorBehavior DefaultRangeLengthBehavior();
@@ -89,6 +93,7 @@ class CORE_EXPORT TextIteratorBehavior final {
       bool ignores_style_visibility : 1;
       bool stops_on_form_controls : 1;
       bool does_not_emit_space_beyond_range_end : 1;
+      bool skips_unselectable_content : 1;
     } bits;
   } values_;
 };
@@ -118,6 +123,7 @@ class CORE_EXPORT TextIteratorBehavior::Builder final {
   Builder& SetIgnoresStyleVisibility(bool);
   Builder& SetStopsOnFormControls(bool);
   Builder& SetDoesNotEmitSpaceBeyondRangeEnd(bool);
+  Builder& SetSkipsUnselectableContent(bool);
 
  private:
   TextIteratorBehavior behavior_;
