8cf656b8fe39b63de3bbf47bb93bd108886d2f35
Luna Lu <loonybear@chromium.org>
2017-11-15 21:38:14 +0000

    Disable AutoSavingGeneratedPassword on Linux and MacOS
    TBR:kolos
    
    Bug: 784861
    Change-Id: I680fdee98444aa62eb0954254a3d138088632bed
    Reviewed-on: https://chromium-review.googlesource.com/773023
    Commit-Queue: Luna Lu <loonybear@chromium.org>
    Reviewed-by: Luna Lu <loonybear@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516837}

--- chrome/browser/password_manager/password_generation_interactive_uitest.cc
+++ chrome/browser/password_manager/password_generation_interactive_uitest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/strings/utf_string_conversions.h"
+#include "build/build_config.h"
 #include "chrome/browser/password_manager/chrome_password_manager_client.h"
 #include "chrome/browser/password_manager/password_manager_test_base.h"
 #include "chrome/browser/password_manager/password_store_factory.h"
@@ -196,8 +197,14 @@ IN_PROC_BROWSER_TEST_F(PasswordGenerationInteractiveTest,
   EXPECT_TRUE(GenerationPopupShowing());
 }
 
+// Flaky on Linux and Mac. See https://crbug.com/784861.
+#if defined(OS_LINUX) || defined(OS_MACOSX)
+#define MAYBE_AutoSavingGeneratedPassword DISABLED_AutoSavingGeneratedPassword
+#else
+#define MAYBE_AutoSavingGeneratedPassword AutoSavingGeneratedPassword
+#endif
 IN_PROC_BROWSER_TEST_F(PasswordGenerationInteractiveTest,
-                       AutoSavingGeneratedPassword) {
+                       MAYBE_AutoSavingGeneratedPassword) {
   scoped_refptr<password_manager::TestPasswordStore> password_store =
       static_cast<password_manager::TestPasswordStore*>(
           PasswordStoreFactory::GetForProfile(
