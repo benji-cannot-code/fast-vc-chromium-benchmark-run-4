e3eb9bbd4e5ad35234010bbf218ca34e74657742
Dirk Pranke <dpranke@chromium.org>
2017-11-15 04:02:55 +0000

    Revert "[Devtools] Requests can now be intercepted on response"
    This reverts commit 7c44d942b61a2f8ea63a0020ea1a4c907c6d98b2.
    
    Reason for revert: Suspect this to cause layout test failures on Linux dbg, MSAN; see crbug.com/785124
    
    Original change's description:
    > [Devtools] Requests can now be intercepted on response
    > 
    > Adds the ability in protocol to intercept requests on the response
    > instead of the request. This will allow us to patch requests instead of
    > just feed in the raw data.
    > 
    > R=​pfeldman,dgozman,caseq,alexclarke
    > BUG=754371
    > 
    > Change-Id: Iabe83509d953a6a958c410d4c260e66582caf91a
    > Reviewed-on: https://chromium-review.googlesource.com/754007
    > Commit-Queue: Blaise Bruer <allada@chromium.org>
    > Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    > Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#516556}
    
    TBR=dgozman@chromium.org,caseq@chromium.org,pfeldman@chromium.org,alexclarke@chromium.org,allada@chromium.org
    
    Change-Id: I519272a1f3dda0901dbaacdd27a2d504f0fdd975
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 754371
    Reviewed-on: https://chromium-review.googlesource.com/770501
    Commit-Queue: Dirk Pranke <dpranke@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516589}

--- content/browser/devtools/devtools_interceptor_controller.h
+++ content/browser/devtools/devtools_interceptor_controller.h
@@ -24,8 +24,6 @@ struct GlobalRequestID;
 
 class DevToolsInterceptorController : public base::SupportsUserData::Data {
  public:
-  using GetResponseBodyForInterceptionCallback =
-      DevToolsURLRequestInterceptor::GetResponseBodyForInterceptionCallback;
   using ContinueInterceptedRequestCallback =
       DevToolsURLRequestInterceptor::ContinueInterceptedRequestCallback;
   using Modifications = DevToolsURLRequestInterceptor::Modifications;
@@ -47,10 +45,6 @@ class DevToolsInterceptorController : public base::SupportsUserData::Data {
   void StopInterceptingRequests(const FrameTreeNode* target_frame);
 
   bool ShouldCancelNavigation(const GlobalRequestID& global_request_id);
-
-  void GetResponseBody(
-      std::string request_id,
-      std::unique_ptr<GetResponseBodyForInterceptionCallback> callback);
 
   ~DevToolsInterceptorController() override;
 
