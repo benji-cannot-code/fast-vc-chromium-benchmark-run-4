f694b4d4e57ae49d521298f6dabf1888e461c514
Raymond Toy <rtoy@chromium.org>
2017-11-15 20:01:06 +0000

    Sync wpt/webaudio audit.js
    Merge in fix in issue 719716 to wpt version of audit.js
    
    Bug: 785317
    Test: 
    Change-Id: Id923469fefa738daea69b9150356d07d1325216a
    Reviewed-on: https://chromium-review.googlesource.com/772092
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Commit-Queue: Raymond Toy <rtoy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516798}

--- third_party/WebKit/LayoutTests/external/wpt/webaudio/resources/audit.js
+++ third_party/WebKit/LayoutTests/external/wpt/webaudio/resources/audit.js
@@ -119,6 +119,8 @@ window.Audit = (function() {
       this._expectedDescription = null;
 
       this._detail = '';
+      // If true and the test failed, print the actual value at the
+      // end of the message.
       this._printActualForFailure = true;
 
       this._result = null;
@@ -621,7 +623,8 @@ window.Audit = (function() {
         passDetail = '${actual} contains only the constant ${expected}.';
       } else {
         let counter = 0;
-        failDetail = 'Expected ${expected} for all values but found ' +
+        failDetail =
+            '${actual}: Expected ${expected} for all values but found ' +
             numberOfErrors + ' unexpected values: ';
         failDetail += '\n\tIndex\tActual';
         for (let errorIndex in errors) {
