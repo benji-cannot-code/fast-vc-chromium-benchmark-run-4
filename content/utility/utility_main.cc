790e58891cb5662a31afe1737eed3468f406ac0f
Tom Sepez <tsepez@chromium.org>
2017-11-16 00:59:57 +0000

    Start up network service syscall broker process.
    This does no actual sandboxing, but ensures that all the file open
    calls can indeed be proxied via the same technique as used in the GPU.
    
    AllowAllPolicy becomes unused as a result and is removed.
    
    Bug: 715679
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: Id7a0ee51d8c98c0bd3f59b2e468dc2a3eb11f50e
    Reviewed-on: https://chromium-review.googlesource.com/764539
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516931}

--- content/utility/utility_main.cc
+++ content/utility/utility_main.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/service_manager/sandbox/sandbox.h"
 
 #if defined(OS_LINUX)
+#include "content/network/network_sandbox_hook_linux.h"
 #include "services/service_manager/sandbox/linux/sandbox_linux.h"
 #endif
 
@@ -54,8 +55,11 @@ int UtilityMain(const MainFunctionParams& parameters) {
       service_manager::SandboxTypeFromCommandLine(parameters.command_line);
   if (parameters.zygote_child ||
       sandbox_type == service_manager::SANDBOX_TYPE_NETWORK) {
+    service_manager::SandboxLinux::PreSandboxHook pre_sandbox_hook;
+    if (sandbox_type == service_manager::SANDBOX_TYPE_NETWORK)
+      pre_sandbox_hook = base::BindOnce(&NetworkPreSandboxHook);
     service_manager::Sandbox::Initialize(
-        sandbox_type, service_manager::SandboxLinux::PreSandboxHook(),
+        sandbox_type, std::move(pre_sandbox_hook),
         service_manager::SandboxLinux::Options());
   }
 #elif defined(OS_WIN)
