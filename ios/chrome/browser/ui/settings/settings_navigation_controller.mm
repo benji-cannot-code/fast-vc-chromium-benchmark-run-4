926c1f442508b3f679d1091d183ed6fd8622d939
Justin Cohen <justincohen@google.com>
2017-11-16 00:43:54 +0000

    Fix Report An Issue layout on iPhone X.
    Use safe area layout for SettingsAppBarContainerViewController.
    
    Bug: 783816, 785484
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I86e351e0c62ab8041d29aee9e764d42900fa0562
    Reviewed-on: https://chromium-review.googlesource.com/769409
    Commit-Queue: Louis Romero <lpromero@chromium.org>
    Reviewed-by: Louis Romero <lpromero@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516920}

--- ios/chrome/browser/ui/settings/settings_navigation_controller.mm
+++ ios/chrome/browser/ui/settings/settings_navigation_controller.mm
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/settings/sync_settings_collection_view_controller.h"
 #include "ios/chrome/browser/ui/ui_util.h"
 #import "ios/chrome/browser/ui/uikit_ui_util.h"
+#import "ios/chrome/browser/ui/util/constraints_ui_util.h"
 #include "ios/chrome/grit/ios_strings.h"
 #import "ios/public/provider/chrome/browser/chrome_browser_provider.h"
 #import "ios/public/provider/chrome/browser/user_feedback/user_feedback_provider.h"
@@ -40,7 +41,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "This file requires ARC support."
 #endif
 
-// TODO(crbug.com/620361): Remove the entire class when iOS 9 is dropped.
+// TODO(crbug.com/785484): Implements workarounds for bugs between iOS and MDC.
+// To be removed or refactored when iOS 9 is dropped.
 @interface SettingsAppBarContainerViewController
     : MDCAppBarContainerViewController
 @end
@@ -51,12 +53,31 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (UIViewController*)childViewControllerForStatusBarHidden {
   if (!base::ios::IsRunningOnIOS10OrLater()) {
-    // TODO(crbug.com/620361): Remove the entire method override when iOS 9 is
+    // TODO(crbug.com/785484): Remove the entire method override when iOS 9 is
     // dropped.
     return self.contentViewController;
   } else {
     return [super childViewControllerForStatusBarHidden];
   }
+}
+
+// TODO(crbug.com/785484): Investigate if this can be fixed in MDC.
+- (void)viewDidLayoutSubviews {
+  [super viewDidLayoutSubviews];
+
+  UILayoutGuide* safeAreaLayoutGuide = SafeAreaLayoutGuideForView(self.view);
+  UIView* contentView = self.contentViewController.view;
+  UIView* headerView = self.appBar.headerViewController.headerView;
+  contentView.translatesAutoresizingMaskIntoConstraints = NO;
+  [NSLayoutConstraint activateConstraints:@[
+    [contentView.topAnchor constraintEqualToAnchor:headerView.bottomAnchor],
+    [contentView.leadingAnchor
+        constraintEqualToAnchor:safeAreaLayoutGuide.leadingAnchor],
+    [contentView.trailingAnchor
+        constraintEqualToAnchor:safeAreaLayoutGuide.trailingAnchor],
+    [contentView.bottomAnchor
+        constraintEqualToAnchor:safeAreaLayoutGuide.bottomAnchor],
+  ]];
 }
 
 - (UIViewController*)childViewControllerForStatusBarStyle {
@@ -575,17 +596,10 @@ initWithRootViewController:(UIViewController*)rootViewController
         [[SettingsAppBarContainerViewController alloc]
             initWithContentViewController:controller];
 
+    // TODO(crbug.com/785484): Investigate if this and below can be removed.
     // Configure the style.
+    appBarContainer.view.backgroundColor = [UIColor whiteColor];
     ConfigureAppBarWithCardStyle(appBarContainer.appBar);
-
-    // Adjust the frame of the contained view controller's view to be below the
-    // app bar.
-    CGRect contentFrame = controller.view.frame;
-    CGSize headerSize = [appBarContainer.appBar.headerViewController.headerView
-        sizeThatFits:contentFrame.size];
-    contentFrame = UIEdgeInsetsInsetRect(
-        contentFrame, UIEdgeInsetsMake(headerSize.height, 0, 0, 0));
-    controller.view.frame = contentFrame;
 
     // Register the app bar container and return it.
     [self registerAppBarContainer:appBarContainer];
