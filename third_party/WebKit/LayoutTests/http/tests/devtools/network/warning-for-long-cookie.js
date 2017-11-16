c3264a18f99b084a04ded50b163800c81d22c45f
Will Chen <chenwilliam@chromium.org>
2017-11-16 03:35:48 +0000

    DevTools: automatically migrate the rest of network tests
    This converts a batch of tests from the old test framework (html file)
    into the new test framework (js file).
    
    Most of it was done through an automated transformation script, with
    a small amount of manual clean-up.
    
    R=allada@chromium.org
    
    Bug: 667560
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: Ia427b15096c06c72d7fa625b9660476b14705ab6
    Reviewed-on: https://chromium-review.googlesource.com/758508
    Commit-Queue: Will Chen <chenwilliam@chromium.org>
    Reviewed-by: Blaise Bruer <allada@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516970}

---
+++ third_party/WebKit/LayoutTests/http/tests/devtools/network/warning-for-long-cookie.js
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(async function() {
+  TestRunner.addResult(`Checks that we show warning message for long cookie.\n`);
+  await TestRunner.loadModule('console_test_runner');
+  await TestRunner.loadModule('network_test_runner');
+  await TestRunner.showPanel('network');
+
+  NetworkTestRunner.makeFetch(
+      'http://127.0.0.1:8000/inspector/network/resources/set-cookie.php?length=4096', {}, step1);
+
+  function step1() {
+    NetworkTestRunner.makeFetch(
+        'http://127.0.0.1:8000/inspector/network/resources/set-cookie.php?length=4097', {}, dumpMessages);
+  }
+
+  function dumpMessages() {
+    ConsoleTestRunner.dumpConsoleMessages();
+    TestRunner.completeTest();
+  }
+})();
