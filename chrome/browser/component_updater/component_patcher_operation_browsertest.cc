a99b254031455ce1cb6d1575f19ef3fcbbd4bb17
Guido Urdaneta <guidou@chromium.org>
2017-11-16 16:04:00 +0000

    Disable flaky PatchTest.CheckCourgetteOperation on Mac
    TBR=laforge@chromium.org
    
    Bug: 785936
    Change-Id: I4c4b1cf5a589ea837e460fde508a7e2e83dc21cd
    Reviewed-on: https://chromium-review.googlesource.com/774840
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517096}

--- chrome/browser/component_updater/component_patcher_operation_browsertest.cc
+++ chrome/browser/component_updater/component_patcher_operation_browsertest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/task_scheduler/post_task.h"
 #include "base/task_scheduler/task_traits.h"
+#include "build/build_config.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "components/patch_service/public/cpp/patch.h"
 #include "components/update_client/component_patcher_operation.h"
@@ -142,6 +143,12 @@ class PatchTest : public InProcessBrowserTest {
   DISALLOW_COPY_AND_ASSIGN(PatchTest);
 };
 
+// Flaky on Mac. http://crbug.com/785936
+#if defined(OS_MACOSX)
+#define MAYBE_CheckBsdiffOperation DISABLED_CheckBsdiffOperation
+#else
+#define MAYBE_CheckBsdiffOperation CheckBsdiffOperation
+#endif
 IN_PROC_BROWSER_TEST_F(PatchTest, CheckBsdiffOperation) {
   constexpr int kExpectedResult = bsdiff::OK;
 
@@ -155,7 +162,13 @@ IN_PROC_BROWSER_TEST_F(PatchTest, CheckBsdiffOperation) {
   EXPECT_TRUE(base::ContentsEqual(TestFile("binary_output.bin"), output_file));
 }
 
-IN_PROC_BROWSER_TEST_F(PatchTest, CheckCourgetteOperation) {
+// Flaky on Mac. http://crbug.com/785936
+#if defined(OS_MACOSX)
+#define MAYBE_CheckCourgetteOperation DISABLED_CheckCourgetteOperation
+#else
+#define MAYBE_CheckCourgetteOperation CheckCourgetteOperation
+#endif
+IN_PROC_BROWSER_TEST_F(PatchTest, MAYBE_CheckCourgetteOperation) {
   constexpr int kExpectedResult = courgette::C_OK;
 
   base::FilePath input_file = InputFilePath("binary_input.bin");
