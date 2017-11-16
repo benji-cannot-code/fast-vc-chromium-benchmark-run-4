79bd24b27b0831dc074935e95bb3f4df8a7c915d
Patti <patricialor@chromium.org>
2017-11-16 05:21:19 +0000

    Profile Chooser/Views: Only one HoverButton can be highlighted at a time.
    When mouse hovering and tabbing through the buttons in the ProfileChooserView
    at the same time, it is possible to get two different buttons to be highlighted.
    Prevent this by requesting focus on hover, then highlighting on focus only. This
    prevents confusion about which item actually has keyboard focus.
    
    This has the desired side effect of making the fade-in animation duration
    instant as well (crbug.com/784684).
    
    Bug: 785164, 785262, 784684
    Change-Id: Id379f2c373307f2c49dbce58cf40d3d61f9f324e
    Reviewed-on: https://chromium-review.googlesource.com/773319
    Commit-Queue: Patti <patricialor@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516996}

--- chrome/browser/ui/views/hover_button.h
+++ chrome/browser/ui/views/hover_button.h
@@ -47,6 +47,7 @@ class HoverButton : public views::LabelButton {
 
  protected:
   // views::LabelButton:
+  void StateChanged(ButtonState old_state) override;
   bool ShouldUseFloodFillInkDrop() const override;
 
   // views::InkDropHostView:
