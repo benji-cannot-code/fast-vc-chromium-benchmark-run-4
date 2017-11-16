8e4bc174981093e17ae6942946d9d7e0395e6c1b
Sarath Singapati <sarath.singapati@huawei.com>
2017-11-16 15:54:52 +0000

    [Android] Delete older browsing history.
    Added new timerange OLDER_THAN_30_DAYS to delete the browsing history.
    Added feature flag for the new timeperiod option in UI.
    Added GetPeriodEnd() in BrowsingDataCounter. Subclasses need to take
    GetPeriodEnd() into use in separate CLs.
    
    Bug: 243833
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I35a605d54e88ea757bac20de382855c3bfef6bd7
    Reviewed-on: https://chromium-review.googlesource.com/765488
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517094}

--- components/browsing_data/core/browsing_data_utils.cc
+++ components/browsing_data/core/browsing_data_utils.cc
@@ -35,6 +35,7 @@ base::Time CalculateBeginDeleteTime(TimePeriod time_period) {
       diff = base::TimeDelta::FromHours(4 * 7 * 24);
       break;
     case TimePeriod::ALL_TIME:
+    case TimePeriod::OLDER_THAN_30_DAYS:
       delete_begin_time = base::Time();
       break;
   }
@@ -42,7 +43,9 @@ base::Time CalculateBeginDeleteTime(TimePeriod time_period) {
 }
 
 base::Time CalculateEndDeleteTime(TimePeriod time_period) {
-  // No TimePeriod currently supports the second time bound.
+  if (time_period == TimePeriod::OLDER_THAN_30_DAYS) {
+    return base::Time::Now() - base::TimeDelta::FromDays(30);
+  }
   return base::Time::Max();
 }
 
@@ -64,6 +67,10 @@ void RecordDeletionForPeriod(TimePeriod period) {
     case TimePeriod::ALL_TIME:
       base::RecordAction(
           base::UserMetricsAction("ClearBrowsingData_Everything"));
+      break;
+    case TimePeriod::OLDER_THAN_30_DAYS:
+      base::RecordAction(
+          base::UserMetricsAction("ClearBrowsingData_OlderThan30Days"));
       break;
   }
 }
@@ -89,6 +96,10 @@ void RecordTimePeriodChange(TimePeriod period) {
     case TimePeriod::ALL_TIME:
       base::RecordAction(base::UserMetricsAction(
           "ClearBrowsingData_TimePeriodChanged_Everything"));
+      break;
+    case TimePeriod::OLDER_THAN_30_DAYS:
+      base::RecordAction(base::UserMetricsAction(
+          "ClearBrowsingData_TimePeriodChanged_OlderThan30Days"));
       break;
   }
 }
