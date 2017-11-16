2a34c9ff2e4fe3d0d49161e9a3aa39e65bc9ad7b
Devlin Cronin <rdevlin.cronin@chromium.org>
2017-11-16 03:49:55 +0000

    [Extensions Bindings] Enforce a max size for passed messages
    Update native bindings message passing to enforce the same max-size
    constraints that JS bindings do. Add unittests for the same.
    
    Bug: 653596
    Change-Id: I0ae97ba4f9a77e1aed6da1530865ba3963fc9b74
    Reviewed-on: https://chromium-review.googlesource.com/770551
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516976}

--- extensions/renderer/gin_port.cc
+++ extensions/renderer/gin_port.cc
@@ -115,10 +115,11 @@ void GinPort::PostMessageHandler(gin::Arguments* arguments,
     return;
   }
 
+  std::string error;
   std::unique_ptr<Message> message =
-      messaging_util::MessageFromV8(context, v8_message);
+      messaging_util::MessageFromV8(context, v8_message, &error);
   if (!message) {
-    ThrowError(isolate, "Illegal argument to Port.postMessage");
+    ThrowError(isolate, error);
     return;
   }
 
