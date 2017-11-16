f4ea862480a52afc71b1fc019005f1f0d902b668
Sylvain Defresne <sdefresne@chromium.org>
2017-11-16 16:57:37 +0000

    Allow WebStateObserver to observe N WebStates [31/N].
    Convert PagePlaceholderTabHelper to directly track
    the registration with the observed WebState instead
    of relying on the deprecated code in WebStateObserver.
    
    Bug: 775684
    Change-Id: I9784be30ecab75126349d401bfe75bc3114dd703
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Reviewed-on: https://chromium-review.googlesource.com/768815
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517108}

--- ios/chrome/browser/web/page_placeholder_tab_helper.mm
+++ ios/chrome/browser/web/page_placeholder_tab_helper.mm
@@ -20,9 +20,9 @@ DEFINE_WEB_STATE_USER_DATA_KEY(PagePlaceholderTabHelper);
 PagePlaceholderTabHelper::PagePlaceholderTabHelper(
     web::WebState* web_state,
     id<PagePlaceholderTabHelperDelegate> delegate)
-    : web::WebStateObserver(web_state),
-      delegate_(delegate),
-      weak_factory_(this) {}
+    : delegate_(delegate), weak_factory_(this) {
+  web_state->AddObserver(this);
+}
 
 PagePlaceholderTabHelper::~PagePlaceholderTabHelper() {
   RemovePlaceholder();
@@ -59,6 +59,7 @@ void PagePlaceholderTabHelper::PageLoaded(web::WebState* web_state,
 }
 
 void PagePlaceholderTabHelper::WebStateDestroyed(web::WebState* web_state) {
+  web_state->RemoveObserver(this);
   RemovePlaceholder();
 }
 
