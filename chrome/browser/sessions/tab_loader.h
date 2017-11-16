f8475b73c71f0a06e39bfe2cc7ccbcad5fbe3502
Peiyong Lin <lpy@chromium.org>
2017-11-16 18:06:21 +0000

    [GRC] Plumb page almost idle signal to TabLoader.
    Page almost idle signal is calculated from main thread task load and network
    activity. This patch plumbs page almost idle signal to TabLoader to experiment
    using this signal to schedule tab loading in SessionRestore. Currently,
    SessionRestore uses timeout and onload as signals to start to load next tab. A
    Finch Trial will be set up to experiment it.
    
    BUG=749789, 778766
    
    Change-Id: Id83b6decc1ade9d3377b5184f0c717ac24bebabb
    Reviewed-on: https://chromium-review.googlesource.com/764569
    Reviewed-by: Zhen Wang <zhenw@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: lpy <lpy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517124}

--- chrome/browser/sessions/tab_loader.h
+++ chrome/browser/sessions/tab_loader.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/memory_coordinator_client.h"
 #include "base/memory/memory_pressure_listener.h"
 #include "base/timer/timer.h"
+#include "chrome/browser/resource_coordinator/page_signal_receiver.h"
 #include "chrome/browser/sessions/session_restore_delegate.h"
 #include "chrome/browser/sessions/tab_loader_delegate.h"
 #include "content/public/browser/notification_observer.h"
@@ -45,7 +46,8 @@ class TabLoaderTest;
 class TabLoader : public content::NotificationObserver,
                   public base::RefCounted<TabLoader>,
                   public TabLoaderCallback,
-                  public base::MemoryCoordinatorClient {
+                  public base::MemoryCoordinatorClient,
+                  public resource_coordinator::PageSignalObserver {
  public:
   using RestoredTab = SessionRestoreDelegate::RestoredTab;
 
@@ -54,6 +56,9 @@ class TabLoader : public content::NotificationObserver,
   void Observe(int type,
                const content::NotificationSource& source,
                const content::NotificationDetails& details) override;
+
+  // resource_coordinator::PageSignalObserver implementation.
+  void OnPageAlmostIdle(content::WebContents* web_contents) override;
 
   // TabLoaderCallback:
   void SetTabLoadingEnabled(bool enable_tab_loading) override;
@@ -64,6 +69,7 @@ class TabLoader : public content::NotificationObserver,
 
  private:
   FRIEND_TEST_ALL_PREFIXES(TabLoaderTest, OnMemoryStateChange);
+  FRIEND_TEST_ALL_PREFIXES(TabLoaderTest, UsePageAlmostIdleSignal);
   FRIEND_TEST_ALL_PREFIXES(TabRestoreTest,
                            TabsFromRestoredWindowsAreLoadedGradually);
 
