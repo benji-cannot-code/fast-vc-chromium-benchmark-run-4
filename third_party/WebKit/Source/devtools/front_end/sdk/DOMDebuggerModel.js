db32b7ec41c3c6bd0e0bc8519b8958af93e64480
Erik Luo <luoe@chromium.org>
2017-11-16 00:10:58 +0000

    DevTools: add missing drag and message EventListenerBreakpoints
    Bug: 756032
    Change-Id: I31e9e3a9ba3d6cc32ba45fe04fa2749edf06c7d6
    Reviewed-on: https://chromium-review.googlesource.com/768094
    Reviewed-by: Alexei Filippov <alph@chromium.org>
    Commit-Queue: Erik Luo <luoe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516909}

--- third_party/WebKit/Source/devtools/front_end/sdk/DOMDebuggerModel.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/DOMDebuggerModel.js
@@ -614,7 +614,9 @@ SDK.DOMDebuggerManager = class {
         ],
         ['*']);
     this._createEventListenerBreakpoints(
-        Common.UIString('Drag / drop'), ['dragenter', 'dragover', 'dragleave', 'drop'], ['*']);
+        Common.UIString('Drag / drop'), ['drag', 'dragstart', 'dragend', 'dragenter', 'dragover', 'dragleave', 'drop'],
+        ['*']);
+
     this._createEventListenerBreakpoints(Common.UIString('Keyboard'), ['keydown', 'keyup', 'keypress', 'input'], ['*']);
     this._createEventListenerBreakpoints(
         Common.UIString('Load'), ['load', 'beforeunload', 'unload', 'abort', 'error', 'hashchange', 'popstate'], ['*']);
@@ -634,6 +636,7 @@ SDK.DOMDebuggerManager = class {
         ['*']);
     this._createEventListenerBreakpoints(
         Common.UIString('Touch'), ['touchstart', 'touchmove', 'touchend', 'touchcancel'], ['*']);
+    this._createEventListenerBreakpoints(Common.UIString('Worker'), ['message', 'messageerror'], ['*']);
     this._createEventListenerBreakpoints(
         Common.UIString('XHR'),
         ['readystatechange', 'load', 'loadstart', 'loadend', 'abort', 'error', 'progress', 'timeout'],
