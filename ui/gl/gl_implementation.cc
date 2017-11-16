b93513cb6a4f7e05f142a9a640ebd0ce5970e1c3
Vlad Tsyrklevich <vtsyrklevich@chromium.org>
2017-11-16 22:29:55 +0000

    [cfi-icall] Use ProtectedMemory for GetProcAddress
    Control Flow Integrity [1] indirect call (cfi-icall) checking can not
    verify that dynamically resolved function pointers call their intended
    function. Instead we place the pointer for GLGetProcAddress in
    ProtectedMemory, a wrapper for keeping variables in read-only memory
    except for when they are initialized.  After setting the pointer in
    protected memory we can use the UnsanitizedCfiCall wrapper to disable
    cfi-icall checking when calling it since we know it can not be tampered
    with.
    
    [1] https://www.chromium.org/developers/testing/control-flow-integrity
    
    Bug: 771365
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ia79c1cfab8e00f88bcc437c34cbb3012537c015a
    Reviewed-on: https://chromium-review.googlesource.com/769654
    Commit-Queue: Peter Collingbourne <pcc@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Peter Collingbourne <pcc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517208}

--- ui/gl/gl_implementation.cc
+++ ui/gl/gl_implementation.cc
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/logging.h"
 #include "base/macros.h"
+#include "base/memory/protected_memory.h"
+#include "base/memory/protected_memory_cfi.h"
 #include "base/memory/ptr_util.h"
 #include "base/stl_util.h"
 #include "base/strings/string_piece.h"
@@ -44,7 +46,10 @@ typedef std::vector<base::NativeLibrary> LibraryArray;
 
 GLImplementation g_gl_implementation = kGLImplementationNone;
 LibraryArray* g_libraries;
-GLGetProcAddressProc g_get_proc_address;
+// Place the function pointer for GetProcAddress in read-only memory after being
+// resolved to prevent it being tampered with. See crbug.com/771365 for details.
+PROTECTED_MEMORY_SECTION base::ProtectedMemory<GLGetProcAddressProc>
+    g_get_proc_address;
 
 void CleanupNativeLibraries(void* unused) {
   if (g_libraries) {
@@ -153,7 +158,8 @@ void UnloadGLNativeLibraries() {
 
 void SetGLGetProcAddressProc(GLGetProcAddressProc proc) {
   DCHECK(proc);
-  g_get_proc_address = proc;
+  auto writer = base::AutoWritableMemory::Create(g_get_proc_address);
+  *g_get_proc_address = proc;
 }
 
 GLFunctionPointerType GetGLProcAddress(const char* name) {
@@ -167,8 +173,9 @@ GLFunctionPointerType GetGLProcAddress(const char* name) {
         return proc;
     }
   }
-  if (g_get_proc_address) {
-    GLFunctionPointerType proc = g_get_proc_address(name);
+  if (*g_get_proc_address) {
+    GLFunctionPointerType proc =
+        base::UnsanitizedCfiCall(g_get_proc_address)(name);
     if (proc)
       return proc;
   }
