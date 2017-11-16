80825b51bdf3069d3fa68d401af5616e305f4171
Mark Cogan <marq@google.com>
2017-11-16 18:35:41 +0000

    [iOS showcase] Remove dependencies on clean/ from showcase.
    This CL removes clean/ view controllers from Showcase.
    
    Bug: 785925, 710662, 772186
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Iebce9f69b7776aa2755e46ec379d7245075a55a7
    Reviewed-on: https://chromium-review.googlesource.com/774465
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Reviewed-by: Louis Romero <lpromero@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517135}

--- ios/showcase/core/showcase_model.mm
+++ ios/showcase/core/showcase_model.mm
@@ -23,16 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       showcase::kUseCaseKey : @"Content Suggestions UI",
     },
     @{
-      showcase::kClassForDisplayKey : @"MenuViewController",
-      showcase::kClassForInstantiationKey : @"SCToolsCoordinator",
-      showcase::kUseCaseKey : @"Tools menu",
-    },
-    @{
-      showcase::kClassForDisplayKey : @"NTPViewController",
-      showcase::kClassForInstantiationKey : @"SCNTPCoordinator",
-      showcase::kUseCaseKey : @"NTP",
-    },
-    @{
       showcase::kClassForDisplayKey : @"PaymentRequestEditViewController",
       showcase::kClassForInstantiationKey : @"SCPaymentsEditorCoordinator",
       showcase::kUseCaseKey : @"Generic payment request editor",
@@ -48,34 +38,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       showcase::kUseCaseKey : @"Payment request selector view",
     },
     @{
-      showcase::kClassForDisplayKey : @"RootContainerViewController",
-      showcase::kClassForInstantiationKey : @"SCRootCoordinator",
-      showcase::kUseCaseKey : @"Root container",
-    },
-    @{
       showcase::kClassForDisplayKey : @"SettingsViewController",
       showcase::kClassForInstantiationKey : @"SCSettingsCoordinator",
       showcase::kUseCaseKey : @"Main settings screen",
-    },
-    @{
-      showcase::kClassForDisplayKey : @"TabGridViewController",
-      showcase::kClassForInstantiationKey : @"SCTabGridCoordinator",
-      showcase::kUseCaseKey : @"Tab grid",
-    },
-    @{
-      showcase::kClassForDisplayKey : @"TopToolbarTabViewController",
-      showcase::kClassForInstantiationKey : @"SCTopToolbarTabCoordinator",
-      showcase::kUseCaseKey : @"Top toolbar tab",
-    },
-    @{
-      showcase::kClassForDisplayKey : @"BottomToolbarTabViewController",
-      showcase::kClassForInstantiationKey : @"SCBottomToolbarTabCoordinator",
-      showcase::kUseCaseKey : @"Bottom toolbar tab",
-    },
-    @{
-      showcase::kClassForDisplayKey : @"ToolbarViewController",
-      showcase::kClassForInstantiationKey : @"SCToolbarCoordinator",
-      showcase::kUseCaseKey : @"Toolbar",
     },
     @{
       showcase::kClassForDisplayKey : @"UITableViewCell",
