dff0beb41762febd554ca3353c419710a9d7eddb
Gayane Petrosyan <gayane@chromium.org>
2017-11-16 17:24:09 +0000

    Adding milestone and channel to variations seed request URL
    Bug: 776071
    Change-Id: I14e4aac7e366d9344f4c25e1f752fce193226784
    Reviewed-on: https://chromium-review.googlesource.com/737558
    Commit-Queue: Gayane Petrosyan <gayane@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Tao Bai <michaelbai@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517113}

--- chrome/android/junit/src/org/chromium/chrome/browser/init/AsyncInitTaskRunnerTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/init/AsyncInitTaskRunnerTest.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.init;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
@@ -87,7 +88,7 @@ public class AsyncInitTaskRunnerTest {
         verify(mLoader).ensureInitialized();
         verify(mLoader).asyncPrefetchLibrariesToMemory();
         verify(mRunner).onSuccess();
-        verify(mVariationsSeedFetcher, never()).fetchSeed("");
+        verify(mVariationsSeedFetcher, never()).fetchSeed(anyString(), anyString(), anyString());
     }
 
     @Test
@@ -101,7 +102,7 @@ public class AsyncInitTaskRunnerTest {
         Robolectric.flushForegroundThreadScheduler();
         assertTrue(mLatch.await(0, TimeUnit.SECONDS));
         verify(mRunner).onFailure();
-        verify(mVariationsSeedFetcher, never()).fetchSeed("");
+        verify(mVariationsSeedFetcher, never()).fetchSeed(anyString(), anyString(), anyString());
     }
 
     @Test
@@ -114,7 +115,7 @@ public class AsyncInitTaskRunnerTest {
         verify(mLoader).ensureInitialized();
         verify(mLoader).asyncPrefetchLibrariesToMemory();
         verify(mRunner).onSuccess();
-        verify(mVariationsSeedFetcher).fetchSeed("");
+        verify(mVariationsSeedFetcher).fetchSeed(anyString(), anyString(), anyString());
     }
 
     // TODO(aberent) Test for allocateChildConnection. Needs refactoring of ChildProcessLauncher to
--- components/variations/service/variations_service.cc
+++ components/variations/service/variations_service.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/metrics/sparse_histogram.h"
+#include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
 #include "base/sys_info.h"
 #include "base/task_runner_util.h"
@@ -39,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/variations/variations_seed_simulator.h"
 #include "components/variations/variations_switches.h"
 #include "components/variations/variations_url_constants.h"
+#include "components/version_info/version_info.h"
 #include "net/base/load_flags.h"
 #include "net/base/net_errors.h"
 #include "net/base/network_change_notifier.h"
@@ -345,6 +347,21 @@ GURL VariationsService::GetVariationsServerURL(
   server_url = net::AppendOrReplaceQueryParameter(server_url, "osname",
                                                   GetPlatformString());
 
+  // Add channel to the request URL.
+  version_info::Channel channel = client_->GetChannel();
+  if (channel != version_info::Channel::UNKNOWN) {
+    server_url = net::AppendOrReplaceQueryParameter(
+        server_url, "channel", version_info::GetChannelString(channel));
+  }
+
+  // Add milestone to the request URL.
+  std::string version = version_info::GetVersionNumber();
+  std::vector<std::string> version_parts = base::SplitString(
+      version, ".", base::TRIM_WHITESPACE, base::SPLIT_WANT_NONEMPTY);
+  if (version_parts.size() > 0) {
+    server_url = net::AppendOrReplaceQueryParameter(server_url, "milestone",
+                                                    version_parts[0]);
+  }
   DCHECK(server_url.is_valid());
   return server_url;
 }
