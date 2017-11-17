a088b3a75ad35d0e605a56663e7eeb628412a20d
Hajime Hoshi <hajimehoshi@chromium.org>
2017-11-17 13:32:30 +0000

    Use the terms Pause/Unpause instead of Suspend/Resume in Blink
    Bug: 780378
    Change-Id: Iac3e165e9721dc9fbe4b1771f8a85e89b98e0056
    Reviewed-on: https://chromium-review.googlesource.com/775611
    Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517392}

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -6408,7 +6408,7 @@ void Document::TasksWerePaused() {
   GetScriptRunner()->Suspend();
 
   if (parser_)
-    parser_->SuspendScheduledTasks();
+    parser_->PauseScheduledTasks();
   if (scripted_animation_controller_)
     scripted_animation_controller_->Pause();
 }
@@ -6417,7 +6417,7 @@ void Document::TasksWereUnpaused() {
   GetScriptRunner()->Resume();
 
   if (parser_)
-    parser_->ResumeScheduledTasks();
+    parser_->UnpauseScheduledTasks();
   if (scripted_animation_controller_)
     scripted_animation_controller_->Unpause();
 
--- third_party/WebKit/Source/core/dom/DocumentParser.h
+++ third_party/WebKit/Source/core/dom/DocumentParser.h
@@ -110,8 +110,8 @@ class CORE_EXPORT DocumentParser
   }
 
   // FIXME: The names are not very accurate :(
-  virtual void SuspendScheduledTasks();
-  virtual void ResumeScheduledTasks();
+  virtual void PauseScheduledTasks();
+  virtual void UnpauseScheduledTasks();
 
   void AddClient(DocumentParserClient*);
   void RemoveClient(DocumentParserClient*);
