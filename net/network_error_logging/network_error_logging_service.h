ea9102e6e86e88d2b77f20a1f1e41fcd62da45dc
Julia Tuttle <juliatuttle@chromium.org>
2017-11-17 16:36:27 +0000

    Network Error Logging: Move from //components to //net.
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ic7038de9351cd34c22a23a0e60694e7330e1ab66
    Reviewed-on: https://chromium-review.googlesource.com/766730
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Julia Tuttle <juliatuttle@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517425}

--- net/network_error_logging/network_error_logging_service.h
+++ net/network_error_logging/network_error_logging_service.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef COMPONENTS_NETWORK_ERROR_LOGGING_NETWORK_ERROR_LOGGING_SERVICE_H_
-#define COMPONENTS_NETWORK_ERROR_LOGGING_NETWORK_ERROR_LOGGING_SERVICE_H_
+#ifndef NET_NETWORK_ERROR_LOGGING_NETWORK_ERROR_LOGGING_SERVICE_H_
+#define NET_NETWORK_ERROR_LOGGING_NETWORK_ERROR_LOGGING_SERVICE_H_
 
 #include <map>
 #include <memory>
@@ -15,8 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/time/tick_clock.h"
 #include "base/time/time.h"
-#include "components/network_error_logging/network_error_logging_export.h"
 #include "net/base/net_errors.h"
+#include "net/base/net_export.h"
 #include "net/url_request/network_error_logging_delegate.h"
 
 namespace base {
@@ -32,13 +32,13 @@ class Origin;
 }  // namespace url
 
 namespace features {
-extern const base::Feature NETWORK_ERROR_LOGGING_EXPORT kNetworkErrorLogging;
+extern const base::Feature NET_EXPORT kNetworkErrorLogging;
 }  // namespace features
 
-namespace network_error_logging {
+namespace net {
 
-class NETWORK_ERROR_LOGGING_EXPORT NetworkErrorLoggingService
-    : public net::NetworkErrorLoggingDelegate {
+class NET_EXPORT NetworkErrorLoggingService
+    : public NetworkErrorLoggingDelegate {
  public:
   static const char kReportType[];
 
@@ -58,11 +58,11 @@ class NETWORK_ERROR_LOGGING_EXPORT NetworkErrorLoggingService
   // base::FeatureList.
   static std::unique_ptr<NetworkErrorLoggingService> Create();
 
-  // net::NetworkErrorLoggingDelegate implementation:
+  // NetworkErrorLoggingDelegate implementation:
 
   ~NetworkErrorLoggingService() override;
 
-  void SetReportingService(net::ReportingService* reporting_service) override;
+  void SetReportingService(ReportingService* reporting_service) override;
 
   void OnHeader(const url::Origin& origin, const std::string& value) override;
 
@@ -117,7 +117,7 @@ class NETWORK_ERROR_LOGGING_EXPORT NetworkErrorLoggingService
   std::unique_ptr<base::TickClock> tick_clock_;
 
   // Unowned.
-  net::ReportingService* reporting_service_;
+  ReportingService* reporting_service_;
 
   PolicyMap policies_;
   WildcardPolicyMap wildcard_policies_;
@@ -125,6 +125,6 @@ class NETWORK_ERROR_LOGGING_EXPORT NetworkErrorLoggingService
   DISALLOW_COPY_AND_ASSIGN(NetworkErrorLoggingService);
 };
 
-}  // namespace network_error_logging
+}  // namespace net
 
-#endif  // COMPONENTS_NETWORK_ERROR_LOGGING_NETWORK_ERROR_LOGGING_SERVICE_H_
+#endif  // NET_NETWORK_ERROR_LOGGING_NETWORK_ERROR_LOGGING_SERVICE_H_
