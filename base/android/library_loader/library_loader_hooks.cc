b166ef6555fdd8d4486ed405b89cf1d7f5539669
Benoit Lize <lizeb@chromium.org>
2017-11-17 12:35:04 +0000

    android: Log and plot native library code residency.
    When enabled with the --log-native-library-residency command line switch,
    periodically collects native library residency, and dumps it to a
    file. process_residency.py then takes the output and creates a graph from
    it. This is used to assess the efficiency of the code ordering logic.
    
    Bug: 746665
    Change-Id: I4a21ce4b9c01f8bb255f5997e665ff3dbdcf240a
    Reviewed-on: https://chromium-review.googlesource.com/756847
    Commit-Queue: Benoit L <lizeb@chromium.org>
    Reviewed-by: agrieve <agrieve@chromium.org>
    Reviewed-by: Matthew Cary <mattcary@chromium.org>
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517385}

--- base/android/library_loader/library_loader_hooks.cc
+++ base/android/library_loader/library_loader_hooks.cc
@@ -84,7 +84,7 @@ void RecordLibraryPreloaderRendereHistogram() {
   }
 }
 
-} // namespace
+}  // namespace
 
 static void RegisterChromiumAndroidLinkerRendererHistogram(
     JNIEnv* env,
@@ -193,6 +193,11 @@ static jint PercentageOfResidentNativeLibraryCode(
     JNIEnv* env,
     const JavaParamRef<jclass>& clazz) {
   return NativeLibraryPrefetcher::PercentageOfResidentNativeLibraryCode();
+}
+
+static void PeriodicallyCollectResidency(JNIEnv* env,
+                                         const JavaParamRef<jclass>& clazz) {
+  return NativeLibraryPrefetcher::PeriodicallyCollectResidency();
 }
 
 void SetVersionNumber(const char* version_number) {
