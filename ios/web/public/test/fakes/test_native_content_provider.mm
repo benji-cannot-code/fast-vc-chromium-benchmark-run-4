4013049aca90dc264c2c432f0263337c1ca4c4ca
Eugene But <eugenebut@chromium.org>
2017-11-17 16:20:05 +0000

    Fixed Navigation callbacks for failed provisional navigation.
    Verified callbacks correctness with
    NavigationCallbacksTest.FailedNavigation integration test.
    
    Before this CL DidStartNavigation was called twice (second time
    for presenting error in Native Content). DidFinishNavigation was
    called once (also for presenting NativeContent). First
    DidStartNavigation did not have a matching DidStartNavigation
    call.
    
    This CL removes DidStartNavigation/DidFinishNavigation callbacks
    for for presenting error in Native Content and adds balanced
    DidFinishNavigation callback in handleLoadingError:.
    
    Bug: 725241
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Id15ef8c9422e571f74f9c00bf88ea98ccbf6607c
    Reviewed-on: https://chromium-review.googlesource.com/758718
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517420}

--- ios/web/public/test/fakes/test_native_content_provider.mm
+++ ios/web/public/test/fakes/test_native_content_provider.mm
@@ -28,17 +28,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (id<CRWNativeContent>)controllerForURL:(const GURL&)URL
                                 webState:(web::WebState*)webState {
   auto nativeContent = _nativeContent.find(URL);
-  if (nativeContent == _nativeContent.end()) {
-    return nil;
-  }
-  return nativeContent->second;
+  return nativeContent == _nativeContent.end() ? nil : nativeContent->second;
 }
 
 - (id<CRWNativeContent>)controllerForURL:(const GURL&)URL
                                withError:(NSError*)error
                                   isPost:(BOOL)isPost {
-  NOTREACHED();
-  return nil;
+  auto nativeContent = _nativeContent.find(URL);
+  return nativeContent == _nativeContent.end() ? nil : nativeContent->second;
 }
 
 @end
