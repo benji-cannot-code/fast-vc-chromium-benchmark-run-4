72496da683ef6889a2c4157d2cb2fdb404c2e75e
Conley Owens <cco3@chromium.org>
2017-11-17 19:04:21 +0000

    bluetooth: Mark GattServiceChanged as deprecated
    Similar adapter observer methods have already been marked deprecated.
    GattServiceChanged is not all too different since it is fired when
    essentially the device changes shape (attributes are added/removed).
    
    This change also removes a platform-specific use of
    gatt_service_changed_count() and gatt_characteristic_added_count().
    
    BUG=710352
    
    Change-Id: Iffe8118083ec10ba762eacb18886bca3acda2d58
    Reviewed-on: https://chromium-review.googlesource.com/776354
    Commit-Queue: Conley Owens <cco3@chromium.org>
    Commit-Queue: Vincent Scheib <scheib@chromium.org>
    Reviewed-by: Vincent Scheib <scheib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517498}

--- device/bluetooth/bluetooth_remote_gatt_service_unittest.cc
+++ device/bluetooth/bluetooth_remote_gatt_service_unittest.cc
@@ -287,10 +287,6 @@ TEST_F(BluetoothRemoteGattServiceTest, GattCharacteristics_ObserversCalls) {
                              /* properties */ 0);
   SimulateGattCharacteristic(service, kTestUUIDReconnectionAddress,
                              /* properties */ 0);
-#if !defined(OS_WIN)
-  // TODO(620895) GattCharacteristicAdded has to be implemented for Windows.
-  EXPECT_EQ(4, observer.gatt_characteristic_added_count());
-#endif  // !defined(OS_WIN)
 
   // Simulate remove of characteristics one by one.
   EXPECT_EQ(4u, service->GetCharacteristics().size());
@@ -318,15 +314,6 @@ TEST_F(BluetoothRemoteGattServiceTest, GattCharacteristics_ObserversCalls) {
   EXPECT_EQ(4, observer.gatt_characteristic_removed_count());
   EXPECT_FALSE(service->GetCharacteristic(removed_char));
   EXPECT_EQ(0u, service->GetCharacteristics().size());
-
-#if defined(OS_MACOSX)
-  // SimulateGattServicesDiscovered
-  // 4 * SimulateGattCharacteristic
-  // 4 * SimulateGattCharacteristicRemoved
-  EXPECT_EQ(9, observer.gatt_service_changed_count());
-#else  // defined(OS_MACOSX)
-  EXPECT_EQ(4, observer.gatt_service_changed_count());
-#endif  // defined(OS_MACOSX)
 }
 #endif  //  defined(OS_MACOSX) || defined(OS_WIN)
 
