68155824635d6258b2067e1bb066131220cc57f3
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2017-11-17 08:29:18 +0000

    Clean up for AppIndexing
    No functional changes.
    
    Bug: None
    Change-Id: I11894fa0b31508293c9d1cdcbdf3213ab8cf63d1
    Reviewed-on: https://chromium-review.googlesource.com/775668
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517343}

--- chrome/android/java/src/org/chromium/chrome/browser/historyreport/AppIndexingReporter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/historyreport/AppIndexingReporter.java
@@ -10,7 +10,6 @@ import org.chromium.chrome.browser.AppHooks;
 
 /** Base class for reporting entities to App Indexing. */
 public class AppIndexingReporter {
-    private static final String TAG = "AppIndexingReporter";
     private static AppIndexingReporter sInstance;
 
     public static AppIndexingReporter getInstance() {
@@ -26,7 +25,7 @@ public class AppIndexingReporter {
      * implement this functionality.
      */
     public void reportWebPage(WebPage webpage) {
-        // Overriden by private class. Base class does nothing.
+        // Overridden by private class. Base class does nothing.
     }
 
     /**
@@ -35,7 +34,7 @@ public class AppIndexingReporter {
      * implement this functionality.
      */
     public void reportWebPageView(String url, String title) {
-        // Overriden by private class. Base class does nothing.
+        // Overridden by private class. Base class does nothing.
     }
 
     /**
@@ -44,6 +43,6 @@ public class AppIndexingReporter {
      * any reporting, and call is a no-op. Child classes should implement this functionality.
      */
     public void clearHistory() {
-        // Overriden by private class. Base class does nothing.
+        // Overridden by private class. Base class does nothing.
     }
 }
