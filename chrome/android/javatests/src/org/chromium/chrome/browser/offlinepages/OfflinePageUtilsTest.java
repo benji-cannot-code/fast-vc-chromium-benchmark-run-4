79d9f1586a59635c5a46394f1e744ce612603556
Tommy Nyquist <nyquist@chromium.org>
2017-11-17 01:59:06 +0000

    Disable flaky test suite OfflinePageUtilsTest
    TBR=fgorski@chromium.org
    BUG=786237
    NOTRY=true
    
    Change-Id: I3407d82f6d1046deb5a3b4075200d4427f6978da
    Reviewed-on: https://chromium-review.googlesource.com/776233
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517246}

--- chrome/android/javatests/src/org/chromium/chrome/browser/offlinepages/OfflinePageUtilsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/offlinepages/OfflinePageUtilsTest.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.RetryOnFailure;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.browser.ChromeSwitches;
 import org.chromium.chrome.browser.offlinepages.OfflinePageBridge.OfflinePageModelObserver;
@@ -142,7 +142,7 @@ public class OfflinePageUtilsTest {
 
     @Test
     @SmallTest
-    @RetryOnFailure
+    @DisabledTest(message = "crbug.com/786237")
     public void testShowOfflineSnackbarIfNecessary() throws Exception {
         // Arrange - build a mock controller for sensing.
         OfflinePageUtils.setSnackbarDurationForTesting(1000);
