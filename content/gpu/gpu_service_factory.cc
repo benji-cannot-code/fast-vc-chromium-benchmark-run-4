faa4d1c4f6d852e41067c6c8c6ef3151748f8599
Xiaohan Wang <xhwang@chromium.org>
2017-11-17 19:13:49 +0000

    media: Run MediaService on a sequence instead of on its own thread
    BUG=783973
    
    Change-Id: I74cdc80d3cd4187ce0741fa722e2527329347ec5
    Reviewed-on: https://chromium-review.googlesource.com/770991
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517503}

--- content/gpu/gpu_service_factory.cc
+++ content/gpu/gpu_service_factory.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/task_scheduler/post_task.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "services/shape_detection/public/interfaces/constants.mojom.h"
 #include "services/shape_detection/shape_detection_service.h"
@@ -38,7 +39,12 @@ void GpuServiceFactory::RegisterServices(ServiceMap* services) {
   info.factory =
       base::Bind(&media::CreateGpuMediaService, gpu_preferences_, task_runner_,
                  media_gpu_channel_manager_, android_overlay_factory_cb_);
-  info.use_own_thread = true;
+  // This service will host audio/video decoders, and if these decoding
+  // operations are blocked, user may hear audio glitch or see video freezing,
+  // hence "user blocking".
+  // TODO(crbug.com/786169): Check whether this needs to be single threaded.
+  info.task_runner = base::CreateSingleThreadTaskRunnerWithTraits(
+      {base::TaskPriority::USER_BLOCKING});
   services->insert(std::make_pair("media", info));
 #endif
 
