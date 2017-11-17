b4f6d8d344b194d8c37fa9bac3b0aa492a8455dc
Christopher Grant <cjgrant@chromium.org>
2017-11-17 23:35:51 +0000

    VR: Add an AutocompleteController instance
    This CL instantiates an AutocompleteController upon first use, and
    supplies resulting matches to the UI.
    
    BUG=777542
    
    Change-Id: I1d7323f0bc4889f4e93664c129253adb921df8f5
    Reviewed-on: https://chromium-review.googlesource.com/772979
    Commit-Queue: Christopher Grant <cjgrant@chromium.org>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517606}

---
+++ chrome/browser/android/vr_shell/autocomplete_controller.h
@@ -0,0 +1,43 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ANDROID_VR_SHELL_AUTOCOMPLETE_CONTROLLER_H_
+#define CHROME_BROWSER_ANDROID_VR_SHELL_AUTOCOMPLETE_CONTROLLER_H_
+
+#include <memory>
+
+#include "base/macros.h"
+#include "base/values.h"
+#include "components/omnibox/browser/autocomplete_controller_delegate.h"
+
+class AutocompleteController;
+class Profile;
+
+namespace vr {
+
+class BrowserUiInterface;
+
+class AutocompleteController : public AutocompleteControllerDelegate {
+ public:
+  explicit AutocompleteController(BrowserUiInterface* ui);
+  AutocompleteController();
+  ~AutocompleteController() override;
+
+  void Start(const base::string16& text);
+  void Stop();
+
+ private:
+  void OnResultChanged(bool default_match_changed) override;
+
+  Profile* profile_;
+  std::unique_ptr<::AutocompleteController> autocomplete_controller_;
+  BrowserUiInterface* ui_;
+
+  DISALLOW_COPY_AND_ASSIGN(AutocompleteController);
+};
+
+}  // namespace vr
+
+#endif  // CHROME_BROWSER_ANDROID_VR_SHELL_AUTOCOMPLETE_CONTROLLER_H_
