7bdeb0672834f6f7ff76378e76ca642ea9dcddda
Yusuf Ozuysal <yusufo@google.com>
2017-11-17 23:48:02 +0000

    [TrustedWeb] Fix threading issues on TrustedWebActivity launch
    - Remove the Android Manifest DAL that is done in
    canSessionLaunchInTrustedWebActivity which is always called on UI
    thread.
    - To reliably handle the above missing check before launching the
    TrustedWebActivity add a related param to SessionParams
    - Make the same call as a part of validateRelationship and cache the
    result to be used later
    - Fixes to validateRelationship threading and calling order.
    
    BUG=782904
    
    Change-Id: Ief5432bc7004e56f9106d58ddbff31d69d0ddc55
    Reviewed-on: https://chromium-review.googlesource.com/759232
    Commit-Queue: Yusuf Ozuysal <yusufo@chromium.org>
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517618}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/ClientManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/ClientManager.java
@@ -22,6 +22,7 @@ import android.support.customtabs.CustomTabsSessionToken;
 import android.text.TextUtils;
 import android.util.SparseBooleanArray;
 
+import org.chromium.base.ThreadUtils;
 import org.chromium.base.VisibleForTesting;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.chrome.browser.ChromeFeatureList;
@@ -39,8 +40,10 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 /** Manages the clients' state for Custom Tabs. This class is threadsafe. */
@@ -130,6 +133,7 @@ class ClientManager {
         public final int uid;
         public final DisconnectCallback disconnectCallback;
         public final PostMessageHandler postMessageHandler;
+        public final Set<Uri> mLinkedUrls = new HashSet<>();
         public OriginVerifier originVerifier;
         public boolean mIgnoreFragments;
         public boolean lowConfidencePrediction;
@@ -378,6 +382,9 @@ class ClientManager {
         validateRelationshipInternal(session, relation, origin, true);
     }
 
+    /**
+     * Can't be called on UI Thread.
+     */
     private synchronized boolean validateRelationshipInternal(CustomTabsSessionToken session,
             int relation, Uri origin, boolean initializePostMessageChannel) {
         SessionParams params = mSessionParams.get(session);
@@ -385,7 +392,13 @@ class ClientManager {
         OriginVerificationListener listener = null;
         if (initializePostMessageChannel) listener = params.postMessageHandler;
         params.originVerifier = new OriginVerifier(listener, params.getPackageName(), relation);
-        params.originVerifier.start(origin);
+        ThreadUtils.runOnUiThread(() -> { params.originVerifier.start(origin); });
+        if (relation == CustomTabsService.RELATION_HANDLE_ALL_URLS
+                && InstalledAppProviderImpl.isAppInstalledAndAssociatedWithOrigin(
+                           params.getPackageName(), URI.create(origin.toString()),
+                           mContext.getPackageManager())) {
+            params.mLinkedUrls.add(origin);
+        }
         return true;
     }
 
@@ -408,9 +421,7 @@ class ClientManager {
         if (params == null) return false;
         String packageName = params.getPackageName();
         if (TextUtils.isEmpty(packageName)) return false;
-        boolean isAppAssociatedWithOrigin =
-                InstalledAppProviderImpl.isAppInstalledAndAssociatedWithOrigin(
-                        packageName, URI.create(origin.toString()), mContext.getPackageManager());
+        boolean isAppAssociatedWithOrigin = params.mLinkedUrls.contains(origin);
         if (!isAppAssociatedWithOrigin) return false;
         if (OriginVerifier.isValidOrigin(
                     packageName, origin, CustomTabsService.RELATION_HANDLE_ALL_URLS)) {
