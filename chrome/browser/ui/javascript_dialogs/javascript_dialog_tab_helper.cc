3aab204d8325088dae3061daae2e10a20e91ef53
Avi Drissman <avi@chromium.org>
2017-11-17 20:31:41 +0000

    Measure the schemes of alerting pages.
    BUG=696454
    
    Change-Id: I7ea839d6c9135c639b1a8a68843721142d4bf81c
    Reviewed-on: https://chromium-review.googlesource.com/776123
    Reviewed-by: Eric Lawrence <elawrence@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517540}

--- chrome/browser/ui/javascript_dialogs/javascript_dialog_tab_helper.cc
+++ chrome/browser/ui/javascript_dialogs/javascript_dialog_tab_helper.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tab_modal_confirm_dialog.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "components/app_modal/javascript_dialog_manager.h"
+#include "components/navigation_metrics/navigation_metrics.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/render_frame_host.h"
 #include "ui/gfx/text_elider.h"
@@ -88,15 +89,23 @@ void JavaScriptDialogTabHelper::RunJavaScriptDialog(
   content::WebContents* parent_web_contents =
       WebContentsObserver::web_contents();
   bool foremost = IsWebContentsForemost(parent_web_contents);
+  navigation_metrics::Scheme scheme =
+      navigation_metrics::GetScheme(alerting_frame_url);
   switch (dialog_type) {
     case content::JAVASCRIPT_DIALOG_TYPE_ALERT:
       UMA_HISTOGRAM_BOOLEAN("JSDialogs.IsForemost.Alert", foremost);
+      UMA_HISTOGRAM_ENUMERATION("JSDialogs.Scheme.Alert", scheme,
+                                navigation_metrics::Scheme::COUNT);
       break;
     case content::JAVASCRIPT_DIALOG_TYPE_CONFIRM:
       UMA_HISTOGRAM_BOOLEAN("JSDialogs.IsForemost.Confirm", foremost);
+      UMA_HISTOGRAM_ENUMERATION("JSDialogs.Scheme.Confirm", scheme,
+                                navigation_metrics::Scheme::COUNT);
       break;
     case content::JAVASCRIPT_DIALOG_TYPE_PROMPT:
       UMA_HISTOGRAM_BOOLEAN("JSDialogs.IsForemost.Prompt", foremost);
+      UMA_HISTOGRAM_ENUMERATION("JSDialogs.Scheme.Prompt", scheme,
+                                navigation_metrics::Scheme::COUNT);
       break;
   }
 
