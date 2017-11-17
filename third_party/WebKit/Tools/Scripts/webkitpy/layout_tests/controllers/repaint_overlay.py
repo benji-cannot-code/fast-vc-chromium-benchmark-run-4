d3140c99767680cebc89a8cd43f03da5168e51ff
Xianzhu Wang <wangxianzhu@chromium.org>
2017-11-17 20:31:00 +0000

    [layout test results.html] Add repaint overlay
    Repaint overlay visualizes the repaint rectangles in the text results of
    repaint tests. This CL brings the feature in the legacy results.html
    into the new test expectations page.
    
    Also slightly tweak the handling of keyboard navigation.
    
    Change-Id: I8240e38449287ac899a33a691f0e234e74575ad1
    Reviewed-on: https://chromium-review.googlesource.com/776094
    Reviewed-by: Aleks Totic <atotic@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517539}

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/controllers/repaint_overlay.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/controllers/repaint_overlay.py
@@ -55,6 +55,7 @@ def generate_repaint_overlay_html(test_name, actual_text, expected_text):
         width: 800px;
         height: 600px;
         border: 0;
+        display: none;
     }
     canvas {
         position: absolute;
@@ -65,7 +66,7 @@ def generate_repaint_overlay_html(test_name, actual_text, expected_text):
 </style>
 </head>
 <body>
-<label><input id="show-test" type="checkbox" checked onchange="toggle_test(this.checked)">Show test</label>
+<label><input id="show-test" type="checkbox" onchange="toggle_test(this.checked)">Show test</label>
 <label><input id="use-solid-colors" type="checkbox" onchange="toggle_solid_color(this.checked)">Use solid colors</label>
 <br>
 <span id="overlay_type">Expected Invalidations</span>
@@ -79,6 +80,8 @@ var overlay_opacity = 0.25;
 
 function toggle_test(show_test) {
     test_frame.style.display = show_test ? 'block' : 'none';
+    if (!test_frame.src)
+        test_frame.src = decodeURIComponent(location.search).substr(1);
 }
 
 function toggle_solid_color(use_solid_color) {
@@ -155,8 +158,6 @@ function draw_repaint_rects() {
 
 draw_repaint_rects();
 
-test_frame.src = decodeURIComponent(location.search).substr(1);
-
 var expected_showing = true;
 function flip() {
     if (expected_showing) {
@@ -170,7 +171,7 @@ function flip() {
     }
     expected_showing = !expected_showing
 }
-setInterval(flip, 3000);
+setInterval(flip, 1500);
 </script>
 </body>
 </html>
