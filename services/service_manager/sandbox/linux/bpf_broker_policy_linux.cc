00df05d13f245a3dc681798d29689deba43c5bfa
Tom Sepez <tsepez@chromium.org>
2017-11-17 21:05:39 +0000

    Make broker policy not inherit from gpu policy.
    Change-Id: I4054f494566b4755d7bd9e411b47f390e0c83458
    Reviewed-on: https://chromium-review.googlesource.com/773498
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517550}

--- services/service_manager/sandbox/linux/bpf_broker_policy_linux.cc
+++ services/service_manager/sandbox/linux/bpf_broker_policy_linux.cc
@@ -32,7 +32,7 @@ ResultExpr BrokerProcessPolicy::EvaluateSyscall(int sysno) const {
 #endif
       return Allow();
     default:
-      return GpuProcessPolicy::EvaluateSyscall(sysno);
+      return BPFBasePolicy::EvaluateSyscall(sysno);
   }
 }
 
