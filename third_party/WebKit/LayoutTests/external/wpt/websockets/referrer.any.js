247a42def5754c56014cc8114af5e49cc1a99a11
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2017-11-17 22:03:57 +0000

    Import wpt@eba4798a9895c60f65dd1efba5483751d8c1e784
    Using wpt-import in Chromium bbac322d926bf7745a32569b5a5c0c8bcfe90518.
    With Chromium commits locally applied on WPT:
    2370563111 "service worker: Upstream sandboxed iframe test to WPT."
    
    
    Build: https://build.chromium.org/p/chromium.infra.cron/builders/wpt-importer/builds/5389
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    TBR=lukebjerring
    
    No-Export: true
    Change-Id: I94eb8b2fdf16c62b380e4ceedeb5eb1bac1abf3e
    Reviewed-on: https://chromium-review.googlesource.com/775869
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517577}

---
+++ third_party/WebKit/LayoutTests/external/wpt/websockets/referrer.any.js
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=constants.js?pipe=sub
+
+async_test(t => {
+  const ws = new WebSocket(SCHEME_DOMAIN_PORT + "/referrer");
+  ws.onmessage = t.step_func_done(e => {
+    assert_equals(e.data, "MISSING AS PER FETCH");
+    ws.close();
+  });
+
+  // Avoid timeouts in case of failure
+  ws.onclose = t.unreached_func("close");
+  ws.onerror = t.unreached_func("error");
+}, "Ensure no Referer header is included");
