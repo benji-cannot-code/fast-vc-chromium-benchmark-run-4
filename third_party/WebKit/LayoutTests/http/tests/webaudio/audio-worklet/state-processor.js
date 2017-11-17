2cb22b2febc8ee045657a85975cfabba81d65315
Hongchan Choi <hongchan@chromium.org>
2017-11-17 05:01:15 +0000

    Implements AudioWorkletProcessorState in AudioWorkletNode
    Spec: https://webaudio.github.io/web-audio-api/#idl-def-audioworkletprocessorstate
    
    This CL has 2 development steps:
    
    1. Implements AudioWorkletProcessorState in AudioWorkletNode
      - This change is relatively simple, and will cover two processor state
        changes: "pending", "running".
    2. Implements the communication channel from AudioWorkletGlobalScope to
       the main execution context and an AudioWorkletNode instance.
      - This change is slightly more involved. AWGS needs a reference to the
        parent frame task runner, so it can fire the processor state change
        event like "stopped" and "error".
    
    Bug: 784526
    Test: LayoutTests/http/tests/webaudio/audio-worklet/audio-worklet-processor-state.html
    Change-Id: If2dcae7c6dd8308d1905e7b8ae9cd4736b871dc6
    Reviewed-on: https://chromium-review.googlesource.com/769330
    Commit-Queue: Hongchan Choi <hongchan@chromium.org>
    Reviewed-by: Raymond Toy <rtoy@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517307}

---
+++ third_party/WebKit/LayoutTests/http/tests/webaudio/audio-worklet/state-processor.js
@@ -0,0 +1,55 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+/**
+ * @class TimedProcessor
+ * @extends AudioWorkletProcessor
+ *
+ * This processor class is for the life cycle and the processor state event.
+ * It only lives for 1 render quantum.
+ */
+class TimedProcessor extends AudioWorkletProcessor {
+  constructor() {
+    super();
+    this.createdAt_ = currentTime;
+    this.lifetime_ = 128 / sampleRate;
+  }
+
+  process() {
+    return currentTime - this.createdAt_ > this.lifetime_ ? false : true;
+  }
+}
+
+
+/**
+ * @class ConstructorErrorProcessor
+ * @extends AudioWorkletProcessor
+ */
+class ConstructorErrorProcessor extends AudioWorkletProcessor {
+  constructor() {
+    throw 'ConstructorErrorProcessor: an error thrown from constructor.';
+  }
+
+  process() {
+    return true;
+  }
+}
+
+
+/**
+ * @class ProcessErrorProcessor
+ * @extends AudioWorkletProcessor
+ */
+class ProcessErrorProcessor extends AudioWorkletProcessor {
+  constructor() {
+    super();
+  }
+
+  process() {
+    throw 'ProcessErrorProcessor: an error throw from process method.';
+    return true;
+  }
+}
+
+
+registerProcessor('timed', TimedProcessor);
+registerProcessor('constructor-error', ConstructorErrorProcessor);
+registerProcessor('process-error', ProcessErrorProcessor);
--- third_party/WebKit/Source/modules/webaudio/AudioWorkletNode.h
+++ third_party/WebKit/Source/modules/webaudio/AudioWorkletNode.h
@@ -20,6 +20,13 @@ class BaseAudioContext;
 class CrossThreadAudioParamInfo;
 class ExceptionState;
 
+enum class AudioWorkletProcessorState {
+  kPending,
+  kRunning,
+  kStopped,
+  kError,
+};
+
 // AudioWorkletNode is a user-facing interface of custom audio processor in
 // Web Audio API. The integration of WebAudio renderer is done via
 // AudioWorkletHandler and the actual audio processing runs on
@@ -50,12 +57,15 @@ class AudioWorkletHandler final : public AudioHandler {
 
   String Name() const { return name_; }
 
-  // Sets |AudioWorkletProcessor|. MUST be called on render thread.
+  // Sets |AudioWorkletProcessor| and changes the state of the processor.
+  // MUST be called from the render thread.
   void SetProcessorOnRenderThread(AudioWorkletProcessor*);
 
   // Finish |AudioWorkletProcessor| and set the tail time to zero, when
   // the user-supplied |process()| method returns false.
   void FinishProcessorOnRenderThread();
+
+  void NotifyProcessorStateChange(AudioWorkletProcessorState);
 
  private:
   AudioWorkletHandler(
@@ -74,6 +84,9 @@ class AudioWorkletHandler final : public AudioHandler {
 
   HashMap<String, scoped_refptr<AudioParamHandler>> param_handler_map_;
   HashMap<String, std::unique_ptr<AudioFloatArray>> param_value_map_;
+
+  // A reference to the main thread task runner.
+  scoped_refptr<WebTaskRunner> task_runner_;
 };
 
 class AudioWorkletNode final : public AudioNode,
@@ -92,8 +105,12 @@ class AudioWorkletNode final : public AudioNode,
   // ActiveScriptWrappable
   bool HasPendingActivity() const final;
 
+  void SetProcessorState(AudioWorkletProcessorState);
+
   // IDL
   AudioParamMap* parameters() const;
+  String processorState() const;
+  DEFINE_ATTRIBUTE_EVENT_LISTENER(processorstatechange);
 
   virtual void Trace(blink::Visitor*);
 
@@ -104,6 +121,7 @@ class AudioWorkletNode final : public AudioNode,
                    const Vector<CrossThreadAudioParamInfo>);
 
   Member<AudioParamMap> parameter_map_;
+  AudioWorkletProcessorState processor_state_;
 };
 
 }  // namespace blink
