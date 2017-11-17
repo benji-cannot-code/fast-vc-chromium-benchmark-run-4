e08e0e572f08cfb2e32b9cb3bf28f042ad5964d3
Rune Lillesveen <futhark@chromium.org>
2017-11-17 16:53:05 +0000

    Removed unnecessary include of HTMLOptGroupElement.h.
    Change-Id: Ie5be4c9508d63fb0a58180942bdee3a8cbb3ef2b
    Reviewed-on: https://chromium-review.googlesource.com/776878
    Reviewed-by: Daniel Bratell <bratell@opera.com>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517430}

--- third_party/WebKit/Source/core/dom/NodeComputedStyle.h
+++ third_party/WebKit/Source/core/dom/NodeComputedStyle.h
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/LayoutTreeBuilderTraversal.h"
 #include "core/dom/Node.h"
 #include "core/dom/V0InsertionPoint.h"
-#include "core/html/forms/HTMLOptGroupElement.h"
 #include "core/layout/LayoutObject.h"
 
 namespace blink {
