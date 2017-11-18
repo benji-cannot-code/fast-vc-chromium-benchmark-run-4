9523442bfd256e83013ffac5e230fad9817f1942
Ian Vollick <vollick@chromium.org>
2017-11-18 00:02:16 +0000

    [vr] Avoid all caps in audio permission prompt strings
    With this CL, these resources are no longer specified in all caps.
    Instead, they are converted to all caps before rendering.
    
    Bug: 779669
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Iac4c83813a649949aa130677b23b06be259a9685
    Reviewed-on: https://chromium-review.googlesource.com/777861
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Reviewed-by: Amirhossein Simjour <asimjour@chromium.org>
    Reviewed-by: Biao She <bshe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517623}

--- chrome/browser/vr/elements/audio_permission_prompt_texture.cc
+++ chrome/browser/vr/elements/audio_permission_prompt_texture.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/vr/elements/audio_permission_prompt_texture.h"
 
+#include "base/i18n/case_conversion.h"
 #include "cc/paint/skia_paint_canvas.h"
 #include "chrome/browser/vr/color_scheme.h"
 #include "chrome/browser/vr/elements/vector_icon.h"
@@ -91,8 +92,8 @@ void AudioPermissionPromptTexture::Draw(SkCanvas* sk_canvas,
   GetFontList(ToPixels(kFontSizePromptButtonText), text, &fonts);
 
   // Secondary button area.
-  text = l10n_util::GetStringUTF16(
-      IDS_VR_SHELL_AUDIO_PERMISSION_PROMPT_ABORT_BUTTON);
+  text = base::i18n::ToUpper(l10n_util::GetStringUTF16(
+      IDS_VR_SHELL_AUDIO_PERMISSION_PROMPT_ABORT_BUTTON));
   lines = PrepareDrawStringRect(
       text, fonts,
       color_scheme().audio_permission_prompt_secondary_button_forground,
@@ -115,8 +116,8 @@ void AudioPermissionPromptTexture::Draw(SkCanvas* sk_canvas,
   canvas->Restore();
 
   // Primary button area.
-  text = l10n_util::GetStringUTF16(
-      IDS_VR_SHELL_AUDIO_PERMISSION_PROMPT_CONTINUE_BUTTON);
+  text = base::i18n::ToUpper(l10n_util::GetStringUTF16(
+      IDS_VR_SHELL_AUDIO_PERMISSION_PROMPT_CONTINUE_BUTTON));
   button_text_size.set_size(gfx::Size(ToPixels(kButtonWidth), 0));
   lines = PrepareDrawStringRect(
       text, fonts, color_scheme().audio_permission_prompt_background,
