8935c5aef1df443ade36cdcf3cb10878f92bf946
kylechar <kylechar@chromium.org>
2017-11-18 00:44:00 +0000

    viz: Add IPC to set authoritative vsync inteval.
    This call originates in the browser process and is passed back to a
    SyntheticBeginFrameSource in the GPU process. This is pretty simple
    plumbing.
    
    The change requires RootCompositorFrameSinkImpl knowing it has a
    SyntheticBeginFrameSource. Wrap up the results of DisplayProvider into a
    struct so that we can handle the ExternalBeginFrameSource case too
    without adding more output parameters.
    
    Bug: 772524
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I9af43d4278475d07d19aefe7a03b6ee2426b536d
    Reviewed-on: https://chromium-review.googlesource.com/767352
    Commit-Queue: kylechar <kylechar@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517637}

--- components/viz/service/frame_sinks/root_compositor_frame_sink_impl.cc
+++ components/viz/service/frame_sinks/root_compositor_frame_sink_impl.cc
@@ -17,7 +17,7 @@ RootCompositorFrameSinkImpl::RootCompositorFrameSinkImpl(
     FrameSinkManagerImpl* frame_sink_manager,
     const FrameSinkId& frame_sink_id,
     std::unique_ptr<Display> display,
-    std::unique_ptr<BeginFrameSource> begin_frame_source,
+    std::unique_ptr<SyntheticBeginFrameSource> synthetic_begin_frame_source,
     mojom::CompositorFrameSinkAssociatedRequest request,
     mojom::CompositorFrameSinkClientPtr client,
     mojom::DisplayPrivateAssociatedRequest display_private_request)
@@ -30,23 +30,23 @@ RootCompositorFrameSinkImpl::RootCompositorFrameSinkImpl(
           frame_sink_id,
           true /* is_root */,
           true /* needs_sync_points */)),
-      display_begin_frame_source_(std::move(begin_frame_source)),
+      synthetic_begin_frame_source_(std::move(synthetic_begin_frame_source)),
       display_(std::move(display)),
       hit_test_aggregator_(frame_sink_manager->hit_test_manager(), this) {
-  DCHECK(display_begin_frame_source_);
+  DCHECK(synthetic_begin_frame_source_);
   DCHECK(display_);
 
   compositor_frame_sink_binding_.set_connection_error_handler(
       base::Bind(&RootCompositorFrameSinkImpl::OnClientConnectionLost,
                  base::Unretained(this)));
   frame_sink_manager->RegisterBeginFrameSource(
-      display_begin_frame_source_.get(), frame_sink_id);
+      synthetic_begin_frame_source_.get(), frame_sink_id);
   display_->Initialize(this, frame_sink_manager->surface_manager());
 }
 
 RootCompositorFrameSinkImpl::~RootCompositorFrameSinkImpl() {
   support_->frame_sink_manager()->UnregisterBeginFrameSource(
-      display_begin_frame_source_.get());
+      synthetic_begin_frame_source_.get());
 }
 
 void RootCompositorFrameSinkImpl::SetDisplayVisible(bool visible) {
@@ -63,6 +63,12 @@ void RootCompositorFrameSinkImpl::SetOutputIsSecure(bool secure) {
   display_->SetOutputIsSecure(secure);
 }
 
+void RootCompositorFrameSinkImpl::SetAuthoritativeVSyncInterval(
+    base::TimeDelta interval) {
+  if (synthetic_begin_frame_source_)
+    synthetic_begin_frame_source_->SetAuthoritativeVSyncInterval(interval);
+}
+
 void RootCompositorFrameSinkImpl::SetNeedsBeginFrame(bool needs_begin_frame) {
   support_->SetNeedsBeginFrame(needs_begin_frame);
 }
@@ -72,7 +78,7 @@ void RootCompositorFrameSinkImpl::SubmitCompositorFrame(
     CompositorFrame frame,
     mojom::HitTestRegionListPtr hit_test_region_list,
     uint64_t submit_time) {
-  // Update |display_| when size or local surface id changes.
+  // Update display when size or local surface id changes.
   if (support_->local_surface_id() != local_surface_id) {
     display_->Resize(frame.size_in_pixels());
     display_->SetLocalSurfaceId(local_surface_id, frame.device_scale_factor());
