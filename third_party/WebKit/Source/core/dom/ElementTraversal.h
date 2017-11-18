96c93f71002ba991d3adcd796e460b41aa48038e
Florin Malita <fmalita@chromium.org>
2017-11-18 14:16:46 +0000

    Fix TraversalChildrenIterator initialization
    TraversalChildrenIterator starts off with TraversalNext::FirstWithin().
    But FirstWithin() returns the first matching *descendant* -- which is
    incorrect for typed iterators.
    
    Use FirstChild() explicitly instead, to limit the iterator to children.
    
    Change-Id: I1dd78f6cc4e508f7d46e2fd0493249952acbb07f
    Reviewed-on: https://chromium-review.googlesource.com/775867
    Commit-Queue: Florin Malita <fmalita@chromium.org>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#517714}

--- third_party/WebKit/Source/core/dom/ElementTraversal.h
+++ third_party/WebKit/Source/core/dom/ElementTraversal.h
@@ -116,7 +116,7 @@ class Traversal {
   }
 
   // First or last ElementType descendant of the node.
-  // For Elements firstWithin() is always the same as firstChild().
+  // For pure Elements firstWithin() is always the same as firstChild().
   static ElementType* FirstWithin(const ContainerNode& current) {
     return FirstWithinTemplate(current);
   }
