b3032d5cd343a56a386bb19952cc15e16c39b9bc
Sadrul Habib Chowdhury <sadrul@chromium.org>
2017-11-18 06:15:36 +0000

    mus/viz: Renderer needs to know the real viz host.
    Renderer needs to talk to the browser if browser is hosting viz, even
    when running with mus. So make sure renderer talks to the right process
    for viz-related communication.
    
    BUG=786453
    
    Change-Id: I3561d9f1d8ae14bc35a34125fd55df773afeeca1
    Reviewed-on: https://chromium-review.googlesource.com/777439
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Fady Samuel <fsamuel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517704}

--- content/renderer/render_thread_impl.cc
+++ content/renderer/render_thread_impl.cc
@@ -334,6 +334,16 @@ void AddHistogramSample(void* hist, int sample) {
   histogram->Add(sample);
 }
 
+bool IsMusHostingViz() {
+#if BUILDFLAG(ENABLE_MUS)
+  const auto* cmdline = base::CommandLine::ForCurrentProcess();
+  return cmdline->GetSwitchValueASCII(switches::kMus) ==
+         switches::kMusHostVizValue;
+#else
+  return false;
+#endif
+}
+
 class FrameFactoryImpl : public mojom::FrameFactory {
  public:
   explicit FrameFactoryImpl(const service_manager::BindSourceInfo& source_info)
@@ -691,7 +701,7 @@ void RenderThreadImpl::Init(
 
   gpu_ = ui::Gpu::Create(
       GetConnector(),
-      IsRunningWithMus() ? ui::mojom::kServiceName : mojom::kBrowserServiceName,
+      IsMusHostingViz() ? ui::mojom::kServiceName : mojom::kBrowserServiceName,
       GetIOTaskRunner());
 
   viz::mojom::SharedBitmapAllocationNotifierPtr
@@ -2044,7 +2054,7 @@ void RenderThreadImpl::RequestNewLayerTreeFrameSink(
   }
 
 #if defined(USE_AURA)
-  if (IsRunningWithMus()) {
+  if (IsMusHostingViz()) {
     if (!RendererWindowTreeClient::Get(routing_id)) {
       callback.Run(nullptr);
       return;
