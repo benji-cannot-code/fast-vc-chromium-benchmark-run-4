3f3e8179e4c9894ebc99b6f1240ff3c771c5c6e9
Charles Harrison <csharrison@chromium.org>
2017-11-20 15:39:54 +0000

    Adjust Navigation metrics to account for synchronous time in resource handlers
    This CL slightly alters the timestamp in Navigation.TimeToURLJobStart to include
    synchronous time spent before (potentially) starting the request. This change
    makes the metric more precisely capture the time until the request reached the
    network stack.
    
    Bug: 779914
    Change-Id: I0eaf1fe7380cb6a14a2fbe328f431e35bb82efa3
    Reviewed-on: https://chromium-review.googlesource.com/774938
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517829}

--- content/browser/loader/navigation_url_loader_impl_core.h
+++ content/browser/loader/navigation_url_loader_impl_core.h
@@ -96,6 +96,8 @@ class NavigationURLLoaderImplCore
   base::WeakPtr<NavigationURLLoaderImpl> loader_;
   NavigationResourceHandler* resource_handler_;
 
+  base::WeakPtrFactory<NavigationURLLoaderImplCore> weak_factory_;
+
   DISALLOW_COPY_AND_ASSIGN(NavigationURLLoaderImplCore);
 };
 
