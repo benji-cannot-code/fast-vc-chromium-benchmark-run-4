692aad66fa40410eb6f13003e93911e54a616b01
Brian Sheedy <bsheedy@chromium.org>
2017-11-20 19:32:28 +0000

    Revert "Use the LegacyLinker instead of the ModernLinker"
    This reverts commit fdb3188391eac0cee74a762e7bdbb4dbad315c30.
    
    Reason for revert: Causing VR segfaults https://logs.chromium.org/v/?s=chromium%2Fbb%2Fchromium.fyi%2FAndroid_VR_Tests%2F13834%2F%2B%2Frecipes%2Fsteps%2Fstack_tool_with_logcat_dump%2F0%2Fstdout
    
    Original change's description:
    > Use the LegacyLinker instead of the ModernLinker
    > 
    > Remove the ModernLinker since it becomes unused. Also remove the possibility to
    > set the linker differently in tests.
    > 
    > Public document explaining the motivation: https://goo.gl/kWnHYr
    > 
    > BUG=719977 (google-internal)
    > 
    > Change-Id: I5b9b85817093b36127fa4a607bced57b4350a5b8
    > Reviewed-on: https://chromium-review.googlesource.com/538582
    > Commit-Queue: Egor Pasko <pasko@chromium.org>
    > Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    > Reviewed-by: Richard Coles <torne@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#517838}
    
    TBR=pasko@chromium.org,yfriedman@chromium.org,torne@chromium.org
    
    Change-Id: I638bd3223567a5a40babd036feca310257deaa6f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 719977 (google-internal)
    Reviewed-on: https://chromium-review.googlesource.com/779921
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517889}

--- base/android/linker/linker_jni.cc
+++ base/android/linker/linker_jni.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string.h>
 
 #include "legacy_linker_jni.h"
+#include "modern_linker_jni.h"
 
 namespace chromium_android_linker {
 
@@ -224,7 +225,8 @@ jint JNI_OnLoad(JavaVM* vm, void* reserved) {
   }
 
   // Initialize linker base and implementations.
-  if (!LinkerJNIInit(vm, env) || !LegacyLinkerJNIInit(vm, env)) {
+  if (!LinkerJNIInit(vm, env)
+      || !LegacyLinkerJNIInit(vm, env) || !ModernLinkerJNIInit(vm, env)) {
     return -1;
   }
 
