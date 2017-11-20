248f1be27affc41a9e5d03b8d0535d9b01812f4d
Mounir Lamouri <mlamouri@chromium.org>
2017-11-20 16:11:33 +0000

    Cast Overlay: show the overlay if the video element is interacted with.
    The overlay should show when the video is tapped or interacted with.
    
    Bug: 785322
    Change-Id: I9ac249005cf08723c35fc6885070c7ec90234aae
    Reviewed-on: https://chromium-review.googlesource.com/774459
    Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517836}

--- third_party/WebKit/Source/modules/media_controls/elements/MediaControlOverlayEnclosureElement.h
+++ third_party/WebKit/Source/modules/media_controls/elements/MediaControlOverlayEnclosureElement.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class Event;
-class EventDispatchHandlingState;
 class MediaControlsImpl;
 
 class MediaControlOverlayEnclosureElement final
@@ -19,8 +18,7 @@ class MediaControlOverlayEnclosureElement final
  public:
   explicit MediaControlOverlayEnclosureElement(MediaControlsImpl&);
 
- private:
-  EventDispatchHandlingState* PreDispatchEventHandler(Event*) override;
+  void DefaultEventHandler(Event*) override;
 };
 
 }  // namespace blink
