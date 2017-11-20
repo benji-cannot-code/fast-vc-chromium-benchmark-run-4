eed582500aadafbe4f7bf11210061496bcb00c7b
Jayson Adams <shrike@chromium.org>
2017-11-20 16:32:44 +0000

    [Mac] Fix regression with incognito tab close button.
    https://chromium-review.googlesource.com/769238 cleans up the TabView
    initialization code but the reordering causes the TabView to never
    receive a pointer to its closeButton. This cl ensures closeButton
    creation occurs before TabView creation.
    
    Bug: 786326, 786387
    Change-Id: Id1fce1b8dcd1014ae015b7806df3541981d08253
    Reviewed-on: https://chromium-review.googlesource.com/777394
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Jayson Adams <shrike@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517843}

--- chrome/browser/ui/cocoa/tabs/tab_controller.mm
+++ chrome/browser/ui/cocoa/tabs/tab_controller.mm
@@ -134,7 +134,23 @@ static const CGFloat kPinnedTabWidth = kDefaultTabHeight * 2;
 
 - (id)init {
   if ((self = [super init])) {
-    // Create the TabView.
+    BOOL isRTL = cocoa_l10n_util::ShouldDoExperimentalRTLLayout();
+
+    // Create the close button.
+    const CGFloat closeButtonXOrigin =
+        isRTL ? kTabTrailingPadding
+              : kInitialTabWidth - kCloseButtonSize - kTabTrailingPadding;
+    NSRect closeButtonFrame = NSMakeRect(closeButtonXOrigin, kTabElementYOrigin,
+                                         kCloseButtonSize, kCloseButtonSize);
+    closeButton_.reset(
+        [[HoverCloseButton alloc] initWithFrame:closeButtonFrame]);
+    [closeButton_
+        setAutoresizingMask:isRTL ? NSViewMaxXMargin : NSViewMinXMargin];
+    [closeButton_ setTarget:self];
+    [closeButton_ setAction:@selector(closeTab:)];
+
+    // Create the TabView. The TabView works directly with the closeButton so
+    // here (the TabView handles adding it as a subview).
     base::scoped_nsobject<TabView> tabView([[TabView alloc]
         initWithFrame:NSMakeRect(0, 0, kInitialTabWidth,
                                  [TabController defaultTabHeight])
@@ -145,8 +161,6 @@ static const CGFloat kPinnedTabWidth = kDefaultTabHeight * 2;
     [tabView setPostsBoundsChangedNotifications:NO];
     [super setView:tabView];
 
-    BOOL isRTL = cocoa_l10n_util::ShouldDoExperimentalRTLLayout();
-
     // Add the favicon view.
     NSRect iconViewFrame =
         NSMakeRect(0, kTabElementYOrigin, gfx::kFaviconSize, gfx::kFaviconSize);
@@ -155,6 +169,7 @@ static const CGFloat kPinnedTabWidth = kDefaultTabHeight * 2;
                                          : NSViewMaxXMargin | NSViewMinYMargin];
     [self updateIconViewFrameWithAnimation:NO];
     [tabView addSubview:iconView_];
+    isIconShowing_ = YES;
 
     // Set up the title.
     const CGFloat titleXOrigin =
@@ -165,22 +180,6 @@ static const CGFloat kPinnedTabWidth = kDefaultTabHeight * 2;
                                    kInitialTitleWidth, kTitleHeight);
     [tabView setTitleFrame:titleFrame];
 
-    // Add the close button.
-    const CGFloat closeButtonXOrigin =
-        isRTL ? kTabTrailingPadding
-              : kInitialTabWidth - kCloseButtonSize - kTabTrailingPadding;
-    NSRect closeButtonFrame = NSMakeRect(closeButtonXOrigin, kTabElementYOrigin,
-                                         kCloseButtonSize, kCloseButtonSize);
-    closeButton_.reset([[HoverCloseButton alloc] initWithFrame:
-        closeButtonFrame]);
-    [closeButton_
-        setAutoresizingMask:isRTL ? NSViewMaxXMargin : NSViewMinXMargin];
-    [closeButton_ setTarget:self];
-    [closeButton_ setAction:@selector(closeTab:)];
-
-    [tabView addSubview:closeButton_];
-
-    isIconShowing_ = YES;
     NSNotificationCenter* defaultCenter = [NSNotificationCenter defaultCenter];
     [defaultCenter addObserver:self
                       selector:@selector(themeChangedNotification:)
