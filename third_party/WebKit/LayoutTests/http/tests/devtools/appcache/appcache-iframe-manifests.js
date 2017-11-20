0ea2c0d0a7e0fc4e2ff926395df3908e3dd43bb7
Eugene Ostroukhov <eostroukhov@chromium.org>
2017-11-20 23:25:38 +0000

    DevTools: Migrate appcache tests
    This converts the appcache test from the old to the new test framework
    primarily through an automatic test conversion script.
    
    Bug: 667560
    Change-Id: If6295681bb07e68dc1bc50e33579f266a9ba3f41
    Reviewed-on: https://chromium-review.googlesource.com/780200
    Commit-Queue: Eugene Ostroukhov <eostroukhov@chromium.org>
    Reviewed-by: Will Chen <chenwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517982}

--- third_party/WebKit/LayoutTests/http/tests/devtools/appcache/appcache-iframe-manifests.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/appcache/appcache-iframe-manifests.js
@@ -1,11 +1,18 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-<html>
-<head>
-<script src="../../inspector/inspector-test.js"></script>
-<script src="../../inspector/resources-test.js"></script>
-<script src="../../inspector/appcache/appcache-test.js"></script>
-<script>
-function test() {
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(async function() {
+  TestRunner.addResult(
+      `Tests that application cache model keeps track of manifest urls and statuses correctly. https://bugs.webkit.org/show_bug.cgi?id=64581\n`);
+  await TestRunner.loadModule('application_test_runner');
+  await TestRunner.showPanel('resources');
+  await TestRunner.loadHTML(`
+      <p>Tests that application cache model keeps track of manifest urls and statuses correctly.</p>
+      <a href="https://bugs.webkit.org/show_bug.cgi?id=64581">Bug 64581</a>
+    `);
+
   var frameId1;
   var frameId2;
   var frameId3;
@@ -88,11 +95,4 @@ function test() {
     ApplicationTestRunner.dumpApplicationCache();
     TestRunner.completeTest();
   }
-}
-</script>
-</head>
-<body onload="runTest()">
-<p>Tests that application cache model keeps track of manifest urls and statuses correctly.</p>
-<a href="https://bugs.webkit.org/show_bug.cgi?id=64581">Bug 64581</a>
-</body>
-</html>
+})();
