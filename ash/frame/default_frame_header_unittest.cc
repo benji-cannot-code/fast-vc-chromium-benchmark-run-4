c24329c3c9236ea8f608f752d5e97e173f85dbf4
Christopher Lam <calamity@chromium.org>
2017-11-20 08:23:39 +0000

    [desktop-pwas] Render 'App name by example.com' string in window title.
    This CL adds an extra string to the Desktop PWA window title that
    denotes which app the window belongs to and the domain of the start URL.
    The window title is painted such that the page title will elide first,
    followed by the app name then the domain. Further work is required to
    maintain this property for all languages.
    
    Bug: 762401
    Change-Id: Ibcc697b02fa643df9c2618ca0ef0249c6f887b64
    Reviewed-on: https://chromium-review.googlesource.com/768601
    Commit-Queue: calamity <calamity@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517782}

--- ash/frame/default_frame_header_unittest.cc
+++ ash/frame/default_frame_header_unittest.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/widget/widget.h"
 #include "ui/views/window/non_client_view.h"
 
-using ash::FrameHeader;
 using views::NonClientFrameView;
 using views::Widget;
 
@@ -27,18 +26,17 @@ using DefaultFrameHeaderTest = AshTestBase;
 TEST_F(DefaultFrameHeaderTest, TitleIconAlignment) {
   std::unique_ptr<Widget> w = CreateTestWidget(
       nullptr, kShellWindowId_DefaultContainer, gfx::Rect(1, 2, 3, 4));
-  ash::FrameCaptionButtonContainerView container(w.get());
+  FrameCaptionButtonContainerView container(w.get());
   views::StaticSizedView window_icon(gfx::Size(16, 16));
   window_icon.SetBounds(0, 0, 16, 16);
   w->SetBounds(gfx::Rect(0, 0, 500, 500));
   w->Show();
 
-  DefaultFrameHeader frame_header;
-  frame_header.Init(w.get(), w->non_client_view()->frame_view(), &container,
-                    nullptr);
+  DefaultFrameHeader frame_header(w.get(), w->non_client_view()->frame_view(),
+                                  &container, nullptr);
   frame_header.UpdateLeftHeaderView(&window_icon);
   frame_header.LayoutHeader();
-  gfx::Rect title_bounds = frame_header.GetTitleBounds();
+  gfx::Rect title_bounds = frame_header.GetAvailableTitleBounds();
   EXPECT_EQ(window_icon.bounds().CenterPoint().y(),
             title_bounds.CenterPoint().y());
 }
@@ -46,15 +44,14 @@ TEST_F(DefaultFrameHeaderTest, TitleIconAlignment) {
 TEST_F(DefaultFrameHeaderTest, BackButtonAlignment) {
   std::unique_ptr<Widget> w = CreateTestWidget(
       nullptr, kShellWindowId_DefaultContainer, gfx::Rect(1, 2, 3, 4));
-  ash::FrameCaptionButtonContainerView container(w.get());
-  ash::FrameBackButton back;
+  FrameCaptionButtonContainerView container(w.get());
+  FrameBackButton back;
 
-  DefaultFrameHeader frame_header;
-  frame_header.Init(w.get(), w->non_client_view()->frame_view(), &container,
-                    nullptr);
+  DefaultFrameHeader frame_header(w.get(), w->non_client_view()->frame_view(),
+                                  &container, nullptr);
   frame_header.UpdateBackButton(&back);
   frame_header.LayoutHeader();
-  gfx::Rect title_bounds = frame_header.GetTitleBounds();
+  gfx::Rect title_bounds = frame_header.GetAvailableTitleBounds();
   // The back button should be positioned at the left edge, and
   // vertically centered.
   EXPECT_EQ(back.bounds().CenterPoint().y(), title_bounds.CenterPoint().y());
@@ -65,15 +62,14 @@ TEST_F(DefaultFrameHeaderTest, BackButtonAlignment) {
 TEST_F(DefaultFrameHeaderTest, LightIcons) {
   std::unique_ptr<Widget> w = CreateTestWidget(
       nullptr, kShellWindowId_DefaultContainer, gfx::Rect(1, 2, 3, 4));
-  ash::FrameCaptionButtonContainerView container(w.get());
+  FrameCaptionButtonContainerView container(w.get());
   views::StaticSizedView window_icon(gfx::Size(16, 16));
   window_icon.SetBounds(0, 0, 16, 16);
   w->SetBounds(gfx::Rect(0, 0, 500, 500));
   w->Show();
 
-  DefaultFrameHeader frame_header;
-  frame_header.Init(w.get(), w->non_client_view()->frame_view(), &container,
-                    nullptr);
+  DefaultFrameHeader frame_header(w.get(), w->non_client_view()->frame_view(),
+                                  &container, nullptr);
 
   // Check by default light icons are not used.
   frame_header.mode_ = FrameHeader::MODE_ACTIVE;
--- chrome/browser/ui/extensions/hosted_app_browser_controller.cc
+++ chrome/browser/ui/extensions/hosted_app_browser_controller.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/common/extension.h"
+#include "net/base/registry_controlled_domains/registry_controlled_domain.h"
 #include "ui/gfx/favicon_size.h"
 #include "ui/gfx/image/image_skia.h"
 #include "url/gurl.h"
@@ -79,9 +80,7 @@ HostedAppBrowserController::HostedAppBrowserController(Browser* browser)
 HostedAppBrowserController::~HostedAppBrowserController() {}
 
 bool HostedAppBrowserController::ShouldShowLocationBar() const {
-  const Extension* extension =
-      ExtensionRegistry::Get(browser_->profile())->GetExtensionById(
-          extension_id_, ExtensionRegistry::EVERYTHING);
+  const Extension* extension = GetExtension();
 
   const content::WebContents* web_contents =
       browser_->tab_strip_model()->GetActiveWebContents();
@@ -155,11 +154,29 @@ base::Optional<SkColor> HostedAppBrowserController::GetThemeColor() const {
 }
 
 base::string16 HostedAppBrowserController::GetTitle() const {
-  content::NavigationEntry* entry = browser_->tab_strip_model()
-                                        ->GetActiveWebContents()
-                                        ->GetController()
-                                        .GetVisibleEntry();
+  content::WebContents* web_contents =
+      browser_->tab_strip_model()->GetActiveWebContents();
+  if (!web_contents)
+    return base::string16();
+
+  content::NavigationEntry* entry =
+      web_contents->GetController().GetVisibleEntry();
   return entry ? entry->GetTitle() : base::string16();
+}
+
+const Extension* HostedAppBrowserController::GetExtension() const {
+  return ExtensionRegistry::Get(browser_->profile())
+      ->GetExtensionById(extension_id_, ExtensionRegistry::EVERYTHING);
+}
+
+std::string HostedAppBrowserController::GetAppShortName() const {
+  return GetExtension()->short_name();
+}
+
+std::string HostedAppBrowserController::GetDomainAndRegistry() const {
+  return net::registry_controlled_domains::GetDomainAndRegistry(
+      AppLaunchInfo::GetLaunchWebURL(GetExtension()),
+      net::registry_controlled_domains::INCLUDE_PRIVATE_REGISTRIES);
 }
 
 }  // namespace extensions
