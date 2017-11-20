6857572e2745489c7c98575d787722d71dac85a1
Maajid <maajid@chromium.org>
2017-11-20 06:22:25 +0000

    Make AddKeyEx pass-through.
    AddKeyEx now takes AuthorizationRequest and AddKeyRequest directly.
    
    Bug: 741274
    Change-Id: I200b7bd8255010b51376d15bcff3e190b651fb13
    Reviewed-on: https://chromium-review.googlesource.com/776518
    Commit-Queue: Maajid <maajid@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517762}

--- chromeos/login/auth/extended_authenticator_impl.h
+++ chromeos/login/auth/extended_authenticator_impl.h
@@ -40,7 +40,7 @@ class CHROMEOS_EXPORT ExtendedAuthenticatorImpl : public ExtendedAuthenticator {
                    const ResultCallback& success_callback) override;
   void AddKey(const UserContext& context,
               const cryptohome::KeyDefinition& key,
-              bool replace_existing,
+              bool clobber_if_exists,
               const base::Closure& success_callback) override;
   void UpdateKeyAuthorized(const UserContext& context,
                            const cryptohome::KeyDefinition& key,
@@ -64,7 +64,7 @@ class CHROMEOS_EXPORT ExtendedAuthenticatorImpl : public ExtendedAuthenticator {
   void DoAuthenticateToCheck(const base::Closure& success_callback,
                              const UserContext& context);
   void DoAddKey(const cryptohome::KeyDefinition& key,
-                bool replace_existing,
+                bool clobber_if_exists,
                 const base::Closure& success_callback,
                 const UserContext& context);
   void DoUpdateKeyAuthorized(const cryptohome::KeyDefinition& key,
