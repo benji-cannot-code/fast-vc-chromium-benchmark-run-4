27e74e601dc77b9c36d1d506865d18c6edc37884
Bugs Nash <bugsnash@chromium.org>
2017-11-20 03:01:24 +0000

    Split grouped property classes into single property classes
    This is preparation for moving generated style builder methods to
    property class header files for properties that use the
    'border-image-modifier' template. These style buider methods are unique
    to each property and therefore require separate property classes.
    
    Created new property classes for the following previously grouped
    properties:
    - WebkitMaskBoxImageWidth
    - WebkitMaskBoxImageSlice
    - WebkitMaskBoxImageRepeats
    - WebkitMaskBoxImageOutsets
    
    Bug: 751354
    Change-Id: Iaf2f782086b13b4f4d84a3e3f16890aa264badd2
    Reviewed-on: https://chromium-review.googlesource.com/775913
    Reviewed-by: nainar <nainar@chromium.org>
    Reviewed-by: meade_UTC10 <meade@chromium.org>
    Commit-Queue: meade_UTC10 <meade@chromium.org>
    Commit-Queue: Bugs Nash <bugsnash@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517748}

---
+++ third_party/WebKit/Source/core/css/properties/longhands/WebkitMaskBoxImageWidth.cpp
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/css/properties/longhands/WebkitMaskBoxImageWidth.h"
+
+#include "core/css/properties/CSSPropertyBorderImageUtils.h"
+
+namespace blink {
+namespace CSSLonghand {
+
+const CSSValue* WebkitMaskBoxImageWidth::ParseSingleValue(
+    CSSParserTokenRange& range,
+    const CSSParserContext&,
+    const CSSParserLocalContext&) const {
+  return CSSPropertyBorderImageUtils::ConsumeBorderImageWidth(range);
+}
+
+}  // namespace CSSLonghand
+}  // namespace blink
