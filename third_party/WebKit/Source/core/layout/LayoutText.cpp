850b95f310269954160b86f1b8316208ad442e6a
Xiaocheng Hu <xiaochengh@chromium.org>
2017-11-20 07:55:39 +0000

    Ensure clean layout before calling LayoutText::CaretMaxOffset
    LayoutText::CaretMaxOffset() requires clean layout, but currently can be
    called from DeleteSelectionCommand when layout is dirty. This patch adds
    layout update before the call sites to fix it.
    
    Bug: 781698
    Change-Id: I17b7596e93f025d61836e39cca4bf736713d3422
    Reviewed-on: https://chromium-review.googlesource.com/778050
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517778}

--- third_party/WebKit/Source/core/layout/LayoutText.cpp
+++ third_party/WebKit/Source/core/layout/LayoutText.cpp
@@ -1963,6 +1963,9 @@ Optional<unsigned> LayoutText::CaretOffsetForPosition(
 }
 
 int LayoutText::CaretMinOffset() const {
+  // TODO(crbug.com/781698): Add the following DCHECK.
+  // DCHECK(!GetDocument().NeedsLayoutTreeUpdate());
+
   if (auto* mapping = GetNGOffsetMapping()) {
     const Position first_position = PositionForCaretOffset(0);
     if (first_position.IsNull())
@@ -1985,6 +1988,9 @@ int LayoutText::CaretMinOffset() const {
 }
 
 int LayoutText::CaretMaxOffset() const {
+  // TODO(crbug.com/781698): Add the following DCHECK.
+  // DCHECK(!GetDocument().NeedsLayoutTreeUpdate());
+
   if (auto* mapping = GetNGOffsetMapping()) {
     const Position last_position = PositionForCaretOffset(TextLength());
     if (last_position.IsNull())
