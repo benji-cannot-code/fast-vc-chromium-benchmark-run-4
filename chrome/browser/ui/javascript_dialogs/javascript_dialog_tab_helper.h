526354f85a25d0d2f2d5516ca3c9d429b8f48f4a
Avi Drissman <avi@chromium.org>
2017-11-20 20:21:40 +0000

    Properly remove the "needs attention" indicator for purged tabs.
    If the resource coordinator purges a tab, the contents for that
    tab will be destroyed after it is removed from the tabstrip.
    Watch the tabstrip to make sure to handle that case.
    
    BUG=786178
    TEST=as in that case
    
    Change-Id: I3d2c2e9fab29fe639615eed330ca769243c9a8f7
    Reviewed-on: https://chromium-review.googlesource.com/777470
    Reviewed-by: Sidney San Martín <sdy@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517907}

--- chrome/browser/ui/javascript_dialogs/javascript_dialog_tab_helper.h
+++ chrome/browser/ui/javascript_dialogs/javascript_dialog_tab_helper.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if !defined(OS_ANDROID)
 #include "chrome/browser/ui/browser_list_observer.h"
+#include "chrome/browser/ui/tabs/tab_strip_model_observer.h"
 #endif
 
 // A class, attached to WebContentses in browser windows, that is the
@@ -44,6 +45,7 @@ class JavaScriptDialogTabHelper
       public content::WebContentsObserver,
 #if !defined(OS_ANDROID)
       public chrome::BrowserListObserver,
+      public TabStripModelObserver,
 #endif
       public content::WebContentsUserData<JavaScriptDialogTabHelper> {
  public:
@@ -82,6 +84,12 @@ class JavaScriptDialogTabHelper
 #if !defined(OS_ANDROID)
   // BrowserListObserver:
   void OnBrowserSetLastActive(Browser* browser) override;
+
+  // TabStripModelObserver:
+  void TabReplacedAt(TabStripModel* tab_strip_model,
+                     content::WebContents* old_contents,
+                     content::WebContents* new_contents,
+                     int index) override;
 #endif
 
  private:
@@ -100,8 +108,16 @@ class JavaScriptDialogTabHelper
                    bool success,
                    const base::string16& user_input);
 
-  // Marks the tab as needing attention.
+  // Marks the tab as needing attention. The WebContents must be in a browser
+  // window.
   void SetTabNeedsAttention(bool attention);
+
+#if !defined(OS_ANDROID)
+  // Marks the tab as needing attention.
+  void SetTabNeedsAttentionImpl(bool attention,
+                                TabStripModel* tab_strip_model,
+                                int index);
+#endif
 
   // There can be at most one dialog (pending or not) being shown at any given
   // time on a tab. Depending on the type of the dialog, the variables
