08a0047a92017227121028ebe36a0696263de87e
Joy Ming <jming@chromium.org>
2017-11-20 23:18:19 +0000

    In_progress_cache : unit test for conversions.
    Unit tests for conversions from/to proto for the in-progress cache.
    
    Bug: 778425
    Change-Id: I5d0ded9abc5cb2bfe80083d2c5a3f31c143ed649
    Reviewed-on: https://chromium-review.googlesource.com/776008
    Commit-Queue: Joy Ming <jming@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517977}

--- components/download/downloader/in_progress/in_progress_conversions.h
+++ components/download/downloader/in_progress/in_progress_conversions.h
@@ -25,9 +25,6 @@ class InProgressConversions {
 
   static metadata_pb::DownloadEntries DownloadEntriesToProto(
       const std::vector<DownloadEntry>& entries);
-
- private:
-  DISALLOW_IMPLICIT_CONSTRUCTORS(InProgressConversions);
 };
 
 }  // namespace download
---
+++ components/download/downloader/in_progress/in_progress_conversions_unittest.cc
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/download/downloader/in_progress/in_progress_conversions.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace download {
+
+class InProgressConversionsTest : public testing::Test,
+                                  public InProgressConversions {
+ public:
+  ~InProgressConversionsTest() override {}
+};
+
+TEST_F(InProgressConversionsTest, DownloadEntry) {
+  // Entry with no fields.
+  DownloadEntry entry;
+  EXPECT_EQ(entry, DownloadEntryFromProto(DownloadEntryToProto(entry)));
+
+  // Entry with guid and request origin.
+  entry.guid = "guid";
+  entry.request_origin = "request origin";
+  EXPECT_EQ(entry, DownloadEntryFromProto(DownloadEntryToProto(entry)));
+}
+
+TEST_F(InProgressConversionsTest, DownloadEntries) {
+  // Entries vector with no entries.
+  std::vector<DownloadEntry> entries;
+  EXPECT_EQ(entries, DownloadEntriesFromProto(DownloadEntriesToProto(entries)));
+
+  // Entries vector with one entry.
+  entries.push_back(DownloadEntry("guid", "request origin"));
+  EXPECT_EQ(entries, DownloadEntriesFromProto(DownloadEntriesToProto(entries)));
+
+  // Entries vector with multiple entries.
+  entries.push_back(DownloadEntry("guid2", "request origin"));
+  EXPECT_EQ(entries, DownloadEntriesFromProto(DownloadEntriesToProto(entries)));
+}
+
+}  // namespace download
