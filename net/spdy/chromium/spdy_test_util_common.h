9b333d542a43d8b9f21ce6b24727b531f4cb1d9a
Bence Béky <bnc@chromium.org>
2017-11-20 16:58:16 +0000

    spdy_test_util_common.{h,cc} cleanup.
    * Remove duplicate includes.
    * Include spdy_session.h for SpdySession::TimeFunc.
    * s/NULL/nullptr/
    * Ever since https://crrev.com/199633019 landed three and a half years
      ago, |expected_status| argument of CreateSpdySessionHelper() is
      unused.  Remove it, and remove the inaccurately named
      TryCreateSpdySessionExpectingFailure().
    * Move |this_header| and |this_value| declaration closest to their first
      use in AppendToHeaderBlock().  The compiler should be smart enough to
      optimize this.
    * In the same method use implicit pointer to bool conversion in DCHECKs.
    * Purge naked news in favor of MakeRefCounted<> or make_unique<>.
    * s/expected_error/expected_status/ for consistency.
    * Use TestCompletionCallback::GetResult() [1] in
      CreateSpdySessionHelper() instead of inlining it.
    
    [1] https://cs.chromium.org/chromium/src/net/base/test_completion_callback.h?l=72
    
    Bug: 
    Change-Id: Id5bc3f37f068f78901afbf8fb027cc6c63744b22
    Reviewed-on: https://chromium-review.googlesource.com/775868
    Commit-Queue: Bence Béky <bnc@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517847}

--- net/spdy/chromium/spdy_test_util_common.h
+++ net/spdy/chromium/spdy_test_util_common.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/proxy/proxy_server.h"
 #include "net/proxy/proxy_service.h"
 #include "net/socket/socket_test_util.h"
+#include "net/spdy/chromium/spdy_session.h"
 #include "net/spdy/core/spdy_protocol.h"
 #include "net/spdy/platform/api/spdy_string.h"
 #include "net/spdy/platform/api/spdy_string_piece.h"
@@ -47,7 +48,6 @@ class CTVerifier;
 class CTPolicyEnforcer;
 class HostPortPair;
 class NetLogWithSource;
-class SpdySession;
 class SpdySessionKey;
 class SpdySessionPool;
 class SpdyStream;
@@ -239,16 +239,6 @@ class SpdyURLRequestContext : public URLRequestContext {
 // NULL.
 bool HasSpdySession(SpdySessionPool* pool, const SpdySessionKey& key);
 
-// Tries to create a SPDY session for the given key but expects the
-// attempt to fail with the given error. A SPDY session for |key| must
-// not already exist. The session will be created but close in the
-// next event loop iteration.
-base::WeakPtr<SpdySession> TryCreateSpdySessionExpectingFailure(
-    HttpNetworkSession* http_session,
-    const SpdySessionKey& key,
-    Error expected_error,
-    const NetLogWithSource& net_log);
-
 // Creates a SPDY session for the given key and puts it in the SPDY
 // session pool in |http_session|. A SPDY session for |key| must not
 // already exist.
@@ -277,7 +267,7 @@ base::WeakPtr<SpdySession> CreateFakeSpdySession(SpdySessionPool* pool,
 base::WeakPtr<SpdySession> TryCreateFakeSpdySessionExpectingFailure(
     SpdySessionPool* pool,
     const SpdySessionKey& key,
-    Error expected_error);
+    Error expected_status);
 
 class SpdySessionPoolPeer {
  public:
