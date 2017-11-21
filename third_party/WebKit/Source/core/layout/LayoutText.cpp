f6bb53caa867660a0824a75bcbc41418228d20c0
Xiaocheng Hu <xiaochengh@chromium.org>
2017-11-21 04:53:16 +0000

    DCHECK clean layout in LayoutText::CaretMin/MaxOffset()
    As now we haven't found any caller of LayoutText::CaretMin/MaxOffset()
    with dirty layout, this patch adds DCHECK of clean layout into
    the two functions to enforce the clean layout requirement.
    
    Bug: 781698
    Change-Id: Id55463a71f309a58906ee65e1f89faa57f8708c6
    Reviewed-on: https://chromium-review.googlesource.com/777956
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518120}

--- third_party/WebKit/Source/core/layout/LayoutText.cpp
+++ third_party/WebKit/Source/core/layout/LayoutText.cpp
@@ -1963,8 +1963,7 @@ Optional<unsigned> LayoutText::CaretOffsetForPosition(
 }
 
 int LayoutText::CaretMinOffset() const {
-  // TODO(crbug.com/781698): Add the following DCHECK.
-  // DCHECK(!GetDocument().NeedsLayoutTreeUpdate());
+  DCHECK(!GetDocument().NeedsLayoutTreeUpdate());
 
   if (auto* mapping = GetNGOffsetMapping()) {
     const Position first_position = PositionForCaretOffset(0);
@@ -1988,8 +1987,7 @@ int LayoutText::CaretMinOffset() const {
 }
 
 int LayoutText::CaretMaxOffset() const {
-  // TODO(crbug.com/781698): Add the following DCHECK.
-  // DCHECK(!GetDocument().NeedsLayoutTreeUpdate());
+  DCHECK(!GetDocument().NeedsLayoutTreeUpdate());
 
   if (auto* mapping = GetNGOffsetMapping()) {
     const Position last_position = PositionForCaretOffset(TextLength());
