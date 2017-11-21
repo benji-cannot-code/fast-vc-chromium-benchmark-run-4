a7415ee4d380c8d3e2a9918aef3f4f932aac25a2
junweifu <junwei.fu@intel.com>
2017-11-21 03:32:21 +0000

    ShapeDetection: Create Face Detector Async on windows 10
    Create Face Detector with Face Detector factory that is async.
    Add a template class represents an asynchronous operation which
    returns a result upon completion.
    Expose PauseIncomingMethodCallProcessing method from StrongBinding
    to prevent FaceDetectionProviderWin from being destroyed before
    the AsyncOperation completes.
    Add unittest to test the result of creating face detector.
    
    Split original large CL[1] up in smaller subpatches including this CL.
    Link Face Detection demo[2] here.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/708336
    [2] https://codepen.io/miguelao/pen/PmJWro
    
    BUG=767021
    
    Cq-Include-Trybots: master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win10_chromium_x64_rel_ng
    Change-Id: Id5b19115f10d7c96da191d7421a626741b075266
    Reviewed-on: https://chromium-review.googlesource.com/750006
    Commit-Queue: Junwei Fu <junwei.fu@intel.com>
    Reviewed-by: Yuzhu Shen <yzshen@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518089}

--- services/shape_detection/face_detection_impl_win.h
+++ services/shape_detection/face_detection_impl_win.h
@@ -6,8 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SERVICES_SHAPE_DETECTION_FACE_DETECTION_IMPL_WIN_H_
 #define SERVICES_SHAPE_DETECTION_FACE_DETECTION_IMPL_WIN_H_
 
+#include <windows.foundation.h>
 #include <wrl/client.h>
 
+#include "detection_utils_win.h"
 #include "services/shape_detection/public/interfaces/facedetection.mojom.h"
 
 class SkBitmap;
@@ -16,7 +18,9 @@ namespace ABI {
 namespace Windows {
 namespace Media {
 namespace FaceAnalysis {
-struct IFaceDetectorStatics;
+interface IFaceDetectorStatics;
+interface IFaceDetector;
+class FaceDetector;
 }  // namespace FaceAnalysis
 }  // namespace Media
 }  // namespace Windows
@@ -24,22 +28,26 @@ struct IFaceDetectorStatics;
 
 namespace shape_detection {
 
+extern base::OnceCallback<void(bool)> g_callback_for_testing;
+
 class FaceDetectionImplWin : public mojom::FaceDetection {
  public:
   using IFaceDetectorStatics =
       ABI::Windows::Media::FaceAnalysis::IFaceDetectorStatics;
+  using FaceDetector = ABI::Windows::Media::FaceAnalysis::FaceDetector;
+  using IFaceDetector = ABI::Windows::Media::FaceAnalysis::IFaceDetector;
 
-  static std::unique_ptr<FaceDetectionImplWin> Create();
-
-  explicit FaceDetectionImplWin(
-      Microsoft::WRL::ComPtr<IFaceDetectorStatics> factory);
+  FaceDetectionImplWin(Microsoft::WRL::ComPtr<IFaceDetectorStatics> factory,
+                       Microsoft::WRL::ComPtr<IFaceDetector> face_detector);
   ~FaceDetectionImplWin() override;
 
+  // mojom::FaceDetection implementation.
   void Detect(const SkBitmap& bitmap,
               mojom::FaceDetection::DetectCallback callback) override;
 
  private:
   Microsoft::WRL::ComPtr<IFaceDetectorStatics> face_detector_factory_;
+  Microsoft::WRL::ComPtr<IFaceDetector> face_detector_;
 
   DISALLOW_COPY_AND_ASSIGN(FaceDetectionImplWin);
 };
