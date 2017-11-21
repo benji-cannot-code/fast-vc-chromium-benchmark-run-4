3ca31d52ee220ac560766e48930fbc29b5f519cf
Nate Fischer <ntfschr@chromium.org>
2017-11-21 04:02:36 +0000

    AW: UMA metrics for CanShowInterstitial
    This CL adds UMA metrics to track how often we show interstitials vs.
    immediately create a network error (because we believe the WebView to be
    not visible).
    
    Bug: 713724
    Test: check metrics are logged under chrome://histograms/SafeBrowsing
    Change-Id: I5882491289de69d49e2f011de58c134c52af2ae3
    Reviewed-on: https://chromium-review.googlesource.com/780360
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518101}

--- android_webview/browser/aw_safe_browsing_ui_manager.cc
+++ android_webview/browser/aw_safe_browsing_ui_manager.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "android_webview/browser/net/aw_url_request_context_getter.h"
 #include "android_webview/common/aw_paths.h"
 #include "base/command_line.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/path_service.h"
 #include "components/prefs/pref_service.h"
 #include "components/safe_browsing/base_ping_manager.h"
@@ -26,6 +27,11 @@ namespace {
 std::string GetProtocolConfigClientName() {
   // Return a webview specific client name, see crbug.com/732373 for details.
   return "android_webview";
+}
+
+// UMA_HISTOGRAM_* macros expand to a lot of code, so wrap this in a helper.
+void RecordIsWebViewViewable(bool isViewable) {
+  UMA_HISTOGRAM_BOOLEAN("SafeBrowsing.WebView.Viewable", isViewable);
 }
 
 }  // namespace
@@ -59,11 +65,12 @@ void AwSafeBrowsingUIManager::DisplayBlockingPage(
   // Check the size of the view
   UIManagerClient* client = UIManagerClient::FromWebContents(web_contents);
   if (!client || !client->CanShowInterstitial()) {
-    LOG(WARNING) << "The view is not suitable to show the SB interstitial";
+    RecordIsWebViewViewable(false);
     OnBlockingPageDone(std::vector<UnsafeResource>{resource}, false,
                        web_contents, resource.url.GetWithEmptyPath());
     return;
   }
+  RecordIsWebViewViewable(true);
   safe_browsing::BaseUIManager::DisplayBlockingPage(resource);
 }
 
