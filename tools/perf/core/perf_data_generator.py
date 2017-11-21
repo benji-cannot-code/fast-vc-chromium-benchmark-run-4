05587c0ac46c961f205cbe0bd2fbdf0c65ee1f85
Aseem Garg <aseemgarg@chromium.org>
2017-11-21 06:38:41 +0000

    Add wasm benchmark for tanks(unity)
    R=bradnelson@chromium.org,nednguyen@google.com
    BUG=v8:5558
    
    Change-Id: I73987b6018806a13fbcf636a6b122a4cb7530252
    Reviewed-on: https://chromium-review.googlesource.com/765013
    Commit-Queue: Aseem Garg <aseemgarg@chromium.org>
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Cr-Commit-Position: refs/heads/master@{#518149}

--- tools/perf/core/perf_data_generator.py
+++ tools/perf/core/perf_data_generator.py
@@ -31,7 +31,8 @@ from core.sharding_map_generator import load_benchmark_sharding_map
 
 
 _UNSCHEDULED_TELEMETRY_BENCHMARKS = set([
-  'experimental.startup.android.coldish'
+  'experimental.startup.android.coldish',
+  'wasm'
   ])
 
 
