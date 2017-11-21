dee207474658fac97257c609a54b44a707200abf
Dominick Ng <dominickn@chromium.org>
2017-11-21 04:31:39 +0000

    Remove unnecessary weak pointer in PermissionResponseCallback.
    This CL partially reverts crrev.com/c/773795, as the crash that CL was
    fixing was actually fixed in crbug.com/757394. Additionally, this CL
    removes the unnecessary weak pointer in PermissionResponseCallback, and
    replaces it with a raw pointer. This is valid as the weak pointer is
    misleading; PermissionManager must outline PermissionResponseCallback,
    and any implication that it does not is a potential bug.
    
    The removal of the weak pointer argument in PermissionResponseCallback
    also allows for the removal of the WeakPtrFactory in PermissionManager.
    
    BUG=751020
    
    Change-Id: I8b089d8126e3b34badb4460bf0e8297003a3bac5
    Reviewed-on: https://chromium-review.googlesource.com/778721
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Reviewed-by: Raymes Khoury <raymes@chromium.org>
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518110}

--- chrome/browser/permissions/permission_manager.h
+++ chrome/browser/permissions/permission_manager.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_forward.h"
 #include "base/containers/id_map.h"
 #include "base/macros.h"
-#include "base/memory/weak_ptr.h"
 #include "chrome/browser/permissions/permission_util.h"
 #include "components/content_settings/core/browser/content_settings_observer.h"
 #include "components/content_settings/core/common/content_settings.h"
@@ -160,8 +159,6 @@ class PermissionManager : public KeyedService,
                      std::unique_ptr<PermissionContextBase>,
                      ContentSettingsTypeHash>
       permission_contexts_;
-
-  base::WeakPtrFactory<PermissionManager> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(PermissionManager);
 };
