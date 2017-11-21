ae12bb24304ebdc0936ad7e65d6823e0ad176c00
Daniel Bratell <bratell@opera.com>
2017-11-21 11:08:29 +0000

    Rename two RasterBufferImpl to make the names more unique
    In jumbo builds (use_jumbo_build=true), cc files are merged so local
    symbols, such as these RasterBufferImpl, will end up in the same
    translation unit and prevent the code from compiling.
    
    Having unique names have other benefits as well in debugging and
    documentation so this CL renames them ZeroCopyRasterBufferImpl
    and BitmapRasterBufferImpl.
    
    Bug: 786926
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I0b0af89cc1c0b3581a18a685742ad3e8c6e6b429
    Reviewed-on: https://chromium-review.googlesource.com/779119
    Reviewed-by: enne <enne@chromium.org>
    Commit-Queue: Daniel Bratell <bratell@opera.com>
    Cr-Commit-Position: refs/heads/master@{#518210}

--- cc/raster/bitmap_raster_buffer_provider.cc
+++ cc/raster/bitmap_raster_buffer_provider.cc
@@ -23,12 +23,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace cc {
 namespace {
 
-class RasterBufferImpl : public RasterBuffer {
+class BitmapRasterBufferImpl : public RasterBuffer {
  public:
-  RasterBufferImpl(LayerTreeResourceProvider* resource_provider,
-                   const Resource* resource,
-                   uint64_t resource_content_id,
-                   uint64_t previous_content_id)
+  BitmapRasterBufferImpl(LayerTreeResourceProvider* resource_provider,
+                         const Resource* resource,
+                         uint64_t resource_content_id,
+                         uint64_t previous_content_id)
       : lock_(resource_provider, resource->id()),
         resource_(resource),
         resource_has_previous_content_(
@@ -63,7 +63,7 @@ class RasterBufferImpl : public RasterBuffer {
   const Resource* resource_;
   bool resource_has_previous_content_;
 
-  DISALLOW_COPY_AND_ASSIGN(RasterBufferImpl);
+  DISALLOW_COPY_AND_ASSIGN(BitmapRasterBufferImpl);
 };
 
 }  // namespace
@@ -86,7 +86,7 @@ BitmapRasterBufferProvider::AcquireBufferForRaster(
     const Resource* resource,
     uint64_t resource_content_id,
     uint64_t previous_content_id) {
-  return std::unique_ptr<RasterBuffer>(new RasterBufferImpl(
+  return std::unique_ptr<RasterBuffer>(new BitmapRasterBufferImpl(
       resource_provider_, resource, resource_content_id, previous_content_id));
 }
 
