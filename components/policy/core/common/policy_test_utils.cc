7427c65d0f3ec4275b66fad3b74228e25916bf85
jdoerrie <jdoerrie@chromium.org>
2017-11-21 17:18:49 +0000

    Remove base::Value::GetType()
    This change replaces remaining usages of base::Value::GetType() with
    base::Value::type() and removes the deprecated method entirely.
    
    Bug: 646113
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I412454d05f56d94125a76e06a09990e2d6a424e9
    Reviewed-on: https://chromium-review.googlesource.com/718458
    Reviewed-by: Brett Wilson <brettw@chromium.org>
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518287}

--- components/policy/core/common/policy_test_utils.cc
+++ components/policy/core/common/policy_test_utils.cc
@@ -58,7 +58,7 @@ bool PolicyServiceIsEmpty(const PolicyService* service) {
 
 #if defined(OS_IOS) || defined(OS_MACOSX)
 CFPropertyListRef ValueToProperty(const base::Value& value) {
-  switch (value.GetType()) {
+  switch (value.type()) {
     case base::Value::Type::NONE:
       return kCFNull;
 
