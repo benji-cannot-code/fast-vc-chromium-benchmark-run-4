72eb26674524007e756a2cd3ebe8c6a1d2a8f09b
Andy Paicu <andypaicu@chromium.org>
2017-11-21 15:31:31 +0000

    Modified the report-uri logic to not respect base elements
    When a relative URL is specified for report-uri, we should not respect
    any base elements.
    
    Spec: https://w3c.github.io/webappsec-csp/#report-violation
    
    Bug: 777350
    Change-Id: I1ca513d056047bcbe71f5faaefcd800d9f70363e
    Reviewed-on: https://chromium-review.googlesource.com/748321
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518260}

--- third_party/WebKit/Source/core/frame/csp/ContentSecurityPolicy.h
+++ third_party/WebKit/Source/core/frame/csp/ContentSecurityPolicy.h
@@ -447,8 +447,6 @@ class CORE_EXPORT ContentSecurityPolicy
 
   void ApplyPolicySideEffectsToExecutionContext();
 
-  KURL CompleteURL(const String&) const;
-
   void LogToConsole(const String& message, MessageLevel = kErrorMessageLevel);
 
   void AddAndReportPolicyFromHeaderValue(const String&,
