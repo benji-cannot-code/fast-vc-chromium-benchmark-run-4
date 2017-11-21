c91fb56024eebc2818eb60d9cc68060e3e82800b
Eric Lawrence <elawrence@chromium.org>
2017-11-21 16:06:05 +0000

    Omit policy-installed certificate message on unsupported platforms
    Only ChromeOS supports policy-installed certificates. Omit the string
    describing this state from the PageInfo UI's resources except when
    compiling for ChromeOS.
    
    Bug: 717802
    Change-Id: I106b03b8cac1a15e3937996585185e2e26279d4a
    Reviewed-on: https://chromium-review.googlesource.com/779880
    Reviewed-by: Emily Stark <estark@chromium.org>
    Commit-Queue: Eric Lawrence <elawrence@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518267}

--- chrome/browser/ui/page_info/page_info_unittest.cc
+++ chrome/browser/ui/page_info/page_info_unittest.cc
@@ -713,6 +713,7 @@ TEST_F(PageInfoTest, HTTPSConnectionError) {
   EXPECT_EQ(base::string16(), page_info()->organization_name());
 }
 
+#if defined(OS_CHROMEOS)
 TEST_F(PageInfoTest, HTTPSPolicyCertConnection) {
   security_info_.security_level =
       security_state::SECURE_WITH_POLICY_INSTALLED_CERT;
@@ -733,6 +734,7 @@ TEST_F(PageInfoTest, HTTPSPolicyCertConnection) {
             page_info()->site_identity_status());
   EXPECT_EQ(base::string16(), page_info()->organization_name());
 }
+#endif
 
 TEST_F(PageInfoTest, HTTPSSHA1) {
   security_info_.security_level = security_state::NONE;
