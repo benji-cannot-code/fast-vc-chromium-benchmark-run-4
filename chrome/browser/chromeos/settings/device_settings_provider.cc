afabadcc1f8eb7fe0e86b4e64af4824c809e70e2
phweiss <phweiss@google.com>
2017-11-21 15:00:47 +0000

    New Device policy to disallow ARC on unaffiliated users
    If the policy is unset or set to true, behaviour is as determined by other
    settings, but if the policy is set to false, ARC will automatically be disabled
    for unaffiliated users (e.g. users from a different domain or non-managed
    gmail-accounts).
    
    BUG=b/68207247
    
    Change-Id: I6aa0b760d9530f704c89b679bab3f266874f0689
    Reviewed-on: https://chromium-review.googlesource.com/772230
    Commit-Queue: Philipp Weiß <phweiss@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518248}

--- chrome/browser/chromeos/settings/device_settings_provider.cc
+++ chrome/browser/chromeos/settings/device_settings_provider.cc
@@ -110,6 +110,7 @@ const char* const kKnownSettings[] = {
     kSystemUse24HourClock,
     kTargetVersionPrefix,
     kTPMFirmwareUpdateSettings,
+    kUnaffiliatedArcAllowed,
     kUpdateDisabled,
     kVariationsRestrictParameter,
 };
@@ -613,6 +614,16 @@ void DecodeGenericPolicies(
       new_values_cache->SetValue(
           kDeviceEnrollmentIdNeeded,
           base::MakeUnique<base::Value>(container.enrollment_id_needed()));
+    }
+  }
+
+  if (policy.has_unaffiliated_arc_allowed()) {
+    const em::UnaffiliatedArcAllowedProto& container(
+        policy.unaffiliated_arc_allowed());
+    if (container.has_unaffiliated_arc_allowed()) {
+      new_values_cache->SetValue(
+          kUnaffiliatedArcAllowed,
+          base::MakeUnique<base::Value>(container.unaffiliated_arc_allowed()));
     }
   }
 }
