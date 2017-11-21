d01df02209b8a93d2e104ae750d2dc4e395b0029
Sidney San Martín <sdy@chromium.org>
2017-11-21 22:46:22 +0000

    Fix a couple of MD downloads memory leaks.
    Bug: 787571
    Change-Id: Id7f2533890ca493b4eb6bee16cd6cdbee475554c
    Reviewed-on: https://chromium-review.googlesource.com/783750
    Commit-Queue: Sidney San Martín <sdy@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518423}

--- chrome/browser/ui/cocoa/harmony_button.mm
+++ chrome/browser/ui/cocoa/harmony_button.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "chrome/browser/ui/cocoa/harmony_button.h"
 
+#import "base/mac/scoped_cftyperef.h"
 #import "chrome/browser/themes/theme_properties.h"
 #import "chrome/browser/ui/cocoa/themed_window.h"
 #include "skia/ext/skia_utils_mac.h"
@@ -220,8 +221,9 @@ constexpr NSTimeInterval kTransitionDuration = 0.25;
 
 - (void)layout {
   CALayer* layer = self.layer;
-  layer.shadowPath = CGPathCreateWithRoundedRect(
-      layer.bounds, layer.cornerRadius, layer.cornerRadius, nullptr);
+  layer.shadowPath =
+      base::ScopedCFTypeRef<CGPathRef>(CGPathCreateWithRoundedRect(
+          layer.bounds, layer.cornerRadius, layer.cornerRadius, nullptr));
   [self updateHoverButtonAppearanceAnimated:NO];
   self.title = self.title;  // Match the theme.
   [super layout];
