a425bc2921d8c970b628b2dda960979c509962cc
Darren Shen <shend@chromium.org>
2017-11-21 07:10:49 +0000

    [css-typed-om] Add tests for mutating StylePropertyMap.
    StylePropertyMap has several methods for mutating its state:
    - append
    - delete
    - set
    - update
    
    This patch adds tests for each of those. Note that 'update' was not
    originally in the IDL so we had to add it.
    
    There are several failing tests.
    
    Spec: https://drafts.css-houdini.org/css-typed-om-1/#stylepropertymap
    
    Bug: 774887
    Change-Id: I1458cacfd95f4c0056f2e7bc6592798b2d3a1f64
    Reviewed-on: https://chromium-review.googlesource.com/768252
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Renée Wright <rjwright@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518164}

--- third_party/WebKit/Source/core/css/cssom/StylePropertyMap.h
+++ third_party/WebKit/Source/core/css/cssom/StylePropertyMap.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef StylePropertyMap_h
 #define StylePropertyMap_h
 
+#include "bindings/core/v8/v8_update_function.h"
 #include "core/css/cssom/StylePropertyMapReadonly.h"
 
 namespace blink {
@@ -27,6 +28,7 @@ class CORE_EXPORT StylePropertyMap : public StylePropertyMapReadonly {
               CSSStyleValueOrCSSStyleValueSequenceOrString& item,
               ExceptionState&);
   void remove(const String& property_name, ExceptionState&);
+  void update(const String&, const V8UpdateFunction*) {}
 
   virtual void set(const ExecutionContext*,
                    CSSPropertyID,
