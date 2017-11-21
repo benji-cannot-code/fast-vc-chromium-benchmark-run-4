5bf73cd3a479b6e70e5aea0e620481cb4337f1a7
Xiyuan Xia <xiyuan@chromium.org>
2017-11-21 23:24:26 +0000

    Fix DCHECK when closing AppWindow before nav commit
    DCHECK happens for AppWindowCreateFunction did_respond() when an
    AppWindow is closed before OnReadyToCommitFirstNavigation is invoked.
    This is because AppWindowCreateFunction respond is deferred
    until navigation is committed when PlzNavigate is enabled.
    
    Bug: 783903
    Change-Id: I2d0ef5ee1611c869314a8c9fe07808235f6e318c
    Reviewed-on: https://chromium-review.googlesource.com/775902
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518437}

--- extensions/browser/app_window/app_window_contents.cc
+++ extensions/browser/app_window/app_window_contents.cc
@@ -77,9 +77,10 @@ void AppWindowContentsImpl::NativeWindowChanged(
                                            "updateAppWindowProperties", args));
 }
 
-void AppWindowContentsImpl::NativeWindowClosed() {
+void AppWindowContentsImpl::NativeWindowClosed(bool send_onclosed) {
   content::RenderFrameHost* rfh = web_contents_->GetMainFrame();
-  rfh->Send(new ExtensionMsg_AppWindowClosed(rfh->GetRoutingID()));
+  rfh->Send(
+      new ExtensionMsg_AppWindowClosed(rfh->GetRoutingID(), send_onclosed));
 }
 
 void AppWindowContentsImpl::OnWindowReady() {
