b54538b7ce5c57269b0137239af505bba418981b
Daniel Erat <derat@chromium.org>
2017-11-21 23:35:35 +0000

    chromeos: Add Power.LidClosedSuspendCountDaily histogram.
    Add a new histogram metric tracking the number of daily
    suspends triggered by Chromebooks' lids being closed.
    
    Bug: 782953
    Change-Id: I86540cec8b677823b00d79d8699dc005c238ab1d
    Reviewed-on: https://chromium-review.googlesource.com/780427
    Commit-Queue: Dan Erat <derat@chromium.org>
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518447}

--- chrome/browser/chromeos/power/power_metrics_reporter.h
+++ chrome/browser/chromeos/power/power_metrics_reporter.h
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_CHROMEOS_POWER_POWER_METRICS_REPORTER_H_
 #define CHROME_BROWSER_CHROMEOS_POWER_POWER_METRICS_REPORTER_H_
 
+#include <map>
 #include <memory>
+#include <string>
 
 #include "base/macros.h"
 #include "base/timer/timer.h"
@@ -28,6 +30,7 @@ class PowerMetricsReporter : public PowerManagerClient::Observer {
   static const char kIdleScreenDimCountName[];
   static const char kIdleScreenOffCountName[];
   static const char kIdleSuspendCountName[];
+  static const char kLidClosedSuspendCountName[];
 
   // Registers prefs used by PowerMetricsReporter in |registry|.
   static void RegisterLocalStatePrefs(PrefRegistrySimple* registry);
@@ -52,10 +55,9 @@ class PowerMetricsReporter : public PowerManagerClient::Observer {
   // |daily_event_|.
   void ReportDailyMetrics(metrics::DailyEvent::IntervalType type);
 
-  // Updates |*_count_| members and the corresponding prefs.
-  void SetIdleScreenDimCount(int count);
-  void SetIdleScreenOffCount(int count);
-  void SetIdleSuspendCount(int count);
+  // Adds |num| to |pref_name|'s count in |daily_counts_| and updates the
+  // corresponding pref.
+  void AddToCount(const std::string& pref_name, int num);
 
   PowerManagerClient* power_manager_client_;  // Not owned.
   PrefService* pref_service_;                 // Not owned.
@@ -68,10 +70,8 @@ class PowerMetricsReporter : public PowerManagerClient::Observer {
   // Last-received screen-idle state from powerd.
   power_manager::ScreenIdleState old_screen_idle_state_;
 
-  // Current counts of various inactivity-triggered power management events.
-  int idle_screen_dim_count_ = 0;
-  int idle_screen_off_count_ = 0;
-  int idle_suspend_count_ = 0;
+  // Map from local store pref name backing a daily count to the count itself.
+  std::map<std::string, int> daily_counts_;
 
   DISALLOW_COPY_AND_ASSIGN(PowerMetricsReporter);
 };
