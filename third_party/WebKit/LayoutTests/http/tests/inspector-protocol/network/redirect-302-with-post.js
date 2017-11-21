4fed566a54b94699cfa4a90fa98baf0a9c47cb94
Nathan Bruer <allada@chromium.org>
2017-11-21 04:31:34 +0000

    Allows renderer gets post data if devtools attached w/ plzNavigate
    Adds a temporary fix to allow devtools to get the post data of
    navigation request if the renderer has devtools attached.
    
    R=caseq,clamy
    BUG=766715
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I875ea05e778fc431b6073a6fb6b1c393bcc904be
    Reviewed-on: https://chromium-review.googlesource.com/770490
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: Blaise Bruer <allada@google.com>
    Commit-Queue: Blaise Bruer <allada@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518109}

---
+++ third_party/WebKit/LayoutTests/http/tests/inspector-protocol/network/redirect-302-with-post.js
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(async function(testRunner) {
+  var {page, session, dp} = await testRunner.startBlank(`Test to ensure devtools gets post data for redirected navigation requests.\n`);
+
+  dp.Network.enable();
+  dp.Page.enable();
+
+  dp.Runtime.evaluate({expression: `
+    document.body.innerHTML = '<form id="form" method="post" action="/loading/resources/redirect-methods-result.php?status=302"><input type="text" name="foo" value="bar" /></form>';
+    var form = document.getElementById('form');
+    form.submit();
+  `});
+
+  dp.Network.onRequestWillBeSent(event => {
+    testRunner.log("Got request: " + event.params.request.url);
+    if (event.params.request.postData)
+      testRunner.log("Post Data: " + event.params.request.postData);
+  });
+  dp.Page.onLoadEventFired(() => testRunner.completeTest());
+});
