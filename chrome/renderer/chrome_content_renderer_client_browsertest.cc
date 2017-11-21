79367a110efc74ff5d6d7bc190653575704b0867
Jun Cai <juncai@chromium.org>
2017-11-21 15:19:01 +0000

    NetworkService: Fix ChromeContentRendererClientBrowserTest browser tests
    This CL fixes ChromeContentRendererClientBrowserTest browser tests by
    adding code to ignore certificate error when setting up the tests.
    
    Bug: 769401
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: Iec113e7471dbe90cbfb0f10aa662df35ff6b8dee
    Reviewed-on: https://chromium-review.googlesource.com/777849
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518255}

--- chrome/renderer/chrome_content_renderer_client_browsertest.cc
+++ chrome/renderer/chrome_content_renderer_client_browsertest.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/threading/thread_task_runner_handle.h"
-#include "chrome/browser/profiles/profile_io_data.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/common/chrome_switches.h"
@@ -21,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/chrome_render_view_test.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
+#include "components/network_session_configurator/common/network_switches.h"
 #include "content/public/common/content_constants.h"
 #include "content/public/common/url_loader_throttle.h"
 #include "content/public/renderer/render_frame.h"
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ipc/ipc_listener.h"
 #include "ipc/ipc_sender.h"
 #include "ipc/ipc_test_sink.h"
-#include "net/cert/mock_cert_verifier.h"
 #include "net/dns/mock_host_resolver.h"
 #include "net/test/embedded_test_server/http_request.h"
 #include "net/test/embedded_test_server/http_response.h"
@@ -154,8 +153,7 @@ class ChromeContentRendererClientBrowserTest :
  public:
   ChromeContentRendererClientBrowserTest()
       : https_server_(std::make_unique<net::EmbeddedTestServer>(
-            net::EmbeddedTestServer::TYPE_HTTPS)),
-        mock_cert_verifier_(std::make_unique<net::MockCertVerifier>()) {}
+            net::EmbeddedTestServer::TYPE_HTTPS)) {}
 
   void MonitorRequestHandler(const net::test_server::HttpRequest& request) {
     // We're only interested in YouTube video embeds
@@ -191,27 +189,18 @@ class ChromeContentRendererClientBrowserTest :
     message_runner_ = new content::MessageLoopRunner();
   }
 
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    // HTTPS server only serves a valid cert for localhost, so this is needed
+    // to load pages from other hosts without an error.
+    command_line->AppendSwitch(switches::kIgnoreCertificateErrors);
+  }
+
  protected:
-  void SetUpInProcessBrowserTestFixture() override {
-    InProcessBrowserTest::SetUpInProcessBrowserTestFixture();
-
-    mock_cert_verifier_->set_default_result(net::OK);
-    ProfileIOData::SetCertVerifierForTesting(mock_cert_verifier_.get());
-  }
-
-  void TearDownInProcessBrowserTestFixture() override {
-    InProcessBrowserTest::TearDownInProcessBrowserTestFixture();
-
-    ProfileIOData::SetCertVerifierForTesting(nullptr);
-  }
-
   net::EmbeddedTestServer* https_server() const { return https_server_.get(); }
 
  private:
   scoped_refptr<content::MessageLoopRunner> message_runner_;
-
   std::unique_ptr<net::EmbeddedTestServer> https_server_;
-  std::unique_ptr<net::MockCertVerifier> mock_cert_verifier_;
 };
 
 IN_PROC_BROWSER_TEST_P(ChromeContentRendererClientBrowserTest,
