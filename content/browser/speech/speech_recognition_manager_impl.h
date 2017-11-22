f0566688a01d5ec780a264bb2e2e236e548324d5
Chandan Padhi <c.padhi@samsung.com>
2017-11-22 19:54:46 +0000

    Use callback to receive media stream device stopped notifications
    This CL removes MediaStreamRequester class and it's corresponding file.
    MediaStreamManager's GenerateStream and OpenDevice methods are modified
    to take a callback in order to receive device stopped notifications.
    
    This CL also removes an unused label string argument from
    MediaStreamDispatcherEventHandler::OnDeviceStopped().
    
    Bug: 742682
    Change-Id: I437e7b8efb8636549c67b9de67852699c7ed8d48
    Reviewed-on: https://chromium-review.googlesource.com/785290
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518716}

--- content/browser/speech/speech_recognition_manager_impl.h
+++ content/browser/speech/speech_recognition_manager_impl.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/compiler_specific.h"
 #include "base/memory/weak_ptr.h"
-#include "content/browser/renderer_host/media/media_stream_requester.h"
 #include "content/public/browser/speech_recognition_event_listener.h"
 #include "content/public/browser/speech_recognition_manager.h"
 #include "content/public/browser/speech_recognition_session_config.h"
