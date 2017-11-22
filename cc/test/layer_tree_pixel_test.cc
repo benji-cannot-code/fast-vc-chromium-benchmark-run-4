0b51b9f9a1d5f70c3b5a0b107f7aa87e112d9ac8
Olga Sharonova <olka@chromium.org>
2017-11-22 12:05:22 +0000

    Revert "cc: Run threaded LayerTreeTests with sync scheduling."
    This reverts commit ee41ffee3151b23dfe5a9e25b2ffe0d8a255b6a1.
    
    Reason for revert: Bug: 787793
    
    Original change's description:
    > cc: Run threaded LayerTreeTests with sync scheduling.
    > 
    > Add an additional mode that runs multi-threaded LayerTreeTests with
    > sync/webview scheduling.
    > 
    > R=​danakj@chromium.org
    > 
    > Bug: 787257
    > Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    > Change-Id: Ie2c12b772de6e992d1caa238159ea9bc2592ee2e
    > Reviewed-on: https://chromium-review.googlesource.com/783637
    > Commit-Queue: Khushal <khushalsagar@chromium.org>
    > Reviewed-by: danakj <danakj@chromium.org>
    > Reviewed-by: enne <enne@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#518510}
    
    TBR=danakj@chromium.org,enne@chromium.org,khushalsagar@chromium.org
    
    Change-Id: Ibb83ff13c50a9c131ec9a8aefd8aa9bcd2df670e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 787257
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Reviewed-on: https://chromium-review.googlesource.com/785390
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518612}

--- cc/test/layer_tree_pixel_test.cc
+++ cc/test/layer_tree_pixel_test.cc
@@ -195,7 +195,7 @@ void LayerTreePixelTest::RunPixelTest(
   content_root_ = content_root;
   readback_target_ = nullptr;
   ref_file_ = file_name;
-  RunTest(CompositorMode::THREADED, false);
+  RunTest(CompositorMode::THREADED);
 }
 
 void LayerTreePixelTest::RunSingleThreadedPixelTest(
@@ -206,7 +206,7 @@ void LayerTreePixelTest::RunSingleThreadedPixelTest(
   content_root_ = content_root;
   readback_target_ = nullptr;
   ref_file_ = file_name;
-  RunTest(CompositorMode::SINGLE_THREADED, false);
+  RunTest(CompositorMode::SINGLE_THREADED);
 }
 
 void LayerTreePixelTest::RunPixelTestWithReadbackTarget(
@@ -218,7 +218,7 @@ void LayerTreePixelTest::RunPixelTestWithReadbackTarget(
   content_root_ = content_root;
   readback_target_ = target;
   ref_file_ = file_name;
-  RunTest(CompositorMode::THREADED, false);
+  RunTest(CompositorMode::THREADED);
 }
 
 void LayerTreePixelTest::SetupTree() {
