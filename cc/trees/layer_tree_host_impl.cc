9cb2fc10019cdfb1d9adb83ec4b5baf693b4e441
Sunny Sachanandani <sunnyps@chromium.org>
2017-11-22 21:51:17 +0000

    cc: Do not evict UI resources when switching gpu rasterization mode.
    We were marking UI resources as evicted when gpu rasterization mode
    changed during commit. However, we don't update the scheduler's CanDraw
    state or ask for a new commit to recreate the resources.
    
    We don't need to evict UI resources for gpu rasterization because these
    are just uploaded from bitmaps to a texture and have nothing to do with
    tile manager or resource pool. The reasons for recreating tiles
    e.g. artifacts at the seams don't apply to UI resources.
    
    R=danakj,enne
    BUG=748991
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I59cd377719a27335e239459bf00ab6d9dabc70ef
    Reviewed-on: https://chromium-review.googlesource.com/773785
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518761}

--- cc/trees/layer_tree_host_impl.cc
+++ cc/trees/layer_tree_host_impl.cc
@@ -1538,7 +1538,7 @@ void LayerTreeHostImpl::SetMemoryPolicy(const ManagedMemoryPolicy& policy) {
   if (!policy.bytes_limit_when_visible && resource_pool_ &&
       settings_.using_synchronous_renderer_compositor) {
     ReleaseTileResources();
-    CleanUpTileManagerAndUIResources();
+    CleanUpTileManagerResources();
 
     // Force a call to NotifyAllTileTasks completed - otherwise this logic may
     // be skipped if no work was enqueued at the time the tile manager was
@@ -2045,7 +2045,7 @@ void LayerTreeHostImpl::UpdateTreeResourcesForGpuRasterizationIfNeeded() {
   // one.
   ReleaseTileResources();
   if (resource_pool_) {
-    CleanUpTileManagerAndUIResources();
+    CleanUpTileManagerResources();
     CreateTileManagerResources();
   }
   RecreateTileResources();
@@ -2606,8 +2606,7 @@ void LayerTreeHostImpl::DidChangeScrollbarVisibility() {
   client_->SetNeedsCommitOnImplThread();
 }
 
-void LayerTreeHostImpl::CleanUpTileManagerAndUIResources() {
-  ClearUIResources();
+void LayerTreeHostImpl::CleanUpTileManagerResources() {
   tile_manager_.FinishTasksAndCleanUp();
   resource_pool_ = nullptr;
   single_thread_synchronous_task_graph_runner_ = nullptr;
@@ -2643,7 +2642,8 @@ void LayerTreeHostImpl::ReleaseLayerTreeFrameSink() {
   ReleaseTreeResources();
 
   // Note: ui resource cleanup uses the |resource_provider_|.
-  CleanUpTileManagerAndUIResources();
+  CleanUpTileManagerResources();
+  ClearUIResources();
   resource_provider_ = nullptr;
 
   // Release any context visibility before we destroy the LayerTreeFrameSink.
