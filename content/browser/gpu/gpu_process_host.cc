01e32d28a6b880f2249b96f25ee22e6a2dfb18ca
Miriam Gershenson <mgersh@chromium.org>
2017-11-22 16:05:18 +0000

    Revert "android: Optionally madvise(MADV_RANDOM) on .text."
    This reverts commit 411a34f5e9b7e19c5b8ca4443e20f5d51e3fa8b9.
    
    Reason for revert: Breaks compile on x86 builders: https://build.chromium.org/p/chromium.android/builders/Android%20Cronet%20x86%20Builder%20%28dbg%29/builds/72101, https://build.chromium.org/p/chromium.android/builders/Android%20Cronet%20x86%20Builder/builds/44466
    
    Original change's description:
    > android: Optionally madvise(MADV_RANDOM) on .text.
    > 
    > This CL adds:
    > 
    > - "anchor" functions at the beginning and end of .text: these are
    >   already used in lightweight_cygprofile.cc, and require the orderfile
    >   to be properly constructed, which is the case since
    >   https://chromium-review.googlesource.com/753882. These functions are
    >   trickier for regular builds, as they use --icf=all.
    > - madvise(MADV_RANDOM): disable kernel readahead on a given range. This makes
    >   the reporting logic for code residency clearer, as it actually shows which
    >   pages are requested. This has to be called as early as possible and from all
    >   processes, and may be enabled in all builds at a later date. This is
    >   controlled by a new command-line flag, madvise-random-executable-code.
    > 
    > Bug: 758566
    > Change-Id: I8c7a5ca759b355b9e632d6ad027f8d9cd8fb84e8
    > Reviewed-on: https://chromium-review.googlesource.com/776895
    > Commit-Queue: Benoit L <lizeb@chromium.org>
    > Reviewed-by: Camille Lamy <clamy@chromium.org>
    > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > Reviewed-by: Egor Pasko <pasko@chromium.org>
    > Reviewed-by: agrieve <agrieve@chromium.org>
    > Reviewed-by: Matthew Cary <mattcary@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#518639}
    
    TBR=pasko@chromium.org,thestig@chromium.org,clamy@chromium.org,agrieve@chromium.org,lizeb@chromium.org,mattcary@chromium.org
    
    Change-Id: Ia5cb28b8a8f39d3e585162aa2974451d3fd7be5e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 758566
    Reviewed-on: https://chromium-review.googlesource.com/786050
    Reviewed-by: Miriam Gershenson <mgersh@chromium.org>
    Commit-Queue: Miriam Gershenson <mgersh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518645}

--- content/browser/gpu/gpu_process_host.cc
+++ content/browser/gpu/gpu_process_host.cc
@@ -165,9 +165,6 @@ static const char* const kSwitchNames[] = {
     switches::kUseCmdDecoder,
     switches::kIgnoreGpuBlacklist,
     switches::kForceVideoOverlays,
-#if defined(OS_ANDROID)
-    switches::kMadviseRandomExecutableCode,
-#endif
 };
 
 enum GPUProcessLifetimeEvent {
