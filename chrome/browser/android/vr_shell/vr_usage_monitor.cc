55394fa9fd0186c1f78eadcf7fc45a61198e9d89
Bill Orr <billorr@chromium.org>
2017-11-22 18:43:00 +0000

    Fix VRSessionTime.Browser metric to avoid inaccurate reports of 0ms
    The issue here is that there were separate methods to set webvr mode and
    whether vr was active at all.  These were called in a row, so direct
    webvr presentation showed up as a ~0ms browsing session.
    
    The fix is to make the metric helper's constructor support specifying
    both webvr enabled and vr active at the same time.
    
    BUG=787585
    
    Change-Id: I8d2b9beba0f9f57b1cf56e8e9f317d25a372285c
    Reviewed-on: https://chromium-review.googlesource.com/783821
    Commit-Queue: Bill Orr <billorr@chromium.org>
    Reviewed-by: Biao She <bshe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518690}

--- chrome/browser/android/vr_shell/vr_usage_monitor.cc
+++ chrome/browser/android/vr_shell/vr_usage_monitor.cc
@@ -280,7 +280,10 @@ void VrMetricsHelper::SetVrMode(VRMode mode) {
   mode_ = mode;
 }
 
-VrMetricsHelper::VrMetricsHelper(content::WebContents* contents) {
+VrMetricsHelper::VrMetricsHelper(content::WebContents* contents,
+                                 VRMode initial_mode)
+    : is_webvr_(initial_mode == VRMode::WEBVR),
+      is_vr_enabled_(initial_mode != VRMode::NO_VR) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
 
   num_videos_playing_ = contents->GetCurrentlyPlayingVideoCount();
@@ -293,6 +296,8 @@ VrMetricsHelper::VrMetricsHelper(content::WebContents* contents) {
   session_video_timer_ =
       base::MakeUnique<SessionTimerImpl<SESSION_VR_WITH_VIDEO>>(
           kMaximumVideoSessionGap, kMinimumVideoSessionDuration);
+
+  UpdateMode();
 }
 
 VrMetricsHelper::~VrMetricsHelper() = default;
