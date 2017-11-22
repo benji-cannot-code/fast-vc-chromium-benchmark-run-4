54d5c0ed02aa95dbf946890d46e8041b0ae3d25f
Louis Romero <lpromero@chromium.org>
2017-11-22 15:17:59 +0000

    Add keyboard shortcuts to change tabs
    There were already shortcuts to switch tabs, but this CL adds support for
    Safari-style shortcuts. Those are already supported on Chrome Mac.
    
    Bug: 786725
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Ib262edf9a251a942bcf52aa223c6e3b373d105b4
    R: pkl@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/781631
    Reviewed-by: Peter Lee <pkl@chromium.org>
    Commit-Queue: Louis Romero <lpromero@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518634}

--- ios/chrome/browser/ui/key_commands_provider.mm
+++ ios/chrome/browser/ui/key_commands_provider.mm
@@ -313,6 +313,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                           action:^{
                             [weakConsumer focusNextTab];
                           }],
+      [UIKeyCommand
+          cr_keyCommandWithInput:@"\t"
+                   modifierFlags:UIKeyModifierControl | UIKeyModifierShift
+                           title:nil
+                          action:^{
+                            [weakConsumer focusPreviousTab];
+                          }],
+      [UIKeyCommand
+          cr_keyCommandWithInput:@"\t"
+                   modifierFlags:UIKeyModifierControl
+                           title:nil
+                          action:^{
+                            [weakConsumer focusNextTab];
+                          }],
     ]];
   }
 
