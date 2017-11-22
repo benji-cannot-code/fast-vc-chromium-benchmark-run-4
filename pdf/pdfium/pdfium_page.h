fa22af5d667a4fbd788c55ecbf0c97eb584e68e8
Henrique Nakashima <hnakashima@chromium.org>
2017-11-22 22:18:10 +0000

    Count annotation subtypes that appear in PDF documents.
    Bug: chromium:768986
    Change-Id: I25fcf447501224d02be40565c8ec770a06edd2e6
    Reviewed-on: https://chromium-review.googlesource.com/740141
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: dsinclair <dsinclair@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518771}

--- pdf/pdfium/pdfium_page.h
+++ pdf/pdfium/pdfium_page.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/optional.h"
 #include "base/strings/string16.h"
+#include "pdf/pdf_engine.h"
 #include "ppapi/cpp/rect.h"
 #include "third_party/pdfium/public/fpdf_doc.h"
 #include "third_party/pdfium/public/fpdf_formfill.h"
@@ -108,6 +109,8 @@ class PDFiumPage {
                         double bottom,
                         int rotation) const;
 
+  const PDFEngine::PageFeatures* GetPageFeatures();
+
   int index() const { return index_; }
   pp::Rect rect() const { return rect_; }
   void set_rect(const pp::Rect& r) { rect_ = r; }
@@ -165,6 +168,7 @@ class PDFiumPage {
   bool calculated_links_;
   std::vector<Link> links_;
   bool available_;
+  PDFEngine::PageFeatures page_features_;
 };
 
 }  // namespace chrome_pdf
