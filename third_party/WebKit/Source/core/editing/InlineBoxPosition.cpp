b8bde8f74e0bbcd37a4358c6eec245265286edcf
Xiaocheng Hu <xiaochengh@chromium.org>
2017-11-22 00:35:41 +0000

    Sanitize function parameters when calculating InlineBoxPosition for text
    This patch changes a parameter type of the following two functions from
    |LayoutObject*| to |const LayoutText*| because they are only called with
    |const LayoutText*|:
    
    - ComputeInlineBoxPositionForTextNode()
    - SearchAheadForBetterMatch()
    
    Bug: 766448
    Change-Id: If964b56061a38b49bf95b1504fcf9804c0be8211
    Reviewed-on: https://chromium-review.googlesource.com/783810
    Reviewed-by: Ryan Landay <rlanday@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518474}

--- third_party/WebKit/Source/core/editing/InlineBoxPosition.cpp
+++ third_party/WebKit/Source/core/editing/InlineBoxPosition.cpp
@@ -52,7 +52,7 @@ bool IsNonTextLeafChild(LayoutObject* object) {
   return true;
 }
 
-InlineTextBox* SearchAheadForBetterMatch(LayoutObject* layout_object) {
+InlineTextBox* SearchAheadForBetterMatch(const LayoutText* layout_object) {
   LayoutBlock* container = layout_object->ContainingBlock();
   for (LayoutObject* next = layout_object->NextInPreOrder(container); next;
        next = next->NextInPreOrder(container)) {
@@ -222,7 +222,7 @@ bool IsCaretAtEdgeOfInlineTextBox(int caret_offset,
 }
 
 InlineBoxPosition ComputeInlineBoxPositionForTextNode(
-    LayoutObject* layout_object,
+    const LayoutText* text_layout_object,
     int caret_offset,
     TextAffinity affinity,
     TextDirection primary_direction) {
@@ -230,8 +230,6 @@ InlineBoxPosition ComputeInlineBoxPositionForTextNode(
   // DCHECK(CanUseInlineBox(*layout_object));
 
   InlineBox* inline_box = nullptr;
-  LayoutText* text_layout_object = ToLayoutText(layout_object);
-
   InlineTextBox* candidate = nullptr;
 
   for (InlineTextBox* box : InlineTextBoxesOf(*text_layout_object)) {
@@ -264,7 +262,7 @@ InlineBoxPosition ComputeInlineBoxPositionForTextNode(
   if (!inline_box)
     return InlineBoxPosition();
   return AdjustInlineBoxPositionForTextDirection(
-      inline_box, caret_offset, layout_object->Style()->GetUnicodeBidi(),
+      inline_box, caret_offset, text_layout_object->Style()->GetUnicodeBidi(),
       primary_direction);
 }
 
@@ -328,8 +326,8 @@ InlineBoxPosition ComputeInlineBoxPositionTemplate(
   DCHECK(layout_object) << position;
 
   if (layout_object->IsText()) {
-    return ComputeInlineBoxPositionForTextNode(layout_object, caret_offset,
-                                               affinity, primary_direction);
+    return ComputeInlineBoxPositionForTextNode(
+        ToLayoutText(layout_object), caret_offset, affinity, primary_direction);
   }
 
   if (layout_object->IsAtomicInlineLevel()) {
