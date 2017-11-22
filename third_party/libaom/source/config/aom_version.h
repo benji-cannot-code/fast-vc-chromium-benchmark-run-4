32cdb861d46434940cda1b0ae9b34d6447b446bc
Johann <johannkoenig@google.com>
2017-11-22 02:13:22 +0000

    Roll src/third_party/libaom/source/libaom/ 535ecf6b3..cccda0db7 (2 commits)
    https://aomedia.googlesource.com/aom.git/+log/535ecf6b31fe..cccda0db727c
    
    $ git log 535ecf6b3..cccda0db7 --date=short --no-merges --format='%ad %ae %s'
    2017-11-20 huisu Terminate decoding at invalid mode and ref frame combo
    2017-11-17 huisu Avoid interger overflow in aom_blend_*_c()
    
    Created with:
      roll-dep src/third_party/libaom/source/libaom
    
    R=jzern@chromium.org
    
    Change-Id: I11f0755a007bedd0e40e801160b10629698e52f2
    Reviewed-on: https://chromium-review.googlesource.com/783827
    Reviewed-by: James Zern <jzern@google.com>
    Commit-Queue: Johann Koenig <johannkoenig@google.com>
    Cr-Commit-Position: refs/heads/master@{#518503}

--- third_party/libaom/source/config/aom_version.h
+++ third_party/libaom/source/config/aom_version.h
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define VERSION_MAJOR  0
 #define VERSION_MINOR  1
 #define VERSION_PATCH  0
-#define VERSION_EXTRA  "5966-g535ecf6b3"
+#define VERSION_EXTRA  "5968-gcccda0db7"
 #define VERSION_PACKED ((VERSION_MAJOR<<16)|(VERSION_MINOR<<8)|(VERSION_PATCH))
-#define VERSION_STRING_NOSP "v0.1.0-5966-g535ecf6b3"
-#define VERSION_STRING      " v0.1.0-5966-g535ecf6b3"
+#define VERSION_STRING_NOSP "v0.1.0-5968-gcccda0db7"
+#define VERSION_STRING      " v0.1.0-5968-gcccda0db7"
