34ffe10e97bc90a4177fbeac64ce14f7d5532d4c
John Z Wu <jzw@chromium.org>
2017-11-22 01:58:33 +0000

    Fake JsAutofillManager for tests.
    //ios/web_view implements autofill and would like to use a fake
    JsAutofillManager for unit tests.
    
    Bug: 778764
    Change-Id: Iff53fdc1bd75dab4f0fbfccd6759ca54d1a9c0c1
    Reviewed-on: https://chromium-review.googlesource.com/782367
    Commit-Queue: John Wu <jzw@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518496}

---
+++ components/autofill/ios/browser/fake_js_autofill_manager.mm
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "components/autofill/ios/browser/fake_js_autofill_manager.h"
+
+#import "base/mac/bind_objc_block.h"
+#include "ios/web/public/web_thread.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+@implementation FakeJSAutofillManager
+
+@synthesize lastClearedFormName = _lastClearedFormName;
+
+- (void)clearAutofilledFieldsForFormNamed:(NSString*)formName
+                        completionHandler:(ProceduralBlock)completionHandler {
+  web::WebThread::PostTask(web::WebThread::UI, FROM_HERE, base::BindBlockArc(^{
+                             _lastClearedFormName = [formName copy];
+                             completionHandler();
+                           }));
+}
+
+@end
