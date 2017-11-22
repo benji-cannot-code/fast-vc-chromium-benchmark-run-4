eb32fdbe92f72c5c7e1f02e4f3d2bd4067e6b55a
Daniel Bratell <bratell@opera.com>
2017-11-22 19:58:52 +0000

    Add jumbo support for heap and heap unit_tests (-0.75 CPU minutes)
    This adds support for jumbo compilation in oilpan which saves about
    0.75 CPU minutes of effort (about 10% of blink/platform or 0.1% of the
    total remaining compilation time).
    
    Bug: 761475
    Change-Id: Id7606583d5a97e9f6692b756acc062c73b27dbe3
    Reviewed-on: https://chromium-review.googlesource.com/693098
    Commit-Queue: Daniel Bratell <bratell@opera.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518718}

--- third_party/WebKit/Source/platform/heap/PersistentTest.cpp
+++ third_party/WebKit/Source/platform/heap/PersistentTest.cpp
@@ -5,19 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "platform/heap/Persistent.h"
 
+#include <memory>
 #include "platform/CrossThreadFunctional.h"
 #include "platform/heap/Handle.h"
+#include "platform/heap/HeapTestUtilities.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include <memory>
 
 namespace blink {
 namespace {
-
-void PreciselyCollectGarbage() {
-  ThreadState::Current()->CollectGarbage(BlinkGC::kNoHeapPointersOnStack,
-                                         BlinkGC::kGCWithSweep,
-                                         BlinkGC::kForcedGC);
-}
 
 class Receiver : public GarbageCollected<Receiver> {
  public:
