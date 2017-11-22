178824c708d518d23ad8066e2a0a721384a0cd63
Justin Donnelly <jdonnelly@chromium.org>
2017-11-22 05:33:02 +0000

    Fix a couple includes in the new omnibox test classes.
    Bug: 
    Change-Id: I2270cde7e1e0b417ea81784284593fc5d3b8283c
    Reviewed-on: https://chromium-review.googlesource.com/783825
    Commit-Queue: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518543}

--- components/omnibox/browser/test_omnibox_client.cc
+++ components/omnibox/browser/test_omnibox_client.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/search_engines/search_terms_data.h"
 #include "components/search_engines/template_url_service.h"
 #include "components/search_engines/template_url_service_client.h"
+#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 TestOmniboxClient::TestOmniboxClient()
--- components/omnibox/browser/test_omnibox_view.h
+++ components/omnibox/browser/test_omnibox_view.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/strings/string16.h"
 #include "components/omnibox/browser/omnibox_view.h"
-#include "components/toolbar/test_toolbar_model.h"
 
 struct AutocompleteMatch;
 class OmniboxEditController;
