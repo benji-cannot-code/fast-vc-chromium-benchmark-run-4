7e66605054b7e1936178408233b192a0bb1f74c1
Bill Orr <billorr@chromium.org>
2017-11-22 16:58:48 +0000

    Reland "Enable OpenVR devices behind a runtime flag on Windows"
    This change moves openvr_api.dll to instead be statically linked so
    there is no extra required dll's to deploy/load.
    
    This is a reland of 14d631ca392fa139a6f6b89d052d089b99758e33
    Original change's description:
    > Enable OpenVR devices behind a runtime flag on Windows
    >
    > BUG=782445
    >
    > Change-Id: Iec695a4db2bf2851f8f23b2c8ab1568a5bbfa588
    > Reviewed-on: https://chromium-review.googlesource.com/757859
    > Reviewed-by: Brandon Jones <bajones@chromium.org>
    > Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    > Reviewed-by: David Dorwin <ddorwin@chromium.org>
    > Commit-Queue: Bill Orr <billorr@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#517684}
    
    Bug: 782445
    Change-Id: I922b4382380230b69ff14de6877cb2f44dfd7235
    Reviewed-on: https://chromium-review.googlesource.com/779623
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Commit-Queue: Bill Orr <billorr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518657}

--- chrome/browser/vr/service/vr_device_manager.cc
+++ chrome/browser/vr/service/vr_device_manager.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/memory/singleton.h"
 #include "build/build_config.h"
+#include "chrome/common/chrome_features.h"
 #include "device/vr/features/features.h"
 
 #if defined(OS_ANDROID)
@@ -36,7 +37,8 @@ VRDeviceManager* VRDeviceManager::GetInstance() {
 #endif
 
 #if BUILDFLAG(ENABLE_OPENVR)
-    providers.emplace_back(std::make_unique<device::OpenVRDeviceProvider>());
+    if (base::FeatureList::IsEnabled(features::kOpenVR))
+      providers.emplace_back(std::make_unique<device::OpenVRDeviceProvider>());
 #endif
     new VRDeviceManager(std::move(providers));
   }
@@ -49,7 +51,6 @@ bool VRDeviceManager::HasInstance() {
 
 VRDeviceManager::VRDeviceManager(ProviderList providers)
     : providers_(std::move(providers)) {
-  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
   CHECK(!g_vr_device_manager);
   g_vr_device_manager = this;
 }
