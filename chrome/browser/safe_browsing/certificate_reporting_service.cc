ce5b2486ad7df9b6b39f16e46f85e85352122094
Mustafa Emre Acer <meacer@chromium.org>
2017-11-22 23:10:20 +0000

    Reland of Add metrics for certificate report uploads
    One of the tests changed by this CL was flaky (CertificateReportingServiceBrowserTest.
    DontSendOldReports). This is now fixed, so relanding the CL.
    
    Original CL: https://codereview.chromium.org/2862453002
    
    When sending certificate reports, we suspect that some content filters, firewalls and
    captive portals delay the response indefinitely without sending back any response.
    Certificate uploads rely on a response to be received in order to decide whether
    to retry uploading the report, so this breaks those assumptions. This CL adds a histogram
    to understand how often this happens. In particular, a difference between submitted
    reports and the sum of failed and successful reports will indicate that some reports simply
    get lost. The histogram also provides insight about how often queued reports are dropped,
    which will help us fine tune the queue size and report time-to-live.
    
    A question that came up while discussing this histogram was whether it would be useful
    since UMA uploads can also be blocked in a similar way. The answer seems to be yes, as UMA
    retries metrics uploads with a different frequency, and it persists metrics uploads whereas
    cert reports are not persisted.
    
    Bug: 682933
    Change-Id: I09616eaf04854157ddeff8d20d39e18afc705967
    Reviewed-on: https://chromium-review.googlesource.com/782642
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Reviewed-by: Jialiu Lin <jialiul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518789}

--- chrome/browser/safe_browsing/certificate_reporting_service.cc
+++ chrome/browser/safe_browsing/certificate_reporting_service.cc
@@ -39,12 +39,21 @@ void RecordUMAOnFailure(int net_error) {
   UMA_HISTOGRAM_SPARSE_SLOWLY("SSL.CertificateErrorReportFailure", -net_error);
 }
 
+void RecordUMAEvent(CertificateReportingService::ReportOutcome outcome) {
+  UMA_HISTOGRAM_ENUMERATION(
+      CertificateReportingService::kReportEventHistogram, outcome,
+      CertificateReportingService::ReportOutcome::EVENT_COUNT);
+}
+
 void CleanupOnIOThread(
     std::unique_ptr<CertificateReportingService::Reporter> reporter) {
   reporter.reset();
 }
 
 }  // namespace
+
+const char CertificateReportingService::kReportEventHistogram[] =
+    "SSL.CertificateErrorReportEvent";
 
 CertificateReportingService::BoundedReportList::BoundedReportList(
     size_t max_size)
@@ -63,10 +72,12 @@ void CertificateReportingService::BoundedReportList::Add(const Report& item) {
     const Report& last = items_.back();
     if (item.creation_time <= last.creation_time) {
       // Report older than the oldest item in the queue, ignore.
+      RecordUMAEvent(ReportOutcome::DROPPED_OR_IGNORED);
       return;
     }
     // Reached the maximum item count, remove the oldest item.
     items_.pop_back();
+    RecordUMAEvent(ReportOutcome::DROPPED_OR_IGNORED);
   }
   items_.push_back(item);
   std::sort(items_.begin(), items_.end(), ReportCompareFunc);
@@ -116,6 +127,7 @@ void CertificateReportingService::Reporter::SendPending() {
   for (Report& report : items) {
     if (report.creation_time < now - report_ttl_) {
       // Report too old, ignore.
+      RecordUMAEvent(ReportOutcome::DROPPED_OR_IGNORED);
       continue;
     }
     if (!report.is_retried) {
@@ -146,6 +158,7 @@ void CertificateReportingService::Reporter::SendInternal(
     const CertificateReportingService::Report& report) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::IO);
   inflight_reports_.insert(std::make_pair(report.report_id, report));
+  RecordUMAEvent(ReportOutcome::SUBMITTED);
   error_reporter_->SendExtendedReportingReport(
       report.serialized_report,
       base::Bind(&CertificateReportingService::Reporter::SuccessCallback,
@@ -161,6 +174,7 @@ void CertificateReportingService::Reporter::ErrorCallback(
     int http_response_code) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::IO);
   RecordUMAOnFailure(net_error);
+  RecordUMAEvent(ReportOutcome::FAILED);
   if (retries_enabled_) {
     auto it = inflight_reports_.find(report_id);
     DCHECK(it != inflight_reports_.end());
@@ -171,6 +185,7 @@ void CertificateReportingService::Reporter::ErrorCallback(
 
 void CertificateReportingService::Reporter::SuccessCallback(int report_id) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::IO);
+  RecordUMAEvent(ReportOutcome::SUCCESSFUL);
   CHECK_GT(inflight_reports_.erase(report_id), 0u);
 }
 
--- chrome/browser/safe_browsing/certificate_reporting_service_test_utils.h
+++ chrome/browser/safe_browsing/certificate_reporting_service_test_utils.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/run_loop.h"
 #include "base/sequence_checker.h"
+#include "base/test/histogram_tester.h"
 #include "chrome/browser/safe_browsing/certificate_reporting_service.h"
 #include "content/public/test/test_browser_thread.h"
 #include "content/public/test/test_browser_thread_bundle.h"
@@ -241,6 +242,28 @@ class CertificateReportingServiceTestHelper {
   uint8_t server_private_key_[32];
 
   DISALLOW_COPY_AND_ASSIGN(CertificateReportingServiceTestHelper);
+};
+
+// Class to test reporting events histogram for CertificateReportingService.
+// We can't use a simple HistogramTester, as we need to wait until test teardown
+// to check the histogram contents. This ensures that all in-flight requests
+// are torn down by the time we check the histograms.
+class EventHistogramTester {
+ public:
+  EventHistogramTester();
+  ~EventHistogramTester();
+
+  void SetExpectedValues(int submitted,
+                         int failed,
+                         int successful,
+                         int dropped);
+
+ private:
+  int submitted_ = 0;
+  int failed_ = 0;
+  int successful_ = 0;
+  int dropped_ = 0;
+  base::HistogramTester histogram_tester_;
 };
 
 }  // namespace certificate_reporting_test_utils
