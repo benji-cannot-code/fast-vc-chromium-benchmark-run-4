7d2ef44f9ce69a659b66019d01c9ee29f10ed4b9
Rakina Zata Amni <rakina@chromium.org>
2017-11-22 08:28:40 +0000

    Add use counter to track window.event usage for V0 Shadow DOM
    window.event should be undefined if an Event's target is in a shadow
    tree, because it is leaking information from inside the shadow tree.
    But we don't know how frequently window.event is used for V0 Shadow DOM.
    
    This change adds a use counter for window.event calls where the event's
    target is in a V0 shadow tree,  so that we will know when usage is low
    and it's safe to remove window.event for V0 Shadow DOM. Support for
    window.event for V1 Shadow DOM is removed in crrev.com/c/773922
    
    Discussion link: https://github.com/whatwg/dom/issues/334
    
    Bug: 779461
    Change-Id: Iadc734ebaeb90f44f832bcbe30f0ac9d0be1cff1
    Reviewed-on: https://chromium-review.googlesource.com/780722
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518567}

--- third_party/WebKit/Source/bindings/core/v8/custom/V8WindowCustom.cpp
+++ third_party/WebKit/Source/bindings/core/v8/custom/V8WindowCustom.cpp
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/ScriptController.h"
 #include "bindings/core/v8/ScriptSourceCode.h"
 #include "bindings/core/v8/V8BindingForCore.h"
+#include "bindings/core/v8/V8Event.h"
 #include "bindings/core/v8/V8EventListener.h"
 #include "bindings/core/v8/V8HTMLCollection.h"
 #include "bindings/core/v8/V8Node.h"
@@ -114,6 +115,22 @@ void V8Window::eventAttributeGetterCustom(
 
   v8::Local<v8::Value> js_event =
       V8PrivateProperty::GetGlobalEvent(isolate).GetOrUndefined(info.Holder());
+
+  // Track usage of window.event when the event's target is inside V0 shadow
+  // tree.
+  // TODO(yukishiino): Make window.event [Replaceable] and simplify the
+  // following IsWrapper/ToImplWithTypeCheck hack.
+  if (V8DOMWrapper::IsWrapper(isolate, js_event)) {
+    if (Event* event = V8Event::ToImplWithTypeCheck(isolate, js_event)) {
+      if (event->target()) {
+        Node* target_node = event->target()->ToNode();
+        if (target_node && target_node->IsInV0ShadowTree()) {
+          UseCounter::Count(CurrentExecutionContext(isolate),
+                            WebFeature::kWindowEventInV0ShadowTree);
+        }
+      }
+    }
+  }
   V8SetReturnValue(info, js_event);
 }
 
