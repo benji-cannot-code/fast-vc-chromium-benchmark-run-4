a79cc0b8eacfdc74152373f3e638d4df4d9c9789
Ian Kilpatrick <ikilpatrick@chromium.org>
2017-11-22 01:37:23 +0000

    [css-paint-api] Restrict paint() function to only parse in secure contexts.
    This is a direct followup to:
    https://chromium-review.googlesource.com/c/chromium/src/+/759112
    
    As per the blink-dev decision:
    https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/Jex3idOld48
    
    Bug: 578252
    Change-Id: I865d25b15581adb688e94788d05f78e0974858c8
    Reviewed-on: https://chromium-review.googlesource.com/780961
    Reviewed-by: nainar <nainar@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518491}

--- third_party/WebKit/Source/core/css/parser/CSSParserContext.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSParserContext.cpp
@@ -191,6 +191,10 @@ const CSSParserContext* StrictCSSParserContext(
   return context;
 }
 
+bool CSSParserContext::IsSecureContext() const {
+  return secure_context_mode_ == SecureContextMode::kSecureContext;
+}
+
 KURL CSSParserContext::CompleteURL(const String& url) const {
   if (url.IsNull())
     return KURL();
