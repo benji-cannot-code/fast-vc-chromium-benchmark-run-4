daec59f64cea3e18040caf6f57e11f6d64b83ff7
cm.sanchi <cm.sanchi@samsung.com>
2017-11-22 03:01:42 +0000

    chrome: Move DuplicateDownloadInfoBarDelegate out of chrome namespace.
    From discussion on chromium-dev, it was decided to remove 'chrome'
    namespace.
    
    TBR=nyquist@chromium.org,jam@chromium.org
    
    Bug: 289619
    Change-Id: I1fad4c4531e249d596372d000b1ef981f9096586
    Reviewed-on: https://chromium-review.googlesource.com/781819
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518513}

--- chrome/browser/android/download/duplicate_download_infobar_delegate.cc
+++ chrome/browser/android/download/duplicate_download_infobar_delegate.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/android/download/duplicate_download_infobar_delegate.h"
 
-namespace chrome {
 namespace android {
 
 bool DuplicateDownloadInfoBarDelegate::IsOfflinePage() const {
@@ -34,4 +33,3 @@ bool DuplicateDownloadInfoBarDelegate::ShouldExpire(
 }
 
 }  // namespace android
-}  // namespace chrome
--- chrome/browser/ui/android/infobars/duplicate_download_infobar.cc
+++ chrome/browser/ui/android/infobars/duplicate_download_infobar.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/android/download/duplicate_download_infobar_delegate.h"
 #include "jni/DuplicateDownloadInfoBar_jni.h"
 
-using chrome::android::DuplicateDownloadInfoBarDelegate;
+using android::DuplicateDownloadInfoBarDelegate;
 
 // static
 std::unique_ptr<infobars::InfoBar> DuplicateDownloadInfoBar::CreateInfoBar(
