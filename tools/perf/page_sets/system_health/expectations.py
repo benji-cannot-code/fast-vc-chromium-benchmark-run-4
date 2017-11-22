9d12ba5915597bc69fb20a6fb61a00bb5c165250
rnephew <rnephew@chromium.org>
2017-11-22 20:45:41 +0000

    [Telemetry] Disable flaky soundcloud benchmark on webview.
    TBR: nednguyen@google.com
    Bug: 787001
    Change-Id: I9af0a363fd6e4c0e62086e62d1afdaef1e8852ce
    Reviewed-on: https://chromium-review.googlesource.com/786291
    Reviewed-by: rnephew <rnephew@chromium.org>
    Commit-Queue: rnephew <rnephew@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518736}

--- tools/perf/page_sets/system_health/expectations.py
+++ tools/perf/page_sets/system_health/expectations.py
@@ -83,6 +83,8 @@ class SystemHealthMobileCommonExpectations(expectations.StoryExpectations):
                       'Webview does not have tabs')
     self.DisableStory('browse:shopping:avito',
                       [expectations.ANDROID_NEXUS6], 'crbug.com/736497')
+    self.DisableStory('load:media:soundcloud',
+                      [expectations.ANDROID_WEBVIEW], 'crbug.com/787001')
 
 
 class SystemHealthMobileMemoryExpectations(expectations.StoryExpectations):
@@ -117,6 +119,8 @@ class SystemHealthMobileMemoryExpectations(expectations.StoryExpectations):
     self.DisableStory('long_running:tools:gmail-background',
                       [expectations.ANDROID_WEBVIEW],
                       'Webview does not have tabs')
+    self.DisableStory('load:media:soundcloud',
+                      [expectations.ANDROID_WEBVIEW], 'crbug.com/787001')
 
 
 class SystemHealthWebviewStartupExpectations(expectations.StoryExpectations):
