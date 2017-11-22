da22be2b2a9e64310826938772e9c73eeec01208
Greg Thompson <grt@chromium.org>
2017-11-22 11:25:10 +0000

    Enable interactive_ui_tests on Windows that now run mostly reliably.
    r516772 was the last of many commits that resolved a number of problems
    that made this test suite flaky on Windows. This CL enables tests that
    had been disabled over time due to flakes, yet now seem to run okay.
    
    BUG=248683,764415
    This CL was uploaded by git cl split.
    
    R=treib@chromium.org
    
    Change-Id: Ib5788e422101becf2c7c834c9d83df1c0a4e4551
    Reviewed-on: https://chromium-review.googlesource.com/785012
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518607}

--- chrome/browser/ui/search/instant_extended_interactive_uitest.cc
+++ chrome/browser/ui/search/instant_extended_interactive_uitest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/strings/utf_string_conversions.h"
+#include "build/build_config.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/search/search.h"
@@ -129,8 +130,15 @@ IN_PROC_BROWSER_TEST_F(InstantExtendedTest, DISABLED_NavigateBackToNTP) {
 }
 
 // Flaky: crbug.com/267119
+#if defined(OS_WIN)
+#define MAYBE_DispatchMVChangeEventWhileNavigatingBackToNTP \
+  DispatchMVChangeEventWhileNavigatingBackToNTP
+#else
+#define MAYBE_DispatchMVChangeEventWhileNavigatingBackToNTP \
+  DISABLED_DispatchMVChangeEventWhileNavigatingBackToNTP
+#endif
 IN_PROC_BROWSER_TEST_F(InstantExtendedTest,
-                       DISABLED_DispatchMVChangeEventWhileNavigatingBackToNTP) {
+                       MAYBE_DispatchMVChangeEventWhileNavigatingBackToNTP) {
   // Setup Instant.
   ASSERT_NO_FATAL_FAILURE(SetupInstant(browser()));
   FocusOmnibox();
