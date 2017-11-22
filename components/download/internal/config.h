3e7b752bacbf004a13d84a26aa261a8739c97af1
Xing Liu <xingliu@chromium.org>
2017-11-22 01:04:55 +0000

    Fix an issue that download should happen when battery is high.
    Currently prefetch download is limited to happen only when the device
    is charging, which reduces the chance for successful prefetch.
    
    Download service should allow battery sensitive download to happen when
    device is not charging but has a higher battery level than the level
    passed in from scheduling params.
    
    Bug: 775996
    Change-Id: Iee7f4210e6bab8074c2feccb4d102f3fac99701f
    Reviewed-on: https://chromium-review.googlesource.com/729357
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518480}

--- components/download/internal/config.h
+++ components/download/internal/config.h
@@ -14,6 +14,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace download {
 
 // Configuration name for max concurrent downloads.
+constexpr char kBatteryQueryIntervalConfig[] = "battery_query_interval_seconds";
+
+// Configuration name for download battery percentage.
+constexpr char kDownloadBatteryPercentageConfig[] =
+    "download_battery_percentage";
+
+// Configuration name for max concurrent downloads.
 constexpr char kMaxConcurrentDownloadsConfig[] = "max_concurrent_downloads";
 
 // Configuration name for maximum running downloads.
@@ -76,6 +83,13 @@ struct Configuration {
   // Create the configuration.
   static std::unique_ptr<Configuration> CreateFromFinch();
   Configuration();
+
+  // An interval to throttle battery status queries.
+  base::TimeDelta battery_query_interval;
+
+  // Minimum battery percentage to start the background task or start the
+  // download when battery requirement is sensitive.
+  int download_battery_percentage;
 
   // The maximum number of downloads the DownloadService can have currently in
   // Active or Paused states.
--- components/download/internal/scheduler/device_status_listener.h
+++ components/download/internal/scheduler/device_status_listener.h
@@ -16,9 +16,57 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace download {
 
+// Helper class to listen to battery changes.
+class BatteryStatusListener : public base::PowerObserver {
+ public:
+  class Observer {
+   public:
+    // Called when device charging state changed.
+    virtual void OnPowerStateChange(bool on_battery_power) = 0;
+
+   protected:
+    virtual ~Observer() {}
+  };
+
+  BatteryStatusListener(const base::TimeDelta& battery_query_interval);
+  ~BatteryStatusListener() override;
+
+  int GetBatteryPercentage();
+  bool IsOnBatteryPower();
+
+  void Start(Observer* observer);
+  void Stop();
+
+ protected:
+  // Platform specific code should override to query the actual battery state.
+  virtual int GetBatteryPercentageInternal();
+
+ private:
+  // Updates battery percentage. Will throttle based on
+  // |battery_query_interval_| when |force| is false.
+  void UpdateBatteryPercentage(bool force);
+
+  // base::PowerObserver implementation.
+  void OnPowerStateChange(bool on_battery_power) override;
+
+  // Cached battery percentage.
+  int battery_percentage_;
+
+  // Interval to throttle battery queries. Cached value will be returned inside
+  // this interval.
+  base::TimeDelta battery_query_interval_;
+
+  // Time stamp to record last battery query.
+  base::Time last_battery_query_;
+
+  Observer* observer_ = nullptr;
+
+  DISALLOW_COPY_AND_ASSIGN(BatteryStatusListener);
+};
+
 // Listens to network and battery status change and notifies the observer.
 class DeviceStatusListener : public NetworkStatusListener::Observer,
-                             public base::PowerObserver {
+                             public BatteryStatusListener::Observer {
  public:
   class Observer {
    public:
@@ -26,14 +74,17 @@ class DeviceStatusListener : public NetworkStatusListener::Observer,
     virtual void OnDeviceStatusChanged(const DeviceStatus& device_status) = 0;
   };
 
-  explicit DeviceStatusListener(const base::TimeDelta& startup_delay,
-                                const base::TimeDelta& online_delay);
+  explicit DeviceStatusListener(
+      const base::TimeDelta& startup_delay,
+      const base::TimeDelta& online_delay,
+      std::unique_ptr<BatteryStatusListener> battery_listener);
   ~DeviceStatusListener() override;
 
   bool is_valid_state() { return is_valid_state_; }
 
-  // Returns the current device status for download scheduling.
-  const DeviceStatus& CurrentDeviceStatus() const;
+  // Returns the current device status for download scheduling. May update
+  // internal device status when called.
+  const DeviceStatus& CurrentDeviceStatus();
 
   // Starts/stops to listen network and battery change events, virtual for
   // testing.
@@ -66,7 +117,7 @@ class DeviceStatusListener : public NetworkStatusListener::Observer,
   void OnNetworkChanged(
       net::NetworkChangeNotifier::ConnectionType type) override;
 
-  // base::PowerObserver implementation.
+  // BatteryStatusListener::Observer implementation.
   void OnPowerStateChange(bool on_battery_power) override;
 
   // Notifies the observer about device status change.
@@ -86,6 +137,9 @@ class DeviceStatusListener : public NetworkStatusListener::Observer,
 
   // Pending network status used to update the current network status.
   NetworkStatus pending_network_status_ = NetworkStatus::DISCONNECTED;
+
+  // Used to listen to battery status.
+  std::unique_ptr<BatteryStatusListener> battery_listener_;
 
   DISALLOW_COPY_AND_ASSIGN(DeviceStatusListener);
 };
