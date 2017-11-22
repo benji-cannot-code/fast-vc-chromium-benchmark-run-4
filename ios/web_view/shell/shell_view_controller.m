5e0cb15d9dae5af60670d0855527691e0b1545e3
John Z Wu <jzw@chromium.org>
2017-11-22 21:03:04 +0000

    Implement Autofill in //ios/web_view shell app.
    Bug: 763797
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I33849590dc0e6b6ebe289f855f79357a942393a4
    Reviewed-on: https://chromium-review.googlesource.com/783797
    Commit-Queue: John Wu <jzw@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518740}

--- ios/web_view/shell/shell_view_controller.m
+++ ios/web_view/shell/shell_view_controller.m
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <MobileCoreServices/MobileCoreServices.h>
 
+#import "ios/web_view/shell/shell_autofill_delegate.h"
 #import "ios/web_view/shell/shell_translation_delegate.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -33,6 +34,8 @@ NSString* const kWebViewShellJavaScriptDialogTextFieldAccessibiltyIdentifier =
 @property(nonatomic, strong) UIBarButtonItem* forwardButton;
 // Toolbar containing navigation buttons and |field|.
 @property(nonatomic, strong) UIToolbar* toolbar;
+// Handles the autofill of the content displayed in |webView|.
+@property(nonatomic, strong) ShellAutofillDelegate* autofillDelegate;
 // Handles the translation of the content displayed in |webView|.
 @property(nonatomic, strong) ShellTranslationDelegate* translationDelegate;
 
@@ -47,6 +50,7 @@ NSString* const kWebViewShellJavaScriptDialogTextFieldAccessibiltyIdentifier =
 
 @implementation ShellViewController
 
+@synthesize autofillDelegate = _autofillDelegate;
 @synthesize backButton = _backButton;
 @synthesize containerView = _containerView;
 @synthesize field = _field;
@@ -252,6 +256,8 @@ NSString* const kWebViewShellJavaScriptDialogTextFieldAccessibiltyIdentifier =
   _webView.UIDelegate = self;
   _translationDelegate = [[ShellTranslationDelegate alloc] init];
   _webView.translationController.delegate = _translationDelegate;
+  _autofillDelegate = [[ShellAutofillDelegate alloc] init];
+  _webView.autofillController.delegate = _autofillDelegate;
 
   [_webView setAutoresizingMask:UIViewAutoresizingFlexibleWidth |
                                 UIViewAutoresizingFlexibleHeight];
