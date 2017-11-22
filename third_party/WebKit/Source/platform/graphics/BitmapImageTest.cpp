2996e3a59ca83d34c1e00b0b9d2d28b7d3ecfe5d
tzik <tzik@chromium.org>
2017-11-22 14:34:31 +0000

    Use base::MakeRefCounted for FakeWebTaskRunner construction
    This allows base::MakeRefCounted() on Blink, and converts FakeWebTaskRunner
    construction to use base::MakeRefCounted(), so that FakeWebTaskRunner
    can switch to the zero-started ref count. That is needed to switch it
    to a subclass of base::SingleThreadTaskRunner.
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Id8701c9cce10eea8a4d1254eee31c9e6ffc8ff5b
    Reviewed-on: https://chromium-review.googlesource.com/778500
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Taiju Tsuiki <tzik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518629}

--- third_party/WebKit/Source/platform/graphics/BitmapImageTest.cpp
+++ third_party/WebKit/Source/platform/graphics/BitmapImageTest.cpp
@@ -472,7 +472,7 @@ TEST_F(BitmapImageTestWithMockDecoder, DontAdvanceToIncompleteFrame) {
   image_->SetData(SharedBuffer::Create("data", sizeof("data")), true);
 
   scoped_refptr<scheduler::FakeWebTaskRunner> task_runner =
-      base::AdoptRef(new scheduler::FakeWebTaskRunner);
+      base::MakeRefCounted<scheduler::FakeWebTaskRunner>();
   image_->SetTaskRunnerForTesting(task_runner);
   task_runner->SetTime(10);
 
@@ -507,7 +507,7 @@ TEST_F(BitmapImageTestWithMockDecoder, FrameSkipTracking) {
   image_->SetData(SharedBuffer::Create("data", sizeof("data")), false);
 
   scoped_refptr<scheduler::FakeWebTaskRunner> task_runner =
-      base::AdoptRef(new scheduler::FakeWebTaskRunner);
+      base::MakeRefCounted<scheduler::FakeWebTaskRunner>();
   image_->SetTaskRunnerForTesting(task_runner);
   task_runner->SetTime(10);
 
