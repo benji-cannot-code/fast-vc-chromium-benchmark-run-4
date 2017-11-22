68ef637d03b367251a64de274b5bdc1662694633
Erik Hendriks <hendriks@chromium.org>
2017-11-22 00:52:09 +0000

    Add missing #include for size_t.
    Bug: 
    Change-Id: Iba89c4fe5003216f4c08047062bb67f0d4eda3bb
    Reviewed-on: https://chromium-review.googlesource.com/783706
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Erik Hendriks <hendriks@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518476}

--- content/browser/webauth/authenticator_utils.h
+++ content/browser/webauth/authenticator_utils.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_BROWSER_WEBAUTH_AUTHENTICATOR_UTILS_H_
 #define CONTENT_BROWSER_WEBAUTH_AUTHENTICATOR_UTILS_H_
 
+#include <stddef.h>
 #include <stdint.h>
 #include <vector>
 
