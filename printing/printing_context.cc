b9fd3b1aa78284489b5017a05135b8d2524cca94
Lei Zhang <thestig@chromium.org>
2017-11-22 18:29:44 +0000

    Change a CHECK to DCHECK in PrintingContext.
    Change-Id: I754717a21029735218f9fdd64eb1c00a78340442
    Reviewed-on: https://chromium-review.googlesource.com/784395
    Reviewed-by: Wei Li <weili@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518687}

--- printing/printing_context.cc
+++ printing/printing_context.cc
@@ -24,7 +24,7 @@ PrintingContext::PrintingContext(Delegate* delegate)
     : delegate_(delegate),
       in_print_job_(false),
       abort_printing_(false) {
-  CHECK(delegate_);
+  DCHECK(delegate_);
 }
 
 PrintingContext::~PrintingContext() {
