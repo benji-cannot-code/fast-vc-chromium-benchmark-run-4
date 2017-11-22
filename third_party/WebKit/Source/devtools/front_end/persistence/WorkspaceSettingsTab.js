90812665857564ec0837d2bf709ba120e95e4fc5
Nathan Bruer <allada@chromium.org>
2017-11-22 02:55:59 +0000

    [Devtools] Added 'Overrides' setting to settings screen
    Adds ability to manage the overrides in devtools in the settings screen.
    
    See: https://imgur.com/a/Sp6gI
    
    R=dgozman,lushnikov,pfeldman
    BUG=760316
    
    Change-Id: I2b53e2df880cb21d3b4f63e63fcca7f6cdd75bc4
    Reviewed-on: https://chromium-review.googlesource.com/780366
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Commit-Queue: Blaise Bruer <allada@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518512}

--- third_party/WebKit/Source/devtools/front_end/persistence/WorkspaceSettingsTab.js
+++ third_party/WebKit/Source/devtools/front_end/persistence/WorkspaceSettingsTab.js
@@ -3,9 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/**
- * @unrestricted
- */
 Persistence.WorkspaceSettingsTab = class extends UI.VBox {
   constructor() {
     super();
@@ -18,9 +15,16 @@ Persistence.WorkspaceSettingsTab = class extends UI.VBox {
                                 .createChild('div', 'settings-tab help-content help-container');
 
     Persistence.isolatedFileSystemManager.addEventListener(
-        Persistence.IsolatedFileSystemManager.Events.FileSystemAdded, this._fileSystemAdded, this);
+        Persistence.IsolatedFileSystemManager.Events.FileSystemAdded,
+        event => this._fileSystemAdded(/** @type {!Persistence.IsolatedFileSystem} */ (event.data)), this);
     Persistence.isolatedFileSystemManager.addEventListener(
-        Persistence.IsolatedFileSystemManager.Events.FileSystemRemoved, this._fileSystemRemoved, this);
+        Persistence.IsolatedFileSystemManager.Events.FileSystemRemoved,
+        event => this._fileSystemRemoved(/** @type {!Persistence.IsolatedFileSystem} */ (event.data)), this);
+    Persistence.networkPersistenceManager.addEventListener(
+        Persistence.NetworkPersistenceManager.Events.ProjectDomainChanged,
+        event => this._overridesProjectChanged(
+            /** @type {!Persistence.FileSystemWorkspaceBinding.FileSystem} */ (event.data)),
+        this);
 
     var folderExcludePatternInput = this._createFolderExcludePatternInput();
     folderExcludePatternInput.classList.add('folder-exclude-pattern');
@@ -86,6 +90,8 @@ Persistence.WorkspaceSettingsTab = class extends UI.VBox {
    * @param {!Persistence.IsolatedFileSystem} fileSystem
    */
   _addItem(fileSystem) {
+    if (Persistence.networkPersistenceManager.projectForFileSystem(fileSystem))
+      return;
     var element = this._renderFileSystem(fileSystem);
     this._elementByPath.set(fileSystem.path(), element);
 
@@ -134,14 +140,17 @@ Persistence.WorkspaceSettingsTab = class extends UI.VBox {
     Persistence.isolatedFileSystemManager.addFileSystem();
   }
 
-  _fileSystemAdded(event) {
-    var fileSystem = /** @type {!Persistence.IsolatedFileSystem} */ (event.data);
+  /**
+   * @param {!Persistence.IsolatedFileSystem} fileSystem
+   */
+  _fileSystemAdded(fileSystem) {
     this._addItem(fileSystem);
   }
 
-  _fileSystemRemoved(event) {
-    var fileSystem = /** @type {!Persistence.IsolatedFileSystem} */ (event.data);
-
+  /**
+   * @param {!Persistence.IsolatedFileSystem} fileSystem
+   */
+  _fileSystemRemoved(fileSystem) {
     var mappingView = this._mappingViewByPath.get(fileSystem.path());
     if (mappingView) {
       mappingView.dispose();
@@ -153,5 +162,16 @@ Persistence.WorkspaceSettingsTab = class extends UI.VBox {
       this._elementByPath.delete(fileSystem.path());
       element.remove();
     }
+  }
+
+  /**
+   * @param {!Persistence.FileSystemWorkspaceBinding.FileSystem} project
+   */
+  _overridesProjectChanged(project) {
+    var fileSystem = Persistence.isolatedFileSystemManager.fileSystem(project.fileSystemPath());
+    if (!fileSystem)
+      return;
+    this._fileSystemRemoved(fileSystem);
+    this._fileSystemAdded(fileSystem);
   }
 };
