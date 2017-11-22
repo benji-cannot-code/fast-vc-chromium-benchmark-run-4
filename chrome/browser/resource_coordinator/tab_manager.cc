69e1745979ea62939c1226952a5c6775349a4e7f
Francois Doray <fdoray@chromium.org>
2017-11-22 18:07:13 +0000

    Remove histogram MultiProfile.DiscardedTabsPerUser.
    Analysis confirms that the number of discards increases with the number
    of logged in users. No action will be taken based on this information.
    
    Analysis: https://docs.google.com/a/google.com/spreadsheets/d/1WQeoTSDXBQ_89RyNU9g63ha6KcqnLIfyypejUldgBpQ/edit?usp=sharing
    
    Note: Multi-profile on ChromeOS is very similar to multi-profile on
    other platforms. The differences are that the "user-switcher" is in
    the bottom-right corner of the screen rather than in the top-left
    corner of every browser window, and that switching users hides all
    windows that belong to other users.
    
    Bug: 775644
    Change-Id: Ic342d6b2d6b96dd0d795f90fb73330a626878f8d
    Reviewed-on: https://chromium-review.googlesource.com/779561
    Reviewed-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Gayane Petrosyan <gayane@chromium.org>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518681}

--- chrome/browser/resource_coordinator/tab_manager.cc
+++ chrome/browser/resource_coordinator/tab_manager.cc
@@ -67,10 +67,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/rect.h"
 
 #if defined(OS_CHROMEOS)
-#include "ash/multi_profile_uma.h"
-#include "ash/shell.h"
 #include "chrome/browser/resource_coordinator/tab_manager_delegate_chromeos.h"
-#include "components/user_manager/user_manager.h"
 #endif
 
 using base::TimeDelta;
@@ -653,14 +650,6 @@ void TabManager::RecordDiscardStatistics() {
   // TODO(jamescook): Maybe incorporate extension count?
   UMA_HISTOGRAM_CUSTOM_COUNTS("Tabs.Discard.TabCount", GetTabCount(), 1, 100,
                               50);
-#if defined(OS_CHROMEOS)
-  // Record the discarded tab in relation to the amount of simultaneously
-  // logged in users.
-  if (ash::Shell::HasInstance()) {
-    ash::MultiProfileUMA::RecordDiscardedTab(
-        user_manager::UserManager::Get()->GetLoggedInUsers().size());
-  }
-#endif
   // TODO(jamescook): If the time stats prove too noisy, then divide up users
   // based on how heavily they use Chrome using tab count as a proxy.
   // Bin into <= 1, <= 2, <= 4, <= 8, etc.
