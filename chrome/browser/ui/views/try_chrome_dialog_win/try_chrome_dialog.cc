66537941494a4e2bdd099b0833bf808516771f91
Greg Thompson <grt@chromium.org>
2017-11-23 12:28:16 +0000

    Make the inactive user toast an always-on-top window.
    BUG=783207
    R=skare@chromium.org
    
    Change-Id: Ic2c8052637225f8fa65795481f8dd48f399423ab
    Reviewed-on: https://chromium-review.googlesource.com/785019
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518904}

--- chrome/browser/ui/views/try_chrome_dialog_win/try_chrome_dialog.cc
+++ chrome/browser/ui/views/try_chrome_dialog_win/try_chrome_dialog.cc
@@ -1146,6 +1146,7 @@ void TryChromeDialog::OnContextInitialized() {
   const gfx::Size preferred = layout->GetPreferredSize(contents_view.get());
   popup_->SetContentsView(contents_view.release());
   popup_->SetBounds(context_->ComputePopupBounds(popup_, preferred));
+  popup_->SetAlwaysOnTop(true);
 
   popup_->ShowInactive();
   delegate_->SetToastLocation(context_->GetToastLocation());
