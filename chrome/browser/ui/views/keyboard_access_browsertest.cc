aae507ff8393bd896a2fc75e0aef922cf54a6081
Greg Thompson <grt@chromium.org>
2017-11-23 13:30:34 +0000

    Enable interactive_ui_tests on Windows that now run mostly reliably.
    r516772 was the last of many commits that resolved a number of problems
    that made this test suite flaky on Windows. This CL enables tests that
    had been disabled over time due to flakes, yet now seem to run okay.
    
    BUG=152938,330735,401226,764415
    This CL was uploaded by git cl split.
    
    R=pkasting@chromium.org
    
    Change-Id: I87b38ec262e7ada41a3ebbcb6c246660ff030371
    Reviewed-on: https://chromium-review.googlesource.com/784970
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518914}

--- chrome/browser/ui/views/keyboard_access_browsertest.cc
+++ chrome/browser/ui/views/keyboard_access_browsertest.cc
@@ -372,8 +372,7 @@ IN_PROC_BROWSER_TEST_F(KeyboardAccessTest,
   TestMenuKeyboardAccess(true, false, true);
 }
 
-IN_PROC_BROWSER_TEST_F(KeyboardAccessTest,
-                       DISABLED_TestSystemMenuWithKeyboard) {
+IN_PROC_BROWSER_TEST_F(KeyboardAccessTest, TestSystemMenuWithKeyboard) {
   TestSystemMenuWithKeyboard();
 }
 #endif
--- chrome/browser/ui/views/menu_view_drag_and_drop_test.cc
+++ chrome/browser/ui/views/menu_view_drag_and_drop_test.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/strings/utf_string_conversions.h"
+#include "build/build_config.h"
 #include "chrome/browser/ui/views/menu_test_base.h"
 #include "chrome/test/base/interactive_test_utils.h"
 #include "ui/base/dragdrop/drag_drop_types.h"
@@ -342,11 +343,15 @@ void MenuViewDragAndDropTestTestInMenuDrag::Step4() {
 // Test that an in-menu (i.e., entirely implemented in the menu code) closes the
 // menu automatically once the drag is complete, and does not ask the delegate
 // to stay open.
-// Disabled on all platforms for being flaky. Tracked in:
+// Disabled for being flaky. Tracked in:
 // TODO(erg): Fix DND tests on linux_aura. http://crbug.com/163931.
-// Windows: http://crbug.com/401226.
 // TODO(tapted): De-flake and run on Mac. http://crbug.com/449058.
-VIEW_TEST(MenuViewDragAndDropTestTestInMenuDrag, DISABLED_TestInMenuDrag)
+#if defined(OS_WIN)
+#define MAYBE_TestInMenuDrag TestInMenuDrag
+#else
+#define MAYBE_TestInMenuDrag DISABLED_TestInMenuDrag
+#endif
+VIEW_TEST(MenuViewDragAndDropTestTestInMenuDrag, MAYBE_TestInMenuDrag)
 
 class MenuViewDragAndDropTestNestedDrag : public MenuViewDragAndDropTest {
  public:
@@ -444,12 +449,17 @@ void MenuViewDragAndDropTestNestedDrag::Step4() {
 // Test that a nested drag (i.e. one via a child view, and not entirely
 // implemented in menu code) will consult the delegate before closing the view
 // after the drag.
-// Disabled on all platforms for being flaky. Tracked in:
+// Disabled for being flaky. Tracked in:
 // TODO(erg): Fix DND tests on linux_aura. http://crbug.com/163931.
-// Windows: http://crbug.com/401226.
 // TODO(tapted): De-flake and run on Mac. http://crbug.com/449058.
+#if defined(OS_WIN)
+#define MAYBE_MenuViewDragAndDropNestedDrag MenuViewDragAndDropNestedDrag
+#else
+#define MAYBE_MenuViewDragAndDropNestedDrag \
+  DISABLED_MenuViewDragAndDropNestedDrag
+#endif
 VIEW_TEST(MenuViewDragAndDropTestNestedDrag,
-          DISABLED_MenuViewDragAndDropNestedDrag)
+          MAYBE_MenuViewDragAndDropNestedDrag)
 
 class MenuViewDragAndDropForDropStayOpen : public MenuViewDragAndDropTest {
  public:
