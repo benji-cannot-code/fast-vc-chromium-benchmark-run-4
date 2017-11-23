f29c86cb526fb121f5bb308af3554cb46e5fd114
Colin Blundell <blundell@chromium.org>
2017-11-23 16:41:57 +0000

    Remove stale signin includes from //chrome/browser/devtools
    Discovered during analysis of work required to implement Identity Service.
    
    Bug: 654990
    Change-Id: I4d5c493f0f42a2f77e3ea60046bdb5f9c52f50e5
    Reviewed-on: https://chromium-review.googlesource.com/785674
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518961}

--- chrome/browser/devtools/device/devtools_android_bridge.cc
+++ chrome/browser/devtools/device/devtools_android_bridge.cc
@@ -42,8 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/pref_names.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "components/prefs/pref_service.h"
-#include "components/signin/core/browser/profile_oauth2_token_service.h"
-#include "components/signin/core/browser/signin_manager.h"
 #include "content/public/browser/devtools_agent_host.h"
 #include "content/public/browser/devtools_external_agent_proxy.h"
 #include "content/public/browser/devtools_external_agent_proxy_delegate.h"
