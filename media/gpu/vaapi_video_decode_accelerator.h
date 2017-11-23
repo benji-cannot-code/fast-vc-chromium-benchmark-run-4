2bffbef387a1a89ef5e1f1db876bcb20b418e2bd
Miguel Casas <mcasas@chromium.org>
2017-11-23 05:11:03 +0000

    vaapi: extract static methods into a VaapiPictureFactory
    This CL creates a new VaapiPictureFactory and moves the static
    methods from VaapiPicture there.  VaapiVideoDecodeAccelerator,
    the only client of the static methods, now uses a fully owned
    instance.
    
    Bug: 784507
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Idbdae95d6833315524568fd1b6ecfb80d170e0b8
    Reviewed-on: https://chromium-review.googlesource.com/767375
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Fredrik Hubinette <hubbe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518853}

--- media/gpu/vaapi_video_decode_accelerator.h
+++ media/gpu/vaapi_video_decode_accelerator.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/gpu/gpu_video_decode_accelerator_helpers.h"
 #include "media/gpu/media_gpu_export.h"
 #include "media/gpu/shared_memory_region.h"
+#include "media/gpu/vaapi/vaapi_picture_factory.h"
 #include "media/gpu/vaapi_wrapper.h"
 #include "media/video/picture.h"
 #include "media/video/video_decode_accelerator.h"
@@ -226,18 +227,10 @@ class MEDIA_GPU_EXPORT VaapiVideoDecodeAccelerator
   using OutputBuffers = base::queue<int32_t>;
   OutputBuffers output_buffers_;
 
+  std::unique_ptr<VaapiPictureFactory> vaapi_picture_factory_;
+
   scoped_refptr<VaapiWrapper> vaapi_wrapper_;
 
-  // TODO(mcasas): Use a VaapiPicture factory instead, https://crbug.com/784507.
-  base::Callback<std::unique_ptr<VaapiPicture>(
-      const scoped_refptr<VaapiWrapper>&,
-      const MakeGLContextCurrentCallback&,
-      const BindGLImageCallback&,
-      int32_t,
-      const gfx::Size&,
-      uint32_t,
-      uint32_t)>
-      create_vaapi_picture_callback_;
   // All allocated Pictures, regardless of their current state. Pictures are
   // allocated once using |create_vaapi_picture_callback_| and destroyed at the
   // end of decode. Comes after |vaapi_wrapper_| to ensure all pictures are
