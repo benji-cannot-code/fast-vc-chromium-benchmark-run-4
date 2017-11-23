8af668add3b982640e407aba84b5cce6ba43e70b
Nicolas Dossou-gbete <dgn@chromium.org>
2017-11-23 13:43:55 +0000

    📱 Add support for classes for method parameters.
    Using generator classes as method parameters allows having a more
    reliable (as it's type checked) and simpler (no parameter triggering
    unused warning, no strings to match parameter and test method, etc.)
    way to define parameters to use for parameterized test methods.
    
    Bug: None
    Change-Id: Ibbf8eba0792aa2175f474c611951c6be3f5ebb19
    Reviewed-on: https://chromium-review.googlesource.com/759036
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: agrieve <agrieve@chromium.org>
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518916}

--- base/test/android/junit/src/org/chromium/base/test/params/ExampleParameterizedTest.java
+++ base/test/android/junit/src/org/chromium/base/test/params/ExampleParameterizedTest.java
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.base.test.params;
 
+import static org.chromium.base.test.params.ParameterAnnotations.MethodParameter;
+import static org.chromium.base.test.params.ParameterAnnotations.UseMethodParameter;
+
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
@@ -12,13 +15,12 @@ import org.junit.rules.MethodRule;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.params.ParameterAnnotations.ClassParameter;
-import org.chromium.base.test.params.ParameterAnnotations.MethodParameter;
-import org.chromium.base.test.params.ParameterAnnotations.UseMethodParameter;
 import org.chromium.base.test.params.ParameterAnnotations.UseMethodParameterAfter;
 import org.chromium.base.test.params.ParameterAnnotations.UseMethodParameterBefore;
+import org.chromium.base.test.params.ParameterAnnotations.UseParameterProvider;
 import org.chromium.base.test.params.ParameterAnnotations.UseRunnerDelegate;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -28,31 +30,29 @@ import java.util.List;
 @UseRunnerDelegate(BlockJUnit4RunnerDelegate.class)
 public class ExampleParameterizedTest {
     @ClassParameter
-    private static List<ParameterSet> sClassParams = new ArrayList<>();
-
-    static {
-        sClassParams.add(new ParameterSet().value("hello", "world").name("HelloWorld"));
-        sClassParams.add(new ParameterSet().value("Xxxx", "Yyyy").name("XxxxYyyy"));
-        sClassParams.add(new ParameterSet().value("aa", "yy").name("AaYy"));
-    }
+    private static List<ParameterSet> sClassParams =
+            Arrays.asList(new ParameterSet().value("hello", "world").name("HelloWorld"),
+                    new ParameterSet().value("Xxxx", "Yyyy").name("XxxxYyyy"),
+                    new ParameterSet().value("aa", "yy").name("AaYy"));
 
     @MethodParameter("A")
-    private static List<ParameterSet> sMethodParamA = new ArrayList<>();
-
-    static {
-        sMethodParamA.add(new ParameterSet().value(1, 2).name("OneTwo"));
-        sMethodParamA.add(new ParameterSet().value(2, 3).name("TwoThree"));
-        sMethodParamA.add(new ParameterSet().value(3, 4).name("ThreeFour"));
-    }
+    private static List<ParameterSet> sMethodParamA =
+            Arrays.asList(new ParameterSet().value(1, 2).name("OneTwo"),
+                    new ParameterSet().value(2, 3).name("TwoThree"),
+                    new ParameterSet().value(3, 4).name("ThreeFour"));
 
     @MethodParameter("B")
-    private static List<ParameterSet> sMethodParamB = new ArrayList<>();
+    private static List<ParameterSet> sMethodParamB =
+            Arrays.asList(new ParameterSet().value("a", "b").name("Ab"),
+                    new ParameterSet().value("b", "c").name("Bc"),
+                    new ParameterSet().value("c", "d").name("Cd"),
+                    new ParameterSet().value("d", "e").name("De"));
 
-    static {
-        sMethodParamB.add(new ParameterSet().value("a", "b").name("Ab"));
-        sMethodParamB.add(new ParameterSet().value("b", "c").name("Bc"));
-        sMethodParamB.add(new ParameterSet().value("c", "d").name("Cd"));
-        sMethodParamB.add(new ParameterSet().value("d", "e").name("De"));
+    public static class MethodParamsA implements ParameterProvider {
+        @Override
+        public List<ParameterSet> getParameters() {
+            return sMethodParamA;
+        }
     }
 
     private String mStringA;
@@ -87,12 +87,18 @@ public class ExampleParameterizedTest {
         mSum = null;
     }
 
+    @Test
+    @UseParameterProvider(MethodParamsA.class)
+    public void testWithOnlyAFromGenerator(int intA, int intB) {
+        Assert.assertEquals(intA + 1, intB);
+    }
+
     private String mConcatenation;
 
     @Test
     @UseMethodParameter("B")
     public void testWithOnlyB(String a, String b) {
-        Assert.assertTrue(a != b);
+        Assert.assertTrue(!a.equals(b));
         mConcatenation = a + b;
     }
 
