3492c927f37ac48e5dd7c65ea2fbf983b13e84e7
Vadym Doroshenko <dvadym@chromium.org>
2017-11-23 17:54:46 +0000

    [IOS Password Manager] password-manager-internals implementation.
    chrome://password-manager-internals is Password Manager logs, that's
    very convenient for debugging. This CL implements IOS WebUI for showing
    the chrome://password-manager-internals on IOS as on other platforms.
    Logs from browser part of PasswordManager are already showed. Logs from
    ios/chrome/browser part of PasswordManager will be implemented in the
    following CLs.
    
    Bug: 775894
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: If8c9bb20a8e1f4de678a4d628fe6de34b12c8770
    Reviewed-on: https://chromium-review.googlesource.com/735479
    Commit-Queue: Vadym Doroshenko <dvadym@chromium.org>
    Reviewed-by: Vaclav Brozek <vabr@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518978}

--- ios/chrome/browser/passwords/ios_chrome_password_manager_client.h
+++ ios/chrome/browser/passwords/ios_chrome_password_manager_client.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ios {
 class ChromeBrowserState;
+class LogManager;
 }
 
 namespace password_manager {
@@ -87,6 +88,7 @@ class IOSChromePasswordManagerClient
   const GURL& GetLastCommittedEntryURL() const override;
   const password_manager::CredentialsFilter* GetStoreResultFilter()
       const override;
+  const password_manager::LogManager* GetLogManager() const override;
   ukm::UkmRecorder* GetUkmRecorder() override;
   ukm::SourceId GetUkmSourceId() override;
   password_manager::PasswordManagerMetricsRecorder& GetMetricsRecorder()
@@ -104,6 +106,8 @@ class IOSChromePasswordManagerClient
   BooleanPrefMember saving_passwords_enabled_;
 
   const password_manager::SyncCredentialsFilter credentials_filter_;
+
+  std::unique_ptr<password_manager::LogManager> log_manager_;
 
   // The URL to which the ukm_source_id_ was bound.
   GURL ukm_source_url_;
