31c4dd8e4d4f96421d2ac26ce1db9a40109e7eb5
Oskar Sundbom <ossu@chromium.org>
2017-11-23 14:39:07 +0000

    Make SerializeCoreAudioPauseResume the default and remove flag
    Bug: 772410
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Idc6cb3547fb20b41a7f637840034161f2ecf26b2
    Reviewed-on: https://chromium-review.googlesource.com/785211
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Henrik Grunell <grunell@chromium.org>
    Commit-Queue: Oskar Sundbom <ossu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518933}

--- media/audio/mac/audio_manager_mac.cc
+++ media/audio/mac/audio_manager_mac.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/command_line.h"
-#include "base/feature_list.h"
 #include "base/mac/mac_logging.h"
 #include "base/mac/scoped_cftyperef.h"
 #include "base/macros.h"
@@ -895,8 +894,7 @@ AudioParameters AudioManagerMac::GetPreferredOutputStreamParameters(
 
 void AudioManagerMac::InitializeOnAudioThread() {
   DCHECK(GetTaskRunner()->BelongsToCurrentThread());
-  if (base::FeatureList::IsEnabled(kSerializeCoreAudioPauseResume))
-    InitializeCoreAudioDispatchOverride();
+  InitializeCoreAudioDispatchOverride();
   power_observer_.reset(new AudioPowerObserver());
 }
 
