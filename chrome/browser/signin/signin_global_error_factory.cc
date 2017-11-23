4b2de2c28b2f7ee319f13756222fe80915a6738f
Colin Blundell <blundell@chromium.org>
2017-11-23 10:53:05 +0000

    Remove dead signin include from SigninGlobalErrorFactory
    Discovered while analyzing work required to implement Identity Service.
    
    Bug: 654990
    Change-Id: Id50327bbc01394784d33d535cffed8c422b39075
    Reviewed-on: https://chromium-review.googlesource.com/785695
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518884}

--- chrome/browser/signin/signin_global_error_factory.cc
+++ chrome/browser/signin/signin_global_error_factory.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/signin/signin_global_error.h"
 #include "chrome/browser/ui/global_error/global_error_service_factory.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
-#include "components/signin/core/browser/profile_oauth2_token_service.h"
 
 SigninGlobalErrorFactory::SigninGlobalErrorFactory()
     : BrowserContextKeyedServiceFactory(
