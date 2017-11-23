b515064560646b2a23e190454a2527aa1e6ff8fd
Finnur Thorarinsson <finnur@chromium.org>
2017-11-23 15:13:54 +0000

    Windows 10 Native Notifications: Type 'list'.
    Bug: 734095
    Change-Id: I9f5739d94c0d3789fee338033b89fa6266fadbf2
    Reviewed-on: https://chromium-review.googlesource.com/781680
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518938}

--- chrome/browser/notifications/notification_template_builder.h
+++ chrome/browser/notifications/notification_template_builder.h
@@ -24,6 +24,7 @@ class Image;
 namespace message_center {
 struct ButtonInfo;
 class Notification;
+struct NotificationItem;
 }  // namespace message_center
 
 class NotificationImageRetainer;
@@ -88,8 +89,13 @@ class NotificationTemplateBuilder {
   // ATTRIBUTION then |content| is treated as the source that the notification
   // is attributed to.
   void WriteTextElement(const std::string& content, TextType text_type);
+
+  // Writes the <text> element containing the list entries.
+  void WriteItems(const std::vector<message_center::NotificationItem>& items);
+
   // Writes the <image> element for the notification icon.
   void WriteIconElement(const message_center::Notification& notification);
+
   // Writes the <image> element for showing a large image within the
   // notification body.
   void WriteLargeImageElement(const message_center::Notification& notification);
