bd9e9f431d0606282777c4c94cd17ca8b4796e66
Marc Treib <treib@chromium.org>
2017-11-23 17:14:53 +0000

    Local NTP tests cleanup: Move utility functions to test_utils
    This moves SetUserSelectedDefaultSearchProvider and SwitchToFrench
    (renamed to SwitchBrowserLanguageToFrench) from local_ntp_browsertest
    to local_ntp_test_utils.
    
    Bug: 788166
    Change-Id: Iefcef3e0f56c0fdd670860c1fc4532c84c3edfc2
    Reviewed-on: https://chromium-review.googlesource.com/788150
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518968}

--- chrome/browser/ui/search/local_ntp_test_utils.h
+++ chrome/browser/ui/search/local_ntp_test_utils.h
@@ -6,8 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_SEARCH_LOCAL_NTP_TEST_UTILS_H_
 #define CHROME_BROWSER_UI_SEARCH_LOCAL_NTP_TEST_UTILS_H_
 
+#include <string>
+
 class Browser;
 class GURL;
+class Profile;
 
 namespace content {
 class WebContents;
@@ -16,6 +19,13 @@ class WebContents;
 namespace local_ntp_test_utils {
 
 content::WebContents* OpenNewTab(Browser* browser, const GURL& url);
+
+// Switches the browser language to French, and returns true iff successful.
+bool SwitchBrowserLanguageToFrench();
+
+void SetUserSelectedDefaultSearchProvider(Profile* profile,
+                                          const std::string& base_url,
+                                          const std::string& ntp_url);
 
 }  // namespace local_ntp_test_utils
 
