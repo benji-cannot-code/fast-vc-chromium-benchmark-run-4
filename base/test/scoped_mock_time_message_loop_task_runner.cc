285820647b49c6206affdfb168a4837d96ed71bd
tzik <tzik@chromium.org>
2017-11-23 07:26:28 +0000

    Remove no longer needed callback adapter around PostTask and OnceCallback
    This CL removes obsolete base::OnceCallback adapters, that was needed
    before PostTask got OnceCallback support.
    
    Change-Id: I5148111286a08b92c53eda64bbff59aa4d33312d
    Reviewed-on: https://chromium-review.googlesource.com/785430
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Commit-Queue: Taiju Tsuiki <tzik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518871}

--- base/test/scoped_mock_time_message_loop_task_runner.cc
+++ base/test/scoped_mock_time_message_loop_task_runner.cc
@@ -14,13 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 
 namespace base {
-namespace {
-
-void RunOnceClosure(OnceClosure closure) {
-  std::move(closure).Run();
-}
-
-}  // namespace
 
 ScopedMockTimeMessageLoopTaskRunner::ScopedMockTimeMessageLoopTaskRunner()
     : task_runner_(new TestMockTimeTaskRunner),
@@ -36,11 +29,8 @@ ScopedMockTimeMessageLoopTaskRunner::~ScopedMockTimeMessageLoopTaskRunner() {
   DCHECK(previous_task_runner_->RunsTasksInCurrentSequence());
   DCHECK_EQ(task_runner_, ThreadTaskRunnerHandle::Get());
   for (auto& pending_task : task_runner_->TakePendingTasks()) {
-    // TODO(tzik): Remove RunOnceClosure once TaskRunner migrates from Closure
-    // to OnceClosure.
     previous_task_runner_->PostDelayedTask(
-        pending_task.location,
-        BindOnce(&RunOnceClosure, Passed(&pending_task.task)),
+        pending_task.location, std::move(pending_task.task),
         pending_task.GetTimeToRun() - task_runner_->NowTicks());
   }
   MessageLoop::current()->SetTaskRunner(std::move(previous_task_runner_));
