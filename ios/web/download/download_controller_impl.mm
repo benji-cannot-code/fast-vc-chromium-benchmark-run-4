83e07521522708226478894cdab6b12ef4d23325
Eugene But <eugenebut@google.com>
2017-11-23 12:06:34 +0000

    Pass non-const WebState* to OnDownloadCreated.
    Embedder's OnDownloadCreated will use passed web_state to create
    TabHelper object. So web_state has to be non-const to create a non-const
    TabHelper.
    
    Bug: 780646
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I5b8877e6c3683148d056d2d7639cf9ef12d7ec5b
    Reviewed-on: https://chromium-review.googlesource.com/786319
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518897}

--- ios/web/download/download_controller_impl.mm
+++ ios/web/download/download_controller_impl.mm
@@ -47,7 +47,7 @@ DownloadControllerImpl::~DownloadControllerImpl() {
 }
 
 void DownloadControllerImpl::CreateDownloadTask(
-    const WebState* web_state,
+    WebState* web_state,
     NSString* identifier,
     const GURL& original_url,
     const std::string& content_disposition,
