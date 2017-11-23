535ce03d15a0575b7715f3012b50bbcc4f3ba8ea
Darren Shen <shend@chromium.org>
2017-11-23 07:10:22 +0000

    [css-typed-om] Refactor StylePropertyMap.set/append.
    This patch is a follow up of crrev.com/770606. We make set/append non-
    virtual. To override behaviour, subclasses need to implement:
    
        void SetProperty(CSSPropertyID);
        // technically, there should be a SetCustomProperty as well, but
        // we don't support setting custom properties yet.
    
    This reduces duplicate boilerplate code across different style maps.
    
    Note: Most of this change is moving code from InlineStylePropertyMap
    to the base StylePropertyMap class.
    
    Bug: 785132
    Change-Id: I7ee0882846298fd58f7cec60b9f51884da1a8355
    Reviewed-on: https://chromium-review.googlesource.com/770839
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: nainar <nainar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518869}

--- third_party/WebKit/Source/core/css/cssom/InlineStylePropertyMap.h
+++ third_party/WebKit/Source/core/css/cssom/InlineStylePropertyMap.h
@@ -19,14 +19,6 @@ class CORE_EXPORT InlineStylePropertyMap final : public StylePropertyMap {
 
   Vector<String> getProperties() override;
 
-  void set(const ExecutionContext*,
-           CSSPropertyID,
-           HeapVector<CSSStyleValueOrString>&,
-           ExceptionState&) override;
-  void append(const ExecutionContext*,
-              CSSPropertyID,
-              HeapVector<CSSStyleValueOrString>&,
-              ExceptionState&) override;
   void remove(CSSPropertyID, ExceptionState&) override;
 
   virtual void Trace(blink::Visitor* visitor) {
@@ -37,6 +29,7 @@ class CORE_EXPORT InlineStylePropertyMap final : public StylePropertyMap {
  protected:
   const CSSValue* GetProperty(CSSPropertyID) override;
   const CSSValue* GetCustomProperty(AtomicString) override;
+  void SetProperty(CSSPropertyID, const CSSValue*) override;
 
   HeapVector<StylePropertyMapEntry> GetIterationEntries() override;
 
