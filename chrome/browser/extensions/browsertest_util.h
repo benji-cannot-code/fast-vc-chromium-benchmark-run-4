37b75eb1621387c0846c84b6e913cf8e9ac8ffd2
Olga Sharonova <olka@chromium.org>
2017-11-23 12:31:10 +0000

    Revert "extensions: Add methods to install and open apps to browsertest_util"
    This reverts commit cd2919bdcacdeaa313d92157875de30181c875eb.
    
    Reason for revert: Bug 788156 (NativeBindings/MessagingApiTest.Messaging/0 failing on linux-chromeos-rel)
    
    Original change's description:
    > extensions: Add methods to install and open apps to browsertest_util
    > 
    > Refactors code to install and open apps out of ExtensionBrowserTest and
    > into browsertest_util. So that other test suites that need to test Bookmark
    > Apps e.g. RenderViewContextMenuBrowserTest and in the future SSLBrowserTest
    > can use them.
    > 
    > Bug: 787989
    > Change-Id: Ic8436e07c888c72d286352345f5ec134c3e3a640
    > Reviewed-on: https://chromium-review.googlesource.com/786690
    > Reviewed-by: Matt Giuca <mgiuca@chromium.org>
    > Reviewed-by: Ben Wells <benwells@chromium.org>
    > Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    > Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#518882}
    
    TBR=benwells@chromium.org,mgiuca@chromium.org,ortuno@chromium.org,jochen@chromium.org
    
    Change-Id: I4f70853c4e8171db92d6a46b0995b7d31d87ee6f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 787989
    Reviewed-on: https://chromium-review.googlesource.com/787750
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518906}

--- chrome/browser/extensions/browsertest_util.h
+++ chrome/browser/extensions/browsertest_util.h
@@ -8,14 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-class Browser;
 class Profile;
-struct WebApplicationInfo;
 
 namespace extensions {
-
-class Extension;
-
 namespace browsertest_util {
 
 // Waits until |script| calls "window.domAutomationController.send(result)",
@@ -39,12 +34,6 @@ bool ExecuteScriptInBackgroundPageNoWait(Profile* profile,
 // via update urls). The chromeos device setup scripts take care of this in
 // actual production devices, but some tests need to do it manually.
 void CreateAndInitializeLocalCache();
-
-// Installs a Bookmark App into |profile| using |info|.
-const Extension* InstallBookmarkApp(Profile* profile, WebApplicationInfo info);
-
-// Launches a new app window for |app| in |profile|.
-Browser* LaunchAppBrowser(Profile* profile, const Extension* app);
 
 }  // namespace browsertest_util
 }  // namespace extensions
