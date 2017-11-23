51cc9efd82bb2f422a6e5c1f8e959ba5448da918
Xiaocheng Hu <xiaochengh@chromium.org>
2017-11-23 00:33:15 +0000

    Change HasRenderedNonAnonymousDescendantsWithHeight to take const Node*
    Bug: 766448
    Change-Id: I38169a93624b85c7efe033913e92f53791345d50
    Reviewed-on: https://chromium-review.googlesource.com/786633
    Reviewed-by: Ryan Landay <rlanday@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518814}

--- third_party/WebKit/Source/core/editing/VisibleUnits.h
+++ third_party/WebKit/Source/core/editing/VisibleUnits.h
@@ -281,7 +281,7 @@ CORE_EXPORT LocalCaretRect
 LocalCaretRectOfPosition(const PositionWithAffinity&);
 CORE_EXPORT LocalCaretRect
 LocalCaretRectOfPosition(const PositionInFlatTreeWithAffinity&);
-bool HasRenderedNonAnonymousDescendantsWithHeight(LayoutObject*);
+bool HasRenderedNonAnonymousDescendantsWithHeight(const LayoutObject*);
 
 // Returns a hit-tested VisiblePosition for the given point in contents-space
 // coordinates.
