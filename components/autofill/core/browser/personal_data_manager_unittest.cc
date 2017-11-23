80f1808e8704c9a54447d2ede066e1ace159918f
Roger McFarlane <rogerm@chromium.org>
2017-11-23 07:32:22 +0000

    [autofill] Turn on disused address/cc suppression by default
    Bug: 752203, 772008
    Change-Id: I1c0b8f45f392eb70af8a366d1bae64f1477c2d1f
    Reviewed-on: https://chromium-review.googlesource.com/777099
    Commit-Queue: Roger McFarlane <rogerm@chromium.org>
    Reviewed-by: Mathieu Perreault <mathp@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518872}

--- components/autofill/core/browser/personal_data_manager_unittest.cc
+++ components/autofill/core/browser/personal_data_manager_unittest.cc
@@ -1578,9 +1578,6 @@ TEST_F(PersonalDataManagerTest,
 // the input field is empty.
 TEST_F(PersonalDataManagerTest,
        GetProfileSuggestions_SuppressDisusedProfilesOnEmptyField) {
-  base::test::ScopedFeatureList scoped_features;
-  scoped_features.InitAndEnableFeature(kAutofillSuppressDisusedAddresses);
-
   // Set up 2 different profiles.
   AutofillProfile profile1(base::GenerateGUID(), "https://www.example.com");
   test::SetProfileInfo(&profile1, "Marion1", "Mitchell", "Morrison",
@@ -1599,6 +1596,9 @@ TEST_F(PersonalDataManagerTest,
   personal_data_->AddProfile(profile2);
 
   ResetPersonalDataManager(USER_MODE_NORMAL);
+
+  base::test::ScopedFeatureList scoped_features;
+  scoped_features.InitAndEnableFeature(kAutofillSuppressDisusedAddresses);
 
   // Query with empty string only returns profile2.
   {
@@ -1636,6 +1636,22 @@ TEST_F(PersonalDataManagerTest,
     EXPECT_EQ(
         base::ASCIIToUTF16("456 Zoo St., Second Line, Third line, unit 5"),
         suggestions[0].value);
+  }
+
+  // When suppression is disabled, returns all suggestions.
+  {
+    base::test::ScopedFeatureList scoped_features;
+    scoped_features.InitAndDisableFeature(kAutofillSuppressDisusedAddresses);
+    std::vector<Suggestion> suggestions = personal_data_->GetProfileSuggestions(
+        AutofillType(ADDRESS_HOME_STREET_ADDRESS), base::string16(), false,
+        std::vector<ServerFieldType>());
+    ASSERT_EQ(2U, suggestions.size());
+    EXPECT_EQ(
+        base::ASCIIToUTF16("456 Zoo St., Second Line, Third line, unit 5"),
+        suggestions[0].value);
+    EXPECT_EQ(
+        base::ASCIIToUTF16("123 Zoo St., Second Line, Third line, unit 5"),
+        suggestions[1].value);
   }
 }
 
@@ -2023,12 +2039,6 @@ TEST_F(PersonalDataManagerTest,
   personal_data_->Refresh();
   WaitForOnPersonalDataChanged();
   ASSERT_EQ(4U, personal_data_->GetCreditCards().size());
-
-  // Verify credit card suppression is disabled by default.
-  {
-    ASSERT_FALSE(
-        base::FeatureList::IsEnabled(kAutofillSuppressDisusedCreditCards));
-  }
 
   // Verify no suppression if feature is disabled.
   {
