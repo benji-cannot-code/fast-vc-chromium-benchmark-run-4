8cf1f614c6076de578b91f3ed8767123c2edaf25
Marc Treib <treib@chromium.org>
2017-11-23 16:08:01 +0000

    Local NTP tests cleanup: Move Doodle tests into separate file
    These are independent of the other local NTP tests.
    This also creates local_ntp_test_utils.h/cc which for now contains only
    a single function, but more will follow.
    
    No actual code changes here, just moving stuff around.
    
    Bug: 788166
    Change-Id: I7336b420f82ddf0e71c40ebe2b5f7cfc4f21141f
    Reviewed-on: https://chromium-review.googlesource.com/787890
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518950}

---
+++ chrome/browser/ui/search/local_ntp_test_utils.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_SEARCH_LOCAL_NTP_TEST_UTILS_H_
+#define CHROME_BROWSER_UI_SEARCH_LOCAL_NTP_TEST_UTILS_H_
+
+class Browser;
+class GURL;
+
+namespace content {
+class WebContents;
+}  // namespace content
+
+namespace local_ntp_test_utils {
+
+content::WebContents* OpenNewTab(Browser* browser, const GURL& url);
+
+}  // namespace local_ntp_test_utils
+
+#endif  // CHROME_BROWSER_UI_SEARCH_LOCAL_NTP_TEST_UTILS_H_
