952159da26bf11db3f21a40b64e21405dd84b138
Christopher Cameron <ccameron@chromium.org>
2017-11-23 03:48:11 +0000

    Separate out platform specific ICCProfile uses
    Wrap the ScreenInfo::icc_profile in a OS_MACOSX guard, since it
    is not used (nor will be used) on other platforms.
    
    Split ColorSpace::GetICCProfile into three separate functions for
    the three different contexts in which it is needed
    - ICCProfile::FromCacheMac for the Mac-specific power issues
    - ICCProfile::FromParametricColorSpace for generating profiles
    - ICCProfile::GetSkColorSpaceFromId for building LUTS
    
    Give all three instances separate caches, since they don't need
    to interact with each other (and trying to unify them creates
    unpredictable behavior).
    
    Enable histogramming of empty profiles (since they were not included
    in UMAs before).
    
    Bug: 787418, 766736
    Change-Id: I6794284fd3ca44d1a4667a8ce0f104948bdd596a
    Reviewed-on: https://chromium-review.googlesource.com/783659
    Commit-Queue: ccameron <ccameron@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518842}

--- ui/gfx/icc_profile.h
+++ ui/gfx/icc_profile.h
@@ -16,10 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size);
 
-namespace mojo {
-template <typename, typename> struct StructTraits;
-}
-
 namespace gfx {
 
 // Used to represent a full ICC profile, usually retrieved from a monitor. It
@@ -46,6 +42,16 @@ class COLOR_SPACE_EXPORT ICCProfile {
   // processes).
   static ICCProfile FromData(const void* icc_profile, size_t size);
 
+  // Create a profile for a parametric color space. Returns an invalid profile
+  // if the specified space is not parametric.
+  static ICCProfile FromParametricColorSpace(
+      const gfx::ColorSpace& color_space);
+
+  // Return the ICCProfile that was used to create the specified ColorSpace, if
+  // available. Otherwise, return an invalid profile. This is used on macOS for
+  // power reasons.
+  static ICCProfile FromCacheMac(const gfx::ColorSpace& color_space);
+
   // Return a ColorSpace that references this ICCProfile. ColorTransforms
   // created using this ColorSpace will match this ICCProfile precisely.
   ColorSpace GetColorSpace() const;
@@ -55,6 +61,9 @@ class COLOR_SPACE_EXPORT ICCProfile {
   // if the parametric approximation is almost exact.
   ColorSpace GetParametricColorSpace() const;
 
+  // Return the data for the profile.
+  std::vector<char> GetData() const;
+
   // Histogram how we this was approximated by a gfx::ColorSpace. Only
   // histogram a given profile once per display.
   void HistogramDisplay(int64_t display_id) const;
@@ -63,6 +72,9 @@ class COLOR_SPACE_EXPORT ICCProfile {
   // This method is used to hard-code the |id_| to a specific value. It is used
   // only when a profile is received via IPC.
   static ICCProfile FromDataWithId(const void* data, size_t size, uint64_t id);
+
+  // This method is used by ColorTransform to construct LUT based transforms.
+  static sk_sp<SkColorSpace> GetSkColorSpaceFromId(uint64_t id);
 
   class Internals : public base::RefCountedThreadSafe<ICCProfile::Internals> {
    public:
@@ -81,13 +93,14 @@ class COLOR_SPACE_EXPORT ICCProfile {
       kICCFailedToCreateXform = 7,
       kICCFailedToApproximateTrFnAccurately = 8,
       kICCExtractedSRGBColorSpace = 9,
-      kICCProfileAnalyzeLast = kICCExtractedSRGBColorSpace,
+      kICCNoProfile = 10,
+      kICCProfileAnalyzeLast = kICCNoProfile,
     };
 
     const std::vector<char> data_;
 
     // The result of attepting to extract a color space from the color profile.
-    AnalyzeResult analyze_result_ = kICCFailedToParse;
+    AnalyzeResult analyze_result_ = kICCNoProfile;
 
     // True iff we can create a valid ColorSpace (and ColorTransform) from this
     // object. The transform may be LUT-based (using an SkColorSpaceXform to
