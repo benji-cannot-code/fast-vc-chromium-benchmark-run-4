3e2119ca44afa524cd4a3ea365cead0682ec545c
John Abd-El-Malek <jam@chromium.org>
2017-11-23 18:54:41 +0000

    Stop using deprecated WebContentsObserver::DidGetRedirectForResourceRequest in identity code.
    This will go away in favor of DidRedirectNavigation, since the former doesn't work with network service.
    
    BUG=787891
    
    Change-Id: Icd94e27b601088c837c28df8d7f6d2f604675ecd
    Reviewed-on: https://chromium-review.googlesource.com/786273
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518996}

--- chrome/browser/extensions/api/identity/web_auth_flow.cc
+++ chrome/browser/extensions/api/identity/web_auth_flow.cc
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_source.h"
 #include "content/public/browser/notification_types.h"
 #include "content/public/browser/render_frame_host.h"
-#include "content/public/browser/resource_request_details.h"
 #include "content/public/browser/web_contents.h"
 #include "crypto/random.h"
 #include "extensions/browser/app_window/app_window.h"
@@ -183,11 +182,6 @@ void WebAuthFlow::RenderProcessGone(base::TerminationStatus status) {
     delegate_->OnAuthFlowFailure(WebAuthFlow::WINDOW_CLOSED);
 }
 
-void WebAuthFlow::DidGetRedirectForResourceRequest(
-    const content::ResourceRedirectDetails& details) {
-  BeforeUrlLoaded(details.new_url);
-}
-
 void WebAuthFlow::TitleWasSet(content::NavigationEntry* entry) {
   if (delegate_)
     delegate_->OnAuthFlowTitleChange(base::UTF16ToUTF8(entry->GetTitle()));
@@ -201,6 +195,11 @@ void WebAuthFlow::DidStartNavigation(
     content::NavigationHandle* navigation_handle) {
   if (navigation_handle->IsInMainFrame())
     BeforeUrlLoaded(navigation_handle->GetURL());
+}
+
+void WebAuthFlow::DidRedirectNavigation(
+    content::NavigationHandle* navigation_handle) {
+  BeforeUrlLoaded(navigation_handle->GetURL());
 }
 
 void WebAuthFlow::DidFinishNavigation(
