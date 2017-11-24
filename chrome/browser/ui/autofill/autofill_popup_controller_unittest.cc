ff00f9da51b64a88054b2b60c6f32b0752d2f3eb
Christos Froussios <cfroussios@chromium.org>
2017-11-24 15:38:03 +0000

    🔐 Clear username/password preview when hidding the popup
    In some cases, the autofill preview stays, making the field appear
    uneditable for the user.
    
    Bug: 783716
    Change-Id: I60e156993ffaa6ead935ca45eec05c01cb70cb2a
    Reviewed-on: https://chromium-review.googlesource.com/789071
    Reviewed-by: Mathieu Perreault <mathp@chromium.org>
    Commit-Queue: Christos Froussios <cfroussios@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519120}

--- chrome/browser/ui/autofill/autofill_popup_controller_unittest.cc
+++ chrome/browser/ui/autofill/autofill_popup_controller_unittest.cc
@@ -35,6 +35,7 @@ using ::testing::_;
 using ::testing::AtLeast;
 using ::testing::Mock;
 using ::testing::NiceMock;
+using ::testing::StrictMock;
 using base::ASCIIToUTF16;
 using base::WeakPtr;
 
@@ -53,10 +54,11 @@ class MockAutofillExternalDelegate : public AutofillExternalDelegate {
   bool RemoveSuggestion(const base::string16& value, int identifier) override {
     return true;
   }
-  void ClearPreviewedForm() override {}
   base::WeakPtr<AutofillExternalDelegate> GetWeakPtr() {
     return AutofillExternalDelegate::GetWeakPtr();
   }
+
+  MOCK_METHOD0(ClearPreviewedForm, void());
 };
 
 class MockAutofillClient : public autofill::TestAutofillClient {
@@ -503,6 +505,24 @@ TEST_F(AutofillPopupControllerUnitTest, ProperlyResetController) {
           NULL, gfx::RectF(), base::i18n::UNKNOWN_DIRECTION);
   EXPECT_FALSE(controller->selected_line());
   EXPECT_EQ(0, controller->GetLineCount());
+}
+
+TEST_F(AutofillPopupControllerUnitTest, HidingClearsPreview) {
+  // Create a new controller, because hiding destroys it and we can't destroy it
+  // twice.
+  ContentAutofillDriverFactory* factory =
+      ContentAutofillDriverFactory::FromWebContents(web_contents());
+  ContentAutofillDriver* driver =
+      factory->DriverForFrame(web_contents()->GetMainFrame());
+  StrictMock<MockAutofillExternalDelegate> delegate(driver->autofill_manager(),
+                                                    driver);
+  StrictMock<TestAutofillPopupController>* test_controller =
+      new StrictMock<TestAutofillPopupController>(delegate.GetWeakPtr(),
+                                                  gfx::RectF());
+
+  EXPECT_CALL(delegate, ClearPreviewedForm());
+  // Hide() also deletes the object itself.
+  test_controller->DoHide();
 }
 
 #if !defined(OS_ANDROID)
