b3b3cfce730bc2f635fb595327b4fb9cabe32b80
Sadrul Habib Chowdhury <sadrul@chromium.org>
2017-11-24 19:11:45 +0000

    glx: Fix shutting down.
    Make sure the global states for glx are cleared during shutdown.
    Fix the flaky test in services/ui by shutting down glx after a
    test.
    
    BUG=787871
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I957f19255eca380297bd01272bcf6790a9ec1f36
    Reviewed-on: https://chromium-review.googlesource.com/785991
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519151}

--- services/ui/ws/gpu_host_unittest.cc
+++ services/ui/ws/gpu_host_unittest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/ui/public/interfaces/gpu.mojom.h"
 #include "services/ui/ws/gpu_client.h"
 #include "services/ui/ws/gpu_host_delegate.h"
+#include "ui/gl/init/gl_factory.h"
 
 #if defined(USE_X11)
 #include <X11/Xlib.h>
@@ -70,7 +71,10 @@ class GpuHostTest : public testing::Test {
     CHECK(io_thread_.Start());
     gpu_service_ = std::make_unique<TestGpuService>(io_thread_.task_runner());
   }
-  ~GpuHostTest() override {}
+  ~GpuHostTest() override {
+    gpu_service_ = nullptr;
+    io_thread_.Stop();
+  }
 
   GpuHost* gpu_host() { return gpu_host_.get(); }
 
@@ -79,6 +83,7 @@ class GpuHostTest : public testing::Test {
 
   // testing::Test
   void SetUp() override;
+  void TearDown() override;
 
  private:
   base::MessageLoop message_loop_;
@@ -108,6 +113,12 @@ void GpuHostTest::SetUp() {
   gpu_host_ = std::make_unique<DefaultGpuHost>(&gpu_host_delegate_, nullptr);
   gpu_service_->Bind(mojo::MakeRequest(&gpu_service_ptr_));
   gpu_host_->gpu_service_ = std::move(gpu_service_ptr_);
+}
+
+void GpuHostTest::TearDown() {
+  gpu_host_ = nullptr;
+  gl::init::ShutdownGL();
+  testing::Test::TearDown();
 }
 
 // Tests to verify, that if a GpuHost is deleted before GpuClient receives a
--- ui/gl/init/gl_initializer_x11.cc
+++ ui/gl/init/gl_initializer_x11.cc
@@ -218,6 +218,7 @@ void InitializeDebugGLBindings() {
 
 void ShutdownGLPlatform() {
   GLSurfaceEGL::ShutdownOneOff();
+  GLSurfaceGLX::ShutdownOneOff();
   ClearBindingsEGL();
   ClearBindingsGL();
   ClearBindingsGLX();
