6e734826e281454b26fa6f962e75f3479d4defb1
Yuki Shiino <yukishiino@chromium.org>
2017-11-24 08:06:01 +0000

    v8binding: Fixes the argument type of FileCallback.
    It turns out that Blink has already been passing a File object
    into FileCallbacks.  So this patch simply changes the argument
    type from Blob to File with no behavioral change.
    
    Bug: 
    Change-Id: Ic8212cfc07e977a805a1b5cd65ff8b77f8337daa
    Reviewed-on: https://chromium-review.googlesource.com/785553
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519065}

--- third_party/WebKit/Source/modules/filesystem/FileCallback.h
+++ third_party/WebKit/Source/modules/filesystem/FileCallback.h
@@ -10,13 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class Blob;
+class File;
 
 class FileCallback : public GarbageCollectedFinalized<FileCallback> {
  public:
   virtual ~FileCallback() = default;
   virtual void Trace(blink::Visitor* visitor) {}
-  virtual void handleEvent(Blob*) = 0;
+  virtual void handleEvent(File*) = 0;
 };
 
 }  // namespace blink
