8611b9066143534ecb42f368bd6ac26b6029fa16
Tibor Goldschwendt <tiborg@chromium.org>
2017-11-24 18:38:26 +0000

    [vr] Register VR assets component if user has paired a DD headset
    Bug: 706150
    Change-Id: Icc0c1983c759f102448750640a7084e363e22253
    Reviewed-on: https://chromium-review.googlesource.com/782664
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519148}

--- chrome/browser/android/vr_shell/vr_shell_delegate.cc
+++ chrome/browser/android/vr_shell/vr_shell_delegate.cc
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_helpers.h"
 #include "chrome/browser/android/vr_shell/vr_metrics_util.h"
 #include "chrome/browser/android/vr_shell/vr_shell.h"
+#include "chrome/browser/browser_process.h"
+#include "chrome/browser/component_updater/vr_assets_component_installer.h"
 #include "chrome/browser/vr/service/vr_device_manager.h"
 #include "chrome/browser/vr/service/vr_service_impl.h"
 #include "content/public/browser/webvr_service_provider.h"
@@ -274,6 +276,13 @@ static void JNI_VrShellDelegate_OnLibraryAvailable(
     const JavaParamRef<jclass>& clazz) {
   device::GvrDelegateProviderFactory::Install(
       new VrShellDelegateProviderFactory);
+}
+
+static void JNI_VrShellDelegate_RegisterVrAssetsComponent(
+    JNIEnv* env,
+    const JavaParamRef<jclass>& clazz) {
+  component_updater::RegisterVrAssetsComponent(
+      g_browser_process->component_updater());
 }
 
 }  // namespace vr_shell
