d8d779607cce417a138eb5b3a32a9d1d1259c95a
Michael Thiessen <mthiesse@chromium.org>
2017-11-24 16:23:01 +0000

    VR: Remove WeakPtrFactory from vr::BindingBase
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I5662511969a226e44daccb839121d6232a91e871
    Reviewed-on: https://chromium-review.googlesource.com/788141
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519126}

--- chrome/browser/vr/databinding/binding_unittest.cc
+++ chrome/browser/vr/databinding/binding_unittest.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/vr/databinding/binding.h"
 
 #include "base/memory/ptr_util.h"
-#include "base/memory/weak_ptr.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace vr {
@@ -53,21 +52,6 @@ TEST(Binding, BoundBool) {
   b.binding->Update();
   EXPECT_EQ(false, a.value);
   EXPECT_EQ(true, b.value);
-}
-
-TEST(Binding, Lifetime) {
-  base::WeakPtr<BindingBase> binding;
-  {
-    TestModel a;
-    TestView b;
-    b.binding = VR_BIND_FIELD(bool, TestModel, &a, value, TestView, &b, value);
-    binding = b.binding->GetWeakPtr();
-  }
-  // This test is not particularly useful, since we're just testing the behavior
-  // of base::WeakPtr, but it confirms that when an object owning a binding
-  // falls out of scope, weak pointers to its bindings are correctly
-  // invalidated.
-  EXPECT_FALSE(!!binding);
 }
 
 }  // namespace vr
