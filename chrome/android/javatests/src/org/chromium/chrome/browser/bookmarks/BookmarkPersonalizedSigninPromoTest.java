49a754ebe50dd5103ff402a61fe1fce5789773f4
Peter Kotwicz <pkotwicz@chromium.org>
2017-11-26 00:19:23 +0000

    [Android] Make BookmarkPersonalizedSigninPromoTest tests less flaky
    This CL programmatically opens the bookmark manager instead of opening it
    through the app menu via Espresso. This seems to decrease the test flakiness.
    
    BUG=778283
    
    Change-Id: Icbcb813bc857f34731f5ee59c793fa6a16e5a202
    Reviewed-on: https://chromium-review.googlesource.com/789630
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519192}

--- chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkPersonalizedSigninPromoTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkPersonalizedSigninPromoTest.java
@@ -12,7 +12,6 @@ import static android.support.test.espresso.assertion.ViewAssertions.doesNotExis
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertFalse;
@@ -33,7 +32,6 @@ import org.junit.runner.RunWith;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.DisableIf;
-import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.browser.ChromeSwitches;
@@ -77,7 +75,6 @@ public class BookmarkPersonalizedSigninPromoTest {
 
     @Test
     @MediumTest
-    @RetryOnFailure(message = "crbug.com/778283")
     public void testManualDismissPromo() throws Exception {
         openBookmarkManager();
         onView(withId(R.id.signin_promo_view_container)).check(matches(isDisplayed()));
@@ -142,7 +139,6 @@ public class BookmarkPersonalizedSigninPromoTest {
 
     @Test
     @MediumTest
-    @RetryOnFailure(message = "crbug.com/778283")
     public void testSigninButtonNewAccount() throws Exception {
         openBookmarkManager();
         onView(withId(R.id.signin_promo_view_container)).check(matches(isDisplayed()));
@@ -163,8 +159,7 @@ public class BookmarkPersonalizedSigninPromoTest {
     }
 
     private void openBookmarkManager() throws InterruptedException {
-        onView(withId(R.id.menu_button)).perform(click());
-        onView(withText("Bookmarks")).perform(click());
+        BookmarkUtils.showBookmarkManager((ChromeActivity) mActivityTestRule.getActivity());
     }
 
     private void addTestAccount() {
