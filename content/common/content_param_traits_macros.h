a170fb04e0a6745c251d022f2c5f47fb98eff021
John Abd-El-Malek <jam@chromium.org>
2017-11-27 18:50:41 +0000

    Convert ClientSideDetectionHost to work with the network service.
    Instead of observing a ResourceDispatcherHostDelegate (which isn't used by the network service), monitor frame requests in the browser and subresource requests from the renderer.
    
    A follow-up cl will remove the callbacks from ResourceDispatcherHostDelegate & WebContentsObserver.
    
    Bug: 783981
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I0130bd5d9250314a36e3dabd7d2053fa16a45ddd
    Reviewed-on: https://chromium-review.googlesource.com/784053
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Jialiu Lin <jialiul@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519351}

--- content/common/content_param_traits_macros.h
+++ content/common/content_param_traits_macros.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/input_event_ack_state.h"
 #include "content/public/common/request_context_frame_type.h"
 #include "content/public/common/request_context_type.h"
-#include "content/public/common/resource_type.h"
 #include "ipc/ipc_message_macros.h"
 #include "third_party/WebKit/common/page/page_visibility_state.mojom.h"
 #include "third_party/WebKit/public/platform/WebAddressSpace.h"
@@ -31,8 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 IPC_ENUM_TRAITS_MAX_VALUE(content::InputEventAckState,
                           content::INPUT_EVENT_ACK_STATE_MAX)
-IPC_ENUM_TRAITS_MAX_VALUE(content::ResourceType,
-                          content::RESOURCE_TYPE_LAST_TYPE - 1)
 IPC_ENUM_TRAITS_MAX_VALUE(content::RequestContextType,
                           content::REQUEST_CONTEXT_TYPE_LAST)
 IPC_ENUM_TRAITS_MAX_VALUE(content::RequestContextFrameType,
