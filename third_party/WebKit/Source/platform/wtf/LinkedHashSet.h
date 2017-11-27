76bb59b66fb78b284d15fb2f227b3375ff400ed7
Chris Watkins <watk@chromium.org>
2017-11-27 07:38:33 +0000

    Update the docs on {List,Linked}HashSet
    This clarifies the differences between the containers and encourages
    people to prefer LinkedHashSet.
    
    Bug: 614112
    Change-Id: Iccdb6c16ee38392a8958d3d04b8298e348b49d1c
    Reviewed-on: https://chromium-review.googlesource.com/786931
    Reviewed-by: Yuta Kitamura <yutak@chromium.org>
    Commit-Queue: Yuta Kitamura <yutak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519238}

--- third_party/WebKit/Source/platform/wtf/LinkedHashSet.h
+++ third_party/WebKit/Source/platform/wtf/LinkedHashSet.h
@@ -30,15 +30,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WTF {
 
-// LinkedHashSet: Just like HashSet, this class provides a Set
-// interface - a collection of unique objects with O(1) insertion,
-// removal and test for containership. However, it also has an
-// order - iterating it will always give back values in the order
-// in which they are added.
-
-// Unlike ListHashSet, but like most WTF collections, iteration is NOT safe
-// against mutation of the LinkedHashSet.
-
+// LinkedHashSet provides a Set interface like HashSet, but also has a
+// predictable iteration order. It has O(1) insertion, removal, and test for
+// containership. It maintains a linked list through its contents such that
+// iterating it yields values in the order in which they were inserted.
+//
+// LinkedHashSet iterators are invalidated by mutation of the set. This means,
+// for example, that you cannot modify the container while iterating
+// over it (this will DCHECK in debug). Instead, you should either copy the
+// entries to a vector before iterating, or maintain a separate list of pending
+// updates.
+//
+// Unlike ListHashSet, this container supports WeakMember<T>.
 template <typename Value,
           typename HashFunctions,
           typename HashTraits,
