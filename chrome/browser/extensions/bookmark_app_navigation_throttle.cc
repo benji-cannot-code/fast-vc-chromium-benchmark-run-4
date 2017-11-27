351f35b7cee42160ed2255dcc719b8e84678627f
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2017-11-27 08:06:23 +0000

    desktop-pwas: Don't intercept links to apps that open in a tab
    When users install a Desktop PWA, the app will open in a window by
    default. User can still change the launch container in chrome://apps or
    by right clicking the app icon on Chrome OS. When users change the launch
    container to tab, we stop intercepting links for that app.
    
    Bug: 771496
    Change-Id: Ie0bd0c7883afbbfffbd94bc577665865699fca6e
    Reviewed-on: https://chromium-review.googlesource.com/789731
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Reviewed-by: Matt Giuca <mgiuca@chromium.org>
    Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519241}

--- chrome/browser/extensions/bookmark_app_navigation_throttle.cc
+++ chrome/browser/extensions/bookmark_app_navigation_throttle.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/logging.h"
 #include "base/threading/thread_task_runner_handle.h"
+#include "chrome/browser/extensions/launch_util.h"
 #include "chrome/browser/prerender/prerender_contents.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
@@ -24,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents.h"
+#include "extensions/browser/extension_prefs.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/common/constants.h"
 #include "extensions/common/extension.h"
@@ -37,13 +39,26 @@ namespace extensions {
 
 namespace {
 
+bool IsWindowedBookmarkApp(const Extension* app,
+                           content::BrowserContext* context) {
+  if (!app || !app->from_bookmark())
+    return false;
+
+  if (GetLaunchContainer(extensions::ExtensionPrefs::Get(context), app) !=
+      LAUNCH_CONTAINER_WINDOW) {
+    return false;
+  }
+
+  return true;
+}
+
 scoped_refptr<const Extension> GetAppForURL(
     const GURL& url,
     const content::WebContents* web_contents) {
-  content::BrowserContext* browser_context = web_contents->GetBrowserContext();
+  content::BrowserContext* context = web_contents->GetBrowserContext();
   for (scoped_refptr<const extensions::Extension> app :
-       ExtensionRegistry::Get(browser_context)->enabled_extensions()) {
-    if (!app->from_bookmark())
+       ExtensionRegistry::Get(context)->enabled_extensions()) {
+    if (!IsWindowedBookmarkApp(app.get(), context))
       continue;
 
     const UrlHandlerInfo* url_handler =
@@ -243,16 +258,16 @@ void BookmarkAppNavigationThrottle::OpenInNewTab() {
 scoped_refptr<const Extension>
 BookmarkAppNavigationThrottle::GetAppForWindow() {
   content::WebContents* source = navigation_handle()->GetWebContents();
+  content::BrowserContext* context = source->GetBrowserContext();
   Browser* browser = chrome::FindBrowserWithWebContents(source);
   if (!browser || !browser->is_app())
     return nullptr;
 
-  const Extension* app =
-      ExtensionRegistry::Get(source->GetBrowserContext())
-          ->GetExtensionById(
-              web_app::GetExtensionIdFromApplicationName(browser->app_name()),
-              extensions::ExtensionRegistry::ENABLED);
-  if (!app || !app->from_bookmark())
+  const Extension* app = ExtensionRegistry::Get(context)->GetExtensionById(
+      web_app::GetExtensionIdFromApplicationName(browser->app_name()),
+      extensions::ExtensionRegistry::ENABLED);
+
+  if (!IsWindowedBookmarkApp(app, context))
     return nullptr;
 
   // Bookmark Apps for installable websites have scope.
--- chrome/browser/extensions/browsertest_util.cc
+++ chrome/browser/extensions/browsertest_util.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "chrome/browser/extensions/bookmark_app_helper.h"
 #include "chrome/browser/extensions/extension_service.h"
+#include "chrome/browser/extensions/launch_util.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
@@ -101,7 +102,14 @@ const Extension* InstallBookmarkApp(Profile* profile, WebApplicationInfo info) {
 
   EXPECT_EQ(++num_extensions,
             ExtensionRegistry::Get(profile)->enabled_extensions().size());
-  return content::Details<const Extension>(windowed_observer.details()).ptr();
+  const Extension* app =
+      content::Details<const Extension>(windowed_observer.details()).ptr();
+  extensions::SetLaunchType(profile, app->id(),
+                            info.open_as_window
+                                ? extensions::LAUNCH_TYPE_WINDOW
+                                : extensions::LAUNCH_TYPE_REGULAR);
+
+  return app;
 }
 
 Browser* LaunchAppBrowser(Profile* profile, const Extension* extension_app) {
