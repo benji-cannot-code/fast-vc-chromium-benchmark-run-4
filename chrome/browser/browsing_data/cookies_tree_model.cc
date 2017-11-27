8fbf8ab3e3e6f873dd741b25b19c0ab659a31c63
Elly Fong-Jones <ellyjones@chromium.org>
2017-11-27 14:35:28 +0000

    chrome: update cookie icon in cookies dialog
    This change moves the collected cookies dialog from the
    colorful pre-MD icon to the monochrome MD icon, and deletes
    the now-unused old icon resource.
    
    Bug: 610428
    Change-Id: I71638b4ec27e17b94472fa6286949b9e50a6d09b
    Reviewed-on: https://chromium-review.googlesource.com/757056
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519296}

--- chrome/browser/browsing_data/cookies_tree_model.cc
+++ chrome/browser/browsing_data/cookies_tree_model.cc
@@ -1063,7 +1063,7 @@ void CookiesTreeModel::GetIcons(std::vector<gfx::ImageSkia>* icons) {
                         .GetNativeImageNamed(IDR_DEFAULT_FAVICON)
                         .ToImageSkia());
   icons->push_back(*ui::ResourceBundle::GetSharedInstance()
-                        .GetNativeImageNamed(IDR_COOKIE_ICON)
+                        .GetNativeImageNamed(IDR_ACCESSED_COOKIES)
                         .ToImageSkia());
   icons->push_back(*ui::ResourceBundle::GetSharedInstance()
                         .GetNativeImageNamed(IDR_COOKIE_STORAGE_ICON)
