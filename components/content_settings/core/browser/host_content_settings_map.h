cdfbb377b6da83f9b321d633c07882e958f351dc
Raymes Khoury <raymes@chromium.org>
2017-11-27 05:33:49 +0000

    Add function to HCSM to get the current user-modifiable ContentSetting
    In order for DSE permissions to be properly migrated when the user
    changes DSE, we need to be able to obtain the current user-defined
    content setting even in the presence of enterprise policy or other
    policy which would generally override the user-defined setting. This CL
    adds a function to HCSM to obtain the user-defined setting.
    
    Bug: 780344
    Change-Id: I1cc75489ac92676475165ced0ef1fa233ab6364f
    Reviewed-on: https://chromium-review.googlesource.com/786875
    Commit-Queue: Raymes Khoury <raymes@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519222}

--- components/content_settings/core/browser/host_content_settings_map.h
+++ components/content_settings/core/browser/host_content_settings_map.h
@@ -55,8 +55,7 @@ class HostContentSettingsMap : public content_settings::Observer,
     // EXTENSION names is a layering violation when this class will move to
     // components.
     // TODO(mukai): find the solution.
-    INTERNAL_EXTENSION_PROVIDER = 0,
-    POLICY_PROVIDER,
+    POLICY_PROVIDER = 0,
     SUPERVISED_PROVIDER,
     CUSTOM_EXTENSION_PROVIDER,
     NOTIFICATION_ANDROID_PROVIDER,
@@ -111,6 +110,14 @@ class HostContentSettingsMap : public content_settings::Observer,
   //
   // May be called on any thread.
   ContentSetting GetContentSetting(
+      const GURL& primary_url,
+      const GURL& secondary_url,
+      ContentSettingsType content_type,
+      const std::string& resource_identifier) const;
+
+  // This is the same as GetContentSetting() but ignores providers which are not
+  // user-controllable (e.g. policy and extensions).
+  ContentSetting GetUserModifiableContentSetting(
       const GURL& primary_url,
       const GURL& secondary_url,
       ContentSettingsType content_type,
@@ -341,6 +348,7 @@ class HostContentSettingsMap : public content_settings::Observer,
       const GURL& secondary_url,
       ContentSettingsType content_type,
       const std::string& resource_identifier,
+      ProviderType first_provider_to_search,
       content_settings::SettingInfo* info) const;
 
   content_settings::PatternPair GetNarrowestPatterns(
