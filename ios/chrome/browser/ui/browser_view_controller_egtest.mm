94a12226e161694343b4ac8f805ad6742a8adc42
Mike Baxley <baxley@chromium.org>
2017-11-27 17:11:14 +0000

    Update EarlGrey iPad simulator typing bug references.
    There are duplicate bugs opened for EarlGrey typing on iPad simulator.
    Update the TODOs, so we can close duplicate bugs.
    
    Bug: 640220,703855
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I460d4809795661cb43327c3191bf7e433868b711
    Reviewed-on: https://chromium-review.googlesource.com/790670
    Reviewed-by: Eric Noyau <noyau@chromium.org>
    Commit-Queue: Eric Noyau <noyau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519319}

--- ios/chrome/browser/ui/browser_view_controller_egtest.mm
+++ ios/chrome/browser/ui/browser_view_controller_egtest.mm
@@ -35,9 +35,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Tests that evaluating JavaScript in the omnibox (e.g, a bookmarklet) works.
 - (void)testJavaScriptInOmnibox {
-  // TODO(crbug.com/640220): Keyboard entry inside the omnibox fails only on
-  // iPad
-  // running iOS X.
+  // TODO(crbug.com/703855): Keyboard entry inside the omnibox fails only on
+  // iPad running iOS 10.
   if (IsIPadIdiom() && base::ios::IsRunningOnIOS10OrLater())
     return;
 
