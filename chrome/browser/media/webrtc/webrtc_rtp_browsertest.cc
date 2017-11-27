2dcd881a49695dc5ab95337464f18f42baaadbe0
Peng Huang <penghuang@chromium.org>
2017-11-27 17:46:08 +0000

    Disable two flaky WebRtcRtpBrowserTests
    Disable two flaky tests:
      WebRtcRtpBrowserTest.AddAndRemoveTracksWithoutStream
      WebRtcRtpBrowserTest.AddAndRemoveTracksWithIndividualStreams
    
    TBR=phoglund@chromium.org
    
    Bug: 788760
    Change-Id: I9409c1c1108f361197f86cf66340e5311b5a4e4d
    Reviewed-on: https://chromium-review.googlesource.com/790950
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519331}

--- chrome/browser/media/webrtc/webrtc_rtp_browsertest.cc
+++ chrome/browser/media/webrtc/webrtc_rtp_browsertest.cc
@@ -79,7 +79,8 @@ IN_PROC_BROWSER_TEST_F(WebRtcRtpBrowserTest, GetReceivers) {
   VerifyRtpReceivers(right_tab_, 6);
 }
 
-IN_PROC_BROWSER_TEST_F(WebRtcRtpBrowserTest, AddAndRemoveTracksWithoutStream) {
+IN_PROC_BROWSER_TEST_F(WebRtcRtpBrowserTest,
+                       DISABLED_AddAndRemoveTracksWithoutStream) {
   StartServerAndOpenTabs();
 
   SetupPeerconnectionWithoutLocalStream(left_tab_);
@@ -275,7 +276,7 @@ IN_PROC_BROWSER_TEST_F(WebRtcRtpBrowserTest,
 }
 
 IN_PROC_BROWSER_TEST_F(WebRtcRtpBrowserTest,
-                       AddAndRemoveTracksWithIndividualStreams) {
+                       DISABLED_AddAndRemoveTracksWithIndividualStreams) {
   StartServerAndOpenTabs();
 
   SetupPeerconnectionWithoutLocalStream(left_tab_);
