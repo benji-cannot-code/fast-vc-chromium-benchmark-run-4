f6ecad53a0a3db5bdd22255bffc794b3b471aa48
Yoshifumi Inoue <yosin@chromium.org>
2017-11-27 07:02:53 +0000

    Get rid of unused type alias LayoutObjectMap in NGInlineFragmentIterator class
    This patch gets rid of unused type alias |LayoutObjectMap| in
    |NGInlineFragmentIterator| class for improving code health.
    
    Change-Id: I02c1c3f2d1e31214371cf928b813b8e5302ebfcd
    Reviewed-on: https://chromium-review.googlesource.com/790034
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519233}

--- third_party/WebKit/Source/core/layout/ng/inline/ng_inline_fragment_iterator.h
+++ third_party/WebKit/Source/core/layout/ng/inline/ng_inline_fragment_iterator.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/CoreExport.h"
 #include "core/layout/ng/geometry/ng_physical_offset.h"
 #include "platform/wtf/Allocator.h"
-#include "platform/wtf/HashMap.h"
 #include "platform/wtf/Vector.h"
 
 namespace blink {
@@ -36,7 +35,6 @@ class CORE_EXPORT NGInlineFragmentIterator {
     NGPhysicalOffset offset_to_container_box;
   };
   using Results = Vector<Result, 1>;
-  using LayoutObjectMap = HashMap<const LayoutObject*, Results>;
 
   Results::const_iterator begin() const { return results_.begin(); }
   Results::const_iterator end() const { return results_.end(); }
