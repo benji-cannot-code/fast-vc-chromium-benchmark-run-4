22e881a1ce5429805318ec95ffc9e97512d03a32
Gauthier Ambard <gambard@chromium.org>
2017-11-27 13:05:44 +0000

    Follow up ToolbarButton
    This CL is a followup of crrev.com/c/787914.
    
    Bug: none
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Iff707bc5516c2833e2222f51c670fb320d7f1b2e
    Reviewed-on: https://chromium-review.googlesource.com/790293
    Reviewed-by: Elodie Banel <lod@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519289}

--- ios/chrome/browser/ui/toolbar/clean/toolbar_button.mm
+++ ios/chrome/browser/ui/toolbar/clean/toolbar_button.mm
@@ -83,11 +83,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)setHiddenInCurrentState:(BOOL)hiddenInCurrentState {
-  BOOL wasHidden = _hiddenInCurrentState;
   _hiddenInCurrentState = hiddenInCurrentState;
-  if (wasHidden != _hiddenInCurrentState) {
-    [self setHiddenForCurrentStateAndSizeClass];
-  }
+  [self setHiddenForCurrentStateAndSizeClass];
 }
 
 #pragma mark - Private
