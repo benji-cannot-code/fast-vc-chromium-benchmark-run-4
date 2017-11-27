f5c70738228a0a246d69675b82d385933cdf4ddf
tzik <tzik@chromium.org>
2017-11-27 11:51:38 +0000

    Use TimeTicks and TimeDelta in FakeWebTaskRunner
    This CL updates FakeWebTaskRunner implementation to use TimeDelta and
    TimeTicks instead of double as the time types.
    
    Change-Id: Ic0d142f73f7982b183e239d64d95526a11a19428
    Reviewed-on: https://chromium-review.googlesource.com/790114
    Commit-Queue: Taiju Tsuiki <tzik@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519279}

--- third_party/WebKit/Source/platform/scheduler/test/fake_web_task_runner.h
+++ third_party/WebKit/Source/platform/scheduler/test/fake_web_task_runner.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/macros.h"
 #include "base/memory/scoped_refptr.h"
+#include "base/time/time.h"
 #include "platform/WebTaskRunner.h"
 
 namespace blink {
@@ -21,7 +22,10 @@ class FakeWebTaskRunner : public WebTaskRunner {
  public:
   FakeWebTaskRunner();
 
-  void SetTime(double new_time);
+  void SetTime(base::TimeTicks new_time);
+  void SetTime(double new_time) {
+    SetTime(base::TimeTicks() + base::TimeDelta::FromSecondsD(new_time));
+  }
 
   // WebTaskRunner implementation:
   bool RunsTasksInCurrentSequence() override;
@@ -30,8 +34,13 @@ class FakeWebTaskRunner : public WebTaskRunner {
       override;
 
   void RunUntilIdle();
-  void AdvanceTimeAndRun(double delta_seconds);
-  std::deque<std::pair<base::OnceClosure, double>> TakePendingTasksForTesting();
+  void AdvanceTimeAndRun(base::TimeDelta delta);
+  void AdvanceTimeAndRun(double delta_seconds) {
+    AdvanceTimeAndRun(base::TimeDelta::FromSecondsD(delta_seconds));
+  }
+
+  using PendingTask = std::pair<base::OnceClosure, base::TimeTicks>;
+  std::deque<PendingTask> TakePendingTasksForTesting();
 
  protected:
   bool PostDelayedTask(const base::Location& location,
