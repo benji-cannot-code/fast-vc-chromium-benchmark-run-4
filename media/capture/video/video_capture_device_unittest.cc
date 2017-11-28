cd0d6bd806ea3819df7840b9376c4f16bfa4ab75
Ricky Liang <jcliang@chromium.org>
2017-11-28 07:08:33 +0000

    media: add system suspend and resume hook to ARC++ camera HALv3 VCD
    The typical way of handling system suspend and resume of Android camera
    HAL is to close the device on suspend and re-open the device on resume.
    We should register to system suspend / resume events and close / open
    the camera device accordingly.
    
    BUG=b:63984332
    TEST=Open Chrome camera app and enter suspend then resume. Verify the
         camera preview still works after resume.
    
    Change-Id: I204b04416e8993d0cb71e14c202021a195d2ef45
    Reviewed-on: https://chromium-review.googlesource.com/597987
    Reviewed-by: Christian Fremerey <chfremer@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519595}

--- media/capture/video/video_capture_device_unittest.cc
+++ media/capture/video/video_capture_device_unittest.cc
@@ -46,6 +46,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if defined(OS_CHROMEOS)
+#include "chromeos/dbus/dbus_thread_manager.h"
+#include "chromeos/dbus/fake_power_manager_client.h"
 #include "media/capture/video/chromeos/camera_buffer_factory.h"
 #include "media/capture/video/chromeos/local_gpu_memory_buffer_manager.h"
 #include "media/capture/video/chromeos/video_capture_device_arc_chromeos.h"
@@ -272,6 +274,7 @@ class VideoCaptureDeviceTest : public testing::TestWithParam<gfx::Size> {
         image_capture_client_(new MockImageCaptureClient()),
 #if defined(OS_CHROMEOS)
         local_gpu_memory_buffer_manager_(new LocalGpuMemoryBufferManager()),
+        dbus_setter_(chromeos::DBusThreadManager::GetSetterForTesting()),
 #endif
         video_capture_device_factory_(VideoCaptureDeviceFactory::CreateFactory(
             base::ThreadTaskRunnerHandle::Get(),
@@ -284,6 +287,10 @@ class VideoCaptureDeviceTest : public testing::TestWithParam<gfx::Size> {
   }
 
   void SetUp() override {
+#if defined(OS_CHROMEOS)
+    dbus_setter_->SetPowerManagerClient(
+        base::MakeUnique<chromeos::FakePowerManagerClient>());
+#endif
 #if defined(OS_ANDROID)
     static_cast<VideoCaptureDeviceFactoryAndroid*>(
         video_capture_device_factory_.get())
@@ -388,6 +395,7 @@ class VideoCaptureDeviceTest : public testing::TestWithParam<gfx::Size> {
 #if defined(OS_CHROMEOS)
   const std::unique_ptr<LocalGpuMemoryBufferManager>
       local_gpu_memory_buffer_manager_;
+  std::unique_ptr<chromeos::DBusThreadManagerSetter> dbus_setter_;
 #endif
   const std::unique_ptr<VideoCaptureDeviceFactory>
       video_capture_device_factory_;
