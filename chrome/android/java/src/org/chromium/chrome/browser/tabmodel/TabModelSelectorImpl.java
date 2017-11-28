e19fb0946b6f60a850930a1612c299724408871f
Lei Tian <ltian@chromium.org>
2017-11-28 01:24:19 +0000

    Reland "Use TabPersistentStoreObserver.onMetadataSavedAsynchronously to background the Browser Actions tab creation service"
    This relands commit 64565fd707ff5a4e2d1b378b26dc2e28e35e4806.
    
    Original change's description:
    > Use TabPersistentStoreObserver.onMetadataSavedAsynchronously to
    background the Browser Actions tab creation service
    >
    > Change the logic to background the Browser Actions tab creation
    service
    > based on TabPersistentStoreObserver.onMetadataSavedAsynchronously. The
    > callback indicates that tab model lists are backuped so the saved tabs
    > can be restored succesfully.
    >
    > Also for the notification when a new tab has not been created (tabId
    is
    > not available), the Intent to bind the notification will be a VIEW
    > intent. This notification is necessary to make the service foreground.
    >
    > Bug: 766349
    > Change-Id: I73d49e2a1e26410b2dd6e51a07f35e9965b9a38a
    > Reviewed-on: https://chromium-review.googlesource.com/769852
    > Reviewed-by: Ted Choc <tedchoc@chromium.org>
    > Commit-Queue: Lei Tian <ltian@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#517492}
    
    Bug: 766349
    Change-Id: Icb35c3b0e3ed2186e48c7f0e77ceb6a07bc1f39f
    Reviewed-on: https://chromium-review.googlesource.com/791392
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Lei Tian <ltian@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519509}

--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabModelSelectorImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabModelSelectorImpl.java
@@ -425,4 +425,12 @@ public class TabModelSelectorImpl extends TabModelSelectorBase implements TabMod
     public void addTabPersistentStoreObserver(TabPersistentStoreObserver observer) {
         mTabSaver.addObserver(observer);
     }
+
+    /**
+     * Remove a {@link TabPersistentStoreObserver} from {@link TabPersistentStore}.
+     * @param observer The observer to remove.
+     */
+    public void removeTabPersistentStoreObserver(TabPersistentStoreObserver observer) {
+        mTabSaver.removeObserver(observer);
+    }
 }
