41a1b311d1108ed5b88b73c420f415d8edea4c20
Scott Little <sclittle@chromium.org>
2017-11-28 17:40:22 +0000

    Revert "[signin] Dice migration: Clear LST tokens when gaia cookies are cleared"
    This reverts commit cc9fb2297c61ac6127949c17e3d0414aebfc0c23.
    
    Reason for revert: This seems to have broken ios_chrome_settings_egtests (iPad Air 2 iOS 10.0) failing on chromium.mac/ios-simulator-full-configs
    
    The AccountCollectionsTestCase/testMDMError test is failing.
    
    See https://uberchromegw.corp.google.com/i/chromium.mac/builders/ios-simulator-full-configs.
    
    Original change's description:
    > [signin] Dice migration: Clear LST tokens when gaia cookies are cleared
    > 
    > This CL adds ShouldReconcileAccount() function on the
    > DiceAccountReconcilor delegate, that is used to revoke the tokens when
    > cookies are empty.
    > 
    > As a cleanup, the chrome_account_ member is changed to a local variable,
    > to reduce the amount of state in the AccountReconcilor, making it
    > hopefully more readable.
    > 
    > Bug: 788315
    > Change-Id: Id4348a6ae2e524de721d39c84cb4e150adb892b2
    > Reviewed-on: https://chromium-review.googlesource.com/789032
    > Commit-Queue: David Roger <droger@chromium.org>
    > Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#519697}
    
    TBR=droger@chromium.org,msarda@chromium.org
    
    Change-Id: If4f5e30f574365d1cf7d58e4ca1c41889fa1b0bd
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 788315
    Reviewed-on: https://chromium-review.googlesource.com/794250
    Reviewed-by: Scott Little <sclittle@chromium.org>
    Commit-Queue: Scott Little <sclittle@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519730}

--- components/signin/core/browser/account_reconcilor_delegate.cc
+++ components/signin/core/browser/account_reconcilor_delegate.cc
@@ -27,9 +27,4 @@ std::string AccountReconcilorDelegate::GetFirstGaiaAccountForReconcile(
   return std::string();
 }
 
-bool AccountReconcilorDelegate::ShouldRevokeAllSecondaryTokensBeforeReconcile(
-    const std::vector<gaia::ListedAccount>& gaia_accounts) {
-  return false;
-}
-
 }  // namespace signin
