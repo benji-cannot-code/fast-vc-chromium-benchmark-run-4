547c426b08aa91d80d694f4dad7245cc492adab5
David Roger <droger@chromium.org>
2017-11-28 15:53:19 +0000

    [signin] Dice migration: Clear LST tokens when gaia cookies are cleared
    This CL adds ShouldReconcileAccount() function on the
    DiceAccountReconcilor delegate, that is used to revoke the tokens when
    cookies are empty.
    
    As a cleanup, the chrome_account_ member is changed to a local variable,
    to reduce the amount of state in the AccountReconcilor, making it
    hopefully more readable.
    
    Bug: 788315
    Change-Id: Id4348a6ae2e524de721d39c84cb4e150adb892b2
    Reviewed-on: https://chromium-review.googlesource.com/789032
    Commit-Queue: David Roger <droger@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519697}

--- components/signin/core/browser/account_reconcilor_delegate.cc
+++ components/signin/core/browser/account_reconcilor_delegate.cc
@@ -27,4 +27,9 @@ std::string AccountReconcilorDelegate::GetFirstGaiaAccountForReconcile(
   return std::string();
 }
 
+bool AccountReconcilorDelegate::ShouldRevokeAllSecondaryTokensBeforeReconcile(
+    const std::vector<gaia::ListedAccount>& gaia_accounts) {
+  return false;
+}
+
 }  // namespace signin
