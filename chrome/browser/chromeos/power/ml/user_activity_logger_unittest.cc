855e38a93b977aafab0066f1bb0fa1792e6f2a83
Jia <jiameng@chromium.org>
2017-11-28 22:05:10 +0000

    Add user and video activity observers to idle event notifier.
    This cl adds ui::UserActivityObserver and
    viz::mojom::VideoDetectorObserver to listen to relevant events when
    generating idle events.
    
    See https://docs.google.com/document/d/123eFd0rUTu0140V6hJKkkWmpqibdIfM-zTtUR5a2qaQ/edit#
    for more detail.
    
    Bug: 784232
    Change-Id: I64bae608ea7500406197c3219f26599962256adc
    Reviewed-on: https://chromium-review.googlesource.com/780480
    Reviewed-by: Emily Stark <estark@chromium.org>
    Reviewed-by: enne <enne@chromium.org>
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Renjie Liu <renjieliu@chromium.org>
    Commit-Queue: Jia Meng <jiameng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519864}

--- chrome/browser/chromeos/power/ml/user_activity_logger_unittest.cc
+++ chrome/browser/chromeos/power/ml/user_activity_logger_unittest.cc
@@ -32,6 +32,7 @@ class TestingUserActivityLoggerDelegate : public UserActivityLoggerDelegate {
 
   const std::vector<UserActivityEvent>& events() const { return events_; }
 
+  // UserActivityLoggerDelegate overrides:
   void LogActivity(const UserActivityEvent& event) override {
     events_.push_back(event);
   }
@@ -45,7 +46,6 @@ class TestingUserActivityLoggerDelegate : public UserActivityLoggerDelegate {
 class UserActivityLoggerTest : public testing::Test {
  public:
   UserActivityLoggerTest() : activity_logger_(&delegate_) {}
-
   ~UserActivityLoggerTest() override = default;
 
  protected:
@@ -70,10 +70,11 @@ class UserActivityLoggerTest : public testing::Test {
 // UserActivityEvent.
 TEST_F(UserActivityLoggerTest, LogAfterIdleEvent) {
   // Trigger an idle event.
-  ReportIdleEvent({base::Time::Now()});
+  base::Time now = base::Time::UnixEpoch();
+  ReportIdleEvent({now, now});
   ReportUserActivity(nullptr);
 
-  const auto& events = GetEvents();
+  const std::vector<UserActivityEvent>& events = GetEvents();
   ASSERT_EQ(1U, events.size());
 
   UserActivityEvent::Event expected_event;
@@ -86,7 +87,8 @@ TEST_F(UserActivityLoggerTest, LogAfterIdleEvent) {
 TEST_F(UserActivityLoggerTest, LogBeforeIdleEvent) {
   ReportUserActivity(nullptr);
   // Trigger an idle event.
-  ReportIdleEvent({base::Time::Now()});
+  base::Time now = base::Time::UnixEpoch();
+  ReportIdleEvent({now, now});
 
   EXPECT_EQ(0U, GetEvents().size());
 }
@@ -96,11 +98,12 @@ TEST_F(UserActivityLoggerTest, LogBeforeIdleEvent) {
 TEST_F(UserActivityLoggerTest, LogSecondEvent) {
   ReportUserActivity(nullptr);
   // Trigger an idle event.
-  ReportIdleEvent({base::Time::Now()});
+  base::Time now = base::Time::UnixEpoch();
+  ReportIdleEvent({now, now});
   // Another user event.
   ReportUserActivity(nullptr);
 
-  const auto& events = GetEvents();
+  const std::vector<UserActivityEvent>& events = GetEvents();
   ASSERT_EQ(1U, events.size());
 
   UserActivityEvent::Event expected_event;
@@ -112,16 +115,18 @@ TEST_F(UserActivityLoggerTest, LogSecondEvent) {
 // Log multiple events.
 TEST_F(UserActivityLoggerTest, LogMultipleEvents) {
   // Trigger an idle event.
-  ReportIdleEvent({base::Time::Now()});
+  base::Time now = base::Time::UnixEpoch();
+  ReportIdleEvent({now, now});
   // First user event.
   ReportUserActivity(nullptr);
 
   // Trigger an idle event.
-  ReportIdleEvent({base::Time::Now()});
+  now = base::Time::UnixEpoch();
+  ReportIdleEvent({now, now});
   // Second user event.
   ReportUserActivity(nullptr);
 
-  const auto& events = GetEvents();
+  const std::vector<UserActivityEvent>& events = GetEvents();
   ASSERT_EQ(2U, events.size());
 
   UserActivityEvent::Event expected_event;
