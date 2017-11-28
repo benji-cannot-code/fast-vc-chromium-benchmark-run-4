ec4d3f5bbc6c8d7e49cf5e8b9567d3563a460b00
Gauthier Ambard <gambard@chromium.org>
2017-11-28 09:42:38 +0000

    Remove setToolsMenuStateProvider from Toolbar
    The setToolsMenuStateProvider method wasn't used in the Toolbar.
    This CL removes it.
    
    Bug: none
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I1314bf4859541c21bb6504ef78b96d8133995177
    Reviewed-on: https://chromium-review.googlesource.com/789190
    Reviewed-by: edchin <edchin@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519628}

--- ios/chrome/browser/ui/toolbar/toolbar_adapter.mm
+++ ios/chrome/browser/ui/toolbar/toolbar_adapter.mm
@@ -122,11 +122,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return;
 }
 
-- (void)setToolsMenuStateProvider:
-    (id<ToolsMenuPresentationStateProvider>)provider {
-  return;
-}
-
 - (void)setToolsMenuIsVisibleForToolsMenuButton:(BOOL)isVisible {
   [self.toolbarCoordinator setToolsMenuIsVisibleForToolsMenuButton:isVisible];
 }
