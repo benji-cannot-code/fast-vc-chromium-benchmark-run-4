76b3e961287f7038d22864c23e618556c4ad3eac
Sammie Quon <sammiequon@google.com>
2017-11-28 23:31:09 +0000

    tray: Fix garbled text in multiprofile dropdown.
    Bug introduced when I was trying to show focus rectangle around user
    card view by removing the background. Draw the background instead on
    the parent, button_from_view, which the focus painter is on so that
    the background does not cover the focus rectangle.
    
    Also fix the separator between the user card view and the drop down,
    before it only reached halfway (see bug for snapshot).
    
    Test: manual
    Bug: 786184
    Change-Id: I54481c414a373a9f761d4b2f6b8097160633fbdc
    Reviewed-on: https://chromium-review.googlesource.com/794853
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519897}

--- ash/system/user/button_from_view.cc
+++ ash/system/user/button_from_view.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/animation/ink_drop_highlight.h"
 #include "ui/views/animation/ink_drop_impl.h"
 #include "ui/views/animation/ink_drop_mask.h"
+#include "ui/views/background.h"
 #include "ui/views/layout/fill_layout.h"
 #include "ui/views/painter.h"
 
@@ -44,6 +45,9 @@ ButtonFromView::ButtonFromView(views::View* content,
     SetFocusForPlatform();
 
   SetFocusPainter(TrayPopupUtils::CreateFocusPainter());
+
+  SetBackground(views::CreateThemedSolidBackground(
+      this, ui::NativeTheme::kColorId_BubbleBackground));
 }
 
 ButtonFromView::~ButtonFromView() = default;
