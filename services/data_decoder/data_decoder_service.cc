2e45a6850b1cbc1926dc8b5576457a3b01fa75fa
Jay Civelli <jcivelli@google.com>
2017-11-28 21:04:52 +0000

    Adding a SafeXMLParser to the data-decoder service.
    Introducing a new XML parser service that parses XML into a
    base::DictionaryValue. It does so by JSONifying the XML content.
    
    Also factoring out TestServiceManagerListener to its own file, now that
    it is used in 2 places (safe_json_parser_browsertest.cc and
    safe_xml_parser_browsertest.cc).
    
    Bug: 784667
    Change-Id: I1f41f44d49910da43837aac8bca52e24533460ef
    Reviewed-on: https://chromium-review.googlesource.com/767890
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Scott Graham <scottmg@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Jay Civelli <jcivelli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519832}

--- services/data_decoder/data_decoder_service.cc
+++ services/data_decoder/data_decoder_service.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/data_decoder/image_decoder_impl.h"
 #include "services/data_decoder/json_parser_impl.h"
 #include "services/data_decoder/public/interfaces/image_decoder.mojom.h"
+#include "services/data_decoder/xml_parser.h"
 #include "services/service_manager/public/cpp/service_context.h"
 
 namespace data_decoder {
@@ -34,6 +35,12 @@ void OnJsonParserRequest(service_manager::ServiceContextRefFactory* ref_factory,
       std::move(request));
 }
 
+void OnXmlParserRequest(service_manager::ServiceContextRefFactory* ref_factory,
+                        mojom::XmlParserRequest request) {
+  mojo::MakeStrongBinding(base::MakeUnique<XmlParser>(ref_factory->CreateRef()),
+                          std::move(request));
+}
+
 }  // namespace
 
 DataDecoderService::DataDecoderService() : weak_factory_(this) {}
@@ -51,6 +58,7 @@ void DataDecoderService::OnStart() {
   registry_.AddInterface(
       base::Bind(&OnImageDecoderRequest, ref_factory_.get()));
   registry_.AddInterface(base::Bind(&OnJsonParserRequest, ref_factory_.get()));
+  registry_.AddInterface(base::Bind(&OnXmlParserRequest, ref_factory_.get()));
 }
 
 void DataDecoderService::OnBindInterface(
