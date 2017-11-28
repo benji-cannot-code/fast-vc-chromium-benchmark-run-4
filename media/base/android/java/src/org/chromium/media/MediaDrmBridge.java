f55b9862690fac10a6d79e683d994453b36f60af
Aidan Wolter <awolter@chromium.org>
2017-11-28 23:56:04 +0000

    Manually release MediaCrypto plugin
    After the end of a stream, we now manually release the MediaCrypto
    plugin, so that it can be cleaned up on the TZ-side. If this is not
    done, then we risk the chance of GC not cleaning up before the TZ heap
    overflows.
    
    TEST=Switch HBO Go cast stream multiple times, and watch the TZ heap
    BUG=b:69682820
    
    Change-Id: I6fda96b74525533b64638d859cb79a4aa6375248
    Reviewed-on: https://chromium-review.googlesource.com/790570
    Commit-Queue: Aidan Wolter <awolter@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519911}

--- media/base/android/java/src/org/chromium/media/MediaDrmBridge.java
+++ media/base/android/java/src/org/chromium/media/MediaDrmBridge.java
@@ -75,6 +75,7 @@ public class MediaDrmBridge {
     private static final byte[] UNPROVISION = "unprovision".getBytes();
 
     private MediaDrm mMediaDrm;
+    private MediaCrypto mMediaCrypto;
     private long mNativeMediaDrmBridge;
     private UUID mSchemeUUID;
 
@@ -307,9 +308,9 @@ public class MediaDrmBridge {
         // Create MediaCrypto object.
         try {
             if (MediaCrypto.isCryptoSchemeSupported(mSchemeUUID)) {
-                MediaCrypto mediaCrypto = new MediaCrypto(mSchemeUUID, mMediaCryptoSession.drmId());
+                mMediaCrypto = new MediaCrypto(mSchemeUUID, mMediaCryptoSession.drmId());
                 Log.d(TAG, "MediaCrypto successfully created!");
-                onMediaCryptoReady(mediaCrypto);
+                onMediaCryptoReady(mMediaCrypto);
                 return true;
             } else {
                 Log.e(TAG, "Cannot create MediaCrypto for unsupported scheme.");
@@ -601,6 +602,11 @@ public class MediaDrmBridge {
         if (mMediaDrm != null) {
             mMediaDrm.release();
             mMediaDrm = null;
+        }
+
+        if (mMediaCrypto != null) {
+            mMediaCrypto.release();
+            mMediaCrypto = null;
         }
     }
 
