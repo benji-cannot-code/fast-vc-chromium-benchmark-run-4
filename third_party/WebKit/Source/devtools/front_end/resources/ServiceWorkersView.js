54b3aeb0dd93592117a13cb5a0cc62af86f07eb5
Eugene Ostroukhov <eostroukhov@chromium.org>
2017-11-28 21:34:51 +0000

    DevTools: Fix service workers list display issues
    1. Show scroll (if needed) for the top-level component.
    2. Disable scrolls for the individual service worker lists.
    3. Fix white background for the "Other origin" list
    
    Bug: 785136
    Change-Id: I55e8ff08c72609b34b0aba8b9e5729d86a54c663
    Reviewed-on: https://chromium-review.googlesource.com/776115
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Eugene Ostroukhov <eostroukhov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519852}

--- third_party/WebKit/Source/devtools/front_end/resources/ServiceWorkersView.js
+++ third_party/WebKit/Source/devtools/front_end/resources/ServiceWorkersView.js
@@ -11,6 +11,7 @@ Resources.ServiceWorkersView = class extends UI.VBox {
     this.registerRequiredCSS('resources/serviceWorkersView.css');
 
     this._currentWorkersView = new UI.ReportView(Common.UIString('Service Workers'));
+    this._currentWorkersView.setBodyScrollable(false);
     this.contentElement.classList.add('service-worker-list');
     this._currentWorkersView.show(this.contentElement);
     this._currentWorkersView.element.classList.add('service-workers-this-origin');
@@ -44,6 +45,7 @@ Resources.ServiceWorkersView = class extends UI.VBox {
     toolbar.appendToolbarItem(this._filter);
 
     this._otherWorkersView = new UI.ReportView();
+    this._otherWorkersView.setBodyScrollable(false);
     this._otherWorkersView.show(this._otherWorkers);
     this._otherWorkersView.element.classList.add('service-workers-for-other-origins');
 
