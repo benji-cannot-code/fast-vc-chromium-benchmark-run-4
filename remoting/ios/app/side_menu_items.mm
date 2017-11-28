7af48da2e7511b43953abcbe7d6a2bc157182de6
Yuwei Huang <yuweih@chromium.org>
2017-11-28 21:52:13 +0000

    [CRD iOS] Updating the side menu of the open source variant
    This CL updates the open source implementation of the side menu so that
    it also shows items from SideMenuItemsProvider. This make it easier for
    developer to debug CRD for iOS on Chromium without checking out
    ios_internal.
    
    Bug: 786625
    Change-Id: I784032e905cfdd78eeef122f302f464b443d2153
    Reviewed-on: https://chromium-review.googlesource.com/780151
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519859}

--- remoting/ios/app/side_menu_items.mm
+++ remoting/ios/app/side_menu_items.mm
@@ -21,14 +21,6 @@ static NSString* const kFeedbackContext = @"SideMenuFeedbackContext";
 
 #pragma mark - SideMenuItem
 
-@interface SideMenuItem ()
-
-- (instancetype)initWithTitle:(NSString*)title
-                         icon:(UIImage*)icon
-                       action:(SideMenuItemAction)action;
-
-@end
-
 @implementation SideMenuItem
 
 @synthesize title = _title;
