cee5b0280b8a0877bdea72b1156eceffc4f22baf
Andrey Kosyakov <caseq@chromium.org>
2017-11-28 21:19:19 +0000

    DevTools: throttle layer panel updates
    Back-end can often produce layerTreeDidChange() at a rate that can't
    be handled by front-end, so throttle it. A better fix would be to change
    the protocol to only send these events when front-end is ready to
    process them, but let's have a trivial fix first.
    
    Change-Id: Id24233cf75697363ea1b7995346a735a2055b226
    Reviewed-on: https://chromium-review.googlesource.com/786902
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519843}

--- third_party/WebKit/Source/devtools/front_end/layers/LayerTreeModel.js
+++ third_party/WebKit/Source/devtools/front_end/layers/LayerTreeModel.js
@@ -45,6 +45,7 @@ Layers.LayerTreeModel = class extends SDK.SDKModel {
     }
     /** @type {?SDK.LayerTreeBase} */
     this._layerTree = null;
+    this._throttler = new Common.Throttler(20);
   }
 
   disable() {
@@ -81,6 +82,13 @@ Layers.LayerTreeModel = class extends SDK.SDKModel {
   async _layerTreeChanged(layers) {
     if (!this._enabled)
       return;
+    this._throttler.schedule(this._innerSetLayers.bind(this, layers));
+  }
+
+  /**
+   * @param {?Array.<!Protocol.LayerTree.Layer>} layers
+   */
+  async _innerSetLayers(layers) {
     var layerTree = /** @type {!Layers.AgentLayerTree} */ (this._layerTree);
 
     await layerTree.setLayers(layers);
