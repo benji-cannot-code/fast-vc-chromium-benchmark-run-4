9d61621b6eddb758a91d5aa99073b08a05717a29
John Budorick <jbudorick@chromium.org>
2017-11-28 21:08:21 +0000

    Roll android SDK to O MR1. (RELAND)
    TBR=agrieve@chromium.org,yfriedman@chromium.org,mef@chromium.org,pasko@chromium.org
    
    Bug: 782848,789177
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I30cdc80f19ee01619e7ce808423c10ffc752644d
    Reviewed-on: https://chromium-review.googlesource.com/794338
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Reviewed-by: Misha Efimov <mef@chromium.org>
    Reviewed-by: agrieve <agrieve@chromium.org>
    Commit-Queue: John Budorick <jbudorick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519833}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/snippets/SnippetArticle.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/snippets/SnippetArticle.java
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 package org.chromium.chrome.browser.ntp.snippets;
 
+import android.annotation.SuppressLint;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.ColorInt;
 import android.support.annotation.Nullable;
@@ -89,6 +90,7 @@ public class SnippetArticle implements OfflinableSuggestion {
     /**
      * Creates a SnippetArticleListItem object that will hold the data.
      */
+    @SuppressLint("SupportAnnotationUsage") // for ColorInt on an Integer rather than int or long
     public SnippetArticle(int category, String idWithinCategory, String title, String publisher,
             String url, long publishTimestamp, float score, long fetchTimestamp,
             boolean isVideoSuggestion, @ColorInt Integer thumbnailDominantColor) {
