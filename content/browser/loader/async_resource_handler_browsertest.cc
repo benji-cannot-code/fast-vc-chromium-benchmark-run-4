f2001c9287a1fd3014203bad38b69440e6219ed9
Egor Pasko <pasko@chromium.org>
2017-11-28 17:19:06 +0000

    Disable Android tests failing on ASAN - take 2
    This is a followup to crrev.com/519345, where the disabling was not done
    properly for AsyncResourceHandlerBrowserTest. Apologies.
    
    TBR=xhwang@chromium.org,csharrison@chromium.org
    
    Bug: 788748
    Change-Id: Ia5397e830a821e6a7f6408416ec6b86da7589947
    Reviewed-on: https://chromium-review.googlesource.com/794131
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519719}

--- content/browser/loader/async_resource_handler_browsertest.cc
+++ content/browser/loader/async_resource_handler_browsertest.cc
@@ -73,7 +73,7 @@ std::unique_ptr<net::test_server::HttpResponse> HandlePostAndRedirectURLs(
 #else
 #define MAYBE_AsyncResourceHandlerBrowserTest AsyncResourceHandlerBrowserTest
 #endif  // defined(OS_ANDROID) && defined(ADDRESS_SANITIZER)
-class AsyncResourceHandlerBrowserTest
+class MAYBE_AsyncResourceHandlerBrowserTest
     : public ContentBrowserTest,
       public testing::WithParamInterface<bool> {
   void SetUpCommandLine(base::CommandLine* command_line) override {
@@ -84,7 +84,7 @@ class AsyncResourceHandlerBrowserTest
   }
 };
 
-IN_PROC_BROWSER_TEST_P(AsyncResourceHandlerBrowserTest, UploadProgress) {
+IN_PROC_BROWSER_TEST_P(MAYBE_AsyncResourceHandlerBrowserTest, UploadProgress) {
   net::EmbeddedTestServer* test_server = embedded_test_server();
   test_server->RegisterRequestHandler(
       base::Bind(&HandlePostAndRedirectURLs, kPostPath));
@@ -101,7 +101,7 @@ IN_PROC_BROWSER_TEST_P(AsyncResourceHandlerBrowserTest, UploadProgress) {
   EXPECT_EQ(js_result, "success");
 }
 
-IN_PROC_BROWSER_TEST_P(AsyncResourceHandlerBrowserTest,
+IN_PROC_BROWSER_TEST_P(MAYBE_AsyncResourceHandlerBrowserTest,
                        UploadProgressRedirect) {
   net::EmbeddedTestServer* test_server = embedded_test_server();
   test_server->RegisterRequestHandler(
@@ -119,8 +119,8 @@ IN_PROC_BROWSER_TEST_P(AsyncResourceHandlerBrowserTest,
   EXPECT_EQ(js_result, "success");
 }
 
-INSTANTIATE_TEST_CASE_P(AsyncResourceHandlerBrowserTest,
-                        AsyncResourceHandlerBrowserTest,
+INSTANTIATE_TEST_CASE_P(MAYBE_AsyncResourceHandlerBrowserTest,
+                        MAYBE_AsyncResourceHandlerBrowserTest,
                         ::testing::Values(false, true));
 
 }  // namespace content
