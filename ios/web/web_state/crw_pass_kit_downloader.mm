d8b7c6cef3523c703c5b0f01f0c60f1145004e80
Eugene But <eugenebut@google.com>
2017-11-28 19:33:39 +0000

    Added feature flags for FileDownload and PassKitDownload features.
    This CL adds kNewPassKitDownload and kNewFileDownload flags usage
    to the web layer. Turning on kNewPassKitDownload flag will disable
    CRWPassKitDownloader and webController:didLoadPassKitObject: callback.
    Turning on kNewFileDownload will disable
    controllerForUnhandledContentAtURL: callback.
    Turning either of these flags on will add a logic, which discards
    pending item if that item is download.
    
    Both flags are turned off by default and there is no way to enable
    them via chrome://flags yet.
    
    This CL slightly changes the code inside WKNavigationDelegate's
    webView:decidePolicyForNavigationResponse:decisionHandler: as follows:
     - CRWPassKitDownloader download is moved behind |!allowNavigation|
     - allowNavigation is not set to no if MIME type is PassKit
    These are non functional changes because if MIME type is PassKit, then
    |allowNavigation| is always NO.
    
    Bug: 780646
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I948ae3e1f38d266eda8aa7b99900900d8c02fef3
    Reviewed-on: https://chromium-review.googlesource.com/783671
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Gregory Chatzinoff <gchatz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519795}

--- ios/web/web_state/crw_pass_kit_downloader.mm
+++ ios/web/web_state/crw_pass_kit_downloader.mm
@@ -7,10 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/feature_list.h"
 #include "base/mac/scoped_block.h"
 #include "base/macros.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/sys_string_conversions.h"
+#include "ios/web/public/features.h"
 #include "net/http/http_response_headers.h"
 #include "net/url_request/url_fetcher.h"
 #include "net/url_request/url_fetcher_delegate.h"
@@ -98,6 +100,7 @@ class PassKitFetcherDelegate : public URLFetcherDelegate {
 
 - (instancetype)initWithContextGetter:(net::URLRequestContextGetter*)getter
                     completionHandler:(web::PassKitCompletionHandler)handler {
+  DCHECK(!base::FeatureList::IsEnabled(web::features::kNewPassKitDownload));
   self = [super init];
   if (self) {
     DCHECK(getter);
