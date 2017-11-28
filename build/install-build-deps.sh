d25d2dea27d1c51d042438fde134caa63821b257
Dominik Röttsches <drott@chromium.org>
2017-11-28 09:09:15 +0000

    Add Noto Sans Khmer to GCS synced linux fonts
    This should fix another set of layout tests and make us less dependent
    on distribution specific font packages. Add a font usage line in
    languages-emoji-rare-glyphs.js to make sure that Noto Sans Khmer is
    used.
    
    Bug: 772116
    Change-Id: I0acfb2186eeb180a3a464cd6337539cbe1cdf45d
    Reviewed-on: https://chromium-review.googlesource.com/779520
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519621}

--- build/install-build-deps.sh
+++ build/install-build-deps.sh
@@ -157,7 +157,6 @@ dev_list="\
   fakeroot
   flex
   fonts-ipafont
-  fonts-thai-tlwg
   g++
   git-core
   git-svn
--- third_party/WebKit/LayoutTests/inspector-protocol/layout-fonts/languages-emoji-rare-glyphs.js
+++ third_party/WebKit/LayoutTests/inspector-protocol/layout-fonts/languages-emoji-rare-glyphs.js
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             <div lang="ar" id="arabic_pangram">نصٌّ حكيمٌ لهُ سِرٌّ قاطِعٌ وَذُو شَأنٍ عَظيمٍ مكتوبٌ على ثوبٍ أخضرَ ومُغلفٌ بجلدٍ أزرق</div>
             <div id="emoji">🌱🌲🌳🌴🌵🌷🌸🌹🌺🌻🌼💐🌾🌿🍀🍁🍂🍃🍄🌰</div>
             <div id="egyptian_hieroglyphs">𓀀𓀁𓀂𓀃𓀄𓀅𓀆𓀇𓀈𓀉𓀊𓀋𓀌𓀍𓀎𓀏</div>
+            <div lang="km" id="khmer">ខ្ញុំអាចញុំកញ្ចក់បាន ដោយគ្មានបញ្ហារ</div>
         </div>
     </body>
     </html>
