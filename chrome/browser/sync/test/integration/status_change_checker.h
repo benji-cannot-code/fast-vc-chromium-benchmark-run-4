d9fc882db06c2ce87fef760c2a2400d513f85119
Henrik Boström <hbos@chromium.org>
2017-11-28 14:41:26 +0000

    Revert "Migrate away from deprecated RunLoop API"
    This reverts commit 07edac9ec1a40b737569afb0d4db3a2402c3f39b.
    
    Reason for revert: Speculative revert for sync_integration_tests /
    TwoClientPasswordsSyncTest.SetPassphraseAndThenSetupSync failure on
    https://uberchromegw.corp.google.com/i/chromium.mac/builders/Mac10.9%20Tests%20%28dbg%29/builds/47531
    
    Original change's description:
    > Migrate away from deprecated RunLoop API
    > 
    > Instead, we keep around an instance of base::RunLoop and pass it
    > explicitly for Quit(), which makes sure the code quits the intended loop
    > and not a nested one.
    > 
    > Bug: 783774
    > Change-Id: I3ea5092699bbae127f581e5c955cb27904d98f0a
    > Reviewed-on: https://chromium-review.googlesource.com/784790
    > Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    > Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#519664}
    
    TBR=pavely@chromium.org,mastiz@chromium.org
    
    Change-Id: I25ed5a61f5b9e13159af82c4ef6d6f652170d4a4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 783774
    Reviewed-on: https://chromium-review.googlesource.com/793630
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Commit-Queue: Henrik Boström <hbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519680}

--- chrome/browser/sync/test/integration/status_change_checker.h
+++ chrome/browser/sync/test/integration/status_change_checker.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "base/run_loop.h"
 #include "base/time/time.h"
 
 // Interface for a helper class that can pump the message loop while waiting
@@ -47,15 +46,7 @@ class StatusChangeChecker {
   // Timeout length when blocking.
   virtual base::TimeDelta GetTimeoutDuration();
 
-  // Stop the nested running of the message loop started in StartBlockingWait().
-  void StopWaiting();
-
-  // Checks IsExitConditionSatisfied() and calls StopWaiting() if it returns
-  // true.
-  void CheckExitCondition();
-
- private:
-  // Helper function to start running the nested run loop (run_loop_).
+  // Helper function to start running the nested run loop.
   //
   // Will exit if IsExitConditionSatisfied() returns true when called from
   // CheckExitCondition(), if a timeout occurs, or if StopWaiting() is called.
@@ -63,10 +54,16 @@ class StatusChangeChecker {
   // The timeout length is specified with GetTimeoutDuration().
   void StartBlockingWait();
 
+  // Stop the nested running of the message loop started in StartBlockingWait().
+  void StopWaiting();
+
+  // Checks IsExitConditionSatisfied() and calls StopWaiting() if it returns
+  // true.
+  void CheckExitCondition();
+
   // Called when the blocking wait timeout is exceeded.
   void OnTimeout();
 
-  base::RunLoop run_loop_;
   bool timed_out_;
 };
 
