d8c4c1490b0e64273b3029cc34a54af94e83ca67
Mohamad Ahmadi <mahmadi@chromium.org>
2017-11-28 20:51:33 +0000

    [PR] Removes GetURLRequestContextGetter from autofill::PersonalDataManager
    Replaces usages of PersonalDataManager::GetURLRequestContextGetter() with
    ApplicationContext::GetSystemURLRequestContext()
    
    Bug: 781942
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I7e42baf9ce61d722410c1f49ee33a55c5e53485a
    Reviewed-on: https://chromium-review.googlesource.com/793850
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Mathieu Perreault <mathp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519825}

--- ios/chrome/browser/payments/payment_request.mm
+++ ios/chrome/browser/payments/payment_request.mm
@@ -90,9 +90,10 @@ PaymentRequest::PaymentRequest(
       web_state_(web_state),
       personal_data_manager_(personal_data_manager),
       payment_request_ui_delegate_(payment_request_ui_delegate),
+      // TODO(crbug.com/788229): Use a factory for the AddressNormalizer.
       address_normalizer_(
           GetAddressInputSource(
-              personal_data_manager_->GetURLRequestContextGetter()),
+              GetApplicationContext()->GetSystemURLRequestContext()),
           GetAddressInputStorage(),
           GetApplicationContext()->GetApplicationLocale()),
       address_normalization_manager_(
@@ -106,7 +107,7 @@ PaymentRequest::PaymentRequest(
       journey_logger_(IsIncognito(), GetLastCommittedURL(), GetUkmRecorder()),
       payment_instruments_ready_(false),
       ios_instrument_finder_(
-          personal_data_manager_->GetURLRequestContextGetter(),
+          GetApplicationContext()->GetSystemURLRequestContext(),
           payment_request_ui_delegate_) {
   PopulateAvailableShippingOptions();
   PopulateProfileCache();
@@ -185,7 +186,7 @@ autofill::AddressNormalizer* PaymentRequest::GetAddressNormalizer() {
 autofill::RegionDataLoader* PaymentRequest::GetRegionDataLoader() {
   return new autofill::RegionDataLoaderImpl(
       GetAddressInputSource(
-          personal_data_manager_->GetURLRequestContextGetter())
+          GetApplicationContext()->GetSystemURLRequestContext())
           .release(),
       GetAddressInputStorage().release(), GetApplicationLocale());
 }
