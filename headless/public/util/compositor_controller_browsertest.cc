9bc87a1bc68228d3b40574a72ca0f3e1fb9b559c
Eric Seckler <eseckler@chromium.org>
2017-11-28 17:36:00 +0000

    headless: Use virtual time stamps in CompositorController.
    This aligns animations with virtual time progression when
    the CompositorController is used to issue BeginFrames.
    
    Also updates VirtualTimeController to use TimeDeltas instead
    of millisecond ints and initializes BeginFrameArgs frame_time
    to its minimum value by default to support emulated negative
    base::TimeTicks values in continuity DCHECKS throughout the
    compositor code.
    
    Bug: 646774
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: Ia28ab131dd1a327907f8570a1f26ce794208d3d4
    Reviewed-on: https://chromium-review.googlesource.com/790910
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Alex Clarke <alexclarke@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519725}

--- headless/public/util/compositor_controller_browsertest.cc
+++ headless/public/util/compositor_controller_browsertest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "build/build_config.h"
 #include "cc/base/switches.h"
+#include "content/public/common/content_switches.h"
 #include "content/public/test/browser_test.h"
 #include "headless/public/devtools/domains/runtime.h"
 #include "headless/public/headless_browser.h"
@@ -23,7 +24,8 @@ namespace headless {
 #if !defined(OS_MACOSX)
 
 namespace {
-static constexpr int kAnimationFrameIntervalMs = 16;
+static constexpr base::TimeDelta kAnimationFrameInterval =
+    base::TimeDelta::FromMilliseconds(16);
 static constexpr base::TimeDelta kWaitForCompositorReadyFrameDelay =
     base::TimeDelta::FromMilliseconds(20);
 }  // namespace
@@ -34,6 +36,7 @@ class CompositorControllerBrowserTest
   void SetUpCommandLine(base::CommandLine* command_line) override {
     HeadlessAsyncDevTooledBrowserTest::SetUpCommandLine(command_line);
     command_line->AppendSwitch(cc::switches::kRunAllCompositorStagesBeforeDraw);
+    command_line->AppendSwitch(switches::kDisableNewContentRenderingTimeout);
   }
 
   bool GetEnableBeginFrameControl() override { return true; }
@@ -43,7 +46,7 @@ class CompositorControllerBrowserTest
         base::MakeUnique<VirtualTimeController>(devtools_client_.get());
     compositor_controller_ = base::MakeUnique<CompositorController>(
         browser()->BrowserMainThread(), devtools_client_.get(),
-        virtual_time_controller_.get(), kAnimationFrameIntervalMs,
+        virtual_time_controller_.get(), kAnimationFrameInterval,
         kWaitForCompositorReadyFrameDelay);
 
     compositor_controller_->WaitForCompositorReady(
@@ -70,12 +73,12 @@ class CompositorControllerBrowserTest
 
     virtual_time_controller_->GrantVirtualTimeBudget(
         emulation::VirtualTimePolicy::ADVANCE,
-        kNumFrames * kAnimationFrameIntervalMs, base::Bind([]() {}),
-        base::Bind(&CompositorControllerBrowserTest::OnVirtualTimeBudgetEpired,
+        kNumFrames * kAnimationFrameInterval, base::Bind([]() {}),
+        base::Bind(&CompositorControllerBrowserTest::OnVirtualTimeBudgetExpired,
                    base::Unretained(this)));
   }
 
-  void OnVirtualTimeBudgetEpired() {
+  void OnVirtualTimeBudgetExpired() {
     // Get animation frame count.
     devtools_client_->GetRuntime()->Evaluate(
         "window.rafCount",
--- headless/public/util/virtual_time_controller.cc
+++ headless/public/util/virtual_time_controller.cc
@@ -27,7 +27,7 @@ VirtualTimeController::~VirtualTimeController() {
 
 void VirtualTimeController::GrantVirtualTimeBudget(
     emulation::VirtualTimePolicy policy,
-    int budget_ms,
+    base::TimeDelta budget,
     const base::Callback<void()>& set_up_complete_callback,
     const base::Callback<void()>& budget_expired_callback) {
   DCHECK(!set_up_complete_callback_);
@@ -35,8 +35,8 @@ void VirtualTimeController::GrantVirtualTimeBudget(
 
   set_up_complete_callback_ = set_up_complete_callback;
   budget_expired_callback_ = budget_expired_callback;
-  requested_budget_ = TimeDelta::FromMilliseconds(budget_ms);
-  accumulated_time_ = TimeDelta();
+  requested_budget_ = budget;
+  accumulated_budget_portion_ = TimeDelta();
   virtual_time_policy_ = policy;
 
   // Notify tasks of new budget request.
@@ -45,7 +45,7 @@ void VirtualTimeController::GrantVirtualTimeBudget(
 
   iterating_over_tasks_ = true;
   for (TaskEntry& entry : tasks_)
-    NotifyTaskBudgetRequested(&entry, budget_ms);
+    NotifyTaskBudgetRequested(&entry, requested_budget_);
   iterating_over_tasks_ = false;
   DeleteTasksIfRequested();
 
@@ -66,21 +66,21 @@ void VirtualTimeController::NotifyTasksAndAdvance() {
   // Give at most as much virtual time as available until the next callback.
   bool ready_to_advance = true;
   iterating_over_tasks_ = true;
-  TimeDelta next_budget = requested_budget_ - accumulated_time_;
+  TimeDelta next_budget = requested_budget_ - accumulated_budget_portion_;
   // TODO(alexclarke): This is a little ugly, find a nicer way.
   for (TaskEntry& entry : tasks_) {
-    if (entry.next_execution_time <= total_elapsed_time_)
+    if (entry.next_execution_time <= total_elapsed_time_offset_)
       entry.ready_to_advance = false;
   }
 
   for (TaskEntry& entry : tasks_) {
-    if (entry.next_execution_time <= total_elapsed_time_)
+    if (entry.next_execution_time <= total_elapsed_time_offset_)
       NotifyTaskIntervalElapsed(&entry);
 
     if (tasks_to_delete_.find(entry.task) == tasks_to_delete_.end()) {
       ready_to_advance &= entry.ready_to_advance;
-      next_budget = std::min(next_budget,
-                             entry.next_execution_time - total_elapsed_time_);
+      next_budget = std::min(
+          next_budget, entry.next_execution_time - total_elapsed_time_offset_);
     }
   }
   iterating_over_tasks_ = false;
@@ -89,13 +89,13 @@ void VirtualTimeController::NotifyTasksAndAdvance() {
   if (!ready_to_advance)
     return;
 
-  if (accumulated_time_ >= requested_budget_) {
+  if (accumulated_budget_portion_ >= requested_budget_) {
     for (TaskEntry& entry : tasks_)
       entry.ready_to_advance = false;
 
     iterating_over_tasks_ = true;
     for (const TaskEntry& entry : tasks_)
-      entry.task->BudgetExpired(total_elapsed_time_);
+      entry.task->BudgetExpired(total_elapsed_time_offset_);
     iterating_over_tasks_ = false;
     DeleteTasksIfRequested();
 
@@ -120,19 +120,19 @@ void VirtualTimeController::DeleteTasksIfRequested() {
 
 void VirtualTimeController::NotifyTaskIntervalElapsed(TaskEntry* entry) {
   DCHECK(!entry->ready_to_advance);
-  entry->next_execution_time = total_elapsed_time_ + entry->interval;
+  entry->next_execution_time = total_elapsed_time_offset_ + entry->interval;
 
   entry->task->IntervalElapsed(
-      total_elapsed_time_,
+      total_elapsed_time_offset_,
       base::Bind(&VirtualTimeController::TaskReadyToAdvance,
                  base::Unretained(this), base::Unretained(entry)));
 }
 
 void VirtualTimeController::NotifyTaskBudgetRequested(TaskEntry* entry,
-                                                      int budget_ms) {
+                                                      base::TimeDelta budget) {
   DCHECK(!entry->ready_to_advance);
   entry->task->BudgetRequested(
-      total_elapsed_time_, budget_ms,
+      total_elapsed_time_offset_, budget,
       base::Bind(&VirtualTimeController::TaskReadyToAdvance,
                  base::Unretained(this), base::Unretained(entry)));
 }
@@ -142,13 +142,13 @@ void VirtualTimeController::TaskReadyToAdvance(TaskEntry* entry) {
   NotifyTasksAndAdvance();
 }
 
-void VirtualTimeController::SetVirtualTimePolicy(const TimeDelta& next_budget) {
+void VirtualTimeController::SetVirtualTimePolicy(base::TimeDelta next_budget) {
   last_used_budget_ = next_budget;
   virtual_time_active_ = true;
   devtools_client_->GetEmulation()->GetExperimental()->SetVirtualTimePolicy(
       emulation::SetVirtualTimePolicyParams::Builder()
           .SetPolicy(virtual_time_policy_)
-          .SetBudget(next_budget.InMilliseconds())
+          .SetBudget(next_budget.InMillisecondsF())
           .SetMaxVirtualTimeTaskStarvationCount(max_task_starvation_count_)
           .Build(),
       base::Bind(&VirtualTimeController::SetVirtualTimePolicyDone,
@@ -156,7 +156,12 @@ void VirtualTimeController::SetVirtualTimePolicy(const TimeDelta& next_budget) {
 }
 
 void VirtualTimeController::SetVirtualTimePolicyDone(
-    std::unique_ptr<emulation::SetVirtualTimePolicyResult>) {
+    std::unique_ptr<emulation::SetVirtualTimePolicyResult> result) {
+  if (result) {
+    virtual_time_base_ = base::Time::FromJsTime(result->GetVirtualTimeBase());
+  } else {
+    LOG(WARNING) << "SetVirtualTimePolicy did not succeed";
+  }
   if (set_up_complete_callback_) {
     set_up_complete_callback_.Run();
     set_up_complete_callback_.Reset();
@@ -170,14 +175,14 @@ void VirtualTimeController::OnVirtualTimeBudgetExpired(
   if (!budget_expired_callback_)
     return;
 
-  accumulated_time_ += last_used_budget_;
-  total_elapsed_time_ += last_used_budget_;
+  accumulated_budget_portion_ += last_used_budget_;
+  total_elapsed_time_offset_ += last_used_budget_;
   virtual_time_active_ = false;
   NotifyTasksAndAdvance();
 }
 
 void VirtualTimeController::ScheduleRepeatingTask(RepeatingTask* task,
-                                                  int interval_ms) {
+                                                  base::TimeDelta interval) {
   if (virtual_time_active_) {
     // We cannot accurately modify any previously granted virtual time budget.
     LOG(WARNING) << "VirtualTimeController tasks should be added while "
@@ -186,8 +191,8 @@ void VirtualTimeController::ScheduleRepeatingTask(RepeatingTask* task,
 
   TaskEntry entry;
   entry.task = task;
-  entry.interval = TimeDelta::FromMilliseconds(interval_ms);
-  entry.next_execution_time = total_elapsed_time_ + entry.interval;
+  entry.interval = interval;
+  entry.next_execution_time = total_elapsed_time_offset_ + entry.interval;
   tasks_.push_back(entry);
 }
 
@@ -198,6 +203,14 @@ void VirtualTimeController::CancelRepeatingTask(RepeatingTask* task) {
   }
   tasks_.remove_if(
       [task](const TaskEntry& entry) { return entry.task == task; });
+}
+
+base::Time VirtualTimeController::GetVirtualTimeBase() const {
+  return virtual_time_base_;
+}
+
+base::TimeDelta VirtualTimeController::GetCurrentVirtualTimeOffset() const {
+  return total_elapsed_time_offset_;
 }
 
 }  // namespace headless
