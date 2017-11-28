38d45e92a5ff54ed5095d5e9c23a42b286ab7ca6
Yoichi Osato <yoichio@chromium.org>
2017-11-28 03:48:10 +0000

    Move AdjustSelectionToAvoidCrossingShadowBoundaries() to SelectionAdjuster.
    This patch moves the function to SelectionAdjuster.
    The file local class ShadowBoundaryAdjuster is introduced to encapsul
    helper functions.
    
    Bug: 787295
    Change-Id: Ic1b1be49b9aa6b4a29ff829b321cb6f1d4b270fd
    Reviewed-on: https://chromium-review.googlesource.com/784757
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519552}

--- third_party/WebKit/Source/core/editing/SelectionAdjuster.h
+++ third_party/WebKit/Source/core/editing/SelectionAdjuster.h
@@ -20,6 +20,7 @@ class CORE_EXPORT SelectionAdjuster final {
   STATIC_ONLY(SelectionAdjuster);
 
  public:
+  // TODO(editing-dev): Move these functions to ShadowBoundaryAdjuster.
   static Position AdjustSelectionStartToAvoidCrossingShadowBoundaries(
       const EphemeralRange&);
   static Position AdjustSelectionEndToAvoidCrossingShadowBoundaries(
@@ -34,6 +35,10 @@ class CORE_EXPORT SelectionAdjuster final {
   static SelectionInFlatTree AdjustSelectionRespectingGranularity(
       const SelectionInFlatTree&,
       TextGranularity);
+  static SelectionInDOMTree AdjustSelectionToAvoidCrossingShadowBoundaries(
+      const SelectionInDOMTree&);
+  static SelectionInFlatTree AdjustSelectionToAvoidCrossingShadowBoundaries(
+      const SelectionInFlatTree&);
 };
 
 }  // namespace blink
