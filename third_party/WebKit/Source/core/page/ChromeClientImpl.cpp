415594a2e68ccb110a63446e23fd65bdf6b92706
Daniel Cheng <dcheng@chromium.org>
2017-11-28 08:50:16 +0000

    Remove unused code for plumbing last frame time through ChromeClient.
    Change-Id: Ic4d02f4b9dfd4650d10899f8adef529bb180e4a0
    Reviewed-on: https://chromium-review.googlesource.com/791943
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519615}

--- third_party/WebKit/Source/core/page/ChromeClientImpl.cpp
+++ third_party/WebKit/Source/core/page/ChromeClientImpl.cpp
@@ -1114,10 +1114,6 @@ std::unique_ptr<WebFrameScheduler> ChromeClientImpl::CreateFrameScheduler(
                                                       frame_type);
 }
 
-double ChromeClientImpl::LastFrameTimeMonotonic() const {
-  return web_view_->LastFrameTimeMonotonic();
-}
-
 WebAutofillClient* ChromeClientImpl::AutofillClientFromFrame(
     LocalFrame* frame) {
   if (!frame) {
