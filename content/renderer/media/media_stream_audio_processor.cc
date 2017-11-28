b2c9985ffcce71b19739a6110772edcca1d511ca
Ivo Creusen <ivoc@chromium.org>
2017-11-28 14:40:23 +0000

    Implement the new statistics interface for the WebRTC AudioProcessingModule.
    The new interface was added in https://webrtc-review.googlesource.com/c/src/+/17942
    It uses optionals and allows passing some needed information to APM, which will prevent stats from getting stuck.
    
    TBR=haraken@chromium.org
    
    Bug: b/67926135
    Change-Id: I816395515a5501158398838bfaee92bda3a67ee1
    Reviewed-on: https://chromium-review.googlesource.com/785570
    Commit-Queue: Ivo Creusen <ivoc@chromium.org>
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519679}

--- content/renderer/media/media_stream_audio_processor.cc
+++ content/renderer/media/media_stream_audio_processor.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/audio_parameters.h"
 #include "media/base/channel_layout.h"
 #include "third_party/webrtc/api/mediaconstraintsinterface.h"
+#include "third_party/webrtc/modules/audio_processing/include/audio_processing_statistics.h"
 #include "third_party/webrtc/modules/audio_processing/typing_detection.h"
 
 namespace content {
@@ -599,6 +600,15 @@ void MediaStreamAudioProcessor::GetStats(AudioProcessorStats* stats) {
   stats->typing_noise_detected =
       (base::subtle::Acquire_Load(&typing_detected_) != false);
   GetAudioProcessingStats(audio_processing_.get(), stats);
+}
+
+webrtc::AudioProcessorInterface::AudioProcessorStatistics
+MediaStreamAudioProcessor::GetStats(bool has_remote_tracks) {
+  AudioProcessorStatistics stats;
+  stats.typing_noise_detected =
+      (base::subtle::Acquire_Load(&typing_detected_) != false);
+  stats.apm_statistics = audio_processing_->GetStatistics(has_remote_tracks);
+  return stats;
 }
 
 void MediaStreamAudioProcessor::InitializeAudioProcessingModule(
