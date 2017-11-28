fa8d928f4595b3b3f99fb7dc6a6fc0c0bad3c614
Yafei Duan <romax@chromium.org>
2017-11-28 23:13:14 +0000

    [Offline Pages] DCHECK raw pointers for tasks and null callbacks.
    Most tasks are taking raw pointers and they needs to be checked against
    nullptr. This CL fixed the potential issue.
    
    Bug: 772206
    Change-Id: Ifab61fd75ed88c9d03fd0af8088454148c88144a
    Reviewed-on: https://chromium-review.googlesource.com/792261
    Reviewed-by: Peter Williamson <petewil@chromium.org>
    Commit-Queue: Yafei Duan <romax@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519890}

--- components/offline_pages/core/model/clear_legacy_temporary_pages_task.cc
+++ components/offline_pages/core/model/clear_legacy_temporary_pages_task.cc
@@ -121,7 +121,10 @@ ClearLegacyTemporaryPagesTask::ClearLegacyTemporaryPagesTask(
     : store_(store),
       policy_controller_(policy_controller),
       legacy_archives_dir_(legacy_archives_dir),
-      weak_ptr_factory_(this) {}
+      weak_ptr_factory_(this) {
+  DCHECK(store_);
+  DCHECK(policy_controller_);
+}
 
 ClearLegacyTemporaryPagesTask::~ClearLegacyTemporaryPagesTask() {}
 
