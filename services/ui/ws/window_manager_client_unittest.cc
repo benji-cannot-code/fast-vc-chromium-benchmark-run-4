2938814c2028513621fcd59c2c8cba8fad895bec
Sadrul Habib Chowdhury <sadrul@chromium.org>
2017-11-28 17:18:01 +0000

    aura: Allow injecting the AcceleratedWidget into aura.
    mus can delegate managing the gfx::AcceleratedWidget it creates to the
    window manager. The window manager (i.e. ash/aura) in this case needs to
    support injecting this AcceleratedWidget into its compositor.
    
    BUG=786453
    
    Change-Id: If9de911ab70e84e750728b0b46a8d4470c1e7da8
    Reviewed-on: https://chromium-review.googlesource.com/792272
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519717}

--- services/ui/ws/window_manager_client_unittest.cc
+++ services/ui/ws/window_manager_client_unittest.cc
@@ -48,6 +48,9 @@ class TestWindowManagerDelegate : public aura::WindowManagerDelegate {
 
   // WindowManagerDelegate:
   void SetWindowManagerClient(aura::WindowManagerClient* client) override {}
+  void OnWmAcceleratedWidgetAvailableForDisplay(
+      int64_t display_id,
+      gfx::AcceleratedWidget widget) override {}
   void OnWmConnected() override {}
   void OnWmSetBounds(aura::Window* window, const gfx::Rect& bounds) override {}
   bool OnWmSetProperty(
