5c19545e499060958df461b254ba57f9eeeba3a1
Jose Dapena Paz <jose.dapena@lge.com>
2017-11-28 01:33:35 +0000

    ATK: refactor BrowserAccessibilityAuraLinux to use AXPlatformNodeAuraLinux
    Remove all the ATK implementation in BrowserAccessibilityAuraLinux, and
    relay on the single implementation available in AXPlatformNodeAuraLinux.
    This way we stop duplicating ATK implementation, and focus on a single
    backend for the whole platform accessibility.
    
    Some changes were needed in AXPlatformNodeAuraLinux to support the
    requirements in BrowserAccessibility:
    * Delegate AccessibilityTreeAuraLinux AddProperties on
      AXPlatformNodeAuraLinux::AddAccessibilityTreeProperties.
    * Regenerate if needed the backing AXPlatformNodeAuraLinux ATK objects
      when OnDataChanged is called in BrowserAccessibilityAuraLinux.
    
    Bug: 784583
    Change-Id: I9538f09e5018a252e56c446270f8197742faa0d0
    Reviewed-on: https://chromium-review.googlesource.com/788395
    Commit-Queue: José Dapena Paz <jose.dapena@lge.com>
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519512}

--- ui/accessibility/platform/ax_platform_node_auralinux.h
+++ ui/accessibility/platform/ax_platform_node_auralinux.h
@@ -29,6 +29,7 @@ namespace ui {
 class AXPlatformNodeAuraLinux : public AXPlatformNodeBase {
  public:
   AXPlatformNodeAuraLinux();
+  ~AXPlatformNodeAuraLinux() override;
 
   // Set or get the root-level Application object that's the parent of all
   // top-level windows.
@@ -39,6 +40,10 @@ class AXPlatformNodeAuraLinux : public AXPlatformNodeBase {
 
   // Do asynchronous static initialization.
   AX_EXPORT static void StaticInitialize();
+
+  AX_EXPORT void DataChanged();
+  void Destroy() override;
+  AX_EXPORT void AddAccessibilityTreeProperties(base::DictionaryValue* dict);
 
   AtkRole GetAtkRole();
   void GetAtkState(AtkStateSet* state_set);
@@ -80,8 +85,6 @@ class AXPlatformNodeAuraLinux : public AXPlatformNodeBase {
   int GetIndexInParent() override;
 
  private:
-  ~AXPlatformNodeAuraLinux() override;
-
   enum AtkInterfaces {
     ATK_ACTION_INTERFACE,
     ATK_COMPONENT_INTERFACE,
@@ -99,6 +102,11 @@ class AXPlatformNodeAuraLinux : public AXPlatformNodeBase {
   int GetGTypeInterfaceMask();
   GType GetAccessibilityGType();
   AtkObject* CreateAtkObject();
+  void DestroyAtkObjects();
+
+  // Keep information of latest AtkInterfaces mask to refresh atk object
+  // interfaces accordingly if needed.
+  int interface_mask_;
 
   // We own a reference to these ref-counted objects.
   AtkObject* atk_object_;
