0226448160fe5db17b64ecf82579faffc8280eb3
Jana Iyengar <jri@chromium.org>
2017-11-28 00:44:23 +0000

    Revert "Default enables option to close QUIC sessions on an IP address change,"
    This reverts commit ba35577855190826b59b6795963d32cc4fdb86aa.
    
    Reason for revert: This change causes QUIC to be marked as broken if it happens ip change happens during a handshake, which may be causing QUIC usage to go down.
    
    Original change's description:
    > Default enables option to close QUIC sessions on an IP address change,
    > when QUIC connection migration is not enabled.
    > 
    > Bug: 544156
    > Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester
    > Change-Id: I6440c4833e798b61d2d83dcef2f1bf24869719be
    > Reviewed-on: https://chromium-review.googlesource.com/655767
    > Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    > Reviewed-by: Ryan Hamilton <rch@chromium.org>
    > Commit-Queue: Jana Iyengar <jri@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#503577}
    
    TBR=pauljensen@chromium.org,rch@chromium.org,jri@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 544156
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I619b6201d33cd4f97bb8be8f1674e319fa9196fe
    Reviewed-on: https://chromium-review.googlesource.com/759356
    Commit-Queue: Jana Iyengar <jri@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Reviewed-by: Jana Iyengar <jri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519483}

--- components/cronet/url_request_context_config_unittest.cc
+++ components/cronet/url_request_context_config_unittest.cc
@@ -48,6 +48,7 @@ TEST(URLRequestContextConfigTest, TestExperimentalOptionParsing) {
       "{\"QUIC\":{\"max_server_configs_stored_in_properties\":2,"
       "\"user_agent_id\":\"Custom QUIC UAID\","
       "\"idle_connection_timeout_seconds\":300,"
+      "\"close_sessions_on_ip_change\":true,"
       "\"race_cert_verification\":true,"
       "\"connection_options\":\"TIME,TBBR,REJ\"},"
       "\"AsyncDNS\":{\"enable\":true},"
@@ -92,6 +93,7 @@ TEST(URLRequestContextConfigTest, TestExperimentalOptionParsing) {
   // Check idle_connection_timeout_seconds.
   EXPECT_EQ(300, params->quic_idle_connection_timeout_seconds);
 
+  EXPECT_TRUE(params->quic_close_sessions_on_ip_change);
   EXPECT_FALSE(params->quic_migrate_sessions_on_network_change);
   EXPECT_FALSE(params->quic_migrate_sessions_on_network_change_v2);
 
@@ -157,6 +159,7 @@ TEST(URLRequestContextConfigTest, SetQuicConnectionMigrationOptions) {
   const net::HttpNetworkSession::Params* params =
       context->GetNetworkSessionParams();
 
+  EXPECT_FALSE(params->quic_close_sessions_on_ip_change);
   EXPECT_TRUE(params->quic_migrate_sessions_on_network_change);
   EXPECT_TRUE(params->quic_migrate_sessions_early);
 }
