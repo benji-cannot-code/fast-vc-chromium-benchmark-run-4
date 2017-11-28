d147f2d06c9a4bacb6cc7c4d071b66a86923f557
Alexander Timin <altimin@chromium.org>
2017-11-28 18:31:34 +0000

    [perf bot sheriff] Disable CastV2PerformanceTestPerformance on Win.
    TBR=miu@chromium.org
    
    BUG=789192
    
    Change-Id: Iac44a0facb28d55b744ce11c1de9add0cf0944a0
    Reviewed-on: https://chromium-review.googlesource.com/794271
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519759}

--- chrome/browser/extensions/api/cast_streaming/performance_test.cc
+++ chrome/browser/extensions/api/cast_streaming/performance_test.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/test/trace_event_analyzer.h"
 #include "base/time/default_tick_clock.h"
+#include "build/build_config.h"
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/tab_helper.h"
@@ -704,7 +705,13 @@ class CastV2PerformanceTest
 
 }  // namespace
 
-IN_PROC_BROWSER_TEST_P(CastV2PerformanceTest, Performance) {
+#if defined(OS_WIN)
+#define MAYBE_Performance DISABLED_Performance
+#else
+#define MAYBE_Performance Performance
+#endif
+
+IN_PROC_BROWSER_TEST_P(CastV2PerformanceTest, MAYBE_Performance) {
   RunTest("CastV2Performance");
 }
 
