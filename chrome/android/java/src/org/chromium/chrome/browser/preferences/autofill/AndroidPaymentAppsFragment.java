ce2567f675d63bd32471259476e9dd965e1430e5
gogerald <gogerald@google.com>
2017-11-28 00:48:20 +0000

    [Payments] List service worker payment apps in settings
    Test video:
    https://drive.google.com/file/d/1oUUkOqj5EJU9XiXUp9fz1FxC6S6knNqW/view?usp=sharing
    
    Bug: 785309
    Change-Id: I14f7ae6b879bea7f805d7a9de8320231447e9a81
    Reviewed-on: https://chromium-review.googlesource.com/788250
    Commit-Queue: Ganggui Tang <gogerald@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519488}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/autofill/AndroidPaymentAppsFragment.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/autofill/AndroidPaymentAppsFragment.java
@@ -5,6 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.preferences.autofill;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
@@ -12,6 +16,7 @@ import android.util.Pair;
 
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.payments.AndroidPaymentAppFactory;
+import org.chromium.chrome.browser.payments.ServiceWorkerPaymentAppBridge;
 import org.chromium.chrome.browser.preferences.PreferenceUtils;
 import org.chromium.chrome.browser.preferences.TextMessagePreference;
 
@@ -39,15 +44,35 @@ public class AndroidPaymentAppsFragment extends PreferenceFragment {
         getPreferenceScreen().removeAll();
         getPreferenceScreen().setOrderingAsAdded(true);
 
-        Map<String, Pair<String, Drawable>> appsInfo =
-                AndroidPaymentAppFactory.getAndroidPaymentAppsInfo();
-        if (appsInfo.isEmpty()) return;
+        ServiceWorkerPaymentAppBridge.getServiceWorkerPaymentAppsInfo(
+                new ServiceWorkerPaymentAppBridge.GetServiceWorkerPaymentAppsInfoCallback() {
+                    @Override
+                    public void onGetServiceWorkerPaymentAppsInfo(
+                            Map<String, Pair<String, Bitmap>> appsInfo) {
+                        addPaymentAppsPreference(
+                                AndroidPaymentAppFactory.getAndroidPaymentAppsInfo(), appsInfo);
+                    }
+                });
+    }
+
+    private void addPaymentAppsPreference(Map<String, Pair<String, Drawable>> androidAppsInfo,
+            Map<String, Pair<String, Bitmap>> serviceWorkerAppsInfo) {
+        if (androidAppsInfo.isEmpty() && serviceWorkerAppsInfo.isEmpty()) return;
 
         AndroidPaymentAppPreference pref = null;
-        for (Map.Entry<String, Pair<String, Drawable>> app : appsInfo.entrySet()) {
+        for (Map.Entry<String, Pair<String, Drawable>> app : androidAppsInfo.entrySet()) {
             pref = new AndroidPaymentAppPreference(getActivity());
             pref.setTitle(app.getValue().first);
             pref.setIcon(app.getValue().second);
+            getPreferenceScreen().addPreference(pref);
+        }
+        for (Map.Entry<String, Pair<String, Bitmap>> app : serviceWorkerAppsInfo.entrySet()) {
+            pref = new AndroidPaymentAppPreference(getActivity());
+            pref.setTitle(app.getValue().first);
+            pref.setSummary(app.getKey());
+            pref.setIcon(app.getValue().second == null
+                            ? new ColorDrawable(Color.TRANSPARENT)
+                            : new BitmapDrawable(getResources(), app.getValue().second));
             getPreferenceScreen().addPreference(pref);
         }
         // Add a divider line at the bottom of the last preference to separate it from below
