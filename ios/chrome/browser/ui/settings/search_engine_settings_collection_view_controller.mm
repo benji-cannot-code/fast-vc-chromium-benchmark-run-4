946f395e9bdc490e61d6153c8112a6a2be93f710
Gauthier Ambard <gambard@chromium.org>
2017-11-28 09:08:32 +0000

    Add SearchEngineObserver
    This CL adds a SearchEngineObserver as a standalone class, instead of
    having it duplicated in two files.
    
    Bug: 764720
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Iea5d4628e75ea4ae1e7afc349cfb958292f6a38d
    Reviewed-on: https://chromium-review.googlesource.com/790990
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519620}

--- ios/chrome/browser/ui/settings/search_engine_settings_collection_view_controller.mm
+++ ios/chrome/browser/ui/settings/search_engine_settings_collection_view_controller.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/search_engines/template_url_service.h"
 #include "components/search_engines/template_url_service_observer.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
+#import "ios/chrome/browser/search_engines/search_engine_observer_bridge.h"
 #include "ios/chrome/browser/search_engines/template_url_service_factory.h"
 #import "ios/chrome/browser/ui/collection_view/cells/collection_view_text_item.h"
 #import "ios/chrome/browser/ui/collection_view/collection_view_model.h"
@@ -23,10 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "This file requires ARC support."
 #endif
 
-@interface SearchEngineSettingsCollectionViewController ()
-- (void)onChange;
-@end
-
 namespace {
 
 typedef NS_ENUM(NSInteger, SectionIdentifier) {
@@ -37,42 +34,15 @@ typedef NS_ENUM(NSInteger, ItemType) {
   ItemTypeSearchEnginesEngine = kItemTypeEnumZero,
 };
 
-// Observer used to reload the Search Engine collection view once the
-// TemplateURLService changes, either on first load or due to a
-// policy change.
-class SearchEngineObserver : public TemplateURLServiceObserver {
- public:
-  SearchEngineObserver(SearchEngineSettingsCollectionViewController* owner,
-                       TemplateURLService* urlService);
-  ~SearchEngineObserver() override;
-  void OnTemplateURLServiceChanged() override;
-
- private:
-  __weak SearchEngineSettingsCollectionViewController* owner_;
-  TemplateURLService* templateURLService_;  // weak
-};
-
-SearchEngineObserver::SearchEngineObserver(
-    SearchEngineSettingsCollectionViewController* owner,
-    TemplateURLService* urlService)
-    : owner_(owner), templateURLService_(urlService) {
-  templateURLService_->AddObserver(this);
-}
-
-SearchEngineObserver::~SearchEngineObserver() {
-  templateURLService_->RemoveObserver(this);
-}
-
-void SearchEngineObserver::OnTemplateURLServiceChanged() {
-  SearchEngineSettingsCollectionViewController* strongOwner = owner_;
-  [strongOwner onChange];
-}
-
 }  // namespace
+
+@interface SearchEngineSettingsCollectionViewController ()<
+    SearchEngineObserving>
+@end
 
 @implementation SearchEngineSettingsCollectionViewController {
   TemplateURLService* templateURLService_;  // weak
-  std::unique_ptr<SearchEngineObserver> observer_;
+  std::unique_ptr<SearchEngineObserverBridge> observer_;
   // Prevent unnecessary notifications when we write to the setting.
   BOOL updatingBackend_;
 }
@@ -87,7 +57,8 @@ void SearchEngineObserver::OnTemplateURLServiceChanged() {
   if (self) {
     templateURLService_ =
         ios::TemplateURLServiceFactory::GetForBrowserState(browserState);
-    observer_.reset(new SearchEngineObserver(self, templateURLService_));
+    observer_ =
+        std::make_unique<SearchEngineObserverBridge>(self, templateURLService_);
     templateURLService_->Load();
     [self setTitle:l10n_util::GetNSString(IDS_IOS_SEARCH_ENGINE_SETTING_TITLE)];
     [self setCollectionViewAccessibilityIdentifier:@"Search Engine"];
@@ -200,7 +171,7 @@ void SearchEngineObserver::OnTemplateURLServiceChanged() {
   updatingBackend_ = NO;
 }
 
-- (void)onChange {
+- (void)searchEngineChanged {
   if (!updatingBackend_)
     [self reloadData];
 }
