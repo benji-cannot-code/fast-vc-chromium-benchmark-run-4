9282e1efb950a19f0b1854c7064643d90c30aae6
David Reveman <reveman@chromium.org>
2017-11-28 02:24:50 +0000

    ui: Fix rounded corner radius for shadows.
    This makes sure nine-patch is updated when rounded corner radius
    has changed and not only when elevation has changed.
    
    WMTestSuite has also been updated to allow tests that call into
    skia and use discardable memory (needed for corner radius test).
    
    Bug: 788461
    Tbr: sadrul@chromium.org
    Test: wm_unittests --gtest_fiter=ShadowTest.AdjustRoundedCornerRadius
    Change-Id: I4489fbf11416559a3da8ac3b194f96c59ed5801c
    Reviewed-on: https://chromium-review.googlesource.com/789730
    Commit-Queue: David Reveman <reveman@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519527}

--- ui/wm/test/run_all_unittests.cc
+++ ui/wm/test/run_all_unittests.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/path_service.h"
 #include "base/test/launcher/unit_test_launcher.h"
+#include "base/test/test_discardable_memory_allocator.h"
 #include "base/test/test_suite.h"
 #include "mojo/edk/embedder/embedder.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -33,6 +34,9 @@ class WMTestSuite : public base::TestSuite {
     ui::ResourceBundle::InitSharedInstanceWithPakPath(ui_test_pak_path);
 
     env_ = aura::Env::CreateInstance();
+
+    base::DiscardableMemoryAllocator::SetInstance(
+        &discardable_memory_allocator_);
   }
 
   void Shutdown() override {
@@ -43,6 +47,7 @@ class WMTestSuite : public base::TestSuite {
 
  private:
   std::unique_ptr<aura::Env> env_;
+  base::TestDiscardableMemoryAllocator discardable_memory_allocator_;
   DISALLOW_COPY_AND_ASSIGN(WMTestSuite);
 };
 
