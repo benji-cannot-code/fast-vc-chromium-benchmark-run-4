74b4a4df75978583df8140bce25afba3841c67bb
Xiangjun Zhang <xjz@chromium.org>
2017-11-29 02:22:50 +0000

    Media Remoting: Remove bitrate estimation.
    The existing logic is to only start media remoting when the content
    bitrate is less than the network bandwidth. This makes the behavior
    unpredictable since the content bitrate might be varied, e.g., by
    adaptive streaming. This CL removed the bitrate estimation to reduce
    the unpredictability of the behavior when starting media remoting.
    
    Bug: 788940
    Change-Id: Id933332b460785a8a516264f4764424e20903541
    Reviewed-on: https://chromium-review.googlesource.com/791864
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Xiangjun Zhang <xjz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519974}

--- media/remoting/renderer_controller_unittest.cc
+++ media/remoting/renderer_controller_unittest.cc
@@ -65,8 +65,6 @@ mojom::RemotingSinkMetadata GetDefaultSinkMetadata(bool enable) {
 
 constexpr base::TimeDelta kDelayedStartDuration =
     base::TimeDelta::FromSeconds(5);
-constexpr double kNormalSpeedBitsPerSecond = 5000000;
-constexpr double kHighSpeedBitsPerSecond = 15000000;
 
 }  // namespace
 
@@ -99,10 +97,6 @@ class RendererControllerTest : public ::testing::Test,
   }
 
   double Duration() const override { return duration_in_sec_; }
-
-  size_t VideoDecodedByteCount() const override { return decoded_bytes_; }
-
-  size_t AudioDecodedByteCount() const override { return 0; }
 
   unsigned DecodedFrameCount() const override { return decoded_frames_; }
 
@@ -149,15 +143,8 @@ class RendererControllerTest : public ::testing::Test,
     return controller_->delayed_start_stability_timer_.IsRunning();
   }
 
-  void DelayedStartEnds(bool too_high_bitrate, double frame_rate = 30) {
+  void DelayedStartEnds(double frame_rate = 30) {
     EXPECT_TRUE(IsInDelayedStart());
-    if (too_high_bitrate) {
-      decoded_bytes_ =
-          kHighSpeedBitsPerSecond * kDelayedStartDuration.InSeconds() / 8.0;
-    } else {
-      decoded_bytes_ =
-          kNormalSpeedBitsPerSecond * kDelayedStartDuration.InSeconds() / 8.0;
-    }
     decoded_frames_ = frame_rate * kDelayedStartDuration.InSeconds();
     clock_->Advance(kDelayedStartDuration);
     RunUntilIdle();
@@ -214,7 +201,7 @@ TEST_F(RendererControllerTest, ToggleRendererOnDominantChange) {
   InitializeControllerAndBecomeDominant(shared_session,
                                         DefaultMetadata(VideoCodec::kCodecVP8),
                                         GetDefaultSinkMetadata(true));
-  DelayedStartEnds(false);
+  DelayedStartEnds();
   RunUntilIdle();
   ExpectInRemoting();  // All requirements now satisfied.
 
@@ -222,21 +209,6 @@ TEST_F(RendererControllerTest, ToggleRendererOnDominantChange) {
   controller_->OnBecameDominantVisibleContent(false);
   RunUntilIdle();
   ExpectInLocalRendering();
-}
-
-TEST_F(RendererControllerTest, StartFailedWithTooHighBitrate) {
-  const scoped_refptr<SharedSession> shared_session =
-      FakeRemoterFactory::CreateSharedSession(false);
-  InitializeControllerAndBecomeDominant(shared_session,
-                                        DefaultMetadata(VideoCodec::kCodecVP8),
-                                        GetDefaultSinkMetadata(true));
-  EXPECT_FALSE(is_rendering_remotely_);
-  EXPECT_TRUE(IsInDelayedStart());
-  DelayedStartEnds(true);
-  RunUntilIdle();
-  EXPECT_TRUE(activate_viewport_intersection_monitoring_);
-  EXPECT_FALSE(is_rendering_remotely_);
-  EXPECT_FALSE(disable_pipeline_suspend_);
 }
 
 TEST_F(RendererControllerTest, ToggleRendererOnSinkCapabilities) {
@@ -260,7 +232,7 @@ TEST_F(RendererControllerTest, ToggleRendererOnSinkCapabilities) {
   controller_->OnBecameDominantVisibleContent(true);
   RunUntilIdle();
   ExpectInDelayedStart();
-  DelayedStartEnds(false);
+  DelayedStartEnds();
   RunUntilIdle();
   ExpectInRemoting();  // All requirements now satisfied.
 }
@@ -273,7 +245,7 @@ TEST_F(RendererControllerTest, ToggleRendererOnDisableChange) {
                                         DefaultMetadata(VideoCodec::kCodecVP8),
                                         GetDefaultSinkMetadata(true));
   ExpectInDelayedStart();
-  DelayedStartEnds(false);
+  DelayedStartEnds();
   RunUntilIdle();
   ExpectInRemoting();  // All requirements now satisfied.
 
@@ -315,7 +287,7 @@ TEST_F(RendererControllerTest, WithVP9VideoCodec) {
   shared_session->OnSinkAvailable(sink_metadata.Clone());
   RunUntilIdle();
   ExpectInDelayedStart();
-  DelayedStartEnds(false);
+  DelayedStartEnds();
   RunUntilIdle();
   ExpectInRemoting();  // All requirements now satisfied.
 }
@@ -341,7 +313,7 @@ TEST_F(RendererControllerTest, WithHEVCVideoCodec) {
   shared_session->OnSinkAvailable(sink_metadata.Clone());
   RunUntilIdle();
   ExpectInDelayedStart();
-  DelayedStartEnds(false);
+  DelayedStartEnds();
   RunUntilIdle();
   ExpectInRemoting();  // All requirements now satisfied.
 }
@@ -371,7 +343,7 @@ TEST_F(RendererControllerTest, WithAACAudioCodec) {
   shared_session->OnSinkAvailable(sink_metadata.Clone());
   RunUntilIdle();
   ExpectInDelayedStart();
-  DelayedStartEnds(false);
+  DelayedStartEnds();
   RunUntilIdle();
   ExpectInRemoting();  // All requirements now satisfied.
 }
@@ -400,7 +372,7 @@ TEST_F(RendererControllerTest, WithOpusAudioCodec) {
   shared_session->OnSinkAvailable(sink_metadata.Clone());
   RunUntilIdle();
   ExpectInDelayedStart();
-  DelayedStartEnds(false);
+  DelayedStartEnds();
   RunUntilIdle();
   ExpectInRemoting();  // All requirements now satisfied.
 }
@@ -413,7 +385,7 @@ TEST_F(RendererControllerTest, StartFailedWithHighFrameRate) {
                                         GetDefaultSinkMetadata(true));
   RunUntilIdle();
   ExpectInDelayedStart();
-  DelayedStartEnds(false, 60);
+  DelayedStartEnds(60);
   RunUntilIdle();
   ExpectInLocalRendering();
 }
@@ -428,7 +400,7 @@ TEST_F(RendererControllerTest, StartSuccessWithHighFrameRate) {
       shared_session, DefaultMetadata(VideoCodec::kCodecVP8), sink_metadata);
   RunUntilIdle();
   ExpectInDelayedStart();
-  DelayedStartEnds(false, 60);
+  DelayedStartEnds(60);
   RunUntilIdle();
   ExpectInRemoting();
 }
@@ -443,7 +415,7 @@ TEST_F(RendererControllerTest, StartFailed) {
                                         GetDefaultSinkMetadata(true));
   RunUntilIdle();
   ExpectInDelayedStart();
-  DelayedStartEnds(false);
+  DelayedStartEnds();
   RunUntilIdle();
   ExpectInLocalRendering();
 }
