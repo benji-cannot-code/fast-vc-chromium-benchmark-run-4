5dfb98df394345568f0f44bc8e50b529cd6785bb
Matt Falkenhagen <falken@chromium.org>
2017-11-29 06:15:59 +0000

    NetworkService: Support re-sending the request body after redirects.
    Some redirects like 307 require reissuing the request with the original request
    body. This was crashing because the data pipe elements were already consumed.
    The solution is to introduce DataPipeGetter backed by a Blob which allows
    getting a data pipe multiple times.
    
    Bug: 761117
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I7fbc09e626f853e1e078aecace5eca14d2788808
    Reviewed-on: https://chromium-review.googlesource.com/784775
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520029}

--- content/public/common/resource_request_body.h
+++ content/public/common/resource_request_body.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "content/common/content_export.h"
 #include "storage/common/data_element.h"
-#include "third_party/WebKit/common/blob/size_getter.mojom.h"
 #include "url/gurl.h"
 
 #if defined(OS_ANDROID)
@@ -70,8 +69,7 @@ class CONTENT_EXPORT ResourceRequestBody
                                  uint64_t offset,
                                  uint64_t length,
                                  const base::Time& expected_modification_time);
-  void AppendDataPipe(mojo::ScopedDataPipeConsumerHandle handle,
-                      blink::mojom::SizeGetterPtr size_getter);
+  void AppendDataPipe(network::mojom::DataPipeGetterPtr data_pipe_getter);
 
   const std::vector<Element>* elements() const { return &elements_; }
   std::vector<Element>* elements_mutable() { return &elements_; }
