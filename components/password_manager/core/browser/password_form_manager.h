267211ee818fc1714cccd02b7da18be6289e37cf
Maxim Kolosovskiy <kolos@chromium.org>
2017-11-29 08:58:51 +0000

    [Password Manager] Calculate form signature of a observed form of PasswordFormManager only once
    The signature of |PasswordFormManager::observed_form_| is used for form matching. Since |observed_form| is const field, its signature doesn't need to be re-calculated many times.
    
    Bug: 786941
    Change-Id: I9705a64055c18ca83fd50dce869468734adee337
    Reviewed-on: https://chromium-review.googlesource.com/793835
    Commit-Queue: Maxim Kolosovskiy <kolos@chromium.org>
    Reviewed-by: Vaclav Brozek <vabr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520050}

--- components/password_manager/core/browser/password_form_manager.h
+++ components/password_manager/core/browser/password_form_manager.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/field_types.h"
 #include "components/autofill/core/browser/form_structure.h"
 #include "components/autofill/core/common/password_form.h"
+#include "components/autofill/core/common/signatures_util.h"
 #include "components/password_manager/core/browser/form_fetcher.h"
 #include "components/password_manager/core/browser/password_form_metrics_recorder.h"
 #include "components/password_manager/core/browser/password_form_user_action.h"
@@ -478,6 +479,9 @@ class PasswordFormManager : public FormFetcher::Consumer {
 
   // The PasswordForm from the page or dialog managed by |this|.
   const autofill::PasswordForm observed_form_;
+
+  // The form signature of |observed_form_|
+  const autofill::FormSignature observed_form_signature_;
 
   // Stores a submitted form.
   std::unique_ptr<const autofill::PasswordForm> submitted_form_;
