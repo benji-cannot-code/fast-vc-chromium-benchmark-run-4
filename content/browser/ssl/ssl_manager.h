95dde06841ed40a3b67f51422d7dde866c4a47af
John Abd-El-Malek <jam@chromium.org>
2017-11-29 21:14:13 +0000

    Add a histogram for how often a request which revokes a certificate exception is for a frame.
    The motivation is that if it's a small % of the time, then monitoring frames only would be sufficient.
    
    Bug: 783981
    Change-Id: Id90a871b1175eddc10597766488be286bc8b763d
    Reviewed-on: https://chromium-review.googlesource.com/795355
    Reviewed-by: Emily Stark <estark@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520243}

--- content/browser/ssl/ssl_manager.h
+++ content/browser/ssl/ssl_manager.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/content_export.h"
 #include "content/public/browser/global_request_id.h"
 #include "content/public/browser/ssl_status.h"
+#include "content/public/common/resource_type.h"
 #include "net/base/net_errors.h"
 #include "net/cert/cert_status_flags.h"
 #include "url/gurl.h"
@@ -76,7 +77,8 @@ class CONTENT_EXPORT SSLManager {
   void DidCommitProvisionalLoad(const LoadCommittedDetails& details);
   void DidStartResourceResponse(const GURL& url,
                                 bool has_certificate,
-                                net::CertStatus ssl_cert_status);
+                                net::CertStatus ssl_cert_status,
+                                ResourceType resource_type);
 
   // The following methods are called when a page includes insecure
   // content. These methods update the SSLStatus on the NavigationEntry
