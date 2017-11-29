f383a8079a2f89e4877a7a4a56a86dcbb7bce692
John Z Wu <jzw@chromium.org>
2017-11-29 18:21:51 +0000

    Add method to remove suggestions.
    Removing suggestions will remove the underlying autofill data from which
    it originated. It is possible to remove both autocomplete suggestions
    as well as profile data for addresses and credit cards.
    
    This method will be covered with integration tests in follow up CLs.
    
    Bug: 
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I53a273eed33024e42c89b85cd10dd718cea00884
    Reviewed-on: https://chromium-review.googlesource.com/792256
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: John Wu <jzw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520168}

--- ios/web_view/internal/autofill/cwv_autofill_controller.mm
+++ ios/web_view/internal/autofill/cwv_autofill_controller.mm
@@ -170,6 +170,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                     }];
 }
 
+- (void)removeSuggestion:(CWVAutofillSuggestion*)suggestion {
+  // Identifier is greater than 0 when it is an autofill profile.
+  if (suggestion.formSuggestion.identifier > 0) {
+    _autofillManager->RemoveAutofillProfileOrCreditCard(
+        suggestion.formSuggestion.identifier);
+  } else {
+    _autofillManager->RemoveAutocompleteEntry(
+        base::SysNSStringToUTF16(suggestion.fieldName),
+        base::SysNSStringToUTF16(suggestion.value));
+  }
+}
+
 #pragma mark - AutofillClientIOSBridge | AutofillDriverIOSBridge
 
 - (void)showAutofillPopup:(const std::vector<autofill::Suggestion>&)suggestions
