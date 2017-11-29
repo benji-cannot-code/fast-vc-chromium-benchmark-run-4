23d274d3615c025aae937e169d4866cf34217509
Robert Hogan <robhogan@gmail.com>
2017-11-29 18:59:46 +0000

    Fix position:sticky on nested table parts
    Bug: 702927
    Change-Id: Ifeaa89a292e96bb66a26507ab85cf4e3a81d42c4
    Reviewed-on: https://chromium-review.googlesource.com/786014
    Commit-Queue: Robert Hogan <robhogan@gmail.com>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520181}

--- third_party/WebKit/Source/core/layout/LayoutBoxModelObject.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBoxModelObject.cpp
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/layout/LayoutFlexibleBox.h"
 #include "core/layout/LayoutGeometryMap.h"
 #include "core/layout/LayoutInline.h"
+#include "core/layout/LayoutTableCell.h"
 #include "core/layout/LayoutView.h"
 #include "core/paint/ObjectPaintInvalidator.h"
 #include "core/paint/PaintLayer.h"
@@ -828,7 +829,9 @@ void LayoutBoxModelObject::UpdateStickyPositionConstraints() const {
   LayoutBlock* containing_block = this->ContainingBlock();
   // The location container for boxes is not always the containing block.
   LayoutObject* location_container =
-      IsLayoutInline() ? Container() : ToLayoutBox(this)->LocationContainer();
+      IsLayoutInline() || IsTableCell()
+          ? Container()
+          : ToLayoutBox(this)->LocationContainer();
   // Skip anonymous containing blocks.
   while (containing_block->IsAnonymous()) {
     containing_block = containing_block->ContainingBlock();
