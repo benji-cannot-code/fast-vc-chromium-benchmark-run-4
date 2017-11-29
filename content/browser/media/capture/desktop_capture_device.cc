b3ad99df83f165710b1072613eb0d0dd1ec69599
Weiyong Yao <braveyao@chromium.org>
2017-11-29 21:55:40 +0000

    [DesktopCapture] add back browser test cases for window/tab capture
    Since cl https://codereview.chromium.org/2721113002/, gUM needs the
    OnStarted event form capture device for the successCallback. (In the past
    gUM will succeed unconditionally even before creating capture device.)
    At that time some browser test cases for window/tab capture were commented
    out because the window/tab capturer can't work with the fake source id and
    will fail gUM eventually.
    
    In the cl, we will create fake capturers to handle the fake source id
    to give the expected OnStart event for window/tab capture. Then we can
    make the browser test for desktop capture really work.
    
    Bug: 699201
    Change-Id: Id503400807f553336605443bf51b8512c1f574d9
    Reviewed-on: https://chromium-review.googlesource.com/782704
    Commit-Queue: Weiyong Yao <braveyao@chromium.org>
    Reviewed-by: Zijie He <zijiehe@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520260}

--- content/browser/media/capture/desktop_capture_device.cc
+++ content/browser/media/capture/desktop_capture_device.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/webrtc/modules/desktop_capture/desktop_capture_options.h"
 #include "third_party/webrtc/modules/desktop_capture/desktop_capturer.h"
 #include "third_party/webrtc/modules/desktop_capture/desktop_frame.h"
+#include "third_party/webrtc/modules/desktop_capture/fake_desktop_capturer.h"
 #include "third_party/webrtc/modules/desktop_capture/mouse_cursor_monitor.h"
 
 namespace content {
@@ -428,6 +429,14 @@ std::unique_ptr<media::VideoCaptureDevice> DesktopCaptureDevice::Create(
     const DesktopMediaID& source) {
   auto options = CreateDesktopCaptureOptions();
   std::unique_ptr<webrtc::DesktopCapturer> capturer;
+  std::unique_ptr<media::VideoCaptureDevice> result;
+
+  // For browser tests, to create a fake desktop capturer.
+  if (source.id == DesktopMediaID::kFakeId) {
+    capturer.reset(new webrtc::FakeDesktopCapturer());
+    result.reset(new DesktopCaptureDevice(std::move(capturer), source.type));
+    return result;
+  }
 
   switch (source.type) {
     case DesktopMediaID::TYPE_SCREEN: {
@@ -461,7 +470,6 @@ std::unique_ptr<media::VideoCaptureDevice> DesktopCaptureDevice::Create(
     default: { NOTREACHED(); }
   }
 
-  std::unique_ptr<media::VideoCaptureDevice> result;
   if (capturer)
     result.reset(new DesktopCaptureDevice(std::move(capturer), source.type));
 
