2fad0c853676d700ab85a7898c5af59432bbc5da
Katie Dektar <katydek@google.com>
2017-11-29 20:07:48 +0000

    Adds |unclippedLocation| to the Automation API.
    |unclippedLocation| is the location of the node if it were not
    clipped by any of its ancestors. This is now used by ChromeVox
    to determine navigation by line, rather than the clipped
    location.
    
    This change reverts bf57671, submitted in
    https://chromium-review.googlesource.com/c/chromium/src/+/777541,
    which disabled clipping until the fix could be added.
    
    Bug: 788810
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I50d4df7364c32384ccfb06a04b226afce67eb091
    Reviewed-on: https://chromium-review.googlesource.com/794511
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Katie D <katie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520212}

--- chrome/renderer/resources/extensions/automation/automation_node.js
+++ chrome/renderer/resources/extensions/automation/automation_node.js
@@ -182,6 +182,14 @@ var GetBoundsForRange = natives.GetBoundsForRange;
 /**
  * @param {number} axTreeID The id of the accessibility tree.
  * @param {number} nodeID The id of a node.
+ * @return {?automation.Rect} The unclipped location of the node, or
+ * undefined if the tree or node wasn't found.
+ */
+var GetUnclippedLocation = natives.GetUnclippedLocation;
+
+/**
+ * @param {number} axTreeID The id of the accessibility tree.
+ * @param {number} nodeID The id of a node.
  * @return {!Array.<number>} The text offset where each line starts, or an empty
  *     array if this node has no text content, or undefined if the tree or node
  *     was not found.
@@ -352,6 +360,13 @@ AutomationNodeImpl.prototype = {
 
   boundsForRange: function(startIndex, endIndex) {
     return GetBoundsForRange(this.treeID, this.id, startIndex, endIndex);
+  },
+
+  get unclippedLocation() {
+    var result = GetUnclippedLocation(this.treeID, this.id);
+    if (result === undefined)
+      result = GetLocation(this.treeID, this.id);
+    return result;
   },
 
   get indexInParent() {
@@ -1274,6 +1289,7 @@ utils.expose(AutomationNode, AutomationNodeImpl, {
       'underline',
       'lineThrough',
       'customActions',
+      'unclippedLocation',
   ]),
 });
 
