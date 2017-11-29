76c324faeda565678b2d8f83f8e1b762970f6f25
Kevin Cernekee <cernekee@chromium.org>
2017-11-29 00:31:52 +0000

    ONC: Add TLSVersionMax EAP property
    This allows CPanel to disable TLSv1.1/TLSv1.2 for compatibility with
    buggy wifi APs.
    
    BUG=605728
    TEST=chromeos_unittests
    TEST="sideload" an .onc file through chrome://net-internals and verify
         in the RADIUS logs that the TLSVersionMax setting takes effect
    TEST=manually set value through shill and check in
         networkingPrivate.getProperties
    
    Change-Id: I1c8447ca5f7c8b0c8ff3c4ce44be01955454d215
    Reviewed-on: https://chromium-review.googlesource.com/789792
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Kevin Cernekee <cernekee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519926}

--- components/onc/onc_constants.cc
+++ components/onc/onc_constants.cc
@@ -316,6 +316,7 @@ const char kServerCAPEMs[] = "ServerCAPEMs";
 const char kServerCARef[] = "ServerCARef";
 const char kServerCARefs[] = "ServerCARefs";
 const char kSubjectMatch[] = "SubjectMatch";
+const char kTLSVersionMax[] = "TLSVersionMax";
 const char kUseSystemCAs[] = "UseSystemCAs";
 const char kUseProactiveKeyCaching[] = "UseProactiveKeyCaching";
 }  // namespace eap
