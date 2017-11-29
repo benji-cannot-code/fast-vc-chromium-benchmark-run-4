20e3e0b5aedc603d81445dbf9879d53a2d45a404
Theresa Wellington <twellington@google.com>
2017-11-29 21:11:26 +0000

    [Bookmarks] Set state before showing search UI
    Set the search state before showing the search UI to prevent mismatch
    between the state stack and the UI's concept of which state is showing.
    
    This prevents a crash when quickly tapping the search icon after taking
    some action to close bookmarks.
    
    BUG=787353
    
    Change-Id: I1728241d7aa386d9b1847754805cb8c255be06ca
    Reviewed-on: https://chromium-review.googlesource.com/797350
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520241}

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkManager.java
@@ -404,9 +404,9 @@ public class BookmarkManager implements BookmarkDelegate, SearchDelegate,
 
     @Override
     public void openSearchUI() {
+        setState(BookmarkUIState.createSearchState());
         mSelectableListLayout.onStartSearch();
         mToolbar.showSearchView();
-        setState(BookmarkUIState.createSearchState());
     }
 
     @Override
