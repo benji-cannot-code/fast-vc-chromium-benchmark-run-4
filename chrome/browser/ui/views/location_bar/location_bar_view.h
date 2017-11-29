459de30a36d5cf35554eaa7c1ad2dc659cdf8980
Christopher Lam <calamity@chromium.org>
2017-11-29 02:11:16 +0000

    Reland "[desktop-pwas] Add content setting indicators to Hosted App frame."
    Relanded with an uninitialized field fixed.
    
    This CL adds ContentSettingImageViews to the HostedAppButtonContainer by:
    - Factoring out a Delegate for ContentSettingImageView which the
      HostedAppButtonContainer and LocationBarView implement
    - Adding an icon color override to ContentSettingImageView
    
    This all allows the content setting indicators to be embedded in the
    hosted app frame, get notified of content setting changes, and respond
    to active/inactive states.
    
    TBR=tapted@chromium.org, jamescook@chromium.org
    
    Bug: 762401
    Change-Id: I28e1f708f25dbb9722384fb23e9ca2825b5b4e10
    Reviewed-on: https://chromium-review.googlesource.com/795271
    Reviewed-by: calamity <calamity@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519962}

--- chrome/browser/ui/views/location_bar/location_bar_view.h
+++ chrome/browser/ui/views/location_bar/location_bar_view.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/dropdown_bar_host.h"
 #include "chrome/browser/ui/views/dropdown_bar_host_delegate.h"
 #include "chrome/browser/ui/views/extensions/extension_popup.h"
+#include "chrome/browser/ui/views/location_bar/content_setting_image_view.h"
 #include "chrome/browser/ui/views/omnibox/omnibox_view_views.h"
 #include "components/prefs/pref_member.h"
 #include "components/security_state/core/security_state.h"
@@ -32,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class CommandUpdater;
 class ContentSettingBubbleModelDelegate;
-class ContentSettingImageView;
 class FindBarIcon;
 class GURL;
 class IntentPickerView;
@@ -70,7 +70,8 @@ class LocationBarView : public LocationBar,
                         public ChromeOmniboxEditController,
                         public DropdownBarHostDelegate,
                         public zoom::ZoomEventManagerObserver,
-                        public views::ButtonListener {
+                        public views::ButtonListener,
+                        public ContentSettingImageView::Delegate {
  public:
   class Delegate {
    public:
@@ -237,6 +238,11 @@ class LocationBarView : public LocationBar,
   void UpdateWithoutTabRestore() override;
   ToolbarModel* GetToolbarModel() override;
   content::WebContents* GetWebContents() override;
+
+  // ContentSettingImageView::Delegate:
+  content::WebContents* GetContentSettingWebContents() override;
+  ContentSettingBubbleModelDelegate* GetContentSettingBubbleModelDelegate()
+      override;
 
   // ZoomEventManagerObserver:
   // Updates the view for the zoom icon when default zoom levels change.
