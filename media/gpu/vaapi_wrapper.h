7e65f3aab62b466656809754dfe4b0480101fe24
Julien Isorce <julien.isorce@chromium.org>
2017-11-29 19:43:39 +0000

    Fix build for VaapiTFPPicture
    Introduced recently by https://chromium-review.googlesource.com/756711
      "Wrap X11 headers in ui/gfx/x/x11.h"
    
    Build error 1:
      media/gpu/vaapi/vaapi_tfp_picture.cc:
        error use of undeclared identifier 'RootWindow'; did you mean 'XRootWindow'?
          x_pixmap_ = XCreatePixmap(x_display_, RootWindow(x_display_, screen),
    
    Build error 2:
      In file included from ../../media/gpu/vaapi_video_decode_accelerator_unittest.cc:14:
      error: expected unqualified-id
        /usr/include/X11/X.h:115:30: note: expanded from macro 'None'
    
    Bug: 782184
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I1b1ecf39baee160e1778e5cdd596426a63e4e054
    Reviewed-on: https://chromium-review.googlesource.com/788111
    Reviewed-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Julien Isorce <julien.isorce@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520205}

--- media/gpu/vaapi_wrapper.h
+++ media/gpu/vaapi_wrapper.h
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/size.h"
 
 #if defined(USE_X11)
-#include <va/va_x11.h>
+#include "ui/gfx/x/x11.h"
 #endif  // USE_X11
 
 namespace gfx {
