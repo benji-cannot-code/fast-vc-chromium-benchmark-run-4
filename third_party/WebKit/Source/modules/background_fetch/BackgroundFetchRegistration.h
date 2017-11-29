e64df061f9d5422fe7f4e6c3f511690959652968
zhuoyu.qian <zhuoyu.qian@samsung.com>
2017-11-29 06:46:58 +0000

    Delete title of BackgroundFetchRegistration
    The BackgroundFetchRegistration.title is deprecated, remove it.
    
    BUG=769770
    R=peter@chromium.org
    
    Signed-off-by: zhuoyu.qian <zhuoyu.qian@samsung.com>
    Change-Id: I8f9da3138ffddffd06d611dd52b3379f5f72289b
    Reviewed-on: https://chromium-review.googlesource.com/792770
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520032}

--- third_party/WebKit/Source/modules/background_fetch/BackgroundFetchRegistration.h
+++ third_party/WebKit/Source/modules/background_fetch/BackgroundFetchRegistration.h
@@ -35,8 +35,7 @@ class BackgroundFetchRegistration final
                               unsigned long long upload_total,
                               unsigned long long uploaded,
                               unsigned long long download_total,
-                              unsigned long long downloaded,
-                              const String& title);
+                              unsigned long long downloaded);
   ~BackgroundFetchRegistration() override;
 
   // Initializes the BackgroundFetchRegistration to be associated with the given
@@ -62,9 +61,6 @@ class BackgroundFetchRegistration final
   DEFINE_ATTRIBUTE_EVENT_LISTENER(progress);
 
   ScriptPromise abort(ScriptState*);
-
-  // TODO(crbug.com/769770): Remove the following deprecated attributes.
-  String title() const;
 
   // EventTargetWithInlineData implementation.
   const AtomicString& InterfaceName() const override;
@@ -92,7 +88,6 @@ class BackgroundFetchRegistration final
   unsigned long long uploaded_;
   unsigned long long download_total_;
   unsigned long long downloaded_;
-  String title_;
 
   mojo::Binding<blink::mojom::blink::BackgroundFetchRegistrationObserver>
       observer_binding_;
