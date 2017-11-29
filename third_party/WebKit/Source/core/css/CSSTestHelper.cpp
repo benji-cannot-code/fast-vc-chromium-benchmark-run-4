67e9585af4eff8e663317bb47a772a9ad82e9110
Eddy Mead <meade@chromium.org>
2017-11-29 04:46:24 +0000

    Update behavior of CSSStyleSheet to match spec for Security origin
    Spec is here:
    https://www.w3.org/TR/cssom-1/#the-cssstylesheet-interface
    
    Updated: the following methods now throw a SecurityError if the
    style sheet is not accessible:
    - cssRules() / rules()
    - insertRule()
    - deleteRule()
    
    Bug: 775525
    Change-Id: I409d486df0f0fdced4684b19e3fae4c3a0ec2868
    Reviewed-on: https://chromium-review.googlesource.com/783911
    Commit-Queue: meade_UTC10 <meade@chromium.org>
    Reviewed-by: nainar <nainar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520005}

--- third_party/WebKit/Source/core/css/CSSTestHelper.cpp
+++ third_party/WebKit/Source/core/css/CSSTestHelper.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/css/CSSTestHelper.h"
 
+#include "bindings/core/v8/ExceptionState.h"
 #include "core/css/CSSRuleList.h"
 #include "core/css/CSSStyleSheet.h"
 #include "core/css/RuleSet.h"
@@ -50,7 +51,10 @@ CSSTestHelper::CSSTestHelper() {
 }
 
 CSSRuleList* CSSTestHelper::CssRules() {
-  return style_sheet_->cssRules();
+  DummyExceptionStateForTesting exception_state;
+  CSSRuleList* result = style_sheet_->cssRules(exception_state);
+  EXPECT_FALSE(exception_state.HadException());
+  return result;
 }
 
 RuleSet& CSSTestHelper::GetRuleSet() {
@@ -64,7 +68,7 @@ void CSSTestHelper::AddCSSRules(const char* css_text) {
   TextPosition position;
   unsigned sheet_length = style_sheet_->length();
   style_sheet_->Contents()->ParseStringAtPosition(css_text, position);
-  ASSERT_TRUE(style_sheet_->length() > sheet_length);
+  ASSERT_GT(style_sheet_->length(), sheet_length);
 }
 
 }  // namespace blink
