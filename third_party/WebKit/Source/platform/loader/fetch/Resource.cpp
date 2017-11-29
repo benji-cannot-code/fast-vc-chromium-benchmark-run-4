ead0363da5353b84085adebd413817b80c3d1d44
Eric Lawrence <elawrence@chromium.org>
2017-11-29 16:54:17 +0000

    Remove misleading comment
    The comment suggested that "must-revalidate" might be a reason not to
    reuse a response. This is misleading because must-revalidate is a poorly
    named directive which simply means that the cache freshness lifetime
    should not be ignored. It does not forbid caching or require
    revalidation of resources within their freshness lifetime.
    
    Change-Id: Ib8e9f9c9f588fb018fc256445dc1f88bd2961a9c
    Reviewed-on: https://chromium-review.googlesource.com/794049
    Reviewed-by: Yoav Weiss <yoav@yoav.ws>
    Commit-Queue: Eric Lawrence <elawrence@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520144}

--- third_party/WebKit/Source/platform/loader/fetch/Resource.cpp
+++ third_party/WebKit/Source/platform/loader/fetch/Resource.cpp
@@ -512,8 +512,6 @@ static bool CanUseResponse(const ResourceResponse& response,
   if (response.IsNull())
     return false;
 
-  // FIXME: Why isn't must-revalidate considered a reason we can't use the
-  // response?
   if (response.CacheControlContainsNoCache() ||
       response.CacheControlContainsNoStore())
     return false;
