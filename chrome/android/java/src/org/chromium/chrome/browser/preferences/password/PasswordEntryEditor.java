66a9be1cc912b3ff20a02c382a0c979d839f9cd0
Vaclav Brozek <vabr@chromium.org>
2017-11-29 19:29:33 +0000

    [Android password settings] Fix reauth explanation
    If the user tries to view or copy passwords in Chrome on Android, the
    settings page asks them to reauthenticate. The explanation on the
    reauthentication screen is currently confusing, telling the user that
    they need to reauthenticate to "continue setting up [their] phone".
    
    This CL changes that message to mirror better the user's original
    intention, mentioning copying or viewing as appropriate.
    
    The change has no test. While the instrumentation tests are able to trigger the
    reauthentication screen, there seems to be no way to read UI elements on that,
    because it is being rendered by Android and not by Chrome.
    
    Bug: 788749
    Change-Id: I06c25c837606aa427cb5cb6db39e8e3fbeb5aa07
    Reviewed-on: https://chromium-review.googlesource.com/796630
    Commit-Queue: Vaclav Brozek <vabr@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520197}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/password/PasswordEntryEditor.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/password/PasswordEntryEditor.java
@@ -366,8 +366,18 @@ public class PasswordEntryEditor extends Fragment {
                 PASSWORD_ACTION_BOUNDARY);
     }
 
-    private void displayReauthenticationFragment() {
+    /**
+     * Initiates the reauthentication prompt with a given description.
+     *
+     * @param descriptionId The resource ID of the string to be displayed to explain the reason for
+     *                      the reauthentication.
+     */
+    private void displayReauthenticationFragment(int descriptionId) {
         Fragment passwordReauthentication = new PasswordReauthenticationFragment();
+        Bundle args = new Bundle();
+        args.putInt(PasswordReauthenticationFragment.DESCRIPTION_ID, descriptionId);
+        passwordReauthentication.setArguments(args);
+
         FragmentManager fragmentManager = getFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(
@@ -392,7 +402,7 @@ public class PasswordEntryEditor extends Fragment {
                     copyPassword();
                 } else {
                     mCopyButtonPressed = true;
-                    displayReauthenticationFragment();
+                    displayReauthenticationFragment(R.string.lockscreen_description_copy);
                 }
             }
         });
@@ -413,7 +423,7 @@ public class PasswordEntryEditor extends Fragment {
                     displayPassword();
                 } else {
                     mViewButtonPressed = true;
-                    displayReauthenticationFragment();
+                    displayReauthenticationFragment(R.string.lockscreen_description_view);
                 }
             }
         });
