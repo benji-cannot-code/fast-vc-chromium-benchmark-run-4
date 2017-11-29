979e2b7c75287ef336a81b41b5b756152fee9e2a
Erik Luo <luoe@chromium.org>
2017-11-29 01:54:51 +0000

    DevTools: fix geolocation toSetting() serialization
    The Geolocation struct used to hold a string-type error property,
    but was changed into a boolean here:
    https://codereview.chromium.org/1917543002
    
    There was a mistake in that CL, where it was still expecting a
    string-type error, causing serialization to always return an
    empty string.
    
    This CL fixes the serializer, allowing the last applied
    geolocation to be saved.
    
    Bug: 649657
    Change-Id: I7f65f7844dca9ee2e182970602c6810a3c776973
    Reviewed-on: https://chromium-review.googlesource.com/778044
    Commit-Queue: Erik Luo <luoe@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519953}

--- third_party/WebKit/Source/devtools/front_end/sdk/EmulationModel.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/EmulationModel.js
@@ -176,7 +176,7 @@ SDK.EmulationModel.Geolocation = class {
         var splitPosition = splitError[0].split('@');
         if (splitPosition.length === 2) {
           return new SDK.EmulationModel.Geolocation(
-              parseFloat(splitPosition[0]), parseFloat(splitPosition[1]), splitError[1]);
+              parseFloat(splitPosition[0]), parseFloat(splitPosition[1]), !!splitError[1]);
         }
       }
     }
@@ -226,9 +226,7 @@ SDK.EmulationModel.Geolocation = class {
    * @return {string}
    */
   toSetting() {
-    return (typeof this.latitude === 'number' && typeof this.longitude === 'number' && typeof this.error === 'string') ?
-        this.latitude + '@' + this.longitude + ':' + this.error :
-        '';
+    return this.latitude + '@' + this.longitude + ':' + (this.error || '');
   }
 };
 
