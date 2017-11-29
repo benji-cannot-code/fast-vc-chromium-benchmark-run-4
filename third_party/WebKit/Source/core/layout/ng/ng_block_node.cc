f3574292c6e1710d7da759cf9e6879621f0d3e65
Yoshifumi Inoue <yosin@chromium.org>
2017-11-29 02:27:01 +0000

    Mark NGBlockNode::FirstChild() as const member function
    This patch marks |NGBlockNode::FirstChild()| as const member function since it
    doesn't mutate its member for improving code health.
    
    Note: This patch is a preparation for using |FirstChild()| in |CanNewLayout()|
    for disable content editable in LayoutNG[1].
    
    [1] http://crrev.com/c/792814  [LayoutNG] Disable LayoutNG for contenteditable
    
    Bug: 591099
    Change-Id: I205aea86bc15c47830c9c86e5949bd688c9c38e5
    Reviewed-on: https://chromium-review.googlesource.com/792813
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519976}

--- third_party/WebKit/Source/core/layout/ng/ng_block_node.cc
+++ third_party/WebKit/Source/core/layout/ng/ng_block_node.cc
@@ -280,7 +280,7 @@ NGLayoutInputNode NGBlockNode::NextSibling() const {
   return nullptr;
 }
 
-NGLayoutInputNode NGBlockNode::FirstChild() {
+NGLayoutInputNode NGBlockNode::FirstChild() const {
   auto* block = ToLayoutBlockFlow(box_);
   auto* child = GetLayoutObjectForFirstChildNode(block);
   if (!child)
