9dfb718a0cef5dc03c80802cf8203999082353f4
danakj <danakj@chromium.org>
2017-11-29 16:30:35 +0000

    viz: Drop and recreate LayerTreeFrameSinks when fallback to software
    This sets up the VizProcessTransportFactory to respond to the notice
    that it should use software compositing. In that case it will call
    OnLostMainThreadSharedContext() and release all the FrameSinks from
    ui::Compositors. These compositors then make a new LayerTreeFrameSink
    coming back to the VizProcessTransportFactory.
    
    Also eliminate the call to EstablishGpuChannel if software compositing
    is being used, and jump right to OnEstablishedGpuChannel() with a null
    GpuChannelHost.
    
    R=kylechar@chromium.org
    
    Bug: 730660
    Change-Id: I54feb3be3b27993c8b87a3ee30a210e540b2b1c8
    Reviewed-on: https://chromium-review.googlesource.com/789553
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520135}

--- content/browser/compositor/viz_process_transport_factory.h
+++ content/browser/compositor/viz_process_transport_factory.h
@@ -128,7 +128,7 @@ class VizProcessTransportFactory : public ui::ContextFactory,
 
   // Finishes creation of LayerTreeFrameSink after GPU channel has been
   // established.
-  void CreateLayerTreeFrameSinkForGpuChannel(
+  void OnEstablishedGpuChannel(
       base::WeakPtr<ui::Compositor> compositor_weak_ptr,
       scoped_refptr<gpu::GpuChannelHost> gpu_channel);
 
@@ -137,6 +137,8 @@ class VizProcessTransportFactory : public ui::ContextFactory,
   // nothing. Returns true if ContextProviders exist.
   bool CreateContextProviders(
       scoped_refptr<gpu::GpuChannelHost> gpu_channel_host);
+
+  void OnLostMainThreadSharedContext();
 
   gpu::GpuChannelEstablishFactory* const gpu_channel_establish_factory_;
   scoped_refptr<base::SingleThreadTaskRunner> const resize_task_runner_;
@@ -148,6 +150,7 @@ class VizProcessTransportFactory : public ui::ContextFactory,
   viz::ForwardingCompositingModeReporterImpl* const forwarding_mode_reporter_;
 
   base::flat_map<ui::Compositor*, CompositorData> compositor_data_map_;
+  bool is_gpu_compositing_disabled_ = false;
 
   // TODO(kylechar): Call OnContextLost() on observers when GPU crashes.
   base::ObserverList<ui::ContextFactoryObserver> observer_list_;
