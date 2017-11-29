e5f824203a15ac45b40da30509d7c46bc4011916
Xiaocheng Hu <xiaochengh@chromium.org>
2017-11-29 08:47:44 +0000

    Fix HTMLOutputElement::CanContainRangeEndPoint
    When an OUTPUT element is in default value mode, its content shouldn't
    be ignored by editing code. This patch changes CanContainRangeEndPoint
    to return true when in default value mode to fix it.
    
    This fixes a crash when computing NextBoundary: when the input
    position is at <output> <table>|</table></output>, it used to
    compute the local variables as:
    
    c = pos = TABLE@0
    boundary = TABLE
    start = OUTPUT@1 as ParentAnchoredEquivalent() moves the position out from table
    search_start = OUTPUT@AfterAnchor as OUTPUT is editing-ignored content
    search_end = TABLE@AfterChildren
    
    which results in a reversed search range. This patch makes OUTPUT not ignored
    by editing, and hence, changes |search_start| to OUTPUT@1 to stop the crash.
    
    Bug: 788661
    Change-Id: I1dda52949f2ba384ae937c4f5d42d5c828fc2a57
    Reviewed-on: https://chromium-review.googlesource.com/791856
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520049}

--- third_party/WebKit/Source/core/html/forms/HTMLOutputElement.h
+++ third_party/WebKit/Source/core/html/forms/HTMLOutputElement.h
@@ -52,7 +52,9 @@ class CORE_EXPORT HTMLOutputElement final : public HTMLFormControlElement {
   void setDefaultValue(const String&);
   DOMTokenList* htmlFor() const;
 
-  bool CanContainRangeEndPoint() const override { return false; }
+  bool CanContainRangeEndPoint() const override {
+    return is_default_value_mode_;
+  }
 
   void Trace(blink::Visitor*) override;
 
