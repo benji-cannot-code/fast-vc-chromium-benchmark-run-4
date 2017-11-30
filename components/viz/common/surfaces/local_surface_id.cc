23643890b105d2850fd3b3b939cf0dce59f4360d
Chris Blume <cblume@google.com>
2017-11-30 22:37:18 +0000

    Rename LocalSurfaceId::local_id() to parent_id()
    The local_id is really the parent_id. And since we will soon be adding a
    child_id to the mix, we should rename this to clarify.
    
    BUG=789259
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I29b48e21de5b1d137a4df4f4d9e9a3169e82c284
    Reviewed-on: https://chromium-review.googlesource.com/794374
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Fady Samuel <fsamuel@chromium.org>
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Commit-Queue: Chris Blume <cblume@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520725}

--- components/viz/common/surfaces/local_surface_id.cc
+++ components/viz/common/surfaces/local_surface_id.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace viz {
 
 std::string LocalSurfaceId::ToString() const {
-  return base::StringPrintf("LocalSurfaceId(%d, %s" PRIu64 ")", local_id_,
+  return base::StringPrintf("LocalSurfaceId(%d, %s" PRIu64 ")", parent_id_,
                             nonce_.ToString().c_str());
 }
 
