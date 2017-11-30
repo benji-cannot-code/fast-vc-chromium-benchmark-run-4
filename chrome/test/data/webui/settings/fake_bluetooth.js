07655e088bba517d90d94b2f57888e20b88add2f
Sarah Hu <xiaoyinh@chromium.org>
2017-11-30 01:06:49 +0000

    cros: limit number of bluetooth devices in the UI.
    If we intentionally send a lot of bluetooth advertisements
    (like 1000 per second), the chromeOS UI will keep updating new devices
    and eventually run out of memory.
    This CL set maximum number of bluetooth devices to be 50
    also adds a timer to avoid frequently update the system tray UI.
    
    
    Bug: 765371
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I1485d690a3e66b8580c2d0296c3297334a6a219a
    Reviewed-on: https://chromium-review.googlesource.com/765014
    Reviewed-by: Rahul Chaturvedi <rkc@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Xiaoyin Hu <xiaoyinh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520359}

--- chrome/test/data/webui/settings/fake_bluetooth.js
+++ chrome/test/data/webui/settings/fake_bluetooth.js
@@ -47,7 +47,7 @@ cr.define('settings', function() {
     setDevicesForTest: function(devices) {
       for (var d of this.devices)
         this.onDeviceRemoved.callListeners(d);
-      this.devices = devices;
+      this.devices = devices.slice();
       for (var d of this.devices)
         this.onDeviceAdded.callListeners(d);
     },
@@ -87,7 +87,7 @@ cr.define('settings', function() {
 
     /** @override */
     getDevices: function(callback) {
-      callback(this.devices);
+      callback(this.devices.slice());
     },
 
     /** @override */
