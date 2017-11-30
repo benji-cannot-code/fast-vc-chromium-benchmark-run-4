19e761fca7e64a62ddbdccf79d430c03ef895337
Benjamin C. Wiley Sittler <bsittler@chromium.org>
2017-11-30 05:00:23 +0000

    apache: pass $TMPDIR (win32: also %TEMP% and %TMP%) through to CGIs
    Bug: 786152
    Change-Id: Ib0e683a0fc93e485f16b4a99278e4e66fc0f6d1a
    Reviewed-on: https://chromium-review.googlesource.com/792610
    Commit-Queue: Benjamin Wiley Sittler <bsittler@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520446}

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/win.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/win.py
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import errno
 import logging
+import tempfile
 
 # The _winreg library is only available on Windows.
 # https://docs.python.org/2/library/_winreg.html
@@ -134,6 +135,12 @@ class WinPort(base.Port):
 
     def setup_environ_for_server(self):
         # A few extra environment variables are required for Apache on Windows.
+        if 'TEMP' not in self.host.environ:
+            self.host.environ['TEMP'] = tempfile.gettempdir()
+        # CGIs are run directory-relative so they need an absolute TEMP
+        self.host.environ['TEMP'] = self._filesystem.abspath(self.host.environ['TEMP'])
+        # Make TMP an alias for TEMP
+        self.host.environ['TMP'] = self.host.environ['TEMP']
         env = super(WinPort, self).setup_environ_for_server()
         apache_envvars = ['SYSTEMDRIVE', 'SYSTEMROOT', 'TEMP', 'TMP']
         for key, value in self.host.environ.copy().items():
