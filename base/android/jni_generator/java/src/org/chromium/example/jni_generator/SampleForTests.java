3991699706de2ce1715ffdb4ed7d0ed828ed4ee3
Sami Kalliomäki <sakal@chromium.org>
2017-11-30 17:15:32 +0000

    Add support for arguments with generic types in jni_generator.py.
    Adds supports for methods that have generic types in arguments. Return
    values with generic types are not supported yet.
    
    Bug: webrtc:8557
    Change-Id: I1af2b23a0adde561a2ce2cc3906124ec06f6c9d5
    Reviewed-on: https://chromium-review.googlesource.com/789941
    Commit-Queue: Sami Kalliomäki <sakal@chromium.org>
    Reviewed-by: agrieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520582}

--- base/android/jni_generator/java/src/org/chromium/example/jni_generator/SampleForTests.java
+++ base/android/jni_generator/java/src/org/chromium/example/jni_generator/SampleForTests.java
@@ -16,7 +16,9 @@ import org.chromium.base.annotations.NativeClassQualifiedName;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 // This class serves as a reference test for the bindings generator, and as example documentation
 // for how to use the jni generator.
@@ -144,6 +146,11 @@ class SampleForTests {
     @CalledByNative
     void packagePrivateJavaMethod() {
     }
+
+    // Method signature with generics in params.
+    @CalledByNative
+    public void methodWithGenericParams(
+            Map<String, Map<String, String>> foo, LinkedList<Integer> bar) {}
 
     // Constructors will be exported to C++ as:
     // Java_SampleForTests_Constructor(JNIEnv* env, jint foo, jint bar)
