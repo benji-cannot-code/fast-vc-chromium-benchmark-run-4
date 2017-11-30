046590a7288b3886ac911b5cf22c1c2c3dd27284
Katie Dektar <katydek@google.com>
2017-11-30 21:43:29 +0000

    Add settings and start and stop speech metrics to Select-to-Speak.
    Bug: 790045
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I438b266980762eff52a81968ae9fb53d8acc4f04
    Reviewed-on: https://chromium-review.googlesource.com/801335
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: Katie D <katie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520702}

--- chrome/browser/resources/chromeos/select_to_speak/test_support.js
+++ chrome/browser/resources/chromeos/select_to_speak/test_support.js
@@ -25,6 +25,11 @@ chrome.automation.RoleType = {
   INLINE_TEXT_BOX: 'inlineTextBox',
   PARAGRAPH: 'paragraph'
 };
+
 chrome.automation.StateType = {
   INVISIBLE: 'invisible'
+};
+
+chrome.metricsPrivate = {
+  recordUserAction: function() {}
 };
