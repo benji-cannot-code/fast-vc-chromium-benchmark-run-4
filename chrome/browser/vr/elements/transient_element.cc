f9007e84ad7d43a618544e2f7b7212462685d8cd
Ian Vollick <vollick@chromium.org>
2017-11-30 01:59:31 +0000

    [vr] Ensure that transient elements start timing when visible
    Previously, we'd set the start time of a transient element when it was
    first made visible. However, if frames are delayed and it is quite a
    long time before we receive another frame, by the time we actually try
    to draw, we will have exceeded the time limit and the transient element
    will hide the element. The net result is that it isn't seen.
    
    With this change, we set the start time in the first frame that it is
    visible.
    
    Bug: None
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ia8ec3decb1bc2ee9eff7e6213183e5eecd0e4215
    Reviewed-on: https://chromium-review.googlesource.com/798193
    Reviewed-by: Yash Malik <ymalik@chromium.org>
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520387}

--- chrome/browser/vr/elements/transient_element.cc
+++ chrome/browser/vr/elements/transient_element.cc
@@ -21,7 +21,7 @@ void TransientElement::SetVisible(bool visible) {
     return;
 
   if (visible)
-    set_visible_time_ = last_frame_time();
+    set_visible_time_ = base::TimeTicks();
 
   super::SetVisible(visible);
 }
@@ -29,7 +29,7 @@ void TransientElement::SetVisible(bool visible) {
 void TransientElement::SetVisibleImmediately(bool visible) {
   bool will_be_visible = GetTargetOpacity() == opacity_when_visible();
   if (!will_be_visible && visible)
-    set_visible_time_ = last_frame_time();
+    set_visible_time_ = base::TimeTicks();
 
   super::SetVisibleImmediately(visible);
 }
@@ -39,7 +39,7 @@ void TransientElement::RefreshVisible() {
   if (GetTargetOpacity() != opacity_when_visible())
     return;
 
-  set_visible_time_ = last_frame_time();
+  set_visible_time_ = base::TimeTicks();
 }
 
 SimpleTransientElement::SimpleTransientElement(const base::TimeDelta& timeout)
@@ -56,11 +56,12 @@ bool SimpleTransientElement::OnBeginFrame(
 
   // SetVisible may have been called during initialization which means that the
   // last frame time would be zero.
-  if (set_visible_time_.is_null())
+  if (set_visible_time_.is_null() && opacity() > 0.0f)
     set_visible_time_ = last_frame_time();
 
   base::TimeDelta duration = time - set_visible_time_;
-  if (duration >= timeout_) {
+
+  if (!set_visible_time_.is_null() && duration >= timeout_) {
     super::SetVisible(false);
     return true;
   }
@@ -86,16 +87,18 @@ bool ShowUntilSignalTransientElement::OnBeginFrame(
 
   // SetVisible may have been called during initialization which means that the
   // last frame time would be zero.
-  if (set_visible_time_.is_null())
+  if (set_visible_time_.is_null() && opacity() > 0.0f)
     set_visible_time_ = last_frame_time();
 
   bool set_invisible = false;
 
   base::TimeDelta duration = time - set_visible_time_;
-  if (duration > timeout_) {
+
+  if (!set_visible_time_.is_null() && duration > timeout_) {
     callback_.Run(TransientElementHideReason::kTimeout);
     set_invisible = true;
-  } else if (duration >= min_duration_ && signaled_) {
+  } else if (!set_visible_time_.is_null() && duration >= min_duration_ &&
+             signaled_) {
     callback_.Run(TransientElementHideReason::kSignal);
     set_invisible = true;
   }
