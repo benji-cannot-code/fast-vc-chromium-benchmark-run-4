530dd1659425b357410166620bd40eb670ca23c3
Mustaq Ahmed <mustaq@google.com>
2017-11-30 03:30:56 +0000

    Enable consuming activation in frame tree, extent expiry time.
    We are switching to the consumption model since Safari 11 seems to be
    matching Chrome & Edge now regarding this.  The activation expiry time
    is also increased to 1h to evaluate if the simplest model with no expiry
    (together with consumtion to confine its "reach") works as expected.
    
    Bug: 776404
    Change-Id: Ib452689131bc0426f0bb39ba91fcee22d4ac5756
    Reviewed-on: https://chromium-review.googlesource.com/794109
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520416}

--- third_party/WebKit/Source/core/frame/UserActivationState.cpp
+++ third_party/WebKit/Source/core/frame/UserActivationState.cpp
@@ -9,10 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-// This is a tentative timespan, which is more than the current limit of 1
-// sec (in UGI) because we want a reasonable value that works even for a slow
-// network.
-constexpr TimeDelta kActivationLifespan = TimeDelta::FromSeconds(10);
+// This is a tentative timespan, which should be more than the current limit of
+// 1 sec (in UGI) because we want a reasonable value that works even for a slow
+// network.  Currently we are experimenting with a vary large value (eqvt to no
+// expiry): crbug.com/776404.
+constexpr TimeDelta kActivationLifespan = TimeDelta::FromSeconds(3600);
 
 void UserActivationState::Activate() {
   has_been_active_ = true;
