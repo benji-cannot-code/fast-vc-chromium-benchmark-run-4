d7846a25303386371adfbc9d71940f0d92a58268
Chris Watkins <watk@chromium.org>
2017-11-30 03:19:57 +0000

    Run clang-tidy modernize-use-equals-{delete,default} on //courgette
    See the bugs and cxx post for justification and details:
    https://groups.google.com/a/chromium.org/forum/#!topic/cxx/RkOHzIK6Tq8
    
    This change was done using clang-tidy as described here:
    https://chromium.googlesource.com/chromium/src/+/lkcr/docs/clang_tidy.md
    
    In some cases the the tool leaves behind a string of commas where it
    replaced a member initializer list
    (https://bugs.llvm.org/show_bug.cgi?id=35051). They were cleaned up with:
      git diff --name-only | \
        xargs sed -E -i 's/(^\s*|\)\s*):[ ,]*= default/\1 = default/'
    
    BUG=778959,778957
    
    Change-Id: If5ff4c9b313efb82c9a53d2ea4540c5ef4a9abb0
    Reviewed-on: https://chromium-review.googlesource.com/789721
    Reviewed-by: Tommi <tommi@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Commit-Queue: Chris Watkins <watk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520412}

--- courgette/difference_estimator.cc
+++ courgette/difference_estimator.cc
@@ -79,7 +79,7 @@ class DifferenceEstimator::Subject {
   DISALLOW_COPY_AND_ASSIGN(Subject);
 };
 
-DifferenceEstimator::DifferenceEstimator() {}
+DifferenceEstimator::DifferenceEstimator() = default;
 
 DifferenceEstimator::~DifferenceEstimator() {
   for (size_t i = 0;  i < owned_bases_.size();  ++i)
--- courgette/streams.cc
+++ courgette/streams.cc
@@ -231,9 +231,7 @@ SourceStreamSet::SourceStreamSet()
     : count_(kMaxStreams) {
 }
 
-SourceStreamSet::~SourceStreamSet() {
-}
-
+SourceStreamSet::~SourceStreamSet() = default;
 
 // Initializes from |source|.
 // The stream set for N streams is serialized as a header
@@ -323,8 +321,7 @@ SinkStreamSet::SinkStreamSet()
   : count_(kMaxStreams) {
 }
 
-SinkStreamSet::~SinkStreamSet() {
-}
+SinkStreamSet::~SinkStreamSet() = default;
 
 void SinkStreamSet::Init(size_t stream_index_limit) {
   count_ = stream_index_limit;
