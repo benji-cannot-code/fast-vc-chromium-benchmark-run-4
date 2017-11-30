75422a1ce265d3f91511fcf411a69d4892ee7abc
Timothy Loh <timloh@chromium.org>
2017-11-30 03:47:49 +0000

    Revert "[css-typed-om] Implement CSSNumericValue.parse."
    This reverts commit 86e5b818d7f465e9b368e50de28d7eb2abce22d7.
    
    Reason for revert: Added test is failing on Mac, e.g.
    https://storage.googleapis.com/chromium-layout-test-archives/Mac10_11_Tests/20861/layout-test-results/results.html
    
    FAIL Parsing a calc with mixed compatible units returns correct CSSMathValue assert_equals: expected "px" but got "number"
    
    Original change's description:
    > [css-typed-om] Implement CSSNumericValue.parse.
    > 
    > This patch implements CSSNumericValue.parse. We reuse the existing
    > calc() parser, modifying it to keep bracket information since the result
    > of CSSNumericValue.parse is sensitive to brackets. We also have to
    > merge certain nodes in the parsed calc tree (e.g. calc(1 + 2 + 3) gets
    > merged into one sum node).
    > 
    > Spec: https://drafts.css-houdini.org/css-typed-om-1/#dom-cssnumericvalue-parse
    > 
    > Bug: 776173, 788570
    > Change-Id: Ia4bef7c3a2eb580d11a5e51d3921ed52e1f17bf3
    > Reviewed-on: https://chromium-review.googlesource.com/792670
    > Reviewed-by: nainar <nainar@chromium.org>
    > Commit-Queue: Darren Shen <shend@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#520361}
    
    TBR=nainar@chromium.org,shend@chromium.org
    
    Change-Id: Ib2acc1f4fc07390c75ecdda9991f984a55009a81
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 776173, 788570
    Reviewed-on: https://chromium-review.googlesource.com/798991
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520421}

--- third_party/WebKit/Source/core/css/CSSCalculationValue.cpp
+++ third_party/WebKit/Source/core/css/CSSCalculationValue.cpp
@@ -682,25 +682,10 @@ static ParseState CheckDepthAndIndex(int* depth, CSSParserTokenRange tokens) {
   return OK;
 }
 
-namespace {
-
-CSSCalcExpressionNode* CreateCalcBinaryOperation(CSSCalcExpressionNode* lhs,
-                                                 CSSCalcExpressionNode* rhs,
-                                                 CalcOperator op,
-                                                 bool simplify) {
-  if (simplify)
-    return CSSCalcBinaryOperation::CreateSimplified(lhs, rhs, op);
-  return CSSCalcBinaryOperation::Create(lhs, rhs, op);
-}
-
-}  // namespace
-
 class CSSCalcExpressionNodeParser {
   STACK_ALLOCATED();
 
  public:
-  CSSCalcExpressionNodeParser(bool simplify) : simplify_(simplify) {}
-
   CSSCalcExpressionNode* ParseCalc(CSSParserTokenRange tokens) {
     Value result;
     tokens.ConsumeWhitespace();
@@ -751,10 +736,7 @@ class CSSCalcExpressionNodeParser {
       CSSParserTokenRange inner_range = tokens.ConsumeBlock();
       tokens.ConsumeWhitespace();
       inner_range.ConsumeWhitespace();
-      if (!ParseValueExpression(inner_range, depth, result))
-        return false;
-      result->value->SetIsNestedCalc();
-      return true;
+      return ParseValueExpression(inner_range, depth, result);
     }
 
     return ParseValue(tokens, result);
@@ -780,10 +762,9 @@ class CSSCalcExpressionNodeParser {
       if (!ParseValueTerm(tokens, depth, &rhs))
         return false;
 
-      result->value = CreateCalcBinaryOperation(
+      result->value = CSSCalcBinaryOperation::CreateSimplified(
           result->value, rhs.value,
-          static_cast<CalcOperator>(operator_character), simplify_);
-
+          static_cast<CalcOperator>(operator_character));
       if (!result->value)
         return false;
     }
@@ -815,10 +796,9 @@ class CSSCalcExpressionNodeParser {
       if (!ParseValueMultiplicativeExpression(tokens, depth, &rhs))
         return false;
 
-      result->value = CreateCalcBinaryOperation(
+      result->value = CSSCalcBinaryOperation::CreateSimplified(
           result->value, rhs.value,
-          static_cast<CalcOperator>(operator_character), simplify_);
-
+          static_cast<CalcOperator>(operator_character));
       if (!result->value)
         return false;
     }
@@ -831,9 +811,6 @@ class CSSCalcExpressionNodeParser {
                             Value* result) {
     return ParseAdditiveValueExpression(tokens, depth, result);
   }
-
- private:
-  const bool simplify_;
 };
 
 CSSCalcExpressionNode* CSSCalcValue::CreateExpressionNode(
@@ -864,15 +841,7 @@ CSSCalcExpressionNode* CSSCalcValue::CreateExpressionNode(double pixels,
 
 CSSCalcValue* CSSCalcValue::Create(const CSSParserTokenRange& tokens,
                                    ValueRange range) {
-  CSSCalcExpressionNodeParser parser(false /* simplify */);
-  CSSCalcExpressionNode* expression = parser.ParseCalc(tokens);
-
-  return expression ? new CSSCalcValue(expression, range) : nullptr;
-}
-
-CSSCalcValue* CSSCalcValue::CreateSimplified(const CSSParserTokenRange& tokens,
-                                             ValueRange range) {
-  CSSCalcExpressionNodeParser parser(true /* simplify */);
+  CSSCalcExpressionNodeParser parser;
   CSSCalcExpressionNode* expression = parser.ParseCalc(tokens);
 
   return expression ? new CSSCalcValue(expression, range) : nullptr;
