d752d27ea50ce6bc073090af8bb13c87d056a977
Doug Turner <dougt@chromium.org>
2017-11-30 21:57:42 +0000

    Errors from the navigation throttle should broadcast to onReceivedError.
    On Android WebView, when a cancellation from a navigation throttle occurs
    we need to broadcast the error so that OnReceivedError can receive them.
    
    Bug: None
    Change-Id: I7c65fe070a702134283b48fc7a57170776111267
    Reviewed-on: https://chromium-review.googlesource.com/797995
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Doug Turner <dougt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520707}

--- android_webview/browser/aw_contents.cc
+++ android_webview/browser/aw_contents.cc
@@ -68,6 +68,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/gpu_data_manager.h"
 #include "content/public/browser/interstitial_page.h"
 #include "content/public/browser/navigation_entry.h"
+#include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/render_view_host.h"
@@ -1332,6 +1333,29 @@ void AwContents::RenderViewHostChanged(content::RenderViewHost* old_host,
   // new compositor is constructed.
   browser_view_renderer_.SetActiveCompositorID(
       CompositorID(process_id, routing_id));
+}
+
+void AwContents::DidFinishNavigation(
+    content::NavigationHandle* navigation_handle) {
+  // If this request was blocked in any way, broadcast an error.
+  net::Error error_code = navigation_handle->GetNetErrorCode();
+  if (error_code != net::ERR_BLOCKED_BY_CLIENT &&
+      error_code != net::ERR_BLOCKED_BY_ADMINISTRATOR &&
+      error_code != net::ERR_ABORTED) {
+    return;
+  }
+  AwContentsClientBridge* client =
+      AwContentsClientBridge::FromWebContents(web_contents_.get());
+  if (!client)
+    return;
+
+  AwWebResourceRequest request(navigation_handle->GetURL().spec(),
+                               navigation_handle->IsPost() ? "POST" : "GET",
+                               navigation_handle->IsInMainFrame(),
+                               navigation_handle->HasUserGesture(),
+                               net::HttpRequestHeaders());
+
+  client->OnReceivedError(request, error_code, false);
 }
 
 void AwContents::DidAttachInterstitialPage() {
