1228a76e599285b31162e39db7ef919d168d9d36
Mihai Sardarescu <msarda@chromium.org>
2017-11-30 15:53:23 +0000

    [signin][dice] Process enable sync Gaia header.
    This changes the way sync is enabled when the DICE sign-in flow is used.
    1. The refresh tokens are always fetched and then saved to the token service as soon
    as the auth code is received from Gaia.
    2. Once Chrome receives the enable sync signal, Chrome starts sync for the account
    as informed by Gaia.
    
    This has a side effect when signing in to Chrome with an enterprise account. In this case
    if the user chooses to create a new profile, then the refresh token is not moved to the
    new profile and the user needs to sign in to Chrome in the new profile.
    
    Bug: 789578
    Change-Id: Ic5174b80a3d891e7d84236279a36001ce5afe53c
    Reviewed-on: https://chromium-review.googlesource.com/796990
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520554}

--- chrome/browser/signin/dice_tab_helper.cc
+++ chrome/browser/signin/dice_tab_helper.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/signin/dice_tab_helper.h"
 #include "chrome/browser/signin/signin_util.h"
 #include "chrome/browser/ui/browser_finder.h"
+#include "components/signin/core/browser/profile_management_switches.h"
 #include "content/public/browser/navigation_handle.h"
 #include "google_apis/gaia/gaia_urls.h"
 
@@ -40,6 +41,13 @@ void DiceTabHelper::SetSigninReason(signin_metrics::Reason reason) {
 
 void DiceTabHelper::DidStartNavigation(
     content::NavigationHandle* navigation_handle) {
+  if (signin::GetAccountConsistencyMethod() !=
+      signin::AccountConsistencyMethod::kDicePrepareMigration) {
+    // Chrome relies on a Gaia signal to enable Chrome sync for all DICE methods
+    // different than prepare migration.
+    return;
+  }
+
   if (!should_start_sync_after_web_signin_)
     return;
 
