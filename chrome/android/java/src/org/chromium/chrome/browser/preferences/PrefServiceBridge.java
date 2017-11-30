8753ba40edca1431f506a566af2651e07e01b8ca
Leo Zhang <googleo@chromium.org>
2017-11-30 22:19:07 +0000

    Add language list and disable button in languages prefs.
    Add turn on/off translate switch.
    Read accept_langauges from native and show them in the languages page.
    For each language item, add a fake option memu.
    Screenshot: https://screenshot.googleplex.com/0HOVztouukP
    
    Bug: 783049
    Change-Id: If420d76fe12ecbff277bcd6e4ab7a3a7a3419f63
    Reviewed-on: https://chromium-review.googlesource.com/762540
    Commit-Queue: Leo Zhang <googleo@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Maria Khomenko <mariakhomenko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520715}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/PrefServiceBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/PrefServiceBridge.java
@@ -187,6 +187,11 @@ public final class PrefServiceBridge {
         list.add(exception);
     }
 
+    @CalledByNative
+    private static void copyLanguageList(List<String> list, String[] source) {
+        list.addAll(Arrays.asList(source));
+    }
+
     /**
      * Return the list of android permission strings for a given {@link ContentSettingsType}.  If
      * there is no permissions associated with the content setting, then an empty array is returned.
@@ -906,6 +911,15 @@ public final class PrefServiceBridge {
         nativeSetChromeHomePersonalizedOmniboxSuggestionsEnabled(enabled);
     }
 
+    /**
+     * Return the list of preferred languages strings.
+     */
+    public List<String> getChromeLanguageList() {
+        List<String> list = new ArrayList<>();
+        nativeGetChromeLanguageList(list);
+        return list;
+    }
+
     private native boolean nativeIsContentSettingEnabled(int contentSettingType);
     private native boolean nativeIsContentSettingManaged(int contentSettingType);
     private native void nativeSetContentSettingEnabled(int contentSettingType, boolean allow);
@@ -1076,4 +1090,5 @@ public final class PrefServiceBridge {
     private native String nativeGetLatestVersionWhenClickedUpdateMenuItem();
     private native void nativeSetSupervisedUserId(String supervisedUserId);
     private native void nativeSetChromeHomePersonalizedOmniboxSuggestionsEnabled(boolean enabled);
+    private native void nativeGetChromeLanguageList(List<String> list);
 }
---
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/languages/LanguageSelectionPreferences.java
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.preferences.languages;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+import org.chromium.chrome.R;
+
+/**
+ * Language selection fragment that lists all languages Chrome supports and allows users to
+ * add one of them into their preferred language list.
+ */
+public class LanguageSelectionPreferences extends PreferenceFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getActivity().setTitle(R.string.prefs_add_language);
+    }
+}
