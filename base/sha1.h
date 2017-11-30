befed8d153d193cefd74d72a4f13d453dce0f86d
Matt Mueller <mattm@chromium.org>
2017-11-30 00:25:04 +0000

    base::SHA1HashString should take a StringPiece rather than a std::string
    Change-Id: I6fba1ccacfe9bc9b3ba3cc84b3157876c97c88b7
    Reviewed-on: https://chromium-review.googlesource.com/798065
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520331}

--- base/sha1.h
+++ base/sha1.h
@@ -8,9 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 
-#include <string>
-
 #include "base/base_export.h"
+#include "base/strings/string_piece.h"
 
 namespace base {
 
@@ -20,7 +19,7 @@ static const size_t kSHA1Length = 20;  // Length in bytes of a SHA-1 hash.
 
 // Computes the SHA-1 hash of the input string |str| and returns the full
 // hash.
-BASE_EXPORT std::string SHA1HashString(const std::string& str);
+BASE_EXPORT std::string SHA1HashString(base::StringPiece str);
 
 // Computes the SHA-1 hash of the |len| bytes in |data| and puts the hash
 // in |hash|. |hash| must be kSHA1Length bytes long.
