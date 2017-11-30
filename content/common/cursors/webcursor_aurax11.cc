91ccced1b5b9cdaad9e40bfea7d3a105c64193bd
Daniel Bratell <bratell@opera.com>
2017-11-30 00:39:30 +0000

    Clearing content from include <X11/*>
    X11 headers leak a lot of macros named things like None, Status, True,
    False and they cause problems with normal code. To avoid that we now
    wrap all commonly named symbols in an x11 namespace and include
    "ui/gfx/x/x11.h" instead of <X11/*>.
    
    Bug: 782184
    Change-Id: I37bf100f2d2acc5d51ec62051e90939b56d9f690
    Reviewed-on: https://chromium-review.googlesource.com/797034
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Daniel Bratell <bratell@opera.com>
    Cr-Commit-Position: refs/heads/master@{#520344}

--- content/common/cursors/webcursor_aurax11.cc
+++ content/common/cursors/webcursor_aurax11.cc
@@ -5,15 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/common/cursors/webcursor.h"
 
-#include <X11/cursorfont.h>
-#include <X11/Xcursor/Xcursor.h>
-#include <X11/Xlib.h>
 
 #include "base/logging.h"
 #include "third_party/WebKit/public/platform/WebCursorInfo.h"
 #include "ui/base/cursor/cursor.h"
 #include "ui/base/cursor/cursor_loader_x11.h"
 #include "ui/base/x/x11_util.h"
+#include "ui/gfx/x/x11.h"
 
 namespace content {
 
--- content/renderer/render_view_browsertest.cc
+++ content/renderer/render_view_browsertest.cc
@@ -97,12 +97,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if defined(USE_AURA) && defined(USE_X11)
-#include <X11/Xlib.h>
 #include "base/memory/ptr_util.h"
 #include "ui/events/event_constants.h"
 #include "ui/events/keycodes/keyboard_code_conversion.h"
 #include "ui/events/test/events_test_utils.h"
 #include "ui/events/test/events_test_utils_x11.h"
+#include "ui/gfx/x/x11.h"
 #endif
 
 #if defined(USE_OZONE)
