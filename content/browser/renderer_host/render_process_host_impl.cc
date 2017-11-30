14eb1b29aa41191c3b67c271b806b07ddbf49cab
Zhenyao Mo <zmo@chromium.org>
2017-11-30 22:25:31 +0000

    [reland] PreEstablish a GPU channel in RenderProcessHostImpl before renderer asks for one.
    This CL was originally reviewed in
    https://chromium-review.googlesource.com/c/chromium/src/+/773538.
    
    Patchset 1 is exactly the same. Later patches are new fixes.
    
    BUG=783512
    TEST=bots
    R=piman@chromium.org
    TBR=sadrul@chromium.org
    
    Change-Id: If664ad636da5382fad35a49766e3a3162031f1ab
    Reviewed-on: https://chromium-review.googlesource.com/801333
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520719}

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -210,6 +210,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/public_features.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "ui/base/ui_base_switches.h"
+#include "ui/base/ui_base_switches_util.h"
 #include "ui/display/display_switches.h"
 #include "ui/gl/gl_switches.h"
 #include "ui/gl/gpu_switching_manager.h"
@@ -1355,6 +1356,9 @@ RenderProcessHostImpl::RenderProcessHostImpl(
   AddObserver(indexed_db_factory_.get());
 
   InitializeChannelProxy();
+
+  if (!switches::IsMusHostingViz())
+    gpu_client_.reset(new GpuClient(GetID()));
 }
 
 // static
@@ -1448,6 +1452,9 @@ bool RenderProcessHostImpl::Init() {
   base::FilePath renderer_path = ChildProcessHost::GetChildPath(flags);
   if (renderer_path.empty())
     return false;
+
+  if (gpu_client_)
+    gpu_client_->PreEstablishGpuChannel();
 
   sent_render_process_ready_ = false;
 
@@ -1884,8 +1891,12 @@ void RenderProcessHostImpl::RegisterMojoInterfaces() {
                  base::WrapRefCounted(
                      storage_partition_impl_->GetBackgroundFetchContext())));
 
-  registry->AddInterface(base::Bind(&RenderProcessHostImpl::CreateMusGpuRequest,
-                                    base::Unretained(this)));
+  if (gpu_client_) {
+    // |gpu_client_| outlives the registry, because its destruction is posted to
+    // IO thread from the destructor of |this|.
+    registry->AddInterface(
+        base::Bind(&GpuClient::Add, base::Unretained(gpu_client_.get())));
+  }
 
   registry->AddInterface(
       base::Bind(
@@ -1990,13 +2001,6 @@ void RenderProcessHostImpl::GetBlobURLLoaderFactory(
   }
   storage_partition_impl_->GetBlobURLLoaderFactory()->HandleRequest(
       std::move(request));
-}
-
-void RenderProcessHostImpl::CreateMusGpuRequest(ui::mojom::GpuRequest request) {
-  DCHECK_CURRENTLY_ON(BrowserThread::IO);
-  if (!gpu_client_)
-    gpu_client_.reset(new GpuClient(GetID()));
-  gpu_client_->Add(std::move(request));
 }
 
 void RenderProcessHostImpl::CreateOffscreenCanvasProvider(
--- services/ui/public/cpp/gpu/gpu.cc
+++ services/ui/public/cpp/gpu/gpu.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/message_loop.h"
 #include "base/synchronization/waitable_event.h"
 #include "base/threading/thread_task_runner_handle.h"
+#include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
 #include "gpu/command_buffer/common/scheduling_priority.h"
 #include "mojo/public/cpp/bindings/sync_call_restrictions.h"
@@ -237,6 +238,7 @@ void Gpu::EstablishGpuChannel(
 
 scoped_refptr<gpu::GpuChannelHost> Gpu::EstablishGpuChannelSync(
     bool* connection_error) {
+  TRACE_EVENT0("mus", "Gpu::EstablishGpuChannelSync");
   DCHECK(main_task_runner_->BelongsToCurrentThread());
   if (connection_error)
     *connection_error = false;
