cdf7ba9ab2b4fc21d62490512133bceee02841e6
Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
2017-11-30 06:25:54 +0000

    Stop persisting zip file volume mounted by Zip Archiver.
    This adds "persistent" flag to MountOptions in the file system provider
    API, so that extensions can choose to mount filesystem only in the current
    sign-in session.
    
    Bug: 789073
    Test: unit_tests --gtest_filter=FileSystemProviderServiceTest.*
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ib1e5f4249e915ca67622fac8ea50a8edd72408b8
    Reviewed-on: https://chromium-review.googlesource.com/792692
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Tomasz Mikolajewski <mtomasz@chromium.org>
    Commit-Queue: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520462}

--- chrome/browser/resources/chromeos/zip_archiver/js/app.js
+++ chrome/browser/resources/chromeos/zip_archiver/js/app.js
@@ -925,7 +925,8 @@ unpacker.app = {
                             {
                               fileSystemId: fileSystemId,
                               displayName: entry.name,
-                              openedFilesLimit: 1
+                              openedFilesLimit: 1,
+                              persistent: false
                             },
                             function() {
                               if (chrome.runtime.lastError) {
