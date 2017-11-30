6b0f40e9b9001adff21814e0a7baf65e8d72335e
Justin Novosad <junov@chromium.org>
2017-11-30 16:09:48 +0000

    Add low latency canvas code path via SurfaceLayer
    This CL re-purposes the commit flow from OffscreenCanvas for push
    <canvas> frames in low latency mode.  It also turns on low latency
    input events when ther are event listeners registered on the canvas.
    
    TBR=zmo@chromium.org
    
    Bug: 788439
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I8be2755ba953fd2c77e2420356ad582d96b95e7a
    Reviewed-on: https://chromium-review.googlesource.com/793950
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Olivia Lai <xlai@chromium.org>
    Commit-Queue: Justin Novosad <junov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520557}

--- third_party/WebKit/Source/core/html/HTMLCanvasElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLCanvasElement.cpp
@@ -75,6 +75,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/graphics/CanvasMetrics.h"
 #include "platform/graphics/GraphicsLayer.h"
 #include "platform/graphics/ImageBuffer.h"
+#include "platform/graphics/OffscreenCanvasFrameDispatcherImpl.h"
 #include "platform/graphics/RecordingImageBufferSurface.h"
 #include "platform/graphics/UnacceleratedImageBufferSurface.h"
 #include "platform/graphics/gpu/AcceleratedImageBufferSurface.h"
@@ -318,6 +319,18 @@ CanvasRenderingContext* HTMLCanvasElement::GetCanvasRenderingContext(
     DidDraw();
   }
 
+  if (attributes.lowLatency() &&
+      RuntimeEnabledFeatures::LowLatencyCanvasEnabled()) {
+    CreateLayer();
+    SetNeedsUnbufferedInputEvents(true);
+    // TODO: rename to CanvasFrameDispatcherImpl
+    frame_dispatcher_ = std::make_unique<OffscreenCanvasFrameDispatcherImpl>(
+        nullptr, surface_layer_bridge_->GetFrameSinkId().client_id(),
+        surface_layer_bridge_->GetFrameSinkId().sink_id(),
+        OffscreenCanvasFrameDispatcherImpl::kInvalidPlaceholderCanvasId,
+        size_.Width(), size_.Height());
+  }
+
   SetNeedsCompositingUpdate();
 
   return context_.Get();
@@ -373,7 +386,7 @@ void HTMLCanvasElement::DidDraw(const FloatRect& rect) {
     return;
   image_buffer_is_clear_ = false;
   ClearCopiedImage();
-  if (GetLayoutObject())
+  if (GetLayoutObject() && !LowLatencyEnabled())
     GetLayoutObject()->SetMayNeedPaintInvalidation();
   if (Is2d() && context_->ShouldAntialias() && GetPage() &&
       GetPage()->DeviceScaleFactorDeprecated() > 1.0f) {
@@ -392,6 +405,7 @@ void HTMLCanvasElement::DidDraw() {
 }
 
 void HTMLCanvasElement::FinalizeFrame() {
+  TRACE_EVENT0("blink", "HTMLCanvasElement::FinalizeFrame");
   if (GetImageBuffer()) {
     // Compute to determine whether disable accleration is needed
     if (IsAccelerated() &&
@@ -410,7 +424,24 @@ void HTMLCanvasElement::FinalizeFrame() {
       }
     }
 
-    image_buffer_->FinalizeFrame();
+    if (!LowLatencyEnabled())
+      image_buffer_->FinalizeFrame();
+
+    if (LowLatencyEnabled() && !dirty_rect_.IsEmpty()) {
+      // Push a frame
+      double start_time = WTF::MonotonicallyIncreasingTime();
+      scoped_refptr<StaticBitmapImage> image = image_buffer_->NewImageSnapshot(
+          kPreferAcceleration, kSnapshotReasonLowLatencyFrame);
+      FloatRect src_rect(0, 0, Size().Width(), Size().Height());
+      dirty_rect_.Intersect(src_rect);
+      IntRect int_dirty = EnclosingIntRect(dirty_rect_);
+      SkIRect damage_rect = SkIRect::MakeXYWH(
+          int_dirty.X(), int_dirty.Y(), int_dirty.Width(), int_dirty.Height());
+      frame_dispatcher_->DispatchFrame(image, start_time, damage_rect);
+      (void)start_time;
+      (void)damage_rect;
+      dirty_rect_ = FloatRect();
+    }
   }
 
   // If the canvas is visible, notifying listeners is taken
@@ -454,6 +485,10 @@ void HTMLCanvasElement::SetNeedsCompositingUpdate() {
 
 void HTMLCanvasElement::DoDeferredPaintInvalidation() {
   DCHECK(!dirty_rect_.IsEmpty());
+  if (LowLatencyEnabled()) {
+    // Low latency canvas handles dirty propagation in FinalizeFrame();
+    return;
+  }
   LayoutBox* layout_box = GetLayoutBox();
   if (Is2d()) {
     FloatRect src_rect(0, 0, Size().Width(), Size().Height());
@@ -707,6 +742,8 @@ void HTMLCanvasElement::SetSurfaceSize(const IntSize& size) {
   if (Is2d() && context_->isContextLost()) {
     context_->DidSetSurfaceSize();
   }
+  if (frame_dispatcher_)
+    frame_dispatcher_->Reshape(size_.Width(), size_.Height());
 }
 
 const AtomicString HTMLCanvasElement::ImageSourceURL() const {
@@ -913,6 +950,10 @@ bool HTMLCanvasElement::OriginClean() const {
 
 bool HTMLCanvasElement::ShouldAccelerate(AccelerationCriteria criteria) const {
   if (context_ && !Is2d())
+    return false;
+
+  // TODO(crbug.com/789232): Make low latency mode work with GPU acceleration
+  if (LowLatencyEnabled())
     return false;
 
   if (RuntimeEnabledFeatures::ForceDisplayList2dCanvasEnabled())
@@ -1124,7 +1165,6 @@ void HTMLCanvasElement::CreateImageBufferInternal(
   DCHECK(surface->IsValid());
   surface->SetCanvasResourceHost(this);
   image_buffer_ = ImageBuffer::Create(std::move(surface));
-
   did_fail_to_create_image_buffer_ = false;
 
   UpdateMemoryUsage();
