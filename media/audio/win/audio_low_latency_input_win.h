42a327c9fcff0688fbbaead21eab97b5e46fb546
Henrik Grunell <grunell@chromium.org>
2017-11-30 16:18:37 +0000

    Add WebRTC logging to Windows audio input implementation.
    Log failures in Open() and Start().
    
    Bug: 757737
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Idaad68af9aabc753cb4fca17b2dfff3cdf69d83b
    Reviewed-on: https://chromium-review.googlesource.com/795727
    Commit-Queue: Henrik Grunell <grunell@chromium.org>
    Reviewed-by: Max Morin <maxmorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520562}

--- media/audio/win/audio_low_latency_input_win.h
+++ media/audio/win/audio_low_latency_input_win.h
@@ -76,6 +76,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/scoped_com_initializer.h"
 #include "base/win/scoped_handle.h"
 #include "media/audio/agc_audio_stream.h"
+#include "media/audio/audio_manager.h"
 #include "media/base/audio_converter.h"
 #include "media/base/audio_parameters.h"
 #include "media/base/media_export.h"
@@ -96,7 +97,8 @@ class MEDIA_EXPORT WASAPIAudioInputStream
   // the audio manager who is creating this object.
   WASAPIAudioInputStream(AudioManagerWin* manager,
                          const AudioParameters& params,
-                         const std::string& device_id);
+                         const std::string& device_id,
+                         const AudioManager::LogCallback& log_callback);
 
   // The dtor is typically called by the AudioManager only and it is usually
   // triggered by calling AudioInputStream::Close().
@@ -124,9 +126,13 @@ class MEDIA_EXPORT WASAPIAudioInputStream
   // The Open() method is divided into these sub methods.
   HRESULT SetCaptureDevice();
   HRESULT GetAudioEngineStreamFormat();
-  bool DesiredFormatIsSupported();
+  // Returns whether the desired format is supported or not and writes the
+  // result of a failing system call to |*hr|, or S_OK if successful. If this
+  // function returns false with |*hr| == S_FALSE, the OS supports a closest
+  // match but we don't support conversion to it.
+  bool DesiredFormatIsSupported(HRESULT* hr);
   HRESULT InitializeAudioEngine();
-  void ReportOpenResult() const;
+  void ReportOpenResult(HRESULT hr) const;
 
   // AudioConverter::InputCallback implementation.
   double ProvideInput(AudioBus* audio_bus, uint32_t frames_delayed) override;
@@ -246,6 +252,9 @@ class MEDIA_EXPORT WASAPIAudioInputStream
   std::unique_ptr<AudioConverter> converter_;
   std::unique_ptr<AudioBus> convert_bus_;
   bool imperfect_buffer_size_conversion_ = false;
+
+  // Callback to send log messages.
+  AudioManager::LogCallback log_callback_;
 
   SEQUENCE_CHECKER(sequence_checker_);
 
