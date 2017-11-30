fed45f7ae2b968619827c8de4efb257083bfb608
Yoshifumi Inoue <yosin@chromium.org>
2017-11-30 07:48:34 +0000

    Introduce NGFragmentWithOffset struct
    This patch introduces |NGPhysicalFragmentWithOffset| as replacement of
    |NGInlineFragmentIterator::Result| to use it for other places such as
    |NGInlineFragmentTraversal::DescnednatsOf()|[1]
    
    
    This patch is a a preparation of the patch[2].
    
    [1] http://crrev.com/c/763188: [LayoutNG] Utilize DCHECK(CanUseInlineBox()) in
    core/layout/
    [2] http://crrev.com/c/795523: Adapt LayoutText::LinesBoundingBox() to LayoutNG
    
    Bug: 591099
    Change-Id: I6ec1f07e6316537a49b600dfa387291b5bc0969f
    Reviewed-on: https://chromium-review.googlesource.com/799610
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520472}

--- third_party/WebKit/Source/core/layout/ng/inline/ng_inline_fragment_iterator.h
+++ third_party/WebKit/Source/core/layout/ng/inline/ng_inline_fragment_iterator.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define NGInlineFragmentIterator_h
 
 #include "core/CoreExport.h"
-#include "core/layout/ng/geometry/ng_physical_offset.h"
+#include "core/layout/ng/ng_physical_fragment.h"
 #include "platform/wtf/Allocator.h"
 #include "platform/wtf/Vector.h"
 
@@ -16,7 +16,6 @@ namespace blink {
 class LayoutObject;
 class NGPhysicalBoxFragment;
 class NGPhysicalContainerFragment;
-class NGPhysicalFragment;
 struct NGPhysicalOffset;
 
 // Iterate through inline descendant fragments.
@@ -29,12 +28,7 @@ class CORE_EXPORT NGInlineFragmentIterator {
   NGInlineFragmentIterator(const NGPhysicalBoxFragment&,
                            const LayoutObject* filter);
 
-  // The data struct the iterator returns.
-  struct Result {
-    const NGPhysicalFragment* fragment;
-    NGPhysicalOffset offset_to_container_box;
-  };
-  using Results = Vector<Result, 1>;
+  using Results = Vector<NGPhysicalFragmentWithOffset, 1>;
 
   Results::const_iterator begin() const { return results_.begin(); }
   Results::const_iterator end() const { return results_.end(); }
