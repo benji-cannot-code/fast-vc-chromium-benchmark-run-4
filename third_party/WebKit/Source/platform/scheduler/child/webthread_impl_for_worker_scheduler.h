09775969debfeeef4408510d1739fc4c06ec68a0
Alexander Timin <altimin@chromium.org>
2017-11-30 00:31:33 +0000

    [scheduler] Fix worker scheduler shutdown.
    - Use base::TimeTicks::Now as a fallback in SchedulerHelper after
    TaskQueueManager was destroyed. This is needed for finalising tracing
    after shutdown.
    - Use thread-safe flag in WebThreadImplForWorkerScheduler to avoid race
    condition.
    
    R=alexclarke@chromium.org
    BUG=788797
    
    Change-Id: Ia3fe758a9884c11e8f83481a3d81ec8438a08863
    Reviewed-on: https://chromium-review.googlesource.com/797040
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520337}

--- third_party/WebKit/Source/platform/scheduler/child/webthread_impl_for_worker_scheduler.h
+++ third_party/WebKit/Source/platform/scheduler/child/webthread_impl_for_worker_scheduler.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_WEBKIT_SOURCE_PLATFORM_SCHEDULER_CHILD_WEBTHREAD_IMPL_FOR_WORKER_SCHEDULER_H_
 
 #include "base/message_loop/message_loop.h"
+#include "base/synchronization/atomic_flag.h"
 #include "base/threading/thread.h"
 #include "public/platform/WebPrivatePtr.h"
 #include "public/platform/scheduler/child/webthread_base.h"
@@ -74,6 +75,8 @@ class PLATFORM_EXPORT WebThreadImplForWorkerScheduler
   scoped_refptr<TaskQueue> task_queue_;
   scoped_refptr<scheduler::SingleThreadIdleTaskRunner> idle_task_runner_;
   WebPrivatePtr<WebTaskRunnerImpl> web_task_runner_;
+
+  base::AtomicFlag was_shutdown_on_thread_;
 };
 
 }  // namespace scheduler
