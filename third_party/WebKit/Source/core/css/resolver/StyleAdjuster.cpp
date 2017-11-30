f00a19423efdd0e7f9966d18d3af4d55b6feec70
Yoshifumi Inoue <yosin@chromium.org>
2017-11-30 14:28:23 +0000

    Disabling LayoutNG for inline content editable
    This patch disables LayoutNG for inline content editable by changing its display
    style to "inline-block" to handle it as block-level content editable.
    
    This is a temporary solution until LayoutNG supports editing.
    
    Bug: 591099
    Change-Id: I766b91ac42d2b0a268bde10c05839c3a021b7cf2
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Reviewed-on: https://chromium-review.googlesource.com/795751
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520535}

--- third_party/WebKit/Source/core/css/resolver/StyleAdjuster.cpp
+++ third_party/WebKit/Source/core/css/resolver/StyleAdjuster.cpp
@@ -55,6 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/svg/SVGSVGElement.h"
 #include "core/svg_names.h"
 #include "platform/Length.h"
+#include "platform/runtime_enabled_features.h"
 #include "platform/transforms/TransformOperations.h"
 #include "platform/wtf/Assertions.h"
 
@@ -604,6 +605,15 @@ void StyleAdjuster::AdjustComputedStyle(StyleResolverState& state,
     if (!StyleResolver::HasAuthorBackground(state)) {
       style.MutableBackgroundInternal().ClearImage();
     }
+  }
+
+  // TODO(layout-dev): Once LayoutnG handles inline content editable, we should
+  // get rid of following code fragment.
+  if (RuntimeEnabledFeatures::LayoutNGEnabled() &&
+      style.UserModify() != EUserModify::kReadOnly &&
+      style.Display() == EDisplay::kInline &&
+      parent_style.UserModify() == EUserModify::kReadOnly) {
+    style.SetDisplay(EDisplay::kInlineBlock);
   }
 }
 }  // namespace blink
