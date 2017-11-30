27cac8f26b38e94ea4775250b2db003e74330d7a
Hiroki Nakagawa <nhiroki@chromium.org>
2017-11-30 03:30:26 +0000

    Worklet: Set ReferrerPolicy in WorkletGlobalScope
    This is a follow-up CL for the CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/737311
    
    WorkletGlobalScope should inherit the owner Document's ReferrerPolicy and the
    previous CL meant to implement it. But, the CL actually forgot to set
    WorkletGlobalScope's ReferrerPolicy to the owner Document's ReferrerPolicy. This
    CL does it.
    
    One test case fails because of a bug in referrer handling on ES6 Modules. See
    issue 786862 for details.
    
    Bug: 773921, 786862
    Change-Id: I0eb95bd742c393ce6b39926ae41a9ef0ec3fc526
    Reviewed-on: https://chromium-review.googlesource.com/754150
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Takeshi Yoshino <tyoshino@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520415}

---
+++ third_party/WebKit/LayoutTests/external/wpt/worklets/resources/import-referrer-checker-worklet-script.sub.js
@@ -0,0 +1,3 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Forward GET parameters to the server.
+import './referrer-checker.py?referrer_policy={{GET[referrer_policy]}}&expected_referrer={{GET[expected_referrer]}}';
---
+++ third_party/WebKit/LayoutTests/external/wpt/worklets/resources/import-remote-origin-referrer-checker-worklet-script.sub.js
@@ -0,0 +1,3 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Forward GET parameters to the server.
+import 'https://{{domains[www1]}}:{{ports[https][0]}}/worklets/resources/referrer-checker.py?referrer_policy={{GET[referrer_policy]}}&expected_referrer={{GET[expected_referrer]}}';
