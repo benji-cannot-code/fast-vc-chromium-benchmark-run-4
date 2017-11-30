a14232598193fba36fd15d5ab1b3d940d860ee29
Alexander Yashkin <a-v-y@yandex-team.ru>
2017-11-30 11:54:45 +0000

    Added EstimateMemoryUsage for base::Value
    This function calculates number of bytes allocated in heap for
    components. See GURL::EstimateMemoryUsage for another example.
    
    base: :Value object. It's useful to calсulate memory trace for
    Change-Id: I9431de56e8fa9d30af46fea9afc470f44e1a8dc0
    Reviewed-on: https://chromium-review.googlesource.com/795719
    Commit-Queue: Alexander Yashkin <a-v-y@yandex-team.ru>
    Reviewed-by: Dmitry Skiba <dskiba@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520512}

--- base/values.cc
+++ base/values.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/trace_event/memory_usage_estimator.h"
 
 namespace base {
 
@@ -620,6 +621,21 @@ bool operator>=(const Value& lhs, const Value& rhs) {
 bool Value::Equals(const Value* other) const {
   DCHECK(other);
   return *this == *other;
+}
+
+size_t Value::EstimateMemoryUsage() const {
+  switch (type_) {
+    case Type::STRING:
+      return base::trace_event::EstimateMemoryUsage(string_value_);
+    case Type::BINARY:
+      return base::trace_event::EstimateMemoryUsage(binary_value_);
+    case Type::DICTIONARY:
+      return base::trace_event::EstimateMemoryUsage(dict_);
+    case Type::LIST:
+      return base::trace_event::EstimateMemoryUsage(list_);
+    default:
+      return 0;
+  }
 }
 
 void Value::InternalMoveConstructFrom(Value&& that) {
