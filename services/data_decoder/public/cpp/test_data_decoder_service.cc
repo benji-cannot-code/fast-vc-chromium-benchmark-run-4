7762021afa4217e2a40b8a0e05ad5bde633b57f2
Jay Civelli <jcivelli@google.com>
2017-11-30 00:08:20 +0000

    Adding attribute and namespace support to the XML parser.
    Several improvements to the safe XML parser:
    - adding attribute support.
    - adding namespace support. A new namespace section exposing the
      namespace declared in a node is generated in the parsed Value.
      Also elements and attributes now use the fully qualified names
      (including namespace).
    - several methods added to the client library, for attribute and
      namespace access and for the needs of upcoming clients.
    - created a TestDataDecoderService class that can be used in tests
      to access a service_manager::Connector that can bind the
      data_decoder's interfaces. This has the advantage of not requiring
      a dependency to the private implementation of data_decoder that is
      required when using TestConnectorFactory directly.
    
    Bug: 784667
    Change-Id: I9b87341eca5eed4a72f4d55cc29837a0e1468538
    Reviewed-on: https://chromium-review.googlesource.com/773460
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Scott Graham <scottmg@chromium.org>
    Reviewed-by: Jay Civelli <jcivelli@chromium.org>
    Commit-Queue: Jay Civelli <jcivelli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520316}

---
+++ services/data_decoder/public/cpp/test_data_decoder_service.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/data_decoder/public/cpp/test_data_decoder_service.h"
+
+#include "services/data_decoder/data_decoder_service.h"
+
+namespace data_decoder {
+
+TestDataDecoderService::TestDataDecoderService()
+    : connector_factory_(std::make_unique<DataDecoderService>()),
+      connector_(connector_factory_.CreateConnector()) {}
+
+TestDataDecoderService::~TestDataDecoderService() = default;
+
+}  // namespace data_decoder
