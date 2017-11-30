cf20a142bd10a37bf8be400018f77f1e14326a81
Daniel Bratell <bratell@opera.com>
2017-11-30 00:40:38 +0000

    Fix include guard for test_fileapi_operation_waiter.h
    Change-Id: Iaa408e8195368c736370a8aee78fe3ceec741a59
    Reviewed-on: https://chromium-review.googlesource.com/788050
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Daniel Bratell <bratell@opera.com>
    Cr-Commit-Position: refs/heads/master@{#520346}

--- content/public/test/test_fileapi_operation_waiter.h
+++ content/public/test/test_fileapi_operation_waiter.h
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #ifndef CONTENT_PUBLIC_TEST_TEST_FILEAPI_OPERATION_WAITER_H_
+#define CONTENT_PUBLIC_TEST_TEST_FILEAPI_OPERATION_WAITER_H_
 
 #include <stdint.h>
 
