9881462e5759e4b3ef0d509317290ca9b55c5312
Robert Sesek <rsesek@chromium.org>
2017-11-30 17:14:20 +0000

    crash_keys: Convert "third-party-modules-*" to the new API.
    Bug: 598854
    Change-Id: Ice5b1a0166c841393966b91b3ff464421aa1b619
    Reviewed-on: https://chromium-review.googlesource.com/794351
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520581}

--- chrome/app/chrome_crash_reporter_client_win.cc
+++ chrome/app/chrome_crash_reporter_client_win.cc
@@ -62,9 +62,6 @@ constexpr char kHungRendererReason[] = "hung-reason";
 constexpr char kInputEventFilterSendFailure[] =
     "input-event-filter-send-failure";
 
-constexpr char kThirdPartyModulesLoaded[] = "third-party-modules-loaded";
-constexpr char kThirdPartyModulesNotLoaded[] = "third-party-modules-not-loaded";
-
 constexpr char kIsEnterpriseManaged[] = "is-enterprise-managed";
 
 constexpr char kViewCount[] = "view-count";
@@ -115,8 +112,6 @@ size_t RegisterCrashKeysHelper() {
       {gpu::crash_keys::kGPUGLContextIsVirtual, kSmallSize},
 
       // browser/:
-      {kThirdPartyModulesLoaded, kSmallSize},
-      {kThirdPartyModulesNotLoaded, kSmallSize},
       {kIsEnterpriseManaged, kSmallSize},
 
       // content/:
