38dab4511ec8020b5de950137351fd74a886e767
Ryan Hamilton <rch@chromium.org>
2017-11-30 07:35:28 +0000

    Fix the logic for QUIC WriteError histograms to correctly handle both sync and async write errors. Add tests.
    Change-Id: I8bf224f0862d7569bd2fbd885a7e363677506b16
    Reviewed-on: https://chromium-review.googlesource.com/797942
    Commit-Queue: Ryan Hamilton <rch@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Reviewed-by: Jana Iyengar <jri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520469}

--- net/quic/chromium/quic_chromium_packet_writer.cc
+++ net/quic/chromium/quic_chromium_packet_writer.cc
@@ -124,7 +124,6 @@ WriteResult QuicChromiumPacketWriter::WritePacketToSocketImpl() {
   WriteStatus status = WRITE_STATUS_OK;
   if (rv < 0) {
     if (rv != ERR_IO_PENDING) {
-      UMA_HISTOGRAM_SPARSE_SLOWLY("Net.QuicSession.WriteError", -rv);
       status = WRITE_STATUS_ERROR;
     } else {
       status = WRITE_STATUS_BLOCKED;
