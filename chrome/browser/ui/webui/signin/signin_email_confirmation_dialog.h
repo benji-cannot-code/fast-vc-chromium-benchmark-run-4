cc79915c3d5fb17766c02b09e995867bbea4adc0
Daniel Bratell <bratell@opera.com>
2017-11-30 19:49:16 +0000

    Forward declare WebUIMessageHandler in the right namespace
    A header forward declared a WebUIMessageHandler in the global
    namespace which seriously confused jumbo builds since the
    actual WebUIMessageHandler is in the content namespace.
    
    Change-Id: I623b2f729d03dbea199c27f62790f8119d0e3926
    Reviewed-on: https://chromium-review.googlesource.com/800933
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520646}

--- chrome/browser/ui/webui/signin/signin_email_confirmation_dialog.h
+++ chrome/browser/ui/webui/signin/signin_email_confirmation_dialog.h
@@ -17,10 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/web_dialogs/web_dialog_ui.h"
 
 class Profile;
-class WebUIMessageHandler;
 
 namespace content {
 class WebContents;
+class WebUIMessageHandler;
 }
 
 // A tab-modal dialog to ask the user to confirm his email before signing in.
