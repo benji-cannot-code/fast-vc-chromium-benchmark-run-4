242c143decd7411f372dbd49e91fc1527dafcbd7
Jose Dapena Paz <jose.dapena@lge.com>
2017-11-30 10:11:38 +0000

    A11y: add AXPlatformNodeAuraLinux unit tests
    This commit adds unit tests for AtkObject, AtkComponent,
    AtkHyperlinkImpl and AtkValue, based on several tests
    also available on Windows backend.
    
    Bug: 789099, 789060
    Change-Id: I8fd683141c1c5693fa3d5b80ecc3489cc8e9ebd8
    Reviewed-on: https://chromium-review.googlesource.com/794139
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: José Dapena Paz <jose.dapena@lge.com>
    Cr-Commit-Position: refs/heads/master@{#520500}

--- ui/accessibility/platform/atk_util_auralinux.h
+++ ui/accessibility/platform/atk_util_auralinux.h
@@ -15,7 +15,7 @@ namespace ui {
 // This singleton class initializes ATK (accessibility toolkit) and
 // registers an implementation of the AtkUtil class, a global class that
 // every accessible application needs to register once.
-class AtkUtilAuraLinux {
+class AX_EXPORT AtkUtilAuraLinux {
  public:
   // Get the single instance of this class.
   static AtkUtilAuraLinux* GetInstance();
@@ -23,6 +23,7 @@ class AtkUtilAuraLinux {
   AtkUtilAuraLinux() = default;
 
   void InitializeAsync();
+  void InitializeForTesting();
 
  private:
   friend struct base::DefaultSingletonTraits<AtkUtilAuraLinux>;
