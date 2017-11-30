8d55cfee5765aa67ed92648d9acfd42da2e9d2a3
khmel <khmel@google.com>
2017-11-30 19:59:40 +0000

    arc: Limit focus traveling only for overlay.
    This closes the loop for tab focus change inside overlay in case it is
    currently showm.
    
    Bug: 788127
    Test: Manually
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I0dc45f0702e0608961718c8fafbd013227de3ee8
    Reviewed-on: https://chromium-review.googlesource.com/797670
    Commit-Queue: Yury Khmel <khmel@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520652}

---
+++ chrome/browser/resources/chromeos/arc_support/arc_optin_focus_manager.js
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Manages focus for ARC OptIn page.
+ * @constructor
+ * @extends {cr.ui.FocusManager}
+ */
+function ArcOptInFocusManager() {
+  cr.ui.FocusManager.call(this);
+}
+
+ArcOptInFocusManager.prototype = {
+  __proto__: cr.ui.FocusManager.prototype,
+
+  /** @override */
+  getFocusParent: function() {
+    var overlay = $('overlay-container');
+    if (overlay.hidden) {
+      return document.body;
+    } else {
+      return $('overlay-page');
+    }
+  }
+};
--- chrome/browser/resources/chromeos/arc_support/background.js
+++ chrome/browser/resources/chromeos/arc_support/background.js
@@ -811,6 +811,9 @@ chrome.app.runtime.onLaunched.addListener(function() {
     appWindow.contentWindow.cr.ui.overlay.globalInitialization();
     overlay.addEventListener('cancelOverlay', hideOverlay);
 
+    focusManager = new appWindow.contentWindow.ArcOptInFocusManager();
+    focusManager.initialize();
+
     connectPort();
   };
 
