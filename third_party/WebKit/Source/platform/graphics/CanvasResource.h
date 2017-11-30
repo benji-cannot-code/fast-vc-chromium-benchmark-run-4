2b6a394c89104dbc4b7e15ae11ce31817d764b3d
Justin Novosad <junov@chromium.org>
2017-11-30 17:31:53 +0000

    Make CanvasResource RefCounted
    This is a step towards unifying OffscreenCanvasResourceProvider
    with CanvasResourceProvider, which will eventually allow low
    latency canvas rendering to use a single buffered GpuMemoryBuffer
    
    BUG=788439, 776801
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Ie9f04e7920e7d74c229c4d45c34c371ab5d06e36
    Reviewed-on: https://chromium-review.googlesource.com/800670
    Commit-Queue: Justin Novosad <junov@chromium.org>
    Reviewed-by: Olivia Lai <xlai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520586}

--- third_party/WebKit/Source/platform/graphics/CanvasResource.h
+++ third_party/WebKit/Source/platform/graphics/CanvasResource.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/geometry/IntSize.h"
 #include "platform/graphics/CanvasColorParams.h"
 #include "platform/graphics/WebGraphicsContext3DProviderWrapper.h"
+#include "platform/wtf/RefCounted.h"
 
 #ifndef CanvasResource_h
 #define CanvasResource_h
@@ -23,7 +24,7 @@ namespace blink {
 
 // Generic resource interface, used for locking (RAII) and recycling pixel
 // buffers of any type.
-class PLATFORM_EXPORT CanvasResource {
+class PLATFORM_EXPORT CanvasResource : public WTF::RefCounted<CanvasResource> {
  public:
   virtual ~CanvasResource();
   virtual void Abandon() = 0;
@@ -47,7 +48,7 @@ class PLATFORM_EXPORT CanvasResource {
 // Resource type for skia Bitmaps (RAM and texture backed)
 class PLATFORM_EXPORT CanvasResource_Skia final : public CanvasResource {
  public:
-  static std::unique_ptr<CanvasResource_Skia> Create(
+  static scoped_refptr<CanvasResource_Skia> Create(
       sk_sp<SkImage>,
       WeakPtr<WebGraphicsContext3DProviderWrapper>);
   virtual ~CanvasResource_Skia() { Abandon(); }
@@ -70,7 +71,7 @@ class PLATFORM_EXPORT CanvasResource_Skia final : public CanvasResource {
 class PLATFORM_EXPORT CanvasResource_GpuMemoryBuffer final
     : public CanvasResource {
  public:
-  static std::unique_ptr<CanvasResource_GpuMemoryBuffer> Create(
+  static scoped_refptr<CanvasResource_GpuMemoryBuffer> Create(
       const IntSize&,
       const CanvasColorParams&,
       WeakPtr<WebGraphicsContext3DProviderWrapper>);
