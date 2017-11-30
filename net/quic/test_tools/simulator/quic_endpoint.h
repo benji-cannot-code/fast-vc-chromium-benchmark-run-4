9a4e511b1c9e5da00f8d6ffe8fd38dee56fbfa7b
Charles 'Buck' Krasic <ckrasic@chromium.org>
2017-11-30 01:36:09 +0000

    Landing Recent QUIC changes until 6:08 AM, Nov 24, 2017 UTC-8
    Sync flags.
    https://chromium-review.googlesource.com/c/798114/
    
    Remove the QUIC_FLAG_COUNT for FLAGS_quic_reloadable_flag_quic_deprecate_largest_observed due to its high cpu usage.
    
    (n/a) Remove QUIC_FLAG_COUNT only.
    
    Merge internal change: 176839829
    
    https://chromium-review.googlesource.com/c/798021/
    
    In quic stream sequencer buffer, refactor data copy part to a standalone function. No functional change expected.
    
    Merge internal change: 176654270
    
    https://chromium-review.googlesource.com/c/798060/
    
    In simulator, do not double-count spurious retransmissions as received
    
    n/a (test-only change)
    
    Merge internal change: 176543251
    
    https://chromium-review.googlesource.com/c/798057/
    
    Deprecate FLAGS_quic_reloadable_flag_quic_ack_decimation.
    
    n/a (Flag deprecation)
    
    Merge internal change: 176524632
    
    https://chromium-review.googlesource.com/c/798017/
    
    Deprecate FLAGS_quic_reloadable_flag_quic_bbr_bandwidth_resumption
    
    Merge internal change: 176456114
    
    https://chromium-review.googlesource.com/c/797234/
    
    Use the new prefetch API.
    
    Merge internal change: 175916878
    
    https://chromium-review.googlesource.com/c/797770/
    
    R=rch@chromium.org
    
    Bug: 
    Change-Id: I1d7ff0f60b2e06b3268bef4943da8c58130bf556
    Reviewed-on: https://chromium-review.googlesource.com/798218
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Buck Krasic <ckrasic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520379}

--- net/quic/test_tools/simulator/quic_endpoint.h
+++ net/quic/test_tools/simulator/quic_endpoint.h
@@ -47,9 +47,7 @@ class QuicEndpoint : public Endpoint,
   inline QuicConnection* connection() { return &connection_; }
   inline QuicByteCount bytes_to_transfer() const { return bytes_to_transfer_; }
   inline QuicByteCount bytes_transferred() const { return bytes_transferred_; }
-  inline QuicByteCount bytes_received() {
-    return connection_.GetStats().stream_bytes_received;
-  }
+  QuicByteCount bytes_received() const;
   inline size_t write_blocked_count() { return write_blocked_count_; }
   inline bool wrong_data_received() const { return wrong_data_received_; }
 
@@ -157,6 +155,9 @@ class QuicEndpoint : public Endpoint,
   // Set to true if the endpoint receives stream data different from what it
   // expects.
   bool wrong_data_received_;
+
+  // Record of received offsets in the data stream.
+  QuicIntervalSet<QuicStreamOffset> offsets_received_;
 };
 
 // Multiplexes multiple connections at the same host on the network.
