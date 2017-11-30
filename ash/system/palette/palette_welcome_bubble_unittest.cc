c9ba11aeea20f63b4517dca06715e8a74ca8154b
Sammie Quon <sammiequon@google.com>
2017-11-30 16:43:44 +0000

    palette: Do not show welcome bubble if guest mode or not in active state.
    Originally tries to show bubble when the pref service (user) changes.
    Added checks in PaletteWelcomeBubble::ShowIfNeeded to check for the
    two cases where the bubble is not wanted. PaletteTray tries to show the
    bubble again when the session state changes.
    
    Test: ash_unittests PaletteWelcomeBubbleTest.*
    Bug: 786418, 786590
    Change-Id: I0695a83383ce6c9d79f8d8f6dfda2eab0fb37071
    Reviewed-on: https://chromium-review.googlesource.com/780841
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520575}

--- ash/system/palette/palette_welcome_bubble_unittest.cc
+++ ash/system/palette/palette_welcome_bubble_unittest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/test/ash_test_base.h"
 #include "base/command_line.h"
 #include "components/prefs/pref_service.h"
+#include "components/session_manager/session_manager_types.h"
 #include "ui/events/test/event_generator.h"
 #include "ui/views/controls/button/image_button.h"
 
@@ -24,6 +25,7 @@ namespace {
 
 constexpr char kUser1Email[] = "user1@palettewelcome.com";
 constexpr char kUser2Email[] = "user2@palettewelcome.com";
+constexpr char kGuestEmail[] = "guest@palettewelcome.com";
 
 }  // namespace
 
@@ -148,6 +150,36 @@ TEST_F(PaletteWelcomeBubbleTest, BubbleShownForSecondUser) {
       user2_pref_service()->GetBoolean(prefs::kShownPaletteWelcomeBubble));
   welcome_bubble_->ShowIfNeeded(false /* shown_by_stylus */);
   EXPECT_TRUE(welcome_bubble_->bubble_shown());
+}
+
+TEST_F(PaletteWelcomeBubbleTest, BubbleNotShownInactiveSession) {
+  GetSessionControllerClient()->SetSessionState(
+      session_manager::SessionState::LOGGED_IN_NOT_ACTIVE);
+  welcome_bubble_->ShowIfNeeded(false /* shown_by_stylus */);
+  EXPECT_FALSE(welcome_bubble_->bubble_shown());
+}
+
+class PaletteWelcomeBubbleGuestModeTest : public AshTestBase {
+ public:
+  PaletteWelcomeBubbleGuestModeTest() = default;
+  ~PaletteWelcomeBubbleGuestModeTest() override = default;
+
+  // AshTestBase:
+  void SetUp() override {
+    AshTestBase::SetUp();
+    GetSessionControllerClient()->AddUserSession(kGuestEmail,
+                                                 user_manager::USER_TYPE_GUEST);
+  }
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(PaletteWelcomeBubbleGuestModeTest);
+};
+
+TEST_F(PaletteWelcomeBubbleGuestModeTest, BubbleNotShownForGuest) {
+  auto welcome_bubble = std::make_unique<PaletteWelcomeBubble>(
+      StatusAreaWidgetTestHelper::GetStatusAreaWidget()->palette_tray());
+  welcome_bubble->ShowIfNeeded(false /* shown_by_stylus */);
+  EXPECT_FALSE(welcome_bubble->bubble_shown());
 }
 
 }  // namespace ash
