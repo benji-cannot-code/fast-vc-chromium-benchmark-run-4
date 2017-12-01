64cb5eb9ada8bb05d80404796f5a9637a6af5859
Sammie Quon <sammiequon@google.com>
2017-12-01 23:22:50 +0000

    cros: Paint headers for snap window when in both splitview and overview.
    Fix a bug where snapped windows in splitview while still in overview
    do not have their headers painted. Make CustomFrameViewAshBase look for
    splitview state changes, and repaint the header if the state is snap
    left or snap right.
    
    Test: ash_unittests CustomFrameViewAshTest.HeaderVisibility*
    Bug: 789378
    Change-Id: I7ff65181134205544af2a717a3ee3e3797aa2ad2
    Reviewed-on: https://chromium-review.googlesource.com/797681
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521114}

--- ash/frame/custom_frame_view_ash.cc
+++ ash/frame/custom_frame_view_ash.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/immersive/immersive_fullscreen_controller.h"
 #include "ash/public/cpp/immersive/immersive_fullscreen_controller_delegate.h"
 #include "ash/shell.h"
+#include "ash/wm/overview/window_selector_controller.h"
 #include "ash/wm/resize_handle_window_targeter.h"
 #include "ash/wm/tablet_mode/tablet_mode_controller.h"
 #include "ash/wm/tablet_mode/tablet_mode_observer.h"
@@ -323,10 +324,13 @@ CustomFrameViewAsh::CustomFrameViewAsh(
                                                   enable_immersive)));
   }
   Shell::Get()->AddShellObserver(this);
+  Shell::Get()->split_view_controller()->AddObserver(this);
 }
 
 CustomFrameViewAsh::~CustomFrameViewAsh() {
   Shell::Get()->RemoveShellObserver(this);
+  if (Shell::Get()->split_view_controller())
+    Shell::Get()->split_view_controller()->RemoveObserver(this);
 }
 
 void CustomFrameViewAsh::InitImmersiveFullscreenControllerForView(
@@ -478,6 +482,26 @@ const views::View* CustomFrameViewAsh::GetAvatarIconViewForTest() const {
   return header_view_->avatar_icon();
 }
 
+void CustomFrameViewAsh::MaybePaintHeaderForSplitview(
+    SplitViewController::State state) {
+  if (state == SplitViewController::NO_SNAP) {
+    header_view_->SetShouldPaintHeader(/*paint=*/false);
+    return;
+  }
+
+  SplitViewController* controller = Shell::Get()->split_view_controller();
+  aura::Window* window = nullptr;
+  if (state == SplitViewController::LEFT_SNAPPED)
+    window = controller->left_window();
+  else if (state == SplitViewController::RIGHT_SNAPPED)
+    window = controller->right_window();
+
+  // TODO(sammiequon): This works for now, but we may have to check if
+  // |frame_|'s native window is in the overview list instead.
+  if (window && window == frame_->GetNativeWindow())
+    header_view_->SetShouldPaintHeader(/*paint=*/true);
+}
+
 void CustomFrameViewAsh::SetShouldPaintHeader(bool paint) {
   header_view_->SetShouldPaintHeader(paint);
 }
@@ -488,6 +512,13 @@ void CustomFrameViewAsh::OnOverviewModeStarting() {
 
 void CustomFrameViewAsh::OnOverviewModeEnded() {
   SetShouldPaintHeader(true);
+}
+
+void CustomFrameViewAsh::OnSplitViewStateChanged(
+    SplitViewController::State /* previous_state */,
+    SplitViewController::State state) {
+  if (Shell::Get()->window_selector_controller()->IsSelecting())
+    MaybePaintHeaderForSplitview(state);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
