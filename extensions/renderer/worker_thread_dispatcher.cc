f4416479bfc154aab01958be2c1de526ee601084
Devlin Cronin <rdevlin.cronin@chromium.org>
2017-12-01 23:17:51 +0000

    [Extensions Bindings] Call InitializeBindingsSystem for worker bindings
    Ensure that we call DispatcherDelegate::InitializeBindingsSystem for
    the bindings system created for worker-thread bindings in order to
    allow adding custom bindings.
    
    Bug: 653596
    Change-Id: I4e5a71d13d27738098daf281ff49400f533b716d
    Reviewed-on: https://chromium-review.googlesource.com/759720
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521108}

--- extensions/renderer/worker_thread_dispatcher.cc
+++ extensions/renderer/worker_thread_dispatcher.cc
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/extension_messages.h"
 #include "extensions/renderer/dispatcher.h"
 #include "extensions/renderer/extension_bindings_system.h"
-#include "extensions/renderer/ipc_message_sender.h"
+#include "extensions/renderer/extensions_renderer_client.h"
 #include "extensions/renderer/js_extension_bindings_system.h"
 #include "extensions/renderer/native_extension_bindings_system.h"
 #include "extensions/renderer/service_worker_data.h"
@@ -149,26 +149,9 @@ void WorkerThreadDispatcher::OnDispatchEvent(
 void WorkerThreadDispatcher::AddWorkerData(
     int64_t service_worker_version_id,
     ScriptContext* context,
-    ResourceBundleSourceMap* source_map) {
+    std::unique_ptr<ExtensionBindingsSystem> bindings_system) {
   ServiceWorkerData* data = g_data_tls.Pointer()->Get();
   if (!data) {
-    std::unique_ptr<ExtensionBindingsSystem> bindings_system;
-    // QUESTION(lazyboy): Why is passing the WorkerThreadDispatcher to the
-    // IPCMessageSender (previously the ServiceWorkerRequestSender) safe? The
-    // WorkerThreadDispatcher is a process-wide singleton, but the
-    // IPCMessageSender is per-context (thus potentially many per process).
-    std::unique_ptr<IPCMessageSender> ipc_message_sender =
-        IPCMessageSender::CreateWorkerThreadIPCMessageSender(
-            this, service_worker_version_id);
-    if (base::FeatureList::IsEnabled(features::kNativeCrxBindings)) {
-      // The Unretained below is safe since the IPC message sender outlives the
-      // bindings system.
-      bindings_system = std::make_unique<NativeExtensionBindingsSystem>(
-          std::move(ipc_message_sender));
-    } else {
-      bindings_system = std::make_unique<JsExtensionBindingsSystem>(
-          source_map, std::move(ipc_message_sender));
-    }
     ServiceWorkerData* new_data = new ServiceWorkerData(
         service_worker_version_id, context, std::move(bindings_system));
     g_data_tls.Pointer()->Set(new_data);
