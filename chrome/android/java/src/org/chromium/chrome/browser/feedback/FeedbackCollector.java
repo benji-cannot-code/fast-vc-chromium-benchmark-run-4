2624450b7703abee29057d21d0964d5829b9e149
David Trainor <dtrainor@chromium.org>
2017-12-01 20:28:50 +0000

    Added a way to send feedback from AppHooks.
    BUG=773403
    
    Change-Id: Ie6eb72cb6b83321c3ca2094645bab8340879da9d
    Reviewed-on: https://chromium-review.googlesource.com/798217
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521040}

--- chrome/android/java/src/org/chromium/chrome/browser/feedback/FeedbackCollector.java
+++ chrome/android/java/src/org/chromium/chrome/browser/feedback/FeedbackCollector.java
@@ -16,6 +16,7 @@ import org.chromium.base.Callback;
 import org.chromium.base.CollectionUtil;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.VisibleForTesting;
+import org.chromium.chrome.browser.AppHooks;
 import org.chromium.chrome.browser.profiles.Profile;
 
 import java.util.ArrayList;
@@ -75,6 +76,7 @@ public class FeedbackCollector implements Runnable {
 
         // This is the list of all synchronous sources of feedback.  Please add new synchronous
         // entries here.
+        sources.addAll(AppHooks.get().getAdditionalFeedbackSources().getSynchronousSources());
         sources.add(new UrlFeedbackSource(url));
         sources.add(new VariationsFeedbackSource(profile));
         sources.add(new DataReductionProxyFeedbackSource(profile));
@@ -92,6 +94,7 @@ public class FeedbackCollector implements Runnable {
 
         // This is the list of all asynchronous sources of feedback.  Please add new asynchronous
         // entries here.
+        sources.addAll(AppHooks.get().getAdditionalFeedbackSources().getAsynchronousSources());
         sources.add(new ConnectivityFeedbackSource(profile));
         sources.add(new SystemInfoFeedbackSource());
 
