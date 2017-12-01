06856d556fe0b61ffdd91687f5a73bb9a096defd
Jared Saul <jsaul@google.com>
2017-12-01 14:39:50 +0000

    Fix comments missed by DialogEventObserver -> DialogEventWaiter rename
    Bug: 772483
    Change-Id: I7a655761660e3d10f25fb0d39f51b13a48c97662
    Reviewed-on: https://chromium-review.googlesource.com/802818
    Reviewed-by: Mathieu Perreault <mathp@chromium.org>
    Commit-Queue: Mathieu Perreault <mathp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520932}

--- chrome/browser/ui/views/payments/payment_request_browsertest_base.h
+++ chrome/browser/ui/views/payments/payment_request_browsertest_base.h
@@ -64,7 +64,7 @@ class PaymentRequestBrowserTestBase
       public PaymentRequestDialogView::ObserverForTest,
       public content::WebContentsObserver {
  public:
-  // Various events that can be waited on by the DialogEventObserver.
+  // Various events that can be waited on by the DialogEventWaiter.
   enum DialogEvent : int {
     DIALOG_OPENED,
     DIALOG_CLOSED,
@@ -245,7 +245,7 @@ class PaymentRequestBrowserTestBase
   // Resets the event waiter for a given |event| or |event_sequence|.
   void ResetEventWaiter(DialogEvent event);
   void ResetEventWaiterForSequence(std::list<DialogEvent> event_sequence);
-  // Wait for the event(s) passed to ResetEventObserver*() to occur.
+  // Wait for the event(s) passed to ResetEventWaiter*() to occur.
   void WaitForObservedEvent();
 
  private:
