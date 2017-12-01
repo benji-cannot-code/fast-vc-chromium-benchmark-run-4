76d66472878eebb9686b05d63c0ed8fd424ab9d7
Dave Schuyler <dschuyler@chromium.org>
2017-12-01 02:57:40 +0000

    [MD extensions] use .hidden rather than has/set/remove Attribute
    This CL is a followup for a comment in CL 792537. There's no user facing
    effect of this change. This changes the calls used to hide/unhide an the
    dev tools in the toolbar.
    
    Bug: None
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I7da1f779b48f942bfb708a4c574c0881269ca3ec
    Reviewed-on: https://chromium-review.googlesource.com/798013
    Reviewed-by: Dan Beam (no longer on Chrome) <dbeam@chromium.org>
    Commit-Queue: Dave Schuyler <dschuyler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520823}

--- chrome/browser/resources/md_extensions/toolbar.js
+++ chrome/browser/resources/md_extensions/toolbar.js
@@ -66,8 +66,8 @@ cr.define('extensions', function() {
     /** @private */
     onDevModeToggleChange_: function() {
       const drawer = this.$.devDrawer;
-      if (drawer.hasAttribute('hidden')) {
-        drawer.removeAttribute('hidden');
+      if (drawer.hidden) {
+        drawer.hidden = false;
         // Requesting the offsetTop will cause a reflow (to account for hidden).
         /** @suppress {suspiciousCode} */ drawer.offsetTop;
       }
@@ -78,11 +78,7 @@ cr.define('extensions', function() {
     onInDevModeChanged_: function() {
       // Set the initial state.
       this.expanded_ = this.inDevMode;
-      if (this.inDevMode) {
-        this.$.devDrawer.removeAttribute('hidden');
-      } else {
-        this.$.devDrawer.setAttribute('hidden', '');
-      }
+      this.$.devDrawer.hidden = !this.inDevMode;
     },
 
     /** @private */
