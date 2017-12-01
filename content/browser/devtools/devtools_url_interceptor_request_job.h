736212f4ae1ab2017d3ff125d479c08e4d8834e1
Nathan Bruer <allada@chromium.org>
2017-12-01 19:16:38 +0000

    [Devtools] Overrides intercepts while navigating
    Overrides will now change projects before the navigation happens
    allowing it to rewrite requests when navigating cross-origin.
    
    R=caseq,lushnikov,einbinder
    BUG=754371
    
    Change-Id: I0201a2a714c06bc7d4babb59e6d02b51c8e1f5e3
    Reviewed-on: https://chromium-review.googlesource.com/799682
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Blaise Bruer <allada@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521009}

--- content/browser/devtools/devtools_url_interceptor_request_job.h
+++ content/browser/devtools/devtools_url_interceptor_request_job.h
@@ -120,6 +120,7 @@ class DevToolsURLInterceptorRequestJob : public net::URLRequestJob {
   // |mock_response_|.  In some cases (e.g. file access) this may be null.
   const net::HttpResponseHeaders* GetHttpResponseHeaders() const;
 
+  void ProcessRedirect(int status_code, const std::string& new_url);
   void ProcessInterceptionRespose(
       std::unique_ptr<DevToolsURLRequestInterceptor::Modifications>
           modification);
---
+++ third_party/WebKit/LayoutTests/http/tests/devtools/network/resources/redirect-cross-origin-empty-html.php
@@ -0,0 +1,3 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+	header("Location: http://localhost:8000/devtools/network/resources/empty.html");
