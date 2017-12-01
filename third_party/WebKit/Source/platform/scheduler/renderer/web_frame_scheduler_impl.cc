c87d840c78c4bf861f05d41925776529bbce60a6
Alexander Timin <altimin@chromium.org>
2017-12-01 10:48:16 +0000

    [scheduler] Run tasks after frame detach
    Stop deleting tasks in task queues upon frame detach.
    
    R=dcheng@chromium.org,hajimehoshi@chromium.org,alexclarke@chromium.org
    BUG=770736
    
    Change-Id: I8c0a7f6da2bd383bddf32789a1c14d1a64d2b959
    Reviewed-on: https://chromium-review.googlesource.com/800943
    Reviewed-by: Hajime Hoshi <hajimehoshi@chromium.org>
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520908}

--- third_party/WebKit/Source/platform/scheduler/renderer/web_frame_scheduler_impl.cc
+++ third_party/WebKit/Source/platform/scheduler/renderer/web_frame_scheduler_impl.cc
@@ -115,9 +115,10 @@ namespace {
 void CleanUpQueue(MainThreadTaskQueue* queue) {
   if (!queue)
     return;
-  queue->ShutdownTaskQueue();
+  queue->DetachFromRendererScheduler();
   queue->SetFrameScheduler(nullptr);
   queue->SetBlameContext(nullptr);
+  queue->SetQueuePriority(TaskQueue::QueuePriority::kLowPriority);
 }
 
 }  // namespace
