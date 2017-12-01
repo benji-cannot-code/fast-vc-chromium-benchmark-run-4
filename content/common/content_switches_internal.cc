6de8bfc8e7560db204b3b90046cdf439a44c276c
Nate Chapin <japhet@chromium.org>
2017-12-01 17:09:24 +0000

    Add a Feature and field trial for SavePreviousDocumentResources experiment
    Bug: 766653
    Change-Id: Iff945c3b637fc566380b6ab8e2ab8e1c409088ba
    Reviewed-on: https://chromium-review.googlesource.com/756196
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520966}

--- content/common/content_switches_internal.cc
+++ content/common/content_switches_internal.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/feature_list.h"
 #include "base/metrics/field_trial.h"
+#include "base/metrics/field_trial_params.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
@@ -56,6 +57,9 @@ const base::Feature kProgressBarCompletionResourcesBeforeDOMContentLoaded {
     "progress-bar-completion-resources-before-domContentLoaded",
     base::FEATURE_DISABLED_BY_DEFAULT};
 #endif
+
+const base::Feature kSavePreviousDocumentResources{
+    "SavePreviousDocumentResources", base::FEATURE_DISABLED_BY_DEFAULT};
 
 #if defined(OS_WIN)
 
@@ -157,6 +161,14 @@ SavePreviousDocumentResources GetSavePreviousDocumentResources() {
   if (save_previous_document_resources == "onDOMContentLoaded")
     return SavePreviousDocumentResources::UNTIL_ON_DOM_CONTENT_LOADED;
   if (save_previous_document_resources == "onload")
+    return SavePreviousDocumentResources::UNTIL_ON_LOAD;
+  // The command line, which is set by the user, takes priority. Otherwise,
+  // fall back to the field trial.
+  std::string until = base::GetFieldTrialParamValueByFeature(
+      kSavePreviousDocumentResources, "until");
+  if (until == "onDOMContentLoaded")
+    return SavePreviousDocumentResources::UNTIL_ON_DOM_CONTENT_LOADED;
+  if (until == "onload")
     return SavePreviousDocumentResources::UNTIL_ON_LOAD;
   return SavePreviousDocumentResources::NEVER;
 }
