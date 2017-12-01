e19c3e2dd7c8c45ef4a63844ff26238c7023099d
Ahmed Fakhry <afakhry@google.com>
2017-12-01 22:33:41 +0000

    Fix tab detaching from immersive fullscreen windows
    When detaching a tab to a new browser, we used to just fullscreen
    the window, which is different from the immersive fullscreen window.
    This caused the new window to block showing the shelf as well as
    revealing the tab strip.
    
    BUG=776557
    TEST=interactive_ui_tests --gtest_filter=TabDragging/DetachToBrowserTabDragControllerTest.DetachToOwnWindowWhileInImmersiveFullscreenMode/*
    
    Change-Id: Idd3714fc9ec00e0e771ed93f77b40113c68b0ff9
    Reviewed-on: https://chromium-review.googlesource.com/804174
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521094}

--- chrome/browser/ui/views/tabs/tab_drag_controller.cc
+++ chrome/browser/ui/views/tabs/tab_drag_controller.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/ash/ash_util.h"
+#include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/tabs/tab_features.h"
@@ -1536,7 +1537,11 @@ void TabDragController::MaximizeAttachedWindow() {
   if (was_source_fullscreen_) {
     // In fullscreen mode it is only possible to get here if the source
     // was in "immersive fullscreen" mode, so toggle it back on.
-    GetAttachedBrowserWidget()->SetFullscreen(true);
+    BrowserView* browser_view = BrowserView::GetBrowserViewForNativeWindow(
+        GetAttachedBrowserWidget()->GetNativeWindow());
+    DCHECK(browser_view);
+    if (!browser_view->IsFullscreen())
+      chrome::ToggleFullscreenMode(browser_view->browser());
   }
 #endif
 }
