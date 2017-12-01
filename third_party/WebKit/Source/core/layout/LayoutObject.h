b3c30329291c885ca245a2a9ca59d745ccf2ec63
Yoichi Osato <yoichio@chromium.org>
2017-12-01 08:03:31 +0000

    Remove unused SetSelectionStateIfNeeded function.
    Xor invalidation was shipped:
    https://chromium-review.googlesource.com/c/chromium/src/+/720817
    
    After that, no one use the function.
    
    Bug: 739062
    Change-Id: Ibb585dfac70453f20e3226357d389b440dc0f3be
    Reviewed-on: https://chromium-review.googlesource.com/802665
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520886}

--- third_party/WebKit/Source/core/layout/LayoutObject.h
+++ third_party/WebKit/Source/core/layout/LayoutObject.h
@@ -1494,7 +1494,6 @@ class CORE_EXPORT LayoutObject : public ImageResourceObserver,
   virtual void SetSelectionState(SelectionState state) {
     bitfields_.SetSelectionState(state);
   }
-  inline void SetSelectionStateIfNeeded(SelectionState);
   bool CanUpdateSelectionOnRootLineBoxes() const;
 
   // A single rectangle that encompasses all of the selected objects within this
@@ -2745,13 +2744,6 @@ inline bool LayoutObject::PreservesNewline() const {
     return false;
 
   return Style()->PreserveNewline();
-}
-
-inline void LayoutObject::SetSelectionStateIfNeeded(SelectionState state) {
-  if (GetSelectionState() == state)
-    return;
-
-  SetSelectionState(state);
 }
 
 inline void LayoutObject::SetHasBoxDecorationBackground(bool b) {
