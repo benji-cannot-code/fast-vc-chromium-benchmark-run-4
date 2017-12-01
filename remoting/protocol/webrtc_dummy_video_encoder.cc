8bb0cf0e93825c36659b95faf8c0ed97b568ce19
Lambros Lambrou <lambroslambrou@chromium.org>
2017-12-01 17:29:28 +0000

    [remoting host] Add Picture ID to VPX frames
    Sending video frames with a picture ID allows the WebRTC receiver's
    jitter buffer to improve the processing of frame references, instead of
    relying on gaps in the packet sequence numbers. This should avoid false
    detections of packet loss in the case of missing or corrupt ULPFEC
    packets (when the frame could still be decoded).
    
    This is only possible for VPX-encoded frames. For H264, jitter buffer
    processing will fall back on using sequence-number gaps.
    
    Bug: 773549
    Change-Id: I354473a60249d85a3319ec88f07124420e078b44
    Reviewed-on: https://chromium-review.googlesource.com/802455
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520972}

--- remoting/protocol/webrtc_dummy_video_encoder.cc
+++ remoting/protocol/webrtc_dummy_video_encoder.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
+#include "base/rand_util.h"
 #include "base/stl_util.h"
 #include "base/synchronization/lock.h"
 #include "base/threading/thread_task_runner_handle.h"
@@ -59,7 +60,11 @@ WebrtcDummyVideoEncoder::WebrtcDummyVideoEncoder(
     base::WeakPtr<VideoChannelStateObserver> video_channel_state_observer)
     : main_task_runner_(main_task_runner),
       state_(kUninitialized),
-      video_channel_state_observer_(video_channel_state_observer) {}
+      video_channel_state_observer_(video_channel_state_observer) {
+  // Initialize randomly to avoid replay attacks.
+  base::RandBytes(&picture_id_, sizeof(picture_id_));
+  picture_id_ &= 0x7fff;
+}
 
 WebrtcDummyVideoEncoder::~WebrtcDummyVideoEncoder() = default;
 
@@ -164,7 +169,8 @@ webrtc::EncodedImageCallback::Result WebrtcDummyVideoEncoder::SendEncodedFrame(
     vp8_info->simulcastIdx = 0;
     vp8_info->temporalIdx = webrtc::kNoTemporalIdx;
     vp8_info->tl0PicIdx = webrtc::kNoTl0PicIdx;
-    vp8_info->pictureId = webrtc::kNoPictureId;
+    vp8_info->pictureId = picture_id_;
+    picture_id_ = (picture_id_ + 1) & 0x7fff;
   } else if (frame.codec == webrtc::kVideoCodecVP9) {
     webrtc::CodecSpecificInfoVP9* vp9_info =
         &codec_specific_info.codecSpecific.VP9;
@@ -180,7 +186,8 @@ webrtc::EncodedImageCallback::Result WebrtcDummyVideoEncoder::SendEncodedFrame(
     vp9_info->temporal_idx = webrtc::kNoTemporalIdx;
     vp9_info->spatial_idx = webrtc::kNoSpatialIdx;
     vp9_info->tl0_pic_idx = webrtc::kNoTl0PicIdx;
-    vp9_info->picture_id = webrtc::kNoPictureId;
+    vp9_info->picture_id = picture_id_;
+    picture_id_ = (picture_id_ + 1) & 0x7fff;
   } else if (frame.codec == webrtc::kVideoCodecH264) {
 #if defined(USE_H264_ENCODER)
     webrtc::CodecSpecificInfoH264* h264_info =
