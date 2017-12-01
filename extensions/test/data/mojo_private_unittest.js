018d2660d4f885c6c13689b3f82ff3091051887c
Yuzhu Shen <yzshen@chromium.org>
2017-12-01 23:50:20 +0000

    Extensions: convert media_router_bindings.js to use new Mojo JS bindings.
    This CL also changes the mojoPrivate API to be simply an async wrapper around
    require().
    
    Bug: 699569, 718047
    Change-Id: I271887d1cc1eb49fd2faefff47467c47be23a3be
    Reviewed-on: https://chromium-review.googlesource.com/783645
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Derek Cheng <imcheng@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Yuzhu Shen <yzshen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521129}

--- extensions/test/data/mojo_private_unittest.js
+++ extensions/test/data/mojo_private_unittest.js
@@ -14,7 +14,7 @@ unittestBindings.exportTests([
   function testRequireAsync() {
     mojoPrivate.requireAsync('add').then(
         test.callbackPass(function(add) {
-          test.assertEq('function', typeof add.Add);
+          test.assertEq('function', typeof add);
         }));
   },
 ], test.runTests, exports);
