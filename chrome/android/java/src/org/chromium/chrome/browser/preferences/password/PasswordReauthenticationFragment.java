0d3f946f2346aec423f4868dc71e4ee5524cd7d7
Vaclav Brozek <vabr@chromium.org>
2017-12-01 07:01:13 +0000

    popBackStack after canceled reauthentication
    In Chrome's passwords settings, the user needs to reauthenticate to
    access their passwords (view, copy, export). Chrome needs to pop the
    reauthentication screen from the back stack once the reauthentication
    completes, but currently only does it when the reauth is successful.
    
    This CL changes that to always popping the reauth screen.
    
    Bug: 789955
    Change-Id: If6130eb8efb231c191e9ec2d86ac59077acadb73
    Reviewed-on: https://chromium-review.googlesource.com/800616
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Vaclav Brozek <vabr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520879}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/password/PasswordReauthenticationFragment.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/password/PasswordReauthenticationFragment.java
@@ -42,8 +42,8 @@ public class PasswordReauthenticationFragment extends Fragment {
         if (requestCode == CONFIRM_DEVICE_CREDENTIAL_REQUEST_CODE) {
             if (resultCode == getActivity().RESULT_OK) {
                 ReauthenticationManager.setLastReauthTimeMillis(System.currentTimeMillis());
-                mFragmentManager.popBackStack();
             }
+            mFragmentManager.popBackStack();
         }
     }
 
