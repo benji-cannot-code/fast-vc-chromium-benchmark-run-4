d04f32208d854b5633bac1611c3734d5a797dcb9
James Cook <jamescook@chromium.org>
2017-12-02 00:04:03 +0000

    cros: Fix PreferencesTest.MultiProfiles and re-enable everywhere
    The crashes under --mash are due to heap corruption from code trying
    to disable multi-user switching animations. That code does not seem
    necessary any more. Per crrev.com/223823004 when it was added this
    test was reliably failing. The multi-user switch animations have
    been recently refactored and a shelf animation removed. The test
    now passes even without the animation disabling code.
    
    I cannot reproduce failures or crashes without --mash. I've done
    ASAN runs as well. I suspect the non-mash problem in the bug is
    unrelated to this test and may have been fixed separately.
    
    Bug: 787050
    Test: browser_tests --gtest_filter=PreferencesTest.* --gtest_repeat=100
    Change-Id: I08dc3897361440fdbd49df1a11429e06fb9bac51
    Reviewed-on: https://chromium-review.googlesource.com/804401
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521134}

--- chrome/browser/chromeos/preferences_chromeos_browsertest.cc
+++ chrome/browser/chromeos/preferences_chromeos_browsertest.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/settings/cros_settings.h"
 #include "chrome/browser/chromeos/settings/stub_cros_settings_provider.h"
 #include "chrome/browser/chromeos/system/fake_input_device_settings.h"
-#include "chrome/browser/ui/ash/multi_user/multi_user_window_manager_chromeos.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/test/base/in_process_browser_test.h"
@@ -135,15 +134,6 @@ class PreferencesTest : public LoginManagerTest {
               prefs->GetBoolean(prefs::kPrimaryMouseButtonRight));
   }
 
-  void DisableAnimations() {
-    // Disable animations for user transitions.
-    MultiUserWindowManagerChromeOS* manager =
-        static_cast<MultiUserWindowManagerChromeOS*>(
-            MultiUserWindowManager::GetInstance());
-    manager->SetAnimationSpeedForTest(
-        MultiUserWindowManagerChromeOS::ANIMATION_SPEED_DISABLED);
-  }
-
   std::vector<AccountId> test_users_;
 
  private:
@@ -214,8 +204,7 @@ IN_PROC_BROWSER_TEST_F(PreferencesTest, PRE_MultiProfiles) {
   chromeos::StartupUtils::MarkOobeCompleted();
 }
 
-// This test is flaking both with and without mash. http://crbug.com/787050
-IN_PROC_BROWSER_TEST_F(PreferencesTest, DISABLED_MultiProfiles) {
+IN_PROC_BROWSER_TEST_F(PreferencesTest, MultiProfiles) {
   user_manager::UserManager* user_manager = user_manager::UserManager::Get();
 
   // Add first user and init its preferences. Check that corresponding
@@ -231,7 +220,6 @@ IN_PROC_BROWSER_TEST_F(PreferencesTest, DISABLED_MultiProfiles) {
   // Add second user and init its prefs with different values.
   UserAddingScreen::Get()->Start();
   content::RunAllPendingInMessageLoop();
-  DisableAnimations();
   AddUser(test_users_[1].GetUserEmail());
   content::RunAllPendingInMessageLoop();
   const user_manager::User* user2 = user_manager->FindUser(test_users_[1]);
