b1289ee384539467a05d7e78b30c56c01781fd67
Patrick Monette <pmonette@chromium.org>
2017-12-02 00:22:54 +0000

    Fix formatting/lint issues in content setting model files.
    Fixes include:
    - Include what you use
    - Running "git cl format"
    - Reordering classes to be consistent
    
    Change-Id: I197eb04cb84babe0f91aa47a7f4792c6289d92ee
    Reviewed-on: https://chromium-review.googlesource.com/780019
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521145}

--- chrome/browser/ui/content_settings/content_setting_image_model.h
+++ chrome/browser/ui/content_settings/content_setting_image_model.h
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_CONTENT_SETTINGS_CONTENT_SETTING_IMAGE_MODEL_H_
 #define CHROME_BROWSER_UI_CONTENT_SETTINGS_CONTENT_SETTING_IMAGE_MODEL_H_
 
+#include <memory>
+#include <vector>
+
 #include "base/macros.h"
 #include "base/strings/string16.h"
 #include "build/build_config.h"
