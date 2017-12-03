e1b20d946ecf3d1084b5aee80eb9da05a715da7a
Gabriel Charette <gab@chromium.org>
2017-12-03 12:47:21 +0000

    Introduce RunLoop::OverrideDelegateForCurrentThreadForTesting().
    This is a prerequisite to enable ScopedTaskEnvironment MOCK_TIME
    on top of any RunLoop::Delegate (i.e. on top of MessageLoopForUI/ForIO).
    
    This CL also removes RunLoop::Delegate::Client::IsNested() as it
    was a mere shortcut for its TLS complement and had to switch to
    using TLS itself to remain valid in override scenarios...
    Ran base_perftests.exe --gtest_filter=*MessageLoop* in static/Release
    and things look the same.
    
    Bug: 708584
    Change-Id: I143f6e6afb47de11f95702c337dbe63eb0887596
    Reviewed-on: https://chromium-review.googlesource.com/784214
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521228}

--- base/run_loop.h
+++ base/run_loop.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_RUN_LOOP_H_
 #define BASE_RUN_LOOP_H_
 
+#include <utility>
 #include <vector>
 
 #include "base/base_export.h"
@@ -160,21 +161,17 @@ class BASE_EXPORT RunLoop {
   class BASE_EXPORT Delegate {
    public:
     Delegate();
-    ~Delegate();
+    virtual ~Delegate();
 
     // The client interface provided back to the caller who registers this
-    // Delegate via RegisterDelegateForCurrentThread.
+    // Delegate via RegisterDelegateForCurrentThread() or
+    // OverrideDelegateForCurrentThreadForTesting().
     class BASE_EXPORT Client {
      public:
       // Returns true if the Delegate should return from the topmost Run() when
       // it becomes idle. The Delegate is responsible for probing this when it
       // becomes idle.
       bool ShouldQuitWhenIdle() const;
-
-      // Returns true if this |outer_| is currently in nested runs. This is a
-      // shortcut for RunLoop::IsNestedOnCurrentThread() for the owner of this
-      // interface.
-      bool IsNested() const;
 
      private:
       // Only a Delegate can instantiate a Delegate::Client.
@@ -222,6 +219,12 @@ class BASE_EXPORT RunLoop {
     RunLoopStack active_run_loops_;
     ObserverList<RunLoop::NestingObserver> nesting_observers_;
 
+    // True if this Delegate was overriden through
+    // OverrideDelegateForCurrentThreadForTesting(). It will from then on always
+    // return from Run() when idle (i.e. its Client's ShouldQuitWhenIdle() will
+    // always be true).
+    bool was_overriden_ = false;
+
 #if DCHECK_IS_ON()
     bool allow_running_for_testing_ = true;
 #endif
@@ -243,6 +246,15 @@ class BASE_EXPORT RunLoop {
   // on forever bound to that thread (including its destruction). The returned
   // Delegate::Client is valid as long as |delegate| is kept alive.
   static Delegate::Client* RegisterDelegateForCurrentThread(Delegate* delegate);
+
+  // Akin to RegisterDelegateForCurrentThread but overrides an existing Delegate
+  // (there must be one). Returning |delegate|'s client interface like
+  // RegisterDelegateForCurrentThread() as well as the overriden Delegate which
+  // the caller is now in charge of driving. The overriden Delegate's Run()
+  // method will always return when idle (or earlier if the Delegate's Quit()
+  // method is explicitly invoked).
+  static std::pair<Delegate::Client*, Delegate*>
+  OverrideDelegateForCurrentThreadForTesting(Delegate* delegate);
 
   // Quits the active RunLoop (when idle) -- there must be one. These were
   // introduced as prefered temporary replacements to the long deprecated
--- base/threading/thread_task_runner_handle.cc
+++ base/threading/thread_task_runner_handle.cc
@@ -40,7 +40,8 @@ bool ThreadTaskRunnerHandle::IsSet() {
 
 // static
 ScopedClosureRunner ThreadTaskRunnerHandle::OverrideForTesting(
-    scoped_refptr<SingleThreadTaskRunner> overriding_task_runner) {
+    scoped_refptr<SingleThreadTaskRunner> overriding_task_runner,
+    ThreadTaskRunnerHandle::OverrideType type) {
   // OverrideForTesting() is not compatible with a SequencedTaskRunnerHandle
   // being set (but SequencedTaskRunnerHandle::IsSet() includes
   // ThreadTaskRunnerHandle::IsSet() so that's discounted as the only valid
@@ -68,7 +69,9 @@ ScopedClosureRunner ThreadTaskRunnerHandle::OverrideForTesting(
   ttrh->task_runner_.swap(overriding_task_runner);
 
   auto no_running_during_override =
-      std::make_unique<RunLoop::ScopedDisallowRunningForTesting>();
+      type == OverrideType::kTakeOverThread
+          ? nullptr
+          : std::make_unique<RunLoop::ScopedDisallowRunningForTesting>();
 
   return ScopedClosureRunner(base::Bind(
       [](scoped_refptr<SingleThreadTaskRunner> task_runner_to_restore,
