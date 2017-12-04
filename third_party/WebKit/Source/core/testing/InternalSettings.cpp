d19819b4ebd4e40e9294a7e13bd808a316886818
Suneel Kota <suneel.kota@samsung.com>
2017-12-04 08:12:31 +0000

    Remove use of setOverlayScrollbarsEnabled in layout tests
    Adding runtimeFlags.overlayScrollbarsEnabled would help to
    make more of these branches take the overlay path
    
    Bug: 661783
    Change-Id: I1d5523922fe3d6c8f671edd1c54789ef9aae399d
    Reviewed-on: https://chromium-review.googlesource.com/792833
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: srirama chandra sekhar <srirama.m@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#521289}

--- third_party/WebKit/Source/core/testing/InternalSettings.cpp
+++ third_party/WebKit/Source/core/testing/InternalSettings.cpp
@@ -169,10 +169,6 @@ void InternalSettings::setExperimentalContentSecurityPolicyFeaturesEnabled(
       enabled);
 }
 
-void InternalSettings::setOverlayScrollbarsEnabled(bool enabled) {
-  RuntimeEnabledFeatures::SetOverlayScrollbarsEnabled(enabled);
-}
-
 void InternalSettings::setViewportEnabled(bool enabled,
                                           ExceptionState& exception_state) {
   InternalSettingsGuardForSettings();
