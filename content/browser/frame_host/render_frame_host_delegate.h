b1da0d39c6e3a8c628e60fa90ddecfc676bb8c0c
John Abd-El-Malek <jam@chromium.org>
2017-12-04 21:45:19 +0000

    Remove SSLManager dependency on WebContentsImpl::DidGetResourceResponseStart.
    That callback is going away as it's not compatible with network service. Plumb information about subresource responses starting from the renderer to WebContents instead.
    
    A followup will undo much of the code added in r519351 after this.
    
    Bug: 783981
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: Id04381daa669cb2ac05dcf0f5d7d890960c6c06b
    Reviewed-on: https://chromium-review.googlesource.com/804947
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521469}

--- content/browser/frame_host/render_frame_host_delegate.h
+++ content/browser/frame_host/render_frame_host_delegate.h
@@ -19,8 +19,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/site_instance.h"
 #include "content/public/common/javascript_dialog_type.h"
 #include "content/public/common/media_stream_request.h"
+#include "content/public/common/resource_type.h"
 #include "device/geolocation/public/interfaces/geolocation_context.mojom.h"
 #include "mojo/public/cpp/bindings/scoped_interface_endpoint_handle.h"
+#include "net/cert/cert_status_flags.h"
 #include "net/http/http_response_headers.h"
 #include "services/device/public/interfaces/wake_lock.mojom.h"
 #include "ui/base/window_open_disposition.h"
@@ -337,6 +339,14 @@ class CONTENT_EXPORT RenderFrameHostDelegate {
   // Whether the delegate is being destroyed, in which case the RenderFrameHost
   // should not be asked to create a RenderFrame.
   virtual bool IsBeingDestroyed() const;
+
+  // Notifies that the render frame started loading a subresource.
+  virtual void SubresourceResponseStarted(const GURL& url,
+                                          const GURL& referrer,
+                                          const std::string& method,
+                                          ResourceType resource_type,
+                                          const std::string& ip,
+                                          net::CertStatus cert_status) {}
 
  protected:
   virtual ~RenderFrameHostDelegate() {}
