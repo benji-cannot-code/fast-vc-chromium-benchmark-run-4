ab290a98f1cf884e04b8a941299915eedefb7211
Helen Li <xunjieli@chromium.org>
2017-12-04 20:56:43 +0000

    Cancel any pending ResumeMainJob task
    When we restart jobs with a different proxy configuration, we might have
    a delayed ResumeMainJob task in our task queue. When this ResumeMainJob() runs,
    we will crash because the old main job is deleted and we have a new main
    job.
    
    This CL cancels the ResumeMainJob task and adds regression test.
    
    Bug: 790776, 789560
    Change-Id: Icd2bc9ec36c98c973f80e14541d9f68d0a046835
    Reviewed-on: https://chromium-review.googlesource.com/805354
    Commit-Queue: Helen Li <xunjieli@chromium.org>
    Reviewed-by: Zhongyi Shi <zhongyi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521444}

--- net/http/http_stream_factory_impl_job_controller.h
+++ net/http/http_stream_factory_impl_job_controller.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/cancelable_callback.h"
 #include "net/base/host_port_pair.h"
 #include "net/base/privacy_mode.h"
 #include "net/http/http_stream_factory_impl_job.h"
@@ -357,6 +358,8 @@ class HttpStreamFactoryImpl::JobController
   // job must not create a connection until it is resumed.
   bool main_job_is_blocked_;
 
+  // Handle for cancelling any posted delayed ResumeMainJob() task.
+  base::CancelableOnceClosure resume_main_job_callback_;
   // True if the main job was blocked and has been resumed in ResumeMainJob().
   bool main_job_is_resumed_;
 
