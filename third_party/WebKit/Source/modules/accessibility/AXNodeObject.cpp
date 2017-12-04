82133633c6c2e789dbec818b93636c631590548a
Nektarios Paisios <nektar@chromium.org>
2017-12-04 16:24:08 +0000

    According to spec native text controls should automatically get the ARIA textbox role
    According to HTML to platform accessibility mappings 1.1 native text boxes input and textarea, but not content editables, should get an ARIA role of textbox.
    I went through all our code in Blink and observed that aria-autocomplete was the only case where we didn't apply the same treatment to ARIA and native text controls.
    R=aleventhal@chromium.org, dmazzoni@chromium.org
    
    Bug: 761230
    Change-Id: Ie25e0b072b24f1f6ebf6d5b2e317b656fb463fdf
    Tested: manually, browser test
    Reviewed-on: https://chromium-review.googlesource.com/804915
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521358}

--- third_party/WebKit/Source/modules/accessibility/AXNodeObject.cpp
+++ third_party/WebKit/Source/modules/accessibility/AXNodeObject.cpp
@@ -1157,7 +1157,7 @@ unsigned AXNodeObject::HierarchicalLevel() const {
 
 // TODO: rename this just AutoComplete, it's not only ARIA.
 String AXNodeObject::AriaAutoComplete() const {
-  if (IsARIATextControl()) {
+  if (IsNativeTextControl() || IsARIATextControl()) {
     const AtomicString& aria_auto_complete =
         GetAOMPropertyOrARIAAttribute(AOMStringProperty::kAutocomplete)
             .DeprecatedLower();
