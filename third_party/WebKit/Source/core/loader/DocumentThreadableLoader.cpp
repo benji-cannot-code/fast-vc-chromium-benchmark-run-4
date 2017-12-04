175eb5ff549a0e6a96d65c7f2b6ee63721c9e6c5
Nate Chapin <japhet@chromium.org>
2017-12-04 22:30:39 +0000

    Reland "Make DocumentThreadableLoader a ResourceOwner"
    It had a custom implementation to support use of RawResourceClientStateChecker.
    Separate these.
    
    The original version (https://chromium.googlesource.com/chromium/src/+/ae15c13530084329ac2509ccded45428d7d7d2c8)
    failed to update DocumentThreadableLoader::Trace(). It introduced an intermediate
    subclass, ResourceOwner<RawResource>, between DocumentThreadableLoader and RawResourceClient, but
    because Trace() wasn't updated, ResourceOwner::resource_ was left untraced. The GC clang plugin
    would normally catch a mistake like this, but the plugin ignores ResourceOwner due to
    https://crbug.com/652966, so it wasn't detected at compile time.
    
    Bug: 790778, 640291
    Change-Id: I114df7eaa97139fbb6aa70b22b2f41019988631f
    Reviewed-on: https://chromium-review.googlesource.com/806753
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521490}

--- third_party/WebKit/Source/core/loader/DocumentThreadableLoader.cpp
+++ third_party/WebKit/Source/core/loader/DocumentThreadableLoader.cpp
@@ -54,6 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/loader/fetch/FetchUtils.h"
 #include "platform/loader/fetch/Resource.h"
 #include "platform/loader/fetch/ResourceFetcher.h"
+#include "platform/loader/fetch/ResourceLoader.h"
 #include "platform/loader/fetch/ResourceLoaderOptions.h"
 #include "platform/loader/fetch/ResourceRequest.h"
 #include "platform/weborigin/SchemeRegistry.h"
@@ -537,7 +538,7 @@ void DocumentThreadableLoader::MakeCrossOriginAccessRequestBlinkCORS(
 
 DocumentThreadableLoader::~DocumentThreadableLoader() {
   CHECK(!client_);
-  DCHECK(!resource_);
+  DCHECK(!GetResource());
 }
 
 void DocumentThreadableLoader::OverrideTimeout(
@@ -586,14 +587,16 @@ void DocumentThreadableLoader::Detach() {
 }
 
 void DocumentThreadableLoader::SetDefersLoading(bool value) {
-  if (GetResource())
-    GetResource()->SetDefersLoading(value);
+  if (GetResource() && GetResource()->Loader())
+    GetResource()->Loader()->SetDefersLoading(value);
 }
 
 void DocumentThreadableLoader::Clear() {
   client_ = nullptr;
   timeout_timer_.Stop();
   request_started_seconds_ = 0.0;
+  if (GetResource())
+    checker_.WillRemoveClient();
   ClearResource();
 }
 
@@ -735,6 +738,8 @@ bool DocumentThreadableLoader::RedirectReceivedBlinkCORS(
 
   // FIXME: consider combining this with CORS redirect handling performed by
   // CrossOriginAccessControl::handleRedirect().
+  if (GetResource())
+    checker_.WillRemoveClient();
   ClearResource();
 
   // If
@@ -1102,6 +1107,8 @@ void DocumentThreadableLoader::DidTimeout(TimerBase* timer) {
 }
 
 void DocumentThreadableLoader::LoadFallbackRequestForServiceWorker() {
+  if (GetResource())
+    checker_.WillRemoveClient();
   ClearResource();
   ResourceRequest fallback_request(fallback_request_for_service_worker_);
   fallback_request_for_service_worker_ = ResourceRequest();
@@ -1117,6 +1124,8 @@ void DocumentThreadableLoader::LoadActualRequest() {
   actual_request_ = ResourceRequest();
   actual_options_ = ResourceLoaderOptions();
 
+  if (GetResource())
+    checker_.WillRemoveClient();
   ClearResource();
 
   PrepareCrossOriginRequest(actual_request);
@@ -1201,6 +1210,8 @@ void DocumentThreadableLoader::LoadRequestAsync(
   } else {
     SetResource(RawResource::Fetch(new_params, fetcher));
   }
+  if (GetResource())
+    checker_.WillAddClient();
 
   if (!GetResource()) {
     probe::documentThreadableLoaderFailedToStartLoadingForClient(
@@ -1366,10 +1377,9 @@ ExecutionContext* DocumentThreadableLoader::GetExecutionContext() const {
 }
 
 void DocumentThreadableLoader::Trace(blink::Visitor* visitor) {
-  visitor->Trace(resource_);
   visitor->Trace(loading_context_);
   ThreadableLoader::Trace(visitor);
-  RawResourceClient::Trace(visitor);
+  ResourceOwner<RawResource>::Trace(visitor);
 }
 
 }  // namespace blink
