1d0713e781f8fc22f3476f2d6714e30e25f1952c
Yutaka Hirano <yhirano@chromium.org>
2017-12-04 23:33:12 +0000

    Set up a finch experiment on RendererSideResourceScheduler
    This CL sets the outstanding limits for ResourceLoadScheduler for the
    experiment, and disables the body detection logic  from
    ResourceScheduler.
    
    Bug: 785770
    Change-Id: I178d65da2681a1790dc1fc67c1318d9c809007a7
    Reviewed-on: https://chromium-review.googlesource.com/799612
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Randy Smith <rdsmith@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521519}

--- content/browser/loader/resource_scheduler.cc
+++ content/browser/loader/resource_scheduler.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/resource_messages.h"
 #include "content/public/browser/resource_request_info.h"
 #include "content/public/browser/resource_throttle.h"
+#include "content/public/common/content_features.h"
 #include "net/base/host_port_pair.h"
 #include "net/base/load_flags.h"
 #include "net/base/request_priority.h"
@@ -424,7 +425,15 @@ class ResourceScheduler::Client {
             resource_scheduler->throttle_delayable_.GetMaxDelayableRequests(
                 network_quality_estimator)),
         resource_scheduler_(resource_scheduler),
-        weak_ptr_factory_(this) {}
+        weak_ptr_factory_(this) {
+    if (base::FeatureList::IsEnabled(
+            features::kRendererSideResourceScheduler)) {
+      // When kRendererSideResourceScheduler is enabled, "layout blocking"
+      // concept is moved to the renderer side, so the shceduler works always
+      // with the normal mode.
+      has_html_body_ = true;
+    }
+  }
 
   ~Client() {}
 
@@ -489,6 +498,14 @@ class ResourceScheduler::Client {
 
   void OnNavigate() {
     has_html_body_ = false;
+    if (base::FeatureList::IsEnabled(
+            features::kRendererSideResourceScheduler)) {
+      // When kRendererSideResourceScheduler is enabled, "layout blocking"
+      // concept is moved to the renderer side, so the shceduler works always
+      // with the normal mode.
+      has_html_body_ = true;
+    }
+
     is_loaded_ = false;
     max_delayable_requests_ =
         resource_scheduler_->throttle_delayable_.GetMaxDelayableRequests(
@@ -961,6 +978,8 @@ class ResourceScheduler::Client {
   bool is_loaded_;
   // Tracks if the main HTML parser has reached the body which marks the end of
   // layout-blocking resources.
+  // This is disabled and the is always true when kRendererSideResourceScheduler
+  // is enabled.
   bool has_html_body_;
   bool using_spdy_proxy_;
   RequestQueue pending_requests_;
