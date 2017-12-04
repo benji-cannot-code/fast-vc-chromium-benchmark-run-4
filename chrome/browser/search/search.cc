0435ccf019d261b07ba9cb2cef105e80f6c19c3c
Marc Treib <treib@chromium.org>
2017-12-04 10:37:51 +0000

    Move search::IsSuggestPrefEnabled to webservice_search_provider.cc
    That's the only call site, and none of the other code in search::
    depends on that preference anymore.
    
    Bug: none
    Change-Id: Id10ab76d2e8566f2e5c48c389ceec79ac38093d3
    Reviewed-on: https://chromium-review.googlesource.com/803282
    Reviewed-by: calamity <calamity@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521305}

--- chrome/browser/search/search.cc
+++ chrome/browser/search/search.cc
@@ -17,10 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/search_engines/ui_thread_search_terms_data.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/chrome_switches.h"
-#include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
 #include "components/google/core/browser/google_util.h"
-#include "components/prefs/pref_service.h"
 #include "components/search/search.h"
 #include "components/search_engines/search_engine_type.h"
 #include "components/search_engines/template_url_service.h"
@@ -318,11 +316,6 @@ bool IsInstantNTPURL(const GURL& url, Profile* profile) {
 
   GURL new_tab_url(GetNewTabPageURL(profile));
   return new_tab_url.is_valid() && MatchesOriginAndPath(url, new_tab_url);
-}
-
-bool IsSuggestPrefEnabled(Profile* profile) {
-  return profile && !profile->IsOffTheRecord() && profile->GetPrefs() &&
-         profile->GetPrefs()->GetBoolean(prefs::kSearchSuggestEnabled);
 }
 
 GURL GetNewTabPageURL(Profile* profile) {
