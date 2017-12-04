404f66e2b20acd178bf263079817c6774e29c31a
Leo Zhang <googleo@chromium.org>
2017-12-04 05:47:18 +0000

    Implementation for Languages settings (Part 2).
    This CL is based on crrev.com/c/762540
    1, Polish the UI.
    2, Reading all language infos from native.
    3, Enable "Remove" in the language menu.
    4, Enable "Add language" for the settings page.
    5, Created "Add language" fragment, it will list all unselected languages
       and provide a "search" function for users to find the language quickly.
    
    Tested on my local devices, demo video link
    
    https: //googleo.users.x20web.corp.google.com/screenrecord/chrome/lang-settings.mp4
    Bug: 783049
    Change-Id: Ie5fdba908a99ad1116538bd69fe9c703c4c9051a
    Reviewed-on: https://chromium-review.googlesource.com/790091
    Commit-Queue: Leo Zhang <googleo@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521276}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/languages/LanguagesPreferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/languages/LanguagesPreferences.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.preferences.languages;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
@@ -15,20 +16,21 @@ import org.chromium.chrome.browser.preferences.ChromeSwitchPreference;
 import org.chromium.chrome.browser.preferences.ManagedPreferenceDelegate;
 import org.chromium.chrome.browser.preferences.PrefServiceBridge;
 import org.chromium.chrome.browser.preferences.PreferenceUtils;
-import org.chromium.chrome.browser.widget.TintedDrawable;
+import org.chromium.chrome.browser.preferences.PreferencesLauncher;
 
 /**
  * Settings fragment that displays information about Chrome languages, which allow users to
  * seamlessly find and manage their languages preferences across platforms.
  */
-public class LanguagesPreferences extends PreferenceFragment {
-    // The keys for each prefernce shown on the langauges page.
-    static final String ADD_LANGUAGE_KEY = "add_language";
+public class LanguagesPreferences
+        extends PreferenceFragment implements AddLanguageFragment.Launcher {
+    private static final int REQUEST_CODE_ADD_LANGUAGES = 1;
+
+    // The keys for each preference shown on the languages page.
     static final String PREFERRED_LANGUAGES_KEY = "preferred_languages";
     static final String TRANSLATE_SWITCH_KEY = "translate_switch";
 
     private LanguageListPreference mLanguageListPref;
-    private Preference mAddLanguagePref;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,11 +39,8 @@ public class LanguagesPreferences extends PreferenceFragment {
         getActivity().setTitle(R.string.prefs_languages);
         PreferenceUtils.addPreferencesFromResource(this, R.xml.languages_preferences);
 
-        mAddLanguagePref = findPreference(ADD_LANGUAGE_KEY);
-        mAddLanguagePref.setIcon(TintedDrawable.constructTintedDrawable(
-                getResources(), R.drawable.plus, R.color.pref_accent_color));
-
         mLanguageListPref = (LanguageListPreference) findPreference(PREFERRED_LANGUAGES_KEY);
+        mLanguageListPref.registerActivityLauncher(this);
 
         ChromeSwitchPreference translateSwitch =
                 (ChromeSwitchPreference) findPreference(TRANSLATE_SWITCH_KEY);
@@ -61,5 +60,28 @@ public class LanguagesPreferences extends PreferenceFragment {
                 return PrefServiceBridge.getInstance().isTranslateManaged();
             }
         });
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        LanguagesManager.recycle();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_ADD_LANGUAGES && resultCode == getActivity().RESULT_OK) {
+            String code = data.getStringExtra(AddLanguageFragment.INTENT_NEW_ACCEPT_LANGAUGE);
+            LanguagesManager.getInstance().addToAcceptLanguages(code);
+        }
+    }
+
+    @Override
+    public void launchAddLanguage() {
+        // Launch preference activity with AddLanguageFragment.
+        Intent intent = PreferencesLauncher.createIntentForSettingsPage(
+                getActivity(), AddLanguageFragment.class.getName());
+        startActivityForResult(intent, REQUEST_CODE_ADD_LANGUAGES);
     }
 }
