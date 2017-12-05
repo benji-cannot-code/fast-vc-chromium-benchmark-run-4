3f7be00a127e53875327dc9101db8b4121d6cdbe
Lei Zhang <thestig@chromium.org>
2017-12-05 22:59:45 +0000

    Build PrinterManagerDialog whenever printing is enabled.
    Originally, it was only used when Print Preview is enabled, but the
    original user no longer exists, and the only user is the Settings page.
    Transitively only build settings::PrintingHandler when printing is
    enabled.
    
    BUG=789790
    
    Change-Id: I8e8b9d0df66b1a5ead01abeeaad78982f16e61ec
    Reviewed-on: https://chromium-review.googlesource.com/806447
    Reviewed-by: Dave Schuyler <dschuyler@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521880}

--- chrome/browser/printing/printer_manager_dialog.h
+++ chrome/browser/printing/printer_manager_dialog.h
@@ -7,10 +7,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_PRINTING_PRINTER_MANAGER_DIALOG_H_
 
 #include "base/macros.h"
+#include "printing/features/features.h"
+
+#if !BUILDFLAG(ENABLE_PRINTING)
+#error "Printing must be enabled"
+#endif
 
 namespace printing {
 
-// An abstraction of a printer manager dialog. This is used for print preview.
+// An abstraction of a printer manager dialog. This is used for the printing
+// sub-section of Settings.
 // This includes the OS-dependent UI to manage the network and local printers.
 class PrinterManagerDialog {
  public:
