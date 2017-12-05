615a512234b5d258f97cc45d3674b91ed39547db
Lei Tian <ltian@chromium.org>
2017-12-05 19:23:46 +0000

    Fix copy items' icon not shown in Chrome context menu.
    Recently all copy related items' change from png to vector drawable,
    this makes TintedDrawable.constructTintedDrawable fail to render the
    icon because VectorDrawable does not extend TintedDrawable. Also since
    ApiCompatibilityUtils.getDrawable cannot render vector drawable on pre-L
    devices, change to use AppCompatResources.getDrawable to render icon for
    context menu.
    
    Bug: 791780
    Change-Id: I35dead93f8ab5d89da49ef32180bebb644effc78
    Reviewed-on: https://chromium-review.googlesource.com/807344
    Commit-Queue: Lei Tian <ltian@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521778}

--- base/android/java/src/org/chromium/base/ApiCompatibilityUtils.java
+++ base/android/java/src/org/chromium/base/ApiCompatibilityUtils.java
@@ -22,6 +22,7 @@ import android.graphics.Color;
 import android.graphics.ColorFilter;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.VectorDrawable;
 import android.net.Uri;
 import android.os.Build;
 import android.os.PowerManager;
@@ -446,6 +447,8 @@ public class ApiCompatibilityUtils {
 
     /**
      * @see android.content.res.Resources#getDrawable(int id).
+     * TODO(ltian): use {@link AppCompatResources} to parse drawable to prevent fail on
+     * {@link VectorDrawable}. (http://crbug.com/792129)
      */
     @SuppressWarnings("deprecation")
     public static Drawable getDrawable(Resources res, int id) throws NotFoundException {
