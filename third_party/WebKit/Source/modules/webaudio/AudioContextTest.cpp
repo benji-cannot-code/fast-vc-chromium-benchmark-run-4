24e1f1a969ceece38c3a607b3fcdf5d48d8e88dc
Hongchan Choi <hongchan@chromium.org>
2017-12-05 22:38:08 +0000

    Remove AudioWorkletThread from AudioContextTest.cpp
    AudioContextTest.cpp tests the audio latency and is not involved
    any AudioWorklet operation. Removing AudioWorkletThread addresses
    the data race found in the bug.
    
    Locally confirmed that TSAN with this patch does not crash with
    the data race any more.
    
    Bug: 792140
    Change-Id: I297533a176c42fb1af5025fdcd77a1cf4e68ee56
    Reviewed-on: https://chromium-review.googlesource.com/809825
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Raymond Toy <rtoy@chromium.org>
    Commit-Queue: Hongchan Choi <hongchan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521866}

--- third_party/WebKit/Source/modules/webaudio/AudioContextTest.cpp
+++ third_party/WebKit/Source/modules/webaudio/AudioContextTest.cpp
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/dom/Document.h"
 #include "core/testing/DummyPageHolder.h"
-#include "modules/webaudio/AudioWorkletThread.h"
 #include "platform/testing/TestingPlatformSupport.h"
 #include "platform/wtf/PtrUtil.h"
 #include "public/platform/WebAudioDevice.h"
@@ -95,7 +94,6 @@ class AudioContextTest : public ::testing::Test {
   }
 
   void SetUp() override {
-    AudioWorkletThread::CreateSharedBackingThreadForTest();
     dummy_page_holder_ = DummyPageHolder::Create();
   }
 
