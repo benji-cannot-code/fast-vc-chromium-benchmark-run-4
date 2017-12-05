5ee5dbac99ae3f9f9764b5f4afcb0a4d79aa74de
Darren Shen <shend@chromium.org>
2017-12-05 00:50:38 +0000

    Do not treat escaped asterisk as universal selector.
    This patch fixes an issue where an escaped asterisk character is being
    treated as a universal selector. The problem was that \* and * were
    represented as g_star_atom (the '*' character) in selector matching.
    
    To differentiate between the two, we picked a different way to represent
    universal selectors. The local name of a universal selector becomes
    g_null_atom instead of g_star_atom. This way, \* and * will no longer
    clash.
    
    Bug: 682747
    Change-Id: Ied14b468c8bfae6ed1022a565f4f46cea4c556a8
    Reviewed-on: https://chromium-review.googlesource.com/765626
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521556}

--- third_party/WebKit/Source/core/css/RuleSet.cpp
+++ third_party/WebKit/Source/core/css/RuleSet.cpp
@@ -108,7 +108,8 @@ static void ExtractSelectorValues(const CSSSelector* selector,
       class_name = selector->Value();
       break;
     case CSSSelector::kTag:
-      if (selector->TagQName().LocalName() != g_star_atom)
+      if (selector->TagQName().LocalName() !=
+          CSSSelector::UniversalSelectorAtom())
         tag_name = selector->TagQName().LocalName();
       break;
     case CSSSelector::kPseudoClass:
