fafab3f87dd05b2ff683d72f512d9fcb071bde08
Alice Boxhall <aboxhall@chromium.org>
2017-12-05 05:46:41 +0000

    [Devtools] Don't try to update _contrastDetails if _contrastInfo doesn't exist
    Bug: 514674
    Change-Id: I122ca163f9738e825e80c179e5dff803b488295d
    Reviewed-on: https://chromium-review.googlesource.com/807336
    Commit-Queue: Alice Boxhall <aboxhall@chromium.org>
    Commit-Queue: Pavel Feldman <pfeldman@chromium.org>
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521621}

--- third_party/WebKit/Source/devtools/front_end/color_picker/Spectrum.js
+++ third_party/WebKit/Source/devtools/front_end/color_picker/Spectrum.js
@@ -592,8 +592,10 @@ ColorPicker.Spectrum = class extends UI.VBox {
    * @param {?SDK.CSSModel.ContrastInfo} contrastInfo
    */
   setContrastInfo(contrastInfo) {
-    if (this._contrastInfo)
-      this._contrastInfo.update(contrastInfo);
+    if (!this._contrastInfo)
+      return;
+
+    this._contrastInfo.update(contrastInfo);
 
     // Contrast info may cause contrast details to become visible.
     if (this._contrastDetails.visible())
