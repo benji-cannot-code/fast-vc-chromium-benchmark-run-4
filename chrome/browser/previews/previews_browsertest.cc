cd393e3683705ca78769fe156d6b53bae759ad60
Doug Arnett <dougarnett@chromium.org>
2017-12-05 22:02:31 +0000

    Fixes InfoBar presentation for NoScript preview
    Bug: 792133
    Change-Id: Iaa99ae16b359560f60e23c07cace1241781b6bbd
    Reviewed-on: https://chromium-review.googlesource.com/809296
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Doug Arnett <dougarnett@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521846}

--- chrome/browser/previews/previews_browsertest.cc
+++ chrome/browser/previews/previews_browsertest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/run_loop.h"
+#include "base/test/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
@@ -98,11 +99,15 @@ class PreviewsBrowserTest : public InProcessBrowserTest {
 // a script resource. Verifies that the noscript tag is not evaluated and the
 // script resource is loaded.
 IN_PROC_BROWSER_TEST_F(PreviewsBrowserTest, NoScriptPreviewsDisabled) {
+  base::HistogramTester histogram_tester;
   ui_test_utils::NavigateToURL(browser(), https_url());
 
   // Verify loaded js resource but not css triggered by noscript tag.
   EXPECT_TRUE(noscript_js_requested());
   EXPECT_FALSE(noscript_css_requested());
+
+  // Verify info bar not presented via histogram check.
+  histogram_tester.ExpectTotalCount("Previews.InfoBarAction.NoScript", 0);
 }
 
 // This test class enables NoScriptPreviews but without OptimizationHints.
@@ -139,11 +144,15 @@ class PreviewsNoScriptBrowserTest : public PreviewsBrowserTest {
 // script resource is not loaded.
 IN_PROC_BROWSER_TEST_F(PreviewsNoScriptBrowserTest,
                        MAYBE_NoScriptPreviewsEnabled) {
+  base::HistogramTester histogram_tester;
   ui_test_utils::NavigateToURL(browser(), https_url());
 
   // Verify loaded noscript tag triggered css resource but not js one.
   EXPECT_TRUE(noscript_css_requested());
   EXPECT_FALSE(noscript_js_requested());
+
+  // Verify info bar presented via histogram check.
+  histogram_tester.ExpectUniqueSample("Previews.InfoBarAction.NoScript", 0, 1);
 }
 
 IN_PROC_BROWSER_TEST_F(PreviewsNoScriptBrowserTest,
@@ -157,11 +166,15 @@ IN_PROC_BROWSER_TEST_F(PreviewsNoScriptBrowserTest,
 
 IN_PROC_BROWSER_TEST_F(PreviewsNoScriptBrowserTest,
                        MAYBE_NoScriptPreviewsEnabledHttpRedirectToHttps) {
+  base::HistogramTester histogram_tester;
   ui_test_utils::NavigateToURL(browser(), redirect_url());
 
   // Verify loaded noscript tag triggered css resource but not js one.
   EXPECT_TRUE(noscript_css_requested());
   EXPECT_FALSE(noscript_js_requested());
+
+  // Verify info bar presented via histogram check.
+  histogram_tester.ExpectUniqueSample("Previews.InfoBarAction.NoScript", 0, 1);
 }
 
 // This test class enables NoScriptPreviews with OptimizationHints.
