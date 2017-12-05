554ec7a54ffd40e013033ca2d3e4e682839aa0e4
Gauthier Ambard <gambard@chromium.org>
2017-12-05 10:35:58 +0000

    Add the shadow below the toolbar
    The toolbar has a shadown below it.
    
    Bug: 791469
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Iaf6ee9fe5190d2c4d5b6243bca3282d01ecba7ca
    Reviewed-on: https://chromium-review.googlesource.com/806175
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521654}

--- ios/chrome/browser/ui/toolbar/clean/toolbar_view_controller.mm
+++ ios/chrome/browser/ui/toolbar/clean/toolbar_view_controller.mm
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/toolbar/public/web_toolbar_controller_constants.h"
 #import "ios/chrome/browser/ui/uikit_ui_util.h"
 #import "ios/chrome/browser/ui/util/constraints_ui_util.h"
+#include "ios/chrome/grit/ios_theme_resources.h"
 #import "ios/third_party/material_components_ios/src/components/ProgressView/src/MaterialProgressView.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -47,6 +48,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @property(nonatomic, assign) BOOL voiceSearchEnabled;
 @property(nonatomic, strong) MDCProgressView* progressBar;
 @property(nonatomic, strong) UIStackView* locationBarContainerStackView;
+// The shadow below the toolbar. Lazily instantiated.
+@property(nonatomic, strong) UIImageView* shadowView;
 // Background view, used to display the incognito NTP background color on the
 // toolbar.
 @property(nonatomic, strong) UIView* backgroundView;
@@ -88,24 +91,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @synthesize voiceSearchEnabled = _voiceSearchEnabled;
 @synthesize progressBar = _progressBar;
 @synthesize locationBarContainerStackView = _locationBarContainerStackView;
+@synthesize shadowView = _shadowView;
 @synthesize expandedToolbarConstraints = _expandedToolbarConstraints;
 @synthesize topSafeAnchor = _topSafeAnchor;
 @synthesize regularToolbarConstraints = _regularToolbarConstraints;
-
-#pragma mark - Properties accessor
-
-- (UIView*)backgroundView {
-  if (!_backgroundView) {
-    _backgroundView = [[UIView alloc] initWithFrame:CGRectZero];
-    _backgroundView.translatesAutoresizingMaskIntoConstraints = NO;
-    _backgroundView.backgroundColor =
-        [UIColor colorWithWhite:kNTPBackgroundColorBrightnessIncognito
-                          alpha:1.0];
-    [self.view insertSubview:_backgroundView atIndex:0];
-    AddSameConstraints(self.view, _backgroundView);
-  }
-  return _backgroundView;
-}
 
 #pragma mark - Public
 
@@ -193,6 +182,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self setUpToolbarStackView];
   [self setUpLocationBarContainerView];
   [self.view addSubview:self.stackView];
+  [self.view addSubview:self.shadowView];
   [self.view addSubview:self.progressBar];
   [self setConstraints];
 }
@@ -257,6 +247,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         constraintEqualToConstant:kProgressBarHeight],
   ];
   [NSLayoutConstraint activateConstraints:progressBarConstraints];
+
+  // Shadow constraints.
+  [NSLayoutConstraint activateConstraints:@[
+    [self.shadowView.topAnchor constraintEqualToAnchor:self.view.bottomAnchor],
+    [self.shadowView.leadingAnchor
+        constraintEqualToAnchor:self.view.leadingAnchor],
+    [self.shadowView.trailingAnchor
+        constraintEqualToAnchor:self.view.trailingAnchor],
+    [self.shadowView.heightAnchor
+        constraintEqualToConstant:kToolbarShadowHeight],
+  ]];
 
   // StackView constraints. The main Toolbar StackView.
   NSArray* stackViewConstraints = @[
@@ -614,6 +615,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #pragma mark - Setters & Getters.
 
+- (UIView*)backgroundView {
+  if (!_backgroundView) {
+    _backgroundView = [[UIView alloc] initWithFrame:CGRectZero];
+    _backgroundView.translatesAutoresizingMaskIntoConstraints = NO;
+    _backgroundView.backgroundColor =
+        [UIColor colorWithWhite:kNTPBackgroundColorBrightnessIncognito
+                          alpha:1.0];
+    [self.view insertSubview:_backgroundView atIndex:0];
+    AddSameConstraints(self.view, _backgroundView);
+  }
+  return _backgroundView;
+}
+
 - (NSLayoutYAxisAnchor*)topSafeAnchor {
   if (!_topSafeAnchor) {
     if (@available(iOS 11, *)) {
@@ -670,6 +684,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [view setContentHuggingPriority:UILayoutPriorityDefaultLow
                           forAxis:UILayoutConstraintAxisHorizontal];
   _locationBarView = view;
+}
+
+- (UIImageView*)shadowView {
+  if (!_shadowView) {
+    _shadowView = [[UIImageView alloc] init];
+    _shadowView.translatesAutoresizingMaskIntoConstraints = NO;
+    _shadowView.userInteractionEnabled = NO;
+    _shadowView.image = NativeImage(IDR_IOS_TOOLBAR_SHADOW);
+  }
+  return _shadowView;
 }
 
 #pragma mark - Private
