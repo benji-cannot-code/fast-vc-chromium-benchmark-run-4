86aeeea75a022035858c292e3f9260eb34537e53
Michael Spang <spang@chromium.org>
2017-12-05 18:22:15 +0000

    chromecast: trace.py: Add system tracing support
    This adds supports for system tracing via trace.py. It is disabled by
    default but can be enable via the --systrace parameter. Requesting
    system tracing does no harm on devices where it is not available.
    
    This requires switching the way data is read back over the websocket, as
    the "ReportEvents" style cannot return data from secondary tracing agents
    such as CastTracingAgent.
    
    BUG=786091
    
    Change-Id: I8b5b39c047ae03073d90d462a4febf1770a8ccd5
    Reviewed-on: https://chromium-review.googlesource.com/804559
    Commit-Queue: Michael Spang <spang@chromium.org>
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521750}

--- chromecast/tools/trace.py
+++ chromecast/tools/trace.py
@@ -71,6 +71,12 @@ def _CreateOptionParser():
            'and used as a ring buffer. If this option is omitted then '
            'recording stops when the trace buffer is full.',
       action='store_true')
+  tracing_opts.add_option(
+      '--systrace',
+      help='Enable system tracing.',
+      action='store_true',
+      dest='systrace',
+      default=False)
   parser.add_option_group(tracing_opts)
 
   output_options = optparse.OptionGroup(parser, 'Output options')
@@ -93,7 +99,7 @@ def main():
 
   with Connect(options) as tracing_backend:
     tracing_backend.StartTracing(options.category_filter,
-                                 options.record_continuously)
+                                 options.record_continuously, options.systrace)
     raw_input('Capturing trace. Press Enter to stop...')
     filepath = GetOutputFilePath(options)
     tracing_backend.StopTracing(filepath)
