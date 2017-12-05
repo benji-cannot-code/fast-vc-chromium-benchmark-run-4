44607cbdb1911f2b1a17c99d07479ce1cded0655
Mike Reed <reed@google.com>
2017-12-05 18:34:13 +0000

    IWYU SkPixelSerializer is no longer brought in by these other skia headers
    Needed to land https://skia-review.googlesource.com/c/skia/+/76980
    
    Bug: 
    Change-Id: I07f1647b0d342baebadfa0549f54e5252ee4395d
    Reviewed-on: https://chromium-review.googlesource.com/809089
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Commit-Queue: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521755}

--- skia/ext/skia_utils_base.cc
+++ skia/ext/skia_utils_base.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/pickle.h"
 #include "third_party/skia/include/core/SkData.h"
 #include "third_party/skia/include/core/SkImageDeserializer.h"
+#include "third_party/skia/include/core/SkPixelSerializer.h"
 #include "third_party/skia/include/core/SkWriteBuffer.h"
 #include "third_party/skia/src/core/SkValidatingReadBuffer.h"
 
