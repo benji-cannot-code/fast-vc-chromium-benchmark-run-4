cb53e3963e050bfc46cd676fa1c7f770572704b1
Adithya Srinivasan <adithyas@chromium.org>
2017-12-05 17:26:40 +0000

    Use DoublyLinkedList instead of ListHashSet in DocumentState
    The only operations carried out on form_controls_ are insertions, removals
    and iterating through the entire list. Insertion and removal can be done
    faster with a DoublyLinkedList.
    
    Since the nodes for the DoublyLinkedList are Oilpan objects, this CL
    introduces HeapDoublyLinkedList that uses Member for the head and tail
    pointers, and traces the pointers.
    
    This improves the performance of HTMLInputElement::InsertedInto and
    
    HTMLInputElement: :RemovedFrom by ~15%.
    Change-Id: Ic6deac03e63cf0583ced9de9caccd1b533c6b88d
    Reviewed-on: https://chromium-review.googlesource.com/803944
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521732}

--- third_party/WebKit/Source/core/html/forms/HTMLFormControlElementWithState.h
+++ third_party/WebKit/Source/core/html/forms/HTMLFormControlElementWithState.h
@@ -28,13 +28,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/CoreExport.h"
 #include "core/html/forms/HTMLFormControlElement.h"
+#include "platform/wtf/DoublyLinkedList.h"
 
 namespace blink {
 
 class FormControlState;
 
+class HTMLFormControlElementWithState;
+// Cannot use eager tracing as HTMLFormControlElementWithState objects are part
+// of a HeapDoublyLinkedList and have pointers to the previous and next elements
+// in the list, which can cause very deep stacks in eager tracing.
+WILL_NOT_BE_EAGERLY_TRACED_CLASS(HTMLFormControlElementWithState);
+
 class CORE_EXPORT HTMLFormControlElementWithState
-    : public HTMLFormControlElement {
+    : public HTMLFormControlElement,
+      public DoublyLinkedListNode<HTMLFormControlElementWithState> {
  public:
   ~HTMLFormControlElementWithState() override;
 
@@ -47,6 +55,8 @@ class CORE_EXPORT HTMLFormControlElementWithState
   virtual void RestoreFormControlState(const FormControlState&) {}
   void NotifyFormStateChanged();
 
+  void Trace(Visitor*) override;
+
  protected:
   HTMLFormControlElementWithState(const QualifiedName& tag_name, Document&);
 
@@ -54,6 +64,15 @@ class CORE_EXPORT HTMLFormControlElementWithState
   InsertionNotificationRequest InsertedInto(ContainerNode*) override;
   void RemovedFrom(ContainerNode*) override;
   bool IsFormControlElementWithState() const final;
+
+ private:
+  // Pointers for DoublyLinkedListNode<HTMLFormControlElementWithState>. This
+  // is used for adding an instance to a list of form controls stored in
+  // DocumentState. Each instance is only added to its containing document's
+  // DocumentState list.
+  friend class WTF::DoublyLinkedListNode<HTMLFormControlElementWithState>;
+  Member<HTMLFormControlElementWithState> prev_;
+  Member<HTMLFormControlElementWithState> next_;
 };
 
 DEFINE_TYPE_CASTS(HTMLFormControlElementWithState,
--- third_party/WebKit/Source/platform/heap/TraceTraits.h
+++ third_party/WebKit/Source/platform/heap/TraceTraits.h
@@ -28,6 +28,10 @@ class CrossThreadPersistent;
 template <typename T>
 class CrossThreadWeakPersistent;
 template <typename T>
+class HeapDoublyLinkedList;
+template <typename T>
+class HeapTerminatedArray;
+template <typename T>
 class Member;
 template <typename T>
 class TraceEagerlyTrait;
@@ -429,6 +433,20 @@ class TraceEagerlyTrait<CrossThreadPersistent<T>> {
 
 template <typename T>
 class TraceEagerlyTrait<CrossThreadWeakPersistent<T>> {
+  STATIC_ONLY(TraceEagerlyTrait);
+
+ public:
+  static const bool value = TraceEagerlyTrait<T>::value;
+};
+
+template <typename T>
+class TraceEagerlyTrait<HeapTerminatedArray<T>> {
+ public:
+  static const bool value = TraceEagerlyTrait<T>::value;
+};
+
+template <typename T>
+class TraceEagerlyTrait<HeapDoublyLinkedList<T>> {
   STATIC_ONLY(TraceEagerlyTrait);
 
  public:
