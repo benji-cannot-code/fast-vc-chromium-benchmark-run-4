74ec125f82ec1bf49c85f8d578df6f178daa3275
Alexandr Ilin <alexilin@chromium.org>
2017-12-05 19:06:27 +0000

    android: Fix Startup.FirstCommitNavigationTime histogram
    This CL makes following changes to the
    Startup.FirstCommitNavigationTime3 histogram:
    * Do not record the histogram multiple times per session
    * Start recording from the application start time instead of the
      foreground time
    * Record the histogram only for ChromeTabbedActivity
    * Ignore foreground state, there will be separate histogram for this
      purpose
    * Discard error, same document and non http(s) pages
    
    The CL also renames the histogram to
    Startup.Android.Experimental.Cold.TimeToFirstNavigationCommit
    
    The rationale behind the recording the histogram from the application
    start time instead of the foreground time:
    
    The current metric doesn't cover the important part of the startup
    latency because the ForegroundStartTime is measured after the native
    library is loaded. That's a lot of important data missed. We're
    exploring various ways to speed up the startup including the time
    of native library initialization. We want to have a metric that will
    show us these improvements.
    Initially, this metric was measuring cold startups from the application
    start but then it was changed. We would like to return to the initial
    meaning. pasko@ described an evolution of the metric in
    https://goo.gl/NyvX39
    
    Bug: 778302
    Change-Id: I01d7a6a73fbc504e31c1e877b1253fcf69a54ad8
    Reviewed-on: https://chromium-review.googlesource.com/788875
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Commit-Queue: Alexandr Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521771}

--- chrome/android/java/src/org/chromium/chrome/browser/tab/TabWebContentsObserver.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/TabWebContentsObserver.java
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.tab;
 
-import android.os.SystemClock;
 import android.support.annotation.IntDef;
 import android.view.View;
 
@@ -21,10 +20,9 @@ import org.chromium.chrome.browser.media.MediaCaptureNotificationService;
 import org.chromium.chrome.browser.metrics.UmaUtils;
 import org.chromium.chrome.browser.policy.PolicyAuditor;
 import org.chromium.chrome.browser.policy.PolicyAuditor.AuditEvent;
+import org.chromium.chrome.browser.util.UrlUtilities;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.content_public.browser.WebContentsObserver;
-
-import java.util.concurrent.TimeUnit;
 
 /**
  * WebContentsObserver used by Tab.
@@ -207,22 +205,10 @@ public class TabWebContentsObserver extends WebContentsObserver {
             recordErrorInPolicyAuditor(url, errorDescription, errorCode);
         }
 
+        boolean isTrackedPage = hasCommitted && isInMainFrame && !isErrorPage && !isSameDocument
+                && !isFragmentNavigation && UrlUtilities.isHttpOrHttps(url);
+        UmaUtils.recordFinishNavigation(isTrackedPage);
         if (!hasCommitted) return;
-        if (isInMainFrame && UmaUtils.hasComeToForeground()) {
-            // Current median is 550ms, and long tail is very long. ZoomedIn gives good view of the
-            // median and ZoomedOut gives a good overview.
-            RecordHistogram.recordCustomTimesHistogram(
-                    "Startup.FirstCommitNavigationTime3.ZoomedIn",
-                    SystemClock.uptimeMillis() - UmaUtils.getForegroundStartTime(), 200, 1000,
-                    TimeUnit.MILLISECONDS, 100);
-            // For ZoomedOut very rarely is it under 50ms and this range matches
-            // CustomTabs.IntentToFirstCommitNavigationTime2.ZoomedOut.
-            RecordHistogram.recordCustomTimesHistogram(
-                    "Startup.FirstCommitNavigationTime3.ZoomedOut",
-                    SystemClock.uptimeMillis() - UmaUtils.getForegroundStartTime(), 50,
-                    TimeUnit.MINUTES.toMillis(10), TimeUnit.MILLISECONDS, 50);
-            UmaUtils.setRunningApplicationStart(false);
-        }
 
         if (isInMainFrame) {
             mTab.setIsTabStateDirty(true);
