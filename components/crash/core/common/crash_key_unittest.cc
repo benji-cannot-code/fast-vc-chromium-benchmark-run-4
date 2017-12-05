a2c7c182a5ab2932d063042dfe67df39ad94adea
Robert Sesek <rsesek@chromium.org>
2017-12-05 22:45:58 +0000

    Skip CrashKeyStringTest.SetStackTrace in official builds if there are no unwind tables.
    In this configuration, StackTrace may produce an empty result,
    which causes the is_set() assertion to fail.
    
    Bug: 791223, 598854
    Change-Id: I4c4cabf2eb1176def431cd5798a2c80808bf8074
    Reviewed-on: https://chromium-review.googlesource.com/809102
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521870}

--- components/crash/core/common/crash_key_unittest.cc
+++ components/crash/core/common/crash_key_unittest.cc
@@ -70,6 +70,9 @@ TEST_F(CrashKeyStringTest, FormatStackTrace64) {
 }
 #endif
 
+// In certain build configurations, StackTrace will produce an
+// empty result, which will cause the test to fail.
+#if !defined(OFFICIAL_BUILD) && !defined(NO_UNWIND_TABLES)
 TEST_F(CrashKeyStringTest, SetStackTrace) {
   static CrashKeyString<1024> key("test-trace");
 
@@ -79,6 +82,7 @@ TEST_F(CrashKeyStringTest, SetStackTrace) {
 
   EXPECT_TRUE(key.is_set());
 }
+#endif
 
 TEST_F(CrashKeyStringTest, BaseSupport) {
   static base::debug::CrashKeyString* crash_key =
