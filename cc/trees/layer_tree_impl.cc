0682f848df6f571e2428936a82ef78015ba5a544
Yi Gu <yigu@chromium.org>
2017-12-05 23:30:21 +0000

    Add metrics to measure time spent finding the closest matching layer in hit testing
    The proposed metrics can help us making compositing related decisions in
    the future. They could also help locating regression reasons.
    
    Bug: 776863
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I7cec76d6112b99fb1d02cff3a8f2ae46dd06f2c3
    Reviewed-on: https://chromium-review.googlesource.com/806690
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521894}

--- cc/trees/layer_tree_impl.cc
+++ cc/trees/layer_tree_impl.cc
@@ -1879,6 +1879,7 @@ static void FindClosestMatchingLayer(const gfx::PointF& screen_space_point,
                                      LayerImpl* root_layer,
                                      const Functor& func,
                                      FindClosestMatchingLayerState* state) {
+  base::ElapsedTimer timer;
   // We want to iterate from front to back when hit testing.
   for (auto* layer : base::Reversed(*root_layer->layer_tree_impl())) {
     if (!func(layer))
@@ -1906,6 +1907,12 @@ static void FindClosestMatchingLayer(const gfx::PointF& screen_space_point,
       state->closest_distance = distance_to_intersection;
       state->closest_match = layer;
     }
+  }
+  if (const char* client_name = GetClientNameForMetrics()) {
+    UMA_HISTOGRAM_COUNTS_1M(
+        base::StringPrintf("Compositing.%s.HitTestTimeToFindClosestLayer",
+                           client_name),
+        timer.Elapsed().InMicroseconds());
   }
 }
 
