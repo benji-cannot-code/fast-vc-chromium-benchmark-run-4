3f6c7aa70c6b5910f029a393eed1189fbabe2956
Jun Cai <juncai@chromium.org>
2017-12-05 18:23:11 +0000

    NetworkService: Fix NewTabPageInterceptorTest.NoInterception by using https test server
    This CL fixes the NewTabPageInterceptorTest.NoInterception browser test
    for network service by using https embedded test server instead of
    URLRequestMockHTTPJob::GetMockHttpsUrl().
    
    Bug: 776589
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I4d1f911ec4db21a2f086c09d3d6a99e41ad75230
    Reviewed-on: https://chromium-review.googlesource.com/802079
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Jun Cai <juncai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521751}

--- chrome/browser/ui/search/new_tab_page_interceptor_browsertest.cc
+++ chrome/browser/ui/search/new_tab_page_interceptor_browsertest.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/navigation_controller.h"
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/web_contents.h"
+#include "net/test/embedded_test_server/embedded_test_server.h"
 #include "net/test/url_request/url_request_mock_http_job.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
@@ -56,8 +57,10 @@ class NewTabPageInterceptorTest : public InProcessBrowserTest {
 };
 
 IN_PROC_BROWSER_TEST_F(NewTabPageInterceptorTest, NoInterception) {
-  GURL new_tab_url =
-      net::URLRequestMockHTTPJob::GetMockHttpsUrl("instant_extended.html");
+  net::EmbeddedTestServer https_test_server(
+      net::EmbeddedTestServer::TYPE_HTTPS);
+  ASSERT_TRUE(https_test_server.Start());
+  GURL new_tab_url = https_test_server.GetURL("/instant_extended.html");
   ChangeDefaultSearchProvider("instant_extended.html");
 
   ui_test_utils::NavigateToURL(browser(), new_tab_url);
