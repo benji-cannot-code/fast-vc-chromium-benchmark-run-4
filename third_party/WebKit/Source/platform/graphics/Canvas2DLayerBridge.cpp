200c47873bb34df57b0640920fbaab72c087a12c
Justin Novosad <junov@chromium.org>
2017-12-05 22:44:49 +0000

    Move TransferableResource creation to Canvas Resource
    This change makes it possible to decouple the actions of capturing an
    animation frame from a canvas and sending it to the compositor.
    This is a prerequisite for mergin OffscreenCanvasResourceProdiver
    with CanvasResourceProvider.
    
    Bug: 788439, 776801
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Iaf4563069ede53b1bba8bde5ea8b693d4c1a2b73
    Reviewed-on: https://chromium-review.googlesource.com/803945
    Commit-Queue: Justin Novosad <junov@chromium.org>
    Reviewed-by: Olivia Lai <xlai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521868}

--- third_party/WebKit/Source/platform/graphics/Canvas2DLayerBridge.cpp
+++ third_party/WebKit/Source/platform/graphics/Canvas2DLayerBridge.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/WebTaskRunner.h"
 #include "platform/graphics/CanvasHeuristicParameters.h"
 #include "platform/graphics/CanvasMetrics.h"
+#include "platform/graphics/CanvasResource.h"
 #include "platform/graphics/CanvasResourceProvider.h"
 #include "platform/graphics/GraphicsLayer.h"
 #include "platform/graphics/ImageBuffer.h"
@@ -651,12 +652,11 @@ bool Canvas2DLayerBridge::PrepareTransferableResource(
   if (!GetOrCreateResourceProvider())
     return false;
 
-  if (!resource_provider_->IsAccelerated()) {
-  }
-
   FlushRecording();
-  if (resource_provider_->PrepareTransferableResource(out_resource,
-                                                      out_release_callback)) {
+  scoped_refptr<CanvasResource> frame = resource_provider_->ProduceFrame();
+  if (frame) {
+    // Note frame is kept alive via a reference kept in out_release_callback.
+    frame->PrepareTransferableResource(out_resource, out_release_callback);
     out_resource->color_space = color_params_.GetSamplerGfxColorSpace();
     return true;
   }
