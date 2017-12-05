2b2933646bf44dcf0bf66a0cc19afa4cfbb5e60f
Robert Sesek <rsesek@chromium.org>
2017-12-05 01:51:26 +0000

    crash_keys: Remove old "newframe_*" keys.
    Bug: 598854, 626802
    Change-Id: I89e8afa29d884a63b6db68b9618d87ea5863432c
    Reviewed-on: https://chromium-review.googlesource.com/807169
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521585}

--- content/renderer/render_thread_impl.cc
+++ content/renderer/render_thread_impl.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/allocator/allocator_extension.h"
 #include "base/at_exit.h"
 #include "base/command_line.h"
-#include "base/debug/crash_logging.h"
 #include "base/lazy_instance.h"
 #include "base/logging.h"
 #include "base/macros.h"
@@ -2229,22 +2228,6 @@ void RenderThreadImpl::CreateView(mojom::CreateViewParamsPtr params) {
 }
 
 void RenderThreadImpl::CreateFrame(mojom::CreateFrameParamsPtr params) {
-  // Debug cases of https://crbug.com/626802.
-  base::debug::SetCrashKeyValue("newframe_routing_id",
-                                base::IntToString(params->routing_id));
-  base::debug::SetCrashKeyValue("newframe_proxy_id",
-                                base::IntToString(params->proxy_routing_id));
-  base::debug::SetCrashKeyValue("newframe_opener_id",
-                                base::IntToString(params->opener_routing_id));
-  base::debug::SetCrashKeyValue("newframe_parent_id",
-                                base::IntToString(params->parent_routing_id));
-  base::debug::SetCrashKeyValue("newframe_widget_id",
-                                base::IntToString(
-                                    params->widget_params->routing_id));
-  base::debug::SetCrashKeyValue("newframe_widget_hidden",
-                                params->widget_params->hidden ? "yes" : "no");
-  base::debug::SetCrashKeyValue("newframe_replicated_origin",
-                                params->replication_state.origin.Serialize());
   CompositorDependencies* compositor_deps = this;
   service_manager::mojom::InterfaceProviderPtr interface_provider(
       std::move(params->interface_provider));
