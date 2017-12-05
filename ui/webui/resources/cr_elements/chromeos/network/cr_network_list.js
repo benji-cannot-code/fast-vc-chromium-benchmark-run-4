47837173f2093c1015521f7b8e30a3d73db7c9fd
Steven Bennetts <stevenjb@chromium.org>
2017-12-05 22:12:45 +0000

    Request focus on update after selecting network item
    This change requests focusing of the first item after selecting
    an item. Generally that should focus the newly connecting item as
    soon as the list is updated.
    
    Bug: 739633
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I728658d1bbc916c56793960c576e47fe5bc91354
    Reviewed-on: https://chromium-review.googlesource.com/806440
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521854}

--- ui/webui/resources/cr_elements/chromeos/network/cr_network_list.js
+++ ui/webui/resources/cr_elements/chromeos/network/cr_network_list.js
@@ -114,9 +114,11 @@ Polymer({
    * @private
    */
   onItemAction_: function(item) {
-    if (item.hasOwnProperty('customItemName'))
+    if (item.hasOwnProperty('customItemName')) {
       this.fire('custom-item-selected', item);
-    else
+    } else {
       this.fire('selected', item);
+      this.focusRequested_ = true;
+    }
   },
 });
