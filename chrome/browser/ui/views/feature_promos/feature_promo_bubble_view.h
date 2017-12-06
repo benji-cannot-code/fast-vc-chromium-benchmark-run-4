9a33898770a783db3f254d6764cfb8ca4e9268dd
Bettina Dea <bdea@chromium.org>
2017-12-06 02:25:27 +0000

    Move desktop IPH promo bubble to the right and removed shadow.
    Previously the new tab desktop in-product help promo bubble was too
    close to the new tab button. Thus, I had to move the the promo bubble
    further from the anchor_view of the button. I also removed the shadow
    from the new tab promo bubble. Note that new tab promo bubbles are
    inactive.
    
    Bug: 734132
    Change-Id: I5ba93f841413d3985ff810fbb0f009b8627289bb
    Reviewed-on: https://chromium-review.googlesource.com/797920
    Commit-Queue: Bettina Dea <bdea@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521958}

--- chrome/browser/ui/views/feature_promos/feature_promo_bubble_view.h
+++ chrome/browser/ui/views/feature_promos/feature_promo_bubble_view.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/timer/timer.h"
+#include "ui/gfx/geometry/rect.h"
 #include "ui/views/bubble/bubble_dialog_delegate.h"
 
 namespace gfx {
@@ -63,12 +64,14 @@ class FeaturePromoBubbleView : public views::BubbleDialogDelegateView {
   bool OnMousePressed(const ui::MouseEvent& event) override;
   void OnMouseEntered(const ui::MouseEvent& event) override;
   void OnMouseExited(const ui::MouseEvent& event) override;
+  gfx::Rect GetBubbleBounds() override;
 
   // Starts a timer to close the promo bubble.
   void StartAutoCloseTimer(base::TimeDelta auto_close_duration);
 
   // Timer used to auto close the bubble.
   base::OneShotTimer timer_;
+  const ActivationAction activation_action_;
 
   DISALLOW_COPY_AND_ASSIGN(FeaturePromoBubbleView);
 };
