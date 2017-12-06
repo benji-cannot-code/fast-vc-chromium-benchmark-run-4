9427382b82ddc532f5ac5aeb4761134c3c8cab3a
liberato@chromium.org <liberato@chromium.org>
2017-12-06 00:55:03 +0000

    Moved D3D11PictureBuffer into its own source files.
    Bug: 775577
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I99dc244ceb3905fd55c948398a7991a5a019e669
    Reviewed-on: https://chromium-review.googlesource.com/806734
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521925}

--- media/gpu/windows/d3d11_video_decoder_impl.cc
+++ media/gpu/windows/d3d11_video_decoder_impl.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/decoder_buffer.h"
 #include "media/base/video_decoder_config.h"
 #include "media/base/video_frame.h"
+#include "media/gpu/windows/d3d11_picture_buffer.h"
 #include "ui/gl/gl_angle_util_win.h"
 #include "ui/gl/gl_bindings.h"
 
@@ -28,7 +29,7 @@ static bool MakeContextCurrent(gpu::CommandBufferStub* stub) {
 
 D3D11VideoDecoderImpl::D3D11VideoDecoderImpl(
     base::Callback<gpu::CommandBufferStub*()> get_stub_cb,
-    OutputWithReleaseMailboxCB output_cb)
+    deprecated::OutputWithReleaseMailboxCB output_cb)
     : get_stub_cb_(get_stub_cb),
       output_cb_(std::move(output_cb)),
       weak_factory_(this) {}
@@ -340,7 +341,7 @@ void D3D11VideoDecoderImpl::CreatePictureBuffers() {
     picture_buffers_[i]->Init(video_device_, out_texture, decoder_guid_);
 
     // Bind the image to each texture.
-    for (int texture_idx = 0; texture_idx < texture_refs.size();
+    for (size_t texture_idx = 0; texture_idx < texture_refs.size();
          texture_idx++) {
       texture_manager->SetLevelImage(texture_refs[texture_idx].get(),
                                      GL_TEXTURE_EXTERNAL_OES, 0,
---
+++ media/gpu/windows/init_guid.cc
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This is defined here to ensure the D3D11, D3D9, and DXVA includes have their
+// GUIDs intialized.
+#define INITGUID
+#include <d3d11.h>
+#include <d3d9.h>
+#include <dxva.h>
+#undef INITGUID
