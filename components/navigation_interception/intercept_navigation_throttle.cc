cf958c36699f7fbc8b2d042dc6e92b2094099bf1
Charles Harrison <csharrison@chromium.org>
2017-12-06 23:31:45 +0000

    Add microsecond histogram for InterceptNavigationThrottle::WillStartRequest
    There have been chrome traces which indicate this throttle is expensive
    on Android. We should perform some measurements in the wild to see how
    often this is true.
    
    Bug: 792524
    Change-Id: I4b35949e0265c9e3b291d623789cbf4fd32d85f0
    Reviewed-on: https://chromium-review.googlesource.com/811828
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522250}

--- components/navigation_interception/intercept_navigation_throttle.cc
+++ components/navigation_interception/intercept_navigation_throttle.cc
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/navigation_interception/intercept_navigation_throttle.h"
 
+#include "base/metrics/histogram_macros.h"
+#include "base/time/time.h"
+#include "base/timer/elapsed_timer.h"
 #include "components/navigation_interception/navigation_params.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/navigation_handle.h"
@@ -24,7 +27,12 @@ InterceptNavigationThrottle::~InterceptNavigationThrottle() {}
 content::NavigationThrottle::ThrottleCheckResult
 InterceptNavigationThrottle::WillStartRequest() {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  return CheckIfShouldIgnoreNavigation(false);
+  base::ElapsedTimer timer;
+
+  auto result = CheckIfShouldIgnoreNavigation(false);
+  UMA_HISTOGRAM_COUNTS_10M("Navigation.Intercept.WillStart",
+                           timer.Elapsed().InMicroseconds());
+  return result;
 }
 
 content::NavigationThrottle::ThrottleCheckResult
