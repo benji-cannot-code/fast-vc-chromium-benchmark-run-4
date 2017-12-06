f228f95b9194c6e47a954a28656a989c3e109d7c
Alice Boxhall <aboxhall@chromium.org>
2017-12-06 03:07:37 +0000

    Return default font size if body font is somehow null in InspectorCSSAgent::getBackgroundColor()
    Bug: 768278
    Change-Id: I3d5f1aadba1ca967ecf8cb2a0f07e1e22de92559
    Reviewed-on: https://chromium-review.googlesource.com/807404
    Commit-Queue: meade_UTC10 <meade@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: meade_UTC10 <meade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521976}

--- third_party/WebKit/Source/core/inspector/InspectorCSSAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorCSSAgent.cpp
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/css/CSSStyleRule.h"
 #include "core/css/CSSStyleSheet.h"
 #include "core/css/CSSVariableData.h"
+#include "core/css/FontSizeFunctions.h"
 #include "core/css/MediaList.h"
 #include "core/css/MediaQuery.h"
 #include "core/css/MediaValues.h"
@@ -2322,7 +2323,21 @@ Response InspectorCSSAgent::getBackgroundColors(
       CSSComputedStyleDeclaration::Create(body, true);
   const CSSValue* body_font_size =
       computed_style_body->GetPropertyCSSValue(GetCSSPropertyFontSize());
-  *computed_body_font_size = body_font_size->CssText();
+  if (body_font_size) {
+    *computed_body_font_size = body_font_size->CssText();
+  } else {
+    // This is an extremely rare and pathological case -
+    // just return the baseline default to avoid a crash.
+    // crbug.com/738777
+    unsigned default_font_size_keyword =
+        FontSizeFunctions::InitialKeywordSize();
+    float default_font_size_pixels = FontSizeFunctions::FontSizeForKeyword(
+        &document, default_font_size_keyword, false);
+    *computed_body_font_size =
+        CSSPrimitiveValue::Create(default_font_size_pixels,
+                                  CSSPrimitiveValue::UnitType::kPixels)
+            ->CssText();
+  }
 
   return Response::OK();
 }
