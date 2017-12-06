6e5b703c136017c0dcf8f2c434d9e8512f2680ea
Vincent Scheib <scheib@chromium.org>
2017-12-06 19:39:14 +0000

    bluetooth: testing: Clarify client and implementation testing APIs.
    Comment only changes to explain the different Bluetooth testing APIs.
    
    TBR: scheib@chromium.org
    Change-Id: I04329dd47f485c2e1042f7287ac05b160599ead4
    Reviewed-on: https://chromium-review.googlesource.com/810087
    Reviewed-by: Vincent Scheib <scheib@chromium.org>
    Commit-Queue: Vincent Scheib <scheib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522166}

--- device/bluetooth/test/fake_read_response.h
+++ device/bluetooth/test/fake_read_response.h
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace bluetooth {
 
 // Holds the necessary values for dispatching a fake read response.
+//
+// Not intended for direct use by clients.  See README.md.
 class FakeReadResponse {
  public:
   FakeReadResponse(uint16_t gatt_code,
