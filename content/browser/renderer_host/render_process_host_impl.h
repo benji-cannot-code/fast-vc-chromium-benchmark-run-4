1349ef1020534a147b589acbc928aa9e18d6e312
Łukasz Anforowicz <lukasza@chromium.org>
2017-12-06 16:37:12 +0000

    Revert "Reject IPC requests for isolated origin, sent by non-isolated renderer."
    This reverts commit 39603d88a9cb45351da34a9e15ae320780fffe2b.
    
    Reason for revert: This CL has caused renderer kills during CrOS login - see https://crbug.com/792500
    
    Original change's description:
    > Reject IPC requests for isolated origin, sent by non-isolated renderer.
    > 
    > The CL tweaks StoragePartitonInterceptor (in
    > isolated_origin_browsertest.cc) so that it can be configured to inject
    > different origins, depending on needs of individual tests.  This tweak
    > is supported by changes in base/lazy_instance.h (adding of inequality
    > operator implemented on top of the already existing equality operator),
    > and in render_process_host_impl.cc/.h (to support creating a test-only
    > StoragePartitionService via a base::Callback, rather than via a function
    > pointer).
    > 
    > Tweaks of StoragePartitonInterceptor allow forking of
    > IsolatedOriginTest.LocalStorageOriginEnforcement test into 2 separate
    > tests:
    > - LocalStorageOriginEnforcement_IsolatedAccessingNonIsolated
    > - LocalStorageOriginEnforcement_NonIsolatedAccessingIsolated
    > The second test is introduced by this CL and was failing before this CL.
    > 
    > Tweaks of ChildProcessSecurityPolicyImpl::CanAccessDataForOrigin
    > disallow requests from non-isolated renderers (in which case
    > CheckOriginLock will return NO_LOCK) if the request is for
    > an isolated origin.  This makes the new test pass.
    > 
    > Bug: 509125, 764958
    > Change-Id: Ibfff2c91cb2ac51966e1d89295f10592a3814c08
    > Reviewed-on: https://chromium-review.googlesource.com/775060
    > Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    > Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#521838}
    
    TBR=thestig@chromium.org,alexmos@chromium.org,lukasza@chromium.org
    
    Change-Id: Id395a4be8f2127f2fbdc7e4f75e0fbd05272012f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 509125, 764958
    Reviewed-on: https://chromium-review.googlesource.com/810986
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522105}

--- content/browser/renderer_host/render_process_host_impl.h
+++ content/browser/renderer_host/render_process_host_impl.h
@@ -14,10 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <queue>
 #include <set>
 #include <string>
-#include <utility>
-#include <vector>
 
-#include "base/callback_forward.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/observer_list.h"
@@ -319,11 +316,11 @@ class CONTENT_EXPORT RenderProcessHostImpl
   // Allows external code to supply a function which creates a
   // StoragePartitionService. Used for supplying test versions of the
   // service.
-  using StoragePartitionServiceFactory = base::RepeatingCallback<void(
-      RenderProcessHostImpl* rph,
-      mojom::StoragePartitionServiceRequest request)>;
-  static void SetStoragePartitionServiceFactoryForTesting(
-      StoragePartitionServiceFactory factory);
+  using CreateStoragePartitionServiceFunction =
+      void (*)(RenderProcessHostImpl* rph,
+               mojom::StoragePartitionServiceRequest request);
+  static void SetCreateStoragePartitionServiceFunction(
+      CreateStoragePartitionServiceFunction function);
 
   RenderFrameMessageFilter* render_frame_message_filter_for_testing() const {
     return render_frame_message_filter_.get();
