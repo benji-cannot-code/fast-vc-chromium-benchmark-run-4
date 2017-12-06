200d2860f22579747ac4394bc69b6a9b4a49bebd
Sébastien Marchand <sebmarchand@chromium.org>
2017-12-06 20:47:15 +0000

    Revert "Media Engagement: Add DAFSA generator and reader"
    This reverts commit 8812513406603d77fa69083eccc96326d9744ed8.
    
    Reason for revert: Breaks the Windows build
    
    This breaks the Windows build (in some situation), https://chromium-review.googlesource.com/c/chromium/src/+/810787 fix some issue but it's not sufficient, e.g. https://ci.chromium.org/buildbot/tryserver.chromium.win/win7_chromium_rel_ng/60438 failed with the following stack trace:
    FAILED: gen/chrome/test/data/media/engagement/preload/test.pb 
    C:/b/depot_tools/win_tools-2_7_6_bin/python/bin/python.exe ../../tools/media_engagement_preload/make_dafsa.py ../../chrome/test/data/media/engagement/preload/test.json gen/chrome/test/data/media/engagement/preload/test.pb
    Traceback (most recent call last):
      File "../../tools/media_engagement_preload/make_dafsa.py", line 17, in <module>
        import media_engagement_preload_pb2
      File "C:\b\c\b\win\src\tools\media_engagement_preload\media_engagement_preload_pb2.py", line 8, in <module>
        from google.protobuf import descriptor as _descriptor
      File "C:\b\c\b\win\src\third_party\protobuf\python\google\protobuf\descriptor.py", line 37, in <module>
        import six
    ImportError: No module named six
    
    Original change's description:
    > Media Engagement: Add DAFSA generator and reader
    > 
    > Add a tool to generate preload data in DAFSA format and
    > add a MediaEngagementPreloadedList class to read it.
    > 
    > BUG=787464
    > 
    > TBR=dpranke@chromium.org
    > 
    > Change-Id: If0d99a2e1091d0b23699f2173d8146582173ee54
    > Reviewed-on: https://chromium-review.googlesource.com/803994
    > Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    > Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    > Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#522068}
    
    TBR=dalecurtis@chromium.org,mlamouri@chromium.org,beccahughes@chromium.org
    
    Change-Id: I1b5bbaf0dc435a28905bfdaf15142115e8636d2e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 787464
    Reviewed-on: https://chromium-review.googlesource.com/811945
    Reviewed-by: Sébastien Marchand <sebmarchand@chromium.org>
    Commit-Queue: Sébastien Marchand <sebmarchand@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522184}

--- tools/media_engagement_preload/build.sh
+++
@@ -1,7 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-# Copyright 2017 The Chromium Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-protoc --python_out . \
-  ../../chrome/browser/media/media_engagement_preload.proto \
-  --proto_path ../../chrome/browser/media
