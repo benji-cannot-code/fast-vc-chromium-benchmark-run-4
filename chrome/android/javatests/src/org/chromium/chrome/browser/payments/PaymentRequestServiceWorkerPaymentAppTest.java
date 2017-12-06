239551b2b24e70c99349099eddb0ac6d1519dae2
gogerald <gogerald@google.com>
2017-12-06 20:52:12 +0000

    Reenable testDoNotCallCanMakePayment after fixing crbug.com/789532
    TBR=rouslan@chromium.org
    
    Bug: 787837
    Change-Id: I86e4e239329a93d4e1e2b28ac83d3732c447f47d
    Reviewed-on: https://chromium-review.googlesource.com/811747
    Reviewed-by: Ganggui Tang <gogerald@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Ganggui Tang <gogerald@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522185}

--- chrome/android/javatests/src/org/chromium/chrome/browser/payments/PaymentRequestServiceWorkerPaymentAppTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/payments/PaymentRequestServiceWorkerPaymentAppTest.java
@@ -14,7 +14,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeFeatureList;
@@ -311,7 +310,6 @@ public class PaymentRequestServiceWorkerPaymentAppTest {
 
     @Test
     @MediumTest
-    @DisabledTest(message = "crbug.com/787837")
     @Feature({"Payments"})
     public void testDoNotCallCanMakePayment()
             throws InterruptedException, ExecutionException, TimeoutException {
