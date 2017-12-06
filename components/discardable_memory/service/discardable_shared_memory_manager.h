199f0ce5474f300a376d86d968c10b61395d6dd3
Peng Huang <penghuang@chromium.org>
2017-12-06 03:48:33 +0000

    discardable_memory: fix a use-after-free issue in asan build during shutdown
    And this CL also fixes an issue to make sure ppapi plugin will request the
    discardable memory interface from ui service instead of browser when the
    browser is launched with --mus flag.
    
    Bug: 791119
    Change-Id: I1ce2b256c2280fe02c364aff8d199ab4c43618f0
    Reviewed-on: https://chromium-review.googlesource.com/806482
    Reviewed-by: David Reveman <reveman@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521991}

--- components/discardable_memory/service/discardable_shared_memory_manager.h
+++ components/discardable_memory/service/discardable_shared_memory_manager.h
@@ -23,12 +23,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/memory/shared_memory.h"
 #include "base/memory/weak_ptr.h"
+#include "base/message_loop/message_loop.h"
 #include "base/process/process_handle.h"
 #include "base/synchronization/lock.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/trace_event/memory_dump_provider.h"
 #include "components/discardable_memory/common/discardable_memory_export.h"
 #include "components/discardable_memory/public/interfaces/discardable_shared_memory_manager.mojom.h"
+
+namespace base {
+class WaitableEvent;
+}
 
 namespace service_manager {
 struct BindSourceInfo;
@@ -44,7 +49,8 @@ namespace discardable_memory {
 class DISCARDABLE_MEMORY_EXPORT DiscardableSharedMemoryManager
     : public base::DiscardableMemoryAllocator,
       public base::trace_event::MemoryDumpProvider,
-      public base::MemoryCoordinatorClient {
+      public base::MemoryCoordinatorClient,
+      public base::MessageLoop::DestructionObserver {
  public:
   DiscardableSharedMemoryManager();
   ~DiscardableSharedMemoryManager() override;
@@ -115,6 +121,9 @@ class DISCARDABLE_MEMORY_EXPORT DiscardableSharedMemoryManager
   void OnMemoryStateChange(base::MemoryState state) override;
   void OnPurgeMemory() override;
 
+  // base::MessageLoop::DestructionObserver implementation:
+  void WillDestroyCurrentMessageLoop() override;
+
   void AllocateLockedDiscardableSharedMemory(
       int client_id,
       size_t size,
@@ -131,6 +140,9 @@ class DISCARDABLE_MEMORY_EXPORT DiscardableSharedMemoryManager
   // Virtual for tests.
   virtual base::Time Now() const;
   virtual void ScheduleEnforceMemoryPolicy();
+
+  // Invalidate weak pointers for the mojo thread.
+  void InvalidateMojoThreadWeakPtrs(base::WaitableEvent* event);
 
   int32_t next_client_id_;
 
@@ -151,7 +163,16 @@ class DISCARDABLE_MEMORY_EXPORT DiscardableSharedMemoryManager
       enforce_memory_policy_task_runner_;
   base::Closure enforce_memory_policy_callback_;
   bool enforce_memory_policy_pending_;
+
+  // The message loop for running mojom::DiscardableSharedMemoryManger
+  // implementations.
+  base::MessageLoop* mojo_thread_message_loop_;
+
   base::WeakPtrFactory<DiscardableSharedMemoryManager> weak_ptr_factory_;
+
+  // WeakPtrFractory for generating weak pointers used in the mojo thread.
+  base::WeakPtrFactory<DiscardableSharedMemoryManager>
+      mojo_thread_weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(DiscardableSharedMemoryManager);
 };
