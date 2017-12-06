34403988c23e70b0234f766cd80c93a6981321ef
Ryan Landay <rlanday@chromium.org>
2017-12-06 05:02:30 +0000

    Simplify some code using SelectionTemplate::ComputeRange()
    I added a helper method, SelectionTemplate::ComputeRange() in this CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/806439
    
    This CL migrates some callers over to using this new method.
    
    Change-Id: I44557d6fb5b401d1e3ca066612b11ad57be27d82
    Reviewed-on: https://chromium-review.googlesource.com/808753
    Reviewed-by: yosin (OOO Dec 11 to Jan 8) <yosin@chromium.org>
    Commit-Queue: Ryan Landay <rlanday@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522009}

--- third_party/WebKit/Source/core/editing/iterators/SimplifiedBackwardsTextIteratorTest.cpp
+++ third_party/WebKit/Source/core/editing/iterators/SimplifiedBackwardsTextIteratorTest.cpp
@@ -28,10 +28,8 @@ class SimplifiedBackwardsTextIteratorTest : public EditingTestBase {
     const SelectionInDOMTree selection = SetSelectionTextToBody(selection_text);
     StringBuilder builder;
     bool is_first = true;
-    for (SimplifiedBackwardsTextIterator iterator(
-             EphemeralRange(selection.ComputeStartPosition(),
-                            selection.ComputeEndPosition()),
-             behavior);
+    for (SimplifiedBackwardsTextIterator iterator(selection.ComputeRange(),
+                                                  behavior);
          !iterator.AtEnd(); iterator.Advance()) {
       BackwardsTextBuffer buffer;
       iterator.CopyTextTo(&buffer);
