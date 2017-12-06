79403bd46f2106eaa282a5b613027f2b28dc469d
Luna Lu <loonybear@chromium.org>
2017-12-06 15:37:58 +0000

    Opt in features to UKM UseCounter
    1. Vibrate features:
    Nav / safe browing team  would like to collect URLs where vibrate is used when:
     a. from the top frame or iframe (ads);
     b. vibrate with user gestures.
    2. Touch events:
     understanding reasons where scrolling is prevented without using touch-action.
    
    Features listed above all have shown <1% of page views from chromestatus.com and satisfy UKM privacy constraints.
    
    Bug: 706389, 123004
    Change-Id: I3d2d48e1e7703d2cc4c05c35b7d4ba6d613d1ca7
    Reviewed-on: https://chromium-review.googlesource.com/801754
    Commit-Queue: Luna Lu <loonybear@chromium.org>
    Reviewed-by: Bryan McQuade <bmcquade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522094}

--- chrome/browser/page_load_metrics/observers/use_counter/ukm_features.cc
+++ chrome/browser/page_load_metrics/observers/use_counter/ukm_features.cc
@@ -6,11 +6,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/page_load_metrics/observers/use_counter/ukm_features.h"
 #include "base/containers/flat_set.h"
 
+using WebFeature = blink::mojom::WebFeature;
+
 // UKM-based UseCounter features (WebFeature) should be defined in
 // opt_in_features list. Currently there have been no features opt-in for
 // UKM-based UseCounter, which is why the list is empty.
 bool IsAllowedUkmFeature(blink::mojom::WebFeature feature) {
-  CR_DEFINE_STATIC_LOCAL(const base::flat_set<blink::mojom::WebFeature>,
-                         opt_in_features, {});
+  CR_DEFINE_STATIC_LOCAL(
+      const base::flat_set<WebFeature>, opt_in_features,
+      ({
+          WebFeature::kNavigatorVibrate, WebFeature::kNavigatorVibrateSubFrame,
+          WebFeature::kVibrateWithoutUserGesture,
+          WebFeature::kTouchEventPreventedNoTouchAction,
+          WebFeature::kTouchEventPreventedForcedDocumentPassiveNoTouchAction,
+      }));
   return opt_in_features.count(feature);
 }
