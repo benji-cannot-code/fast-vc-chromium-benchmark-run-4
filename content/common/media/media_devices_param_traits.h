485502e17610e54eb1ce39775a1ad7a401dda2a4
Chandan Padhi <c.padhi@samsung.com>
2017-12-06 02:53:03 +0000

    Define EnumTraits for content::MediaDeviceType
    This CL introduces Mojom enum for content::MediaDeviceType and removes
    its IPC_ENUM_TRAITS in favor of EnumTraits.
    This is in preparation for migration of MediaDevices.enumerateDevices()
    processing from content to Blink.
    
    Bug: 764680
    Change-Id: I5e3c96adb7fd96e1d0ea41dff933ecac017228fa
    Reviewed-on: https://chromium-review.googlesource.com/806054
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Chandan Padhi <c.padhi@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#521971}

--- content/common/media/media_devices_param_traits.h
+++ content/common/media/media_devices_param_traits.h
@@ -12,9 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #undef IPC_MESSAGE_EXPORT
 #define IPC_MESSAGE_EXPORT CONTENT_EXPORT
 
-IPC_ENUM_TRAITS_MAX_VALUE(content::MediaDeviceType,
-                          content::NUM_MEDIA_DEVICE_TYPES - 1)
-
 IPC_STRUCT_TRAITS_BEGIN(content::MediaDeviceInfo)
   IPC_STRUCT_TRAITS_MEMBER(device_id)
   IPC_STRUCT_TRAITS_MEMBER(label)
