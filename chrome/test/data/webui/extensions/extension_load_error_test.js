496c58d2714b9a71f0b6da731649cd6f7beb46c1
Dave Schuyler <dschuyler@chromium.org>
2017-12-06 00:41:21 +0000

    [MD extensions] browser test retrying extensions load
    This CL adds a test for retrying the load of an extension that had errors.
    Both a retry that fails and a retry that succeeds are tested.
    This is a follow-up for CL 798123.
    
    Bug: 789751
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I161f35127d5c37769d6c5e577c2e7d6689dbb482
    Reviewed-on: https://chromium-review.googlesource.com/807448
    Commit-Queue: Dave Schuyler <dschuyler@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521920}

--- chrome/test/data/webui/extensions/extension_load_error_test.js
+++ chrome/test/data/webui/extensions/extension_load_error_test.js
@@ -7,7 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 cr.define('extension_load_error_tests', function() {
   /** @enum {string} */
   var TestNames = {
-    Interaction: 'Interaction',
+    RetryError: 'RetryError',
+    RetrySuccess: 'RetrySuccess',
     CodeSection: 'Code Section',
   };
 
@@ -37,25 +38,32 @@ cr.define('extension_load_error_tests', function() {
       document.body.appendChild(loadError);
     });
 
-    test(assert(TestNames.Interaction), function() {
+    test(assert(TestNames.RetryError), function() {
       var dialogElement = loadError.$$('dialog');
-      var isDialogVisible = function() {
-        var rect = dialogElement.getBoundingClientRect();
-        return rect.width * rect.height > 0;
-      };
-
-      expectFalse(isDialogVisible());
+      expectFalse(extension_test_util.isElementVisible(dialogElement));
       loadError.show();
-      expectTrue(isDialogVisible());
+      expectTrue(extension_test_util.isElementVisible(dialogElement));
+
+      mockDelegate.setRetryLoadUnpackedError(stubLoadError);
+      MockInteractions.tap(loadError.$$('.action-button'));
+      return mockDelegate.whenCalled('retryLoadUnpacked').then(arg => {
+        expectEquals(fakeGuid, arg);
+        expectTrue(extension_test_util.isElementVisible(dialogElement));
+        MockInteractions.tap(loadError.$$('.cancel-button'));
+        expectFalse(extension_test_util.isElementVisible(dialogElement));
+      });
+    });
+
+    test(assert(TestNames.RetrySuccess), function() {
+      var dialogElement = loadError.$$('dialog');
+      expectFalse(extension_test_util.isElementVisible(dialogElement));
+      loadError.show();
+      expectTrue(extension_test_util.isElementVisible(dialogElement));
 
       MockInteractions.tap(loadError.$$('.action-button'));
       return mockDelegate.whenCalled('retryLoadUnpacked').then(arg => {
         expectEquals(fakeGuid, arg);
-        expectFalse(isDialogVisible());
-
-        loadError.show();
-        MockInteractions.tap(loadError.$$('.cancel-button'));
-        expectFalse(isDialogVisible());
+        expectFalse(extension_test_util.isElementVisible(dialogElement));
       });
     });
 
