b1e390af684bef887fb0696d4a9e5344545fe468
Nate Chapin <japhet@chromium.org>
2017-12-06 00:20:02 +0000

    Fix infinite recursion in css preload scanning
    A self-referencing css stylesheet can attempt to preload itself reentrantly.
    Ensure a HTMLResourcePreloader doesn't scan the same Resource twice.
    
    Bug: 789198, 790940, 790945
    Change-Id: I5a5ca56e3c12978c4a8b7fcbba79ae2a772671f8
    Reviewed-on: https://chromium-review.googlesource.com/804496
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521911}

--- third_party/WebKit/Source/core/html/parser/HTMLDocumentParser.cpp
+++ third_party/WebKit/Source/core/html/parser/HTMLDocumentParser.cpp
@@ -933,6 +933,9 @@ void HTMLDocumentParser::end() {
   // deletes this).
   tree_builder_->Finished();
 
+  // All preloads should be done.
+  preloader_.Clear();
+
   DocumentParser::StopParsing();
 }
 
