e41e99e351a0393cf1160788473b535bbc61c8c7
Vaclav Brozek <vabr@chromium.org>
2017-12-06 11:24:30 +0000

    [Android password settings] Display warning before export
    This CL adds a warning to be displayed before the user can export
    their passwords.
    
    Mocks:
    https://docs.google.com/presentation/d/1nIm5OmaOnb85ZAwMZPSVqHT0vkFbsRYZhOzbmznWU_c/edit#slide=id.g289b1efcd8_0_21
    
    Note: the mock shows slightly different styling, but with the UX
    designer we settled on using a standard dialog style already existing
    in Chromium and checking separately whether that style needs fixing,
    rather than introducing inconsistent styling among Chrome's dialogs.
    
    Design: go chrome-pwd-export
    
    Screenshot: https://crbug.com/788701#c7
    
    Bug: 788701
    Change-Id: I3a2dbdc24429c5662fdc2b178630a8e45ba603a1
    Reviewed-on: https://chromium-review.googlesource.com/803955
    Commit-Queue: Vaclav Brozek <vabr@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522062}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/password/SavePasswordsPreferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/password/SavePasswordsPreferences.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.preferences.password;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -13,6 +14,7 @@ import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceScreen;
+import android.support.v7.app.AlertDialog;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
 import android.view.Menu;
@@ -128,7 +130,21 @@ public class SavePasswordsPreferences
 
     /** Continues with the password export flow after the user successfully reauthenticated. */
     private void exportAfterReauth() {
-        // TODO(crbug.com/788701): Show the warning, start the export.
+        ExportWarningDialogFragment exportWarningDialogFragment = new ExportWarningDialogFragment();
+        exportWarningDialogFragment.setExportWarningHandler(new DialogInterface.OnClickListener() {
+            /** On positive button response asks the parent to continue with the export flow. */
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (which == AlertDialog.BUTTON_POSITIVE) {
+                    exportAfterWarning();
+                }
+            }
+        });
+        exportWarningDialogFragment.show(getFragmentManager(), null);
+    }
+
+    private void exportAfterWarning() {
+        // TODO(crbug.com/788701): Start the export.
     }
 
     /**
