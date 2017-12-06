95ac7b5c450601d7fa3e4a8356338637870f0dde
Erik Chen <erikchen@chromium.org>
2017-12-06 19:06:25 +0000

    OOP HP: Add a flag to keep small allocations.
    By default, small allocations are pruned from the heap dump. This reduces the
    size of the compressed trace by 100x. If pruning is disabled, the
    chrome://tracing UI may be unable to take or load the trace. The trace can
    still be saved directly using chrome://memory-internals.
    
    Other mechanisms [e.g. diff_heap_profiler.py] may be necessary to examine the
    trace. Note that automatically uploaded traces will always be pruned. This only
    affects manually taken memory-infra traces.;
    
    Bug: 
    Change-Id: I9aacc1d4e9ea65185b87f0a6ad53d727083576e8
    Reviewed-on: https://chromium-review.googlesource.com/808995
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Brett Wilson <brettw@chromium.org>
    Reviewed-by: Albert J. Wong <ajwong@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522151}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -321,6 +321,8 @@ extern const char kEnableOutOfProcessHeapProfilingModeAll[];
 extern const char kEnableOutOfProcessHeapProfilingModeBrowser[];
 extern const char kEnableOutOfProcessHeapProfilingModeGpu[];
 extern const char kEnableOutOfProcessHeapProfilingModeRendererSampling[];
+extern const char kOutOfProcessHeapProfilingKeepSmallAllocations[];
+extern const char kOutOfProcessHeapProfilingKeepSmallAllocationsDescription[];
 
 extern const char kEnablePictureInPictureName[];
 extern const char kEnablePictureInPictureDescription[];
