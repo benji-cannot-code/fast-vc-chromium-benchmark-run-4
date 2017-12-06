64672519b7beb29eb3f593b6c0b2586160d1cf56
Jay Civelli <jcivelli@chromium.org>
2017-12-06 20:39:33 +0000

    Moving PdfRenderSettings to its own mojom.
    Moving PdfRenderSettings from pdf_to_pwg_raster_converter.mojom
    to its own mojom/typemap/traits files as it will soon be used by other
    interfaces.
    
    Bug: 766451
    Change-Id: Ib50ceeefa62a56c91048d6a346323b4cad601359
    Reviewed-on: https://chromium-review.googlesource.com/811448
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Jay Civelli <jcivelli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522181}

--- chrome/common/printing/pdf_to_pwg_raster_converter_struct_traits.cc
+++ chrome/common/printing/pdf_to_pwg_raster_converter_struct_traits.cc
@@ -5,22 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/common/printing/pdf_to_pwg_raster_converter_struct_traits.h"
 
-#include "ui/gfx/geometry/mojo/geometry_struct_traits.h"
-
 namespace mojo {
 
 // static
-bool StructTraits<printing::mojom::PDFRenderSettingsDataView,
-                  printing::PdfRenderSettings>::
-    Read(printing::mojom::PDFRenderSettingsDataView data,
-         printing::PdfRenderSettings* out) {
-  out->dpi = data.dpi();
-  out->autorotate = data.autorotate();
-
-  return data.ReadArea(&out->area) && data.ReadOffsets(&out->offsets) &&
-         data.ReadMode(&out->mode);
-}
-
 bool StructTraits<printing::mojom::PWGRasterSettingsDataView,
                   printing::PwgRasterSettings>::
     Read(printing::mojom::PWGRasterSettingsDataView data,
