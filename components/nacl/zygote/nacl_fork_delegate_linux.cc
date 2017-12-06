bdf641d9e619beff9a13ee9172fbca73d1bd3b62
Mostyn Bramley-Moore <mostynb@vewd.com>
2017-12-06 09:28:38 +0000

    remove valgrind checks from //components/nacl/zygote
    We have not supported valgrind for some time (it was never ported to
    the GN build IIUC, and has been obsoleted by ASan/MSan/TSan).
    
    BUG=791518
    
    Change-Id: I9d6f5125de817b9334b295a8a78589004bb08294
    Reviewed-on: https://chromium-review.googlesource.com/809132
    Commit-Queue: Mostyn Bramley-Moore <mostynb@vewd.com>
    Reviewed-by: Mark Seaborn <mseaborn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522051}

--- components/nacl/zygote/nacl_fork_delegate_linux.cc
+++ components/nacl/zygote/nacl_fork_delegate_linux.cc
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/process/kill.h"
 #include "base/process/launch.h"
 #include "base/strings/string_split.h"
-#include "base/third_party/dynamic_annotations/dynamic_annotations.h"
 #include "build/build_config.h"
 #include "components/nacl/common/nacl_nonsfi_util.h"
 #include "components/nacl/common/nacl_paths.h"
@@ -211,8 +210,6 @@ void NaClForkDelegate::Init(const int sandboxdesc,
              !PathService::Get(nacl::FILE_NACL_HELPER_BOOTSTRAP,
                                &helper_bootstrap_exe)) {
     status_ = kNaClHelperBootstrapMissing;
-  } else if (RunningOnValgrind()) {
-    status_ = kNaClHelperValgrind;
   } else {
     base::CommandLine::StringVector argv_to_launch;
     {
