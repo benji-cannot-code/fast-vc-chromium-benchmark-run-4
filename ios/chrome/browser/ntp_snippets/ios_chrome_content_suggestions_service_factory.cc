21c7f89f4a37dfdbaa65eefc90ffe93cf79316eb
Sylvain Defresne <sdefresne@chromium.org>
2017-12-07 15:23:38 +0000

    Fix comments of KeyedService factories.
    The GetDefaultFactory return a factory that can create instance as
    the default factory does. It can be used by factories when default
    is to not create the instance for test BrowserStates.
    
    Bug: none
    Change-Id: I02192ff82e16748eb8f7b69a4c482b3589527b5b
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Reviewed-on: https://chromium-review.googlesource.com/811085
    Reviewed-by: Elodie Banel <lod@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522430}

--- ios/chrome/browser/ntp_snippets/ios_chrome_content_suggestions_service_factory.cc
+++ ios/chrome/browser/ntp_snippets/ios_chrome_content_suggestions_service_factory.cc
@@ -22,18 +22,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using ntp_snippets::ContentSuggestionsService;
 
 // static
-IOSChromeContentSuggestionsServiceFactory*
-IOSChromeContentSuggestionsServiceFactory::GetInstance() {
-  return base::Singleton<IOSChromeContentSuggestionsServiceFactory>::get();
-}
-
-// static
 ContentSuggestionsService*
 IOSChromeContentSuggestionsServiceFactory::GetForBrowserState(
     ios::ChromeBrowserState* browser_state) {
   DCHECK(!browser_state->IsOffTheRecord());
   return static_cast<ContentSuggestionsService*>(
       GetInstance()->GetServiceForBrowserState(browser_state, true));
+}
+
+// static
+IOSChromeContentSuggestionsServiceFactory*
+IOSChromeContentSuggestionsServiceFactory::GetInstance() {
+  return base::Singleton<IOSChromeContentSuggestionsServiceFactory>::get();
 }
 
 // static
