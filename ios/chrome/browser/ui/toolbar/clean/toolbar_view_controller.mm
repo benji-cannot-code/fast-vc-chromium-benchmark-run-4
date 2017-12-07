29a0ad49e7aa4b9849958a25c097e7599f270f5a
Gauthier Ambard <gambard@chromium.org>
2017-12-07 13:15:15 +0000

    Fix progress bar issue when leaving NTP
    When leaving the NTP, the progress bar of the toolbar was progressing
    backward before starting the loading.
    This CL fixes it.
    
    Bug: 792791
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I49cbe4073b3d33d1e2fe45e44c588403c44dea7a
    Reviewed-on: https://chromium-review.googlesource.com/813734
    Reviewed-by: Elodie Banel <lod@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522410}

--- ios/chrome/browser/ui/toolbar/clean/toolbar_view_controller.mm
+++ ios/chrome/browser/ui/toolbar/clean/toolbar_view_controller.mm
@@ -572,8 +572,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   if (!loading) {
     [self stopProgressBar];
   } else {
-    [self.progressBar setProgress:0];
     [self.progressBar setHidden:NO animated:YES completion:nil];
+    [self.progressBar setProgress:0];
   }
 }
 
