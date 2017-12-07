220b6f318c2c0262489fe10c011f6e4a14659ae4
Xi Han <hanxi@google.com>
2017-12-07 18:37:17 +0000

    [WebAPKs] Fix: notification breaks on pre-O.
    This is caused by a missing check of whether the android os version of the
    device is o+.
    
    Bug: 700228
    Change-Id: Id944f681465f4bd5b2b0ec869f13506196b1140a
    Reviewed-on: https://chromium-review.googlesource.com/814138
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522485}

--- chrome/android/webapk/libs/runtime_library/src/org/chromium/webapk/lib/runtime_library/WebApkServiceImpl.java
+++ chrome/android/webapk/libs/runtime_library/src/org/chromium/webapk/lib/runtime_library/WebApkServiceImpl.java
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.webapk.lib.runtime_library;
 
-import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
@@ -85,16 +84,17 @@ public class WebApkServiceImpl extends IWebApkApi.Stub {
         return NotificationManagerCompat.from(mContext).areNotificationsEnabled();
     }
 
-    @TargetApi(Build.VERSION_CODES.O)
+    @SuppressWarnings("NewApi")
     @Override
     public void notifyNotificationWithChannel(
             String platformTag, int platformID, Notification notification, String channelName) {
         NotificationManager notificationManager = getNotificationManager();
-        if (notification.getChannelId() != null) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && notification.getChannelId() != null) {
             NotificationChannel channel = new NotificationChannel(notification.getChannelId(),
                     channelName, NotificationManager.IMPORTANCE_DEFAULT);
             notificationManager.createNotificationChannel(channel);
         }
+
         notificationManager.notify(platformTag, platformID, notification);
     }
 
