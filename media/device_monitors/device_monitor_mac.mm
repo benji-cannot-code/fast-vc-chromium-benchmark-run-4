9a85ac3ff6ec4cb1c6c768b2ada9a0233b25ef1d
Guido Urdaneta <guidou@chromium.org>
2017-12-07 20:14:10 +0000

    Improve audio-device monitoring on Mac
    Before this CL, base::SystemMonitor on Mac only reported addition and
    removal of input devices.
    
    With this patch, the following events are reported:
    * Addition and removal of output-only devices.
    * Changes to the default input or output device.
    
    With this change, it is safe to enable the SYSTEM_MONITOR caching
    policy for MediaDevicesManager.
    
    Bug: 788687, 648173
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I57549eb586716a3739d652c386f7740875521be8
    Reviewed-on: https://chromium-review.googlesource.com/803218
    Reviewed-by: Max Morin <maxmorin@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522524}

--- media/device_monitors/device_monitor_mac.mm
+++ media/device_monitors/device_monitor_mac.mm
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/task_runner_util.h"
 #include "base/threading/thread_checker.h"
+#include "media/audio/mac/audio_device_listener_mac.h"
 
 namespace {
 
@@ -83,9 +84,7 @@ class DeviceMonitorMacImpl {
 void DeviceMonitorMacImpl::ConsolidateDevicesListAndNotify(
     const std::vector<DeviceInfo>& snapshot_devices) {
   bool video_device_added = false;
-  bool audio_device_added = false;
   bool video_device_removed = false;
-  bool audio_device_removed = false;
 
   // Compare the current system devices snapshot with the ones cached to detect
   // additions, present in the former but not in the latter. If we find a device
@@ -98,9 +97,7 @@ void DeviceMonitorMacImpl::ConsolidateDevicesListAndNotify(
     if (cached_devices_iterator == cached_devices_.end()) {
       video_device_added |= ((it->type() == DeviceInfo::kVideo) ||
                              (it->type() == DeviceInfo::kMuxed));
-      audio_device_added |= ((it->type() == DeviceInfo::kAudio) ||
-                             (it->type() == DeviceInfo::kMuxed));
-      DVLOG(1) << "Device has been added, id: " << it->unique_id();
+      DVLOG(1) << "Video device has been added, id: " << it->unique_id();
     } else {
       cached_devices_.erase(cached_devices_iterator);
     }
@@ -110,18 +107,13 @@ void DeviceMonitorMacImpl::ConsolidateDevicesListAndNotify(
     video_device_removed |= ((it->type() == DeviceInfo::kVideo) ||
                              (it->type() == DeviceInfo::kMuxed) ||
                              (it->type() == DeviceInfo::kInvalid));
-    audio_device_removed |= ((it->type() == DeviceInfo::kAudio) ||
-                             (it->type() == DeviceInfo::kMuxed) ||
-                             (it->type() == DeviceInfo::kInvalid));
-    DVLOG(1) << "Device has been removed, id: " << it->unique_id();
+    DVLOG(1) << "Video device has been removed, id: " << it->unique_id();
   }
   // Update the cached devices with the current system snapshot.
   cached_devices_ = snapshot_devices;
 
   if (video_device_added || video_device_removed)
     monitor_->NotifyDeviceChanged(base::SystemMonitor::DEVTYPE_VIDEO_CAPTURE);
-  if (audio_device_added || audio_device_removed)
-    monitor_->NotifyDeviceChanged(base::SystemMonitor::DEVTYPE_AUDIO);
 }
 
 // Forward declaration for use by CrAVFoundationDeviceObserver.
@@ -451,8 +443,16 @@ DeviceMonitorMac::~DeviceMonitorMac() {}
 void DeviceMonitorMac::StartMonitoring() {
   DCHECK(thread_checker_.CalledOnValidThread());
   DVLOG(1) << "Monitoring via AVFoundation";
-  device_monitor_impl_.reset(
-      new AVFoundationMonitorImpl(this, device_task_runner_));
+  device_monitor_impl_ =
+      std::make_unique<AVFoundationMonitorImpl>(this, device_task_runner_);
+  audio_device_listener_ = std::make_unique<AudioDeviceListenerMac>(
+      base::BindRepeating([] {
+        if (base::SystemMonitor::Get()) {
+          base::SystemMonitor::Get()->ProcessDevicesChanged(
+              base::SystemMonitor::DEVTYPE_AUDIO);
+        }
+      }),
+      true /* monitor_default_input */, true /* monitor_addition_removal */);
 }
 
 void DeviceMonitorMac::NotifyDeviceChanged(
