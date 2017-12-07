f295ccbd843a5f935585d3990b9f874a0f4829c1
Andrey Lushnikov <lushnikov@chromium.org>
2017-12-07 21:28:46 +0000

    DevTools: migrate extensions tests
    This patch migrates extensions tests onto a new framework.
    As a drive-by, it rebaselines a few tests that used to be
    failing.
    
    BUG=733448, 626703
    R=dgozman
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: If17deb076bdb73d101a536bf5fa430a7355535ff
    Reviewed-on: https://chromium-review.googlesource.com/798531
    Commit-Queue: Andrey Lushnikov <lushnikov@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522552}

---
+++ third_party/WebKit/LayoutTests/http/tests/devtools/extensions/extensions-iframe-eval.js
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(async function() {
+  TestRunner.addResult(`Tests WebInspector extension API\n`);
+  await TestRunner.loadModule('extensions_test_runner');
+  await TestRunner.loadHTML(`<iframe src="${TestRunner.url('../resources/extensions-frame-eval.html')}"></iframe>`);
+  await ExtensionsTestRunner.runExtensionTests([
+    function extension_testEvalInIFrame(nextTest) {
+      var url = 'http://127.0.0.1:8000/devtools/resources/extensions-frame-eval.html';
+      var origin = "http://127.0.0.1:8000"
+      var options = {
+        frameURL: url
+      };
+      var loc = "window.location.pathname";
+      webInspector.inspectedWindow.eval(loc, options, callbackAndNextTest(extension_onEval, nextTest));
+    },
+
+    function extension_testEvalInIFrameBadOption(nextTest) {
+      var url = 'http://127.0.0.1:8000/devtools/resources/extensions-frame-eval.html';
+      var origin = "http://127.0.0.1:8000"
+      var options = {
+        frameURL: url,
+        scriptExecutionContext: "bogus"
+      };
+      var loc = "window.location.pathname";
+      webInspector.inspectedWindow.eval(loc, options, callbackAndNextTest(extension_onEval, nextTest));
+    },
+
+    function extension_onEval(value, isException) {
+      output("Evaluate: " + JSON.stringify(value) + " (exception: " + isException + ")");
+    }
+  ]);
+})();
