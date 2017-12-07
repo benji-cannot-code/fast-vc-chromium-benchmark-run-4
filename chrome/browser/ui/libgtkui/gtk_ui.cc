ede827a6d575fc6b1334044d2b71f6119bda12f4
Tim Brown <timbrown@chromium.org>
2017-12-07 00:17:16 +0000

    Use GSettings to retrieve button layout
    This will be the replacement backup way of getting the setting, rather
    than using the deprecated gconf. I will remove the gconf code in a
    separate CL.
    
    gsettings app to change values and test behavior in chromium.
    
    Test: Force use of gsettings for config (see patchset 3) and used
    Bug: 768027
    Change-Id: I957edc5f03c20f42e74d0ba9d1e57a352cc2d418
    Reviewed-on: https://chromium-review.googlesource.com/809730
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Elliot Glaysher <erg@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522266}

--- chrome/browser/ui/libgtkui/gtk_ui.cc
+++ chrome/browser/ui/libgtkui/gtk_ui.cc
@@ -74,6 +74,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/libgtkui/nav_button_provider_gtk3.h"  // nogncheck
 #endif
 
+#if defined(USE_GIO)
+#include "chrome/browser/ui/libgtkui/nav_button_layout_manager_gsettings.h"
+#endif
+
 #if BUILDFLAG(ENABLE_BASIC_PRINTING)
 #include "printing/printing_context_linux.h"
 #endif
@@ -282,10 +286,13 @@ std::unique_ptr<NavButtonLayoutManager> CreateNavButtonLayoutManager(
   if (GtkVersionCheck(3, 14))
     return std::make_unique<NavButtonLayoutManagerGtk3>(gtk_ui);
 #endif
-#if defined(USE_GCONF)
+#if defined(USE_GIO)
+  return std::make_unique<NavButtonLayoutManagerGSettings>(gtk_ui);
+#elif defined(USE_GCONF)
   return std::make_unique<NavButtonLayoutManagerGconf>(gtk_ui);
-#endif
+#else
   return nullptr;
+#endif
 }
 
 // Returns a gfx::FontRenderParams corresponding to GTK's configuration.
