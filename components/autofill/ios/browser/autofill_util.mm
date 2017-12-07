40f274e93d1d2615f845bddddbe2baa7a64d84a6
John Z Wu <jzw@chromium.org>
2017-12-07 19:46:50 +0000

    Remove AutofillClientIOS and refactor common code to util class.
    WebViewAutofillClientIOS and ChromeAutofillClientIOS should directly
    inherit from AutofillClient and share code through helpers. This is the
    preferred way to share code between //ios/web_view and //ios/chrome.
    
    Bug: 774517
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I52b55a03b3bb2038cb9af276a7ee7a208a469dcd
    Reviewed-on: https://chromium-review.googlesource.com/804474
    Commit-Queue: John Wu <jzw@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522511}

---
+++ components/autofill/ios/browser/autofill_util.mm
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/autofill/ios/browser/autofill_util.h"
+
+#import "ios/web/public/navigation_item.h"
+#import "ios/web/public/navigation_manager.h"
+#include "ios/web/public/ssl_status.h"
+
+namespace autofill {
+
+bool IsContextSecureForWebState(web::WebState* web_state) {
+  // This implementation differs slightly from other platforms. Other platforms'
+  // implementations check for the presence of active mixed content, but because
+  // the iOS web view blocks active mixed content without an option to run it,
+  // there is no need to check for active mixed content here.
+  web::NavigationManager* manager = web_state->GetNavigationManager();
+  web::NavigationItem* nav_item = manager->GetLastCommittedItem();
+  if (!nav_item)
+    return false;
+
+  const web::SSLStatus& ssl = nav_item->GetSSL();
+  return nav_item->GetURL().SchemeIsCryptographic() && ssl.certificate &&
+         (!net::IsCertStatusError(ssl.cert_status) ||
+          net::IsCertStatusMinorError(ssl.cert_status));
+}
+
+}  // namespace autofill
--- ios/chrome/browser/ui/autofill/chrome_autofill_client_ios.mm
+++ ios/chrome/browser/ui/autofill/chrome_autofill_client_ios.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/feature_list.h"
+#include "base/logging.h"
 #include "base/memory/ptr_util.h"
 #include "components/autofill/core/browser/autofill_credit_card_filling_infobar_delegate_mobile.h"
 #include "components/autofill/core/browser/autofill_save_card_infobar_delegate_mobile.h"
@@ -17,22 +18,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/webdata/autofill_webdata_service.h"
 #include "components/autofill/core/common/autofill_features.h"
 #include "components/autofill/core/common/autofill_pref_names.h"
+#include "components/autofill/ios/browser/autofill_util.h"
 #include "components/infobars/core/infobar.h"
-#include "components/infobars/core/infobar_manager.h"
 #include "components/keyed_service/core/service_access_type.h"
-#include "components/password_manager/core/browser/password_generation_manager.h"
-#include "components/prefs/pref_service.h"
-#include "google_apis/gaia/identity_provider.h"
 #include "ios/chrome/browser/application_context.h"
 #include "ios/chrome/browser/autofill/address_normalizer_factory.h"
 #include "ios/chrome/browser/autofill/personal_data_manager_factory.h"
-#include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #include "ios/chrome/browser/infobars/infobar_utils.h"
 #import "ios/chrome/browser/ssl/insecure_input_tab_helper.h"
 #include "ios/chrome/browser/ui/autofill/card_unmask_prompt_view_bridge.h"
 #include "ios/chrome/browser/web_data_service_factory.h"
 #include "ios/public/provider/chrome/browser/chrome_browser_provider.h"
-#import "ios/web/public/web_state/web_state.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -47,13 +43,13 @@ ChromeAutofillClientIOS::ChromeAutofillClientIOS(
     id<AutofillClientIOSBridge> bridge,
     password_manager::PasswordGenerationManager* password_generation_manager,
     std::unique_ptr<IdentityProvider> identity_provider)
-    : AutofillClientIOS(
-          browser_state->GetPrefs(),
-          PersonalDataManagerFactory::GetForBrowserState(
-              browser_state->GetOriginalChromeBrowserState()),
-          web_state,
-          bridge,
-          std::move(identity_provider),
+    : pref_service_(browser_state->GetPrefs()),
+      personal_data_manager_(PersonalDataManagerFactory::GetForBrowserState(
+          browser_state->GetOriginalChromeBrowserState())),
+      web_state_(web_state),
+      bridge_(bridge),
+      identity_provider_(std::move(identity_provider)),
+      autofill_web_data_service_(
           ios::WebDataServiceFactory::GetAutofillWebDataForBrowserState(
               browser_state,
               ServiceAccessType::EXPLICIT_ACCESS)),
@@ -62,7 +58,26 @@ ChromeAutofillClientIOS::ChromeAutofillClientIOS(
       unmask_controller_(browser_state->GetPrefs(),
                          browser_state->IsOffTheRecord()) {}
 
-ChromeAutofillClientIOS::~ChromeAutofillClientIOS() {}
+ChromeAutofillClientIOS::~ChromeAutofillClientIOS() {
+  HideAutofillPopup();
+}
+
+PersonalDataManager* ChromeAutofillClientIOS::GetPersonalDataManager() {
+  return personal_data_manager_;
+}
+
+PrefService* ChromeAutofillClientIOS::GetPrefs() {
+  return pref_service_;
+}
+
+// TODO(crbug.com/535784): Implement this when adding credit card upload.
+syncer::SyncService* ChromeAutofillClientIOS::GetSyncService() {
+  return nullptr;
+}
+
+IdentityProvider* ChromeAutofillClientIOS::GetIdentityProvider() {
+  return identity_provider_.get();
+}
 
 ukm::UkmRecorder* ChromeAutofillClientIOS::GetUkmRecorder() {
   return GetApplicationContext()->GetUkmRecorder();
@@ -72,6 +87,15 @@ AddressNormalizer* ChromeAutofillClientIOS::GetAddressNormalizer() {
   if (base::FeatureList::IsEnabled(features::kAutofillAddressNormalizer))
     return AddressNormalizerFactory::GetInstance();
   return nullptr;
+}
+
+SaveCardBubbleController*
+ChromeAutofillClientIOS::GetSaveCardBubbleController() {
+  return nullptr;
+}
+
+void ChromeAutofillClientIOS::ShowAutofillSettings() {
+  NOTREACHED();
 }
 
 void ChromeAutofillClientIOS::ShowUnmaskPrompt(
@@ -132,6 +156,38 @@ void ChromeAutofillClientIOS::LoadRiskData(
   callback.Run(ios::GetChromeBrowserProvider()->GetRiskData());
 }
 
+bool ChromeAutofillClientIOS::HasCreditCardScanFeature() {
+  return false;
+}
+
+void ChromeAutofillClientIOS::ScanCreditCard(
+    const CreditCardScanCallback& callback) {
+  NOTREACHED();
+}
+
+void ChromeAutofillClientIOS::ShowAutofillPopup(
+    const gfx::RectF& element_bounds,
+    base::i18n::TextDirection text_direction,
+    const std::vector<Suggestion>& suggestions,
+    base::WeakPtr<AutofillPopupDelegate> delegate) {
+  [bridge_ showAutofillPopup:suggestions popupDelegate:delegate];
+}
+
+void ChromeAutofillClientIOS::HideAutofillPopup() {
+  [bridge_ hideAutofillPopup];
+}
+
+bool ChromeAutofillClientIOS::IsAutocompleteEnabled() {
+  // For browser, Autocomplete is always enabled as part of Autofill.
+  return GetPrefs()->GetBoolean(prefs::kAutofillEnabled);
+}
+
+void ChromeAutofillClientIOS::UpdateAutofillPopupDataListValues(
+    const std::vector<base::string16>& values,
+    const std::vector<base::string16>& labels) {
+  NOTREACHED();
+}
+
 void ChromeAutofillClientIOS::PropagateAutofillPredictions(
     content::RenderFrameHost* rfh,
     const std::vector<FormStructure*>& forms) {
@@ -140,9 +196,33 @@ void ChromeAutofillClientIOS::PropagateAutofillPredictions(
   }
 }
 
+void ChromeAutofillClientIOS::DidFillOrPreviewField(
+    const base::string16& autofilled_value,
+    const base::string16& profile_full_name) {}
+
+scoped_refptr<AutofillWebDataService> ChromeAutofillClientIOS::GetDatabase() {
+  return autofill_web_data_service_;
+}
+
 void ChromeAutofillClientIOS::DidInteractWithNonsecureCreditCardInput() {
-  InsecureInputTabHelper::GetOrCreateForWebState(web_state())
+  InsecureInputTabHelper::GetOrCreateForWebState(web_state_)
       ->DidInteractWithNonsecureCreditCardInput();
+}
+
+bool ChromeAutofillClientIOS::IsContextSecure() {
+  return IsContextSecureForWebState(web_state_);
+}
+
+bool ChromeAutofillClientIOS::ShouldShowSigninPromo() {
+  return false;
+}
+
+void ChromeAutofillClientIOS::ExecuteCommand(int id) {
+  NOTIMPLEMENTED();
+}
+
+bool ChromeAutofillClientIOS::IsAutofillSupported() {
+  return true;
 }
 
 }  // namespace autofill
