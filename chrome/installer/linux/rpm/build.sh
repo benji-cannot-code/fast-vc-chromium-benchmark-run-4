b63b70a0b744efdea0a8beab62ca03d47be3679d
Tom Anderson <thomasanderson@chromium.org>
2017-12-07 02:18:42 +0000

    [Linux packager] Add check that RPATH is unset in binaries
    BUG=584597
    R=thestig@chromium.org
    
    Change-Id: Ie131b7eb5f994fef3a0808992ec02fa8aa6beed7
    Reviewed-on: https://chromium-review.googlesource.com/803738
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522310}

--- chrome/installer/linux/rpm/build.sh
+++ chrome/installer/linux/rpm/build.sh
@@ -127,7 +127,7 @@ cleanup() {
 
 usage() {
   echo "usage: $(basename $0) [-a target_arch] [-b 'dir'] -c channel"
-  echo "                      -d branding [-f] [-o 'dir']"
+  echo "                      -d branding [-f] [-o 'dir'] -t target_os"
   echo "-a arch     package architecture (ia32 or x64)"
   echo "-b dir      build input directory    [${BUILDDIR}]"
   echo "-c channel  the package channel (unstable, beta, stable)"
@@ -135,6 +135,7 @@ usage() {
   echo "-f          indicates that this is an official build"
   echo "-h          this help message"
   echo "-o dir      package output directory [${OUTPUTDIR}]"
+  echo "-t platform target platform"
 }
 
 # Check that the channel name is one of the allowable ones.
@@ -159,7 +160,7 @@ verify_channel() {
 }
 
 process_opts() {
-  while getopts ":a:b:c:d:fho:" OPTNAME
+  while getopts ":a:b:c:d:fho:t:" OPTNAME
   do
     case $OPTNAME in
       a )
@@ -186,7 +187,10 @@ process_opts() {
         OUTPUTDIR=$(readlink -f "${OPTARG}")
         mkdir -p "${OUTPUTDIR}"
         ;;
-      \: )
+      t )
+        TARGET_OS="$OPTARG"
+        ;;
+     \: )
         echo "'-$OPTARG' needs an argument."
         usage
         exit 1
