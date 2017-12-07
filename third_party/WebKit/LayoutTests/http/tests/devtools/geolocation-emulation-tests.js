5e4932d672c5d3c50b9df885e0f746efde9c0ea8
Matt Reynolds <mattreynolds@google.com>
2017-12-07 21:59:51 +0000

    Reject stale geolocation estimates
    The Geolocation API allows callers to specify a maximum age when
    requesting the current location. If the most recent location fix is
    older, the API specifies that a new estimate should be returned.
    
    With this CL, if we receive a location estimate that is too old to
    satisfy the maximum age option, we will now wait for the provider to
    provide a new estimate.
    
    BUG=769231
    
    Change-Id: I2bea9a6df5327bc21e81e1154c6dfa25ef5c40c9
    Reviewed-on: https://chromium-review.googlesource.com/703976
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522568}

--- third_party/WebKit/LayoutTests/http/tests/devtools/geolocation-emulation-tests.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/geolocation-emulation-tests.js
@@ -49,7 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
               console.log(serializeGeolocationError(error));
           }
 
-          navigator.geolocation.getCurrentPosition(testSuccess, testFailed);
+          navigator.geolocation.getCurrentPosition(testSuccess, testFailed, { maximumAge: Infinity, timeout: 1000 });
       }
 
       function overridenTimestampGeolocation()
@@ -67,7 +67,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
               console.log(serializeGeolocationError(error));
           }
 
-          navigator.geolocation.getCurrentPosition(testSuccess, testFailed);
+          navigator.geolocation.getCurrentPosition(testSuccess, testFailed, { maximumAge: Infinity, timeout: 1000 });
       }
   `);
 
