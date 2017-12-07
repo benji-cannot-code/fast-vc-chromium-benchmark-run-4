fc87b5fecffa07449c89be7b5d1b6d4aa912e35f
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2017-12-07 02:14:08 +0000

    desktop-pwas: Record delta between opening an app window and last navigation
    If the time difference is small, then we probably opened an app window
    in response to a client side redirect and left the tab opened. This
    will let us know how common this problem is.
    
    Bug: 785661
    Change-Id: Icc5a015cd5c0ae9fda5ad457ad564ead4b812332
    Reviewed-on: https://chromium-review.googlesource.com/798951
    Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Matt Giuca <mgiuca@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522307}

--- chrome/browser/extensions/bookmark_app_navigation_throttle.cc
+++ chrome/browser/extensions/bookmark_app_navigation_throttle.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/extensions/api/url_handlers/url_handlers_parser.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents.h"
@@ -348,6 +349,19 @@ BookmarkAppNavigationThrottle::ProcessNavigation(bool is_redirect) {
       RecordProcessNavigationResult(
           ProcessNavigationResult::kCancelPrerenderContents);
       return content::NavigationThrottle::CANCEL_AND_IGNORE;
+    }
+
+    content::NavigationEntry* last_entry = navigation_handle()
+                                               ->GetWebContents()
+                                               ->GetController()
+                                               .GetLastCommittedEntry();
+    // We are about to open a new app window context. Record the time since the
+    // last navigation in this context. (If it is very small, this context
+    // probably redirected immediately, which is a bad user experience.)
+    if (last_entry && !last_entry->GetTimestamp().is_null()) {
+      UMA_HISTOGRAM_MEDIUM_TIMES(
+          "Extensions.BookmarkApp.OpenAppDeltaSinceLastNavigation",
+          base::Time::Now() - last_entry->GetTimestamp());
     }
 
     content::NavigationThrottle::ThrottleCheckResult result =
