caf3c5dc37cc7d19f3947406b6dbfb02c0f22f1f
Koji Ishii <kojii@chromium.org>
2017-12-07 17:41:56 +0000

    [LayoutNG] Support 'visibility' in NGTextFragmentPainter
    This patch supports the CSS 'visibility' property in
    NGTextFragmentPainter, in the same manner as InlineTextBoxPainter.
    
    The logic for line break painting is copied, but to be verified
    when selection is supported in LayoutNGPaintFragments.
    
    Bug: 714962
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I2334fa2af6588257c77cdc26b0f58daf39d48b63
    Reviewed-on: https://chromium-review.googlesource.com/810305
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522462}

--- third_party/WebKit/Source/core/paint/ng/ng_text_fragment_painter.cc
+++ third_party/WebKit/Source/core/paint/ng/ng_text_fragment_painter.cc
@@ -18,6 +18,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+namespace {
+
+inline bool ShouldPaintTextFragment(const NGPhysicalTextFragment& text_fragment,
+                                    const ComputedStyle& style) {
+  if (style.Visibility() != EVisibility::kVisible)
+    return false;
+
+  // When painting selection, we want to include a highlight when the
+  // selection spans line breaks. In other cases such as invisible elements
+  // or those with no text that are not line breaks, we can skip painting
+  // wholesale.
+  // TODO(wkorman): Constrain line break painting to appropriate paint phase.
+  // This code path is only called in PaintPhaseForeground whereas we would
+  // expect PaintPhaseSelection. The existing haveSelection logic in paint()
+  // tests for != PaintPhaseTextClip.
+  if (!text_fragment.Length() || !text_fragment.TextShapeResult())
+    return false;
+
+  return true;
+}
+
+}  // namespace
+
 NGTextFragmentPainter::NGTextFragmentPainter(
     const NGPaintFragment& text_fragment)
     : fragment_(text_fragment) {
@@ -27,7 +50,12 @@ NGTextFragmentPainter::NGTextFragmentPainter(
 void NGTextFragmentPainter::Paint(const Document& document,
                                   const PaintInfo& paint_info,
                                   const LayoutPoint& paint_offset) {
+  const NGPhysicalTextFragment& text_fragment =
+      ToNGPhysicalTextFragment(fragment_.PhysicalFragment());
   const ComputedStyle& style = fragment_.Style();
+
+  if (!ShouldPaintTextFragment(text_fragment, style))
+    return;
 
   NGPhysicalSize size_;
   NGPhysicalOffset offset_;
@@ -79,9 +107,6 @@ void NGTextFragmentPainter::Paint(const Document& document,
   // 2. Now paint the foreground, including text and decorations.
   int selection_start = 0;
   int selection_end = 0;
-
-  const NGPhysicalTextFragment& text_fragment =
-      ToNGPhysicalTextFragment(fragment_.PhysicalFragment());
 
   LayoutRect box_rect(box_origin, fragment_.Size().ToLayoutSize());
   Optional<GraphicsContextStateSaver> state_saver;
