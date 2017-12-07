c86416d2abd45cb1d85d7da324191b97c53c4976
Jay Civelli <jcivelli@chromium.org>
2017-12-07 19:18:08 +0000

    Mojofying PdfToEmfConverter.
    Mojofying PdfToEmfConverter in preparation of its servicification.
    The PDF engine running in the service process might query for
    pre-caching of fonts, which was previously done as part of the utility
    process initialization in PrintingHandler. It is now done through a
    client interface that must be provided when creating an instance of the
    PdfToEmfConverter interface through the PdfToEmfConverterFactory.
    
    Bug: 766451
    Change-Id: Ie0a990121c0676c5a0a4d48ef0ea15339aefb4b9
    Reviewed-on: https://chromium-review.googlesource.com/801778
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Jay Civelli <jcivelli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522501}

--- chrome/utility/chrome_content_utility_client.cc
+++ chrome/utility/chrome_content_utility_client.cc
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_WIN)
 #include "chrome/services/util_win/public/interfaces/constants.mojom.h"
 #include "chrome/services/util_win/util_win_service.h"
+#include "chrome/utility/printing/pdf_to_emf_converter_factory_impl.h"
 #endif
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
@@ -180,6 +181,11 @@ void ChromeContentUtilityClient::UtilityThreadStarted() {
     registry->AddInterface(base::Bind(CreateResourceUsageReporter),
                            base::ThreadTaskRunnerHandle::Get());
 #endif  // !defined(OS_ANDROID)
+#if defined(OS_WIN)
+    registry->AddInterface(
+        base::Bind(printing::PdfToEmfConverterFactoryImpl::Create),
+        base::ThreadTaskRunnerHandle::Get());
+#endif
   }
 
   connection->AddConnectionFilter(
