889890af937155273a6cdf8ed2d40d9bc4d5cbe5
Francois Doray <fdoray@chromium.org>
2017-12-07 19:15:45 +0000

    Add resource_coordinator::LifecycleUnitSource.
    This is the interface for any class that creates and destroys
    LifecycleUnits.
    
    Bug: 775644
    Change-Id: I500480492f80456788a903ed98ac71327b87d02b
    Reviewed-on: https://chromium-review.googlesource.com/812167
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Commit-Queue: Chris Hamilton <chrisha@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522500}

---
+++ chrome/browser/resource_coordinator/lifecycle_unit_source.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_RESOURCE_COORDINATOR_LIFECYCLE_UNIT_SOURCE_H_
+#define CHROME_BROWSER_RESOURCE_COORDINATOR_LIFECYCLE_UNIT_SOURCE_H_
+
+namespace resource_coordinator {
+
+class LifecycleUnitSourceObserver;
+
+// Interface for a class that creates and destroys LifecycleUnits.
+class LifecycleUnitSource {
+ public:
+  virtual ~LifecycleUnitSource() = default;
+
+  // Adds / removes an observer that is notified when a LifecycleUnit is created
+  // or destroyed by this LifecycleUnitSource.
+  virtual void AddObserver(LifecycleUnitSourceObserver* observer) = 0;
+  virtual void RemoveObserver(LifecycleUnitSourceObserver* observer) = 0;
+};
+
+}  // namespace resource_coordinator
+
+#endif  // CHROME_BROWSER_RESOURCE_COORDINATOR_LIFECYCLE_UNIT_SOURCE_H_
