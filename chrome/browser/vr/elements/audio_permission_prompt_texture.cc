b78fcc7f0881409e2f1482aa1eed0221f802a1a9
Biao She <bshe@chromium.org>
2017-12-07 15:27:02 +0000

    Change the text font in audio permission button to san-serif-medium
    Bug: 792600
    Change-Id: I5b49dc226ca27cea0b686d3b118956b18d8ddbd4
    Reviewed-on: https://chromium-review.googlesource.com/811837
    Reviewed-by: Amirhossein Simjour <asimjour@chromium.org>
    Commit-Queue: Biao She <bshe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522432}

--- chrome/browser/vr/elements/audio_permission_prompt_texture.cc
+++ chrome/browser/vr/elements/audio_permission_prompt_texture.cc
@@ -38,7 +38,7 @@ constexpr float kButtonRadius = 0.0035f;
 
 constexpr float kButtonWidth = 0.162f;
 
-constexpr char kPreferredFontNameForButtons[] = "sans-serif.medium";
+constexpr char kPreferredFontNameForButtons[] = "sans-serif-medium";
 
 }  // namespace
 
