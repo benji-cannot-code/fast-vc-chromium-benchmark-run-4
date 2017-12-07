780b086e24c047361f2bd7947d4bbfcda40131ce
sebsg <sebsg@chromium.org>
2017-12-07 18:27:45 +0000

    [Autofill] Fix crash when getting origin for forms.
    There are some crashed happening when trying to get the SecurityOrigin
    of some forms in Autofill. There was a DCHECK present but I added a
    if statement to handle that case more elegantly.
    
    I could not repro the issue however. The result is that a default
    Origin will be set in the case where the is no frame for the document.
    
    As a result nothing will be logged. But nothing bad will happen.
    
    Bug: 792688
    Change-Id: I26eb916268a1ce4d3adb03f652aef2e006537b67
    Reviewed-on: https://chromium-review.googlesource.com/814137
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Reviewed-by: Mathieu Perreault <mathp@chromium.org>
    Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522480}

--- components/autofill/content/renderer/form_autofill_util.cc
+++ components/autofill/content/renderer/form_autofill_util.cc
@@ -1143,8 +1143,12 @@ bool UnownedFormElementsAndFieldSetsToFormData(
     FormData* form,
     FormFieldData* field) {
   form->origin = GetCanonicalOriginForDocument(document);
-  DCHECK(document.GetFrame()->Top());
-  form->main_frame_origin = document.GetFrame()->Top()->GetSecurityOrigin();
+  if (document.GetFrame()) {
+    form->main_frame_origin = document.GetFrame()->Top()->GetSecurityOrigin();
+  } else {
+    form->main_frame_origin = url::Origin();
+    NOTREACHED();
+  }
 
   form->is_form_tag = false;
 
@@ -1471,8 +1475,12 @@ bool WebFormElementToFormData(
   form->name = GetFormIdentifier(form_element);
   form->origin = GetCanonicalOriginForDocument(frame->GetDocument());
   form->action = frame->GetDocument().CompleteURL(form_element.Action());
-  DCHECK(frame->Top());
-  form->main_frame_origin = frame->Top()->GetSecurityOrigin();
+  if (frame->Top()) {
+    form->main_frame_origin = frame->Top()->GetSecurityOrigin();
+  } else {
+    form->main_frame_origin = url::Origin();
+    NOTREACHED();
+  }
   // If the completed URL is not valid, just use the action we get from
   // WebKit.
   if (!form->action.is_valid())
