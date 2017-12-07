2be582084d009f3f5a65f0465bd1cdff9c6622dc
Luke Halliwell <halliwell@chromium.org>
2017-12-07 03:00:20 +0000

    [Chromecast] Remove CastContentBrowserClient::PreCreateThreads
    Moved to CastBrowserMainParts, per the bug.
    
    Bug: internal b/27811461
    Test: none
    Change-Id: Ic3fd8f7ed51473e8a61caff62c5caa762af03b84
    Reviewed-on: https://chromium-review.googlesource.com/811286
    Reviewed-by: Sergey Volk <servolk@chromium.org>
    Commit-Queue: Luke Halliwell <halliwell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522320}

---
+++ chromecast/browser/cast_browser_main_parts_simple.cc
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromecast/browser/cast_browser_main_parts.h"
+
+namespace chromecast {
+namespace shell {
+
+// static
+std::unique_ptr<CastBrowserMainParts> CastBrowserMainParts::Create(
+    const content::MainFunctionParams& parameters,
+    URLRequestContextFactory* url_request_context_factory) {
+  return std::make_unique<CastBrowserMainParts>(parameters,
+                                                url_request_context_factory);
+}
+
+}  // namespace shell
+}  // namespace chromecast
