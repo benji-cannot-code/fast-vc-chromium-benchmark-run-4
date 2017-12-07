8fa54930bfbd81d7171beafd89a4d099e81765ff
Tomas Popela <tomas.popela@gmail.com>
2017-12-07 22:33:34 +0000

    memcpy used without including string.h
    Compiling Chromium with Clang 4.0.1 and using libstdc++ will fail on using
    memcpy without including string.h.
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: Idced1d5de3baf6b520d4a2d61774120642ead1a8
    Reviewed-on: https://chromium-review.googlesource.com/813737
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522579}

--- cc/paint/raw_memory_transfer_cache_entry.cc
+++ cc/paint/raw_memory_transfer_cache_entry.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "cc/paint/raw_memory_transfer_cache_entry.h"
 
+#include <string.h>
+
 namespace cc {
 
 ClientRawMemoryTransferCacheEntry::ClientRawMemoryTransferCacheEntry(
