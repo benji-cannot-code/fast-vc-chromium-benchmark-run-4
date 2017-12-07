3824d42ac76e5acb40d03f5c8f9516f215a1d7bb
Ilya Sherman <isherman@chromium.org>
2017-12-07 14:41:49 +0000

    [Cleanup] Fix a typo.
    R=asvitkine@chromium.org
    
    Bug: none
    Change-Id: I392d54d332cd0ec4d43fa3afbfcc8317a1eac165
    Reviewed-on: https://chromium-review.googlesource.com/812479
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522423}

--- components/variations/client_filterable_state.h
+++ components/variations/client_filterable_state.h
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace variations {
 
-// A contianer for all of the client state which is used for filtering studies.
+// A container for all of the client state which is used for filtering studies.
 struct ClientFilterableState {
   static Study::Platform GetCurrentPlatform();
 
