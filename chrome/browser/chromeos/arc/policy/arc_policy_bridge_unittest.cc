77d56cde9a7e4430081d8e8dc858d0f065be8a52
Luis Hector Chavez <lhchavez@google.com>
2017-12-07 20:26:11 +0000

    arc: Introduce CloseInstance()
    Previously we used to have a single SetInstance() method where nullptr
    could be passed. We don't support multiple concurrent instances, and the
    code didn't do a great job of handling this case. For example, if
    instance A was closed just after instance B was connected, we would lose
    track of both!
    
    This change introduces an explicit CloseInstance(), and all close
    operations where the stored instance does not match the instance that
    the caller wanted to close (since it was already closed), we just no-op.
    
    Bug: 782895
    Test: git cl try
    Change-Id: Iaf57685be862f65d03bd9a90133b372655661955
    Reviewed-on: https://chromium-review.googlesource.com/802823
    Commit-Queue: Luis Hector Chavez <lhchavez@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522530}

--- chrome/browser/chromeos/arc/policy/arc_policy_bridge_unittest.cc
+++ chrome/browser/chromeos/arc/policy/arc_policy_bridge_unittest.cc
@@ -154,7 +154,7 @@ class ArcPolicyBridgeTest : public testing::Test {
   }
 
   void TearDown() override {
-    bridge_service_->policy()->SetInstance(nullptr, 0);
+    bridge_service_->policy()->CloseInstance(policy_instance_.get());
     policy_instance_.reset();
   }
 
