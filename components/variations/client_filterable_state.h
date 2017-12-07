aef17ff1c03d388f6966f014228465734139b0c5
Ilya Sherman <isherman@chromium.org>
2017-12-07 14:37:46 +0000

    [Cleanup] Add a missing #include.
    R=asvitkine@chromium.org
    
    Bug: none
    Change-Id: I4e256bef6d7cc6f3af24a5b1241d2279eaf82cad
    Reviewed-on: https://chromium-review.googlesource.com/813499
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522418}

--- components/variations/client_filterable_state.h
+++ components/variations/client_filterable_state.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_VARIATIONS_CLIENT_FILTERABLE_STATE_H_
 #define COMPONENTS_VARIATIONS_CLIENT_FILTERABLE_STATE_H_
 
+#include <string>
+
 #include "base/macros.h"
 #include "base/time/time.h"
 #include "base/version.h"
