b13aaf326a649cc7ba053b42e8c4a21d4ccbe748
Eugene Ostroukhov <eostroukhov@chromium.org>
2017-12-08 02:44:28 +0000

    [DevTools] Single item deletion for IndexedDB
    Added grid button and right click for object store/index item deletion.
    
    This CL is a rebased and updated version of the
    https://chromium-review.googlesource.com/c/chromium/src/+/688755 by
    kristipark@chromium.org
    
    Bug: 729797
    Change-Id: I6720b0fde77a25c4f8839d3d1929ec41f88e2585
    Reviewed-on: https://chromium-review.googlesource.com/804599
    Commit-Queue: Eugene Ostroukhov <eostroukhov@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522687}

--- third_party/WebKit/Source/devtools/front_end/resources/ApplicationPanelSidebar.js
+++ third_party/WebKit/Source/devtools/front_end/resources/ApplicationPanelSidebar.js
@@ -1300,6 +1300,13 @@ Resources.IDBObjectStoreTreeElement = class extends Resources.BaseStorageTreeEle
     contextMenu.show();
   }
 
+  _refreshObjectStore() {
+    if (this._view)
+      this._view.refreshData();
+    for (var indexName in this._idbIndexTreeElements)
+      this._idbIndexTreeElements[indexName].refreshIndex();
+  }
+
   async _clearObjectStore() {
     await this._model.clearObjectStore(this._databaseId, this._objectStore.name);
     this.update(this._objectStore, true);
@@ -1318,7 +1325,8 @@ Resources.IDBObjectStoreTreeElement = class extends Resources.BaseStorageTreeEle
       indexNames[index.name] = true;
       if (!this._idbIndexTreeElements[index.name]) {
         var idbIndexTreeElement = new Resources.IDBIndexTreeElement(
-            this._storagePanel, this._model, this._databaseId, this._objectStore, index);
+            this._storagePanel, this._model, this._databaseId, this._objectStore, index,
+            this._refreshObjectStore.bind(this));
         this._idbIndexTreeElements[index.name] = idbIndexTreeElement;
         this.appendChild(idbIndexTreeElement);
       }
@@ -1358,8 +1366,10 @@ Resources.IDBObjectStoreTreeElement = class extends Resources.BaseStorageTreeEle
    */
   onselect(selectedByUser) {
     super.onselect(selectedByUser);
-    if (!this._view)
-      this._view = new Resources.IDBDataView(this._model, this._databaseId, this._objectStore, null);
+    if (!this._view) {
+      this._view = new Resources.IDBDataView(
+          this._model, this._databaseId, this._objectStore, null, this._refreshObjectStore.bind(this));
+    }
 
     this.showView(this._view);
     return false;
@@ -1393,13 +1403,15 @@ Resources.IDBIndexTreeElement = class extends Resources.BaseStorageTreeElement {
    * @param {!Resources.IndexedDBModel.DatabaseId} databaseId
    * @param {!Resources.IndexedDBModel.ObjectStore} objectStore
    * @param {!Resources.IndexedDBModel.Index} index
+   * @param {function()} refreshObjectStore
    */
-  constructor(storagePanel, model, databaseId, objectStore, index) {
+  constructor(storagePanel, model, databaseId, objectStore, index, refreshObjectStore) {
     super(storagePanel, index.name, false);
     this._model = model;
     this._databaseId = databaseId;
     this._objectStore = objectStore;
     this._index = index;
+    this._refreshObjectStore = refreshObjectStore;
   }
 
   get itemURL() {
@@ -1410,6 +1422,11 @@ Resources.IDBIndexTreeElement = class extends Resources.BaseStorageTreeElement {
   markNeedsRefresh() {
     if (this._view)
       this._view.markNeedsRefresh();
+  }
+
+  refreshIndex() {
+    if (this._view)
+      this._view.refreshData();
   }
 
   /**
@@ -1444,8 +1461,10 @@ Resources.IDBIndexTreeElement = class extends Resources.BaseStorageTreeElement {
    */
   onselect(selectedByUser) {
     super.onselect(selectedByUser);
-    if (!this._view)
-      this._view = new Resources.IDBDataView(this._model, this._databaseId, this._objectStore, this._index);
+    if (!this._view) {
+      this._view = new Resources.IDBDataView(
+          this._model, this._databaseId, this._objectStore, this._index, this._refreshObjectStore);
+    }
 
     this.showView(this._view);
     return false;
