01b1e6dcf27ac38d1c179f1b189a7e738fd39129
Vladimir Levin <vmpstr@chromium.org>
2017-12-08 23:29:11 +0000

    oop: Add PaintTypeface transfer cache entry and use it in reader/writer.
    This patch adds a new PaintTypeface transfer cache entry. Before using
    this in the actual transfer cache, which has a few things to resolve,
    this patch adds the use in the PaintOp{Reader,Writer} to verify
    correctness.
    
    R=ericrk@chromium.org
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I1dd36ae4b1ecdc23e854133e86ab29233f4041c3
    Reviewed-on: https://chromium-review.googlesource.com/815947
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522916}

--- cc/paint/transfer_cache_entry.cc
+++ cc/paint/transfer_cache_entry.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "cc/paint/image_transfer_cache_entry.h"
+#include "cc/paint/paint_typeface_transfer_cache_entry.h"
 #include "cc/paint/raw_memory_transfer_cache_entry.h"
 
 namespace cc {
@@ -20,6 +21,8 @@ std::unique_ptr<ServiceTransferCacheEntry> ServiceTransferCacheEntry::Create(
       return std::make_unique<ServiceRawMemoryTransferCacheEntry>();
     case TransferCacheEntryType::kImage:
       return std::make_unique<ServiceImageTransferCacheEntry>();
+    case TransferCacheEntryType::kPaintTypeface:
+      return std::make_unique<ServicePaintTypefaceTransferCacheEntry>();
   }
 
   NOTREACHED();
