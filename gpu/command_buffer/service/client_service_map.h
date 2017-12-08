f5a3f8bbc517012e3b194d0433fdd18ac15c6bc2
Geoff Lang <geofflang@chromium.org>
2017-12-08 17:04:29 +0000

    Use ClientServiceMap for TexturePassthrough objects.
    Allows TexturePassthrough object lookup to get the benefit of the
    performance optimizations in ClientServiceMap.
    
    BUG=angleproject:2188
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I51a2b735f55aefa800bdebb080db389264173aeb
    Reviewed-on: https://chromium-review.googlesource.com/806359
    Commit-Queue: Geoff Lang <geofflang@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522799}

--- gpu/command_buffer/service/client_service_map.h
+++ gpu/command_buffer/service/client_service_map.h
@@ -20,7 +20,10 @@ template <typename ClientType, typename ServiceType>
 class ClientServiceMap {
  public:
   ClientServiceMap()
-      : client_to_service_array_(kInitialFlatArraySize, invalid_service_id()),
+      : ClientServiceMap(std::numeric_limits<ServiceType>::max()) {}
+  explicit ClientServiceMap(ServiceType invalid_service_id)
+      : invalid_service_id_(invalid_service_id),
+        client_to_service_array_(kInitialFlatArraySize, invalid_service_id),
         client_to_service_map_() {}
 
   void SetIDMapping(ClientType client_id, ServiceType service_id) {
@@ -124,9 +127,7 @@ class ClientServiceMap {
     return false;
   }
 
-  ServiceType invalid_service_id() const {
-    return std::numeric_limits<ServiceType>::max();
-  }
+  const ServiceType& invalid_service_id() const { return invalid_service_id_; }
 
   template <typename FunctionType>
   void ForEach(FunctionType func) const {
@@ -148,6 +149,7 @@ class ClientServiceMap {
   static constexpr size_t kMaxFlatArraySize = 0x4000;
 
  private:
+  ServiceType invalid_service_id_;
   std::vector<ServiceType> client_to_service_array_;
   std::unordered_map<ClientType, ServiceType> client_to_service_map_;
 };
