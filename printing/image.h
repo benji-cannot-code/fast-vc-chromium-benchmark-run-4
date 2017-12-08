498b1c5dee8588317028bbf12c02f7c6d4c189fa
Sergey Ulanov <sergeyu@chromium.org>
2017-12-08 20:07:01 +0000

    move printing::Image to //printing:test_support
    //printing target.
    
    printing: :Image is used only for tests, but it was compiled as part of
    Change-Id: I6b6cead63882b82aa6c406cbfcb75a1666216bc8
    Reviewed-on: https://chromium-review.googlesource.com/802184
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522854}

--- printing/image.h
+++ printing/image.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/logging.h"
-#include "printing/printing_export.h"
 #include "ui/gfx/geometry/size.h"
 
 namespace base {
@@ -26,7 +25,7 @@ class Metafile;
 
 // Lightweight raw-bitmap management. The image, once initialized, is immutable.
 // The main purpose is testing image contents.
-class PRINTING_EXPORT Image {
+class Image {
  public:
   // Creates the image from the metafile.  Deduces bounds based on bounds in
   // metafile.  If loading fails size().IsEmpty() will be true.
