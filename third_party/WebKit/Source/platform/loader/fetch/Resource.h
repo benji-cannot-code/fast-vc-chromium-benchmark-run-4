b748743850619c9ab3e1aff72272fc1716119aca
Nate Chapin <japhet@chromium.org>
2017-12-08 20:27:05 +0000

    Move SetIntegrityMetadata() calls to central location in ResourceFetcher
    Bug: 
    Change-Id: I8a96369725e6f72fab0f9d76fe73033c7cc4621d
    Reviewed-on: https://chromium-review.googlesource.com/815290
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Daniel Vogelheim <vogelheim@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522861}

--- third_party/WebKit/Source/platform/loader/fetch/Resource.h
+++ third_party/WebKit/Source/platform/loader/fetch/Resource.h
@@ -255,11 +255,8 @@ class PLATFORM_EXPORT Resource : public GarbageCollectedFinalized<Resource>,
   bool HasCacheControlNoStoreHeader() const;
   bool MustReloadDueToVaryHeader(const ResourceRequest& new_request) const;
 
-  void SetIntegrityMetadata(const IntegrityMetadataSet& metadata) {
-    integrity_metadata_ = metadata;
-  }
   const IntegrityMetadataSet& IntegrityMetadata() const {
-    return integrity_metadata_;
+    return options_.integrity_metadata;
   }
   ResourceIntegrityDisposition IntegrityDisposition() const {
     return integrity_disposition_;
@@ -474,7 +471,6 @@ class PLATFORM_EXPORT Resource : public GarbageCollectedFinalized<Resource>,
   bool detachable_ = false;
 
   ResourceIntegrityDisposition integrity_disposition_;
-  IntegrityMetadataSet integrity_metadata_;
   SubresourceIntegrity::ReportInfo integrity_report_info_;
 
   // Ordered list of all redirects followed while fetching this resource.
