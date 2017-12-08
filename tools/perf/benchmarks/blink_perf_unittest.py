f3d86ea50bff559cb6a1b0017e718a7bd04924a2
Ned Nguyen <nednguyen@google.com>
2017-12-08 19:12:59 +0000

    Revert "Disable failing tests on chromium.android/KitKat Phone Tester (dbg)"
    This reverts commit c305155dc03b95d03c93a9b21c2bdc5698327d0b.
    
    Reason for revert: we no longer run tests on " chromium.android/KitKat Phone Tester (dbg)"
    
    Original change's description:
    > Disable failing tests on chromium.android/KitKat Phone Tester (dbg)
    > 
    > Bug:793063
    > Change-Id: Ibcb22ee48cd3f1084b12d999d435e6d599681208
    > 
    > TBR=perezju@chromium.org, charliea@chromium.org
    > 
    > Change-Id: Ibcb22ee48cd3f1084b12d999d435e6d599681208
    > Reviewed-on: https://chromium-review.googlesource.com/815238
    > Reviewed-by: Ned Nguyen <nednguyen@google.com>
    > Commit-Queue: Shimi Zhang <ctzsm@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#522668}
    
    TBR=nednguyen@google.com,ctzsm@chromium.org
    
    Change-Id: I6b007123eb7ef49a73696ead7c689f13dde4445b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 793063
    Reviewed-on: https://chromium-review.googlesource.com/817376
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Commit-Queue: Ned Nguyen <nednguyen@google.com>
    Cr-Commit-Position: refs/heads/master@{#522835}

--- tools/perf/benchmarks/blink_perf_unittest.py
+++ tools/perf/benchmarks/blink_perf_unittest.py
@@ -6,7 +6,6 @@ import os
 import unittest
 
 from telemetry import story
-from telemetry import decorators
 from telemetry.page import page as page_module
 from telemetry.testing import options_for_unittests
 from telemetry.testing import page_test_test_case
@@ -40,7 +39,6 @@ class BlinkPerfTest(page_test_test_case.PageTestTestCase):
     story_set.AddStory(page)
     return story_set
 
-  @decorators.Disabled('android')  # crbug.com/793063
   def testBlinkPerfTracingMetricsForMeasureTime(self):
     results = self.RunMeasurement(measurement=self._measurement,
         ps=self._CreateStorySetForTestFile('append-child-measure-time.html'),
@@ -63,7 +61,6 @@ class BlinkPerfTest(page_test_test_case.PageTestTestCase):
 
     self.assertGreater(update_layout_trees[0].GetRepresentativeNumber(), 0.001)
 
-  @decorators.Disabled('android')  # crbug.com/793063
   def testBlinkPerfTracingMetricsForMeasureFrameTime(self):
     results = self.RunMeasurement(measurement=self._measurement,
         ps=self._CreateStorySetForTestFile(
@@ -88,7 +85,6 @@ class BlinkPerfTest(page_test_test_case.PageTestTestCase):
     self.assertGreater(frame_view_painttrees[0].GetRepresentativeNumber(),
         0.001)
 
-  @decorators.Disabled('android')  # crbug.com/793063
   def testBlinkPerfTracingMetricsForMeasurePageLoadTime(self):
     results = self.RunMeasurement(measurement=self._measurement,
         ps=self._CreateStorySetForTestFile(
