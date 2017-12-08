65f42ecb34641ba453d85d26c923d8db14fa6810
Morten Stenshorne <mstensho@chromium.org>
2017-12-08 16:36:24 +0000

    Subpixel support for column-gap.
    Moved code away from LayoutBlock, as this is multicol specific.
    
    Change-Id: I10de70497c0622b56362f08d05ab4bb2a21c910f
    Reviewed-on: https://chromium-review.googlesource.com/817121
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522791}

--- third_party/WebKit/Source/core/layout/LayoutBlock.h
+++ third_party/WebKit/Source/core/layout/LayoutBlock.h
@@ -227,8 +227,6 @@ class CORE_EXPORT LayoutBlock : public LayoutBox {
   LayoutBox* CreateAnonymousBoxWithSameTypeAs(
       const LayoutObject* parent) const override;
 
-  int ColumnGap() const;
-
   // Accessors for logical width/height and margins in the containing block's
   // block-flow direction.
   LayoutUnit LogicalWidthForChild(const LayoutBox& child) const {
