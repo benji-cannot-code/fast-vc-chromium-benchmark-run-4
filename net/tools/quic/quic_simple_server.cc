9e27284ccaaa12871fbb22b010fe2939ec869f70
Charles 'Buck' Krasic <ckrasic@chromium.org>
2017-12-08 03:29:05 +0000

    QUIC - fix flaky URLRequestQuicTest.
    o root cause test was needlessly using a fixed port with
      QuicSimpleServer, causing server initialization to fail
      if tests run in parallel.
    o In case QuicSimpleServer fails during startup, Shutdown would
      crash because it null-dereferenced socket_.
    
    R=rch@chromium.org
    
    Bug: 763098
    Change-Id: Ic73ea2ec4aa710473829aac3243074a4c1131322
    Reviewed-on: https://chromium-review.googlesource.com/816294
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Buck Krasic <ckrasic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522699}

--- net/tools/quic/quic_simple_server.cc
+++ net/tools/quic/quic_simple_server.cc
@@ -147,6 +147,9 @@ void QuicSimpleServer::Shutdown() {
   // notify clients that they're closing.
   dispatcher_->Shutdown();
 
+  if (!socket_) {
+    return;
+  }
   socket_->Close();
   socket_.reset();
 }
