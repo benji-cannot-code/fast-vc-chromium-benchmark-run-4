8f03414ee962b93afcadf103d2a1fa0386731801
Mustafa Emre Acer <meacer@chromium.org>
2017-12-08 01:27:22 +0000

    Remove obsolete grit code for Google Desktop
    Bug: 
    Change-Id: I62232b6859e1f6c84fcf39c6ba285280ea30b35b
    Reviewed-on: https://chromium-review.googlesource.com/814736
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522669}

--- tools/grit/grit/format/rc.py
+++ tools/grit/grit/format/rc.py
@@ -415,7 +415,7 @@ def FormatInclude(item, lang, output_dir, type=None, process_html=False):
   else:
     assert (isinstance(item, structure.StructureNode) and item.attrs['type'] in
         ['admin_template', 'chrome_html', 'chrome_scaled_image',
-         'muppet', 'tr_html', 'txt'])
+         'tr_html', 'txt'])
     filename_only = False
     relative_path = False
 
@@ -462,7 +462,6 @@ _STRUCTURE_FORMATTERS = {
   'chrome_html'         : partial(FormatInclude, type='BINDATA',
                                                  process_html=True),
   'chrome_scaled_image' : partial(FormatInclude, type='BINDATA'),
-  'muppet'              : partial(FormatInclude, type='XML'),
   'tr_html'             : partial(FormatInclude, type='HTML'),
   'txt'                 : partial(FormatInclude, type='TXT'),
   'policy_template_metafile': _DoNotFormat,
