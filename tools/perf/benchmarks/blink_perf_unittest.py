8c16527078fa43fd67a059da0513bb444aa58f41
Ned Nguyen <nednguyen@google.com>
2017-12-08 01:24:01 +0000

    Disable failing tests on chromium.android/KitKat Phone Tester (dbg)
    Bug:793063
    Change-Id: Ibcb22ee48cd3f1084b12d999d435e6d599681208
    
    TBR=perezju@chromium.org, charliea@chromium.org
    
    Change-Id: Ibcb22ee48cd3f1084b12d999d435e6d599681208
    Reviewed-on: https://chromium-review.googlesource.com/815238
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Commit-Queue: Shimi Zhang <ctzsm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522668}

--- tools/perf/benchmarks/blink_perf_unittest.py
+++ tools/perf/benchmarks/blink_perf_unittest.py
@@ -6,6 +6,7 @@ import os
 import unittest
 
 from telemetry import story
+from telemetry import decorators
 from telemetry.page import page as page_module
 from telemetry.testing import options_for_unittests
 from telemetry.testing import page_test_test_case
@@ -39,6 +40,7 @@ class BlinkPerfTest(page_test_test_case.PageTestTestCase):
     story_set.AddStory(page)
     return story_set
 
+  @decorators.Disabled('android')  # crbug.com/793063
   def testBlinkPerfTracingMetricsForMeasureTime(self):
     results = self.RunMeasurement(measurement=self._measurement,
         ps=self._CreateStorySetForTestFile('append-child-measure-time.html'),
@@ -61,6 +63,7 @@ class BlinkPerfTest(page_test_test_case.PageTestTestCase):
 
     self.assertGreater(update_layout_trees[0].GetRepresentativeNumber(), 0.001)
 
+  @decorators.Disabled('android')  # crbug.com/793063
   def testBlinkPerfTracingMetricsForMeasureFrameTime(self):
     results = self.RunMeasurement(measurement=self._measurement,
         ps=self._CreateStorySetForTestFile(
@@ -85,6 +88,7 @@ class BlinkPerfTest(page_test_test_case.PageTestTestCase):
     self.assertGreater(frame_view_painttrees[0].GetRepresentativeNumber(),
         0.001)
 
+  @decorators.Disabled('android')  # crbug.com/793063
   def testBlinkPerfTracingMetricsForMeasurePageLoadTime(self):
     results = self.RunMeasurement(measurement=self._measurement,
         ps=self._CreateStorySetForTestFile(
