c24cc2a1a31af8b67c056cf9304568c53ad72f61
Kenneth Russell <kbr@chromium.org>
2017-12-09 16:11:06 +0000

    Revert "[Autofill] Create AddressValidator in a background task."
    This reverts commit dedd6128557ac421342b4103c60f32240e58a191.
    
    Reason for revert: Likely cause of http://crbug.com/793520 .
    
    Original change's description:
    > [Autofill] Create AddressValidator in a background task.
    >
    > Will start a background task to create an AddressValidator in the
    > constructor.
    >
    > When the AddressValidator is loaded, it will load rules for any
    > pending normalization request.
    >
    > Bug: 789711
    > Change-Id: I59542f6a1bbaba1a58805ab3d51fa7c7b1ceeff3
    > Reviewed-on: https://chromium-review.googlesource.com/806715
    > Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    > Reviewed-by: François Doray <fdoray@chromium.org>
    > Commit-Queue: Mathieu Perreault <mathp@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#522875}
    
    TBR=rogerm@chromium.org,fdoray@chromium.org,mathp@chromium.org
    
    NOTRY=true
    
    Change-Id: Ifec15561b6e7a5466dfccb47922e8173e3666552
    No-Tree-Checks: true
    Bug: 789711, 793520
    Reviewed-on: https://chromium-review.googlesource.com/817766
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522999}

--- components/autofill/core/browser/address_normalizer_impl.h
+++ components/autofill/core/browser/address_normalizer_impl.h
@@ -12,8 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/macros.h"
-#include "base/memory/weak_ptr.h"
-#include "base/sequence_checker.h"
 #include "components/autofill/core/browser/address_normalizer.h"
 
 namespace i18n {
@@ -25,7 +23,6 @@ class Storage;
 
 namespace autofill {
 
-class AddressValidator;
 class AutofillProfile;
 
 // A class used to normalize addresses.
@@ -53,25 +50,14 @@ class AddressNormalizerImpl : public AddressNormalizer {
   void OnAddressValidationRulesLoaded(const std::string& region_code,
                                       bool success) override;
 
-  // Callback for when the AddressValidator's initialization comes back from the
-  // background task.
-  void OnAddressValidatorCreated(std::unique_ptr<AddressValidator> validator);
-
-  // Associating a region code to pending normalizations.
+  // Map associating a region code to pending normalizations.
   class NormalizationRequest;
-  void AddNormalizationRequestForRegion(
-      std::unique_ptr<NormalizationRequest> request,
-      const std::string& region_code);
   std::map<std::string, std::vector<std::unique_ptr<NormalizationRequest>>>
       pending_normalization_;
 
   // The address validator used to normalize addresses.
-  std::unique_ptr<AddressValidator> address_validator_;
+  AddressValidator address_validator_;
   const std::string app_locale_;
-
-  SEQUENCE_CHECKER(sequence_checker_);
-
-  base::WeakPtrFactory<AddressNormalizerImpl> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(AddressNormalizerImpl);
 };
