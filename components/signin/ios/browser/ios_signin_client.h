014001170bb78a889f52344f82d361b9ec34e93b
John Z Wu <jzw@chromium.org>
2017-12-09 00:19:23 +0000

    Introduce a helper that will be used by embedders of SigninClient to delay callbacks when network is offline and fire them when online.
    Both //ios/chrome and //ios/web_view needs to subclass SigninClient, and
    this helper will be used to share code between them.
    
    Bug: 774517
    Change-Id: I1da0e5163ee65f0287483a022120ebc51045cd8b
    Reviewed-on: https://chromium-review.googlesource.com/807328
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: John Wu <jzw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522932}

--- components/signin/ios/browser/ios_signin_client.h
+++ components/signin/ios/browser/ios_signin_client.h
@@ -13,13 +13,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/browser/host_content_settings_map.h"
 #include "components/signin/core/browser/signin_client.h"
 #include "components/signin/core/browser/signin_error_controller.h"
-#include "net/base/network_change_notifier.h"
 #include "net/url_request/url_request_context_getter.h"
+#include "components/signin/ios/browser/wait_for_network_callback_helper.h"
 
 // iOS specific signin client.
 class IOSSigninClient
     : public SigninClient,
-      public net::NetworkChangeNotifier::NetworkChangeObserver,
       public SigninErrorController::Observer {
  public:
   IOSSigninClient(
@@ -58,10 +57,6 @@ class IOSSigninClient
   // KeyedService implementation.
   void Shutdown() override;
 
-  // net::NetworkChangeController::NetworkChangeObserver implementation.
-  void OnNetworkChanged(
-      net::NetworkChangeNotifier::ConnectionType type) override;
-
  private:
   PrefService* pref_service_;
   net::URLRequestContextGetter* url_request_context_;
@@ -69,7 +64,7 @@ class IOSSigninClient
   scoped_refptr<content_settings::CookieSettings> cookie_settings_;
   scoped_refptr<HostContentSettingsMap> host_content_settings_map_;
   scoped_refptr<TokenWebData> token_web_data_;
-  std::list<base::Closure> delayed_callbacks_;
+  std::unique_ptr<WaitForNetworkCallbackHelper> callback_helper_;
 
   DISALLOW_COPY_AND_ASSIGN(IOSSigninClient);
 };
