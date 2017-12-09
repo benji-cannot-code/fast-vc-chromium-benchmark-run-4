030e0d6c70831b8d9ec207777eabfbed9bf78c9c
Peng Huang <penghuang@chromium.org>
2017-12-09 01:10:01 +0000

    exo: use cursor manager in mus
    Bug: 631103
    Change-Id: Ic765184940847ff42aa8a237da2c04220e37e09e
    Reviewed-on: https://chromium-review.googlesource.com/818115
    Reviewed-by: Elliot Glaysher <erg@chromium.org>
    Reviewed-by: David Reveman <reveman@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522946}

--- components/exo/wm_helper.cc
+++ components/exo/wm_helper.cc
@@ -73,8 +73,7 @@ void WMHelper::RemoveFocusObserver(
 }
 
 void WMHelper::AddCursorObserver(aura::client::CursorClientObserver* observer) {
-  // TODO(crbug.com/631103): Mushrome doesn't have a cursor manager yet.
-  if (ash::Shell::GetAshConfig() == ash::Config::CLASSIC)
+  if (ash::Shell::GetAshConfig() != ash::Config::MASH)
     ash::Shell::Get()->cursor_manager()->AddObserver(observer);
 }
 
@@ -184,15 +183,13 @@ aura::Window* WMHelper::GetFocusedWindow() const {
 }
 
 ui::CursorSize WMHelper::GetCursorSize() const {
-  // TODO(crbug.com/631103): Mushrome doesn't have a cursor manager yet.
-  if (ash::Shell::GetAshConfig() == ash::Config::MUS)
+  if (ash::Shell::GetAshConfig() == ash::Config::MASH)
     return ui::CursorSize::kNormal;
   return ash::Shell::Get()->cursor_manager()->GetCursorSize();
 }
 
 const display::Display& WMHelper::GetCursorDisplay() const {
-  // TODO(crbug.com/631103): Mushrome doesn't have a cursor manager yet.
-  if (ash::Shell::GetAshConfig() == ash::Config::MUS) {
+  if (ash::Shell::GetAshConfig() == ash::Config::MASH) {
     static const display::Display display;
     return display;
   }
