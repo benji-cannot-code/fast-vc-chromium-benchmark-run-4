e3b905402e3a10b670cdfb6ead0e4ccfb842fa5b
Michael Giuffrida <michaelpg@chromium.org>
2017-12-09 23:49:23 +0000

    SequenceId for TabMetrics UKM events
    UKMs are not timestamped and are not guaranteed to arrive in the same
    order they were logged. This CL adds a SequenceId field to the
    TabMetrics event to provide a simple ordering -- the SequenceId field is
    just a counter that is incremented each time it is logged.
    
    Session Restore UKMs use a similar field for the same purpose.
    
    Bug: 793147
    Change-Id: I06c486d44cc5e19583d3f7469c2ae68ec728cef6
    Reviewed-on: https://chromium-review.googlesource.com/815539
    Commit-Queue: Michael Giuffrida <michaelpg@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523014}

--- chrome/browser/ui/tabs/tab_activity_watcher.h
+++ chrome/browser/ui/tabs/tab_activity_watcher.h
@@ -30,9 +30,6 @@ class TabActivityWatcher : public TabStripModelObserver,
 
   // TODO(michaelpg): Track more events.
 
-  // Forces logging even when a timeout would have prevented it.
-  void DisableLogTimeoutForTest();
-
   // Returns the single instance, creating it if necessary.
   static TabActivityWatcher* GetInstance();
 
@@ -41,6 +38,8 @@ class TabActivityWatcher : public TabStripModelObserver,
   static void WatchWebContents(content::WebContents* web_contents);
 
  private:
+  friend class TabActivityWatcherTest;
+
   // TabStripModelObserver:
   void TabPinnedStateChanged(TabStripModel* tab_strip_model,
                              content::WebContents* contents,
@@ -57,6 +56,12 @@ class TabActivityWatcher : public TabStripModelObserver,
   // Logs the tab with |web_contents| if the tab hasn't been logged for the same
   // source ID within a timeout window.
   void MaybeLogTab(content::WebContents* web_contents);
+
+  // Forces logging even when a timeout would have prevented it.
+  void DisableLogTimeoutForTesting();
+
+  // Resets internal state.
+  void ResetForTesting();
 
   std::unique_ptr<TabMetricsLogger> tab_metrics_logger_;
 
