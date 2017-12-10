9fdf0af2cb6990b9aad52953be4f978f8e687cd0
Hiroshige Hayashizaki <hiroshige@chromium.org>
2017-12-10 21:00:30 +0000

    Always use ScriptSourceCode in V8ScriptRunner::CompileScript()
    Bug: 788828
    Change-Id: I5874bb6e4a283a3049a94c0f66292101dfa5cec7
    Reviewed-on: https://chromium-review.googlesource.com/794514
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523030}

--- third_party/WebKit/Source/core/inspector/DevToolsHost.cpp
+++ third_party/WebKit/Source/core/inspector/DevToolsHost.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/inspector/DevToolsHost.h"
 
+#include "bindings/core/v8/ScriptSourceCode.h"
 #include "bindings/core/v8/V8BindingForCore.h"
 #include "bindings/core/v8/V8ScriptRunner.h"
 #include "core/clipboard/Pasteboard.h"
@@ -138,11 +139,10 @@ void DevToolsHost::EvaluateScript(const String& expression) {
       Frame::NotifyUserActivation(frontend_frame_);
   v8::MicrotasksScope microtasks(script_state->GetIsolate(),
                                  v8::MicrotasksScope::kRunMicrotasks);
-  v8::Local<v8::String> source =
-      V8AtomicString(script_state->GetIsolate(), expression.Utf8().data());
-  V8ScriptRunner::CompileAndRunInternalScript(script_state, source,
-                                              script_state->GetIsolate(),
-                                              String(), TextPosition());
+  ScriptSourceCode source_code(expression, ScriptSourceLocationType::kInternal,
+                               nullptr, KURL(), TextPosition());
+  V8ScriptRunner::CompileAndRunInternalScript(script_state, source_code,
+                                              script_state->GetIsolate());
 }
 
 void DevToolsHost::DisconnectClient() {
--- third_party/WebKit/Source/core/inspector/ThreadDebugger.cpp
+++ third_party/WebKit/Source/core/inspector/ThreadDebugger.cpp
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/inspector/ThreadDebugger.h"
 
 #include <memory>
+#include "bindings/core/v8/ScriptSourceCode.h"
 #include "bindings/core/v8/SourceLocation.h"
 #include "bindings/core/v8/V8BindingForCore.h"
 #include "bindings/core/v8/V8Blob.h"
@@ -279,8 +280,9 @@ void ThreadDebugger::installAdditionalCommandLineAPI(
   bool success =
       V8ScriptRunner::CompileAndRunInternalScript(
           ScriptState::From(context),
-
-          V8String(isolate_, "(function(e) { console.log(e.type, e); })"),
+          ScriptSourceCode("(function(e) { console.log(e.type, e); })",
+                           ScriptSourceLocationType::kInternal, nullptr, KURL(),
+                           TextPosition()),
           isolate_)
           .ToLocal(&function_value) &&
       function_value->IsFunction();
