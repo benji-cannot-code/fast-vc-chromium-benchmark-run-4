92cd5e47e8930b37ca4365d8c83a64408426d371
Mitsuru Oshima <oshima@chromium.org>
2017-12-11 22:27:48 +0000

    Introduce ShellSurfaceBase
    Separated window state change methods into ShellSurface and ClientControlledShellSurface
    
    Bug: 809488
    Change-Id: Ifec29e7a7ce2df0dd3b3c4a606479682d9f60dc2
    Reviewed-on: https://chromium-review.googlesource.com/814835
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: David Reveman <reveman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523233}

--- components/exo/client_controlled_shell_surface.cc
+++ components/exo/client_controlled_shell_surface.cc
@@ -27,9 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/display/screen.h"
 #include "ui/views/widget/widget.h"
 #include "ui/wm/core/coordinate_conversion.h"
-////#include "ui/wm/core/shadow.h"
-//#include "ui/wm/core/shadow_controller.h"
-//#include "ui/wm/core/shadow_types.h"
 #include "ui/wm/core/window_util.h"
 
 namespace exo {
@@ -71,11 +68,7 @@ Orientation SizeToOrientation(const gfx::Size& size) {
 ClientControlledShellSurface::ClientControlledShellSurface(Surface* surface,
                                                            bool can_minimize,
                                                            int container)
-    : ShellSurface(surface,
-                   gfx::Point(),
-                   true,
-                   can_minimize,
-                   container),
+    : ShellSurfaceBase(surface, gfx::Point(), true, can_minimize, container),
       primary_display_id_(
           display::Screen::GetScreen()->GetPrimaryDisplay().id()) {
   WMHelper::GetInstance()->AddDisplayConfigurationObserver(this);
@@ -85,6 +78,55 @@ ClientControlledShellSurface::ClientControlledShellSurface(Surface* surface,
 ClientControlledShellSurface::~ClientControlledShellSurface() {
   WMHelper::GetInstance()->RemoveDisplayConfigurationObserver(this);
   display::Screen::GetScreen()->RemoveObserver(this);
+}
+
+void ClientControlledShellSurface::SetMaximized() {
+  TRACE_EVENT0("exo", "ClientControlledShellSurface::SetMaximized");
+
+  if (!widget_)
+    CreateShellSurfaceWidget(ui::SHOW_STATE_MAXIMIZED);
+
+  // Note: This will ask client to configure its surface even if already
+  // maximized.
+  ScopedConfigure scoped_configure(this, true);
+  widget_->Maximize();
+}
+
+void ClientControlledShellSurface::SetMinimized() {
+  TRACE_EVENT0("exo", "ClientControlledShellSurface::SetMinimized");
+
+  if (!widget_)
+    CreateShellSurfaceWidget(ui::SHOW_STATE_MINIMIZED);
+
+  // Note: This will ask client to configure its surface even if already
+  // minimized.
+  ScopedConfigure scoped_configure(this, true);
+  widget_->Minimize();
+}
+
+void ClientControlledShellSurface::SetRestored() {
+  TRACE_EVENT0("exo", "ClientControlledShellSurface::SetRestored");
+
+  if (!widget_)
+    return;
+
+  // Note: This will ask client to configure its surface even if not already
+  // maximized or minimized.
+  ScopedConfigure scoped_configure(this, true);
+  widget_->Restore();
+}
+
+void ClientControlledShellSurface::SetFullscreen(bool fullscreen) {
+  TRACE_EVENT1("exo", "ClientControlledShellSurface::SetFullscreen",
+               "fullscreen", fullscreen);
+
+  if (!widget_)
+    CreateShellSurfaceWidget(ui::SHOW_STATE_FULLSCREEN);
+
+  // Note: This will ask client to configure its surface even if fullscreen
+  // state doesn't change.
+  ScopedConfigure scoped_configure(this, true);
+  widget_->SetFullscreen(fullscreen);
 }
 
 void ClientControlledShellSurface::SetPinned(ash::mojom::WindowPinType type) {
@@ -161,7 +203,7 @@ void ClientControlledShellSurface::SetTopInset(int height) {
 // SurfaceDelegate overrides:
 
 void ClientControlledShellSurface::OnSurfaceCommit() {
-  ShellSurface::OnSurfaceCommit();
+  ShellSurfaceBase::OnSurfaceCommit();
   if (widget_) {
     // Apply new top inset height.
     if (pending_top_inset_height_ != top_inset_height_) {
@@ -281,7 +323,7 @@ void ClientControlledShellSurface::CompositorLockTimedOut() {
 
 void ClientControlledShellSurface::SetWidgetBounds(const gfx::Rect& bounds) {
   if (!resizer_) {
-    ShellSurface::SetWidgetBounds(bounds);
+    ShellSurfaceBase::SetWidgetBounds(bounds);
     return;
   }
 
--- components/exo/client_controlled_shell_surface.h
+++ components/exo/client_controlled_shell_surface.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/display/window_tree_host_manager.h"
 #include "base/macros.h"
-#include "components/exo/shell_surface.h"
+#include "components/exo/shell_surface_base.h"
 #include "ui/base/hit_test.h"
 #include "ui/compositor/compositor_lock.h"
 #include "ui/display/display_observer.h"
@@ -32,7 +32,7 @@ enum class Orientation { PORTRAIT, LANDSCAPE };
 // position specified as part of the geometry is relative to the origin of
 // the screen coordinate system.
 class ClientControlledShellSurface
-    : public ShellSurface,
+    : public ShellSurfaceBase,
       public display::DisplayObserver,
       public ash::WindowTreeHostManager::Observer,
       public ui::CompositorLockClient {
@@ -41,6 +41,18 @@ class ClientControlledShellSurface
                                bool can_minimize,
                                int container);
   ~ClientControlledShellSurface() override;
+
+  // Called when the client was maximized.
+  void SetMaximized();
+
+  // Called when the client was minimized.
+  void SetMinimized();
+
+  // Called when the client was restored.
+  void SetRestored();
+
+  // Called when the client chagned the fullscreen state.
+  void SetFullscreen(bool fullscreen);
 
   // Pin/unpin the surface. Pinned surface cannot be switched to
   // other windows unless its explicitly unpinned.
