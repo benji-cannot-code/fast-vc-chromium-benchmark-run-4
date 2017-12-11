bbeb462d593105f29aa064b639bc4e81ca7f0ee3
Hui(Andy) Wu <wuandy@chromium.org>
2017-12-11 19:35:30 +0000

    [Autofill] Change autofill metrics to log correctly for rationalized phone fields.
    After a phone number field is marked as fill_only_when_focused by rationalization,
    we should log the quality metrics differently compared to normal fields. For
    example, if a field is left empty and being fill_only_when_focused, it should be
    logged as true negative, meaning we anticipated this field will be left empty.
    
    Bug: 780471
    Change-Id: I3457b6381430055c7c4cb8431f0da193f396065d
    Reviewed-on: https://chromium-review.googlesource.com/786340
    Commit-Queue: Hui Wu <wuandy@chromium.org>
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523172}

--- components/autofill/core/browser/autofill_manager.h
+++ components/autofill/core/browser/autofill_manager.h
@@ -265,9 +265,7 @@ class AutofillManager : public AutofillHandler,
   }
 
   // Exposed for testing.
-  AutofillExternalDelegate* external_delegate() {
-    return external_delegate_;
-  }
+  AutofillExternalDelegate* external_delegate() { return external_delegate_; }
 
   // Exposed for testing.
   void set_download_manager(AutofillDownloadManager* manager) {
@@ -604,6 +602,17 @@ class AutofillManager : public AutofillHandler,
   FRIEND_TEST_ALL_PREFIXES(AutofillMetricsTest, QualityMetrics);
   FRIEND_TEST_ALL_PREFIXES(AutofillMetricsTest,
                            QualityMetrics_BasedOnAutocomplete);
+  FRIEND_TEST_ALL_PREFIXES(
+      AutofillMetricsTest,
+      QualityMetrics_LoggedCorrecltyForOnlyFillWhenFocusedField);
+  FRIEND_TEST_ALL_PREFIXES(AutofillMetricsTest,
+                           QualityMetrics_LoggedCorrecltyForRationalizationOk);
+  FRIEND_TEST_ALL_PREFIXES(
+      AutofillMetricsTest,
+      QualityMetrics_LoggedCorrecltyForRationalizationGood);
+  FRIEND_TEST_ALL_PREFIXES(AutofillMetricsTest,
+                           QualityMetrics_LoggedCorrecltyForRationalizationBad);
+
   FRIEND_TEST_ALL_PREFIXES(AutofillMetricsTest, SaneMetricsWithCacheMismatch);
   FRIEND_TEST_ALL_PREFIXES(AutofillManagerTest, TestExternalDelegate);
   FRIEND_TEST_ALL_PREFIXES(AutofillManagerTest,
@@ -614,8 +623,7 @@ class AutofillManager : public AutofillHandler,
                            UserHappinessFormInteraction_AddressForm);
   FRIEND_TEST_ALL_PREFIXES(AutofillMetricsTest,
                            UserHappinessFormInteraction_CreditCardForm);
-  FRIEND_TEST_ALL_PREFIXES(AutofillManagerTest,
-                           OnLoadedServerPredictions);
+  FRIEND_TEST_ALL_PREFIXES(AutofillManagerTest, OnLoadedServerPredictions);
   FRIEND_TEST_ALL_PREFIXES(AutofillManagerTest,
                            OnLoadedServerPredictions_ResetManager);
   FRIEND_TEST_ALL_PREFIXES(AutofillManagerTest, DontOfferToSavePaymentsCard);
