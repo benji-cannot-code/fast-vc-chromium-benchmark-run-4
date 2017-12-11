2669023487e766d7c425c2e384b3567a468781d6
Julien Isorce <julien.isorce@chromium.org>
2017-12-11 19:29:00 +0000

    Use the generic VaapiPictureFactory::GetGLTextureTarget
    Instead of hardcoding GL_TEXTURE_EXTERNAL_OES or GL_TEXTURE_2D
    
    This will be useful for the subsequent CL,
    https://chromium-review.googlesource.com/c/chromium/src/+/766787
    
    Bug: 785201
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I2678181b4b9389c39999d07cb9e56c102fc5f299
    Tests: media_unittests --gtest_filter=*VaapiVideoDecodeAcceleratorTest*
    Reviewed-on: https://chromium-review.googlesource.com/819131
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Julien Isorce <julien.isorce@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523168}

--- media/gpu/vaapi/vaapi_picture.h
+++ media/gpu/vaapi/vaapi_picture.h
@@ -58,7 +58,8 @@ class MEDIA_GPU_EXPORT VaapiPicture {
                int32_t picture_buffer_id,
                const gfx::Size& size,
                uint32_t texture_id,
-               uint32_t client_texture_id);
+               uint32_t client_texture_id,
+               uint32_t texture_target);
 
   scoped_refptr<VaapiWrapper> vaapi_wrapper_;
 
@@ -68,6 +69,7 @@ class MEDIA_GPU_EXPORT VaapiPicture {
   const gfx::Size size_;
   const uint32_t texture_id_;
   const uint32_t client_texture_id_;
+  const uint32_t texture_target_;
 
   SEQUENCE_CHECKER(sequence_checker_);
 
