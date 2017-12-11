5dd6e0f34981eb080ce69bd538dc6e5fd9599439
Yuzhu Shen <yzshen@chromium.org>
2017-12-11 20:50:07 +0000

    Reland "Remove AMD-style module support from gin/."
    This is a reland of c1ab49a9e23036de2f94da601782da9985a9a481
    Original change's description:
    > Remove AMD-style module support from gin/.
    >
    > Bug: 718047
    > Change-Id: If480fff65a2e5993ed5143eede8354f890de3a3b
    > Reviewed-on: https://chromium-review.googlesource.com/812172
    > Commit-Queue: Yuzhu Shen <yzshen@chromium.org>
    > Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#522850}
    
    TBR=jochen@chromium.org
    (There is no changes to the original CL.)
    
    Bug: 718047
    Change-Id: I95600ef92ce7014e5240712b4dc4e962f431b526
    Reviewed-on: https://chromium-review.googlesource.com/820091
    Commit-Queue: Yuzhu Shen <yzshen@chromium.org>
    Reviewed-by: Yuzhu Shen <yzshen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523198}

--- content/shell/renderer/layout_test/layout_test_content_renderer_client.cc
+++ content/shell/renderer/layout_test/layout_test_content_renderer_client.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/shell/test_runner/web_test_runner.h"
 #include "content/shell/test_runner/web_view_test_proxy.h"
 #include "content/test/mock_webclipboard_impl.h"
-#include "gin/modules/module_registry.h"
 #include "media/base/audio_latency.h"
 #include "media/base/mime_util.h"
 #include "media/media_features.h"
