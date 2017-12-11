b346ca50404527a526c0604c751a311c3bb60ca2
Sadrul Habib Chowdhury <sadrul@chromium.org>
2017-12-11 21:09:03 +0000

    mus/aura: Fix setting up LayerTreeFrameSink for a window.
    Make sure the LayerTreeFrameSink has the LocalSurfaceId correctly set,
    even if the LocalSurfaceId is generated before a LayerTreeFrameSink is
    created.
    
    BUG=786453
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I768f06ab84ede54218afd2f9c3dbaf8bbb27296a
    Reviewed-on: https://chromium-review.googlesource.com/817632
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523206}

--- ui/aura/mus/window_port_mus.h
+++ ui/aura/mus/window_port_mus.h
@@ -38,7 +38,7 @@ namespace aura {
 class ClientSurfaceEmbedder;
 class PropertyConverter;
 class Window;
-class WindowPortMusTestApi;
+class WindowPortMusTest;
 class WindowTreeClient;
 class WindowTreeClientPrivate;
 class WindowTreeHostMus;
@@ -100,7 +100,7 @@ class AURA_EXPORT WindowPortMus : public WindowPort, public WindowMus {
   viz::FrameSinkId GetFrameSinkId() const override;
 
  private:
-  friend class WindowPortMusTestApi;
+  friend class WindowPortMusTest;
   friend class WindowTreeClient;
   friend class WindowTreeClientPrivate;
   friend class WindowTreeHostMus;
