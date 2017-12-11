8ea8f2058ae10f5e0a2ee01241bacad1ff4590ad
Xiaohan Wang <xhwang@chromium.org>
2017-12-11 23:25:55 +0000

    media: Reduce latency in MojoDecoderBufferConverter
    Today we always write and read from the DataPipe asynchronously. This
    might introduce unnecessary latency since the DataPipe might already be
    readable/writable when we schedule the read/write.
    
    This also indirectly contributes to out-of-order dispatch of Decode()
    and Reset() calls in some MojoDecoderBufferConverter clients, e.g.
    MojoAudioDecoderService, MojoVideoDecoderService, MojoDecryptorService.
    See BUG for more details. Unit tests are added to cover this issue.
    
    Note that this CL does not completely fix the BUG. A follow up CL will
    come next, with more tests added.
    
    BUG=792281
    TEST=More tests added.
    
    Change-Id: Ia6dc25f8621cca2a041b6299b104de2074c06a02
    Reviewed-on: https://chromium-review.googlesource.com/818329
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: Xiangjun Zhang <xjz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523246}

--- media/mojo/common/mojo_decoder_buffer_converter.h
+++ media/mojo/common/mojo_decoder_buffer_converter.h
@@ -52,10 +52,11 @@ class MojoDecoderBufferReader {
 
  private:
   void CancelReadCB(ReadCB read_cb);
+  void CancelAllPendingReadCBs();
   void CompleteCurrentRead();
   void ScheduleNextRead();
   void OnPipeReadable(MojoResult result, const mojo::HandleSignalsState& state);
-  void ReadDecoderBufferData();
+  void ProcessPendingReads();
   void OnPipeError(MojoResult result);
 
   // Read side of the DataPipe for receiving DecoderBuffer data.
@@ -110,7 +111,7 @@ class MojoDecoderBufferWriter {
  private:
   void ScheduleNextWrite();
   void OnPipeWritable(MojoResult result, const mojo::HandleSignalsState& state);
-  void WriteDecoderBufferData();
+  void ProcessPendingWrites();
   void OnPipeError(MojoResult result);
 
   // Write side of the DataPipe for sending DecoderBuffer data.
