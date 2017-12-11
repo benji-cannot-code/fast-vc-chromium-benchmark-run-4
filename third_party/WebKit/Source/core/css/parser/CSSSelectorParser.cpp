f74c32eb5dc40886c98242fddf3af7a57fec5b4d
Hayato Ito <hayato@chromium.org>
2017-12-11 07:03:03 +0000

    Remove Runtime flags for /deep/ and ::shadow used in CSS dynamic profile
    M63 stable was released and we've not heard any issue about the removal of
    /deep/ from any web sites.  Let's remove the runtime flags.
    
    TBR=haraken
    
    Bug: 489954
    Change-Id: Ie7831904824e3a08e3e121004bdecabb0871e850
    Reviewed-on: https://chromium-review.googlesource.com/818678
    Commit-Queue: Hayato Ito <hayato@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Takayoshi Kochi <kochi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523051}

--- third_party/WebKit/Source/core/css/parser/CSSSelectorParser.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSSelectorParser.cpp
@@ -1,4 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+
 // Copyright 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -645,9 +646,6 @@ CSSSelector::RelationType CSSSelectorParser::ConsumeCombinator(
       const CSSParserToken& slash = range.ConsumeIncludingWhitespace();
       if (slash.GetType() != kDelimiterToken || slash.Delimiter() != '/')
         failed_parsing_ = true;
-      if (RuntimeEnabledFeatures::DeepCombinatorInCSSDynamicProfileEnabled()) {
-        return CSSSelector::kShadowDeep;
-      }
       return context_->IsDynamicProfile() ? CSSSelector::kShadowDeepAsDescendant
                                           : CSSSelector::kShadowDeep;
     }
