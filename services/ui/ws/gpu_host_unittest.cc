b425110bf42321f486fc36f79a9fa7cc28f91cec
Daniel Bratell <bratell@opera.com>
2017-12-11 17:28:00 +0000

    Remove X11 includes and replace with the ui/gfx/x/x11.h wrapper
    X11 headers pollute the global namespace with macros with
    common words like None, Status, False. To avoid problems
    because of that we now wrap X11 includes in ui/gfx/x/x11.h
    and move all problematic words to the x11 namespace.
    
    Bug: 782184
    Change-Id: I81859c8aabae677c40614d3c26036c5d4d1e715a
    Reviewed-on: https://chromium-review.googlesource.com/819731
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Daniel Bratell <bratell@opera.com>
    Cr-Commit-Position: refs/heads/master@{#523121}

--- services/ui/ws/gpu_host_unittest.cc
+++ services/ui/ws/gpu_host_unittest.cc
@@ -17,12 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/ui/ws/gpu_host_delegate.h"
 #include "ui/gl/init/gl_factory.h"
 
-#if defined(USE_X11)
-#include <X11/Xlib.h>
-#undef None
-#undef Bool
-#endif  // USE_X11
-
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace ui {
