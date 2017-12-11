ea39b357ed4b06018d059983f74f3780e58865bb
Daniel Bratell <bratell@opera.com>
2017-12-11 16:30:50 +0000

    Avoid using GetMessage in Windows since its a Win macro.
    Depending on whether certain headers had been included,
    GetMessage was a macro that expanded to GetMessageW. This
    caused later linker errors in jumbo builds.
    
    This patch renames the function GetIpcMessage to avoid any
    confusion.
    Alternatives are:
    * #undef the macro (can cause problems with later code)
    * Make sure that everyone that includes dwrite_font_fake_sender_win.h
    also has Windows.h included so that the macro is always there.
    
    Bug: 746953
    Change-Id: Ia172ee74dc430c5fb25d676f018695dc001f3470
    Reviewed-on: https://chromium-review.googlesource.com/819552
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523107}

--- content/test/dwrite_font_fake_sender_win.cc
+++ content/test/dwrite_font_fake_sender_win.cc
@@ -55,7 +55,7 @@ size_t FakeFontCollection::MessageCount() {
   return messages_.size();
 }
 
-IPC::Message* FakeFontCollection::GetMessage(size_t index) {
+IPC::Message* FakeFontCollection::GetIpcMessage(size_t index) {
   return messages_[index].get();
 }
 
