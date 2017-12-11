0d75d8ec0980072433d33b73063d1f95990b251a
Tetsui Ohkubo <tetsui@chromium.org>
2017-12-11 18:10:08 +0000

    Fix bubble null check in WebNotificationTray::HideMessageCenter.
    In https://crrev.com/c/748983 , sidebar implementation is added to
    WebNotificationTray behind the flag. Unfortunately, null check condition
    in WebNotificationTray::HideMessageCenter() was changed in a way that
    affects environment with the flag disabled.
    As a result, several crash bugs are introduced becuase
    |message_center_bubble_| is not freed even after the tray is closed.
    
    TEST=manual
    BUG=792583
    
    Change-Id: I96de43ca3e2bbdfc19519c3b296d29d445a17fcf
    Reviewed-on: https://chromium-review.googlesource.com/816778
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523135}

--- ash/system/web_notification/web_notification_tray.cc
+++ ash/system/web_notification/web_notification_tray.cc
@@ -380,7 +380,8 @@ bool WebNotificationTray::ShowMessageCenter(bool show_by_click) {
 }
 
 void WebNotificationTray::HideMessageCenter() {
-  if (!IsMessageCenterVisible())
+  if ((switches::IsSidebarEnabled() && !IsMessageCenterVisible()) ||
+      (!switches::IsSidebarEnabled() && !message_center_bubble()))
     return;
 
   SetIsActive(false);
