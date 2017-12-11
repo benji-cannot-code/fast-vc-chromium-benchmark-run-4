9e183ce7ccb212b0e2571c707afbe0213e794c69
Aaron Leventhal <aleventhal@chromium.org>
2017-12-11 21:01:52 +0000

    Do not treat omnibox search as answer
    When arrow to search with screen reader, do not say "answer" unless there
    really is an answer (such as the current weather).
    
    Bug: 793468
    Change-Id: Ic7de2826796f1f62b552940c3b8bfefd1f2878a4
    Reviewed-on: https://chromium-review.googlesource.com/817956
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523205}

--- chrome/browser/ui/cocoa/omnibox/omnibox_view_mac.mm
+++ chrome/browser/ui/cocoa/omnibox/omnibox_view_mac.mm
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/browser/omnibox_edit_controller.h"
 #include "components/omnibox/browser/omnibox_field_trial.h"
 #include "components/omnibox/browser/omnibox_popup_model.h"
-#include "components/omnibox/browser/suggestion_answer.h"
 #include "components/toolbar/toolbar_model.h"
 #include "content/public/browser/web_contents.h"
 #include "extensions/common/constants.h"
@@ -615,12 +614,8 @@ void OmniboxViewMac::OnTemporaryTextMaybeChanged(
   [field_ clearUndoChain];
 
   // Get friendly accessibility label.
-  base::string16 description =
-      match.answer ? match.answer->second_line().AccessibleText()
-                   : match.description;
   AnnounceAutocompleteForScreenReader(
-      AutocompleteMatchType::ToAccessibilityLabel(match.type, display_text,
-                                                  description));
+      AutocompleteMatchType::ToAccessibilityLabel(match, display_text));
 }
 
 bool OmniboxViewMac::OnInlineAutocompleteTextMaybeChanged(
