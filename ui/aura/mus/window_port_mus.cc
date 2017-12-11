06d4f62fec47966f5c1b718eb540e6c070bbee2e
Sadrul Habib Chowdhury <sadrul@chromium.org>
2017-12-11 21:00:18 +0000

    mus/aura: Embed windows should have valid LocalSurfaceId.
    If a window is embedding another client, then it needs to be able
    to generate a LocalSurfaceId that the embedded client will use.
    
    BUG=786453
    
    Change-Id: I2fa66355399a057536f5c07d512f5c82df10fe90
    Reviewed-on: https://chromium-review.googlesource.com/818156
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523204}

--- ui/aura/mus/window_port_mus.cc
+++ ui/aura/mus/window_port_mus.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/window.h"
 #include "ui/aura/window_delegate.h"
 #include "ui/aura/window_observer.h"
-#include "ui/aura/window_tree_host.h"
 #include "ui/base/class_property.h"
 #include "ui/base/ui_base_switches_util.h"
 #include "ui/display/display.h"
@@ -405,10 +404,10 @@ void WindowPortMus::AllocateLocalSurfaceId() {
 const viz::LocalSurfaceId& WindowPortMus::GetLocalSurfaceId() {
   if (switches::IsMusHostingViz())
     return local_surface_id_;
-  if (window_->GetRootWindow() != window_)
+  if (!window_->IsEmbeddingClient() && !window_->IsRootWindow())
     return local_surface_id_;
-  return GetOrAllocateLocalSurfaceId(
-      window_->GetHost()->GetBoundsInPixels().size());
+  return GetOrAllocateLocalSurfaceId(gfx::ConvertSizeToPixel(
+      GetDeviceScaleFactor(), window_->bounds().size()));
 }
 
 std::unique_ptr<WindowMusChangeData>
