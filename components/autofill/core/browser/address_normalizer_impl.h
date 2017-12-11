0fa38b18bdf78c1a594edb8864ece389740c52e7
Mathieu Perreault <mathp@chromium.org>
2017-12-11 20:05:50 +0000

    [Reland] [Autofill] Create AddressValidator in a background task.
    Will start a background task to create an AddressValidator in the
    constructor.
    
    When the AddressValidator is loaded, it will load rules for any
    pending normalization request.
    
    Reland with ValidationRulesStorageFactory destruction fix.
    
    Bug: 789711
    Test: components_unittests
    Change-Id: Ib08581c5afd48ddfe016d3024f28b228e33f646f
    Reviewed-on: https://chromium-review.googlesource.com/819594
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Commit-Queue: Mathieu Perreault <mathp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523182}

--- components/autofill/core/browser/address_normalizer_impl.h
+++ components/autofill/core/browser/address_normalizer_impl.h
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/macros.h"
+#include "base/memory/weak_ptr.h"
+#include "base/sequence_checker.h"
 #include "components/autofill/core/browser/address_normalizer.h"
 
 namespace i18n {
@@ -23,6 +25,7 @@ class Storage;
 
 namespace autofill {
 
+class AddressValidator;
 class AutofillProfile;
 
 // A class used to normalize addresses.
@@ -50,14 +53,25 @@ class AddressNormalizerImpl : public AddressNormalizer {
   void OnAddressValidationRulesLoaded(const std::string& region_code,
                                       bool success) override;
 
-  // Map associating a region code to pending normalizations.
+  // Callback for when the AddressValidator's initialization comes back from the
+  // background task.
+  void OnAddressValidatorCreated(std::unique_ptr<AddressValidator> validator);
+
+  // Associating a region code to pending normalizations.
   class NormalizationRequest;
+  void AddNormalizationRequestForRegion(
+      std::unique_ptr<NormalizationRequest> request,
+      const std::string& region_code);
   std::map<std::string, std::vector<std::unique_ptr<NormalizationRequest>>>
       pending_normalization_;
 
   // The address validator used to normalize addresses.
-  AddressValidator address_validator_;
+  std::unique_ptr<AddressValidator> address_validator_;
   const std::string app_locale_;
+
+  SEQUENCE_CHECKER(sequence_checker_);
+
+  base::WeakPtrFactory<AddressNormalizerImpl> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(AddressNormalizerImpl);
 };
