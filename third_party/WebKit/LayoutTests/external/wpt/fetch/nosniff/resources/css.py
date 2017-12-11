8421de7819bf4abc8990462d408d2e004686c628
Mike West <mkwst@chromium.org>
2017-12-11 10:04:36 +0000

    Check `nosniff` on the original response, not revalidation responses.
    Bug: 792412
    Change-Id: Id805387dea88e18e71d21ca1e799d1076d0b2c5e
    Reviewed-on: https://chromium-review.googlesource.com/813835
    Commit-Queue: Mike West <mkwst@chromium.org>
    Reviewed-by: Daniel Vogelheim <vogelheim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523068}

--- third_party/WebKit/LayoutTests/external/wpt/fetch/nosniff/resources/css.py
+++ third_party/WebKit/LayoutTests/external/wpt/fetch/nosniff/resources/css.py
@@ -1,16 +1,24 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 def main(request, response):
-    outcome = request.GET.first("outcome", "f")
     type = request.GET.first("type", None)
+    is_revalidation = request.headers.get("If-Modified-Since", None)
 
     content = "/* nothing to see here */"
 
     response.add_required_headers = False
-    response.writer.write_status(200)
-    response.writer.write_header("x-content-type-options", "nosniff")
-    response.writer.write_header("content-length", len(content))
-    if(type != None):
-      response.writer.write_header("content-type", type)
-    response.writer.end_headers()
-
-    response.writer.write(content)
+    if is_revalidation is not None:
+      response.writer.write_status(304)
+      response.writer.write_header("x-content-type-options", "nosniff")
+      response.writer.write_header("content-length", 0)
+      if(type != None):
+        response.writer.write_header("content-type", type)
+      response.writer.end_headers()
+      response.writer.write("")
+    else:
+      response.writer.write_status(200)
+      response.writer.write_header("x-content-type-options", "nosniff")
+      response.writer.write_header("content-length", len(content))
+      if(type != None):
+        response.writer.write_header("content-type", type)
+      response.writer.end_headers()
+      response.writer.write(content)
