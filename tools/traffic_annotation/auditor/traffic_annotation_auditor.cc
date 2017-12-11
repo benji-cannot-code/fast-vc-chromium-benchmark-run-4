7df96c24d002c1d095efb78d606052c27467ae77
rhalavati@google.com <rhalavati@google.com>
2017-12-11 15:39:23 +0000

    Extend annotations.xml.
    Extends annotations.xml to include incomplete annotations, file paths,
    and the list of fields in the incomplete annotations.
    
    Bug: 656607
    Bug: 788035
    Change-Id: Ibabbbd91fa8d618571d3fe00dc32fd0aec0277ed
    Reviewed-on: https://chromium-review.googlesource.com/817749
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523102}

--- tools/traffic_annotation/auditor/traffic_annotation_auditor.cc
+++ tools/traffic_annotation/auditor/traffic_annotation_auditor.cc
@@ -410,18 +410,6 @@ std::set<int> TrafficAnnotationAuditor::GetReservedIDsSet() {
   return reserved_ids;
 }
 
-void TrafficAnnotationAuditor::PurgeAnnotations(
-    const std::set<int>& hash_codes) {
-  extracted_annotations_.erase(
-      std::remove_if(extracted_annotations_.begin(),
-                     extracted_annotations_.end(),
-                     [&hash_codes](AnnotationInstance& annotation) {
-                       return base::ContainsKey(hash_codes,
-                                                annotation.unique_id_hash_code);
-                     }),
-      extracted_annotations_.end());
-}
-
 void TrafficAnnotationAuditor::CheckAllRequiredFunctionsAreAnnotated() {
   for (const CallInstance& call : extracted_calls_) {
     if (!call.is_annotated && !CheckIfCallCanBeUnannotated(call)) {
@@ -500,19 +488,15 @@ void TrafficAnnotationAuditor::CheckAnnotationsContents() {
   std::vector<AnnotationInstance*> partial_annotations;
   std::vector<AnnotationInstance*> completing_annotations;
   std::vector<AnnotationInstance> new_annotations;
-  std::set<int> to_be_purged;
 
   // Process complete annotations and separate the others.
   for (AnnotationInstance& instance : extracted_annotations_) {
-    bool keep_annotation = false;
     switch (instance.type) {
       case AnnotationInstance::Type::ANNOTATION_COMPLETE: {
         AuditorResult result = instance.IsComplete();
         if (result.IsOK())
           result = instance.IsConsistent();
-        if (result.IsOK())
-          keep_annotation = true;
-        else
+        if (!result.IsOK())
           errors_.push_back(result);
         break;
       }
@@ -522,8 +506,6 @@ void TrafficAnnotationAuditor::CheckAnnotationsContents() {
       default:
         completing_annotations.push_back(&instance);
     }
-    if (!keep_annotation)
-      to_be_purged.insert(instance.unique_id_hash_code);
   }
 
   std::set<AnnotationInstance*> used_completing_annotations;
@@ -572,7 +554,6 @@ void TrafficAnnotationAuditor::CheckAnnotationsContents() {
     }
   }
 
-  PurgeAnnotations(to_be_purged);
   if (new_annotations.size())
     extracted_annotations_.insert(extracted_annotations_.end(),
                                   new_annotations.begin(),
@@ -581,6 +562,7 @@ void TrafficAnnotationAuditor::CheckAnnotationsContents() {
 
 bool TrafficAnnotationAuditor::RunAllChecks() {
   std::set<int> deprecated_ids;
+
   if (!TrafficAnnotationExporter(source_path_)
            .GetDeprecatedHashCodes(&deprecated_ids)) {
     return false;
@@ -596,5 +578,6 @@ bool TrafficAnnotationAuditor::RunAllChecks() {
     CheckAnnotationsContents();
 
   CheckAllRequiredFunctionsAreAnnotated();
+
   return true;
 }
