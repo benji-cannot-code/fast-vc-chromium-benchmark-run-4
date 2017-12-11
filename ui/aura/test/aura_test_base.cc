b4f3e2da2e29960a66550c7d50bdd826a06b9574
Sadrul Habib Chowdhury <sadrul@chromium.org>
2017-12-11 17:54:36 +0000

    mus/aura: Run tests where mus is hosting viz.
    The mus-backend for aura sometimes does things differently depending
    on whether mus is hosting viz or not. So run the unit-tests with mus
    hosting viz (which is the case when chrome is run with --mash), and
    also with mus not hosting viz (which is the case when chrome is run
    with --mus).
    
    Make sure context_fatory(), context_factory_private() are set up
    correctly when mus is not hosting viz.
    
    BUG=786453
    
    Change-Id: Icb968cb1fff386f174ffe798315fa7e2301aa341
    Reviewed-on: https://chromium-review.googlesource.com/818546
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523130}

--- ui/aura/test/aura_test_base.cc
+++ ui/aura/test/aura_test_base.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/aura/test/aura_test_base.h"
 
+#include "base/command_line.h"
 #include "base/memory/ptr_util.h"
 #include "ui/aura/client/window_parenting_client.h"
 #include "ui/aura/mus/property_utils.h"
@@ -16,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/ime/input_method_initializer.h"
 #include "ui/base/material_design/material_design_controller.h"
 #include "ui/base/test/material_design_controller_test_api.h"
+#include "ui/base/ui_base_switches.h"
+#include "ui/base/ui_base_switches_util.h"
 #include "ui/compositor/test/context_factories_for_test.h"
 #include "ui/events/event_dispatcher.h"
 #include "ui/events/event_sink.h"
@@ -81,7 +84,7 @@ void AuraTestBase::SetUp() {
   // The ContextFactory must exist before any Compositors are created.
   ui::ContextFactory* context_factory = nullptr;
   ui::ContextFactoryPrivate* context_factory_private = nullptr;
-  if (use_mus_) {
+  if (use_mus_ && switches::IsMusHostingViz()) {
     mus_context_factory_ = std::make_unique<AuraTestContextFactory>();
     context_factory = mus_context_factory_.get();
   } else {
@@ -146,8 +149,12 @@ void AuraTestBase::DeleteWindowTreeClient() {
 }
 
 void AuraTestBase::ConfigureBackend(BackendType type) {
-  if (type == BackendType::MUS)
+  if (type != BackendType::CLASSIC)
     EnableMusWithTestWindowTree();
+  if (type == BackendType::MUS_HOSTING_VIZ) {
+    base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
+        switches::kMus, switches::kMusHostVizValue);
+  }
 }
 
 void AuraTestBase::RunAllPendingInMessageLoop() {
--- ui/aura/window_unittest.cc
+++ ui/aura/window_unittest.cc
@@ -424,6 +424,8 @@ TEST_P(WindowTest, ContainsMouse) {
 
 // Tests that the root window gets a valid LocalSurfaceId.
 TEST_P(WindowTest, RootWindowHasValidLocalSurfaceId) {
+  if (GetParam() == BackendType::MUS_HOSTING_VIZ)
+    return;
   EXPECT_TRUE(root_window()->GetLocalSurfaceId().is_valid());
 }
 
@@ -3221,12 +3223,14 @@ TEST_P(WindowTest, LocalSurfaceIdChanges) {
 INSTANTIATE_TEST_CASE_P(/* no prefix */,
                         WindowTest,
                         ::testing::Values(BackendType::CLASSIC,
-                                          BackendType::MUS));
+                                          BackendType::MUS,
+                                          BackendType::MUS_HOSTING_VIZ));
 
 INSTANTIATE_TEST_CASE_P(/* no prefix */,
                         WindowObserverTest,
                         ::testing::Values(BackendType::CLASSIC,
-                                          BackendType::MUS));
+                                          BackendType::MUS,
+                                          BackendType::MUS_HOSTING_VIZ));
 
 }  // namespace test
 }  // namespace aura
