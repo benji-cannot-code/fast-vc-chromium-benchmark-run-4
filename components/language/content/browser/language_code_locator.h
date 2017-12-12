9c7c61ce04249142d2afcb3138f33b01c39d3920
Renjie Liu <renjieliu@chromium.org>
2017-12-12 00:40:18 +0000

    move language code locator to content.
    Move the language code locator util from components/language =>
    components/language/content/browser as it will only used by the
    GeoLanguageProvider.
    
    Bug: 76915
    Change-Id: I45695bf2c1149f89e7f6d13a41a5f80ea0084ba8
    Reviewed-on: https://chromium-review.googlesource.com/818596
    Commit-Queue: Renjie Liu <renjieliu@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523272}

--- components/language/content/browser/language_code_locator.h
+++ components/language/content/browser/language_code_locator.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef COMPONENTS_LANGUAGE_LANGUAGE_CODE_LOCATOR_H_
-#define COMPONENTS_LANGUAGE_LANGUAGE_CODE_LOCATOR_H_
+#ifndef COMPONENTS_LANGUAGE_CONTENT_BROWSER_LANGUAGE_CODE_LOCATOR_H_
+#define COMPONENTS_LANGUAGE_CONTENT_BROWSER_LANGUAGE_CODE_LOCATOR_H_
 
 #include <string>
 #include <vector>
@@ -33,4 +33,4 @@ class LanguageCodeLocator {
 
 }  // namespace language
 
-#endif  // COMPONENTS_LANGUAGE_LANGUAGE_CODE_LOCATOR_H_
+#endif  // COMPONENTS_LANGUAGE_CONTENT_BROWSER_LANGUAGE_CODE_LOCATOR_H_
