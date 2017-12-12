47dbe9cee7e9de8a2eb80f9328b632c40125e1b6
Theresa Wellington <twellington@google.com>
2017-12-12 21:46:16 +0000

    [Home] Add accessibility description to toolbar
    When the sheet is closed, add an accessibility description to the toolbar
    so that it provides useful information when focused during
    swipe-explore.
    
    BUG=748357
    
    Change-Id: Ie7fb7221b75e86fb19423455cfe8c3449201dc67
    Reviewed-on: https://chromium-review.googlesource.com/820593
    Commit-Queue: Theresa <twellington@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523563}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/BottomToolbarPhone.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/BottomToolbarPhone.java
@@ -68,6 +68,7 @@ public class BottomToolbarPhone extends ToolbarPhone {
             if (hasFocus() && !urlHasFocus()) mBottomSheet.requestFocus();
 
             mLocationBar.updateLoadingState(true);
+            updateContentDescription();
         }
 
         @Override
@@ -76,6 +77,7 @@ public class BottomToolbarPhone extends ToolbarPhone {
             updateMenuButtonClickableState();
 
             mLocationBar.updateLoadingState(true);
+            updateContentDescription();
         }
 
         @Override
@@ -372,12 +374,19 @@ public class BottomToolbarPhone extends ToolbarPhone {
             boolean delayAnimation, boolean animate) {
         super.setTabSwitcherMode(inTabSwitcherMode, showToolbar, delayAnimation, animate);
         mExpandButton.setClickable(!inTabSwitcherMode);
+        updateContentDescription();
 
         // Reset top shadow drawable state.
         if (inTabSwitcherMode) {
             mBottomToolbarTopShadowDrawable.getDrawable(0).setAlpha(255);
             mBottomToolbarTopShadowDrawable.getDrawable(1).setAlpha(0);
         }
+    }
+
+    @Override
+    protected void onTabSwitcherTransitionFinished() {
+        super.onTabSwitcherTransitionFinished();
+        updateContentDescription();
     }
 
     @Override
@@ -478,6 +487,8 @@ public class BottomToolbarPhone extends ToolbarPhone {
         mBottomSheet = sheet;
         getLocationBar().setBottomSheet(mBottomSheet);
         mBottomSheet.addObserver(mBottomSheetObserver);
+
+        updateContentDescription();
     }
 
     @Override
@@ -1144,5 +1155,14 @@ public class BottomToolbarPhone extends ToolbarPhone {
     private void updateMenuButtonClickableState() {
         mMenuButton.setClickable(
                 !urlHasFocus() && (!mBottomSheet.isSheetOpen() || mBottomSheet.isShowingNewTab()));
+    }
+
+    private void updateContentDescription() {
+        if (isInTabSwitcherMode() || mBottomSheet.isSheetOpen()) {
+            setContentDescription(null);
+        } else {
+            setContentDescription(
+                    getResources().getString(R.string.bottom_sheet_accessibility_toolbar));
+        }
     }
 }
