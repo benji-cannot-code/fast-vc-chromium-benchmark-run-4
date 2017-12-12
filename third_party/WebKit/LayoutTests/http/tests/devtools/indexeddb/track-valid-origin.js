772a5d0a9a16ffcb756f72244308d58ff28757ab
Will Chen <chenwilliam@chromium.org>
2017-12-12 19:58:32 +0000

    DevTools: migrate assorted tests to new integration test framework
    Part manual, part automated migration for these tests. Many of these weren't html
    tests and require special treatment (typically navigating the inspected page
    to a special resource file).
    
    Bug: 667560
    Change-Id: I70d5813346e2a59abf1f72390189c76563fbd023
    Reviewed-on: https://chromium-review.googlesource.com/820605
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Will Chen <chenwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523521}

---
+++ third_party/WebKit/LayoutTests/http/tests/devtools/indexeddb/track-valid-origin.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(async function() {
+  TestRunner.addResult(`Tests that IndexedDB live update only tracks valid security origins.\n`);
+  await TestRunner.loadModule('application_test_runner');
+
+  let indexedDBModel = TestRunner.mainTarget.model(Resources.IndexedDBModel);
+  let invalidOrigins = ['http', 'test://fake', 'test://fake.origin.com', 'chrome://test'];
+  let validOrigins = ['http://fake.origin.com', 'https://fake.origin.com'];
+
+  TestRunner.addResult('Invalid Origins:');
+  invalidOrigins.map(origin => {
+    TestRunner.addResult(origin + ', valid = ' + indexedDBModel._isValidSecurityOrigin(origin));
+  });
+  TestRunner.addResult('\nValid Origins:');
+  validOrigins.map(origin => {
+    TestRunner.addResult(origin + ', valid = ' + indexedDBModel._isValidSecurityOrigin(origin));
+  });
+  TestRunner.completeTest();
+})();
