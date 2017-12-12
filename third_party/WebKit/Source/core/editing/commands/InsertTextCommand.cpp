cefa3cdf0c3b717e0e76e81b88dd0898f01d45d2
Ryan Landay <rlanday@chromium.org>
2017-12-12 01:16:39 +0000

    Remove select_inserted_text param from Insert[Incremental]TextCommand
    After https://chromium-review.googlesource.com/c/chromium/src/+/801979, we no
    longer need the select_inserted_text functionality on either InsertTextCommand
    or InsertIncrementalTextCommand, so we can remove the parameter.
    
    Bug: 784039
    Change-Id: Icfb2688a83383cea9e295186f351b6f43ffadf6c
    Reviewed-on: https://chromium-review.googlesource.com/807286
    Commit-Queue: Ryan Landay <rlanday@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523288}

--- third_party/WebKit/Source/core/editing/commands/InsertTextCommand.cpp
+++ third_party/WebKit/Source/core/editing/commands/InsertTextCommand.cpp
@@ -41,11 +41,9 @@ namespace blink {
 
 InsertTextCommand::InsertTextCommand(Document& document,
                                      const String& text,
-                                     bool select_inserted_text,
                                      RebalanceType rebalance_type)
     : CompositeEditCommand(document),
       text_(text),
-      select_inserted_text_(select_inserted_text),
       rebalance_type_(rebalance_type) {}
 
 String InsertTextCommand::TextDataForInputEvent() const {
@@ -93,8 +91,7 @@ void InsertTextCommand::SetEndingSelectionWithoutValidation(
 
 // This avoids the expense of a full fledged delete operation, and avoids a
 // layout that typically results from text removal.
-bool InsertTextCommand::PerformTrivialReplace(const String& text,
-                                              bool select_inserted_text) {
+bool InsertTextCommand::PerformTrivialReplace(const String& text) {
   // We may need to manipulate neighboring whitespace if we're deleting text.
   // This case is tested in
   // InsertTextCommandTest_InsertEmptyTextAfterWhitespaceThatNeedsFixup.
@@ -113,8 +110,6 @@ bool InsertTextCommand::PerformTrivialReplace(const String& text,
     return false;
 
   SetEndingSelectionWithoutValidation(start, end_position);
-  if (select_inserted_text)
-    return true;
   SetEndingSelection(SelectionForUndoStep::From(
       SelectionInDOMTree::Builder()
           .Collapse(EndingVisibleSelection().End())
@@ -123,8 +118,7 @@ bool InsertTextCommand::PerformTrivialReplace(const String& text,
   return true;
 }
 
-bool InsertTextCommand::PerformOverwrite(const String& text,
-                                         bool select_inserted_text) {
+bool InsertTextCommand::PerformOverwrite(const String& text) {
   Position start = EndingVisibleSelection().Start();
   if (start.IsNull() || !start.IsOffsetInAnchor() ||
       !start.ComputeContainerNode()->IsTextNode())
@@ -143,7 +137,7 @@ bool InsertTextCommand::PerformOverwrite(const String& text,
   Position end_position =
       Position(text_node, start.OffsetInContainerNode() + text.length());
   SetEndingSelectionWithoutValidation(start, end_position);
-  if (select_inserted_text || EndingSelection().IsNone())
+  if (EndingSelection().IsNone())
     return true;
   SetEndingSelection(SelectionForUndoStep::From(
       SelectionInDOMTree::Builder()
@@ -166,7 +160,7 @@ void InsertTextCommand::DoApply(EditingState* editing_state) {
   // Delete the current selection.
   // FIXME: This delete operation blows away the typing style.
   if (EndingSelection().IsRange()) {
-    if (PerformTrivialReplace(text_, select_inserted_text_))
+    if (PerformTrivialReplace(text_))
       return;
     GetDocument().UpdateStyleAndLayoutIgnorePendingStylesheets();
     bool end_of_selection_was_at_start_of_block =
@@ -186,7 +180,7 @@ void InsertTextCommand::DoApply(EditingState* editing_state) {
         typing_style->RemoveBlockProperties();
     }
   } else if (GetDocument().GetFrame()->GetEditor().IsOverwriteModeEnabled()) {
-    if (PerformOverwrite(text_, select_inserted_text_))
+    if (PerformOverwrite(text_))
       return;
   }
 
@@ -301,15 +295,13 @@ void InsertTextCommand::DoApply(EditingState* editing_state) {
     }
   }
 
-  if (!select_inserted_text_) {
-    SelectionInDOMTree::Builder builder;
-    const VisibleSelection& selection = EndingVisibleSelection();
-    builder.SetAffinity(selection.Affinity());
-    builder.SetIsDirectional(EndingSelection().IsDirectional());
-    if (selection.End().IsNotNull())
-      builder.Collapse(selection.End());
-    SetEndingSelection(SelectionForUndoStep::From(builder.Build()));
-  }
+  SelectionInDOMTree::Builder builder;
+  const VisibleSelection& selection = EndingVisibleSelection();
+  builder.SetAffinity(selection.Affinity());
+  builder.SetIsDirectional(EndingSelection().IsDirectional());
+  if (selection.End().IsNotNull())
+    builder.Collapse(selection.End());
+  SetEndingSelection(SelectionForUndoStep::From(builder.Build()));
 }
 
 Position InsertTextCommand::InsertTab(const Position& pos,
