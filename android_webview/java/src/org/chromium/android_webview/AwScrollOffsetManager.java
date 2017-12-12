fa7e67ddb49f378fcc66d704872958bf05cae141
Jaebaek Seo <jaebaek@chromium.org>
2017-12-12 00:31:19 +0000

    Relocate select popup of Android WebView
    As shown in crbug.com/786852/#c9, the current select popup in Android WebView
    is not correctly located. The main reason is that the left margin for the anchor
    rectangle of the popup is added by x value of scroll manager. This CL adds y
    value of scroll manager to the left margin instead.
    
    Bug: 786852
    Change-Id: I8ebf983de64eea3c89d8fc7d2063962a7f1c7bb1
    Reviewed-on: https://chromium-review.googlesource.com/818679
    Reviewed-by: Tao Bai <michaelbai@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Jaebaek Seo <jaebaek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523268}

--- android_webview/java/src/org/chromium/android_webview/AwScrollOffsetManager.java
+++ android_webview/java/src/org/chromium/android_webview/AwScrollOffsetManager.java
@@ -249,6 +249,10 @@ public class AwScrollOffsetManager {
         mDelegate.scrollNativeTo(x, y);
     }
 
+    int getScrollX() {
+        return mNativeScrollX;
+    }
+
     int getScrollY() {
         return mNativeScrollY;
     }
