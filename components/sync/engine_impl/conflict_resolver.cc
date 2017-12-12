370ebfaeaacc50bae38c868a43e297b97e81c21a
Jens Widell <jl@opera.com>
2017-12-12 19:11:41 +0000

    Stop importing syncable::WriteTransaction into syncer namespace
    In jumbo/unity builds it conflicts with syncer::WriteTransaction defined
    in components/sync/syncable/write_transaction.h.
    
    Change-Id: I19a325af6299d955577aec10d36f21b948859035
    Reviewed-on: https://chromium-review.googlesource.com/822091
    Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    Commit-Queue: Jens Widell <jl@opera.com>
    Cr-Commit-Position: refs/heads/master@{#523502}

--- components/sync/engine_impl/conflict_resolver.cc
+++ components/sync/engine_impl/conflict_resolver.cc
@@ -27,7 +27,6 @@ using syncable::Directory;
 using syncable::Entry;
 using syncable::Id;
 using syncable::MutableEntry;
-using syncable::WriteTransaction;
 
 namespace {
 
@@ -66,7 +65,7 @@ ConflictResolver::ConflictResolver() {}
 
 ConflictResolver::~ConflictResolver() {}
 
-void ConflictResolver::ProcessSimpleConflict(WriteTransaction* trans,
+void ConflictResolver::ProcessSimpleConflict(syncable::WriteTransaction* trans,
                                              const Id& id,
                                              const Cryptographer* cryptographer,
                                              StatusController* status,
