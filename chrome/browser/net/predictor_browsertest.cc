7c2e6b43c9d04ec56a2a92247fdfbf5028b158e9
Charlie Harrison <csharrison@chromium.org>
2017-12-12 18:40:48 +0000

    Revert "Predictor: Dispatch all predictions async from the resource throttle"
    This reverts commit 24e730d2d5a98e1ec4fe01cf35f62f9d171e6503.
    
    Reason for revert: The delay navigation throttle will ruin data here.
    Let me re-land tomorrow.
    
    Original change's description:
    > Predictor: Dispatch all predictions async from the resource throttle
    > 
    > This is a small refactor that should have some performance wins. Now
    > that PlzNavigate has launched, we should be dispatching our preconnects
    > after the initial navigation request has gone out, not before. This is
    > because frequently (especially on Android), dispatching preconnects
    > tends to hog the IO thread, delaying the initial request.
    > 
    > Note that this predictor is probably going away, but this CPU work
    > is in the critical path of some other work I am planning on tackling.
    > 
    > Bug: 792524
    > Change-Id: I87e3c235be0480d766e3e45c9a97c4a26b15f60d
    > Reviewed-on: https://chromium-review.googlesource.com/821090
    > Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    > Reviewed-by: Matt Menke <mmenke@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#523473}
    
    TBR=mmenke@chromium.org,csharrison@chromium.org
    
    Change-Id: I596b40d4d3f527de7e0df6c00f376281a2dbe381
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 792524
    Reviewed-on: https://chromium-review.googlesource.com/823110
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523486}

--- chrome/browser/net/predictor_browsertest.cc
+++ chrome/browser/net/predictor_browsertest.cc
@@ -62,7 +62,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/url_request_test_job.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "url/gurl.h"
-#include "url/url_constants.h"
 
 using content::BrowserThread;
 using testing::HasSubstr;
@@ -1412,14 +1411,9 @@ IN_PROC_BROWSER_TEST_F(PredictorBrowserTest,
   EXPECT_EQ(0u, cross_site_connection_listener_->GetAcceptedSocketCount());
 
   // Now, navigate using a renderer initiated navigation and expect preconnects.
-  // Need to navigate to about:blank first so the referring site is different
-  // from the request site.
-  ui_test_utils::NavigateToURL(browser(), GURL(url::kAboutBlankURL));
   EXPECT_TRUE(content::ExecuteScript(
       browser()->tab_strip_model()->GetActiveWebContents(),
-      base::StringPrintf(
-          "window.location.href='%s'",
-          embedded_test_server()->GetURL("/title1.html").spec().c_str())));
+      "window.location.href='/title1.html'"));
 
   // The renderer initiated navigation is not synchronous, so just wait for the
   // preconnects to go through.
