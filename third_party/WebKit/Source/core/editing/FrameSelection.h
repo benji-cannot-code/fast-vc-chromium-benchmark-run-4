963ec00dd6f43984ff0378a0fc014b2cf45d7358
Yoichi Osato <yoichio@chromium.org>
2017-12-12 00:17:45 +0000

    [LayoutNG] Implement NG offset conversion and LayoutSelectionStartEndForNG().
    This patch is a part of crrev.com/c/813336
    ([LayoutNG] Paint text selection on load.)
    
    LayoutSelection.cpp:
    - This converts legacy offset to NG offset if LayoutObject has
    enclosing NGBlockFlowlayout before paint phase.
    - LayoutSelectionStartEndForNG() returns
    start/end offsets that the NGPhysicalTextFragment is selected.
    
    LayoutSelectionTest.cpp:
    - This patch adds NGLayoutSelectionTest to confirm/demonstrate
    how we use the function. Also you can refer my following
    painting patch(crrev.com/c/818884).
    
    Bug: 708452
    Change-Id: Ic9fc6822f83e0821c5f2f4917d4ac316543702e2
    Reviewed-on: https://chromium-review.googlesource.com/818864
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523263}

--- third_party/WebKit/Source/core/editing/FrameSelection.h
+++ third_party/WebKit/Source/core/editing/FrameSelection.h
@@ -50,6 +50,7 @@ class LocalFrame;
 class FrameCaret;
 class GranularityStrategy;
 class GraphicsContext;
+class NGPhysicalTextFragment;
 class Range;
 class SelectionEditor;
 class LayoutSelection;
@@ -227,6 +228,8 @@ class CORE_EXPORT FrameSelection final
   WTF::Optional<unsigned> LayoutSelectionStart() const;
   WTF::Optional<unsigned> LayoutSelectionEnd() const;
   void ClearLayoutSelection();
+  std::pair<unsigned, unsigned> LayoutSelectionStartEndForNG(
+      const NGPhysicalTextFragment&);
 
   void Trace(blink::Visitor*);
 
