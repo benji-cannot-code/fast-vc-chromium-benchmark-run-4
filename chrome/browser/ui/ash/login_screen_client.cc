9ebd3f0a63e93a6d8820e41e569e0787aa50dc8a
Sarah Hu <xiaoyinh@chromium.org>
2017-12-12 18:00:05 +0000

    Modify Autotest API to support views based lockscreen
    1. re-landing the change of
    https://chromium-review.googlesource.com/c/chromium/src/+/812264
    2. Add a mojo call IsAuthenticating to check the authentication state
    in ash lock screen.
    
    Bug: 781998
    Change-Id: I40a985cced8fe9cdaad404b068ffeacc669677a4
    Reviewed-on: https://chromium-review.googlesource.com/816022
    Commit-Queue: Xiaoyin Hu <xiaoyinh@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Jacob Dufault <jdufault@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523474}

--- chrome/browser/ui/ash/login_screen_client.cc
+++ chrome/browser/ui/ash/login_screen_client.cc
@@ -172,6 +172,11 @@ void LoginScreenClient::SetDevChannelInfo(
                                    bluetooth_name);
 }
 
+void LoginScreenClient::IsReadyForPassword(
+    ash::mojom::LoginScreen::IsReadyForPasswordCallback callback) {
+  login_screen_->IsReadyForPassword(std::move(callback));
+}
+
 void LoginScreenClient::SetDelegate(Delegate* delegate) {
   delegate_ = delegate;
 }
