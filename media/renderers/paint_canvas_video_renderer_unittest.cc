fc202aae46ea8042aa83eb940f9b4d8e31abd834
Brian Salomon <bsalomon@google.com>
2017-12-12 21:08:41 +0000

    Update GrContext creation to use newer factories.
    These factories don't require casting of GrGLInterface/GrVkBackendContext
    to GrBackendContext and return a sk_sp<GrContext> rather than a bare
    pointer.
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ie0673c724a9d1d853a571e68ad001ce491860e85
    Reviewed-on: https://chromium-review.googlesource.com/817677
    Commit-Queue: Brian Salomon <bsalomon@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: David Reveman <reveman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523547}

--- media/renderers/paint_canvas_video_renderer_unittest.cc
+++ media/renderers/paint_canvas_video_renderer_unittest.cc
@@ -631,9 +631,7 @@ void MailboxHoldersReleased(const gpu::SyncToken& sync_token) {}
 // abandoned.
 TEST_F(PaintCanvasVideoRendererTest, ContextLost) {
   sk_sp<const GrGLInterface> null_interface(GrGLCreateNullInterface());
-  sk_sp<GrContext> gr_context(GrContext::Create(
-      kOpenGL_GrBackend,
-      reinterpret_cast<GrBackendContext>(null_interface.get())));
+  sk_sp<GrContext> gr_context = GrContext::MakeGL(std::move(null_interface));
   gr_context->abandonContext();
 
   cc::SkiaPaintCanvas canvas(AllocBitmap(kWidth, kHeight));
@@ -661,9 +659,7 @@ TEST_F(PaintCanvasVideoRendererTest, CorrectFrameSizeToVisibleRect) {
       SkImageInfo::MakeN32(fWidth, fHeight, kOpaque_SkAlphaType);
 
   sk_sp<const GrGLInterface> glInterface(GrGLCreateNullInterface());
-  sk_sp<GrContext> grContext(
-      GrContext::Create(kOpenGL_GrBackend,
-                        reinterpret_cast<GrBackendContext>(glInterface.get())));
+  sk_sp<GrContext> grContext = GrContext::MakeGL(std::move(glInterface));
 
   sk_sp<SkSurface> surface =
       SkSurface::MakeRenderTarget(grContext.get(), SkBudgeted::kYes, imInfo);
