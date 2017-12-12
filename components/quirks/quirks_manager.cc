40a4ceb45a4191e85cb2dfe1b07639f57b7b639d
glevin <glevin@chromium.org>
2017-12-12 17:07:05 +0000

    Add network traffic annotations for Quirks
    Bug: 656607
    Change-Id: I0719514369f834d93049d0161d2e1d33b9713c87
    Reviewed-on: https://chromium-review.googlesource.com/817368
    Commit-Queue: Greg Levin <glevin@chromium.org>
    Reviewed-by: Greg Levin <glevin@chromium.org>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523459}

--- components/quirks/quirks_manager.cc
+++ components/quirks/quirks_manager.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/scoped_user_pref_update.h"
 #include "components/quirks/pref_names.h"
 #include "components/quirks/quirks_client.h"
+#include "net/traffic_annotation/network_traffic_annotation.h"
 #include "net/url_request/url_fetcher.h"
 #include "net/url_request/url_request_context_getter.h"
 #include "url/gurl.h"
@@ -158,7 +159,29 @@ std::unique_ptr<net::URLFetcher> QuirksManager::CreateURLFetcher(
   if (!fake_quirks_fetcher_creator_.is_null())
     return fake_quirks_fetcher_creator_.Run(url, delegate);
 
-  return net::URLFetcher::Create(url, net::URLFetcher::GET, delegate);
+  net::NetworkTrafficAnnotationTag traffic_annotation =
+      net::DefineNetworkTrafficAnnotation("quirks_display_fetcher", R"(
+          semantics {
+            sender: "Quirks"
+            description: "Download custom display calibration file."
+            trigger:
+                "Chrome OS attempts to download monitor calibration files on"
+                "first device login, and then once every 30 days."
+            data: "ICC files to calibrate and improve the quality of a display."
+            destination: GOOGLE_OWNED_SERVICE
+          }
+          policy {
+            cookies_allowed: NO
+            chrome_policy {
+              DeviceQuirksDownloadEnabled {
+                  DeviceQuirksDownloadEnabled: false
+              }
+            }
+          }
+        )");
+
+  return net::URLFetcher::Create(url, net::URLFetcher::GET, delegate,
+                                 traffic_annotation);
 }
 
 void QuirksManager::OnIccFilePathRequestCompleted(
