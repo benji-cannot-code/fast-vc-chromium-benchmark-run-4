2b1213b932b9c5deb560d056a067fde899c4a96f
DongJun Kim <djmix.kim@samsung.com>
2017-12-12 08:03:30 +0000

    Change function names in AXObject
    This patch changes function names for
    readability in AXObject as below.
    
    - Supports*** / CanSupportARIA*** -> SupportsARIA***
    
    Bug: none
    Change-Id: I77e9f717ce5ba1bfcb53c6152f57ce99fdda943a
    Reviewed-on: https://chromium-review.googlesource.com/821792
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523368}

--- third_party/WebKit/Source/modules/accessibility/AXNodeObject.cpp
+++ third_party/WebKit/Source/modules/accessibility/AXNodeObject.cpp
@@ -1010,7 +1010,7 @@ AXRestriction AXNodeObject::Restriction() const {
 
   // Check aria-readonly if supported by current role.
   bool is_read_only;
-  if (CanSupportAriaReadOnly() &&
+  if (SupportsARIAReadOnly() &&
       HasAOMPropertyOrARIAAttribute(AOMBooleanProperty::kReadOnly,
                                     is_read_only)) {
     // ARIA overrides other readonly state markup.
@@ -1418,7 +1418,7 @@ InvalidState AXNodeObject::GetInvalidState() const {
 }
 
 int AXNodeObject::PosInSet() const {
-  if (SupportsSetSizeAndPosInSet()) {
+  if (SupportsARIASetSizeAndPosInSet()) {
     uint32_t pos_in_set;
     if (HasAOMPropertyOrARIAAttribute(AOMUIntProperty::kPosInSet, pos_in_set))
       return pos_in_set;
@@ -1430,7 +1430,7 @@ int AXNodeObject::PosInSet() const {
 }
 
 int AXNodeObject::SetSize() const {
-  if (SupportsSetSizeAndPosInSet()) {
+  if (SupportsARIASetSizeAndPosInSet()) {
     int32_t set_size;
     if (HasAOMPropertyOrARIAAttribute(AOMIntProperty::kSetSize, set_size))
       return set_size;
