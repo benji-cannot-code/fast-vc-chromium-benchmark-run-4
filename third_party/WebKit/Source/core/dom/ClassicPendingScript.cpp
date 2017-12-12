390bc44b48a58dc28cb19fa69079c528b60d955d
Nate Chapin <japhet@chromium.org>
2017-12-12 21:59:22 +0000

    Have ScriptStreamerTest go through ClassicPendingScript::Fetch()
    This lets us get rid of the last non-trival usage of
    ResourceClient::SetResource in ClassicPendingScript
    
    Bug: 793028
    Change-Id: I39a4859cb1b941ff6a399fad88d40e353df4d462
    Reviewed-on: https://chromium-review.googlesource.com/822176
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523569}

--- third_party/WebKit/Source/core/dom/ClassicPendingScript.cpp
+++ third_party/WebKit/Source/core/dom/ClassicPendingScript.cpp
@@ -55,18 +55,6 @@ ClassicPendingScript* ClassicPendingScript::Fetch(
   return pending_script;
 }
 
-ClassicPendingScript* ClassicPendingScript::CreateExternalForTest(
-    ScriptElementBase* element,
-    ScriptResource* resource) {
-  DCHECK(resource);
-  ClassicPendingScript* pending_script = new ClassicPendingScript(
-      element, TextPosition(), ScriptSourceLocationType::kExternalFile,
-      ScriptFetchOptions(), true /* is_external */);
-  pending_script->SetResource(resource);
-  pending_script->CheckState();
-  return pending_script;
-}
-
 ClassicPendingScript* ClassicPendingScript::CreateInline(
     ScriptElementBase* element,
     const TextPosition& starting_position,
@@ -115,7 +103,7 @@ void ClassicPendingScript::Prefinalize() {
 
 void ClassicPendingScript::DisposeInternal() {
   MemoryCoordinator::Instance().UnregisterClient(this);
-  SetResource(nullptr);
+  ClearResource();
   integrity_failure_ = false;
   CancelStreaming();
 }
