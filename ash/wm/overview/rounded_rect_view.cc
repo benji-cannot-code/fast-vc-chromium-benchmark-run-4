5112de5f285b2c1f5ea0c3ed322a43a9704abd09
Sammie Quon <sammiequon@google.com>
2017-12-12 01:11:37 +0000

    splitview: Update split view indicators to match mocks.
    Window selector item indicator and drag indicators share some design
    so created split_view_constants.h to share constants.
    
    Animate the selector item indicator label, and the drag indicator
    widget opacity so they fade in.
    
    Change phantom windows to when selector item dragged over to drag
    indicator.
    
    mocks:
    https://gallery.googleplex.com/projects/MCHbtQVoQ2HCZQc4sf-S_quE/files/MCEpUArMVI4QsSaRnFNKc4g8wMSJbxAVNpw
    
    screenshots:
    https://screenshot.googleplex.com/D6yHyySYLhb
    https://screenshot.googleplex.com/zWwVzisZuPC
    https://screenshot.googleplex.com/ZiYRYhM6i62
    
    Test: manual, ash_unittests SplitViewWindowSelectorTest
    Bug: 786119
    Change-Id: Iedd5e9193e73c16f2d0b0c494a86e626b32705f6
    Reviewed-on: https://chromium-review.googlesource.com/779940
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523286}

--- ash/wm/overview/rounded_rect_view.cc
+++ ash/wm/overview/rounded_rect_view.cc
@@ -10,8 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
-RoundedRectView::RoundedRectView(int corner_radius, SkColor background)
-    : corner_radius_(corner_radius), background_(background) {}
+RoundedRectView::RoundedRectView(int corner_radius, SkColor background_color)
+    : corner_radius_(corner_radius), background_color_(background_color) {}
 
 RoundedRectView::~RoundedRectView() = default;
 
@@ -26,7 +26,15 @@ void RoundedRectView::OnPaint(gfx::Canvas* canvas) {
   path.addRoundRect(gfx::RectToSkRect(bounds), kRadius);
 
   canvas->ClipPath(path, true);
-  canvas->DrawColor(background_);
+  canvas->DrawColor(background_color_);
+}
+
+void RoundedRectView::SetBackgroundColor(SkColor background_color) {
+  if (background_color_ == background_color)
+    return;
+
+  background_color_ = background_color;
+  SchedulePaint();
 }
 
 }  // namespace ash
---
+++ ash/wm/splitview/split_view_utils.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_WM_SPLITVIEW_SPLIT_VIEW_UTILS_H_
+#define ASH_WM_SPLITVIEW_SPLIT_VIEW_UTILS_H_
+
+namespace ui {
+class Layer;
+}  // namespace ui
+
+namespace ash {
+
+// Animate |layer|'s opacity from 0 -> 1 if we are showing the layer, and
+// 1 -> 0 if we are hiding the layer.
+void AnimateSplitviewLabelOpacity(ui::Layer* layer, bool visible);
+
+}  // namespace ash
+
+#endif  // ASH_WM_SPLITVIEW_SPLIT_VIEW_UTILS_H_
