cb31374bf1127937d89a7a03a7c236b9cd0c77a5
Derek Cheng <imcheng@chromium.org>
2017-12-12 21:51:15 +0000

    [Media Router] Remove the EnableDialLocalDiscovery feature.
    The feature is default enabled in crrev.com/c/794715.
    
    Next step is to remove extension-side DIAL discovery logic.
    
    Bug: 718154
    Change-Id: I67569a9a4acf0a68c461307592b229c5b91e45d1
    Reviewed-on: https://chromium-review.googlesource.com/809925
    Commit-Queue: Derek Cheng <imcheng@chromium.org>
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523567}

--- chrome/browser/media/router/media_router_feature.cc
+++ chrome/browser/media/router/media_router_feature.cc
@@ -20,10 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace media_router {
 
 #if !defined(OS_ANDROID)
-// Controls if browser side DIAL device discovery is enabled.
-const base::Feature kEnableDialLocalDiscovery{"EnableDialLocalDiscovery",
-                                              base::FEATURE_ENABLED_BY_DEFAULT};
-
 // Controls if browser side Cast device discovery is enabled.
 const base::Feature kEnableCastDiscovery{"EnableCastDiscovery",
                                          base::FEATURE_DISABLED_BY_DEFAULT};
@@ -62,11 +58,6 @@ bool MediaRouterEnabled(content::BrowserContext* context) {
 }
 
 #if !defined(OS_ANDROID)
-// Returns true if browser side DIAL discovery is enabled.
-bool DialLocalDiscoveryEnabled() {
-  return base::FeatureList::IsEnabled(kEnableDialLocalDiscovery);
-}
-
 // Returns true if browser side Cast discovery is enabled.
 bool CastDiscoveryEnabled() {
   return base::FeatureList::IsEnabled(kEnableCastDiscovery);
@@ -76,7 +67,6 @@ bool CastDiscoveryEnabled() {
 bool CastLocalMediaEnabled() {
   return base::FeatureList::IsEnabled(kEnableCastLocalMedia);
 }
-
 #endif
 
 }  // namespace media_router
