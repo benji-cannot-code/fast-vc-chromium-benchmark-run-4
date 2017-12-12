d73dcb04b16b77619538c564f0ddd19040910a89
Eric Willigers <ericwilligers@chromium.org>
2017-12-12 08:41:21 +0000

    CSS Parser: Detect incomplete rgba colors
    args.Peek() - 1  is only valid if we are not at the end (or beginning)
    of the token stream.
    
    BUG=792464
    
    Change-Id: I1a475ce873723877d6bbb9d7b24c3773dd1090ee
    Reviewed-on: https://chromium-review.googlesource.com/821793
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523375}

--- third_party/WebKit/Source/core/css/parser/CSSPropertyParserTest.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSPropertyParserTest.cpp
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/css/parser/CSSPropertyParser.h"
 
+#include "core/css/CSSColorValue.h"
 #include "core/css/CSSValueList.h"
 #include "core/css/parser/CSSParser.h"
 #include "platform/testing/RuntimeEnabledFeaturesTestHelpers.h"
@@ -295,6 +296,21 @@ TEST(CSSPropertyParserTest, GridPositionLimit8) {
       StrictCSSParserContext(SecureContextMode::kSecureContext));
   DCHECK(value);
   EXPECT_EQ(GetGridPositionInteger(*value), -1000);
+}
+
+TEST(CSSPropertyParserTest, ColorFunction) {
+  const CSSValue* value = CSSParser::ParseSingleValue(
+      CSSPropertyBackgroundColor, "rgba(0, 0, 0, 1)",
+      StrictCSSParserContext(SecureContextMode::kSecureContext));
+  ASSERT_TRUE(value);
+  EXPECT_EQ(Color::kBlack, cssvalue::ToCSSColorValue(*value).Value());
+}
+
+TEST(CSSPropertyParserTest, IncompleteColor) {
+  const CSSValue* value = CSSParser::ParseSingleValue(
+      CSSPropertyBackgroundColor, "rgba(123 45",
+      StrictCSSParserContext(SecureContextMode::kSecureContext));
+  ASSERT_FALSE(value);
 }
 
 }  // namespace blink
