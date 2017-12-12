2905654b0bc03d7b9013e710fe474942fd3f2e45
Carlos Knippschild <carlosk@chromium.org>
2017-12-12 23:50:31 +0000

    Prefetching download task takes limitless mode into account.
    This change takes into account the "limitless prefetching" feature flag
    to choose to ignore the daily download quota and increase the concurrent
    downloads limit when it is enabled.
    
    Bug: 793109
    Change-Id: I6a3aea110976bcd1c2afac1dbfa86f06e0327b25
    Reviewed-on: https://chromium-review.googlesource.com/817705
    Commit-Queue: Carlos Knippschild <carlosk@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523608}

--- components/offline_pages/core/prefetch/download_archives_task.cc
+++ components/offline_pages/core/prefetch/download_archives_task.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "base/time/default_clock.h"
 #include "base/time/time.h"
+#include "components/offline_pages/core/offline_page_feature.h"
 #include "components/offline_pages/core/offline_store_utils.h"
 #include "components/offline_pages/core/prefetch/prefetch_downloader.h"
 #include "components/offline_pages/core/prefetch/prefetch_types.h"
@@ -87,11 +88,16 @@ std::unique_ptr<ItemsToDownload> SelectAndMarkItemsForDownloadSync(
   if (!transaction.Begin())
     return nullptr;
 
+  const int max_concurrent_downloads =
+      IsLimitlessPrefetchingEnabled()
+          ? DownloadArchivesTask::kMaxConcurrentDownloadsForLimitless
+          : DownloadArchivesTask::kMaxConcurrentDownloads;
+
   // Get current count of concurrent downloads and bail early if we are already
   // downloading more than we can.
   std::unique_ptr<int> concurrent_downloads(CountDownloadsInProgress(db));
   if (!concurrent_downloads ||
-      *concurrent_downloads >= DownloadArchivesTask::kMaxConcurrentDownloads) {
+      *concurrent_downloads >= max_concurrent_downloads) {
     return nullptr;
   }
 
@@ -103,7 +109,7 @@ std::unique_ptr<ItemsToDownload> SelectAndMarkItemsForDownloadSync(
   base::DefaultClock clock;
   PrefetchDownloaderQuota downloader_quota(db, &clock);
   int64_t available_quota = downloader_quota.GetAvailableQuotaBytes();
-  if (available_quota <= 0)
+  if (available_quota <= 0 && !IsLimitlessPrefetchingEnabled())
     return nullptr;
 
   ItemsToDownload ready_items = FindItemsReadyForDownload(db);
@@ -116,12 +122,16 @@ std::unique_ptr<ItemsToDownload> SelectAndMarkItemsForDownloadSync(
   auto items_to_download = base::MakeUnique<ItemsToDownload>();
   for (auto& ready_item : ready_items) {
     // Concurrent downloads check.
-    if (*concurrent_downloads >= DownloadArchivesTask::kMaxConcurrentDownloads)
+    if (*concurrent_downloads >= max_concurrent_downloads)
       break;
 
-    // Quota check. Skips all items that violate quota.
-    if (ready_item.archive_body_length > available_quota)
+    // Quota check. Skips all items that violate quota if limitless prefetching
+    // is disabled. If it is enabled then always proceed but still decrement the
+    // quota allowing it to become negative.
+    if (ready_item.archive_body_length > available_quota &&
+        !IsLimitlessPrefetchingEnabled()) {
       continue;
+    }
     available_quota -= ready_item.archive_body_length;
 
     // Explicitly not reusing the GUID from the last archive download attempt
@@ -134,7 +144,8 @@ std::unique_ptr<ItemsToDownload> SelectAndMarkItemsForDownloadSync(
     ++(*concurrent_downloads);
   }
 
-  // Write new remaining quota with date here.
+  // Write new remaining quota, limited to a minimum of 0.
+  available_quota = available_quota < 0 ? 0 : available_quota;
   if (!downloader_quota.SetAvailableQuotaBytes(available_quota))
     return nullptr;
 
@@ -148,6 +159,13 @@ std::unique_ptr<ItemsToDownload> SelectAndMarkItemsForDownloadSync(
 
 // static
 const int DownloadArchivesTask::kMaxConcurrentDownloads = 2;
+
+// This value matches the maximum number of concurrent downloads allowed by the
+// downloads service.
+// TODO(https://crbug.com/793158): obtain this value directly from the downloads
+// service once it is exposed.
+// static
+const int DownloadArchivesTask::kMaxConcurrentDownloadsForLimitless = 4;
 
 DownloadArchivesTask::DownloadArchivesTask(
     PrefetchStore* prefetch_store,
