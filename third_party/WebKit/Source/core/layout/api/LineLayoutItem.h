c9a6529a0ead7d2f61ee501000b0f445d4dfd863
Xianzhu Wang <wangxianzhu@chromium.org>
2017-12-12 16:59:25 +0000

    [SPv175+] Support partial invalidation for selections
    Bug: 732612
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I685af535011500633865953799a6c4ae128ab5e8
    Reviewed-on: https://chromium-review.googlesource.com/820043
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523454}

--- third_party/WebKit/Source/core/layout/api/LineLayoutItem.h
+++ third_party/WebKit/Source/core/layout/api/LineLayoutItem.h
@@ -295,6 +295,9 @@ class LineLayoutItem {
   }
 
   LayoutRect VisualRect() const { return layout_object_->VisualRect(); }
+  LayoutRect PartialInvalidationRect() const {
+    return layout_object_->PartialInvalidationRect();
+  }
 
   bool IsHashTableDeletedValue() const {
     return layout_object_ == kHashTableDeletedValue;
