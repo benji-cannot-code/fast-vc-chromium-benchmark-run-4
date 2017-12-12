9f57d67300be760f74f1efd116ef9b496c75a28d
Ahmed Fakhry <afakhry@google.com>
2017-12-12 17:42:17 +0000

    Night Light Rendering [Step 1.3]: Hook up to the UI compositor
    This is the final CL in step 1. This CL removes all color temperature
    code from the UI layer, and hooks up the night light controller to the
    newly added display color matrix in the UI compositor.
    
    BUG=749250
    TEST=covered by tests
    
    Change-Id: Ia0263d0e530508c5820a3c8332b00228bff7ec38
    Reviewed-on: https://chromium-review.googlesource.com/820173
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523467}

--- ui/compositor/layer_animation_delegate.h
+++ ui/compositor/layer_animation_delegate.h
@@ -39,8 +39,6 @@ class COMPOSITOR_EXPORT LayerAnimationDelegate {
                                          PropertyChangeReason reason) = 0;
   virtual void SetColorFromAnimation(SkColor color,
                                      PropertyChangeReason reason) = 0;
-  virtual void SetTemperatureFromAnimation(float temperature,
-                                           PropertyChangeReason reason) = 0;
   virtual void ScheduleDrawForAnimation() = 0;
   virtual const gfx::Rect& GetBoundsForAnimation() const = 0;
   virtual gfx::Transform GetTransformForAnimation() const = 0;
@@ -49,7 +47,6 @@ class COMPOSITOR_EXPORT LayerAnimationDelegate {
   virtual float GetBrightnessForAnimation() const = 0;
   virtual float GetGrayscaleForAnimation() const = 0;
   virtual SkColor GetColorForAnimation() const = 0;
-  virtual float GetTemperatureFromAnimation() const = 0;
   virtual float GetDeviceScaleFactor() const = 0;
   virtual ui::Layer* GetLayer() = 0;
   virtual cc::Layer* GetCcLayer() const = 0;
--- ui/compositor/test/test_layer_animation_delegate.h
+++ ui/compositor/test/test_layer_animation_delegate.h
@@ -55,8 +55,6 @@ class TestLayerAnimationDelegate : public LayerAnimationDelegate {
                                  PropertyChangeReason reason) override;
   void SetColorFromAnimation(SkColor color,
                              PropertyChangeReason reason) override;
-  void SetTemperatureFromAnimation(float temperature,
-                                   PropertyChangeReason reason) override;
   void ScheduleDrawForAnimation() override;
   const gfx::Rect& GetBoundsForAnimation() const override;
   gfx::Transform GetTransformForAnimation() const override;
@@ -65,7 +63,6 @@ class TestLayerAnimationDelegate : public LayerAnimationDelegate {
   float GetBrightnessForAnimation() const override;
   float GetGrayscaleForAnimation() const override;
   SkColor GetColorForAnimation() const override;
-  float GetTemperatureFromAnimation() const override;
   float GetDeviceScaleFactor() const override;
   LayerAnimatorCollection* GetLayerAnimatorCollection() override;
   ui::Layer* GetLayer() override;
@@ -90,7 +87,6 @@ class TestLayerAnimationDelegate : public LayerAnimationDelegate {
   float brightness_;
   float grayscale_;
   SkColor color_;
-  float temperature_;
   scoped_refptr<cc::Layer> cc_layer_;
 
   // Allow copy and assign.
