cd07a0053a3a6b2968f9fd227e262079d65b9332
Tarun Bansal <tbansal@chromium.org>
2017-12-12 17:14:26 +0000

    Do not restart probe requests to data saver proxy
    Probe requests (i.e., warmup URL fetches) may be
    intercepted and restarted by the data reduction proxy
    component. This CL prevents the probe requests from
    being restarted.
    
    Bug: 760294,792841
    Change-Id: Iee54a9dce507d48121e71c58ff2079b9b1b67f1b
    Reviewed-on: https://chromium-review.googlesource.com/818385
    Reviewed-by: Doug Arnett <dougarnett@chromium.org>
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523463}

--- components/data_reduction_proxy/core/browser/data_reduction_proxy_interceptor.cc
+++ components/data_reduction_proxy/core/browser/data_reduction_proxy_interceptor.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/data_reduction_proxy/core/browser/data_reduction_proxy_config_service_client.h"
 #include "components/data_reduction_proxy/core/common/data_reduction_proxy_event_creator.h"
 #include "components/data_reduction_proxy/core/common/data_reduction_proxy_headers.h"
+#include "components/data_reduction_proxy/core/common/data_reduction_proxy_params.h"
 #include "net/base/load_timing_info.h"
 #include "net/http/http_request_headers.h"
 #include "net/http/http_response_headers.h"
@@ -70,6 +71,15 @@ DataReductionProxyInterceptor::MaybeInterceptResponseOrRedirect(
   DCHECK(request);
   if (request->response_info().was_cached)
     return nullptr;
+
+  const GURL& warmup_url = params::GetWarmupURL();
+  if (request->url().host() == warmup_url.host() &&
+      request->url().path() == warmup_url.path()) {
+    // No need to retry fetch of warmup URLs since it is useful to fetch the
+    // warmup URL only via a data saver proxy.
+    return nullptr;
+  }
+
   bool should_retry = false;
   // Consider retrying due to an authentication failure from the Data Reduction
   // Proxy server when using the config service.
