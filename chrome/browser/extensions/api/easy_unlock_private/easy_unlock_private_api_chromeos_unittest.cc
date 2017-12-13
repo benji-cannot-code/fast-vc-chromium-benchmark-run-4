2546b395d7e2ab169a5bcf616f418f11f59968c0
Scott Violet <sky@chromium.org>
2017-12-13 05:27:16 +0000

    chromeos: get EasyUnlockPrivateApiTests working with mus
    Creating BluezDBusManager is unnecessary for --mus, as
    WindowManagerService (in ash) takes care of it.
    
    BUG=776514
    TEST=test only change
    
    Change-Id: Ie3b7e39a507a9263779ce8f1045141e51d81192f
    Reviewed-on: https://chromium-review.googlesource.com/823526
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523697}

--- chrome/browser/extensions/api/easy_unlock_private/easy_unlock_private_api_chromeos_unittest.cc
+++ chrome/browser/extensions/api/easy_unlock_private/easy_unlock_private_api_chromeos_unittest.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/test_event_router.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/cros_system_api/dbus/service_constants.h"
+#include "ui/aura/env.h"
 
 namespace {
 
@@ -131,9 +132,11 @@ class EasyUnlockPrivateApiTest : public extensions::ExtensionApiUnittest {
         proximity_auth::switches::kDisableBluetoothLowEnergyDiscovery);
 
     chromeos::DBusThreadManager::Initialize();
-    bluez::BluezDBusManager::Initialize(
-        chromeos::DBusThreadManager::Get()->GetSystemBus(),
-        chromeos::DBusThreadManager::Get()->IsUsingFakes());
+    if (aura::Env::GetInstance()->mode() == aura::Env::Mode::LOCAL) {
+      bluez::BluezDBusManager::Initialize(
+          chromeos::DBusThreadManager::Get()->GetSystemBus(),
+          chromeos::DBusThreadManager::Get()->IsUsingFakes());
+    }
     client_ = chromeos::DBusThreadManager::Get()->GetEasyUnlockClient();
 
     extensions::ExtensionApiUnittest::SetUp();
@@ -142,7 +145,8 @@ class EasyUnlockPrivateApiTest : public extensions::ExtensionApiUnittest {
   void TearDown() override {
     extensions::ExtensionApiUnittest::TearDown();
 
-    bluez::BluezDBusManager::Shutdown();
+    if (aura::Env::GetInstance()->mode() == aura::Env::Mode::LOCAL)
+      bluez::BluezDBusManager::Shutdown();
     chromeos::DBusThreadManager::Shutdown();
   }
 
