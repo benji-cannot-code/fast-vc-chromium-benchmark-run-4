2344b4a8e20310092c6170e755f05921089b6cc7
Daniel Bratell <bratell@opera.com>
2017-12-13 10:50:35 +0000

    Give some ServiceWorker and TLS related symbols more unique names
    In jumbo builds more code compile in the same translation unit
    so it's more important to keep the names unique to avoid
    compilation clashes.
    
    Bug: 746953
    Change-Id: I649731d0725a9c5b5ab0ec72775cc81ea54434a9
    Reviewed-on: https://chromium-review.googlesource.com/819556
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Commit-Queue: Daniel Bratell <bratell@opera.com>
    Cr-Commit-Position: refs/heads/master@{#523737}

--- content/renderer/service_worker/service_worker_dispatcher.cc
+++ content/renderer/service_worker/service_worker_dispatcher.cc
@@ -39,7 +39,8 @@ namespace {
 base::LazyInstance<ThreadLocalPointer<void>>::Leaky g_dispatcher_tls =
     LAZY_INSTANCE_INITIALIZER;
 
-void* const kHasBeenDeleted = reinterpret_cast<void*>(0x1);
+void* const kDeletedServiceWorkerDispatcherMarker =
+    reinterpret_cast<void*>(0x1);
 
 }  // namespace
 
@@ -56,7 +57,7 @@ ServiceWorkerDispatcher::~ServiceWorkerDispatcher() {
     g_dispatcher_tls.Pointer()->Set(nullptr);
     return;
   }
-  g_dispatcher_tls.Pointer()->Set(kHasBeenDeleted);
+  g_dispatcher_tls.Pointer()->Set(kDeletedServiceWorkerDispatcherMarker);
 }
 
 void ServiceWorkerDispatcher::OnMessageReceived(const IPC::Message& msg) {
@@ -77,7 +78,8 @@ ServiceWorkerDispatcher*
 ServiceWorkerDispatcher::GetOrCreateThreadSpecificInstance(
     scoped_refptr<ThreadSafeSender> thread_safe_sender,
     scoped_refptr<base::SingleThreadTaskRunner> main_thread_task_runner) {
-  if (g_dispatcher_tls.Pointer()->Get() == kHasBeenDeleted) {
+  if (g_dispatcher_tls.Pointer()->Get() ==
+      kDeletedServiceWorkerDispatcherMarker) {
     NOTREACHED() << "Re-instantiating TLS ServiceWorkerDispatcher.";
     g_dispatcher_tls.Pointer()->Set(nullptr);
   }
@@ -93,7 +95,8 @@ ServiceWorkerDispatcher::GetOrCreateThreadSpecificInstance(
 }
 
 ServiceWorkerDispatcher* ServiceWorkerDispatcher::GetThreadSpecificInstance() {
-  if (g_dispatcher_tls.Pointer()->Get() == kHasBeenDeleted)
+  if (g_dispatcher_tls.Pointer()->Get() ==
+      kDeletedServiceWorkerDispatcherMarker)
     return nullptr;
   return static_cast<ServiceWorkerDispatcher*>(
       g_dispatcher_tls.Pointer()->Get());
