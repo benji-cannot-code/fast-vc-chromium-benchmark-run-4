8a1d293eee2865b66d75766754717e5b13cf9233
Michael Thiessen <mthiesse@chromium.org>
2017-12-13 23:58:55 +0000

    Delete some unused CompositorView/Holder code.
    Bug: 
    Change-Id: Ia72a555a03f5eb6a4b9ec9f5639f137c1ac60450
    Reviewed-on: https://chromium-review.googlesource.com/826162
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523939}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/CompositorViewHolder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/CompositorViewHolder.java
@@ -57,7 +57,6 @@ import org.chromium.content.browser.ContentView;
 import org.chromium.content.browser.ContentViewCore;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.ui.UiUtils;
-import org.chromium.ui.base.DeviceFormFactor;
 import org.chromium.ui.base.SPenSupport;
 import org.chromium.ui.base.WindowAndroid;
 import org.chromium.ui.resources.ResourceManager;
@@ -112,7 +111,6 @@ public class CompositorViewHolder extends FrameLayout
 
     private TabModelSelectorObserver mTabModelSelectorObserver;
     private TabObserver mTabObserver;
-    private boolean mEnableCompositorTabStrip;
 
     // Cache objects that should not be created frequently.
     private final RectF mCacheViewport = new RectF();
@@ -221,8 +219,6 @@ public class CompositorViewHolder extends FrameLayout
                 initializeTab(tab);
             }
         };
-
-        mEnableCompositorTabStrip = DeviceFormFactor.isTablet();
 
         addOnLayoutChangeListener(new OnLayoutChangeListener() {
             @Override
@@ -589,16 +585,7 @@ public class CompositorViewHolder extends FrameLayout
         TraceEvent.begin("CompositorViewHolder:layout");
         if (mLayoutManager != null) {
             mLayoutManager.onUpdate();
-
-            if (!DeviceFormFactor.isTablet() && mControlContainer != null) {
-                if (mProgressBarDrawingInfo == null) mProgressBarDrawingInfo = new DrawingInfo();
-                mControlContainer.getProgressBarDrawingInfo(mProgressBarDrawingInfo);
-            } else {
-                assert mProgressBarDrawingInfo == null;
-            }
-
-            mCompositorView.finalizeLayers(mLayoutManager, false,
-                    mProgressBarDrawingInfo);
+            mCompositorView.finalizeLayers(mLayoutManager, false);
         }
 
         TraceEvent.end("CompositorViewHolder:layout");
