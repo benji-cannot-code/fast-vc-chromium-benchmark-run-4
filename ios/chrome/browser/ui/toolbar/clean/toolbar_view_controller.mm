109bf0610e62fd017c27303c0bc486bf4c91bf22
stkhapugin@chromium.org <stkhapugin@chromium.org>
2017-12-13 10:47:55 +0000

    Make location bar clip to bounds.
    Makes the location bar clip to bounds.
    
    Bug: 791068
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I59eff7def75de42d7e75b8bac87d0ada726a61ba
    Reviewed-on: https://chromium-review.googlesource.com/822255
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523736}

--- ios/chrome/browser/ui/toolbar/clean/toolbar_view_controller.mm
+++ ios/chrome/browser/ui/toolbar/clean/toolbar_view_controller.mm
@@ -939,6 +939,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     [_locationBarView
         setContentHuggingPriority:UILayoutPriorityDefaultLow
                           forAxis:UILayoutConstraintAxisHorizontal];
+    _locationBarView.clipsToBounds = YES;
   }
   return _locationBarView;
 }
