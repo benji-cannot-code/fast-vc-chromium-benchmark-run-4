365521a13cb13b45ffd36b722e9d6ada2be624cf
Sammie Quon <sammiequon@google.com>
2017-12-13 10:41:42 -0800

    cros: Add window icons to overview mode.
    Fetch window icons before entering overview.
    Add logo to tabbed chrome windows as window icon.
    Add a switch to hide this and future overview
    mode work behind.
    
    Test: manual
    Bug: 782320
    Change-Id: I46e02e33aa0ca378f19424f276c7cbcb37e649d5
    Reviewed-on: https://chromium-review.googlesource.com/813200
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527182}

--- chrome/browser/ui/views/frame/browser_non_client_frame_view_ash.cc
+++ chrome/browser/ui/views/frame/browser_non_client_frame_view_ash.cc
@@ -352,6 +352,13 @@ void BrowserNonClientFrameViewAsh::ChildPreferredSizeChanged(
 // ash::ShellObserver:
 
 void BrowserNonClientFrameViewAsh::OnOverviewModeStarting() {
+  // Update the window icon so that overview mode can grab the icon from
+  // aura::client::kWindowIcon to display.
+  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
+          ash::switches::kAshEnableNewOverviewUi)) {
+    frame()->UpdateWindowIcon();
+  }
+
   frame()->GetNativeWindow()->SetProperty(aura::client::kTopViewColor,
                                           GetFrameColor());
   OnOverviewModeChanged(true);
