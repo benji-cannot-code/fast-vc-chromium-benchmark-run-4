278156583aeb44495cb370e424bdfd762f131e4d
kylechar <kylechar@chromium.org>
2017-12-13 04:20:26 +0000

    Turn on surface sync with --enable-viz.
    Always turn on surface synchronization when the --enable-viz flag is
    present. Disable a few extra tests which now fail in the new
    configuration.
    
    The --enable-surface-synchronization flag was added to an FYI trybot to
    check for failures. No failures were observed over the weekend. Remove
    the now unneeded flag from the FYI trybot.
    
    Bug: 793302
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I6bc0aff21d968ba5ee38f7453fbd7b5779efb34a
    Reviewed-on: https://chromium-review.googlesource.com/812409
    Commit-Queue: kylechar <kylechar@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Fady Samuel <fsamuel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523685}

--- components/viz/common/features.cc
+++ components/viz/common/features.cc
@@ -15,9 +15,10 @@ const base::Feature kEnableSurfaceSynchronization{
     "SurfaceSynchronization", base::FEATURE_DISABLED_BY_DEFAULT};
 
 bool IsSurfaceSynchronizationEnabled() {
+  auto* command_line = base::CommandLine::ForCurrentProcess();
   return base::FeatureList::IsEnabled(kEnableSurfaceSynchronization) ||
-         base::CommandLine::ForCurrentProcess()->HasSwitch(
-             switches::kEnableSurfaceSynchronization);
+         command_line->HasSwitch(switches::kEnableSurfaceSynchronization) ||
+         command_line->HasSwitch(switches::kEnableViz);
 }
 
 }  // namespace features
