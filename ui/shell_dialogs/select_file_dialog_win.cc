fbd65fdf159ec5f2f4bedd1bc3d28a04e03393a2
Eric Lawrence <elawrence@chromium.org>
2017-12-13 23:58:50 +0000

    Set Folder Upload prompt's button text
    On Windows, the folder upload prompt previously used "OK" as the default
    button text, leading to the possibility that the user would not
    understand that their selected folder would be uploaded to the server.
    
    This CL matches other platforms and uses a localized string ("Upload")
    as the text of the prompt's commit button.
    
    Bug: 780881
    Change-Id: I256d42fc8ba3fdd72d5af360585509b6573b7900
    Reviewed-on: https://chromium-review.googlesource.com/777681
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Eric Lawrence <elawrence@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523938}

--- ui/shell_dialogs/select_file_dialog_win.cc
+++ ui/shell_dialogs/select_file_dialog_win.cc
@@ -10,8 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wrl/client.h>
 
 #include <algorithm>
+#include <memory>
 #include <set>
 #include <tuple>
+#include <utility>
+#include <vector>
 
 #include "base/bind.h"
 #include "base/files/file_path.h"
@@ -224,6 +227,11 @@ class SelectFileDialogImpl : public ui::SelectFileDialog,
     void* params;
   };
 
+  struct SelectFolderDialogOptions {
+    const wchar_t* default_path;
+    bool is_upload;
+  };
+
   // Shows the file selection dialog modal to |owner| and calls the result
   // back on the ui thread. Run on the dialog thread.
   void ExecuteSelectFile(const ExecuteSelectParams& params);
@@ -266,11 +274,9 @@ class SelectFileDialogImpl : public ui::SelectFileDialog,
 
   // Runs a Folder selection dialog box, passes back the selected folder in
   // |path| and returns true if the user clicks OK. If the user cancels the
-  // dialog box the value in |path| is not modified and returns false. |title|
-  // is the user-supplied title text to show for the dialog box. Run on the
-  // dialog thread.
-  bool RunSelectFolderDialog(const std::wstring& title,
-                             HWND owner,
+  // dialog box the value in |path| is not modified and returns false. Run
+  // on the dialog thread.
+  bool RunSelectFolderDialog(const ExecuteSelectParams& params,
                              base::FilePath* path);
 
   // Runs an Open file dialog box, with similar semantics for input paramaters
@@ -368,15 +374,7 @@ void SelectFileDialogImpl::ExecuteSelectFile(
   bool success = false;
   unsigned filter_index = params.file_type_index;
   if (params.type == SELECT_FOLDER || params.type == SELECT_UPLOAD_FOLDER) {
-    std::wstring title = params.title;
-    if (title.empty() && params.type == SELECT_UPLOAD_FOLDER) {
-      // If it's for uploading don't use default dialog title to
-      // make sure we clearly tell it's for uploading.
-      title = l10n_util::GetStringUTF16(IDS_SELECT_UPLOAD_FOLDER_DIALOG_TITLE);
-    }
-    success = RunSelectFolderDialog(title,
-                                    params.run_state.owner,
-                                    &path);
+    success = RunSelectFolderDialog(params, &path);
   } else if (params.type == SELECT_SAVEAS_FILE) {
     std::wstring path_as_wstring = path.value();
     success = SaveFileAsWithFilter(params.run_state.owner,
@@ -516,35 +514,60 @@ int CALLBACK SelectFileDialogImpl::BrowseCallbackProc(HWND window,
                                                       LPARAM parameter,
                                                       LPARAM data) {
   if (message == BFFM_INITIALIZED) {
-    // WParam is TRUE since passing a path.
-    // data lParam member of the BROWSEINFO structure.
-    SendMessage(window, BFFM_SETSELECTION, TRUE, (LPARAM)data);
+    SelectFolderDialogOptions* options =
+        reinterpret_cast<SelectFolderDialogOptions*>(data);
+    if (options->is_upload) {
+      SendMessage(window, BFFM_SETOKTEXT, 0,
+                  reinterpret_cast<LPARAM>(
+                      l10n_util::GetStringUTF16(
+                          IDS_SELECT_UPLOAD_FOLDER_DIALOG_UPLOAD_BUTTON)
+                          .c_str()));
+    }
+    if (options->default_path) {
+      SendMessage(window, BFFM_SETSELECTION, TRUE,
+                  reinterpret_cast<LPARAM>(options->default_path));
+    }
   }
   return 0;
 }
 
-bool SelectFileDialogImpl::RunSelectFolderDialog(const std::wstring& title,
-                                                 HWND owner,
-                                                 base::FilePath* path) {
+bool SelectFileDialogImpl::RunSelectFolderDialog(
+    const ExecuteSelectParams& params,
+    base::FilePath* path) {
   DCHECK(path);
+  std::wstring title = params.title;
+  if (title.empty() && params.type == SELECT_UPLOAD_FOLDER) {
+    // If it's for uploading don't use default dialog title to
+    // make sure we clearly tell it's for uploading.
+    title = l10n_util::GetStringUTF16(IDS_SELECT_UPLOAD_FOLDER_DIALOG_TITLE);
+  }
 
   wchar_t dir_buffer[MAX_PATH + 1];
 
   bool result = false;
   BROWSEINFO browse_info = {0};
-  browse_info.hwndOwner = owner;
+  browse_info.hwndOwner = params.run_state.owner;
   browse_info.lpszTitle = title.c_str();
   browse_info.pszDisplayName = dir_buffer;
   browse_info.ulFlags = BIF_USENEWUI | BIF_RETURNONLYFSDIRS;
 
-  if (path->value().length()) {
-    // Highlight the current value.
-    browse_info.lParam = (LPARAM)path->value().c_str();
+  // If uploading or a default path was provided, update the BROWSEINFO
+  // and set the callback function for the dialog so the strings can be set in
+  // the callback.
+  SelectFolderDialogOptions dialog_options = {0};
+  if (path->value().length())
+    dialog_options.default_path = path->value().c_str();
+  if (params.type == SELECT_UPLOAD_FOLDER) {
+    dialog_options.is_upload = true;
+    browse_info.ulFlags |= BIF_NONEWFOLDERBUTTON;
+  }
+  if (dialog_options.is_upload || dialog_options.default_path) {
+    browse_info.lParam = reinterpret_cast<LPARAM>(&dialog_options);
     browse_info.lpfn = &BrowseCallbackProc;
   }
 
   LPITEMIDLIST list = SHBrowseForFolder(&browse_info);
-  DisableOwner(owner);
+  DisableOwner(params.run_state.owner);
   if (list) {
     STRRET out_dir_buffer;
     ZeroMemory(&out_dir_buffer, sizeof(out_dir_buffer));
