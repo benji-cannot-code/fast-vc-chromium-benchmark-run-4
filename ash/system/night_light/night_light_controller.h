72e9d61286c21294b27f4275f90b3fff317856d4
Ahmed Fakhry <afakhry@google.com>
2017-12-13 17:50:27 +0000

    Night Light: Fix a bug in how the schedule is refreshed.
    - There was a bug when the start and end times are inverted
      (i.e. start time is greater than end time as a time of day
       today).
    - The schedule should refresh when device resumes from sleep
      since time will have changed while the device was suspended.
    
    BUG=768902
    TEST=Added new tests
    
    Change-Id: I105085e36e9a68db97506e69286b471b8a738731
    Reviewed-on: https://chromium-review.googlesource.com/821413
    Reviewed-by: Dan Erat <derat@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523807}

--- ash/system/night_light/night_light_controller.h
+++ ash/system/night_light/night_light_controller.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
+#include "chromeos/dbus/power_manager_client.h"
 #include "components/prefs/pref_change_registrar.h"
 #include "mojo/public/cpp/bindings/binding.h"
 
@@ -28,9 +29,11 @@ class ColorTemperatureAnimation;
 
 // Controls the NightLight feature that adjusts the color temperature of the
 // screen.
-class ASH_EXPORT NightLightController : public mojom::NightLightController,
-                                        public WindowTreeHostManager::Observer,
-                                        public SessionObserver {
+class ASH_EXPORT NightLightController
+    : public mojom::NightLightController,
+      public WindowTreeHostManager::Observer,
+      public SessionObserver,
+      public chromeos::PowerManagerClient::Observer {
  public:
   using ScheduleType = mojom::NightLightController::ScheduleType;
 
@@ -125,6 +128,9 @@ class ASH_EXPORT NightLightController : public mojom::NightLightController,
   // ash::mojom::NightLightController:
   void SetCurrentGeoposition(mojom::SimpleGeopositionPtr position) override;
   void SetClient(mojom::NightLightClientPtr client) override;
+
+  // chromeos::PowerManagerClient::Observer:
+  void SuspendDone(const base::TimeDelta& sleep_duration) override;
 
   void SetDelegateForTesting(std::unique_ptr<Delegate> delegate);
 
