2c42c33411c7274bc88c35352a3ff37134d4c4e2
Jacob Dufault <jdufault@google.com>
2017-12-13 00:02:35 +0000

    cros: Accessibility fixes for views-based lock.
    - Allow ChromeVox to navigate to dropdown arrow using Search+Arrow keys.
    - Fix accessibility label for dropdown arrow next to user label.
    - Fire alert when showing bubble so ChromeVox reads the contents.
    
    Bug: 757273
    Change-Id: I7fc5cc8b730a2b3d796cf040679e0de549f8fc92
    Reviewed-on: https://chromium-review.googlesource.com/822979
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Jacob Dufault <jdufault@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523613}

--- ash/login/ui/login_bubble.cc
+++ ash/login/ui/login_bubble.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/login/ui/login_button.h"
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/shell.h"
+#include "ui/accessibility/ax_node_data.h"
 #include "ui/compositor/layer_animator.h"
 #include "ui/compositor/scoped_layer_animation_settings.h"
 #include "ui/gfx/font.h"
@@ -77,6 +78,9 @@ class LoginErrorBubbleView : public LoginBaseBubbleView {
 
   // views::View:
   const char* GetClassName() const override { return "LoginErrorBubbleView"; }
+  void GetAccessibleNodeData(ui::AXNodeData* node_data) override {
+    node_data->role = ui::AX_ROLE_TOOLTIP;
+  }
 
  private:
   DISALLOW_COPY_AND_ASSIGN(LoginErrorBubbleView);
@@ -116,6 +120,11 @@ class LoginTooltipView : public LoginBaseBubbleView {
     views::Label* text = CreateLabel(message, SK_ColorWHITE);
     text->SetMultiLine(true);
     AddChildView(text);
+  }
+
+  // LoginBaseBubbleView:
+  void GetAccessibleNodeData(ui::AXNodeData* node_data) override {
+    node_data->role = ui::AX_ROLE_TOOLTIP;
   }
 
  private:
@@ -224,6 +233,9 @@ void LoginBubble::Show() {
   bubble_view_->GetWidget()->AddObserver(this);
 
   ScheduleAnimation(true /*visible*/);
+
+  // Fire an alert so ChromeVox will read the contents of the bubble.
+  bubble_view_->NotifyAccessibilityEvent(ui::AX_EVENT_ALERT, true);
 }
 
 void LoginBubble::CloseImmediately() {
