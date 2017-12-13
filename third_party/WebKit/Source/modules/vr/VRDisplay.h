5a16e2febcd0f3969427091ba4942929c0c6986d
Christos Froussios <cfroussios@chromium.org>
2017-12-13 09:13:51 +0000

    Revert "Avoid null poses in WebVR magic window mode"
    This reverts commit 28e0c070af0eff724a7e28c8a5196817db028f90.
    
    Reason for revert: Suspected of breaking vr/VRDisplay_rAF_fires_with_window_rAF.html on Win7 Tests (dbg)
    E.g https://uberchromegw.corp.google.com/i/chromium.win/builders/Win7%20Tests%20%28dbg%29%281%29/builds/65158
    
    Original change's description:
    > Avoid null poses in WebVR magic window mode
    > 
    > There's a race condition where the first magic window VSync could arrive before
    > ever getting a pose. Add a check and deferred execution to avoid this.
    > 
    > BUG=787196
    > 
    > Change-Id: If804e4d6acc3da7b903ba36108f23c00b1c8fe5b
    > Reviewed-on: https://chromium-review.googlesource.com/783887
    > Commit-Queue: Klaus Weidner <klausw@chromium.org>
    > Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    > Reviewed-by: Bill Orr <billorr@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#523657}
    
    TBR=mthiesse@chromium.org,bajones@chromium.org,klausw@chromium.org,billorr@chromium.org
    
    Change-Id: Ic70d3ebdb294e04bf70a0488d21790e05d43e1d5
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 787196
    Reviewed-on: https://chromium-review.googlesource.com/823903
    Reviewed-by: Christos Froussios <cfroussios@chromium.org>
    Commit-Queue: Christos Froussios <cfroussios@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523728}

--- third_party/WebKit/Source/modules/vr/VRDisplay.h
+++ third_party/WebKit/Source/modules/vr/VRDisplay.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/Timer.h"
 #include "platform/heap/Handle.h"
 #include "platform/wtf/Forward.h"
-#include "platform/wtf/Functional.h"
 #include "platform/wtf/text/WTFString.h"
 #include "public/platform/WebGraphicsContext3DProvider.h"
 
@@ -248,8 +247,6 @@ class VRDisplay final : public EventTargetWithInlineData,
   bool pending_presenting_vsync_ = false;
   bool pending_magic_window_vsync_ = false;
   int pending_magic_window_vsync_id_ = -1;
-  base::OnceClosure magic_window_vsync_waiting_for_pose_;
-  WTF::TimeTicks magic_window_pose_time_;
   bool in_animation_frame_ = false;
   bool did_submit_this_frame_ = false;
   bool display_blurred_ = false;
