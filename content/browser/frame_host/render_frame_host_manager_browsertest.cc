bd5714f6ab2fae569e02520737e23ebe29078b13
John Abd-El-Malek <jam@chromium.org>
2017-12-13 02:58:32 +0000

    Add a regression test for bug 793432.
    Bug: 793432
    Change-Id: Ic88a3aeedcb5c3d0f06f0e7da8edb3ef6433b155
    Reviewed-on: https://chromium-review.googlesource.com/820672
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523667}

--- content/browser/frame_host/render_frame_host_manager_browsertest.cc
+++ content/browser/frame_host/render_frame_host_manager_browsertest.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "build/build_config.h"
 #include "content/browser/child_process_security_policy_impl.h"
+#include "content/browser/frame_host/navigation_handle_impl.h"
 #include "content/browser/frame_host/render_frame_host_impl.h"
 #include "content/browser/frame_host/render_frame_proxy_host.h"
 #include "content/browser/renderer_host/render_view_host_impl.h"
@@ -3525,6 +3526,39 @@ IN_PROC_BROWSER_TEST_F(RenderFrameHostManagerTest,
   EXPECT_EQ(url, grandchild->current_url());
   ASSERT_EQ(1U, grandchild->child_count());
   EXPECT_EQ(child_url, grandchild->child_at(0)->current_url());
+}
+
+// Ensures that we don't reset a speculative RFH if a JavaScript URL is loaded
+// while there's an ongoing cross-process navigation. See
+// https://crbug.com/793432.
+IN_PROC_BROWSER_TEST_F(RenderFrameHostManagerTest,
+                       JavaScriptLoadDoesntResetSpeculativeRFH) {
+  if (!IsBrowserSideNavigationEnabled())
+    return;
+  EXPECT_TRUE(embedded_test_server()->Start());
+
+  GURL site1 = embedded_test_server()->GetURL("a.com", "/title1.html");
+  GURL site2 = embedded_test_server()->GetURL("b.com", "/title2.html");
+
+  NavigateToURL(shell(), site1);
+
+  TestNavigationManager cross_site_navigation(shell()->web_contents(), site2);
+  shell()->LoadURL(site2);
+  EXPECT_TRUE(cross_site_navigation.WaitForRequestStart());
+
+  RenderFrameHostImpl* speculative_rfh =
+      static_cast<WebContentsImpl*>(shell()->web_contents())
+          ->GetFrameTree()
+          ->root()
+          ->render_manager()
+          ->speculative_frame_host();
+  CHECK(speculative_rfh);
+  shell()->web_contents()->GetController().LoadURL(
+      GURL("javascript:(0)"), Referrer(), ui::PAGE_TRANSITION_TYPED,
+      std::string());
+
+  cross_site_navigation.ResumeNavigation();
+  // No crash means everything worked!
 }
 
 }  // namespace content
