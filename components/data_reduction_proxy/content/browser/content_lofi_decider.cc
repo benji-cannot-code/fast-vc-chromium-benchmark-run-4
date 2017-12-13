6f7ee32ffa5dae011ac4323a60527237e01ae219
Doug Arnett <dougarnett@chromium.org>
2017-12-13 06:59:34 +0000

    Makes navigation commit-time decision for previews in one location
    This is a refactor to consolidate the commit-time previews decision
    in one place (in ChromeResourceDispatcherHostDelegate) for both
    server-determined and client-determined previews with propagation
    of decision to renderer (via PreviewsState in mainframe response
    info) and UI thread (via PreviewsUserData).
    
    Bug: 782922, 725645
    Change-Id: I8f7e5ef3a8a45b9ba2e1bbc07f09bfb9670e16b1
    Reviewed-on: https://chromium-review.googlesource.com/807305
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Scott Little <sclittle@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Doug Arnett <dougarnett@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523705}

--- components/data_reduction_proxy/content/browser/content_lofi_decider.cc
+++ components/data_reduction_proxy/content/browser/content_lofi_decider.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/data_reduction_proxy/core/common/data_reduction_proxy_params.h"
 #include "components/previews/core/previews_decider.h"
 #include "content/public/browser/resource_request_info.h"
-#include "content/public/common/previews_state.h"
 #include "content/public/common/resource_type.h"
 #include "net/base/load_flags.h"
 #include "net/http/http_request_headers.h"
@@ -28,6 +27,38 @@ namespace data_reduction_proxy {
 ContentLoFiDecider::ContentLoFiDecider() {}
 
 ContentLoFiDecider::~ContentLoFiDecider() {}
+
+// Static
+content::PreviewsState
+ContentLoFiDecider::DetermineCommittedServerPreviewsState(
+    const net::URLRequest& request,
+    content::PreviewsState initial_state) {
+  DCHECK_EQ(
+      content::RESOURCE_TYPE_MAIN_FRAME,
+      content::ResourceRequestInfo::ForRequest(&request)->GetResourceType())
+      << "Request was not for main frame";
+  data_reduction_proxy::DataReductionProxyData* drp_data =
+      data_reduction_proxy::DataReductionProxyData::GetData(request);
+  if (!drp_data) {
+    return initial_state &=
+           ~(content::SERVER_LITE_PAGE_ON | content::SERVER_LOFI_ON);
+  }
+  content::PreviewsState updated_state = initial_state;
+  if (!drp_data->lite_page_received()) {
+    // Turn off LitePage bit.
+    updated_state &= ~(content::SERVER_LITE_PAGE_ON);
+  }
+  if (!drp_data->lofi_requested()) {
+    // Turn off LoFi bit(s).
+    updated_state &= ~(content::SERVER_LOFI_ON);
+    if (drp_data->used_data_reduction_proxy()) {
+      // Turn off Client LoFi bit also if using proxy but proxy did not
+      // request LoFi.
+      updated_state &= ~(content::CLIENT_LOFI_ON);
+    }
+  }
+  return updated_state;
+}
 
 bool ContentLoFiDecider::IsUsingLoFi(const net::URLRequest& request) const {
   const content::ResourceRequestInfo* request_info =
--- components/data_reduction_proxy/content/browser/content_lofi_decider.h
+++ components/data_reduction_proxy/content/browser/content_lofi_decider.h
@@ -7,7 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_DATA_REDUCTION_PROXY_CONTENT_BROWSER_CONTENT_LOFI_DECIDER_H_
 
 #include "components/data_reduction_proxy/core/common/lofi_decider.h"
+
 #include "base/macros.h"
+#include "components/data_reduction_proxy/core/browser/data_reduction_proxy_data.h"
+#include "content/public/common/previews_state.h"
 
 class GURL;
 
@@ -32,6 +35,14 @@ class ContentLoFiDecider : public LoFiDecider {
  public:
   ContentLoFiDecider();
   ~ContentLoFiDecider() override;
+
+  // Returns an updated PreviewsState with respect to server previews
+  // given the main frame's committed |request| and the |initial_state|
+  // of enabled previews. |request| must have already been updated with
+  // respect to the main frame response headers.
+  static content::PreviewsState DetermineCommittedServerPreviewsState(
+      const net::URLRequest& request,
+      content::PreviewsState initial_state);
 
   // LoFiDecider implementation:
   bool IsUsingLoFi(const net::URLRequest& request) const override;
