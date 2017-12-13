ed349e66ea171d09a3a51509a1a9cddb371462e2
Hiroki Nakagawa <nhiroki@chromium.org>
2017-12-13 07:40:17 +0000

    Worklet: Fix credentials tests for worklets
    Before this CL, credentials tests for worklets are completely broken as follows:
    
    - In credentials.py, |credentials_mode| is compared by "is" that conducts the
      object equality check. As a result, all conditions unexpectedly pass through
      and the script always returns a valid response.
    - In credentials.py, |is_cross_origin| value is always |False| because requests
      don't have the GET param.
    - fetch() needs { credentials: 'include' } option for configuring the document's
      cookie with the Set-Cookie header, but it's not specified.
    - Response headers are not specified for a 404 response. This results in test
      timeout instead of a network error.
    
    This CL fixes these bugs and cleans up tests.
    
    Bug: 738769
    Change-Id: I9aeb11fca23dec3fd3057a765fa60eb6e59d8258
    Reviewed-on: https://chromium-review.googlesource.com/799730
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523713}

--- third_party/WebKit/LayoutTests/external/wpt/worklets/resources/set-cookie.py
+++ third_party/WebKit/LayoutTests/external/wpt/worklets/resources/set-cookie.py
@@ -1,7 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 def main(request, response):
     name = request.GET.first("name")
-    source_origin = request.headers.get("origin", None);
-    response.headers.set("Set-Cookie", name + "=value")
-    response.headers.set("Access-Control-Allow-Origin", source_origin)
-    response.headers.set("Access-Control-Allow-Credentials", "true")
+    value = request.GET.first("value")
+    source_origin = request.headers.get("origin", None)
+
+    response_headers = [("Set-Cookie", name + "=" + value),
+                        ("Access-Control-Allow-Origin", source_origin),
+                        ("Access-Control-Allow-Credentials", "true")]
+    return (200, response_headers, "")
