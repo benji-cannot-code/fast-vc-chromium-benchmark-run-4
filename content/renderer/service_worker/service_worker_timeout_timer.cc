03d87dfc745997707f20a364484fcd1b277ccf93
Makoto Shimazu <shimazu@chromium.org>
2017-12-13 03:44:20 +0000

    S13nServiceWorker: postpone fetch events for subresources when idle
    This patch implements a part of idle timer's logic. Fetch events coming through
    mojom::ControllerServiceWorker after the worker has requested termination to the
    browser should be queued until the worker receives the next event or StopWorker
    message from the browser. If the worker receives the next event from the
    browser, the queued events should run before the event from the browser,
    otherwise the order of the events would be messed up. If the worker receives
    StopWorker message instead, the worker shouldn't invoke the callback for the
    fetch event, and silently should disconnect the pipe to the clients.
    
    Bug: 774374
    Change-Id: I6f3c90405ff037fe07df0310e2234cb81cb2ead9
    Reviewed-on: https://chromium-review.googlesource.com/816458
    Commit-Queue: Makoto Shimazu <shimazu@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523677}

--- content/renderer/service_worker/service_worker_timeout_timer.cc
+++ content/renderer/service_worker/service_worker_timeout_timer.cc
@@ -54,6 +54,15 @@ ServiceWorkerTimeoutTimer::~ServiceWorkerTimeoutTimer() {
 
 int ServiceWorkerTimeoutTimer::StartEvent(
     base::OnceCallback<void(int /* event_id */)> abort_callback) {
+  if (did_idle_timeout()) {
+    idle_time_ = base::TimeTicks();
+    did_idle_timeout_ = false;
+    while (!pending_tasks_.empty()) {
+      std::move(pending_tasks_.front()).Run();
+      pending_tasks_.pop();
+    }
+  }
+
   idle_time_ = base::TimeTicks();
   const int event_id = NextEventId();
   std::set<EventInfo>::iterator iter;
@@ -73,6 +82,13 @@ void ServiceWorkerTimeoutTimer::EndEvent(int event_id) {
   id_event_map_.erase(iter);
   if (inflight_events_.empty())
     idle_time_ = tick_clock_->NowTicks() + kIdleDelay;
+}
+
+void ServiceWorkerTimeoutTimer::PushPendingTask(
+    base::OnceClosure pending_task) {
+  DCHECK(ServiceWorkerUtils::IsServicificationEnabled());
+  DCHECK(did_idle_timeout());
+  pending_tasks_.emplace(std::move(pending_task));
 }
 
 void ServiceWorkerTimeoutTimer::UpdateStatus() {
@@ -95,8 +111,10 @@ void ServiceWorkerTimeoutTimer::UpdateStatus() {
   if (inflight_events_.empty() && idle_time_.is_null())
     idle_time_ = tick_clock_->NowTicks() + kIdleDelay;
 
-  if (!idle_time_.is_null() && idle_time_ < now)
+  if (!idle_time_.is_null() && idle_time_ < now) {
+    did_idle_timeout_ = true;
     idle_callback_.Run();
+  }
 }
 
 ServiceWorkerTimeoutTimer::EventInfo::EventInfo(
