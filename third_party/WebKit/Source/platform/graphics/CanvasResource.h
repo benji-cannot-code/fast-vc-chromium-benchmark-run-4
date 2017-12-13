8f9c5da8ba7802bfe77b59940ae4b932b16be1d2
Justin Novosad <junov@chromium.org>
2017-12-13 22:33:29 +0000

    Re-enable overlays for 2d canvas
    In this refactor CL:
    https://crrev.com/4f6bce25c2f94e59611343ddab1fffcc13f79f07
    overlays got disabled by accident, causing performance regressions
    on mac.
    
    BUG=792952
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I8acdd0f9157ab4fb31c5cfe62366387cdae7a4ca
    Reviewed-on: https://chromium-review.googlesource.com/825702
    Reviewed-by: Olivia Lai <xlai@chromium.org>
    Commit-Queue: Justin Novosad <junov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523917}

--- third_party/WebKit/Source/platform/graphics/CanvasResource.h
+++ third_party/WebKit/Source/platform/graphics/CanvasResource.h
@@ -121,7 +121,7 @@ class PLATFORM_EXPORT CanvasResource_GpuMemoryBuffer final
  private:
   void TearDown();
   GLenum TextureTarget() const final;
-  bool IsOverlayCandidate() const final { return false; }
+  bool IsOverlayCandidate() const final { return true; }
 
   CanvasResource_GpuMemoryBuffer(
       const IntSize&,
