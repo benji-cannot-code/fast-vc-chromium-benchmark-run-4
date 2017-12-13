b89659464e7cbf644f127aeae3fd5a9430fb77f1
Mounir Lamouri <mlamouri@chromium.org>
2017-12-13 11:53:25 +0000

    Autoplay: override autoplay restrictions on Chrome Apps/Extensions.
    This is forcing no autoplay restrictions to all `Extensions`. It does
    not apply to WebViews.
    
    Bug: 788302
    Change-Id: I9c7e89133279453382976ff0f79d285672a5f1ac
    Reviewed-on: https://chromium-review.googlesource.com/803440
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523747}

---
+++ chrome/test/data/extensions/autoplay/background.js
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.test.runTests([
+  function autoplay_allowed_without_gesture() {
+    var audio = new Audio();
+    audio.src = 'test.mp4';
+    var allowed = false;
+    audio.play().then(function() {
+      allowed = true;
+    }, function(e) {
+      allowed = e.name != 'NotAllowedError';
+    }).then(function() {
+      chrome.test.assertTrue(allowed);
+      chrome.test.succeed();
+    });
+  }
+]);
---
+++ chrome/test/data/extensions/platform_apps/web_view/autoplay/main.js
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.test.getConfig(function(config) {
+  var guestUrl = 'http://localhost:' + config.testServer.port +
+      '/extensions/platform_apps/web_view/autoplay/guest.html';
+
+  var webview = document.querySelector('webview');
+  webview.addEventListener('loadstop', function() {
+    webview.onconsolemessage = function(e) {
+      chrome.test.assertEq('NotAllowedError', e.message);
+      chrome.test.succeed();
+    };
+
+    webview.contentWindow.postMessage(JSON.stringify('start'), '*');
+  }, { once: true });
+
+  webview.src = guestUrl;
+});
