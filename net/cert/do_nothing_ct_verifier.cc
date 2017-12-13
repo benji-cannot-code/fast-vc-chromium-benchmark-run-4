0a9f368994af9171227c06afc0b7ad43bf4e5bdc
Rob Percival <robpercival@chromium.org>
2017-12-13 08:24:42 +0000

    Avoid SingleTreeTracker revealing hostnames to a DNS resolver
    SingleTreeTracker could previously reveal to a DNS resolver that a user
    was accessing a particular host, as a side-effect of performing a
    Certificate Transparency inclusion check over DNS. It will now first
    check that a DNS lookup for that host has already occurred (i.e. the DNS
    resolver already knows that the user has been accessing that host),
    otherwise it will avoid revealing that information by not performing an
    inclusion check.
    
    Bug: 506227, 682193
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester;master.tryserver.chromium.linux:linux_mojo;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ifd5ef10773155b5b27aea32bee9c460320c1e264
    Reviewed-on: https://chromium-review.googlesource.com/768790
    Commit-Queue: Rob Percival <robpercival@chromium.org>
    Reviewed-by: Miriam Gershenson <mgersh@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523717}

--- net/cert/do_nothing_ct_verifier.cc
+++ net/cert/do_nothing_ct_verifier.cc
@@ -13,6 +13,7 @@ DoNothingCTVerifier::DoNothingCTVerifier() = default;
 DoNothingCTVerifier::~DoNothingCTVerifier() = default;
 
 void DoNothingCTVerifier::Verify(
+    base::StringPiece hostname,
     X509Certificate* cert,
     base::StringPiece stapled_ocsp_response,
     base::StringPiece sct_list_from_tls_extension,
@@ -22,5 +23,9 @@ void DoNothingCTVerifier::Verify(
 }
 
 void DoNothingCTVerifier::SetObserver(Observer* observer) {}
+
+CTVerifier::Observer* DoNothingCTVerifier::GetObserver() const {
+  return nullptr;
+}
 
 }  // namespace net
