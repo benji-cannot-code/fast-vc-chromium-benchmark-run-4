9f8ac04cfdb03a81fff7aaccbfc624264b4cf9e3
Katie Dektar <katydek@google.com>
2017-12-13 02:13:28 +0000

    Fixes accessiblity updates in native views that scroll or animate.
    Bug: 789920,778080,789282
    
    Change-Id: Ie4b9d18ffbff48602126f6a4024fdd39f49fe032
    Reviewed-on: https://chromium-review.googlesource.com/815878
    Commit-Queue: Katie D <katie@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523654}

--- ui/views/view.cc
+++ ui/views/view.cc
@@ -1465,7 +1465,11 @@ void View::NotifyAccessibilityEvent(
     if (native_view_accessibility_)
       native_view_accessibility_->NotifyAccessibilityEvent(event_type);
   }
+
+  OnAccessibilityEvent(event_type);
 }
+
+void View::OnAccessibilityEvent(ui::AXEvent event_type) {}
 
 // Scrolling -------------------------------------------------------------------
 
@@ -2284,6 +2288,8 @@ void View::BoundsChanged(const gfx::Rect& previous_bounds) {
   }
 
   OnBoundsChanged(previous_bounds);
+  if (bounds_ != previous_bounds)
+    NotifyAccessibilityEvent(ui::AX_EVENT_LOCATION_CHANGED, false);
 
   if (needs_layout_ || previous_bounds.size() != size()) {
     needs_layout_ = false;
