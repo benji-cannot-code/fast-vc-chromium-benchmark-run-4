d7809aef35de8d15ccddf4f379ed3b6ac60768d7
Tom Sepez <tsepez@chromium.org>
2017-12-13 17:56:57 +0000

    Place network service into namespace sandbox on linux, try 2
    Next step forward towards sandboxing network service.
    
    Ensures that all FS access goes through the proxy, even if it
    is not being subject to any whitelist restrictions yet. All
    other syscalls are still allowed.
    
    Bug: 715679
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: Ibe8dd5a09bbf498bc10c9ae289222b8e46b8e4b7
    Reviewed-on: https://chromium-review.googlesource.com/817697
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523811}

--- content/network/network_sandbox_hook_linux.cc
+++ content/network/network_sandbox_hook_linux.cc
@@ -15,8 +15,10 @@ using sandbox::syscall_broker::MakeBrokerCommandSet;
 namespace content {
 
 bool NetworkPreSandboxHook(service_manager::SandboxLinux::Options options) {
+  auto* instance = service_manager::SandboxLinux::GetInstance();
+
   // TODO(tsepez): remove universal permission under filesytem root.
-  service_manager::SandboxLinux::GetInstance()->StartBrokerProcess(
+  instance->StartBrokerProcess(
       MakeBrokerCommandSet({
           sandbox::syscall_broker::COMMAND_ACCESS,
           sandbox::syscall_broker::COMMAND_MKDIR,
@@ -30,6 +32,7 @@ bool NetworkPreSandboxHook(service_manager::SandboxLinux::Options options) {
       {BrokerFilePermission::ReadWriteCreateRecursive("/")},
       service_manager::SandboxLinux::PreSandboxHook(), options);
 
+  instance->EngageNamespaceSandbox(false /* from_zygote */);
   return true;
 }
 
