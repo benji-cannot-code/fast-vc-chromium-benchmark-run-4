062f2bc4f327488d267da07bda2c156a6b47142c
Xiaocheng Hu <xiaochengh@chromium.org>
2017-12-14 04:20:54 +0000

    Unify marker removal logic when spellchecking is globally disabled
    When spellchecking is globally disabled (via context menu or
    settings), all spelling markers should be removed. Currently, the
    marker removal logic is duplicated at two places.
    
    This patch unifies the logic and ensures that the removal is done
    exactly once for each frame.
    
    This is also a preparation patch for removing Blink-side maintenance
    of the global spellcheck enabled status.
    
    All-in-one patch: crrev.com/c/821496
    
    Removal plan of Blink-side status: https://goo.gl/grtURZ
    
    Bug: 710097
    Change-Id: I84d33cef5db2aa6ffeb1b66568dee037cba4a737
    Reviewed-on: https://chromium-review.googlesource.com/826259
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524003}

--- components/spellcheck/renderer/spellcheck_provider.cc
+++ components/spellcheck/renderer/spellcheck_provider.cc
@@ -250,8 +250,6 @@ void SpellCheckProvider::OnRespondTextCheck(
 void SpellCheckProvider::EnableSpellcheck(bool enable) {
   WebLocalFrame* frame = render_frame()->GetWebFrame();
   frame->EnableSpellChecking(enable);
-  if (!enable)
-    frame->RemoveSpellingMarkers();
 }
 
 bool SpellCheckProvider::SatisfyRequestFromCache(
