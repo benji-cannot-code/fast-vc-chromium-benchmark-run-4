183a2532591552a1a58a2aa65b764664c44ffa55
Steven Bennetts <stevenjb@chromium.org>
2017-12-14 19:38:32 +0000

    Network Config: enable settings config by default and invert switch
    This adds a wrapper, IsNetworkSettingsConfigEnabled() so simplify
    inverting the switch again if necessary.
    
    Bug: 380937
    Change-Id: I5036beeb359d0381d04ec9ae0fc36a33dfee215c
    Reviewed-on: https://chromium-review.googlesource.com/818567
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524145}

--- chrome/browser/ui/webui/settings/md_settings_localized_strings_provider.cc
+++ chrome/browser/ui/webui/settings/md_settings_localized_strings_provider.cc
@@ -1076,8 +1076,7 @@ void AddInternetStrings(content::WebUIDataSource* html_source) {
                           arraysize(localized_strings));
 
   html_source->AddBoolean("networkSettingsConfig",
-                          base::CommandLine::ForCurrentProcess()->HasSwitch(
-                              chromeos::switches::kNetworkSettingsConfig));
+                          chromeos::switches::IsNetworkSettingsConfigEnabled());
   html_source->AddString("networkGoogleNameserversLearnMoreUrl",
                          chrome::kGoogleNameserversLearnMoreURL);
 
