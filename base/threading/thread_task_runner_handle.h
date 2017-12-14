839b271ea5d7d118c2d91fb60af059dd4f9a72a5
Gabriel Charette <gab@chromium.org>
2017-12-14 21:10:25 +0000

    WARN_UNUSED_RESULT on ThreadTaskRunnerHandle::OverrideForTesting()
    R=thestig@chromium.org
    
    Bug: Returns scoped object which should never go unused, warn for good
    measure.
    
    Change-Id: I15992a1cd0d1d83157a66532f883311e36f4e621
    Reviewed-on: https://chromium-review.googlesource.com/827133
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524175}

--- base/threading/thread_task_runner_handle.h
+++ base/threading/thread_task_runner_handle.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/base_export.h"
 #include "base/callback_helpers.h"
+#include "base/compiler_specific.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/single_thread_task_runner.h"
@@ -47,7 +48,7 @@ class BASE_EXPORT ThreadTaskRunnerHandle {
   };
   static ScopedClosureRunner OverrideForTesting(
       scoped_refptr<SingleThreadTaskRunner> overriding_task_runner,
-      OverrideType type = OverrideType::kDefault);
+      OverrideType type = OverrideType::kDefault) WARN_UNUSED_RESULT;
 
   // Binds |task_runner| to the current thread. |task_runner| must belong
   // to the current thread for this to succeed.
