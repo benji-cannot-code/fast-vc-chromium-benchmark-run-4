a179cef8c3bdc7543ce79188e1b20257d2235b31
Jun Choi <hongjunchoi@chromium.org>
2017-12-14 23:04:09 +0000

    Allow CBOR maps to use integer keys
    Currently, CBOR maps only permit keys to be UTF-8 strings. Add support
    for unsigned integer keys. Support for negative integer type keys will
    be integrated as CL 777807 is lands.
    
    Bug: 786218
    Change-Id: Id28864a9c62d0b0d93e71a663b6e8b2b84b2de95
    Reviewed-on: https://chromium-review.googlesource.com/781207
    Commit-Queue: Jun Choi <hongjunchoi@chromium.org>
    Reviewed-by: Chris Palmer <palmer@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Jeffrey Yasskin <jyasskin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524223}

--- content/browser/webauth/cbor/cbor_reader.h
+++ content/browser/webauth/cbor/cbor_reader.h
@@ -40,6 +40,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //  - Callers can decode CBOR values with at most 16 nested depth layer. More
 //    strict restrictions on nesting layer size of CBOR values can be enforced
 //    by setting |max_nesting_level|.
+//  - Only CBOR maps with integer or string type keys are supported due to the
+//    cost of serialization when sorting map keys.
 
 namespace content {
 
@@ -89,9 +91,9 @@ class CONTENT_EXPORT CBORReader {
   base::Optional<CBORValue> ReadCBORMap(uint64_t length, int max_nesting_level);
   bool CanConsume(uint64_t bytes);
   void CheckExtraneousData();
-  bool CheckDuplicateKey(const std::string& new_key, CBORValue::MapValue* map);
+  bool CheckDuplicateKey(const CBORValue& new_key, CBORValue::MapValue* map);
   bool HasValidUTF8Format(const std::string& string_data);
-  bool CheckOutOfOrderKey(const std::string& new_key, CBORValue::MapValue* map);
+  bool CheckOutOfOrderKey(const CBORValue& new_key, CBORValue::MapValue* map);
   bool CheckUintEncodedByteLength(uint8_t additional_bytes, uint64_t uint_data);
 
   DecoderError GetErrorCode();
