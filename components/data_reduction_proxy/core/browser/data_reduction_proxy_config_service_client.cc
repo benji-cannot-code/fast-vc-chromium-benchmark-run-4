55d97b710fd01a56cd3bdbf8496723cc389fc48a
Ryan Sturm <ryansturm@chromium.org>
2017-12-14 22:36:26 +0000

    Adding device memory in kb to the client config request
    pingback will also have this information
    
    Bug: 793373
    Change-Id: I0c999c35b5e968f6b2e19696a08a722b3399d0d9
    Reviewed-on: https://chromium-review.googlesource.com/823176
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Ryan Sturm <ryansturm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524203}

--- components/data_reduction_proxy/core/browser/data_reduction_proxy_config_service_client.cc
+++ components/data_reduction_proxy/core/browser/data_reduction_proxy_config_service_client.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/sys_info.h"
 #include "base/values.h"
 #include "build/build_config.h"
 #include "components/data_reduction_proxy/core/browser/data_reduction_proxy_config.h"
@@ -384,6 +385,10 @@ void DataReductionProxyConfigServiceClient::RetrieveRemoteConfig() {
   request.set_telephony_network_operator(
       net::android::GetTelephonyNetworkOperator());
 #endif
+
+  data_reduction_proxy::DeviceInfo* device_info = request.mutable_device_info();
+  device_info->set_total_device_memory_kb(
+      base::SysInfo::AmountOfPhysicalMemory() / 1024);
   const std::string& session_key = request_options_->GetSecureSession();
   if (!session_key.empty())
     request.set_session_key(request_options_->GetSecureSession());
