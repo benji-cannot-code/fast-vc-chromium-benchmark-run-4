8ebff3a9b012982ab58aa34955909cace6f155bf
Doug Arnett <dougarnett@chromium.org>
2017-12-14 23:40:36 +0000

    Adds new observer method to DRPDataUseObserver for DidFinishLoad
    This is plumbing preparation for estimating data savings for the
    NoScript preview (determined when page load finished event occurs).
    
    Bug: 781885
    Change-Id: I145b9d270327c757ea1fbfbc44aa5932822c592e
    Reviewed-on: https://chromium-review.googlesource.com/820891
    Commit-Queue: Doug Arnett <dougarnett@chromium.org>
    Reviewed-by: rajendrant <rajendrant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524231}

--- chrome/browser/data_use_measurement/chrome_data_use_ascriber_service.h
+++ chrome/browser/data_use_measurement/chrome_data_use_ascriber_service.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "components/keyed_service/core/keyed_service.h"
+#include "url/gurl.h"
 
 namespace content {
 class NavigationHandle;
@@ -66,6 +67,10 @@ class ChromeDataUseAscriberService : public KeyedService {
 
   // Forwarded from DataUseWebContentsObserver
   void DidFinishNavigation(content::NavigationHandle* navigation_handle);
+
+  // Forwarded from DataUseWebContentsObserver
+  void DidFinishLoad(content::RenderFrameHost* main_render_frame_host,
+                     const GURL& validated_url);
 
  private:
   friend class ChromeDataUseAscriberServiceTest;
