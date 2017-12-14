459d091bbbdcc5f42049823383885dc4d5077fba
Xi Han <hanxi@google.com>
2017-12-14 14:54:43 +0000

    Webapp offline dialog handles more network errors
    Currently the offline dialog only handles ERR_INTERNET_DISCONNECTED error, and
    we would like it to catch up more network errors. A generic message is added
    to shown on the dialog.
    
    Bug: 789566
    Change-Id: I9c36319241f8754822e6336eca8ae13379edc764
    Reviewed-on: https://chromium-review.googlesource.com/803835
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524066}

--- chrome/android/java/src/org/chromium/chrome/browser/webapps/WebappSplashScreenController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/WebappSplashScreenController.java
@@ -19,6 +19,7 @@ import org.chromium.base.ContextUtils;
 import org.chromium.base.VisibleForTesting;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.compositor.CompositorViewHolder;
+import org.chromium.chrome.browser.metrics.WebApkUma;
 import org.chromium.chrome.browser.metrics.WebappUma;
 import org.chromium.chrome.browser.tab.EmptyTabObserver;
 import org.chromium.chrome.browser.tab.Tab;
@@ -29,6 +30,9 @@ import org.chromium.net.NetworkChangeNotifier;
 
 /** Shows and hides splash screen. */
 class WebappSplashScreenController extends EmptyTabObserver {
+    // No error.
+    public static final int ERROR_OK = 0;
+
     /** Used to schedule splash screen hiding. */
     private CompositorViewHolder mCompositorViewHolder;
 
@@ -145,24 +149,24 @@ class WebappSplashScreenController extends EmptyTabObserver {
             boolean isErrorPage, boolean hasCommitted, boolean isSameDocument,
             boolean isFragmentNavigation, Integer pageTransition, int errorCode,
             int httpStatusCode) {
-        if (mActivityType == WebappActivity.ACTIVITY_TYPE_WEBAPP) return;
+        if (mActivityType == WebappActivity.ACTIVITY_TYPE_WEBAPP || !isInMainFrame) return;
 
         mErrorCode = errorCode;
-
         switch (mErrorCode) {
-            case NetError.ERR_NETWORK_CHANGED:
-                onNetworkChanged(tab);
-                break;
-            case NetError.ERR_INTERNET_DISCONNECTED:
-                onNetworkDisconnected(tab);
-                break;
-            default:
+            case ERROR_OK:
                 if (mOfflineDialog != null) {
                     mOfflineDialog.cancel();
                     mOfflineDialog = null;
                 }
                 break;
+            case NetError.ERR_NETWORK_CHANGED:
+                onNetworkChanged(tab);
+                break;
+            default:
+                onNetworkError(tab, errorCode);
+                break;
         }
+        WebApkUma.recordNetworkErrorWhenLaunch(-errorCode);
     }
 
     protected boolean canHideSplashScreen() {
@@ -182,7 +186,7 @@ class WebappSplashScreenController extends EmptyTabObserver {
         mAllowReloads = false;
     }
 
-    private void onNetworkDisconnected(final Tab tab) {
+    private void onNetworkError(final Tab tab, int errorCode) {
         if (mOfflineDialog != null || tab.getActivity() == null) return;
 
         final NetworkChangeNotifier.ConnectionTypeObserver observer =
@@ -200,8 +204,8 @@ class WebappSplashScreenController extends EmptyTabObserver {
 
         NetworkChangeNotifier.addConnectionTypeObserver(observer);
         mOfflineDialog = new WebappOfflineDialog();
-        mOfflineDialog.show(
-                tab.getActivity(), mAppName, mActivityType == WebappActivity.ACTIVITY_TYPE_WEBAPK);
+        mOfflineDialog.show(tab.getActivity(), mAppName,
+                mActivityType == WebappActivity.ACTIVITY_TYPE_WEBAPK, errorCode);
     }
 
     /** Sets the splash screen layout and sets the splash screen's title and icon. */
