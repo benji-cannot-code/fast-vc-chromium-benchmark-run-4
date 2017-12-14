30d7c99fd59256ad6f6577cc6a859635c8d22647
Yuly Novikov <ynovikov@chromium.org>
2017-12-14 23:25:17 +0000

    Conditionally link ANGLE statically into Android Chrome
    When use_static_angle is set
    
    BUG=726002
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I4d716a48002f03af29a0280cd9207460e470143d
    Reviewed-on: https://chromium-review.googlesource.com/822952
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Yuly Novikov <ynovikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524226}

--- gpu/config/gpu_info_collector_android.cc
+++ gpu/config/gpu_info_collector_android.cc
@@ -23,7 +23,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/egl_util.h"
 #include "ui/gl/gl_bindings.h"
 #include "ui/gl/gl_context.h"
+#include "ui/gl/gl_features.h"
 #include "ui/gl/gl_surface.h"
+
+#if BUILDFLAG(USE_STATIC_ANGLE)
+#include <EGL/egl.h>
+#endif  // BUILDFLAG(USE_STATIC_ANGLE)
 
 namespace {
 
@@ -69,6 +74,12 @@ std::string GetDriverVersionFromString(const std::string& version_string) {
 }
 
 gpu::CollectInfoResult CollectDriverInfo(gpu::GPUInfo* gpu_info) {
+#if BUILDFLAG(USE_STATIC_ANGLE)
+#pragma push_macro("eglGetProcAddress")
+#undef eglGetProcAddress
+#define LOOKUP_FUNC(x) \
+  auto x##Fn = reinterpret_cast<gl::x##Proc>(eglGetProcAddress(#x))
+#else  // BUILDFLAG(USE_STATIC_ANGLE)
   // Go through the process of loading GL libs and initializing an EGL
   // context so that we can get GL vendor/version/renderer strings.
   base::NativeLibrary gles_library, egl_library;
@@ -113,6 +124,7 @@ gpu::CollectInfoResult CollectDriverInfo(gpu::GPUInfo* gpu_info) {
   };
 
 #define LOOKUP_FUNC(x) auto x##Fn = reinterpret_cast<gl::x##Proc>(get_func(#x))
+#endif  // BUILDFLAG(USE_STATIC_ANGLE)
 
   LOOKUP_FUNC(eglGetError);
   LOOKUP_FUNC(eglQueryString);
@@ -132,6 +144,9 @@ gpu::CollectInfoResult CollectDriverInfo(gpu::GPUInfo* gpu_info) {
   LOOKUP_FUNC(glGetIntegerv);
 
 #undef LOOKUP_FUNC
+#if BUILDFLAG(USE_STATIC_ANGLE)
+#pragma pop_macro("eglGetProcAddress")
+#endif  // BUILDFLAG(USE_STATIC_ANGLE)
 
   EGLDisplay curr_display = eglGetCurrentDisplayFn();
   EGLContext curr_context = eglGetCurrentContextFn();
