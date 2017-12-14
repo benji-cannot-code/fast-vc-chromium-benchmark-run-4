e991591c4144476bfaa15aac93acfa1afd944ec3
Erik Luo <luoe@chromium.org>
2017-12-14 01:29:51 +0000

    DevTools: linkify relative links in error traces
    Screenshot: https://imgur.com/a/HvEpQ
    
    Bug: 670883
    Change-Id: Ife7c85f35e4ead089c7251931db033f1afa1f9dd
    Reviewed-on: https://chromium-review.googlesource.com/821595
    Commit-Queue: Erik Luo <luoe@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523971}

--- third_party/WebKit/Source/devtools/front_end/console/ConsoleViewMessage.js
+++ third_party/WebKit/Source/devtools/front_end/console/ConsoleViewMessage.js
@@ -1228,6 +1228,7 @@ Console.ConsoleViewMessage = class {
     if (!this._message.runtimeModel() || !errorPrefixes.some(startsWith))
       return null;
     var debuggerModel = this._message.runtimeModel().debuggerModel();
+    var baseURL = this._message.runtimeModel().target().inspectedURL();
 
     var lines = string.split('\n');
     var links = [];
@@ -1256,15 +1257,12 @@ Console.ConsoleViewMessage = class {
       if (!splitResult)
         return null;
 
-      var parsed = splitResult.url.asParsedURL();
-      var url;
-      if (parsed)
-        url = parsed.url;
-      else if (debuggerModel.scriptsForSourceURL(splitResult.url).length)
-        url = splitResult.url;
-      else if (splitResult.url === '<anonymous>')
+      if (splitResult.url === '<anonymous>')
         continue;
-      else
+      var url = parseOrScriptMatch(splitResult.url);
+      if (!url && Common.ParsedURL.isRelativeURL(splitResult.url))
+        url = parseOrScriptMatch(Common.ParsedURL.completeURL(baseURL, splitResult.url));
+      if (!url)
         return null;
 
       links.push({
@@ -1293,6 +1291,21 @@ Console.ConsoleViewMessage = class {
       formattedResult.appendChild(Console.ConsoleViewMessage._linkifyStringAsFragment(string.substring(start)));
 
     return formattedResult;
+
+    /**
+     * @param {?string} url
+     * @return {?string}
+     */
+    function parseOrScriptMatch(url) {
+      if (!url)
+        return null;
+      var parsedURL = url.asParsedURL();
+      if (parsedURL)
+        return parsedURL.url;
+      if (debuggerModel.scriptsForSourceURL(url).length)
+        return url;
+      return null;
+    }
   }
 
   /**
