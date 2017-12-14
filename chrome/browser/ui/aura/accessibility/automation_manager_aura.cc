c20d7cbf6e2db2e0b5baddb8307864aca03e9a30
Katie Dektar <katydek@google.com>
2017-12-14 03:01:37 +0000

    Fix hit test: DIP must be converted to PX for RenderFrameHost hittest.
    This fixes a bug where in gmail selecting text in an open draft also
    selected text behind that pop up window. The bug was occuring because
    the hittest was returning a node that was behind the open dialog/window,
    and select-to-speak walked up its tree to find a higher level root with
    which to build the nodes to speak.
    
    The bug only showed up on high density screens because then DIP and
    PX were significantly different. On 1:1 screens it could not be
    found.
    
    A test is added which fails without this one-line change but passes
    with it.
    
    Bug: 792545
    Change-Id: I21519b6cfaae97cb11c711b11b7a6b3e4b6fe017
    Reviewed-on: https://chromium-review.googlesource.com/826043
    Commit-Queue: Katie D <katie@chromium.org>
    Reviewed-by: Alice Boxhall <aboxhall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523992}

--- chrome/browser/ui/aura/accessibility/automation_manager_aura.cc
+++ chrome/browser/ui/aura/accessibility/automation_manager_aura.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/accessibility/platform/aura_window_properties.h"
 #include "ui/aura/env.h"
 #include "ui/aura/window.h"
+#include "ui/aura/window_tree_host.h"
 #include "ui/views/accessibility/ax_aura_obj_wrapper.h"
 #include "ui/views/view.h"
 #include "ui/views/widget/widget.h"
@@ -249,8 +250,11 @@ void AutomationManagerAura::PerformHitTest(
 
     content::RenderFrameHost* rfh =
         content::RenderFrameHost::FromAXTreeID(child_ax_tree_id);
-    if (rfh)
+    if (rfh) {
+      // Convert to pixels for the RenderFrameHost HitTest.
+      window->GetHost()->ConvertDIPToPixels(&action.target_point);
       rfh->AccessibilityPerformAction(action);
+    }
     return;
   }
 
