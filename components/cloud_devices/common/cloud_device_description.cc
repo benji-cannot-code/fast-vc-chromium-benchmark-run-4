41488b1c8c170affd685b4f5b465fea82a8213f3
Lei Zhang <thestig@chromium.org>
2017-12-14 22:00:41 +0000

    Simplify CloudDeviceDescription.
    Fold some small methods that are only called once into their callers.
    
    Change-Id: Ib3e9563113292dc381e0393da09eb6c8f1935088
    Reviewed-on: https://chromium-review.googlesource.com/827587
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524198}

--- components/cloud_devices/common/cloud_device_description.cc
+++ components/cloud_devices/common/cloud_device_description.cc
@@ -10,42 +10,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/json/json_reader.h"
 #include "base/json/json_writer.h"
 #include "base/logging.h"
-#include "base/memory/ptr_util.h"
 #include "base/values.h"
 #include "components/cloud_devices/common/cloud_device_description_consts.h"
 
 namespace cloud_devices {
 
-CloudDeviceDescription::CloudDeviceDescription() {
-  Reset();
-}
-
-CloudDeviceDescription::~CloudDeviceDescription() {
-}
-
-void CloudDeviceDescription::Reset() {
-  root_.reset(new base::DictionaryValue);
+CloudDeviceDescription::CloudDeviceDescription()
+    : root_(std::make_unique<base::DictionaryValue>()) {
   root_->SetString(json::kVersion, json::kVersion10);
 }
 
-bool CloudDeviceDescription::InitFromDictionary(
-    std::unique_ptr<base::DictionaryValue> root) {
-  if (!root)
-    return false;
-  Reset();
-  root_ = std::move(root);
-  std::string version;
-  root_->GetString(json::kVersion, &version);
-  return version == json::kVersion10;
-}
+CloudDeviceDescription::~CloudDeviceDescription() = default;
 
 bool CloudDeviceDescription::InitFromString(const std::string& json) {
-  std::unique_ptr<base::Value> parsed = base::JSONReader::Read(json);
-  base::DictionaryValue* description = nullptr;
-  if (!parsed || !parsed->GetAsDictionary(&description))
+  auto parsed = base::DictionaryValue::From(base::JSONReader::Read(json));
+  if (!parsed)
     return false;
-  ignore_result(parsed.release());
-  return InitFromDictionary(base::WrapUnique(description));
+
+  root_ = std::move(parsed);
+  const base::Value* version = root_->FindKey(json::kVersion);
+  return version && version->GetString() == json::kVersion10;
 }
 
 std::string CloudDeviceDescription::ToString() const {
@@ -64,7 +48,7 @@ const base::DictionaryValue* CloudDeviceDescription::GetItem(
 
 base::DictionaryValue* CloudDeviceDescription::CreateItem(
     const std::string& path) {
-  return root_->SetDictionary(path, base::MakeUnique<base::DictionaryValue>());
+  return root_->SetDictionary(path, std::make_unique<base::DictionaryValue>());
 }
 
 const base::ListValue* CloudDeviceDescription::GetListItem(
@@ -76,7 +60,7 @@ const base::ListValue* CloudDeviceDescription::GetListItem(
 
 base::ListValue* CloudDeviceDescription::CreateListItem(
     const std::string& path) {
-  return root_->SetList(path, base::MakeUnique<base::ListValue>());
+  return root_->SetList(path, std::make_unique<base::ListValue>());
 }
 
 }  // namespace cloud_devices
--- components/cloud_devices/common/cloud_device_description.h
+++ components/cloud_devices/common/cloud_device_description.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
-#include "base/callback.h"
 #include "base/macros.h"
 
 namespace base {
@@ -27,9 +26,6 @@ class CloudDeviceDescription {
   CloudDeviceDescription();
   ~CloudDeviceDescription();
 
-  void Reset();
-
-  bool InitFromDictionary(std::unique_ptr<base::DictionaryValue> root);
   bool InitFromString(const std::string& json);
 
   std::string ToString() const;
