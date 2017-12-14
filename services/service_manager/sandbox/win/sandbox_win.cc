45ed7edcbb0115a9146079ad2f06238a398218f3
Brian White <bcwhite@chromium.org>
2017-12-14 17:57:52 +0000

    Track process only if launch is successful.
    If the process launch fails it ends up trying to track PID #0 which
    causes a DCHECK.
    
    Bug: 757946
    Change-Id: If4558b1707aad237273127365c5b5be64d32f327
    Reviewed-on: https://chromium-review.googlesource.com/826085
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Commit-Queue: Brian White <bcwhite@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524111}

--- services/service_manager/sandbox/win/sandbox_win.cc
+++ services/service_manager/sandbox/win/sandbox_win.cc
@@ -839,13 +839,6 @@ sandbox::ResultCode SandboxWin::StartSandboxedProcess(
 
   TRACE_EVENT_END0("startup", "StartProcessWithAccess::LAUNCHPROCESS");
 
-  base::debug::GlobalActivityTracker* tracker =
-      base::debug::GlobalActivityTracker::Get();
-  if (tracker) {
-    tracker->RecordProcessLaunch(target.process_id(),
-                                 cmd_line->GetCommandLineString());
-  }
-
   if (sandbox::SBOX_ALL_OK != result) {
     base::UmaHistogramSparse("Process.Sandbox.Launch.Error", last_error);
     if (result == sandbox::SBOX_ERROR_GENERIC)
@@ -853,6 +846,13 @@ sandbox::ResultCode SandboxWin::StartSandboxedProcess(
     else
       DLOG(ERROR) << "Failed to launch process. Error: " << result;
     return result;
+  }
+
+  base::debug::GlobalActivityTracker* tracker =
+      base::debug::GlobalActivityTracker::Get();
+  if (tracker) {
+    tracker->RecordProcessLaunch(target.process_id(),
+                                 cmd_line->GetCommandLineString());
   }
 
   if (sandbox::SBOX_ALL_OK != last_warning)
