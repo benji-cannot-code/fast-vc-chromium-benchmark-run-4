28ee0aebd6e86b3991ce3ae955ab2ed02cf69f92
James Cook <jamescook@chromium.org>
2017-12-14 18:36:31 +0000

    Only allow switch --mash on Chrome OS builds
    The switch is used to enable out-of-process chrome os system ui (ash).
    It used to be used by the Linux Ozone build, but isn't anymore.
    Limit it to OS_CHROMEOS builds to make it clear where it is actually
    used.
    
    Bug: 722527
    Test: bots, chrome --mash, browser_tests --mash
    Change-Id: If980d1f843950f93f89404b658108ae0091639d9
    Reviewed-on: https://chromium-review.googlesource.com/823259
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524122}

--- chrome/browser/chromeos/first_run/goodies_displayer_browsertest.cc
+++ chrome/browser/chromeos/first_run/goodies_displayer_browsertest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/test_launcher_utils.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/prefs/pref_service.h"
+#include "ui/base/ui_base_switches.h"
 
 namespace chromeos {
 
