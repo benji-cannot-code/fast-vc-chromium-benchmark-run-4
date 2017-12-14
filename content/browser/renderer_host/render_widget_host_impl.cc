20234bc4b030d6bdddc0ff3ba98ea9ac5042e21d
Pedro Amaral <amaralp@chromium.org>
2017-12-14 02:08:26 +0000

    Remove Hang Monitor UMA
    Removing unused metric
    
    Bug: 794038
    Change-Id: I53c4c6cfd09db60946bd4e02119ba0363c77b266
    Reviewed-on: https://chromium-review.googlesource.com/823174
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Pedro Amaral <amaralp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523985}

--- content/browser/renderer_host/render_widget_host_impl.cc
+++ content/browser/renderer_host/render_widget_host_impl.cc
@@ -1049,8 +1049,6 @@ void RenderWidgetHostImpl::ProcessIgnoreInputEventsChanged(
 void RenderWidgetHostImpl::StartHangMonitorTimeout(
     base::TimeDelta delay,
     blink::WebInputEvent::Type event_type) {
-  if (hang_start_time_.is_null())
-    hang_start_time_ = TimeTicks::Now();
   if (!hang_monitor_timeout_)
     return;
   if (!hang_monitor_timeout_->IsRunning())
@@ -1060,12 +1058,8 @@ void RenderWidgetHostImpl::StartHangMonitorTimeout(
 }
 
 void RenderWidgetHostImpl::RestartHangMonitorTimeoutIfNecessary() {
-  if (in_flight_event_count_ > 0 && !is_hidden_) {
-    LogHangMonitorUnresponsive();
-    hang_start_time_ = TimeTicks::Now();
-    if (hang_monitor_timeout_)
-      hang_monitor_timeout_->Restart(hung_renderer_delay_);
-  }
+  if (hang_monitor_timeout_ && in_flight_event_count_ > 0 && !is_hidden_)
+    hang_monitor_timeout_->Restart(hung_renderer_delay_);
 }
 
 bool RenderWidgetHostImpl::IsCurrentlyUnresponsive() const {
@@ -1073,18 +1067,9 @@ bool RenderWidgetHostImpl::IsCurrentlyUnresponsive() const {
 }
 
 void RenderWidgetHostImpl::StopHangMonitorTimeout() {
-  LogHangMonitorUnresponsive();
-  hang_start_time_ = TimeTicks();
   if (hang_monitor_timeout_)
     hang_monitor_timeout_->Stop();
   RendererIsResponsive();
-}
-
-void RenderWidgetHostImpl::LogHangMonitorUnresponsive() {
-  if (!hang_start_time_.is_null()) {
-    UMA_HISTOGRAM_TIMES("MPArch.RWH_HangMonitorUnresponsive",
-                        TimeTicks::Now() - hang_start_time_);
-  }
 }
 
 void RenderWidgetHostImpl::StartNewContentRenderingTimeout(
