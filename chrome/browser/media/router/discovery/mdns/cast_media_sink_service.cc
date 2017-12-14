7005f82c1720f6aab5362675a23568c332e8f0e0
Derek Cheng <imcheng@chromium.org>
2017-12-14 00:01:03 +0000

    [Media Router] Gate mDNS discovery on Windows.
    mDNS discovery can trigger a firewall prompt on a fresh user-level
    Chrome install on Windows or if there are no existing firewall rules
    for mDNS traffic(?). This patch applies the same gating logic to
    the mdns logic in in-browser cast discovery.
    
    TODO: merge this patch back to M64.
    
    Bug: 794317
    Change-Id: I5df1a2f60eb3a8411b642fc8996f2e0279912401
    Reviewed-on: https://chromium-review.googlesource.com/823244
    Commit-Queue: Derek Cheng <imcheng@chromium.org>
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523942}

--- chrome/browser/media/router/discovery/mdns/cast_media_sink_service.cc
+++ chrome/browser/media/router/discovery/mdns/cast_media_sink_service.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/memory/ptr_util.h"
 #include "base/strings/string_number_conversions.h"
+#include "build/build_config.h"
 #include "chrome/browser/media/router/discovery/discovery_network_monitor.h"
 #include "chrome/browser/media/router/discovery/mdns/cast_media_sink_service_impl.h"
 #include "chrome/browser/media/router/discovery/mdns/dns_sd_delegate.h"
@@ -118,14 +119,9 @@ void CastMediaSinkService::Start(
       FROM_HERE, base::BindOnce(&CastMediaSinkServiceImpl::Start,
                                 base::Unretained(impl_.get())));
 
-  // |dns_sd_registry_| is already set to a mock version in unit tests only.
-  // |impl_| must be initialized first because AddObserver might end up calling
-  // |OnDnsSdEvent| right away.
-  if (!dns_sd_registry_) {
-    dns_sd_registry_ = DnsSdRegistry::GetInstance();
-    dns_sd_registry_->AddObserver(this);
-    dns_sd_registry_->RegisterDnsSdListener(kCastServiceType);
-  }
+#if !defined(OS_WIN)
+  StartMdnsDiscovery();
+#endif
 }
 
 std::unique_ptr<CastMediaSinkServiceImpl, base::OnTaskRunnerDeleter>
@@ -141,6 +137,18 @@ CastMediaSinkService::CreateImpl(
           DiscoveryNetworkMonitor::GetInstance(),
           Profile::FromBrowserContext(browser_context_)->GetRequestContext()),
       base::OnTaskRunnerDeleter(task_runner));
+}
+
+void CastMediaSinkService::StartMdnsDiscovery() {
+  // |dns_sd_registry_| is already set to a mock version in unit tests only.
+  // |impl_| must be initialized first because AddObserver might end up calling
+  // |OnDnsSdEvent| right away.
+  DCHECK(impl_);
+  if (!dns_sd_registry_) {
+    dns_sd_registry_ = DnsSdRegistry::GetInstance();
+    dns_sd_registry_->AddObserver(this);
+    dns_sd_registry_->RegisterDnsSdListener(kCastServiceType);
+  }
 }
 
 void CastMediaSinkService::ForceSinkDiscoveryCallback() {
