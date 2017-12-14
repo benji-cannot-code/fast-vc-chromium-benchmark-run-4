a069ed8764c5595b00d69d23f6f2c31f9cada711
David Jacobo <djacobo@chromium.org>
2017-12-14 00:57:30 +0000

    Adjust IntentPickerBubbleView wrt spec.
    - Adding an horizontal Separator between the list of apps and the "Rememeber
    my choice" checkbox.
    - Adjusting the row size for app candidates from 40 to 32, reducing the
    icon from 20 to 16 pt.
    - Changing the color of the InkDrop from black to gray.
    
    Bug: 678141
    Test: Manually checked the UI. Build
    Change-Id: I5697d0aa3e0aa2b2f2b2e9cb627061c43c74f587
    Reviewed-on: https://chromium-review.googlesource.com/825963
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: David Jacobo <djacobo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523962}

--- chrome/browser/ui/views/intent_picker_bubble_view.cc
+++ chrome/browser/ui/views/intent_picker_bubble_view.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/button/checkbox.h"
 #include "ui/views/controls/button/image_button.h"
 #include "ui/views/controls/scroll_view.h"
+#include "ui/views/controls/separator.h"
 #include "ui/views/layout/box_layout.h"
 #include "ui/views/layout/grid_layout.h"
 #include "ui/views/window/dialog_client_view.h"
@@ -38,8 +39,10 @@ namespace {
 constexpr size_t kMaxAppResults = arc::ArcNavigationThrottle::kMaxAppResults;
 // Main components sizes
 constexpr int kTitlePadding = 16;
-constexpr int kRowHeight = 40;
+constexpr int kRowHeight = 32;
 constexpr int kMaxWidth = 320;
+constexpr gfx::Insets kSeparatorPadding(16, 0, 16, 0);
+constexpr SkColor kSeparatorColor = SkColorSetARGB(0x1F, 0x0, 0x0, 0x0);
 
 // UI position wrt the Top Container
 constexpr int kTopContainerMerge = 3;
@@ -49,6 +52,13 @@ constexpr char kInvalidPackageName[] = "";
 bool IsKeyboardCodeArrow(ui::KeyboardCode key_code) {
   return key_code == ui::VKEY_UP || key_code == ui::VKEY_DOWN ||
          key_code == ui::VKEY_RIGHT || key_code == ui::VKEY_LEFT;
+}
+
+views::Separator* CreateHorizontalSeparator() {
+  views::Separator* separator = new views::Separator;
+  separator->SetColor(kSeparatorColor);
+  separator->SetBorder(views::CreateEmptyBorder(kSeparatorPadding));
+  return separator;
 }
 
 }  // namespace
@@ -70,10 +80,10 @@ class IntentPickerLabelButton : public views::LabelButton {
     SetInkDropMode(InkDropMode::ON);
     if (!icon->IsEmpty())
       SetImage(views::ImageButton::STATE_NORMAL, *icon->ToImageSkia());
-    SetBorder(views::CreateEmptyBorder(10, 16, 10, 0));
+    SetBorder(views::CreateEmptyBorder(8, 16, 8, 0));
   }
 
-  SkColor GetInkDropBaseColor() const override { return SK_ColorBLACK; }
+  SkColor GetInkDropBaseColor() const override { return SK_ColorGRAY; }
 
   void MarkAsUnselected(const ui::Event* event) {
     AnimateInkDrop(views::InkDropState::HIDDEN,
@@ -223,6 +233,7 @@ void IntentPickerBubbleView::Init() {
     app_button->set_tag(i);
     scrollable_view->AddChildViewAt(app_button, i++);
   }
+
   // We should delete at most one entry, this is the case when Chrome is listed
   // as a candidate to handle a given URL.
   if (to_erase != app_info_.size())
@@ -249,6 +260,8 @@ void IntentPickerBubbleView::Init() {
 
   layout->StartRowWithPadding(0, kColumnSetId, 0, kTitlePadding);
   layout->AddView(scroll_view_);
+  layout->StartRow(0, kColumnSetId, 0);
+  layout->AddView(CreateHorizontalSeparator());
 
   // This second ColumnSet has a padding column in order to manipulate the
   // Checkbox positioning freely.
@@ -259,12 +272,12 @@ void IntentPickerBubbleView::Init() {
                        views::GridLayout::FIXED, kMaxWidth - 2 * kTitlePadding,
                        0);
 
-  layout->StartRowWithPadding(0, kColumnSetIdPadded, 0, kTitlePadding / 2);
+  layout->StartRowWithPadding(0, kColumnSetIdPadded, 0, 0);
   remember_selection_checkbox_ = new views::Checkbox(l10n_util::GetStringUTF16(
       IDS_INTENT_PICKER_BUBBLE_VIEW_REMEMBER_SELECTION));
   layout->AddView(remember_selection_checkbox_);
 
-  layout->AddPaddingRow(0, kTitlePadding / 2);
+  layout->AddPaddingRow(0, kTitlePadding);
 }
 
 base::string16 IntentPickerBubbleView::GetWindowTitle() const {
