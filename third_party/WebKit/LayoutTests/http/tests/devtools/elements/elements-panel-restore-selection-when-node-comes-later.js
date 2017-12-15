27883e18c376261466b78369fc7ce3bbb574375e
Andrey Lushnikov <lushnikov@chromium.org>
2017-12-15 02:13:05 +0000

    DevTools: fix elements-panel-restore-selection-when-node-comes-later.js
    BUG=660295
    TBR=dgozman
    
    Change-Id: I977dc25453e2dbe0f92e95bf1111d6122de747b7
    Reviewed-on: https://chromium-review.googlesource.com/828148
    Commit-Queue: Andrey Lushnikov <lushnikov@chromium.org>
    Reviewed-by: Andrey Lushnikov <lushnikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524291}

--- third_party/WebKit/LayoutTests/http/tests/devtools/elements/elements-panel-restore-selection-when-node-comes-later.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/elements/elements-panel-restore-selection-when-node-comes-later.js
@@ -8,16 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       `Verify that last selected element is restored properly later, even if it failed to do so once.\n`);
   await TestRunner.loadModule('elements_test_runner');
   await TestRunner.showPanel('elements');
-  await TestRunner.loadHTML(`
-      <p>
-      Verify that last selected element is restored properly later, even if
-      it failed to do so once.
-      </p>
-      <div>
-          <span id="inspected"></span>
-      </div>
-    `);
-  await TestRunner.addScriptTag('../resources/elements-panel-shadow-selection-on-refresh.js');
+  await TestRunner.navigatePromise('./resources/elements-panel-restore-selection-when-node-comes-later.html');
 
   var node;
 
@@ -81,17 +72,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 
   /**
-     * @param {string} pathToIgnore
-     */
+   * @param {string} pathToIgnore
+   */
   function overridePushNodeForPath(pathToIgnore) {
     var original = TestRunner.override(SDK.DOMModel.prototype, 'pushNodeByPathToFrontend', override);
 
-    function override(nodePath, callback) {
-      if (nodePath === pathToIgnore) {
-        setTimeout(callback.bind(null), 0);
-        return;
-      }
-      original(nodePath, callback);
+    function override(nodePath) {
+      if (nodePath === pathToIgnore)
+        return Promise.resolve(null);
+      return original(nodePath);
     }
   }
 })();
