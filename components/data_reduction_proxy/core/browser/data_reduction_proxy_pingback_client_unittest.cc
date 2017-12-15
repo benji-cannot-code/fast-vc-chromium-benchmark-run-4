3047b5e2e2476363a67e8c002facfe9bcd780f87
Ryan Sturm <ryansturm@chromium.org>
2017-12-15 02:20:46 +0000

    Revert "Adding device memory in kb to the client config request"
    This reverts commit 1e920b32c7c3000523870464e0a8954a0474790e.
    
    Reason for revert: I want to move device info in
    Pageload metrics up to the broader report.
    
    
    Original change's description:
    > Adding device memory in kb to the client config request
    > 
    > pingback will also have this information
    > 
    > Bug: 793373
    > Change-Id: I0c999c35b5e968f6b2e19696a08a722b3399d0d9
    > Reviewed-on: https://chromium-review.googlesource.com/823176
    > Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    > Commit-Queue: Ryan Sturm <ryansturm@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#524203}
    
    TBR=tbansal@chromium.org,prabhur@chromium.org,ryansturm@chromium.org
    
    Change-Id: Iba67a80dfbefa90a065b2f02c692ac2e1c23fd70
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 793373
    Reviewed-on: https://chromium-review.googlesource.com/828540
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Ryan Sturm <ryansturm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524295}

--- components/data_reduction_proxy/core/browser/data_reduction_proxy_pingback_client_unittest.cc
+++ components/data_reduction_proxy/core/browser/data_reduction_proxy_pingback_client_unittest.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/field_trial.h"
 #include "base/optional.h"
 #include "base/run_loop.h"
-#include "base/sys_info.h"
 #include "base/test/histogram_tester.h"
 #include "base/time/time.h"
 #include "components/data_reduction_proxy/core/browser/data_reduction_proxy_data.h"
@@ -223,9 +222,6 @@ TEST_F(DataReductionProxyPingbackClientTest, VerifyPingbackContent) {
       PageloadMetrics_EffectiveConnectionType_EFFECTIVE_CONNECTION_TYPE_OFFLINE,
       pageload_metrics.effective_connection_type());
   EXPECT_EQ(std::string(), pageload_metrics.holdback_group());
-  EXPECT_EQ(base::SysInfo::AmountOfPhysicalMemory() / 1024,
-            pageload_metrics.device_info().total_device_memory_kb());
-
   test_fetcher->delegate()->OnURLFetchComplete(test_fetcher);
   histogram_tester().ExpectUniqueSample(kHistogramSucceeded, true, 1);
   EXPECT_FALSE(factory()->GetFetcherByID(0));
@@ -336,9 +332,6 @@ TEST_F(DataReductionProxyPingbackClientTest, VerifyTwoPingbacksBatchedContent) {
     EXPECT_EQ(
         PageloadMetrics_EffectiveConnectionType_EFFECTIVE_CONNECTION_TYPE_OFFLINE,
         pageload_metrics.effective_connection_type());
-
-    EXPECT_EQ(base::SysInfo::AmountOfPhysicalMemory() / 1024,
-              pageload_metrics.device_info().total_device_memory_kb());
   }
 
   test_fetcher->delegate()->OnURLFetchComplete(test_fetcher);
