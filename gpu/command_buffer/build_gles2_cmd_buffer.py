6c90be685163f8a302e636370b20120b2c0afa7a
Sunny Sachanandani <sunnyps@chromium.org>
2017-12-15 23:08:53 +0000

    gpu: Document ordering barrier, shallow flush, and shallow finish.
    These have been undocumented for a long time. This CL documents these
    in the CHROMIUM_ordering_barrier extension.
    
    R=piman
    BUG=NONE
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I3c9a4a7e4634e5bfb760f3852fb07b05e8eed5eb
    Reviewed-on: https://chromium-review.googlesource.com/826486
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524504}

--- gpu/command_buffer/build_gles2_cmd_buffer.py
+++ gpu/command_buffer/build_gles2_cmd_buffer.py
@@ -4312,15 +4312,15 @@ _FUNCTION_INFO = {
   },
   'ShallowFinishCHROMIUM': {
     'type': 'NoCommand',
-    'extension': True,
+    'extension': 'CHROMIUM_ordering_barrier',
   },
   'ShallowFlushCHROMIUM': {
     'type': 'NoCommand',
-    'extension': "CHROMIUM_miscellaneous",
+    'extension': 'CHROMIUM_ordering_barrier',
   },
   'OrderingBarrierCHROMIUM': {
     'type': 'NoCommand',
-    'extension': "CHROMIUM_miscellaneous",
+    'extension': 'CHROMIUM_ordering_barrier',
   },
   'TraceBeginCHROMIUM': {
     'type': 'Custom',
