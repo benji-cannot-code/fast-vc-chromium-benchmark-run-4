e782ce4bd6dea3f97f200bcb3093f742047a0d36
Andrey Kosyakov <caseq@chromium.org>
2017-12-15 05:29:56 +0000

    DevTools: properly plumb loadId so one in Network.* events matches one in Page.navigate
    This fixes a problem where we were using a loader id on the blink side that is different
    from DevTools navigation token on the browser side.
    
    Change-Id: Ieb82d2a7fea7f0e233eeeaf2158d46803cbb7e80
    Reviewed-on: https://chromium-review.googlesource.com/828074
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Pavel Feldman <pfeldman@chromium.org>
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524334}

---
+++ third_party/WebKit/LayoutTests/http/tests/inspector-protocol/page/navigate-loader-id.js
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  var {page, session, dp} = await testRunner.startBlank(
+      `Tests that Page.navigate() returns the loader id that matches one in network events`);
+
+  await dp.Page.enable();
+  await dp.Network.enable();
+  var navigatePromise = dp.Page.navigate({url: testRunner.url('../resources/blank.html')});
+  var requestWillBeSent = (await dp.Network.onceRequestWillBeSent()).params;
+  var navigateResult = (await  navigatePromise).result;
+
+  if (navigateResult.loaderId !== requestWillBeSent.loaderId) {
+    testRunner.fail(`loaderId from Page.navigate is ${navigateResult.loaderId}
+      and loaderId in RequestWillBeSent is ${requestWillBeSent.loaderId}`);
+  }
+  testRunner.completeTest();
+})
