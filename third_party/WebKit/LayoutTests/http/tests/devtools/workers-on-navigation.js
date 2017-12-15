6c236f5190b47ab8f14fa19bbc22ad70c593d7bd
Andrey Lushnikov <lushnikov@chromium.org>
2017-12-15 00:05:24 +0000

    DevTools: fix http/tests/devtools/workers-on-navigation.js
    The test was adding observer too late.
    
    TBR=dgozman
    BUG=700374
    
    Change-Id: Ibbe66b9fa8234142aca0ddeef6dbd8c78fc358cb
    Reviewed-on: https://chromium-review.googlesource.com/828142
    Reviewed-by: Andrey Lushnikov <lushnikov@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Andrey Lushnikov <lushnikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524244}

--- third_party/WebKit/LayoutTests/http/tests/devtools/workers-on-navigation.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/workers-on-navigation.js
@@ -32,8 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       }
     }
   };
-  await TestRunner.navigatePromise('resources/workers-on-navigation-resource.html');
   SDK.targetManager.observeTargets(observer);
+  await TestRunner.navigatePromise('resources/workers-on-navigation-resource.html');
   await TestRunner.evaluateInPagePromise('startWorker()');
   await TestRunner.reloadPagePromise();
   navigated = true;
