c396175935af8e407de47d4b6bea66a092b6fea8
Charles Harrison <csharrison@chromium.org>
2017-12-15 03:36:35 +0000

    Reland: Predictor: Dispatch all predictions async from the resource throttle
    This patch relands the following CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/821090
    
    This is a small refactor that should have some performance wins. Now
    that PlzNavigate has launched, we should be dispatching our preconnects
    after the initial navigation request has gone out, not before. This is
    because frequently (especially on Android), dispatching preconnects
    tends to hog the IO thread, delaying the initial request.
    
    Note that this predictor is probably going away, but this CPU work
    is in the critical path of some other work I am planning on tackling.
    
    Bug: 792524
    Change-Id: I03b058af249088f779dfb574c8552b32150bea2c
    Reviewed-on: https://chromium-review.googlesource.com/826783
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524308}

--- chrome/browser/loader/predictor_resource_throttle.h
+++ chrome/browser/loader/predictor_resource_throttle.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/macros.h"
+#include "base/memory/weak_ptr.h"
 #include "content/public/browser/resource_throttle.h"
 
 namespace chrome_browser_net {
@@ -20,20 +21,19 @@ struct RedirectInfo;
 class URLRequest;
 }
 
+class GURL;
 class ProfileIOData;
 
 // This resource throttle tracks requests in order to help the predictor learn
 // resource relationships. It notifies the predictor of redirect and referrer
 // relationships, and populates the predictor's timed cache of ongoing
-// navigations. It also initiates predictive actions based on subframe
+// navigations. It also initiates predictive actions based on navigation
 // requests and redirects.
 // Note: This class does not issue predictive actions off of the initial main
 // frame request (before any redirects). That is done on the UI thread in
 // response to navigation callbacks in predictor_tab_helper.cc.
 // TODO(csharrison): This class shouldn't depend on chrome_browser_net. The
 // predictor should probably be moved here (along with its dependencies).
-// TODO(csharrison): When the PreconnectMore experiment is over, move all
-// prediction dispatching to the tab helper.
 class PredictorResourceThrottle : public content::ResourceThrottle {
  public:
   PredictorResourceThrottle(net::URLRequest* request,
@@ -51,8 +51,13 @@ class PredictorResourceThrottle : public content::ResourceThrottle {
   const char* GetNameForLogging() const override;
 
  private:
+  void DispatchPredictions(const GURL& url, const GURL& site_for_cookies);
+  void DoPredict(const GURL& url, const GURL& site_for_cookies);
+
   net::URLRequest* request_;
   chrome_browser_net::Predictor* predictor_;
+
+  base::WeakPtrFactory<PredictorResourceThrottle> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(PredictorResourceThrottle);
 };
