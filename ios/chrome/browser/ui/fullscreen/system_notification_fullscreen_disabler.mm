2a76b220811cfc782ed219cbc121c64154bb8165
Kurt Horimoto <kkhorimoto@chromium.org>
2017-12-15 23:40:32 +0000

    [iOS] Disable fullscreen while the keyboard is visible.
    In addition to adding keyboard notification observation, this
    CL also moves VoiceOver fullscreen disabling into a shared
    object.
    
    Bug: 793129
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Ia8f8501ff7f5e97ec9d794e6c3d33687e90f4d9f
    Reviewed-on: https://chromium-review.googlesource.com/816356
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524518}

--- ios/chrome/browser/ui/fullscreen/system_notification_fullscreen_disabler.mm
+++ ios/chrome/browser/ui/fullscreen/system_notification_fullscreen_disabler.mm
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/ui/fullscreen/voice_over_fullscreen_disabler.h"
+#import "ios/chrome/browser/ui/fullscreen/system_notification_fullscreen_disabler.h"
 
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
@@ -14,23 +14,31 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "This file requires ARC support."
 #endif
 
-@interface VoiceOverFullscreenDisabler () {
+@interface SystemNotificationFullscreenDisabler () {
   // The disabler created when VoiceOver is enabled.
-  std::unique_ptr<ScopedFullscreenDisabler> _disabler;
+  std::unique_ptr<ScopedFullscreenDisabler> _voiceOverDisabler;
+  // The disabler created when the keyboard is visible.
+  std::unique_ptr<ScopedFullscreenDisabler> _keyboardDisabler;
 }
 // The FullscreenController being enabled/disabled for VoiceOver.
 @property(nonatomic, readonly, nonnull) FullscreenController* controller;
-// Creates or destroys |_disabler| depending on whether VoiceOver is enabled.
+// Creates or destroys |_voiceOverDisabler| depending on whether VoiceOver is
+// enabled.
 - (void)voiceOverStatusChanged;
+// Called when the keyboard is shown/hidden to reset |_keyboardDisabler|.
+- (void)keyboardWillShow;
+- (void)keyboardDidHide;
 @end
 
-@implementation VoiceOverFullscreenDisabler
+@implementation SystemNotificationFullscreenDisabler
 @synthesize controller = _controller;
 
 - (instancetype)initWithController:(FullscreenController*)controller {
   if (self = [super init]) {
     _controller = controller;
     DCHECK(_controller);
+    // Register for VoiceOVer status change notifications.  The notification
+    // name has been updated in iOS 11.
     if (@available(iOS 11, *)) {
       [[NSNotificationCenter defaultCenter]
           addObserver:self
@@ -44,8 +52,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                  name:UIAccessibilityVoiceOverStatusChanged
                object:nil];
     }
-    if (UIAccessibilityIsVoiceOverRunning())
-      _disabler = base::MakeUnique<ScopedFullscreenDisabler>(_controller);
+    // Create a disabler if VoiceOver is enabled.
+    if (UIAccessibilityIsVoiceOverRunning()) {
+      _voiceOverDisabler =
+          base::MakeUnique<ScopedFullscreenDisabler>(_controller);
+    }
+    // Regsiter for keyboard visibility notifications.
+    [[NSNotificationCenter defaultCenter]
+        addObserver:self
+           selector:@selector(keyboardWillShow)
+               name:UIKeyboardWillShowNotification
+             object:nil];
+    [[NSNotificationCenter defaultCenter]
+        addObserver:self
+           selector:@selector(keyboardDidHide)
+               name:UIKeyboardDidHideNotification
+             object:nil];
   }
   return self;
 }
@@ -65,9 +87,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma mark Private
 
 - (void)voiceOverStatusChanged {
-  _disabler = UIAccessibilityIsVoiceOverRunning()
-                  ? base::MakeUnique<ScopedFullscreenDisabler>(_controller)
-                  : nullptr;
+  _voiceOverDisabler =
+      UIAccessibilityIsVoiceOverRunning()
+          ? base::MakeUnique<ScopedFullscreenDisabler>(self.controller)
+          : nullptr;
+}
+
+- (void)keyboardWillShow {
+  _keyboardDisabler =
+      base::MakeUnique<ScopedFullscreenDisabler>(self.controller);
+}
+
+- (void)keyboardDidHide {
+  _keyboardDisabler = nullptr;
 }
 
 @end
