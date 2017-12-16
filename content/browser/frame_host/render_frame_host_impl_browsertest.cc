e19c53bc8465915176b2c2975d08c02bfcc3abe8
Kenneth Russell <kbr@chromium.org>
2017-12-16 01:09:03 +0000

    Disable IframeBeforeUnloadParentHang on Linux TSAN.
    Disabling this RenderFrameHostImplBrowserTest on this configuration as
    it's frequently hanging and blocking the CQ.
    
    BUG=795326
    TBR=avi@chromium.org
    
    Change-Id: I88e5b4158de3c92113b767b00b64d88e11ae09ca
    Reviewed-on: https://chromium-review.googlesource.com/831127
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524544}

--- content/browser/frame_host/render_frame_host_impl_browsertest.cc
+++ content/browser/frame_host/render_frame_host_impl_browsertest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/test/bind_test_util.h"
 #include "base/test/mock_callback.h"
+#include "build/build_config.h"
 #include "content/browser/frame_host/navigation_handle_impl.h"
 #include "content/browser/interface_provider_filtering.h"
 #include "content/browser/web_contents/web_contents_impl.h"
@@ -278,8 +279,14 @@ class DropBeforeUnloadACKFilter : public BrowserMessageFilter {
 
 // Tests that a beforeunload dialog in an iframe doesn't stop the beforeunload
 // timer of a parent frame.
+// TODO(avi): flaky on Linux TSAN: http://crbug.com/795326
+#if defined(OS_LINUX) && defined(THREAD_SANITIZER)
+#define MAYBE_IframeBeforeUnloadParentHang DISABLED_IframeBeforeUnloadParentHang
+#else
+#define MAYBE_IframeBeforeUnloadParentHang IframeBeforeUnloadParentHang
+#endif
 IN_PROC_BROWSER_TEST_F(RenderFrameHostImplBrowserTest,
-                       IframeBeforeUnloadParentHang) {
+                       MAYBE_IframeBeforeUnloadParentHang) {
   WebContentsImpl* wc = static_cast<WebContentsImpl*>(shell()->web_contents());
   TestJavaScriptDialogManager dialog_manager;
   wc->SetDelegate(&dialog_manager);
