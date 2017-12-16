db708c07040d1719e512e21e7736d1b01c0a7e19
Aleks Totic <atotic@chromium.org>
2017-12-16 00:41:39 +0000

    [LayoutNG] UpdateMargins is no longer needed by LayoutNGBlockFlow
    After CL 827548, margins are always copied to Legacy.
    Additional copy in LayoutNGBlockFlow is no longer needed.
    Ian says so.
    
    Bug: 740993
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: If1aee4b15a966d58bd62f66190fdf00f035ed0b4
    Reviewed-on: https://chromium-review.googlesource.com/830474
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Aleks Totic <atotic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524538}

--- third_party/WebKit/Source/core/layout/ng/layout_ng_mixin.h
+++ third_party/WebKit/Source/core/layout/ng/layout_ng_mixin.h
@@ -76,8 +76,6 @@ class CORE_TEMPLATE_CLASS_EXPORT LayoutNGMixin : public Base {
 
   const NGBaseline* FragmentBaseline(NGBaselineAlgorithmType) const;
 
-  void UpdateMargins(const NGConstraintSpace&);
-
   std::unique_ptr<NGInlineNodeData> ng_inline_node_data_;
 
   scoped_refptr<NGLayoutResult> cached_result_;
