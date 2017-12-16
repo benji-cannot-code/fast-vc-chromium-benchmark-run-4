eb0837139ee53653d9e457195a79a86901580847
dpapad <dpapad@chromium.org>
2017-12-16 01:13:45 +0000

    MD Extensions: Disable "Extension options" button when extension is disabled.
    Convert the "Extension options" row to a cr-link-row and leverage the existing
    styling for the disabled case.
    
    Bug: 794756
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I7139e20a17d5e0ac5c85f0e1da7a5ad3bd2ab3d9
    Reviewed-on: https://chromium-review.googlesource.com/826484
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Dave Schuyler <dschuyler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524548}

--- chrome/browser/resources/md_extensions/detail_view.js
+++ chrome/browser/resources/md_extensions/detail_view.js
@@ -147,7 +147,7 @@ cr.define('extensions', function() {
     },
 
     /** @private */
-    onOptionsTap_: function() {
+    onExtensionOptionsTap_: function() {
       this.delegate.showItemOptionsPage(this.data);
     },
 
