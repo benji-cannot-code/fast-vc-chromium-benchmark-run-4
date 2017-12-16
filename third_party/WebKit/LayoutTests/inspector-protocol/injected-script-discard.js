e18f9baf95e92b408e06dfe93324904bee391f34
Will Chen <chenwilliam@chromium.org>
2017-12-16 00:01:45 +0000

    DevTools: migrate the last startup tests
    * reattach-after-editing-styles.html - migrated into a protocol test.
    * shadow-dom-rules-restart.html - migrated into a protocol test.
    * injected-script-discard.html - migrated into a protocol test.
    * tabbed-pane-closeable-persistence-restore.html - removed. It required an
    unusual testing API and has adequate coverage from tabbed-pane-closeable-persistence.js
    and browser test "DevToolsSanityTest.TestSettings"
    * open-close-open.html - tried migrating into a browser test, but found a race condition
    with quickly opening, closing, and re-opening DevTools window. This test covered a very
    specific regression and had a very specific implementation fix which doesn't seem applicable
    anymore: https://bugs.webkit.org/show_bug.cgi?id=56747
    
    Bug: 667560
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: I41b0ea3e7837dee5049cc973354c431d09d9ae7f
    Reviewed-on: https://chromium-review.googlesource.com/827647
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Andrey Lushnikov <lushnikov@chromium.org>
    Commit-Queue: Will Chen <chenwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524530}

---
+++ third_party/WebKit/LayoutTests/inspector-protocol/injected-script-discard.js
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  testRunner.log('Tests that injected script is discarded upon front-end close.');
+  var page = await testRunner.createPage();
+
+  testRunner.log('Opening session #1');
+  var session = await page.createSession();
+  var {result} = await session.protocol.Runtime.evaluate({expression: '({ handle : "handle" })' });
+  var objectId = result.result.objectId;
+  var properties = await session.protocol.Runtime.getProperties({objectId: objectId, ownProperties: false});
+  testRunner.log('Can resolve object: ' + !properties.error);
+  testRunner.log('Disconnecting session #1');
+  await session.disconnect();
+
+  testRunner.log('Opening session #2');
+  session = await page.createSession();
+  properties = await session.protocol.Runtime.getProperties({objectId: objectId, ownProperties: false});
+  testRunner.log('Can resolve object: ' + !properties.error);
+  testRunner.log('Error:');
+  testRunner.log(properties.error);
+  testRunner.completeTest();
+})
