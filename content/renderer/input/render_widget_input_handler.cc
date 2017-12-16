54fb54994b5cdf0b722216d3502abe015dcf87fc
Navid Zolghadr <nzolghadr@chromium.org>
2017-12-16 07:03:36 +0000

    Add mojo interface for renderer hit test API
    Expose renderer hit test API to the browser
    with a mojo interface. With this API browser
    can ask a renderer about the target frame
    of a given point. The API returns the sink id
    of the target frame. The result frame could be
    either a remote or local frame from the
    renderer's point of view.
    
    Bug: 732405
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I2606dbeb71eb0cc7d593c80eb73cbcb24d2774ad
    Reviewed-on: https://chromium-review.googlesource.com/751721
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Navid Zolghadr <nzolghadr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524597}

--- content/renderer/input/render_widget_input_handler.cc
+++ content/renderer/input/render_widget_input_handler.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/web/WebLocalFrame.h"
 #include "third_party/WebKit/public/web/WebNode.h"
 #include "ui/events/blink/web_input_event_traits.h"
+#include "ui/gfx/geometry/dip_util.h"
 #include "ui/gfx/geometry/point_conversions.h"
 #include "ui/latency/latency_info.h"
 
@@ -160,10 +161,13 @@ RenderWidgetInputHandler::~RenderWidgetInputHandler() {}
 
 int RenderWidgetInputHandler::GetWidgetRoutingIdAtPoint(
     const gfx::Point& point) {
-  blink::WebNode result_node =
-      widget_->GetWebWidget()
-          ->HitTestResultAt(blink::WebPoint(point.x(), point.y()))
-          .GetNode();
+  gfx::PointF point_in_pixel =
+      gfx::ConvertPointToPixel(widget_->GetOriginalDeviceScaleFactor(),
+                               gfx::PointF(point.x(), point.y()));
+  blink::WebNode result_node = widget_->GetWebWidget()
+                                   ->HitTestResultAt(blink::WebPoint(
+                                       point_in_pixel.x(), point_in_pixel.y()))
+                                   .GetNode();
 
   blink::WebFrame* result_frame =
       blink::WebFrame::FromFrameOwnerElement(result_node);
