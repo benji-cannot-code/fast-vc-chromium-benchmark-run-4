d24ee05be7ffea03958691931edb76836a65eb7e
Xiaohui Chen <xiaohuic@google.com>
2017-12-17 03:39:05 +0000

    Make VoiceInteractionControllerClient a singleton.
    Previously VoiceInteractionControllerClient has the same life cycle as
    ArcVoiceInteractionFrameworkService, which is only alive when primary
    profile running and has ARC++ enabled.  This life cycle isn't a good
    fit for VoiceInteractionControllerClient since it is used to update
    Ash states, which has a global life time.  It caused bugs like in multi-
    profile, and restricted profile, Ash UI wasn't properly rendered.
    
    BUG=b:68664196
    TEST=unit tests and locally build and manually test multi-profile
    
    Change-Id: I8495e2c41274db385741b86d778c96e15a25b977
    Reviewed-on: https://chromium-review.googlesource.com/827540
    Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524616}

--- chrome/browser/chromeos/arc/voice_interaction/arc_voice_interaction_framework_service.h
+++ chrome/browser/chromeos/arc/voice_interaction/arc_voice_interaction_framework_service.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/arc/common/voice_interaction_framework.mojom.h"
 #include "components/arc/connection_observer.h"
 #include "components/keyed_service/core/keyed_service.h"
-#include "components/session_manager/core/session_manager_observer.h"
 
 class KeyedServiceBaseFactory;
 
@@ -34,7 +33,6 @@ namespace arc {
 
 class ArcBridgeService;
 class HighlighterControllerClient;
-class VoiceInteractionControllerClient;
 
 // This provides voice interaction context (currently screenshots)
 // to ARC to be used by VoiceInteractionSession. This class lives on the UI
@@ -44,8 +42,7 @@ class ArcVoiceInteractionFrameworkService
       public KeyedService,
       public mojom::VoiceInteractionFrameworkHost,
       public ConnectionObserver<mojom::VoiceInteractionFrameworkInstance>,
-      public ArcSessionManager::Observer,
-      public session_manager::SessionManagerObserver {
+      public ArcSessionManager::Observer {
  public:
   // Returns singleton instance for the given BrowserContext,
   // or nullptr if the browser |context| is not allowed to use ARC.
@@ -73,9 +70,6 @@ class ArcVoiceInteractionFrameworkService
 
   // ArcSessionManager::Observer overrides.
   void OnArcPlayStoreEnabledChanged(bool enabled) override;
-
-  // session_manager::SessionManagerObserver overrides.
-  void OnSessionStateChanged() override;
 
   // CrasAudioHandler::AudioObserver overrides.
   void OnHotwordTriggered(uint64_t tv_sec, uint64_t tv_nsec) override;
@@ -130,11 +124,6 @@ class ArcVoiceInteractionFrameworkService
     return state_;
   }
 
-  VoiceInteractionControllerClient*
-  GetVoiceInteractionControllerClientForTesting() const {
-    return voice_interaction_controller_client_.get();
-  }
-
   // For supporting ArcServiceManager::GetService<T>().
   static const char kArcServiceName[];
 
@@ -179,9 +168,6 @@ class ArcVoiceInteractionFrameworkService
   int32_t context_request_remaining_count_ = 0;
 
   std::unique_ptr<HighlighterControllerClient> highlighter_client_;
-
-  std::unique_ptr<VoiceInteractionControllerClient>
-      voice_interaction_controller_client_;
 
   base::WeakPtrFactory<ArcVoiceInteractionFrameworkService> weak_ptr_factory_;
 
--- chrome/browser/chromeos/chrome_browser_main_chromeos.cc
+++ chrome/browser/chromeos/chrome_browser_main_chromeos.cc
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/app_mode/kiosk_app_manager.h"
 #include "chrome/browser/chromeos/app_mode/kiosk_mode_idle_app_name_notification.h"
 #include "chrome/browser/chromeos/arc/arc_service_launcher.h"
+#include "chrome/browser/chromeos/arc/voice_interaction/voice_interaction_controller_client.h"
 #include "chrome/browser/chromeos/ash_config.h"
 #include "chrome/browser/chromeos/boot_times_recorder.h"
 #include "chrome/browser/chromeos/dbus/chrome_component_updater_service_provider_delegate.h"
@@ -750,6 +751,8 @@ void ChromeBrowserMainPartsChromeos::PreMainMessageLoopRun() {
       new NetworkThrottlingObserver(g_browser_process->local_state()));
 
   arc_service_launcher_ = base::MakeUnique<arc::ArcServiceLauncher>();
+  arc_voice_interaction_controller_client_ =
+      std::make_unique<arc::VoiceInteractionControllerClient>();
 
   chromeos::ResourceReporter::GetInstance()->StartMonitoring(
       task_manager::TaskManagerInterface::GetTaskManager());
@@ -1132,6 +1135,8 @@ void ChromeBrowserMainPartsChromeos::PostMainMessageLoopRun() {
   NoteTakingHelper::Shutdown();
 
   arc_service_launcher_->Shutdown();
+
+  arc_voice_interaction_controller_client_.reset();
 
   // Unregister CrosSettings observers before CrosSettings is destroyed.
   shutdown_policy_forwarder_.reset();
