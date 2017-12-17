88ef527a01ba3e28ff1ecff7d68818c06f0629bc
Morten Stenshorne <mstensho@chromium.org>
2017-12-17 21:09:08 +0000

    [LayoutNG] Fragmentainer breaks between line boxes.
    Constraint spaces for inline nodes also need to support block
    fragmentation, due to floats.
    
    We need to allow a (0,0) inline break token if we break before the first
    line. If there's no break token for the inline layout context, it means
    that we have finished all lines. Which is quite the opposite of being at
    the very beginning.
    
    There are some new test failures. One source is that we don't yet
    support column-span:all, and now that we break between lines, what used
    to pass by accident may no longer pass.
    
    Making floats in an inline formatting context play along was tricky, and
    I'm pretty sure that the solution I came up with needs to be improved.
    For now, skip floats associated with the line when we resume layout at.
    Floats that got broken inside will create break token children of the
    token created for the containing block node, and will be break token
    siblings of the inline break token to resume at (although they are not
    siblings, node-wise).
    
    The legacy layout write-back code doesn't work too great, especially
    with LayoutNGPaintFragments enabled (but there are problems if that flag
    is disabled too).
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: I01634daf75a7d80a663ec24991eec8b4094aeeda
    Reviewed-on: https://chromium-review.googlesource.com/829376
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524628}

--- third_party/WebKit/Source/core/layout/ng/ng_block_child_iterator.cc
+++ third_party/WebKit/Source/core/layout/ng/ng_block_child_iterator.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/layout/ng/ng_block_child_iterator.h"
 
 #include "core/layout/ng/ng_block_break_token.h"
+#include "core/layout/ng/ng_block_node.h"
 #include "core/layout/ng/ng_layout_input_node.h"
 
 namespace blink {
@@ -27,6 +28,9 @@ NGBlockChildIterator::NGBlockChildIterator(NGLayoutInputNode first_child,
     }
     return;
   }
+  auto first_node_child = ToNGBlockNode(break_token->InputNode()).FirstChild();
+  resuming_at_inline_formatting_context_ =
+      first_node_child && first_node_child.IsInline();
   child_ = child_break_tokens[0]->InputNode();
 }
 
@@ -50,6 +54,22 @@ NGBlockChildIterator::Entry NGBlockChildIterator::NextChild(
     //
     // [1] https://drafts.csswg.org/css-break/#parallel-flows
     const auto& child_break_tokens = break_token_->ChildBreakTokens();
+
+    if (resuming_at_inline_formatting_context_) {
+      // When resuming inside an inline formatting context, just process the
+      // break tokens. There'll be any number of break tokens for broken floats,
+      // followed by at most one break token for the actual inline formatting
+      // context where we had to give up in the previous fragmentainer. The node
+      // structure will not be of any help at all, since the break tokens will
+      // be associated with nodes that are not siblings.
+      while (child_token_idx_ < child_break_tokens.size()) {
+        const auto& token = child_break_tokens[child_token_idx_];
+        child_token_idx_++;
+        if (!token->IsFinished())
+          return Entry(token->InputNode(), token.get());
+      }
+      return Entry(nullptr, nullptr);
+    }
 
     do {
       // Early exit if we've exhausted our child break tokens.
