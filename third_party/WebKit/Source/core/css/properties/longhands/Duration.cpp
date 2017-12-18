a590926be4af34e869b55b3567e9263dd65292e7
Christian Dullweber <dullweber@chromium.org>
2017-12-18 15:00:02 +0000

    Revert "[Ribbon] Remove CSSProperty groups"
    This reverts commit e9cc36e4b58dfa03c0e882d6a9a9c8322e03a8f3.
    
    Reason for revert: https://crbug.com/795744
    
    Original change's description:
    > [Ribbon] Remove CSSProperty groups
    > 
    > Gist for gen diff here:
    > 
    > https: //gist.github.com/rjwright/d32835d21f8b313c46ec7a1c6d4d0155/revisions
    > Bug: 788554
    > Change-Id: Ia19d78686e9742a91455fa044c69c22c94c05552
    > Reviewed-on: https://chromium-review.googlesource.com/804645
    > Commit-Queue: Renée Wright <rjwright@chromium.org>
    > Reviewed-by: meade_UTC10 <meade@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#524692}
    
    TBR=rjwright@chromium.org,meade@chromium.org
    
    Change-Id: I0c1d41d5ef0f93b4563ad755b3b1743810b8a9b8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 788554
    Reviewed-on: https://chromium-review.googlesource.com/832469
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524708}

---
+++ third_party/WebKit/Source/core/css/properties/longhands/Duration.cpp
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/css/properties/longhands/Duration.h"
+
+#include "core/css/parser/CSSPropertyParserHelpers.h"
+#include "platform/Length.h"
+
+namespace blink {
+namespace CSSLonghand {
+
+const CSSValue* Duration::ParseSingleValue(CSSParserTokenRange& range,
+                                           const CSSParserContext&,
+                                           const CSSParserLocalContext&) const {
+  return CSSPropertyParserHelpers::ConsumeCommaSeparatedList(
+      CSSPropertyParserHelpers::ConsumeTime, range, kValueRangeNonNegative);
+}
+
+}  // namespace CSSLonghand
+}  // namespace blink
---
+++ third_party/WebKit/Source/core/css/properties/longhands/GridRowOrColumnGap.cpp
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/css/properties/longhands/GridRowOrColumnGap.h"
+
+#include "core/css/parser/CSSParserContext.h"
+#include "core/css/parser/CSSPropertyParserHelpers.h"
+#include "platform/Length.h"
+
+namespace blink {
+namespace CSSLonghand {
+
+const CSSValue* GridRowOrColumnGap::ParseSingleValue(
+    CSSParserTokenRange& range,
+    const CSSParserContext& context,
+    const CSSParserLocalContext&) const {
+  return CSSPropertyParserHelpers::ConsumeLengthOrPercent(
+      range, context.Mode(), kValueRangeNonNegative);
+}
+
+}  // namespace CSSLonghand
+}  // namespace blink
