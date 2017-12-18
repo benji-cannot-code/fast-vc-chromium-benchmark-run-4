c652483b408a14a5687cc7d611cd8dc5326d3a03
Rune Lillesveen <futhark@chromium.org>
2017-12-18 12:39:32 +0000

    Tests and implementation of display:contents in SVG.
    Adjusted how display:contents affect various SVG elements as specified
    in https://drafts.csswg.org/css-display/#unbox-svg
    
    By using IsOutermostSVGSVGElement() for svg root testing , this change
    also fixes 795685.
    
    Bug: 794498, 795685
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I6b45f1ad65cfe9b29378d9ea9f2c4f9d855da231
    Reviewed-on: https://chromium-review.googlesource.com/829633
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#524687}

--- third_party/WebKit/Source/core/svg/SVGUseElement.cpp
+++ third_party/WebKit/Source/core/svg/SVGUseElement.cpp
@@ -483,7 +483,9 @@ void SVGUseElement::BuildShadowAndInstanceTree(SVGElement& target) {
   UpdateRelativeLengthsInformation();
 }
 
-LayoutObject* SVGUseElement::CreateLayoutObject(const ComputedStyle&) {
+LayoutObject* SVGUseElement::CreateLayoutObject(const ComputedStyle& style) {
+  if (style.Display() == EDisplay::kContents)
+    return nullptr;
   return new LayoutSVGTransformableContainer(this);
 }
 
