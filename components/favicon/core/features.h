ab17d8315db7ccebebe0a57193d1cb652772dcba
Mikel Astiz <mastiz@chromium.org>
2017-12-18 21:28:41 +0000

    Remove feature toggle and flag for Web Manifest icons
    The toggle is about loading Web Manifests on page load with the goal to
    parse the icons listed there, and use this list in addition to the
    favicons that are inlined in the HTML.
    
    The feature is launched since M61 and we are not aware of related
    issues, so let's clean up and remove the feature toggles as well as
    about flags.
    
    Bug: 711187
    Change-Id: I6cec06569d565261d8c68e31117fcc30d73615b4
    Reviewed-on: https://chromium-review.googlesource.com/832267
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Gayane Petrosyan <gayane@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524804}

--- components/favicon/core/features.h
+++ components/favicon/core/features.h
@@ -12,7 +12,6 @@ struct Feature;
 
 namespace favicon {
 
-extern const base::Feature kFaviconsFromWebManifest;
 extern const base::Feature kAllowPropagationOfFaviconCacheHits;
 extern const base::Feature kAllowDeletionOfFaviconMappings;
 
