4ddaa3e4f8e10590616a91b4e044270290972655
Anita Woodruff <awdf@chromium.org>
2017-12-18 16:34:14 +0000

    [Notifications] Rename non_persistent_notification_id to request_id
    - Now all the integer ids are request ids to avoid confusion with the
    string notification ids.
    
    Bug: 595685
    Change-Id: Idd8008f59bb4a3db8eadf1455c6185fc7a01b338
    Reviewed-on: https://chromium-review.googlesource.com/829553
    Commit-Queue: Anita Woodruff <awdf@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524725}

--- content/common/platform_notification_messages.h
+++ content/common/platform_notification_messages.h
@@ -74,16 +74,13 @@ IPC_STRUCT_TRAITS_END()
 // Messages sent from the browser to the renderer.
 
 // Informs the renderer that the browser has displayed the notification.
-IPC_MESSAGE_CONTROL1(PlatformNotificationMsg_DidShow,
-                     int /* notification_id */)
+IPC_MESSAGE_CONTROL1(PlatformNotificationMsg_DidShow, int /* request_id */)
 
 // Informs the renderer that the notification has been closed.
-IPC_MESSAGE_CONTROL1(PlatformNotificationMsg_DidClose,
-                     int /* notification_id */)
+IPC_MESSAGE_CONTROL1(PlatformNotificationMsg_DidClose, int /* request_id */)
 
 // Informs the renderer that the notification has been clicked on.
-IPC_MESSAGE_CONTROL1(PlatformNotificationMsg_DidClick,
-                     int /* notification_id */)
+IPC_MESSAGE_CONTROL1(PlatformNotificationMsg_DidClick, int /* request_id */)
 
 // Reply to PlatformNotificationHostMsg_ShowPersistent indicating that a
 // persistent notification has been shown on the platform (if |success| is
@@ -103,7 +100,7 @@ IPC_MESSAGE_CONTROL2(PlatformNotificationMsg_DidGetNotifications,
 
 IPC_MESSAGE_CONTROL4(
     PlatformNotificationHostMsg_Show,
-    int /* non_persistent_notification_id */,
+    int /* request_id */,
     GURL /* origin */,
     content::PlatformNotificationData /* notification_data */,
     content::NotificationResources /* notification_resources */)
@@ -125,7 +122,7 @@ IPC_MESSAGE_CONTROL4(PlatformNotificationHostMsg_GetNotifications,
 IPC_MESSAGE_CONTROL3(PlatformNotificationHostMsg_Close,
                      GURL /* origin */,
                      std::string /* tag */,
-                     int /* non_persistent_notification_id */)
+                     int /* request_id */)
 
 IPC_MESSAGE_CONTROL3(PlatformNotificationHostMsg_ClosePersistent,
                      GURL /* origin */,
--- content/renderer/notifications/notification_manager.cc
+++ content/renderer/notifications/notification_manager.cc
@@ -94,10 +94,10 @@ void NotificationManager::Show(
 
   GURL origin_gurl = url::Origin(origin).GetURL();
 
-  int notification_id =
+  int request_id =
       notification_dispatcher_->GenerateNotificationId(NotificationWorkerId());
 
-  active_page_notifications_[notification_id] = ActiveNotificationData(
+  active_page_notifications_[request_id] = ActiveNotificationData(
       delegate, origin_gurl,
       notification_data.tag.Utf8(
           WebString::UTF8ConversionMode::kStrictReplacingErrorsWithFFFD));
@@ -106,8 +106,7 @@ void NotificationManager::Show(
   // origins. Perhaps also 'file:', 'blob:' and 'filesystem:'. See
   // https://crbug.com/490074 for detail.
   thread_safe_sender_->Send(new PlatformNotificationHostMsg_Show(
-      notification_id, origin_gurl,
-      ToPlatformNotificationData(notification_data),
+      request_id, origin_gurl, ToPlatformNotificationData(notification_data),
       ToNotificationResources(std::move(notification_resources))));
 }
 
@@ -246,8 +245,8 @@ bool NotificationManager::OnMessageReceived(const IPC::Message& message) {
   return handled;
 }
 
-void NotificationManager::OnDidShow(int notification_id) {
-  const auto& iter = active_page_notifications_.find(notification_id);
+void NotificationManager::OnDidShow(int request_id) {
+  const auto& iter = active_page_notifications_.find(request_id);
   if (iter == active_page_notifications_.end())
     return;
 
@@ -270,8 +269,8 @@ void NotificationManager::OnDidShowPersistent(int request_id, bool success) {
   pending_show_notification_requests_.Remove(request_id);
 }
 
-void NotificationManager::OnDidClose(int notification_id) {
-  const auto& iter = active_page_notifications_.find(notification_id);
+void NotificationManager::OnDidClose(int request_id) {
+  const auto& iter = active_page_notifications_.find(request_id);
   if (iter == active_page_notifications_.end())
     return;
 
@@ -280,8 +279,8 @@ void NotificationManager::OnDidClose(int notification_id) {
   active_page_notifications_.erase(iter);
 }
 
-void NotificationManager::OnDidClick(int notification_id) {
-  const auto& iter = active_page_notifications_.find(notification_id);
+void NotificationManager::OnDidClick(int request_id) {
+  const auto& iter = active_page_notifications_.find(request_id);
   if (iter == active_page_notifications_.end())
     return;
 
