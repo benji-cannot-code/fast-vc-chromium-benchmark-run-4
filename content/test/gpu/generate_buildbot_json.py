a7f9a7775d6878732fa05e8d16ace7aa817092ba
Kenneth Russell <kbr@chromium.org>
2017-12-18 18:32:04 +0000

    Fix specification of Swarmed Intel HD 630 bots.
    They're in the Chrome-GPU pool, not the Chrome pool.
    
    BUG=754875
    NOTRY=true
    TBR=jmadill@chromium.org
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I23a033fc1dd22abe59e851ba47dac7cce2e504d1
    Reviewed-on: https://chromium-review.googlesource.com/833020
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524745}

--- content/test/gpu/generate_buildbot_json.py
+++ content/test/gpu/generate_buildbot_json.py
@@ -397,6 +397,7 @@ FYI_WATERFALL = {
         {
           'gpu': '8086:5912',
           'os': 'Windows-10',
+          'pool': 'Chrome-GPU',
         },
       ],
       'build_config': 'Release',
@@ -680,7 +681,8 @@ FYI_WATERFALL = {
       'swarming_dimensions': [
         {
           'gpu': '8086:5912',
-          'os': 'Ubuntu'
+          'os': 'Ubuntu',
+          'pool': 'Chrome-GPU',
         },
       ],
       'build_config': 'Release',
