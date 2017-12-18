c24e5f5c0b49cafae53edf5146e85a6e3001493f
Renee Wright <rjwright@chromium.org>
2017-12-18 13:05:08 +0000

    [Ribbon] Remove CSSProperty groups
    Gist for gen diff here:
    
    https: //gist.github.com/rjwright/d32835d21f8b313c46ec7a1c6d4d0155/revisions
    Bug: 788554
    Change-Id: Ia19d78686e9742a91455fa044c69c22c94c05552
    Reviewed-on: https://chromium-review.googlesource.com/804645
    Commit-Queue: Renée Wright <rjwright@chromium.org>
    Reviewed-by: meade_UTC10 <meade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524692}

---
+++ third_party/WebKit/Source/core/css/properties/longhands/GridAutoRows.cpp
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/css/properties/longhands/GridAutoRows.h"
+
+#include "core/css/parser/CSSParserContext.h"
+#include "core/css/properties/CSSParsingUtils.h"
+
+namespace blink {
+namespace CSSLonghand {
+
+const CSSValue* GridAutoRows::ParseSingleValue(
+    CSSParserTokenRange& range,
+    const CSSParserContext& context,
+    const CSSParserLocalContext&) const {
+  return CSSParsingUtils::ConsumeGridTrackList(
+      range, context.Mode(), CSSParsingUtils::TrackListType::kGridAuto);
+}
+
+}  // namespace CSSLonghand
+}  // namespace blink
