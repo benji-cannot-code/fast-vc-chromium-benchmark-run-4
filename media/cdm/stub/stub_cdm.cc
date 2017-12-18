e952cc99371d0f434a9f2400ae62364bc9799ac2
Wojciech Dzierżanowski <wdzierzanowski@opera.com>
2017-12-18 07:56:08 +0000

    Avoid race in MSE_Widevine tests
    When run with the stub Widevine CDM, these tests suffer from a race
    between the result of a failed StubCdm::Decrypt() and a failed
    StubCdm::UpdateSession().  The tests expect the latter, but the former
    often arrives first.
    
    By allowing the stub to InitializeVideoDecoder() successfully and
    returning cdm::kNoKey upon DecryptAndDecodeFrame() attempt we avoid the
    DecryptingDemuxerStream errors altogether, so there is no race anymore.
    
    Bug: 768745
    Test: 'browser_tests --gtest_filter=*MSE_Widevine*' pass when run with the stub Widevine CDM
    
    Change-Id: I35a35d006d118f8f7f9a8a64cf9334017644f4ea
    Reviewed-on: https://chromium-review.googlesource.com/831026
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: Wojciech Dzierżanowski <wdzierzanowski@opera.com>
    Cr-Commit-Position: refs/heads/master@{#524663}

--- media/cdm/stub/stub_cdm.cc
+++ media/cdm/stub/stub_cdm.cc
@@ -115,12 +115,12 @@ cdm::Status StubCdm::Decrypt(const cdm::InputBuffer& /* encrypted_buffer */,
 
 cdm::Status StubCdm::InitializeAudioDecoder(
     const cdm::AudioDecoderConfig& /* audio_decoder_config */) {
-  return cdm::kDecryptError;
+  return cdm::kSuccess;
 }
 
 cdm::Status StubCdm::InitializeVideoDecoder(
     const cdm::VideoDecoderConfig& /* video_decoder_config */) {
-  return cdm::kDecryptError;
+  return cdm::kSuccess;
 }
 
 void StubCdm::ResetDecoder(cdm::StreamType /* decoder_type */) {
@@ -132,13 +132,13 @@ void StubCdm::DeinitializeDecoder(cdm::StreamType /* decoder_type */) {
 cdm::Status StubCdm::DecryptAndDecodeFrame(
     const cdm::InputBuffer& /* encrypted_buffer */,
     cdm::VideoFrame* /* decoded_frame */) {
-  return cdm::kDecryptError;
+  return cdm::kNoKey;
 }
 
 cdm::Status StubCdm::DecryptAndDecodeSamples(
     const cdm::InputBuffer& /* encrypted_buffer */,
     cdm::AudioFrames* /* audio_frames */) {
-  return cdm::kDecryptError;
+  return cdm::kNoKey;
 }
 
 void StubCdm::Destroy() {
