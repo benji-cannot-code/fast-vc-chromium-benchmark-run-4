6dd2ab8ead47cd97b7681fbd7f48621fb34e9b93
Shimi Zhang <ctzsm@chromium.org>
2017-12-18 23:57:48 +0000

    aw: Pass middle point of selection/insertion bound to onSelectionEvent()
    This CL passes the middle point of bound corresponding to active
    selection/insertion handle (under dragging), if there is no active
    handle, it will return (0, 0).
    
    Bug: 794385
    Change-Id: I8c0cd52a253b14797de980292f57f66e2724c907
    Reviewed-on: https://chromium-review.googlesource.com/822187
    Commit-Queue: Shimi Zhang <ctzsm@chromium.org>
    Reviewed-by: Mohsen Izadi <mohsen@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524868}

--- content/browser/renderer_host/render_widget_host_view_android.cc
+++ content/browser/renderer_host/render_widget_host_view_android.cc
@@ -1437,7 +1437,8 @@ void RenderWidgetHostViewAndroid::OnSelectionEvent(
     ResetGestureDetection();
   }
   selection_popup_controller_->OnSelectionEvent(
-      event, GetSelectionRect(*touch_selection_controller_));
+      event, GetSelectionRect(*touch_selection_controller_),
+      touch_selection_controller_->GetActiveHandleBoundPoint());
 }
 
 ui::TouchSelectionControllerClient*
