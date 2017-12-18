bb4c987af0bdd17470e21894fe99f88956ad0210
Kurt Horimoto <kkhorimoto@chromium.org>
2017-12-18 20:40:20 +0000

    [iOS] Disable fullscreen while the location bar is focused.
    Bug: 793138
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I8007d2d96be03886cae14c7a9977deafb7735552
    Reviewed-on: https://chromium-review.googlesource.com/830922
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524788}

--- ios/chrome/browser/ui/omnibox/location_bar_controller_impl.mm
+++ ios/chrome/browser/ui/omnibox/location_bar_controller_impl.mm
@@ -21,6 +21,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/chrome_url_constants.h"
 #include "ios/chrome/browser/experimental_flags.h"
 #import "ios/chrome/browser/ui/commands/browser_commands.h"
+#import "ios/chrome/browser/ui/fullscreen/fullscreen_controller_factory.h"
+#import "ios/chrome/browser/ui/fullscreen/fullscreen_features.h"
+#import "ios/chrome/browser/ui/fullscreen/scoped_fullscreen_disabler.h"
 #include "ios/chrome/browser/ui/omnibox/location_bar_delegate.h"
 #import "ios/chrome/browser/ui/omnibox/location_bar_view.h"
 #include "ios/chrome/browser/ui/omnibox/omnibox_popup_view_ios.h"
@@ -152,8 +155,10 @@ LocationBarControllerImpl::LocationBarControllerImpl(
                                                   browser_state)),
       location_bar_view_(location_bar_view),
       delegate_(delegate),
-      dispatcher_(dispatcher) {
+      dispatcher_(dispatcher),
+      browser_state_(browser_state) {
   DCHECK([delegate_ toolbarModel]);
+  DCHECK(browser_state_);
   show_hint_text_ = true;
 
   InstallLocationIcon();
@@ -272,6 +277,10 @@ void LocationBarControllerImpl::OnKillFocus() {
     [location_bar_view_.textField setPlaceholder:placeholderText];
   }
 
+  // Stop disabling fullscreen since the loation bar is no longer focused.
+  if (base::FeatureList::IsEnabled(fullscreen::features::kNewFullscreen))
+    fullscreen_disabler_ = nullptr;
+
   UpdateRightDecorations();
   [delegate_ locationBarHasResignedFirstResponder];
 }
@@ -293,6 +302,15 @@ void LocationBarControllerImpl::OnSetFocus() {
   if (IsIPadIdiom()) {
     [location_bar_view_.textField setPlaceholder:nil];
   }
+
+  // Disable fullscreen while focused so that the location bar cannot be scolled
+  // offscreen.
+  if (base::FeatureList::IsEnabled(fullscreen::features::kNewFullscreen)) {
+    fullscreen_disabler_ = base::MakeUnique<ScopedFullscreenDisabler>(
+        FullscreenControllerFactory::GetInstance()->GetForBrowserState(
+            browser_state_));
+  }
+
   UpdateRightDecorations();
   [delegate_ locationBarHasBecomeFirstResponder];
 }
