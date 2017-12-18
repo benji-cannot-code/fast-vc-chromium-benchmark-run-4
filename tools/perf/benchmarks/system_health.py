132d40cfe9d2de0dffff36d76bcc10baa7a9f22b
rnephew <rnephew@chromium.org>
2017-12-18 20:19:30 +0000

    [Telemetry] Update system_health_smoke_test to use expectations file.
    It composes its storysets by iterating over other benchmarks. This
    updates the benchmark to also load expectations from those benchmarks.
    
    Bug: 781409
    Change-Id: I857788e8483e953e53b05bc59ecff9f968fc52cc
    Reviewed-on: https://chromium-review.googlesource.com/832381
    Reviewed-by: Charlie Andrews <charliea@chromium.org>
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Commit-Queue: rnephew <rnephew@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524781}

--- tools/perf/benchmarks/system_health.py
+++ tools/perf/benchmarks/system_health.py
@@ -140,9 +140,6 @@ class DesktopMemorySystemHealth(_MemorySystemHealthBenchmark):
     #  '--enable-heap-profiling=native',
     #])
 
-  def GetExpectations(self):
-    return page_sets.SystemHealthDesktopMemoryExpectations()
-
 
 @benchmark.Owner(emails=['perezju@chromium.org'])
 class MobileMemorySystemHealth(_MemorySystemHealthBenchmark):
@@ -162,9 +159,6 @@ class MobileMemorySystemHealth(_MemorySystemHealthBenchmark):
   @classmethod
   def Name(cls):
     return 'system_health.memory_mobile'
-
-  def GetExpectations(self):
-    return page_sets.SystemHealthMobileMemoryExpectations()
 
 
 @benchmark.Owner(emails=['perezju@chromium.org', 'torne@chromium.org'])
