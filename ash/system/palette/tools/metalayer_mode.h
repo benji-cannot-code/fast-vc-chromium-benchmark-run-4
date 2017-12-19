c02a8cc3664b80c601cb8a8946b22372b415a95f
Xiaohui Chen <xiaohuic@google.com>
2017-12-19 00:27:58 +0000

    Disable metalayer tool when assistant is not available
    BUG=b:68664196
    TEST=unit tests and manually check on device
    
    Change-Id: If4c997a048ffc7ec3afa3e9ab2199f8c2b140c7a
    Reviewed-on: https://chromium-review.googlesource.com/828265
    Reviewed-by: Vladislav Kaznacheev <kaznacheev@chromium.org>
    Reviewed-by: Jenny Zhang <jennyz@chromium.org>
    Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524881}

--- ash/system/palette/tools/metalayer_mode.h
+++ ash/system/palette/tools/metalayer_mode.h
@@ -32,7 +32,8 @@ class ASH_EXPORT MetalayerMode : public CommonPaletteTool,
   // from |enabled| which means that the palette tool is currently selected by
   // the user.
   bool feature_enabled() const {
-    return voice_interaction_enabled_ && voice_interaction_context_enabled_;
+    return voice_interaction_enabled_ && voice_interaction_context_enabled_ &&
+           assistant_allowed_state_ == mojom::AssistantAllowedState::ALLOWED;
   }
 
   // Whether the tool is in "loading" state.
@@ -67,6 +68,8 @@ class ASH_EXPORT MetalayerMode : public CommonPaletteTool,
       mojom::VoiceInteractionState state) override;
   void OnVoiceInteractionSettingsEnabled(bool enabled) override;
   void OnVoiceInteractionContextEnabled(bool enabled) override;
+  void OnAssistantFeatureAllowedChanged(
+      mojom::AssistantAllowedState state) override;
 
   // Update the state of the tool based on the current availability of the tool.
   void UpdateState();
@@ -83,6 +86,9 @@ class ASH_EXPORT MetalayerMode : public CommonPaletteTool,
   bool voice_interaction_enabled_ = false;
 
   bool voice_interaction_context_enabled_ = false;
+
+  mojom::AssistantAllowedState assistant_allowed_state_ =
+      mojom::AssistantAllowedState::ALLOWED;
 
   base::TimeTicks previous_stroke_end_;
 
