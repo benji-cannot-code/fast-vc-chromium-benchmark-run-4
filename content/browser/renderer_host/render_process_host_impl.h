f3222a6299bd7e4488c0faa2c144cc4da41eadb5
Yutaka Hirano <yhirano@chromium.org>
2017-12-19 06:12:35 +0000

    Use mojo-fied loading IPCs in SecurityExploitBrowserTest
    In order to delete messages defined in resource_messages.h, this CL
    replaces such messages in SecurityExploitBrowserTest with mojo
    messages.
    
    Bug: 788241
    Change-Id: I05c66ed5cbfd42e6335c011643c91cf1d5b77dee
    Reviewed-on: https://chromium-review.googlesource.com/831769
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524952}

--- content/browser/renderer_host/render_process_host_impl.h
+++ content/browser/renderer_host/render_process_host_impl.h
@@ -414,6 +414,7 @@ class CONTENT_EXPORT RenderProcessHostImpl
   friend class ChildProcessLauncherBrowserTest_ChildSpawnFail_Test;
   friend class VisitRelayingRenderProcessHost;
   friend class StoragePartitonInterceptor;
+  friend class SecurityExploitBrowserTest;
   class ConnectionFilterController;
   class ConnectionFilterImpl;
 
