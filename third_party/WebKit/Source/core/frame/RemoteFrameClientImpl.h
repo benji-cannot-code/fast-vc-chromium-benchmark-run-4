0ff72f8e70cd31eb6492e7112938ebbaea06b4cd
Zhuoyu Qian <zhuoyu.qian@samsung.com>
2017-12-19 01:54:26 +0000

    Move WillBeDetached From FrameClient to LocalFrameClient.
    As the comment in FrameClient.h by dcheng@, move WillBeDetached()
    from to LocalFrameClient, since remote frames don't need it.
    
    BUG=
    R=dcheng@chromium.org
    
    Signed-off-by: Zhuoyu Qian <zhuoyu.qian@samsung.com>
    Change-Id: I645613990480535025452e6edd5f51579706e2e9
    Reviewed-on: https://chromium-review.googlesource.com/831366
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524905}

--- third_party/WebKit/Source/core/frame/RemoteFrameClientImpl.h
+++ third_party/WebKit/Source/core/frame/RemoteFrameClientImpl.h
@@ -19,7 +19,6 @@ class RemoteFrameClientImpl final : public RemoteFrameClient {
 
   // FrameClient overrides:
   bool InShadowTree() const override;
-  void WillBeDetached() override;
   void Detached(FrameDetachType) override;
   Frame* Opener() const override;
   void SetOpener(Frame*) override;
