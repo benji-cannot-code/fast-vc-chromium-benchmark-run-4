a5be7657ec490c1eec6293154a0b2f23db8b1b5a
John Abd-El-Malek <jam@chromium.org>
2017-12-19 22:33:02 +0000

    Fix iOS QUIC unittests on ios-simulator-cronet after r525102.
    Bug: 796311
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I7aeb3767ef4e5b883a8b83e29fe24b786a84f1c1
    Reviewed-on: https://chromium-review.googlesource.com/834994
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Misha Efimov <mef@chromium.org>
    Reviewed-by: Misha Efimov <mef@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525160}

--- components/cronet/ios/test/cronet_pkp_test.mm
+++ components/cronet/ios/test/cronet_pkp_test.mm
@@ -20,7 +20,7 @@ const bool kIncludeSubdomains = true;
 const bool kExcludeSubdomains = false;
 const bool kSuccess = true;
 const bool kError = false;
-const std::string kServerCert = "quic_test.example.com.crt";
+const std::string kServerCert = "quic-chain.pem";
 NSDate* const kDistantFuture = [NSDate distantFuture];
 }  // namespace
 
