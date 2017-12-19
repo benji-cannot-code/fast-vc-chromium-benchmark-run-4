dfee1af1b9eb1b20a4c917b92e66a98ce6cf3ba8
Emily Stark <estark@google.com>
2017-12-19 21:29:51 +0000

    Mac: do not enable file upload until selection has changed
    To avoid files being uploaded without the user realizing, require the
    user to change the selection on the file dialog before the OK button
    is enabled.
    
    Bug: 637098
    Change-Id: Ic6faffb3f6075fb969ae930992d2553162346d02
    Reviewed-on: https://chromium-review.googlesource.com/833399
    Reviewed-by: ccameron <ccameron@chromium.org>
    Commit-Queue: Emily Stark <estark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525148}

--- ui/shell_dialogs/select_file_dialog_mac.mm
+++ ui/shell_dialogs/select_file_dialog_mac.mm
@@ -57,6 +57,9 @@ NSString* GetDescriptionFromExtension(const base::FilePath::StringType& ext) {
 @interface SelectFileDialogBridge : NSObject<NSOpenSavePanelDelegate> {
  @private
   ui::SelectFileDialogImpl* selectFileDialogImpl_;  // WEAK; owns us
+  // File upload is disabled until the selection is changed, to avoid files
+  // being uploaded without user action.
+  bool enable_;
 }
 
 - (id)initWithSelectFileDialogImpl:(ui::SelectFileDialogImpl*)s;
@@ -67,6 +70,7 @@ NSString* GetDescriptionFromExtension(const base::FilePath::StringType& ext) {
 
 // NSSavePanel delegate method
 - (BOOL)panel:(id)sender shouldEnableURL:(NSURL *)url;
+- (void)panelSelectionDidChange:(id)sender;
 
 @end
 
@@ -380,6 +384,7 @@ SelectFileDialog* CreateSelectFileDialog(
 
 - (id)initWithSelectFileDialogImpl:(ui::SelectFileDialogImpl*)s {
   if ((self = [super init])) {
+    enable_ = false;
     selectFileDialogImpl_ = s;
   }
   return self;
@@ -427,7 +432,11 @@ SelectFileDialog* CreateSelectFileDialog(
 }
 
 - (BOOL)panel:(id)sender shouldEnableURL:(NSURL *)url {
-  return [url isFileURL];
+  return enable_ && [url isFileURL];
+}
+
+- (void)panelSelectionDidChange:(id)sender {
+  enable_ = true;
 }
 
 @end
