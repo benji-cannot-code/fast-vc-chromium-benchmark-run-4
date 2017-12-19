7827231d6bed4f3160986a5a658f4af9d31662fa
John Abd-El-Malek <jam@chromium.org>
2017-12-19 19:21:36 +0000

    Switch QUIC browser tests to make real requests to determine if QUIC is enabled.
    This allows the test to work when the URLRequestContext is in another process.
    
    This change also adds new certificates for testing QUIC, as the existing ones don't work with the
    production CertVerifier (thanks to estark@ who diagnosed this and provided the fix). This also
    includes a fix for QuicHttpResponseCache to work with URLs that have ports.
    
    Bug: 793769
    Change-Id: Ia03a18469df064258f7f5cde6b2578d6df137500
    Reviewed-on: https://chromium-review.googlesource.com/833749
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525102}

--- components/grpc_support/test/quic_test_server.h
+++ components/grpc_support/test/quic_test_server.h
@@ -22,6 +22,7 @@ extern const char kTestServerUrl[];
 
 extern const char kStatusHeader[];
 
+extern const char kHelloPath[];
 extern const char kHelloBodyValue[];
 extern const char kHelloStatus[];
 
