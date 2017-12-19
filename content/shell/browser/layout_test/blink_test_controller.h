0ce84c94a5986c32231543c9a6fe5a4a71f316cb
Lukasz Anforowicz <lukasza@chromium.org>
2017-12-19 16:44:17 +0000

    Fix for UaF of |this| in BlinkTestController::OnDumpFrameLayoutResponse.
    As shown in https://crbug.com/765581, it is possible that
    BlinkTestController is destroyed before
    BlinkTestController::OnDumpFrameLayoutResponse gets called:
    
    1. To prevent UaF crashes in this case, this CL wraps |this| in a
       base::WeakPtr.  This is a speculative fix (I was never able to
       repro the UaF locally), but I am pretty confident it should avoid
       the crashes reported in https://crbug.com/765581
    
    2. BlinkTestController should only be destroyed *after* a test is over.
       Therefore it is rather surprising that BlinkTestController gets
       destroyed while we are in the middle of generating a layout dump.
       To help demystify this surprise the CL adds a DCHECK to
       BlinkTestController::OnInitiateLayoutDump to make sure that
       only one layout dump is happening at a time.
    
    Bug: 765581
    Change-Id: I07431bb60316d668c4bd1b465d25bcbfb12e6b96
    Reviewed-on: https://chromium-review.googlesource.com/677786
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525056}

--- content/shell/browser/layout_test/blink_test_controller.h
+++ content/shell/browser/layout_test/blink_test_controller.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/cancelable_callback.h"
 #include "base/files/file_path.h"
 #include "base/macros.h"
+#include "base/memory/weak_ptr.h"
 #include "base/scoped_observer.h"
 #include "base/sequence_checker.h"
 #include "base/synchronization/lock.h"
@@ -309,6 +310,8 @@ class BlinkTestController : public WebContentsObserver,
 #endif
 
   SEQUENCE_CHECKER(sequence_checker_);
+
+  base::WeakPtrFactory<BlinkTestController> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(BlinkTestController);
 };
