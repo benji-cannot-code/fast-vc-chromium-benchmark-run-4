9a89b6144d39722f18905da07157fe8133a6b861
CJ DiMeglio <lethalantidote@chromium.org>
2017-12-19 01:55:31 +0000

    Adds unit test for MediaSessionControllersManager.
    This CL modifies MediaSessionControllersManager to make testing easier
    and provides unit tests.
    
    Bug: 590183
    Change-Id: Ibb99bdff065305d3126cab02599584e754b0b5f5
    Reviewed-on: https://chromium-review.googlesource.com/770351
    Commit-Queue: CJ DiMeglio <lethalantidote@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524907}

--- content/browser/media/session/media_session_controllers_manager.h
+++ content/browser/media/session/media_session_controllers_manager.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/time/time.h"
+#include "content/common/content_export.h"
 #include "content/public/browser/web_contents_observer.h"  // For MediaPlayerId.
 
 namespace media {
@@ -26,9 +27,11 @@ class RenderFrameHost;
 
 // MediaSessionControllersManager is a delegate of MediaWebContentsObserver that
 // handles MediaSessionController instances.
-class MediaSessionControllersManager {
+class CONTENT_EXPORT MediaSessionControllersManager {
  public:
   using MediaPlayerId = WebContentsObserver::MediaPlayerId;
+  using ControllersMap =
+      std::map<MediaPlayerId, std::unique_ptr<MediaSessionController>>;
 
   explicit MediaSessionControllersManager(
       MediaWebContentsObserver* media_web_contents_observer);
@@ -53,11 +56,11 @@ class MediaSessionControllersManager {
   void OnEnd(const MediaPlayerId& id);
 
  private:
+  friend class MediaSessionControllersManagerTest;
+
   // Weak pointer because |this| is owned by |media_web_contents_observer_|.
   MediaWebContentsObserver* const media_web_contents_observer_;
 
-  using ControllersMap =
-      std::map<MediaPlayerId, std::unique_ptr<MediaSessionController>>;
   ControllersMap controllers_map_;
 
   DISALLOW_COPY_AND_ASSIGN(MediaSessionControllersManager);
