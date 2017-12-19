6e33846042bcc7d514986cf71dcffcd6d2bc54b8
Jonathan Backer <backer@chromium.org>
2017-12-19 18:16:09 +0000

    Return ContextGroup instead of GLES2Decoder
    As part of crbug.com/789238, I'm creating a new type of decoder
    called a RasterDecoder that is different from a GLES2Decoder.
    
    AFAICT, VDA just uses TextureManager (which is part of the
    ContextGroup), not the full GLES2Decoder.
    
    In this CL, I opted to return a ContextGroup instead of a
    TextureManager because it indicates that it is accessing the
    state for everything in the share group. But I'm happy to change
    this.
    
    TESTED=presubmits
    
    Bug: 789238
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I0248a477ace75e9fb76425bef1f92bfd9dd8f5a3
    Reviewed-on: https://chromium-review.googlesource.com/832707
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Jonathan Backer <backer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525085}

--- media/gpu/android/avda_state_provider.h
+++ media/gpu/android/avda_state_provider.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace gpu {
 namespace gles2 {
-class GLES2Decoder;
+class ContextGroup;
 }
 }  // namespace gpu
 
@@ -26,7 +26,7 @@ class AVDAStateProvider {
  public:
   // Various handy getters.
   virtual const gfx::Size& GetSize() const = 0;
-  virtual base::WeakPtr<gpu::gles2::GLES2Decoder> GetGlDecoder() const = 0;
+  virtual gpu::gles2::ContextGroup* GetContextGroup() const = 0;
 
   // Report a fatal error. This will post NotifyError(), and transition to the
   // error state.
