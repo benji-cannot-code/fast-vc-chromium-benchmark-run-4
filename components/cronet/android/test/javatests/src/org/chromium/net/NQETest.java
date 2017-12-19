d08fbeb2723d4cb119aa8fd982dfb727d33556e4
Tarun Bansal <tbansal@chromium.org>
2017-12-19 19:25:19 +0000

    Disable failing NQE Cronet tests
    Bug: 796260
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester
    Change-Id: Ibbf08a5cc52474fa556ecd19006e9061836bc4a9
    TBR: xunjieli@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/834452
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525105}

--- components/cronet/android/test/javatests/src/org/chromium/net/NQETest.java
+++ components/cronet/android/test/javatests/src/org/chromium/net/NQETest.java
@@ -24,6 +24,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.Log;
 import org.chromium.base.test.BaseJUnit4ClassRunner;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.MetricsUtils.HistogramDelta;
 import org.chromium.net.CronetTestRule.OnlyRunNativeCronet;
@@ -164,6 +165,7 @@ public class NQETest {
     @SmallTest
     @Feature({"Cronet"})
     @OnlyRunNativeCronet
+    @DisabledTest(message = "crbug.com/796260")
     public void testQuicDisabled() throws Exception {
         ExperimentalCronetEngine.Builder cronetEngineBuilder =
                 new ExperimentalCronetEngine.Builder(getContext());
@@ -354,6 +356,7 @@ public class NQETest {
     @SmallTest
     @Feature({"Cronet"})
     @OnlyRunNativeCronet
+    @DisabledTest(message = "crbug.com/796260")
     public void testQuicDisabledWithParams() throws Exception {
         ExperimentalCronetEngine.Builder cronetEngineBuilder =
                 new ExperimentalCronetEngine.Builder(getContext());
