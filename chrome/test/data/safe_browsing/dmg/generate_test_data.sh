f0a300eae8f4c6853a1a2808defbf4ac01955180
Sidney San Martín <sdy@chromium.org>
2017-12-19 18:24:18 +0000

    Fix "hdiutil: create failed - Invalid argument" building tests on APFS.
    APFS isn't compatible with some layouts, and these tests only handle
    HFS+, so specify it instead of inheriting the filesystem from the build
    volume.
    
    Bug: 796020
    Change-Id: Id04638a69d6d0130828f025b2d19a79e2fc966f4
    Reviewed-on: https://chromium-review.googlesource.com/832790
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Sidney San Martín <sdy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525088}

--- chrome/test/data/safe_browsing/dmg/generate_test_data.sh
+++ chrome/test/data/safe_browsing/dmg/generate_test_data.sh
@@ -52,7 +52,7 @@ generate_test_data() {
     DMG_NAME="dmg_${DMG_TEMPLATE_FORMAT}_${layout}"
     hdiutil create -srcfolder "${DMG_SOURCE}" \
       -format "${DMG_TEMPLATE_FORMAT}" -layout "${layout}" \
-      -volname "${DMG_NAME}" \
+      -fs JHFS+ -volname "${DMG_NAME}" \
       "${OUT_DIR}/${DMG_NAME}"
   done
 
@@ -82,7 +82,7 @@ generate_test_data() {
 
   hdiutil create -srcfolder "${DMG_SOURCE}" \
     -format UDZO -layout SPUD -volname "Mach-O in DMG" -ov \
-    "${OUT_DIR}/mach_o_in_dmg"
+    -fs JHFS+ "${OUT_DIR}/mach_o_in_dmg"
 
   rm -rf "${DMG_SOURCE}"
 
