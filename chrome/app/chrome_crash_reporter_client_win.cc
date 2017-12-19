5da354f2998072dcb8b4079b9a70d85e1b6a25ec
Robert Sesek <rsesek@chromium.org>
2017-12-19 23:50:31 +0000

    crash_keys: Covert "gl-context-set-current-stack-trace" to the new API.
    Bug: 598854, 724999
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I3ccc320d8dbb6acb900cb7b31366d38e3fa809ac
    Reviewed-on: https://chromium-review.googlesource.com/832710
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525187}

--- chrome/app/chrome_crash_reporter_client_win.cc
+++ chrome/app/chrome_crash_reporter_client_win.cc
@@ -47,9 +47,6 @@ size_t RegisterCrashKeysHelper() {
   static constexpr base::debug::CrashKey kFixedKeys[] = {
       {kNumVariations, kSmallSize},
       {kVariations, kHugeSize},
-
-      // TODO(sunnyps): Remove after fixing crbug.com/724999.
-      {"gl-context-set-current-stack-trace", kMediumSize},
   };
 
   // This dynamic set of keys is used for sets of key value pairs when gathering
