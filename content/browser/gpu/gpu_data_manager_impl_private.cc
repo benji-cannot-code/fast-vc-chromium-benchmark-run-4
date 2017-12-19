8b243c78307dbf4ebf2ef34f37facc932ebed6aa
Zhenyao Mo <zmo@chromium.org>
2017-12-19 20:16:50 +0000

    Shift Telemetry and about:gpu to use GpuFeatureInfo.
    So we can retire the old data structure soon.
    
    Also, this CL makes Telemetry crash if Chrome doesn't send GPU data back in 5s.
    
    BUG=744658
    TEST=bots
    R=kbr@chromium.org,piman@chromium.org
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Iae1ddb56c5681934b17f3539f070d0c1a15e9f80
    Reviewed-on: https://chromium-review.googlesource.com/832933
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525124}

--- content/browser/gpu/gpu_data_manager_impl_private.cc
+++ content/browser/gpu/gpu_data_manager_impl_private.cc
@@ -449,6 +449,16 @@ bool GpuDataManagerImplPrivate::IsCompleteGpuInfoAvailable() const {
   return IsEssentialGpuInfoAvailable();
 }
 
+bool GpuDataManagerImplPrivate::IsGpuFeatureInfoAvailable() const {
+  return gpu_feature_info_.IsValid();
+}
+
+gpu::GpuFeatureStatus GpuDataManagerImplPrivate::GetFeatureStatus(
+    gpu::GpuFeatureType feature) const {
+  DCHECK(feature >= 0 && feature < gpu::NUMBER_OF_GPU_FEATURE_TYPES);
+  return gpu_feature_info_.status_values[feature];
+}
+
 void GpuDataManagerImplPrivate::RequestVideoMemoryUsageStatsUpdate(
     const base::Callback<void(const gpu::VideoMemoryUsageStats& stats)>&
         callback) const {
@@ -656,11 +666,8 @@ void GpuDataManagerImplPrivate::UpdateGpuInfo(const gpu::GPUInfo& gpu_info) {
 
 void GpuDataManagerImplPrivate::UpdateGpuFeatureInfo(
     const gpu::GpuFeatureInfo& gpu_feature_info) {
-  if (!use_swiftshader_) {
-    gpu_feature_info_ = gpu_feature_info;
-    UpdateDriverBugListStats(gpu_feature_info);
-    NotifyGpuInfoUpdate();
-  }
+  gpu_feature_info_ = gpu_feature_info;
+  UpdateDriverBugListStats(gpu_feature_info);
 }
 
 gpu::GpuFeatureInfo GpuDataManagerImplPrivate::GetGpuFeatureInfo() const {
@@ -779,7 +786,9 @@ void GpuDataManagerImplPrivate::DisableHardwareAcceleration() {
   }
 
   card_blacklisted_ = true;
-
+  gpu::GpuFeatureInfo gpu_feature_info =
+      gpu::ComputeGpuFeatureInfoWithHardwareAccelerationDisabled();
+  UpdateGpuFeatureInfo(gpu_feature_info);
   for (int i = 0; i < gpu::NUMBER_OF_GPU_FEATURE_TYPES; ++i)
     blacklisted_features_.insert(i);
 
