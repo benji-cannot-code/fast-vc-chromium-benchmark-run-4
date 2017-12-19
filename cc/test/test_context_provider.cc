cb65f4f4d2ee5ab8c04ff818d4cf6977d8241dd8
Brian Salomon <bsalomon@google.com>
2017-12-19 16:56:26 +0000

    Report 8 vertex attributes supported in TestGLES2InterfaceForContextProvider.
    This is the minimum allowed by ES 2.
    
    Bug: 795587
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I2e2a89d2cf3c0d3b410af1e95ba2d5ce71721a4e
    Reviewed-on: https://chromium-review.googlesource.com/833202
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Commit-Queue: Brian Salomon <bsalomon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525060}

--- cc/test/test_context_provider.cc
+++ cc/test/test_context_provider.cc
@@ -75,6 +75,9 @@ class TestGLES2InterfaceForContextProvider : public TestGLES2Interface {
       case GL_MAX_RENDERBUFFER_SIZE:
         *params = 2048;
         break;
+      case GL_MAX_VERTEX_ATTRIBS:
+        *params = 8;
+        break;
       default:
         break;
     }
