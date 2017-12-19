b668c5fe36bec96c0ad592ae8c10e9b6a66ebae6
sczs <sczs@chromium.org>
2017-12-19 16:09:59 +0000

    [ios] Adds Incognito icon to Clean Toolbar.
    Bug: 791463
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I2a82e57108835fe133fbcb3073bb777543b17154
    Reviewed-on: https://chromium-review.googlesource.com/832771
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525048}

--- ios/chrome/browser/ui/toolbar/clean/toolbar_view_controller.mm
+++ ios/chrome/browser/ui/toolbar/clean/toolbar_view_controller.mm
@@ -204,6 +204,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     [self setHorizontalTranslationOffset:-kToolbarButtonAnimationOffset
                                 forViews:@[ self.locationBarLeadingButton ]];
     [animator addAnimations:^{
+      self.locationBarLeadingButton.hidden = NO;
       [self setHorizontalTranslationOffset:0
                                   forViews:@[ self.locationBarLeadingButton ]];
       self.locationBarLeadingButton.alpha = 1;
