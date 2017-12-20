1d3a9c0c16b7c6ea000fec457fcf4521a1e6432e
Christopher Cameron <ccameron@chromium.org>
2017-12-20 00:12:59 +0000

    viz/mac: Add surface id for DelegatedFrameHost
    This gets web content on-screen
    
    Resizing is very broken on macOS and will require quite some
    refactoring to work.
    
    Bug: 772576
    Change-Id: Ia7e6378550736fb95220c511d35fdad1fcbd5a6c
    Reviewed-on: https://chromium-review.googlesource.com/833351
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Commit-Queue: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525191}

--- content/browser/renderer_host/render_widget_host_view_mac.mm
+++ content/browser/renderer_host/render_widget_host_view_mac.mm
@@ -1526,6 +1526,11 @@ RenderWidgetHostImpl* RenderWidgetHostViewMac::GetRenderWidgetHostImpl() const {
   return render_widget_host_;
 }
 
+viz::LocalSurfaceId RenderWidgetHostViewMac::GetLocalSurfaceId() const {
+  // Call to the DelegatedFrameHost interface.
+  return browser_compositor_->GetLocalSurfaceId();
+}
+
 viz::FrameSinkId RenderWidgetHostViewMac::GetFrameSinkId() {
   return browser_compositor_->GetDelegatedFrameHost()->GetFrameSinkId();
 }
