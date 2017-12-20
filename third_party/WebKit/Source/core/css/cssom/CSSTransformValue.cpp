d18556a846d03f0c12e0d0b7f3139fc5df0107aa
Darren Shen <shend@chromium.org>
2017-12-20 22:58:54 +0000

    [css-typed-om] Update tests for CSSTransformValue.
    Update tests to reflect new spec. We have to update behaviour
    to throw error when there's no components.
    
    Spec:
    https://github.com/w3c/css-houdini-drafts/issues/534
    
    Bug: 545318
    Change-Id: Ifba7a1e6142bf19ff304d09eb4e45317d9dbc669
    Reviewed-on: https://chromium-review.googlesource.com/826825
    Reviewed-by: nainar <nainar@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525503}

--- third_party/WebKit/Source/core/css/cssom/CSSTransformValue.cpp
+++ third_party/WebKit/Source/core/css/cssom/CSSTransformValue.cpp
@@ -11,6 +11,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+CSSTransformValue* CSSTransformValue::Create(
+    const HeapVector<Member<CSSTransformComponent>>& transform_components,
+    ExceptionState& exception_state) {
+  CSSTransformValue* value = Create(transform_components);
+  if (!value) {
+    exception_state.ThrowTypeError(
+        "CSSTransformValue must have at least one component");
+    return nullptr;
+  }
+  return value;
+}
+
+CSSTransformValue* CSSTransformValue::Create(
+    const HeapVector<Member<CSSTransformComponent>>& transform_components) {
+  if (transform_components.IsEmpty())
+    return nullptr;
+  return new CSSTransformValue(transform_components);
+}
+
 CSSTransformValue* CSSTransformValue::FromCSSValue(const CSSValue& css_value) {
   if (!css_value.IsValueList()) {
     // TODO(meade): Also need to check the separator here if we care.
@@ -28,12 +47,8 @@ CSSTransformValue* CSSTransformValue::FromCSSValue(const CSSValue& css_value) {
 }
 
 bool CSSTransformValue::is2D() const {
-  for (size_t i = 0; i < transform_components_.size(); i++) {
-    if (!transform_components_[i]->is2D()) {
-      return false;
-    }
-  }
-  return true;
+  return std::all_of(transform_components_.begin(), transform_components_.end(),
+                     [](const auto& component) { return component->is2D(); });
 }
 
 DOMMatrix* CSSTransformValue::toMatrix(ExceptionState& exception_state) const {
