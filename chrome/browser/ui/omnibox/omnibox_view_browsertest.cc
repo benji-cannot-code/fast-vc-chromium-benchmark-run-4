333264ab01647a8753c6572004347def8b23b575
Tommy C. Li <tommycli@chromium.org>
2017-12-20 21:59:09 +0000

    Omnibox: OmniboxViewTest.* TLC - Remove HistoryServiceObserver.
    After migrating to TaskScheduler, we can simplify the mechanism that
    synchronizes the HistoryService with the test.
    
    Bug: 751031
    Change-Id: Ib91039959b6c83afe0f0ad87942f50c3f73d41a7
    Reviewed-on: https://chromium-review.googlesource.com/837700
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525481}

--- chrome/browser/ui/omnibox/omnibox_view_browsertest.cc
+++ chrome/browser/ui/omnibox/omnibox_view_browsertest.cc
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/bookmarks/browser/bookmark_utils.h"
 #include "components/bookmarks/test/bookmark_test_helpers.h"
 #include "components/history/core/browser/history_service.h"
-#include "components/history/core/browser/history_service_observer.h"
 #include "components/omnibox/browser/autocomplete_input.h"
 #include "components/omnibox/browser/autocomplete_match.h"
 #include "components/omnibox/browser/history_quick_provider.h"
@@ -151,8 +150,7 @@ const int kCtrlOrCmdMask = ui::EF_CONTROL_DOWN;
 }  // namespace
 
 class OmniboxViewTest : public InProcessBrowserTest,
-                        public content::NotificationObserver,
-                        public history::HistoryServiceObserver {
+                        public content::NotificationObserver {
  public:
   OmniboxViewTest() {}
 
@@ -319,19 +317,9 @@ class OmniboxViewTest : public InProcessBrowserTest,
                                         history::SOURCE_BROWSED);
     if (entry.starred)
       bookmarks::AddIfNotBookmarked(bookmark_model, url, base::string16());
-    // Wait at least for the AddPageWithDetails() call to finish.
-    {
-      ScopedObserver<history::HistoryService, history::HistoryServiceObserver>
-          observer(this);
-      observer.Add(history_service);
-      content::RunMessageLoop();
-      // We don't want to return until all observers have processed this
-      // notification, because some (e.g. the in-memory history database) may do
-      // something important.  Since we don't know where in the observer list we
-      // stand, just spin the message loop once more to allow the current
-      // callstack to complete.
-      content::RunAllPendingInMessageLoop();
-    }
+
+    // Running the task scheduler until idle finishes AddPageWithDetails.
+    content::RunAllTasksUntilIdle();
   }
 
   void SetupHistory() {
@@ -367,11 +355,6 @@ class OmniboxViewTest : public InProcessBrowserTest,
       default:
         FAIL() << "Unexpected notification type";
     }
-    base::RunLoop::QuitCurrentWhenIdleDeprecated();
-  }
-
-  void OnURLsModified(history::HistoryService* history_service,
-                      const history::URLRows& changed_urls) override {
     base::RunLoop::QuitCurrentWhenIdleDeprecated();
   }
 
