4bde55d0441367915d64ca7ad299e949a9950ced
clamy <clamy@chromium.org>
2017-12-20 18:51:59 +0000

    Remove NavigationResourceThrottle
    This CL removes the class NavigationResourceThrottle which is no longer
    used. Also removes several ResourceDispatcherHostTests using it that are
    now obsolete due to the navigation architecture change.
    
    Bug=789577
    
    Change-Id: Ia9718ff161ef942b64218106be29b8530df8eca1
    Reviewed-on: https://chromium-review.googlesource.com/776894
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Camille Lamy <clamy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525388}

--- content/browser/loader/navigation_url_loader_network_service.cc
+++ content/browser/loader/navigation_url_loader_network_service.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/frame_host/frame_tree_node.h"
 #include "content/browser/frame_host/navigation_request_info.h"
 #include "content/browser/loader/navigation_resource_handler.h"
-#include "content/browser/loader/navigation_resource_throttle.h"
 #include "content/browser/loader/navigation_url_loader_delegate.h"
 #include "content/browser/loader/resource_dispatcher_host_impl.h"
 #include "content/browser/loader/url_loader_request_handler.h"
