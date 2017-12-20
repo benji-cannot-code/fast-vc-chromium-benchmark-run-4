12850b9ee2787df4d7b36c63ed627e562160dc81
Allen Vicencio <allenvic@chromium.org>
2017-12-20 18:54:55 +0000

    Implement ReadDirectory for SmbFileSystem
    Implements ReadDirectory for SmbFileSystem. This will allow the files
    app to be able to populate entries when inside a directory.
    
    Bug: chromium:757625
    Change-Id: I972bc19a0791acf607f181dd4d051c5b6d4ed9d4
    Reviewed-on: https://chromium-review.googlesource.com/824912
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Allen Vicencio <allenvic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525390}

--- chrome/browser/chromeos/smb_client/smb_file_system.cc
+++ chrome/browser/chromeos/smb_client/smb_file_system.cc
@@ -12,6 +12,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromeos {
 namespace smb_client {
 
+namespace {
+
+storage::DirectoryEntry::DirectoryEntryType MapEntryType(bool is_directory) {
+  return is_directory ? storage::DirectoryEntry::DIRECTORY
+                      : storage::DirectoryEntry::FILE;
+}
+
+}  // namespace
+
 using file_system_provider::AbortCallback;
 
 SmbFileSystem::SmbFileSystem(
@@ -123,7 +132,10 @@ AbortCallback SmbFileSystem::ExecuteAction(
 AbortCallback SmbFileSystem::ReadDirectory(
     const base::FilePath& directory_path,
     const storage::AsyncFileUtil::ReadDirectoryCallback& callback) {
-  NOTIMPLEMENTED();
+  GetSmbProviderClient()->ReadDirectory(
+      GetMountId(), directory_path,
+      base::BindOnce(&SmbFileSystem::HandleRequestReadDirectoryCallback,
+                     weak_ptr_factory_.GetWeakPtr(), callback));
   return AbortCallback();
 }
 
@@ -272,6 +284,18 @@ void SmbFileSystem::SmbFileSystem::Notify(
 void SmbFileSystem::Configure(
     const storage::AsyncFileUtil::StatusCallback& callback) {
   NOTIMPLEMENTED();
+}
+
+void SmbFileSystem::HandleRequestReadDirectoryCallback(
+    const storage::AsyncFileUtil::ReadDirectoryCallback& callback,
+    smbprovider::ErrorType error,
+    const smbprovider::DirectoryEntryList& entries) const {
+  storage::AsyncFileUtil::EntryList entry_list;
+  for (const smbprovider::DirectoryEntry& entry : entries.entries()) {
+    entry_list.emplace_back(entry.name(), MapEntryType(entry.is_directory()));
+  }
+  // TODO(allenvic): Implement has_more (https://crbug.com/796246).
+  callback.Run(TranslateError(error), entry_list, false /* has_more */);
 }
 
 base::WeakPtr<file_system_provider::ProvidedFileSystemInterface>
