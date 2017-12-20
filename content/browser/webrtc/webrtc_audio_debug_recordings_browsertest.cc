4f04be8ccdbff78517d15993b0ad172a1b11357c
Henrik Grunell <grunell@chromium.org>
2017-12-20 19:44:26 +0000

    Set autoplay policy command line switch for WebRtcAudioDebugRecordingsBrowserTest.
    This following https://chromium-review.googlesource.com/c/chromium/src/+/817739, see that CL for details.
    
    Without the policy set in the test, it was broken due to not all output streams were being played out, and thus not all expected files were created.
    
    Also, re-enables the test cases that were recently disabled in https://chromium-review.googlesource.com/c/chromium/src/+/833930.
    
    Bug: None
    Change-Id: If99abb951ae7b1f2eb12b7d46f9e644b0b647aa6
    Reviewed-on: https://chromium-review.googlesource.com/833872
    Commit-Queue: Henrik Grunell <grunell@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525414}

--- content/browser/webrtc/webrtc_audio_debug_recordings_browsertest.cc
+++ content/browser/webrtc/webrtc_audio_debug_recordings_browsertest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/content_browser_test_utils.h"
 #include "content/shell/browser/shell.h"
+#include "media/base/media_switches.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
 
 #if defined(OS_WIN)
@@ -113,13 +114,16 @@ class WebRtcAudioDebugRecordingsBrowserTest
 // therefore timing dependent and flaky prone. If it becomes flaky, it's
 // probably good enough to change the test to retry deleting after a short
 // sleep.
-// TODO(https://crbug.com/796168): Fails on all platforms. Disabled temporarily.
 IN_PROC_BROWSER_TEST_F(WebRtcAudioDebugRecordingsBrowserTest,
-                       DISABLED_CallWithAudioDebugRecordings) {
+                       MAYBE_CallWithAudioDebugRecordings) {
   if (!HasAudioOutputDevices()) {
     LOG(INFO) << "Missing output devices: skipping test...";
     return;
   }
+
+  base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
+      switches::kAutoplayPolicy,
+      switches::autoplay::kNoUserGestureRequiredPolicy);
 
   bool prev_io_allowed = base::ThreadRestrictions::SetIOAllowed(true);
 
@@ -250,13 +254,16 @@ IN_PROC_BROWSER_TEST_F(WebRtcAudioDebugRecordingsBrowserTest,
 #endif
 
 // Same test as CallWithAudioDebugRecordings, but does two parallel calls.
-// TODO(https://crbug.com/796168): Fails on all platforms. Disabled temporarily.
 IN_PROC_BROWSER_TEST_F(WebRtcAudioDebugRecordingsBrowserTest,
-                       DISABLED_TwoCallsWithAudioDebugRecordings) {
+                       MAYBE_TwoCallsWithAudioDebugRecordings) {
   if (!HasAudioOutputDevices()) {
     LOG(INFO) << "Missing output devices: skipping test...";
     return;
   }
+
+  base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
+      switches::kAutoplayPolicy,
+      switches::autoplay::kNoUserGestureRequiredPolicy);
 
   bool prev_io_allowed = base::ThreadRestrictions::SetIOAllowed(true);
 
