0c21b77ec524a589c95c92db2f3271e3a182763a
John Abd-El-Malek <jam@chromium.org>
2017-12-20 21:35:11 +0000

    Follow-up to r525102 to remove old QUIC certs in unittests.
    Bug: 769401
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I97d40b2383c0f29f1f79ced1cf94f136ef64cb10
    Reviewed-on: https://chromium-review.googlesource.com/834748
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Misha Efimov <mef@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525466}

--- components/grpc_support/test/quic_test_server.cc
+++ components/grpc_support/test/quic_test_server.cc
@@ -26,8 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace grpc_support {
 
 const char kTestServerDomain[] = "example.com";
-// This must match the certificate used (quic_test.example.com.crt and
-// quic_test.example.com.key.pkcs8).
+// This must match the certificate used (quic-chain.pem and quic-leaf-cert.key).
 const char kTestServerHost[] = "test.example.com";
 const char kTestServerUrl[] = "https://test.example.com/hello.txt";
 
@@ -85,7 +84,7 @@ void StartQuicServerOnServerThread(const base::FilePath& test_files_root,
   std::unique_ptr<net::ProofSourceChromium> proof_source(
       new net::ProofSourceChromium());
   CHECK(proof_source->Initialize(directory.AppendASCII("quic-chain.pem"),
-                                 directory.AppendASCII("quic-cert.key"),
+                                 directory.AppendASCII("quic-leaf-cert.key"),
                                  base::FilePath()));
   SetupQuicHttpResponseCache();
 
