19330dcd4ddbb81ca9db5410cc995eedf0db3e89
Andrew Grieve <agrieve@chromium.org>
2017-12-20 16:16:47 -0500

    Add use_unprocessed_jars option to dist_jar
    Plus some tidying up of the related build rules.
    unprocessed_jars are useful when you want to export jars to other build
    systems. (e.g. cronet)
    
    Change-Id: I22043b725ef5615c7bee7c9de287393e2072e869
    Reviewed-on: https://chromium-review.googlesource.com/833158
    Reviewed-by: Eric Stevenson <estevenson@chromium.org>
    Commit-Queue: agrieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525705}

--- build/android/gyp/create_dist_jar.py
+++ build/android/gyp/create_dist_jar.py
@@ -7,24 +7,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 """Merges a list of jars into a single jar."""
 
-import optparse
+import argparse
 import sys
 
 from util import build_utils
 
+
 def main(args):
   args = build_utils.ExpandFileArgs(args)
-  parser = optparse.OptionParser()
+  parser = argparse.ArgumentParser()
   build_utils.AddDepfileOption(parser)
-  parser.add_option('--output', help='Path to output jar.')
-  parser.add_option('--inputs', action='append', help='List of jar inputs.')
-  options, _ = parser.parse_args(args)
-  build_utils.CheckOptions(options, parser, ['output', 'inputs'])
+  parser.add_argument('--output', required=True, help='Path to output jar.')
+  parser.add_argument('--jars', required=True, help='GN list of jar inputs.')
+  options = parser.parse_args(args)
 
-  input_jars = []
-  for inputs_arg in options.inputs:
-    input_jars.extend(build_utils.ParseGnList(inputs_arg))
-
+  input_jars = build_utils.ParseGnList(options.jars)
   build_utils.MergeZips(options.output, input_jars)
 
   if options.depfile:
@@ -32,4 +29,4 @@ def main(args):
 
 
 if __name__ == '__main__':
-  sys.exit(main(sys.argv[1:]))
+  main(sys.argv[1:])
