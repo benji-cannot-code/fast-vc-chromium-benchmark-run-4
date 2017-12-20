7dcf7b453e96ee1deafde9648dc5fba5d2b1ddaf
Tommy Steimel <steimel@chromium.org>
2017-12-20 15:16:16 -0800

    Call superclass event handler in MediaControlOverlayPlayButtonElement
    This CL adds a call to properly propagate events on the overlay play
    button element. This fixes an issue where right-clicks on the outer
    parts of the overlay were not opening the video context menu.
    
    Bug: 787447
    Change-Id: Ic80966fc2e757bec68167bc03e959b9000d82d4b
    Reviewed-on: https://chromium-review.googlesource.com/838081
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525684}

--- third_party/WebKit/Source/modules/media_controls/elements/MediaControlOverlayPlayButtonElement.cpp
+++ third_party/WebKit/Source/modules/media_controls/elements/MediaControlOverlayPlayButtonElement.cpp
@@ -250,8 +250,7 @@ void MediaControlOverlayPlayButtonElement::DefaultEventHandler(Event* event) {
       event->SetDefaultHandled();
     }
   }
-
-  // TODO(mlamouri): should call MediaControlInputElement::DefaultEventHandler.
+  MediaControlInputElement::DefaultEventHandler(event);
 }
 
 bool MediaControlOverlayPlayButtonElement::KeepEventInNode(Event* event) {
