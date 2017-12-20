0d036f41f74c6e3620a64395b3303038bd159cff
Peter Laurens <peterlaurens@google.com>
2017-12-20 18:08:33 +0000

    Encapsulate dismissal cleanup within SettingsNavigationController
    Bug: 782943
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: If5ebc06503448526b57f42ea6543b5373efe2c90
    Reviewed-on: https://chromium-review.googlesource.com/826024
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: edchin <edchin@chromium.org>
    Commit-Queue: Peter Laurens <peterlaurens@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525372}

--- ios/chrome/browser/ui/settings/settings_navigation_controller.h
+++ ios/chrome/browser/ui/settings/settings_navigation_controller.h
@@ -42,7 +42,8 @@ class ChromeBrowserState;
 
 // Controller to modify user settings.
 @interface SettingsNavigationController
-    : UINavigationController<ApplicationSettingsCommands>
+    : UINavigationController<ApplicationSettingsCommands,
+                             SettingsControllerProtocol>
 
 // Whether sync changes should be committed when the settings are being
 // dismissed. Defaults to YES.
@@ -138,10 +139,6 @@ initWithRootViewController:(UIViewController*)rootViewController
 
 // Returns the current main browser state.
 - (ios::ChromeBrowserState*)mainBrowserState;
-
-// Notifies this |SettingsNavigationController| that it will be dismissed such
-// that it has a possibility to do necessary clean up.
-- (void)settingsWillBeDismissed;
 
 // Closes this |SettingsNavigationController| by asking its delegate.
 - (void)closeSettings;
