e4bdab80ccb64fd68bc5c293d9bff75dae31335d
Dmitry Skiba <dskiba@chromium.org>
2017-12-20 22:24:18 +0000

    Don't call non-CCT pending intents on FRE failures.
    FirstRunActivity calls pending intent even if it was canceled. This
    causes visual glitch in the most common case when Chrome is started
    from a launcher icon (i.e. via MAIN intent), because pending intent
    in this case is CTA, and it has window background. So when CTA is
    called back with EXTRA_FIRST_RUN_COMPLETE=false, window background
    is displayed for a few moments it takes CTA to abort itself again in
    AsyncInitializationActivity.onCreateInternal().
    
    However, there is no point in calling back the pending intent on
    failure, unless the intent is for CCT, in which case it always needs
    to invoke a callback.
    
    This CL changes FRE to call pending intent on failure only when it's
    a CCT intent.
    
    Bug: 788153
    Change-Id: Ibfad5063ee389c54d02200da9e6ab8390daf75a8
    Reviewed-on: https://chromium-review.googlesource.com/833176
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Dmitry Skiba <dskiba@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525495}

--- chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunFlowSequencer.java
+++ chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunFlowSequencer.java
@@ -21,6 +21,7 @@ import org.chromium.base.Log;
 import org.chromium.base.VisibleForTesting;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeSwitches;
+import org.chromium.chrome.browser.LaunchIntentDispatcher;
 import org.chromium.chrome.browser.locale.LocaleManager;
 import org.chromium.chrome.browser.net.spdyproxy.DataReductionProxySettings;
 import org.chromium.chrome.browser.preferences.PrefServiceBridge;
@@ -312,6 +313,8 @@ public abstract class FirstRunFlowSequencer  {
                     caller, FIRST_RUN_EXPERIENCE_REQUEST_CODE, fromIntent, pendingIntentFlags);
         }
         firstRunIntent.putExtra(FirstRunActivity.EXTRA_CHROME_LAUNCH_INTENT, pendingIntent);
+        firstRunIntent.putExtra(FirstRunActivity.EXTRA_CHROME_LAUNCH_INTENT_IS_CCT,
+                LaunchIntentDispatcher.isCustomTabIntent(fromIntent));
     }
 
     /**
