75d1191abebe7495d6a3dd1c58cb3992a38c3e4f
Xiaohan Wang <xhwang@chromium.org>
2017-12-20 22:11:50 +0000

    media: Remove fake audio/video decoders from ClearKeyCdm
    These fake decoders were used in the early stage of the project and has
    not been used for long time. In fact, there's no easy way to enable them
    now, possible due to gyp->gn migration. They don't provide much value
    either given we already have the "real" decoders.
    
    BUG=796345
    TEST=Test cleanup
    
    Change-Id: I94d0351ee5ea980a14a673ca30750aea5ea85207
    Reviewed-on: https://chromium-review.googlesource.com/836415
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525488}

--- media/cdm/ppapi/external_clear_key/cdm_video_decoder.cc
+++ media/cdm/ppapi/external_clear_key/cdm_video_decoder.cc
@@ -5,10 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/cdm/ppapi/external_clear_key/cdm_video_decoder.h"
 
-#if defined(CLEAR_KEY_CDM_USE_FAKE_VIDEO_DECODER)
-#include "media/cdm/ppapi/external_clear_key/fake_cdm_video_decoder.h"
-#endif
-
 #if defined(CLEAR_KEY_CDM_USE_FFMPEG_DECODER)
 #include "media/cdm/ppapi/external_clear_key/ffmpeg_cdm_video_decoder.h"
 #endif
@@ -23,12 +19,6 @@ std::unique_ptr<CdmVideoDecoder> CreateVideoDecoder(
     CdmHostProxy* cdm_host_proxy,
     const cdm::VideoDecoderConfig& config) {
   std::unique_ptr<CdmVideoDecoder> video_decoder;
-#if defined(CLEAR_KEY_CDM_USE_FAKE_VIDEO_DECODER)
-  video_decoder.reset(new FakeCdmVideoDecoder(cdm_host_proxy));
-
-  if (!video_decoder->Initialize(config))
-    video_decoder.reset();
-#else
 
 #if defined(CLEAR_KEY_CDM_USE_LIBVPX_DECODER)
   if (config.codec == cdm::VideoDecoderConfig::kCodecVp8 ||
@@ -48,8 +38,6 @@ std::unique_ptr<CdmVideoDecoder> CreateVideoDecoder(
   if (!video_decoder->Initialize(config))
     video_decoder.reset();
 #endif
-
-#endif  // CLEAR_KEY_CDM_USE_FAKE_VIDEO_DECODER
 
   return video_decoder;
 }
