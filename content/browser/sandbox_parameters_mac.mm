1a77486dd56cbf26be62f51aabd224f513e9107d
Greg Kerr <kerrnel@chromium.org>
2017-12-20 21:49:57 +0000

    macOS V2 Sandbox: Sandbox PPAPI Processes.
    This launches PPAPI processes under the new macOS V2 sandbox.
    
    Bug: 689306
    Change-Id: I56877dd163d5120f251ad6a791c83383814c641e
    Reviewed-on: https://chromium-review.googlesource.com/830976
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Mike Pinkerton <pinkerton@chromium.org>
    Commit-Queue: Greg Kerr <kerrnel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525475}

--- content/browser/sandbox_parameters_mac.mm
+++ content/browser/sandbox_parameters_mac.mm
@@ -40,6 +40,8 @@ std::string GetOSVersion() {
   return std::to_string(final_os_version);
 }
 
+}  // namespace
+
 void SetupCommonSandboxParameters(sandbox::SeatbeltExecClient* client) {
   const base::CommandLine* command_line =
       base::CommandLine::ForCurrentProcess();
@@ -88,12 +90,6 @@ void SetupCommonSandboxParameters(sandbox::SeatbeltExecClient* client) {
       service_manager::SandboxMac::GetCanonicalPath(base::GetHomeDir()).value();
   CHECK(client->SetParameter(
       service_manager::SandboxMac::kSandboxHomedirAsLiteral, homedir));
-}
-
-}  // namespace
-
-void SetupRendererSandboxParameters(sandbox::SeatbeltExecClient* client) {
-  SetupCommonSandboxParameters(client);
 }
 
 void SetupUtilitySandboxParameters(sandbox::SeatbeltExecClient* client,
