e171c7937526fcca2ea2633da5ce6622d6fd7490
Mohamad Ahmadi <mahmadi@chromium.org>
2017-12-20 14:49:56 -0500

    [PR] Fixes crash when page navigates away before calling response.complete
    If the page never calls response.complete(), and instead navigates away,
    this causes the pending request to be rejected and set to nil. Once
    response.complete() times out and gets called automatically there's no
    pending request to record metrics on. The solution is to invalidate the
    response.complete() timeout if the request promise is rejected for any
    reason.
    
    Bug: 796652
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I534be661e4808507babf35a3330069e3992d5891
    Reviewed-on: https://chromium-review.googlesource.com/836759
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525723}

--- ios/chrome/browser/ui/payments/payment_request_manager.mm
+++ ios/chrome/browser/ui/payments/payment_request_manager.mm
@@ -388,6 +388,10 @@ struct PendingPaymentResponse {
   DCHECK(_pendingPaymentRequest->state() ==
          payments::PaymentRequest::State::INTERACTIVE);
 
+  [_unblockEventQueueTimer invalidate];
+  [_paymentResponseTimeoutTimer invalidate];
+  [_updateEventTimeoutTimer invalidate];
+
   [self resetIOSPaymentInstrumentLauncherDelegate];
 
   __weak PaymentRequestManager* weakSelf = self;
@@ -662,10 +666,6 @@ paymentRequestFromMessage:(const base::DictionaryValue&)message
     return YES;
   }
 
-  [_unblockEventQueueTimer invalidate];
-  [_paymentResponseTimeoutTimer invalidate];
-  [_updateEventTimeoutTimer invalidate];
-
   __weak PaymentRequestManager* weakSelf = self;
   ProceduralBlockWithBool cancellationCallback = ^(BOOL) {
     [[weakSelf paymentRequestJsManager]
@@ -755,10 +755,6 @@ paymentRequestFromMessage:(const base::DictionaryValue&)message
           payments::PaymentRequest::State::INTERACTIVE) {
     return YES;
   }
-
-  [_unblockEventQueueTimer invalidate];
-  [_paymentResponseTimeoutTimer invalidate];
-  [_updateEventTimeoutTimer invalidate];
 
   __weak PaymentRequestManager* weakSelf = self;
   ProceduralBlock callback = ^{
