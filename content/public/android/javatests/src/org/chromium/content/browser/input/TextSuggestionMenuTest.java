a304da903d219b5e326b04f25ef77d8cfe6c6db5
Xiaocheng Hu <xiaochengh@chromium.org>
2017-12-20 21:44:43 +0000

    Re-Enable TextSuggestionMenuTest.testDeleteWordMarkedWithSpellingMarker
    A previous CL [1] disabled the test to ease a change in spellchecker [2].
    
    Now that the change is done, the test can be re-enabled.
    
    [1] crrev.com/c/823604
    
    [2] crrev.com/c/821496
    
    Tbr: changwan@chromium.org
    Bug: 710097
    Change-Id: Iaf666bd0b4f45c6dabdb3fa34537e0f6156cdbed
    Reviewed-on: https://chromium-review.googlesource.com/827595
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Changwan Ryu <changwan@chromium.org>
    Reviewed-by: Ryan Landay <rlanday@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525471}

--- content/public/android/javatests/src/org/chromium/content/browser/input/TextSuggestionMenuTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/input/TextSuggestionMenuTest.java
@@ -21,7 +21,6 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.content.R;
 import org.chromium.content.browser.ContentViewCore;
 import org.chromium.content.browser.test.ContentJUnit4ClassRunner;
@@ -87,7 +86,6 @@ public class TextSuggestionMenuTest {
     }
 
     @Test
-    @DisabledTest(message = "crbug.com/710097")
     @LargeTest
     public void testDeleteWordMarkedWithSpellingMarker()
             throws InterruptedException, Throwable, TimeoutException {
@@ -115,11 +113,8 @@ public class TextSuggestionMenuTest {
         });
 
         // Add a spelling marker on "hello".
-        // Note: we disable spell checking first to avoid the spell checker immediately clearing
-        // the added marker.
         JavaScriptUtils.executeJavaScriptAndWaitForResult(webContents,
-                "internals.setSpellCheckingEnabled(false);"
-                        + "const div = document.getElementById('div');"
+                "const div = document.getElementById('div');"
                         + "const text = div.firstChild;"
                         + "const range = document.createRange();"
                         + "range.setStart(text, 0);"
