e3a0671f1b5343569dbb654965d7abb9870aae20
Sandra Sun <sunyunjia@chromium.org>
2017-12-20 05:12:29 +0000

    Share SnapPoints data between cc and blink
    This patch does the following:
    1. Moves the data structure of SnapPoints from blink to cc and shares it back with blink.
    2. Use the coordinate of ScrollPosition and the type of FloatPoint to do calculations in
       SnapCoordinator instead of ScrollOffset, as gfx::ScrollOffset uses ScrollPosition.
    3. Update the logic in SnapCoordinator to work with vertical-rl writing-mode.
    4. Use LocalToAncestorQuad() for the snap_area to ensure transforms are properly dealt with.
    
    
    Bug: 795404
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I538327e169ebee4d3e03ec2ddd0d5b282e36f5de
    Reviewed-on: https://chromium-review.googlesource.com/825994
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Majid Valipour <majidvp@chromium.org>
    Commit-Queue: Sandra Sun <sunyunjia@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525271}

--- third_party/WebKit/Source/core/page/scrolling/SnapCoordinator.h
+++ third_party/WebKit/Source/core/page/scrolling/SnapCoordinator.h
@@ -14,10 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class LayoutBox;
-struct ScrollSnapType;
-struct ScrollSnapAlign;
-struct SnapAreaData;
-struct SnapContainerData;
 
 // Snap Coordinator keeps track of snap containers and all of their associated
 // snap areas. It also contains the logic to generate the list of valid snap
@@ -49,8 +45,7 @@ class CORE_EXPORT SnapCoordinator final
   // container.
   SnapAreaData CalculateSnapAreaData(const LayoutBox& snap_area,
                                      const LayoutBox& snap_container,
-                                     const ScrollOffset& min_offset,
-                                     const ScrollOffset& max_offset);
+                                     const FloatPoint& max_position);
 
   // Called by LocalFrameView::PerformPostLayoutTasks(), so that the snap data
   // are updated whenever a layout happens.
@@ -66,8 +61,8 @@ class CORE_EXPORT SnapCoordinator final
   bool GetSnapPosition(const LayoutBox& snap_container,
                        bool did_scroll_x,
                        bool did_scroll_y,
-                       ScrollOffset* snap_offset);
-  static ScrollOffset FindSnapOffset(const ScrollOffset& current_offset,
+                       FloatPoint* snap_position);
+  static FloatPoint FindSnapPosition(const FloatPoint& current_position,
                                      const SnapContainerData&,
                                      bool should_snap_on_x,
                                      bool should_snap_on_y);
