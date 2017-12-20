c2d141a778cd23e4342e54c2c029c2f6dedb8fe4
Chrome Cunningham <chcunningham@chromium.org>
2017-12-20 18:59:03 +0000

    Revert "Clean up proprietary codec checks/expectations in LayoutTests"
    This reverts commit 117b9d3a698a52d96857b31caeb72c2e74f2a782.
    
    Reason for revert: CL is fine, but it depends on another CL that was reverted: https://chromium-review.googlesource.com/c/chromium/src/+/836414
    
    That CL will probably land again, so expecting to reland this one as is % rebase.
    
    Original change's description:
    > Clean up proprietary codec checks/expectations in LayoutTests
    > 
    > This is mostly a reland of
    > https://chromium-review.googlesource.com/c/chromium/src/+/807604
    > with some improvements and now unblocked by changes to bots.
    > 
    > Media layout tests need repair
    > 1) Many tests check for proprietary codec support by only asking about
    >    "video/mp4" without specifying a codec. This check is busted
    >    because MP4 is no longer considered proprietary while codecs like
    >    H264 and AAC still are. We currently claim "video/mp4" is entirely
    >    unsupported via test hacks to avoid breaking those checks.
    > 2) Some MSE tests focus on MP4 with H264. While many bots now have the
    >    needed codecs, we artificially remove their support via test-only
    >    hacks in order to keep expectations the same for all runners.
    > 
    > This CL does the following
    > 1) Remove incomplete checks for "video/mp4" - instead hardcode use of
    >    the .ogv files (preserving the existing behavior).
    > 2) Update MSE mp4/h264 test expectations to PASS. Starting this week
    >    all webkit bots include the needed codecs.
    > 3) Remove test hacks that artificially reduce codec support.
    > 
    > It is also not a goal to make LayoutTests try all the supported codecs.
    > That is covered by unit/integration tests.
    > 
    > Due to 3) above, some external/wpt/media-source tests now fail because
    > they select mp4. Fixing these failures is tracked in Issue 794338.
    > 
    > TBR: dalecurtis@chromium.org, peter@chromium.org
    > Bug: 327115,746579,787575,568704,794338
    > Change-Id: I8443d32fc9871c887d0009605cf8876521844916
    > Reviewed-on: https://chromium-review.googlesource.com/827649
    > Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    > Commit-Queue: Chrome Cunningham <chcunningham@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#525212}
    
    TBR=dalecurtis@chromium.org,peter@chromium.org,chcunningham@chromium.org
    
    Change-Id: I4dbb4a6ad23a18e2eab5af62a2d669a477e8eaf2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 327115, 746579, 787575, 568704, 794338
    Reviewed-on: https://chromium-review.googlesource.com/837247
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Commit-Queue: Chrome Cunningham <chcunningham@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525391}

--- third_party/WebKit/LayoutTests/media/media-fragments/media-fragments.js
+++ third_party/WebKit/LayoutTests/media/media-fragments/media-fragments.js
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     var consoleDiv = null;
     var currentTest = null;
     var fragmentEndTime;
-    var testData =
+    var testData = 
     {
         // http://www.w3.org/2008/WebVideo/Fragments/TC/ua-test-cases
 
@@ -232,10 +232,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     function start()
     {
         video = document.createElement('video');
-        video.setAttribute('id', 'vid');
-        video.setAttribute('width', '320');
-        video.setAttribute('height', '240');
-        video.setAttribute('controls', '');
+        video.setAttribute('id', 'vid'); 
+        video.setAttribute('width', '320'); 
+        video.setAttribute('height', '240'); 
+        video.setAttribute('controls', ''); 
         var paragraph = document.createElement('p');
         paragraph.appendChild(video);
         document.body.appendChild(paragraph);
@@ -250,6 +250,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         consoleWrite("<br>Title: <b>" + currentTest + "</b>");
         consoleWrite("Fragment: '<i>" + info.fragment + "</i>'");
         consoleWrite("Comment: <i>" + info.comment + "</i>");
-        url = "../content/counting.ogv" + "#" + info.fragment;
+        url = findMediaFile("video", "../content/counting") + "#" + info.fragment;
         video.src = url;
     }
