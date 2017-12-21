e3c10857c0b3ee5a316b5664c0ffa1d61775ac32
Saman Sami <samans@chromium.org>
2017-12-21 01:47:37 +0000

    Surface synchronization: Allocate new id when ScreenInfo changes
    RenderWidgetHostViewAura::UpdateScreenInfo only calls
    window_->AllocateLocalSurfaceId() in auto-resize mode. We want
    ScreenInfo to be synchronized across clients independent of whether
    auto-resize is on or not. This CL fixes crbug.com/791154, in
    which the renderer crashes because it doesn't receive a new
    LocalSurfaceId when device scale factor changes.
    
    Bug: 672962,791154
    Change-Id: If9c3836aee980a31e8d50159f6491b52bf812a6d
    Reviewed-on: https://chromium-review.googlesource.com/836820
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Saman Sami <samans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525557}

--- content/browser/renderer_host/render_widget_host_view_aura_unittest.cc
+++ content/browser/renderer_host/render_widget_host_view_aura_unittest.cc
@@ -2323,9 +2323,9 @@ TEST_F(RenderWidgetHostViewAuraTest, AutoResizeWithScale) {
   sink_->ClearMessages();
   aura_test_helper_->test_screen()->SetDeviceScaleFactor(2.0f);
 
-  EXPECT_EQ(2u, sink_->message_count());
+  EXPECT_EQ(1u, sink_->message_count());
   {
-    const IPC::Message* msg = sink_->GetMessageAt(1);
+    const IPC::Message* msg = sink_->GetMessageAt(0);
     EXPECT_EQ(static_cast<uint32_t>(ViewMsg_SetLocalSurfaceIdForAutoResize::ID),
               msg->type());
     ViewMsg_SetLocalSurfaceIdForAutoResize::Param params;
@@ -3082,6 +3082,7 @@ TEST_F(RenderWidgetHostViewAuraSurfaceSynchronizationTest,
       view_->GetNativeView(), parent_view_->GetNativeView()->GetRootWindow(),
       gfx::Rect());
 
+  view_->window_->layer()->SetShowSolidColorContent();
   EXPECT_FALSE(view_->HasPrimarySurface());
   ASSERT_TRUE(view_->delegated_frame_host_);
 
@@ -3108,7 +3109,6 @@ TEST_F(RenderWidgetHostViewAuraSurfaceSynchronizationTest, SurfaceChanges) {
 
   // Prevent the DelegatedFrameHost from skipping frames.
   view_->DisableResizeLock();
-  EXPECT_FALSE(view_->HasPrimarySurface());
   ASSERT_TRUE(view_->delegated_frame_host_);
 
   view_->SetSize(gfx::Size(300, 300));
@@ -3144,7 +3144,6 @@ TEST_F(RenderWidgetHostViewAuraSurfaceSynchronizationTest,
 
   // Prevent the DelegatedFrameHost from skipping frames.
   view_->DisableResizeLock();
-  EXPECT_FALSE(view_->HasPrimarySurface());
 
   view_->SetSize(gfx::Size(300, 300));
   ASSERT_TRUE(view_->HasPrimarySurface());
