a726c182270fff6a7af4a784b7c96266e2ed541b
bsheedy <bsheedy@chromium.org>
2017-12-21 14:41:21 -0800

    Deduplicate native URL lists for VR tests
    Moves the native URL lists that VrShellNativeUiTest#testUrlOnNativeUi
    and VrShellNavigationTest#testNativeNavigateAndInteraction use to a
    constant, and switches testUrlOnNativeUi to use a loop instead of
    duplicating lines for each URL.
    
    Bug: 
    Change-Id: I0a4e7889a25375ebd760bdd408ba2014442f1927
    Reviewed-on: https://chromium-review.googlesource.com/841402
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526018}

--- chrome/android/javatests/src/org/chromium/chrome/browser/vr_shell/VrShellNavigationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/vr_shell/VrShellNavigationTest.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.vr_shell;
 
+import static org.chromium.chrome.browser.vr_shell.VrTestFramework.NATIVE_URLS_OF_INTEREST;
 import static org.chromium.chrome.browser.vr_shell.VrTestFramework.PAGE_LOAD_TIMEOUT_S;
 import static org.chromium.chrome.browser.vr_shell.VrTestFramework.POLL_TIMEOUT_LONG_MS;
 import static org.chromium.chrome.browser.vr_shell.VrTestFramework.POLL_TIMEOUT_SHORT_MS;
@@ -358,10 +359,7 @@ public class VrShellNavigationTest {
     @Restriction(RESTRICTION_TYPE_VIEWER_DAYDREAM)
     public void testNativeNavigationAndInteraction()
             throws IllegalArgumentException, InterruptedException {
-        String[] nativeUrls = {UrlConstants.NTP_URL, UrlConstants.BOOKMARKS_URL,
-                UrlConstants.DOWNLOADS_URL, UrlConstants.RECENT_TABS_URL,
-                UrlConstants.NATIVE_HISTORY_URL};
-        for (String url : nativeUrls) {
+        for (String url : NATIVE_URLS_OF_INTEREST) {
             mVrTestRule.loadUrl(TEST_PAGE_2D_URL, PAGE_LOAD_TIMEOUT_S);
             mVrTestRule.loadUrl(url, PAGE_LOAD_TIMEOUT_S);
             ClickUtils.mouseSingleClickView(InstrumentationRegistry.getInstrumentation(),
