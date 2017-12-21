387b2f90bfda7034f90767488e2862843c511d1b
Jason Chase <chasej@chromium.org>
2017-12-21 12:33:30 -0500

    Minor cleanup to Origin Trials test code
    Best practice for integrating with origin trials is to use feature-specific
    methods, provided by the generated OriginTrials class. This CL corrects the
    setup code for layout tests to use appropriate generated method.
    
    Also, trivial comment cleanup for changes to the generated file names.
    
    Bug: 
    Change-Id: If032e968dd83626783b35bb636d72f9ff5733f2c
    Reviewed-on: https://chromium-review.googlesource.com/840181
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Jason Chase <chasej@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525791}

--- third_party/WebKit/Source/core/testing/v8/WebCoreTestSupport.cpp
+++ third_party/WebKit/Source/core/testing/v8/WebCoreTestSupport.cpp
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/Document.h"
 #include "core/dom/ExecutionContext.h"
 #include "core/frame/LocalFrame.h"
-#include "core/origin_trials/OriginTrialContext.h"
+#include "core/origin_trials/origin_trials.h"
 #include "core/testing/InternalSettings.h"
 #include "core/testing/Internals.h"
 #include "core/testing/WorkerInternals.h"
@@ -93,11 +93,9 @@ void InstallOriginTrialFeaturesForTesting(
 
   blink::ExecutionContext* execution_context =
       blink::ExecutionContext::From(script_state);
-  const blink::OriginTrialContext* originTrialContext =
-      blink::OriginTrialContext::From(execution_context);
 
   if (type == &blink::V8OriginTrialsTest::wrapperTypeInfo) {
-    if (originTrialContext && originTrialContext->IsTrialEnabled("Frobulate")) {
+    if (blink::OriginTrials::originTrialsSampleAPIEnabled(execution_context)) {
       blink::V8OriginTrialsTest::installOriginTrialsSampleAPI(
           script_state->GetIsolate(), script_state->World(),
           v8::Local<v8::Object>(), prototype_object, interface_object);
