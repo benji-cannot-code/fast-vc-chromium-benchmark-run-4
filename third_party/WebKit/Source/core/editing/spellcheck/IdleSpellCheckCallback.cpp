93eff0e0624d1e28dd1dad0d8a88cc85ac47d529
Xiaocheng Hu <xiaochengh@chromium.org>
2017-12-20 16:04:44 -0800

    Make IdleSpellCheckerCallback get Document properly
    IdleSpellCheckerCallback is a DocumentShutdownObserver but gets document
    via LocalFrame::GetDocument(). This leads to some invalid access to
    inactive document and have caused crashes (crbug.com/793964).
    
    This patch changes the class to get document via DSO::LifecycleObserver
    so that it can check document activeness in a sane way, and never gets
    an inactive document.
    
    Bug: 
    Change-Id: If1ad88f969b106105de7eb1fecbf9c16b8d26dd6
    Reviewed-on: https://chromium-review.googlesource.com/837588
    Reviewed-by: Ryan Landay <rlanday@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525846}

--- third_party/WebKit/Source/core/editing/spellcheck/IdleSpellCheckCallback.cpp
+++ third_party/WebKit/Source/core/editing/spellcheck/IdleSpellCheckCallback.cpp
@@ -74,13 +74,13 @@ void IdleSpellCheckCallback::Deactivate() {
   state_ = State::kInactive;
   if (cold_mode_timer_.IsActive())
     cold_mode_timer_.Stop();
-  if (idle_callback_handle_ != kInvalidHandle)
-    GetFrame().GetDocument()->CancelIdleCallback(idle_callback_handle_);
+  if (idle_callback_handle_ != kInvalidHandle && IsAvailable())
+    GetDocument().CancelIdleCallback(idle_callback_handle_);
   idle_callback_handle_ = kInvalidHandle;
 }
 
 void IdleSpellCheckCallback::SetNeedsInvocation() {
-  if (!IsSpellCheckingEnabled()) {
+  if (!IsSpellCheckingEnabled() || !IsAvailable()) {
     Deactivate();
     return;
   }
@@ -94,14 +94,13 @@ void IdleSpellCheckCallback::SetNeedsInvocation() {
   }
 
   if (state_ == State::kColdModeRequested) {
-    GetFrame().GetDocument()->CancelIdleCallback(idle_callback_handle_);
+    GetDocument().CancelIdleCallback(idle_callback_handle_);
     idle_callback_handle_ = kInvalidHandle;
   }
 
   IdleRequestOptions options;
   options.setTimeout(kHotModeRequestTimeoutMS);
-  idle_callback_handle_ =
-      GetFrame().GetDocument()->RequestIdleCallback(this, options);
+  idle_callback_handle_ = GetDocument().RequestIdleCallback(this, options);
   state_ = State::kHotModeRequested;
 }
 
@@ -128,13 +127,13 @@ void IdleSpellCheckCallback::ColdModeTimerFired(TimerBase*) {
   DCHECK(RuntimeEnabledFeatures::IdleTimeColdModeSpellCheckingEnabled());
   DCHECK_EQ(State::kColdModeTimerStarted, state_);
 
-  if (!IsSpellCheckingEnabled()) {
+  if (!IsSpellCheckingEnabled() || !IsAvailable()) {
     Deactivate();
     return;
   }
 
   idle_callback_handle_ =
-      GetFrame().GetDocument()->RequestIdleCallback(this, IdleRequestOptions());
+      GetDocument().RequestIdleCallback(this, IdleRequestOptions());
   state_ = State::kColdModeRequested;
 }
 
@@ -142,7 +141,7 @@ void IdleSpellCheckCallback::HotModeInvocation(IdleDeadline* deadline) {
   TRACE_EVENT0("blink", "IdleSpellCheckCallback::hotModeInvocation");
 
   // TODO(xiaochengh): Figure out if this has any performance impact.
-  GetFrame().GetDocument()->UpdateStyleAndLayout();
+  GetDocument().UpdateStyleAndLayout();
 
   HotModeSpellCheckRequester requester(GetSpellCheckRequester());
 
@@ -161,22 +160,17 @@ void IdleSpellCheckCallback::HotModeInvocation(IdleDeadline* deadline) {
     // The ending selection stored in undo stack can be invalid, disconnected
     // or have been moved to another document, so we should check its validity
     // before using it.
-    if (!step->EndingSelection().IsValidFor(*GetFrame().GetDocument()))
+    if (!step->EndingSelection().IsValidFor(GetDocument()))
       continue;
     requester.CheckSpellingAt(step->EndingSelection().Extent());
   }
 }
 
 void IdleSpellCheckCallback::invoke(IdleDeadline* deadline) {
-  // TODO(xiaochengh): IdleSpellCheckCallback should get Document via
-  // SynchronousMutationObserver::GetContext().
-  if (!GetFrame().GetDocument() || !GetFrame().GetDocument()->IsActive())
-    return;
-
   DCHECK_NE(idle_callback_handle_, kInvalidHandle);
   idle_callback_handle_ = kInvalidHandle;
 
-  if (!IsSpellCheckingEnabled()) {
+  if (!IsSpellCheckingEnabled() || !IsAvailable()) {
     Deactivate();
     return;
   }
@@ -224,7 +218,7 @@ void IdleSpellCheckCallback::ForceInvocationForTesting() {
       break;
     case State::kHotModeRequested:
     case State::kColdModeRequested:
-      GetFrame().GetDocument()->CancelIdleCallback(idle_callback_handle_);
+      GetDocument().CancelIdleCallback(idle_callback_handle_);
       invoke(deadline);
       break;
     case State::kInactive:
