9e7e99887eb7b335d459c7d863ec3a3058a86d55
Xiaohan Wang <xhwang@chromium.org>
2017-12-21 11:31:25 -0800

    media: Fix MediaDrmStorageImplTest dependency
    It should depend on content/public/test, not content/test.
    
    Note in the BUILD.gn file, the test target still depend on
    "//content/test:test_support" because all the content/public/test/*
    files are included in that target.
    
    BUG=796976
    
    Change-Id: I1fd885301c40bc48ec65e4d19ace8aa0e8388032
    Reviewed-on: https://chromium-review.googlesource.com/840615
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525833}

--- components/cdm/browser/media_drm_storage_impl_unittest.cc
+++ components/cdm/browser/media_drm_storage_impl_unittest.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/unguessable_token.h"
 #include "components/prefs/testing_pref_service.h"
 #include "content/public/test/navigation_simulator.h"
-#include "content/test/test_render_frame_host.h"
+#include "content/public/test/test_renderer_host.h"
 #include "media/mojo/services/mojo_media_drm_storage.h"
 #include "mojo/public/cpp/bindings/interface_request.h"
 #include "mojo/public/cpp/bindings/strong_binding.h"
