728d7cf4f9a0bd350f318fed6db7a75c7051b65a
Xianzhu Wang <wangxianzhu@chromium.org>
2017-12-21 12:06:14 -0800

    [SPv175+] Match object fragment to layer fragment
    When painting an object with multiple fragments, find which fragment we
    are painting and use the paint properties from that fragment. Record the
    current layer fragment in PaintInfo, and match object fragments against
    it by comparing the pagination offset.
    
    Bug: 771643
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Ieb29d97ba19ed538599d0034ebce6565ad21218b
    Reviewed-on: https://chromium-review.googlesource.com/838256
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525863}

--- third_party/WebKit/Source/core/frame/LocalFrameView.cpp
+++ third_party/WebKit/Source/core/frame/LocalFrameView.cpp
@@ -121,7 +121,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/graphics/compositing/PaintArtifactCompositor.h"
 #include "platform/graphics/paint/CullRect.h"
 #include "platform/graphics/paint/PaintController.h"
-#include "platform/graphics/paint/ScopedPaintChunkProperties.h"
 #include "platform/instrumentation/tracing/TraceEvent.h"
 #include "platform/instrumentation/tracing/TracedValue.h"
 #include "platform/json/JSONValues.h"
