2eac7205389f8cbdeeaaa0a6b023f52f4b0ea68b
dpapad <dpapad@chromium.org>
2017-12-21 04:18:43 +0000

    MD Settings: Migrate usage of paper-spinner to paper-spinner-lite.
    Command used to find usages:
    ack-grep "paper-spinner" | ack-grep -v "paper-spinner-"
    
    This is in preparation of removing <paper-spinner> from
    third_party/polymer/ folder.
    
    Bug: 796661
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ie7c997690b8fad37e4f965e0774a5e0d459d0f1d
    Reviewed-on: https://chromium-review.googlesource.com/837900
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525600}

--- chrome/browser/resources/settings/search_settings.js
+++ chrome/browser/resources/settings/search_settings.js
@@ -52,7 +52,7 @@ cr.define('settings', function() {
     'PAPER-ICON-BUTTON',
     'PAPER-RIPPLE',
     'PAPER-SLIDER',
-    'PAPER-SPINNER',
+    'PAPER-SPINNER-LITE',
     'STYLE',
     'TEMPLATE',
   ]);
