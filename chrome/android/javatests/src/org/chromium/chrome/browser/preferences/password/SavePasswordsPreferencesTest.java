cd27f45832bf545cf70a214cfef2cf725d30a54f
Ted Choc <tedchoc@google.com>
2017-12-21 08:45:37 -0800

    Disable SavePasswordsPreferencesTest testExportMenuItem, testExportMenuItemNoLock
    BUG=796939
    TBR=vabr@chromium.org
    
    Change-Id: Ifaa8e3e64edc0329fa055256b451e2f974b915f9
    Reviewed-on: https://chromium-review.googlesource.com/840081
    Commit-Queue: Ted Choc (OOO til 2018) <tedchoc@chromium.org>
    Reviewed-by: Ted Choc (OOO til 2018) <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525739}

--- chrome/android/javatests/src/org/chromium/chrome/browser/preferences/password/SavePasswordsPreferencesTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/preferences/password/SavePasswordsPreferencesTest.java
@@ -31,6 +31,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.BaseJUnit4ClassRunner;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.PasswordManagerHandler;
@@ -356,6 +357,7 @@ public class SavePasswordsPreferencesTest {
     @SmallTest
     @Feature({"Preferences"})
     @EnableFeatures("password-export")
+    @DisabledTest(message = "crbug.com/796939")
     public void testExportMenuItem() throws Exception {
         setPasswordSource(new SavedPasswordEntry("https://example.com", "test user", "password"));
 
@@ -386,6 +388,7 @@ public class SavePasswordsPreferencesTest {
     @SmallTest
     @Feature({"Preferences"})
     @EnableFeatures("password-export")
+    @DisabledTest(message = "crbug.com/796939")
     public void testExportMenuItemNoLock() throws Exception {
         setPasswordSource(new SavedPasswordEntry("https://example.com", "test user", "password"));
 
