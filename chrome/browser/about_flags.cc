1b9fd42c17cf1189d3a934d6f48c96ba8045aeed
Leo Zhang <googleo@chromium.org>
2017-12-21 14:26:17 +1100

    Remove compact translate UI flag from chrome://flags.
    Bug: 727436
    Change-Id: I2ca1864627998febcae1cbd68c5a4308481fd5ad
    Reviewed-on: https://chromium-review.googlesource.com/838800
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Leo Zhang <googleo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526613}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -85,7 +85,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/suggestions/features.h"
 #include "components/sync/driver/sync_driver_switches.h"
 #include "components/tracing/common/tracing_switches.h"
-#include "components/translate/core/browser/translate_infobar_delegate.h"
 #include "components/translate/core/browser/translate_prefs.h"
 #include "components/translate/core/browser/translate_ranker_impl.h"
 #include "components/version_info/version_info.h"
@@ -1834,9 +1833,6 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kPullToRefreshEffectName,
      flag_descriptions::kPullToRefreshEffectDescription, kOsAndroid,
      SINGLE_DISABLE_VALUE_TYPE(switches::kDisablePullToRefreshEffect)},
-    {"translate-compact-infobar", flag_descriptions::kTranslateCompactUIName,
-     flag_descriptions::kTranslateCompactUIDescription, kOsAndroid,
-     FEATURE_VALUE_TYPE(translate::kTranslateCompactUI)},
 #endif  // OS_ANDROID
 #if defined(OS_MACOSX)
     {"enable-translate-new-ux", flag_descriptions::kTranslateNewUxName,
