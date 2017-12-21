37b7c0466774b3a412e2e0a3e2203269737a8c35
Parastoo Geranmayeh <parastoog@google.com>
2017-12-21 17:47:49 -0500

    [Autofill] Avoid Crash on Editor Dialog.
    Right now, if we sign in and click on a previously added address
    and then click back instantly, we may have a crash.
    
    The crash could be for two reasons:
    
    1. An async task (subkey request) is calling the EditorDialog.show(),
    while the activity is finishing.
    
    2. The EditorDialog.onShow() is called, while the dialog is already
    dismissed.
    
    This fix, checks for these conditions before continuing.
    
    Bug: 786962, 794892
    Change-Id: I973860fece013c33759c95e178a667d63020ae68
    Reviewed-on: https://chromium-review.googlesource.com/841263
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Parastoo Geranmayeh <parastoog@google.com>
    Cr-Commit-Position: refs/heads/master@{#525995}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/ui/EditorDialog.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/ui/EditorDialog.java
@@ -497,6 +497,9 @@ public class EditorDialog
      * @param editorModel The description of the editor user interface to display.
      */
     public void show(EditorModel editorModel) {
+        // If an asynchronous task calls show, while the activity is already finishing, return.
+        if (((Activity) mContext).isFinishing()) return;
+
         setOnShowListener(this);
         setOnDismissListener(this);
         mEditorModel = editorModel;
@@ -521,7 +524,7 @@ public class EditorDialog
 
     @Override
     public void onShow(DialogInterface dialog) {
-        if (mDialogInOutAnimator != null) return;
+        if (mDialogInOutAnimator != null && mIsDismissed) return;
 
         // Hide keyboard and disable EditText views for animation efficiency.
         if (getCurrentFocus() != null) UiUtils.hideKeyboard(getCurrentFocus());
