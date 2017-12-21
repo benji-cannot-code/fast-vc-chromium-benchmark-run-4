97e8811fc83ad6229789bbc042280f0aea159907
Michał Pichliński <mpichlinski@opera.com>
2017-12-21 16:54:09 +0100

    Allow delegate to handle ContentMainRunner fatal initialization error.
    Report ContentMainRunner fatal error during initialization to the delegate,
    which may gracefuly exit with some message instead crashing.
    Default behavior is CHECK(false).
    
    Bug: 792839
    Change-Id: I1e7c85c58f1a008e8818e2efb00dbba7f4774826
    Reviewed-on: https://chromium-review.googlesource.com/815054
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Ken Rockot <rockot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527056}

--- content/public/app/content_main_delegate.cc
+++ content/public/app/content_main_delegate.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/public/app/content_main_delegate.h"
 
+#include "base/logging.h"
 #include "build/build_config.h"
 #include "content/public/gpu/content_gpu_client.h"
 #include "content/public/renderer/content_renderer_client.h"
@@ -48,6 +49,11 @@ void ContentMainDelegate::ZygoteStarting(
     std::vector<std::unique_ptr<ZygoteForkDelegate>>* delegates) {}
 
 #endif  // defined(OS_LINUX)
+
+int ContentMainDelegate::TerminateForFatalInitializationError() {
+  CHECK(false);
+  return 0;
+}
 
 bool ContentMainDelegate::ShouldEnableProfilerRecording() {
   return false;
