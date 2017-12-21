b1a96b404dbbff2b5e639829806e168286faff69
Xiaohan Wang <xhwang@chromium.org>
2017-12-21 13:30:56 -0800

    media: Add CdmProxyTest
    - Add EncryptedMediaTestExperimentalCdmInterface to support testing
      ClearKeyCdm using experimental CDM interface.
    - Add CdmProxyTest to support testing CdmProxy in ClearKeyCdm.
    
    Currently the test only covers a failure case (mojo connection failed).
    In the next CL, more test steps will be added.
    
    BUG=796345
    
    Change-Id: Ie80fc33a1a211f94c02c82f27c81374740fad5db
    Reviewed-on: https://chromium-review.googlesource.com/840787
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525900}

--- media/cdm/ppapi/clear_key_cdm/clear_key_cdm.cc
+++ media/cdm/ppapi/clear_key_cdm/clear_key_cdm.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/cdm/ppapi/cdm_file_io_test.h"
 #include "media/cdm/ppapi/clear_key_cdm/cdm_host_proxy.h"
 #include "media/cdm/ppapi/clear_key_cdm/cdm_host_proxy_impl.h"
+#include "media/cdm/ppapi/clear_key_cdm/cdm_proxy_test.h"
 #include "media/cdm/ppapi/clear_key_cdm/cdm_video_decoder.h"
 #include "media/media_features.h"
 
@@ -73,6 +74,8 @@ const char kExternalClearKeyStorageIdTestKeySystem[] =
     "org.chromium.externalclearkey.storageidtest";
 const char kExternalClearKeyDifferentGuidTestKeySystem[] =
     "org.chromium.externalclearkey.differentguid";
+const char kExternalClearKeyCdmProxyTestKeySystem[] =
+    "org.chromium.externalclearkey.cdmproxytest";
 
 const int64_t kSecondsPerMinute = 60;
 const int64_t kMsPerSecond = 1000;
@@ -262,7 +265,8 @@ void* CreateCdmInstance(int cdm_interface_version,
       key_system_string != kExternalClearKeyCrashKeySystem &&
       key_system_string != kExternalClearKeyVerifyCdmHostTestKeySystem &&
       key_system_string != kExternalClearKeyStorageIdTestKeySystem &&
-      key_system_string != kExternalClearKeyDifferentGuidTestKeySystem) {
+      key_system_string != kExternalClearKeyDifferentGuidTestKeySystem &&
+      key_system_string != kExternalClearKeyCdmProxyTestKeySystem) {
     DVLOG(1) << "Unsupported key system:" << key_system_string;
     return nullptr;
   }
@@ -433,6 +437,8 @@ void ClearKeyCdm::CreateSessionAndGenerateRequest(
     VerifyCdmHostTest();
   } else if (key_system_ == kExternalClearKeyStorageIdTestKeySystem) {
     StartStorageIdTest();
+  } else if (key_system_ == kExternalClearKeyCdmProxyTestKeySystem) {
+    StartCdmProxyTest();
   }
 }
 
@@ -958,6 +964,23 @@ void ClearKeyCdm::StartStorageIdTest() {
 
   // Request the latest available version.
   cdm_host_proxy_->RequestStorageId(0);
+}
+
+void ClearKeyCdm::StartCdmProxyTest() {
+  DVLOG(1) << __func__;
+  DCHECK(!cdm_proxy_test_);
+
+  cdm_proxy_test_.reset(new CdmProxyTest(cdm_host_proxy_.get()));
+  cdm_proxy_test_->Run(base::BindOnce(&ClearKeyCdm::OnCdmProxyTestComplete,
+                                      base::Unretained(this)));
+}
+
+void ClearKeyCdm::OnCdmProxyTestComplete(bool success) {
+  DVLOG(1) << __func__;
+  DCHECK(cdm_proxy_test_);
+
+  cdm_proxy_test_.reset();
+  OnUnitTestComplete(success);
 }
 
 }  // namespace media
--- media/cdm/ppapi/clear_key_cdm/clear_key_cdm.h
+++ media/cdm/ppapi/clear_key_cdm/clear_key_cdm.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace media {
 
 class CdmHostProxy;
+class CdmProxyTest;
 class CdmVideoDecoder;
 class DecoderBuffer;
 class FFmpegCdmAudioDecoder;
@@ -129,14 +130,15 @@ class ClearKeyCdm : public cdm::ContentDecryptionModule_9,
   void OnUnitTestComplete(bool success);
 
   void StartFileIOTest();
-
-  // Callback for CDM File IO test.
   void OnFileIOTestComplete(bool success);
 
   void StartOutputProtectionTest();
   void StartPlatformVerificationTest();
   void VerifyCdmHostTest();
   void StartStorageIdTest();
+
+  void StartCdmProxyTest();
+  void OnCdmProxyTestComplete(bool success);
 
   std::unique_ptr<CdmHostProxy> cdm_host_proxy_;
   scoped_refptr<ContentDecryptionModule> cdm_;
@@ -161,6 +163,7 @@ class ClearKeyCdm : public cdm::ContentDecryptionModule_9,
   std::unique_ptr<CdmVideoDecoder> video_decoder_;
 
   std::unique_ptr<FileIOTestRunner> file_io_test_runner_;
+  std::unique_ptr<CdmProxyTest> cdm_proxy_test_;
 
   bool is_running_output_protection_test_;
   bool is_running_platform_verification_test_;
