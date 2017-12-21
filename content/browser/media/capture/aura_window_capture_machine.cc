2b5a864158b8014ad0db8474c5d6018c5837faf7
Yuri Wiitala <miu@chromium.org>
2017-12-21 04:46:03 +0000

    Add undo capability to CursorRenderer.
    For the upcoming tab capture implementation replacement, it is necessary
    to be able to undo the changes being made to VideoFrames before
    "returning" the frame to its source. This change adds and tests that new
    functionality.
    
    Bug: 754872
    Change-Id: I30aef805644820d2a01de1b8f6e9d1b9da341b80
    Reviewed-on: https://chromium-review.googlesource.com/836147
    Commit-Queue: Yuri Wiitala <miu@chromium.org>
    Reviewed-by: Xiangjun Zhang <xjz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525613}

--- content/browser/media/capture/aura_window_capture_machine.cc
+++ content/browser/media/capture/aura_window_capture_machine.cc
@@ -392,7 +392,7 @@ void AuraWindowCaptureMachine::CopyOutputFinishedForVideo(
   if (machine) {
     if (machine->cursor_renderer_ && result)
       machine->cursor_renderer_->RenderOnVideoFrame(target.get(),
-                                                    region_in_frame);
+                                                    region_in_frame, nullptr);
   } else {
     VLOG(1) << "Aborting capture: AuraWindowCaptureMachine has gone away.";
     result = false;
