f6142e56a35767ada6391cf5c0ea376e4c036308
Takashi Toyoshima <toyoshim@chromium.org>
2017-12-21 20:30:05 +0900

    MidiManager: remove unnecessary midi_scheduler.h includes
    MidiScheduler is not used today in MidiManagerAndroid and
    MidiManagerUsb, but the header file is still included
    mistakenly.
    
    Bug: 
    Change-Id: I99a2c65545d617d5b0a2c9980f322eba3eb3e521
    Reviewed-on: https://chromium-review.googlesource.com/839482
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525959}

--- media/midi/midi_manager_android.h
+++ media/midi/midi_manager_android.h
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "media/midi/midi_input_port_android.h"
 #include "media/midi/midi_manager.h"
-#include "media/midi/midi_scheduler.h"
 
 namespace midi {
 
