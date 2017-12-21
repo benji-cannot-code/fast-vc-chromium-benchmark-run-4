583116819e6d55579dbe2ecc65788cdea4122e6c
John Rummell <jrummell@chromium.org>
2017-12-21 10:07:17 -0800

    media: Keep track of codecs as enums rather than strings
    To ensure that the values for supported video codecs are valid,
    keep track of them as enums.
    
    BUG=764143
    TEST=updated content_unittest passes
    
    Change-Id: Ic984d580aa5967fa64b72cad3684b3c3ee9c5b9e
    Reviewed-on: https://chromium-review.googlesource.com/838192
    Commit-Queue: John Rummell <jrummell@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525789}

--- content/public/common/cdm_info.h
+++ content/public/common/cdm_info.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/version.h"
 #include "content/common/content_export.h"
+#include "media/base/video_codecs.h"
 
 namespace content {
 
@@ -22,7 +23,7 @@ struct CONTENT_EXPORT CdmInfo {
           const base::Version& version,
           const base::FilePath& path,
           const std::string& file_system_id,
-          const std::vector<std::string>& supported_codecs,
+          const std::vector<media::VideoCodec>& supported_video_codecs,
           bool supports_persistent_license,
           const std::string& supported_key_system,
           bool supports_sub_key_systems);
@@ -47,10 +48,12 @@ struct CONTENT_EXPORT CdmInfo {
   // digits(0-9), or "._-".
   std::string file_system_id;
 
-  // List of codecs supported by the CDM (e.g. vp8).
-  // TODO(jrummell): use the enums from media::AudioCodec and media::VideoCodec
-  // instead of strings.
-  std::vector<std::string> supported_codecs;
+  // List of video codecs supported by the CDM (e.g. vp8). This is the set of
+  // codecs that can be decrypted and decoded by the CDM. As this is generic,
+  // not all profiles or levels of the specified codecs may actually be
+  // supported.
+  // TODO(crbug.com/796725) Find a way to include profiles and levels.
+  std::vector<media::VideoCodec> supported_video_codecs;
 
   // Whether this CDM supports persistent licenses.
   bool supports_persistent_license;
