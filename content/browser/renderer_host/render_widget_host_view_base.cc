df85c9e4b547f4377bb870d1be3d9747d5f5a8c2
Ken Buchanan <kenrb@chromium.org>
2017-12-22 09:59:26 -0500

    Browser quad-based hit testing returns confidence level
    This CL adds an output bool to the quad-based hit testing APIs to
    SurfaceHitTest. When the bool is true, it means that there is reduced
    confidence in the hit test result being returned, and the caller should
    invoke InputTargetClient to perform an asynchronous hit test in the
    renderer before routing a given input event.
    
    Bug: 796648
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I5d5f4bf18977a85c5e4d7f4c4291a4ec89bca70f
    Reviewed-on: https://chromium-review.googlesource.com/840644
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Lucas Gadani <lfg@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526003}

--- content/browser/renderer_host/render_widget_host_view_base.cc
+++ content/browser/renderer_host/render_widget_host_view_base.cc
@@ -436,7 +436,8 @@ viz::LocalSurfaceId RenderWidgetHostViewBase::GetLocalSurfaceId() const {
 viz::FrameSinkId RenderWidgetHostViewBase::FrameSinkIdAtPoint(
     viz::SurfaceHittestDelegate* delegate,
     const gfx::PointF& point,
-    gfx::PointF* transformed_point) {
+    gfx::PointF* transformed_point,
+    bool* out_query_renderer) {
   NOTREACHED();
   return viz::FrameSinkId();
 }
