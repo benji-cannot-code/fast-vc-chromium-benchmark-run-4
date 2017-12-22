ac82ca329fc776d9d892443902f337edb1fca7f5
Xiaocheng Hu <xiaochengh@chromium.org>
2017-12-21 16:49:11 -0800

    [LayoutNG] Introduce NGPhysicalTextFragment::IsLineBreak()
    This patch adds the method to allows us to check if an NGPhysicalTextFragment
    is a forced line break or not. It will be used in a follow-up patch
    crrev.com/c/803015
    
    Bug: 636993
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: I051d45c00e8ed47b9e10fed9c7378512f69822cc
    Reviewed-on: https://chromium-review.googlesource.com/841539
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525916}

--- third_party/WebKit/Source/core/layout/ng/inline/ng_physical_text_fragment.cc
+++ third_party/WebKit/Source/core/layout/ng/inline/ng_physical_text_fragment.cc
@@ -59,4 +59,10 @@ NGPhysicalOffsetRect NGPhysicalTextFragment::SelfVisualRect() const {
   return {};
 }
 
+bool NGPhysicalTextFragment::IsLineBreak() const {
+  // TODO(xiaochengh): Introduce and set a text fragment type flag in fragment
+  // builder, instead of check text content string.
+  return Text() == "\n";
+}
+
 }  // namespace blink
