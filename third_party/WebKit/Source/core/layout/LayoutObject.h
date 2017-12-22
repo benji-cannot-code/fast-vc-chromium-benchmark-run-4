a7ee136d0e1a562c95c350a84ddc0906d020db10
Zhuoyu Qian <zhuoyu.qian@samsung.com>
2017-12-22 12:37:08 +0800

    Move PreservesNewline from LayoutObject to LineLayoutItem
    As the comment in LineLayoutItem.h by dgrogan@,
    LineLayoutItem::PreservesNewline() is the only caller, move the logic
    from LayoutObject to LineLayoutItem.
    
    BUG=
    
    Signed-off-by: Zhuoyu Qian <zhuoyu.qian@samsung.com>
    Change-Id: Ib2a008e183b6c0ea9d8615883dcacb1ce99870da
    Reviewed-on: https://chromium-review.googlesource.com/842162
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526135}

--- third_party/WebKit/Source/core/layout/LayoutObject.h
+++ third_party/WebKit/Source/core/layout/LayoutObject.h
@@ -883,8 +883,6 @@ class CORE_EXPORT LayoutObject : public ImageResourceObserver,
 
   bool HasShapeOutside() const { return Style() && Style()->ShapeOutside(); }
 
-  inline bool PreservesNewline() const;
-
   // The pseudo element style can be cached or uncached.  Use the cached method
   // if the pseudo element doesn't respect any pseudo classes (and therefore
   // has no concept of changing state).
@@ -2717,13 +2715,6 @@ inline void LayoutObject::SetNeedsPositionedMovementLayout() {
 #endif
   if (!already_needed_layout)
     MarkContainerChainForLayout();
-}
-
-inline bool LayoutObject::PreservesNewline() const {
-  if (IsSVGInlineText())
-    return false;
-
-  return Style()->PreserveNewline();
 }
 
 inline void LayoutObject::SetHasBoxDecorationBackground(bool b) {
