b1c2e604d2b31700127d864498dd96dd219e32dd
Daniel Bratell <bratell@opera.com>
2017-12-22 11:43:14 +0100

    Add missing include guard to test_overscroll_delegate.h
    Change-Id: I3a9785519bfe9d6b10972bbe7b70a3c9c3f5fdbc
    Reviewed-on: https://chromium-review.googlesource.com/842803
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Commit-Queue: Daniel Bratell <bratell@opera.com>
    Cr-Commit-Position: refs/heads/master@{#526029}

--- content/test/test_overscroll_delegate.h
+++ content/test/test_overscroll_delegate.h
@@ -3,6 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifndef CONTENT_TEST_TEST_OVERSCROLL_DELEGATE_H_
+#define CONTENT_TEST_TEST_OVERSCROLL_DELEGATE_H_
+
 #include <vector>
 
 #include "base/optional.h"
@@ -53,3 +56,5 @@ class TestOverscrollDelegate : public OverscrollControllerDelegate {
 };
 
 }  // namespace content
+
+#endif  // CONTENT_TEST_TEST_OVERSCROLL_DELEGATE_H_
