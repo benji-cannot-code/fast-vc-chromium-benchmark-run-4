341e47f39a1775dda907f9f5ce892263bf07ebf1
Dominic Mazzoni <dmazzoni@chromium.org>
2017-12-22 14:03:43 -0800

    Fix crash in PdfAccessibilityTree.
    PdfAccessibilityTree::CreateNode calls
    content::RenderAccessibility::GenerateAXID, which ends up calling
    BlinkAXTreeSource::ComputeRoot(), which accesses the WebFrame and a
    WebDocument. If that fails, it returns an empty/uninitialized
    WebAXObject as the root, which returns -1 from GenerateAXID.
    A -1 in the accessibility tree isn't necessarily bad, but reusing the
    same ID for multiple nodes will quickly lead to a CHECK failure.
    
    Fix it by checking that GenerateAXID is returning valid values
    in each plugin callback.
    
    Bug: 770886
    Change-Id: Ibe641d06302253a2e242d59fd723a626b6b6c0cd
    Reviewed-on: https://chromium-review.googlesource.com/840883
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526092}

--- content/public/renderer/render_frame.h
+++ content/public/renderer/render_frame.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/platform/TaskType.h"
 #include "third_party/WebKit/public/web/WebNavigationPolicy.h"
 #include "third_party/WebKit/public/web/WebTriggeringEventInfo.h"
+#include "ui/accessibility/ax_modes.h"
 
 namespace blink {
 class AssociatedInterfaceProvider;
@@ -263,6 +264,9 @@ class CONTENT_EXPORT RenderFrame : public IPC::Listener,
   // Returns a default ChildURLLoaderFactoryGetter for the RenderFrame.
   // Used to obtain a right mojom::URLLoaderFactory.
   virtual ChildURLLoaderFactoryGetter* GetDefaultURLLoaderFactoryGetter() = 0;
+
+  // Set the accessibility mode to force creation of RenderAccessibility.
+  virtual void SetAccessibilityModeForTest(ui::AXMode new_mode) = 0;
 
  protected:
   ~RenderFrame() override {}
