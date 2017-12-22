db1b4b3e0bef3a7f557be2c17c234278258fb278
Ian Vollick <vollick@chromium.org>
2017-12-22 13:43:44 -0500

    Enable Layer Squashing Prevention
    With this CL, a layer may preclude squashing by subsequent layers
    in paint order. NB: it makes no attempt to handle cases where
    layers change sort order due to relative position in a 3D rendering
    context.
    
    I plan to add the test separately. It's in progress here:
    crrev.com/c/842946
    
    Bug: 788807
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I817a7ddc6f446b7d65dde2c4b4d88a6ac8912549
    Reviewed-on: https://chromium-review.googlesource.com/832668
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526046}

--- third_party/WebKit/Source/core/paint/compositing/CompositingLayerAssigner.cpp
+++ third_party/WebKit/Source/core/paint/compositing/CompositingLayerAssigner.cpp
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/paint/compositing/CompositingLayerAssigner.h"
 
+#include "core/html/HTMLFrameOwnerElement.h"
 #include "core/inspector/InspectorTraceEvents.h"
 #include "core/layout/LayoutView.h"
 #include "core/page/Page.h"
@@ -100,6 +101,26 @@ bool CompositingLayerAssigner::NeedsOwnBacking(const PaintLayer* layer) const {
          (compositor_->StaleInCompositingMode() && layer->IsRootLayer());
 }
 
+bool CompositingLayerAssigner::PreventsSquashing(
+    const PaintLayer* layer) const {
+  // TODO(crbug.com/788807): This is a temporary heuristic to avoid hit testing
+  // bugs with remote frames. Remove for M65.
+  if (!RuntimeEnabledFeatures::PreventLayerSquashingEnabled())
+    return false;
+
+  auto& layout_object = layer->GetLayoutObject();
+  if (!layout_object.IsLayoutEmbeddedContent())
+    return false;
+
+  auto* node = layout_object.GetNode();
+  if (!node || !node->IsHTMLElement() || !node->IsFrameOwnerElement())
+    return false;
+
+  HTMLFrameOwnerElement* element =
+      ToHTMLFrameOwnerElement(layout_object.GetNode());
+  return element->ContentFrame() && element->ContentFrame()->IsRemoteFrame();
+}
+
 CompositingStateTransitionType
 CompositingLayerAssigner::ComputeCompositedLayerUpdate(PaintLayer* layer) {
   CompositingStateTransitionType update = kNoCompositingStateChange;
@@ -129,6 +150,9 @@ CompositingLayerAssigner::GetReasonsPreventingSquashing(
     const CompositingLayerAssigner::SquashingState& squashing_state) {
   if (!squashing_state.have_assigned_backings_to_entire_squashing_layer_subtree)
     return SquashingDisallowedReason::kWouldBreakPaintOrder;
+
+  if (squashing_state.preceding_layer_prevents_squashing)
+    return SquashingDisallowedReason::kPrecedingLayerPrecludesSquashing;
 
   DCHECK(squashing_state.has_most_recent_mapping);
   const PaintLayer& squashing_layer =
@@ -287,6 +311,9 @@ void CompositingLayerAssigner::AssignLayersToBackingsInternal(
       layer->SetSquashingDisallowedReasons(reasons_preventing_squashing);
     }
   }
+
+  if (PreventsSquashing(layer))
+    squashing_state.preceding_layer_prevents_squashing = true;
 
   CompositingStateTransitionType composited_layer_update =
       ComputeCompositedLayerUpdate(layer);
