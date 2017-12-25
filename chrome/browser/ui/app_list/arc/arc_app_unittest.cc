40102a9ecec8c71ce0a76793a38ef77f7c0615a3
Sergey Poromov <poromov@chromium.org>
2017-12-25 23:21:42 +0100

    Remove Play Store app from Public Session and Kiosk modes.
    While Play Store is available in this modes, it shouldn't be exposed
    in the UI items, such as shelf and app launcher.
    In order to do it, the app is not added in
    ArcAppListPrefs::AddAppAndShortcut()
    
    TEST=Set policies, start public session.
    
    Bug: 731097
    Change-Id: I46eed5328085524b6fb8e54dfd838cebbd9a0f0a
    Reviewed-on: https://chromium-review.googlesource.com/834708
    Commit-Queue: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526181}

--- chrome/browser/ui/app_list/arc/arc_app_unittest.cc
+++ chrome/browser/ui/app_list/arc/arc_app_unittest.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/arc/arc_support_host.h"
 #include "chrome/browser/chromeos/arc/arc_util.h"
 #include "chrome/browser/chromeos/arc/voice_interaction/arc_voice_interaction_arc_home_service.h"
+#include "chrome/browser/chromeos/login/users/fake_chrome_user_manager.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/extension_service_test_base.h"
 #include "chrome/browser/policy/profile_policy_connector.h"
@@ -148,6 +149,8 @@ enum class ArcState {
   ARC_PERSISTENT_PLAY_STORE_MANAGED_AND_DISABLED,
   // ARC is persistent but without Play Store UI support.
   ARC_PERSISTENT_WITHOUT_PLAY_STORE,
+  // ARC is persistent, Play Store is managed, enabled, but hidden.
+  ARC_PERSISTENT_MANAGED_ENABLED_AND_PLAY_STORE_HIDDEN,
 };
 
 constexpr ArcState kManagedArcStates[] = {
@@ -155,6 +158,7 @@ constexpr ArcState kManagedArcStates[] = {
     ArcState::ARC_PLAY_STORE_MANAGED_AND_DISABLED,
     ArcState::ARC_PERSISTENT_PLAY_STORE_MANAGED_AND_ENABLED,
     ArcState::ARC_PERSISTENT_PLAY_STORE_MANAGED_AND_DISABLED,
+    ArcState::ARC_PERSISTENT_MANAGED_ENABLED_AND_PLAY_STORE_HIDDEN,
 };
 
 constexpr ArcState kUnmanagedArcStates[] = {
@@ -188,6 +192,7 @@ class ArcAppModelBuilderTest : public extensions::ExtensionServiceTestBase,
       case ArcState::ARC_PERSISTENT_PLAY_STORE_UNMANAGED:
       case ArcState::ARC_PERSISTENT_PLAY_STORE_MANAGED_AND_ENABLED:
       case ArcState::ARC_PERSISTENT_PLAY_STORE_MANAGED_AND_DISABLED:
+      case ArcState::ARC_PERSISTENT_MANAGED_ENABLED_AND_PLAY_STORE_HIDDEN:
         arc::SetArcAlwaysStartForTesting(true);
         break;
       case ArcState::ARC_PERSISTENT_WITHOUT_PLAY_STORE:
@@ -586,6 +591,7 @@ class ArcDefaulAppForManagedUserTest : public ArcPlayStoreAppTest {
     switch (GetParam()) {
       case ArcState::ARC_PLAY_STORE_MANAGED_AND_ENABLED:
       case ArcState::ARC_PERSISTENT_PLAY_STORE_MANAGED_AND_ENABLED:
+      case ArcState::ARC_PERSISTENT_MANAGED_ENABLED_AND_PLAY_STORE_HIDDEN:
         return true;
       case ArcState::ARC_PLAY_STORE_MANAGED_AND_DISABLED:
       case ArcState::ARC_PERSISTENT_PLAY_STORE_MANAGED_AND_DISABLED:
@@ -599,6 +605,13 @@ class ArcDefaulAppForManagedUserTest : public ArcPlayStoreAppTest {
 
   // ArcPlayStoreAppTest:
   void OnBeforeArcTestSetup() override {
+    if (GetParam() ==
+        ArcState::ARC_PERSISTENT_MANAGED_ENABLED_AND_PLAY_STORE_HIDDEN) {
+      const AccountId account_id(
+          AccountId::FromUserEmail(profile_->GetProfileUserName()));
+      arc_test()->GetUserManager()->AddPublicAccountUser(account_id);
+      arc_test()->GetUserManager()->LoginUser(account_id);
+    }
     policy::ProfilePolicyConnector* const connector =
         policy::ProfilePolicyConnectorFactory::GetForBrowserContext(profile());
     connector->OverrideIsManagedForTesting(true);
@@ -2071,7 +2084,9 @@ TEST_P(ArcDefaulAppForManagedUserTest, DefaultAppsForManagedUser) {
   // PlayStor exists for managed and enabled state.
   std::unique_ptr<ArcAppListPrefs::AppInfo> app_info =
       prefs->GetApp(arc::kPlayStoreAppId);
-  if (IsEnabledByPolicy()) {
+  if (IsEnabledByPolicy() &&
+      GetParam() !=
+          ArcState::ARC_PERSISTENT_MANAGED_ENABLED_AND_PLAY_STORE_HIDDEN) {
     ASSERT_TRUE(app_info);
     EXPECT_FALSE(app_info->ready);
   } else {
