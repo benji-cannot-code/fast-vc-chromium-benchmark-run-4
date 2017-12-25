955061c9d4b1551e5e4a2453cfd5c73acf3e8118
Shu Chen <shuchen@google.com>
2017-12-25 10:52:42 +0800

    Deprecates the merge-key-char-events flag.
    Bug: 797550
    Change-Id: I3f47e5b7b86627dd3a356458e2ee1c4910121b6a
    Reviewed-on: https://chromium-review.googlesource.com/844036
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Shu Chen <shuchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526365}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1392,9 +1392,6 @@ extern const char kEnableDesktopIosPromotionsDescription[];
 extern const char kGdiTextPrinting[];
 extern const char kGdiTextPrintingDescription[];
 
-extern const char kMergeKeyCharEventsName[];
-extern const char kMergeKeyCharEventsDescription[];
-
 extern const char kTraceExportEventsToEtwName[];
 extern const char kTraceExportEventsToEtwDesription[];
 
