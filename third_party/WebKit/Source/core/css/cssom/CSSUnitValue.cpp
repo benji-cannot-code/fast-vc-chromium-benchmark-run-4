10c470fa618fbef4d4d6d55e948b2c483941e475
Hwanseung Lee <hs1217.lee@samsung.com>
2017-12-26 23:16:29 +0900

    [css-typed-om] CSSUnitValue should not have type attribute
    type attribute was removed in CSSUnitValue.idl
    
    https://drafts.css-houdini.org/css-typed-om-1/#cssunitvalue
    https://github.com/w3c/css-houdini-drafts/pull/497
    
    Bug: 797668
    Change-Id: Ic29193979f3336d735f6484ba199c652753e7ca0
    Reviewed-on: https://chromium-review.googlesource.com/844305
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Hwanseung Lee <hs1217.lee@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#526203}

--- third_party/WebKit/Source/core/css/cssom/CSSUnitValue.cpp
+++ third_party/WebKit/Source/core/css/cssom/CSSUnitValue.cpp
@@ -79,10 +79,6 @@ String CSSUnitValue::unit() const {
   return CSSPrimitiveValue::UnitTypeToString(unit_);
 }
 
-String CSSUnitValue::type() const {
-  return StyleValueTypeToString(GetType());
-}
-
 CSSStyleValue::StyleValueType CSSUnitValue::GetType() const {
   if (unit_ == CSSPrimitiveValue::UnitType::kNumber)
     return StyleValueType::kNumberType;
