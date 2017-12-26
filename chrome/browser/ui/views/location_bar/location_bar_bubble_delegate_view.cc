96195c163e0bd205a4a7ff79c85170950ebc1880
Gajendra N <gajendra.n@samsung.com>
2017-12-26 16:51:27 +0530

    Remove an overridden constructor in LocationBarBubbleDelegateView
    This CL does a minor cleanup by removing an overridden constructor
    which isn't required.
    
    BUG=None
    R=msw@chromium.org
    
    Change-Id: I3855f5efa83532c53e866c1bb68bcb25b38f5655
    Reviewed-on: https://chromium-review.googlesource.com/844304
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Commit-Queue: srirama chandra sekhar <srirama.m@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#526628}

--- chrome/browser/ui/views/location_bar/location_bar_bubble_delegate_view.cc
+++ chrome/browser/ui/views/location_bar/location_bar_bubble_delegate_view.cc
@@ -75,11 +75,6 @@ LocationBarBubbleDelegateView::LocationBarBubbleDelegateView(
       GetLayoutConstant(LOCATION_BAR_BUBBLE_ANCHOR_VERTICAL_INSET), 0));
 }
 
-LocationBarBubbleDelegateView::LocationBarBubbleDelegateView(
-    views::View* anchor_view,
-    content::WebContents* web_contents)
-    : LocationBarBubbleDelegateView(anchor_view, gfx::Point(), web_contents) {}
-
 LocationBarBubbleDelegateView::~LocationBarBubbleDelegateView() {}
 
 void LocationBarBubbleDelegateView::ShowForReason(DisplayReason reason) {
