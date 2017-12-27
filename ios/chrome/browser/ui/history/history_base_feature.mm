b3d04f2cb2d82d12bf110379d551fc5bc806044c
stkhapugin@chromium.org <stkhapugin@chromium.org>
2017-12-27 16:35:21 +0100

    [ObjC ARC] Converts ios/chrome/browser/ui/history:history_base_feature to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Ieb7c37d3468cd2641c1efc32a6332c6b3f21a0e3
    Reviewed-on: https://chromium-review.googlesource.com/844698
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526290}

--- ios/chrome/browser/ui/history/history_base_feature.mm
+++ ios/chrome/browser/ui/history/history_base_feature.mm
@@ -5,5 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/history/history_base_feature.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 const base::Feature kHistoryBatchUpdatesFilter{
     "HistoryBatchUpdatesFilter", base::FEATURE_ENABLED_BY_DEFAULT};
