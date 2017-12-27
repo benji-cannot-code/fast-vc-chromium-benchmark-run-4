95a0b6b3d08eb20d9cf759eafab0f0558d563db4
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2017-12-27 15:20:49 -0800

    Import wpt@b30e7bfdd5b4f0eab0d76ea8d138475274eed035
    Using wpt-import in Chromium b66ff967c10f72e5a2cc46aa2479cf4c77bc9f97.
    With Chromium commits locally applied on WPT:
    79c28bd2bb "Suppress leak for request-keepalive-quota.html"
    
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/8737
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    kojii@chromium.org:
      external/wpt/css/css-writing-modes
    mstensho@chromium.org:
      external/wpt/css/css-multicol
    rego@igalia.com:
      external/wpt/css/css-grid
    
    TBR=raphael.kubo.da.costa@intel.com
    
    No-Export: true
    Change-Id: Ie4eeb41ea2b509be4594c59962bba3ca8c4e2d22
    Reviewed-on: https://chromium-review.googlesource.com/844856
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526251}

--- third_party/WebKit/LayoutTests/external/wpt/websockets/constants.js
+++ third_party/WebKit/LayoutTests/external/wpt/websockets/constants.js
@@ -2,8 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //This file requires server-side substitutions and must be included as constants.js?pipe=sub
 
 var PORT = "{{ports[ws][0]}}";
-//FIXME: Add support for wss
-var PORT_SSL = "{{ports[ws][0]}}";
+var PORT_SSL = "{{ports[wss][0]}}";
 
 var SCHEME_DOMAIN_PORT;
 if (location.search == '?wss') {
