899f51f184085d3b6e7fb628f131a6a7c4dc1ae7
Emily Stark <estark@google.com>
2017-12-27 07:22:46 -0800

    Move HTTP-Bad to Feature API and implement new options
    This CL moves the HTTP-Bad from outdated field trial groups to the Feature API
    with parameters to control the UI treatment. It adds three new UI treatments for
    nonsecure  pages:
    
    - mark all nonsecure pages as Not Secure
    - mark all nonsecure pages as Not Secure and treat them as actively dangerous when
      a form field is edited
    - mark all nonsecure pages as Not Secure and treat them as actively dangerous for
      passwords and credit card fields
    
    This doesn't implement any DevTools or Page Info changes for these configurations.
    
    Bug: 797437,740395
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I646502a05967b63d3c34015d0d597ce5382248f9
    Reviewed-on: https://chromium-review.googlesource.com/843456
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Eric Lawrence <elawrence@chromium.org>
    Commit-Queue: Emily Stark <estark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526857}

--- chrome/browser/ssl/security_state_tab_helper_unittest.cc
+++ chrome/browser/ssl/security_state_tab_helper_unittest.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/histogram_tester.h"
 #include "chrome/test/base/chrome_render_view_host_test_harness.h"
 #include "components/security_state/content/ssl_status_input_event_data.h"
-#include "components/security_state/core/switches.h"
 #include "content/public/browser/navigation_entry.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
--- components/security_state/content/content_utils_unittest.cc
+++ components/security_state/content/content_utils_unittest.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/histogram_tester.h"
 #include "components/security_state/core/security_state.h"
-#include "components/security_state/core/switches.h"
 #include "components/strings/grit/components_strings.h"
 #include "content/public/browser/security_style_explanation.h"
 #include "content/public/browser/security_style_explanations.h"
