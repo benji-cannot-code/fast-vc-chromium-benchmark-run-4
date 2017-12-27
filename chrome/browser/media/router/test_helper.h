20830523af61d026f6f6e9fdc1ab427f51f234be
Derek Cheng <imcheng@chromium.org>
2017-12-27 11:01:36 -0800

    [Media Router] DualMediaSinkService Singleton.
    - Dial/CastMediaSinkService are now singletons. Previously they depended
    on the BrowserContext for its URLRequestContextGetter. This patch
    changes it so that the system URLRequestContextGetter from
    g_browser_process is used. Note that they are technically singletons
    by virtue of being owned by DualMediaSinkService; no API currently
    exists to access the singleton Dial/CastMediaSinkService instance.
    - Simplified thread hopping for the callbacks passed into the public
    Start API.
    - Wrap Dial/CastMediaSinkService in a DualMediaSinkService. It also
    encapsulates the management of the 2 services and in-browser cast
    discovery feature logic, which used to be in MediaRouterDesktop. Note
    DualMediaSinkService itself is also a singleton. The class is located
    in c/b/m/r/providers/cast. (Further movings TBD in future patches)
    - This pattern will be helpful when we move DualMediaSinkService into
    a in-browser Cast MRP implementation.
    - DualMediaSinkService has 2 public APIs, both exist solely for
    providing extension with in-browser discovered sinks:
    -- current_sinks() - replaces ForceSinkDiscoveryCallback() for syncing
    sinks to the extension.
    -- AddSinksDiscoveredCallback() - get notified for sink updates
    
    BUG=698940
    
    Change-Id: I3e754305c17ef2937af1eca30ba70a8baa783a3b
    Reviewed-on: https://chromium-review.googlesource.com/821490
    Commit-Queue: Derek Cheng <imcheng@chromium.org>
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526348}

--- chrome/browser/media/router/test_helper.h
+++ chrome/browser/media/router/test_helper.h
@@ -13,6 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/macros.h"
+#include "build/build_config.h"
+#include "chrome/browser/media/router/discovery/dial/dial_media_sink_service.h"
+#include "chrome/browser/media/router/discovery/mdns/cast_media_sink_service.h"
 #include "chrome/browser/media/router/issue_manager.h"
 #include "chrome/browser/media/router/issues_observer.h"
 #include "chrome/browser/media/router/media_routes_observer.h"
@@ -103,6 +106,31 @@ class MockPresentationConnectionProxy
                void(content::PresentationConnectionState state));
   MOCK_METHOD0(RequestClose, void());
 };
+
+#if !defined(OS_ANDROID)
+class MockDialMediaSinkService : public DialMediaSinkService {
+ public:
+  explicit MockDialMediaSinkService(
+      const scoped_refptr<net::URLRequestContextGetter>& request_context);
+  ~MockDialMediaSinkService() override;
+
+  MOCK_METHOD2(Start,
+               void(const OnSinksDiscoveredCallback&,
+                    const OnDialSinkAddedCallback&));
+  MOCK_METHOD0(OnUserGesture, void());
+};
+
+class MockCastMediaSinkService : public CastMediaSinkService {
+ public:
+  explicit MockCastMediaSinkService(
+      const scoped_refptr<net::URLRequestContextGetter>& request_context);
+  ~MockCastMediaSinkService() override;
+
+  MOCK_METHOD1(Start, void(const OnSinksDiscoveredCallback&));
+  MOCK_METHOD0(OnUserGesture, void());
+  MOCK_METHOD0(StartMdnsDiscovery, void());
+};
+#endif  // !defined(OS_ANDROID)
 
 }  // namespace media_router
 
--- chrome/common/media_router/discovery/media_sink_service_base.cc
+++ chrome/common/media_router/discovery/media_sink_service_base.cc
@@ -37,7 +37,11 @@ void MediaSinkServiceBase::OnDiscoveryComplete() {
     return;
   }
 
-  ForceSinkDiscoveryCallback();
+  DVLOG(2) << "Send sinks to media router, [size]: " << current_sinks_.size();
+  on_sinks_discovered_cb_.Run(std::vector<MediaSinkInternal>(
+      current_sinks_.begin(), current_sinks_.end()));
+  mrp_sinks_ = current_sinks_;
+  discovery_timer_->Stop();
   RecordDeviceCounts();
 }
 
@@ -54,15 +58,6 @@ void MediaSinkServiceBase::RestartTimer() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   if (!discovery_timer_->IsRunning())
     DoStart();
-}
-
-void MediaSinkServiceBase::ForceSinkDiscoveryCallback() {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  DVLOG(2) << "Send sinks to media router, [size]: " << current_sinks_.size();
-  on_sinks_discovered_cb_.Run(std::vector<MediaSinkInternal>(
-      current_sinks_.begin(), current_sinks_.end()));
-  mrp_sinks_ = current_sinks_;
-  discovery_timer_->Stop();
 }
 
 void MediaSinkServiceBase::DoStart() {
