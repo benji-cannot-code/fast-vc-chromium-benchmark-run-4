34c4781df56a1e5f82f3a48cb5bfc189fcf430e7
Koji Ishii <kojii@chromium.org>
2017-12-28 03:06:53 +0900

    [LayoutNG] Fix 'text-indent'
    This patch fixes the 'text-indent' property in a few ways:
    1. Worked around crbug.com/734554 which creates anonymous block
       when an inline formatting context has leading floats or
       out-of-flow objects.
    2. NGConstraintSpace::ParentPercentResolutionInlineSize() was
       broken and no longer set at some point. Along with the fix,
       made it non-optional.
    
    Bug: 636993, 734554
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: I9a67318564e31474bf1083e99ae0ba00baf45b8f
    Reviewed-on: https://chromium-review.googlesource.com/844410
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526263}

--- third_party/WebKit/Source/core/layout/ng/ng_constraint_space.cc
+++ third_party/WebKit/Source/core/layout/ng/ng_constraint_space.cc
@@ -33,7 +33,7 @@ NGConstraintSpace::NGConstraintSpace(
     TextDirection direction,
     NGLogicalSize available_size,
     NGLogicalSize percentage_resolution_size,
-    Optional<LayoutUnit> parent_percentage_resolution_inline_size,
+    LayoutUnit parent_percentage_resolution_inline_size,
     NGPhysicalSize initial_containing_block_size,
     LayoutUnit fragmentainer_block_size,
     LayoutUnit fragmentainer_space_at_bfc_start,
@@ -184,12 +184,9 @@ NGConstraintSpace::PercentageResolutionInlineSizeForParentWritingMode() const {
   return InitialContainingBlockSize().width;
 }
 
-Optional<LayoutUnit> NGConstraintSpace::ParentPercentageResolutionInlineSize()
-    const {
-  if (!parent_percentage_resolution_inline_size_.has_value())
-    return {};
-  if (*parent_percentage_resolution_inline_size_ != NGSizeIndefinite)
-    return *parent_percentage_resolution_inline_size_;
+LayoutUnit NGConstraintSpace::ParentPercentageResolutionInlineSize() const {
+  if (parent_percentage_resolution_inline_size_ != NGSizeIndefinite)
+    return parent_percentage_resolution_inline_size_;
   return initial_containing_block_size_.ConvertToLogical(GetWritingMode())
       .inline_size;
 }
--- third_party/WebKit/Source/core/layout/ng/ng_constraint_space_builder.cc
+++ third_party/WebKit/Source/core/layout/ng/ng_constraint_space_builder.cc
@@ -12,7 +12,9 @@ namespace blink {
 NGConstraintSpaceBuilder::NGConstraintSpaceBuilder(
     const NGConstraintSpace& parent_space)
     : NGConstraintSpaceBuilder(parent_space.GetWritingMode(),
-                               parent_space.InitialContainingBlockSize()) {}
+                               parent_space.InitialContainingBlockSize()) {
+  parent_percentage_resolution_size_ = parent_space.PercentageResolutionSize();
+}
 
 NGConstraintSpaceBuilder::NGConstraintSpaceBuilder(WritingMode writing_mode,
                                                    NGPhysicalSize icb_size)
@@ -169,13 +171,12 @@ scoped_refptr<NGConstraintSpace> NGConstraintSpaceBuilder::ToConstraintSpace(
 
   NGLogicalSize available_size = available_size_;
   NGLogicalSize percentage_resolution_size = percentage_resolution_size_;
-  Optional<NGLogicalSize> parent_percentage_resolution_size =
-      parent_percentage_resolution_size_;
+  NGLogicalSize parent_percentage_resolution_size =
+      parent_percentage_resolution_size_.value_or(percentage_resolution_size);
   if (!is_in_parallel_flow) {
     available_size.Flip();
     percentage_resolution_size.Flip();
-    if (parent_percentage_resolution_size.has_value())
-      parent_percentage_resolution_size->Flip();
+    parent_percentage_resolution_size.Flip();
   }
 
   // If inline size is indefinite, use size of initial containing block.
@@ -198,12 +199,6 @@ scoped_refptr<NGConstraintSpace> NGConstraintSpaceBuilder::ToConstraintSpace(
           initial_containing_block_size_.height;
     }
   }
-  // parent_percentage_resolution_size is so rarely used that compute indefinite
-  // fallback in NGConstraintSpace when it is used.
-  Optional<LayoutUnit> parent_percentage_resolution_inline_size =
-      parent_percentage_resolution_size.has_value()
-          ? parent_percentage_resolution_size->inline_size
-          : Optional<LayoutUnit>();
 
   DEFINE_STATIC_LOCAL(NGExclusionSpace, empty_exclusion_space, ());
 
@@ -230,7 +225,8 @@ scoped_refptr<NGConstraintSpace> NGConstraintSpaceBuilder::ToConstraintSpace(
     return base::AdoptRef(new NGConstraintSpace(
         static_cast<WritingMode>(out_writing_mode), false,
         static_cast<TextDirection>(text_direction_), available_size,
-        percentage_resolution_size, parent_percentage_resolution_inline_size,
+        percentage_resolution_size,
+        parent_percentage_resolution_size.inline_size,
         initial_containing_block_size_, fragmentainer_block_size_,
         fragmentainer_space_at_bfc_start_, is_fixed_size_inline_,
         is_fixed_size_block_, is_shrink_to_fit_,
@@ -244,9 +240,10 @@ scoped_refptr<NGConstraintSpace> NGConstraintSpaceBuilder::ToConstraintSpace(
   return base::AdoptRef(new NGConstraintSpace(
       out_writing_mode, true, static_cast<TextDirection>(text_direction_),
       available_size, percentage_resolution_size,
-      parent_percentage_resolution_inline_size, initial_containing_block_size_,
-      fragmentainer_block_size_, fragmentainer_space_at_bfc_start_,
-      is_fixed_size_block_, is_fixed_size_inline_, is_shrink_to_fit_,
+      parent_percentage_resolution_size.inline_size,
+      initial_containing_block_size_, fragmentainer_block_size_,
+      fragmentainer_space_at_bfc_start_, is_fixed_size_block_,
+      is_fixed_size_inline_, is_shrink_to_fit_,
       is_block_direction_triggers_scrollbar_,
       is_inline_direction_triggers_scrollbar_,
       static_cast<NGFragmentationType>(fragmentation_type_), is_new_fc_,
