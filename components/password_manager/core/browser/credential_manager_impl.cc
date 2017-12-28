c8bc12aff03028ddde59378a5493ad3ea00dc6e7
jdoerrie <jdoerrie@chromium.org>
2017-12-28 19:31:39 +0100

    Fix Credential Management API Store() for existing Credentials
    This changes fixes the Credential Management API to correctly handle
    storing of already existing credentials. In the previous version
    `preferred_match()` was updated, which is not necessarily the credential
    selected by the user.
    
    Bug: 795878
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I269f465861f44cdd784f0ce077e755191d3bd7bd
    Reviewed-on: https://chromium-review.googlesource.com/843022
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Maxim Kolosovskiy <kolos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526313}

--- components/password_manager/core/browser/credential_manager_impl.cc
+++ components/password_manager/core/browser/credential_manager_impl.cc
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/credential_manager_impl.h"
 
 #include <memory>
+#include <string>
+#include <utility>
+#include <vector>
 
 #include "base/metrics/user_metrics.h"
 #include "components/password_manager/core/browser/credential_manager_logger.h"
@@ -242,7 +245,11 @@ void CredentialManagerImpl::OnProvisionalSaveComplete() {
     // 'skip_zero_click' state, as we've gotten an explicit signal that the page
     // understands the credential management API and so can be trusted to notify
     // us when they sign the user out.
-    form_manager_->Update(*form_manager_->preferred_match());
+    auto best_match = form_manager_->best_matches().find(form.username_value);
+    // NOTE: We can't use DCHECK_NE here, since std::map<>::iterator does not
+    // support operator<<.
+    DCHECK(best_match != form_manager_->best_matches().end());
+    form_manager_->Update(*best_match->second);
     return;
   }
 
