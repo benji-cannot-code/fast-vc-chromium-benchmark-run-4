7a1b223ad03621894601dcab43c6a8cb06197e6d
Xiaocheng Hu <xiaochengh@chromium.org>
2017-12-27 20:37:02 -0800

    Move editing/SurroundingTextTest.cpp to core/exported
    This patch finishes a TODO left in a previous patch crrev.com/c/841629,
    which only modified the file without renaming it in order not to break
    the git revision log.
    
    Bug: 757719
    Change-Id: If3aa6a7b650f90133ffe0bcca20ee0ac05f01422
    Reviewed-on: https://chromium-review.googlesource.com/845075
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526404}

--- third_party/WebKit/Source/core/exported/WebSurroundingTextTest.cpp
+++ third_party/WebKit/Source/core/exported/WebSurroundingTextTest.cpp
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "public/web/WebSurroundingText.h"
 
-// TODO(xiaochengh): Rename this file as core/exported/WebSurroundingText.cpp
-
 #include <memory>
 #include "core/dom/Document.h"
 #include "core/dom/Range.h"
