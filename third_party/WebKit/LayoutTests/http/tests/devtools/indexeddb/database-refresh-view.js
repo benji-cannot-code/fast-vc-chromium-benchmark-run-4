5ea2a225a58f20b8a49fd51eb6481e222a62855e
Will Chen <chenwilliam@chromium.org>
2017-12-28 11:50:50 -0800

    DevTools: remove faulty rebaselines from wpt / testharness.js related cleanups
    A handful of DevTools tests got platform-specific test expectations from CLs
    related to wpt / testharness.js which shouldn't affect DevTools tests.
    
    This removes platform-specific expectations where appropriate and rebaselines
    the primary expectation. Some of these might actually not be flaky anymore
    and will be followed-up once I review the new baselines on the flakiness dashboard.
    
    Bug: 626703, 751952
    Change-Id: I898eb03c65c9fe35f9f9de499856e3e98290dbd3
    Reviewed-on: https://chromium-review.googlesource.com/846026
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Will Chen <chenwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526329}

--- third_party/WebKit/LayoutTests/http/tests/devtools/indexeddb/database-refresh-view.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/indexeddb/database-refresh-view.js
@@ -84,12 +84,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   await waitRefreshDatabase();
   TestRunner.addResult('Created second objectstore.');
   ApplicationTestRunner.dumpIndexedDBTree();
-  InspectorTest.dumpObjectStores();
+  ApplicationTestRunner.dumpObjectStores();
 
   // Add entries
   await ApplicationTestRunner.addIDBValueAsync(databaseName, objectStoreName1, 'testKey', 'testValue');
   TestRunner.addResult('Added ' + objectStoreName1 + ' entry.');
-  InspectorTest.dumpObjectStores();
+  ApplicationTestRunner.dumpObjectStores();
 
   // Refresh database view
   await waitRefreshDatabase();
@@ -97,12 +97,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   await waitUpdateDataView();
   await waitUpdateDataView();
   TestRunner.addResult('Refreshed database view.');
-  InspectorTest.dumpObjectStores();
+  ApplicationTestRunner.dumpObjectStores();
 
   // Add entries
   await ApplicationTestRunner.addIDBValueAsync(databaseName, objectStoreName2, 'testKey2', 'testValue2');
   TestRunner.addResult('Added ' + objectStoreName2 + ' entry.');
-  InspectorTest.dumpObjectStores();
+  ApplicationTestRunner.dumpObjectStores();
 
   // Right-click refresh database view
   await waitRefreshDatabaseRightClick();
@@ -110,7 +110,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   await waitUpdateDataView();
   await waitUpdateDataView();
   TestRunner.addResult('Right-click refreshed database.');
-  InspectorTest.dumpObjectStores();
+  ApplicationTestRunner.dumpObjectStores();
 
   await ApplicationTestRunner.deleteDatabaseAsync(databaseName);
   TestRunner.completeTest();
