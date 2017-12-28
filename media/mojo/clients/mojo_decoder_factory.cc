5e939aa3329e1dbf325b1508b15110b3c27bfd3f
liberato@chromium.org <liberato@chromium.org>
2017-12-28 14:05:42 -0800

    Add runtime feature for MojoVideoDecoder.
    Remove the build-time flag for MCVD, and replace it with a runtime
    flag media::kMojoVideoDecoder.  When enabled, chrome will consider
    using MojoVideoDecoder but not GpuVideoDecoder.
    
    Regardless of the flag, WebRTC can still use the VDAs directly.
    
    that the correct decoder was used.
    
    Bug: 765542
    Test: Manually set feature, tried WebRTC and H264 playback.  Verified
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ibf4fe584507f7d426a75c53e8fd07033f6c4900e
    Reviewed-on: https://chromium-review.googlesource.com/786148
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Chris Watkins <watk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526600}

--- media/mojo/clients/mojo_decoder_factory.cc
+++ media/mojo/clients/mojo_decoder_factory.cc
@@ -5,8 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/mojo/clients/mojo_decoder_factory.h"
 
+#include "base/feature_list.h"
 #include "base/memory/ptr_util.h"
 #include "base/single_thread_task_runner.h"
+#include "build/build_config.h"
+#include "media/base/media_switches.h"
 #include "media/mojo/clients/mojo_audio_decoder.h"
 #include "media/mojo/clients/mojo_video_decoder.h"
 #include "media/mojo/features.h"
@@ -43,6 +46,9 @@ void MojoDecoderFactory::CreateVideoDecoders(
     const RequestOverlayInfoCB& request_overlay_info_cb,
     std::vector<std::unique_ptr<VideoDecoder>>* video_decoders) {
 #if BUILDFLAG(ENABLE_MOJO_VIDEO_DECODER)
+  // If MojoVideoDecoder is not enabled, then return without adding anything.
+  if (!base::FeatureList::IsEnabled(media::kMojoVideoDecoder))
+    return;
   mojom::VideoDecoderPtr video_decoder_ptr;
   interface_factory_->CreateVideoDecoder(mojo::MakeRequest(&video_decoder_ptr));
 
