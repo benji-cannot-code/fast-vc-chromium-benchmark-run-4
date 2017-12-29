eb70bcd07fb479a257c9bbf5b823dd986d2890ca
Randy Smith <rdsmith@chromium.org>
2017-12-28 21:38:42 -0500

    Move serialization code for net::RedirectInfo to //services/network.
    Bug: 753658
    Change-Id: I68a98c15c64428d3faed7730d1837f9bbe3ac274
    Reviewed-on: https://chromium-review.googlesource.com/844025
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Randy Smith <rdsmith@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527002}

--- services/network/public/cpp/network_param_ipc_traits.h
+++ services/network/public/cpp/network_param_ipc_traits.h
@@ -17,8 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/cert/signed_certificate_timestamp_and_status.h"
 #include "net/http/http_request_headers.h"
 #include "net/ssl/ssl_info.h"
+#include "net/url_request/redirect_info.h"
 #include "services/network/public/cpp/cors_error_status.h"
 #include "services/network/public/cpp/url_loader_completion_status.h"
+#include "url/ipc/url_param_traits.h"
 
 #ifndef INTERNAL_SERVICES_NETWORK_PUBLIC_CPP_NETWORK_PARAM_IPC_TRAITS_H_
 #define INTERNAL_SERVICES_NETWORK_PUBLIC_CPP_NETWORK_PARAM_IPC_TRAITS_H_
@@ -26,7 +28,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // services/network/public/cpp is currently packaged as a static library,
 // so there's no need for export defines; it's linked directly into whatever
 // other components need it.
-// This redefinition is present for the IPC macros below.
+// This redefinition is present for the IPC macros below, including in the
+// included header files.
 #undef IPC_MESSAGE_EXPORT
 #define IPC_MESSAGE_EXPORT
 
@@ -153,6 +156,19 @@ IPC_STRUCT_TRAITS_BEGIN(network::URLLoaderCompletionStatus)
   IPC_STRUCT_TRAITS_MEMBER(cors_error_status)
   IPC_STRUCT_TRAITS_MEMBER(ssl_info)
   IPC_STRUCT_TRAITS_MEMBER(blocked_cross_site_document)
+IPC_STRUCT_TRAITS_END()
+
+IPC_ENUM_TRAITS_MAX_VALUE(net::URLRequest::ReferrerPolicy,
+                          net::URLRequest::MAX_REFERRER_POLICY - 1)
+
+IPC_STRUCT_TRAITS_BEGIN(net::RedirectInfo)
+  IPC_STRUCT_TRAITS_MEMBER(status_code)
+  IPC_STRUCT_TRAITS_MEMBER(new_method)
+  IPC_STRUCT_TRAITS_MEMBER(new_url)
+  IPC_STRUCT_TRAITS_MEMBER(new_site_for_cookies)
+  IPC_STRUCT_TRAITS_MEMBER(new_referrer)
+  IPC_STRUCT_TRAITS_MEMBER(new_referrer_policy)
+  IPC_STRUCT_TRAITS_MEMBER(referred_token_binding_host)
 IPC_STRUCT_TRAITS_END()
 
 #endif  // SERVICES_NETWORK_PUBLIC_CPP_NETWORK_PARAM_IPC_TRAITS_H_
