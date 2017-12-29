613bd75949e8df626d7e5c53b7ec983787270517
Saman Sami <samans@chromium.org>
2017-12-29 00:26:11 -0500

    Reland "Surface synchronization: Allocate new id when ScreenInfo changes"
    Reland the original CL with the failing unit test fixed.
    
    Original change's description:
    > Surface synchronization: Allocate new id when ScreenInfo changes
    >
    > RenderWidgetHostViewAura::UpdateScreenInfo only calls
    > window_->AllocateLocalSurfaceId() in auto-resize mode. We want
    > ScreenInfo to be synchronized across clients independent of whether
    > auto-resize is on or not. This CL fixes crbug.com/791154, in
    > which the renderer crashes because it doesn't receive a new
    > LocalSurfaceId when device scale factor changes.
    >
    > Bug: 672962,791154
    > Change-Id: If9c3836aee980a31e8d50159f6491b52bf812a6d
    > Reviewed-on: https://chromium-review.googlesource.com/836820
    > Reviewed-by: Antoine Labour <piman@chromium.org>
    > Commit-Queue: Saman Sami <samans@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#525557}
    
    TBR=piman@chromium.org
    
    Bug: 672962,791154
    Change-Id: I90bacdadb91c779759eb83d10844473487db00d8
    Reviewed-on: https://chromium-review.googlesource.com/846079
    Commit-Queue: Saman Sami <samans@chromium.org>
    Reviewed-by: Saman Sami <samans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526364}

--- content/browser/renderer_host/render_widget_host_view_mac.h
+++ content/browser/renderer_host/render_widget_host_view_mac.h
@@ -311,7 +311,6 @@ class CONTENT_EXPORT RenderWidgetHostViewMac
                          int error_code) override;
   void Destroy() override;
   void SetTooltipText(const base::string16& tooltip_text) override;
-  void UpdateScreenInfo(gfx::NativeView view) override;
   gfx::Size GetRequestedRendererSize() const override;
   bool IsSurfaceAvailableForCopy() const override;
   void CopyFromSurface(const gfx::Rect& src_rect,
@@ -343,6 +342,7 @@ class CONTENT_EXPORT RenderWidgetHostViewMac
 
   bool HasAcceleratedSurface(const gfx::Size& desired_size) override;
   gfx::Rect GetBoundsInRootWindow() override;
+  void OnSynchronizedDisplayPropertiesChanged() override;
 
   bool LockMouse() override;
   void UnlockMouse() override;
