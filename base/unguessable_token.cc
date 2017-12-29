d5395e59735f84244a4b6c01e46ebc8623d01b6d
Pavel Feldman <pfeldman@chromium.org>
2017-12-28 17:42:56 -0800

    Simplify UnguessableToken::ToString, don't wrap it in ().
    This change migrates unguessable tokens' serialization format from
    `(790345...265EE7)` to `7903415...265EE7`.
    
    When unguessable token is used for instrumentation and tracing, it can be used
    as a handle. Having non-URL characters such as punctuation in the serialized
    form of the token limits its use and requires it to be escaped / post-processed.
    
    If wrapping in () is not essential to the token, I'm suggesting that we drop it.
    
    Bug: 778263
    Change-Id: Ib88c15c478f6dca89425f272e35893dd399f91fd
    Reviewed-on: https://chromium-review.googlesource.com/845146
    Reviewed-by: Primiano Tucci <primiano@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Pavel Feldman <pfeldman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526627}

--- base/unguessable_token.cc
+++ base/unguessable_token.cc
@@ -15,7 +15,7 @@ UnguessableToken::UnguessableToken(uint64_t high, uint64_t low)
     : high_(high), low_(low) {}
 
 std::string UnguessableToken::ToString() const {
-  return base::StringPrintf("(%08" PRIX64 "%08" PRIX64 ")", high_, low_);
+  return base::StringPrintf("%08" PRIX64 "%08" PRIX64, high_, low_);
 }
 
 // static
@@ -36,7 +36,7 @@ UnguessableToken UnguessableToken::Deserialize(uint64_t high, uint64_t low) {
 }
 
 std::ostream& operator<<(std::ostream& out, const UnguessableToken& token) {
-  return out << token.ToString();
+  return out << "(" << token.ToString() << ")";
 }
 
 }  // namespace base
