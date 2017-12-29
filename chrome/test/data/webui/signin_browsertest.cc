263ac02868d1a81847a34f95878a60b1dd2e6d2a
Scott Chen <scottchen@chromium.org>
2017-12-28 16:54:05 -0800

    [signin] fix iron-icon path in sync confirmation dialog.
    Bug: 796536
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Icdd1b45f3c87ef18a7ece9f361788c2011746f35
    Reviewed-on: https://chromium-review.googlesource.com/837590
    Commit-Queue: Scott Chen <scottchen@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526353}

---
+++ chrome/test/data/webui/signin_browsertest.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/test/data/webui/signin_browsertest.h"
+
+#include "components/signin/core/browser/profile_management_switches.h"
+#include "components/signin/core/browser/scoped_account_consistency.h"
+
+SigninBrowserTest::SigninBrowserTest() {}
+
+SigninBrowserTest::~SigninBrowserTest() {}
+
+void SigninBrowserTest::EnableDice() {
+  scoped_account_consistency_ =
+      base::MakeUnique<signin::ScopedAccountConsistency>(
+          signin::AccountConsistencyMethod::kDice);
+}
