d148a9ece2ac6099e7f27627695a2a68e88bd0b9
Marti Wong <martiw@chromium.org>
2017-12-29 18:09:16 +1100

    Modify egtests/unittests for the new iOS bookmarks.
    - Modify the following tests, make them pass when the new iOS
       bookmarks flag is ON:
       1. BookmarkHomeViewControllerTest: LoadBookmarks
       2. keyboard_commands_egtest: testKeyboard...BookmarksPresented
       3. signin_interaction_controller_egtest: testSignIn...Bookmarks
    
    - Add some TODOs to cleanup old bookmarks' properties, functions &
      following NTP Bookmarks tests:
       1. NewTabPageControllerTest: SelectBookmarkPanel
       2. new_tab_page_egtest: testAccessibilityOnBookmarks
    
    Bug: 782551
    Change-Id: I4d60ad016d252643e5a04563671cf4f2d20df499
    Reviewed-on: https://chromium-review.googlesource.com/846679
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Marti Wong <martiw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526373}

--- ios/chrome/browser/ui/bookmarks/bookmark_home_view_controller.mm
+++ ios/chrome/browser/ui/bookmarks/bookmark_home_view_controller.mm
@@ -82,9 +82,6 @@ std::vector<GURL> GetUrlsToOpen(const std::vector<const BookmarkNode*>& nodes) {
     SigninPresenter,
     UIGestureRecognizerDelegate>
 
-// The app bar for the bookmarks.
-@property(nonatomic, strong) MDCAppBar* appBar;
-
 @end
 
 @implementation BookmarkHomeViewController
