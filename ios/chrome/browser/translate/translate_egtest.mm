054426eac44680ffe07d77b3af5630e92c293d33
Sylvain Defresne <sdefresne@chromium.org>
2017-12-29 16:37:14 +0100

    Wait until translate infobar is presented.
    The method -simulateTranslationFromSpanishToEnglish was causing
    flakyness of TranslateTestCase/test{,Incognito}TranslateInfoBar
    as the presentation of the infobar is animated. The animation
    is implemented in C++ so EarlGrey cannot synchronize with it.
    
    Instead add a method to wait for a matcher to return an element
    that is sufficiently visible and call it from flaky method to
    ensure that the element is visible.
    
    Use grey_sufficientlyVisible() instead of grey_notNil() in the
    test as the latter match elements that are outside of the window
    but grey_tap() cannot tap on them.
    
    Re-enable the test as the flakyness has been fixed.
    
    Bug: 798063
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I1047e2d4780e2f999b51ce3df9f3eaf7daf3e77b
    Reviewed-on: https://chromium-review.googlesource.com/846746
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526378}

--- ios/chrome/browser/translate/translate_egtest.mm
+++ ios/chrome/browser/translate/translate_egtest.mm
@@ -657,8 +657,7 @@ using translate::LanguageDetectionController;
 }
 
 // Tests that the Translate infobar is displayed after translation.
-// TODO(crbug.com/798063): test is flaky.
-- (void)FLAKY_testTranslateInfobar {
+- (void)testTranslateInfobar {
   const GURL URL =
       web::test::HttpServer::MakeUrl("http://scenarioTranslateInfobar");
   std::map<GURL, std::string> responses;
@@ -689,7 +688,7 @@ using translate::LanguageDetectionController;
   [[EarlGrey
       selectElementWithMatcher:chrome_test_util::ButtonWithAccessibilityLabel(
                                    switchLabel)]
-      assertWithMatcher:grey_notNil()];
+      assertWithMatcher:grey_sufficientlyVisible()];
 
   // Toggle "Always Translate" and check the preference.
   [[EarlGrey
@@ -861,17 +860,25 @@ using translate::LanguageDetectionController;
   client->GetTranslateManager()->PageTranslated(
       "es", "en", translate::TranslateErrors::NONE);
 
+  // The infobar is presented with an animation. Wait for the "Done" button
+  // to become visibile before considering the animation as complete.
+  [ChromeEarlGrey
+      waitForElementWithMatcherSufficientlyVisible:
+          chrome_test_util::ButtonWithAccessibilityLabelId(IDS_DONE)];
+
   // Assert that the infobar is visible.
   [[EarlGrey
       selectElementWithMatcher:chrome_test_util::ButtonWithAccessibilityLabelId(
-                                   IDS_DONE)] assertWithMatcher:grey_notNil()];
+                                   IDS_DONE)]
+      assertWithMatcher:grey_sufficientlyVisible()];
   [[EarlGrey
       selectElementWithMatcher:chrome_test_util::ButtonWithAccessibilityLabelId(
                                    IDS_TRANSLATE_INFOBAR_REVERT)]
-      assertWithMatcher:grey_notNil()];
+      assertWithMatcher:grey_sufficientlyVisible()];
   [[EarlGrey
       selectElementWithMatcher:chrome_test_util::ButtonWithAccessibilityLabelId(
-                                   IDS_CLOSE)] assertWithMatcher:grey_notNil()];
+                                   IDS_CLOSE)]
+      assertWithMatcher:grey_sufficientlyVisible()];
 }
 
 @end
