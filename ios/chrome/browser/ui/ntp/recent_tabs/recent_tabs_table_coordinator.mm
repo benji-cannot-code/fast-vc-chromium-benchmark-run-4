2edd4fa663b11ee554c1ec1477ae425155860bf4
Jérôme Lebel <jlebel@chromium.org>
2018-01-02 18:27:49 +0100

    [iOS][Signin] Fixing workflow when starting the sign-in in Recent Tabs
    The SigninPromoViewMediator should not be deallocated until the sign-in
    is finished. When the user clicks on "Continue as ...", the sign-in
    is done as soon as the sign-in dialogs arrives.
    
    RecentTabsTableViewController should not be updated as long as the
    sign-in started by SigninPromoViewMediator is in progress.
    
    Once the sign-in is done, the RecentTabsTableViewController can be
    reloaded.
    
    cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    
    Bug: 798443
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I4a0937986e236473a7c0314c056cf437a0088c36
    Reviewed-on: https://chromium-review.googlesource.com/846995
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526678}

--- ios/chrome/browser/ui/ntp/recent_tabs/recent_tabs_table_coordinator.mm
+++ ios/chrome/browser/ui/ntp/recent_tabs/recent_tabs_table_coordinator.mm
@@ -252,4 +252,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self.delegate updateNtpBarShadowForPanelController:self];
 }
 
+- (void)refreshSessionsViewRecentTabsTableViewController:
+    (RecentTabsTableViewController*)controller {
+  [self refreshSessionsView];
+}
+
 @end
