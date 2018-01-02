51b58e07b0547a0663d9b2eceaadd2306d89a047
Morten Stenshorne <mstensho@chromium.org>
2018-01-02 19:04:36 +0100

    Deduplicate code that both NG and legacy layout need.
    Just have legacy layout call the code in ng/.
    
    There were two identical definitions of FragmentainerBreakPrecedence(), which
    may cause trouble for Jumbo builds at some point.
    
    Also remove (static) LayoutBox::JoinFragmentainerBreakValues(), since NG
    already provides an identical function (with the same name).
    
    Change-Id: I1af407aa1fbde8411176ce23e8a99742f60f4fca
    Reviewed-on: https://chromium-review.googlesource.com/846996
    Reviewed-by: Daniel Bratell <bratell@opera.com>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526550}

--- third_party/WebKit/Source/core/layout/LayoutBlockFlow.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBlockFlow.cpp
@@ -51,6 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/layout/line/InlineTextBox.h"
 #include "core/layout/line/LineWidth.h"
 #include "core/layout/ng/layout_ng_block_flow.h"
+#include "core/layout/ng/ng_fragmentation_utils.h"
 #include "core/layout/ng/ng_layout_result.h"
 #include "core/layout/ng/ng_unpositioned_float.h"
 #include "core/layout/shapes/ShapeOutsideInfo.h"
