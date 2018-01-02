2694c749a9e0b8d4c01206d96244a604cd7b13d0
Darren Shen <shend@chromium.org>
2018-01-02 17:14:36 +1100

    [css-typed-om] Implement CSSPositionValue normalization.
    Spec: https://drafts.css-houdini.org/css-typed-om-1/#positionvalue-normalization
    
    Note that our implementation is much simpler than the spec algorithm
    because the parser already handles a lot of the logic.
    
    Bug: 788570
    Change-Id: Ice161237890222b6f28437a3846d8eb84bc0050c
    Reviewed-on: https://chromium-review.googlesource.com/846579
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: meade_UTC10 <meade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526445}

--- third_party/WebKit/Source/core/css/cssom/StyleValueFactory.cpp
+++ third_party/WebKit/Source/core/css/cssom/StyleValueFactory.cpp
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/css/cssom/CSSKeywordValue.h"
 #include "core/css/cssom/CSSNumericValue.h"
 #include "core/css/cssom/CSSOMTypes.h"
+#include "core/css/cssom/CSSPositionValue.h"
 #include "core/css/cssom/CSSStyleValue.h"
 #include "core/css/cssom/CSSStyleVariableReferenceValue.h"
 #include "core/css/cssom/CSSTransformValue.h"
@@ -32,6 +33,8 @@ CSSStyleValue* CreateStyleValueWithPropertyInternal(CSSPropertyID property_id,
   switch (property_id) {
     case CSSPropertyTransform:
       return CSSTransformValue::FromCSSValue(value);
+    case CSSPropertyObjectPosition:
+      return CSSPositionValue::FromCSSValue(value);
     default:
       // TODO(meade): Implement other properties.
       break;
