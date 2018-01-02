d5ab9030433c762e2f1fe058d8f344b4c02fac7c
Tom Anderson <thomasanderson@chromium.org>
2018-01-02 13:01:15 -0800

    Gtk3: Fix fuzzy breeze frame buttons when using scale factors > 1
    When using scale factors > 1, it is necessary to set the scale factor on all
    GtkStyleContext's so that GTK will fetch higher resolution assets.
    
    This was only seen on the Breeze theme, because it's the only one that uses
    bitmap backgrounds.
    
    BUG=794358
    R=erg@chromium.org
    
    Change-Id: Ib2c42e4869156caba94f377b583cbac96f44ad98
    Reviewed-on: https://chromium-review.googlesource.com/847683
    Reviewed-by: Elliot Glaysher <erg@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526638}

--- chrome/browser/ui/libgtkui/gtk_util.cc
+++ chrome/browser/ui/libgtkui/gtk_util.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/keycodes/keyboard_code_conversion_x.h"
 #include "ui/gfx/color_utils.h"
 #include "ui/gfx/geometry/size.h"
+#include "ui/views/linux_ui/linux_ui.h"
 
 namespace {
 
@@ -52,6 +53,11 @@ void CommonInitFromCommandLine(const base::CommandLine& command_line,
   for (size_t i = 0; i < args.size(); ++i) {
     free(argv[i]);
   }
+}
+
+float GetDeviceScaleFactor() {
+  views::LinuxUI* linux_ui = views::LinuxUI::instance();
+  return linux_ui ? linux_ui->GetDeviceScaleFactor() : 1;
 }
 
 }  // namespace
@@ -447,6 +453,7 @@ ScopedStyleContext AppendCssNodeToStyleContext(GtkStyleContext* context,
     }
     gtk_style_context_set_state(child_context, child_state);
   }
+  gtk_style_context_set_scale(child_context, std::ceil(GetDeviceScaleFactor()));
   gtk_style_context_set_parent(child_context, context);
   gtk_widget_path_unref(path);
   return child_context;
