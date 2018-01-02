d186f11ead45ea903e06b8a9622d2210df37ddb7
Shirish S <shirish.s@amd.com>
2018-01-02 11:02:56 +0530

    Fix WebGL performance regression on AMD Stoney platform
    The regression is caused since the below commit:
    "cdc786f5d2a2 Use 8x MSAA rather than 4x MSAA when available."
    
    It has been found that althogh 8x improves the image quality
    but leads to a performance regression of around 30%.
    Hence to resolve the same this patch introduces a workaround
    to limit the MSAA max sample count to 4x, for AMD platform _only_.
    
    Added self to AUTHORS
    
    BUG=chromium:798936,b:70820067
    TEST=test_that graphics_WebGLAquarium
         results :
         avg_fps_0050_fishes=59.2230100192
         avg_fps_1000_fishes=56.5302789847
    
    Change-Id: I3133c558ee641f126e2f932a8398d4ae4855f392
    Signed-off-by: Shirish S <shirish.s@amd.com>
    Reviewed-on: https://chromium-review.googlesource.com/846639
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527464}

--- gpu/config/gpu_driver_bug_workaround_type.h
+++ gpu/config/gpu_driver_bug_workaround_type.h
@@ -156,6 +156,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
          max_copy_texture_chromium_size_262144)              \
   GPU_OP(MAX_FRAGMENT_UNIFORM_VECTORS_32,                    \
          max_fragment_uniform_vectors_32)                    \
+  GPU_OP(MAX_MSAA_SAMPLE_COUNT_4,                             \
+         max_msaa_sample_count_4)                             \
   GPU_OP(MAX_TEXTURE_SIZE_LIMIT_4096,                        \
          max_texture_size_limit_4096)                        \
   GPU_OP(MAX_VARYING_VECTORS_16,                             \
