9dd6d2c321f27c7379cdd1d519418caa463a889e
Alexey Kozyatinskiy <kozyatinskiy@chromium.org>
2018-01-02 11:45:22 -0800

    [DevTools] unflake ..PauseInSharedWorkerInitialization
    TBR=dgozman@chromium.org
    
    Bug: chromium:432444
    Change-Id: I4e703013cc1c0ddb243bf18e95eda199da618859
    Reviewed-on: https://chromium-review.googlesource.com/847673
    Reviewed-by: Aleksey Kozyatinskiy <kozyatinskiy@chromium.org>
    Commit-Queue: Aleksey Kozyatinskiy <kozyatinskiy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526594}

--- third_party/WebKit/Source/devtools/front_end/Tests.js
+++ third_party/WebKit/Source/devtools/front_end/Tests.js
@@ -1199,7 +1199,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       if (SDK.targetManager.targets().length >= n)
         callback.call(null);
       else
-        this.addSniffer(SDK.TargetManager.prototype, 'addTarget', checkTargets.bind(this));
+        this.addSniffer(SDK.TargetManager.prototype, 'createTarget', checkTargets.bind(this));
     }
   };
 
