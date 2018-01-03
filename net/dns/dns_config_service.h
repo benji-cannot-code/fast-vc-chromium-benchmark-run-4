57906a4413cb714f239f640f0e5e77d2c8dea532
Matt Menke <mmenke@chromium.org>
2018-01-03 12:42:17 -0500

    Remove virtual from DnsConfig's destructor
    It's a struct, and there are no subclasses.
    
    Bug: none
    Change-Id: Ie3078977c9b59315869752d092f0e2382327fca2
    Reviewed-on: https://chromium-review.googlesource.com/848640
    Reviewed-by: Miriam Gershenson <mgersh@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526788}

--- net/dns/dns_config_service.h
+++ net/dns/dns_config_service.h
@@ -35,7 +35,7 @@ const int64_t kDnsDefaultTimeoutMs = 1000;
 struct NET_EXPORT_PRIVATE DnsConfig {
   DnsConfig();
   DnsConfig(const DnsConfig& other);
-  virtual ~DnsConfig();
+  ~DnsConfig();
 
   bool Equals(const DnsConfig& d) const;
 
