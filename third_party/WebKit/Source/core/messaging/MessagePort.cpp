fbb63db056d2e8b0cc52b9df715e4e4902f8c521
Marijn Kruisselbrink <mek@chromium.org>
2018-01-03 09:19:49 -0800

    Skip mojo serialization for MessagePort messages that stay in process.
    This will help enable SharedArrayBuffer to be posted to other agents
    in the same agent cluster.
    
    Bug: 798557
    Change-Id: I2fe3baa11e7aa910ecff75fb11bb5f521810c84f
    Reviewed-on: https://chromium-review.googlesource.com/840606
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526832}

--- third_party/WebKit/Source/core/messaging/MessagePort.cpp
+++ third_party/WebKit/Source/core/messaging/MessagePort.cpp
@@ -90,7 +90,7 @@ void MessagePort::postMessage(ScriptState* script_state,
     return;
 
   channel_.PostMojoMessage(
-      mojom::blink::TransferableMessage::SerializeAsMessage(&msg));
+      mojom::blink::TransferableMessage::WrapAsMessage(std::move(msg)));
 }
 
 MessagePortChannel MessagePort::Disentangle() {
