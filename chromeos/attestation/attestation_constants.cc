85d822254e262aa8d7409d99709525c4d4100484
Yves Arrouye <drcrash@google.com>
2018-01-03 15:04:01 -0800

    Use a specific key name for enterprise enrollment certificates
    Up to now we only needed an enrollment certificate before we would
    request a machine certificate so using the same key name for both
    worked since the machine certificate was requested after any
    enrollment one, and would be generated with a new key.
    
    However with automatic re-enrollment we can request an enrollment
    certificate after obtaining a machine one. Without a distinct key
    name, a request for an enrollment certificate may in fact return the
    latter requested machine certificate.
    
    BUG=chromium:798872
    TEST=chromeos_unittests
    
    Change-Id: Iec64b3cb210c6f75ce3cbca11ec4315874926f52
    Reviewed-on: https://chromium-review.googlesource.com/847943
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Commit-Queue: Yves Arrouye <drcrash@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527878}

--- chromeos/attestation/attestation_constants.cc
+++ chromeos/attestation/attestation_constants.cc
@@ -9,6 +9,7 @@ namespace chromeos {
 namespace attestation {
 
 const char kEnterpriseMachineKey[] = "attest-ent-machine";
+const char kEnterpriseEnrollmentKey[] = "attest-ent-enrollment";
 const char kEnterpriseUserKey[] = "attest-ent-user";
 const char kContentProtectionKeyPrefix[] = "attest-cp-";
 
