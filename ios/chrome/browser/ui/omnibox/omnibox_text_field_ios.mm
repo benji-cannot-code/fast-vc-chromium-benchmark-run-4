23fde31d2bdb265f13e41127cbc02c5cd4c85964
stkhapugin@chromium.org <stkhapugin@chromium.org>
2018-01-03 14:44:28 +0100

    Reland of  "Fix URL clipping on iOS 11."
    This reverts CL 779191.
    
    Wraps omnibox textfield into a container that positions the textfield
    inside of itself and clips its bounds so that the optimal clipping is
    used. The clipping logic replicates that of the pre-iOS 11 solution
    with UITextField overrides that are now broken in iOS 11.
    To mask the clipped ends of the omnibox, a ClippingMaskView is used
    as a maskView of the clipping view.
    All of this is behind kClippingTextfield flag that is enabled by
    default.
    
    Bug: 749788
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I4cc6673200a8fbdc57451dc9120dfd4eeec5a853
    Reviewed-on: https://chromium-review.googlesource.com/844079
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526693}

--- ios/chrome/browser/ui/omnibox/omnibox_text_field_ios.mm
+++ ios/chrome/browser/ui/omnibox/omnibox_text_field_ios.mm
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/autocomplete/autocomplete_scheme_classifier_impl.h"
 #include "ios/chrome/browser/experimental_flags.h"
 #import "ios/chrome/browser/ui/animation_util.h"
+#import "ios/chrome/browser/ui/omnibox/omnibox_clipping_feature.h"
 #include "ios/chrome/browser/ui/omnibox/omnibox_util.h"
 #import "ios/chrome/browser/ui/reversed_animation.h"
 #include "ios/chrome/browser/ui/rtl_geometry.h"
@@ -534,6 +535,12 @@ NSString* const kOmniboxFadeAnimationKey = @"OmniboxFadeAnimation";
 
 // Enumerate url components (host, path) and draw each one in different rect.
 - (void)drawTextInRect:(CGRect)rect {
+  if (base::FeatureList::IsEnabled(kClippingTextfield)) {
+    // With the new clipping logic, this override is unnecessary.
+    [super drawTextInRect:rect];
+    return;
+  }
+
   if (base::ios::IsRunningOnOrLater(11, 1, 0)) {
     // -[UITextField drawTextInRect:] ignores the argument, so we can't do
     // anything on 11.1 and up.
@@ -840,6 +847,11 @@ NSString* const kOmniboxFadeAnimationKey = @"OmniboxFadeAnimation";
 }
 
 - (CGRect)rectForDrawTextInRect:(CGRect)rect {
+  if (base::FeatureList::IsEnabled(kClippingTextfield)) {
+    // With the new clipping logic, this override is unnecessary.
+    return rect;
+  }
+
   // The goal is to always show the most significant part of the hostname
   // (i.e. the end of the TLD).
   //
