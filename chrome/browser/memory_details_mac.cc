c21c259e2c454c8df0f15f0ccf1719e951bfa72f
Erik Chen <erikchen@chromium.org>
2018-01-03 09:44:56 -0800

    Remove temporary estimates for private memory footprint
    The equivalent metrics are now uploaded by ProcessMemoryMetricsEmitter, except
    for all platforms rather than just macOS.
    
    Bug: 720541
    Change-Id: I36ed3a888bb0021744e12df84587c796cfd80865
    Reviewed-on: https://chromium-review.googlesource.com/837656
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526759}

--- chrome/browser/memory_details_mac.cc
+++ chrome/browser/memory_details_mac.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/file_version_info.h"
 #include "base/files/file_path.h"
 #include "base/mac/foundation_util.h"
-#include "base/mac/mac_util.h"
 #include "base/process/process_iterator.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
@@ -66,14 +65,6 @@ void CollectProcessDataForChromeProcess(
   metrics->GetCommittedAndWorkingSetKBytes(&info.committed, &info.working_set);
   base::ProcessMetrics::TaskVMInfo vm_info = metrics->GetTaskVMInfo();
   info.phys_footprint = vm_info.phys_footprint;
-
-  // TODO(erikchen): Remove this temporary estimate for private memory once the
-  // memory infra service emits the same metric. https://crbug.com/720541.
-  if (base::mac::IsAtLeastOS10_12()) {
-    info.private_memory_footprint = vm_info.phys_footprint;
-  } else {
-    info.private_memory_footprint = vm_info.internal + vm_info.compressed;
-  }
 
   processes->push_back(info);
 }
