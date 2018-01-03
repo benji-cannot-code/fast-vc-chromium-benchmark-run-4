9d5b24d287d3c052b37da2109d39500641f77c20
Ted Choc <tedchoc@google.com>
2018-01-03 13:49:22 -0800

    Throw exceptions instead of asserts in ApplicationStatus error cases.
    BUG=796932
    
    Change-Id: I7ce6be0b361162573430020d4b7a2de7cf1fc644
    Reviewed-on: https://chromium-review.googlesource.com/848241
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Ted Choc (back but slow, ping me) <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526849}

--- base/android/java/src/org/chromium/base/ApplicationStatus.java
+++ base/android/java/src/org/chromium/base/ApplicationStatus.java
@@ -299,7 +299,9 @@ public class ApplicationStatus {
      * Asserts that initialize method has been called.
      */
     private static void assertInitialized() {
-        assert sIsInitialized;
+        if (!sIsInitialized) {
+            throw new IllegalStateException("ApplicationStatus has not been initialized yet.");
+        }
     }
 
     /**
@@ -497,7 +499,9 @@ public class ApplicationStatus {
     @SuppressLint("NewApi")
     public static void registerStateListenerForActivity(ActivityStateListener listener,
             Activity activity) {
-        assert activity != null;
+        if (activity == null) {
+            throw new IllegalStateException("Attempting to register listener on a null activity.");
+        }
         ApplicationStatus.assertInitialized();
 
         ActivityInfo info = sActivityInfo.get(activity);
@@ -509,7 +513,11 @@ public class ApplicationStatus {
             info = new ActivityInfo();
             sActivityInfo.put(activity, info);
         }
-        assert info != null && info.getStatus() != ActivityState.DESTROYED;
+        if (info == null) {
+            throw new IllegalStateException(
+                    "Attempting to register listener on an untracked activity.");
+        }
+        assert info.getStatus() != ActivityState.DESTROYED;
         info.getListeners().addObserver(listener);
     }
 
