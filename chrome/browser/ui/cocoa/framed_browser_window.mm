fd290c54c7b0717be795018c106aa4f3318d8190
Sidney San Martín <sdy@chromium.org>
2018-01-03 13:37:19 -0500

    Fix strip which appears over the toolbar on exiting immersive fullscreen.
    This regressed in r492972 (switch to a custom window frame view class).
    
    When not using NSWindowStyleMaskFullSizeContentView (macOS <10.12, but
    it can probably be turned on back to 10.10), certain circumstances
    caused the tab strip background view to partially cover up the toolbar.
    The most reliable way to reproduce it, for me, is:
    
    - Be running macOS 10.11 or older.
    - Have multiple monitors connected.
    - Turn off "Displays have separate Spaces" in System Preferences.
    - Enter web content fullscreen (e.g. on YouTube).
    - Exit fullscreen by pressing Esc.
    
    Three things came together to make this happen:
    
    1. |-[FramedBrowserWindow browserFrameViewPaintHeight]| returned a
       hard-coded value when not using FullSizeContentView. I believe it
       used to represent the height of the tab strip but no longer does, and
       I believe it was just kept out of caution when the full size content
       view path was added, but it's bigger than it needs to be and makes
       the tab strip background view slightly larger than necessary. (This
       value originally appeared in r40677).
    
    2. |-[TabWindowController
       insertTabStripBackgroundViewIntoWindow:titleBar:]| explicitly
       inserted the background view behind all other views.
    
    3. The immersive fullscreen path, which is only taken when multiple
       monitors are connected and "Displays have separate Spaces" is turned
       off (see |-[BrowserWindowController enterWebContentFullscreen]|)
       inserts the content view behind all other views on exiting fullscreen
       (see |-[BrowserWindowController
       moveViewsForImmersiveFullscreen:regularWindow:fullscreenWindow:]|),
       which means that it's now behind the tab strip background view.
    
    I'm fixing this with three changes:
    
    1. Get rid of the special value in |-browserFrameViewPaintHeight| and
       return |chrome::kTabStripHeight| in both cases.
    
    2. Hide the |visualEffectView_|/|tabStripBackgroundView_| in fullscreen
       always, not just when using FullSizeContentView — othersise, it can be seen
       visible on top of the content in fullscreen.
    
    3. When setting up a window, insert the tab strip background view
       normally instead of behind its siblings. It just made these bugs
       harder to find (the strip would have appeared all the time,
       otherwise).
    
    Bug: 779082
    Change-Id: Id9536dc6ff8593b3e7e2b3f38bbf9149418d0ac6
    Reviewed-on: https://chromium-review.googlesource.com/848641
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Sidney San Martín <sdy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526766}

--- chrome/browser/ui/cocoa/framed_browser_window.mm
+++ chrome/browser/ui/cocoa/framed_browser_window.mm
@@ -35,8 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @implementation FramedBrowserWindow
 
 + (CGFloat)browserFrameViewPaintHeight {
-  return chrome::ShouldUseFullSizeContentView() ? chrome::kTabStripHeight
-                                                : 60.0;
+  return chrome::kTabStripHeight;
 }
 
 + (NSUInteger)defaultStyleMask {
--- chrome/browser/ui/cocoa/tabs/tab_window_controller.mm
+++ chrome/browser/ui/cocoa/tabs/tab_window_controller.mm
@@ -135,24 +135,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     if (hasTabStrip)
       [windowView addSubview:tabStripView_];
 
-    if (chrome::ShouldUseFullSizeContentView()) {
-      // |windowWillEnterFullScreen:| and |windowWillExitFullScreen:| are
-      // already called because self is a delegate for the window. However this
-      // class is designed for subclassing and can not implement
-      // NSWindowDelegate methods (because subclasses can do so as well and they
-      // should be able to). TODO(crbug.com/654656): Move |visualEffectView_| to
-      // subclass.
-      [[NSNotificationCenter defaultCenter]
-          addObserver:self
-             selector:@selector(windowWillEnterFullScreenNotification:)
-                 name:NSWindowWillEnterFullScreenNotification
-               object:window];
-      [[NSNotificationCenter defaultCenter]
-          addObserver:self
-             selector:@selector(windowWillExitFullScreenNotification:)
-                 name:NSWindowWillExitFullScreenNotification
-               object:window];
-    }
+    // |windowWillEnterFullScreen:| and |windowWillExitFullScreen:| are already
+    // called because self is a delegate for the window. However this class is
+    // designed for subclassing and can not implement NSWindowDelegate methods
+    // (because subclasses can do so as well and they should be able to).
+    // TODO(crbug.com/654656): Move |visualEffectView_| to subclass.
+    [[NSNotificationCenter defaultCenter]
+        addObserver:self
+           selector:@selector(windowWillEnterFullScreenNotification:)
+               name:NSWindowWillEnterFullScreenNotification
+             object:window];
+    [[NSNotificationCenter defaultCenter]
+        addObserver:self
+           selector:@selector(windowWillExitFullScreenNotification:)
+               name:NSWindowWillExitFullScreenNotification
+             object:window];
   }
   return self;
 }
@@ -461,18 +458,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
     [visualEffectWrapperView addSubview:visualEffectView_];
 
-    [[window contentView] addSubview:visualEffectWrapperView
-                          positioned:NSWindowBelow
-                          relativeTo:nil];
+    [[window contentView] addSubview:visualEffectWrapperView];
 
     // Make the |tabStripBackgroundView_| a child of the NSVisualEffectView.
     [tabStripBackgroundView_ setFrame:[visualEffectView_ bounds]];
     [visualEffectView_ addSubview:tabStripBackgroundView_];
   } else {
     DCHECK(!chrome::ShouldUseFullSizeContentView());
-    [[window contentView] addSubview:tabStripBackgroundView_
-                          positioned:NSWindowBelow
-                          relativeTo:nil];
+    [[window contentView] addSubview:tabStripBackgroundView_];
   }
 }
 
@@ -483,11 +476,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)windowWillEnterFullScreenNotification:(NSNotification*)notification {
-  [[visualEffectView_ animator] setAlphaValue:0.0];
+  [(visualEffectView_ ? visualEffectView_.get()
+                      : tabStripBackgroundView_.get()) setHidden:YES];
 }
 
 - (void)windowWillExitFullScreenNotification:(NSNotification*)notification {
-  [[visualEffectView_ animator] setAlphaValue:1.0];
+  [(visualEffectView_ ? visualEffectView_.get()
+                      : tabStripBackgroundView_.get()) setHidden:NO];
 }
 
 @end
