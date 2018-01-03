2976ebb2e896fe9c4099e47ab6b12bb6710fbc9f
Andrew Grieve <agrieve@chromium.org>
2018-01-03 14:22:09 -0500

    Supersize: Show empty <> in template short-names
    Noticed that bloaty does this and I think it's a good idea. It enables you to
    distinguish templates from non-templates, while still allowing grouping.
    
    Also fixes template stripping of operator>>
    
    Change-Id: I6deddcf01464e191c64f6d4569f8479c628a2c94
    Reviewed-on: https://chromium-review.googlesource.com/849133
    Commit-Queue: agrieve <agrieve@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526771}

--- tools/binary_size/libsupersize/function_signature_test.py
+++ tools/binary_size/libsupersize/function_signature_test.py
@@ -17,7 +17,7 @@ class AnalyzeTest(unittest.TestCase):
     def check(ret_part, name_part, params_part, after_part='',
               name_without_templates=None):
       if name_without_templates is None:
-        name_without_templates = re.sub(r'<.*?>', '', name_part) + after_part
+        name_without_templates = re.sub(r'<.*?>', '<>', name_part) + after_part
 
       signature = ''.join((name_part, params_part, after_part))
       got_full_name, got_template_name, got_name = (
@@ -61,7 +61,22 @@ class AnalyzeTest(unittest.TestCase):
     check('std::basic_ostream<char, std::char_traits<char> >& ',
           'std::operator<< <std::char_traits<char> >',
           '(std::basic_ostream<char, std::char_traits<char> >&, char)',
-          name_without_templates='std::operator<<')
+          name_without_templates='std::operator<< <>')
+    check('',
+          'std::basic_istream<char, std::char_traits<char> >'
+          '::operator>>',
+          '(unsigned int&)',
+          name_without_templates='std::basic_istream<>::operator>>')
+    check('',
+          'std::operator><std::allocator<char> >', '()',
+          name_without_templates='std::operator><>')
+    check('',
+          'std::operator>><std::allocator<char> >',
+          '(std::basic_istream<char, std::char_traits<char> >&)',
+          name_without_templates='std::operator>><>')
+    check('',
+          'std::basic_istream<char>::operator>', '(unsigned int&)',
+          name_without_templates='std::basic_istream<>::operator>')
     check('v8::internal::SlotCallbackResult ',
           'v8::internal::UpdateTypedSlotHelper::UpdateCodeTarget'
           '<v8::PointerUpdateJobTraits<(v8::Direction)1>::Foo(v8::Heap*, '
@@ -72,7 +87,7 @@ class AnalyzeTest(unittest.TestCase):
           '{lambda(v8::SlotType)#2}::operator()(v8::SlotType) const::'
           '{lambda(v8::Object**)#1})',
           name_without_templates=(
-              'v8::internal::UpdateTypedSlotHelper::UpdateCodeTarget'))
+              'v8::internal::UpdateTypedSlotHelper::UpdateCodeTarget<>'))
     check('',
           'WTF::StringAppend<WTF::String, WTF::String>::operator WTF::String',
           '()',
@@ -87,7 +102,7 @@ class AnalyzeTest(unittest.TestCase):
 
     # Test with multiple template args.
     check('int ', 'Foo<int()>::bar<a<b> >', '()',
-          name_without_templates='Foo::bar')
+          name_without_templates='Foo<>::bar<>')
 
     # SkArithmeticImageFilter.cpp has class within function body. e.g.:
     #   ArithmeticFP::onCreateGLSLInstance() looks like:
@@ -126,7 +141,7 @@ class AnalyzeTest(unittest.TestCase):
     check('', 'blink::CSSValueKeywordsHash::findValueImpl', '(char const*)',
           '::value_word_list')
     check('', 'foo::Bar<Z<Y> >::foo<bar>', '(abc)', '::var<baz>',
-          name_without_templates='foo::Bar::foo::var')
+          name_without_templates='foo::Bar<>::foo<>::var<>')
 
 if __name__ == '__main__':
   logging.basicConfig(level=logging.DEBUG,
