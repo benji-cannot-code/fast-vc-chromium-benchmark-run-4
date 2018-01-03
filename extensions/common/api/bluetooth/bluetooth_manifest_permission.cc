341475a238dfe1fd01e31096adb714e7b5a10626
Jens Widell <jl@opera.com>
2018-01-03 19:32:23 +0100

    Drop some jumbo-incompatible namespace aliases
    Many source files declare a namespace alias `errors` for a specific
    namespace containing error constants. This practice is incompatible with
    jumbo/unity builds, when different namespaces are aliased.
    
    The by far most common namespace to alias is `manifest_errors`. These
    aliases are kept, since they are not a problem as long as there are no
    other conflicting aliases. Other aliases are simply dropped, and the full
    namespace is used instead.
    
    Change-Id: Ie90f7e7a8f8be52b811b0b1e9c9839970669d96c
    Reviewed-on: https://chromium-review.googlesource.com/848918
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Jens Widell <jl@opera.com>
    Cr-Commit-Position: refs/heads/master@{#526952}

--- extensions/common/api/bluetooth/bluetooth_manifest_permission.cc
+++ extensions/common/api/bluetooth/bluetooth_manifest_permission.cc
@@ -27,8 +27,6 @@ namespace bluetooth_errors {
 const char kErrorInvalidUuid[] = "Invalid UUID '*'";
 }
 
-namespace errors = bluetooth_errors;
-
 namespace {
 
 bool ParseUuid(BluetoothManifestPermission* permission,
@@ -37,7 +35,7 @@ bool ParseUuid(BluetoothManifestPermission* permission,
   device::BluetoothUUID bt_uuid(uuid);
   if (!bt_uuid.IsValid()) {
     *error = ErrorUtils::FormatErrorMessageUTF16(
-        errors::kErrorInvalidUuid, uuid);
+        bluetooth_errors::kErrorInvalidUuid, uuid);
     return false;
   }
   permission->AddPermission(uuid);
