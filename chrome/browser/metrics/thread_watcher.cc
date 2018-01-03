3b6ad0f7a45982608641047de1b4f026369ee703
Gabriel Charette <gab@chromium.org>
2018-01-03 11:55:52 -0500

    Reland "Remove hang watching of deprecated BrowserThreads."
    Partial reland... keep StartWatching() calls to try to isolate
    the issue
    
    This is a reland of 91c22dbfcbb85107b0a26c5952881aa904692611
    Original change's description:
    > Remove hang watching of deprecated BrowserThreads.
    >
    > This is a partial reland of https://chromium-review.googlesource.com/c/chromium/src/+/705775
    >
    > That CL caused issue 768886 (spike in hang reports). I suspect this
    > was caused by a change in enum IDs rather than this change to the
    > hang watching code and this CL will help narrow down the cause.
    >
    > R=asvitkine@chromium.org
    >
    > Bug: 689520,768886
    > Change-Id: Id7bf1c7b5f5d34c4b1e7b1e71e9b015725695997
    > Reviewed-on: https://chromium-review.googlesource.com/743846
    > Commit-Queue: Gabriel Charette <gab@chromium.org>
    > Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#512509}
    
    Bug: 689520, 768886
    Change-Id: I138fc4b33e6316496ff35e927b2cfb5246b53a6d
    
    NOPRESUBMIT=TRUE (shuffling of code using deprecated BrowserThread IDs)
    
    Change-Id: I138fc4b33e6316496ff35e927b2cfb5246b53a6d
    Reviewed-on: https://chromium-review.googlesource.com/848355
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526736}

--- chrome/browser/metrics/thread_watcher.cc
+++ chrome/browser/metrics/thread_watcher.cc
@@ -467,13 +467,11 @@ void ThreadWatcherList::ParseCommandLine(
     crash_on_hang_thread_names =
         command_line.GetSwitchValueASCII(switches::kCrashOnHangThreads);
   } else if (channel != version_info::Channel::STABLE) {
-    // Default to crashing the browser if UI or IO or FILE threads are not
-    // responsive except in stable channel.
-    crash_on_hang_thread_names = base::StringPrintf(
-        "UI:%d:%d,IO:%d:%d,FILE:%d:%d",
-        kLiveThreadsThreshold, crash_seconds,
-        kLiveThreadsThreshold, crash_seconds,
-        kLiveThreadsThreshold, crash_seconds * 5);
+    // Default to crashing the browser if UI or IO threads are not responsive
+    // except in stable channel.
+    crash_on_hang_thread_names =
+        base::StringPrintf("UI:%d:%d,IO:%d:%d", kLiveThreadsThreshold,
+                           crash_seconds, kLiveThreadsThreshold, crash_seconds);
   }
 
   ParseCommandLineCrashOnHangThreads(crash_on_hang_thread_names,
@@ -553,6 +551,7 @@ void ThreadWatcherList::InitializeAndStartWatching(
                 unresponsive_threshold, crash_on_hang_threads);
   StartWatching(BrowserThread::IO, "IO", kSleepTime, kUnresponsiveTime,
                 unresponsive_threshold, crash_on_hang_threads);
+  // TODO(gab): Stop watching deprecated BrowserThreads, crbug.com/768886.
   StartWatching(BrowserThread::DB, "DB", kSleepTime, kUnresponsiveTime,
                 unresponsive_threshold, crash_on_hang_threads);
   StartWatching(BrowserThread::FILE, "FILE", kSleepTime, kUnresponsiveTime,
