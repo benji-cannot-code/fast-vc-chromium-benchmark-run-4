9a0c821b78aca559c88b21f8793e7b13e8bf7f53
Trent Apted <tapted@chromium.org>
2018-01-04 10:15:46 +1100

    Close the Zoom Bubble when its WebContents is destroyed or hidden.
    ZoomBubbleView has a raw, weak pointer to WebContents which can result
    in lifetime problems. On Mac, a UAF can result. Elsewhere, the result
    is typically a deleted pointer being passed to FindBrowserWithWebContents
    (which is less likely to cause badness).
    
    Also on Mac currently, the zoom bubble isn't immediately hidden on tab
    switch. This is because Cocoa bubbles are managed by
    base_bubble_controller.mm in a general way: it observes the tab strip
    via a bridge and dismisses a bubble of whatever type is currently
    showing. However, views bubbles can not opt into this framework.
    
    On other platforms, BrowserView explicitly closes the zoom bubble on
    tab switch.
    
    Most other views bubbles observe WebContentsObserver::WasHidden(),
    so do the same for the zoom bubble. And test.
    
    Bug: 791907, 404979
    Change-Id: I36d174530423dbcdb7f93d5e3a575ce07f74d8e7
    Reviewed-on: https://chromium-review.googlesource.com/848113
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526878}

--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -83,7 +83,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/infobars/infobar_container_view.h"
 #include "chrome/browser/ui/views/location_bar/location_bar_view.h"
 #include "chrome/browser/ui/views/location_bar/star_view.h"
-#include "chrome/browser/ui/views/location_bar/zoom_bubble_view.h"
 #include "chrome/browser/ui/views/new_back_shortcut_bubble.h"
 #include "chrome/browser/ui/views/omnibox/omnibox_view_views.h"
 #include "chrome/browser/ui/views/profiles/profile_indicator_icon.h"
@@ -797,7 +796,6 @@ void BrowserView::OnActiveTabChanged(content::WebContents* old_contents,
   UpdateTitleBar();
 
   TranslateBubbleView::CloseCurrentBubble();
-  ZoomBubbleView::CloseCurrentBubble();
 }
 
 void BrowserView::ZoomChangedForActiveTab(bool can_show_bubble) {
