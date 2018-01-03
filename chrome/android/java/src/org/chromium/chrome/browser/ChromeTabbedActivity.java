0307859ba895eb08f27d9a5f7280ff2625020ae2
Daniel Park <danielpark@chromium.org>
2018-01-02 16:30:48 -0800

    Hide incognito tab snapshot in Android Recents
    Sets attributes flags to secure if there is an incognito tab visible
      on the screen using TabModelSelectorObserver & OverviewModeObserver
    
    Bug: 764599
    Change-Id: Iac33dd9cad83a697316f7f925bcbb80444e54b4a
    Reviewed-on: https://chromium-review.googlesource.com/809508
    Commit-Queue: Daniel Park <danielpark@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Ted Choc (back but slow, ping me) <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526749}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -79,6 +79,7 @@ import org.chromium.chrome.browser.firstrun.FirstRunStatus;
 import org.chromium.chrome.browser.fullscreen.ChromeFullscreenManager;
 import org.chromium.chrome.browser.fullscreen.ComposedBrowserControlsVisibilityDelegate;
 import org.chromium.chrome.browser.incognito.IncognitoNotificationManager;
+import org.chromium.chrome.browser.incognito.IncognitoTabSnapshotController;
 import org.chromium.chrome.browser.infobar.DataReductionPromoInfoBar;
 import org.chromium.chrome.browser.locale.LocaleManager;
 import org.chromium.chrome.browser.metrics.ActivityStopMetrics;
@@ -897,6 +898,12 @@ public class ChromeTabbedActivity
             }
 
             mScreenshotMonitor = ScreenshotMonitor.create(ChromeTabbedActivity.this);
+
+            if (!CommandLine.getInstance().hasSwitch(
+                        ChromeSwitches.ENABLE_INCOGNITO_SNAPSHOTS_IN_ANDROID_RECENTS)) {
+                IncognitoTabSnapshotController.createIncognitoTabSnapshotController(
+                        getWindow(), mLayoutManager, mTabModelSelectorImpl);
+            }
 
             mUIInitialized = true;
         } finally {
