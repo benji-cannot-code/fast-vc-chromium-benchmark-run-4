083b91db04024de320c20cd51c732d30abb93046
Ke He <ke.he@intel.com>
2018-01-03 11:21:14 +0800

    Remove base::IDMap from screen_orientation_dispatcher.
    As a part of Onion Soup, the screen_orientation_dispatcher will be
    moved into blink where the base::IDMap is not allowed to use. So we
    remove the base::IDMap first in this CL, which makes the next moving
    step easier.
    
    BUG=678545
    
    Change-Id: Id46a80d07c4da90ce8807b1e97a9bee947b1944f
    Reviewed-on: https://chromium-review.googlesource.com/846699
    Commit-Queue: Ke He <ke.he@intel.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526657}

--- content/renderer/screen_orientation/screen_orientation_dispatcher.h
+++ content/renderer/screen_orientation/screen_orientation_dispatcher.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/compiler_specific.h"
-#include "base/containers/id_map.h"
 #include "base/macros.h"
 #include "content/public/renderer/render_frame_observer.h"
 #include "device/screen_orientation/public/interfaces/screen_orientation.mojom.h"
@@ -64,14 +63,8 @@ class CONTENT_EXPORT ScreenOrientationDispatcher
     screen_orientation_ = std::move(screen_orientation_for_tests);
   }
 
-  // The pending_callbacks_ map is mostly meant to have a unique ID to associate
-  // with every callback going trough the dispatcher. The map will own the
-  // pointer in the sense that it will destroy it when Remove() will be called.
-  // Furthermore, we only expect to have one callback at a time in this map,
-  // which is what IDMap was designed for.
-  using CallbackMap =
-      base::IDMap<std::unique_ptr<blink::WebLockOrientationCallback>>;
-  CallbackMap pending_callbacks_;
+  std::unique_ptr<blink::WebLockOrientationCallback> pending_callback_;
+  int request_id_ = 0;
 
   ScreenOrientationAssociatedPtr screen_orientation_;
 
