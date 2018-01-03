c5d94149fa4adf76281afadf8fa5761d05c39298
Tarun Bansal <tbansal@chromium.org>
2018-01-03 09:27:37 -0800

    Add histogram to record changes in the network quality
    Add histogram to record how frequently network quality
    is detected as meaningfully changed.
    
    Also, add DCHECKs to verify that the RTTs and throughput
    values are as unexpected.
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester;master.tryserver.chromium.mac:ios-simulator-cronet
    Bug: 798488
    Change-Id: Ie0bbf1f691a002a9257bff4c089e9dc8d0156b86
    Reviewed-on: https://chromium-review.googlesource.com/846285
    Reviewed-by: Helen Li <xunjieli@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Doug Arnett <dougarnett@chromium.org>
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526898}

--- content/browser/net/network_quality_observer_impl.cc
+++ content/browser/net/network_quality_observer_impl.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/net/network_quality_observer_impl.h"
 
 #include "base/memory/ptr_util.h"
+#include "base/metrics/histogram_macros.h"
 #include "content/browser/renderer_host/render_process_host_impl.h"
 #include "content/common/view_messages.h"
 #include "content/public/browser/browser_thread.h"
@@ -19,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // Returns true if the |current_value| is meaningfully different from the
-// |past_value|.
+// |past_value|. May be called with either RTT or throughput values to compare.
 bool MetricChangedMeaningfully(int32_t past_value, int32_t current_value) {
   if ((past_value == net::nqe::internal::INVALID_RTT_THROUGHPUT) !=
       (current_value == net::nqe::internal::INVALID_RTT_THROUGHPUT)) {
@@ -30,6 +31,9 @@ bool MetricChangedMeaningfully(int32_t past_value, int32_t current_value) {
       current_value == net::nqe::internal::INVALID_RTT_THROUGHPUT) {
     return false;
   }
+
+  DCHECK_LE(0, past_value);
+  DCHECK_LE(0, current_value);
 
   // Metric has changed meaningfully only if (i) the difference between the two
   // values exceed the threshold; and, (ii) the ratio of the values also exceeds
@@ -64,6 +68,10 @@ class NetworkQualityObserverImpl::UiThreadObserver
       : last_notified_type_(net::EFFECTIVE_CONNECTION_TYPE_UNKNOWN) {}
 
   ~UiThreadObserver() override {
+    if (!registrar_.IsRegistered(this, NOTIFICATION_RENDERER_PROCESS_CREATED,
+                                 NotificationService::AllSources())) {
+      return;
+    }
     registrar_.Remove(this, NOTIFICATION_RENDERER_PROCESS_CREATED,
                       NotificationService::AllSources());
   }
@@ -198,7 +206,12 @@ void NetworkQualityObserverImpl::OnRTTOrThroughputEstimatesComputed(
       last_notified_network_quality_.downstream_throughput_kbps(),
       downstream_throughput_kbps);
 
-  if (!http_rtt_changed && !transport_rtt_changed && !kbps_changed) {
+  bool network_quality_meaningfully_changed =
+      http_rtt_changed || transport_rtt_changed || kbps_changed;
+  UMA_HISTOGRAM_BOOLEAN("NQE.ContentObserver.NetworkQualityMeaningfullyChanged",
+                        network_quality_meaningfully_changed);
+
+  if (!network_quality_meaningfully_changed) {
     // Return since none of the metrics changed meaningfully. This reduces
     // the number of notifications to the different renderers every time
     // the network quality is recomputed.
--- net/nqe/network_quality.h
+++ net/nqe/network_quality.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/gtest_prod_util.h"
 #include "base/macros.h"
+#include "base/sequence_checker.h"
 #include "base/time/time.h"
 #include "net/base/net_export.h"
 
@@ -54,28 +55,46 @@ class NET_EXPORT_PRIVATE NetworkQuality {
   bool IsFaster(const NetworkQuality& other) const;
 
   // Returns the estimate of the round trip time at the HTTP layer.
-  const base::TimeDelta& http_rtt() const { return http_rtt_; }
+  const base::TimeDelta& http_rtt() const {
+    DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+    return http_rtt_;
+  }
 
-  void set_http_rtt(const base::TimeDelta& http_rtt) { http_rtt_ = http_rtt; }
+  void set_http_rtt(base::TimeDelta http_rtt) {
+    DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+    http_rtt_ = http_rtt;
+    DCHECK_LE(INVALID_RTT_THROUGHPUT, http_rtt_.InMilliseconds());
+  }
 
   // Returns the estimate of the round trip time at the transport layer.
-  const base::TimeDelta& transport_rtt() const { return transport_rtt_; }
+  const base::TimeDelta& transport_rtt() const {
+    DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+    return transport_rtt_;
+  }
 
-  void set_transport_rtt(const base::TimeDelta& transport_rtt) {
+  void set_transport_rtt(base::TimeDelta transport_rtt) {
+    DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
     transport_rtt_ = transport_rtt;
+    DCHECK_LE(INVALID_RTT_THROUGHPUT, transport_rtt_.InMilliseconds());
   }
 
   // Returns the estimate of the downstream throughput in Kbps (Kilobits per
   // second).
   int32_t downstream_throughput_kbps() const {
+    DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
     return downstream_throughput_kbps_;
   }
 
   void set_downstream_throughput_kbps(int32_t downstream_throughput_kbps) {
+    DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
     downstream_throughput_kbps_ = downstream_throughput_kbps;
+    DCHECK_LE(INVALID_RTT_THROUGHPUT, downstream_throughput_kbps_);
   }
 
  private:
+  // Verifies that the value of network quality is within the expected range.
+  void VerifyValueCorrectness() const;
+
   // Estimated round trip time at the HTTP layer.
   base::TimeDelta http_rtt_;
 
@@ -84,6 +103,8 @@ class NET_EXPORT_PRIVATE NetworkQuality {
 
   // Estimated downstream throughput in kilobits per second.
   int32_t downstream_throughput_kbps_;
+
+  SEQUENCE_CHECKER(sequence_checker_);
 };
 
 }  // namespace internal
