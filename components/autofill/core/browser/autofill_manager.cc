ccf7d4353d6617cd74a2c3ad41f77fcec537562a
Mathieu Perreault <mathp@chromium.org>
2018-01-03 11:27:32 -0500

    [Autofill] Cleanup around Mac Address Book support, which was unlaunched
    Bug: 488146
    Test: existing
    Change-Id: I41d59690688ca8dbba01fee9260fd25497061ff5
    Reviewed-on: https://chromium-review.googlesource.com/848056
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526764}

--- components/autofill/core/browser/autofill_manager.cc
+++ components/autofill/core/browser/autofill_manager.cc
@@ -766,8 +766,7 @@ void AutofillManager::FillOrPreviewForm(
   if (!IsValidFormData(form) || !IsValidFormFieldData(field))
     return;
 
-  // NOTE: RefreshDataModels may invalidate |data_model| because it causes the
-  // PersonalDataManager to reload Mac address book entries. Thus it must come
+  // NOTE: RefreshDataModels may invalidate |data_model|. Thus it must come
   // before GetProfile or GetCreditCard.
   if (!RefreshDataModels() || !driver()->RendererIsAvailable())
     return;
@@ -1332,8 +1331,7 @@ void AutofillManager::FillOrPreviewDataModelForm(
       }
     }
 
-    // Note that this may invalidate |data_model|, particularly if it is a Mac
-    // address book entry.
+    // Note that this may invalidate |data_model|.
     if (action == AutofillDriver::FORM_DATA_ACTION_FILL)
       personal_data_->RecordUseOf(data_model);
 
@@ -1388,8 +1386,7 @@ void AutofillManager::FillOrPreviewDataModelForm(
   if (autofilled_form_signatures_.size() > kMaxRecentFormSignaturesToRemember)
     autofilled_form_signatures_.pop_back();
 
-  // Note that this may invalidate |data_model|, particularly if it is a Mac
-  // address book entry.
+  // Note that this may invalidate |data_model|.
   if (action == AutofillDriver::FORM_DATA_ACTION_FILL)
     personal_data_->RecordUseOf(data_model);
 
