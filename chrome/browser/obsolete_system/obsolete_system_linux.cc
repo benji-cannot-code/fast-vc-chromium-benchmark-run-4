d3796d3a2416e67522d9c5aa224f5da134c1119e
Peter Kasting <pkasting@chromium.org>
2018-01-03 16:31:26 -0800

    Remove dead code.
    BUG=none
    TEST=none
    
    Change-Id: Iac87a469d61d677e188a670a4bb8fd501abe914a
    Reviewed-on: https://chromium-review.googlesource.com/849642
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526895}

--- chrome/browser/obsolete_system/obsolete_system_linux.cc
+++ chrome/browser/obsolete_system/obsolete_system_linux.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/obsolete_system/obsolete_system.h"
 
-#include <stdint.h>
-
 // static
 bool ObsoleteSystem::IsObsoleteNowOrSoon() {
   return false;
--- chrome/browser/obsolete_system/obsolete_system_mac.cc
+++ chrome/browser/obsolete_system/obsolete_system_mac.cc
@@ -5,11 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/obsolete_system/obsolete_system.h"
 
-#include "base/mac/mac_util.h"
-#include "chrome/common/url_constants.h"
-#include "chrome/grit/chromium_strings.h"
-#include "ui/base/l10n/l10n_util.h"
-
 // static
 bool ObsoleteSystem::IsObsoleteNowOrSoon() {
   return false;
@@ -17,9 +12,7 @@ bool ObsoleteSystem::IsObsoleteNowOrSoon() {
 
 // static
 base::string16 ObsoleteSystem::LocalizedObsoleteString() {
-  return l10n_util::GetStringUTF16(IsEndOfTheLine()
-                                       ? IDS_MAC_10_678_OBSOLETE_NOW
-                                       : IDS_MAC_10_678_OBSOLETE_SOON);
+  return base::string16();
 }
 
 // static
@@ -29,5 +22,5 @@ bool ObsoleteSystem::IsEndOfTheLine() {
 
 // static
 const char* ObsoleteSystem::GetLinkURL() {
-  return chrome::kMac10_678_DeprecationURL;
+  return "";
 }
