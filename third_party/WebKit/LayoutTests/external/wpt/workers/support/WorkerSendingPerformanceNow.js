c4649d38acc2e801f1823f98badb85eba2789bfc
Nicolas Pena <npm@chromium.org>
2018-01-04 16:20:17 -0500

    Move http/tests/w3c/webperf/submission/Google/HighResolutionTime to wpt
    Bug: chromium:498037
    Change-Id: Ia2e6435c8401922aa02f818b6f75c49ecf869869
    Reviewed-on: https://chromium-review.googlesource.com/850584
    Commit-Queue: Nicolás Peña Moreno <npm@chromium.org>
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527140}

---
+++ third_party/WebKit/LayoutTests/external/wpt/workers/support/WorkerSendingPerformanceNow.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+function calcResponse() {
+  const response = [
+    typeof(workerStart),
+    typeof(performance),
+    typeof(performance.now),
+    performance.now()
+  ];
+  return response;
+}
+
+self.onmessage = function(event) {
+  postMessage(calcResponse());
+  self.close();
+}
+
+self.addEventListener("connect", function(event) {
+  const port = event.ports[0];
+  port.onmessage = function(event) {
+    port.postMessage(calcResponse());
+    port.close();
+  };
+});
