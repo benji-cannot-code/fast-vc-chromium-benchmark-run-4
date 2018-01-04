b21ee702f88ab991e8264b92d2d1499c7b89253a
Brad Nelson <bradnelson@chromium.org>
2018-01-03 16:50:19 -0800

    Revert "Remove SharedArrayBuffer flag"
    This reverts commit 81ffb00e5f18db69247362a1b5f9c586c34099dd.
    
    (Note partial revert, skipping wpt expectations.)
    
    Bug: chromium:798864
    Change-Id: I6466c1428aeb5c37f3f7e7c0c12ced06445bc5f6
    Reviewed-on: https://chromium-review.googlesource.com/849473
    Reviewed-by: Darin Fisher <darin@chromium.org>
    Reviewed-by: Ben Smith <binji@chromium.org>
    Commit-Queue: Brad Nelson <bradnelson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526908}

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -121,6 +121,9 @@ void SetRuntimeFeaturesDefaultsAndUpdateFromArgs(
   WebRuntimeFeatures::EnableWebAssemblyStreaming(
       base::FeatureList::IsEnabled(features::kWebAssemblyStreaming));
 
+  WebRuntimeFeatures::EnableSharedArrayBuffer(
+      base::FeatureList::IsEnabled(features::kSharedArrayBuffer));
+
   if (command_line.HasSwitch(switches::kDisableSharedWorkers))
     WebRuntimeFeatures::EnableSharedWorker(false);
 
