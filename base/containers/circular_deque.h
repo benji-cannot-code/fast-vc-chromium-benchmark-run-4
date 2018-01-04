4dff6f7b70a7eae43a518a7cc583e2016b97183b
jdoerrie <jdoerrie@chromium.org>
2018-01-04 18:33:52 +0100

    Return references from base::circular_deque::emplace_*
    C++17 changes the return type of emplace{,_front,_back} from void to
    reference for sequence containers [1]. This change updates
    base::circular_deque to reflect this change and be compatible with
    C++17. Note that this change has no effect on base::queue when using
    C++14, as std::queue does not know yet about the changed return type.
    
    [1] http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0084r2.pdf
    
    Bug: 752720
    Change-Id: I163a6b60ade83ba135f429c8677fd60ab0bad451
    Reviewed-on: https://chromium-review.googlesource.com/839773
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Brett Wilson <brettw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527259}

--- base/containers/circular_deque.h
+++ base/containers/circular_deque.h
@@ -107,8 +107,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //   void push_front(T&&);
 //   void push_back(const T&);
 //   void push_back(T&&);
-//   void emplace_front(Args&&...);
-//   void emplace_back(Args&&...);
+//   T& emplace_front(Args&&...);
+//   T& emplace_back(Args&&...);
 //   void pop_front();
 //   void pop_back();
 //
@@ -836,7 +836,7 @@ class circular_deque {
   void push_back(T&& value) { emplace_back(std::move(value)); }
 
   template <class... Args>
-  void emplace_front(Args&&... args) {
+  reference emplace_front(Args&&... args) {
     ExpandCapacityIfNecessary(1);
     if (begin_ == 0)
       begin_ = buffer_.capacity() - 1;
@@ -844,10 +844,11 @@ class circular_deque {
       begin_--;
     IncrementGeneration();
     new (&buffer_[begin_]) T(std::forward<Args>(args)...);
+    return front();
   }
 
   template <class... Args>
-  void emplace_back(Args&&... args) {
+  reference emplace_back(Args&&... args) {
     ExpandCapacityIfNecessary(1);
     new (&buffer_[end_]) T(std::forward<Args>(args)...);
     if (end_ == buffer_.capacity() - 1)
@@ -855,6 +856,7 @@ class circular_deque {
     else
       end_++;
     IncrementGeneration();
+    return back();
   }
 
   void pop_front() {
