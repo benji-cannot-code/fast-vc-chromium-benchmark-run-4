836f46cdf8ba849e264a0334f899c8138a9eded8
Matt Reynolds <mattreynolds@chromium.org>
2018-01-04 19:22:56 +0000

    Revert "Move WifiPollingPolicy to a global instance"
    This reverts commit 29212f31a0213aa687111857a48267f494ca6d20.
    
    Reason for revert: Crashing on Canary builds, see crbug.com/798987
    
    Original change's description:
    > Move WifiPollingPolicy to a global instance
    > 
    > The WifiPollingPolicy controls how frequently the network location
    > provider will initiate Wi-Fi scans. Previously, the policy was attached
    > to the WifiDataProvider, which is destroyed when there are no active
    > geolocation API calls. Destruction of the provider causes us to lose track
    > of our polling policy.
    > 
    > The policy allows the first scan to be performed immediately and schedules
    > subsequent scans at regular intervals. When the provider is destroyed and
    > recreated rapidly it considers each scan to be "first", which allows many
    > Wi-Fi scans to be performed in a short window.
    > 
    > This change moves the policy state to a global instance so it can be saved
    > when the provider is destroyed and recreated. The new policy allows the
    > first-ever scan to be performed immediately, and subsequent scans may also
    > be performed immediately if enough time has lapsed since the previous scan.
    > In all other situations, the configured polling interval is enforced.
    > 
    > BUG=764954
    > 
    > Change-Id: I00649dde2a707127e53e0330511172fef4a43471
    > Reviewed-on: https://chromium-review.googlesource.com/834988
    > Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    > Reviewed-by: Reilly Grant <reillyg@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#526782}
    
    TBR=reillyg@chromium.org,mattreynolds@chromium.org
    
    Change-Id: I25d5596418df55e9de9e0f09e6ba7546b54551a4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 764954
    Reviewed-on: https://chromium-review.googlesource.com/851132
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527058}

--- device/geolocation/location_arbitrator.cc
+++ device/geolocation/location_arbitrator.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "device/geolocation/network_location_provider.h"
 #include "device/geolocation/public/cpp/geoposition.h"
-#include "device/geolocation/wifi_polling_policy.h"
 
 namespace device {
 
@@ -35,12 +34,7 @@ LocationArbitrator::LocationArbitrator(
       is_permission_granted_(false),
       is_running_(false) {}
 
-LocationArbitrator::~LocationArbitrator() {
-  // Destroy the global WifiPollingPolicy. The policy is created and used by the
-  // network location provider but should be retained across network provider
-  // restarts to ensure the time of the most recent WiFi scan is not lost.
-  WifiPollingPolicy::Shutdown();
-}
+LocationArbitrator::~LocationArbitrator() = default;
 
 bool LocationArbitrator::HasPermissionBeenGrantedForTest() const {
   return is_permission_granted_;
