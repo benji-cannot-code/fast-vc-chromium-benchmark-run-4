037ae77afc30a10085901ae796418552a96263d4
Mohamed Heikal <mheikal@google.com>
2018-01-04 11:37:01 -0500

    Android: Start activity tracking in ChromeApplication.attachBaseContext
    Start activity tracking in ChromeApplication.attachBaseContext rather
    than in ChromeBrowserProvider.onCreate
    
    This is because, activity tracking should start early on in the init
    phase however some third party android builds do not run
    ChromeBrowserProvider.oncreate early enough, causing some crashes.
    
    Bug: 796932
    Change-Id: Idba5bbf24e41b5df39db18a837d0d6ee36b22e0b
    Reviewed-on: https://chromium-review.googlesource.com/850715
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: agrieve <agrieve@chromium.org>
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527023}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeApplication.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeApplication.java
@@ -49,6 +49,7 @@ public class ChromeApplication extends ContentApplication {
         super.attachBaseContext(base);
         ContextUtils.initApplicationContext(this);
         BuildHooksAndroid.initCustomResources(this);
+        ApplicationStatus.initialize(this);
         Boolean isIsolatedProcess = PureJavaExceptionReporter.detectIsIsolatedProcess();
         if (isIsolatedProcess != null && !isIsolatedProcess.booleanValue()) {
             PureJavaExceptionHandler.installHandler();
