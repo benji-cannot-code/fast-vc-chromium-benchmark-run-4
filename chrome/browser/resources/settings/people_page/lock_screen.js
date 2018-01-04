8467c55e5d4de2ba27c3086d1bd4a6db7301877a
Sammie Quon <sammiequon@google.com>
2018-01-04 11:57:55 -0800

    settings: Disable tabbing to lock screen options when blocked by policy.
    Test: manual
    Bug: 798894
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I37638d56ed5aa7eceef30ac234a5af8f1eb997f0
    Reviewed-on: https://chromium-review.googlesource.com/850633
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527183}

--- chrome/browser/resources/settings/people_page/lock_screen.js
+++ chrome/browser/resources/settings/people_page/lock_screen.js
@@ -265,8 +265,10 @@ Polymer({
     this.showPasswordPromptDialog_ = false;
     if (!this.setModes_)
       settings.navigateToPreviousRoute();
-    else
+    else if (!this.$$('#unlockType').disabled)
       cr.ui.focusWithoutInk(assert(this.$$('#unlockType')));
+    else
+      cr.ui.focusWithoutInk(assert(this.$$('#screenLockDiv')));
   },
 
   /**
