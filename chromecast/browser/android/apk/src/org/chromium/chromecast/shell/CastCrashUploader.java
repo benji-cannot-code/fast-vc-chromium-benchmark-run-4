d40af609fba88e6ae2a765d7ec2bcf52bead8394
Simeon Anfinrud <sanfin@chromium.org>
2018-01-04 15:11:21 -0800

    [chromecast] Fix lint warnings in CastCrashUploader.
    Use ArrayList instead of LinkedList, suppress warnings for
    ignored Futures, and use UTF-8 to decode headers.
    
    Bug: internal b/70158727
    Test: build cast_shell_java
    Change-Id: I6a702e30b936409cfd519f4938c5214d441642c4
    Reviewed-on: https://chromium-review.googlesource.com/851064
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Commit-Queue: Simeon Anfinrud <sanfin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527314}

--- chromecast/browser/android/apk/src/org/chromium/chromecast/shell/CastCrashUploader.java
+++ chromecast/browser/android/apk/src/org/chromium/chromecast/shell/CastCrashUploader.java
@@ -19,7 +19,10 @@ import java.io.OutputStream;
 import java.io.SequenceInputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.LinkedList;
+import java.nio.charset.Charset;
+import java.nio.charset.UnsupportedCharsetException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledExecutorService;
@@ -46,6 +49,8 @@ public final class CastCrashUploader {
     private final String mCrashReportUploadUrl;
     private final String mUuid;
     private final String mApplicationFeedback;
+    private final Runnable mQueueAllCrashDumpUploadsRunnable =
+            () -> queueAllCrashDumpUploads(false);
 
     public CastCrashUploader(ScheduledExecutorService executorService, String crashDumpPath,
             String uuid, String applicationFeedback, boolean uploadCrashToStaging) {
@@ -60,25 +65,17 @@ public final class CastCrashUploader {
     }
 
     /** Sets up a periodic uploader, that checks for new dumps to upload every 20 minutes */
+    @SuppressWarnings("FutureReturnValueIgnored")
     public void startPeriodicUpload() {
-        mExecutorService.scheduleWithFixedDelay(
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        queueAllCrashDumpUploads(false);
-                    }
-                },
+        mExecutorService.scheduleWithFixedDelay(mQueueAllCrashDumpUploadsRunnable,
                 0, // Do first run immediately
                 20, // Run once every 20 minutes
                 TimeUnit.MINUTES);
     }
 
+    @SuppressWarnings("FutureReturnValueIgnored")
     public void uploadOnce() {
-        mExecutorService.schedule(new Runnable() {
-            public void run() {
-                queueAllCrashDumpUploads(false);
-            }
-        }, 0, TimeUnit.MINUTES);
+        mExecutorService.schedule(mQueueAllCrashDumpUploadsRunnable, 0, TimeUnit.MINUTES);
     }
 
     public void removeCrashDumps() {
@@ -101,7 +98,7 @@ public final class CastCrashUploader {
         if (mCrashDumpPath == null) return;
         Log.i(TAG, "Checking for crash dumps");
 
-        LinkedList<Future> tasks = new LinkedList<Future>();
+        List<Future> tasks = new ArrayList<Future>();
         File crashDumpDirectory = new File(mCrashDumpPath);
 
         final String log = getLogs(crashDumpDirectory);
@@ -109,11 +106,7 @@ public final class CastCrashUploader {
         for (final File potentialDump : crashDumpDirectory.listFiles()) {
             String dumpName = potentialDump.getName();
             if (dumpName.matches(DUMP_FILE_REGEX)) {
-                tasks.add(mExecutorService.submit(new Runnable() {
-                    public void run() {
-                        uploadCrashDump(potentialDump, log);
-                    }
-                }));
+                tasks.add(mExecutorService.submit(() -> uploadCrashDump(potentialDump, log)));
             }
         }
 
@@ -167,8 +160,8 @@ public final class CastCrashUploader {
                 logHeader.append("Content-Type: text/plain\n\n");
                 logHeader.append(log);
                 logHeader.append("\n");
-                InputStream logHeaderStream =
-                        new ByteArrayInputStream(logHeader.toString().getBytes());
+                InputStream logHeaderStream = new ByteArrayInputStream(
+                        logHeader.toString().getBytes(Charset.forName("UTF-8")));
                 // Upload: prepend the log file for uploading
                 uploadCrashDumpStream =
                         new SequenceInputStream(logHeaderStream, uploadCrashDumpStream);
@@ -184,7 +177,8 @@ public final class CastCrashUploader {
                 uuidBuilder.append(mUuid);
                 uuidBuilder.append("\n");
                 uploadCrashDumpStream = new SequenceInputStream(
-                        new ByteArrayInputStream(uuidBuilder.toString().getBytes()),
+                        new ByteArrayInputStream(
+                                uuidBuilder.toString().getBytes(Charset.forName("UTF-8"))),
                         uploadCrashDumpStream);
             } else {
                 Log.d(TAG, "No UUID");
@@ -200,8 +194,8 @@ public final class CastCrashUploader {
                 feedbackHeader.append("Content-Type: text/plain\n\n");
                 feedbackHeader.append(mApplicationFeedback);
                 feedbackHeader.append("\n");
-                InputStream feedbackHeaderStream =
-                        new ByteArrayInputStream(feedbackHeader.toString().getBytes());
+                InputStream feedbackHeaderStream = new ByteArrayInputStream(
+                        feedbackHeader.toString().getBytes(Charset.forName("UTF-8")));
                 // Upload: prepend the log file for uploading
                 uploadCrashDumpStream =
                         new SequenceInputStream(feedbackHeaderStream, uploadCrashDumpStream);
@@ -242,6 +236,8 @@ public final class CastCrashUploader {
             } catch (FileNotFoundException fnfe) {
                 // Android's HttpURLConnection implementation fires FNFE on some errors.
                 Log.e(TAG, "Failed response: " + connection.getResponseCode(), fnfe);
+            } catch (UnsupportedCharsetException e) {
+                Log.wtf(TAG, "UTF-8 not supported");
             } finally {
                 connection.disconnect();
                 dumpFileStream.close();
@@ -280,9 +276,12 @@ public final class CastCrashUploader {
      * @throws IOException
      */
     private String getFirstLine(InputStream inputStream) throws IOException {
-        try (InputStreamReader streamReader = new InputStreamReader(inputStream);
+        try (InputStreamReader streamReader = new InputStreamReader(inputStream, "UTF-8");
                 BufferedReader reader = new BufferedReader(streamReader)) {
             return reader.readLine();
+        } catch (UnsupportedCharsetException e) {
+            Log.wtf(TAG, "UTF-8 not supported");
+            return "";
         }
     }
 
