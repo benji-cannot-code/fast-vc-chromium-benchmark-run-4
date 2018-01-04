1de5e683c82007e6f2e3e2c3cc8cab2ec9008e41
Mike Reed <reed@google.com>
2018-01-04 13:23:24 -0500

    IWYU to fix an odd inclusion of SkMalloc.h inside SkTypes.h
    Bug: 
    Change-Id: Ife8c994a3874f74d00143761db4b2ac613a6cd10
    Reviewed-on: https://chromium-review.googlesource.com/850920
    Reviewed-by: Mike Klein <mtklein@chromium.org>
    Commit-Queue: Mike Reed <reed@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527070}

--- skia/ext/SkMemory_new_handler.cpp
+++ skia/ext/SkMemory_new_handler.cpp
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/process/memory.h"
 #include "build/build_config.h"
 #include "third_party/skia/include/core/SkTypes.h"
+#include "third_party/skia/include/private/SkMalloc.h"
 
 #if defined(OS_WIN)
 #include <windows.h>
