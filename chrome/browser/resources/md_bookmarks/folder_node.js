8427206e53dc731dc503345463f59d7d4fc7ece2
Christopher Lam <calamity@chromium.org>
2018-01-05 14:34:29 +1100

    [MD Bookmarks] Make sidebar scroll to folder node when folder selected.
    This CL makes the sidebar scroll the selected folder node into view
    whenever a folder is selected. It also adds a tooltip to folder nodes.
    
    Bug: 794684
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ia35556065da78d673974352f58c0deece598de2c
    Reviewed-on: https://chromium-review.googlesource.com/851395
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527579}

--- chrome/browser/resources/md_bookmarks/folder_node.js
+++ chrome/browser/resources/md_bookmarks/folder_node.js
@@ -59,6 +59,7 @@ Polymer({
 
   observers: [
     'updateAriaExpanded_(hasChildFolder_, isOpen)',
+    'scrollIntoViewIfNeeded_(isSelectedFolder_)',
   ],
 
   /** @override */
@@ -77,12 +78,6 @@ Polymer({
     });
 
     this.updateFromStore();
-
-    if (this.isSelectedFolder_) {
-      this.async(function() {
-        this.scrollIntoViewIfNeeded();
-      });
-    }
   },
 
   /**
@@ -385,6 +380,17 @@ Polymer({
       this.getFocusTarget().setAttribute('aria-expanded', String(isOpen));
     else
       this.getFocusTarget().removeAttribute('aria-expanded');
+  },
+
+  /**
+   * Scrolls the folder node into view when the folder is selected.
+   * @private
+   */
+  scrollIntoViewIfNeeded_: function() {
+    if (!this.isSelectedFolder_)
+      return;
+
+    this.async(() => this.$.container.scrollIntoViewIfNeeded());
   },
 
   /**
