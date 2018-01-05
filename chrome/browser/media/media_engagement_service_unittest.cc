8734d12261765eda383247ac902fe661c38a2a83
Mounir Lamouri <mlamouri@chromium.org>
2018-01-05 14:56:24 +0100

    Media Engagement: update threshold.
    Bug: None
    Change-Id: I6c7328779c3936273d65a573718eb9e5c77a87f6
    Reviewed-on: https://chromium-review.googlesource.com/848714
    Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527275}

--- chrome/browser/media/media_engagement_service_unittest.cc
+++ chrome/browser/media/media_engagement_service_unittest.cc
@@ -313,7 +313,7 @@ TEST_F(MediaEngagementServiceTest, CleanupOriginsOnHistoryDeletion) {
   // and we will ensure it has the same score. origin2 will have a score
   // that is zero and will remain zero. origin3 will have a score
   // and will be cleared. origin4 will have a normal score.
-  SetScores(origin1, MediaEngagementScore::GetScoreMinVisits() + 3, 2);
+  SetScores(origin1, MediaEngagementScore::GetScoreMinVisits() + 2, 4);
   SetScores(origin2, 2, 1);
   SetScores(origin3, 2, 1);
   SetScores(origin4, MediaEngagementScore::GetScoreMinVisits(), 2);
@@ -337,14 +337,14 @@ TEST_F(MediaEngagementServiceTest, CleanupOriginsOnHistoryDeletion) {
   history->AddPage(origin3a, yesterday_afternoon, history::SOURCE_BROWSED);
 
   // Check that the scores are valid at the beginning.
-  ExpectScores(origin1, 0.25, MediaEngagementScore::GetScoreMinVisits() + 3, 2,
-               TimeNotSet());
+  ExpectScores(origin1, 2.0 / 3.0,
+               MediaEngagementScore::GetScoreMinVisits() + 2, 4, TimeNotSet());
   EXPECT_TRUE(GetActualScore(origin1));
   ExpectScores(origin2, 0.0, 2, 1, TimeNotSet());
   EXPECT_FALSE(GetActualScore(origin2));
   ExpectScores(origin3, 0.0, 2, 1, TimeNotSet());
   EXPECT_FALSE(GetActualScore(origin3));
-  ExpectScores(origin4, 0.4, MediaEngagementScore::GetScoreMinVisits(), 2,
+  ExpectScores(origin4, 0.5, MediaEngagementScore::GetScoreMinVisits(), 2,
                TimeNotSet());
   EXPECT_TRUE(GetActualScore(origin4));
 
@@ -363,14 +363,13 @@ TEST_F(MediaEngagementServiceTest, CleanupOriginsOnHistoryDeletion) {
     // should have a score that is zero but it's visits and playbacks should
     // have decreased. origin3 should have had a decrease in the number of
     // visits. origin4 should have the old score.
-    ExpectScores(origin1, 1.0 / 6.0,
-                 MediaEngagementScore::GetScoreMinVisits() + 1, 1,
+    ExpectScores(origin1, 0.5, MediaEngagementScore::GetScoreMinVisits(), 2,
                  TimeNotSet());
     EXPECT_TRUE(GetActualScore(origin1));
     ExpectScores(origin2, 0.0, 1, 0, TimeNotSet());
     EXPECT_FALSE(GetActualScore(origin2));
     ExpectScores(origin3, 0.0, 1, 0, TimeNotSet());
-    ExpectScores(origin4, 0.4, MediaEngagementScore::GetScoreMinVisits(), 2,
+    ExpectScores(origin4, 0.5, MediaEngagementScore::GetScoreMinVisits(), 2,
                  TimeNotSet());
 
     histogram_tester.ExpectTotalCount(
@@ -378,7 +377,7 @@ TEST_F(MediaEngagementServiceTest, CleanupOriginsOnHistoryDeletion) {
     histogram_tester.ExpectBucketCount(
         MediaEngagementService::kHistogramURLsDeletedScoreReductionName, 0, 2);
     histogram_tester.ExpectBucketCount(
-        MediaEngagementService::kHistogramURLsDeletedScoreReductionName, 8, 1);
+        MediaEngagementService::kHistogramURLsDeletedScoreReductionName, 17, 1);
   }
 
   {
@@ -398,17 +397,17 @@ TEST_F(MediaEngagementServiceTest, CleanupOriginsOnHistoryDeletion) {
     waiter.Wait();
 
     // origin1's score should have changed but the rest should remain the same.
-    ExpectScores(origin1, 0.0, MediaEngagementScore::GetScoreMinVisits(), 0,
+    ExpectScores(origin1, 0.0, MediaEngagementScore::GetScoreMinVisits() - 1, 1,
                  TimeNotSet());
     ExpectScores(origin2, 0.0, 1, 0, TimeNotSet());
     ExpectScores(origin3, 0.0, 1, 0, TimeNotSet());
-    ExpectScores(origin4, 0.4, MediaEngagementScore::GetScoreMinVisits(), 2,
+    ExpectScores(origin4, 0.5, MediaEngagementScore::GetScoreMinVisits(), 2,
                  TimeNotSet());
 
     histogram_tester.ExpectTotalCount(
         MediaEngagementService::kHistogramURLsDeletedScoreReductionName, 1);
     histogram_tester.ExpectBucketCount(
-        MediaEngagementService::kHistogramURLsDeletedScoreReductionName, 17, 1);
+        MediaEngagementService::kHistogramURLsDeletedScoreReductionName, 50, 1);
   }
 
   {
@@ -430,11 +429,11 @@ TEST_F(MediaEngagementServiceTest, CleanupOriginsOnHistoryDeletion) {
     // origin3's score should be removed but the rest should remain the same.
     std::map<GURL, double> scores = GetScoreMapForTesting();
     EXPECT_TRUE(scores.find(origin3) == scores.end());
-    ExpectScores(origin1, 0.0, MediaEngagementScore::GetScoreMinVisits(), 0,
+    ExpectScores(origin1, 0.0, MediaEngagementScore::GetScoreMinVisits() - 1, 1,
                  TimeNotSet());
     ExpectScores(origin2, 0.0, 1, 0, TimeNotSet());
     ExpectScores(origin3, 0.0, 0, 0, TimeNotSet());
-    ExpectScores(origin4, 0.4, MediaEngagementScore::GetScoreMinVisits(), 2,
+    ExpectScores(origin4, 0.5, MediaEngagementScore::GetScoreMinVisits(), 2,
                  TimeNotSet());
 
     histogram_tester.ExpectTotalCount(
@@ -526,7 +525,7 @@ TEST_F(MediaEngagementServiceTest, HasHighEngagement) {
   GURL url3("https://www.example.com");
 
   SetScores(url1, 6, 5);
-  SetScores(url2, 6, 3);
+  SetScores(url2, 6, 1);
 
   EXPECT_TRUE(HasHighEngagement(url1));
   EXPECT_FALSE(HasHighEngagement(url2));
