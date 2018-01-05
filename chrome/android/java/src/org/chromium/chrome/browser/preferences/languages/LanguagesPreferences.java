19f60d82f294dfa1bd9f03d511533d175de1ceb3
Leo Zhang <googleo@chromium.org>
2018-01-05 11:56:12 +1100

    Add loggings for languages settings.
    Add page impression and actions logging to monitor the new feature.
    Tested on my devices, all metrics were logged in the chrome://histograms
    
    Bug: 783049
    Change-Id: I3d4ac98ed9550763987a2c1f912037715e8e61b8
    Reviewed-on: https://chromium-review.googlesource.com/850493
    Commit-Queue: Leo Zhang <googleo@chromium.org>
    Reviewed-by: Ted Choc (back but slow, ping me) <tedchoc@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527209}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/languages/LanguagesPreferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/languages/LanguagesPreferences.java
@@ -50,7 +50,11 @@ public class LanguagesPreferences
         translateSwitch.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
             @Override
             public boolean onPreferenceChange(Preference preference, Object newValue) {
-                PrefServiceBridge.getInstance().setTranslateEnabled((boolean) newValue);
+                boolean enabled = (boolean) newValue;
+                PrefServiceBridge.getInstance().setTranslateEnabled(enabled);
+                LanguagesManager.recordAction(enabled
+                                ? LanguagesManager.ACTION_ENABLE_TRANSLATE_GLOBALLY
+                                : LanguagesManager.ACTION_DISABLE_TRANSLATE_GLOBALLY);
                 return true;
             }
         });
@@ -60,6 +64,7 @@ public class LanguagesPreferences
                 return PrefServiceBridge.getInstance().isTranslateManaged();
             }
         });
+        LanguagesManager.recordImpression(LanguagesManager.PAGE_MAIN);
     }
 
     @Override
@@ -74,6 +79,7 @@ public class LanguagesPreferences
         if (requestCode == REQUEST_CODE_ADD_LANGUAGES && resultCode == getActivity().RESULT_OK) {
             String code = data.getStringExtra(AddLanguageFragment.INTENT_NEW_ACCEPT_LANGAUGE);
             LanguagesManager.getInstance().addToAcceptLanguages(code);
+            LanguagesManager.recordAction(LanguagesManager.ACTION_LANGUAGE_ADDED);
         }
     }
 
