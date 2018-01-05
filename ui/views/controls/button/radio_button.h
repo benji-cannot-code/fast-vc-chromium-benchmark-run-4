104c349a0f05f8f20b5be686ad82f110c4d223eb
Bret Sepulveda <bsep@chromium.org>
2018-01-04 17:34:38 -0800

    Fix radio buttons not taking focus from other buttons in the group.
    Radio buttons shouldn't take focus when clicked. But if another radio
    button in the group has focus it looks wrong to not take the focus from
    it, because the selection and focus don't match any more. So now we take
    focus only if the focus is already in the group.
    
    Bug: 795656
    Change-Id: If0b69d61842208d57ad00761ce3dd4d2cffad752
    Reviewed-on: https://chromium-review.googlesource.com/848232
    Commit-Queue: Bret Sepulveda <bsep@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527204}

--- ui/views/controls/button/radio_button.h
+++ ui/views/controls/button/radio_button.h
@@ -31,6 +31,7 @@ class VIEWS_EXPORT RadioButton : public Checkbox {
   void OnFocus() override;
 
   // Overridden from Button:
+  void RequestFocusFromEvent() override;
   void NotifyClick(const ui::Event& event) override;
 
   // Overridden from LabelButton:
