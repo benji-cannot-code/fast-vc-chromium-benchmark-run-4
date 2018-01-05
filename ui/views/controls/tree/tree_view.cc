7ed92744e53cf2cde75829ad844349ff824368a5
Elly Fong-Jones <ellyjones@chromium.org>
2018-01-05 15:32:00 -0500

    views: don't show focus ring on TreeView's editor
    This change:
    1) Undoes the positioning special case from a1a251f4dd
    2) Sets a 1pt GoogleBlue700 solid border on the TreeView's
       editor
    
    Change (2) has the effect of suppressing the focus ring in
    Harmony mode, while not changing the focused appearance in
    pre-Harmony. Also, since the TreeView blurs the editor when
    
    it itself blurs (and ends editing), this ends up having
    the same behavior as a regular focus ring but a different
    appearance.
    
    Bug: 788128
    Change-Id: I5cf34868a9075478bbfc8ffe4414c93b1f696187
    Reviewed-on: https://chromium-review.googlesource.com/852377
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527617}

--- ui/views/controls/tree/tree_view.cc
+++ ui/views/controls/tree/tree_view.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/skia_util.h"
 #include "ui/native_theme/native_theme.h"
 #include "ui/resources/grit/ui_resources.h"
+#include "ui/views/border.h"
 #include "ui/views/controls/focus_ring.h"
 #include "ui/views/controls/prefix_selector.h"
 #include "ui/views/controls/scroll_view.h"
@@ -52,7 +53,6 @@ static const int kArrowRegionSize = 12;
 // Padding around the text (on each side).
 static const int kTextVerticalPadding = 3;
 static const int kTextHorizontalPadding = 2;
-static const int kTextHorizontalPaddingWithFocusRing = 4;
 // How much children are indented from their parent.
 static const int kIndent = 20;
 
@@ -105,16 +105,6 @@ TreeView::TreeView()
   }
   text_offset_ = closed_icon_.width() + kImagePadding + kImagePadding +
       kArrowRegionSize;
-
-  // The horizontal padding will be needed before an editor text field is ever
-  // constructed, otherwise it would be nice to use editor_->uses_focus_ring()
-  // or similar. Since Textfields only use focus rings when
-  // IsSecondaryUiMaterial() and when they have no border, checking that here
-  // is safe.
-  text_horizontal_padding_ =
-      ui::MaterialDesignController::IsSecondaryUiMaterial()
-          ? kTextHorizontalPaddingWithFocusRing
-          : kTextHorizontalPadding;
 }
 
 TreeView::~TreeView() {
@@ -187,6 +177,7 @@ void TreeView::StartEditing(TreeModelNode* node) {
   editing_ = true;
   if (!editor_) {
     editor_ = new Textfield;
+    editor_->SetBorder(views::CreateSolidBorder(1, gfx::kGoogleBlue700));
     // Add the editor immediately as GetPreferredSize returns the wrong thing if
     // not parented.
     AddChildView(editor_);
@@ -725,7 +716,7 @@ void TreeView::UpdatePreferredSize() {
 
   preferred_size_.SetSize(
       root_.GetMaxWidth(this, text_offset_, root_shown_ ? 1 : 0) +
-          text_horizontal_padding_ * 2,
+          kTextHorizontalPadding * 2,
       row_height_ * GetRowCount());
 }
 
@@ -740,7 +731,7 @@ void TreeView::LayoutEditor() {
       GetMirroredXWithWidthInView(row_bounds.x(), row_bounds.width()));
   row_bounds.set_x(row_bounds.x() + text_offset_);
   row_bounds.set_width(row_bounds.width() - text_offset_);
-  row_bounds.Inset(text_horizontal_padding_, kTextVerticalPadding);
+  row_bounds.Inset(kTextHorizontalPadding, kTextVerticalPadding);
   row_bounds.Inset(-empty_editor_size_.width() / 2,
                    -(empty_editor_size_.height() - font_list_.GetHeight()) / 2);
   // Give a little extra space for editing.
@@ -829,9 +820,9 @@ void TreeView::PaintRow(gfx::Canvas* canvas,
 
   // Paint the text.
   const gfx::Rect internal_bounds(
-      text_bounds.x() + text_horizontal_padding_,
+      text_bounds.x() + kTextHorizontalPadding,
       text_bounds.y() + kTextVerticalPadding,
-      text_bounds.width() - text_horizontal_padding_ * 2,
+      text_bounds.width() - kTextHorizontalPadding * 2,
       text_bounds.height() - kTextVerticalPadding * 2);
   canvas->DrawStringRect(
       node->model_node()->GetTitle(), font_list_,
@@ -932,15 +923,14 @@ gfx::Rect TreeView::GetTextBoundsForNode(InternalNode* node) {
 // leading aligned.
 gfx::Rect TreeView::GetAuxiliaryTextBoundsForNode(InternalNode* node) {
   gfx::Rect text_bounds = GetTextBoundsForNode(node);
-  int width = base::i18n::IsRTL()
-                  ? text_bounds.x() - text_horizontal_padding_ * 2
-                  : bounds().width() - text_bounds.right() -
-                        2 * text_horizontal_padding_;
+  int width = base::i18n::IsRTL() ? text_bounds.x() - kTextHorizontalPadding * 2
+                                  : bounds().width() - text_bounds.right() -
+                                        2 * kTextHorizontalPadding;
   if (width < 0)
     return gfx::Rect();
   int x = base::i18n::IsRTL()
-              ? text_horizontal_padding_
-              : bounds().right() - width - text_horizontal_padding_;
+              ? kTextHorizontalPadding
+              : bounds().right() - width - kTextHorizontalPadding;
   return gfx::Rect(x, text_bounds.y(), width, text_bounds.height());
 }
 
@@ -949,9 +939,8 @@ gfx::Rect TreeView::GetForegroundBoundsForNodeImpl(InternalNode* node,
                                                    int depth) {
   int width =
       drawing_provider()->ShouldDrawIconForNode(this, node->model_node())
-          ? text_offset_ + node->text_width() + text_horizontal_padding_ * 2
-          : kArrowRegionSize + node->text_width() +
-                text_horizontal_padding_ * 2;
+          ? text_offset_ + node->text_width() + kTextHorizontalPadding * 2
+          : kArrowRegionSize + node->text_width() + kTextHorizontalPadding * 2;
 
   gfx::Rect rect(depth * kIndent + kHorizontalInset, row * row_height_, width,
                  row_height_);
