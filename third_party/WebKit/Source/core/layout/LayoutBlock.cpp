6e415a6aab93800aa57117b4503ceb06cda2f0e6
Mike Wittman <wittman@chromium.org>
2018-01-05 18:36:52 +0000

    Revert "Add implicit rootScroller feature"
    This reverts commit 635201011d3d7c4cccad0f31a571a153e3cf5694.
    
    Reason for revert: Suspected of causing WebKit Linux Trusty Leak webkit_layout_tests failures in virtual/android/fullscreen/video-scrolled-iframe.html
    
    08:04:30.229 24767 [1/1] virtual/android/fullscreen/video-scrolled-iframe.html failed unexpectedly (image diff)
    
    https://uberchromegw.corp.google.com/i/chromium.webkit/builders/WebKit%20Linux%20Trusty%20Leak/builds/13992
    
    
    Original change's description:
    > Add implicit rootScroller feature
    > 
    > This patch adds an "implicit rootScroller" run-time flag. When turned
    > on, Blink can designate an element to be the rootScroller without the
    > page manually doing so using the rootScroller API (i.e.
    > document.rootScroller = myElement).
    > 
    > Currently, this will happen when an element is viewport-filling (is a
    > valid rootScroller) and, in the case of non-iframes, is explicitly
    > scrollable, having either overflow: auto or overflow: scroll. If there
    > are multiple elements that meet these conditions, the one with the
    > higher z-index wins. In the case of equal z-index - no rootScroller is
    > set implicitly.
    > 
    > This patch extends RootScrollerController by tracking a set of
    > "implicit candidates". As layout is performed, elements that meet the
    > requirements are added to this list (this is expected to be small). At
    > the end of layout, when we compute the effective root scroller, we
    > first iterate this set and decide on an implicit root scroller. If the
    > page didn't set an explicit root scroller, we use the implicit root
    > scroller, as if it were set via document.rootScroller.
    > 
    > Bug: 798719
    > Change-Id: If6e68f9f17731c5cad7159e2df4284abeae43568
    > Reviewed-on: https://chromium-review.googlesource.com/841722
    > Reviewed-by: Steve Kobes <skobes@chromium.org>
    > Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    > Commit-Queue: David Bokan <bokan@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#527281}
    
    TBR=bokan@chromium.org,jbroman@chromium.org,skobes@chromium.org
    
    Change-Id: I55bd27f941120f16924e1d12459384078f4d3aba
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 798719
    Reviewed-on: https://chromium-review.googlesource.com/852794
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Commit-Queue: Mike Wittman <wittman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527330}

--- third_party/WebKit/Source/core/layout/LayoutBlock.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBlock.cpp
@@ -52,7 +52,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/layout/api/LineLayoutItem.h"
 #include "core/layout/line/InlineTextBox.h"
 #include "core/page/Page.h"
-#include "core/page/scrolling/RootScrollerController.h"
 #include "core/paint/BlockPaintInvalidator.h"
 #include "core/paint/BlockPainter.h"
 #include "core/paint/ObjectPaintInvalidator.h"
@@ -411,10 +410,6 @@ void LayoutBlock::RemoveLeftoverAnonymousBlock(LayoutBlock* child) {
 
 void LayoutBlock::UpdateAfterLayout() {
   InvalidateStickyConstraints();
-
-  if (RuntimeEnabledFeatures::ImplicitRootScrollerEnabled() && GetNode())
-    GetDocument().GetRootScrollerController().ConsiderForImplicit(*GetNode());
-
   LayoutBox::UpdateAfterLayout();
 }
 
--- third_party/WebKit/Source/core/layout/LayoutIFrame.cpp
+++ third_party/WebKit/Source/core/layout/LayoutIFrame.cpp
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/layout/LayoutIFrame.h"
 
 #include "core/layout/LayoutAnalyzer.h"
-#include "core/page/scrolling/RootScrollerController.h"
 
 namespace blink {
 
@@ -60,13 +59,6 @@ void LayoutIFrame::UpdateLayout() {
   UpdateAfterLayout();
 
   ClearNeedsLayout();
-}
-
-void LayoutIFrame::UpdateAfterLayout() {
-  if (RuntimeEnabledFeatures::ImplicitRootScrollerEnabled() && GetNode())
-    GetDocument().GetRootScrollerController().ConsiderForImplicit(*GetNode());
-
-  LayoutEmbeddedContent::UpdateAfterLayout();
 }
 
 }  // namespace blink
