61387eb76af68caff898ff36a1267f9db4347c83
Will Chen <chenwilliam@chromium.org>
2018-01-05 10:57:09 -0800

    Reland "DevTools: deflake http/tests/devtools/console/console-context-selector.js"
    Leaving it marked as a flaky test so we can see which platforms
    this test is flaky for.
    
    This is a reland of 10126b3ab2b3d9986dc1d3a6bae1d860eaf73034
    Original change's description:
    > DevTools: deflake http/tests/devtools/console/console-context-selector.js
    >
    > This test was flaky because it wasn't waiting for the execution context
    > flavor to change.
    >
    > Drive-by: remove unused lines of code.
    >
    > Bug: 785571
    > Change-Id: I00c65fb18150d4289e70aaa1bb0a825b8ba2260f
    > Reviewed-on: https://chromium-review.googlesource.com/851076
    > Reviewed-by: Erik Luo <luoe@chromium.org>
    > Commit-Queue: Will Chen <chenwilliam@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#527157}
    
    TBR=luoe@chromium.org
    
    Bug: 785571
    Change-Id: I22454ab91e4e58cfe7db72f01a5c4b7115248164
    Reviewed-on: https://chromium-review.googlesource.com/852892
    Reviewed-by: Will Chen <chenwilliam@chromium.org>
    Commit-Queue: Will Chen <chenwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527404}

--- third_party/WebKit/LayoutTests/http/tests/devtools/console/console-context-selector.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/console/console-context-selector.js
@@ -41,7 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   await new Promise(f => SourcesTestRunner.startDebuggerTest(f, true));
   await TestRunner.evaluateInPageAsync('setup()');
   var workerTarget = await TestRunner.waitForTarget(target => target.parentTarget() === TestRunner.mainTarget);
-  var workerExecutionContext = await TestRunner.waitForExecutionContext(workerTarget.model(SDK.RuntimeModel));
+  await TestRunner.waitForExecutionContext(workerTarget.model(SDK.RuntimeModel));
   dump();
 
   TestRunner.addResult('');
@@ -49,9 +49,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   UI.context.setFlavor(SDK.Target, workerTarget);
   dump();
 
-  var mainFrame = TestRunner.resourceTreeModel.mainFrame;
-  var mainExecutionContext =
-      TestRunner.runtimeModel.executionContexts().find(context => context.frameId === mainFrame.id);
   var childFrame =
       TestRunner.resourceTreeModel.frames().find(frame => frame !== TestRunner.resourceTreeModel.mainFrame);
   var childExecutionContext =
@@ -61,35 +58,54 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   UI.context.setFlavor(SDK.ExecutionContext, childExecutionContext);
   dump();
 
+  var handleExecutionContextFlavorChanged;
+  UI.context.addFlavorChangeListener(SDK.DebuggerModel.CallFrame, () => {
+    if (handleExecutionContextFlavorChanged)
+      handleExecutionContextFlavorChanged();
+    handleExecutionContextFlavorChanged = undefined;
+  });
+
+  function waitForExecutionContextFlavorChanged() {
+    return new Promise(fulfill => {
+      handleExecutionContextFlavorChanged = fulfill;
+    });
+  }
+
   TestRunner.evaluateInPage('pauseInMain()');
   await SourcesTestRunner.waitUntilPausedPromise();
+  await waitForExecutionContextFlavorChanged();
   TestRunner.addResult('');
   TestRunner.addResult('Paused in main');
   dump();
 
   await new Promise(f => SourcesTestRunner.resumeExecution(f));
+  await waitForExecutionContextFlavorChanged();
   TestRunner.addResult('');
   TestRunner.addResult('Resumed');
   dump();
 
   TestRunner.evaluateInPage('pauseInWorker()');
   await SourcesTestRunner.waitUntilPausedPromise();
+  await waitForExecutionContextFlavorChanged();
   TestRunner.addResult('');
   TestRunner.addResult('Paused in worker');
   dump();
 
   await new Promise(f => SourcesTestRunner.resumeExecution(f));
+  await waitForExecutionContextFlavorChanged();
   TestRunner.addResult('');
   TestRunner.addResult('Resumed');
   dump();
 
   TestRunner.evaluateInPage('pauseInIframe()');
   await SourcesTestRunner.waitUntilPausedPromise();
+  await waitForExecutionContextFlavorChanged();
   TestRunner.addResult('');
   TestRunner.addResult('Paused in iframe');
   dump();
 
   await new Promise(f => SourcesTestRunner.resumeExecution(f));
+  await waitForExecutionContextFlavorChanged();
   TestRunner.addResult('');
   TestRunner.addResult('Resumed');
   dump();
