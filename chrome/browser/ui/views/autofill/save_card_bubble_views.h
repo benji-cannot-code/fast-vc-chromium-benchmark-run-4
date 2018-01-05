8145474c614b1595c525b9f45f9f1eb6a8c870f6
Jared Saul <jsaul@google.com>
2018-01-05 14:17:22 -0800

    Add browsertests for SendDetectedValues experiment
    Bug: 789645, 791861
    Change-Id: I3bfa66427a58dd52c466a9dc9658c1830ebfa5e7
    Reviewed-on: https://chromium-review.googlesource.com/837155
    Commit-Queue: Jared Saul <jsaul@google.com>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527477}

--- chrome/browser/ui/views/autofill/save_card_bubble_views.h
+++ chrome/browser/ui/views/autofill/save_card_bubble_views.h
@@ -83,6 +83,10 @@ class SaveCardBubbleViews : public SaveCardBubbleView,
   views::View* GetFootnoteViewForTesting();
 
  private:
+  FRIEND_TEST_ALL_PREFIXES(
+      SaveCardBubbleViewsFullFormBrowserTest,
+      Upload_ClickingCloseClosesBubbleIfSecondaryUiMdExpOn);
+
   // The current step of the save card flow.  Accounts for:
   //  1) Local save vs. Upload save
   //  2) Upload save can have all information or be missing CVC
