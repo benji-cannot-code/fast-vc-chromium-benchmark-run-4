d084191fa8c903c98a28c57d35763a0d87d95800
Dave Tapuska <dtapuska@chromium.org>
2018-01-05 15:05:45 -0500

    Avoid shadowed parameters in media content/media files.
    BUG=794619
    
    Change-Id: I2e506128ebedb5e0bd0a10a7cbd29604a5cc6ebd
    Reviewed-on: https://chromium-review.googlesource.com/852640
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527407}

--- content/renderer/media/media_stream_constraints_util.cc
+++ content/renderer/media/media_stream_constraints_util.cc
@@ -31,9 +31,9 @@ bool ScanConstraintsForExactValue(const blink::WebMediaConstraints& constraints,
     return true;
   }
   for (const auto& advanced_constraint : constraints.Advanced()) {
-    const auto& the_field = advanced_constraint.*picker;
-    if (the_field.HasExact()) {
-      *value = the_field.Exact();
+    const auto& advanced_field = advanced_constraint.*picker;
+    if (advanced_field.HasExact()) {
+      *value = advanced_field.Exact();
       return true;
     }
   }
@@ -46,7 +46,6 @@ bool ScanConstraintsForMaxValue(const blink::WebMediaConstraints& constraints,
                                 T* value) {
   if (constraints.IsNull())
     return false;
-
   const auto& the_field = constraints.Basic().*picker;
   if (the_field.HasMax()) {
     *value = the_field.Max();
@@ -57,13 +56,13 @@ bool ScanConstraintsForMaxValue(const blink::WebMediaConstraints& constraints,
     return true;
   }
   for (const auto& advanced_constraint : constraints.Advanced()) {
-    const auto& the_field = advanced_constraint.*picker;
-    if (the_field.HasMax()) {
-      *value = the_field.Max();
+    const auto& advanced_field = advanced_constraint.*picker;
+    if (advanced_field.HasMax()) {
+      *value = advanced_field.Max();
       return true;
     }
-    if (the_field.HasExact()) {
-      *value = the_field.Exact();
+    if (advanced_field.HasExact()) {
+      *value = advanced_field.Exact();
       return true;
     }
   }
@@ -76,7 +75,6 @@ bool ScanConstraintsForMinValue(const blink::WebMediaConstraints& constraints,
                                 T* value) {
   if (constraints.IsNull())
     return false;
-
   const auto& the_field = constraints.Basic().*picker;
   if (the_field.HasMin()) {
     *value = the_field.Min();
@@ -87,13 +85,13 @@ bool ScanConstraintsForMinValue(const blink::WebMediaConstraints& constraints,
     return true;
   }
   for (const auto& advanced_constraint : constraints.Advanced()) {
-    const auto& the_field = advanced_constraint.*picker;
-    if (the_field.HasMin()) {
-      *value = the_field.Min();
+    const auto& advanced_field = advanced_constraint.*picker;
+    if (advanced_field.HasMin()) {
+      *value = advanced_field.Min();
       return true;
     }
-    if (the_field.HasExact()) {
-      *value = the_field.Exact();
+    if (advanced_field.HasExact()) {
+      *value = advanced_field.Exact();
       return true;
     }
   }
