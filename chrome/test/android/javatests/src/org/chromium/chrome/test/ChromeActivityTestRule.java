b62da89eac37cb21013d0cc6d2d0223f4adaf1d6
Eric Karl <ericrk@chromium.org>
2018-01-04 17:18:40 -0800

    Wait for interactability in loadUrl* Android test helpers
    Currently, when Chrome Android tests load a URL, they wait for the page
    load event as well as instrumentation idle sync. At this point, many
    tests immediately start interacting with the page. Unfortunately, this
    doesn't guarantee page interactability, as Android waits for any layout
    animations to complete before a tab becomes interactive. This can lead
    to timing issues (as experienced in
    https://chromium-review.googlesource.com/c/chromium/src/+/832959/24).
    
    This change adds a new call, ChromeTabUtils.waitForInteractable, which
    ensures that the tab is in a state where it can receive interactions.
    The loadUrl* functions are updated to wait for this signal before
    returning.
    
    Bug: 799256
    Change-Id: I1fa92dc825e13f13a6273311a215064a7cb5b3ef
    Reviewed-on: https://chromium-review.googlesource.com/851335
    Commit-Queue: Eric Karl <ericrk@chromium.org>
    Reviewed-by: Maria Khomenko <mariakhomenko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527339}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/ChromeActivityTestRule.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/ChromeActivityTestRule.java
@@ -275,6 +275,7 @@ public class ChromeActivityTestRule<T extends ChromeActivity> extends ActivityTe
                 });
             }
         }, secondsToWait);
+        ChromeTabUtils.waitForInteractable(tab);
         InstrumentationRegistry.getInstrumentation().waitForIdleSync();
         return result.get();
     }
@@ -330,6 +331,7 @@ public class ChromeActivityTestRule<T extends ChromeActivity> extends ActivityTe
             Assert.fail("Failed to create new tab");
         }
         ChromeTabUtils.waitForTabPageLoaded(tab, url);
+        ChromeTabUtils.waitForInteractable(tab);
         InstrumentationRegistry.getInstrumentation().waitForIdleSync();
         return tab;
     }
