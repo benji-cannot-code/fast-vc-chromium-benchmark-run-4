8e235d6e2dd558275c7e2bd865bda5339a4264c8
Kevin Bailey <krb@chromium.org>
2018-01-05 09:19:55 -0800

    In browser navigation, do browser and tab search simultaneously
    Previously, Browser::Navigate() first searched for a suitable browser,
    then separately searched for a suitable tab within the browser, all
    depending on the disposition.  Since a tab-searching disposition was
    added, we now choose a browser depending on the tab within it.  Thus it
    makes more sense to search for the browser and tab simultaneously,
    saving that work.
    
    Bug: 780835
    Change-Id: I60848fb0e725547ed55962faad07b45376d4e2fd
    Reviewed-on: https://chromium-review.googlesource.com/848745
    Commit-Queue: Kevin Bailey <krb@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527379}

--- chrome/browser/ui/browser_list.cc
+++ chrome/browser/ui/browser_list.cc
@@ -57,8 +57,7 @@ Browser* BrowserList::GetLastActive() const {
 
 // static
 BrowserList* BrowserList::GetInstance() {
-  BrowserList** list = NULL;
-  list = &instance_;
+  BrowserList** list = &instance_;
   if (!*list)
     *list = new BrowserList;
   return *list;
