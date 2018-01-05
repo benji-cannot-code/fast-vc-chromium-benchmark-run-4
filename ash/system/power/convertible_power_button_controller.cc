210096e00975abc89cb2f26ebf0875e8429d8575
MinChen <minch@chromium.org>
2018-01-05 14:04:55 -0800

    Rename TabletPowerButtonController to ConvertiblePowerButtonController.
    This is a leading cl for tablet/detachable power off menu.
    ConvertiblePowerButtonController will only help handle the power button
    behavior of convertible devices. Will create TabletPowerButtonController
    to help handle the power button controller for tablet/detachable only.
    
    Bug: 799524
    Change-Id: I3dea68ffb2ab6b6d2492b58dde7a166e916f35ea
    Reviewed-on: https://chromium-review.googlesource.com/852518
    Commit-Queue: min c <minch@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527470}

--- ash/system/power/convertible_power_button_controller.cc
+++ ash/system/power/convertible_power_button_controller.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "ash/system/power/tablet_power_button_controller.h"
+#include "ash/system/power/convertible_power_button_controller.h"
 
 #include "ash/accessibility/accessibility_delegate.h"
 #include "ash/public/cpp/ash_switches.h"
@@ -46,12 +46,13 @@ constexpr base::TimeDelta kIgnorePowerButtonAfterResumeDelay =
 
 }  // namespace
 
-constexpr base::TimeDelta TabletPowerButtonController::kScreenStateChangeDelay;
+constexpr base::TimeDelta
+    ConvertiblePowerButtonController::kScreenStateChangeDelay;
 
 constexpr base::TimeDelta
-    TabletPowerButtonController::kIgnoreRepeatedButtonUpDelay;
+    ConvertiblePowerButtonController::kIgnoreRepeatedButtonUpDelay;
 
-TabletPowerButtonController::TabletPowerButtonController(
+ConvertiblePowerButtonController::ConvertiblePowerButtonController(
     PowerButtonDisplayController* display_controller,
     base::TickClock* tick_clock)
     : lock_state_controller_(Shell::Get()->lock_state_controller()),
@@ -62,14 +63,14 @@ TabletPowerButtonController::TabletPowerButtonController(
   Shell::Get()->tablet_mode_controller()->AddObserver(this);
 }
 
-TabletPowerButtonController::~TabletPowerButtonController() {
+ConvertiblePowerButtonController::~ConvertiblePowerButtonController() {
   if (Shell::Get()->tablet_mode_controller())
     Shell::Get()->tablet_mode_controller()->RemoveObserver(this);
   chromeos::DBusThreadManager::Get()->GetPowerManagerClient()->RemoveObserver(
       this);
 }
 
-void TabletPowerButtonController::OnPowerButtonEvent(
+void ConvertiblePowerButtonController::OnPowerButtonEvent(
     bool down,
     const base::TimeTicks& timestamp) {
   if (down) {
@@ -120,43 +121,43 @@ void TabletPowerButtonController::OnPowerButtonEvent(
   }
 }
 
-void TabletPowerButtonController::SuspendDone(
+void ConvertiblePowerButtonController::SuspendDone(
     const base::TimeDelta& sleep_duration) {
   last_resume_time_ = tick_clock_->NowTicks();
 }
 
-void TabletPowerButtonController::OnTabletModeStarted() {
+void ConvertiblePowerButtonController::OnTabletModeStarted() {
   shutdown_timer_.Stop();
   if (lock_state_controller_->CanCancelShutdownAnimation())
     lock_state_controller_->CancelShutdownAnimation();
 }
 
-void TabletPowerButtonController::OnTabletModeEnded() {
+void ConvertiblePowerButtonController::OnTabletModeEnded() {
   shutdown_timer_.Stop();
   if (lock_state_controller_->CanCancelShutdownAnimation())
     lock_state_controller_->CancelShutdownAnimation();
 }
 
-void TabletPowerButtonController::CancelTabletPowerButton() {
+void ConvertiblePowerButtonController::CancelTabletPowerButton() {
   if (lock_state_controller_->CanCancelShutdownAnimation())
     lock_state_controller_->CancelShutdownAnimation();
   force_off_on_button_up_ = false;
   shutdown_timer_.Stop();
 }
 
-void TabletPowerButtonController::StartShutdownTimer() {
+void ConvertiblePowerButtonController::StartShutdownTimer() {
   base::TimeDelta timeout = screen_off_when_power_button_down_
                                 ? kShutdownWhenScreenOffTimeout
                                 : kShutdownWhenScreenOnTimeout;
   shutdown_timer_.Start(FROM_HERE, timeout, this,
-                        &TabletPowerButtonController::OnShutdownTimeout);
+                        &ConvertiblePowerButtonController::OnShutdownTimeout);
 }
 
-void TabletPowerButtonController::OnShutdownTimeout() {
+void ConvertiblePowerButtonController::OnShutdownTimeout() {
   lock_state_controller_->StartShutdownAnimation(ShutdownReason::POWER_BUTTON);
 }
 
-void TabletPowerButtonController::LockScreenIfRequired() {
+void ConvertiblePowerButtonController::LockScreenIfRequired() {
   SessionController* session_controller = Shell::Get()->session_controller();
   if (session_controller->ShouldLockScreenAutomatically() &&
       session_controller->CanLockScreen() &&
--- ash/system/power/convertible_power_button_controller.h
+++ ash/system/power/convertible_power_button_controller.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef ASH_SYSTEM_POWER_TABLET_POWER_BUTTON_CONTROLLER_H_
-#define ASH_SYSTEM_POWER_TABLET_POWER_BUTTON_CONTROLLER_H_
+#ifndef ASH_SYSTEM_POWER_CONVERTIBLE_POWER_BUTTON_CONTROLLER_H_
+#define ASH_SYSTEM_POWER_CONVERTIBLE_POWER_BUTTON_CONTROLLER_H_
 
 #include <memory>
 #include <utility>
@@ -29,9 +29,9 @@ namespace ash {
 class LockStateController;
 class PowerButtonDisplayController;
 
-// Handles power button events on convertible/tablet device. This class is
+// Handles power button events on convertible device. This class is
 // instantiated and used in PowerButtonController.
-class ASH_EXPORT TabletPowerButtonController
+class ASH_EXPORT ConvertiblePowerButtonController
     : public chromeos::PowerManagerClient::Observer,
       public TabletModeObserver {
  public:
@@ -49,9 +49,10 @@ class ASH_EXPORT TabletPowerButtonController
   static constexpr base::TimeDelta kIgnoreRepeatedButtonUpDelay =
       base::TimeDelta::FromMilliseconds(500);
 
-  TabletPowerButtonController(PowerButtonDisplayController* display_controller,
-                              base::TickClock* tick_clock);
-  ~TabletPowerButtonController() override;
+  ConvertiblePowerButtonController(
+      PowerButtonDisplayController* display_controller,
+      base::TickClock* tick_clock);
+  ~ConvertiblePowerButtonController() override;
 
   // Handles a power button event.
   void OnPowerButtonEvent(bool down, const base::TimeTicks& timestamp);
@@ -63,11 +64,11 @@ class ASH_EXPORT TabletPowerButtonController
   void OnTabletModeStarted() override;
   void OnTabletModeEnded() override;
 
-  // Cancel the ongoing tablet power button behavior.
+  // Cancel the ongoing power button behavior of convertible devices.
   void CancelTabletPowerButton();
 
  private:
-  friend class TabletPowerButtonControllerTestApi;
+  friend class ConvertiblePowerButtonControllerTestApi;
 
   // Starts |shutdown_timer_| when the power button is pressed while in
   // tablet mode.
@@ -93,7 +94,7 @@ class ASH_EXPORT TabletPowerButtonController
   // True if power button released should force off display.
   bool force_off_on_button_up_ = true;
 
-  // Started when the tablet power button is pressed and stopped when it's
+  // Started when the convertible power button is pressed and stopped when it's
   // released. Runs OnShutdownTimeout() to start shutdown.
   base::OneShotTimer shutdown_timer_;
 
@@ -105,9 +106,9 @@ class ASH_EXPORT TabletPowerButtonController
   // Time source for performed action times.
   base::TickClock* tick_clock_;  // Not owned.
 
-  DISALLOW_COPY_AND_ASSIGN(TabletPowerButtonController);
+  DISALLOW_COPY_AND_ASSIGN(ConvertiblePowerButtonController);
 };
 
 }  // namespace ash
 
-#endif  // ASH_SYSTEM_POWER_TABLET_POWER_BUTTON_CONTROLLER_H_
+#endif  // ASH_SYSTEM_POWER_CONVERTIBLE_POWER_BUTTON_CONTROLLER_H_
