fefeda6670bb7c13379ae8a9a6c02d6987e17a2c
Vladimir Levin <vmpstr@chromium.org>
2018-01-05 09:40:40 -0800

    oop: Serialize blur paint filter.
    This patch adds the paint filter serialization plumbing. Most functions
    still need to be implemented, but as a proof of concept, this serializes
    the blur filter.
    
    R=enne@chromium.org
    
    Bug: 777636
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I9ad8859ea266e7d4a005ca1d66c846e7631d7037
    Reviewed-on: https://chromium-review.googlesource.com/851269
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Reviewed-by: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527365}

--- cc/paint/paint_filter.h
+++ cc/paint/paint_filter.h
@@ -47,6 +47,9 @@ class FilterOperations;
 class CC_PAINT_EXPORT PaintFilter : public SkRefCnt {
  public:
   enum class Type : uint8_t {
+    // For serialization purposes, we reserve one enum to indicate that there
+    // was no PaintFilter, ie the filter is "null".
+    kNullFilter,
     kColorFilter,
     kBlur,
     kDropShadow,
@@ -73,6 +76,8 @@ class CC_PAINT_EXPORT PaintFilter : public SkRefCnt {
     kLightingDistant,
     kLightingPoint,
     kLightingSpot,
+    // Update the following if kLightingSpot is not the max anymore.
+    kMaxFilterType = kLightingSpot
   };
   enum class LightingType : uint8_t {
     kDiffuse,
@@ -163,6 +168,10 @@ class CC_PAINT_EXPORT BlurPaintFilter final : public PaintFilter {
   ~BlurPaintFilter() override;
 
   const sk_sp<PaintFilter>& input() const { return input_; }
+
+  SkScalar sigma_x() const { return sigma_x_; }
+  SkScalar sigma_y() const { return sigma_y_; }
+  TileMode tile_mode() const { return tile_mode_; }
 
  private:
   SkScalar sigma_x_;
