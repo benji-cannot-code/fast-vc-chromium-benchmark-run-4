1f689b0b65be7493c6672c1b35a10e835fba78bf
James Cook <jamescook@chromium.org>
2018-01-05 15:19:06 -0800

    cros: Tighten DEPS on ash in //chrome/browser/ui/webui
    For go/mustash code in //chrome cannot depend on //ash. Remove the
    "+ash" DEPS entry from //c/b/ui/webui and switch to per-file deps
    in the few remaining places.
    
    This will catch issues like the mash browser_tests failure in
    crbug.com/798797 at the checkdeps stage.
    
    Bug: 770866
    Test: buildtools/checkdeps/checkdeps.py
    Change-Id: I5d1a5687c7ea1885803fa5b1841d6f88b66ed3d8
    Reviewed-on: https://chromium-review.googlesource.com/853152
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527442}

--- chrome/browser/ui/webui/settings/md_settings_ui.cc
+++ chrome/browser/ui/webui/settings/md_settings_ui.cc
@@ -65,7 +65,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_CHROMEOS)
 #include "ash/public/cpp/stylus_utils.h"
-#include "ash/system/power/power_status.h"
 #include "chrome/browser/chromeos/arc/arc_util.h"
 #include "chrome/browser/chromeos/login/quick_unlock/quick_unlock_utils.h"
 #include "chrome/browser/ui/ash/ash_util.h"
