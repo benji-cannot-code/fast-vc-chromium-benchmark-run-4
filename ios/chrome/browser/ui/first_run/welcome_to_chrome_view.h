403b2e6fdcfc5aa4ee5e783dbd2fab6f2a942e99
Justin Cohen <justincohen@google.com>
2018-01-04 23:42:07 -0500

    [ios] Add privacy notice to first run.
    Bug: 786913
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I990d7642a757cca34edff502868750462150a4f9
    Reviewed-on: https://chromium-review.googlesource.com/822299
    Reviewed-by: Peter Lee <pkl@chromium.org>
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527336}

--- ios/chrome/browser/ui/first_run/welcome_to_chrome_view.h
+++ ios/chrome/browser/ui/first_run/welcome_to_chrome_view.h
@@ -14,7 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @protocol WelcomeToChromeViewDelegate<NSObject>
 
 // Called when the user taps on the "Terms of Service" link.
-- (void)welcomeToChromeViewDidTapTOSLink:(WelcomeToChromeView*)view;
+- (void)welcomeToChromeViewDidTapTOSLink;
+
+// Called when the user taps on the "Privacy Notice" link.
+- (void)welcomeToChromeViewDidTapPrivacyLink;
 
 // Called when the user taps the "Accept & Continue" button.
 - (void)welcomeToChromeViewDidTapOKButton:(WelcomeToChromeView*)view;
