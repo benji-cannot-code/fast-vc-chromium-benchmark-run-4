4d642f70d1585eb8fcb74ad43472c1f89304e76f
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-01-06 01:25:24 -0800

    Import wpt@56adc3d3119b1db0ce477cea67140d2b994077b6
    Using wpt-import in Chromium f73678bbce80c12199a4c20eb308b1b067fc1aa5.
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/9616
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    chongz@chromium.org:
      external/wpt/touch-events
    rego@igalia.com:
      external/wpt/css/selectors
    tyoshino@chromium.org, yhirano@chromium.org, mkwst@chromium.org, japhet@chromium.org:
      external/wpt/fetch
    
    TBR=leon.han@intel.com
    
    No-Export: true
    Change-Id: I476be6bf6f9b9d8a353478c7b905edc0fcab61f2
    Reviewed-on: https://chromium-review.googlesource.com/852700
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527517}

---
+++ third_party/WebKit/LayoutTests/external/wpt/fetch/api/request/destination/resources/fetch-destination-worker-no-load-event.js
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+self.addEventListener('fetch', function(event) {
+    if (event.request.url.includes('dummy')) {
+        event.waitUntil(async function() {
+            let destination = new URL(event.request.url).searchParams.get("dest");
+            let client = await self.clients.get(event.clientId);
+            if (event.request.destination == destination) {
+                client.postMessage("PASS");
+            } else {
+                client.postMessage("FAIL");
+            }
+        }())
+    }
+    event.respondWith(fetch(event.request));
+});
+
+
