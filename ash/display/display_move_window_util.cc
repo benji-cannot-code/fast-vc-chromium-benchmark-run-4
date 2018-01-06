c17677d6d82079184b1fa3217a1e884a61ab1ed9
Qiang Xu <warx@chromium.org>
2018-01-06 10:37:34 +0800

    cros: move first-non-transient transient-parent if transient
    changes:
    Transient child window could get activated. In this case, shortcut to
    move active window between displays will move its first-non-transient
    transient-parent window.
    
    move browser window that has permission prompt shown and activated.
    
    Bug: 778438
    Test: added test coverage. Also tested on device operating shortcuts to
    Change-Id: I5d24c2c8ecfc17c30b97fe16db025307c5b23810
    Reviewed-on: https://chromium-review.googlesource.com/848696
    Commit-Queue: Qiang(Joe) Xu <warx@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527498}

--- ash/display/display_move_window_util.cc
+++ ash/display/display_move_window_util.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/display/display.h"
 #include "ui/display/screen.h"
 #include "ui/display/types/display_constants.h"
+#include "ui/wm/core/window_util.h"
 
 namespace ash {
 
@@ -143,12 +144,20 @@ void HandleMoveActiveWindowToDisplay(DisplayMoveWindowDirection direction) {
   if (!window)
     return;
 
-  // When |window_list| is not empty, |window| can only be the first one of the
-  // fresh built list if it is in the list.
-  MruWindowTracker::WindowList window_list =
-      Shell::Get()->mru_window_tracker()->BuildWindowForCycleList();
-  if (window_list.empty() || window_list.front() != window)
-    return;
+  // If |window| is transient window, move its first non-transient
+  // transient-parent window instead. Otherwise, it should be the first one in
+  // window cycle list.
+  if (::wm::GetTransientParent(window)) {
+    while (::wm::GetTransientParent(window))
+      window = ::wm::GetTransientParent(window);
+    if (window == window->GetRootWindow())
+      return;
+  } else {
+    MruWindowTracker::WindowList window_list =
+        Shell::Get()->mru_window_tracker()->BuildWindowForCycleList();
+    if (window_list.empty() || window_list.front() != window)
+      return;
+  }
 
   display::Display origin_display =
       display::Screen::GetScreen()->GetDisplayNearestWindow(window);
