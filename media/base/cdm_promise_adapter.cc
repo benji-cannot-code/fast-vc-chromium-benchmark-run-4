457af0d95778c6c93dd291d1db7347fef2c7b300
Xiaohan Wang <xhwang@chromium.org>
2018-01-07 09:43:04 -0800

    media: Support async Initialize() in CDM_10
    - Roll CDM interface version.
    - Update Chromium to support the new async Initialize() call in CDM_10.
    - Also fix a static_cast bug in clear_key_cdm.cc.
    - Also add CdmPromiseAdapter::kInvalidPromiseId to help check the
      validity of promise IDs.
    
    BUG=733821,796345
    
    Change-Id: If5f1e9cb88378e8af7226e6ea8f9321577d38022
    Reviewed-on: https://chromium-review.googlesource.com/851339
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527847}

--- media/base/cdm_promise_adapter.cc
+++ media/base/cdm_promise_adapter.cc
@@ -9,8 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media {
 
-CdmPromiseAdapter::CdmPromiseAdapter() : next_promise_id_(1) {
-}
+CdmPromiseAdapter::CdmPromiseAdapter()
+    : next_promise_id_(kInvalidPromiseId + 1) {}
 
 CdmPromiseAdapter::~CdmPromiseAdapter() {
   DCHECK(promises_.empty());
@@ -20,7 +20,12 @@ CdmPromiseAdapter::~CdmPromiseAdapter() {
 
 uint32_t CdmPromiseAdapter::SavePromise(std::unique_ptr<CdmPromise> promise) {
   DCHECK(thread_checker_.CalledOnValidThread());
+  DCHECK_NE(kInvalidPromiseId, next_promise_id_);
+
   uint32_t promise_id = next_promise_id_++;
+  if (next_promise_id_ == kInvalidPromiseId)
+    next_promise_id_++;
+
   promises_[promise_id] = std::move(promise);
   return promise_id;
 }
