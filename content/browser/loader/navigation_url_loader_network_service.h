fb96bde55427d7dc8fb48020de06c6a0c166437e
arthursonzogni <arthursonzogni@chromium.org>
2018-01-08 17:38:49 +0100

    NavigationMojoResponse: Transmit missing data.
    It includes:
     * is_download
     * is_stream
     * request_id
     * navigation_data
    
    Bug: 705744
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo;master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: Idbe5c7049c07054a65ba9fbff695511cfe5403cf
    Reviewed-on: https://chromium-review.googlesource.com/765494
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527969}

--- content/browser/loader/navigation_url_loader_network_service.h
+++ content/browser/loader/navigation_url_loader_network_service.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
+#include "base/optional.h"
 #include "base/time/time.h"
 #include "content/browser/loader/navigation_url_loader.h"
 #include "content/public/browser/content_browser_client.h"
@@ -21,10 +22,12 @@ struct RedirectInfo;
 
 namespace content {
 
-class ResourceContext;
+class NavigationData;
 class NavigationPostDataHandler;
+class ResourceContext;
 class StoragePartition;
 class URLLoaderRequestHandler;
+struct GlobalRequestID;
 
 // This is an implementation of NavigationURLLoader used when
 // --enable-network-service is used.
@@ -49,9 +52,13 @@ class CONTENT_EXPORT NavigationURLLoaderNetworkService
   void ProceedWithResponse() override;
 
   void OnReceiveResponse(
-      mojom::URLLoaderClientEndpointsPtr url_loader_client_endpoints,
       scoped_refptr<ResourceResponse> response,
-      const base::Optional<net::SSLInfo>& ssl_info,
+      mojom::URLLoaderClientEndpointsPtr url_loader_client_endpoints,
+      const base::Optional<net::SSLInfo>& maybe_ssl_info,
+      std::unique_ptr<NavigationData> navigation_data,
+      const GlobalRequestID& global_request_id,
+      bool is_download,
+      bool is_stream,
       mojom::DownloadedTempFilePtr downloaded_file);
   void OnReceiveRedirect(const net::RedirectInfo& redirect_info,
                          scoped_refptr<ResourceResponse> response);
@@ -71,14 +78,6 @@ class CONTENT_EXPORT NavigationURLLoaderNetworkService
   std::unique_ptr<URLLoaderRequestController> request_controller_;
 
   bool allow_download_;
-
-  // If this request was triggered by an anchor tag with a download attribute,
-  // the |suggested_filename_| will be the (possibly empty) value of said
-  // attribute.
-  base::Optional<std::string> suggested_filename_;
-
-  // Current URL that is being navigated, updated after redirection.
-  GURL url_;
 
   // Factories to handle navigation requests for non-network resources.
   ContentBrowserClient::NonNetworkURLLoaderFactoryMap
