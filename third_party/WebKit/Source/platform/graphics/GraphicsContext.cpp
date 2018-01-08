f91554c03372741c132c1a7c74aad06ca475bb8e
Khushal <khushalsagar@chromium.org>
2018-01-08 14:02:10 -0800

    cc: Remove PaintFlags::getSkShader.
    R=chrishtr@chromium.org
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I633aebe968cc6b7eebc961f409ed54cc1a3165be
    Reviewed-on: https://chromium-review.googlesource.com/854987
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527816}

--- third_party/WebKit/Source/platform/graphics/GraphicsContext.cpp
+++ third_party/WebKit/Source/platform/graphics/GraphicsContext.cpp
@@ -1443,7 +1443,7 @@ PaintFlags GraphicsContext::ApplyHighContrastFilter(
   PaintFlags output;
   if (input)
     output = *input;
-  if (output.getSkShader()) {
+  if (output.HasShader()) {
     output.setColorFilter(high_contrast_filter_);
   } else {
     output.setColor(high_contrast_filter_->filterColor(output.getColor()));
