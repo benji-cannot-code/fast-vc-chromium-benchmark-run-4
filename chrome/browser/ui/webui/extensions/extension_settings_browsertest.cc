1576f2eff097c83527e8249b863b4de9cb8647fa
Hector Carmona <hcarmona@chromium.org>
2018-01-08 14:04:27 -0800

    MD Extensions: Error Console: Enable Automated A11y Audit.
    Bug: 799597
    Change-Id: I6697ab223d736c4271bf8b3f1ac3b2f22c4a2ce7
    Reviewed-on: https://chromium-review.googlesource.com/853163
    Commit-Queue: Hector Carmona <hcarmona@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527843}

--- chrome/browser/ui/webui/extensions/extension_settings_browsertest.cc
+++ chrome/browser/ui/webui/extensions/extension_settings_browsertest.cc
@@ -17,7 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/web_contents_sizer.h"
 #include "chrome/common/chrome_paths.h"
+#include "chrome/common/pref_names.h"
 #include "chrome/test/base/ui_test_utils.h"
+#include "components/prefs/pref_service.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test_utils.h"
@@ -89,6 +91,11 @@ void ExtensionSettingsUIBrowserTest::SetAutoConfirmUninstall() {
 void ExtensionSettingsUIBrowserTest::EnableErrorConsole() {
   error_console_override_.reset(new extensions::FeatureSwitch::ScopedOverride(
       extensions::FeatureSwitch::error_console(), true));
+}
+
+void ExtensionSettingsUIBrowserTest::SetDevModeEnabled(bool enabled) {
+  browser()->profile()->GetPrefs()->SetBoolean(
+      prefs::kExtensionsUIDeveloperMode, enabled);
 }
 
 void ExtensionSettingsUIBrowserTest::ShrinkWebContentsView() {
