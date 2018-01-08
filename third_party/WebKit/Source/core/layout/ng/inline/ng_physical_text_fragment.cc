2d31cbd9217934aca387ad60aead0560c27afa95
Xiaocheng Hu <xiaochengh@chromium.org>
2018-01-08 13:31:38 -0800

    [LayoutNG] Introduce NGPhysicalTextFragment::IsAnonymousText
    This patch introduce the function to tell whether an NGPhysicalTextFragment
    is due to generated text (from, e.g., a pseudo element) or DOM text node.
    
    This patch is split off from crrev.com/c/803015
    
    Bug: 771398
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: I69d053cf50493b3d1dd6e605d7bb68b90056fe98
    Reviewed-on: https://chromium-review.googlesource.com/854728
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527855}

--- third_party/WebKit/Source/core/layout/ng/inline/ng_physical_text_fragment.cc
+++ third_party/WebKit/Source/core/layout/ng/inline/ng_physical_text_fragment.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/layout/ng/inline/ng_physical_text_fragment.h"
 
+#include "core/dom/Node.h"
+#include "core/layout/LayoutTextFragment.h"
 #include "core/layout/ng/geometry/ng_physical_offset_rect.h"
 #include "core/layout/ng/inline/ng_line_height_metrics.h"
 #include "core/style/ComputedStyle.h"
@@ -74,6 +76,16 @@ bool NGPhysicalTextFragment::IsLineBreak() const {
   // TODO(xiaochengh): Introduce and set a text fragment type flag in fragment
   // builder, instead of check text content string.
   return Text() == "\n";
+}
+
+bool NGPhysicalTextFragment::IsAnonymousText() const {
+  // TODO(xiaochengh): Introduce and set a flag for anonymous text.
+  const LayoutObject* layout_object = GetLayoutObject();
+  if (layout_object && layout_object->IsText() &&
+      ToLayoutText(layout_object)->IsTextFragment())
+    return !ToLayoutTextFragment(layout_object)->AssociatedTextNode();
+  const Node* node = GetNode();
+  return !node || node->IsPseudoElement();
 }
 
 }  // namespace blink
