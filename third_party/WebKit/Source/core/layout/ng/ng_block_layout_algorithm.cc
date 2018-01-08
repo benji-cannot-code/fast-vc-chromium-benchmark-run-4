fc0f37f201c412974683210c6e884919ea98e549
Morten Stenshorne <mstensho@chromium.org>
2018-01-08 12:49:18 +0100

    [LayoutNG] Detect forced breaks even when out of space.
    Forced breaks need to be reported even if we're out of space, so check
    for forced breaks before we check whether we're out of space.
    
    Currently, being recognized as forced or not only makes a difference for
    column balancing, but it will also make a difference for margins at
    fragmentainer breaks, once we have implemented that correctly.
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: I7db3820124b25e91b3997e43fae17e7718047f80
    Reviewed-on: https://chromium-review.googlesource.com/853509
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527686}

--- third_party/WebKit/Source/core/layout/ng/ng_block_layout_algorithm.cc
+++ third_party/WebKit/Source/core/layout/ng/ng_block_layout_algorithm.cc
@@ -1406,13 +1406,6 @@ NGBlockLayoutAlgorithm::BreakType NGBlockLayoutAlgorithm::BreakTypeBeforeChild(
     return fragment.BlockSize() > space_left ? SoftBreak : NoBreak;
   }
 
-  // If the block offset is past the fragmentainer boundary (or exactly at the
-  // boundary), no part of the fragment is going to fit in the current
-  // fragmentainer. Fragments may be pushed past the fragmentainer boundary by
-  // margins.
-  if (space_left <= LayoutUnit())
-    return SoftBreak;
-
   EBreakBetween break_before = JoinFragmentainerBreakValues(
       child.Style().BreakBefore(), layout_result.InitialBreakBefore());
   EBreakBetween break_between =
@@ -1423,6 +1416,13 @@ NGBlockLayoutAlgorithm::BreakType NGBlockLayoutAlgorithm::BreakTypeBeforeChild(
     if (has_processed_first_child_)
       return ForcedBreak;
   }
+
+  // If the block offset is past the fragmentainer boundary (or exactly at the
+  // boundary), no part of the fragment is going to fit in the current
+  // fragmentainer. Fragments may be pushed past the fragmentainer boundary by
+  // margins.
+  if (space_left <= LayoutUnit())
+    return SoftBreak;
 
   const auto* token = physical_fragment.BreakToken();
   if (!token || token->IsFinished())
