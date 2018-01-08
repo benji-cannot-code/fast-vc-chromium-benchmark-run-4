45a226b62861f30e87a9efcd71d0819bbb91f5c6
Hidehiko Abe <hidehiko@chromium.org>
2018-01-08 18:38:16 +0900

    Implement conditional constexpr copy-/move- constructors.
    To catch up with C++17 spec, this implements conditional
    constexpr-ness for copy-/move- constructors.
    
    BUG=784732
    TEST=Ran trybot.
    
    Change-Id: I6a5ffdaf4dbe5ad5ff458494e07287d9e5fc0333
    Reviewed-on: https://chromium-review.googlesource.com/839842
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527672}

--- base/optional_unittest.cc
+++ base/optional_unittest.cc
@@ -152,8 +152,8 @@ TEST(OptionalTest, DefaultConstructor) {
 
 TEST(OptionalTest, CopyConstructor) {
   {
-    Optional<float> first(0.1f);
-    Optional<float> other(first);
+    constexpr Optional<float> first(0.1f);
+    constexpr Optional<float> other(first);
 
     EXPECT_TRUE(other);
     EXPECT_EQ(other.value(), 0.1f);
@@ -208,8 +208,8 @@ TEST(OptionalTest, ValueConstructor) {
 
 TEST(OptionalTest, MoveConstructor) {
   {
-    Optional<float> first(0.1f);
-    Optional<float> second(std::move(first));
+    constexpr Optional<float> first(0.1f);
+    constexpr Optional<float> second(std::move(first));
 
     EXPECT_TRUE(second);
     EXPECT_EQ(second.value(), 0.1f);
