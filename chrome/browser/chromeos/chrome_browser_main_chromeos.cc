68f7b5db218d606dcd508504736579cd6f0838ff
Steven Bennetts <stevenjb@chromium.org>
2018-01-08 09:39:58 -0700

    Move keyboard init to ChromeBrowserMainChromeOS
    ChromeBrowserMainPartsAsh should only contain initialization
    that depends on c/b/ui/ash.
    
    This also moves all calls to RootWindowController::ActivateKeyboard
    to ash::Shell.
    
    Bug: 796007
    Change-Id: I22704c765e1fa17b16b29aa995509d6e7cf44052
    Reviewed-on: https://chromium-review.googlesource.com/851134
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527662}

--- chrome/browser/chromeos/chrome_browser_main_chromeos.cc
+++ chrome/browser/chromeos/chrome_browser_main_chromeos.cc
@@ -195,6 +195,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/chromeos/events/event_rewriter_chromeos.h"
 #include "ui/chromeos/events/pref_names.h"
 #include "ui/events/event_utils.h"
+#include "ui/keyboard/content/keyboard.h"
 #include "ui/message_center/message_center.h"
 
 #if BUILDFLAG(ENABLE_RLZ)
@@ -856,8 +857,14 @@ void ChromeBrowserMainPartsChromeos::PreProfileInit() {
 
   arc_kiosk_app_manager_.reset(new ArcKioskAppManager());
 
-  // NOTE: Initializes ash::Shell.
+  // NOTE: Calls ChromeBrowserMainParts::PreProfileInit() which calls
+  // ChromeBrowserMainExtraPartsAsh::PreProfileInit() which initializes
+  // ash::Shell.
   ChromeBrowserMainPartsLinux::PreProfileInit();
+
+  // Initialize the keyboard before any session state changes (i.e. before
+  // loading the default profile).
+  keyboard::InitializeKeyboard();
 
   PushProcessCreationTimeToAsh();
 
