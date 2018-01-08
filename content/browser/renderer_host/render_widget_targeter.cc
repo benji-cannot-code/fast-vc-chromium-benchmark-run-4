a3a7a133809bb50a0b94001593411619c107f5a3
Sadrul Habib Chowdhury <sadrul@chromium.org>
2018-01-08 01:04:40 -0500

    oopif events: Fix a couple of issues with async event targeting.
    . The renderer already applies the device-scale-factor during hit-test.
      So avoid doing any scaling on the browser side.
    . When querying a nested client for targeting, make sure the location
      is correctly transformed to that view's coordinate space.
    
    BUG=797661, 793018
    
    Change-Id: I65d74ad49fb25d1a9cb05745e2a2f8d3ac26763e
    Reviewed-on: https://chromium-review.googlesource.com/848033
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: James MacLean <wjmaclean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527632}

--- content/browser/renderer_host/render_widget_targeter.cc
+++ content/browser/renderer_host/render_widget_targeter.cc
@@ -126,13 +126,10 @@ void RenderWidgetTargeter::QueryClient(
   if (mouse_event.GetType() == blink::WebInputEvent::kUndefined)
     return;
   request_in_flight_ = true;
-  float scale_factor = root_view->current_device_scale_factor();
   auto* target_client =
       target->GetRenderWidgetHostImpl()->input_target_client();
   target_client->FrameSinkIdAt(
-      gfx::ScaleToCeiledPoint(
-          gfx::ToCeiledPoint(mouse_event.PositionInWidget()), scale_factor,
-          scale_factor),
+      gfx::ToCeiledPoint(mouse_event.PositionInWidget()),
       base::BindOnce(&RenderWidgetTargeter::FoundFrameSinkId,
                      weak_ptr_factory_.GetWeakPtr(), root_view->GetWeakPtr(),
                      target->GetWeakPtr(),
@@ -170,7 +167,15 @@ void RenderWidgetTargeter::FoundFrameSinkId(
   if (view == target.get()) {
     FoundTarget(root_view.get(), view, event, latency, target_location);
   } else {
-    QueryClient(root_view.get(), view, event, latency, target_location);
+    base::Optional<gfx::PointF> location = target_location;
+    if (target_location) {
+      gfx::PointF updated_location = *target_location;
+      if (target->TransformPointToCoordSpaceForView(updated_location, view,
+                                                    &updated_location)) {
+        location.emplace(updated_location);
+      }
+    }
+    QueryClient(root_view.get(), view, event, latency, location);
   }
 }
 
