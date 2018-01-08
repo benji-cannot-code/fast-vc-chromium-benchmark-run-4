187fe32105cc0dda6ea3ee68293bc77982d0fa06
Tarun Bansal <tbansal@chromium.org>
2018-01-08 09:24:10 -0800

    Reland "Pass network quality estimator to warmup probe URL fetcher"
    Update the tests to ensure that the network
    quality estimator is set on the URLRequestContextGetter that
    is passed to the warmup URL fetcher. This would be used
    in a subsequent CL to compute the fetcher timeout.
    
    Bug: 760294
    Change-Id: Icacf6a68a616a7da7c6f729028f1205d2fb3532f
    Reviewed-on: https://chromium-review.googlesource.com/854477
    Reviewed-by: Doug Arnett <dougarnett@chromium.org>
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527713}

--- components/data_reduction_proxy/core/browser/warmup_url_fetcher.cc
+++ components/data_reduction_proxy/core/browser/warmup_url_fetcher.cc
@@ -17,8 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/data_use_measurement/core/data_use_user_data.h"
 #include "net/base/load_flags.h"
 #include "net/http/http_status_code.h"
+#include "net/nqe/network_quality_estimator.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
 #include "net/url_request/url_fetcher.h"
+#include "net/url_request/url_request_context.h"
 #include "net/url_request/url_request_context_getter.h"
 #include "net/url_request/url_request_status.h"
 
@@ -32,6 +34,8 @@ WarmupURLFetcher::WarmupURLFetcher(
       is_fetch_in_flight_(false),
       callback_(callback) {
   DCHECK(url_request_context_getter_);
+  DCHECK(url_request_context_getter_->GetURLRequestContext()
+             ->network_quality_estimator());
 }
 
 WarmupURLFetcher::~WarmupURLFetcher() {}
@@ -128,6 +132,8 @@ void WarmupURLFetcher::GetWarmupURLWithQueryParam(
 
 void WarmupURLFetcher::OnURLFetchComplete(const net::URLFetcher* source) {
   DCHECK_EQ(source, fetcher_.get());
+  DCHECK(is_fetch_in_flight_);
+
   is_fetch_in_flight_ = false;
   UMA_HISTOGRAM_BOOLEAN(
       "DataReductionProxy.WarmupURL.FetchSuccessful",
