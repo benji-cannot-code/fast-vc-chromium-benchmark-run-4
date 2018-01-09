e8e96b865a997ca67bf931aa436d442e275d9a19
lgcheng <lgcheng@google.com>
2018-01-08 18:59:46 -0800

    Hide Arc USB host features behind flag.
    Hide Arc USB host features behind a flag. Android side always gets empty
    device list when the flag is disabled.
    
    Bug: 776476
    Test: Manual
    Change-Id: I0e6aeea60e241dc9c5b4ae1f035cfe2fd064ca9d
    Reviewed-on: https://chromium-review.googlesource.com/854990
    Commit-Queue: Long Cheng <lgcheng@google.com>
    Reviewed-by: Luis Hector Chavez <lhchavez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527893}

--- components/arc/usb/usb_host_bridge.cc
+++ components/arc/usb/usb_host_bridge.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/dbus/permission_broker_client.h"
 #include "components/arc/arc_bridge_service.h"
 #include "components/arc/arc_browser_context_keyed_service_factory_base.h"
+#include "components/arc/arc_features.h"
 #include "device/base/device_client.h"
 #include "device/usb/mojo/type_converters.h"
 #include "device/usb/usb_device_handle.h"
@@ -230,6 +231,11 @@ void ArcUsbHostBridge::OnConnectionReady() {
 }
 
 void ArcUsbHostBridge::OnDeviceChecked(const std::string& guid, bool allowed) {
+  if (!base::FeatureList::IsEnabled(arc::kUsbHostFeature)) {
+    VLOG(1) << "AndroidUSBHost: feature is disabled; ignoring";
+    return;
+  }
+
   if (!allowed)
     return;
 
