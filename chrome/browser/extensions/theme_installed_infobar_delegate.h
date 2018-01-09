3065e0fd87453de6b3e5474451905da0dceb756b
Peter Kasting <pkasting@chromium.org>
2018-01-09 14:01:49 -0800

    Test ThemeInstalledInfoBarDelegate.
    BUG=686285
    TEST=none
    
    Change-Id: I8111f658e3736ab1b21896b07b994d4b2916902d
    Reviewed-on: https://chromium-review.googlesource.com/853156
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528154}

--- chrome/browser/extensions/theme_installed_infobar_delegate.h
+++ chrome/browser/extensions/theme_installed_infobar_delegate.h
@@ -12,31 +12,32 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/infobars/core/confirm_infobar_delegate.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
+#include "extensions/common/extension_id.h"
 
 class ExtensionService;
-class Profile;
+class InfoBarService;
 class ThemeService;
-
-namespace extensions {
-class Extension;
-}
 
 // When a user installs a theme, we display it immediately, but provide an
 // infobar allowing them to cancel.
 class ThemeInstalledInfoBarDelegate : public ConfirmInfoBarDelegate,
                                       public content::NotificationObserver {
  public:
-  // Creates a theme installed infobar and delegate and adds the infobar to the
-  // last active tab on |profile|.
-  static void Create(const extensions::Extension* new_theme,
-                     Profile* profile,
+  // Creates a theme installed infobar and delegate and adds the infobar to
+  // |infobar_service|, replacing any previous theme infobar.
+  static void Create(InfoBarService* infobar_service,
+                     ExtensionService* extension_service,
+                     ThemeService* theme_service,
+                     const std::string& theme_name,
+                     const std::string& theme_id,
                      const std::string& previous_theme_id,
                      bool previous_using_system_theme);
 
  private:
   ThemeInstalledInfoBarDelegate(ExtensionService* extension_service,
                                 ThemeService* theme_service,
-                                const extensions::Extension* new_theme,
+                                const std::string& theme_name,
+                                const std::string& theme_id,
                                 const std::string& previous_theme_id,
                                 bool previous_using_system_theme);
   ~ThemeInstalledInfoBarDelegate() override;
@@ -60,7 +61,7 @@ class ThemeInstalledInfoBarDelegate : public ConfirmInfoBarDelegate,
   ThemeService* theme_service_;
 
   // Name of theme that's just been installed.
-  std::string name_;
+  std::string theme_name_;
 
   // ID of theme that's just been installed.
   std::string theme_id_;
--- chrome/browser/themes/theme_service.cc
+++ chrome/browser/themes/theme_service.cc
@@ -24,12 +24,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/theme_installed_infobar_delegate.h"
+#include "chrome/browser/infobars/infobar_service.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/themes/browser_theme_pack.h"
 #include "chrome/browser/themes/custom_theme_supplier.h"
 #include "chrome/browser/themes/theme_properties.h"
 #include "chrome/browser/themes/theme_service_factory.h"
 #include "chrome/browser/themes/theme_syncable_service.h"
+#include "chrome/browser/ui/browser_finder.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/features.h"
 #include "chrome/common/pref_names.h"
@@ -918,9 +920,22 @@ void ThemeService::OnThemeBuiltFromExtension(
   }
 
   // Offer to revert to the old theme.
-  if (can_revert_theme && !suppress_infobar) {
-    ThemeInstalledInfoBarDelegate::Create(
-        extension, profile_, previous_theme_id, previous_using_system_theme);
+  if (can_revert_theme && !suppress_infobar && extension->is_theme()) {
+    // FindTabbedBrowser() is called with |match_original_profiles| true because
+    // a theme install in either a normal or incognito window for a profile
+    // affects all normal and incognito windows for that profile.
+    Browser* browser = chrome::FindTabbedBrowser(profile_, true);
+    if (browser) {
+      content::WebContents* web_contents =
+          browser->tab_strip_model()->GetActiveWebContents();
+      if (web_contents) {
+        ThemeInstalledInfoBarDelegate::Create(
+            InfoBarService::FromWebContents(web_contents),
+            extensions::ExtensionSystem::Get(profile_)->extension_service(),
+            ThemeServiceFactory::GetForProfile(profile_), extension->name(),
+            extension->id(), previous_theme_id, previous_using_system_theme);
+      }
+    }
   }
   building_extension_id_.clear();
 }
