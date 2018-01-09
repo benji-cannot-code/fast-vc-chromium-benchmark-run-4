b3209917f11d268e57ae00af73113b8a628402ac
Jared Saul <jsaul@google.com>
2018-01-09 15:17:01 -0800

    [Autofill] Remove fake geolocation code from SaveCardBubbleViewsBrowserTest
    Bug: 772483, 791155
    Change-Id: I31e3043f5c2f8d4b861171e0a800494a123e7c19
    Reviewed-on: https://chromium-review.googlesource.com/853085
    Commit-Queue: Jared Saul <jsaul@google.com>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: anthonyvd <anthonyvd@chromium.org>
    Reviewed-by: Mathieu Perreault <mathp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528177}

--- chrome/browser/ui/autofill/chrome_autofill_client.h
+++ chrome/browser/ui/autofill/chrome_autofill_client.h
@@ -30,10 +30,6 @@ namespace content {
 class WebContents;
 }
 
-namespace service_manager {
-class Connector;
-}
-
 namespace autofill {
 
 class AutofillPopupControllerImpl;
@@ -116,7 +112,6 @@ class ChromeAutofillClient
 #endif  // !defined(OS_ANDROID)
 
  private:
-  friend class SaveCardBubbleViewsBrowserTestBase;
   friend class content::WebContentsUserData<ChromeAutofillClient>;
 
   explicit ChromeAutofillClient(content::WebContents* web_contents);
@@ -128,13 +123,6 @@ class ChromeAutofillClient
 
   // The identity provider, used for Payments integration.
   std::unique_ptr<IdentityProvider> identity_provider_;
-
-  // An optional connector to expose test services. For example, if set, this
-  // connector is used when loading risk data. The test class that sets this
-  // pointer is responsible for the ownership of its object.
-  // TODO(crbug.com/791155): Necessary for testing due to leaks in the
-  // geolocation setup code when running browsertests.
-  service_manager::Connector* connector_ = nullptr;
 
   DISALLOW_COPY_AND_ASSIGN(ChromeAutofillClient);
 };
