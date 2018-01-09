97c0c812ccc5e91bdcac5f6b16b89173f2484ed7
Miguel Casas <mcasas@chromium.org>
2018-01-09 14:17:36 -0500

    GpuVideoAcceleratorFactories: add HDR indication and wire it
    This CL adds a new method to GpuVideoAcceleratorFactories to
    set HDR rendering enabled (or not), and pings it from
    RenderThreadImpl on both creation and ScreenInfo update.
    
    RenderThreadImpl, in turn, gets pinged by RenderWidget::Resize(),
    which is actually pinged every time the Renderer sees a change
    in parameters, e.g. a pure resize or a new Screen plugged in
    causing the compositor to change some configuration.
    
    This HDR rendering indication cannot be part of GpuInfo nor
    gpu::Capabilities because it depends on the actual setting of
    the screen and/or hot plug events.
    
    This CL precedes crrev.com/c/843132, where a 16to8 bpc conversion
    will happen during copy to GpuMemoryBuffers if HDR is not active
    (Factories will test |is_hdr_rendering_enabled_|)
    
    Bug: 798485
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I2d30119ed92c69bbbfca9487e72dda62c02d7c3b
    Reviewed-on: https://chromium-review.googlesource.com/852912
    Reviewed-by: Fredrik Hubinette <hubbe@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528093}

--- content/renderer/media/gpu/gpu_video_accelerator_factories_impl.h
+++ content/renderer/media/gpu/gpu_video_accelerator_factories_impl.h
@@ -103,6 +103,8 @@ class CONTENT_EXPORT GpuVideoAcceleratorFactoriesImpl
 
   viz::ContextProvider* GetMediaContextProvider() override;
 
+  void SetRenderingColorSpace(const gfx::ColorSpace& color_space) override;
+
   void ReleaseContextProvider();
   scoped_refptr<ui::ContextProviderCommandBuffer> ContextProviderMainThread();
 
@@ -139,6 +141,8 @@ class CONTENT_EXPORT GpuVideoAcceleratorFactoriesImpl
   bool enable_gpu_memory_buffer_video_frames_;
   // Whether video acceleration encoding/decoding should be enabled.
   const bool video_accelerator_enabled_;
+
+  gfx::ColorSpace rendering_color_space_;
 
   gpu::GpuMemoryBufferManager* const gpu_memory_buffer_manager_;
 
