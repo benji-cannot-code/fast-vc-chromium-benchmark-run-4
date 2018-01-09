0521e9ba15baceca2a7f5f9850ee7f0aa3b65825
Lei Zhang <thestig@chromium.org>
2018-01-09 00:25:34 -0800

    Modernize PrintJobManager code.
    Change-Id: I3b9ea563c32946375667a088d9def1adb2a80521
    Reviewed-on: https://chromium-review.googlesource.com/856258
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528069}

--- chrome/browser/printing/print_job_manager.h
+++ chrome/browser/printing/print_job_manager.h
@@ -44,11 +44,11 @@ class PrintQueriesQueue : public base::RefCountedThreadSafe<PrintQueriesQueue> {
 
  private:
   friend class base::RefCountedThreadSafe<PrintQueriesQueue>;
-  typedef std::vector<scoped_refptr<PrinterQuery> > PrinterQueries;
+  using PrinterQueries = std::vector<scoped_refptr<PrinterQuery>>;
 
   virtual ~PrintQueriesQueue();
 
-  // Used to serialize access to queued_workers_.
+  // Used to serialize access to |queued_queries_|.
   base::Lock lock_;
 
   PrinterQueries queued_queries_;
@@ -74,7 +74,7 @@ class PrintJobManager : public content::NotificationObserver {
   scoped_refptr<PrintQueriesQueue> queue();
 
  private:
-  typedef std::set<scoped_refptr<PrintJob> > PrintJobs;
+  using PrintJobs = std::set<scoped_refptr<PrintJob>>;
 
   // Processes a NOTIFY_PRINT_JOB_EVENT notification.
   void OnPrintJobEvent(PrintJob* print_job,
@@ -91,7 +91,7 @@ class PrintJobManager : public content::NotificationObserver {
 
   scoped_refptr<PrintQueriesQueue> queue_;
 
-  bool is_shutdown_;
+  bool is_shutdown_ = false;
 
   DISALLOW_COPY_AND_ASSIGN(PrintJobManager);
 };
