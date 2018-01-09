ea0edf960649785a2b669e9a4c678a426b436c7c
Parastoo Geranmayeh <parastoog@google.com>
2018-01-09 12:49:05 -0500

    [Autofill] Fix the credit card section problem
    Problem:
    
    In some forms, there are hidden fields, which are the recurrence
    of others, like having more than one expiration month.
    This causes some problems when we are identifying the sections of
    the form, because our heuristics assume one of a kind for each
    section (one exp month  for one section, the second for another one.)
    
    Solution:
    
    Change the heuristics so that we assume only one credit card section
    for each form, this way, all credit card fields will be in a single
    section.
    
    Fixes: freepeople.com (Desktop), Home Depot (Desktop),
           ysl.com (Desktop), and etc.
    
    Bug: 793408
    Change-Id: Ic2987a38efa2bd3db1c8ce7c768e86e76481212b
    Reviewed-on: https://chromium-review.googlesource.com/854772
    Commit-Queue: Parastoo Geranmayeh <parastoog@google.com>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528113}

--- components/autofill/content/renderer/form_autofill_util.cc
+++ components/autofill/content/renderer/form_autofill_util.cc
@@ -804,7 +804,6 @@ void ForEachMatchingFormFieldCommon(
         // skipped.
         (IsAutofillableInputElement(input_element) ||
          IsTextAreaElement(*element)) &&
-        !element->Value().IsEmpty() &&
         !SanitizedFieldIsEmpty(element->Value().Utf16()) &&
         (!element->HasAttribute(kValue) ||
          element->GetAttribute(kValue) != element->Value()) &&
