a469b7857bc03009204080446e1eee38f3f1b688
Jeremy Roman <jbroman@chromium.org>
2018-01-08 19:56:21 -0500

    Replace WTF::WrapArrayUnique(new T[size]) with std::make_unique<T[]>(size).
    This is the standard pattern for this. Since there are no other uses of
    WrapArrayUnique (and base hasn't needed a similar function), remove
    WTF::WrapArrayUnique altogether.
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ib2ed9e416f279890a2051990b7ef0c16a42587b4
    Reviewed-on: https://chromium-review.googlesource.com/855053
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Yuta Kitamura <yutak@chromium.org>
    Reviewed-by: Yuta Kitamura <yutak@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527873}

--- third_party/WebKit/Source/platform/text/TextRun.cpp
+++ third_party/WebKit/Source/platform/text/TextRun.cpp
@@ -69,7 +69,7 @@ std::unique_ptr<UChar[]> TextRun::NormalizedUTF16(
     source = Characters16();
   }
 
-  UChar* buffer = new UChar[len_ + 1];
+  auto buffer = std::make_unique<UChar[]>(len_ + 1);
   *result_length = 0;
 
   bool error = false;
@@ -98,7 +98,7 @@ std::unique_ptr<UChar[]> TextRun::NormalizedUTF16(
   }
 
   DCHECK(*result_length <= len_);
-  return WrapArrayUnique(buffer);
+  return buffer;
 }
 
 unsigned TextRun::IndexOfSubRun(const TextRun& sub_run) const {
