e714fb3b20e6014286b1268acbefbfae05a19f25
Kevin Marshall <kmarshall@chromium.org>
2018-01-09 11:23:38 -0800

    Initial implementation of new SSH-based Fuchsia runner libraries.
    The new runner is based heavily around the use of SSH and SCP.
    Rather than build large boot filesystems with Chrome or test binaries,
    as we do now, we will instantiate a basic Fuchsia instance and use
    SSH for our binary deployment, execution, and log extraction needs.
    It can also be used as a layer to facilitate connections between the
    client and server portions of the Blink layout test system.
    
    This CL defines the abstraction layer for connecting and interacting
    with a Fuchsia target, and implementation code for using it on QEMU.
    
    
    Bug: 799309
    Change-Id: Ia2613c36d1a6f10457aa9c90435d4c95f9721e6c
    Reviewed-on: https://chromium-review.googlesource.com/851141
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528179}

---
+++ build/fuchsia/runner_v2/common.py
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+
+DIR_SOURCE_ROOT = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, os.pardir))
+SDK_ROOT = os.path.join(DIR_SOURCE_ROOT, 'third_party', 'fuchsia-sdk')
+
