ccb6df6597cf19a271f4a3520b97a78452e6aaa5
Takashi Toyoshima <toyoshim@chromium.org>
2018-01-09 16:04:04 +0900

    Web MIDI: remove dynamic instantiation field study code
    This patch removes dynamic instantation mode flag from the
    MidiService implementation, and adopts dynamic instantiation
    mode for all existing unit tests.
    
    This patch tries to minimize production code changes other than
    just removing the flag so that we should not overlook wrong
    code changes during modifying unit tests.
    
    Follow-up changes will remove unnecessary structures and
    threading support code from MidiService and MidiManager.
    
    Bug: 672793
    Change-Id: Ife34c9cb742a974f6e559605003c13d020b4a091
    Reviewed-on: https://chromium-review.googlesource.com/822630
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527937}

--- media/midi/midi_manager_usb_unittest.cc
+++ media/midi/midi_manager_usb_unittest.cc
@@ -228,7 +228,7 @@ class MidiManagerUsbTest : public ::testing::Test {
     service_ = std::make_unique<MidiService>(std::move(factory));
   }
   ~MidiManagerUsbTest() override {
-    manager()->Shutdown();
+    service_->Shutdown();
     base::RunLoop run_loop;
     run_loop.RunUntilIdle();
 
@@ -241,10 +241,10 @@ class MidiManagerUsbTest : public ::testing::Test {
  protected:
   void Initialize() {
     client_.reset(new FakeMidiManagerClient(&logger_));
-    manager()->StartSession(client_.get());
+    service_->StartSession(client_.get());
   }
 
-  void Finalize() { manager()->EndSession(client_.get()); }
+  void Finalize() { service_->EndSession(client_.get()); }
 
   bool IsInitializationCallbackInvoked() {
     return client_->complete_start_session_;
