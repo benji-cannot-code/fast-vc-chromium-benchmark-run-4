a9fbbe78749721ccb701795220282b1562d35919
Matt Menke <mmenke@chromium.org>
2018-01-09 12:52:26 -0500

    Make requests to fetch PAC scripts bypass extensions.
    Bug: 794674
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I2c51dc0281452d9739cc1637555c7008438577db
    Reviewed-on: https://chromium-review.googlesource.com/847936
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Eric Roman <eroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528077}

--- chrome/test/data/extensions/api_test/webrequest_clients_google_com/background.js
+++ chrome/test/data/extensions/api_test/webrequest_clients_google_com/background.js
@@ -7,6 +7,6 @@ window.webRequestCount = 0;
 
 chrome.webRequest.onBeforeRequest.addListener(function(details) {
   ++window.webRequestCount;
-}, {urls:['https://clients1.google.com/']});
+}, {urls: ['https://clients1.google.com/']});
 
 chrome.test.sendMessage('ready');
