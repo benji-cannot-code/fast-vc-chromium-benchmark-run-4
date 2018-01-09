635861adf24e9cb1e1100ef3be98ce058ce278b0
Colin Blundell <blundell@chromium.org>
2018-01-09 11:34:07 +0100

    Have PrimaryAccountAccessTokenFetcher post task via WeakPtr
    https://chromium-review.googlesource.com/c/chromium/src/+/839681
    changed PrimaryAccountAccessTokenFetcher to make access token requests
    to PO2TS via posting a task rather than a synchronous call. However,
    that CL neglected a corner case: PrimaryAccountAccessTokenFetcher can
    die before its posted task (which is to an instance method) executes.
    In that case, the instance method will have nondeterministic behavior
    when it runs, since it is running after the instance was destroyed.
    
    This CL has PrimaryAccountAccessTokenFetcher supply a WeakPtr rather
    than a raw pointer to |this| when posting the task. The CL also adds
    a unittest that crashes without this change.
    
    Bug: 800263
    Change-Id: I8c54a55da77ff3e607bd6789a49bac9210847e09
    Reviewed-on: https://chromium-review.googlesource.com/856557
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527975}

--- services/identity/public/cpp/primary_account_access_token_fetcher.h
+++ services/identity/public/cpp/primary_account_access_token_fetcher.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/macros.h"
+#include "base/memory/weak_ptr.h"
 #include "components/signin/core/browser/signin_manager_base.h"
 #include "google_apis/gaia/google_service_auth_error.h"
 #include "google_apis/gaia/oauth2_token_service.h"
@@ -93,6 +94,8 @@ class PrimaryAccountAccessTokenFetcher : public SigninManagerBase::Observer,
   bool access_token_retried_;
 
   Mode mode_;
+
+  base::WeakPtrFactory<PrimaryAccountAccessTokenFetcher> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(PrimaryAccountAccessTokenFetcher);
 };
