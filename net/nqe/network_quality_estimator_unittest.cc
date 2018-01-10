a4c8053d4cca8c2f2f443e7e93500f1462dca669
Tarun Bansal <tbansal@chromium.org>
2018-01-10 18:00:06 +0000

    Use transport RTT as a lower bound for HTTP RTT.
    In network quality estimator, use the transport RTT as a lower
    bound for the HTTP RTT.
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester
    Bug: 753436
    Change-Id: Iec080cb49429436cdac8870ee3c1711faa933a09
    Reviewed-on: https://chromium-review.googlesource.com/858295
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528352}

--- net/nqe/network_quality_estimator_unittest.cc
+++ net/nqe/network_quality_estimator_unittest.cc
@@ -1785,7 +1785,7 @@ TEST(NetworkQualityEstimatorTest, TestTransportRttUsedForHttpRttComputation) {
       {
           base::TimeDelta::FromMilliseconds(100),
           base::TimeDelta::FromMilliseconds(200), "", "",
-          base::TimeDelta::FromMilliseconds(100), EFFECTIVE_CONNECTION_TYPE_4G,
+          base::TimeDelta::FromMilliseconds(200), EFFECTIVE_CONNECTION_TYPE_4G,
       },
       {
           base::TimeDelta::FromMilliseconds(100),
@@ -1835,12 +1835,13 @@ TEST(NetworkQualityEstimatorTest, TestTransportRttUsedForHttpRttComputation) {
       {
           base::TimeDelta::FromMilliseconds(100),
           base::TimeDelta::FromMilliseconds(200), "foobar", "",
-          base::TimeDelta::FromMilliseconds(100), EFFECTIVE_CONNECTION_TYPE_4G,
+          base::TimeDelta::FromMilliseconds(200), EFFECTIVE_CONNECTION_TYPE_4G,
       },
       {
           base::TimeDelta::FromMilliseconds(100),
           base::TimeDelta::FromMilliseconds(4000), "", "",
-          base::TimeDelta::FromMilliseconds(100), EFFECTIVE_CONNECTION_TYPE_4G,
+          base::TimeDelta::FromMilliseconds(4000),
+          EFFECTIVE_CONNECTION_TYPE_SLOW_2G,
       },
       {
           base::TimeDelta::FromMilliseconds(100),
