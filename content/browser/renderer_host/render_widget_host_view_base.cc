a15db5d01514bf49522b2b2b85f835e7bb6e0fc3
Sadrul Habib Chowdhury <sadrul@chromium.org>
2018-01-10 19:45:03 +0000

    oopif events: Improve some of the hit-testing related tests.
    . Code unification for some tests that involve dispatching a mouse-down
      event to a view at a certain location, and validating that the event
      reaches the desired target view at the desired location.
    . Add a browser-test for layer squashing: if layer-squashing causes parts
      of a layer in the embedder to overlap the oopif, the events on top of
      that layer should still reach the oopif.
    . Add a browser-test for watermarking: if a translucent div on the main
      page is overlapping the oopif, with pointer-events: none, then the
      oopif should receive events correctly.
    
    BUG=796651
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Idbc2bc793584874d4fc8aebca78e32669a492826
    Reviewed-on: https://chromium-review.googlesource.com/853699
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: James MacLean <wjmaclean@chromium.org>
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528387}

--- content/browser/renderer_host/render_widget_host_view_base.cc
+++ content/browser/renderer_host/render_widget_host_view_base.cc
@@ -453,7 +453,7 @@ viz::FrameSinkId RenderWidgetHostViewBase::FrameSinkIdAtPoint(
       gfx::ConvertPointToPixel(device_scale_factor, point);
   viz::SurfaceId surface_id = GetCurrentSurfaceId();
   if (!surface_id.is_valid()) {
-    return viz::FrameSinkId();
+    return GetFrameSinkId();
   }
   viz::SurfaceHittest hittest(delegate,
                               GetFrameSinkManager()->surface_manager());
