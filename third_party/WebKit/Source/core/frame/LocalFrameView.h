754c35e001df36016214884f799d84889b5b5556
Joel Hockey <joelhockey@chromium.org>
2018-01-09 16:23:00 -0800

    Remove unused ListHashSet import in LocalFrameView
    Bug: 614112
    Change-Id: I4c7904d7f3a69349723209e1e228af727d092498
    Reviewed-on: https://chromium-review.googlesource.com/857360
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528204}

--- third_party/WebKit/Source/core/frame/LocalFrameView.h
+++ third_party/WebKit/Source/core/frame/LocalFrameView.h
@@ -59,7 +59,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/wtf/AutoReset.h"
 #include "platform/wtf/Forward.h"
 #include "platform/wtf/HashSet.h"
-#include "platform/wtf/ListHashSet.h"
 #include "platform/wtf/text/WTFString.h"
 #include "public/platform/ShapeProperties.h"
 #include "public/platform/WebDisplayMode.h"
