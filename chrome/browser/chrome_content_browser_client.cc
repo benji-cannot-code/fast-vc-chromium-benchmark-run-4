f6c25ecd372175f65bdf0e4d67ba2729b3ae0b75
David Jacobo <djacobo@chromium.org>
2018-01-10 20:46:46 +0000

    Remove duplicated include
    One-line fix, removing
    services/proxy_resolver/public/interfaces/proxy_resolver.mojom.h
    duplicated include.
    
    Bug: None
    Test: Build
    Change-Id: If493e99d09f6914cb544f110b1b6dfd45e1dd74b
    Reviewed-on: https://chromium-review.googlesource.com/860746
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Commit-Queue: David Jacobo <djacobo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528417}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -302,7 +302,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/navigation_interception/intercept_navigation_delegate.h"
 #include "content/public/browser/android/java_interfaces.h"
 #include "services/proxy_resolver/proxy_resolver_service.h"
-#include "services/proxy_resolver/public/interfaces/proxy_resolver.mojom.h"
 #include "services/service_manager/public/cpp/interface_provider.h"
 #include "third_party/WebKit/public/platform/modules/payments/payment_request.mojom.h"
 #include "ui/base/resource/resource_bundle_android.h"
