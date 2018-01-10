6525258f12c8b75b26e4a844da03704cf9fcf834
Yutaka Hirano <yhirano@chromium.org>
2018-01-10 13:28:04 +0000

    Add DCHECKs to SecurityFilterPeer methods
    A SecurityFilterPeer is created only when the associated request is
    rejected, which means content::RequestPeer methods other than
    OnCompletedRequest must not be called.
    
    Bug: 706288
    Change-Id: I12eaef300909d1bc76ae2a9794c7b4d707b70c86
    Reviewed-on: https://chromium-review.googlesource.com/859260
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528292}

--- chrome/renderer/security_filter_peer.h
+++ chrome/renderer/security_filter_peer.h
@@ -21,10 +21,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // unsafe resources (such as mixed-content resource).
 // Call the factory method CreateSecurityFilterPeer() to obtain an instance of
 // SecurityFilterPeer based on the original Peer.
-// TODO(jcampan): For now the resource is still being fetched, but ignored, as
-// once we have provided the replacement content, the associated pending request
-// in ResourceDispatcher is removed and further OnReceived* notifications are
-// ignored.
+// A SecurityFilterPeer is created only when the associated request is rejected,
+// which means content::RequestPeer methods other than OnCompletedRequest must
+// not be called.
 class SecurityFilterPeer final : public content::RequestPeer {
  public:
   ~SecurityFilterPeer() override;
@@ -40,7 +39,7 @@ class SecurityFilterPeer final : public content::RequestPeer {
   bool OnReceivedRedirect(const net::RedirectInfo& redirect_info,
                           const content::ResourceResponseInfo& info) override;
   void OnReceivedResponse(const content::ResourceResponseInfo& info) override;
-  void OnDownloadedData(int len, int encoded_data_length) override {}
+  void OnDownloadedData(int len, int encoded_data_length) override;
   void OnReceivedData(std::unique_ptr<ReceivedData> data) override;
   void OnTransferSizeUpdated(int transfer_size_diff) override;
   void OnCompletedRequest(
