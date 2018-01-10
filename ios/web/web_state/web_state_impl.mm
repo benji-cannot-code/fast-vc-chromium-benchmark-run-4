943c54c55c421e4766e82afd1a85747e1af32e35
Danyao Wang <danyao@chromium.org>
2018-01-10 19:52:20 +0000

    [Nav Experiment] Remove placeholder URL shortcircuit in CRWWebController
    So that windowID is injected into the placeholder page (which is
    considered an HTML page). This is an invariant of
    [ChromeEarlGrey loadURL:]
    
    This change fixes the following egtest:
    ActivityServiceControllerTestCase testActivityServiceControllerIsDisabled
    WebUITestCase testVersion
    ToolbarTestCase testTypeJavaScriptIntoOmniboxWithWebUIPage
    ToolbarTestCase testToolbarUI
    
    Bug: 798832
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: If06049f63e60f2f7f3589d5d9938e45deae3fb0e
    Reviewed-on: https://chromium-review.googlesource.com/858045
    Commit-Queue: Danyao Wang <danyao@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528390}

--- ios/web/web_state/web_state_impl.mm
+++ ios/web/web_state/web_state_impl.mm
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/navigation/crw_session_controller.h"
 #import "ios/web/navigation/legacy_navigation_manager_impl.h"
 #import "ios/web/navigation/navigation_item_impl.h"
+#include "ios/web/navigation/placeholder_navigation_util.h"
 #import "ios/web/navigation/session_storage_builder.h"
 #import "ios/web/navigation/wk_based_navigation_manager_impl.h"
 #include "ios/web/public/browser_state.h"
@@ -48,6 +49,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
+
+using web::placeholder_navigation_util::IsPlaceholderUrl;
 
 namespace web {
 
@@ -246,6 +249,11 @@ bool WebStateImpl::IsBeingDestroyed() const {
 }
 
 void WebStateImpl::OnPageLoaded(const GURL& url, bool load_success) {
+  // Native Content and WebUI placeholder URL is an internal implementation
+  // detail of //ios/web/ navigation. Do not trigger external callbacks.
+  if (IsPlaceholderUrl(url))
+    return;
+
   PageLoadCompletionStatus load_completion_status =
       load_success ? PageLoadCompletionStatus::SUCCESS
                    : PageLoadCompletionStatus::FAILURE;
@@ -674,7 +682,8 @@ const GURL& WebStateImpl::GetLastCommittedURL() const {
 GURL WebStateImpl::GetCurrentURL(URLVerificationTrustLevel* trust_level) const {
   GURL URL = [web_controller_ currentURLWithTrustLevel:trust_level];
   bool equalOrigins = URL.GetOrigin() == GetLastCommittedURL().GetOrigin();
-  DCHECK(equalOrigins);
+  DCHECK(equalOrigins) << "Origin mismatch. URL: " << URL.spec()
+                       << " Last committed: " << GetLastCommittedURL().spec();
   UMA_HISTOGRAM_BOOLEAN("Web.CurrentOriginEqualsLastCommittedOrigin",
                         equalOrigins);
   return URL;
@@ -727,11 +736,21 @@ void WebStateImpl::TakeSnapshot(const SnapshotCallback& callback,
 }
 
 void WebStateImpl::OnNavigationStarted(web::NavigationContext* context) {
+  // Native Content and WebUI placeholder URL is an internal implementation
+  // detail of //ios/web/ navigation. Do not trigger external callbacks.
+  if (IsPlaceholderUrl(context->GetUrl()))
+    return;
+
   for (auto& observer : observers_)
     observer.DidStartNavigation(this, context);
 }
 
 void WebStateImpl::OnNavigationFinished(web::NavigationContext* context) {
+  // Native Content and WebUI placeholder URL is an internal implementation
+  // detail of //ios/web/ navigation. Do not trigger external callbacks.
+  if (IsPlaceholderUrl(context->GetUrl()))
+    return;
+
   for (auto& observer : observers_)
     observer.DidFinishNavigation(this, context);
 
