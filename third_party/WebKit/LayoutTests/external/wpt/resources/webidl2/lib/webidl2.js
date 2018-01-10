b912be924b1495aebaca38223c2f8966d80fdf47
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-01-10 23:34:54 +0000

    Import wpt@a1de23740cb079b71517dff563b919416198e6ca
    Using wpt-import in Chromium 3f2eca97e8ac199144c5c679516a8b9ae7aeeaf1.
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/9885
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    cvazac@akamai.com, igrigorik@chromium.org:
      external/wpt/server-timing
    dcheng@chromium.org, scottmg@chromium.org:
      external/wpt/domparsing
    jsbell@chromium.org:
      external/wpt/resources
    jsbell@chromium.org, mek@chromium.org:
      external/wpt/FileAPI
    
    TBR=rbyers
    
    No-Export: true
    Change-Id: Ia1e813871b74de6dd77da36fcd712e01e70b64ff
    Reviewed-on: https://chromium-review.googlesource.com/860631
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528479}

--- third_party/WebKit/LayoutTests/external/wpt/resources/webidl2/lib/webidl2.js
+++ third_party/WebKit/LayoutTests/external/wpt/resources/webidl2/lib/webidl2.js
@@ -15,7 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     const types = ["float", "integer", "identifier", "string", "whitespace", "other"];
     while (str.length > 0) {
       let matched = false;
-      for (const type of types) {
+      for (var i in types) {
+        const type = types[i];
         str = str.replace(re[type], tok => {
           tokens.push({ type, value: tok });
           matched = true;
@@ -120,10 +121,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             "multiline-comment": /^\/\*((?:.|\n|\r)*?)\*\//
           };
           const wsTypes = [];
-          for (const k in re) wsTypes.push(k);
+          for (var k in re) wsTypes.push(k);
           while (w.length) {
             let matched = false;
-            for (const type of wsTypes) {
+            for (var i in wsTypes) {
+              const type = wsTypes[i];
               w = w.replace(re[type], (tok, m1) => {
                 store.push({ type: type + (pea ? ("-" + pea) : ""), value: m1 });
                 matched = true;
