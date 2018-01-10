9899b50773804901d04bd4aaff867481dee3fae2
Robert Sesek <rsesek@chromium.org>
2018-01-10 22:39:19 +0000

    Stop allocating a SimpleStringDictionary for Crashpad clients.
    Bug: 598854, 749970
    Change-Id: I1886528b886b14f5151842de486a058d726f8ace
    Reviewed-on: https://chromium-review.googlesource.com/850952
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Penny MacNeil <pennymac@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528451}

--- components/crash/content/app/crashpad_mac.mm
+++ components/crash/content/app/crashpad_mac.mm
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/crashpad/crashpad/client/crashpad_client.h"
 #include "third_party/crashpad/crashpad/client/crashpad_info.h"
 #include "third_party/crashpad/crashpad/client/settings.h"
-#include "third_party/crashpad/crashpad/client/simple_string_dictionary.h"
 #include "third_party/crashpad/crashpad/client/simulate_crash.h"
 
 namespace crash_reporter {
