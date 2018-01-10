1a8eed369c0da8077f475ce134c49602bcb8de3b
Gauthier Ambard <gambard@chromium.org>
2018-01-10 15:17:37 +0000

    Create secondary toolbar ViewController/Coordinator
    This CL creates the secondary toolbar ViewController and Coordinator.
    For now they are not used.
    
    Bug: 799438, 799422
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I332788d513aa95f86e0bf409afd07238c149ec2a
    Reviewed-on: https://chromium-review.googlesource.com/854277
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528316}

--- ios/chrome/browser/ui/toolbar/clean/toolbar_button_factory.h
+++ ios/chrome/browser/ui/toolbar/clean/toolbar_button_factory.h
@@ -61,6 +61,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (ToolbarButton*)voiceSearchButton;
 // ContractToolbar ToolbarButton.
 - (ToolbarButton*)contractButton;
+// ToolbarButton to focus the omnibox.
+- (ToolbarButton*)omniboxButton;
 // LocationBar LeadingButton. Currently used for the incognito icon when the
 // Toolbar is expanded on incognito mode. It can return nil.
 - (ToolbarButton*)locationBarLeadingButton;
