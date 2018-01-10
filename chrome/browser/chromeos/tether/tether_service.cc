53aade0f93b0025455617c111ed2d618abac1008
Kyle Horimoto <khorimoto@google.com>
2018-01-10 23:33:56 +0000

    [CrOS Tether] Improve the InstantTethering.FeatureState metric.
    This CL relands a reverted CL.
     Original: https://chromium-review.googlesource.com/c/chromium/src/+/849252
     Revert  : https://chromium-review.googlesource.com/c/chromium/src/+/853594
    
    The original CL is uploaded as patchset 1, and the updated CL is
    uploaded as patchset 2. To fix the test, I only check the FeatureState
    metric in TearDown() if the service was actually created during the test.
    
    
    Original CL description is below:
    
    Previously, nearly half of our reported metrics were in the
    OTHER_OR_UNKNOWN category, which negated much of the value of this
    metric.
    
    This CL splits that category into two: SUSPENDED and SHUT_DOWN.
      * SUSPENDED is logged when the device has been put to sleep (i.e., the
        laptop lid has been closed).
      * SHUT_DOWN is now deprecated. The FeatureState metric was meant to
        provide insight into how the Tether component was being used, but
        SHUT_DOWN does not provide any insights since the Tether component
        is always eventually shut down any time the user logs out,
        regardless of any usage patterns. Thus, without this change, we
        would expect nearly half of all logs to this metric to be for
        SHUT_DOWN, which dilutes the impact of meaningful logs.
    
    Bug: 798605, 672263
    Change-Id: If99a054d815d23319749f84df931743f1c7e55a0
    TBR: holte@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/861045
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Jeremy Klein <jlklein@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528478}

--- chrome/browser/chromeos/tether/tether_service.cc
+++ chrome/browser/chromeos/tether/tether_service.cc
@@ -81,10 +81,10 @@ bool TetherService::IsFeatureFlagEnabled() {
 
 // static.
 std::string TetherService::TetherFeatureStateToString(
-    TetherFeatureState state) {
+    const TetherFeatureState& state) {
   switch (state) {
-    case (TetherFeatureState::OTHER_OR_UNKNOWN):
-      return "[other or unknown]";
+    case (TetherFeatureState::SHUT_DOWN):
+      return "[TetherService shut down]";
     case (TetherFeatureState::BLE_ADVERTISING_NOT_SUPPORTED):
       return "[BLE advertising not supported]";
     case (TetherFeatureState::NO_AVAILABLE_HOSTS):
@@ -101,10 +101,17 @@ std::string TetherService::TetherFeatureStateToString(
       return "[Enabled]";
     case (TetherFeatureState::BLE_NOT_PRESENT):
       return "[BLE is not present on the device]";
-    default:
+    case (TetherFeatureState::WIFI_NOT_PRESENT):
+      return "[Wi-Fi is not present on the device]";
+    case (TetherFeatureState::SUSPENDED):
+      return "[Suspended]";
+    case (TetherFeatureState::TETHER_FEATURE_STATE_MAX):
       // |previous_feature_state_| is initialized to TETHER_FEATURE_STATE_MAX,
       // and this value is never actually used in practice.
       return "[TetherService initializing]";
+    default:
+      NOTREACHED();
+      return "[Invalid state]";
   }
 }
 
@@ -379,7 +386,8 @@ TetherService::GetTetherTechnologyState() {
   }
 
   switch (new_feature_state) {
-    case OTHER_OR_UNKNOWN:
+    case SHUT_DOWN:
+    case SUSPENDED:
     case BLE_NOT_PRESENT:
     case BLE_ADVERTISING_NOT_SUPPORTED:
     case WIFI_NOT_PRESENT:
@@ -494,8 +502,11 @@ bool TetherService::IsEnabledbyPreference() const {
 }
 
 TetherService::TetherFeatureState TetherService::GetTetherFeatureState() {
-  if (shut_down_ || suspended_)
-    return OTHER_OR_UNKNOWN;
+  if (shut_down_)
+    return SHUT_DOWN;
+
+  if (suspended_)
+    return SUSPENDED;
 
   if (!IsBluetoothPresent())
     return BLE_NOT_PRESENT;
@@ -530,6 +541,15 @@ TetherService::TetherFeatureState TetherService::GetTetherFeatureState() {
 void TetherService::RecordTetherFeatureState() {
   TetherFeatureState tether_feature_state = GetTetherFeatureState();
   DCHECK(tether_feature_state != TetherFeatureState::TETHER_FEATURE_STATE_MAX);
+
+  // If the feature is shut down, there is no need to log a metric. Since this
+  // state occurs every time the user logs out (as of crbug.com/782879), logging
+  // a metric here does not provide any value since it does not indicate
+  // anything about how the user utilizes Instant Tethering and would dilute the
+  // contributions of meaningful states.
+  if (tether_feature_state == TetherFeatureState::SHUT_DOWN)
+    return;
+
   UMA_HISTOGRAM_ENUMERATION("InstantTethering.FeatureState",
                             tether_feature_state,
                             TetherFeatureState::TETHER_FEATURE_STATE_MAX);
