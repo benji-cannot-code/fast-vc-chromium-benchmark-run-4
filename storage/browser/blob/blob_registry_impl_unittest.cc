4bf0f0d8caf1307df49826a8dea340be6f2fac6b
Marijn Kruisselbrink <mek@chromium.org>
2018-01-10 22:16:07 +0000

    Stop using BlobStatus::ERR_FILE_WRITE_FAILED for two unrelated reasons.
    Instead introduce ERR_REFERENCED_FILE_UNAVAILABLE for the case where a
    blob tries to reference a file it doesn't have access to.
    
    Bug: 740730
    Change-Id: I66616c5fcfcbf788a53d1a22fb0b2af618edffb5
    Reviewed-on: https://chromium-review.googlesource.com/860279
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528443}

--- storage/browser/blob/blob_registry_impl_unittest.cc
+++ storage/browser/blob/blob_registry_impl_unittest.cc
@@ -508,7 +508,8 @@ TEST_F(BlobRegistryImplTest, Register_UnreadableFile) {
   WaitForBlobCompletion(handle.get());
 
   EXPECT_TRUE(handle->IsBroken());
-  EXPECT_EQ(BlobStatus::ERR_FILE_WRITE_FAILED, handle->GetBlobStatus());
+  EXPECT_EQ(BlobStatus::ERR_REFERENCED_FILE_UNAVAILABLE,
+            handle->GetBlobStatus());
   EXPECT_EQ(0u, BlobsUnderConstruction());
 }
 
@@ -557,7 +558,8 @@ TEST_F(BlobRegistryImplTest, Register_FileSystemFile_InvalidScheme) {
   WaitForBlobCompletion(handle.get());
 
   EXPECT_TRUE(handle->IsBroken());
-  EXPECT_EQ(BlobStatus::ERR_FILE_WRITE_FAILED, handle->GetBlobStatus());
+  EXPECT_EQ(BlobStatus::ERR_REFERENCED_FILE_UNAVAILABLE,
+            handle->GetBlobStatus());
   EXPECT_EQ(0u, BlobsUnderConstruction());
 }
 
@@ -580,7 +582,8 @@ TEST_F(BlobRegistryImplTest, Register_FileSystemFile_UnreadablFile) {
   WaitForBlobCompletion(handle.get());
 
   EXPECT_TRUE(handle->IsBroken());
-  EXPECT_EQ(BlobStatus::ERR_FILE_WRITE_FAILED, handle->GetBlobStatus());
+  EXPECT_EQ(BlobStatus::ERR_REFERENCED_FILE_UNAVAILABLE,
+            handle->GetBlobStatus());
   EXPECT_EQ(0u, BlobsUnderConstruction());
 }
 
