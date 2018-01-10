595c581c3831fc39fab554c74aa2575d34d8ed29
Zhiqiang Zhang <zqzhang@chromium.org>
2018-01-10 18:13:35 +0000

    Move BaseMediaRouteProvider to cast/
    This is to keep the router/ directory clean of v2-specific
    implementation.
    
    Bug: 
    Change-Id: Ic72eb459615135a86c5d03d74d8054594d9301a3
    Reviewed-on: https://chromium-review.googlesource.com/856256
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Zhiqiang Zhang <zqzhang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528356}

--- chrome/android/java/src/org/chromium/chrome/browser/media/router/cast/BaseMediaRouteProvider.java
+++ chrome/android/java/src/org/chromium/chrome/browser/media/router/cast/BaseMediaRouteProvider.java
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-package org.chromium.chrome.browser.media.router;
+package org.chromium.chrome.browser.media.router.cast;
 
 import android.os.Handler;
 import android.support.v7.media.MediaRouteSelector;
@@ -10,11 +10,10 @@ import android.support.v7.media.MediaRouter;
 import android.support.v7.media.MediaRouter.RouteInfo;
 
 import org.chromium.base.Log;
-import org.chromium.chrome.browser.media.router.cast.CastSession;
-import org.chromium.chrome.browser.media.router.cast.ChromeCastSessionManager;
-import org.chromium.chrome.browser.media.router.cast.DiscoveryCallback;
-import org.chromium.chrome.browser.media.router.cast.MediaSink;
-import org.chromium.chrome.browser.media.router.cast.MediaSource;
+import org.chromium.chrome.browser.media.router.DiscoveryDelegate;
+import org.chromium.chrome.browser.media.router.MediaRoute;
+import org.chromium.chrome.browser.media.router.MediaRouteManager;
+import org.chromium.chrome.browser.media.router.MediaRouteProvider;
 
 import java.util.ArrayList;
 import java.util.Collections;
