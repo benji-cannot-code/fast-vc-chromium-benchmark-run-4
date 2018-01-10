c1caca33095316007f5ce629c0c5f264610a87c2
Joel Hockey <joelhockey@chromium.org>
2018-01-09 16:09:43 -0800

    Remove unused ListHashSet import in HitTestResult
    Bug: 614112
    Change-Id: I0d63208b4e2bfacb129614e7f29bd6af70063be5
    Reviewed-on: https://chromium-review.googlesource.com/858238
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528228}

--- third_party/WebKit/Source/core/layout/HitTestResult.h
+++ third_party/WebKit/Source/core/layout/HitTestResult.h
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/heap/Handle.h"
 #include "platform/text/TextDirection.h"
 #include "platform/wtf/Forward.h"
-#include "platform/wtf/ListHashSet.h"
 #include "platform/wtf/VectorTraits.h"
 #include "platform/wtf/text/WTFString.h"
 
