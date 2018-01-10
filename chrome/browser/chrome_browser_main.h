9ec47baf68c9acee75653dfa08befa6e71334834
Scott Violet <sky@chromium.org>
2018-01-10 22:20:29 +0000

    Removes ChromeBrowserMainParts::local_state_
    It's not needed as a member.
    
    BUG=none
    TEST=none
    
    Change-Id: I2548c36090332ecdd7a8f7b9ffebee6ecccfaedb
    Reviewed-on: https://chromium-review.googlesource.com/860909
    Reviewed-by: Elliot Glaysher <erg@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528445}

--- chrome/browser/chrome_browser_main.h
+++ chrome/browser/chrome_browser_main.h
@@ -96,8 +96,6 @@ class ChromeBrowserMainParts : public content::BrowserMainParts {
 
   Profile* profile() { return profile_; }
 
-  const PrefService* local_state() const { return local_state_; }
-
  private:
   // Sets up the field trials and related initialization. Call only after
   // about:flags have been converted to switches.
@@ -115,7 +113,7 @@ class ChromeBrowserMainParts : public content::BrowserMainParts {
 
   // Reads origin trial policy data from local state and configures command line
   // for child processes.
-  void SetupOriginTrialsCommandLine();
+  void SetupOriginTrialsCommandLine(PrefService* local_state);
 
   // Methods for Main Message Loop -------------------------------------------
 
@@ -183,9 +181,6 @@ class ChromeBrowserMainParts : public content::BrowserMainParts {
   // Initialized in |SetupFieldTrials()|.
   scoped_refptr<FieldTrialSynchronizer> field_trial_synchronizer_;
 
-  // Members initialized in PreMainMessageLoopRun, needed in
-  // PreMainMessageLoopRunThreadsCreated.
-  PrefService* local_state_;
   base::FilePath user_data_dir_;
 
   DISALLOW_COPY_AND_ASSIGN(ChromeBrowserMainParts);
