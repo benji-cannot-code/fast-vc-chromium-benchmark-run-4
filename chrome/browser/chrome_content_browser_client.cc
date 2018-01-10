31864306bc46a89115d62201b8715540d3ea8fc0
Lukasz Anforowicz <lukasza@chromium.org>
2018-01-10 15:24:48 +0000

    Avoid propagating switches::kIsolateOrigins to renderer processes.
    Presence of switches::kIsolateOrigins is only inspected inside the
    browser process and therefore this cmdline flag doesn't need to be
    forwarded into cmdline of renderer processes.  This saves some room and
    avoids crashing due to exceeded kZygoteMaxMessageLength.
    
    Bug: 793350
    Change-Id: Id4b194b2b06810980e9275dfa224e0264e9302e9
    Reviewed-on: https://chromium-review.googlesource.com/857236
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528319}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -1839,7 +1839,6 @@ void ChromeContentBrowserClient::AppendExtraCommandLineSwitches(
       switches::kForcePNaClSubzero,
 #endif
       switches::kForceUIDirection,
-      switches::kIsolateOrigins,
       switches::kJavaScriptHarmony,
       switches::kOriginTrialDisabledFeatures,
       switches::kOriginTrialDisabledTokens,
--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -2623,7 +2623,6 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     switches::kForceVideoOverlays,
     switches::kFullMemoryCrashReport,
     switches::kIPCConnectionTimeout,
-    switches::kIsolateOrigins,
     switches::kJavaScriptFlags,
     switches::kLoggingLevel,
     switches::kMainFrameResizesAreOrientationChanges,
