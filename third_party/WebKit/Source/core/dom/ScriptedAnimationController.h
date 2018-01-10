5c5a21fc5741c42a40c5c0d29a0b299ba6da0a7f
Joel Hockey <joelhockey@chromium.org>
2018-01-09 16:36:57 -0800

    Remove unused ListHashSet import in ScriptedAnimationController
    Bug: 614112
    Change-Id: I3f06cef7485ef7974e65a71d8dab1ca9a054bbac
    Reviewed-on: https://chromium-review.googlesource.com/857361
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528205}

--- third_party/WebKit/Source/core/dom/ScriptedAnimationController.h
+++ third_party/WebKit/Source/core/dom/ScriptedAnimationController.h
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/FrameRequestCallbackCollection.h"
 #include "platform/bindings/TraceWrapperMember.h"
 #include "platform/heap/Handle.h"
-#include "platform/wtf/ListHashSet.h"
 #include "platform/wtf/Vector.h"
 #include "platform/wtf/text/AtomicString.h"
 #include "platform/wtf/text/StringImpl.h"
