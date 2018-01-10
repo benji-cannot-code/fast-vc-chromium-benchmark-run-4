fa76f1d5c085dc5b0a16c4f798b115318dc3ace0
Joel Hockey <joelhockey@chromium.org>
2018-01-09 16:04:19 -0800

    Remove unused ListHashSet import in FormController
    Bug: 614112
    Change-Id: Ib12a2a4e00572cae4b6f0344857938a752b1a572
    Reviewed-on: https://chromium-review.googlesource.com/858357
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528227}

--- third_party/WebKit/Source/core/html/forms/FormController.h
+++ third_party/WebKit/Source/core/html/forms/FormController.h
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/heap/HeapAllocator.h"
 #include "platform/wtf/Allocator.h"
 #include "platform/wtf/Forward.h"
-#include "platform/wtf/ListHashSet.h"
 #include "platform/wtf/Vector.h"
 #include "platform/wtf/text/AtomicStringHash.h"
 
