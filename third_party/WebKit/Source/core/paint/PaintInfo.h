20cea4d75ecebfd65756ca95707a8d725df061ac
Joel Hockey <joelhockey@chromium.org>
2018-01-09 16:12:04 -0800

    Remove unused ListHashSet import in PaintInfo
    Bug: 614112
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Id6e59edfcd7b524466259e53765a733971623b7a
    Reviewed-on: https://chromium-review.googlesource.com/858239
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528200}

--- third_party/WebKit/Source/core/paint/PaintInfo.h
+++ third_party/WebKit/Source/core/paint/PaintInfo.h
@@ -44,7 +44,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/transforms/AffineTransform.h"
 #include "platform/wtf/Allocator.h"
 #include "platform/wtf/HashMap.h"
-#include "platform/wtf/ListHashSet.h"
 
 #include <limits>
 
