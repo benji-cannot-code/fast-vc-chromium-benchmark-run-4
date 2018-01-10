c7e2e0b6914eb69ff757ffa138efa6bdd01c0244
Hui(Andy) Wu <wuandy@chromium.org>
2018-01-10 14:20:53 +0000

    [PaymentRequest] Change phone number validation to use IsPossibleNumber from libphonenumber.
    Right now payment request calls to libphonenumber's IsValidPhoneNumber for number validation,
    this might be too strict for our use case. IsValidPhoneNumber requires that
    area code/carrier code must exists in the real world for it to consider as a valid number. Due
    to chrome's release cycle, it might not have the latest metadata to make a correct call.
    
    More importantly, PaymentRequest probably should not care if a number is valid or not, because
    user can always input a fake number if they really want to. We should take any number that our
    code can work with(parsing into different components for example).
    
    Autofill has switched to use IsPossibleNumber in cl 827163.
    
    Bug: 792471
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Ie2d52c8c6fc904206a275ffa9e13e2cf507ec31c
    Reviewed-on: https://chromium-review.googlesource.com/836813
    Reviewed-by: anthonyvd <anthonyvd@chromium.org>
    Reviewed-by: Mathieu Perreault <mathp@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Hui Wu <wuandy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528303}

--- ios/chrome/browser/ui/payments/contact_info_edit_mediator.mm
+++ ios/chrome/browser/ui/payments/contact_info_edit_mediator.mm
@@ -117,8 +117,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         const std::string countryCode =
             autofill::AutofillCountry::CountryCodeForLocale(
                 self.paymentRequest->GetApplicationLocale());
-        if (!autofill::IsValidPhoneNumber(base::SysNSStringToUTF16(field.value),
-                                          countryCode)) {
+        if (!autofill::IsPossiblePhoneNumber(
+                base::SysNSStringToUTF16(field.value), countryCode)) {
           return l10n_util::GetNSString(
               IDS_PAYMENTS_PHONE_INVALID_VALIDATION_MESSAGE);
         }
