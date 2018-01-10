3799cb1b25f2c99b9b518e7c7e5a4849aea50b42
Alexander Timin <altimin@chromium.org>
2018-01-10 18:49:44 +0000

    [scheduler] Switch worker and compositor thread to use MetricsHelper.
    This patch starts to actually use WorkerMetricsHelper and
    CompositorMetricsHelper introduced in
    https://chromium-review.googlesource.com/c/chromium/src/+/859497.
    
    R=alexclarke@chromium.org
    BUG=776416
    
    Change-Id: Ifc025a439aba25613c2394a25fff08238f017b39
    Reviewed-on: https://chromium-review.googlesource.com/860101
    Reviewed-by: Alex Clarke <alexclarke@chromium.org>
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528369}

--- third_party/WebKit/Source/platform/scheduler/child/compositor_worker_scheduler.h
+++ third_party/WebKit/Source/platform/scheduler/child/compositor_worker_scheduler.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/message_loop.h"
 #include "base/single_thread_task_runner.h"
 #include "platform/PlatformExport.h"
+#include "platform/scheduler/child/compositor_metrics_helper.h"
 #include "platform/scheduler/child/worker_scheduler.h"
 #include "platform/scheduler/util/task_duration_metric_reporter.h"
 #include "platform/scheduler/util/thread_type.h"
@@ -60,8 +61,8 @@ class PLATFORM_EXPORT CompositorWorkerScheduler
 
  private:
   base::Thread* thread_;
-  TaskDurationMetricReporter<ThreadType>
-      compositor_thread_task_duration_reporter_;
+
+  CompositorMetricsHelper compositor_metrics_helper_;
 
   DISALLOW_COPY_AND_ASSIGN(CompositorWorkerScheduler);
 };
