18ed40249e19a837abbcfe57100dd2eb419015dc
Michael Giuffrida <michaelpg@chromium.org>
2018-01-10 21:08:37 +0000

    Move audio_debug_recordings_handler.h next to .cc file
    The .cc file was moved from chrome/browser/media/ to
    chrome/browser/media/webrtc/ in crrev.com/2307083002, but for some
    reason the header file stayed behind.
    
    Move it to the webrtc directory alongside its .cc file to reflect that
    the class does in fact use webrtc.
    
    No files in chrome/browser/media include this header file except for its
    .cc file, so moving it doesn't affect the original goal of the CL above.
    
    R=tommi@chromium.org
    
    Change-Id: If58ea2c860d95952b9001c29f2b6381cf4e32012
    Reviewed-on: https://chromium-review.googlesource.com/818487
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Michael Giuffrida <michaelpg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528422}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -402,7 +402,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if BUILDFLAG(ENABLE_WEBRTC)
-#include "chrome/browser/media/audio_debug_recordings_handler.h"
+#include "chrome/browser/media/webrtc/audio_debug_recordings_handler.h"
 #include "chrome/browser/media/webrtc/webrtc_logging_handler_host.h"
 #endif
 
--- chrome/browser/media/webrtc/audio_debug_recordings_handler.h
+++ chrome/browser/media/webrtc/audio_debug_recordings_handler.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_MEDIA_AUDIO_DEBUG_RECORDINGS_HANDLER_H_
-#define CHROME_BROWSER_MEDIA_AUDIO_DEBUG_RECORDINGS_HANDLER_H_
+#ifndef CHROME_BROWSER_MEDIA_WEBRTC_AUDIO_DEBUG_RECORDINGS_HANDLER_H_
+#define CHROME_BROWSER_MEDIA_WEBRTC_AUDIO_DEBUG_RECORDINGS_HANDLER_H_
 
 #include <stddef.h>
 #include <stdint.h>
@@ -96,4 +96,4 @@ class AudioDebugRecordingsHandler
   DISALLOW_COPY_AND_ASSIGN(AudioDebugRecordingsHandler);
 };
 
-#endif  // CHROME_BROWSER_MEDIA_AUDIO_DEBUG_RECORDINGS_HANDLER_H_
+#endif  // CHROME_BROWSER_MEDIA_WEBRTC_AUDIO_DEBUG_RECORDINGS_HANDLER_H_
