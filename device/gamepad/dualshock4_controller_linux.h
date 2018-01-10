e8ab954ecc960e28dee1ddf3c836f8b179691e4d
Matt Reynolds <mattreynolds@google.com>
2018-01-10 22:19:13 +0000

    Add haptics for Dualshock4 gamepads on Linux
    Haptics for Dualshock4 gamepads is not compatible with the force feedback
    feature of the Linux input subsystem. To support vibration effects on
    these devices, we enumerate the gamepad through the hidraw interface and
    communicate haptics commands using the proprietary Dualshock4 USB protocol.
    
    BUG=749295
    
    Change-Id: I5d732a7502eaea5ad5b1e801825375a659d6fc29
    Reviewed-on: https://chromium-review.googlesource.com/854984
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528444}

---
+++ device/gamepad/dualshock4_controller_linux.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef DEVICE_GAMEPAD_DUALSHOCK4_CONTROLLER_LINUX_
+#define DEVICE_GAMEPAD_DUALSHOCK4_CONTROLLER_LINUX_
+
+#include "device/gamepad/dualshock4_controller_base.h"
+
+namespace device {
+
+class Dualshock4ControllerLinux : public Dualshock4ControllerBase {
+ public:
+  Dualshock4ControllerLinux(int fd);
+  ~Dualshock4ControllerLinux() override;
+
+ private:
+  void WriteOutputReport(void* report, size_t report_length) override;
+
+  int fd_;
+};
+
+}  // namespace device
+
+#endif  // DEVICE_GAMEPAD_DUALSHOCK4_CONTROLLER_LINUX_
