6336d449e1e051d429bdc68186fc2adf7a9b72a7
Joel Hockey <joelhockey@chromium.org>
2018-01-09 16:21:05 -0800

    Remove unused ListHashSet import in WorkerGlobalScope
    Bug: 614112
    Change-Id: I5145dba678b1f4a74ad50f7ef2a9d5bec658d743
    Reviewed-on: https://chromium-review.googlesource.com/858240
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528211}

--- third_party/WebKit/Source/core/workers/WorkerGlobalScope.h
+++ third_party/WebKit/Source/core/workers/WorkerGlobalScope.h
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/workers/WorkerSettings.h"
 #include "platform/heap/Handle.h"
 #include "platform/loader/fetch/CachedMetadataHandler.h"
-#include "platform/wtf/ListHashSet.h"
 #include "services/network/public/interfaces/fetch_api.mojom-shared.h"
 #include "services/service_manager/public/cpp/interface_provider.h"
 #include "services/service_manager/public/interfaces/interface_provider.mojom-blink.h"
