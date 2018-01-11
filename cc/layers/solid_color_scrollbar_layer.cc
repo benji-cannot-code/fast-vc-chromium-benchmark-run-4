77cd549b1ad6547cfdde7f51ca7b95e46b771fc5
David Bokan <bokan@chromium.org>
2018-01-11 18:51:49 +0000

    Make scroll element id SetNeedsCommit
    Currently, a scrollbar layer getting a new scroll element id marks the
    tree as needing a full tree sync. This seems somewhat overkill as this
    (by itself) doesn't cause any changes to the tree structure. Worse, this
    only marks the _structure_ of the tree as needing to be synced. It
    doesn't mark the layer as needing to push its properties across the
    commit. So if the layer doesn't need to push properties for any other
    reasons, the new scroll element id wouldn't get committed to the impl
    side layer!
    
    Some code archaeology shows this goes all the way back to
    https://crrev.com/0bf9472475c27b921b18c55dad34f9d7ad15d18e. At that
    time, it looks like a tree sync also pushed all properties so at some
    point this was optimized out. We typically don't notice as changing a
    scrolling layer on a scrollbar doesn't really happen today. This is
    changing with document.rootScroller since the viewport scrollbars can be
    reparented to the new rootScroller. This can cause a crash when the impl
    side scrollbar layers have an invalid scroll element id after a commit.
    
    This CL changes the SetScrollElementId on each type of scrollbar layer
    to call SetNeedsCommit instead of SetNeedsFullTreeSync. SetNeedsCommit
    calls SetNeedsPushProperties on the layer so the scroll element id is
    now committed.
    
    Bug: 773362
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I3387e71a4de44fb24d2911b2900299cc1842334c
    Reviewed-on: https://chromium-review.googlesource.com/861323
    Reviewed-by: enne <enne@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528686}

--- cc/layers/solid_color_scrollbar_layer.cc
+++ cc/layers/solid_color_scrollbar_layer.cc
@@ -97,7 +97,7 @@ void SolidColorScrollbarLayer::SetScrollElementId(ElementId element_id) {
     return;
 
   solid_color_scrollbar_layer_inputs_.scroll_element_id = element_id;
-  SetNeedsFullTreeSync();
+  SetNeedsCommit();
 }
 
 }  // namespace cc
