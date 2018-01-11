29eea1b5c3c29e81dd4bf14d073e211ad71b0ec5
Trent Apted <tapted@chromium.org>
2018-01-11 02:04:53 +0000

    Pass the Typesetter::HARFBUZZ to ElideRectangleText unittests
    Two tests started failing after r528483 on macOS 10.11 only:
     - TextEliderTest.ElideRectangleTextPunctuation
     - TextEliderTest.ElideRectangleTextLongWords
    
    This is because ElideRectangleText switched to HARFBUZZ. Update
    the tests to use HARFBUZZ when determining test string widths.
    
    TBR=msw@chromium.org
    
    Bug: 801029
    Change-Id: I7f84415545ec9acd2b3bba470c11b423fa8f1b14
    Reviewed-on: https://chromium-review.googlesource.com/860973
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528533}

--- ui/gfx/text_elider_unittest.cc
+++ ui/gfx/text_elider_unittest.cc
@@ -638,7 +638,8 @@ TEST(TextEliderTest, ElideString) {
 TEST(TextEliderTest, MAYBE_ElideRectangleText) {
   const FontList font_list;
   const int line_height = font_list.GetHeight();
-  const float test_width = GetStringWidthF(ASCIIToUTF16("Test"), font_list);
+  const float test_width =
+      GetStringWidthF(ASCIIToUTF16("Test"), font_list, Typesetter::HARFBUZZ);
 
   struct TestData {
     const char* input;
@@ -703,8 +704,10 @@ TEST(TextEliderTest, MAYBE_ElideRectangleText) {
 TEST(TextEliderTest, MAYBE_ElideRectangleTextPunctuation) {
   const FontList font_list;
   const int line_height = font_list.GetHeight();
-  const float test_width = GetStringWidthF(ASCIIToUTF16("Test"), font_list);
-  const float test_t_width = GetStringWidthF(ASCIIToUTF16("Test T"), font_list);
+  const float test_width =
+      GetStringWidthF(ASCIIToUTF16("Test"), font_list, Typesetter::HARFBUZZ);
+  const float test_t_width =
+      GetStringWidthF(ASCIIToUTF16("Test T"), font_list, Typesetter::HARFBUZZ);
 
   struct TestData {
     const char* input;
@@ -752,8 +755,10 @@ TEST(TextEliderTest, MAYBE_ElideRectangleTextLongWords) {
   const int kAvailableHeight = 1000;
   const base::string16 kElidedTesting =
       UTF8ToUTF16(std::string("Tes") + kEllipsis);
-  const float elided_width = GetStringWidthF(kElidedTesting, font_list);
-  const float test_width = GetStringWidthF(ASCIIToUTF16("Test"), font_list);
+  const float elided_width =
+      GetStringWidthF(kElidedTesting, font_list, Typesetter::HARFBUZZ);
+  const float test_width =
+      GetStringWidthF(ASCIIToUTF16("Test"), font_list, Typesetter::HARFBUZZ);
 
   struct TestData {
     const char* input;
@@ -837,8 +842,10 @@ TEST(TextEliderTest, MAYBE_ElideRectangleTextCheckLineWidth) {
                                   WRAP_LONG_WORDS,
                                   &lines));
   ASSERT_EQ(2u, lines.size());
-  EXPECT_LE(GetStringWidthF(lines[0], font_list), kAvailableWidth);
-  EXPECT_LE(GetStringWidthF(lines[1], font_list), kAvailableWidth);
+  EXPECT_LE(GetStringWidthF(lines[0], font_list, Typesetter::HARFBUZZ),
+            kAvailableWidth);
+  EXPECT_LE(GetStringWidthF(lines[1], font_list, Typesetter::HARFBUZZ),
+            kAvailableWidth);
 }
 
 #if defined(OS_CHROMEOS)
