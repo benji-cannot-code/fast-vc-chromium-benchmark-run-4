55bf820a8a09fa91a6822332220bf142a1b4042c
Eugene Ostroukhov <eostroukhov@chromium.org>
2018-01-11 20:36:25 +0000

    DevTools: allow lazy loading for request data
    A new optional parameter had been added to Network.Enable -
    "maxPostBodySize". Setting it to a non-zero value limits request
    body sizes that are sent with the requestWillBeSent notification.
    A new "getRequestPostBody" function was added to fetch request bodies.
    
    All request bodies will be sent to the client if the "maxPostBodySize"
    was not set or was set to 0.
    
    Bug: 772402
    Change-Id: Ib6b4b96fb2ba002fbeeb633fa1de89d45d1ad0e9
    Reviewed-on: https://chromium-review.googlesource.com/849117
    Commit-Queue: Eugene Ostroukhov <eostroukhov@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528738}

--- third_party/WebKit/Source/core/inspector/InspectorNetworkAgent.h
+++ third_party/WebKit/Source/core/inspector/InspectorNetworkAgent.h
@@ -46,7 +46,6 @@ namespace blink {
 
 class Document;
 class DocumentLoader;
-class EncodedFormData;
 class ExecutionContext;
 struct FetchInitiatorInfo;
 class LocalFrame;
@@ -132,7 +131,6 @@ class CORE_EXPORT InspectorNetworkAgent final
                    const AtomicString& method,
                    const KURL&,
                    bool async,
-                   scoped_refptr<EncodedFormData> body,
                    const HTTPHeaderMap& headers,
                    bool include_crendentials);
   void DidFailXHRLoading(ExecutionContext*,
@@ -201,7 +199,8 @@ class CORE_EXPORT InspectorNetworkAgent final
 
   // Called from frontend
   protocol::Response enable(Maybe<int> total_buffer_size,
-                            Maybe<int> resource_buffer_size) override;
+                            Maybe<int> resource_buffer_size,
+                            Maybe<int> max_post_data_size) override;
   protocol::Response disable() override;
   protocol::Response setUserAgentOverride(const String&) override;
   protocol::Response setExtraHTTPHeaders(
@@ -236,6 +235,9 @@ class CORE_EXPORT InspectorNetworkAgent final
       const String& origin,
       std::unique_ptr<protocol::Array<String>>* certificate) override;
 
+  protocol::Response getRequestPostData(const String& request_id,
+                                        String* post_data) override;
+
   // Called from other agents.
   protocol::Response GetResponseBody(const String& request_id,
                                      String* content,
@@ -247,7 +249,9 @@ class CORE_EXPORT InspectorNetworkAgent final
   bool CacheDisabled();
 
  private:
-  void Enable(int total_buffer_size, int resource_buffer_size);
+  void Enable(int total_buffer_size,
+              int resource_buffer_size,
+              int max_post_data_size);
   void WillSendRequestInternal(ExecutionContext*,
                                unsigned long identifier,
                                DocumentLoader*,
@@ -297,6 +301,7 @@ class CORE_EXPORT InspectorNetworkAgent final
   HeapHashSet<Member<XMLHttpRequest>> replay_xhrs_;
   HeapHashSet<Member<XMLHttpRequest>> replay_xhrs_to_be_deleted_;
   TaskRunnerTimer<InspectorNetworkAgent> remove_finished_replay_xhr_timer_;
+  int max_post_data_size_;
 };
 
 }  // namespace blink
