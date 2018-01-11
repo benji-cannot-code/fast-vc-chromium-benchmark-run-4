1b790b33ddd04395cb933d8a50ef7b1e2a3f37e9
Tommy C. Li <tommycli@chromium.org>
2018-01-11 20:44:09 +0000

    Omnibox UI Experiments: Expire FaviconCache when history deleted.
    Currently we keep cached favicons even when the user clears their
    history. After this CL, we observe the HistoryService and appropriately
    clear cached favicons.
    
    It's not strictly speaking necessary -- but meets intuitive user
    expectations for history clearing.
    
    Bug: 799469, 798490
    Change-Id: I6705005e067f6c51864234b01682acb2cdc24777
    Reviewed-on: https://chromium-review.googlesource.com/860534
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Kevin Bailey <krb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528744}

--- chrome/browser/ui/omnibox/chrome_omnibox_client.cc
+++ chrome/browser/ui/omnibox/chrome_omnibox_client.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/command_updater.h"
 #include "chrome/browser/extensions/api/omnibox/omnibox_api.h"
 #include "chrome/browser/favicon/favicon_service_factory.h"
+#include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/net/predictor.h"
 #include "chrome/browser/predictors/autocomplete_action_predictor.h"
 #include "chrome/browser/predictors/autocomplete_action_predictor_factory.h"
@@ -103,8 +104,11 @@ ChromeOmniboxClient::ChromeOmniboxClient(OmniboxEditController* controller,
       scheme_classifier_(profile),
       request_id_(BitmapFetcherService::REQUEST_ID_INVALID),
       favicon_cache_(FaviconServiceFactory::GetForProfile(
-          profile,
-          ServiceAccessType::EXPLICIT_ACCESS)) {}
+                         profile,
+                         ServiceAccessType::EXPLICIT_ACCESS),
+                     HistoryServiceFactory::GetForProfile(
+                         profile,
+                         ServiceAccessType::EXPLICIT_ACCESS)) {}
 
 ChromeOmniboxClient::~ChromeOmniboxClient() {
   BitmapFetcherService* image_service =
--- chrome/browser/ui/omnibox/favicon_cache.cc
+++ chrome/browser/ui/omnibox/favicon_cache.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/mru_cache.h"
 #include "components/favicon/core/favicon_service.h"
+#include "components/history/core/browser/history_service.h"
 #include "components/omnibox/browser/autocomplete_result.h"
 
 namespace {
@@ -19,10 +20,15 @@ size_t GetFaviconCacheSize() {
 
 }  // namespace
 
-FaviconCache::FaviconCache(favicon::FaviconService* favicon_service)
+FaviconCache::FaviconCache(favicon::FaviconService* favicon_service,
+                           history::HistoryService* history_service)
     : favicon_service_(favicon_service),
+      history_observer_(this),
       mru_cache_(GetFaviconCacheSize()),
-      weak_factory_(this) {}
+      weak_factory_(this) {
+  if (history_service)
+    history_observer_.Add(history_service);
+}
 
 FaviconCache::~FaviconCache() {}
 
@@ -73,4 +79,25 @@ void FaviconCache::OnFaviconFetched(
     std::move(callback).Run(result.image);
   }
   pending_requests_.erase(it);
+}
+
+void FaviconCache::OnURLsDeleted(history::HistoryService* history_service,
+                                 bool all_history,
+                                 bool expired,
+                                 const history::URLRows& deleted_rows,
+                                 const std::set<GURL>& favicon_urls) {
+  // We only care about actual user (or sync) deletions.
+  if (expired)
+    return;
+
+  if (all_history) {
+    mru_cache_.Clear();
+    return;
+  }
+
+  for (const history::URLRow& row : deleted_rows) {
+    auto it = mru_cache_.Peek(row.url());
+    if (it != mru_cache_.end())
+      mru_cache_.Erase(it);
+  }
 }
