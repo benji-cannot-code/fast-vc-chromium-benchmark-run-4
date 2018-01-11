03dadbb32feb98b978894a494e6b578050f278ab
Tsuyoshi Horo <horo@chromium.org>
2018-01-11 10:18:28 +0000

    Create ServiceWorkerRequestHandler for ExternalFileScheme
    When a file in Drive is opened from ChromeOS Files app, Chrome opens
    "externalfile" scheme URL. And ExternalFileURLRequestJob redirects the request
    to the Drive's HTTPS URL.
    
    Currently ServiceWorkerRequestHandler::InitializeForNavigation() doesn't create
    a ServiceWorkerProviderHost for the "externalfile" scheme URL.
    So ServiceWorkerProviderHost is not created during the navigation.
    
    A new ServiceWorkerProviderHost will be created when ServiceWorkerDispatcherHost
    receives OnProviderCreated() IPC from the renderer process.
    https://chromium.googlesource.com/chromium/src/+/86ff9bc/content/browser/service_worker/service_worker_dispatcher_host.cc#355
    
    But SetDocumentUrl() of the ServiceWorkerProviderHost which is created here will
    not be called because the navigation is already finished.
    So all ServiceWorker related API in the page will fail with
    "No URL is associated with the caller's document." error.
    
    Bug: 776408
    Change-Id: Ic4a0cd2ec5fb16ff995fb96927ca3fea61c3359a
    Reviewed-on: https://chromium-review.googlesource.com/861558
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528590}

--- content/browser/service_worker/service_worker_request_handler.cc
+++ content/browser/service_worker/service_worker_request_handler.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/origin_util.h"
 #include "content/public/common/resource_request_body.h"
 #include "content/public/common/service_worker_modes.h"
+#include "content/public/common/url_constants.h"
 #include "ipc/ipc_message.h"
 #include "net/base/url_util.h"
 #include "net/url_request/url_request.h"
@@ -59,6 +60,14 @@ class ServiceWorkerRequestInterceptor
   DISALLOW_COPY_AND_ASSIGN(ServiceWorkerRequestInterceptor);
 };
 
+bool SchemeMaySupportRedirectingToHTTPS(const GURL& url) {
+#if defined(OS_CHROMEOS)
+  return url.SchemeIs(kExternalFileScheme);
+#else   // OS_CHROMEOS
+  return false;
+#endif  // OS_CHROMEOS
+}
+
 }  // namespace
 
 // static
@@ -87,7 +96,8 @@ void ServiceWorkerRequestHandler::InitializeForNavigation(
   // Create the handler even for insecure HTTP since it's used in the
   // case of redirect to HTTPS.
   if (!request->url().SchemeIsHTTPOrHTTPS() &&
-      !OriginCanAccessServiceWorkers(request->url())) {
+      !OriginCanAccessServiceWorkers(request->url()) &&
+      !SchemeMaySupportRedirectingToHTTPS(request->url())) {
     return;
   }
 
