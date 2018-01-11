265714e42469271a1eff04484ed0b09ed56dcd19
Bence Béky <bnc@chromium.org>
2018-01-11 01:16:50 +0000

    Rename ConstructInitialSpdyPushFrame to ConstructSpdyPushPromise.
    Use this method in some unittests instead of creating a
    SpdyPushPromiseIR and calling SerializeFrame() on them.
    
    Also change order of arguments to match SpdyPushPromiseIR member order.
    
    Bug: 
    Change-Id: Ic102f361e148364484d468f31197aad7a5d478fa
    Reviewed-on: https://chromium-review.googlesource.com/860632
    Reviewed-by: Helen Li <xunjieli@chromium.org>
    Commit-Queue: Bence Béky <bnc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528516}

--- net/spdy/chromium/spdy_network_transaction_unittest.cc
+++ net/spdy/chromium/spdy_network_transaction_unittest.cc
@@ -2138,7 +2138,7 @@ TEST_F(SpdyNetworkTransactionTest,
                                  &push_headers);
 
   SpdySerializedFrame push_init_frame(
-      spdy_util_.ConstructInitialSpdyPushFrame(std::move(push_headers), 2, 1));
+      spdy_util_.ConstructSpdyPushPromise(1, 2, std::move(push_headers)));
 
   SpdySerializedFrame push_headers_frame(
       spdy_util_.ConstructSpdyPushHeaders(2, nullptr, 0));
@@ -2804,15 +2804,13 @@ TEST_F(SpdyNetworkTransactionTest, ServerPushInvalidUrl) {
   // Can't use ConstructSpdyPush here since it wants to parse a URL and
   // split it into the appropriate :header pieces. So we have to hand-fill
   // those pieces in.
-  SpdyFramer response_spdy_framer(SpdyFramer::ENABLE_COMPRESSION);
   SpdyHeaderBlock push_promise_header_block;
   push_promise_header_block[kHttp2AuthorityHeader] = "";
   push_promise_header_block[kHttp2SchemeHeader] = "";
   push_promise_header_block[kHttp2PathHeader] = "/index.html";
 
-  SpdyPushPromiseIR push_promise(1, 2, std::move(push_promise_header_block));
-  SpdySerializedFrame push_promise_frame(
-      response_spdy_framer.SerializeFrame(push_promise));
+  SpdySerializedFrame push_promise(spdy_util_.ConstructSpdyPushPromise(
+      1, 2, std::move(push_promise_header_block)));
 
   SpdySerializedFrame stream2_rst(
       spdy_util_.ConstructSpdyRstStream(2, ERROR_CODE_REFUSED_STREAM));
@@ -2820,7 +2818,7 @@ TEST_F(SpdyNetworkTransactionTest, ServerPushInvalidUrl) {
   MockWrite writes[] = {CreateMockWrite(req, 0),
                         CreateMockWrite(stream2_rst, 2)};
   MockRead reads[] = {
-      CreateMockRead(push_promise_frame, 1), MockRead(ASYNC, 0, 3) /* EOF */
+      CreateMockRead(push_promise, 1), MockRead(ASYNC, 0, 3) /* EOF */
   };
   SequencedSocketData data(reads, arraysize(reads), writes, arraysize(writes));
   RunBrokenPushTest(&data, ERR_CONNECTION_CLOSED);
@@ -2885,8 +2883,8 @@ TEST_F(SpdyNetworkTransactionTest, ServerPushNoURL) {
   SpdyHeaderBlock incomplete_headers;
   incomplete_headers[kHttp2StatusHeader] = "200 OK";
   incomplete_headers["hello"] = "bye";
-  SpdySerializedFrame stream2_syn(spdy_util_.ConstructInitialSpdyPushFrame(
-      std::move(incomplete_headers), 2, 1));
+  SpdySerializedFrame stream2_syn(
+      spdy_util_.ConstructSpdyPushPromise(1, 2, std::move(incomplete_headers)));
   MockRead reads[] = {
       CreateMockRead(stream1_reply, 1), CreateMockRead(stream2_syn, 2),
       CreateMockRead(stream1_body, 4),
@@ -3258,14 +3256,13 @@ TEST_F(SpdyNetworkTransactionTest, RejectServerPushWithNoMethod) {
   SpdyHeaderBlock push_promise_header_block;
   spdy_util_.AddUrlToHeaderBlock(GetDefaultUrlWithPath("/foo.dat").c_str(),
                                  &push_promise_header_block);
-  SpdyPushPromiseIR push_promise(1, 2, std::move(push_promise_header_block));
-  SpdySerializedFrame push_promise_frame(
-      spdy_util_.SerializeFrame(push_promise));
+  SpdySerializedFrame push_promise(spdy_util_.ConstructSpdyPushPromise(
+      1, 2, std::move(push_promise_header_block)));
 
   SpdySerializedFrame body(spdy_util_.ConstructSpdyDataFrame(1, true));
-  MockRead reads[] = {
-      CreateMockRead(reply, 1), CreateMockRead(push_promise_frame, 2),
-      CreateMockRead(body, 4), MockRead(SYNCHRONOUS, ERR_IO_PENDING, 5)};
+  MockRead reads[] = {CreateMockRead(reply, 1), CreateMockRead(push_promise, 2),
+                      CreateMockRead(body, 4),
+                      MockRead(SYNCHRONOUS, ERR_IO_PENDING, 5)};
 
   SequencedSocketData data(reads, arraysize(reads), writes, arraysize(writes));
   NormalSpdyTransactionHelper helper(request_, DEFAULT_PRIORITY, log_, nullptr);
@@ -3286,14 +3283,13 @@ TEST_F(SpdyNetworkTransactionTest, RejectServerPushWithInvalidMethod) {
   push_promise_header_block[":method"] = "POST";
   spdy_util_.AddUrlToHeaderBlock(GetDefaultUrlWithPath("/foo.dat").c_str(),
                                  &push_promise_header_block);
-  SpdyPushPromiseIR push_promise(1, 2, std::move(push_promise_header_block));
-  SpdySerializedFrame push_promise_frame(
-      spdy_util_.SerializeFrame(push_promise));
+  SpdySerializedFrame push_promise(spdy_util_.ConstructSpdyPushPromise(
+      1, 2, std::move(push_promise_header_block)));
 
   SpdySerializedFrame body(spdy_util_.ConstructSpdyDataFrame(1, true));
-  MockRead reads[] = {
-      CreateMockRead(reply, 1), CreateMockRead(push_promise_frame, 2),
-      CreateMockRead(body, 4), MockRead(SYNCHRONOUS, ERR_IO_PENDING, 5)};
+  MockRead reads[] = {CreateMockRead(reply, 1), CreateMockRead(push_promise, 2),
+                      CreateMockRead(body, 4),
+                      MockRead(SYNCHRONOUS, ERR_IO_PENDING, 5)};
 
   SequencedSocketData data(reads, arraysize(reads), writes, arraysize(writes));
   NormalSpdyTransactionHelper helper(request_, DEFAULT_PRIORITY, log_, nullptr);
@@ -4839,8 +4835,8 @@ TEST_F(SpdyNetworkTransactionTest, ServerPushWithHeaders) {
   initial_headers[":method"] = "GET";
   spdy_util_.AddUrlToHeaderBlock(GetDefaultUrlWithPath("/foo.dat"),
                                  &initial_headers);
-  SpdySerializedFrame stream2_syn(spdy_util_.ConstructInitialSpdyPushFrame(
-      std::move(initial_headers), 2, 1));
+  SpdySerializedFrame stream2_syn(
+      spdy_util_.ConstructSpdyPushPromise(1, 2, std::move(initial_headers)));
 
   SpdyHeaderBlock late_headers;
   late_headers[kHttp2StatusHeader] = "200";
@@ -4898,8 +4894,8 @@ TEST_F(SpdyNetworkTransactionTest, ServerPushClaimBeforeHeaders) {
   initial_headers[":method"] = "GET";
   spdy_util_.AddUrlToHeaderBlock(GetDefaultUrlWithPath("/foo.dat"),
                                  &initial_headers);
-  SpdySerializedFrame stream2_syn(spdy_util_.ConstructInitialSpdyPushFrame(
-      std::move(initial_headers), 2, 1));
+  SpdySerializedFrame stream2_syn(
+      spdy_util_.ConstructSpdyPushPromise(1, 2, std::move(initial_headers)));
   SpdySerializedFrame stream1_body(spdy_util_.ConstructSpdyDataFrame(1, true));
   SpdyHeaderBlock late_headers;
   late_headers[kHttp2StatusHeader] = "200";
@@ -6392,7 +6388,7 @@ TEST_F(SpdyNetworkTransactionTest, GoAwayOnOddPushStreamId) {
   SpdyHeaderBlock push_headers;
   spdy_util_.AddUrlToHeaderBlock("http://www.example.org/a.dat", &push_headers);
   SpdySerializedFrame push(
-      spdy_util_.ConstructInitialSpdyPushFrame(std::move(push_headers), 3, 1));
+      spdy_util_.ConstructSpdyPushPromise(1, 3, std::move(push_headers)));
   MockRead reads[] = {CreateMockRead(push, 1)};
 
   SpdySerializedFrame req(
@@ -6418,8 +6414,8 @@ TEST_F(SpdyNetworkTransactionTest,
   SpdyHeaderBlock push_b_headers;
   spdy_util_.AddUrlToHeaderBlock(GetDefaultUrlWithPath("/b.dat"),
                                  &push_b_headers);
-  SpdySerializedFrame push_b(spdy_util_.ConstructInitialSpdyPushFrame(
-      std::move(push_b_headers), 2, 1));
+  SpdySerializedFrame push_b(
+      spdy_util_.ConstructSpdyPushPromise(1, 2, std::move(push_b_headers)));
   MockRead reads[] = {
       CreateMockRead(push_a, 1), CreateMockRead(push_b, 3),
   };
@@ -6807,8 +6803,8 @@ TEST_F(SpdyNetworkTransactionTest, PushCanceledByServerAfterClaimed) {
                         CreateMockWrite(req2, 6)};
 
   // Construct a Push Promise frame, with no response.
-  SpdySerializedFrame push_promise(spdy_util_.ConstructInitialSpdyPushFrame(
-      spdy_util_.ConstructGetHeaderBlock(pushed_url), 2, 1));
+  SpdySerializedFrame push_promise(spdy_util_.ConstructSpdyPushPromise(
+      1, 2, spdy_util_.ConstructGetHeaderBlock(pushed_url)));
   // Construct a RST frame, canceling stream 2.
   SpdySerializedFrame rst_server(
       spdy_util_.ConstructSpdyRstStream(2, ERROR_CODE_CANCEL));
