a771a7ac0cd1ee7ad641e0c51b195329fe33314f
Mohamad Ahmadi <mahmadi@chromium.org>
2018-01-11 23:07:15 +0000

    [AF] Adds autofill_controller to the early page scripts of Chrome WebView
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I5a90e0a28acceecf7862007cf4db102078e123f1
    Reviewed-on: https://chromium-review.googlesource.com/861324
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528801}

--- ios/web_view/internal/web_view_web_client.mm
+++ ios/web_view/internal/web_view_web_client.mm
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/web_view/internal/web_view_web_client.h"
 
+#include "base/logging.h"
+#include "base/mac/bundle_locations.h"
 #include "base/memory/ptr_util.h"
 #include "base/strings/sys_string_conversions.h"
 #include "ios/web/public/user_agent.h"
@@ -19,6 +21,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 namespace ios_web_view {
+namespace {
+// Returns an autoreleased string containing the JavaScript loaded from a
+// bundled resource file with the given name (excluding extension).
+NSString* GetPageScript(NSString* script_file_name) {
+  DCHECK(script_file_name);
+  NSString* path =
+      [base::mac::FrameworkBundle() pathForResource:script_file_name
+                                             ofType:@"js"];
+  DCHECK(path) << "Script file not found: "
+               << base::SysNSStringToUTF8(script_file_name) << ".js";
+  NSError* error = nil;
+  NSString* content = [NSString stringWithContentsOfFile:path
+                                                encoding:NSUTF8StringEncoding
+                                                   error:&error];
+  DCHECK(!error) << "Error fetching script: "
+                 << base::SysNSStringToUTF8(error.description);
+  DCHECK(content);
+  return content;
+}
+}  // namespace
 
 WebViewWebClient::WebViewWebClient() = default;
 
@@ -51,8 +73,15 @@ base::RefCountedMemory* WebViewWebClient::GetDataResourceBytes(
 
 NSString* WebViewWebClient::GetEarlyPageScriptForMainFrame(
     web::BrowserState* browser_state) const {
-  return WebViewEarlyPageScriptProvider::FromBrowserState(browser_state)
-      .GetScript();
+  NSMutableArray* scripts = [NSMutableArray array];
+
+  WebViewEarlyPageScriptProvider& provider =
+      WebViewEarlyPageScriptProvider::FromBrowserState(browser_state);
+  [scripts addObject:provider.GetScript()];
+
+  [scripts addObject:GetPageScript(@"web_view_bundle")];
+
+  return [scripts componentsJoinedByString:@";"];
 }
 
 }  // namespace ios_web_view
