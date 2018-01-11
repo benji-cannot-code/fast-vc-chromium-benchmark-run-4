ab603cfecfe440aff70a2015b55de75385c34be3
Jiaquan He <hejq@google.com>
2018-01-11 18:39:49 +0000

    app_list: clean up some inclusions in chrome.
    Bug: 733662
    Change-Id: If264a77993330272878f59300ea3f90bb4284ac6
    Reviewed-on: https://chromium-review.googlesource.com/861101
    Commit-Queue: Jiaquan He <hejq@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528681}

--- chrome/browser/ui/app_list/speech_recognizer.cc
+++ chrome/browser/ui/app_list/speech_recognizer.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 
-#include "ash/app_list/model/speech/speech_ui_model_observer.h"
 #include "base/bind.h"
 #include "base/macros.h"
 #include "base/strings/string16.h"
--- chrome/browser/ui/app_list/start_page_service.h
+++ chrome/browser/ui/app_list/start_page_service.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-#include "ash/app_list/model/speech/speech_ui_model_observer.h"
 #include "base/callback.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
