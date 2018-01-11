e4de858ba1e43188712a1313a78c952b3aad488a
Doug Arnett <dougarnett@chromium.org>
2018-01-11 00:21:09 +0000

    Considers Cache-Control:no-transform header for committed PreviewsState
    Sets PreviewsState to PREVIEWS_OFF if main frame response has the
    no-transform header for an otherwise eligible client preview.
    
    Bug: 796952
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: Ic0a026c340c54529f3a8bd796599fb2aaab9e482
    Reviewed-on: https://chromium-review.googlesource.com/853122
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Doug Arnett <dougarnett@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528500}

--- components/previews/content/previews_content_util.cc
+++ components/previews/content/previews_content_util.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/previews/content/previews_content_util.h"
 
+#include "base/metrics/histogram_macros.h"
+#include "components/previews/core/previews_user_data.h"
 #include "net/url_request/url_request.h"
 
 namespace previews {
@@ -54,13 +56,27 @@ content::PreviewsState DetermineCommittedClientPreviewsState(
   bool is_https = url_request.url().SchemeIs(url::kHttpsScheme);
 
   // If a server preview is set, retain only the bits determined for the server.
-  // |previes_state| must already have been updated for server previews from the
-  // main frame response headers (so if they are set here, then they are the
-  // specify the committed preview).
+  // |previews_state| must already have been updated for server previews from
+  // the main frame response headers (so if they are set here, then they are
+  // the specify the committed preview). Note: for Server LoFi we keep the
+  // Client LoFi bit on so that it is applied to both HTTP and HTTPS images.
   if (previews_state &
       (content::SERVER_LITE_PAGE_ON | content::SERVER_LOFI_ON)) {
     return previews_state & (content::SERVER_LITE_PAGE_ON |
                              content::SERVER_LOFI_ON | content::CLIENT_LOFI_ON);
+  }
+
+  previews::PreviewsUserData* previews_user_data =
+      previews::PreviewsUserData::GetData(url_request);
+  if (previews_user_data &&
+      previews_user_data->cache_control_no_transform_directive()) {
+    if (HasEnabledPreviews(previews_state)) {
+      UMA_HISTOGRAM_ENUMERATION(
+          "Previews.CacheControlNoTransform.BlockedPreview",
+          GetMainFramePreviewsType(previews_state),
+          previews::PreviewsType::LAST);
+    }
+    return content::PREVIEWS_OFF;
   }
 
   // Make priority decision among allow client preview types that can be decided
@@ -88,8 +104,12 @@ previews::PreviewsType GetMainFramePreviewsType(
     content::PreviewsState previews_state) {
   if (previews_state & content::SERVER_LITE_PAGE_ON) {
     return previews::PreviewsType::LITE_PAGE;
+  } else if (previews_state & content::SERVER_LOFI_ON) {
+    return previews::PreviewsType::LOFI;
   } else if (previews_state & content::NOSCRIPT_ON) {
     return previews::PreviewsType::NOSCRIPT;
+  } else if (previews_state & content::CLIENT_LOFI_ON) {
+    return previews::PreviewsType::LOFI;
   }
   return previews::PreviewsType::NONE;
 }
