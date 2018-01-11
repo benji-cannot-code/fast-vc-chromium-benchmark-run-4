5dec9fe6b4db7ab297e67adee4308eba72d060ee
Michael Lippautz <mlippautz@chromium.org>
2018-01-11 07:51:37 +0000

    [oilpan] More write barrier fixes and tests
    - Avoid copies when dealing with deleted values
    - More tests
    
    Bug: chromium:757440
    Change-Id: I3769abb68f624f498667e52669171eb8f39df44e
    Reviewed-on: https://chromium-review.googlesource.com/860922
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528576}

--- third_party/WebKit/Source/platform/heap/HeapAllocator.h
+++ third_party/WebKit/Source/platform/heap/HeapAllocator.h
@@ -696,7 +696,7 @@ struct HashTraits<blink::Member<T>> : SimpleClassHashTraits<blink::Member<T>> {
   static void ConstructDeletedValue(blink::Member<T>& slot, bool) {
     slot = WTF::kHashTableDeletedValue;
   }
-  static bool IsDeletedValue(blink::Member<T> value) {
+  static bool IsDeletedValue(const blink::Member<T>& value) {
     return value.IsHashTableDeletedValue();
   }
 };
