10ed369961115d8db84f2d55860e08e13c5ec0cb
Daniel Bratell <bratell@opera.com>
2018-01-11 23:13:16 +0000

    Restore the UNICODE macro after third_party/ced has undefined it
    third_party/ced (more specifically
    third_party/ced/src/util/encodings/encodings.h) undefs UNICODE
    which is a central define in Windows. It controls whether
    Windows SDK headers should have 2 byte or 1 byte strings and must
    always be set in a modern application.
    
    In Jumbo builds this include of the dangerous ced header happen
    in the same translation unit as the include of Windows headers
    which will then fail to do the right thing.
    
    Restoring the UNICODE define is a workaround.
    
    Bug: 775547
    Change-Id: I7323cf6f9fff3efe2780c3125b9b099a8f9faa09
    Reviewed-on: https://chromium-review.googlesource.com/861883
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Bratell <bratell@opera.com>
    Cr-Commit-Position: refs/heads/master@{#528803}

--- base/i18n/encoding_detection.cc
+++ base/i18n/encoding_detection.cc
@@ -5,7 +5,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/i18n/encoding_detection.h"
 
+#include "build/build_config.h"
 #include "third_party/ced/src/compact_enc_det/compact_enc_det.h"
+
+// third_party/ced/src/util/encodings/encodings.h, which is included
+// by the include above, undefs UNICODE because that is a macro used
+// internally in ced. If we later in the same translation unit do
+// anything related to Windows or Windows headers those will then use
+// the ASCII versions which we do not want. To avoid that happening in
+// jumbo builds, we redefine UNICODE again here.
+#if defined(OS_WIN)
+#define UNICODE 1
+#endif  // OS_WIN
 
 namespace base {
 
