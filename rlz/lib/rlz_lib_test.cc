71a5643fedcc7fd91aa18c407b850b882bfdc6a1
Roger Tawa <rogerta@chromium.org>
2018-01-11 18:29:34 +0000

    Change RLZ to use HTTPS instead of HTTP for financial pings.
    Change-Id: I7562703649498c74b101791d8b556569e3729bb6
    Reviewed-on: https://chromium-review.googlesource.com/834391
    Commit-Queue: Roger Tawa <rogerta@chromium.org>
    Reviewed-by: Thiemo Nagel <tnagel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528674}

--- rlz/lib/rlz_lib_test.cc
+++ rlz/lib/rlz_lib_test.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/message_loop.h"
 #include "base/posix/eintr_wrapper.h"
 #include "base/single_thread_task_runner.h"
+#include "base/test/scoped_task_environment.h"
 #include "build/build_config.h"
 #include "rlz/lib/financial_ping.h"
 #include "rlz/lib/net_response_check.h"
@@ -63,6 +64,8 @@ class MachineDealCodeHelper
 };
 
 class RlzLibTest : public RlzLibTestBase {
+ protected:
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
 };
 
 TEST_F(RlzLibTest, RecordProductEvent) {
@@ -514,8 +517,7 @@ TEST_F(RlzLibTest, SendFinancialPingDuringShutdown) {
   rlz_lib::test::ResetSendFinancialPingInterrupted();
   EXPECT_FALSE(rlz_lib::test::WasSendFinancialPingInterrupted());
 
-  base::MessageLoop loop;
-  loop.task_runner()->PostTask(FROM_HERE, base::Bind(&ResetContext));
+  io_thread.task_runner()->PostTask(FROM_HERE, base::BindOnce(&ResetContext));
   std::string request;
   EXPECT_FALSE(rlz_lib::SendFinancialPing(rlz_lib::TOOLBAR_NOTIFIER, points,
       "swg", "GGLA", "SwgProductId1234", "en-UK", false,
