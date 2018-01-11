83cbdfd601ab5d9388fbc3296fd1907625200163
Tibor Goldschwendt <tiborg@chromium.org>
2018-01-11 23:06:38 +0000

    [vr] Add script to facilitate assets component upload
    Also adds a list of files that are part of the assets component. This
    list is also added to the presubmit checks to ensure the version is
    incremented if you change the list of files. The list added here just
    represents the current state of the assets component.
    
    However, adding the check makes presubmit fail. Thus, I have to bypass
    the CQ to land this change.
    
    Bug: 706156
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: If87a7dfb84679a3de032749f6dd44cda0f1256d2
    Reviewed-on: https://chromium-review.googlesource.com/860523
    Reviewed-by: Christopher Grant <cjgrant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528800}

---
+++ chrome/browser/resources/vr/assets/parse_version.py
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import collections
+
+
+def ParseVersion(lines):
+  version_keys = ['MAJOR', 'MINOR']
+  version_vals = {}
+  for line in lines:
+    key, val = line.strip().split('=', 1)
+    if key in version_keys:
+      if key in version_vals or not val.isdigit():
+        return None
+      version_vals[key] = int(val)
+
+  if set(version_keys) != set(version_vals):
+    # We didn't see all parts of the version.
+    return None
+
+  return collections.namedtuple('Version', ['major', 'minor'])(
+      major=version_vals['MAJOR'], minor=version_vals['MINOR'])
