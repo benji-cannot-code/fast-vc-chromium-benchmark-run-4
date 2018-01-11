9b120fb470fbfab8d03b4614ac392c2fb432d1cf
Sadrul Habib Chowdhury <sadrul@chromium.org>
2018-01-11 00:52:46 +0000

    ozone/drm: Use the ctor task-runner if available.
    When running with --mus, OzonePlatform instance is created after the
    thread already has a task-runner. Use that as the task-runner for the
    UI thread from ozone-drm. If the task runner is not available during
    creation (which is the case with regular chrome), then get the task
    runner from the caller of OnGpuProcessLaunched().
    
    Also, update GpuPlatformSupportHost so that OnMessageReceived() can be
    called from any thread. The implementation itself (in drm) is then
    responsible for making sure the message is handled in the right thread.
    This allows the message to be handled in the UI thread when not running
    with mus, and from the window-service thread when mus is running.
    
    Additionally, introduce a lock for the overlay cache, since it can now
    be accessed from multiple threads with mus (UI thread by the layer
    compositor, and the window-server thread for handling the relevant IPC
    messages from the viz/gpu process).
    
    BUG=795069
    
    Change-Id: Ieeba8ec5a536bc180d3d8a8ca06cce03e6e39f10
    Reviewed-on: https://chromium-review.googlesource.com/830749
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528509}

--- ui/ozone/platform/drm/host/drm_gpu_platform_support_host.cc
+++ ui/ozone/platform/drm/host/drm_gpu_platform_support_host.cc
@@ -7,7 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 
+#include "base/command_line.h"
+#include "base/threading/thread_task_runner_handle.h"
 #include "base/trace_event/trace_event.h"
+#include "ui/base/ui_base_switches.h"
 #include "ui/ozone/common/gpu/ozone_gpu_message_params.h"
 #include "ui/ozone/common/gpu/ozone_gpu_messages.h"
 #include "ui/ozone/platform/drm/common/drm_util.h"
@@ -84,7 +87,17 @@ void CursorIPC::Send(IPC::Message* message) {
 }  // namespace
 
 DrmGpuPlatformSupportHost::DrmGpuPlatformSupportHost(DrmCursor* cursor)
-    : cursor_(cursor), weak_ptr_factory_(this) {}
+    : ui_runner_(base::ThreadTaskRunnerHandle::IsSet()
+                     ? base::ThreadTaskRunnerHandle::Get()
+                     : nullptr),
+      cursor_(cursor),
+      weak_ptr_factory_(this) {
+  if (ui_runner_) {
+    weak_ptr_ = weak_ptr_factory_.GetWeakPtr();
+  } else {
+    DCHECK(!base::CommandLine::ForCurrentProcess()->HasSwitch(switches::kMus));
+  }
+}
 
 DrmGpuPlatformSupportHost::~DrmGpuPlatformSupportHost() {}
 
@@ -110,8 +123,12 @@ void DrmGpuPlatformSupportHost::OnGpuProcessLaunched(
     scoped_refptr<base::SingleThreadTaskRunner> ui_runner,
     scoped_refptr<base::SingleThreadTaskRunner> send_runner,
     const base::Callback<void(IPC::Message*)>& send_callback) {
-  DCHECK(!ui_runner->BelongsToCurrentThread());
-  ui_runner_ = std::move(ui_runner);
+  // If there was a task runner set during construction, prefer using that.
+  if (!ui_runner_) {
+    ui_runner_ = std::move(ui_runner);
+    weak_ptr_ = weak_ptr_factory_.GetWeakPtr();
+  }
+  DCHECK(!ui_runner_->BelongsToCurrentThread());
   TRACE_EVENT1("drm", "DrmGpuPlatformSupportHost::OnGpuProcessLaunched",
                "host_id", host_id);
   host_id_ = host_id;
@@ -122,8 +139,9 @@ void DrmGpuPlatformSupportHost::OnGpuProcessLaunched(
     observer.OnGpuProcessLaunched();
 
   ui_runner_->PostTask(
-      FROM_HERE, base::Bind(&DrmGpuPlatformSupportHost::OnChannelEstablished,
-                            weak_ptr_factory_.GetWeakPtr()));
+      FROM_HERE,
+      base::BindOnce(&DrmGpuPlatformSupportHost::OnChannelEstablished,
+                     weak_ptr_));
 }
 
 void DrmGpuPlatformSupportHost::OnChannelDestroyed(int host_id) {
@@ -142,13 +160,17 @@ void DrmGpuPlatformSupportHost::OnChannelDestroyed(int host_id) {
 
 }
 
-bool DrmGpuPlatformSupportHost::OnMessageReceived(const IPC::Message& message) {
-  if (OnMessageReceivedForDrmDisplayHostManager(message))
-    return true;
-  if (OnMessageReceivedForDrmOverlayManager(message))
-    return true;
-
-  return false;
+void DrmGpuPlatformSupportHost::OnMessageReceived(const IPC::Message& message) {
+  DCHECK(ui_runner_);
+  if (ui_runner_->BelongsToCurrentThread()) {
+    if (OnMessageReceivedForDrmDisplayHostManager(message))
+      return;
+    OnMessageReceivedForDrmOverlayManager(message);
+  } else {
+    ui_runner_->PostTask(
+        FROM_HERE, base::BindOnce(&DrmGpuPlatformSupportHost::OnMessageReceived,
+                                  weak_ptr_, message));
+  }
 }
 
 bool DrmGpuPlatformSupportHost::Send(IPC::Message* message) {
