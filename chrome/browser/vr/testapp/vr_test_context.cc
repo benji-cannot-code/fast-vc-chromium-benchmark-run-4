ff3a0cb01a9e2c4d5198b9f1d91f27ed16efe6aa
Ian Vollick <vollick@chromium.org>
2018-01-11 22:52:28 +0000

    [vr] Fix testapp build
    Depending or your build config, the testapp will currently fail to build
    due to an unused function. This CL elides that function when it's not
    used.
    
    TBR=tiborg@chromium.org
    
    Bug: None
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I42beb5ce770749475e2a7b92b95f64d05845640c
    Reviewed-on: https://chromium-review.googlesource.com/862263
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528792}

--- chrome/browser/vr/testapp/vr_test_context.cc
+++ chrome/browser/vr/testapp/vr_test_context.cc
@@ -64,6 +64,7 @@ void RotateToward(const gfx::Vector3dF& fwd, gfx::Transform* transform) {
   transform->PreconcatTransform(gfx::Transform(quat));
 }
 
+#if defined(GOOGLE_CHROME_BUILD)
 bool LoadPng(int resource_id, std::unique_ptr<SkBitmap>* out_image) {
   base::StringPiece data =
       ui::ResourceBundle::GetSharedInstance().GetRawDataResource(resource_id);
@@ -72,6 +73,7 @@ bool LoadPng(int resource_id, std::unique_ptr<SkBitmap>* out_image) {
       reinterpret_cast<const unsigned char*>(data.data()), data.size(),
       out_image->get());
 }
+#endif
 
 }  // namespace
 
