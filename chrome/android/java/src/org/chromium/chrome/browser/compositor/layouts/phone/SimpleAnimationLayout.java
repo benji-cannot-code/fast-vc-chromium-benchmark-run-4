5f108a8eab40deedfd3182d0770b3d659b6da3ea
Matthew Jones <mdjones@chromium.org>
2018-01-11 18:15:34 +0000

    Replace animation property in SimpleAnimationLayout
    This change replaces the ChromeAnimation property DISCARD_AMOUNT in
    SimpleLayoutAnimation with a CompositorAnimator.
    
    BUG=750381
    
    Change-Id: Ia9b774e52121f1b2fbc8d6de8b8495703760f0f5
    Reviewed-on: https://chromium-review.googlesource.com/832906
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528668}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/phone/SimpleAnimationLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/phone/SimpleAnimationLayout.java
@@ -10,7 +10,7 @@ import android.graphics.RectF;
 import android.view.animation.Interpolator;
 
 import org.chromium.chrome.browser.compositor.LayerTitleCache;
-import org.chromium.chrome.browser.compositor.layouts.ChromeAnimation.Animatable;
+import org.chromium.chrome.browser.compositor.animation.CompositorAnimator;
 import org.chromium.chrome.browser.compositor.layouts.Layout;
 import org.chromium.chrome.browser.compositor.layouts.LayoutRenderHost;
 import org.chromium.chrome.browser.compositor.layouts.LayoutUpdateHost;
@@ -34,12 +34,9 @@ import java.util.LinkedList;
 /**
  * This class handles animating the opening of new tabs.
  */
-public class SimpleAnimationLayout
-        extends Layout implements Animatable<SimpleAnimationLayout.Property> {
-    /**
-     * Animation properties
-     */
-    public enum Property { DISCARD_AMOUNT }
+public class SimpleAnimationLayout extends Layout {
+    /** Animation for discarding a tab. */
+    private CompositorAnimator mDiscardAnimator;
 
     /** Duration of the first step of the background animation: zooming out, rotating in */
     private static final long BACKGROUND_STEP1_DURATION = 300;
@@ -336,9 +333,11 @@ public class SimpleAnimationLayout
 
             forceAnimationToFinish();
             mAnimatedTab = mClosedTab;
-            addToAnimation(this, Property.DISCARD_AMOUNT, 0, getDiscardRange(),
-                    TAB_CLOSED_ANIMATION_DURATION, 0, false,
-                    BakedBezierInterpolator.FADE_OUT_CURVE);
+            mDiscardAnimator = CompositorAnimator.ofFloat(getAnimationHandler(), 0,
+                    getDiscardRange(), TAB_CLOSED_ANIMATION_DURATION,
+                    (CompositorAnimator a) -> setDiscardAmount(a.getAnimatedValue()));
+            mDiscardAnimator.setInterpolator(BakedBezierInterpolator.FADE_OUT_CURVE);
+            mDiscardAnimator.start();
 
             mClosedTab = null;
             if (nextModel != null) {
@@ -378,8 +377,9 @@ public class SimpleAnimationLayout
     }
 
     @Override
-    public boolean onUpdateAnimation(long time, boolean jumpToEnd) {
-        return super.onUpdateAnimation(time, jumpToEnd) && mClosedTab == null;
+    protected void forceAnimationToFinish() {
+        super.forceAnimationToFinish();
+        if (mDiscardAnimator != null) mDiscardAnimator.end();
     }
 
     /**
@@ -390,24 +390,6 @@ public class SimpleAnimationLayout
         mAnimatedTab = null;
         mClosedTab = null;
     }
-
-    /**
-     * Sets a property for an animation.
-     * @param prop The property to update
-     * @param value New value of the property
-     */
-    @Override
-    public void setProperty(Property prop, float value) {
-        switch (prop) {
-            case DISCARD_AMOUNT:
-                setDiscardAmount(value);
-                break;
-            default:
-        }
-    }
-
-    @Override
-    public void onPropertyAnimationFinished(Property prop) {}
 
     @Override
     protected EventFilter getEventFilter() {
