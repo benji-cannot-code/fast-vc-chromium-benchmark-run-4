a6306ea27f0f19fe1783ed13447f63ed8c74d53f
Xiaocheng Hu <xiaochengh@chromium.org>
2018-01-11 04:21:47 +0000

    [LayoutNG] Stop emitting ZWS for collapsed space after no-wrap space
    When there is a collapsed space following a nowrap space, we used to convert
    the collapsed space into a ZWS to allow wrapping, which, however, makes
    TextIterator emit a ZWS for the collapsed space.
    
    This patch fixes the issue by not treating the ZWS part of the text node, but
    as generated text between the two text nodes instead.
    
    Bug: 800887
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: I6cda36042b145f3733a33067f9787fd6c2fa4c94
    Reviewed-on: https://chromium-review.googlesource.com/861056
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528558}

--- third_party/WebKit/Source/core/editing/iterators/TextIteratorTest.cpp
+++ third_party/WebKit/Source/core/editing/iterators/TextIteratorTest.cpp
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/editing/testing/EditingTestBase.h"
 #include "core/frame/LocalFrameView.h"
 #include "core/html/forms/TextControlElement.h"
+#include "platform/testing/RuntimeEnabledFeaturesTestHelpers.h"
 
 namespace blink {
 namespace text_iterator_test {
@@ -134,6 +135,19 @@ Range* TextIteratorTest::GetBodyRange() const {
   range->selectNode(GetDocument().body());
   return range;
 }
+
+class ParameterizedTextIteratorTest
+    : public ::testing::WithParamInterface<bool>,
+      private ScopedLayoutNGForTest,
+      private ScopedLayoutNGPaintFragmentsForTest,
+      public TextIteratorTest {
+ public:
+  ParameterizedTextIteratorTest()
+      : ScopedLayoutNGForTest(GetParam()),
+        ScopedLayoutNGPaintFragmentsForTest(GetParam()) {}
+};
+
+INSTANTIATE_TEST_CASE_P(All, ParameterizedTextIteratorTest, ::testing::Bool());
 
 TEST_F(TextIteratorTest, BitStackOverflow) {
   const unsigned kBitsInWord = sizeof(unsigned) * 8;
@@ -1012,6 +1026,11 @@ TEST_F(TextIteratorTest, BasicIterationInputiWithBr) {
   const Position end = Position::LastPositionInNode(*shadow_root);
   GetDocument().UpdateStyleAndLayout();
   EXPECT_EQ("[b]", IteratePartial<DOMTree>(start, end));
+}
+
+TEST_P(ParameterizedTextIteratorTest, NoZWSForSpaceAfterNoWrapSpace) {
+  SetBodyContent("<span style='white-space: nowrap'>foo </span> bar");
+  EXPECT_EQ("[foo ][bar]", Iterate<DOMTree>());
 }
 
 }  // namespace text_iterator_test
