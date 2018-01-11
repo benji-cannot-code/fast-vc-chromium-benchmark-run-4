61f36bb78a99c33807bb146332256551c963613b
sczs <sczs@chromium.org>
2018-01-11 07:42:53 +0000

    [ios] Add M letter to tabSwitcherButton when memexTabSwitcher is enabled
    Bug: 
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I7a22ea87facb14894fd51f0c6a71d45e869a2866
    Reviewed-on: https://chromium-review.googlesource.com/861225
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528574}

--- ios/chrome/browser/ui/toolbar/clean/toolbar_view_controller.mm
+++ ios/chrome/browser/ui/toolbar/clean/toolbar_view_controller.mm
@@ -491,6 +491,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     }
   }
 
+  // TODO(crbug.com/799601): Delete this once its not needed.
+  if (base::FeatureList::IsEnabled(kMemexTabSwitcher)) {
+    tabStripButtonTitle = @"M";
+    [[self.view.tabSwitchStripButton titleLabel]
+        setFont:[fontLoader boldFontOfSize:kFontSizeFewerThanTenTabs]];
+  }
+
   [self.view.tabSwitchStripButton setTitle:tabStripButtonTitle
                                   forState:UIControlStateNormal];
   [self.view.tabSwitchStripButton setAccessibilityValue:tabStripButtonValue];
