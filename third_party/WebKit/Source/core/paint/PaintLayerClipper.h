fd0428a729c43b94c526e41346d6b9d7c5b5bf98
Chris Harrelson <chrishtr@chromium.org>
2018-01-12 06:19:20 +0000

    Don't include scroll offset in absolute clip rects for compositing bounds.
    The scroll offset can be added after the fact. Not including scrolling lets us
    cache the results regardless of scroll.
    
    Bug: 400589
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I3b81507cd961986afd758587554669c152d8de5d
    Reviewed-on: https://chromium-review.googlesource.com/862391
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528892}

--- third_party/WebKit/Source/core/paint/PaintLayerClipper.h
+++ third_party/WebKit/Source/core/paint/PaintLayerClipper.h
@@ -61,7 +61,8 @@ class PropertyTreeState;
 
 enum ShouldRespectOverflowClipType {
   kIgnoreOverflowClip,
-  kRespectOverflowClip
+  kRespectOverflowClip,
+  kIgnoreOverflowClipAndScroll
 };
 
 class ClipRectsContext {
