9e837974078b3e5e3899029a5242078c890d33dc
Scott Chen <scottchen@chromium.org>
2018-01-12 19:47:41 +0000

    MD Settings: add error handling for spell-check add-word page
    TBR=groby
    
    Bug: 726279
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: If98b70dd2aa8ab652a515092b52264365364941f
    Reviewed-on: https://chromium-review.googlesource.com/848305
    Commit-Queue: Scott Chen <scottchen@chromium.org>
    Reviewed-by: Dave Schuyler <dschuyler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529029}

--- chrome/browser/resources/settings/languages_page/edit_dictionary_page.js
+++ chrome/browser/resources/settings/languages_page/edit_dictionary_page.js
@@ -7,6 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @fileoverview 'settings-edit-dictionary-page' is a sub-page for editing
  * the "dictionary" of custom words used for spell check.
  */
+
+// Max valid word size defined in
+// https://cs.chromium.org/chromium/src/components/spellcheck/common/spellcheck_common.h?l=28
+const MAX_CUSTOM_DICTIONARY_WORD_BYTES = 99;
+
 Polymer({
   is: 'settings-edit-dictionary-page',
 
@@ -55,13 +60,32 @@ Polymer({
   },
 
   /**
-   * Check if the new word text-field is empty.
+   * Check if the field is empty or invalid.
+   * @param {string} word
+   * @return {boolean}
    * @private
-   * @param {string} value
-   * @return {boolean} true if value is empty, false otherwise.
    */
-  validateWord_: function(value) {
-    return !!value.trim();
+  disableAddButton_: function(word) {
+    return word.trim().length == 0 || this.isWordInvalid_(word);
+  },
+
+  /**
+   * If the word is invalid, returns true (or a message if one is provided).
+   * Otherwise returns false.
+   * @param {string} word
+   * @param {string} duplicateError
+   * @param {string} lengthError
+   * @return {string|boolean}
+   * @private
+   */
+  isWordInvalid_: function(word, duplicateError, lengthError) {
+    const trimmedWord = word.trim();
+    if (this.words_.indexOf(trimmedWord) != -1)
+      return duplicateError || true;
+    else if (trimmedWord.length > MAX_CUSTOM_DICTIONARY_WORD_BYTES)
+      return lengthError || true;
+
+    return false;
   },
 
   /**
@@ -97,10 +121,12 @@ Polymer({
    * @param {!{detail: !{key: string}}} e
    */
   onKeysPress_: function(e) {
-    if (e.detail.key == 'enter')
+    if (e.detail.key == 'enter' &&
+        !this.disableAddButton_(this.newWordValue_)) {
       this.addWordFromInput_();
-    else if (e.detail.key == 'esc')
+    } else if (e.detail.key == 'esc') {
       e.detail.keyboardEvent.target.value = '';
+    }
   },
 
   /**
@@ -129,10 +155,7 @@ Polymer({
     if (!word)
       return;
 
-    const index = this.words_.indexOf(word);
-    if (index == -1) {
-      this.languageSettingsPrivate.addSpellcheckWord(word);
-    }
+    this.languageSettingsPrivate.addSpellcheckWord(word);
   },
 
   /**
