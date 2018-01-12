85d0e71f898dce73fdf4f7cebc6e8b12b72258da
Zhenyao Mo <zmo@chromium.org>
2018-01-12 03:43:08 +0000

    Add a test_group field for GPU blacklist and driver bug list.
    This is not used yet, but with a unittest to make sure the mechanism works.
    
    BUG=801331
    TEST=gpu_unittests
    R=kbr@chromium.org
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Iac97dd0a73a2c0275f9645eac79a12e948c05135
    Reviewed-on: https://chromium-review.googlesource.com/862729
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528870}

--- gpu/config/gpu_control_list_testing_arrays_and_structs_autogen.h
+++ gpu/config/gpu_control_list_testing_arrays_and_structs_autogen.h
@@ -94,6 +94,7 @@ const GpuControlList::More kMoreForEntry9 = {
     true,       // direct_rendering
     {GpuControlList::kUnknown, GpuControlList::kVersionStyleNumerical, nullptr,
      nullptr},  // gpu_count
+    0,          // test_group
 };
 
 const int kFeatureListForEntry10[1] = {
@@ -111,6 +112,7 @@ const GpuControlList::More kMoreForEntry10 = {
     true,       // direct_rendering
     {GpuControlList::kUnknown, GpuControlList::kVersionStyleNumerical, nullptr,
      nullptr},  // gpu_count
+    0,          // test_group
 };
 
 const int kFeatureListForEntry11[1] = {
@@ -128,6 +130,7 @@ const GpuControlList::More kMoreForEntry11 = {
     true,       // direct_rendering
     {GpuControlList::kUnknown, GpuControlList::kVersionStyleNumerical, nullptr,
      nullptr},  // gpu_count
+    0,          // test_group
 };
 
 const int kFeatureListForEntry12[1] = {
@@ -237,6 +240,7 @@ const GpuControlList::More kMoreForEntry23 = {
     true,       // direct_rendering
     {GpuControlList::kUnknown, GpuControlList::kVersionStyleNumerical, nullptr,
      nullptr},  // gpu_count
+    0,          // test_group
 };
 
 const int kFeatureListForEntry24[3] = {
@@ -402,6 +406,7 @@ const GpuControlList::More kMoreForEntry40 = {
     true,       // direct_rendering
     {GpuControlList::kUnknown, GpuControlList::kVersionStyleNumerical, nullptr,
      nullptr},  // gpu_count
+    0,          // test_group
 };
 
 const int kFeatureListForEntry41[1] = {
@@ -471,6 +476,7 @@ const GpuControlList::More kMoreForEntry48 = {
     true,       // direct_rendering
     {GpuControlList::kUnknown, GpuControlList::kVersionStyleNumerical, nullptr,
      nullptr},  // gpu_count
+    0,          // test_group
 };
 
 const int kFeatureListForEntry49[1] = {
@@ -548,10 +554,47 @@ const GpuControlList::More kMoreForEntry57 = {
     false,      // direct_rendering
     {GpuControlList::kUnknown, GpuControlList::kVersionStyleNumerical, nullptr,
      nullptr},  // gpu_count
+    0,          // test_group
 };
 
 const int kFeatureListForEntry58[1] = {
     TEST_FEATURE_0,
+};
+
+const int kFeatureListForEntry59[1] = {
+    TEST_FEATURE_0,
+};
+
+const GpuControlList::More kMoreForEntry59 = {
+    GpuControlList::kGLTypeNone,  // gl_type
+    {GpuControlList::kUnknown, GpuControlList::kVersionStyleNumerical, nullptr,
+     nullptr},  // gl_version
+    {GpuControlList::kUnknown, GpuControlList::kVersionStyleNumerical, nullptr,
+     nullptr},  // pixel_shader_version
+    false,      // in_process_gpu
+    0,          // gl_reset_notification_strategy
+    true,       // direct_rendering
+    {GpuControlList::kUnknown, GpuControlList::kVersionStyleNumerical, nullptr,
+     nullptr},  // gpu_count
+    1,          // test_group
+};
+
+const int kFeatureListForEntry60[1] = {
+    TEST_FEATURE_1,
+};
+
+const GpuControlList::More kMoreForEntry60 = {
+    GpuControlList::kGLTypeNone,  // gl_type
+    {GpuControlList::kUnknown, GpuControlList::kVersionStyleNumerical, nullptr,
+     nullptr},  // gl_version
+    {GpuControlList::kUnknown, GpuControlList::kVersionStyleNumerical, nullptr,
+     nullptr},  // pixel_shader_version
+    false,      // in_process_gpu
+    0,          // gl_reset_notification_strategy
+    true,       // direct_rendering
+    {GpuControlList::kUnknown, GpuControlList::kVersionStyleNumerical, nullptr,
+     nullptr},  // gpu_count
+    2,          // test_group
 };
 
 }  // namespace gpu
--- gpu/config/process_json.py
+++ gpu/config/process_json.py
@@ -255,6 +255,8 @@ def write_string_value(string, name_tag, data_file):
 def write_boolean_value(value, name_tag, data_file):
   data_file.write('%s,  // %s\n' % (str(value).lower(), name_tag))
 
+def write_integer_value(value, name_tag, data_file):
+  data_file.write('%s,  // %s\n' % (str(value), name_tag))
 
 def write_machine_model_info(entry_id, is_exception, exception_id,
                              machine_model_name, machine_model_version,
@@ -361,6 +363,7 @@ def write_conditions(entry_id, is_exception, exception_id, entry,
   gl_reset_notification_strategy = None
   direct_rendering = True
   gpu_count = None
+  test_group = 0
   machine_model_name = None
   machine_model_version = None
   exception_count = 0
@@ -431,6 +434,9 @@ def write_conditions(entry_id, is_exception, exception_id, entry,
       direct_rendering = False
     elif key == 'gpu_count':
       gpu_count = entry[key]
+    elif key == 'test_group':
+      assert entry[key] > 0
+      test_group = entry[key]
     elif key == 'machine_model_name':
       machine_model_name = entry[key]
     elif key == 'machine_model_version':
@@ -469,11 +475,11 @@ def write_conditions(entry_id, is_exception, exception_id, entry,
   # group a bunch of less used conditions
   if (gl_version != None or pixel_shader_version != None or in_process_gpu or
       gl_reset_notification_strategy != None or (not direct_rendering) or
-      gpu_count != None):
+      gpu_count != None or test_group != 0):
     write_entry_more_data(entry_id, is_exception, exception_id, gl_type,
                           gl_version, pixel_shader_version, in_process_gpu,
                           gl_reset_notification_strategy, direct_rendering,
-                          gpu_count, data_file, data_helper_file)
+                          gpu_count, test_group, data_file, data_helper_file)
   else:
     data_file.write('nullptr,  // more conditions\n')
 
@@ -481,7 +487,7 @@ def write_conditions(entry_id, is_exception, exception_id, entry,
 def write_entry_more_data(entry_id, is_exception, exception_id, gl_type,
                           gl_version, pixel_shader_version, in_process_gpu,
                           gl_reset_notification_strategy, direct_rendering,
-                          gpu_count, data_file, data_helper_file):
+                          gpu_count, test_group, data_file, data_helper_file):
   # write more data
   var_name = 'kMoreForEntry' + str(entry_id)
   if is_exception:
@@ -497,6 +503,7 @@ def write_entry_more_data(entry_id, is_exception, exception_id, gl_type,
                          gl_reset_notification_strategy)
   write_boolean_value(direct_rendering, 'direct_rendering', data_helper_file)
   write_version(gpu_count, 'gpu_count', data_helper_file)
+  write_integer_value(test_group, 'test_group', data_helper_file)
   data_helper_file.write('};\n\n')
   # reference more data in entry
   data_file.write('&%s,  // more data\n' % var_name)
