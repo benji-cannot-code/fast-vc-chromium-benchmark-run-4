50733dec8403dee3d5d6cb21e09abfa733468e81
Hajime Hoshi <hajimehoshi@chromium.org>
2018-01-12 09:43:26 +0000

    Deprecate RenderThradImpl::GetMainTaskRunner() and replace its usages
    This is part of efforts to replace base::ThreadTaskRunnerHandle::Get()
    and SequencedTaskRunnerHandle::Get() with other appropriate task runners
    in the renderer.
    
    Bug: 786332
    Change-Id: If002697262af2982eec003e89aba9039b120d838
    Reviewed-on: https://chromium-review.googlesource.com/858880
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528914}

--- content/renderer/render_thread_impl.cc
+++ content/renderer/render_thread_impl.cc
@@ -612,7 +612,7 @@ void RenderThreadImpl::SetRendererBlinkPlatformImplForTesting(
 
 // static
 scoped_refptr<base::SingleThreadTaskRunner>
-RenderThreadImpl::GetMainTaskRunner() {
+RenderThreadImpl::DeprecatedGetMainTaskRunner() {
   return g_main_task_runner.Get();
 }
 
@@ -730,7 +730,7 @@ void RenderThreadImpl::Init(
   registry->AddInterface(
       base::BindRepeating(&AppCacheDispatcher::Bind,
                           base::Unretained(appcache_dispatcher())),
-      GetMainTaskRunner());
+      GetRendererScheduler()->IPCTaskRunner());
   dom_storage_dispatcher_.reset(new DomStorageDispatcher());
   main_thread_indexed_db_dispatcher_.reset(new IndexedDBDispatcher());
   main_thread_cache_storage_dispatcher_.reset(
