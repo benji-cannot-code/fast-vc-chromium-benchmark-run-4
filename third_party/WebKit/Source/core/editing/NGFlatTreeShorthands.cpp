debaf18a037b8f52a22aba2fc9f80415d3b885cb
Xiaocheng Hu <xiaochengh@chromium.org>
2018-01-12 20:13:17 +0000

    Introduce NGFlatTreeShorthands.h/cpp
    LayoutNG utility functions only take DOM-tree varaints of editing
    objects (Position, EphemeralRange, ...), and do not want to be
    complicated by flat tree.
    
    To reduce verbosity of the flat-to-DOM tree conversion in editing
    functions, this patch introduces NGFlatTreeShorthands to encapsulate
    such conversions.
    
    Bug: 
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: I5b3c1b446648cc6461c7cd2438b2cee12896ea82
    Reviewed-on: https://chromium-review.googlesource.com/862782
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529040}

---
+++ third_party/WebKit/Source/core/editing/NGFlatTreeShorthands.cpp
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/editing/NGFlatTreeShorthands.h"
+
+#include "core/editing/Position.h"
+#include "core/layout/ng/inline/ng_offset_mapping.h"
+
+namespace blink {
+
+const LayoutBlockFlow* NGInlineFormattingContextOf(
+    const PositionInFlatTree& position) {
+  return NGInlineFormattingContextOf(ToPositionInDOMTree(position));
+}
+
+}  // namespace blink
---
+++ third_party/WebKit/Source/core/editing/NGFlatTreeShorthands.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NGFlatTreeShorthands_h
+#define NGFlatTreeShorthands_h
+
+#include "core/editing/Forward.h"
+
+namespace blink {
+
+class LayoutBlockFlow;
+
+// This file contains shorthands that converts FlatTree-variants of editing
+// objects into DOM tree variants, and then pass them to LayoutNG utility
+// functions that accept DOM tree variants only.
+
+const LayoutBlockFlow* NGInlineFormattingContextOf(const PositionInFlatTree&);
+
+}  // namespace blink
+
+#endif
