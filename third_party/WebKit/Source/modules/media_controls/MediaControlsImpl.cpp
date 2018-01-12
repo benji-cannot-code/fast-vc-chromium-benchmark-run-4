5297943ee5ec4cbf034b979979b65e5ef993a0cd
Tommy Steimel <steimel@chromium.org>
2018-01-12 17:41:49 +0000

    Hide the loading panel when the media controls are hidden
    This CL adds handler methods to the loading panel element so that the
    MediaControlsImpl can inform the loading panel that the controls are
    hidden, and therefore the loading panel should hide as well. This
    solves a bug where the loading panel was displaying on videos that did
    not use the native media controls.
    
    Bug: 787448
    Change-Id: Ie0349b2090a4ad354974a28d8c6469349e1968e4
    Reviewed-on: https://chromium-review.googlesource.com/839983
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528985}

--- third_party/WebKit/Source/modules/media_controls/MediaControlsImpl.cpp
+++ third_party/WebKit/Source/modules/media_controls/MediaControlsImpl.cpp
@@ -712,6 +712,8 @@ void MediaControlsImpl::MaybeShow() {
     MakeOpaque();
   if (download_iph_manager_)
     download_iph_manager_->SetControlsVisibility(true);
+  if (loading_panel_)
+    loading_panel_->OnControlsShown();
 }
 
 void MediaControlsImpl::Hide() {
@@ -726,6 +728,8 @@ void MediaControlsImpl::Hide() {
     overlay_play_button_->SetIsWanted(false);
   if (download_iph_manager_)
     download_iph_manager_->SetControlsVisibility(false);
+  if (loading_panel_)
+    loading_panel_->OnControlsHidden();
 }
 
 bool MediaControlsImpl::IsVisible() const {
