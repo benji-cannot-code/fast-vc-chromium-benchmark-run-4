fab1f80c496b5592fbcb43ca1d84bcc3a1dc7b28
Vasilii Sukhanov <vasilii@chromium.org>
2018-01-12 16:43:59 +0000

    Handle properly addition of a new credential in the password UI.
    ManagePasswordsUIController tracks changes to the password store in order to show relevant credentials in the bubble. The condition was comparing the whole URL. When a credential for the same origin is added it should be depicted in the "Manage passwords" bubble.
    
    Bug: 799414
    Change-Id: Ibd167e02ff0aa6b573ed73f954dacd3363f632a7
    Reviewed-on: https://chromium-review.googlesource.com/860012
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Maxim Kolosovskiy <kolos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528969}

--- chrome/browser/ui/passwords/manage_passwords_state.cc
+++ chrome/browser/ui/passwords/manage_passwords_state.cc
@@ -229,7 +229,7 @@ void ManagePasswordsState::ClearData() {
 }
 
 void ManagePasswordsState::AddForm(const autofill::PasswordForm& form) {
-  if (form.origin != origin_)
+  if (form.origin.GetOrigin() != origin_.GetOrigin())
     return;
   if (UpdateForm(form))
     return;
