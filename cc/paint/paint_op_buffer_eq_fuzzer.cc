ab6044dd753d7c59b172188ea9f9ed5c36a22a16
Vladimir Levin <vmpstr@chromium.org>
2018-01-12 04:11:48 +0000

    oop: Add more types to PaintOpHelper::ToString + fix comparison.
    This patch adds a bunch more types to the ToString serialization. It
    also changes the comparison of crop rect to handle NaN values.
    
    R=enne@chromium.org
    
    Bug: 801001
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: Ic5d465be6d659a60d33133864c4877594c4a6271
    Reviewed-on: https://chromium-review.googlesource.com/861918
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Reviewed-by: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528873}

--- cc/paint/paint_op_buffer_eq_fuzzer.cc
+++ cc/paint/paint_op_buffer_eq_fuzzer.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include "cc/paint/paint_op_buffer.h"
+#include "cc/test/paint_op_helper.h"
 #include "cc/test/transfer_cache_test_helper.h"
 #include "third_party/skia/include/utils/SkNoDrawCanvas.h"
 
@@ -116,7 +117,9 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
       serialized3.get(), written_bytes2, serialize_options);
   CHECK_EQ(written_bytes2, written_bytes3);
 
-  CHECK(*deserialized_op1 == *deserialized_op2);
+  CHECK(*deserialized_op1 == *deserialized_op2)
+      << "\n1: " << cc::PaintOpHelper::ToString(deserialized_op1)
+      << "\n2: " << cc::PaintOpHelper::ToString(deserialized_op2);
   CHECK_EQ(0, memcmp(serialized2.get(), serialized3.get(), written_bytes2));
 
   deserialized_op1->DestroyThis();
