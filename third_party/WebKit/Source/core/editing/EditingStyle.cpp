3cce8fe7768a79c4cd090ffa5f91afa10c5a74db
Zhuoyu Qian <zhuoyu.qian@samsung.com>
2018-01-12 01:42:02 +0000

    Get rid of ToCSSPropertyValue().
    Remove ToCSSPropertyValue(), just use CSSPropertyValue().
    
    BUG=
    
    Signed-off-by: Zhuoyu Qian <zhuoyu.qian@samsung.com>
    Change-Id: I953dcb4884328a6fccd6951397c9b6d8dd4834f4
    Reviewed-on: https://chromium-review.googlesource.com/861552
    Reviewed-by: meade_UTC10 <meade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528841}

--- third_party/WebKit/Source/core/editing/EditingStyle.cpp
+++ third_party/WebKit/Source/core/editing/EditingStyle.cpp
@@ -1361,8 +1361,10 @@ void EditingStyle::MergeStyle(const CSSPropertyValueSet* style,
       value = nullptr;
     }
 
-    if (mode == kOverrideValues || (mode == kDoNotOverrideValues && !value))
-      mutable_style_->SetProperty(property.ToCSSPropertyValue());
+    if (mode == kOverrideValues || (mode == kDoNotOverrideValues && !value)) {
+      mutable_style_->SetProperty(
+          CSSPropertyValue(property.PropertyMetadata(), property.Value()));
+    }
   }
 }
 
