01b0cd8d14a7ef6c66cf64829e0793a4506857f5
Tetsui Ohkubo <tetsui@chromium.org>
2018-01-12 07:42:08 +0000

    Reland "Anchor system tray bubble to the rightmost on tablet mode."
    This is a reland of 428919da9e6fca97c109fe9f304634133b983c44
    Change from the original CL:
    Stopped changing next focusable view of web notification tray
    depending on whether the device is in tablet mode or not.
    Original change's description:
    > Anchor system tray bubble to the rightmost on tablet mode.
    >
    > The system tray bubble and notification center tray bubble were not
    > anchored to the rightmost on tablet mode.
    > This CL adds GetSystemTrayAnchor() to StatusAreaWidget and Shelf,
    > and anchor these bubbles to the TrayBackgroundView that is returned by
    > the method.
    > The overview button on the rightmost will be used when tablet mode is
    > enabled.
    >
    > TEST=manual
    > BUG=769512
    >
    > Change-Id: I3956b75ee524a31056da3a272c879997b2274e5c
    > Reviewed-on: https://chromium-review.googlesource.com/844053
    > Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    > Reviewed-by: James Cook <jamescook@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#528225}
    
    Bug: 769512
    Change-Id: I648f60a2dc7067b452461f5d6cf5e25d45037f50
    Reviewed-on: https://chromium-review.googlesource.com/861482
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528904}

--- ash/system/status_area_widget.h
+++ ash/system/status_area_widget.h
@@ -25,6 +25,7 @@ class PaletteTray;
 class Shelf;
 class StatusAreaWidgetDelegate;
 class SystemTray;
+class TrayBackgroundView;
 class VirtualKeyboardTray;
 class WebNotificationTray;
 
@@ -51,6 +52,12 @@ class ASH_EXPORT StatusAreaWidget : public views::Widget,
 
   // Sets system tray visibility. Shows or hides widget if needed.
   void SetSystemTrayVisibility(bool visible);
+
+  // Get the tray button that the system tray bubble and the notification center
+  // bubble will be anchored. Usually |system_tray_|, but when the overview
+  // button is visible (i.e. tablet mode is enabled), it returns
+  // |overview_button_tray_|.
+  TrayBackgroundView* GetSystemTrayAnchor() const;
 
   StatusAreaWidgetDelegate* status_area_widget_delegate() {
     return status_area_widget_delegate_;
