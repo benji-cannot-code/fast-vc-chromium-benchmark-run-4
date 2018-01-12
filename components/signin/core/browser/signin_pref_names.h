b39663a06156117a8aa6c4808b9899afb50dd48b
Colin Blundell <blundell@chromium.org>
2018-01-12 15:44:34 +0000

    Remove RefreshTokenAnnotationRequest
    This code is no longer needed (see crbug.com/797883).
    
    Bug: 797883
    Change-Id: I5b1d0b4b1568f24d8fabbf0d7f461c876c2f31b1
    Reviewed-on: https://chromium-review.googlesource.com/857459
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528954}

--- components/signin/core/browser/signin_pref_names.h
+++ components/signin/core/browser/signin_pref_names.h
@@ -20,7 +20,6 @@ extern const char kGoogleServicesAccountId[];
 extern const char kGoogleServicesHostedDomain[];
 extern const char kGoogleServicesLastAccountId[];
 extern const char kGoogleServicesLastUsername[];
-extern const char kGoogleServicesRefreshTokenAnnotateScheduledTime[];
 extern const char kGoogleServicesSigninScopedDeviceId[];
 extern const char kGoogleServicesUserAccountId[];
 extern const char kGoogleServicesUsername[];
