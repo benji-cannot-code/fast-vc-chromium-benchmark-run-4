5f48f5f0dbb1232fa43cfd164c7de55b7474f98d
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-01-12 06:08:53 +0000

    Import wpt@89916db5aad2517bfbd1b0f6dc5ace912d099c0a
    Using wpt-import in Chromium aea690c41bc793de26be1243f233b1079cf7485a.
    With Chromium commits locally applied on WPT:
    b1acc99cf1 "bluetooth: Migrate characteristic tests"
    
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/9946
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    eroman@chromium.org, rsleevi@chromium.org:
      external/wpt/WebCryptoAPI
    
    TBR=rbyers
    
    No-Export: true
    Change-Id: I48996af0f302a63080c0787768781098d4f77b38
    Reviewed-on: https://chromium-review.googlesource.com/862864
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528891}

--- third_party/WebKit/LayoutTests/external/wpt/beacon/beacon-cors.sub.window.js
+++ third_party/WebKit/LayoutTests/external/wpt/beacon/beacon-cors.sub.window.js
@@ -39,4 +39,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     runTests(sampleTests);
 });
 
+// Now test a cross-origin request that doesn't use a safelisted Content-Type and ensure
+// we are applying the proper restrictions. Since a non-safelisted Content-Type request
+// header is used there should be a preflight/options request and we should only succeed
+// send the payload if the proper CORS headers are used.
+{
+    // Implement the self.buildId extension to identify the parameterized
+    // test in the report.
+    self.buildId = function (baseId) {
+        return `${baseId}-PREFLIGHT-ALLOW`;
+    };
+
+    // Implement the self.buildBaseUrl and self.buildTargetUrl extensions
+    // to change the target URL to use a cross-origin domain name.
+    self.buildBaseUrl = function (baseUrl) {
+        return "http://{{domains[www]}}:{{ports[http][0]}}";
+    };
+
+    // Implement the self.buildTargetUrl extension to append a directive
+    // to the handler, that it should return CORS headers for the preflight we expect.
+    self.buildTargetUrl = function (targetUrl) {
+        return `${targetUrl}&origin=http://{{host}}:{{ports[http][0]}}&credentials=true&preflightExpected=true`;
+    }
+
+    runTests(preflightTests);
+}
+
 done();
