d90c3046cfc032fb8d89e7b4529aff4c6e90bec3
Dmitry Skiba <dskiba@chromium.org>
2018-01-12 20:18:37 +0000

    Reorder content inflation, library loading, and renderer warmup.
    On Android Go devices dlopen() called to load Chrome's library can take
    a long time. That blocks UI thread, because Android also calls dlopen()
    during startup, and dlopen() is synchronous.
    
    The solution is to "hide" Chrome's library loading behind UI inflation
    done in setContentView(), since they are comparable in length. To reduce
    pressure on the OS we also delay starting warmup renderer until library
    is loaded.
    
    Surprisingly this change also helps Android One devices running M, because
    library loading takes even longer there, and so UI thread spends long time
    idling, waiting for onCreateWithNative() to be called.
    
    Bug: 796957
    Change-Id: Icb476e66c85f51ead63f1972fc19c0d404104e4c
    Reviewed-on: https://chromium-review.googlesource.com/862390
    Reviewed-by: Maria Khomenko <mariakhomenko@chromium.org>
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Commit-Queue: Dmitry Skiba <dskiba@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529044}

--- chrome/android/java/src/org/chromium/chrome/browser/init/AsyncInitTaskRunner.java
+++ chrome/android/java/src/org/chromium/chrome/browser/init/AsyncInitTaskRunner.java
@@ -30,6 +30,7 @@ import java.util.concurrent.Executor;
 public abstract class AsyncInitTaskRunner {
     private boolean mFetchingVariations;
     private boolean mLibraryLoaded;
+    private boolean mAllocateChildConnection;
 
     private LoadTask mLoadTask;
     private FetchSeedTask mFetchSeedTask;
@@ -136,9 +137,11 @@ public abstract class AsyncInitTaskRunner {
             });
         }
 
-        if (allocateChildConnection) {
-            ChildProcessLauncherHelper.warmUp(ContextUtils.getApplicationContext());
-        }
+        // Remember to allocate child connection once library loading completes. We do it after
+        // the loading to reduce stress on the OS caused by running library loading in parallel
+        // with UI inflation, see AsyncInitializationActivity.setContentViewAndLoadLibrary().
+        mAllocateChildConnection = allocateChildConnection;
+
         mLoadTask = new LoadTask();
         mLoadTask.executeOnExecutor(getExecutor());
     }
@@ -153,6 +156,9 @@ public abstract class AsyncInitTaskRunner {
         }
 
         if (mLibraryLoaded && !mFetchingVariations) {
+            if (mAllocateChildConnection) {
+                ChildProcessLauncherHelper.warmUp(ContextUtils.getApplicationContext());
+            }
             onSuccess();
         }
     }
