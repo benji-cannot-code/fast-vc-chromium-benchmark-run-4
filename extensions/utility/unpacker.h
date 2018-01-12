9046a343a1f98bcfcc400ab551c010d7bcead963
Ivan Afanasyev <ivafanas@yandex-team.ru>
2018-01-12 14:52:43 +0000

    Fix unpack the same icon twice on unpackers layer
    Previous fix tried to return unqiue normalized paths from
    ExtensionIconSet::GetPaths method. But external code uses assumption
    that ExtensionIconSet::GetPaths returns icons paths as they are written
    in extension manifest.
    
    This patchset reverts the previous one and tries to fix the issue on
    unpackers layer:
    1. Unpacker::Run normalizes paths from ExtensionIconSet and removes
       duplicates. It writes normalized paths to kDecodedImagesFilename.
    2. SandboxedUnpacker::RewriteImageFiles knows about normalization and
       does checks for extensions paths accordingly.
    
    This reverts commit 21c6dc80fae7f2767d73438883bab77f5afda583.
    
    Bug: 799418
    Change-Id: I309cd8e54489cfde54c7676d34e15c829291e9cc
    Reviewed-on: https://chromium-review.googlesource.com/856579
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528942}

--- extensions/utility/unpacker.h
+++ extensions/utility/unpacker.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define EXTENSIONS_UTILITY_UNPACKER_H_
 
 #include <memory>
+#include <set>
 #include <string>
 #include <vector>
 
@@ -86,6 +87,10 @@ class Unpacker {
   // Decodes the image at the given path and puts it in our list of decoded
   // images.
   bool AddDecodedImage(const base::FilePath& path);
+
+  // Verifies original image paths from extension. Returns true iff all paths
+  // are valid. Sets an error if at least one path is invalid.
+  bool VerifyOriginalImagePaths(const std::set<base::FilePath>& image_paths);
 
   // Parses the catalog at the given path and puts it in our list of parsed
   // catalogs.
