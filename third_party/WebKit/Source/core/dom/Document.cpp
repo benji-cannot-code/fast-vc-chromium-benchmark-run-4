dcceef17734c25b9c6e4328dc011d2e751b1c96e
Yuta Kitamura <yutak@chromium.org>
2018-01-12 05:24:19 +0000

    Remove all the obsolete calls of WebTaskRunner::Post[Delayed]CancellableTask().
    This patch removes all the use sites of WebTaskRunner::Post[Delayed]-
    CancellableTask(), which is now obsolete in favor of namespace-level
    functions with the same names.
    
    The number of the existing use sites was relatively small, so this patch
    removes the use sites and the definitions of the obsolete functions at
    once.
    
    Bug: 794845
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ieabcfa2e3502d42f9ebccbda4908b88579799a21
    Reviewed-on: https://chromium-review.googlesource.com/861598
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Taiju Tsuiki <tzik@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Yuta Kitamura <yutak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528885}

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -3928,11 +3928,10 @@ void Document::DidRemoveAllPendingBodyStylesheets() {
 void Document::DidLoadAllScriptBlockingResources() {
   // Use wrapWeakPersistent because the task should not keep this Document alive
   // just for executing scripts.
-  execute_scripts_waiting_for_resources_task_handle_ =
-      GetTaskRunner(TaskType::kNetworking)
-          ->PostCancellableTask(
-              FROM_HERE, WTF::Bind(&Document::ExecuteScriptsWaitingForResources,
-                                   WrapWeakPersistent(this)));
+  execute_scripts_waiting_for_resources_task_handle_ = PostCancellableTask(
+      *GetTaskRunner(TaskType::kNetworking), FROM_HERE,
+      WTF::Bind(&Document::ExecuteScriptsWaitingForResources,
+                WrapWeakPersistent(this)));
 
   if (IsHTMLDocument() && body()) {
     // For HTML if we have no more stylesheets to load and we're past the body
@@ -4896,12 +4895,10 @@ void Document::SendSensitiveInputVisibility() {
   if (sensitive_input_visibility_task_.IsActive())
     return;
 
-  sensitive_input_visibility_task_ =
-      GetTaskRunner(TaskType::kUnspecedLoading)
-          ->PostCancellableTask(
-              FROM_HERE,
-              WTF::Bind(&Document::SendSensitiveInputVisibilityInternal,
-                        WrapWeakPersistent(this)));
+  sensitive_input_visibility_task_ = PostCancellableTask(
+      *GetTaskRunner(TaskType::kUnspecedLoading), FROM_HERE,
+      WTF::Bind(&Document::SendSensitiveInputVisibilityInternal,
+                WrapWeakPersistent(this)));
 }
 
 void Document::SendSensitiveInputVisibilityInternal() {
@@ -7180,11 +7177,10 @@ void Document::MaybeQueueSendDidEditFieldInInsecureContext() {
     return;
   }
   logged_field_edit_ = true;
-  sensitive_input_edited_task_ =
-      GetTaskRunner(TaskType::kUserInteraction)
-          ->PostCancellableTask(
-              FROM_HERE, WTF::Bind(&Document::SendDidEditFieldInInsecureContext,
-                                   WrapWeakPersistent(this)));
+  sensitive_input_edited_task_ = PostCancellableTask(
+      *GetTaskRunner(TaskType::kUserInteraction), FROM_HERE,
+      WTF::Bind(&Document::SendDidEditFieldInInsecureContext,
+                WrapWeakPersistent(this)));
 }
 
 CoreProbeSink* Document::GetProbeSink() {
