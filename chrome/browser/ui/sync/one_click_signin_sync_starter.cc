ded71fdabf6fce373e6d5496f3bfc43cc6590bcd
David Roger <droger@chromium.org>
2018-01-12 09:56:32 +0000

    [Dice] Always revoke refresh token when the signin flow is cancelled
    The refresh token was only revoked in some cases (i.e. when the user
    clicked "Undo" at the last confirmation screen). In other cases, such as
    Syncing with a new profile, the token was not deleted.
    
    Bug: 797342
    TBR: pkasting
    Change-Id: I90105d0471c94de525f18a9ae57ab069c67b2432
    Reviewed-on: https://chromium-review.googlesource.com/852293
    Commit-Queue: David Roger <droger@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528916}

--- chrome/browser/ui/sync/one_click_signin_sync_starter.cc
+++ chrome/browser/ui/sync/one_click_signin_sync_starter.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/profiles/profile_window.h"
 #include "chrome/browser/signin/account_tracker_service_factory.h"
+#include "chrome/browser/signin/profile_oauth2_token_service_factory.h"
 #include "chrome/browser/signin/signin_manager_factory.h"
 #include "chrome/browser/signin/signin_tracker_factory.h"
 #include "chrome/browser/signin/signin_util.h"
@@ -39,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/browser_sync/profile_sync_service.h"
 #include "components/prefs/pref_service.h"
 #include "components/signin/core/browser/profile_management_switches.h"
+#include "components/signin/core/browser/profile_oauth2_token_service.h"
 #include "components/signin/core/browser/signin_manager.h"
 #include "components/signin/core/browser/signin_metrics.h"
 #include "components/sync/base/sync_prefs.h"
@@ -376,6 +378,8 @@ void OneClickSigninSyncStarter::CopyCredentialsToNewProfileAndFinishSignin(
   // the signin for the original profile was cancelled (must do this after
   // we have called Initialize() with the new profile, as otherwise this
   // object will get freed when the signin on the old profile is cancelled.
+  // SignoutAndRemoveAllAccounts does not actually remove the accounts. See
+  // http://crbug.com/799437.
   old_signin_manager->SignOutAndRemoveAllAccounts(
       signin_metrics::TRANSFER_CREDENTIALS,
       signin_metrics::SignoutDelete::IGNORE_METRIC);
@@ -406,12 +410,20 @@ void OneClickSigninSyncStarter::CopyCredentialsToNewProfileAndFinishSignin(
 }
 
 void OneClickSigninSyncStarter::CancelSigninAndDelete() {
+  SigninManager* signin_manager = SigninManagerFactory::GetForProfile(profile_);
+  DCHECK(signin_manager->AuthInProgress());
   if (signin::IsDicePrepareMigrationEnabled()) {
-    SigninManagerFactory::GetForProfile(profile_)->SignOutAndKeepAllAccounts(
+    // TODO: Do not delete the token if it existed prior this login flow. See
+    // http://crbug.com/797342.
+    ProfileOAuth2TokenServiceFactory::GetForProfile(profile_)
+        ->RevokeCredentials(signin_manager->GetAccountIdForAuthInProgress());
+    signin_manager->SignOutAndKeepAllAccounts(
         signin_metrics::ABORT_SIGNIN,
         signin_metrics::SignoutDelete::IGNORE_METRIC);
   } else {
-    SigninManagerFactory::GetForProfile(profile_)->SignOutAndRemoveAllAccounts(
+    // SignoutAndRemoveAllAccounts does not actually remove the accounts if the
+    // signin is still pending. See http://crbug.com/799437.
+    signin_manager->SignOutAndRemoveAllAccounts(
         signin_metrics::ABORT_SIGNIN,
         signin_metrics::SignoutDelete::IGNORE_METRIC);
   }
