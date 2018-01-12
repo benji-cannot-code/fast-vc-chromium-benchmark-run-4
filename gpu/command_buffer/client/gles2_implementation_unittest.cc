a231a108d8cdfb5700425ab70a6e382be5d96f58
Antoine Labour <piman@chromium.org>
2018-01-12 21:01:52 +0000

    Fix TransferBuffer::GetFreeSize and introduce GetFragmentedFreeSize
    Users of TransferBuffer::GetFreeSize expect the return value to be
    usable for an allocation without waiting, so it should return the
    largest block we can allocate. For memory reporting purpose we want the
    total size (including fragmented regions), so provide
    GetFragmentedFreeSize for that.
    
    Bug: None
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I6b62d3266279acf7ae82ef72f794d27aa21734bb
    Reviewed-on: https://chromium-review.googlesource.com/862883
    Reviewed-by: Victor Miura <vmiura@chromium.org>
    Commit-Queue: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529061}

--- gpu/command_buffer/client/gles2_implementation_unittest.cc
+++ gpu/command_buffer/client/gles2_implementation_unittest.cc
@@ -152,6 +152,7 @@ class MockTransferBuffer : public TransferBufferInterface {
   void FreePendingToken(void* p, unsigned int /* token */) override;
   unsigned int GetSize() const override;
   unsigned int GetFreeSize() const override;
+  unsigned int GetFragmentedFreeSize() const override;
   void ShrinkLastBlock(unsigned int new_size) override;
 
   size_t MaxTransferBufferSize() {
@@ -334,6 +335,10 @@ unsigned int MockTransferBuffer::GetSize() const {
 }
 
 unsigned int MockTransferBuffer::GetFreeSize() const {
+  return 0;
+}
+
+unsigned int MockTransferBuffer::GetFragmentedFreeSize() const {
   return 0;
 }
 
--- gpu/command_buffer/client/transfer_buffer.cc
+++ gpu/command_buffer/client/transfer_buffer.cc
@@ -103,6 +103,10 @@ unsigned int TransferBuffer::GetSize() const {
 }
 
 unsigned int TransferBuffer::GetFreeSize() const {
+  return HaveBuffer() ? ring_buffer_->GetLargestFreeSizeNoWaiting() : 0;
+}
+
+unsigned int TransferBuffer::GetFragmentedFreeSize() const {
   return HaveBuffer() ? ring_buffer_->GetTotalFreeSizeNoWaiting() : 0;
 }
 
