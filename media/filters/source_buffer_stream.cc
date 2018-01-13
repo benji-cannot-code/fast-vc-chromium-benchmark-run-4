3cc7d8e949ed94aec8f259b98eabfbd4b25ff65e
Matt Wolenetz <wolenetz@chromium.org>
2018-01-13 01:29:17 +0000

    MSE: Improve sorted range checking in debug builds
    Now that known range adjacency issues have been fixed (previous fixes in
    bug 791095), this change increases the strength of IsRangeListSorted()
    to ensure that the ranges' presentation intervals (in DTS if buffering
    ByDTS, in PTS if buffering ByPTS) are sorted and disjoint.  This
    strengthened logic caught the known errors fixed by prereq CL
    https://chromium-review.googlesource.com/c/chromium/src/+/865849.
    
    Also, the check for sorted ranges is moved from ::RemoveInternal to
    ::Remove, because it is possible that interim |ranges_| state may have
    abutting, non-overlapping, ranges during the scope of ::Append
    processing.
    
    BUG=791095
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Iccd018f88ba773a8e8ec21b97f975c2f872024b9
    Reviewed-on: https://chromium-review.googlesource.com/865826
    Commit-Queue: Matthew Wolenetz <wolenetz@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529134}

--- media/filters/source_buffer_stream.cc
+++ media/filters/source_buffer_stream.cc
@@ -55,7 +55,7 @@ bool IsRangeListSorted(
   for (const auto& range_ptr : ranges) {
     if (prev != kNoDecodeTimestamp() && prev >= range_ptr->GetStartTimestamp())
       return false;
-    prev = range_ptr->GetEndTimestamp();
+    prev = range_ptr->GetBufferedEndTimestamp();
   }
   return true;
 }
@@ -67,7 +67,7 @@ bool IsRangeListSorted(
   for (const auto& range_ptr : ranges) {
     if (prev != kNoTimestamp && prev >= range_ptr->GetStartTimestamp())
       return false;
-    prev = range_ptr->GetEndTimestamp();
+    prev = range_ptr->GetBufferedEndTimestamp();
   }
   return true;
 }
@@ -539,6 +539,9 @@ void SourceBufferStream<RangeClass>::Remove(base::TimeDelta start,
       Seek(seek_buffer_timestamp_);
     }
   }
+
+  DCHECK(OnlySelectedRangeIsSeeked());
+  DCHECK(IsRangeListSorted(ranges_));
 }
 
 template <typename RangeClass>
@@ -705,7 +708,6 @@ void SourceBufferStream<RangeClass>::RemoveInternal(
   DVLOG(3) << __func__ << " " << GetStreamTypeName()
            << ": after remove ranges_=" << RangesToString<RangeClass>(ranges_);
 
-  DCHECK(IsRangeListSorted(ranges_));
   DCHECK(OnlySelectedRangeIsSeeked());
 }
 
