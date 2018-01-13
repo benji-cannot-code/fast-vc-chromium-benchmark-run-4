e54053aa54d49bd5ae1984817a02a3076271c30d
Lukasz Anforowicz <lukasza@chromium.org>
2018-01-13 03:42:44 +0000

    Moving DEBUG_ALIAS_FOR_CSTR into //base/debug/alias.h
    DEBUG_ALIAS_FOR_CSTR was introduced in r527458 as a helper macro
    for implementing DEBUG_ALIAS_FOR_ORIGIN.  This CL moves the
    DEBUG_ALIAS_FOR_CSTR macrodefinition into //base/debug/alias.h.
    
    Additionally, the CL:
    
    - Uses the DEBUG_ALIAS_FOR_CSTR macro to replace some existing uses of
      base::debug::Alias + strlcpu
    
    - Introduces DEBUG_ALIAS_FOR_GURL macro (and unittests) and uses it in 5
      places (where base::debug::Alias + strlcpy was used before)
    
    Bug: 797968
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I986ea90243f58768177a9ea68a7e2440eaa317c4
    Reviewed-on: https://chromium-review.googlesource.com/857815
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Brett Wilson <brettw@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529155}

--- url/gurl.h
+++ url/gurl.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
+#include "base/debug/alias.h"
 #include "base/strings/string16.h"
 #include "base/strings/string_piece.h"
 #include "url/third_party/mozilla/url_parse.h"
@@ -487,5 +488,11 @@ URL_EXPORT bool operator==(const GURL& x, const base::StringPiece& spec);
 URL_EXPORT bool operator==(const base::StringPiece& spec, const GURL& x);
 URL_EXPORT bool operator!=(const GURL& x, const base::StringPiece& spec);
 URL_EXPORT bool operator!=(const base::StringPiece& spec, const GURL& x);
+
+// DEBUG_ALIAS_FOR_GURL(var_name, url) copies |url| into a new stack-allocated
+// variable named |<var_name>|.  This helps ensure that the value of |url| gets
+// preserved in crash dumps.
+#define DEBUG_ALIAS_FOR_GURL(var_name, url) \
+  DEBUG_ALIAS_FOR_CSTR(var_name, url.possibly_invalid_spec().c_str(), 128)
 
 #endif  // URL_GURL_H_
