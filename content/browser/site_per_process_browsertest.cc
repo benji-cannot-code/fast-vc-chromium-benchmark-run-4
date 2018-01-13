97c9a0d0bef5268bf22c6f5b83b7effe6350ba40
Ria Jiang <riajiang@chromium.org>
2018-01-13 00:48:02 +0000

    Fix SitePerProcessMouseWheelBrowserTest.SubframeWheelEventsOnMainThread.
    SitePerProcessMouseWheelBrowserTest.SubframeWheelEventsOnMainThread is
    flaky (TIMEOUT) on win7_chromium_rel_ng after CL [1] that changed
    mouse-wheel targeting to be async in some cases.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/854239
    
    Bug: 801617
    Change-Id: I1c36b8649791f24ef92b04c3ee1f472a88835308
    Reviewed-on: https://chromium-review.googlesource.com/865558
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Ria Jiang <riajiang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529121}

--- content/browser/site_per_process_browsertest.cc
+++ content/browser/site_per_process_browsertest.cc
@@ -6561,7 +6561,7 @@ class SitePerProcessMouseWheelBrowserTest : public SitePerProcessBrowserTest {
     rwhv_root_ = rwhv_root;
   }
 
-  void RunTest(gfx::Point pos) {
+  void RunTest(gfx::Point pos, RenderWidgetHostViewBase* expected_target) {
     content::DOMMessageQueue msg_queue;
     std::string reply;
 
@@ -6569,7 +6569,10 @@ class SitePerProcessMouseWheelBrowserTest : public SitePerProcessBrowserTest {
         web_contents()->GetRenderWidgetHostView());
     set_rwhv_root(rwhv_root);
 
+    InputEventAckWaiter waiter(expected_target->GetRenderWidgetHost(),
+                               blink::WebInputEvent::kMouseWheel);
     SendMouseWheel(pos);
+    waiter.Wait();
 
     // Expect both wheel and scroll handlers to fire.
     EXPECT_TRUE(msg_queue.WaitForMessage(&reply));
@@ -6631,7 +6634,7 @@ IN_PROC_BROWSER_TEST_F(SitePerProcessMouseWheelBrowserTest,
   gfx::Rect bounds = child_rwhv->GetViewBounds();
   gfx::Point pos(bounds.x() + 10, bounds.y() + 10);
 
-  RunTest(pos);
+  RunTest(pos, child_rwhv);
 }
 
 // Verifies that test in SubframeWheelEventsOnMainThread also makes sense for
@@ -6648,7 +6651,7 @@ IN_PROC_BROWSER_TEST_F(SitePerProcessMouseWheelBrowserTest,
 
   gfx::Point pos(10, 10);
 
-  RunTest(pos);
+  RunTest(pos, rfhi->GetRenderWidgetHost()->GetView());
 }
 
 IN_PROC_BROWSER_TEST_F(SitePerProcessMouseWheelBrowserTest,
