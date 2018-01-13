7dda24106b797da78ff5e0bd02d90440d6706262
Dave Schuyler <dschuyler@chromium.org>
2018-01-13 01:22:44 +0000

    [MD settings] On Startup url entry messages
    This CL adds descriptive text (error messages) when an entered URL is
    invalid or too long. Too long is currently defined as 100KB, which was
    chosen empirically based on how long a URL can be before the UI lags
    (appears to freeze).
    
    Bug: 575606
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I51e473de88d3370e3eba8d5eb0cf8fe27759eb9e
    Reviewed-on: https://chromium-review.googlesource.com/854713
    Reviewed-by: Scott Chen <scottchen@chromium.org>
    Commit-Queue: Dave Schuyler <dschuyler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529132}

--- chrome/test/data/webui/settings/startup_urls_page_test.js
+++ chrome/test/data/webui/settings/startup_urls_page_test.js
@@ -131,6 +131,7 @@ cr.define('settings_startup_urls_page', function() {
       return browserProxy.whenCalled('validateStartupPage').then(function(url) {
         assertEquals(expectedUrl, url);
         assertTrue(actionButton.disabled);
+        assertTrue(!!inputElement.invalid);
 
         browserProxy.setUrlValidity(true);
         browserProxy.resetResolver('validateStartupPage');
@@ -139,6 +140,7 @@ cr.define('settings_startup_urls_page', function() {
         return browserProxy.whenCalled('validateStartupPage');
       }).then(function() {
         assertFalse(actionButton.disabled);
+        assertFalse(!!inputElement.invalid);
       });
     });
 
