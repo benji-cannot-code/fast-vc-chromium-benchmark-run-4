78f96ea6a3f024b6723db9431ba720e6c9318059
Paul Irish <paulirish@chromium.org>
2018-01-13 05:43:06 +0000

    DevTools: Roll Lighthouse to 2.8.0
    Also improve dark mode styles
    
    Bug: 772558
    Change-Id: Ifb61b0039f96d290c6151302a982c333b49549ea
    Reviewed-on: https://chromium-review.googlesource.com/865817
    Reviewed-by: Will Chen <chenwilliam@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Paul Irish <paulirish@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529165}

--- third_party/WebKit/Source/devtools/front_end/audits2/lighthouse/renderer/report-renderer.js
+++ third_party/WebKit/Source/devtools/front_end/audits2/lighthouse/renderer/report-renderer.js
@@ -184,6 +184,7 @@ if (typeof module !== 'undefined' && module.exports) {
  *       description: string,
  *       informative: boolean,
  *       manual: boolean,
+ *       notApplicable: boolean,
  *       debugString: string,
  *       displayValue: string,
  *       helpText: string,
