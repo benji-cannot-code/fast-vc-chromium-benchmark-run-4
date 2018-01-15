b2fe8c6a2fe8eb638e75c4627ff159467a59b7e6
Sam McNally <sammc@chromium.org>
2018-01-15 05:53:49 +0000

    Remove blink::ParseJSONHeader.
    blink::ParseJSONHeader is unused so remove it.
    
    ResourceResponse.h was relying on a transitive include of Noncopyable.h
    so add a direct include to avoid breaking compile.
    
    Change-Id: Ib42cf55017e83f7d1c5ffc5463a7f2841bbc9815
    Reviewed-on: https://chromium-review.googlesource.com/863149
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529206}

--- third_party/WebKit/Source/platform/loader/fetch/ResourceResponse.h
+++ third_party/WebKit/Source/platform/loader/fetch/ResourceResponse.h
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/network/HTTPHeaderMap.h"
 #include "platform/network/HTTPParsers.h"
 #include "platform/weborigin/KURL.h"
+#include "platform/wtf/Noncopyable.h"
 #include "platform/wtf/RefCounted.h"
 #include "platform/wtf/Time.h"
 #include "platform/wtf/Vector.h"
--- third_party/WebKit/Source/platform/network/HTTPParsers.cpp
+++ third_party/WebKit/Source/platform/network/HTTPParsers.cpp
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_content_disposition.h"
 #include "net/http/http_response_headers.h"
 #include "net/http/http_util.h"
-#include "platform/json/JSONParser.h"
 #include "platform/loader/fetch/ResourceResponse.h"
 #include "platform/network/HeaderFieldTokenizer.h"
 #include "platform/network/http_names.h"
@@ -809,18 +808,6 @@ bool ParseMultipartFormHeadersFromBody(const char* bytes,
   }
 
   return true;
-}
-
-// See https://tools.ietf.org/html/draft-ietf-httpbis-jfv-01, Section 4.
-std::unique_ptr<JSONArray> ParseJSONHeader(const String& header,
-                                           int max_parse_depth) {
-  StringBuilder sb;
-  sb.Append("[");
-  sb.Append(header);
-  sb.Append("]");
-  std::unique_ptr<JSONValue> header_value =
-      ParseJSON(sb.ToString(), max_parse_depth);
-  return JSONArray::From(std::move(header_value));
 }
 
 bool ParseContentRangeHeaderFor206(const String& content_range,
