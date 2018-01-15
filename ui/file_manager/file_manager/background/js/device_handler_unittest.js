b3594c2f63b0b2b815be00679465714b6ba646a5
Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
2018-01-15 05:37:34 +0000

    Revert "Stop Files app in the incognito context to react to device events."
    This reverts commit e9b2e312171933e64ed390c7b369ae37c98ab219.
    
    Reason for revert: It caused a regression in the Guest mode.
    
    Original change's description:
    > Stop Files app in the incognito context to react to device events.
    > 
    > While 1+ incognito browser windows are opened, it'll have separate Files
    > app extension background and foreground scripts running. The ones in
    > the incognito context should only be visible by the file picker window
    > invoked by the browser, but should not show fullscreen app windows or
    > notifications.
    > 
    > - Prevent duplicated "Removable device detected" notifications
    >   (when no DCIM folder in it)
    > - Prevent duplicated Files app windows popup
    >   (when plugging a disk with /DCIM directory)
    > - Files app fullscreen mode opened with incognito context (which caused
    >   some errors), when clicking the "show in folder" link in "Removable
    >   device detected" notification.
    > - Other duplicated notifications such as "formatting finished".
    > 
    > Bug: 800658
    > Test: browser_tests --gtest_filter=FileManagerJsTest.DeviceHandler*
    > Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    > Change-Id: I6dc1bc650e1b5cb669e17907c34b680535d0b532
    > Reviewed-on: https://chromium-review.googlesource.com/861589
    > Commit-Queue: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    > Reviewed-by: Daichi Hirono <hirono@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#528896}
    
    TBR=hirono@chromium.org,yamaguchi@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 800658
    Change-Id: Ifcf3e4996543b7d3a07b8892069d58e5779ff5df
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Reviewed-on: https://chromium-review.googlesource.com/866353
    Commit-Queue: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Reviewed-by: Daichi Hirono <hirono@chromium.org>
    Reviewed-by: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529203}

--- ui/file_manager/file_manager/background/js/device_handler_unittest.js
+++ ui/file_manager/file_manager/background/js/device_handler_unittest.js
@@ -60,11 +60,6 @@ function setUp() {
   handler = new DeviceHandler();
 }
 
-function setUpInIncognitoContext() {
-  chrome.extension.inIncognitoContext = true;
-  handler = new DeviceHandler();
-}
-
 function testGoodDevice(callback) {
   chrome.fileManagerPrivate.onMountCompleted.dispatch({
     eventType: 'mount',
@@ -659,36 +654,6 @@ function testNotificationClicked(callback) {
       callback);
 }
 
-function testMiscMessagesInIncognito() {
-  setUpInIncognitoContext();
-  chrome.fileManagerPrivate.onDeviceChanged.dispatch(
-      {type: 'format_start', devicePath: '/device/path'});
-  // No notification sent by this instance in incognito context.
-  assertEquals(0, Object.keys(chrome.notifications.items).length);
-  assertFalse(chrome.notifications.resolver.settled);
-}
-
-function testMountCompleteInIncognito() {
-  setUpInIncognitoContext();
-  chrome.fileManagerPrivate.onMountCompleted.dispatch({
-    eventType: 'mount',
-    status: 'success',
-    volumeMetadata: {
-      isParentDevice: false,
-      deviceType: 'usb',
-      devicePath: '/device/path',
-      deviceLabel: 'label'
-    },
-    shouldNotify: true
-  });
-
-  assertEquals(0, Object.keys(chrome.notifications.items).length);
-  // TODO(yamaguchi): I think this test is incomplete.
-  // This looks as if notification is not generated yet because the promise
-  // is not settled yet. Same for testGoodDeviceNotNavigated.
-  assertFalse(chrome.notifications.resolver.settled);
-}
-
 /**
  * @param {!VolumeManagerCommon.VolumeType} volumeType
  * @param {string} volumeId
@@ -714,7 +679,6 @@ function setupChromeApis() {
       },
       cloudImportDisabled: false
     },
-    extension: {inIncognitoContext: false},
     fileManagerPrivate: {
       onDeviceChanged: {
         addListener: function(listener) {
