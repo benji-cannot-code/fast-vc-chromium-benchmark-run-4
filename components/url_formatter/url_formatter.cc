a3384f825118e1faa6b2b9e6fffa5b54f42b7e95
Christopher Grant <cjgrant@chromium.org>
2018-01-15 17:11:51 +0000

    FormatUrl: Promote trivial subdomain stripping from experimental state
    VR would like to make use of this handy feature, and it's been deemed
    ready to progress out of experimental state.  Rename the bit, and enable
    it in VR.
    
    BUG=
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Idb0b299f4a7e68cce9bd2772da69026e7077c189
    Reviewed-on: https://chromium-review.googlesource.com/862297
    Commit-Queue: Christopher Grant <cjgrant@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529305}

--- components/url_formatter/url_formatter.cc
+++ components/url_formatter/url_formatter.cc
@@ -408,7 +408,7 @@ const FormatUrlType kFormatUrlOmitHTTP = 1 << 1;
 const FormatUrlType kFormatUrlOmitTrailingSlashOnBareHostname = 1 << 2;
 const FormatUrlType kFormatUrlOmitHTTPS = 1 << 3;
 const FormatUrlType kFormatUrlExperimentalElideAfterHost = 1 << 4;
-const FormatUrlType kFormatUrlExperimentalOmitTrivialSubdomains = 1 << 5;
+const FormatUrlType kFormatUrlOmitTrivialSubdomains = 1 << 5;
 
 const FormatUrlType kFormatUrlOmitDefaults =
     kFormatUrlOmitUsernamePassword | kFormatUrlOmitHTTP |
@@ -527,7 +527,7 @@ base::string16 FormatUrlWithAdjustments(
 
   // Host.
   bool trim_trivial_subdomains =
-      (format_types & kFormatUrlExperimentalOmitTrivialSubdomains) != 0;
+      (format_types & kFormatUrlOmitTrivialSubdomains) != 0;
   AppendFormattedComponent(spec, parsed.host,
                            HostComponentTransform(trim_trivial_subdomains),
                            &url_string, &new_parsed->host, adjustments);
