9efa7bb3e5326c56138f503846428ddbc94558bf
jdoerrie <jdoerrie@chromium.org>
2018-01-16 15:46:08 +0000

    [u2f] Require explicit Connect() in U2fBleDevice
    This change modifies U2fBleDevice to not immediately create a connection
    in the constructor. Now an explicit call to Connect() needs to happen
    before a GATT connection is attempted.
    
    Bug: 763303
    Change-Id: Iec9d8f5d8cc89137ee3ca10e19827e43d5535bbe
    Reviewed-on: https://chromium-review.googlesource.com/867034
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529430}

--- device/u2f/u2f_ble_device.h
+++ device/u2f/u2f_ble_device.h
@@ -33,6 +33,7 @@ class U2fBleDevice : public U2fDevice {
   explicit U2fBleDevice(std::unique_ptr<U2fBleConnection> connection);
   ~U2fBleDevice() override;
 
+  void Connect();
   void SendPing(std::vector<uint8_t> data, MessageCallback callback);
   static std::string GetId(base::StringPiece address);
 
@@ -43,7 +44,6 @@ class U2fBleDevice : public U2fDevice {
   U2fBleConnection::ConnectionStatusCallback
   GetConnectionStatusCallbackForTesting();
   U2fBleConnection::ReadCallback GetReadCallbackForTesting();
-  void TransitionForTesting();
 
  protected:
   // U2fDevice:
