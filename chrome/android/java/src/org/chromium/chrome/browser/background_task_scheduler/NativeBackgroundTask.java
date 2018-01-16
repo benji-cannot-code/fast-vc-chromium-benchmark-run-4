b2f9253e6d030e26fe64b9143d4d2bb8c8eb8449
David Trainor <dtrainor@chromium.org>
2018-01-16 18:13:22 +0000

    Add TaskLoadedNative step to BackgroundTaskScheduler
    Add a metric for when the NativeBackgroundTask actually starts up the
    native library.  Since NativeBackgroundTask does live in the same
    package as the UMA code, added an external UMA class that is publicly
    accessible that exposes public-facing UMA methods.
    
    BUG=800494
    
    Change-Id: I2df44d0d7fc746189eb4e2485e512d6d47c1879f
    Reviewed-on: https://chromium-review.googlesource.com/857331
    Commit-Queue: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529462}

--- chrome/android/java/src/org/chromium/chrome/browser/background_task_scheduler/NativeBackgroundTask.java
+++ chrome/android/java/src/org/chromium/chrome/browser/background_task_scheduler/NativeBackgroundTask.java
@@ -16,6 +16,7 @@ import org.chromium.chrome.browser.init.BrowserParts;
 import org.chromium.chrome.browser.init.ChromeBrowserInitializer;
 import org.chromium.chrome.browser.init.EmptyBrowserParts;
 import org.chromium.components.background_task_scheduler.BackgroundTask;
+import org.chromium.components.background_task_scheduler.BackgroundTaskSchedulerExternalUma;
 import org.chromium.components.background_task_scheduler.TaskParameters;
 import org.chromium.content.browser.BrowserStartupController;
 
@@ -45,10 +46,15 @@ public abstract class NativeBackgroundTask implements BackgroundTask {
     /** Indicates that the task has already been stopped. Should only be accessed on UI Thread. */
     private boolean mTaskStopped;
 
+    /** The id of the task from {@link TaskParameters} used for metrics logging. */
+    private int mTaskId;
+
     @Override
     public final boolean onStartTask(
             Context context, TaskParameters taskParameters, TaskFinishedCallback callback) {
         ThreadUtils.assertOnUiThread();
+        mTaskId = taskParameters.getTaskId();
+
         @StartBeforeNativeResult
         int beforeNativeResult = onStartTaskBeforeNativeLoaded(context, taskParameters, callback);
 
@@ -110,6 +116,8 @@ public abstract class NativeBackgroundTask implements BackgroundTask {
             public void run() {
                 // If task was stopped before we got here, don't start native initialization.
                 if (mTaskStopped) return;
+
+                BackgroundTaskSchedulerExternalUma.reportTaskStartedNative(mTaskId);
                 try {
                     ChromeBrowserInitializer.getInstance(context).handlePreNativeStartup(parts);
 
---
+++ components/background_task_scheduler/android/java/src/org/chromium/components/background_task_scheduler/BackgroundTaskSchedulerExternalUma.java
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.background_task_scheduler;
+
+/**
+ * Helper class to allow external code (typically Chrome-specific BackgroundTaskScheduler code) to
+ * report UMA.
+ */
+public final class BackgroundTaskSchedulerExternalUma {
+    private BackgroundTaskSchedulerExternalUma() {}
+
+    /**
+     * Reports metrics for when a NativeBackgroundTask loads the native library.
+     * @param taskId An id from {@link TaskIds}.
+     */
+    public static void reportTaskStartedNative(int taskId) {
+        BackgroundTaskSchedulerUma.getInstance().reportTaskStartedNative(taskId);
+    }
+}
