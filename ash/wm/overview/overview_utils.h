cdc0f25decd4aed2ee97bf3d882bc68e655f40cd
Sammie Quon <sammiequon@google.com>
2018-01-16 17:48:14 +0000

    overview: Add new ui for overview shield and selection widget.
    The overview shield now blurs the wallpaper and is colored relative to
    the wallpaper. The selection widget has some minor updates.
    
    This work is behind a flag.
    
    wallpaper: https://screenshot.googleplex.com/jooNZTUEn8m
    with flag: https://screenshot.googleplex.com/mqR4BtAZKrk
    without flag: https://screenshot.googleplex.com/0i1GPV238EC
    
    Test: manual
    Bug: 782320
    Change-Id: Ica3138e68415b756cf8716260adcc7e679238873
    Reviewed-on: https://chromium-review.googlesource.com/858382
    Reviewed-by: David Reveman <reveman@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529456}

---
+++ ash/wm/overview/overview_utils.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_WM_OVERVIEW_OVERVIEW_UTILS_H_
+#define ASH_WM_OVERVIEW_OVERVIEW_UTILS_H_
+
+namespace ash {
+
+// Returns true if overview mode should use the new ui.
+// TODO(sammiequon): Remove this function when the old overview mode ui becomes
+// obsolete. See https://crbug.com/782320.
+bool IsNewOverviewUi();
+
+}  // namespace ash
+
+#endif  // ASH_WM_OVERVIEW_OVERVIEW_UTILS_H_
