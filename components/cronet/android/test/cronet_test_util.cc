6d05ee8993d5ff33dfc857914e26da434addcf9e
Misha Efimov <mef@chromium.org>
2018-01-16 21:46:51 +0000

    [Cronet] Extract common CronetURLRequest from Android CronetURLRequestAdapter.
    Note:
    - Patchset 1 copies android/cronet_url_request_adapter.* into cronet_url_request.*
    - android/cronet_url_request_adapter.* use cronet_url_request.*
    
    Bug: 786559
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I6712cadc28bb83e8f558cd2145df78924064fef0
    Reviewed-on: https://chromium-review.googlesource.com/849134
    Commit-Queue: Misha Efimov <mef@chromium.org>
    Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529506}

--- components/cronet/android/test/cronet_test_util.cc
+++ components/cronet/android/test/cronet_test_util.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_task_runner_handle.h"
 #include "components/cronet/android/cronet_url_request_adapter.h"
 #include "components/cronet/android/cronet_url_request_context_adapter.h"
+#include "components/cronet/cronet_url_request.h"
 #include "components/cronet/cronet_url_request_context.h"
 #include "jni/CronetTestUtil_jni.h"
 #include "net/url_request/url_request.h"
@@ -66,7 +67,7 @@ void TestUtil::RunAfterContextInit(jlong jcontext_adapter,
 net::URLRequest* TestUtil::GetURLRequest(jlong jrequest_adapter) {
   CronetURLRequestAdapter* request_adapter =
       reinterpret_cast<CronetURLRequestAdapter*>(jrequest_adapter);
-  return request_adapter->url_request_.get();
+  return request_adapter->request_->network_tasks_.url_request_.get();
 }
 
 static void PrepareNetworkThreadOnNetworkThread(jlong jcontext_adapter) {
