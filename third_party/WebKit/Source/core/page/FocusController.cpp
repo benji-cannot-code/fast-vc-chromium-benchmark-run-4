ce7a7c6be94784a838f6a04b01416e76fd827527
JunHo Seo <junho0924.seo@lge.com>
2018-01-17 14:01:09 +0000

    Snav: Keep the search start point constant throughout the search
    Background:
    If snav fails to find a focusable element in the first search
    scope (activeElement's enclosing container C), snav expands
    the scope to C's parent container.
    
    starting_rect =
       visual rectangle from where snav looks for new focusables.
    
    Focusables close to the starting_rect gets priority.
    
    Problem:
    As snav expands the scope, starting_rect changes from being
    the enclosing container's visual rectangle to being the
    visual rectangle of the parent container =>
    This makes focus go to an undesirable element because snav's
    use of a faulty "base" when evaluating distances to focusables.
    
    Solution:
    Keep the initial starting_rect throughout the search, even
    if the search scope changes.
    
    While here, add an early break that avoids an unnecessary layout.
    
    TBR=szager@chromium.org, this was already discussed in:
    https://chromium-review.googlesource.com/c/chromium/src/+/781326
    
    Bug: 770147, 802998
    Change-Id: I00690a93d6a329ca30aa14432b9a7d25f3a3d65f
    Reviewed-on: https://chromium-review.googlesource.com/870029
    Commit-Queue: Hugo Holgersson <hugoh@vewd.com>
    Reviewed-by: Hugo Holgersson <hugoh@vewd.com>
    Cr-Commit-Position: refs/heads/master@{#529730}

--- third_party/WebKit/Source/core/page/FocusController.cpp
+++ third_party/WebKit/Source/core/page/FocusController.cpp
@@ -1500,8 +1500,9 @@ bool FocusController::AdvanceFocusDirectionally(WebFocusType type) {
   while (!consumed && container) {
     consumed =
         AdvanceFocusDirectionallyInContainer(container, starting_rect, type);
-    starting_rect =
-        NodeRectInAbsoluteCoordinates(container, true /* ignore border */);
+    if (consumed)
+      break;
+
     container =
         ScrollableEnclosingBoxOrParentFrameForNodeInDirection(type, container);
     if (container && container->IsDocumentNode())
