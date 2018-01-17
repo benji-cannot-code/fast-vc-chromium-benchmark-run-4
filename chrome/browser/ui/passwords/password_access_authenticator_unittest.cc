aa3e3dbe38558b2f6e7e381dfa15d527152413f2
Christos Froussios <cfroussios@chromium.org>
2018-01-17 10:07:34 +0000

    [Password Manager] Exporting passwords does not reuse previous reauth
    Viewing passwords on the settins page requires reauth, but only once per
    minute. Exporting the entire password list now ignores this grace period.
    
    Bug: 800686,789561
    Change-Id: I5f46afc3f0ec123bac371a6fa2329f55c6c20632
    Reviewed-on: https://chromium-review.googlesource.com/864149
    Reviewed-by: Vaclav Brozek <vabr@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Christos Froussios <cfroussios@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529690}

--- chrome/browser/ui/passwords/password_access_authenticator_unittest.cc
+++ chrome/browser/ui/passwords/password_access_authenticator_unittest.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/passwords/password_access_authenticator.h"
 
+#include <utility>
+
 #include "base/bind.h"
 #include "base/test/simple_test_clock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -47,6 +49,29 @@ TEST(PasswordAccessAuthenticatorTest, Expiration) {
   reauth_called = false;
 
   EXPECT_TRUE(authenticator.EnsureUserIsAuthenticated());
+  EXPECT_TRUE(reauth_called);
+}
+
+// Check that a forced authentication ignores previous successful challenges.
+TEST(PasswordAccessAuthenticatorTest, ForceReauth) {
+  bool reauth_called = false;
+
+  auto clock_owned = std::make_unique<base::SimpleTestClock>();
+  base::SimpleTestClock* clock = clock_owned.get();
+  clock->Advance(base::TimeDelta::FromSeconds(123));
+
+  PasswordAccessAuthenticator authenticator(base::BindRepeating(
+      &FakeOsReauthCall, &reauth_called, ReauthResult::PASS));
+  authenticator.SetClockForTesting(std::move(clock_owned));
+
+  EXPECT_TRUE(authenticator.EnsureUserIsAuthenticated());
+  EXPECT_TRUE(reauth_called);
+
+  clock->Advance(base::TimeDelta::FromSeconds(
+      PasswordAccessAuthenticator::kAuthValidityPeriodSeconds - 1));
+  reauth_called = false;
+
+  EXPECT_TRUE(authenticator.ForceUserReauthentication());
   EXPECT_TRUE(reauth_called);
 }
 
