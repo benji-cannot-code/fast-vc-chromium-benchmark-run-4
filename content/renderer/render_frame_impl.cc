9482465b92cb7eb2edbe79c1b9613e3aaff2f5cd
arthursonzogni <arthursonzogni@chromium.org>
2018-01-17 14:14:26 +0000

    Upgrade Insecure Requests: OOPIF support, bugfixes & tests.
    This CL fixes several bugs with Upgrade Insecure Requests. The main one
    is that URL were compared against the 'upgrade insecure navigation set'
    of the frame that is navigating instead of the frame that has initiated
    the navigation.
    
    It fixes bug 796538.
    8 new tests are added to ensure regressions won't happen anymore.
    
    Finally, it adds the support for OOPIF. The 'upgrade insecure
    navigations set' is now replicated across the different processes.
    
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    
    BUG=670219, 796538
    
    Change-Id: I1d138989a1873cd902435de25845ae660769ff98
    Reviewed-on: https://chromium-review.googlesource.com/848836
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529732}

--- content/renderer/render_frame_impl.cc
+++ content/renderer/render_frame_impl.cc
@@ -3850,6 +3850,11 @@ void RenderFrameImpl::DidEnforceInsecureRequestPolicy(
   GetFrameHost()->EnforceInsecureRequestPolicy(policy);
 }
 
+void RenderFrameImpl::DidEnforceInsecureNavigationsSet(
+    const std::vector<uint32_t>& set) {
+  GetFrameHost()->EnforceInsecureNavigationsSet(set);
+}
+
 void RenderFrameImpl::DidChangeFramePolicy(
     blink::WebFrame* child_frame,
     blink::WebSandboxFlags flags,
@@ -5397,6 +5402,7 @@ RenderFrameImpl::MakeDidCommitProvisionalLoadParams(
   params->origin = frame_origin;
 
   params->insecure_request_policy = frame_->GetInsecureRequestPolicy();
+  params->insecure_navigations_set = frame_->GetInsecureRequestToUpgrade();
 
   params->has_potentially_trustworthy_unique_origin =
       frame_origin.IsUnique() && frame_origin.IsPotentiallyTrustworthy();
