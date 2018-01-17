097da70c4f7e568332251489a632d2a9510c3c36
Daniel Bratell <bratell@opera.com>
2018-01-17 11:04:54 +0000

    Replacing custom http status constants with net::HTTP_*
    A couple of places defined their own kHttpResponseOk which
    is a bit unnecessary when there already is a net::HTTP_OK which
    the rest of the code uses.
    
    This was noticed in jumbo build experiments because two
    kHttpResponseOk ended up in the same translation unit and caused
    compilation errors.
    
    Bug: 746957
    Change-Id: Ia5cf32636bbcf8e0e1d7b8e1c161c5b4a5cdcbdf
    Reviewed-on: https://chromium-review.googlesource.com/868156
    Reviewed-by: dsinclair <dsinclair@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Daniel Bratell <bratell@opera.com>
    Cr-Commit-Position: refs/heads/master@{#529705}

--- chrome/browser/media/webrtc/webrtc_log_uploader.cc
+++ chrome/browser/media/webrtc/webrtc_log_uploader.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_thread.h"
 #include "net/base/load_flags.h"
 #include "net/base/mime_util.h"
+#include "net/http/http_status_code.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
 #include "net/url_request/url_fetcher.h"
 #include "third_party/zlib/zlib.h"
@@ -41,8 +42,6 @@ const int kLogListLimitLines = 50;
 const char kUploadContentType[] = "multipart/form-data";
 const char kMultipartBoundary[] =
     "----**--yradnuoBgoLtrapitluMklaTelgooG--**----";
-
-const int kHttpResponseOk = 200;
 
 // Adds the header section for a gzip file to the multipart |post_data|.
 void AddMultipartFileContentHeader(std::string* post_data,
@@ -165,7 +164,7 @@ void WebRtcLogUploader::PrepareMultipartPostData(
   // implemented according to the test plan. http://crbug.com/257329.
   if (post_data_) {
     *post_data_ = *post_data;
-    NotifyUploadDone(kHttpResponseOk, "", upload_done_data);
+    NotifyUploadDone(net::HTTP_OK, "", upload_done_data);
     return;
   }
 
@@ -303,7 +302,7 @@ void WebRtcLogUploader::OnURLFetchComplete(
     // The log path can be empty here if we failed getting it before. We still
     // upload the log if that's the case.
     std::string report_id;
-    if (response_code == kHttpResponseOk &&
+    if (response_code == net::HTTP_OK &&
         source->GetResponseAsString(&report_id) &&
         !it->second.log_path.empty()) {
       // TODO(jiayl): Add the RTP dump records to chrome://webrtc-logs.
@@ -621,7 +620,7 @@ void WebRtcLogUploader::NotifyUploadDone(
       base::BindOnce(&WebRtcLoggingHandlerHost::UploadLogDone,
                      upload_done_data.host));
   if (!upload_done_data.callback.is_null()) {
-    bool success = response_code == kHttpResponseOk;
+    bool success = response_code == net::HTTP_OK;
     std::string error_message;
     if (!success) {
       error_message = "Uploading failed, response code: " +
