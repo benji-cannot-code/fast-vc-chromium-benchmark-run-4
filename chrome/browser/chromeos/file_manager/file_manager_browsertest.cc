7b312d3655d1c318d5a2c41405e06fef93a60e0e
Stephen McGruer <smcgruer@chromium.org>
2018-01-17 20:35:04 +0000

    Disable CreateNewFolder/FileManagerBrowserTest due to flake.
    https://test-results.appspot.com/dashboards/flakiness_dashboard.html#showExpectations=true&tests=CreateNewFolder%2FFileManagerBrowserTest.Test%2F3
    
    Bug: 715963
    Change-Id: I3cfdbb17f0a6478a1cb36e068905cbc9313b326b
    
    TBR=yoshiki@chromium.org,tetsui@chromium.org
    
    Change-Id: I3cfdbb17f0a6478a1cb36e068905cbc9313b326b
    Reviewed-on: https://chromium-review.googlesource.com/870752
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529862}

--- chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
@@ -133,7 +133,7 @@ WRAPPED_INSTANTIATE_TEST_CASE_P(
 
 // Flaky: crbug.com/715963
 WRAPPED_INSTANTIATE_TEST_CASE_P(
-    CreateNewFolder,
+    DISABLED_CreateNewFolder,
     FileManagerBrowserTest,
     ::testing::Values(
         TestParameter(NOT_IN_GUEST_MODE, "createNewFolderAfterSelectFile"),
