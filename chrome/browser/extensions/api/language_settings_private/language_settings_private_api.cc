418f59494d2f3eaa4b29ed696abae0ff66f4b799
Lei Zhang <thestig@chromium.org>
2018-01-17 04:13:58 +0000

    Modernize / simplify some Spellcheck code.
    Change-Id: Ib79c8dfa08ba505a2b4aeacd9aca41ac9c766bd1
    Reviewed-on: https://chromium-review.googlesource.com/838245
    Reviewed-by: Rachel Blum <groby@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529598}

--- chrome/browser/extensions/api/language_settings_private/language_settings_private_api.cc
+++ chrome/browser/extensions/api/language_settings_private/language_settings_private_api.cc
@@ -157,8 +157,8 @@ std::vector<std::string> GetSortedExtensionIMEs(
   return extension_ime_list;
 }
 
-}  // anonymous namespace
-#endif
+}  // namespace
+#endif  // defined(OS_CHROMEOS)
 
 LanguageSettingsPrivateGetLanguageListFunction::
     LanguageSettingsPrivateGetLanguageListFunction()
@@ -185,8 +185,8 @@ LanguageSettingsPrivateGetLanguageListFunction::Run() {
                                                    locales.end());
 
   // Get the list of spell check languages and convert to a set.
-  std::vector<std::string> spellcheck_languages;
-  spellcheck::SpellCheckLanguages(&spellcheck_languages);
+  std::vector<std::string> spellcheck_languages =
+      spellcheck::SpellCheckLanguages();
   const std::unordered_set<std::string> spellcheck_language_set(
       spellcheck_languages.begin(), spellcheck_languages.end());
 
