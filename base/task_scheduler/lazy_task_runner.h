4cb7695fe1b3430e987b6eaa88ceec506ab86949
Gabriel Charette <gab@chromium.org>
2018-01-17 20:59:07 +0000

    Make Singleton and LazyInstance use the same logic for lazy creation.
    They had more or less duplicated code for a while.
    
    Creation logic for LazyInstance was extracted to base::internal methods
    a little while back for LazyTaskRunner. It can now also be used by
    Singleton to avoid code duplication.
    
    Note: because of code duplication, until this CL: Singleton still
    didn't benefit from the recent priority inversion mitigation added
    to LazyInstance in r527445.
    
    Bug: 797129
    Change-Id: I4cfe0d94bce282901ec2471212e38b8e298d9f81
    Reviewed-on: https://chromium-review.googlesource.com/868013
    Reviewed-by: François Doray <fdoray@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529885}

--- base/task_scheduler/lazy_task_runner.h
+++ base/task_scheduler/lazy_task_runner.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/atomicops.h"
 #include "base/callback.h"
 #include "base/compiler_specific.h"
-#include "base/lazy_instance.h"
+#include "base/lazy_instance_helpers.h"
 #include "base/sequenced_task_runner.h"
 #include "base/single_thread_task_runner.h"
 #include "base/task_scheduler/scheduler_lock.h"
