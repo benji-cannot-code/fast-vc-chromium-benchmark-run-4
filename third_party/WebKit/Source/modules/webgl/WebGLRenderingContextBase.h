041b2545176fd2253c9dd25694b8c858cddc675b
Brandon Jones <bajones@chromium.org>
2018-01-17 06:54:53 +0000

    Remove WebGL's DrawingBuffer immediately upon context loss
    Fixes an issue that occured when the DrawingBuffer was deleted during a
    call to LoseContextImpl that was initiated by the CommandBufferProxy it
    owns. This allows the DrawingBuffer to be removed from the
    WebGLRenderingContextBase immediately upon loss in all cases.
    
    Bug: 801720
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: If256babc75f2622312cac937dc80c6191c52138a
    Reviewed-on: https://chromium-review.googlesource.com/868936
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Brandon Jones <bajones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529656}

--- third_party/WebKit/Source/modules/webgl/WebGLRenderingContextBase.h
+++ third_party/WebKit/Source/modules/webgl/WebGLRenderingContextBase.h
@@ -711,6 +711,7 @@ class MODULES_EXPORT WebGLRenderingContextBase : public CanvasRenderingContext,
   TaskRunnerTimer<WebGLRenderingContextBase> dispatch_context_lost_event_timer_;
   bool restore_allowed_;
   TaskRunnerTimer<WebGLRenderingContextBase> restore_timer_;
+  scoped_refptr<WebTaskRunner> task_runner_;
 
   bool marked_canvas_dirty_;
   bool animation_frame_in_progress_;
@@ -1697,6 +1698,7 @@ class MODULES_EXPORT WebGLRenderingContextBase : public CanvasRenderingContext,
   bool IsPaintable() const final { return GetDrawingBuffer(); }
   bool CopyRenderingResultsFromDrawingBuffer(CanvasResourceProvider*,
                                              SourceDrawingBuffer) const;
+  void HoldReferenceToDrawingBuffer(DrawingBuffer*);
 };
 
 // TODO(fserb): remove this.
