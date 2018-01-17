15bf59c9c120a3e9a80f1eda69aec886c9044049
Bret Sepulveda <bsep@chromium.org>
2018-01-17 02:28:40 +0000

    Add >1 page case to Hung Renderer UI tests and fix button label.
    The Hung Renderer UI tests weren't compiling and were missing a test
    case for multiple hung pages, this patch fixes both. Also, the "Exit
    Page" button was updated to read "Exit Pages" when appropriate.
    
    Bug: 652507
    Change-Id: I794bc5bc7cd0bd2a1cddb746bd679ca02e2c1694
    Reviewed-on: https://chromium-review.googlesource.com/854689
    Commit-Queue: Bret Sepulveda <bsep@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529549}

--- chrome/browser/ui/views/hung_renderer_view.cc
+++ chrome/browser/ui/views/hung_renderer_view.cc
@@ -43,7 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/label.h"
 #include "ui/views/layout/grid_layout.h"
 #include "ui/views/widget/widget.h"
-#include "ui/views/window/client_view.h"
 
 #if defined(OS_WIN)
 #include "chrome/browser/hang_monitor/hang_crash_dump_win.h"
@@ -298,13 +297,7 @@ void HungRendererDialogView::ShowForWebContents(WebContents* contents) {
     // one is showing.
     hung_pages_table_model_->InitForWebContents(contents);
 
-    info_label_->SetText(
-        l10n_util::GetPluralStringFUTF16(IDS_BROWSER_HANGMONITOR_RENDERER,
-                                         hung_pages_table_model_->RowCount()));
-    Layout();
-
-    // Make Widget ask for the window title again.
-    GetWidget()->UpdateWindowTitle();
+    UpdateLabels();
 
     GetWidget()->Show();
   }
@@ -349,9 +342,12 @@ int HungRendererDialogView::GetDialogButtons() const {
 
 base::string16 HungRendererDialogView::GetDialogButtonLabel(
     ui::DialogButton button) const {
-  return l10n_util::GetStringUTF16(button == ui::DIALOG_BUTTON_OK
-                                       ? IDS_BROWSER_HANGMONITOR_RENDERER_WAIT
-                                       : IDS_BROWSER_HANGMONITOR_RENDERER_END);
+  if (button == ui::DIALOG_BUTTON_CANCEL) {
+    return l10n_util::GetPluralStringFUTF16(
+        IDS_BROWSER_HANGMONITOR_RENDERER_END,
+        hung_pages_table_model_->RowCount());
+  }
+  return l10n_util::GetStringUTF16(IDS_BROWSER_HANGMONITOR_RENDERER_WAIT);
 }
 
 bool HungRendererDialogView::Cancel() {
@@ -460,4 +456,12 @@ void HungRendererDialogView::RestartHangTimer() {
   auto* render_view_host = hung_pages_table_model_->GetRenderViewHost();
   if (render_view_host)
     render_view_host->GetWidget()->RestartHangMonitorTimeoutIfNecessary();
+}
+
+void HungRendererDialogView::UpdateLabels() {
+  GetWidget()->UpdateWindowTitle();
+  info_label_->SetText(l10n_util::GetPluralStringFUTF16(
+      IDS_BROWSER_HANGMONITOR_RENDERER, hung_pages_table_model_->RowCount()));
+  // Update the "Exit" button.
+  DialogModelChanged();
 }
