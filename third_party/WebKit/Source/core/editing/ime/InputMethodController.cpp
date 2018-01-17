0b1430a84bd638b34bcac2746da6f120f91d36cc
Ryan Landay <rlanday@chromium.org>
2018-01-17 08:06:57 +0000

    Fire composition{start,update} events for SetCompositionFromExistingText()
    Currently, an IME calling
    InputMethodController::SetCompositionFromExistingText() (e.g. an Android IME
    calling InputConnection#setComposingRegion()) does not fire the compositionstart
    and compositionupdate events. This CL fixes this method to fire these two
    events.
    
    Bug: 764439
    Change-Id: Ib15f0355b32d75379021d1a8d32e4cc7cdfced8a
    Reviewed-on: https://chromium-review.googlesource.com/865825
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Ryan Landay <rlanday@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529672}

--- third_party/WebKit/Source/core/editing/ime/InputMethodController.cpp
+++ third_party/WebKit/Source/core/editing/ime/InputMethodController.cpp
@@ -679,6 +679,18 @@ void InputMethodController::CancelComposition() {
   DispatchCompositionEndEvent(GetFrame(), g_empty_string);
 }
 
+bool InputMethodController::DispatchCompositionStartEvent(const String& text) {
+  Element* target = GetDocument().FocusedElement();
+  if (!target)
+    return IsAvailable();
+
+  CompositionEvent* event = CompositionEvent::Create(
+      EventTypeNames::compositionstart, GetFrame().DomWindow(), text);
+  target->DispatchEvent(event);
+
+  return IsAvailable();
+}
+
 void InputMethodController::SetComposition(
     const String& text,
     const Vector<ImeTextSpan>& ime_text_spans,
@@ -754,12 +766,9 @@ void InputMethodController::SetComposition(
   // We should send a 'compositionstart' event only when the given text is not
   // empty because this function doesn't create a composition node when the text
   // is empty.
-  if (!HasComposition()) {
-    target->DispatchEvent(CompositionEvent::Create(
-        EventTypeNames::compositionstart, GetFrame().DomWindow(),
-        GetFrame().SelectedText()));
-    if (!IsAvailable())
-      return;
+  if (!HasComposition() &&
+      !DispatchCompositionStartEvent(GetFrame().SelectedText())) {
+    return;
   }
 
   DCHECK(!text.IsEmpty());
@@ -875,6 +884,13 @@ void InputMethodController::SetCompositionFromExistingText(
     const Vector<ImeTextSpan>& ime_text_spans,
     unsigned composition_start,
     unsigned composition_end) {
+  Element* target = GetDocument().FocusedElement();
+  if (!target)
+    return;
+
+  if (!HasComposition() && !DispatchCompositionStartEvent(""))
+    return;
+
   Element* editable = GetFrame()
                           .Selection()
                           .ComputeVisibleSelectionInDOMTreeDeprecated()
@@ -906,6 +922,8 @@ void InputMethodController::SetCompositionFromExistingText(
     composition_range_ = Range::Create(GetDocument());
   composition_range_->setStart(range.StartPosition());
   composition_range_->setEnd(range.EndPosition());
+
+  DispatchCompositionUpdateEvent(GetFrame(), ComposingText());
 }
 
 EphemeralRange InputMethodController::CompositionEphemeralRange() const {
