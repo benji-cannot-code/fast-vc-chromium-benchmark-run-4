f5ed4febc709eaa587cc3863f242ecc107ed507d
Dmitry Gozman <dgozman@chromium.org>
2018-01-17 04:15:04 +0000

    [DevTools] Emulate touch for non-main targets
    Bug: 802360
    Change-Id: I002f6849e7cf787b6fed42cc696dc8652181dc79
    Reviewed-on: https://chromium-review.googlesource.com/868646
    Reviewed-by: Andrey Lushnikov <lushnikov@chromium.org>
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529599}

--- third_party/WebKit/Source/devtools/front_end/emulation/DeviceModeModel.js
+++ third_party/WebKit/Source/devtools/front_end/emulation/DeviceModeModel.js
@@ -58,6 +58,8 @@ Emulation.DeviceModeModel = class extends Common.Object {
     this._mode = null;
     /** @type {number} */
     this._fitScale = 1;
+    this._touchEnabled = false;
+    this._touchMobile = false;
 
     /** @type {?SDK.EmulationModel} */
     this._emulationModel = null;
@@ -351,6 +353,8 @@ Emulation.DeviceModeModel = class extends Common.Object {
         this._onModelAvailable = null;
         callback();
       }
+    } else {
+      emulationModel.emulateTouch(this._touchEnabled, this._touchMobile);
     }
   }
 
@@ -685,6 +689,8 @@ Emulation.DeviceModeModel = class extends Common.Object {
    * @param {boolean} mobile
    */
   _applyTouch(touchEnabled, mobile) {
+    this._touchEnabled = touchEnabled;
+    this._touchMobile = mobile;
     for (var emulationModel of SDK.targetManager.models(SDK.EmulationModel))
       emulationModel.emulateTouch(touchEnabled, mobile);
   }
