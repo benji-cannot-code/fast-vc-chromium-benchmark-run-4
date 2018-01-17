323aebe7330266bf3ae7ffc5c6dfdd4733533311
Ryan Landay <rlanday@chromium.org>
2018-01-17 06:58:06 +0000

    Add layout test for DeleteSurroundingText() JavaScript events
    I was working on adding a missing beforeinput event in
    https://chromium-review.googlesource.com/c/chromium/src/+/862521
    and observed that InputMethodController::DeleteSurroundingText() is also not
    properly firing a beforeinput event. This CL adds a layout test to record the
    current behavior. Currently, we do not fire beforeinput at all, but fire input
    twice (once for the text before the selection, and once for the text after the
    selection; note that if we have a range selection, these pieces of text may not
    be contiguous).
    
    Bug: 802381
    Change-Id: Ia2194ecc98c1a6767fc8a088fff74efe819153f7
    Reviewed-on: https://chromium-review.googlesource.com/868952
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Commit-Queue: Ryan Landay <rlanday@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529657}

--- content/shell/test_runner/text_input_controller.cc
+++ content/shell/test_runner/text_input_controller.cc
@@ -49,6 +49,7 @@ class TextInputControllerBindings
   void UnmarkText();
   void UnmarkAndUnselectText();
   void DoCommand(const std::string& text);
+  void DeleteSurroundingText(int before, int after);
   void SetMarkedText(const std::string& text, int start, int length);
   void SetMarkedTextFromExistingText(int start, int length);
   bool HasMarkedText();
@@ -102,6 +103,8 @@ TextInputControllerBindings::GetObjectTemplateBuilder(v8::Isolate* isolate) {
       .SetMethod("unmarkAndUnselectText",
                  &TextInputControllerBindings::UnmarkAndUnselectText)
       .SetMethod("doCommand", &TextInputControllerBindings::DoCommand)
+      .SetMethod("deleteSurroundingText",
+                 &TextInputControllerBindings::DeleteSurroundingText)
       .SetMethod("setMarkedText", &TextInputControllerBindings::SetMarkedText)
       .SetMethod("setMarkedTextFromExistingText",
                  &TextInputControllerBindings::SetMarkedTextFromExistingText)
@@ -133,6 +136,11 @@ void TextInputControllerBindings::UnmarkAndUnselectText() {
 void TextInputControllerBindings::DoCommand(const std::string& text) {
   if (controller_)
     controller_->DoCommand(text);
+}
+
+void TextInputControllerBindings::DeleteSurroundingText(int before, int after) {
+  if (controller_)
+    controller_->DeleteSurroundingText(before, after);
 }
 
 void TextInputControllerBindings::SetMarkedText(const std::string& text,
@@ -217,6 +225,16 @@ void TextInputController::DoCommand(const std::string& text) {
                                                      "is not a local frame.";
     view()->MainFrame()->ToWebLocalFrame()->ExecuteCommand(
         blink::WebString::FromUTF8(text));
+  }
+}
+
+void TextInputController::DeleteSurroundingText(int before, int after) {
+  if (view()->MainFrame()) {
+    CHECK(view()->MainFrame()->ToWebLocalFrame()) << "This function cannot be "
+                                                     "called if the main frame "
+                                                     "is not a local frame.";
+    view()->MainFrame()->ToWebLocalFrame()->DeleteSurroundingText(before,
+                                                                  after);
   }
 }
 
