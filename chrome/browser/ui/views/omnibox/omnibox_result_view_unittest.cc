4263f30684fb95fb26e769d876b82d92f1e0ed90
Stephen McGruer <smcgruer@chromium.org>
2018-01-17 17:47:21 +0000

    Revert "Omnibox: Re-add stripped scheme for omnibox dropdown entries"
    This reverts commit a58335a1000da7ddfc9861ae72710a99619dc803.
    
    Reason for revert: OmniboxEditModelTest.AlternateNavHasHTTP fails on Linux ChromiumOS MSan Tests:
    
    https://ci.chromium.org/buildbot/chromium.memory/Linux%20ChromiumOS%20MSan%20Tests/5286
    
    Original change's description:
    > Omnibox: Re-add stripped scheme for omnibox dropdown entries
    > 
    > When a user uses the arrow key to get to a navsuggest in the dropdown,
    > and then selects all and copies the URL, the stripped "http://" is
    > currently not re-added.
    > 
    > This re-adds the stripped "http://".
    > 
    > Bug: 492711
    > Change-Id: I17aae1abf888645e693d2df9d986694941f039a9
    > Reviewed-on: https://chromium-review.googlesource.com/843208
    > Commit-Queue: Tommy Li <tommycli@chromium.org>
    > Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#529552}
    
    TBR=mpearson@chromium.org,tommycli@chromium.org,jdonnelly@chromium.org
    
    Change-Id: I5e41b8c2e8cfa48fc33e6e882d6b1f04be6ebd4b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 492711
    Reviewed-on: https://chromium-review.googlesource.com/871070
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529794}

--- chrome/browser/ui/views/omnibox/omnibox_result_view_unittest.cc
+++ chrome/browser/ui/views/omnibox/omnibox_result_view_unittest.cc
@@ -6,9 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/omnibox/omnibox_result_view.h"
 
 #include "base/memory/ptr_util.h"
+#include "chrome/browser/ui/omnibox/test_omnibox_client.h"
 #include "chrome/browser/ui/views/omnibox/omnibox_popup_contents_view.h"
 #include "components/omnibox/browser/omnibox_edit_model.h"
-#include "components/omnibox/browser/test_omnibox_client.h"
 #include "content/public/test/test_browser_thread_bundle.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/accessibility/ax_node_data.h"
