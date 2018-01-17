29cd90356463f8d6e8fcaad75ffddfd398dddcc5
Marc Treib <treib@chromium.org>
2018-01-17 12:32:56 +0000

    search_provider_logos: Clear cached logo on sign-out
    Doodle fetches may include cookies, and so the response may contain
    personalized data. To avoid leaking such data, clear the cache when
    the sign-in cookies are updated.
    
    Bug: 799899
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Ica97a6aee7dd4220a243b461281066944ee8bda7
    Reviewed-on: https://chromium-review.googlesource.com/866749
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Chris Pickel <sfiera@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529720}

--- chrome/browser/search_provider_logos/logo_service_factory.cc
+++ chrome/browser/search_provider_logos/logo_service_factory.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/search/suggestions/image_decoder_impl.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
+#include "chrome/browser/signin/gaia_cookie_manager_service_factory.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "components/search_provider_logos/logo_service.h"
 #include "components/search_provider_logos/logo_service_impl.h"
@@ -53,6 +54,7 @@ LogoServiceFactory::LogoServiceFactory()
     : BrowserContextKeyedServiceFactory(
           "LogoService",
           BrowserContextDependencyManager::GetInstance()) {
+  DependsOn(GaiaCookieManagerServiceFactory::GetInstance());
   DependsOn(TemplateURLServiceFactory::GetInstance());
 }
 
@@ -62,9 +64,10 @@ KeyedService* LogoServiceFactory::BuildServiceInstanceFor(
     content::BrowserContext* context) const {
   Profile* profile = static_cast<Profile*>(context);
   DCHECK(!profile->IsOffTheRecord());
-  return new LogoServiceImpl(profile->GetPath().Append(kCachedLogoDirectory),
-                             TemplateURLServiceFactory::GetForProfile(profile),
-                             base::MakeUnique<suggestions::ImageDecoderImpl>(),
-                             profile->GetRequestContext(),
-                             base::BindRepeating(&UseGrayLogo));
+  return new LogoServiceImpl(
+      profile->GetPath().Append(kCachedLogoDirectory),
+      GaiaCookieManagerServiceFactory::GetForProfile(profile),
+      TemplateURLServiceFactory::GetForProfile(profile),
+      base::MakeUnique<suggestions::ImageDecoderImpl>(),
+      profile->GetRequestContext(), base::BindRepeating(&UseGrayLogo));
 }
--- components/search_provider_logos/logo_service_impl.h
+++ components/search_provider_logos/logo_service_impl.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/search_provider_logos/logo_common.h"
 #include "components/search_provider_logos/logo_service.h"
 
+class GaiaCookieManagerService;
 class TemplateURLService;
 
 namespace base {
@@ -37,6 +38,7 @@ class LogoServiceImpl : public LogoService {
  public:
   LogoServiceImpl(
       const base::FilePath& cache_directory,
+      GaiaCookieManagerService* cookie_service,
       TemplateURLService* template_url_service,
       std::unique_ptr<image_fetcher::ImageDecoder> image_decoder,
       scoped_refptr<net::URLRequestContextGetter> request_context_getter,
@@ -44,7 +46,10 @@ class LogoServiceImpl : public LogoService {
 
   ~LogoServiceImpl() override;
 
-  // LogoService
+  // KeyedService implementation.
+  void Shutdown() override;
+
+  // LogoService implementation.
   void GetLogo(LogoCallbacks callbacks) override;
   void GetLogo(LogoObserver* observer) override;
 
@@ -55,6 +60,12 @@ class LogoServiceImpl : public LogoService {
   void SetClockForTests(std::unique_ptr<base::Clock> clock);
 
  private:
+  class SigninObserver;
+
+  void InitializeLogoTrackerIfNecessary();
+
+  void SigninStatusChanged();
+
   // Constructor arguments.
   const base::FilePath cache_directory_;
   TemplateURLService* const template_url_service_;
@@ -66,6 +77,8 @@ class LogoServiceImpl : public LogoService {
 
   // logo_tracker_ takes ownership if/when it is initialized.
   std::unique_ptr<image_fetcher::ImageDecoder> image_decoder_;
+
+  std::unique_ptr<SigninObserver> signin_observer_;
 
   // For testing. logo_tracker_ takes ownership if/when it is initialized.
   std::unique_ptr<base::Clock> clock_for_test_;
