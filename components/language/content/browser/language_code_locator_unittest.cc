d98dd8835995cbed876e6975f283a4e80430002e
Renjie Liu <renjieliu@chromium.org>
2018-01-17 02:34:40 +0000

    Refactor language_code_locator.cc to reduce binary size.
    1) store enum instead of strings
    2) store uint64s instead of uint32s (lose a little bit accuracy)
    
    Add an additional test in India.
    
    Bug: 796578
    Change-Id: I5f8e858353769aa8840642dbc1059f5f63b13ef0
    Reviewed-on: https://chromium-review.googlesource.com/863262
    Commit-Queue: Renjie Liu <renjieliu@chromium.org>
    Reviewed-by: agrieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529550}

--- components/language/content/browser/language_code_locator_unittest.cc
+++ components/language/content/browser/language_code_locator_unittest.cc
@@ -13,11 +13,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace language {
 
-TEST(LanguageCodeLocatorTest, LocatedLanguage) {
+TEST(LanguageCodeLocatorTest, LocatedLanguageOne) {
   LanguageCodeLocator locator;
   std::vector<std::string> expected_langs = {"hi", "mr", "ur"};
   // Random place in Madhya Pradesh, expected langs should be hi;mr;ur.
   const auto& result = locator.GetLanguageCode(23.0, 80.0);
+  EXPECT_EQ(expected_langs, result);
+}
+
+TEST(LanguageCodeLocatorTest, LocatedLanguageTwo) {
+  LanguageCodeLocator locator;
+  std::vector<std::string> expected_langs = {"bn"};
+  // Random place in Tripura, expected langs should be bn.
+  const auto& result = locator.GetLanguageCode(23.7f, 91.7f);
   EXPECT_EQ(expected_langs, result);
 }
 
