07648077f040e59ecdc5813e4900c94217fc7c7c
Daniel Bratell <bratell@opera.com>
2018-01-17 16:05:52 +0000

    Removed "using namespace" from a password_manager file
    "using namespace" is not allowed by the coding style, and also
    it is not compatible with jumbo builds. This patch removes
    using namespace and gives the three symbols namespace prefixes
    instead.
    
    Bug: 746957
    Change-Id: Ibf3810287deb370ad1ac90968ba0e24020f681fb
    Reviewed-on: https://chromium-review.googlesource.com/868334
    Reviewed-by: Maxim Kolosovskiy <kolos@chromium.org>
    Commit-Queue: Daniel Bratell <bratell@opera.com>
    Cr-Commit-Position: refs/heads/master@{#529757}

--- chrome/browser/password_manager/native_backend_gnome_x.cc
+++ chrome/browser/password_manager/native_backend_gnome_x.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using autofill::PasswordForm;
 using base::UTF8ToUTF16;
 using base::UTF16ToUTF8;
-using namespace password_manager::metrics_util;
 using password_manager::MatchResult;
 using password_manager::PasswordStore;
 
@@ -105,7 +104,9 @@ std::unique_ptr<PasswordForm> FormFromAttributes(
   if (!string_attr_map["form_data"].empty()) {
     bool success = DeserializeFormDataFromBase64String(
         string_attr_map["form_data"], &form->form_data);
-    FormDeserializationStatus status = success ? GNOME_SUCCESS : GNOME_FAILURE;
+    password_manager::metrics_util::FormDeserializationStatus status =
+        success ? password_manager::metrics_util::GNOME_SUCCESS
+                : password_manager::metrics_util::GNOME_FAILURE;
     LogFormDataDeserializationStatus(status);
   }
   return form;
