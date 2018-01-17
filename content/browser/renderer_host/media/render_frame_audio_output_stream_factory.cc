649da63b838e7721ae4f5ca5e5044431e648afa2
Max Morin <maxmorin@chromium.org>
2018-01-17 11:43:56 +0000

    Add new audio stream stats.
    Used for debugging in 721732. Input stats are added for consistency,
    we'll need them sooner or later.
    
    Bug: 721732
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I52c43de180e55f73018b8686d68fa2f33375d300
    Reviewed-on: https://chromium-review.googlesource.com/866861
    Commit-Queue: Max Morin <maxmorin@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529712}

--- content/browser/renderer_host/media/render_frame_audio_output_stream_factory.cc
+++ content/browser/renderer_host/media/render_frame_audio_output_stream_factory.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <utility>
 
+#include "base/metrics/histogram_macros.h"
 #include "base/task_runner_util.h"
 #include "content/browser/renderer_host/media/audio_output_authorization_handler.h"
 #include "content/browser/renderer_host/media/audio_output_stream_observer_impl.h"
@@ -70,6 +71,8 @@ RenderFrameAudioOutputStreamFactory::RenderFrameAudioOutputStreamFactory(
 
 RenderFrameAudioOutputStreamFactory::~RenderFrameAudioOutputStreamFactory() {
   DCHECK(thread_checker_.CalledOnValidThread());
+  UMA_HISTOGRAM_EXACT_LINEAR("Media.Audio.OutputStreamsCanceledByBrowser",
+                             stream_providers_.size(), 50);
   // Make sure to close all streams.
   stream_providers_.clear();
 }
