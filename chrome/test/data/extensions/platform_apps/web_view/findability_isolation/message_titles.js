e8f7e465e402403ed7a1653e416ecce14c87382a
Lukasz Anforowicz <lukasza@chromium.org>
2018-01-17 06:42:39 +0000

    Don't leak a renderer process when guestview is set as window.opener.
    RenderViewHostImpl will be leaked, unless it will be associated
    with either a RenderFrameHostImpl or RenderFrameProxyHost (which
    maintain RVHI's ref count by calling FrameTree::AddRenderViewHostRef
    from their constructors and FrameTree::ReleaseRenderViewHostRef from
    their destructors).  Leaking a RenderViewHostImpl leads to leaking
    a renderer process.
    
    This CL makes sure that such leak doesn't happen when a guestview is set
    as window.opener (in this case a RenderFrameProxyHost exists without an
    associated RenderViewHostImpl - we should not create (and leak) a
    RenderViewHostImpl in this case).
    
    This CL adds a test that accidentally found the leak of the renderer
    process - this test will act as a regression test for the leak.  The
    main purpose of the new test though is to act as a regression test
    for findability of guestviews that share the same storage partition.
    
    Bug: 802278, 794079
    Change-Id: Ibcac8952328b290050f66f4b82d3376b6001c757
    Reviewed-on: https://chromium-review.googlesource.com/868631
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Lucas Gadani <lfg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529653}

---
+++ chrome/test/data/extensions/platform_apps/web_view/findability_isolation/message_titles.js
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var SET_WINDOW_NAME = 'Set window.name.';
+var SET_WINDOW_NAME_COMPLETE = 'Set window.name - complete.';
+
+var FIND_WINDOW_BY_NAME = 'Find window by name.';
+var FIND_WINDOW_BY_NAME_COMPLETE = 'Find window by name - complete.';
---
+++ chrome/test/data/extensions/platform_apps/web_view/findability_isolation/page.js
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function setWindowName(name) {
+  window.name = name;
+}
+
+function findWindowByName(name) {
+  var w = window.open('', name);
+  var found = w.location.href != "about:blank";
+  if (!found)
+    w.close();
+  return found;
+}
+
+function initialize() {
+  var messageHandler = new Messaging.Handler();
+  messageHandler.addHandler(SET_WINDOW_NAME, function(message, portFrom) {
+    setWindowName(message.windowName);
+    messageHandler.sendMessage({title: SET_WINDOW_NAME_COMPLETE}, portFrom);
+  });
+  messageHandler.addHandler(FIND_WINDOW_BY_NAME, function(message, portFrom) {
+    var found = findWindowByName(message.windowName);
+    messageHandler.sendMessage(
+        {title: FIND_WINDOW_BY_NAME_COMPLETE, found: found},
+        portFrom);
+  });
+}
+window.onload = initialize;
