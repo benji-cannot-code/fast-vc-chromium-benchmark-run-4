cd8bc2fe53120c1f60f93de33cb1bb3c5218dc85
Joel Einbinder <einbinder@chromium.org>
2018-01-17 05:44:24 +0000

    DevTools: treat network content as truth when saving for overrides
    This prevents a race where rapidly changing a uiSourceCode recently saved for
    overrides would lose commits immediately after saving.
    
    Bug: 760316
    
    Change-Id: I4249ca6e6cfa1f6a4ecb32cc7ac61371a08b99df
    Reviewed-on: https://chromium-review.googlesource.com/869372
    Commit-Queue: Joel Einbinder <einbinder@chromium.org>
    Reviewed-by: Andrey Lushnikov <lushnikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529636}

--- third_party/WebKit/Source/devtools/front_end/persistence/NetworkPersistenceManager.js
+++ third_party/WebKit/Source/devtools/front_end/persistence/NetworkPersistenceManager.js
@@ -11,6 +11,7 @@ Persistence.NetworkPersistenceManager = class extends Common.Object {
     super();
     this._bindingSymbol = Symbol('NetworkPersistenceBinding');
     this._originalResponseContentPromiseSymbol = Symbol('OriginalResponsePromise');
+    this._savingSymbol = Symbol('SavingForOverrides');
 
     this._enabledSetting = Common.settings.moduleSetting('persistenceNetworkOverridesEnabled');
     this._enabledSetting.addChangeListener(this._enabledChanged, this);
@@ -220,8 +221,9 @@ Persistence.NetworkPersistenceManager = class extends Common.Object {
     networkUISourceCode[this._bindingSymbol] = binding;
     fileSystemUISourceCode[this._bindingSymbol] = binding;
     Persistence.persistence.addBinding(binding);
-    var content = await fileSystemUISourceCode.requestContent();
-    Persistence.persistence.syncContent(fileSystemUISourceCode, content);
+    var uiSourceCodeOfTruth = networkUISourceCode[this._savingSymbol] ? networkUISourceCode : fileSystemUISourceCode;
+    var content = await uiSourceCodeOfTruth.requestContent();
+    Persistence.persistence.syncContent(uiSourceCodeOfTruth, content);
   }
 
   /**
@@ -236,7 +238,7 @@ Persistence.NetworkPersistenceManager = class extends Common.Object {
    */
   canSaveUISourceCodeForOverrides(uiSourceCode) {
     return this._active && uiSourceCode.project().type() === Workspace.projectTypes.Network &&
-        !uiSourceCode[this._bindingSymbol];
+        !uiSourceCode[this._bindingSymbol] && !uiSourceCode[this._savingSymbol];
   }
 
   /**
@@ -245,6 +247,7 @@ Persistence.NetworkPersistenceManager = class extends Common.Object {
   async saveUISourceCodeForOverrides(uiSourceCode) {
     if (!this.canSaveUISourceCodeForOverrides(uiSourceCode))
       return;
+    uiSourceCode[this._savingSymbol] = true;
     var encodedPath = this._encodedPathFromUrl(uiSourceCode.url());
     var content = await uiSourceCode.requestContent();
     var encoded = await uiSourceCode.contentEncoded();
@@ -253,6 +256,7 @@ Persistence.NetworkPersistenceManager = class extends Common.Object {
     encodedPath = encodedPath.substr(0, lastIndexOfSlash);
     await this._project.createFile(encodedPath, encodedFileName, content, encoded);
     this._fileCreatedForTest(encodedPath, encodedFileName);
+    uiSourceCode[this._savingSymbol] = false;
   }
 
   /**
