886aad10bc13f3da5529292fe60a527085f9ccaf
Matt Reynolds <mattreynolds@google.com>
2018-01-17 20:33:56 +0000

    Don't clear devices during gamepad enumeration
    In GamepadPlatformDataFetcherLinux, when enumerating devices with
    EnumerateSubsystemDevices, we incorrectly clear the device list. Since
    we enumerate multiple subsystems, this causes us to lose all devices
    except those found in the last subsystem. This CL instead clears the
    device list before enumeration begins.
    
    This CL also combines some subsystem strings that were duplicated.
    
    BUG=749295
    
    Change-Id: I905b75ba37e1e53cbd67777ed7e07c2a66df4073
    Reviewed-on: https://chromium-review.googlesource.com/865807
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529859}

--- device/gamepad/gamepad_platform_data_fetcher_linux.cc
+++ device/gamepad/gamepad_platform_data_fetcher_linux.cc
@@ -20,11 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "device/udev_linux/scoped_udev.h"
 #include "device/udev_linux/udev_linux.h"
 
-namespace {
-const char kInputSubsystem[] = "input";
-const char kHidrawSubsystem[] = "hidraw";
-}  // namespace
-
 namespace device {
 
 GamepadPlatformDataFetcherLinux::GamepadPlatformDataFetcherLinux() = default;
@@ -42,14 +37,20 @@ GamepadSource GamepadPlatformDataFetcherLinux::source() {
 
 void GamepadPlatformDataFetcherLinux::OnAddedToProvider() {
   std::vector<UdevLinux::UdevMonitorFilter> filters;
-  filters.push_back(UdevLinux::UdevMonitorFilter(kInputSubsystem, nullptr));
-  filters.push_back(UdevLinux::UdevMonitorFilter(kHidrawSubsystem, nullptr));
+  filters.push_back(
+      UdevLinux::UdevMonitorFilter(UdevGamepadLinux::kInputSubsystem, nullptr));
+  filters.push_back(UdevLinux::UdevMonitorFilter(
+      UdevGamepadLinux::kHidrawSubsystem, nullptr));
   udev_.reset(new UdevLinux(
       filters, base::Bind(&GamepadPlatformDataFetcherLinux::RefreshDevice,
                           base::Unretained(this))));
 
-  EnumerateSubsystemDevices(kInputSubsystem);
-  EnumerateSubsystemDevices(kHidrawSubsystem);
+  for (auto it = devices_.begin(); it != devices_.end(); ++it)
+    it->get()->Shutdown();
+  devices_.clear();
+
+  EnumerateSubsystemDevices(UdevGamepadLinux::kInputSubsystem);
+  EnumerateSubsystemDevices(UdevGamepadLinux::kHidrawSubsystem);
 }
 
 void GamepadPlatformDataFetcherLinux::GetGamepadData(bool) {
@@ -133,7 +134,7 @@ void GamepadPlatformDataFetcherLinux::RefreshJoydevDevice(
   // This function should just walk up the tree one level.
   udev_device* parent_dev =
       device::udev_device_get_parent_with_subsystem_devtype(
-          dev, kInputSubsystem, nullptr);
+          dev, UdevGamepadLinux::kInputSubsystem, nullptr);
   if (!parent_dev) {
     device->CloseJoydevNode();
     if (device->IsEmpty())
@@ -249,8 +250,6 @@ void GamepadPlatformDataFetcherLinux::EnumerateSubsystemDevices(
   ret = udev_enumerate_scan_devices(enumerate.get());
   if (ret != 0)
     return;
-
-  devices_.clear();
 
   udev_list_entry* devices = udev_enumerate_get_list_entry(enumerate.get());
   for (udev_list_entry* dev_list_entry = devices; dev_list_entry != nullptr;
--- device/gamepad/udev_gamepad_linux.h
+++ device/gamepad/udev_gamepad_linux.h
@@ -23,6 +23,9 @@ class UdevGamepadLinux {
     HIDRAW,
   };
 
+  static const char kInputSubsystem[];
+  static const char kHidrawSubsystem[];
+
   ~UdevGamepadLinux() = default;
 
   // Factory method for creating UdevGamepadLinux instances. Extracts info
