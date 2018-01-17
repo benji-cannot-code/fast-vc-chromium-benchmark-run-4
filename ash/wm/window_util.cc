f2c704ba6ed3ecc6d18bdc567d1da6cb661032d4
Mitsuru Oshima <oshima@chromium.org>
2018-01-17 21:03:44 +0000

    Move client controlled window to other displays correctly.
    * Client controlled window will not change the bounds when
     the window is added to another root window, until it is
     instructe by a client. Explicitly send the bounds request.
    
    BUG=778438
    TEST=Covered by unit test. Manual.
    
    Change-Id: I5d0df9becb3168eaf5f372c8d83e4112d3dcb7f7
    Reviewed-on: https://chromium-review.googlesource.com/869358
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Qiang(Joe) Xu <warx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529889}

--- ash/wm/window_util.cc
+++ ash/wm/window_util.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "ash/wm/resize_handle_window_targeter.h"
 #include "ash/wm/widget_finder.h"
+#include "ash/wm/window_positioning_utils.h"
 #include "ash/wm/window_properties.h"
 #include "ash/wm/window_state.h"
 #include "ash/wm/wm_event.h"
@@ -31,6 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/window_event_dispatcher.h"
 #include "ui/base/hit_test.h"
 #include "ui/compositor/dip_util.h"
+#include "ui/display/display.h"
+#include "ui/display/screen.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/views/view.h"
@@ -127,6 +130,20 @@ void SetAutoHideShelf(aura::Window* window, bool autohide) {
 
 bool MoveWindowToDisplay(aura::Window* window, int64_t display_id) {
   DCHECK(window);
+  WindowState* window_state = GetWindowState(window);
+  if (window_state->allow_set_bounds_direct()) {
+    aura::Window* root = Shell::GetRootWindowForDisplayId(display_id);
+    if (root) {
+      gfx::Rect bounds = window->bounds();
+      MoveWindowToRoot(window, root);
+      // Client controlled won't update the bounds upon the root window
+      // Change. Explicitly update the bounds so that the client can
+      // make decision.
+      window->SetBounds(bounds);
+      return true;
+    }
+    return false;
+  }
   aura::Window* root = Shell::GetRootWindowForDisplayId(display_id);
   return root && MoveWindowToRoot(window, root);
 }
