861b178a11498dc14fa51ceb05380a73f12f69cc
Eugene But <eugenebut@google.com>
2018-01-17 17:34:52 +0000

    Removed navigation_context_impl.h include from tab_unittest.mm.
    Replaced NavigationContextImpl with FakeNavigationContext.
    Also renamed method arguments to follow C++ Style Guide.
    
    Bug: 620465
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I3d1860c6d2fc8c726c81739c83db6bf40b33ccb1
    Reviewed-on: https://chromium-review.googlesource.com/868638
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529791}

--- ios/chrome/browser/tabs/tab_unittest.mm
+++ ios/chrome/browser/tabs/tab_unittest.mm
@@ -45,8 +45,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/web/public/navigation_item.h"
 #import "ios/web/public/navigation_manager.h"
 #include "ios/web/public/referrer.h"
+#import "ios/web/public/test/fakes/fake_navigation_context.h"
 #include "ios/web/public/test/test_web_thread_bundle.h"
-#import "ios/web/web_state/navigation_context_impl.h"
 #import "ios/web/web_state/ui/crw_web_controller.h"
 #import "ios/web/web_state/web_state_impl.h"
 #import "net/base/mac/url_conversions.h"
@@ -206,29 +206,27 @@ class TabTest : public BlockCleanupTest {
     BlockCleanupTest::TearDown();
   }
 
-  void BrowseTo(const GURL& userUrl, const GURL& redirectUrl, NSString* title) {
+  void BrowseTo(const GURL& user_url,
+                const GURL& redirect_url,
+                NSString* title) {
     DCHECK_EQ(tab_.webState, web_state_impl_.get());
 
-    std::unique_ptr<web::NavigationContext> context1 =
-        web::NavigationContextImpl::CreateNavigationContext(
-            web_state_impl_.get(), userUrl,
-            ui::PageTransition::PAGE_TRANSITION_TYPED, false);
-    web_state_impl_->OnNavigationStarted(context1.get());
+    web::FakeNavigationContext context1;
+    context1.SetUrl(user_url);
+    web_state_impl_->OnNavigationStarted(&context1);
 
     web::Referrer empty_referrer;
     [tab_ navigationManagerImpl]->AddPendingItem(
-        redirectUrl, empty_referrer, ui::PAGE_TRANSITION_CLIENT_REDIRECT,
+        redirect_url, empty_referrer, ui::PAGE_TRANSITION_CLIENT_REDIRECT,
         web::NavigationInitiationType::RENDERER_INITIATED,
         web::NavigationManager::UserAgentOverrideOption::INHERIT);
 
-    std::unique_ptr<web::NavigationContext> context2 =
-        web::NavigationContextImpl::CreateNavigationContext(
-            web_state_impl_.get(), redirectUrl,
-            ui::PageTransition::PAGE_TRANSITION_TYPED, false);
-    web_state_impl_->OnNavigationStarted(context2.get());
+    web::FakeNavigationContext context2;
+    context2.SetUrl(redirect_url);
+    web_state_impl_->OnNavigationStarted(&context2);
     [tab_ navigationManagerImpl]->CommitPendingItem();
-    web_state_impl_->UpdateHttpResponseHeaders(redirectUrl);
-    web_state_impl_->OnNavigationFinished(context2.get());
+    web_state_impl_->UpdateHttpResponseHeaders(redirect_url);
+    web_state_impl_->OnNavigationFinished(&context2);
     web_state_impl_->SetIsLoading(true);
 
     base::string16 new_title = base::SysNSStringToUTF16(title);
@@ -236,7 +234,7 @@ class TabTest : public BlockCleanupTest {
 
     web_state_impl_->OnTitleChanged();
     web_state_impl_->SetIsLoading(false);
-    web_state_impl_->OnPageLoaded(redirectUrl, true);
+    web_state_impl_->OnPageLoaded(redirect_url, true);
   }
 
   void BrowseToNewTab() {
