431a87e7bfa62ca772042d258ac129330cd924d0
Xianzhu Wang <wangxianzhu@chromium.org>
2018-01-17 22:12:48 +0000

    [SPv175+] Send real visual rects to cc
    Bug: 771643
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I54f5a8525f3e79a1ccaed85bdbb5aec56d69eb82
    Reviewed-on: https://chromium-review.googlesource.com/868954
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529911}

--- third_party/WebKit/Source/platform/graphics/compositing/CompositedLayerRasterInvalidator.cpp
+++ third_party/WebKit/Source/platform/graphics/compositing/CompositedLayerRasterInvalidator.cpp
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <utility>
 
+#include "platform/graphics/compositing/PaintChunksToCcLayer.h"
 #include "platform/graphics/paint/GeometryMapper.h"
 
 namespace blink {
@@ -33,17 +34,8 @@ void CompositedLayerRasterInvalidator::SetTracksRasterInvalidations(
 IntRect CompositedLayerRasterInvalidator::MapRectFromChunkToLayer(
     const FloatRect& r,
     const PaintChunk& chunk) const {
-  FloatClipRect rect(r);
-  GeometryMapper::LocalToAncestorVisualRect(
-      chunk.properties.property_tree_state, layer_state_, rect);
-  if (rect.Rect().IsEmpty())
-    return IntRect();
-
-  // Now rect is in the space of the containing transform node of pending_layer,
-  // so need to subtract off the layer offset.
-  rect.Rect().Move(-layer_bounds_.x(), -layer_bounds_.y());
-  rect.Rect().Inflate(chunk.outset_for_raster_effects);
-  return ClipByLayerBounds(EnclosingIntRect(rect.Rect()));
+  return ClipByLayerBounds(PaintChunksToCcLayer::MapRectFromChunkToLayer(
+      r, chunk, layer_state_, layer_bounds_.OffsetFromOrigin()));
 }
 
 TransformationMatrix CompositedLayerRasterInvalidator::ChunkToLayerTransform(
