e7b7c08b97df903b1c88571c6f10005b7f88fec4
Jonathan Backer <backer@chromium.org>
2018-01-17 17:00:10 +0000

    Fix autoformat and pylint presubmit
    Three changes
    
    1) Use depot_tools/clang-format --- it's different from clang-format on
    your workstation. This makes "python
    gpu/command_buffer/build_gles2_cmd_buffer.py" idempotent.
    
    2) Run pylint as a presubmit step to keep the autogen python scripts
    clean.
    
    3) Fix up pylint errors other than redefined-builtin
    
    BUG=789238
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I69da2da0e5b5b1c65085a00806e3733b98b19a2e
    Reviewed-on: https://chromium-review.googlesource.com/867470
    Commit-Queue: Jonathan Backer <backer@chromium.org>
    Reviewed-by: Victor Miura <vmiura@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529777}

--- gpu/command_buffer/build_gles2_cmd_buffer.py
+++ gpu/command_buffer/build_gles2_cmd_buffer.py
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-#
-# pylint: disable=interface-not-implemented
 """code generator for GLES2 command buffers."""
 
 import itertools
@@ -11386,7 +11384,7 @@ const size_t GLES2Util::enum_to_string_table_len_ =
 
 
 def Format(generated_files):
-  formatter = "clang-format"
+  formatter = "third_party/depot_tools/clang-format"
   if platform.system() == "Windows":
     formatter += ".bat"
   for filename in generated_files:
