c0460a8081ce6a189f799b27a4630f1cfa6f2097
Maksim Sisov <msisov@igalia.com>
2018-01-17 09:13:37 +0000

    [ozone] Move MockPlatformWindowDelegate to ui/ozone/test
    This is an effort to Move MockPlatformWindowDelegate to
    //ui/ozone/test and create //ui/ozone:test_support in order
    to share some helpful mocked classes or any other stuff between
    ozone unittests.
    
    Bug: 
    Change-Id: Id4a1ef79b2d524187247adb5ac8c2dac6e2346ae
    Reviewed-on: https://chromium-review.googlesource.com/827081
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529680}

--- ui/ozone/platform/wayland/wayland_test.h
+++ ui/ozone/platform/wayland/wayland_test.h
@@ -10,9 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/ui_features.h"
 #include "ui/ozone/platform/wayland/fake_server.h"
-#include "ui/ozone/platform/wayland/mock_platform_window_delegate.h"
 #include "ui/ozone/platform/wayland/wayland_connection.h"
 #include "ui/ozone/platform/wayland/wayland_window.h"
+#include "ui/ozone/test/mock_platform_window_delegate.h"
 
 #if BUILDFLAG(USE_XKBCOMMON)
 #include "ui/events/ozone/layout/xkb/xkb_evdev_codes.h"
--- ui/ozone/test/mock_platform_window_delegate.h
+++ ui/ozone/test/mock_platform_window_delegate.h
@@ -3,12 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef UI_OZONE_PLATFORM_WAYLAND_MOCK_PLATFORM_WINDOW_DELEGATE_H_
-#define UI_OZONE_PLATFORM_WAYLAND_MOCK_PLATFORM_WINDOW_DELEGATE_H_
+#ifndef UI_OZONE_TEST_MOCK_PLATFORM_WINDOW_DELEGATE_H_
+#define UI_OZONE_TEST_MOCK_PLATFORM_WINDOW_DELEGATE_H_
 
-#include "ui/platform_window/platform_window_delegate.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "ui/gfx/geometry/rect.h"
+#include "ui/platform_window/platform_window_delegate.h"
 
 namespace ui {
 
@@ -35,4 +35,4 @@ class MockPlatformWindowDelegate : public PlatformWindowDelegate {
 
 }  // namespace ui
 
-#endif  // UI_OZONE_PLATFORM_WAYLAND_MOCK_PLATFORM_WINDOW_DELEGATE_H_
+#endif  // UI_OZONE_TEST_MOCK_PLATFORM_WINDOW_DELEGATE_H_
