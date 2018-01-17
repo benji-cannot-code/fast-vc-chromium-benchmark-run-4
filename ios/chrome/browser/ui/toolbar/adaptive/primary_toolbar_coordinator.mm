7b17b0b198e3f5eca03223a03b487361cea6a003
Gauthier Ambard <gambard@chromium.org>
2018-01-17 16:06:02 +0000

    Focus and unfocus the omnibox from the toolbar
    This CL implements the focus/unfocus methods for the adaptive toolbar.
    
    Bug: 799422
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I48da1eb9ec563f7c4c4e0f1d19a24d19e1523e6e
    Reviewed-on: https://chromium-review.googlesource.com/866740
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529758}

--- ios/chrome/browser/ui/toolbar/adaptive/primary_toolbar_coordinator.mm
+++ ios/chrome/browser/ui/toolbar/adaptive/primary_toolbar_coordinator.mm
@@ -102,11 +102,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma mark - OmniboxFocuser
 
 - (void)focusOmnibox {
-  // TODO(crbug.com/799438): Implement that.
+  [self.locationBarCoordinator.locationBarView.textField becomeFirstResponder];
 }
 
 - (void)cancelOmniboxEdit {
-  // TODO(crbug.com/799438): Implement that.
+  _locationBar->HideKeyboardAndEndEditing();
+  _locationBar->SetShouldShowHintText(
+      [self.delegate toolbarModelIOS]->ShouldDisplayHintText());
+  _locationBar->OnToolbarUpdated();
 }
 
 - (void)focusFakebox {
