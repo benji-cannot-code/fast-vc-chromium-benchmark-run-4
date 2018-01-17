61ff7b4ba514da49e8f59d54d3191acefbbd061b
Michael Lippautz <mlippautz@chromium.org>
2018-01-17 14:37:16 +0000

    [bindings] Fix external memory accounting for SerializedScriptValue
    Upon unpacking a SerializedScriptValue into an
    UnpackedSerializedScriptValue we also move the ArrayBufferContents to
    the unpacked value. Subsequent calls to
    RegisterMemoryAllocatedWithCurrentScriptContext will not find the
    contents object and thus fail to re-register external memory.
    
    This CL moves re-registering into the unpacking step.
    
    Bug: 801484
    Change-Id: If4fa5082579629fb5f4c3c0666e704bd14bb5ec0
    Reviewed-on: https://chromium-review.googlesource.com/868019
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529738}

--- third_party/WebKit/Source/core/events/MessageEvent.cpp
+++ third_party/WebKit/Source/core/events/MessageEvent.cpp
@@ -110,9 +110,6 @@ MessageEvent::MessageEvent(scoped_refptr<SerializedScriptValue> data,
       last_event_id_(last_event_id),
       source_(source),
       ports_(ports) {
-  if (data_as_serialized_script_value_)
-    data_as_serialized_script_value_->Value()
-        ->RegisterMemoryAllocatedWithCurrentScriptContext();
   DCHECK(IsValidSource(source_.Get()));
 }
 
@@ -131,9 +128,6 @@ MessageEvent::MessageEvent(scoped_refptr<SerializedScriptValue> data,
       source_(source),
       channels_(std::move(channels)),
       suborigin_(suborigin) {
-  if (data_as_serialized_script_value_)
-    data_as_serialized_script_value_->Value()
-        ->RegisterMemoryAllocatedWithCurrentScriptContext();
   DCHECK(IsValidSource(source_.Get()));
 }
 
@@ -219,10 +213,6 @@ void MessageEvent::initMessageEvent(const AtomicString& type,
   ports_ = ports;
   is_ports_dirty_ = true;
   suborigin_ = "";
-
-  if (data_as_serialized_script_value_)
-    data_as_serialized_script_value_->Value()
-        ->RegisterMemoryAllocatedWithCurrentScriptContext();
 }
 
 void MessageEvent::initMessageEvent(const AtomicString& type,
