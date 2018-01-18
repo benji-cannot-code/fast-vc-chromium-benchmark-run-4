9cf29bcc598c15123ac3fc08e8f4fe003b598ee9
Tetsui Ohkubo <tetsui@chromium.org>
2018-01-18 01:53:29 +0000

    Set clickable to Sign-in and Sync error notifications
    As clickable flag is not set for these notifications, even though they
    are clickable, the mouse cursor did not change for them.
    
    TEST=manual
    BUG=801089
    
    Change-Id: I378acab7339137db3c3f93bf33075fe9d297dccb
    Reviewed-on: https://chromium-review.googlesource.com/869913
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530009}

--- chrome/browser/signin/signin_error_notifier_ash.cc
+++ chrome/browser/signin/signin_error_notifier_ash.cc
@@ -106,6 +106,7 @@ void SigninErrorNotifier::OnErrorChanged() {
               base::Bind(&HandleNotificationClick)),
           kNotificationWarningIcon,
           message_center::SystemNotificationWarningLevel::WARNING);
+  notification->set_clickable(true);
   notification->SetSystemPriority();
 
   // Update or add the notification.
