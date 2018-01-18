d6dcad7f693fd162621cc25e99ecea1fac7e848b
MinChen <minch@chromium.org>
2018-01-18 22:15:30 +0000

    cros: Manual revert rename TabletPowerButtonController to ConvertiblePowerButtonController and create PowerButtonUtil.
    changes in this cl,
    Since we will put the logic of power button menu in
    TabletPowerButtonController instead of creating a new controller.
    Reverting related previous cls,
    1. Revert rename TabletPowerButtonController to ConvertiblePowerButtonController.
       Related cl,
       https://chromium-review.googlesource.com/c/chromium/src/+/852518
    2. Revert the creating of PowerButtonUtil.
       Related cl,
       https://chromium-review.googlesource.com/c/chromium/src/+/852905
    
    Bug: 799524
    Change-Id: I4883c7eb9f4b73fad9e8d862fbc106aee22072d4
    Reviewed-on: https://chromium-review.googlesource.com/874911
    Reviewed-by: Dan Erat <derat@chromium.org>
    Commit-Queue: min c <minch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530295}

--- ash/system/power/tablet_power_button_controller_test_api.cc
+++ ash/system/power/tablet_power_button_controller_test_api.cc
@@ -3,26 +3,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "ash/system/power/convertible_power_button_controller_test_api.h"
+#include "ash/system/power/tablet_power_button_controller_test_api.h"
 
-#include "ash/system/power/convertible_power_button_controller.h"
 #include "ash/system/power/power_button_display_controller.h"
+#include "ash/system/power/tablet_power_button_controller.h"
 
 namespace ash {
 
-ConvertiblePowerButtonControllerTestApi::
-    ConvertiblePowerButtonControllerTestApi(
-        ConvertiblePowerButtonController* controller)
+TabletPowerButtonControllerTestApi::TabletPowerButtonControllerTestApi(
+    TabletPowerButtonController* controller)
     : controller_(controller) {}
 
-ConvertiblePowerButtonControllerTestApi::
-    ~ConvertiblePowerButtonControllerTestApi() = default;
+TabletPowerButtonControllerTestApi::~TabletPowerButtonControllerTestApi() =
+    default;
 
-bool ConvertiblePowerButtonControllerTestApi::ShutdownTimerIsRunning() const {
+bool TabletPowerButtonControllerTestApi::ShutdownTimerIsRunning() const {
   return controller_->shutdown_timer_.IsRunning();
 }
 
-bool ConvertiblePowerButtonControllerTestApi::TriggerShutdownTimeout() {
+bool TabletPowerButtonControllerTestApi::TriggerShutdownTimeout() {
   if (!controller_->shutdown_timer_.IsRunning())
     return false;
 
@@ -32,8 +31,7 @@ bool ConvertiblePowerButtonControllerTestApi::TriggerShutdownTimeout() {
   return true;
 }
 
-void ConvertiblePowerButtonControllerTestApi::SendKeyEvent(
-    ui::KeyEvent* event) {
+void TabletPowerButtonControllerTestApi::SendKeyEvent(ui::KeyEvent* event) {
   controller_->display_controller_->OnKeyEvent(event);
 }
 
