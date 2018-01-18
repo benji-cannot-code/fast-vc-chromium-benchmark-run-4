16a18479706ae26dc5430ac10ad420dff5377778
Christopher Grant <cjgrant@chromium.org>
2018-01-18 03:00:47 +0000

    VR: Tweaks to URL and omnibox colors and layout
    BUG=
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I5c237eb211138e093cdfa59247ffda7d9f49af97
    Reviewed-on: https://chromium-review.googlesource.com/872257
    Commit-Queue: Christopher Grant <cjgrant@chromium.org>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530032}

--- chrome/browser/vr/model/color_scheme.cc
+++ chrome/browser/vr/model/color_scheme.cc
@@ -117,7 +117,7 @@ void InitializeColorSchemes() {
   normal_scheme.omnibox_hint = 0xFF999999;
   normal_scheme.suggestion_text = 0xFF595959;
   normal_scheme.suggestion_dim_text = 0xFF999999;
-  normal_scheme.suggestion_url_text = 0xFF9999FF;
+  normal_scheme.suggestion_url_text = 0xFF5595FE;
   normal_scheme.suggestion_button_colors.foreground =
       normal_scheme.suggestion_text;
   normal_scheme.suggestion_button_colors.background =
@@ -235,7 +235,7 @@ void InitializeColorSchemes() {
   incognito_scheme.omnibox_hint = 0x80FFFFFF;
   incognito_scheme.suggestion_text = 0xCCFFFFFF;
   incognito_scheme.suggestion_dim_text = 0x88FFFFFF;
-  incognito_scheme.suggestion_url_text = 0xFF9999FF;
+  incognito_scheme.suggestion_url_text = 0xFF5595FE;
   incognito_scheme.suggestion_button_colors.foreground =
       incognito_scheme.suggestion_text;
   incognito_scheme.suggestion_button_colors.background =
