2bdc1d8b9b1587a98d57dc239daf7817c7c4e548
Tom Anderson <thomasanderson@chromium.org>
2018-01-18 22:42:44 +0000

    Relayout frame buttons when fullscreen state changes
    This CL fixes a bug where frame buttons are not rendered after these steps:
    1. Maximize a video
    2. Alt-tab to another app
    3. Alt-tab back to the video
    4. Unmaximize the video
    
    The buttons were getting rendered at zero-size since the tabstrip has 0 height
    when the video is playing.  The solution is to relayout the buttons when the
    maximized state changes.
    
    BUG=803063
    R=sky
    
    Change-Id: Ic1c1e98ce08ead6e8dee284a333515ed022520a5
    Reviewed-on: https://chromium-review.googlesource.com/874992
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530315}

--- ui/views/widget/desktop_aura/desktop_window_tree_host_x11.cc
+++ ui/views/widget/desktop_aura/desktop_window_tree_host_x11.cc
@@ -1000,6 +1000,7 @@ void DesktopWindowTreeHostX11::SetFullscreen(bool fullscreen) {
   if (is_fullscreen_ == fullscreen)
     return;
   is_fullscreen_ = fullscreen;
+  OnFullscreenStateChanged();
   if (is_fullscreen_)
     delayed_resize_task_.Cancel();
 
@@ -1327,6 +1328,8 @@ void DesktopWindowTreeHostX11::OnDisplayMetricsChanged(
 }
 
 void DesktopWindowTreeHostX11::OnMaximizedStateChanged() {}
+
+void DesktopWindowTreeHostX11::OnFullscreenStateChanged() {}
 
 ////////////////////////////////////////////////////////////////////////////////
 // DesktopWindowTreeHostX11, private:
