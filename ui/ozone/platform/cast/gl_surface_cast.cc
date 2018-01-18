fb612aefb93c74c10d195707a015704ff5fd8772
Daniel Nicoara <dnicoara@chromium.org>
2018-01-18 22:52:40 +0000

    [Chromecast] Add flag to override the vsync interval
    Adds more flexibility in setting vsync to a custom value.
    
    Also fixed propagation of --cast-initial-screen-[width,height] flags.
    
    Bug: b/72119055
    Test: Ran on device with flag set and verified that the GPU process uses
    the overwritten value.
    
    Change-Id: I9b15223bf2763e7ee8ef0560595738c52bd87c43
    Reviewed-on: https://chromium-review.googlesource.com/874910
    Commit-Queue: Daniel Nicoara <dnicoara@chromium.org>
    Reviewed-by: Sergey Volk <servolk@chromium.org>
    Reviewed-by: Alex Sakhartchouk <alexst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530322}

--- ui/ozone/platform/cast/gl_surface_cast.cc
+++ ui/ozone/platform/cast/gl_surface_cast.cc
@@ -7,7 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "base/memory/ptr_util.h"
+#include "base/strings/string_number_conversions.h"
 #include "chromecast/base/cast_features.h"
+#include "chromecast/base/chromecast_switches.h"
 #include "ui/gfx/vsync_provider.h"
 #include "ui/ozone/common/egl_util.h"
 #include "ui/ozone/platform/cast/gl_ozone_egl_cast.h"
@@ -18,10 +20,20 @@ namespace {
 // or make it dynamic that throttles framerate if device is overheating.
 base::TimeDelta GetVSyncInterval() {
   if (base::FeatureList::IsEnabled(chromecast::kTripleBuffer720)) {
-    return base::TimeDelta::FromSeconds(1) / 59.9;
-  } else {
-    return base::TimeDelta::FromSeconds(2) / 59.9;
+    return base::TimeDelta::FromSeconds(1) / 59.94;
   }
+
+  base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
+  if (command_line->HasSwitch(switches::kVSyncInterval)) {
+    const std::string interval_str =
+        command_line->GetSwitchValueASCII(switches::kVSyncInterval);
+    double interval = 0;
+    if (base::StringToDouble(interval_str, &interval) && interval > 0) {
+      return base::TimeDelta::FromSeconds(1) / interval;
+    }
+  }
+
+  return base::TimeDelta::FromSeconds(2) / 59.94;
 }
 
 }  // namespace
