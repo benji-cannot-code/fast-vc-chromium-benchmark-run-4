f6e7e4187728e5d638bf7c5d66512b96de9a67c3
Evan Stade <estade@chromium.org>
2018-01-18 17:27:15 +0000

    Remove obsolete includes of message_center.h
    Bug: 783018
    Change-Id: Ib6ad3bd5d811dfe756b5f3a54c26fa50639d418e
    Reviewed-on: https://chromium-review.googlesource.com/872331
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530180}

--- chrome/browser/chromeos/tether/tether_service.cc
+++ chrome/browser/chromeos/tether/tether_service.cc
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service.h"
 #include "components/proximity_auth/logging/logging.h"
 #include "device/bluetooth/bluetooth_adapter_factory.h"
-#include "ui/message_center/message_center.h"
 
 namespace {
 
