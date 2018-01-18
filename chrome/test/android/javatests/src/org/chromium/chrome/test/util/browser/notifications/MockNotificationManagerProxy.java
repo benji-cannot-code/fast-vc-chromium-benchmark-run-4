95f0566dbad983af0749f5fdd7cf3c0264fa5713
Mugdha Lakhani <nator@chromium.org>
2018-01-18 17:14:55 +0000

    [Android Notifications] Implement GetDisplayed() for Android O+
    Bug: 800363
    Change-Id: I41bddcffac5ee7c94f9813872b8a825c1cc761c1
    Reviewed-on: https://chromium-review.googlesource.com/871051
    Commit-Queue: Mugdha Lakhani <nator@chromium.org>
    Reviewed-by: Anita Woodruff <awdf@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530177}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/util/browser/notifications/MockNotificationManagerProxy.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/util/browser/notifications/MockNotificationManagerProxy.java
@@ -10,6 +10,7 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationChannelGroup;
 import android.os.Build;
+import android.service.notification.StatusBarNotification;
 
 import org.chromium.chrome.browser.notifications.NotificationManagerProxy;
 
@@ -112,8 +113,8 @@ public class MockNotificationManagerProxy implements NotificationManagerProxy {
         return key;
     }
 
-    // The following Channel methods are not implemented because a naive implementation would
-    // have compatibility issues (NotificationChannel is new in O), and we currently don't need them
+    // The following methods are not implemented because a naive implementation would
+    // have compatibility issues (this functionality is new in O), and we currently don't need them
     // where the MockNotificationManagerProxy is used in tests.
 
     @TargetApi(Build.VERSION_CODES.O)
@@ -148,4 +149,9 @@ public class MockNotificationManagerProxy implements NotificationManagerProxy {
 
     @Override
     public void deleteNotificationChannelGroup(String groupId) {}
+
+    @Override
+    public StatusBarNotification[] getActiveNotifications() {
+        return new StatusBarNotification[0];
+    }
 }
