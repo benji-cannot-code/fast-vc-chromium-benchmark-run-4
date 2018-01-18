1e182dcb195a79f187cdefb53db9e24793b60868
Alexey Kozyatinskiy <kozyatinskiy@chromium.org>
2018-01-18 22:11:45 +0000

    [DevTools] reenable some of debugger tests
    TBR=lushnikov@chromium.org
    
    Bug: none
    Change-Id: If35d4a79b67574b03b438ffcd22ab5804cc79238
    Reviewed-on: https://chromium-review.googlesource.com/874603
    Reviewed-by: Aleksey Kozyatinskiy <kozyatinskiy@chromium.org>
    Commit-Queue: Aleksey Kozyatinskiy <kozyatinskiy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530293}

--- third_party/WebKit/LayoutTests/http/tests/devtools/sources/debugger-ui/function-generator-details.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/sources/debugger-ui/function-generator-details.js
@@ -47,13 +47,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   `);
 
   function performStandardTestCase(pageExpression, next) {
-    TestRunner.evaluateInPage(pageExpression, didEvaluate);
+    UI.context.flavor(SDK.ExecutionContext)
+        .evaluate({expression: pageExpression})
+        .then(didEvaluate);
 
-    function didEvaluate(remote) {
+    function didEvaluate({object}) {
       TestRunner.addResult(
-          pageExpression + ': type = ' + remote.type +
-          ', subtype = ' + remote.subtype);
-      remote.getOwnPropertiesPromise().then(dumpInternalProperties);
+          pageExpression + ': type = ' + object.type +
+          ', subtype = ' + object.subtype);
+      object.getOwnPropertiesPromise().then(dumpInternalProperties);
     }
 
     function dumpInternalProperties(properties) {
