209353f9dab0ca05ad19c68c3907c5d5920e8554
Wenzhao Zang <wzang@chromium.org>
2018-01-18 09:33:26 +0000

    Chrome OS: Update fine-grained time zone resolve UI after review.
    This CL contains implementation of Time Zone Settings subpage in Chrome
    Settings.
    
    Bug: 721578
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I0d8227ba6ad748482d88429eadc5b2c6d365fcd3
    Reviewed-on: https://chromium-review.googlesource.com/852829
    Commit-Queue: Wenzhao (Colin) Zang <wzang@chromium.org>
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530098}

--- chrome/browser/ui/webui/settings/chromeos/date_time_handler.cc
+++ chrome/browser/ui/webui/settings/chromeos/date_time_handler.cc
@@ -84,13 +84,6 @@ DateTimeHandler* DateTimeHandler::Create(
       "timeZoneID",
       system::TimezoneSettings::GetInstance()->GetCurrentTimezoneID());
 
-  if (!IsTimezoneAutomaticDetectionUserEditable()) {
-    html_source->AddBoolean("timeZoneAutoDetectValueFromPolicy",
-                            g_browser_process->platform_part()
-                                ->GetTimezoneResolverManager()
-                                ->ShouldApplyResolvedTimezone());
-  }
-
   return new DateTimeHandler;
 }
 
