87f82b8ddb917066b52677d0d6d774b9ff733b9d
Pengcheng <pengcheng@google.com>
2018-01-18 01:57:17 +0000

    Do not create TaskScheduler/NetworkChangeNotifierFactory if it exists.
    We have an android project which want to embed webview (not system one) and
    cronet (native impl) together into one android app, but now webview and cronet
    are conflicts with each other, because both initialize TaskScheduler and
    NetworkChangeNotifierFactory, which will crash the app.
    
    This cl adds interface to get current NetworkChangeNotifierFactory instance,
    and ignore creating one if it exists; and for TaskScheduler, adds a new
    interface to ContentBrowserClient to control create it or not at
    browser_main_loop.cc, therefore we can disable it but initialize it at our own
    entry point.
    
    Bug: 801077
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I5fe5323677536f948847f6d7d56d132d42abff0d
    Reviewed-on: https://chromium-review.googlesource.com/818924
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    Commit-Queue: Pengcheng Xu <pengcheng@google.com>
    Cr-Commit-Position: refs/heads/master@{#530011}

---
+++ android_webview/browser/test/aw_content_browser_client_unittest.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "android_webview/browser/aw_content_browser_client.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace android_webview {
+
+class AwContentBrowserClientTest : public testing::Test {};
+
+TEST_F(AwContentBrowserClientTest, DisableCreatingTaskScheduler) {
+  AwContentBrowserClient client;
+  EXPECT_TRUE(client.ShouldCreateTaskScheduler());
+
+  AwContentBrowserClient::DisableCreatingTaskScheduler();
+  EXPECT_FALSE(client.ShouldCreateTaskScheduler());
+}
+
+}  // namespace android_webview
