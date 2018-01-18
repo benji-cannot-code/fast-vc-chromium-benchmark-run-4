2c44948f735b4b119f393ac14b332b94088b2396
Ria Jiang <riajiang@chromium.org>
2018-01-18 20:37:33 +0000

    Change to use gesture-target delta for touchpad/screen gesture events.
    For touchpad/touchscreen gesture events after begin/start phase, we used
    to use touchpad_gesture_target_.delta and
    touchscreen_gesture_target_.delta (stored from begin/start phase event)
    to transform the event location. In [1] and [2], when we changed them to
    do async targeting, we changed them to always transform events using
    TransformPointToCoordSpaceForView, which caused some crashes. Before we
    fix it properly (details in the bug), changing back to use the delta
    scheme to make sure async targeting works correctly before the branch
    cut.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/855198
    [2] https://chromium-review.googlesource.com/c/chromium/src/+/857417
    
    Bug: 802190
    Change-Id: Iac0e47a610d91ab78d3732413f2e4b81c9ef2cb6
    Reviewed-on: https://chromium-review.googlesource.com/872395
    Commit-Queue: Ria Jiang <riajiang@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: James MacLean <wjmaclean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530255}

--- content/browser/renderer_host/render_widget_host_input_event_router.h
+++ content/browser/renderer_host/render_widget_host_input_event_router.h
@@ -159,7 +159,8 @@ class CONTENT_EXPORT RenderWidgetHostInputEventRouter
       RenderWidgetHostViewBase* root_view,
       RenderWidgetHostViewBase* target,
       const blink::WebGestureEvent& touchpad_gesture_event,
-      const ui::LatencyInfo& latency);
+      const ui::LatencyInfo& latency,
+      const base::Optional<gfx::PointF>& target_location);
 
   // MouseMove/Enter/Leave events might need to be processed by multiple frames
   // in different processes for MouseEnter and MouseLeave event handlers to
@@ -217,7 +218,8 @@ class CONTENT_EXPORT RenderWidgetHostInputEventRouter
       RenderWidgetHostViewBase* root_view,
       RenderWidgetHostViewBase* target,
       const blink::WebGestureEvent& gesture_event,
-      const ui::LatencyInfo& latency);
+      const ui::LatencyInfo& latency,
+      const base::Optional<gfx::PointF>& target_location);
 
   // RenderWidgetTargeter::Delegate:
   RenderWidgetTargetResult FindTargetSynchronously(
