76f458516cd138168deea1be81019724beff8d74
Sadrul Habib Chowdhury <sadrul@chromium.org>
2018-01-18 01:45:54 +0000

    tab_switching: Add tracing to measure tab-switching latency.
    Add tracing data for tab-switching latency. Include that tracing data in
    the tab_switching telemetry report as 'tab_switch_to_paint'. The current
    data collected from histogram is also included in the report.
    
    BUG=802106
    
    Change-Id: I2e5fbb50ee36b2d93f66637dd1601865cbd1553d
    Reviewed-on: https://chromium-review.googlesource.com/826184
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530007}

--- ui/latency/latency_tracker.cc
+++ ui/latency/latency_tracker.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/rand_util.h"
+#include "base/trace_event/trace_event.h"
 #include "services/metrics/public/cpp/ukm_entry_builder.h"
 #include "services/metrics/public/cpp/ukm_recorder.h"
 #include "ui/latency/latency_histogram_macros.h"
@@ -84,6 +85,8 @@ void LatencyTracker::OnGpuSwapBuffersCompleted(const LatencyInfo& latency) {
         gpu_swap_end_component.event_time - tab_switch_component.event_time;
     for (size_t i = 0; i < tab_switch_component.event_count; i++) {
       UMA_HISTOGRAM_TIMES("MPArch.RWH_TabSwitchPaintDuration", delta);
+      TRACE_EVENT_ASYNC_END0("latency", "TabSwitching::Latency",
+                             latency.trace_id());
     }
   }
 
