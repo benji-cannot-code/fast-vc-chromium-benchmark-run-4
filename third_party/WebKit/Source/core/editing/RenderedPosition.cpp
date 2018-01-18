969bf46df2cea2a3d0015ac9a6af2fb5e31c0fca
Yoichi Osato <yoichio@chromium.org>
2018-01-18 02:39:05 +0000

    Refactor RenderPosition::PositionInGraphicsLayerBacking #2
    This patch moves CompositedSelectionBound.hidden assignment from
    LocalFrameView to PositionInGraphicsLayerBacking() for code health.
    
    Bug: 789870
    Change-Id: I007aee8b5e6acc78c57127feb4191b94d2789852
    Reviewed-on: https://chromium-review.googlesource.com/869697
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530025}

--- third_party/WebKit/Source/core/editing/RenderedPosition.cpp
+++ third_party/WebKit/Source/core/editing/RenderedPosition.cpp
@@ -342,6 +342,7 @@ CompositedSelectionBound RenderedPosition::PositionInGraphicsLayerBacking(
   bound.edge_bottom_in_layer =
       LocalToInvalidationBackingPoint(edge_bottom_in_layer);
   bound.layer = GetGraphicsLayerBacking(*layout_object_);
+  bound.hidden = !IsVisible(selection_start);
   return bound;
 }
 
@@ -357,7 +358,7 @@ LayoutPoint RenderedPosition::GetSamplePointForVisibility(
   return sample_point;
 }
 
-bool RenderedPosition::IsVisible(bool selection_start) {
+bool RenderedPosition::IsVisible(bool selection_start) const {
   if (IsNull())
     return false;
 
