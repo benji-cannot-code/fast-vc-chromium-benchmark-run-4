5647caec8ba76b5ed027069fcc7170cca6e2d788
Jun Cai <juncai@chromium.org>
2018-01-18 19:24:54 +0000

    NetworkService: Add X509Certificate::CreateFromPickleUnsafeOptions
    This CL is from the discussion:
    https://chromium-review.googlesource.com/c/chromium/src/+/848303/12
    
    This CL adds a static function:
    X509Certificate::CreateFromPickleUnsafeOptions() so that
    ParamTraits<scoped_refptr<net::X509Certificate>>::Read() can use it to
    deal with issues with client certs that depend on the
    printable_string_is_utf8 hack (See https://crbug.com/770323 and
    https://crbug.com/788655).
    
    Bug: 783996
    Change-Id: I9008e8640de76cee8a078f06f1082034256cf75a
    Reviewed-on: https://chromium-review.googlesource.com/871526
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Jun Cai <juncai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530225}

--- net/cert/x509_certificate.cc
+++ net/cert/x509_certificate.cc
@@ -199,6 +199,14 @@ scoped_refptr<X509Certificate> X509Certificate::CreateFromBufferUnsafeOptions(
 // static
 scoped_refptr<X509Certificate> X509Certificate::CreateFromDERCertChain(
     const std::vector<base::StringPiece>& der_certs) {
+  return CreateFromDERCertChainUnsafeOptions(der_certs, {});
+}
+
+// static
+scoped_refptr<X509Certificate>
+X509Certificate::CreateFromDERCertChainUnsafeOptions(
+    const std::vector<base::StringPiece>& der_certs,
+    UnsafeCreateOptions options) {
   TRACE_EVENT0("io", "X509Certificate::CreateFromDERCertChain");
   if (der_certs.empty())
     return nullptr;
@@ -222,7 +230,8 @@ scoped_refptr<X509Certificate> X509Certificate::CreateFromDERCertChain(
   if (!handle)
     return nullptr;
 
-  return CreateFromBuffer(std::move(handle), std::move(intermediate_ca_certs));
+  return CreateFromBufferUnsafeOptions(
+      std::move(handle), std::move(intermediate_ca_certs), options);
 }
 
 // static
@@ -250,6 +259,13 @@ scoped_refptr<X509Certificate> X509Certificate::CreateFromBytesUnsafeOptions(
 // static
 scoped_refptr<X509Certificate> X509Certificate::CreateFromPickle(
     base::PickleIterator* pickle_iter) {
+  return CreateFromPickleUnsafeOptions(pickle_iter, {});
+}
+
+// static
+scoped_refptr<X509Certificate> X509Certificate::CreateFromPickleUnsafeOptions(
+    base::PickleIterator* pickle_iter,
+    UnsafeCreateOptions options) {
   int chain_length = 0;
   if (!pickle_iter->ReadLength(&chain_length))
     return nullptr;
@@ -262,7 +278,7 @@ scoped_refptr<X509Certificate> X509Certificate::CreateFromPickle(
       return nullptr;
     cert_chain.push_back(base::StringPiece(data, data_length));
   }
-  return CreateFromDERCertChain(cert_chain);
+  return CreateFromDERCertChainUnsafeOptions(cert_chain, options);
 }
 
 // static
