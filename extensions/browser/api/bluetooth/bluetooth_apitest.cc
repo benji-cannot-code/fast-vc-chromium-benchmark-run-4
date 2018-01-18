beff556dc38b314af932b97349c427b38f7c248e
Devlin Cronin <rdevlin.cronin@chromium.org>
2018-01-18 02:10:37 +0000

    [Extensions] Disallow ExtensionService includes from //extensions
    //extensions should not depend on //chrome at all. Currently, there are
    exceptions for tests, but these shouldn't really be allowed either.
    Remove extension_service.h from the list of test-allowed includes, and
    move the runtime_apitest to the chrome layer. This test fundamentally
    relies on Chrome concepts, so doesn't belong at the //extensions layer.
    
    Bug: 773004
    Change-Id: I33933ef42cc1ec1e456adb557cb83a26292d6564
    Reviewed-on: https://chromium-review.googlesource.com/857259
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530014}

--- extensions/browser/api/bluetooth/bluetooth_apitest.cc
+++ extensions/browser/api/bluetooth/bluetooth_apitest.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/browser/extensions/extension_function_test_utils.h"
-#include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "device/bluetooth/bluetooth_adapter.h"
--- extensions/browser/api/cast_channel/cast_channel_apitest.cc
+++ extensions/browser/api/cast_channel/cast_channel_apitest.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/browser/extensions/extension_function_test_utils.h"
-#include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/ui/browser.h"
 #include "components/cast_channel/cast_socket.h"
 #include "components/cast_channel/cast_socket_service.h"
