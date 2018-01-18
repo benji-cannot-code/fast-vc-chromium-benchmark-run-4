414a27f52491bcdc97edb720f8137144063a7d4d
rhalavati@chromium.org <rhalavati@google.com>
2018-01-18 07:34:30 +0000

    Add traffic annotation to DNSTransaction queries.
    To add network traffic annotation to query commands of DNSTransaction,
    Network traffic annotation is added to DNSQuery class so that the
    required annotations come from the origin of the query.
    
    Bug: 656607
    Change-Id: I91403724948e896398b13ed310fe74665a5431fb
    Reviewed-on: https://chromium-review.googlesource.com/824045
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Eric Roman <eroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530084}

--- net/dns/dns_transaction.cc
+++ net/dns/dns_transaction.cc
@@ -44,10 +44,38 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/log/net_log_with_source.h"
 #include "net/socket/datagram_client_socket.h"
 #include "net/socket/stream_socket.h"
+#include "net/traffic_annotation/network_traffic_annotation.h"
 
 namespace net {
 
 namespace {
+
+constexpr net::NetworkTrafficAnnotationTag kTrafficAnnotation =
+    net::DefineNetworkTrafficAnnotation("dns_transaction", R"(
+        semantics {
+          sender: "DNS Transaction"
+          description:
+            "DNS Transaction implements a stub DNS resolver as defined in RFC "
+            "1034."
+          trigger:
+            "Any network request that may require DNS resolution, including "
+            "navigations, connecting to a proxy server, detecting proxy "
+            "settings, getting proxy config, certificate checking, and more."
+          data:
+            "Domain name that needs resolution."
+          destination: OTHER
+          destination_other:
+            "The connection is made to a DNS server based on user's network "
+            "settings."
+        }
+        policy {
+          cookies_allowed: NO
+          setting:
+            "This feature cannot be disabled. Without DNS Transactions Chrome "
+            "cannot resolve host names."
+          policy_exception_justification:
+            "Essential for Chrome's navigation."
+        })");
 
 // Count labels in the fully-qualified name in DNS format.
 int CountLabels(const std::string& name) {
@@ -248,10 +276,10 @@ class DnsUDPAttempt : public DnsAttempt {
 
   int DoSendQuery() {
     next_state_ = STATE_SEND_QUERY_COMPLETE;
-    return socket()->Write(query_->io_buffer(),
-                           query_->io_buffer()->size(),
-                           base::Bind(&DnsUDPAttempt::OnIOComplete,
-                                      base::Unretained(this)));
+    return socket()->Write(
+        query_->io_buffer(), query_->io_buffer()->size(),
+        base::Bind(&DnsUDPAttempt::OnIOComplete, base::Unretained(this)),
+        kTrafficAnnotation);
   }
 
   int DoSendQueryComplete(int rv) {
@@ -444,9 +472,9 @@ class DnsTCPAttempt : public DnsAttempt {
     if (buffer_->BytesRemaining() > 0) {
       next_state_ = STATE_SEND_LENGTH;
       return socket_->Write(
-          buffer_.get(),
-          buffer_->BytesRemaining(),
-          base::Bind(&DnsTCPAttempt::OnIOComplete, base::Unretained(this)));
+          buffer_.get(), buffer_->BytesRemaining(),
+          base::Bind(&DnsTCPAttempt::OnIOComplete, base::Unretained(this)),
+          kTrafficAnnotation);
     }
     buffer_ = new DrainableIOBuffer(query_->io_buffer(),
                                     query_->io_buffer()->size());
@@ -463,9 +491,9 @@ class DnsTCPAttempt : public DnsAttempt {
     if (buffer_->BytesRemaining() > 0) {
       next_state_ = STATE_SEND_QUERY;
       return socket_->Write(
-          buffer_.get(),
-          buffer_->BytesRemaining(),
-          base::Bind(&DnsTCPAttempt::OnIOComplete, base::Unretained(this)));
+          buffer_.get(), buffer_->BytesRemaining(),
+          base::Bind(&DnsTCPAttempt::OnIOComplete, base::Unretained(this)),
+          kTrafficAnnotation);
     }
     buffer_ =
         new DrainableIOBuffer(length_buffer_.get(), length_buffer_->size());
