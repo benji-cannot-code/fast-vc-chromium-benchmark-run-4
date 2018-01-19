b74148df28ce6eeba4a8ab510db5503b128681e2
Troy Hildebrandt <thildebr@chromium.org>
2018-01-19 22:47:10 +0000

    Fix a bunch of Android chromium/chrome/browser test build warnings.
    Fixes CatchFail warnings by throwing AssertionErrors with the original
    exception instead of calling Assert.fail with a message and no stack
    trace.
    
    A couple tests were using assert instead of the appropriate junit.Assert
    method, and causing a UseCorrectAssertInTests warning. These were fixed.
    
    Change-Id: I72089ab76197b048baa9cb319f8269613a4674fe
    Reviewed-on: https://chromium-review.googlesource.com/876982
    Reviewed-by: Maria Khomenko <mariakhomenko@chromium.org>
    Commit-Queue: Troy Hildebrandt <thildebr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530642}

--- chrome/android/javatests/src/org/chromium/chrome/browser/TabsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/TabsTest.java
@@ -1635,7 +1635,7 @@ public class TabsTest {
             try {
                 selectCallback.waitForCallback(0);
             } catch (TimeoutException e) {
-                Assert.fail("Tab selected event was never received");
+                throw new AssertionError("Tab selected event was never received", e);
             }
             ThreadUtils.runOnUiThreadBlocking(() -> {
                 TabModelSelector selector = mActivityTestRule.getActivity().getTabModelSelector();
@@ -1985,7 +1985,8 @@ public class TabsTest {
             try {
                 pageLoadedCallbacks[i].waitForCallback(0);
             } catch (TimeoutException e) {
-                Assert.fail("PAGE_LOAD_FINISHED was not received for tabId=" + tabIds[i]);
+                throw new AssertionError(
+                        "PAGE_LOAD_FINISHED was not received for tabId=" + tabIds[i], e);
             }
         }
     }
