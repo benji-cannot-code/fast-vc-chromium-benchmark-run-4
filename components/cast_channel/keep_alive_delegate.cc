7a0e60030dcec5f216caaf1d9df95a5598e906e3
Ramin Halavati <rhalavati@chromium.org>
2018-01-19 10:12:38 +0000

    Add network traffic annotation to Cast Channel.
    Adds network traffic annotation tag to the socket writes of the
    following files:
     - components/cast_channel/cast_socket.cc
     - components/cast_channel/keep_alive_delegate.cc
    
    Bug: 656607
    Change-Id: Ie504981e612e6eaa913095fa55da77dd23a9d285
    Reviewed-on: https://chromium-review.googlesource.com/873411
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Derek Cheng <imcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530466}

--- components/cast_channel/keep_alive_delegate.cc
+++ components/cast_channel/keep_alive_delegate.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/cast_channel/logger.h"
 #include "components/cast_channel/proto/cast_channel.pb.h"
 #include "net/base/net_errors.h"
+#include "net/traffic_annotation/network_traffic_annotation.h"
 
 namespace cast_channel {
 
@@ -86,12 +87,34 @@ void KeepAliveDelegate::SendKeepAliveMessage(const CastMessage& message,
                                              CastMessageType message_type) {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
   DVLOG(2) << "Sending " << CastMessageTypeToString(message_type);
-  // TODO(https://crbug.com/656607): Add proper annotation.
+
+  net::NetworkTrafficAnnotationTag traffic_annotation =
+      net::DefineNetworkTrafficAnnotation("cast_keep_alive_delegate", R"(
+        semantics {
+          sender: "Cast Socket Keep Alive Delegate"
+          description:
+            "A ping/pong message sent periodically to a Cast device to keep "
+            "the connection alive."
+          trigger:
+            "Periodically while a connection to a Cast device is established."
+          data:
+            "A protobuf message representing a ping/pong message. No user data."
+          destination: OTHER
+          destination_other:
+            "Data will be sent to a Cast device in local network."
+        }
+        policy {
+          cookies_allowed: NO
+          setting:
+            "This request cannot be disabled, but it would not be sent if user "
+            "does not connect to a Cast device."
+          policy_exception_justification: "Not implemented."
+        })");
   socket_->transport()->SendMessage(
       message,
       base::Bind(&KeepAliveDelegate::SendKeepAliveMessageComplete,
                  base::Unretained(this), message_type),
-      NO_TRAFFIC_ANNOTATION_BUG_656607);
+      traffic_annotation);
 }
 
 void KeepAliveDelegate::SendKeepAliveMessageComplete(
