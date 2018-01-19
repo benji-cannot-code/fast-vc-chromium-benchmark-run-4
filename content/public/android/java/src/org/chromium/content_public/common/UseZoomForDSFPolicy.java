76b5544ffbf2ab2d563cc27f16fd0805085bcd35
Jaebaek Seo <jaebaek@chromium.org>
2018-01-19 06:56:52 +0000

    Adjust AndroidWebview smooth scroll and scrollto delta
    LocalDOMWindow::scrollX() must return a CSS pixel scaled value, but it returns
    a wrong value when enabling --use-zoom-for-dsf. Since LocalDOMWindow::scrollX()
    returns viewport_x / PageZoomFactor() and PageZoomFactor() is DSF when
    --use-zoom-for-dsf is enabled, viewport_x must be in physical pixel scale.
    root_offset given to SynchronousCompositor::DidChangeRootLayerScrollOffset() as
    an argument will be the value of viewport_x. It must be in physical pixel scale
    if --use-zoom-for-dsf is enabled (otherwise, it must be in DIP scale). This CL
    lets BrowserViewRenderer pass physical pixel scaled value to
    SynchronousCompositor::DidChangeRootLayerScrollOffset() when --use-zoom-for-dsf
    is enabled.
    
    Bug: 737777
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I36c3593633a90cb55c6e9eb7a48d4c406694c751
    Reviewed-on: https://chromium-review.googlesource.com/844266
    Commit-Queue: Jaebaek Seo <jaebaek@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530444}

---
+++ content/public/android/java/src/org/chromium/content_public/common/UseZoomForDSFPolicy.java
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.content_public.common;
+
+import org.chromium.base.annotations.JNINamespace;
+
+/**
+ * This is a utility class to wrap use_zoom_for_dsf_policy.cc.
+ */
+@JNINamespace("content")
+public final class UseZoomForDSFPolicy {
+    public static boolean isUseZoomForDSFEnabled() {
+        return nativeIsUseZoomForDSFEnabled();
+    }
+
+    private static native boolean nativeIsUseZoomForDSFEnabled();
+
+    // Do not instantiate this class.
+    private UseZoomForDSFPolicy() {}
+}
