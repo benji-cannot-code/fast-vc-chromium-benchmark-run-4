c64ffde365f652c3c36b0d29943b247df2afbbcd
Maajid <maajid@chromium.org>
2018-01-19 04:46:54 +0000

    Remove RenameCryptohome from HomedirMethods.
    Bug: 741274
    Change-Id: Ib6a62720c7f3e89a7d480a63c4bc4a470d75aed9
    Reviewed-on: https://chromium-review.googlesource.com/861368
    Commit-Queue: Maajid <maajid@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530423}

--- chromeos/cryptohome/cryptohome_util.cc
+++ chromeos/cryptohome/cryptohome_util.cc
@@ -18,23 +18,37 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace cryptohome {
 
-MountError BaseReplyToMountError(const base::Optional<BaseReply>& reply) {
+namespace {
+
+bool IsEmpty(const base::Optional<BaseReply>& reply) {
   if (!reply.has_value()) {
-    LOGIN_LOG(ERROR) << "MountEx failed with no reply.";
-    return MOUNT_ERROR_FATAL;
+    LOGIN_LOG(ERROR) << "Cryptohome call failed with empty reply.";
+    return true;
   }
+  return false;
+}
+
+}  // namespace
+
+MountError MountExReplyToMountError(const base::Optional<BaseReply>& reply) {
+  if (IsEmpty(reply))
+    return MOUNT_ERROR_FATAL;
+
   if (!reply->HasExtension(MountReply::reply)) {
     LOGIN_LOG(ERROR) << "MountEx failed with no MountReply extension in reply.";
     return MOUNT_ERROR_FATAL;
   }
-  if (reply->has_error() && reply->error() != CRYPTOHOME_ERROR_NOT_SET) {
-    LOGIN_LOG(ERROR) << "MountEx failed (CryptohomeErrorCode): "
-                     << reply->error();
-  }
   return CryptohomeErrorToMountError(reply->error());
 }
 
-const std::string& BaseReplyToMountHash(const BaseReply& reply) {
+MountError BaseReplyToMountError(const base::Optional<BaseReply>& reply) {
+  if (IsEmpty(reply))
+    return MOUNT_ERROR_FATAL;
+
+  return CryptohomeErrorToMountError(reply->error());
+}
+
+const std::string& MountExReplyToMountHash(const BaseReply& reply) {
   return reply.GetExtension(MountReply::reply).sanitized_username();
 }
 
