b908806966d90addafe21195b9d7fe448ae610cc
Mounir Lamouri <mlamouri@chromium.org>
2018-01-19 10:34:30 +0000

    Media Engagement: add metrics for time to load and time to lookup.
    Bug: 800758
    Change-Id: Iea3614bd17563ef0106be267e5f597a79c23bec0
    Reviewed-on: https://chromium-review.googlesource.com/874690
    Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530477}

--- chrome/browser/media/media_engagement_preloaded_list.cc
+++ chrome/browser/media/media_engagement_preloaded_list.cc
@@ -22,6 +22,12 @@ const char MediaEngagementPreloadedList::kHistogramCheckResultName[] =
 const char MediaEngagementPreloadedList::kHistogramLoadResultName[] =
     "Media.Engagement.PreloadedList.LoadResult";
 
+const char MediaEngagementPreloadedList::kHistogramLoadTimeName[] =
+    "Media.Engagement.PreloadedList.LoadTime";
+
+const char MediaEngagementPreloadedList::kHistogramLookupTimeName[] =
+    "Media.Engagement.PreloadedList.LookupTime";
+
 // static
 MediaEngagementPreloadedList* MediaEngagementPreloadedList::GetInstance() {
   CR_DEFINE_STATIC_LOCAL(MediaEngagementPreloadedList, instance, ());
@@ -48,6 +54,7 @@ bool MediaEngagementPreloadedList::empty() const {
 }
 
 bool MediaEngagementPreloadedList::LoadFromFile(const base::FilePath& path) {
+  SCOPED_UMA_HISTOGRAM_TIMER(kHistogramLoadTimeName);
   DETACH_FROM_SEQUENCE(sequence_checker_);
 
   // Check the file exists.
@@ -82,6 +89,7 @@ bool MediaEngagementPreloadedList::LoadFromFile(const base::FilePath& path) {
 
 bool MediaEngagementPreloadedList::CheckOriginIsPresent(
     const url::Origin& origin) const {
+  SCOPED_UMA_HISTOGRAM_TIMER(kHistogramLookupTimeName);
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   // Check if we have loaded the data.
