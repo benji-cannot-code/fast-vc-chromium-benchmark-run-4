ba1bea875b86236b2fce764ee7ead69b9b295134
Troy Hildebrandt <thildebr@chromium.org>
2018-01-19 23:00:14 +0000

    Fix CatchFail build warning for WarmupManagerTest.
    Removes the try/catch block that leads to an Assert.fail in favor of
    using a Callable vs. Runnable to let the exception be thrown as is.
    
    Change-Id: I9987ebfa93354d05239753800cf58df4bb1b6560
    Reviewed-on: https://chromium-review.googlesource.com/876948
    Reviewed-by: Maria Khomenko <mariakhomenko@chromium.org>
    Commit-Queue: Troy Hildebrandt <thildebr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530647}

--- chrome/android/javatests/src/org/chromium/chrome/browser/WarmupManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/WarmupManagerTest.java
@@ -36,6 +36,7 @@ import org.chromium.content_public.browser.WebContents;
 import org.chromium.content_public.browser.WebContentsObserver;
 import org.chromium.net.test.EmbeddedTestServer;
 
+import java.util.concurrent.Callable;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -56,13 +57,13 @@ public class WarmupManagerTest {
         mContext = InstrumentationRegistry.getInstrumentation()
                            .getTargetContext()
                            .getApplicationContext();
-        ThreadUtils.runOnUiThreadBlocking(() -> {
-            try {
+        ThreadUtils.runOnUiThreadBlocking(new Callable<Void>() {
+            @Override
+            public Void call() throws Exception {
                 ChromeBrowserInitializer.getInstance(mContext).handleSynchronousStartup();
-            } catch (Exception e) {
-                Assert.fail();
+                mWarmupManager = WarmupManager.getInstance();
+                return null;
             }
-            mWarmupManager = WarmupManager.getInstance();
         });
     }
 
