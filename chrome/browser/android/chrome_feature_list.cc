cdc0ae75e04a952c5b57cc47e53bc3e748a677ee
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2018-01-19 06:29:20 +0000

    Add Finch flag for Sole integration
    Sole integration needs to know whether it's enabled or not before
    native is loaded, so it's cached in shared preference, and requires
    restarting twice after the flag is modified.
    
    Bug: 778805
    Change-Id: I5885b2fb44990d6bb0cdcbba4cc382772c5732be
    Reviewed-on: https://chromium-review.googlesource.com/875322
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530437}

--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -116,6 +116,7 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &kReaderModeInCCT,
     &kSearchEnginePromoExistingDevice,
     &kSearchEnginePromoNewDevice,
+    &kSoleIntegration,
     &kSpannableInlineAutocomplete,
     &kSpecialLocaleFeature,
     &kSpecialLocaleWrapper,
@@ -346,6 +347,9 @@ const base::Feature kPwaPersistentNotification{
     "PwaPersistentNotification", base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kReaderModeInCCT{"ReaderModeInCCT",
+                                     base::FEATURE_DISABLED_BY_DEFAULT};
+
+const base::Feature kSoleIntegration{"SoleIntegration",
                                      base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kSpannableInlineAutocomplete{
