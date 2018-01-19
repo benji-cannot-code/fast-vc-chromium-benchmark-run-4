f13e22617bcaa72161b15f434e88b2f023d19e12
Findit <findit-for-me@appspot.gserviceaccount.com>
2018-01-19 00:20:19 +0000

    Revert "cros: Manual revert rename TabletPowerButtonController to ConvertiblePowerButtonController and create PowerButtonUtil."
    This reverts commit 790d637dd981a052acc1a3f43ac549a02643b03a.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 530295 as the
    culprit for failures in the build cycles as shown on:
    https://findit-for-me.appspot.com/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtLzc5MGQ2MzdkZDk4MWEwNTJhY2MxYTNmNDNhYzU0OWEwMjY0M2IwM2EM
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium.chromiumos/linux-chromeos-dbg/3814
    
    Original change's description:
    > cros: Manual revert rename TabletPowerButtonController to
    > ConvertiblePowerButtonController and create PowerButtonUtil.
    > 
    > changes in this cl,
    > Since we will put the logic of power button menu in
    > TabletPowerButtonController instead of creating a new controller.
    > Reverting related previous cls,
    > 1. Revert rename TabletPowerButtonController to ConvertiblePowerButtonController.
    >    Related cl,
    >    https://chromium-review.googlesource.com/c/chromium/src/+/852518
    > 2. Revert the creating of PowerButtonUtil.
    >    Related cl,
    >    https://chromium-review.googlesource.com/c/chromium/src/+/852905
    > 
    > Bug: 799524
    > Change-Id: I4883c7eb9f4b73fad9e8d862fbc106aee22072d4
    > Reviewed-on: https://chromium-review.googlesource.com/874911
    > Reviewed-by: Dan Erat <derat@chromium.org>
    > Commit-Queue: min c <minch@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#530295}
    
    Change-Id: I973eeb163c50b1eab4ace70f0171d5f3f948a16d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 799524
    Reviewed-on: https://chromium-review.googlesource.com/874952
    Cr-Commit-Position: refs/heads/master@{#530352}

--- ash/system/power/convertible_power_button_controller_test_api.cc
+++ ash/system/power/convertible_power_button_controller_test_api.cc
@@ -3,25 +3,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "ash/system/power/tablet_power_button_controller_test_api.h"
+#include "ash/system/power/convertible_power_button_controller_test_api.h"
 
+#include "ash/system/power/convertible_power_button_controller.h"
 #include "ash/system/power/power_button_display_controller.h"
-#include "ash/system/power/tablet_power_button_controller.h"
 
 namespace ash {
 
-TabletPowerButtonControllerTestApi::TabletPowerButtonControllerTestApi(
-    TabletPowerButtonController* controller)
+ConvertiblePowerButtonControllerTestApi::
+    ConvertiblePowerButtonControllerTestApi(
+        ConvertiblePowerButtonController* controller)
     : controller_(controller) {}
 
-TabletPowerButtonControllerTestApi::~TabletPowerButtonControllerTestApi() =
-    default;
+ConvertiblePowerButtonControllerTestApi::
+    ~ConvertiblePowerButtonControllerTestApi() = default;
 
-bool TabletPowerButtonControllerTestApi::ShutdownTimerIsRunning() const {
+bool ConvertiblePowerButtonControllerTestApi::ShutdownTimerIsRunning() const {
   return controller_->shutdown_timer_.IsRunning();
 }
 
-bool TabletPowerButtonControllerTestApi::TriggerShutdownTimeout() {
+bool ConvertiblePowerButtonControllerTestApi::TriggerShutdownTimeout() {
   if (!controller_->shutdown_timer_.IsRunning())
     return false;
 
@@ -31,7 +32,8 @@ bool TabletPowerButtonControllerTestApi::TriggerShutdownTimeout() {
   return true;
 }
 
-void TabletPowerButtonControllerTestApi::SendKeyEvent(ui::KeyEvent* event) {
+void ConvertiblePowerButtonControllerTestApi::SendKeyEvent(
+    ui::KeyEvent* event) {
   controller_->display_controller_->OnKeyEvent(event);
 }
 
