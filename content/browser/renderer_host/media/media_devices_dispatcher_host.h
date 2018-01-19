492d2a2c8842165ba96f1699c36039a16ba955a5
Chandan Padhi <c.padhi@samsung.com>
2018-01-19 17:12:42 +0000

    Move MediaDeviceSaltAndOriginCallback to MediaDevicesManager
    This CL is in preparation for device change event migration to blink.
    This change will allow MediaDevicesManager to translate enumeration
    results before sending them to the renderers.
    
    Bug: 793297
    Change-Id: I30f3533efb7c7eac8b7db5ab97fb6b155b28d99e
    Reviewed-on: https://chromium-review.googlesource.com/869638
    Commit-Queue: Chandan Padhi <c.padhi@samsung.com>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530544}

--- content/browser/renderer_host/media/media_devices_dispatcher_host.h
+++ content/browser/renderer_host/media/media_devices_dispatcher_host.h
@@ -66,34 +66,9 @@ class CONTENT_EXPORT MediaDevicesDispatcherHost
   void SetDeviceChangeListenerForTesting(
       blink::mojom::MediaDevicesListenerPtr listener);
 
-  void set_salt_and_origin_callback_for_testing(
-      MediaDeviceSaltAndOriginCallback callback) {
-    salt_and_origin_callback_ = std::move(callback);
-  }
-
  private:
   using GetVideoInputDeviceFormatsCallback =
       GetAllVideoInputDeviceFormatsCallback;
-
-  void CheckPermissionsForEnumerateDevices(
-      const MediaDevicesManager::BoolDeviceTypes& requested_types,
-      EnumerateDevicesCallback client_callback,
-      const std::pair<std::string, url::Origin>& salt_and_origin);
-
-  void DoEnumerateDevices(
-      const MediaDevicesManager::BoolDeviceTypes& requested_types,
-      EnumerateDevicesCallback client_callback,
-      std::string device_id_salt,
-      const url::Origin& security_origin,
-      const MediaDevicesManager::BoolDeviceTypes& has_permissions);
-
-  void DevicesEnumerated(
-      const MediaDevicesManager::BoolDeviceTypes& requested_types,
-      EnumerateDevicesCallback client_callback,
-      const std::string& device_id_salt,
-      const url::Origin& security_origin,
-      const MediaDevicesManager::BoolDeviceTypes& has_permissions,
-      const MediaDeviceEnumeration& enumeration);
 
   void GetDefaultVideoInputDeviceID(
       GetVideoInputCapabilitiesCallback client_callback,
@@ -155,8 +130,6 @@ class CONTENT_EXPORT MediaDevicesDispatcherHost
                                     MediaDeviceType type,
                                     const MediaDeviceInfoArray& device_infos);
 
-  std::string ComputeGroupIDSalt(const std::string& device_id_salt);
-
   // The following const fields can be accessed on any thread.
   const int render_process_id_;
   const int render_frame_id_;
@@ -180,9 +153,6 @@ class CONTENT_EXPORT MediaDevicesDispatcherHost
   size_t num_pending_audio_input_parameters_;
   std::vector<blink::mojom::AudioInputDeviceCapabilities>
       current_audio_input_capabilities_;
-
-  // Callback used to obtain the current device ID salt and security origin.
-  MediaDeviceSaltAndOriginCallback salt_and_origin_callback_;
 
   base::WeakPtrFactory<MediaDevicesDispatcherHost> weak_factory_;
 
