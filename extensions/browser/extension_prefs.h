5100162df3f4558ae51e5c3896189cc371307d88
Devlin Cronin <rdevlin.cronin@chromium.org>
2018-01-19 03:07:06 +0000

    [Extensions Cleanup] Kill ExtensionScopedPrefs
    ExtensionScopedPrefs is an object that provides a more limited set of
    methods than ExtensionPrefs. However, it is only used in a small handful
    of places, only has a single implementation (ExtensionPrefs), and
    has no way of retrieving itself as a KeyedService (ExtensionPrefs::Get
    returns an ExtensionPrefs*), which means that it also doesn't reduce any
    set of required dependencies.
    
    Remove the class, and make the few callers that knew about it use
    ExtensionPrefs.
    
    TBR=benwells@chromium.org (c/b/ui/app_list)
    
    Bug: None
    Change-Id: Ifcfc8e657a698e16bb44e898913246ee452e0894
    Reviewed-on: https://chromium-review.googlesource.com/857839
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530410}

--- extensions/browser/extension_prefs.h
+++ extensions/browser/extension_prefs.h
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync/model/string_ordinal.h"
 #include "extensions/browser/blacklist_state.h"
 #include "extensions/browser/disable_reason.h"
-#include "extensions/browser/extension_scoped_prefs.h"
 #include "extensions/browser/install_flag.h"
 #include "extensions/common/constants.h"
 #include "extensions/common/extension.h"
@@ -71,7 +70,7 @@ class URLPatternSet;
 //       preference. Extension-controlled preferences are stored in
 //       PrefValueStore::extension_prefs(), which this class populates and
 //       maintains as the underlying extensions change.
-class ExtensionPrefs : public ExtensionScopedPrefs, public KeyedService {
+class ExtensionPrefs : public KeyedService {
  public:
   using ExtensionsInfo = std::vector<std::unique_ptr<ExtensionInfo>>;
 
@@ -234,35 +233,33 @@ class ExtensionPrefs : public ExtensionScopedPrefs, public KeyedService {
   // Populates |out| with the ids of all installed extensions.
   void GetExtensions(ExtensionIdList* out) const;
 
-  // ExtensionScopedPrefs methods:
   void UpdateExtensionPref(const std::string& id,
                            base::StringPiece key,
-                           std::unique_ptr<base::Value> value) override;
+                           std::unique_ptr<base::Value> value);
 
-  void DeleteExtensionPrefs(const std::string& id) override;
+  void DeleteExtensionPrefs(const std::string& id);
 
   bool ReadPrefAsBoolean(const std::string& extension_id,
                          base::StringPiece pref_key,
-                         bool* out_value) const override;
+                         bool* out_value) const;
 
   bool ReadPrefAsInteger(const std::string& extension_id,
                          base::StringPiece pref_key,
-                         int* out_value) const override;
+                         int* out_value) const;
 
   bool ReadPrefAsString(const std::string& extension_id,
                         base::StringPiece pref_key,
-                        std::string* out_value) const override;
+                        std::string* out_value) const;
 
   bool ReadPrefAsList(const std::string& extension_id,
                       base::StringPiece pref_key,
-                      const base::ListValue** out_value) const override;
+                      const base::ListValue** out_value) const;
 
-  bool ReadPrefAsDictionary(
-      const std::string& extension_id,
-      base::StringPiece pref_key,
-      const base::DictionaryValue** out_value) const override;
+  bool ReadPrefAsDictionary(const std::string& extension_id,
+                            base::StringPiece pref_key,
+                            const base::DictionaryValue** out_value) const;
 
-  bool HasPrefForExtension(const std::string& extension_id) const override;
+  bool HasPrefForExtension(const std::string& extension_id) const;
 
   // Did the extension ask to escalate its permission during an upgrade?
   bool DidExtensionEscalatePermissions(const std::string& id) const;
