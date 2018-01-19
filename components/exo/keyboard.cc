da4e25f61147083d2967cfdced4f3ad5f1f651ef
Yuichiro Hanada <yhanada@chromium.org>
2018-01-19 00:27:13 +0000

    exo: Discard a reserved keyboard shortcut only if it's enabled.
    When seeing a reserved keyboard shortcut, exo display server sends
    keyboard leave/enter event with pressed keys information instead of
    key events.
    
          only one IME is enabled.
    
    Bug: 801088
    Test: Manual. Confirm that Ctrl-Space is sent to Android apps when
    Change-Id: I207434b45690f231601cb6187e35a806f8c69f89
    Reviewed-on: https://chromium-review.googlesource.com/866655
    Reviewed-by: David Reveman <reveman@chromium.org>
    Commit-Queue: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530360}

--- components/exo/keyboard.cc
+++ components/exo/keyboard.cc
@@ -135,6 +135,11 @@ bool IsReservedAccelerator(const ui::KeyEvent* event) {
   return false;
 }
 
+// Returns false if an accelerator is not reserved or it's not enabled.
+bool ProcessAcceleratorIfReserved(Surface* surface, ui::KeyEvent* event) {
+  return IsReservedAccelerator(event) && ProcessAccelerator(surface, event);
+}
+
 }  // namespace
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -219,6 +224,17 @@ void Keyboard::OnKeyEvent(ui::KeyEvent* event) {
       delegate_->OnKeyboardModifiers(modifier_flags_);
   }
 
+  // Process reserved accelerators before sending it to client.
+  if (focus_ && ProcessAcceleratorIfReserved(focus_, event)) {
+    // Discard a key press event if it's a reserved accelerator and it's
+    // enabled.
+    event->SetHandled();
+    // Send leave/enter event instead of key event, so the client can know the
+    // actual state of the keyboard.
+    SetFocus(focus_);
+    return;
+  }
+
   // When IME ate a key event, we use the event only for tracking key states and
   // ignore for further processing. Otherwise it is handled in two places (IME
   // and client) and causes undesired behavior.
@@ -226,7 +242,7 @@ void Keyboard::OnKeyEvent(ui::KeyEvent* event) {
 
   switch (event->type()) {
     case ui::ET_KEY_PRESSED:
-      if (focus_ && !consumed_by_ime && !IsReservedAccelerator(event)) {
+      if (focus_ && !consumed_by_ime && !event->handled()) {
         uint32_t serial =
             delegate_->OnKeyboardKey(event->time_stamp(), event->code(), true);
         if (are_keyboard_key_acks_needed_) {
@@ -239,7 +255,7 @@ void Keyboard::OnKeyEvent(ui::KeyEvent* event) {
       }
       break;
     case ui::ET_KEY_RELEASED:
-      if (focus_ && !consumed_by_ime && !IsReservedAccelerator(event)) {
+      if (focus_ && !consumed_by_ime && !event->handled()) {
         uint32_t serial =
             delegate_->OnKeyboardKey(event->time_stamp(), event->code(), false);
         if (are_keyboard_key_acks_needed_) {
