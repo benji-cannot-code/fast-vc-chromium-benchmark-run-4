ad915546eefbadb0baff96526016cb547fad293c
Daniel Cheng <dcheng@chromium.org>
2018-01-19 00:28:15 +0000

    Add base::NoDestructor<T>.
    This is simlar to base::LazyInstance<T>::Leaky but intended for use in
    function-local scope.
    
    This is intended to be a drop-in replacement for:
    - CR_DEFINE_STATIC_LOCAL(...); in function scope
    - static T* x = new X; in function scope
    - static T& x = *new X; in function scope
    - base::LazyInstance<T> x; in global scope
    and should be preferred in new code.
    
    Change-Id: I75eda47e7dca8205f4086bd298839c261454a3e7
    Reviewed-on: https://chromium-review.googlesource.com/869351
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530361}

--- base/memory/singleton.h
+++ base/memory/singleton.h
@@ -2,8 +2,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
-// PLEASE READ: Do you really need a singleton?
+//
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// PLEASE READ: Do you really need a singleton? If possible, use a
+// function-local static of type base::NoDestructor<T> instead:
+//
+// Factory& Factory::GetInstance() {
+//   static base::NoDestructor<Factory> instance;
+//   return *instance;
+// }
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 //
 // Singletons make it hard to determine the lifetime of an object, which can
 // lead to buggy code and spurious crashes.
