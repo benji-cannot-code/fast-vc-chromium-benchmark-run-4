78136d49be5856293ed161f17032b1fe1d7098e4
Tao Bai <michaelbai@chromium.org>
2018-01-19 23:44:56 +0000

    Keep autofill session alive.
    Cancel the previous autofill session until the new autofill session
    is triggered. We don't cancel the autofill session when WebView
    navigates to the new page now.
    
    This should also improve the detection of form submission.
    
    Bug: 791182
    Change-Id: I883b322f921ef29221995681ad07044d35bfb76d
    Reviewed-on: https://chromium-review.googlesource.com/876507
    Reviewed-by: Changwan Ryu <changwan@chromium.org>
    Commit-Queue: Tao Bai <michaelbai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530660}

--- android_webview/java/src/org/chromium/android_webview/AwAutofillProvider.java
+++ android_webview/java/src/org/chromium/android_webview/AwAutofillProvider.java
@@ -237,7 +237,7 @@ public class AwAutofillProvider extends AutofillProvider {
 
     @Override
     public void autofill(final SparseArray<AutofillValue> values) {
-        if (mNativeAutofillProvider != 0 && mRequest.autofill((values))) {
+        if (mNativeAutofillProvider != 0 && mRequest != null && mRequest.autofill((values))) {
             autofill(mNativeAutofillProvider, mRequest.mFormData);
         }
     }
@@ -367,8 +367,9 @@ public class AwAutofillProvider extends AutofillProvider {
 
     @Override
     protected void reset() {
-        mAutofillManager.cancel();
-        mRequest = null;
+        // We don't need to reset anything here, it should be safe to cancel
+        // current autofill session when new one starts in
+        // startAutofillSession().
     }
 
     @Override
@@ -383,7 +384,7 @@ public class AwAutofillProvider extends AutofillProvider {
         // possible to know which case we're in, so just catch and
         // ignore the exception.
         try {
-            if (mNativeAutofillProvider != 0) reset();
+            if (mNativeAutofillProvider != 0) mRequest = null;
             mNativeAutofillProvider = nativeAutofillProvider;
             if (nativeAutofillProvider == 0) mAutofillManager.destroy();
         } catch (IllegalStateException e) {
@@ -393,7 +394,7 @@ public class AwAutofillProvider extends AutofillProvider {
     @Override
     public void setWebContents(WebContents webContents) {
         if (webContents == mWebContents) return;
-        if (mWebContents != null) reset();
+        if (mWebContents != null) mRequest = null;
         mWebContents = webContents;
     }
 
