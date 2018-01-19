6ee8e8ecf4356b209abe4a49b60cf30b14e01a12
Yoichi Osato <yoichio@chromium.org>
2018-01-19 05:09:11 +0000

    Disable flaky test "RTCRtpSenderTest.ReplaceTrackWithNullTrack"
    TBR: hbos@chromium.org
    Bug: 803597
    Change-Id: I9230416edbd8fc86f4fa47a4d172b17fefb6e540
    Reviewed-on: https://chromium-review.googlesource.com/874988
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530426}

--- content/renderer/media/webrtc/rtc_rtp_sender_unittest.cc
+++ content/renderer/media/webrtc/rtc_rtp_sender_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
 #include "base/single_thread_task_runner.h"
+#include "build/build_config.h"
 #include "content/child/child_process.h"
 #include "content/renderer/media/media_stream_audio_source.h"
 #include "content/renderer/media/webrtc/mock_peer_connection_dependency_factory.h"
@@ -135,7 +136,14 @@ TEST_F(RTCRtpSenderTest, ReplaceTrackSetsTrack) {
   EXPECT_EQ(web_track2.UniqueId(), sender_->Track().UniqueId());
 }
 
-TEST_F(RTCRtpSenderTest, ReplaceTrackWithNullTrack) {
+// This test is flaky on Android and Linux.
+// See crbug.com/803597 for detail.
+#if defined(OS_ANDROID) || defined(OS_LINUX)
+#define MAYBE_ReplaceTrackWithNullTrack DISABLED_ReplaceTrackWithNullTrack
+#else
+#define MAYBE_ReplaceTrackWithNullTrack ReplaceTrackWithNullTrack
+#endif
+TEST_F(RTCRtpSenderTest, MAYBE_ReplaceTrackWithNullTrack) {
   auto web_track = CreateWebTrack("track_id");
   sender_ = CreateSender(web_track);
 
