34abc34452bf06e2e1200535d4059bea49e7b7c9
Andrey Kosyakov <caseq@chromium.org>
2018-01-19 18:26:21 +0000

    DevTools: fix main resource id in ResourceSendRequest trace event
    This was regressed by https://chromium-review.googlesource.com/865939
    
    Change-Id: Id5d0cc587e4ea29c3e705fec1112b4b1301f3e78
    Reviewed-on: https://chromium-review.googlesource.com/875124
    Commit-Queue: Alexei Filippov <alph@chromium.org>
    Reviewed-by: Alexei Filippov <alph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530569}

--- third_party/WebKit/Source/core/inspector/InspectorTraceEvents.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorTraceEvents.cpp
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/wtf/DynamicAnnotations.h"
 #include "platform/wtf/Vector.h"
 #include "platform/wtf/text/TextPosition.h"
+#include "services/network/public/interfaces/request_context_frame_type.mojom-shared.h"
 #include "v8/include/v8-profiler.h"
 #include "v8/include/v8.h"
 
@@ -688,6 +689,8 @@ std::unique_ptr<TracedValue> InspectorSendRequestEvent::Data(
     LocalFrame* frame,
     const ResourceRequest& request) {
   String request_id = IdentifiersFactory::RequestId(loader, identifier);
+  if (request.GetFrameType() != network::mojom::RequestContextFrameType::kNone)
+    request_id = IdentifiersFactory::LoaderId(loader);
 
   std::unique_ptr<TracedValue> value = TracedValue::Create();
   value->SetString("requestId", request_id);
