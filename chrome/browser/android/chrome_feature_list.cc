59730a1697d0d79518630ed6e4a15de558aabc4d
Michael Thiessen <mthiesse@chromium.org>
2018-01-19 20:15:54 +0000

    Reland "Reland "VR: Add dynamically-toggled icon in Daydream Home""
    Original change's description:
    > Revert "Reland "VR: Add dynamically-toggled icon in Daydream Home""
    > 
    > This reverts commit 4c1967840d59d494ab8d55a24e37da79b0a92c52.
    > 
    > Reason for revert: Failed downstream on the x64 Android bots. 'Unused resources'.
    > 
    > Original change's description:
    > > Reland "VR: Add dynamically-toggled icon in Daydream Home"
    > > 
    > > This reverts commit 0c357720eef65cf038f52093a5a20e25b448c8cd.
    > > 
    > > Original change's description:
    > > > Revert "VR: Add dynamically-toggled icon in Daydream Home"
    > > > 
    > > > This reverts commit 559f95d6420338760f285a1b726abffd8a8bcd1c.
    > > > 
    > > > Reason for revert: Fails APK validation because of 'missing Daydream icons'.
    > > > 
    > > > Original change's description:
    > > > > VR: Add dynamically-toggled icon in Daydream Home
    > > > > 
    > > > > This CL adds the ability to show the Chrome icon in Daydream Home, which
    > > > > launches Chrome in VR mode, behind a feature flag.
    > > > > 
    > > > > We're able to do this by enabling/disabling the component that adds the
    > > > > icon at runtime.
    > > > > 
    > > > > Bug: 802080
    > > > > Change-Id: I878202c110d2a7bd092173457de73ce1578d2ff6
    > > > > Reviewed-on: https://chromium-review.googlesource.com/867512
    > > > > Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    > > > > Reviewed-by: Yash Malik <ymalik@chromium.org>
    > > > > Reviewed-by: David Trainor <dtrainor@chromium.org>
    > > > > Cr-Commit-Position: refs/heads/master@{#529842}
    > > > 
    > > > TBR=mthiesse@chromium.org,dtrainor@chromium.org,ymalik@chromium.org
    > > > 
    > > > # Not skipping CQ checks because original CL landed > 1 day ago.
    > > > 
    > > > Bug: 802080
    > > > Change-Id: I9a01dc2dcfaa0e5de07ccc16671721d61beb6feb
    > > > Reviewed-on: https://chromium-review.googlesource.com/874781
    > > > Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    > > > Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    > > > Cr-Commit-Position: refs/heads/master@{#530252}
    > > 
    > > TBR=mthiesse@chromium.org,dtrainor@chromium.org,ymalik@chromium.org
    > > 
    > > Change-Id: Id5738ec9f9e67ef52d57d99af33e450b0af6e0ad
    > > No-Presubmit: true
    > > No-Tree-Checks: true
    > > No-Try: true
    > > Bug: 802080
    > > Reviewed-on: https://chromium-review.googlesource.com/875070
    > > Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    > > Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    > > Reviewed-by: David Trainor <dtrainor@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#530531}
    > 
    > TBR=mthiesse@chromium.org,dtrainor@chromium.org,ymalik@chromium.org
    > 
    > Change-Id: Idcd8f40b0f49c9959619e5cbf6ed0dcee6b615d2
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 802080
    > Reviewed-on: https://chromium-review.googlesource.com/876263
    > Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    > Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#530558}
    
    TBR=mthiesse@chromium.org,dtrainor@chromium.org,ymalik@chromium.org
    Bug: 802080
    
    Change-Id: Ie51c6022bce6adbf0c7f2d39f4755829d88b84b8
    Reviewed-on: https://chromium-review.googlesource.com/876622
    Reviewed-by: agrieve <agrieve@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530598}

--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -125,6 +125,7 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &kVideoPersistence,
     &kVrBrowsingFeedback,
     &kVrBrowsingInCustomTab,
+    &kVrIconInDaydreamHome,
     &kVrLaunchIntents,
     &payments::features::kWebPaymentsMethodSectionOrderV2,
     &payments::features::kWebPaymentsModifiers,
@@ -381,6 +382,9 @@ const base::Feature kVrBrowsingFeedback{"VrBrowsingFeedback",
 
 const base::Feature kVrBrowsingInCustomTab{"VrBrowsingInCustomTab",
                                            base::FEATURE_DISABLED_BY_DEFAULT};
+
+const base::Feature kVrIconInDaydreamHome{"VrIconInDaydreamHome",
+                                          base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kVrLaunchIntents{"VrLaunchIntents",
                                      base::FEATURE_DISABLED_BY_DEFAULT};
