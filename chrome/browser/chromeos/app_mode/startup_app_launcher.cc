b907156bc6d946eb38043470602a06e6602db67c
Toni Barzic <tbarzic@google.com>
2018-01-20 00:50:45 +0000

    Remove NOTIFICATION_KIOSK_APP_LAUNCHED
    Replace it's usages (in tests) with a new KioskAppManagerObserver
    method.
    
    BUG=None
    
    Change-Id: I45b569ed23bc69ac0d4888959769828453b3ed3d
    Reviewed-on: https://chromium-review.googlesource.com/862242
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530699}

--- chrome/browser/chromeos/app_mode/startup_app_launcher.cc
+++ chrome/browser/chromeos/app_mode/startup_app_launcher.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/time/time.h"
 #include "base/values.h"
-#include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/chromeos/app_mode/kiosk_app_manager.h"
 #include "chrome/browser/chromeos/app_mode/kiosk_diagnosis_runner.h"
 #include "chrome/browser/chromeos/app_mode/startup_app_launcher_update_checker.h"
@@ -28,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "components/crx_file/id_util.h"
 #include "components/session_manager/core/session_manager.h"
-#include "content/public/browser/notification_service.h"
 #include "extensions/browser/extension_system.h"
 #include "extensions/common/constants.h"
 #include "extensions/common/extension.h"
@@ -392,11 +390,6 @@ void StartupAppLauncher::LaunchApp() {
 
   KioskAppManager::Get()->InitSession(profile_, app_id_);
   session_manager::SessionManager::Get()->SessionStarted();
-
-  content::NotificationService::current()->Notify(
-      chrome::NOTIFICATION_KIOSK_APP_LAUNCHED,
-      content::NotificationService::AllSources(),
-      content::NotificationService::NoDetails());
 
   if (diagnostic_mode_)
     KioskDiagnosisRunner::Run(profile_, app_id_);
