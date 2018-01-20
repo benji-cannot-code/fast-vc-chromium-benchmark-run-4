73c62c074776c1d91aaffbbbb3556a289ae05bb2
Khushal <khushalsagar@chromium.org>
2018-01-20 00:02:55 +0000

    gpu: Init skia tracing in the GPU process.
    R=piman@chromium.org
    
    Change-Id: If0c213fe5b96ce874908a9ebafd92923c7c56be8
    Reviewed-on: https://chromium-review.googlesource.com/877183
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530672}

--- content/gpu/gpu_child_thread.cc
+++ content/gpu/gpu_child_thread.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/service_manager/public/cpp/binder_registry.h"
 #include "services/service_manager/public/cpp/connector.h"
 #include "services/viz/privileged/interfaces/gl/gpu_service.mojom.h"
+#include "skia/ext/event_tracer_impl.h"
 
 #if defined(USE_OZONE)
 #include "ui/ozone/public/ozone_platform.h"
@@ -211,6 +212,8 @@ void GpuChildThread::Init(const base::Time& process_start_time) {
   GetServiceManagerConnection()->AddConnectionFilter(std::move(filter));
 
   StartServiceManagerConnection();
+
+  InitSkiaEventTracer();
 }
 
 void GpuChildThread::CreateVizMainService(
