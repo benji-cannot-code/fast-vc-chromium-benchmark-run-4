e490863f6820ebb65e6bfc47bc5b098e1bdefb3c
Pavel Feldman <pfeldman@chromium.org>
2018-01-20 04:32:35 +0000

    DevTools: allow fetching iframe owner for given out-of-process target id.
    Bug: 800613
    Change-Id: I57c3cb407d210359b3aa1a1418b920690b10fb1b
    Reviewed-on: https://chromium-review.googlesource.com/874525
    Commit-Queue: Pavel Feldman <pfeldman@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530749}

--- third_party/WebKit/Source/core/inspector/InspectorDOMAgent.h
+++ third_party/WebKit/Source/core/inspector/InspectorDOMAgent.h
@@ -205,6 +205,9 @@ class CORE_EXPORT InspectorDOMAgent final
       protocol::Maybe<bool> pierce,
       std::unique_ptr<protocol::DOM::Node>*) override;
 
+  protocol::Response getFrameOwner(const String& frame_id,
+                                   int* node_id) override;
+
   bool Enabled() const;
   void ReleaseDanglingNodes();
 
