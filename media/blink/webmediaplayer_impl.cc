188f70825c7dce7bd9746284a4e50c9356b75bee
Jennifer Apacible <apacible@chromium.org>
2018-01-20 00:28:51 +0000

    Remove commented out line.
    Change-Id: I2c5b0d9fb800923cc68d49ee3293fe8299edfc12
    Reviewed-on: https://chromium-review.googlesource.com/876684
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: apacible <apacible@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530687}

--- media/blink/webmediaplayer_impl.cc
+++ media/blink/webmediaplayer_impl.cc
@@ -802,10 +802,8 @@ void WebMediaPlayerImpl::SetVolume(double volume) {
 }
 
 void WebMediaPlayerImpl::PictureInPicture() {
-  if (client_) {
+  if (client_)
     client_->PictureInPictureStarted();
-    // client_->MediaRemotingStarted("eh");
-  }
 }
 
 void WebMediaPlayerImpl::SetSinkId(
