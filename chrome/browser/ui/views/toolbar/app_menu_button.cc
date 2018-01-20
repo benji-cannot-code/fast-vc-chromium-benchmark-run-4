061d479c485c586b63b698ccbfd6b772c5002d0c
Sarah Chan <spqchan@chromium.org>
2018-01-20 01:34:22 +0000

    [Views] Add Accessibility Text to App Menu Button
    The App Menu Button AX text currently don't report if an
    update is available. This CL fixes the issue by adding
    "Update is available" to the AX text.
    
    Bug: 770745
    Change-Id: I9e0768d21667c2b986ad7a5bc17c75f37b0bcb4d
    Reviewed-on: https://chromium-review.googlesource.com/869113
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Sarah Chan <spqchan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530720}

--- chrome/browser/ui/views/toolbar/app_menu_button.cc
+++ chrome/browser/ui/views/toolbar/app_menu_button.cc
@@ -27,6 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/toolbar/toolbar_button.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_view.h"
 #include "chrome/common/chrome_features.h"
+#include "chrome/grit/chromium_strings.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/theme_provider.h"
 #include "ui/gfx/canvas.h"
@@ -76,6 +78,11 @@ void AppMenuButton::SetSeverity(AppMenuIconController::IconType type,
                                 bool animate) {
   type_ = type;
   severity_ = severity;
+
+  SetTooltipText(
+      severity_ == AppMenuIconController::Severity::NONE
+          ? l10n_util::GetStringUTF16(IDS_APPMENU_TOOLTIP)
+          : l10n_util::GetStringUTF16(IDS_APPMENU_TOOLTIP_UPDATE_AVAILABLE));
   UpdateIcon(animate);
 }
 
