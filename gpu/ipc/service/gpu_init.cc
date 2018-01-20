4591bc534d1a22241f89a71d77567dddef9b4bda
Zhenyao Mo <zmo@chromium.org>
2018-01-20 05:39:01 +0000

    On GPU startup, don't switch to SwiftShader if blacklist decision isn't final.
    On Linux AMD, we have an entry to blacklist all GPU features on AMD GPU, except
    for newer drivers on AMD / Mesa drivers etc.
    
    If we fallback to SwiftShader on vendor_id/device_id, then we put all Linux AMD
    to SwiftShader unnecessarily.
    
    BUG=719213
    TEST=linux AMD
    R=piman@chromium.org,kbr@chromium.org
    TBR=boliu@chromium.org
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I6e6209931025383272669ee731bd503ccd6b94ba
    Reviewed-on: https://chromium-review.googlesource.com/877410
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530757}

--- gpu/ipc/service/gpu_init.cc
+++ gpu/ipc/service/gpu_init.cc
@@ -111,6 +111,10 @@ bool GpuInit::InitializeAndStartSandbox(
     const GPUInfo* gpu_info,
     const GpuFeatureInfo* gpu_feature_info) {
   gpu_preferences_ = gpu_preferences;
+  // Blacklist decisions based on basic GPUInfo may not be final. It might
+  // need more context based GPUInfo. In such situations, switching to
+  // SwiftShader needs to wait until creating a context.
+  bool needs_more_info = false;
 #if !defined(OS_ANDROID)
   if (gpu_info) {
     gpu_info_ = *gpu_info;
@@ -139,7 +143,8 @@ bool GpuInit::InitializeAndStartSandbox(
     gpu_feature_info_ = gpu::ComputeGpuFeatureInfo(
         gpu_info_, gpu_preferences.ignore_gpu_blacklist,
         gpu_preferences.disable_gpu_driver_bug_workarounds,
-        gpu_preferences.log_gpu_control_list_decisions, command_line);
+        gpu_preferences.log_gpu_control_list_decisions, command_line,
+        &needs_more_info);
   }
   if (gpu::SwitchableGPUsSupported(gpu_info_, *command_line)) {
     gpu::InitializeSwitchableGPUs(
@@ -208,7 +213,7 @@ bool GpuInit::InitializeAndStartSandbox(
   ui::OzonePlatform::InitializeForGPU(params);
 #endif
 
-  bool use_swiftshader = ShouldEnableSwiftShader(command_line);
+  bool use_swiftshader = ShouldEnableSwiftShader(command_line, needs_more_info);
   // Load and initialize the GL implementation and locate the GL entry points if
   // needed. This initialization may have already happened if running in the
   // browser process, for example.
@@ -239,8 +244,8 @@ bool GpuInit::InitializeAndStartSandbox(
     gpu_feature_info_ = gpu::ComputeGpuFeatureInfo(
         gpu_info_, gpu_preferences.ignore_gpu_blacklist,
         gpu_preferences.disable_gpu_driver_bug_workarounds,
-        gpu_preferences.log_gpu_control_list_decisions, command_line);
-    use_swiftshader = ShouldEnableSwiftShader(command_line);
+        gpu_preferences.log_gpu_control_list_decisions, command_line, nullptr);
+    use_swiftshader = ShouldEnableSwiftShader(command_line, false);
     if (use_swiftshader) {
       gl::init::ShutdownGL(true);
       gl_initialized = gl::init::InitializeGLNoExtensionsOneOff();
@@ -333,6 +338,7 @@ void GpuInit::InitializeInProcess(base::CommandLine* command_line,
     }
 #endif
   }
+  bool needs_more_info = false;
   if (gpu_feature_info) {
     gpu_feature_info_ = *gpu_feature_info;
   } else {
@@ -341,7 +347,8 @@ void GpuInit::InitializeInProcess(base::CommandLine* command_line,
       gpu_feature_info_ = gpu::ComputeGpuFeatureInfo(
           gpu_info_, gpu_preferences.ignore_gpu_blacklist,
           gpu_preferences.disable_gpu_driver_bug_workarounds,
-          gpu_preferences.log_gpu_control_list_decisions, command_line);
+          gpu_preferences.log_gpu_control_list_decisions, command_line,
+          &needs_more_info);
     }
 #endif
   }
@@ -350,7 +357,7 @@ void GpuInit::InitializeInProcess(base::CommandLine* command_line,
         gpu_feature_info_.enabled_gpu_driver_bug_workarounds);
   }
 
-  bool use_swiftshader = ShouldEnableSwiftShader(command_line);
+  bool use_swiftshader = ShouldEnableSwiftShader(command_line, needs_more_info);
   if (!gl::init::InitializeGLNoExtensionsOneOff()) {
     VLOG(1) << "gl::init::InitializeGLNoExtensionsOneOff failed";
     return;
@@ -361,8 +368,8 @@ void GpuInit::InitializeInProcess(base::CommandLine* command_line,
     gpu_feature_info_ = gpu::ComputeGpuFeatureInfo(
         gpu_info_, gpu_preferences.ignore_gpu_blacklist,
         gpu_preferences.disable_gpu_driver_bug_workarounds,
-        gpu_preferences.log_gpu_control_list_decisions, command_line);
-    use_swiftshader = ShouldEnableSwiftShader(command_line);
+        gpu_preferences.log_gpu_control_list_decisions, command_line, nullptr);
+    use_swiftshader = ShouldEnableSwiftShader(command_line, false);
     if (use_swiftshader) {
       gl::init::ShutdownGL(true);
       if (!gl::init::InitializeGLNoExtensionsOneOff()) {
@@ -384,15 +391,17 @@ void GpuInit::InitializeInProcess(base::CommandLine* command_line,
   }
 }
 
-bool GpuInit::ShouldEnableSwiftShader(base::CommandLine* command_line) {
+bool GpuInit::ShouldEnableSwiftShader(base::CommandLine* command_line,
+                                      bool blacklist_needs_more_info) {
 #if BUILDFLAG(ENABLE_SWIFTSHADER)
   if (gpu_preferences_.disable_software_rasterizer)
     return false;
   // Don't overwrite user preference.
   if (command_line->HasSwitch(switches::kUseGL))
     return false;
-  if (gpu_feature_info_.status_values[GPU_FEATURE_TYPE_ACCELERATED_WEBGL] !=
-      kGpuFeatureStatusEnabled) {
+  if (!blacklist_needs_more_info &&
+      gpu_feature_info_.status_values[GPU_FEATURE_TYPE_ACCELERATED_WEBGL] !=
+          kGpuFeatureStatusEnabled) {
     command_line->AppendSwitchASCII(
         switches::kUseGL, gl::kGLImplementationSwiftShaderForWebGLName);
     return true;
