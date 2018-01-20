4886be3626ebae7fe8b594fca68248c68697d193
Scott Violet <sky@chromium.org>
2018-01-20 00:26:40 +0000

    Fix event targeting bug
    The bug occurred in finding the valid target. Specifically if the root
    has a transform we transform the event first, and then compare against
    the size of the root. For transforms that result in increasing the
    location (transform value < 1) the location would end up outside the
    bounds of the window, which results in treating the location as in the
    non-client area and targeting the root.
    
    The fix for now is not to consider the root as a target for the non-client
    area. A better fix may be to potentially transform the size by the
    targets transform (if there is one), but I'm holding off on that for
    now. We don't do scale bounds in WindowTargeter either.
    
    BUG=794262
    TEST=covered by tests
    
    Change-Id: If7a913d934808fd4ffc404f74eea73d9f98b71ab
    Reviewed-on: https://chromium-review.googlesource.com/877219
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530683}

--- services/ui/ws/window_finder.cc
+++ services/ui/ws/window_finder.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/adapters.h"
 #include "services/ui/ws/server_window.h"
-#include "services/ui/ws/server_window_delegate.h"
 #include "ui/gfx/geometry/point.h"
 #include "ui/gfx/geometry/point3_f.h"
 #include "ui/gfx/geometry/point_f.h"
@@ -88,6 +87,7 @@ gfx::Transform TransformFromParent(const ServerWindow* window,
 
 bool FindDeepestVisibleWindowForLocationImpl(
     ServerWindow* window,
+    bool is_root_window,
     EventSource event_source,
     const gfx::Point& location_in_root,
     const gfx::Point& location_in_window,
@@ -96,7 +96,11 @@ bool FindDeepestVisibleWindowForLocationImpl(
   // The non-client area takes precedence over descendants, as otherwise the
   // user would likely not be able to hit the non-client area as it's common
   // for descendants to go into the non-client area.
-  if (IsLocationInNonclientArea(window, location_in_window)) {
+  //
+  // Display roots aren't allowed to have non-client areas. This is important
+  // as roots may have a transform, which causes problem in comparing sizes.
+  if (!is_root_window &&
+      IsLocationInNonclientArea(window, location_in_window)) {
     deepest_window->window = window;
     deepest_window->in_non_client_area = true;
     return true;
@@ -134,9 +138,10 @@ bool FindDeepestVisibleWindowForLocationImpl(
         continue;
       }
 
+      const bool child_is_root = false;
       if (FindDeepestVisibleWindowForLocationImpl(
-              child, event_source, location_in_root, location_in_child,
-              child_transform, deepest_window)) {
+              child, child_is_root, event_source, location_in_root,
+              location_in_child, child_transform, deepest_window)) {
         return true;
       }
     }
@@ -165,9 +170,10 @@ DeepestWindow FindDeepestVisibleWindowForLocation(ServerWindow* root_window,
   DeepestWindow result;
   // Allow the root to have a transform, which mirrors what happens with
   // WindowManagerDisplayRoot.
-  FindDeepestVisibleWindowForLocationImpl(root_window, event_source, location,
-                                          initial_location, root_transform,
-                                          &result);
+  const bool is_root_window = true;
+  FindDeepestVisibleWindowForLocationImpl(
+      root_window, is_root_window, event_source, location, initial_location,
+      root_transform, &result);
   return result;
 }
 
