9a72474ebc78b5968ced011733103db0331b758d
Megumi Hattori <megumihattori@google.com>
2018-01-22 04:20:32 +0000

    Remove an unnecessary arrow from the lock screen notification center.
    The arrow was shown in the the lock screen notification center,
    but it should not be shown as it is no-op here.
    This implementation removes the arrow.
    
    TEST=ash_unittests
    BUG=797861
    
    Change-Id: Idf9db7bdb284cc2979e1fc3166d2be20ca8bdbee
    Reviewed-on: https://chromium-review.googlesource.com/867790
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Commit-Queue: Megumi Hattori <megumihattori@google.com>
    Cr-Commit-Position: refs/heads/master@{#530808}

--- ash/message_center/message_center_button_bar.cc
+++ ash/message_center/message_center_button_bar.cc
@@ -170,6 +170,7 @@ MessageCenterButtonBar::MessageCenterButtonBar(
   button_container_->AddChildView(CreateVerticalSeparator());
 
   collapse_button_ = new MessageCenterButton(this);
+  collapse_button_->SetVisible(false);
   collapse_button_->SetBackground(
       views::CreateSolidBackground(message_center_style::kBackgroundColor));
   collapse_button_->SetPaintToLayer();
@@ -221,6 +222,10 @@ views::Button* MessageCenterButtonBar::GetQuietModeButtonForTest() const {
 
 views::Button* MessageCenterButtonBar::GetSettingsButtonForTest() const {
   return settings_button_;
+}
+
+views::Button* MessageCenterButtonBar::GetCollapseButtonForTest() const {
+  return collapse_button_;
 }
 
 void MessageCenterButtonBar::SetBackArrowVisible(bool visible) {
--- ash/message_center/message_center_view_unittest.cc
+++ ash/message_center/message_center_view_unittest.cc
@@ -867,6 +867,8 @@ TEST_F(MessageCenterViewTest, LockScreen) {
   ASSERT_NE(nullptr, quiet_mode_button);
   views::Button* settings_button = GetButtonBar()->GetSettingsButtonForTest();
   ASSERT_NE(nullptr, settings_button);
+  views::Button* collapse_button = GetButtonBar()->GetCollapseButtonForTest();
+  ASSERT_NE(nullptr, collapse_button);
 
   EXPECT_TRUE(close_button->visible());
   EXPECT_TRUE(quiet_mode_button->visible());
@@ -905,6 +907,7 @@ TEST_F(MessageCenterViewTest, LockScreen) {
   EXPECT_FALSE(close_button->visible());
   EXPECT_FALSE(quiet_mode_button->visible());
   EXPECT_FALSE(settings_button->visible());
+  EXPECT_FALSE(collapse_button->visible());
 
   // Unlock!
   SetLockedState(false);
@@ -929,6 +932,7 @@ TEST_F(MessageCenterViewTest, LockScreen) {
   EXPECT_FALSE(close_button->visible());
   EXPECT_FALSE(quiet_mode_button->visible());
   EXPECT_FALSE(settings_button->visible());
+  EXPECT_FALSE(collapse_button->visible());
 }
 
 TEST_F(MessageCenterViewTest, NoNotification) {
