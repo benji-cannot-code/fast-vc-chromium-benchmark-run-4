639bf91c60466100003fcb4e0294644040b05f50
Elad Alon <eladalon@chromium.org>
2018-01-22 13:05:27 +0000

    More unit tests in WebRtcEventLogManager
    This CL adds unit tests in WebRtcEventLogManager, that will make sure
    that WebRTC is correctly instructed to start/stop sending event logs
    depending on whether those need to be logged.
    
    Bug: 775415
    Change-Id: I7fe8897a833186c48366b99a42f1f760f3729cb9
    Reviewed-on: https://chromium-review.googlesource.com/866770
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Elad Alon <eladalon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530847}

--- content/browser/webrtc/webrtc_event_log_manager.h
+++ content/browser/webrtc/webrtc_event_log_manager.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_BROWSER_WEBRTC_WEBRTC_EVENT_LOG_MANAGER_H_
 
 #include <map>
+#include <memory>
 #include <type_traits>
 
 #include "base/callback.h"
@@ -31,6 +32,18 @@ namespace content {
 class CONTENT_EXPORT WebRtcEventLogManager
     : protected WebRtcLocalEventLogsObserver {
  public:
+  // To turn WebRTC on and off, we go through PeerConnectionTrackerProxy. In
+  // order to make this toggling easily testable, PeerConnectionTrackerProxyImpl
+  // will send real messages to PeerConnectionTracker, whereas
+  // PeerConnectionTrackerProxyForTesting will be a mock that just makes sure
+  // the correct messages were attempted to be sent.
+  class PeerConnectionTrackerProxy {
+   public:
+    virtual ~PeerConnectionTrackerProxy() = default;
+    virtual void StartEventLogOutput(WebRtcEventLogPeerConnectionKey key) = 0;
+    virtual void StopEventLogOutput(WebRtcEventLogPeerConnectionKey key) = 0;
+  };
+
   // Ensures that no previous instantiation of the class was performed, then
   // instantiates the class and returns the object. Subsequent calls to
   // GetInstance() will return this object.
@@ -170,7 +183,10 @@ class CONTENT_EXPORT WebRtcEventLogManager
   void UpdateWebRtcEventLoggingState(PeerConnectionKey peer_connection,
                                      bool enabled);
 
-  void InjectClockForTesting(base::Clock* clock);
+  void SetClockForTesting(base::Clock* clock);
+
+  void SetPeerConnectionTrackerProxyForTesting(
+      std::unique_ptr<PeerConnectionTrackerProxy> pc_tracker_proxy);
 
   // Observer which will be informed whenever a local log file is started or
   // stopped. Its callbacks are called synchronously from |task_runner_|,
@@ -185,6 +201,11 @@ class CONTENT_EXPORT WebRtcEventLogManager
   // in WebRTC, and for which client(s).
   std::map<PeerConnectionKey, LoggingTargetBitmap>
       peer_connections_with_event_logging_enabled_;
+
+  // In production, this holds a small object that just tells WebRTC (via
+  // PeerConnectionTracker) to start/stop producing event logs for a specific
+  // peer connection. In (relevant) unit tests, a mock will be injected.
+  std::unique_ptr<PeerConnectionTrackerProxy> pc_tracker_proxy_;
 
   // The main logic will run sequentially on this runner, on which blocking
   // tasks are allowed.
