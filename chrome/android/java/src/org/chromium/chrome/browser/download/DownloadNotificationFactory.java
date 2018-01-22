ae2e977c2ff6b609a310da8c2fa3775aa54dc6f6
Joy Ming <jming@chromium.org>
2018-01-22 22:46:00 +0000

    [Downloads notification] Swiping away paused notification doesnt cancel.
    Before this change, the behavior between the having the flag
    (#enable-downloads-foreground) enabled or not was different. With the
    flag enabled, swiping away a paused download notification cancels it and
    without the flag enabled, swiping away did not cancel. This change makes
    the behavior consistent in that swiping away a paused download does not
    cancel the download, rather, allows the user to go through downloads
    home to cancel it themselves.
    
    Bug: 803999
    Change-Id: I1bd413575629885f7caeda11c0a5eff59f6c7375
    Reviewed-on: https://chromium-review.googlesource.com/877187
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Joy Ming <jming@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531025}

--- chrome/android/java/src/org/chromium/chrome/browser/download/DownloadNotificationFactory.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/DownloadNotificationFactory.java
@@ -154,8 +154,6 @@ public final class DownloadNotificationFactory {
                         downloadUpdate.getContentId(), downloadUpdate.getIsOffTheRecord());
                 cancelIntent = buildActionIntent(context, ACTION_DOWNLOAD_CANCEL,
                         downloadUpdate.getContentId(), downloadUpdate.getIsOffTheRecord());
-                PendingIntent deleteIntent = buildPendingIntent(
-                        context, cancelIntent, downloadUpdate.getNotificationId());
 
                 builder.setAutoCancel(false)
                         .setLargeIcon(downloadUpdate.getIcon())
@@ -168,8 +166,12 @@ public final class DownloadNotificationFactory {
                                 context.getResources().getString(
                                         R.string.download_notification_cancel_button),
                                 buildPendingIntent(
-                                        context, cancelIntent, downloadUpdate.getNotificationId()))
-                        .setDeleteIntent(deleteIntent);
+                                        context, cancelIntent, downloadUpdate.getNotificationId()));
+
+                if (downloadUpdate.getIsTransient()) {
+                    builder.setDeleteIntent(buildPendingIntent(
+                            context, cancelIntent, downloadUpdate.getNotificationId()));
+                }
 
                 break;
 
