852e15b059ac4e991f117b2b3c6f9f731332b80d
Yuri Wiitala <miu@chromium.org>
2018-01-22 02:05:43 +0000

    Fix crash-on-start caused by recent Network Service browser tests change.
    The crash was caused by https://chromium-review.googlesource.com/853301
    because it added a call into base::CommandLine::ForCurrentProcess().
    This change adds the missing instantiation of base::CommandLine in the
    main() function for performance_browser_tests.
    
    TBR=jam@chromium.org
    
    Bug: 803455, 769401
    Change-Id: I8450b01559359d12f5258922d4b570e2d8b43647
    Reviewed-on: https://chromium-review.googlesource.com/877823
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Commit-Queue: Yuri Wiitala <miu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530798}

--- chrome/test/base/browser_perf_tests_main.cc
+++ chrome/test/base/browser_perf_tests_main.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/chrome_test_suite.h"
 
 int main(int argc, char** argv) {
+  base::CommandLine::Init(argc, argv);
+
   // Always run browser perf tests serially - parallel running would be less
   // deterministic and distort perf measurements.
   size_t parallel_jobs = 1U;
