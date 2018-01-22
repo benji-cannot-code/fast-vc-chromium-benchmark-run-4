1bb5d5464b838e9e059546963531c85d68ba3199
Nate Chapin <japhet@chromium.org>
2018-01-22 21:17:02 +0000

    Fix null deref in Internals::textAffinity
    Bug: 801826
    Change-Id: Ib8552a33c551d887a39d8eab9a3317311ac26385
    Reviewed-on: https://chromium-review.googlesource.com/876927
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530994}

--- third_party/WebKit/Source/core/testing/Internals.cpp
+++ third_party/WebKit/Source/core/testing/Internals.cpp
@@ -924,13 +924,13 @@ DOMRectReadOnly* Internals::absoluteCaretBounds(
 }
 
 String Internals::textAffinity() {
-  if (GetFrame()
-          ->GetPage()
-          ->GetFocusController()
-          .FocusedFrame()
-          ->Selection()
-          .GetSelectionInDOMTree()
-          .Affinity() == TextAffinity::kUpstream) {
+  if (GetFrame() && GetFrame()
+                            ->GetPage()
+                            ->GetFocusController()
+                            .FocusedFrame()
+                            ->Selection()
+                            .GetSelectionInDOMTree()
+                            .Affinity() == TextAffinity::kUpstream) {
     return "Upstream";
   }
   return "Downstream";
