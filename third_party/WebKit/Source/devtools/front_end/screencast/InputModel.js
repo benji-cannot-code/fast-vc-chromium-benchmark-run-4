42459087975f251038f587fa07942eaede225b0b
Deepanjan Roy <dproy@chromium.org>
2018-01-22 19:51:02 +0000

    Use host timestamp for devtools screencast
    During remote debugging, the host is a more reliable source of timestamp
    than the remote debugging agent. This CL makes the timestamp optional in
    for EmulateTouchFromMouseEvent, and stops supplying the timestamp from
    input methods on the frontend. The debugging agent retains the option to
    provide a custom timestamp if necessary.
    
    No test is included for the frontend fix. A test is added for the
    backend to check empty timestamp is accepted for
    EmulateTouchFromMouseEvent, and that the default timestamp comes from a
    monotonic clock.
    
    Bug: 797334
    Change-Id: I2f01cf225cd2db6a1315f7c2c2a9305a0c522643
    Reviewed-on: https://chromium-review.googlesource.com/850728
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Deepanjan Roy <dproy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530957}

--- third_party/WebKit/Source/devtools/front_end/screencast/InputModel.js
+++ third_party/WebKit/Source/devtools/front_end/screencast/InputModel.js
@@ -38,7 +38,6 @@ Screencast.InputModel = class extends SDK.SDKModel {
     this._inputAgent.invoke_dispatchKeyEvent({
       type: type,
       modifiers: this._modifiersForEvent(event),
-      timestamp: event.timeStamp / 1000,
       text: text,
       unmodifiedText: text ? text.toLowerCase() : undefined,
       keyIdentifier: event.keyIdentifier,
@@ -81,7 +80,6 @@ Screencast.InputModel = class extends SDK.SDKModel {
       x: x,
       y: y,
       modifiers: this._modifiersForEvent(event),
-      timestamp: event.timeStamp / 1000,
       button: buttons[event.which],
       clickCount: 0
     };
