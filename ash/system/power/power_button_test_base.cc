36266fe8370c5f812bb4f1f8ca582f5e8bde87da
MinChen <minch@chromium.org>
2018-01-22 23:32:21 +0000

    cros: Create power button menu.
    changes in this cl,
    1. Create power button menu if ash::switches::kShowPowerButtonMenu is set.
       Create the corresponding view hierarchy of the menu.
    2. Add PowerButtonScreenshotShowMenuTest* and
       TabletPowerButtonControllerShowMenuTest*.
    
    Following up cls,
    1. Add corresponding switch in chromeos.
    2. Adjust the menu position according to the position of the power button.
       The menu is put at the center of the screen currently.
    
    Bug: 799524
    Change-Id: Id435328537fe08c7faf3e073564ba81498a86f56
    Reviewed-on: https://chromium-review.googlesource.com/874784
    Commit-Queue: min c <minch@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Qiang(Joe) Xu <warx@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531058}

--- ash/system/power/power_button_test_base.cc
+++ ash/system/power/power_button_test_base.cc
@@ -49,6 +49,10 @@ void PowerButtonTestBase::SetUp() {
     base::CommandLine::ForCurrentProcess()->AppendSwitch(
         switches::kAshEnableTabletMode);
   }
+  if (show_power_button_menu_) {
+    base::CommandLine::ForCurrentProcess()->AppendSwitch(
+        switches::kShowPowerButtonMenu);
+  }
   AshTestBase::SetUp();
 
   lock_state_controller_ = Shell::Get()->lock_state_controller();
@@ -153,6 +157,12 @@ void PowerButtonTestBase::UnlockScreen() {
 
 void PowerButtonTestBase::EnableTabletMode(bool enable) {
   Shell::Get()->tablet_mode_controller()->EnableTabletModeWindowManager(enable);
+}
+
+void PowerButtonTestBase::AdvanceClockToAvoidIgnoring() {
+  tick_clock_->Advance(
+      TabletPowerButtonController::kIgnoreRepeatedButtonUpDelay +
+      base::TimeDelta::FromMilliseconds(1));
 }
 
 }  // namespace ash
--- ash/system/power/tablet_power_button_controller_test_api.cc
+++ ash/system/power/tablet_power_button_controller_test_api.cc
@@ -6,7 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/power/tablet_power_button_controller_test_api.h"
 
 #include "ash/system/power/power_button_display_controller.h"
+#include "ash/system/power/power_button_menu_screen_view.h"
+#include "ash/system/power/power_button_menu_view.h"
 #include "ash/system/power/tablet_power_button_controller.h"
+#include "ui/gfx/geometry/rect.h"
+#include "ui/views/widget/widget.h"
 
 namespace ash {
 
@@ -31,8 +35,44 @@ bool TabletPowerButtonControllerTestApi::TriggerShutdownTimeout() {
   return true;
 }
 
+bool TabletPowerButtonControllerTestApi::PowerButtonMenuTimerIsRunning() const {
+  return controller_->power_button_menu_timer_.IsRunning();
+}
+
+bool TabletPowerButtonControllerTestApi::TriggerPowerButtonMenuTimeout() {
+  if (!controller_->power_button_menu_timer_.IsRunning())
+    return false;
+
+  base::Closure task = controller_->power_button_menu_timer_.user_task();
+  controller_->power_button_menu_timer_.Stop();
+  task.Run();
+  return true;
+}
+
 void TabletPowerButtonControllerTestApi::SendKeyEvent(ui::KeyEvent* event) {
   controller_->display_controller_->OnKeyEvent(event);
+}
+
+gfx::Rect TabletPowerButtonControllerTestApi::GetMenuBoundsInScreen() const {
+  return IsMenuOpened() ? GetPowerButtonMenuView()->GetBoundsInScreen()
+                        : gfx::Rect();
+}
+
+PowerButtonMenuView*
+TabletPowerButtonControllerTestApi::GetPowerButtonMenuView() const {
+  return IsMenuOpened() ? static_cast<PowerButtonMenuScreenView*>(
+                              controller_->menu_widget_->GetContentsView())
+                              ->power_button_menu_view()
+                        : nullptr;
+}
+
+bool TabletPowerButtonControllerTestApi::IsMenuOpened() const {
+  return controller_->IsMenuOpened();
+}
+
+bool TabletPowerButtonControllerTestApi::MenuHasSignOutItem() const {
+  return IsMenuOpened() &&
+         GetPowerButtonMenuView()->sign_out_item_for_testing();
 }
 
 }  // namespace ash
