ee3fa548ccebebc338ca3218036ebeb429917565
Francois Doray <fdoray@chromium.org>
2018-01-22 22:41:26 +0000

    Use base::NoDestructor for TimeTicks::UnixEpoch.
    Change-Id: Ib3306e8c136b74c2cdaa9c5e9c33f995a2f4c5e6
    Reviewed-on: https://chromium-review.googlesource.com/879390
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531017}

--- base/time/time.cc
+++ base/time/time.cc
@@ -11,9 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <ostream>
 #include <sstream>
 
-#include "base/lazy_instance.h"
 #include "base/logging.h"
 #include "base/macros.h"
+#include "base/no_destructor.h"
 #include "base/strings/stringprintf.h"
 #include "base/third_party/nspr/prtime.h"
 #include "build/build_config.h"
@@ -299,27 +299,11 @@ std::ostream& operator<<(std::ostream& os, Time time) {
                             exploded.millisecond);
 }
 
-// Local helper class to hold the conversion from Time to TickTime at the
-// time of the Unix epoch.
-class UnixEpochSingleton {
- public:
-  UnixEpochSingleton()
-      : unix_epoch_(TimeTicks::Now() - (Time::Now() - Time::UnixEpoch())) {}
-
-  TimeTicks unix_epoch() const { return unix_epoch_; }
-
- private:
-  const TimeTicks unix_epoch_;
-
-  DISALLOW_COPY_AND_ASSIGN(UnixEpochSingleton);
-};
-
-static LazyInstance<UnixEpochSingleton>::Leaky
-    leaky_unix_epoch_singleton_instance = LAZY_INSTANCE_INITIALIZER;
-
 // Static
 TimeTicks TimeTicks::UnixEpoch() {
-  return leaky_unix_epoch_singleton_instance.Get().unix_epoch();
+  static const base::NoDestructor<base::TimeTicks> epoch(
+      []() { return TimeTicks::Now() - (Time::Now() - Time::UnixEpoch()); }());
+  return *epoch;
 }
 
 TimeTicks TimeTicks::SnappedToNextTick(TimeTicks tick_phase,
