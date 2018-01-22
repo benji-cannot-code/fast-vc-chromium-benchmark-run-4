6129982b50956f8ba1d31e264b9f7a2b734bcab0
Dmitry Gozman <dgozman@chromium.org>
2018-01-22 21:26:36 +0000

    [DevTools] Fix InspectorFrontendHost.isUnderTest not present in newer Chrome
    We should return false for backward-compatibility with older frontends.
    Regressed here: https://chromium.googlesource.com/chromium/src/+/baf650e823bb8fd2615e6ec2920b46600d2a7585.
    
    Bug: 802928, 801835
    Change-Id: Ibbe3a932467f5c4cb7c6db49bef12f5c10a40926
    Reviewed-on: https://chromium-review.googlesource.com/879166
    Commit-Queue: Andrey Lushnikov <lushnikov@chromium.org>
    Reviewed-by: Andrey Lushnikov <lushnikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530999}

--- third_party/WebKit/Source/devtools/front_end/devtools_compatibility.js
+++ third_party/WebKit/Source/devtools/front_end/devtools_compatibility.js
@@ -757,6 +757,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     // Backward-compatible methods below this line --------------------------------------------
 
     /**
+     * Support for legacy front-ends (<M65).
+     * @return {boolean}
+     */
+    isUnderTest() {
+      return false;
+    }
+
+    /**
      * Support for legacy front-ends (<M50).
      * @param {string} message
      */
