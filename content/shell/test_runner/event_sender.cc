aeadd38d81ce6e8304e00a7273d7554a1dd960c0
Mustaq Ahmed <mustaq@google.com>
2018-01-22 19:43:44 +0000

    Update media layout tests that rely on stack-allocated activation
    These tests assume (current) UserGestureToken like behavior that
    the user activation state is gone outside of the triggering event
    loop.  UserActivationV2 would change it, fixing the tests
    accordingly.
    
    Bug: 772432
    Change-Id: I2bac89bb9043bb7545fd6e6860fb6b83dd74bb0c
    Reviewed-on: https://chromium-review.googlesource.com/868573
    Reviewed-by: Rick Byers <rbyers@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Commit-Queue: Mustaq Ahmed <mustaq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530954}

--- content/shell/test_runner/event_sender.cc
+++ content/shell/test_runner/event_sender.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/web/WebKit.h"
 #include "third_party/WebKit/public/web/WebLocalFrame.h"
 #include "third_party/WebKit/public/web/WebPagePopup.h"
+#include "third_party/WebKit/public/web/WebUserGestureIndicator.h"
 #include "third_party/WebKit/public/web/WebView.h"
 #include "ui/events/blink/blink_event_util.h"
 #include "ui/events/keycodes/dom/keycode_converter.h"
@@ -620,6 +621,7 @@ class EventSenderBindings : public gin::Wrappable<EventSenderBindings> {
   void MouseScrollBy(gin::Arguments* args);
   void ScheduleAsynchronousClick(gin::Arguments* args);
   void ScheduleAsynchronousKeyDown(gin::Arguments* args);
+  void ConsumeUserActivation();
   void MouseDown(gin::Arguments* args);
   void MouseUp(gin::Arguments* args);
   void SetMouseButtonState(gin::Arguments* args);
@@ -754,6 +756,8 @@ gin::ObjectTemplateBuilder EventSenderBindings::GetObjectTemplateBuilder(
                  &EventSenderBindings::ScheduleAsynchronousClick)
       .SetMethod("scheduleAsynchronousKeyDown",
                  &EventSenderBindings::ScheduleAsynchronousKeyDown)
+      .SetMethod("consumeUserActivation",
+                 &EventSenderBindings::ConsumeUserActivation)
       .SetProperty("forceLayoutOnEvents",
                    &EventSenderBindings::ForceLayoutOnEvents,
                    &EventSenderBindings::SetForceLayoutOnEvents)
@@ -1071,6 +1075,11 @@ void EventSenderBindings::ScheduleAsynchronousKeyDown(gin::Arguments* args) {
   }
   sender_->ScheduleAsynchronousKeyDown(code_str, modifiers,
                                        static_cast<KeyLocationCode>(location));
+}
+
+void EventSenderBindings::ConsumeUserActivation() {
+  if (sender_)
+    sender_->ConsumeUserActivation();
 }
 
 void EventSenderBindings::MouseDown(gin::Arguments* args) {
@@ -2257,6 +2266,11 @@ void EventSender::ScheduleAsynchronousKeyDown(const std::string& code_str,
   delegate()->PostTask(base::Bind(&EventSender::KeyDown,
                                   weak_factory_.GetWeakPtr(), code_str,
                                   modifiers, location));
+}
+
+void EventSender::ConsumeUserActivation() {
+  blink::WebUserGestureIndicator::ConsumeUserGesture(
+      view()->MainFrame()->ToWebLocalFrame());
 }
 
 double EventSender::GetCurrentEventTimeSec() {
