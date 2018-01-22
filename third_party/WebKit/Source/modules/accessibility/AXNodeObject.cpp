4220360724437f02f35a806b3b16bcd29ab77f07
Rob Dodson <robdodson@chromium.org>
2018-01-22 22:52:06 +0000

    Progress elements with -webkit-appearance: none should retain their role.
    If a <progress> element has its appearance removed with -webkit-appearance:none,
    it should still retain its role for accessibility. This CL also adds a test
    to check that various other control types are not affected by this issue.
    
    R=aboxhall@chromium.org
    
    Bug: 687828
    Change-Id: I1a30e581dd109b34cae69ec9737cb233ceb5f1fb
    Reviewed-on: https://chromium-review.googlesource.com/861054
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Rob Dodson <robdodson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531034}

--- third_party/WebKit/Source/modules/accessibility/AXNodeObject.cpp
+++ third_party/WebKit/Source/modules/accessibility/AXNodeObject.cpp
@@ -423,6 +423,9 @@ AccessibilityRole AXNodeObject::NativeAccessibilityRoleIgnoringAria() const {
   if (IsHTMLMeterElement(*GetNode()))
     return kMeterRole;
 
+  if (IsHTMLProgressElement(*GetNode()))
+    return kProgressIndicatorRole;
+
   if (IsHTMLOutputElement(*GetNode()))
     return kStatusRole;
 
