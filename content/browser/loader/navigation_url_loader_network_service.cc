97035c72b47dc79da86f88dbe12b401bfae06796
Jochen Eisinger <jochen@chromium.org>
2018-01-23 12:58:29 +0000

    Move suggested_filename from begin to common nav params
    R=mkwst@chromium.org,clamy@chromium.org
    
    Bug: 714373
    Change-Id: Ib0b8fd1ee8fc2118e6d4e0b11ae5e19dd99b8066
    Reviewed-on: https://chromium-review.googlesource.com/880721
    Commit-Queue: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531217}

--- content/browser/loader/navigation_url_loader_network_service.cc
+++ content/browser/loader/navigation_url_loader_network_service.cc
@@ -791,7 +791,7 @@ NavigationURLLoaderNetworkService::NavigationURLLoaderNetworkService(
         /* default_url_factory_getter = */ nullptr,
         request_info->common_params.url,
         request_info->begin_params->initiator_origin,
-        request_info->begin_params->suggested_filename,
+        request_info->common_params.suggested_filename,
         weak_factory_.GetWeakPtr());
 
     BrowserThread::PostTask(
@@ -875,7 +875,7 @@ NavigationURLLoaderNetworkService::NavigationURLLoaderNetworkService(
       std::move(initial_handlers), std::move(new_request), resource_context,
       partition->url_loader_factory_getter(), request_info->common_params.url,
       request_info->begin_params->initiator_origin,
-      request_info->begin_params->suggested_filename,
+      request_info->common_params.suggested_filename,
       weak_factory_.GetWeakPtr());
   BrowserThread::PostTask(
       BrowserThread::IO, FROM_HERE,
