813393eb4a70b56308efc7683582319e957411f0
Boris Sazonov <bsazonov@chromium.org>
2018-01-23 15:40:22 +0000

    [Signin][Android] Fix confirmation screen if view is not attached yet
    This CL addresses the issue when AccountSigninView doesn't show
    confirmation page, showing empty account list instead. It could happen
    if AccountSigninView is initialized before being attached to a window.
    This CL also renames some methods in AccountSigninView to make them
    less obscure.
    
    Bug: 800665
    Change-Id: I749bdb871877ac102cfb673989d9d318db117412
    Reviewed-on: https://chromium-review.googlesource.com/881323
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531246}

--- chrome/android/java/src/org/chromium/chrome/browser/signin/AccountSigninView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/AccountSigninView.java
@@ -153,6 +153,7 @@ public class AccountSigninView extends FrameLayout {
     private UserRecoverableErrorHandler.ModalDialog mGooglePlayServicesUpdateErrorHandler;
     private AlertDialog mGmsIsUpdatingDialog;
     private long mGmsIsUpdatingDialogShowTime;
+    private boolean mShouldShowConfirmationPageWhenAttachedToWindow;
 
     private AccountSigninConfirmationView mSigninConfirmationView;
     private ImageView mSigninAccountImage;
@@ -250,7 +251,7 @@ public class AccountSigninView extends FrameLayout {
                 String accountName = arguments.getString(ARGUMENT_ACCOUNT_NAME);
                 assert accountName != null;
                 boolean isDefaultAccount = arguments.getBoolean(ARGUMENT_IS_DEFAULT_ACCOUNT, false);
-                showConfirmSigninPageAccountTrackerServiceCheck(accountName, isDefaultAccount);
+                showConfirmationPageForAccount(accountName, isDefaultAccount);
                 triggerUpdateAccounts();
                 break;
             }
@@ -312,6 +313,10 @@ public class AccountSigninView extends FrameLayout {
         triggerUpdateAccounts();
         AccountManagerFacade.get().addObserver(mAccountsChangedObserver);
         mProfileDataCache.addObserver(mProfileDataCacheObserver);
+        if (mShouldShowConfirmationPageWhenAttachedToWindow) {
+            // Can happen if init is invoked before attaching to window (https://crbug.com/800665).
+            seedAccountsAndShowConfirmationPage();
+        }
     }
 
     @Override
@@ -435,7 +440,7 @@ public class AccountSigninView extends FrameLayout {
         }
 
         if (shouldJumpToConfirmationScreen) {
-            showConfirmSigninPageAccountTrackerServiceCheck();
+            showConfirmationPageForSelectedAccount();
         }
     }
 
@@ -559,7 +564,7 @@ public class AccountSigninView extends FrameLayout {
         triggerUpdateAccounts();
     }
 
-    private void showConfirmSigninPage() {
+    private void showConfirmationPage() {
         updateSignedInAccountInfo();
         mProfileDataCache.update(Collections.singletonList(mSelectedAccountName));
 
@@ -586,26 +591,29 @@ public class AccountSigninView extends FrameLayout {
                         new SpanInfo(SETTINGS_LINK_OPEN, SETTINGS_LINK_CLOSE, settingsSpan)));
     }
 
-    private void showConfirmSigninPageAccountTrackerServiceCheck() {
+    private void showConfirmationPageForSelectedAccount() {
         int index = mSigninChooseView.getSelectedAccountPosition();
-        showConfirmSigninPageAccountTrackerServiceCheck(mAccountNames.get(index), index == 0);
+        showConfirmationPageForAccount(mAccountNames.get(index), index == 0);
     }
 
-    private void showConfirmSigninPageAccountTrackerServiceCheck(
-            final String accountName, final boolean isDefaultAccount) {
+    private void showConfirmationPageForAccount(String accountName, boolean isDefaultAccount) {
         assert accountName != null;
+
         // Disable the buttons to prevent them being clicked again while waiting for the callbacks.
         setButtonsEnabled(false);
 
         mSelectedAccountName = accountName;
         mIsDefaultAccountSelected = isDefaultAccount;
+        seedAccountsAndShowConfirmationPage();
+    }
 
+    private void seedAccountsAndShowConfirmationPage() {
         // Ensure that the AccountTrackerService has a fully up to date GAIA id <-> email mapping,
         // as this is needed for the previous account check.
         final long seedingStartTime = SystemClock.elapsedRealtime();
         if (AccountTrackerService.get().checkAndSeedSystemAccounts()) {
             recordAccountTrackerServiceSeedingTime(seedingStartTime);
-            showConfirmSigninPagePreviousAccountCheck();
+            runStateMachineAndShowConfirmationPage();
         } else {
             AccountTrackerService.get().addSystemAccountsSeededListener(
                     new OnSystemAccountsSeededListener() {
@@ -615,7 +623,9 @@ public class AccountSigninView extends FrameLayout {
                             recordAccountTrackerServiceSeedingTime(seedingStartTime);
                             // Don't show dialogs and confirmation page if activity was destroyed.
                             if (ViewCompat.isAttachedToWindow(AccountSigninView.this)) {
-                                showConfirmSigninPagePreviousAccountCheck();
+                                runStateMachineAndShowConfirmationPage();
+                            } else {
+                                mShouldShowConfirmationPageWhenAttachedToWindow = true;
                             }
                         }
 
@@ -625,7 +635,7 @@ public class AccountSigninView extends FrameLayout {
         }
     }
 
-    private void showConfirmSigninPagePreviousAccountCheck() {
+    private void runStateMachineAndShowConfirmationPage() {
         mConfirmSyncDataStateMachine = new ConfirmSyncDataStateMachine(getContext(),
                 mDelegate.getFragmentManager(), ImportSyncType.PREVIOUS_DATA_FOUND,
                 PrefServiceBridge.getInstance().getSyncLastAccountName(), mSelectedAccountName,
@@ -634,7 +644,7 @@ public class AccountSigninView extends FrameLayout {
                     public void onConfirm(boolean wipeData) {
                         mConfirmSyncDataStateMachine = null;
                         SigninManager.wipeSyncUserDataIfRequired(wipeData).then(
-                                (Void v) -> showConfirmSigninPage());
+                                (Void v) -> showConfirmationPage());
                     }
 
                     @Override
@@ -664,8 +674,7 @@ public class AccountSigninView extends FrameLayout {
     private void setUpSigninButton(boolean hasAccounts) {
         if (hasAccounts) {
             mPositiveButton.setText(R.string.continue_sign_in);
-            mPositiveButton.setOnClickListener(
-                    view -> showConfirmSigninPageAccountTrackerServiceCheck());
+            mPositiveButton.setOnClickListener(view -> showConfirmationPageForSelectedAccount());
         } else {
             mPositiveButton.setText(R.string.choose_account_sign_in);
             mPositiveButton.setOnClickListener(view -> {
