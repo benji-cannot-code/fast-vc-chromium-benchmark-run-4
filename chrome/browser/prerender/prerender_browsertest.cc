36f39913cdb9d2aea50cd99406f1589352a40191
Egor Pasko <pasko@chromium.org>
2018-01-23 19:01:45 +0000

    prerender: disable browser_tests on MacASAN
    Also revert the time scaling, since the timeouts happen faster than the
    scaled time goes. Not doing a revert to eliminate some duplication of
    disabling that we had previously.
    
    TBR=mattcary@chromium.org
    
    Bug: 708158
    Change-Id: I367e4bd3c5e309a3f72078e7cd165e7d1182f257
    Reviewed-on: https://chromium-review.googlesource.com/881184
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531292}

--- chrome/browser/prerender/prerender_browsertest.cc
+++ chrome/browser/prerender/prerender_browsertest.cc
@@ -157,6 +157,9 @@ using prerender::test_utils::TestPrerender;
 using prerender::test_utils::TestPrerenderContents;
 using task_manager::browsertest_util::WaitForTaskManagerRows;
 
+// crbug.com/708158
+#if !defined(OS_MACOSX) || !defined(ADDRESS_SANITIZER)
+
 // Prerender tests work as follows:
 //
 // A page with a prefetch link to the test page is loaded.  Once prerendered,
@@ -3860,3 +3863,5 @@ IN_PROC_BROWSER_TEST_F(PrerenderBrowserTestWithNaCl,
 #endif  // BUILDFLAG(ENABLE_NACL)
 
 }  // namespace prerender
+
+#endif  // !defined(OS_MACOSX) || !defined(ADDRESS_SANITIZER)
--- chrome/browser/prerender/prerender_config.cc
+++ chrome/browser/prerender/prerender_config.cc
@@ -5,18 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/prerender/prerender_config.h"
 
-namespace {
-
-// The test_timeouts.cc defines general timeout multipliers for Asan builds.
-// Increase the timeout similarly here.
-#if defined(ADDRESS_SANITIZER)
-constexpr int kTimeToLiveMinutes = 10;
-#else
-constexpr int kTimeToLiveMinutes = 5;
-#endif
-
-}  // namespace
-
 namespace prerender {
 
 Config::Config()
@@ -25,7 +13,7 @@ Config::Config()
       max_link_concurrency_per_launcher(1),
       rate_limit_enabled(true),
       max_wait_to_launch(base::TimeDelta::FromMinutes(4)),
-      time_to_live(base::TimeDelta::FromMinutes(kTimeToLiveMinutes)),
+      time_to_live(base::TimeDelta::FromMinutes(5)),
       abandon_time_to_live(base::TimeDelta::FromSeconds(3)),
       default_tab_bounds(640, 480),
       is_overriding_user_agent(false) {}
