5681a9be5d1d9b287e77912740f178e0259659e5
Erik Luo <luoe@chromium.org>
2018-01-23 05:22:39 +0000

    DevTools: add console viewport visibility test cleanup
    Besides minor cleanup, in this CL:
    - Viewport selection.js and stick-to-bottom.js tests no longer
      do "verifyViewportIsTallEnough". This sort of test is now covered
      by "console-viewport-indices.js"
    - The actual visible indices can be determined in tests with a
      helper that uses getBoundingClientRect()
    
    Bug: 803537
    Change-Id: I7642051b6a5eb358c536ae29568ec9de91a02a8b
    Reviewed-on: https://chromium-review.googlesource.com/874778
    Reviewed-by: Joel Einbinder <einbinder@chromium.org>
    Reviewed-by: Andrey Lushnikov <lushnikov@chromium.org>
    Commit-Queue: Erik Luo <luoe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531158}

--- third_party/WebKit/LayoutTests/http/tests/devtools/console/console-viewport-selection.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/console/console-viewport-selection.js
@@ -26,22 +26,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   var viewportMessagesCount;
 
   var testSuite = [
-    function verifyViewportIsTallEnough(next) {
+    function testSelectionSingleLineText(next) {
       viewport.invalidate();
       viewport.forceScrollItemToBeFirst(0);
       viewportMessagesCount = viewport.lastVisibleIndex() - viewport.firstVisibleIndex() + 1;
-      if (viewportMessagesCount < minimumViewportMessagesCount) {
-        TestRunner.addResult(String.sprintf(
-            'Test cannot be run as viewport is not tall enough. It is required to contain at least %d messages, but %d only fit',
-            minimumViewportMessagesCount, viewportMessagesCount));
-        TestRunner.completeTest();
-        return;
-      }
-      TestRunner.addResult(String.sprintf('Viewport contains %d messages', viewportMessagesCount));
-      next();
-    },
-
-    function testSelectionSingleLineText(next) {
       selectMessages(middleMessage, 2, middleMessage, 7);
       dumpSelectionText();
       dumpViewportRenderedItems();
@@ -77,7 +65,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     },
 
     function testHalfScrollSelectionDown(next) {
-      viewport.forceScrollItemToBeFirst(middleMessage - viewportMessagesCount + 1);
+      viewport.forceScrollItemToBeLast(middleMessage);
       dumpSelectionModel();
       dumpViewportRenderedItems();
       next();
--- third_party/WebKit/LayoutTests/http/tests/devtools/console/console-viewport-stick-to-bottom.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/console/console-viewport-stick-to-bottom.js
@@ -10,42 +10,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   await TestRunner.evaluateInPagePromise(`
       function populateConsoleWithMessages(count)
       {
-          for (var i = 0; i < count - 1; ++i)
+          for (var i = 0; i < count; ++i)
               console.log("Multiline\\nMessage #" + i);
-          console.log("hello %cworld", "color: blue");
       }
 
-      //# sourceURL=console-viewport-selection.js
+      //# sourceURL=console-viewport-stick-to-bottom.js
     `);
 
   var viewportHeight = 200;
   ConsoleTestRunner.fixConsoleViewportDimensions(600, viewportHeight);
   var consoleView = Console.ConsoleView.instance();
   var viewport = consoleView._viewport;
-  const minimumViewportMessagesCount = 10;
   const messagesCount = 150;
-  const middleMessage = messagesCount / 2;
-  var viewportMessagesCount;
 
   logMessagesToConsole(messagesCount, () => TestRunner.runTestSuite(testSuite));
 
   var testSuite = [
-    function verifyViewportIsTallEnough(next) {
-      viewport.invalidate();
-      viewport.forceScrollItemToBeFirst(0);
-      viewportMessagesCount = viewport.lastVisibleIndex() - viewport.firstVisibleIndex() + 1;
-      if (viewportMessagesCount < minimumViewportMessagesCount) {
-        TestRunner.addResult(String.sprintf(
-            'Test cannot be run as viewport is not tall enough. It is required to contain at least %d messages, but %d only fit',
-            minimumViewportMessagesCount, viewportMessagesCount));
-        TestRunner.completeTest();
-        return;
-      }
-      TestRunner.addResult(String.sprintf('Viewport contains %d messages', viewportMessagesCount));
-      next();
-    },
-
     function testScrollViewportToBottom(next) {
+      viewport.invalidate();
       consoleView._immediatelyScrollToBottom();
       dumpAndContinue(next);
     },
@@ -94,8 +76,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       ConsoleTestRunner.evaluateInConsole('1 + 1');
 
       /**
-             * @param {boolean} muted
-             */
+       * @param {boolean} muted
+       */
       function onMessageAdded(muted) {
         TestRunner.addResult('New messages were muted: ' + muted);
         TestRunner.addSniffer(
@@ -105,8 +87,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       }
 
       /**
-             * @param {boolean} muted
-             */
+       * @param {boolean} muted
+       */
       function onMouseUpScheduledRefresh(muted) {
         TestRunner.addResult('Refresh was scheduled after dirty state');
       }
