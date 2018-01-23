98ac738a79aa0324a769a3fcd843150b213cbce1
F#m <malaykeshav@chromium.org>
2018-01-23 00:27:49 +0000

    Updates system_display.js based on updated IDL
    This patch updates the externs for 'system_display.js' for display zoom
    factor.
    This is a follow up patch to
    https://chromium-review.googlesource.com/c/chromium/src/+/864831
    
    Bug: 790723
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I629f333d0965ad34e34c9d83adbffef1466fa17a
    Component: system display, extern, IDL
    Reviewed-on: https://chromium-review.googlesource.com/879263
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Malay Keshav <malaykeshav@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531082}

--- third_party/closure_compiler/externs/system_display.js
+++ third_party/closure_compiler/externs/system_display.js
@@ -129,7 +129,8 @@ chrome.system.display.DisplayLayout;
  *   workArea: !chrome.system.display.Bounds,
  *   modes: !Array<!chrome.system.display.DisplayMode>,
  *   hasTouchSupport: boolean,
- *   hasAccelerometerSupport: boolean
+ *   hasAccelerometerSupport: boolean,
+ *   displayZoomFactor: number
  * }}
  * @see https://developer.chrome.com/extensions/system.display#type-DisplayUnitInfo
  */
@@ -144,7 +145,8 @@ chrome.system.display.DisplayUnitInfo;
  *   rotation: (number|undefined),
  *   boundsOriginX: (number|undefined),
  *   boundsOriginY: (number|undefined),
- *   displayMode: (!chrome.system.display.DisplayMode|undefined)
+ *   displayMode: (!chrome.system.display.DisplayMode|undefined),
+ *   displayZoomFactor: (number|undefined)
  * }}
  * @see https://developer.chrome.com/extensions/system.display#type-DisplayProperties
  */
