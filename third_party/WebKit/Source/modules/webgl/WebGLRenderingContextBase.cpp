92d177fb83630b4fb69724a1834596adac368b57
Daniel Cheng <dcheng@chromium.org>
2018-01-23 01:23:44 +0000

    Use WTF::Vector for canvas resource provider cache.
    Using std::unique_ptr<std::unique_ptr<T>[]> seems to make MSVC 2017
    unhappy if T has a pure virtual function. See https://goo.gl/3DzEZK.
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I5afea59add7e6a9163865f5fd888940d24726092
    Reviewed-on: https://chromium-review.googlesource.com/865756
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531107}

--- third_party/WebKit/Source/modules/webgl/WebGLRenderingContextBase.cpp
+++ third_party/WebKit/Source/modules/webgl/WebGLRenderingContextBase.cpp
@@ -7605,17 +7605,14 @@ String WebGLRenderingContextBase::EnsureNotNull(const String& text) const {
 }
 
 WebGLRenderingContextBase::LRUCanvasResourceProviderCache::
-    LRUCanvasResourceProviderCache(int capacity)
-    : resource_providers_(
-          std::make_unique<std::unique_ptr<CanvasResourceProvider>[]>(
-              capacity)),
-      capacity_(capacity) {}
+    LRUCanvasResourceProviderCache(size_t capacity)
+    : resource_providers_(capacity) {}
 
 CanvasResourceProvider* WebGLRenderingContextBase::
     LRUCanvasResourceProviderCache::GetCanvasResourceProvider(
         const IntSize& size) {
-  int i;
-  for (i = 0; i < capacity_; ++i) {
+  size_t i;
+  for (i = 0; i < resource_providers_.size(); ++i) {
     CanvasResourceProvider* resource_provider = resource_providers_[i].get();
     if (!resource_provider)
       break;
@@ -7629,7 +7626,7 @@ CanvasResourceProvider* WebGLRenderingContextBase::
       size, CanvasResourceProvider::kSoftwareResourceUsage));
   if (!temp)
     return nullptr;
-  i = std::min(capacity_ - 1, i);
+  i = std::min(resource_providers_.size() - 1, i);
   resource_providers_[i] = std::move(temp);
 
   CanvasResourceProvider* resource_provider = resource_providers_[i].get();
@@ -7638,8 +7635,8 @@ CanvasResourceProvider* WebGLRenderingContextBase::
 }
 
 void WebGLRenderingContextBase::LRUCanvasResourceProviderCache::BubbleToFront(
-    int idx) {
-  for (int i = idx; i > 0; --i)
+    size_t idx) {
+  for (size_t i = idx; i > 0; --i)
     resource_providers_[i].swap(resource_providers_[i - 1]);
 }
 
