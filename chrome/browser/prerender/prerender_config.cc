719ee280e6b5d2d05e5d15bc8d241665ec58b3ad
Egor Pasko <pasko@chromium.org>
2018-01-23 10:42:40 +0000

    prerender: increase timeout for Asan
    Generally ASAN-instrumented builds run slower and hit various timeouts
    in tests, so we scale the test timeouts for it. We have not been scaling
    timeouts for PrerenderContents lifetime. The bug below lists at least
    one failure due to this issue, other logs/evidence is not easy to get.
    
    This patch changes two things:
    1. Increases PrerenderContents lifetime 2x when running with ASAN
    2. Re-enables all tests under ASAN to start collecting new flake
    
    The change (1) may cause more memory consumption to those rare users who
    force prerender=ON with about:flags in those occasional Dev/Canary
    builds that ship with ASAN enabled. This situation seems rare and not
    worth plumbing testing overrides around prerender_config.
    
    Bug: 708158
    Change-Id: I8df6e54f5dac1247f6bff1125a8bfd77d51bcc7b
    Reviewed-on: https://chromium-review.googlesource.com/878802
    Reviewed-by: Matthew Cary <mattcary@chromium.org>
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531201}

--- chrome/browser/prerender/prerender_config.cc
+++ chrome/browser/prerender/prerender_config.cc
@@ -5,18 +5,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/prerender/prerender_config.h"
 
+namespace {
+
+// The test_timeouts.cc defines general timeout multipliers for Asan builds.
+// Increase the timeout similarly here.
+#if defined(ADDRESS_SANITIZER)
+constexpr int kTimeToLiveMinutes = 10;
+#else
+constexpr int kTimeToLiveMinutes = 5;
+#endif
+
+}  // namespace
+
 namespace prerender {
 
-Config::Config() : max_bytes(150 * 1024 * 1024),
-                   max_link_concurrency(1),
-                   max_link_concurrency_per_launcher(1),
-                   rate_limit_enabled(true),
-                   max_wait_to_launch(base::TimeDelta::FromMinutes(4)),
-                   time_to_live(base::TimeDelta::FromMinutes(5)),
-                   abandon_time_to_live(base::TimeDelta::FromSeconds(3)),
-                   default_tab_bounds(640, 480),
-                   is_overriding_user_agent(false) {
-}
+Config::Config()
+    : max_bytes(150 * 1024 * 1024),
+      max_link_concurrency(1),
+      max_link_concurrency_per_launcher(1),
+      rate_limit_enabled(true),
+      max_wait_to_launch(base::TimeDelta::FromMinutes(4)),
+      time_to_live(base::TimeDelta::FromMinutes(kTimeToLiveMinutes)),
+      abandon_time_to_live(base::TimeDelta::FromSeconds(3)),
+      default_tab_bounds(640, 480),
+      is_overriding_user_agent(false) {}
 
 Config::~Config() { }
 
