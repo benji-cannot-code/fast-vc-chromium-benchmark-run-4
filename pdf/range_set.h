8dd47a32b61268d08de24b4a12c2f361f1ebc443
Lei Zhang <thestig@chromium.org>
2018-01-23 19:56:29 +0000

    Make the pdf build target a component.
    The code in pdf/ can be a GN component instead of a static_library.
    Add a PDF_EXPORT macro and set boundaries on what can be used from
    external callers.
    
    Change-Id: Ib855736f145c5708b8c62c7fd0e3622827bcc623
    Reviewed-on: https://chromium-review.googlesource.com/879853
    Reviewed-by: dsinclair <dsinclair@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531314}

--- pdf/range_set.h
+++ pdf/range_set.h
@@ -12,11 +12,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 #include <string>
 
+#include "pdf/pdf_export.h"
 #include "ui/gfx/range/range.h"
 
 namespace chrome_pdf {
 
-class RangeSet {
+class PDF_EXPORT RangeSet {
  public:
   RangeSet();
   explicit RangeSet(const gfx::Range& range);
@@ -72,7 +73,7 @@ class RangeSet {
 
 }  // namespace chrome_pdf
 
-std::ostream& operator<<(std::ostream& os,
-                         const chrome_pdf::RangeSet& range_set);
+PDF_EXPORT std::ostream& operator<<(std::ostream& os,
+                                    const chrome_pdf::RangeSet& range_set);
 
 #endif  // PDF_RANGE_SET_H_
