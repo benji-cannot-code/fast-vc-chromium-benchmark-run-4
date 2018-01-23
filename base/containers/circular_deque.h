36d9706f2a2ad904c55b95504164997a88e5c686
Gabriel Charette <gab@chromium.org>
2018-01-23 17:12:57 +0000

    Revert "Reland "Make many base:: classes constexpr default constructible""
    This reverts commit 2e4eab147b94a947ed4650234637b85802000b2b.
    
    Reason for revert: Testing as culprit for crbug.com/803517
    
    Original change's description:
    > Reland "Make many base:: classes constexpr default constructible"
    > 
    > This is a reland of d74c23a26e423c3fb70b6216f5dcd2acb85f9157
    > Original change's description:
    > > Make many base:: classes constexpr default constructible
    > 
    > Same CL but extracted scoped_refptr changes to separate CL.
    > 
    > >
    > > Bug: 800760
    > > Change-Id: Ib713771347283825ff53a8517b38a8abccd9d1bb
    > > Reviewed-on: https://chromium-review.googlesource.com/860157
    > > Commit-Queue: Gabriel Charette <gab@chromium.org>
    > > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#528805}
    > 
    > Bug: 800760
    > Change-Id: Ia989d978f632075c21f11cd81687f52b9e2bd2ca
    > 
    > TBR=dcheng@chromium.org
    > 
    > Change-Id: Ia989d978f632075c21f11cd81687f52b9e2bd2ca
    > Reviewed-on: https://chromium-review.googlesource.com/863622
    > Reviewed-by: Gabriel Charette <gab@chromium.org>
    > Commit-Queue: Gabriel Charette <gab@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#528934}
    
    TBR=dcheng@chromium.org,gab@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 800760, 803517
    Change-Id: I1c2e3be23dbde603befffa0f7ec2153e0e5a1851
    Reviewed-on: https://chromium-review.googlesource.com/881342
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531267}

--- base/containers/circular_deque.h
+++ base/containers/circular_deque.h
@@ -418,7 +418,7 @@ class circular_deque {
   // ---------------------------------------------------------------------------
   // Constructor
 
-  constexpr circular_deque() = default;
+  circular_deque() = default;
 
   // Constructs with |count| copies of |value| or default constructed version.
   circular_deque(size_type count) { resize(count); }
