825cbf4dbef076fd58dd7311df1b061d3b855fc6
Joel Einbinder <einbinder@chromium.org>
2018-01-23 01:53:44 +0000

    DevTools: Don't steal focus after editing
    Bug: 791494
    Change-Id: Icd5f9a5f379758b481f373af71b17a6fa8f3feeb
    Reviewed-on: https://chromium-review.googlesource.com/876929
    Reviewed-by: Andrey Lushnikov <lushnikov@chromium.org>
    Commit-Queue: Andrey Lushnikov <lushnikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531126}

--- third_party/WebKit/Source/devtools/front_end/elements/StylesSidebarPane.js
+++ third_party/WebKit/Source/devtools/front_end/elements/StylesSidebarPane.js
@@ -1806,7 +1806,7 @@ Elements.StylePropertiesSection = class {
     this.element.style.removeProperty('contain');
     this.element.removeEventListener('input', this._scheduleHeightUpdate, true);
     this._editing = false;
-    if (this._parentPane.element.hasFocus())
+    if (this._parentPane.element === this._parentPane.element.ownerDocument.deepActiveElement())
       this.element.focus();
   }
 };
@@ -2752,6 +2752,7 @@ Elements.StylePropertyTreeElement = class extends UI.TreeElement {
    * @param {string} moveDirection
    */
   _editingCommitted(userInput, context, moveDirection) {
+    var hadFocus = this._parentPane.element.hasFocus();
     this._removePrompt();
     this.editingEnded(context);
     var isEditingName = context.isEditingName;
@@ -2783,7 +2784,8 @@ Elements.StylePropertyTreeElement = class extends UI.TreeElement {
         (isPropertySplitPaste || moveToOther || (!moveDirection && !isEditingName) || (isEditingName && blankInput));
     var section = /** @type {!Elements.StylePropertiesSection} */ (this.section());
     if (((userInput !== context.previousContent || isDirtyViaPaste) && !this._newProperty) || shouldCommitNewProperty) {
-      this._parentPane.element.focus();
+      if (hadFocus)
+        this._parentPane.element.focus();
       section._afterUpdate = moveToNextCallback.bind(this, this._newProperty, !blankInput, section);
       var propertyText;
       if (blankInput || (this._newProperty && this.valueElement.textContent.isWhitespace())) {
