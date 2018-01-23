6227010e74fd61e8305c82e8298ebf02eca23e7e
Daniel Nicoara <dnicoara@chromium.org>
2018-01-23 15:53:52 +0000

    [Chromecast] Fix WindowTreeHost rotation transform
    Set the transform only on the WTH window since the WTH itself already
    has its bounds set correctly at creation and SetRootTransform() will
    also update the WTH bounds.
    
    rotations.
    
    Bug: b/70711217
    Test: Ran cast_shell and verified UI shows correctly with and without
    Change-Id: I81f48aec9278ea6fc2361a3740c2af50c6eeea09
    Reviewed-on: https://chromium-review.googlesource.com/879226
    Reviewed-by: Sergey Volk <servolk@chromium.org>
    Commit-Queue: Daniel Nicoara <dnicoara@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531248}

--- chromecast/graphics/cast_window_manager_aura.cc
+++ chromecast/graphics/cast_window_manager_aura.cc
@@ -31,10 +31,7 @@ gfx::Transform GetPrimaryDisplayRotationTransform() {
     case display::Display::ROTATE_0:
       break;
     case display::Display::ROTATE_90:
-      // TODO(dnicoara): Figure out why this is not correct.
-      // rotation.Translate(display.height(), 0);
-      rotation.Translate(
-          (display.bounds().height() + display.bounds().width()) / 2, 0);
+      rotation.Translate(display.bounds().height(), 0);
       rotation.Rotate(90);
       break;
     case display::Display::ROTATE_180:
@@ -42,10 +39,7 @@ gfx::Transform GetPrimaryDisplayRotationTransform() {
       rotation.Rotate(180);
       break;
     case display::Display::ROTATE_270:
-      // TODO(dnicoara): Figure out why this is not correct.
-      // rotation.Translate(0, display.width());
-      rotation.Translate(
-          0, (display.bounds().height() + display.bounds().width()) / 2);
+      rotation.Translate(0, display.bounds().width());
       rotation.Rotate(270);
       break;
   }
@@ -234,7 +228,8 @@ void CastWindowManagerAura::Setup() {
       new CastWindowTreeHost(enable_input_, gfx::Rect(display_size)));
   window_tree_host_->InitHost();
   window_tree_host_->window()->SetLayoutManager(new CastLayoutManager());
-  window_tree_host_->SetRootTransform(GetPrimaryDisplayRotationTransform());
+  window_tree_host_->window()->SetTransform(
+      GetPrimaryDisplayRotationTransform());
 
   // Allow seeing through to the hardware video plane:
   window_tree_host_->compositor()->SetBackgroundColor(SK_ColorTRANSPARENT);
