e6917da4b3b9f5d097b198964bcdcc76a1c4d190
Ryan Landay <rlanday@chromium.org>
2018-01-23 20:26:45 +0000

    Fix bug composing text with IME immediately after non-editable element
    When an IME tries to set a composition immediately after a non-editable element
    inside an editable region, it's possible for the selection base to get stuck
    inside the non-editable region, in which case the attempt to replace the
    selection fails. The fix is to pass the kCanSkipOverEditingBoundary flag when we
    call MostForwardCaretPosition() on the selection base so we can find the actual
    editable region.
    
    Bug: 790777,801441
    Change-Id: I1014901742671f6fbb153c7117eaa1b271a265f7
    Reviewed-on: https://chromium-review.googlesource.com/879839
    Commit-Queue: Ryan Landay <rlanday@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531330}

--- third_party/WebKit/Source/core/editing/ime/InputMethodControllerTest.cpp
+++ third_party/WebKit/Source/core/editing/ime/InputMethodControllerTest.cpp
@@ -14,18 +14,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/editing/FrameSelection.h"
 #include "core/editing/SelectionTemplate.h"
 #include "core/editing/markers/DocumentMarkerController.h"
+#include "core/editing/testing/EditingTestBase.h"
 #include "core/events/MouseEvent.h"
 #include "core/frame/LocalFrame.h"
 #include "core/frame/LocalFrameView.h"
 #include "core/frame/Settings.h"
 #include "core/html/forms/HTMLInputElement.h"
 #include "core/html/forms/HTMLTextAreaElement.h"
-#include "core/testing/PageTestBase.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace blink {
 
-class InputMethodControllerTest : public PageTestBase {
+class InputMethodControllerTest : public EditingTestBase {
  protected:
   InputMethodController& Controller() {
     return GetFrame().GetInputMethodController();
@@ -3162,6 +3162,30 @@ TEST_F(InputMethodControllerTest, ExecCommandDuringComposition) {
 
   // "world" should be bold.
   EXPECT_EQ("hello<b>world</b>", div->InnerHTMLAsString());
+}
+
+TEST_F(InputMethodControllerTest, SetCompositionAfterNonEditableElement) {
+  GetFrame().Selection().SetSelectionAndEndTyping(
+      SetSelectionTextToBody("<div id='sample' contenteditable='true'>"
+                             "<span contenteditable='false'>a</span>|b</div>"));
+  Element* const div = GetDocument().getElementById("sample");
+  div->focus();
+
+  // Open a composition and insert some text.
+  Controller().SetComposition(String::FromUTF8("c"), Vector<ImeTextSpan>(), 1,
+                              1);
+
+  // Add some more text to the composition.
+  Controller().SetComposition(String::FromUTF8("cd"), Vector<ImeTextSpan>(), 2,
+                              2);
+
+  EXPECT_EQ(
+      "<div contenteditable=\"true\" id=\"sample\">"
+      "<span contenteditable=\"false\">a</span>^cd|b</div>",
+      GetSelectionTextFromBody(
+          SelectionInDOMTree::Builder()
+              .SetBaseAndExtent(Controller().CompositionEphemeralRange())
+              .Build()));
 }
 
 }  // namespace blink
