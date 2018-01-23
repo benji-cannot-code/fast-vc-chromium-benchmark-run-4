43173af440ea63e974272b4b8e33b97e1c7f17b2
wutao <wutao@chromium.org>
2018-01-23 00:23:44 +0000

    cros: Implement infra code to gather shortcuts metadata - KSV part 1
    This cl is the first part of the Keyboard Shortcut Viewer (KSV) backend
    to demostrate how to gather and organize shortcuts metadata.
    
    Changes:
    1. //ui/chromeos/ksv/* for ksv logic, metadata, strings, views.
    2. //chrome/browser/ui/ash/ksv/* with limited deps to open ksv and test.
    
    Bug: 793997, 768932
    Test: KeyboardShortcutViewerMetadataTest
    Change-Id: I0b728e004b6af74c8cb532f4ac195d706fc4b87c
    Reviewed-on: https://chromium-review.googlesource.com/863043
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531078}

--- ash/new_window_controller.h
+++ ash/new_window_controller.h
@@ -35,6 +35,7 @@ class ASH_EXPORT NewWindowController : public mojom::NewWindowController {
   void OpenCrosh();
   void OpenGetHelp();
   void RestoreTab();
+  void ShowKeyboardShortcutViewer();
   void ShowTaskManager();
   void OpenFeedbackPage();
 
