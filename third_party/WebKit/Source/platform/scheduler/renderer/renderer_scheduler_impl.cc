a59292bb00185b8adc12ced1419caad8fd09de56
Hajime Hoshi <hajimehoshi@chromium.org>
2018-01-23 08:44:53 +0000

    media: Use per-frame task runners at RenderMediaLog
    This is part of efforts to replace base::ThreadTaskRunnerHandle::Get()
    and SequencedTaskRunnerHandle::Get() with other appropriate task runners
    in the renderer.
    
    Bug: 786332
    Change-Id: Ia812986308a92b4169548bb788ae14b9905f7010
    Reviewed-on: https://chromium-review.googlesource.com/872832
    Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531186}

--- third_party/WebKit/Source/platform/scheduler/renderer/renderer_scheduler_impl.cc
+++ third_party/WebKit/Source/platform/scheduler/renderer/renderer_scheduler_impl.cc
@@ -177,6 +177,8 @@ const char* TaskTypeToString(TaskType task_type) {
       return "InternalWebCrypto";
     case TaskType::kInternalIndexedDB:
       return "InternalIndexedDB";
+    case TaskType::kInternalMedia:
+      return "InternalMedia";
     case TaskType::kCount:
       return "Count";
   }
