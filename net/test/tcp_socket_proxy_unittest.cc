1e3fe33649b8aaa48b83337029f7f02f387e8f23
Rohit Rao <rohitrao@chromium.org>
2018-01-23 19:46:31 +0000

    Disables TcpSocketProxyTest.DisconnectServer and DisconnectClient on iOS
    These tests were originally only run when the remote test server was in
    use, but that restriction was relaxed in
    https://chromium-review.googlesource.com/c/chromium/src/+/858289.
    
    The tests hang when run on iOS, so this CL disables them.
    
    BUG=804429
    
    Change-Id: I73cabb162ffd9439b367793d003c0df00f1011af
    Reviewed-on: https://chromium-review.googlesource.com/879682
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531307}

--- net/test/tcp_socket_proxy_unittest.cc
+++ net/test/tcp_socket_proxy_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/message_loop/message_loop.h"
 #include "base/threading/thread.h"
+#include "build/build_config.h"
 #include "net/base/io_buffer.h"
 #include "net/base/test_completion_callback.h"
 #include "net/socket/tcp_client_socket.h"
@@ -130,7 +131,13 @@ TEST_F(TcpSocketProxyTest, TwoConnections) {
 
 // Close socket on the server side and verify that it's closed on the client
 // side.
-TEST_F(TcpSocketProxyTest, DisconnectServer) {
+// TODO(crbug.com/804429): This test hangs occasionally on iOS.
+#if defined(OS_IOS)
+#define MAYBE_DisconnectServer DISABLED_DisconnectServer
+#else
+#define MAYBE_DisconnectServer DisconnectServer
+#endif
+TEST_F(TcpSocketProxyTest, MAYBE_DisconnectServer) {
   std::unique_ptr<StreamSocket> client_socket;
   std::unique_ptr<StreamSocket> server_socket;
   MakeConnection(&client_socket, &server_socket);
@@ -140,7 +147,13 @@ TEST_F(TcpSocketProxyTest, DisconnectServer) {
 
 // Close socket on the client side and verify that it's closed on the server
 // side.
-TEST_F(TcpSocketProxyTest, DisconnectClient) {
+// TODO(crbug.com/804429): This test hangs occasionally on iOS.
+#if defined(OS_IOS)
+#define MAYBE_DisconnectClient DISABLED_DisconnectClient
+#else
+#define MAYBE_DisconnectClient DisconnectClient
+#endif
+TEST_F(TcpSocketProxyTest, MAYBE_DisconnectClient) {
   std::unique_ptr<StreamSocket> client_socket;
   std::unique_ptr<StreamSocket> server_socket;
   MakeConnection(&client_socket, &server_socket);
