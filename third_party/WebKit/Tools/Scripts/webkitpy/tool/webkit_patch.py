6cf0b6010520689531cc7fce12edf5ca289ee4ce
Quinten Yearsley <qyearsley@chromium.org>
2018-01-23 22:07:55 +0000

    Remove webkit-patch rebaseline-server.
    This has probably be unused for sometime.
    We proposed removing it early last year:
    https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/J-oJz1zL-HA
    
    Relatedly, webkit-patch layout-tests-server can be removed in another CL.
    
    Bug: 704384
    Change-Id: I706835aac9cc0f7c1d5c80b5fffb6966352da556
    Reviewed-on: https://chromium-review.googlesource.com/847641
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Commit-Queue: Quinten Yearsley <qyearsley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531366}

--- third_party/WebKit/Tools/Scripts/webkitpy/tool/webkit_patch.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/tool/webkit_patch.py
@@ -56,7 +56,6 @@ from webkitpy.tool.commands.queries import PrintExpectations
 from webkitpy.tool.commands.rebaseline import Rebaseline
 from webkitpy.tool.commands.rebaseline import RebaselineExpectations
 from webkitpy.tool.commands.rebaseline_cl import RebaselineCL
-from webkitpy.tool.commands.rebaseline_server import RebaselineServer
 from webkitpy.tool.commands.rebaseline_test import RebaselineTest
 
 
@@ -92,7 +91,6 @@ class WebKitPatch(Host):
             Rebaseline(),
             RebaselineCL(),
             RebaselineExpectations(),
-            RebaselineServer(),
             RebaselineTest(),
         ]
         self.help_command = HelpCommand(tool=self)
