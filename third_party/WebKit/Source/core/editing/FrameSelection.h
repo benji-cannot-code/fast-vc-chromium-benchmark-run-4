aa403ab13f85aeb8a4f8139139e51e84c7da0cc6
Yoshifumi Inoue <yosin@chromium.org>
2018-01-23 03:36:52 +0000

    Introduce FrameSelection::CharacterIndexForPoint() for WebLocalFrameImpl
    This patch introduces |FrameSelection::CharacterIndexForPoint()| for
    |WebLocalFrameImpl| as a preparation of moving |LocalFrame::RangeForPoint()|
    into "FrameSelection.cpp" to reduce usage of |VisiblePosition| out side
    editing for improving code health.
    
    Bug: 657237
    Change-Id: Idea5ef3ea3806fcf4b7538093fd4540666b0efbe
    Reviewed-on: https://chromium-review.googlesource.com/877207
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531145}

--- third_party/WebKit/Source/core/editing/FrameSelection.h
+++ third_party/WebKit/Source/core/editing/FrameSelection.h
@@ -81,6 +81,7 @@ class CORE_EXPORT FrameSelection final
   Document& GetDocument() const;
   LocalFrame* GetFrame() const { return frame_; }
   Element* RootEditableElementOrDocumentElement() const;
+  size_t CharacterIndexForPoint(const IntPoint&) const;
 
   // An implementation of |WebFrame::moveCaretSelection()|
   void MoveCaretSelection(const IntPoint&);
