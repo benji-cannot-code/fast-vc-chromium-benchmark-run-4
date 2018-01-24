eea9355f079c2593a2713e402e2599b52f8061c2
kylechar <kylechar@chromium.org>
2018-01-24 23:57:02 +0000

    Remove ui::Gpu from SyncCallRestrictions friends.
    I had changed ui::Gpu to not use ScopedAllowSyncCall previously. Remove
    ui::Gpu from SyncCallRestrictions friends since it's not needed.
    
    Bug: none
    Change-Id: I8e0c80a87970882b83aec8de046e17fdb90e016f
    Reviewed-on: https://chromium-review.googlesource.com/883766
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531750}

--- services/ui/public/cpp/gpu/gpu.cc
+++ services/ui/public/cpp/gpu/gpu.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
 #include "gpu/command_buffer/common/scheduling_priority.h"
-#include "mojo/public/cpp/bindings/sync_call_restrictions.h"
 #include "services/service_manager/public/cpp/connector.h"
 #include "services/ui/public/cpp/gpu/client_gpu_memory_buffer_manager.h"
 #include "services/ui/public/cpp/gpu/context_provider_command_buffer.h"
