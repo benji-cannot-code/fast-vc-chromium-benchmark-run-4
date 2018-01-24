825004b1a6566645f4a72efa493e17307eb8db9a
jdoerrie <jdoerrie@chromium.org>
2018-01-24 03:45:58 +0000

    [bluetooth] Implement SetPowered() on Mac
    This change implements SetPowered() for BluetoothAdapterMac.
    It uses undocumented IOBluetooth APIs in order to achieve this.
    
    Bug: 364359
    Change-Id: I12f4bddc765870d828f4e9e17dc63b5b22580b65
    Reviewed-on: https://chromium-review.googlesource.com/854856
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Reviewed-by: Conley Owens <cco3@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531415}

--- device/bluetooth/bluetooth_adapter_mac_unittest.mm
+++ device/bluetooth/bluetooth_adapter_mac_unittest.mm
@@ -30,6 +30,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <IOBluetooth/IOBluetooth.h>
 #endif  // defined(OS_IOS)
 
+// List of undocumented IOBluetooth APIs used for BluetoothAdapterMac.
+extern "C" {
+int IOBluetoothPreferenceGetControllerPowerState();
+void IOBluetoothPreferenceSetControllerPowerState(int state);
+}
+
 namespace {
 // |kTestHashAddress| is the hash corresponding to identifier |kTestNSUUID|.
 const char kTestNSUUID[] = "00000000-1111-2222-3333-444444444444";
@@ -142,6 +148,15 @@ class BluetoothAdapterMacTest : public testing::Test {
   int callback_count_;
   int error_callback_count_;
 };
+
+// Test if private IOBluetooth APIs are callable on all supported macOS
+// versions.
+TEST_F(BluetoothAdapterMacTest, IOBluetoothPrivateAPIs) {
+  // Obtain current power state, toggle it, and reset it to it's original value.
+  int previous_state = IOBluetoothPreferenceGetControllerPowerState();
+  IOBluetoothPreferenceSetControllerPowerState(!previous_state);
+  IOBluetoothPreferenceSetControllerPowerState(previous_state);
+}
 
 TEST_F(BluetoothAdapterMacTest, Poll) {
   PollAdapter();
--- device/bluetooth/test/bluetooth_test_mac.mm
+++ device/bluetooth/test/bluetooth_test_mac.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <CoreBluetooth/CoreBluetooth.h>
 #include <stdint.h>
 
+#include "base/bind.h"
 #import "base/mac/foundation_util.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/sys_string_conversions.h"
@@ -138,6 +139,9 @@ void BluetoothTestMac::InitWithFakeAdapter() {
     mock_central_manager_->get().bluetoothTestMac = this;
     [mock_central_manager_->get() setState:CBCentralManagerStatePoweredOn];
     adapter_mac_->SetCentralManagerForTesting((id)mock_central_manager_->get());
+    adapter_mac_->SetPowerStateFunctionForTesting(
+        base::BindRepeating(&BluetoothTestMac::SetMockControllerPowerState,
+                            base::Unretained(this)));
   }
 }
 
@@ -561,6 +565,33 @@ void BluetoothTestMac::SimulateGattDescriptorReadNSNumberMac(
     short value) {
   NSNumber* number = [NSNumber numberWithShort:value];
   [GetCBMockDescriptor(descriptor) simulateReadWithValue:number error:nil];
+}
+
+void BluetoothTestMac::SetMockControllerPowerState(int powered) {
+  // We are posting a task so that the state only gets updated in the next cycle
+  // and pending callbacks are not executed immediately.
+  adapter_mac_->ui_task_runner_->PostTask(
+      FROM_HERE,
+      base::BindOnce(
+          [](base::WeakPtr<BluetoothAdapterMac> adapter_mac, int powered) {
+            // Guard against deletion of the adapter.
+            if (!adapter_mac)
+              return;
+
+            auto* mock_central_manager =
+                base::mac::ObjCCastStrict<MockCentralManager>(
+                    adapter_mac->GetCentralManager());
+            [mock_central_manager
+                setState:powered ? CBCentralManagerStatePoweredOn
+                                 : CBCentralManagerStatePoweredOff];
+            [mock_central_manager.delegate
+                centralManagerDidUpdateState:adapter_mac->GetCentralManager()];
+            // On real devices, the Bluetooth classic code will dispatch the
+            // AdapterPoweredChanged event. Test code does not fake Bluetooth
+            // classic behavior so we dispatch the event directly.
+            adapter_mac->NotifyAdapterPoweredChanged(powered);
+          },
+          adapter_mac_->weak_ptr_factory_.GetWeakPtr(), powered));
 }
 
 void BluetoothTest::AddServicesToDeviceMac(
