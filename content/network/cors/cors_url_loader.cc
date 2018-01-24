07d1961408e100e30da3c94cfffa6409396603e4
Takashi Toyoshima <toyoshim@chromium.org>
2018-01-24 06:08:41 +0000

    OOR-CORS: Move CORSURLLoader from content/renderer to content/network
    This patch moves content::CORSURLLoader from content/renderer to
    the browser process, and hooks up URLLoaderFactoryImpl to use the
    CORSURLLoader when kOutOfBlinkCORS feature is enabled.
    
    With this patch, "chrome --enable-features=OutOfBlinkCORS" will handle
    basic CORS checks in the browser process.
    
    Test: ./Tools/Scripts/run-webkit-tests virtual/outofblink-cors
    Test: ./out/Debug/chrome --enable-features=OutOfBlinkCORS
    Test: ./out/Debug/content_unittests --gtest_filter="CORS*.*"
    Bug: 736308
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: If646da1f022ceb040a6069ef8942c3909b7f01a1
    Reviewed-on: https://chromium-review.googlesource.com/866657
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Takeshi Yoshino <tyoshino@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531454}

--- content/network/cors/cors_url_loader.cc
+++ content/network/cors/cors_url_loader.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "content/renderer/loader/cors_url_loader.h"
+#include "content/network/cors/cors_url_loader.h"
 
 #include "content/public/common/origin_util.h"
 #include "content/public/common/resource_type.h"
--- content/network/cors/cors_url_loader.h
+++ content/network/cors/cors_url_loader.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CONTENT_RENDERER_LOADER_CORS_URL_LOADER_H_
-#define CONTENT_RENDERER_LOADER_CORS_URL_LOADER_H_
+#ifndef CONTENT_NETWORK_CORS_CORS_URL_LOADER_H_
+#define CONTENT_NETWORK_CORS_CORS_URL_LOADER_H_
 
 #include "content/common/content_export.h"
 #include "mojo/public/cpp/bindings/binding.h"
@@ -101,4 +101,4 @@ class CONTENT_EXPORT CORSURLLoader : public network::mojom::URLLoader,
 
 }  // namespace content
 
-#endif  // CONTENT_RENDERER_LOADER_CORS_URL_LOADER_H_
+#endif  // CONTENT_NETWORK_CORS_CORS_URL_LOADER_H_
