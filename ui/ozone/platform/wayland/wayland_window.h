a639f38af3cd746ab06135268cc57074018c8d8c
Maksim Sisov <msisov@igalia.com>
2018-01-24 16:45:12 +0000

    [ozone/wayland] Provide Wayland with desired bounds on request.
    This CL handles a case when Wayland asks its client about desired
    bounds.
    
    That is, under some circumstances, Wayland can send a configure
    event with width and height set to 0, which means it wants its
    client to send back desired bounds.
    
    There is also another case, or a bug in some Wayland protocol
    implementations like Gnome Shell, which sends width and height set
    to 0 instead of 1 if the browser is started with --start-fullscreen
    flag and then exits the fullscreen state.
    
    There are several cases of how we keep track of our desired bounds:
    we store previous bounds before going to maximize or fullscreen
    states, or we use our current bounds if a window has not had any
    state changes.
    
    If using our current bounds is naturally understandable, keeping
    the track of bounds before going to maximize and then fullscreen is
    a bit tricky. The restored bounds mustn't be overridden if we have
    gone from normal to maximized and then to fullscreen and then back
    to maximized, because |restored_bounds_| has already saved right bounds
    right before going from normal to maximized state.
    
    But if the state is changed from normal to maximized or from normal
    to fullscreen, we just store bounds on every change and restore
    them on Wayland request.
    
    Bug: 784836, 578890
    Change-Id: I0ae593235cccaf89719384f046ab446b814033e9
    Reviewed-on: https://chromium-review.googlesource.com/881481
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531559}

--- ui/ozone/platform/wayland/wayland_window.h
+++ ui/ozone/platform/wayland/wayland_window.h
@@ -84,6 +84,8 @@ class WaylandWindow : public PlatformWindow, public PlatformEventDispatcher {
   bool IsMaximized() const;
   bool IsFullscreen() const;
 
+  void SetPendingBounds(int32_t width, int32_t height);
+
   // Creates a surface window, which is visible as a main window.
   void CreateXdgSurface();
 
@@ -104,6 +106,8 @@ class WaylandWindow : public PlatformWindow, public PlatformEventDispatcher {
 
   gfx::Rect bounds_;
   gfx::Rect pending_bounds_;
+  // The bounds of our window before we were maximized or fullscreen.
+  gfx::Rect restored_bounds_;
   bool has_pointer_focus_ = false;
   bool has_keyboard_focus_ = false;
 
