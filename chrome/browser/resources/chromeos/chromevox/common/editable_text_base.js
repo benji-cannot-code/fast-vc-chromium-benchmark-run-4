9dc4f2c31915a6aff61fd602fe31e8cf7552d678
David Tseng <dtseng@chromium.org>
2018-01-24 19:10:50 +0000

    Do not apply deleted personality for single character deletions
    Bug: 801594
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I26207a6e721114ae89b1dd162f768f4e05e6eeb1
    Reviewed-on: https://chromium-review.googlesource.com/881943
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531638}

--- chrome/browser/resources/chromeos/chromevox/common/editable_text_base.js
+++ chrome/browser/resources/chromeos/chromevox/common/editable_text_base.js
@@ -465,7 +465,7 @@ cvox.ChromeVoxEditableTextBase.prototype.describeSelectionChanged = function(
  */
 cvox.ChromeVoxEditableTextBase.prototype.describeTextChanged = function(evt) {
   var personality = {};
-  if (evt.value.length < this.value.length) {
+  if (evt.value.length < (this.value.length - 1)) {
     personality = cvox.AbstractTts.PERSONALITY_DELETED;
   }
   if (this.isPassword) {
