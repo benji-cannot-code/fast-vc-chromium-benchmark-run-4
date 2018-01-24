767d7c35242f88071412970c85ab72090ef82ba2
Michael Spang <spang@chromium.org>
2018-01-24 19:06:21 +0000

    chromecast: Add touchscreen device manager
    This adds very primitive touchscreen management support to cast.
    Advanced features such as support for display modes with unusual
    geometries (letterboxing, pillarboxing, etc) are not supported
    unlike on Chrome OS.
    
    Bug: b/72054342
    Test: poke at screen while on https://rbyers.github.io/paint.html
    
    Change-Id: I456e3cd40f5a44a801a8bef5de3922a68b389298
    Reviewed-on: https://chromium-review.googlesource.com/879750
    Commit-Queue: Michael Spang <spang@chromium.org>
    Reviewed-by: Alex Sakhartchouk <alexst@chromium.org>
    Reviewed-by: Sergey Volk <servolk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531634}

--- chromecast/browser/cast_browser_main_parts.h
+++ chromecast/browser/cast_browser_main_parts.h
@@ -47,6 +47,7 @@ class VideoPlaneController;
 namespace shell {
 class CastBrowserProcess;
 class CastDisplayConfigurator;
+class CastTouchDeviceManager;
 class URLRequestContextFactory;
 
 class CastBrowserMainParts : public content::BrowserMainParts {
@@ -92,6 +93,7 @@ class CastBrowserMainParts : public content::BrowserMainParts {
   std::unique_ptr<CastWindowManager> window_manager_;
 #if defined(USE_AURA)
   std::unique_ptr<CastDisplayConfigurator> display_configurator_;
+  std::unique_ptr<CastTouchDeviceManager> touch_device_manager_;
 #endif
 
 #if BUILDFLAG(IS_CAST_USING_CMA_BACKEND)
