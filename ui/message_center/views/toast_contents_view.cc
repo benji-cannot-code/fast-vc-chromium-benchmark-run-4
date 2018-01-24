800f672a35aeb2f44a7954b353e241f06288716e
Tetsui Ohkubo <tetsui@chromium.org>
2018-01-24 09:04:27 +0000

    Revert "Collapse older notification popups."
    This reverts commit 4497e01a173b6fbaed337cb1164f453699862ab1.
    
    Reason for revert: UpdateWidgets crash https://crbug.com/804389
    
    Original change's description:
    > Collapse older notification popups.
    >
    > According to UX, in addition to expanding the latest notification,
    > older notification popups should be collapsed.
    >
    > See the screencast:
    > - Without this CL:
    >   http://go/scrcast/NTY3OTAwNjEyOTA2MTg4OHxjMjY3MGMxMS0xNw
    > - With this CL: http://go/scrcast/NTY0MjAyMzc3NjI4ODc2OHxhYjBmNWQ4Yy02NA
    >
    > TEST=NotificationViewMDTest.ExpandLongMessage
    > BUG=792582
    >
    > Change-Id: Ie6693386acda5a47405350822a48b4b0a279029f
    > Reviewed-on: https://chromium-review.googlesource.com/830998
    > Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    > Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#525034}
    
    TBR=yoshiki@chromium.org,tetsui@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 792582, 804389
    Change-Id: Ibbf9f56660235be61ca2ba98ed8e0086f828e4ef
    Reviewed-on: https://chromium-review.googlesource.com/882908
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531483}

--- ui/message_center/views/toast_contents_view.cc
+++ ui/message_center/views/toast_contents_view.cc
@@ -84,7 +84,6 @@ ToastContentsView::~ToastContentsView() {
 
 void ToastContentsView::SetContents(MessageView* view,
                                     bool a11y_feedback_for_updates) {
-  message_view_ = view;
   bool already_has_contents = child_count() > 0;
   RemoveAllChildViews(true);
   AddChildView(view);
