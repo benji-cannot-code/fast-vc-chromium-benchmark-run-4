2fad225d5aa14f1d4c621d92cd5c9686f6e7f9a9
Mostyn Bramley-Moore <mostynb@vewd.com>
2018-01-24 18:20:21 +0000

    [jumbo] add missing include guards in //chrome
    Also, move some existing include guards closer to the top of the file.
    
    And fix a license header typo (required by PRESUBMIT).
    
    Change-Id: I7f7153ea0e9787ea473753b69b6b83ce0c5ea6d3
    Reviewed-on: https://chromium-review.googlesource.com/882325
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: apacible <apacible@chromium.org>
    Commit-Queue: Mostyn Bramley-Moore <mostynb@vewd.com>
    Cr-Commit-Position: refs/heads/master@{#531611}

--- chrome/browser/chromeos/printing/printer_detector_test_util.h
+++ chrome/browser/chromeos/printing/printer_detector_test_util.h
@@ -1,7 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2017 the Chromium Authors. All rights reserved.
+// Copyright 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_CHROMEOS_PRINTING_PRINTER_DETECTOR_TEST_UTIL_H_
+#define CHROME_BROWSER_CHROMEOS_PRINTING_PRINTER_DETECTOR_TEST_UTIL_H_
 
 #include "chrome/browser/chromeos/printing/printer_detector.h"
 #include "content/public/test/test_browser_thread_bundle.h"
@@ -58,3 +61,5 @@ class PrinterDetectorTest : public testing::Test {
 
 }  // namespace
 }  // namespace chromeos
+
+#endif  // CHROME_BROWSER_CHROMEOS_PRINTING_PRINTER_DETECTOR_TEST_UTIL_H_
--- chrome/test/media_router/test_media_sinks_observer.h
+++ chrome/test/media_router/test_media_sinks_observer.h
@@ -3,14 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifndef CHROME_TEST_MEDIA_ROUTER_TEST_MEDIA_SINKS_OBSERVER_H_
+#define CHROME_TEST_MEDIA_ROUTER_TEST_MEDIA_SINKS_OBSERVER_H_
+
 #include <map>
 #include <string>
 #include <vector>
 
 #include "chrome/browser/media/router/media_sinks_observer.h"
-
-#ifndef CHROME_TEST_MEDIA_ROUTER_TEST_MEDIA_SINKS_OBSERVER_H_
-#define CHROME_TEST_MEDIA_ROUTER_TEST_MEDIA_SINKS_OBSERVER_H_
 
 namespace media_router {
 
