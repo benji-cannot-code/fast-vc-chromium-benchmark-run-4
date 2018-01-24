589bd8eb86d6a4a0d87160ab9ee7c44e1798c6c8
Sonny Sasaka <sonnysasaka@chromium.org>
2018-01-24 03:51:27 +0000

    Fix toggleable disabled bluetooth button.
    Currently the bluetooth toggle is toggleable even though it's disabled
    (i.e. property disabled=true and grayed). This is due to
    bluetoothToggleState_ is set in js without checking whether the button
    is currently disabled. This CL fixes this by always guarding it each
    time bluetoothToggleState_ is to be set.
    
    Additionally, this CL also fixes how the "in-progress" flag is cleared.
    The right way to clear the flag is at the callback of
    bluetoothPrivate.setAdapterState instead of at
    onBluetoothAdapterStateChanged_ because onBluetoothAdapterStateChanged_
    may not happen at all if the setAdapterState operation fails, whereas
    the callback is always called reliably after setAdapterState finishes
    its work regardless error or success.
    
    Note: Ideally, though, the disabled property of the button should be
    decided by a model layer instead of the UI, and this requires much
    larger refactoring and is tracked in http://crbug.com/804494.
    
    BUG=795190
    TEST=Simulate delay of bluetooth power change, open the settings page
    and click the bluetooth toggle button twice rapidly, the second click
    should be ignored.
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ieb172495f76e543957bdebde179dccf89dbac1a2
    Reviewed-on: https://chromium-review.googlesource.com/877413
    Commit-Queue: Sonny Sasaka <sonnysasaka@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531425}

--- chrome/browser/resources/settings/bluetooth_page/bluetooth_subpage.js
+++ chrome/browser/resources/settings/bluetooth_page/bluetooth_subpage.js
@@ -32,7 +32,7 @@ Polymer({
     },
 
     /** Reflects the bluetooth-page property. */
-    bluetoothToggleDisabled: Boolean,
+    stateChangeInProgress: Boolean,
 
     /**
      * The bluetooth adapter state, cached by bluetooth-page.
@@ -380,7 +380,8 @@ Polymer({
    * @private
    */
   onEnableTap_: function(event) {
-    this.bluetoothToggleState = !this.bluetoothToggleState;
+    if (this.isToggleEnabled_())
+      this.bluetoothToggleState = !this.bluetoothToggleState;
     event.stopPropagation();
   },
 
@@ -393,6 +394,15 @@ Polymer({
    */
   getOnOffString_: function(enabled, onstr, offstr) {
     return enabled ? onstr : offstr;
+  },
+
+  /**
+   * @return {boolean}
+   * @private
+   */
+  isToggleEnabled_: function() {
+    return this.adapterState !== undefined && this.adapterState.available &&
+        !this.stateChangeInProgress;
   },
 
   /**
