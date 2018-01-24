f58298d300c0ccde1c311bed2963d7fa8c101fb6
Eugene But <eugenebut@google.com>
2018-01-24 16:54:09 +0000

    Return bool from WebTestWithWebState::LoadHtml.
    For now this always retrun true and callers asserts that result is true.
    In future CLs this method will return false if html has failed to load.
    
    Bug: 780062
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I19555e2dc9e797b466f70df0f644c2e280ba7340
    Reviewed-on: https://chromium-review.googlesource.com/875321
    Reviewed-by: Mike Baxley <baxley@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531564}

--- ios/web/public/test/web_test_with_web_state.h
+++ ios/web/public/test/web_test_with_web_state.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/compiler_specific.h"
 #import "base/ios/block_types.h"
 #include "base/message_loop/message_loop.h"
 #include "ios/web/public/test/web_test.h"
@@ -43,7 +44,7 @@ class WebTestWithWebState : public WebTest,
   // Loads the specified HTML content into the WebState, using test url name.
   void LoadHtml(NSString* html);
   // Loads the specified HTML content into the WebState, using test url name.
-  void LoadHtml(const std::string& html);
+  bool LoadHtml(const std::string& html) WARN_UNUSED_RESULT;
   // Blocks until both known NSRunLoop-based and known message-loop-based
   // background tasks have completed
   void WaitForBackgroundTasks();
