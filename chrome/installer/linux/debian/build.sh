4842539870eefc38caecf8d97cc0c8b7a19cb9e2
Milko Leporis <milko.leporis@mips.com>
2018-01-24 21:40:19 +0000

    [MIPS64] Add support for building Chromium Linux for mips64r2
    This change adds missing parts of mips64r2 support.
    
    Bug: 800754
    Change-Id: If37183b7c996effe28b7f687ca86ec7f11d90514
    Reviewed-on: https://chromium-review.googlesource.com/860058
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531691}

--- chrome/installer/linux/debian/build.sh
+++ chrome/installer/linux/debian/build.sh
@@ -286,6 +286,9 @@ case "$TARGETARCH" in
   mipsel )
     export ARCHITECTURE="mipsel"
     ;;
+  mips64el )
+    export ARCHITECTURE="mips64el"
+    ;;
   * )
     echo
     echo "ERROR: Don't know how to build DEBs for '$TARGETARCH'."
