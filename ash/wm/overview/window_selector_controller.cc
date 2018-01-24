218f487d18b5a892432f9bc3cb56092946521490
Sammie Quon <sammiequon@google.com>
2018-01-24 23:42:17 +0000

    overview: Add empty window indicator to overview.
    Currently attempts to enter overview mode are blocked when there are no
    windows in use. Allow entering overview mode in the new ui, and display
    a message if there are no windows in use. We still exit overview if
    there is one window left and we close it (like android).
    
    For multi-display, all displays will enter overview when F5 is pressed.
    Displays with windows will work normally and displays without windows
    will show the indicator.
    
    The indicator will not show in splitview overview at the moment because
    splitview requires 2 or more windows on the MRU list to activate
    splitview overview. Compatibility with splitview will be done in another
    CL.
    
    This is hidden behind switch.
    
    Test: ash_unittests WindowSelectorTest.*
    Bug: 782320
    Change-Id: Ia82338cb285fb394a54d36df0ed5d26089aa2604
    Reviewed-on: https://chromium-review.googlesource.com/869111
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531746}

--- ash/wm/overview/window_selector_controller.cc
+++ ash/wm/overview/window_selector_controller.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/session/session_controller.h"
 #include "ash/shell.h"
 #include "ash/wm/mru_window_tracker.h"
+#include "ash/wm/overview/overview_utils.h"
 #include "ash/wm/overview/window_grid.h"
 #include "ash/wm/overview/window_selector.h"
 #include "ash/wm/overview/window_selector_item.h"
@@ -86,7 +87,7 @@ bool WindowSelectorController::ToggleOverview() {
 
     if (!Shell::Get()->IsSplitViewModeActive()) {
       // Don't enter overview with no window if the split view mode is inactive.
-      if (windows.empty())
+      if (!IsNewOverviewUi() && windows.empty())
         return false;
     } else {
       // Don't enter overview with less than 1 window if the split view mode is
