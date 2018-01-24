22bb1bc882b191cdb76d6ffe84bd23a162217d18
Antonio Gomes <tonikitoo@igalia.com>
2018-01-24 00:51:27 +0000

    Move EventAutoRepeatHandler out of /evdev umbrella
    Code is not evdev dependent, so CL moves it out of it.
    
    BUG=None
    
    Change-Id: I43c7df188bc3193e8b684c334e3cac71ab98d8af
    Reviewed-on: https://chromium-review.googlesource.com/881961
    Reviewed-by: Michael Spang <spang@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#531395}

--- ui/events/ozone/keyboard/event_auto_repeat_handler.cc
+++ ui/events/ozone/keyboard/event_auto_repeat_handler.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "ui/events/ozone/evdev/event_auto_repeat_handler.h"
+#include "ui/events/ozone/keyboard/event_auto_repeat_handler.h"
 
 #include "base/threading/thread_task_runner_handle.h"
 #include "ui/events/base_event_utils.h"
