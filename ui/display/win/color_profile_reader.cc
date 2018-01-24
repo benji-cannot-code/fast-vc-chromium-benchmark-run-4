6297deaa61e80e4a8367699874148062e717b8fe
Francois Doray <fdoray@chromium.org>
2018-01-24 21:38:01 +0000

    Remove usage of SequencedWorkerPool in ColorProfileReader.
    Bug: 667892
    Change-Id: I59ba41ab12c7a29a7011a7dc6bca16b0daa38895
    Reviewed-on: https://chromium-review.googlesource.com/881582
    Commit-Queue: François Doray <fdoray@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531686}

--- ui/display/win/color_profile_reader.cc
+++ ui/display/win/color_profile_reader.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_util.h"
 #include "base/task_scheduler/post_task.h"
-#include "base/threading/sequenced_worker_pool.h"
 #include "ui/display/win/display_info.h"
 #include "ui/gfx/icc_profile.h"
 
@@ -59,9 +58,6 @@ void ColorProfileReader::UpdateIfNeeded() {
 
   DeviceToPathMap new_device_to_path_map = BuildDeviceToPathMap();
   if (device_to_path_map_ == new_device_to_path_map)
-    return;
-
-  if (!base::SequencedWorkerPool::IsEnabled())
     return;
 
   update_in_flight_ = true;
