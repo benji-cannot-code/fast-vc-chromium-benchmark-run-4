f99696476c7fcca133bee796ef21ac0a316220b9
Mugdha Lakhani <nator@chromium.org>
2018-01-24 11:01:44 +0000

    [Android Notifications] Remove unused fields
    Remove origin and tag fields from various functions since they're no longer used.
    
    Bug: 801221, 801535
    Change-Id: I0b105acbf48cd3097b96ab40358dbec7ca0ec1fc
    Reviewed-on: https://chromium-review.googlesource.com/879202
    Reviewed-by: Anita Woodruff <awdf@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Mugdha Lakhani <nator@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531497}

--- chrome/browser/notifications/notification_platform_bridge_android.h
+++ chrome/browser/notifications/notification_platform_bridge_android.h
@@ -48,7 +48,6 @@ class NotificationPlatformBridgeAndroid : public NotificationPlatformBridge {
       const base::android::JavaParamRef<jstring>& java_scope_url,
       const base::android::JavaParamRef<jstring>& java_profile_id,
       jboolean incognito,
-      const base::android::JavaParamRef<jstring>& java_tag,
       const base::android::JavaParamRef<jstring>& java_webapk_package,
       jint action_index,
       const base::android::JavaParamRef<jstring>& java_reply);
@@ -69,7 +68,6 @@ class NotificationPlatformBridgeAndroid : public NotificationPlatformBridge {
       const base::android::JavaParamRef<jstring>& java_origin,
       const base::android::JavaParamRef<jstring>& java_profile_id,
       jboolean incognito,
-      const base::android::JavaParamRef<jstring>& java_tag,
       jboolean by_user);
 
   // NotificationPlatformBridge implementation.
@@ -105,20 +103,11 @@ class NotificationPlatformBridgeAndroid : public NotificationPlatformBridge {
   struct RegeneratedNotificationInfo {
     RegeneratedNotificationInfo();
     RegeneratedNotificationInfo(
-        // TODO(https://crbug.com/801221): origin no longer used, can be
-        // removed.
-        const GURL& origin,
         const GURL& service_worker_scope,
-        // TODO(https://crbug.com/801221): tag no longer used, can be removed
-        const std::string& tag,
         const base::Optional<std::string>& webapk_package);
     ~RegeneratedNotificationInfo();
 
-    // TODO(https://crbug.com/801221): origin no longer used, can be removed.
-    GURL origin;
     GURL service_worker_scope;
-    // TODO(https://crbug.com/801221): tag no longer used, can be removed.
-    std::string tag;
     base::Optional<std::string> webapk_package;
   };
 
