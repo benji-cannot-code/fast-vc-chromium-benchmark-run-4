4fa9cfa5163eb869d82d88fb7f448c0040e32afe
Tim Volodine <timvolodine@google.com>
2018-01-24 18:19:01 +0000

    [Android WebView] Tracing API: add trace argument filtering by default.
    Add filtering of trace arguments data by default to the WebView Tracing
    API. Only specifically whitelisted strings are allowed in trace args
    to avoid exposing any potential PII and address privacy concerns.
    
    Currently only a few metadata category events are whitelisted, e.g.
    the "process_name", "thread_name" arguments which are useful for
    viewing in "chrome://tracing".
    
    BUG=804414
    
    Change-Id: I1477cd363d36cd9a3b5cf3c6bd4de0e8a3a63e63
    Reviewed-on: https://chromium-review.googlesource.com/879144
    Commit-Queue: Tim Volodine <timvolodine@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531609}

--- android_webview/lib/aw_main_delegate.cc
+++ android_webview/lib/aw_main_delegate.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "android_webview/browser/browser_view_renderer.h"
 #include "android_webview/browser/command_line_helper.h"
 #include "android_webview/browser/deferred_gpu_command_service.h"
+#include "android_webview/browser/tracing/aw_trace_event_args_whitelist.h"
 #include "android_webview/common/aw_descriptors.h"
 #include "android_webview/common/aw_paths.h"
 #include "android_webview/common/aw_switches.h"
@@ -161,6 +162,11 @@ bool AwMainDelegate::BasicStartupComplete(int* exit_code) {
       new safe_browsing::SafeBrowsingApiHandlerBridge());
   safe_browsing::SafeBrowsingApiHandler::SetInstance(
       safe_browsing_api_handler_.get());
+
+  // Used only if the argument filter is enabled in tracing config,
+  // as is the case by default in aw_tracing_controller.cc
+  base::trace_event::TraceLog::GetInstance()->SetArgumentFilterPredicate(
+      base::Bind(&IsTraceEventArgsWhitelisted));
 
   return false;
 }
