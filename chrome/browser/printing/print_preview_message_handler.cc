955a86b744a3a5ef3097c1e61b6422a1ffa0f8f1
Wei Li <weili@chromium.org>
2018-01-24 21:38:04 +0000

    Complete pdf compositor service
    Add interfaces and functions to allow passing in and compositing
    contents from multiple frames in pdf compositor service.
    
    Each frame is identified by its global unique id (rph id and rfh id).
    Content id is a unique id within its process. Each frame may have oop
    subframes. The pdf compositor service will keep track of all the frame
    contents and their relationship. When all subframes are painted, the
    service can composite the entire frame and convert it into PDF format.
    
    Please refer document goo.gl/ACV6VM for more explanations.
    
    BUG=455764
    
    Change-Id: I3213d9ae0ca85d0ea695dd13acb00938499aa73b
    Reviewed-on: https://chromium-review.googlesource.com/767912
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Wei Li <weili@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531687}

--- chrome/browser/printing/print_preview_message_handler.cc
+++ chrome/browser/printing/print_preview_message_handler.cc
@@ -25,10 +25,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/printing/browser/print_composite_client.h"
 #include "components/printing/browser/print_manager_utils.h"
 #include "components/printing/common/print_messages.h"
+#include "components/printing/service/public/cpp/pdf_service_mojo_types.h"
 #include "components/printing/service/public/cpp/pdf_service_mojo_utils.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/browser/render_frame_host.h"
+#include "content/public/browser/render_process_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
+#include "printing/common/pdf_metafile_utils.h"
 #include "printing/page_size_margins.h"
 #include "printing/print_job_constants.h"
 #include "printing/print_settings.h"
@@ -126,6 +130,7 @@ void PrintPreviewMessageHandler::OnDidGetPreviewPageCount(
 }
 
 void PrintPreviewMessageHandler::OnDidPreviewPage(
+    content::RenderFrameHost* render_frame_host,
     const PrintHostMsg_DidPreviewPage_Params& params) {
   int page_number = params.page_number;
   if (page_number < FIRST_PAGE_INDEX || !params.data_size)
@@ -140,8 +145,12 @@ void PrintPreviewMessageHandler::OnDidPreviewPage(
     DCHECK(client);
 
     // Use utility process to convert skia metafile to pdf.
-    client->DoComposite(
-        params.metafile_data_handle, params.data_size,
+    client->DoCompositePageToPdf(
+        params.document_cookie,
+        GenFrameGuid(render_frame_host->GetProcess()->GetID(),
+                     render_frame_host->GetRoutingID()),
+        params.page_number, params.metafile_data_handle, params.data_size,
+        ContentToFrameMap(),
         base::BindOnce(&PrintPreviewMessageHandler::OnCompositePdfPageDone,
                        weak_ptr_factory_.GetWeakPtr(), params.page_number,
                        params.preview_request_id));
@@ -153,6 +162,7 @@ void PrintPreviewMessageHandler::OnDidPreviewPage(
 }
 
 void PrintPreviewMessageHandler::OnMetafileReadyForPrinting(
+    content::RenderFrameHost* render_frame_host,
     const PrintHostMsg_DidPreviewDocument_Params& params) {
   // Always try to stop the worker.
   StopWorker(params.document_cookie);
@@ -170,8 +180,11 @@ void PrintPreviewMessageHandler::OnMetafileReadyForPrinting(
     auto* client = PrintCompositeClient::FromWebContents(web_contents());
     DCHECK(client);
 
-    client->DoComposite(
-        params.metafile_data_handle, params.data_size,
+    client->DoCompositeDocumentToPdf(
+        params.document_cookie,
+        GenFrameGuid(render_frame_host->GetProcess()->GetID(),
+                     render_frame_host->GetRoutingID()),
+        params.metafile_data_handle, params.data_size, ContentToFrameMap(),
         base::BindOnce(&PrintPreviewMessageHandler::OnCompositePdfDocumentDone,
                        weak_ptr_factory_.GetWeakPtr(),
                        params.expected_pages_count, params.preview_request_id));
@@ -295,6 +308,9 @@ bool PrintPreviewMessageHandler::OnMessageReceived(
                                    render_frame_host)
     IPC_MESSAGE_HANDLER(PrintHostMsg_RequestPrintPreview,
                         OnRequestPrintPreview)
+    IPC_MESSAGE_HANDLER(PrintHostMsg_DidPreviewPage, OnDidPreviewPage)
+    IPC_MESSAGE_HANDLER(PrintHostMsg_MetafileReadyForPrinting,
+                        OnMetafileReadyForPrinting)
     IPC_MESSAGE_UNHANDLED(handled = false)
   IPC_END_MESSAGE_MAP()
   if (handled)
@@ -304,9 +320,6 @@ bool PrintPreviewMessageHandler::OnMessageReceived(
   IPC_BEGIN_MESSAGE_MAP(PrintPreviewMessageHandler, message)
     IPC_MESSAGE_HANDLER(PrintHostMsg_DidGetPreviewPageCount,
                         OnDidGetPreviewPageCount)
-    IPC_MESSAGE_HANDLER(PrintHostMsg_DidPreviewPage, OnDidPreviewPage)
-    IPC_MESSAGE_HANDLER(PrintHostMsg_MetafileReadyForPrinting,
-                        OnMetafileReadyForPrinting)
     IPC_MESSAGE_HANDLER(PrintHostMsg_PrintPreviewFailed,
                         OnPrintPreviewFailed)
     IPC_MESSAGE_HANDLER(PrintHostMsg_DidGetDefaultPageLayout,
