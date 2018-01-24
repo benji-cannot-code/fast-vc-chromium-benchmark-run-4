1dad82291ac919067bea6f08bd36a5e4086ec9fa
Mostyn Bramley-Moore <mostynb@vewd.com>
2018-01-24 14:17:22 +0000

    [jumbo] add missing include guard to gcm_request_test_base.h
    Change-Id: Ifa8bd35150af05007030f2e5aad49cd0112f9ba9
    Reviewed-on: https://chromium-review.googlesource.com/882421
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531520}

--- google_apis/gcm/engine/gcm_request_test_base.h
+++ google_apis/gcm/engine/gcm_request_test_base.h
@@ -3,6 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifndef GOOGLE_APIS_GCM_ENGINE_GCM_REQUEST_TEST_BASE_H_
+#define GOOGLE_APIS_GCM_ENGINE_GCM_REQUEST_TEST_BASE_H_
+
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/test/test_mock_time_task_runner.h"
@@ -59,3 +62,5 @@ class GCMRequestTestBase : public testing::Test {
 };
 
 }  // namespace gcm
+
+#endif  // GOOGLE_APIS_GCM_ENGINE_GCM_REQUEST_TEST_BASE_H_
