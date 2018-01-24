c023f14f0d3445d867ff38d1c6a3dc5eca3e6086
Takayoshi Kochi <kochi@chromium.org>
2018-01-24 06:01:44 +0000

    Remove dead code after multiple shadow removal [2/5]
    Remove
     - ElementShadow::YoungestShadowRoot()
     - ElementShadow::OldestShadowRoot()
    
    TBR=mlamouri@chromium.org for media_controls
    
    Bug: 795221
    Change-Id: I391b737acf04b45978ab1474725314442c979a06
    Reviewed-on: https://chromium-review.googlesource.com/828344
    Commit-Queue: Takayoshi Kochi <kochi@chromium.org>
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531452}

--- third_party/WebKit/Source/core/dom/ElementShadow.h
+++ third_party/WebKit/Source/core/dom/ElementShadow.h
@@ -49,19 +49,13 @@ class CORE_EXPORT ElementShadow final : public GarbageCollected<ElementShadow>,
     return shadow_root_->host();
   }
 
-  // TODO(hayato): Remove youngestShadowRoot() and oldestShadowRoot() from
-  // ElementShadow
-  ShadowRoot& YoungestShadowRoot() const;
-  ShadowRoot& OldestShadowRoot() const {
+  ShadowRoot& GetShadowRoot() const {
     DCHECK(shadow_root_);
     return *shadow_root_;
   }
-
   ElementShadow* ContainingShadow() const;
 
   ShadowRoot& AddShadowRoot(Element& shadow_host, ShadowRootType);
-
-  bool HasSameStyles(const ElementShadow&) const;
 
   void Attach(const Node::AttachContext&);
   void Detach(const Node::AttachContext&);
@@ -72,8 +66,8 @@ class CORE_EXPORT ElementShadow final : public GarbageCollected<ElementShadow>,
   void SetNeedsDistributionRecalc();
   bool NeedsDistributionRecalc() const { return needs_distribution_recalc_; }
 
-  bool IsV1() const { return YoungestShadowRoot().IsV1(); }
-  bool IsOpenOrV0() const { return YoungestShadowRoot().IsOpenOrV0(); }
+  bool IsV1() const { return GetShadowRoot().IsV1(); }
+  bool IsOpenOrV0() const { return GetShadowRoot().IsOpenOrV0(); }
 
   ElementShadowV0& V0() const {
     DCHECK(element_shadow_v0_);
@@ -86,7 +80,6 @@ class CORE_EXPORT ElementShadow final : public GarbageCollected<ElementShadow>,
  private:
   ElementShadow();
 
-  void AppendShadowRoot(ShadowRoot&);
   void Distribute();
 
   TraceWrapperMember<ElementShadowV0> element_shadow_v0_;
@@ -103,7 +96,7 @@ inline ShadowRoot* Node::YoungestShadowRoot() const {
 
 inline ShadowRoot* Element::YoungestShadowRoot() const {
   if (ElementShadow* shadow = Shadow())
-    return &shadow->YoungestShadowRoot();
+    return &shadow->GetShadowRoot();
   return nullptr;
 }
 
--- third_party/WebKit/Source/core/editing/serializers/StyledMarkupSerializer.cpp
+++ third_party/WebKit/Source/core/editing/serializers/StyledMarkupSerializer.cpp
@@ -80,7 +80,7 @@ bool HandleSelectionBoundary<EditingInFlatTreeStrategy>(const Node& node) {
   ElementShadow* shadow = ToElement(node).Shadow();
   if (!shadow)
     return false;
-  return shadow->YoungestShadowRoot().IsUserAgent();
+  return shadow->GetShadowRoot().IsUserAgent();
 }
 
 }  // namespace
