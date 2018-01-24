132cfff83cbfe7decda8b2e1ddfec46dff6c97b6
Makoto Shimazu <shimazu@chromium.org>
2018-01-24 07:29:24 +0000

    Add ServiceWorkerServicification flag
    This CL tentatively adds ServiceWorkerServicification feature flag. Currently it
    doesn't work since S13nServiceWorker features are now implemented with
    NetworkService. If we ship S13nServiceWorker before NetworkService, we need to
    change some parts so that service worker can work with SWURLRequestRequest*
    objects.
    
    Bug: 752394
    Change-Id: I605214b738e802f74df72d6f90c265206a6ec488
    Reviewed-on: https://chromium-review.googlesource.com/880125
    Commit-Queue: Makoto Shimazu <shimazu@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531472}

--- content/common/service_worker/service_worker_utils.cc
+++ content/common/service_worker/service_worker_utils.cc
@@ -143,8 +143,8 @@ bool ServiceWorkerUtils::AllOriginsMatchAndCanAccessServiceWorkers(
 
 // static
 bool ServiceWorkerUtils::IsServicificationEnabled() {
-  return IsBrowserSideNavigationEnabled() &&
-         base::FeatureList::IsEnabled(features::kNetworkService);
+  return base::FeatureList::IsEnabled(features::kNetworkService) ||
+         base::FeatureList::IsEnabled(features::kServiceWorkerServicification);
 }
 
 // static
