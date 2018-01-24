0d4e583984b503da1389b0646ca7761c0ce406ef
Takayoshi Kochi <kochi@chromium.org>
2018-01-24 15:33:23 +0000

    Revert "Remove dead code after multiple shadow removal [5/5]"
    This reverts commit 0b95262849c3c386d4216f5fc08816970911c256.
    
    Reason for revert: crbug.com/805383
    This and some prior changes may have made Mac bots' layout tests
    flaky.
    
    Original change's description:
    > Remove dead code after multiple shadow removal [5/5]
    > 
    > Remove window.internals.{youngest/oldest/younger}ShadowRoot
    > Adjust layout tests.
    > 
    > Bug: 795221
    > Change-Id: I75024668007771c87ddd9598fbd1728859d7f9e9
    > Reviewed-on: https://chromium-review.googlesource.com/831815
    > Commit-Queue: Takayoshi Kochi <kochi@chromium.org>
    > Reviewed-by: Hayato Ito <hayato@chromium.org>
    > Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#531490}
    
    TBR=kochi@chromium.org,hayato@chromium.org,futhark@chromium.org
    
    Change-Id: I02653b0f2f0874465c6078aa46c6ebc9365148a6
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 795221
    Reviewed-on: https://chromium-review.googlesource.com/883921
    Reviewed-by: Takayoshi Kochi <kochi@chromium.org>
    Commit-Queue: Takayoshi Kochi <kochi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531534}

--- third_party/WebKit/LayoutTests/fast/forms/resources/common.js
+++ third_party/WebKit/LayoutTests/fast/forms/resources/common.js
@@ -121,7 +121,7 @@ function mouseMoveToIndexInListbox(index, listboxId) {
 }
 
 function getUserAgentShadowTextContent(element) {
-    return internals.shadowRoot(element).textContent;
+    return internals.youngestShadowRoot(element).textContent;
 };
 
 function cumulativeOffset(element) {
