82d3c5a0917109d4b2c8189ce21a46caa2eff3ac
Jonathan Backer <backer@chromium.org>
2018-01-25 18:14:19 +0000

    Update raster_decoder.cc to use autogen
    This uses the new raster specific command IDs and autogen of boilerplate
    handler code.
    
    All of the handlers are stubbed out with NOTIMPLEMENTED().
    
    Compiles and links.
    
    Bug: 789238
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I791f1105d191197628273beaf0c6aa46c0c4b763
    Reviewed-on: https://chromium-review.googlesource.com/884205
    Commit-Queue: Jonathan Backer <backer@chromium.org>
    Reviewed-by: Victor Miura <vmiura@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531947}

--- gpu/command_buffer/build_raster_cmd_buffer.py
+++ gpu/command_buffer/build_raster_cmd_buffer.py
@@ -719,12 +719,10 @@ _FUNCTION_INFO = {
   'CopyTexImage2D': {
     'decoder_func': 'DoCopyTexImage2D',
     'unit_test': False,
-    'defer_reads': True,
     'trace_level': 1,
   },
   'CopyTexSubImage2D': {
     'decoder_func': 'DoCopyTexSubImage2D',
-    'defer_reads': True,
     'trace_level': 1,
   },
   'CreateImageCHROMIUM': {
@@ -750,7 +748,6 @@ _FUNCTION_INFO = {
     'impl_func': False,
     'client_test': False,
     'decoder_func': 'DoFinish',
-    'defer_reads': True,
     'trace_level': 1,
   },
   'Flush': {
@@ -1094,8 +1091,8 @@ def main(argv):
   #   "gpu/command_buffer/client/raster_c_lib_autogen.h")
   # gen.WriteCmdHelperHeader(
   #   "gpu/command_buffer/client/raster_cmd_helper_autogen.h")
-  # gen.WriteServiceImplementation(
-  #   "gpu/command_buffer/service/raster_cmd_decoder_autogen.h")
+  gen.WriteServiceImplementation(
+    "gpu/command_buffer/service/raster_cmd_decoder_autogen.h")
   # gen.WriteServiceUnitTests(
   #   "gpu/command_buffer/service/raster_cmd_decoder_unittest_%d_autogen.h")
   # gen.WriteServiceUnitTestsForExtensions(
