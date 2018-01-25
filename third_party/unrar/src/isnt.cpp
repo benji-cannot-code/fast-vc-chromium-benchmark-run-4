af819b0961cbc7c152763940bb606edf47a226be
Varun Khaneja <vakh@chromium.org>
2018-01-25 17:07:21 +0000

    [2] Build unrar only for full safe browsing mode.
    - Enables exception handling: throw/try/catch.
    - Disables a bunch of warnings that this library is full of.
      - "-Wno-dangling-else"
      - "-Wno-logical-op-parentheses"
      - "-Wno-switch"
    
    Bug: 750327
    Change-Id: Ic08f67e328da2f22f3503eab074be6aaf2f2fc90
    Reviewed-on: https://chromium-review.googlesource.com/872219
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Reviewed-by: Nathan Parker <nparker@chromium.org>
    Reviewed-by: Jialiu Lin <jialiul@chromium.org>
    Commit-Queue: Varun Khaneja <vakh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531926}

--- third_party/unrar/src/isnt.cpp
+++ third_party/unrar/src/isnt.cpp
@@ -1,25 +1,17 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#include "rar.hpp"
+#include "isnt.hpp"
 
 #ifdef _WIN_ALL
+#include "versionhelpers.h"
+
 DWORD WinNT()
 {
-  static int dwPlatformId=-1;
-  static DWORD dwMajorVersion,dwMinorVersion;
-  if (dwPlatformId==-1)
-  {
-    OSVERSIONINFO WinVer;
-    WinVer.dwOSVersionInfoSize=sizeof(WinVer);
-    GetVersionEx(&WinVer);
-    dwPlatformId=WinVer.dwPlatformId;
-    dwMajorVersion=WinVer.dwMajorVersion;
-    dwMinorVersion=WinVer.dwMinorVersion;
-  }
-  DWORD Result=0;
-  if (dwPlatformId==VER_PLATFORM_WIN32_NT)
-    Result=dwMajorVersion*0x100+dwMinorVersion;
-
-
-  return Result;
+  if (!IsWinXpOrGreater()) return WNT_NONE;
+  if (!IsWinVistaOrGreater()) return WNT_WXP;
+  if (!IsWindows7OrGreater()) return WNT_VISTA;
+  if (!IsWindows8OrGreater()) return WNT_W7;
+  if (!IsWindows8Point1OrGreater()) return WNT_W8;
+  if (!IsWindows10OrGreater()) return WNT_W81;
+  return WNT_W10;
 }
 #endif
