8c951cdba5b3c3f0e3efc951f182bcaa4792aca3
Kush Sinha <sinhak@chromium.org>
2018-01-25 11:50:38 +0000

    Use specialized Mirror reconciliation for child accounts on Chrome OS
    Child accounts on Chrome OS will be forcibly logged out if account
    reconciliation timeout is reached without a successful reconciliation.
    Other accounts will maintain their existing behavior.
    
    Bug: 795808
    Change-Id: If7dc163e2d0921a7e360aed9673b235d1a2d9c7c
    Reviewed-on: https://chromium-review.googlesource.com/883285
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531866}

--- chrome/browser/signin/account_reconcilor_factory.cc
+++ chrome/browser/signin/account_reconcilor_factory.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/signin/account_reconcilor_factory.h"
 
+#include <memory>
 #include <utility>
 
 #include "base/logging.h"
@@ -21,9 +22,47 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/core/browser/profile_management_switches.h"
 #include "components/signin/core/browser/signin_features.h"
 
+#if defined(OS_CHROMEOS)
+#include "base/time/time.h"
+#include "chrome/browser/lifetime/application_lifetime.h"
+#include "components/user_manager/user_manager.h"
+#include "google_apis/gaia/google_service_auth_error.h"
+#endif
+
 #if BUILDFLAG(ENABLE_DICE_SUPPORT)
 #include "components/signin/core/browser/dice_account_reconcilor_delegate.h"
 #endif
+
+namespace {
+
+#if defined(OS_CHROMEOS)
+class ChromeOSChildAccountReconcilorDelegate
+    : public signin::MirrorAccountReconcilorDelegate {
+ public:
+  explicit ChromeOSChildAccountReconcilorDelegate(
+      SigninManagerBase* signin_manager)
+      : signin::MirrorAccountReconcilorDelegate(signin_manager) {}
+
+  base::TimeDelta GetReconcileTimeout() const override {
+    return base::TimeDelta::FromSeconds(10);
+  }
+
+  void OnReconcileError(const GoogleServiceAuthError& error) override {
+    if (error.state() == GoogleServiceAuthError::CONNECTION_FAILED) {
+      // Mark the account to require an online sign in.
+      const user_manager::User* primary_user =
+          user_manager::UserManager::Get()->GetPrimaryUser();
+      user_manager::UserManager::Get()->SaveForceOnlineSignin(
+          primary_user->GetAccountId(), true /* force_online_signin */);
+
+      // Force a logout.
+      chrome::AttemptUserExit();
+    }
+  }
+};
+#endif
+
+}  // namespace
 
 AccountReconcilorFactory::AccountReconcilorFactory()
     : BrowserContextKeyedServiceFactory(
@@ -66,6 +105,14 @@ KeyedService* AccountReconcilorFactory::BuildServiceInstanceFor(
 std::unique_ptr<signin::AccountReconcilorDelegate>
 AccountReconcilorFactory::CreateAccountReconcilorDelegate(Profile* profile) {
   if (AccountConsistencyModeManager::IsMirrorEnabledForProfile(profile)) {
+#if defined(OS_CHROMEOS)
+    // Only for child accounts on Chrome OS, use the specialized Mirror
+    // delegate.
+    if (profile->IsChild()) {
+      return std::make_unique<ChromeOSChildAccountReconcilorDelegate>(
+          SigninManagerFactory::GetForProfile(profile));
+    }
+#endif
     return std::make_unique<signin::MirrorAccountReconcilorDelegate>(
         SigninManagerFactory::GetForProfile(profile));
   }
@@ -73,8 +120,10 @@ AccountReconcilorFactory::CreateAccountReconcilorDelegate(Profile* profile) {
   // the source of truth.
   switch (signin::GetAccountConsistencyMethod()) {
     case signin::AccountConsistencyMethod::kMirror:
-      return std::make_unique<signin::MirrorAccountReconcilorDelegate>(
-          SigninManagerFactory::GetForProfile(profile));
+      // It is not possible for |IsMirrorEnabledForProfile| to return false,
+      // and this case being true.
+      NOTREACHED();
+      return nullptr;
     case signin::AccountConsistencyMethod::kDisabled:
     case signin::AccountConsistencyMethod::kDiceFixAuthErrors:
       return std::make_unique<signin::AccountReconcilorDelegate>();
