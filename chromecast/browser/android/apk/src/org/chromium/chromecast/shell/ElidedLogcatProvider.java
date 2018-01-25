32cd95fae9e64268ed8bd7a2de9c3de28b14a74b
Sandeep Vijayasekar <sandv@chromium.org>
2018-01-25 02:20:11 +0000

    [Chromecast] Added AIDL to get assistant device logs
    Bug: 71798364
    Bug: 71646628
    Test: Send feedback on home app
    Change-Id: Iba23a4e07369a9557a8ef43c23a7f93f3184a9c6
    Reviewed-on: https://chromium-review.googlesource.com/882493
    Commit-Queue: Sandeep Vijayasekar <sandv@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Reviewed-by: Stephen Lanham <slan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531793}

---
+++ chromecast/browser/android/apk/src/org/chromium/chromecast/shell/ElidedLogcatProvider.java
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chromecast.shell;
+
+import org.chromium.base.VisibleForTesting;
+
+/**
+ * Extracts logcat out of Android devices and elide PII sensitive info from it.
+ *
+ * <p>Elided information includes: Emails, IP address, MAC address, URL/domains as well as
+ * Javascript console messages.
+ */
+abstract class ElidedLogcatProvider {
+    protected abstract void getRawLogcat(RawLogcatCallback rawLogcatCallback);
+
+    protected interface RawLogcatCallback { public void onLogsDone(Iterable<String> logs); }
+    public interface LogcatCallback { public void onLogsDone(String logs); }
+
+    public void getElidedLogcat(LogcatCallback callback) {
+        getRawLogcat((Iterable<String> rawLogs) -> callback.onLogsDone(elideLogcat(rawLogs)));
+    }
+
+    @VisibleForTesting
+    protected static String elideLogcat(Iterable<String> rawLogcat) {
+        StringBuilder builder = new StringBuilder();
+        for (String line : rawLogcat) {
+            builder.append(LogcatElision.elide(line));
+            builder.append("\n");
+        }
+        return builder.toString();
+    }
+}
