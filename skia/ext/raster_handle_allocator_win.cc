bda1823e7c8377b30cdb7abbc28055b594c06769
Mike Klein <mtklein@chromium.org>
2018-01-25 16:33:24 +0000

    remove bzero() define
    While attempting to s/SK_BUILD_FOR_WIN32/SK_BUILD_FOR_WIN/g,
    I stumbled upon this define for bzero.  Skia doesn't use bzero,
    and there's no good reason for skia/ext to use it either.
    
    Change-Id: I12cbc5d40c17491fe1d648540655fad156196549
    Reviewed-on: https://chromium-review.googlesource.com/884007
    Reviewed-by: Mike Reed <reed@chromium.org>
    Commit-Queue: Mike Klein <mtklein@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531916}

--- skia/ext/raster_handle_allocator_win.cc
+++ skia/ext/raster_handle_allocator_win.cc
@@ -4,8 +4,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include <windows.h>
+
 #include <psapi.h>
 #include <stddef.h>
+#include <string.h>
 
 #include "base/debug/gdi_debug_util_win.h"
 #include "base/memory/ptr_util.h"
@@ -58,7 +60,7 @@ static bool Create(int width,
 
   size_t row_bytes = skia::PlatformCanvasStrideForWidth(width);
   if (do_clear)
-    bzero(pixels, row_bytes * height);
+    memset(pixels, 0, row_bytes * height);
 
   HDC hdc = CreateCompatibleDC(nullptr);
   if (!hdc) {
