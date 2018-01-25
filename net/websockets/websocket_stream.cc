a832de485a75295824e774ae1bbe4609cee56a80
Ramin Halavati <rhalavati@chromium.org>
2018-01-25 12:38:19 +0000

    Add traffic annotation to WebSocketBasicStream.
    Network traffic annotaiton is added to the socket write in
    WebSocketBasicStream::WriteEverything().
    
    Bug: 656607
    Change-Id: Id9ae38031afb9b8bd6485c1b6db8932a83527fa8
    Reviewed-on: https://chromium-review.googlesource.com/877888
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531872}

--- net/websockets/websocket_stream.cc
+++ net/websockets/websocket_stream.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
+// Please refer to the comment in class header if the usage changes.
 constexpr net::NetworkTrafficAnnotationTag kTrafficAnnotation =
     net::DefineNetworkTrafficAnnotation("websocket_stream", R"(
         semantics {
