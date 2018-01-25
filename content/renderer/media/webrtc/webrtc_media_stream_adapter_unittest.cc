c60fd05787622761480b983c6c15cbeb4adc8655
Hajime Hoshi <hajimehoshi@chromium.org>
2018-01-25 05:39:49 +0000

    media: Use per-frame task runners at WebRTCMediaStreamAdapterMap and WebRtcMediaStreamTrackAdapterMap
    This is part of efforts to replace base::ThreadTaskRunnerHandle::Get()
    and SequencedTaskRunnerHandle::Get() with other appropriate task runners
    in the renderer.
    
    Bug: 786332
    Change-Id: I300a38816bef6bb8f371203aaad52cb31154309a
    Reviewed-on: https://chromium-review.googlesource.com/882713
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531820}

--- content/renderer/media/webrtc/webrtc_media_stream_adapter_unittest.cc
+++ content/renderer/media/webrtc/webrtc_media_stream_adapter_unittest.cc
@@ -42,8 +42,9 @@ class WebRtcMediaStreamAdapterTest : public ::testing::Test {
  public:
   void SetUp() override {
     dependency_factory_.reset(new MockPeerConnectionDependencyFactory());
-    track_adapter_map_ =
-        new WebRtcMediaStreamTrackAdapterMap(dependency_factory_.get());
+    track_adapter_map_ = new WebRtcMediaStreamTrackAdapterMap(
+        dependency_factory_.get(),
+        blink::scheduler::GetSingleThreadTaskRunnerForTesting());
   }
 
   void TearDown() override {
