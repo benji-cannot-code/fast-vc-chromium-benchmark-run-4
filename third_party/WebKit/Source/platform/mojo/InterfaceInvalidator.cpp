1cc2f20b9e1d32235d2353ea23b09b844ccf439a
Austin Tankiang <austinct@google.com>
2018-01-25 05:54:57 +0000

    Add InterfaceInvalidator and WeakInterfacePtr classes
    Mojo InterfacePtrs need a way to be automatically revoked, so wrap them
    in a WeakInterfacePtr class. This class is associated with an
    InterfaceInvalidator class that revokes the weak interface pointer when
    the invalidator is destroyed.
    
    Add tests to verify that invalidation works well with the existing
    InterfacePtr functions. Change PingService to be [Sync] so both async
    and sync calls can be tested.
    
    Also introduce a new typedef for blink code, |WeakFooPtr|. Existing code
    will be gradually changed to use the new typedef.
    
    Bug: 800641
    Change-Id: I29778b233b81eec14ffca048a70f84cc3e31fb44
    Reviewed-on: https://chromium-review.googlesource.com/848432
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Commit-Queue: Austin Tankiang <austinct@google.com>
    Cr-Commit-Position: refs/heads/master@{#531823}

---
+++ third_party/WebKit/Source/platform/mojo/InterfaceInvalidator.cpp
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "platform/mojo/InterfaceInvalidator.h"
+
+namespace blink {
+
+InterfaceInvalidator::InterfaceInvalidator() : weak_factory_(this) {}
+
+InterfaceInvalidator::~InterfaceInvalidator() {
+  weak_factory_.InvalidateWeakPtrs();
+  NotifyInvalidate();
+}
+
+void InterfaceInvalidator::AddObserver(Observer* observer) {
+  DCHECK(observer);
+  observers_.AddObserver(observer);
+}
+
+void InterfaceInvalidator::RemoveObserver(const Observer* observer) {
+  DCHECK(observer);
+  observers_.RemoveObserver(observer);
+}
+
+base::WeakPtr<InterfaceInvalidator> InterfaceInvalidator::GetWeakPtr() {
+  return weak_factory_.GetWeakPtr();
+}
+
+void InterfaceInvalidator::NotifyInvalidate() {
+  for (auto& observer : observers_)
+    observer.OnInvalidate();
+}
+
+}  // namespace blink
---
+++ third_party/WebKit/Source/platform/mojo/InterfaceInvalidator.h
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef InterfaceInvalidator_h
+#define InterfaceInvalidator_h
+
+#include "base/macros.h"
+#include "base/memory/weak_ptr.h"
+#include "base/observer_list.h"
+#include "platform/PlatformExport.h"
+
+namespace blink {
+
+// Notifies weak interface bindings to be invalidated when this object is
+// destroyed.
+class PLATFORM_EXPORT InterfaceInvalidator {
+ public:
+  InterfaceInvalidator();
+  ~InterfaceInvalidator();
+
+  class Observer {
+   public:
+    virtual void OnInvalidate() = 0;
+  };
+
+  void AddObserver(Observer*);
+  void RemoveObserver(const Observer*);
+
+  base::WeakPtr<InterfaceInvalidator> GetWeakPtr();
+
+ private:
+  void NotifyInvalidate();
+
+  base::ObserverList<Observer> observers_;
+  base::WeakPtrFactory<InterfaceInvalidator> weak_factory_;
+
+  DISALLOW_COPY_AND_ASSIGN(InterfaceInvalidator);
+};
+
+}  // namespace blink
+
+#endif  // InterfaceInvalidator_h
