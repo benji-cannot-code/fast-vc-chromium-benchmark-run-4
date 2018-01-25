b643be3c856aebca0cec55be699bb1409946d351
Joe Downing <joedow@chromium.org>
2018-01-25 00:32:38 +0000

    Fixing build break due to unreachable code.
    A previous CL added some code to SandboxUnpacker which resulted in
    some lines of a method from bing reached:
    https://chromium-review.googlesource.com/c/chromium/src/+/871800
    
    A subsequent change was checked in so instead of reverting the first
    change, I am going to remove the unreachable code.
    
    TBR=jcivelli@chromium.org
    
    Change-Id: If37d3641734cf9ab71b04329dbab0e346aa93949
    Reviewed-on: https://chromium-review.googlesource.com/884983
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531757}

--- extensions/browser/sandboxed_unpacker.cc
+++ extensions/browser/sandboxed_unpacker.cc
@@ -867,9 +867,6 @@ base::string16 SandboxedUnpacker::FailureReasonToString16(
       NOTREACHED();
       return base::string16();
   }
-
-  NOTREACHED();
-  return base::string16();
 }
 
 void SandboxedUnpacker::FailWithPackageError(FailureReason reason) {
