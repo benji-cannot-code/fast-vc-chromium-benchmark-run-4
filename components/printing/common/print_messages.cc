0871f1a93972165f320adc614f587dcee633dab7
rbpotter <rbpotter@chromium.org>
2018-01-25 01:10:33 +0000

    Printing: Make rectangular DPI printers work
    Some printers use rectangular DPI only. Not accounting for this results
    in distorted printouts when printing from Chrome. Send rectangular DPI
    to Pdfium so that these printers can print correctly.
    
    Bug: 738950
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I0b9e90905b7fefc38ca1d73d176d94e8e4a3b727
    Reviewed-on: https://chromium-review.googlesource.com/664249
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Jianzhou Feng <jzfeng@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531769}

--- components/printing/common/print_messages.cc
+++ components/printing/common/print_messages.cc
@@ -66,7 +66,7 @@ PrintMsg_Print_Params::PrintMsg_Print_Params()
       printable_area(),
       margin_top(0),
       margin_left(0),
-      dpi(0),
+      dpi(),
       scale_factor(1.0f),
       rasterize_pdf(false),
       document_cookie(0),
@@ -96,7 +96,7 @@ void PrintMsg_Print_Params::Reset() {
   printable_area = gfx::Rect();
   margin_top = 0;
   margin_left = 0;
-  dpi = 0;
+  dpi = gfx::Size();
   scale_factor = 1.0f;
   rasterize_pdf = false;
   document_cookie = 0;
