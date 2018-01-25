f8d88f836c8ee214a1c0a23ff43f71dfd34a08a0
Alan Cutter <alancutter@chromium.org>
2018-01-25 05:12:22 +0000

    Restrict neutral keyframes from being composited on top of finished animations
    This CL prevents animations from running on the compositor when they contain
    neutral keyframes and apply on top of existing animaions on the same property.
    
    Bug: 805323
    Change-Id: Iba19882454211f127358b78fcbfa32bfdf183048
    Reviewed-on: https://chromium-review.googlesource.com/884781
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531814}

--- third_party/WebKit/Source/core/animation/EffectModel.h
+++ third_party/WebKit/Source/core/animation/EffectModel.h
@@ -65,6 +65,7 @@ class CORE_EXPORT EffectModel : public GarbageCollectedFinalized<EffectModel> {
                       Vector<scoped_refptr<Interpolation>>&) const = 0;
 
   virtual bool Affects(const PropertyHandle&) const { return false; }
+  virtual bool AffectedByUnderlyingAnimations() const = 0;
   virtual bool IsTransformRelatedEffect() const { return false; }
   virtual bool IsKeyframeEffectModel() const { return false; }
 
