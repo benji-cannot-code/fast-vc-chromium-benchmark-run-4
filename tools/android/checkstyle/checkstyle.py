406759322244522a851a653eaf2f05545b67a0c5
Misha Efimov <mef@chromium.org>
2018-01-25 15:55:16 +0000

    Only run tools/android/checkstyle on Linux.
    Bug: 804891
    Change-Id: I1329a27ab71dce901d8494ffeb06aecb61ab5da9
    Reviewed-on: https://chromium-review.googlesource.com/881662
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Commit-Queue: Misha Efimov <mef@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531905}

--- tools/android/checkstyle/checkstyle.py
+++ tools/android/checkstyle/checkstyle.py
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import os
 import subprocess
+import sys
 import xml.dom.minidom
 
 
@@ -26,6 +27,10 @@ def FormatCheckstyleOutput(checkstyle_output):
 
 
 def RunCheckstyle(input_api, output_api, style_file, black_list=None):
+  # Android toolchain is only available on Linux.
+  if not sys.platform.startswith('linux'):
+    return []
+
   if not os.path.exists(style_file):
     file_error = ('  Java checkstyle configuration file is missing: '
                   + style_file)
