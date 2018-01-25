e58e1655119cbe46fdf8606b39b237f1491d1fbc
Saman Sami <samans@chromium.org>
2018-01-25 01:31:25 +0000

    content: Use DidCommitAndDraw to ack resize
    Once the renderer generates a CompositorFrame after resize,
    there is really nothing more it can do and it should start the next
    resize. Waiting for DidReceiveCompositorFrameAck wastes renderer's time
    (2 IPC messages + the time it takes for Viz to ack a frame) and doesn't
    provide any value.
    
    Change-Id: Iba1d9c7862b6540fc2133e787c8c531d53e0c845
    Reviewed-on: https://chromium-review.googlesource.com/882612
    Reviewed-by: Fady Samuel <fsamuel@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Saman Sami <samans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531774}

--- content/renderer/render_widget.cc
+++ content/renderer/render_widget.cc
@@ -1017,6 +1017,8 @@ void RenderWidget::DidCommitAndDrawCompositorFrame() {
   // tab_capture_performancetest.cc.
   TRACE_EVENT0("gpu", "RenderWidget::DidCommitAndDrawCompositorFrame");
 
+  DidResizeOrRepaintAck();
+
   for (auto& observer : render_frames_)
     observer.DidCommitAndDrawCompositorFrame();
 
@@ -1029,8 +1031,6 @@ void RenderWidget::DidCommitCompositorFrame() {}
 void RenderWidget::DidCompletePageScaleAnimation() {}
 
 void RenderWidget::DidReceiveCompositorFrameAck() {
-  TRACE_EVENT0("renderer", "RenderWidget::DidReceiveCompositorFrameAck");
-  DidResizeOrRepaintAck();
 }
 
 bool RenderWidget::IsClosing() const {
