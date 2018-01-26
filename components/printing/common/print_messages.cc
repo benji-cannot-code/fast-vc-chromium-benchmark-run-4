dff9b1ad2149baa48b8dcef9f1d8f2a2bd02457a
Daniel Bratell <bratell@opera.com>
2018-01-26 23:41:14 +0000

    Allow error free multiple inclusion of print_messages.h
    In jumbo builds, there is no careful inclusion of headers
    so files that require very careful handling of when they are
    included cause problems. One such file was print_messages.h
    because of an undef it did of another include guard.
    
    This patch moves the undef one layer out of print_messages.h
    so that print_messages.h itself can be included many time.
    
    Bug: 746957
    Change-Id: Ie44fe5dbd4665cc487788fe7b6e42ebcd20cc50f
    Reviewed-on: https://chromium-review.googlesource.com/878325
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532092}

--- components/printing/common/print_messages.cc
+++ components/printing/common/print_messages.cc
@@ -7,8 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "printing/features/features.h"
 #include "ui/gfx/geometry/size.h"
 
+// Generating implementations for all aspects of the IPC message
+// handling by setting appropriate IPC macros and including the
+// message file, over and over again until all versions have been
+// generated.
+
 #define IPC_MESSAGE_IMPL
 #undef COMPONENTS_PRINTING_COMMON_PRINT_MESSAGES_H_
+#undef COMPONENTS_PRINTING_COMMON_PRINTING_PARAM_TRAITS_MACROS_H_
 #include "components/printing/common/print_messages.h"
 #ifndef COMPONENTS_PRINTING_COMMON_PRINT_MESSAGES_H_
 #error "Failed to include header components/printing/common/print_messages.h"
@@ -17,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Generate constructors.
 #include "ipc/struct_constructor_macros.h"
 #undef COMPONENTS_PRINTING_COMMON_PRINT_MESSAGES_H_
+#undef COMPONENTS_PRINTING_COMMON_PRINTING_PARAM_TRAITS_MACROS_H_
 #include "components/printing/common/print_messages.h"
 #ifndef COMPONENTS_PRINTING_COMMON_PRINT_MESSAGES_H_
 #error "Failed to include header components/printing/common/print_messages.h"
@@ -25,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Generate destructors.
 #include "ipc/struct_destructor_macros.h"
 #undef COMPONENTS_PRINTING_COMMON_PRINT_MESSAGES_H_
+#undef COMPONENTS_PRINTING_COMMON_PRINTING_PARAM_TRAITS_MACROS_H_
 #include "components/printing/common/print_messages.h"
 #ifndef COMPONENTS_PRINTING_COMMON_PRINT_MESSAGES_H_
 #error "Failed to include header components/printing/common/print_messages.h"
@@ -34,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ipc/param_traits_write_macros.h"
 namespace IPC {
 #undef COMPONENTS_PRINTING_COMMON_PRINT_MESSAGES_H_
+#undef COMPONENTS_PRINTING_COMMON_PRINTING_PARAM_TRAITS_MACROS_H_
 #include "components/printing/common/print_messages.h"
 #ifndef COMPONENTS_PRINTING_COMMON_PRINT_MESSAGES_H_
 #error "Failed to include header components/printing/common/print_messages.h"
@@ -44,6 +53,7 @@ namespace IPC {
 #include "ipc/param_traits_read_macros.h"
 namespace IPC {
 #undef COMPONENTS_PRINTING_COMMON_PRINT_MESSAGES_H_
+#undef COMPONENTS_PRINTING_COMMON_PRINTING_PARAM_TRAITS_MACROS_H_
 #include "components/printing/common/print_messages.h"
 #ifndef COMPONENTS_PRINTING_COMMON_PRINT_MESSAGES_H_
 #error "Failed to include header components/printing/common/print_messages.h"
@@ -54,6 +64,9 @@ namespace IPC {
 #include "ipc/param_traits_log_macros.h"
 namespace IPC {
 #undef COMPONENTS_PRINTING_COMMON_PRINT_MESSAGES_H_
+// Force multiple inclusion of the param traits file to generate all methods.
+#undef COMPONENTS_PRINTING_COMMON_PRINTING_PARAM_TRAITS_MACROS_H_
+
 #include "components/printing/common/print_messages.h"
 #ifndef COMPONENTS_PRINTING_COMMON_PRINT_MESSAGES_H_
 #error "Failed to include header components/printing/common/print_messages.h"
--- components/printing/common/print_messages.h
+++ components/printing/common/print_messages.h
@@ -29,9 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/ipc/skia/gfx_skia_param_traits.h"
 #include "ui/gfx/native_widget_types.h"
 
-// Force multiple inclusion of the param traits file to generate all methods.
-#undef COMPONENTS_PRINTING_COMMON_PRINTING_PARAM_TRAITS_MACROS_H_
-
 #ifndef INTERNAL_COMPONENTS_PRINTING_COMMON_PRINT_MESSAGES_H_
 #define INTERNAL_COMPONENTS_PRINTING_COMMON_PRINT_MESSAGES_H_
 
