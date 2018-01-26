b3fdcaa2167779ac811dce0b92c4d2b4ab3cea73
Steve Kobes <skobes@chromium.org>
2018-01-26 20:06:55 +0000

    Update WebFrame::VisibleContentRect for RLS.
    TESTED=RenderViewImplTest.PreferredSizeZoomed passes with RLS
    
    Bug: 785088
    Change-Id: Ibce3e168a7b777b7ee4a51593177d6cbc162ec87
    Reviewed-on: https://chromium-review.googlesource.com/886837
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532048}

--- third_party/WebKit/Source/core/frame/WebLocalFrameImpl.cpp
+++ third_party/WebKit/Source/core/frame/WebLocalFrameImpl.cpp
@@ -616,7 +616,7 @@ bool WebLocalFrameImpl::HasVisibleContent() const {
 
 WebRect WebLocalFrameImpl::VisibleContentRect() const {
   if (LocalFrameView* view = GetFrameView())
-    return view->VisibleContentRect();
+    return view->LayoutViewportScrollableArea()->VisibleContentRect();
   return WebRect();
 }
 
