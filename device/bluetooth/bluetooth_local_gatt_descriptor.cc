b6774190afa2a91dc67213b881c4a9a96f5a32ed
Albert Chaulk <achaulk@google.com>
2018-01-26 20:07:59 +0000

    Fix is_linux && !use_dbus build
    Currently there is an implicit assumption that certain dbus files
    are built if is_linux=true which causes link errors if they aren't.
    This changes the ifdef condition to also build stubs in this case.
    
    Bug: b/72507234
    Test: Local build
    Change-Id: Ie1857a0400eb2bfbd4420a1c2402fa2d95089b8e
    Reviewed-on: https://chromium-review.googlesource.com/871259
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Commit-Queue: Albert Chaulk <achaulk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532049}

--- device/bluetooth/bluetooth_local_gatt_descriptor.cc
+++ device/bluetooth/bluetooth_local_gatt_descriptor.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace device {
 
-#if !defined(OS_LINUX)
+#if !defined(OS_LINUX) || defined(LINUX_WITHOUT_DBUS)
 // static
 base::WeakPtr<BluetoothLocalGattDescriptor>
 BluetoothLocalGattDescriptor::Create(
--- device/bluetooth/bluetooth_local_gatt_service.cc
+++ device/bluetooth/bluetooth_local_gatt_service.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace device {
 
-#if !defined(OS_LINUX)
+#if !defined(OS_LINUX) || defined(LINUX_WITHOUT_DBUS)
 // static
 base::WeakPtr<BluetoothLocalGattService> BluetoothLocalGattService::Create(
     BluetoothAdapter* adapter,
