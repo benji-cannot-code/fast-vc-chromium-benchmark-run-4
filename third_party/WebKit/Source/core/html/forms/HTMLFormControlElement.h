b35f0d39565290530d5adf905d7f19af3f3edc27
Ryan Landay <rlanday@chromium.org>
2018-01-26 18:12:11 +0000

    Add support for inheriting autocapitalize attribute from form owner
    This CL adds support for inheriting the autocapitalize attribute from a form
    owner as described in the new spec for autocapitalize:
    https://html.spec.whatwg.org/multipage/interaction.html#autocapitalization
    
    This CL also adds tests for this new behavior.
    
    Bug: 776618
    Change-Id: Ib96354fb320736255369bd2df3113e172dfdb42e
    Reviewed-on: https://chromium-review.googlesource.com/872090
    Reviewed-by: Changwan Ryu <changwan@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Ryan Landay <rlanday@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532020}

--- third_party/WebKit/Source/core/html/forms/HTMLFormControlElement.h
+++ third_party/WebKit/Source/core/html/forms/HTMLFormControlElement.h
@@ -126,6 +126,8 @@ class CORE_EXPORT HTMLFormControlElement : public LabelableElement,
   bool IsAutofilled() const { return is_autofilled_; }
   void SetAutofilled(bool = true);
 
+  const AtomicString& autocapitalize() const final;
+
   static const HTMLFormControlElement* EnclosingFormControlElement(const Node*);
 
   String NameForAutofill() const;
