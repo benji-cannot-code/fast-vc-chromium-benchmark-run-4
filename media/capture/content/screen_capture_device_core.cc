a6e84f2740c7a392c12bbd3455d7551981733304
Thomas Anderson <thomasanderson@chromium.org>
2018-01-27 19:54:48 +0000

    Revert "FrameSinkVideoCapturer: Fix refresh logic for damaged sources."
    This reverts commit 93f7aa878977c00c2bfebb70b7c6f2ff73dcdee0.
    
    Reason for revert: Causes failure in WebContentsVideoCaptureDeviceBrowserTest on Linux Tests (dbg)(1)(32):
    https://ci.chromium.org/buildbot/chromium.linux/Linux%20Tests%20%28dbg%29%281%29%2832%29/47651
    
    Original change's description:
    > FrameSinkVideoCapturer: Fix refresh logic for damaged sources.
    > 
    > Fixes a couple of issues where frame capture becomes frozen or choppy,
    > with the following changes:
    > 
    > 1. Move the "source dirty" logic out of VideoCaptureOracle and into
    > FrameSinkVideoCapturerImpl.
    > 
    > 2. More-agressive response to frame refresh requests (needed for
    > improved mouse cursor update response).
    > 
    > 3. Remove the distinction between passive and active refreshes from
    > VideoCaptureOracle. FrameSinkVideoCapturer now tracks source damage, and
    > can execute any refresh passively when there is no damage.
    > 
    > 4. Add logic to ensure that FrameSinkVideoCapturerImpl provides
    > consumers with a refresh frame whenever key events occur; such as params
    > changes, capture target changes, etc.; that are known to require a full
    > update.
    > 
    > 5. Re-enable the content_browsertests that were disabled due to
    > flakiness.
    > 
    > Bug: 785072, 754872
    > Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel
    > Change-Id: I402f3f928de61e6754e04014327421e4f6917d5f
    > Reviewed-on: https://chromium-review.googlesource.com/884996
    > Reviewed-by: Yuri Wiitala <miu@chromium.org>
    > Reviewed-by: Xiangjun Zhang <xjz@chromium.org>
    > Reviewed-by: kylechar <kylechar@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#532161}
    
    TBR=miu@chromium.org,kylechar@chromium.org,xjz@chromium.org
    
    Change-Id: I32fd2fb0bf1e1dc32c026198809d12ff2ecc69f1
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 785072, 754872
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Reviewed-on: https://chromium-review.googlesource.com/890380
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532247}

--- media/capture/content/screen_capture_device_core.cc
+++ media/capture/content/screen_capture_device_core.cc
@@ -71,6 +71,18 @@ void ScreenCaptureDeviceCore::RequestRefreshFrame() {
   if (state_ != kCapturing)
     return;
 
+  // Try to use the less resource-intensive "passive" refresh mechanism, unless
+  // this is the first refresh following a Resume().
+  if (force_active_refresh_once_) {
+    capture_machine_->MaybeCaptureForRefresh();
+    force_active_refresh_once_ = false;
+    return;
+  }
+
+  // Make a best-effort attempt at a passive refresh, but fall-back to an active
+  // refresh if that fails.
+  if (oracle_proxy_->AttemptPassiveRefresh())
+    return;
   capture_machine_->MaybeCaptureForRefresh();
 }
 
@@ -91,6 +103,7 @@ void ScreenCaptureDeviceCore::Resume() {
   if (state_ != kSuspended)
     return;
 
+  force_active_refresh_once_ = true;
   TransitionStateTo(kCapturing);
 
   capture_machine_->Resume();
@@ -128,7 +141,9 @@ void ScreenCaptureDeviceCore::CaptureStarted(bool success) {
 
 ScreenCaptureDeviceCore::ScreenCaptureDeviceCore(
     std::unique_ptr<VideoCaptureMachine> capture_machine)
-    : state_(kIdle), capture_machine_(std::move(capture_machine)) {
+    : state_(kIdle),
+      capture_machine_(std::move(capture_machine)),
+      force_active_refresh_once_(false) {
   DCHECK(capture_machine_.get());
 }
 
