a8198f052b97196e6cab383054c0b553a750273e
Jialiu Lin <jialiul@chromium.org>
2018-01-27 02:39:59 +0000

    Add PasswordProtectionWarningTrigger and P~P~RiskTrigger policies for PhishGuard
    Add 2 policy settings for PhishGuard.
    PasswordProtectionWarningTrigger controls if we show the model dialog warning.
    PasswordProtectionRiskTrigger controls if we do event logging for at-risk account.
    
    If there is no policy for these for settings, then PhishGuard works the
    same way as for non-enterprise users. With the exception that, for
    GSUITE users, PhishGuard warning and at-risk account flagging is off by
    default.
    
    Bug: 804490
    Change-Id: I6ff1cc71222217c168ca0bbebf36dac4be946a43
    Reviewed-on: https://chromium-review.googlesource.com/879429
    Commit-Queue: Jialiu Lin <jialiul@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Bartosz Fabianowski <bartfab@chromium.org>
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Luke Z <lpz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532132}

--- chrome/browser/safe_browsing/chrome_password_protection_service.cc
+++ chrome/browser/safe_browsing/chrome_password_protection_service.cc
@@ -386,7 +386,7 @@ void ChromePasswordProtectionService::MaybeLogPasswordReuseDetectedEvent(
     content::WebContents* web_contents) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
-  if (!base::FeatureList::IsEnabled(kGaiaPasswordReuseReporting))
+  if (!IsEventLoggingEnabled())
     return;
 
   syncer::UserEventService* user_event_service =
@@ -425,7 +425,7 @@ void ChromePasswordProtectionService::LogPasswordReuseDialogInteraction(
     PasswordReuseDialogInteraction::InteractionResult interaction_result) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
-  if (!base::FeatureList::IsEnabled(kGaiaPasswordReuseReporting))
+  if (!IsEventLoggingEnabled())
     return;
 
   syncer::UserEventService* user_event_service =
@@ -446,7 +446,7 @@ void ChromePasswordProtectionService::LogPasswordReuseDialogInteraction(
 }
 
 LoginReputationClientRequest::PasswordReuseEvent::SyncAccountType
-ChromePasswordProtectionService::GetSyncAccountType() {
+ChromePasswordProtectionService::GetSyncAccountType() const {
   const AccountInfo account_info = GetAccountInfo();
   if (account_info.account_id.empty() || account_info.hosted_domain.empty()) {
     return LoginReputationClientRequest::PasswordReuseEvent::NOT_SIGNED_IN;
@@ -530,7 +530,7 @@ void ChromePasswordProtectionService::MaybeLogPasswordReuseLookupEvent(
     content::WebContents* web_contents,
     PasswordProtectionService::RequestOutcome outcome,
     const LoginReputationClientResponse* response) {
-  if (!base::FeatureList::IsEnabled(kGaiaPasswordReuseReporting))
+  if (!IsEventLoggingEnabled())
     return;
 
   switch (outcome) {
@@ -674,7 +674,7 @@ bool ChromePasswordProtectionService::UserClickedThroughSBInterstitial(
          current_threat_type == SB_THREAT_TYPE_URL_CLIENT_SIDE_MALWARE;
 }
 
-AccountInfo ChromePasswordProtectionService::GetAccountInfo() {
+AccountInfo ChromePasswordProtectionService::GetAccountInfo() const {
   SigninManagerBase* signin_manager =
       SigninManagerFactory::GetForProfileIfExists(profile_);
 
@@ -804,7 +804,7 @@ ChromePasswordProtectionService::ChromePasswordProtectionService(
 
 std::unique_ptr<PasswordProtectionNavigationThrottle>
 MaybeCreateNavigationThrottle(content::NavigationHandle* navigation_handle) {
-  if (!base::FeatureList::IsEnabled(kGaiaPasswordReuseReporting))
+  if (!base::FeatureList::IsEnabled(kGoogleBrandedPhishingWarning))
     return nullptr;
   Profile* profile = Profile::FromBrowserContext(
       navigation_handle->GetWebContents()->GetBrowserContext());
@@ -813,6 +813,22 @@ MaybeCreateNavigationThrottle(content::NavigationHandle* navigation_handle) {
   // |service| can be null in tests.
   return service ? service->MaybeCreateNavigationThrottle(navigation_handle)
                  : nullptr;
+}
+
+PasswordProtectionTrigger
+ChromePasswordProtectionService::GetPasswordProtectionTriggerPref(
+    const std::string& pref_name) const {
+  const PrefService::Preference* warning_trigger =
+      profile_->GetPrefs()->FindPreference(pref_name);
+  bool is_policy_managed =
+      (warning_trigger && warning_trigger->IsManaged()) ||
+      GetSyncAccountType() ==
+          LoginReputationClientRequest::PasswordReuseEvent::GSUITE;
+  // TODO(jialiul): Remove the special treatment for GSUITE, when password
+  // protection enterprise control is ready.
+  return is_policy_managed ? static_cast<PasswordProtectionTrigger>(
+                                 profile_->GetPrefs()->GetInteger(pref_name))
+                           : PHISHING_REUSE;
 }
 
 }  // namespace safe_browsing
