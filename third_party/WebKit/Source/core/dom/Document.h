102121e036c1609942b304f088825ac621249ec2
Kent Tamura <tkent@chromium.org>
2018-01-27 02:44:37 +0000

    Make a separated header file for blink::CreateElementFlags
    We're going to use blink::CreateElementFlags in more header files, and
    would like to avoid to include huge Document.h.
    
    This CL has no behavior changes.
    
    Bug: 805280
    Change-Id: I64c89995cda3f76f1e75b6cf20af05f0e8eb2e24
    Reviewed-on: https://chromium-review.googlesource.com/888191
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532134}

--- third_party/WebKit/Source/core/dom/Document.h
+++ third_party/WebKit/Source/core/dom/Document.h
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/CoreExport.h"
 #include "core/animation/WorkletAnimationController.h"
 #include "core/dom/ContainerNode.h"
+#include "core/dom/CreateElementFlags.h"
 #include "core/dom/DocumentEncodingData.h"
 #include "core/dom/DocumentInit.h"
 #include "core/dom/DocumentLifecycle.h"
@@ -217,25 +218,6 @@ enum ShadowCascadeOrder {
   kShadowCascadeNone,
   kShadowCascadeV0,
   kShadowCascadeV1
-};
-
-enum CreateElementFlags {
-  kCreatedByParser = 1 << 0,
-  // Synchronous custom elements flag:
-  // https://dom.spec.whatwg.org/#concept-create-element
-  // TODO(kojii): Remove these flags, add an option not to queue upgrade, and
-  // let parser/DOM methods to upgrade synchronously when necessary.
-  kSynchronousCustomElements = 0 << 1,
-  kAsynchronousCustomElements = 1 << 1,
-
-  // Aliases by callers.
-  // Clone a node: https://dom.spec.whatwg.org/#concept-node-clone
-  kCreatedByCloneNode = kAsynchronousCustomElements,
-  kCreatedByImportNode = kCreatedByCloneNode,
-  // https://dom.spec.whatwg.org/#dom-document-createelement
-  kCreatedByCreateElement = kSynchronousCustomElements,
-  // https://html.spec.whatwg.org/#create-an-element-for-the-token
-  kCreatedByFragmentParser = kCreatedByParser | kAsynchronousCustomElements,
 };
 
 // Collect data about deferred loading of offscreen cross-origin documents. All
