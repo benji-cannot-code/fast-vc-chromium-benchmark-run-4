124742a3330e8b888b027654e841cf0691428dbc
Nico Weber <thakis@chromium.org>
2018-01-27 01:03:10 +0000

    Work on getting things built with -Wimplicit-fallthrough. This CL was uploaded by git cl split.
    Bug: 177475
    R=estade@chromium.org
    
    Change-Id: I4f3b6133bb2c4ea46f3db57482c96351bdb3e551
    Reviewed-on: https://chromium-review.googlesource.com/889763
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532103}

--- ui/native_theme/native_theme_aura.cc
+++ ui/native_theme/native_theme_aura.cc
@@ -149,7 +149,7 @@ void NativeThemeAura::PaintArrowButton(cc::PaintCanvas* canvas,
       break;
     case kHovered:
       bg_color = SkColorSetRGB(0xD2, 0xD2, 0xD2);
-    // Fall through.
+      FALLTHROUGH;
     case kNormal:
       arrow_color = SkColorSetRGB(0x50, 0x50, 0x50);
       break;
