7f9af4fe83d02d2efd9b0c0aebd15f9f04fb962c
Nico Weber <thakis@chromium.org>
2018-01-27 16:29:59 +0000

    Work more on getting things built with -Wimplicit-fallthrough.
    This CL was uploaded by git cl split.
    
    R=svillar@igalia.com
    
    Bug: 177475
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Ie848e8562e5cfac15458e83142bee69df76c4c88
    Reviewed-on: https://chromium-review.googlesource.com/889727
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532226}

--- third_party/WebKit/Source/core/svg/SVGPathByteStreamSource.cpp
+++ third_party/WebKit/Source/core/svg/SVGPathByteStreamSource.cpp
@@ -31,11 +31,11 @@ PathSegmentData SVGPathByteStreamSource::ParseSegment() {
     case kPathSegCurveToCubicRel:
     case kPathSegCurveToCubicAbs:
       segment.point1 = ReadFloatPoint();
-    /* fall through */
+      FALLTHROUGH;
     case kPathSegCurveToCubicSmoothRel:
     case kPathSegCurveToCubicSmoothAbs:
       segment.point2 = ReadFloatPoint();
-    /* fall through */
+      FALLTHROUGH;
     case kPathSegMoveToRel:
     case kPathSegMoveToAbs:
     case kPathSegLineToRel:
