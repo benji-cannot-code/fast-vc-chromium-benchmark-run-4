fec5374716d58116b120a4375e88e8c6bf9eacdf
Ian Vollick <vollick@chromium.org>
2018-01-27 18:30:47 +0000

    [vr] Use alternate colors for environments with gradients
    This CL uses base::Versions built-in comparison logic rather than
    my incorrect, hand-rolled code. It also swaps out the color scheme
    when the component has a gradient.
    
    Also reduces the number of stars used with these environments.
    
    Bug: 806452,806471
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_vr;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I250692105e680e1f3d0fee372f6886f35f2ced95
    Reviewed-on: https://chromium-review.googlesource.com/889842
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Christopher Grant <cjgrant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532240}

--- chrome/browser/vr/elements/environment/stars.cc
+++ chrome/browser/vr/elements/environment/stars.cc
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace vr {
 
 namespace {
-constexpr size_t kNumStars = 2000lu;
+constexpr size_t kNumStars = 500lu;
 
 // Position & opacity.
 constexpr size_t kFloatsPerStarVertex = 5lu;
--- chrome/browser/vr/model/color_scheme.h
+++ chrome/browser/vr/model/color_scheme.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_VR_MODEL_COLOR_SCHEME_H_
 #define CHROME_BROWSER_VR_MODEL_COLOR_SCHEME_H_
 
+#include "base/version.h"
 #include "third_party/skia/include/core/SkColor.h"
 
 namespace vr {
@@ -44,6 +45,7 @@ struct ColorScheme {
   };
 
   static const ColorScheme& GetColorScheme(Mode mode);
+  static void UpdateForComponent(const base::Version& component_version);
 
   ColorScheme();
   ColorScheme(const ColorScheme& other);
