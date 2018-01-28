38c92b9780775a4224939747783b9d7e7993ad4b
Aleks Totic <atotic@chromium.org>
2018-01-28 08:09:23 +0000

    LayoutNG inline outline initial implementation
    I think that this patch is a good starting point for outline implementation.
    It implements a basic outline painting algorithm:
    - Outlines are painted by containing self painting fragment.
    - Outlines are computed as:
       traverse fragment's descendants
       combine outlines for fragments with common layout objects
       paint combined outlines, using first fragment as DisplayItem
    
    There are many problems:
    - All descendant fragments are traversed for each outline paint, even if
      there are no outlines to be painted.
      Fix: detect outline existence while descendants are being painted,
      and only call this code if there are outlines.
    
    There are 119 tests in our test suite that trigger inline outline paint code.
    With this patch, we get 4 regressions:
    fast/history/visited-link-hover-outline-color.html
    paint/invalidation/overflow/inline-block-overflow-repaint.html
    paint/invalidation/overflow/inline-box-overflow-repaint.html
      cause: improper invalidation
    fast/inline/nested-text-descendants.html
      cause: inline text position
    
    15 additional tests pass.
    
    Bug: 714962
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_layout_ng;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Icccd225abac7e38fd9a871ee2914ff8d235474e3
    Reviewed-on: https://chromium-review.googlesource.com/887633
    Commit-Queue: Aleks Totic <atotic@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532270}

--- third_party/WebKit/Source/core/paint/ng/ng_fragment_painter.h
+++ third_party/WebKit/Source/core/paint/ng/ng_fragment_painter.h
@@ -7,10 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ng_fragment_painter_h
 
 #include "core/paint/ObjectPainterBase.h"
-#include "platform/wtf/Allocator.h"
+#include "platform/wtf/HashMap.h"
 
 namespace blink {
 
+class LayoutObject;
 class LayoutPoint;
 class NGPaintFragment;
 struct PaintInfo;
@@ -26,7 +27,15 @@ class NGFragmentPainter : public ObjectPainterBase {
 
   void PaintOutline(const PaintInfo&, const LayoutPoint& paint_offset);
 
+  void PaintDescendantOutlines(const PaintInfo&,
+                               const LayoutPoint& paint_offset);
+
  private:
+  void CollectDescendantOutlines(
+      const LayoutPoint& paint_offset,
+      HashMap<const LayoutObject*, NGPaintFragment*>* anchor_fragment_map,
+      HashMap<const LayoutObject*, Vector<LayoutRect>>* outline_rect_map);
+
   const NGPaintFragment& paint_fragment_;
 };
 
