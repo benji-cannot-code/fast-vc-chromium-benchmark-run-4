21e13050c8957436b1f1c9a4e6a5b481fd327451
Adam Langley <agl@chromium.org>
2018-01-28 21:23:36 +0000

    webauthn: support individual attestation signals.
    In the U2F code we have supported an individual attestation signal to
    tokens based on a magic AppID. Since we hate that sort of special
    casing, it is being transitioned to using an enterprise policy to
    enumerate AppIDs and RP IDs which should get this treatment.
    
    This change adds the policy-based behaviour to webauthn so that we can,
    hopefully, eventually discontinue the use of the U2F API.
    
    Bug: none
    Change-Id: Ifc68792c91947f9233e1be50477f338e7cb4bb6c
    Reviewed-on: https://chromium-review.googlesource.com/876803
    Commit-Queue: Adam Langley <agl@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Kim Paulhamus <kpaulhamus@chromium.org>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532288}

--- device/u2f/u2f_register.h
+++ device/u2f/u2f_register.h
@@ -30,6 +30,7 @@ class U2fRegister : public U2fRequest {
               const std::vector<std::vector<uint8_t>>& registered_keys,
               const std::vector<uint8_t>& challenge_hash,
               const std::vector<uint8_t>& app_param,
+              bool individual_attestation_ok,
               RegisterResponseCallback completion_callback);
   ~U2fRegister() override;
 
@@ -39,6 +40,7 @@ class U2fRegister : public U2fRequest {
       const std::vector<std::vector<uint8_t>>& registered_keys,
       const std::vector<uint8_t>& challenge_hash,
       const std::vector<uint8_t>& app_param,
+      bool individual_attestation_ok,
       RegisterResponseCallback completion_callback);
 
  private:
@@ -64,6 +66,9 @@ class U2fRegister : public U2fRequest {
   const std::vector<std::vector<uint8_t>> registered_keys_;
   std::vector<uint8_t> challenge_hash_;
   std::vector<uint8_t> app_param_;
+  // Indicates whether the token should be signaled that using an individual
+  // attestation certificate is acceptable.
+  const bool individual_attestation_ok_;
   RegisterResponseCallback completion_callback_;
 
   // List of authenticators that did not create any of the key handles in the
