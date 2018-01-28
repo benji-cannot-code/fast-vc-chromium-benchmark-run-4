f9092ec4679fc0000abc0909bfd73886c16496fe
Peter Boström <pbos@chromium.org>
2018-01-28 20:24:26 +0000

    Update style of empty manage-passwords dialog.
    Replaces the dialog showing "Saved passwords for this site:" as title
    alongside "No passwords saved." in the body. Instead this now shows only
    "No passwords saved for this site" as title.
    
    Bug: chromium:654115
    Change-Id: Id8c340991fd22a5406112788ddf24e04b79487b5
    Reviewed-on: https://chromium-review.googlesource.com/847908
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532287}

--- chrome/browser/ui/passwords/manage_passwords_view_utils.cc
+++ chrome/browser/ui/passwords/manage_passwords_view_utils.cc
@@ -134,6 +134,7 @@ void GetSavePasswordDialogTitleTextAndLinkRange(
 
 void GetManagePasswordsDialogTitleText(const GURL& user_visible_url,
                                        const GURL& password_origin_url,
+                                       bool has_credentials,
                                        base::string16* title) {
   DCHECK(!password_manager::IsValidAndroidFacetURI(password_origin_url.spec()));
   // Check whether the registry controlled domains for user-visible URL
@@ -143,9 +144,14 @@ void GetManagePasswordsDialogTitleText(const GURL& user_visible_url,
     base::string16 formatted_url =
         url_formatter::FormatUrlForSecurityDisplay(password_origin_url);
     *title = l10n_util::GetStringFUTF16(
-        IDS_MANAGE_PASSWORDS_TITLE_DIFFERENT_DOMAIN, formatted_url);
+        has_credentials
+            ? IDS_MANAGE_PASSWORDS_DIFFERENT_DOMAIN_TITLE
+            : IDS_MANAGE_PASSWORDS_DIFFERENT_DOMAIN_NO_PASSWORDS_TITLE,
+        formatted_url);
   } else {
-    *title = l10n_util::GetStringUTF16(IDS_MANAGE_PASSWORDS_TITLE);
+    *title = l10n_util::GetStringUTF16(
+        has_credentials ? IDS_MANAGE_PASSWORDS_TITLE
+                        : IDS_MANAGE_PASSWORDS_NO_PASSWORDS_TITLE);
   }
 }
 
