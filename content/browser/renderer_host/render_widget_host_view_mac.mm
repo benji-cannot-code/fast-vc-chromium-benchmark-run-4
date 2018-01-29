b3101f4224a5d936183e9e479cfb4f48432eac2c
Christopher Cameron <ccameron@chromium.org>
2018-01-29 08:39:12 +0000

    viz/mac: Merge surface sync and non surface sync behavior
    On Mac, the browser's ui::Compositor is always the same size
    as the renderer's ui::Compositor.
    
    In non-viz and non-surface-sync mode, this is done by resizing
    the ui::Compositor to match the renderer frame size at
    SubmitCompositorFrame.
    
    This isn't suitable anymore, since SubmitCompositorFrame is
    going away.
    
    Change this so that the ui::Compositor is resized to match the
    surface size, at OnFirstSurfaceActivation. Add a method to
    DelegatedFrameHostClient to plumb this signal through to the
    BrowserCompositorViewMac. Also rename WasResized to
    OnNSViewWasResized, to be more clear.
    
    This mechanism works with and without surface synchronization
    enabled.
    
    Bug: 805774
    Change-Id: I7a85b2e4eba8c962ef1c67446b38425637fdeb62
    Reviewed-on: https://chromium-review.googlesource.com/887966
    Commit-Queue: ccameron <ccameron@chromium.org>
    Reviewed-by: Fady Samuel <fsamuel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532351}

--- content/browser/renderer_host/render_widget_host_view_mac.mm
+++ content/browser/renderer_host/render_widget_host_view_mac.mm
@@ -1090,7 +1090,7 @@ void RenderWidgetHostViewMac::SetTooltipText(
 }
 
 void RenderWidgetHostViewMac::OnSynchronizedDisplayPropertiesChanged() {
-  browser_compositor_->WasResized();
+  browser_compositor_->OnNSViewWasResized();
 }
 
 void RenderWidgetHostViewMac::DidNavigate() {
@@ -1446,8 +1446,10 @@ void RenderWidgetHostViewMac::SubmitCompositorFrame(
 
   page_at_minimum_scale_ =
       frame.metadata.page_scale_factor == frame.metadata.min_page_scale_factor;
-  browser_compositor_->SubmitCompositorFrame(local_surface_id,
-                                             std::move(frame));
+
+  browser_compositor_->GetDelegatedFrameHost()->SubmitCompositorFrame(
+      local_surface_id, std::move(frame), nullptr);
+
   UpdateDisplayVSyncParameters();
 }
 
@@ -2853,8 +2855,12 @@ Class GetRenderWidgetHostViewCocoaClassForTesting() {
     renderWidgetHostView_->render_widget_host_->delegate()->SendScreenRects();
   else
     renderWidgetHostView_->render_widget_host_->SendScreenRects();
+
+  // RenderWidgetHostImpl will query BrowserCompositorMac for the dimensions
+  // to send to the renderer, so it is required that BrowserCompositorMac be
+  // updated first.
+  renderWidgetHostView_->browser_compositor_->OnNSViewWasResized();
   renderWidgetHostView_->render_widget_host_->WasResized();
-  renderWidgetHostView_->browser_compositor_->WasResized();
 
   // Wait for the frame that WasResize might have requested. If the view is
   // being made visible at a new size, then this call will have no effect
