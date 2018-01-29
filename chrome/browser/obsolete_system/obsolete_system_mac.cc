521bed78565037d362edcc62f729fd21025d9969
Elly Fong-Jones <ellyjones@chromium.org>
2018-01-29 13:49:55 +0000

    mac: show obsolete system infobar on 10.9
    This change:
    1) Resurrects the 10.6/10.7/10.8 deprecation strings and updates them to 10.9;
    2) Adds a feature "Enable109ObsoleteInfobar";
    3) Shows the obsolete system infobar on 10.9 systems when that feature is
       enabled;
    4) Adds a *placeholder* chrome::kMac10_9_ObsoleteURL link for the "learn more"
       link in the infobar.
    
    Bug: 806071
    Change-Id: I48ab95e50fd2be7438650bc7f7cbb35fd2c2b02b
    Reviewed-on: https://chromium-review.googlesource.com/889020
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532411}

--- chrome/browser/obsolete_system/obsolete_system_mac.cc
+++ chrome/browser/obsolete_system/obsolete_system_mac.cc
@@ -5,14 +5,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/obsolete_system/obsolete_system.h"
 
+#include "base/mac/mac_util.h"
+#include "chrome/common/chrome_features.h"
+#include "chrome/common/url_constants.h"
+#include "chrome/grit/chromium_strings.h"
+#include "ui/base/l10n/l10n_util.h"
+
 // static
 bool ObsoleteSystem::IsObsoleteNowOrSoon() {
-  return false;
+  return base::mac::IsOS10_9() &&
+         base::FeatureList::IsEnabled(features::kShow10_9ObsoleteInfobar);
 }
 
 // static
 base::string16 ObsoleteSystem::LocalizedObsoleteString() {
-  return base::string16();
+  return l10n_util::GetStringUTF16(IDS_MAC_10_9_OBSOLETE_NOW);
 }
 
 // static
@@ -22,5 +29,5 @@ bool ObsoleteSystem::IsEndOfTheLine() {
 
 // static
 const char* ObsoleteSystem::GetLinkURL() {
-  return "";
+  return chrome::kMac10_9_ObsoleteURL;
 }
