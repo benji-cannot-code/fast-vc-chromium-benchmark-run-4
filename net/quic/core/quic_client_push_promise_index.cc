8fd5b64f2a75e82dc7a78b1ba40846027fab578b
Makoto Shimazu <shimazu@chromium.org>
2018-01-29 04:33:17 +0000

    Revert "Make PUSH_PROMISE headers URL-parsing more robust and conform to HTTP/2 spec."
    This reverts commit 734de9630d8fd39ed515f95f692ad1671097d6c5.
    
    Reason for revert: 
    This looks causing a failure in MSan bots.
    See also https://crbug.com/806634.
    
    Original change's description:
    > Make PUSH_PROMISE headers URL-parsing more robust and conform to HTTP/2 spec.
    > 
    > Change-Id: If94561f453bbc56b86238918d374baf8c2254646
    > Reviewed-on: https://chromium-review.googlesource.com/887970
    > Reviewed-by: Ryan Hamilton <rch@chromium.org>
    > Commit-Queue: Yixin Wang <wangyix@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#532202}
    
    TBR=rch@chromium.org,wangyix@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Change-Id: I210f101bfa5c43a238f2a21e8a9c513731c877dd
    Reviewed-on: https://chromium-review.googlesource.com/890838
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Commit-Queue: Makoto Shimazu <shimazu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532321}

--- net/quic/core/quic_client_push_promise_index.cc
+++ net/quic/core/quic_client_push_promise_index.cc
@@ -34,7 +34,7 @@ QuicAsyncStatus QuicClientPushPromiseIndex::Try(
     const SpdyHeaderBlock& request,
     QuicClientPushPromiseIndex::Delegate* delegate,
     TryHandle** handle) {
-  string url(SpdyUtils::GetPromisedUrlFromHeaderBlock(request));
+  string url(SpdyUtils::GetUrlFromHeaderBlock(request));
   QuicPromisedByUrlMap::iterator it = promised_by_url_.find(url);
   if (it != promised_by_url_.end()) {
     QuicClientPromisedInfo* promised = it->second;
