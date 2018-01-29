83d6cc3b64db82b3282cbeeab922d7d1ba3c16dd
Sandra Sun <sunyunjia@chromium.org>
2018-01-29 14:52:27 +0000

    Automate overscrollBehavior web-platform test.
    Bug: 762054
    Change-Id: If11cd4b3bf8cc24a97e4edf90a7dfbde983c0fb5
    Reviewed-on: https://chromium-review.googlesource.com/862971
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Commit-Queue: Sandra Sun <sunyunjia@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532426}

--- third_party/WebKit/LayoutTests/resources/testharnessreport.js
+++ third_party/WebKit/LayoutTests/resources/testharnessreport.js
@@ -106,12 +106,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             // Fullscreen tests all use the same automation script.
             src = automationPath + '/fullscreen/auto-click.js';
         } else if (
+            pathAndBase.startsWith('/css/') ||
             pathAndBase.startsWith('/pointerevents/') ||
             pathAndBase.startsWith('/uievents/') ||
             pathAndBase.startsWith('/pointerlock/') ||
             pathAndBase.startsWith('/html/') ||
             pathAndBase.startsWith('/input-events/') ||
-            pathAndBase.startsWith('/css/selectors/')) {
+            pathAndBase.startsWith('/css/selectors/') ||
+            pathAndBase.startsWith('/css/cssom-view/')) {
             // Per-test automation scripts.
             src = automationPath + pathAndBase + '-automation.js';
         } else {
