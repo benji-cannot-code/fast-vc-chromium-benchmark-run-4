e2d6e17a9eb19cb1fe4d2e32ab9ec520c7363fc6
David Benjamin <davidben@chromium.org>
2018-01-29 15:54:52 +0000

    Clear a few warnings in Cronet.
    I noticed these scroll by:
    
    ../../components/cronet/android/java/src/org/chromium/net/impl/CronetMetrics.java:203:
    warning: [NullablePrimitive] @Nullable should not be used for primitive types since they cannot be null
        @Nullable
        ^
        (see http://errorprone.info/bugpattern/NullablePrimitive)
      Did you mean to remove this line?
    
    ../../components/cronet/android/java/src/org/chromium/net/impl/JavaUrlRequest.java:848:
    warning: [ShortCircuitBoolean] Prefer the short-circuiting boolean operators && and || to & and |.
            return state == State.COMPLETE | state == State.ERROR | state == State.CANCELLED;
                                                                  ^
        (see http://errorprone.info/bugpattern/ShortCircuitBoolean)
      Did you mean 'return state == State.COMPLETE || state == State.ERROR || state == State.CANCELLED;'?
    
    ../../components/cronet/android/api/src/org/chromium/net/RequestFinishedInfo.java:215:
    warning: [NullablePrimitive] @Nullable should not be used for primitive types since they cannot be null
            @Nullable
            ^
        (see http://errorprone.info/bugpattern/NullablePrimitive)
      Did you mean to remove this line?
    
    Bug: none
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester
    Change-Id: Ie3aa3a6004fa5ed094f45c259522d3282023eb99
    Reviewed-on: https://chromium-review.googlesource.com/890361
    Reviewed-by: Miriam Gershenson <mgersh@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532443}

--- components/cronet/android/java/src/org/chromium/net/impl/CronetMetrics.java
+++ components/cronet/android/java/src/org/chromium/net/impl/CronetMetrics.java
@@ -201,7 +201,6 @@ public final class CronetMetrics extends RequestFinishedInfo.Metrics {
         return toDate(mRequestEndMs);
     }
 
-    @Nullable
     public boolean getSocketReused() {
         return mSocketReused;
     }
