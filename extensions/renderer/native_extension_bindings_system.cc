bbcb03cf913ac7bea7e568c161bbfa7b1351e706
Nico Weber <thakis@chromium.org>
2018-01-29 23:57:13 +0000

    Remaining changes to build all code in src.git with -Wimplicit-fallthrough.
    This CL was uploaded by git cl split.
    
    R=rdevlin.cronin@chromium.org
    
    Bug: 177475
    Change-Id: I30f6a677a1010ecd066fc06255aa0270e9f540fe
    Reviewed-on: https://chromium-review.googlesource.com/891479
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532670}

--- extensions/renderer/native_extension_bindings_system.cc
+++ extensions/renderer/native_extension_bindings_system.cc
@@ -435,7 +435,7 @@ void NativeExtensionBindingsSystem::UpdateBindingsForContext(
     case Feature::SERVICE_WORKER_CONTEXT:
       DCHECK(ExtensionsClient::Get()
                  ->ExtensionAPIEnabledInExtensionServiceWorkers());
-    // Intentional fallthrough.
+      FALLTHROUGH;
     case Feature::BLESSED_EXTENSION_CONTEXT:
     case Feature::LOCK_SCREEN_EXTENSION_CONTEXT:
     case Feature::UNBLESSED_EXTENSION_CONTEXT:
