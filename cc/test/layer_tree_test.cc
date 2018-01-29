8ce15250b08dfd333b009a1ae4246e1561ddb238
James Wallace-Lee <jamwalla@chromium.org>
2018-01-29 22:24:23 +0000

    WebView: Abort draw if no damage
    This CL adds the --cc-check-damage-early flag. If the flag is enabled, an
    additional call to HasDamage is made in LayerTreeHostImpl::WillBeginImplFrame.
    If that check returns false, we can abort the frame early, which on WebView
    prevents the compositor from needlessly drawing the frame.
    
    
    Bug: 687695
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: Iaae6749b40b09762b2ac581f784a7e6937d20229
    Reviewed-on: https://chromium-review.googlesource.com/767851
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: James Wallace-Lee <jamwalla@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532627}

--- cc/test/layer_tree_test.cc
+++ cc/test/layer_tree_test.cc
@@ -219,9 +219,10 @@ class LayerTreeHostImplForTesting : public LayerTreeHostImpl {
         this, raster_buffer_provider, resource_pool);
   }
 
-  void WillBeginImplFrame(const viz::BeginFrameArgs& args) override {
-    LayerTreeHostImpl::WillBeginImplFrame(args);
+  bool WillBeginImplFrame(const viz::BeginFrameArgs& args) override {
+    bool has_damage = LayerTreeHostImpl::WillBeginImplFrame(args);
     test_hooks_->WillBeginImplFrameOnThread(this, args);
+    return has_damage;
   }
 
   void DidFinishImplFrame() override {
@@ -362,6 +363,11 @@ class LayerTreeHostImplForTesting : public LayerTreeHostImpl {
   void InvalidateContentOnImplSide() override {
     LayerTreeHostImpl::InvalidateContentOnImplSide();
     test_hooks_->DidInvalidateContentOnImplSide(this);
+  }
+
+  void InvalidateLayerTreeFrameSink() override {
+    LayerTreeHostImpl::InvalidateLayerTreeFrameSink();
+    test_hooks_->DidInvalidateLayerTreeFrameSink(this);
   }
 
   void RequestImplSideInvalidationForCheckerImagedTiles() override {
