6b10de8c8b5f101e9653daf001905d35200c2fca
Meredith Lane <meredithl@google.com>
2018-01-29 02:34:46 +0000

    Implement int accessors for ComputedAccessibleNode
    Adds the attributes into the ComputedAccessibleNode interface, and
    accessors on the content tree for an AXNode with a given ID. This is the
    first of the accessors for the wider set of ComputedAccessibleNode's
    properties. The other types will be added in seperate CLs.
    
    This change also introduces an AOMIntAttribute enum, which is translated
    to an AXIntAttribute inside content to retrieve the corresponding
    accessible property. This will also be expanded to the other types in
    later CLs.
    
    BUG=792783
    
    Change-Id: I39875ae8eef03620b85c8480fd1ffc590c3591d5
    Reviewed-on: https://chromium-review.googlesource.com/869911
    Commit-Queue: Meredith Lane <meredithl@google.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Alice Boxhall <aboxhall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532306}

--- third_party/WebKit/public/platform/WebComputedAXTree.h
+++ third_party/WebKit/public/platform/WebComputedAXTree.h
@@ -10,6 +10,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+enum WebAOMIntAttribute {
+  AOM_INT_ATTRIBUTE_NONE,
+  AOM_ATTR_COLUMN_COUNT,
+  AOM_ATTR_COLUMN_INDEX,
+  AOM_ATTR_COLUMN_SPAN,
+  AOM_ATTR_HIERARCHICAL_LEVEL,
+  AOM_ATTR_POS_IN_SET,
+  AOM_ATTR_ROW_COUNT,
+  AOM_ATTR_ROW_INDEX,
+  AOM_ATTR_ROW_SPAN,
+  AOM_ATTR_SET_SIZE,
+};
+
 class WebComputedAXTree {
  public:
   virtual ~WebComputedAXTree() {}
@@ -17,6 +30,13 @@ class WebComputedAXTree {
   virtual bool ComputeAccessibilityTree() = 0;
   virtual WebString GetNameForAXNode(int32_t axID) = 0;
   virtual WebString GetRoleForAXNode(int32_t axID) = 0;
+
+  // Get the specified attribute for a given AXID, returning true if the
+  // node exists in the tree and contains the attribute. Stores the result in
+  // |out_param|.
+  virtual bool GetIntAttributeForAXNode(int32_t axID,
+                                        WebAOMIntAttribute,
+                                        int32_t* out_param) = 0;
 };
 
 }  // namespace blink
