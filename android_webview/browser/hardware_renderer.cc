2a2743bdf1598c0fb492ebb56cfb4207e0bc8afb
Mohsen Izadi <mohsen@chromium.org>
2018-01-29 21:03:39 +0000

    Improve CompositorFrameSinkSupport::SubmitCompositorFrame() interface
    There are two versions of this method available, one inherited from
    mojom::CompositorFrameSink interface and one that actually implements
    the functionality. Both are public and a bit confusing as to which
    should be used. After this change, callers should prefer to use the one
    that is inherited from mojom::CompositorFrameSink and call the other one
    only when they want to see if the call was successful or not. The first
    version DCHECK's on the success of the call.
    
    BUG=776154
    TEST=none
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I26a0a23f8e2ee807c3a2ea8a5ce7004ac2801c85
    Reviewed-on: https://chromium-review.googlesource.com/865475
    Reviewed-by: Fady Samuel <fsamuel@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Saman Sami <samans@chromium.org>
    Commit-Queue: Mohsen Izadi <mohsen@chromium.org>
    Commit-Queue: Fady Samuel <fsamuel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532586}

--- android_webview/browser/hardware_renderer.cc
+++ android_webview/browser/hardware_renderer.cc
@@ -136,9 +136,8 @@ void HardwareRenderer::DrawGL(AwDrawGLInfo* draw_info) {
       device_scale_factor_ = device_scale_factor;
     }
 
-    bool result = support_->SubmitCompositorFrame(
-        child_id_, std::move(*child_compositor_frame));
-    DCHECK(result);
+    support_->SubmitCompositorFrame(child_id_,
+                                    std::move(*child_compositor_frame));
   }
 
   gfx::Transform transform(gfx::Transform::kSkipInitialization);
