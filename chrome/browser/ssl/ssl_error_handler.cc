e51ffca3850a4836f594a0e3bd2a3be4c4e354ea
Nico Weber <thakis@chromium.org>
2018-01-29 21:56:23 +0000

    Remaining changes to build all code in src.git with -Wimplicit-fallthrough.
    This CL was uploaded by git cl split.
    
    R=agl@chromium.org
    
    Bug: 177475
    Change-Id: I222339d1ce16c92c54e3ad7ddbdd8b4e9cc92b5e
    Reviewed-on: https://chromium-review.googlesource.com/891920
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532608}

--- chrome/browser/ssl/ssl_error_handler.cc
+++ chrome/browser/ssl/ssl_error_handler.cc
@@ -881,6 +881,7 @@ void SSLErrorHandler::ShowDynamicInterstitial(
   switch (dynamic_interstitial.interstitial_type) {
     case chrome_browser_ssl::DynamicInterstitial::INTERSTITIAL_PAGE_NONE:
       NOTREACHED();
+      return;
     case chrome_browser_ssl::DynamicInterstitial::INTERSTITIAL_PAGE_SSL:
       delegate_->ShowSSLInterstitial(dynamic_interstitial.support_url);
       return;
