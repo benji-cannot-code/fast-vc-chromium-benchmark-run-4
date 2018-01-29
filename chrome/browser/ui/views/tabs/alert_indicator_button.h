0b6faeeb1cc63c1efb51435de339a5a00bb62a7b
Tommy Steimel <steimel@chromium.org>
2018-01-29 23:22:08 +0000

    Only show mute icon on tab when sound is playing
    This CL adds a check to WasRecentlyAudible before adding a mute icon to
    a tab to avoid always showing a mute icon when a tab is muted. This also
    adds the fade animation to the (mute icon)->(no icon) transition. Since
    adding the fade animation breaks a TabTest unit test (which worked under
    the assumption that each of its transitions was instantaneous), this CL
    also manually stops fade animations in that test.
    
    Bug: 785406
    Change-Id: I0d7fd732b605f634f9df59b736dd7f19c382a09e
    Reviewed-on: https://chromium-review.googlesource.com/889178
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532652}

--- chrome/browser/ui/views/tabs/alert_indicator_button.h
+++ chrome/browser/ui/views/tabs/alert_indicator_button.h
@@ -86,6 +86,7 @@ class AlertIndicatorButton : public views::ImageButton,
 
  private:
   friend class AlertIndicatorButtonTest;
+  friend class TabTest;
   class FadeAnimationDelegate;
 
   // Returns the tab (parent view) of this AlertIndicatorButton.
