9e8ae037bc8a7b80f77cbdbc26772b079032d9c7
Nico Weber <thakis@chromium.org>
2018-01-29 23:45:57 +0000

    Remaining changes to build all code in src.git with -Wimplicit-fallthrough.
    This CL was uploaded by git cl split.
    
    R=msw@chromium.org
    
    Bug: 177475
    Change-Id: I3aa4567f8f5ffd0b25369015475545918ecf48cd
    Reviewed-on: https://chromium-review.googlesource.com/891747
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532664}

--- chrome/browser/ui/browser_command_controller.cc
+++ chrome/browser/ui/browser_command_controller.cc
@@ -319,7 +319,7 @@ bool BrowserCommandController::ExecuteCommandWithDisposition(
       break;
     case IDC_RELOAD_CLEARING_CACHE:
       ClearCache(browser_);
-      // FALL THROUGH
+      FALLTHROUGH;
     case IDC_RELOAD_BYPASSING_CACHE:
       ReloadBypassingCache(browser_, disposition);
       break;
