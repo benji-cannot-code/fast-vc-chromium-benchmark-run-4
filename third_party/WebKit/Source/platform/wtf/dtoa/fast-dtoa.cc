f188934d5446b11826705951208630a46e533e65
Nico Weber <thakis@chromium.org>
2018-01-29 14:59:04 +0000

    Work on getting things built with -Wimplicit-fallthrough. This CL was uploaded by git cl split.
    Bug: 177475
    R=jochen@chromium.org
    
    Change-Id: Id6c4b989d652860b5bdbdb04ace4199ab1e1db0f
    Reviewed-on: https://chromium-review.googlesource.com/889840
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532428}

--- third_party/WebKit/Source/platform/wtf/dtoa/fast-dtoa.cc
+++ third_party/WebKit/Source/platform/wtf/dtoa/fast-dtoa.cc
@@ -251,7 +251,8 @@ static void BiggestPowerTen(uint32_t number,
         *power = kTen9;
         *exponent = 9;
         break;
-      }  // else fallthrough
+      }
+      FALLTHROUGH;
     case 29:
     case 28:
     case 27:
@@ -259,7 +260,8 @@ static void BiggestPowerTen(uint32_t number,
         *power = kTen8;
         *exponent = 8;
         break;
-      }  // else fallthrough
+      }
+      FALLTHROUGH;
     case 26:
     case 25:
     case 24:
@@ -267,7 +269,8 @@ static void BiggestPowerTen(uint32_t number,
         *power = kTen7;
         *exponent = 7;
         break;
-      }  // else fallthrough
+      }
+      FALLTHROUGH;
     case 23:
     case 22:
     case 21:
@@ -276,7 +279,8 @@ static void BiggestPowerTen(uint32_t number,
         *power = kTen6;
         *exponent = 6;
         break;
-      }  // else fallthrough
+      }
+      FALLTHROUGH;
     case 19:
     case 18:
     case 17:
@@ -284,7 +288,8 @@ static void BiggestPowerTen(uint32_t number,
         *power = kTen5;
         *exponent = 5;
         break;
-      }  // else fallthrough
+      }
+      FALLTHROUGH;
     case 16:
     case 15:
     case 14:
@@ -292,7 +297,8 @@ static void BiggestPowerTen(uint32_t number,
         *power = kTen4;
         *exponent = 4;
         break;
-      }  // else fallthrough
+      }
+      FALLTHROUGH;
     case 13:
     case 12:
     case 11:
@@ -301,7 +307,8 @@ static void BiggestPowerTen(uint32_t number,
         *power = 1000;
         *exponent = 3;
         break;
-      }  // else fallthrough
+      }
+      FALLTHROUGH;
     case 9:
     case 8:
     case 7:
@@ -309,7 +316,8 @@ static void BiggestPowerTen(uint32_t number,
         *power = 100;
         *exponent = 2;
         break;
-      }  // else fallthrough
+      }
+      FALLTHROUGH;
     case 6:
     case 5:
     case 4:
@@ -317,7 +325,8 @@ static void BiggestPowerTen(uint32_t number,
         *power = 10;
         *exponent = 1;
         break;
-      }  // else fallthrough
+      }
+      FALLTHROUGH;
     case 3:
     case 2:
     case 1:
@@ -325,7 +334,8 @@ static void BiggestPowerTen(uint32_t number,
         *power = 1;
         *exponent = 0;
         break;
-      }  // else fallthrough
+      }
+      FALLTHROUGH;
     case 0:
       *power = 0;
       *exponent = -1;
