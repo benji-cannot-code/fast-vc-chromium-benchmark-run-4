004b9b0bff9c64e835f451d5ddae4de231e9c7cc
Nico Weber <thakis@chromium.org>
2018-01-29 22:21:04 +0000

    Remaining changes to build all code in src.git with -Wimplicit-fallthrough.
    This CL was uploaded by git cl split.
    
    R=dgozman@chromium.org
    
    Bug: 177475
    Change-Id: If1482432b8a150d242c77c53f24f21a3b7cc8b6c
    Reviewed-on: https://chromium-review.googlesource.com/891881
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532621}

--- chrome/browser/devtools/devtools_window.cc
+++ chrome/browser/devtools/devtools_window.cc
@@ -1002,7 +1002,7 @@ GURL DevToolsWindow::GetDevToolsURL(Profile* profile,
       break;
     case kFrontendNode:
       url_string += "&nodeFrontend=true";
-    // Fall through
+      FALLTHROUGH;
     case kFrontendV8:
       url_string += "&v8only=true";
       break;
