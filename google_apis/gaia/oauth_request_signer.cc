28d2816367a4c0a24d32a569e25fce65837cb57c
Nico Weber <thakis@chromium.org>
2018-01-29 08:59:28 +0000

    Work on getting things built with -Wimplicit-fallthrough. This CL was uploaded by git cl split.
    Bug: 177475
    R=msarda@chromium.org
    
    Change-Id: Iab13eae6474e49f9890fc55f9ecf66b4ae11879d
    Reviewed-on: https://chromium-review.googlesource.com/889724
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532363}

--- google_apis/gaia/oauth_request_signer.cc
+++ google_apis/gaia/oauth_request_signer.cc
@@ -412,7 +412,7 @@ bool OAuthRequestSigner::SignURL(
     switch (http_method) {
       case GET_METHOD:
         signed_text = request_base_url.spec() + '?';
-        // Intentionally falling through
+        FALLTHROUGH;
       case POST_METHOD:
         signed_text += BuildBaseStringParameters(parameters);
         break;
