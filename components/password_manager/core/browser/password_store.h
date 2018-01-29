05dfa763dc056bd182951dcac478b0d48c7e0b6d
Jialiu Lin <jialiul@chromium.org>
2018-01-29 20:27:33 +0000

    Only Log SyncPasswordHashChange if there is actual change
    Move LogSyncPasswordHashChange call into password store class,
    such that "PasswordManager.SyncPasswordHashChange" will only
    be logged if there is actual change.
    
    Bug: 657041
    Change-Id: I3afbea76ee9a31376f7a6752b3933cf278328378
    Reviewed-on: https://chromium-review.googlesource.com/885243
    Commit-Queue: Jialiu Lin <jialiul@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532568}

--- components/password_manager/core/browser/password_store.h
+++ components/password_manager/core/browser/password_store.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // TODO(crbug.com/706392): Fix password reuse detection for Android.
 #if defined(SYNC_PASSWORD_REUSE_DETECTION_ENABLED)
 #include "components/password_manager/core/browser/hash_password_manager.h"
+#include "components/password_manager/core/browser/password_manager_metrics_util.h"
 #include "components/password_manager/core/browser/password_reuse_detector.h"
 #include "components/password_manager/core/browser/password_reuse_detector_consumer.h"
 #endif
@@ -260,10 +261,14 @@ class PasswordStore : protected PasswordStoreSync,
                           PasswordReuseDetectorConsumer* consumer);
 
   // Saves a hash of |password| for password reuse checking.
-  virtual void SaveSyncPasswordHash(const base::string16& password);
+  // |event| is used for metric logging.
+  virtual void SaveSyncPasswordHash(const base::string16& password,
+                                    metrics_util::SyncPasswordHashChange event);
 
   // Saves |sync_password_data| for password reuse checking.
-  virtual void SaveSyncPasswordHash(const SyncPasswordData& sync_password_data);
+  // |event| is used for metric logging.
+  virtual void SaveSyncPasswordHash(const SyncPasswordData& sync_password_data,
+                                    metrics_util::SyncPasswordHashChange event);
 
   // Clears the saved sync password hash.
   virtual void ClearSyncPasswordHash();
