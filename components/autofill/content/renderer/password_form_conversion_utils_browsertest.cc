cbe8cf8423762a9a9c70a837e7d530e34196df1c
nikhil <nikhil.sahni@samsung.com>
2018-01-29 07:33:03 +0000

    Fixing names of password_manager kEnableHtmlBasedUsernameDetector feature.
    Fixing  names of password_manager kEnableHtmlBasedUsernameDetector feature
    as per the naming convention.
    
    Bug: 785953
    Change-Id: I8818a2e2335d9a651d02ab9925bd7e4987a661a3
    Reviewed-on: https://chromium-review.googlesource.com/878101
    Commit-Queue: NIKHIL SAHNI <nikhil.sahni@samsung.com>
    Reviewed-by: Vaclav Brozek <vabr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532343}

--- components/autofill/content/renderer/password_form_conversion_utils_browsertest.cc
+++ components/autofill/content/renderer/password_form_conversion_utils_browsertest.cc
@@ -351,7 +351,7 @@ TEST_F(MAYBE_PasswordFormConversionUtilsTest,
        HTMLDetector_DeveloperGroupAttributes) {
   base::test::ScopedFeatureList feature_list;
   feature_list.InitAndEnableFeature(
-      password_manager::features::kEnableHtmlBasedUsernameDetector);
+      password_manager::features::kHtmlBasedUsernameDetector);
 
   // Each test case consists of a set of parameters to be plugged into the
   // PasswordFormBuilder below, plus the corresponding expectations.
@@ -473,7 +473,7 @@ TEST_F(MAYBE_PasswordFormConversionUtilsTest,
 TEST_F(MAYBE_PasswordFormConversionUtilsTest, HTMLDetector_SeveralDetections) {
   base::test::ScopedFeatureList feature_list;
   feature_list.InitAndEnableFeature(
-      password_manager::features::kEnableHtmlBasedUsernameDetector);
+      password_manager::features::kHtmlBasedUsernameDetector);
 
   // If word matches in more than 2 fields, we don't match on it.
   // We search for match with another word.
@@ -507,7 +507,7 @@ TEST_F(MAYBE_PasswordFormConversionUtilsTest,
        HTMLDetector_UserGroupAttributes) {
   base::test::ScopedFeatureList feature_list;
   feature_list.InitAndEnableFeature(
-      password_manager::features::kEnableHtmlBasedUsernameDetector);
+      password_manager::features::kHtmlBasedUsernameDetector);
 
   // Each test case consists of a set of parameters to be plugged into the
   // PasswordFormBuilder below, plus the corresponding expectations.
@@ -627,7 +627,7 @@ TEST_F(MAYBE_PasswordFormConversionUtilsTest,
 TEST_F(MAYBE_PasswordFormConversionUtilsTest, HTMLDetectorCache) {
   base::test::ScopedFeatureList feature_list;
   feature_list.InitAndEnableFeature(
-      password_manager::features::kEnableHtmlBasedUsernameDetector);
+      password_manager::features::kHtmlBasedUsernameDetector);
 
   PasswordFormBuilder builder(kTestFormActionURL);
   builder.AddTextField("unknown", "12345", nullptr);
@@ -2415,7 +2415,7 @@ TEST_F(MAYBE_PasswordFormConversionUtilsTest, ResetPasswordForm) {
   // without any text fields.
   base::test::ScopedFeatureList feature_list;
   feature_list.InitAndEnableFeature(
-      password_manager::features::kEnableHtmlBasedUsernameDetector);
+      password_manager::features::kHtmlBasedUsernameDetector);
   PasswordFormBuilder builder(kTestFormActionURL);
   builder.AddPasswordField("password", "secret", nullptr);
   builder.AddSubmitButton("submit");
