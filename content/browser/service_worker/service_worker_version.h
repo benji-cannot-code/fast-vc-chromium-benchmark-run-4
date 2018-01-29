cd1af379dc6f2afb06b93d2b311d32a6d3d53303
Han Leon <leon.han@intel.com>
2018-01-29 02:56:37 +0000

    [ServiceWorker] Implement ServiceWorkerHost.GetClient()
    This CL mojofies the 2 legacy IPCs:
      ServiceWorkerHostMsg_GetClient
      ServiceWorkerMsg_DidGetClient
    
    BUG=772793
    
    Change-Id: I3a069713c1ed0068883214c6ba583eca758ed323
    Reviewed-on: https://chromium-review.googlesource.com/880486
    Commit-Queue: Han Leon <leon.han@intel.com>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532309}

--- content/browser/service_worker/service_worker_version.h
+++ content/browser/service_worker/service_worker_version.h
@@ -631,6 +631,8 @@ class CONTENT_EXPORT ServiceWorkerVersion
   void ClaimClients(ClaimClientsCallback callback) override;
   void GetClients(blink::mojom::ServiceWorkerClientQueryOptionsPtr options,
                   GetClientsCallback callback) override;
+  void GetClient(const std::string& client_uuid,
+                 GetClientCallback callback) override;
 
   void OnSetCachedMetadataFinished(int64_t callback_id,
                                    size_t size,
@@ -638,9 +640,6 @@ class CONTENT_EXPORT ServiceWorkerVersion
   void OnClearCachedMetadataFinished(int64_t callback_id, int result);
 
   // Message handlers.
-
-  // This corresponds to the spec's get(id) steps.
-  void OnGetClient(int request_id, const std::string& client_uuid);
 
   // Currently used for Clients.openWindow() only.
   void OnOpenNewTab(int request_id, const GURL& url);
@@ -692,10 +691,6 @@ class CONTENT_EXPORT ServiceWorkerVersion
   void OnSimpleEventFinished(int request_id,
                              blink::mojom::ServiceWorkerEventStatus status,
                              base::Time dispatch_event_time);
-
-  void OnGetClientFinished(
-      int request_id,
-      blink::mojom::ServiceWorkerClientInfoPtr client_info);
 
   // The timeout timer periodically calls OnTimeoutTimer, which stops the worker
   // if it is excessively idle or unresponsive to ping.
