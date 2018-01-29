267a463195b55c158d90952b04b769ad44170c1d
Nico Weber <thakis@chromium.org>
2018-01-29 02:24:49 +0000

    Work more on getting things built with -Wimplicit-fallthrough.
    This CL was uploaded by git cl split.
    
    R=yosin@chromium.org
    
    Bug: 177475
    Change-Id: Ia3fdc134c6bf9a85f215156a8da48d2fcfbf4e83
    Reviewed-on: https://chromium-review.googlesource.com/889962
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532303}

--- third_party/WebKit/Source/core/editing/state_machines/BackspaceStateMachine.cpp
+++ third_party/WebKit/Source/core/editing/state_machines/BackspaceStateMachine.cpp
@@ -194,6 +194,7 @@ TextSegmentationMachineState BackspaceStateMachine::FeedPrecedingCodeUnit(
       return MoveToNextState(BackspaceState::kOddNumberedRIS);
     case BackspaceState::kFinished:
       NOTREACHED() << "Do not call feedPrecedingCodeUnit() once it finishes.";
+      break;
     default:
       NOTREACHED() << "Unhandled state: " << state_;
   }
