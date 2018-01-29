b7ac8e9af822bb9ecdc3b02d7d6601bd4403eb67
David 'Digit' Turner <digit@google.com>
2018-01-29 15:11:02 +0000

    android_crazy_linker: Disable r_brk() call for release builds.
    Completely remove the ability to call r_brk() at runtime for
    release builds. Normally, this is controlled at runtime by
    calling crazy_set_debugger_support(false), which is actually
    performed by the //base/android/linker/legacy_linker.cc.
    
    However, due to yet-unsolved reasons, runtime crashes in
    r_brk() still happen on Intel-based devices, even though
    this function is now supposed to never be called.
    
    This patch is an ugly hack that removes the ability to
    call r_brk() entirely from the library's code, to unlock
    the release.
    
    A proper fix would understand what's really going on (e.g.
    passing of dangling pointers in deferred task callbacks?)
    and address that accordingly. Unfortunately, the issue is
    really difficult to reproduce locally.
    
    BUG=796938
    R=agrieve@chromium.org,pasko@chromium.org,rmcilroy@chromium.org
    
    Change-Id: I10a08fe3d551be39b6b9919420224b925b8be9a0
    Reviewed-on: https://chromium-review.googlesource.com/889757
    Reviewed-by: agrieve <agrieve@chromium.org>
    Commit-Queue: David Turner <digit@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532429}

--- third_party/android_crazy_linker/src/src/crazy_linker_rdebug.h
+++ third_party/android_crazy_linker/src/src/crazy_linker_rdebug.h
@@ -123,6 +123,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // linker's actions, so to avoid clashing with it we may need to try and
 // move 'r_map' updates to a different thread, to serialize them with
 // other system linker activity.
+//
+// TECHNICAL NOTE: If CRAZY_DISABLE_R_BRK is defined at compile time,
+// then the crazy linker will never try to call the r_brk() GDB Hook
+// function. This can be useful to avoid runtime crashes on certain
+// Android devices with x86 processors, running ARM binaries with
+// a machine translator like Houdini. See http://crbug.com/796938
+//
 namespace crazy {
 
 struct link_map_t {
@@ -198,17 +205,6 @@ class RDebug {
 
   r_debug* GetAddress() { return r_debug_; }
 
-  // Debugger support, which is the default, implies calling the special
-  // hook function r_brk(). Unfortunately, this sometimes results in rare
-  // runtime crashes (see https://crbug.com/796938). This method allows the
-  // client to disable this operation. If |enabled| is false, shared
-  // libraries loaded through the crazy linker will *not* be visible to GDB
-  // (but will continue to appear in stack traces).
-  void SetDebuggerSupport(bool enabled);
-
-  // Return the state of debugger support.
-  bool GetDebuggerSupport() const;
-
  private:
   // Try to find the address of the global _r_debug variable, even
   // though there is no symbol for it. Returns true on success.
@@ -259,7 +255,6 @@ class RDebug {
   r_debug* r_debug_;
   bool init_;
   bool readonly_entries_;
-  bool call_r_brk_;
   rdebug_callback_poster_t post_for_later_execution_;
   void* post_for_later_execution_context_;
 };
