21b2a29693b2795d082ef6534c00a1530fce1b2b
Qiang Xu <warx@google.com>
2018-01-29 19:36:50 +0000

    cros: add ash-enable-persistent-window-bounds switch
    changes:
    Add a switch to make the feature dev behind the flag.
    
    Bug: 805046
    Test: unit_tests --gtest_filter=AboutFlagsHistogramTest*
    Change-Id: I1b63060cf6b08557d94447b4a87f9ab07b192e1c
    Reviewed-on: https://chromium-review.googlesource.com/890633
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Commit-Queue: Qiang Xu <warx@google.com>
    Cr-Commit-Position: refs/heads/master@{#532546}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1806,6 +1806,9 @@ extern const char kAshEnableKeyboardShortcutViewerDescription[];
 extern const char kAshEnableMirroredScreenName[];
 extern const char kAshEnableMirroredScreenDescription[];
 
+extern const char kAshEnablePersistentWindowBoundsName[];
+extern const char kAshEnablePersistentWindowBoundsDescription[];
+
 extern const char kAshShelfColorName[];
 extern const char kAshShelfColorDescription[];
 
