4405b079eb5aee8080ba3f6875d05bd4cddd56b0
wutao <wutao@chromium.org>
2018-01-30 18:04:47 +0000

    cros: Fix UIChromeOSTestSuite.
    The UIChromeOSTestSuite will fail some of the unit tests based on
    views::ViewsTestBase. In the retries, the tests will pass.
    
    By comparing UIChromeOSTestSuite and ViewsTestSuite, the only difference
    is that UIChromeOSTestSuite does not init/clear up aura env.
    
    This cl properly sets up and cleans up the aura env for UIChromeOSTestSuite.
    
    Bug: 807148
    Test: ui_chromeos_unittests
    Change-Id: Ife3bae23c2003a6e219764f7483cdff7eb785c72
    Reviewed-on: https://chromium-review.googlesource.com/892495
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532944}

--- ui/chromeos/run_all_unittests.cc
+++ ui/chromeos/run_all_unittests.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <memory>
+
 #include "base/bind.h"
 #include "base/compiler_specific.h"
 #include "base/macros.h"
@@ -11,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/test_suite.h"
 #include "mojo/edk/embedder/embedder.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "ui/aura/env.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/ui_base_paths.h"
 #include "ui/gl/test/gl_surface_test_support.h"
@@ -31,14 +34,18 @@ class UIChromeOSTestSuite : public base::TestSuite {
     base::FilePath ui_test_pak_path;
     ASSERT_TRUE(PathService::Get(ui::UI_TEST_PAK, &ui_test_pak_path));
     ui::ResourceBundle::InitSharedInstanceWithPakPath(ui_test_pak_path);
+    env_ = aura::Env::CreateInstance();
   }
 
   void Shutdown() override {
+    env_.reset();
     ui::ResourceBundle::CleanupSharedInstance();
     base::TestSuite::Shutdown();
   }
 
  private:
+  std::unique_ptr<aura::Env> env_;
+
   DISALLOW_COPY_AND_ASSIGN(UIChromeOSTestSuite);
 };
 
