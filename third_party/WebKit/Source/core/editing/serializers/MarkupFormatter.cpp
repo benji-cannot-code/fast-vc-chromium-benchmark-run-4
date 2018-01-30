8ee977a8a5c99b1174f1e636abf99a1f57dfa2fd
Rob Buis <rob.buis@samsung.com>
2018-01-30 13:09:43 +0000

    Fix null-dereference caused by r532302
    We need to check if there is a frame before calling CanExecuteScripts.
    
    Bug: 806681, 806705
    Change-Id: I717cbb92478806b8f2687b099c62558f5abc7f03
    Reviewed-on: https://chromium-review.googlesource.com/891304
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Rob Buis <rob.buis@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#532850}

--- third_party/WebKit/Source/core/editing/serializers/MarkupFormatter.cpp
+++ third_party/WebKit/Source/core/editing/serializers/MarkupFormatter.cpp
@@ -480,7 +480,7 @@ EntityMask MarkupFormatter::EntityMaskForText(const Text& text) const {
        *parent_name == xmpTag || *parent_name == iframeTag ||
        *parent_name == plaintextTag || *parent_name == noembedTag ||
        *parent_name == noframesTag ||
-       (*parent_name == noscriptTag &&
+       (*parent_name == noscriptTag && text.GetDocument().GetFrame() &&
         text.GetDocument().CanExecuteScripts(kNotAboutToExecuteScript))))
     return kEntityMaskInCDATA;
   return kEntityMaskInHTMLPCDATA;
