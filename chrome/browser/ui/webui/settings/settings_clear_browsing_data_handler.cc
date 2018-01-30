0b036568d66618a67035d47769dded887aa2e181
Nico Weber <thakis@chromium.org>
2018-01-30 00:31:58 +0000

    Remaining changes to build all code in src.git with -Wimplicit-fallthrough.
    This CL was uploaded by git cl split.
    
    R=michaelpg@chromium.org
    
    Bug: 177475
    Change-Id: Ia5cea2af0f72d24b611ae85a09051e5a7e41cb70
    Reviewed-on: https://chromium-review.googlesource.com/891500
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532694}

--- chrome/browser/ui/webui/settings/settings_clear_browsing_data_handler.cc
+++ chrome/browser/ui/webui/settings/settings_clear_browsing_data_handler.cc
@@ -225,8 +225,10 @@ void ClearBrowsingDataHandler::HandleClearBrowsingData(
       case BrowsingDataType::BOOKMARKS:
         // Only implemented on Android.
         NOTREACHED();
+        break;
       case BrowsingDataType::NUM_TYPES:
         NOTREACHED();
+        break;
     }
   }
 
