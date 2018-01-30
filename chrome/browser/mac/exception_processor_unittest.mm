bdf0b4e11c7155d3d565bee5dc8decb20e25cada
François Degros <fdegros@chromium.org>
2018-01-30 02:57:54 +0000

    Simplified StatisticsRecorder.
    Removed methods GetSnapshot, GetKnownHistograms and GetHistogramsWithPredicate.
    They were replaced by GetHistogram + filter functions WithName and
    NonPersistent.
    
    Change-Id: Ia974675e38f29c8ad19a13e1d1fa0c3716503b40
    TBR: avi@chromium.org, rsesek@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/858244
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532740}

--- chrome/browser/mac/exception_processor_unittest.mm
+++ chrome/browser/mac/exception_processor_unittest.mm
@@ -49,7 +49,8 @@ TEST(ExceptionProcessorTest, ExceptionBinning) {
 }
 
 TEST(ExceptionProcessorTest, RecordException) {
-  EXPECT_THAT(StatisticsRecorder::GetSnapshot("OSX.NSException"),
+  EXPECT_THAT(StatisticsRecorder::WithName(StatisticsRecorder::GetHistograms(),
+                                           "OSX.NSException"),
               testing::IsEmpty());
 
   // Record some known exceptions.
@@ -72,7 +73,8 @@ TEST(ExceptionProcessorTest, RecordException) {
 
   // We should have exactly the right number of exceptions.
   const StatisticsRecorder::Histograms histograms =
-      StatisticsRecorder::GetSnapshot("OSX.NSException");
+      StatisticsRecorder::WithName(StatisticsRecorder::GetHistograms(),
+                                   "OSX.NSException");
   ASSERT_THAT(histograms, testing::SizeIs(1));
   EXPECT_EQ(HistogramBase::kUmaTargetedHistogramFlag, histograms[0]->flags());
 
