bf46e94664f3feceac98e8af35548515bfbb109a
Chris Harrelson <chrishtr@chromium.org>
2018-01-30 20:43:55 +0000

    [PE] Stop ref-counting PropertyTreeState in most cases.
    Only PaintChunks need ref-counted PropertyTreeState objects, because they
    are the only ones that store the objects in a persistent manner. Further
    the performance overhead of RefPtr for other use-cases is too big to
    justify.
    
    Bug:798378
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Iac4d4e1bcf0e377033ab9c129f290e0745830343
    Reviewed-on: https://chromium-review.googlesource.com/892561
    Reviewed-by: Tien-Ren Chen <trchen@chromium.org>
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533012}

--- third_party/WebKit/Source/platform/graphics/paint/PaintChunkProperties.h
+++ third_party/WebKit/Source/platform/graphics/paint/PaintChunkProperties.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <iosfwd>
 #include "platform/PlatformExport.h"
-#include "platform/graphics/paint/PropertyTreeState.h"
+#include "platform/graphics/paint/RefCountedPropertyTreeState.h"
 #include "platform/wtf/Allocator.h"
 #include "platform/wtf/Forward.h"
 #include "platform/wtf/Noncopyable.h"
@@ -34,7 +34,7 @@ struct PLATFORM_EXPORT PaintChunkProperties {
       : property_tree_state(nullptr, nullptr, nullptr),
         backface_hidden(false) {}
 
-  PropertyTreeState property_tree_state;
+  RefCountedPropertyTreeState property_tree_state;
   bool backface_hidden;
 
   String ToString() const;
