b08e8425748722e6f6d33a80612da4afaa7ab094
Troy Hildebrandt <thildebr@chromium.org>
2018-01-30 21:51:27 +0000

    Add metric for deleting omnibox text on Android.
    Adds the MobileOmniboxDeleteUrl metric and records it whenever the user
    clears the contents of the omnibox with the delete button.
    
    Change-Id: Idddeccb89999ceeea45873a05ba2b052f0e7d2d2
    Reviewed-on: https://chromium-review.googlesource.com/892140
    Reviewed-by: Maria Khomenko <mariakhomenko@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Commit-Queue: Troy Hildebrandt <thildebr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533033}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarLayout.java
@@ -1943,6 +1943,7 @@ public class LocationBarLayout extends FrameLayout
             }
 
             startZeroSuggest();
+            RecordUserAction.record("MobileOmniboxDeleteUrl");
             return;
         } else if (!mUrlHasFocus && shouldShowPageInfoForView(v)) {
             if (mToolbarDataProvider.hasTab() && getCurrentTab().getWebContents() != null
