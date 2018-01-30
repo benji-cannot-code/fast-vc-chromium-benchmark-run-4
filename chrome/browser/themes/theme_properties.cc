d4717c1e4ad4a2a40d80265b289fa34efa97150c
Nico Weber <thakis@chromium.org>
2018-01-30 00:08:15 +0000

    Remaining changes to build all code in src.git with -Wimplicit-fallthrough.
    This CL was uploaded by git cl split.
    
    R=pkotwicz@chromium.org
    
    Bug: 177475
    Change-Id: I0b25a266bda563f3ad25524cb337f8b6b6a30564
    Reviewed-on: https://chromium-review.googlesource.com/891501
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532679}

--- chrome/browser/themes/theme_properties.cc
+++ chrome/browser/themes/theme_properties.cc
@@ -219,6 +219,7 @@ color_utils::HSL ThemeProperties::GetDefaultTint(int id, bool incognito) {
       NOTREACHED() << "These values should be queried via their respective "
                       "non-incognito equivalents and an appropriate "
                       "|incognito| value.";
+      FALLTHROUGH;
     default:
       return {-1, -1, -1};
   }
