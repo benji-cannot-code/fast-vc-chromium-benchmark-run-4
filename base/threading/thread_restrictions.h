3b223074663fb0add8ccef7d2c88d304cae6f379
Tommi <tommi@chromium.org>
2018-01-30 16:55:26 +0000

    Add rtc::ScopedAllowBaseSyncPrimitives for WebRTC.
    This class links together Chromium's ScopedAllowBaseSyncPrimitives and WebRTCs.
    Now that WebRTC is starting to use Chromium's sync primitives, this allows
    us to continue reduce locking inside of WebRTC that can cause slowdown
    on Chromium threads, while guarding against regressions.
    
    Bug: 796889, 795340
    Change-Id: Icd71c13e04433aa67e3f22f5d510aa724b413fa9
    Reviewed-on: https://chromium-review.googlesource.com/871631
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Tommi <tommi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532920}

--- base/threading/thread_restrictions.h
+++ base/threading/thread_restrictions.h
@@ -124,6 +124,10 @@ namespace viz {
 class ServerGpuMemoryBufferManager;
 }
 
+namespace webrtc {
+class DesktopConfigurationMonitor;
+}
+
 namespace base {
 
 namespace android {
@@ -277,6 +281,7 @@ class BASE_EXPORT ScopedAllowBaseSyncPrimitives {
   friend class net::MultiThreadedCertVerifierScopedAllowBaseSyncPrimitives;
   friend class rlz_lib::FinancialPing;
   friend class shell_integration::LaunchXdgUtilityScopedAllowBaseSyncPrimitives;
+  friend class webrtc::DesktopConfigurationMonitor;
 
   ScopedAllowBaseSyncPrimitives() EMPTY_BODY_IF_DCHECK_IS_OFF;
   ~ScopedAllowBaseSyncPrimitives() EMPTY_BODY_IF_DCHECK_IS_OFF;
--- third_party/webrtc_overrides/rtc_base/event.h
+++ third_party/webrtc_overrides/rtc_base/event.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/synchronization/waitable_event.h"
+#include "base/threading/thread_restrictions.h"
 
 namespace rtc {
 
@@ -30,6 +31,11 @@ class Event {
   base::WaitableEvent event_;
   DISALLOW_IMPLICIT_CONSTRUCTORS(Event);
 };
+
+// Pull ScopedAllowBaseSyncPrimitives into the rtc namespace.
+// Managing what types in WebRTC are allowed to use
+// ScopedAllowBaseSyncPrimitives, is done via thread_restrictions.h.
+using base::ScopedAllowBaseSyncPrimitives;
 
 }  // namespace rtc
 
