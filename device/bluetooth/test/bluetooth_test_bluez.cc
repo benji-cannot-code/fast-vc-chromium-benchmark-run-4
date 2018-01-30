7f99a2d4b8d11ffd7af22a2b60a49f072b7ba156
Nico Weber <thakis@chromium.org>
2018-01-30 15:02:00 +0000

    Remaining changes to build all code in src.git with -Wimplicit-fallthrough.
    Fixes a bug where device ordinal 5 would fall through to device ordinal 6
    after resetting the name, cf
    https://codereview.chromium.org/2440043002/diff/120001/device/bluetooth/test/bluetooth_test_bluez.cc
    
    This CL was uploaded by git cl split.
    
    R=ortuno@chromium.org
    
    Bug: 177475
    Change-Id: I93fd60b42f634c4b84c464452861f2cf3aa32f37
    Reviewed-on: https://chromium-review.googlesource.com/891502
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532871}

--- device/bluetooth/test/bluetooth_test_bluez.cc
+++ device/bluetooth/test/bluetooth_test_bluez.cc
@@ -149,6 +149,7 @@ BluetoothDevice* BluetoothTestBlueZ::SimulateLowEnergyDevice(
       break;
     case 5:
       device_name = base::nullopt;
+      break;
     case 6:
       device_address = kTestDeviceAddress2;
       device_type = BLUETOOTH_TRANSPORT_DUAL;
