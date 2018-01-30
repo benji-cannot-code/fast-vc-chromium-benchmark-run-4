f4ca7724c009591470761809eec82f68dc0cc5b1
Eugene But <eugenebut@google.com>
2018-01-30 02:08:28 +0000

    Extend downloadManagerTabHelper:didCreateDownload: callback with "webStateIsVisible" flag.
    This API will be used by DownloadManagerCoordinator to ignore callbacks
    from hidden tabs.
    
    Bug: 791806
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I453db842ba93c50bcbb9a9987cf614a29e963d21
    Reviewed-on: https://chromium-review.googlesource.com/884363
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532732}

--- ios/chrome/test/fakes/fake_download_manager_tab_helper_delegate.mm
+++ ios/chrome/test/fakes/fake_download_manager_tab_helper_delegate.mm
@@ -18,8 +18,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)downloadManagerTabHelper:(nonnull DownloadManagerTabHelper*)tabHelper
-               didCreateDownload:(nonnull web::DownloadTask*)download {
-  _state = std::make_unique<web::DownloadTask::State>(download->GetState());
+               didCreateDownload:(nonnull web::DownloadTask*)download
+               webStateIsVisible:(BOOL)vebStateIsVisible {
+  if (vebStateIsVisible) {
+    _state = std::make_unique<web::DownloadTask::State>(download->GetState());
+  }
 }
 
 - (void)downloadManagerTabHelper:(nonnull DownloadManagerTabHelper*)tabHelper
