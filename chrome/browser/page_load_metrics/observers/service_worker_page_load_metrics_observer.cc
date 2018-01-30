96462df9b1c61e0ed1f608f9f3aaf07887e9ea09
Matt Falkenhagen <falken@chromium.org>
2018-01-30 22:19:06 +0000

    service worker: Move PageTransition/RedirectChain metrics out of SWControlleeRequestHandler.
    These metrics don't work well at the ServiceWorkerControlleeRequestHandler
    layer for S13nServiceWorker, for the reasons in the bug. Actually they are
    pretty suspect for non-S13nServiceWorker too because we are recording them on
    every URL request, not just committed page load, so we are double-counting on
    redirects.
    
    This patch:
    1) Moves the PageTransition metric to ServiceWorkerPageLoadMetricsObserver.
    This is a better fit also because page transition info is expected to be
    removed from ResourceRequest as per the TODO there.
    2) Removes the RedirectChain metric. We haven't been using it. If needed
    we can always add one to ServiceWorkerPageLoadMetricsObserver alongside
    PageTransition.
    
    R=bmcquade, kinuko
    
    Bug: 805805
    Change-Id: I58029612f8c7697ec280bcc80bec97f6f64d9dd9
    Reviewed-on: https://chromium-review.googlesource.com/886102
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Bryan McQuade <bmcquade@chromium.org>
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533042}

--- chrome/browser/page_load_metrics/observers/service_worker_page_load_metrics_observer.cc
+++ chrome/browser/page_load_metrics/observers/service_worker_page_load_metrics_observer.cc
@@ -14,6 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace internal {
 
+const char kHistogramServiceWorkerPageTransition[] =
+    "PageLoad.Clients.ServiceWorker.PageTransition";
+
 const char kHistogramServiceWorkerParseStart[] =
     "PageLoad.Clients.ServiceWorker.ParseTiming.NavigationToParseStart";
 const char kHistogramServiceWorkerParseStartForwardBack[] =
@@ -323,6 +326,17 @@ void ServiceWorkerPageLoadMetricsObserver::OnParseStart(
     const page_load_metrics::PageLoadExtraInfo& info) {
   if (!IsServiceWorkerControlled(info))
     return;
+
+  // TODO(falken): It may be cleaner to record page transition in OnCommit() but
+  // at that point we don't yet know if the page is controlled by a service
+  // worker. It should be possible to plumb the information there since the
+  // browser process already sends the controller service worker in the
+  // navigation commit IPC.
+  UMA_HISTOGRAM_ENUMERATION(
+      internal::kHistogramServiceWorkerPageTransition,
+      static_cast<int>(ui::PageTransitionStripQualifier(transition_)),
+      static_cast<int>(ui::PAGE_TRANSITION_LAST_CORE) + 1);
+
   if (WasStartedInForegroundOptionalEventInForeground(
           timing.parse_timing->parse_start, info)) {
     PAGE_LOAD_HISTOGRAM(internal::kHistogramServiceWorkerParseStart,
