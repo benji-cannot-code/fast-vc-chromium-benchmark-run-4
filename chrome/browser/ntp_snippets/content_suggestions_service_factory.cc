61c8e62f587d30297eaa49eb68a618f2b9824898
Colin Blundell <blundell@chromium.org>
2018-01-30 15:45:53 +0000

    [NTP Snippets] Reland RemoteSuggestionsFetcher usage of IdentityManager
    This CL reverts d8bd981fe9, in effect relanding the changes to have
    RemoteSuggestionsFetcherImpl and its unittest use IdentityManager. In
    the interim I have landed a fix to the bug that necessitated
    reversion of those CLs, i.e. the raciness between IdentityManager
    getting updated on signin/signout and other SigninManager::Observers
    getting updated on those same events
    (fix is here:
     https://chromium-review.googlesource.com/c/chromium/src/+/883347).
    
    To test this change, do the following:
    - Sign in to Chrome on Android (in a chrome_apk build if building locally)
    - Observe that there are personalized suggestions on the NTP
    - Sign out
    - Observe that there are different, generic suggestions on the NTP
    
    You can also more generally that the suggestions get updated on various
    transitions between a signed-out state and a signed-in state.
    
    TBR=gambard@chromium.org
    
    Bug: 798413
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I37d182718ef0b53a85be1a083f203aa0acdb3d8d
    Reviewed-on: https://chromium-review.googlesource.com/888617
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532888}

--- chrome/browser/ntp_snippets/content_suggestions_service_factory.cc
+++ chrome/browser/ntp_snippets/content_suggestions_service_factory.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ntp_snippets/dependent_features.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/search/suggestions/image_decoder_impl.h"
+#include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/signin/profile_oauth2_token_service_factory.h"
 #include "chrome/browser/signin/signin_manager_factory.h"
 #include "chrome/browser/sync/profile_sync_service_factory.h"
@@ -339,8 +340,8 @@ void RegisterArticleProviderIfEnabled(ContentSuggestionsService* service,
   }
 
   PrefService* pref_service = profile->GetPrefs();
-  OAuth2TokenService* token_service =
-      ProfileOAuth2TokenServiceFactory::GetForProfile(profile);
+  identity::IdentityManager* identity_manager =
+      IdentityManagerFactory::GetForProfile(profile);
   UrlLanguageHistogram* language_histogram =
       UrlLanguageHistogramFactory::GetForBrowserContext(profile);
 
@@ -376,8 +377,7 @@ void RegisterArticleProviderIfEnabled(ContentSuggestionsService* service,
   }
 #endif  // BUILDFLAG(ENABLE_OFFLINE_PAGES)
   auto suggestions_fetcher = base::MakeUnique<RemoteSuggestionsFetcherImpl>(
-      signin_manager, token_service, request_context, pref_service,
-      language_histogram,
+      identity_manager, request_context, pref_service, language_histogram,
       base::Bind(
           &data_decoder::SafeJsonParser::Parse,
           content::ServiceManagerConnection::GetForProcess()->GetConnector()),
@@ -455,6 +455,7 @@ ContentSuggestionsServiceFactory::ContentSuggestionsServiceFactory()
           BrowserContextDependencyManager::GetInstance()) {
   DependsOn(BookmarkModelFactory::GetInstance());
   DependsOn(HistoryServiceFactory::GetInstance());
+  DependsOn(IdentityManagerFactory::GetInstance());
   DependsOn(LargeIconServiceFactory::GetInstance());
 #if BUILDFLAG(ENABLE_OFFLINE_PAGES)
   DependsOn(OfflinePageModelFactory::GetInstance());
