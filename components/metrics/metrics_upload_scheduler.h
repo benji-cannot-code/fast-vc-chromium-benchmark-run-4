2e8cf4cb1e8719f3e901dd279cd9b316b5cf02d8
Steven Holte <holte@google.com>
2018-01-30 21:24:04 +0000

    Remove unused UMAUploadScheduler params.
    Change-Id: I6de19017f4f49a062f41121a90df990c400785bb
    Reviewed-on: https://chromium-review.googlesource.com/889651
    Reviewed-by: Gayane Petrosyan <gayane@chromium.org>
    Commit-Queue: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533025}

--- components/metrics/metrics_upload_scheduler.h
+++ components/metrics/metrics_upload_scheduler.h
@@ -14,8 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace metrics {
 
-extern const base::Feature kUploadSchedulerFeature;
-
 // Scheduler task to drive a ReportingService object's uploading.
 class MetricsUploadScheduler : public MetricsScheduler {
  public:
