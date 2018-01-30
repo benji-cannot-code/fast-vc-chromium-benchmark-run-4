2392503a33242b5e0b609171b14437b98d5b1769
Peter Kotwicz <pkotwicz@chromium.org>
2018-01-30 05:00:12 +0000

    Remove ability to register multiple ChildProcessCreationParams objects part 3
    This CL changes the custom-package-name specified in
    ChildProcessCreationParams to apply to both sandboxed and unsandboxed
    services.
    
    Perviously when a user launched a WebAPK:
    - Renderers would use a non-Chrome sandboxed process defined in the WebAPK APK
    - The GPU process would run in a Chrome process
    
    WebAPKs no longer use a non-Chrome process for renderers. This leaves us with
    two cases:
    Case #1 - Chrome
    - Renderer runs in Chrome-process (Package name provided by application
      context)
    - GPU process runs in Chrome-process (Package name provided by application
      context)
    Case #2 - Android Webview
    - Renderer runs in Chrome-process (Package name different than application
      context)
    - Does not use GPU process or other non-sandboxed processes
    
    Bug=797999
    
    Change-Id: I050e9e16a6a9a17c99d46c33e0ce190d17e66d98
    Reviewed-on: https://chromium-review.googlesource.com/885781
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532762}

--- content/public/android/java/src/org/chromium/content/browser/ChildProcessLauncherHelper.java
+++ content/public/android/java/src/org/chromium/content/browser/ChildProcessLauncherHelper.java
@@ -234,11 +234,6 @@ public class ChildProcessLauncherHelper {
         sSpareSandboxedConnection = new SpareChildConnection(context, allocator, serviceBundle);
     }
 
-    public static String getPackageNameForService(boolean sandboxed) {
-        return sandboxed ? ChildProcessCreationParams.getPackageNameForSandboxedService()
-                         : ContextUtils.getApplicationContext().getPackageName();
-    }
-
     /**
      * Starts the moderate binding management that adjust a process priority in response to various
      * signals (app sent to background/foreground for example).
@@ -315,7 +310,7 @@ public class ChildProcessLauncherHelper {
     @VisibleForTesting
     static ChildConnectionAllocator getConnectionAllocator(Context context, boolean sandboxed) {
         assert LauncherThread.runningOnLauncherThread();
-        final String packageName = getPackageNameForService(sandboxed);
+        final String packageName = ChildProcessCreationParams.getPackageNameForService();
         boolean bindToCaller = ChildProcessCreationParams.getBindToCallerCheck();
         boolean bindAsExternalService =
                 sandboxed && ChildProcessCreationParams.getIsSandboxedServiceExternal();
@@ -485,7 +480,7 @@ public class ChildProcessLauncherHelper {
         }
 
         final Context context = ContextUtils.getApplicationContext();
-        final String packageName = ChildProcessCreationParams.getPackageNameForSandboxedService();
+        final String packageName = ChildProcessCreationParams.getPackageNameForService();
         try {
             return ChildConnectionAllocator.getNumberOfServices(
                     context, packageName, NUM_SANDBOXED_SERVICES_KEY);
