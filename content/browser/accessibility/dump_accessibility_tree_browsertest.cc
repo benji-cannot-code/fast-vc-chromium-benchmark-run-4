9c066b95a90d30309fc564118db033d2d8207b3f
Aaron Leventhal <aleventhal@chromium.org>
2018-01-31 04:40:23 +0000

    Table with role presentation should expose cells as generic containers
    When screen readers traverse a <table role="presentation">, they need
    generic container objects in place of the cells, to help them understand
    and present formatted content. Specifically, this helps present the text
    without jamming it together, as well as providing line breaks.
    
    Bug: 787664
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I53b691f0ccae3f4090ca0f8ecfcbeb622bd3dfeb
    Reviewed-on: https://chromium-review.googlesource.com/887599
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533202}

--- content/browser/accessibility/dump_accessibility_tree_browsertest.cc
+++ content/browser/accessibility/dump_accessibility_tree_browsertest.cc
@@ -1675,6 +1675,11 @@ IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityTableLayout) {
 }
 
 IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest,
+                       AccessibilityTablePresentation) {
+  RunHtmlTest(FILE_PATH_LITERAL("table-presentation.html"));
+}
+
+IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest,
                        AccessibilityTableThColHeader) {
   RunHtmlTest(FILE_PATH_LITERAL("table-th-colheader.html"));
 }
