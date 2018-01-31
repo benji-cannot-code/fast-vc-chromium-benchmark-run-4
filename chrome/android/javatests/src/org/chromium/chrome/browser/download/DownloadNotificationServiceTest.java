dde79ade11eb3112d094a30660545113a99ce505
Yoland Yan <yolandyan@chromium.org>
2018-01-31 02:17:48 +0000

    Remove multiple ITR from manifest and fix service test run on J4 issue
    Use InstrumentationRegistry to get actual target context in the two
    service test cases. Andr remove BaseITR, BaseChromiumITR, and ChromeITR
    
    Bug: 807251
    Change-Id: I9d945ce7c2ce264f84f10124af6b399cba690eef
    Reviewed-on: https://chromium-review.googlesource.com/894125
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Yoland Yan <yolandyan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533165}

--- chrome/android/javatests/src/org/chromium/chrome/browser/download/DownloadNotificationServiceTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/download/DownloadNotificationServiceTest.java
@@ -11,11 +11,14 @@ import android.content.SharedPreferences;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.IBinder;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.SmallTest;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.ServiceTestCase;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -113,17 +116,20 @@ public class DownloadNotificationServiceTest
         });
     }
 
+    @Before
     @Override
-    protected void setUp() throws Exception {
+    public void setUp() throws Exception {
         super.setUp();
+        setContext(InstrumentationRegistry.getTargetContext());
         SharedPreferences sharedPrefs = ContextUtils.getAppSharedPreferences();
         // TODO(yolandyan): added for debugging reasons, remove if tests no longer flakes
         Assert.assertNull(sharedPrefs.getStringSet(
             DownloadSharedPreferenceHelper.KEY_PENDING_DOWNLOAD_NOTIFICATIONS, null));
     }
 
+    @After
     @Override
-    protected void tearDown() throws Exception {
+    public void tearDown() throws Exception {
         SharedPreferences sharedPrefs = ContextUtils.getAppSharedPreferences();
         SharedPreferences.Editor editor = sharedPrefs.edit();
         editor.remove(DownloadSharedPreferenceHelper.KEY_PENDING_DOWNLOAD_NOTIFICATIONS);
@@ -262,7 +268,7 @@ public class DownloadNotificationServiceTest
     @DisabledTest(message = "crbug.com/773346")
     public void testPausingWithOngoingDownloads() {
         setupService();
-        Context mockContext = new AdvancedMockContext(getSystemContext());
+        Context mockContext = new AdvancedMockContext(InstrumentationRegistry.getTargetContext());
         getService().setContext(mockContext);
         Set<String> notifications = new HashSet<>();
         notifications.add(buildEntryString(1, "test1", true, true));
@@ -296,7 +302,7 @@ public class DownloadNotificationServiceTest
     @DisabledTest(message = "crbug.com/773346")
     public void testAddingAndCancelingNotifications() {
         setupService();
-        Context mockContext = new AdvancedMockContext(getSystemContext());
+        Context mockContext = new AdvancedMockContext(InstrumentationRegistry.getTargetContext());
         getService().setContext(mockContext);
         Set<String> notifications = new HashSet<>();
         String guid1 = UUID.randomUUID().toString();
@@ -383,7 +389,7 @@ public class DownloadNotificationServiceTest
     @DisabledTest(message = "crbug.com/773346")
     public void testResumeAllPendingDownloads() throws Exception {
         setupService();
-        Context mockContext = new AdvancedMockContext(getSystemContext());
+        Context mockContext = new AdvancedMockContext(InstrumentationRegistry.getTargetContext());
         getService().setContext(mockContext);
         Set<String> notifications = new HashSet<>();
         String guid1 = UUID.randomUUID().toString();
@@ -403,8 +409,8 @@ public class DownloadNotificationServiceTest
         DownloadNotificationService service = bindNotificationService();
         DownloadManagerService.disableNetworkListenerForTest();
 
-        final MockDownloadManagerService manager =
-                new MockDownloadManagerService(getSystemContext().getApplicationContext());
+        final MockDownloadManagerService manager = new MockDownloadManagerService(
+                InstrumentationRegistry.getTargetContext().getApplicationContext());
         ThreadUtils.runOnUiThreadBlocking(new Runnable() {
             @Override
             public void run() {
@@ -432,7 +438,7 @@ public class DownloadNotificationServiceTest
     @DisabledTest(message = "crbug.com/773346")
     public void testIncognitoDownloadCanceledOnServiceShutdown() throws Exception {
         setupService();
-        Context mockContext = new AdvancedMockContext(getSystemContext());
+        Context mockContext = new AdvancedMockContext(InstrumentationRegistry.getTargetContext());
         getService().setContext(mockContext);
         Set<String> notifications = new HashSet<>();
         String uuid = UUID.randomUUID().toString();
