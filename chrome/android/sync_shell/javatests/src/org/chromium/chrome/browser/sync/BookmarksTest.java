0a3cdcfc9170e26d88d880cdd7da7410977ba946
Steven Holte <holte@google.com>
2018-01-31 20:21:00 +0000

    Remove stale workaround in sync_shell tests.
    The mentioned bug (crbug.com/637448) is already fixed.
    
    Change-Id: I7e788330624f3eee7caecc76df3e458cf7f1aaeb
    Reviewed-on: https://chromium-review.googlesource.com/894471
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533379}

--- chrome/android/sync_shell/javatests/src/org/chromium/chrome/browser/sync/BookmarksTest.java
+++ chrome/android/sync_shell/javatests/src/org/chromium/chrome/browser/sync/BookmarksTest.java
@@ -19,7 +19,6 @@ import org.junit.runner.RunWith;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Feature;
-import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.chrome.browser.ChromeSwitches;
 import org.chromium.chrome.browser.bookmarks.BookmarkBridge;
 import org.chromium.chrome.browser.profiles.Profile;
@@ -41,7 +40,6 @@ import java.util.concurrent.Callable;
  */
 @RunWith(ChromeJUnit4ClassRunner.class)
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
-@RetryOnFailure // crbug.com/637448
 public class BookmarksTest {
     @Rule
     public SyncTestRule mSyncTestRule = new SyncTestRule();
