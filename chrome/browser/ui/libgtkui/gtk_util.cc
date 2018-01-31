ba716ed193e20a3b50b4acd6be0b3ad24753f0a1
Tom Anderson <thomasanderson@chromium.org>
2018-01-31 18:15:06 +0000

    Fix Gtk2 build
    R=erg
    
    Change-Id: I7707cf02b88a08e416d2fc0a17bfd75b918fe42c
    Reviewed-on: https://chromium-review.googlesource.com/894993
    Reviewed-by: Elliot Glaysher <erg@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533330}

--- chrome/browser/ui/libgtkui/gtk_util.cc
+++ chrome/browser/ui/libgtkui/gtk_util.cc
@@ -57,14 +57,6 @@ void CommonInitFromCommandLine(const base::CommandLine& command_line,
   }
 }
 
-using GtkSetState = void (*)(GtkWidgetPath*, gint, GtkStateFlags);
-PROTECTED_MEMORY_SECTION base::ProtectedMemory<GtkSetState>
-    _gtk_widget_path_iter_set_state;
-
-using GtkSetObjectName = void (*)(GtkWidgetPath*, gint, const char*);
-PROTECTED_MEMORY_SECTION base::ProtectedMemory<GtkSetObjectName>
-    _gtk_widget_path_iter_set_object_name;
-
 }  // namespace
 
 namespace libgtkui {
@@ -245,6 +237,14 @@ float GetDeviceScaleFactor() {
   views::LinuxUI* linux_ui = views::LinuxUI::instance();
   return linux_ui ? linux_ui->GetDeviceScaleFactor() : 1;
 }
+
+using GtkSetState = void (*)(GtkWidgetPath*, gint, GtkStateFlags);
+PROTECTED_MEMORY_SECTION base::ProtectedMemory<GtkSetState>
+    _gtk_widget_path_iter_set_state;
+
+using GtkSetObjectName = void (*)(GtkWidgetPath*, gint, const char*);
+PROTECTED_MEMORY_SECTION base::ProtectedMemory<GtkSetObjectName>
+    _gtk_widget_path_iter_set_object_name;
 
 }  // namespace
 
