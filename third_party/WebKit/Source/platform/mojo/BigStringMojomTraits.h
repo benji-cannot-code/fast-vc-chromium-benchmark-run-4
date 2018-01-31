34dc40d461d1abbf565ea8dbd4564ad9c9d8bbc8
Dmitry Gozman <dgozman@chromium.org>
2018-01-31 03:15:38 +0000

    Introduce mojo_base.mojom.BigString
    BigString uses BigBuffer internally to accomodate
    large strings.
    
    It maps to std::string and WTF::String and internally
    converts data to UTF8 similary to regular string.
    
    Bug: 776009
    Change-Id: I05547f6f55f0a20dafd6a3d99e26b5cc2cbe2eed
    Reviewed-on: https://chromium-review.googlesource.com/891980
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533182}

---
+++ third_party/WebKit/Source/platform/mojo/BigStringMojomTraits.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BigStringMojomTraits_h
+#define BigStringMojomTraits_h
+
+#include "mojo/public/cpp/bindings/struct_traits.h"
+#include "mojo/public/mojom/base/big_string.mojom-blink.h"
+#include "platform/PlatformExport.h"
+
+namespace mojo_base {
+class BigBuffer;
+}
+
+namespace mojo {
+
+template <>
+struct PLATFORM_EXPORT
+    StructTraits<mojo_base::mojom::BigStringDataView, WTF::String> {
+  static bool IsNull(const WTF::String& input) { return input.IsNull(); }
+  static void SetToNull(WTF::String* output) { *output = WTF::String(); }
+
+  static mojo_base::BigBuffer data(const WTF::String& input);
+  static bool Read(mojo_base::mojom::BigStringDataView, WTF::String* out);
+};
+
+}  // namespace mojo
+
+#endif  // BigStringMojomTraits_h
