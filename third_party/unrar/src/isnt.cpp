382cfd91d0429468bbd0afba06d4e52e3ed6750e
Varun Khaneja <vakh@chromium.org>
2018-01-31 18:29:17 +0000

    Revert "[2] Build unrar only for full safe browsing mode."
    This reverts commit 29a153c379373ce057506a971542c61003faca59.
    
    Reason for revert: Causing build failures https://bugs.chromium.org/p/chromium/issues/detail?id=807391
    
    Original change's description:
    > [2] Build unrar only for full safe browsing mode.
    > 
    > - Enables exception handling: throw/try/catch.
    > - Disables a bunch of warnings that this library is full of.
    >   - "-Wno-dangling-else"
    >   - "-Wno-logical-op-parentheses"
    >   - "-Wno-switch"
    > 
    > Bug: 750327
    > Change-Id: Ic08f67e328da2f22f3503eab074be6aaf2f2fc90
    > Reviewed-on: https://chromium-review.googlesource.com/872219
    > Reviewed-by: Ken Rockot <rockot@chromium.org>
    > Reviewed-by: Nathan Parker <nparker@chromium.org>
    > Reviewed-by: Jialiu Lin <jialiul@chromium.org>
    > Commit-Queue: Varun Khaneja <vakh@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#531926}
    
    TBR=rockot@chromium.org,nparker@chromium.org,vakh@chromium.org,jialiul@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 750327
    Change-Id: Ided3cdfc2b4849de090a329a29de94288b616a3c
    Reviewed-on: https://chromium-review.googlesource.com/894999
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Commit-Queue: Varun Khaneja <vakh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533337}

--- third_party/unrar/src/isnt.cpp
+++ third_party/unrar/src/isnt.cpp
@@ -1,17 +1,25 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#include "isnt.hpp"
+#include "rar.hpp"
 
 #ifdef _WIN_ALL
-#include "versionhelpers.h"
-
 DWORD WinNT()
 {
-  if (!IsWinXpOrGreater()) return WNT_NONE;
-  if (!IsWinVistaOrGreater()) return WNT_WXP;
-  if (!IsWindows7OrGreater()) return WNT_VISTA;
-  if (!IsWindows8OrGreater()) return WNT_W7;
-  if (!IsWindows8Point1OrGreater()) return WNT_W8;
-  if (!IsWindows10OrGreater()) return WNT_W81;
-  return WNT_W10;
+  static int dwPlatformId=-1;
+  static DWORD dwMajorVersion,dwMinorVersion;
+  if (dwPlatformId==-1)
+  {
+    OSVERSIONINFO WinVer;
+    WinVer.dwOSVersionInfoSize=sizeof(WinVer);
+    GetVersionEx(&WinVer);
+    dwPlatformId=WinVer.dwPlatformId;
+    dwMajorVersion=WinVer.dwMajorVersion;
+    dwMinorVersion=WinVer.dwMinorVersion;
+  }
+  DWORD Result=0;
+  if (dwPlatformId==VER_PLATFORM_WIN32_NT)
+    Result=dwMajorVersion*0x100+dwMinorVersion;
+
+
+  return Result;
 }
 #endif
--- third_party/unrar/src/os.hpp
+++ third_party/unrar/src/os.hpp
@@ -24,22 +24,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifdef _WIN_ALL
 
 #define STRICT
-
-#if !defined(CHROMIUM_UNRAR)
 #define UNICODE
 #undef WINVER
 #undef _WIN32_WINNT
 #define WINVER 0x0501
 #define _WIN32_WINNT 0x0501
-#endif  // CHROMIUM_UNRAR
 
-#if !defined(ZIPSFX) && !defined(CHROMIUM_UNRAR)
+#if !defined(ZIPSFX)
 #define RAR_SMP
 #endif
 
-#if !defined(CHROMIUM_UNRAR)
 #define WIN32_LEAN_AND_MEAN
-#endif  // CHROMIUM_UNRAR
 
 #include <windows.h>
 #include <prsht.h>
@@ -71,11 +66,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   #include <direct.h>
   #include <intrin.h>
 
-#if !defined(CHROMIUM_UNRAR)
   #define USE_SSE
   #define SSE_ALIGNMENT 16
-#endif  // CHROMIUM_UNRAR
-
 #else
   #include <dirent.h>
 #endif // _MSC_VER
