7d00b9e9c530b81f4d2a226dcd09a67d0b9004b8
arthursonzogni <arthursonzogni@chromium.org>
2018-01-31 08:42:29 +0000

    Fix wrong RenderView history.
    This fixes https://crbug.com/796561 and adds a regression test.
    
    The issue was a race between the browser and renderer processes while
    updating the renderer's history.
    
    The renderer's history is updated in
    * RenderFrameImpl::CommitNavigation and in
    * RenderFrameImpl::DidCommitProvisionalLoad.
    
     ┌────────┐                  ┌───────┐
     │Renderer│                  │Browser│
     └───┬────┘                  └───┬───┘
         │  BeginNavigation          │
         │   - url=iframe.html       │
         │   - history_offset = 0    │
         │   - history_length = 1    │
    1)   │ ──────────────────────────> ┐
         │                           │ │
         │ DidCommitProvisionalLoad  │ │
         │  - "pushState"            │ │
         │  - history_offset = 1     │ │
         │  - history_length = 2     │ │
    2)   │ ──────────────────────────> │
         │                           │ │
         │ CommitNavigation          │ │
         │   - url=iframe.html       │ │
         │   - history_offset = 0    │ │
         │   - history_length = 1    │ │
       ┌─│ <───────────────────────────┘
       │ │                           │
       │ │ DidCommitProvisionalLoad  │
       │ │  - url=iframe.hmtl        │
       │ │  - history_offset = 0     │
       │ │  - history_length = 1     │
       └>│ ──────────────────────────>
     ┌───┴────┐                  ┌───┴───┐
     │Renderer│                  │Browser│
     └────────┘                  └───────┘
    
    What happens in the bug.
    1) An iframe is created: [history.offset = 0, history.size = 1].
    2) history.pushState: history is updated [offset = 1, size = 2].
    3) CommitNavigation is received, history is updated using the old
       values transmitted in 1) with BeginNavigation.
    
    This CL update the values sent in CommitNavigation. This reduce
    considerably the duration when the race can happen.
    
    Before this CL, the race could happen if a history.pushState is sent in
    between BeginNavigation and CommitNavigation (~300ms <-> severals
    seconds).  After this CL, the race happens if the history.pushState is
    sent between when the CommitNavigation message is sent and when it is
    received by the renderer process.
    
    Bug: 796561
    Change-Id: I7bf72fadbf9f4946da28126a394adddf051f49a8
    Reviewed-on: https://chromium-review.googlesource.com/881171
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533233}

--- content/browser/frame_host/navigation_request.cc
+++ content/browser/frame_host/navigation_request.cc
@@ -1298,6 +1298,7 @@ void NavigationRequest::OnWillProcessResponseChecksComplete(
 void NavigationRequest::CommitErrorPage(
     RenderFrameHostImpl* render_frame_host,
     const base::Optional<std::string>& error_page_content) {
+  UpdateRequestNavigationParamsHistory();
   frame_tree_node_->TransferNavigationRequestOwnership(render_frame_host);
   navigation_handle_->ReadyToCommitNavigation(render_frame_host);
   render_frame_host->FailedNavigation(common_params_, request_params_,
@@ -1306,6 +1307,7 @@ void NavigationRequest::CommitErrorPage(
 }
 
 void NavigationRequest::CommitNavigation() {
+  UpdateRequestNavigationParamsHistory();
   DCHECK(response_ || !IsURLHandledByNetworkStack(common_params_.url) ||
          navigation_handle_->IsSameDocument());
   DCHECK(!common_params_.url.SchemeIs(url::kJavaScriptScheme));
@@ -1451,6 +1453,15 @@ NavigationRequest::CheckLegacyProtocolInSubresource() const {
   parent->AddMessageToConsole(CONSOLE_MESSAGE_LEVEL_WARNING, console_message);
 
   return LegacyProtocolInSubresourceCheckResult::BLOCK_REQUEST;
+}
+
+void NavigationRequest::UpdateRequestNavigationParamsHistory() {
+  NavigationController* navigation_controller =
+      frame_tree_node_->navigator()->GetController();
+  request_params_.current_history_list_offset =
+      navigation_controller->GetCurrentEntryIndex();
+  request_params_.current_history_list_length =
+      navigation_controller->GetEntryCount();
 }
 
 }  // namespace content
