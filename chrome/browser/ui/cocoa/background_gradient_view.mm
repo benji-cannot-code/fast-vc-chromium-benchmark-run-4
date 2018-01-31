c8a93383a9d3e81a38c496711b682e5fb3cb00c1
Elly Fong-Jones <ellyjones@google.com>
2018-01-31 19:35:41 +0000

    cocoa: use proper color for gradient view separators
    The separators should be drawn in COLOR_TOOLBAR_BOTTOM_SEPARATOR; this change
    was already made on the Views side in eba855f22deff3a00b76384582808925841f4be5.
    Screenshots are attached on the bug showing the effect of this change.
    
    Bug: 749732
    Change-Id: Ie522f47d77dc492642e352d6b22b1d2d6776c0f0
    Reviewed-on: https://chromium-review.googlesource.com/895809
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533359}

--- chrome/browser/ui/cocoa/background_gradient_view.mm
+++ chrome/browser/ui/cocoa/background_gradient_view.mm
@@ -106,7 +106,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       return [NSColor blackColor];
   }
   return themeProvider->GetNSColor(
-             ThemeProperties::COLOR_DETACHED_BOOKMARK_BAR_SEPARATOR);
+      ThemeProperties::COLOR_TOOLBAR_BOTTOM_SEPARATOR);
 }
 
 - (NSColor*)backgroundImageColor {
