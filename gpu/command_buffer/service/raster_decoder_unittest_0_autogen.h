4934d375ea51a011190d15e58685509fa68f16da
Jonathan Backer <backer@chromium.org>
2018-01-31 16:25:04 +0000

    Add RasterDecoder autogen unittests
    This CL adds a few things:
    - autogenerated unittests
    - associated mocks
    - associated test drivers
    - enough implementation in raster_decoder.cc to make the tests pass
    
    No changes to GLES2 autogen.
    
    Bug: 789238
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Iac2ee4922a21f3bd11ff58693e2e5fb1fe26bd45
    Reviewed-on: https://chromium-review.googlesource.com/891988
    Commit-Queue: Jonathan Backer <backer@chromium.org>
    Reviewed-by: Victor Miura <vmiura@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533291}

---
+++ gpu/command_buffer/service/raster_decoder_unittest_0_autogen.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This file is auto-generated from
+// gpu/command_buffer/build_raster_cmd_buffer.py
+// It's formatted by clang-format using chromium coding style:
+//    clang-format -i -style=chromium filename
+// DO NOT EDIT!
+
+// It is included by raster_cmd_decoder_unittest_base.cc
+#ifndef GPU_COMMAND_BUFFER_SERVICE_RASTER_DECODER_UNITTEST_0_AUTOGEN_H_
+#define GPU_COMMAND_BUFFER_SERVICE_RASTER_DECODER_UNITTEST_0_AUTOGEN_H_
+
+#endif  // GPU_COMMAND_BUFFER_SERVICE_RASTER_DECODER_UNITTEST_0_AUTOGEN_H_
