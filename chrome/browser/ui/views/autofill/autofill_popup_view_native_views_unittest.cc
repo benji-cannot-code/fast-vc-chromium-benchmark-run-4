54d3fb99aebbeee74bbdecdf739c92dc5aba3539
Tommy Martino <tmartino@chromium.org>
2018-01-31 22:41:04 +0000

    [Autofill Views] Adding events and styling
    This CL fills in missing pieces of the Views implementation of the
    Autofill popup. Notably, this reintroduces styling, hover effects,
    and separators. It also gives the general bones of the code structure
    for creating child rows, which will in the future be used for testing
    variant designs (e.g., A/B testing new UX), and possibly also for
    allowing divergence between use cases (e.g., bespoke designs for
    autofill vs. passwords, different approaches for insecure context
    warnings, etc.).
    
    Bug: 768881
    Change-Id: Ie6a733b392ae33ef557e20fcc43ddd05c9d06f6b
    Reviewed-on: https://chromium-review.googlesource.com/815256
    Commit-Queue: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: anthonyvd <anthonyvd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533451}

--- chrome/browser/ui/views/autofill/autofill_popup_view_native_views_unittest.cc
+++ chrome/browser/ui/views/autofill/autofill_popup_view_native_views_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/strings/string_util.h"
 #include "build/build_config.h"
 #include "chrome/browser/ui/autofill/autofill_popup_controller.h"
 #include "chrome/browser/ui/autofill/autofill_popup_layout_model.h"
@@ -82,12 +83,17 @@ class MockAutofillPopupController : public autofill::AutofillPopupController {
   const base::string16& GetElidedValueAt(int i) const override {
     return suggestions_[i].value;
   }
-  MOCK_CONST_METHOD1(GetElidedLabelAt, const base::string16&(int row));
+
+  const base::string16& GetElidedLabelAt(int row) const override {
+    return base::EmptyString16();
+  }
+
   MOCK_METHOD3(GetRemovalConfirmationText,
                bool(int index, base::string16* title, base::string16* body));
   MOCK_METHOD1(RemoveSuggestion, bool(int index));
   MOCK_CONST_METHOD1(GetBackgroundColorIDForRow,
                      ui::NativeTheme::ColorId(int index));
+  MOCK_METHOD1(SetSelectedLine, void(base::Optional<int> selected_line));
   MOCK_CONST_METHOD0(selected_line, base::Optional<int>());
   const autofill::AutofillPopupLayoutModel& layout_model() const override {
     return *layout_model_;
