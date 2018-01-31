117cdb3e6bc1f30c4721a39cff433bc5a869c24e
Ilya Sherman <isherman@chromium.org>
2018-01-31 04:28:32 +0000

    [Variations] Record the safe seed's freshness in safe mode.
    R=asvitkine@chromium.org
    
    Bug: 727984
    Change-Id: I1ac800cce5cbfc7de67885ac6f9c3c6a715f25cb
    Reviewed-on: https://chromium-review.googlesource.com/892323
    Commit-Queue: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533199}

--- components/variations/service/safe_seed_manager.h
+++ components/variations/service/safe_seed_manager.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/macros.h"
+#include "base/time/time.h"
 
 class PrefRegistrySimple;
 class PrefService;
@@ -42,7 +43,8 @@ class SafeSeedManager {
   virtual void SetActiveSeedState(
       const std::string& seed_data,
       const std::string& base64_seed_signature,
-      std::unique_ptr<ClientFilterableState> client_filterable_state);
+      std::unique_ptr<ClientFilterableState> client_filterable_state,
+      base::Time seed_fetch_time);
 
   // Records that a fetch has started: pessimistically increments the
   // corresponding failure streak for safe mode.
@@ -60,7 +62,8 @@ class SafeSeedManager {
     ActiveSeedState(
         const std::string& seed_data,
         const std::string& base64_seed_signature,
-        std::unique_ptr<ClientFilterableState> client_filterable_state);
+        std::unique_ptr<ClientFilterableState> client_filterable_state,
+        base::Time seed_fetch_time);
     ~ActiveSeedState();
 
     // The serialized variations seed data.
@@ -71,6 +74,10 @@ class SafeSeedManager {
 
     // The client state which is used for filtering studies.
     const std::unique_ptr<ClientFilterableState> client_filterable_state;
+
+    // The latest timestamp at which this seed was fetched. This is always a
+    // client-side timestamp, never a server-provided timestamp.
+    const base::Time seed_fetch_time;
   };
   std::unique_ptr<ActiveSeedState> active_seed_state_;
 
--- components/variations/service/variations_service.cc
+++ components/variations/service/variations_service.cc
@@ -377,7 +377,6 @@ void VariationsService::RegisterPrefs(PrefRegistrySimple* registry) {
   SafeSeedManager::RegisterPrefs(registry);
   VariationsSeedStore::RegisterPrefs(registry);
 
-  registry->RegisterTimePref(prefs::kVariationsLastFetchTime, base::Time());
   // This preference will only be written by the policy service, which will fill
   // it according to a value stored in the User Policy.
   registry->RegisterStringPref(prefs::kVariationsRestrictParameter,
@@ -739,7 +738,7 @@ void VariationsService::PerformSimulationWithVersion(
 }
 
 void VariationsService::RecordSuccessfulFetch() {
-  field_trial_creator_.RecordLastFetchTime();
+  field_trial_creator_.seed_store()->RecordLastFetchTime();
   safe_seed_manager_.RecordSuccessfulFetch(field_trial_creator_.seed_store());
 }
 
