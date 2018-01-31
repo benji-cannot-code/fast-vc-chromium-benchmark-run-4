3493b4c927a66e1c821132e6802104fc41ade8fb
Shivani Sharma <shivanisha@chromium.org>
2018-01-31 18:07:12 +0000

    Propagation of ad tagging from parent frame to child frame.
    This CL tags a subframe as an ad if its parent frame is tagged as well.
    This extends the coverage of frames tagged as ads from just those that
    match the ruleset to those that do not match but their parent frame is
    tagged as an ad frame.
    
    BUG: 807640
    Change-Id: Ifee656298cdbe8bc16bcd24a16bc9a9905e2c2a3
    Reviewed-on: https://chromium-review.googlesource.com/894542
    Commit-Queue: Shivani Sharma <shivanisha@chromium.org>
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533323}

--- components/subresource_filter/content/browser/content_subresource_filter_throttle_manager.h
+++ components/subresource_filter/content/browser/content_subresource_filter_throttle_manager.h
@@ -6,13 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_SUBRESOURCE_FILTER_CONTENT_BROWSER_CONTENT_SUBRESOURCE_FILTER_THROTTLE_MANAGER_H_
 #define COMPONENTS_SUBRESOURCE_FILTER_CONTENT_BROWSER_CONTENT_SUBRESOURCE_FILTER_THROTTLE_MANAGER_H_
 
+#include <map>
 #include <memory>
-#include <unordered_map>
+#include <set>
 #include <vector>
 
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observer.h"
+#include "base/stl_util.h"
 #include "components/subresource_filter/content/browser/subresource_filter_observer.h"
 #include "components/subresource_filter/content/browser/verified_ruleset_dealer.h"
 #include "components/subresource_filter/core/common/activation_decision.h"
@@ -85,6 +87,8 @@ class ContentSubresourceFilterThrottleManager
     return ruleset_handle_.get();
   }
 
+  bool IsFrameTaggedAsAdForTesting(content::RenderFrameHost* frame_host);
+
  protected:
   // content::WebContentsObserver:
   void RenderFrameDeleted(content::RenderFrameHost* frame_host) override;
@@ -144,8 +148,8 @@ class ContentSubresourceFilterThrottleManager
   // For each RenderFrameHost where the last committed load has subresource
   // filtering activated, owns the corresponding AsyncDocumentSubresourceFilter.
   // It is possible for a frame to have a null filter.
-  std::unordered_map<content::RenderFrameHost*,
-                     std::unique_ptr<AsyncDocumentSubresourceFilter>>
+  std::map<content::RenderFrameHost*,
+           std::unique_ptr<AsyncDocumentSubresourceFilter>>
       activated_frame_hosts_;
 
   // For each ongoing navigation that requires activation state computation,
@@ -158,8 +162,12 @@ class ContentSubresourceFilterThrottleManager
     bool is_ad_subframe = false;
   };
 
-  std::unordered_map<content::NavigationHandle*, OngoingThrottleInfo>
+  std::map<content::NavigationHandle*, OngoingThrottleInfo>
       ongoing_activation_throttles_;
+
+  // Set of frames that have been identified as ads, either through matching the
+  // ruleset or if their parent frame was an ad frame.
+  std::set<content::RenderFrameHost*> ad_frames_;
 
   ScopedObserver<SubresourceFilterObserverManager, SubresourceFilterObserver>
       scoped_observer_;
