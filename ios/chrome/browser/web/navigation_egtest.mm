095c495ed9f302c052d7915ef6d8213880d13b89
Danyao Wang <danyao@chromium.org>
2018-01-31 18:56:29 +0000

    [Nav Experiment] Fix NavigationTestCase/testHistoryBackNavigation.
    This EarlGrey test tests the behavior of renderer-initiated back
    navigation. With WKBasedNavigationManager, renderer-initiated
    back navigation happens completely in the WebProcess. Adding the wait
    makes sure the Omnibox URL check is performed after the navigation
    finishes.
    
    Bug: 807428
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I636f4b991e6bcb901b09851de08b4fef6970002a
    Reviewed-on: https://chromium-review.googlesource.com/894177
    Commit-Queue: Danyao Wang <danyao@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533342}

--- ios/chrome/browser/web/navigation_egtest.mm
+++ ios/chrome/browser/web/navigation_egtest.mm
@@ -272,6 +272,7 @@ std::unique_ptr<net::test_server::HttpResponse> WindowLocationHashHandlers(
 
   // Tap the back button in the HTML and verify the first URL is loaded.
   [ChromeEarlGrey tapWebViewElementWithID:kGoBackID];
+  [ChromeEarlGrey waitForWebViewContainingText:"pony"];
   [[EarlGrey selectElementWithMatcher:OmniboxText(firstURL.GetContent())]
       assertWithMatcher:grey_notNil()];
 
