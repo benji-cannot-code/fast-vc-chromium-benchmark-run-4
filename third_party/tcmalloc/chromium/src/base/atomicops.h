b6ac47762baf602a929e90605a1d3600945c1c87
Wang Qing <wangqing-hf@loongson.cn>
2018-01-31 03:52:47 +0000

    [MIPS64] Add support for using tcmalloc on loongson platform.
    Currently, tcmalloc is not supported on loongson, and the build
    failed with use tcmalloc. This CL adds support for tcmalloc on
    loongson, and resolves many compiler errors, bugs when test
    base_unittests on loongosn.
    
    Bug: 799810
    
    R= wfh@chromium.org, thakis@chromium.org
    
    Change-Id: I68bea09334cb9af02d083a24a94654aaf6c884c8
    Reviewed-on: https://chromium-review.googlesource.com/853695
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: 汪 清 <wangqing-hf@loongson.cn>
    Cr-Commit-Position: refs/heads/master@{#533192}

--- third_party/tcmalloc/chromium/src/base/atomicops.h
+++ third_party/tcmalloc/chromium/src/base/atomicops.h
@@ -102,6 +102,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/atomicops-internals-x86.h"
 #elif defined(__linux__) && defined(__PPC__)
 #include "base/atomicops-internals-linuxppc.h"
+#elif defined(__GNUC__) && defined(__mips__)
+#include "base/atomicops-internals-mips.h"
 #else
 // Assume x86 for now.  If you need to support a new architecture and
 // don't know how to implement atomic ops, you can probably get away
