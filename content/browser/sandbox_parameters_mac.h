3ac870115a776ee521d6bc199d65b5c052d22e03
Greg Kerr <kerrnel@chromium.org>
2018-01-31 23:14:25 +0000

    macOS V2 Sandbox: Add component flash player to PPAPI sandbox profile.
    This adds access to the component flash player to the PPAPI sandbox
    profile.
    
    Tbr: creis@chromium.org
    Bug: 802276
    Change-Id: Iddf87ef8a14c2b6ffe8950e870ea81503a415395
    Reviewed-on: https://chromium-review.googlesource.com/882219
    Commit-Queue: Greg Kerr <kerrnel@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533467}

--- content/browser/sandbox_parameters_mac.h
+++ content/browser/sandbox_parameters_mac.h
@@ -23,6 +23,8 @@ void SetupCommonSandboxParameters(sandbox::SeatbeltExecClient* client);
 
 void SetupCDMSandboxParameters(sandbox::SeatbeltExecClient* client);
 
+void SetupPPAPISandboxParameters(sandbox::SeatbeltExecClient* client);
+
 void SetupUtilitySandboxParameters(sandbox::SeatbeltExecClient* client,
                                    const base::CommandLine& command_line);
 
