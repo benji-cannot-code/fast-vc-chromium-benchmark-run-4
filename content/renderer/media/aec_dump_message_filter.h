b0587141ef5cb556ba31c07f7331f18cd67bb866
Henrik Grunell <grunell@chromium.org>
2018-01-31 12:55:49 +0000

    Remove possibility to set AEC3 after MediaStreamAudioProcessor creation.
    That is, after input stream creation.
    
    It will be removed from the underlying WebRTC component shortly.
    
    Bug: webrtc:8346
    Change-Id: Ib80b27b4580b737bcfec7f80acc9207ddafab419
    Reviewed-on: https://chromium-review.googlesource.com/888703
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Henrik Grunell <grunell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533259}

--- content/renderer/media/aec_dump_message_filter.h
+++ content/renderer/media/aec_dump_message_filter.h
@@ -31,7 +31,6 @@ class CONTENT_EXPORT AecDumpMessageFilter : public IPC::MessageFilter {
     virtual void OnAecDumpFile(
         const IPC::PlatformFileForTransit& file_handle) = 0;
     virtual void OnDisableAecDump() = 0;
-    virtual void OnAec3Enable(bool enable) = 0;
     virtual void OnIpcClosing() = 0;
   };
 
@@ -62,6 +61,12 @@ class CONTENT_EXPORT AecDumpMessageFilter : public IPC::MessageFilter {
 
  protected:
   ~AecDumpMessageFilter() override;
+
+  // When this variable is not set, the use of AEC3 is governed by the Finch
+  // experiment and/or WebRTC's own default. When set to true/false, Finch and
+  // WebRTC defaults will be overridden, and AEC3/AEC2 (respectively) will be
+  // used.
+  base::Optional<bool> override_aec3_;
 
  private:
   // Sends an IPC message using |sender_|.
@@ -113,12 +118,6 @@ class CONTENT_EXPORT AecDumpMessageFilter : public IPC::MessageFilter {
 
   // The singleton instance for this filter.
   static AecDumpMessageFilter* g_filter;
-
-  // When this variable is not set, the use of AEC3 is governed by the Finch
-  // experiment and/or WebRTC's own default. When set to true/false, Finch and
-  // WebRTC defaults will be overridden, and AEC3/AEC2 (respectively) will be
-  // used.
-  base::Optional<bool> override_aec3_;
 
   DISALLOW_COPY_AND_ASSIGN(AecDumpMessageFilter);
 };
