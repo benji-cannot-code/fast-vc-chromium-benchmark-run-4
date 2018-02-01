6551d31dbc3d832b1ca3d3e08fc1475cd6700c0c
Shubhie Panicker <panicker@chromium.org>
2018-02-01 01:40:19 +0000

    Make the scheduler observe and respect "keepalive".
    keepalive (renamed to KeepActive) is true if there is a
    running service worker, or shared worker or unfinished fetch
    keep-alive. In this case the scheduler should not freeze the
    renderer.
    
    If keepalive state changes then run UpdatePolicy,
    as a frozen renderer may need to be woken up.
    
    For full discussion, see:
    https://groups.google.com/a/google.com/d/msg/chrome-lifecycle-apis/qjPzJF0quRs/lurC3EWqAAAJ
    
    Change-Id: Iafa3e1b745ede38d2dcd6d89b232cc4d12e1e785
    Reviewed-on: https://chromium-review.googlesource.com/869172
    Commit-Queue: Shubhie Panicker <panicker@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533517}

--- third_party/WebKit/Source/platform/scheduler/renderer/renderer_scheduler_impl.cc
+++ third_party/WebKit/Source/platform/scheduler/renderer/renderer_scheduler_impl.cc
@@ -385,6 +385,11 @@ RendererSchedulerImpl::MainThreadOnly::MainThreadOnly(
                             renderer_scheduler_impl,
                             &renderer_scheduler_impl->tracing_controller_,
                             BackgroundStateToString),
+      keep_active_fetch_or_worker(false,
+                                  "RendererScheduler.RendererKeepAactive",
+                                  renderer_scheduler_impl,
+                                  &renderer_scheduler_impl->tracing_controller_,
+                                  YesNoStateToString),
       stopping_when_backgrounded_enabled(
           false,
           "RendererScheduler.StoppingWhenBackgroundedEnabled",
@@ -934,6 +939,13 @@ void RendererSchedulerImpl::SetRendererBackgrounded(bool backgrounded) {
   }
 }
 
+void RendererSchedulerImpl::SetSchedulerKeepActive(bool keep_active) {
+  if (main_thread_only().keep_active_fetch_or_worker == keep_active)
+    return;
+  main_thread_only().keep_active_fetch_or_worker = keep_active;
+  UpdatePolicy();
+}
+
 #if defined(OS_ANDROID)
 void RendererSchedulerImpl::PauseTimersForAndroidWebView() {
   main_thread_only().pause_timers_for_webview = true;
@@ -1371,7 +1383,8 @@ void RendererSchedulerImpl::UpdatePolicyLocked(UpdateType update_type) {
       main_thread_only().stopped_when_backgrounded;
   bool newly_stopped = false;
   if (main_thread_only().renderer_backgrounded &&
-      main_thread_only().stopping_when_backgrounded_enabled) {
+      main_thread_only().stopping_when_backgrounded_enabled &&
+      !main_thread_only().keep_active_fetch_or_worker) {
     base::TimeTicks stop_at = main_thread_only().background_status_changed_at +
                               delay_for_background_tab_stopping_;
 
@@ -2034,6 +2047,8 @@ RendererSchedulerImpl::AsValueLocked(base::TimeTicks optional_now) const {
       main_thread_only().have_reported_blocking_intervention_since_navigation);
   state->SetBoolean("renderer_backgrounded",
                     main_thread_only().renderer_backgrounded);
+  state->SetBoolean("keep_active_fetch_or_worker",
+                    main_thread_only().keep_active_fetch_or_worker);
   state->SetBoolean("stopped_when_backgrounded",
                     main_thread_only().stopped_when_backgrounded);
   state->SetDouble("now", (optional_now - base::TimeTicks()).InMillisecondsF());
