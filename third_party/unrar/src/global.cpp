bbfee4e258a0d725d82d4d456e8ee6f21a1599c0
Varun Khaneja <vakh@chromium.org>
2018-02-01 15:52:33 +0000

    Revert "[1] Add unrar source code (v5.6.1.4) to third_party."
    This reverts commit 0941a32d0fe8820862a438e2dc3a1a19c0af41b2.
    
    Reason for revert: Causing build failures https://bugs.chromium.org/p/chromium/issues/detail?id=807391
    
    Original change's description:
    > [1] Add unrar source code (v5.6.1.4) to third_party.
    >
    > Safe Browsing Download Protection for .RAR files:
    > This library will be used to unpack any .rar files
    > downloaded by the user to ensure that they are safe.
    > This code would execute inside a sandboxed process.
    >
    > This code is being fuzzed via OSS-Fuzz here:
    > https://github.com/google/oss-fuzz/tree/master/projects/unrar
    >
    > Here's the author's permission to use this library
    > for fuzzing and inclusion in Chrome: http://go/wknlc
    >
    > Followed the process listed at:
    > https://chromium.googlesource.com/chromium/src.git/+/master/docs/adding_to_third_party.md
    >
    > Bug: 750327
    > Change-Id: Ia6978fcf43f1ba570d6e8dad5daf2903cbdb3834
    > Reviewed-on: https://chromium-review.googlesource.com/855044
    > Commit-Queue: Varun Khaneja <vakh@chromium.org>
    > Reviewed-by: Justin Schuh <jschuh@chromium.org>
    > Reviewed-by: Varun Khaneja <vakh@chromium.org>
    > Reviewed-by: Jialiu Lin <jialiul@chromium.org>
    > Reviewed-by: Nathan Parker <nparker@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#531767}
    
    TBR=thakis@chromium.org,jschuh@chromium.org,xam@google.com,nparker@chromium.org,vakh@chromium.org,jialiul@chromium.org,rend@google.com,jschuh@google.com,morbo@google.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 750327, 807391
    Change-Id: Id2494b74b1eceebd67cef769219e0b2149dad8d9
    Reviewed-on: https://chromium-review.googlesource.com/895399
    Reviewed-by: Nathan Parker <nparker@chromium.org>
    Reviewed-by: Jialiu Lin <jialiul@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Commit-Queue: Varun Khaneja <vakh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533682}

--- third_party/unrar/src/global.cpp
+++
@@ -1,8 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#define INCLUDEGLOBAL
-
-#if defined(__BORLANDC__) || defined(_MSC_VER)
-#pragma hdrstop
-#endif
-
-#include "rar.hpp"
