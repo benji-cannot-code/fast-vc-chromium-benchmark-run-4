3306764490b4a2a9b9c06885a8d01c8353239442
John Williams <jrw@google.com>
2018-02-01 03:20:21 +0000

    Added media router component extension.
    This CL adds the media router git repository to the top-level DEPS
    file and adds supporting files that allow the code to be built as part
    of the main build process.  The version of the media router that can
    be built from the Chromium tree is crippled because it contains to
    media route providers.
    
    Things coming in future CLs:
    - Nontrivial media route providers.
    - Execution of unit tests.
    - Incorporating the built extension into released build
      artifacts.
    - Integration with Chromium waterfall.
    
    Bug: 698796
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: If5b4a02dfe27f618fd34e586bd8be07833b30d23
    Reviewed-on: https://chromium-review.googlesource.com/783555
    Commit-Queue: John Williams <jrw@chromium.org>
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533543}

---
+++ chrome/browser/resources/media_router/extension/prelude.js
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+goog = {};
+goog.require = () => undefined;
+goog.provide = goog.require;
+goog.module = goog.require;
+goog.forwardDeclare = goog.require;
+goog.scope = body => {
+  body.call(window);
+};
