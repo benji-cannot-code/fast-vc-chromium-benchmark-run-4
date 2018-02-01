341db312ca0c2df4267dfa99eee13b287b17c2a8
Fredrik Söderquist <fs@opera.com>
2018-02-01 17:26:37 +0000

    [PE] Support 'pathLength' for <path>s referenced from <textPath>
    A 'pathLength' attribute on a <path> referenced from <textPath> will
    affect the starting offset on the path as computed from 'startOffset'
    attribute.
    Refactor the PathPositionMapper setup a bit so that the start offset is
    computed together with all the other path related state.
    
    Bug: 803127
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Id12c88580dd74db403598d2aa05ac9764ad68cdb
    Reviewed-on: https://chromium-review.googlesource.com/897626
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#533712}

--- third_party/WebKit/Source/core/layout/svg/LayoutSVGTextPath.h
+++ third_party/WebKit/Source/core/layout/svg/LayoutSVGTextPath.h
@@ -35,8 +35,11 @@ class PathPositionMapper {
   USING_FAST_MALLOC(PathPositionMapper);
 
  public:
-  static std::unique_ptr<PathPositionMapper> Create(const Path& path) {
-    return WTF::WrapUnique(new PathPositionMapper(path));
+  static std::unique_ptr<PathPositionMapper> Create(const Path& path,
+                                                    float computed_path_length,
+                                                    float start_offset) {
+    return WTF::WrapUnique(
+        new PathPositionMapper(path, computed_path_length, start_offset));
   }
 
   enum PositionType {
@@ -46,12 +49,16 @@ class PathPositionMapper {
   };
   PositionType PointAndNormalAtLength(float length, FloatPoint&, float& angle);
   float length() const { return path_length_; }
+  float StartOffset() const { return path_start_offset_; }
 
  private:
-  explicit PathPositionMapper(const Path&);
+  PathPositionMapper(const Path&,
+                     float computed_path_length,
+                     float start_offset);
 
   Path::PositionCalculator position_calculator_;
   float path_length_;
+  float path_start_offset_;
 };
 
 class LayoutSVGTextPath final : public LayoutSVGInline {
@@ -59,7 +66,6 @@ class LayoutSVGTextPath final : public LayoutSVGInline {
   explicit LayoutSVGTextPath(Element*);
 
   std::unique_ptr<PathPositionMapper> LayoutPath() const;
-  float CalculateStartOffset(float) const;
 
   bool IsChildAllowed(LayoutObject*, const ComputedStyle&) const override;
 
