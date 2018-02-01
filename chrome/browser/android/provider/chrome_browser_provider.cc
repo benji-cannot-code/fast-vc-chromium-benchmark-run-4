648bff82042f04e4d9c54be2081a389eea027b00
Ran Ji <ranj@chromium.org>
2018-02-01 18:33:08 +0000

    Free jobjects inside loop
    We have limited number of jni local references, leaking jobjects inside
    loop may cause local reference table to overflow.
    
    Bug: 726426
    Change-Id: I4b1ffc9fcb74d1a78d0415f9d9c79533facb7839
    Reviewed-on: https://chromium-review.googlesource.com/895843
    Commit-Queue: Ran Ji <ranj@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533744}

--- chrome/browser/android/provider/chrome_browser_provider.cc
+++ chrome/browser/android/provider/chrome_browser_provider.cc
@@ -143,8 +143,9 @@ std::vector<base::string16> ConvertJStringArrayToString16Array(
   if (array) {
     jsize len = env->GetArrayLength(array);
     for (int i = 0; i < len; i++) {
-      results.push_back(ConvertJavaStringToUTF16(env,
-          static_cast<jstring>(env->GetObjectArrayElement(array, i))));
+      ScopedJavaLocalRef<jstring> j_str(
+          env, static_cast<jstring>(env->GetObjectArrayElement(array, i)));
+      results.push_back(ConvertJavaStringToUTF16(env, j_str));
     }
   }
   return results;
@@ -940,8 +941,9 @@ ScopedJavaLocalRef<jobject> ChromeBrowserProvider::QueryBookmarkFromAPI(
   if (projection) {
     jsize len = env->GetArrayLength(projection);
     for (int i = 0; i < len; i++) {
-      std::string name = ConvertJavaStringToUTF8(env, static_cast<jstring>(
-          env->GetObjectArrayElement(projection, i)));
+      ScopedJavaLocalRef<jstring> j_name(
+          env, static_cast<jstring>(env->GetObjectArrayElement(projection, i)));
+      std::string name = ConvertJavaStringToUTF8(env, j_name);
       history::HistoryAndBookmarkRow::ColumnID id =
           history::HistoryAndBookmarkRow::GetColumnID(name);
       if (id == history::HistoryAndBookmarkRow::COLUMN_END) {
@@ -1081,8 +1083,9 @@ ScopedJavaLocalRef<jobject> ChromeBrowserProvider::QuerySearchTermFromAPI(
   if (projection) {
     jsize len = env->GetArrayLength(projection);
     for (int i = 0; i < len; i++) {
-      std::string name = ConvertJavaStringToUTF8(env, static_cast<jstring>(
-          env->GetObjectArrayElement(projection, i)));
+      ScopedJavaLocalRef<jstring> j_name(
+          env, static_cast<jstring>(env->GetObjectArrayElement(projection, i)));
+      std::string name = ConvertJavaStringToUTF8(env, j_name);
       history::SearchRow::ColumnID id =
           history::SearchRow::GetColumnID(name);
       if (id == history::SearchRow::COLUMN_END) {
