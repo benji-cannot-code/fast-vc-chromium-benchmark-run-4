607068915187ca88cec7e67b7d2098bb258aaced
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-02-01 15:18:43 +0000

    Import wpt@4c7563419bffe3c71bda970da693d9fa6bae1c6c
    Using wpt-import in Chromium c851ddb7773246a17d8b3351727cb785d50a451c.
    With Chromium commits locally applied on WPT:
    c851ddb777 "custom-elements: Update some tests depending on uncommon features."
    
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/11420
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    jsbell@chromium.org, mek@chromium.org:
      external/wpt/FileAPI
    
    TBR=raphael.kubo.da.costa@intel.com
    
    No-Export: true
    Change-Id: I0b9faa1b3b02d31aebff37b7d402dc486639b918
    Reviewed-on: https://chromium-review.googlesource.com/896986
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533666}

---
+++ third_party/WebKit/LayoutTests/external/wpt/service-workers/service-worker/resources/fetch-event-respond-with-body-loaded-in-chunk-worker.js
@@ -0,0 +1,8 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+'use strict';
+
+self.addEventListener('fetch', event => {
+    if (!event.request.url.match(/body-in-chunk$/))
+        return;
+    event.respondWith(fetch("../../../fetch/api/resources/trickle.py?count=4&delay=50"));
+});
