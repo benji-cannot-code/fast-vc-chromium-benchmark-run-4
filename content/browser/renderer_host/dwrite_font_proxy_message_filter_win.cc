6fcfefb0548ef36331518b0f72ea9ce9529f187f
Sam McNally <sammc@chromium.org>
2018-02-01 05:26:37 +0000

    Handle null |collection_| in DWriteFontProxyImpl::GetFamilyCount again.
    Bug: 804793
    Change-Id: I5db57099fd3a0d3e348dc97a95e30af21e79d5b2
    Reviewed-on: https://chromium-review.googlesource.com/892543
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533572}

--- content/browser/renderer_host/dwrite_font_proxy_message_filter_win.cc
+++ content/browser/renderer_host/dwrite_font_proxy_message_filter_win.cc
@@ -201,7 +201,6 @@ void DWriteFontProxyImpl::FindFamily(const base::string16& family_name,
                                      FindFamilyCallback callback) {
   InitializeDirectWrite();
   TRACE_EVENT0("dwrite", "FontProxyHost::OnFindFamily");
-  DCHECK(collection_);
   UINT32 family_index = UINT32_MAX;
   if (collection_) {
     BOOL exists = FALSE;
@@ -219,8 +218,7 @@ void DWriteFontProxyImpl::FindFamily(const base::string16& family_name,
 void DWriteFontProxyImpl::GetFamilyCount(GetFamilyCountCallback callback) {
   InitializeDirectWrite();
   TRACE_EVENT0("dwrite", "FontProxyHost::OnGetFamilyCount");
-  DCHECK(collection_);
-  std::move(callback).Run(collection_->GetFontFamilyCount());
+  std::move(callback).Run(collection_ ? collection_->GetFontFamilyCount() : 0);
 }
 
 void DWriteFontProxyImpl::GetFamilyNames(UINT32 family_index,
@@ -229,7 +227,6 @@ void DWriteFontProxyImpl::GetFamilyNames(UINT32 family_index,
   TRACE_EVENT0("dwrite", "FontProxyHost::OnGetFamilyNames");
   callback = mojo::WrapCallbackWithDefaultInvokeIfNotRun(
       std::move(callback), std::vector<mojom::DWriteStringPairPtr>());
-  DCHECK(collection_);
   if (!collection_)
     return;
 
@@ -292,7 +289,6 @@ void DWriteFontProxyImpl::GetFontFiles(uint32_t family_index,
   callback = mojo::WrapCallbackWithDefaultInvokeIfNotRun(
       std::move(callback), std::vector<base::FilePath>(),
       std::vector<base::File>());
-  DCHECK(collection_);
   if (!collection_)
     return;
 
