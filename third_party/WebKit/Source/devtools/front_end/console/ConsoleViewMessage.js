a60e76bfbd4930f8c3ab1c3af0651f5230aa7920
Erik Luo <luoe@chromium.org>
2018-02-01 20:15:40 +0000

    DevTools: one objectValue css for console instead of per message
    Before, each visible console message created a 'objectValue.css'
    style tag, resulting in a lot of duplicate rules
    ( https://imgur.com/a/cT1QX )
    
    This CL replaces per-message style tags with a single one for
    ConsoleView, since messages only appear within ConsoleView.
    
    Bug: none
    Change-Id: I91b4d4ac766e2d6b29f5d68068c9d7a9d850c16a
    Reviewed-on: https://chromium-review.googlesource.com/879848
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Erik Luo <luoe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533794}

--- third_party/WebKit/Source/devtools/front_end/console/ConsoleViewMessage.js
+++ third_party/WebKit/Source/devtools/front_end/console/ConsoleViewMessage.js
@@ -115,9 +115,7 @@ Console.ConsoleViewMessage = class {
    * @return {!Element}
    */
   _buildTableMessage() {
-    var formattedMessage = createElement('span');
-    UI.appendStyle(formattedMessage, 'object_ui/objectValue.css');
-    formattedMessage.className = 'source-code';
+    var formattedMessage = createElementWithClass('span', 'source-code');
     var anchorElement = this._buildMessageAnchor();
     if (anchorElement)
       formattedMessage.appendChild(anchorElement);
@@ -272,10 +270,7 @@ Console.ConsoleViewMessage = class {
     }
     messageElement.classList.add('console-message-text');
 
-    var formattedMessage = createElement('span');
-    UI.appendStyle(formattedMessage, 'object_ui/objectValue.css');
-    formattedMessage.className = 'source-code';
-
+    var formattedMessage = createElementWithClass('span', 'source-code');
     var anchorElement = this._buildMessageAnchor();
     if (anchorElement)
       formattedMessage.appendChild(anchorElement);
