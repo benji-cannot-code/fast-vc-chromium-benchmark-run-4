5ba6346de162f9f75238949ec38c48c554f86ffb
Evan Stade <estade@chromium.org>
2018-02-01 02:20:30 +0000

    [Mac] Update QuitWithAppsController to use NotificationDisplayService.
    Bug: 783018
    Change-Id: Id77ed586c6db7401688c72deabbdcba4d8d6293d
    Reviewed-on: https://chromium-review.googlesource.com/892420
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533532}

--- chrome/browser/ui/cocoa/apps/quit_with_apps_controller_mac.cc
+++ chrome/browser/ui/cocoa/apps/quit_with_apps_controller_mac.cc
@@ -13,7 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/lifetime/application_lifetime.h"
-#include "chrome/browser/notifications/notification_ui_manager.h"
+#include "chrome/browser/notifications/notification_display_service.h"
+#include "chrome/browser/notifications/notification_handler.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_window.h"
@@ -38,9 +39,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using extensions::ExtensionRegistry;
 
+namespace {
+
 const char kQuitWithAppsOriginUrl[] = "chrome://quit-with-apps";
 const int kQuitAllAppsButtonIndex = 0;
 const int kDontShowAgainButtonIndex = 1;
+
+void CloseNotification(Profile* profile) {
+  NotificationDisplayService::GetForProfile(profile)->Close(
+      NotificationHandler::Type::TRANSIENT,
+      QuitWithAppsController::kQuitWithAppsNotificationID);
+}
+
+}  // namespace
 
 const char QuitWithAppsController::kQuitWithAppsNotificationID[] =
     "quit-with-apps";
@@ -82,15 +93,11 @@ void QuitWithAppsController::Close(bool by_user) {
 }
 
 void QuitWithAppsController::Click() {
-  g_browser_process->notification_ui_manager()->CancelById(
-      kQuitWithAppsNotificationID,
-      NotificationUIManager::GetProfileID(notification_profile_));
+  CloseNotification(notification_profile_);
 }
 
 void QuitWithAppsController::ButtonClick(int button_index) {
-  g_browser_process->notification_ui_manager()->CancelById(
-      kQuitWithAppsNotificationID,
-      NotificationUIManager::GetProfileID(notification_profile_));
+  CloseNotification(notification_profile_);
 
   if (button_index == kQuitAllAppsButtonIndex) {
     if (hosted_app_quit_notification_) {
@@ -174,14 +181,11 @@ bool QuitWithAppsController::ShouldQuit() {
   // Delete any existing notification to ensure this one is shown. If
   // notification_profile_ is NULL then it must be that no notification has been
   // added by this class yet.
-  if (notification_profile_) {
-    g_browser_process->notification_ui_manager()->CancelById(
-        kQuitWithAppsNotificationID,
-        NotificationUIManager::GetProfileID(notification_profile_));
-  }
+  if (notification_profile_)
+    CloseNotification(notification_profile_);
   notification_profile_ = profiles[0];
-  g_browser_process->notification_ui_manager()->Add(*notification_,
-                                                    notification_profile_);
+  NotificationDisplayService::GetForProfile(notification_profile_)
+      ->Display(NotificationHandler::Type::TRANSIENT, *notification_);
 
   // Always return false, the notification UI can be used to quit all apps which
   // will cause Chrome to quit.
