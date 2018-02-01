96ad97cd33526ea22e5fee8407608ef634036c61
Kevin Bailey <krb@chromium.org>
2018-02-01 01:45:13 +0000

    [omnibox] Avoid calls to Classify() surrounding CurrentTextIsURL()
    OmniboxView::UpdateTextStyle() needs to know if the text is a URL, but
    so does its caller. Rather than calculate it twice - a non-trivial
    run through Classify() - pass it.
    
    Bug: 669727
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I6a8be0bfaaf7fd6c2b5ada831de65340e71d47d6
    Reviewed-on: https://chromium-review.googlesource.com/890658
    Commit-Queue: Kevin Bailey <krb@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533518}

--- chrome/browser/ui/views/omnibox/omnibox_view_views_unittest.cc
+++ chrome/browser/ui/views/omnibox/omnibox_view_views_unittest.cc
@@ -126,7 +126,8 @@ void TestingOmniboxView::CheckUpdatePopupCallInfo(
 }
 
 void TestingOmniboxView::EmphasizeURLComponents() {
-  UpdateTextStyle(text(), model()->client()->GetSchemeClassifier());
+  UpdateTextStyle(text(), model()->CurrentTextIsURL(),
+                  model()->client()->GetSchemeClassifier());
 }
 
 void TestingOmniboxView::OnFocus() {
