3de89f4f6199d91bf2b474ddc2d2fe105ddbf834
Weidong Guo <weidongg@chromium.org>
2018-02-01 17:53:10 +0000

    Add new style folder animation
    Changes:
    Add four animations to transit from the closed folder item icon to the
    opened folder view:
    
    1. Background animation:
    This is a slide animation which transits from the rounded background of
    the folder item's icon to the folder's background when opening the
    folder.
    
    2. Folder item title animation:
    This is a slide animtion which changes the opacity of the folder item's
    title.
    
    3. Top icon animation:
    A group of layer animations to transit from the items within the folder
    item icon to the same items in the opened folder.
    
    4. Contents container animation:
    A layer animation to transit from the bounds of the folder item icon to
    the opened folder's bounds and transit opacity from 0 to 1.
    
    Specs:
    https://drive.google.com/file/d/1-qeCz6YEzQPRUKWDAI1HXdL4bKag2Q_G/view
    
    2500 ms duration video demo:
    https://drive.google.com/file/d/1Q-3s9dl1bT0xSzpzbENCX4vyD0Ud2eqG/view?usp=sharing
    
    250 ms duration video demo:
    https://drive.google.com/file/d/1vXkOCaDKoQEvKpyLXnJO79OKAyo-E7m6/view?usp=sharing
    
    BUG: 796632
    Change-Id: I00aa09dbb65f10c6611aad89194b2f8310b0ece7
    Reviewed-on: https://chromium-review.googlesource.com/890019
    Commit-Queue: Weidong Guo <weidongg@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533724}

--- ui/app_list/views/apps_container_view.h
+++ ui/app_list/views/apps_container_view.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/app_list/model/app_list_folder_item.h"
 #include "base/macros.h"
 #include "ui/app_list/views/app_list_page.h"
-#include "ui/app_list/views/top_icon_animation_view.h"
 
 namespace gfx {
 class Rect;
@@ -33,8 +32,7 @@ class PageSwitcher;
 // AppsContainerView contains a root level AppsGridView to render the root level
 // app items, and a AppListFolderView to render the app items inside the
 // active folder. Only one if them is visible to user at any time.
-class APP_LIST_EXPORT AppsContainerView : public AppListPage,
-                                          public TopIconAnimationObserver {
+class APP_LIST_EXPORT AppsContainerView : public AppListPage {
  public:
   AppsContainerView(AppListMainView* app_list_main_view, AppListModel* model);
   ~AppsContainerView() override;
@@ -90,18 +88,11 @@ class APP_LIST_EXPORT AppsContainerView : public AppListPage,
   gfx::Rect GetPageBoundsDuringDragging(ash::AppListState state) const override;
   views::View* GetSelectedView() const override;
 
-  // TopIconAnimationObserver overrides:
-  void OnTopIconAnimationsComplete() override;
-
   AppsGridView* apps_grid_view() { return apps_grid_view_; }
   FolderBackgroundView* folder_background_view() {
     return folder_background_view_;
   }
   AppListFolderView* app_list_folder_view() { return app_list_folder_view_; }
-
-  void set_folder_top_items_animation_enabled_for_test(bool enabled) {
-    is_folder_top_items_animation_enabled_ = enabled;
-  }
 
  private:
   enum ShowState {
@@ -112,19 +103,6 @@ class APP_LIST_EXPORT AppsContainerView : public AppListPage,
   };
 
   void SetShowState(ShowState show_state, bool show_apps_with_animation);
-
-  // Calculates the top item icon bounds in the active folder icon. The bounds
-  // is relative to AppsContainerView.
-  // Returns the bounds of top items' icon in sequence of top left, top right,
-  // bottom left, bottom right.
-  std::vector<gfx::Rect> GetTopItemIconBoundsInActiveFolder();
-
-  // Creates the transitional views for animating the top items in the folder
-  // when opening or closing a folder.
-  void CreateViewsForFolderTopItemsAnimation(AppListFolderItem* active_folder,
-                                             bool open_folder);
-
-  void PrepareToShowApps(AppListFolderItem* folder_item);
 
   // Gets the final top padding of search box.
   int GetSearchBoxFinalTopPadding() const;
@@ -139,15 +117,6 @@ class APP_LIST_EXPORT AppsContainerView : public AppListPage,
   FolderBackgroundView* folder_background_view_ = nullptr;
 
   ShowState show_state_ = SHOW_NONE;
-
-  // The transitional views for animating the top items in folder
-  // when opening or closing a folder.
-  std::vector<views::View*> top_icon_views_;
-
-  size_t top_icon_animation_pending_count_ = 0u;
-
-  // True if the animation for the folder top items is enabled.
-  bool is_folder_top_items_animation_enabled_ = true;
 
   DISALLOW_COPY_AND_ASSIGN(AppsContainerView);
 };
--- ui/app_list/views/apps_grid_view_unittest.cc
+++ ui/app_list/views/apps_grid_view_unittest.cc
@@ -830,10 +830,8 @@ TEST_F(AppsGridViewTest,
 }
 
 TEST_F(AppsGridViewTest, CloseFolderByClickingBackground) {
-  // Disable the animation for the folder top items for test purpose.
   AppsContainerView* apps_container_view =
       contents_view_->apps_container_view();
-  apps_container_view->set_folder_top_items_animation_enabled_for_test(false);
 
   const size_t kTotalItems = kMaxFolderItemsPerPage;
   model_->CreateAndPopulateFolderWithApps(kTotalItems);
@@ -854,10 +852,6 @@ TEST_F(AppsGridViewTest, CloseFolderByClickingBackground) {
 }
 
 TEST_F(AppsGridViewTest, PageResetAfterOpenFolder) {
-  // Disable the animation for the folder top items for test purpose.
-  contents_view_->apps_container_view()
-      ->set_folder_top_items_animation_enabled_for_test(false);
-
   const size_t kTotalItems = kMaxFolderPages * kMaxFolderItemsPerPage;
   model_->CreateAndPopulateFolderWithApps(kTotalItems);
   EXPECT_EQ(1u, model_->top_level_item_list()->item_count());
@@ -883,10 +877,6 @@ TEST_F(AppsGridViewTest, PageResetAfterOpenFolder) {
 }
 
 TEST_F(AppsGridViewTest, FolderColsAndRows) {
-  // Disable the animation for the folder top items for test purpose.
-  contents_view_->apps_container_view()
-      ->set_folder_top_items_animation_enabled_for_test(false);
-
   // Populate folders with different number of apps.
   model_->CreateAndPopulateFolderWithApps(2);
   model_->CreateAndPopulateFolderWithApps(5);
