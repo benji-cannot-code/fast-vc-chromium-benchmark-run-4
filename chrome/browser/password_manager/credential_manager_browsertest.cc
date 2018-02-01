8219eb0335480737b10ee7f6265d367529fe949b
Jay Civelli <jcivelli@google.com>
2018-02-01 19:43:57 +0000

    Fixing CredentialManagerBrowserTests tests with the network service.
    Making the CredentialManagerBrowserTests work with the network service.
    They were failing because the certificate verifier was not set up for
    the network service and an interstitial would be shown when navigating
    to an HTTPS page.
    
    Bug: 807739
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I11500770f119c677b4a5b62f929b3e09dafd5047
    Reviewed-on: https://chromium-review.googlesource.com/896869
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Jay Civelli <jcivelli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533778}

--- chrome/browser/password_manager/credential_manager_browsertest.cc
+++ chrome/browser/password_manager/credential_manager_browsertest.cc
@@ -48,6 +48,8 @@ class CredentialManagerBrowserTest : public PasswordManagerBrowserTestBase {
   }
 
   void SetUpCommandLine(base::CommandLine* command_line) override {
+    PasswordManagerBrowserTestBase::SetUpCommandLine(command_line);
+
     // To permit using webauthentication features.
     command_line->AppendSwitch(
         switches::kEnableExperimentalWebPlatformFeatures);
--- chrome/browser/password_manager/password_manager_test_base.h
+++ chrome/browser/password_manager/password_manager_test_base.h
@@ -10,10 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/run_loop.h"
+#include "chrome/browser/ssl/cert_verifier_browser_test.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "components/password_manager/core/browser/password_store_consumer.h"
 #include "content/public/browser/web_contents_observer.h"
-#include "net/cert/mock_cert_verifier.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
 
 namespace autofill {
@@ -128,7 +128,7 @@ class BubbleObserver {
   DISALLOW_COPY_AND_ASSIGN(BubbleObserver);
 };
 
-class PasswordManagerBrowserTestBase : public InProcessBrowserTest {
+class PasswordManagerBrowserTestBase : public CertVerifierBrowserTest {
  public:
   PasswordManagerBrowserTestBase();
   ~PasswordManagerBrowserTestBase() override;
@@ -136,7 +136,6 @@ class PasswordManagerBrowserTestBase : public InProcessBrowserTest {
   // InProcessBrowserTest:
   void SetUpOnMainThread() override;
   void TearDownOnMainThread() override;
-  void SetUpInProcessBrowserTestFixture() override;
   void TearDownInProcessBrowserTestFixture() override;
 
  protected:
@@ -199,15 +198,13 @@ class PasswordManagerBrowserTestBase : public InProcessBrowserTest {
 
   // Accessors
   // Return the first created tab with a custom ManagePasswordsUIController.
-  content::WebContents* WebContents();
-  content::RenderViewHost* RenderViewHost();
-  content::RenderFrameHost* RenderFrameHost();
+  content::WebContents* WebContents() const;
+  content::RenderViewHost* RenderViewHost() const;
+  content::RenderFrameHost* RenderFrameHost() const;
   net::EmbeddedTestServer& https_test_server() { return https_test_server_; }
-  net::MockCertVerifier& mock_cert_verifier() { return mock_cert_verifier_; }
 
  private:
   net::EmbeddedTestServer https_test_server_;
-  net::MockCertVerifier mock_cert_verifier_;
   // A tab with some hooks injected.
   content::WebContents* web_contents_;
   DISALLOW_COPY_AND_ASSIGN(PasswordManagerBrowserTestBase);
