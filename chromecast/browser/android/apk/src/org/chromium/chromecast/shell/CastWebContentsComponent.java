d435a79f540188ee62045e41249f0ec95bad402f
Zhiheng Vincent Li <vincentli@chromium.org>
2018-02-01 04:12:17 +0000

    Update test casts for CastWebContentsComponent
    Bug: b/72722832
    
    Test: cast_shell_junit_tests
    
    Change-Id: Ie1f26c346f7b5c0b95771c091f429b63bc6a49e2
    Reviewed-on: https://chromium-review.googlesource.com/896374
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Commit-Queue: Luke Halliwell <halliwell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533550}

--- chromecast/browser/android/apk/src/org/chromium/chromecast/shell/CastWebContentsComponent.java
+++ chromecast/browser/android/apk/src/org/chromium/chromecast/shell/CastWebContentsComponent.java
@@ -13,6 +13,7 @@ import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.net.Uri;
 import android.os.IBinder;
+import android.os.PatternMatcher;
 import android.support.v4.content.LocalBroadcastManager;
 
 import org.chromium.base.ContextUtils;
@@ -214,7 +215,11 @@ public class CastWebContentsComponent {
         IntentFilter filter = new IntentFilter();
         filter.addAction(ACTION_ACTIVITY_STOPPED);
         filter.addAction(ACTION_KEY_EVENT);
+        filter.addDataScheme(instanceUri.getScheme());
+        filter.addDataAuthority(instanceUri.getAuthority(), null);
+        filter.addDataPath(instanceUri.getPath(), PatternMatcher.PATTERN_LITERAL);
 
+        if (DEBUG) Log.d(TAG, "Registering mReceiver");
         getLocalBroadcastManager().registerReceiver(mReceiver, filter);
 
         mDelegate.start(context, webContents);
@@ -225,6 +230,7 @@ public class CastWebContentsComponent {
         if (DEBUG) Log.d(TAG, "stop");
         if (mStarted) {
             getLocalBroadcastManager().unregisterReceiver(mReceiver);
+            if (DEBUG) Log.d(TAG, "Call delegate to stop");
             mDelegate.stop(context);
             mStarted = false;
         }
