7c19102dcd15eb7489426541f609617ff85a0d1d
David Valleau <valleau@chromium.org>
2018-02-01 20:07:13 +0000

    Adding basic verification to PPDs selected by the user
    When the user selects a file to be used as a PPD for a printer to be
    added, a simple check is made to verify if the selected file looks to be
    a PPD. This does not perform a full validation of the PPD file and is
    just to help notify the user if they've selected a file which is
    obviously wrong.
    
    R=skau@chromium.org, xdai@chromium.org
    
    Bug: 778462
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I794a48c84d176b979e9bfdcf90ee5049f08f4541
    Reviewed-on: https://chromium-review.googlesource.com/887858
    Commit-Queue: David Valleau <valleau@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533792}

--- chromeos/printing/ppd_line_reader.cc
+++ chromeos/printing/ppd_line_reader.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <utility>
 
+#include "base/strings/string_util.h"
 #include "net/base/io_buffer.h"
 #include "net/filter/gzip_header.h"
 #include "net/filter/gzip_source_stream.h"
@@ -16,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chromeos {
 namespace {
+
+constexpr char kPPDMagicNumberString[] = "*PPD-Adobe:";
 
 // Return true if contents has a valid Gzip header.
 bool IsGZipped(const std::string& contents) {
@@ -173,6 +176,16 @@ std::unique_ptr<PpdLineReader> PpdLineReader::Create(
     const std::string& contents,
     int max_line_length) {
   return std::make_unique<PpdLineReaderImpl>(contents, max_line_length);
+}
+
+// static
+bool PpdLineReader::ContainsMagicNumber(const std::string& contents,
+                                        int max_line_length) {
+  auto line_reader = PpdLineReader::Create(contents, max_line_length);
+  std::string line;
+  return line_reader->NextLine(&line) &&
+         base::StartsWith(line, kPPDMagicNumberString,
+                          base::CompareCase::SENSITIVE);
 }
 
 }  // namespace chromeos
