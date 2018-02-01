63dc53a27839683a20bae1cdbdb0a3a089671fd0
Casey Piper <piperc@chromium.org>
2018-02-01 01:25:18 +0000

    Fallback to legacy U2F GetVersion command when needed
    Initial U2F specifications specified a U2F
    version that was not compatible with ISO 7816-4.
    As some authenticators require this older GetVersion
    command, falling back to that initial command
    upon failure with the current GetVersion command.
    
    R=hongjunchoi@chromium.org, jdoerrie@chromium.org
    
    Bug: 805778
    Change-Id: Id7f8a040aae323ba59c1d3c1418d8b5b8bb8953d
    Reviewed-on: https://chromium-review.googlesource.com/894894
    Commit-Queue: Casey Piper <piperc@chromium.org>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Juan Lang <juanlang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533511}

--- device/u2f/u2f_device.h
+++ device/u2f/u2f_device.h
@@ -73,6 +73,7 @@ class U2fDevice {
                       bool success,
                       std::unique_ptr<U2fApduResponse> sign_response);
   void OnVersionComplete(VersionCallback callback,
+                         bool legacy,
                          bool success,
                          std::unique_ptr<U2fApduResponse> version_response);
 
