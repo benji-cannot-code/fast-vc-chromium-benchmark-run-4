38090cb13d87a7795d8e14f0071c331581829d44
Xiaohan Wang <xhwang@chromium.org>
2018-02-01 04:35:13 +0000

    media: Remove Rappor reporting in WebEncryptedMediaClientImpl
    Also remove MediaLog member in WebEncryptedMediaClientImpl since it's
    not needed anymore.
    
    This also fixes the memory leak of RenderMediaLog created.
    
    BUG=807723
    
    Change-Id: Ibe6dfc23964a0890147c2108e4bb4bd387e99f04
    Reviewed-on: https://chromium-review.googlesource.com/896363
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533558}

--- media/blink/webencryptedmediaclient_impl.cc
+++ media/blink/webencryptedmediaclient_impl.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "media/base/key_systems.h"
-#include "media/base/media_log.h"
 #include "media/base/media_permission.h"
 #include "media/blink/webcontentdecryptionmodule_impl.h"
 #include "media/blink/webcontentdecryptionmoduleaccess_impl.h"
@@ -84,11 +83,9 @@ class WebEncryptedMediaClientImpl::Reporter {
 
 WebEncryptedMediaClientImpl::WebEncryptedMediaClientImpl(
     CdmFactory* cdm_factory,
-    MediaPermission* media_permission,
-    MediaLog* media_log)
+    MediaPermission* media_permission)
     : cdm_factory_(cdm_factory),
       key_system_config_selector_(KeySystems::GetInstance(), media_permission),
-      media_log_(media_log),
       weak_factory_(this) {
   DCHECK(cdm_factory_);
 }
@@ -98,11 +95,6 @@ WebEncryptedMediaClientImpl::~WebEncryptedMediaClientImpl() = default;
 void WebEncryptedMediaClientImpl::RequestMediaKeySystemAccess(
     blink::WebEncryptedMediaRequest request) {
   GetReporter(request.KeySystem())->ReportRequested();
-
-  media_log_->RecordRapporWithSecurityOrigin("Media.OriginUrl.EME");
-  if (!request.GetSecurityOrigin().IsPotentiallyTrustworthy()) {
-    media_log_->RecordRapporWithSecurityOrigin("Media.OriginUrl.EME.Insecure");
-  }
 
   key_system_config_selector_.SelectConfig(
       request.KeySystem(), request.SupportedConfigurations(),
