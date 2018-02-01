7092b193e0cf4d360eaee006dc82549dc253fcac
Dominick Ng <dominickn@chromium.org>
2018-02-01 18:35:16 +0000

    Reland "Consolidate app banner testing."
    This is a reland of 58ab87ff414c2799e2546aa20f27d53e6da9fa10.
    
    Original change's description:
    > Consolidate app banner testing.
    > 
    > This CL removes several different app banner testing files, and replaces
    > them by calling the necessary JavaScript setup code in one test file.
    > For instance, all tests will no longer have the "appinstalled" title
    > overwriting, as that will only be active when the appropriate setup
    > JavaScript is run.
    > 
    > This allows future tests to be more easily added, and reduces collisions
    > when manipulating the page title.
    > 
    > BUG=806923
    > 
    > Change-Id: Ia28837b8675a2e1fe329cb1df1be4798ddb03d02
    > Reviewed-on: https://chromium-review.googlesource.com/892182
    > Commit-Queue: Dominick Ng <dominickn@chromium.org>
    > Reviewed-by: Matt Giuca <mgiuca@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#532978}
    
    Bug: 806923
    Change-Id: Ia85a5be0828ad0820a891f8cde9d9e492dc0b831
    Reviewed-on: https://chromium-review.googlesource.com/896326
    Reviewed-by: Matt Giuca <mgiuca@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533745}

--- chrome/android/javatests/src/org/chromium/chrome/browser/webapps/AddToHomescreenManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/webapps/AddToHomescreenManagerTest.java
@@ -33,6 +33,7 @@ import org.chromium.chrome.test.ChromeActivityTestRule;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.util.browser.TabLoadObserver;
 import org.chromium.chrome.test.util.browser.TabTitleObserver;
+import org.chromium.chrome.test.util.browser.WebappTestPage;
 import org.chromium.content.browser.test.util.Criteria;
 import org.chromium.content.browser.test.util.CriteriaHelper;
 import org.chromium.content.common.ContentSwitches;
@@ -88,10 +89,6 @@ public class AddToHomescreenManagerTest {
 
     private static final String MANIFEST_PATH = "/chrome/test/data/banners/manifest_test_page.html";
     private static final String MANIFEST_TITLE = "Web app banner test page";
-
-    private static final String EVENT_WEBAPP_PATH =
-            "/chrome/test/data/banners/appinstalled_test_page.html";
-    private static final String EVENT_WEBAPP_TITLE = "appinstalled event test page";
 
     private static class TestShortcutHelperDelegate extends ShortcutHelper.Delegate {
         public String mRequestedShortcutTitle;
@@ -303,7 +300,9 @@ public class AddToHomescreenManagerTest {
     @SmallTest
     @Feature("{Webapp}")
     public void testAddWebappShortcutAppInstalledEvent() throws Exception {
-        loadUrl(mTestServerRule.getServer().getURL(EVENT_WEBAPP_PATH), EVENT_WEBAPP_TITLE);
+        loadUrl(WebappTestPage.getBannerUrlWithAction(
+                        mTestServerRule.getServer(), "verify_appinstalled"),
+                MANIFEST_TITLE);
         addShortcutToTab(mTab, "", true);
 
         // Wait for the tab title to change. This will happen (due to the JavaScript that runs
