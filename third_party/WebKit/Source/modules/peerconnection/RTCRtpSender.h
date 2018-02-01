2770ffdfdef92a554afe64c97ab4fdb71f29f6d3
Henrik Boström <hbos@chromium.org>
2018-02-01 14:38:12 +0000

    Define legacy getLocalStreams() on top of track-based API.
    getLocalStreams() is redefined from a list of streams modified by
    addStream() and removeStream() to "all streams of all senders".
    When using the stream-based APIs, getLocalStreams() behave exactly
    like it did before this CL. Because senders are added or removed by
    addTrack() and removeTrack(), this CL makes it possible for
    spec-compliant APIs to modify the legacy getLocalStreams(), which was
    not possible before this CL.
    
    In follow-up CLs addStream() and removeStream() will also be implemented
    on top of the track-based APIs and RTCPeerConnectionHandler code will be
    removed.
    
    This CL adds LayoutTests for expected behavior of legacy stream-based
    APIs when fully implemented on top of track-based APIs. This includes
    the first tests for interaction between legacy and non-legacy APIs.
    
    Design doc: https://docs.google.com/document/d/1Obbeg-B4_04twVctHdf7C7vkUQrGlLvOaV0C8J5_-Gs/edit?usp=sharing
    
    Bug: 803021, 738929
    Change-Id: Ibab3f8329809437b6058c3350cd11e747054efd2
    Reviewed-on: https://chromium-review.googlesource.com/893385
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Commit-Queue: Henrik Boström <hbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533656}

--- third_party/WebKit/Source/modules/peerconnection/RTCRtpSender.h
+++ third_party/WebKit/Source/modules/peerconnection/RTCRtpSender.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define RTCRtpSender_h
 
 #include "bindings/core/v8/ScriptPromise.h"
+#include "modules/mediastream/MediaStream.h"
 #include "platform/bindings/ScriptWrappable.h"
 #include "platform/heap/GarbageCollected.h"
 #include "platform/heap/Member.h"
@@ -28,7 +29,8 @@ class RTCRtpSender final : public ScriptWrappable {
   // https://github.com/w3c/webrtc-pc/issues/1712
   RTCRtpSender(RTCPeerConnection*,
                std::unique_ptr<WebRTCRtpSender>,
-               MediaStreamTrack*);
+               MediaStreamTrack*,
+               MediaStreamVector streams);
 
   MediaStreamTrack* track();
   ScriptPromise replaceTrack(ScriptState*, MediaStreamTrack*);
@@ -37,6 +39,7 @@ class RTCRtpSender final : public ScriptWrappable {
   // Sets the track. This must be called when the |WebRTCRtpSender| has its
   // track updated, and the |track| must match the |WebRTCRtpSender::Track|.
   void SetTrack(MediaStreamTrack*);
+  MediaStreamVector streams() const;
 
   virtual void Trace(blink::Visitor*);
 
@@ -44,6 +47,7 @@ class RTCRtpSender final : public ScriptWrappable {
   Member<RTCPeerConnection> pc_;
   std::unique_ptr<WebRTCRtpSender> sender_;
   Member<MediaStreamTrack> track_;
+  MediaStreamVector streams_;
 };
 
 }  // namespace blink
