6e472153478ce47578958f351f2aeb34f41a2a20
Tim Dresser <tdresser@chromium.org>
2018-02-02 22:43:21 +0000

    First Input Delay: include swipes, add UKM.
    Previously we excluded swiping input. We now include swipes (map panning,
    carousel rotation), but still exclude scroll.
    
    Also records a UKM.
    
    Bug: 803935
    Change-Id: I8f9d5a77ad61221eb9b614e8d4e11e2b757af9e2
    Reviewed-on: https://chromium-review.googlesource.com/894826
    Commit-Queue: Timothy Dresser <tdresser@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Bryan McQuade <bmcquade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534178}

--- chrome/browser/page_load_metrics/observers/ukm_page_load_metrics_observer.cc
+++ chrome/browser/page_load_metrics/observers/ukm_page_load_metrics_observer.cc
@@ -33,6 +33,7 @@ const char kUkmFirstContentfulPaintName[] =
 const char kUkmFirstMeaningfulPaintName[] =
     "Experimental.PaintTiming.NavigationToFirstMeaningfulPaint";
 const char kUkmInteractiveName[] = "Experimental.NavigationToInteractive";
+const char kUkmFirstInputDelayName[] = "InteractiveTiming.FirstInputDelay";
 const char kUkmForegroundDurationName[] = "PageTiming.ForegroundDuration";
 const char kUkmFailedProvisionaLoadName[] =
     "PageTiming.NavigationToFailedProvisionalLoad";
@@ -204,6 +205,12 @@ void UkmPageLoadMetricsObserver::RecordTimingMetrics(
       builder.SetExperimental_NavigationToInteractive(
           time_to_interactive.InMilliseconds());
     }
+  }
+  if (timing.interactive_timing->first_input_delay) {
+    base::TimeDelta first_input_delay =
+        timing.interactive_timing->first_input_delay.value();
+    builder.SetInteractiveTiming_FirstInputDelay(
+        first_input_delay.InMilliseconds());
   }
 
   // Use a bucket spacing factor of 1.3 for bytes.
--- third_party/WebKit/Source/core/frame/LocalFrame.h
+++ third_party/WebKit/Source/core/frame/LocalFrame.h
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/frame/Frame.h"
 #include "core/frame/LocalFrameView.h"
 #include "core/loader/FrameLoader.h"
+#include "core/loader/InteractiveDetector.h"
 #include "core/page/FrameTree.h"
 #include "platform/Supplementable.h"
 #include "platform/heap/Handle.h"
