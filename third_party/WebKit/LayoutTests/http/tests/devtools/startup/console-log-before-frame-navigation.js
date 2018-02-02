cc3bf40fb2e1a6c11503f316bf52556a0ace40ac
Dmitry Gozman <dgozman@chromium.org>
2018-02-02 00:22:30 +0000

    Fix console-log-before-frame-navigation.js
    Bug: none
    Change-Id: Ibd8f22a4f484ca375043576d46af521b8ae5bfc1
    Reviewed-on: https://chromium-review.googlesource.com/898186
    Reviewed-by: Aleksey Kozyatinskiy <kozyatinskiy@chromium.org>
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533867}

--- third_party/WebKit/LayoutTests/http/tests/devtools/startup/console-log-before-frame-navigation.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/startup/console-log-before-frame-navigation.js
@@ -4,13 +4,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 (async function() {
+  await TestRunner.setupStartupTest('resources/console-log-before-frame-navigation.html');
   TestRunner.addResult(
       `Tests that Web Inspector won't crash if there are messages written to console from a frame which has already navigated to a page from a different domain.\n`);
+  await TestRunner.loadModule('console_test_runner');
+  var waitFor = 7 - ConsoleModel.consoleModel.messages().length;
+  if (waitFor > 0)
+    await ConsoleTestRunner.waitUntilNthMessageReceivedPromise(waitFor);
   var messages = ConsoleModel.consoleModel.messages();
   TestRunner.addResult('Received console messages:');
   for (var i = 0; i < messages.length; ++i) {
     var m = messages[i];
-    TestRunner.addResult('Message[' + i + ']:');
     TestRunner.addResult('Message: ' + Bindings.displayNameForURL(m.url) + ':' + m.line + ' ' + m.messageText);
   }
   TestRunner.addResult('TEST COMPLETE.');
