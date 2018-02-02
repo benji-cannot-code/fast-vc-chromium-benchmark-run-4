0319132f36eb06dd4efb645ec79f6f2a1d52c2d2
Morten Stenshorne <mstensho@chromium.org>
2018-02-02 21:32:30 +0000

    New logic for maximum number of fragments.
    Instead of unconditionally clamping at 500 columns, try to identify legitimate
    use cases for many columns, and allow those. At the same time, limit (what we
    consider to be) illegitimate reasons even more brutally.
    
    Bug: 808189
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Icf70d655c2da0b4a1af7e256cf7f3ef8c72f8715
    Reviewed-on: https://chromium-review.googlesource.com/898438
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534158}

--- third_party/WebKit/Source/core/layout/MultiColumnFragmentainerGroup.h
+++ third_party/WebKit/Source/core/layout/MultiColumnFragmentainerGroup.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MultiColumnFragmentainerGroup_h
 #define MultiColumnFragmentainerGroup_h
 
+#include "core/CoreExport.h"
 #include "core/layout/LayoutMultiColumnFlowThread.h"
 #include "platform/wtf/Allocator.h"
 
@@ -30,7 +31,7 @@ namespace blink {
 // or they'd overflow the outer fragmentainer in the inline direction. If we
 // need more columns than what a group has room for, we'll create another group
 // and put them there (and make them appear in the next outer fragmentainer).
-class MultiColumnFragmentainerGroup {
+class CORE_EXPORT MultiColumnFragmentainerGroup {
   DISALLOW_NEW_EXCEPT_PLACEMENT_NEW();
 
  public:
@@ -161,6 +162,13 @@ class MultiColumnFragmentainerGroup {
 
   unsigned ColumnIndexAtOffset(LayoutUnit offset_in_flow_thread,
                                LayoutBox::PageBoundaryRule) const;
+
+  // Like ColumnIndexAtOffset(), but with the return value clamped to actual
+  // column count. While there are legitimate reasons for dealing with columns
+  // out of bounds during layout, this should not happen when performing read
+  // operations on the tree (like painting and hit-testing).
+  unsigned ConstrainedColumnIndexAtOffset(LayoutUnit offset_in_flow_thread,
+                                          LayoutBox::PageBoundaryRule) const;
 
   // The "CSS actual" value of column-count. This includes overflowing columns,
   // if any.
