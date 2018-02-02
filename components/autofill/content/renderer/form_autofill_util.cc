9200f7936c68cf8f3264a8f67106f3741cdc852b
Parastoo Geranmayeh <parastoog@google.com>
2018-02-02 00:31:30 +0000

    [Autofill] Handle placeholder format changes.
    Sometimes websites change the format of the placeholders. This
    makes us detect the second one as a value entered by the user,
    and then we miss the field.
    
    Solution:
    
    While filling the form in the renderer, compare the placeholder value
    with the element value case-insensitively.
    
    Fixes:
    www.cargurus.com, etc.
    
    Bug: 807747
    Change-Id: Id97a2b9978818ed22dacfd9ee427886562bf6f41
    Reviewed-on: https://chromium-review.googlesource.com/898072
    Commit-Queue: Parastoo Geranmayeh <parastoog@google.com>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533870}

--- components/autofill/content/renderer/form_autofill_util.cc
+++ components/autofill/content/renderer/form_autofill_util.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/command_line.h"
+#include "base/i18n/case_conversion.h"
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
 #include "base/stl_util.h"
@@ -808,7 +809,8 @@ void ForEachMatchingFormFieldCommon(
         (!element->HasAttribute(kValue) ||
          element->GetAttribute(kValue) != element->Value()) &&
         (!element->HasAttribute(kPlaceholder) ||
-         element->GetAttribute(kPlaceholder) != element->Value()))
+         base::i18n::ToLower(element->GetAttribute(kPlaceholder).Utf16()) !=
+             base::i18n::ToLower(element->Value().Utf16())))
       continue;
 
     DCHECK(!g_prevent_layout || !(filters & FILTER_NON_FOCUSABLE_ELEMENTS))
