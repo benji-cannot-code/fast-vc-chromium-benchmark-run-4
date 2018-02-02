0b1a35d629d1205894019e7d2ff63812421e7fde
Sidney San Martín <sdy@chromium.org>
2018-02-02 20:32:40 +0000

    Fix a hang on 10.11 caused by -[MDHoverButton layout] making itself need layout.
    Updating self.image (on an NSButton subclass) in -layout or -viewWillDraw both
    seem to lead to an infinite loop in 10.11. I'm not sure if there's a better way
    to fix this; using different triggers to update the image seems safest.
    
    Bug: 805109
    Change-Id: I468576069f44eae4a2f4b9058e078d88d157265a
    Reviewed-on: https://chromium-review.googlesource.com/899586
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Sidney San Martín <sdy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534137}

--- chrome/browser/ui/cocoa/md_hover_button.h
+++ chrome/browser/ui/cocoa/md_hover_button.h
@@ -8,13 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <AppKit/AppKit.h>
 
+#import "chrome/browser/ui/cocoa/themed_window.h"
 #import "ui/base/cocoa/hover_button.h"
 #include "ui/gfx/vector_icon_types.h"
 
 // MDHoverButton has a gray background with rounded corners. The background is
 // only visible on hover and gets darker on click. It's friendly to subviews.
 
-@interface MDHoverButton : HoverButton
+@interface MDHoverButton : HoverButton<ThemedWindowDrawing>
 
 // An icon that's displayed in the middle of the button.
 @property(nonatomic) const gfx::VectorIcon* icon;
