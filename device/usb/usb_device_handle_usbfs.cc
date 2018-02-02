5ef19685bab0e1fb842948c5be0b7ea373dce8f9
Reilly Grant <reillyg@chromium.org>
2018-02-02 18:11:48 +0000

    Intepret EOVERFLOW as babble in Linux USB backend
    This adds a missing case to ConvertTransferResult which maps the
    low-level error code EOVERFLOW to UsbTransferStatus::BABBLE, indicating
    that the device sent more data than was expected.
    
    Bug: 808181
    Change-Id: I697171615153e6de53d751f216fc3a2c6287efd5
    Reviewed-on: https://chromium-review.googlesource.com/898323
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534097}

--- device/usb/usb_device_handle_usbfs.cc
+++ device/usb/usb_device_handle_usbfs.cc
@@ -112,6 +112,8 @@ UsbTransferStatus ConvertTransferResult(int rc) {
   switch (rc) {
     case 0:
       return UsbTransferStatus::COMPLETED;
+    case EOVERFLOW:
+      return UsbTransferStatus::BABBLE;
     case EPIPE:
       return UsbTransferStatus::STALLED;
     default:
