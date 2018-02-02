957adade77b71593c601c70a5c8e65d61f6bdd23
Nico Weber <thakis@chromium.org>
2018-02-02 19:28:43 +0000

    Enable -Wimplicit-fallthrough on Android.
    Fixes a bug where vr_shell counted VRMode::VR_BROWSER also as
    VRMode::WEBVR and VRMode::VR_FULLSCREEN, and VRMode::WEBVR also
    as VRMode::VR_FULLSCREEN in rappor.
    
    Bug: 177475
    Change-Id: I23a0c2b5fb94d34d407070ef0a9378d02fe4ba54
    Reviewed-on: https://chromium-review.googlesource.com/899722
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534124}

--- chrome/browser/android/browsing_data/browsing_data_bridge.cc
+++ chrome/browser/android/browsing_data/browsing_data_bridge.cc
@@ -113,6 +113,7 @@ static void JNI_BrowsingDataBridge_ClearBrowsingData(
       case browsing_data::BrowsingDataType::HOSTED_APPS_DATA:
         // Only implemented on Desktop.
         NOTREACHED();
+        FALLTHROUGH;
       case browsing_data::BrowsingDataType::NUM_TYPES:
         NOTREACHED();
     }
--- content/browser/renderer_host/render_widget_host_view_android.cc
+++ content/browser/renderer_host/render_widget_host_view_android.cc
@@ -2398,6 +2398,7 @@ void RenderWidgetHostViewAndroid::ComputeEventLatencyOSTouchHistograms(
     case ui::MotionEvent::ACTION_POINTER_UP:
       UMA_HISTOGRAM_CUSTOM_COUNTS("Event.Latency.OS.TOUCH_RELEASED",
                                   delta.InMicroseconds(), 1, 1000000, 50);
+      return;
     default:
       return;
   }
