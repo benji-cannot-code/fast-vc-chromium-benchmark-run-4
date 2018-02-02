d5835ac341a9cb5e55ceb7a6b7826bbda2ca6fdb
Alex Clarke <alexclarke@chromium.org>
2018-02-02 16:33:21 +0000

    Refactor away TaskQueueManager::DoWork in favor of the Sequence interface
    This patch changes the Sequence interface slightly by introducing
    DelayTillNextTask which helps work around problems scheduling delayed
    tasks when TakeTask doesn't have a task to return (due to cancellation).
    
    Change-Id: I5935e41ad5bc725b6109a187b329c82ce5a70689
    Bug: 783309
    Reviewed-on: https://chromium-review.googlesource.com/883346
    Commit-Queue: Alex Clarke <alexclarke@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534070}

--- third_party/WebKit/Source/platform/scheduler/base/task_queue_selector.h
+++ third_party/WebKit/Source/platform/scheduler/base/task_queue_selector.h
@@ -82,7 +82,11 @@ class PLATFORM_EXPORT TaskQueueSelector {
 
   // Returns true if all the enabled work queues are empty. Returns false
   // otherwise.
-  bool EnabledWorkQueuesEmpty() const;
+  bool AllEnabledWorkQueuesAreEmpty() const;
+
+  // No enabled queue could be selected, check if we could have chosen a
+  // disabled (blocked) work queue instead.
+  void TrySelectingBlockedQueue();
 
  protected:
   class PLATFORM_EXPORT PrioritizingSelector {
@@ -171,9 +175,6 @@ class PLATFORM_EXPORT TaskQueueSelector {
   void DidSelectQueueWithPriority(TaskQueue::QueuePriority priority,
                                   bool chose_delayed_over_immediate);
 
-  // No enabled queue could be selected, check if we could have chosen a
-  // disabled (blocked) work queue instead.
-  void TrySelectingBlockedQueue();
 
   // Check if we could have chosen a disabled (blocked) work queue instead.
   // |chosen_enabled_queue| is the enabled queue that got chosen.
