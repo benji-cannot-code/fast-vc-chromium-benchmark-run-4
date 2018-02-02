46ac9893fbbeec61630553098940149d6fbd02d1
Dominic Battre <battre@chromium.org>
2018-02-02 13:15:47 +0000

    More robust capturing <form>s that are taken out of the DOM.
    Chrome's password manager tries to capture events where <form> elements
    are taken out of the DOM or made invisible. This is used to detect
    form submissions. This trigger failed in the past if a parent element
    for the form was removed from the DOM or made invisible (as opposed
    to the form it self). This CL makes the trigger logic more robust.
    
    Bug: 760579, 710431
    Change-Id: Idbc72d7ab57d9e6c61b0907ac5c9b3f96346d4a5
    Reviewed-on: https://chromium-review.googlesource.com/892882
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534037}

--- third_party/WebKit/Source/core/exported/WebFormElementObserverImpl.cpp
+++ third_party/WebKit/Source/core/exported/WebFormElementObserverImpl.cpp
@@ -35,6 +35,7 @@ class WebFormElementObserverImpl::ObserverCallback
 
  private:
   Member<HTMLElement> element_;
+  HeapHashSet<Member<Node>> parents_;
   Member<MutationObserver> mutation_observer_;
   std::unique_ptr<WebFormElementObserverCallback> callback_;
 };
@@ -51,11 +52,15 @@ WebFormElementObserverImpl::ObserverCallback::ObserverCallback(
     init.setAttributeFilter({"action", "class", "style"});
     mutation_observer_->observe(element_, init, ASSERT_NO_EXCEPTION);
   }
-  if (element_->parentElement()) {
+  for (Node* node = element_; node->parentElement();
+       node = node->parentElement()) {
     MutationObserverInit init;
     init.setChildList(true);
-    mutation_observer_->observe(element_->parentElement(), init,
+    init.setAttributes(true);
+    init.setAttributeFilter({"class", "style"});
+    mutation_observer_->observe(node->parentElement(), init,
                                 ASSERT_NO_EXCEPTION);
+    parents_.insert(node->parentElement());
   }
 }
 
@@ -70,8 +75,11 @@ void WebFormElementObserverImpl::ObserverCallback::Deliver(
   for (const auto& record : records) {
     if (record->type() == "childList") {
       for (unsigned i = 0; i < record->removedNodes()->length(); ++i) {
-        if (record->removedNodes()->item(i) != element_)
+        Node* removed_node = record->removedNodes()->item(i);
+        if (removed_node != element_ &&
+            parents_.find(removed_node) == parents_.end()) {
           continue;
+        }
         callback_->ElementWasHiddenOrRemoved();
         Disconnect();
         return;
@@ -101,11 +109,14 @@ void WebFormElementObserverImpl::ObserverCallback::Deliver(
 void WebFormElementObserverImpl::ObserverCallback::Disconnect() {
   mutation_observer_->disconnect();
   callback_.reset();
+  parents_.clear();
+  element_.Clear();
 }
 
 void WebFormElementObserverImpl::ObserverCallback::Trace(
     blink::Visitor* visitor) {
   visitor->Trace(element_);
+  visitor->Trace(parents_);
   visitor->Trace(mutation_observer_);
   MutationObserver::Delegate::Trace(visitor);
 }
