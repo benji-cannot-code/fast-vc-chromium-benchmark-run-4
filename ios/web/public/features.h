4d247b8d0b45de62f12bdab138d68babf2a56e04
Mike Dougherty <michaeldo@chromium.org>
2018-02-02 07:10:35 +0000

    Add kContextMenuElementPostMessage feature flag.
    This feature flag will be used for adding Context Menu support to
    iFrames by using the PostMessage WebKit API to return the found element.
    (Instead of directly returning the element details from
    __gCrWeb.getElementFromPoint)
    
    Bug: 228355
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I17fec9afb74e7fe5f7b2818902f4b872ba8d445a
    Reviewed-on: https://chromium-review.googlesource.com/896546
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533983}

--- ios/web/public/features.h
+++ ios/web/public/features.h
@@ -11,6 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace web {
 namespace features {
 
+// Used to enable asynchronous DOM element fetching for context menu.
+extern const base::Feature kContextMenuElementPostMessage;
+
 // Used to enable the WKBackForwardList based navigation manager.
 extern const base::Feature kSlimNavigationManager;
 
