f0d022e3499917a930325ea266aeec1c1fb3c660
Xing Liu <xingliu@chromium.org>
2018-02-02 01:47:46 +0000

    Parallel Download: Make all parallel request half open range request.
    This CL does the following:
    
    1. Make all parallel download requests to be able to fetch the whole
    content. Range header will be "Range:%offset-" where %offset is the
    starting range.
    
    2. Fix recover bug where failed response of 2nd or 3rd request, e.g.
    http 404, will make download never complete nor interrupted.
    
    3. Fix a bug that when input stream can't be inserted into download
    file or download file is completed on the file thread, the http
    connection is left open. This is a by product of new browser tests.
    
    4. Add more browser tests to cover edge cases of server failures, and
    fix minor bug in TestDownloadHttpResponse where it sent one less byte.
    
    Bug: 804000
    Change-Id: If9545409379d5a33f758d05c62d6b6890ed896df
    Reviewed-on: https://chromium-review.googlesource.com/892089
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533909}

--- content/browser/download/parallel_download_utils.h
+++ content/browser/download/parallel_download_utils.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "content/browser/download/download_create_info.h"
+#include "content/browser/download/download_file_impl.h"
 #include "content/common/content_export.h"
 #include "content/public/browser/download_item.h"
 
@@ -59,6 +60,12 @@ CONTENT_EXPORT std::vector<DownloadItem::ReceivedSlice> FindSlicesToDownload(
 CONTENT_EXPORT size_t AddOrMergeReceivedSliceIntoSortedArray(
     const DownloadItem::ReceivedSlice& new_slice,
     std::vector<DownloadItem::ReceivedSlice>& received_slices);
+
+// Returns if a preceding stream can still download the part of content that
+// was arranged to |error_stream|.
+CONTENT_EXPORT bool CanRecoverFromError(
+    const DownloadFileImpl::SourceStream* error_stream,
+    const DownloadFileImpl::SourceStream* preceding_neighbor);
 
 // Finch configuration utilities.
 //
