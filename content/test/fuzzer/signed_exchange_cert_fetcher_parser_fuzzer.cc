4817f043053a1dca1d7c763efe0975a6bf98184b
Tsuyoshi Horo <horo@chromium.org>
2018-02-02 10:37:12 +0000

    Implement the parser for TLS 1.3 Certificate message for signed HTTP exchange.
    This CL adds the following data files for fuzzer:
      1: Same as SignedExchangeCertFetcherTest.OneCert
      2: Same as SignedExchangeCertFetcherTest.OneCertWithExtension
      3: Same as SignedExchangeCertFetcherTest.TwoCerts
      4: The actual cert of www.example.com and the intermediate cert.
    
    Bug: 803774
    Change-Id: I55182556a39c1ed852f2412df18707eae7a7c2c2
    Reviewed-on: https://chromium-review.googlesource.com/896770
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534016}

---
+++ content/test/fuzzer/signed_exchange_cert_fetcher_parser_fuzzer.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/loader/signed_exchange_cert_fetcher.h"  // nogncheck
+
+#include "base/strings/string_piece.h"
+
+namespace content {
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  base::StringPiece input(reinterpret_cast<const char*>(data), size);
+  SignedExchangeCertFetcher::GetCertChainFromMessage(input);
+  return 0;
+}
+
+}  // namespace content
