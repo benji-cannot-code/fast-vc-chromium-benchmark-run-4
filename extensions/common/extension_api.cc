3a4c6a538bf786973d1c8b3b895638815be9770f
Jens Widell <jl@opera.com>
2018-02-02 08:41:43 +0000

    Rename conflicting `Static` classes/structs
    They conflict in jumbo/unity builds.
    
    Also rename the overly generic `g_static`/`g_lazy_instance` variables.
    They did not conflict with anything currently, but generic names in the
    anonymous namespace are likely to become a problem eventually, sadly.
    
    Change-Id: Ie90ca3c7eee3d357c3aca205a5551034b768eb77
    Reviewed-on: https://chromium-review.googlesource.com/897488
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Jens Widell <jl@opera.com>
    Cr-Commit-Position: refs/heads/master@{#533993}

--- extensions/common/extension_api.cc
+++ extensions/common/extension_api.cc
@@ -88,16 +88,15 @@ const base::DictionaryValue* GetSchemaChild(
   return NULL;
 }
 
-struct Static {
-  Static()
-      : api(ExtensionAPI::CreateWithDefaultConfiguration()) {
-  }
+struct ExtensionAPIStatic {
+  ExtensionAPIStatic() : api(ExtensionAPI::CreateWithDefaultConfiguration()) {}
   std::unique_ptr<ExtensionAPI> api;
 };
 
-base::LazyInstance<Static>::Leaky g_lazy_instance = LAZY_INSTANCE_INITIALIZER;
+base::LazyInstance<ExtensionAPIStatic>::Leaky g_extension_api_static =
+    LAZY_INSTANCE_INITIALIZER;
 
-// May override |g_lazy_instance| for a test.
+// May override |g_extension_api_static| for a test.
 ExtensionAPI* g_shared_instance_for_test = NULL;
 
 }  // namespace
@@ -105,7 +104,7 @@ ExtensionAPI* g_shared_instance_for_test = NULL;
 // static
 ExtensionAPI* ExtensionAPI::GetSharedInstance() {
   return g_shared_instance_for_test ? g_shared_instance_for_test
-                                    : g_lazy_instance.Get().api.get();
+                                    : g_extension_api_static.Get().api.get();
 }
 
 // static
--- extensions/common/features/feature_provider.cc
+++ extensions/common/features/feature_provider.cc
@@ -24,10 +24,11 @@ namespace extensions {
 
 namespace {
 
-class Static {
+class FeatureProviderStatic {
  public:
-  Static() {
-    TRACE_EVENT0("startup", "extensions::FeatureProvider::Static");
+  FeatureProviderStatic() {
+    TRACE_EVENT0("startup",
+                 "extensions::FeatureProvider::FeatureProviderStatic");
     base::Time begin_time = base::Time::Now();
 
     ExtensionsClient* client = ExtensionsClient::Get();
@@ -62,10 +63,11 @@ class Static {
  private:
   std::map<std::string, std::unique_ptr<FeatureProvider>> feature_providers_;
 
-  DISALLOW_COPY_AND_ASSIGN(Static);
+  DISALLOW_COPY_AND_ASSIGN(FeatureProviderStatic);
 };
 
-base::LazyInstance<Static>::Leaky g_static = LAZY_INSTANCE_INITIALIZER;
+base::LazyInstance<FeatureProviderStatic>::Leaky g_feature_provider_static =
+    LAZY_INSTANCE_INITIALIZER;
 
 const Feature* GetFeatureFromProviderByName(const std::string& provider_name,
                                             const std::string& feature_name) {
@@ -85,7 +87,7 @@ FeatureProvider::~FeatureProvider() {}
 
 // static
 const FeatureProvider* FeatureProvider::GetByName(const std::string& name) {
-  return g_static.Get().GetFeatures(name);
+  return g_feature_provider_static.Get().GetFeatures(name);
 }
 
 // static
