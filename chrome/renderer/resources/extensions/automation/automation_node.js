201d1652f614fcd527a8683db970c418877c2ea4
Chris Pickel <sfiera@chromium.org>
2018-02-02 11:27:20 +0000

    Revert "Expose reverse relationships in the automation API."
    This reverts commit a55fe78f801dbf3c10f34a665bb9673393f9e494.
    
    Reason for revert: Possible source of flakiness in AutomationApiTest.DesktopHitTestIframe
    
    Original change's description:
    > Expose reverse relationships in the automation API.
    > 
    > AXTree now keeps track of reverse relationships automatically,
    > so take advantage of that to expose reverse relationships for
    > some attributes already exposed in the automation API.
    > Also add two relation attributes that are supported by Blink
    > but weren't added to automation yet: details, errorMessage.
    > 
    > The new attributes are:
    > 
    > detailsFor (reverse of details)
    > errorMessageFor (reverse of errorMessage)
    > controlledBy (reverse of controls)
    > descriptionFor (reverse of describedBy)
    > flowFrom (reverse of flowTo)
    > labelFor (reverse of labelledBy)
    > 
    > Bug: 757707
    > Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    > Change-Id: I112c283c87274388fc9e7e725bcc126393133e39
    > TBR: michaelpg@chromium.org
    > Reviewed-on: https://chromium-review.googlesource.com/837643
    > Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    > Reviewed-by: David Tseng <dtseng@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#533819}
    
    TBR=dmazzoni@chromium.org,dtseng@chromium.org,michaelpg@chromium.org
    
    Bug: 757707
    Bug: 808343
    Change-Id: Ie61c369e0f234eca7435ac80d056a6c10ea75a41
    Reviewed-on: https://chromium-review.googlesource.com/897864
    Reviewed-by: Chris Pickel <sfiera@chromium.org>
    Commit-Queue: Chris Pickel <sfiera@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534020}

--- chrome/renderer/resources/extensions/automation/automation_node.js
+++ chrome/renderer/resources/extensions/automation/automation_node.js
@@ -228,16 +228,6 @@ var GetIntAttribute = natives.GetIntAttribute;
  * @param {number} axTreeID The id of the accessibility tree.
  * @param {number} nodeID The id of a node.
  * @param {string} attr The name of an attribute.
- * @return {?Array.<number>} The ids of nodes who have a relationship pointing
- *     to |nodeID| (a reverse relationship).
- */
-var GetIntAttributeReverseRelations =
-    natives.GetIntAttributeReverseRelations;
-
-/**
- * @param {number} axTreeID The id of the accessibility tree.
- * @param {number} nodeID The id of a node.
- * @param {string} attr The name of an attribute.
  * @return {?number} The value of this attribute, or undefined if the tree,
  *     node, or attribute wasn't found.
  */
@@ -252,16 +242,6 @@ var GetFloatAttribute = natives.GetFloatAttribute;
  */
 var GetIntListAttribute =
     natives.GetIntListAttribute;
-
-/**
- * @param {number} axTreeID The id of the accessibility tree.
- * @param {number} nodeID The id of a node.
- * @param {string} attr The name of an attribute.
- * @return {?Array.<number>} The ids of nodes who have a relationship pointing
- *     to |nodeID| (a reverse relationship).
- */
-var GetIntListAttributeReverseRelations =
-    natives.GetIntListAttributeReverseRelations;
 
 /**
  * @param {number} axTreeID The id of the accessibility tree.
@@ -905,19 +885,16 @@ var intAttributes = [
     'textSelEnd',
     'textSelStart'];
 
-// Int attribute, relation property to expose, reverse relation to expose.
 var nodeRefAttributes = [
-    ['activedescendantId', 'activeDescendant', null],
-    ['detailsId', 'details', 'detailsFor'],
-    ['errorMessageId', 'errorMessage', 'errorMessageFor'],
-    ['inPageLinkTargetId', 'inPageLinkTarget', null],
-    ['nextFocusId', 'nextFocus', null],
-    ['nextOnLineId', 'nextOnLine', null],
-    ['previousFocusId', 'previousFocus', null],
-    ['previousOnLineId', 'previousOnLine', null],
-    ['tableColumnHeaderId', 'tableColumnHeader', null],
-    ['tableHeaderId', 'tableHeader', null],
-    ['tableRowHeaderId', 'tableRowHeader', null]];
+    ['activedescendantId', 'activeDescendant'],
+    ['inPageLinkTargetId', 'inPageLinkTarget'],
+    ['nextFocusId', 'nextFocus'],
+    ['nextOnLineId', 'nextOnLine'],
+    ['previousFocusId', 'previousFocus'],
+    ['previousOnLineId', 'previousOnLine'],
+    ['tableColumnHeaderId', 'tableColumnHeader'],
+    ['tableHeaderId', 'tableHeader'],
+    ['tableRowHeaderId', 'tableRowHeader']];
 
 var intListAttributes = [
     'lineBreaks',
@@ -927,12 +904,11 @@ var intListAttributes = [
     'wordEnds',
     'wordStarts'];
 
-// Intlist attribute, relation property to expose, reverse relation to expose.
 var nodeRefListAttributes = [
-    ['controlsIds', 'controls', 'controlledBy'],
-    ['describedbyIds', 'describedBy', 'descriptionFor'],
-    ['flowtoIds', 'flowTo', 'flowFrom'],
-    ['labelledbyIds', 'labelledBy', 'labelFor']];
+    ['controlsIds', 'controls'],
+    ['describedbyIds', 'describedBy'],
+    ['flowtoIds', 'flowTo'],
+    ['labelledbyIds', 'labelledBy']];
 
 var floatAttributes = [
     'valueForRange',
@@ -977,7 +953,6 @@ $Array.forEach(intAttributes, function(attributeName) {
 $Array.forEach(nodeRefAttributes, function(params) {
   var srcAttributeName = params[0];
   var dstAttributeName = params[1];
-  var dstReverseAttributeName = params[2];
   $Array.push(publicAttributes, dstAttributeName);
   $Object.defineProperty(AutomationNodeImpl.prototype, dstAttributeName, {
     __proto__: null,
@@ -989,26 +964,6 @@ $Array.forEach(nodeRefAttributes, function(params) {
         return undefined;
     }
   });
-  if (dstReverseAttributeName) {
-    $Array.push(publicAttributes, dstReverseAttributeName);
-    $Object.defineProperty(AutomationNodeImpl.prototype,
-                           dstReverseAttributeName, {
-      __proto__: null,
-      get: function() {
-        var ids = GetIntAttributeReverseRelations(
-            this.treeID, this.id, srcAttributeName);
-        if (!ids || !this.rootImpl)
-          return undefined;
-        var result = [];
-        for (var i = 0; i < ids.length; ++i) {
-          var node = this.rootImpl.get(ids[i]);
-          if (node)
-          $Array.push(result, node);
-        }
-        return result;
-      }
-    });
-  }
 });
 
 $Array.forEach(intListAttributes, function(attributeName) {
@@ -1024,7 +979,6 @@ $Array.forEach(intListAttributes, function(attributeName) {
 $Array.forEach(nodeRefListAttributes, function(params) {
   var srcAttributeName = params[0];
   var dstAttributeName = params[1];
-  var dstReverseAttributeName = params[2];
   $Array.push(publicAttributes, dstAttributeName);
   $Object.defineProperty(AutomationNodeImpl.prototype, dstAttributeName, {
     __proto__: null,
@@ -1041,26 +995,6 @@ $Array.forEach(nodeRefListAttributes, function(params) {
       return result;
     }
   });
-  if (dstReverseAttributeName) {
-    $Array.push(publicAttributes, dstReverseAttributeName);
-    $Object.defineProperty(AutomationNodeImpl.prototype,
-                           dstReverseAttributeName, {
-      __proto__: null,
-      get: function() {
-        var ids = GetIntListAttributeReverseRelations(
-            this.treeID, this.id, srcAttributeName);
-        if (!ids || !this.rootImpl)
-          return undefined;
-        var result = [];
-        for (var i = 0; i < ids.length; ++i) {
-          var node = this.rootImpl.get(ids[i]);
-          if (node)
-          $Array.push(result, node);
-        }
-        return result;
-      }
-    });
-  }
 });
 
 $Array.forEach(floatAttributes, function(attributeName) {
