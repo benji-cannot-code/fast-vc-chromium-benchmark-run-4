1930d43917e9a5ebd24c3090cf2623ff93f83a35
Sadrul Habib Chowdhury <sadrul@chromium.org>
2018-02-03 01:43:18 +0000

    sampling profiler: Set up startup profiler for renderer.
    Set up a startup sampling-profiler for the compositor thread in the
    renderer process. Notable changes:
    . Callback to the RendererClient when the compositor-thread is created.
      The client uses this callback to set-up the sampling profiler.
    . Allow the renderer processes to connect to the browser to report the
      profiler data over mojom. Add the --start-stack-profiler flag to the
      renderer's cmd-line so that it can initiate sampling profiler. Select
      every fifth renderer for collecting the profile data from, so that we
      can still get enough useful data, but don't overburden bandwidth or
      memory usage.
    
    BUG=788808
    
    Change-Id: I478184502a17f4dbb83f03a67a7ef482c92c0a78
    Reviewed-on: https://chromium-review.googlesource.com/894848
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534235}

--- chrome/common/stack_sampling_configuration.cc
+++ chrome/common/stack_sampling_configuration.cc
@@ -48,6 +48,11 @@ bool IsBrowserProcess() {
   return process_type.empty();
 }
 
+bool ShouldEnableProfilerForNextRendererProcess() {
+  // Enable for every N-th renderer process, where N = 5.
+  return base::RandInt(0, 4) == 0;
+}
+
 }  // namespace
 
 StackSamplingConfiguration::StackSamplingConfiguration()
@@ -123,8 +128,13 @@ void StackSamplingConfiguration::AppendCommandLineSwitchForChildProcess(
 
   bool enable =
       configuration_ == PROFILE_ENABLED || configuration_ == PROFILE_CONTROL;
-  if (enable && process_type == switches::kGpuProcess)
+  if (!enable)
+    return;
+  if (process_type == switches::kGpuProcess ||
+      (process_type == switches::kRendererProcess &&
+       ShouldEnableProfilerForNextRendererProcess())) {
     command_line->AppendSwitch(switches::kStartStackProfiler);
+  }
 }
 
 // static
--- content/public/renderer/content_renderer_client.cc
+++ content/public/renderer/content_renderer_client.cc
@@ -90,6 +90,9 @@ ContentRendererClient::OverrideSpeechSynthesizer(
   return nullptr;
 }
 
+void ContentRendererClient::PostCompositorThreadCreated(
+    base::SingleThreadTaskRunner* compositor_thread_task_runner) {}
+
 bool ContentRendererClient::RunIdleHandlerWhenWidgetsHidden() {
   return true;
 }
