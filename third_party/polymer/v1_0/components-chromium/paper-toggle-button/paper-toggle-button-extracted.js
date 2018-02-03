ccd82f3b73e6dc2b2fbb097d6e9cb5c94ad9f1f1
dpapad <dpapad@chromium.org>
2018-02-03 01:02:20 +0000

    WebUI: Update Polymer paper-toggle-button version 1.3.0 -> 2.0.0
    Bug: 738611
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I7da1eb6af56c5a6dbf6c8c6ea73d1e738df26de0
    Reviewed-on: https://chromium-review.googlesource.com/898203
    Reviewed-by: Scott Chen <scottchen@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534223}

--- third_party/polymer/v1_0/components-chromium/paper-toggle-button/paper-toggle-button-extracted.js
+++ third_party/polymer/v1_0/components-chromium/paper-toggle-button/paper-toggle-button-extracted.js
@@ -31,7 +31,7 @@ Polymer({
 
       attached: function() {
         Polymer.RenderStatus.afterNextRender(this, function() {
-          this.setScrollDirection('y');
+          Polymer.Gestures.setTouchAction(this, 'pan-y');
         });
       },
 
