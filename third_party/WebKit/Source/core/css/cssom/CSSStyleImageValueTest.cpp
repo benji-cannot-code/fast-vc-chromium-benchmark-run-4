00293ec082811f5da72a07ad9a36402cd60c640f
Darren Shen <shend@chromium.org>
2018-02-05 04:52:59 +0000

    Revert "Revert "[css-typed-om] Refactor CSSStyleImageValue to prepare for gradients.""
    This reverts commit 1f977216838ab364205dcd288b0b3efc0de1b01d.
    
    Reason for revert: Relanding
    
    Original change's description:
    > Revert "[css-typed-om] Refactor CSSStyleImageValue to prepare for gradients."
    > 
    > This reverts commit 6d10aad7a6f28294851e37f6ab7f0b0ac5086508.
    > 
    > Reason for revert: Caused MSAN bot failure.
    > 
    > https://ci.chromium.org/buildbot/chromium.memory/Linux%20MSan%20Tests/7639
    > 
    > Original change's description:
    > > [css-typed-om] Refactor CSSStyleImageValue to prepare for gradients.
    > > 
    > > Currently CSSStyleImageValue implements the CSSImageValue class in
    > > Typed OM. CSSImageValue can be anything that's an <image> i.e. a <url>,
    > > <gradient>, or <cross-fade>.
    > > 
    > > However, the current implementation of CSSStyleImageValue only really
    > > works for <url>s. This patch moves <url> specific logic into
    > > CSSURLImageValue. This prepares for the implementation of <gradient> and
    > > <cross-fade>, which will be subclasses of CSSStyleImageValue.
    > > 
    > > We also do some cleanup work (e.g. making things const, moving code in
    > > header to cpp).
    > > 
    > > Bug: 803680
    > > Change-Id: I848b3eab5dbbcb9bbe7f0aea0d377497f63bfda3
    > > Reviewed-on: https://chromium-review.googlesource.com/882786
    > > Commit-Queue: Darren Shen <shend@chromium.org>
    > > Reviewed-by: nainar <nainar@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#533949}
    > 
    > TBR=nainar@chromium.org,shend@chromium.org
    > 
    > Change-Id: I9aedca3ac9e3ecb07ed283447febede0628d7aeb
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 803680
    > Reviewed-on: https://chromium-review.googlesource.com/896671
    > Reviewed-by: Yuta Kitamura <yutak@chromium.org>
    > Commit-Queue: Yuta Kitamura <yutak@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#534002}
    
    TBR=yutak@chromium.org,nainar@chromium.org,shend@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 803680
    Change-Id: Iccabf87cde1e9bd30ab1d08e31fe1b774a59f029
    Reviewed-on: https://chromium-review.googlesource.com/900563
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534328}

--- third_party/WebKit/Source/core/css/cssom/CSSStyleImageValueTest.cpp
+++ third_party/WebKit/Source/core/css/cssom/CSSStyleImageValueTest.cpp
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/css/cssom/CSSStyleImageValue.h"
 
+#include "platform/graphics/Image.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace blink {
@@ -13,22 +14,30 @@ namespace {
 
 class FakeCSSStyleImageValue : public CSSStyleImageValue {
  public:
-  FakeCSSStyleImageValue(CSSImageValue* image_value,
-                         bool cache_pending,
-                         IntSize size)
-      : CSSStyleImageValue(image_value),
-        cache_pending_(cache_pending),
-        size_(size) {}
+  FakeCSSStyleImageValue(bool cache_pending, IntSize size)
+      : cache_pending_(cache_pending), size_(size) {}
 
-  bool IsCachePending() const final { return cache_pending_; }
-  IntSize ImageSize() const final { return size_; }
+  // CSSStyleImageValue
+  WTF::Optional<IntSize> IntrinsicSize() const final {
+    if (cache_pending_)
+      return WTF::nullopt;
+    return size_;
+  }
 
+  // CanvasImageSource
+  scoped_refptr<Image> GetSourceImageForCanvas(SourceImageStatus*,
+                                               AccelerationHint,
+                                               const FloatSize&) final {
+    return nullptr;
+  }
   ResourceStatus Status() const final {
-    if (IsCachePending())
+    if (cache_pending_)
       return ResourceStatus::kNotStarted;
     return ResourceStatus::kCached;
   }
+  bool IsAccelerated() const final { return false; }
 
+  // CSSStyleValue
   const CSSValue* ToCSSValue() const final { return nullptr; }
   StyleValueType GetType() const final { return kUnknownType; }
 
@@ -40,22 +49,20 @@ class FakeCSSStyleImageValue : public CSSStyleImageValue {
 }  // namespace
 
 TEST(CSSStyleImageValueTest, PendingCache) {
-  FakeCSSStyleImageValue* style_image_value = new FakeCSSStyleImageValue(
-      CSSImageValue::Create(""), true, IntSize(100, 100));
-  bool is_null;
-  EXPECT_EQ(style_image_value->intrinsicWidth(is_null), 0);
-  EXPECT_EQ(style_image_value->intrinsicHeight(is_null), 0);
-  EXPECT_EQ(style_image_value->intrinsicRatio(is_null), 0);
+  FakeCSSStyleImageValue style_image_value(true, IntSize(100, 100));
+  bool is_null = false;
+  EXPECT_EQ(style_image_value.intrinsicWidth(is_null), 0);
+  EXPECT_EQ(style_image_value.intrinsicHeight(is_null), 0);
+  EXPECT_EQ(style_image_value.intrinsicRatio(is_null), 0);
   EXPECT_TRUE(is_null);
 }
 
 TEST(CSSStyleImageValueTest, ValidLoadedImage) {
-  FakeCSSStyleImageValue* style_image_value = new FakeCSSStyleImageValue(
-      CSSImageValue::Create(""), false, IntSize(480, 120));
-  bool is_null;
-  EXPECT_EQ(style_image_value->intrinsicWidth(is_null), 480);
-  EXPECT_EQ(style_image_value->intrinsicHeight(is_null), 120);
-  EXPECT_EQ(style_image_value->intrinsicRatio(is_null), 4);
+  FakeCSSStyleImageValue style_image_value(false, IntSize(480, 120));
+  bool is_null = false;
+  EXPECT_EQ(style_image_value.intrinsicWidth(is_null), 480);
+  EXPECT_EQ(style_image_value.intrinsicHeight(is_null), 120);
+  EXPECT_EQ(style_image_value.intrinsicRatio(is_null), 4);
   EXPECT_FALSE(is_null);
 }
 
