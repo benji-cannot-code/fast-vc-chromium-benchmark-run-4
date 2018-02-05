0219c581387bd20640d24ab8ea5114398d2915a7
Yixin Wang <wangyix@chromium.org>
2018-02-05 20:09:57 +0000

    Revert "Inline QuicUrlUtilImpl::GetPushPromiseUrl() into SpdyUtils::GetPromisedUrlFromHeaders()"
    This reverts commit f5e7879afcb108c3feb928c16aa7eec7525f548a.
    
    Reason for revert: had to revert internal change 184066984:
    https://critique.corp.google.com/#review/184317140
    
    Original change's description:
    > Inline QuicUrlUtilImpl::GetPushPromiseUrl() into SpdyUtils::GetPromisedUrlFromHeaders()
    > 
    > Since google3's implementation will be identical, there's no need for the code to be in quic/platform/impl.
    > 
    > This CL along with https://chromium-review.googlesource.com/c/chromium/src/+/893625 together correspond with google3 CL https://critique.corp.google.com/#review/184066984
    > 
    > Change-Id: I335b43b82ae7b53f4de794e0ba465f03165fe4ae
    > Reviewed-on: https://chromium-review.googlesource.com/894178
    > Commit-Queue: Yixin Wang <wangyix@chromium.org>
    > Reviewed-by: Ryan Hamilton <rch@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#533536}
    
    TBR=rch@chromium.org,wangyix@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Change-Id: Ib59f9ee3a1b167e78a2eee287377a61bdd9ef7d7
    Reviewed-on: https://chromium-review.googlesource.com/899880
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Yixin Wang <wangyix@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534473}

--- net/quic/core/quic_client_push_promise_index.cc
+++ net/quic/core/quic_client_push_promise_index.cc
@@ -34,7 +34,7 @@ QuicAsyncStatus QuicClientPushPromiseIndex::Try(
     const SpdyHeaderBlock& request,
     QuicClientPushPromiseIndex::Delegate* delegate,
     TryHandle** handle) {
-  string url(SpdyUtils::GetPromisedUrlFromHeaders(request));
+  string url(SpdyUtils::GetPromisedUrlFromHeaderBlock(request));
   QuicPromisedByUrlMap::iterator it = promised_by_url_.find(url);
   if (it != promised_by_url_.end()) {
     QuicClientPromisedInfo* promised = it->second;
