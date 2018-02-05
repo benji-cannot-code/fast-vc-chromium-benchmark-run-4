d0cb2927c130d36c9e87c91e27c967641fcb9fce
jdoerrie <jdoerrie@chromium.org>
2018-02-05 22:03:44 +0000

    [fido] Move contents of //device/ctap into //device/fido
    As a follow up to r534391 this changes moves the contents of
    //device/ctap into //device/fido completing the merge of //device/u2f
    and //device/ctap. Similarly to r534391 it updates relevant includes,
    header guards, and BUILD.gn, DEPS and OWNER files.
    
    Bug: 808637
    Change-Id: I9b0b51b249ee07bae8e88dd846cce189d9b1dbed
    Reviewed-on: https://chromium-review.googlesource.com/902282
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534501}

---
+++ device/fido/authenticator_make_credential_response.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "device/fido/authenticator_make_credential_response.h"
+
+#include <utility>
+
+namespace device {
+
+AuthenticatorMakeCredentialResponse::AuthenticatorMakeCredentialResponse(
+    CTAPDeviceResponseCode response_code,
+    std::vector<uint8_t> attestation_object)
+    : response_code_(response_code),
+      attestation_object_(std::move(attestation_object)) {}
+
+AuthenticatorMakeCredentialResponse::AuthenticatorMakeCredentialResponse(
+    AuthenticatorMakeCredentialResponse&& that) = default;
+
+AuthenticatorMakeCredentialResponse& AuthenticatorMakeCredentialResponse::
+operator=(AuthenticatorMakeCredentialResponse&& other) = default;
+
+AuthenticatorMakeCredentialResponse::~AuthenticatorMakeCredentialResponse() =
+    default;
+
+}  // namespace device
