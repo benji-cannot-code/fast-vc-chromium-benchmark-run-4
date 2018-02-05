24e752b9d2df926181bcb194f9f13d914198cc42
Fady Samuel <fsamuel@chromium.org>
2018-02-05 16:02:55 +0000

    Surface synchronization: Fix OOPIF crash
    This CL fixes a surface invariants violation crash.
    CrossProcessFrameConnector::OnUpdateResizeParams plumbs a bunch of
    synchronized visual properties down the frame hierarchy. Once all the
    properties have been updated, we call RWHI::WasResized to push the
    updated properties to the renderer. RWHVChildFrame::SetSize was prematurely
    calling WasResized() while we were updating resize parameters. Resizes always
    propagate from the parent renderer now in order to grab a LocalSurfaceId and
    so calling WasResized in SetSize is unnecessary and puts the RenderWidget
    in a bad state.
    
    Bug: 791154, 672962
    TBR: creis@chromium.org
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I7f323681b2cea8ab8475545791bfcf5844fcb859
    Reviewed-on: https://chromium-review.googlesource.com/900315
    Commit-Queue: Fady Samuel <fsamuel@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534390}

--- content/browser/renderer_host/frame_connector_delegate.h
+++ content/browser/renderer_host/frame_connector_delegate.h
@@ -51,7 +51,7 @@ class WebCursor;
 // into, and whether the view currently has keyboard focus.
 class CONTENT_EXPORT FrameConnectorDelegate {
  public:
-  virtual void SetView(RenderWidgetHostViewChildFrame* view) {}
+  virtual void SetView(RenderWidgetHostViewChildFrame* view);
 
   // Returns the parent RenderWidgetHostView or nullptr if it doesn't have one.
   virtual RenderWidgetHostViewBase* GetParentRenderWidgetHostView();
@@ -71,6 +71,13 @@ class CONTENT_EXPORT FrameConnectorDelegate {
   // its corresponding remote frame in the parent frame's renderer.
   virtual void SendIntrinsicSizingInfoToParent(
       const blink::WebIntrinsicSizingInfo&) {}
+
+  // Sends new resize parameters to the sub-frame's renderer.
+  void UpdateResizeParams(const gfx::Rect& screen_space_rect,
+                          const gfx::Size& local_frame_size,
+                          const ScreenInfo& screen_info,
+                          uint64_t sequence_number,
+                          const viz::SurfaceId& surface_id);
 
   // Return the size of the CompositorFrame to use in the child renderer.
   const gfx::Size& local_frame_size_in_pixels() {
@@ -217,6 +224,9 @@ class CONTENT_EXPORT FrameConnectorDelegate {
   explicit FrameConnectorDelegate(bool use_zoom_for_device_scale_factor);
 
   virtual ~FrameConnectorDelegate() {}
+
+  // The RenderWidgetHostView for the frame. Initially NULL.
+  RenderWidgetHostViewChildFrame* view_ = nullptr;
 
   // This is here rather than in the implementation class so that
   // ViewportIntersection() can return a reference.
