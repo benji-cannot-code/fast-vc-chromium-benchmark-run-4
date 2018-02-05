32021322af8b648ff2d305be6e15273e332a581d
Tommi <tommi@chromium.org>
2018-02-05 09:51:46 +0000

    Update MediaStreamAudioProcessorTest to use ScopedTaskEnvironment instead of MessageLoop. This in anticipation of WebRTC using SequencedTaskRunner via TaskQueue.
    Change-Id: Ibe863aab34424c2d3c4dba3d885dadfc5c6f7f79
    Bug: 808801
    Reviewed-on: https://chromium-review.googlesource.com/900642
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534356}

--- content/renderer/media/media_stream_audio_processor_unittest.cc
+++ content/renderer/media/media_stream_audio_processor_unittest.cc
@@ -15,8 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/macros.h"
 #include "base/memory/aligned_memory.h"
-#include "base/message_loop/message_loop.h"
 #include "base/path_service.h"
+#include "base/test/scoped_task_environment.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
@@ -234,7 +234,7 @@ class MediaStreamAudioProcessorTest : public ::testing::Test {
 #endif
   }
 
-  base::MessageLoop main_thread_message_loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
   media::AudioParameters params_;
 };
 
