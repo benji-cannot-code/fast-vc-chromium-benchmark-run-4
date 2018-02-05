c6112848143f1fd4e3e52901c471324636093e61
Mounir Lamouri <mlamouri@chromium.org>
2018-02-05 14:19:05 +0000

    Media Engagement: ignore prerender/preload navigations.
    Prerender/preload navigations are not tagged as such in the
    NavigationHandle, requiring us to explicitly check with the
    PrerenderManager if we are running in such a context.
    
    This should solve a cause for many fake visits.
    
    Bug: 805918
    Change-Id: I9b397822465086dcb97ca72e3e4d1df324cf324e
    Reviewed-on: https://chromium-review.googlesource.com/897500
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Commit-Queue: Mounir Lamouri (slow) <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534379}

--- chrome/browser/media/media_engagement_service.cc
+++ chrome/browser/media/media_engagement_service.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/media/media_engagement_contents_observer.h"
 #include "chrome/browser/media/media_engagement_score.h"
 #include "chrome/browser/media/media_engagement_service_factory.h"
+#include "chrome/browser/prerender/prerender_contents.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/pref_names.h"
 #include "components/content_settings/core/browser/host_content_settings_map.h"
@@ -83,6 +84,11 @@ MediaEngagementService* MediaEngagementService::Get(Profile* profile) {
 void MediaEngagementService::CreateWebContentsObserver(
     content::WebContents* web_contents) {
   DCHECK(IsEnabled());
+
+  // Ignore WebContents that are used for prerender/prefetch.
+  if (prerender::PrerenderContents::FromWebContents(web_contents))
+    return;
+
   MediaEngagementService* service =
       Get(Profile::FromBrowserContext(web_contents->GetBrowserContext()));
   if (!service)
