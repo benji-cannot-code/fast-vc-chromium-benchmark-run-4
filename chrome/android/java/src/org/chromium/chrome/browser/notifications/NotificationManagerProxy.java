21e8ef0350393f23b79c4d728923ebd6e849931f
Mugdha Lakhani <nator@chromium.org>
2018-02-05 16:19:58 +0000

    Revert "[Android Notifications] Implement GetDisplayed() for Android O+"
    This reverts commit a99fad2c5b37e0ede596a45d891645300b26311c.
    See Issue 806982 for details.
    
    Bug: 806982, 800363
    Change-Id: I08672ec28eb607cb6686fbb3dfd9673caee8eb9b
    Reviewed-on: https://chromium-review.googlesource.com/901606
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534395}

--- chrome/android/java/src/org/chromium/chrome/browser/notifications/NotificationManagerProxy.java
+++ chrome/android/java/src/org/chromium/chrome/browser/notifications/NotificationManagerProxy.java
@@ -10,7 +10,6 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationChannelGroup;
 import android.os.Build;
-import android.service.notification.StatusBarNotification;
 
 import java.util.List;
 
@@ -46,7 +45,4 @@ public interface NotificationManagerProxy {
 
     @TargetApi(Build.VERSION_CODES.O)
     void deleteNotificationChannelGroup(String groupId);
-
-    @TargetApi(Build.VERSION_CODES.M)
-    StatusBarNotification[] getActiveNotifications();
 }
--- chrome/android/java/src/org/chromium/chrome/browser/notifications/NotificationManagerProxyImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/notifications/NotificationManagerProxyImpl.java
@@ -11,7 +11,6 @@ import android.app.NotificationChannel;
 import android.app.NotificationChannelGroup;
 import android.app.NotificationManager;
 import android.os.Build;
-import android.service.notification.StatusBarNotification;
 
 import java.util.List;
 
@@ -104,12 +103,5 @@ public class NotificationManagerProxyImpl implements NotificationManagerProxy {
     public void deleteNotificationChannelGroup(String groupId) {
         assert Build.VERSION.SDK_INT >= Build.VERSION_CODES.O;
         mNotificationManager.deleteNotificationChannelGroup(groupId);
-    }
-
-    @TargetApi(Build.VERSION_CODES.M)
-    @Override
-    public StatusBarNotification[] getActiveNotifications() {
-        assert Build.VERSION.SDK_INT >= Build.VERSION_CODES.O;
-        return mNotificationManager.getActiveNotifications();
     }
 }
