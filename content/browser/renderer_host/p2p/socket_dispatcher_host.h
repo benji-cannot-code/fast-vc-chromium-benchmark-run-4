6479191ec6e3b9218723b510d393e4a0bc696f0f
Ramin Halavati <rhalavati@chromium.org>
2018-02-05 12:05:36 +0000

    Add traffic annotation argument to P2PHostMsg_Send.
    To have proper network traffic annotation in
    P2PSocketDispatcherHost::OnSend, the argument is added to
    P2PHostMsg_Send IPC message.
    
    Bug: 656607
    Change-Id: Ie91e4ddc4ec61624dd8835c0df19cfb7ca476235
    Reviewed-on: https://chromium-review.googlesource.com/886482
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534267}
    
    Relanding after revert: crrev.com/c/899184
    
    TBR=tommi@chromium.org,sergeyu@chromium.org,tsepez@chromium.org,jochen@chromium.org
    
    Change-Id: Ie91e4ddc4ec61624dd8835c0df19cfb7ca476235
    Reviewed-on: https://chromium-review.googlesource.com/900783
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534368}

--- content/browser/renderer_host/p2p/socket_dispatcher_host.h
+++ content/browser/renderer_host/p2p/socket_dispatcher_host.h
@@ -26,15 +26,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/network_change_notifier.h"
 
 namespace net {
+struct MutableNetworkTrafficAnnotationTag;
 class URLRequestContextGetter;
 }
 
 namespace network {
 class ProxyResolvingClientSocketFactory;
-}
-
-namespace rtc {
-struct PacketOptions;
 }
 
 namespace content {
@@ -92,11 +89,11 @@ class P2PSocketDispatcherHost
   void OnAcceptIncomingTcpConnection(int listen_socket_id,
                                      const net::IPEndPoint& remote_address,
                                      int connected_socket_id);
-  void OnSend(int socket_id,
-              const net::IPEndPoint& socket_address,
-              const std::vector<char>& data,
-              const rtc::PacketOptions& options,
-              uint64_t packet_id);
+  void OnSend(
+      int socket_id,
+      const std::vector<char>& data,
+      const P2PPacketInfo& packet_info,
+      const net::MutableNetworkTrafficAnnotationTag& traffic_annotation);
   void OnSetOption(int socket_id, P2PSocketOption option, int value);
   void OnDestroySocket(int socket_id);
 
