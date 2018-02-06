28a80375ef55a5d4722506edef467d8eb2338023
kylechar <kylechar@chromium.org>
2018-02-06 20:44:50 +0000

    Delete mojom::OffscreenCanvasSurface interface.
    The interface isn't needed after an unrelated refactoring. Listen for
    connection errors on OffscreenCanvasSurfaceClient interface instead,
    since the message pipe endpoints are the same, and use that signal to
    destroy OffscreenCanvasSurfaceImpl. Also cleanup data members to remove
    an unneeded callback.
    
    Bug: none
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Ib21e0974b981dcd63eb76a9ac1f9f10591eea08f
    Reviewed-on: https://chromium-review.googlesource.com/901922
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Olivia Lai <xlai@chromium.org>
    Commit-Queue: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534786}

--- content/browser/renderer_host/offscreen_canvas_provider_impl.cc
+++ content/browser/renderer_host/offscreen_canvas_provider_impl.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/renderer_host/offscreen_canvas_provider_impl.h"
 
 #include "base/bind.h"
+#include "components/viz/host/host_frame_sink_manager.h"
 #include "content/browser/renderer_host/offscreen_canvas_surface_impl.h"
 
 namespace content {
@@ -26,8 +27,7 @@ void OffscreenCanvasProviderImpl::Add(
 void OffscreenCanvasProviderImpl::CreateOffscreenCanvasSurface(
     const viz::FrameSinkId& parent_frame_sink_id,
     const viz::FrameSinkId& frame_sink_id,
-    blink::mojom::OffscreenCanvasSurfaceClientPtr client,
-    blink::mojom::OffscreenCanvasSurfaceRequest request) {
+    blink::mojom::OffscreenCanvasSurfaceClientPtr client) {
   // TODO(kylechar): Kill the renderer too.
   if (parent_frame_sink_id.client_id() != renderer_client_id_) {
     DLOG(ERROR) << "Invalid parent client id " << parent_frame_sink_id;
@@ -44,7 +44,7 @@ void OffscreenCanvasProviderImpl::CreateOffscreenCanvasSurface(
 
   canvas_map_[frame_sink_id] = std::make_unique<OffscreenCanvasSurfaceImpl>(
       host_frame_sink_manager_, parent_frame_sink_id, frame_sink_id,
-      std::move(client), std::move(request), std::move(destroy_callback));
+      std::move(client), std::move(destroy_callback));
 }
 
 void OffscreenCanvasProviderImpl::CreateCompositorFrameSink(
