9b2f2e1939744af6e45dd22e58d42373707dacbe
Allen Vicencio <allenvic@chromium.org>
2018-02-06 23:34:56 +0000

    Implement WriteFile in SmbFileSystem
    Implements WriteFile in SmbFileSystem using TempFileManager.
    
    Bug: chromium:757625
    Change-Id: I176079aca1bb752f628c86942f2ee89b46c19729
    Reviewed-on: https://chromium-review.googlesource.com/894983
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Allen Vicencio <allenvic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534834}

--- chrome/browser/chromeos/smb_client/smb_file_system.h
+++ chrome/browser/chromeos/smb_client/smb_file_system.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/file_system_provider/provided_file_system_interface.h"
 #include "chrome/browser/chromeos/file_system_provider/watcher.h"
 #include "chrome/browser/chromeos/smb_client/smb_service.h"
+#include "chrome/browser/chromeos/smb_client/temp_file_manager.h"
 #include "chromeos/dbus/smb_provider_client.h"
 #include "storage/browser/fileapi/async_file_util.h"
 #include "storage/browser/fileapi/watcher_manager.h"
@@ -214,6 +215,10 @@ class SmbFileSystem : public file_system_provider::ProvidedFileSystemInterface {
       const storage::AsyncFileUtil::StatusCallback& callback,
       smbprovider::ErrorType error) const;
 
+  void HandleRequestWriteFileCallback(
+      const storage::AsyncFileUtil::StatusCallback& callback,
+      smbprovider::ErrorType error) const;
+
   base::File::Error RunUnmountCallback(
       const ProviderId& provider_id,
       const std::string& file_system_id,
@@ -235,6 +240,7 @@ class SmbFileSystem : public file_system_provider::ProvidedFileSystemInterface {
   file_system_provider::Watchers watchers_;
 
   UnmountCallback unmount_callback_;
+  TempFileManager temp_file_manager_;
 
   base::WeakPtrFactory<SmbFileSystem> weak_ptr_factory_;
   DISALLOW_COPY_AND_ASSIGN(SmbFileSystem);
