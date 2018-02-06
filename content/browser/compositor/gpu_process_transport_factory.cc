dc5533a9ee7052c9a4d8f28f264a7e658d8b622e
Ria Jiang <riajiang@chromium.org>
2018-02-06 20:18:45 +0000

    Pass use_viz_hit_test to DirectLayerTreeFrameSink and set up hit-test.
    Pass in features::IsVizHitTestingEnabled() value when creating a
    DirectLayerTreeFrameSink and set up Viz hit-test in DLTFS when it is
    enabled.
    
    Bug: 805581
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: Iec2e2c81444fda85a5d1f99824527360ae22ac5c
    Reviewed-on: https://chromium-review.googlesource.com/901945
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Ria Jiang <riajiang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534771}

--- content/browser/compositor/gpu_process_transport_factory.cc
+++ content/browser/compositor/gpu_process_transport_factory.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/base/switches.h"
 #include "cc/raster/single_thread_task_graph_runner.h"
 #include "cc/raster/task_graph_runner.h"
+#include "components/viz/common/features.h"
 #include "components/viz/common/frame_sinks/begin_frame_args.h"
 #include "components/viz/common/frame_sinks/begin_frame_source.h"
 #include "components/viz/common/frame_sinks/delay_based_time_source.h"
@@ -641,14 +642,16 @@ void GpuProcessTransportFactory::EstablishedGpuChannel(
                 GetFrameSinkManager(), data->display.get(),
                 data->display_client.get(),
                 static_cast<scoped_refptr<viz::VulkanContextProvider>>(
-                    vulkan_context_provider))
+                    vulkan_context_provider),
+                features::IsVizHitTestingEnabled())
           : std::make_unique<viz::DirectLayerTreeFrameSink>(
                 compositor->frame_sink_id(), GetHostFrameSinkManager(),
                 GetFrameSinkManager(), data->display.get(),
                 data->display_client.get(), context_provider,
                 shared_worker_context_provider_, compositor->task_runner(),
                 GetGpuMemoryBufferManager(),
-                viz::ServerSharedBitmapManager::current());
+                viz::ServerSharedBitmapManager::current(),
+                features::IsVizHitTestingEnabled());
   data->display->Resize(compositor->size());
   data->display->SetOutputIsSecure(data->output_is_secure);
   compositor->SetLayerTreeFrameSink(std::move(layer_tree_frame_sink));
