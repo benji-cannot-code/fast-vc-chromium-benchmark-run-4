a3c076f4673c76eb9ccebdb4b87d9971e6b687e9
Christos Froussios <cfroussios@chromium.org>
2018-02-06 18:32:11 +0000

    [Password Manager] Show export menu item only for a non-empty password list
    Exporting an empty password list has no value for the user. Additionally,
    exporting while the password list is still loading is undefined behaviour.
    
    For these reasons, we only show the Export Passwords menu item only once
    the password list has been read and is non-empty.
    
    Bug: 789561,805534,807975
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ief82b4b46a231402dbaa7251e514c4078f7be31a
    Reviewed-on: https://chromium-review.googlesource.com/897486
    Commit-Queue: Christos Froussios <cfroussios@chromium.org>
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534728}

--- chrome/browser/resources/settings/passwords_and_forms_page/passwords_section.js
+++ chrome/browser/resources/settings/passwords_and_forms_page/passwords_section.js
@@ -295,10 +295,7 @@ Polymer({
     /** @private */
     showExportPasswords_: {
       type: Boolean,
-      value: function() {
-        return loadTimeData.valueExists('showExportPasswords') &&
-            loadTimeData.getBoolean('showExportPasswords');
-      }
+      computed: 'showExportPasswordsAndReady_(savedPasswords)'
     },
 
     /** @private */
@@ -579,6 +576,16 @@ Polymer({
    */
   getOnOffLabel_: function(toggleValue) {
     return toggleValue ? this.i18n('toggleOn') : this.i18n('toggleOff');
+  },
+
+  /**
+   * @private
+   * @param {!Array<!PasswordManager.PasswordUiEntry>} savedPasswords
+   */
+  showExportPasswordsAndReady_: function(savedPasswords) {
+    return loadTimeData.valueExists('showExportPasswords') &&
+        loadTimeData.getBoolean('showExportPasswords') &&
+        savedPasswords.length > 0;
   },
 
   /**
