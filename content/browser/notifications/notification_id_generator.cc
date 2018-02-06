3617bd89529014ea8be7ef588ea464ceca20c9c3
Anita Woodruff <awdf@chromium.org>
2018-02-06 18:04:54 +0000

    [Cleanup] Use chars for NotificationIdGenerator constants
    R=peter@chromium.org
    
    Change-Id: I7f66591e63807506f50614c7d19d0dbaf77d681d
    Reviewed-on: https://chromium-review.googlesource.com/904702
    Commit-Queue: Anita Woodruff <awdf@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534716}

--- content/browser/notifications/notification_id_generator.cc
+++ content/browser/notifications/notification_id_generator.cc
@@ -15,24 +15,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 namespace {
 
-const char kNotificationTagSeparator[] = "#";
-const char kPersistentNotificationPrefix[] = "p";
-const char kNonPersistentNotificationPrefix[] = "n";
+const char kNotificationTagSeparator = '#';
+const char kPersistentNotificationPrefix = 'p';
+const char kNonPersistentNotificationPrefix = 'n';
 
 }  // namespace
 
 // static
 bool NotificationIdGenerator::IsPersistentNotification(
     const base::StringPiece& notification_id) {
-  return notification_id.starts_with(
-      std::string(kPersistentNotificationPrefix));
+  return notification_id.length() > 0 &&
+         notification_id.front() == kPersistentNotificationPrefix;
 }
 
 // static
 bool NotificationIdGenerator::IsNonPersistentNotification(
     const base::StringPiece& notification_id) {
-  return notification_id.starts_with(
-      std::string(kNonPersistentNotificationPrefix));
+  return notification_id.length() > 0 &&
+         notification_id.front() == kNonPersistentNotificationPrefix;
 }
 
 // Notification Id is of the following format:
