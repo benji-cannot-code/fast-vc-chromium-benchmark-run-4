5c9a27346e001bab09fb6f90488ae12fb8248de4
Colin Blundell <blundell@chromium.org>
2018-02-06 19:31:38 +0000

    [NTP Snippets] Have SubscriptionManager use IdentityManager
    This CL converts SubscriptionManager from using
    //components/signin to using the Identity Service client library.
    The conversion is straightforward:
    
    - Observer IdentityManager rather than SigninManager.
    - Get info of primary account via IdentityManager rather than
    SigninManager.
    - Create PrimaryAccountAccessTokenFetcher via IdentityManager rather
    than directly.
    
    I tested the relevant behavior manually as follows, based on
    instructions from mamir@chromium.org:
    
    1. Enabled Breaking News Push by turning on "enable-breaking-newspush
    on chrome://flags:
    https://cs.chromium.org/chromium/src/chrome/browser/about_flags.cc?type=cs&q=kEnableBreakingNewsPushName&sq=package:chromium&l=2497
    
    2. Checked the about:histograms for the feature's metrics by doing
    "find in page" for "subscription". Saw no histogram entries after
    visiting the NTP when signed out but *did* see histogram entries after
    visiting the NTP when signed in. Metrics listed here:
    https://cs.chromium.org/chromium/src/components/ntp_snippets/breaking_news/breaking_news_metrics.cc?type=cs&l=17
    
    A followup change will convert the test to use IdentityTestEnvironment.
    
    Bug: 798413
    Change-Id: I9193d25c2793cf52fd825186cd5d875b352eb8bb
    Reviewed-on: https://chromium-review.googlesource.com/888520
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534750}

--- components/ntp_snippets/breaking_news/subscription_manager_impl_unittest.cc
+++ components/ntp_snippets/breaking_news/subscription_manager_impl_unittest.cc
@@ -19,6 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/net_errors.h"
 #include "net/url_request/test_url_fetcher_factory.h"
 #include "net/url_request/url_request_test_util.h"
+#include "services/identity/public/cpp/identity_manager.h"
+#include "services/identity/public/cpp/identity_test_utils.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -26,7 +28,10 @@ using testing::ElementsAre;
 
 namespace ntp_snippets {
 
+#if !defined(OS_CHROMEOS)
 const char kTestEmail[] = "test@email.com";
+#endif
+
 const char kAPIKey[] = "fakeAPIkey";
 const char kSubscriptionUrl[] = "http://valid-url.test/subscribe";
 const char kSubscriptionUrlSignedIn[] = "http://valid-url.test/subscribe";
@@ -43,7 +48,8 @@ class SubscriptionManagerImplTest
       public OAuth2TokenService::DiagnosticsObserver {
  public:
   SubscriptionManagerImplTest()
-      : request_context_getter_(
+      : identity_manager_(utils_.fake_signin_manager(), utils_.token_service()),
+        request_context_getter_(
             new net::TestURLRequestContextGetter(message_loop_.task_runner())) {
   }
 
@@ -67,13 +73,14 @@ class SubscriptionManagerImplTest
     return utils_.token_service();
   }
 
+  identity::IdentityManager* GetIdentityManager() { return &identity_manager_; }
+
   SigninManagerBase* GetSigninManager() { return utils_.fake_signin_manager(); }
 
   std::unique_ptr<SubscriptionManagerImpl> BuildSubscriptionManager() {
     return std::make_unique<SubscriptionManagerImpl>(
         GetRequestContext(), GetPrefService(),
-        /*variations_service=*/nullptr, GetSigninManager(),
-        GetOAuth2TokenService(),
+        /*variations_service=*/nullptr, GetIdentityManager(),
         /*locale=*/"", kAPIKey, GURL(kSubscriptionUrl),
         GURL(kUnsubscriptionUrl));
   }
@@ -131,19 +138,26 @@ class SubscriptionManagerImplTest
 
 #if !defined(OS_CHROMEOS)
   void SignIn() {
-    utils_.fake_signin_manager()->SignIn(kTestEmail, "user", "pass");
+    identity::MakePrimaryAccountAvailable(utils_.fake_signin_manager(),
+                                          GetOAuth2TokenService(),
+                                          GetIdentityManager(), kTestEmail);
   }
 
-  void SignOut() { utils_.fake_signin_manager()->ForceSignOut(); }
+  void SignOut() {
+    identity::ClearPrimaryAccount(utils_.fake_signin_manager(),
+                                  GetIdentityManager());
+  }
 #endif  // !defined(OS_CHROMEOS)
 
   void IssueRefreshToken(FakeProfileOAuth2TokenService* auth_token_service) {
-    auth_token_service->GetDelegate()->UpdateCredentials(kTestEmail, "token");
+    auth_token_service->GetDelegate()->UpdateCredentials(
+        GetIdentityManager()->GetPrimaryAccountInfo().account_id, "token");
   }
 
   void IssueAccessToken(FakeProfileOAuth2TokenService* auth_token_service) {
-    auth_token_service->IssueAllTokensForAccount(kTestEmail, "access_token",
-                                                 base::Time::Max());
+    auth_token_service->IssueAllTokensForAccount(
+        GetIdentityManager()->GetPrimaryAccountInfo().account_id,
+        "access_token", base::Time::Max());
   }
 
   void set_on_access_token_request_callback(base::OnceClosure callback) {
@@ -179,6 +193,7 @@ class SubscriptionManagerImplTest
 
   base::MessageLoop message_loop_;
   test::RemoteSuggestionsTestUtils utils_;
+  identity::IdentityManager identity_manager_;
   scoped_refptr<net::TestURLRequestContextGetter> request_context_getter_;
   net::TestURLFetcherFactory url_fetcher_factory_;
   base::OnceClosure on_access_token_request_callback_;
