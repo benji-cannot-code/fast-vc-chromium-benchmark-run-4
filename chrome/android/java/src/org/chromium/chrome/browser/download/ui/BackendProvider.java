cbf7621155f6633465f529fdb9e497fa5ece11fd
Xing Liu <xingliu@chromium.org>
2018-02-06 18:53:27 +0000

    Android Download: Fix an issue that Chrome removes external renamed file.
    When user downloads a file in Chrome and remove the file in other
    application, we check if the file is externally removed and call
    Android API DownloadManager.remove(id) to avoid a crash on Android.
    
    However, if the user rename the file, currently Chrome still deletes it
    via the id provided by Android DownloadManager.
    
    In this CL, we only let Android remove the file if it's deleted in
    Chrome.
    
    
    Bug: 774870
    Change-Id: I5448a155c1e05e44ec3a812e93d3035d7061ecf7
    Reviewed-on: https://chromium-review.googlesource.com/899770
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534738}

--- chrome/android/java/src/org/chromium/chrome/browser/download/ui/BackendProvider.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/ui/BackendProvider.java
@@ -34,7 +34,7 @@ public interface BackendProvider {
         void checkForExternallyRemovedDownloads(boolean isOffTheRecord);
 
         /** See {@link DownloadManagerService#removeDownload}. */
-        void removeDownload(String guid, boolean isOffTheRecord);
+        void removeDownload(String guid, boolean isOffTheRecord, boolean externallyRemoved);
 
         /** See {@link DownloadManagerService#isDownloadOpenableInBrowser}. */
         boolean isDownloadOpenableInBrowser(boolean isOffTheRecord, String mimeType);
