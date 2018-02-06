9206ecd8e15d43ea03010b6fb0365c11c035c853
Jian Li <jianli@chromium.org>
2018-02-06 01:46:07 +0000

    Support message/rfc822 content type as MHTML
    Content type "message/rfc822" is interpreted as MHTML on Windows.
    Google Drive and some other appes choose to serve "message/rfc822" for
    MHTML files. So we should also support opening this type of content.
    
    Since now "message/rfc822" can be rendered in Blink, we also force
    downloading it if from the remote server.
    
    Bug: 758690
    Change-Id: I8cbe1a16f35239e805492a2abf381e2b04602f8e
    Reviewed-on: https://chromium-review.googlesource.com/898249
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Jian Li <jianli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534575}

--- third_party/WebKit/Source/core/loader/DocumentLoader.cpp
+++ third_party/WebKit/Source/core/loader/DocumentLoader.cpp
@@ -97,9 +97,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 // The MHTML mime type should be same as the one we check in the browser
-// process's ChromeResourceDispatcherHostDelegate.
+// process's IsDownload (navigation_url_loader_network_service.cc).
 static bool IsArchiveMIMEType(const String& mime_type) {
-  return DeprecatedEqualIgnoringCase("multipart/related", mime_type);
+  return DeprecatedEqualIgnoringCase("multipart/related", mime_type) ||
+         DeprecatedEqualIgnoringCase("message/rfc822", mime_type);
 }
 
 DocumentLoader::DocumentLoader(
