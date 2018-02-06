43cfc4b69edd4f31a7b8109f6c7222125e111f39
Pavel Feldman <pfeldman@chromium.org>
2018-02-06 01:18:35 +0000

    DevTools: split ChildTargetManager into Browser and Node versions.
    Change-Id: If11cfc47b3ad2f242ca3bd1a0d7690321bfad386
    Reviewed-on: https://chromium-review.googlesource.com/902952
    Commit-Queue: Pavel Feldman <pfeldman@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534566}

--- third_party/WebKit/LayoutTests/http/tests/devtools/network/network-xhr-same-url-as-main-resource.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/network/network-xhr-same-url-as-main-resource.js
@@ -24,7 +24,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   TestRunner.evaluateInPage('loadIframe()');
 
   function step2() {
-    NetworkTestRunner.makeSimpleXHR('GET', 'resources/resource.php', true, step3);
+    NetworkTestRunner.makeSimpleXHR('GET', 'resources/resource.php', true, () => {
+      TestRunner.deprecatedRunAfterPendingDispatches(step3);
+    });
   }
 
   function step3() {
