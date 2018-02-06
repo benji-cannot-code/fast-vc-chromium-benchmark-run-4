5715fa0dd432f300a4fe2246f65d959aca965e8c
Hidehiko Abe <hidehiko@chromium.org>
2018-02-06 18:08:06 +0000

    Migrate callbacks in MediaAnalyticsClient to DBusMethodCallback.
    BUG=739622
    TEST=Ran on bots.
    
    Change-Id: I6e968439859b4409a14621a03bd3a20f8ebe36f2
    Reviewed-on: https://chromium-review.googlesource.com/901682
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534718}

--- chromeos/dbus/fake_media_analytics_client.cc
+++ chromeos/dbus/fake_media_analytics_client.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromeos/dbus/fake_media_analytics_client.h"
 
+#include <utility>
+
 #include "base/bind.h"
 #include "base/logging.h"
 #include "base/threading/thread_task_runner_handle.h"
@@ -44,20 +46,23 @@ void FakeMediaAnalyticsClient::ClearMediaPerceptionSignalHandler() {
   media_perception_signal_handler_.Reset();
 }
 
-void FakeMediaAnalyticsClient::GetState(const StateCallback& callback) {
+void FakeMediaAnalyticsClient::GetState(
+    DBusMethodCallback<mri::State> callback) {
   if (!process_running_) {
-    callback.Run(false, current_state_);
+    std::move(callback).Run(base::nullopt);
     return;
   }
   base::ThreadTaskRunnerHandle::Get()->PostTask(
-      FROM_HERE, base::Bind(&FakeMediaAnalyticsClient::OnState,
-                            weak_ptr_factory_.GetWeakPtr(), callback));
+      FROM_HERE,
+      base::BindOnce(&FakeMediaAnalyticsClient::OnState,
+                     weak_ptr_factory_.GetWeakPtr(), std::move(callback)));
 }
 
-void FakeMediaAnalyticsClient::SetState(const mri::State& state,
-                                        const StateCallback& callback) {
+void FakeMediaAnalyticsClient::SetState(
+    const mri::State& state,
+    DBusMethodCallback<mri::State> callback) {
   if (!process_running_) {
-    callback.Run(false, current_state_);
+    std::move(callback).Run(base::nullopt);
     return;
   }
   DCHECK(state.has_status()) << "Trying to set state without status.";
@@ -68,8 +73,9 @@ void FakeMediaAnalyticsClient::SetState(const mri::State& state,
          "RESTARTING.";
   current_state_ = state;
   base::ThreadTaskRunnerHandle::Get()->PostTask(
-      FROM_HERE, base::Bind(&FakeMediaAnalyticsClient::OnState,
-                            weak_ptr_factory_.GetWeakPtr(), callback));
+      FROM_HERE,
+      base::BindOnce(&FakeMediaAnalyticsClient::OnState,
+                     weak_ptr_factory_.GetWeakPtr(), std::move(callback)));
 }
 
 void FakeMediaAnalyticsClient::SetStateSuspended() {
@@ -81,25 +87,27 @@ void FakeMediaAnalyticsClient::SetStateSuspended() {
   current_state_ = suspended;
 }
 
-void FakeMediaAnalyticsClient::OnState(const StateCallback& callback) {
-  callback.Run(true, current_state_);
+void FakeMediaAnalyticsClient::OnState(
+    DBusMethodCallback<mri::State> callback) {
+  std::move(callback).Run(current_state_);
 }
 
 void FakeMediaAnalyticsClient::GetDiagnostics(
-    const DiagnosticsCallback& callback) {
+    DBusMethodCallback<mri::Diagnostics> callback) {
   if (!process_running_) {
     LOG(ERROR) << "Fake media analytics process not running.";
-    callback.Run(false, mri::Diagnostics());
+    std::move(callback).Run(base::nullopt);
     return;
   }
   base::ThreadTaskRunnerHandle::Get()->PostTask(
-      FROM_HERE, base::Bind(&FakeMediaAnalyticsClient::OnGetDiagnostics,
-                            weak_ptr_factory_.GetWeakPtr(), callback));
+      FROM_HERE,
+      base::BindOnce(&FakeMediaAnalyticsClient::OnGetDiagnostics,
+                     weak_ptr_factory_.GetWeakPtr(), std::move(callback)));
 }
 
 void FakeMediaAnalyticsClient::OnGetDiagnostics(
-    const DiagnosticsCallback& callback) {
-  callback.Run(true, diagnostics_);
+    DBusMethodCallback<mri::Diagnostics> callback) {
+  std::move(callback).Run(diagnostics_);
 }
 
 void FakeMediaAnalyticsClient::OnMediaPerception(
