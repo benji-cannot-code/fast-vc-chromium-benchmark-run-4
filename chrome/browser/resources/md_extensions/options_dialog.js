772154848903c458f4c93d6fd00368945b8685f3
dpapad <dpapad@chromium.org>
2018-02-07 15:46:11 +0000

    MD Extensions: Ensure custom ExtensionOptions element is registered before used.
    This fixes a case that only reproduced during testing (ASAN only), where the
    ExtensionOptions custom element was not registered yet by the time it was created.
    
    Bug: 800502
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I71453b53042cfb2dd4eada557223c255d2e78a58
    Reviewed-on: https://chromium-review.googlesource.com/905521
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535008}

--- chrome/browser/resources/md_extensions/options_dialog.js
+++ chrome/browser/resources/md_extensions/options_dialog.js
@@ -6,6 +6,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 cr.define('extensions', function() {
   'use strict';
 
+  /**
+   * @return {!Promise} A signal that the document is ready. Need to wait for
+   *     this, otherwise the custom ExtensionOptions element might not have been
+   *     registered yet.
+   */
+  function whenDocumentReady() {
+    if (document.readyState == 'complete')
+      return Promise.resolve();
+
+    return new Promise(function(resolve) {
+      document.addEventListener('readystatechange', function f() {
+        if (document.readyState == 'complete') {
+          document.removeEventListener('readystatechange', f);
+          resolve();
+        }
+      });
+    });
+  }
+
   const OptionsDialog = Polymer({
     is: 'extensions-options-dialog',
 
@@ -26,21 +45,23 @@ cr.define('extensions', function() {
     /** @param {chrome.developerPrivate.ExtensionInfo} data */
     show: function(data) {
       this.data_ = data;
-      if (!this.extensionOptions_)
-        this.extensionOptions_ = document.createElement('ExtensionOptions');
-      this.extensionOptions_.extension = this.data_.id;
-      this.extensionOptions_.onclose = this.close.bind(this);
+      whenDocumentReady().then(() => {
+        if (!this.extensionOptions_)
+          this.extensionOptions_ = document.createElement('ExtensionOptions');
+        this.extensionOptions_.extension = this.data_.id;
+        this.extensionOptions_.onclose = this.close.bind(this);
 
-      const onSizeChanged = e => {
-        this.extensionOptions_.style.height = e.height + 'px';
-        this.extensionOptions_.style.width = e.width + 'px';
+        const onSizeChanged = e => {
+          this.extensionOptions_.style.height = e.height + 'px';
+          this.extensionOptions_.style.width = e.width + 'px';
 
-        if (!this.$$('dialog').open)
-          this.$$('dialog').showModal();
-      };
+          if (!this.$$('dialog').open)
+            this.$$('dialog').showModal();
+        };
 
-      this.extensionOptions_.onpreferredsizechanged = onSizeChanged;
-      this.$.body.appendChild(this.extensionOptions_);
+        this.extensionOptions_.onpreferredsizechanged = onSizeChanged;
+        this.$.body.appendChild(this.extensionOptions_);
+      });
     },
 
     close: function() {
