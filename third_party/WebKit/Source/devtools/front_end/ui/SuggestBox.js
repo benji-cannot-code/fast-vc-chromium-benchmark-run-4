ac767ece9edba35059f66d3b0baaabaa9f79cb58
Joel Einbinder <einbinder@chromium.org>
2018-02-07 03:06:33 +0000

    DevTools: Alert screen reader users to autocomplete items
    This uses 'aria-alert' to read out SuggestBox items in StylesSidebar and
    Console, and FilteredListWidget items in the CommandMenu.
    
    Change-Id: I4126e2344ec585adb81f512156bc8506d0ebb70f
    Reviewed-on: https://chromium-review.googlesource.com/867238
    Reviewed-by: Andrey Lushnikov <lushnikov@chromium.org>
    Reviewed-by: Alice Boxhall <aboxhall@chromium.org>
    Commit-Queue: Joel Einbinder <einbinder@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534896}

--- third_party/WebKit/Source/devtools/front_end/ui/SuggestBox.js
+++ third_party/WebKit/Source/devtools/front_end/ui/SuggestBox.js
@@ -169,6 +169,7 @@ UI.SuggestBox = class {
    */
   _applySuggestion(isIntermediateSuggestion) {
     if (this._onlyCompletion) {
+      UI.ARIAUtils.alert(ls`${this._onlyCompletion}, suggestion`, this._element);
       this._suggestBoxDelegate.applySuggestion(this._onlyCompletion, isIntermediateSuggestion);
       return true;
     }
@@ -180,6 +181,7 @@ UI.SuggestBox = class {
     if (!suggestion)
       return false;
 
+    UI.ARIAUtils.alert(ls`${suggestion}, suggestion`, this._element);
     this._suggestBoxDelegate.applySuggestion(suggestion, isIntermediateSuggestion);
     return true;
   }
