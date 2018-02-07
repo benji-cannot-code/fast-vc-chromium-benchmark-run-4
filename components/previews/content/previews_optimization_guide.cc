291d201cef24d2689f2519e665b91b82c1bddf66
Doug Arnett <dougarnett@chromium.org>
2018-02-07 00:38:47 +0000

    Improved per-host data savings estimate for NoScript Previews
    Uses NoScript inflation_percent from optimization guide hints if not 0.
    
    Bug: 807662
    Change-Id: I6478e751bc073c49159098eed37b702e9160641b
    Reviewed-on: https://chromium-review.googlesource.com/895286
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Doug Arnett <dougarnett@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534854}

--- components/previews/content/previews_optimization_guide.cc
+++ components/previews/content/previews_optimization_guide.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task_runner_util.h"
 #include "base/task_scheduler/post_task.h"
 #include "components/optimization_guide/proto/hints.pb.h"
+#include "components/previews/core/previews_user_data.h"
 #include "net/url_request/url_request.h"
 #include "url/gurl.h"
 
@@ -110,8 +111,11 @@ class PreviewsOptimizationGuide::Hints {
       const optimization_guide::proto::Configuration& config,
       const optimization_guide::ComponentInfo& info);
 
-  // Whether the URL is whitelisted for the given previews type.
-  bool IsWhitelisted(const GURL& url, PreviewsType type);
+  // Whether the URL is whitelisted for the given previews type. If so,
+  // |out_inflation_percent| will be populated if meta data available for it.
+  bool IsWhitelisted(const GURL& url,
+                     PreviewsType type,
+                     int* out_inflation_percent);
 
  private:
   Hints();
@@ -120,9 +124,10 @@ class PreviewsOptimizationGuide::Hints {
   // it.
   url_matcher::URLMatcher url_matcher_;
 
-  // A map from the condition set ID to the preview types it is whitelisted by
-  // the server for.
-  std::map<url_matcher::URLMatcherConditionSet::ID, std::set<PreviewsType>>
+  // A map from the condition set ID to associated whitelist Optimization
+  // details.
+  std::map<url_matcher::URLMatcherConditionSet::ID,
+           std::set<std::pair<PreviewsType, int>>>
       whitelist_;
 };
 
@@ -176,11 +181,12 @@ PreviewsOptimizationGuide::Hints::CreateFromConfig(
 
     // Create whitelist condition set out of the optimizations that are
     // whitelisted for the host suffix.
-    std::set<PreviewsType> whitelisted_optimizations;
+    std::set<std::pair<PreviewsType, int>> whitelisted_optimizations;
     for (const auto optimization : hint.whitelisted_optimizations()) {
       if (optimization.optimization_type() ==
           optimization_guide::proto::NOSCRIPT) {
-        whitelisted_optimizations.insert(PreviewsType::NOSCRIPT);
+        whitelisted_optimizations.insert(std::make_pair(
+            PreviewsType::NOSCRIPT, optimization.inflation_percent()));
       }
     }
     url_matcher::URLMatcherCondition condition =
@@ -200,8 +206,10 @@ PreviewsOptimizationGuide::Hints::CreateFromConfig(
   return hints;
 }
 
-bool PreviewsOptimizationGuide::Hints::IsWhitelisted(const GURL& url,
-                                                     PreviewsType type) {
+bool PreviewsOptimizationGuide::Hints::IsWhitelisted(
+    const GURL& url,
+    PreviewsType type,
+    int* out_inflation_percent) {
   std::set<url_matcher::URLMatcherConditionSet::ID> matches =
       url_matcher_.MatchURL(url);
 
@@ -217,8 +225,16 @@ bool PreviewsOptimizationGuide::Hints::IsWhitelisted(const GURL& url,
     return false;
   }
 
-  const auto& whitelisted_previews = whitelist_iter->second;
-  return whitelisted_previews.find(type) != whitelisted_previews.end();
+  const auto& whitelisted_optimizations = whitelist_iter->second;
+  for (auto optimization_iter = whitelisted_optimizations.begin();
+       optimization_iter != whitelisted_optimizations.end();
+       ++optimization_iter) {
+    if (optimization_iter->first == type) {
+      *out_inflation_percent = optimization_iter->second;
+      return true;
+    }
+  }
+  return false;
 }
 
 PreviewsOptimizationGuide::PreviewsOptimizationGuide(
@@ -242,7 +258,16 @@ bool PreviewsOptimizationGuide::IsWhitelisted(const net::URLRequest& request,
   if (!hints_)
     return false;
 
-  return hints_->IsWhitelisted(request.url(), type);
+  int inflation_percent = 0;
+  if (!hints_->IsWhitelisted(request.url(), type, &inflation_percent))
+    return false;
+
+  previews::PreviewsUserData* previews_user_data =
+      previews::PreviewsUserData::GetData(request);
+  if (inflation_percent != 0 && previews_user_data)
+    previews_user_data->SetDataSavingsInflationPercent(inflation_percent);
+
+  return true;
 }
 
 void PreviewsOptimizationGuide::OnHintsProcessed(
--- components/previews/content/previews_optimization_guide_unittest.cc
+++ components/previews/content/previews_optimization_guide_unittest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/optimization_guide_service.h"
 #include "components/optimization_guide/optimization_guide_service_observer.h"
 #include "components/optimization_guide/proto/hints.pb.h"
+#include "components/previews/core/previews_user_data.h"
 #include "net/traffic_annotation/network_traffic_annotation_test_helper.h"
 #include "net/url_request/url_request.h"
 #include "net/url_request/url_request_test_util.h"
@@ -312,6 +313,7 @@ TEST_F(PreviewsOptimizationGuideTest, IsWhitelistedWithMultipleHintMatches) {
   optimization_guide::proto::Optimization* optimization1 =
       hint1->add_whitelisted_optimizations();
   optimization1->set_optimization_type(optimization_guide::proto::NOSCRIPT);
+  optimization1->set_inflation_percent(10);
 
   // No optimizations for sports.yahoo.com:
   optimization_guide::proto::Hint* hint2 = config.add_hints();
@@ -325,6 +327,7 @@ TEST_F(PreviewsOptimizationGuideTest, IsWhitelistedWithMultipleHintMatches) {
   optimization_guide::proto::Optimization* optimization3 =
       hint3->add_whitelisted_optimizations();
   optimization3->set_optimization_type(optimization_guide::proto::NOSCRIPT);
+  optimization3->set_inflation_percent(30);
 
   // No optimizations for mail.yahoo.com:
   optimization_guide::proto::Hint* hint4 = config.add_hints();
@@ -336,7 +339,10 @@ TEST_F(PreviewsOptimizationGuideTest, IsWhitelistedWithMultipleHintMatches) {
 
   std::unique_ptr<net::URLRequest> request1 =
       CreateRequestWithURL(GURL("https://yahoo.com"));
+  previews::PreviewsUserData::Create(request1.get(), 1);
   EXPECT_TRUE(guide()->IsWhitelisted(*request1, PreviewsType::NOSCRIPT));
+  EXPECT_EQ(30, previews::PreviewsUserData::GetData(*request1)
+                    ->data_savings_inflation_percent());
 
   std::unique_ptr<net::URLRequest> request2 =
       CreateRequestWithURL(GURL("https://sports.yahoo.com"));
@@ -345,13 +351,19 @@ TEST_F(PreviewsOptimizationGuideTest, IsWhitelistedWithMultipleHintMatches) {
 
   std::unique_ptr<net::URLRequest> request3 =
       CreateRequestWithURL(GURL("https://mail.yahoo.com"));
+  previews::PreviewsUserData::Create(request3.get(), 3);
   // Uses "yahoo.com" match before "mail.yahoo.com" match.
   EXPECT_TRUE(guide()->IsWhitelisted(*request3, PreviewsType::NOSCRIPT));
+  EXPECT_EQ(30, previews::PreviewsUserData::GetData(*request3)
+                    ->data_savings_inflation_percent());
 
   std::unique_ptr<net::URLRequest> request4 =
       CreateRequestWithURL(GURL("https://indoor.sports.yahoo.com"));
+  previews::PreviewsUserData::Create(request4.get(), 4);
   // Uses "indoor.sports.yahoo.com" match before "sports.yahoo.com" match.
   EXPECT_TRUE(guide()->IsWhitelisted(*request4, PreviewsType::NOSCRIPT));
+  EXPECT_EQ(10, previews::PreviewsUserData::GetData(*request4)
+                    ->data_savings_inflation_percent());
 
   std::unique_ptr<net::URLRequest> request5 =
       CreateRequestWithURL(GURL("https://outdoor.sports.yahoo.com"));
