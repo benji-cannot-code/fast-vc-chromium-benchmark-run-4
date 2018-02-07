280c286003414e9dec4b616aa8419d5240c78bad
Sergey Ulanov <sergeyu@chromium.org>
2018-02-07 20:14:21 +0000

    Add DIR_ASSETS in PathService.
    Currently DIR_MODULE is used when loading assets such as ICU data or .pak
    files. On some platforms these files are stored in a separate directory.
    Fuchsia solves this problem with DIR_FUCHSIA_RESOURCE. Android uses
    DIR_ANDROID_APP_DATA in tests. This approach doesn't scale well because it
    requires platform-specific ifdefs when loading asset files, and this
    defeats the purpose of base::PathServer.
    This change adds cross-platform DIR_ASSETS that can be used on all
    platforms. The values is platform specific:
      Win/Linux/ChromeOS: DIR_ASSETS = DIR_MODULE
      Mac: DIR_ASSETS = <bundle_path>/Resources
                        or DIR_MODULE when not bundled.
      Android: DIR_ASSETS = Undefined since resources are loaded from APK.
                            Overriden in test.
      Fuchsia: DIR_ASSETS = DIR_FUCHSIA_RESOURCE (i.e. /pkg)
                            or DIR_MODULE when not packaged.
    
    Also removed DIR_FUCHSIA_RESOURCE and marked DIR_ANDROID_APP_DATA as
    deprecated.
    
    Bug: 805727, 617734
    Change-Id: Id54305669f786c41c2dde0851daea1504547a866
    Reviewed-on: https://chromium-review.googlesource.com/885002
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535118}

--- base/base_paths_mac.mm
+++ base/base_paths_mac.mm
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/logging.h"
+#include "base/mac/bundle_locations.h"
 #include "base/mac/foundation_util.h"
 #include "base/path_service.h"
 #include "base/strings/string_util.h"
@@ -110,6 +111,13 @@ bool PathProviderMac(int key, base::FilePath* result) {
 #else
       return base::mac::GetUserDirectory(NSDesktopDirectory, result);
 #endif
+    case base::DIR_ASSETS:
+      if (!base::mac::AmIBundled()) {
+        return PathService::Get(base::DIR_MODULE, result);
+      }
+      *result = base::mac::FrameworkBundlePath().Append(
+          FILE_PATH_LITERAL("Resources"));
+      return true;
     case base::DIR_CACHE:
       return base::mac::GetUserDirectory(NSCachesDirectory, result);
     default:
