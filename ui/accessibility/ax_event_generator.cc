6c328a3e1456060d476a054b6fe925a518200eb3
David Tseng <dtseng@chromium.org>
2018-02-07 02:25:01 +0000

    Add hooks for AXTreeDelegate when a relation target changes
    Problem
    Suppose a node a has an active descendant b. A page author might change |b| to aria-selected="true" immediately after setting |b| to be the active descendant.
    As a result, we have the events and actions:
    active descendant changed on |a|
    client gets event on |a| and emits output
    attribute changed on |b|
    client gets attribute changed on |b| but focus remains over |a|
    
    Solution
    
    Let the client know about changes in its related nodes.
    For example, changes to |b| would cause an event on |a|.
    
    Bug: 808061
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I58a3d99d3187db348320223f0bde588015ab6f55
    Reviewed-on: https://chromium-review.googlesource.com/899862
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534885}

--- ui/accessibility/ax_event_generator.cc
+++ ui/accessibility/ax_event_generator.cc
@@ -129,6 +129,9 @@ void AXEventGenerator::OnStateChanged(AXTree* tree,
   AddEvent(node, Event::STATE_CHANGED);
   if (state == ax::mojom::State::kExpanded) {
     AddEvent(node, new_value ? Event::EXPANDED : Event::COLLAPSED);
+
+    // TODO(accessibility): tree in the midst of updates. Disallow access to
+    // |node|.
     if (node->data().role == ax::mojom::Role::kRow ||
         node->data().role == ax::mojom::Role::kTreeItem) {
       ui::AXNode* container = node;
@@ -159,6 +162,9 @@ void AXEventGenerator::OnStringAttributeChanged(AXTree* tree,
   switch (attr) {
     case ax::mojom::StringAttribute::kName:
       AddEvent(node, Event::NAME_CHANGED);
+
+      // TODO(accessibility): tree in the midst of updates. Disallow
+      // access to |node|.
       if (node->data().HasStringAttribute(
               ax::mojom::StringAttribute::kContainerLiveStatus)) {
         FireLiveRegionEvents(node);
@@ -174,6 +180,8 @@ void AXEventGenerator::OnStringAttributeChanged(AXTree* tree,
       AddEvent(node, Event::INVALID_STATUS_CHANGED);
       break;
     case ax::mojom::StringAttribute::kLiveStatus:
+      // TODO(accessibility): tree in the midst of updates. Disallow access to
+      // |node|.
       if (node->data().role != ax::mojom::Role::kAlert)
         AddEvent(node, Event::LIVE_REGION_CREATED);
       break;
@@ -329,6 +337,8 @@ void AXEventGenerator::OnAtomicUpdateFinished(
         FireLiveRegionEvents(change.node);
       }
     }
+
+    FireRelationSourceEvents(tree, change.node);
   }
 
   FireActiveDescendantEvents();
@@ -370,6 +380,31 @@ void AXEventGenerator::FireActiveDescendantEvents() {
     }
   }
   active_descendant_changed_.clear();
+}
+
+void AXEventGenerator::FireRelationSourceEvents(AXTree* tree,
+                                                AXNode* target_node) {
+  int32_t target_id = target_node->id();
+  auto callback = [&](const auto entry) {
+    const auto target_to_sources = entry.second;
+    auto sources_it = target_to_sources.find(target_id);
+    if (sources_it == target_to_sources.end())
+      return;
+
+    auto sources = sources_it->second;
+    std::for_each(sources.begin(), sources.end(), [&](int32_t source_id) {
+      AXNode* source_node = tree->GetFromId(source_id);
+      if (!source_node)
+        return;
+
+      AddEvent(source_node, Event::RELATED_NODE_CHANGED);
+    });
+  };
+
+  std::for_each(tree->int_reverse_relations().begin(),
+                tree->int_reverse_relations().end(), callback);
+  std::for_each(tree->intlist_reverse_relations().begin(),
+                tree->intlist_reverse_relations().end(), callback);
 }
 
 }  // namespace ui
