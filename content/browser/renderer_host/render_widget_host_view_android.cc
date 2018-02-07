77b7e504cfa42cf3970452fa3d21dcbb46eabfbe
Navid Zolghadr <nzolghadr@chromium.org>
2018-02-07 15:59:05 +0000

    Indicate whether a WebTouchEvent is hovering or not
    When pen is touching the digitizer we send touch events
    instead of mouse events to get the direct manipulation
    behavior such as scroll on drag. However, as oppose to
    normal touch when pointerup (or touchend) happens the
    pen might still be in the range of the digitizer and
    Blink shouldn't just send the out/leave events right
    after the up event. So this CL comminucates this
    information from the browser to Blink so that it only
    sends boundary events after up event when it does not
    support hover.
    
    Bug: 805011
    Change-Id: Ifab24950e22a55835ed069955228236d2ec84c9b
    Reviewed-on: https://chromium-review.googlesource.com/893802
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Rick Byers <rbyers@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Navid Zolghadr <nzolghadr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535015}

--- content/browser/renderer_host/render_widget_host_view_android.cc
+++ content/browser/renderer_host/render_widget_host_view_android.cc
@@ -965,7 +965,8 @@ bool RenderWidgetHostViewAndroid::OnTouchEvent(
     return false;
 
   blink::WebTouchEvent web_event = ui::CreateWebTouchEventFromMotionEvent(
-      event, result.moved_beyond_slop_region);
+      event, result.moved_beyond_slop_region /* may_cause_scrolling */,
+      false /* hovering */);
   if (web_event.GetType() == blink::WebInputEvent::kUndefined)
     return false;
 
@@ -1016,8 +1017,9 @@ void RenderWidgetHostViewAndroid::ResetGestureDetection() {
     bool causes_scrolling = false;
     ui::LatencyInfo latency_info(ui::SourceEventType::TOUCH);
     latency_info.AddLatencyNumber(ui::INPUT_EVENT_LATENCY_UI_COMPONENT, 0, 0);
-    blink::WebTouchEvent web_event =
-        ui::CreateWebTouchEventFromMotionEvent(*cancel_event, causes_scrolling);
+    blink::WebTouchEvent web_event = ui::CreateWebTouchEventFromMotionEvent(
+        *cancel_event, causes_scrolling /* may_cause_scrolling */,
+        false /* hovering */);
     if (host_->delegate()->GetInputEventRouter()) {
       host_->delegate()->GetInputEventRouter()->RouteTouchEvent(
           this, &web_event, latency_info);
