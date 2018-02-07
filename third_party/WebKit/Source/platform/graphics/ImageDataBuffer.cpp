2fe2054db91b3aec70e05a0d2fb178f300e7820c
Reza.Zakerinasab <zakerinasab@chromium.org>
2018-02-07 18:56:39 +0000

    HTMLCanvasElement: Add color management to toDataURL() and toBlob()
    This change adds color management support to HTMLCanvasElement::toDataURL()
    and HTMLCanvasElement::toBlob(). This is a retry after
    chromium-review.googlesource.com/c/chromium/src/+/771541 got abandoned because
    ImageData was removed from toBlob/toDataURL code path.
    
    Bug: 782278
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I4018aa4ca9ffc53d5c5671c804964dd0ce58ee68
    Reviewed-on: https://chromium-review.googlesource.com/824462
    Commit-Queue: Mohammad Reza Zakerinasab <zakerinasab@chromium.org>
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535081}

--- third_party/WebKit/Source/platform/graphics/ImageDataBuffer.cpp
+++ third_party/WebKit/Source/platform/graphics/ImageDataBuffer.cpp
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/graphics/StaticBitmapImage.h"
 #include "platform/image-encoders/ImageEncoder.h"
 #include "platform/network/mime/MIMETypeRegistry.h"
+#include "platform/runtime_enabled_features.h"
 #include "platform/wtf/Vector.h"
 #include "platform/wtf/text/Base64.h"
 #include "platform/wtf/text/WTFString.h"
@@ -44,12 +45,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/encode/SkJpegEncoder.h"
 
 namespace blink {
-
-const unsigned char* ImageDataBuffer::Pixels() const {
-  if (uses_pixmap_)
-    return static_cast<const unsigned char*>(pixmap_.addr());
-  return data_;
-}
 
 std::unique_ptr<ImageDataBuffer> ImageDataBuffer::Create(
     scoped_refptr<StaticBitmapImage> image) {
@@ -83,11 +78,13 @@ bool ImageDataBuffer::EncodeImage(const String& mime_type,
   if (uses_pixmap_) {
     src = pixmap_;
   } else {
-    SkImageInfo info =
-        SkImageInfo::Make(Width(), Height(), kRGBA_8888_SkColorType,
-                          kUnpremul_SkAlphaType, nullptr);
-    const size_t rowBytes = info.minRowBytes();
-    src.reset(info, Pixels(), rowBytes);
+    SkColorType color_type = kRGBA_8888_SkColorType;
+    if (color_params_.GetSkColorType() != kN32_SkColorType)
+      color_type = kRGBA_F16_SkColorType;
+    SkImageInfo info = SkImageInfo::Make(size_.Width(), size_.Height(),
+                                         color_type, kUnpremul_SkAlphaType,
+                                         color_params_.GetSkColorSpace());
+    src.reset(info, data_, info.minRowBytes());
   }
 
   if (mime_type == "image/jpeg") {
