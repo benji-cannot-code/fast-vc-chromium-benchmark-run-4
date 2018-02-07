ddf590b31ef27b09d9a49d724b574575be6551b9
Elad Alon <eladalon@chromium.org>
2018-02-07 16:54:31 +0000

    Revert "Revert "Introduce WebRtcRemoteEventLogManager""
    This reverts commit c318f63aff3137cf98d2d29ab5f017ad67cb6322.
    
    Reason for revert: Build break fixed (see delta between PS1 and last PS).
    
    Original change's description:
    > Revert "Introduce WebRtcRemoteEventLogManager"
    >
    > This reverts commit 036ea24be3257b22c860a147e36f4c5829009cdd.
    >
    > Reason for revert: Causing build failure on Cast Audio Linux:
    > https://ci.chromium.org/buildbot/chromium.linux/Cast%20Audio%20Linux/8575
    >
    > Original change's description:
    > > Introduce WebRtcRemoteEventLogManager
    > >
    > > WebRtcRemoteEventLogManager is a member of WebRtcEventLogManager, in
    > > charge of managing remote-bound WebRTC event logs:
    > > * Store remote-bound log files locally, persisting them between session
    > >   of Chrome.
    > > * Uploading them when permissible (when no active peer connections are
    > >   present, with which the upload's increased bandwidth utilization might
    > >   interfere).
    > > * Purge remote-bound log files from local disk once an upload attempt
    > >   was made, regardless of whether it was successful. (Otherwise, we might
    > >   end up attempting multiple times, wasting bandwidth on an upload that
    > >   always fails after a non-trivial amount of data was pushed upstream.
    > >   More refined retry behavior is to be added at a later stage.)
    > >
    > > Upcoming related work:
    > > * Actual implementation of the uploader (this CL deals with managing the
    > >   files to be uploaded, and the timing of the upload).
    > > * Implementation of the JS hooks.
    > >
    > > Bug: 775415
    > > Change-Id: Ib5ee6081ca13d85cb8a3f38d6462bbd61a99dcae
    > > Reviewed-on: https://chromium-review.googlesource.com/891219
    > > Commit-Queue: Elad Alon <eladalon@chromium.org>
    > > Reviewed-by: Avi Drissman <avi@chromium.org>
    > > Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#534826}
    >
    > Change-Id: I2bc87f1002eac26f4c77bc5082be45db80b0449d
    > Bug: 775415
    > Reviewed-on: https://chromium-review.googlesource.com/905826
    > Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    > Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#534847}
    
    Change-Id: I84aadb92c7878a27bcd1d50d3799cd2e626a5521
    Bug: 775415
    Reviewed-on: https://chromium-review.googlesource.com/904905
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Elad Alon <eladalon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535030}

--- content/browser/browser_context.cc
+++ content/browser/browser_context.cc
@@ -58,6 +58,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "storage/browser/database/database_tracker.h"
 #include "storage/browser/fileapi/external_mount_points.h"
 
+#if BUILDFLAG(ENABLE_WEBRTC)
+#include "content/browser/webrtc/webrtc_event_log_manager.h"
+#endif
+
 using base::UserDataAdapter;
 
 namespace content {
@@ -446,7 +450,6 @@ void BrowserContext::SetDownloadManagerForTesting(
 void BrowserContext::Initialize(
     BrowserContext* browser_context,
     const base::FilePath& path) {
-
   std::string new_id;
   if (GetContentClient() && GetContentClient()->browser()) {
     new_id = GetContentClient()->browser()->GetServiceUserIdForBrowserContext(
@@ -512,6 +515,15 @@ void BrowserContext::Initialize(
     RegisterCommonBrowserInterfaces(connection);
     connection->Start();
   }
+
+#if BUILDFLAG(ENABLE_WEBRTC)
+  if (!browser_context->IsOffTheRecord()) {
+    auto* webrtc_event_log_manager = WebRtcEventLogManager::GetInstance();
+    if (webrtc_event_log_manager) {
+      webrtc_event_log_manager->EnableForBrowserContext(browser_context);
+    }
+  }
+#endif
 }
 
 // static
@@ -561,6 +573,14 @@ BrowserContext::~BrowserContext() {
 
   DCHECK(!GetUserData(kStoragePartitionMapKeyName))
       << "StoragePartitionMap is not shut down properly";
+
+#if BUILDFLAG(ENABLE_WEBRTC)
+  auto* webrtc_event_log_manager = WebRtcEventLogManager::GetInstance();
+  if (webrtc_event_log_manager) {
+    const auto id = WebRtcEventLogManager::GetBrowserContextId(this);
+    webrtc_event_log_manager->DisableForBrowserContext(id);
+  }
+#endif
 
   RemoveBrowserContextFromUserIdMap(this);
 
--- content/public/test/test_browser_context.cc
+++ content/public/test/test_browser_context.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/files/file_path.h"
+#include "base/logging.h"
 #include "base/single_thread_task_runner.h"
 #include "base/test/null_task_runner.h"
 #include "content/public/browser/permission_manager.h"
@@ -46,8 +47,13 @@ class TestContextURLRequestContextGetter : public net::URLRequestContextGetter {
 
 namespace content {
 
-TestBrowserContext::TestBrowserContext() {
-  EXPECT_TRUE(browser_context_dir_.CreateUniqueTempDir());
+TestBrowserContext::TestBrowserContext(
+    base::FilePath browser_context_dir_path) {
+  if (browser_context_dir_path.empty()) {
+    EXPECT_TRUE(browser_context_dir_.CreateUniqueTempDir());
+  } else {
+    EXPECT_TRUE(browser_context_dir_.Set(browser_context_dir_path));
+  }
   BrowserContext::Initialize(this, browser_context_dir_.GetPath());
 }
 
