8d7406c5cab2720207d7ba21a87df0aceb07d094
Greg Thompson <grt@chromium.org>
2018-02-07 21:26:52 +0000

    RelaunchNotification and RelaunchNotificationPeriod policy settings.
    BUG=511300
    
    Change-Id: I9f75ac645097a3333027c433db2e02d869d7fd9a
    Reviewed-on: https://chromium-review.googlesource.com/891820
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535146}

--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -734,6 +734,13 @@ extern const char kShowCastIconInToolbar[];
 #endif  // !defined(OS_ANDROID)
 
 #if !defined(OS_ANDROID)
+#if !defined(OS_CHROMEOS)
+extern const char kRelaunchNotification[];
+#endif  // !defined(OS_CHROMEOS)
+extern const char kRelaunchNotificationPeriod[];
+#endif  // !defined(OS_ANDROID)
+
+#if !defined(OS_ANDROID)
 extern const char kAttemptedToEnableAutoupdate[];
 
 extern const char kMediaGalleriesUniqueId[];
