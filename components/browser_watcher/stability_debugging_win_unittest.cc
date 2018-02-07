82fd240c134a909e6bfde5da7756d69ac1cf7318
Hans Wennborg <hans@chromium.org>
2018-02-07 08:49:29 +0000

    Disable StabilityDebuggingTest.CrashingTest under WinASan
    Bug: 809524
    Change-Id: Idb19580820a149899613ec2e4b0875ff1a8d2b21
    Reviewed-on: https://chromium-review.googlesource.com/905306
    Reviewed-by: Reid Kleckner <rnk@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534952}

--- components/browser_watcher/stability_debugging_win_unittest.cc
+++ components/browser_watcher/stability_debugging_win_unittest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/process/process.h"
 #include "base/test/multiprocess_test.h"
 #include "base/test/test_timeouts.h"
+#include "build/build_config.h"
 #include "components/browser_watcher/stability_report.pb.h"
 #include "components/browser_watcher/stability_report_extractor.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -56,7 +57,13 @@ class StabilityDebuggingTest : public testing::Test {
   base::FilePath debug_path_;
 };
 
-TEST_F(StabilityDebuggingTest, CrashingTest) {
+#if defined(ADDRESS_SANITIZER) && defined(OS_WIN)
+// The test does not pass under WinASan. See crbug.com/809524.
+#define MAYBE_CrashingTest DISABLED_CrashingTest
+#else
+#define MAYBE_CrashingTest CrashingTest
+#endif
+TEST_F(StabilityDebuggingTest, MAYBE_CrashingTest) {
   RegisterStabilityVEH();
 
   // Raise an exception, then continue.
