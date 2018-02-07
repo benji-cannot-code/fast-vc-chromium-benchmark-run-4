314f2472d3341dcaaf16ccdecb30a2827bed5b6a
Torne (Richard Coles) <torne@google.com>
2018-02-07 18:48:30 +0000

    Work around font preloading bug in Android O framework.
    Use reflection during Application.onCreate to wrap the package manager
    and strip out font preloading information when the query originates in a
    Chromium isolated process. This prevents the framework from crashing due
    to a bug in the case where an application that uses WebView has
    preloaded fonts specified in its own manifest.
    
    Bug: 809636
    Change-Id: Ife1d647f1dfd096b79775f8da2249661ac67e958
    Reviewed-on: https://chromium-review.googlesource.com/905106
    Reviewed-by: Tobias Sargeant <tobiasjs@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535073}

--- chrome/android/java/src/org/chromium/chrome/browser/MonochromeApplication.java
+++ chrome/android/java/src/org/chromium/chrome/browser/MonochromeApplication.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser;
 
+import com.android.webview.chromium.FontPreloadingWorkaround;
 import com.android.webview.chromium.MonochromeLibraryPreloader;
 
 import org.chromium.base.library_loader.LibraryLoader;
@@ -22,6 +23,7 @@ public class MonochromeApplication extends ChromeApplication {
     @Override
     public void onCreate() {
         super.onCreate();
+        FontPreloadingWorkaround.maybeInstallWorkaround(this);
         LibraryLoader.setNativeLibraryPreloader(new MonochromeLibraryPreloader());
         // ChildProcessCreationParams is only needed for browser process, though it is
         // created and set in all processes.
