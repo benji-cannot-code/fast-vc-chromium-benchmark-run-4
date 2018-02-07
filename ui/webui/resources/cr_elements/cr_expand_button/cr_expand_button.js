b1e052c6a80d55e1a0897085eb90a4474047a727
Nektarios Paisios <nektar@chromium.org>
2018-02-07 04:24:18 +0000

    Correctly reports the expanded state for cr_expand_buttons to accessibility
    I prefer aria-pressed instead of aria-expanded because Windows screen readers report the button as a toggle button, which is what it actually is - look at the "toggle" attribute on the button.
    R=dmazzoni@chromium.org
    
    Tested: Manually with Jaws
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I7dfe5bfb3b303893ae5b70126deb297e051c43a3
    Reviewed-on: https://chromium-review.googlesource.com/905109
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534914}

--- ui/webui/resources/cr_elements/cr_expand_button/cr_expand_button.js
+++ ui/webui/resources/cr_elements/cr_expand_button/cr_expand_button.js
@@ -45,4 +45,9 @@ Polymer({
     this.expanded = !this.expanded;
     event.stopPropagation();
   },
+
+  /** @private */
+  getAriaPressed_: function(expanded) {
+    return expanded ? 'true' : 'false';
+  },
 });
