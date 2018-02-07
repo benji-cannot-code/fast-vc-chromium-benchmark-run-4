29b03c18178b168a90ee5960a0709348b8a207b1
Kent Tamura <tkent@chromium.org>
2018-02-07 06:49:29 +0000

    custom-elements: Fold CustomElement::CreateCustomElement() and CustomElementDefinition::CreateElementAsync().
    The merged function is CustomElementDefinition::CreateElement(). This CL
    makes our code very close to the algorithm description in DOM standard.
    
    Also, rename CustomElementDefinition::CreateElementSync() to
    CreateAutonomousCustomElementSync().
    
    This CL has no behavior changes.
    
    Bug: 806641
    Change-Id: I5edb770246f476b9632b7226c3d9b2f6989fa21c
    Reviewed-on: https://chromium-review.googlesource.com/906045
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534938}

--- third_party/WebKit/Source/bindings/core/v8/ScriptCustomElementDefinition.h
+++ third_party/WebKit/Source/bindings/core/v8/ScriptCustomElementDefinition.h
@@ -47,7 +47,8 @@ class CORE_EXPORT ScriptCustomElementDefinition final
 
   v8::Local<v8::Object> Constructor() const;
 
-  HTMLElement* CreateElementSync(Document&, const QualifiedName&) override;
+  HTMLElement* CreateAutonomousCustomElementSync(Document&,
+                                                 const QualifiedName&) override;
 
   bool HasConnectedCallback() const override;
   bool HasDisconnectedCallback() const override;
--- third_party/WebKit/Source/core/html/custom/CustomElementTestHelpers.h
+++ third_party/WebKit/Source/core/html/custom/CustomElementTestHelpers.h
@@ -63,8 +63,9 @@ class TestCustomElementDefinition : public CustomElementDefinition {
     return true;
   }
 
-  HTMLElement* CreateElementSync(Document& document,
-                                 const QualifiedName&) override {
+  HTMLElement* CreateAutonomousCustomElementSync(
+      Document& document,
+      const QualifiedName&) override {
     return CreateElementForConstructor(document);
   }
 
