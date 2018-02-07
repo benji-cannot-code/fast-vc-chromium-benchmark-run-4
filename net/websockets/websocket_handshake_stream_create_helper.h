1cd8990e8aa27961d173a36aae32e0e201bf373a
Bence Béky <bnc@chromium.org>
2018-02-07 12:48:15 +0000

    Remove fake WebSocket test classes.
    Create DummyConnectDelegate and DummyWebSocketStreamRequest.  Rename
    DeterministicKeyWebSocketHandshakeStreamCreateHelper to
    TestWebSocketHandshakeStreamCreateHelper, and add a constructor to use
    these dummy classes.
    
    Remove FakeWebSocketHandshakeStreamBase and its derived classes
    FakeWebSocketHandshakeStream and AsyncFakeWebSocketHandshakeStream.
    Also remove FakeWebSocketStreamCreateHelper that hands out
    FakeWebSocketHandshakeStream instances.  Convert five tests to use
    TestWebSocketHandshakeStreamCreateHelper, which is based on the real
    WebSocketHandshakeStreamCreateHelper, and add/modify mock data
    accordingly.
    
    This change is not directly related to but done in preparation for
    implementing WebSockets over HTTP/2.
    
    Bug: 801564
    Change-Id: I7a9fee80f0ce6b4dccf59f62570e52b5a64131f8
    Reviewed-on: https://chromium-review.googlesource.com/904823
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Bence Béky <bnc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534986}

--- net/websockets/websocket_handshake_stream_create_helper.h
+++ net/websockets/websocket_handshake_stream_create_helper.h
@@ -52,7 +52,7 @@ class NET_EXPORT_PRIVATE WebSocketHandshakeStreamCreateHelper
   }
 
  protected:
-  // This is used by DeterministicKeyWebSocketHandshakeStreamCreateHelper.
+  // This is used by TestWebSocketHandshakeStreamCreateHelper.
   // The default implementation does nothing.
   virtual void OnBasicStreamCreated(WebSocketBasicHandshakeStream* stream);
 
--- net/websockets/websocket_stream_create_test_base.cc
+++ net/websockets/websocket_stream_create_test_base.cc
@@ -24,26 +24,6 @@ namespace net {
 
 using HeaderKeyValuePair = WebSocketStreamCreateTestBase::HeaderKeyValuePair;
 
-// A sub-class of WebSocketHandshakeStreamCreateHelper which always sets a
-// deterministic key to use in the WebSocket handshake.
-class DeterministicKeyWebSocketHandshakeStreamCreateHelper
-    : public WebSocketHandshakeStreamCreateHelper {
- public:
-  DeterministicKeyWebSocketHandshakeStreamCreateHelper(
-      WebSocketStream::ConnectDelegate* connect_delegate,
-      const std::vector<std::string>& requested_subprotocols)
-      : WebSocketHandshakeStreamCreateHelper(connect_delegate,
-                                             requested_subprotocols) {}
-
-  void OnBasicStreamCreated(WebSocketBasicHandshakeStream* stream) override {
-    stream->SetWebSocketKeyForTesting("dGhlIHNhbXBsZSBub25jZQ==");
-  }
-
- private:
-  DISALLOW_COPY_AND_ASSIGN(
-      DeterministicKeyWebSocketHandshakeStreamCreateHelper);
-};
-
 class WebSocketStreamCreateTestBase::TestConnectDelegate
     : public WebSocketStream::ConnectDelegate {
  public:
@@ -115,9 +95,9 @@ void WebSocketStreamCreateTestBase::CreateAndConnectStream(
   std::unique_ptr<WebSocketStream::ConnectDelegate> connect_delegate(
       new TestConnectDelegate(this, connect_run_loop_.QuitClosure()));
   WebSocketStream::ConnectDelegate* delegate = connect_delegate.get();
-  std::unique_ptr<WebSocketHandshakeStreamCreateHelper> create_helper(
-      new DeterministicKeyWebSocketHandshakeStreamCreateHelper(delegate,
-                                                               sub_protocols));
+  auto create_helper =
+      std::make_unique<TestWebSocketHandshakeStreamCreateHelper>(delegate,
+                                                                 sub_protocols);
   stream_request_ = WebSocketStream::CreateAndConnectStreamForTesting(
       socket_url, std::move(create_helper), origin, site_for_cookies,
       additional_headers, url_request_context_host_.GetURLRequestContext(),
