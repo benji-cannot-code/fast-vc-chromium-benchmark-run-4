0b008fc60bf1ef7c296c0739ea79b796cdb9981b
Corentin Wallez <cwallez@chromium.org>
2018-02-07 07:48:20 +0000

    Support --use-gl=angle on macOS
    This allows running Chrome on top of ANGLE's EGL and OpenGL ES2
    implementation on macOS via the EGL_ANGLE_iosurface_client_buffer that
    allows creating EGL pbuffers from iosurfaces and binding them to
    textures.
    
    A use_egl_on_mac flag is introduced that enables support for EGL on
    macOS builds, and move the ANGLE libraries in the correct place in the
    Chromium.app bundle. This flag is disabled by default.
    
    GLImageIOSurfaceEGL is introduced that subclasses GLImageIOSurface and
    has a new code path to bind IOSurfaces to textures using the ANGLE
    extension.
    
    The changes don't add support for Swiftshader as it doesn't implement
    the EGL_ANGLE_iosurface_client_buffer extension yet.
    
    BUG=chromium:757974
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I2a819d1780415ac144d94e1fdad6030d64d6f8c5
    Reviewed-on: https://chromium-review.googlesource.com/558810
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534948}

--- gpu/ipc/service/image_transport_surface_mac.mm
+++ gpu/ipc/service/image_transport_surface_mac.mm
@@ -54,6 +54,7 @@ scoped_refptr<gl::GLSurface> ImageTransportSurface::CreateNativeSurface(
     case gl::kGLImplementationDesktopGL:
     case gl::kGLImplementationDesktopGLCoreProfile:
     case gl::kGLImplementationAppleGL:
+    case gl::kGLImplementationEGLGLES2:
       return base::WrapRefCounted<gl::GLSurface>(
           new ImageTransportSurfaceOverlayMac(delegate));
     case gl::kGLImplementationMockGL:
