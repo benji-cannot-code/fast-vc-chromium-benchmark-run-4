ef7ef6a201f179e7a2129ab4aed8224c82653751
Devlin Cronin <rdevlin.cronin@chromium.org>
2018-02-07 17:59:49 +0000

    [Extensions Bindings] Add native bindings for the i18n API
    Add native bindings for the i18n API. This is the last API that's
    exposed to content scripts that hasn't been converted to support native
    bindings; with this, content scripts shouldn't require any JS binding
    support.
    
    Extract out common functionality between i18n_custom_bindings.cc and
    the new i18n_hooks_delegate into a shared i18n_hooks_util file. Add
    unittests for the new hooks delegate, which also exercises the
    shared methods.
    
    Finally, update the GetMessage handling to not crash if an error is
    thrown in string conversions.
    
    Bug: 653596, 807771
    
    Change-Id: I8b9fd5e746298699d589b3aa4d3ef7364488eac7
    Reviewed-on: https://chromium-review.googlesource.com/896303
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535054}

--- extensions/renderer/native_extension_bindings_system.cc
+++ extensions/renderer/native_extension_bindings_system.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/renderer/extension_frame_helper.h"
 #include "extensions/renderer/extension_js_runner.h"
 #include "extensions/renderer/get_script_context.h"
+#include "extensions/renderer/i18n_hooks_delegate.h"
 #include "extensions/renderer/ipc_message_sender.h"
 #include "extensions/renderer/module_system.h"
 #include "extensions/renderer/runtime_hooks_delegate.h"
@@ -358,6 +359,8 @@ NativeExtensionBindingsSystem::NativeExtensionBindingsSystem(
       ->SetDelegate(std::make_unique<WebRequestHooks>());
   api_system_.GetHooksForAPI("declarativeContent")
       ->SetDelegate(std::make_unique<DeclarativeContentHooksDelegate>());
+  api_system_.GetHooksForAPI("i18n")->SetDelegate(
+      std::make_unique<I18nHooksDelegate>());
   api_system_.GetHooksForAPI("runtime")->SetDelegate(
       std::make_unique<RuntimeHooksDelegate>(&messaging_service_));
 }
