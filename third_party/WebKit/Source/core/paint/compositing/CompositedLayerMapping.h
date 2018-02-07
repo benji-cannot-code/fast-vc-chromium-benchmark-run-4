68a1a907798611dc84167bd3b98bc848e3c660b7
Xianzhu Wang <wangxianzhu@chromium.org>
2018-02-07 04:18:29 +0000

    [SPv175] Don't paint GraphicsLayer if the containing frame is throttled
    The crash happens in the following sequence:
    1. A frame is throttled (CanThrottleRendering is true)
    2. Without throttling enabled (ShouldThrottleRendering is false),
       lifecycle is updated to CompositingClean, thus GraphicsLayer for the
       throttled frame is created.
    3. With throttling enabled (ShouldThrottleRendering is true),
       full lifecycle is updated. We skipped setting the GraphicsLayer's
       layer state because the containing frame is throttled.
    4. During PaintTree, when painting the GraphicsLayer, layer state is
       unexpectedly null.
    
    Now skip painting the GraphicsLayer if the containing frame is throttled.
    
    Bug: 805049
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_layout_ng;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Iec7ebbc0362b2353513bcd8f44f864307683eb28
    Reviewed-on: https://chromium-review.googlesource.com/900262
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534912}

--- third_party/WebKit/Source/core/paint/compositing/CompositedLayerMapping.h
+++ third_party/WebKit/Source/core/paint/compositing/CompositedLayerMapping.h
@@ -224,7 +224,7 @@ class CORE_EXPORT CompositedLayerMapping final : public GraphicsLayerClient {
                      GraphicsContext&,
                      GraphicsLayerPaintingPhase,
                      const IntRect& interest_rect) const override;
-
+  bool ShouldThrottleRendering() const override;
   bool IsTrackingRasterInvalidations() const override;
 
 #if DCHECK_IS_ON()
