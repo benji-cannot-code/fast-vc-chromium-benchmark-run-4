8e7a382c5961d332af2714eefb64bc334ee83f43
Noel Gordon <noel@chromium.org>
2018-02-07 09:33:01 +0000

    Remove x86_check_features() from inflate
    adler32 can be freely called like crc32, per the "zlib.h" docs
    so move the x86 feature check there, which allows us to remove
    x86_check_features() from zlib inflate. Note inflate() calls
    
      adler32(0, Z_NULL, 0);
    
    (once only) before using the zlib adler32() routine. A reading
    from third_party/libpng png.c
    
      2324:  /* Now calculate the adler32 if not done already. */
      2327:     adler = adler32(0, NULL, 0);
      2328:     adler = adler32(adler, profile, length);
    
    suggests they also conform to the "zlib.h" doc rules about use
    of the adler32 routine.
    
    Bug: 762564
    Change-Id: Id84da0eb52bc10edb541a284eab9aef652ba2c72
    Reviewed-on: https://chromium-review.googlesource.com/901043
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Reviewed-by: Mike Klein <mtklein@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534964}

--- third_party/zlib/contrib/optimizations/inflate.c
+++ third_party/zlib/contrib/optimizations/inflate.c
@@ -86,7 +86,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "inflate.h"
 #include "contrib/optimizations/inffast_chunk.h"
 #include "contrib/optimizations/chunkcopy.h"
-#include "x86.h"
 
 #ifdef MAKEFIXED
 #  ifndef BUILDFIXED
@@ -203,8 +202,6 @@ int stream_size;
 {
     int ret;
     struct inflate_state FAR *state;
-
-    x86_check_features();
 
     if (version == Z_NULL || version[0] != ZLIB_VERSION[0] ||
         stream_size != (int)(sizeof(z_stream)))
