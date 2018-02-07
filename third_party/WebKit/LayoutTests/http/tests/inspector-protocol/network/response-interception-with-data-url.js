694347a52c98351881613b83e40d27eed7e65c71
Andrey Kosyakov <caseq@chromium.org>
2018-02-07 02:09:09 +0000

    DevTools: emit requestWillBeSent & responseReceived for nav requests from browser
    This moves the instrumentation for Network.{requestWillBeSent,responseReceived,loadingFailed}
    to browser and prevents renderer from reporting these. The dataReceived and loadingFinished
    events are still reported from the renderer.
    
    TBR=alexclarke
    
    Bug: 791838
    Change-Id: I6f7ce1f0c243342ad15cb1bd6290c0f68a0da647
    Reviewed-on: https://chromium-review.googlesource.com/861952
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534878}

--- third_party/WebKit/LayoutTests/http/tests/inspector-protocol/network/response-interception-with-data-url.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector-protocol/network/response-interception-with-data-url.js
@@ -26,6 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   var requestId = '';
   session.protocol.Network.onRequestWillBeSent(event => {
+    if (!event.params.documentURL.startsWith('data:'))
+      return;
     if (requestId)
       throw "requestId already set";
     requestId = event.params.requestId;
