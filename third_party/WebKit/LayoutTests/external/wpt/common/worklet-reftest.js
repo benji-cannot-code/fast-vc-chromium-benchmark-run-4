df2834a155a4fcfadf32a6fcd60f5617efdde93d
Ian Kilpatrick <ikilpatrick@chromium.org>
2018-02-07 18:27:40 +0000

    [css-layout-api][css-paint-api] Generalize paintWorklet test runner to work with layoutWorklet.
    This should have no behaviour change.
    
    This preparation for some CSS.layoutWorklet reftests.
    
    Change-Id: I0f270a0e60aeadb367abe0ab7ef34e58b3d65096
    Reviewed-on: https://chromium-review.googlesource.com/907028
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535067}

--- third_party/WebKit/LayoutTests/external/wpt/common/worklet-reftest.js
+++ third_party/WebKit/LayoutTests/external/wpt/common/worklet-reftest.js
@@ -2,12 +2,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // To make sure that we take the snapshot at the right time, we do double
 // requestAnimationFrame. In the second frame, we take a screenshot, that makes
 // sure that we already have a full frame.
-function importPaintWorkletAndTerminateTestAfterAsyncPaint(code) {
-    if (typeof CSS.paintWorklet == "undefined") {
+function importWorkletAndTerminateTestAfterAsyncPaint(worklet, code) {
+    if (typeof worklet == "undefined") {
         takeScreenshot();
     } else {
         var blob = new Blob([code], {type: 'text/javascript'});
-        CSS.paintWorklet.addModule(URL.createObjectURL(blob)).then(function() {
+        worklet.addModule(URL.createObjectURL(blob)).then(function() {
             requestAnimationFrame(function() {
                 requestAnimationFrame(function() {
                     takeScreenshot();
