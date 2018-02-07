e983fc35e03cbe5a8f6d8d79536be31fe23d8b01
Nick Harper <nharper@chromium.org>
2018-02-07 03:47:07 +0000

    Refactor QuicConnectionVisitorInterface::OnSuccessfulVersionNegotiation
    This is a merge of internal change 184350463
    
    Change-Id: Ifca239fb8f9941cb718baba9e01f3d22ba1c3918
    Reviewed-on: https://chromium-review.googlesource.com/900486
    Commit-Queue: Buck Krasic <ckrasic@chromium.org>
    Reviewed-by: Buck Krasic <ckrasic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534905}

--- net/quic/chromium/quic_chromium_client_session.cc
+++ net/quic/chromium/quic_chromium_client_session.cc
@@ -1531,8 +1531,8 @@ void QuicChromiumClientSession::OnConnectionClosed(
 }
 
 void QuicChromiumClientSession::OnSuccessfulVersionNegotiation(
-    const QuicTransportVersion& version) {
-  logger_->OnSuccessfulVersionNegotiation(version);
+    const ParsedQuicVersion& version) {
+  logger_->OnSuccessfulVersionNegotiation(version.transport_version);
   QuicSpdySession::OnSuccessfulVersionNegotiation(version);
 }
 
--- net/quic/chromium/quic_chromium_client_session.h
+++ net/quic/chromium/quic_chromium_client_session.h
@@ -449,7 +449,7 @@ class NET_EXPORT_PRIVATE QuicChromiumClientSession
                           const std::string& error_details,
                           ConnectionCloseSource source) override;
   void OnSuccessfulVersionNegotiation(
-      const QuicTransportVersion& version) override;
+      const ParsedQuicVersion& version) override;
   void OnConnectivityProbeReceived(
       const QuicSocketAddress& self_address,
       const QuicSocketAddress& peer_address) override;
