748789f3ec8f928626fff643c313c9a75fd35712
Ted Choc <tedchoc@google.com>
2018-02-07 18:00:55 +0000

    [Android] Check for FEATURE_APP_WIDGETS before triggering widget code.
    BUG=804983
    
    Change-Id: I2551108ac4d2b368c1c60713aee42bac16de59b5
    Reviewed-on: https://chromium-review.googlesource.com/905802
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Commit-Queue: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535056}

--- chrome/android/java/src/org/chromium/chrome/browser/searchwidget/SearchWidgetProvider.java
+++ chrome/android/java/src/org/chromium/chrome/browser/searchwidget/SearchWidgetProvider.java
@@ -13,6 +13,7 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.StrictMode;
@@ -59,7 +60,12 @@ public class SearchWidgetProvider extends AppWidgetProvider {
 
         public SearchWidgetProviderDelegate(Context context) {
             mContext = context == null ? ContextUtils.getApplicationContext() : context;
-            mManager = AppWidgetManager.getInstance(mContext);
+
+            if (mContext.getPackageManager().hasSystemFeature(PackageManager.FEATURE_APP_WIDGETS)) {
+                mManager = AppWidgetManager.getInstance(mContext);
+            } else {
+                mManager = null;
+            }
         }
 
         /** Returns the Context to pull resources from. */
@@ -74,12 +80,14 @@ public class SearchWidgetProvider extends AppWidgetProvider {
 
         /** Returns IDs for all search widgets that exist. */
         protected int[] getAllSearchWidgetIds() {
+            if (mManager == null) return new int[0];
             return mManager.getAppWidgetIds(
                     new ComponentName(getContext(), SearchWidgetProvider.class.getName()));
         }
 
         /** See {@link AppWidgetManager#updateAppWidget}. */
         protected void updateAppWidget(int id, RemoteViews views) {
+            assert mManager != null;
             mManager.updateAppWidget(id, views);
         }
     }
