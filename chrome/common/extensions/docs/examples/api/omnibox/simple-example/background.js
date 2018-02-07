8ceace534b34e68f8429306fb3f2aa4dd2c84c59
CrystalFaith <crystallambert@chromium.org>
2018-02-07 23:02:03 +0000

    [Extension Docs]Update Sample Extension
    Updated manifest to non persistent background.
    
    Bug: 808592
    Change-Id: Ib67576b8b5573eda32b2781c748f4e661a6ea305
    Reviewed-on: https://chromium-review.googlesource.com/899509
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Crystal Lambert <crystallambert@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535177}

--- chrome/common/extensions/docs/examples/api/omnibox/simple-example/background.js
+++ chrome/common/extensions/docs/examples/api/omnibox/simple-example/background.js
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+'use strict';
+
 // This event is fired each time the user updates the text in the omnibox,
 // as long as the extension's keyword mode is still active.
 chrome.omnibox.onInputChanged.addListener(
