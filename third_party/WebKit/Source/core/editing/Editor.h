90370ddfae8ecae22ceb5cedf15dd7d98a395878
Zhuoyu Qian <zhuoyu.qian@samsung.com>
2018-02-07 04:59:18 +0000

    Move ApplyStyleToSelection() to EditorCommand.cpp
    Editor::ApplyStyleToSelection() is only called in EditorCommand.cpp.
    Move it to make |Editor| class simpler for improving code health.
    
    BUG=672405
    
    Signed-off-by: Zhuoyu Qian <zhuoyu.qian@samsung.com>
    Change-Id: I9a9cafa0ffff7da560ee2bc7fcd2e5bf15059665
    Reviewed-on: https://chromium-review.googlesource.com/905962
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534919}

--- third_party/WebKit/Source/core/editing/Editor.h
+++ third_party/WebKit/Source/core/editing/Editor.h
@@ -122,7 +122,6 @@ class CORE_EXPORT Editor final : public GarbageCollectedFinalized<Editor> {
 
   void ApplyStyle(CSSPropertyValueSet*, InputEvent::InputType);
   void ApplyParagraphStyle(CSSPropertyValueSet*, InputEvent::InputType);
-  void ApplyStyleToSelection(CSSPropertyValueSet*, InputEvent::InputType);
   void ApplyParagraphStyleToSelection(CSSPropertyValueSet*,
                                       InputEvent::InputType);
 
