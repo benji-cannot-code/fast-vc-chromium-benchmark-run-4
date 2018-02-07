1e4de4905d8ef716e7d444bbd8acea010d59d302
Mitch Foley <mitchfoley@chromium.org>
2018-02-07 16:52:56 +0000

    adding ink wasm prebuilts.
    Change-Id: I3026038c32ebfce579d1696138f7c74af7ce5f32
    Reviewed-on: https://chromium-review.googlesource.com/905022
    Commit-Queue: Mitch Foley <mitchfoley@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535029}

--- third_party/ink/prebuilt/wasm/ink_demo.js
+++ third_party/ink/prebuilt/wasm/ink_demo.js
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 (function() {
   var config = new ink.embed.Config({
     parentEl: document.querySelector('#ink-canvas'),
-    nativeClientManifestUrl: 'ink.nmf',
     sengineType: 'makeSEngineInMemory'
   });
   ink.embed.EmbedComponent.execute(config, (embed) => {
