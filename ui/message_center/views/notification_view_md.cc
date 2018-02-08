690526c8c89d5fb17fc9be77e9178433ee4932fa
Megumi Hattori <megumihattori@google.com>
2018-02-08 07:41:10 +0000

    Fix animation area when inline settings is appearing.
    The animation when inline settings was appearing overlapped with the border outside of it. The border is shown only if the notification is nested.
    
    This CL fixes the animation area.
    
    BUG=809861
    
    Change-Id: I998e9afd294fef6059de1141e7211036877c1401
    Reviewed-on: https://chromium-review.googlesource.com/905965
    Commit-Queue: Megumi Hattori <megumihattori@google.com>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535329}

--- ui/message_center/views/notification_view_md.cc
+++ ui/message_center/views/notification_view_md.cc
@@ -683,6 +683,16 @@ void NotificationViewMD::Layout() {
     action_buttons_row_->set_clip_path(path);
     inline_reply_->set_clip_path(path);
   }
+
+  // The animation is needed to run inside of the border, which is shown only
+  // when the notification is nested.
+  if (is_nested()) {
+    gfx::Rect ink_drop_bounds = GetLocalBounds();
+    ink_drop_bounds.Inset(gfx::Insets(kNotificationBorderThickness));
+    ink_drop_container_->SetBoundsRect(ink_drop_bounds);
+  } else {
+    ink_drop_container_->SetBoundsRect(GetLocalBounds());
+  }
 }
 
 void NotificationViewMD::OnFocus() {
@@ -1241,11 +1251,10 @@ void NotificationViewMD::ToggleInlineSettings(const ui::LocatedEvent& event) {
 
   PreferredSizeChanged();
 
-  if (inline_settings_visible) {
+  if (inline_settings_visible)
     AddBackgroundAnimation(event);
-  } else {
+  else
     RemoveBackgroundAnimation();
-  }
 
   Layout();
   SchedulePaint();
@@ -1346,8 +1355,8 @@ void NotificationViewMD::RemoveInkDropLayer(ui::Layer* ink_drop_layer) {
 std::unique_ptr<views::InkDropRipple> NotificationViewMD::CreateInkDropRipple()
     const {
   return std::make_unique<views::FloodFillInkDropRipple>(
-      size(), GetInkDropCenterBasedOnLastEvent(), GetInkDropBaseColor(),
-      ink_drop_visible_opacity());
+      ink_drop_container_->size(), GetInkDropCenterBasedOnLastEvent(),
+      GetInkDropBaseColor(), ink_drop_visible_opacity());
 }
 
 SkColor NotificationViewMD::GetInkDropBaseColor() const {
