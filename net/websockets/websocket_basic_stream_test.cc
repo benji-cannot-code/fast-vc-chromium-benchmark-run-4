4c1227a0a9e33fb0ea09cf2cf59e2d1f256ea32f
Bence Béky <bnc@chromium.org>
2018-02-08 13:02:37 +0000

    Remove non-existing SPDY WebSocket pseudoheaders.
    It just happens so that
    https://tools.ietf.org/html/draft-ietf-httpbis-h2-websockets-00 defines
    these as headers not pseudoheaders (no ":" at the beginning).
    
    Also fix some includes.
    
    Bug: 801564
    Change-Id: Ied8b4c1fdf27d3207b4fa29f264e816bd9cdfbf6
    Reviewed-on: https://chromium-review.googlesource.com/906811
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Bence Béky <bnc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535363}

--- net/websockets/websocket_basic_stream_test.cc
+++ net/websockets/websocket_basic_stream_test.cc
@@ -12,9 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdint.h>
 #include <string.h>  // for memcpy() and memset().
-#include <string>
 #include <utility>
-#include <vector>
 
 #include "base/big_endian.h"
 #include "base/macros.h"
