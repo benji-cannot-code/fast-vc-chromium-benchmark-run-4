de198fe6550c50426e21692b21899dc4879ad3af
Illia Martyniuk <illiam@google.com>
2018-02-08 01:38:14 +0000

    Updating Local_Surface_Id::ToString()
    If logging level is less that 1 (not verbose) we print LocalSurfaceId's corresponding
    UnguessableToken as ABCD...; otherwise, we print full 16-character UnguessableToken.
    
    Added TEST=LocalSurfaceIdTest.VerifyToString
    
    Bug: 712308
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I577b15f23fc50643a8db4c555bdd95dcd0057755
    Reviewed-on: https://chromium-review.googlesource.com/904855
    Commit-Queue: Illia Martyniuk <illiam@google.com>
    Reviewed-by: Fady Samuel <fsamuel@chromium.org>
    Reviewed-by: Yi Xu <yiyix@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535241}

--- components/viz/common/surfaces/local_surface_id.cc
+++ components/viz/common/surfaces/local_surface_id.cc
@@ -10,9 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace viz {
 
 std::string LocalSurfaceId::ToString() const {
-  return base::StringPrintf("LocalSurfaceId(%d, %d, %s" PRIu64 ")",
+  std::string nonce = VLOG_IS_ON(1) ? nonce_.ToString()
+                                    : nonce_.ToString().substr(0, 4) + "...";
+
+  return base::StringPrintf("LocalSurfaceId(%d, %d, %s)",
                             parent_sequence_number_, child_sequence_number_,
-                            nonce_.ToString().c_str());
+                            nonce.c_str());
 }
 
 std::ostream& operator<<(std::ostream& out,
