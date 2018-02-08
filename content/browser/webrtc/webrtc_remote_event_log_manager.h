0b312279c11f8f0067a7325bf9281eba4689b8d6
Elad Alon <eladalon@chromium.org>
2018-02-08 19:56:43 +0000

    Add support for metadata in StartRemoteLogging
    Allow callers of WebRtcEventLogManager::StartRemoteLogging() to specify
    a string of metadata to be attached to the remote-bound log file.
    Typically, this would allow the user of the JS hooks to add an identifer
    to the log.
    
    Bug: 775415
    Change-Id: I9cf34d86154a3fe2370ad8add8c1dbd6ec606137
    Reviewed-on: https://chromium-review.googlesource.com/895586
    Commit-Queue: Elad Alon <eladalon@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535480}

--- content/browser/webrtc/webrtc_remote_event_log_manager.h
+++ content/browser/webrtc/webrtc_remote_event_log_manager.h
@@ -71,7 +71,8 @@ class CONTENT_EXPORT WebRtcRemoteEventLogManager final
                           int lid,
                           BrowserContextId browser_context,
                           const base::FilePath& browser_context_dir,
-                          size_t max_file_size_bytes);
+                          size_t max_file_size_bytes,
+                          const std::string& metadata);
 
   // If an active remote-bound log exists for the given peer connection, this
   // will append |message| to that log. If writing |message| to the log would
@@ -159,7 +160,8 @@ class CONTENT_EXPORT WebRtcRemoteEventLogManager final
                        int lid,
                        BrowserContextId browser_context,
                        const base::FilePath& browser_context_dir,
-                       size_t max_file_size_bytes);
+                       size_t max_file_size_bytes,
+                       const std::string& metadata);
 
   // Checks if the referenced peer connection has an associated active
   // remote-bound log. If it does, the log is changed from ACTIVE to PENDING.
