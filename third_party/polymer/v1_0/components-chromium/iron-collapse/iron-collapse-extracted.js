bad471e8297e9893d3957b668306868b02c9eb1c
dpapad <dpapad@chromium.org>
2018-02-08 22:04:41 +0000

    WebUI: Update Polymer iron-scroll-threshold, iron-collapse to 2.x.
    Bug: 738611
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ib1a4680ce7ce15e945fd3a73dffa3f17a58ff2cb
    Reviewed-on: https://chromium-review.googlesource.com/906974
    Reviewed-by: Scott Chen <scottchen@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535538}

--- third_party/polymer/v1_0/components-chromium/iron-collapse/iron-collapse-extracted.js
+++ third_party/polymer/v1_0/components-chromium/iron-collapse/iron-collapse-extracted.js
@@ -33,6 +33,18 @@ Polymer({
       },
 
       /**
+       * When true, the element is transitioning its opened state. When false,
+       * the element has finished opening/closing.
+       *
+       * @attribute transitioning
+       */
+      transitioning: {
+        type: Boolean,
+        notify: true,
+        readOnly: true
+      },
+
+      /**
        * Set noAnimation to true to disable animations.
        *
        * @attribute noAnimation
@@ -74,16 +86,10 @@ Polymer({
     hostAttributes: {
       role: 'group',
       'aria-hidden': 'true',
-      'aria-expanded': 'false'
     },
 
     listeners: {
-      transitionend: '_transitionEnd'
-    },
-
-    attached: function() {
-      // It will take care of setting correct classes and styles.
-      this._transitionEnd();
+      transitionend: '_onTransitionEnd'
     },
 
     /**
@@ -111,15 +117,13 @@ Polymer({
     updateSize: function(size, animated) {
       // Consider 'auto' as '', to take full size.
       size = size === 'auto' ? '' : size;
-      // No change!
-      if (this._desiredSize === size) {
-        return;
-      }
+
+      var willAnimate = animated && !this.noAnimation &&
+                        this.isAttached && this._desiredSize !== size;
 
       this._desiredSize = size;
 
       this._updateTransition(false);
-      var willAnimate = animated && !this.noAnimation && this._isDisplayed;
       // If we can animate, must do some prep work.
       if (willAnimate) {
         // Animation will start at the current size.
@@ -172,9 +176,9 @@ Polymer({
     },
 
     _openedChanged: function() {
-      this.setAttribute('aria-expanded', this.opened);
       this.setAttribute('aria-hidden', !this.opened);
 
+      this._setTransitioning(true);
       this.toggleClass('iron-collapse-closed', false);
       this.toggleClass('iron-collapse-opened', false);
       this.updateSize(this.opened ? 'auto' : '0px', true);
@@ -191,19 +195,13 @@ Polymer({
       this.toggleClass('iron-collapse-opened', this.opened);
       this._updateTransition(false);
       this.notifyResize();
+      this._setTransitioning(false);
     },
 
-    /**
-     * Simplistic heuristic to detect if element has a parent with display: none
-     *
-     * @private
-     */
-    get _isDisplayed() {
-      var rect = this.getBoundingClientRect();
-      for (var prop in rect) {
-        if (rect[prop] !== 0) return true;
+    _onTransitionEnd: function(event) {
+      if (Polymer.dom(event).rootTarget === this) {
+        this._transitionEnd();
       }
-      return false;
     },
 
     _calcSize: function() {
