b6246570ed803cc4504c2aca6c9ae03ecf68e9f6
Tsuyoshi Horo <horo@chromium.org>
2018-02-08 06:06:03 +0000

    Move SignedExchange layout test to browser test.
    When we will implement verification logic of signed exchange, we will need to
    install mock root certificate and generate signatures of test data while
    executing the tests. But it is difficult to do that in LayoutTest.
    So this CL moves the existing LayoutTest to browser test.
    
    Bug: 803774
    Change-Id: Id7807691c639d0dad1b2a0fbd31fe902eba5228e
    Reviewed-on: https://chromium-review.googlesource.com/908032
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535318}

--- third_party/WebKit/LayoutTests/http/tests/loading/htxg/resources/origin-signed-response-iframe.php
+++
@@ -1,7 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-<?php
-$fp = fopen("origin-signed-response-iframe.htxg", "rb");
-header('Content-Type: application/http-exchange+cbor');
-header("HTTP/1.0 200 OK");
-fpassthru($fp);
-?>
