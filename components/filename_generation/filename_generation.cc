e2a662b0a2d37389d47074474c50389cd1d329fa
Yafei Duan <romax@chromium.org>
2018-02-08 02:35:31 +0000

    Moving filename truncation to filename_generation.
    Filename truncation needs to be accessed by //components/offline_pages
    for a specific filename generation as well, hence moving the function
    from //chrome/browser to the new component for filename generation.
    
    Tests in previous location are kept since those are still testing
    related functionality.
    
    More context and plan:
    go/offline-pages-filename-generation-base
    
    Bug: 757073
    Change-Id: Iecbd8851e49918fe1afb64cce532c1b4c7eed599
    Reviewed-on: https://chromium-review.googlesource.com/903260
    Commit-Queue: Yafei Duan <romax@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535272}

--- components/filename_generation/filename_generation.cc
+++ components/filename_generation/filename_generation.cc
@@ -12,7 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/third_party/icu/icu_utf.h"
 #include "base/threading/thread_restrictions.h"
+#include "build/build_config.h"
 #include "components/url_formatter/url_formatter.h"
 #include "net/base/filename_util.h"
 #include "net/base/mime_util.h"
@@ -21,6 +23,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace filename_generation {
 
 namespace {
+
+// The lower bound for file name truncation. If the truncation results in a name
+// shorter than this limit, we give up automatic truncation and prompt the user.
+const size_t kTruncatedNameLengthLowerbound = 5;
 
 const base::FilePath::CharType kDefaultHtmlExtension[] =
     FILE_PATH_LITERAL("html");
@@ -131,6 +137,41 @@ base::FilePath GenerateFilename(const base::string16& title,
   base::FilePath::StringType file_name = name_with_proper_ext.value();
   base::i18n::ReplaceIllegalCharactersInPath(&file_name, '_');
   return base::FilePath(file_name);
+}
+
+bool TruncateFilename(base::FilePath* path, size_t limit) {
+  base::FilePath basename(path->BaseName());
+  // It is already short enough.
+  if (basename.value().size() <= limit)
+    return true;
+
+  base::FilePath dir(path->DirName());
+  base::FilePath::StringType ext(basename.Extension());
+  base::FilePath::StringType name(basename.RemoveExtension().value());
+
+  // Impossible to satisfy the limit.
+  if (limit < kTruncatedNameLengthLowerbound + ext.size())
+    return false;
+  limit -= ext.size();
+
+  // Encoding specific truncation logic.
+  base::FilePath::StringType truncated;
+#if defined(OS_CHROMEOS) || defined(OS_MACOSX)
+  // UTF-8.
+  base::TruncateUTF8ToByteSize(name, limit, &truncated);
+#elif defined(OS_WIN)
+  // UTF-16.
+  DCHECK(name.size() > limit);
+  truncated = name.substr(0, CBU16_IS_TRAIL(name[limit]) ? limit - 1 : limit);
+#else
+// We cannot generally assume that the file name encoding is in UTF-8 (see
+// the comment for FilePath::AsUTF8Unsafe), hence no safe way to truncate.
+#endif
+
+  if (truncated.size() < kTruncatedNameLengthLowerbound)
+    return false;
+  *path = dir.Append(truncated + ext);
+  return true;
 }
 
 }  // namespace filename_generation
