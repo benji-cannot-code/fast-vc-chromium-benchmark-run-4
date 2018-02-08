ff9d7c260151ac788567c58c9d3f27cb9f6c27f2
Mugdha Lakhani <nator@chromium.org>
2018-02-08 19:15:02 +0000

    [GCM Driver] Use OnceCallback and OnceClosure for
    GCM key store instead of Callback and Closure, respectively.
    This is because we only are using these callbacks once.
    This also lays the foundation for replacing KeyPair with ECPrivateKey.
    
    Bug: 603319
    Change-Id: I01632b0d3be7cf92cdced81a3b35d99134d37ad7
    Reviewed-on: https://chromium-review.googlesource.com/907348
    Commit-Queue: Mugdha Lakhani <nator@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535468}

---
+++ components/gcm_driver/features.h
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_GCM_DRIVER_FEATURES_H
+#define COMPONENTS_GCM_DRIVER_FEATURES_H
+
+#include "base/feature_list.h"
+//#include "components/gcm_driver/common/gcm_driver_export.h"
+
+namespace gcm {
+
+namespace features {
+
+extern const base::Feature kInvalidateTokenFeature;
+const int kDefaultTokenInvalidationPeriod = 0;
+const char kParamNameTokenInvalidationPeriodDays[] =
+    "token_invalidation_period";
+const char kGroupName[] = "token_validity_data";
+
+// The period after which the GCM token becomes stale.
+base::TimeDelta GetTokenInvalidationInterval();
+
+}  // namespace features
+
+}  // namespace gcm
+
+#endif  // COMPONENTS_GCM_DRIVER_FEATURES_H
