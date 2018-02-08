355336fa63e36af8ed85969961596b00a650aa83
Elad Alon <eladalon@chromium.org>
2018-02-08 15:05:46 +0000

    Make WebRtcEventLogManager final
    To make sure that posting base::Unretained(this) to the internal
    task runner is safe, we need to make sure that it cannot be
    sub-classed, or else, during destruction, running tasks might
    invoke virtual functions of the sub-class, which would by then
    have been invalidated.
    
    Bug: 775415
    Change-Id: I40af10f820755ac052fe65661261f5ca2f910ef0
    Reviewed-on: https://chromium-review.googlesource.com/905663
    Commit-Queue: Elad Alon <eladalon@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535377}

--- content/browser/webrtc/webrtc_internals_unittest.cc
+++ content/browser/webrtc/webrtc_internals_unittest.cc
@@ -74,14 +74,6 @@ class MockWakeLock : public device::mojom::WakeLock {
 
 }  // namespace
 
-class WebRtcEventLogManagerForTesting : public WebRtcEventLogManager {
- public:
-  WebRtcEventLogManagerForTesting() {
-    SetTaskRunnerForTesting(base::ThreadTaskRunnerHandle::Get());
-  }
-  ~WebRtcEventLogManagerForTesting() override = default;
-};
-
 // Derived class for testing only.  Allows the tests to have their own instance
 // for testing and control the period for which WebRTCInternals will bulk up
 // updates (changes down from 500ms to 1ms).
@@ -89,7 +81,9 @@ class WebRTCInternalsForTest : public WebRTCInternals {
  public:
   WebRTCInternalsForTest()
       : WebRTCInternals(1, true),
-        mock_wake_lock_(mojo::MakeRequest(&wake_lock_)) {}
+        mock_wake_lock_(mojo::MakeRequest(&wake_lock_)),
+        synchronous_webrtc_event_log_manager_(
+            base::ThreadTaskRunnerHandle::Get()) {}
 
   ~WebRTCInternalsForTest() override {}
 
@@ -97,7 +91,7 @@ class WebRTCInternalsForTest : public WebRTCInternals {
 
  private:
   MockWakeLock mock_wake_lock_;
-  WebRtcEventLogManagerForTesting synchronous_webrtc_event_log_manager_;
+  WebRtcEventLogManager synchronous_webrtc_event_log_manager_;
 };
 
 class WebRtcInternalsTest : public testing::Test {
