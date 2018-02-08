62d4fa8697d577b63445eb80220cf2762ade0bf0
Kristian H. Kristensen <hoegsberg@chromium.org>
2018-02-08 18:35:32 +0000

    ozone/drm: Always export format modifiers
    We tried to be careful to only export modifiers if we knew we'd be
    able to import them again. That is, if we could pass modifiers to
    addfb2. It turns out, Tegra requires modifiers for eglCreateImageKHR
    to correctly import dma-bufs, but Tegra KMS doesn't support modifiers.
    Instead, let's always export modifiers, but only try to create a KMS
    framebuffer with modifiers if the kernel says it supports it.
    
    Bug: 809670, 809666
    Change-Id: I065498ee5da2b53ec9577a3c9c8099c9f0fe1aab
    Reviewed-on: https://chromium-review.googlesource.com/907756
    Commit-Queue: Kristian H. Kristensen <hoegsberg@chromium.org>
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535450}

--- ui/ozone/platform/drm/gpu/drm_device.h
+++ ui/ozone/platform/drm/gpu/drm_device.h
@@ -51,6 +51,8 @@ class DrmDevice : public base::RefCountedThreadSafe<DrmDevice> {
 
   bool is_primary_device() const { return is_primary_device_; }
 
+  bool allow_addfb2_modifiers() const { return allow_addfb2_modifiers_; }
+
   // Open device.
   virtual bool Initialize(bool use_atomic);
 
@@ -206,6 +208,8 @@ class DrmDevice : public base::RefCountedThreadSafe<DrmDevice> {
   std::unique_ptr<IOWatcher> watcher_;
 
   bool is_primary_device_;
+
+  bool allow_addfb2_modifiers_;
 
   DISALLOW_COPY_AND_ASSIGN(DrmDevice);
 };
--- ui/ozone/platform/drm/gpu/gbm_buffer.h
+++ ui/ozone/platform/drm/gpu/gbm_buffer.h
@@ -80,8 +80,7 @@ class GbmBuffer : public ScanoutBuffer {
       gbm_bo* bo,
       uint32_t format,
       const gfx::Size& size,
-      uint32_t flags,
-      uint64_t modifier);
+      uint32_t flags);
 
   scoped_refptr<GbmDevice> drm_;
   gbm_bo* bo_;
