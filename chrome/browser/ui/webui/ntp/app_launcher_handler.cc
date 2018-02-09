dc6017c73a08a53d59908135b5bb282129f553e0
Alan Cutter <alancutter@chromium.org>
2018-02-09 04:35:03 +0000

    Make "App info" navigate to "Site settings" for bookmark apps in chrome:apps
    Bookmark apps don't get special Chrome app permissions and it doesn't make
    sense to display the permissions allocated to them when it will always be
    none.
    This CL changes the "App info" context menu entry to go to "Site settings"
    for the bookmark app's URL. This is inline with our UX mocks for PWAs.
    
    Bug: 800205
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I4d4d203aa0cbb8611824ee4090eb7542fdbacc52
    Reviewed-on: https://chromium-review.googlesource.com/905725
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535645}

--- chrome/browser/ui/webui/ntp/app_launcher_handler.cc
+++ chrome/browser/ui/webui/ntp/app_launcher_handler.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_tabstrip.h"
 #include "chrome/browser/ui/browser_window.h"
+#include "chrome/browser/ui/chrome_pages.h"
 #include "chrome/browser/ui/extensions/app_launch_params.h"
 #include "chrome/browser/ui/extensions/application_launch.h"
 #include "chrome/browser/ui/extensions/extension_enable_flow.h"
@@ -608,6 +609,13 @@ void AppLauncherHandler::HandleShowAppInfo(const base::ListValue* args) {
       extension_service_->GetExtensionById(extension_id, true);
   if (!extension)
     return;
+
+  if (extension->is_hosted_app() && extension->from_bookmark()) {
+    chrome::ShowSiteSettings(
+        chrome::FindBrowserWithWebContents(web_ui()->GetWebContents()),
+        extensions::AppLaunchInfo::GetFullLaunchURL(extension));
+    return;
+  }
 
   UMA_HISTOGRAM_ENUMERATION("Apps.AppInfoDialog.Launches",
                             AppInfoLaunchSource::FROM_APPS_PAGE,
