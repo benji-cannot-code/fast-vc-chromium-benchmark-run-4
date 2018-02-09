fd9be8da75104554a6f97f0e43130a279de1b92d
Hongchan Choi <hongchan@chromium.org>
2018-02-09 22:03:24 +0000

    Check finished_source_handlers_ for scheduling clean up task
    There has been an obvious mistake in handling stoppable source nodes;
    the check looks up active_source_nodes_ instead of
    finished_source_handlers_. This CL is one-line change to fix it.
    
    This change reduces 8% of CPU load within AudioDestinationHandler::Render()
    call. More profiling data can be found:
    https://docs.google.com/document/d/13ogSCFiXWCMjQv-D_nnKkEKoZlN8g8N9WUN1kMHJeKg/edit#bookmark=id.wd27wugm4ok9
    
    Bug: 804946
    Test: CL passes existing layout tests
    Change-Id: I2dcd3809384732a80b6bec207eeb8a7051a66758
    Reviewed-on: https://chromium-review.googlesource.com/911935
    Commit-Queue: Hongchan Choi <hongchan@chromium.org>
    Reviewed-by: Raymond Toy <rtoy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535844}

--- third_party/WebKit/Source/modules/webaudio/BaseAudioContext.cpp
+++ third_party/WebKit/Source/modules/webaudio/BaseAudioContext.cpp
@@ -744,7 +744,7 @@ void BaseAudioContext::HandleStoppableSourceNodes() {
   DCHECK(IsAudioThread());
   DCHECK(IsGraphOwner());
 
-  if (active_source_nodes_.size())
+  if (finished_source_handlers_.size())
     ScheduleMainThreadCleanup();
 }
 
