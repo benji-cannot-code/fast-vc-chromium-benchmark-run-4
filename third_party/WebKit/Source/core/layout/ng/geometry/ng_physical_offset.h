e19b8b58444d2eab5a5e71d59a65b5000dc9f6d3
Koji Ishii <kojii@chromium.org>
2018-02-09 17:23:45 +0000

    [LayoutNG] Fix VisualRect for inline block, relative position, etc.
    This patch fixes VisualRect for NGPaintFragment in:
    1. When line wraps (and that block offset is not zero,)
       VisualRect were often incorrect. This was fixed, and
       added to NGPaintFragmentTest.
    2. Inline block and when 'position: relative' is applied.
       NGPaintFragmentTest that were disabled are now enabled.
    3. This patch moves the most of the compute logic to
       PaintInvalidator::UpdateVisualRect(). chrishtr@ wanted to
       compute here in the original CL review but I couldn't manage
       it to work at that point.
    
    52 tests pass, and 34 regressions. Remaining work are:
    1. Multi-col and writing-modes are not supported yet. Some of
       these were working somehow and most of regressions are for
       these two features.
    2. Computing VisualRect for line boxes still needs post-process
       after traversal. I haven't come up with a good way to solve
       this yet.
    3. It turns out that we need to do full paint invalidation (or
       at least re-compute VisualRect and offset) for each layout,
       because NGPaintFragments are re-created. This may need to
       revisit for optimizations.
    4. There are still some cases where we skip computing VisualRect.
       These are to be investigated in following patches.
    5. Some of regressions were unexpected; there should be something
       that I'm not aware of yet. These are to be investigated further:
     fast/css/tab-size-complex-path.html
     fast/css/tab-size.html
     fast/text/midword-break-hang.html
     fast/text/shadow-no-blur.html
     fast/text/text-letter-spacing.html
     fast/text/international/rtl-negative-letter-spacing.html
    
    Bug: 714962
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_layout_ng;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I27fd5026b75cb8a7fe8fa0ebed7397618f1e4606
    Reviewed-on: https://chromium-review.googlesource.com/899622
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Aleks Totic <atotic@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535739}

--- third_party/WebKit/Source/core/layout/ng/geometry/ng_physical_offset.h
+++ third_party/WebKit/Source/core/layout/ng/geometry/ng_physical_offset.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/CoreExport.h"
 #include "platform/LayoutUnit.h"
+#include "platform/geometry/LayoutPoint.h"
+#include "platform/geometry/LayoutSize.h"
 #include "platform/text/TextDirection.h"
 #include "platform/text/WritingMode.h"
 
@@ -45,11 +47,14 @@ struct CORE_EXPORT NGPhysicalOffset {
 
   // Conversions from/to existing code. New code prefers type safety for
   // logical/physical distinctions.
-  explicit NGPhysicalOffset(const LayoutPoint&);
+  explicit NGPhysicalOffset(const LayoutPoint& point)
+      : left(point.X()), top(point.Y()) {}
+  explicit NGPhysicalOffset(const LayoutSize& size)
+      : left(size.Width()), top(size.Height()) {}
 
   // Conversions from/to existing code. New code prefers type safety for
   // logical/physical distinctions.
-  LayoutPoint ToLayoutPoint() const;
+  LayoutPoint ToLayoutPoint() const { return {left, top}; }
 
   String ToString() const;
 };
--- third_party/WebKit/Source/core/paint/PrePaintTreeWalk.cpp
+++ third_party/WebKit/Source/core/paint/PrePaintTreeWalk.cpp
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/paint/PaintLayer.h"
 #include "core/paint/PaintPropertyTreePrinter.h"
 #include "core/paint/compositing/CompositingLayerPropertyUpdater.h"
+#include "core/paint/ng/ng_paint_fragment.h"
 #include "platform/graphics/paint/GeometryMapper.h"
 
 namespace blink {
@@ -266,6 +267,14 @@ void PrePaintTreeWalk::Walk(const LayoutObject& object,
       Walk(*local_frame_view, context);
     }
     // TODO(pdr): Investigate RemoteFrameView (crbug.com/579281).
+  }
+
+  // Because current |PrePaintTreeWalk| walks LayoutObject tree, NGPaintFragment
+  // that are not mapped to LayoutObject are not updated. Ensure they are
+  // updated after all descendants were updated.
+  if (RuntimeEnabledFeatures::LayoutNGEnabled() && object.IsLayoutNGMixin()) {
+    if (NGPaintFragment* fragment = ToLayoutBlockFlow(object).PaintFragment())
+      fragment->UpdateVisualRectForNonLayoutObjectChildren();
   }
 
   object.GetMutableForPainting().ClearPaintFlags();
