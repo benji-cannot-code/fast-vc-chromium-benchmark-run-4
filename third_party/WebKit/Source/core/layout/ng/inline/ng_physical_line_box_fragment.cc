85f8e8ddb9b417e1f2682dc9fbbb88895ed9a7b1
Xiaocheng Hu <xiaochengh@chromium.org>
2018-02-09 01:35:54 +0000

    [LayoutNG] Refactor IsFragmentBefore/AfterLineWrap
    This patch refactors the two functions, moving line-box-inspection code to
    NGPhysicalLineBoxFragment, so that the overall implementation is less hacky.
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: Iad68595ad4231d2e98690668da6c3ae40d881b2f
    Reviewed-on: https://chromium-review.googlesource.com/906680
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535609}

--- third_party/WebKit/Source/core/layout/ng/inline/ng_physical_line_box_fragment.cc
+++ third_party/WebKit/Source/core/layout/ng/inline/ng_physical_line_box_fragment.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/layout/ng/inline/ng_physical_line_box_fragment.h"
 
+#include "core/layout/ng/inline/ng_inline_break_token.h"
 #include "core/style/ComputedStyle.h"
 
 namespace blink {
@@ -74,6 +75,21 @@ const NGPhysicalFragment* NGPhysicalLineBoxFragment::LastLogicalLeaf() const {
   }
   DCHECK_NE(runner, this);
   return runner;
+}
+
+bool NGPhysicalLineBoxFragment::HasSoftWrapToNextLine() const {
+  DCHECK(BreakToken());
+  DCHECK(BreakToken()->IsInlineType());
+  const NGInlineBreakToken& break_token = ToNGInlineBreakToken(*BreakToken());
+  return !break_token.IsFinished() && !break_token.IsForcedBreak();
+}
+
+// TODO(xiaochengh): Try avoid passing |previous_line|.
+bool NGPhysicalLineBoxFragment::HasSoftWrapFromPreviousLine(
+    const NGPhysicalLineBoxFragment* previous_line) const {
+  if (!previous_line)
+    return false;
+  return previous_line->HasSoftWrapToNextLine();
 }
 
 }  // namespace blink
