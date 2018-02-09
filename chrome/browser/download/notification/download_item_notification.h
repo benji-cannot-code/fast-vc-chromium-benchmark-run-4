149aa605a10e5503c270c64cc0c24d08fb22081f
Evan Stade <estade@chromium.org>
2018-02-09 22:26:17 +0000

    Move generation of "small image" from vector icon to NotificationViewMD.
    benefits:
    a) setting the vector image does something even if you don't call
       CreateSystemNotification
    b) we can one day move the small image constant to the view code,
       rather than being publicly exposed)
    c) changing the vector icon is less work (don't have to set the raster
       small image every time --- see DownloadItemNotification)
    d) don't have to serialize both fields (for Mojo)
    
    Bug: 578868
    Change-Id: I93f91ad21d608b007c66c6e463b7bcf92bc7cd60
    Reviewed-on: https://chromium-review.googlesource.com/906693
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535852}

--- chrome/browser/download/notification/download_item_notification.h
+++ chrome/browser/download/notification/download_item_notification.h
@@ -21,10 +21,6 @@ namespace test {
 class DownloadItemNotificationTest;
 }
 
-namespace gfx {
-struct VectorIcon;
-}
-
 namespace message_center {
 class Notification;
 }
@@ -61,10 +57,7 @@ class DownloadItemNotification : public ImageDecoder::ImageRequest {
   void CloseNotification();
   void Update();
   void UpdateNotificationData(bool display, bool bump_priority);
-  void UpdateNotificationIcon();
-
-  // Set icon of the notification.
-  void SetNotificationIcon(const gfx::VectorIcon& icon, SkColor color);
+  SkColor GetNotificationIconColor();
 
   // Set preview image of the notification. Must be called on IO thread.
   void OnImageLoaded(const std::string& image_data);
