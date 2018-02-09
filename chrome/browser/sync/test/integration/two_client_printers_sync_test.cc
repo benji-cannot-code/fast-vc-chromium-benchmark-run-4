0a26485c4eca6c076b276530a74419f8e4631c39
Olga Sharonova <olka@chromium.org>
2018-02-09 15:11:55 +0000

    Disabling TwoClientPrintersSyncTest.ConflictResolution: flaky on ChromeOS
    TBR=zea@chromium.org
    
    Bug: 810408
    Change-Id: I7f8fed6314d1a4653a95303602290c158e22f742
    Reviewed-on: https://chromium-review.googlesource.com/911473
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535715}

--- chrome/browser/sync/test/integration/two_client_printers_sync_test.cc
+++ chrome/browser/sync/test/integration/two_client_printers_sync_test.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/run_loop.h"
+#include "build/build_config.h"
 #include "chrome/browser/sync/test/integration/printers_helper.h"
 #include "chrome/browser/sync/test/integration/sync_test.h"
 
@@ -103,7 +104,13 @@ IN_PROC_BROWSER_TEST_F(TwoClientPrintersSyncTest, RemoveAndEditPrinters) {
             GetPrinterStore(1)->GetConfiguredPrinters()[0].description());
 }
 
-IN_PROC_BROWSER_TEST_F(TwoClientPrintersSyncTest, ConflictResolution) {
+// Flaky on ChromeOS:  http://crbug.com/810408
+#if defined(OS_CHROMEOS)
+#define MAYBE_ConflictResolution DISABLED_ConflictResolution
+#else
+#define MAYBE_ConflictResolution ConflictResolution
+#endif
+IN_PROC_BROWSER_TEST_F(TwoClientPrintersSyncTest, MAYBE_ConflictResolution) {
   ASSERT_TRUE(SetupSync());
 
   AddPrinter(GetPrinterStore(0), CreateTestPrinter(0));
