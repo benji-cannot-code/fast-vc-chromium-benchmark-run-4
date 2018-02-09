95d4a49c0b3ef39541ca7033c6216a064bc11440
Dominik Laskowski <domlaskowski@chromium.org>
2018-02-09 19:25:18 +0000

    exo: Dispatch input events to root surface for ARC
    Client-driven dragging/resizing relies on implicit grab. However, the
    client may destroy the focused surface, which prematurely ends the
    drag/resize. This CL prevents this by enforcing that the target for
    input events is always the root surface, which has the same lifetime
    as the client-side window.
    
    Bug: b:72627652
    Test: Wine applications can be resized by dragging window borders.
    Change-Id: I9badb6624ec32bd1e1286dc19b7ace0a8b136ff5
    Reviewed-on: https://chromium-review.googlesource.com/911812
    Commit-Queue: Dominik Laskowski <domlaskowski@chromium.org>
    Reviewed-by: David Reveman <reveman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535781}

--- components/exo/shell_surface_base.cc
+++ components/exo/shell_surface_base.cc
@@ -685,7 +685,7 @@ void ShellSurfaceBase::OnSurfaceCommit() {
   widget_->OnSizeConstraintsChanged();
 }
 
-bool ShellSurfaceBase::IsTouchEnabled(Surface*) const {
+bool ShellSurfaceBase::IsInputEnabled(Surface*) const {
   return true;
 }
 
