73812d45576de9294cf66cdb92b32446ad97421d
Thomas Tangl <tangltom@chromium.org>
2018-02-09 11:06:26 +0000

    [Dice] Add spacing to items in accounts submenu
    This CL adds spacing to the menu items of the accounts
    submenu in the user menu to match the specs of a bubble
    |HoverButton|.
    
    Bug: 786369
    Change-Id: Ic4360d71066d6aa61047d042b549443eaf1a1227
    Reviewed-on: https://chromium-review.googlesource.com/906932
    Commit-Queue: Thomas Tangl <tangltom@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535685}

--- chrome/browser/ui/views/profiles/dice_accounts_menu.cc
+++ chrome/browser/ui/views/profiles/dice_accounts_menu.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/generated_resources.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
+#include "ui/gfx/canvas.h"
+#include "ui/gfx/image/canvas_image_source.h"
 #include "ui/views/view.h"
 
 namespace {
@@ -19,9 +21,37 @@ constexpr int kAvatarIconSize = 16;
 // Used to identify the "Use another account" button.
 constexpr int kUseAnotherAccountCmdId = std::numeric_limits<int>::max();
 
+// TODO(tangltom): Calculate these values considering the existing menu config
+constexpr int kVerticalImagePadding = 9;
+constexpr int kHorizontalImagePadding = 6;
+
+class PaddedImageSource : public gfx::CanvasImageSource {
+ public:
+  explicit PaddedImageSource(const gfx::Image& image)
+      : CanvasImageSource(gfx::Size(image.Width() + 2 * kHorizontalImagePadding,
+                                    image.Height() + 2 * kVerticalImagePadding),
+                          false),
+        image_(image) {}
+
+  // gfx::CanvasImageSource:
+  void Draw(gfx::Canvas* canvas) override {
+    canvas->DrawImageInt(*image_.ToImageSkia(), kHorizontalImagePadding,
+                         kVerticalImagePadding);
+  }
+
+ private:
+  gfx::Image image_;
+
+  DISALLOW_COPY_AND_ASSIGN(PaddedImageSource);
+};
+
 gfx::Image SizeAndCircleIcon(const gfx::Image& icon) {
-  return profiles::GetSizedAvatarIcon(icon, true, kAvatarIconSize,
-                                      kAvatarIconSize, profiles::SHAPE_CIRCLE);
+  gfx::Image circled_icon = profiles::GetSizedAvatarIcon(
+      icon, true, kAvatarIconSize, kAvatarIconSize, profiles::SHAPE_CIRCLE);
+
+  gfx::Image padded_icon = gfx::Image(gfx::ImageSkia(
+      std::make_unique<PaddedImageSource>(circled_icon), 1 /* scale */));
+  return padded_icon;
 }
 
 }  // namespace
@@ -43,7 +73,6 @@ DiceAccountsMenu::DiceAccountsMenu(const std::vector<AccountInfo>& accounts,
     menu_.SetIcon(
         menu_.GetIndexOfCommandId(idx),
         SizeAndCircleIcon(icons[idx].IsEmpty() ? default_icon : icons[idx]));
-    menu_.AddSeparator(ui::SPACING_SEPARATOR);
   }
   // Add the "Use another account" button at the bottom.
   menu_.AddItem(
