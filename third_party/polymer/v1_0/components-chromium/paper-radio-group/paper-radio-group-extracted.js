afd97a2e1431affd718261adc71f01b2ed74f8f5
dpapad <dpapad@chromium.org>
2018-02-09 23:36:33 +0000

    WebUI: Update Polymer papar-radio-{button,group} to 2.x.
    Bug: 738611
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Iad4686cc3f09acb94834596be741d1b2d2e39bac
    Reviewed-on: https://chromium-review.googlesource.com/910313
    Reviewed-by: Scott Chen <scottchen@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535882}

--- third_party/polymer/v1_0/components-chromium/paper-radio-group/paper-radio-group-extracted.js
+++ third_party/polymer/v1_0/components-chromium/paper-radio-group/paper-radio-group-extracted.js
@@ -8,7 +8,6 @@ Polymer({
 
     hostAttributes: {
       role: 'radiogroup',
-      tabindex: 0
     },
 
     properties: {
@@ -54,7 +53,7 @@ Polymer({
     /**
      * Selects the given value.
      */
-     select: function(value) {
+    select: function(value) {
       var newItem = this._valueToItem(value);
       if (newItem && newItem.hasAttribute('disabled')) {
         return;
