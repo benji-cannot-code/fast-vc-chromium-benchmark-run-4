f95fea1c1daf47016376ce29bf3b65c741db1fcb
Darren Shen <shend@chromium.org>
2018-02-09 04:10:42 +0000

    [css-typed-om] Don't use mutable property value set unnecessarily.
    We currently call EnsureMutableInlineStyle() when calling styleMap.get,
    even though we don't need it to be mutable. EnsureMutableInlineStyle()
    also has some overhead of copy-on-write. This patch just calls
    InlineStyle() instead, which should be slightly faster as well.
    
    Bug: 808933
    Change-Id: I3f0db400b896a0be3888e61bdd8dd80783f7225c
    Reviewed-on: https://chromium-review.googlesource.com/910168
    Reviewed-by: nainar <nainar@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535630}

--- third_party/WebKit/Source/core/css/cssom/InlineStylePropertyMap.cpp
+++ third_party/WebKit/Source/core/css/cssom/InlineStylePropertyMap.cpp
@@ -12,14 +12,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 const CSSValue* InlineStylePropertyMap::GetProperty(CSSPropertyID property_id) {
-  return owner_element_->EnsureMutableInlineStyle().GetPropertyCSSValue(
-      property_id);
+  const CSSPropertyValueSet* inline_style = owner_element_->InlineStyle();
+  return inline_style ? inline_style->GetPropertyCSSValue(property_id)
+                      : nullptr;
 }
 
 const CSSValue* InlineStylePropertyMap::GetCustomProperty(
     AtomicString property_name) {
-  return owner_element_->EnsureMutableInlineStyle().GetPropertyCSSValue(
-      property_name);
+  const CSSPropertyValueSet* inline_style = owner_element_->InlineStyle();
+  return inline_style ? inline_style->GetPropertyCSSValue(property_name)
+                      : nullptr;
 }
 
 void InlineStylePropertyMap::SetProperty(CSSPropertyID property_id,
