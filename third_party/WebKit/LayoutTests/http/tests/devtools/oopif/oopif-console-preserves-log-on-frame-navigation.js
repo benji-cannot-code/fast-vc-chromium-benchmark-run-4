92139bd2e776d22578e2bb4b0ebf501dfd0b79ea
Dmitry Gozman <dgozman@chromium.org>
2018-02-09 22:44:18 +0000

    [DevTools] Fix console problems with oopif
    - Do not clear console on iframe navigation;
    - Do not print "Navigated to ..." message on iframe navigation;
    - Do not reset all UIMessages on iframe navigation.
    
    Bug: 809970
    Change-Id: I591872eae4d4276a9362180a53d7a6dce2dd56d5
    Reviewed-on: https://chromium-review.googlesource.com/909864
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535860}

---
+++ third_party/WebKit/LayoutTests/http/tests/devtools/oopif/oopif-console-preserves-log-on-frame-navigation.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(async function() {
+  TestRunner.addResult(`Tests that console preserves log on oopif navigation`);
+  await TestRunner.loadModule('console_test_runner');
+  await TestRunner.showPanel('console');
+
+  Common.settingForTest('preserveConsoleLog').set(false);
+  await TestRunner.navigatePromise('resources/empty.html');
+  await TestRunner.evaluateInPage(`console.log('Before navigation')`);
+  await TestRunner.addIframe('http://devtools.oopif.test:8000/devtools/oopif/resources/empty.html');
+  await TestRunner.evaluateInPage(`console.log('After navigation')`);
+  ConsoleTestRunner.dumpConsoleMessages();
+  TestRunner.completeTest();
+})();
