37d25b21477334b19f90a1b8a7e16cd7f743d9dc
wutao <wutao@chromium.org>
2018-02-09 22:31:40 +0000

    cros: Introduce KSV search box. KSV - part 4.
    Introduce the search box to Keyboard Shortcut Viewer (KSV).
    
    Changes:
    1. Add search box view.
    2. Handle searching shortcut.
    
    Bug: 768932
    Test: Manually test the search behavior.
    Change-Id: I78648f2c17ee4359f0889a8aa67ccca0b6980161
    Reviewed-on: https://chromium-review.googlesource.com/902002
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535854}

--- ui/chromeos/ksv/keyboard_shortcut_item.h
+++ ui/chromeos/ksv/keyboard_shortcut_item.h
@@ -15,9 +15,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace keyboard_shortcut_viewer {
 
 // The categories the shortcut belongs to. Used to group shortcuts in the
-// viewer.
+// viewer. This order is significant as it determines the order in which the
+// categories will be displayed in the view.
 enum class ShortcutCategory {
-  kPopular = 0,
+  kUnknown = 0,
+  kPopular,
   kTabAndWindow,
   kPageAndBrowser,
   kSystemAndDisplay,
