13a79b0a7b22782cda0798b07c9d08a9207efb87
Christos Froussios <cfroussios@chromium.org>
2018-02-09 19:13:43 +0000

    [Password Manager] Implement cancel passwords export
    Exporting passwords is cancelled if the user cancels the file selection
    dialog. It is necessary to cancel, so that PasswordManagerExporter will
    cancel any pending tasks and clear any cached password list.
    
    Bug: 789561
    Change-Id: I3184d6355b21618998b8816e2ccf990a6b785826
    Reviewed-on: https://chromium-review.googlesource.com/911791
    Reviewed-by: Vaclav Brozek <vabr@chromium.org>
    Commit-Queue: Christos Froussios <cfroussios@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535777}

--- chrome/browser/ui/passwords/password_manager_porter.h
+++ chrome/browser/ui/passwords/password_manager_porter.h
@@ -77,6 +77,7 @@ class PasswordManagerPorter : public ui::SelectFileDialog::Listener,
   void FileSelected(const base::FilePath& path,
                     int index,
                     void* params) override;
+  void FileSelectionCanceled(void* params) override;
 
   virtual void ImportPasswordsFromPath(const base::FilePath& path);
 
