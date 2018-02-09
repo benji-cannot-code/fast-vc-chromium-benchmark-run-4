21a406fe5651d1405e8702c7f2270b8327a4d94a
Dale Curtis <dalecurtis@chromium.org>
2018-02-09 20:55:06 +0000

    Add support for suspending the media::Pipeline after metadata.
    This is a reland of http://crrev.com/535519 which fixes the test
    expectations when run without DCHECKs enabled. The IsSuspended()
    call is in a DCHECK, so may not always be called during normal
    tests.
    
    -------------[ Original Description Follows ] --------------
    
    Introduces a new StartType for media::Pipeline, which controls when
    to suspend after metadata. The valid values are kNormal (existing
    flow), kStartSuspendedForAudioOnly (suspend for audio-only),
    kStartSuspended (always suspend).
    
    This reuses the existing suspend/resume functionality and just trains
    the PipelineController to be aware that this is a possible state
    transition after startup. A new Pipeline::IsSuspended() method is
    introduced to facilitate this instead of modifying the SeekCB used
    in startup to avoid unnecessary churn and strengthen the existing
    suspend/resume state checks in the controller.
    
    We handle the suspend decision inside the Pipeline to avoid unnecessary
    startup delays in the autoplay path which hopping back to WMPI for such
    a decision would require.
    
    This does not actually enable suspend after metdata, that will be part
    of a larger base::Feature experiment.
    
    BUG=694855
    TEST=new unittests
    TBR=xhwang, liberato
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I2b338a4158f261f2815eeffd0399aba04dee1c2d
    Reviewed-on: https://chromium-review.googlesource.com/911622
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535822}

--- media/base/mock_filters.h
+++ media/base/mock_filters.h
@@ -75,8 +75,9 @@ class MockPipeline : public Pipeline {
   // take unique_ptr* instead of unique_ptr so that they can be mock methods.
   // Private stubs for Start() and Resume() implement the actual Pipeline
   // interface by forwarding to these mock methods.
-  MOCK_METHOD4(Start,
-               void(Demuxer*,
+  MOCK_METHOD5(Start,
+               void(StartType start_type,
+                    Demuxer*,
                     std::unique_ptr<Renderer>*,
                     Client*,
                     const PipelineStatusCB&));
@@ -97,6 +98,8 @@ class MockPipeline : public Pipeline {
   // Stop(). (Or better, remove it from the interface entirely.)
   MOCK_CONST_METHOD0(IsRunning, bool());
 
+  MOCK_CONST_METHOD0(IsSuspended, bool());
+
   // TODO(sandersd): These should be regular getters/setters.
   MOCK_CONST_METHOD0(GetPlaybackRate, double());
   MOCK_METHOD1(SetPlaybackRate, void(double));
@@ -114,7 +117,8 @@ class MockPipeline : public Pipeline {
 
  private:
   // Forwarding stubs (see comment above).
-  void Start(Demuxer* demuxer,
+  void Start(StartType start_type,
+             Demuxer* demuxer,
              std::unique_ptr<Renderer> renderer,
              Client* client,
              const PipelineStatusCB& seek_cb) override;
