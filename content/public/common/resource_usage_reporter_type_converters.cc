05a95893c9351a784cd6e58edac22e992d7efba6
Ben Goodger <ben@chromium.org>
2018-02-09 22:54:01 +0000

    Move ResourceUsageReporter to //content.
    This is part of an effort to eliminate manifest overlays. (See bug).
    
    This interface actually needs to be hooked up in some utility processes & renderers. At face value it seems like something like this could be part of the service manager API but it turns out to be very blink/v8 specific. Ultimately it should probably be exposed by the blink & proxy resolver services. Due to the way the task manager obtains these APIs currently moving to content makes the most sense.
    
    http://crbug.com/810851
    
    Change-Id: Ic7948d41487120d3207e96b866645854f7405b92
    Reviewed-on: https://chromium-review.googlesource.com/887429
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Ben Goodger <ben@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535868}

--- content/public/common/resource_usage_reporter_type_converters.cc
+++ content/public/common/resource_usage_reporter_type_converters.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/common/resource_usage_reporter_type_converters.h"
+#include "content/public/common/resource_usage_reporter_type_converters.h"
 
 #include <stddef.h>
 
@@ -13,10 +13,10 @@ namespace mojo {
 
 namespace {
 
-chrome::mojom::ResourceTypeStatPtr StatToMojo(
+content::mojom::ResourceTypeStatPtr StatToMojo(
     const blink::WebCache::ResourceTypeStat& obj) {
-  chrome::mojom::ResourceTypeStatPtr stat =
-      chrome::mojom::ResourceTypeStat::New();
+  content::mojom::ResourceTypeStatPtr stat =
+      content::mojom::ResourceTypeStat::New();
   stat->count = obj.count;
   stat->size = obj.size;
   stat->decoded_size = obj.decoded_size;
@@ -24,7 +24,7 @@ chrome::mojom::ResourceTypeStatPtr StatToMojo(
 }
 
 blink::WebCache::ResourceTypeStat StatFromMojo(
-    const chrome::mojom::ResourceTypeStat& obj) {
+    const content::mojom::ResourceTypeStat& obj) {
   blink::WebCache::ResourceTypeStat stat;
   stat.count = base::saturated_cast<size_t>(obj.count);
   stat.size = base::saturated_cast<size_t>(obj.size);
@@ -35,12 +35,12 @@ blink::WebCache::ResourceTypeStat StatFromMojo(
 }  // namespace
 
 // static
-chrome::mojom::ResourceTypeStatsPtr
-TypeConverter<chrome::mojom::ResourceTypeStatsPtr,
+content::mojom::ResourceTypeStatsPtr
+TypeConverter<content::mojom::ResourceTypeStatsPtr,
               blink::WebCache::ResourceTypeStats>::
     Convert(const blink::WebCache::ResourceTypeStats& obj) {
-  chrome::mojom::ResourceTypeStatsPtr stats =
-      chrome::mojom::ResourceTypeStats::New();
+  content::mojom::ResourceTypeStatsPtr stats =
+      content::mojom::ResourceTypeStats::New();
   stats->images = StatToMojo(obj.images);
   stats->css_style_sheets = StatToMojo(obj.css_style_sheets);
   stats->scripts = StatToMojo(obj.scripts);
@@ -53,8 +53,8 @@ TypeConverter<chrome::mojom::ResourceTypeStatsPtr,
 // static
 blink::WebCache::ResourceTypeStats
 TypeConverter<blink::WebCache::ResourceTypeStats,
-              chrome::mojom::ResourceTypeStats>::
-    Convert(const chrome::mojom::ResourceTypeStats& obj) {
+              content::mojom::ResourceTypeStats>::
+    Convert(const content::mojom::ResourceTypeStats& obj) {
   if (!obj.images || !obj.css_style_sheets || !obj.scripts ||
       !obj.xsl_style_sheets || !obj.fonts || !obj.other) {
     return {};
---
+++ content/public/common/resource_usage_reporter_type_converters.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_PUBLIC_COMMON_RESOURCE_USAGE_REPORTER_TYPE_CONVERTERS_H_
+#define CONTENT_PUBLIC_COMMON_RESOURCE_USAGE_REPORTER_TYPE_CONVERTERS_H_
+
+#include "content/common/content_export.h"
+#include "content/public/common/resource_usage_reporter.mojom.h"
+#include "mojo/public/cpp/bindings/type_converter.h"
+#include "third_party/WebKit/public/platform/WebCache.h"
+
+namespace mojo {
+
+template <>
+struct CONTENT_EXPORT TypeConverter<content::mojom::ResourceTypeStatsPtr,
+                                    blink::WebCache::ResourceTypeStats> {
+  static content::mojom::ResourceTypeStatsPtr Convert(
+      const blink::WebCache::ResourceTypeStats& obj);
+};
+
+template <>
+struct CONTENT_EXPORT TypeConverter<blink::WebCache::ResourceTypeStats,
+                                    content::mojom::ResourceTypeStats> {
+  static blink::WebCache::ResourceTypeStats Convert(
+      const content::mojom::ResourceTypeStats& obj);
+};
+
+}  // namespace mojo
+
+#endif  // CONTENT_PUBLIC_COMMON_RESOURCE_USAGE_REPORTER_TYPE_CONVERTERS_H_
