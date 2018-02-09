35668aac2fb7ef5b834258f67ed98cabdda8ae6c
Kunihiko Sakamoto <ksakamoto@chromium.org>
2018-02-09 06:07:31 +0000

    Revert "Use a PromiseResolver to store reference to Element in getComputedAccessibleNode() implementation."
    This reverts commit af8ce55a31a279998ce52532132c9eecdbce6c78.
    
    Reason for revert: WebKit Linux Trusty Leak bot detected leak in the following tests:
    
    * accessibility/aom-computed-accessible-node.html
    * accessibility/aom-computed-int-properties.html
    * accessibility/aom-computed-relation-accessors.html
    * accessibility/aom-computed-string-properties.html
    
    https://ci.chromium.org/buildbot/chromium.webkit/WebKit%20Linux%20Trusty%20Leak/15388
    
    
    Original change's description:
    > Use a PromiseResolver to store reference to Element in getComputedAccessibleNode() implementation.
    > 
    > This means that we don't need to store a reference to the Element in ComputedAccessibleNode itself, as we can request an AXID after layout is guaranteed to have occurred.
    > 
    > It also means that, for now, a new ComputedAccessibleNode is created for each call to getComputedAccessibleNode() and all relation accessors.
    > 
    > BUG=792783
    > 
    > Change-Id: Ia61f5f7a0745c85cd56276731792e3d034431e5f
    > Reviewed-on: https://chromium-review.googlesource.com/897230
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    > Commit-Queue: Alice Boxhall <aboxhall@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#535544}
    
    TBR=dmazzoni@chromium.org,aboxhall@chromium.org,haraken@chromium.org,skyostil@chromium.org,altimin@chromium.org,meredithl@google.com
    
    Change-Id: Ia172014529bf26e8efdd675b231e71e869e513d9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 792783
    Reviewed-on: https://chromium-review.googlesource.com/910553
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Commit-Queue: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535659}

--- third_party/WebKit/Source/core/dom/Element.h
+++ third_party/WebKit/Source/core/dom/Element.h
@@ -46,6 +46,7 @@ namespace blink {
 class AccessibleNode;
 class Attr;
 class Attribute;
+class ComputedAccessibleNode;
 class CSSStyleDeclaration;
 class CustomElementDefinition;
 class DOMRect;
@@ -295,6 +296,8 @@ class CORE_EXPORT Element : public ContainerNode {
 
   AccessibleNode* ExistingAccessibleNode() const;
   AccessibleNode* accessibleNode();
+
+  ComputedAccessibleNode* GetComputedAccessibleNode();
 
   void DidMoveToNewDocument(Document&) override;
 
