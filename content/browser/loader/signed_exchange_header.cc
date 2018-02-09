a1ab9a6ec2ccf1ae4c9b73f1250e1dbdcd44cbba
Kouhei Ueno <kouhei@chromium.org>
2018-02-09 04:16:47 +0000

    Introduce SignedExchangeHeader
    This CL introduces SignedExchangeHeader class, which holds information about
    a signed exchange envelope [1] but its payload.
    
    The class will be used as output of SignedExchangeParser, and will be used as
    one of the input to SignedExchangeVerifier.
    
    [1] https://wicg.github.io/webpackage/draft-yasskin-http-origin-signed-responses.html
    
    Bug: 803774
    Change-Id: Ie308ca415110a91530eda87924b685b4b100be2e
    Reviewed-on: https://chromium-review.googlesource.com/910468
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535640}

---
+++ content/browser/loader/signed_exchange_header.cc
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/loader/signed_exchange_header.h"
+
+#include <utility>
+
+namespace content {
+
+SignedExchangeHeader::SignedExchangeHeader() = default;
+SignedExchangeHeader::~SignedExchangeHeader() = default;
+
+void SignedExchangeHeader::AddResponseHeader(base::StringPiece name,
+                                             base::StringPiece value) {
+  response_headers_.insert(std::make_pair(name, value));
+}
+
+}  // namespace content
