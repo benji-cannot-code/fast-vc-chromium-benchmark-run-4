ec282a8380ae32ece95491df18fcb308df6b6000
Elly Fong-Jones <ellyjones@google.com>
2018-02-09 18:18:59 +0000

    extensions: never use noisy errors in unit tests
    Using LoadErrorReporter::Init() in any unit test leaves the singleton
    LoadErrorReporter in the same state for any subsequent tests that use the same
    process. If any test uses LoadErrorReporter::Init(true) to enable "noisy"
    errors, all subsequent tests have that setting too, and since "noisy" errors
    pop up a message box (which blocks the process on Mac), this introduces very
    hard-to-debug test failures. One sample pair of tests is:
    
    ToolbarActionsBarUnitTest.ReuploadExtensionFailed/0
    RuleIndexingTest.DuplicateResourceTypes/1
    
    Running unit_tests with those two tests in that order pops up a modal on the
    second test's run, which hangs it.
    
    Bug: None
    Change-Id: I752cdae2fe05908dffe6d2af1036c2ef3d39542a
    Reviewed-on: https://chromium-review.googlesource.com/911190
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535756}

--- chrome/browser/ui/toolbar/toolbar_actions_bar_unittest.cc
+++ chrome/browser/ui/toolbar/toolbar_actions_bar_unittest.cc
@@ -150,7 +150,7 @@ ToolbarActionsBarUnitTest::~ToolbarActionsBarUnitTest() {}
 
 void ToolbarActionsBarUnitTest::SetUp() {
   BrowserWithTestWindowTest::SetUp();
-  extensions::LoadErrorReporter::Init(true);
+  extensions::LoadErrorReporter::Init(false);
 
   // The toolbar typically displays extension icons, so create some extension
   // test infrastructure.
