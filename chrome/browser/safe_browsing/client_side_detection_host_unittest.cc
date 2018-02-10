5f8c02f4eb2394f72a5ba5f58ee71deb342ece22
Eugene But <eugenebut@chromium.org>
2018-02-10 16:02:00 +0000

    Rename TestPreClassificationCheckInPageNavigation to TestPreClassificationCheckSameDocumentNavigation.
    Old name was presumably used to match was_within_same_page IPC, but
    SameDocument name better reflects the the type of the navigation
    (navigation did not change the document object).
    
    This change will make naming more consistent with the rest of Chromium
    code.
    
    Bug: 695189
    Change-Id: Ib0e1229d1730d6ac01038aed112a47d9eea78015
    Reviewed-on: https://chromium-review.googlesource.com/912451
    Reviewed-by: Nathan Parker <nparker@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535978}

--- chrome/browser/safe_browsing/client_side_detection_host_unittest.cc
+++ chrome/browser/safe_browsing/client_side_detection_host_unittest.cc
@@ -978,7 +978,7 @@ TEST_F(ClientSideDetectionHostTest, TestPreClassificationCheckPass) {
 }
 
 TEST_F(ClientSideDetectionHostTest,
-       TestPreClassificationCheckInPageNavigation) {
+       TestPreClassificationCheckSameDocumentNavigation) {
   GURL url("http://host.com/");
   ExpectPreClassificationChecks(url, &kFalse, &kFalse, &kFalse, &kFalse,
                                 &kFalse, &kFalse, &kFalse);
@@ -988,7 +988,7 @@ TEST_F(ClientSideDetectionHostTest,
   ExpectStartPhishingDetection(&url);
   ExpectShouldClassifyForMalwareResult(true);
 
-  // Now try an in-page navigation.  This should not trigger an IPC.
+  // Now try an same-document navigation.  This should not trigger an IPC.
   EXPECT_CALL(*csd_service_, IsPrivateIPAddress(_)).Times(0);
   GURL inpage("http://host.com/#foo");
   ExpectPreClassificationChecks(inpage, NULL, NULL, NULL, NULL, NULL, NULL,
