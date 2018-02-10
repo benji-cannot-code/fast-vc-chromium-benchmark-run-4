13c684c19a7b33010e9b5080edeaf5b74a3e3809
Greg Thompson <grt@chromium.org>
2018-02-10 08:52:09 +0000

    Hoist the time at which an update was detected into UpgradeDetector.
    The two implementations of this class each had their own member
    representing the time that an update was detected. This is good to have
    in the base class so that consumers of UpgradeDetector can make use of
    it.
    
    BUG=511300
    
    Change-Id: Ib0e9a36f309fb630b5ec34e317eed6388873239b
    Reviewed-on: https://chromium-review.googlesource.com/908449
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535971}

--- chrome/browser/upgrade_detector.h
+++ chrome/browser/upgrade_detector.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "base/macros.h"
 #include "base/observer_list.h"
+#include "base/time/time.h"
 #include "base/timer/timer.h"
 #include "chrome/browser/upgrade_observer.h"
 #include "ui/base/idle/idle.h"
@@ -52,6 +53,11 @@ class UpgradeDetector {
   virtual ~UpgradeDetector();
 
   static void RegisterPrefs(PrefRegistrySimple* registry);
+
+  // Returns the time at which an available upgrade was detected.
+  base::TimeTicks upgrade_detected_time() const {
+    return upgrade_detected_time_;
+  }
 
   // Whether the user should be notified about an upgrade.
   bool notify_upgrade() const { return notify_upgrade_; }
@@ -148,6 +154,10 @@ class UpgradeDetector {
     upgrade_available_ = available;
   }
 
+  void set_upgrade_detected_time(base::TimeTicks upgrade_detected_time) {
+    upgrade_detected_time_ = upgrade_detected_time;
+  }
+
   void set_best_effort_experiment_updates_available(bool available) {
     best_effort_experiment_updates_available_ = available;
   }
@@ -186,6 +196,9 @@ class UpgradeDetector {
   // Whether any software updates are available (experiment updates are tracked
   // separately via additional member variables below).
   UpgradeAvailable upgrade_available_;
+
+  // The time at which an available upgrade was detected.
+  base::TimeTicks upgrade_detected_time_;
 
   // Whether "best effort" experiment updates are available.
   bool best_effort_experiment_updates_available_;
