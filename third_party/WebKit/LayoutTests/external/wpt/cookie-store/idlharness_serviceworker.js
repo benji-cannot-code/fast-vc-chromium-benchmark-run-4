dad63fcf415a52331386f4975231390ff7b14c51
Joshua Bell <jsbell@chromium.org>
2018-02-10 01:56:37 +0000

    Async Cookies: Add idlharness test
    Bug: 729800
    Change-Id: If6cfed3427f778cf990721214240757cb06f320a
    Reviewed-on: https://chromium-review.googlesource.com/912242
    Commit-Queue: Joshua Bell <jsbell@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535928}

---
+++ third_party/WebKit/LayoutTests/external/wpt/cookie-store/idlharness_serviceworker.js
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+self.GLOBAL = {
+  isWindow: function() { return false; },
+  isWorker: function() { return true; },
+};
+importScripts('/resources/testharness.js',
+              '/resources/WebIDLParser.js',
+              '/resources/idlharness.js');
+
+promise_test(async t => {
+  const urls = ['cookie-store.idl'];
+  const [cookie_store] = await Promise.all(
+    urls.map(url => fetch(url).then(response => response.text())));
+
+  const idl_array = new IdlArray();
+
+  idl_array.add_untested_idls('[Exposed=ServiceWorker] interface ServiceWorkerGlobalScope {};');
+  idl_array.add_untested_idls('[Exposed=Window] interface Window {};');
+
+  idl_array.add_idls(cookie_store);
+
+  idl_array.add_objects({
+    CookieStore: [self.cookieStore],
+  });
+  idl_array.test();
+}, 'Interface test');
+
+done();
