c4387b107e35ba35e045d0b20f19f99737be3507
Christopher Grant <cjgrant@chromium.org>
2018-02-12 17:15:58 +0000

    VR: Enable inline-match only with experimental features
    This feature requires selection support in the keyboard, and is broken
    without it.  Therefore, keep the feature disabled until further notice.
    
    BUG=
    R=vollick
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_vr;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I845c6b719ecdca70e5534573f7bff36cb5ee1bdb
    Reviewed-on: https://chromium-review.googlesource.com/907230
    Commit-Queue: Christopher Grant <cjgrant@chromium.org>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536116}

--- chrome/browser/vr/elements/omnibox_text_field_unittest.cc
+++ chrome/browser/vr/elements/omnibox_text_field_unittest.cc
@@ -33,6 +33,7 @@ class OmniboxTest : public testing::Test {
                             base::Unretained(&browser_)));
     omnibox_->OnFocusChanged(true);
     omnibox_->SetTextInputDelegate(&text_input_delegate_);
+    omnibox_->set_allow_inline_autocomplete(true);
   }
 
  protected:
