9b365fb8d130c9b43189b70878269581d23b56a1
Ria Jiang <riajiang@chromium.org>
2018-02-12 19:02:19 +0000

    Use two different flags for getting HTD from draw quad or surface layer.
    We are using -use-viz-hit-test flag to set the HitTestDataProvider to
    use in both draw-quad and surface-layer viz-hit-test versions, so we
    still get draw-quad hit-test data when trying to test the surface-layer
    version.
    
    This CL separates them to be:
    1. --use-viz-hit-test-draw-quad, which turns on viz-hit-test with data
    coming from draw quad.
    2. --use-viz-hit-test-surface-layer, which turns on viz-hit-test with
    data coming from surface layer.
    
    Bug: 810128
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I76609fe0b51f67a6c45a163dad9e3e1fc5b69766
    Reviewed-on: https://chromium-review.googlesource.com/910072
    Commit-Queue: Ria Jiang <riajiang@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Gary Klassen <gklassen@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536148}

--- content/renderer/render_thread_impl.cc
+++ content/renderer/render_thread_impl.cc
@@ -2103,7 +2103,7 @@ void RenderThreadImpl::RequestNewLayerTreeFrameSink(
       features::IsSurfaceSynchronizationEnabled();
   params.local_surface_id_provider =
       std::make_unique<RendererLocalSurfaceIdProvider>();
-  if (features::IsVizHitTestingEnabled()) {
+  if (features::IsVizHitTestingDrawQuadEnabled()) {
     params.hit_test_data_provider =
         std::make_unique<viz::HitTestDataProviderSimpleBounds>();
   }
