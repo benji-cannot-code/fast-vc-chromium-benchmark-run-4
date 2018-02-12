6bbf599ec11f197cfff998c6588e0e4e7fe63a36
Raymond Toy <rtoy@chromium.org>
2018-02-12 19:05:20 +0000

    Use special internal event for marking the end of a curve
    Previously, creation of a SetValueCurve event would automatically
    insert a SetValue event at the end of the SetValueCurve to establish
    an end value for the curve so that following events would start from
    the end of the curve instead of the beginning.
    
    However, this causes incorrect errors about overlapping SetValueCurve
    events if a SetValueCurve is scheduled to start at the very end of a
    preceeding SetValueCurve.  The implicitly inserted SetValue would
    overlap the new SetValueCurve.  Using an internal event avoids this
    conflict, but still establishes the appropriate event needed to manage
    the timeline.
    
    Bug: 768740
    Test: 
    Change-Id: Ibc798e0427bb49c3e163f5a7535f26c49b166303
    Reviewed-on: https://chromium-review.googlesource.com/731186
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Commit-Queue: Raymond Toy <rtoy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536150}

--- third_party/WebKit/Source/modules/webaudio/AudioParamTimeline.h
+++ third_party/WebKit/Source/modules/webaudio/AudioParamTimeline.h
@@ -113,6 +113,8 @@ class AudioParamTimeline {
       kSetValueCurve,
       // For cancelValuesAndHold
       kCancelValues,
+      // Special marker for the end of a |kSetValueCurve| event.
+      kSetValueCurveEnd,
       kLastType
     };
 
@@ -134,6 +136,8 @@ class AudioParamTimeline {
         const Vector<float>& curve,
         double time,
         double duration);
+    static std::unique_ptr<ParamEvent> CreateSetValueCurveEndEvent(float value,
+                                                                   double time);
     static std::unique_ptr<ParamEvent> CreateCancelValuesEvent(
         double time,
         std::unique_ptr<ParamEvent> saved_event);
