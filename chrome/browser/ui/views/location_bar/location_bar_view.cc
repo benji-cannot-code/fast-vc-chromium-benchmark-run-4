103f5c32ddf9eac3e72355c0e744462ac528f52a
Patti <patricialor@chromium.org>
2018-02-12 00:45:54 +0000

    Omnibox/Views: Update clear all text button icon.
    Update the "clear all text" button's icon and show it while editing in touch
    mode.
    
    Screenshots -
    https://drive.google.com/file/d/1EFdKPFsvo70ZD0FfK0mfKLS-avijyCtN/view?usp=sharing
    
    Bug: 801583
    Change-Id: I9b5dca22654d8f24c467e3f9c21d278a7c63005e
    Reviewed-on: https://chromium-review.googlesource.com/910828
    Commit-Queue: Patti <patricialor@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536015}

--- chrome/browser/ui/views/location_bar/location_bar_view.cc
+++ chrome/browser/ui/views/location_bar/location_bar_view.cc
@@ -60,6 +60,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/favicon/content/content_favicon_driver.h"
 #include "components/omnibox/browser/omnibox_popup_model.h"
 #include "components/omnibox/browser/omnibox_popup_view.h"
+#include "components/omnibox/browser/vector_icons.h"
 #include "components/prefs/pref_service.h"
 #include "components/search_engines/template_url.h"
 #include "components/search_engines/template_url_service.h"
@@ -115,6 +116,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using content::WebContents;
 using views::View;
+
+namespace {
+
+bool InTouchableMode() {
+  return ui::MaterialDesignController::GetMode() ==
+         ui::MaterialDesignController::MATERIAL_TOUCH_OPTIMIZED;
+}
+
+}  // namespace
 
 // LocationBarView -----------------------------------------------------------
 
@@ -841,7 +851,9 @@ void LocationBarView::RefreshClearAllButtonIcon() {
   if (!clear_all_button_)
     return;
 
-  SetImageFromVectorIcon(clear_all_button_, kTabCloseNormalIcon,
+  const gfx::VectorIcon& icon =
+      InTouchableMode() ? omnibox::kTouchableClearIcon : kTabCloseNormalIcon;
+  SetImageFromVectorIcon(clear_all_button_, icon,
                          GetNativeTheme()->GetSystemColor(
                              ui::NativeTheme::kColorId_TextfieldDefaultColor));
 }
@@ -1095,8 +1107,9 @@ void LocationBarView::AnimationEnded(const gfx::Animation* animation) {
 void LocationBarView::OnChanged() {
   RefreshLocationIcon();
   location_icon_view_->set_show_tooltip(!GetOmniboxView()->IsEditingOrEmpty());
-  clear_all_button_->SetVisible(GetToolbarModel()->input_in_progress() &&
-                                LocationBarView::IsVirtualKeyboardVisible());
+  clear_all_button_->SetVisible(
+      GetToolbarModel()->input_in_progress() &&
+      (InTouchableMode() || LocationBarView::IsVirtualKeyboardVisible()));
   Layout();
   SchedulePaint();
 }
