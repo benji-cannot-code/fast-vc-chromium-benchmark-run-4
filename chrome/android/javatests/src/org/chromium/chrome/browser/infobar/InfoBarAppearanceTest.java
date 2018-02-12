155946c7df65388752a850b5a246962ff80925f4
Anthony Berent <aberent@chromium.org>
2018-02-12 19:34:28 +0000

    Switch to using UiCatalogue test CatalogueFeature
    All other tests capturing screen shots use @Feature("UiCatalogue").
    Update this test to match.
    
    BUG=None
    
    Change-Id: I21e1ebcd604e4593cf867fb5357dac74f85abe0e
    Reviewed-on: https://chromium-review.googlesource.com/913336
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Commit-Queue: Anthony Berent <aberent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536156}

--- chrome/android/javatests/src/org/chromium/chrome/browser/infobar/InfoBarAppearanceTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/infobar/InfoBarAppearanceTest.java
@@ -62,7 +62,7 @@ public class InfoBarAppearanceTest {
 
     @Test
     @MediumTest
-    @Feature({"InfoBars", "Catalogue"})
+    @Feature({"InfoBars", "UiCatalogue"})
     public void testFramebustBlockInfoBar() throws Exception {
         FramebustBlockInfoBar infobar = new FramebustBlockInfoBar("http://very.evil.biz");
         captureMiniAndRegularInfobar(infobar);
@@ -146,7 +146,7 @@ public class InfoBarAppearanceTest {
 
     @Test
     @MediumTest
-    @Feature({"InfoBars", "Catalogue"})
+    @Feature({"InfoBars", "UiCatalogue"})
     public void testFramebustBlockInfoBarWithLongMessages() throws Exception {
         FramebustBlockInfoBar infobar = new FramebustBlockInfoBar("https://someverylonglink"
                 + "thatwilldefinitelynotfitevenwhenremovingthefilepath.com/somemorestuff");
@@ -155,7 +155,7 @@ public class InfoBarAppearanceTest {
 
     @Test
     @MediumTest
-    @Feature({"InfoBars", "Catalogue"})
+    @Feature({"InfoBars", "UiCatalogue"})
     public void testOomInfoBar() throws TimeoutException, InterruptedException {
         ThreadUtils.runOnUiThreadBlocking(
                 () -> mTab.getInfoBarContainer().addInfoBarForTesting(new NearOomInfoBar()));
