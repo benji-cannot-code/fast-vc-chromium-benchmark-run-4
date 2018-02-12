44197e83a138dcd521e82e460380ce5105f33bd5
Bence Béky <bnc@chromium.org>
2018-02-12 21:20:19 +0000

    Reject HTTP/2 ALTSVC frames with invalid origin.
    I have locally verified that the regression test crashes
    without the change in spdy_session.cc.
    
    Bug: 810404
    Change-Id: Ic431b2c626c0ef99c82ece9dc4b545f16b6fb498
    Reviewed-on: https://chromium-review.googlesource.com/913728
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Ryan Hamilton <rch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536193}

--- net/spdy/chromium/spdy_session.cc
+++ net/spdy/chromium/spdy_session.cc
@@ -2974,6 +2974,8 @@ void SpdySession::OnAltSvc(
     if (origin.empty())
       return;
     const GURL gurl(origin);
+    if (!gurl.is_valid() || gurl.host().empty())
+      return;
     if (!gurl.SchemeIs(url::kHttpsScheme))
       return;
     SSLInfo ssl_info;
