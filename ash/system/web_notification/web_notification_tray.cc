03899e2fd5f51bb4ca667be2b5bab8fcb02ff1a5
Tetsui Ohkubo <tetsui@chromium.org>
2018-02-12 16:53:01 +0000

    Make notification center background semi-transparent.
    According to the mock (go/cros-md-notification-mock), notification
    center background should be semi-transparent.
    Especially, when blur is enabled on the device, it should show blurred
    vision of the background.
    
    It is appropriate to use app_list::IsBackgroundBlurEnabled, becuase
    app_list is moving from ui/app_list to ash/app_list (go/move-applist),
    and also it returns true only on high-end devices which have small
    performance impact when the blur is enabled.
    
    TEST=out/Release/chrome --enable-features=EnableBackgroundBlur
    BUG=779725
    
    Change-Id: I3c734c4b5df588f864e0cc959ec9af9f91531a48
    Reviewed-on: https://chromium-review.googlesource.com/844406
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536105}

--- ash/system/web_notification/web_notification_tray.cc
+++ ash/system/web_notification/web_notification_tray.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/i18n/rtl.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/threading/thread_task_runner_handle.h"
+#include "ui/app_list/app_list_features.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/display/display.h"
 #include "ui/display/screen.h"
@@ -65,6 +66,8 @@ constexpr size_t kMaximumNotificationNumber = 99;
 
 constexpr size_t kPaddingFromScreenTop = 8;  // in px. See crbug.com/754307.
 
+constexpr float kBackgroundBlurRadius = 30.f;
+
 // Flag to disable animation. Only for testing.
 bool disable_animations_for_test = false;
 
@@ -91,14 +94,20 @@ class WebNotificationBubbleWrapper {
     init_params.min_width = width;
     init_params.max_width = width;
     init_params.max_height = bubble->max_height();
-    init_params.bg_color = SkColorSetRGB(0xe7, 0xe7, 0xe7);
     init_params.show_by_click = show_by_click;
 
     views::TrayBubbleView* bubble_view = new views::TrayBubbleView(init_params);
+    bubble_view->set_color(SK_ColorTRANSPARENT);
+    bubble_view->layer()->SetFillsBoundsOpaquely(false);
     bubble_view->set_anchor_view_insets(anchor_tray->GetBubbleAnchorInsets());
     bubble_wrapper_ = std::make_unique<TrayBubbleWrapper>(
         tray, bubble_view, false /* is_persistent */);
     bubble->InitializeContents(bubble_view);
+
+    if (app_list::features::IsBackgroundBlurEnabled()) {
+      // ClientView's layer (See TrayBubbleView::InitializeAndShowBubble())
+      bubble_view->layer()->parent()->SetBackgroundBlur(kBackgroundBlurRadius);
+    }
   }
 
   MessageCenterBubble* bubble() const { return bubble_.get(); }
