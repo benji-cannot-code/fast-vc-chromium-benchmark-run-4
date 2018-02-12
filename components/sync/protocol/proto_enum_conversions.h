cf2d53c875fd683569143adb4f370754fb45f4cd
Martin Sramek <msramek@chromium.org>
2018-02-12 20:53:47 +0000

    Update the consent auditor proto according to the latest discussions
    - Replace feature name string with enum
    - Remove the string placeholder replacements
    - Rename GIVEN/REVOKED to GIVEN/NOT_GIVEN
    
    Note that the server is not in production yet, so changing the proto
    on the spot is safe.
    
    Bug: 781765
    Change-Id: Ie70f8cabe041fc649c18efe3bc11a9b5d1234304
    Reviewed-on: https://chromium-review.googlesource.com/902048
    Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Markus Heintz <markusheintz@chromium.org>
    Commit-Queue: Martin Šrámek <msramek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536183}

--- components/sync/protocol/proto_enum_conversions.h
+++ components/sync/protocol/proto_enum_conversions.h
@@ -70,6 +70,9 @@ const char* ProtoEnumToString(
     sync_pb::UserEventSpecifics::Translation::Interaction interaction);
 
 const char* ProtoEnumToString(
+    sync_pb::UserEventSpecifics::UserConsent::Feature feature);
+
+const char* ProtoEnumToString(
     sync_pb::UserEventSpecifics::UserConsent::ConsentStatus status);
 
 const char* ProtoEnumToString(
