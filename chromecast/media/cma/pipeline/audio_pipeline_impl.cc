b1f40a5d724077f253eec931529c756dad246087
yucliu <yucliu@chromium.org>
2018-02-12 23:37:19 +0000

    Reland "[Chromecast] Add API to get clear audio buffer back"
    This is a reland of fe5171ee083e50c5f705dced32876376d79bbe73.
    
    Original change's description:
    > [Chromecast] Add API to get clear audio buffer back
    > 
    > Add an optional public API to get clear audio buffer back to media
    > pipeline, if the license policy allows. This allows Cast to play
    > encrypted streams with software decoded codec (OPUS, FLAC).
    > 
    > The new API returns a decryptor which returns clear buffers
    > asynchronously. Media pipeline keeps pushing buffers to decryptor so
    > that decryptor won't be paused to wait for more buffers.
    > 
    > Media pipeline creates a StreamDecryptor for decryption. All the buffers
    > are pushed to StreamDecryptor before pushing to decoder, to keep the
    > order of input buffers.
    > 
    > There're two types of StreamDecryptor:
    > 1. BackendDecryptor, which is built on top of decryptor returned by
    > media backend.
    > 2. CdmDecryptor, which is built on top of standard CDM APIs.
    > 
    > The below describes a typical way to use StreamDecryptor in media
    > pipeline:
    > 1. Fetch buffer from frame provider.
    > 2. Push buffer to StreamDecryptor.
    > 3. Once buffer pushed to StreamDecryptor, check if there're decrypted
    > buffers. If no, go to step 1.
    > 4. Push buffer to Decoder.
    > 5. Once buffer pushed to Decoder, check if there're decrypted buffers.
    > If yes, go to step 4. If no, go to step 1.
    > 
    > BUG=internal b/64914604
    > TEST=PlayMovie, YouTube, Netflix.
    > 
    > Change-Id: Ia6e3190245d13e59a0a3ff6497bc1c7cdc78a109
    > Reviewed-on: https://chromium-review.googlesource.com/898437
    > Commit-Queue: Yuchen Liu <yucliu@chromium.org>
    > Reviewed-by: Sergey Volk <servolk@chromium.org>
    > Reviewed-by: Kenneth MacKay <kmackay@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#535639}
    
    Bug: internal b/64914604
    Change-Id: Ic11bfc319b757551a16d2efc16981a12c39725eb
    Reviewed-on: https://chromium-review.googlesource.com/914855
    Reviewed-by: Kenneth MacKay <kmackay@chromium.org>
    Reviewed-by: Sergey Volk <servolk@chromium.org>
    Commit-Queue: Yuchen Liu <yucliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536241}

--- chromecast/media/cma/pipeline/audio_pipeline_impl.cc
+++ chromecast/media/cma/pipeline/audio_pipeline_impl.cc
@@ -13,7 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromecast/media/cma/base/cma_logging.h"
 #include "chromecast/media/cma/base/coded_frame_provider.h"
 #include "chromecast/media/cma/base/decoder_config_adapter.h"
+#include "chromecast/media/cma/pipeline/backend_decryptor.h"
+#include "chromecast/media/cma/pipeline/cdm_decryptor.h"
 #include "chromecast/public/media/decoder_config.h"
+#include "chromecast/public/media/media_capabilities_shlib.h"
 #include "media/base/audio_decoder_config.h"
 
 namespace chromecast {
@@ -21,6 +24,12 @@ namespace media {
 
 namespace {
 const size_t kMaxAudioFrameSize = 32 * 1024;
+
+bool ShouldUseSoftwareDecoder(const AudioConfig& config) {
+  // If the config isn't supported by the backend natively, we'll use software
+  // decoder to support it.
+  return !MediaCapabilitiesShlib::IsSupportedAudioConfig(config);
+}
 }
 
 AudioPipelineImpl::AudioPipelineImpl(
@@ -30,7 +39,7 @@ AudioPipelineImpl::AudioPipelineImpl(
   DCHECK(audio_decoder_);
 }
 
-AudioPipelineImpl::~AudioPipelineImpl() {}
+AudioPipelineImpl::~AudioPipelineImpl() = default;
 
 ::media::PipelineStatus AudioPipelineImpl::Initialize(
     const ::media::AudioDecoderConfig& audio_config,
@@ -43,10 +52,9 @@ AudioPipelineImpl::~AudioPipelineImpl() {}
   }
 
   DCHECK(audio_config.IsValidConfig());
-  AudioConfig cast_audio_config =
+  audio_config_ =
       DecoderConfigAdapter::ToCastAudioConfig(kPrimary, audio_config);
-  encryption_scheme_ = cast_audio_config.encryption_scheme;
-  if (!audio_decoder_->SetConfig(cast_audio_config)) {
+  if (!audio_decoder_->SetConfig(audio_config_)) {
     return ::media::PIPELINE_ERROR_INITIALIZATION_FAILED;
   }
   set_state(kFlushed);
@@ -65,10 +73,8 @@ void AudioPipelineImpl::OnUpdateConfig(
     CMALOG(kLogControl) << __FUNCTION__ << " id:" << id << " "
                         << audio_config.AsHumanReadableString();
 
-    AudioConfig cast_audio_config =
-        DecoderConfigAdapter::ToCastAudioConfig(id, audio_config);
-    encryption_scheme_ = cast_audio_config.encryption_scheme;
-    bool success = audio_decoder_->SetConfig(cast_audio_config);
+    audio_config_ = DecoderConfigAdapter::ToCastAudioConfig(id, audio_config);
+    bool success = audio_decoder_->SetConfig(audio_config_);
     if (!success && !client().playback_error_cb.is_null())
       client().playback_error_cb.Run(::media::PIPELINE_ERROR_DECODE);
   }
@@ -76,7 +82,19 @@ void AudioPipelineImpl::OnUpdateConfig(
 
 const EncryptionScheme& AudioPipelineImpl::GetEncryptionScheme(
     StreamId id) const {
-  return encryption_scheme_;
+  return audio_config_.encryption_scheme;
+}
+
+std::unique_ptr<StreamDecryptor> AudioPipelineImpl::CreateDecryptor() {
+  // TODO(yucliu): Enable it on devices that support multiroom.
+  if (audio_config_.encryption_scheme.is_encrypted() &&
+      MediaPipelineBackend::CreateAudioDecryptor &&
+      ShouldUseSoftwareDecoder(audio_config_)) {
+    CMALOG(kLogControl) << __func__ << " Create backend decryptor for audio.";
+    return std::make_unique<BackendDecryptor>(audio_config_.encryption_scheme);
+  }
+
+  return std::make_unique<CdmDecryptor>();
 }
 
 void AudioPipelineImpl::UpdateStatistics() {
