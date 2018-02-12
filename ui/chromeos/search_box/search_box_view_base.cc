5e55db2bb14b1c0259febf9002fd2a439afe6125
wutao <wutao@chromium.org>
2018-02-12 20:33:00 +0000

    Reland: cros: Introduce KSV search box. KSV - part 4.
    Introduce the search box to Keyboard Shortcut Viewer (KSV).
    
    Changes:
    1. Add search box view.
    2. Handle searching shortcut.
    
    Bug: 768932
    Test: KeyboardShortcutViewTest and Manually test the search behavior.
    Change-Id: I30b4dcc1c6c32d3a4edb5c7fc5e466bee88fb1dd
    Reviewed-on: https://chromium-review.googlesource.com/913716
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536175}

--- ui/chromeos/search_box/search_box_view_base.cc
+++ ui/chromeos/search_box/search_box_view_base.cc
@@ -168,8 +168,6 @@ SearchBoxViewBase::SearchBoxViewBase(SearchBoxViewDelegate* delegate)
       search_box_(new SearchBoxTextfield(this)) {
   DCHECK(delegate_);
   SetLayoutManager(std::make_unique<views::FillLayout>());
-  SetPreferredSize(
-      gfx::Size(kSearchBoxPreferredWidth, kSearchBoxPreferredHeight));
   AddChildView(content_container_);
 
   content_container_->SetBackground(std::make_unique<SearchBoxBackground>(
@@ -283,6 +281,8 @@ void SearchBoxViewBase::SetSearchBoxActive(bool active) {
   UpdateSearchBoxBorder();
   UpdateKeyboardVisibility();
 
+  NotifyActiveChanged();
+
   content_container_->Layout();
   SchedulePaint();
 }
@@ -309,6 +309,10 @@ bool SearchBoxViewBase::OnTextfieldEvent() {
 
   SetSearchBoxActive(true);
   return true;
+}
+
+gfx::Size SearchBoxViewBase::CalculatePreferredSize() const {
+  return gfx::Size(kSearchBoxPreferredWidth, kSearchBoxPreferredHeight);
 }
 
 bool SearchBoxViewBase::OnMouseWheel(const ui::MouseWheelEvent& event) {
@@ -373,9 +377,21 @@ void SearchBoxViewBase::OnOnSearchBoxFocusedChanged() {
   SchedulePaint();
 }
 
+bool SearchBoxViewBase::IsSearchBoxTrimmedQueryEmpty() const {
+  base::string16 trimmed_query;
+  base::TrimWhitespace(search_box_->text(), base::TrimPositions::TRIM_ALL,
+                       &trimmed_query);
+  return trimmed_query.empty();
+}
+
 void SearchBoxViewBase::NotifyQueryChanged() {
   DCHECK(delegate_);
   delegate_->QueryChanged(this);
+}
+
+void SearchBoxViewBase::NotifyActiveChanged() {
+  DCHECK(delegate_);
+  delegate_->ActiveChanged(this);
 }
 
 // TODO(crbug.com/755219): Unify this with UpdateBackgroundColor.
@@ -410,13 +426,6 @@ void SearchBoxViewBase::UpdateCloseButtonVisisbility() {
   content_container_->Layout();
 }
 
-bool SearchBoxViewBase::IsSearchBoxTrimmedQueryEmpty() const {
-  base::string16 trimmed_query;
-  base::TrimWhitespace(search_box_->text(), base::TrimPositions::TRIM_ALL,
-                       &trimmed_query);
-  return trimmed_query.empty();
-}
-
 void SearchBoxViewBase::ContentsChanged(views::Textfield* sender,
                                         const base::string16& new_contents) {
   // Set search box focused when query changes.
@@ -440,6 +449,10 @@ bool SearchBoxViewBase::HandleGestureEvent(
 
 void SearchBoxViewBase::SetSearchBoxBackgroundCornerRadius(int corner_radius) {
   GetSearchBoxBackground()->set_corner_radius(corner_radius);
+}
+
+void SearchBoxViewBase::SetSearchBoxBackgroundColor(SkColor color) {
+  GetSearchBoxBackground()->set_color(color);
 }
 
 void SearchBoxViewBase::SetSearchIconImage(gfx::ImageSkia image) {
