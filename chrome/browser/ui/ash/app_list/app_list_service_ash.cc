acd99e7cd24b021b3a9a24b3a450c594e80d431f
Jiaquan He <hejq@google.com>
2018-02-12 17:38:08 +0000

    app_list: return a view delegate without passing a profile in AppListServiceImpl.
    Bug: 733662
    Change-Id: I9dfb043ad538bd3374f44e29ced4489a421dc70a
    Reviewed-on: https://chromium-review.googlesource.com/910114
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Jiaquan He <hejq@google.com>
    Cr-Commit-Position: refs/heads/master@{#536121}

--- chrome/browser/ui/ash/app_list/app_list_service_ash.cc
+++ chrome/browser/ui/ash/app_list/app_list_service_ash.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/ash/app_list/app_list_presenter_service.h"
 #include "chrome/browser/ui/ash/ash_util.h"
 #include "chrome/browser/ui/ash/launcher/chrome_launcher_controller.h"
-#include "chrome/browser/ui/ash/session_util.h"
 #include "ui/app_list/app_list_features.h"
 #include "ui/app_list/app_list_switches.h"
 #include "ui/app_list/presenter/app_list_presenter_delegate_factory.h"
@@ -43,8 +42,7 @@ class ViewDelegateFactoryImpl : public app_list::AppListViewDelegateFactory {
 
   // app_list::AppListViewDelegateFactory:
   app_list::AppListViewDelegate* GetDelegate() override {
-    return factory_->GetViewDelegate(
-        Profile::FromBrowserContext(GetActiveBrowserContext()));
+    return factory_->GetViewDelegate();
   }
 
  private:
