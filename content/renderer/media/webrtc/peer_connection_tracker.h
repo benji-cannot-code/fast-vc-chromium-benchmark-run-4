5e59c6372f037d6350aa455b27d8b60f2a060adb
mallikarjun82 <vm.arjun@samsung.com>
2018-02-13 17:28:24 +0000

    Migrate PeerConnection Tracker IPC to Mojo-part 1
    This CL migrates IPC messages from renderer to browser with basic data
    types. This is first in upcoming CLs to migrate from IPC to Mojo.
    
    Bug: 792801
    Change-Id: I79e892b4961df597c17e9ffa612be25e0d8356f9
    Reviewed-on: https://chromium-review.googlesource.com/805675
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536397}

--- content/renderer/media/webrtc/peer_connection_tracker.h
+++ content/renderer/media/webrtc/peer_connection_tracker.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "base/threading/thread_checker.h"
+#include "content/common/media/peer_connection_tracker.mojom.h"
 #include "content/public/renderer/render_thread_observer.h"
 #include "ipc/ipc_platform_file.h"
 #include "third_party/WebKit/public/platform/WebMediaStream.h"
@@ -45,6 +46,7 @@ class CONTENT_EXPORT PeerConnectionTracker
       public base::SupportsWeakPtr<PeerConnectionTracker> {
  public:
   PeerConnectionTracker();
+  PeerConnectionTracker(mojom::PeerConnectionTrackerHostAssociatedPtr host);
   ~PeerConnectionTracker() override;
 
   enum Source {
@@ -171,7 +173,6 @@ class CONTENT_EXPORT PeerConnectionTracker
   // Sends a new fragment on an RtcEventLog.
   virtual void TrackRtcEventLogWrite(RTCPeerConnectionHandler* pc_handler,
                                      const std::string& output);
-
   // For testing: Override the class that gets posted messages.
   void OverrideSendTargetForTesting(RenderThread* target);
 
@@ -220,6 +221,8 @@ class CONTENT_EXPORT PeerConnectionTracker
                                 const std::string& value);
 
   RenderThread* SendTarget();
+  const mojom::PeerConnectionTrackerHostAssociatedPtr&
+  GetPeerConnectionTrackerHost();
 
   // This map stores the local ID assigned to each RTCPeerConnectionHandler.
   typedef std::map<RTCPeerConnectionHandler*, int> PeerConnectionIdMap;
@@ -229,6 +232,8 @@ class CONTENT_EXPORT PeerConnectionTracker
   int next_local_id_;
   base::ThreadChecker main_thread_;
   RenderThread* send_target_for_test_;
+  mojom::PeerConnectionTrackerHostAssociatedPtr
+      peer_connection_tracker_host_ptr_;
 
   DISALLOW_COPY_AND_ASSIGN(PeerConnectionTracker);
 };
