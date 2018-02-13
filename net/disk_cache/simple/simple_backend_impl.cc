4a2cb0a697e186edc4dc87b4527d1b6f03093c7c
Maks Orlovich <morlovich@chromium.org>
2018-02-13 21:14:57 +0000

    SimpleCache: add feature param for disk task priority
    Bug: 811272
    Change-Id: I5cc89d66e0f1ed90f4ef9543c448a89574e8e3de
    Reviewed-on: https://chromium-review.googlesource.com/913589
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536478}

--- net/disk_cache/simple/simple_backend_impl.cc
+++ net/disk_cache/simple/simple_backend_impl.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/macros.h"
 #include "base/metrics/field_trial.h"
+#include "base/metrics/field_trial_params.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/single_thread_task_runner.h"
@@ -58,6 +59,19 @@ namespace disk_cache {
 
 namespace {
 
+const base::FeatureParam<base::TaskPriority>::Option prio_modes[] = {
+    {base::TaskPriority::USER_BLOCKING, "default"},
+    {base::TaskPriority::USER_VISIBLE, "user_visible"}};
+const base::Feature kSimpleCachePriorityExperiment = {
+    "SimpleCachePriorityExperiment", base::FEATURE_DISABLED_BY_DEFAULT};
+const base::FeatureParam<base::TaskPriority> priority_mode{
+    &kSimpleCachePriorityExperiment, "mode", base::TaskPriority::USER_BLOCKING,
+    &prio_modes};
+
+base::TaskPriority PriorityToUse() {
+  return priority_mode.Get();
+}
+
 // Maximum fraction of the cache that one entry can consume.
 const int kMaxFileRatio = 8;
 
@@ -66,7 +80,7 @@ scoped_refptr<base::SequencedTaskRunner> FallbackToInternalIfNull(
   if (cache_runner)
     return cache_runner;
   return base::CreateSequencedTaskRunnerWithTraits(
-      {base::MayBlock(), base::TaskPriority::USER_BLOCKING,
+      {base::MayBlock(), PriorityToUse(),
        base::TaskShutdownBehavior::BLOCK_SHUTDOWN});
 }
 
@@ -256,8 +270,7 @@ SimpleBackendImpl::~SimpleBackendImpl() {
 
 int SimpleBackendImpl::Init(const CompletionCallback& completion_callback) {
   worker_pool_ = base::TaskScheduler::GetInstance()->CreateTaskRunnerWithTraits(
-      {base::MayBlock(), base::WithBaseSyncPrimitives(),
-       base::TaskPriority::USER_BLOCKING,
+      {base::MayBlock(), base::WithBaseSyncPrimitives(), PriorityToUse(),
        base::TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN});
 
   index_ = std::make_unique<SimpleIndex>(
