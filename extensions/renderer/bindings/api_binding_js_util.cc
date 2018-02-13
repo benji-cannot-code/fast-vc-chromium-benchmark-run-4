6306199adb7cef71f4cd8b1ed22a41f07faf0016
Devlin Cronin <rdevlin.cronin@chromium.org>
2018-02-13 16:25:18 +0000

    [Extensions Bindings] Add a native ValidateType method
    Add a utility method exposed to JS to allow bindings to validate a type
    against an expected schema. This is used to e.g. match expected
    instances used in the declarative APIs.
    
    This eliminates one of the remaining reliances on the utils/schemaUtils
    JS files for native bindings.
    
    Bug: 810487
    
    Change-Id: Id87d38f8196f56b285e9a69221a34f643957d237
    Reviewed-on: https://chromium-review.googlesource.com/905422
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536374}

--- extensions/renderer/bindings/api_binding_js_util.cc
+++ extensions/renderer/bindings/api_binding_js_util.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/renderer/bindings/api_request_handler.h"
 #include "extensions/renderer/bindings/api_signature.h"
 #include "extensions/renderer/bindings/api_type_reference_map.h"
+#include "extensions/renderer/bindings/argument_spec.h"
 #include "extensions/renderer/bindings/declarative_event.h"
 #include "extensions/renderer/bindings/exception_handler.h"
 #include "extensions/renderer/bindings/js_runner.h"
@@ -50,7 +51,8 @@ gin::ObjectTemplateBuilder APIBindingJSUtil::GetObjectTemplateBuilder(
       .SetMethod("runCallbackWithLastError",
                  &APIBindingJSUtil::RunCallbackWithLastError)
       .SetMethod("handleException", &APIBindingJSUtil::HandleException)
-      .SetMethod("setExceptionHandler", &APIBindingJSUtil::SetExceptionHandler);
+      .SetMethod("setExceptionHandler", &APIBindingJSUtil::SetExceptionHandler)
+      .SetMethod("validateType", &APIBindingJSUtil::ValidateType);
 }
 
 void APIBindingJSUtil::SendRequest(
@@ -244,6 +246,28 @@ void APIBindingJSUtil::SetExceptionHandler(gin::Arguments* arguments,
                                            v8::Local<v8::Function> handler) {
   exception_handler_->SetHandlerForContext(
       arguments->GetHolderCreationContext(), handler);
+}
+
+void APIBindingJSUtil::ValidateType(gin::Arguments* arguments,
+                                    const std::string& type_name,
+                                    v8::Local<v8::Value> value) {
+  v8::Isolate* isolate = arguments->isolate();
+  v8::HandleScope handle_scope(isolate);
+  v8::Local<v8::Context> context = arguments->GetHolderCreationContext();
+
+  const ArgumentSpec* spec = type_refs_->GetSpec(type_name);
+  if (!spec) {
+    // We shouldn't be asked to validate unknown specs, but since this comes
+    // from JS, assume nothing.
+    NOTREACHED();
+    return;
+  }
+
+  std::string error;
+  if (!spec->ParseArgument(context, value, *type_refs_, nullptr, nullptr,
+                           &error)) {
+    arguments->ThrowTypeError(error);
+  }
 }
 
 }  // namespace extensions
