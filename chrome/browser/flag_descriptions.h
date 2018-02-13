228101d1dadd7a9b3fbacf41fb03bcdf488617fe
Kyle Horimoto <khorimoto@google.com>
2018-02-13 22:19:48 +0000

    [CrOS MultiDevice] Add a runtime flag for the MultiDevice API.
    This CL was originally written by hsuregan@ (former intern) as part of
    https://chromium-review.googlesource.com/c/chromium/src/+/608752,
    but that CL never ended up being submitted.
    
    Bug: 752273
    Change-Id: I41f80cf3924d1f34a517c5ed61f2fd89c377be1a
    Reviewed-on: https://chromium-review.googlesource.com/900383
    Reviewed-by: Jeremy Klein <jlklein@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536508}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1660,6 +1660,9 @@ extern const char kMtpWriteSupportDescription[];
 extern const char kMultideviceName[];
 extern const char kMultideviceDescription[];
 
+extern const char kMultiDeviceApiName[];
+extern const char kMultiDeviceApiDescription[];
+
 extern const char kNativeSmbName[];
 extern const char kNativeSmbDescription[];
 
