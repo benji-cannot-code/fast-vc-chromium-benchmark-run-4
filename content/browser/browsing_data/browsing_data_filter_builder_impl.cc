5b096ca2f116acb219eea850e72e9dffd2fa9be6
Peter Kasting <pkasting@chromium.org>
2018-02-13 22:42:49 +0000

    Use a lambda for "no op, return true" callbacks.
    base::DoNothing isn't usable for these, but we need not declare a separate
    function.
    
    BUG=none
    TEST=none
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I55b727c3b3f52a56a73b58f4dfe0e7f0d7256b7e
    Reviewed-on: https://chromium-review.googlesource.com/915185
    Reviewed-by: Pawel Osciak <posciak@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536525}

--- content/browser/browsing_data/browsing_data_filter_builder_impl.cc
+++ content/browser/browsing_data/browsing_data_filter_builder_impl.cc
@@ -107,10 +107,6 @@ bool MatchesPluginSiteForRegisterableDomainsAndIPs(
       (domains_and_ips.find(domain_or_ip) != domains_and_ips.end()));
 }
 
-bool NoopFilter(const GURL& url) {
-  return true;
-}
-
 }  // namespace
 
 // static
@@ -121,7 +117,7 @@ BrowsingDataFilterBuilder::Create(Mode mode) {
 
 // static
 base::Callback<bool(const GURL&)> BrowsingDataFilterBuilder::BuildNoopFilter() {
-  return base::Bind(&NoopFilter);
+  return base::Bind([](const GURL&) { return true; });
 }
 
 BrowsingDataFilterBuilderImpl::BrowsingDataFilterBuilderImpl(Mode mode)
