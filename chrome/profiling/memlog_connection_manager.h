0d077c3c187cd399bc61c8cd8b14aa2fbe84ffab
Marc Treib <treib@chromium.org>
2018-02-13 14:30:42 +0000

    Revert "Clean up of memory instrumentation interface."
    This reverts commit 99b4a583f634ba382fd597cda1148cc4299dc2e0.
    
    Reason for revert: Breaks Memlog/MemlogBrowserTest.EndToEnd/13 on Windows; see crbug.com/811711
    
    Original change's description:
    > Clean up of memory instrumentation interface.
    > 
    > This CL cleans up both the public interface and private implementation of memory
    > instrumentation. It is in preparation for allowing memory instrumentation to
    > asynchronously add heap dumps to traces.
    > 
    > memory_instrumentation supports two interfaces: HeapProfilerHelper and
    > Coordinator [specifically RequestGlobalMemoryDump]. They are used by different
    > clients, with different privileges, for different purposes. The internal
    > implementation used a single code path to support both, which further results in
    > leakage of implementation details into the mojom interface, in the form of
    > LevelOfDetail::VM_REGIONS_ONLY_FOR_HEAP_PROFILER.
    > 
    > The previous implementation only allowed 1 global dump to be issued at a time.
    > This causes problems for my upcoming refactor, which makes
    > memory_instrumentation, during a global dump, request a heap dump from the
    > profiling service. The profiling service in turn requests heaps from
    > memory_instrumentation, resulting in a deadlock.
    > 
    > This CL updates the implementation of CoordinatorImpl to use a separate queue of
    > |in_progress_vm_region_requests_| to track heap dump requests. These are
    > executed as soon as they are received. This CL also removes
    > LevelOfDetail::VM_REGIONS_ONLY_FOR_HEAP_PROFILER, and updates
    > GetVmRegionsForHeapProfiler to explicitly specify PIDs to dump.
    > 
    > Bug: 758739
    > Change-Id: I1ee41e272ac4379f791abe469e0d8efda42636de
    > Reviewed-on: https://chromium-review.googlesource.com/900222
    > Commit-Queue: Erik Chen <erikchen@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Ilya Sherman <isherman@chromium.org>
    > Reviewed-by: Primiano Tucci <primiano@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#536263}
    
    TBR=dcheng@chromium.org,isherman@chromium.org,primiano@chromium.org,erikchen@chromium.org,lalitm@chromium.org
    
    Change-Id: I6ba2c5548b4fcd2950a3e0a1734a10140c6c152a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 758739, 811711
    Reviewed-on: https://chromium-review.googlesource.com/916063
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536353}

--- chrome/profiling/memlog_connection_manager.h
+++ chrome/profiling/memlog_connection_manager.h
@@ -32,10 +32,6 @@ class SequencedTaskRunner;
 
 namespace profiling {
 
-using VmRegions =
-    std::unordered_map<base::ProcessId,
-                       std::vector<memory_instrumentation::mojom::VmRegionPtr>>;
-
 // Manages all connections and logging for each process. Pipes are supplied by
 // the pipe server and this class will connect them to a parser and logger.
 //
@@ -72,7 +68,7 @@ class MemlogConnectionManager {
       bool keep_small_allocations,
       bool strip_path_from_mapped_files,
       mojom::ProfilingService::DumpProcessesForTracingCallback callback,
-      VmRegions vm_regions);
+      memory_instrumentation::mojom::GlobalMemoryDumpPtr dump);
 
   void OnNewConnection(base::ProcessId pid,
                        mojom::ProfilingClientPtr client,
@@ -81,7 +77,6 @@ class MemlogConnectionManager {
                        mojom::ProfilingParamsPtr params);
 
   std::vector<base::ProcessId> GetConnectionPids();
-  std::vector<base::ProcessId> GetConnectionPidsThatNeedVmRegions();
 
  private:
   struct Connection;
