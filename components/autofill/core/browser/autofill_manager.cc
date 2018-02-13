d869cb6f133134f83443656e4af87ca805584357
Parastoo Geranmayeh <parastoog@google.com>
2018-02-13 23:47:39 +0000

    [Autofill] Fill the most recent version of forms.
    Problem:
    
    When filling forms, some parameters keep changing.
    A field can go from hidden to visible, or .. . These changes affect
    our decisions, and we need to make the decisions based on the most
    recent version of the form.
    
    Example: When filling the forms in www.cvs.com, both address forms
    are initially hidden, and will appear one by one. Currently, this
    causes a field-by-field situation for the second address form.
    
    Solution:
    
    Look at the most recent version of the form when filling it.
    To do so, we need not to update the whole form from the cached one,
    but only retrieve the server data, such as the form signature and
    the server type predictions.
    
    Note:
    
    Since, after this change, we will know the visibility of a field
    at the time of filling the form, IdentifySection can be simplified.
    It will no more need to handle hidden cases.
    
    Bug: 807786
    Change-Id: I94c2c877560b1705b60468f3adef87480603a974
    Reviewed-on: https://chromium-review.googlesource.com/905785
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Parastoo Geranmayeh <parastoog@google.com>
    Cr-Commit-Position: refs/heads/master@{#536556}

--- components/autofill/core/browser/autofill_manager.cc
+++ components/autofill/core/browser/autofill_manager.cc
@@ -1412,7 +1412,9 @@ std::unique_ptr<FormStructure> AutofillManager::ValidateSubmittedForm(
   if (!FindCachedForm(form, &cached_submitted_form))
     return std::unique_ptr<FormStructure>();
 
-  submitted_form->UpdateFromCache(*cached_submitted_form, false);
+  submitted_form->RetrieveFromCache(*cached_submitted_form,
+                                    /* apply_is_autofilled */ false,
+                                    /* only_server_and_autofill_state */ false);
   return submitted_form;
 }
 
@@ -1514,9 +1516,11 @@ bool AutofillManager::UpdateCachedForm(const FormData& live_form,
   if (!ParseForm(live_form, updated_form))
     return false;
 
+  // We need to keep the server data.
   if (cached_form)
-    (*updated_form)->UpdateFromCache(*cached_form, true);
-
+    (*updated_form)
+        ->RetrieveFromCache(*cached_form, /* apply_is_autofilled */ true,
+                            /* only_server_and_autofill_state */ true);
   // Annotate the updated form with its predicted types.
   driver()->SendAutofillTypePredictionsToRenderer({*updated_form});
 
