f855c0fc1f16d9af9de0f4ef84e4baa5570b2ceb
Andrey Lushnikov <lushnikov@chromium.org>
2018-02-13 08:15:35 +0000

    DevTools: support provisional breakpoings with persistence and Node.js
    This patch starts unconditionally translating fileURLs into node.js paths
    if the attached target is node.js.
    
    R=dgozman, pfeldman
    
    Change-Id: Ief2677e21001c7c1dea3e0f2b7ef74372ae89145
    Reviewed-on: https://chromium-review.googlesource.com/896594
    Commit-Queue: Andrey Lushnikov <lushnikov@chromium.org>
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536287}

--- third_party/WebKit/LayoutTests/http/tests/devtools/sources/debugger-breakpoints/nodejs-set-breakpoint.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/sources/debugger-breakpoints/nodejs-set-breakpoint.js
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 
   var functionText = 'function foobar() { \nconsole.log(\'foobar execute!\');\n}';
-  var sourceURL = '\n//# sourceURL=/usr/local/home/prog/foobar.js';
+  var sourceURL = Host.isWin() ? '\n//# sourceURL=c:\\prog\\foobar.js' : '\n//# sourceURL=/usr/local/home/prog/foobar.js';
   await TestRunner.evaluateInPageAnonymously(functionText + sourceURL);
   SourcesTestRunner.showScriptSource('foobar.js', didShowScriptSource);
 
