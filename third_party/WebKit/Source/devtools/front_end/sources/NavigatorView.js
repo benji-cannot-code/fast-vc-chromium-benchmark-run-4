d023e8988bef769b48652761689d7a0c8bc53aea
Dmitry Gozman <dgozman@chromium.org>
2018-02-13 16:10:44 +0000

    [DevTools] Show OOPIFs as children of local frames in navigator
    - Plumb devtools_frame_token to remote parent frame in child frame process.
    - Report remote parent frame id over protocol.
    - Introduce ResourceTreeFrame.crossTargetParentFrame().
    - Update NavigatorView to account for remote parent frames.
    
    Bug: none
    Change-Id: Ief2327740e16352be4052d0955ca31f9ad80791b
    Reviewed-on: https://chromium-review.googlesource.com/910449
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536367}

--- third_party/WebKit/Source/devtools/front_end/sources/NavigatorView.js
+++ third_party/WebKit/Source/devtools/front_end/sources/NavigatorView.js
@@ -485,9 +485,6 @@ Sources.NavigatorView = class extends UI.VBox {
     if (!this._groupByFrame || !frame)
       return this._targetNode(project, target);
 
-    if (!frame.parentFrame && target.parentTarget())
-      return this._targetNode(project, target);
-
     var frameNode = this._frameNodes.get(frame);
     if (frameNode)
       return frameNode;
@@ -496,8 +493,11 @@ Sources.NavigatorView = class extends UI.VBox {
         this, project, target.id() + ':' + frame.id, Sources.NavigatorView.Types.Frame, frame.displayName());
     frameNode.setHoverCallback(hoverCallback);
     this._frameNodes.set(frame, frameNode);
-    this._frameNode(project, target, frame.parentFrame).appendChild(frameNode);
-    if (!frame.parentFrame)
+
+    var parentFrame = frame.parentFrame || frame.crossTargetParentFrame();
+    this._frameNode(project, parentFrame ? parentFrame.resourceTreeModel().target() : target, parentFrame)
+        .appendChild(frameNode);
+    if (!parentFrame)
       frameNode.treeNode()._boostOrder = true;
 
     /**
