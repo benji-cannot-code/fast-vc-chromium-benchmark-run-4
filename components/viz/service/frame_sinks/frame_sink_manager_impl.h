b283f1d5621a0cef8f600b8e42d030d457d24962
Scott Violet <sky@chromium.org>
2018-02-13 19:07:36 +0000

    Adds base::MatchesUniquePtr to ease lookup by pointer
    I also added CreateMatchesUniquePtr() so that you need not specify the
    type when creating MatchesUniquePtr().
    
    BUG=none
    TEST=none
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: Ifc96ae2397d7f1855f34a4d2969ec19daa6bbeee
    Reviewed-on: https://chromium-review.googlesource.com/912516
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536407}

--- components/viz/service/frame_sinks/frame_sink_manager_impl.h
+++ components/viz/service/frame_sinks/frame_sink_manager_impl.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/flat_map.h"
 #include "base/containers/flat_set.h"
-#include "base/containers/unique_ptr_comparator.h"
+#include "base/containers/unique_ptr_adapters.h"
 #include "base/logging.h"
 #include "base/macros.h"
 #include "base/threading/thread_checker.h"
--- content/browser/shared_worker/shared_worker_service_impl.h
+++ content/browser/shared_worker/shared_worker_service_impl.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/compiler_specific.h"
-#include "base/containers/unique_ptr_comparator.h"
+#include "base/containers/unique_ptr_adapters.h"
 #include "base/macros.h"
 #include "base/memory/singleton.h"
 #include "content/browser/shared_worker/shared_worker_host.h"
