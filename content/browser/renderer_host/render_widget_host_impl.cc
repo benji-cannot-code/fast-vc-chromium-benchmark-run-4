fc93fd0b6c6897d9c808659630bad8aa3c3ea707
Christopher Cameron <ccameron@chromium.org>
2018-02-13 23:41:19 +0000

    Some RenderWidgetHostDelegate cleanup
    This is step two in the great content::ScreenInfo refactor.
    
    The method RenderWidgetHostDelegate::ScreenInfoChanged is only used
    by test code. Remove it.
    
    The method RenderWidgetHostDelegate::RenderWidgetWasResized
    re-computes the (hopefully same) ScreenInfo that was computed by the
    caller. Don't re-compute it -- pass the ScreenInfo that the caller
    used.
    
    Bug: 805774
    Change-Id: Ie3e67bd27e6f8bc1c56570a8e7c57387779d4599
    Reviewed-on: https://chromium-review.googlesource.com/915845
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536552}

--- content/browser/renderer_host/render_widget_host_impl.cc
+++ content/browser/renderer_host/render_widget_host_impl.cc
@@ -840,6 +840,7 @@ void RenderWidgetHostImpl::WasResized(bool scroll_focused_node_into_view) {
     return;
   params->scroll_focused_node_into_view = scroll_focused_node_into_view;
 
+  ScreenInfo screen_info = params->screen_info;
   bool width_changed =
       !old_resize_params_ ||
       old_resize_params_->new_size.width() != params->new_size.width();
@@ -850,7 +851,7 @@ void RenderWidgetHostImpl::WasResized(bool scroll_focused_node_into_view) {
   }
 
   if (delegate_)
-    delegate_->RenderWidgetWasResized(this, width_changed);
+    delegate_->RenderWidgetWasResized(this, screen_info, width_changed);
 }
 
 void RenderWidgetHostImpl::GotFocus() {
@@ -1569,9 +1570,6 @@ mojom::WidgetInputHandler* RenderWidgetHostImpl::GetWidgetInputHandler() {
 }
 
 void RenderWidgetHostImpl::NotifyScreenInfoChanged() {
-  if (delegate_)
-    delegate_->ScreenInfoChanged();
-
   // The resize message (which may not happen immediately) will carry with it
   // the screen info as well as the new size (if the screen has changed scale
   // factor).
--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -1946,13 +1946,12 @@ void WebContentsImpl::RenderWidgetLostFocus(
 
 void WebContentsImpl::RenderWidgetWasResized(
     RenderWidgetHostImpl* render_widget_host,
+    const ScreenInfo& screen_info,
     bool width_changed) {
   RenderFrameHostImpl* rfh = GetMainFrame();
   if (!rfh || render_widget_host != rfh->GetRenderWidgetHost())
     return;
 
-  ScreenInfo screen_info;
-  GetScreenInfo(&screen_info);
   SendPageMessage(new PageMsg_UpdateScreenInfo(MSG_ROUTING_NONE, screen_info));
 
   for (auto& observer : observers_)
