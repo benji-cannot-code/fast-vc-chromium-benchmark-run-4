7adf7b70633761d386abfdf8223eb5ac01300b08
Wenzhao Zang <wzang@chromium.org>
2018-02-13 00:36:27 +0000

    cros: Move focus to 'Take photo' button when camera selected in avatar selection
    Bug: 805497
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I2e5d2ffc4b3fac8bc561e42765b5d6bfc1fa4279
    Reviewed-on: https://chromium-review.googlesource.com/914700
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Wenzhao (Colin) Zang <wzang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536246}

--- ui/webui/resources/cr_elements/chromeos/cr_picture/cr_camera.js
+++ ui/webui/resources/cr_elements/chromeos/cr_picture/cr_camera.js
@@ -78,6 +78,7 @@ Polymer({
     this.$.cameraVideo.addEventListener('canplay', function() {
       this.$.userImageStreamCrop.classList.add('preview');
       this.cameraOnline_ = true;
+      this.focusTakePhotoButton();
     }.bind(this));
     this.startCamera();
   },
