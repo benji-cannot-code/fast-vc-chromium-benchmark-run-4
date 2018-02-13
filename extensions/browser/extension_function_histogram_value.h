46e9c90bd14e91c31e4ab2c935f300ff8b49d547
Wenzhao Zang <wzang@chromium.org>
2018-02-13 19:12:00 +0000

    cros: Implement 'My Photos' section in new wallpaper picker (Part I)
    The process of generating and showing the thumbnails for local images can be
    divided into:
    
    1) The wallpaper picker gets the names of all images files under the Downloads
       directory (implemented in this CL).
    
    2) The picker requests the thumbnails to be generated for each file name.
       (We want to separate the two steps, since generating thumbnails can be an
       expensive operation. The wallpaper picker doesn't have to wait until all
       the thumbnails to be created before showing anything.)
    
    3) When user clicks on a thumbnail, set the image corresponding to it as the
       custom wallpaper (This part will reuse the existing code.)
    
    Bug: 809793
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I0786453259d5dc1dae71bcf8c6d9624f31d91a01
    Reviewed-on: https://chromium-review.googlesource.com/905809
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Wenzhao (Colin) Zang <wzang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536410}

--- extensions/browser/extension_function_histogram_value.h
+++ extensions/browser/extension_function_histogram_value.h
@@ -1278,6 +1278,7 @@ enum HistogramValue {
   WALLPAPERPRIVATE_GETCOLLECTIONSINFO,
   WALLPAPERPRIVATE_GETIMAGESINFO,
   ACCESSIBILITY_PRIVATE_SENDSYNTHETICKEYEVENT,
+  WALLPAPERPRIVATE_GETLOCALIMAGEPATHS,
   // Last entry: Add new entries above, then run:
   // python tools/metrics/histograms/update_extension_histograms.py
   ENUM_BOUNDARY
