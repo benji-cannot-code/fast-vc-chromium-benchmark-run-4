4ada4c6a87fd8d4c1a703fed91d1cb8ba287d416
Misha Efimov <mef@chromium.org>
2018-02-13 19:50:12 +0000

    [Cronet] Change native structs to copy array elements instead of taking ownership.
    Bug: 786559
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ib0cef7304f326ba2cdd2fc1ef5ff07ff9435568c
    Reviewed-on: https://chromium-review.googlesource.com/915001
    Reviewed-by: Andrei Kapishnikov <kapishnikov@chromium.org>
    Commit-Queue: Misha Efimov <mef@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536428}

--- components/cronet/native/generated/cronet.idl_impl_struct.h
+++ components/cronet/native/generated/cronet.idl_impl_struct.h
@@ -20,22 +20,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 struct Cronet_Error {
  public:
   Cronet_Error();
+  explicit Cronet_Error(const Cronet_Error& from);
   ~Cronet_Error();
 
-  Cronet_Error_ERROR_CODE errorCode = Cronet_Error_ERROR_CODE_ERROR_CALLBACK;
+  Cronet_Error_ERROR_CODE error_code = Cronet_Error_ERROR_CODE_ERROR_CALLBACK;
   std::string message;
   int32_t internal_error_code = 0;
   bool immediately_retryable = false;
   int32_t quic_detailed_error_code = 0;
 
  private:
-  DISALLOW_COPY_AND_ASSIGN(Cronet_Error);
+  DISALLOW_ASSIGN(Cronet_Error);
 };
 
 // Struct Cronet_QuicHint.
 struct Cronet_QuicHint {
  public:
   Cronet_QuicHint();
+  explicit Cronet_QuicHint(const Cronet_QuicHint& from);
   ~Cronet_QuicHint();
 
   std::string host;
@@ -43,13 +45,14 @@ struct Cronet_QuicHint {
   int32_t alternate_port = 0;
 
  private:
-  DISALLOW_COPY_AND_ASSIGN(Cronet_QuicHint);
+  DISALLOW_ASSIGN(Cronet_QuicHint);
 };
 
 // Struct Cronet_PublicKeyPins.
 struct Cronet_PublicKeyPins {
  public:
   Cronet_PublicKeyPins();
+  explicit Cronet_PublicKeyPins(const Cronet_PublicKeyPins& from);
   ~Cronet_PublicKeyPins();
 
   std::string host;
@@ -58,13 +61,14 @@ struct Cronet_PublicKeyPins {
   int64_t expiration_date;
 
  private:
-  DISALLOW_COPY_AND_ASSIGN(Cronet_PublicKeyPins);
+  DISALLOW_ASSIGN(Cronet_PublicKeyPins);
 };
 
 // Struct Cronet_EngineParams.
 struct Cronet_EngineParams {
  public:
   Cronet_EngineParams();
+  explicit Cronet_EngineParams(const Cronet_EngineParams& from);
   ~Cronet_EngineParams();
 
   bool enable_check_result = true;
@@ -77,56 +81,59 @@ struct Cronet_EngineParams {
   Cronet_EngineParams_HTTP_CACHE_MODE http_cache_mode =
       Cronet_EngineParams_HTTP_CACHE_MODE_DISABLED;
   int64_t http_cache_max_size = 0;
-  std::vector<std::unique_ptr<Cronet_QuicHint>> quic_hints;
-  std::vector<std::unique_ptr<Cronet_PublicKeyPins>> public_key_pins;
+  std::vector<Cronet_QuicHint> quic_hints;
+  std::vector<Cronet_PublicKeyPins> public_key_pins;
   bool enable_public_key_pinning_bypass_for_local_trust_anchors = true;
   std::string experimental_options;
 
  private:
-  DISALLOW_COPY_AND_ASSIGN(Cronet_EngineParams);
+  DISALLOW_ASSIGN(Cronet_EngineParams);
 };
 
 // Struct Cronet_HttpHeader.
 struct Cronet_HttpHeader {
  public:
   Cronet_HttpHeader();
+  explicit Cronet_HttpHeader(const Cronet_HttpHeader& from);
   ~Cronet_HttpHeader();
 
   std::string name;
   std::string value;
 
  private:
-  DISALLOW_COPY_AND_ASSIGN(Cronet_HttpHeader);
+  DISALLOW_ASSIGN(Cronet_HttpHeader);
 };
 
 // Struct Cronet_UrlResponseInfo.
 struct Cronet_UrlResponseInfo {
  public:
   Cronet_UrlResponseInfo();
+  explicit Cronet_UrlResponseInfo(const Cronet_UrlResponseInfo& from);
   ~Cronet_UrlResponseInfo();
 
   std::string url;
   std::vector<std::string> url_chain;
   int32_t http_status_code;
   std::string http_status_text;
-  std::vector<std::unique_ptr<Cronet_HttpHeader>> all_headers_list;
+  std::vector<Cronet_HttpHeader> all_headers_list;
   bool was_cached;
   std::string negotiated_protocol;
   std::string proxy_server;
   int64_t received_byte_count;
 
  private:
-  DISALLOW_COPY_AND_ASSIGN(Cronet_UrlResponseInfo);
+  DISALLOW_ASSIGN(Cronet_UrlResponseInfo);
 };
 
 // Struct Cronet_UrlRequestParams.
 struct Cronet_UrlRequestParams {
  public:
   Cronet_UrlRequestParams();
+  explicit Cronet_UrlRequestParams(const Cronet_UrlRequestParams& from);
   ~Cronet_UrlRequestParams();
 
   std::string http_method;
-  std::vector<std::unique_ptr<Cronet_HttpHeader>> request_headers;
+  std::vector<Cronet_HttpHeader> request_headers;
   bool disable_cache = false;
   Cronet_UrlRequestParams_REQUEST_PRIORITY priority =
       Cronet_UrlRequestParams_REQUEST_PRIORITY_REQUEST_PRIORITY_MEDIUM;
@@ -136,17 +143,18 @@ struct Cronet_UrlRequestParams {
   std::vector<Cronet_RawDataPtr> annotations;
 
  private:
-  DISALLOW_COPY_AND_ASSIGN(Cronet_UrlRequestParams);
+  DISALLOW_ASSIGN(Cronet_UrlRequestParams);
 };
 
 // Struct Cronet_RequestFinishedInfo.
 struct Cronet_RequestFinishedInfo {
  public:
   Cronet_RequestFinishedInfo();
+  explicit Cronet_RequestFinishedInfo(const Cronet_RequestFinishedInfo& from);
   ~Cronet_RequestFinishedInfo();
 
  private:
-  DISALLOW_COPY_AND_ASSIGN(Cronet_RequestFinishedInfo);
+  DISALLOW_ASSIGN(Cronet_RequestFinishedInfo);
 };
 
 #endif  // COMPONENTS_CRONET_NATIVE_GENERATED_CRONET_IDL_IMPL_STRUCT_H_
