af8cb1abb31b45f1800422551a89faeedc8c3d79
Justin Cohen <justincohen@google.com>
2018-02-14 14:18:54 +0000

    [ios] Show toolbar in ntp header view.
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I913bc2bbe5d6ef5a2434d44c020c34bf5299343e
    Reviewed-on: https://chromium-review.googlesource.com/916712
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536707}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_header_view.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_header_view.mm
@@ -35,14 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return self;
 }
 
-- (void)traitCollectionDidChange:(UITraitCollection*)previousTraitCollection {
-  [super traitCollectionDidChange:previousTraitCollection];
-  // TODO(crbug.com/808429) This logic is specific to phone.  This will need to
-  // be updated when tablet also shows the |toolBarView|.
-  self.toolBarView.hidden =
-      self.traitCollection.verticalSizeClass == UIUserInterfaceSizeClassRegular;
-}
-
 #pragma mark - NTPHeaderViewAdapter
 
 - (void)addToolbarView:(UIView*)toolbarView {
