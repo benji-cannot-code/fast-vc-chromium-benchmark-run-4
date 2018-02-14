ef637af43cf546f09712bdba2f5c78598584a407
Khushal <khushalsagar@chromium.org>
2018-02-14 00:40:58 +0000

    Revert "content: Enable CompositorImageAnimation by default."
    This reverts commit aae978e7a9b6b80bc876c67ff6a2f7751ff58aa7.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Original change's description:
    > content: Enable CompositorImageAnimation by default.
    > 
    > BUG:776216
    > R=​piman@chromium.org
    > 
    > Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    > Change-Id: I880d15a5eecd877687d83b96313b452ebde9a2c8
    > Reviewed-on: https://chromium-review.googlesource.com/900205
    > Commit-Queue: Khushal <khushalsagar@chromium.org>
    > Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    > Reviewed-by: enne <enne@chromium.org>
    > Reviewed-by: Antoine Labour <piman@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#536526}
    
    TBR=chrishtr@chromium.org,enne@chromium.org,khushalsagar@chromium.org,piman@chromium.org
    
    Change-Id: I895a368307374ea2824fd84bfd1218ed6dcce869
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Reviewed-on: https://chromium-review.googlesource.com/917581
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536574}

--- cc/trees/layer_tree_host_unittest.cc
+++ cc/trees/layer_tree_host_unittest.cc
@@ -6480,12 +6480,11 @@ class LayerTreeHostTestSynchronousCompositeSwapPromise
 
   void BeginTest() override {
     // Successful composite.
-    const bool raster = true;
     std::unique_ptr<SwapPromise> swap_promise0(
         new TestSwapPromise(&swap_promise_result_[0]));
     layer_tree_host()->GetSwapPromiseManager()->QueueSwapPromise(
         std::move(swap_promise0));
-    layer_tree_host()->Composite(base::TimeTicks::Now(), raster);
+    layer_tree_host()->Composite(base::TimeTicks::Now());
 
     // Fail to swap (no damage) if not reclaiming resources from the Display.
     std::unique_ptr<SwapPromise> swap_promise1(
@@ -6493,7 +6492,7 @@ class LayerTreeHostTestSynchronousCompositeSwapPromise
     layer_tree_host()->GetSwapPromiseManager()->QueueSwapPromise(
         std::move(swap_promise1));
     layer_tree_host()->SetNeedsCommit();
-    layer_tree_host()->Composite(base::TimeTicks::Now(), raster);
+    layer_tree_host()->Composite(base::TimeTicks::Now());
 
     // Fail to draw (not visible).
     std::unique_ptr<SwapPromise> swap_promise2(
@@ -6502,7 +6501,7 @@ class LayerTreeHostTestSynchronousCompositeSwapPromise
         std::move(swap_promise2));
     layer_tree_host()->SetNeedsDisplayOnAllLayers();
     layer_tree_host()->SetVisible(false);
-    layer_tree_host()->Composite(base::TimeTicks::Now(), raster);
+    layer_tree_host()->Composite(base::TimeTicks::Now());
 
     EndTest();
   }
--- third_party/WebKit/Source/core/frame/WebFrameWidgetImpl.cpp
+++ third_party/WebKit/Source/core/frame/WebFrameWidgetImpl.cpp
@@ -300,11 +300,6 @@ void WebFrameWidgetImpl::UpdateLifecycle(LifecycleUpdate requested_update) {
   UpdateLayerTreeBackgroundColor();
 }
 
-void WebFrameWidgetImpl::UpdateAllLifecyclePhasesAndCompositeForTesting() {
-  if (layer_tree_view_)
-    layer_tree_view_->SynchronouslyCompositeNoRasterForTesting();
-}
-
 void WebFrameWidgetImpl::Paint(WebCanvas* canvas, const WebRect& rect) {
   // Out-of-process iframes require compositing.
   NOTREACHED();
