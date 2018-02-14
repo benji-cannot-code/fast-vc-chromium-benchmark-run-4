89d60b9ecea520ef367e77e458b38fb93efcca1a
Shimi Zhang <ctzsm@chromium.org>
2018-02-14 02:33:29 +0000

    Disable ModalDialogManagerTest#testShow_UrlBarFocused
    TBR=ctzsm@chromium.org
    
    Bug: 812066
    Change-Id: I52b90c8b76b59fd81c26ab66e0d1bef3f4bc1a3c
    Reviewed-on: https://chromium-review.googlesource.com/917667
    Reviewed-by: Shimi Zhang <ctzsm@chromium.org>
    Commit-Queue: Shimi Zhang <ctzsm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536621}

--- chrome/android/javatests/src/org/chromium/chrome/browser/modaldialog/ModalDialogManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/modaldialog/ModalDialogManagerTest.java
@@ -236,6 +236,7 @@ public class ModalDialogManagerTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "crbug.com/812066")
     public void testShow_UrlBarFocused() throws Exception {
         // Show a dialog. The dialog should be shown on top of the toolbar.
         showDialog(0, ModalDialogManager.TAB_MODAL);
