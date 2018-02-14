6b6b87e5ca3ff6f3ca76d47f03d2f4b2c286a7d8
Yuri Wiitala <miu@chromium.org>
2018-02-14 20:52:27 +0000

    Migrate RWHVChildFrame to use new CopyOutputRequest APIs.
    This change brings RenderWidgetHostViewChildFrame::CopyFromSurface()
    in-sync with DelegatedFrameHost. It switches over to using the new
    CopyOutputRequest scaling APIs, freeing up one of the last dependencies
    on the "GLHelper mass of code" in surface_utils.cc.
    
    Bug: 759310, 812059
    Change-Id: I7fc5e997cf1e442dfd674bdc131e9de7f6d349eb
    Reviewed-on: https://chromium-review.googlesource.com/918361
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Yuri Wiitala <miu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536810}

--- content/browser/renderer_host/render_widget_host_view_child_frame.cc
+++ content/browser/renderer_host/render_widget_host_view_child_frame.cc
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/service_manager/runner/common/client_util.h"
 #include "third_party/WebKit/public/platform/WebTouchEvent.h"
 #include "ui/base/ui_base_switches_util.h"
+#include "ui/gfx/geometry/dip_util.h"
 #include "ui/gfx/geometry/size_conversions.h"
 #include "ui/gfx/geometry/size_f.h"
 #include "ui/touch_selection/touch_selection_controller.h"
@@ -838,6 +839,12 @@ void RenderWidgetHostViewChildFrame::CopyFromSurface(
     const gfx::Size& output_size,
     const ReadbackRequestCallback& callback,
     const SkColorType preferred_color_type) {
+  // TODO(crbug/759310): Only NavigationEntryScreenshotManager needs grayscale.
+  // Move that transformation to there; and then remove |preferred_color_type|
+  // from this API and the end-to-end code path.
+  DCHECK(preferred_color_type == kN32_SkColorType ||
+         preferred_color_type == kAlpha_8_SkColorType);
+
   if (!IsSurfaceAvailableForCopy()) {
     // Defer submitting the copy request until after a frame is drawn, at which
     // point we should be guaranteed that the surface is available.
@@ -856,16 +863,35 @@ void RenderWidgetHostViewChildFrame::SubmitSurfaceCopyRequest(
     const gfx::Size& output_size,
     const ReadbackRequestCallback& callback,
     const SkColorType preferred_color_type) {
+  // TODO(crbug.com/812059): Need a "copy from surface" VIZ API.
+  if (enable_viz_) {
+    callback.Run(SkBitmap(), content::READBACK_SURFACE_UNAVAILABLE);
+    return;
+  }
+
   DCHECK(IsSurfaceAvailableForCopy());
   DCHECK(support_);
 
   std::unique_ptr<viz::CopyOutputRequest> request =
       std::make_unique<viz::CopyOutputRequest>(
-          viz::CopyOutputRequest::ResultFormat::RGBA_TEXTURE,
-          base::BindOnce(&CopyFromCompositingSurfaceHasResult, output_size,
+          viz::CopyOutputRequest::ResultFormat::RGBA_BITMAP,
+          base::BindOnce(&CopyFromCompositingSurfaceHasResult, gfx::Size(),
                          preferred_color_type, callback));
-  if (!src_subrect.IsEmpty())
-    request->set_area(src_subrect);
+
+  if (src_subrect.IsEmpty()) {
+    request->set_area(gfx::Rect(current_surface_size_));
+  } else {
+    // |src_subrect| is in DIP coordinates; convert to Surface coordinates.
+    request->set_area(
+        gfx::ScaleToRoundedRect(src_subrect, current_surface_scale_factor_));
+  }
+
+  if (!output_size.IsEmpty()) {
+    request->set_result_selection(gfx::Rect(output_size));
+    request->SetScaleRatio(
+        gfx::Vector2d(request->area().width(), request->area().height()),
+        gfx::Vector2d(output_size.width(), output_size.height()));
+  }
 
   support_->RequestCopyOfSurface(std::move(request));
 }
