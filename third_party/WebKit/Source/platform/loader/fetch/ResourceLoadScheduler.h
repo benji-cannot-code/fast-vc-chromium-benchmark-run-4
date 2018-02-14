ab1653e08b9cc1820fb6822a38bc739b137c4ff9
Yutaka Hirano <yhirano@chromium.org>
2018-02-14 05:28:27 +0000

    ResourceLoadScheduler: Count the number of throttlable requests
    In order to mimic content::ResourceScheduler, this CL has
    ResourceLoadScheduler count the number of throttlable requests instead
    of the total number of requests when |frame_scheduler_throttling_state_|
    is |kNotThrottled|.
    
    Bug: 785770
    Change-Id: I58a6d4e1f7792443d1051030d8705074e536259e
    Reviewed-on: https://chromium-review.googlesource.com/915882
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536655}

--- third_party/WebKit/Source/platform/loader/fetch/ResourceLoadScheduler.h
+++ third_party/WebKit/Source/platform/loader/fetch/ResourceLoadScheduler.h
@@ -244,7 +244,7 @@ class PLATFORM_EXPORT ResourceLoadScheduler final
   void MaybeRun();
 
   // Grants a client to run,
-  void Run(ClientId, ResourceLoadSchedulerClient*);
+  void Run(ClientId, ResourceLoadSchedulerClient*, bool throttlable);
 
   size_t GetOutstandingLimit() const;
 
@@ -276,6 +276,8 @@ class PLATFORM_EXPORT ResourceLoadScheduler final
 
   // Holds clients that were granted and are running.
   HashSet<ClientId> running_requests_;
+
+  HashSet<ClientId> running_throttlable_requests_;
 
   // Largest number of running requests seen so far.
   unsigned maximum_running_requests_seen_ = 0;
