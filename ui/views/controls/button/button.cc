917710451c0be1eac05fdb7749a1812d88dc77eb
Aaron Leventhal <aleventhal@chromium.org>
2018-02-14 20:29:07 +0000

    Remove extra ax_node_data_include
    TBR=tapted
    
    Bug: None
    Change-Id: I8a567ca1e533de447fbd486f4a0235f538c7faaa
    Reviewed-on: https://chromium-review.googlesource.com/919185
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536801}

--- ui/views/controls/button/button.cc
+++ ui/views/controls/button/button.cc
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/utf_string_conversions.h"
 #include "ui/accessibility/ax_node_data.h"
-
-#include "ui/accessibility/ax_node_data.h"
 #include "ui/base/class_property.h"
 #include "ui/events/event.h"
 #include "ui/events/event_utils.h"
