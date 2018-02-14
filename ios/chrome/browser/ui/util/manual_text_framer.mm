55bdc9d583c56e0b629accf282ebb99b2636ff38
Kurt Horimoto <kkhorimoto@chromium.org>
2018-02-14 23:50:05 +0000

    [iOS] Remove unecessary include.
    base/i18n/rtl.h was never actually used in ManualTextFramer.
    
    Bug: none
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I4477a659bc619e45e8c36aa6f7acaf0e8d7ce0f2
    Reviewed-on: https://chromium-review.googlesource.com/919428
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536894}

--- ios/chrome/browser/ui/util/manual_text_framer.mm
+++ ios/chrome/browser/ui/util/manual_text_framer.mm
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <UIKit/UIKit.h>
 
-#include "base/i18n/rtl.h"
 #include "base/logging.h"
 #include "base/mac/foundation_util.h"
 #import "ios/chrome/browser/ui/util/core_text_util.h"
