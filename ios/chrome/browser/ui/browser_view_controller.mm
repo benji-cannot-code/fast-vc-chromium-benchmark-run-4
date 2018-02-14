600d35a2c80f86a0c96f2e00e7dcf0f7aca1b22c
Gauthier Ambard <gambard@chromium.org>
2018-02-14 07:56:33 +0000

    Dismiss navigation menu when clearing presented state
    When the app is foregrouneded, the presented state is cleared,
    dismissing all the presented modals. The navigation popup wasn't
    dismissed. This CL fixes it.
    
    Bug: 810375
    Change-Id: Iddd42daeb722f6b0f03bb387f816bc0629493d8c
    Reviewed-on: https://chromium-review.googlesource.com/916041
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536673}

--- ios/chrome/browser/ui/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view_controller.mm
@@ -1450,6 +1450,7 @@ applicationCommandEndpoint:(id<ApplicationCommands>)applicationCommandEndpoint {
   [_printController dismissAnimated:YES];
   _printController = nil;
   [self.dispatcher dismissToolsMenu];
+  [self.dispatcher dismissHistoryPopup];
   [_contextMenuCoordinator stop];
   [self dismissRateThisAppDialog];
 
--- ios/chrome/browser/ui/commands/history_popup_commands.h
+++ ios/chrome/browser/ui/commands/history_popup_commands.h
@@ -19,6 +19,9 @@ class NavigationItem;
 // Shows the tab history popup containing the tab's forward history.
 - (void)showTabHistoryPopupForForwardHistory;
 
+// Dismiss the presented Tab History Popup.
+- (void)dismissHistoryPopup;
+
 // Navigate back/forward to the selected entry in the tab's history.
 - (void)navigateToHistoryItem:(const web::NavigationItem*)item;
 @end
