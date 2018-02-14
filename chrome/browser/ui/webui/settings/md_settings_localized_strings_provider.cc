4a357f2e067fa0519b5f58c04d02ed338e056b8a
Kush Sinha <sinhak@chromium.org>
2018-02-14 10:13:13 +0000

    Use AccountConsistencyModeManager for checking Mirror in Settings page
    Mirror is now available on Chrome OS via a preference. Use
    of truth for checking whether Mirror is enabled in the Settings page.
    
    AccountConsistencyModeManager: :IsMirrorEnabledForProfile as the source
    Change-Id: I4a8c2ddd4242953bd13fdb06ace2ef9d715850b3
    Reviewed-on: https://chromium-review.googlesource.com/913608
    Reviewed-by: Scott Chen <scottchen@chromium.org>
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536692}

--- chrome/browser/ui/webui/settings/md_settings_localized_strings_provider.cc
+++ chrome/browser/ui/webui/settings/md_settings_localized_strings_provider.cc
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/ownership/owner_settings_service_chromeos_factory.h"
 #include "chrome/browser/chromeos/policy/browser_policy_connector_chromeos.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
+#include "chrome/browser/signin/account_consistency_mode_manager.h"
 #include "chrome/browser/ui/webui/chromeos/bluetooth_dialog_localized_strings_provider.h"
 #include "chrome/browser/ui/webui/chromeos/network_element_localized_strings_provider.h"
 #include "chromeos/chromeos_switches.h"
@@ -464,15 +465,13 @@ void AddChangePasswordStrings(content::WebUIDataSource* html_source) {
 #endif
 }
 
-void AddClearBrowsingDataStrings(content::WebUIDataSource* html_source) {
+void AddClearBrowsingDataStrings(content::WebUIDataSource* html_source,
+                                 Profile* profile) {
   int clear_cookies_summary_msg_id =
       IDS_SETTINGS_CLEAR_COOKIES_AND_SITE_DATA_SUMMARY_BASIC;
 
 #if defined(OS_CHROMEOS)
-  // Mirror account reconciliation behavior is turned on for child accounts on
-  // Chrome OS.
-  if (user_manager::UserManager::Get()->GetPrimaryUser()->GetType() ==
-      user_manager::USER_TYPE_CHILD) {
+  if (AccountConsistencyModeManager::IsMirrorEnabledForProfile(profile)) {
     clear_cookies_summary_msg_id =
         IDS_SETTINGS_CLEAR_COOKIES_AND_SITE_DATA_MIRROR_SUMMARY_BASIC;
   }
@@ -2268,7 +2267,7 @@ void AddLocalizedStrings(content::WebUIDataSource* html_source,
 #endif  // defined(OS_WIN) && defined(GOOGLE_CHROME_BUILD)
 
   AddChangePasswordStrings(html_source);
-  AddClearBrowsingDataStrings(html_source);
+  AddClearBrowsingDataStrings(html_source, profile);
   AddCommonStrings(html_source, profile);
   AddDownloadsStrings(html_source);
   AddLanguagesStrings(html_source);
