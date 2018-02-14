7ec0b165fc39d08fd152528d0e6a3161c5a35036
Dave Schuyler <dschuyler@chromium.org>
2018-02-14 23:09:41 +0000

    [Views] removed unused is_first_paint_text_ member
    It looks like some test code was added in
    https://codereview.chromium.org/795823002
    and later (partially) removed by
    https://chromium-review.googlesource.com/c/chromium/src/+/651337
    this Cl finishes removing the test code.
    There was a note that test code was to help with crbug.com/441028 (fyi).
    
    (code health)
    
    Bug: None
    Change-Id: I46391a9d3adc81e26b0f494e9aad2f52a5b693ca
    Reviewed-on: https://chromium-review.googlesource.com/910043
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Dave Schuyler <dschuyler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536865}

--- ui/views/controls/label.h
+++ ui/views/controls/label.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_VIEWS_CONTROLS_LABEL_H_
 #define UI_VIEWS_CONTROLS_LABEL_H_
 
+#include <memory>
+
 #include "base/compiler_specific.h"
 #include "base/gtest_prod_util.h"
 #include "base/macros.h"
@@ -366,10 +368,6 @@ class VIEWS_EXPORT Label : public View,
   bool collapse_when_hidden_;
   int fixed_width_;
   int max_width_;
-
-  // TODO(ckocagil): Remove is_first_paint_text_ before crbug.com/441028 is
-  // closed.
-  bool is_first_paint_text_;
 
   std::unique_ptr<SelectionController> selection_controller_;
 
