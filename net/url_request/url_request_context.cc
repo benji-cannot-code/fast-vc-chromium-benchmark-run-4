12b228baa319be328dbd7e2635b0fe2f9448867a
Matt Menke <mmenke@chromium.org>
2018-02-14 00:43:31 +0000

    Add URLRequest creation stack traces to AssertNoURLRequests crashes.
    We have a top crasher here (Again...), and need to figure out just
    what's going on. Hopefully once we ship the network service, we'll
    never see these crashes again (Well, at least in the network
    process).
    
    Bug: 800391
    Change-Id: Ifbd45274ac73ca90c7103aea66ba0f7d254c9e6e
    Reviewed-on: https://chromium-review.googlesource.com/916894
    Reviewed-by: Eric Roman <eroman@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536575}

--- net/url_request/url_request_context.cc
+++ net/url_request/url_request_context.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "base/debug/alias.h"
+#include "base/debug/stack_trace.h"
 #include "base/memory/ptr_util.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
@@ -143,9 +144,12 @@ void URLRequestContext::AssertNoURLRequests() const {
     // many we leaked so we have an idea of how bad it is.
     const URLRequest* request = *url_requests_->begin();
     int load_flags = request->load_flags();
+    base::debug::StackTrace creation_stack_trace =
+        request->creation_stack_trace();
     DEBUG_ALIAS_FOR_GURL(url_buf, request->url());
     base::debug::Alias(&num_requests);
     base::debug::Alias(&load_flags);
+    base::debug::Alias(&creation_stack_trace);
     CHECK(false) << "Leaked " << num_requests << " URLRequest(s). First URL: "
                  << request->url().spec().c_str() << ".";
   }
