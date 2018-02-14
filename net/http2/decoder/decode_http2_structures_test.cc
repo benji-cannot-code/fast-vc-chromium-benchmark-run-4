5cae5661976b4c880d315029f75ede7cbd947df6
Dianna Hu <diannahu@chromium.org>
2018-02-14 03:08:30 +0000

    Remove unused "using" declarations in a test.
    This CL lands server change 183704709 by assar.
    
    BUG=488484
    
    Change-Id: I66e02490abd25bde171c83cbcf0b368c21c7e012
    Reviewed-on: https://chromium-review.googlesource.com/916543
    Reviewed-by: Bence Béky <bnc@chromium.org>
    Commit-Queue: Dianna Hu <diannahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536630}

--- net/http2/decoder/decode_http2_structures_test.cc
+++ net/http2/decoder/decode_http2_structures_test.cc
@@ -21,9 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http2/tools/http2_random.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-using ::testing::AssertionFailure;
 using ::testing::AssertionResult;
-using ::testing::AssertionSuccess;
 
 namespace net {
 namespace test {
