cbb13d45c956daddce58bf2b8d20cee3b3ceb692
Matt Falkenhagen <falken@chromium.org>
2018-02-14 04:38:54 +0000

    Fetch API: Don't DCHECK on a 302 response with a Location header with empty value.
    Blink was failing a DCHECK when it expected a redirect to be taken because
    the request mode was 'follow' and the response had a redirect status code
    and Location header. However, Chrome's //net considers a Location header
    with an empty value to be the same as no Location header at all. See the
    comments on bug 810288 about net::HttpResponseHeaders::IsRedirect().
    
    WPT tests are added. The spec is unclear (https://github.com/whatwg/url/issues/373)
    but seems to either say to fail with network error or enter a loop of self-redirects
    that will eventually hit the redirect limit and fail with network error.
    Chrome currently fails this test because it treats the Location as
    as non-existent, i.e., not a redirect response.
    
    Bug: 810288,707185
    Change-Id: Iaca93976aaa697380b1542ab45d63c318f5050b0
    Reviewed-on: https://chromium-review.googlesource.com/910753
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536644}

--- third_party/WebKit/Source/core/fetch/FetchManager.cpp
+++ third_party/WebKit/Source/core/fetch/FetchManager.cpp
@@ -55,6 +55,13 @@ namespace blink {
 
 namespace {
 
+bool HasNonEmptyLocationHeader(const FetchHeaderList* headers) {
+  String value;
+  if (!headers->Get(HTTPNames::Location, value))
+    return false;
+  return !value.IsEmpty();
+}
+
 class SRIBytesConsumer final : public BytesConsumer {
  public:
   // BytesConsumer implementation
@@ -452,7 +459,7 @@ void FetchManager::Loader::DidReceiveResponse(
   FetchResponseData* tainted_response = nullptr;
 
   DCHECK(!(NetworkUtils::IsRedirectResponseCode(response_http_status_code_) &&
-           response_data->HeaderList()->Has(HTTPNames::Location) &&
+           HasNonEmptyLocationHeader(response_data->HeaderList()) &&
            request_->Redirect() != network::mojom::FetchRedirectMode::kManual));
 
   if (NetworkUtils::IsRedirectResponseCode(response_http_status_code_) &&
