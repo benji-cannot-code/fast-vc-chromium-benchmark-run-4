90055e7ed48cda8a096e0e8e686762d465b9f915
wutao <wutao@chromium.org>
2018-02-14 17:57:11 +0000

    cros: Allow client to set custom_frame_view color.
    This cl allows client to set custom_frame_view color.
    Changes:
    1. Add OnWindowPropertyChanged to CustomFrameViewAshWindowStateDelegate.
    2. Client can set custom_frame_view color by setting window property
    aura::client::kTopViewColor.
    
    Bug: 811159, 768932
    Test: CustomFrameViewAshTest.KTopViewColor and manually test color update.
    Change-Id: Ic2306c399b0d4cda88ffb0f96c48a15e3e6f430b
    Reviewed-on: https://chromium-review.googlesource.com/912518
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Reviewed-by: Mitsuru Oshima (In Tokyo) <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536748}

--- ash/frame/custom_frame_view_ash.h
+++ ash/frame/custom_frame_view_ash.h
@@ -123,6 +123,9 @@ class ASH_EXPORT CustomFrameViewAsh : public views::NonClientFrameView,
 
   const views::View* GetAvatarIconViewForTest() const;
 
+  SkColor GetActiveFrameColorForTest() const;
+  SkColor GetInactiveFrameColorForTest() const;
+
  private:
   class AvatarObserver;
   class OverlayView;
