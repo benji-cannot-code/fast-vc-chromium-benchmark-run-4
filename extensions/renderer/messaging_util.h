ba2a76752ea3d4270c308674d99491aabeb9055a
Mustaq Ahmed <mustaq@google.com>
2018-02-14 21:59:08 +0000

    [Extensions] Fix user gesture indication for extension messaging
    blink::WebUserGestureIndicator::IsProcessingUserGesture() expects a
    WebFrame associated when looking for if a user gesture is being
    processed with UserActivationV2.  This CL Updates messaging_util to
    pass in the current frame when constructing an extension message.
    
    Bug: 806938
    Change-Id: I748f78c6a2e2ec440cf4a156350e8197c8490fb3
    Reviewed-on: https://chromium-review.googlesource.com/907648
    Commit-Queue: Mustaq Ahmed <mustaq@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536835}

--- extensions/renderer/messaging_util.h
+++ extensions/renderer/messaging_util.h
@@ -11,6 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "v8/include/v8.h"
 
+namespace blink {
+class WebLocalFrame;
+}
+
 namespace extensions {
 class ScriptContext;
 struct Message;
@@ -37,8 +41,10 @@ std::unique_ptr<Message> MessageFromV8(v8::Local<v8::Context> context,
                                        v8::Local<v8::Value> value,
                                        std::string* error);
 // Same as above, but expects a serialized JSON string instead of a value.
-std::unique_ptr<Message> MessageFromJSONString(v8::Local<v8::String> json,
-                                               std::string* error);
+std::unique_ptr<Message> MessageFromJSONString(
+    v8::Local<v8::String> json,
+    std::string* error,
+    blink::WebLocalFrame* web_frame = nullptr);
 
 // Converts a message to a v8 value. This is expected not to fail, since it
 // should only be used for messages that have been validated.
