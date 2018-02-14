5c399ce36d0aaa1a5a66754f3fcdfeb6a842d87e
Mustaq Ahmed <mustaq@google.com>
2018-02-14 16:20:06 +0000

    Move user activation check to RemoteFrame::Navigate's callers.
    Currently RemoteFrame::Navigate is the user of
    Frame::HasTransientUserActivation that passes a RemoteFrame*, and
    it seems wrong because the user activation (user gesture) needed by
    the navigation should belong to the LocalFrame that initiated the
    navigation.
    
    Follow-up CLs after this one will update UserActivation code in
    Frame to take a LocalFrame* instead of a Frame*, and get rid of
    redundant IPCs.
    
    Bug: 811414
    Change-Id: I771c1694043edb54374a44213d16715d9c7da704
    Reviewed-on: https://chromium-review.googlesource.com/914736
    Commit-Queue: Mustaq Ahmed <mustaq@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536728}

--- third_party/WebKit/Source/core/frame/RemoteFrame.cpp
+++ third_party/WebKit/Source/core/frame/RemoteFrame.cpp
@@ -75,8 +75,6 @@ void RemoteFrame::Navigate(const FrameLoadRequest& passed_request) {
   FrameLoader::UpgradeInsecureRequest(frame_request.GetResourceRequest(),
                                       frame_request.OriginDocument());
 
-  frame_request.GetResourceRequest().SetHasUserGesture(
-      Frame::HasTransientUserActivation(this));
   Client()->Navigate(frame_request.GetResourceRequest(),
                      frame_request.ReplacesCurrentItem());
 }
