6eb90d11aa2ab6f21c51f8858c375541d3d796d9
Alexander Shabalin <alshabalin@yandex-team.ru>
2018-02-14 02:22:30 +0000

    Switch BaseScrollBar to SimpleMenuModel from MenuItemView.
    On MacViewsBrowser this makes menu be NSMenu-based.
    
    Change-Id: I9f5d322e880fd2d79bf6820fa12f213797bb9d47
    Reviewed-on: https://chromium-review.googlesource.com/915343
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536619}

--- ui/views/controls/scrollbar/base_scroll_bar.h
+++ ui/views/controls/scrollbar/base_scroll_bar.h
@@ -8,10 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/gtest_prod_util.h"
 #include "base/macros.h"
+#include "ui/base/models/simple_menu_model.h"
 #include "ui/views/animation/scroll_animator.h"
 #include "ui/views/context_menu_controller.h"
 #include "ui/views/controls/button/image_button.h"
-#include "ui/views/controls/menu/menu_delegate.h"
 #include "ui/views/controls/scrollbar/scroll_bar.h"
 #include "ui/views/repeat_controller.h"
 
@@ -31,7 +31,7 @@ class MenuRunner;
 class VIEWS_EXPORT BaseScrollBar : public ScrollBar,
                                    public ScrollDelegate,
                                    public ContextMenuController,
-                                   public MenuDelegate {
+                                   public ui::SimpleMenuModel::Delegate {
  public:
   explicit BaseScrollBar(bool horizontal);
   ~BaseScrollBar() override;
@@ -91,10 +91,10 @@ class VIEWS_EXPORT BaseScrollBar : public ScrollBar,
                               const gfx::Point& point,
                               ui::MenuSourceType source_type) override;
 
-  // Menu::Delegate overrides:
-  base::string16 GetLabel(int id) const override;
-  bool IsCommandEnabled(int id) const override;
-  void ExecuteCommand(int id) override;
+  // ui::SimpleMenuModel::Delegate overrides:
+  bool IsCommandIdChecked(int id) const override;
+  bool IsCommandIdEnabled(int id) const override;
+  void ExecuteCommand(int id, int event_flags) override;
 
  protected:
   BaseScrollBarThumb* GetThumb() const;
@@ -167,6 +167,7 @@ class VIEWS_EXPORT BaseScrollBar : public ScrollBar,
   // was invoked.
   int context_menu_mouse_position_;
 
+  std::unique_ptr<ui::SimpleMenuModel> menu_model_;
   std::unique_ptr<MenuRunner> menu_runner_;
   std::unique_ptr<ScrollAnimator> scroll_animator_;
 
