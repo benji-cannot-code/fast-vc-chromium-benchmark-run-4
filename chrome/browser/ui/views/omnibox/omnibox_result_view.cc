50f675f6661e1b6f9273980376b6b7be616fd913
Trent Apted <tapted@chromium.org>
2018-02-14 23:44:11 +0000

    Use multi-line omnibox results for touchable chrome.
    Bug: 801583, 725599
    Change-Id: I502172cfaa8292a80f1ce239491a089b77281a83
    Reviewed-on: https://chromium-review.googlesource.com/915641
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536889}

--- chrome/browser/ui/views/omnibox/omnibox_result_view.cc
+++ chrome/browser/ui/views/omnibox/omnibox_result_view.cc
@@ -172,6 +172,12 @@ TextStyle GetTextStyle(int type) {
   }
 }
 
+// Whether to use the two-line layout.
+bool IsTwoLineLayout() {
+  return base::FeatureList::IsEnabled(omnibox::kUIExperimentVerticalLayout) ||
+         ui::MaterialDesignController::IsTouchOptimizedUiEnabled();
+}
+
 }  // namespace
 
 // This class is a utility class for calculations affected by whether the result
@@ -402,7 +408,7 @@ gfx::Size OmniboxResultView::CalculatePreferredSize() const {
   int height = GetTextHeight() + (2 * GetVerticalMargin());
   if (match_.answer)
     height += GetAnswerHeight();
-  else if (base::FeatureList::IsEnabled(omnibox::kUIExperimentVerticalLayout))
+  else if (IsTwoLineLayout())
     height += GetTextHeight();
   return gfx::Size(0, height);
 }
@@ -439,8 +445,7 @@ void OmniboxResultView::PaintMatch(const AutocompleteMatch& match,
     description->SetDisplayRect(gfx::Rect(gfx::Size(INT_MAX, 0)));
   int contents_max_width, description_max_width;
   bool description_on_separate_line =
-      match.answer != nullptr ||
-      base::FeatureList::IsEnabled(omnibox::kUIExperimentVerticalLayout);
+      match.answer != nullptr || IsTwoLineLayout();
   OmniboxPopupModel::ComputeMatchMaxWidths(
       contents->GetContentWidth(), separator_width_,
       description ? description->GetContentWidth() : 0,
@@ -473,7 +478,7 @@ void OmniboxResultView::PaintMatch(const AutocompleteMatch& match,
   }
 
   // Regular results.
-  if (base::FeatureList::IsEnabled(omnibox::kUIExperimentVerticalLayout)) {
+  if (IsTwoLineLayout()) {
     // For no description, shift down halfway to draw contents in middle.
     if (description_max_width == 0)
       y += GetTextHeight() / 2;
@@ -589,10 +594,9 @@ void OmniboxResultView::InitContentsRenderTextIfNecessary() const {
       contents_rendertext_ =
           CreateAnswerText(match_.answer->first_line(), font_list_);
     } else {
-      bool swap_match_text =
-          base::FeatureList::IsEnabled(omnibox::kUIExperimentVerticalLayout) &&
-          !AutocompleteMatch::IsSearchType(match_.type) &&
-          !match_.description.empty();
+      bool swap_match_text = IsTwoLineLayout() &&
+                             !AutocompleteMatch::IsSearchType(match_.type) &&
+                             !match_.description.empty();
 
       contents_rendertext_ = CreateClassifiedRenderText(
           swap_match_text ? match_.description : match_.contents,
@@ -759,7 +763,7 @@ void OmniboxResultView::Layout() {
   const gfx::Image icon = GetIcon();
 
   int row_height = GetTextHeight();
-  if (base::FeatureList::IsEnabled(omnibox::kUIExperimentVerticalLayout))
+  if (IsTwoLineLayout())
     row_height += match_.answer ? GetAnswerHeight() : GetTextHeight();
 
   const int icon_y = GetVerticalMargin() + (row_height - icon.Height()) / 2;
@@ -810,9 +814,8 @@ void OmniboxResultView::OnPaint(gfx::Canvas* canvas) {
       } else if (!match_.description.empty()) {
         // If the description is empty, we wouldn't swap with the contents --
         // nor would we create the description RenderText object anyways.
-        bool swap_match_text = base::FeatureList::IsEnabled(
-                                   omnibox::kUIExperimentVerticalLayout) &&
-                               !AutocompleteMatch::IsSearchType(match_.type);
+        bool swap_match_text =
+            IsTwoLineLayout() && !AutocompleteMatch::IsSearchType(match_.type);
 
         description_rendertext_ = CreateClassifiedRenderText(
             swap_match_text ? match_.contents : match_.description,
