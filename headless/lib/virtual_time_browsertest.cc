5226eba4044b1207017f6577d7ce6b854e3a2df0
Alex Clarke <alexclarke@chromium.org>
2018-02-14 15:46:37 +0000

    Fix bug Emulation.setVirtualTimePolicy and waitForNavigation
    If only a single call to Emulation.setVirtualTimePolicy is made with
    waitForNavigation:true then addInspectorEmulationAgent wasn't getting
    called in time, which meant InspectorEmulationAgent::FrameStartedLoading
    wasn't called for the navigation, which meant the virtual time policy
    was never changing.
    
    Bug: 777763
    Change-Id: I7525ed1de924319c195ecfdd602c3fad808eba59
    Reviewed-on: https://chromium-review.googlesource.com/918665
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Alex Clarke <alexclarke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536721}

--- headless/lib/virtual_time_browsertest.cc
+++ headless/lib/virtual_time_browsertest.cc
@@ -47,32 +47,25 @@ class VirtualTimeBrowserTest : public HeadlessAsyncDevTooledBrowserTest,
     if (!runtime_enabled)
       return;
 
-    // To avoid race conditions start with virtual time paused.
-    devtools_client_->GetEmulation()->GetExperimental()->SetVirtualTimePolicy(
-        emulation::SetVirtualTimePolicyParams::Builder()
-            .SetPolicy(emulation::VirtualTimePolicy::PAUSE)
-            .Build(),
-        base::BindRepeating(&VirtualTimeBrowserTest::SetVirtualTimePolicyDone,
-                            base::Unretained(this)));
-
-    SetAfterLoadVirtualTimePolicy();
+    SetVirtualTimePolicy();
   }
 
   void SetVirtualTimePolicyDone(
       std::unique_ptr<emulation::SetVirtualTimePolicyResult> result) {
-    EXPECT_LT(0, result->GetVirtualTimeBase());
     // Virtual time is paused, so start navigating.
     devtools_client_->GetPage()->Navigate(initial_url_);
   }
 
-  virtual void SetAfterLoadVirtualTimePolicy() {
+  virtual void SetVirtualTimePolicy() {
     devtools_client_->GetEmulation()->GetExperimental()->SetVirtualTimePolicy(
         emulation::SetVirtualTimePolicyParams::Builder()
             .SetPolicy(
                 emulation::VirtualTimePolicy::PAUSE_IF_NETWORK_FETCHES_PENDING)
             .SetBudget(5000)
             .SetWaitForNavigation(true)
-            .Build());
+            .Build(),
+        base::BindRepeating(&VirtualTimeBrowserTest::SetVirtualTimePolicyDone,
+                            base::Unretained(this)));
   }
 
   // runtime::Observer implementation:
@@ -150,7 +143,7 @@ class MaxVirtualTimeTaskStarvationCountTest : public VirtualTimeBrowserTest {
                       .spec());
   }
 
-  void SetAfterLoadVirtualTimePolicy() override {
+  void SetVirtualTimePolicy() override {
     devtools_client_->GetEmulation()->GetExperimental()->SetVirtualTimePolicy(
         emulation::SetVirtualTimePolicyParams::Builder()
             .SetPolicy(
@@ -158,7 +151,9 @@ class MaxVirtualTimeTaskStarvationCountTest : public VirtualTimeBrowserTest {
             .SetBudget(4001)
             .SetMaxVirtualTimeTaskStarvationCount(100)
             .SetWaitForNavigation(true)
-            .Build());
+            .Build(),
+        base::BindRepeating(&VirtualTimeBrowserTest::SetVirtualTimePolicyDone,
+                            base::Unretained(this)));
   }
 
   // emulation::Observer implementation:
@@ -277,16 +272,6 @@ class VirtualTimeLocalStorageTest : public VirtualTimeBrowserTest {
                       .spec());
   }
 
-  void SetAfterLoadVirtualTimePolicy() override {
-    devtools_client_->GetEmulation()->GetExperimental()->SetVirtualTimePolicy(
-        emulation::SetVirtualTimePolicyParams::Builder()
-            .SetPolicy(
-                emulation::VirtualTimePolicy::PAUSE_IF_NETWORK_FETCHES_PENDING)
-            .SetBudget(5000)
-            .SetWaitForNavigation(true)
-            .Build());
-  }
-
   void OnConsoleAPICalled(
       const runtime::ConsoleAPICalledParams& params) override {
     EXPECT_EQ(runtime::ConsoleAPICalledType::LOG, params.GetType());
@@ -321,16 +306,6 @@ class VirtualTimeSessionStorageTest : public VirtualTimeBrowserTest {
     SetInitialURL(embedded_test_server()
                       ->GetURL("/virtual_time_session_storage.html")
                       .spec());
-  }
-
-  void SetAfterLoadVirtualTimePolicy() override {
-    devtools_client_->GetEmulation()->GetExperimental()->SetVirtualTimePolicy(
-        emulation::SetVirtualTimePolicyParams::Builder()
-            .SetPolicy(
-                emulation::VirtualTimePolicy::PAUSE_IF_NETWORK_FETCHES_PENDING)
-            .SetBudget(5000)
-            .SetWaitForNavigation(true)
-            .Build());
   }
 
   void OnConsoleAPICalled(
@@ -691,7 +666,7 @@ class VirtualTimeAndResourceErrorLoopTest : public VirtualTimeBrowserTest {
     VirtualTimeBrowserTest::RunDevTooledTest();
   }
 
-  void SetAfterLoadVirtualTimePolicy() override {
+  void SetVirtualTimePolicy() override {
     devtools_client_->GetEmulation()->GetExperimental()->SetVirtualTimePolicy(
         emulation::SetVirtualTimePolicyParams::Builder()
             .SetPolicy(
@@ -699,7 +674,9 @@ class VirtualTimeAndResourceErrorLoopTest : public VirtualTimeBrowserTest {
             .SetBudget(5000)
             .SetMaxVirtualTimeTaskStarvationCount(1000000)  // Prevent flakes.
             .SetWaitForNavigation(true)
-            .Build());
+            .Build(),
+        base::BindRepeating(&VirtualTimeBrowserTest::SetVirtualTimePolicyDone,
+                            base::Unretained(this)));
   }
 
   // emulation::Observer implementation:
@@ -776,16 +753,6 @@ class JavascriptTimeAlwaysAdvancesTest : public VirtualTimeBrowserTest,
   void CustomizeHeadlessBrowserContext(
       HeadlessBrowserContext::Builder& builder) override {
     builder.SetInitialVirtualTime(base::Time::FromJsTime(1000000.0));
-  }
-
-  void SetAfterLoadVirtualTimePolicy() override {
-    devtools_client_->GetEmulation()->GetExperimental()->SetVirtualTimePolicy(
-        emulation::SetVirtualTimePolicyParams::Builder()
-            .SetPolicy(
-                emulation::VirtualTimePolicy::PAUSE_IF_NETWORK_FETCHES_PENDING)
-            .SetBudget(5000)
-            .SetWaitForNavigation(true)
-            .Build());
   }
 
   void OnConsoleAPICalled(
