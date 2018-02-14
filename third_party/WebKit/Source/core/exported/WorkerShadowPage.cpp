d29bc2dfe72ef56c808783f40227fe5d353d9ab6
Luna Lu <loonybear@chromium.org>
2018-02-14 16:41:48 +0000

    Add a flag in Settings for UseCounter to avoid measuring shadow pages.
    Shadow page has a frame and upon service worker startup, UseCounter will
    count page visits.
    This CL adds a flag "is_shadow_page_" in Settings and sets the flag to
    be true in WorkerPageShadow initialization (by default the flag's value
    is false). In UseCounter it checks for shadow pages and drop counts for
    shadow pages.
    
    Note that this flag will eventually go away since browser side use
    counter does not have problems with workers. So once the blink side
    use counter is removed, this change can be undone.
    
    This change has also been verified to work locally.
    
    Bug: 694880
    Change-Id: I1cbe857d776591098bbbbf256ef8aa546c399af5
    Reviewed-on: https://chromium-review.googlesource.com/917028
    Commit-Queue: Luna Lu <loonybear@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536736}

--- third_party/WebKit/Source/core/exported/WorkerShadowPage.cpp
+++ third_party/WebKit/Source/core/exported/WorkerShadowPage.cpp
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/exported/WorkerShadowPage.h"
 
 #include "core/exported/WebViewImpl.h"
+#include "core/frame/Settings.h"
 #include "core/frame/csp/ContentSecurityPolicy.h"
 #include "core/loader/FrameLoadRequest.h"
 #include "platform/loader/fetch/SubstituteData.h"
@@ -31,6 +32,10 @@ WorkerShadowPage::WorkerShadowPage(Client* client)
   // TODO(http://crbug.com/363843): This needs to find a better way to
   // not create graphics layers.
   web_view_->GetSettings()->SetAcceleratedCompositingEnabled(false);
+  // TODO(lunalu): Service worker and shared worker count feature usage on the
+  // blink side use counter. Once the blink side use counter is removed
+  // (crbug.com/811948), remove this instant from Settings.
+  main_frame_->GetFrame()->GetSettings()->SetIsShadowPage(true);
 
   main_frame_->SetDevToolsAgentImpl(
       WebDevToolsAgentImpl::CreateForWorker(main_frame_, client_));
