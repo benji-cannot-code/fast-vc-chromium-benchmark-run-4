b0ef071c67151f42e57c929e5440ac9087430f7c
kylechar <kylechar@chromium.org>
2018-02-14 01:45:48 +0000

    viz: Fix VizDisplayCompositor + EGL crashing GPU process.
    Using --enable-features=VizDisplayCompositor and EGL hardware
    acceleration didn't work. The GPU process would lose the GL context and
    crash because of bugs in InProcessCommandBuffer.
    
    InProcessCommandBuffer wasn't using the same command decoder as
    GLES2CommandBufferStub. This was happening because GpuPreferences
    weren't plumbed into GpuInProcessThreadService.
    
    InProcessCommandBuffer was also always using virtualized GL contexts.
    Virtualized contexts are broken on windows apparently. Plumb this into
    GpuInProcessThreadService. The default constructed version will continue
    to use virtualized contexts, while the VizMainImpl constructed version
    will not used virtualized contexts.
    
    There are some other differences in how InProcessCommandBuffer and
    GLES2CommandBufferStub decide whether to use virtualized contexts that
    needs to be resolved separately.
    
    Bug: 810424, 811979
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I230aeeefb4560407da816c15c47e4f5b4ce11138
    Reviewed-on: https://chromium-review.googlesource.com/917014
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536596}

--- gpu/ipc/gpu_in_process_thread_service.cc
+++ gpu/ipc/gpu_in_process_thread_service.cc
@@ -11,15 +11,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace gpu {
 
 GpuInProcessThreadService::GpuInProcessThreadService(
+    bool use_virtualized_gl_context,
     scoped_refptr<base::SingleThreadTaskRunner> task_runner,
     gpu::SyncPointManager* sync_point_manager,
     gpu::MailboxManager* mailbox_manager,
     scoped_refptr<gl::GLShareGroup> share_group,
-    const GpuFeatureInfo& gpu_feature_info)
-    : gpu::InProcessCommandBuffer::Service(GpuPreferences(),
+    const GpuFeatureInfo& gpu_feature_info,
+    const GpuPreferences& gpu_preferences)
+    : gpu::InProcessCommandBuffer::Service(gpu_preferences,
                                            mailbox_manager,
                                            share_group,
                                            gpu_feature_info),
+      use_virtualized_gl_context_(use_virtualized_gl_context),
       task_runner_(task_runner),
       sync_point_manager_(sync_point_manager) {}
 
@@ -32,7 +35,7 @@ void GpuInProcessThreadService::ScheduleDelayedWork(const base::Closure& task) {
                                 base::TimeDelta::FromMilliseconds(2));
 }
 bool GpuInProcessThreadService::UseVirtualizedGLContexts() {
-  return true;
+  return use_virtualized_gl_context_;
 }
 
 gpu::SyncPointManager* GpuInProcessThreadService::sync_point_manager() {
