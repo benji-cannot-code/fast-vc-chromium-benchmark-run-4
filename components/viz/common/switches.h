b5ab66d639da6aea23e2f30c5fcf8995024131c2
Fady Samuel <fsamuel@chromium.org>
2018-02-14 21:25:54 +0000

    Surface synchronization: Add support to drop deadlines.
    Headless Chrome would like surface synchronization to always have unlimited
    deadlines. This CL makes that possible.
    
    --deadlines-to-synchronize-surfaces=unlimited (or any non-int) will override
    all client specified deadlines with unlimited deadlines.
    
    If unlimited deadlines are specified, viz::Surface does not get a
    viz::SurfaceDependencyDeadline. It will only activate once all dependenices
    are resolved.
    
    Bug: 672962
    TBR: sadrul@chromium.org, piman@chromium.org
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: Id2bbd8130fda8e10725e3643780c7ca5bd6ce4b3
    Reviewed-on: https://chromium-review.googlesource.com/917522
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Saman Sami <samans@chromium.org>
    Commit-Queue: Fady Samuel <fsamuel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536821}

--- components/viz/common/switches.h
+++ components/viz/common/switches.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 #include <string>
 
+#include "base/optional.h"
 #include "components/viz/common/viz_common_export.h"
 
 namespace switches {
@@ -19,7 +20,7 @@ VIZ_COMMON_EXPORT extern const char kEnableSurfaceSynchronization[];
 VIZ_COMMON_EXPORT extern const char kUseVizHitTestDrawQuad[];
 VIZ_COMMON_EXPORT extern const char kUseVizHitTestSurfaceLayer[];
 
-VIZ_COMMON_EXPORT uint32_t GetDeadlineToSynchronizeSurfaces();
+VIZ_COMMON_EXPORT base::Optional<uint32_t> GetDeadlineToSynchronizeSurfaces();
 
 }  // namespace switches
 
