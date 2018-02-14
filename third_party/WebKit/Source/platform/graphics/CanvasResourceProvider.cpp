ba91bda66a07e5d99ab15ed566f5053262c99a63
Khushal <khushalsagar@chromium.org>
2018-02-14 21:25:50 +0000

    canvas: Flush GrContext at regular intervals during draw.
    When drawing to an SkCanvas backed by a GrContext, skia internally
    batches the GrOps which may be flushed only at the end of the canvas
    draw. This results in inefficient parallelization with the GPU, since
    these ops are not transferred via the CommandBuffer until the GrContext
    is flushed, and it can quickly exceed the command buffer capacity which
    results in synchronously waiting on the GPU before more commands can
    be issued.
    
    This change adds a setting to SkiaPaintCanvas for accelerated Canvas2D
    that flushes the GrContext after a fixed number of draw calls have been
    made to the SkCanvas.
    
    R=piman@chromium.org
    
    Bug: 809441
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Id85fc2a91db07b51b72d903117b68d8c4db14693
    Reviewed-on: https://chromium-review.googlesource.com/907867
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536820}

--- third_party/WebKit/Source/platform/graphics/CanvasResourceProvider.cpp
+++ third_party/WebKit/Source/platform/graphics/CanvasResourceProvider.cpp
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/common/capabilities.h"
 #include "gpu/command_buffer/common/gpu_memory_buffer_support.h"
 #include "platform/graphics/AcceleratedStaticBitmapImage.h"
+#include "platform/graphics/CanvasHeuristicParameters.h"
 #include "platform/graphics/CanvasResource.h"
 #include "platform/graphics/StaticBitmapImage.h"
 #include "platform/graphics/gpu/SharedGpuContext.h"
@@ -376,13 +377,21 @@ PaintCanvas* CanvasResourceProvider::Canvas() {
       image_provider = &*canvas_image_provider_;
     }
 
+    cc::SkiaPaintCanvas::ContextFlushes context_flushes;
+    if (IsAccelerated()) {
+      context_flushes.enable =
+          CanvasHeuristicParameters::kEnableGrContextFlushes;
+      context_flushes.max_draws_before_flush =
+          CanvasHeuristicParameters::kMaxDrawsBeforeContextFlush;
+    }
     if (ColorParams().NeedsSkColorSpaceXformCanvas()) {
       canvas_ = std::make_unique<cc::SkiaPaintCanvas>(
           GetSkSurface()->getCanvas(), ColorParams().GetSkColorSpace(),
-          std::move(image_provider));
+          std::move(image_provider), context_flushes);
     } else {
       canvas_ = std::make_unique<cc::SkiaPaintCanvas>(
-          GetSkSurface()->getCanvas(), std::move(image_provider));
+          GetSkSurface()->getCanvas(), std::move(image_provider),
+          context_flushes);
     }
   }
 
