382a8ab24672fdb55c5373c7fb832bb64f358f2f
Peter Kasting <pkasting@chromium.org>
2018-02-14 03:14:27 +0000

    Make interactive tests have unlimited timeout on Windows.
    This upgrades --interactive to a test-launcher-scope switch (necessarily
    renaming it to --test-launcher-interactive) and treats it as a timeout
    specifier.  For browser UI tests on Windows, this means the parent process will
    now get an (effectively) unlimited timeout and not just the child process, so
    the test won't terminate early.
    
    BUG=none
    TEST=none
    
    Change-Id: I56b02ba30ad0df6617b0f1f95d9986724c5035a0
    Reviewed-on: https://chromium-review.googlesource.com/909920
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536634}

--- base/test/test_timeouts.h
+++ base/test/test_timeouts.h
@@ -14,9 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // the timeouts for different environments (like TSan).
 class TestTimeouts {
  public:
-  // Argument that can be passed on the command line to indicate "no timeout".
-  static constexpr const char kNoTimeoutSwitchValue[] = "-1";
-
   // Initializes the timeouts. Non thread-safe. Should be called exactly once
   // by the test suite.
   static void Initialize();
