08a62423ea9a992c3403323abaf8052b894211b1
Rohit Rao <rohitrao@chromium.org>
2018-02-15 17:26:23 +0000

    [ios] Uses ChromeTableViewController for the Settings cell catalog.
    BUG=811075
    
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I6e05590248f2d54e395463c10a660582cf72731a
    Reviewed-on: https://chromium-review.googlesource.com/921763
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537058}

--- ios/chrome/browser/ui/settings/table_cell_catalog_view_controller.mm
+++ ios/chrome/browser/ui/settings/table_cell_catalog_view_controller.mm
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/settings/table_cell_catalog_view_controller.h"
 
+#import "ios/chrome/browser/ui/table_view/table_view_model.h"
+
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
@@ -20,6 +22,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)viewDidLoad {
   [super viewDidLoad];
   self.title = @"Table Cell Catalog";
+
+  [self loadModel];
+}
+
+- (void)loadModel {
+  [super loadModel];
 }
 
 @end
