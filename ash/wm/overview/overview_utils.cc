0a6427cdc62cd14b39a477f1b87314ef18595b2a
wutao <wutao@chromium.org>
2018-02-15 07:09:38 +0000

    cros: Overview animations toning down.
    Overview mode is one of the complex areas for improving animation speed.
    This cl implements a new proposal to tone down the animations.
    
    Changes:
    1. Enter animation: only animate currently visible windows and put the
    rest of the windows in place.
    2. Exit animation: animate all of the Most Recently Used (MRU) windows
    up to and including the MRU maximized/fullscreen window, with the other
    windows behind not moving until the maximized/fullscreen window finishes
    animation.
    
    Bug: 801465, 795988
    Test: Tested new animations in different modes: normal, tablet, and splitview.
    
    Change-Id: Ia7046c670bb484447752b8e5545fa81e981256d2
    Reviewed-on: https://chromium-review.googlesource.com/882008
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Reviewed-by: Mitsuru Oshima (In Tokyo) <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536957}

--- ash/wm/overview/overview_utils.cc
+++ ash/wm/overview/overview_utils.cc
@@ -6,8 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/overview/overview_utils.h"
 
 #include "ash/public/cpp/ash_switches.h"
+#include "ash/shell.h"
+#include "ash/wm/splitview/split_view_controller.h"
+#include "ash/wm/window_state.h"
 #include "base/command_line.h"
 #include "base/optional.h"
+#include "ui/aura/client/aura_constants.h"
+#include "ui/aura/window.h"
 
 namespace ash {
 
@@ -15,9 +20,28 @@ namespace {
 
 // Cache the result of the command line lookup so the command line is only
 // looked up once.
+base::Optional<bool> g_enable_new_overview_animations = base::nullopt;
 base::Optional<bool> g_enable_new_overview_ui = base::nullopt;
 
 }  // namespace
+
+bool CanCoverAvailableWorkspace(aura::Window* window) {
+  SplitViewController* split_view_controller =
+      Shell::Get()->split_view_controller();
+  if (split_view_controller->IsSplitViewModeActive())
+    return split_view_controller->CanSnap(window);
+  return wm::GetWindowState(window)->IsMaximizedOrFullscreenOrPinned();
+}
+
+bool IsNewOverviewAnimationsEnabled() {
+  if (g_enable_new_overview_animations == base::nullopt) {
+    g_enable_new_overview_animations =
+        base::make_optional(base::CommandLine::ForCurrentProcess()->HasSwitch(
+            ash::switches::kAshEnableNewOverviewAnimations));
+  }
+
+  return g_enable_new_overview_animations.value();
+}
 
 bool IsNewOverviewUi() {
   if (g_enable_new_overview_ui == base::nullopt) {
@@ -27,6 +51,10 @@ bool IsNewOverviewUi() {
   }
 
   return g_enable_new_overview_ui.value();
+}
+
+void ResetCachedOverviewAnimationsValueForTesting() {
+  g_enable_new_overview_animations = base::nullopt;
 }
 
 void ResetCachedOverviewUiValueForTesting() {
