ad90a8a171aa792b0240fb2028d3aad869b02301
Patti <patricialor@chromium.org>
2018-02-15 07:25:48 +0000

    ImageView: Fix CENTER alignment on ImageViews with borders.
    When positioning its internal ImageSkia for painting, CENTER-aligned ImageViews
    currently assume that any insets it may have are symmetrical (i.e. the
    left and right or top and bottom insets are the same value). This causes the
    image to be drawn in the incorrect position (e.g. it may cross the boundaries
    set by the ImageView insets) when the insets are not symmetrical. Fix by
    accounting for the inset width and height during positioning.
    
    Bug: 801583
    Change-Id: I65766fbea4c088ca45781a6a69cbae7b52178625
    Reviewed-on: https://chromium-review.googlesource.com/917843
    Commit-Queue: Patti <patricialor@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536962}

--- ui/views/controls/image_view.h
+++ ui/views/controls/image_view.h
@@ -83,6 +83,8 @@ class VIEWS_EXPORT ImageView : public View {
   views::PaintInfo::ScaleType GetPaintScaleType() const override;
 
  private:
+  friend class ImageViewTest;
+
   void OnPaintImage(gfx::Canvas* canvas);
 
   // Returns true if |img| is the same as the last image we painted. This is
