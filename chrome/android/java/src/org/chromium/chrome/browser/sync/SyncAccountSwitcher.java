51ab7b3085310f021c8b706460ef5afe80a508d1
Boris Sazonov <bsazonov@chromium.org>
2018-02-15 13:11:08 +0000

    [Signin][Android] Add Signin.SwitchSyncAccount.Source histogram
    This CL adds Signin.SwitchSyncAccount.Source histogram that tracks the
    usage of different flows that switch sync accounts. There are currently
    two such flows: manual signout-signin and SyncAccountSwitcher.
    
    Bug: 704709
    Change-Id: I0afe79d5110d55b3ca0287c18b7d485c93473ee8
    Reviewed-on: https://chromium-review.googlesource.com/914902
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536999}

--- chrome/android/java/src/org/chromium/chrome/browser/sync/SyncAccountSwitcher.java
+++ chrome/android/java/src/org/chromium/chrome/browser/sync/SyncAccountSwitcher.java
@@ -13,6 +13,7 @@ import android.text.TextUtils;
 import org.chromium.base.Callback;
 import org.chromium.base.Promise;
 import org.chromium.chrome.browser.preferences.SyncedAccountPreference;
+import org.chromium.chrome.browser.signin.AccountSigninActivity;
 import org.chromium.chrome.browser.signin.ConfirmImportSyncDataDialog;
 import org.chromium.chrome.browser.signin.ConfirmImportSyncDataDialog.ImportSyncType;
 import org.chromium.chrome.browser.signin.ConfirmSyncDataStateMachine;
@@ -87,6 +88,9 @@ public class SyncAccountSwitcher
                                 mNewAccountName, mActivity, SyncAccountSwitcher.this);
                     }
                 });
+
+        AccountSigninActivity.recordSwitchAccountSourceHistogram(
+                AccountSigninActivity.SwitchAccountSource.SYNC_ACCOUNT_SWITCHER);
     }
 
     @Override
