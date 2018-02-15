ce54dc39a93e71b9d7da3691b826c8ab11148566
Dominic Mazzoni <dmazzoni@chromium.org>
2018-02-15 00:15:06 +0000

    Add word boundaries to the PDF accessibility tree.
    Bug: 812304
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: Ib729933d425af0630201e11092e406089b7084ff
    Reviewed-on: https://chromium-review.googlesource.com/919543
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: dsinclair <dsinclair@chromium.org>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536903}

--- components/pdf/renderer/pdf_accessibility_tree.cc
+++ components/pdf/renderer/pdf_accessibility_tree.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 
 #include "base/debug/crash_logging.h"
+#include "base/i18n/break_iterator.h"
 #include "base/memory/ptr_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversion_utils.h"
@@ -163,6 +164,7 @@ void PdfAccessibilityTree::SetAccessibilityPageInfo(
     inline_text_box_node->location = text_run_bounds;
     inline_text_box_node->AddIntListAttribute(
         ax::mojom::IntListAttribute::kCharacterOffsets, char_offsets);
+    AddWordStartsAndEnds(inline_text_box_node);
 
     para_node->location.Union(inline_text_box_node->location);
     static_text_node->location.Union(inline_text_box_node->location);
@@ -337,6 +339,28 @@ gfx::Transform* PdfAccessibilityTree::MakeTransformFromViewInfo() {
   transform->Translate(offset_);
   transform->Translate(-scroll_);
   return transform;
+}
+
+void PdfAccessibilityTree::AddWordStartsAndEnds(
+    ui::AXNodeData* inline_text_box) {
+  base::string16 text =
+      inline_text_box->GetString16Attribute(ax::mojom::StringAttribute::kName);
+  base::i18n::BreakIterator iter(text, base::i18n::BreakIterator::BREAK_WORD);
+  if (!iter.Init())
+    return;
+
+  std::vector<int32_t> word_starts;
+  std::vector<int32_t> word_ends;
+  while (iter.Advance()) {
+    if (iter.IsWord()) {
+      word_starts.push_back(iter.prev());
+      word_ends.push_back(iter.pos());
+    }
+  }
+  inline_text_box->AddIntListAttribute(ax::mojom::IntListAttribute::kWordStarts,
+                                       word_starts);
+  inline_text_box->AddIntListAttribute(ax::mojom::IntListAttribute::kWordEnds,
+                                       word_ends);
 }
 
 //
--- components/pdf/renderer/pdf_accessibility_tree.h
+++ components/pdf/renderer/pdf_accessibility_tree.h
@@ -81,6 +81,7 @@ class PdfAccessibilityTree
   float GetDeviceScaleFactor() const;
   content::RenderAccessibility* GetRenderAccessibility();
   gfx::Transform* MakeTransformFromViewInfo();
+  void AddWordStartsAndEnds(ui::AXNodeData* inline_text_box);
 
   ui::AXTree tree_;
   content::RendererPpapiHost* host_;
