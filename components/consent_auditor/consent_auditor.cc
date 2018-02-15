5807849cedb90d1949e17d1c39a1313071a74962
Martin Sramek <msramek@chromium.org>
2018-02-15 19:41:05 +0000

    Measure the number of times a consent is recorded for a feature.
    Currently, consent auditor only supports one feature (Chrome Sync), but
    we use the enum-type histogram for extensibility in the future.
    
    As this histogram didn't fit into existing categories, a new top-level
    category "Privacy" is introduced.
    
    TBR=markusheintz@chromium.org
    
    Bug: 781765
    Change-Id: I6e4d93e1a45ce1f06e4d18b0cb6e5fa5b4673273
    Reviewed-on: https://chromium-review.googlesource.com/911572
    Commit-Queue: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537103}

--- components/consent_auditor/consent_auditor.cc
+++ components/consent_auditor/consent_auditor.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/metrics/histogram_macros.h"
 #include "base/values.h"
 #include "components/consent_auditor/pref_names.h"
 #include "components/prefs/pref_registry_simple.h"
@@ -32,6 +33,12 @@ UserEventSpecifics::UserConsent::Feature FeatureToProtoEnum(
   switch (feature) {
     case consent_auditor::Feature::CHROME_SYNC:
       return UserEventSpecifics::UserConsent::CHROME_SYNC;
+    case consent_auditor::Feature::FEATURE_COUNT:
+      // TODO(crbug.com/645032): There is currently just one supported feature,
+      // but histograms with only one bucket are not supported. Remove this hack
+      // when more features are added, or when crbug.com/645032 is fixed.
+      NOTREACHED();
+      return UserEventSpecifics::UserConsent::FEATURE_UNSPECIFIED;
   }
   NOTREACHED();
   return UserEventSpecifics::UserConsent::FEATURE_UNSPECIFIED;
@@ -81,6 +88,22 @@ void ConsentAuditor::RecordGaiaConsent(
     ConsentStatus status) {
   if (!base::FeatureList::IsEnabled(switches::kSyncUserConsentEvents))
     return;
+
+  DCHECK_NE(consent_auditor::Feature::FEATURE_COUNT, feature);
+
+  switch (status) {
+    case ConsentStatus::GIVEN:
+      UMA_HISTOGRAM_ENUMERATION(
+          "Privacy.ConsentAuditor.ConsentGiven.Feature", feature,
+          static_cast<int>(consent_auditor::Feature::FEATURE_COUNT));
+      break;
+    case ConsentStatus::NOT_GIVEN:
+      UMA_HISTOGRAM_ENUMERATION(
+          "Privacy.ConsentAuditor.ConsentNotGiven.Feature", feature,
+          static_cast<int>(consent_auditor::Feature::FEATURE_COUNT));
+      break;
+  }
+
   std::unique_ptr<sync_pb::UserEventSpecifics> specifics = ConstructUserConsent(
       feature, description_grd_ids, confirmation_grd_id, status);
   // For real usage, UserEventSyncBridge should always be ready to receive
