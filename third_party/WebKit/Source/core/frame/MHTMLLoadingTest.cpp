31ab023f691b5cc207e1f64b94ca9f598ba56806
Mostyn Bramley-Moore <mostynb@vewd.com>
2018-02-15 08:44:32 +0000

    [jumbo] move duplicated test code to UnitTestHelpers.cpp
    Fixes jumbo builds after https://chromium-review.googlesource.com/c/chromium/src/+/919199
    
    Change-Id: I350604ae19b68859b622568b0d1ac78b223900a5
    Reviewed-on: https://chromium-review.googlesource.com/920982
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Mostyn Bramley-Moore <mostynb@vewd.com>
    Cr-Commit-Position: refs/heads/master@{#536971}

--- third_party/WebKit/Source/core/frame/MHTMLLoadingTest.cpp
+++ third_party/WebKit/Source/core/frame/MHTMLLoadingTest.cpp
@@ -27,31 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using blink::URLTestHelpers::ToKURL;
 
 namespace blink {
-
-class LineReader {
- public:
-  LineReader(const std::string& text) : text_(text), index_(0) {}
-  bool GetNextLine(std::string* line) {
-    line->clear();
-    if (index_ >= text_.length())
-      return false;
-
-    size_t end_of_line_index = text_.find("\r\n", index_);
-    if (end_of_line_index == std::string::npos) {
-      *line = text_.substr(index_);
-      index_ = text_.length();
-      return true;
-    }
-
-    *line = text_.substr(index_, end_of_line_index - index_);
-    index_ = end_of_line_index + 2;
-    return true;
-  }
-
- private:
-  std::string text_;
-  size_t index_;
-};
+namespace testing {
 
 class MHTMLLoadingTest : public ::testing::Test {
  public:
@@ -250,4 +226,5 @@ TEST_F(MHTMLLoadingTest, LoadMHTMLContainingSoftLineBreaks) {
       "AVeryLongID012345678901234567890123456789012345678901234567890End"));
 }
 
+}  // namespace testing
 }  // namespace blink
