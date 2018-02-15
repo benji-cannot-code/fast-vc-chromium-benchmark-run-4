0caa0af741902f1868b31f81c61c34819daaa01e
sczs <sczs@chromium.org>
2018-02-15 04:07:27 +0000

    [ios] Creates ChromeTableViewController
    -Creates ChromeTableViewController with basic unittests.
    -Implements basic UITableViewDataSource methods.
    
    Bug: 805136
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Idbc59f0c0b71c01b9c8513e8bd7907b0f9713971
    Reviewed-on: https://chromium-review.googlesource.com/915261
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536942}

---
+++ ios/chrome/browser/ui/table_view/chrome_table_view_controller.h
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_TABLE_VIEW_CHROME_TABLE_VIEW_CONTROLLER_H_
+#define IOS_CHROME_BROWSER_UI_TABLE_VIEW_CHROME_TABLE_VIEW_CONTROLLER_H_
+
+#import <UIKit/UIKit.h>
+
+#import "ios/chrome/browser/ui/table_view/table_view_model.h"
+
+@class TableViewItem;
+
+// Chrome-specific TableViewController.
+@interface ChromeTableViewController : UITableViewController
+
+// The model of this controller.
+@property(nonatomic, readonly, strong)
+    TableViewModel<TableViewItem*>* tableViewModel;
+
+// Initializes the collection view model. Must be called by subclasses if they
+// override this method in order to get a clean tableViewModel.
+- (void)loadModel NS_REQUIRES_SUPER;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_TABLE_VIEW_CHROME_TABLE_VIEW_CONTROLLER_H_
