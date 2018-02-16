513bb9ecc572eba8ba3224f3f28d3986dde90592
Rune Lillesveen <futhark@chromium.org>
2018-02-16 17:19:04 +0000

    [Squad] Use const option ComputedStyle in menu list.
    Bug: 813068
    Change-Id: Ie5e3b8ccafd801575a5f886f1902abc547d9bacc
    Reviewed-on: https://chromium-review.googlesource.com/923983
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537335}

--- third_party/WebKit/Source/core/layout/LayoutMenuList.cpp
+++ third_party/WebKit/Source/core/layout/LayoutMenuList.cpp
@@ -210,7 +210,7 @@ void LayoutMenuList::UpdateFromElement() {
 
     if (selected_count == 1) {
       text = selected_option_element->TextIndentedToRespectGroupLabel();
-      option_style_ = selected_option_element->MutableComputedStyle();
+      option_style_ = selected_option_element->GetComputedStyle();
     } else {
       Locale& locale = select->GetLocale();
       String localized_number_string =
@@ -222,7 +222,7 @@ void LayoutMenuList::UpdateFromElement() {
   } else {
     if (option) {
       text = option->TextIndentedToRespectGroupLabel();
-      option_style_ = option->MutableComputedStyle();
+      option_style_ = option->GetComputedStyle();
     }
   }
 
