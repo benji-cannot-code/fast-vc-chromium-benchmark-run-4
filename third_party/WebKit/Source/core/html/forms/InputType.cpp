b84d24ba216445166bd49f471f26493f1a4fd08f
Takayoshi Kochi <kochi@chromium.org>
2018-02-16 07:59:57 +0000

    Remove unnecessary include in InputType.cpp
    HTMLShadowElement.h is unnecessary.
    
    Change-Id: I5153baa3c3022d67b4f2754a6804e2f45c226495
    Reviewed-on: https://chromium-review.googlesource.com/923682
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Takayoshi Kochi <kochi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537251}

--- third_party/WebKit/Source/core/html/forms/InputType.cpp
+++ third_party/WebKit/Source/core/html/forms/InputType.cpp
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/events/ScopedEventQueue.h"
 #include "core/events/KeyboardEvent.h"
 #include "core/fileapi/FileList.h"
-#include "core/html/HTMLShadowElement.h"
 #include "core/html/forms/ButtonInputType.h"
 #include "core/html/forms/CheckboxInputType.h"
 #include "core/html/forms/ColorChooser.h"
