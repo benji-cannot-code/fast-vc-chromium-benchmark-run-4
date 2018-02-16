a64de690af937324a4cc62e12c571586f3b77cde
Francois Doray <fdoray@chromium.org>
2018-02-16 14:49:45 +0000

    Do not get TaskScheduler field trial params from command line in renderers.
    base::GetFieldTrialParams() used not to work in child processes. For
    that reason, we built a system to pass scheduler field trial params
    to renderers on the command line. Now that base::GetFieldTrialParams()
    works fine in child processes
    (https://cs.chromium.org/chromium/src/content/app/content_main_runner.cc?l=160&rcl=9030625190ec0a2b60be2af279bcb234f47d3f51),
    we don't need to use that system anymore.
    
    TBR=rohitrao@chromium.org
    
    Bug: 810049
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Ic40653eece1a103a9c913cb286925061fed1121c
    Reviewed-on: https://chromium-review.googlesource.com/909169
    Commit-Queue: François Doray <fdoray@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537305}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -1903,11 +1903,6 @@ void ChromeContentBrowserClient::AppendExtraCommandLineSwitches(
   StackSamplingConfiguration::Get()->AppendCommandLineSwitchForChildProcess(
       process_type,
       command_line);
-
-  if (process_type == switches::kRendererProcess) {
-    task_scheduler_util::AddVariationParamsToCommandLine("Renderer",
-                                                         command_line);
-  }
 }
 
 void ChromeContentBrowserClient::AdjustUtilityServiceProcessCommandLine(
@@ -3694,9 +3689,7 @@ void ChromeContentBrowserClient::CreateMediaRemoter(
 
 std::unique_ptr<base::TaskScheduler::InitParams>
 ChromeContentBrowserClient::GetTaskSchedulerInitParams() {
-  // TaskScheduler variation params for the browser process have no prefix.
-  constexpr char kBrowserPrefix[] = "";
-  return task_scheduler_util::GetTaskSchedulerInitParams(kBrowserPrefix);
+  return task_scheduler_util::GetTaskSchedulerInitParamsForBrowser();
 }
 
 base::FilePath ChromeContentBrowserClient::GetLoggingFileName(
--- chrome/renderer/chrome_content_renderer_client.cc
+++ chrome/renderer/chrome_content_renderer_client.cc
@@ -90,7 +90,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/startup_metric_utils/common/startup_metric.mojom.h"
 #include "components/subresource_filter/content/renderer/subresource_filter_agent.h"
 #include "components/subresource_filter/content/renderer/unverified_ruleset_dealer.h"
-#include "components/task_scheduler_util/renderer/initialization.h"
+#include "components/task_scheduler_util/common/variations_util.h"
 #include "components/variations/variations_switches.h"
 #include "components/version_info/version_info.h"
 #include "components/visitedlink/renderer/visitedlink_slave.h"
@@ -1714,8 +1714,7 @@ GURL ChromeContentRendererClient::OverrideFlashEmbedWithHTML(const GURL& url) {
 
 std::unique_ptr<base::TaskScheduler::InitParams>
 ChromeContentRendererClient::GetTaskSchedulerInitParams() {
-  return task_scheduler_util::
-      GetRendererTaskSchedulerInitParamsFromCommandLine();
+  return task_scheduler_util::GetTaskSchedulerInitParamsForRenderer();
 }
 
 void ChromeContentRendererClient::InitSafeBrowsingIfNecessary() {
