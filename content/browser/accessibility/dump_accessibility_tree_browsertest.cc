a82796a44e0c42e9eebe233044cd35cb231e3bcd
Dominic Mazzoni <dmazzoni@chromium.org>
2018-02-16 23:55:39 +0000

    Android accessibility: Only expose character locations on some elements
    On Android, we can expose EXTRA_DATA_TEXT_CHARACTER_LOCATION_KEY
    on accessible nodes in order to allow services like Select To Speak
    to get the bounding box of individual characters or character
    ranges.
    
    When we implemented support for this, we exposed it on almost any
    element, but this leads to Select To Speak not being able to
    distinguish between real text, and things like images with no
    visible text that it might be able to OCR.
    
    Fix this by exposing this character location key only on nodes
    that are static text or have a static text descendant.
    
    Add a test that covers some common cases.
    
    BUG=812772
    
    Change-Id: I48941edfaa7522fdd36a5bd7fc88a0a4054713f3
    Reviewed-on: https://chromium-review.googlesource.com/922688
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537465}

--- content/browser/accessibility/dump_accessibility_tree_browsertest.cc
+++ content/browser/accessibility/dump_accessibility_tree_browsertest.cc
@@ -247,6 +247,7 @@ void DumpAccessibilityTreeTest::AddDefaultFilters(
 
   AddFilter(filters, "hint=*");
   AddFilter(filters, "interesting", Filter::DENY);
+  AddFilter(filters, "has_character_locations", Filter::DENY);
 
   //
   // General
@@ -999,6 +1000,11 @@ IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityCanvas) {
 
 IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityCaption) {
   RunHtmlTest(FILE_PATH_LITERAL("caption.html"));
+}
+
+IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest,
+                       AccessibilityCharacterLocations) {
+  RunHtmlTest(FILE_PATH_LITERAL("character-locations.html"));
 }
 
 IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest,
