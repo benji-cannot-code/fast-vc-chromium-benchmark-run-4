677a0342e5054189eb9a7eb548717f0b727e0ead
Jared Saul <jsaul@google.com>
2018-02-16 19:07:47 +0000

    Clean up kAutofillCreditCardBankNameDisplay flag
    Bug: 812710
    Change-Id: I4344c258668c5017a0a145e487ca2eb526bd49bc
    Reviewed-on: https://chromium-review.googlesource.com/923281
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Jared Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/master@{#537368}

--- components/autofill/core/browser/autofill_experiments.cc
+++ components/autofill/core/browser/autofill_experiments.cc
@@ -33,8 +33,6 @@ const base::Feature kAutofillCreditCardAssist{
     "AutofillCreditCardAssist", base::FEATURE_DISABLED_BY_DEFAULT};
 const base::Feature kAutofillScanCardholderName{
     "AutofillScanCardholderName", base::FEATURE_DISABLED_BY_DEFAULT};
-const base::Feature kAutofillCreditCardBankNameDisplay{
-    "AutofillCreditCardBankNameDisplay", base::FEATURE_DISABLED_BY_DEFAULT};
 const base::Feature kAutofillCreditCardAblationExperiment{
     "AutofillCreditCardAblationExperiment", base::FEATURE_DISABLED_BY_DEFAULT};
 const base::Feature kAutofillCreditCardPopupLayout{
@@ -122,10 +120,6 @@ bool IsAutofillCreditCardPopupLayoutExperimentEnabled() {
 
 bool IsAutofillCreditCardLastUsedDateDisplayExperimentEnabled() {
   return base::FeatureList::IsEnabled(kAutofillCreditCardLastUsedDateDisplay);
-}
-
-bool IsAutofillCreditCardBankNameDisplayExperimentEnabled() {
-  return base::FeatureList::IsEnabled(kAutofillCreditCardBankNameDisplay);
 }
 
 // |GetCreditCardPopupParameterUintValue| returns 0 if experiment parameter is
