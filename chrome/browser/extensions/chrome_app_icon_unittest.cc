0d06dc31a99c1e531351beed6f489a7d46c6b3d9
Istiaque Ahmed <lazyboy@chromium.org>
2018-02-16 18:25:03 +0000

    Extensions: Cleanup usages of ResetAndReturn on OnceCallbacks.
    These do not need base::ResetAndReturn, use std::move(callack).Run()
    instead.
    
    Change-Id: Ifa90ffffcae8e64fd8cced276e482b3ecf21d6a9
    Bug: 812717
    Reviewed-on: https://chromium-review.googlesource.com/920645
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537353}

--- chrome/browser/extensions/chrome_app_icon_unittest.cc
+++ chrome/browser/extensions/chrome_app_icon_unittest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include <memory>
+#include <utility>
 #include <vector>
 
 #include "base/macros.h"
@@ -75,8 +76,8 @@ class TestAppIcon : public ChromeAppIconDelegate {
   void OnIconUpdated(ChromeAppIcon* icon) override {
     ++icon_update_count_;
     if (icon_update_count_ == icon_update_count_expected_ &&
-        !icon_updated_callback_.is_null()) {
-      base::ResetAndReturn(&icon_updated_callback_).Run();
+        icon_updated_callback_) {
+      std::move(icon_updated_callback_).Run();
     }
   }
 
