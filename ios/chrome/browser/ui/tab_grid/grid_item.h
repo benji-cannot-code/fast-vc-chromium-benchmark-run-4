e0013c4e80b527ef3d5203dc1a0f9c2e6d6ff2b9
edchin <edchin@chromium.org>
2018-02-16 02:46:19 +0000

    [ios] First pass at GridCell
    This is a first pass at GridCell. Icon, title, close button, and
    the snapshot were added. Styling is already pretty close but will
    require more tweaking. A showcase entry displays various uses of this
    cell including different sizes and dark/light themes.
    
    This approach attempts to solve for dark variants without the need to
    subclass.
    
    Bug: 804526
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I5bbeb5d8adb5ee93e6941d77bcac2b481569da74
    Reviewed-on: https://chromium-review.googlesource.com/920627
    Commit-Queue: edchin <edchin@chromium.org>
    Reviewed-by: edchin <edchin@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537138}

---
+++ ios/chrome/browser/ui/tab_grid/grid_item.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_TAB_GRID_GRID_ITEM_H_
+#define IOS_CHROME_BROWSER_UI_TAB_GRID_GRID_ITEM_H_
+
+#import <Foundation/Foundation.h>
+
+// Model object representing an item in a grid.
+@interface GridItem : NSObject
+@property(nonatomic, copy) NSString* title;
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_TAB_GRID_GRID_ITEM_H_
--- ios/showcase/core/showcase_model.mm
+++ ios/showcase/core/showcase_model.mm
@@ -72,6 +72,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       showcase::kClassForInstantiationKey : @"SCTabGridCoordinator",
       showcase::kUseCaseKey : @"Tab grid",
     },
+    @{
+      showcase::kClassForDisplayKey : @"GridCell",
+      showcase::kClassForInstantiationKey : @"SCGridCellViewController",
+      showcase::kUseCaseKey : @"Grid cells",
+    },
   ];
 }
 
