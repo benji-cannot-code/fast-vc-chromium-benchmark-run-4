59d45cb76a6e9189d1c33ddaa7fdb9bcca57796c
edchin <edchin@chromium.org>
2018-02-16 23:08:21 +0000

    [ios] Tab grid toolbars initial implementations
    Bug: 804499
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I066fc6b53f8f2775f559f8dd75ab63d629b74e9d
    Reviewed-on: https://chromium-review.googlesource.com/924381
    Commit-Queue: edchin <edchin@chromium.org>
    Reviewed-by: edchin <edchin@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537447}

--- ios/showcase/core/showcase_model.mm
+++ ios/showcase/core/showcase_model.mm
@@ -77,6 +77,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       showcase::kClassForInstantiationKey : @"SCGridCellViewController",
       showcase::kUseCaseKey : @"Grid cells",
     },
+    @{
+      showcase::kClassForDisplayKey : @"TabGridViewController",
+      showcase::kClassForInstantiationKey : @"SCTabGridCoordinator",
+      showcase::kUseCaseKey : @"Full tab grid UI",
+    },
   ];
 }
 
