f3ae125dc126d030093f18bcaf3715fdb5160fd3
Takeshi Yoshino <tyoshino@chromium.org>
2018-02-16 04:37:34 +0000

    Disable a flaky test SecurityStateTabHelperTest.PasswordSecurityLevelDowngraded on Mac bots
    Bug: 812960
    Change-Id: I1bb1f5db9bdc5ceb7ca73ee52ac7edc0ad5545f9
    Tbr: rsleevi@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/923502
    Commit-Queue: Takeshi Yoshino <tyoshino@chromium.org>
    Reviewed-by: Takeshi Yoshino <tyoshino@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537202}

--- chrome/browser/ssl/security_state_tab_helper_browsertest.cc
+++ chrome/browser/ssl/security_state_tab_helper_browsertest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/histogram_tester.h"
 #include "base/test/scoped_command_line.h"
+#include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/profiles/profile_window.h"
@@ -1189,8 +1190,16 @@ IN_PROC_BROWSER_TEST_F(SecurityStateLoadingTest, NavigationStateChanges) {
 
 // Tests that when a visible password field is detected on an HTTP page
 // load, the security level is downgraded to HTTP_SHOW_WARNING.
+//
+// Disabled on Mac bots. See crbug.com/812960.
+#if defined(OS_MACOSX)
+#define MAYBE_PasswordSecurityLevelDowngraded \
+  DISABLED_PasswordSecurityLevelDowngraded
+#else
+#define MAYBE_PasswordSecurityLevelDowngraded PasswordSecurityLevelDowngraded
+#endif
 IN_PROC_BROWSER_TEST_F(SecurityStateTabHelperTest,
-                       PasswordSecurityLevelDowngraded) {
+                       MAYBE_PasswordSecurityLevelDowngraded) {
   content::WebContents* contents =
       browser()->tab_strip_model()->GetActiveWebContents();
   ASSERT_TRUE(contents);
