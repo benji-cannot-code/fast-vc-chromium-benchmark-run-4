5f578d013ced52f586c54ade0655c5e5adf8d4ee
Tom Anderson <thomasanderson@chromium.org>
2018-02-16 23:37:19 +0000

    Use IPA Mincho font on Linux instead of Kochi Mincho
    Kochi Mincho is old and is not available on newer linux distros.  IPA Mincho is
    a suitable replacement.  However, the IPA font metrics are slightly different
    than Kochi.  There were some test expectations that certain font metrics (like
    baseline) be different from Arial, but they're conincidentally not for IPA.  I
    also swapped out Arial for Verdana so that the metrics would still be different.
    
    BUG=812911
    R=derat
    TBR=asvitkine
    
    Change-Id: Ida904c06ad8aa962ac850ff47cda1170103638fa
    Reviewed-on: https://chromium-review.googlesource.com/923551
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537456}

--- ui/gfx/font_names_testing.cc
+++ ui/gfx/font_names_testing.cc
@@ -16,7 +16,7 @@ const char kSymbolFontName[] = "Symbol";
 #endif
 
 #if defined(OS_LINUX)
-const char kCJKFontName[] = "Kochi Mincho";
+const char kCJKFontName[] = "IPAMincho";
 #elif defined(OS_MACOSX)
 const char kCJKFontName[] = "Heiti SC";
 #else
