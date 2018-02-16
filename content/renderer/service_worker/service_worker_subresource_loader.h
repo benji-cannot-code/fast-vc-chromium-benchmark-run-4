4eb4b43e80e531be1a71e90235cd2f7caec62a6d
Kenichi Ishibashi <bashi@chromium.org>
2018-02-16 02:55:59 +0000

    S13nSW: Introduce ServiceWorkerSubresourceLoader::StreamWaiter
    We should delay completing a resource request until all data has
    been received. Before this CL, we completed a request immediately after
    the response header comes in. To delay completing requests this CL
    introduces ServiceWorkerSubresourceLoader::StreamWaiter, which is
    similar to ServiceWorkerURLLoaderJob::StreamWaiter. It completes
    a request after OnCompleted() callback is called (or OnAborted() is
    called).
    
    This CL also fixes some typos.
    
    Bug: 778110
    Change-Id: I6f3292eeb483b83e74f0d0be1df8b5e555e8c8f5
    Reviewed-on: https://chromium-review.googlesource.com/920045
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537153}

--- content/renderer/service_worker/service_worker_subresource_loader.h
+++ content/renderer/service_worker/service_worker_subresource_loader.h
@@ -56,6 +56,8 @@ class CONTENT_EXPORT ServiceWorkerSubresourceLoader
   void OnConnectionClosed() override;
 
  private:
+  class StreamWaiter;
+
   void DeleteSoon();
 
   void StartRequest(const network::ResourceRequest& resource_request);
@@ -80,7 +82,7 @@ class CONTENT_EXPORT ServiceWorkerSubresourceLoader
   void OnFallback(base::Time dispatch_event_time) override;
 
   void StartResponse(const ServiceWorkerResponse& response,
-                     blink::mojom::BlobPtr blob,
+                     blink::mojom::BlobPtr body_as_blob,
                      blink::mojom::ServiceWorkerStreamHandlePtr body_as_stream);
 
   // network::mojom::URLLoader overrides:
@@ -130,6 +132,8 @@ class CONTENT_EXPORT ServiceWorkerSubresourceLoader
   // over the lifetime of this loader due to redirects.
   network::ResourceRequest resource_request_;
 
+  std::unique_ptr<StreamWaiter> stream_waiter_;
+
   // For network fallback.
   scoped_refptr<SharedURLLoaderFactory> default_loader_factory_;
 
@@ -138,7 +142,6 @@ class CONTENT_EXPORT ServiceWorkerSubresourceLoader
     kStarted,
     kSentHeader,
     kCompleted,
-    kCancelled
   };
   Status status_ = Status::kNotStarted;
 
