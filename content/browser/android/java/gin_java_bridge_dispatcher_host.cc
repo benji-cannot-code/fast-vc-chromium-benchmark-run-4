7cb6ad2b9708fc70fd700262caa20e3a73cbd1e5
Torne (Richard Coles) <torne@google.com>
2018-02-16 20:17:33 +0000

    Convert Java bridge to use JNI generator.
    Have the JNI generator generate stubs for the classes used to do
    reflection in the Java bridge, and call those instead of using the Java
    bridge's own jni_helper.h functions that reimplement much of the same
    logic in a different way.
    
    Bug: 787557
    Change-Id: I72930937ca30abb1139d025ed359f77ea6986cc2
    Reviewed-on: https://chromium-review.googlesource.com/922803
    Reviewed-by: agrieve <agrieve@chromium.org>
    Reviewed-by: Tao Bai <michaelbai@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537397}

--- content/browser/android/java/gin_java_bridge_dispatcher_host.cc
+++ content/browser/android/java/gin_java_bridge_dispatcher_host.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/android/java/gin_java_bound_object_delegate.h"
 #include "content/browser/android/java/gin_java_bridge_message_filter.h"
 #include "content/browser/android/java/java_bridge_thread.h"
-#include "content/browser/android/java/jni_helper.h"
 #include "content/common/android/gin_java_bridge_value.h"
 #include "content/common/android/hash_set.h"
 #include "content/common/gin_java_bridge_messages.h"
