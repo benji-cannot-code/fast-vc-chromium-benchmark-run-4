a122d8c4ab11ff9a66b946d317861b4d8bb558cb
Bence Béky <bnc@chromium.org>
2018-02-16 02:45:02 +0000

    Implement WebSocketSpdyStreamAdapter.
    WebSocketSpdyStreamAdapter wraps a SpdyStream so that it can be used by
    the future WebSocketHttp2HandshakeStream class to do the  WebSocket
    handshake, then handed off to WebSocketBasicStream.
    
    Bug: 801564
    Change-Id: Icac1d4e19d12b390bd61562de9150698be81f5f7
    Reviewed-on: https://chromium-review.googlesource.com/912009
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Bence Béky <bnc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537135}

--- net/websockets/websocket_basic_stream.h
+++ net/websockets/websocket_basic_stream.h
@@ -10,8 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-#include "base/callback.h"
 #include "base/memory/ref_counted.h"
+#include "net/base/completion_callback.h"
 #include "net/base/net_export.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
 #include "net/websockets/websocket_frame_parser.h"
