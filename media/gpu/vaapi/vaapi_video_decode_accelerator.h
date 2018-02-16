753f8de1053e3b18b7abfb6b8461274ea5f8a74b
Miguel Casas <mcasas@chromium.org>
2018-02-16 04:36:35 +0000

    vaapi deco cleanup: extract VaapiDecodeSurface into its own file
    This CL extracts VaapiDecodeSurface out of VaapiVideoDecodeAccelerator
    into a file on its own, because vaapi_video_decode_accelerator.cc is
    quite large at 1902 lines.
    
    It also introduces =default and adds DISALLOW_COPY_AND_ASSIGN to the
    extracted class. Otherwise no new code, just moving things around.
    
    There's also a side bonus: VaapiVideoDecodeAccelerator::VaapiDecodeSurface
    uses are shortened in vaapi_video_decode_accelerator.
    
    Test: Compile and run in simplechrome on soraka. Ran vp8, vp9
    videos of crosvideo.appspot.com, verified GpuVideoDecoder engaged.
    
    Bug: 717265
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ibd7c88357b2f993a5d72e45f65dfff5d67c4194a
    Reviewed-on: https://chromium-review.googlesource.com/922981
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Emircan Uysaler <emircan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537201}

--- media/gpu/vaapi/vaapi_video_decode_accelerator.h
+++ media/gpu/vaapi/vaapi_video_decode_accelerator.h
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/macros.h"
 #include "base/memory/linked_ptr.h"
-#include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
 #include "base/single_thread_task_runner.h"
 #include "base/synchronization/condition_variable.h"
@@ -32,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/gpu/gpu_video_decode_accelerator_helpers.h"
 #include "media/gpu/media_gpu_export.h"
 #include "media/gpu/shared_memory_region.h"
+#include "media/gpu/vaapi/vaapi_decode_surface.h"
 #include "media/gpu/vaapi/vaapi_picture_factory.h"
 #include "media/gpu/vaapi/vaapi_wrapper.h"
 #include "media/video/picture.h"
@@ -57,9 +57,6 @@ class VaapiPicture;
 class MEDIA_GPU_EXPORT VaapiVideoDecodeAccelerator
     : public VideoDecodeAccelerator {
  public:
-  // Wrapper of a VASurface with id and visible area.
-  class VaapiDecodeSurface;
-
   VaapiVideoDecodeAccelerator(
       const MakeGLContextCurrentCallback& make_context_current_cb,
       const BindGLImageCallback& bind_image_cb);
