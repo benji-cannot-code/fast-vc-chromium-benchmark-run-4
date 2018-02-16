1bffcba71f0b38f21db6f5c33dce673782ac21f2
sangwoo.ko <sangwoo108@gmail.com>
2018-02-16 22:49:46 +0000

    Try saving pinned tabs on OnBrowserRemoved
    Bug:71939
    
    PinnedTabService is observing two events:
    NOTIFICATION_CLOSE_ALL_BROWSERS_REQUEST and BrowserList::OnBrowserClosing.
    
    The first is when users want to shut down entire app and
    the latter is when users close a browser at once even if it has multiple tabs.
    
    But the bug is edge case which doesn't belong to any case above for now.
    When tab strip is empty, BrowserListObserver::OnBrowserClosing is not called even
    this will end up with closing the browser.
    
    So try saving pinned tabs again on BrowserListObserver::OnBrowserRemoved.
    
    Change-Id: Id73891b9509918bca3b1dcaf0b34cc32a7a687a7
    Test: PinnedTabServiceBrowserTest.*
    Reviewed-on: https://chromium-review.googlesource.com/892539
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537442}

--- chrome/browser/ui/tabs/pinned_tab_service.h
+++ chrome/browser/ui/tabs/pinned_tab_service.h
@@ -35,6 +35,7 @@ class PinnedTabService : public content::NotificationObserver,
 
   // BrowserListObserver:
   void OnBrowserClosing(Browser* browser) override;
+  void OnBrowserRemoved(Browser* browser) override;
 
   Profile* profile_;
 
