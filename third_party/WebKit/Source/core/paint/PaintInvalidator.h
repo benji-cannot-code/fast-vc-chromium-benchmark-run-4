61c1c9f4474479ec4a8ed3054bb6ea57a0c11c14
Vladimir Levin <vmpstr@chromium.org>
2018-02-16 21:22:55 +0000

    [PE] Remove GetDocument() calls from a frequently used PaintInvalidator func.
    This patch removes GetDocument() call which loads a large object into
    memory for every PaintInvalidator::InvalidatePaint call on a LayoutObject.
    
    Instead, it stores this value on the paint invalidator and updates it
    whwnever we encounter a new LocalFrameView during the prepaint walk.
    
    On small dom / small invalidation this only has a small benefit, but
    for complicated dom, my local perf tests show large improvements.
    
    R=chrishtr@chromium.org
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I51139b8f8a802d608b583656d0be8bd2b7f13022
    Reviewed-on: https://chromium-review.googlesource.com/924277
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537414}

--- third_party/WebKit/Source/core/paint/PaintInvalidator.h
+++ third_party/WebKit/Source/core/paint/PaintInvalidator.h
@@ -154,6 +154,7 @@ class PaintInvalidator {
 
  private:
   friend struct PaintInvalidatorContext;
+  friend class PrePaintTreeWalk;
   template <typename Rect, typename Point>
   static LayoutRect MapLocalRectToVisualRectInBacking(
       const LayoutObject&,
@@ -176,6 +177,7 @@ class PaintInvalidator {
                                       PaintInvalidatorContext&);
 
   Vector<const LayoutObject*> pending_delayed_paint_invalidations_;
+  bool document_printing_ = false;
 };
 
 }  // namespace blink
--- third_party/WebKit/Source/core/paint/PrePaintTreeWalk.cpp
+++ third_party/WebKit/Source/core/paint/PrePaintTreeWalk.cpp
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/paint/compositing/CompositingLayerPropertyUpdater.h"
 #include "core/paint/ng/ng_paint_fragment.h"
 #include "platform/graphics/paint/GeometryMapper.h"
+#include "platform/wtf/AutoReset.h"
 
 namespace blink {
 
@@ -89,6 +90,9 @@ void PrePaintTreeWalk::Walk(LocalFrameView& frame_view) {
   auto context = [this]() -> PrePaintTreeWalkContext& {
     return context_storage_.back();
   };
+  AutoReset<bool> printing_reset(
+      &paint_invalidator_.document_printing_,
+      frame_view.GetFrame().GetDocument()->Printing());
 
   // ancestorOverflowLayer does not cross frame boundaries.
   context().ancestor_overflow_paint_layer = nullptr;
