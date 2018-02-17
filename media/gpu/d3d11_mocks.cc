b7e65d4af78654f4d0ad977326215eab5e7f9d80
Rintaro Kuroiwa <rkuroiwa@chromium.org>
2018-02-17 01:54:51 +0000

    CdmProxy implementation using D3D11
    - D3D11CdmProxy allows injecting stream type GUID, protocol, function IDs
      for possible different implementations that use D3D11.
    - D3D11 mocks for testing.
    
    Bug: 787656
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Id491699da9a03ab279f8cbc860007bc0ddc94695
    Reviewed-on: https://chromium-review.googlesource.com/792590
    Commit-Queue: Rintaro Kuroiwa <rkuroiwa@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537502}

---
+++ media/gpu/d3d11_mocks.cc
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "media/gpu/d3d11_mocks.h"
+namespace media {
+
+D3D11Texture2DMock::D3D11Texture2DMock() = default;
+D3D11Texture2DMock::~D3D11Texture2DMock() = default;
+
+D3D11DeviceMock::D3D11DeviceMock() = default;
+D3D11DeviceMock::~D3D11DeviceMock() = default;
+
+D3D11VideoDeviceMock::D3D11VideoDeviceMock() = default;
+D3D11VideoDeviceMock::~D3D11VideoDeviceMock() = default;
+
+D3D11VideoDevice1Mock::D3D11VideoDevice1Mock() = default;
+D3D11VideoDevice1Mock::~D3D11VideoDevice1Mock() = default;
+
+D3D11VideoContextMock::D3D11VideoContextMock() = default;
+D3D11VideoContextMock::~D3D11VideoContextMock() = default;
+
+D3D11VideoContext1Mock::D3D11VideoContext1Mock() = default;
+D3D11VideoContext1Mock::~D3D11VideoContext1Mock() = default;
+
+D3D11VideoDecoderMock::D3D11VideoDecoderMock() = default;
+D3D11VideoDecoderMock::~D3D11VideoDecoderMock() = default;
+
+D3D11CryptoSessionMock::D3D11CryptoSessionMock() = default;
+D3D11CryptoSessionMock::~D3D11CryptoSessionMock() = default;
+
+D3D11DeviceContextMock::D3D11DeviceContextMock() = default;
+D3D11DeviceContextMock::~D3D11DeviceContextMock() = default;
+
+}  // namespace media
