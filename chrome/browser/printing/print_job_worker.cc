cd0e8ba7ceba7e0982db7fef38da17d01bd22e51
Alexander Alekseev <alemate@chromium.org>
2018-02-17 04:51:51 +0000

    Revert "ARC Print"
    This reverts commit 53ccd0b5bfc61d946a9597b45d0a8f5d50f561ec.
    
    Reason for revert: broke  build for devices without CUPS.
    
    Original change's description:
    > ARC Print
    >
    > This allows container to use native printers.
    >
    > Bug: 625126
    > Change-Id: Idb1b1de9a11877fdfa9e9eb4e9ec87d4bf089ef7
    > Reviewed-on: https://chromium-review.googlesource.com/648979
    > Commit-Queue: Vladislav Kuzkokov <vkuzkokov@chromium.org>
    > Reviewed-by: Luis Hector Chavez <lhchavez@chromium.org>
    > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > Reviewed-by: Mattias Nissler <mnissler@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#536784}
    
    TBR=thestig@chromium.org,lhchavez@chromium.org,vkuzkokov@chromium.org,mnissler@chromium.org,skau@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 625126, 813246
    Change-Id: I271836252c755f2586a2869d2770eb065426920d
    Reviewed-on: https://chromium-review.googlesource.com/924245
    Commit-Queue: Wei Li <weili@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537531}

--- chrome/browser/printing/print_job_worker.cc
+++ chrome/browser/printing/print_job_worker.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include <string>
-#include <utility>
 
 #include "base/bind.h"
 #include "base/bind_helpers.h"
@@ -207,20 +206,6 @@ void PrintJobWorker::SetSettings(
                          base::Unretained(this), std::move(new_settings))));
 }
 
-#if defined(OS_CHROMEOS)
-void PrintJobWorker::SetSettingsFromPOD(
-    std::unique_ptr<printing::PrintSettings> new_settings) {
-  DCHECK(task_runner_->RunsTasksInCurrentSequence());
-
-  BrowserThread::PostTask(
-      BrowserThread::UI, FROM_HERE,
-      base::BindOnce(
-          &HoldRefCallback, base::WrapRefCounted(owner_),
-          base::BindOnce(&PrintJobWorker::UpdatePrintSettingsFromPOD,
-                         base::Unretained(this), std::move(new_settings))));
-}
-#endif
-
 void PrintJobWorker::UpdatePrintSettings(
     std::unique_ptr<base::DictionaryValue> new_settings) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
@@ -228,16 +213,6 @@ void PrintJobWorker::UpdatePrintSettings(
       printing_context_->UpdatePrintSettings(*new_settings);
   GetSettingsDone(result);
 }
-
-#if defined(OS_CHROMEOS)
-void PrintJobWorker::UpdatePrintSettingsFromPOD(
-    std::unique_ptr<printing::PrintSettings> new_settings) {
-  DCHECK_CURRENTLY_ON(BrowserThread::UI);
-  PrintingContext::Result result =
-      printing_context_->UpdatePrintSettingsFromPOD(std::move(new_settings));
-  GetSettingsDone(result);
-}
-#endif
 
 void PrintJobWorker::GetSettingsDone(PrintingContext::Result result) {
   // Most PrintingContext functions may start a message loop and process
