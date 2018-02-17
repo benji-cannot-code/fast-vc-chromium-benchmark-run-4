ce412a45ceea09b34a0b6b019d45822a3dbd0ff6
Nasko Oskov <nasko@chromium.org>
2018-02-17 02:44:29 +0000

    Add regular versions of ReadyToCommitUntilCommit and TimeToReadyToCommit
    Currently, these two histograms are only recorded for back/forward
    navigations. In order to have more complete picture of navigation
    performance, these should be recorded for all successful, non-same
    document navigations.
    
    Bug: 808114
    Change-Id: Ifecd66ad82fc0f495e918edf21865a2a61a32939
    Reviewed-on: https://chromium-review.googlesource.com/924457
    Commit-Queue: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537514}

--- content/browser/frame_host/navigation_request.cc
+++ content/browser/frame_host/navigation_request.cc
@@ -550,7 +550,7 @@ void NavigationRequest::BeginNavigation() {
                                                            common_params_.url);
 
   // Inform the NavigationHandle that the navigation will commit.
-  navigation_handle_->ReadyToCommitNavigation(render_frame_host);
+  navigation_handle_->ReadyToCommitNavigation(render_frame_host, false);
 
   CommitNavigation();
 }
@@ -1332,7 +1332,7 @@ void NavigationRequest::CommitErrorPage(
     const base::Optional<std::string>& error_page_content) {
   UpdateRequestNavigationParamsHistory();
   frame_tree_node_->TransferNavigationRequestOwnership(render_frame_host);
-  navigation_handle_->ReadyToCommitNavigation(render_frame_host);
+  navigation_handle_->ReadyToCommitNavigation(render_frame_host, true);
   render_frame_host->FailedNavigation(common_params_, request_params_,
                                       has_stale_copy_in_cache_, net_error_,
                                       error_page_content);
