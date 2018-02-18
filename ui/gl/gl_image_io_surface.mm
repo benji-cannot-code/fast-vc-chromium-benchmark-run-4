e9156f39932a12866d3b2a48d4247408a51bf432
Miguel Casas <mcasas@chromium.org>
2018-02-18 13:58:28 +0000

    GpuMemoryBuffer: add image_xb30 capability and RGBX_1010102 BufferFormat
    This CL adds a new Gpu FeatureInfo::FeatureFlags and command buffer
    Capabilities to signal support for image_xb30, in parallel to the
    existing image_xr30. The new flag is used on platforms supporting
    glTexImage2D() with data format GL_RGB(A), namely >=gles3, >=gl3.3
    or those with the appropriate GL extension.
    
    This CL also adds a new GpuMemoryBuffer format RGBX_1010102 to
    be used where image_xb30 is signalled.
    
    With all this, Mac will use image_xr30 <-> BGRX_1010102, and the
    GLES3+/GL3.3+ platforms will use image_xb30 <-> RGBX_1010102.
    
    Bug: 803975, 776093, 803451, 808172
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I00c834b68ce81d93e0f55694d891186ed3b75611
    Reviewed-on: https://chromium-review.googlesource.com/920361
    Reviewed-by: David Reveman <reveman@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537561}

--- ui/gl/gl_image_io_surface.mm
+++ ui/gl/gl_image_io_surface.mm
@@ -77,6 +77,7 @@ bool ValidFormat(gfx::BufferFormat format) {
     case gfx::BufferFormat::BGR_565:
     case gfx::BufferFormat::RGBA_4444:
     case gfx::BufferFormat::RGBX_8888:
+    case gfx::BufferFormat::RGBX_1010102:
     case gfx::BufferFormat::YVU_420:
       return false;
   }
@@ -114,6 +115,7 @@ GLenum TextureFormat(gfx::BufferFormat format) {
     case gfx::BufferFormat::BGR_565:
     case gfx::BufferFormat::RGBA_4444:
     case gfx::BufferFormat::RGBX_8888:
+    case gfx::BufferFormat::RGBX_1010102:
     case gfx::BufferFormat::YVU_420:
       NOTREACHED();
       return 0;
@@ -148,6 +150,7 @@ GLenum DataFormat(gfx::BufferFormat format) {
     case gfx::BufferFormat::BGR_565:
     case gfx::BufferFormat::RGBA_4444:
     case gfx::BufferFormat::RGBX_8888:
+    case gfx::BufferFormat::RGBX_1010102:
     case gfx::BufferFormat::YVU_420:
     case gfx::BufferFormat::YUV_420_BIPLANAR:
       NOTREACHED() << gfx::BufferFormatToString(format);
@@ -183,6 +186,7 @@ GLenum DataType(gfx::BufferFormat format) {
     case gfx::BufferFormat::BGR_565:
     case gfx::BufferFormat::RGBA_4444:
     case gfx::BufferFormat::RGBX_8888:
+    case gfx::BufferFormat::RGBX_1010102:
     case gfx::BufferFormat::YVU_420:
     case gfx::BufferFormat::YUV_420_BIPLANAR:
       NOTREACHED() << gfx::BufferFormatToString(format);
