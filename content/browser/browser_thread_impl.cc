71128f9d17f463b4c5587d720bd00c78e16fe3ce
Dave Tapuska <dtapuska@chromium.org>
2018-02-18 16:10:54 +0000

    Fix shadowed variables in the content layer.
    Shadowed variables can make code harder to read. Don't support them
    in the content layer.
    
    BUG=794619
    
    This CL was uploaded by git cl split.
    
    R=boliu@chromium.org
    
    Change-Id: Iefa946eb8500d28baf16b7e7519303cc29233794
    Reviewed-on: https://chromium-review.googlesource.com/923349
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537565}

--- content/browser/browser_thread_impl.cc
+++ content/browser/browser_thread_impl.cc
@@ -423,7 +423,7 @@ void BrowserThreadImpl::StopRedirectionOfThreadID(
       FROM_HERE,
       base::BindOnce(&base::WaitableEvent::Signal, base::Unretained(&flushed)));
   {
-    base::AutoUnlock auto_lock(globals.lock);
+    base::AutoUnlock auto_unlock(globals.lock);
     flushed.Wait();
   }
 
