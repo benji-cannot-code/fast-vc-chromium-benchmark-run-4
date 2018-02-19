ecf8c054377e99e373a937a3bab46340e4586b08
Yutaka Hirano <yhirano@chromium.org>
2018-02-19 05:33:43 +0000

    Make WebURLRequest::ExtraData ownership transfer explicit
    This is probably not going to fix the leak but will make things
    clearer.
    
    Bug: 808390
    Change-Id: I625ed2fde63d1eb1bb09fcb5ed3e522929d9f025
    Reviewed-on: https://chromium-review.googlesource.com/923447
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537581}

--- content/renderer/pepper/url_request_info_util.cc
+++ content/renderer/pepper/url_request_info_util.cc
@@ -226,7 +226,7 @@ bool CreateWebURLRequest(PP_Instance instance,
   }
 
   if (data->has_custom_user_agent || !name_version.empty()) {
-    RequestExtraData* extra_data = new RequestExtraData();
+    auto extra_data = std::make_unique<RequestExtraData>();
     if (data->has_custom_user_agent) {
       extra_data->set_custom_user_agent(
           WebString::FromUTF8(data->custom_user_agent));
@@ -234,7 +234,7 @@ bool CreateWebURLRequest(PP_Instance instance,
     if (!name_version.empty()) {
       extra_data->set_requested_with(WebString::FromUTF8(name_version));
     }
-    dest->SetExtraData(extra_data);
+    dest->SetExtraData(std::move(extra_data));
   }
 
   return true;
