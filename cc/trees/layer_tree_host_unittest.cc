b6a8033dd35e3c39c625daf2234100183eb5b10d
Thiemo Nagel <tnagel@chromium.org>
2018-02-19 15:32:13 +0000

    Disable flaky LayerTreeHostTestEarlyDamageCheckStops on Windows
    BUG=813578
    TBR=vmpstr
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I17a59d1d7d20c4bdd36388b2133892625253977d
    Reviewed-on: https://chromium-review.googlesource.com/924197
    Commit-Queue: Thiemo Nagel <tnagel@chromium.org>
    Reviewed-by: Thiemo Nagel <tnagel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537658}

--- cc/trees/layer_tree_host_unittest.cc
+++ cc/trees/layer_tree_host_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/synchronization/lock.h"
 #include "base/threading/thread_task_runner_handle.h"
+#include "build/build_config.h"
 #include "cc/animation/timing_function.h"
 #include "cc/input/scroll_elasticity_helper.h"
 #include "cc/layers/content_layer_client.h"
@@ -1266,9 +1267,12 @@ class LayerTreeHostTestEarlyDamageCheckStops : public LayerTreeHostTest {
   int damaged_frame_limit_;
 };
 
+// Flaky on Win7 Tests (dbg)(1). https://crbug.com/813578
+#if !defined(OS_WIN)
 // This behavior is specific to Android WebView, which only uses
 // multi-threaded compositor.
 MULTI_THREAD_TEST_F(LayerTreeHostTestEarlyDamageCheckStops);
+#endif
 
 // Verify CanDraw() is false until first commit.
 class LayerTreeHostTestCantDrawBeforeCommit : public LayerTreeHostTest {
