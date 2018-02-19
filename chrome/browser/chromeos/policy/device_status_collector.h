8a319f42c0006f7c61155b39cd018db74963c2c7
Takeshi Yoshino <tyoshino@chromium.org>
2018-02-19 07:30:07 +0000

    Revert "Add user to active sessions reporting."
    This reverts commit d9b74d0c8c1e8686ffd0bb06cf16c7fcc89bdf66.
    
    Reason for revert: See the FindIt message. It looks this CL has broken the "Linux Chromium OS ASan LSan Tests" bot
    
    Original change's description:
    > Add user to active sessions reporting.
    > 
    > Previously, active sessions and recent users were reported separately,
    > that is confusing on the server side.
    > This change adds 'user_email' field to ActiveTimePeriod proto message
    > and populates it for affiliated users that are eligible for reporting.
    > Also, user email is reported only if user reporting is turned on.
    > 
    > BUG=810093
    > TEST=Browser tests added.
    > 
    > Change-Id: I92fec6725349972895ec0a439f78efa59853670e
    > Reviewed-on: https://chromium-review.googlesource.com/904527
    > Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Commit-Queue: Sergey Poromov <poromov@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#537435}
    
    TBR=xiyuan@chromium.org,emaxx@chromium.org,poromov@chromium.org,pmarko@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 810093
    Change-Id: I96152915d5da8d763c32132bade51b2bc5a4bcd7
    Reviewed-on: https://chromium-review.googlesource.com/923228
    Reviewed-by: Takeshi Yoshino <tyoshino@chromium.org>
    Commit-Queue: Takeshi Yoshino <tyoshino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537593}

--- chrome/browser/chromeos/policy/device_status_collector.h
+++ chrome/browser/chromeos/policy/device_status_collector.h
@@ -42,7 +42,6 @@ namespace user_manager {
 class User;
 }
 
-class PrefChangeRegistrar;
 class PrefRegistrySimple;
 class PrefService;
 class Profile;
@@ -169,9 +168,7 @@ class DeviceStatusCollector {
                                  int min_day_trim_duration,
                                  int64_t max_day_key);
 
-  void AddActivePeriod(base::Time start,
-                       base::Time end,
-                       const std::string& active_user_email);
+  void AddActivePeriod(base::Time start, base::Time end);
 
   // Clears the cached hardware resource usage.
   void ClearCachedResourceUsage();
@@ -221,9 +218,6 @@ class DeviceStatusCollector {
 
   // Callback invoked to update our cpu usage information.
   void ReceiveCPUStatistics(const std::string& statistics);
-
-  // Callback invoked when reporting users pref is changed.
-  void ReportingUsersChanged();
 
   PrefService* const local_state_;
 
@@ -305,8 +299,6 @@ class DeviceStatusCollector {
       os_update_status_subscription_;
   std::unique_ptr<chromeos::CrosSettings::ObserverSubscription>
       running_kiosk_app_subscription_;
-
-  std::unique_ptr<PrefChangeRegistrar> pref_change_registrar_;
 
   // Task runner in the creation thread where responses are sent to.
   scoped_refptr<base::SequencedTaskRunner> task_runner_;
