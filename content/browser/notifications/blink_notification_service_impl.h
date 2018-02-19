65702d47c7818e76dcf15329b69bbbe7772d8f36
Anita Woodruff <awdf@chromium.org>
2018-02-19 17:07:42 +0000

    [Testing++] Add BlinkNotificationServiceImpl unit tests
    Change-Id: Id007aa212c96d2d171a9f658da16d47e17ba21e3
    Reviewed-on: https://chromium-review.googlesource.com/911573
    Commit-Queue: Anita Woodruff <awdf@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537669}

--- content/browser/notifications/blink_notification_service_impl.h
+++ content/browser/notifications/blink_notification_service_impl.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
+#include "content/common/content_export.h"
 #include "content/public/browser/browser_context.h"
 #include "mojo/public/cpp/bindings/binding.h"
 #include "mojo/public/cpp/bindings/interface_request.h"
@@ -23,7 +24,8 @@ class ResourceContext;
 // Implementation of the NotificationService used for Web Notifications. Is
 // responsible for displaying, updating and reading of both non-persistent
 // and persistent notifications. Lives on the IO thread.
-class BlinkNotificationServiceImpl : public blink::mojom::NotificationService {
+class CONTENT_EXPORT BlinkNotificationServiceImpl
+    : public blink::mojom::NotificationService {
  public:
   BlinkNotificationServiceImpl(
       PlatformNotificationContextImpl* notification_context,
