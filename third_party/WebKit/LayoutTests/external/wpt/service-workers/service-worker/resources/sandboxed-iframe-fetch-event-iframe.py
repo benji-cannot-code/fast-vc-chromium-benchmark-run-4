649d76137a9667a82d42db8a6191957cab381f2c
Makoto Shimazu <shimazu@chromium.org>
2018-02-19 09:13:48 +0000

    Refactor tests for sandbox iframes controlled by service worker
    This patch is to split sandboxed-iframe-fetch-event.https.html into small
    chunks. Each promise_test has smaller number of assertions, so that now we can
    see the results for each situation.
    
    Bug: 771815
    Change-Id: I75cdd67f92bbb07d6b538b209e1f0819752c3bc8
    Reviewed-on: https://chromium-review.googlesource.com/923901
    Commit-Queue: Makoto Shimazu <shimazu@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537606}

---
+++ third_party/WebKit/LayoutTests/external/wpt/service-workers/service-worker/resources/sandboxed-iframe-fetch-event-iframe.py
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import os.path
+
+def main(request, response):
+  header = [('Content-Type', 'text/html')]
+  if 'test' in request.GET:
+    with open(os.path.join(os.path.dirname(__file__),'blank.html'), 'r') as f:
+      body = f.read()
+    return (header, body)
+
+  with open(os.path.join(os.path.dirname(__file__),
+                         'sandboxed-iframe-fetch-event-iframe.html'), 'r') as f:
+    body = f.read()
+  return (header, body)
--- third_party/WebKit/LayoutTests/external/wpt/service-workers/service-worker/resources/sandboxed-iframe-fetch-event-worker.js
+++ third_party/WebKit/LayoutTests/external/wpt/service-workers/service-worker/resources/sandboxed-iframe-fetch-event-worker.js
@@ -11,6 +11,7 @@ self.addEventListener('message', function(event) {
           client_urls = client_urls.sort();
           event.data.port.postMessage(
               {clients: client_urls, requests: requests});
+          requests = [];
         }));
   });
 
