de3a56de7fd02d6fd512cdd3ab9706b6f771178e
Alexander Khaustov <akhaustov@yandex-team.ru>
2018-02-19 19:18:49 +0000

    Reland "remove ThreadLocalStorage::StaticSlot"
    This is a reland of 88a96c6e1b592b920cd00655b88225334c0c4541.
    
    Original change's description:
    > remove ThreadLocalStorage::StaticSlot
    >
    > in favor of the pattern with
    > NoDestructor<ThreadLocalStorage::Slot> as discussed on PS1 @
    > https://chromium-review.googlesource.com/c/chromium/src/+/881017/1
    >
    > StaticSlot has been subject to a possible initialization race:
    > CHECK_NE(slot_, kInvalidSlotValue) in StaticSlot::Initialize
    > could fail if another thread initialized it to kInvalidSlotValue
    >
    > Change-Id: Iefaa1dde6f85842d20561851a9a26ad5f3a20973
    > Reviewed-on: https://chromium-review.googlesource.com/881017
    > Reviewed-by: Erik Chen <erikchen@chromium.org>
    > Reviewed-by: Miriam Gershenson <mgersh@chromium.org>
    > Reviewed-by: Robert Sesek <rsesek@chromium.org>
    > Reviewed-by: Peter Kasting <pkasting@chromium.org>
    > Reviewed-by: Gabriel Charette <gab@chromium.org>
    > Commit-Queue: Gabriel Charette <gab@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#537009}
    
    TBR=gab@chromium.org (already reviewed by others in original CL)
    
    Change-Id: I5675a9fe38d09486489155f897d602c87e008bb4
    Reviewed-on: https://chromium-review.googlesource.com/924001
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537681}

--- components/url_formatter/idn_spoof_checker.cc
+++ components/url_formatter/idn_spoof_checker.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/url_formatter/idn_spoof_checker.h"
 
+#include "base/no_destructor.h"
 #include "base/numerics/safe_conversions.h"
 #include "base/strings/string_piece.h"
 #include "base/strings/string_split.h"
@@ -21,10 +22,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace url_formatter {
 
 namespace {
-base::ThreadLocalStorage::StaticSlot tls_index = TLS_INITIALIZER;
 
 void OnThreadTermination(void* regex_matcher) {
   delete reinterpret_cast<icu::RegexMatcher*>(regex_matcher);
+}
+
+base::ThreadLocalStorage::Slot& DangerousPatternTLS() {
+  static base::NoDestructor<base::ThreadLocalStorage::Slot>
+      dangerous_pattern_tls(&OnThreadTermination);
+  return *dangerous_pattern_tls;
 }
 
 #include "components/url_formatter/top_domains/alexa_skeletons-inc.cc"
@@ -250,10 +256,8 @@ bool IDNSpoofChecker::SafeToDisplayAsUnicode(base::StringPiece16 label,
       !lgc_letters_n_ascii_.containsAll(label_string))
     return false;
 
-  if (!tls_index.initialized())
-    tls_index.Initialize(&OnThreadTermination);
   icu::RegexMatcher* dangerous_pattern =
-      reinterpret_cast<icu::RegexMatcher*>(tls_index.Get());
+      reinterpret_cast<icu::RegexMatcher*>(DangerousPatternTLS().Get());
   if (!dangerous_pattern) {
     // Disallow the katakana no, so, zo, or n, as they may be mistaken for
     // slashes when they're surrounded by non-Japanese scripts (i.e. scripts
@@ -296,7 +300,7 @@ bool IDNSpoofChecker::SafeToDisplayAsUnicode(base::StringPiece16 label,
             R"([ijl]\u0307)",
             -1, US_INV),
         0, status);
-    tls_index.Set(dangerous_pattern);
+    DangerousPatternTLS().Set(dangerous_pattern);
   }
   dangerous_pattern->reset(label_string);
   return !dangerous_pattern->find();
