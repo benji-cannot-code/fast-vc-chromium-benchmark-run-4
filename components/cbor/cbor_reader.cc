00c7e526b8dee3e9709938db90ad155dba4c0d35
Kouhei Ueno <kouhei@chromium.org>
2018-02-20 03:03:09 +0000

    CBORReader: Add alternate ::Read() which allows extraneous data
    Before this CL, CBORReader::Read() rejected CBOR-encoded data with extra bytes
    at the end, and reported CBORReader::DecoderError::EXTRANEOUS_DATA.
    
    This CL introduces alternative CBORReader::Read() which allows extraneous data
    following CBOR-encoded data. Callers can tell the size of the CBOR-encoded data
    via |num_consumed_bytes|.
    
    Bug: 803774
    Change-Id: I154534f5049e94f3d20e67c4889e711332b1726f
    Reviewed-on: https://chromium-review.googlesource.com/920802
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537699}

--- components/cbor/cbor_reader.cc
+++ components/cbor/cbor_reader.cc
@@ -58,7 +58,11 @@ const char kOutOfRangeIntegerValue[] =
 
 CBORReader::CBORReader(base::span<const uint8_t>::const_iterator it,
                        const base::span<const uint8_t>::const_iterator end)
-    : it_(it), end_(end), error_code_(DecoderError::CBOR_NO_ERROR) {}
+    : begin_(it),
+      it_(it),
+      end_(end),
+      error_code_(DecoderError::CBOR_NO_ERROR) {}
+
 CBORReader::~CBORReader() {}
 
 // static
@@ -66,7 +70,8 @@ base::Optional<CBORValue> CBORReader::Read(base::span<uint8_t const> data,
                                            DecoderError* error_code_out,
                                            int max_nesting_level) {
   CBORReader reader(data.cbegin(), data.cend());
-  base::Optional<CBORValue> decoded_cbor = reader.DecodeCBOR(max_nesting_level);
+  base::Optional<CBORValue> decoded_cbor =
+      reader.DecodeCompleteDataItem(max_nesting_level);
 
   if (decoded_cbor)
     reader.CheckExtraneousData();
@@ -78,7 +83,29 @@ base::Optional<CBORValue> CBORReader::Read(base::span<uint8_t const> data,
   return decoded_cbor;
 }
 
-base::Optional<CBORValue> CBORReader::DecodeCBOR(int max_nesting_level) {
+// static
+base::Optional<CBORValue> CBORReader::Read(base::span<uint8_t const> data,
+                                           size_t* num_bytes_consumed,
+                                           DecoderError* error_code_out,
+                                           int max_nesting_level) {
+  CBORReader reader(data.cbegin(), data.cend());
+  base::Optional<CBORValue> decoded_cbor =
+      reader.DecodeCompleteDataItem(max_nesting_level);
+
+  if (error_code_out)
+    *error_code_out = reader.GetErrorCode();
+
+  if (reader.GetErrorCode() != DecoderError::CBOR_NO_ERROR) {
+    *num_bytes_consumed = 0;
+    return base::nullopt;
+  }
+
+  *num_bytes_consumed = reader.num_bytes_consumed();
+  return decoded_cbor;
+}
+
+base::Optional<CBORValue> CBORReader::DecodeCompleteDataItem(
+    int max_nesting_level) {
   if (max_nesting_level < 0 || max_nesting_level > kCBORMaxDepth) {
     error_code_ = DecoderError::TOO_MUCH_NESTING;
     return base::nullopt;
@@ -225,7 +252,8 @@ base::Optional<CBORValue> CBORReader::ReadCBORArray(uint64_t length,
                                                     int max_nesting_level) {
   CBORValue::ArrayValue cbor_array;
   while (length-- > 0) {
-    base::Optional<CBORValue> cbor_element = DecodeCBOR(max_nesting_level - 1);
+    base::Optional<CBORValue> cbor_element =
+        DecodeCompleteDataItem(max_nesting_level - 1);
     if (!cbor_element.has_value())
       return base::nullopt;
     cbor_array.push_back(std::move(cbor_element.value()));
@@ -237,8 +265,10 @@ base::Optional<CBORValue> CBORReader::ReadCBORMap(uint64_t length,
                                                   int max_nesting_level) {
   CBORValue::MapValue cbor_map;
   while (length-- > 0) {
-    base::Optional<CBORValue> key = DecodeCBOR(max_nesting_level - 1);
-    base::Optional<CBORValue> value = DecodeCBOR(max_nesting_level - 1);
+    base::Optional<CBORValue> key =
+        DecodeCompleteDataItem(max_nesting_level - 1);
+    base::Optional<CBORValue> value =
+        DecodeCompleteDataItem(max_nesting_level - 1);
     if (!key.has_value() || !value.has_value())
       return base::nullopt;
 
