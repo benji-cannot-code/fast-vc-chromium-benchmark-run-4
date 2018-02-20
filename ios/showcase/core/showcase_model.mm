4e04805dfec12961ed8703540f73e9cd97dfe362
sczs <sczs@chromium.org>
2018-02-20 17:12:20 +0000

    [ios] Creates TableContainerViewController.
    Creates a TableContainerViewController that contains a ChromeTableViewController.
    Adds it to showcase.
    
    Screenshot:
    https://drive.google.com/open?id=1kCC5xDoreepzp2Y-uQmVUxMUP_Vae_Y_
    
    
    Bug: 805013
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I5f5d58eab32a8c13cbcc8ed32f4a6d5cbc7b7598
    Reviewed-on: https://chromium-review.googlesource.com/923408
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537805}

--- ios/showcase/core/showcase_model.mm
+++ ios/showcase/core/showcase_model.mm
@@ -82,6 +82,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       showcase::kClassForInstantiationKey : @"SCTabGridCoordinator",
       showcase::kUseCaseKey : @"Full tab grid UI",
     },
+    @{
+      showcase::kClassForDisplayKey : @"TableContainerViewController",
+      showcase::kClassForInstantiationKey : @"SCTableContainerCoordinator",
+      showcase::kUseCaseKey : @"Table View",
+    },
   ];
 }
 
