4507e071b0c9bbc7dbd526d8dc6c5fa898639275
Peter K. Lee <pkl@chromium.org>
2018-02-20 19:59:39 +0000

    Use base::FeatureList for FeedbackKit V2
    Use base::FeatureList and about:flags to enable V2 feature.
    Default for this flag is DISABLED.
    
    Bug: 657076
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I4b5ee8c95a79f533fded6b717efa0dd6c0bae895
    Reviewed-on: https://chromium-review.googlesource.com/900543
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Peter Lee <pkl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537860}

--- ios/chrome/browser/about_flags.mm
+++ ios/chrome/browser/about_flags.mm
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/omnibox/omnibox_clipping_feature.h"
 #import "ios/chrome/browser/ui/toolbar/public/toolbar_controller_base_feature.h"
 #include "ios/chrome/browser/ui/ui_feature_flags.h"
+#include "ios/chrome/browser/ui/user_feedback_features.h"
 #include "ios/chrome/grit/ios_strings.h"
 #include "ios/public/provider/chrome/browser/chrome_browser_provider.h"
 #include "ios/web/public/features.h"
@@ -244,6 +245,9 @@ const flags_ui::FeatureEntry kFeatureEntries[] = {
     {"tab-switcher-tab-grid", flag_descriptions::kTabSwitcherTabGridName,
      flag_descriptions::kTabSwitcherTabGridDescription, flags_ui::kOsIos,
      FEATURE_VALUE_TYPE(kTabSwitcherTabGrid)},
+    {"feedback-kit-v2", flag_descriptions::kFeedbackKitV2Name,
+     flag_descriptions::kFeedbackKitV2Description, flags_ui::kOsIos,
+     FEATURE_VALUE_TYPE(kFeedbackKitV2)},
 };
 
 // Add all switches from experimental flags to |command_line|.
