d497f669f7d1cfa0d8ba60df545070245959892c
Gauthier Ambard <gambard@chromium.org>
2018-02-20 14:43:25 +0000

    Add EG test to check the toolbar visibility
    This CL adds an Earl Grey test to ensure that the toolbar buttons and
    the toolbar visibility is correctly updated when the size class changes.
    
    Bug: 804726
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Iee3937485ddf379695fabe420ce3311ddea4aba3
    Reviewed-on: https://chromium-review.googlesource.com/919061
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: edchin <edchin@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537775}

--- ios/chrome/browser/ui/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view_controller.mm
@@ -2070,7 +2070,7 @@ applicationCommandEndpoint:(id<ApplicationCommands>)applicationCommandEndpoint {
 // The height of the secondary toolbar with the bottom safe area inset included.
 // Returns 0 if the toolbar should be hidden.
 - (CGFloat)secondaryToolbarHeightWithInset {
-  if (!IsSplitToolbarMode())
+  if (!IsSplitToolbarMode(self))
     return 0;
 
   UIView* secondaryToolbar =
