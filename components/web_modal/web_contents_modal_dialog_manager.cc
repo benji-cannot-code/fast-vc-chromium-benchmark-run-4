ab48334849610a42da0eacef64d0be2d3506644a
Francois Doray <fdoray@chromium.org>
2018-02-20 18:10:58 +0000

    Remove unwanted logging from web_contents_modal_dialog_manager.cc.
    The log statements were added by error by
    https://chromium-review.googlesource.com/c/chromium/src/+/867511
    
    TBR=wittman@chromium.org
    
    Bug: 668690
    Change-Id: Iefaa5d6cf254762230d8dd4bf04b903689d36777
    Reviewed-on: https://chromium-review.googlesource.com/926782
    Reviewed-by: François Doray <fdoray@chromium.org>
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537821}

--- components/web_modal/web_contents_modal_dialog_manager.cc
+++ components/web_modal/web_contents_modal_dialog_manager.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <utility>
 
-#include "base/debug/stack_trace.h"
 #include "base/logging.h"
 #include "components/web_modal/web_contents_modal_dialog_manager_delegate.h"
 #include "content/public/browser/navigation_handle.h"
@@ -48,11 +47,8 @@ void WebContentsModalDialogManager::ShowDialogWithManager(
 
   if (child_dialogs_.size() == 1) {
     BlockWebContentsInteraction(true);
-    if (delegate_ && delegate_->IsWebContentsVisible(web_contents())) {
-      LOG(ERROR) << "Initial show";
-      base::debug::StackTrace().Print();
+    if (delegate_ && delegate_->IsWebContentsVisible(web_contents()))
       child_dialogs_.back().manager->Show();
-    }
   }
 }
 
@@ -169,15 +165,10 @@ void WebContentsModalDialogManager::OnVisibilityChanged(
     return;
   }
 
-  if (web_contents_is_hidden_) {
-    LOG(ERROR) << "Hide";
-    base::debug::StackTrace().Print();
+  if (web_contents_is_hidden_)
     child_dialogs_.front().manager->Hide();
-  } else {
-    LOG(ERROR) << "Show";
-    base::debug::StackTrace().Print();
+  else
     child_dialogs_.front().manager->Show();
-  }
 }
 
 void WebContentsModalDialogManager::WebContentsDestroyed() {
