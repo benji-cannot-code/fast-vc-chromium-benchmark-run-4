e81ee466786d73d04ccb28620597da11a31a3ce5
Miguel Casas <mcasas@chromium.org>
2018-02-20 21:35:23 +0000

    RELAND: GpuMemoryBuffer: add image_xb30 capability and RGBX_1010102 BufferFormat
    The original CL was reverted due to two gl_unittests_ozone failures:
     EGLApiTest.DisabledExtensionBitTest
     EGLApiTest.DisabledExtensionStringTest
    these two tests also fail on my soraka ToT, so I think they're just
    flaky. The fix is to check for GetSurfaceFactoryOzone() before using
    it in HasGLOzone():
    http://crrev.com/c/926862/1..3/ui/gl/init/ozone_util.h
    
    TBR=reveman@chromium.org, piman@chromium.org, rsesek@chromium.org, dalecurtis@chomium.org
    since the code they reviewed and LGTM'd hasn't changed.
    
    Original CL description ------------------------------------------------
    
    This CL adds a new Gpu FeatureInfo::FeatureFlags and command buffer
    Capabilities to signal support for image_xb30, in parallel to the
    existing image_xr30. The new flag is used on platforms supporting
    glTexImage2D() with data format GL_RGB(A), namely >=gles3, >=gl3.3
    or those with the appropriate GL extension.
    
    This CL also adds a new GpuMemoryBuffer format RGBX_1010102 to
    be used where image_xb30 is signalled.
    
    With all this, Mac will use image_xr30 <-> BGRX_1010102, and the
    GLES3+/GL3.3+ platforms will use image_xb30 <-> RGBX_1010102.
    
    Bug: 803975, 776093, 803451, 808172
    Change-Id: I8a5a047aabf08f08f6f6fa1d27196e29ddc67120
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Reviewed-on: https://chromium-review.googlesource.com/920361
    Reviewed-by: David Reveman <reveman@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#537561}
    Reviewed-on: https://chromium-review.googlesource.com/926862
    Cr-Commit-Position: refs/heads/master@{#537893}

--- ui/gfx/buffer_format_util.cc
+++ ui/gfx/buffer_format_util.cc
@@ -12,17 +12,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace gfx {
 namespace {
 
-const BufferFormat kBufferFormats[] = {
-    BufferFormat::ATC,       BufferFormat::ATCIA,
-    BufferFormat::DXT1,      BufferFormat::DXT5,
-    BufferFormat::ETC1,      BufferFormat::R_8,
-    BufferFormat::R_16,      BufferFormat::RG_88,
-    BufferFormat::BGR_565,   BufferFormat::RGBA_4444,
-    BufferFormat::RGBX_8888, BufferFormat::RGBA_8888,
-    BufferFormat::BGRX_8888, BufferFormat::BGRX_1010102,
-    BufferFormat::BGRA_8888, BufferFormat::RGBA_F16,
-    BufferFormat::UYVY_422,  BufferFormat::YUV_420_BIPLANAR,
-    BufferFormat::YVU_420};
+const BufferFormat kBufferFormats[] = {BufferFormat::ATC,
+                                       BufferFormat::ATCIA,
+                                       BufferFormat::DXT1,
+                                       BufferFormat::DXT5,
+                                       BufferFormat::ETC1,
+                                       BufferFormat::R_8,
+                                       BufferFormat::R_16,
+                                       BufferFormat::RG_88,
+                                       BufferFormat::BGR_565,
+                                       BufferFormat::RGBA_4444,
+                                       BufferFormat::RGBX_8888,
+                                       BufferFormat::RGBA_8888,
+                                       BufferFormat::BGRX_8888,
+                                       BufferFormat::BGRX_1010102,
+                                       BufferFormat::RGBX_1010102,
+                                       BufferFormat::BGRA_8888,
+                                       BufferFormat::RGBA_F16,
+                                       BufferFormat::UYVY_422,
+                                       BufferFormat::YUV_420_BIPLANAR,
+                                       BufferFormat::YVU_420};
 
 static_assert(arraysize(kBufferFormats) ==
                   (static_cast<int>(BufferFormat::LAST) + 1),
@@ -64,6 +73,7 @@ bool RowSizeForBufferFormatChecked(
       return true;
     case BufferFormat::BGRX_8888:
     case BufferFormat::BGRX_1010102:
+    case BufferFormat::RGBX_1010102:
     case BufferFormat::RGBX_8888:
     case BufferFormat::RGBA_8888:
     case BufferFormat::BGRA_8888:
@@ -114,6 +124,7 @@ size_t NumberOfPlanesForBufferFormat(BufferFormat format) {
     case BufferFormat::RGBA_8888:
     case BufferFormat::BGRX_8888:
     case BufferFormat::BGRX_1010102:
+    case BufferFormat::RGBX_1010102:
     case BufferFormat::BGRA_8888:
     case BufferFormat::RGBA_F16:
     case BufferFormat::UYVY_422:
@@ -143,6 +154,7 @@ size_t SubsamplingFactorForBufferFormat(BufferFormat format, size_t plane) {
     case BufferFormat::RGBA_8888:
     case BufferFormat::BGRX_8888:
     case BufferFormat::BGRX_1010102:
+    case BufferFormat::RGBX_1010102:
     case BufferFormat::BGRA_8888:
     case BufferFormat::RGBA_F16:
     case BufferFormat::UYVY_422:
@@ -217,6 +229,7 @@ size_t BufferOffsetForBufferFormat(const Size& size,
     case BufferFormat::RGBA_8888:
     case BufferFormat::BGRX_8888:
     case BufferFormat::BGRX_1010102:
+    case BufferFormat::RGBX_1010102:
     case BufferFormat::BGRA_8888:
     case BufferFormat::RGBA_F16:
     case BufferFormat::UYVY_422:
@@ -268,6 +281,8 @@ const char* BufferFormatToString(BufferFormat format) {
       return "BGRX_8888";
     case BufferFormat::BGRX_1010102:
       return "BGRX_1010102";
+    case BufferFormat::RGBX_1010102:
+      return "RGBX_1010102";
     case BufferFormat::BGRA_8888:
       return "BGRA_8888";
     case BufferFormat::RGBA_F16:
