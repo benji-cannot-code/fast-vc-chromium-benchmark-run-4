94815376880a6b1e7b611452b6285baf9abfa4f8
Alex Clarke <alexclarke@chromium.org>
2018-02-20 15:58:07 +0000

    Add trace events for Emulation.setVirtualTimePolicy
    This makes it much easier to diagnose traces where virtual time is enabled.
    
    Bug: 777763
    Change-Id: I844b2cc189fea2d324692bd2474fe597a2794380
    Reviewed-on: https://chromium-review.googlesource.com/926126
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Alex Clarke <alexclarke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537787}

--- third_party/WebKit/Source/core/inspector/InspectorEmulationAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorEmulationAgent.cpp
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/page/Page.h"
 #include "platform/geometry/DoubleRect.h"
 #include "platform/graphics/Color.h"
+#include "platform/instrumentation/tracing/TraceEvent.h"
 #include "platform/scheduler/util/thread_cpu_throttler.h"
 #include "platform/wtf/Time.h"
 #include "public/platform/Platform.h"
@@ -229,6 +230,8 @@ WTF::TimeTicks InspectorEmulationAgent::ApplyVirtualTimePolicy(
   WTF::TimeTicks virtual_time_base_ticks(
       web_local_frame_->View()->Scheduler()->EnableVirtualTime());
   if (new_policy.virtual_time_budget_ms) {
+    TRACE_EVENT_ASYNC_BEGIN1("renderer.scheduler", "VirtualTimeBudget", this,
+                             "budget", *new_policy.virtual_time_budget_ms);
     WTF::TimeDelta budget_amount =
         WTF::TimeDelta::FromMillisecondsD(*new_policy.virtual_time_budget_ms);
     web_local_frame_->View()->Scheduler()->GrantVirtualTimeBudget(
@@ -259,6 +262,7 @@ Response InspectorEmulationAgent::setNavigatorOverrides(
 }
 
 void InspectorEmulationAgent::VirtualTimeBudgetExpired() {
+  TRACE_EVENT_ASYNC_END0("renderer.scheduler", "VirtualTimeBudget", this);
   web_local_frame_->View()->Scheduler()->SetVirtualTimePolicy(
       WebViewScheduler::VirtualTimePolicy::kPause);
   GetFrontend()->virtualTimeBudgetExpired();
