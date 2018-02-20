868d298ec6bc80a62e87a378980f6b44353f9b3e
spqchan <spqchan@chromium.org>
2018-02-20 19:49:04 +0000

    [Mac] Credit Card Autofill Touch Bar Refactor
    Move the credit card autofill touch bar logic from
    AutofillPopupViewCocoa to AutofillPopupControllerImplMac
    so that is can be used on both Toolkit and Cocoa
    autofill popups.
    
    Bug: 812658
    Change-Id: Ic8d788dddabb909edee46da46899d759447a7c7d
    Reviewed-on: https://chromium-review.googlesource.com/915438
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Sarah Chan <spqchan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537852}

--- chrome/browser/ui/cocoa/web_textfield_touch_bar_controller.mm
+++ chrome/browser/ui/cocoa/web_textfield_touch_bar_controller.mm
@@ -7,7 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/mac/scoped_nsobject.h"
 #include "base/mac/sdk_forward_declarations.h"
-#import "chrome/browser/ui/cocoa/autofill/autofill_popup_view_cocoa.h"
+#include "chrome/browser/ui/autofill/autofill_popup_controller.h"
+#import "chrome/browser/ui/cocoa/autofill/credit_card_autofill_touch_bar_controller.h"
 #import "chrome/browser/ui/cocoa/tab_contents/tab_contents_controller.h"
 #import "ui/base/cocoa/touch_bar_util.h"
 
@@ -26,41 +27,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [super dealloc];
 }
 
-- (void)showCreditCardAutofillForPopupView:(AutofillPopupViewCocoa*)popupView {
-  DCHECK(popupView);
-  DCHECK([popupView window]);
-
-  window_ = [popupView window];
-
-  // Remove any existing notifications before registering for new ones.
-  NSNotificationCenter* center = [NSNotificationCenter defaultCenter];
-  [center removeObserver:self name:NSWindowWillCloseNotification object:nil];
-
-  [center addObserver:self
-             selector:@selector(popupWindowWillClose:)
-                 name:NSWindowWillCloseNotification
-               object:window_];
-  popupView_ = popupView;
-
-  if ([owner_ respondsToSelector:@selector(setTouchBar:)])
-    [owner_ performSelector:@selector(setTouchBar:) withObject:nil];
+- (void)showCreditCardAutofillWithController:
+    (autofill::AutofillPopupController*)controller {
+  autofillTouchBarController_.reset(
+      [[CreditCardAutofillTouchBarController alloc]
+          initWithController:controller]);
+  [self invalidateTouchBar];
 }
 
-- (void)popupWindowWillClose:(NSNotification*)notif {
-  popupView_ = nil;
+- (void)hideCreditCardAutofillTouchBar {
+  autofillTouchBarController_.reset();
+  [self invalidateTouchBar];
+}
 
+- (void)invalidateTouchBar {
   if ([owner_ respondsToSelector:@selector(setTouchBar:)])
     [owner_ performSelector:@selector(setTouchBar:) withObject:nil];
-
-  [[NSNotificationCenter defaultCenter]
-      removeObserver:self
-                name:NSWindowWillCloseNotification
-              object:window_];
 }
 
 - (NSTouchBar*)makeTouchBar {
-  if (popupView_)
-    return [popupView_ makeTouchBar];
+  if (autofillTouchBarController_)
+    return [autofillTouchBarController_ makeTouchBar];
 
   return nil;
 }
