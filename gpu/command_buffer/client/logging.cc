5c9ef414984c10941008165f15c2c0ca76ef7d16
Jonathan Backer <backer@chromium.org>
2018-02-21 20:51:59 +0000

    Refactor --enable-gpu-client-logging for reuse.
    This moves it out for easy reuse in crrev.com/c/914701
    
    Bug: 789238
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I789f226d5825c49f73cd453f31327278c64c9fce
    Reviewed-on: https://chromium-review.googlesource.com/929316
    Reviewed-by: Victor Miura <vmiura@chromium.org>
    Commit-Queue: Jonathan Backer <backer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538202}

---
+++ gpu/command_buffer/client/logging.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "gpu/command_buffer/client/logging.h"
+
+#if defined(GPU_CLIENT_DEBUG)
+#include "base/command_line.h"
+#include "gpu/command_buffer/client/gpu_switches.h"
+#endif  // defined(GPU_CLIENT_DEBUG)
+
+namespace gpu {
+
+LogSettings::LogSettings() : enabled_(false) {
+  GPU_CLIENT_LOG_CODE_BLOCK({
+    enabled_ = base::CommandLine::ForCurrentProcess()->HasSwitch(
+        switches::kEnableGPUClientLogging);
+  });
+}
+
+LogSettings::~LogSettings() = default;
+
+}  // namespace gpu
