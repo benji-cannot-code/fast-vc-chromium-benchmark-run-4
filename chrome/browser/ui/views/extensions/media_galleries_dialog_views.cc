978979e0b4b3f97756cab2205ec96b1eda8a0211
Allen Bauer <kylixrd@chromium.org>
2018-02-21 19:05:35 +0000

    Harmony: Removed the close button on the media-galleries dialog view.
    Bug: 766300
    Change-Id: I5a900c9542afccdfb33acc7dba90993bfdd4cce3
    Reviewed-on: https://chromium-review.googlesource.com/929201
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538175}

--- chrome/browser/ui/views/extensions/media_galleries_dialog_views.cc
+++ chrome/browser/ui/views/extensions/media_galleries_dialog_views.cc
@@ -219,6 +219,10 @@ base::string16 MediaGalleriesDialogViews::GetWindowTitle() const {
   return controller_->GetHeader();
 }
 
+bool MediaGalleriesDialogViews::ShouldShowCloseButton() const {
+  return false;
+}
+
 void MediaGalleriesDialogViews::DeleteDelegate() {
   controller_->DialogFinished(accepted_);
 }
