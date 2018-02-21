b319c566511eefbcee59d0fc120c43f1ee60e16d
Sylvain Defresne <sdefresne@chromium.org>
2018-02-21 16:46:40 +0000

    Add BookmarkRemoverHelper & fix ReadingListRemoverHelper
    Add BookmarkRemoverHelper to allow clearing bookmarks even
    if the model has not been loaded yet. The code is based on
    ReadingListRemoverHelper.
    
    Fix one bug in ReadingListRemoverHelper where the wrong
    variable was used when calling the completion callback if
    reading_list_model_ was null.
    
    Use PostTask() to execute the callback asynchronously and
    thus make it safe to delete the object from the callback.
    
    Bug: none
    Change-Id: Ie7cb2b59aac8a91c75f3920103445981dbd4a006
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Reviewed-on: https://chromium-review.googlesource.com/926803
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538134}

--- ios/chrome/browser/reading_list/reading_list_remover_helper.h
+++ ios/chrome/browser/reading_list/reading_list_remover_helper.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/scoped_observer.h"
+#include "base/sequence_checker.h"
 #include "components/reading_list/core/reading_list_model_observer.h"
 
 namespace ios {
@@ -18,14 +19,17 @@ class ReadingListModel;
 class ReadingListDownloadService;
 
 namespace reading_list {
+
+// Helper class to asynchronously remove reading list entries.
 class ReadingListRemoverHelper : public ReadingListModelObserver {
  public:
   using Callback = base::OnceCallback<void(bool)>;
-  ReadingListRemoverHelper(ios::ChromeBrowserState* browser_state);
+
+  explicit ReadingListRemoverHelper(ios::ChromeBrowserState* browser_state);
   ~ReadingListRemoverHelper() override;
 
-  // Removes all Reading list items and call completion with |true| if the
-  // cleaning was successful.
+  // Removes all Reading list items and asynchronously invoke |completion| with
+  // boolean indicating success or failure.
   void RemoveAllUserReadingListItemsIOS(Callback completion);
 
   // ReadingListModelObserver implementation.
@@ -33,13 +37,21 @@ class ReadingListRemoverHelper : public ReadingListModelObserver {
   void ReadingListModelBeingDeleted(const ReadingListModel* model) override;
 
  private:
+  // Invoked when the reading list items have been deleted. Invoke the
+  // completion callback with |success| (invocation is asynchronous so
+  // the object won't be deleted immediately).
+  void ReadlingListItemsRemoved(bool success);
+
   Callback completion_;
-  ReadingListModel* reading_list_model_;
-  ReadingListDownloadService* reading_list_download_service_;
+  ReadingListModel* reading_list_model_ = nullptr;
+  ReadingListDownloadService* reading_list_download_service_ = nullptr;
   ScopedObserver<ReadingListModel, ReadingListModelObserver> scoped_observer_;
+
+  SEQUENCE_CHECKER(sequence_checker_);
 
   DISALLOW_COPY_AND_ASSIGN(ReadingListRemoverHelper);
 };
+
 }  // namespace reading_list
 
 #endif  // IOS_CHROME_BROWSER_READING_LIST_READING_LIST_REMOVER_HELPER_H_
