482e017c2d07fd4dfd847dc7efe8d588733cf457
Peter K. Lee <pkl@chromium.org>
2018-02-21 21:43:55 +0000

    For FeedbackKit V2, do not replace right button with Cancel.
    FeedbackKit V2 UI already has a "X" on the left of the Navigation Bar,
    so a Cancel on the right is not necessary. In the past (V1), the
    UIViewController for FeedbackKit was subclassed to change the buttons
    on the Navigation Bar. This is no longer possible in V2.
    
    Bug: 657076
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I81128d7e7a9ae693b4c7439f4da19530ef7f8d69
    Reviewed-on: https://chromium-review.googlesource.com/929742
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Peter Lee <pkl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538217}

--- ios/chrome/browser/ui/settings/settings_navigation_controller.mm
+++ ios/chrome/browser/ui/settings/settings_navigation_controller.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/foundation_util.h"
 #include "components/strings/grit/components_strings.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
+#include "ios/chrome/browser/experimental_flags.h"
 #include "ios/chrome/browser/sync/sync_setup_service.h"
 #include "ios/chrome/browser/sync/sync_setup_service_factory.h"
 #import "ios/chrome/browser/ui/icons/chrome_icon.h"
@@ -168,7 +169,9 @@ newUserFeedbackController:(ios::ChromeBrowserState*)browserState
       initWithRootViewController:controller
                     browserState:browserState
                         delegate:delegate];
-  [controller navigationItem].rightBarButtonItem = [nc cancelButton];
+  if (!experimental_flags::IsNewFeedbackKitEnabled()) {
+    [controller navigationItem].rightBarButtonItem = [nc cancelButton];
+  }
   return nc;
 }
 
