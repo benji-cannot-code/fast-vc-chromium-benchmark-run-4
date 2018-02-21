4bd966d0e4e9e480e25aad3c43e45d81043cb314
Yoshifumi Inoue <yosin@chromium.org>
2018-02-21 01:33:57 +0000

    Rename non-class member TypingCommand::InsertText() to InsertTextInternal()
    This patch renames non-class member |TypingCommand::InsertText()| to
    |InsertTextInternal()| because we also have static member of |InsertText()|
    for ease of readability.
    
    This patch also moves |InsertTextInternal()| to private section because it
    is used only inside class.
    
    Change-Id: Iefdcc6e7f8d9bfd3e1eeafb5a6999f9bfe822eed
    Reviewed-on: https://chromium-review.googlesource.com/925986
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537981}

--- third_party/WebKit/Source/core/editing/commands/TypingCommand.cpp
+++ third_party/WebKit/Source/core/editing/commands/TypingCommand.cpp
@@ -406,8 +406,8 @@ void TypingCommand::InsertText(
     last_typing_command->input_type_ = input_type;
 
     EventQueueScope event_queue_scope;
-    last_typing_command->InsertText(new_text, options & kSelectInsertedText,
-                                    editing_state);
+    last_typing_command->InsertTextInternal(
+        new_text, options & kSelectInsertedText, editing_state);
     return;
   }
 
@@ -529,7 +529,7 @@ void TypingCommand::DoApply(EditingState* editing_state) {
       InsertParagraphSeparatorInQuotedContent(editing_state);
       return;
     case kInsertText:
-      InsertText(text_to_insert_, select_inserted_text_, editing_state);
+      InsertTextInternal(text_to_insert_, select_inserted_text_, editing_state);
       return;
   }
 
@@ -580,9 +580,9 @@ void TypingCommand::TypingAddedToOpenCommand(
   frame->GetEditor().AppliedEditing(this);
 }
 
-void TypingCommand::InsertText(const String& text,
-                               bool select_inserted_text,
-                               EditingState* editing_state) {
+void TypingCommand::InsertTextInternal(const String& text,
+                                       bool select_inserted_text,
+                                       EditingState* editing_state) {
   text_to_insert_ = text;
 
   if (text.IsEmpty()) {
