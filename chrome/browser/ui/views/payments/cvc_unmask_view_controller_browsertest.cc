1ec26af669150e14d43c6d9a730a0dcb6810fc5e
Maxim Kolosovskiy <kolos@chromium.org>
2018-02-21 15:18:08 +0000

    Disable flaky PaymentRequestCvcUnmaskViewControllerTest.OpenGoBackOpenPay in Win
    TBR=anthonyvd@chromium.org
    
    Bug: 814313
    Change-Id: Iebfedb8b0c1ffe7b7683853b2de4f46b7d817ba5
    Reviewed-on: https://chromium-review.googlesource.com/928726
    Reviewed-by: Maxim Kolosovskiy <kolos@chromium.org>
    Commit-Queue: Maxim Kolosovskiy <kolos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538108}

--- chrome/browser/ui/views/payments/cvc_unmask_view_controller_browsertest.cc
+++ chrome/browser/ui/views/payments/cvc_unmask_view_controller_browsertest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/strings/utf_string_conversions.h"
+#include "build/build_config.h"
 #include "chrome/browser/ui/views/payments/payment_request_browsertest_base.h"
 #include "chrome/browser/ui/views/payments/payment_request_dialog_view_ids.h"
 #include "components/autofill/core/browser/autofill_test_utils.h"
@@ -36,10 +37,16 @@ IN_PROC_BROWSER_TEST_F(PaymentRequestCvcUnmaskViewControllerTest,
   ExpectBodyContains({"\"cardSecurityCode\": \"012\""});
 }
 
+// Test is flaky crbug.com/814313
+#if defined(OS_WIN)
+#define MAYBE_OpenGoBackOpenPay DISABLED_OpenGoBackOpenPay
+#else
+#define MAYBE_OpenGoBackOpenPay OpenGoBackOpenPay
+#endif  // defined(OS_WIN)
 // Test that going in the CVC editor, backing out and opening it again to pay
 // does not crash.
 IN_PROC_BROWSER_TEST_F(PaymentRequestCvcUnmaskViewControllerTest,
-                       OpenGoBackOpenPay) {
+                       MAYBE_OpenGoBackOpenPay) {
   NavigateTo("/payment_request_no_shipping_test.html");
   autofill::AutofillProfile profile(autofill::test::GetFullProfile());
   AddAutofillProfile(profile);
