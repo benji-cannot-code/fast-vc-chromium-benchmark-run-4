80e56a05eb9d6930b3fa64f54a27f91f69197d44
Andrei Kapishnikov <kapishnikov@chromium.org>
2018-02-21 00:04:17 +0000

    Revert "Add --disable-background-tasks flag."
    This reverts commit 7b4401f0ecb3645eb61c7e9f7a2b4348bfb036a2.
    
    Reason for revert: Broke Cronet tests:
    https://ci.chromium.org/buildbot/chromium.android/Android%20Cronet%20Marshmallow%2064bit%20Perf/16545
    
    Original change's description:
    > Add --disable-background-tasks flag.
    > 
    > This flag delays execution of base::TaskPriority::BACKGROUND tasks
    > until shutdown.
    > 
    > Bug: 726937
    > Change-Id: I2ce529a410382e7199558a0ed95476767b14786b
    > Reviewed-on: https://chromium-review.googlesource.com/919866
    > Commit-Queue: François Doray <fdoray@chromium.org>
    > Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    > Reviewed-by: Tommy Martino <tmartino@chromium.org>
    > Reviewed-by: Robert Liao <robliao@chromium.org>
    > Reviewed-by: Gabriel Charette <gab@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#537862}
    
    TBR=gab@chromium.org,jam@chromium.org,fdoray@chromium.org,robliao@chromium.org,tmartino@chromium.org
    
    Change-Id: Ibc7b70e5bdebd953ad6397507bd1f0673622942f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 726937
    Reviewed-on: https://chromium-review.googlesource.com/927282
    Reviewed-by: Andrei Kapishnikov <kapishnikov@chromium.org>
    Commit-Queue: Andrei Kapishnikov <kapishnikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537943}

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -2520,7 +2520,6 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     switches::kDisable2dCanvasImageChromium,
     switches::kDisableAcceleratedJpegDecoding,
     switches::kDisableAcceleratedVideoDecode,
-    switches::kDisableBackgroundTasks,
     switches::kDisableBackgroundTimerThrottling,
     switches::kDisableBreakpad,
     switches::kDisableCompositorUkmForTests,
