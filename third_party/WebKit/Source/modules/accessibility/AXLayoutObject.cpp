bec3c7af2871a2c7cb97fadcd9c80fb2211de902
Dominic Mazzoni <dmazzoni@chromium.org>
2018-02-21 00:59:24 +0000

    Avoid creating AXObject at unsafe times
    When a Node is deleted, we need to walk the accessibility tree to
    determine events to fire, but we shouldn't create any new AXObjects
    at that time because the DOM may be untrustworthy, leading to a crash.
    
    We already had ComputeParentIfExists to handle that case, but it was
    calling MenuButtonForMenu(), which was sometimes creating a node. Add
    MenuButtonForMenuIfExists() for that important safe case.
    
    Bug: 810146
    Change-Id: I27c5df196ff6f39dcefb5a353d73d75aa7a8f677
    Reviewed-on: https://chromium-review.googlesource.com/927486
    Reviewed-by: Alice Boxhall <aboxhall@chromium.org>
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537961}

--- third_party/WebKit/Source/modules/accessibility/AXLayoutObject.cpp
+++ third_party/WebKit/Source/modules/accessibility/AXLayoutObject.cpp
@@ -1551,7 +1551,7 @@ AXObject* AXLayoutObject::ComputeParentIfExists() const {
   // menuButton and its corresponding menu are DOM siblings, but Accessibility
   // needs them to be parent/child.
   if (AriaRoleAttribute() == kMenuRole) {
-    AXObject* parent = MenuButtonForMenu();
+    AXObject* parent = MenuButtonForMenuIfExists();
     if (parent)
       return parent;
   }
