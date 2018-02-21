082ec905d074954108539a87033627be5841ef60
Lukasz Anforowicz <lukasza@chromium.org>
2018-02-21 02:43:01 +0000

    Speculative flakiness fix: CrossSiteDocumentBlockingServiceWorkerTest.*
    This CL speculatively tries to avoid test flakiness (see the referenced
    bug) by explicitly reloading the test page after service worker signals
    its readiness - the service worker should be active on the reloaded
    page.  Before this CL, the service worker tried to depend on the call
    to |self.clients.claim()| to make sure the service worker is activated
    on the test page (and we speculate that this wasn't properly
    synchronized with the rest of the test).
    
    Bug: 809735
    Change-Id: Ic88039b6ab6ab6676d4a75310342ead634b6c5c9
    Reviewed-on: https://chromium-review.googlesource.com/919256
    Reviewed-by: Nick Carter <nick@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538005}

--- content/test/data/cross_site_document_blocking/service_worker.js
+++ content/test/data/cross_site_document_blocking/service_worker.js
@@ -6,10 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // This service worker is used by the CrossSiteDocumentBlockingServiceWorkerTest
 // browser test - please see the comments there for more details.
 
-self.addEventListener('activate', function(event) {
-  event.waitUntil(self.clients.claim()); // Become available to all pages
-});
-
 function createHtmlNoSniffResponse() {
   var headers = new Headers();
   headers.append('Content-Type', 'text/html');
@@ -31,7 +27,8 @@ self.addEventListener('fetch', function(event) {
 
   // This handles response to the first request in the
   // CrossSiteDocumentBlockingServiceWorkerTest.NetworkAndOpaqueResponse test.
-  if (previousUrl != event.request.url || !previousResponse) {
+  if (event.request.url.endsWith('nosniff.html') &&
+      (previousUrl != event.request.url || !previousResponse)) {
     event.respondWith(new Promise(function(resolve, reject) {
         fetch(event.request)
             .then(function(response) {
@@ -53,4 +50,7 @@ self.addEventListener('fetch', function(event) {
     event.respondWith(previousResponse.clone());
     return;
   }
+
+  // Let the request go to the network in all the other cases (e.g. when
+  // reloading the test page at cross_site_document_blocking/request.html).
 });
