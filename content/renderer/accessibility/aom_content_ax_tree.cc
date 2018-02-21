1ed6f47b58a36b9fac22e04d7371ef9c37f70941
Meredith Lane <meredithl@google.com>
2018-02-21 13:37:33 +0000

    Cleanup method for ComputedAccessibleNode and related classes
    ComputedAccessibleNode, WebComputedAXTree and AOMContentAXTree now
    follow the pattern of method declaration of primitive return types first
    (boolean and int) followed by string return types, then ComputedAccessibleNode
    return types. This change enforces this ordering across these three
    classes to try and improve readability.
    
    BUG=792783
    
    Change-Id: Id356b71ee3c1cef286cd54669a17bbb7ddfd3310
    Reviewed-on: https://chromium-review.googlesource.com/925723
    Commit-Queue: Meredith Lane <meredithl@google.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Alice Boxhall <aboxhall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538085}

--- content/renderer/accessibility/aom_content_ax_tree.cc
+++ content/renderer/accessibility/aom_content_ax_tree.cc
@@ -15,6 +15,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
+ax::mojom::BoolAttribute GetCorrespondingAXAttribute(
+    blink::WebAOMBoolAttribute attr) {
+  switch (attr) {
+    case blink::WebAOMBoolAttribute::AOM_ATTR_ATOMIC:
+      return ax::mojom::BoolAttribute::kLiveAtomic;
+    case blink::WebAOMBoolAttribute::AOM_ATTR_BUSY:
+      return ax::mojom::BoolAttribute::kBusy;
+    case blink::WebAOMBoolAttribute::AOM_ATTR_MODAL:
+      return ax::mojom::BoolAttribute::kModal;
+    default:
+      return ax::mojom::BoolAttribute::kNone;
+  }
+}
+
 ax::mojom::IntAttribute GetCorrespondingAXAttribute(
     blink::WebAOMIntAttribute attr) {
   switch (attr) {
@@ -61,20 +75,6 @@ ax::mojom::StringAttribute GetCorrespondingAXAttribute(
   }
 }
 
-ax::mojom::BoolAttribute GetCorrespondingAXAttribute(
-    blink::WebAOMBoolAttribute attr) {
-  switch (attr) {
-    case blink::WebAOMBoolAttribute::AOM_ATTR_ATOMIC:
-      return ax::mojom::BoolAttribute::kLiveAtomic;
-    case blink::WebAOMBoolAttribute::AOM_ATTR_BUSY:
-      return ax::mojom::BoolAttribute::kBusy;
-    case blink::WebAOMBoolAttribute::AOM_ATTR_MODAL:
-      return ax::mojom::BoolAttribute::kModal;
-    default:
-      return ax::mojom::BoolAttribute::kNone;
-  }
-}
-
 }  // namespace
 
 namespace content {
@@ -101,13 +101,25 @@ bool AomContentAxTree::ComputeAccessibilityTree() {
   return tree_.Unserialize(tree_update);
 }
 
-bool AomContentAxTree::GetRoleForAXNode(int32_t ax_id,
-                                        blink::WebString* out_param) {
+bool AomContentAxTree::GetBoolAttributeForAXNode(
+    int32_t ax_id,
+    blink::WebAOMBoolAttribute attr,
+    bool* out_param) {
   ui::AXNode* node = tree_.GetFromId(ax_id);
   if (!node)
     return false;
-  *out_param = blink::WebString::FromUTF8(ui::ToString(node->data().role));
-  return true;
+  ax::mojom::BoolAttribute ax_attr = GetCorrespondingAXAttribute(attr);
+  return node->data().GetBoolAttribute(ax_attr, out_param);
+}
+
+bool AomContentAxTree::GetIntAttributeForAXNode(int32_t ax_id,
+                                                blink::WebAOMIntAttribute attr,
+                                                int32_t* out_param) {
+  ui::AXNode* node = tree_.GetFromId(ax_id);
+  if (!node)
+    return false;
+  ax::mojom::IntAttribute ax_attr = GetCorrespondingAXAttribute(attr);
+  return node->data().GetIntAttribute(ax_attr, out_param);
 }
 
 bool AomContentAxTree::GetStringAttributeForAXNode(
@@ -126,14 +138,13 @@ bool AomContentAxTree::GetStringAttributeForAXNode(
   return false;
 }
 
-bool AomContentAxTree::GetIntAttributeForAXNode(int32_t ax_id,
-                                                blink::WebAOMIntAttribute attr,
-                                                int32_t* out_param) {
+bool AomContentAxTree::GetRoleForAXNode(int32_t ax_id,
+                                        blink::WebString* out_param) {
   ui::AXNode* node = tree_.GetFromId(ax_id);
   if (!node)
     return false;
-  ax::mojom::IntAttribute ax_attr = GetCorrespondingAXAttribute(attr);
-  return node->data().GetIntAttribute(ax_attr, out_param);
+  *out_param = blink::WebString::FromUTF8(ui::ToString(node->data().role));
+  return true;
 }
 
 bool AomContentAxTree::GetParentIdForAXNode(int32_t ax_id, int32_t* out_param) {
@@ -202,17 +213,6 @@ bool AomContentAxTree::GetNextSiblingIdForAXNode(int32_t ax_id,
   DCHECK(sibling);
   *out_param = sibling->id();
   return true;
-}
-
-bool AomContentAxTree::GetBoolAttributeForAXNode(
-    int32_t ax_id,
-    blink::WebAOMBoolAttribute attr,
-    bool* out_param) {
-  ui::AXNode* node = tree_.GetFromId(ax_id);
-  if (!node)
-    return false;
-  ax::mojom::BoolAttribute ax_attr = GetCorrespondingAXAttribute(attr);
-  return node->data().GetBoolAttribute(ax_attr, out_param);
 }
 
 }  // namespace content
