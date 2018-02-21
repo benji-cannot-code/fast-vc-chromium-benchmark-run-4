46a0ec2de0b374454cd55d18ce584f7f4a1235d4
jonross <jonross@chromium.org>
2018-02-21 16:35:46 +0000

    Remove RenderFrameMetadata from SwapPromise
    With the render_frame_metadata.mojom path for observing RenderFrameMetadata we
    no longer need to have SwapPromise pass them to the browser. This change removes
    that code path.
    
    This reverts most of:
      https://chromium-review.googlesource.com/c/chromium/src/+/860994
    aside from the parts that landed RenderFrameMetadata itself.
    
    TEST=cc_unittests, content_unittests
    
    Bug: 785013
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: Iff77b631b74b2b99241f4e0eecbbbd7306d6e269
    Reviewed-on: https://chromium-review.googlesource.com/923550
    Reviewed-by: Fady Samuel <fsamuel@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538130}

--- content/renderer/gpu/queue_message_swap_promise.h
+++ content/renderer/gpu/queue_message_swap_promise.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include "base/memory/ref_counted.h"
-#include "cc/trees/render_frame_metadata.h"
 #include "cc/trees/swap_promise.h"
 
 namespace IPC {
@@ -29,8 +28,7 @@ class QueueMessageSwapPromise : public cc::SwapPromise {
   ~QueueMessageSwapPromise() override;
 
   void DidActivate() override;
-  void WillSwap(viz::CompositorFrameMetadata* compositor_frame_metadata,
-                cc::RenderFrameMetadata* render_frame_metadata) override;
+  void WillSwap(viz::CompositorFrameMetadata* metadata) override;
   void DidSwap() override;
   DidNotSwapAction DidNotSwap(DidNotSwapReason reason) override;
 
