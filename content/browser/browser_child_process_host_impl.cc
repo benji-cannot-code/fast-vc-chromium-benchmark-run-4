ac30a99b0be62c6003f04173470cd1d0746a7e4d
Lei Zhang <thestig@chromium.org>
2018-02-21 18:36:53 +0000

    Delete CanShutdown() from content::ChildProcessHostDelegate.
    It has no callers, except from content::BrowserChildProcessHostDelegate,
    which also has no callers for its CanShutdown() method.
    
    Change-Id: I9070bc84adbb2f1221f6e7822420a1e743088b84
    Reviewed-on: https://chromium-review.googlesource.com/928098
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538156}

--- content/browser/browser_child_process_host_impl.cc
+++ content/browser/browser_child_process_host_impl.cc
@@ -388,10 +388,6 @@ void BrowserChildProcessHostImpl::TerminateOnBadMessageReceived(
   child_process_->GetProcess().Terminate(RESULT_CODE_KILLED_BAD_MESSAGE, false);
 }
 
-bool BrowserChildProcessHostImpl::CanShutdown() {
-  return delegate_->CanShutdown();
-}
-
 void BrowserChildProcessHostImpl::OnChannelInitialized(IPC::Channel* channel) {
   channel_ = channel;
 }
