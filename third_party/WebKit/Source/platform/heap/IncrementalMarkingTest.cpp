0d24d8ef13ab39c02d5a261eb3b42e18e4285a0b
Keishi Hattori <keishi@chromium.org>
2018-02-22 05:22:07 +0000

    Oilpan: Fix IncrementalMarkintTest build and DCHECK failures
    Use WeakHandlingFlag to fix build. Also adjust code so we don't hit DCHECKs
    
    Bug: 757440
    Change-Id: I883a9b76b08488f4bf2d8c96a796af6ae237c794
    Reviewed-on: https://chromium-review.googlesource.com/928661
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538355}

--- third_party/WebKit/Source/platform/heap/IncrementalMarkingTest.cpp
+++ third_party/WebKit/Source/platform/heap/IncrementalMarkingTest.cpp
@@ -32,7 +32,7 @@ class IncrementalMarkingScope {
         heap_(thread_state_->Heap()),
         marking_stack_(heap_.MarkingStack()) {
     EXPECT_TRUE(marking_stack_->IsEmpty());
-    marking_stack_->Commit();
+    heap_.CommitCallbackStacks();
     heap_.EnableIncrementalMarkingBarrier();
     thread_state->current_gc_data_.visitor =
         Visitor::Create(thread_state, Visitor::kGlobalMarking);
@@ -41,7 +41,7 @@ class IncrementalMarkingScope {
   ~IncrementalMarkingScope() {
     EXPECT_TRUE(marking_stack_->IsEmpty());
     heap_.DisableIncrementalMarkingBarrier();
-    marking_stack_->Decommit();
+    heap_.DecommitCallbackStacks();
   }
 
   CallbackStack* marking_stack() const { return marking_stack_; }
@@ -858,9 +858,9 @@ class StrongWeakPair : public std::pair<Member<Object>, WeakMember<Object>> {
   // TraceInCollection will be called for weak processing.
   template <typename VisitorDispatcher>
   bool TraceInCollection(VisitorDispatcher visitor,
-                         WTF::ShouldWeakPointersBeMarkedStrongly strongify) {
+                         WTF::WeakHandlingFlag weakness) {
     visitor->Trace(first);
-    if (strongify == WTF::kWeakPointersActStrong) {
+    if (weakness == WTF::kNoWeakHandling) {
       visitor->Trace(second);
     }
     return false;
@@ -875,8 +875,7 @@ namespace WTF {
 template <>
 struct HashTraits<blink::incremental_marking_test::StrongWeakPair>
     : SimpleClassHashTraits<blink::incremental_marking_test::StrongWeakPair> {
-  static const WTF::WeakHandlingFlag kWeakHandlingFlag =
-      WTF::kWeakHandlingInCollections;
+  static const WTF::WeakHandlingFlag kWeakHandlingFlag = WTF::kWeakHandling;
 
   template <typename U = void>
   struct IsTraceableInCollection {
@@ -905,8 +904,8 @@ struct HashTraits<blink::incremental_marking_test::StrongWeakPair>
   static bool TraceInCollection(
       VisitorDispatcher visitor,
       blink::incremental_marking_test::StrongWeakPair& t,
-      WTF::ShouldWeakPointersBeMarkedStrongly strongify) {
-    return t.TraceInCollection(visitor, strongify);
+      WTF::WeakHandlingFlag weakness) {
+    return t.TraceInCollection(visitor, weakness);
   }
 };
 
