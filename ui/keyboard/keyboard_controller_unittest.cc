6d0d1d9d3f4831dae186cc3750e9d5717fd9a199
Yuichiro Hanada <yhanada@chromium.org>
2018-02-22 04:11:37 +0000

    Fix crashes in keyboard_unittests.
    - Creatig aura::Env in TestSuite::Initialize() instead of SetUp in
      each test case.
    - Using AuraTestBase instead of using AuraTestHelper directly in
      KeyboardControllerTest.
    
    Bug: 813453
    Test: Run unit tests 100 times.
    Change-Id: I3bebc9a8fc7a132c36f4fb182bc2739848e30007
    Reviewed-on: https://chromium-review.googlesource.com/928446
    Reviewed-by: Blake O'Hare <blakeo@chromium.org>
    Commit-Queue: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538345}

--- ui/keyboard/keyboard_controller_unittest.cc
+++ ui/keyboard/keyboard_controller_unittest.cc
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/aura/client/focus_client.h"
-#include "ui/aura/test/aura_test_helper.h"
+#include "ui/aura/test/aura_test_base.h"
 #include "ui/aura/test/test_window_delegate.h"
 #include "ui/aura/window.h"
 #include "ui/base/ime/dummy_text_input_client.h"
@@ -205,13 +205,11 @@ class SetModeCallbackInvocationCounter {
 
 }  // namespace
 
-class KeyboardControllerTest : public testing::Test,
+class KeyboardControllerTest : public aura::test::AuraTestBase,
                                public KeyboardControllerObserver {
  public:
   KeyboardControllerTest()
-      : scoped_task_environment_(
-            base::test::ScopedTaskEnvironment::MainThreadType::UI),
-        visible_bounds_number_of_calls_(0),
+      : visible_bounds_number_of_calls_(0),
         occluding_bounds_number_of_calls_(0),
         is_available_number_of_calls_(0),
         is_available_(false),
@@ -219,24 +217,14 @@ class KeyboardControllerTest : public testing::Test,
   ~KeyboardControllerTest() override {}
 
   void SetUp() override {
-    // The ContextFactory must exist before any Compositors are created.
-    bool enable_pixel_output = false;
-    ui::ContextFactory* context_factory = nullptr;
-    ui::ContextFactoryPrivate* context_factory_private = nullptr;
-
-    ui::InitializeContextFactoryForTests(enable_pixel_output, &context_factory,
-                                         &context_factory_private);
-
     ui::SetUpInputMethodFactoryForTesting();
-    aura_test_helper_.reset(new aura::test::AuraTestHelper());
-    aura_test_helper_->SetUp(context_factory, context_factory_private);
-    new wm::DefaultActivationClient(aura_test_helper_->root_window());
+    aura::test::AuraTestBase::SetUp();
+    new wm::DefaultActivationClient(root_window());
     focus_controller_.reset(new TestFocusController(root_window()));
     layout_delegate_.reset(new TestKeyboardLayoutDelegate());
-    controller_.reset(
-        new KeyboardController(std::make_unique<TestKeyboardUI>(
-                                   aura_test_helper_->host()->GetInputMethod()),
-                               layout_delegate_.get()));
+    controller_.reset(new KeyboardController(
+        std::make_unique<TestKeyboardUI>(host()->GetInputMethod()),
+        layout_delegate_.get()));
     controller()->AddObserver(this);
   }
 
@@ -245,11 +233,9 @@ class KeyboardControllerTest : public testing::Test,
       controller()->RemoveObserver(this);
     controller_.reset();
     focus_controller_.reset();
-    aura_test_helper_->TearDown();
-    ui::TerminateContextFactoryForTests();
+    aura::test::AuraTestBase::TearDown();
   }
 
-  aura::Window* root_window() { return aura_test_helper_->root_window(); }
   KeyboardUI* ui() { return controller_->ui(); }
   KeyboardController* controller() { return controller_.get(); }
 
@@ -335,8 +321,6 @@ class KeyboardControllerTest : public testing::Test,
     run_loop->Run();
   }
 
-  base::test::ScopedTaskEnvironment scoped_task_environment_;
-  std::unique_ptr<aura::test::AuraTestHelper> aura_test_helper_;
   std::unique_ptr<TestFocusController> focus_controller_;
 
  private:
--- ui/keyboard/test/run_all_unittests.cc
+++ ui/keyboard/test/run_all_unittests.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/test_suite.h"
 #include "mojo/edk/embedder/embedder.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "ui/aura/env.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/ui_base_paths.h"
 #include "ui/gl/test/gl_surface_test_support.h"
@@ -25,6 +26,7 @@ class KeyboardTestSuite : public base::TestSuite {
   void Initialize() override {
     base::TestSuite::Initialize();
     gl::GLSurfaceTestSupport::InitializeOneOff();
+    env_ = aura::Env::CreateInstance();
     ui::RegisterPathProvider();
 
     base::FilePath ui_test_pak_path;
@@ -33,11 +35,14 @@ class KeyboardTestSuite : public base::TestSuite {
   }
 
   void Shutdown() override {
+    env_.reset();
     ui::ResourceBundle::CleanupSharedInstance();
     base::TestSuite::Shutdown();
   }
 
  private:
+  std::unique_ptr<aura::Env> env_;
+
   DISALLOW_COPY_AND_ASSIGN(KeyboardTestSuite);
 };
 
