607817fc89af0c1b41754d43cdf10c57dbeba611
Avi Drissman <avi@chromium.org>
2018-02-22 20:41:01 +0000

    Give the JS dialog manager the alerting frame.
    Back when this interface was originally designed, frames did not
    have a proper type. Now that they do, plumb it through. That
    allows the manager to make more intelligent decisions about
    presenting the dialogs.
    
    BUG=696454, 802007
    
    Change-Id: I8aef92770bd80cfb00a59761ac492394b78d1953
    Reviewed-on: https://chromium-review.googlesource.com/928828
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538552}

--- android_webview/browser/aw_javascript_dialog_manager.cc
+++ android_webview/browser/aw_javascript_dialog_manager.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "android_webview/browser/aw_contents_client_bridge.h"
 #include "content/public/browser/javascript_dialog_manager.h"
+#include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents.h"
 
 namespace android_webview {
@@ -19,7 +20,7 @@ AwJavaScriptDialogManager::~AwJavaScriptDialogManager() {}
 
 void AwJavaScriptDialogManager::RunJavaScriptDialog(
     content::WebContents* web_contents,
-    const GURL& origin_url,
+    content::RenderFrameHost* render_frame_host,
     content::JavaScriptDialogType dialog_type,
     const base::string16& message_text,
     const base::string16& default_prompt_text,
@@ -32,8 +33,9 @@ void AwJavaScriptDialogManager::RunJavaScriptDialog(
     return;
   }
 
-  bridge->RunJavaScriptDialog(dialog_type, origin_url, message_text,
-                              default_prompt_text, std::move(callback));
+  bridge->RunJavaScriptDialog(
+      dialog_type, render_frame_host->GetLastCommittedURL(), message_text,
+      default_prompt_text, std::move(callback));
 }
 
 void AwJavaScriptDialogManager::RunBeforeUnloadDialog(
