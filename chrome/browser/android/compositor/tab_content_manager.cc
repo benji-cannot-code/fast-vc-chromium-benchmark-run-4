6a151aad8107ee19a64e78215673cd9914a7858e
Yuri Wiitala <miu@chromium.org>
2018-02-22 23:21:38 +0000

    Migrate RWHVAndroid to use new CopyOutputRequest APIs.
    This change brings RenderWidgetHostViewAndroid::CopyFromSurface() and
    DelegatedFrameHostAndroid in-sync with the desktop platform versions of
    the same, and also fixes a bug where the output_size argument was being
    device-scaled. It switches over to using the new CopyOutputRequest
    scaling APIs, and deletes all the special client-side use of GLHelper
    (which was being used to perform scaling post-copy).
    
    Also, two important fixes were made:
    
    1. A weirdness in the existing switcher UI where there was a width-
    stretching problem, as mentioned in the description of a prior change:
    https://codereview.chromium.org/2734043003 (commit
    caa6a6165d28c3bb499fc0aec9bf391c6bda1578). Now the tab transition UI
    thumbnails are *perfectly* aligned with the actual content rendering.
    
    2. Devtools remote screen cast impl: The calculations in PageHandler
    were rather confusing, and this was masking a bug where the wrong scale
    factor was being applied to the remote's requested image size. This
    change eliminates most of the calculations, since all that matters is
    that whatever the Android device has locally is being scaled-to-fit
    within the remote's requested width+height.
    
    Manual Testing (on a Pixel XL; device scale factor of 3.5):
    
    1. Android tab switcher UI.
    2. DevTools remote screen cast inspector tool (resized window during
    session to make sure it works for several remote image sizes).
    
    Bug: 759310, 812059, 582955
    Change-Id: If0726d4947edfc1efa58fe926123b443b6649e55
    Reviewed-on: https://chromium-review.googlesource.com/924388
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Yuri Wiitala <miu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538610}

--- chrome/browser/android/compositor/tab_content_manager.cc
+++ chrome/browser/android/compositor/tab_content_manager.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "jni/TabContentManager_jni.h"
 #include "ui/android/resources/ui_resource_provider.h"
+#include "ui/android/view_android.h"
 #include "ui/gfx/android/java_bitmap.h"
 #include "ui/gfx/geometry/dip_util.h"
 #include "ui/gfx/geometry/rect.h"
@@ -62,9 +63,14 @@ class TabContentManager::TabReadbackRequest {
                    weak_factory_.GetWeakPtr());
 
     SkColorType color_type = kN32_SkColorType;
-    gfx::Size view_size_on_screen = rwhv->GetViewBounds().size();
+    gfx::Size view_size_in_pixels =
+        rwhv->GetNativeView()->GetPhysicalBackingSize();
+    if (view_size_in_pixels.IsEmpty()) {
+      result_callback.Run(SkBitmap(), content::READBACK_SURFACE_UNAVAILABLE);
+      return;
+    }
     gfx::Size thumbnail_size(
-        gfx::ScaleToCeiledSize(view_size_on_screen, thumbnail_scale_));
+        gfx::ScaleToCeiledSize(view_size_in_pixels, thumbnail_scale_));
     rwhv->CopyFromSurface(gfx::Rect(), thumbnail_size, result_callback,
                           color_type);
   }
