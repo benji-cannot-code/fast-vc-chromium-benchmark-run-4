41351205f5ad27d24b854c32665d3c8f91502752
Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
2018-02-22 03:47:06 +0000

    Revert "Zip Archiver: Disable storing mount and password info."
    This reverts commit c3169366954d82dc6b35ad6a869907cde0d714cd.
    
    Reason for revert: This caused regression Issue 812123.
    This had caused that mount resuming succeeds superficially by
    the caller despite it actually failed, thus leaving inaccessible
    zip mount point.
    
    Original change's description:
    > Zip Archiver: Disable storing mount and password info.
    >
    > We are not restoring mount files at session restart anymore.
    > Therefore the mount information and passwords should not be persisted.
    > Othrewise it will be persisted until the user opens the same archive
    > again manually after a long time and the files will be opened without
    > requiring passwords.
    >
    > Bug: 789073,803752
    > Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    > Change-Id: Idadcff694b20109eb7e31710b57d88400601d30a
    > Reviewed-on: https://chromium-review.googlesource.com/876705
    > Commit-Queue: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    > Reviewed-by: Yuki Awano <yawano@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#531467}
    
    TBR=yawano@chromium.org,yamaguchi@chromium.org
    
    Bug: 789073, 803752
    Change-Id: I387e16717b5187389d01c5d5bc4a8b6884dcf035
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Reviewed-on: https://chromium-review.googlesource.com/928063
    Reviewed-by: Naoki Fukino <fukino@chromium.org>
    Commit-Queue: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538338}

--- chrome/browser/resources/chromeos/zip_archiver/js/background.js
+++ chrome/browser/resources/chromeos/zip_archiver/js/background.js
@@ -9,6 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // declared in the manifest file.
 chrome.app.runtime.onLaunched.addListener(unpacker.app.onLaunched);
 
+// Save the state before suspending the event page, so we can resume it
+// once new events arrive.
+chrome.runtime.onSuspend.addListener(unpacker.app.onSuspend);
+
 chrome.fileSystemProvider.onUnmountRequested.addListener(
     unpacker.app.onUnmountRequested);
 chrome.fileSystemProvider.onGetMetadataRequested.addListener(
