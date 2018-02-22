f74fde656e7580fee5042b8d090727b792a2741d
Hongchan Choi <hongchan@chromium.org>
2018-02-22 01:18:20 +0000

    Add CHECK in BaseAudioContext::PerformCleanupOnMainThread
    This is to assert the size of HeapVector. We speculate the requested
    capacity can be negative.
    
    Bug: 814108
    Change-Id: Ic3c5f1ad896f4a80dee52a7b27882eacb51a45b5
    Reviewed-on: https://chromium-review.googlesource.com/929782
    Reviewed-by: Raymond Toy <rtoy@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Hongchan Choi <hongchan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538294}

--- third_party/WebKit/Source/modules/webaudio/BaseAudioContext.cpp
+++ third_party/WebKit/Source/modules/webaudio/BaseAudioContext.cpp
@@ -846,6 +846,7 @@ void BaseAudioContext::PerformCleanupOnMainThread() {
 
     // Copy over the surviving active nodes.
     HeapVector<Member<AudioNode>> actives;
+    CHECK_GE(active_source_nodes_.size(), remove_count);
     actives.ReserveInitialCapacity(active_source_nodes_.size() - remove_count);
     for (unsigned i = 0; i < removables.size(); ++i) {
       if (!removables[i])
