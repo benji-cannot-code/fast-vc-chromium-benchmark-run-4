ed6eb4a8fd404bc6c90eeb8181a468d6238d1d28
Kurt Horimoto <kkhorimoto@chromium.org>
2018-02-22 07:19:56 +0000

    [iOS] Show toolbar when a new foreground tab is opened.
    This CL creates NewForegroundTabFullscreenDisabler, which creates an
    AnimatedScopedFullscreenDisabler when a new foreground tab is added to
    the WebStateList.
    
    To support this, this CL also updates AnimatedScopedFullscreenDisabler
    to have an observer API that can be notified when the animation
    finishes.
    
    Additionally. this CL updates BVC to be updated for fullscreen events
    even when not broadcasting UI.
    
    Bug: 809120
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I4d4d4adc0de009f759b5136b32d267c7c68cac3d
    Reviewed-on: https://chromium-review.googlesource.com/920792
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538372}

--- ios/chrome/browser/ui/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view_controller.mm
@@ -167,6 +167,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/main_content/main_content_ui_broadcasting_util.h"
 #import "ios/chrome/browser/ui/main_content/main_content_ui_state.h"
 #import "ios/chrome/browser/ui/main_content/web_scroll_view_main_content_ui_forwarder.h"
+#import "ios/chrome/browser/ui/new_foreground_tab_fullscreen_disabler.h"
 #import "ios/chrome/browser/ui/ntp/new_tab_page_controller.h"
 #import "ios/chrome/browser/ui/ntp/recent_tabs/recent_tabs_handset_coordinator.h"
 #import "ios/chrome/browser/ui/overscroll_actions/overscroll_actions_controller.h"
@@ -598,6 +599,10 @@ NSString* const kBrowserViewControllerSnackbarCategory =
   // The updater that adjusts the toolbar's layout for fullscreen events.
   std::unique_ptr<FullscreenUIUpdater> _fullscreenUIUpdater;
 
+  // The fullscreen disabler for the new foreground tab animation.
+  std::unique_ptr<NewForegroundTabFullscreenDisabler>
+      _foregroundTabAnimationFullscreenDisabler;
+
   // Coordinator for the External Search UI.
   ExternalSearchCoordinator* _externalSearchCoordinator;
 
@@ -1022,6 +1027,9 @@ applicationCommandEndpoint:(id<ApplicationCommands>)applicationCommandEndpoint {
     [[UpgradeCenter sharedInstance] registerClient:self
                                     withDispatcher:self.dispatcher];
     _inNewTabAnimation = NO;
+
+    _footerFullscreenProgress = 1.0;
+
     if (model && browserState)
       [self updateWithTabModel:model browserState:browserState];
   }
@@ -1227,10 +1235,7 @@ applicationCommandEndpoint:(id<ApplicationCommands>)applicationCommandEndpoint {
            webStateList:[_model webStateList]];
     StartBroadcastingMainContentUI(self, broadcaster);
 
-    _fullscreenUIUpdater = std::make_unique<FullscreenUIUpdater>(self);
     fullscreenController->AddObserver(_fullscreenUIUpdater.get());
-
-    fullscreenController->SetWebStateList([_model webStateList]);
   } else {
     StopBroadcastingToolbarUI(broadcaster);
     StopBroadcastingMainContentUI(broadcaster);
@@ -1239,9 +1244,8 @@ applicationCommandEndpoint:(id<ApplicationCommands>)applicationCommandEndpoint {
     _mainContentUIUpdater = nil;
     [_webMainContentUIForwarder disconnect];
     _webMainContentUIForwarder = nil;
+
     fullscreenController->RemoveObserver(_fullscreenUIUpdater.get());
-    _fullscreenUIUpdater = nullptr;
-    fullscreenController->SetWebStateList(nullptr);
   }
 }
 
@@ -1365,6 +1369,15 @@ applicationCommandEndpoint:(id<ApplicationCommands>)applicationCommandEndpoint {
   [_paymentRequestManager close];
   _paymentRequestManager = nil;
   [_model browserStateDestroyed];
+
+  FullscreenController* fullscreenController =
+      FullscreenControllerFactory::GetInstance()->GetForBrowserState(
+          _browserState);
+  _foregroundTabAnimationFullscreenDisabler->Disconnect();
+  _foregroundTabAnimationFullscreenDisabler = nullptr;
+  fullscreenController->RemoveObserver(_fullscreenUIUpdater.get());
+  _fullscreenUIUpdater = nullptr;
+  fullscreenController->SetWebStateList(nullptr);
 
   // Disconnect child coordinators.
   [_activityServiceCoordinator disconnect];
@@ -1929,6 +1942,18 @@ applicationCommandEndpoint:(id<ApplicationCommands>)applicationCommandEndpoint {
   // Register for bookmark changed notification (BookmarkModel may be null
   // during testing, so explicitly support this).
   _bookmarkModel = ios::BookmarkModelFactory::GetForBrowserState(_browserState);
+
+  // Add a FullscreenUIUpdater for self.
+  FullscreenController* controller =
+      FullscreenControllerFactory::GetInstance()->GetForBrowserState(
+          _browserState);
+  _fullscreenUIUpdater = std::make_unique<FullscreenUIUpdater>(self);
+  // Crate the disabler for any new foreground tab animations in the tab model.
+  _foregroundTabAnimationFullscreenDisabler =
+      std::make_unique<NewForegroundTabFullscreenDisabler>(_model.webStateList,
+                                                           controller);
+  // Set the FullscreenController's WebStateList.
+  controller->SetWebStateList(_model.webStateList);
 }
 
 - (void)installFakeStatusBar {
--- ios/chrome/browser/ui/browser_view_controller_unittest.mm
+++ ios/chrome/browser/ui/browser_view_controller_unittest.mm
@@ -288,6 +288,7 @@ class BrowserViewControllerTest : public BlockCleanupTest {
 
   void TearDown() override {
     [[bvc_ view] removeFromSuperview];
+    [bvc_ browserStateDestroyed];
     [bvc_ shutdown];
 
     BlockCleanupTest::TearDown();
