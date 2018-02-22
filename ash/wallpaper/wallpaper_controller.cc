fbbc08bc47418969b34ee73d1bc6551fbb300c64
Wenzhao Zang <wzang@chromium.org>
2018-02-22 23:53:13 +0000

    cros: Clean up duplicate GetDevicePolicyWallpaperFilePath methods
    A trivial CL that removes some legacy code during the refactoring.
    
    TBR=bshe@chromium.org
    
    Bug: NONE
    Change-Id: I43f427ee0db4b46f6093c54dfa6492545a0a0549
    Reviewed-on: https://chromium-review.googlesource.com/932649
    Reviewed-by: Wenzhao (Colin) Zang <wzang@chromium.org>
    Commit-Queue: Wenzhao (Colin) Zang <wzang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538622}

--- ash/wallpaper/wallpaper_controller.cc
+++ ash/wallpaper/wallpaper_controller.cc
@@ -425,17 +425,6 @@ base::FilePath WallpaperController::GetCustomWallpaperDir(
 }
 
 // static
-base::FilePath WallpaperController::GetDeviceWallpaperDir() {
-  DCHECK(!dir_chrome_os_wallpapers_path_.empty());
-  return dir_chrome_os_wallpapers_path_.Append(kDeviceWallpaperDir);
-}
-
-// static
-base::FilePath WallpaperController::GetDeviceWallpaperFilePath() {
-  return GetDeviceWallpaperDir().Append(kDeviceWallpaperFile);
-}
-
-// static
 bool WallpaperController::ResizeImage(
     const gfx::ImageSkia& image,
     wallpaper::WallpaperLayout layout,
@@ -509,6 +498,13 @@ bool WallpaperController::ResizeAndSaveWallpaper(
         path, reinterpret_cast<const char*>(data->front()), data->size());
   }
   return false;
+}
+
+// static
+base::FilePath WallpaperController::GetDevicePolicyWallpaperFilePath() {
+  DCHECK(!dir_chrome_os_wallpapers_path_.empty());
+  return dir_chrome_os_wallpapers_path_.Append(kDeviceWallpaperDir)
+      .Append(kDeviceWallpaperFile);
 }
 
 // static
@@ -587,13 +583,6 @@ bool WallpaperController::WriteJPEGFileForTesting(const base::FilePath& path,
     return false;
   }
   return true;
-}
-
-// static
-base::FilePath WallpaperController::GetDevicePolicyWallpaperFilePath() {
-  DCHECK(!dir_chrome_os_wallpapers_path_.empty());
-  return dir_chrome_os_wallpapers_path_.Append(kDeviceWallpaperDir)
-      .Append(kDeviceWallpaperFile);
 }
 
 void WallpaperController::BindRequest(
@@ -1307,7 +1296,7 @@ void WallpaperController::ShowUserWallpaper(
 
   base::FilePath wallpaper_path;
   if (info.type == wallpaper::DEVICE) {
-    wallpaper_path = GetDeviceWallpaperFilePath();
+    wallpaper_path = GetDevicePolicyWallpaperFilePath();
   } else {
     std::string sub_dir = GetCustomWallpaperSubdirForCurrentResolution();
     // Wallpaper is not resized when layout is
