ab2e4279f320e7ecbcf7f60a8e051c1bd8afa02c
Daniel Bratell <bratell@opera.com>
2018-02-22 18:55:13 +0000

    Add include guard to WebFontTypefaceFactory.h
    Newly added header missing include guard. There is a presubmit
    check coming to catch these since they break some builds.
    
    TBR=drott@chromium.org
    
    Change-Id: Ie705b6b3404bcb67d829bcc1a7ba449daf1cb277
    Reviewed-on: https://chromium-review.googlesource.com/931881
    Reviewed-by: Daniel Bratell <bratell@opera.com>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Commit-Queue: Daniel Bratell <bratell@opera.com>
    Cr-Commit-Position: refs/heads/master@{#538503}

--- third_party/WebKit/Source/platform/fonts/WebFontTypefaceFactory.h
+++ third_party/WebKit/Source/platform/fonts/WebFontTypefaceFactory.h
@@ -3,6 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifndef WebFontTypefaceFactory_h
+#define WebFontTypefaceFactory_h
+
 #include "third_party/skia/include/ports/SkFontMgr.h"
 
 #include "build/build_config.h"
@@ -45,3 +48,5 @@ class WebFontTypefaceFactory {
 };
 
 }  // namespace blink
+
+#endif  // WebFontTypefaceFactory_h
