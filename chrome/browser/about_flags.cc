08bb564a93c968ac54e631bae52df61ad72f1260
Nicholas Verne <nverne@chromium.org>
2018-02-22 01:22:04 +0000

    First checkin of Crostini app model builder and related classes.
    This is placeholder only and the button doesn't yet connect to any action.
    
    Bug: 813699
    Change-Id: Idc84916be75ee25ac1f570a5917af8363f6f5655
    Reviewed-on: https://chromium-review.googlesource.com/925921
    Commit-Queue: Nicholas Verne <nverne@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538296}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -3738,6 +3738,10 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kAshEnableNewOverviewAnimationsName,
      flag_descriptions::kAshEnableNewOverviewAnimationsDescription, kOsCrOS,
      SINGLE_VALUE_TYPE(ash::switches::kAshEnableNewOverviewAnimations)},
+    {"enable-experimental-crostini-ui",
+     flag_descriptions::kExperimentalCrostiniUIName,
+     flag_descriptions::kExperimentalCrostiniUIDescription, kOsCrOS,
+     FEATURE_VALUE_TYPE(features::kExperimentalCrostiniUI)},
 #endif  // OS_CHROMEOS
 
 #if defined(OS_ANDROID)
---
+++ chrome/browser/ui/app_list/crostini/crostini_app_model_builder.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+#ifndef CHROME_BROWSER_UI_APP_LIST_CROSTINI_CROSTINI_APP_MODEL_BUILDER_H_
+#define CHROME_BROWSER_UI_APP_LIST_CROSTINI_CROSTINI_APP_MODEL_BUILDER_H_
+
+#include "base/macros.h"
+#include "chrome/browser/ui/app_list/app_list_model_builder.h"
+
+class AppListControllerDelegate;
+
+// This class populates and maintains Crostini apps.
+class CrostiniAppModelBuilder : public AppListModelBuilder {
+ public:
+  explicit CrostiniAppModelBuilder(AppListControllerDelegate* controller);
+  ~CrostiniAppModelBuilder() override;
+
+ private:
+  // AppListModelBuilder:
+  void BuildModel() override;
+
+  DISALLOW_COPY_AND_ASSIGN(CrostiniAppModelBuilder);
+};
+
+#endif  // CHROME_BROWSER_UI_APP_LIST_CROSTINI_CROSTINI_APP_MODEL_BUILDER_H_
