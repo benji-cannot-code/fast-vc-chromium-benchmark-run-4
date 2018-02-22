39091cafb7c5e05e07e3d0b4bd214a0819ba9323
Darren Shen <shend@chromium.org>
2018-02-22 02:20:14 +0000

    Revert "Revert "[css-typed-om] Ensure StylePropertyMapReadonly IDL matches spec.""
    This reverts commit 35bfb10373c5b34c193ad9a7c569d6aa429028fc.
    
    Reason for revert: Trying to reland.
    
    Original change's description:
    > Revert "[css-typed-om] Ensure StylePropertyMapReadonly IDL matches spec."
    >
    > This reverts commit 43700726a48ebedf10af437c644ec46d862fb46b.
    >
    > Reason for revert: Causes build failure on Google Chrome ChromeOS:
    > https://ci.chromium.org/buildbot/chromium.chrome/Google%20Chrome%20ChromeOS/45929
    >
    > Original change's description:
    > > [css-typed-om] Ensure StylePropertyMapReadonly IDL matches spec.
    > >
    > > Spec added a new .size member, which we implement in this patch.
    > >
    > > Bug: 812915
    > > Change-Id: Ib48fc7b8053c41e4c79dc9cc280bc18376e5fbe0
    > > Reviewed-on: https://chromium-review.googlesource.com/930101
    > > Reviewed-by: nainar <nainar@chromium.org>
    > > Commit-Queue: Darren Shen <shend@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#538304}
    >
    > TBR=nainar@chromium.org,shend@chromium.org
    >
    > Change-Id: I442aebc331ddc97c09c870299ab0ab76f5ce7773
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 812915
    > Reviewed-on: https://chromium-review.googlesource.com/930179
    > Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    > Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#538309}
    
    TBR=nainar@chromium.org
    
    Bug: 812915
    Change-Id: Ia45bc81d58a807448e00d21a33c97e2210712bb2
    Reviewed-on: https://chromium-review.googlesource.com/930521
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538321}

--- third_party/WebKit/Source/core/css/cssom/DeclaredStylePropertyMap.h
+++ third_party/WebKit/Source/core/css/cssom/DeclaredStylePropertyMap.h
@@ -32,6 +32,8 @@ class CORE_EXPORT DeclaredStylePropertyMap final : public StylePropertyMap {
     StylePropertyMap::Trace(visitor);
   }
 
+  int size() final;
+
  protected:
   const CSSValue* GetProperty(CSSPropertyID) override;
   const CSSValue* GetCustomProperty(AtomicString) override;
