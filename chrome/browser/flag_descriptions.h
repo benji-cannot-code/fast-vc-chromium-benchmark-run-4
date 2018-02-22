a66ae1690e4fc1c0d73bde2137475b22ce7b36d6
Vladimir Levin <vmpstr@chromium.org>
2018-02-22 19:17:42 +0000

    [Cleaup] Remove unused SPInvalidation vars and obsolete a related UMA.
    This patch removes SlimmingPaintInvalidation flags descriptions, since
    the flag is not available anymore.
    
    Also, it obsoletes an Blink.PaintInvalidation.UpdateTime UMA which is
    no longer being logged.
    
    R=chrishtr@chromium.org
    
    Change-Id: I5eb0209eb4142253e505a71c011b7e0509c91c83
    Reviewed-on: https://chromium-review.googlesource.com/924751
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538517}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -867,9 +867,6 @@ extern const char kSitePerProcessDescription[];
 extern const char kSiteSettings[];
 extern const char kSiteSettingsDescription[];
 
-extern const char kSlimmingPaintInvalidationName[];
-extern const char kSlimmingPaintInvalidationDescription[];
-
 extern const char kSmoothScrollingName[];
 extern const char kSmoothScrollingDescription[];
 
