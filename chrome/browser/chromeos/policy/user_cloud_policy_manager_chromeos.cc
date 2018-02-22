b91d94785790049f1f9c56b66dd010e14150a98b
Bartosz Fabianowski <bartfab@chromium.org>
2018-02-22 11:43:09 +0000

    Add app push-install log uploader
    This is the glue code between the log store and the CloudPolicyClient
    that sends logs to the server. It retrieves logs from the store
    asynchronously, forwards them to the CloudPolicyClient, schedules
    retries with exponential backoff and manages the lifetime difference
    between log store and CloudPolicyClient.
    
    Bug: b/73277923
    Test: components_unittests and browser_tests
    Change-Id: If9e87ae056ecb77d589b4dbd049ddf6b2a632239
    Reviewed-on: https://chromium-review.googlesource.com/922661
    Commit-Queue: Bartosz Fabianowski <bartfab@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538402}

--- chrome/browser/chromeos/policy/user_cloud_policy_manager_chromeos.cc
+++ chrome/browser/chromeos/policy/user_cloud_policy_manager_chromeos.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/session/user_session_manager.h"
 #include "chrome/browser/chromeos/login/users/affiliation.h"
 #include "chrome/browser/chromeos/login/users/chrome_user_manager_impl.h"
+#include "chrome/browser/chromeos/policy/app_install_event_log_uploader.h"
 #include "chrome/browser/chromeos/policy/policy_oauth2_token_fetcher.h"
 #include "chrome/browser/chromeos/policy/user_policy_manager_factory_chromeos.h"
 #include "chrome/browser/chromeos/policy/wildcard_login_checker.h"
@@ -193,6 +194,9 @@ void UserCloudPolicyManagerChromeOS::Connect(
     // Wait for the CloudPolicyStore to finish initializing.
     service()->AddObserver(this);
   }
+
+  app_install_event_log_uploader_ =
+      std::make_unique<AppInstallEventLogUploader>(client());
 }
 
 void UserCloudPolicyManagerChromeOS::OnAccessTokenAvailable(
@@ -246,7 +250,13 @@ void UserCloudPolicyManagerChromeOS::EnableWildcardLoginCheck(
   wildcard_username_ = username;
 }
 
+AppInstallEventLogUploader*
+UserCloudPolicyManagerChromeOS::GetAppInstallEventLogUploader() {
+  return app_install_event_log_uploader_.get();
+}
+
 void UserCloudPolicyManagerChromeOS::Shutdown() {
+  app_install_event_log_uploader_.reset();
   if (client())
     client()->RemoveObserver(this);
   if (service())
