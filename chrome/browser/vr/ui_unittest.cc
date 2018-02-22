460661c6ced975557280c3be72a5d19a08a6b53b
Ian Vollick <vollick@chromium.org>
2018-02-22 22:29:50 +0000

    [vr] Content repositioning tweaks
    With this change, we reset the position of the window after recentering
    the scene. We also adjust the roll of the window while repositioning
    rather than snapping afterward.
    
    Bug: 800558
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_vr;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I7e939f28e826ae3e58a0afad4149953df84e2e36
    Reviewed-on: https://chromium-review.googlesource.com/932164
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Reviewed-by: Christopher Grant <cjgrant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538592}

--- chrome/browser/vr/ui_unittest.cc
+++ chrome/browser/vr/ui_unittest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/vr/elements/disc_button.h"
 #include "chrome/browser/vr/elements/exit_prompt.h"
 #include "chrome/browser/vr/elements/rect.h"
+#include "chrome/browser/vr/elements/repositioner.h"
 #include "chrome/browser/vr/elements/ui_element.h"
 #include "chrome/browser/vr/elements/ui_element_name.h"
 #include "chrome/browser/vr/elements/vector_icon.h"
@@ -1174,6 +1175,21 @@ TEST_F(UiTest, RepositionButton) {
   button->OnHoverLeave();
   OnBeginFrame();
   EXPECT_EQ(kRepositionButtonMinOpacity, button->GetTargetOpacity());
+}
+
+TEST_F(UiTest, ResetRepositioner) {
+  CreateScene(kNotInCct, kNotInWebVr);
+  Repositioner* repositioner = static_cast<Repositioner*>(
+      scene_->GetUiElementByName(k2dBrowsingRepositioner));
+  repositioner->set_laser_direction(kForwardVector);
+  repositioner->SetEnabled(true);
+  repositioner->set_laser_direction({0, 1, 0});
+  OnBeginFrame();
+  EXPECT_FALSE(repositioner->world_space_transform().IsIdentity());
+  repositioner->SetEnabled(false);
+  model_->controller.recentered = true;
+  OnBeginFrame();
+  EXPECT_TRUE(repositioner->world_space_transform().IsIdentity());
 }
 
 // No element in the controller root's subtree should be hit testable.
