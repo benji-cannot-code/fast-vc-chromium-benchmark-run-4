238c254e1ddfedc563eda353c5fecb8751c972e8
Elly Fong-Jones <ellyjones@chromium.org>
2018-02-22 14:36:53 +0000

    extensions: disable shell operations in DownloadsShowDefaultFolder test
    On Mac, this test opens a Finder window which remains open after the test
    ends; suppress actually opening the window. Failing to do this causes
    Finder windows to accumulate over time on Mac test bots.
    
    Bug: 814356
    Change-Id: I276b97ede5061faa354785fd7f96f308135873a4
    Reviewed-on: https://chromium-review.googlesource.com/929701
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538430}

--- chrome/browser/extensions/api/downloads/downloads_api_browsertest.cc
+++ chrome/browser/extensions/api/downloads/downloads_api_browsertest.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/browser/extensions/extension_function_test_utils.h"
 #include "chrome/browser/net/url_request_mock_util.h"
+#include "chrome/browser/platform_util_internal.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_tabstrip.h"
@@ -1123,6 +1124,7 @@ IN_PROC_BROWSER_TEST_F(DownloadExtensionTest,
 
 IN_PROC_BROWSER_TEST_F(DownloadExtensionTest,
                        DownloadsShowDefaultFolderFunction) {
+  platform_util::internal::DisableShellOperationsForTesting();
   ScopedCancellingItem item(CreateFirstSlowTestDownload());
   ASSERT_TRUE(item.get());
 
