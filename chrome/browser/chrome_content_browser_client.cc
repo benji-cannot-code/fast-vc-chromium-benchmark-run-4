e6eaf29988f3e1245b3b25b4b0c8a13e68727b92
Chris Mumford <cmumford@chromium.org>
2018-02-22 00:27:57 +0000

    [Reland] Load resources from extension bundles in the network service.
    This is a relanding of c67e063843e4bd37522692d9b74586a2304575e5
    reviewed at https://chromium-review.googlesource.com/834749 which
    broke the Linux CFI builds.
    
    Bug: 782025
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I24e9e3e6dc3312af2a94c4f30232da0f798ab541
    Reviewed-on: https://chromium-review.googlesource.com/923565
    Commit-Queue: Chris Mumford <cmumford@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538274}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -377,6 +377,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_navigation_throttle.h"
 #include "extensions/browser/extension_protocols.h"
 #include "extensions/browser/extension_registry.h"
+#include "extensions/browser/extension_system.h"
 #include "extensions/browser/extension_util.h"
 #include "extensions/browser/guest_view/web_view/web_view_guest.h"
 #include "extensions/browser/guest_view/web_view/web_view_permission_helper.h"
@@ -3756,9 +3757,13 @@ void ChromeContentBrowserClient::RegisterNonNetworkNavigationURLLoaderFactories(
     content::RenderFrameHost* frame_host,
     NonNetworkURLLoaderFactoryMap* factories) {
 #if BUILDFLAG(ENABLE_EXTENSIONS)
+  content::BrowserContext* browser_context =
+      frame_host->GetProcess()->GetBrowserContext();
   factories->emplace(
       extensions::kExtensionScheme,
-      extensions::CreateExtensionNavigationURLLoaderFactory(frame_host));
+      extensions::CreateExtensionNavigationURLLoaderFactory(
+          frame_host,
+          extensions::ExtensionSystem::Get(browser_context)->info_map()));
 #endif
 }
 
@@ -3768,8 +3773,11 @@ void ChromeContentBrowserClient::
         const GURL& frame_url,
         NonNetworkURLLoaderFactoryMap* factories) {
 #if BUILDFLAG(ENABLE_EXTENSIONS)
+  content::BrowserContext* browser_context =
+      frame_host->GetProcess()->GetBrowserContext();
   auto factory = extensions::MaybeCreateExtensionSubresourceURLLoaderFactory(
-      frame_host, frame_url);
+      frame_host, frame_url,
+      extensions::ExtensionSystem::Get(browser_context)->info_map());
   if (factory)
     factories->emplace(extensions::kExtensionScheme, std::move(factory));
 #endif
