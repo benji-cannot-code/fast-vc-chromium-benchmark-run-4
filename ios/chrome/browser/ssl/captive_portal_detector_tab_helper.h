25b9fa80212e7f815fefc63f8ed544d2707c9ffc
John Abd-El-Malek <jam@chromium.org>
2018-02-22 20:06:55 +0000

    Fix check in ios_chrome_unittests after r535472.
    The problem is that the IOSSSLErrorHandlerTest were indirectly creating network requests which depended on the message loop type being an IO thread. The tests don't actually need to make a network request, so fix it by using a TestURLLoaderFactory.
    
    Bug: 803149
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I4b8573bd3b5a378f3afe5de307045d4c6daa7b4f
    Reviewed-on: https://chromium-review.googlesource.com/926922
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538537}

--- ios/chrome/browser/ssl/captive_portal_detector_tab_helper.h
+++ ios/chrome/browser/ssl/captive_portal_detector_tab_helper.h
@@ -14,6 +14,12 @@ namespace captive_portal {
 class CaptivePortalDetector;
 }
 
+namespace network {
+namespace mojom {
+class URLLoaderFactory;
+}
+}  // namespace network
+
 namespace web {
 class WebState;
 }
@@ -28,7 +34,8 @@ class CaptivePortalDetectorTabHelper
   // retained by the CaptivePortalDetectorTabHelper and must not be nil.
   static void CreateForWebState(
       web::WebState* web_state,
-      id<CaptivePortalDetectorTabHelperDelegate> delegate);
+      id<CaptivePortalDetectorTabHelperDelegate> delegate,
+      network::mojom::URLLoaderFactory* loader_factory_for_testing = nullptr);
 
   // Returns the associated captive portal detector.
   captive_portal::CaptivePortalDetector* detector();
@@ -39,7 +46,8 @@ class CaptivePortalDetectorTabHelper
  private:
   CaptivePortalDetectorTabHelper(
       web::WebState* web_state,
-      id<CaptivePortalDetectorTabHelperDelegate> delegate);
+      id<CaptivePortalDetectorTabHelperDelegate> delegate,
+      network::mojom::URLLoaderFactory* loader_factory_for_testing);
 
   // The delegate to notify when the user performs an action in response to the
   // captive portal detector state.
