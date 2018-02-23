634bde35f8c615d7cfd3f4dcc994441937201a3f
Danyao Wang <danyao@chromium.org>
2018-02-23 21:07:14 +0000

    Manual import wpt@db1a703c3785094e38bbf29497157ff4652e04df
    Using wpt-import in Chromium b1ae03c5e123e96be4b58d4d68ea477e66c82cc9.
    With Chromium commits locally applied on WPT:
    73f5acd546 "[css-typed-om] Rejig CSSResourceValue hierarchy."
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/12684
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    foolip@chromium.org:
      external/wpt/fullscreen
    garykac@chromium.org:
      external/wpt/clipboard-apis
    igrigorik@chromium.org, panicker@chromium.org:
      external/wpt/performance-timeline
    jsbell@chromium.org:
      external/wpt/resources
    tyoshino@chromium.org:
      external/wpt/navigation-timing
    worker-dev@chromium.org:
      external/wpt/workers
    
    TBR=danyao
    
    No-Export: true
    Change-Id: I1a3f32c7a9cefd2099c7e733dd6289a6a0e54f19
    Reviewed-on: https://chromium-review.googlesource.com/934841
    Commit-Queue: Danyao Wang <danyao@chromium.org>
    Reviewed-by: Danyao Wang <danyao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538899}

--- third_party/WebKit/LayoutTests/external/wpt/workers/support/name-as-accidental-global.js
+++ third_party/WebKit/LayoutTests/external/wpt/workers/support/name-as-accidental-global.js
@@ -1,9 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 "use strict";
+importScripts("/resources/testharness.js");
 
 var name = "something else";
 
 // This just makes the test name not "Untitled"
-test(() => { }, "Declaring name as an accidental global must not cause a harness error");
+test(() => { }, `Declaring name as an accidental global must not cause a harness error for ${self.constructor.name}`);
 
 done();
