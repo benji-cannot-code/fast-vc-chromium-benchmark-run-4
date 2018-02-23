dd7164606b1c2df1c87cf65c5b8bd2bd0f959e3c
David Valleau <valleau@chromium.org>
2018-02-23 01:11:52 +0000

    Adding a "IsPrinterRegistered" Function to SyncedPrintersManager
    R=skau@chromium.org
    
    Bug: 777035
    Change-Id: Ia33b81c1fe77b8b373fd16b91693e18287782fef
    Reviewed-on: https://chromium-review.googlesource.com/929873
    Reviewed-by: Sean Kau <skau@chromium.org>
    Commit-Queue: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538651}

--- chrome/browser/chromeos/printing/synced_printers_manager.cc
+++ chrome/browser/chromeos/printing/synced_printers_manager.cc
@@ -142,7 +142,7 @@ class SyncedPrintersManagerImpl : public SyncedPrintersManager,
         PrinterConfigurer::SetupFingerprint(printer);
 
     // Register this printer if it's the first time we're using it.
-    if (GetPrinterLocked(printer.id()) == nullptr) {
+    if (!IsPrinterRegistered(printer.id())) {
       UpdateConfiguredPrinterLocked(printer);
     }
   }
@@ -190,6 +190,14 @@ class SyncedPrintersManagerImpl : public SyncedPrintersManager,
     base::Optional<sync_pb::PrinterSpecifics> printer =
         sync_bridge_->GetPrinter(printer_id);
     return printer.has_value() ? SpecificsToPrinter(*printer) : nullptr;
+  }
+
+  // Determines whether or not the printer with the given |printer_id| has
+  // already been registered.
+  bool IsPrinterRegistered(const std::string& printer_id) {
+    return enterprise_printers_.find(printer_id) !=
+               enterprise_printers_.end() ||
+           sync_bridge_->HasPrinter(printer_id);
   }
 
   void UpdateConfiguredPrinterLocked(const Printer& printer_arg) {
