e0afd48239f08d8a7bc5b052e159a76e0a91f15d
Ahmed Fakhry <afakhry@chromium.org>
2018-02-23 02:28:52 +0000

    Fix new tab button ink drop layer sizes
    The default implementation of OnBoundsChanged() in InkDropHostView is
    to use the host view's size to update the ink drop layer and mask sizes.
    This results in the ink drop layer changing its bounds from what it was
    initially set for the new tab button, resulting in a DCHECK failure.
    
    This CL makes sure that the correct bounds are always used.
    
    BUG=814105
    
    Change-Id: Idf952db8f49a21d31969f65f3fee4d4356193f34
    Reviewed-on: https://chromium-review.googlesource.com/927905
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Mohsen Izadi <mohsen@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538670}

--- chrome/browser/ui/views/tabs/new_tab_button.cc
+++ chrome/browser/ui/views/tabs/new_tab_button.cc
@@ -84,8 +84,6 @@ NewTabButton::NewTabButton(TabStrip* tab_strip, views::ButtonListener* listener)
     // Initialize the ink drop mode for a ripple highlight on button press.
     ink_drop_container_ = new views::InkDropContainerView();
     AddChildView(ink_drop_container_);
-    ink_drop_container_->SetPaintToLayer();
-    ink_drop_container_->layer()->SetFillsBoundsOpaquely(false);
     ink_drop_container_->SetVisible(false);
 
     SetInkDropMode(InkDropMode::ON_NO_GESTURE_HANDLER);
@@ -145,6 +143,10 @@ void NewTabButton::CloseBubble() {
     new_tab_promo_->CloseBubble();
 }
 
+void NewTabButton::AnimateInkDropToStateForTesting(views::InkDropState state) {
+  GetInkDrop()->AnimateToState(state);
+}
+
 #if defined(OS_WIN)
 void NewTabButton::OnMouseReleased(const ui::MouseEvent& event) {
   if (event.IsOnlyRightMouseButton()) {
@@ -173,6 +175,8 @@ void NewTabButton::OnGestureEvent(ui::GestureEvent* event) {
 }
 
 void NewTabButton::AddInkDropLayer(ui::Layer* ink_drop_layer) {
+  DCHECK(ink_drop_layer->bounds().size() == GetVisibleBounds().size());
+  DCHECK(ink_drop_container_->bounds().size() == GetVisibleBounds().size());
   ink_drop_container_->AddInkDropLayer(ink_drop_layer);
   InstallInkDropMask(ink_drop_layer);
 }
@@ -320,8 +324,8 @@ void NewTabButton::Layout() {
     if (plus_icon_.isNull())
       InitButtonIcons();
 
-    gfx::Rect ink_drop_bounds = GetLocalBounds();
-    ink_drop_bounds.Offset(0, GetTopOffset());
+    const gfx::Rect ink_drop_bounds(gfx::Point(0, GetTopOffset()),
+                                    GetVisibleBounds().size());
     ink_drop_container_->SetBoundsRect(ink_drop_bounds);
   }
 }
@@ -334,6 +338,12 @@ void NewTabButton::OnThemeChanged() {
 
   InitButtonIcons();
   UpdateInkDropBaseColor();
+}
+
+void NewTabButton::OnBoundsChanged(const gfx::Rect& previous_bounds) {
+  const gfx::Size ink_drop_size = GetVisibleBounds().size();
+  GetInkDrop()->HostSizeChanged(ink_drop_size);
+  UpdateInkDropMaskLayerSize(ink_drop_size);
 }
 
 bool NewTabButton::GetHitTestMask(gfx::Path* mask) const {
--- chrome/browser/ui/views/tabs/new_tab_button.h
+++ chrome/browser/ui/views/tabs/new_tab_button.h
@@ -53,6 +53,8 @@ class NewTabButton : public views::ImageButton,
   // when it exists.
   void CloseBubble();
 
+  void AnimateInkDropToStateForTesting(views::InkDropState state);
+
   NewTabPromoBubbleView* new_tab_promo() { return new_tab_promo_; }
 
  private:
@@ -70,6 +72,7 @@ class NewTabButton : public views::ImageButton,
   void PaintButtonContents(gfx::Canvas* canvas) override;
   void Layout() override;
   void OnThemeChanged() override;
+  void OnBoundsChanged(const gfx::Rect& previous_bounds) override;
 
   // views::MaskedTargeterDelegate:
   bool GetHitTestMask(gfx::Path* mask) const override;
