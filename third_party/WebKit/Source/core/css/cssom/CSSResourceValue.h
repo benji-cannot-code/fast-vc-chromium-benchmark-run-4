8c8ab60c6ad3ac3f0aafd4dcceeb244c80fb4db9
Darren Shen <shend@chromium.org>
2018-02-23 08:54:12 +0000

    [css-typed-om] Rejig CSSResourceValue hierarchy.
    We implement the new CSSImageValue, which is an opaque object with no
    constructor. We also had to change the testsuite.js a bit to test it.
    
    Bug: 545318
    Change-Id: I0ae719c5380e01ff9423e0ff0684afe547de1bb0
    Reviewed-on: https://chromium-review.googlesource.com/934001
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: nainar <nainar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538730}

--- third_party/WebKit/Source/core/css/cssom/CSSResourceValue.h
+++ third_party/WebKit/Source/core/css/cssom/CSSResourceValue.h
@@ -13,8 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class CORE_EXPORT CSSResourceValue : public CSSStyleValue {
-  DEFINE_WRAPPERTYPEINFO();
-
  public:
   virtual ~CSSResourceValue() = default;
 
--- third_party/WebKit/Source/core/css/cssom/CSSURLImageValue.h
+++ third_party/WebKit/Source/core/css/cssom/CSSURLImageValue.h
@@ -15,8 +15,6 @@ class ScriptState;
 class CSSImageValue;
 
 class CORE_EXPORT CSSURLImageValue final : public CSSStyleImageValue {
-  DEFINE_WRAPPERTYPEINFO();
-
  public:
   static CSSURLImageValue* Create(ScriptState*,
                                   const AtomicString& url,
