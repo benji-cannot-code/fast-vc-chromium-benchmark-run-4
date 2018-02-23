5e87d7574a54d0d23359cffab93caa0fbdd951f5
Devlin Cronin <rdevlin.cronin@chromium.org>
2018-02-23 01:40:56 +0000

    [Extensions] Canonicalize URLPattern hosts
    Ensure that URLPattern hosts, when non-empty, are canonicalized.
    Silently update non-canonical hosts (e.g. gOOgle.com) to be
    canonical (google.com). Fail only if canonicalization fails.
    
    Bug: 811932
    
    Change-Id: I87dbea12b972af00a7648f6d715eccb836613366
    Reviewed-on: https://chromium-review.googlesource.com/916942
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538659}

--- extensions/common/url_pattern.cc
+++ extensions/common/url_pattern.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/url_constants.h"
 #include "extensions/common/constants.h"
 #include "net/base/registry_controlled_domains/registry_controlled_domain.h"
+#include "net/base/url_util.h"
 #include "url/gurl.h"
 #include "url/url_util.h"
 
@@ -306,6 +307,16 @@ URLPattern::ParseResult URLPattern::Parse(base::StringPiece pattern,
   if (host_.find('*') != std::string::npos)
     return PARSE_ERROR_INVALID_HOST_WILDCARD;
 
+  if (!host_.empty()) {
+    // If |host_| is present (i.e., isn't a wildcard), we need to canonicalize
+    // it.
+    url::CanonHostInfo host_info;
+    host_ = net::CanonicalizeHost(host_, &host_info);
+    // net::CanonicalizeHost() returns an empty string on failure.
+    if (host_.empty())
+      return PARSE_ERROR_INVALID_HOST;
+  }
+
   // Null characters are not allowed in hosts.
   if (host_.find('\0') != std::string::npos)
     return PARSE_ERROR_INVALID_HOST;
@@ -438,7 +449,10 @@ bool URLPattern::MatchesScheme(base::StringPiece test) const {
 }
 
 bool URLPattern::MatchesHost(base::StringPiece host) const {
-  // TODO(devlin): This is a bit sad. Parsing urls is expensive.
+  // TODO(devlin): This is a bit sad. Parsing urls is expensive. However, it's
+  // important that we do this conversion to a GURL in order to canonicalize the
+  // host (the pattern's host_ already is canonicalized from Parse()). We can't
+  // just do string comparison.
   return MatchesHost(
       GURL(base::StringPrintf("%s%s%s/", url::kHttpScheme,
                               url::kStandardSchemeSeparator, host.data())));
