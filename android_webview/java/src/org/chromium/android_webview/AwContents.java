75c61f7871ba303100a9f3d923d2b6abadb248fe
Jinsuk Kim <jinsukkim@chromium.org>
2018-02-23 01:10:36 +0000

    Define JoystickHandler for Android
    JoystickHandler in content/ is WebContentsUserData-backed
    class that handles generic motion events by converting them
    to fling event. This sets ContentViewCore free from having to
    manage joystick-related states and keep several view event flows
    together inside.
    
    Fling events that joystick input is converted to are now
    routed to EventForwarder and going down ViewAndroid tree.
    Updated the flow to handle more gesture events relevant to
    fling scroll.
    
    Bug: 811168
    
    Change-Id: I6904e92e708fdea33dba8fc0df2ace48134331fa
    Reviewed-on: https://chromium-review.googlesource.com/915747
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538650}

--- android_webview/java/src/org/chromium/android_webview/AwContents.java
+++ android_webview/java/src/org/chromium/android_webview/AwContents.java
@@ -701,7 +701,7 @@ public class AwContents implements SmartClipProvider {
 
         @Override
         public void cancelFling() {
-            mContentViewCore.cancelFling(SystemClock.uptimeMillis());
+            mWebContents.getEventForwarder().onCancelFling(SystemClock.uptimeMillis());
         }
     }
 
@@ -2274,7 +2274,8 @@ public class AwContents implements SmartClipProvider {
     public void flingScroll(int velocityX, int velocityY) {
         if (TRACE) Log.i(TAG, "%s flingScroll", this);
         if (isDestroyedOrNoOperation(WARN)) return;
-        mContentViewCore.flingViewport(SystemClock.uptimeMillis(), -velocityX, -velocityY, false);
+        mWebContents.getEventForwarder().onStartFling(
+                SystemClock.uptimeMillis(), -velocityX, -velocityY, false);
     }
 
     /**
