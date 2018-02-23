4f27f2224d8a41297ad3142ee6ae65016ae42c19
Bartosz Fabianowski <bartfab@chromium.org>
2018-02-23 17:25:03 +0000

    Add app push-install event log manager
    This class ties together collection, storage and upload of app
    push-install event logs. It creates an |AppInstallEventLogger|
    that generates log entries, collects the entries, schedules their
    upload to the server and their storage on disk.
    
    All operations related to the log file happen on a background task
    runner.
    
    This CL also adds the |AppInstallEventLogger| which, however, for
    now does not generate any log entries yet.
    
    Bug: b/73277923
    Test: unit_tests
    Change-Id: I3eb421111339b1566aa6c7f5d8897af6e16fe090
    Reviewed-on: https://chromium-review.googlesource.com/929502
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Luis Hector Chavez <lhchavez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538814}

---
+++ chrome/browser/chromeos/policy/app_install_event_logger.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/policy/app_install_event_logger.h"
+
+#include "chrome/browser/profiles/profile.h"
+#include "components/arc/arc_prefs.h"
+#include "components/pref_registry/pref_registry_syncable.h"
+#include "components/prefs/pref_service.h"
+
+namespace policy {
+
+AppInstallEventLogger::AppInstallEventLogger(Delegate* delegate,
+                                             Profile* profile) {}
+
+// static
+void AppInstallEventLogger::RegisterProfilePrefs(
+    user_prefs::PrefRegistrySyncable* registry) {
+  registry->RegisterListPref(arc::prefs::kArcPushInstallAppsRequested);
+  registry->RegisterListPref(arc::prefs::kArcPushInstallAppsPending);
+}
+
+// static
+void AppInstallEventLogger::Clear(Profile* profile) {
+  profile->GetPrefs()->ClearPref(arc::prefs::kArcPushInstallAppsRequested);
+  profile->GetPrefs()->ClearPref(arc::prefs::kArcPushInstallAppsPending);
+}
+
+}  // namespace policy
--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -221,6 +221,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/users/multi_profile_user_controller.h"
 #include "chrome/browser/chromeos/net/network_throttling_observer.h"
 #include "chrome/browser/chromeos/platform_keys/key_permissions.h"
+#include "chrome/browser/chromeos/policy/app_install_event_logger.h"
 #include "chrome/browser/chromeos/policy/auto_enrollment_client.h"
 #include "chrome/browser/chromeos/policy/browser_policy_connector_chromeos.h"
 #include "chrome/browser/chromeos/policy/device_cloud_policy_manager_chromeos.h"
@@ -631,6 +632,7 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
   extensions::EPKPChallengeUserKey::RegisterProfilePrefs(registry);
   flags_ui::PrefServiceFlagsStorage::RegisterProfilePrefs(registry);
   lock_screen_apps::StateController::RegisterProfilePrefs(registry);
+  policy::AppInstallEventLogger::RegisterProfilePrefs(registry);
   policy::DeviceStatusCollector::RegisterProfilePrefs(registry);
   ::onc::RegisterProfilePrefs(registry);
 #endif
