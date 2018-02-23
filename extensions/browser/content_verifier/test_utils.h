cea21801079c96eafb601030589a971086e5d9ed
Istiaque Ahmed <lazyboy@chromium.org>
2018-02-23 02:52:34 +0000

    Move mock ContentVerifierDelegate to test_utils.
    This will be used in other tests in the future, so extract it to
    test_utils.
    
    Bug: 796395
    Change-Id: Iddc6a764d56acdc41b65a4ca1424f1ad124d8f1a
    Reviewed-on: https://chromium-review.googlesource.com/932910
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538677}

--- extensions/browser/content_verifier/test_utils.h
+++ extensions/browser/content_verifier/test_utils.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/optional.h"
 #include "base/run_loop.h"
+#include "extensions/browser/content_verifier_delegate.h"
 #include "extensions/browser/content_verify_job.h"
 #include "extensions/common/extension_id.h"
 
@@ -51,6 +52,29 @@ class TestContentVerifyJobObserver : ContentVerifyJob::TestObserver {
   bool seen_on_hashes_ready_ = false;
 
   DISALLOW_COPY_AND_ASSIGN(TestContentVerifyJobObserver);
+};
+
+// An extensions/ implementation of ContentVerifierDelegate for using in tests.
+// Provides mock versions of content verification mode, keys and fetch url.
+class MockContentVerifierDelegate : public ContentVerifierDelegate {
+ public:
+  MockContentVerifierDelegate();
+  ~MockContentVerifierDelegate() override;
+
+  // ContentVerifierDelegate:
+  ContentVerifierDelegate::Mode ShouldBeVerified(
+      const Extension& extension) override;
+  ContentVerifierKey GetPublicKey() override;
+  GURL GetSignatureFetchUrl(const ExtensionId& extension_id,
+                            const base::Version& version) override;
+  std::set<base::FilePath> GetBrowserImagePaths(
+      const extensions::Extension* extension) override;
+  void VerifyFailed(const ExtensionId& extension_id,
+                    ContentVerifyJob::FailureReason reason) override;
+  void Shutdown() override;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(MockContentVerifierDelegate);
 };
 
 namespace content_verifier_test_utils {
