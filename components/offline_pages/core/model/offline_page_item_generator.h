667eae65813d5940eedb33227bb507c1e388cae5
Jian Li <jianli@chromium.org>
2018-02-23 23:12:43 +0000

    Get offline page by file size and digest
    When a saved page is opened from Android Downloads or 3rd party app,
    we need to query the Offline Pages DB based on the size and digest
    of the MHTML content in order to find out if there is a trusted
    offline page assoicated with it.
    
    Bug: 758690
    Change-Id: I732a9a41e367310231436c5701377d38516849a6
    Reviewed-on: https://chromium-review.googlesource.com/932744
    Commit-Queue: Jian Li <jianli@chromium.org>
    Reviewed-by: Dmitry Titov <dimich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538939}

--- components/offline_pages/core/model/offline_page_item_generator.h
+++ components/offline_pages/core/model/offline_page_item_generator.h
@@ -35,6 +35,7 @@ class OfflinePageItemGenerator {
   void SetLastAccessTime(base::Time time);
   void SetAccessCount(int access_count);
   void SetArchiveDirectory(const base::FilePath& archive_dir);
+  void SetDigest(const std::string& digest);
 
  private:
   std::string namespace_ = kDefaultNamespace;
@@ -46,6 +47,7 @@ class OfflinePageItemGenerator {
   base::Time last_access_time_;
   int access_count_ = 0;
   base::FilePath archive_dir_;
+  std::string digest_;
 };
 }  // namespace offline_pages
 
