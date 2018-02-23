17f85fccbde8eb010d359f63d5837c9c071adbc6
Tim Brown <timbrown@chromium.org>
2018-02-23 00:18:13 +0000

    Use libappindicator to show icons in all cases
    libappindicator will transparently use GtkStatusIcon if an AppIndicator
    listener is not available. We will lose the left click action however,
    and instead the right-click menu will be shown in all instances.
    
    Using libappindicator in all cases not only reduces code complexity,
    but also abstracts away the complexity of determining when to use
    application indicators (vs legacy icons), which is a little more
    complicated with newer versions of GNOME removing the legacy icon tray,
    but older versions of GNOME not showing the application indicator by
    default.
    
    I also fixed an issue with detecting GNOME. The environment variable
    value is often prefixed with the distro.
    
    Bug: 799144, 797332, 419673
    Change-Id: I156698f1b37ba216b4df11fa2f17dd7012c593fa
    Reviewed-on: https://chromium-review.googlesource.com/911820
    Reviewed-by: Elliot Glaysher <erg@chromium.org>
    Reviewed-by: Scott Graham <scottmg@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Tim Brown <timbrown@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538635}

--- chrome/browser/ui/libgtkui/gtk_ui.cc
+++ chrome/browser/ui/libgtkui/gtk_ui.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/libgtkui/chrome_gtk_frame.h"
 #include "chrome/browser/ui/libgtkui/gtk_event_loop.h"
 #include "chrome/browser/ui/libgtkui/gtk_key_bindings_handler.h"
-#include "chrome/browser/ui/libgtkui/gtk_status_icon.h"
 #include "chrome/browser/ui/libgtkui/gtk_util.h"
 #include "chrome/browser/ui/libgtkui/print_dialog_gtk.h"
 #include "chrome/browser/ui/libgtkui/printing_gtk_util.h"
@@ -635,15 +634,10 @@ bool GtkUi::IsStatusIconSupported() const {
 std::unique_ptr<views::StatusIconLinux> GtkUi::CreateLinuxStatusIcon(
     const gfx::ImageSkia& image,
     const base::string16& tool_tip) const {
-  if (AppIndicatorIcon::CouldOpen()) {
-    ++indicators_count;
-    return std::unique_ptr<views::StatusIconLinux>(new AppIndicatorIcon(
-        base::StringPrintf("%s%d", kAppIndicatorIdPrefix, indicators_count),
-        image, tool_tip));
-  } else {
-    return std::unique_ptr<views::StatusIconLinux>(
-        new Gtk2StatusIcon(image, tool_tip));
-  }
+  ++indicators_count;
+  return std::make_unique<libgtkui::AppIndicatorIcon>(
+      base::StringPrintf("%s%d", kAppIndicatorIdPrefix, indicators_count),
+      image, tool_tip);
 }
 
 gfx::Image GtkUi::GetIconForContentType(const std::string& content_type,
