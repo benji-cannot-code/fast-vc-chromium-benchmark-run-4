4d442ea358f996e87f18024cf95a0777f0e3eb3d
Thomas Tangl <tangltom@chromium.org>
2018-02-23 09:40:11 +0000

    [Dice] Add minimum height to DiceSigninButton
    This CL adds a minimum height to the DiceSigninButton
    to match the mocks under:
    https://gallery.googleplex.com/projects/MCHbtQVoQ2HCZWnUS9sdsb0Q/files/MCHtA7U1iMGr6-WzjO3kn_RsRHBgUr2ucD8
    
    Bug: 813567
    Change-Id: I998d799f5dc4d4cea8e71c2051c3685c1c0d0239
    Reviewed-on: https://chromium-review.googlesource.com/931542
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Thomas Tangl <tangltom@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538737}

--- chrome/browser/ui/views/sync/dice_signin_button.cc
+++ chrome/browser/ui/views/sync/dice_signin_button.cc
@@ -30,6 +30,9 @@ constexpr int kDividerHorizontalPadding = 4;
 constexpr int kDividerWidth = 1;
 constexpr int kDividerVerticalPadding = 10;
 
+// Minimum height for the personalized Dice sign-in button.
+constexpr int kMinAccountButtonHeight = 64;
+
 int GetDividerAndArrowReservedWidth() {
   return 2 * kDividerHorizontalPadding + kDividerWidth +
          kDropDownArrowButtonWidth;
@@ -134,7 +137,8 @@ int DiceSigninButton::GetHeightForWidth(int width) const {
   int title_subtitle_height = label()->GetHeightForWidth(width) +
                               subtitle_->GetHeightForWidth(width) +
                               GetInsets().height();
-  return std::max(height_without_subtitle, title_subtitle_height);
+  return std::max({height_without_subtitle, title_subtitle_height,
+                   kMinAccountButtonHeight});
 }
 
 gfx::Size DiceSigninButton::CalculatePreferredSize() const {
@@ -154,7 +158,7 @@ gfx::Size DiceSigninButton::CalculatePreferredSize() const {
                                   GetInsets().height();
   int pref_height =
       std::max(parent_pref_size.height(), pref_height_with_subtitle);
-  return gfx::Size(pref_width, pref_height);
+  return gfx::Size(pref_width, std::max(pref_height, kMinAccountButtonHeight));
 }
 
 void DiceSigninButton::Layout() {
