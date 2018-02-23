89c40c9d24c86ebb222190b653471ea8fa743f40
Avi Drissman <avi@chromium.org>
2018-02-23 17:55:39 +0000

    Move IconLoader/Manager to OnceCallbacks.
    BUG=none
    TEST=existing tests
    
    Change-Id: I65793a97dd3a3798014a39756441094fb4d4d4a7
    Reviewed-on: https://chromium-review.googlesource.com/931624
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538815}

--- chrome/browser/icon_loader_auralinux.cc
+++ chrome/browser/icon_loader_auralinux.cc
@@ -51,6 +51,7 @@ void IconLoader::ReadIcon() {
   }
 
   target_task_runner_->PostTask(
-      FROM_HERE, base::BindOnce(callback_, std::move(image), group_));
+      FROM_HERE,
+      base::BindOnce(std::move(callback_), std::move(image), group_));
   delete this;
 }
