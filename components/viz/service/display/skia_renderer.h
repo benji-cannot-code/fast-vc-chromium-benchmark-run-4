e408e27c4ca2bbb553ee77be3a862d7892b0d28a
Weiliang Chen <weiliangc@chromium.org>
2018-02-23 23:01:27 +0000

    viz: Use GLRenderer's SyncQuery for SkiaRenderer
    Copy out GLRenderer's implementation of SyncQuery to be shared with
    SkiaRenderer.
    
    R=enne
    Bug: 644851
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: Iab621087534fc608d0fd9d319255a44335ac9ce4
    Reviewed-on: https://chromium-review.googlesource.com/933705
    Reviewed-by: enne <enne@chromium.org>
    Commit-Queue: weiliangc <weiliangc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538935}

--- components/viz/service/display/skia_renderer.h
+++ components/viz/service/display/skia_renderer.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "cc/cc_export.h"
 #include "components/viz/service/display/direct_renderer.h"
+#include "components/viz/service/display/sync_query_collection.h"
 #include "components/viz/service/viz_service_export.h"
 #include "gpu/vulkan/features.h"
 #include "ui/latency/latency_info.h"
@@ -129,6 +130,8 @@ class VIZ_SERVICE_EXPORT SkiaRenderer : public DirectRenderer {
   SkCanvas* current_canvas_ = nullptr;
   SkPaint current_paint_;
 
+  bool use_sync_query_ = false;
+  SyncQueryCollection sync_queries_;
   bool use_swap_with_bounds_ = false;
 
   gfx::Rect swap_buffer_rect_;
