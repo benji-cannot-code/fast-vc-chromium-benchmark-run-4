5b5e427256d7dc52d210123e8e20cbc76a669580
Rob Buis <rob.buis@samsung.com>
2018-02-23 21:18:33 +0000

    Fix errorprone JavaLangClash warnings
    Fix errorprone JavaLangClash warnings and make
    sure it is treated as error after this CL.
    
    http://errorprone.info/bugpattern/JavaLangClash
    
    Bug: 801253
    Change-Id: I5958e0e1c3d15e637b04c12e2810dee0cfb16cfc
    Reviewed-on: https://chromium-review.googlesource.com/929755
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Rob Buis <rob.buis@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#538903}

--- remoting/android/java/src/org/chromium/chromoting/jni/ConnectionListener.java
+++ remoting/android/java/src/org/chromium/chromoting/jni/ConnectionListener.java
@@ -8,6 +8,7 @@ package org.chromium.chromoting.jni;
 import org.chromium.chromoting.R;
 
 /** Interface used for connection state notifications. */
+@SuppressWarnings("JavaLangClash")
 public interface ConnectionListener {
     /**
      * This enum must match the C++ enumeration remoting::protocol::ConnectionToHost::State.
