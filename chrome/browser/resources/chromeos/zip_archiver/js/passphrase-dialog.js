d57f3b458cd5558ba7d2a51594aa92517080ca50
Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
2018-02-23 06:12:11 +0000

    Zip Archiver: Stop memorizing and remembering zip passwords.
    This is a partial reland of 876705.
    Since the extension mounts files with persistent:false, remembering
    passwords (for reboot/suspend) does not have any intended effect.
    Therefore this change removes the "remember" checkbox for the password
    input UI and stops remembering new passwords of encrypted files in ZIP
    files.
    This change also stops using remembered passwords by old versions of the
    extension.
    
    As a remaining issue after this, when there is already memorized mount
    info by older versions of the extension, it will still mount the zip file
    upon reboot/suspend until user successfully unmounts it.
    
    
    Bug: 789073
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ibaa2afd21dbbebfa67f2e5616913313be9ad518e
    Reviewed-on: https://chromium-review.googlesource.com/930644
    Reviewed-by: Yuki Awano <yawano@chromium.org>
    Commit-Queue: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538711}

--- chrome/browser/resources/chromeos/zip_archiver/js/passphrase-dialog.js
+++ chrome/browser/resources/chromeos/zip_archiver/js/passphrase-dialog.js
@@ -20,7 +20,7 @@ Polymer({
   },
 
   accept: function() {
-    window.onPassphraseSuccess(this.$.input.value, this.$.remember.checked);
+    window.onPassphraseSuccess(this.$.input.value);
     window.close();
   },
 
