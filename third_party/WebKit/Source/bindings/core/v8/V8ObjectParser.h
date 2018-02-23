26c93a0e49d230f1fe3ede8eedeb2c77b5dd76f1
Ian Clelland <iclelland@chromium.org>
2018-02-23 21:03:15 +0000

    Add header guards to V8ObjectParser.h
    Change-Id: I1e6e6225611f468cf710b8d4c9cfa5f28fccb6dd
    Reviewed-on: https://chromium-review.googlesource.com/935150
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Ian Clelland <iclelland@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538895}

--- third_party/WebKit/Source/bindings/core/v8/V8ObjectParser.h
+++ third_party/WebKit/Source/bindings/core/v8/V8ObjectParser.h
@@ -3,6 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifndef V8ObjectParser_h
+#define V8ObjectParser_h
+
 #include "core/CSSPropertyNames.h"
 #include "core/CoreExport.h"
 #include "platform/wtf/Allocator.h"
@@ -53,3 +56,5 @@ class CORE_EXPORT V8ObjectParser final {
 };
 
 }  // namespace blink
+
+#endif  // V8ObjectParser_h
