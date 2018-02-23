cf2bd06df61c330719d0a071534ae3d3c7466909
Kim Paulhamus <kpaulhamus@chromium.org>
2018-02-23 18:14:45 +0000

    Update excludeList => excludeCredentials in browsertest.
    Per https://w3c.github.io/webauthn/#dictdef-publickeycredentialcreationoptions
    
    Change-Id: I78783bf1722d727e7014893e5f98e8e77c313b39
    Reviewed-on: https://chromium-review.googlesource.com/933372
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Commit-Queue: Kim Paulhamus <kpaulhamus@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538823}

--- chrome/browser/password_manager/credential_manager_browsertest.cc
+++ chrome/browser/password_manager/credential_manager_browsertest.cc
@@ -98,7 +98,7 @@ class CredentialManagerBrowserTest : public PasswordManagerBrowserTestBase {
         "    icon: 'https://pics.acme.com/00/p/aBjjjpqPb.png'},"
         "  pubKeyCredParams: [{ type: 'public-key', alg: '123'}],"
         "  timeout: 60000,"
-        "  excludeList: [] }"
+        "  excludeCredentials: [] }"
         "}).catch(c => window.domAutomationController.send(c.toString()));";
     ASSERT_TRUE(
         content::ExecuteScriptAndExtractString(web_contents, script, &result));
@@ -122,7 +122,7 @@ class CredentialManagerBrowserTest : public PasswordManagerBrowserTestBase {
         "    icon: 'https://pics.acme.com/00/p/aBjjjpqPb.png'},"
         "  pubKeyCredParams: [{ type: 'public-key', alg: '-7'}],"
         "  timeout: 60000,"
-        "  excludeList: [] }"
+        "  excludeCredentials: [] }"
         "}).catch(c => window.domAutomationController.send(c.toString()));";
     ASSERT_TRUE(
         content::ExecuteScriptAndExtractString(web_contents, script, &result));
@@ -144,7 +144,7 @@ class CredentialManagerBrowserTest : public PasswordManagerBrowserTestBase {
         "    icon: 'https://pics.acme.com/00/p/aBjjjpqPb.png'},"
         "  pubKeyCredParams: [{ type: 'public-key', alg: '-7'}],"
         "  timeout: 60000,"
-        "  excludeList: [] }"
+        "  excludeCredentials: [] }"
         "}).catch(c => window.domAutomationController.send(c.toString()));";
     ASSERT_TRUE(
         content::ExecuteScriptAndExtractString(web_contents, script, &result));
