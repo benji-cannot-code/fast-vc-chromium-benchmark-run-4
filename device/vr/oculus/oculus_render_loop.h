37f65bb3e465e207e5e03c099a8936e6cf5673d7
Bill Orr <billorr@chromium.org>
2018-02-24 02:08:45 +0000

    Correctly handle Oculus SRGB
    Oculus wants submitted textures to be in SRGB format. WebGL is rendering
    linear color space, so we need to convert.
    
    Bug: 812408
    Change-Id: I25fc2ca95a62ec95ef7773c17ad2001910a845e5
    Reviewed-on: https://chromium-review.googlesource.com/935494
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Bill Orr <billorr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538967}

--- device/vr/oculus/oculus_render_loop.h
+++ device/vr/oculus/oculus_render_loop.h
@@ -49,6 +49,7 @@ class OculusRenderLoop : public base::Thread, mojom::VRPresentationProvider {
  private:
   // base::Thread overrides:
   void Init() override;
+  void CleanUp() override;
 
   mojom::VRPosePtr GetPose();
 
