45c0cbae1438870d96a435637c3df2e571adba13
Lei Zhang <thestig@chromium.org>
2018-02-24 00:28:19 +0000

    Fix child process leaks in the Cloud Print Connector.
    Before r526819, the utility process performing the PDF to EMF conversion
    disconnects itself. Its ServiceUtilityProcessHost notices the
    disconnection and deletes itself. With r526819, the utility process no
    longer disconnects itself, and waits forever for its host to tear down
    the connection.
    
    Meet this new expectation and tear down the connection from the host
    side.
    
    BUG=814984
    
    Change-Id: I505673a82eb4748898819c6664802238738443bb
    Reviewed-on: https://chromium-review.googlesource.com/935631
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538959}

--- chrome/service/service_utility_process_host.cc
+++ chrome/service/service_utility_process_host.cc
@@ -499,6 +499,8 @@ void ServiceUtilityProcessHost::OnChildDisconnected() {
         FROM_HERE, base::Bind(&Client::OnChildDied, client_.get()));
     ReportUmaEvent(SERVICE_UTILITY_DISCONNECTED);
   }
+
+  // The child process has died for some reason. This host is no longer needed.
   delete this;
 }
 
@@ -575,6 +577,9 @@ void ServiceUtilityProcessHost::OnPDFToEmfFinished(bool success) {
       FROM_HERE, base::Bind(&Client::OnRenderPDFPagesToMetafileDone,
                             client_.get(), success));
   pdf_to_emf_state_.reset();
+
+  // The child process has finished at this point. This host is done as well.
+  delete this;
 }
 
 void ServiceUtilityProcessHost::OnGetPrinterCapsAndDefaultsSucceeded(
@@ -586,6 +591,8 @@ void ServiceUtilityProcessHost::OnGetPrinterCapsAndDefaultsSucceeded(
   client_task_runner_->PostTask(
       FROM_HERE, base::Bind(&Client::OnGetPrinterCapsAndDefaults, client_.get(),
                             true, printer_name, caps_and_defaults));
+  // The child process disconnects itself and this host deletes itself via
+  // OnChildDisconnected().
 }
 
 void ServiceUtilityProcessHost::OnGetPrinterSemanticCapsAndDefaultsSucceeded(
@@ -598,6 +605,8 @@ void ServiceUtilityProcessHost::OnGetPrinterSemanticCapsAndDefaultsSucceeded(
       FROM_HERE,
       base::Bind(&Client::OnGetPrinterSemanticCapsAndDefaults, client_.get(),
                  true, printer_name, caps_and_defaults));
+  // The child process disconnects itself and this host deletes itself via
+  // OnChildDisconnected().
 }
 
 void ServiceUtilityProcessHost::OnGetPrinterCapsAndDefaultsFailed(
@@ -609,6 +618,8 @@ void ServiceUtilityProcessHost::OnGetPrinterCapsAndDefaultsFailed(
       FROM_HERE,
       base::Bind(&Client::OnGetPrinterCapsAndDefaults, client_.get(), false,
                  printer_name, printing::PrinterCapsAndDefaults()));
+  // The child process disconnects itself and this host deletes itself via
+  // OnChildDisconnected().
 }
 
 void ServiceUtilityProcessHost::OnGetPrinterSemanticCapsAndDefaultsFailed(
@@ -620,6 +631,8 @@ void ServiceUtilityProcessHost::OnGetPrinterSemanticCapsAndDefaultsFailed(
       FROM_HERE, base::Bind(&Client::OnGetPrinterSemanticCapsAndDefaults,
                             client_.get(), false, printer_name,
                             printing::PrinterSemanticCapsAndDefaults()));
+  // The child process disconnects itself and this host deletes itself via
+  // OnChildDisconnected().
 }
 
 bool ServiceUtilityProcessHost::Client::MetafileAvailable(float scale_factor,
