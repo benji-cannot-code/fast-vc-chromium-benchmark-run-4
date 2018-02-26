f53139e80b53b66a6c5cea1520b8f7dd9dcc0eac
vasilii <vasilii@chromium.org>
2018-02-26 10:33:13 +0000

    Increase test coverage of "Show All" manual fallback for passwords.
    Bug: 782287
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I534f2b892efe8d2af08bbea6ee724c38509a55c9
    Reviewed-on: https://chromium-review.googlesource.com/926581
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539102}

--- ios/chrome/browser/passwords/password_controller_unittest.mm
+++ ios/chrome/browser/passwords/password_controller_unittest.mm
@@ -1277,7 +1277,11 @@ TEST_F(PasswordControllerTest, SelectingSuggestionShouldFillPasswordForm) {
                           webState:web_state()
                  completionHandler:^(NSArray* suggestions,
                                      id<FormSuggestionProvider> provider) {
-                   EXPECT_EQ(2u, [suggestions count]);
+                   NSMutableArray* suggestion_values = [NSMutableArray array];
+                   for (FormSuggestion* suggestion in suggestions)
+                     [suggestion_values addObject:suggestion.value];
+                   EXPECT_NSEQ((@[ @"abc", @"Show All\u2026" ]),
+                               suggestion_values);
                    block_was_called = YES;
                  }];
     EXPECT_TRUE(block_was_called);
