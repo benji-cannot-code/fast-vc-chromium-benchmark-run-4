bf2bf8a24fa74380915aa0154cb7985d8bacc718
Daniel Bratell <bratell@opera.com>
2018-02-26 16:37:45 +0000

    Correct some broken include guards.
    Found while working on a PRESUBMIT test for include guards.
    
    Change-Id: Ib622cbaae28c48a469337076b57f5139971baff5
    Reviewed-on: https://chromium-review.googlesource.com/932662
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539155}

--- components/gcm_driver/gcm_driver_constants.h
+++ components/gcm_driver/gcm_driver_constants.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Messaging) Driver.
 
 #ifndef COMPONENTS_GCM_DRIVER_GCM_DRIVER_CONSTANTS_H_
+#define COMPONENTS_GCM_DRIVER_GCM_DRIVER_CONSTANTS_H_
 
 #include "base/files/file_path.h"
 
--- third_party/WebKit/Source/core/leak_detector/BlinkLeakDetectorClient.h
+++ third_party/WebKit/Source/core/leak_detector/BlinkLeakDetectorClient.h
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #ifndef BlinkLeakDetectorClient_h
-
 #define BlinkLeakDetectorClient_h
 
 namespace blink {
