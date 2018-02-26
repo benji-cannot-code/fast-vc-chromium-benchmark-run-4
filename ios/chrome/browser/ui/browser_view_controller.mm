8a904887112a1b2f8cc455eeec2aa79345352d1a
Kurt Horimoto <kkhorimoto@chromium.org>
2018-02-26 19:53:21 +0000

    [iOS] Continue broadcasting while new tab animations are occurring.
    This allows BVC to intercept the animated fullscreen disabling action
    for new foreground tabs.
    
    Bug: 809120
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I8b3e4865a98cde6eeb36cd8fbd6d1ec05983d5ce
    Reviewed-on: https://chromium-review.googlesource.com/935442
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539244}

--- ios/chrome/browser/ui/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view_controller.mm
@@ -2417,8 +2417,7 @@ applicationCommandEndpoint:(id<ApplicationCommands>)applicationCommandEndpoint {
 }
 
 - (void)updateBroadcastState {
-  self.broadcasting =
-      self.active && self.viewVisible && !self.inNewTabAnimation;
+  self.broadcasting = self.active && self.viewVisible;
 }
 
 - (void)updateDialogPresenterActiveState {
