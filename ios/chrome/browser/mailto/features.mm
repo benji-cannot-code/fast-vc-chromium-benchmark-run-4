adfa7aca96926e2871a9f7ade2fc0b8ce02f6efa
Daniel Blakemore <danblakemore@google.com>
2018-02-26 16:39:08 +0000

    Re-relands 697d4f8f3f26c04a954cf5d1d9f9c974b82e61e7 after fixing downstream change.
    Bug: 808113
    
    Original change's description:
    >Enables Mailto Handling with Google UI by default.
    >
    >Bug: 808113
    >Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    >Change-Id: I2b475e6fd474c900ed1984010d15a3e1e31c2af0
    >Reviewed-on: https://chromium-review.googlesource.com/922930
    >Commit-Queue: Daniel Blakemore <danblakemore@google.com>
    >Reviewed-by: Peter Lee <pkl@chromium.org>
    >Cr-Commit-Position: refs/heads/master@{#537373}
    
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I85b528de28e34453f97d00a8d4bf4f9618f6bb57
    Reviewed-on: https://chromium-review.googlesource.com/927526
    Reviewed-by: Peter Lee <pkl@chromium.org>
    Commit-Queue: Daniel Blakemore <danblakemore@google.com>
    Cr-Commit-Position: refs/heads/master@{#539157}

--- ios/chrome/browser/mailto/features.mm
+++ ios/chrome/browser/mailto/features.mm
@@ -10,4 +10,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 const base::Feature kMailtoHandledWithGoogleUI{
-    "kMailtoHandledWithGoogleUI", base::FEATURE_DISABLED_BY_DEFAULT};
+    "kMailtoHandledWithGoogleUI", base::FEATURE_ENABLED_BY_DEFAULT};
