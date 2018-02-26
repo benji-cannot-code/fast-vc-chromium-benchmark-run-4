3080d4a9821b71cadb5beaf17653d47008221e2e
Aaron Leventhal <aleventhal@chromium.org>
2018-02-26 22:20:37 +0000

    Use alert role so that keyword hint view alert event is spoken by screen readers
    The correct accessibility alert event was fired but screen readers ignored it
    since it was not on an object with the alert role.
    
    Bug: 812647
    Change-Id: Id0cc1fbcbaeaeb20e2ef546b463304a3aaaa1af3
    Reviewed-on: https://chromium-review.googlesource.com/924884
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539285}

--- chrome/browser/ui/views/location_bar/keyword_hint_view.cc
+++ chrome/browser/ui/views/location_bar/keyword_hint_view.cc
@@ -18,9 +18,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/location_bar/location_bar_view.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/search_engines/template_url_service.h"
+#include "ui/accessibility/ax_node_data.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/gfx/color_utils.h"
 #include "ui/strings/grit/ui_strings.h"
+#include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/border.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/layout/fill_layout.h"
@@ -63,6 +65,11 @@ KeywordHintView::KeywordHintView(views::ButtonListener* listener,
       CreateLabel(font_list, text_color, background_color);
 
   SetFocusBehavior(FocusBehavior::NEVER);
+
+  // Use leaf alert role so that name is spoken by screen reader, but redundant
+  // child label text is not also spoken.
+  GetViewAccessibility().OverrideRole(ax::mojom::Role::kAlert);
+  GetViewAccessibility().OverrideIsLeaf();
 }
 
 KeywordHintView::~KeywordHintView() {}
@@ -119,6 +126,7 @@ void KeywordHintView::SetKeyword(const base::string16& keyword) {
                       trailing_label_->text());
   }
 
+  // Fire an accessibility alert event, causing the hint to be spoken.
   NotifyAccessibilityEvent(ax::mojom::Event::kAlert, true);
 }
 
