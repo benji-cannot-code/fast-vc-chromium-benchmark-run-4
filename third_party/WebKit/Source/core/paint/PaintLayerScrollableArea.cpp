ad193d148ce4695464b40d4aff9101557af3861e
Chris Harrelson <chrishtr@chromium.org>
2018-02-26 02:19:59 +0000

    [SPv175] Don't squash across a clip-path or mask boundary.
    Otherwise a clip-path or mask will incorrectly apply to the squashed layers.
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I1b22732f2d4cb47bcd0d0869c5c901bd9141a639
    Reviewed-on: https://chromium-review.googlesource.com/935822
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539058}

--- third_party/WebKit/Source/core/paint/PaintLayerScrollableArea.cpp
+++ third_party/WebKit/Source/core/paint/PaintLayerScrollableArea.cpp
@@ -2209,7 +2209,7 @@ bool PaintLayerScrollableArea::ComputeNeedsCompositedScrolling(
   }
 
   if (layer->GetLayoutObject().HasClip() ||
-      layer->HasDescendantWithClipPath() || layer->HasAncestorWithClipPath()) {
+      layer->HasDescendantWithClipPath() || !!layer->ClipPathAncestor()) {
     non_composited_main_thread_scrolling_reasons_ |=
         MainThreadScrollingReason::kHasClipRelatedProperty;
     needs_composited_scrolling = false;
