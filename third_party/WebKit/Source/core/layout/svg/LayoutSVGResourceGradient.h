c25844be9815d9125e22b63d67417f4b8175fe45
Fredrik Söderquist <fs@opera.com>
2018-02-26 18:38:03 +0000

    [CI] Reduce LayoutSVGResourcePaintServer dependencies on LayoutObject
    In preparation for passing/using an SVGResourceClient as the 'client',
    make sure the LayoutObject& is only used as a cache key by extracting
    and passing the object bounding box as a separate argument.
    
    Bug: 769774
    Change-Id: I673ef74f2e65d645037a65d85e75846f37d3859b
    Reviewed-on: https://chromium-review.googlesource.com/931465
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#539206}

--- third_party/WebKit/Source/core/layout/svg/LayoutSVGResourceGradient.h
+++ third_party/WebKit/Source/core/layout/svg/LayoutSVGResourceGradient.h
@@ -48,7 +48,8 @@ class LayoutSVGResourceGradient : public LayoutSVGResourcePaintServer {
   void RemoveClientFromCache(LayoutObject&,
                              bool mark_for_invalidation = true) final;
 
-  SVGPaintServer PreparePaintServer(const LayoutObject&) final;
+  SVGPaintServer PreparePaintServer(const LayoutObject&,
+                                    const FloatRect& object_bounding_box) final;
 
   bool IsChildAllowed(LayoutObject* child, const ComputedStyle&) const final;
 
