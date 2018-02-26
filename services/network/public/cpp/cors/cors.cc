72bb90f89668f31428fe4cea15e889db4da4ef3a
Takashi Toyoshima <toyoshim@chromium.org>
2018-02-26 08:52:53 +0000

    OOR-CORS: introduce cors::PreflightController
    This patch introduces cors::PreflightController that will handle
    URLLoader interfaces to make CORS-preflight request and response for
    CORS-preflight enabled request from CORSURLLoader in the near future.
    
    This will also own a unified CORS-preflight cache.
    
    The first patch implements a function that takes an original
    ResourceRequest and creates a ResourceRequest for a CORS-preflight
    request.
    
    Bug: 803766
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: Iad50a2d297e445bacca8c3fdb364b942d237b5aa
    Reviewed-on: https://chromium-review.googlesource.com/923907
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Takeshi Yoshino <tyoshino@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539089}

--- services/network/public/cpp/cors/cors.cc
+++ services/network/public/cpp/cors/cors.cc
@@ -52,6 +52,9 @@ namespace header_names {
 const char kAccessControlAllowCredentials[] =
     "Access-Control-Allow-Credentials";
 const char kAccessControlAllowOrigin[] = "Access-Control-Allow-Origin";
+const char kAccessControlRequestExternal[] = "Access-Control-Request-External";
+const char kAccessControlRequestHeaders[] = "Access-Control-Request-Headers";
+const char kAccessControlRequestMethod[] = "Access-Control-Request-Method";
 
 }  // namespace header_names
 
