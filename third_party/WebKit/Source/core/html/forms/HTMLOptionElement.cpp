05c05341647fbc6a01fdf470c5ba8d26fa2a9f05
Henrik Boström <hbos@chromium.org>
2018-02-26 14:56:27 +0000

    Revert "Use StyleForLayoutObject for option to update stacking context properly."
    This reverts commit 4b96f800bd6d6c017ac14ab19e7b341c1a6efcc6.
    
    Reason for revert: Suspected for being culprit of these consistent failures:
    https://uberchromegw.corp.google.com/i/chromium.webkit/builders/WebKit%20Android%20%28Nexus4%29/builds/74755
    
    Original change's description:
    > Use StyleForLayoutObject for option to update stacking context properly.
    > 
    > We used OriginalStyleForLayoutObject which doesn't update animations or
    > stacking context. Hopefully a better fix for 813348.
    > 
    > Bug: 813348, 813439, 813836
    > Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    > Change-Id: I93584261a935c4813f618a9291d384a5b1203ec7
    > Reviewed-on: https://chromium-review.googlesource.com/924705
    > Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    > Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#539093}
    
    TBR=chrishtr@chromium.org,futhark@chromium.org
    
    Change-Id: Ia2651fbe12ba83c4af65174370c969652fa61c65
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 813348, 813439, 813836
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Reviewed-on: https://chromium-review.googlesource.com/937621
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Commit-Queue: Henrik Boström <hbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539130}

--- third_party/WebKit/Source/core/html/forms/HTMLOptionElement.cpp
+++ third_party/WebKit/Source/core/html/forms/HTMLOptionElement.cpp
@@ -92,7 +92,7 @@ void HTMLOptionElement::AttachLayoutTree(AttachContext& context) {
   if (!GetNonAttachedStyle() && ParentComputedStyle()) {
     if (HTMLSelectElement* select = OwnerSelectElement())
       select->UpdateListOnLayoutObject();
-    SetNonAttachedStyle(StyleForLayoutObject());
+    SetNonAttachedStyle(OriginalStyleForLayoutObject());
   }
   HTMLElement::AttachLayoutTree(option_context);
 }
