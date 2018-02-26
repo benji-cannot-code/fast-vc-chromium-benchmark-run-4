2e59772f4a32be4275de1c838656b205db619d12
Joshua Bell <jsbell@chromium.org>
2018-02-26 20:55:32 +0000

    Teach DatabaseIdentifiers about url::Origin
    Storage APIs (DOM Storage, WebSQL, FileSystem, Indexed DB,
    Cache Storage API) use the DatabaseIdentifier type and helper
    functions as a way to map origins to pathnames on disk. Add
    overloads/additional methods to allow url::Origin to be used
    without converting at each call site.
    
    Bug: 598424
    Change-Id: I61d0ee9881ff563d1ea693b88e3a9ce3acdff027
    Reviewed-on: https://chromium-review.googlesource.com/935295
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Joshua Bell <jsbell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539264}

--- content/browser/renderer_host/web_database_host_impl.cc
+++ content/browser/renderer_host/web_database_host_impl.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "storage/common/database/database_identifier.h"
 #include "third_party/WebKit/common/quota/quota_types.mojom.h"
 #include "third_party/sqlite/sqlite3.h"
+#include "url/origin.h"
 
 using storage::DatabaseUtil;
 using storage::VfsBackend;
@@ -263,11 +264,10 @@ void WebDatabaseHostImpl::Opened(const url::Origin& origin,
     return;
   }
 
-  GURL origin_url(origin.Serialize());
-  UMA_HISTOGRAM_BOOLEAN("websql.OpenDatabase", IsOriginSecure(origin_url));
+  UMA_HISTOGRAM_BOOLEAN("websql.OpenDatabase", IsOriginSecure(origin.GetURL()));
 
   int64_t database_size = 0;
-  std::string origin_identifier(storage::GetIdentifierFromOrigin(origin_url));
+  std::string origin_identifier(storage::GetIdentifierFromOrigin(origin));
   db_tracker_->DatabaseOpened(origin_identifier, database_name,
                               database_description, estimated_size,
                               &database_size);
@@ -285,8 +285,7 @@ void WebDatabaseHostImpl::Modified(const url::Origin& origin,
     return;
   }
 
-  std::string origin_identifier(
-      storage::GetIdentifierFromOrigin(origin.GetURL()));
+  std::string origin_identifier(storage::GetIdentifierFromOrigin(origin));
   if (!database_connections_.IsDatabaseOpened(origin_identifier,
                                               database_name)) {
     mojo::ReportBadMessage("Database not opened on modify");
@@ -304,8 +303,7 @@ void WebDatabaseHostImpl::Closed(const url::Origin& origin,
     return;
   }
 
-  std::string origin_identifier(
-      storage::GetIdentifierFromOrigin(origin.GetURL()));
+  std::string origin_identifier(storage::GetIdentifierFromOrigin(origin));
   if (!database_connections_.IsDatabaseOpened(origin_identifier,
                                               database_name)) {
     mojo::ReportBadMessage("Database not opened on close");
@@ -324,8 +322,8 @@ void WebDatabaseHostImpl::HandleSqliteError(const url::Origin& origin,
     return;
   }
 
-  db_tracker_->HandleSqliteError(
-      storage::GetIdentifierFromOrigin(origin.GetURL()), database_name, error);
+  db_tracker_->HandleSqliteError(storage::GetIdentifierFromOrigin(origin),
+                                 database_name, error);
 }
 
 void WebDatabaseHostImpl::OnDatabaseSizeChanged(
@@ -338,8 +336,8 @@ void WebDatabaseHostImpl::OnDatabaseSizeChanged(
   }
 
   GetWebDatabase().UpdateSize(
-      url::Origin::Create(storage::GetOriginFromIdentifier(origin_identifier)),
-      database_name, database_size);
+      storage::GetOriginFromIdentifier(origin_identifier), database_name,
+      database_size);
 }
 
 void WebDatabaseHostImpl::OnDatabaseScheduledForDeletion(
@@ -348,8 +346,7 @@ void WebDatabaseHostImpl::OnDatabaseScheduledForDeletion(
   DCHECK(db_tracker_->task_runner()->RunsTasksInCurrentSequence());
 
   GetWebDatabase().CloseImmediately(
-      url::Origin::Create(storage::GetOriginFromIdentifier(origin_identifier)),
-      database_name);
+      storage::GetOriginFromIdentifier(origin_identifier), database_name);
 }
 
 blink::mojom::WebDatabase& WebDatabaseHostImpl::GetWebDatabase() {
