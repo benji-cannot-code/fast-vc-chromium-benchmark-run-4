0075e9d3a578f15e86fc17ae3c345439a4a9ec9b
John Z Wu <jzw@chromium.org>
2018-02-27 17:51:26 +0000

    Change CWVTranslationController dependency injection to use init.
    This will help in writing tests for CWVTranslationController.
    
    Bug: 770983
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I8f0b67e14872e654fcf276f0fba20dab4289b6b9
    Reviewed-on: https://chromium-review.googlesource.com/939045
    Commit-Queue: John Wu <jzw@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539494}

--- ios/web_view/internal/translate/web_view_translate_client.mm
+++ ios/web_view/internal/translate/web_view_translate_client.mm
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/web/public/browser_state.h"
 #import "ios/web/public/web_state/web_state.h"
 #include "ios/web_view/internal/language/web_view_language_model_factory.h"
+#import "ios/web_view/internal/language/web_view_url_language_histogram_factory.h"
 #include "ios/web_view/internal/pref_names.h"
 #import "ios/web_view/internal/translate/cwv_translation_controller_internal.h"
 #include "ios/web_view/internal/translate/web_view_translate_accept_languages_factory.h"
@@ -46,6 +47,11 @@ WebViewTranslateClient::WebViewTranslateClient(web::WebState* web_state)
                         web_state->GetNavigationManager(),
                         translate_manager_.get()) {
   web_state->AddObserver(this);
+  language::IOSLanguageDetectionTabHelper::CreateForWebState(
+      web_state, translate_driver_.CreateLanguageDetectionCallback(),
+      ios_web_view::WebViewUrlLanguageHistogramFactory::GetForBrowserState(
+          ios_web_view::WebViewBrowserState::FromBrowserState(
+              web_state->GetBrowserState())));
 }
 
 WebViewTranslateClient::~WebViewTranslateClient() = default;
