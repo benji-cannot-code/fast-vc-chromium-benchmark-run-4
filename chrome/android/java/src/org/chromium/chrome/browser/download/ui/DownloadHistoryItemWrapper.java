0eb361f23a47f89b5fc6b83aca8abffbb5d01112
Candice Sy <cmsy@google.com>
2018-02-27 00:07:23 +0000

    Use empty progress bar for pending downloads in Downloads Home
    Use an empty progress bar instead of an indeterminate progress bar for
    pending downloads in Downloads Home.
    
    Bug: 796324
    Change-Id: I9ecd0e244aec8527449e7c30af6a314b93e9fb81
    Reviewed-on: https://chromium-review.googlesource.com/933298
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Cathy Li <chili@chromium.org>
    Reviewed-by: Joy Ming <jming@chromium.org>
    Commit-Queue: Candice Sy <cmsy@google.com>
    Cr-Commit-Position: refs/heads/master@{#539312}

--- chrome/android/java/src/org/chromium/chrome/browser/download/ui/DownloadHistoryItemWrapper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/ui/DownloadHistoryItemWrapper.java
@@ -201,6 +201,9 @@ public abstract class DownloadHistoryItemWrapper extends TimedItem {
     /** @return Whether the download is currently paused. */
     abstract boolean isPaused();
 
+    /** @return Whether the download is currently pending. */
+    abstract boolean isPending();
+
     /** Called when the user wants to open the file. */
     abstract void open();
 
@@ -433,6 +436,11 @@ public abstract class DownloadHistoryItemWrapper extends TimedItem {
         }
 
         @Override
+        public boolean isPending() {
+            return DownloadUtils.isDownloadPending(mItem);
+        }
+
+        @Override
         boolean isVisibleToUser(@DownloadFilter.Type int filter) {
             if (!super.isVisibleToUser(filter)) return false;
 
@@ -618,6 +626,11 @@ public abstract class DownloadHistoryItemWrapper extends TimedItem {
         @Override
         public boolean isPaused() {
             return mItem.state == OfflineItemState.PAUSED;
+        }
+
+        @Override
+        public boolean isPending() {
+            return mItem.state == OfflineItemState.PENDING;
         }
     }
 }
