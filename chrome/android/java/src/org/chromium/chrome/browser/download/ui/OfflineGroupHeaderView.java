05c818afa2f9bf1deb254010ea27c0ce39fd5ed8
Shakti Sahu <shaktisahu@chromium.org>
2018-02-27 19:47:37 +0000

    Download Home : Disable NEW badge
    Disabling the NEW badge for M66 until exact UX is being determined.
    
    Bug: 810098
    Change-Id: If24edd22235e6d74f89100a66214c78d9db011fc
    Reviewed-on: https://chromium-review.googlesource.com/923284
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539525}

--- chrome/android/java/src/org/chromium/chrome/browser/download/ui/OfflineGroupHeaderView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/ui/OfflineGroupHeaderView.java
@@ -11,7 +11,6 @@ import android.content.res.ColorStateList;
 import android.text.format.DateUtils;
 import android.text.format.Formatter;
 import android.util.AttributeSet;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -45,7 +44,6 @@ public class OfflineGroupHeaderView
     private TextView mDescriptionTextView;
     private ImageView mExpandImage;
     private TintedImageView mIconImageView;
-    private View mNewBadgeView;
 
     public OfflineGroupHeaderView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -70,7 +68,6 @@ public class OfflineGroupHeaderView
         mIconImageView = (TintedImageView) findViewById(R.id.icon_view);
         mDescriptionTextView = (TextView) findViewById(R.id.description);
         mExpandImage = (ImageView) findViewById(R.id.expand_icon);
-        mNewBadgeView = findViewById(R.id.new_badge);
     }
 
     /**
@@ -113,7 +110,6 @@ public class OfflineGroupHeaderView
         updateExpandIcon(header.isExpanded());
         setChecked(mSelectionDelegate.isHeaderSelected(header));
         updateCheckIcon(isChecked());
-        mNewBadgeView.setVisibility(header.shouldShowRecentBadge() ? View.VISIBLE : View.GONE);
     }
 
     private void updateExpandIcon(boolean expanded) {
