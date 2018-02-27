6604d86e68e0de650583d5f4493f95320cdb79f5
Greg Thompson <grt@chromium.org>
2018-02-27 16:22:52 +0000

    Relaunch recommended bubble.
    This bubble is shown to a user when a relaunch is needed for an update
    and the user's administrator has set the RelaunchNotification policy to
    1 (recommended). The bubble is shown each time the UpgradeDetector moves
    the UpgradeNotificationAnnoyanceLevel to LOW, ELEVATED, or HIGH; and is
    shown on a recurring schedule while the level is at or above HIGH.
    
    BUG=511300
    
    Change-Id: Idd235ec0a3bef3b24484ab5cd40cb7b2a28bf450
    Reviewed-on: https://chromium-review.googlesource.com/906774
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539447}

--- chrome/browser/ui/views/chrome_browser_main_extra_parts_views.cc
+++ chrome/browser/ui/views/chrome_browser_main_extra_parts_views.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/command_line.h"
+#include "build/build_config.h"
 #include "chrome/browser/ui/views/chrome_constrained_window_views_client.h"
 #include "chrome/browser/ui/views/chrome_views_delegate.h"
 #include "chrome/browser/ui/views/harmony/chrome_layout_provider.h"
