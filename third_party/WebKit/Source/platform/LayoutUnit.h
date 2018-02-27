1ddd04a6a515030156e637b697589411651bce52
Emil A Eklund <eae@chromium.org>
2018-02-27 19:19:00 +0000

    Prevent overflow in LayoutUnit operator++
    Bug: 772263
    Test: Source/platform/LayoutUnitTest.cpp
    Change-Id: I470b0dd4ce654c777d178dba077e523d3748b926
    Reviewed-on: https://chromium-review.googlesource.com/938850
    Reviewed-by: Justin Schuh <jschuh@chromium.org>
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539518}

--- third_party/WebKit/Source/platform/LayoutUnit.h
+++ third_party/WebKit/Source/platform/LayoutUnit.h
@@ -126,7 +126,7 @@ class LayoutUnit {
   operator bool() const { return value_; }
 
   LayoutUnit operator++(int) {
-    value_ += kFixedPointDenominator;
+    value_ = ClampAdd(value_, kFixedPointDenominator);
     return *this;
   }
 
