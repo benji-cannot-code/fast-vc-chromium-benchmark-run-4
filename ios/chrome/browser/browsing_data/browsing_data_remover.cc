3615671511614f95a558f70ce7cb7525912c347f
Sylvain Defresne <sdefresne@chromium.org>
2018-02-27 17:41:02 +0000

    Add BrowsingDataRemoverObserver interface
    Bug: none
    Change-Id: Ie877b9c71a1851c5f663ecb3ef194391aa6bd9be
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Reviewed-on: https://chromium-review.googlesource.com/939173
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Elodie Banel <lod@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539490}

---
+++ ios/chrome/browser/browsing_data/browsing_data_remover.cc
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/chrome/browser/browsing_data/browsing_data_remover.h"
+
+#include "ios/chrome/browser/browsing_data/browsing_data_remover_observer.h"
+
+BrowsingDataRemover::BrowsingDataRemover() = default;
+
+BrowsingDataRemover::~BrowsingDataRemover() = default;
+
+void BrowsingDataRemover::AddObserver(BrowsingDataRemoverObserver* observer) {
+  observers_.AddObserver(observer);
+}
+
+void BrowsingDataRemover::RemoveObserver(
+    BrowsingDataRemoverObserver* observer) {
+  observers_.RemoveObserver(observer);
+}
+
+void BrowsingDataRemover::NotifyBrowsingDataRemoved(
+    BrowsingDataRemoveMask mask) {
+  for (BrowsingDataRemoverObserver& observer : observers_) {
+    observer.OnBrowsingDataRemoved(this, mask);
+  }
+}
