39ed0e6c0e69f37119dc6ad64a838575fd3a8e59
Peter Beverloo <peter@chromium.org>
2018-02-27 17:17:29 +0000

    Calling PushManager.subscribe() should always hit the GCM Driver
    The PushMessagingManager currently maintains its own subscription cache
    in the Service Worker database, keeping track of the subscriptions it
    thinks are valid. This means that the PushMessagingManager assumes that
    the subscription is valid.
    
    There are cases where the underlying push service, in our case Google
    Cloud Messaging implemented through our GCM Driver, invalidates a
    subscription. We need to find out about that when it happens, so change
    the PushMessagingManager to *always* attempt to create a subscription,
    upon which we rely on the push service client to return the same
    information given the same input.
    
    The GCM Driver has its own cache, so in the vast majority of cases these
    calls won't hit the server. They will, however, on occasion.
    
    Bug: 799483
    Change-Id: I715c6f2eb8296b4512b6b7e9e31734ce9f254744
    Reviewed-on: https://chromium-review.googlesource.com/932401
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Anita Woodruff <awdf@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539473}

--- content/renderer/push_messaging/push_provider.cc
+++ content/renderer/push_messaging/push_provider.cc
@@ -29,6 +29,8 @@ const char* PushRegistrationStatusToString(
     mojom::PushRegistrationStatus status) {
   switch (status) {
     case mojom::PushRegistrationStatus::SUCCESS_FROM_PUSH_SERVICE:
+    case mojom::PushRegistrationStatus::
+        SUCCESS_NEW_SUBSCRIPTION_FROM_PUSH_SERVICE:
       return "Registration successful - from push service";
 
     case mojom::PushRegistrationStatus::NO_SERVICE_WORKER:
@@ -109,6 +111,8 @@ blink::WebPushError PushRegistrationStatusToWebPushError(
       error_type = blink::WebPushError::kErrorTypeInvalidState;
       break;
     case mojom::PushRegistrationStatus::SUCCESS_FROM_PUSH_SERVICE:
+    case mojom::PushRegistrationStatus::
+        SUCCESS_NEW_SUBSCRIPTION_FROM_PUSH_SERVICE:
     case mojom::PushRegistrationStatus::NO_SERVICE_WORKER:
     case mojom::PushRegistrationStatus::SERVICE_NOT_AVAILABLE:
     case mojom::PushRegistrationStatus::LIMIT_REACHED:
@@ -212,6 +216,8 @@ void PushProvider::DidSubscribe(
   DCHECK(callbacks);
 
   if (status == mojom::PushRegistrationStatus::SUCCESS_FROM_PUSH_SERVICE ||
+      status == mojom::PushRegistrationStatus::
+                    SUCCESS_NEW_SUBSCRIPTION_FROM_PUSH_SERVICE ||
       status == mojom::PushRegistrationStatus::SUCCESS_FROM_CACHE) {
     DCHECK(endpoint);
     DCHECK(options);
