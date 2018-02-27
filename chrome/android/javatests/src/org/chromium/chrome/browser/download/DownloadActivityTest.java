16f08b7f95ff08a52de523ed6a8e0227d07a42e1
Shakti Sahu <shaktisahu@chromium.org>
2018-02-27 19:46:16 +0000

    Download home : Fixing back button on tablets
    This CL tries to fix the back navigation which was broken after
    introduction of "?showPrefetchedContent=true" query param in the
    Download Home URL. This causes duplicate entries in navigation
    history and tablets get stuck in a loop and cannot come out of
    download page.
    
    In this CL we are removing this query param and sending the
    boolean through and intent extra instead. This is only intended
    for phone as tablets will not have the animated expansion.
    
    Bug: 813023
    Change-Id: I79f5773209a6b9fecd6c37ca9e9a389f447de647
    Reviewed-on: https://chromium-review.googlesource.com/927829
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539523}

--- chrome/android/javatests/src/org/chromium/chrome/browser/download/DownloadActivityTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/download/DownloadActivityTest.java
@@ -33,7 +33,6 @@ import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.chrome.R;
-import org.chromium.chrome.browser.UrlConstants;
 import org.chromium.chrome.browser.download.ui.DownloadHistoryAdapter;
 import org.chromium.chrome.browser.download.ui.DownloadHistoryItemViewHolder;
 import org.chromium.chrome.browser.download.ui.DownloadHistoryItemWrapper;
@@ -722,7 +721,6 @@ public class DownloadActivityTest {
         // Start the activity up.
         Intent intent = new Intent();
         intent.setClass(InstrumentationRegistry.getTargetContext(), DownloadActivity.class);
-        intent.putExtra(DownloadUtils.EXTRA_DOWNLOAD_HOME_URL, UrlConstants.DOWNLOADS_URL);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         return mActivityTestRule.launchActivity(intent);
     }
