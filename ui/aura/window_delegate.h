4c4a6569fa413d85ef4697d0ca5e4cf1541d9de2
Francois Doray <fdoray@chromium.org>
2018-02-27 17:24:03 +0000

    Add aura::Window::OcclusionState:HIDDEN.
    This CL adds an occlusion state to allow delegates to be notified when
    a window goest from hidden <-> occluded.
    
    Bug: 668690
    Change-Id: I3b16a699ab5d8e6050332e5146007766b0c3949f
    Reviewed-on: https://chromium-review.googlesource.com/927205
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539479}

--- ui/aura/window_delegate.h
+++ ui/aura/window_delegate.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/memory/ref_counted.h"
 #include "ui/aura/aura_export.h"
+#include "ui/aura/window.h"
 #include "ui/events/event_constants.h"
 #include "ui/events/event_handler.h"
 #include "ui/gfx/native_widget_types.h"
@@ -91,9 +92,10 @@ class AURA_EXPORT WindowDelegate : public ui::EventHandler {
   virtual void OnWindowTargetVisibilityChanged(bool visible) = 0;
 
   // Called when the occlusion state of the Window changes while tracked (see
-  // WindowOcclusionTracker::Track). |is_occluded| indicates whether the Window
-  // is occluded. Impls must not change any aura::Window.
-  virtual void OnWindowOcclusionChanged(bool is_occluded) {}
+  // WindowOcclusionTracker::Track). |occlusion_state| is the new occlusion
+  // state of the Window.
+  virtual void OnWindowOcclusionChanged(
+      Window::OcclusionState occlusion_state) {}
 
   // Called from Window::HitTest to check if the window has a custom hit test
   // mask. It works similar to the views counterparts. That is, if the function
