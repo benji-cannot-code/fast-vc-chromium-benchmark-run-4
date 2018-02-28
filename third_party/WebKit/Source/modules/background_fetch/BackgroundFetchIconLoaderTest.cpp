deeafc801044ae330fe76027069cf9c651776109
Jinho Bang <jinho.bang@samsung.com>
2018-02-28 03:36:08 +0000

    Jumbo: Fix a build error
    A build error occurs while building webkit_unit_tests with jumbo build.
    It has occured since https://chromium-review.googlesource.com/932121.
    
    Perhaps Jumbo build doesn't seem to distinguish anonymous namespaces.
    So, there is name conflicts during merging two tests. Basically, we need
    to improve the logic of the jumbo build, but this is breaking build
    currently. So, makes the variables have unique names temporarily to
    avoid name conflicts.
    
    Bug: none
    Change-Id: Ie4a62b80dfe988639333f1a8c9081c06a9f8bc29
    Reviewed-on: https://chromium-review.googlesource.com/940641
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Jinho Bang <jinho.bang@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#539692}

--- third_party/WebKit/Source/modules/background_fetch/BackgroundFetchIconLoaderTest.cpp
+++ third_party/WebKit/Source/modules/background_fetch/BackgroundFetchIconLoaderTest.cpp
@@ -18,11 +18,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 namespace {
 
-enum class LoadState { kNotLoaded, kLoadFailed, kLoadSuccessful };
+enum class BackgroundFetchLoadState {
+  kNotLoaded,
+  kLoadFailed,
+  kLoadSuccessful
+};
 
-constexpr char kImageLoaderBaseUrl[] = "http://test.com/";
-constexpr char kImageLoaderBaseDir[] = "notifications/";
-constexpr char kImageLoaderIcon500x500[] = "500x500.png";
+constexpr char kBackgroundFetchImageLoaderBaseUrl[] = "http://test.com/";
+constexpr char kBackgroundFetchImageLoaderBaseDir[] = "notifications/";
+constexpr char kBackgroundFetchImageLoaderIcon500x500[] = "500x500.png";
 
 class BackgroundFetchIconLoaderTest : public PageTestBase {
  public:
@@ -38,7 +42,8 @@ class BackgroundFetchIconLoaderTest : public PageTestBase {
   // Registers a mocked URL.
   WebURL RegisterMockedURL(const String& file_name) {
     WebURL registered_url = URLTestHelpers::RegisterMockedURLLoadFromBase(
-        kImageLoaderBaseUrl, testing::CoreTestDataPath(kImageLoaderBaseDir),
+        kBackgroundFetchImageLoaderBaseUrl,
+        testing::CoreTestDataPath(kBackgroundFetchImageLoaderBaseDir),
         file_name, "image/png");
     return registered_url;
   }
@@ -48,9 +53,9 @@ class BackgroundFetchIconLoaderTest : public PageTestBase {
   void IconLoaded(const SkBitmap& bitmap) {
     LOG(ERROR) << "did icon get loaded?";
     if (!bitmap.empty())
-      loaded_ = LoadState::kLoadSuccessful;
+      loaded_ = BackgroundFetchLoadState::kLoadSuccessful;
     else
-      loaded_ = LoadState::kLoadFailed;
+      loaded_ = BackgroundFetchLoadState::kLoadFailed;
   }
 
   void LoadIcon(const KURL& url) {
@@ -63,17 +68,17 @@ class BackgroundFetchIconLoaderTest : public PageTestBase {
 
  protected:
   ScopedTestingPlatformSupport<TestingPlatformSupport> platform_;
-  LoadState loaded_ = LoadState::kNotLoaded;
+  BackgroundFetchLoadState loaded_ = BackgroundFetchLoadState::kNotLoaded;
 
  private:
   Persistent<BackgroundFetchIconLoader> loader_;
 };
 
 TEST_F(BackgroundFetchIconLoaderTest, SuccessTest) {
-  KURL url = RegisterMockedURL(kImageLoaderIcon500x500);
+  KURL url = RegisterMockedURL(kBackgroundFetchImageLoaderIcon500x500);
   LoadIcon(url);
   platform_->GetURLLoaderMockFactory()->ServeAsynchronousRequests();
-  EXPECT_EQ(LoadState::kLoadSuccessful, loaded_);
+  EXPECT_EQ(BackgroundFetchLoadState::kLoadSuccessful, loaded_);
 }
 
 }  // namespace
