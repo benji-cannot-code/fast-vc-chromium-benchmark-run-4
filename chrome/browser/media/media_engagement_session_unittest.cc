4cc943c7e5a1ae8ec829ac0995a61cf7d9dc826b
Mounir Lamouri <mlamouri@chromium.org>
2018-02-28 12:58:03 +0000

    Media Engagement: record the number of "isHigh" status changes.
    These changes are sent over with UKM data and wiped when local data are
    wiped.
    
    Bug: 814351
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I189a430e46fb2a68af34aa7d4b237adad45d781a
    Reviewed-on: https://chromium-review.googlesource.com/931471
    Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539807}

--- chrome/browser/media/media_engagement_session_unittest.cc
+++ chrome/browser/media/media_engagement_session_unittest.cc
@@ -243,7 +243,7 @@ TEST_F(MediaEngagementSessionTest, TotalPlayers) {
 }
 
 // Checks that ukm_source_id_ is set when GetUkmRecorder is called.
-TEST_F(MediaEngagementSessionTest, GetkmRecorder_SetsUkmSourceId) {
+TEST_F(MediaEngagementSessionTest, GetUkmRecorder_SetsUkmSourceId) {
   scoped_refptr<MediaEngagementSession> session = new MediaEngagementSession(
       service(), origin(), MediaEngagementSession::RestoreType::kNotRestored);
 
@@ -254,7 +254,7 @@ TEST_F(MediaEngagementSessionTest, GetkmRecorder_SetsUkmSourceId) {
 }
 
 // Checks that GetUkmRecorder() does not return nullptr.
-TEST_F(MediaEngagementSessionTest, GetkmRecorder_NotNull) {
+TEST_F(MediaEngagementSessionTest, GetUkmRecorder_NotNull) {
   scoped_refptr<MediaEngagementSession> session = new MediaEngagementSession(
       service(), origin(), MediaEngagementSession::RestoreType::kNotRestored);
 
@@ -566,6 +566,8 @@ TEST_F(MediaEngagementSessionTest, RecordUkmMetrics) {
                      ukm_entry, Entry::kPlayer_Significant_TotalName));
     EXPECT_EQ(0, *test_ukm_recorder().GetEntryMetric(
                      ukm_entry, Entry::kPlaybacks_SecondsSinceLastName));
+    EXPECT_EQ(0, *test_ukm_recorder().GetEntryMetric(
+                     ukm_entry, Entry::kEngagement_IsHigh_ChangesName));
   }
 
   SetSignificantPlaybackRecordedForSession(session.get(), false);
@@ -599,6 +601,8 @@ TEST_F(MediaEngagementSessionTest, RecordUkmMetrics) {
                      ukm_entry, Entry::kPlayer_Significant_TotalName));
     EXPECT_EQ(0, *test_ukm_recorder().GetEntryMetric(
                      ukm_entry, Entry::kPlaybacks_SecondsSinceLastName));
+    EXPECT_EQ(0, *test_ukm_recorder().GetEntryMetric(
+                     ukm_entry, Entry::kEngagement_IsHigh_ChangesName));
   }
 }
 
