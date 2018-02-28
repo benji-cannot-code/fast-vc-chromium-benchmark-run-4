11f5a7626ed2e9f1bd08c3f46ee76e8678fc11a6
Ahmed Fakhry <afakhry@chromium.org>
2018-02-28 03:44:15 +0000

    Make the Docked and Fullscreen magnifiers mutually exclusive
    This CL implements a quick way to make the two magnifiers
    mutually exclusive. The fullscreen magnifier code requires
    some involved refactoring that is planned to be done shortly
    after.
    
    BUG=709824
    TEST=ash_unittests --enable-features=Mus --gtest_filter=DockedMagnifierTest.MutuallyExclusiveMagnifiers
    
    Change-Id: If68662450df9b443e1cbe3d2a66af3409cc3d7bd
    Reviewed-on: https://chromium-review.googlesource.com/940635
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539695}

--- ash/magnifier/docked_magnifier_controller.cc
+++ ash/magnifier/docked_magnifier_controller.cc
@@ -352,6 +352,19 @@ void DockedMagnifierController::OnDisplayConfigurationChanged() {
   CenterOnPoint(GetCursorScreenPoint());
 }
 
+bool DockedMagnifierController::GetFullscreenMagnifierEnabled() const {
+  return active_user_pref_service_ &&
+         active_user_pref_service_->GetBoolean(
+             prefs::kAccessibilityScreenMagnifierEnabled);
+}
+
+void DockedMagnifierController::SetFullscreenMagnifierEnabled(bool enabled) {
+  if (active_user_pref_service_) {
+    active_user_pref_service_->SetBoolean(
+        prefs::kAccessibilityScreenMagnifierEnabled, enabled);
+  }
+}
+
 void DockedMagnifierController::FlushClientPtrForTesting() {
   client_.FlushForTesting();
 }
@@ -451,6 +464,11 @@ void DockedMagnifierController::InitFromUserPrefs() {
       prefs::kDockedMagnifierScale,
       base::BindRepeating(&DockedMagnifierController::OnScalePrefChanged,
                           base::Unretained(this)));
+  pref_change_registrar_->Add(
+      prefs::kAccessibilityScreenMagnifierEnabled,
+      base::BindRepeating(
+          &DockedMagnifierController::OnFullscreenMagnifierEnabledPrefChanged,
+          base::Unretained(this)));
 
   OnEnabledPrefChanged();
   NotifyClientWithStatusChanged();
@@ -459,6 +477,8 @@ void DockedMagnifierController::InitFromUserPrefs() {
 void DockedMagnifierController::OnEnabledPrefChanged() {
   Shell* shell = Shell::Get();
   if (GetEnabled()) {
+    // Enabling the Docked Magnifier disables the Fullscreen Magnifier.
+    SetFullscreenMagnifierEnabled(false);
     // Calling refresh will result in the creation of the magnifier viewport and
     // its associated layers.
     Refresh();
@@ -495,6 +515,12 @@ void DockedMagnifierController::OnScalePrefChanged() {
     is_minimum_point_of_interest_height_valid_ = false;
     Refresh();
   }
+}
+
+void DockedMagnifierController::OnFullscreenMagnifierEnabledPrefChanged() {
+  // Enabling the Fullscreen Magnifier disables the Docked Magnifier.
+  if (GetFullscreenMagnifierEnabled())
+    SetEnabled(false);
 }
 
 void DockedMagnifierController::Refresh() {
