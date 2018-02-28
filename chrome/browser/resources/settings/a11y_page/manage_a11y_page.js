ba5a6ef8b187822c60ebdbf5ab1ab8f5e87df734
Katie D <katie@chromium.org>
2018-02-28 01:43:07 +0000

    Add docked magnifier to the chrome://settings/manageAccessibility page.
    Makes docked magnifier and fullscreen magnifier mutually exclusive
    on the a11y settings page.
    
    Bug: 709824
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I7051f73c6ce7f5a416a5b729ac5d075651da2e0f
    Reviewed-on: https://chromium-review.googlesource.com/934973
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539636}

--- chrome/browser/resources/settings/a11y_page/manage_a11y_page.js
+++ chrome/browser/resources/settings/a11y_page/manage_a11y_page.js
@@ -79,6 +79,17 @@ Polymer({
       },
     },
 
+    /**
+     * Whether the docked magnifier flag is enabled.
+     * @private {boolean}
+     */
+    dockedMagnifierFeatureEnabled_: {
+      type: Boolean,
+      value: function() {
+        return loadTimeData.getBoolean('dockedMagnifierFeatureEnabled');
+      },
+    },
+
     /** @private */
     isGuest_: {
       type: Boolean,
