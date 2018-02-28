fac235119b0ea576fa7d3078cd76ca845ba1973c
David Tseng <dtseng@chromium.org>
2018-02-28 07:28:28 +0000

    ARC a11y: static text nodes can be clickable
    Test: in Skype, click the 'next button' text view. Verify screen transitions appropriately.
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ic3fcfa4c29091e16674a08b2707c0ec867e01b1a
    Reviewed-on: https://chromium-review.googlesource.com/938566
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Yuki Awano <yawano@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539745}

--- chrome/browser/resources/chromeos/chromevox/cvox2/background/command_handler.js
+++ chrome/browser/resources/chromeos/chromevox/cvox2/background/command_handler.js
@@ -480,6 +480,15 @@ CommandHandler.onCommand = function(command) {
     case 'forceClickOnCurrentItem':
       if (ChromeVoxState.instance.currentRange) {
         var actionNode = ChromeVoxState.instance.currentRange.start.node;
+        // Scan for a clickable, which overrides the |actionNode|.
+        var clickable = actionNode;
+        while (clickable && !clickable.clickable)
+          clickable = clickable.parent;
+        if (clickable) {
+          clickable.doDefault();
+          return false;
+        }
+
         while (actionNode.role == RoleType.INLINE_TEXT_BOX ||
                actionNode.role == RoleType.STATIC_TEXT)
           actionNode = actionNode.parent;
@@ -487,11 +496,7 @@ CommandHandler.onCommand = function(command) {
           ChromeVoxState.instance.navigateToRange(
               cursors.Range.fromNode(actionNode.inPageLinkTarget));
         } else {
-          // Scan for a clickable, which overrides the |actionNode|.
-          var clickable = actionNode;
-          while (clickable && !clickable.clickable)
-            clickable = clickable.parent;
-          clickable ? clickable.doDefault() : actionNode.doDefault();
+          actionNode.doDefault();
         }
       }
       // Skip all other processing; if focus changes, we should get an event
