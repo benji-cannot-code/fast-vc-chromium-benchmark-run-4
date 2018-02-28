f5c52eeb3932a7cfdb699e6255fcbc06ffd48b22
John Chen <johnchen@chromium.org>
2018-02-28 18:30:00 +0000

    [ChromeDriver] Fix OOPIF handling on Android
    When dispatching mouse events to an OOPIF on android, the coordinates
    need to be relative to browser windows, while on desktop they are
    relative to the frame.
    
    Change-Id: I2953e42457b4ac3395edcf4db53f2c8780047260
    Reviewed-on: https://chromium-review.googlesource.com/941401
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539894}

--- chrome/test/chromedriver/element_util.cc
+++ chrome/test/chromedriver/element_util.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/values.h"
 #include "chrome/test/chromedriver/basic_types.h"
+#include "chrome/test/chromedriver/chrome/browser_info.h"
 #include "chrome/test/chromedriver/chrome/chrome.h"
 #include "chrome/test/chromedriver/chrome/js.h"
 #include "chrome/test/chromedriver/chrome/status.h"
@@ -634,7 +635,8 @@ Status ScrollElementRegionIntoView(
   WebPoint saved_region_offset;
   for (std::list<FrameInfo>::reverse_iterator rit = session->frames.rbegin();
        rit != session->frames.rend(); ++rit) {
-    if (!has_saved_region_offset && web_view->IsOOPIF(rit->frame_id)) {
+    if (!session->chrome->GetBrowserInfo()->is_android &&
+        !has_saved_region_offset && web_view->IsOOPIF(rit->frame_id)) {
       saved_region_offset = region_offset;
       has_saved_region_offset = true;
     }
