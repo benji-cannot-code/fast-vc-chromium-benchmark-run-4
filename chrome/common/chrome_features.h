1b236250fc0bb2306bd4dc1b78e92814019c9e56
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2018-02-28 04:18:59 +0000

    desktop-pwas: Introduce experimental link capturing flag
    Currently, link capturing is enabled by the EnableDesktopPWAWindowing
    feature, but in the future, it will be disabled unless you also have
    this flag turned on.
    
    Bug: 814102
    Change-Id: Ia13f48d38d963d8a2b93b327cb9866213390cd19
    Reviewed-on: https://chromium-review.googlesource.com/936524
    Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Reviewed-by: Matt Giuca <mgiuca@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539707}

--- chrome/common/chrome_features.h
+++ chrome/common/chrome_features.h
@@ -109,6 +109,8 @@ extern const base::Feature kDesktopIOSPromotion;
 
 extern const base::Feature kDesktopPWAWindowing;
 
+extern const base::Feature kDesktopPWAsLinkCapturing;
+
 #if !defined(OS_ANDROID)
 extern const base::Feature kDoodlesOnLocalNtp;
 #endif
