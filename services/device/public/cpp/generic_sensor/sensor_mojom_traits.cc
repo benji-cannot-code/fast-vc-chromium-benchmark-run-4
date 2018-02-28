69464e7649297e1deec82ebad7c7a1f96368b2b4
Ke He <ke.he@intel.com>
2018-02-28 03:39:18 +0000

    Rename traits in //services/device.
    This change renames *_struct_traits_* to *_mojom_traits_* in
    //services/device.
    
    BUG=806965
    
    Change-Id: Ib80b7c3d8e88408712952bb497ded2551e48539e
    Reviewed-on: https://chromium-review.googlesource.com/936434
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Ke He <ke.he@intel.com>
    Cr-Commit-Position: refs/heads/master@{#539693}

--- services/device/public/cpp/generic_sensor/sensor_mojom_traits.cc
+++ services/device/public/cpp/generic_sensor/sensor_mojom_traits.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "services/device/public/cpp/generic_sensor/sensor_struct_traits.h"
+#include "services/device/public/cpp/generic_sensor/sensor_mojom_traits.h"
 
 namespace mojo {
 
--- services/device/public/cpp/generic_sensor/sensor_mojom_traits.h
+++ services/device/public/cpp/generic_sensor/sensor_mojom_traits.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef SERVICES_DEVICE_PUBLIC_CPP_GENERIC_SENSOR_SENSOR_STRUCT_TRAITS_H_
-#define SERVICES_DEVICE_PUBLIC_CPP_GENERIC_SENSOR_SENSOR_STRUCT_TRAITS_H_
+#ifndef SERVICES_DEVICE_PUBLIC_CPP_GENERIC_SENSOR_SENSOR_MOJOM_TRAITS_H_
+#define SERVICES_DEVICE_PUBLIC_CPP_GENERIC_SENSOR_SENSOR_MOJOM_TRAITS_H_
 
 #include "services/device/public/cpp/generic_sensor/platform_sensor_configuration.h"
 #include "services/device/public/mojom/sensor.mojom.h"
@@ -24,4 +24,4 @@ struct StructTraits<device::mojom::SensorConfigurationDataView,
 
 }  // namespace mojo
 
-#endif  // SERVICES_DEVICE_PUBLIC_CPP_GENERIC_SENSOR_SENSOR_STRUCT_TRAITS_H_
+#endif  // SERVICES_DEVICE_PUBLIC_CPP_GENERIC_SENSOR_SENSOR_MOJOM_TRAITS_H_
