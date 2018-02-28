e9ca001928aca31ade80c44ef377e0c943c26b13
Xianzhu Wang <wangxianzhu@chromium.org>
2018-02-28 02:31:51 +0000

    [PE] Fix EndLayer without BeginLayer in BoxPainter::PaintBoxDecorationBackgroundWithRect()
    Not the fix for crbug.com/816812, but fix the DCHECK failure encountered
    during reproduction.
    
    Bug: 816812
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I61d36f96bda6abeba3c98e7645072ec544071327
    Reviewed-on: https://chromium-review.googlesource.com/939716
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539667}

--- third_party/WebKit/Source/core/paint/BoxPainter.cpp
+++ third_party/WebKit/Source/core/paint/BoxPainter.cpp
@@ -145,6 +145,7 @@ void BoxPainter::PaintBoxDecorationBackgroundWithRect(
           BoundsForDrawingRecorder(paint_info, LayoutPoint())))
     recorder.SetKnownToBeOpaque();
 
+  bool needs_end_layer = false;
   if (!painting_overflow_contents) {
     // FIXME: Should eventually give the theme control over whether the box
     // shadow should paint, since controls could have custom shadows of their
@@ -156,8 +157,10 @@ void BoxPainter::PaintBoxDecorationBackgroundWithRect(
       FloatRoundedRect border = style.GetRoundedBorderFor(paint_rect);
       paint_info.context.ClipRoundedRect(border);
 
-      if (box_decoration_data.bleed_avoidance == kBackgroundBleedClipLayer)
+      if (box_decoration_data.bleed_avoidance == kBackgroundBleedClipLayer) {
         paint_info.context.BeginLayer();
+        needs_end_layer = true;
+      }
     }
   }
 
@@ -203,7 +206,7 @@ void BoxPainter::PaintBoxDecorationBackgroundWithRect(
     }
   }
 
-  if (box_decoration_data.bleed_avoidance == kBackgroundBleedClipLayer)
+  if (needs_end_layer)
     paint_info.context.EndLayer();
 }
 
