7e4b165d4fcaf693e25526a97c187c2030181625
Eugene But <eugenebut@google.com>
2018-02-28 00:07:55 +0000

    s/vebStateIsVisible/webStateIsVisible (fixed typo).
    Bug: None
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I3b759e02b5fa65dbdee517224e1a915e266d083c
    Reviewed-on: https://chromium-review.googlesource.com/939541
    Reviewed-by: Peter Lee <pkl@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539593}

--- ios/chrome/browser/ui/download/download_manager_coordinator.mm
+++ ios/chrome/browser/ui/download/download_manager_coordinator.mm
@@ -81,8 +81,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)downloadManagerTabHelper:(nonnull DownloadManagerTabHelper*)tabHelper
                didCreateDownload:(nonnull web::DownloadTask*)download
-               webStateIsVisible:(BOOL)vebStateIsVisible {
-  if (!vebStateIsVisible) {
+               webStateIsVisible:(BOOL)webStateIsVisible {
+  if (!webStateIsVisible) {
     // Do nothing if a background Tab requested download UI presentation.
     return;
   }
