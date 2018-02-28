3d8085067e13f3de5903d749598aa00b5446638b
tzik <tzik@chromium.org>
2018-02-28 11:25:20 +0000

    Remove an unused forward decl in //base/timer/timer.h
    Tbr: jennyz@chromium.org
    Change-Id: Iac44f0b08ffef83322e9811fe4bc65cab11ce1c1
    Reviewed-on: https://chromium-review.googlesource.com/940706
    Commit-Queue: Taiju Tsuiki <tzik@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539787}

--- chromeos/audio/cras_audio_handler.h
+++ chromeos/audio/cras_audio_handler.h
@@ -27,6 +27,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/dbus/volume_state.h"
 #include "media/base/video_facing.h"
 
+namespace base {
+class SingleThreadTaskRunner;
+}
+
 namespace chromeos {
 
 class AudioDevicesPrefHandler;
