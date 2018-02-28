d681a401573a78e98dc1fd896ead6552159177be
Khushal <khushalsagar@chromium.org>
2018-02-28 02:48:45 +0000

    Reland cc: Ensure correct scaling for non-lazy images in GPU cache.
    This reverts commit 4c1f61ac59a284ef92af29b01b4a9c561c33a56d. The test
    was using unitialized memory which broke the MSAN bots. This patch fixes
    that.
    
    TBR=ericrk@chromium.org, zmo@chromium.org
    
    Bug: 815045
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I3d15d740dceba3a6cc90f3d9374514d403164ab7
    Reviewed-on: https://chromium-review.googlesource.com/939539
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539674}

--- cc/test/skia_common.cc
+++ cc/test/skia_common.cc
@@ -108,6 +108,7 @@ PaintImage CreateAnimatedImage(const gfx::Size& size,
 PaintImage CreateBitmapImage(const gfx::Size& size) {
   SkBitmap bitmap;
   bitmap.allocN32Pixels(size.width(), size.height());
+  bitmap.eraseColor(SK_AlphaTRANSPARENT);
   return PaintImageBuilder::WithDefault()
       .set_id(PaintImage::GetNextId())
       .set_image(SkImage::MakeFromBitmap(bitmap),
