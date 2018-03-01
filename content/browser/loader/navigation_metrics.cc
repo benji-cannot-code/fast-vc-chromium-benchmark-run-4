1b380697a86635073b694e671822f4dde1f1ee3b
arthursonzogni <arthursonzogni@chromium.org>
2018-03-01 02:06:36 +0000

    Add navigations histograms in the ResourceHandler.
    Here "ResourceHandler" refers to the NavigationResourceHandler or the
    MojoAsyncResourceHandler depending on whether NavigationMojoResponse is
    enabled or not.
    
    It adds two histograms:
    * Navigation.ResourceHandler.ResponseStartedUntilProceedWithResponse
    * Navigation.ResourceHandder.ProceedWithResponseUntilStartLoadingResponseBody.
    
    It records the time delta between these 3 events.
    1) OnResponseStarted().
    2) ProceedWithResponse().
    3) OnReadCompleted() is called for the first time.
    
    Bug: 705744
    Change-Id: I10e26016df13b9f9a7c0687774986f8775ce59c3
    Reviewed-on: https://chromium-review.googlesource.com/937516
    Commit-Queue: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539982}

---
+++ content/browser/loader/navigation_metrics.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/loader/navigation_metrics.h"
+
+#include "base/metrics/histogram_macros.h"
+
+namespace content {
+
+void RecordNavigationResourceHandlerMetrics(
+    base::TimeTicks response_started,
+    base::TimeTicks proceed_with_response,
+    base::TimeTicks first_read_completed) {
+  UMA_HISTOGRAM_TIMES(
+      "Navigation.ResourceHandler."
+      "ResponseStartedUntilProceedWithResponse",
+      proceed_with_response - response_started);
+  UMA_HISTOGRAM_TIMES(
+      "Navigation.ResourceHandler."
+      "ProceedWithResponseUntilFirstReadCompleted",
+      first_read_completed - proceed_with_response);
+}
+
+}  // namespace content
--- content/browser/loader/navigation_resource_handler.h
+++ content/browser/loader/navigation_resource_handler.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
+#include "base/time/time.h"
 #include "content/browser/loader/layered_resource_handler.h"
 #include "content/public/browser/stream_handle.h"
 
@@ -47,6 +48,8 @@ class NavigationResourceHandler : public LayeredResourceHandler {
   void OnResponseStarted(
       network::ResourceResponse* response,
       std::unique_ptr<ResourceController> controller) override;
+  void OnReadCompleted(int bytes_read,
+                       std::unique_ptr<ResourceController> controller) override;
   void OnResponseCompleted(
       const net::URLRequestStatus& status,
       std::unique_ptr<ResourceController> controller) override;
@@ -61,6 +64,11 @@ class NavigationResourceHandler : public LayeredResourceHandler {
   // checks to execute.
   scoped_refptr<network::ResourceResponse> response_;
   std::unique_ptr<net::RedirectInfo> redirect_info_;
+
+  // Used for UMA histograms.
+  bool has_completed_one_read_ = false;
+  base::TimeTicks time_response_started_;
+  base::TimeTicks time_proceed_with_response_;
 
   // NavigationResourceHandler has joint ownership of the
   // NavigationURLLoaderImplCore with the NavigationURLLoaderImpl.
