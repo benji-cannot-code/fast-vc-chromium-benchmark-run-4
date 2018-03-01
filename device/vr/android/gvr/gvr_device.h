ac3f7e93e5cfd9d79566afbd8c95ff4b37e39e8b
Michael Thiessen <mthiesse@chromium.org>
2018-03-01 23:50:00 +0000

    VR: Fix WebVR magic window poses with rotated device.
    Starting with gvr 1.13, GVR ignores the screen rotation and always
    delivers poses as though the phone was in portrait orientation.
    This CL applies the proper rotation to gvr-provided poses to account
    for the screen rotation.
    
    Bug: 817889
    Change-Id: Ie705046439775b2a34d30b177db6191414acaa10
    Reviewed-on: https://chromium-review.googlesource.com/944006
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Bill Orr <billorr@chromium.org>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540353}

--- device/vr/android/gvr/gvr_device.h
+++ device/vr/android/gvr/gvr_device.h
@@ -39,6 +39,9 @@ class DEVICE_VR_EXPORT GvrDevice : public VRDeviceBase {
 
   void OnDIPScaleChanged(JNIEnv* env,
                          const base::android::JavaRef<jobject>& obj);
+  void OnRotationChanged(JNIEnv* env,
+                         const base::android::JavaRef<jobject>& obj,
+                         jint rotation_degrees);
 
   void Activate(mojom::VRDisplayEventReason reason,
                 base::Callback<void(bool)> on_handled);
@@ -60,6 +63,7 @@ class DEVICE_VR_EXPORT GvrDevice : public VRDeviceBase {
 
   base::android::ScopedJavaGlobalRef<jobject> non_presenting_context_;
   std::unique_ptr<gvr::GvrApi> gvr_api_;
+  int rotation_degrees_;
 
   base::WeakPtrFactory<GvrDevice> weak_ptr_factory_;
 
