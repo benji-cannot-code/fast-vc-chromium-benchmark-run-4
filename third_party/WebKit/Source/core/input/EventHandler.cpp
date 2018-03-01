ce9d080ad55c27aa133db9a47748db07604499e7
Navid Zolghadr <nzolghadr@chromium.org>
2018-03-01 23:10:48 +0000

    Remove WebMouseEvent from PointerEventFactory APIs
    Fully migrate PointerEventFactory to WebPointerEvent
    by removing the last API that was expecting
    WebMouseEvent.
    
    Bug: 814471
    Change-Id: Id4bebab1eefae842f7e368e0e2b328127f65e43f
    Reviewed-on: https://chromium-review.googlesource.com/929861
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Ella Ge <eirage@chromium.org>
    Reviewed-by: Mustaq Ahmed <mustaq@chromium.org>
    Commit-Queue: Navid Zolghadr <nzolghadr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540331}

--- third_party/WebKit/Source/core/input/EventHandler.cpp
+++ third_party/WebKit/Source/core/input/EventHandler.cpp
@@ -659,7 +659,7 @@ WebInputEventResult EventHandler::HandleMousePressEvent(
     frame_->Selection().SetCaretBlinkingSuspended(true);
 
   WebInputEventResult event_result = DispatchMousePointerEvent(
-      EventTypeNames::mousedown, mev.InnerNode(), mev.CanvasRegionId(),
+      WebInputEvent::kPointerDown, mev.InnerNode(), mev.CanvasRegionId(),
       mev.Event(), Vector<WebMouseEvent>());
 
   if (event_result == WebInputEventResult::kNotHandled && frame_->View()) {
@@ -823,7 +823,7 @@ WebInputEventResult EventHandler::HandleMouseMoveOrLeaveEvent(
   }
 
   if (frame_set_being_resized_) {
-    return DispatchMousePointerEvent(EventTypeNames::mousemove,
+    return DispatchMousePointerEvent(WebInputEvent::kPointerMove,
                                      frame_set_being_resized_.Get(), String(),
                                      mouse_event, coalesced_events);
   }
@@ -934,7 +934,7 @@ WebInputEventResult EventHandler::HandleMouseMoveOrLeaveEvent(
     return event_result;
 
   event_result = DispatchMousePointerEvent(
-      EventTypeNames::mousemove, mev.InnerNode(), mev.CanvasRegionId(),
+      WebInputEvent::kPointerMove, mev.InnerNode(), mev.CanvasRegionId(),
       mev.Event(), coalesced_events);
   if (event_result != WebInputEventResult::kNotHandled)
     return event_result;
@@ -978,7 +978,7 @@ WebInputEventResult EventHandler::HandleMouseReleaseEvent(
     mouse_event_manager_->InvalidateClick();
     last_scrollbar_under_mouse_->MouseUp(mouse_event);
     return DispatchMousePointerEvent(
-        EventTypeNames::mouseup, mouse_event_manager_->GetNodeUnderMouse(),
+        WebInputEvent::kPointerUp, mouse_event_manager_->GetNodeUnderMouse(),
         String(), mouse_event, Vector<WebMouseEvent>());
   }
 
@@ -1017,7 +1017,7 @@ WebInputEventResult EventHandler::HandleMouseReleaseEvent(
   }
 
   WebInputEventResult event_result = DispatchMousePointerEvent(
-      EventTypeNames::mouseup, mev.InnerNode(), mev.CanvasRegionId(),
+      WebInputEvent::kPointerUp, mev.InnerNode(), mev.CanvasRegionId(),
       mev.Event(), Vector<WebMouseEvent>());
 
   WebInputEventResult click_event_result =
@@ -1272,18 +1272,14 @@ void EventHandler::ElementRemoved(EventTarget* target) {
 }
 
 WebInputEventResult EventHandler::DispatchMousePointerEvent(
-    const AtomicString& mouse_event_type,
+    const WebInputEvent::Type event_type,
     Node* target_node,
     const String& canvas_region_id,
     const WebMouseEvent& mouse_event,
     const Vector<WebMouseEvent>& coalesced_events) {
-  DCHECK(mouse_event_type == EventTypeNames::mousedown ||
-         mouse_event_type == EventTypeNames::mousemove ||
-         mouse_event_type == EventTypeNames::mouseup);
-
   const auto& event_result = pointer_event_manager_->SendMousePointerEvent(
-      EffectiveMouseEventTargetNode(target_node), canvas_region_id,
-      mouse_event_type, mouse_event, coalesced_events);
+      EffectiveMouseEventTargetNode(target_node), canvas_region_id, event_type,
+      mouse_event, coalesced_events);
   return event_result;
 }
 
--- third_party/WebKit/Source/core/input/PointerEventManager.h
+++ third_party/WebKit/Source/core/input/PointerEventManager.h
@@ -46,7 +46,7 @@ class CORE_EXPORT PointerEventManager
   WebInputEventResult SendMousePointerEvent(
       Node* target,
       const String& canvas_region_id,
-      const AtomicString& type,
+      const WebInputEvent::Type,
       const WebMouseEvent&,
       const Vector<WebMouseEvent>& coalesced_events);
 
