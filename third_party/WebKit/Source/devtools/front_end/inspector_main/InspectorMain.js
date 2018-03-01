fa0621d74cafb77402c5fa4d64b8132811cd3ba1
Charles Harrison <csharrison@chromium.org>
2018-03-01 16:51:57 +0000

    [subresource_filter] Fix devtools toggle with site isolation
    Multiple PageHandlers can be created with isolating subframes. This
    breaks the devtools toggle which expects a single PageHandler for the
    main frame.
    
    This CL adds a bit to the PageHandler which classifies whether it is
    for the main frame. If it is not, then it cannot change the filtering
    toggle.
    
    Bug: 813197
    Change-Id: I7698985822f5ee159399bd1d37884436c5278d0b
    Reviewed-on: https://chromium-review.googlesource.com/939602
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540185}

--- third_party/WebKit/Source/devtools/front_end/inspector_main/InspectorMain.js
+++ third_party/WebKit/Source/devtools/front_end/inspector_main/InspectorMain.js
@@ -213,7 +213,8 @@ InspectorMain.BackendSettingsSync = class {
    * @param {!SDK.Target} target
    */
   _updateTarget(target) {
-    target.pageAgent().setAdBlockingEnabled(this._adBlockEnabledSetting.get());
+    if (!target.parentTarget())
+      target.pageAgent().setAdBlockingEnabled(this._adBlockEnabledSetting.get());
   }
 
   _updateAutoAttach() {
