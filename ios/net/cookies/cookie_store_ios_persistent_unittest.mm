cebba0a2a411f43d584364d8e1e7bd1c1c1510a9
Victor Costan <pwnall@chromium.org>
2018-03-01 22:48:53 +0000

    Cookie Store: Expand CookieChangeDispatcher's unit tests.
    Bug: 729800
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I57c1ec0fbb6ec96164376d63c3cb5cfc30b5a610
    Reviewed-on: https://chromium-review.googlesource.com/943402
    Reviewed-by: Randy Smith <rdsmith@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540323}

--- ios/net/cookies/cookie_store_ios_persistent_unittest.mm
+++ ios/net/cookies/cookie_store_ios_persistent_unittest.mm
@@ -56,7 +56,10 @@ INSTANTIATE_TYPED_TEST_CASE_P(InactiveCookieStoreIOS,
                               CookieStoreTest,
                               InactiveCookieStoreIOSTestTraits);
 INSTANTIATE_TYPED_TEST_CASE_P(InactiveCookieStoreIOS,
-                              CookieStoreChangeTest,
+                              CookieStoreChangeGlobalTest,
+                              InactiveCookieStoreIOSTestTraits);
+INSTANTIATE_TYPED_TEST_CASE_P(InactiveCookieStoreIOS,
+                              CookieStoreChangeNamedTest,
                               InactiveCookieStoreIOSTestTraits);
 
 namespace {
--- net/cookies/cookie_monster_unittest.cc
+++ net/cookies/cookie_monster_unittest.cc
@@ -132,7 +132,10 @@ INSTANTIATE_TYPED_TEST_CASE_P(CookieMonster,
                               CookieStoreTest,
                               CookieMonsterTestTraits);
 INSTANTIATE_TYPED_TEST_CASE_P(CookieMonster,
-                              CookieStoreChangeTest,
+                              CookieStoreChangeGlobalTest,
+                              CookieMonsterTestTraits);
+INSTANTIATE_TYPED_TEST_CASE_P(CookieMonster,
+                              CookieStoreChangeNamedTest,
                               CookieMonsterTestTraits);
 
 template <typename T>
