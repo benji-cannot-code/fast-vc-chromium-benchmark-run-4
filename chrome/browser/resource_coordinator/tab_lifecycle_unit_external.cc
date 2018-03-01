10365fd542d4cb0151d620d052abac5be50e8877
Francois Doray <fdoray@chromium.org>
2018-03-01 20:15:46 +0000

    Use LifecycleUnits for tab discarding.
    TabManager discards tabs, but also ARC processes.
    LifecycleUnit is an abstraction to help us having one
    code path for all discards.
    
    Bug: 775644
    Change-Id: I4d2e89a710afe097cc2d6222a6c156f57c774144
    Reviewed-on: https://chromium-review.googlesource.com/842306
    Commit-Queue: François Doray <fdoray@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: Sébastien Marchand <sebmarchand@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540266}

--- chrome/browser/resource_coordinator/tab_lifecycle_unit_external.cc
+++ chrome/browser/resource_coordinator/tab_lifecycle_unit_external.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/resource_coordinator/tab_lifecycle_unit_external.h"
 
+#include "base/logging.h"
 #include "chrome/browser/resource_coordinator/tab_lifecycle_unit_source.h"
 
 namespace resource_coordinator {
@@ -13,9 +14,24 @@ namespace resource_coordinator {
 TabLifecycleUnitExternal* TabLifecycleUnitExternal::FromWebContents(
     content::WebContents* web_contents) {
   TabLifecycleUnitSource* source = TabLifecycleUnitSource::GetInstance();
-  if (!source)
-    return nullptr;
+  DCHECK(source);
   return source->GetTabLifecycleUnitExternal(web_contents);
+}
+
+// static
+void TabLifecycleUnitExternal::AddTabLifecycleObserver(
+    TabLifecycleObserver* observer) {
+  TabLifecycleUnitSource* source = TabLifecycleUnitSource::GetInstance();
+  DCHECK(source);
+  source->AddTabLifecycleObserver(observer);
+}
+
+// static
+void TabLifecycleUnitExternal::RemoveTabLifecycleObserver(
+    TabLifecycleObserver* observer) {
+  TabLifecycleUnitSource* source = TabLifecycleUnitSource::GetInstance();
+  DCHECK(source);
+  source->RemoveTabLifecycleObserver(observer);
 }
 
 }  // namespace resource_coordinator
