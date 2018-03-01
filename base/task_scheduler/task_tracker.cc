0495ef521c6c31f6392ef37e2ccfaf312e33af3e
Gabriel Charette <gab@chromium.org>
2018-03-01 12:47:58 +0000

    Introduce task_scheduler.flow category.
    This extracts task_scheduler.flow from toplevel.flow. toplevel.flow has
    way too many events at the moment and is essentially unusable (results
    in massive traces which are slow to visualize).
    
    Unfortunately the tracing macros result in a static variable
    that assumes the category never changes (INTERNAL_TRACE_EVENT_GET_CATEGORY_INFO).
    As such the code in task_annotator.cc cannot be reused to log events
    to a provided category. Instead TaskAnnotator now supports nullptr to
    disable trace events and TaskScheduler adds its own trace flow events.
    
    Bonus: restricted the scope of the TRACE_EVENT_FLAG_FLOW_IN events
    such that the event "receiving the task" precedes TaskAnnotator::RunTask
    instead of overlapping with it. This will make flow arrows line up
    nicer.
    
    R=fdoray@chromium.org, primiano@chromium.org
    
    Bug: 811035
    Change-Id: I1b4042c225486df0caf6c242f1f111291b2a1101
    Reviewed-on: https://chromium-review.googlesource.com/939177
    Reviewed-by: François Doray <fdoray@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540135}

--- base/task_scheduler/task_tracker.cc
+++ base/task_scheduler/task_tracker.cc
@@ -73,6 +73,9 @@ void TaskTracingInfo::AppendAsTraceFormat(std::string* out) const {
 constexpr char kQueueFunctionName[] = "TaskScheduler PostTask";
 constexpr char kRunFunctionName[] = "TaskScheduler RunTask";
 
+constexpr char kTaskSchedulerFlowTracingCategory[] =
+    TRACE_DISABLED_BY_DEFAULT("task_scheduler.flow");
+
 HistogramBase* GetTaskLatencyHistogram(StringPiece histogram_label,
                                        StringPiece task_type_suffix) {
   DCHECK(!histogram_label.empty());
@@ -294,7 +297,14 @@ bool TaskTracker::WillPostTask(const Task& task) {
   if (task.delayed_run_time.is_null())
     subtle::NoBarrier_AtomicIncrement(&num_incomplete_undelayed_tasks_, 1);
 
-  task_annotator_.DidQueueTask(kQueueFunctionName, task);
+  {
+    TRACE_EVENT_WITH_FLOW0(
+        kTaskSchedulerFlowTracingCategory, kQueueFunctionName,
+        TRACE_ID_MANGLE(task_annotator_.GetTaskTraceID(task)),
+        TRACE_EVENT_FLAG_FLOW_OUT);
+  }
+
+  task_annotator_.DidQueueTask(nullptr, task);
 
   return true;
 }
@@ -439,7 +449,16 @@ void TaskTracker::RunOrSkipTask(Task task,
                    std::make_unique<TaskTracingInfo>(
                        task.traits, execution_mode, sequence_token));
 
-      task_annotator_.RunTask(kQueueFunctionName, &task);
+      {
+        // Put this in its own scope so it preceeds rather than overlaps with
+        // RunTask() in the trace view.
+        TRACE_EVENT_WITH_FLOW0(
+            kTaskSchedulerFlowTracingCategory, kQueueFunctionName,
+            TRACE_ID_MANGLE(task_annotator_.GetTaskTraceID(task)),
+            TRACE_EVENT_FLAG_FLOW_IN);
+      }
+
+      task_annotator_.RunTask(nullptr, &task);
     }
 
     // Make sure the arguments bound to the callback are deleted within the
