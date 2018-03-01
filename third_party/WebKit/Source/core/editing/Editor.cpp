97037db4b94d16960114549731b04b3bfaae7df9
Zhuoyu Qian <zhuoyu.qian@samsung.com>
2018-03-01 08:10:44 +0000

    Move DispatchClipboardEvent() to EditorCommand.cpp
    Editor::DispatchClipboardEvent() is only called in EditorCommand.cpp,
    Move it to make |Editor| class simpler for improving code health.
    
    Bug: 672405
    
    Signed-off-by: Zhuoyu Qian <zhuoyu.qian@samsung.com>
    Change-Id: Id7f8647cbd3eaa7d101ce46dae1f56b42b08edc0
    Reviewed-on: https://chromium-review.googlesource.com/940785
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540100}

--- third_party/WebKit/Source/core/editing/Editor.cpp
+++ third_party/WebKit/Source/core/editing/Editor.cpp
@@ -65,7 +65,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/editing/serializers/Serialization.h"
 #include "core/editing/spellcheck/SpellChecker.h"
 #include "core/event_names.h"
-#include "core/events/ClipboardEvent.h"
 #include "core/events/KeyboardEvent.h"
 #include "core/events/TextEvent.h"
 #include "core/frame/LocalFrame.h"
@@ -328,33 +327,6 @@ void Editor::DeleteSelectionWithSmartDelete(
           .Build(),
       input_type, reference_move_position)
       ->Apply();
-}
-
-bool Editor::DispatchClipboardEvent(const AtomicString& event_type,
-                                    DataTransferAccessPolicy policy,
-                                    EditorCommandSource source,
-                                    PasteMode paste_mode) {
-  Element* target = FindEventTargetForClipboardEvent(source);
-  if (!target)
-    return true;
-
-  DataTransfer* data_transfer =
-      DataTransfer::Create(DataTransfer::kCopyAndPaste, policy,
-                           policy == kDataTransferWritable
-                               ? DataObject::Create()
-                               : DataObject::CreateFromPasteboard(paste_mode));
-
-  Event* evt = ClipboardEvent::Create(event_type, data_transfer);
-  target->DispatchEvent(evt);
-  bool no_default_processing = evt->defaultPrevented();
-  if (no_default_processing && policy == kDataTransferWritable)
-    Pasteboard::GeneralPasteboard()->WriteDataObject(
-        data_transfer->GetDataObject());
-
-  // Invalidate clipboard here for security.
-  data_transfer->SetAccessPolicy(kDataTransferNumb);
-
-  return !no_default_processing;
 }
 
 void Editor::ReplaceSelectionWithFragment(DocumentFragment* fragment,
