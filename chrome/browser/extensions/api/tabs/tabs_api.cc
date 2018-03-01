4ead794f06223c65764c886f34690af9e22f90d1
Konstantin Ganenko <ganenkokb@yandex-team.ru>
2018-03-01 02:26:54 +0000

    Fix captureVisibleTab function.
    Previously OnCaptureFailure could be called just after CaptureAsync call
     in case, when view actually was absent. That led to 2 calls of
     SendResponse. Extension functions are designed to receive 1 response
     on each call, so 2 calls is bad behavior.
    Now web capture clients behavior fixed.
    
    R=fsamuel@chromium.org, tbarzic@chromium.org
    
    Change-Id: I2ce195aaa3698d9c38ca2fa0e3735e6f9555f873
    Reviewed-on: https://chromium-review.googlesource.com/930762
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539994}

--- chrome/browser/extensions/api/tabs/tabs_api.cc
+++ chrome/browser/extensions/api/tabs/tabs_api.cc
@@ -1716,10 +1716,9 @@ bool TabsCaptureVisibleTabFunction::HasPermission() {
   return true;
 }
 
-bool TabsCaptureVisibleTabFunction::IsScreenshotEnabled() {
+bool TabsCaptureVisibleTabFunction::IsScreenshotEnabled() const {
   PrefService* service = chrome_details_.GetProfile()->GetPrefs();
   if (service->GetBoolean(prefs::kDisableScreenshots)) {
-    error_ = keys::kScreenshotsDisabled;
     return false;
   }
   return true;
@@ -1764,10 +1763,14 @@ bool TabsCaptureVisibleTabFunction::RunAsync() {
 
   WebContents* contents = GetWebContentsForID(context_id);
 
-  return CaptureAsync(
+  const CaptureResult capture_result = CaptureAsync(
       contents, image_details.get(),
       base::BindOnce(&TabsCaptureVisibleTabFunction::CopyFromSurfaceComplete,
                      this));
+  if (capture_result == OK)
+    return true;
+  SetErrorMessage(capture_result);
+  return false;
 }
 
 void TabsCaptureVisibleTabFunction::OnCaptureSuccess(const SkBitmap& bitmap) {
@@ -1781,9 +1784,14 @@ void TabsCaptureVisibleTabFunction::OnCaptureSuccess(const SkBitmap& bitmap) {
   SendResponse(true);
 }
 
-void TabsCaptureVisibleTabFunction::OnCaptureFailure(FailureReason reason) {
+void TabsCaptureVisibleTabFunction::OnCaptureFailure(CaptureResult result) {
+  SetErrorMessage(result);
+  SendResponse(false);
+}
+
+void TabsCaptureVisibleTabFunction::SetErrorMessage(CaptureResult result) {
   const char* reason_description = "internal error";
-  switch (reason) {
+  switch (result) {
     case FAILURE_REASON_READBACK_FAILED:
       reason_description = "image readback failed";
       break;
@@ -1793,10 +1801,16 @@ void TabsCaptureVisibleTabFunction::OnCaptureFailure(FailureReason reason) {
     case FAILURE_REASON_VIEW_INVISIBLE:
       reason_description = "view is invisible";
       break;
+    case FAILURE_REASON_SCREEN_SHOTS_DISABLED:
+      error_ = keys::kScreenshotsDisabled;
+      return;
+    case OK:
+      NOTREACHED()
+          << "SetErrorMessage should not be called with a successful result";
+      return;
   }
   error_ = ErrorUtils::FormatErrorMessage("Failed to capture tab: *",
                                           reason_description);
-  SendResponse(false);
 }
 
 void TabsCaptureVisibleTabFunction::RegisterProfilePrefs(
---
+++ chrome/test/data/extensions/api_test/tabs/capture_visible_tab_null_window/background.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// API test for chrome.tabs.captureVisibleTab(), when current window is null
+// browser_tests.exe --gtest_filter=ExtensionApiTest.CaptureNullWindow
+
+var fail = chrome.test.callbackFail;
+
+chrome.test.runTests([function captureNullWindow() {
+  // Create a new window so we don't close the only active window.
+  chrome.windows.create(function(newWindow) {
+    chrome.windows.remove(newWindow.id);
+    chrome.tabs.captureVisibleTab(
+        newWindow.id, fail('Failed to capture tab: view is invisible'));
+  });
+}]);
