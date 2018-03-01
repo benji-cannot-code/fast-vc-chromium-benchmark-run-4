e7fcb552a3856870779af3484d6998b9bb08814e
gogerald <gogerald@google.com>
2018-03-01 01:50:46 +0000

    [Payments] Use url.mojom.Url for stricter type check
    Bug: 812297
    Change-Id: Ifb855b4e52ed3fde171569a017fa856cad5cc2dd
    Reviewed-on: https://chromium-review.googlesource.com/940324
    Commit-Queue: Ganggui Tang <gogerald@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539979}

--- content/browser/payments/payment_manager.cc
+++ content/browser/payments/payment_manager.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/service_worker/service_worker_context_wrapper.h"
 #include "content/browser/service_worker/service_worker_registration.h"
 #include "content/public/browser/browser_thread.h"
+#include "url/origin.h"
 
 namespace content {
 
@@ -35,12 +36,26 @@ PaymentManager::PaymentManager(
       &PaymentManager::OnConnectionError, base::Unretained(this)));
 }
 
-void PaymentManager::Init(const std::string& context,
-                          const std::string& scope) {
+void PaymentManager::Init(const GURL& context_url, const std::string& scope) {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
+
   should_set_payment_app_info_ = true;
-  context_ = GURL(context);
+  context_url_ = context_url;
   scope_ = GURL(scope);
+
+  if (!context_url_.is_valid()) {
+    binding_.CloseWithReason(0U, "Invalid context URL.");
+    return;
+  }
+  if (!scope_.is_valid()) {
+    binding_.CloseWithReason(1U, "Invalid scope URL.");
+    return;
+  }
+  if (!url::IsSameOriginWith(context_url_, scope_)) {
+    binding_.CloseWithReason(
+        2U, "Scope URL is not from the same origin of the context URL.");
+    return;
+  }
 }
 
 void PaymentManager::DeletePaymentInstrument(
@@ -108,7 +123,7 @@ void PaymentManager::SetPaymentInstrumentIntermediateCallback(
   }
 
   payment_app_context_->payment_app_database()->FetchAndWritePaymentAppInfo(
-      context_, scope_, user_hint_, std::move(callback));
+      context_url_, scope_, user_hint_, std::move(callback));
   should_set_payment_app_info_ = false;
 }
 
