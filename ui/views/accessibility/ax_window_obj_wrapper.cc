3500c07b548342f75d8d19a753f73cdc6ab65a37
David Tseng <dtseng@chromium.org>
2018-03-01 04:41:32 +0000

    Use screen bounds in AXWindowObjWrapper
    Bug: 816828
    Test: verify that touch typing works again in ChromeVox
    Change-Id: I56e273aeaf225cf38d53efcc7ffff3eca6cf5387
    Reviewed-on: https://chromium-review.googlesource.com/940814
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540054}

--- ui/views/accessibility/ax_window_obj_wrapper.cc
+++ ui/views/accessibility/ax_window_obj_wrapper.cc
@@ -19,6 +19,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace views {
 
+void FireLocationChanges(aura::Window* window) {
+  AXAuraObjCache::GetInstance()->FireEvent(
+      AXAuraObjCache::GetInstance()->GetOrCreate(window),
+      ax::mojom::Event::kLocationChanged);
+
+  Widget* widget = Widget::GetWidgetForNativeView(window);
+  if (widget) {
+    AXAuraObjCache::GetInstance()->FireEvent(
+        AXAuraObjCache::GetInstance()->GetOrCreate(widget),
+        ax::mojom::Event::kLocationChanged);
+
+    views::View* root_view = widget->GetRootView();
+    if (root_view)
+      root_view->NotifyAccessibilityEvent(ax::mojom::Event::kLocationChanged,
+                                          true);
+  }
+
+  aura::Window::Windows children = window->children();
+  for (size_t i = 0; i < children.size(); ++i)
+    FireLocationChanges(children[i]);
+}
+
 AXWindowObjWrapper::AXWindowObjWrapper(aura::Window* window)
     : window_(window),
       is_alert_(false),
@@ -70,13 +92,7 @@ void AXWindowObjWrapper::Serialize(ui::AXNodeData* out_node_data) {
                                     base::UTF16ToUTF8(window_->GetTitle()));
   if (!window_->IsVisible())
     out_node_data->AddState(ax::mojom::State::kInvisible);
-
-  out_node_data->location = gfx::RectF(window_->bounds());
-  if (window_->parent()) {
-    out_node_data->offset_container_id =
-        AXAuraObjCache::GetInstance()->GetID(window_->parent());
-  }
-
+  out_node_data->location = gfx::RectF(window_->GetBoundsInScreen());
   ui::AXTreeIDRegistry::AXTreeID child_ax_tree_id =
       window_->GetProperty(ui::kChildAXTreeID);
   if (child_ax_tree_id != ui::AXTreeIDRegistry::kNoAXTreeID) {
@@ -124,20 +140,7 @@ void AXWindowObjWrapper::OnWindowBoundsChanged(
   if (window != window_)
     return;
 
-  AXAuraObjCache::GetInstance()->FireEvent(this,
-                                           ax::mojom::Event::kLocationChanged);
-
-  Widget* widget = Widget::GetWidgetForNativeView(window);
-  if (widget) {
-    AXAuraObjCache::GetInstance()->FireEvent(
-        AXAuraObjCache::GetInstance()->GetOrCreate(widget),
-        ax::mojom::Event::kLocationChanged);
-
-    views::View* root_view = widget->GetRootView();
-    if (root_view)
-      root_view->NotifyAccessibilityEvent(ax::mojom::Event::kLocationChanged,
-                                          true);
-  }
+  FireLocationChanges(window_);
 }
 
 void AXWindowObjWrapper::OnWindowPropertyChanged(aura::Window* window,
