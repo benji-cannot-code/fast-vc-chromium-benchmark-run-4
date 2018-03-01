7d963953f5817f6afd5db90a4fac8b47976c689f
Rakina Zata Amni <rakina@chromium.org>
2018-03-01 15:01:53 +0000

    Apply constructed stylesheets CSSOM changes
    This change is the continuation of crrev.com/c/925763. With this change,
    CSSOM changes to constructed stylesheets are reflected on all TreeScopes
    where the style is applied to.
    
    Spec draft: https://wicg.github.io/construct-stylesheets/#dom-cssstylesheet-cssstylesheet
    
    Bug: 807560
    Change-Id: I1cddeea9dc346aa238fa4d0aabe233397166e2a5
    Reviewed-on: https://chromium-review.googlesource.com/938872
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540152}

--- third_party/WebKit/Source/core/css/CSSStyleSheet.cpp
+++ third_party/WebKit/Source/core/css/CSSStyleSheet.cpp
@@ -214,13 +214,19 @@ void CSSStyleSheet::DidMutateRules() {
   DCHECK_LE(contents_->ClientSize(), 1u);
 
   Document* owner = OwnerDocument();
-  if (!owner)
-    return;
-  if (ownerNode() && ownerNode()->isConnected()) {
+  if (owner && ownerNode() && ownerNode()->isConnected()) {
     owner->GetStyleEngine().SetNeedsActiveStyleUpdate(
         ownerNode()->GetTreeScope());
     if (StyleResolver* resolver = owner->GetStyleEngine().Resolver())
       resolver->InvalidateMatchedPropertiesCache();
+  } else if (!constructed_tree_scopes_.IsEmpty()) {
+    for (auto tree_scope : constructed_tree_scopes_) {
+      tree_scope->GetDocument().GetStyleEngine().SetNeedsActiveStyleUpdate(
+          *tree_scope);
+      if (StyleResolver* resolver =
+              tree_scope->GetDocument().GetStyleEngine().Resolver())
+        resolver->InvalidateMatchedPropertiesCache();
+    }
   }
 }
 
@@ -348,7 +354,6 @@ unsigned CSSStyleSheet::insertRule(const String& rule_string,
     return 0;
   }
   RuleMutationScope mutation_scope(this);
-
   bool success = contents_->WrapperInsertRule(rule, index);
   if (!success) {
     if (rule->IsNamespaceRule())
@@ -549,6 +554,7 @@ void CSSStyleSheet::Trace(blink::Visitor* visitor) {
   visitor->Trace(contents_);
   visitor->Trace(owner_node_);
   visitor->Trace(owner_rule_);
+  visitor->Trace(constructed_tree_scopes_);
   visitor->Trace(media_cssom_wrapper_);
   visitor->Trace(child_rule_cssom_wrappers_);
   visitor->Trace(rule_list_cssom_wrapper_);
--- third_party/WebKit/Source/core/css/CSSStyleSheet.h
+++ third_party/WebKit/Source/core/css/CSSStyleSheet.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/css/CSSRule.h"
 #include "core/css/MediaQueryEvaluator.h"
 #include "core/css/StyleSheet.h"
+#include "core/dom/TreeScope.h"
 #include "platform/heap/Handle.h"
 #include "platform/wtf/Noncopyable.h"
 #include "platform/wtf/text/TextEncoding.h"
@@ -127,6 +128,14 @@ class CORE_EXPORT CSSStyleSheet final : public StyleSheet {
   void SetAllowRuleAccessFromOrigin(
       scoped_refptr<const SecurityOrigin> allowed_origin);
 
+  void AddedConstructedToTreeScope(TreeScope* tree_scope) {
+    constructed_tree_scopes_.insert(tree_scope);
+  }
+
+  void RemovedConstructedFromTreeScope(TreeScope* tree_scope) {
+    constructed_tree_scopes_.erase(tree_scope);
+  }
+
   class RuleMutationScope {
     STACK_ALLOCATED();
 
@@ -200,6 +209,7 @@ class CORE_EXPORT CSSStyleSheet final : public StyleSheet {
 
   Member<Node> owner_node_;
   Member<CSSRule> owner_rule_;
+  HeapHashSet<Member<TreeScope>> constructed_tree_scopes_;
 
   TextPosition start_position_;
   Member<MediaList> media_cssom_wrapper_;
