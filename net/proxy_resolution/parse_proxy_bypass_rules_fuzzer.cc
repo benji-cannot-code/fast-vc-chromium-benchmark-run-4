f92dd42ee7c81bfce6163096ec78bae3588e1b87
Eric Roman <eroman@chromium.org>
2018-03-01 02:53:58 +0000

    Limit the size of input to parse_proxy_bypass_rules_fuzzer to 512 bytes.
    Clusterfuzz is currently sending inputs in excess of 814 KB, which results in a correct but slow execution (times out).
    
    Given the simple grammar of proxy bypass rules, 512 bytes is more than enough to explore it.
    
    Bug: 813619
    Change-Id: I1415a1a15bb2996b6698b96b9cdef856a4988110
    Reviewed-on: https://chromium-review.googlesource.com/942098
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Abhishek Arya <inferno@chromium.org>
    Commit-Queue: Eric Roman <eroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540004}

--- net/proxy_resolution/parse_proxy_bypass_rules_fuzzer.cc
+++ net/proxy_resolution/parse_proxy_bypass_rules_fuzzer.cc
@@ -10,6 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Entry point for LibFuzzer.
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  // Don't waste time parsing if the input is too large
+  // (https://crbug.com/813619). According to
+  // //testing/libfuzzer/efficient_fuzzer.md setting max_len in the build
+  // target is insufficient since AFL doesn't respect it.
+  if (size > 512)
+    return 0;
+
   net::ProxyBypassRules rules;
   std::string input(data, data + size);
   rules.ParseFromString(input);
