2d201e893e79f45093ec80ae58a123865b300c1a
Dominic Mazzoni <dmazzoni@chromium.org>
2018-03-01 07:47:52 +0000

    accLocation should return unclipped bounds.
    See bug for motivation. Adds separate AXPlatformNodeDelegate interfaces
    for clipped and unclipped for maximum code clarity.
    
    Note that we still want clipped bounds around for hit testing.
    
    Bug: 810762
    Change-Id: I1b08977e9753fb918b6a4b41f152a238caae12e3
    Reviewed-on: https://chromium-review.googlesource.com/938024
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540094}

--- ui/accessibility/platform/ax_platform_node_base.h
+++ ui/accessibility/platform/ax_platform_node_base.h
@@ -23,7 +23,6 @@ class AX_EXPORT AXPlatformNodeBase : public AXPlatformNode {
 
   // These are simple wrappers to our delegate.
   const AXNodeData& GetData() const;
-  gfx::Rect GetBoundsInScreen() const;
   gfx::NativeViewAccessible GetParent();
   int GetChildCount();
   gfx::NativeViewAccessible ChildAtIndex(int index);
--- ui/accessibility/platform/ax_platform_node_mac.mm
+++ ui/accessibility/platform/ax_platform_node_mac.mm
@@ -298,9 +298,10 @@ bool AlsoUseShowMenuActionForDefaultAction(const ui::AXNodeData& data) {
 }
 
 - (NSRect)boundsInScreen {
-  if (!node_)
+  if (!node_ || !node_->GetDelegate())
     return NSZeroRect;
-  return gfx::ScreenRectToNSRect(node_->GetBoundsInScreen());
+  return gfx::ScreenRectToNSRect(
+      node_->GetDelegate()->GetClippedScreenBoundsRect());
 }
 
 - (NSString*)getStringAttribute:(ax::mojom::StringAttribute)attribute {
