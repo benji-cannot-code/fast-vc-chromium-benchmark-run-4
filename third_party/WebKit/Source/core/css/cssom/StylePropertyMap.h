082cf10acd224b7227e281c07cb7afe05e3e1b43
Darren Shen <shend@chromium.org>
2018-03-01 02:19:16 +0000

    [css-typed-om] Implement shorthands for StylePropertyMap.set/delete.
    This patch implements shorthands for stylemap.set/delete. When we
    receive an UnsupportedShorthandProperty, we should get the CSSValues
    out of them and set them on the style map.
    
    We leave parsing of shorthand values for a future patch.
    
    Bug: 816722
    Change-Id: Iebd0b33f89b1d8872bea5d3f80d1dde71d72bc45
    Reviewed-on: https://chromium-review.googlesource.com/938684
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: nainar <nainar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539992}

--- third_party/WebKit/Source/core/css/cssom/StylePropertyMap.h
+++ third_party/WebKit/Source/core/css/cssom/StylePropertyMap.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class CSSProperty;
 class ExceptionState;
 class ExecutionContext;
 
@@ -40,6 +41,8 @@ class CORE_EXPORT StylePropertyMap : public StylePropertyMapReadOnly {
   StylePropertyMap() = default;
 
  private:
+  bool SetShorthandProperty(const CSSProperty&, const CSSStyleValue&);
+
   DISALLOW_COPY_AND_ASSIGN(StylePropertyMap);
 };
 
