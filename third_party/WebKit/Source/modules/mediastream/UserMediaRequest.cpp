9ae51722358e80a89b8d6a70f32aa9da4d89ee3f
Oskar Sundbom <ossu@chromium.org>
2018-03-01 11:45:03 +0000

    Add experimental support for the native macOS echo canceller
    This CL adds platform support for the native echo canceller available in macOS 10.12+, as well as a way to enable it through a runtime flag or Origin Trial.
    A native echo canceller has the opportunity to perform better than a general one in that it can be tuned for the hardware platform in question. In this case, it's also closer to the hardware, so it's less likely to be affected by IPC glitches.
    
    Intent to Implement discussion:
    https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/kEld5OudUOM
    
    Bug: chromium:776327
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ia828ae2b9e6503ca814de4154dee96c63b838e1d
    Reviewed-on: https://chromium-review.googlesource.com/941321
    Commit-Queue: Oskar Sundbom <ossu@chromium.org>
    Reviewed-by: Rick Byers <rbyers@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Tommi <tommi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540130}

--- third_party/WebKit/Source/modules/mediastream/UserMediaRequest.cpp
+++ third_party/WebKit/Source/modules/mediastream/UserMediaRequest.cpp
@@ -428,11 +428,18 @@ UserMediaRequest::UserMediaRequest(ExecutionContext* context,
       video_(video),
       should_disable_hardware_noise_suppression_(
           OriginTrials::disableHardwareNoiseSuppressionEnabled(context)),
+      should_enable_experimental_hw_echo_cancellation_(
+          OriginTrials::experimentalHardwareEchoCancellationEnabled(context)),
       controller_(controller),
       callbacks_(callbacks) {
   if (should_disable_hardware_noise_suppression_) {
     UseCounter::Count(context,
                       WebFeature::kUserMediaDisableHardwareNoiseSuppression);
+  }
+  if (should_enable_experimental_hw_echo_cancellation_) {
+    UseCounter::Count(
+        context,
+        WebFeature::kUserMediaEnableExperimentalHardwareEchoCancellation);
   }
 }
 
@@ -456,6 +463,11 @@ WebMediaConstraints UserMediaRequest::VideoConstraints() const {
 
 bool UserMediaRequest::ShouldDisableHardwareNoiseSuppression() const {
   return should_disable_hardware_noise_suppression_;
+}
+
+bool UserMediaRequest::ShouldEnableExperimentalHardwareEchoCancellation()
+    const {
+  return should_enable_experimental_hw_echo_cancellation_;
 }
 
 bool UserMediaRequest::IsSecureContextUse(String& error_message) {
