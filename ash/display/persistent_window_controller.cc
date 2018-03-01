d5b69f6720059ec2d2e84f4301968d741abc712c
Qiang Xu <warx@google.com>
2018-03-01 23:22:21 +0000

    cros: record nums of window restored for persistent restore
    changes:
    According to the design doc: go/cros-persistent-window-bounds, add UMA
    for recording the number of windows restored for persistent window
    restore in multi-display scenario.
    
    Bug: 805046
    Test: tested on device, checked chrome://histograms and added test coverage
    Change-Id: I4f8c815c1b88dd605a02b509b27311d567998da1
    Reviewed-on: https://chromium-review.googlesource.com/942186
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Commit-Queue: Qiang Xu <warx@google.com>
    Cr-Commit-Position: refs/heads/master@{#540339}

--- ash/display/persistent_window_controller.cc
+++ ash/display/persistent_window_controller.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/mru_window_tracker.h"
 #include "ash/wm/window_state.h"
 #include "base/command_line.h"
+#include "base/metrics/histogram_macros.h"
 #include "ui/display/manager/display_manager.h"
 #include "ui/display/screen.h"
 
@@ -50,6 +51,7 @@ void MaybeRestorePersistentWindowBounds() {
     return;
 
   display::Screen* screen = display::Screen::GetScreen();
+  int window_restored_count = 0;
   for (auto* window : GetWindowList()) {
     wm::WindowState* window_state = wm::GetWindowState(window);
     if (!window_state->persistent_window_info())
@@ -81,10 +83,20 @@ void MaybeRestorePersistentWindowBounds() {
     window->SetBoundsInScreen(persistent_window_bounds, display);
     // Reset persistent window info everytime the window bounds have restored.
     window_state->ResetPersistentWindowInfo();
+
+    ++window_restored_count;
+  }
+
+  if (window_restored_count != 0) {
+    UMA_HISTOGRAM_COUNTS_100(
+        PersistentWindowController::kNumOfWindowsRestoredHistogramName,
+        window_restored_count);
   }
 }
 
 }  // namespace
+
+constexpr char PersistentWindowController::kNumOfWindowsRestoredHistogramName[];
 
 PersistentWindowController::PersistentWindowController() {
   display::Screen::GetScreen()->AddObserver(this);
