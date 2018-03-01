47748db96ed50c2cceec87ba8d04bf676e24181b
wutao <wutao@chromium.org>
2018-03-01 21:48:07 +0000

    cros: Add user actions to KSV.
    This cl adds two user actions to Keyboard Shortcut Viewer (KSV):
    1. The times user creates a new window of Keyboard Shortcut Viewer.
    2. The times user enters search mode in Keyboard Shortcut Viewer.
    
    Bug: 768932
    Test: can see the counts in chrome://user-actions.
    Change-Id: Id04f20c755def88f6f360d3c66ac1417180d5a94
    Reviewed-on: https://chromium-review.googlesource.com/938381
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540300}

--- ash/components/shortcut_viewer/views/keyboard_shortcut_view.cc
+++ ash/components/shortcut_viewer/views/keyboard_shortcut_view.cc
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/shelf_item.h"
 #include "ash/public/cpp/window_properties.h"
 #include "base/i18n/string_search.h"
+#include "base/metrics/user_metrics.h"
+#include "base/metrics/user_metrics_action.h"
 #include "ui/aura/client/aura_constants.h"
 #include "ui/aura/window.h"
 #include "ui/base/default_style.h"
@@ -96,6 +98,9 @@ views::Widget* KeyboardShortcutView::Show(gfx::NativeWindow context) {
     // it.
     g_ksv_view->GetWidget()->Activate();
   } else {
+    base::RecordAction(
+        base::UserMetricsAction("KeyboardShortcutViewer.CreateWindow"));
+
     constexpr gfx::Size kKSVWindowSize(768, 512);
     gfx::Rect window_bounds(kKSVWindowSize);
     if (context) {
@@ -349,7 +354,10 @@ void KeyboardShortcutView::ActiveChanged(
   sender->ShowBackOrGoogleIcon(is_active);
   search_results_container_->SetVisible(is_active);
   tabbed_pane_->SetVisible(!is_active);
-  if (!is_active) {
+  if (is_active) {
+    base::RecordAction(
+        base::UserMetricsAction("KeyboardShortcutViewer.Search"));
+  } else {
     search_results_container_->RemoveAllChildViews(true);
     RequestFocusForActiveTab();
   }
