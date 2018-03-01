f9ceb06af995b74c3a9d3fe89fe4da7d17084ecf
Shu Chen <shuchen@google.com>
2018-03-01 05:24:07 +0000

    Supports JP106 keyboard for VDI web apps.
    Currently the input_method_chromeos_unittest.cc is mistakenly excluded in ui_base_unittests, by this cl:
    https://chromium-review.googlesource.com/c/chromium/src/+/632636
    So, this cl includes the tests back again and clean up the compiling errors.
    This cl also adds some unit tests in InputMethodChromeOSKeyEventTest to cover the logic for JP106 keys.
    
    This cl doesn't remove the similar logic in ash::ImeController. It will be removed after a couple of milestones.
    
    Bug: 816341
    Change-Id: Id3d8b7dd2b8a70632af21cb16220fe7fbd09e9b5
    Reviewed-on: https://chromium-review.googlesource.com/936923
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: James Su <suzhe@chromium.org>
    Commit-Queue: Shu Chen <shuchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540063}

--- ash/accelerators/accelerator_table.cc
+++ ash/accelerators/accelerator_table.cc
@@ -16,12 +16,6 @@ const int kDebugModifier =
 const AcceleratorData kAcceleratorData[] = {
     {true, ui::VKEY_SPACE, ui::EF_CONTROL_DOWN, PREVIOUS_IME},
     {false, ui::VKEY_SPACE, ui::EF_CONTROL_DOWN, PREVIOUS_IME},
-    // Shortcuts for Japanese IME.
-    {true, ui::VKEY_CONVERT, ui::EF_NONE, SWITCH_IME},
-    {true, ui::VKEY_NONCONVERT, ui::EF_NONE, SWITCH_IME},
-    {true, ui::VKEY_DBE_SBCSCHAR, ui::EF_NONE, SWITCH_IME},
-    {true, ui::VKEY_DBE_DBCSCHAR, ui::EF_NONE, SWITCH_IME},
-
     {true, ui::VKEY_TAB, ui::EF_ALT_DOWN, CYCLE_FORWARD_MRU},
     {true, ui::VKEY_TAB, ui::EF_SHIFT_DOWN | ui::EF_ALT_DOWN,
      CYCLE_BACKWARD_MRU},
@@ -338,7 +332,6 @@ const AcceleratorAction kActionsAllowedAtLoginOrLockScreen[] = {
     SCALE_UI_RESET,
     SCALE_UI_UP,
     SHOW_IME_MENU_BUBBLE,
-    SWITCH_IME,  // Switch to another IME depending on the accelerator.
     TAKE_PARTIAL_SCREENSHOT,
     TAKE_SCREENSHOT,
     TAKE_WINDOW_SCREENSHOT,
@@ -399,7 +392,6 @@ const AcceleratorAction kActionsAllowedAtModalWindow[] = {
     SHOW_KEYBOARD_OVERLAY,
     SUSPEND,
     SWAP_PRIMARY_DISPLAY,
-    SWITCH_IME,
     TAKE_PARTIAL_SCREENSHOT,
     TAKE_SCREENSHOT,
     TAKE_WINDOW_SCREENSHOT,
@@ -462,7 +454,6 @@ const AcceleratorAction kActionsAllowedInAppModeOrPinnedMode[] = {
     SCALE_UI_RESET,
     SCALE_UI_UP,
     SWAP_PRIMARY_DISPLAY,
-    SWITCH_IME,  // Switch to another IME depending on the accelerator.
     TOGGLE_CAPS_LOCK,
     TOGGLE_DICTATION,
     TOGGLE_HIGH_CONTRAST,
@@ -522,7 +513,6 @@ const AcceleratorAction kActionsKeepingMenuOpen[] = {
     NEXT_IME,
     PREVIOUS_IME,
     PRINT_UI_HIERARCHIES,
-    SWITCH_IME,
     TAKE_PARTIAL_SCREENSHOT,
     TAKE_SCREENSHOT,
     TAKE_WINDOW_SCREENSHOT,
