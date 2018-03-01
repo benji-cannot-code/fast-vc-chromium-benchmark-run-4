f27ad63ce18756cf3bec5b83eb91df6bc941c6f1
Paul Miller <paulmiller@google.com>
2018-03-01 02:52:39 +0000

    Unify Android version and channel info
    Android has 2 mechanisms for getting the channel: in Java, there's
    ChromeVersionConstants, which is generated at build-time using
    chrome/VERSION and the $android_channel GN variable. In native, there's
    channel_android.cc, which determines channel at run-time from the
    package name. Replace the native side with a JNI call to get the Java
    constant, to avoid requiring the package name.
    
    WebView needs channel and version info as well as Chrome, so move that
    info from ChromeVersionConstants into a new VersionConstants base class
    in version_info/. WebView currently adds channel info to UMA data, and
    plans to use channel and major version for downloading Finch seeds.
    
    Move Android-specific parts of version_info/ into version_info/android/.
    
    Remove remnants of the "work" channel, which were missed by commit
    0d9dc4f2c7aa6c2d41354ddc9650ebada1ca6a83.
    
    BUG=733857
    
    Change-Id: I61c952862e907ae666f910f4d386e712f6a52f2e
    Reviewed-on: https://chromium-review.googlesource.com/927212
    Commit-Queue: Paul Miller <paulmiller@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540003}

--- chrome/common/channel_info_android.cc
+++ chrome/common/channel_info_android.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/build_info.h"
 #include "base/logging.h"
 #include "base/strings/string_util.h"
-#include "components/version_info/channel_android.h"
+#include "components/version_info/android/channel_getter.h"
 #include "components/version_info/version_info.h"
 
 namespace chrome {
@@ -26,9 +26,7 @@ std::string GetChannelString() {
 }
 
 version_info::Channel GetChannel() {
-  const base::android::BuildInfo* bi = base::android::BuildInfo::GetInstance();
-  DCHECK(bi && bi->package_name());
-  return version_info::ChannelFromPackageName(bi->package_name());
+  return version_info::GetChannel();
 }
 
 }  // namespace chrome
