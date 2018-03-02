e31469897b095ae39e36aa0e7efe6932cd4b07aa
Ken MacKay <kmackay@chromium.org>
2018-03-02 22:46:48 +0000

    [Chromecast] Add public API for direct audio playback
    BUG= internal b/71559266
    
    Change-Id: Ia7d00e82bca0a50385c58b853b4cba05670e0dde
    Reviewed-on: https://chromium-review.googlesource.com/937812
    Commit-Queue: Kenneth MacKay <kmackay@chromium.org>
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540671}

--- chromecast/public/media/media_pipeline_backend.h
+++ chromecast/public/media/media_pipeline_backend.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROMECAST_PUBLIC_MEDIA_MEDIA_PIPELINE_BACKEND_H_
 
 #include <stdint.h>
+
 #include <string>
 
 #include "cast_key_status.h"
