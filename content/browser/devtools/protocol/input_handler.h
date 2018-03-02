973c7fc1c893a77403d1a74085b770ecd40ae0fc
Dmitry Gozman <dgozman@chromium.org>
2018-03-02 02:30:39 +0000

    [DevTools] Route input events to the correct RenderWidgetHost
    With OOPIFs, we route input events sent to main frame's target
    to the correct RenderWidgetHost inside.
    
    We might consider doing the same for non-main frames as well, or
    instead prohibit input domain on them.
    
    This does not touch gestures yet, those will follow in next patch.
    
    Bug: 746266
    
    Change-Id: I7ba87c6d227ea24a559ebb7b5ca6a3cf9764d0f0
    Reviewed-on: https://chromium-review.googlesource.com/939540
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540419}

--- content/browser/devtools/protocol/input_handler.h
+++ content/browser/devtools/protocol/input_handler.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/circular_deque.h"
 #include "base/containers/flat_map.h"
+#include "base/containers/flat_set.h"
+#include "base/containers/unique_ptr_adapters.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "content/browser/devtools/protocol/devtools_domain_handler.h"
@@ -24,12 +26,11 @@ class CompositorFrameMetadata;
 namespace content {
 class DevToolsAgentHostImpl;
 class RenderFrameHostImpl;
+class RenderWidgetHostImpl;
 
 namespace protocol {
 
-class InputHandler : public DevToolsDomainHandler,
-                     public Input::Backend,
-                     public RenderWidgetHost::InputEventObserver {
+class InputHandler : public DevToolsDomainHandler, public Input::Backend {
  public:
   InputHandler();
   ~InputHandler() override;
@@ -124,11 +125,7 @@ class InputHandler : public DevToolsDomainHandler,
       std::unique_ptr<SynthesizeTapGestureCallback> callback) override;
 
  private:
-  // InputEventObserver
-  void OnInputEvent(const blink::WebInputEvent& event) override;
-  void OnInputEventAck(InputEventAckSource source,
-                       InputEventAckState state,
-                       const blink::WebInputEvent& event) override;
+  class InputInjector;
 
   void SynthesizeRepeatingScroll(
       SyntheticSmoothScrollGestureParams gesture_params,
@@ -149,15 +146,13 @@ class InputHandler : public DevToolsDomainHandler,
 
   void ClearInputState();
   bool PointIsWithinContents(gfx::PointF point) const;
+  InputInjector* EnsureInjector(RenderWidgetHostImpl* widget_host);
+  RenderWidgetHostImpl* FindTargetWidgetHost(const gfx::PointF& point,
+                                             gfx::PointF* transformed);
 
   RenderFrameHostImpl* host_;
-  // Callbacks for calls to Input.dispatchKey/MouseEvent that have been sent to
-  // the renderer, but that we haven't yet received an ack for.
-  bool input_queued_;
-  base::circular_deque<std::unique_ptr<DispatchKeyEventCallback>>
-      pending_key_callbacks_;
-  base::circular_deque<std::unique_ptr<DispatchMouseEventCallback>>
-      pending_mouse_callbacks_;
+  base::flat_set<std::unique_ptr<InputInjector>, base::UniquePtrComparator>
+      injectors_;
   float page_scale_factor_;
   int last_id_;
   bool ignore_input_events_ = false;
