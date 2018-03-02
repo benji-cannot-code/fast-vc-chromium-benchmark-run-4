a3632021df9baf440dd893dce292f7e6f5cb1340
edchin <edchin@chromium.org>
2018-03-02 21:32:47 +0000

    [ios] TabGridAdaptor needs to implement setTransitionContext
    This sets a stub implementation of setTransitionContext so that tab grid
    can run on tablet. This implementation can eventually be removed when
    the iPad-specific TabSwitcher is removed.
    
    Bug: 804537
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I3b5b8e7620d67c2d02a284b320df5ce003f78023
    Reviewed-on: https://chromium-review.googlesource.com/946110
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: edchin <edchin@chromium.org>
    Commit-Queue: edchin <edchin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540636}

--- ios/chrome/browser/ui/tab_grid/tab_grid_adaptor.mm
+++ ios/chrome/browser/ui/tab_grid/tab_grid_adaptor.mm
@@ -85,4 +85,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   self.incognitoMediator.tabModel = otrModel;
 }
 
+- (void)setTransitionContext:(TabSwitcherTransitionContext*)transitionContext {
+  // No-op. Tab grid will not use this iPad TabSwitcher-specific mechanism.
+}
+
 @end
