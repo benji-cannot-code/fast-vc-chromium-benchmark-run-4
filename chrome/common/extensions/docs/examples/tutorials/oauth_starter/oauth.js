2181c245d7b8499b7057ae66799c6a38373eccc1
CrystalFaith <crystallambert@chromium.org>
2018-03-02 23:46:52 +0000

    [Extension Documentation] OAuth Tutorial Update
    This document updates the current OAuth Tutorial.
    It specifies OAuth2 with Google and the Identity API.
    Example extension will connect will request access to user's
    contacts then display contact photos in a div.
    
    
    Bug: 486948, 306867, 127559, 126010
    Change-Id: Iba3efd91ef894064398921d6bcc538e04e366398
    Reviewed-on: https://chromium-review.googlesource.com/865459
    Commit-Queue: Crystal Lambert <crystallambert@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540680}

---
+++ chrome/common/extensions/docs/examples/tutorials/oauth_starter/oauth.js
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+window.onload = function() {
+  document.querySelector('button').addEventListener('click', function() {
+    chrome.identity.getAuthToken({interactive: true}, function(token) {
+      console.log(token);
+    });
+  });
+};
