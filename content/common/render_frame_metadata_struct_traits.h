6881a2eb09a7c9c9d6086c0b40166ff7989ecf57
jonross <jonross@chromium.org>
2018-03-02 17:12:02 +0000

    Rate Limit RenderFrameMetadataObserver
    RenderFrameMetadataObserverImpl was sending a message from the renderer to the
    browser whenever any metadata changes. One such piece of data is the scroll
    offset. Due to this, when scrolling, there would be an IPC from the renderer
    to the browser on each frame.
    
    This was picked up as a regression in the number of tasks.
    
    After analysing the plans for which data will exist in RenderFrameMetadata we
    determined that there are some which are high frequency, and some which are low.
    Additionally the majority of the usage of the high frequency data is in tests.
    
    In this change RenderFrameMetadataObserverImpl is updated to check for the
    difference in RenderFrameMetadata based on high frequency and low frequency
    fields. It will always send updates for low frequency fields. However for high
    frequency fields, such as root_scroll_offset, it will only send in testing mode.
    
    The currently implemented tests continue to pass with this change.
    
    TEST=NonBlockingEventBrowserTest
    
    Bug: 813970
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I4c21030a703c207519a56a851e052d9087708533
    Reviewed-on: https://chromium-review.googlesource.com/934626
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540539}

--- content/common/render_frame_metadata_struct_traits.h
+++ content/common/render_frame_metadata_struct_traits.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_COMMON_RENDER_FRAME_METADATA_STRUCT_TRAITS_H_
 #define CONTENT_COMMON_RENDER_FRAME_METADATA_STRUCT_TRAITS_H_
 
+#include "base/optional.h"
 #include "cc/trees/render_frame_metadata.h"
 #include "content/common/render_frame_metadata.mojom-shared.h"
 
@@ -14,7 +15,7 @@ namespace mojo {
 template <>
 struct StructTraits<content::mojom::RenderFrameMetadataDataView,
                     cc::RenderFrameMetadata> {
-  static gfx::Vector2dF root_scroll_offset(
+  static base::Optional<gfx::Vector2dF> root_scroll_offset(
       const cc::RenderFrameMetadata& metadata) {
     return metadata.root_scroll_offset;
   }
