4c800c8075cdf2160f65eebd603220431611d3f1
Fabio Tirelo <ftirelo@chromium.org>
2018-03-02 16:34:38 +0000

    Chrome Cleaner UI: string fixes from UI review
    Bug: 803714
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ib2182f9ce842f4b515eb7d14322139bed9348e9c
    Reviewed-on: https://chromium-review.googlesource.com/931743
    Commit-Queue: Fabio Tirelo <ftirelo@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540533}

--- chrome/browser/resources/settings/chrome_cleanup_page/chrome_cleanup_page.js
+++ chrome/browser/resources/settings/chrome_cleanup_page/chrome_cleanup_page.js
@@ -820,7 +820,7 @@ Polymer({
       [
         settings.ChromeCleanerCardState.CLEANING, {
           title: this.i18n('chromeCleanupTitleRemoving'),
-          explanation: this.i18n('chromeCleanupExplanationRemove'),
+          explanation: this.i18n('chromeCleanupExplanationRemoving'),
           icon: null,
           actionButton: null,
           flags: settings.ChromeCleanupCardFlags.WAITING_FOR_RESULT |
@@ -897,7 +897,8 @@ Polymer({
         }
       ],
       [
-        settings.ChromeCleanerCardState.CLEANER_DOWNLOAD_FAILED, {
+        settings.ChromeCleanerCardState.CLEANER_DOWNLOAD_FAILED,
+        {
           // TODO(crbug.com/776538): distinguish between missing network
           // connectivity and cleanups being disabled by the server.
           title: this.i18n('chromeCleanupTitleCleanupUnavailable'),
