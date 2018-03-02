036b86ec57ffbc137ee6858a38723d644bbf55e4
edchin <edchin@chromium.org>
2018-03-02 00:41:40 +0000

    [ios] Snapshots enabled in tab grid
    Bug: 804529
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I5a410831b3d6c7dabe066330b1250761a9b21243
    Reviewed-on: https://chromium-review.googlesource.com/944060
    Commit-Queue: edchin <edchin@chromium.org>
    Reviewed-by: edchin <edchin@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540378}

--- ios/chrome/browser/ui/tab_grid/tab_grid_coordinator.mm
+++ ios/chrome/browser/ui/tab_grid/tab_grid_coordinator.mm
@@ -101,6 +101,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   self.incognitoTabsMediator = [[TabGridMediator alloc]
       initWithConsumer:mainViewController.incognitoTabsConsumer];
   self.incognitoTabsMediator.tabModel = self.incognitoTabModel;
+
+  mainViewController.regularTabsImageDataSource = self.regularTabsMediator;
+  mainViewController.incognitoTabsImageDataSource = self.incognitoTabsMediator;
 }
 
 - (void)stop {
