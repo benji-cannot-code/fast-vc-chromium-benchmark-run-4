bd0ee028d55bd8a20177ef1ffdfd24d182dacb34
Sammie Quon <sammiequon@google.com>
2018-03-02 00:35:09 +0000

    overview: Fix shadow placed in the wrong spot.
    Bug caused by press while still animation, then shadow gets applied after
    animation end. Fix this by canceling animation watching if press event.
    
    Test: manual
    Bug: 817436
    Change-Id: Icd1677d4854db8fc6b5ec12c7c71d1a76e955ea6
    Reviewed-on: https://chromium-review.googlesource.com/941721
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540374}

--- ash/wm/overview/scoped_transform_overview_window.cc
+++ ash/wm/overview/scoped_transform_overview_window.cc
@@ -531,6 +531,23 @@ void ScopedTransformOverviewWindow::EnsureVisible() {
   original_opacity_ = 1.f;
 }
 
+aura::Window*
+ScopedTransformOverviewWindow::GetOverviewWindowForMinimizedState() const {
+  return minimized_widget_ ? minimized_widget_->GetNativeWindow() : nullptr;
+}
+
+void ScopedTransformOverviewWindow::UpdateWindowDimensionsType() {
+  if (!IsNewOverviewUi())
+    return;
+
+  type_ = GetWindowDimensionsType(window_);
+  window_selector_bounds_.reset();
+}
+
+void ScopedTransformOverviewWindow::CancelAnimationsListener() {
+  StopObservingImplicitAnimations();
+}
+
 void ScopedTransformOverviewWindow::OnImplicitAnimationsCompleted() {
   // Add the mask which gives the window selector items rounded corners, and add
   // the shadow around the window.
@@ -548,19 +565,6 @@ void ScopedTransformOverviewWindow::OnImplicitAnimationsCompleted() {
   layer->SetMaskLayer(mask_->layer());
   selector_item_->SetShadowBounds(base::make_optional(GetTransformedBounds()));
   selector_item_->EnableBackdropIfNeeded();
-}
-
-aura::Window*
-ScopedTransformOverviewWindow::GetOverviewWindowForMinimizedState() const {
-  return minimized_widget_ ? minimized_widget_->GetNativeWindow() : nullptr;
-}
-
-void ScopedTransformOverviewWindow::UpdateWindowDimensionsType() {
-  if (!IsNewOverviewUi())
-    return;
-
-  type_ = GetWindowDimensionsType(window_);
-  window_selector_bounds_.reset();
 }
 
 void ScopedTransformOverviewWindow::CreateMirrorWindowForMinimizedState() {
