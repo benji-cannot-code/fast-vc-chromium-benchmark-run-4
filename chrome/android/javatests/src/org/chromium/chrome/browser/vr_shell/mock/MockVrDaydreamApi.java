7ac3b01655b481a8d92a4aa7cfb9b89da59191a9
Benjamin Pastene <bpastene@chromium.org>
2018-03-02 19:50:50 +0000

    Revert "VR: Use isInVrSession API to keep Chrome in VR until user has left VR"
    This reverts commit dc55fd14dc6a42a6c6cd0dadcb713c9309f23318.
    
    Reason for revert: Suspected to have broken chrome_public_test_vr_apk on main waterfall
    https://ci.chromium.org/buildbot/chromium.android/Marshmallow%20Phone%20Tester%20%28rel%29/?limit=200
    first failing build contains this cl:
    https://ci.chromium.org/buildbot/chromium.android/Marshmallow%20Phone%20Tester%20%28rel%29/3088
    
    Original change's description:
    > VR: Use isInVrSession API to keep Chrome in VR until user has left VR
    > 
    > This fixes the Daydream Dashboard issues so that if Chrome is simply
    > stopped/started in VR without any intents, Chrome stays in VR.
    > 
    > Also makes resuming Chrome from DD home way faster if Chrome was
    > already in VR.
    > 
    > Bug: 780887
    > Change-Id: Iab7f713bfc5b85972f908ea141e3b495297d7286
    > Reviewed-on: https://chromium-review.googlesource.com/944547
    > Reviewed-by: Yash Malik <ymalik@chromium.org>
    > Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#540403}
    
    TBR=mthiesse@chromium.org,ymalik@chromium.org
    
    Change-Id: I7ae67f5d23d816796cc7f567792e181900b3012a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 780887
    Reviewed-on: https://chromium-review.googlesource.com/946709
    Reviewed-by: Benjamin Pastene <bpastene@chromium.org>
    Commit-Queue: Benjamin Pastene <bpastene@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540588}

--- chrome/android/javatests/src/org/chromium/chrome/browser/vr_shell/mock/MockVrDaydreamApi.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/vr_shell/mock/MockVrDaydreamApi.java
@@ -101,9 +101,4 @@ public class MockVrDaydreamApi implements VrDaydreamApi {
     public void setForwardSetupIntent(boolean forward) {
         mForwardSetupIntent = forward;
     }
-
-    @Override
-    public boolean isInVrSession() {
-        return true;
-    }
 }
