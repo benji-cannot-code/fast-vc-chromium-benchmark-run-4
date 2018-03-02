b43c879bbd5203832c0889716563bb690cebdf75
Xing Liu <xingliu@chromium.org>
2018-03-02 01:42:22 +0000

    Add media download metrics.
    This CL adds two media download metrics to track:
    
    1. Where does the user open media download file on Android.
    
    2. How long does the user keep the media file on disk before deleting
    the file.
    
    Bug: 817220
    Change-Id: I07be85572fe312dc39cd1120b3fc4cae7b7f5b20
    Reviewed-on: https://chromium-review.googlesource.com/940819
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540401}

--- chrome/android/java/src/org/chromium/chrome/browser/download/DownloadManagerService.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/DownloadManagerService.java
@@ -31,6 +31,7 @@ import org.chromium.base.library_loader.LibraryProcessType;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeFeatureList;
+import org.chromium.chrome.browser.download.DownloadMetrics.DownloadOpenSource;
 import org.chromium.chrome.browser.download.ui.BackendProvider;
 import org.chromium.chrome.browser.download.ui.DownloadHistoryAdapter;
 import org.chromium.chrome.browser.externalnav.ExternalNavigationDelegateImpl;
@@ -569,7 +570,8 @@ public class DownloadManagerService
                     download.getDownloadInfo(), download.getSystemDownloadId());
             return;
         }
-        openDownloadedContent(download.getDownloadInfo(), download.getSystemDownloadId());
+        openDownloadedContent(download.getDownloadInfo(), download.getSystemDownloadId(),
+                DownloadMetrics.AUTO_OPEN);
     }
 
     /**
@@ -812,11 +814,12 @@ public class DownloadManagerService
     }
 
     /** See {@link #openDownloadedContent(Context, String, boolean, boolean, String, long)}. */
-    protected void openDownloadedContent(final DownloadInfo downloadInfo, final long downloadId) {
+    protected void openDownloadedContent(final DownloadInfo downloadInfo, final long downloadId,
+            @DownloadOpenSource int source) {
         openDownloadedContent(mContext, downloadInfo.getFilePath(),
                 isSupportedMimeType(downloadInfo.getMimeType()), downloadInfo.isOffTheRecord(),
                 downloadInfo.getDownloadGuid(), downloadId, downloadInfo.getOriginalUrl(),
-                downloadInfo.getReferrer());
+                downloadInfo.getReferrer(), source);
     }
 
     /**
@@ -831,11 +834,12 @@ public class DownloadManagerService
      * @param downloadId          ID of the download item in DownloadManager.
      * @param originalUrl         The original url of the downloaded file.
      * @param referrer            Referrer of the downloaded file.
+     * @param source              The source that tries to open the download.
      */
     protected static void openDownloadedContent(final Context context, final String filePath,
             final boolean isSupportedMimeType, final boolean isOffTheRecord,
             final String downloadGuid, final long downloadId, final String originalUrl,
-            final String referrer) {
+            final String referrer, @DownloadOpenSource int source) {
         new AsyncTask<Void, Void, Intent>() {
             @Override
             public Intent doInBackground(Void... params) {
@@ -857,6 +861,10 @@ public class DownloadManagerService
                 if (didLaunchIntent && hasDownloadManagerService()) {
                     DownloadManagerService.getDownloadManagerService().updateLastAccessTime(
                             downloadGuid, isOffTheRecord);
+                    DownloadManager manager =
+                            (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+                    String mimeType = manager.getMimeTypeForDownloadedFile(downloadId);
+                    DownloadMetrics.recordDownloadOpen(source, mimeType);
                 }
             }
         }.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
