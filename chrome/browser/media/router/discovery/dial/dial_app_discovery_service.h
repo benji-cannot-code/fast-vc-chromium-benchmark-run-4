106c55a6be86f406c60095eaeb4f0e420db7dbdc
mark a. foltz <mfoltz@chromium.org>
2018-03-02 00:16:46 +0000

    Revert "[DIAL] Added a DialURLFetcher to handle Dial related HTTP requests"
    This reverts commit 32778ae41b20ac2a151319d333cca660016be53d.
    
    Reason for revert: Likely culprit for ASAN crash.
    
    Original change's description:
    > [DIAL] Added a DialURLFetcher to handle Dial related HTTP requests
    > 
    > DeviceDescriptionFetcher and DialAppInfoFetcher have some common code.
    > Create a DialURLFetcher class to get rid of some duplications.
    > 
    > The common URLFetcher class can also be used to launch and stop Dial
    > apps when we implement DIAL launch at browser side.
    > 
    > Bug: 816628
    > Change-Id: I26bdfbbf7e2d59a68e3bf5aeaba8b6e3664bf430
    > Reviewed-on: https://chromium-review.googlesource.com/938158
    > Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    > Reviewed-by: Derek Cheng <imcheng@chromium.org>
    > Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    > Commit-Queue: Bin Zhao <zhaobin@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#540242}
    
    TBR=mfoltz@chromium.org,imcheng@chromium.org,rhalavati@chromium.org,zhaobin@chromium.org
    
    Change-Id: Ibfc4a97935f88ac4520383e6b26cd2ebca9ecb2e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 816628
    Reviewed-on: https://chromium-review.googlesource.com/944728
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Commit-Queue: mark a. foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540363}

--- chrome/browser/media/router/discovery/dial/dial_app_discovery_service.h
+++ chrome/browser/media/router/discovery/dial/dial_app_discovery_service.h
@@ -25,7 +25,7 @@ class Connector;
 
 namespace media_router {
 
-class DialURLFetcher;
+class DialAppInfoFetcher;
 class SafeDialAppInfoParser;
 
 // Represents DIAL app status on receiver device.
@@ -118,7 +118,7 @@ class DialAppDiscoveryService {
                            SafeDialAppInfoParser::ParsingResult parsing_result);
 
   // Map of pending app info fetchers, keyed by request id.
-  base::flat_map<std::string, std::unique_ptr<DialURLFetcher>>
+  base::flat_map<std::string, std::unique_ptr<DialAppInfoFetcher>>
       pending_fetcher_map_;
 
   // See comments for DialAppInfoParseCompletedCallback.
