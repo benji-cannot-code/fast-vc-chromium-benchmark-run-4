2ac028015d5b14d1455e02d0d19687e7b9cebc5c
Tom Sepez <tsepez@chromium.org>
2018-03-02 18:40:21 +0000

    Bound ppapi process totals when isolating by origin.
    Otherwise, we risk having an unbounded number of processes.
    Results in a "failed to load plugin" page once N == 15 created.
    Add test to fork off a handful.
    
    No plugins are yet isolated by origin, so no observable change
    to chrome. That will happen in a subsequent CL.
    
    Follow-up to:
      https://chromium-review.googlesource.com/c/chromium/src/+/915182
    
    Bug: 809614
    Change-Id: Ie71d41d03d380e31f006173b659023e3de0e3ac8
    Reviewed-on: https://chromium-review.googlesource.com/942235
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Ehsan Karamad <ekaramad@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540565}

--- content/browser/plugin_service_impl_browsertest.cc
+++ content/browser/plugin_service_impl_browsertest.cc
@@ -6,9 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/plugin_service_impl.h"
 
 #include <memory>
+#include <string>
 #include <utility>
 
 #include "base/optional.h"
+#include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
 #include "content/browser/ppapi_plugin_process_host.h"
@@ -87,6 +89,7 @@ class PluginServiceImplBrowserTest : public ContentBrowserTest {
                        base::Unretained(service), 0, plugin_path_, profile_dir_,
                        origin, base::Unretained(client)));
     client->WaitForQuit();
+    client->SetRunLoop(nullptr);
   }
 
   base::FilePath plugin_path_;
@@ -131,6 +134,42 @@ IN_PROC_BROWSER_TEST_F(PluginServiceImplBrowserTest, OriginLock) {
   EXPECT_NE(client2a.plugin_pid(), client3a.plugin_pid());
   EXPECT_NE(client2a.plugin_pid(), client3b.plugin_pid());
   EXPECT_EQ(client3a.plugin_pid(), client3b.plugin_pid());
+}
+
+IN_PROC_BROWSER_TEST_F(PluginServiceImplBrowserTest, NoForkBombs) {
+  RegisterFakePlugin();
+
+  PluginServiceImpl* service = PluginServiceImpl::GetInstance();
+  service->SetMaxPpapiProcessesPerProfileForTesting(4);
+
+  const char* kFakeURLTemplate = "https://foo.fake%d.com/";
+  TestPluginClient client;
+  for (int i = 0; i < 4; ++i) {
+    std::string url = base::StringPrintf(kFakeURLTemplate, i);
+    url::Origin origin = url::Origin::Create(GURL(url));
+    OpenChannelToFakePlugin(origin, &client);
+    EXPECT_NE(base::kNullProcessId, client.plugin_pid());
+  }
+
+  // After a while we stop handing out processes per-origin.
+  for (int i = 4; i < 8; ++i) {
+    std::string url = base::StringPrintf(kFakeURLTemplate, i);
+    url::Origin origin = url::Origin::Create(GURL(url));
+    OpenChannelToFakePlugin(origin, &client);
+    EXPECT_EQ(base::kNullProcessId, client.plugin_pid());
+  }
+
+  // But there's always room for the empty origin case.
+  OpenChannelToFakePlugin(base::nullopt, &client);
+  EXPECT_NE(base::kNullProcessId, client.plugin_pid());
+
+  // And re-using existing processes is always possible.
+  for (int i = 0; i < 4; ++i) {
+    std::string url = base::StringPrintf(kFakeURLTemplate, i);
+    url::Origin origin = url::Origin::Create(GURL(url));
+    OpenChannelToFakePlugin(origin, &client);
+    EXPECT_NE(base::kNullProcessId, client.plugin_pid());
+  }
 }
 
 }  // namespace content
