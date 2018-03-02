4070ca984e17ddc1ffaacb1cffd160ce8a70a4e7
Emil A Eklund <eae@chromium.org>
2018-03-02 00:46:36 +0000

    Check actual text length in InlineTextBox::IsLineBreak
    Check the length of the underlying string, rather than the length of the
    text box as the two may differ due to text normalization.
    
    Bug: 816768
    Change-Id: Ibb91256200d51f6ec19dcff6e84bc12e5230f776
    Reviewed-on: https://chromium-review.googlesource.com/942367
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540382}

--- third_party/WebKit/Source/core/layout/line/InlineTextBox.cpp
+++ third_party/WebKit/Source/core/layout/line/InlineTextBox.cpp
@@ -473,6 +473,7 @@ LayoutUnit InlineTextBox::PlaceEllipsisBox(bool flow_is_ltr,
 bool InlineTextBox::IsLineBreak() const {
   return GetLineLayoutItem().IsBR() ||
          (GetLineLayoutItem().Style()->PreserveNewline() && Len() == 1 &&
+          GetLineLayoutItem().GetText().length() > Start() &&
           (*GetLineLayoutItem().GetText().Impl())[Start()] == '\n');
 }
 
