943caa49894e7bdd83ba280c20c13f439baea6d5
Tien-Ren Chen <trchen@chromium.org>
2018-03-04 09:26:28 +0000

    [Blink] Clamp clip path bounds to LayoutRect::InfiniteIntRect()
    Extreme clip path values can create numerical issues including integer
    overflow, which is undefined behavior. This CL clamps clip path bounds
    to some sane value early in the pipeline.
    
    BUG=813340
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Ie35ea2899b7103ed36ba42f0c78711811b253e60
    Reviewed-on: https://chromium-review.googlesource.com/942303
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Tien-Ren Chen <trchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540770}

--- third_party/WebKit/Source/core/paint/ClipPathClipper.h
+++ third_party/WebKit/Source/core/paint/ClipPathClipper.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ClipPathClipper_h
 #define ClipPathClipper_h
 
+#include "core/CoreExport.h"
 #include "core/paint/FloatClipRecorder.h"
 #include "platform/geometry/FloatRect.h"
 #include "platform/graphics/paint/ClipPathRecorder.h"
@@ -17,7 +18,7 @@ namespace blink {
 class GraphicsContext;
 class LayoutObject;
 
-class ClipPathClipper {
+class CORE_EXPORT ClipPathClipper {
   DISALLOW_NEW_EXCEPT_PLACEMENT_NEW();
 
  public:
