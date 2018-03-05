4953c936a72cb1f38a3a75fe8e56b84b16ba90a4
Xi Cheng <chengx@chromium.org>
2018-03-05 18:15:00 +0000

    Remove the inclusion of chrome_crash_reporter_client_win.h
    This header file is not used in main_dll_loader_win.cc, hence removed.
    
    Change-Id: I812df3f167d104fa012ae204083f3627d08b60e5
    Reviewed-on: https://chromium-review.googlesource.com/947815
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Xi Cheng <chengx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540867}

--- chrome/app/main_dll_loader_win.cc
+++ chrome/app/main_dll_loader_win.cc
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/trace_event.h"
 #include "base/win/scoped_handle.h"
 #include "base/win/windows_version.h"
-#include "chrome/app/chrome_crash_reporter_client_win.h"
 #include "chrome/app/chrome_watcher_client_win.h"
 #include "chrome/app/chrome_watcher_command_line_win.h"
 #include "chrome/app/file_pre_reader_win.h"
