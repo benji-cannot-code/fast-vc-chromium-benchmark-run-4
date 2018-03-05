5c023afae3d27fd9483d620c76e2b4f07f6c6a49
Justin Cohen <justincohen@google.com>
2018-03-05 16:58:24 +0000

    [ios] Never show reading list items for UI refresh phase 1.
    The NTP already has a reading list icon with a badged count
    when there are unread items so the larger RL collection
    items is superfluous.
    
    Bug: 805638
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Ic34ed523bb6946017ec312aca6e6c9c11ac2b2f3
    Reviewed-on: https://chromium-review.googlesource.com/947836
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540851}

--- ios/chrome/browser/ntp_snippets/ios_chrome_content_suggestions_service_factory_util.cc
+++ ios/chrome/browser/ntp_snippets/ios_chrome_content_suggestions_service_factory_util.cc
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/pref_names.h"
 #include "ios/chrome/browser/reading_list/reading_list_model_factory.h"
 #include "ios/chrome/browser/signin/identity_manager_factory.h"
+#include "ios/chrome/browser/ui/ui_util.h"
 #include "ios/chrome/common/channel_info.h"
 #include "ios/web/public/browser_state.h"
 #include "net/url_request/url_request_context_getter.h"
@@ -140,6 +141,10 @@ std::unique_ptr<KeyedService> CreateChromeContentSuggestionsService(
 
 void RegisterReadingListProvider(ContentSuggestionsService* service,
                                  web::BrowserState* browser_state) {
+  // Prevent loading any reading list items for refresh.
+  if (IsUIRefreshPhase1Enabled())
+    return;
+
   ios::ChromeBrowserState* chrome_browser_state =
       ios::ChromeBrowserState::FromBrowserState(browser_state);
 
