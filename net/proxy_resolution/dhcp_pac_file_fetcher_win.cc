8494bdc50f8e7fb62ee60128674d08621f2ac18a
Eric Roman <eroman@chromium.org>
2018-03-05 21:37:35 +0000

    Skip network adapters that are not in state IfOperStatusUp when probing for WPAD via DHCP.
    This is a speculative fix for calls to dhcpsvc!DhcpRequestParams being very slow following network changes.
    
    Bug: 770201
    Change-Id: I506f44d51ee4d14625bb4d64974f6e4dd618c103
    Reviewed-on: https://chromium-review.googlesource.com/946870
    Commit-Queue: Eric Roman <eroman@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540947}

--- net/proxy_resolution/dhcp_pac_file_fetcher_win.cc
+++ net/proxy_resolution/dhcp_pac_file_fetcher_win.cc
@@ -26,6 +26,33 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace net {
 
+namespace {
+
+// Returns true if |adapter| should be considered when probing for WPAD via
+// DHCP.
+bool IsDhcpCapableAdapter(IP_ADAPTER_ADDRESSES* adapter) {
+  if (adapter->IfType == IF_TYPE_SOFTWARE_LOOPBACK)
+    return false;
+  if ((adapter->Flags & IP_ADAPTER_DHCP_ENABLED) == 0)
+    return false;
+
+  // Don't probe interfaces which are not up and ready to pass packets.
+  //
+  // This is a speculative fix for https://crbug.com/770201, in case calling
+  // dhcpsvc!DhcpRequestParams on interfaces that aren't ready yet blocks for
+  // a long time.
+  //
+  // Since ProxyResolutionService restarts WPAD probes in response to other
+  // network level changes, this will likely get called again once the
+  // interface is up.
+  if (adapter->OperStatus != IfOperStatusUp)
+    return false;
+
+  return true;
+}
+
+}  // namespace
+
 // This struct contains logging information describing how
 // GetCandidateAdapterNames() performed, for output to NetLog.
 struct DhcpAdapterNamesLoggingInfo {
@@ -203,9 +230,8 @@ std::unique_ptr<base::Value> NetLogGetAdaptersDoneCallback(
     SetInt("TunnelType", adapter->TunnelType, &adapter_value);
 
     // "skipped" means the adapter was not ultimately chosen as a candidate for
-    // testing WPAD. This replicates the logic in GetAdapterNames().
-    bool skipped = (adapter->IfType == IF_TYPE_SOFTWARE_LOOPBACK) ||
-                   ((adapter->Flags & IP_ADAPTER_DHCP_ENABLED) == 0);
+    // testing WPAD.
+    bool skipped = !IsDhcpCapableAdapter(adapter);
     adapter_value.SetKey("skipped", base::Value(skipped));
 
     adapters_value.GetList().push_back(std::move(adapter_value));
@@ -565,13 +591,10 @@ bool DhcpProxyScriptFetcherWin::GetCandidateAdapterNames(
 
   IP_ADAPTER_ADDRESSES* adapter = NULL;
   for (adapter = adapters.get(); adapter; adapter = adapter->Next) {
-    if (adapter->IfType == IF_TYPE_SOFTWARE_LOOPBACK)
-      continue;
-    if ((adapter->Flags & IP_ADAPTER_DHCP_ENABLED) == 0)
-      continue;
-
-    DCHECK(adapter->AdapterName);
-    adapter_names->insert(adapter->AdapterName);
+    if (IsDhcpCapableAdapter(adapter)) {
+      DCHECK(adapter->AdapterName);
+      adapter_names->insert(adapter->AdapterName);
+    }
   }
 
   // Transfer the buffer containing the adapters, so it can be used later for
