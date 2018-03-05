3b6abb8d9248652206362def152462ee40eb325b
Robert Liao <robliao@chromium.org>
2018-03-05 18:45:39 +0000

    Revert "Temporarily Disable ExtensionPopup Views for Mac"
    This reverts commit cd92b8f19e21242c9e086d4501ef341be2ab8999.
    
    Reason for revert: M66 has branched.
    
    Original change's description:
    > Temporarily Disable ExtensionPopup Views for Mac
    > 
    > This change will be reverted after the M66 branch point.
    > 
    > ExtensionPopup Views will need some harmonization work to bring it up to
    > parity with some behavior provided by Mac.
    > 
    > BUG=728174
    > 
    > Change-Id: Ia01160117c9dbda10e57d4f4b5a4fe706a39f36c
    > Reviewed-on: https://chromium-review.googlesource.com/941883
    > Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    > Commit-Queue: Robert Liao <robliao@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#540203}
    
    TBR=ellyjones@chromium.org,robliao@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 728174
    Change-Id: I8db100c708ec2baf1cd0871f5e2bdc31db08c3c4
    Reviewed-on: https://chromium-review.googlesource.com/947230
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540880}

--- chrome/browser/ui/cocoa/extensions/browser_action_test_util_views_mac.mm
+++ chrome/browser/ui/cocoa/extensions/browser_action_test_util_views_mac.mm
@@ -179,8 +179,7 @@ class ExtensionPopupTestManagerViews : public ExtensionPopupTestManager {
 };
 
 std::unique_ptr<ExtensionPopupTestManager> GetExtensionPopupTestManager() {
-  if (!chrome::ShowAllDialogsWithViewsToolkit() ||
-      !chrome::ShowExtensionPopupWithViewsToolkit()) {
+  if (!chrome::ShowAllDialogsWithViewsToolkit()) {
     return std::make_unique<ExtensionPopupTestManagerCocoa>();
   }
   return std::make_unique<ExtensionPopupTestManagerViews>();
