c042bf678b22eace42d03fab21934e70d74127a1
Tien-Ren Chen <trchen@chromium.org>
2018-03-05 08:04:38 +0000

    [Blink/Paint] Fix scrolling content background with writing mode
    The paint rect for background in scrolling content layer should be
    expanded to the full scrollable rect, i.e. layout overflow rect.
    
    This CL fixes a bug in BoxPainter and BoxPaintInvalidator that we
    forgot to convert LayoutBox::LayoutOverflowRect() from flipped
    coordinates to physical coordinates for painting and invalidation.
    
    BUG=787666
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I4b4c45e910ea1d860e61eece6005263d0a8c708e
    Reviewed-on: https://chromium-review.googlesource.com/942485
    Commit-Queue: Tien-Ren Chen <trchen@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540791}

--- third_party/WebKit/Source/core/layout/LayoutBox.h
+++ third_party/WebKit/Source/core/layout/LayoutBox.h
@@ -111,7 +111,7 @@ struct LayoutBoxRareData {
   // layout overflow rect after the last paint invalidation. They are valid if
   // m_hasPreviousContentBoxSizeAndLayoutOverflowRect is true.
   LayoutSize previous_content_box_size_;
-  LayoutRect previous_layout_overflow_rect_;
+  LayoutRect previous_physical_layout_overflow_rect_;
 
   // Used by LocalFrameView::ScrollIntoView. When the scroll is sequenced
   // rather than instantly performed, we need the pending_offset_to_scroll
@@ -466,6 +466,11 @@ class CORE_EXPORT LayoutBox : public LayoutBoxModelObject {
   LayoutRect NoOverflowRect() const;
   LayoutRect LayoutOverflowRect() const {
     return overflow_ ? overflow_->LayoutOverflowRect() : NoOverflowRect();
+  }
+  LayoutRect PhysicalLayoutOverflowRect() const {
+    LayoutRect overflow_rect = LayoutOverflowRect();
+    FlipForWritingMode(overflow_rect);
+    return overflow_rect;
   }
   IntRect PixelSnappedLayoutOverflowRect() const {
     return PixelSnappedIntRect(LayoutOverflowRect());
@@ -1394,11 +1399,11 @@ class CORE_EXPORT LayoutBox : public LayoutBoxModelObject {
                ? rare_data_->previous_content_box_size_
                : PreviousSize();
   }
-  LayoutRect PreviousLayoutOverflowRect() const {
+  LayoutRect PreviousPhysicalLayoutOverflowRect() const {
     return rare_data_ &&
                    rare_data_
                        ->has_previous_content_box_size_and_layout_overflow_rect_
-               ? rare_data_->previous_layout_overflow_rect_
+               ? rare_data_->previous_physical_layout_overflow_rect_
                : LayoutRect(LayoutPoint(), PreviousSize());
   }
 
