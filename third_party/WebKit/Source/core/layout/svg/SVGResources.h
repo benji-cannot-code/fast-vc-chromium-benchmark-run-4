91f690f29e88545e2c68553816e72b5f68cb91fa
Fredrik Söderquist <fs@opera.com>
2018-03-05 19:51:57 +0000

    [CI] Move invalidation out of SVGResources::RemoveClientFromCache
    This pushes the actual call to MarkClientForInvalidation out of said
    function, leaving it to callers to perform the invalidation.
    
    Bug: 769774
    Change-Id: Ib9063899da132a8f31290205ace6a52d5fc8457b
    Reviewed-on: https://chromium-review.googlesource.com/946092
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540903}

--- third_party/WebKit/Source/core/layout/svg/SVGResources.h
+++ third_party/WebKit/Source/core/layout/svg/SVGResources.h
@@ -98,8 +98,7 @@ class SVGResources {
   void BuildSetOfResources(HashSet<LayoutSVGResourceContainer*>&);
 
   // Methods operating on all cached resources
-  void RemoveClientFromCache(LayoutObject&,
-                             bool mark_for_invalidation = true) const;
+  InvalidationModeMask RemoveClientFromCache(LayoutObject&) const;
   InvalidationModeMask RemoveClientFromCacheAffectingObjectBounds(
       LayoutObject&) const;
   void ResourceDestroyed(LayoutSVGResourceContainer*);
