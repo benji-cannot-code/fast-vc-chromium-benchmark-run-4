43d2043d8c10681d09f8b3da70ebd9601dba7372
Christopher Cameron <ccameron@chromium.org>
2018-03-05 23:25:10 +0000

    mac: Crash on surface invariant violations
    If a LayerTreeHost's LocalSurfaceId has been committed to a
    LayerTreImpl, then never allow that LocalSurfaceId to be used with
    incompatible size or scale factor.
    
    This CHECK should eventually be enabled on all platforms, but for now
    only passes tests on non-Aura.
    
    Ideally, this CHECK should not depend on has_pushed_local_surface_id,
    but scale factor adjusting layout tests use transiently-invalid
    settings.
    
    Bug: 814690
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: Ia610cbbb0859260e620cc79639daed9b39ba7a1b
    Reviewed-on: https://chromium-review.googlesource.com/947713
    Reviewed-by: Fady Samuel <fsamuel@chromium.org>
    Commit-Queue: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540971}

--- cc/trees/layer_tree_host.cc
+++ cc/trees/layer_tree_host.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/timer/elapsed_timer.h"
 #include "base/trace_event/trace_event.h"
 #include "base/trace_event/trace_event_argument.h"
+#include "build/build_config.h"
 #include "cc/base/devtools_instrumentation.h"
 #include "cc/base/histograms.h"
 #include "cc/base/math_util.h"
@@ -1055,7 +1056,6 @@ void LayerTreeHost::SetViewportSizeAndScale(
       << "A valid LocalSurfaceId has been provided with an empty device "
          "viewport size "
       << local_surface_id;
-  // TODO(ccameron): Add CHECKs here for surface invariants violations.
   if (settings_.enable_surface_synchronization)
     SetLocalSurfaceId(local_surface_id);
 
@@ -1081,6 +1081,11 @@ void LayerTreeHost::SetViewportSizeAndScale(
   if (changed) {
     SetPropertyTreesNeedRebuild();
     SetNeedsCommit();
+#if defined(OS_MACOSX)
+    // TODO(ccameron): This check is not valid on Aura or Mus yet, but should
+    // be.
+    CHECK(!has_pushed_local_surface_id_ || !local_surface_id_.is_valid());
+#endif
   }
 }
 
@@ -1172,6 +1177,7 @@ void LayerTreeHost::SetLocalSurfaceId(
   if (local_surface_id_ == local_surface_id)
     return;
   local_surface_id_ = local_surface_id;
+  has_pushed_local_surface_id_ = false;
   UpdateDeferCommitsInternal();
   SetNeedsCommit();
 }
@@ -1362,6 +1368,7 @@ void LayerTreeHost::PushLayerTreePropertiesTo(LayerTreeImpl* tree_impl) {
   tree_impl->set_content_source_id(content_source_id_);
 
   tree_impl->set_local_surface_id(local_surface_id_);
+  has_pushed_local_surface_id_ = true;
 
   if (pending_page_scale_animation_) {
     tree_impl->SetPendingPageScaleAnimation(
