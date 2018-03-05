744ec14143398f9a50d1edc522e14939a48dcf96
Jay Civelli <jcivelli@google.com>
2018-03-05 19:59:44 +0000

    Changing TestExtensionSystem to mock more services
    In preparation of changing the extension install code to use the
    unzipper service, making the TestExtensionSystem register the
    data_decoder and the unzip_service (instead of only the data_decoder).
    
    Bug: 799220
    Change-Id: Ib6698721ee597a4a27fd470b37fafd00a9e0097b
    Reviewed-on: https://chromium-review.googlesource.com/940522
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Jay Civelli <jcivelli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540906}

--- chrome/browser/extensions/test_extension_system.h
+++ chrome/browser/extensions/test_extension_system.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "extensions/browser/extension_system.h"
 #include "extensions/common/one_shot_event.h"
-#include "services/data_decoder/public/cpp/test_data_decoder_service.h"
 
 #if defined(OS_CHROMEOS)
 #include "chrome/browser/chromeos/login/users/scoped_test_user_manager.h"
@@ -27,6 +26,11 @@ class FilePath;
 namespace content {
 class BrowserContext;
 }
+
+namespace service_manager {
+class Connector;
+class TestConnectorFactory;
+}  // namespace service_manager
 
 namespace extensions {
 
@@ -103,8 +107,8 @@ class TestExtensionSystem : public ExtensionSystem {
   std::unique_ptr<QuotaService> quota_service_;
   std::unique_ptr<AppSorting> app_sorting_;
   OneShotEvent ready_;
-  std::unique_ptr<data_decoder::TestDataDecoderService>
-      test_data_decoder_service_;
+  std::unique_ptr<service_manager::TestConnectorFactory> connector_factory_;
+  std::unique_ptr<service_manager::Connector> connector_;
 
 #if defined(OS_CHROMEOS)
   std::unique_ptr<chromeos::ScopedTestUserManager> test_user_manager_;
