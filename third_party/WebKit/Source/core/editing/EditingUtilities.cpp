9d12a04464a67a179b327d0755d51b3ae9132b0e
Zhuoyu Qian <zhuoyu.qian@samsung.com>
2018-03-05 05:46:08 +0000

    Change EditorParagraphSeparator to be an enum class.
    This patch makes EditorParagraphSeparator to be an enum class for better
    type safety.
    
    Bug:
    
    Signed-off-by: Zhuoyu Qian <zhuoyu.qian@samsung.com>
    Change-Id: I6e8b6182c4838b2163ea138c2d621f3249900451
    Reviewed-on: https://chromium-review.googlesource.com/945588
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540784}

--- third_party/WebKit/Source/core/editing/EditingUtilities.cpp
+++ third_party/WebKit/Source/core/editing/EditingUtilities.cpp
@@ -1273,9 +1273,9 @@ bool IsTableCell(const Node* node) {
 
 HTMLElement* CreateDefaultParagraphElement(Document& document) {
   switch (document.GetFrame()->GetEditor().DefaultParagraphSeparator()) {
-    case kEditorParagraphSeparatorIsDiv:
+    case EditorParagraphSeparator::kIsDiv:
       return HTMLDivElement::Create(document);
-    case kEditorParagraphSeparatorIsP:
+    case EditorParagraphSeparator::kIsP:
       return HTMLParagraphElement::Create(document);
   }
 
