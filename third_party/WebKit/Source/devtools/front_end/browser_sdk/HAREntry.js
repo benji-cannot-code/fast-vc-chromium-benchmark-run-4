d33d8c240b47ecfa3b7576f3e2bc3829dabe883f
Eugene Ostroukhov <eostroukhov@chromium.org>
2018-03-06 21:29:07 +0000

    DevTools: Parse date passed to Cookie::expiresDate
    Bug: 817970
    Change-Id: Ia7f3ccacf3231011e7955cee4766035e481b72be
    Reviewed-on: https://chromium-review.googlesource.com/947255
    Commit-Queue: Eugene Ostroukhov <eostroukhov@chromium.org>
    Commit-Queue: Pavel Feldman <pfeldman@chromium.org>
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541196}

--- third_party/WebKit/Source/devtools/front_end/browser_sdk/HAREntry.js
+++ third_party/WebKit/Source/devtools/front_end/browser_sdk/HAREntry.js
@@ -71,7 +71,7 @@ BrowserSDK.HAREntry = class {
       time += Math.max(t, 0);
 
     const entry = {
-      startedDateTime: BrowserSDK.HARLog.pseudoWallTime(harEntry._request, harEntry._request.issueTime()),
+      startedDateTime: BrowserSDK.HARLog.pseudoWallTime(harEntry._request, harEntry._request.issueTime()).toJSON(),
       time: time,
       request: await harEntry._buildRequest(),
       response: harEntry._buildResponse(),
@@ -334,10 +334,10 @@ BrowserSDK.HARLog = class {
   /**
    * @param {!SDK.NetworkRequest} request
    * @param {number} monotonicTime
-   * @return {string}
+   * @return {!Date}
    */
   static pseudoWallTime(request, monotonicTime) {
-    return new Date(request.pseudoWallTime(monotonicTime) * 1000).toJSON();
+    return new Date(request.pseudoWallTime(monotonicTime) * 1000);
   }
 
   /**
@@ -384,7 +384,7 @@ BrowserSDK.HARLog = class {
    */
   _convertPage(page, request) {
     return {
-      startedDateTime: BrowserSDK.HARLog.pseudoWallTime(request, page.startTime),
+      startedDateTime: BrowserSDK.HARLog.pseudoWallTime(request, page.startTime).toJSON(),
       id: 'page_' + page.id,
       title: page.url,  // We don't have actual page title here. URL is probably better than nothing.
       pageTimings: {
