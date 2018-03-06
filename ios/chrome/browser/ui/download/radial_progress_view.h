9b7e0606de9d2c987649d6df810b8cf09b907f04
Eugene But <eugenebut@google.com>
2018-03-06 19:24:27 +0000

    Add RadialProgressView to use in New Download Manager UI.
    This CL only adds UIView. The view will be integrated into Download
    Manager UI in a separate CL. There are no tests as testing view drawing
    is hard and not worth the effort.
    
    UI Mocks: https://docs.google.com/presentation/d/1GzbAoJrpW9IAQF78afh5SZLWJWErNcC67t_ctujjEus/edit#slide=id.g2b7a689b42_0_196
    
    Bug: 791806
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I9dba8b92726a9da6865866a49020f9bb0bc51c74
    Reviewed-on: https://chromium-review.googlesource.com/949882
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Reviewed-by: edchin <edchin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541171}

---
+++ ios/chrome/browser/ui/download/radial_progress_view.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_DOWNLOAD_RADIAL_PROGRESS_VIEW_H_
+#define IOS_CHROME_BROWSER_UI_DOWNLOAD_RADIAL_PROGRESS_VIEW_H_
+
+#import <UIKit/UIKit.h>
+
+// View that draws progress as arc. Arc starts at 12 o'clock and drawn
+// clockwise. The arc color is tintColor.
+@interface RadialProgressView : UIView
+
+// Progress in [0.0f, 1.0f] range.
+@property(nonatomic) float progress;
+
+// The line width used when stroking the progress arc.
+@property(nonatomic) CGFloat lineWidth;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_DOWNLOAD_RADIAL_PROGRESS_VIEW_H_
