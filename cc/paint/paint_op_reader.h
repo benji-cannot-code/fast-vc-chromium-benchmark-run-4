6e355462ea83c173c405e8544ecd4c2796fe3fee
Adrienne Walker <enne@chromium.org>
2018-03-06 03:07:45 +0000

    oop: Serialize transfer cache image color spaces
    This patch fixes some TODOs in image transfer cache serialization around
    SkColorSpace.  Because SkImages are stored in the transfer cache, this
    patch does not send the SkColorSpace separately in the transfer cache
    and serializes it along with the image itself.
    
    The SkColorSpace serialization in PaintOpReader/Writer is not exercised
    by existing fuzzers, so the followup to this patch is to write a
    transfer cache service-side deserialization fuzzer.
    
    This patch also modifies the logic of the GpuImageDecodeCache to be
    more consistent about when it applies color conversion.  Previously,
    if color_type_ was 4444 then color space conversion would not be
    applied to uploaded images but would be applied to "too large"
    images that were kept as software bitmaps.  With this patch, no
    color space conversion is applied in any mode if the color_type_
    is not supported.
    
    Bug: 815354
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I100acafb89bed2f7d8e80f0a0e6af2997b7d5590
    Reviewed-on: https://chromium-review.googlesource.com/939725
    Commit-Queue: enne <enne@chromium.org>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541037}

--- cc/paint/paint_op_reader.h
+++ cc/paint/paint_op_reader.h
@@ -67,6 +67,7 @@ class CC_PAINT_EXPORT PaintOpReader {
   void Read(SkMatrix* matrix);
   void Read(SkColorType* color_type);
   void Read(SkImageInfo* info);
+  void Read(sk_sp<SkColorSpace>* color_space);
 
   void Read(SkClipOp* op) {
     uint8_t value = 0u;
