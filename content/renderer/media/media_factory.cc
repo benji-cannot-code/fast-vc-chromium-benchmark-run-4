e4ca154f9581002295a12e5e70fb3e63e50b3379
Xiaohan Wang <xhwang@chromium.org>
2018-03-06 22:53:51 +0000

    media: Remove kMojoCdm feature
    Now Mojo CDM is enabled by default on desktop platforms (when library
    CDM is enabled), remove kMojoCdm feature.
    
    Will start to remove pepper CDM support in later CLs.
    
    Bug: 772160
    Change-Id: Id5b0692db992500d97cd5902f1ddd97147ab97d9
    Reviewed-on: https://chromium-review.googlesource.com/946430
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541224}

--- content/renderer/media/media_factory.cc
+++ content/renderer/media/media_factory.cc
@@ -75,11 +75,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/remoting/renderer_controller.h"         // nogncheck
 #endif
 
-#if BUILDFLAG(ENABLE_LIBRARY_CDMS)
-#include "content/renderer/media/cdm/pepper_cdm_wrapper_impl.h"
-#include "content/renderer/media/cdm/render_cdm_factory.h"
-#endif
-
 namespace {
 class FrameFetchContext : public media::ResourceFetchContext {
  public:
@@ -521,25 +516,12 @@ media::mojom::RemoterFactory* MediaFactory::GetRemoterFactory() {
 #endif
 
 media::CdmFactory* MediaFactory::GetCdmFactory() {
-  blink::WebLocalFrame* web_frame = render_frame_->GetWebFrame();
-  DCHECK(web_frame);
-
   if (cdm_factory_)
     return cdm_factory_.get();
 
-#if BUILDFLAG(ENABLE_LIBRARY_CDMS)
-  static_assert(
-      BUILDFLAG(ENABLE_MOJO_CDM),
-      "Mojo CDM should always be enabled when library CDM is enabled");
-  if (base::FeatureList::IsEnabled(media::kMojoCdm)) {
-    cdm_factory_.reset(new media::MojoCdmFactory(GetMediaInterfaceFactory()));
-  } else {
-    cdm_factory_.reset(new RenderCdmFactory(
-        base::Bind(&PepperCdmWrapperImpl::Create, web_frame)));
-  }
-#elif BUILDFLAG(ENABLE_MOJO_CDM)
+#if BUILDFLAG(ENABLE_MOJO_CDM)
   cdm_factory_.reset(new media::MojoCdmFactory(GetMediaInterfaceFactory()));
-#endif  // BUILDFLAG(ENABLE_LIBRARY_CDMS)
+#endif  // BUILDFLAG(ENABLE_MOJO_CDM)
 
   return cdm_factory_.get();
 }
