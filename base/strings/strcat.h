dd04b573fa8b6ba1c771a3eb2de5a3d90e9bd132
Daniel Bratell <bratell@opera.com>
2018-03-06 16:43:48 +0000

    Resolve conflict between base::StrCat and Win32 StrCat
    StrCat is a Win32 API function and it uses a macro to redirect
    StrCat calls to StrCatW. That macro will be available when
    shlwapi.h has been included but not otherwise so mentions of
    StrCat will sometimes refer to StrCat, and sometimes to StrCatW.
    
    This patch makes it always be StrCatW as in many other functions.
    
    Alternatives are to rename base::StrCat or undef StrCat whenever
    shlwapi.h is included.
    
    Bug: 817738
    Change-Id: I6fca88d7514867ef15e63c8498ddac6a5e9f43dc
    Reviewed-on: https://chromium-review.googlesource.com/950923
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Daniel Bratell <bratell@opera.com>
    Cr-Commit-Position: refs/heads/master@{#541122}

--- base/strings/strcat.h
+++ base/strings/strcat.h
@@ -12,6 +12,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/containers/span.h"
 #include "base/strings/string_piece.h"
+#include "build/build_config.h"
+
+#if defined(OS_WIN)
+// To resolve a conflict with Win32 API StrCat macro.
+#include "base/win/windows_types.h"
+#endif
 
 namespace base {
 
