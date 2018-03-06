e602af180972b7cae3ead60d48aa6a676a26cb80
Yutaka Hirano <yhirano@chromium.org>
2018-03-06 13:49:09 +0000

    Reject SRI blindly when response is ineligible for validation
    We should reject SRI blindly when the response is not eligible for
    integrity validation. The logic is correctly implemented in
    ResourceLoader but not in FetchManager.
    
    Bug: 812667
    Change-Id: If0c29e2e541a1bb3edb09a72fbba3ae55c134303
    Reviewed-on: https://chromium-review.googlesource.com/948229
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541092}

--- third_party/WebKit/Source/platform/loader/SubresourceIntegrity.h
+++ third_party/WebKit/Source/platform/loader/SubresourceIntegrity.h
@@ -22,7 +22,7 @@ class PLATFORM_EXPORT SubresourceIntegrity final {
   STATIC_ONLY(SubresourceIntegrity);
 
  public:
-  class ReportInfo final {
+  class PLATFORM_EXPORT ReportInfo final {
    public:
     enum class UseCounterFeature {
       kSRIElementWithMatchingIntegrityAttribute,
