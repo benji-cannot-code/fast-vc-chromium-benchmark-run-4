757c67517d532eca29e428047729ad7c8dbf3139
Scott Violet <sky@chromium.org>
2018-03-06 20:10:28 +0000

    Moves remaining ui interfaces to once-callback
    BUG=714018
    TEST=none
    
    Change-Id: I29070e75f7040f50f8e4b2c0e4fd738913a4cf76
    Reviewed-on: https://chromium-review.googlesource.com/949086
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541179}

--- services/ui/ws/window_tree_client_unittest.cc
+++ services/ui/ws/window_tree_client_unittest.cc
@@ -434,14 +434,14 @@ class TestWindowTreeClient : public mojom::WindowTreeClient,
                    uint32_t key_state,
                    const gfx::Point& position,
                    uint32_t effect_bitmask,
-                   const OnDragEnterCallback& callback) override {
+                   OnDragEnterCallback callback) override {
     NOTIMPLEMENTED();
   }
   void OnDragOver(Id window,
                   uint32_t key_state,
                   const gfx::Point& position,
                   uint32_t effect_bitmask,
-                  const OnDragOverCallback& callback) override {
+                  OnDragOverCallback callback) override {
     NOTIMPLEMENTED();
   }
   void OnDragLeave(Id window) override { NOTIMPLEMENTED(); }
@@ -449,7 +449,7 @@ class TestWindowTreeClient : public mojom::WindowTreeClient,
                       uint32_t key_state,
                       const gfx::Point& position,
                       uint32_t effect_bitmask,
-                      const OnCompleteDropCallback& callback) override {
+                      OnCompleteDropCallback callback) override {
     NOTIMPLEMENTED();
   }
 
@@ -522,8 +522,8 @@ class TestWindowTreeClient : public mojom::WindowTreeClient,
                         const gfx::Vector2d& drag_image_offset,
                         ui::mojom::PointerKind source) override {}
   void WmMoveDragImage(const gfx::Point& screen_location,
-                       const WmMoveDragImageCallback& callback) override {
-    callback.Run();
+                       WmMoveDragImageCallback callback) override {
+    std::move(callback).Run();
   }
   void WmDestroyDragImage() override {}
   void WmPerformMoveLoop(uint32_t change_id,
