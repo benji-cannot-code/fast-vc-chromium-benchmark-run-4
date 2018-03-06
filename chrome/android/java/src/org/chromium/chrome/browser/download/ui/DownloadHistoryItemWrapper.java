b35187b36ae7b87b4fd1ca8ebdace12fbe2843a8
Shakti Sahu <shaktisahu@chromium.org>
2018-03-06 22:08:29 +0000

    Thumbnails : Fetch thumbnails from offline item backend for download home
    Added basic support to download home to be able to fetch thumbnails from
    the offline pages backend. It doesn't have any caching which will be
    added in future CLs.
    
    Bug: 817853
    Change-Id: I2885f649fb240fa9a28328b6af83a7e49897b092
    Reviewed-on: https://chromium-review.googlesource.com/946904
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541213}

--- chrome/android/java/src/org/chromium/chrome/browser/download/ui/DownloadHistoryItemWrapper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/ui/DownloadHistoryItemWrapper.java
@@ -27,6 +27,7 @@ import org.chromium.components.offline_items_collection.OfflineItem;
 import org.chromium.components.offline_items_collection.OfflineItem.Progress;
 import org.chromium.components.offline_items_collection.OfflineItemFilter;
 import org.chromium.components.offline_items_collection.OfflineItemState;
+import org.chromium.components.offline_items_collection.VisualsCallback;
 import org.chromium.components.url_formatter.UrlFormatter;
 import org.chromium.ui.widget.Toast;
 
@@ -177,6 +178,9 @@ public abstract class DownloadHistoryItemWrapper extends TimedItem {
 
     /** @return The file extension type. See list at the top of the file. */
     public abstract int getFileExtensionType();
+
+    /** Requests the backend provider to provide thumbnail for this item. */
+    public abstract void requestVisualsFromProvider(VisualsCallback callback);
 
     /** @return How much of the download has completed, or null if there is no progress. */
     abstract Progress getDownloadProgress();
@@ -349,6 +353,9 @@ public abstract class DownloadHistoryItemWrapper extends TimedItem {
 
             return mFileExtensionType;
         }
+
+        @Override
+        public void requestVisualsFromProvider(VisualsCallback callback) {}
 
         @Override
         public Progress getDownloadProgress() {
@@ -557,6 +564,11 @@ public abstract class DownloadHistoryItemWrapper extends TimedItem {
         public int getFileExtensionType() {
             // TODO(shaktisahu): Fix this.
             return FILE_EXTENSION_OTHER;
+        }
+
+        @Override
+        public void requestVisualsFromProvider(VisualsCallback callback) {
+            getOfflineContentProvider().getVisualsForItem(mItem.id, callback);
         }
 
         @Override
