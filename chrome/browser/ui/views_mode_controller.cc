6558ea956bf9bd4bb500b57322bebe3e53f2434b
Leonard Grey <lgrey@chromium.org>
2018-03-06 16:50:53 +0000

    [Mac] Move Polychrome feature to ui/base
    This is needed because ShowModal needs to know if a Polychrome browser
    is in Views or Cocoa, and it live in components/, which can't depend on
    chrome/
    
    This leaves the views_mode_controller version of |IsViewsBrowserCocoa|
    in place to avoid changing all its callers.
    
    Bug: 817302
    Change-Id: Ia8e06927410fb9aad3fe696a2446418b4d115a7c
    Reviewed-on: https://chromium-review.googlesource.com/949362
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541125}

--- chrome/browser/ui/views_mode_controller.cc
+++ chrome/browser/ui/views_mode_controller.cc
@@ -6,13 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views_mode_controller.h"
 
 #include "chrome/common/chrome_features.h"
+#include "ui/base/ui_base_features.h"
 
 #if defined(OS_MACOSX) && BUILDFLAG(MAC_VIEWS_BROWSER)
 
 namespace views_mode_controller {
 
 bool IsViewsBrowserCocoa() {
-  return !base::FeatureList::IsEnabled(features::kViewsBrowserWindows);
+  return features::IsViewsBrowserCocoa();
 }
 
 }  // namespace views_mode_controller
