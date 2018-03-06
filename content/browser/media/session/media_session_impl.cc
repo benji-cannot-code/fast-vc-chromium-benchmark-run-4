0401a7bf88c4035938967d04bc896ac277838bc0
Chris Kuiper <ckuiper@google.com>
2018-03-06 21:54:46 +0000

    Add function to set the MediaSession ducking multiplier
    This allows an app (Chromecast, e.g.) to change the multiplier and better
    control ducking behavior.
    
    Bug: internal b/69630398
    Test: Ran on Android Things speaker with different multipliers,
          observed proper ducking volume.
    
    Change-Id: I5078fd11606a5d648aca9b17938c3c8caefd3826
    Reviewed-on: https://chromium-review.googlesource.com/942055
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Stephen Lanham <slan@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Commit-Queue: Chris Kuiper <ckuiper@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541209}

--- content/browser/media/session/media_session_impl.cc
+++ content/browser/media/session/media_session_impl.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 
 #include "base/memory/ptr_util.h"
+#include "base/numerics/ranges.h"
 #include "content/browser/media/session/audio_focus_delegate.h"
 #include "content/browser/media/session/media_session_controller.h"
 #include "content/browser/media/session/media_session_player_observer.h"
@@ -30,8 +31,8 @@ using MediaSessionUserAction = MediaSessionUmaHelper::MediaSessionUserAction;
 
 namespace {
 
-const double kDefaultVolumeMultiplier = 1.0;
-const double kDuckingVolumeMultiplier = 0.2;
+const double kUnduckedVolumeMultiplier = 1.0;
+const double kDefaultDuckingVolumeMultiplier = 0.2;
 
 using MapRenderFrameHostToDepth = std::map<RenderFrameHost*, size_t>;
 
@@ -424,6 +425,10 @@ bool MediaSessionImpl::IsActuallyPaused() const {
   return !IsActive();
 }
 
+void MediaSessionImpl::SetDuckingVolumeMultiplier(double multiplier) {
+  ducking_volume_multiplier_ = base::ClampToRange(multiplier, 0.0, 1.0);
+}
+
 void MediaSessionImpl::StartDucking() {
   if (is_ducking_)
     return;
@@ -446,7 +451,7 @@ void MediaSessionImpl::UpdateVolumeMultiplier() {
 }
 
 double MediaSessionImpl::GetVolumeMultiplier() const {
-  return is_ducking_ ? kDuckingVolumeMultiplier : kDefaultVolumeMultiplier;
+  return is_ducking_ ? ducking_volume_multiplier_ : kUnduckedVolumeMultiplier;
 }
 
 bool MediaSessionImpl::IsActive() const {
@@ -533,7 +538,8 @@ void MediaSessionImpl::OnSuspendInternal(SuspendType suspend_type,
   }
 
   for (const auto& it : pepper_players_)
-    it.observer->OnSetVolumeMultiplier(it.player_id, kDuckingVolumeMultiplier);
+    it.observer->OnSetVolumeMultiplier(it.player_id,
+                                       ducking_volume_multiplier_);
 
   NotifyAboutStateChange();
 }
@@ -559,6 +565,7 @@ MediaSessionImpl::MediaSessionImpl(WebContents* web_contents)
       audio_focus_type_(
           AudioFocusManager::AudioFocusType::GainTransientMayDuck),
       is_ducking_(false),
+      ducking_volume_multiplier_(kDefaultDuckingVolumeMultiplier),
       routed_service_(nullptr) {
 #if defined(OS_ANDROID)
   session_android_.reset(new MediaSessionAndroid(this));
@@ -714,7 +721,7 @@ void MediaSessionImpl::DidReceiveAction(
     for (const auto& player : pepper_players_) {
       if (player.observer->render_frame_host() != rfh_of_routed_service) {
         player.observer->OnSetVolumeMultiplier(player.player_id,
-                                               kDuckingVolumeMultiplier);
+                                               ducking_volume_multiplier_);
       }
     }
     for (const auto& player : one_shot_players_) {
