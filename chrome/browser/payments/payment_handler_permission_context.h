62e3cb9346c25749d5abbcf70e608344db4b0f7a
Jinho Bang <jinho.bang@samsung.com>
2018-03-06 00:04:57 +0000

    PaymentHandler: Disable permission prompt for payment handler
    As discussed in crrev.com/934741, we no longer want to show the
    permission prompt.
    
    Bug: 665949, 816980
    Change-Id: I80f5bdae056df841d8edea797d8578aacfaf6015
    Reviewed-on: https://chromium-review.googlesource.com/939882
    Reviewed-by: Raymes Khoury <raymes@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Jinho Bang <jinho.bang@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#540995}

--- chrome/browser/payments/payment_handler_permission_context.h
+++ chrome/browser/payments/payment_handler_permission_context.h
@@ -9,7 +9,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "chrome/browser/permissions/permission_context_base.h"
 
+class GURL;
+class PermissionRequestID;
 class Profile;
+
+namespace content {
+class WebContents;
+}  // namespace content
 
 namespace payments {
 
@@ -20,6 +26,12 @@ class PaymentHandlerPermissionContext : public PermissionContextBase {
 
  private:
   // PermissionContextBase
+  void DecidePermission(content::WebContents* web_contents,
+                        const PermissionRequestID& id,
+                        const GURL& requesting_origin,
+                        const GURL& embedding_origin,
+                        bool user_gesture,
+                        const BrowserPermissionCallback& callback) override;
   bool IsRestrictedToSecureOrigins() const override;
 
   DISALLOW_COPY_AND_ASSIGN(PaymentHandlerPermissionContext);
