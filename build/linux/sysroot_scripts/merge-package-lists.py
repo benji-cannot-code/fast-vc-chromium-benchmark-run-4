8fd82b0e6b2e35e91e308dacaa65ddb9bbba21e8
Tom Anderson <thomasanderson@chromium.org>
2018-03-06 01:39:14 +0000

    Remove the Debian stretch sysroot
    The sid sysroot change seems to have stuck.  Now remove the stretch sysroot so
    we don't have to maintain both of them.
    
    R=thestig
    
    Change-Id: I5ae295143a3ca2db70299ea0773fd316ff04ac48
    Reviewed-on: https://chromium-review.googlesource.com/950310
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541023}

--- build/linux/sysroot_scripts/merge-package-lists.py
+++ build/linux/sysroot_scripts/merge-package-lists.py
@@ -7,10 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 """Merge package entries from different package lists.
 """
 
-# This is used for replacing packages in eg. stretch with those in
-# stretch-updates.  The updated packages are ABI compatible, but
-# include security patches, so we should use those instead in our
-# sysroots.
+# This is used for replacing packages in eg. sid with those in experimental.
+# The updated packages are ABI compatible, but include security patches, so we
+# should use those instead in our sysroots.
 
 import sys
 
--- build/linux/sysroot_scripts/sysroot-creator.sh
+++ build/linux/sysroot_scripts/sysroot-creator.sh
@@ -4,13 +4,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 #
 # This script should not be run directly but sourced by the other
-# scripts (e.g. sysroot-creator-stretch.sh).  Its up to the parent scripts
+# scripts (e.g. sysroot-creator-sid.sh).  Its up to the parent scripts
 # to define certain environment variables: e.g.
 #  DISTRO=debian
-#  DIST=stretch
+#  DIST=sid
 #  # Similar in syntax to /etc/apt/sources.list
-#  APT_SOURCES_LIST="http://ftp.us.debian.org/debian/ stretch main"
-#  KEYRING_FILE=debian-archive-stretch-stable.gpg
+#  APT_SOURCES_LIST="http://ftp.us.debian.org/debian/ sid main"
+#  KEYRING_FILE=debian-archive-sid-stable.gpg
 #  DEBIAN_PACKAGES="gcc libz libssl"
 
 #@ This script builds Debian/Ubuntu sysroot images for building Google Chrome.
