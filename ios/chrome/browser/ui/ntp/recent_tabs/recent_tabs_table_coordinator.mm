89239dc70f041a8740154d528dce3da01cc4dcd0
sczs <sczs@chromium.org>
2018-03-06 17:22:21 +0000

    [ios] Moves implementation from Legacy to New RecentTabsVC.
    - Moves implementation from Legacy to New RecentTabsVC.
    - Adds some delegate methods to recent_table_coordinator so it supports dismissal.
    - Creates a mediator and hooks it up to the RecentTabsTableVC.
    
    Screenshot:
    https://drive.google.com/open?id=1xahYQ4Tw-ipPeE--LZkNTUZezx98aR4n
    
    Bug: 805135
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I2d14751ca78d19abe84211ea42e291ff1ce3cf7d
    Reviewed-on: https://chromium-review.googlesource.com/942185
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541131}

--- ios/chrome/browser/ui/ntp/recent_tabs/recent_tabs_table_coordinator.mm
+++ ios/chrome/browser/ui/ntp/recent_tabs/recent_tabs_table_coordinator.mm
@@ -5,6 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/ntp/recent_tabs/recent_tabs_table_coordinator.h"
 
+#include "base/ios/block_types.h"
+#include "base/mac/foundation_util.h"
+#import "ios/chrome/browser/ui/ntp/recent_tabs/recent_tabs_handset_view_controller.h"
+#import "ios/chrome/browser/ui/ntp/recent_tabs/recent_tabs_mediator.h"
 #import "ios/chrome/browser/ui/ntp/recent_tabs/recent_tabs_table_view_controller.h"
 #import "ios/chrome/browser/ui/table_view/table_container_view_controller.h"
 #import "ios/chrome/browser/ui/util/form_sheet_navigation_controller.h"
@@ -15,15 +19,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "This file requires ARC support."
 #endif
 
-@interface RecentTabsTableCoordinator ()
+// TODO(crbug.com/805135): Remove RecentTabsHandsetViewControllerCommand and
+// recent_tabs_handset_view_controller.h import. We need this to dismiss for
+// now, but it can be improved.
+@interface RecentTabsTableCoordinator ()<RecentTabsHandsetViewControllerCommand>
+// Completion block called once the recentTabsViewController is dismissed.
+@property(nonatomic, copy) ProceduralBlock completion;
+// Mediator being managed by this Coordinator.
+@property(nonatomic, strong) RecentTabsMediator* mediator;
 // ViewController being managed by this Coordinator.
 @property(nonatomic, strong)
     TableContainerViewController* recentTabsContainerViewController;
 @end
 
 @implementation RecentTabsTableCoordinator
+@synthesize completion = _completion;
 @synthesize dispatcher = _dispatcher;
 @synthesize loader = _loader;
+@synthesize mediator = _mediator;
 @synthesize recentTabsContainerViewController =
     _recentTabsContainerViewController;
 
@@ -34,6 +47,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   recentTabsTableViewController.browserState = self.browserState;
   recentTabsTableViewController.loader = self.loader;
   recentTabsTableViewController.dispatcher = self.dispatcher;
+  recentTabsTableViewController.handsetCommandHandler = self;
+
+  // Initialize and configure RecentTabsMediator.
+  self.mediator = [[RecentTabsMediator alloc] init];
+  self.mediator.browserState = self.browserState;
+  self.mediator.consumer = recentTabsTableViewController;
+  [self.mediator initObservers];
+  [self.mediator reloadSessions];
 
   // Initialize and configure RecentTabsViewController.
   self.recentTabsContainerViewController = [[TableContainerViewController alloc]
@@ -59,9 +80,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)stop {
-  [self.recentTabsContainerViewController dismissViewControllerAnimated:YES
-                                                             completion:nil];
+  // TODO(crbug.com/805135): Move this dismissal code to RecentTabsContainerVC
+  // once its created. Remove "base/mac/foundation_util.h" import then.
+  RecentTabsTableViewController* recentTabsTableViewController =
+      base::mac::ObjCCastStrict<RecentTabsTableViewController>(
+          self.recentTabsContainerViewController.tableViewController);
+  [recentTabsTableViewController dismissModals];
+  [self.recentTabsContainerViewController
+      dismissViewControllerAnimated:YES
+                         completion:self.completion];
   self.recentTabsContainerViewController = nil;
+  [self.mediator disconnect];
+}
+
+#pragma mark - RecentTabsHandsetViewControllerCommand
+
+- (void)dismissRecentTabsWithCompletion:(void (^)())completion {
+  self.completion = completion;
+  [self stop];
 }
 
 @end
