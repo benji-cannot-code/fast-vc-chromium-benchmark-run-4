4cf8d22eec40c6e4179782ac83958d8f51dcab13
Fernando Serboncini <fserb@chromium.org>
2018-03-07 16:46:11 +0000

    Add support for mid ligature position on ShapeResult
    Makes XPositionForOffset and CharacterIndexForPosition support
    return results inside a ligature glyph (while still not breaking ZWJs)
    by using a cursor-based TextBreakIterator inside glyphs.
    
    This also changes how InlineTextBoxPainter handles selection paint, to
    support partial glyph selection. More specifically, for text selection
    we now render the selected glyphs twice: once in the normal text style,
    clipped out of the selection area and once in the selection style,
    clipped in the selection area.
    
    Bug: 473476
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I121d03b436de88b367047efb8342cd975047a9ea
    Reviewed-on: https://chromium-review.googlesource.com/752342
    Commit-Queue: Fernando Serboncini <fserb@chromium.org>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541474}

--- third_party/WebKit/Source/core/layout/line/InlineTextBox.h
+++ third_party/WebKit/Source/core/layout/line/InlineTextBox.h
@@ -206,8 +206,7 @@ class CORE_EXPORT InlineTextBox : public InlineBox {
   LayoutUnit TextPos() const;
 
  public:
-  virtual int OffsetForPosition(LayoutUnit x,
-                                bool include_partial_glyphs = true) const;
+  virtual int OffsetForPosition(LayoutUnit x, OffsetForPositionType) const;
   virtual LayoutUnit PositionForOffset(int offset) const;
 
   // Returns false for offset after line break.
--- third_party/WebKit/Source/core/paint/InlineTextBoxPainter.h
+++ third_party/WebKit/Source/core/paint/InlineTextBoxPainter.h
@@ -86,6 +86,12 @@ class InlineTextBoxPainter {
                       Color text_color,
                       LayoutTextCombine* = nullptr);
 
+  template <PaintOptions>
+  LayoutRect GetSelectionRect(const LayoutRect& box_rect,
+                              const ComputedStyle&,
+                              const Font&,
+                              LayoutTextCombine* = nullptr);
+
   void PaintStyleableMarkerUnderline(GraphicsContext&,
                                      const LayoutPoint& box_origin,
                                      const StyleableMarker&,
