8bc27ab9b53d7eadbb283a90747b5e78f38875af
Makoto Shimazu <shimazu@chromium.org>
2018-03-07 06:22:27 +0000

    ServiceWorker: Changed old CHECKs for debugging to DCHECKs
    R=nhiroki@chromium.org
    
    Bug: 736203, 750267
    Change-Id: Ie51d9a7ab06367ac2a67b5919dda0ac1e79baaec
    Reviewed-on: https://chromium-review.googlesource.com/952783
    Commit-Queue: Makoto Shimazu <shimazu@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541367}

--- content/browser/service_worker/service_worker_provider_host.cc
+++ content/browser/service_worker/service_worker_provider_host.cc
@@ -110,8 +110,7 @@ class ServiceWorkerURLTrackingRequestHandler
 void RemoveProviderHost(base::WeakPtr<ServiceWorkerContextCore> context,
                         int process_id,
                         int provider_id) {
-  // Temporary CHECK for debugging https://crbug.com/750267.
-  CHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
   TRACE_EVENT0("ServiceWorker",
                "ServiceWorkerProviderHost::RemoveProviderHost");
   if (!context || !context->GetProviderHost(process_id, provider_id)) {
@@ -221,8 +220,7 @@ ServiceWorkerProviderHost::ServiceWorkerProviderHost(
 }
 
 ServiceWorkerProviderHost::~ServiceWorkerProviderHost() {
-  // Temporary CHECK for debugging https://crbug.com/750267.
-  CHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
   if (context_)
     context_->UnregisterProviderHostByClientID(client_uuid_);
 
