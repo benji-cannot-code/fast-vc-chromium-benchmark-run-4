6f00f567426b57631de6293942d898c15d368556
Conley Owens <cco3@chromium.org>
2018-03-07 20:19:37 +0000

    Add a SubresourceLoadInfo struct to content/
    This data type is useful for sending stats from the renderer to the
    browser, in SubresourceResponseStarted methods.  Currently there are six
    fields that are passed around, but soon there may be more in order to
    get PageLoadMetrics working with the Network Service.
    
    BUG=816684
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I26a62ebb5aa82f225a70fe54098433c5c3342941
    Reviewed-on: https://chromium-review.googlesource.com/947582
    Commit-Queue: Conley Owens <cco3@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Jay Civelli <jcivelli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541550}

--- content/public/browser/web_contents_observer.h
+++ content/public/browser/web_contents_observer.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/visibility.h"
 #include "content/public/common/frame_navigate_params.h"
 #include "content/public/common/resource_type.h"
+#include "content/public/common/subresource_load_info.mojom.h"
 #include "ipc/ipc_listener.h"
 #include "mojo/public/cpp/system/message_pipe.h"
 #include "services/service_manager/public/cpp/bind_source_info.h"
@@ -258,11 +259,8 @@ class CONTENT_EXPORT WebContentsObserver : public IPC::Listener {
 
   // This method is invoked when a response has been received for a subresource
   // request.
-  virtual void SubresourceResponseStarted(const GURL& url,
-                                          const GURL& referrer,
-                                          const std::string& method,
-                                          ResourceType resource_type,
-                                          const std::string& ip) {}
+  virtual void SubresourceResponseStarted(
+      const mojom::SubresourceLoadInfoPtr& subresource_load_info) {}
 
   // This method is invoked when a new non-pending navigation entry is created.
   // This corresponds to one NavigationController entry being created
