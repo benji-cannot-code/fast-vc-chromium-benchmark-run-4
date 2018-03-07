b9a3b758a3cb342507131f4b0060eca362796a30
Leonard Grey <lgrey@chromium.org>
2018-03-07 18:10:24 +0000

    MacViews: reland 'Shim |ShowModalDialog| for Polychrome'
    This was originally https://chromium-review.googlesource.com/c/chromium/src/+/929942
    but was reverted because of the hacky way it implemented a components/ function
    in Chrome.
    
    It's now rebased on https://chromium-review.googlesource.com/c/chromium/src/+/949362
    which made that behavior unnecessary.
    
    Background:
    Polychrome is an intermediate step to shipping MacViews browser.
    Currently, switching between Cocoa browser and MacViews browser requires
    a buildflag. Polychrome builds both Cocoa and MacViews into the same
    binary and allows switching between them based on a feature flag.
    More details at crbug.com/802257
    
    As Cocoa and (much of) Views were not typically built together,
    some symbols were reused. With Polychrome, these are duplicate symbols.
    This CL resolves the issue by renaming the Cocoa symbols, and then
    shimming them for the (regular, not Polychrome) Cocoa build.
    
    Bug: 808017
    Change-Id: Ia42c202e474614c97759aab28c88f580bc0ffabd
    Reviewed-on: https://chromium-review.googlesource.com/950063
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541500}

--- components/constrained_window/show_modal_dialog_views.cc
+++ components/constrained_window/show_modal_dialog_views.cc
@@ -5,17 +5,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "build/build_config.h"
 #include "components/constrained_window/constrained_window_views.h"
 #include "components/constrained_window/native_web_contents_modal_dialog_manager_views.h"
 #include "components/web_modal/single_web_contents_dialog_manager.h"
 #include "components/web_modal/web_contents_modal_dialog_manager.h"
 #include "content/public/browser/web_contents.h"
+#include "ui/base/ui_base_features.h"
 #include "ui/gfx/native_widget_types.h"
 
 namespace constrained_window {
 
 void ShowModalDialog(gfx::NativeWindow dialog,
                      content::WebContents* web_contents) {
+#if defined(OS_MACOSX)
+  if (features::IsViewsBrowserCocoa())
+    return ShowModalDialogCocoa(dialog, web_contents);
+#endif
   web_modal::WebContentsModalDialogManager* manager =
       web_modal::WebContentsModalDialogManager::FromWebContents(web_contents);
   DCHECK(manager);
