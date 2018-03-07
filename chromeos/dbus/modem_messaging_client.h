264fada8a8ac2821c7ff4f90a737eb2337f7b587
Hidehiko Abe <hidehiko@chromium.org>
2018-03-07 02:43:24 +0000

    Migrate ModemMessagingClient into DBusMethodCallback.
    BUG=739622
    TEST=Ran chromeos_unittests.
    
    Change-Id: Id389c09d95edae181f7862174ab58de765b0ee22
    Reviewed-on: https://chromium-review.googlesource.com/937741
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541295}

--- chromeos/dbus/modem_messaging_client.h
+++ chromeos/dbus/modem_messaging_client.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "chromeos/chromeos_export.h"
 #include "chromeos/dbus/dbus_client.h"
+#include "chromeos/dbus/dbus_method_call_status.h"
 
 namespace dbus {
 class ObjectPath;
@@ -26,11 +27,8 @@ namespace chromeos {
 // initializes the DBusThreadManager instance.
 class CHROMEOS_EXPORT ModemMessagingClient : public DBusClient {
  public:
-  typedef base::Callback<void()> DeleteCallback;
   typedef base::Callback<void(const dbus::ObjectPath& message_path,
                               bool complete)> SmsReceivedHandler;
-  typedef base::Callback<void(const std::vector<dbus::ObjectPath>& paths)>
-      ListCallback;
 
   ~ModemMessagingClient() override;
 
@@ -47,16 +45,17 @@ class CHROMEOS_EXPORT ModemMessagingClient : public DBusClient {
   virtual void ResetSmsReceivedHandler(const std::string& service_name,
                                        const dbus::ObjectPath& object_path) = 0;
 
-  // Calls Delete method.  |callback| is called after the method call succeeds.
+  // Calls Delete method.  |callback| is called on method completion.
   virtual void Delete(const std::string& service_name,
                       const dbus::ObjectPath& object_path,
                       const dbus::ObjectPath& sms_path,
-                      const DeleteCallback& callback) = 0;
+                      VoidDBusMethodCallback callback) = 0;
 
-  // Calls List method.  |callback| is called after the method call succeeds.
+  // Calls List method.  |callback| is called on method completion.
+  using ListCallback = DBusMethodCallback<std::vector<dbus::ObjectPath>>;
   virtual void List(const std::string& service_name,
                     const dbus::ObjectPath& object_path,
-                    const ListCallback& callback) = 0;
+                    ListCallback callback) = 0;
 
  protected:
   friend class ModemMessagingClientTest;
