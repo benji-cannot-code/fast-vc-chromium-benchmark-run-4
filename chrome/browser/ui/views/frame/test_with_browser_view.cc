694328205bccd7abb1206b068a69a83121bb8533
Leonard Grey <lgrey@chromium.org>
2018-03-07 17:51:54 +0000

    Polychrome: fix reference to Polychrome feature in test
    The setup for this test refers to features::kViewsBrowserWindows which
    was moved to ui/base in https://chromium-review.googlesource.com/c/chromium/src/+/949362
    
    Change-Id: I69ea6064330a094dd041fbe7b244dfae0f806802
    Reviewed-on: https://chromium-review.googlesource.com/952482
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541492}

--- chrome/browser/ui/views/frame/test_with_browser_view.cc
+++ chrome/browser/ui/views/frame/test_with_browser_view.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_MACOSX)
 #include "chrome/common/chrome_features.h"
+#include "ui/base/ui_base_features.h"
 #endif
 
 namespace {
