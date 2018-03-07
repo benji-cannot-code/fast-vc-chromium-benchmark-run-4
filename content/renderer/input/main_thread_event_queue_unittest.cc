25caf2e594ee78bc020fe93bd99a3bdb8206d27a
Dave Tapuska <dtapuska@chromium.org>
2018-03-07 15:03:26 +0000

    Remove coalesced count from MainThreadEventQueue.
    Remove code to log histogram that isn't used anymore.
    
    BUG=819059
    
    Change-Id: I6141aec67fa44ea1c7883659f1b88274ddaa2c26
    Reviewed-on: https://chromium-review.googlesource.com/951710
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541433}

--- content/renderer/input/main_thread_event_queue_unittest.cc
+++ content/renderer/input/main_thread_event_queue_unittest.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/field_trial.h"
 #include "base/strings/string_piece.h"
 #include "base/strings/string_util.h"
-#include "base/test/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/test_simple_task_runner.h"
 #include "build/build_config.h"
@@ -44,9 +43,6 @@ namespace {
 
 // Simulate a 16ms frame signal.
 const base::TimeDelta kFrameInterval = base::TimeDelta::FromMilliseconds(16);
-
-const char* kCoalescedCountHistogram =
-    "Event.MainThreadEventQueue.CoalescedCount";
 
 }  // namespace
 
@@ -252,8 +248,6 @@ class MainThreadEventQueueTest : public testing::Test,
 };
 
 TEST_F(MainThreadEventQueueTest, NonBlockingWheel) {
-  base::HistogramTester histogram_tester;
-
   WebMouseWheelEvent kEvents[4] = {
       SyntheticWebMouseWheelEventBuilder::Build(10, 10, 0, 53, 0, false),
       SyntheticWebMouseWheelEventBuilder::Build(20, 20, 0, 53, 0, false),
@@ -347,13 +341,9 @@ TEST_F(MainThreadEventQueueTest, NonBlockingWheel) {
         WebInputEvent::DispatchType::kListenersNonBlockingPassive;
     EXPECT_EQ(coalesced_event, *coalesced_wheel_event1);
   }
-
-  histogram_tester.ExpectUniqueSample(kCoalescedCountHistogram, 1, 2);
 }
 
 TEST_F(MainThreadEventQueueTest, NonBlockingTouch) {
-  base::HistogramTester histogram_tester;
-
   EXPECT_CALL(renderer_scheduler_,
               DidHandleInputEventOnMainThread(testing::_, testing::_))
       .Times(3);
@@ -447,13 +437,9 @@ TEST_F(MainThreadEventQueueTest, NonBlockingTouch) {
         WebInputEvent::DispatchType::kListenersNonBlockingPassive;
     EXPECT_EQ(coalesced_event, *coalesced_touch_event1);
   }
-
-  histogram_tester.ExpectBucketCount(kCoalescedCountHistogram, 0, 1);
-  histogram_tester.ExpectBucketCount(kCoalescedCountHistogram, 1, 1);
 }
 
 TEST_F(MainThreadEventQueueTest, BlockingTouch) {
-  base::HistogramTester histogram_tester;
   SyntheticWebTouchEvent kEvents[4];
   kEvents[0].PressPoint(10, 10);
   kEvents[1].PressPoint(10, 10);
@@ -504,7 +490,6 @@ TEST_F(MainThreadEventQueueTest, BlockingTouch) {
   EXPECT_THAT(GetAndResetCallbackResults(),
               testing::Each(ReceivedCallback(
                   CallbackReceivedState::kCalledWhileHandlingEvent, false)));
-  histogram_tester.ExpectUniqueSample(kCoalescedCountHistogram, 2, 2);
 }
 
 TEST_F(MainThreadEventQueueTest, InterleavedEvents) {
