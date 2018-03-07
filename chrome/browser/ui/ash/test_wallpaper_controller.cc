9c28d027a9450638e6ba58b35519b05db0c1c0f1
Wenzhao Zang <wzang@chromium.org>
2018-03-07 03:00:01 +0000

    cros: Support preview for custom wallpapers (Part I)
    1) Didn't implement it using a dummy html background since we want to
       reuse code in WallpaperResizer.
    
    2) The original idea was to create ShowPreviewWallpaper(imageSkia)
       and do not pass any other info to //ash until the user confirms the
       choice. But this approach requires lots of changes within
       wallpaper_private_api.cc. Instead, send all the info one-time and
       let //ash maintain the information until user either confirms or
       cancels the preview.
    
    (Please see bug for the feature description.)
    
    Screenshots:
    https://goo.gl/p6KKF8
    
          and select an image under 'My photos'.
    
    Bug: 818075
    Test: Turn on 'enable-new-wallpaper-picker' under chrome://flags,
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I86d2a82f0329bf0c357ee196301dcb0b1eef271f
    Reviewed-on: https://chromium-review.googlesource.com/945075
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Oliver Chang <ochang@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Wenzhao (Colin) Zang <wzang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541299}

--- chrome/browser/ui/ash/test_wallpaper_controller.cc
+++ chrome/browser/ui/ash/test_wallpaper_controller.cc
@@ -35,7 +35,7 @@ void TestWallpaperController::SetCustomWallpaper(
     const std::string& file_name,
     wallpaper::WallpaperLayout layout,
     const SkBitmap& image,
-    bool show_wallpaper) {
+    bool preview_mode) {
   set_custom_wallpaper_count_++;
 }
 
@@ -69,6 +69,14 @@ void TestWallpaperController::SetPolicyWallpaper(
 }
 
 void TestWallpaperController::SetDeviceWallpaperPolicyEnforced(bool enforced) {
+  NOTIMPLEMENTED();
+}
+
+void TestWallpaperController::ConfirmPreviewWallpaper() {
+  NOTIMPLEMENTED();
+}
+
+void TestWallpaperController::CancelPreviewWallpaper() {
   NOTIMPLEMENTED();
 }
 
