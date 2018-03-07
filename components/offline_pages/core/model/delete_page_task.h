5fdcff736e7223af3f5e7942e59b7c7994441ad3
Dan Harrington <harringtond@chromium.org>
2018-03-07 22:55:46 +0000

    Remove OfflinePageMetadataStore
    OfflinePageMetadataStore was only referenced in the unittest, now it has been
    removed. A few methods/types were migrated to OfflinePageMetadataStoreSQL,
    others were removed because the were no longer in use.
    
    I removed some tests that only tested the removed methods. Other methods
    that were used to test the database and were moved to the test file, like
    GetOfflinePages and AddOfflinePage.
    
    offline_page_metadata_store_unittest.cc will be renamed
    offline_page_metadata_store_sql_unittest.cc in a follow-up.
    
    Bug: 778813
    Change-Id: I6315cd5fb43b8a56b84dcede8c388286dd96e39c
    Reviewed-on: https://chromium-review.googlesource.com/949502
    Reviewed-by: Yafei Duan <romax@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Commit-Queue: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541621}

--- components/offline_pages/core/model/delete_page_task.h
+++ components/offline_pages/core/model/delete_page_task.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
-#include "components/offline_pages/core/offline_page_metadata_store.h"
+#include "components/offline_pages/core/offline_page_metadata_store_sql.h"
 #include "components/offline_pages/core/offline_page_model.h"
 #include "components/offline_pages/core/offline_page_types.h"
 #include "components/offline_pages/core/task.h"
--- components/offline_pages/core/model/offline_page_model_taskified.cc
+++ components/offline_pages/core/model/offline_page_model_taskified.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/offline_pages/core/model/persistent_pages_consistency_check_task.h"
 #include "components/offline_pages/core/model/temporary_pages_consistency_check_task.h"
 #include "components/offline_pages/core/offline_page_feature.h"
-#include "components/offline_pages/core/offline_page_metadata_store.h"
 #include "components/offline_pages/core/offline_page_metadata_store_sql.h"
 #include "components/offline_pages/core/offline_page_model.h"
 #include "components/offline_pages/core/offline_store_utils.h"
