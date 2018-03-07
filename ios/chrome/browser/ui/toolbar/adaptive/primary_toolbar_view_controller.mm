b0a9013e7313735ae29f4f9ea475630bb206a3ec
Justin Cohen <justincohen@google.com>
2018-03-07 16:39:34 +0000

    [ios] Restore refresh ntp toolbars.
    Followup to https://chromium-review.googlesource.com/c/chromium/src/+/941441, as
    hiding blur will hide the entire toolbar.
    
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I2b051fc0573e45922d35bb695bfd3cdff289b3e8
    Reviewed-on: https://chromium-review.googlesource.com/951748
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541463}

--- ios/chrome/browser/ui/toolbar/adaptive/primary_toolbar_view_controller.mm
+++ ios/chrome/browser/ui/toolbar/adaptive/primary_toolbar_view_controller.mm
@@ -62,14 +62,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   self.view.backgroundColor =
       self.buttonFactory.toolbarConfiguration.NTPBackgroundColor;
-  self.view.blur.hidden = YES;
   self.view.locationBarContainer.hidden = YES;
 }
 
 - (void)resetAfterSideSwipeSnapshot {
   [super resetAfterSideSwipeSnapshot];
   self.view.backgroundColor = nil;
-  self.view.blur.hidden = NO;
   self.view.locationBarContainer.hidden = NO;
 }
 
