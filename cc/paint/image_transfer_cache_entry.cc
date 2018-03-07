e43a71b3387c724db05c6bad9005b1f23eff4014
Adrienne Walker <enne@chromium.org>
2018-03-07 20:06:01 +0000

    oop: Fix transfer cache early out on bad image
    This fixes a fuzzer null dereference.
    
    Bug: 819562
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: Ia2e7138b6fd143ecd187679843f695c81e65bb63
    Reviewed-on: https://chromium-review.googlesource.com/953143
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Commit-Queue: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541541}

--- cc/paint/image_transfer_cache_entry.cc
+++ cc/paint/image_transfer_cache_entry.cc
@@ -132,7 +132,7 @@ bool ServiceImageTransferCacheEntry::Deserialize(
     if (!image)
       return false;
     image_ = image->makeTextureImage(context, nullptr);
-    if (!image)
+    if (!image_)
       return false;
     if (target_color_space) {
       image_ = image_->makeColorSpace(target_color_space,
