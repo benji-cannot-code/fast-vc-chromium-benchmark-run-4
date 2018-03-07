4cf86d3c866a1954b5f88c02a2a4c48d752974f1
Aaron Leventhal <aleventhal@chromium.org>
2018-03-07 22:01:52 +0000

    Pass accessible name tests
    This is split out from the CL to test all widgets and views for nameless
    focusable objects. See crrev.com/c/951933.
    
    Bug: 819350
    Change-Id: I0b8b9b17a69d01986adf328b808c71bacd2fb8fe
    Reviewed-on: https://chromium-review.googlesource.com/953046
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541600}

--- chrome/browser/ui/views/constrained_window_views_browsertest.cc
+++ chrome/browser/ui/views/constrained_window_views_browsertest.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test_utils.h"
 #include "ui/base/accelerators/accelerator.h"
+#include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/focus/focus_manager.h"
 #include "ui/views/widget/widget.h"
 
@@ -28,7 +29,10 @@ namespace {
 
 class TestDialog : public views::DialogDelegateView {
  public:
-  TestDialog() { SetFocusBehavior(FocusBehavior::ALWAYS); }
+  TestDialog() {
+    SetFocusBehavior(FocusBehavior::ALWAYS);
+    GetViewAccessibility().OverrideName("Test dialog");
+  }
   ~TestDialog() override {}
 
   views::View* GetInitiallyFocusedView() override { return this; }
