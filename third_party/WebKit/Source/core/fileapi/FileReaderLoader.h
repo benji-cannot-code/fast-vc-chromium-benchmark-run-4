34d66b05213c1cbec571b509e422c4f5b9a55778
Marijn Kruisselbrink <mek@chromium.org>
2018-03-07 02:50:15 +0000

    [FileAPI] Merge  FileReaderLoader and FileReaderLoaderMojo back together.
    This extra layer of abstraction existed for when mojo blobs wasn't the
    default yet, but now there is only one implementation of FileReaderLoader
    it makes more sense to merge them back together.
    
    First step of some code cleanup in FileReaderLoader. Also reorders methods
    so definitions and declarations are in the same order.
    
    Bug: None
    Change-Id: I5f9fac23b24c19be64e5bef8b5cd4550540861e1
    Reviewed-on: https://chromium-review.googlesource.com/950239
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541297}

--- third_party/WebKit/Source/core/fileapi/FileReaderLoader.h
+++ third_party/WebKit/Source/core/fileapi/FileReaderLoader.h
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include "core/CoreExport.h"
 #include "core/fileapi/FileError.h"
+#include "mojo/public/cpp/bindings/binding.h"
 #include "platform/heap/Handle.h"
 #include "platform/weborigin/KURL.h"
 #include "platform/wtf/Forward.h"
@@ -42,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/wtf/text/TextEncoding.h"
 #include "platform/wtf/text/WTFString.h"
 #include "platform/wtf/typed_arrays/ArrayBufferBuilder.h"
+#include "third_party/WebKit/public/mojom/blob/blob.mojom-blink.h"
 
 namespace blink {
 
@@ -60,7 +62,7 @@ class TextResourceDecoder;
 //
 // Each FileReaderLoader instance is only good for reading one Blob, and will
 // leak resources if used multiple times.
-class CORE_EXPORT FileReaderLoader {
+class CORE_EXPORT FileReaderLoader : public mojom::blink::BlobReaderClient {
   USING_FAST_MALLOC(FileReaderLoader);
 
  public:
@@ -76,10 +78,10 @@ class CORE_EXPORT FileReaderLoader {
   // synchronously.
   static std::unique_ptr<FileReaderLoader> Create(ReadType,
                                                   FileReaderLoaderClient*);
+  FileReaderLoader(ReadType, FileReaderLoaderClient*);
+  ~FileReaderLoader() override;
 
-  virtual ~FileReaderLoader();
-
-  virtual void Start(ExecutionContext*, scoped_refptr<BlobDataHandle>) = 0;
+  void Start(ExecutionContext*, scoped_refptr<BlobDataHandle>);
   void Cancel();
 
   DOMArrayBuffer* ArrayBufferResult();
@@ -107,10 +109,8 @@ class CORE_EXPORT FileReaderLoader {
 
   bool HasFinishedLoading() const { return finished_loading_; }
 
- protected:
-  FileReaderLoader(ReadType, FileReaderLoaderClient*);
-
-  virtual void Cleanup();
+ private:
+  void Cleanup();
   void Failed(FileError::ErrorCode);
 
   void OnStartLoading(long long total_bytes);
@@ -119,11 +119,12 @@ class CORE_EXPORT FileReaderLoader {
 
   bool IsSyncLoad() const { return !client_; }
 
-#if DCHECK_IS_ON()
-  bool started_loading_ = false;
-#endif  // DCHECK_IS_ON()
+  // BlobReaderClient:
+  void OnCalculatedSize(uint64_t total_size,
+                        uint64_t expected_content_size) override;
+  void OnComplete(int32_t status, uint64_t data_length) override;
+  void OnDataPipeReadable(MojoResult);
 
- private:
   void AdjustReportedMemoryUsageToV8(int64_t usage);
   void UnadjustReportedMemoryUsageToV8();
 
@@ -156,6 +157,16 @@ class CORE_EXPORT FileReaderLoader {
   int64_t memory_usage_reported_to_v8_ = 0;
 
   FileError::ErrorCode error_code_ = FileError::kOK;
+
+  mojo::ScopedDataPipeConsumerHandle consumer_handle_;
+  mojo::SimpleWatcher handle_watcher_;
+  mojo::Binding<mojom::blink::BlobReaderClient> binding_;
+  int64_t expected_content_size_ = -1;
+  bool received_all_data_ = false;
+  bool received_on_complete_ = false;
+#if DCHECK_IS_ON()
+  bool started_loading_ = false;
+#endif  // DCHECK_IS_ON()
 };
 
 }  // namespace blink
