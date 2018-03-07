3c0ecbe9bd035af3d82207d936df6ed7335995e3
Jani Hautakangas <jani.hautakangas@lge.com>
2018-03-07 12:08:09 +0000

    GCC build fix: constexpr static data member must be initialized
    static constexpr member data is incorrectly initialized as
    static const data.
    
    Bug: 819294
    
    Change-Id: I96f06032e00b6bd4d2b5df0ed9b466028453912b
    Reviewed-on: https://chromium-review.googlesource.com/945997
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Taiju Tsuiki <tzik@chromium.org>
    Commit-Queue: Jani Hautakangas <jani.hautakangas@lge.com>
    Cr-Commit-Position: refs/heads/master@{#541412}

--- third_party/WebKit/Source/core/loader/IdlenessDetector.cpp
+++ third_party/WebKit/Source/core/loader/IdlenessDetector.cpp
@@ -16,8 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-const TimeDelta IdlenessDetector::kNetworkQuietWindow;
-const TimeDelta IdlenessDetector::kNetworkQuietWatchdog;
+constexpr TimeDelta IdlenessDetector::kNetworkQuietWindow;
+constexpr TimeDelta IdlenessDetector::kNetworkQuietWatchdog;
 
 void IdlenessDetector::Shutdown() {
   Stop();
