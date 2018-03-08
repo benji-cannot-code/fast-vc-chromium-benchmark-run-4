2ea55ce1cf0a4ebd60364e5fa3342d89491346d2
Tommy C. Li <tommycli@chromium.org>
2018-03-08 03:12:50 +0000

    Omnibox UI: Remove DeleteAtEnd dead code.
    Per pkasting@ sugestion, DeleteAtEnd is dead code and can be deleted.
    
    TBR=jdonnelly@chromium.org
    
    Bug: NONE
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Ie1149a3f7b2aecd88252eeda7ff366e9504249da
    Reviewed-on: https://chromium-review.googlesource.com/954222
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541705}

--- chrome/browser/ui/cocoa/omnibox/omnibox_view_mac.h
+++ chrome/browser/ui/cocoa/omnibox/omnibox_view_mac.h
@@ -70,7 +70,6 @@ class OmniboxViewMac : public OmniboxView,
   void SetCaretPos(size_t caret_pos) override;
   void EnterKeywordModeForDefaultSearchProvider() override;
   bool IsSelectAll() const override;
-  bool DeleteAtEndPressed() override;
   void GetSelectionBounds(base::string16::size_type* start,
                           base::string16::size_type* end) const override;
   void SelectAll(bool reversed) override;
@@ -221,9 +220,6 @@ class OmniboxViewMac : public OmniboxView,
 
   // Was delete pressed?
   bool delete_was_pressed_;
-
-  // Was the delete key pressed with an empty selection at the end of the edit?
-  bool delete_at_end_pressed_;
 
   // State used to coalesce changes to text and selection to avoid drawing
   // transient state.
