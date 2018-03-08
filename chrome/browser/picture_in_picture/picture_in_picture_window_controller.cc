a37edb2a587468ff074fbcbc8962ac394cf8577b
Jennifer Apacible <apacible@chromium.org>
2018-03-08 16:56:12 +0000

    [Picture in Picture] Determine initial window bounds.
    Besides the initial window position and size, the user controls this
    behavior. This change makes OverlayWindowViews a views::Widget, which
    gives it more control over windowing behavior. Update sizing behavior
    and determining window bounds, per UX guidelines.
    
    This change also makes minor cosmetic changes (e.g. window type) to
    temporarily make work in progress changes easier.
    
    BUG: 726621
    Change-Id: I4cfbacb276a14ea86e00d11dcbfa17781a02533a
    Reviewed-on: https://chromium-review.googlesource.com/938580
    Commit-Queue: apacible <apacible@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541813}

--- chrome/browser/picture_in_picture/picture_in_picture_window_controller.cc
+++ chrome/browser/picture_in_picture/picture_in_picture_window_controller.cc
@@ -33,7 +33,6 @@ PictureInPictureWindowController::PictureInPictureWindowController(
     : initiator_(initiator) {
   DCHECK(initiator_);
   window_ = OverlayWindow::Create();
-  window_->Init();
 }
 
 void PictureInPictureWindowController::Show() {
