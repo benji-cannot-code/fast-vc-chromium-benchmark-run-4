e9b2d91f0afc52eb7d2dc5cda328bc615426889f
Michael Lippautz <mlippautz@chromium.org>
2018-03-08 09:59:20 +0000

    [oilpan] Rename AdjustAndMark to AdjustPointer
    Reflects the change that there marking is actually implemented in the
    marking visitors and the adjust traits are only used for getting
    the proper descriptions.
    
    Bug: chromium:802273
    Change-Id: Ib5df76a4689696b766c657fc7676780c694760f0
    Reviewed-on: https://chromium-review.googlesource.com/948742
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541762}

--- third_party/WebKit/Source/platform/heap/TraceTraits.h
+++ third_party/WebKit/Source/platform/heap/TraceTraits.h
@@ -43,12 +43,12 @@ class WeakMember;
 template <typename T>
 class WeakPersistent;
 
-template <typename T, bool = NeedsAdjustAndMark<T>::value>
-class AdjustAndMarkTrait;
+template <typename T, bool = NeedsAdjustPointer<T>::value>
+class AdjustPointerTrait;
 
 template <typename T>
-class AdjustAndMarkTrait<T, false> {
-  STATIC_ONLY(AdjustAndMarkTrait);
+class AdjustPointerTrait<T, false> {
+  STATIC_ONLY(AdjustPointerTrait);
 
  public:
   static TraceDescriptor GetTraceDescriptor(void* self) {
@@ -70,8 +70,8 @@ class AdjustAndMarkTrait<T, false> {
 };
 
 template <typename T>
-class AdjustAndMarkTrait<T, true> {
-  STATIC_ONLY(AdjustAndMarkTrait);
+class AdjustPointerTrait<T, true> {
+  STATIC_ONLY(AdjustPointerTrait);
 
  public:
   static TraceDescriptor GetTraceDescriptor(const T* self) {
@@ -191,16 +191,16 @@ class TraceTrait {
 
  public:
   static TraceDescriptor GetTraceDescriptor(void* self) {
-    return AdjustAndMarkTrait<T>::GetTraceDescriptor(static_cast<T*>(self));
+    return AdjustPointerTrait<T>::GetTraceDescriptor(static_cast<T*>(self));
   }
 
   static TraceWrapperDescriptor GetTraceWrapperDescriptor(void* self) {
-    return AdjustAndMarkTrait<T>::GetTraceWrapperDescriptor(
+    return AdjustPointerTrait<T>::GetTraceWrapperDescriptor(
         static_cast<T*>(self));
   }
 
   static HeapObjectHeader* GetHeapObjectHeader(void* self) {
-    return AdjustAndMarkTrait<T>::GetHeapObjectHeader(static_cast<T*>(self));
+    return AdjustPointerTrait<T>::GetHeapObjectHeader(static_cast<T*>(self));
   }
 
   static void Trace(Visitor*, void* self);
@@ -223,7 +223,6 @@ void TraceTrait<T>::TraceWrappers(ScriptWrappableVisitor* visitor, void* self) {
                 "only objects deriving from GarbageCollected can be used");
   visitor->DispatchTraceWrappers(static_cast<T*>(self));
 }
-
 
 template <typename T, typename Traits>
 struct TraceTrait<HeapVectorBacking<T, Traits>> {
