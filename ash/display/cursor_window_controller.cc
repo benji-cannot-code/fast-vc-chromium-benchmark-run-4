5a2c7ab9c8069683c09162ac19e6b642e07c27e0
Weidong Guo <weidongg@chromium.org>
2018-03-08 01:22:07 +0000

    Fix software cursor issue with screen magnification enabled
    After screen magnification is enabled/disabled, the preferences for the
    active profile have not been updated when the cursor compositing is
    being updated. Use MagnificationController::IsEnabled() instead.
    
    BUG=814634
    
    Change-Id: Id72c579bd3395ae2e9748858622df37086bc4220
    Reviewed-on: https://chromium-review.googlesource.com/951639
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Weidong Guo <weidongg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541676}

--- ash/display/cursor_window_controller.cc
+++ ash/display/cursor_window_controller.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ash_constants.h"
 #include "ash/display/mirror_window_controller.h"
 #include "ash/display/window_tree_host_manager.h"
+#include "ash/magnifier/magnification_controller.h"
 #include "ash/public/cpp/ash_pref_names.h"
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/root_window_controller.h"
@@ -126,6 +127,9 @@ bool CursorWindowController::ShouldEnableCursorCompositing() {
     return true;
   }
 
+  if (ash::Shell::Get()->magnification_controller()->IsEnabled())
+    return true;
+
   PrefService* prefs =
       Shell::Get()->session_controller()->GetActivePrefService();
   if (!prefs) {
@@ -134,7 +138,6 @@ bool CursorWindowController::ShouldEnableCursorCompositing() {
   }
   return prefs->GetBoolean(prefs::kAccessibilityLargeCursorEnabled) ||
          prefs->GetBoolean(prefs::kAccessibilityHighContrastEnabled) ||
-         prefs->GetBoolean(prefs::kAccessibilityScreenMagnifierEnabled) ||
          prefs->GetBoolean(prefs::kDockedMagnifierEnabled) ||
          prefs->GetBoolean(prefs::kNightLightEnabled);
 }
