b904572aa9eebf0118f29d7ee3c2bfebab889355
Chandan Padhi <c.padhi@samsung.com>
2018-03-08 11:37:46 +0000

    Introduce InputDeviceInfo interface
    InputDeviceInfo extends MediaDeviceInfo and represents audio and video input
    devices. These objects are the result of calling MediaDevices.enumerateDevices().
    This interface gives access to the capabilities of the input device it represents
    via getCapabilities() method which will be implemented in asubsequent CL.
    
    Intent to implement and ship:
    https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/h4sCuIqb_78
    
    Bug: 817769
    Change-Id: Ic9a016a05e622ee02b86d567f64c3ed84f0926c3
    Reviewed-on: https://chromium-review.googlesource.com/942961
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541772}

---
+++ third_party/WebKit/Source/modules/mediastream/InputDeviceInfo.h
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef InputDeviceInfo_h
+#define InputDeviceInfo_h
+
+#include "modules/mediastream/MediaDeviceInfo.h"
+
+namespace blink {
+
+class MediaTrackCapabilities;
+
+class InputDeviceInfo final : public MediaDeviceInfo {
+  DEFINE_WRAPPERTYPEINFO();
+
+ public:
+  static InputDeviceInfo* Create(const String& device_id,
+                                 const String& label,
+                                 const String& group_id,
+                                 MediaDeviceType);
+
+  void getCapabilities(MediaTrackCapabilities&);
+
+ private:
+  InputDeviceInfo(const String& device_id,
+                  const String& label,
+                  const String& group_id,
+                  MediaDeviceType);
+};
+
+}  // namespace blink
+
+#endif  // InputDeviceInfo_h
--- third_party/WebKit/Source/modules/mediastream/MediaDeviceInfo.h
+++ third_party/WebKit/Source/modules/mediastream/MediaDeviceInfo.h
@@ -40,7 +40,7 @@ namespace blink {
 class ScriptState;
 class ScriptValue;
 
-class MODULES_EXPORT MediaDeviceInfo final : public ScriptWrappable {
+class MODULES_EXPORT MediaDeviceInfo : public ScriptWrappable {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
@@ -59,12 +59,13 @@ class MODULES_EXPORT MediaDeviceInfo final : public ScriptWrappable {
 
   ScriptValue toJSONForBinding(ScriptState*);
 
- private:
+ protected:
   MediaDeviceInfo(const String& device_id,
                   const String& label,
                   const String& group_id,
                   MediaDeviceType);
 
+ private:
   String device_id_;
   String label_;
   String group_id_;
