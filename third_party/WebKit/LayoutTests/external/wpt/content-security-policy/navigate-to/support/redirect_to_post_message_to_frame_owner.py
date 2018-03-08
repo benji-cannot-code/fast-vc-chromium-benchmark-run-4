cbcea97bf1f0067e2c63c8bff419e17ee55e0b1f
Andy Paicu <andypaicu@chromium.org>
2018-03-08 10:57:03 +0000

    'navigate-to' directive tests
    PR: https://github.com/w3c/webappsec-csp/pull/290
    
    Bug: 805886
    Change-Id: I5bdda65c7e70e729b33a3647135fee6453e97e66
    Reviewed-on: https://chromium-review.googlesource.com/934181
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541769}

---
+++ third_party/WebKit/LayoutTests/external/wpt/content-security-policy/navigate-to/support/redirect_to_post_message_to_frame_owner.py
@@ -0,0 +1,4 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+def main(request, response):
+    response.status = 302
+    response.headers.set("Location", "post_message_to_frame_owner.html")
