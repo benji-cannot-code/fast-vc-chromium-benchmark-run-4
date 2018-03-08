676f7b041dffaa47f988bb02c99ac80213696fd1
Kevin Bailey <krb@chromium.org>
2018-03-08 17:22:48 +0000

    [omnibox] Initial MdTextButton support
    Switch to use a base class of MdTextButton and make changes necessary
    to support it.
    
    Also includes some recent Omnibox coloring merges.
    
    Bug: 780835
    Change-Id: If3a9b007ff9ef82188f4ffa079ea8d8139d19daa
    Reviewed-on: https://chromium-review.googlesource.com/949824
    Commit-Queue: Kevin Bailey <krb@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541820}

--- chrome/browser/ui/views/omnibox/omnibox_tab_switch_button.h
+++ chrome/browser/ui/views/omnibox/omnibox_tab_switch_button.h
@@ -6,11 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_VIEWS_OMNIBOX_OMNIBOX_TAB_SWITCH_BUTTON_H_
 #define CHROME_BROWSER_UI_VIEWS_OMNIBOX_OMNIBOX_TAB_SWITCH_BUTTON_H_
 
-#include "ui/views/controls/button/label_button.h"
+#include "ui/views/controls/button/md_text_button.h"
 
 class OmniboxResultView;
 
-class OmniboxTabSwitchButton : public views::LabelButton,
+class OmniboxTabSwitchButton : public views::MdTextButton,
                                public views::ButtonListener {
  public:
   explicit OmniboxTabSwitchButton(OmniboxResultView* result_view);
