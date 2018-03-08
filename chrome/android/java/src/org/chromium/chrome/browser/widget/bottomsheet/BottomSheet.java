cbed3c0f95fbfad365073b6cb3c32d87c28b8ac5
Matthew Jones <mdjones@chromium.org>
2018-03-08 22:53:15 +0000

    Consolidate BottomSheet setters and clean up a bit
    This change removes two setters for the fullscreen manager and the
    tab model selector as they can be accessed in init from
    ChromeActivity. The height ratios of the sheet are now also referenced
    by their respective state.
    
    BUG=814528
    
    Change-Id: I5a58a2ff367c7ad567d6b88b7bb8a3920632a57c
    Reviewed-on: https://chromium-review.googlesource.com/956342
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541912}

--- chrome/android/java/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheet.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheet.java
@@ -640,25 +640,10 @@ public class BottomSheet
     }
 
     /**
-     * @param tabModelSelector A TabModelSelector for getting the current tab and activity.
-     */
-    public void setTabModelSelector(TabModelSelector tabModelSelector) {
-        mTabModelSelector = tabModelSelector;
-    }
-
-    /**
      * @param layoutManager The {@link LayoutManagerChrome} used to show and hide overview mode.
      */
     public void setLayoutManagerChrome(LayoutManagerChrome layoutManager) {
         getIphBubbleController().setLayoutManagerChrome(layoutManager);
-    }
-
-    /**
-     * @param fullscreenManager Chrome's fullscreen manager for information about toolbar offsets.
-     */
-    public void setFullscreenManager(ChromeFullscreenManager fullscreenManager) {
-        mFullscreenManager = fullscreenManager;
-        getIphBubbleController().setFullscreenManager(fullscreenManager);
     }
 
     /**
@@ -686,6 +671,10 @@ public class BottomSheet
      * @param activity The activity displaying the bottom sheet.
      */
     public void init(View root, ChromeActivity activity) {
+        mTabModelSelector = activity.getTabModelSelector();
+        mFullscreenManager = activity.getFullscreenManager();
+
+        getIphBubbleController().setFullscreenManager(mFullscreenManager);
         mToolbarHolder =
                 (TouchRestrictingFrameLayout) findViewById(R.id.bottom_sheet_toolbar_container);
         mDefaultToolbarView = mToolbarHolder.findViewById(R.id.bottom_sheet_toolbar);
@@ -804,7 +793,7 @@ public class BottomSheet
             }
         });
 
-        mActivity.getFullscreenManager().addListener(new FullscreenListener() {
+        mFullscreenManager.addListener(new FullscreenListener() {
             @Override
             public void onToggleOverlayVideoMode(boolean enabled) {
                 if (isSheetOpen()) setSheetState(SHEET_STATE_PEEK, false);
@@ -1166,10 +1155,11 @@ public class BottomSheet
         // the correct toolbar height and container height are not know until those views are
         // inflated. The other views are a specific DP distance from the top and bottom and are
         // also updated.
-        mStateRatios[0] = (mToolbarHeight + mToolbarShadowHeight) / mContainerHeight;
-        mStateRatios[1] = HALF_HEIGHT_RATIO;
+        mStateRatios[SHEET_STATE_PEEK] = (mToolbarHeight + mToolbarShadowHeight) / mContainerHeight;
+        mStateRatios[SHEET_STATE_HALF] = HALF_HEIGHT_RATIO;
         // The max height ratio will be greater than 1 to account for the toolbar shadow.
-        mStateRatios[2] = (mContainerHeight + mToolbarShadowHeight) / mContainerHeight;
+        mStateRatios[SHEET_STATE_FULL] =
+                (mContainerHeight + mToolbarShadowHeight) / mContainerHeight;
 
         if (mCurrentState == SHEET_STATE_HALF && isSmallScreen()) {
             setSheetState(SHEET_STATE_FULL, false);
@@ -1278,7 +1268,7 @@ public class BottomSheet
      */
     @VisibleForTesting
     public float getPeekRatio() {
-        return mStateRatios[0];
+        return mStateRatios[SHEET_STATE_PEEK];
     }
 
     /**
@@ -1286,7 +1276,7 @@ public class BottomSheet
      */
     @VisibleForTesting
     public float getHalfRatio() {
-        return mStateRatios[1];
+        return mStateRatios[SHEET_STATE_HALF];
     }
 
     /**
@@ -1294,7 +1284,7 @@ public class BottomSheet
      */
     @VisibleForTesting
     public float getFullRatio() {
-        return mStateRatios[2];
+        return mStateRatios[SHEET_STATE_FULL];
     }
 
     /**
