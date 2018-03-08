9d106bdcba485b3f41d696e11062b62634601df0
Jinho Bang <jinho.bang@samsung.com>
2018-03-08 18:23:31 +0000

    PaymentHandler: Add a permission check to GetAllPaymentApps()
    When PaymentRequest API is initiated from merchant site,
    GetAllPaymentApps() will be called to query all installed payment
    handler on the browser. At this point, we should check a permission
    whether each payment handler is allowed or blocked. If the permission is
    BLOCK, the payment handler is not supported in payment request
    perspective.
    
    Bug: 665949
    Change-Id: I720ffcaab8068506bb165c0353b6cd658973937e
    Reviewed-on: https://chromium-review.googlesource.com/948122
    Commit-Queue: Jinho Bang <jinho.bang@samsung.com>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541841}

--- content/browser/payments/payment_app_provider_impl_unittest.cc
+++ content/browser/payments/payment_app_provider_impl_unittest.cc
@@ -11,8 +11,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "content/browser/payments/payment_app_content_unittest_base.h"
 #include "content/browser/payments/payment_app_provider_impl.h"
+#include "content/public/browser/permission_type.h"
+#include "content/public/test/mock_permission_manager.h"
+#include "content/public/test/test_browser_context.h"
+#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/WebKit/public/platform/modules/payments/payment_app.mojom.h"
+#include "third_party/WebKit/public/platform/modules/permissions/permission_status.mojom.h"
 #include "url/gurl.h"
 
 namespace content {
@@ -52,7 +57,17 @@ void PaymentEventResultCallback(base::OnceClosure callback,
 
 class PaymentAppProviderTest : public PaymentAppContentUnitTestBase {
  public:
-  PaymentAppProviderTest() {}
+  PaymentAppProviderTest() {
+    std::unique_ptr<MockPermissionManager> mock_permission_manager(
+        new testing::NiceMock<MockPermissionManager>());
+    ON_CALL(*mock_permission_manager,
+            GetPermissionStatus(PermissionType::PAYMENT_HANDLER, testing::_,
+                                testing::_))
+        .WillByDefault(
+            testing::Return(blink::mojom::PermissionStatus::GRANTED));
+    static_cast<TestBrowserContext*>(browser_context())
+        ->SetPermissionManager(std::move(mock_permission_manager));
+  }
   ~PaymentAppProviderTest() override {}
 
   void SetPaymentInstrument(
