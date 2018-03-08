a4778920ded690f100a43e48f8ed0aebf4611c4a
Sasha Morrissey <sashab@chromium.org>
2018-03-08 03:25:01 +0000

    Fix typo in UMA_INDEX_KNOWN_EXTENSIONS
    Bug: None
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I4b0931917ec864739843a72d7de2dd430362db93
    Reviewed-on: https://chromium-review.googlesource.com/952803
    Reviewed-by: Naoki Fukino <fukino@chromium.org>
    Commit-Queue: Sasha Morrissey <sashab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541708}

--- ui/file_manager/file_manager/foreground/js/file_tasks.js
+++ ui/file_manager/file_manager/foreground/js/file_tasks.js
@@ -314,7 +314,7 @@ FileTasks.UMA_INDEX_KNOWN_EXTENSIONS = Object.freeze([
   '.wma',      '.wmv',         '.xls',
   '.xlsx',     '.crdownload',  '.crx',
   '.dmg',      '.exe',         '.html',
-  'htm',       '.jar',         '.ps',
+  '.htm',      '.jar',         '.ps',
   '.torrent',  '.txt',         '.zip',
   'directory', 'no extension', 'unknown extension',
   '.mhtml'
