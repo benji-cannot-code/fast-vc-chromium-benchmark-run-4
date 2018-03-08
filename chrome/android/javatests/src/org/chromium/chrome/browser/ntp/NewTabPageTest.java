35f3535d3708142ad4b0b413d28fa352a9d70a7b
Theresa <twellington@chromium.org>
2018-03-08 15:16:16 +0000

    Disable NewTabPageTest#testOpenMostVisitedItemInIncognitoTab
    This test appears to be causing NewTabPageTest#testRender to flake.
    Spectulatively disabling.
    
    BUG=813589
    
    Change-Id: I0bdd566c14d1e6d3521e00e097e55a9f079e76a1
    Reviewed-on: https://chromium-review.googlesource.com/954333
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Commit-Queue: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541799}

--- chrome/android/javatests/src/org/chromium/chrome/browser/ntp/NewTabPageTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ntp/NewTabPageTest.java
@@ -285,7 +285,7 @@ public class NewTabPageTest {
      * Tests opening a most visited item in a new tab.
      */
     @Test
-    @DisabledTest // Flaked on the try bot. http://crbug.com/543138
+    @DisabledTest(message = "Flaked on the try bot. http://crbug.com/543138")
     @SmallTest
     @Feature({"NewTabPage"})
     public void testOpenMostVisitedItemInNewTab() throws InterruptedException {
@@ -297,6 +297,8 @@ public class NewTabPageTest {
      * Tests opening a most visited item in a new incognito tab.
      */
     @Test
+    @DisabledTest(
+            message = "Suspected to be causing #testRender to flake. https://crbug.com/813589.")
     @SmallTest
     @Feature({"NewTabPage"})
     public void testOpenMostVisitedItemInIncognitoTab() throws InterruptedException {
