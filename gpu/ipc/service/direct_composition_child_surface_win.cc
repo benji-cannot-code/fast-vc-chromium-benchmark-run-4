b232383998ac35938951df17b429f777b2bae8f9
Sunny Sachanandani <sunnyps@chromium.org>
2018-03-08 02:06:19 +0000

    gpu: Make --disable-gpu-vsync work with direct composition
    This broke because the direct composition path doesn't use swap interval
    set by eglSwapInterval in ANGLE. Since we only care about disabling via
    command line, it's ok to just handle that instead of SetSwapInterval.
    
    This will still not work when we use a direct composition surface
    instead of swap chain but that only happens when we use overlays. There
    may be no way to make this case work since the swap chain is not
    controlled by Chrome.
    
    Tested with vsynctester.com on my home desktop.
    
    R=kbr,piman
    BUG=480361,787485
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ife5804b4e9bcede41bb29cd01e54e81f0d278b62
    Reviewed-on: https://chromium-review.googlesource.com/954020
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541687}

--- gpu/ipc/service/direct_composition_child_surface_win.cc
+++ gpu/ipc/service/direct_composition_child_surface_win.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/gl_angle_util_win.h"
 #include "ui/gl/gl_context.h"
 #include "ui/gl/gl_surface_egl.h"
+#include "ui/gl/gl_switches.h"
 #include "ui/gl/scoped_make_current.h"
 
 #ifndef EGL_ANGLE_flexible_surface_compatibility
@@ -50,7 +51,9 @@ DirectCompositionChildSurfaceWin::DirectCompositionChildSurfaceWin(
       size_(size),
       is_hdr_(is_hdr),
       has_alpha_(has_alpha),
-      enable_dc_layers_(enable_dc_layers) {}
+      enable_dc_layers_(enable_dc_layers),
+      disable_vsync_(base::CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kDisableGpuVsync)) {}
 
 DirectCompositionChildSurfaceWin::~DirectCompositionChildSurfaceWin() {
   Destroy();
@@ -152,7 +155,7 @@ void DirectCompositionChildSurfaceWin::ReleaseDrawTexture(bool will_discard) {
       base::debug::Alias(&surface_size);
       params.DirtyRectsCount = 1;
       params.pDirtyRects = &dirty_rect;
-      swap_chain_->Present1(first_swap_ ? 0 : 1, 0, &params);
+      swap_chain_->Present1(first_swap_ || disable_vsync_ ? 0 : 1, 0, &params);
       if (first_swap_) {
         // Wait for the GPU to finish executing its commands before
         // committing the DirectComposition tree, or else the swapchain
