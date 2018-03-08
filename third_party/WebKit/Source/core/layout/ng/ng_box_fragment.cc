2e2367bd723a3d81cfc53e211b70766507083157
Koji Ishii <kojii@chromium.org>
2018-03-08 19:30:12 +0000

    [LayoutNG] Fix list marker positioning when the first child is block
    This patch fixes list marker positioning when the first child of
    the list item is block-level, and the child does not produce a
    baseline. Before this patch, list marker is positioned at the
    synthesized baseline. This is not well-defined, but 3 browsers
    positions the list marker at the first child that has a baseline.
    This is filed to CSS WG at
    https://github.com/w3c/csswg-drafts/issues/2417.
    
    By doing so, a new case appears where there are no children that
    has a baseline. This case is also not well-defined and non-
    interoperable. Blink/Edge/WebKit creates an empty line box in
    this case, but this isn't easy as we have to insert an empty
    line box before the children we have already laid out. This
    patch follows Gecko behavior to position the list marker at the
    top of the list item.
    
    Also, whether list marker should affect block size or not is not
    defined and not interoperable. For now, this patch includes the
    list marker block size into list item's block size because it
    looks more reasonable to me, and Blink/Edge/WebKit do so. This
    is filed as:
    https://github.com/w3c/csswg-drafts/issues/2418
    
    SetListMarkerPosition() is a left over in the last patch and
    therefore this patch removed it.
    
    Some tests turn to good, though they need to rebaseline due to
    list marker image differences.
    
    Bug: 725277
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: If47ab7daea214c0593a917a0c8f505274dae5ca2
    Reviewed-on: https://chromium-review.googlesource.com/952843
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541864}

--- third_party/WebKit/Source/core/layout/ng/ng_box_fragment.cc
+++ third_party/WebKit/Source/core/layout/ng/ng_box_fragment.cc
@@ -14,16 +14,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-NGLineHeightMetrics NGBoxFragment::BaselineMetrics(
+NGLineHeightMetrics NGBoxFragment::BaselineMetricsWithoutSynthesize(
     const NGBaselineRequest& request,
     const NGConstraintSpace& constraint_space) const {
   const auto& physical_fragment = ToNGPhysicalBoxFragment(physical_fragment_);
-
-  LayoutBox* layout_box = ToLayoutBox(physical_fragment_.GetLayoutObject());
   bool is_parallel_writing_mode =
-      IsHorizontalWritingMode(constraint_space.GetWritingMode()) ==
-      layout_box->IsHorizontalWritingMode();
-
+      IsParallelWritingMode(constraint_space.GetWritingMode(),
+                            physical_fragment.Style().GetWritingMode());
   if (is_parallel_writing_mode) {
     // Find the baseline from the computed results.
     if (const NGBaseline* baseline = physical_fragment.Baseline(request)) {
@@ -33,6 +30,7 @@ NGLineHeightMetrics NGBoxFragment::BaselineMetrics(
       // For replaced elements, inline-block elements, and inline-table
       // elements, the height is the height of their margin box.
       // https://drafts.csswg.org/css2/visudet.html#line-height
+      LayoutBox* layout_box = ToLayoutBox(physical_fragment_.GetLayoutObject());
       if (layout_box->IsAtomicInlineLevel()) {
         ascent += layout_box->MarginOver();
         descent += layout_box->MarginUnder();
@@ -42,12 +40,25 @@ NGLineHeightMetrics NGBoxFragment::BaselineMetrics(
     }
   }
 
+  return NGLineHeightMetrics();
+}
+
+NGLineHeightMetrics NGBoxFragment::BaselineMetrics(
+    const NGBaselineRequest& request,
+    const NGConstraintSpace& constraint_space) const {
+  NGLineHeightMetrics metrics =
+      BaselineMetricsWithoutSynthesize(request, constraint_space);
+  if (!metrics.IsEmpty())
+    return metrics;
+
   // The baseline type was not found. This is either this box should synthesize
   // box-baseline without propagating from children, or caller forgot to add
   // baseline requests to constraint space when it called Layout().
   LayoutUnit block_size = BlockSize();
 
+  const auto& physical_fragment = ToNGPhysicalBoxFragment(physical_fragment_);
   const ComputedStyle& style = physical_fragment.Style();
+  LayoutBox* layout_box = ToLayoutBox(physical_fragment_.GetLayoutObject());
   if (style.HasAppearance() &&
       !LayoutTheme::GetTheme().IsControlContainer(style.Appearance())) {
     return NGLineHeightMetrics(
@@ -58,6 +69,9 @@ NGLineHeightMetrics NGBoxFragment::BaselineMetrics(
 
   // If atomic inline, use the margin box. See above.
   if (layout_box->IsAtomicInlineLevel()) {
+    bool is_parallel_writing_mode =
+        IsParallelWritingMode(constraint_space.GetWritingMode(),
+                              physical_fragment.Style().GetWritingMode());
     if (is_parallel_writing_mode)
       block_size += layout_box->MarginLogicalHeight();
     else
