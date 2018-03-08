c974c2e62be3a20c0d66c3f80fbd8062fb46bda4
Nico Weber <thakis@chromium.org>
2018-03-08 19:49:21 +0000

    win: Fix two -Wmicrosoft-cast warnings.
    The C++ standard doesn't allow implicit conversion of function pointers
    (such as returned by GetProcAddress()) to void*.  MSVC does accept that,
    and so does clang-cl (albeit with a warning).  To fix the warning,
    just make LoadComBaseFunction() return FARPROC.
    
    Bug: 550065
    Change-Id: I95eeed86c31d45358aabc31414435ae79a1a0e5c
    Reviewed-on: https://chromium-review.googlesource.com/955905
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541872}

--- base/win/core_winrt_util.cc
+++ base/win/core_winrt_util.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-void* LoadComBaseFunction(const char* function_name) {
+FARPROC LoadComBaseFunction(const char* function_name) {
   static HMODULE const handle = ::LoadLibrary(L"combase.dll");
   return handle ? ::GetProcAddress(handle, function_name) : nullptr;
 }
