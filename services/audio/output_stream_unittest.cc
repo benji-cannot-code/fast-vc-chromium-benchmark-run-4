2e97280a762ab0e2fb6c9b1ff23dd3361d7267ba
Max Morin <maxmorin@chromium.org>
2018-03-08 14:06:02 +0000

    Add factory for streams in audio service.
    Just testing the factory is a bit tricky, so I test it as part of the
    audio::OutputStream tests.
    
    Doc: go/audio-service-streams-design
    
    Bug: 803102
    
    Change-Id: I463e7adbda7de1a8a5724fa23fc98015cb425a3c
    Reviewed-on: https://chromium-review.googlesource.com/939179
    Commit-Queue: Max Morin <maxmorin@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541785}

--- services/audio/output_stream_unittest.cc
+++ services/audio/output_stream_unittest.cc
@@ -9,11 +9,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/test/mock_callback.h"
 #include "base/test/scoped_task_environment.h"
+#include "base/unguessable_token.h"
 #include "media/audio/audio_io.h"
 #include "media/audio/mock_audio_manager.h"
 #include "media/audio/test_audio_thread.h"
 #include "mojo/edk/system/core.h"
 #include "mojo/public/cpp/bindings/associated_binding.h"
+#include "services/audio/stream_factory.h"
 #include "services/audio/test/mock_log.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -75,12 +77,12 @@ class MockObserver : public media::mojom::AudioOutputStreamObserver {
   MockObserver() : binding_(this) {}
 
   // Should only be called once.
-  media::mojom::AudioOutputStreamObserverAssociatedPtr MakePtr() {
-    media::mojom::AudioOutputStreamObserverAssociatedPtr ptr;
-    binding_.Bind(mojo::MakeRequestAssociatedWithDedicatedPipe(&ptr));
+  media::mojom::AudioOutputStreamObserverAssociatedPtrInfo MakePtrInfo() {
+    media::mojom::AudioOutputStreamObserverAssociatedPtrInfo ptr_info;
+    binding_.Bind(mojo::MakeRequest(&ptr_info));
     binding_.set_connection_error_handler(base::BindOnce(
         &MockObserver::BindingConnectionError, base::Unretained(this)));
-    return ptr;
+    return ptr_info;
   }
 
   void CloseBinding() { binding_.Close(); }
@@ -125,7 +127,10 @@ class MockCreatedCallback {
 class TestEnvironment {
  public:
   TestEnvironment()
-      : audio_manager_(std::make_unique<media::TestAudioThread>(false)) {
+      : audio_manager_(std::make_unique<media::TestAudioThread>(false)),
+        stream_factory_(&audio_manager_),
+        stream_factory_binding_(&stream_factory_,
+                                mojo::MakeRequest(&stream_factory_ptr_)) {
     mojo::edk::SetDefaultProcessErrorCallback(bad_message_callback_.Get());
   }
 
@@ -135,12 +140,14 @@ class TestEnvironment {
   using MockBadMessageCallback =
       base::MockCallback<base::RepeatingCallback<void(const std::string&)>>;
 
-  std::unique_ptr<OutputStream> CreateStream(
-      media::mojom::AudioOutputStreamRequest request) {
-    return std::make_unique<OutputStream>(
-        created_callback_.Get(), delete_callback_.Get(), std::move(request),
-        client_.MakePtr(), observer_.MakePtr(), log_.MakePtr(), &audio_manager_,
-        "", media::AudioParameters::UnavailableDeviceParams());
+  media::mojom::AudioOutputStreamPtr CreateStream() {
+    media::mojom::AudioOutputStreamPtr stream_ptr;
+    stream_factory_ptr_->CreateOutputStream(
+        mojo::MakeRequest(&stream_ptr), client_.MakePtr(),
+        observer_.MakePtrInfo(), log_.MakePtr(), "",
+        media::AudioParameters::UnavailableDeviceParams(),
+        base::UnguessableToken::Create(), created_callback_.Get());
+    return stream_ptr;
   }
 
   media::MockAudioManager& audio_manager() { return audio_manager_; }
@@ -153,8 +160,6 @@ class TestEnvironment {
 
   MockCreatedCallback& created_callback() { return created_callback_; }
 
-  MockDeleteCallback& delete_callback() { return delete_callback_; }
-
   MockBadMessageCallback& bad_message_callback() {
     return bad_message_callback_;
   }
@@ -162,11 +167,13 @@ class TestEnvironment {
  private:
   base::test::ScopedTaskEnvironment tasks_;
   media::MockAudioManager audio_manager_;
+  StreamFactory stream_factory_;
+  mojom::StreamFactoryPtr stream_factory_ptr_;
+  mojo::Binding<mojom::StreamFactory> stream_factory_binding_;
   StrictMock<MockClient> client_;
   StrictMock<MockObserver> observer_;
   NiceMock<MockLog> log_;
   StrictMock<MockCreatedCallback> created_callback_;
-  StrictMock<MockDeleteCallback> delete_callback_;
   StrictMock<MockBadMessageCallback> bad_message_callback_;
 
   DISALLOW_COPY_AND_ASSIGN(TestEnvironment);
@@ -185,9 +192,7 @@ TEST(OutputStreamTest, ConstructDestruct) {
   EXPECT_CALL(mock_stream, SetVolume(1));
   EXPECT_CALL(env.log(), OnCreated(_, _));
 
-  media::mojom::AudioOutputStreamPtr stream_ptr;
-  std::unique_ptr<OutputStream> stream =
-      env.CreateStream(mojo::MakeRequest(&stream_ptr));
+  media::mojom::AudioOutputStreamPtr stream_ptr = env.CreateStream();
   base::RunLoop().RunUntilIdle();
   Mock::VerifyAndClear(&mock_stream);
   Mock::VerifyAndClear(&env.created_callback());
@@ -196,6 +201,8 @@ TEST(OutputStreamTest, ConstructDestruct) {
   EXPECT_CALL(mock_stream, Close());
   EXPECT_CALL(env.observer(), BindingConnectionError());
   EXPECT_CALL(env.client(), BindingConnectionError());
+  stream_ptr.reset();
+  base::RunLoop().RunUntilIdle();
 }
 
 TEST(OutputStreamTest, ConstructStreamAndDestructObserver_DestructsStream) {
@@ -210,22 +217,17 @@ TEST(OutputStreamTest, ConstructStreamAndDestructObserver_DestructsStream) {
   EXPECT_CALL(mock_stream, Open()).WillOnce(Return(true));
   EXPECT_CALL(mock_stream, SetVolume(1));
 
-  media::mojom::AudioOutputStreamPtr stream_ptr;
-  std::unique_ptr<OutputStream> stream =
-      env.CreateStream(mojo::MakeRequest(&stream_ptr));
+  media::mojom::AudioOutputStreamPtr stream_ptr = env.CreateStream();
   base::RunLoop().RunUntilIdle();
   Mock::VerifyAndClear(&mock_stream);
   Mock::VerifyAndClear(&env.created_callback());
 
   EXPECT_CALL(mock_stream, Close());
   EXPECT_CALL(env.client(), BindingConnectionError());
-  EXPECT_CALL(env.delete_callback(), Run(stream.release()))
-      .WillOnce(DeleteArg<0>());
 
   env.observer().CloseBinding();
   base::RunLoop().RunUntilIdle();
 
-  Mock::VerifyAndClear(&env.delete_callback());
   Mock::VerifyAndClear(&mock_stream);
   Mock::VerifyAndClear(&env.client());
 }
@@ -242,22 +244,17 @@ TEST(OutputStreamTest, ConstructStreamAndDestructClient_DestructsStream) {
   EXPECT_CALL(mock_stream, Open()).WillOnce(Return(true));
   EXPECT_CALL(mock_stream, SetVolume(1));
 
-  media::mojom::AudioOutputStreamPtr stream_ptr;
-  std::unique_ptr<OutputStream> stream =
-      env.CreateStream(mojo::MakeRequest(&stream_ptr));
+  media::mojom::AudioOutputStreamPtr stream_ptr = env.CreateStream();
   base::RunLoop().RunUntilIdle();
   Mock::VerifyAndClear(&mock_stream);
   Mock::VerifyAndClear(&env.created_callback());
 
   EXPECT_CALL(mock_stream, Close());
   EXPECT_CALL(env.observer(), BindingConnectionError());
-  EXPECT_CALL(env.delete_callback(), Run(stream.release()))
-      .WillOnce(DeleteArg<0>());
 
   env.client().CloseBinding();
   base::RunLoop().RunUntilIdle();
 
-  Mock::VerifyAndClear(&env.delete_callback());
   Mock::VerifyAndClear(&mock_stream);
   Mock::VerifyAndClear(&env.observer());
 }
@@ -274,9 +271,7 @@ TEST(OutputStreamTest, ConstructStreamAndReleaseStreamPtr_DestructsStream) {
   EXPECT_CALL(mock_stream, Open()).WillOnce(Return(true));
   EXPECT_CALL(mock_stream, SetVolume(1));
 
-  media::mojom::AudioOutputStreamPtr stream_ptr;
-  std::unique_ptr<OutputStream> stream =
-      env.CreateStream(mojo::MakeRequest(&stream_ptr));
+  media::mojom::AudioOutputStreamPtr stream_ptr = env.CreateStream();
   base::RunLoop().RunUntilIdle();
   Mock::VerifyAndClear(&mock_stream);
   Mock::VerifyAndClear(&env.created_callback());
@@ -284,13 +279,10 @@ TEST(OutputStreamTest, ConstructStreamAndReleaseStreamPtr_DestructsStream) {
   EXPECT_CALL(mock_stream, Close());
   EXPECT_CALL(env.observer(), BindingConnectionError());
   EXPECT_CALL(env.client(), BindingConnectionError());
-  EXPECT_CALL(env.delete_callback(), Run(stream.release()))
-      .WillOnce(DeleteArg<0>());
 
   stream_ptr.reset();
   base::RunLoop().RunUntilIdle();
 
-  Mock::VerifyAndClear(&env.delete_callback());
   Mock::VerifyAndClear(&mock_stream);
   Mock::VerifyAndClear(&env.client());
   Mock::VerifyAndClear(&env.observer());
@@ -308,9 +300,7 @@ TEST(OutputStreamTest, Play_Plays) {
   EXPECT_CALL(mock_stream, Open()).WillOnce(Return(true));
   EXPECT_CALL(mock_stream, SetVolume(1));
 
-  media::mojom::AudioOutputStreamPtr stream_ptr;
-  std::unique_ptr<OutputStream> stream =
-      env.CreateStream(mojo::MakeRequest(&stream_ptr));
+  media::mojom::AudioOutputStreamPtr stream_ptr = env.CreateStream();
   base::RunLoop().RunUntilIdle();
   Mock::VerifyAndClear(&mock_stream);
   Mock::VerifyAndClear(&env.created_callback());
@@ -332,6 +322,8 @@ TEST(OutputStreamTest, Play_Plays) {
   EXPECT_CALL(env.observer(), DidStopPlaying()).Times(AtMost(1));
   EXPECT_CALL(env.observer(), BindingConnectionError());
   EXPECT_CALL(env.client(), BindingConnectionError());
+  stream_ptr.reset();
+  base::RunLoop().RunUntilIdle();
 }
 
 TEST(OutputStreamTest, PlayAndPause_PlaysAndStops) {
@@ -346,9 +338,7 @@ TEST(OutputStreamTest, PlayAndPause_PlaysAndStops) {
   EXPECT_CALL(mock_stream, Open()).WillOnce(Return(true));
   EXPECT_CALL(mock_stream, SetVolume(1));
 
-  media::mojom::AudioOutputStreamPtr stream_ptr;
-  std::unique_ptr<OutputStream> stream =
-      env.CreateStream(mojo::MakeRequest(&stream_ptr));
+  media::mojom::AudioOutputStreamPtr stream_ptr = env.CreateStream();
   base::RunLoop().RunUntilIdle();
   Mock::VerifyAndClear(&mock_stream);
   Mock::VerifyAndClear(&env.created_callback());
@@ -375,6 +365,8 @@ TEST(OutputStreamTest, PlayAndPause_PlaysAndStops) {
   EXPECT_CALL(mock_stream, Close());
   EXPECT_CALL(env.observer(), BindingConnectionError());
   EXPECT_CALL(env.client(), BindingConnectionError());
+  stream_ptr.reset();
+  base::RunLoop().RunUntilIdle();
 }
 
 TEST(OutputStreamTest, SetVolume_SetsVolume) {
@@ -390,9 +382,7 @@ TEST(OutputStreamTest, SetVolume_SetsVolume) {
   EXPECT_CALL(mock_stream, Open()).WillOnce(Return(true));
   EXPECT_CALL(mock_stream, SetVolume(1));
 
-  media::mojom::AudioOutputStreamPtr stream_ptr;
-  std::unique_ptr<OutputStream> stream =
-      env.CreateStream(mojo::MakeRequest(&stream_ptr));
+  media::mojom::AudioOutputStreamPtr stream_ptr = env.CreateStream();
   base::RunLoop().RunUntilIdle();
   Mock::VerifyAndClear(&mock_stream);
   Mock::VerifyAndClear(&env.created_callback());
@@ -406,6 +396,8 @@ TEST(OutputStreamTest, SetVolume_SetsVolume) {
   EXPECT_CALL(mock_stream, Close());
   EXPECT_CALL(env.observer(), BindingConnectionError());
   EXPECT_CALL(env.client(), BindingConnectionError());
+  stream_ptr.reset();
+  base::RunLoop().RunUntilIdle();
 }
 
 TEST(OutputStreamTest, SetNegativeVolume_BadMessage) {
@@ -420,9 +412,7 @@ TEST(OutputStreamTest, SetNegativeVolume_BadMessage) {
   EXPECT_CALL(mock_stream, Open()).WillOnce(Return(true));
   EXPECT_CALL(mock_stream, SetVolume(1));
 
-  media::mojom::AudioOutputStreamPtr stream_ptr;
-  std::unique_ptr<OutputStream> stream =
-      env.CreateStream(mojo::MakeRequest(&stream_ptr));
+  media::mojom::AudioOutputStreamPtr stream_ptr = env.CreateStream();
   base::RunLoop().RunUntilIdle();
   Mock::VerifyAndClear(&mock_stream);
   Mock::VerifyAndClear(&env.created_callback());
@@ -431,8 +421,6 @@ TEST(OutputStreamTest, SetNegativeVolume_BadMessage) {
   EXPECT_CALL(env.observer(), BindingConnectionError());
   EXPECT_CALL(env.client(), BindingConnectionError());
   EXPECT_CALL(env.bad_message_callback(), Run(_));
-  EXPECT_CALL(env.delete_callback(), Run(stream.release()))
-      .WillOnce(DeleteArg<0>());
   stream_ptr->SetVolume(-0.1);
   base::RunLoop().RunUntilIdle();
 }
@@ -449,9 +437,7 @@ TEST(OutputStreamTest, SetVolumeGreaterThanOne_BadMessage) {
   EXPECT_CALL(mock_stream, Open()).WillOnce(Return(true));
   EXPECT_CALL(mock_stream, SetVolume(1));
 
-  media::mojom::AudioOutputStreamPtr stream_ptr;
-  std::unique_ptr<OutputStream> stream =
-      env.CreateStream(mojo::MakeRequest(&stream_ptr));
+  media::mojom::AudioOutputStreamPtr stream_ptr = env.CreateStream();
   base::RunLoop().RunUntilIdle();
   Mock::VerifyAndClear(&mock_stream);
   Mock::VerifyAndClear(&env.created_callback());
@@ -460,8 +446,6 @@ TEST(OutputStreamTest, SetVolumeGreaterThanOne_BadMessage) {
   EXPECT_CALL(env.observer(), BindingConnectionError());
   EXPECT_CALL(env.client(), BindingConnectionError());
   EXPECT_CALL(env.bad_message_callback(), Run(_));
-  EXPECT_CALL(env.delete_callback(), Run(stream.release()))
-      .WillOnce(DeleteArg<0>());
   stream_ptr->SetVolume(1.1);
   base::RunLoop().RunUntilIdle();
 }
@@ -471,19 +455,14 @@ TEST(OutputStreamTest, ConstructWithStreamCreationFailure_SignalsError) {
 
   // By default, the MockAudioManager fails to create a stream.
 
-  media::mojom::AudioOutputStreamPtr stream_ptr;
-  std::unique_ptr<OutputStream> stream =
-      env.CreateStream(mojo::MakeRequest(&stream_ptr));
+  media::mojom::AudioOutputStreamPtr stream_ptr = env.CreateStream();
 
   EXPECT_CALL(env.created_callback(), Created(unsuccessfully_));
   EXPECT_CALL(env.observer(), BindingConnectionError());
   EXPECT_CALL(env.log(), OnError());
   EXPECT_CALL(env.client(), OnError());
   EXPECT_CALL(env.client(), BindingConnectionError());
-  EXPECT_CALL(env.delete_callback(), Run(stream.release()))
-      .WillOnce(DeleteArg<0>());
   base::RunLoop().RunUntilIdle();
-  Mock::VerifyAndClear(&env.delete_callback());
   Mock::VerifyAndClear(&env.client());
   Mock::VerifyAndClear(&env.observer());
 }
@@ -496,16 +475,12 @@ TEST(OutputStreamTest,
 
   // By default, the MockAudioManager fails to create a stream.
 
-  media::mojom::AudioOutputStreamPtr stream_ptr;
-  {
-    EXPECT_CALL(env.created_callback(), Created(unsuccessfully_));
-    std::unique_ptr<OutputStream> stream =
-        env.CreateStream(mojo::MakeRequest(&stream_ptr));
+  media::mojom::AudioOutputStreamPtr stream_ptr = env.CreateStream();
+  EXPECT_CALL(env.created_callback(), Created(unsuccessfully_));
 
-    EXPECT_CALL(env.observer(), BindingConnectionError());
-    EXPECT_CALL(env.client(), OnError());
-    EXPECT_CALL(env.client(), BindingConnectionError());
-  }
+  EXPECT_CALL(env.observer(), BindingConnectionError());
+  EXPECT_CALL(env.client(), OnError());
+  EXPECT_CALL(env.client(), BindingConnectionError());
 
   base::RunLoop().RunUntilIdle();
   Mock::VerifyAndClear(&env.client());
