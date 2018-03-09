c4153ceacd4fa582ade43811755534be569ba560
Qiang Xu <warx@google.com>
2018-03-09 20:51:45 +0000

    cros: no persistent window restore on swapping primary
    Changes:
    OnDisplayConfigurationChanged could not distinguish the case of
    swapping primary display. We now bind a callback in OnDisplayAdded (
    it is called on reconnecting, leaving dock mode, exiting mirror mode)
    and run the callback in OnDisplayConfigurationChanged.
    
    Bug: 805046
    Test: tested on device and added test coverage
    Change-Id: I332c9d368e9df4fc5630317902f2776eab5dc983
    Reviewed-on: https://chromium-review.googlesource.com/956928
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Qiang Xu <warx@google.com>
    Cr-Commit-Position: refs/heads/master@{#542205}

--- ash/display/persistent_window_controller.h
+++ ash/display/persistent_window_controller.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ash_export.h"
 #include "ash/display/window_tree_host_manager.h"
 #include "ash/session/session_observer.h"
+#include "base/callback.h"
 #include "base/macros.h"
 #include "ui/display/display_observer.h"
 
@@ -31,12 +32,19 @@ class ASH_EXPORT PersistentWindowController
  private:
   // display::DisplayObserver:
   void OnWillProcessDisplayChanges() override;
+  void OnDisplayAdded(const display::Display& new_display) override;
 
   // SessionObserver:
   void OnSessionStateChanged(session_manager::SessionState state) override;
 
   // WindowTreeHostManager::Observer:
   void OnDisplayConfigurationChanged() override;
+
+  // Called when restoring persistent window placement is wanted.
+  void MaybeRestorePersistentWindowBounds();
+
+  // Callback binded on display added and run on display configuration changed.
+  base::OnceCallback<void()> restore_callback_;
 
   DISALLOW_COPY_AND_ASSIGN(PersistentWindowController);
 };
