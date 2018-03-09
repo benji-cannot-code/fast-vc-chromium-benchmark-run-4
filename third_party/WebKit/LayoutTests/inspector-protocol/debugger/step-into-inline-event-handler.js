456d55736829a8e254dfb1fa8f7ed509984d1b85
Yang Guo <yangguo@chromium.org>
2018-03-09 13:16:54 +0000

    Mark LayoutTest for rebaseline for V8 change.
    Inline event handlers are compiled with v8::ScriptCompiler::CompileFunctionInContext.
    Previously, it compiles a wrapper script containing the event handler
    and executes this wrapper script to object the event handler function.
    This script execution is observable in debug stepping.
    
    An upcoming V8 change skips this wrapper script and creates the event
    handler function directly. This changes the debug stepping behavior.
    
    R=kozyatinskiy@chromium.org, machenbach@chromium.org
    
    Bug: v8:7172
    Change-Id: I64ee3487f9ca893da305aa41dfc5d2e1e3da4ecc
    Reviewed-on: https://chromium-review.googlesource.com/955594
    Reviewed-by: Michael Achenbach <machenbach@chromium.org>
    Commit-Queue: Yang Guo <yangguo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542093}

--- third_party/WebKit/LayoutTests/inspector-protocol/debugger/step-into-inline-event-handler.js
+++ third_party/WebKit/LayoutTests/inspector-protocol/debugger/step-into-inline-event-handler.js
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   function dumpTopCallFrame(result) {
     var frame = result.params.callFrames[0];
-    testRunner.log('functionName (should be empty): ' + (frame.functionName.length ? frame.functionName : 'empty'));
+    testRunner.log('functionName (should be onclick): ' + (frame.functionName.length ? frame.functionName : 'empty'));
   }
 
   await dp.Debugger.enable();
