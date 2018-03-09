1dab78019ff075b9d8efedf7ba07e7363f3f85f2
Aleksandar Stojiljkovic <aleksandar.stojiljkovic@intel.com>
2018-03-09 05:37:33 +0000

    Access depth (KSCATEGORY_SENSOR_CAMERA) cameras on Windows.
    The recent change in MediaFoundation API requires explicit enumeration
    of sensor (newer depth Hello) cameras.
    
    Pass-through mode fixes in MF capture implementation.
    
    Verified using both Intel® RealSense™ Depth Camera D435 and previous depth
    cameras, those that are not marked as sensors in driver inf, e.g. SR300.
    
    Minor refactoring static -> anonymous namespace.
    
    BUG=807293
    
    Change-Id: I8c13d360bfc30cae8902f8e03d553dff69dcbf34
    Reviewed-on: https://chromium-review.googlesource.com/944627
    Commit-Queue: Aleksandar Stojiljkovic <aleksandar.stojiljkovic@intel.com>
    Reviewed-by: Max Morin <maxmorin@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Christian Fremerey <chfremer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542042}

--- media/capture/video/win/video_capture_device_factory_win.h
+++ media/capture/video/win/video_capture_device_factory_win.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MEDIA_CAPTURE_VIDEO_WIN_VIDEO_CAPTURE_DEVICE_FACTORY_WIN_H_
 #define MEDIA_CAPTURE_VIDEO_WIN_VIDEO_CAPTURE_DEVICE_FACTORY_WIN_H_
 
+#include <mfidl.h>
 #include "base/macros.h"
 #include "media/capture/video/video_capture_device_factory.h"
 
@@ -23,6 +24,8 @@ class CAPTURE_EXPORT VideoCaptureDeviceFactoryWin
   VideoCaptureDeviceFactoryWin();
   ~VideoCaptureDeviceFactoryWin() override {}
 
+  using MFEnumDeviceSourcesFunc = decltype(&MFEnumDeviceSources);
+
   std::unique_ptr<VideoCaptureDevice> CreateDevice(
       const VideoCaptureDeviceDescriptor& device_descriptor) override;
   void GetDeviceDescriptors(
@@ -34,9 +37,17 @@ class CAPTURE_EXPORT VideoCaptureDeviceFactoryWin
   void set_use_media_foundation_for_testing(bool use) {
     use_media_foundation_ = use;
   }
+  void set_mf_enum_device_sources_func_for_testing(
+      MFEnumDeviceSourcesFunc func) {
+    mf_enum_device_sources_func_ = func;
+  }
 
  private:
   bool use_media_foundation_;
+  // In production code, when Media Foundation libraries are available,
+  // |mf_enum_device_sources_func_| points to MFEnumDeviceSources. It enables
+  // mock of Media Foundation API in unit tests.
+  MFEnumDeviceSourcesFunc mf_enum_device_sources_func_ = nullptr;
 
   DISALLOW_COPY_AND_ASSIGN(VideoCaptureDeviceFactoryWin);
 };
