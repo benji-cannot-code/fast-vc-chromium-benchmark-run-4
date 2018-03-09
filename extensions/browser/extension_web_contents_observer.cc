0b9251700abb94eb43eac85e760b2b3afcdc380f
Karan Bhatia <karandeepb@chromium.org>
2018-03-09 03:30:50 +0000

    Extensions: Ensure InitializeRenderFrame is called for non-extension frames as well.
    r501291 changed ExtensionWebContentsObserver::RenderFrameCreated such that
    InitializeRenderFrame was no longer called for non-extension frames. This CL
    changes it back so that InitializeRenderFrame is called for all frames; and
    documents the same in the header. This would cause us to initialize the window
    ID for non- extension render frames as well (which is consistent with other
    code).
    
    BUG=811384
    
    Change-Id: I20a7039f8ba82fffbb1d17b635cfe0beb0279469
    Reviewed-on: https://chromium-review.googlesource.com/956549
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542011}

--- extensions/browser/extension_web_contents_observer.cc
+++ extensions/browser/extension_web_contents_observer.cc
@@ -115,6 +115,8 @@ void ExtensionWebContentsObserver::RenderFrameCreated(
   // looked up again on the IO thread for the webRequest API.
   ExtensionApiFrameIdMap::Get()->InitializeRenderFrameData(render_frame_host);
 
+  InitializeRenderFrame(render_frame_host);
+
   const Extension* extension = GetExtensionFromFrame(render_frame_host, false);
   if (!extension)
     return;
@@ -147,8 +149,6 @@ void ExtensionWebContentsObserver::RenderFrameCreated(
   // isolation is turned on.
   RendererStartupHelperFactory::GetForBrowserContext(browser_context_)
       ->ActivateExtensionInProcess(*extension, render_frame_host->GetProcess());
-
-  InitializeRenderFrame(render_frame_host);
 }
 
 void ExtensionWebContentsObserver::RenderFrameDeleted(
