c3e96dd5009df1bc1066342e84c38ee188241352
Finnur Thorarinsson <finnur@chromium.org>
2018-03-09 14:22:40 +0000

    Windows Native Notifications: Inline reply.
    Bug: 734095
    Change-Id: I87213656fd5e692650d75e8ca7929931bcb54f2d
    Reviewed-on: https://chromium-review.googlesource.com/955482
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Xi Cheng <chengx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542100}

--- notification_helper/notification_activator.cc
+++ notification_helper/notification_activator.cc
@@ -14,6 +14,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "notification_helper/notification_helper_util.h"
 #include "notification_helper/trace_util.h"
 
+namespace {
+
+// The response entered by the user while interacting with the toast.
+const wchar_t kUserResponse[] = L"userResponse";
+
+}  // namespace
+
 namespace notification_helper {
 
 NotificationActivator::~NotificationActivator() = default;
@@ -56,6 +63,16 @@ HRESULT NotificationActivator::Activate(
   base::CommandLine command_line(base::CommandLine::NO_PROGRAM);
   command_line.AppendSwitchNative(switches::kNotificationLaunchId,
                                   invoked_args);
+
+  // Check to see if a user response (inline reply) is also supplied.
+  for (ULONG i = 0; i < count; ++i) {
+    if (lstrcmpW(kUserResponse, data[i].Key) == 0) {
+      command_line.AppendSwitchNative(switches::kNotificationInlineReply,
+                                      data[i].Value);
+      break;
+    }
+  }
+
   base::string16 params(command_line.GetCommandLineString());
 
   SHELLEXECUTEINFO info;
