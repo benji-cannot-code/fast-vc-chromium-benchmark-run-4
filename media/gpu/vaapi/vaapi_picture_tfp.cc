31918346dab8dd71b5f2cf18b11d692186e59253
Miguel Casas <mcasas@chromium.org>
2018-03-09 14:55:06 +0000

    vaapi: split VaapiDrmPicture into common and ozone/egl parts
    This CL splits VaapiDrmPicture into a common part named
    VaapiPictureNativePixmap, and two subclasses, namely:
    - VaapiPictureNativePixmapOzone, containing the current
     #if defined(USE_OZONE) sandwiched parts, and
    - VaapiPixtureNativePixmapEgl with the other parts.
    
    So the class tree should look like:
    
      +-----------------+
      |   VaapiPicture  |
      +-----------------+
       ^               ^
       |               +----+
      +--------------+    +------------------+
      | VaapiPicture |    | VaapiPictureTFP  |
      | NativePixmap |    | (existing on ToT)|
      +--------------+    +------------------+
       ^
       +---------------+
       |               |
     +-------+    +-------+
     | VPNP  |    | VPNP  |
     | Ozone |    | Egl   |
     +-------+    +-------+
    
    The point here is to reduce the amount of if-defs to one single
    point, the VaapiPictureFactory.
    
    BUILD.gn is changed to reflect that VaapiDrmPicture is
    egl-specific (see bug, |use_egl| definition in [1]), while
    VaapiDrmPictureOzone is |use_ozone| [2] specific. Note
    that |use_egl| can coexist with |use_x11| [3].
    
    This CL just moves code around, no new code introduced in
    either vaapi_picture*.cc.
    
    [1] https://cs.chromium.org/chromium/src/ui/gl/features.gni?q=ui/gl/features.gni&sq=package:chromium&dr&l=16
    [2] https://cs.chromium.org/chromium/src/build/config/ui.gni?q=build/config/ui.gni&sq=package:chromium&dr&l=26
    [3] https://cs.chromium.org/chromium/src/build/config/ui.gni?type=cs&sq=package:chromium&l=47
    
    Bug: 785201
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I67f6f4d994322ef7f23a8ed255405963a4be0152
    Reviewed-on: https://chromium-review.googlesource.com/951928
    Reviewed-by: Julien Isorce <julien.isorce@chromium.org>
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542107}

--- media/gpu/vaapi/vaapi_picture_tfp.cc
+++ media/gpu/vaapi/vaapi_picture_tfp.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "media/gpu/vaapi/vaapi_tfp_picture.h"
+#include "media/gpu/vaapi/vaapi_picture_tfp.h"
 
 #include "media/gpu/vaapi/va_surface.h"
 #include "media/gpu/vaapi/vaapi_wrapper.h"
