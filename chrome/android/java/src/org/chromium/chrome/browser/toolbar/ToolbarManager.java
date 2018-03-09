4140cf524c4866bd1e2b73887f2e1faeb2b21be2
Matthew Jones <mdjones@chromium.org>
2018-03-09 16:52:39 +0000

    Revert "Update tab switcher state in ToolbarPhone after animation completes"
    This reverts commit 99c1a2c3394f7bfab9e11f846aaa708853fb8095.
    
    Reason for revert: Causes bugs 820349 & 816858
    
    Original change's description:
    > Update tab switcher state in ToolbarPhone after animation completes
    > 
    > Previously onTabSwitcherTransitionFinished was only called when
    > exiting the tab switcher. This change implements
    > onOverviewModeFinishedShowing to call this method.
    > 
    > BUG=710750
    > 
    > Change-Id: I1a34551a6cd93a54a9461441c25ce022144efa9e
    > Reviewed-on: https://chromium-review.googlesource.com/929991
    > Reviewed-by: David Trainor <dtrainor@chromium.org>
    > Reviewed-by: Ted Choc <tedchoc@chromium.org>
    > Commit-Queue: Matthew Jones <mdjones@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#539205}
    
    TBR=dtrainor@chromium.org,tedchoc@chromium.org,mdjones@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 710750
    Change-Id: Ia5dd268f289f1fbb5bb0e0614c6242ec905a4ecc
    Reviewed-on: https://chromium-review.googlesource.com/956937
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542143}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
@@ -585,11 +585,6 @@ public class ToolbarManager implements ToolbarTabController, UrlFocusChangeListe
             public void onOverviewModeFinishedHiding() {
                 mToolbar.onTabSwitcherTransitionFinished();
             }
-
-            @Override
-            public void onOverviewModeFinishedShowing() {
-                mToolbar.onTabSwitcherTransitionFinished();
-            }
         };
 
         mSceneChangeObserver = new SceneChangeObserver() {
