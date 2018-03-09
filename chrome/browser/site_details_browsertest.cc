5aac5d18765b68e2b30f753c1ef5ae8900f9abdd
erikchen <erikchen@chromium.org>
2018-03-09 16:53:34 +0000

    Remove unused memory metrics.
    This CL removes several memory metrics that are not being used. These are:
      * Memory.{ProcessType}Count
      * Memory.*.Committed
      * Memory.Renderer{Shrink,Growth}In30Min
      * Memory.Swap.*
    
    The memory metrics actively being used and developed are emitted in
    ProcessMemoryMetricsEmitter. These are emitted in both UMA and UKM form. The UKM
    form implicitly includes ProcessType, though with less granularity. Both the UMA
    and UKMs include PrivateMemoryFootprint, a cross-platform consistent metric that
    supercedes Memory.*.Committed. There is no replacement for
    Memory.Renderer{Shrink,Growth}In30Min.
    
    Bug: 765470
    Change-Id: I857f710eeb3ddf233b63be3139ea90ebe8d9a5c9
    Reviewed-on: https://chromium-review.googlesource.com/952985
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Reviewed-by: Hajime Hoshi <hajimehoshi@chromium.org>
    Reviewed-by: Nick Carter <nick@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542144}

--- chrome/browser/site_details_browsertest.cc
+++ chrome/browser/site_details_browsertest.cc
@@ -55,7 +55,7 @@ namespace {
 
 class TestMemoryDetails : public MetricsMemoryDetails {
  public:
-  TestMemoryDetails() : MetricsMemoryDetails(base::DoNothing(), nullptr) {}
+  TestMemoryDetails() : MetricsMemoryDetails(base::DoNothing()) {}
 
   void StartFetchAndWait() {
     uma_.reset(new base::HistogramTester());
