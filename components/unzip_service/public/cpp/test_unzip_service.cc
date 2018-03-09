6fc704e2f5fff9861c1fd33b04d0fbd87ab47da6
Jay Civelli <jcivelli@google.com>
2018-03-09 16:33:26 +0000

    Add a test Unzip service to simulate crashers
    Adding a test Unzip service that disconnects when accessed to simulate
    crashers. It will be used in a follow up CL to ensure extension
    installation deals correctly with the unzip service crashing.
    
    Bug: 799220
    Change-Id: I511b591e5740ac4c0e64f63d7b28cd32173a4990
    Reviewed-on: https://chromium-review.googlesource.com/955930
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Jay Civelli <jcivelli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542136}

---
+++ components/unzip_service/public/cpp/test_unzip_service.cc
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/unzip_service/public/cpp/test_unzip_service.h"
+
+namespace unzip {
+
+CrashyUnzipService::CrashyUnzipService() = default;
+
+CrashyUnzipService::~CrashyUnzipService() = default;
+
+// service_manager::Service implmentation:
+void CrashyUnzipService::OnStart() {}
+
+void CrashyUnzipService::OnBindInterface(
+    const service_manager::BindSourceInfo& source_info,
+    const std::string& interface_name,
+    mojo::ScopedMessagePipeHandle interface_pipe) {
+  DCHECK(interface_name == mojom::Unzipper::Name_);
+  DCHECK(!unzipper_binding_);
+  unzipper_binding_ = std::make_unique<mojo::Binding<mojom::Unzipper>>(
+      this, mojom::UnzipperRequest(std::move(interface_pipe)));
+}
+
+// unzip::mojom::Unzipper:
+void CrashyUnzipService::Unzip(base::File zip_file,
+                               filesystem::mojom::DirectoryPtr output_dir,
+                               UnzipCallback callback) {
+  unzipper_binding_.reset();
+}
+
+void CrashyUnzipService::UnzipWithFilter(
+    base::File zip_file,
+    filesystem::mojom::DirectoryPtr output_dir,
+    mojom::UnzipFilterPtr filter,
+    UnzipWithFilterCallback callback) {
+  unzipper_binding_.reset();
+}
+
+}  // namespace unzip
