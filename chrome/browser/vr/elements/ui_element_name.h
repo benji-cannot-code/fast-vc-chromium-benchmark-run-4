34d8112193c44c3a5a218c57c0e1479e5704a11b
Ian Vollick <vollick@chromium.org>
2018-03-09 02:41:56 +0000

    [vr] Hide content at unsupported feature prompt
    Previously this UI would be overlaid atop the content quad, making it
    quite hard to read.
    
    Bug: 820261
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_vr;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ib44d055e1774bdbb86c9a18f92d1fbe1d6e17bbb
    Reviewed-on: https://chromium-review.googlesource.com/956504
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542001}

--- chrome/browser/vr/elements/ui_element_name.h
+++ chrome/browser/vr/elements/ui_element_name.h
@@ -65,6 +65,7 @@ enum UiElementName {
   kOmniboxOuterLayoutSpacer,
   kOmniboxShadow,
   k2dBrowsingVisibiltyControlForVoice,
+  k2dBrowsingVisibilityControlForPrompt,
   k2dBrowsingVisibiltyControlForSiteInfoPrompt,
   k2dBrowsingOpacityControlForAudioPermissionPrompt,
   k2dBrowsingOpacityControlForNativeDialogPrompt,
