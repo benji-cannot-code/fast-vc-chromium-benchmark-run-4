d67aeb874052f200a85915522ca8c04d82f97b0f
Ahmed Fakhry <afakhry@chromium.org>
2018-03-09 19:22:49 +0000

    Touchable Chrome: update caption buttons icons colors
    BUG=805267
    
    Change-Id: I39a7b31ad3f796cd059bafece3fa851da3e25049
    Reviewed-on: https://chromium-review.googlesource.com/954066
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542194}

--- ash/frame/caption_buttons/frame_caption_button.cc
+++ ash/frame/caption_buttons/frame_caption_button.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/frame/caption_buttons/frame_caption_button.h"
 
 #include "ash/ash_constants.h"
+#include "ui/base/material_design/material_design_controller.h"
 #include "ui/gfx/animation/slide_animation.h"
 #include "ui/gfx/animation/throb_animation.h"
 #include "ui/gfx/canvas.h"
@@ -57,7 +58,17 @@ FrameCaptionButton::~FrameCaptionButton() = default;
 
 // static
 SkColor FrameCaptionButton::GetButtonColor(bool use_light_images) {
+  if (ui::MaterialDesignController::IsTouchOptimizedUiEnabled())
+    return use_light_images ? gfx::kGoogleGrey100 : gfx::kGoogleGrey800;
+
   return use_light_images ? SK_ColorWHITE : gfx::kChromeIconGrey;
+}
+
+// static
+float FrameCaptionButton::GetInactiveButtonColorAlphaRatio() {
+  return ui::MaterialDesignController::IsTouchOptimizedUiEnabled()
+             ? kInactiveFrameButtonIconAlphaRatioTouch
+             : kInactiveFrameButtonIconAlphaRatio;
 }
 
 void FrameCaptionButton::SetImage(CaptionButtonIcon icon,
@@ -186,7 +197,7 @@ int FrameCaptionButton::GetAlphaForIcon(int base_alpha) const {
     return base_alpha;
 
   // Paint icons as active when they are hovered over or pressed.
-  double inactive_alpha = kInactiveFrameButtonIconAlphaRatio;
+  double inactive_alpha = GetInactiveButtonColorAlphaRatio();
 
   if (hover_animation().is_animating()) {
     inactive_alpha =
