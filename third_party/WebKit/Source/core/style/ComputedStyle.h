13ee14b1f907abb8fe0bcc9ad81a4a6b658972f7
Kent Tamura <tkent@chromium.org>
2018-03-09 14:11:59 +0000

    Apply snake_case to generated ComputedStyleBase*.*
    Bug: 770603
    Change-Id: I6a161d1508353fe0fa2c17fb1580ce5385555353
    Reviewed-on: https://chromium-review.googlesource.com/956655
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542096}

--- third_party/WebKit/Source/core/style/ComputedStyle.h
+++ third_party/WebKit/Source/core/style/ComputedStyle.h
@@ -28,8 +28,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ComputedStyle_h
 
 #include <memory>
-#include "core/ComputedStyleBase.h"
 #include "core/CoreExport.h"
+#include "core/computed_style_base.h"
 #include "core/css/StyleAutoColor.h"
 #include "core/css/StyleColor.h"
 #include "core/css/properties/CSSProperty.h"
--- third_party/WebKit/Source/core/style/ComputedStyleConstants.h
+++ third_party/WebKit/Source/core/style/ComputedStyleConstants.h
@@ -29,8 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ComputedStyleConstants_h
 #define ComputedStyleConstants_h
 
-#include "core/ComputedStyleBaseConstants.h"
 #include <cstddef>
+#include "core/computed_style_base_constants.h"
 
 namespace blink {
 
