492e39244f99bd72993c6ced0368673ef37d5bff
Eric Karl <ericrk@chromium.org>
2018-03-09 22:37:58 +0000

    Clean up redundant logic in TestInProcessContextProvider
    We used to fake out the Gpu::Capabilities in TestInProcessContextProvider with some custom logic.
    This logic appears to be redundant with the actual capabilities we can get from the real context.
    Additionally, as this code is used in pixel tests, having fake capabilities that mismatch with the
    real capabilities seems risky, so if the real caps are working it feels like we should use them.
    
    Also cleans up some other unused code.
    
    R=enne
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I1bf82739387a358e95372efb1dac571d7ed15cbe
    Reviewed-on: https://chromium-review.googlesource.com/957315
    Reviewed-by: enne <enne@chromium.org>
    Commit-Queue: Eric Karl <ericrk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542257}

--- components/viz/service/display/renderer_pixeltest.cc
+++ components/viz/service/display/renderer_pixeltest.cc
@@ -1450,13 +1450,10 @@ class VideoGLRendererPixelHiLoTest
           ::testing::tuple<bool, HighbitTexture>> {
  public:
   void SetSupportHighbitTexture(HighbitTexture texture) {
-    cc::TestInProcessContextProvider* context_provider =
-        GetTestInProcessContextProvider();
     switch (texture) {
       case HighbitTexture::Y8:
         break;
       case HighbitTexture::R16_EXT:
-        context_provider->SetSupportTextureNorm16(true);
         video_resource_updater_->SetUseR16ForTesting(true);
         break;
     }
