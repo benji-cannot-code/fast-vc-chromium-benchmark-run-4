95112c7554b77a0f59d0a27bba4a372094e58f6c
Sylvain Defresne <sdefresne@chromium.org>
2018-03-09 11:46:19 +0000

    Remove GetToolbarVoiceSearchButtonImageId
    The method is no longer called nor overridden, so it can
    safely be removed (it was deprecated).
    
    Bug: none
    Change-Id: I31770bfd26e33977a4007ae50cf423196c914ebe
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Reviewed-on: https://chromium-review.googlesource.com/951692
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542082}

--- ios/chrome/browser/providers/images/chromium_branded_image_provider.mm
+++ ios/chrome/browser/providers/images/chromium_branded_image_provider.mm
@@ -46,11 +46,6 @@ ChromiumBrandedImageProvider::GetSigninConfirmationPersonalizeServicesImage() {
   return rb.GetNativeImageNamed(IDR_IOS_SETTINGS_INFO_24).ToUIImage();
 }
 
-bool ChromiumBrandedImageProvider::GetToolbarVoiceSearchButtonImageId(
-    int* image_id) {
-  return false;
-}
-
 NSArray<UIImage*>*
 ChromiumBrandedImageProvider::GetToolbarVoiceSearchButtonImages(
     bool incognito) {
