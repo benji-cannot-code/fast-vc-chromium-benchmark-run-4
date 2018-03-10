ff86a6a328e3efe0fcc03e78f52655f913929671
Scott Violet <sky@chromium.org>
2018-03-10 02:27:29 +0000

    WindowService: Removes EventProcessorDelegate::IsWindowInDisplayRoot
    And instead changes GetRootWindowForEventDispatch() to return null if
    not in a valid hierarchy for event dispatch.
    
    BUG=none
    TEST=none
    
    Change-Id: I9e8d23d3607fb600d2cf0c80ff1086688a4b6c13
    Reviewed-on: https://chromium-review.googlesource.com/956849
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542324}

--- services/ui/ws/event_processor_unittest.cc
+++ services/ui/ws/event_processor_unittest.cc
@@ -205,10 +205,7 @@ class TestEventProcessorDelegate : public EventProcessorDelegate {
     return nullptr;
   }
   ServerWindow* GetRootWindowForEventDispatch(ServerWindow* window) override {
-    return window->GetRootForDrawn();
-  }
-  bool IsWindowInDisplayRoot(const ServerWindow* window) override {
-    return is_window_in_display_root_;
+    return is_window_in_display_root_ ? window->GetRootForDrawn() : nullptr;
   }
 
   Delegate* delegate_;
