7acd928a37041febce5f636f7a14ace9b8bb65b4
Joel Einbinder <einbinder@chromium.org>
2018-03-11 01:07:21 +0000

    DevTools: Protocol Monitor
    Change-Id: Ib3db9d58f1c4b1f91a1fc876a354907208958e11
    Reviewed-on: https://chromium-review.googlesource.com/912466
    Commit-Queue: Paul Irish <paulirish@chromium.org>
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542391}

--- third_party/WebKit/Source/devtools/front_end/ui/Panel.js
+++ third_party/WebKit/Source/devtools/front_end/ui/Panel.js
@@ -27,12 +27,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 // For testing.
-UI.panels = [];
+UI.panels = {};
 
 /**
  * @unrestricted
  */
 UI.Panel = class extends UI.VBox {
+  /**
+   * @param {string} name
+   */
   constructor(name) {
     super();
 
