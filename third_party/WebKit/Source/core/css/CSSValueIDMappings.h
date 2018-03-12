7fd1fca310411044c36dfa12965a6ad4c737ad64
Kent Tamura <tkent@chromium.org>
2018-03-12 17:00:28 +0000

    Apply snake_case to generated CSSValueIDMappingsGenerated.h
    This CL has no behavior changes.
    
    Bug: 770603
    Change-Id: If833ca44fbf30eaa592905d4283c593f353908af
    Reviewed-on: https://chromium-review.googlesource.com/958765
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542514}

--- third_party/WebKit/Source/core/css/CSSValueIDMappings.h
+++ third_party/WebKit/Source/core/css/CSSValueIDMappings.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CSSValueIDMappings_h
 #define CSSValueIDMappings_h
 
-#include "core/CSSValueIDMappingsGenerated.h"
+#include "core/css_value_id_mappings_generated.h"
 
 namespace blink {
 
