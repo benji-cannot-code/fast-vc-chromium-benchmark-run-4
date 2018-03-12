4820644fbf9c88061be718adfcf66b34a3d590c3
Misha Efimov <mef@chromium.org>
2018-03-12 14:50:23 +0000

    [Cronet] Improve received byte count in UrlRequest.Callback.onResponseStarted().
    Bug: 819302
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I218c03a6a308afa265468876db28ff710a09399b
    Reviewed-on: https://chromium-review.googlesource.com/957126
    Commit-Queue: Misha Efimov <mef@chromium.org>
    Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542481}

--- components/cronet/android/java/src/org/chromium/net/impl/CronetUrlRequest.java
+++ components/cronet/android/java/src/org/chromium/net/impl/CronetUrlRequest.java
@@ -72,7 +72,6 @@ public final class CronetUrlRequest extends UrlRequestBase {
      * mCallback.onRedirectReceived is called.
      */
     private final List<String> mUrlChain = new ArrayList<String>();
-    private long mReceivedByteCountFromRedirects;
 
     private final VersionSafeCallbacks.UrlRequestCallback mCallback;
     private final String mInitialUrl;
@@ -409,14 +408,15 @@ public final class CronetUrlRequest extends UrlRequestBase {
 
     private UrlResponseInfoImpl prepareResponseInfoOnNetworkThread(int httpStatusCode,
             String httpStatusText, String[] headers, boolean wasCached, String negotiatedProtocol,
-            String proxyServer) {
+            String proxyServer, long receivedByteCount) {
         HeadersList headersList = new HeadersList();
         for (int i = 0; i < headers.length; i += 2) {
             headersList.add(new AbstractMap.SimpleImmutableEntry<String, String>(
                     headers[i], headers[i + 1]));
         }
         return new UrlResponseInfoImpl(new ArrayList<String>(mUrlChain), httpStatusCode,
-                httpStatusText, headersList, wasCached, negotiatedProtocol, proxyServer);
+                httpStatusText, headersList, wasCached, negotiatedProtocol, proxyServer,
+                receivedByteCount);
     }
 
     private void checkNotStarted() {
@@ -503,10 +503,9 @@ public final class CronetUrlRequest extends UrlRequestBase {
     private void onRedirectReceived(final String newLocation, int httpStatusCode,
             String httpStatusText, String[] headers, boolean wasCached, String negotiatedProtocol,
             String proxyServer, long receivedByteCount) {
-        final UrlResponseInfoImpl responseInfo = prepareResponseInfoOnNetworkThread(httpStatusCode,
-                httpStatusText, headers, wasCached, negotiatedProtocol, proxyServer);
-        mReceivedByteCountFromRedirects += receivedByteCount;
-        responseInfo.setReceivedByteCount(mReceivedByteCountFromRedirects);
+        final UrlResponseInfoImpl responseInfo =
+                prepareResponseInfoOnNetworkThread(httpStatusCode, httpStatusText, headers,
+                        wasCached, negotiatedProtocol, proxyServer, receivedByteCount);
 
         // Have to do this after creating responseInfo.
         mUrlChain.add(newLocation);
@@ -539,9 +538,10 @@ public final class CronetUrlRequest extends UrlRequestBase {
     @SuppressWarnings("unused")
     @CalledByNative
     private void onResponseStarted(int httpStatusCode, String httpStatusText, String[] headers,
-            boolean wasCached, String negotiatedProtocol, String proxyServer) {
+            boolean wasCached, String negotiatedProtocol, String proxyServer,
+            long receivedByteCount) {
         mResponseInfo = prepareResponseInfoOnNetworkThread(httpStatusCode, httpStatusText, headers,
-                wasCached, negotiatedProtocol, proxyServer);
+                wasCached, negotiatedProtocol, proxyServer, receivedByteCount);
         Runnable task = new Runnable() {
             @Override
             public void run() {
@@ -585,7 +585,7 @@ public final class CronetUrlRequest extends UrlRequestBase {
     @CalledByNative
     private void onReadCompleted(final ByteBuffer byteBuffer, int bytesRead, int initialPosition,
             int initialLimit, long receivedByteCount) {
-        mResponseInfo.setReceivedByteCount(mReceivedByteCountFromRedirects + receivedByteCount);
+        mResponseInfo.setReceivedByteCount(receivedByteCount);
         if (byteBuffer.position() != initialPosition || byteBuffer.limit() != initialLimit) {
             failWithException(
                     new CronetExceptionImpl("ByteBuffer modified externally during read", null));
@@ -608,7 +608,7 @@ public final class CronetUrlRequest extends UrlRequestBase {
     @SuppressWarnings("unused")
     @CalledByNative
     private void onSucceeded(long receivedByteCount) {
-        mResponseInfo.setReceivedByteCount(mReceivedByteCountFromRedirects + receivedByteCount);
+        mResponseInfo.setReceivedByteCount(receivedByteCount);
         Runnable task = new Runnable() {
             @Override
             public void run() {
@@ -646,7 +646,7 @@ public final class CronetUrlRequest extends UrlRequestBase {
     private void onError(int errorCode, int nativeError, int nativeQuicError, String errorString,
             long receivedByteCount) {
         if (mResponseInfo != null) {
-            mResponseInfo.setReceivedByteCount(mReceivedByteCountFromRedirects + receivedByteCount);
+            mResponseInfo.setReceivedByteCount(receivedByteCount);
         }
         if (errorCode == NetworkException.ERROR_QUIC_PROTOCOL_FAILED) {
             failWithException(new QuicExceptionImpl(
--- components/cronet/android/test/javatests/src/org/chromium/net/BidirectionalStreamTest.java
+++ components/cronet/android/test/javatests/src/org/chromium/net/BidirectionalStreamTest.java
@@ -110,9 +110,8 @@ public class BidirectionalStreamTest {
             headersList.add(new AbstractMap.SimpleImmutableEntry<String, String>(
                     headers[i], headers[i + 1]));
         }
-        UrlResponseInfoImpl urlResponseInfo = new UrlResponseInfoImpl(
-                Arrays.asList(urls), statusCode, message, headersList, false, "h2", null);
-        urlResponseInfo.setReceivedByteCount(receivedBytes);
+        UrlResponseInfoImpl urlResponseInfo = new UrlResponseInfoImpl(Arrays.asList(urls),
+                statusCode, message, headersList, false, "h2", null, receivedBytes);
         return urlResponseInfo;
     }
 
