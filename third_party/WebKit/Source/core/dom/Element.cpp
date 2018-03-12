062b7bee377749f8ae18a4b50e5f8251f79e659b
Rune Lillesveen <futhark@chromium.org>
2018-03-12 13:16:32 +0000

    [Squad] Don't compute style for attaching non-flat-tree elements.
    RecalcStyleForReattach would compute style for slot elements even
    though they were not part of the flat tree before SlotInFlatTree runtime
    flag was introduced.
    
    Moved SetNonAttachedStyle(nullptr) to Element to make sure we only ever
    set or clear non-attached style on Elements, and added DCHECKs to
    support that and the fact that we only set non-attached style on
    elements which are part of the flat tree.
    
    This was partly the regression for 792080, but will not have an effect
    for the performance tests on master since SlotInFlatTree is enabled.
    
    Bug: 792080
    Change-Id: I47659300dea9cf4411a4e06f574dbad15c29fdfb
    Reviewed-on: https://chromium-review.googlesource.com/958465
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542470}

--- third_party/WebKit/Source/core/dom/Element.cpp
+++ third_party/WebKit/Source/core/dom/Element.cpp
@@ -1961,6 +1961,7 @@ void Element::AttachLayoutTree(AttachContext& context) {
     shadow->Attach(children_context);
 
   ContainerNode::AttachLayoutTree(children_context);
+  SetNonAttachedStyle(nullptr);
   AddCallbackSelectors();
 
   CreateAndAttachPseudoElementIfNeeded(kPseudoIdAfter, children_context);
@@ -2279,13 +2280,23 @@ StyleRecalcChange Element::RecalcOwnStyle(StyleRecalcChange change) {
 }
 
 void Element::RecalcStyleForReattach() {
-  scoped_refptr<ComputedStyle> non_attached_style = StyleForLayoutObject();
-  SetNonAttachedStyle(non_attached_style);
-  SetNeedsReattachLayoutTree();
-  if (LayoutObjectIsNeeded(*non_attached_style) ||
-      ShouldStoreNonLayoutObjectComputedStyle(*non_attached_style)) {
-    RecalcShadowIncludingDescendantStylesForReattach();
+  bool recalc_descendants = false;
+  if (ParentComputedStyle()) {
+    scoped_refptr<ComputedStyle> non_attached_style = StyleForLayoutObject();
+    SetNeedsReattachLayoutTree();
+    SetNonAttachedStyle(non_attached_style);
+    recalc_descendants =
+        LayoutObjectIsNeeded(*non_attached_style) ||
+        ShouldStoreNonLayoutObjectComputedStyle(*non_attached_style);
+  } else {
+    // Elements which cannot participate in the flat tree are <content> and
+    // <slot> if SlotInFlatTree is not enabled. Even though we should not
+    // compute their styles for re-attachment, we may need to compute their
+    // children's style if fallback is rendered.
+    recalc_descendants = !CanParticipateInFlatTree();
   }
+  if (recalc_descendants)
+    RecalcShadowIncludingDescendantStylesForReattach();
 }
 
 void Element::RecalcShadowIncludingDescendantStylesForReattach() {
