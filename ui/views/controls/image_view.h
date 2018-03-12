7b4979701c9b3a6a1e220fd77a997a844641650e
qi1988.yang <qi1988.yang@samsung.com>
2018-03-12 08:05:17 +0000

    Use base::Optional instead of ImageView::image_size_set_
    As close as possible to the C++17 standard, use std::Optional.
    
    Bug: 805207
    
    Signed-off-by: qi1988.yang <qi1988.yang@samsung.com>
    Change-Id: I88560f320ad38b986eb84a73e488eb4968ea1660
    Reviewed-on: https://chromium-review.googlesource.com/956652
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542441}

--- ui/views/controls/image_view.h
+++ ui/views/controls/image_view.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define UI_VIEWS_CONTROLS_IMAGE_VIEW_H_
 
 #include "base/macros.h"
+#include "base/optional.h"
 #include "ui/gfx/image/image_skia.h"
 #include "ui/views/view.h"
 
@@ -99,11 +100,8 @@ class VIEWS_EXPORT ImageView : public View {
   // properties.
   gfx::Point ComputeImageOrigin(const gfx::Size& image_size) const;
 
-  // Whether the image size is set.
-  bool image_size_set_;
-
   // The actual image size.
-  gfx::Size image_size_;
+  base::Optional<gfx::Size> image_size_;
 
   // The underlying image.
   gfx::ImageSkia image_;
