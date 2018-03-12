ce810be1229d66e3a19db242d244b2309c498cbb
Patricia Lor <patricialor@chromium.org>
2018-03-12 07:00:57 +0000

    MacViews: Make sure highlights are unaffected by the ripple ban on Mac.
    r534573 fixed a bug where hover highlights would disappear when a
    normally-ripple-enabled button was long-pressed by always showing the highlight
    during the ripple animation (which is invisible on MacViews). However, this fix
    also caused non-highlight enabled bubbles to start showing the default highlight
    implementation, which looks like a transparent reddish square. Fix this by
    allowing ink drop clients to set their AutoHighlightMode according to the
    platform.
    
    Bug: 809896, 795224
    Change-Id: I4e1269900c38b75affb7606b6eab5f034cd609d4
    Reviewed-on: https://chromium-review.googlesource.com/930522
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Reviewed-by: Mohsen Izadi <mohsen@chromium.org>
    Commit-Queue: Patti <patricialor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542433}

--- ui/views/animation/ink_drop_impl.cc
+++ ui/views/animation/ink_drop_impl.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/animation/ink_drop_highlight.h"
 #include "ui/views/animation/ink_drop_host.h"
 #include "ui/views/animation/square_ink_drop_ripple.h"
+#include "ui/views/style/platform_style.h"
 
 namespace views {
 
@@ -611,6 +612,12 @@ void InkDropImpl::SetAutoHighlightMode(AutoHighlightMode auto_highlight_mode) {
   highlight_state_factory_ =
       std::make_unique<HighlightStateFactory>(auto_highlight_mode, this);
   SetHighlightState(highlight_state_factory_->CreateStartState());
+}
+
+void InkDropImpl::SetAutoHighlightModeForPlatform() {
+  SetAutoHighlightMode(PlatformStyle::kUseRipples
+                           ? AutoHighlightMode::HIDE_ON_RIPPLE
+                           : AutoHighlightMode::SHOW_ON_RIPPLE);
 }
 
 void InkDropImpl::HostSizeChanged(const gfx::Size& new_size) {
