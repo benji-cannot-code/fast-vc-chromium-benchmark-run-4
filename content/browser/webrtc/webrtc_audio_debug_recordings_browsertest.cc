6244f2aa8f74010e73106225bad2f3fb602e0255
Patrik Höglund <phoglund@chromium.org>
2018-03-12 15:38:46 +0000

    Disable CallWithAudioDebugRecordingsEnabledThenDisabled.
    It hits flaky renderer crashes.
    
    Bug: chromium:820934
    Change-Id: I7d379fb5a4bfb5e50caf6109632756d7fb03b7ee
    Reviewed-on: https://chromium-review.googlesource.com/958519
    Commit-Queue: Patrik Höglund <phoglund@chromium.org>
    Reviewed-by: Henrik Grunell <grunell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542489}

--- content/browser/webrtc/webrtc_audio_debug_recordings_browsertest.cc
+++ content/browser/webrtc/webrtc_audio_debug_recordings_browsertest.cc
@@ -105,11 +105,10 @@ class WebRtcAudioDebugRecordingsBrowserTest
   ~WebRtcAudioDebugRecordingsBrowserTest() override {}
 };
 
-#if defined(OS_ANDROID) && defined(ADDRESS_SANITIZER)
+#if defined(OS_ANDROID)
 // Renderer crashes under Android ASAN: https://crbug.com/408496.
-#define MAYBE_CallWithAudioDebugRecordings DISABLED_CallWithAudioDebugRecordings
-#elif defined(OS_ANDROID)
-// Renderer crashes on Android M. https://crbug.com/535728.
+// Renderer crashes under Android: https://crbug.com/820934.
+// Failures on Android M. https://crbug.com/535728.
 #define MAYBE_CallWithAudioDebugRecordings DISABLED_CallWithAudioDebugRecordings
 #else
 #define MAYBE_CallWithAudioDebugRecordings CallWithAudioDebugRecordings
@@ -204,8 +203,9 @@ IN_PROC_BROWSER_TEST_F(WebRtcAudioDebugRecordingsBrowserTest,
 // TODO(grunell): Add test for multiple dumps when re-use of
 // MediaStreamAudioProcessor in AudioCapturer has been removed.
 
-#if defined(OS_ANDROID) && defined(ADDRESS_SANITIZER)
+#if defined(OS_ANDROID)
 // Renderer crashes under Android ASAN: https://crbug.com/408496.
+// Renderer crashes under Android: https://crbug.com/820934.
 #define MAYBE_CallWithAudioDebugRecordingsEnabledThenDisabled \
   DISABLED_CallWithAudioDebugRecordingsEnabledThenDisabled
 #else
@@ -253,12 +253,10 @@ IN_PROC_BROWSER_TEST_F(WebRtcAudioDebugRecordingsBrowserTest,
   base::ThreadRestrictions::SetIOAllowed(prev_io_allowed);
 }
 
-#if defined(OS_ANDROID) && defined(ADDRESS_SANITIZER)
+#if defined(OS_ANDROID)
 // Renderer crashes under Android ASAN: https://crbug.com/408496.
-#define MAYBE_TwoCallsWithAudioDebugRecordings \
-  DISABLED_TwoCallsWithAudioDebugRecordings
-#elif defined(OS_ANDROID)
-// Renderer crashes on Android M. https://crbug.com/535728.
+// Renderer crashes under Android: https://crbug.com/820934.
+// Failures on Android M. https://crbug.com/535728.
 #define MAYBE_TwoCallsWithAudioDebugRecordings \
   DISABLED_TwoCallsWithAudioDebugRecordings
 #else
