749640a5ba45a9f48cf7af22b2c23be6bc6dffb7
Michael Thiessen <mthiesse@chromium.org>
2018-03-12 15:44:43 +0000

    VR: Enable Chrome Daydream icons by default.
    Bug: 820993
    Change-Id: I12fe9b0a899152802d8d3e509fb8513f0356c51a
    Reviewed-on: https://chromium-review.googlesource.com/959048
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542493}

--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -395,7 +395,7 @@ const base::Feature kVrBrowsingNativeAndroidUi{
     "VrBrowsingNativeAndroidUi", base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kVrIconInDaydreamHome{"VrIconInDaydreamHome",
-                                          base::FEATURE_DISABLED_BY_DEFAULT};
+                                          base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kVrLaunchIntents{"VrLaunchIntents",
                                      base::FEATURE_DISABLED_BY_DEFAULT};
