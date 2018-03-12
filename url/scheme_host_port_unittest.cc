b0c38ff25fdb56af10788032a11c8eddb818d1a5
Charles Harrison <csharrison@chromium.org>
2018-03-12 17:30:55 +0000

    [url] For empty hosts, just trust GURL to make the invalid/valid decision
    The SchemeHostPort always considers tuples with SCHEME_WITHOUT_PORT to
    have empty hosts without being invalid (to cater to cases like file
    schemes which can have hosts or not).
    
    This led to a mismatch in SchemeHostPost::GetURL where we always
    considered URLs with empty hosts to be valid GURLs, but this is not an
    assumption that holds true. This CL just punts the valid decision in this
    case to GURL for re-parsing, since it should be a rare occurance.
    
    Bug: 820194
    Change-Id: I3f4441d8c71a9b9500c71dbe98f0769f42ad13b5
    Reviewed-on: https://chromium-review.googlesource.com/956424
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542527}

--- url/scheme_host_port_unittest.cc
+++ url/scheme_host_port_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
 #include "url/scheme_host_port.h"
+#include "url/url_util.h"
 
 namespace {
 
@@ -251,6 +252,23 @@ TEST(SchemeHostPortTest, Comparison) {
       EXPECT_EQ(j < i, to_compare < current) << j << " < " << i;
     }
   }
+}
+
+// Some schemes have optional authority. Make sure that GURL conversion from
+// SchemeHostPort is not opinionated in that regard. For more info, See
+// crbug.com/820194, where we considered all SchemeHostPorts with
+// SCHEME_WITHOUT_PORT as valid with empty hosts, even though some are not (e.g.
+// chrome URLs).
+TEST(SchemeHostPortTest, EmptyHostGurlConversion) {
+  url::AddStandardScheme("chrome", url::SCHEME_WITHOUT_PORT);
+
+  GURL chrome_url("chrome:");
+  EXPECT_FALSE(chrome_url.is_valid());
+
+  url::SchemeHostPort chrome_tuple("chrome", "", 0);
+  EXPECT_FALSE(chrome_tuple.GetURL().is_valid());
+  ExpectParsedUrlsEqual(GURL(chrome_tuple.Serialize()), chrome_tuple.GetURL());
+  ExpectParsedUrlsEqual(chrome_url, chrome_tuple.GetURL());
 }
 
 }  // namespace url
