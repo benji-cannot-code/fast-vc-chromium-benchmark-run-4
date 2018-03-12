55d2266c179d21062cb89dfff92d92aaf2fd2f53
Michael Thiessen <mthiesse@chromium.org>
2018-03-12 19:02:22 +0000

    Revert "VR: Remove VR Launch Intents flag"
    This reverts commit c83231b713167dea576212396302177502c1ed91.
    
    Reason for revert: Should not have removed flags from enums.xml
    
    Original change's description:
    > VR: Remove VR Launch Intents flag
    > 
    > This flag was really only for development while we weren't sure how to
    > handle VR Intents. With Chrome VR browsing already shipped, there's no
    > reason not to allow Intents to launch it.
    > 
    > Change-Id: Ibbc7e5ea41668c073a4a04340ec45bfe40025b0f
    > Reviewed-on: https://chromium-review.googlesource.com/959051
    > Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    > Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#542508}
    
    TBR=yfriedman@chromium.org,mthiesse@chromium.org
    
    Change-Id: I6b92369c483f7f0d8a11290bb9a50f6cfd1ea766
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/958021
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542550}

--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -129,6 +129,7 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &kVrBrowsingInCustomTab,
     &kVrBrowsingNativeAndroidUi,
     &kVrIconInDaydreamHome,
+    &kVrLaunchIntents,
     &payments::features::kWebPaymentsMethodSectionOrderV2,
     &payments::features::kWebPaymentsModifiers,
     &kWebPaymentsSingleAppUiSkip,
@@ -387,6 +388,9 @@ const base::Feature kVrBrowsingNativeAndroidUi{
 
 const base::Feature kVrIconInDaydreamHome{"VrIconInDaydreamHome",
                                           base::FEATURE_ENABLED_BY_DEFAULT};
+
+const base::Feature kVrLaunchIntents{"VrLaunchIntents",
+                                     base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kWebPaymentsSingleAppUiSkip{
     "WebPaymentsSingleAppUiSkip", base::FEATURE_ENABLED_BY_DEFAULT};
--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1789,6 +1789,9 @@ extern const char kVrBrowsingInCustomTabDescription[];
 extern const char kVrIconInDaydreamHomeName[];
 extern const char kVrIconInDaydreamHomeDescription[];
 
+extern const char kVrLaunchIntentsName[];
+extern const char kVrLaunchIntentsDescription[];
+
 extern const char kWebVrAutopresentFromIntentName[];
 extern const char kWebVrAutopresentFromIntentDescription[];
 
