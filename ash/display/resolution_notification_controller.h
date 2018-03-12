d231b1ba4c56307a1edb12acaf91cbe5cce58ca6
Evan Stade <estade@chromium.org>
2018-03-12 20:40:50 +0000

    Use ThunkNotificationDelegate in ResolutionNotificationController.
    Bug: none
    Change-Id: Id4c4d26cd02c673b820a4b2cfcdcb226ac1f2759
    Reviewed-on: https://chromium-review.googlesource.com/958092
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542587}

--- ash/display/resolution_notification_controller.h
+++ ash/display/resolution_notification_controller.h
@@ -13,9 +13,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/gtest_prod_util.h"
 #include "base/macros.h"
+#include "base/memory/weak_ptr.h"
 #include "base/timer/timer.h"
 #include "ui/display/display_observer.h"
 #include "ui/gfx/geometry/size.h"
+#include "ui/message_center/public/cpp/notification_delegate.h"
 
 namespace chromeos {
 FORWARD_DECLARE_TEST(DisplayPrefsTest, PreventStore);
@@ -27,7 +29,8 @@ namespace ash {
 // also manages the timeout in case the new resolution is unusable.
 class ASH_EXPORT ResolutionNotificationController
     : public display::DisplayObserver,
-      public WindowTreeHostManager::Observer {
+      public WindowTreeHostManager::Observer,
+      public message_center::NotificationObserver {
  public:
   ResolutionNotificationController();
   ~ResolutionNotificationController() override;
@@ -65,14 +68,10 @@ class ASH_EXPORT ResolutionNotificationController
   // the notification times out.
   bool DoesNotificationTimeout();
 
-  // Called by the notification delegate when the user accepts the display
-  // resolution change. Set |close_notification| to true when the notification
-  // should be removed.
-  void AcceptResolutionChange(bool close_notification);
-
-  // Called by the notification delegate when the user wants to revert the
-  // display resolution change.
-  void RevertResolutionChange(bool display_was_removed);
+  // message_center::NotificationObserver
+  void Close(bool by_user) override;
+  void Click() override;
+  void ButtonClick(int button_index) override;
 
  private:
   friend class ResolutionNotificationControllerTest;
@@ -91,6 +90,13 @@ class ASH_EXPORT ResolutionNotificationController
   // feedback.
   void CreateOrUpdateNotification(bool enable_spoken_feedback);
 
+  // Called when the user accepts the display resolution change. Set
+  // |close_notification| to true when the notification should be removed.
+  void AcceptResolutionChange(bool close_notification);
+
+  // Called when the user wants to revert the display resolution change.
+  void RevertResolutionChange(bool display_was_removed);
+
   // Called every second for timeout.
   void OnTimerTick();
 
@@ -106,6 +112,8 @@ class ASH_EXPORT ResolutionNotificationController
   static void SuppressTimerForTest();
 
   std::unique_ptr<ResolutionChangeInfo> change_info_;
+
+  base::WeakPtrFactory<ResolutionNotificationController> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(ResolutionNotificationController);
 };
