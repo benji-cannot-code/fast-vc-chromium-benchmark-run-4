5ec630a586d793557f91f9300a28f00155767920
Khushal <khushalsagar@chromium.org>
2018-03-12 19:40:13 +0000

    cc: Disable decoding of PaintRecord shaders in cc.
    For PaintRecord backed shaders, skia performs the rasterization of the
    shader's record at the required scale before tiling. In order to decode
    images in these records using cc's decode cache, we create a temporary
    SkPicture with images replaced with decodes from cc's cache and use
    that in a new SkPictureShader each time the PaintShader is rasterized.
    
    Skia internally maintains a cache of rasterized pictures used in these
    shaders, keyed on the SkPictureShader's uniqueID. Since a new
    SkPictureShader is created for each raster call, we don't get a cache
    hit in skia and end up re-rasterizing the shader's record.
    
    The ideal solution for this would be to maintain a PaintShaderCache
    which caches rasterized records. But given the rarity of these cases,
    its not worth duplicating skia's picture cache. However, if any of
    the images in these records are animated, they must go through cc's
    decode stack to update them and use cached animation frames. The
    change ensures that we only decode shaders in cc if they have animated
    images. Note that the performance hit of re-rasterization of records
    in these cases will still exist.
    
    R=chrishtr@chromium.org, enne@chromium.org, ericrk@chromium.org
    
    Bug: 817640
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ibd1f3db9902f21fca20c99be1cf9e36b54cd7085
    Reviewed-on: https://chromium-review.googlesource.com/952529
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Reviewed-by: enne <enne@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542570}

--- cc/paint/discardable_image_map.h
+++ cc/paint/discardable_image_map.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/paint/paint_export.h"
 #include "cc/paint/paint_flags.h"
 #include "cc/paint/paint_image.h"
+#include "cc/paint/paint_shader.h"
 #include "third_party/skia/include/core/SkCanvas.h"
 #include "third_party/skia/include/core/SkRefCnt.h"
 #include "ui/gfx/geometry/rect.h"
