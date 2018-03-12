44bbc5055959e086dc1b87e498950b07d0b8b0d5
Luciano Pacheco <lucmult@chromium.org>
2018-03-12 06:36:11 +0000

    Remove includes of <vector> on Blink
    Remove include of <vector> added by me on previous CLs. Even though
    these files use std::vector we don't want to promote the use of this
    thus we remove these includes.
    
    Bug: 820271
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I66a466a60d78ed66f8d93049ace52c673477d8bc
    Reviewed-on: https://chromium-review.googlesource.com/956824
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Luciano Pacheco (SYD) <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542431}

--- third_party/WebKit/Source/platform/graphics/Canvas2DLayerBridgeTest.cpp
+++ third_party/WebKit/Source/platform/graphics/Canvas2DLayerBridgeTest.cpp
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/graphics/Canvas2DLayerBridge.h"
 
 #include <utility>
-#include <vector>
 
 #include "base/location.h"
 #include "base/memory/scoped_refptr.h"
--- third_party/WebKit/Source/platform/graphics/ImageFrameGenerator.cpp
+++ third_party/WebKit/Source/platform/graphics/ImageFrameGenerator.cpp
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include <utility>
-#include <vector>
 
 #include "SkData.h"
 #include "platform/graphics/ImageDecodingStore.h"
