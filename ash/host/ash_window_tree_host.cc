bb0750b702baf4fe2243dbf4bdf7ecf376fade74
Christopher Cameron <ccameron@chromium.org>
2018-03-12 08:56:47 +0000

    Remove aura::WindowTreeHost::SetOutputSurfacePaddingInPixels
    This isn't used.
    
    TBR=sky
    
    Bug: 818085
    Change-Id: I2998682053bffc7a1b8b016d4eb8280dfcd20993
    Reviewed-on: https://chromium-review.googlesource.com/958003
    Reviewed-by: ccameron <ccameron@chromium.org>
    Commit-Queue: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542443}

--- ash/host/ash_window_tree_host.cc
+++ ash/host/ash_window_tree_host.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/env.h"
 #include "ui/aura/window_tree_host.h"
 #include "ui/events/event.h"
+#include "ui/gfx/geometry/insets.h"
 #include "ui/gfx/geometry/rect.h"
 
 namespace ash {
