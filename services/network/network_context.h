f9852545e68db102f5e731463c5ced33fd8b7f70
Helen Li <xunjieli@chromium.org>
2018-03-12 21:32:55 +0000

    Add TCP socket API to network service
    This CL adds TCP socket API to network service.
    This is partly based on mandoline implementation
    (https://codereview.chromium.org/1873463003/)
    
    Two TCP socket interfaces are added.
    - TCPConnectedSocket
    - TCPServerSocket
    
    TBR=miu@chromium.org
    
    Bug: 721401
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I56e2702c02b9c86a72eba68c85391c3b017a4d64
    Reviewed-on: https://chromium-review.googlesource.com/868711
    Reviewed-by: Helen Li <xunjieli@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Commit-Queue: Helen Li <xunjieli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542606}

--- services/network/network_context.h
+++ services/network/network_context.h
@@ -21,8 +21,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/cookie_manager.h"
 #include "services/network/http_cache_data_remover.h"
 #include "services/network/public/mojom/network_service.mojom.h"
+#include "services/network/public/mojom/tcp_socket.mojom.h"
 #include "services/network/public/mojom/udp_socket.mojom.h"
 #include "services/network/public/mojom/url_loader_factory.mojom.h"
+#include "services/network/socket_factory.h"
 #include "services/network/url_request_context_owner.h"
 
 namespace net {
@@ -34,7 +36,6 @@ namespace network {
 class NetworkService;
 class ResourceScheduler;
 class ResourceSchedulerClient;
-class UDPSocketFactory;
 class URLRequestContextBuilderMojo;
 
 // A NetworkContext creates and manages access to a URLRequestContext.
@@ -113,6 +114,19 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) NetworkContext
                             mojom::NetworkConditionsPtr conditions) override;
   void CreateUDPSocket(mojom::UDPSocketRequest request,
                        mojom::UDPSocketReceiverPtr receiver) override;
+  void CreateTCPServerSocket(
+      const net::IPEndPoint& local_addr,
+      uint32_t backlog,
+      const net::MutableNetworkTrafficAnnotationTag& traffic_annotation,
+      mojom::TCPServerSocketRequest request,
+      CreateTCPServerSocketCallback callback) override;
+  void CreateTCPConnectedSocket(
+      const base::Optional<net::IPEndPoint>& local_addr,
+      const net::AddressList& remote_addr_list,
+      const net::MutableNetworkTrafficAnnotationTag& traffic_annotation,
+      mojom::TCPConnectedSocketRequest request,
+      mojom::TCPConnectedSocketObserverPtr observer,
+      CreateTCPConnectedSocketCallback callback) override;
   void AddHSTSForTesting(const std::string& host,
                          base::Time expiry,
                          bool include_subdomains,
@@ -170,7 +184,7 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) NetworkContext
 
   std::unique_ptr<CookieManager> cookie_manager_;
 
-  std::unique_ptr<UDPSocketFactory> udp_socket_factory_;
+  SocketFactory socket_factory_;
 
   std::vector<std::unique_ptr<HttpCacheDataRemover>> http_cache_data_removers_;
 
