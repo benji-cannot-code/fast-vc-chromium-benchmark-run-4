bd8e6065e74066c1850d325a0a77095168d5d059
James Cook <jamescook@chromium.org>
2018-03-12 16:59:04 +0000

    cros: Make ChromeVox panel work under mash
    For mash, the ash window manager runs outside the browser process.
    Remove direct access to ash::Shell from ChromeVoxPanel.
    
    * Init the panel widget using mus properties
    * Introduce mojo API for setting whether the panel should be fullscreen
    * Remove mash checks from AccessibilityManager
    * Close ChromeVoxPanel immediately on shutdown and during reloads
    by session state changes and multi-profile user switching. This helps
    prevent having multiple panels open during shutdown, especially in
    tests.
    
    ChromeVox doesn't read things yet under mash, but at least the panel
    opens now.
    
    Bug: 628655
    Test: existing ash_unittests, browser_tests
    Change-Id: I14cbf946809787791115930ad33c1c1d3357bf88
    Reviewed-on: https://chromium-review.googlesource.com/956235
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542513}

--- chrome/browser/chromeos/accessibility/accessibility_manager.cc
+++ chrome/browser/chromeos/accessibility/accessibility_manager.cc
@@ -329,7 +329,7 @@ AccessibilityManager::~AccessibilityManager() {
   session_manager::SessionManager::Get()->RemoveObserver(this);
 
   if (chromevox_panel_) {
-    chromevox_panel_->Close();
+    chromevox_panel_->CloseNow();
     chromevox_panel_ = nullptr;
   }
 }
@@ -1255,8 +1255,7 @@ void AccessibilityManager::PostLoadChromeVox() {
   event_router->DispatchEventWithLazyListener(
       extension_misc::kChromeVoxExtensionId, std::move(event));
 
-  // TODO(mash): Support ChromeVoxPanel. http://crbug.com/628655
-  if (!chromevox_panel_ && chromeos::GetAshConfig() != ash::Config::MASH) {
+  if (!chromevox_panel_) {
     chromevox_panel_ = new ChromeVoxPanel(
         profile_,
         session_manager::SessionManager::Get()->IsUserSessionBlocked());
@@ -1297,12 +1296,8 @@ void AccessibilityManager::PostSwitchChromeVoxProfile() {
 }
 
 void AccessibilityManager::ReloadChromeVoxPanel() {
-  // TODO(mash): Support ChromeVoxPanel. http://crbug.com/628655
-  if (chromeos::GetAshConfig() == ash::Config::MASH)
-    return;
-
   if (chromevox_panel_) {
-    chromevox_panel_->Close();
+    chromevox_panel_->CloseNow();
     chromevox_panel_ = nullptr;
   }
   chromevox_panel_ = new ChromeVoxPanel(
--- chrome/browser/chromeos/accessibility/accessibility_manager_browsertest.cc
+++ chrome/browser/chromeos/accessibility/accessibility_manager_browsertest.cc
@@ -768,7 +768,7 @@ IN_PROC_BROWSER_TEST_P(AccessibilityManagerUserTypeLoginTest,
   EXPECT_EQ(kTestAutoclickDelayMs, GetAutoclickDelay());
   EXPECT_TRUE(IsMonoAudioEnabled());
 
-  session_manager::SessionManager::Get()->SessionStarted();
+  StartUserSession(GetParam());
 
   // Confirms that the features keep enabled after session starts.
   EXPECT_TRUE(IsLargeCursorEnabled());
