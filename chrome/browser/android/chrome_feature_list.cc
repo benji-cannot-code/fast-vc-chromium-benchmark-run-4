701ab99f6d9ab4a4a9f83c1e46a75df553be1fb8
Michael Thiessen <mthiesse@chromium.org>
2018-03-12 19:41:21 +0000

    Reland "VR: Remove VR Launch Intents flag"
    This reverts commit 848f0ea02034fd3068ffa6d6ca9a1df9891714d6.
    
    Reason for revert: Reland after reverting enums.xml change
    
    Original change's description:
    > Revert "VR: Remove VR Launch Intents flag"
    > 
    > This reverts commit c83231b713167dea576212396302177502c1ed91.
    > 
    > Reason for revert: Should not have removed flags from enums.xml
    > 
    > Original change's description:
    > > VR: Remove VR Launch Intents flag
    > > 
    > > This flag was really only for development while we weren't sure how to
    > > handle VR Intents. With Chrome VR browsing already shipped, there's no
    > > reason not to allow Intents to launch it.
    > > 
    > > Change-Id: Ibbc7e5ea41668c073a4a04340ec45bfe40025b0f
    > > Reviewed-on: https://chromium-review.googlesource.com/959051
    > > Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    > > Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#542508}
    > 
    > TBR=yfriedman@chromium.org,mthiesse@chromium.org
    > 
    > Change-Id: I6b92369c483f7f0d8a11290bb9a50f6cfd1ea766
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/958021
    > Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    > Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#542550}
    
    TBR=yfriedman@chromium.org,mthiesse@chromium.org
    
    Change-Id: Iadb349fe60ea6a3a843228f3a4dcf24c6b9f9589
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/958103
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542572}

--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -129,7 +129,6 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &kVrBrowsingInCustomTab,
     &kVrBrowsingNativeAndroidUi,
     &kVrIconInDaydreamHome,
-    &kVrLaunchIntents,
     &payments::features::kWebPaymentsMethodSectionOrderV2,
     &payments::features::kWebPaymentsModifiers,
     &kWebPaymentsSingleAppUiSkip,
@@ -388,9 +387,6 @@ const base::Feature kVrBrowsingNativeAndroidUi{
 
 const base::Feature kVrIconInDaydreamHome{"VrIconInDaydreamHome",
                                           base::FEATURE_ENABLED_BY_DEFAULT};
-
-const base::Feature kVrLaunchIntents{"VrLaunchIntents",
-                                     base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kWebPaymentsSingleAppUiSkip{
     "WebPaymentsSingleAppUiSkip", base::FEATURE_ENABLED_BY_DEFAULT};
