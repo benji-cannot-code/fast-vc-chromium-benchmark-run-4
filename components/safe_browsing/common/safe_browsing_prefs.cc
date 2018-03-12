22118abbe084df7e9bf5cbbff8afcf704ca78c91
Jialiu Lin <jialiul@chromium.org>
2018-03-12 16:15:47 +0000

    Add Finch feature to gate Enterprise Password Protection
    Also added finch experiment to fieldtrial_testing_config.json
    
    Bug: 804490,818559
    Change-Id: I70b0bfdaf7d59ca91f27d634363221b5f3cdb403
    Reviewed-on: https://chromium-review.googlesource.com/942308
    Commit-Queue: Jialiu Lin <jialiul@chromium.org>
    Reviewed-by: Luke Z <lpz@chromium.org>
    Reviewed-by: Gayane Petrosyan <gayane@chromium.org>
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542502}

--- components/safe_browsing/common/safe_browsing_prefs.cc
+++ components/safe_browsing/common/safe_browsing_prefs.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/pref_service.h"
+#include "components/safe_browsing/features.h"
 #include "content/public/browser/browser_thread.h"
 #include "net/base/url_util.h"
 #include "url/gurl.h"
@@ -504,9 +505,11 @@ base::ListValue GetSafeBrowsingPreferencesList(PrefService* prefs) {
 void GetSafeBrowsingWhitelistDomainsPref(
     const PrefService& prefs,
     std::vector<std::string>* out_canonicalized_domain_list) {
-  const base::ListValue* pref_value =
-      prefs.GetList(prefs::kSafeBrowsingWhitelistDomains);
-  CanonicalizeDomainList(*pref_value, out_canonicalized_domain_list);
+  if (base::FeatureList::IsEnabled(kEnterprisePasswordProtectionV1)) {
+    const base::ListValue* pref_value =
+        prefs.GetList(prefs::kSafeBrowsingWhitelistDomains);
+    CanonicalizeDomainList(*pref_value, out_canonicalized_domain_list);
+  }
 }
 
 void CanonicalizeDomainList(
@@ -527,8 +530,10 @@ void CanonicalizeDomainList(
 bool IsURLWhitelistedByPolicy(const GURL& url,
                               StringListPrefMember* pref_member) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::IO);
-  if (!pref_member)
+  if (!pref_member ||
+      !base::FeatureList::IsEnabled(kEnterprisePasswordProtectionV1)) {
     return false;
+  }
   std::vector<std::string> sb_whitelist_domains = pref_member->GetValue();
   return std::find_if(sb_whitelist_domains.begin(), sb_whitelist_domains.end(),
                       [&url](const std::string& domain) {
@@ -538,6 +543,9 @@ bool IsURLWhitelistedByPolicy(const GURL& url,
 
 bool IsURLWhitelistedByPolicy(const GURL& url, const PrefService& pref) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+  if (!base::FeatureList::IsEnabled(kEnterprisePasswordProtectionV1))
+    return false;
+
   if (!pref.HasPrefPath(prefs::kSafeBrowsingWhitelistDomains))
     return false;
   const base::ListValue* whitelist =
@@ -564,8 +572,10 @@ void GetPasswordProtectionLoginURLsPref(const PrefService& prefs,
 
 bool MatchesPasswordProtectionLoginURL(const GURL& url,
                                        const PrefService& prefs) {
-  if (!url.is_valid())
+  if (!base::FeatureList::IsEnabled(kEnterprisePasswordProtectionV1) ||
+      !url.is_valid()) {
     return false;
+  }
 
   std::vector<GURL> login_urls;
   GetPasswordProtectionLoginURLsPref(prefs, &login_urls);
@@ -597,8 +607,10 @@ GURL GetPasswordProtectionChangePasswordURLPref(const PrefService& prefs) {
 
 bool MatchesPasswordProtectionChangePasswordURL(const GURL& url,
                                                 const PrefService& prefs) {
-  if (!url.is_valid())
+  if (!base::FeatureList::IsEnabled(kEnterprisePasswordProtectionV1) ||
+      !url.is_valid()) {
     return false;
+  }
 
   GURL change_password_url = GetPasswordProtectionChangePasswordURLPref(prefs);
   if (change_password_url.is_empty())
--- components/safe_browsing/features.cc
+++ components/safe_browsing/features.cc
@@ -66,6 +66,9 @@ const base::Feature kAppendRecentNavigationEvents{
 const base::Feature kInspectDownloadedRarFiles{
     "InspectDownloadedRarFiles", base::FEATURE_DISABLED_BY_DEFAULT};
 
+const base::Feature kEnterprisePasswordProtectionV1{
+    "EnterprisePasswordProtectionV1", base::FEATURE_DISABLED_BY_DEFAULT};
+
 namespace {
 // List of experimental features. Boolean value for each list member should be
 // set to true if the experiment is currently running at a probability other
@@ -80,6 +83,7 @@ constexpr struct {
     {&kAppendRecentNavigationEvents, true},
     {&kCheckByURLLoaderThrottle, true},
     {&kDispatchSafetyNetCheckOffThread, false},
+    {&kEnterprisePasswordProtectionV1, true},
     {&kGaiaPasswordReuseReporting, true},
     {&kGoogleBrandedPhishingWarning, true},
     {&kInspectDownloadedRarFiles, true},
