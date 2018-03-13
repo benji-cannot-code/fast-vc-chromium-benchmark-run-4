00077a3215187d52736c3300bc99d7d8b997526d
Tarun Bansal <tbansal@chromium.org>
2018-03-13 04:14:56 +0000

    External WPT tests for Client hints
    This CL includes the test for "Accept-CH" header.
    
    If the origin includes "Accept-CH" in the response header,
    then the browser should include the specified client hints
    in the HTTP request headers for the resources contained
    in that page.
    
    Bug: 817049
    Change-Id: Id74193ab7a1cc3c4b03d66e080c43c64a6fcfbb2
    Reviewed-on: https://chromium-review.googlesource.com/951867
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542724}

---
+++ third_party/WebKit/LayoutTests/external/wpt/client-hints/echo_device_memory_header_received.py
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+def main(request, response):
+    """
+    Simple handler that sets a response header based on if device-memory
+    request header was received or not.
+    """
+
+    if "device-memory" in request.headers:
+            response.headers.set("device-memory-received", "true")
