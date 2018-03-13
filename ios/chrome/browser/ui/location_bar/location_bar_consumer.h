31a6d46d5191e27ceeb58e056c622e89a04dedf4
stkhapugin@chromium.org <stkhapugin@chromium.org>
2018-03-13 16:11:30 +0000

    Defocus the omnibox when switching tabs.
    CL 905511 introduced a bug where previously BVC was defocusing the
    omnibox by sending selectedTabChanged to the toolbar, but this was
    broken.
    
    Since the location bar now directly observes the webstate with its
    mediator, use this to defocus omnibox when the webstate changes.
    
    Bug: 813944
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Ibc67faaae115032e0a3c6e6b5e3879d7e52f8f0f
    Reviewed-on: https://chromium-review.googlesource.com/960663
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542817}

--- ios/chrome/browser/ui/location_bar/location_bar_consumer.h
+++ ios/chrome/browser/ui/location_bar/location_bar_consumer.h
@@ -13,6 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // text and the cursor position.
 - (void)updateOmniboxState;
 
+// Notifies consumer to defocus the omnibox (for example on tab change).
+- (void)defocusOmnibox;
+
 @end
 
 #endif  // IOS_CHROME_BROWSER_UI_LOCATION_BAR_LOCATION_BAR_CONSUMER_H_
--- ios/chrome/browser/ui/location_bar/location_bar_mediator.mm
+++ ios/chrome/browser/ui/location_bar/location_bar_mediator.mm
@@ -116,6 +116,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                      reason:(int)reason {
   DCHECK_EQ(_webStateList, webStateList);
   self.webState = newWebState;
+  [self.consumer defocusOmnibox];
 }
 
 #pragma mark - Setters
