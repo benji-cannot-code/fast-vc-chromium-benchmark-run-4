301921d378f53dfd95143bf4dff6799faa88844c
Chris Harrelson <chrishtr@chromium.org>
2018-03-13 00:23:04 +0000

    Devirtualize ScrollableArea::UsesCompositedScrolling.
    This method is called in various hot code paths, in particular
    in ObjectPaintInvalidator.
    
    Bug: 818709
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I6eaf1acadc5ac5721c7e42c900964bcc31fee769
    Reviewed-on: https://chromium-review.googlesource.com/959155
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542665}

--- third_party/WebKit/Source/core/paint/PaintLayerScrollableArea.h
+++ third_party/WebKit/Source/core/paint/PaintLayerScrollableArea.h
@@ -266,7 +266,6 @@ class CORE_EXPORT PaintLayerScrollableArea final
   GraphicsLayer* LayerForVerticalScrollbar() const override;
   GraphicsLayer* LayerForScrollCorner() const override;
 
-  bool UsesCompositedScrolling() const override;
   bool ShouldScrollOnMainThread() const override;
   bool ShouldUseIntegerScrollOffset() const override;
   bool IsActive() const override;
