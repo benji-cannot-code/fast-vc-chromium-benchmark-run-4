51d0f46b0142a9771c37b604deded1d7813a1034
Toni Barzic <tbarzic@chromium.org>
2018-03-13 23:08:10 +0000

    Wait for animated wallpaper changes to finish before suspending displays
    PowerEventObserver delays display suspend until lock screen UI is shown
    and compositors submit enough frames to know the UI changes have reached
    displays.
    This does not account for wallpaper changes due to screen lock:
      * On screen lock, the active wallpaper will be changed with its blurred
        version.
      * If screen lock changes the active user (e.g. if the screen is locked
        from the secondary user), the wallpaper will change to the new
        active user's blurred wallpaper.
    Given that PowerEventObserver does not ensure wallpaper switch is done
    before it stops drawing to the display, the first frame shown on the
    device resume might still contain the UI from before wallpaper change.
    
    This changes PowerEventObserver to additionally wait for any active
    wallpaper changes (which can be detected by checking
    WallpaperWidgetController::IsAnimating - set while wallpaper widget is
    being changed) to finish before it starts waiting for compositor frames
    to get composited, thus ensuring the correct wallpaper is set when the
    display/compositing is suspended.
    
    Adds the following methods to WallpaperWidgetController:
      * AddPendingAnimationEndCallback - to provide a way for
        PowerEventObserver to get notified when the pending wallpaper change
        completes
      * EndPendingAnimation which can be used to force immediate wallpaper
        change when device starts suspending (and prevents suspend flow from
        being blocked on unnecessarily waiting for new wallpaper animation)
    
    BUG=820436,803762
    
    Change-Id: Iad2074280ebba740b95c50b9d60a028bf5a0d45c
    Reviewed-on: https://chromium-review.googlesource.com/956757
    Reviewed-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Wenzhao (Colin) Zang <wzang@chromium.org>
    Commit-Queue: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542945}

--- ash/wallpaper/wallpaper_widget_controller.h
+++ ash/wallpaper/wallpaper_widget_controller.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_WALLPAPER_WALLPAPER_WIDGET_CONTROLLER_H_
 #define ASH_WALLPAPER_WALLPAPER_WIDGET_CONTROLLER_H_
 
+#include <list>
 #include <memory>
 
 #include "ash/ash_export.h"
@@ -36,6 +37,17 @@ class ASH_EXPORT WallpaperWidgetController {
   // Whether a wallpaper change is in progress - it will be set if
   // |animating_widget_| exists.
   bool IsAnimating() const;
+
+  // If an animating wallpaper change is in progress, it ends the animation and
+  // changes the wallpaper immediately.
+  // No-op if IsAnimation() returns false.
+  void EndPendingAnimation();
+
+  // If an animating wallpaper change is in progress, it adds a callback that
+  // will be run when the pending animation ends.
+  // The callback will not be run if a wallpaper animation is not in progress -
+  // in that case the method will return false.
+  void AddPendingAnimationEndCallback(base::OnceClosure callback);
 
   // Sets a new wallpaper widget - this will not change the primary widget
   // immediately. The primary widget will be switched when |widget|'s showing
@@ -72,6 +84,9 @@ class ASH_EXPORT WallpaperWidgetController {
   // Moves |animated_widget_| to |active_widget_|.
   void SetAnimatingWidgetAsActive();
 
+  // Runs callbacks in |pending_animation_end_callbacks_|.
+  void RunPendingAnimationEndCallbacks();
+
   // Callback that will be run when |active_widget_| is first set.
   base::OnceClosure wallpaper_set_callback_;
 
@@ -81,6 +96,10 @@ class ASH_EXPORT WallpaperWidgetController {
   // The pending wallpaper widget, which is currently in process of being
   // shown.
   std::unique_ptr<WidgetHandler> animating_widget_;
+
+  // Callbacks to be run when the |animating_widget_| stops animating and gets
+  // set as the active widget.
+  std::list<base::OnceClosure> pending_animation_end_callbacks_;
 
   DISALLOW_COPY_AND_ASSIGN(WallpaperWidgetController);
 };
