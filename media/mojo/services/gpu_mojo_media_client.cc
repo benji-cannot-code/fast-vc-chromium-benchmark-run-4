e167462f346310dd989b007503f59115c913e8c1
Rintaro Kuroiwa <rkuroiwa@chromium.org>
2018-03-13 17:29:46 +0000

    Build D3D11VideoDecoder by default
    - It keeps breaking and requires updating.
    - So build it by default, the feature is still behind a runtime flag.
    - GN files are cleaned up so that "mojo_media_host" is set with
      clearer logic.
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ic78d4c949e0c8d57707f1d7f213c3b2fabd4b689
    Reviewed-on: https://chromium-review.googlesource.com/959446
    Commit-Queue: Rintaro Kuroiwa <rkuroiwa@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542840}

--- media/mojo/services/gpu_mojo_media_client.cc
+++ media/mojo/services/gpu_mojo_media_client.cc
@@ -32,9 +32,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif  // defined(OS_ANDROID)
 
 // OS_WIN guards are needed for cross-compiling on linux.
-#if defined(OS_WIN) && BUILDFLAG(ENABLE_D3D11_VIDEO_DECODER)
+#if defined(OS_WIN)
 #include "media/gpu/windows/d3d11_video_decoder.h"
-#endif  // BUILDFLAG(ENABLE_D3D11_VIDEO_DECODER)
+#endif  // defined(OS_WIN)
 
 namespace media {
 
@@ -62,8 +62,7 @@ std::unique_ptr<MediaDrmStorage> CreateMediaDrmStorage(
 }
 #endif  // defined(OS_ANDROID)
 
-#if defined(OS_ANDROID) || \
-    (defined(OS_WIN) && BUILDFLAG(ENABLE_D3D11_VIDEO_DECODER))
+#if defined(OS_ANDROID) || defined(OS_WIN)
 gpu::CommandBufferStub* GetCommandBufferStub(
     base::WeakPtr<MediaGpuChannelManager> media_gpu_channel_manager,
     base::UnguessableToken channel_token,
@@ -78,7 +77,7 @@ gpu::CommandBufferStub* GetCommandBufferStub(
 
   return channel->LookupCommandBuffer(route_id);
 }
-#endif  // OS_ANDROID || (OS_WIN && BUILDFLAG(ENABLE_D3D11_VIDEO_DECODER))
+#endif  // OS_ANDROID || OS_WIN
 
 }  // namespace
 
@@ -124,7 +123,7 @@ std::unique_ptr<VideoDecoder> GpuMojoMediaClient::CreateVideoDecoder(
       android_overlay_factory_cb_, std::move(request_overlay_info_cb),
       std::make_unique<VideoFrameFactoryImpl>(gpu_task_runner_,
                                               std::move(get_stub_cb)));
-#elif defined(OS_WIN) && BUILDFLAG(ENABLE_D3D11_VIDEO_DECODER)
+#elif defined(OS_WIN)
   return std::make_unique<D3D11VideoDecoder>(
       gpu_task_runner_,
       base::BindRepeating(&GetCommandBufferStub, media_gpu_channel_manager_,
