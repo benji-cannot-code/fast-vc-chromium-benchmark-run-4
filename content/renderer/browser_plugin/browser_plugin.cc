cb8ef548a34edcf4288771a92bf0885f0405d367
Saman Sami <samans@chromium.org>
2018-03-13 20:03:49 +0000

    Fix crash in content::BrowserPlugin::Destroy
    We thought embedding_render_widget_ would outlive the BrowserPlugin, but
    apparently not, so use weak pointers to ensure we don't access
    embedding_render_widget_ once it's gone.
    
    Bug: 812966
    Change-Id: I7f374a63bd34db3cb616fa961d7c504570c5ef2d
    Reviewed-on: https://chromium-review.googlesource.com/959239
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Saman Sami <samans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542890}

--- content/renderer/browser_plugin/browser_plugin.cc
+++ content/renderer/browser_plugin/browser_plugin.cc
@@ -390,7 +390,7 @@ gfx::Rect BrowserPlugin::FrameRectInPixels() const {
 }
 
 float BrowserPlugin::GetDeviceScaleFactor() const {
-  return embedding_render_widget_->GetOriginalScreenInfo().device_scale_factor;
+  return pending_resize_params_.screen_info.device_scale_factor;
 }
 
 void BrowserPlugin::UpdateInternalInstanceId() {
@@ -461,9 +461,8 @@ bool BrowserPlugin::Initialize(WebPluginContainer* container) {
 
   embedding_render_widget_ =
       RenderFrameImpl::FromWebFrame(container_->GetDocument().GetFrame())
-          ->GetRenderWidget();
-  pending_resize_params_.screen_info =
-      embedding_render_widget_->GetOriginalScreenInfo();
+          ->GetRenderWidget()
+          ->AsWeakPtr();
   embedding_render_widget_->RegisterBrowserPlugin(this);
 
   return true;
@@ -526,6 +525,12 @@ void BrowserPlugin::UpdateGeometry(const WebRect& plugin_rect_in_viewport,
                                    const WebRect& clip_rect,
                                    const WebRect& unobscured_rect,
                                    bool is_visible) {
+  // Ignore this call during teardown. If the embedding RenderWidget is gone,
+  // don't bother sending new geometry to the child because it's not being shown
+  // anymore.
+  if (!embedding_render_widget_)
+    return;
+
   // Convert the plugin_rect_in_viewport to window coordinates, which is css.
   WebRect rect_in_css(plugin_rect_in_viewport);
 
@@ -542,8 +547,6 @@ void BrowserPlugin::UpdateGeometry(const WebRect& plugin_rect_in_viewport,
   }
 
   pending_resize_params_.frame_rect = frame_rect;
-  pending_resize_params_.screen_info =
-      embedding_render_widget_->GetOriginalScreenInfo();
   if (guest_crashed_) {
     // Update the sad page to match the current ScreenInfo.
     compositing_helper_->ChildFrameGone(frame_rect.size(),
