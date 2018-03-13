7240988425389ce5edb78932f84bca64d2f44b36
Bailey Berro <baileyberro@chromium.org>
2018-03-13 23:48:31 +0000

    Use SupportsWeakPtr<SmbProviderClient>
    This change switches SmbProviderClient to using base::SupportsWeakPtr
    and SmbProviderClientImpl to using a helper class that wraps it
    instead of a manually implemented WeakPtrFactory.
    
    Bug: chromium:757625
    Change-Id: I18f3b04c18ba28bdf8bf9c356b3ebeb98d546cd3
    Reviewed-on: https://chromium-review.googlesource.com/956417
    Commit-Queue: Bailey Berro <baileyberro@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542951}

--- chromeos/dbus/smb_provider_client.h
+++ chromeos/dbus/smb_provider_client.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/files/file_path.h"
 #include "base/files/scoped_file.h"
+#include "base/memory/weak_ptr.h"
 #include "chromeos/chromeos_export.h"
 #include "chromeos/dbus/dbus_client.h"
 #include "chromeos/dbus/smbprovider/directory_entry.pb.h"
@@ -20,7 +21,9 @@ namespace chromeos {
 // SmbProviderClient is used to communicate with the org.chromium.SmbProvider
 // service. All methods should be called from the origin thread (UI thread)
 // which initializes the DBusThreadManager instance.
-class CHROMEOS_EXPORT SmbProviderClient : public DBusClient {
+class CHROMEOS_EXPORT SmbProviderClient
+    : public DBusClient,
+      public base::SupportsWeakPtr<SmbProviderClient> {
  public:
   using GetMetdataEntryCallback =
       base::OnceCallback<void(smbprovider::ErrorType error,
