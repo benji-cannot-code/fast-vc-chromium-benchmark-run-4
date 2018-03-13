ddbd83899c50005dc49789a1965bd2f2e3b20b5b
Albert Chaulk <achaulk@google.com>
2018-03-13 18:26:31 +0000

    [Chromecast] Change CastExtensionSystem to use ExtensionRegistrar
    app_shell has updated to move a lot of load and init code into
    ExtensionRegistrar
    
    Change-Id: Iaf8b1c4c50dd0a526f7dfc7a0a8aba759f55fe6c
    Reviewed-on: https://chromium-review.googlesource.com/941548
    Commit-Queue: Albert Chaulk <achaulk@chromium.org>
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Reviewed-by: Kevin Schoedel <kpschoedel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542863}

--- chromecast/browser/extensions/cast_extension_system.h
+++ chromecast/browser/extensions/cast_extension_system.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
+#include "extensions/browser/extension_registrar.h"
 #include "extensions/browser/extension_system.h"
 #include "extensions/common/one_shot_event.h"
 
@@ -28,7 +29,8 @@ class ValueStoreFactory;
 
 // A simplified version of ExtensionSystem for cast_shell. Allows
 // cast_shell to skip initialization of services it doesn't need.
-class CastExtensionSystem : public ExtensionSystem {
+class CastExtensionSystem : public ExtensionSystem,
+                            public ExtensionRegistrar::Delegate {
  public:
   explicit CastExtensionSystem(content::BrowserContext* browser_context);
   ~CastExtensionSystem() override;
@@ -86,6 +88,20 @@ class CastExtensionSystem : public ExtensionSystem {
   bool FinishDelayedInstallationIfReady(const std::string& extension_id,
                                         bool install_immediately) override;
 
+  // ExtensionRegistrar::Delegate implementation:
+  void PreAddExtension(const Extension* extension,
+                       const Extension* old_extension) override;
+  void PostActivateExtension(scoped_refptr<const Extension> extension) override;
+  void PostDeactivateExtension(
+      scoped_refptr<const Extension> extension) override;
+  void LoadExtensionForReload(
+      const ExtensionId& extension_id,
+      const base::FilePath& path,
+      ExtensionRegistrar::LoadErrorBehavior load_error_behavior) override;
+  bool CanEnableExtension(const Extension* extension) override;
+  bool CanDisableExtension(const Extension* extension) override;
+  bool ShouldBlockExtension(const Extension* extension) override;
+
  private:
   void OnExtensionRegisteredWithRequestContexts(
       scoped_refptr<Extension> extension);
@@ -99,6 +115,7 @@ class CastExtensionSystem : public ExtensionSystem {
   std::unique_ptr<QuotaService> quota_service_;
   std::unique_ptr<AppSorting> app_sorting_;
   std::unique_ptr<SharedUserScriptMaster> shared_user_script_master_;
+  std::unique_ptr<ExtensionRegistrar> extension_registrar_;
 
   scoped_refptr<ValueStoreFactory> store_factory_;
 
