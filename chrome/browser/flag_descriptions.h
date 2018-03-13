8f867ffd1e83fbd1e11b927c4ea69f358bab1b0a
Ian Clelland <iclelland@chromium.org>
2018-03-13 16:09:57 +0000

    Remove the browser feature for VibrateWithoutUserGesture
    The renderer feature was removed in https://crrev.com/86dad9f3, except
    for the linkage between browser and renderer in c/c/runtime_features.cc.
    This CL removes that link, as well as the base::Feature for the removed
    feature, and the entry in chrome://flags.
    
    Bug: 704650
    Change-Id: Iddf4038c7c2f4c8d7a45770262b977d78996b696
    Reviewed-on: https://chromium-review.googlesource.com/957478
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Ian Clelland <iclelland@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542816}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1003,9 +1003,6 @@ extern const char kV8ContextSnapshotDescription[];
 extern const char kV8VmFutureName[];
 extern const char kV8VmFutureDescription[];
 
-extern const char kVibrateRequiresUserGestureName[];
-extern const char kVibrateRequiresUserGestureDescription[];
-
 extern const char kVideoFullscreenOrientationLockName[];
 extern const char kVideoFullscreenOrientationLockDescription[];
 
