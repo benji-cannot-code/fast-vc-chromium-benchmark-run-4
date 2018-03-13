10b55652f0a20d9c0061d463244a9c4a9619b74a
chaopeng <chaopeng@chromium.org>
2018-03-13 03:35:03 +0000

    Pinch zoom support for Windows Precision Touchpad
    In this patch, we covert the scale factor from Direct Manipulation
    to GesturePinch event. The ctrl+wheel will create by WebViewImpl::
    HandleSyntheticWheelFromTouchpadPinchEvent.
    
    Bug: 647140
    Change-Id: Ib041d7805daac7e71b2f43872c818a9f36b14abd
    Reviewed-on: https://chromium-review.googlesource.com/786348
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Jianpeng Chao <chaopeng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542719}

--- ui/base/win/direct_manipulation.h
+++ ui/base/win/direct_manipulation.h
@@ -53,6 +53,10 @@ class DirectManipulationHandler
   DirectManipulationHandler();
   ~DirectManipulationHandler() override;
 
+  enum class Gesture { kNone, kScroll, kPinch };
+
+  void TransitionToState(Gesture gesture);
+
   HRESULT STDMETHODCALLTYPE
   OnViewportStatusChanged(_In_ IDirectManipulationViewport* viewport,
                           _In_ DIRECTMANIPULATION_STATUS current,
@@ -71,6 +75,9 @@ class DirectManipulationHandler
   int last_x_offset_ = 0;
   int last_y_offset_ = 0;
   bool first_ready_ = false;
+
+  // Current recognized gesture from Direct Manipulation.
+  Gesture gesture_state_ = Gesture::kNone;
 
   DISALLOW_COPY_AND_ASSIGN(DirectManipulationHandler);
 };
