f5ebfbf0d9e7c905f4784e24c6049b391ff0a3ea
Derek Cheng <imcheng@chromium.org>
2018-03-13 00:10:59 +0000

    [Cast MRP] Fix icon type of sinks created by the browser.
    Currently the icon type is always hard coded to CAST, but it should be
    determined using the Cast device capabilities. This patch implements
    that logic so that with the CastMediaRouteProvider feature enabled,
    the right icon type will be displayed on the device list.
    
    Note this does not impact the MR extension since it has its own logic
    to determine icon type.
    
    Bug: 698940
    Change-Id: I6afa8eb9db1553fabaf2a8ee617812edb354ae8c
    Reviewed-on: https://chromium-review.googlesource.com/956538
    Commit-Queue: Derek Cheng <imcheng@chromium.org>
    Reviewed-by: Bin Zhao <zhaobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542660}

--- chrome/browser/media/router/media_router.h
+++ chrome/browser/media/router/media_router.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "chrome/browser/media/cast_remoting_connector.h"
 #include "chrome/browser/media/router/route_message_observer.h"
-#include "chrome/common/media_router/discovery/media_sink_internal.h"
 #include "chrome/common/media_router/media_route.h"
 #include "chrome/common/media_router/media_sink.h"
 #include "chrome/common/media_router/media_source.h"
