92a8e40b3cc21c46cb12c3253a018ed43078e4f3
Jialiu Lin <jialiul@chromium.org>
2018-03-13 10:25:13 +0000

    Revert "Add Finch feature to gate Enterprise Password Protection"
    This reverts commit 063960e4ca4cd28221fa2d63ee11c9b8726dd739.
    
    Reason for revert: Breaking waterfall builds
    
    Original change's description:
    > Add Finch feature to gate Enterprise Password Protection
    > 
    > Also added finch experiment to fieldtrial_testing_config.json
    > 
    > Bug: 804490,818559
    > Change-Id: I70b0bfdaf7d59ca91f27d634363221b5f3cdb403
    > Reviewed-on: https://chromium-review.googlesource.com/942308
    > Commit-Queue: Jialiu Lin <jialiul@chromium.org>
    > Reviewed-by: Luke Z <lpz@chromium.org>
    > Reviewed-by: Gayane Petrosyan <gayane@chromium.org>
    > Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#542502}
    
    TBR=gayane@chromium.org,dvadym@chromium.org,jialiul@chromium.org,lpz@chromium.org
    
    Change-Id: Iaf5149ca999a951775bdd4b90282e31ee8507469
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 804490, 818559
    Reviewed-on: https://chromium-review.googlesource.com/960201
    Reviewed-by: Jialiu Lin <jialiul@chromium.org>
    Commit-Queue: Jialiu Lin <jialiul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542760}

--- components/safe_browsing/common/safe_browsing_prefs.cc
+++ components/safe_browsing/common/safe_browsing_prefs.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/pref_service.h"
-#include "components/safe_browsing/features.h"
 #include "content/public/browser/browser_thread.h"
 #include "net/base/url_util.h"
 #include "url/gurl.h"
@@ -505,11 +504,9 @@ base::ListValue GetSafeBrowsingPreferencesList(PrefService* prefs) {
 void GetSafeBrowsingWhitelistDomainsPref(
     const PrefService& prefs,
     std::vector<std::string>* out_canonicalized_domain_list) {
-  if (base::FeatureList::IsEnabled(kEnterprisePasswordProtectionV1)) {
-    const base::ListValue* pref_value =
-        prefs.GetList(prefs::kSafeBrowsingWhitelistDomains);
-    CanonicalizeDomainList(*pref_value, out_canonicalized_domain_list);
-  }
+  const base::ListValue* pref_value =
+      prefs.GetList(prefs::kSafeBrowsingWhitelistDomains);
+  CanonicalizeDomainList(*pref_value, out_canonicalized_domain_list);
 }
 
 void CanonicalizeDomainList(
@@ -530,10 +527,8 @@ void CanonicalizeDomainList(
 bool IsURLWhitelistedByPolicy(const GURL& url,
                               StringListPrefMember* pref_member) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::IO);
-  if (!pref_member ||
-      !base::FeatureList::IsEnabled(kEnterprisePasswordProtectionV1)) {
+  if (!pref_member)
     return false;
-  }
   std::vector<std::string> sb_whitelist_domains = pref_member->GetValue();
   return std::find_if(sb_whitelist_domains.begin(), sb_whitelist_domains.end(),
                       [&url](const std::string& domain) {
@@ -543,9 +538,6 @@ bool IsURLWhitelistedByPolicy(const GURL& url,
 
 bool IsURLWhitelistedByPolicy(const GURL& url, const PrefService& pref) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  if (!base::FeatureList::IsEnabled(kEnterprisePasswordProtectionV1))
-    return false;
-
   if (!pref.HasPrefPath(prefs::kSafeBrowsingWhitelistDomains))
     return false;
   const base::ListValue* whitelist =
@@ -572,10 +564,8 @@ void GetPasswordProtectionLoginURLsPref(const PrefService& prefs,
 
 bool MatchesPasswordProtectionLoginURL(const GURL& url,
                                        const PrefService& prefs) {
-  if (!base::FeatureList::IsEnabled(kEnterprisePasswordProtectionV1) ||
-      !url.is_valid()) {
+  if (!url.is_valid())
     return false;
-  }
 
   std::vector<GURL> login_urls;
   GetPasswordProtectionLoginURLsPref(prefs, &login_urls);
@@ -607,10 +597,8 @@ GURL GetPasswordProtectionChangePasswordURLPref(const PrefService& prefs) {
 
 bool MatchesPasswordProtectionChangePasswordURL(const GURL& url,
                                                 const PrefService& prefs) {
-  if (!base::FeatureList::IsEnabled(kEnterprisePasswordProtectionV1) ||
-      !url.is_valid()) {
+  if (!url.is_valid())
     return false;
-  }
 
   GURL change_password_url = GetPasswordProtectionChangePasswordURLPref(prefs);
   if (change_password_url.is_empty())
