95e4d43a4348cc5634a70b5594f3a8559f2c4a08
tzik <tzik@chromium.org>
2018-03-13 09:13:48 +0000

    Migrate LaunchUnitTests callers to OnceCallback
    This updates callers of base::LaunchUnitTests in several directory
    to use base::OnceCallback instead of legacy base::Callback.
    No intended functional change is included.
    
    This reduces the number of 'base::Bind' in //gin, //notification_helper,
    //pdf, //sql, //url, and //chrome_elf from 7 to 0 as tracked at
    http://goo.gl/LUVhDj
    
    Bug: 714018
    Change-Id: Ie1bf3ad1ca90f732356d4612cfd8e3e8eca8afda
    Reviewed-on: https://chromium-review.googlesource.com/954882
    Commit-Queue: Taiju Tsuiki <tzik@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542743}

--- sql/test/run_all_unittests.cc
+++ sql/test/run_all_unittests.cc
@@ -11,7 +11,6 @@ int main(int argc, char** argv) {
   sql::SQLTestSuite test_suite(argc, argv);
 
   return base::LaunchUnitTests(
-      argc,
-      argv,
-      base::Bind(&sql::SQLTestSuite::Run, base::Unretained(&test_suite)));
+      argc, argv,
+      base::BindOnce(&sql::SQLTestSuite::Run, base::Unretained(&test_suite)));
 }
