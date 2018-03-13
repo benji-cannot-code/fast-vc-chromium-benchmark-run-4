fd0c3665379a985039912745ff7f61c8034b4933
Nicolás Peña <npm@chromium.org>
2018-03-13 01:48:47 +0000

    Move UpdateOpenTabsURLs() to UserActivityLogger
    This CL moves UpdateOpenTabsURLs() from the UserActivityLoggerDelegate to the
    UserActivityLogger. This is done in preparation for adding a predictor which
    will also use some of the URL features as well as other features calculated by
    the UserActivityLogger. This predictor will be called from UserActivityLogger.
    In a followup, some of these classes will be renamed.
    
    To allow testing UpdateOpenTabsURLs() in the UserActivityLoggerTest instead of
    UserActivityLoggerDelegateUkmTest, the unittests are refactored.
    
    Design doc:
    https://docs.google.com/document/d/1QXXnRFKiRDvXaxwNTc6djMDC-5e7SXk4i2FI3fpxMdA/edit#heading=h.gjgriz8s7dlc
    
    Change-Id: Icd8515437f0524bf56ad5d4c537814773aa247d7
    Reviewed-on: https://chromium-review.googlesource.com/950602
    Commit-Queue: Nicolás Peña Moreno <npm@chromium.org>
    Reviewed-by: Jia Meng <jiameng@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542689}

--- chrome/browser/chromeos/power/ml/user_activity_logger_delegate.h
+++ chrome/browser/chromeos/power/ml/user_activity_logger_delegate.h
@@ -6,10 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_CHROMEOS_POWER_ML_USER_ACTIVITY_LOGGER_DELEGATE_H_
 #define CHROME_BROWSER_CHROMEOS_POWER_ML_USER_ACTIVITY_LOGGER_DELEGATE_H_
 
+#include <map>
+
+#include "services/metrics/public/cpp/ukm_source_id.h"
+
 namespace chromeos {
 namespace power {
 namespace ml {
 
+struct TabProperty;
 class UserActivityEvent;
 
 // Interface to log UserActivityEvent to UKM.
@@ -17,13 +22,10 @@ class UserActivityLoggerDelegate {
  public:
   virtual ~UserActivityLoggerDelegate() = default;
 
-  // Get source IDs of all open tabs' URLs. This method should be called when
-  // user activity logger runs feature extraction to collect source IDs of all
-  // open tabs' URLs.
-  virtual void UpdateOpenTabsURLs() = 0;
-
-  // Log user activity event.
-  virtual void LogActivity(const UserActivityEvent& event) = 0;
+  // Log user activity event using URL data.
+  virtual void LogActivity(
+      const UserActivityEvent& event,
+      const std::map<ukm::SourceId, TabProperty>& open_tabs) = 0;
 };
 
 }  // namespace ml
