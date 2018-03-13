3e0d6318548b3f44f2295875efed25aa0e62cbdb
Carlos IL <carlosil@chromium.org>
2018-03-13 17:54:46 +0000

    Added mojo interface for supervised user commands.
    Added mojo interface to handle interstitial commands and implemented it
    in SupervisedUserNavigationObserver which passes the commands to
    SupervisedUserInterstitial when committed interstitials are enabled.
    
    In a future CL an extra class will be added which will listen to JS
    commands from the interstitial and passes them via mojo to the
    observer.
    
    Bug: 817099
    Change-Id: I5285e1fdaa16ee4e96ec7167d470b8ada9b73685
    Reviewed-on: https://chromium-review.googlesource.com/950413
    Commit-Queue: Carlos IL <carlosil@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542848}

--- chrome/browser/supervised_user/supervised_user_navigation_observer.h
+++ chrome/browser/supervised_user/supervised_user_navigation_observer.h
@@ -13,8 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/supervised_user/supervised_user_service_observer.h"
 #include "chrome/browser/supervised_user/supervised_user_url_filter.h"
 #include "chrome/browser/supervised_user/supervised_users.h"
+#include "chrome/common/supervised_user_commands.mojom.h"
 #include "components/sessions/core/serialized_navigation_entry.h"
 #include "components/supervised_user_error_page/supervised_user_error_page.h"
+#include "content/public/browser/web_contents_binding_set.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
 
@@ -29,7 +31,8 @@ class WebContents;
 class SupervisedUserNavigationObserver
     : public content::WebContentsUserData<SupervisedUserNavigationObserver>,
       public content::WebContentsObserver,
-      public SupervisedUserServiceObserver {
+      public SupervisedUserServiceObserver,
+      public supervised_user::mojom::SupervisedUserCommands {
  public:
   ~SupervisedUserNavigationObserver() override;
 
@@ -43,6 +46,7 @@ class SupervisedUserNavigationObserver
       content::WebContents* web_contents,
       const GURL& url,
       supervised_user_error_page::FilteringBehaviorReason reason,
+      int64_t navigation_id,
       const base::Callback<
           void(SupervisedUserNavigationThrottle::CallbackActions)>& callback);
 
@@ -61,6 +65,7 @@ class SupervisedUserNavigationObserver
   void OnRequestBlockedInternal(
       const GURL& url,
       supervised_user_error_page::FilteringBehaviorReason reason,
+      int64_t navigation_id,
       const base::Callback<
           void(SupervisedUserNavigationThrottle::CallbackActions)>& callback);
 
@@ -74,6 +79,7 @@ class SupervisedUserNavigationObserver
       const GURL& url,
       supervised_user_error_page::FilteringBehaviorReason reason,
       bool initial_page_load,
+      int64_t navigation_id,
       const base::Callback<
           void(SupervisedUserNavigationThrottle::CallbackActions)>& callback);
 
@@ -82,11 +88,21 @@ class SupervisedUserNavigationObserver
           void(SupervisedUserNavigationThrottle::CallbackActions)>& callback,
       bool result);
 
+  // supervised_user::mojom::SupervisedUserCommands implementation. Should not
+  // be called when an interstitial is no longer showing. This should be
+  // enforced by the mojo caller.
+  void GoBack() override;
+  void RequestPermission() override;
+  void Feedback() override;
+
   // Owned by SupervisedUserService.
   const SupervisedUserURLFilter* url_filter_;
 
   // Owned by SupervisedUserServiceFactory (lifetime of Profile).
   SupervisedUserService* supervised_user_service_;
+
+  // Navigation ID of the navigation that triggered the last interstitial.
+  int64_t interstitial_navigation_id_;
 
   bool is_showing_interstitial_ = false;
 
@@ -94,6 +110,10 @@ class SupervisedUserNavigationObserver
       blocked_navigations_;
 
   std::unique_ptr<SupervisedUserInterstitial> interstitial_;
+
+  content::WebContentsFrameBindingSet<
+      supervised_user::mojom::SupervisedUserCommands>
+      binding_;
 
   base::WeakPtrFactory<SupervisedUserNavigationObserver> weak_ptr_factory_;
 
