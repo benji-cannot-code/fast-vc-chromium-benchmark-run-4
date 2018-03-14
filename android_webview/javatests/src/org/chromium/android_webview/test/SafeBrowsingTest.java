b203fec3e716f2c90b5a1bf6d81d5a59811eb840
Caleb Rouleau <crouleau@chromium.org>
2018-03-14 20:18:47 +0000

    Revert "Disable flaky testSafeBrowsingOnSafeBrowsingHitProceed"
    This reverts commit cf978e37558334aa1451b38442b500b0a73e470e.
    
    Reason for revert: not more flaky than the other ones. https://bugs.chromium.org/p/chromium/issues/detail?id=821697#c17
    
    Original change's description:
    > Disable flaky testSafeBrowsingOnSafeBrowsingHitProceed
    > 
    > TBR=ntfschr
    > 
    > Bug: 821697
    > Change-Id: I61515e735a542cf94dcb00c50263ea512d66fcea
    > Reviewed-on: https://chromium-review.googlesource.com/963081
    > Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    > Commit-Queue: Caleb Rouleau <crouleau@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#543146}
    
    TBR=crouleau@chromium.org,ntfschr@chromium.org
    
    Change-Id: I141561d5448a77c0dc12a91032850c9dd2dc2e1c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 821697
    Reviewed-on: https://chromium-review.googlesource.com/963230
    Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    Commit-Queue: Caleb Rouleau <crouleau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543180}

--- android_webview/javatests/src/org/chromium/android_webview/test/SafeBrowsingTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/SafeBrowsingTest.java
@@ -779,7 +779,6 @@ public class SafeBrowsingTest {
     @Test
     @SmallTest
     @Feature({"AndroidWebView"})
-    @DisabledTest
     public void testSafeBrowsingOnSafeBrowsingHitProceed() throws Throwable {
         mContentsClient.setSafeBrowsingAction(SafeBrowsingAction.PROCEED);
 
