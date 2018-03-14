9334d5d20db1fd33f5738d4da1e144bb63d9bc20
Sammie Quon <sammiequon@google.com>
2018-03-14 18:41:45 +0000

    cros: Convert switch to feature for new overview ui.
    Convert to feature which is enabled by default.
    
    Test: manual
    Bug: 821608
    Change-Id: If513c4d2a6bdfd4197525d7922d99cfc01f4c616
    Reviewed-on: https://chromium-review.googlesource.com/961721
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543139}

--- chrome/browser/ui/views/frame/browser_non_client_frame_view_ash.cc
+++ chrome/browser/ui/views/frame/browser_non_client_frame_view_ash.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/frame/frame_header_origin_text.h"
 #include "ash/frame/frame_header_util.h"
 #include "ash/public/cpp/app_types.h"
+#include "ash/public/cpp/ash_features.h"
 #include "ash/public/cpp/ash_switches.h"
 #include "ash/public/cpp/window_properties.h"
 #include "ash/public/interfaces/constants.mojom.h"
@@ -24,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/overview/window_selector_controller.h"
 #include "ash/wm/window_util.h"
 #include "base/command_line.h"
+#include "base/feature_list.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/task_runner.h"
 #include "base/threading/sequenced_task_runner_handle.h"
@@ -443,10 +445,8 @@ void BrowserNonClientFrameViewAsh::OnOverviewModeStarting() {
 
   // Update the window icon so that overview mode can grab the icon from
   // aura::client::kWindowIcon to display.
-  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
-          ash::switches::kAshEnableNewOverviewUi)) {
+  if (base::FeatureList::IsEnabled(ash::features::kNewOverviewUi))
     frame()->UpdateWindowIcon();
-  }
 
   frame()->GetNativeWindow()->SetProperty(aura::client::kTopViewColor,
                                           GetFrameColor());
