118b1b93fa6ed56d5c5a017a8aa12698d3c66a74
Tatsuhisa Yamaguchi <yamaguchi@google.com>
2018-03-14 11:10:42 +0000

    Zip Archiver: Stop persisting mount info.
    Make Zip Archiver to forget password when requested to resume, as well
    as returning error to make VolumeManager remove the persisted provided
    volume info.
    Remove all code for persisting zip mount state.
    
    Bug: 789073,803752
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Icb9a6e9ca04b7ed633480a17973d1dcb126d02e0
    Reviewed-on: https://chromium-review.googlesource.com/940742
    Commit-Queue: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Reviewed-by: Yuki Awano <yawano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543048}

--- chrome/browser/resources/chromeos/zip_archiver/js/main.js
+++ chrome/browser/resources/chromeos/zip_archiver/js/main.js
@@ -9,6 +9,7 @@ function init() {
       loadTimeData.data = strings;
       i18nTemplate.process(document, loadTimeData);
     });
+    backgroundPage.unpacker.app.cleanupOldStorageInfo();
   });
 }
 
