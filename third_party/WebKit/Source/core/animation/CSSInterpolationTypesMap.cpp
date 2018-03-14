bbff1c1c957d9972e7a87ff6ae0651fd5cf404ad
Alper Cakan <alpercakan98@gmail.com>
2018-03-14 11:30:19 +0000

    make text-size-adjust animatable
    Make text-size-adjust animatable, as it is supposed be so according to:
    drafts.csswg.org/css-size-adjust/#adjustment-control
    
    R=nainar@chromium.org, shend@chromium.org, smcgruer@chromium.org
    
    Bug: 818526
    Change-Id: I0c3b98c4726f6898599aa5626ad2fec876057ec0
    Reviewed-on: https://chromium-review.googlesource.com/956462
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543050}

--- third_party/WebKit/Source/core/animation/CSSInterpolationTypesMap.cpp
+++ third_party/WebKit/Source/core/animation/CSSInterpolationTypesMap.cpp
@@ -163,6 +163,7 @@ const InterpolationTypes& CSSInterpolationTypesMap::Get(
     case CSSPropertyStrokeMiterlimit:
     case CSSPropertyStrokeOpacity:
     case CSSPropertyColumnCount:
+    case CSSPropertyTextSizeAdjust:
     case CSSPropertyWidows:
     case CSSPropertyZIndex:
       applicable_types->push_back(
