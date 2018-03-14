6302a0034f2075ee6995b079d3c76a8e8d702e42
Toni Barzic <tbarzic@chromium.org>
2018-03-14 17:23:08 +0000

    Show a notification the detachable base needs an update
    If hammerd detects that the attached detachable base needs a frimware
    update (which is applied on device boot), show the user a system
    notification asking them to reboot the device in order to apply the
    update.
    The notification is closed if the update succeeds, or if the detachable
    base gets detached (i.e. the Chrome OS device switches to tablet mode).
    
    Note that, unlike detachable base switch notification, this notification
    should be shown on login and lock screen (because login and lock UI do
    not display error bubbles in this case), and is thus set as a system
    priority notification.
    
    BUG=765416
    
    Change-Id: Ie8d5d30985aab4d0d85e3b35ebb147d212f92658
    Reviewed-on: https://chromium-review.googlesource.com/957468
    Commit-Queue: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543113}

--- ash/detachable_base/detachable_base_handler.cc
+++ ash/detachable_base/detachable_base_handler.cc
@@ -148,11 +148,15 @@ void DetachableBaseHandler::OnLocalStatePrefServiceInitialized(
     NotifyPairingStatusChanged();
 }
 
-void DetachableBaseHandler::BaseFirmwareUpdateNeeded() {}
+void DetachableBaseHandler::BaseFirmwareUpdateNeeded() {
+  NotifyBaseRequiresFirmwareUpdate(true /*requires_update*/);
+}
 
 void DetachableBaseHandler::BaseFirmwareUpdateStarted() {}
 
-void DetachableBaseHandler::BaseFirmwareUpdateSucceeded() {}
+void DetachableBaseHandler::BaseFirmwareUpdateSucceeded() {
+  NotifyBaseRequiresFirmwareUpdate(false /*requires_update*/);
+}
 
 void DetachableBaseHandler::BaseFirmwareUpdateFailed() {}
 
@@ -212,6 +216,7 @@ void DetachableBaseHandler::UpdateTabletMode(
   if (*tablet_mode_ == chromeos::PowerManagerClient::TabletMode::ON) {
     authenticated_base_id_.clear();
     pairing_status_ = DetachableBasePairingStatus::kNone;
+    NotifyBaseRequiresFirmwareUpdate(false /*requires_update*/);
   }
 
   if (GetPairingStatus() != old_pairing_status)
@@ -244,6 +249,12 @@ DetachableBaseHandler::GetLastUsedDeviceForUser(
 void DetachableBaseHandler::NotifyPairingStatusChanged() {
   for (auto& observer : observers_)
     observer.OnDetachableBasePairingStatusChanged(GetPairingStatus());
+}
+
+void DetachableBaseHandler::NotifyBaseRequiresFirmwareUpdate(
+    bool requires_update) {
+  for (auto& observer : observers_)
+    observer.OnDetachableBaseRequiresUpdateChanged(requires_update);
 }
 
 }  // namespace ash
