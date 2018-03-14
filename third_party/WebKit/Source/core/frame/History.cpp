23d31c728aea2d4f8712f0d10b7b93e269a164ad
Chris Palmer <palmer@chromium.org>
2018-03-14 19:28:31 +0000

    Log a message on the console when the {push,replace}State quota is exceeded.
    Previously, history.{push,replace}State spam failed silently, causing developer
    confusion.
    
    This change also introduces the --disable-pushstate-throttle flag to allow users
    to disable the throttling.
    
    This work was primarily authored by Trent Willis <trentmwillis@gmail.com>.
    
    R=creis@chromium.org, dcheng@chromium.org
    
    Bug: 769592, 794923
    Change-Id: I969456b190d535f8dee3e03b5c4f4f36730d6b38
    Reviewed-on: https://chromium-review.googlesource.com/851000
    Commit-Queue: Chris Palmer <palmer@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543154}

--- third_party/WebKit/Source/core/frame/History.cpp
+++ third_party/WebKit/Source/core/frame/History.cpp
@@ -29,8 +29,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/ExceptionState.h"
 #include "core/dom/Document.h"
 #include "core/dom/ExecutionContext.h"
+#include "core/frame/FrameConsole.h"
 #include "core/frame/LocalFrame.h"
 #include "core/frame/LocalFrameClient.h"
+#include "core/frame/Settings.h"
+#include "core/inspector/ConsoleMessage.h"
 #include "core/loader/DocumentLoader.h"
 #include "core/loader/FrameLoader.h"
 #include "core/loader/HistoryItem.h"
@@ -143,6 +146,9 @@ HistoryScrollRestorationType History::ScrollRestorationInternal() const {
 // need. However, it does somewhat mitigate the immediate concern of |pushState|
 // and |replaceState| DoS (assuming the renderer has not been compromised).
 bool History::ShouldThrottleStateObjectChanges() {
+  if (!GetFrame()->GetSettings()->GetShouldThrottlePushState())
+    return false;
+
   const int kStateUpdateLimit = 50;
 
   if (state_flood_guard.count > kStateUpdateLimit) {
@@ -287,8 +293,21 @@ void History::StateObjectAdded(scoped_refptr<SerializedScriptValue> data,
     return;
   }
 
-  if (ShouldThrottleStateObjectChanges())
+  if (ShouldThrottleStateObjectChanges()) {
+    // TODO(769592): Get an API spec change so that we can throw an exception:
+    //
+    //  exception_state.ThrowDOMException(kQuotaExceededError,
+    //                                    "Throttling history state changes to "
+    //                                    "prevent the browser from hanging.");
+    //
+    // instead of merely warning.
+
+    GetFrame()->Console().AddMessage(
+        ConsoleMessage::Create(kJSMessageSource, kWarningMessageLevel,
+                               "Throttling history state changes to prevent "
+                               "the browser from hanging."));
     return;
+  }
 
   GetFrame()->Loader().UpdateForSameDocumentNavigation(
       full_url, kSameDocumentNavigationHistoryApi, std::move(data),
