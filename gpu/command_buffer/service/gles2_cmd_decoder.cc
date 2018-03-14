d69da8cde8f9008dce1d1c1ca2ec18fb04318620
James Darpinian <jdarpinian@chromium.org>
2018-03-14 04:34:54 +0000

    Fix validation of transform feedback buffer sizes at draw time.
    There were three problems:
    1. We didn't multiply the number of vertices by the number of instances.
    2. We didn't account for vertices written by previous draw calls.
    3. We didn't round the vertex count down to the nearest number of whole
       primitives.
    
    Fixes the WebGL 2 conformance test added here:
    https://github.com/KhronosGroup/WebGL/pull/2604
    
    Bug: 820639
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I9566ad14aa6671eca127a1b7f68b73e6408acd10
    Reviewed-on: https://chromium-review.googlesource.com/957743
    Commit-Queue: James Darpinian <jdarpinian@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543008}

--- gpu/command_buffer/service/gles2_cmd_decoder.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder.cc
@@ -10581,12 +10581,11 @@ error::Error GLES2DecoderImpl::DoDrawArrays(
         return error::kNoError;
       }
       if (!buffer_manager()->RequestBuffersAccess(
-              state_.GetErrorState(),
-              state_.bound_transform_feedback.get(),
+              state_.GetErrorState(), state_.bound_transform_feedback.get(),
               state_.current_program->GetTransformFeedbackVaryingSizes(),
-              count,
-              function_name,
-              "transformfeedback buffers")) {
+              state_.bound_transform_feedback->VerticesNeededForDraw(
+                  mode, count, primcount),
+              function_name, "transformfeedback buffers")) {
         return error::kNoError;
       }
     }
@@ -10634,6 +10633,10 @@ error::Error GLES2DecoderImpl::DoDrawArrays(
         api()->glDrawArraysFn(mode, first, count);
       } else {
         api()->glDrawArraysInstancedANGLEFn(mode, first, count, primcount);
+      }
+      if (state_.bound_transform_feedback.get()) {
+        state_.bound_transform_feedback->OnVerticesDrawn(mode, count,
+                                                         primcount);
       }
       if (textures_set) {
         RestoreStateForTextures();
