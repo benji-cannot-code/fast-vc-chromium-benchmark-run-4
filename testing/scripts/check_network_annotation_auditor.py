f7bb77a142936f644c613e8118ee3cb867e5d74d
Ramin Halavati <rhalavati@chromium.org>
2018-03-14 06:48:40 +0000

    Change Traffic Annotation FYI bot's script.
    The script that traffic annotations FYI bot runs is changed to a new one
    that specifically checks network traffic annotation auditor's health.
    
    Bug: 690323
    Change-Id: Iaa97d5ce27b2b9f9d2c523deae7fda803ffb5079
    Reviewed-on: https://chromium-review.googlesource.com/955688
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543020}

--- testing/scripts/check_network_annotation_auditor.py
+++ testing/scripts/check_network_annotation_auditor.py
@@ -4,7 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""//testing/scripts wrapper for the network traffic annotations checks."""
+"""//testing/scripts wrapper for the network traffic annotation auditor
+checks."""
 
 import json
 import os
@@ -18,7 +19,7 @@ def main_run(args):
   command_line = [
       sys.executable,
       os.path.join(common.SRC_DIR, 'tools', 'traffic_annotation', 'scripts',
-                   'check_annotations.py'),
+                   'traffic_annotation_auditor_tests.py'),
       '--build-path',
       os.path.join(args.paths['checkout'], 'out', args.build_config_fs),
   ]
