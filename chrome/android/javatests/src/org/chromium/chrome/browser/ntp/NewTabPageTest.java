a7e1b4c8e4ca5e3e4547b1b072b0bc04c2c04c41
Theresa <twellington@chromium.org>
2018-03-14 19:06:04 +0000

    Revert "Disable NewTabPageTest#testOpenMostVisitedItemInIncognitoTab"
    This reverts commit bd5fdccdcef0c2b35dcab9d1cd42a1ed290523e4.
    
    Reason for revert: #testRender is still flaky with this test disabled
    
    Original change's description:
    > Disable NewTabPageTest#testOpenMostVisitedItemInIncognitoTab
    > 
    > This test appears to be causing NewTabPageTest#testRender to flake.
    > Spectulatively disabling.
    > 
    > BUG=813589
    > 
    > Change-Id: I0bdd566c14d1e6d3521e00e097e55a9f079e76a1
    > Reviewed-on: https://chromium-review.googlesource.com/954333
    > Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    > Commit-Queue: Theresa <twellington@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#541799}
    
    TBR=bauerb@chromium.org,twellington@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 813589
    Change-Id: Idcace61dcf6b0a98c94fddee83bea9d2563ff1c0
    Reviewed-on: https://chromium-review.googlesource.com/958007
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Commit-Queue: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543148}

--- chrome/android/javatests/src/org/chromium/chrome/browser/ntp/NewTabPageTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ntp/NewTabPageTest.java
@@ -317,7 +317,7 @@ public class NewTabPageTest {
      * Tests opening a most visited item in a new tab.
      */
     @Test
-    @DisabledTest(message = "Flaked on the try bot. http://crbug.com/543138")
+    @DisabledTest // Flaked on the try bot. http://crbug.com/543138
     @SmallTest
     @Feature({"NewTabPage"})
     public void testOpenMostVisitedItemInNewTab() throws InterruptedException {
@@ -329,8 +329,6 @@ public class NewTabPageTest {
      * Tests opening a most visited item in a new incognito tab.
      */
     @Test
-    @DisabledTest(
-            message = "Suspected to be causing #testRender to flake. https://crbug.com/813589.")
     @SmallTest
     @Feature({"NewTabPage"})
     public void testOpenMostVisitedItemInIncognitoTab() throws InterruptedException {
