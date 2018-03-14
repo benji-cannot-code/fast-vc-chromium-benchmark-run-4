c961e377a8d994940731387ca11fd8c580300984
Tom Anderson <thomasanderson@chromium.org>
2018-03-14 20:11:42 +0000

    Fix build with unbundled zlib
    Linux distros like Gentoo have a philosophy of not statically-linking
    dependencies.  The reason is not necessarily to save disk space, but
    because they like to compile with their own flags and patches applied.
    This CL fixes the zlib unbundle for them.
    
    BUG=800977
    
    Change-Id: I129d95a5a4b6c4183666f712609b2e3f0509526e
    Reviewed-on: https://chromium-review.googlesource.com/947464
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Chris Blume <cblume@chromium.org>
    Reviewed-by: Adenilson Cavalcanti <cavalcantii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543178}

--- third_party/zlib/google/compression_utils.cc
+++ third_party/zlib/google/compression_utils.cc
@@ -14,7 +14,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bit_cast.h"
 #include "base/logging.h"
 #include "base/sys_byteorder.h"
+
+#if defined(USE_SYSTEM_ZLIB)
+#include <zlib.h>
+#else
 #include "third_party/zlib/zlib.h"
+#endif
 
 namespace {
 
