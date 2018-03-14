fbee90e6b81bea77f3fa733786eac9d4a69200d1
Helen Li <xunjieli@chromium.org>
2018-03-14 13:27:42 +0000

    Reland: Add TCP socket API to network service
    This CL adds TCP socket API to network service.
    This is partly based on mandoline implementation
    (https://codereview.chromium.org/1873463003/)
    
    Two TCP socket interfaces are added.
    - TCPConnectedSocket
    - TCPServerSocket
    
    Change in reland:
    - adds logging to TCPSocketTest.ReadAndWriteMultiple to investigate
      the test timeout observed in crbug.com/821232
    - Modifies TestServer slightly to own all client connections instead
      of only the latest one.
    
    R=mmenke@chromium.org
    TBR=miu@chromium.org
    TBR=tsepez@chromium.org
    
    Bug: 721401
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I0426a7aeda30cce7a2a1e84afae5208958dc4a10
    Reviewed-on: https://chromium-review.googlesource.com/960250
    Commit-Queue: Helen Li <xunjieli@chromium.org>
    Reviewed-by: Helen Li <xunjieli@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543064}

--- services/network/udp_socket.h
+++ services/network/udp_socket.h
@@ -14,8 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/span.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
-#include "mojo/public/cpp/bindings/binding.h"
-#include "mojo/public/cpp/bindings/interface_request.h"
 #include "net/base/address_family.h"
 #include "net/base/completion_callback.h"
 #include "net/base/ip_endpoint.h"
@@ -27,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace net {
 class IOBuffer;
 class IOBufferWithSize;
+class NetLog;
 }  // namespace net
 
 namespace network {
@@ -71,12 +70,8 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) UDPSocket : public mojom::UDPSocket {
                          const net::CompletionCallback& callback) = 0;
   };
 
-  UDPSocket(mojom::UDPSocketRequest request,
-            mojom::UDPSocketReceiverPtr receiver);
+  UDPSocket(mojom::UDPSocketReceiverPtr receiver, net::NetLog* net_log);
   ~UDPSocket() override;
-
-  // Sets connection error handler.
-  void set_connection_error_handler(base::OnceClosure handler);
 
   // UDPSocket implementation.
   void Connect(const net::IPEndPoint& remote_addr,
@@ -138,6 +133,8 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) UDPSocket : public mojom::UDPSocket {
   void OnRecvFromCompleted(uint32_t buffer_size, int net_result);
   void OnSendToCompleted(int net_result);
 
+  net::NetLog* net_log_;
+
   // Whether a Bind() has been successfully executed.
   bool is_bound_;
 
@@ -166,8 +163,6 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) UDPSocket : public mojom::UDPSocket {
   // The queue owns the PendingSendRequest instances.
   base::circular_deque<std::unique_ptr<PendingSendRequest>>
       pending_send_requests_;
-
-  mojo::Binding<mojom::UDPSocket> binding_;
 
   DISALLOW_COPY_AND_ASSIGN(UDPSocket);
 };
