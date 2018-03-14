2b97210c581eb4430bedf82824f5863ae7673d0e
Benoit Lize <lizeb@chromium.org>
2018-03-14 17:55:01 +0000

    customtabs: Add CustomTabs APIs for detached parallel requests.
    This CL adds the CustomTabs "client" APIs for parallel request:
    - Enable and query parallel request state
    - Get the URL and requested origin from the Intent
    
    Bug: 816837
    Change-Id: I6c5cc86453a941f08f118be6229bf3a2c497d344
    Reviewed-on: https://chromium-review.googlesource.com/962761
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Commit-Queue: Benoit L <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543122}

--- chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/DetachedResourceRequestTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/DetachedResourceRequestTest.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser.customtabs;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.customtabs.CustomTabsService;
 import android.support.customtabs.CustomTabsSessionToken;
 import android.support.test.InstrumentationRegistry;
@@ -35,7 +36,6 @@ import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.util.browser.Features;
 import org.chromium.chrome.test.util.browser.Features.EnableFeatures;
 import org.chromium.content.browser.test.util.JavaScriptUtils;
-import org.chromium.net.GURLUtils;
 import org.chromium.net.test.EmbeddedTestServer;
 
 /** Tests for detached resource requests. */
@@ -51,7 +51,7 @@ public class DetachedResourceRequestTest {
     private EmbeddedTestServer mServer;
 
     private static final String PRIVATE_DATA_DIRECTORY_SUFFIX = "chrome";
-    private static final String ORIGIN = "http://cats.google.com";
+    private static final Uri ORIGIN = Uri.parse("http://cats.google.com");
 
     @Before
     public void setUp() throws Exception {
@@ -84,8 +84,8 @@ public class DetachedResourceRequestTest {
                 () -> { Assert.assertFalse(mConnection.canDoParallelRequest(session, ORIGIN)); });
         ThreadUtils.runOnUiThreadBlocking(() -> {
             String packageName = mContext.getPackageName();
-            OriginVerifier.addVerifiedOriginForPackage(
-                    packageName, new Origin(ORIGIN), CustomTabsService.RELATION_USE_AS_ORIGIN);
+            OriginVerifier.addVerifiedOriginForPackage(packageName, new Origin(ORIGIN.toString()),
+                    CustomTabsService.RELATION_USE_AS_ORIGIN);
             Assert.assertTrue(mConnection.canDoParallelRequest(session, ORIGIN));
         });
     }
@@ -98,12 +98,14 @@ public class DetachedResourceRequestTest {
         ThreadUtils.runOnUiThreadBlocking(() -> {
             Assert.assertFalse("Should not allow android-app:// scheme",
                     mConnection.startParallelRequest(
-                            session, "android-app://this.is.an.android.app", ORIGIN));
+                            session, Uri.parse("android-app://this.is.an.android.app"), ORIGIN));
             Assert.assertFalse("Should not allow an empty URL",
-                    mConnection.startParallelRequest(session, "", ORIGIN));
+                    mConnection.startParallelRequest(session, Uri.parse(""), ORIGIN));
             Assert.assertFalse("Should not allow an arbitrary origin",
-                    mConnection.startParallelRequest(session, "HTTPS://foo.bar", "wrong://origin"));
-            Assert.assertTrue(mConnection.startParallelRequest(session, "HTTP://foo.bar", ORIGIN));
+                    mConnection.startParallelRequest(
+                            session, Uri.parse("HTTPS://foo.bar"), Uri.parse("wrong://origin")));
+            Assert.assertTrue(
+                    mConnection.startParallelRequest(session, Uri.parse("HTTP://foo.bar"), ORIGIN));
         });
     }
 
@@ -123,7 +125,7 @@ public class DetachedResourceRequestTest {
         });
         mServer.start();
 
-        String url = mServer.getURL("/echotitle");
+        Uri url = Uri.parse(mServer.getURL("/echotitle"));
         ThreadUtils.runOnUiThread(
                 () -> Assert.assertTrue(mConnection.startParallelRequest(session, url, ORIGIN)));
         cb.waitForCallback(0, 1);
@@ -135,7 +137,7 @@ public class DetachedResourceRequestTest {
     public void testCanSetCookie() throws Exception {
         CustomTabsSessionToken session = prepareSession();
         mServer = EmbeddedTestServer.createAndStartServer(mContext);
-        final String url = mServer.getURL("/set-cookie?acookie");
+        final Uri url = Uri.parse(mServer.getURL("/set-cookie?acookie"));
         ThreadUtils.runOnUiThreadBlocking(
                 () -> Assert.assertTrue(mConnection.startParallelRequest(session, url, ORIGIN)));
 
@@ -160,7 +162,7 @@ public class DetachedResourceRequestTest {
             Assert.assertFalse(prefs.isBlockThirdPartyCookiesEnabled());
             prefs.setBlockThirdPartyCookiesEnabled(true);
         });
-        final String url = mServer.getURL("/set-cookie?acookie");
+        final Uri url = Uri.parse(mServer.getURL("/set-cookie?acookie"));
         ThreadUtils.runOnUiThreadBlocking(
                 () -> Assert.assertTrue(mConnection.startParallelRequest(session, url, ORIGIN)));
 
@@ -185,9 +187,9 @@ public class DetachedResourceRequestTest {
             Assert.assertFalse(prefs.isBlockThirdPartyCookiesEnabled());
             prefs.setBlockThirdPartyCookiesEnabled(true);
         });
-        final String url = mServer.getURL("/set-cookie?acookie");
-        String origin = GURLUtils.getOrigin(url);
-        CustomTabsSessionToken session = prepareSession(origin);
+        final Uri url = Uri.parse(mServer.getURL("/set-cookie?acookie"));
+        final Uri origin = Uri.parse(new Origin(url).toString());
+        CustomTabsSessionToken session = prepareSession(url);
 
         ThreadUtils.runOnUiThreadBlocking(
                 () -> Assert.assertTrue(mConnection.startParallelRequest(session, url, origin)));
@@ -206,14 +208,14 @@ public class DetachedResourceRequestTest {
         return prepareSession(ORIGIN);
     }
 
-    private CustomTabsSessionToken prepareSession(String origin) throws Exception {
+    private CustomTabsSessionToken prepareSession(Uri origin) throws Exception {
         final CustomTabsSessionToken session =
                 CustomTabsSessionToken.createMockSessionTokenForTesting();
         Assert.assertTrue(mConnection.newSession(session));
         CustomTabsTestUtils.warmUpAndWait();
         ThreadUtils.runOnUiThreadBlocking(() -> {
             OriginVerifier.addVerifiedOriginForPackage(mContext.getPackageName(),
-                    new Origin(origin), CustomTabsService.RELATION_USE_AS_ORIGIN);
+                    new Origin(origin.toString()), CustomTabsService.RELATION_USE_AS_ORIGIN);
             Assert.assertTrue(mConnection.canDoParallelRequest(session, origin));
         });
         return session;
