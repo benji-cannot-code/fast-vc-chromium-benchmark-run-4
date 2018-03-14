76dfb73d44fd9b17387f8d4b293f91c4129d14f3
Stephen Martinis <martiniss@chromium.org>
2018-03-14 00:14:42 +0000

    Rename "WebKit Mac10.11 (retina)" to "WebKit Mac10.12 (retina)"
    src side CL. Build side is https://chromium-review.googlesource.com/c/chromium/tools/build/+/953260
    
    NOTRY=true
    
    Bug: 818524
    Change-Id: I8eb2a70c80720ebaf0810536b3454eacb2d49f38
    Reviewed-on: https://chromium-review.googlesource.com/953210
    Commit-Queue: Stephen Martinis <martiniss@chromium.org>
    Reviewed-by: Robert Ma <robertma@chromium.org>
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Reviewed-by: Ryan Tseng <hinoka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542959}

--- testing/buildbot/manage.py
+++ testing/buildbot/manage.py
@@ -60,7 +60,7 @@ SKIP = {
   'WebKit Mac10.11',
   'WebKit Mac10.12',
   'WebKit Mac10.11 (dbg)',
-  'WebKit Mac10.11 (retina)',
+  'WebKit Mac10.12 (retina)',
   'Chromium Mac10.10 Tests',
   'Chromium Mac10.11 Tests',
 
