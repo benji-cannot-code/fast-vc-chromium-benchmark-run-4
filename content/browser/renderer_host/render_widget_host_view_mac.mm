c6b65a657b9fa2cb7171a5e3ce39aa1df64b14cc
Sidney San Martín <sdy@chromium.org>
2018-03-14 20:02:29 +0000

    Fix dialogs losing focus while using ⌘` to switch windows.
    - If a browser window becomes key while a dialog is up and the web
      content is first responder, give focus to the dialog.
    
    - If the web content *loses* first responder while the dialog is key,
      make the browser window key. This makes sure that clicking the omnibox
      or typing ⌘L, for instance, focuses the browser window.
    
    The final version of this change is incomplete: The separation between
    "focused" ("the web content is focused within its window") and "active" ("the
    web content's window is the active window") is mostly an illusion because it
    only exists on Mac and I kept running into cases where it breaks down (e.g.
    setting "focused" implicitly sets "active" deep inside WebViewImpl). A bunch of
    tests also check that certain functions and IPCs happen in a certain order, so
    fiddling with anything just breaks a bunch of tests in ways that don't map to
    real-world behavior. Now isn't the right time to mess with that, especially in
    Cocoa code, so this leaves one case unfixed:
    
    - If a dialog becomes key some other way, make sure that the web content
      is first responder in its parent window (so that focus returns to the
      web content on dismissing the dialog, and no other views have first
      responder appearance).
    
    I filed https://crbug.com/821931 to track it.
    
    Bug: 658405
    Change-Id: I7ec11568d860e7371f552baa44a0d6b45db6dc22
    Reviewed-on: https://chromium-review.googlesource.com/935424
    Commit-Queue: Sidney San Martín <sdy@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543170}

--- content/browser/renderer_host/render_widget_host_view_mac.mm
+++ content/browser/renderer_host/render_widget_host_view_mac.mm
@@ -2819,7 +2819,9 @@ Class GetRenderWidgetHostViewCocoaClassForTesting() {
 - (void)windowDidBecomeKey:(NSNotification*)notification {
   DCHECK([self window]);
   DCHECK_EQ([self window], [notification object]);
-  if ([[self window] firstResponder] == self)
+  if ([responderDelegate_ respondsToSelector:@selector(windowDidBecomeKey)])
+    [responderDelegate_ windowDidBecomeKey];
+  if ([self window].isKeyWindow && [[self window] firstResponder] == self)
     renderWidgetHostView_->SetActive(true);
 }
 
@@ -2841,6 +2843,8 @@ Class GetRenderWidgetHostViewCocoaClassForTesting() {
 - (BOOL)becomeFirstResponder {
   if (!renderWidgetHostView_->render_widget_host_)
     return NO;
+  if ([responderDelegate_ respondsToSelector:@selector(becomeFirstResponder)])
+    [responderDelegate_ becomeFirstResponder];
 
   renderWidgetHostView_->render_widget_host_->GotFocus();
   renderWidgetHostView_->SetTextInputActive(true);
@@ -2865,6 +2869,8 @@ Class GetRenderWidgetHostViewCocoaClassForTesting() {
 }
 
 - (BOOL)resignFirstResponder {
+  if ([responderDelegate_ respondsToSelector:@selector(resignFirstResponder)])
+    [responderDelegate_ resignFirstResponder];
   renderWidgetHostView_->SetTextInputActive(false);
   if (!renderWidgetHostView_->render_widget_host_)
     return YES;
