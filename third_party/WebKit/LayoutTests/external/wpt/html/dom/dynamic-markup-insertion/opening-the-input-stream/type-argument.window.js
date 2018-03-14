ef92882ec8baaf034fe2d39bc5d844a7cbc13893
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-03-14 14:16:46 +0000

    Import wpt@9cd6f7a49659597a7c0b485a9dadf1e7d60d346a
    Using wpt-import in Chromium 7f9a64d3d178a323ac21f039310bfceb45bc4d6a.
    With Chromium commits locally applied on WPT:
    ad9cc15699 "Add WPT tests for feature policy frame policy"
    
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/13840
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    kojii@chromium.org:
      external/wpt/css/css-text/line-break
    
    TBR=leon.han@intel.com
    
    No-Export: true
    Change-Id: I75a7369414f6594b2fb3bb9cdd0a4a031d13391c
    Reviewed-on: https://chromium-review.googlesource.com/962343
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543074}

---
+++ third_party/WebKit/LayoutTests/external/wpt/html/dom/dynamic-markup-insertion/opening-the-input-stream/type-argument.window.js
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+["replace",
+ "NOBODY",
+ "@ FD ;",
+ "it does not matter, you see \f",
+ "text/plain",
+ "text/xml",
+ "application/octet-stream",
+ "\0"].forEach(type => {
+  async_test(t => {
+    const frame = document.body.appendChild(document.createElement("iframe"));
+    t.add_cleanup(() => frame.remove());
+    frame.contentDocument.open(type);
+    frame.contentDocument.write("<B>heya</b>");
+    frame.contentDocument.close();
+    assert_equals(frame.contentDocument.body.firstChild.localName, "b");
+    assert_equals(frame.contentDocument.body.textContent, "heya");
+    assert_equals(frame.contentDocument.contentType, "text/html");
+    t.done();
+  }, "document.open() with type set to: " + type + " (type argument is supposed to be ignored)");
+});
