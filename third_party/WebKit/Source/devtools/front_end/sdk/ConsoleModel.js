30f95900d55d522eb40fd712f1bf3a668e6fed54
Erik Luo <luoe@chromium.org>
2018-03-14 22:03:08 +0000

    DevTools: show specific note for queryObjects result
    This modifies the 'i' note next to the queryObjects result array to
    describe how objects may not be collected if they are previewed until
    console.clear
    
    Screenshot: https://imgur.com/a/q03rG
    
    Bug: 815263
    Change-Id: I872ca733fd4b3ae3f64487e6c969167c26119c62
    Reviewed-on: https://chromium-review.googlesource.com/961556
    Commit-Queue: Erik Luo <luoe@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543215}

--- third_party/WebKit/Source/devtools/front_end/sdk/ConsoleModel.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/ConsoleModel.js
@@ -245,8 +245,8 @@ SDK.ConsoleModel = class extends Common.Object {
    */
   _queryObjectRequested(runtimeModel, event) {
     const consoleMessage = new SDK.ConsoleMessage(
-        runtimeModel, SDK.ConsoleMessage.MessageSource.ConsoleAPI, SDK.ConsoleMessage.MessageLevel.Info, '', undefined,
-        undefined, undefined, undefined, [event.data.objects]);
+        runtimeModel, SDK.ConsoleMessage.MessageSource.ConsoleAPI, SDK.ConsoleMessage.MessageLevel.Info, '',
+        SDK.ConsoleMessage.MessageType.QueryObjectResult, undefined, undefined, undefined, [event.data.objects]);
     this.addMessage(consoleMessage);
   }
 
@@ -630,7 +630,8 @@ SDK.ConsoleMessage.MessageType = {
   Profile: 'profile',
   ProfileEnd: 'profileEnd',
   Command: 'command',
-  System: 'system'
+  System: 'system',
+  QueryObjectResult: 'queryObjectResult'
 };
 
 /**
