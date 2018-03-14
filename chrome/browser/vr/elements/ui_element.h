6d68309ee2bf68b95e31ecd69975d7551137f3f5
Christopher Grant <cjgrant@chromium.org>
2018-03-14 18:54:07 +0000

    VR: Allow linear layout to target a specified size
    In most cases, linear layouts expand to contain all children. However,
    in some cases, we want elements to fit into a specified space.  In this
    case, one element can now be tagged as allowing sizing by its parent,
    and LinearLayout can be configured to adjust that size according to the
    other elements it needs to fit.
    
    The first concrete example of this is a URL bar, where the text field
    consumes all remaining space after any icons or indicators are added.
    
    BUG=806886
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_vr;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ib9261c3b882f248aec2269a660cad9e9c6d8bbbf
    Reviewed-on: https://chromium-review.googlesource.com/962712
    Commit-Queue: Christopher Grant <cjgrant@chromium.org>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543144}

--- chrome/browser/vr/elements/ui_element.h
+++ chrome/browser/vr/elements/ui_element.h
@@ -379,6 +379,10 @@ class UiElement : public cc::AnimationTarget {
   void RemoveKeyframeModel(int keyframe_model_id);
   bool IsAnimatingProperty(TargetProperty property) const;
 
+  // Recursive method that sizes and lays out element subtrees. This method may
+  // be overridden by elements that have custom layout requirements.
+  virtual bool SizeAndLayOut();
+
   void DoLayOutChildren();
 
   // Handles positioning adjustments for children. This will be overridden by
@@ -454,6 +458,11 @@ class UiElement : public cc::AnimationTarget {
   // Set the sounds that play when an applicable handler is executed.  Elements
   // that override element hover and click methods must manage their own sounds.
   void SetSounds(SoundId hover, SoundId click, AudioDelegate* delegate);
+
+  bool resizable_by_layout() { return resizable_by_layout_; }
+  void set_resizable_by_layout(bool resizable) {
+    resizable_by_layout_ = resizable;
+  }
 
  protected:
   Animation& animation() { return animation_; }
@@ -583,6 +592,9 @@ class UiElement : public cc::AnimationTarget {
   AudioDelegate* audio_delegate_ = nullptr;
   SoundId hover_sound_id_ = kSoundNone;
   SoundId click_sound_id_ = kSoundNone;
+
+  // Indicates that this element may be resized by parent layout elements.
+  bool resizable_by_layout_ = false;
 
   DISALLOW_COPY_AND_ASSIGN(UiElement);
 };
