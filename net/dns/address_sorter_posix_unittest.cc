33bf021d994753d9c1af926f2d070241a66788ec
Yixin Wang <wangyix@chromium.org>
2018-03-15 17:18:52 +0000

    Add SetMsgConfirm(bool) to udp_socket_posix and udp_socket_win
    Bug: 818893
    Change-Id: I58d04134464770368736cfb513a515d0debfead5
    Reviewed-on: https://chromium-review.googlesource.com/956929
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Yixin Wang <wangyix@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543416}

--- net/dns/address_sorter_posix_unittest.cc
+++ net/dns/address_sorter_posix_unittest.cc
@@ -80,6 +80,7 @@ class TestUDPClientSocket : public DatagramClientSocket {
     return NetworkChangeNotifier::kInvalidNetworkHandle;
   }
   void ApplySocketTag(const SocketTag& tag) override {}
+  void SetMsgConfirm(bool confirm) override {}
 
   int Connect(const IPEndPoint& remote) override {
     if (connected_)
