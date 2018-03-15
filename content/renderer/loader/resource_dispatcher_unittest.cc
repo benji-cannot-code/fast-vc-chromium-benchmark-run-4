980bb420b3f80a1bc5b21c3f0890b0fd78e54086
Conley Owens <cco3@chromium.org>
2018-03-15 01:27:09 +0000

    Report was_cached in SubresourceLoadInfo
    This change adds a boolean `was_cached` to the SubresourceLoadInfo
    struct.  This lets consumers know whether or not the response was
    fetched from the network cache.  Notably, this is important for
    recording page load metrics when the Network Service is enabled.
    
    BUG=816684
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I28d60ff7286feca5d333203405ee9c351f43c0ab
    Reviewed-on: https://chromium-review.googlesource.com/938650
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Jay Civelli <jcivelli@chromium.org>
    Commit-Queue: Conley Owens <cco3@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543276}

--- content/renderer/loader/resource_dispatcher_unittest.cc
+++ content/renderer/loader/resource_dispatcher_unittest.cc
@@ -247,7 +247,6 @@ TEST_F(ResourceDispatcherTest, DelegateTest) {
   // peer at once.
   network::URLLoaderCompletionStatus status;
   status.error_code = net::OK;
-  status.exists_in_cache = false;
   status.encoded_data_length = strlen(kTestPageContents);
   client->OnComplete(status);
 
@@ -293,7 +292,6 @@ TEST_F(ResourceDispatcherTest, CancelDuringCallbackWithWrapperPeer) {
   // OnCompletedRequest, but it should not lead to crashes.)
   network::URLLoaderCompletionStatus status;
   status.error_code = net::OK;
-  status.exists_in_cache = false;
   status.encoded_data_length = strlen(kTestPageContents);
   client->OnComplete(status);
 
