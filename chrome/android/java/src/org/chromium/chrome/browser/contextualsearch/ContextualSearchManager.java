ec71121eea416c32852b92fa40186813899b1d59
Donn Denman <donnd@google.com>
2018-03-15 19:44:44 +0000

    [TTS] Add a histogram to validate Ranker logging.
    Adds another histogram to use to validate that our logging to Ranker for
    ML using features and outcomes is complete and balanced.
    
    This new histogram logs that features are available to Ranker when two key
    UX events occur:
    1) A tap gesture is recognized that we commit to processing,
    2) Each show of the Bar (for outcomes).
    The resulting histogram should highly correlate with the existing
    histogram that tracks actual logging of features and outcomes to Ranker.
    
    BUG=802963
    
    Change-Id: I43e6ba711fa8fb91ea1d484e7399ef07aa5aa6bd
    Reviewed-on: https://chromium-review.googlesource.com/959445
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Donn Denman <donnd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543473}

--- chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManager.java
@@ -1587,6 +1587,7 @@ public class ContextualSearchManager
                 // Set up the next batch of Ranker logging.
                 mTapSuppressionRankerLogger.setupLoggingForPage(getBaseWebContents());
                 mSearchPanel.getPanelMetrics().setRankerLogger(mTapSuppressionRankerLogger);
+                ContextualSearchUma.logRankerFeaturesAvailable(false);
                 mInternalStateController.notifyFinishedWorkOn(InternalState.TAP_GESTURE_COMMIT);
             }
 
@@ -1675,6 +1676,7 @@ public class ContextualSearchManager
             public void showContextualSearchTapUi() {
                 mInternalStateController.notifyStartingWorkOn(InternalState.SHOW_FULL_TAP_UI);
                 showContextualSearch(StateChangeReason.TEXT_SELECT_TAP);
+                ContextualSearchUma.logRankerFeaturesAvailable(true);
                 mInternalStateController.notifyFinishedWorkOn(InternalState.SHOW_FULL_TAP_UI);
             }
 
