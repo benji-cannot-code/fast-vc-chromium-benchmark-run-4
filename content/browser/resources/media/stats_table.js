1b1036a312f75831968d8c8c546e5dc09d623523
Roger Tawa <rogerta@chromium.org>
2018-03-15 15:39:04 +0000

    Revert "webrtc-internals: only highlight currently active connection"
    This reverts commit 9eaaf8d98dca80307d0ba8e4f2a9e942c613ca0a.
    
    Reason for revert: Speculatively reverting to try and fix compile error:
    
    https://ci.chromium.org/buildbot/chromium.webkit/Android%20Builder/133884
    
    https://logs.chromium.org/v/?s=chromium%2Fbb%2Fchromium.webkit%2FAndroid_Builder%2F133884%2F%2B%2Frecipes%2Fsteps%2Fcompile%2F0%2Fstdout
    
    Original change's description:
    > webrtc-internals: only highlight currently active connection
    > 
    > and remove the class from any past ones.
    > 
    > BUG=678292
    > 
    > Change-Id: I5a3bc354f45363573ed8604fbbc27ee2530faa66
    > Reviewed-on: https://chromium-review.googlesource.com/615500
    > Reviewed-by: Tommi <tommi@chromium.org>
    > Commit-Queue: Harald Alvestrand <hta@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#543371}
    
    TBR=tommi@chromium.org,hta@chromium.org,philipp.hancke@googlemail.com
    
    Change-Id: Ibaa7ea4489e195b39180ad24efdb21796e285faf
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 678292
    Reviewed-on: https://chromium-review.googlesource.com/964394
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Commit-Queue: Roger Tawa <rogerta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543376}

--- content/browser/resources/media/stats_table.js
+++ content/browser/resources/media/stats_table.js
@@ -134,7 +134,6 @@ var StatsTable = (function(ssrcInfoManager) {
     updateStatsTableRow_: function(statsTable, rowName, value) {
       var trId = statsTable.id + '-' + rowName;
       var trElement = $(trId);
-      var activeConnectionClass = 'stats-table-active-connection';
       if (!trElement) {
         trElement = document.createElement('tr');
         trElement.id = trId;
@@ -144,13 +143,8 @@ var StatsTable = (function(ssrcInfoManager) {
       trElement.cells[1].textContent = value;
 
       // Highlights the table for the active connection.
-      if (rowName == 'googActiveConnection') {
-        if (value === 'true')
-          statsTable.parentElement.classList.add(activeConnectionClass);
-        } else {
-          statsTable.parentElement.classList.remove(activeConnectionClass);
-        }
-      }
+      if (rowName == 'googActiveConnection' && value == true)
+        statsTable.parentElement.classList.add('stats-table-active-connection');
     }
   };
 
