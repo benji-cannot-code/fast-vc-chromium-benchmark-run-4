f410543d7ceff43cf8d68b2f0a2a651027a024b8
Jennifer Apacible <apacible@chromium.org>
2018-03-15 02:41:55 +0000

    [cc::SurfaceLayer] Allow cc::SurfaceLayer to stretch content via ui::Layer.
    ui::Layer has a cc::SurfaceLayer and can set its primary surface. This
    change allows ui::Layer to also set whether or not cc::SurfaceLayer
    should stretch its content to fill the surface bounds, which will either
    stretch or shrink to fit the surface.
    
    This change is driven by the intent for Picture-in-Picture to be able
    to scale the video (contents) to fit into the PiP window bounds.
    Since the PiP surface embedder, or OverlaySurfaceEmbedder, utilizes a
    ui::Layer, this change would allow the ability to customize the
    cc::SurfaceLayer be exposed. Further work for the PiP sizing will be
    done, including maintaining the aspect ratio.
    
    BUG: 819414 726621
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I3bdbf54635702414a4007912271118a57733ad05
    Reviewed-on: https://chromium-review.googlesource.com/952476
    Commit-Queue: apacible <apacible@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Fady Samuel <fsamuel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543289}

--- content/browser/renderer_host/delegated_frame_host.cc
+++ content/browser/renderer_host/delegated_frame_host.cc
@@ -289,7 +289,7 @@ void DelegatedFrameHost::WasResized(
 #endif
       client_->DelegatedFrameHostGetLayer()->SetShowPrimarySurface(
           surface_id, current_frame_size_in_dip_, GetGutterColor(),
-          deadline_policy);
+          deadline_policy, false /* stretch_content_to_fill_bounds */);
       if (compositor_ && !base::CommandLine::ForCurrentProcess()->HasSwitch(
                              switches::kDisableResizeLock)) {
         compositor_->OnChildResizing();
@@ -522,7 +522,8 @@ void DelegatedFrameHost::OnFirstSurfaceActivation(
   } else {
     client_->DelegatedFrameHostGetLayer()->SetShowPrimarySurface(
         surface_info.id(), frame_size_in_dip, GetGutterColor(),
-        cc::DeadlinePolicy::UseDefaultDeadline());
+        cc::DeadlinePolicy::UseDefaultDeadline(),
+        false /* stretch_content_to_fill_bounds */);
   }
 
   client_->DelegatedFrameHostGetLayer()->SetFallbackSurfaceId(
