e00d7905ab72c09ce9d6977b0ffb1bbd716cdf9b
Tomasz Moniuszko <tmoniuszko@opera.com>
2018-03-15 12:24:30 +0000

    Fix crash on ResetWindowControls with no non-client view
    Crash happens when Widget with no non-client view (for example
    TYPE_WINDOW_FRAMELESS) is being shown on Windows 7 with Aero Glass
    disabled.
    
    This is a regression introduced by
    https://chromium-review.googlesource.com/924315.
    ResetWindowControls is called from HWNDMessageHandler::OnSysCommand.
    Previously it wasn't called if non-client view was null because
    ShouldHandleSystemCommands returned false in such case.
    
    Bug: 189112
    Change-Id: Ia12013ab11fd67dd28bcaf5d490c846928569a78
    Reviewed-on: https://chromium-review.googlesource.com/960029
    Commit-Queue: Tomasz Moniuszko <tmoniuszko@opera.com>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543350}

--- ui/views/widget/desktop_aura/desktop_window_tree_host_win.cc
+++ ui/views/widget/desktop_aura/desktop_window_tree_host_win.cc
@@ -728,7 +728,8 @@ gfx::Size DesktopWindowTreeHostWin::DIPToScreenSize(
 }
 
 void DesktopWindowTreeHostWin::ResetWindowControls() {
-  GetWidget()->non_client_view()->ResetWindowControls();
+  if (GetWidget()->non_client_view())
+    GetWidget()->non_client_view()->ResetWindowControls();
 }
 
 gfx::NativeViewAccessible DesktopWindowTreeHostWin::GetNativeViewAccessible() {
@@ -846,7 +847,8 @@ void DesktopWindowTreeHostWin::HandleFrameChanged() {
   CheckForMonitorChange();
   SetWindowTransparency();
   // Replace the frame and layout the contents.
-  GetWidget()->non_client_view()->UpdateFrame();
+  if (GetWidget()->non_client_view())
+    GetWidget()->non_client_view()->UpdateFrame();
 }
 
 void DesktopWindowTreeHostWin::HandleNativeFocus(HWND last_focused_window) {
