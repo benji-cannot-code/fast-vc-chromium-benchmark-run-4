74bc42e14cc02b00b3b507449d513861e938997b
Tarun Bansal <tbansal@chromium.org>
2018-03-15 17:48:31 +0000

    Test that client hints are not sent on insecure transports
    Also, add tests for other client hints.
    
    Finally. use the built-in sub pipe to enable running the
    cross-origin test.
    
    Bug: 817049
    Change-Id: Ib4155f50e0ffd3a0447cf250cd4018b650f3b419
    Reviewed-on: https://chromium-review.googlesource.com/963403
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Robert Ma <robertma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543433}

--- third_party/WebKit/LayoutTests/external/wpt/client-hints/echo_client_hints_received.py
+++ third_party/WebKit/LayoutTests/external/wpt/client-hints/echo_client_hints_received.py
@@ -7,3 +7,7 @@ def main(request, response):
 
     if "device-memory" in request.headers:
             response.headers.set("device-memory-received", "true")
+    if "dpr" in request.headers:
+            response.headers.set("dpr-received", "true")
+    if "viewport-width" in request.headers:
+            response.headers.set("viewport-width-received", "true")
