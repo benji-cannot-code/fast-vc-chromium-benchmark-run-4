51ad300979e10b18f8e67ef4613003c852515d19
Xi Cheng <chengx@chromium.org>
2018-03-15 18:52:57 +0000

    Fix passing activation information from startup to NotificationPlatformBridgeWin
    This should fix the issue that notification activation is not working in some cases.
    
    Bug: 734095, 821793
    Change-Id: If19f1c802b7da3b1cd0476c0a585138a2d2edcaa
    Reviewed-on: https://chromium-review.googlesource.com/963685
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Xi Cheng <chengx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543461}

--- chrome/browser/notifications/notification_platform_bridge_win.h
+++ chrome/browser/notifications/notification_platform_bridge_win.h
@@ -15,6 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sequenced_task_runner.h"
 #include "chrome/browser/notifications/notification_platform_bridge.h"
 
+namespace base {
+class CommandLine;
+}
+
 class NotificationPlatformBridgeWinImpl;
 class NotificationTemplateBuilder;
 
@@ -39,9 +43,11 @@ class NotificationPlatformBridgeWin : public NotificationPlatformBridge {
       const GetDisplayedNotificationsCallback& callback) const override;
   void SetReadyCallback(NotificationBridgeReadyCallback callback) override;
 
-  // Handles notification activation from the notification_helper process.
-  // Returns false if the launch id, passed via the command line, is invalid.
-  static bool HandleActivation();
+  // Handles notification activation encoded in |command_line| from the
+  // notification_helper process.
+  // Returns false if |command_line| does not contain a valid
+  // notification-launch-id switch.
+  static bool HandleActivation(const base::CommandLine& command_line);
 
   // Extracts the profile ID from |launch_id_str|.
   static std::string GetProfileIdFromLaunchId(const std::string& launch_id_str);
