06d55d7a1d9b96bd2dcc2a131d1c83e7578e33c2
Shubhie Panicker <panicker@chromium.org>
2018-03-15 23:06:34 +0000

    Add UMA for when StartWorker is received on renderer.
    This is to debug an issue with bit increased SW timeouts in
    StopLoadingInBackground experiment.
    
    Bug: 790903
    Change-Id: I89edec7f1b077e4ecd35256ecc09c6083db14879
    Reviewed-on: https://chromium-review.googlesource.com/955680
    Commit-Queue: Shubhie Panicker <panicker@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543537}

--- content/renderer/service_worker/embedded_worker_instance_client_impl.cc
+++ content/renderer/service_worker/embedded_worker_instance_client_impl.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/memory/ptr_util.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/strings/utf_string_conversions.h"
 #include "content/child/scoped_child_process_reference.h"
 #include "content/common/service_worker/service_worker_utils.h"
@@ -71,6 +72,13 @@ void EmbeddedWorkerInstanceClientImpl::StartWorker(
       io_thread_runner_);
   client->set_blink_initialized_time(blink_initialized_time_);
   client->set_start_worker_received_time(base::TimeTicks::Now());
+  // Record UMA to indicate StartWorker is received on renderer.
+  StartWorkerHistogramEnum metric =
+      params->is_installed ? StartWorkerHistogramEnum::RECEIVED_ON_INSTALLED
+                           : StartWorkerHistogramEnum::RECEIVED_ON_UNINSTALLED;
+  UMA_HISTOGRAM_ENUMERATION(
+      "ServiceWorker.EmbeddedWorkerInstanceClient.StartWorker", metric,
+      StartWorkerHistogramEnum::NUM_TYPES);
   wrapper_ = StartWorkerContext(std::move(params), std::move(client),
                                 std::move(interface_provider));
 }
