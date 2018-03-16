8c53a5b9359d722b8a35e6c8a4653b49f93d0133
CrystalFaith <crystallambert@chromium.org>
2018-03-16 20:43:01 +0000

    [Extension Docs]Update Mappy 2.0
    Mappy used deprecated APIs.
    Updated sendRequest and onRequest to sendMessage and onMessage.
    
    Bug: 808592
    Change-Id: I52a24f12d16cfde1bab8f9729bfe996ba97a356d
    Reviewed-on: https://chromium-review.googlesource.com/907214
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Crystal Lambert <crystallambert@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543814}

--- chrome/common/extensions/docs/examples/extensions/mappy/background.js
+++ chrome/common/extensions/docs/examples/extensions/mappy/background.js
@@ -3,43 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Global accessor that the popup uses.
-var addresses = {};
-var selectedAddress = null;
-var selectedId = null;
+'use strict'
 
-function updateAddress(tabId) {
-  chrome.tabs.sendRequest(tabId, {}, function(address) {
-    addresses[tabId] = address;
-    if (!address) {
-      chrome.pageAction.hide(tabId);
-    } else {
-      chrome.pageAction.show(tabId);
-      if (selectedId == tabId) {
-        updateSelected(tabId);
-      }
-    }
-  });
-}
-
-function updateSelected(tabId) {
-  selectedAddress = addresses[tabId];
-  if (selectedAddress)
-    chrome.pageAction.setTitle({tabId:tabId, title:selectedAddress});
-}
-
-chrome.tabs.onUpdated.addListener(function(tabId, change, tab) {
-  if (change.status == "complete") {
-    updateAddress(tabId);
-  }
-});
-
-chrome.tabs.onSelectionChanged.addListener(function(tabId, info) {
-  selectedId = tabId;
-  updateSelected(tabId);
-});
-
-// Ensure the current selected tab is set up.
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  updateAddress(tabs[0].id);
+chrome.runtime.onMessage.addListener(function(req, sender) {
+  chrome.storage.local.set({address: req.address})
+  chrome.pageAction.show(sender.tab.id);
+  chrome.pageAction.setTitle({tabId: sender.tab.id, title: req.address});
 });
