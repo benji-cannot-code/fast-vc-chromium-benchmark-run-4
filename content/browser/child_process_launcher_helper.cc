53b2096b4e1cfb972e314bbe6b7adbe5e48c134a
Xi Han <hanxi@google.com>
2018-03-16 20:50:49 +0000

    Use LazySingleThreadTaskRunner for process launcher thread.
    This is a follow up to CL (https://chromium-review.googlesource.com/c/chromium/src/+/941264).
    LazySingleThreadTaskRunner allows the created single-thread task runner be be
    recycled in unittests.
    
    Bug: 815225
    Change-Id: I70402798883f3550aced26ef07d48d8e882967f4
    Reviewed-on: https://chromium-review.googlesource.com/964782
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543822}

--- content/browser/child_process_launcher_helper.cc
+++ content/browser/child_process_launcher_helper.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "base/no_destructor.h"
 #include "base/single_thread_task_runner.h"
+#include "base/task_scheduler/lazy_task_runner.h"
 #include "base/task_scheduler/post_task.h"
 #include "base/task_scheduler/single_thread_task_runner_thread_mode.h"
 #include "base/task_scheduler/task_traits.h"
@@ -207,17 +208,17 @@ base::SingleThreadTaskRunner* GetProcessLauncherTaskRunner() {
   static base::NoDestructor<scoped_refptr<base::SingleThreadTaskRunner>>
       launcher_task_runner(
           android::LauncherThread::GetMessageLoop()->task_runner());
-#else   // defined(OS_ANDROID)
-  constexpr base::TaskTraits task_traits = {
-      base::MayBlock(), base::TaskPriority::USER_BLOCKING,
-      base::TaskShutdownBehavior::BLOCK_SHUTDOWN};
-  // TODO(wez): Investigates whether we could use SequencedTaskRunner on
-  // platforms other than Windows. http://crbug.com/820200.
-  static base::NoDestructor<scoped_refptr<base::SingleThreadTaskRunner>>
-      launcher_task_runner(base::CreateSingleThreadTaskRunnerWithTraits(
-          task_traits, base::SingleThreadTaskRunnerThreadMode::DEDICATED));
-#endif  // defined(OS_ANDROID)
   return (*launcher_task_runner).get();
+#else   // defined(OS_ANDROID)
+  // TODO(http://crbug.com/820200): Investigate whether we could use
+  // SequencedTaskRunner on platforms other than Windows.
+  static base::LazySingleThreadTaskRunner launcher_task_runner =
+      LAZY_SINGLE_THREAD_TASK_RUNNER_INITIALIZER(
+          base::TaskTraits({base::MayBlock(), base::TaskPriority::USER_BLOCKING,
+                            base::TaskShutdownBehavior::BLOCK_SHUTDOWN}),
+          base::SingleThreadTaskRunnerThreadMode::DEDICATED);
+  return launcher_task_runner.Get().get();
+#endif  // defined(OS_ANDROID)
 }
 
 // static
