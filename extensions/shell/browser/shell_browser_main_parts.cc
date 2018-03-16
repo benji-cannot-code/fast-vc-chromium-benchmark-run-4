b8ac3317594547f0e97162b282cf26bc1e1d7e9c
Michael Giuffrida <michaelpg@chromium.org>
2018-03-16 17:42:50 +0000

    AppShell: Support --crash-test
    Crash intentionally, e.g. to test out the crash reporter.
    
    Example:
      $ ./out/rel/app_shell --crash_test --enable-reporting
      DevTools listening on
      ws://127.0.0.1:37703/devtools/browser/ef324347-b8b4-4660-9270-aca50d1fb9d8
    
      Redirecting output to ‘wget-log’.
    
      Crash dump id: d920a038bd86b598
      zsh: illegal hardware instruction  ./out/rel/app_shell
    
    Bug: None
    Change-Id: I3f69080f19a81b2661cce4b7ee867378ea166b13
    Reviewed-on: https://chromium-review.googlesource.com/966545
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Michael Giuffrida <michaelpg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543754}

--- extensions/shell/browser/shell_browser_main_parts.cc
+++ extensions/shell/browser/shell_browser_main_parts.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/child_process_security_policy.h"
 #include "content/public/browser/context_factory.h"
 #include "content/public/browser/devtools_agent_host.h"
+#include "content/public/common/content_switches.h"
 #include "content/public/common/result_codes.h"
 #include "content/shell/browser/shell_devtools_manager_delegate.h"
 #include "extensions/browser/browser_context_keyed_service_factories.h"
@@ -87,6 +88,16 @@ using content::BrowserThread;
 #endif
 
 namespace extensions {
+
+namespace {
+
+// Intentionally dereferences a null pointer to test the crash reporter.
+void CrashForTest() {
+  int* bad_pointer = nullptr;
+  *bad_pointer = 0;
+}
+
+}  // namespace
 
 ShellBrowserMainParts::ShellBrowserMainParts(
     const content::MainFunctionParams& parameters,
@@ -233,6 +244,10 @@ void ShellBrowserMainParts::PreMainMessageLoopRun() {
 
   content::ShellDevToolsManagerDelegate::StartHttpHandler(
       browser_context_.get());
+
+  if (cmd->HasSwitch(::switches::kBrowserCrashTest))
+    CrashForTest();
+
   if (parameters_.ui_task) {
     // For running browser tests.
     parameters_.ui_task->Run();
