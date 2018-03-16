49135a3fba380b401acc7edf118efdbcea62f059
Julia Tuttle <juliatuttle@chromium.org>
2018-03-16 16:58:35 +0000

    Reporting: Log 'JSON too big' HeaderOutcome
    Change-Id: I75ecc7e893dfababd7669554433e674d77d95c1a
    Reviewed-on: https://chromium-review.googlesource.com/953502
    Commit-Queue: Julia Tuttle <juliatuttle@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543732}

--- net/reporting/reporting_service.cc
+++ net/reporting/reporting_service.cc
@@ -55,7 +55,7 @@ class ReportingServiceImpl : public ReportingService {
         base::BindRepeating(&ReportingServiceImpl::ProcessHeaderValue,
                             weak_factory_.GetWeakPtr(), url),
         base::BindRepeating(
-            &ReportingHeaderParser::RecordHeaderDiscardedForInvalidJson));
+            &ReportingHeaderParser::RecordHeaderDiscardedForJsonInvalid));
   }
 
   void RemoveBrowsingData(int data_type_mask,
