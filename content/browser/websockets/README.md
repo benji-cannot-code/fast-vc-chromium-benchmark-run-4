5d527bd57b10dc84db98aa4dbb0ac7dd23e90e36
Yutaka Hirano <yhirano@chromium.org>
2018-03-16 05:23:54 +0000

    Move websocket.mojom from third_party/WebKit to services/network
    As a preparation to integrate websocket with the network service, this
    CL moves the mojom file to /services/network.
    
    Bug: 721400
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I1284cbd9dd3292c41d1acba34835c6c6d44d48a3
    Reviewed-on: https://chromium-review.googlesource.com/961173
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543632}

--- content/browser/websockets/README.md
+++ content/browser/websockets/README.md
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # WebSocket service
 
-This directory contains the blink::mojom::WebSocket implementation. It hosts
+This directory contains the network::mojom::WebSocket implementation. It hosts
 the WebSocket API implementations in Blink over Mojo, and exports the WebSocket
 protocol implementation in net/websockets/ as a service.
--- content/browser/websockets/websocket_impl.h
+++ content/browser/websockets/websocket_impl.h
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "content/common/content_export.h"
 #include "mojo/public/cpp/bindings/binding.h"
-#include "third_party/WebKit/public/platform/modules/websockets/websocket.mojom.h"
+#include "services/network/public/mojom/websocket.mojom.h"
 #include "url/origin.h"
 
 class GURL;
@@ -30,7 +30,7 @@ class WebSocketChannel;
 namespace content {
 
 // Host of net::WebSocketChannel.
-class CONTENT_EXPORT WebSocketImpl : public blink::mojom::WebSocket {
+class CONTENT_EXPORT WebSocketImpl : public network::mojom::WebSocket {
  public:
   class Delegate {
    public:
@@ -42,7 +42,7 @@ class CONTENT_EXPORT WebSocketImpl : public blink::mojom::WebSocket {
   };
 
   WebSocketImpl(Delegate* delegate,
-                blink::mojom::WebSocketRequest request,
+                network::mojom::WebSocketRequest request,
                 int child_id,
                 int frame_id,
                 url::Origin origin,
@@ -53,14 +53,14 @@ class CONTENT_EXPORT WebSocketImpl : public blink::mojom::WebSocket {
   // This function is virtual for testing.
   virtual void GoAway();
 
-  // blink::mojom::WebSocket methods:
+  // network::mojom::WebSocket methods:
   void AddChannelRequest(const GURL& url,
                          const std::vector<std::string>& requested_protocols,
                          const GURL& site_for_cookies,
                          const std::string& user_agent_override,
-                         blink::mojom::WebSocketClientPtr client) override;
+                         network::mojom::WebSocketClientPtr client) override;
   void SendFrame(bool fin,
-                 blink::mojom::WebSocketMessageType type,
+                 network::mojom::WebSocketMessageType type,
                  const std::vector<uint8_t>& data) override;
   void SendFlowControl(int64_t quota) override;
   void StartClosingHandshake(uint16_t code, const std::string& reason) override;
@@ -78,9 +78,9 @@ class CONTENT_EXPORT WebSocketImpl : public blink::mojom::WebSocket {
                   const std::string& user_agent_override);
 
   Delegate* delegate_;
-  mojo::Binding<blink::mojom::WebSocket> binding_;
+  mojo::Binding<network::mojom::WebSocket> binding_;
 
-  blink::mojom::WebSocketClientPtr client_;
+  network::mojom::WebSocketClientPtr client_;
 
   // The channel we use to send events to the network.
   std::unique_ptr<net::WebSocketChannel> channel_;
