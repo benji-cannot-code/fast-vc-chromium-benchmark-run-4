cb6f5602c2c59dbe712190a90eece388bd8b9a1f
Ian Kilpatrick <ikilpatrick@chromium.org>
2018-03-16 20:39:16 +0000

    [shapes] Remove shape-outside avoiding behaviour for new-FCs/replaced.
    See intent: https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/pfDg-VAeKWs
    
    This makes new formatting contexts behave exactly like floats would
    when they are positioned. I.e. they don't look at the shape-outside
    of the float, and instead as positioned for their margin box.
    
    I've added a .tentative test as per the intent.
    
    This will allow us to have a (relatively) simple implementation of
    css-shapes in LayoutNG as we only have to consider the shape area for
    line-boxes. \o/
    
    Bug: 820520
    Change-Id: Ib07600a291eb4838675a9a1068c5fc16cac0633f
    Reviewed-on: https://chromium-review.googlesource.com/739961
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543813}

--- third_party/WebKit/Source/core/layout/FloatingObjects.h
+++ third_party/WebKit/Source/core/layout/FloatingObjects.h
@@ -230,10 +230,6 @@ class FloatingObjects {
       LayoutUnit logical_top,
       LayoutUnit* height_remaining);
 
-  // NOTE(ikilpatrick): These methods exist for determining how shape-outside
-  // affects positioning and sizing of block children avoiding floats.
-  // If usage is low enough these may be used instead of LogicalLeftOffset,
-  // LogicalLeftOffset for children avoiding floats.
   LayoutUnit LogicalLeftOffsetForAvoidingFloats(LayoutUnit fixed_offset,
                                                 LayoutUnit logical_top,
                                                 LayoutUnit logical_height);
