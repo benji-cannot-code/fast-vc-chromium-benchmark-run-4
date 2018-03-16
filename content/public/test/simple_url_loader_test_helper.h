94bd08fdf8cf6c1ed5624b80b448faf1a529fc63
Chong Zhang <chongz@chromium.org>
2018-03-16 23:10:18 +0000

    Fix flaky NetworkServiceRestartBrowserTest.BrowserIOSharedFactoryAfterStoragePartitionGone
    This test is flaky on Linux ASAN and Android, sample failure from Android:
    ```
    logging::LogMessage::~LogMessage()
    base::internal::WeakReference::Flag::Invalidate()
    base::internal::WeakReferenceOwner::~WeakReferenceOwner()
    content::SimpleURLLoaderTestHelper::~SimpleURLLoaderTestHelper()
    content::(anonymous namespace)::LoadBasicRequestOnIOThread(network::mojom::URLLoaderFactory*, GURL const&)
    content::NetworkServiceRestartBrowserTest_BrowserIOSharedFactoryAfterStoragePartitionGone_Test::RunTestOnMainThread()
    ```
    
    This CL makes sure that |simple_loader_helper.GetCallback()| was
    only accessed on UI thread (where |simple_loader_helper| lives).
    
    (I cannot reproduce the flakiness locally, will see if this fixes
    the issue.)
    
    Bug: 822585
    Change-Id: I74de7dd4708f20dcb2f0145228d6de19ac7f15b3
    Reviewed-on: https://chromium-review.googlesource.com/966985
    Commit-Queue: Chong Zhang <chongz@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543859}

--- content/public/test/simple_url_loader_test_helper.h
+++ content/public/test/simple_url_loader_test_helper.h
@@ -12,9 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_forward.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
-#include "base/optional.h"
 #include "base/run_loop.h"
-#include "content/public/browser/browser_thread.h"
 #include "services/network/public/cpp/simple_url_loader.h"
 
 namespace content {
@@ -33,9 +31,6 @@ class SimpleURLLoaderTestHelper {
   // Waits until the callback returned by GetCallback() is invoked.
   void WaitForCallback();
 
-  // Specify the thread to quit the runloop.
-  void SetRunLoopQuitThread(BrowserThread::ID);
-
   // Response body passed to the callback returned by GetCallback, if there was
   // one.
   const std::string* response_body() const { return response_body_.get(); }
@@ -51,10 +46,6 @@ class SimpleURLLoaderTestHelper {
   bool wait_started_ = false;
 
   base::RunLoop run_loop_;
-
-  // When set, will post |run_loop_.Quit()| to |run_loop_quit_thread_| if it's
-  // not current thread.
-  base::Optional<BrowserThread::ID> run_loop_quit_thread_;
 
   std::unique_ptr<std::string> response_body_;
 
