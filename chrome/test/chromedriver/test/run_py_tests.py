5d9c5530a5605898b3e0dc087ad831c47512f3fc
John Chen <johnchen@chromium.org>
2018-03-16 16:21:39 +0000

    [ChromeDriver] Disable testWindowMinimize on Android
    TBR=kereliuk@chromium.org
    
    Change-Id: I5910c15bcb442cb152f50f75a9008cc7a37887b7
    Reviewed-on: https://chromium-review.googlesource.com/966585
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543722}

--- chrome/test/chromedriver/test/run_py_tests.py
+++ chrome/test/chromedriver/test/run_py_tests.py
@@ -196,6 +196,7 @@ _ANDROID_NEGATIVE_FILTER['chrome'] = (
         'ChromeDriverTest.testWindowPosition',
         'ChromeDriverTest.testWindowSize',
         'ChromeDriverTest.testWindowMaximize',
+        'ChromeDriverTest.testWindowMinimize',
         'ChromeLogPathCapabilityTest.testChromeLogPath',
         'RemoteBrowserTest.*',
         # Don't enable perf testing on Android yet.
