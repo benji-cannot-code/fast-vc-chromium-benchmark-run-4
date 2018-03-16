15e7b9fedd3817bff4457a32950ddc9b4bd63666
Marijn Kruisselbrink <mek@chromium.org>
2018-03-16 06:22:50 +0000

    Change DataElement to hold DataPipeGetterPtrInfo instead of Ptr.
    Holding an InterfacePtr makes the struct weirdly sequence bound. Also
    get rid of const casts that invalidate a const DataElement and replace
    them with cloning the DataPipeGetter. This is needed because the same
    ResourceRequestBody is sent to multiple mojo methods, and thus the
    param traits should not modify the DataElements.
    
    On of several changes towards making uploading blobs with network service work.
    
    Tested in a separate CL because multiple unrelated bugfixes are needed to make this
    all work and testable (https://chromium-review.googlesource.com/c/chromium/src/+/963008).
    
    Bug: 821878, 761117
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: Ic1d63e6f0f7112cf37c75526c561c524a9c15201
    Reviewed-on: https://chromium-review.googlesource.com/963085
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543639}

--- services/network/public/cpp/data_element.h
+++ services/network/public/cpp/data_element.h
@@ -63,9 +63,6 @@ class COMPONENT_EXPORT(NETWORK_CPP_BASE) DataElement {
   const base::FilePath& path() const { return path_; }
   const base::File& file() const { return file_; }
   const std::string& blob_uuid() const { return blob_uuid_; }
-  const mojom::DataPipeGetterPtr& data_pipe() const {
-    return data_pipe_getter_;
-  }
   uint64_t offset() const { return offset_; }
   uint64_t length() const { return length_; }
   const base::Time& expected_modification_time() const {
@@ -172,7 +169,8 @@ class COMPONENT_EXPORT(NETWORK_CPP_BASE) DataElement {
   base::File ReleaseFile();
 
   // Takes ownership of the DataPipeGetter, if this is of TYPE_DATA_PIPE.
-  mojom::DataPipeGetterPtr ReleaseDataPipeGetter();
+  mojom::DataPipeGetterPtrInfo ReleaseDataPipeGetter();
+  mojom::DataPipeGetterPtr CloneDataPipeGetter() const;
 
   // Takes ownership of the DataPipeGetter, if this is of
   // TYPE_CHUNKED_DATA_PIPE.
@@ -193,7 +191,7 @@ class COMPONENT_EXPORT(NETWORK_CPP_BASE) DataElement {
   // For TYPE_BLOB.
   std::string blob_uuid_;
   // For TYPE_DATA_PIPE.
-  mojom::DataPipeGetterPtr data_pipe_getter_;
+  mojom::DataPipeGetterPtrInfo data_pipe_getter_;
   // For TYPE_CHUNKED_DATA_PIPE.
   mojom::ChunkedDataPipeGetterPtr chunked_data_pipe_getter_;
   uint64_t offset_;
