630acad59ad870eab013842abe29053802d94274
Tetsui Ohkubo <tetsui@chromium.org>
2018-03-16 07:55:22 +0000

    Relayout on battery status change.
    A child label of BatteryView can change their visibility and size.
    Also, EnterpriseManagedView can change its visibility.
    We have to explicitly call Layout() in ChildPreferredSizeChanged() &
    ChildVisibilityChanged().
    
    TEST=manual (connect/disconnect power cable while opening the tray)
    BUG=none
    
    Change-Id: Ia1c16669bcdce0740510b7c255642d7985763225
    Reviewed-on: https://chromium-review.googlesource.com/959944
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543644}

--- ash/system/unified/unified_system_info_view.cc
+++ ash/system/unified/unified_system_info_view.cc
@@ -101,6 +101,10 @@ class BatteryView : public views::View, public PowerStatus::Observer {
   BatteryView();
   ~BatteryView() override;
 
+  // views::View:
+  void ChildPreferredSizeChanged(views::View* child) override;
+  void ChildVisibilityChanged(views::View* child) override;
+
   // PowerStatus::Observer:
   void OnPowerStatusChanged() override;
 
@@ -141,6 +145,14 @@ BatteryView::BatteryView()
 
 BatteryView::~BatteryView() {
   PowerStatus::Get()->RemoveObserver(this);
+}
+
+void BatteryView::ChildPreferredSizeChanged(views::View* child) {
+  PreferredSizeChanged();
+}
+
+void BatteryView::ChildVisibilityChanged(views::View* child) {
+  PreferredSizeChanged();
 }
 
 void BatteryView::OnPowerStatusChanged() {
@@ -270,5 +282,13 @@ UnifiedSystemInfoView::UnifiedSystemInfoView()
 }
 
 UnifiedSystemInfoView::~UnifiedSystemInfoView() = default;
+
+void UnifiedSystemInfoView::ChildVisibilityChanged(views::View* child) {
+  Layout();
+}
+
+void UnifiedSystemInfoView::ChildPreferredSizeChanged(views::View* child) {
+  Layout();
+}
 
 }  // namespace ash
