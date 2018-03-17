ca26b4f8f7d9cb51283ad5ca92a25378a2f10763
Esmael El-Moslimany <aee@chromium.org>
2018-03-17 00:59:11 +0000

    WebUI cr-toast: clear timeout before showing
    Bug: 819996
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I8a9fff7afcc4fa17f742bcad6bb102a8f6271881
    Reviewed-on: https://chromium-review.googlesource.com/963694
    Commit-Queue: Esmael El-Moslimany <aee@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543893}

--- chrome/test/data/webui/cr_elements/cr_elements_browsertest.js
+++ chrome/test/data/webui/cr_elements/cr_elements_browsertest.js
@@ -302,6 +302,10 @@ CrElementsToastTest.prototype = {
   ]),
 };
 
+TEST_F('CrElementsToastTest', 'All', function() {
+  mocha.run();
+});
+
 /**
  * @constructor
  * @extends {CrElementsBrowserTest}
