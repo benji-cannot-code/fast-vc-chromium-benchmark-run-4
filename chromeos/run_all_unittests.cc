34b5b68c9925d95078cd9c035d1efa49e339fbb5
Kyle Horimoto <khorimoto@google.com>
2018-03-17 01:00:23 +0000

    [CrOS MultiDevice] Create a skeleton MultiDeviceSetup service.
    See go/unified-better-together-cros for an outline of this service.
    
    This CL does the following:
    (1) Creates a Mojo interface for the service, adding only a single
        function for now.
    (2) Creates the MultiDeviceSetupService and MultiDeviceSetupImpl classes
        which implement this interface, as well as a test for the service.
    (3) Registers the service in the browser process via ProfileImpl.
    (4) Generates JavaScript files for the interface; because these files
        are the first resources generated in //chromeos, this CL also adds a
        new chromeos_resources.grd.
    (5) Adds the new "multidevice_setup" capability to the content_browser
        manifest file so that it can be used by content_renderer.
    (6) Adds a new button on the chrome://proximity-auth debug WebUI page
        which makes a Mojo call to the service.
    
    Change-Id: I0b74373b78a1c92fe01d789028342707321c2134
    Reviewed-on: https://chromium-review.googlesource.com/954010
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: agrieve <agrieve@chromium.org>
    Reviewed-by: anthonyvd <anthonyvd@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543894}

---
+++ chromeos/run_all_unittests.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/bind.h"
+#include "base/test/launcher/unit_test_launcher.h"
+#include "base/test/test_suite.h"
+#include "mojo/edk/embedder/embedder.h"
+
+int main(int argc, char** argv) {
+  // Some unit tests make Mojo calls.
+  mojo::edk::Init();
+
+  base::TestSuite test_suite(argc, argv);
+  return base::LaunchUnitTests(
+      argc, argv,
+      base::Bind(&base::TestSuite::Run, base::Unretained(&test_suite)));
+}
