8d8330adfb6bcb1922927aa55d29c18ca3831d44
Pedro Amaral <amaralp@chromium.org>
2018-03-19 22:54:08 +0000

    Removed BottomSheetContent#isIncognitoThemedContent
    BottomSheetContent#isIncognitoThemedContent() always returns false
    so this CL removes it.
    
    
    Bug: 814528
    Change-Id: I85ad1676fd9847b3055ee69dc196baee1882579e
    Reviewed-on: https://chromium-review.googlesource.com/967098
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Pedro Amaral <amaralp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544196}

--- chrome/android/java/src/org/chromium/chrome/browser/suggestions/SuggestionsBottomSheetContent.java
+++ chrome/android/java/src/org/chromium/chrome/browser/suggestions/SuggestionsBottomSheetContent.java
@@ -203,11 +203,6 @@ public class SuggestionsBottomSheetContent implements BottomSheet.BottomSheetCon
     }
 
     @Override
-    public boolean isIncognitoThemedContent() {
-        return false;
-    }
-
-    @Override
     public int getVerticalScrollOffset() {
         return mRecyclerView.computeVerticalScrollOffset();
     }
