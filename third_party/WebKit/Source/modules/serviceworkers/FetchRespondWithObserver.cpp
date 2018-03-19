287327f289dc4eb97df6596b030f949cbb3f70da
Hajime Hoshi <hajimehoshi@chromium.org>
2018-03-19 05:49:54 +0000

    core/fetch: Pass an appropriate task runner to SimpleWatcher via FetchDataLoader
    This is part of efforts to replace base::ThreadTaskRunnerHandle::Get()
    and SequencedTaskRunnerHandle::Get() with other appropriate task runners
    in the renderer.
    
    Bug: 786332
    Change-Id: I2069aba52ec65f4ff6fd7cda0f7c437f072eac89
    Reviewed-on: https://chromium-review.googlesource.com/965902
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543977}

--- third_party/WebKit/Source/modules/serviceworkers/FetchRespondWithObserver.cpp
+++ third_party/WebKit/Source/modules/serviceworkers/FetchRespondWithObserver.cpp
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/inspector/ConsoleTypes.h"
 #include "modules/serviceworkers/ServiceWorkerGlobalScopeClient.h"
 #include "modules/serviceworkers/WaitUntilObserver.h"
+#include "public/platform/TaskType.h"
 #include "public/platform/modules/serviceworker/WebServiceWorkerResponse.h"
 #include "services/network/public/mojom/fetch_api.mojom-blink.h"
 #include "services/network/public/mojom/request_context_frame_type.mojom-blink.h"
@@ -265,9 +266,9 @@ void FetchRespondWithObserver::OnResponseFulfilled(const ScriptValue& value) {
                                                 body_stream_handle.get(),
                                                 event_dispatch_time_);
 
-    buffer->StartLoading(
-        FetchDataLoader::CreateLoaderAsDataPipe(std::move(producer)),
-        new FetchLoaderClient(std::move(body_stream_handle)));
+    buffer->StartLoading(FetchDataLoader::CreateLoaderAsDataPipe(
+                             std::move(producer), task_runner_),
+                         new FetchLoaderClient(std::move(body_stream_handle)));
     return;
   }
   ServiceWorkerGlobalScopeClient::From(GetExecutionContext())
@@ -293,7 +294,8 @@ FetchRespondWithObserver::FetchRespondWithObserver(
       request_mode_(request_mode),
       redirect_mode_(redirect_mode),
       frame_type_(frame_type),
-      request_context_(request_context) {}
+      request_context_(request_context),
+      task_runner_(context->GetTaskRunner(TaskType::kNetworking)) {}
 
 void FetchRespondWithObserver::Trace(blink::Visitor* visitor) {
   RespondWithObserver::Trace(visitor);
