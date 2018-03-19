b9ce6376a0fc5028017c9c51576c7be602ec2e30
edchin <edchin@chromium.org>
2018-03-19 18:43:57 +0000

    [ios] Grid cell needs to reset before reuse
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I808e52f47db5dbc9ddc609b123be1e10dabde996
    Reviewed-on: https://chromium-review.googlesource.com/967889
    Reviewed-by: edchin <edchin@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: edchin <edchin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544083}

--- ios/chrome/browser/ui/tab_grid/grid_cell.mm
+++ ios/chrome/browser/ui/tab_grid/grid_cell.mm
@@ -83,6 +83,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // NO-OP to disable highlighting and only allow selection.
 }
 
+- (void)prepareForReuse {
+  [super prepareForReuse];
+  self.itemIdentifier = nil;
+  self.title = nil;
+  self.icon = nil;
+  self.snapshot = nil;
+  self.selected = NO;
+}
+
 #pragma mark - Accessibility
 
 - (BOOL)isAccessibilityElement {
