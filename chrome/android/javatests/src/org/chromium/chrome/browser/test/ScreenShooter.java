f1d15f16ccf441ebc81b2f3a6f92332a5e786bcb
Anthony Berent <aberent@chromium.org>
2018-03-19 16:00:09 +0000

    Create a top level ui screenshot description (Reland)
    Merge the screenshot descriptions produced by the instrumentation tests
    into a single description. If the tests are run locally print a link
    to this description, if the are run remotely add a link to the
    description to the json file produced by test_results_presentation.
    
    Revert was
    https://chromium-review.googlesource.com/c/chromium/src/+/964931.
    The revert was because it appears that, on Android L and M, the
    instrumentation tests are not allowed to create, from Java, files
    on /data/local/tmp. This seems to be caused by an SELinux restriction.
    
    TBR=bauerb@chromium.org
    
    Bug: NONE
    Change-Id: Ic9352289a6eea12e95f9126d038c0fbaab295988
    Reviewed-on: https://chromium-review.googlesource.com/968527
    Reviewed-by: Anthony Berent <aberent@chromium.org>
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Commit-Queue: Anthony Berent <aberent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544051}

--- chrome/android/javatests/src/org/chromium/chrome/browser/test/ScreenShooter.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/test/ScreenShooter.java
@@ -11,6 +11,7 @@ import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import android.annotation.SuppressLint;
 import android.app.Instrumentation;
 import android.content.res.Configuration;
 import android.graphics.Point;
@@ -94,6 +95,7 @@ import java.util.Map;
  * }
  * </pre>
  */
+@SuppressLint("SetWorldReadable")
 public class ScreenShooter extends TestWatcher {
     private static final String SCREENSHOT_DIR =
             "org.chromium.base.test.util.Screenshooter.ScreenshotDir";
@@ -205,6 +207,9 @@ public class ScreenShooter extends TestWatcher {
             File shotFile = File.createTempFile(shotName, IMAGE_SUFFIX, new File(mBaseDir));
             assertTrue("Screenshot " + shotName, mDevice.takeScreenshot(shotFile));
             writeImageDescription(shotFile, filters, tags, metadata);
+            // Set as world readable so that the test runner can read it from /data/local/tmp
+            // without having to run as root
+            shotFile.setReadable(true, false);
         } catch (IOException e) {
             fail("Cannot create shot files " + e.toString());
         }
@@ -232,8 +237,12 @@ public class ScreenShooter extends TestWatcher {
         String jsonFileName =
                 shotFileName.substring(0, shotFileName.length() - IMAGE_SUFFIX.length())
                 + JSON_SUFFIX;
-        try (FileWriter fileWriter = new FileWriter(new File(mBaseDir, jsonFileName));) {
+        File descriptionFile = new File(mBaseDir, jsonFileName);
+        try (FileWriter fileWriter = new FileWriter(descriptionFile)) {
             fileWriter.write(imageDescription.toString());
         }
+        // Set as world readable so that the test runner can read it from /data/local/tmp without
+        // having to run as root
+        descriptionFile.setReadable(true, false);
     }
 }
