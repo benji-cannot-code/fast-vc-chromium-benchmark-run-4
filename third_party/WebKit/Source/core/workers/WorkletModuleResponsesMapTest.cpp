6e0765eca1f54058483623e395afaf44b9333bda
Daniel Bratell <bratell@opera.com>
2018-03-19 18:40:40 +0000

    Followup WorkerModuleFetchCoordinater, move test code to shared file
    Both the new and old code use an identical ClientImpl test class.
    In jumbo builds those end up in the same translation unit and
    it won't compile. This patch moves the class to a shared file
    WorkerFetchTestHelper.h
    
    Bug: 680046
    Change-Id: Iabe58f2f2c93e4d77fb36c8c454952af994d302c
    Reviewed-on: https://chromium-review.googlesource.com/968363
    Commit-Queue: Daniel Bratell <bratell@opera.com>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544077}

--- third_party/WebKit/Source/core/workers/WorkletModuleResponsesMapTest.cpp
+++ third_party/WebKit/Source/core/workers/WorkletModuleResponsesMapTest.cpp
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "core/loader/modulescript/ModuleScriptCreationParams.h"
+#include "core/workers/WorkerFetchTestHelper.h"
 #include "core/workers/WorkletModuleResponsesMap.h"
 #include "platform/loader/testing/FetchTestingPlatformSupport.h"
 #include "platform/loader/testing/MockFetchContext.h"
@@ -17,38 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace blink {
-
-namespace {
-
-class ClientImpl final : public GarbageCollectedFinalized<ClientImpl>,
-                         public WorkerOrWorkletModuleFetchCoordinator::Client {
-  USING_GARBAGE_COLLECTED_MIXIN(ClientImpl);
-
- public:
-  enum class Result { kInitial, kOK, kFailed };
-
-  void OnFetched(const ModuleScriptCreationParams& params) override {
-    ASSERT_EQ(Result::kInitial, result_);
-    result_ = Result::kOK;
-    params_.emplace(params);
-  }
-
-  void OnFailed() override {
-    ASSERT_EQ(Result::kInitial, result_);
-    result_ = Result::kFailed;
-  }
-
-  Result GetResult() const { return result_; }
-  WTF::Optional<ModuleScriptCreationParams> GetParams() const {
-    return params_;
-  }
-
- private:
-  Result result_ = Result::kInitial;
-  WTF::Optional<ModuleScriptCreationParams> params_;
-};
-
-}  // namespace
 
 class WorkletModuleResponsesMapTest : public ::testing::Test {
  public:
