e8c05195db95a0fd3db761b1953898cbcef74a55
Jan Krcal <jkrcal@chromium.org>
2018-03-19 19:11:19 +0000

    [protobuf] Convert sync tests from nano to lite
    Bug: 782237
    Change-Id: Ic1f84717cd457218cc91dc08f57af0c138d5b36b
    Reviewed-on: https://chromium-review.googlesource.com/859762
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Jan Krcal <jkrcal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544101}

--- chrome/android/sync_shell/javatests/src/org/chromium/chrome/browser/sync/FakeServerHelper.java
+++ chrome/android/sync_shell/javatests/src/org/chromium/chrome/browser/sync/FakeServerHelper.java
@@ -7,8 +7,7 @@ package org.chromium.chrome.browser.sync;
 
 import android.content.Context;
 
-import com.google.protobuf.nano.InvalidProtocolBufferNanoException;
-import com.google.protobuf.nano.MessageNano;
+import com.google.protobuf.InvalidProtocolBufferException;
 
 import org.chromium.base.ThreadUtils;
 import org.chromium.components.sync.protocol.EntitySpecifics;
@@ -191,14 +190,12 @@ public class FakeServerHelper {
         checkFakeServerInitialized("useFakeServer must be called before getting sync entities.");
         return ThreadUtils.runOnUiThreadBlocking(new Callable<List<SyncEntity>>() {
             @Override
-            public List<SyncEntity> call() throws InvalidProtocolBufferNanoException {
+            public List<SyncEntity> call() throws InvalidProtocolBufferException {
                 byte[][] serializedEntities = nativeGetSyncEntitiesByModelType(
                         mNativeFakeServerHelperAndroid, sNativeFakeServer, modelType);
                 List<SyncEntity> entities = new ArrayList<SyncEntity>(serializedEntities.length);
                 for (int i = 0; i < serializedEntities.length; i++) {
-                    SyncEntity entity = new SyncEntity();
-                    MessageNano.mergeFrom(entity, serializedEntities[i]);
-                    entities.add(entity);
+                    entities.add(SyncEntity.parseFrom(serializedEntities[i]));
                 }
                 return entities;
             }
@@ -221,7 +218,7 @@ public class FakeServerHelper {
                 // The protocol buffer is serialized as a byte array because it can be easily
                 // deserialized from this format in native code.
                 nativeInjectUniqueClientEntity(mNativeFakeServerHelperAndroid, sNativeFakeServer,
-                        name, MessageNano.toByteArray(entitySpecifics));
+                        name, entitySpecifics.toByteArray());
                 return null;
             }
         });
@@ -241,7 +238,7 @@ public class FakeServerHelper {
                 // The protocol buffer is serialized as a byte array because it can be easily
                 // deserialized from this format in native code.
                 nativeModifyEntitySpecifics(mNativeFakeServerHelperAndroid, sNativeFakeServer, id,
-                        MessageNano.toByteArray(entitySpecifics));
+                        entitySpecifics.toByteArray());
                 return null;
             }
         });
