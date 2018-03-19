b4657a90a1c38a7483a96b0c504942e2b143216c
Scott Violet <sky@chromium.org>
2018-03-19 16:41:04 +0000

    chromeos: Makes WindowTreeClient install PlatformEventSource
    This only works for Ozone and is necessary for both
    UserActivityDetector and remoting. Long term we need to rewrite both
    of these to use something else, but for now this accomplishes what
    they need.
    
    BUG=822577
    TEST=covered by tests
    
    Change-Id: I432cbdcce17bb199bf8bb0f00ec51de81d3c71f0
    Reviewed-on: https://chromium-review.googlesource.com/967447
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544063}

---
+++ ui/aura/mus/platform_event_source_mus_ozone.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/aura/mus/platform_event_source_mus_ozone.h"
+
+#include "ui/events/event.h"
+#include "ui/events/platform/platform_event_observer.h"
+
+namespace aura {
+
+PlatformEventSourceMus::PlatformEventSourceMus() = default;
+
+PlatformEventSourceMus::~PlatformEventSourceMus() = default;
+
+void PlatformEventSourceMus::OnWillProcessEvent(ui::Event* event) {
+  for (ui::PlatformEventObserver& observer : observers())
+    observer.WillProcessEvent(event);
+}
+
+void PlatformEventSourceMus::OnDidProcessEvent(ui::Event* event) {
+  for (ui::PlatformEventObserver& observer : observers())
+    observer.DidProcessEvent(event);
+}
+
+}  // namespace aura
