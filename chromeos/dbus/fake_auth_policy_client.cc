ad9c5652afb4d418377c676923a21f72a0d0fa5a
Roman Sorokin <rsorokin@chromium.org>
2018-03-20 04:44:58 +0000

    FakeAuthPolicyClient: RefreshDevicePolicy stores saved device policy
    RefreshDevicePolicy stores saved device policy in the session manager.
    E.g. it is unusable for tests to set specific device policies.
    
    BUG=chromium:819657
    TEST=FakeAuthPolicyClientTest.RefreshDevicePolicyStoresPolicy
    
    Change-Id: Id90e090453da27ffa4527ed810bc1f45c5337a3a
    Reviewed-on: https://chromium-review.googlesource.com/955842
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: Lutz Justen <ljusten@chromium.org>
    Commit-Queue: Roman Sorokin <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544281}

--- chromeos/dbus/fake_auth_policy_client.cc
+++ chromeos/dbus/fake_auth_policy_client.cc
@@ -24,9 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/dbus/session_manager_client.h"
 #include "chromeos/login/auth/authpolicy_login_helper.h"
-#include "components/policy/proto/chrome_device_policy.pb.h"
 #include "components/policy/proto/cloud_policy.pb.h"
-#include "components/policy/proto/device_management_backend.pb.h"
 #include "components/signin/core/account_id/account_id.h"
 #include "dbus/message.h"
 #include "third_party/cros_system_api/dbus/service_constants.h"
@@ -61,11 +59,11 @@ void RunSignalCallback(const std::string& interface_name,
 }
 
 void StoreDevicePolicy(
+    const em::ChromeDeviceSettingsProto& device_policy,
     const std::string& machine_name,
     chromeos::AuthPolicyClient::RefreshPolicyCallback callback) {
-  em::ChromeDeviceSettingsProto policy;
   std::string payload;
-  CHECK(policy.SerializeToString(&payload));
+  CHECK(device_policy.SerializeToString(&payload));
 
   em::PolicyFetchResponse response;
   em::PolicyData policy_data;
@@ -216,7 +214,7 @@ void FakeAuthPolicyClient::RefreshDevicePolicy(RefreshPolicyCallback callback) {
     return;
   }
 
-  StoreDevicePolicy(machine_name_, std::move(callback));
+  StoreDevicePolicy(device_policy_, machine_name_, std::move(callback));
 }
 
 void FakeAuthPolicyClient::RefreshUserPolicy(const AccountId& account_id,
@@ -275,7 +273,7 @@ void FakeAuthPolicyClient::OnDevicePolicyRetrieved(
   policy_data.ParseFromString(response.policy_data());
   if (policy_data.has_device_id())
     machine_name_ = policy_data.device_id();
-  StoreDevicePolicy(machine_name_, std::move(callback));
+  StoreDevicePolicy(device_policy_, machine_name_, std::move(callback));
 }
 
 }  // namespace chromeos
