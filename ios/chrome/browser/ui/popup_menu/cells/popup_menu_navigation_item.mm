41445042bff4377f4d544f38925b97317f07aace
Gauthier Ambard <gambard@chromium.org>
2018-03-20 14:55:34 +0000

    Create table view for the popup menu
    This CL creates a table view used to display the elements of the popup
    menus and the items to be presented by this table view.
    It also creates the mediator used to create and manage the items.
    
    Bug: 804770
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Icc861cc1d8d2c37bdc3d5dc70f2b072d657dd721
    Reviewed-on: https://chromium-review.googlesource.com/966201
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544365}

---
+++ ios/chrome/browser/ui/popup_menu/cells/popup_menu_navigation_item.mm
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/popup_menu/cells/popup_menu_navigation_item.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+@implementation PopupMenuNavigationItem
+
+- (instancetype)initWithType:(NSInteger)type {
+  self = [super initWithType:type];
+  if (self) {
+    self.cellClass = [PopupMenuNavigationCell class];
+  }
+  return self;
+}
+
+@end
+
+@implementation PopupMenuNavigationCell
+@end
--- ios/chrome/browser/ui/popup_menu/popup_menu_coordinator.mm
+++ ios/chrome/browser/ui/popup_menu/popup_menu_coordinator.mm
@@ -8,7 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #import "ios/chrome/browser/ui/commands/command_dispatcher.h"
 #import "ios/chrome/browser/ui/commands/popup_menu_commands.h"
+#import "ios/chrome/browser/ui/popup_menu/popup_menu_mediator.h"
 #import "ios/chrome/browser/ui/popup_menu/popup_menu_presenter.h"
+#import "ios/chrome/browser/ui/popup_menu/popup_menu_table_view_controller.h"
 #import "ios/chrome/browser/ui/util/layout_guide_names.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -60,12 +62,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)showToolsMenuPopup {
-  UIViewController* viewController = [[UIViewController alloc] init];
-  UILabel* label = [[UILabel alloc] init];
-  label.text = @"Tools menu";
-  viewController.view = label;
-  // TODO(crbug.com/804773): Use the tools menu instead of a label.
-  [self presentPopupForContent:viewController fromNamedGuide:kToolsMenuGuide];
+  PopupMenuTableViewController* tableViewController =
+      [[PopupMenuTableViewController alloc] init];
+
+  PopupMenuMediator* mediator =
+      [[PopupMenuMediator alloc] initWithType:PopupMenuTypeToolsMenu];
+  [mediator setUp];
+  [mediator configurePopupMenu:tableViewController];
+
+  [self presentPopupForContent:tableViewController
+                fromNamedGuide:kToolsMenuGuide];
 }
 
 - (void)showTabGridButtonPopup {
