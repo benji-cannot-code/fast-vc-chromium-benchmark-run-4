ddaa60cf6998bf488db08628830dff0b8bbd25c6
Finnur Thorarinsson <finnur@chromium.org>
2018-03-20 12:26:06 +0000

    Notifications: Minor cleanup.
    Convert ...
      const CallBack& callback
    ... to ...
      CallBack callback
    
    Bug: 734095
    Change-Id: I96125e214b5337c78763ba304e8a7135cd42b635
    Reviewed-on: https://chromium-review.googlesource.com/964327
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544336}

--- chrome/browser/notifications/notification_display_service_impl.cc
+++ chrome/browser/notifications/notification_display_service_impl.cc
@@ -257,7 +257,7 @@ void NotificationDisplayServiceImpl::Close(
 }
 
 void NotificationDisplayServiceImpl::GetDisplayed(
-    const DisplayedNotificationsCallback& callback) {
+    DisplayedNotificationsCallback callback) {
   if (!bridge_initialized_) {
     actions_.push(base::BindOnce(&NotificationDisplayServiceImpl::GetDisplayed,
                                  weak_factory_.GetWeakPtr(), callback));
@@ -265,7 +265,7 @@ void NotificationDisplayServiceImpl::GetDisplayed(
   }
 
   bridge_->GetDisplayed(GetProfileId(profile_), profile_->IsOffTheRecord(),
-                        callback);
+                        std::move(callback));
 }
 
 // Callback to run once the profile has been loaded in order to perform a
