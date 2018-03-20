653fc550cacbbc31f5bd3ed7a38b84999f9d3ce6
Mikhail Lopatkin <mlopatkin@yandex-team.ru>
2018-03-20 20:11:12 +0000

    Remove ENABLE_EXTENSIONS ifdefs from chrome_metrics_helper.cc/h
    All extension-specific functionality was removed from
    ChromeMetricsHelper in https://crrev.com/c/915121. The only thing left
    is web_contents_ field; this field is unused if ENABLE_EXTENSIONS is
    enabled but ENABLE_CAPTIVE_PORTAL_DETECTION is not, triggering a
    compilation error. There is no such configuration in Chromium but it is
    a error anyway.
    
    R=cthomp@chromium.org, felt@chromium.org
    
    Bug: none
    Change-Id: Iff1673e9c28cc6c71091b01a8e35870eca397468
    Reviewed-on: https://chromium-review.googlesource.com/970544
    Reviewed-by: Adrienne Porter Felt <felt@chromium.org>
    Commit-Queue: Adrienne Porter Felt <felt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544502}

--- chrome/browser/interstitials/chrome_metrics_helper.cc
+++ chrome/browser/interstitials/chrome_metrics_helper.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/buildflags.h"
 #include "components/history/core/browser/history_service.h"
 #include "content/public/browser/web_contents.h"
-#include "extensions/buildflags/buildflags.h"
 
 #if BUILDFLAG(ENABLE_CAPTIVE_PORTAL_DETECTION)
 #include "chrome/browser/ssl/captive_portal_metrics_recorder.h"
@@ -27,7 +26,7 @@ ChromeMetricsHelper::ChromeMetricsHelper(
           HistoryServiceFactory::GetForProfile(
               Profile::FromBrowserContext(web_contents->GetBrowserContext()),
               ServiceAccessType::EXPLICIT_ACCESS)),
-#if BUILDFLAG(ENABLE_CAPTIVE_PORTAL_DETECTION) || BUILDFLAG(ENABLE_EXTENSIONS)
+#if BUILDFLAG(ENABLE_CAPTIVE_PORTAL_DETECTION)
       web_contents_(web_contents),
 #endif
       request_url_(request_url) {
--- chrome/browser/interstitials/chrome_metrics_helper.h
+++ chrome/browser/interstitials/chrome_metrics_helper.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "chrome/common/buildflags.h"
 #include "components/security_interstitials/core/metrics_helper.h"
-#include "extensions/buildflags/buildflags.h"
 #include "url/gurl.h"
 
 namespace content {
@@ -40,7 +39,7 @@ class ChromeMetricsHelper : public security_interstitials::MetricsHelper {
   void RecordExtraShutdownMetrics() override;
 
  private:
-#if BUILDFLAG(ENABLE_CAPTIVE_PORTAL_DETECTION) || BUILDFLAG(ENABLE_EXTENSIONS)
+#if BUILDFLAG(ENABLE_CAPTIVE_PORTAL_DETECTION)
   content::WebContents* web_contents_;
 #endif
   const GURL request_url_;
