70bb638ba334d295be87106449a5779342100398
Eugene But <eugenebut@google.com>
2018-03-20 20:57:47 +0000

    Log MobileDownloadRetryDownload for New Download Manager.
    This is an existing action logged for the Old Download Manager
    which is already defined in a private repository. This CL
    adds action to actions.xml. MobileDownloadRetryDownload will be
    removed from private repository in a separate CL.
    
    Bug: 791806
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I279d088a1996f79e9d21af04f51b1e03a08be162
    Reviewed-on: https://chromium-review.googlesource.com/965167
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544515}

--- ios/chrome/browser/ui/download/download_manager_coordinator.mm
+++ ios/chrome/browser/ui/download/download_manager_coordinator.mm
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/presenters/contained_presenter_delegate.h"
 #include "ios/chrome/grit/ios_strings.h"
 #import "ios/web/public/download/download_task.h"
+#include "net/base/net_errors.h"
 #include "net/url_request/url_fetcher_response_writer.h"
 #include "ui/base/l10n/l10n_util_mac.h"
 
@@ -189,6 +190,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)downloadManagerViewControllerDidStartDownload:
     (DownloadManagerViewController*)controller {
+  if (_downloadTask->GetErrorCode() != net::OK) {
+    base::RecordAction(base::UserMetricsAction("MobileDownloadRetryDownload"));
+  }
   _mediator.StartDowloading();
 }
 
