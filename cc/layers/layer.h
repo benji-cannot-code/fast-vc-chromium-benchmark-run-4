ae7b754f5ab8158f44306a6cff1ce22ed8e2d704
Robert Flack <flackr@chromium.org>
2018-03-20 14:43:46 +0000

    Convert Layer's LayerClient to a WeakPtr.
    Bug: 813520
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I3f5b734202c69346c4681c11a40ebb58922f7e64
    Reviewed-on: https://chromium-review.googlesource.com/969587
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Commit-Queue: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544359}

--- cc/layers/layer.h
+++ cc/layers/layer.h
@@ -336,7 +336,9 @@ class CC_EXPORT Layer : public base::RefCounted<Layer> {
   TakeDebugInfo();
   virtual void didUpdateMainThreadScrollingReasons();
 
-  void SetLayerClient(LayerClient* client) { inputs_.client = client; }
+  void SetLayerClient(base::WeakPtr<LayerClient> client) {
+    inputs_.client = std::move(client);
+  }
 
   virtual bool IsSnapped();
 
@@ -628,7 +630,7 @@ class CC_EXPORT Layer : public base::RefCounted<Layer> {
     bool hide_layer_and_subtree : 1;
 
     // The following elements can not and are not serialized.
-    LayerClient* client;
+    base::WeakPtr<LayerClient> client;
     base::Callback<void(const gfx::ScrollOffset&, const ElementId&)>
         did_scroll_callback;
     std::vector<std::unique_ptr<viz::CopyOutputRequest>> copy_requests;
--- third_party/WebKit/Source/platform/graphics/GraphicsLayer.h
+++ third_party/WebKit/Source/platform/graphics/GraphicsLayer.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define GraphicsLayer_h
 
 #include <memory>
+#include "base/memory/weak_ptr.h"
 #include "cc/layers/layer_client.h"
 #include "platform/PlatformExport.h"
 #include "platform/geometry/FloatPoint.h"
@@ -431,6 +432,8 @@ class PLATFORM_EXPORT GraphicsLayer : public cc::LayerClient,
   std::unique_ptr<LayerState> layer_state_;
 
   std::unique_ptr<CompositedLayerRasterInvalidator> raster_invalidator_;
+
+  base::WeakPtrFactory<GraphicsLayer> weak_ptr_factory_;
 };
 
 // ObjectPaintInvalidatorWithContext::InvalidatePaintRectangleWithContext uses
