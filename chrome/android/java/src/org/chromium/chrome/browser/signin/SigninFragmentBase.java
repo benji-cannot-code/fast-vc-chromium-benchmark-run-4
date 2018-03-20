746cc4d37e0f3412948db349c5f46bc3a2201f5d
Boris Sazonov <bsazonov@chromium.org>
2018-03-20 14:17:40 +0000

    [Unity][Android] Set and track consent text in SigninFragmentBase
    This CL extracts ConsentTextTracker from AccountSigninView and
    implements consent recording in SigninFragmentBase by using
    ConsentTextTracker.
    
    Bug: 814728
    Change-Id: I7c699e63bea7a2c61244e72956d4774d46d79556
    Reviewed-on: https://chromium-review.googlesource.com/964841
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544350}

--- chrome/android/java/src/org/chromium/chrome/browser/signin/SigninFragmentBase.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/SigninFragmentBase.java
@@ -12,9 +12,11 @@ import android.support.annotation.StringRes;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.consent_auditor.ConsentAuditorFeature;
 
 /**
  * This fragment implements sign-in screen with account picker and descriptions of signin-related
@@ -29,6 +31,7 @@ public abstract class SigninFragmentBase extends Fragment {
     private @SigninAccessPoint int mSigninAccessPoint;
 
     private SigninView mView;
+    private ConsentTextTracker mConsentTextTracker;
     private @StringRes int mCancelButtonTextId = R.string.cancel;
 
     /**
@@ -107,9 +110,15 @@ public abstract class SigninFragmentBase extends Fragment {
             onSigninRefused();
         });
 
-        // TODO(https://crbug.com/814728): Set texts for UI elements
+        updateConsentText();
 
         return mView;
+    }
+
+    private void updateConsentText() {
+        mConsentTextTracker.setText(mView.getRefuseButton(), mCancelButtonTextId);
+        mConsentTextTracker.setText(mView.getAcceptButton(), R.string.signin_accept_button);
+        mConsentTextTracker.setText(mView.getMoreButton(), R.string.more);
     }
 
     private void showAcceptButton() {
@@ -121,5 +130,14 @@ public abstract class SigninFragmentBase extends Fragment {
     private void setButtonsEnabled(boolean enabled) {
         mView.getAcceptButton().setEnabled(enabled);
         mView.getRefuseButton().setEnabled(enabled);
+    }
+
+    /**
+     * Records the Sync consent.
+     * @param confirmationView The view that the user clicked when consenting.
+     */
+    private void recordConsent(TextView confirmationView) {
+        mConsentTextTracker.recordConsent(
+                ConsentAuditorFeature.CHROME_SYNC, confirmationView, mView);
     }
 }
