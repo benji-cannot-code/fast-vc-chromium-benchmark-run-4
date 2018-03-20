79fb3b8a670ecd7ef38259d79075be8df911e6ca
Joshua Bell <jsbell@chromium.org>
2018-03-20 17:48:00 +0000

    Indexed DB: Consolidate window/worker IDL tests into any.js
    Also, rely on HTML and DOM interfaces for dependencies rather
    than inline definitions for Event, EventTarget, etc.
    
    Change-Id: I58ac6515f8d2769aec6bbf1b90d9a4105639ad24
    Reviewed-on: https://chromium-review.googlesource.com/965452
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Commit-Queue: Joshua Bell <jsbell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544429}

---
+++ third_party/WebKit/LayoutTests/external/wpt/IndexedDB/interfaces.any.js
@@ -0,0 +1,33 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/resources/WebIDLParser.js
+// META: script=/resources/idlharness.js
+
+promise_test(async t => {
+  const [html, dom, indexeddb] = await Promise.all([
+    '/interfaces/html.idl',
+    '/interfaces/dom.idl',
+    '/interfaces/IndexedDB.idl',
+  ].map(url => fetch(url).then(response => response.text())));
+
+  const idl_array = new IdlArray();
+  idl_array.add_untested_idls('interface LinkStyle {};');  // Needed by html
+  idl_array.add_untested_idls(html);
+  idl_array.add_untested_idls(dom);
+  idl_array.add_idls(indexeddb);
+  idl_array.add_objects({
+    IDBCursor: [],
+    IDBCursorWithValue: [],
+    IDBDatabase: [],
+    IDBFactory: [self.indexedDB],
+    IDBIndex: [],
+    IDBKeyRange: [IDBKeyRange.only(0)],
+    IDBObjectStore: [],
+    IDBOpenDBRequest: [],
+    IDBRequest: [],
+    IDBTransaction: [],
+    IDBVersionChangeEvent: [new IDBVersionChangeEvent('')],
+    DOMStringList: [],
+  });
+
+  idl_array.test();
+}, 'Test driver');
