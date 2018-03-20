6cf5df43996af44e7bdeac16224db6dc2c7e7e8c
Xiaohan Wang <xhwang@chromium.org>
2018-03-20 22:18:35 +0000

    media: Remove widevinecdmadapter target
    Pepper CDM is being deprecated so widevinecdmadapter should not be
    needed anywhere now. Targets that need Widevine CDM should depend
    on "widevinecdm" target directly.
    
    Details of change:
    - Remove widevinecdmadapter from third_party/widevine/cdm/BUILD.gn
    - Remove files required to build widevinecdmadapter
    - Remove stub CDM since it's only needed to build the CDM adapter
    - Update installer code to remove widevinecdmadapter
    
    Bug: 772160
    Change-Id: I5c17d3d34a98839ecb48438cd09aaa8a565dd5dd
    Reviewed-on: https://chromium-review.googlesource.com/957868
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Reviewed-by: Prasad Vuppalapu <prasadv@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544557}

--- tools/bisect_repackage/bisect_repackage.py
+++ tools/bisect_repackage/bisect_repackage.py
@@ -48,7 +48,6 @@ CHROME_REQUIRED_FILES = {
         'icudtl.dat',
         'libclearkeycdm.so',
         'libwidevinecdm.so',
-        'libwidevinecdmadapter.so',
         'locales/',
         'nacl_helper',
         'nacl_helper_bootstrap',
