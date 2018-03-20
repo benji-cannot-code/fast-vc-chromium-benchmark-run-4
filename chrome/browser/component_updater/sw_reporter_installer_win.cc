5dd735ffbccf6acf417489cdbd195a0186744433
David Benjamin <davidben@chromium.org>
2018-03-20 21:45:55 +0000

    Fix base::WStringTokenizer::token_piece.
    There's only one WStringTokenizer use, so I've gone ahead and
    switched its use to token_piece which saves a copy. But this
    was primarily because I noticed token_piece didn't quite work
    right.
    
    Bug: none
    Change-Id: I139dfea5bc34466075afc83b05555763c62dbdf1
    Reviewed-on: https://chromium-review.googlesource.com/956285
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544535}

--- chrome/browser/component_updater/sw_reporter_installer_win.cc
+++ chrome/browser/component_updater/sw_reporter_installer_win.cc
@@ -94,7 +94,7 @@ void ReportUploadsWithUma(const base::string16& upload_results) {
   int current_failure_run = 0;
   bool last_result = false;
   while (tokenizer.GetNext()) {
-    if (tokenizer.token() == L"0") {
+    if (tokenizer.token_piece() == L"0") {
       ++failure_count;
       ++current_failure_run;
       last_result = false;
