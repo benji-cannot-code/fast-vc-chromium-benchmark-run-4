da3e166441a2e8009e4f4660e8916e57bb70673b
Dmitry Gozman <dgozman@chromium.org>
2018-03-20 19:08:25 +0000

    [DevTools] Do not reference detached frames from console messages
    TBR=yukishiino@chromium.org
    
    Bug: 805882
    Change-Id: Iac5ecc118ddfcc05c0e3de39e13af4e080744031
    Reviewed-on: https://chromium-review.googlesource.com/971166
    Reviewed-by: Aleksey Kozyatinskiy <kozyatinskiy@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544471}

--- third_party/WebKit/Source/core/inspector/ConsoleMessage.cpp
+++ third_party/WebKit/Source/core/inspector/ConsoleMessage.cpp
@@ -103,7 +103,10 @@ const String& ConsoleMessage::WorkerId() const {
 }
 
 LocalFrame* ConsoleMessage::Frame() const {
-  return frame_;
+  // Do not reference detached frames.
+  if (frame_ && frame_->Client())
+    return frame_;
+  return nullptr;
 }
 
 Vector<DOMNodeId>& ConsoleMessage::Nodes() {
