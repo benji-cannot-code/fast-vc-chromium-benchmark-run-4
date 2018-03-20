dbb927903c7deb0252017aeb4d7143eb86545bb2
David Bertoni <dbertoni@chromium.org>
2018-03-20 00:31:04 +0000

    Remove the extensions-update-frequency command line flag.
    Metrics indicate this flag is not being used.
    
    Bug: 357781
    Change-Id: I138151e4726b2c8e3c9a6b59dccb8e37b123063b
    Reviewed-on: https://chromium-review.googlesource.com/946670
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544231}

--- chrome/common/chrome_switches.h
+++ chrome/common/chrome_switches.h
@@ -120,7 +120,6 @@ extern const char kExtensionContentVerificationEnforce[];
 extern const char kExtensionContentVerificationEnforceStrict[];
 extern const char kExtensionsInstallVerification[];
 extern const char kExtensionsNotWebstore[];
-extern const char kExtensionsUpdateFrequency[];
 extern const char kFastStart[];
 extern const char kForceAndroidAppMode[];
 extern const char kForceAppMode[];
