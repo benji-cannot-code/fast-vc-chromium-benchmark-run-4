fe37f22b335f0c6f885aee16918eadedab74fbeb
Findit <findit-for-me@appspot.gserviceaccount.com>
2018-03-20 18:56:56 +0000

    Revert "Wire-up the ThirdPartyBlocking group policy"
    This reverts commit a54726ff975e09efe95d241091a32954bdd58468.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 544439 as the
    culprit for failures in the build cycles as shown on:
    https://findit-for-me.appspot.com/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtL2E1NDcyNmZmOTc1ZTA5ZWZlOTVkMjQxMDkxYTMyOTU0YmRkNTg0NjgM
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium.chrome/Google%20Chrome%20Win/28248
    
    Sample Failed Step: compile
    
    Original change's description:
    > Wire-up the ThirdPartyBlocking group policy
    > 
    > To make this happen, the ProblematicProgramsUpdater class needed a
    > refactoring. This is because we do not support dynamic refresh of the
    > group policy, so its value needed to be cached somehow (as it can still
    > change during runtime).
    > 
    > Now, the presence of the ThirdPartyConflictsManager indicates if the
    > feature is enabled or not.
    > 
    > Bug: 822399
    > Change-Id: Ia9b0b5e52df2c777b343ccc3e524ee17a1ab0e57
    > Reviewed-on: https://chromium-review.googlesource.com/964785
    > Commit-Queue: Patrick Monette <pmonette@chromium.org>
    > Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    > Reviewed-by: Greg Thompson <grt@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#544439}
    
    Change-Id: I0502506465a1a356d941f5baa838350a2cf3b349
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 822399
    Reviewed-on: https://chromium-review.googlesource.com/971563
    Cr-Commit-Position: refs/heads/master@{#544460}

--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -273,7 +273,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/app_launch_for_metro_restart_win.h"
 #include "chrome/browser/component_updater/sw_reporter_installer_win.h"
 #if defined(GOOGLE_CHROME_BUILD)
-#include "chrome/browser/conflicts/module_database_win.h"
 #include "chrome/browser/conflicts/problematic_programs_updater_win.h"
 #endif  // defined(GOOGLE_CHROME_BUILD)
 #include "chrome/browser/safe_browsing/chrome_cleaner/settings_resetter_win.h"
@@ -468,7 +467,6 @@ void RegisterLocalState(PrefRegistrySimple* registry) {
   desktop_ios_promotion::RegisterLocalPrefs(registry);
   password_manager::PasswordManager::RegisterLocalPrefs(registry);
 #if defined(GOOGLE_CHROME_BUILD)
-  ModuleDatabase::RegisterLocalStatePrefs(registry);
   ProblematicProgramsUpdater::RegisterLocalStatePrefs(registry);
 #endif  // defined(GOOGLE_CHROME_BUILD)
 #endif
