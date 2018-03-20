91124699b4cc23cda38a9b81355abb7977cf98b8
Alexander Alekseev <alemate@chromium.org>
2018-03-20 23:41:11 +0000

    Chrome OS OOBE: Skip input pairing screen for devices currently in tablet mode.
    Bug: 780703
    Change-Id: Iff327629ebad1323141270ace01ea712fee9cc77
    Reviewed-on: https://chromium-review.googlesource.com/970561
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: Wenzhao (Colin) Zang <wzang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544585}

--- chrome/browser/chromeos/login/wizard_controller.cc
+++ chrome/browser/chromeos/login/wizard_controller.cc
@@ -75,6 +75,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/ash/ash_util.h"
+#include "chrome/browser/ui/ash/tablet_mode_client.h"
 #include "chrome/browser/ui/webui/chromeos/login/oobe_ui.h"
 #include "chrome/browser/ui/webui/chromeos/login/signin_screen_handler.h"
 #include "chrome/browser/ui/webui/help/help_utils_chromeos.h"
@@ -131,10 +132,12 @@ const chromeos::OobeScreen kResumableScreens[] = {
     chromeos::OobeScreen::SCREEN_ARC_TERMS_OF_SERVICE,
     chromeos::OobeScreen::SCREEN_AUTO_ENROLLMENT_CHECK};
 
-// Checks flag for HID-detection screen show.
+// Checks if device is in tablet mode, and that HID-detection screen is not
+// disabled by flag.
 bool CanShowHIDDetectionScreen() {
-  return !base::CommandLine::ForCurrentProcess()->HasSwitch(
-      chromeos::switches::kDisableHIDDetectionOnOOBE);
+  return !TabletModeClient::Get()->tablet_mode_enabled() &&
+         !base::CommandLine::ForCurrentProcess()->HasSwitch(
+             chromeos::switches::kDisableHIDDetectionOnOOBE);
 }
 
 bool IsResumableScreen(chromeos::OobeScreen screen) {
