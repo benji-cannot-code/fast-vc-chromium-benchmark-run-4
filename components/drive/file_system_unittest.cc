876ad4b342a3dd50638030748fd03884e8dc2967
Tatsuhisa Yamaguchi <yamaguchi@google.com>
2018-03-20 02:18:39 +0000

    Prevent error when an archive is already mounted.
    When an archive file (.rar) on Drive cache is already marked as mounted,
    it doesn't change the cache state but still need the cache file path.
    
    Bug: 798024
    Test: manually verified as noted in the bug
    Test: unit_tests --gtest_filter=FileCacheTest.*:FileSystemTest.*
    Change-Id: I44984dfe4ae4aa25885239b221d723fa3ba70866
    Reviewed-on: https://chromium-review.googlesource.com/896765
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Commit-Queue: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544264}

--- components/drive/file_system_unittest.cc
+++ components/drive/file_system_unittest.cc
@@ -1002,6 +1002,15 @@ TEST_F(FileSystemTest, MarkCacheFileAsMountedAndUnmounted) {
   content::RunAllTasksUntilIdle();
   EXPECT_EQ(FILE_ERROR_OK, error);
 
+  error = FILE_ERROR_FAILED;
+  bool is_marked_as_mounted = false;
+  file_system_->IsCacheFileMarkedAsMounted(
+      file_in_root, google_apis::test_util::CreateCopyResultCallback(
+                        &error, &is_marked_as_mounted));
+  content::RunAllTasksUntilIdle();
+  EXPECT_EQ(FILE_ERROR_OK, error);
+  EXPECT_TRUE(is_marked_as_mounted);
+
   // Cannot remove a cache entry while it's being mounted.
   EXPECT_EQ(FILE_ERROR_IN_USE, cache_->Remove(entry->local_id()));
 
@@ -1012,6 +1021,15 @@ TEST_F(FileSystemTest, MarkCacheFileAsMountedAndUnmounted) {
       google_apis::test_util::CreateCopyResultCallback(&error));
   content::RunAllTasksUntilIdle();
   EXPECT_EQ(FILE_ERROR_OK, error);
+
+  error = FILE_ERROR_FAILED;
+  is_marked_as_mounted = true;
+  file_system_->IsCacheFileMarkedAsMounted(
+      file_in_root, google_apis::test_util::CreateCopyResultCallback(
+                        &error, &is_marked_as_mounted));
+  content::RunAllTasksUntilIdle();
+  EXPECT_EQ(FILE_ERROR_OK, error);
+  EXPECT_FALSE(is_marked_as_mounted);
 
   // Now able to remove the cache entry.
   EXPECT_EQ(FILE_ERROR_OK, cache_->Remove(entry->local_id()));
