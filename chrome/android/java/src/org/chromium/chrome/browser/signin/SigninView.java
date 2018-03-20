76eba22b664ea7c96716b30ea6d5796147eb590d
Boris Sazonov <bsazonov@chromium.org>
2018-03-20 15:49:07 +0000

    [Unity][Android] Add account picker to sign-in screen
    This CL adds account picker to the streamlined sign-in screen. It will
    be wired up by subsequent CLs. RelativeLayout is used to avoid using
    nested LinearLayouts and minimize the depth of the layout. Strings in
    grd file are currently marked as not translateable as they haven't been
    confirmed by the UI team yet.
    This CL also adds initialization for ConsentTextTracker in
    SigninFragmentBase.
    
    Bug: 814728
    Change-Id: I60ae3841998d5dd9c13d35583915e7fe443d3b31
    Reviewed-on: https://chromium-review.googlesource.com/966902
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544384}

--- chrome/android/java/src/org/chromium/chrome/browser/signin/SigninView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/SigninView.java
@@ -10,7 +10,9 @@ import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import org.chromium.chrome.R;
 import org.chromium.ui.widget.ButtonCompat;
@@ -18,6 +20,10 @@ import org.chromium.ui.widget.ButtonCompat;
 /** View that wraps signin screen and caches references to UI elements. */
 public class SigninView extends LinearLayout {
     private SigninScrollView mScrollView;
+    private TextView mTitle;
+    private ImageView mAccountImage;
+    private TextView mAccountName;
+    private TextView mAccountEmail;
     private ButtonCompat mAcceptButton;
     private Button mRefuseButton;
     private Button mMoreButton;
@@ -32,6 +38,10 @@ public class SigninView extends LinearLayout {
         super.onFinishInflate();
 
         mScrollView = (SigninScrollView) findViewById(R.id.signin_scroll_view);
+        mTitle = (TextView) findViewById(R.id.signin_title);
+        mAccountImage = (ImageView) findViewById(R.id.account_image);
+        mAccountName = (TextView) findViewById(R.id.account_name);
+        mAccountEmail = (TextView) findViewById(R.id.account_email);
         mAcceptButton = (ButtonCompat) findViewById(R.id.positive_button);
         mRefuseButton = (Button) findViewById(R.id.negative_button);
         mMoreButton = (Button) findViewById(R.id.more_button);
@@ -40,6 +50,22 @@ public class SigninView extends LinearLayout {
 
     public SigninScrollView getScrollView() {
         return mScrollView;
+    }
+
+    public TextView getTitleView() {
+        return mTitle;
+    }
+
+    public ImageView getAccountImageView() {
+        return mAccountImage;
+    }
+
+    public TextView getAccountNameView() {
+        return mAccountName;
+    }
+
+    public TextView getAccountEmailView() {
+        return mAccountEmail;
     }
 
     public ButtonCompat getAcceptButton() {
