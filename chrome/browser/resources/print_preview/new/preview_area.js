83b99d934b703c4e233a2f663e1917ffa7ecd434
rbpotter <rbpotter@chromium.org>
2018-03-21 22:36:58 +0000

    Print Preview: Add Preview Generation tests
    Bug: 773928
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: If137f14a5bd9cb23585629710b62e9b018cced96
    Reviewed-on: https://chromium-review.googlesource.com/969704
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544883}

--- chrome/browser/resources/print_preview/new/preview_area.js
+++ chrome/browser/resources/print_preview/new/preview_area.js
@@ -362,7 +362,7 @@ Polymer({
     if (this.inFlightRequestId_ != previewResponseId)
       return;
     this.documentInfo.updatePageCount(pageCount);
-    this.documentInfo.fitToPageScaling_ = fitToPageScaling;
+    this.documentInfo.updateFitToPageScaling(fitToPageScaling);
     this.notifyPath('documentInfo.pageCount');
     this.notifyPath('documentInfo.fitToPageScaling');
   },
