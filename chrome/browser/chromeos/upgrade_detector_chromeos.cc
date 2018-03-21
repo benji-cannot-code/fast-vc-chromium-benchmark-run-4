78b476ee4d69ec15a219b1fe3bc207e1e572f930
Greg Thompson <grt@chromium.org>
2018-03-21 21:38:33 +0000

    The default high annoyance threshold is platform-specific.
    Update the policy documentation and the pref's default value
    accordingly.
    
    BUG=511300
    
    Change-Id: I1160474dbe48478d95bfdcdd17017e808b398492
    Reviewed-on: https://chromium-review.googlesource.com/968523
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544854}

--- chrome/browser/chromeos/upgrade_detector_chromeos.cc
+++ chrome/browser/chromeos/upgrade_detector_chromeos.cc
@@ -174,3 +174,8 @@ UpgradeDetectorChromeos* UpgradeDetectorChromeos::GetInstance() {
 UpgradeDetector* UpgradeDetector::GetInstance() {
   return UpgradeDetectorChromeos::GetInstance();
 }
+
+// static
+base::TimeDelta UpgradeDetector::GetDefaultHighAnnoyanceThreshold() {
+  return base::TimeDelta::FromDays(kHighDaysThreshold);
+}
