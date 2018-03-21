338f0ca8c1bfc303a5d238c99f2a50d71fb9ade5
Mark Cogan <marq@google.com>
2018-03-21 14:03:48 +0000

    [iOS] Handle nil in GridTransitionLayout setSelectedItem:
    In some cases, there may be no visible items in a grid, so the selected
    item will be nil, which is never a member of any collection. This CL
    updates the DCHECK condition in [GridTransitionLayout setSelectedItem:]
    to not trigger in this case.
    
    Bug: 822912
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I845355feee5f4af1d3443ff3d362aae45c05d9c9
    Reviewed-on: https://chromium-review.googlesource.com/973121
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544687}

--- ios/chrome/browser/ui/tab_grid/transitions/grid_transition_layout.mm
+++ ios/chrome/browser/ui/tab_grid/transitions/grid_transition_layout.mm
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)setSelectedItem:(GridTransitionLayoutItem*)selectedItem {
-  DCHECK([self.items containsObject:selectedItem]);
+  DCHECK(!selectedItem || [self.items containsObject:selectedItem]);
   _selectedItem = selectedItem;
 }
 
