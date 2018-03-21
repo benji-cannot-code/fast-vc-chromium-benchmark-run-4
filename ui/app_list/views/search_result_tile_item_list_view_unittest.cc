75d4d18d5d12fd29a01c3b2339e4991c59e0b606
Jiaquan He <hejq@google.com>
2018-03-21 01:33:30 +0000

    app_list: create mojo structures for SearchResult.
    This commit moves those data structures shared between Chrome and Ash
    from SearchResult to ash/public/cpp/app_list/app_list_types, creates
    corresponding mojo structures, and type-maps them.
    
    Bug: 733662
    Change-Id: I5af815ee789b2b9d4653689fbc619733a57e7801
    Reviewed-on: https://chromium-review.googlesource.com/965922
    Commit-Queue: Jiaquan He <hejq@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544611}

--- ui/app_list/views/search_result_tile_item_list_view_unittest.cc
+++ ui/app_list/views/search_result_tile_item_list_view_unittest.cc
@@ -76,8 +76,8 @@ class SearchResultTileItemListViewTest
       std::unique_ptr<TestSearchResult> result =
           std::make_unique<TestSearchResult>();
       result->set_result_id(base::StringPrintf("InstalledApp %d", i));
-      result->set_display_type(SearchResult::DISPLAY_TILE);
-      result->set_result_type(SearchResult::RESULT_INSTALLED_APP);
+      result->set_display_type(ash::SearchResultDisplayType::kTile);
+      result->set_result_type(ash::SearchResultType::kInstalledApp);
       result->set_title(
           base::UTF8ToUTF16(base::StringPrintf("InstalledApp %d", i)));
       results->Add(std::move(result));
@@ -89,8 +89,8 @@ class SearchResultTileItemListViewTest
         std::unique_ptr<TestSearchResult> result =
             std::make_unique<TestSearchResult>();
         result->set_result_id(base::StringPrintf("PlayStoreApp %d", i));
-        result->set_display_type(SearchResult::DISPLAY_TILE);
-        result->set_result_type(SearchResult::RESULT_PLAYSTORE_APP);
+        result->set_display_type(ash::SearchResultDisplayType::kTile);
+        result->set_result_type(ash::SearchResultType::kPlayStoreApp);
         result->set_title(
             base::UTF8ToUTF16(base::StringPrintf("PlayStoreApp %d", i)));
         result->SetRating(1 + i);
