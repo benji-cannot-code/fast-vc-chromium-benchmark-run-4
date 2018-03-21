3cb9723db123537c93dbc59a4f8f84072f44a7c4
Zhongyi Shi <zhongyi@chromium.org>
2018-03-21 23:52:42 +0000

    QUIC OnPathDegrading() will use new QuicAlarm instead of existing retransmission_alarm_.
    QuicConnection::path_degrading_alarm_ is reset whenever forward progress is made,
    i.e. a previously-unacked packet is acked, or when a first retransmittable packet
    is put on the wire. When the alarm fires, OnPathDegrading() will be called.
    relnote: flag-protected by FLAGS_quic_reloadable_flag_quic_path_degrading_alarm.
    
    Manually merge internal change: 189842846
    
    Change-Id: Iabfa10ddae6e4db3a7dfc77e87efb6618c476736
    Reviewed-on: https://chromium-review.googlesource.com/974307
    Commit-Queue: Zhongyi Shi <zhongyi@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544911}

--- net/quic/test_tools/quic_connection_peer.cc
+++ net/quic/test_tools/quic_connection_peer.cc
@@ -179,6 +179,12 @@ QuicAlarm* QuicConnectionPeer::GetRetransmittableOnWireAlarm(
 }
 
 // static
+QuicAlarm* QuicConnectionPeer::GetPathDegradingAlarm(
+    QuicConnection* connection) {
+  return connection->path_degrading_alarm_.get();
+}
+
+// static
 QuicPacketWriter* QuicConnectionPeer::GetWriter(QuicConnection* connection) {
   return connection->writer_;
 }
