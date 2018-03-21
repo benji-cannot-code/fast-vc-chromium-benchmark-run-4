4abedbddbabc3e1f23daf62d239aaa7da6aed326
Andrey Lushnikov <lushnikov@chromium.org>
2018-03-21 00:48:35 +0000

    DevTools(Persistence): teach Persistence to bind with dirty files
    This patch teaches Persistence2 to bind against original contents
    instead of working copies. As a result, not saving CSS files
    and consequently reloading the page doesn't affect the binding
    status.
    
    R=dgozman, einbinder
    
    Change-Id: Ied08a4ddfc6760b52b2386d5ffa31f939d672dc7
    Reviewed-on: https://chromium-review.googlesource.com/969748
    Commit-Queue: Andrey Lushnikov <lushnikov@chromium.org>
    Reviewed-by: Joel Einbinder <einbinder@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544606}

--- third_party/WebKit/Source/devtools/front_end/persistence/Persistence.js
+++ third_party/WebKit/Source/devtools/front_end/persistence/Persistence.js
@@ -92,6 +92,16 @@ Persistence.Persistence = class extends Common.Object {
 
     this._moveBreakpoints(binding.fileSystem, binding.network);
 
+    console.assert(!binding.fileSystem.isDirty() || !binding.network.isDirty());
+    if (binding.fileSystem.isDirty()) {
+      this._syncWorkingCopy(binding.fileSystem);
+    } else if (binding.network.isDirty()) {
+      this._syncWorkingCopy(binding.network);
+    } else if (binding.network.hasCommits()) {
+      binding.network.setWorkingCopy(binding.network.content());
+      this._syncWorkingCopy(binding.network);
+    }
+
     this._notifyBindingEvent(binding.network);
     this._notifyBindingEvent(binding.fileSystem);
     this.dispatchEventToListeners(Persistence.Persistence.Events.BindingCreated, binding);
@@ -149,6 +159,13 @@ Persistence.Persistence = class extends Common.Object {
    */
   _onWorkingCopyChanged(event) {
     const uiSourceCode = /** @type {!Workspace.UISourceCode} */ (event.data);
+    this._syncWorkingCopy(uiSourceCode);
+  }
+
+  /**
+   * @param {!Workspace.UISourceCode} uiSourceCode
+   */
+  _syncWorkingCopy(uiSourceCode) {
     const binding = uiSourceCode[Persistence.Persistence._binding];
     if (!binding || binding[Persistence.Persistence._muteWorkingCopy])
       return;
