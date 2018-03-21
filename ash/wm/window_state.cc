d4c0fd3d7f157ef31e98348244b65686090b491c
Qiang Xu <warx@google.com>
2018-03-21 19:00:01 +0000

    cros: do not clear kPreMinimizedShowStateKey in tablet mode
    changes:
    Do not clear kPreMinimizedShowStateKey in tablet mode, instead let it
    persist before/after tablet mode.
    
    Bug: 823175, 783310
    Test: manual test and added test coverage.
    Change-Id: Icec1675da9c7d1b5b955ae5c05a71d96547afeee
    Reviewed-on: https://chromium-review.googlesource.com/969455
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Qiang Xu <warx@google.com>
    Cr-Commit-Position: refs/heads/master@{#544789}

--- ash/wm/window_state.cc
+++ ash/wm/window_state.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/screen_util.h"
 #include "ash/shell.h"
 #include "ash/wm/default_state.h"
+#include "ash/wm/tablet_mode/tablet_mode_controller.h"
 #include "ash/wm/window_animations.h"
 #include "ash/wm/window_positioning_utils.h"
 #include "ash/wm/window_properties.h"
@@ -39,6 +40,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 namespace wm {
 namespace {
+
+bool IsTabletModeEnabled() {
+  return Shell::Get()
+      ->tablet_mode_controller()
+      ->IsTabletModeWindowManagerEnabled();
+}
 
 // A tentative class to set the bounds on the window.
 // TODO(oshima): Once all logic is cleaned up, move this to the real layout
@@ -534,6 +541,12 @@ void WindowState::AdjustSnappedBounds(gfx::Rect* bounds) {
 void WindowState::UpdateWindowPropertiesFromStateType() {
   ui::WindowShowState new_window_state =
       ToWindowShowState(current_state_->GetType());
+  // Clear |kPreMinimizedShowStateKey| property only when the window is actually
+  // Unminimized and not in tablet mode.
+  if (new_window_state != ui::SHOW_STATE_MINIMIZED && IsMinimized() &&
+      !IsTabletModeEnabled()) {
+    window()->ClearProperty(aura::client::kPreMinimizedShowStateKey);
+  }
   if (new_window_state != GetShowState()) {
     base::AutoReset<bool> resetter(&ignore_property_change_, true);
     window_->SetProperty(aura::client::kShowStateKey, new_window_state);
