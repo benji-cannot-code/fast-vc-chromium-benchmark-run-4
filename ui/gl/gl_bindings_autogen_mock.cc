6b1c6ba0f9cf7cfbacd2d2c69d9d9473d36050dc
Antoine Labour <piman@chromium.org>
2018-03-21 01:37:14 +0000

    Fix glGetDebugMessageLog bindings
    It returns GLuint, not void.
    
    Bug: 786591
    Cq-Include-Trybots: luci.chromium.try:linux_optional_gpu_tests_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I5d684c194d121cc0924ee25e377e49e18178cde4
    Reviewed-on: https://chromium-review.googlesource.com/967568
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544612}

--- ui/gl/gl_bindings_autogen_mock.cc
+++ ui/gl/gl_bindings_autogen_mock.cc
@@ -1501,7 +1501,7 @@ MockGLInterface::Mock_glGetBufferPointervRobustANGLE(GLenum target,
                                            params);
 }
 
-void GL_BINDING_CALL
+GLuint GL_BINDING_CALL
 MockGLInterface::Mock_glGetDebugMessageLog(GLuint count,
                                            GLsizei bufSize,
                                            GLenum* sources,
@@ -1511,11 +1511,11 @@ MockGLInterface::Mock_glGetDebugMessageLog(GLuint count,
                                            GLsizei* lengths,
                                            char* messageLog) {
   MakeFunctionUnique("glGetDebugMessageLog");
-  interface_->GetDebugMessageLog(count, bufSize, sources, types, ids,
-                                 severities, lengths, messageLog);
+  return interface_->GetDebugMessageLog(count, bufSize, sources, types, ids,
+                                        severities, lengths, messageLog);
 }
 
-void GL_BINDING_CALL
+GLuint GL_BINDING_CALL
 MockGLInterface::Mock_glGetDebugMessageLogKHR(GLuint count,
                                               GLsizei bufSize,
                                               GLenum* sources,
@@ -1525,8 +1525,8 @@ MockGLInterface::Mock_glGetDebugMessageLogKHR(GLuint count,
                                               GLsizei* lengths,
                                               char* messageLog) {
   MakeFunctionUnique("glGetDebugMessageLogKHR");
-  interface_->GetDebugMessageLog(count, bufSize, sources, types, ids,
-                                 severities, lengths, messageLog);
+  return interface_->GetDebugMessageLog(count, bufSize, sources, types, ids,
+                                        severities, lengths, messageLog);
 }
 
 GLenum GL_BINDING_CALL MockGLInterface::Mock_glGetError(void) {
