02c9ad943159110b92bb06f6a5473b97b2bc5f56
Chris Harrelson <chrishtr@chromium.org>
2018-03-21 04:43:24 +0000

    [PE] Apply overflow clip to scrolling contents layers' interest rect bounds when mapping to root.
    Currently, the overflow clip of these layers is not applied. This can lead to an overly large
    interest rect, in case when the overflow clip is significantly smaller than the clip of the
    root frame.
    
    This CL also makes regular box scrollers have the same behavior as composited LayoutViews.
    The latter receive the clip due to special logic in
    LayoutView::MapToVisualRectInAncestorSpaceInternal that intersects with the ViewRect.
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I1a585d0a69e2dbd82ecc515ea0782fea6f651d4b
    Reviewed-on: https://chromium-review.googlesource.com/967870
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544637}

--- third_party/WebKit/Source/core/paint/compositing/CompositedLayerMapping.h
+++ third_party/WebKit/Source/core/paint/compositing/CompositedLayerMapping.h
@@ -306,7 +306,8 @@ class CORE_EXPORT CompositedLayerMapping final : public GraphicsLayerClient {
 
   void UpdateScrollingBlockSelection();
 
-  void AdjustForCompositedScrolling(const GraphicsLayer*,
+  // Returns whether an adjustment happend.
+  bool AdjustForCompositedScrolling(const GraphicsLayer*,
                                     IntSize& offset) const;
 
   // Returns true for layers with scrollable overflow which have a background
