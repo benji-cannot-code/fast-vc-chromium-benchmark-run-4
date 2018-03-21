0d4642556f400e0f974d4f4182cf67b409ab7be4
chaopeng <chaopeng@chromium.org>
2018-03-21 19:03:48 +0000

    Update the force_enable_zoom state to recreated input router in RenderWidgetHostImpl
    This issue is caused by force_enable_zoom is not updated to the recreated input
    router when open the new tab.
    
    In this patch, we record the force_enable_zoom in RenderWidgetHostImpl, then
    update the state to new input router when we create.
    
    Bug: 821447
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I33308abe484de70f1e4d2189de4896a04b05d7ef
    Reviewed-on: https://chromium-review.googlesource.com/961781
    Commit-Queue: Jianpeng Chao <chaopeng@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544791}

--- content/browser/renderer_host/input/input_router_impl.h
+++ content/browser/renderer_host/input/input_router_impl.h
@@ -39,6 +39,8 @@ namespace content {
 
 class InputDispositionHandler;
 
+class MockRenderWidgetHost;
+
 class CONTENT_EXPORT InputRouterImplClient : public InputRouterClient {
  public:
   virtual mojom::WidgetInputHandler* GetWidgetInputHandler() = 0;
@@ -103,6 +105,7 @@ class CONTENT_EXPORT InputRouterImpl
 
  private:
   friend class InputRouterImplTest;
+  friend class MockRenderWidgetHost;
 
   // Keeps track of last position of touch points and sets MovementXY for them.
   void SetMovementXYForTouchPoints(blink::WebTouchEvent* event);
--- content/browser/renderer_host/input/touch_action_filter.h
+++ content/browser/renderer_host/input/touch_action_filter.h
@@ -16,6 +16,8 @@ class WebGestureEvent;
 
 namespace content {
 
+class MockRenderWidgetHost;
+
 // The TouchActionFilter is responsible for filtering scroll and pinch gesture
 // events according to the CSS touch-action values the renderer has sent for
 // each touch point.
@@ -54,6 +56,8 @@ class CONTENT_EXPORT TouchActionFilter {
   void SetForceEnableZoom(bool enabled) { force_enable_zoom_ = enabled; }
 
  private:
+  friend class MockRenderWidgetHost;
+
   bool ShouldSuppressManipulation(const blink::WebGestureEvent&);
   bool FilterManipulationEventAndResetState();
 
