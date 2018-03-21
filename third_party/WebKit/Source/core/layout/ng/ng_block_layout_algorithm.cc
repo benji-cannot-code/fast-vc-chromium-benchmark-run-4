eb330c07d67746f4720d7a4a394bdd077c1d8740
Aleks Totic <atotic@chromium.org>
2018-03-21 19:45:45 +0000

    [LayoutNG] Fix missing image icon
    Broken image was caused by not positioning of placed floats
    
    Bug: 635619
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: Ifb60bee7defeaa9e4ca2bacab9c6607b482b5e8f
    Reviewed-on: https://chromium-review.googlesource.com/973700
    Commit-Queue: Aleks Totic <atotic@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544805}

--- third_party/WebKit/Source/core/layout/ng/ng_block_layout_algorithm.cc
+++ third_party/WebKit/Source/core/layout/ng/ng_block_layout_algorithm.cc
@@ -960,12 +960,6 @@ bool NGBlockLayoutAlgorithm::HandleInflow(
         std::make_unique<NGExclusionSpace>(*layout_result->ExclusionSpace());
   }
 
-  // A line-box may have a list of floats which we add as children.
-  if (child.IsInline() &&
-      (container_builder_.BfcOffset() || ConstraintSpace().FloatsBfcOffset())) {
-    AddPositionedFloats(layout_result->PositionedFloats());
-  }
-
   // We have special behaviour for an empty block which gets pushed down due to
   // clearance, see comment inside ComputeInflowPosition.
   bool empty_block_affected_by_clearance = false;
@@ -1039,6 +1033,12 @@ bool NGBlockLayoutAlgorithm::HandleInflow(
     DCHECK(layout_result->ExclusionSpace());
     exclusion_space_ =
         std::make_unique<NGExclusionSpace>(*layout_result->ExclusionSpace());
+  }
+
+  // A line-box may have a list of floats which we add as children.
+  if (child.IsInline() &&
+      (container_builder_.BfcOffset() || ConstraintSpace().FloatsBfcOffset())) {
+    AddPositionedFloats(layout_result->PositionedFloats());
   }
 
   // We must have an actual fragment at this stage.
