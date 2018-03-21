37e357094f735d2a99733da641d50dde71cf0944
Vaclav Brozek <vabr@chromium.org>
2018-03-21 17:30:45 +0000

    Measure the use of progress bar when exporting passwords
    On Android, when the user exports passwords from Chrome's settings, a
    progress bar might be shown if the action takes too long. Once shown,
    it is also kept up for at least one second.
    
    The delaying logic in particular is rather complicated, and it is not
    clear how often it is used. Therefore this CL adds UMA to measure its
    use.
    
    Bug: 823695
    Change-Id: I074a123cbca0187b9c8da4ef9577a768aa9e3cbb
    Reviewed-on: https://chromium-review.googlesource.com/970343
    Commit-Queue: Vaclav Brozek <vabr@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544755}

--- chrome/android/javatests/src/org/chromium/chrome/browser/preferences/password/SavePasswordsPreferencesTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/preferences/password/SavePasswordsPreferencesTest.java
@@ -860,6 +860,10 @@ public class SavePasswordsPreferencesTest {
         MetricsUtils.HistogramDelta countDelta = new MetricsUtils.HistogramDelta(
                 "PasswordManager.ExportedPasswordsPerUserInCSV", 123);
 
+        MetricsUtils.HistogramDelta progressBarDelta = new MetricsUtils.HistogramDelta(
+                "PasswordManager.Android.ExportPasswordsProgressBarUsage",
+                SavePasswordsPreferences.PROGRESS_NOT_SHOWN);
+
         // Confirm the export warning to fire the sharing intent.
         Espresso.onView(withText(R.string.save_password_preferences_export_action_title))
                 .perform(click());
@@ -872,6 +876,7 @@ public class SavePasswordsPreferencesTest {
 
         Assert.assertEquals(1, successDelta.getDelta());
         Assert.assertEquals(1, countDelta.getDelta());
+        Assert.assertEquals(1, progressBarDelta.getDelta());
     }
 
     /**
@@ -1122,6 +1127,10 @@ public class SavePasswordsPreferencesTest {
         Espresso.onView(withText(R.string.settings_passwords_preparing_export))
                 .check(matches(isDisplayed()));
 
+        MetricsUtils.HistogramDelta progressBarDelta = new MetricsUtils.HistogramDelta(
+                "PasswordManager.Android.ExportPasswordsProgressBarUsage",
+                SavePasswordsPreferences.PROGRESS_HIDDEN_DELAYED);
+
         // Now pretend that passwords have been serialized.
         mHandler.getExportCallback().onResult(new byte[] {5, 6, 7}, 12);
 
@@ -1140,6 +1149,7 @@ public class SavePasswordsPreferencesTest {
                         allOf(hasAction(equalTo(Intent.ACTION_SEND)), hasType("text/csv"))))));
 
         Intents.release();
+        Assert.assertEquals(1, progressBarDelta.getDelta());
     }
 
     /**
@@ -1179,6 +1189,10 @@ public class SavePasswordsPreferencesTest {
         Espresso.onView(withText(R.string.settings_passwords_preparing_export))
                 .check(matches(isDisplayed()));
 
+        MetricsUtils.HistogramDelta progressBarDelta = new MetricsUtils.HistogramDelta(
+                "PasswordManager.Android.ExportPasswordsProgressBarUsage",
+                SavePasswordsPreferences.PROGRESS_HIDDEN_DIRECTLY);
+
         // Now pretend that passwords have been serialized.
         allowProgressBarToBeHidden(preferences);
         mHandler.getExportCallback().onResult(new byte[] {5, 6, 7}, 12);
@@ -1193,6 +1207,7 @@ public class SavePasswordsPreferencesTest {
                         allOf(hasAction(equalTo(Intent.ACTION_SEND)), hasType("text/csv"))))));
 
         Intents.release();
+        Assert.assertEquals(1, progressBarDelta.getDelta());
     }
 
     /**
