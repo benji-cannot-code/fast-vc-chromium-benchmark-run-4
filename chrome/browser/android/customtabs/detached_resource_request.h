dc687a18962d0f9fd1a463022a43b06618178011
Benoit Lize <lizeb@chromium.org>
2018-03-21 17:58:37 +0000

    customtabs: Count redirects for detached requests.
    Bug: 816837
    Change-Id: Ic2d2b600c5ab2483f85c6be1447fd091c8aec1fe
    Reviewed-on: https://chromium-review.googlesource.com/971222
    Commit-Queue: Benoit L <lizeb@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544766}

--- chrome/browser/android/customtabs/detached_resource_request.h
+++ chrome/browser/android/customtabs/detached_resource_request.h
@@ -15,13 +15,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/url_request.h"
 #include "url/gurl.h"
 
-namespace network {
-class SimpleURLLoader;
-}
-
 namespace content {
 class BrowserContext;
 }
+
+namespace net {
+struct RedirectInfo;
+}
+
+namespace network {
+class SimpleURLLoader;
+struct ResourceResponseHead;
+}  // namespace network
 
 namespace customtabs {
 
@@ -55,6 +60,8 @@ class DetachedResourceRequest {
 
   static void Start(std::unique_ptr<DetachedResourceRequest> request,
                     content::BrowserContext* browser_context);
+  void OnRedirectCallback(const net::RedirectInfo& redirect_info,
+                          const network::ResourceResponseHead& response_head);
   void OnResponseCallback(std::unique_ptr<std::string> response_body);
 
   const GURL url_;
@@ -62,6 +69,7 @@ class DetachedResourceRequest {
   base::TimeTicks start_time_;
   OnResultCallback cb_;
   std::unique_ptr<network::SimpleURLLoader> url_loader_;
+  int redirects_;
 
   DISALLOW_COPY_AND_ASSIGN(DetachedResourceRequest);
 };
