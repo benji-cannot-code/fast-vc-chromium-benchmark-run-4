13042eb9d811da24e8a5e04f461fb1dd4ba87897
Brian White <bcwhite@chromium.org>
2018-03-21 13:21:46 +0000

    Strip path components from extensions URLs.
    Bug: 810766
    Change-Id: Ib2804c6218211e75ca2f98d6ddc345740e599d32
    Reviewed-on: https://chromium-review.googlesource.com/969406
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Bryan McQuade <bmcquade@chromium.org>
    Commit-Queue: Brian White <bcwhite@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544678}

--- components/ukm/ukm_service_unittest.cc
+++ components/ukm/ukm_service_unittest.cc
@@ -1140,6 +1140,8 @@ TEST_F(UkmServiceTest, SanitizeChromeUrlParams) {
       {"http://google.ca/?foo=bar", "http://google.ca/?foo=bar"},
       {"https://google.ca/?foo=bar", "https://google.ca/?foo=bar"},
       {"ftp://google.ca/?foo=bar", "ftp://google.ca/?foo=bar"},
+      {"chrome-extension://bhcnanendmgjjeghamaccjnochlnhcgj/foo.html?a=b",
+       "chrome-extension://bhcnanendmgjjeghamaccjnochlnhcgj/"},
   };
 
   for (const auto& test : test_cases) {
@@ -1147,10 +1149,13 @@ TEST_F(UkmServiceTest, SanitizeChromeUrlParams) {
 
     UkmService service(&prefs_, &client_);
     TestRecordingHelper recorder(&service);
+    service.SetIsWebstoreExtensionCallback(
+        base::BindRepeating(&TestIsWebstoreExtension));
+
     EXPECT_EQ(0, GetPersistedLogCount());
     service.Initialize();
     task_runner_->RunUntilIdle();
-    service.EnableRecording(/*extensions=*/false);
+    service.EnableRecording(/*extensions=*/true);
     service.EnableReporting();
 
     auto id = GetWhitelistedSourceId(0);
