c7580691e5a04fc5a6a0bc5d54318e7504afd1ec
Marc Treib <treib@chromium.org>
2018-03-21 17:27:27 +0000

    Add SyncService::GetUploadToGoogleState
    It returns whether uploading data to Google is enabled for a given
    ModelType, i.e. whether Sync is active and not in an error state.
    There are three possible return values: INITIALIZING (enabled in
    principle, but we don't know yet whether we'll encounter e.g. an
    auth error), ACTIVE (data is being uploaded to Google), or
    NOT_ACTIVE (uploading is either disabled entirely or in a permanent
    auth error state).
    
    Change-Id: I86894453bba17cfb6cd94bb3a5c82f915750ea06
    Reviewed-on: https://chromium-review.googlesource.com/968821
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544751}

--- components/sync/driver/sync_service.cc
+++ components/sync/driver/sync_service.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/sync/driver/sync_service.h"
 
-#include "components/sync/engine/sync_manager.h"
-
 namespace syncer {
 
 SyncSetupInProgressHandle::SyncSetupInProgressHandle(base::Closure on_destroy)
