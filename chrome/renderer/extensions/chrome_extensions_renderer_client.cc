d203fb93451e59a40a056f43cab0c88cdb611a3e
Nasko Oskov <nasko@chromium.org>
2018-03-21 19:00:46 +0000

    Allow content script requests to attach SameSite cookies.
    When a content script is injected into a document, it can make requests
    to origins in its manifest. With recent changes to allow Chrome to block
    cross-site documents, the initiator of content scripts requests was
    changed to reflect the extension origin. This change meant that SameSite
    cookies were not attached to those requests.
    
    This CL introduces a check for content scripts making subresource requests
    and tags those requests to allow attaching SameSite cookies.
    
    Bug: 796480
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I021156e346035267d98802cfb0205fc419e970bd
    Reviewed-on: https://chromium-review.googlesource.com/896690
    Commit-Queue: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544790}

--- chrome/renderer/extensions/chrome_extensions_renderer_client.cc
+++ chrome/renderer/extensions/chrome_extensions_renderer_client.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/constants.h"
 #include "extensions/common/extension.h"
 #include "extensions/common/extension_set.h"
+#include "extensions/common/permissions/permissions_data.h"
 #include "extensions/common/switches.h"
 #include "extensions/renderer/dispatcher.h"
 #include "extensions/renderer/extension_frame_helper.h"
@@ -40,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/web/WebDocument.h"
 #include "third_party/WebKit/public/web/WebLocalFrame.h"
 #include "third_party/WebKit/public/web/WebPluginParams.h"
+#include "url/origin.h"
 
 using extensions::Extension;
 
@@ -216,19 +218,40 @@ bool ChromeExtensionsRendererClient::AllowPopup() {
   }
 }
 
-bool ChromeExtensionsRendererClient::WillSendRequest(
+void ChromeExtensionsRendererClient::WillSendRequest(
     blink::WebLocalFrame* frame,
     ui::PageTransition transition_type,
     const blink::WebURL& url,
-    GURL* new_url) {
+    base::Optional<url::Origin> initiator_origin,
+    GURL* new_url,
+    bool* attach_same_site_cookies) {
+  if (initiator_origin &&
+      initiator_origin.value().scheme() == extensions::kExtensionScheme) {
+    const extensions::RendererExtensionRegistry* extension_registry =
+        extensions::RendererExtensionRegistry::Get();
+    const Extension* extension =
+        extension_registry->GetByID(initiator_origin.value().host());
+    if (extension) {
+      int tab_id = extensions::ExtensionFrameHelper::Get(
+                       content::RenderFrame::FromWebFrame(frame))
+                       ->tab_id();
+      GURL request_url(url);
+      if (extension->permissions_data()->GetPageAccess(extension, request_url,
+                                                       tab_id, nullptr) ==
+              extensions::PermissionsData::ACCESS_ALLOWED ||
+          extension->permissions_data()->GetContentScriptAccess(
+              extension, request_url, tab_id, nullptr) ==
+              extensions::PermissionsData::ACCESS_ALLOWED) {
+        *attach_same_site_cookies = true;
+      }
+    }
+  }
+
   if (url.ProtocolIs(extensions::kExtensionScheme) &&
       !resource_request_policy_->CanRequestResource(GURL(url), frame,
                                                     transition_type)) {
     *new_url = GURL(chrome::kExtensionInvalidRequestURL);
-    return true;
   }
-
-  return false;
 }
 
 void ChromeExtensionsRendererClient::SetExtensionDispatcherForTest(
