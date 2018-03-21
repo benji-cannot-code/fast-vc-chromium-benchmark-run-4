253175af25ec591d53f23ec932d7728451511a7d
Yuwei Huang <yuweih@chromium.org>
2018-03-21 20:44:19 +0000

    [CRD iOS] Fix a crash when session is disconnected while the cursor is still flinging
    The issue is that DisconnectFromHost() invalidates gestureInterpreter
    before ClientGesture is destroyed (which is destroyed when the host view
    is removed). If the user is interacting with the view after
    gestureInterpreter is destroyed but before the view is removed then
    ClientGesture will call gestureInterpreter's methods on null, which
    crashes the app.
    
    This CL fixes this by always checking _client and
    _client.gestureInterpreter before using them.
    
    Bug: 823983
    Change-Id: I3889c95daf28b84bbf9aecd26f0024dcb15adbe4
    Reviewed-on: https://chromium-review.googlesource.com/972631
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544830}

--- remoting/client/gesture_interpreter.h
+++ remoting/client/gesture_interpreter.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/macros.h"
+#include "base/memory/weak_ptr.h"
 #include "remoting/client/input/touch_input_strategy.h"
 #include "remoting/client/ui/desktop_viewport.h"
 #include "remoting/client/ui/fling_animation.h"
@@ -78,6 +80,8 @@ class GestureInterpreter {
   void OnSurfaceSizeChanged(int width, int height);
   void OnDesktopSizeChanged(int width, int height);
 
+  base::WeakPtr<GestureInterpreter> GetWeakPtr();
+
  private:
   void PanWithoutAbortAnimations(float translation_x, float translation_y);
 
@@ -111,9 +115,10 @@ class GestureInterpreter {
   FlingAnimation pan_animation_;
   FlingAnimation scroll_animation_;
 
+  base::WeakPtrFactory<GestureInterpreter> weak_factory_;
+
   // GestureInterpreter is neither copyable nor movable.
-  GestureInterpreter(const GestureInterpreter&) = delete;
-  GestureInterpreter& operator=(const GestureInterpreter&) = delete;
+  DISALLOW_COPY_AND_ASSIGN(GestureInterpreter);
 };
 
 }  // namespace remoting
