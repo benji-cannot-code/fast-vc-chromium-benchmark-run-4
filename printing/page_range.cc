f8d063684dd3673e95271c4eabfbb2d98a80ba40
Lei Zhang <thestig@chromium.org>
2018-03-22 16:22:14 +0000

    Remove a shadow variable in printing::PageRange.
    Change-Id: I269b9b0249500cee9ea4c123951d89c347f791e3
    Reviewed-on: https://chromium-review.googlesource.com/974319
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545106}

--- printing/page_range.cc
+++ printing/page_range.cc
@@ -9,20 +9,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <set>
 
-namespace {
-const std::size_t kMaxNumberOfPages = 100000;
-}
-
 namespace printing {
 
-/* static */
+// static
 std::vector<int> PageRange::GetPages(const PageRanges& ranges) {
   // TODO(vitalybuka): crbug.com/95548 Remove this method as part fix.
   std::set<int> pages;
-  for (unsigned i = 0; i < ranges.size(); ++i) {
-    const PageRange& range = ranges[i];
+  for (const PageRange& range : ranges) {
     // Ranges are inclusive.
     for (int i = range.from; i <= range.to; ++i) {
+      static constexpr size_t kMaxNumberOfPages = 100000;
       pages.insert(i);
       if (pages.size() >= kMaxNumberOfPages)
         return std::vector<int>(pages.begin(), pages.end());
--- printing/page_range.h
+++ printing/page_range.h
@@ -8,13 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
-#include "printing_export.h"
+#include "printing/printing_export.h"
 
 namespace printing {
 
 struct PageRange;
 
-typedef std::vector<PageRange> PageRanges;
+using PageRanges = std::vector<PageRange>;
 
 // Print range is inclusive. To select one page, set from == to.
 struct PRINTING_EXPORT PageRange {
