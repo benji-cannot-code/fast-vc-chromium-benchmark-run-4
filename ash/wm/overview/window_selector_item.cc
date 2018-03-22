64e2b50af33ff2b40ef0fb7ed8218062bc47b714
Sammie Quon <sammiequon@google.com>
2018-03-22 01:03:08 +0000

    overview: Modify how multi display overview works for new ui.
    "No recent items" only shows up on the primary window grid now.
    If all the windows on a grid are closed, the grid is still kept alive
    until all windows on all grids are closed, so that the wallpaper blur
    and opacity change are still seen.
    
    Previously, the grid would destroy as soon as all its items were closed,
    removing the blur and fade from that display, but the system was still
    in overview mode because another grid was active.
    
    If old UI is enabled, the behaviour is unchanged.
    
    Test: WindowSelectorTest.OverviewNoWindowsIndicatorMultiDisplay
    Bug: 804979
    Change-Id: I2ed9d4d13301a14ddf7a2ed39a299a5b8183eb2f
    Reviewed-on: https://chromium-review.googlesource.com/967367
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544931}

--- ash/wm/overview/window_selector_item.cc
+++ ash/wm/overview/window_selector_item.cc
@@ -803,7 +803,8 @@ void WindowSelectorItem::SetBounds(const gfx::Rect& target_bounds,
 
 void WindowSelectorItem::SetSelected(bool selected) {
   selected_ = selected;
-  background_view_->AnimateBackgroundOpacity(selected ? 0.f : kHeaderOpacity);
+  if (!IsNewOverviewUi())
+    background_view_->AnimateBackgroundOpacity(selected ? 0.f : kHeaderOpacity);
 }
 
 void WindowSelectorItem::SendAccessibleSelectionEvent() {
