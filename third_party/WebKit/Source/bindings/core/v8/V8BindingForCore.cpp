fa4ddf422045be208104ff7f9308573342b42ffa
Rob Buis <rob.buis@samsung.com>
2018-03-22 21:50:14 +0000

    Remove superflous include from r545078
    Should have removed this before landing.
    
    Bug: 823812
    Change-Id: Icfb4e9c145d2a6e3da98d4ee3b7cf7ecc62c285b
    Reviewed-on: https://chromium-review.googlesource.com/975921
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Rob Buis <rob.buis@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#545264}

--- third_party/WebKit/Source/bindings/core/v8/V8BindingForCore.cpp
+++ third_party/WebKit/Source/bindings/core/v8/V8BindingForCore.cpp
@@ -44,7 +44,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/WindowProxy.h"
 #include "bindings/core/v8/WorkerOrWorkletScriptController.h"
 #include "bindings/core/v8/custom/V8CustomXPathNSResolver.h"
-#include "build/build_config.h"
 #include "core/dom/Document.h"
 #include "core/dom/Element.h"
 #include "core/dom/QualifiedName.h"
