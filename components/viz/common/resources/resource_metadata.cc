c139563d1bc111934d11e0e4f5766c77209c6ec1
Peng Huang <penghuang@chromium.org>
2018-03-22 17:51:17 +0000

    cc::DisplayResourceProivder: Add method GetResourceMetadataForExternalUse
    This method will be used by viz::SkiaRenderer to get a resource
    metadata, and then the viz::SkiaRenderer will make a SkImage by
    SkDeferredDisplayListRecorder::makePromiseTexture(), and then draw the
    SkImage to a Skia DDL which will be played back on GPU thread later.
    
    This CL is part of CL https://crrev.com/c/947047 which makes
    SkiaRenderer use Skia DDL to render frames.
    
    Bug: 824382
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ie182ee9f8ebba0f6f838e6fb191f84b97ee43545
    Reviewed-on: https://chromium-review.googlesource.com/973806
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545145}

---
+++ components/viz/common/resources/resource_metadata.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/viz/common/resources/resource_metadata.h"
+
+namespace viz {
+
+ResourceMetadata::ResourceMetadata() = default;
+
+ResourceMetadata::ResourceMetadata(ResourceMetadata&& other) = default;
+
+ResourceMetadata::~ResourceMetadata() = default;
+
+ResourceMetadata& ResourceMetadata::operator=(ResourceMetadata&& other) =
+    default;
+
+}  // namespace viz
