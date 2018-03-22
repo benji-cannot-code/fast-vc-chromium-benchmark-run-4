6dd45fc0b77478d2bd43c370d9f1967bb1cb385a
Miriam Gershenson <mgersh@chromium.org>
2018-03-22 22:54:47 +0000

    Reduce load flags that affect the host cache
    Previously, all three of the load flags that prevent use of the HTTP
    cache also prevented use of the DNS cache when using the socket pools.
    This is not the desired behavior for most consumers of those load flags.
    LOAD_BYPASS_CACHE has some consumers that might be depending on the
    behavior, so it stays the same. LOAD_DISABLE_CACHE and
    LOAD_VALIDATE_CACHE will not affect the host cache.
    
    Details: https://docs.google.com/document/d/1CuGd8ZnGd56L0wpX1jkB96Lm6IF9fF409h234AqjtNw/edit?usp=sharing
    
    Bug: 743235, 621958
    Change-Id: I4560cc3bf96da2f19c10a5c68179d9294012f549
    Reviewed-on: https://chromium-review.googlesource.com/976539
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Miriam Gershenson <mgersh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545279}

--- net/base/load_flags_list.h
+++ net/base/load_flags_list.h
@@ -17,10 +17,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 LOAD_FLAG(NORMAL, 0)
 
-// This is "normal reload", meaning an if-none-match/if-modified-since query
+// This is "normal reload", meaning an if-none-match/if-modified-since query. It
+// has no effect on the host cache.
 LOAD_FLAG(VALIDATE_CACHE, 1 << 0)
 
-// This is "shift-reload", meaning a "pragma: no-cache" end-to-end fetch
+// This is "shift-reload", meaning a "pragma: no-cache" end-to-end fetch. It
+// also disables use of the host cache for resolutions that go through the
+// socket pools.
 LOAD_FLAG(BYPASS_CACHE, 1 << 1)
 
 // This is a back/forward style navigation where the cached content should
@@ -32,7 +35,7 @@ LOAD_FLAG(SKIP_CACHE_VALIDATION, 1 << 2)
 LOAD_FLAG(ONLY_FROM_CACHE, 1 << 3)
 
 // This is a navigation that will not use the cache at all.  It does not
-// impact the HTTP request headers.
+// impact the HTTP request headers or use of the host cache.
 LOAD_FLAG(DISABLE_CACHE, 1 << 4)
 
 // If present, causes certificate revocation checks to be skipped on secure
