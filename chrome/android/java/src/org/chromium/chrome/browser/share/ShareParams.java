a4c5bc2f83d728026b95e8ba325f1ae2d469e113
Lei Tian <ltian@chromium.org>
2018-03-22 19:22:03 +0000

    Fix Browser Actions crash on showing share dialog for preLMR1 device
    Browser Actons crashes on preLMR1 devices when showing the share dialog.
    This is because on preLMR1, share dialog is a Android dialog. When
    dialog is shown, its attached activity is destroy.
    
    To fix it, BrowserActionActivity would not be closed if share option is
    selected on preLMR1 devices. The activity would be closed when an item
    is selected form the dialog.
    
    Bug: 821631
    Change-Id: Ie40120e9aab2b7b45cc84dc532888c02611e5ec6
    Reviewed-on: https://chromium-review.googlesource.com/969715
    Commit-Queue: Lei Tian <ltian@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545188}

--- chrome/android/java/src/org/chromium/chrome/browser/share/ShareParams.java
+++ chrome/android/java/src/org/chromium/chrome/browser/share/ShareParams.java
@@ -57,10 +57,14 @@ public class ShareParams {
     /** The package name of the app who requests for share. If Null, it is requested by Chrome */
     private final String mSourcePackageName;
 
+    /** The {@link Runnable} called when the share dialog is dismissed. */
+    @Nullable
+    private final Runnable mOnDialogDismissed;
+
     private ShareParams(boolean shareDirectly, boolean saveLastUsed, Activity activity,
             String title, String text, String url, @Nullable Uri offlineUri,
             @Nullable Uri screenshotUri, @Nullable TargetChosenCallback callback,
-            @Nullable String sourcePackageName) {
+            @Nullable String sourcePackageName, @Nullable Runnable onDialogDismissed) {
         mShareDirectly = shareDirectly;
         mSaveLastUsed = saveLastUsed;
         mActivity = activity;
@@ -71,6 +75,7 @@ public class ShareParams {
         mScreenshotUri = screenshotUri;
         mCallback = callback;
         mSourcePackageName = sourcePackageName;
+        mOnDialogDismissed = onDialogDismissed;
     }
 
     /**
@@ -147,6 +152,14 @@ public class ShareParams {
         return mSourcePackageName;
     }
 
+    /**
+     * @return The {@link Runnable} to be called when the share dialog is dismissed.
+     */
+    @Nullable
+    public Runnable getOnDialogDismissed() {
+        return mOnDialogDismissed;
+    }
+
     /** The builder for {@link ShareParams} objects. */
     public static class Builder {
         private boolean mShareDirectly;
@@ -160,6 +173,7 @@ public class ShareParams {
         private TargetChosenCallback mCallback;
         private String mSourcePackageName;
         private boolean mIsExternalUrl;
+        private Runnable mOnDialogDismissed;
 
         public Builder(@NonNull Activity activity, @NonNull String title, @NonNull String url) {
             mActivity = activity;
@@ -240,6 +254,14 @@ public class ShareParams {
             return this;
         }
 
+        /**
+         * Set a runnable to be called when the share dialog is dismissed.
+         */
+        public Builder setOnDialogDismissed(Runnable onDialogDismised) {
+            mOnDialogDismissed = onDialogDismised;
+            return this;
+        }
+
         /** @return A fully constructed {@link ShareParams} object. */
         public ShareParams build() {
             if (!TextUtils.isEmpty(mUrl)) {
@@ -254,7 +276,7 @@ public class ShareParams {
                 }
             }
             return new ShareParams(mShareDirectly, mSaveLastUsed, mActivity, mTitle, mText, mUrl,
-                    mOfflineUri, mScreenshotUri, mCallback, mSourcePackageName);
+                    mOfflineUri, mScreenshotUri, mCallback, mSourcePackageName, mOnDialogDismissed);
         }
     }
 }
