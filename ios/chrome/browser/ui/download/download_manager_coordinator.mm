e1039503389279be4fbdc98285312f07e09d9c3c
Eugene But <eugenebut@google.com>
2018-03-22 15:58:27 +0000

    Gracefully handle failure to present Open In... menu from New Download Manager.
    On iOS 10, where Files app does not exist it is possible for
    UIDocumentInteractionController to not present Open In... menu.
    In this case Download Manager should show the prompt to install Google
    Drive app.
    
    Bug: 823702
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I393c8724186deaefd3dc192a61759c24799dd1d5
    Reviewed-on: https://chromium-review.googlesource.com/974748
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545098}

--- ios/chrome/browser/ui/download/download_manager_coordinator.mm
+++ ios/chrome/browser/ui/download/download_manager_coordinator.mm
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/download/google_drive_app_util.h"
 #import "ios/chrome/browser/installation_notifier.h"
 #import "ios/chrome/browser/store_kit/store_kit_coordinator.h"
+#import "ios/chrome/browser/ui/alert_coordinator/alert_coordinator.h"
 #import "ios/chrome/browser/ui/download/download_manager_mediator.h"
 #import "ios/chrome/browser/ui/download/download_manager_view_controller.h"
 #import "ios/chrome/browser/ui/presenters/contained_presenter.h"
@@ -82,6 +83,10 @@ class UnopenedDownloadsTracker : public web::DownloadTaskObserver {
   UIDocumentInteractionController* _openInController;
   DownloadManagerMediator _mediator;
   StoreKitCoordinator* _storeKitCoordinator;
+  // Coordinator for displaying the alert informing the user that no application
+  // on the device can open the file. The alert offers the user to install
+  // Google Drive app.
+  AlertCoordinator* _installDriveAlertCoordinator;
   UnopenedDownloadsTracker _unopenedDownloads;
 }
 @end
@@ -125,6 +130,8 @@ class UnopenedDownloadsTracker : public web::DownloadTaskObserver {
 
   [_storeKitCoordinator stop];
   _storeKitCoordinator = nil;
+  [_installDriveAlertCoordinator stop];
+  _installDriveAlertCoordinator = nil;
 }
 
 - (UIViewController*)viewController {
@@ -234,19 +241,7 @@ class UnopenedDownloadsTracker : public web::DownloadTaskObserver {
 
 - (void)installDriveForDownloadManagerViewController:
     (DownloadManagerViewController*)controller {
-  if (!_storeKitCoordinator) {
-    _storeKitCoordinator = [[StoreKitCoordinator alloc]
-        initWithBaseViewController:self.baseViewController];
-    _storeKitCoordinator.iTunesItemIdentifier =
-        kGoogleDriveITunesItemIdentifier;
-  }
-  [_storeKitCoordinator start];
-  [controller setInstallDriveButtonVisible:NO animated:YES];
-
-  [[InstallationNotifier sharedInstance]
-      registerForInstallationNotifications:self
-                              withSelector:@selector(didInstallGoogleDriveApp)
-                                 forScheme:kGoogleDriveAppURLScheme];
+  [self presentStoreKitForGoogleDriveApp];
 }
 
 - (void)downloadManagerViewControllerDidStartDownload:
@@ -270,7 +265,12 @@ class UnopenedDownloadsTracker : public web::DownloadTaskObserver {
       [_openInController presentOpenInMenuFromRect:layoutGuide.layoutFrame
                                             inView:layoutGuide.owningView
                                           animated:YES];
-  DCHECK(menuShown);
+
+  // No application on this device can open the file. Typically happens on
+  // iOS 10, where Files app does not exist.
+  if (!menuShown) {
+    [self didFailOpenInMenuPresentation];
+  }
 }
 
 #pragma mark - Private
@@ -320,6 +320,54 @@ class UnopenedDownloadsTracker : public web::DownloadTaskObserver {
 - (void)didInstallGoogleDriveApp {
   base::RecordAction(
       base::UserMetricsAction(kDownloadManagerGoogleDriveInstalled));
+}
+
+// Called when Open In... menu was not presented. This method shows the alert
+// which offers the user to install Google Drive app.
+- (void)didFailOpenInMenuPresentation {
+  NSString* title =
+      l10n_util::GetNSString(IDS_IOS_DOWNLOAD_MANAGER_UNABLE_TO_OPEN_FILE);
+  NSString* message =
+      l10n_util::GetNSString(IDS_IOS_DOWNLOAD_MANAGER_NO_APP_MESSAGE);
+
+  _installDriveAlertCoordinator = [[AlertCoordinator alloc]
+      initWithBaseViewController:self.baseViewController
+                           title:title
+                         message:message];
+
+  NSString* googleDriveButtonTitle =
+      l10n_util::GetNSString(IDS_IOS_DOWNLOAD_MANAGER_UPLOAD_TO_GOOGLE_DRIVE);
+  __weak DownloadManagerCoordinator* weakSelf = self;
+  [_installDriveAlertCoordinator
+      addItemWithTitle:googleDriveButtonTitle
+                action:^{
+                  [weakSelf presentStoreKitForGoogleDriveApp];
+                }
+                 style:UIAlertActionStyleDefault];
+
+  [_installDriveAlertCoordinator
+      addItemWithTitle:l10n_util::GetNSString(IDS_CANCEL)
+                action:nil
+                 style:UIAlertActionStyleCancel];
+
+  [_installDriveAlertCoordinator start];
+}
+
+// Presents StoreKit dialog for Google Drive application.
+- (void)presentStoreKitForGoogleDriveApp {
+  if (!_storeKitCoordinator) {
+    _storeKitCoordinator = [[StoreKitCoordinator alloc]
+        initWithBaseViewController:self.baseViewController];
+    _storeKitCoordinator.iTunesItemIdentifier =
+        kGoogleDriveITunesItemIdentifier;
+  }
+  [_storeKitCoordinator start];
+  [_viewController setInstallDriveButtonVisible:NO animated:YES];
+
+  [[InstallationNotifier sharedInstance]
+      registerForInstallationNotifications:self
+                              withSelector:@selector(didInstallGoogleDriveApp)
+                                 forScheme:kGoogleDriveAppURLScheme];
 }
 
 @end
--- ios/chrome/test/fakes/fake_document_interaction_controller.h
+++ ios/chrome/test/fakes/fake_document_interaction_controller.h
@@ -22,6 +22,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @property(nonatomic, weak) id<UIDocumentInteractionControllerDelegate> delegate;
 
+// Whether or not this controller can present Open In... menu. Defaults to YES.
+@property(nonatomic) BOOL presentsOpenInMenu;
+
 // Menu that is currently being presented.
 @property(nonatomic, readonly) OpenInMenu* presentedOpenInMenu;
 
