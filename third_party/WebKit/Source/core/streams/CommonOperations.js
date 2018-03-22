ca74413472d601651336e496f4075a98669988d5
Adam Rice <ricea@chromium.org>
2018-03-22 11:21:04 +0000

    Update TransformStream to latest standard version
    Update TransformStream to match the standard version
    https://github.com/whatwg/streams/commit/7b8dffe8ec270bb918340e011349e2c1b5521a15.
    
    Significant changes:
    
    Method lookups are now cached at construction time. Changing
    underlyingSink.write after construction will no longer do anything. This
    is https://github.com/whatwg/streams/pull/860.
    
    When the readable side is cancelled, the stored error on the writable side will
    be set to the |reason| that was passed to cancel(), rather than a TypeError.
    This is https://github.com/whatwg/streams/pull/903.
    
    Bug: 780689, 820387
    
    Change-Id: I33c59541eea1bfc5f96722bce750ba4fa7ff96ee
    Reviewed-on: https://chromium-review.googlesource.com/964112
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545038}

--- third_party/WebKit/Source/core/streams/CommonOperations.js
+++ third_party/WebKit/Source/core/streams/CommonOperations.js
@@ -194,7 +194,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // Internal utility functions. Not exported.
   const callFunction = v8.uncurryThis(global.Function.prototype.call);
   const errTmplMustBeFunctionOrUndefined = name =>
-        `${name} must be a function or undefined`;
+      `${name} must be a function or undefined`;
   const Promise_resolve = Promise.resolve.bind(Promise);
   const Promise_reject = Promise.reject.bind(Promise);
   const Function_bind = v8.uncurryThis(global.Function.prototype.bind);
@@ -214,13 +214,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     // assert(underlyingObject !== undefined,
     //        'underlyingObject is not undefined.');
     // assert(IsPropertyKey(methodName),
-    //        '! IsPropertyKey(methodName) is true.');
+    // '! IsPropertyKey(methodName) is true.');
     // assert(algoArgCount === 0 || algoArgCount === 1,
-    //        'algoArgCount is 0 or 1.');
-    // assert(typeof methodNameForError === 'string',
-    //        'methodNameForError is a string');
-    const method = resolveMethod(underlyingObject, methodName,
-                                 methodNameForError);
+    // 'algoArgCount is 0 or 1.');
+    // assert(
+    //     typeof methodNameForError === 'string',
+    //     'methodNameForError is a string');
+    const method =
+        resolveMethod(underlyingObject, methodName, methodNameForError);
     // The implementation uses bound functions rather than lambdas where
     // possible to give the compiler the maximum opportunity to optimise.
     if (method === undefined) {
@@ -235,27 +236,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 
   function CreateAlgorithmFromUnderlyingMethodPassingController(
-      underlyingObject, methodName,  algoArgCount, controller,
+      underlyingObject, methodName, algoArgCount, controller,
       methodNameForError) {
     // assert(underlyingObject !== undefined,
     //        'underlyingObject is not undefined.');
     // assert(IsPropertyKey(methodName),
-    //        '! IsPropertyKey(methodName) is true.');
+    // '! IsPropertyKey(methodName) is true.');
     // assert(algoArgCount === 0 || algoArgCount === 1,
-    //        'algoArgCount is 0 or 1.');
-    // assert(typeof controller === 'object'
-    //       'controller is an object');
-    // assert(typeof methodNameForError === 'string',
-    //        'methodNameForError is a string');
-    const method = resolveMethod(underlyingObject, methodName,
-                                 methodNameForError);
+    // 'algoArgCount is 0 or 1.');
+    // assert(typeof controller === 'object', 'controller is an object');
+    // assert(
+    //     typeof methodNameForError === 'string',
+    //     'methodNameForError is a string');
+    const method =
+        resolveMethod(underlyingObject, methodName, methodNameForError);
     if (method === undefined) {
       return () => Promise_resolve();
     }
 
     if (algoArgCount === 0) {
-      return Function_bind(PromiseCall1, undefined, method, underlyingObject,
-                           controller);
+      return Function_bind(
+          PromiseCall1, undefined, method, underlyingObject, controller);
     }
 
     return arg => PromiseCall2(method, underlyingObject, arg, controller);
@@ -281,10 +282,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 
   function PromiseCall0(F, V) {
-    // assert(typeof F === 'function',
-    //        'IsCallable(F) is true.');
-    // assert(V !== undefined,
-    //        'V is not undefined.');
+    // assert(typeof F === 'function', 'IsCallable(F) is true.');
+    // assert(V !== undefined, 'V is not undefined.');
     try {
       return Promise_resolve(callFunction(F, V));
     } catch (e) {
@@ -293,10 +292,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 
   function PromiseCall1(F, V, arg0) {
-    // assert(typeof F === 'function',
-    //        'IsCallable(F) is true.');
-    // assert(V !== undefined,
-    //        'V is not undefined.');
+    // assert(typeof F === 'function', 'IsCallable(F) is true.');
+    // assert(V !== undefined, 'V is not undefined.');
     try {
       return Promise_resolve(callFunction(F, V, arg0));
     } catch (e) {
@@ -305,10 +302,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 
   function PromiseCall2(F, V, arg0, arg1) {
-    // assert(typeof F === 'function',
-    //        'IsCallable(F) is true.');
-    // assert(V !== undefined,
-    //        'V is not undefined.');
+    // assert(typeof F === 'function', 'IsCallable(F) is true.');
+    // assert(V !== undefined, 'V is not undefined.');
     try {
       return Promise_resolve(callFunction(F, V, arg0, arg1));
     } catch (e) {
@@ -335,5 +330,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     MakeSizeAlgorithmFromSizeFunction,
     CallOrNoop1,
     PromiseCallOrNoop1,
+    PromiseCall2
   };
 });
