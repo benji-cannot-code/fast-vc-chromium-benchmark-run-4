852760e7e9f715f5321f7981751978283137526b
Thomas Anderson <thomasanderson@chromium.org>
2018-03-22 17:49:11 +0000

    Revert "[3p-Conflicts] Fix whitelisting of modules with matching cert's subject"
    This reverts commit fdf8f25c66658ea5a60e7856f9fdfcfaabb1acc1.
    
    Reason for revert: Broke Google Chrome Win:
    https://ci.chromium.org/buildbot/chromium.chrome/Google%20Chrome%20Win/28332
    
    Original change's description:
    > [3p-Conflicts] Fix whitelisting of modules with matching cert's subject
    > 
    > A previous CL was meant to whitelist modules whose cert's subject
    > matches the cert's subject of the executable, but the code was added in
    > the wrong file.
    > 
    > Move the check from ModuleListFilter where it doesn't belong and move it
    > to the ProblematicProgramsUpdater class.
    > 
    > Also added a unit test to make sure the issue is really fixed.
    > 
    > Bug: 819793
    > Change-Id: Ibd6a3c599d6b5ec2bf0a50c5ae0536159bb30fc6
    > Reviewed-on: https://chromium-review.googlesource.com/969601
    > Commit-Queue: Patrick Monette <pmonette@chromium.org>
    > Reviewed-by: Greg Thompson <grt@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#545128}
    
    TBR=pmonette@chromium.org,grt@chromium.org
    
    Change-Id: Id7b2f78ae9caaf950920185551099c40acc36988
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 819793
    Reviewed-on: https://chromium-review.googlesource.com/976161
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545143}

--- chrome/browser/conflicts/module_list_filter_win.h
+++ chrome/browser/conflicts/module_list_filter_win.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/macros.h"
+#include "chrome/browser/conflicts/module_info_util_win.h"
 #include "chrome/browser/conflicts/proto/module_list.pb.h"
 
 struct ModuleInfoKey;
@@ -50,6 +51,9 @@ class ModuleListFilter {
       const ModuleInfoData& module_data) const;
 
  private:
+  // The certificate info of the current executable.
+  CertificateInfo exe_certificate_info_;
+
   chrome::conflicts::ModuleList module_list_;
 
   // Indicates if Initalize() has been succesfully called.
