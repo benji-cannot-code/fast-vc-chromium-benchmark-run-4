825b8fa43514e790a97b867b132b57addf0c43ab
Christopher Cameron <ccameron@chromium.org>
2018-03-22 18:18:23 +0000

    Remove redundant code from RWHVCocoa, move some code to RWHVNSViewBridge
    Remove suppressNextEscapeKeyUp_ because its functionality is made
    redundant by keyDownCodes_. The escape-key-specific version was
    introduced in crrev.com/165615 while the generic version was introduced
    in crrev.com/304728.
    
    While in the -[NSView keyEvent:] method, replace repeated method calls
    with local variables in preparation for moving most of the funciton
    across an IPC boundary, and fold shouldAutohideCursorForEvent into
    the function (since it's called nowhere else).
    
    Move SetBackgroundColor and Show/Hide/IsHidden functionality into
    the RWHVNSViewBridge.
    
    Bug: 821651
    Change-Id: I31382a46b330fedbd3c4a6593ffafdb4a0b83be1
    Reviewed-on: https://chromium-review.googlesource.com/974887
    Commit-Queue: ccameron <ccameron@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545160}

--- content/browser/renderer_host/render_widget_host_view_mac.h
+++ content/browser/renderer_host/render_widget_host_view_mac.h
@@ -258,9 +258,6 @@ class CONTENT_EXPORT RenderWidgetHostViewMac
   // These member variables should be private, but the associated ObjC class
   // needs access to them and can't be made a friend.
 
-  // The background CoreAnimation layer which is hosted by |cocoa_view_|.
-  base::scoped_nsobject<CALayer> background_layer_;
-
   // Delegated frame management and compositor interface.
   std::unique_ptr<BrowserCompositorMac> browser_compositor_;
   BrowserCompositorMac* BrowserCompositorForTesting() const {
@@ -283,10 +280,6 @@ class CONTENT_EXPORT RenderWidgetHostViewMac
   }
 
   CONTENT_EXPORT void release_pepper_fullscreen_window_for_testing();
-
-  RenderWidgetHostViewMac* fullscreen_parent_host_view() const {
-    return fullscreen_parent_host_view_;
-  }
 
   int window_number() const;
 
@@ -385,6 +378,9 @@ class CONTENT_EXPORT RenderWidgetHostViewMac
 
   // Interface through which the NSView is to be manipulated.
   std::unique_ptr<RenderWidgetHostNSViewBridge> ns_view_bridge_;
+
+  // State tracked by Show/Hide/IsShowing.
+  bool is_visible_ = false;
 
   // Indicates if the page is loading.
   bool is_loading_;
