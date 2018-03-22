990f59ec541ee528293158b15747b56220610073
Ovidio Henriquez <odejesush@chromium.org>
2018-03-22 16:16:22 +0000

    bluetooth: FakeBluetoothChooser never timeout scan
    Allows FakeBluetoothChooser to set the test scan duration to never
    timeout.
    
    BUG=719827
    
    Change-Id: I38c74a1bbb0de026edb7d578b98258941d0c3349
    Reviewed-on: https://chromium-review.googlesource.com/961544
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Commit-Queue: Ovidio Henriquez <odejesush@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545105}

--- content/shell/browser/layout_test/fake_bluetooth_chooser.cc
+++ content/shell/browser/layout_test/fake_bluetooth_chooser.cc
@@ -9,15 +9,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "content/public/browser/bluetooth_chooser.h"
+#include "content/public/test/layouttest_support.h"
 #include "content/shell/common/layout_test/fake_bluetooth_chooser.mojom.h"
 
 namespace content {
 
-FakeBluetoothChooser::~FakeBluetoothChooser() = default;
+FakeBluetoothChooser::~FakeBluetoothChooser() {
+  SetTestBluetoothScanDuration(
+      BluetoothTestScanDurationSetting::kImmediateTimeout);
+}
 
 // static
 std::unique_ptr<FakeBluetoothChooser> FakeBluetoothChooser::Create(
     mojom::FakeBluetoothChooserRequest request) {
+  SetTestBluetoothScanDuration(BluetoothTestScanDurationSetting::kNeverTimeout);
   return std::unique_ptr<FakeBluetoothChooser>(
       new FakeBluetoothChooser(std::move(request)));
 }
--- content/shell/browser/layout_test/layout_test_bluetooth_fake_adapter_setter_impl.cc
+++ content/shell/browser/layout_test/layout_test_bluetooth_fake_adapter_setter_impl.cc
@@ -33,7 +33,8 @@ void LayoutTestBluetoothFakeAdapterSetterImpl::Create(
 void LayoutTestBluetoothFakeAdapterSetterImpl::Set(
     const std::string& adapter_name,
     SetCallback callback) {
-  SetTestBluetoothScanDuration();
+  SetTestBluetoothScanDuration(
+      BluetoothTestScanDurationSetting::kImmediateTimeout);
 
   device::BluetoothAdapterFactoryWrapper::Get().SetBluetoothAdapterForTesting(
       LayoutTestBluetoothAdapterProvider::GetBluetoothAdapter(adapter_name));
