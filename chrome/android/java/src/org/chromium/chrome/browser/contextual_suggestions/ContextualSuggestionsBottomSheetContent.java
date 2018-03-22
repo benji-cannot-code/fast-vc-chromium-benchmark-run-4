a05aaaf3d8ad73bbfd9ea49e200585537e382100
Matthew Jones <mdjones@chromium.org>
2018-03-22 19:24:28 +0000

    Add concept of priority to BottomSheetContent
    This patch adds the concept of priority to content that can be shown
    in the bottom sheet. Currently only two priorities are supported: HIGH
    and LOW.
    
    BUG=821568
    
    Change-Id: Id2473a48618b5095ab9b1cca20831c7547c4d36d
    Reviewed-on: https://chromium-review.googlesource.com/969063
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545193}

--- chrome/android/java/src/org/chromium/chrome/browser/contextual_suggestions/ContextualSuggestionsBottomSheetContent.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextual_suggestions/ContextualSuggestionsBottomSheetContent.java
@@ -8,6 +8,7 @@ package org.chromium.chrome.browser.contextual_suggestions;
 import android.view.View;
 
 import org.chromium.chrome.browser.widget.bottomsheet.BottomSheet.BottomSheetContent;
+import org.chromium.chrome.browser.widget.bottomsheet.BottomSheet.ContentPriority;
 
 /** A {@link BottomSheetContent} that displays contextual suggestions. */
 public class ContextualSuggestionsBottomSheetContent implements BottomSheetContent {
@@ -48,5 +49,10 @@ public class ContextualSuggestionsBottomSheetContent implements BottomSheetConte
     @Override
     public boolean applyDefaultTopPadding() {
         return false;
+    }
+
+    @Override
+    public @ContentPriority int getPriority() {
+        return ContentPriority.LOW;
     }
 }
