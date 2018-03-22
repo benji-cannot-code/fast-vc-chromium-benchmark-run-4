40a3606689627d07cf78445d03ce69dcc80c82e0
Peng Huang <penghuang@chromium.org>
2018-03-22 17:29:27 +0000

    GpuServiceImpl: add a GLContext and a GrContext
    This CL adds a GLContext and a GrContext in GpuServiceImpl. They are
    created when viz::SkiaOutputSurface needs a GrContext to create a SkSurface
    for the framebuffer. This CL also creates a SequenceId for
    viz::SkiaOutputSurface to play back Skia DDLs on the GPU thread.
    
    This CL is part of CL https://crrev.com/c/947047 which makes
    SkiaRenderer use Skia DDL to render frames.
    
    Bug: 824382
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel
    Change-Id: I9091fd46cbaafe0ace363bfe63e61b5a0d9fe32d
    Reviewed-on: https://chromium-review.googlesource.com/973757
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545129}

--- components/viz/service/gl/gpu_service_impl.h
+++ components/viz/service/gl/gpu_service_impl.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/client/gpu_memory_buffer_manager.h"
 #include "gpu/command_buffer/common/activity_flags.h"
 #include "gpu/command_buffer/service/gpu_preferences.h"
+#include "gpu/command_buffer/service/sequence_id.h"
 #include "gpu/config/gpu_info.h"
 #include "gpu/ipc/common/surface_handle.h"
 #include "gpu/ipc/service/gpu_channel.h"
@@ -27,7 +28,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/binding_set.h"
 #include "services/viz/privileged/interfaces/gl/gpu_host.mojom.h"
 #include "services/viz/privileged/interfaces/gl/gpu_service.mojom.h"
+#include "third_party/skia/include/core/SkRefCnt.h"
 #include "ui/gfx/native_widget_types.h"
+
+class GrContext;
 
 #if defined(OS_CHROMEOS)
 namespace arc {
@@ -70,6 +74,8 @@ class VIZ_SERVICE_EXPORT GpuServiceImpl : public gpu::GpuChannelManagerDelegate,
                           base::WaitableEvent* shutdown_event = nullptr);
   void Bind(mojom::GpuServiceRequest request);
 
+  bool CreateGrContextIfNecessary(gl::GLSurface* surface);
+
   bool is_initialized() const { return !!gpu_host_; }
 
   media::MediaGpuChannelManager* media_gpu_channel_manager() {
@@ -94,6 +100,7 @@ class VIZ_SERVICE_EXPORT GpuServiceImpl : public gpu::GpuChannelManagerDelegate,
   }
 
   gpu::SyncPointManager* sync_point_manager() { return sync_point_manager_; }
+  gpu::Scheduler* scheduler() { return scheduler_.get(); }
 
   gpu::GpuWatchdogThread* watchdog_thread() { return watchdog_thread_.get(); }
 
@@ -109,6 +116,12 @@ class VIZ_SERVICE_EXPORT GpuServiceImpl : public gpu::GpuChannelManagerDelegate,
   const gpu::GpuPreferences& gpu_preferences() const {
     return gpu_preferences_;
   }
+
+  gpu::SequenceId skia_output_surface_sequence_id() const {
+    return skia_output_surface_sequence_id_;
+  }
+  gl::GLContext* context_for_skia() { return context_for_skia_.get(); }
+  GrContext* gr_context() { return gr_context_.get(); }
 
  private:
   void RecordLogMessage(int severity,
@@ -215,6 +228,15 @@ class VIZ_SERVICE_EXPORT GpuServiceImpl : public gpu::GpuChannelManagerDelegate,
   gpu::SyncPointManager* sync_point_manager_ = nullptr;
 
   std::unique_ptr<gpu::Scheduler> scheduler_;
+
+  // sequence id for running tasks from SkiaOutputSurface;
+  gpu::SequenceId skia_output_surface_sequence_id_;
+
+  // A GLContext for |gr_context_|. It can only be accessed by Skia.
+  scoped_refptr<gl::GLContext> context_for_skia_;
+
+  // A GrContext for SkiaOutputSurface (maybe raster as well).
+  sk_sp<GrContext> gr_context_;
 
   // An event that will be signalled when we shutdown. On some platforms it
   // comes from external sources.
