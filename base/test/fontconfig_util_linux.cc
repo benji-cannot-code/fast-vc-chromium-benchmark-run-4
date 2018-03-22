0317d7966ab1fa15e7601ef01db80f43a62c8644
Tom Anderson <thomasanderson@chromium.org>
2018-03-22 07:27:45 +0000

    Replace Courier New with Cousine GCS font
    BUG=787020
    TBR=thestig,dpranke
    CC=​drott
    
    Change-Id: I794aab43f62f9c56f00a8c4d9d3959f1a8f36e85
    Reviewed-on: https://chromium-review.googlesource.com/973560
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545003}

--- base/test/fontconfig_util_linux.cc
+++ base/test/fontconfig_util_linux.cc
@@ -64,18 +64,6 @@ const char kFontsConfTemplate[] = R"(<?xml version="1.0"?>
       <glob>/usr/share/fonts/truetype/msttcorefonts/Comic_Sans_MS_Bold.ttf</glob>
     </acceptfont>
     <acceptfont>
-      <glob>/usr/share/fonts/truetype/msttcorefonts/Courier_New.ttf</glob>
-    </acceptfont>
-    <acceptfont>
-      <glob>/usr/share/fonts/truetype/msttcorefonts/Courier_New_Bold.ttf</glob>
-    </acceptfont>
-    <acceptfont>
-      <glob>/usr/share/fonts/truetype/msttcorefonts/Courier_New_Bold_Italic.ttf</glob>
-    </acceptfont>
-    <acceptfont>
-      <glob>/usr/share/fonts/truetype/msttcorefonts/Courier_New_Italic.ttf</glob>
-    </acceptfont>
-    <acceptfont>
       <glob>/usr/share/fonts/truetype/msttcorefonts/Impact.ttf</glob>
     </acceptfont>
     <acceptfont>
@@ -200,7 +188,7 @@ const char kFontsConfTemplate[] = R"(<?xml version="1.0"?>
       <string>mono</string>
     </test>
     <edit name="family" mode="assign">
-      <string>Courier New</string>
+      <string>Cousine</string>
     </edit>
   </match>
 
@@ -209,7 +197,7 @@ const char kFontsConfTemplate[] = R"(<?xml version="1.0"?>
       <string>monospace</string>
     </test>
     <edit name="family" mode="assign">
-      <string>Courier New</string>
+      <string>Cousine</string>
     </edit>
   </match>
 
@@ -218,7 +206,7 @@ const char kFontsConfTemplate[] = R"(<?xml version="1.0"?>
       <string>Courier</string>
     </test>
     <edit name="family" mode="assign">
-      <string>Courier New</string>
+      <string>Cousine</string>
     </edit>
   </match>
 
@@ -246,6 +234,15 @@ const char kFontsConfTemplate[] = R"(<?xml version="1.0"?>
     </test>
     <edit name="family" mode="assign">
       <string>Times New Roman</string>
+    </edit>
+  </match>
+
+  <match target="pattern">
+    <test qual="any" name="family">
+      <string>Courier New</string>
+    </test>
+    <edit name="family" mode="assign">
+      <string>Cousine</string>
     </edit>
   </match>
 
