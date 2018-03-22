a5bb7657c58f2f66dd560e4988efc3f87a5351e1
Sadrul Habib Chowdhury <sadrul@chromium.org>
2018-03-22 17:04:56 +0000

    events: Make it difficult to set the wrong flag.
    Validate that the |changed_button_flags_| field is allowed only the
    set of flags that makes sense.
    
    BUG=779372, 824629
    
    Change-Id: I5e4d8b8f07fbf6d881d8510f45414814d074278d
    Reviewed-on: https://chromium-review.googlesource.com/974078
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545120}

--- ui/views/controls/textfield/textfield_unittest.cc
+++ ui/views/controls/textfield/textfield_unittest.cc
@@ -732,7 +732,7 @@ class TextfieldTest : public ViewsTestBase, public TextfieldController {
   void PressMouseButton(ui::EventFlags mouse_button_flags, int extra_flags) {
     ui::MouseEvent press(ui::ET_MOUSE_PRESSED, mouse_position_, mouse_position_,
                          ui::EventTimeForNow(), mouse_button_flags,
-                         mouse_button_flags | extra_flags);
+                         mouse_button_flags);
     textfield_->OnMousePressed(press);
   }
 
