3cdf45d1ab91fa329d14e4a7bc1f47abac2ed954
MinChen <minch@chromium.org>
2018-03-22 17:32:01 +0000

    ash: Position power button menu next to power button.
    doc: go/add-power-button-location
    
    changes in this cl:
    - Add a switch kAshPowerButtonPosition with value to store the related
      power button position info that read at ChromeOS side.
    
    - Parse the value of the switch in PowerButtonController.
    
    - Get the menu bounds origins in different screen orientations according
      to the position info in PowerButtonMenuScreenView. This will help to
      set the bounds of the menu in each screen orientation.
    
    - Get the menu animation direction and transform distance according to the
      position info in PowerButtonMenuView. We need to minus the animation
      transform when setting the bounds of the menu before showing the menu.
    
    - Add PowerButtonControllerWithPositionTest*.
    
    
    Bug: 818331
    Change-Id: I0a1191072b85db59ed2d1d20e5ce25266a357e95
    Reviewed-on: https://chromium-review.googlesource.com/963644
    Commit-Queue: min c <minch@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Qiang Xu <warx@google.com>
    Cr-Commit-Position: refs/heads/master@{#545130}

--- ash/system/power/power_button_menu_view.h
+++ ash/system/power/power_button_menu_view.h
@@ -3,10 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef ASH_SYSTEM_POWER_BUTTON_MENU_VIEW_H_
-#define ASH_SYSTEM_POWER_BUTTON_MENU_VIEW_H_
+#ifndef ASH_SYSTEM_POWER_POWER_BUTTON_MENU_VIEW_H_
+#define ASH_SYSTEM_POWER_POWER_BUTTON_MENU_VIEW_H_
 
 #include "ash/ash_export.h"
+#include "ash/system/power/power_button_controller.h"
 #include "base/macros.h"
 #include "ui/compositor/layer_animation_observer.h"
 #include "ui/views/controls/button/button.h"
@@ -24,16 +25,30 @@ class ASH_EXPORT PowerButtonMenuView : public views::View,
   // The duration of the animation to show or hide the power button menu view.
   static constexpr int kAnimationTimeoutMs = 500;
 
-  // Top padding of the menu view.
-  static constexpr int kMenuViewTopPadding = 16;
+  // Distance of the menu animation transform.
+  static constexpr int kMenuViewTransformDistanceDp = 16;
 
-  PowerButtonMenuView();
+  // Direction of the animation transform. X means to translate from
+  // x-coordinate. Y means to translate from y-coordinate.
+  enum class TransformDirection { NONE, X, Y };
+
+  // The translate direction and distance of the animation transform.
+  struct TransformDisplacement {
+    TransformDirection direction;
+    int distance;
+  };
+
+  explicit PowerButtonMenuView(
+      PowerButtonController::PowerButtonPosition power_button_position);
   ~PowerButtonMenuView() override;
 
   bool sign_out_item_for_testing() const { return sign_out_item_; }
 
   // Schedules an animation to show or hide the view.
   void ScheduleShowHideAnimation(bool show);
+
+  // Gets the transform displacement, which contains direction and distance.
+  TransformDisplacement GetTransformDisplacement() const;
 
  private:
   // Creates the items that in the menu.
@@ -54,9 +69,12 @@ class ASH_EXPORT PowerButtonMenuView : public views::View,
   PowerButtonMenuItemView* power_off_item_ = nullptr;
   PowerButtonMenuItemView* sign_out_item_ = nullptr;
 
+  // The physical display side of power button in landscape primary.
+  PowerButtonController::PowerButtonPosition power_button_position_;
+
   DISALLOW_COPY_AND_ASSIGN(PowerButtonMenuView);
 };
 
 }  // namespace ash
 
-#endif  // ASH_SYSTEM_POWER_BUTTON_MENU_VIEW_H_
+#endif  // ASH_SYSTEM_POWER_POWER_BUTTON_MENU_VIEW_H_
