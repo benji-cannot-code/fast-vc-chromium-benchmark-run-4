b5503114ed70f75cba870604ff8e7ee54a8da866
Bailey Forrest <bcf@chromium.org>
2018-03-22 18:50:16 +0000

    [chromecast] BLE upstream: Stage 1 copy files
    - This process is broken into stages to make review easier
    - Stage 1 does nothing but move files.
    - Stage 1 does not build without depending on internal code.
    
    BUG=internal b/76121501
    TEST=Build and run. cast_bluetooth_unittests.
    
    Change-Id: I7b1d48c3cac61934970e86b45cc804918ac97f63
    Reviewed-on: https://chromium-review.googlesource.com/976221
    Reviewed-by: Stephen Lanham <slan@chromium.org>
    Commit-Queue: Stephen Lanham <slan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545174}

---
+++ chromecast/device/bluetooth/shlib/mock_le_scanner.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMECAST_DEVICE_BLUETOOTH_SHLIB_MOCK_LE_SCANNER_H_
+#define CHROMECAST_DEVICE_BLUETOOTH_SHLIB_MOCK_LE_SCANNER_H_
+
+#include "chromecast/device/bluetooth/shlib/le_scanner.h"
+#include "testing/gmock/include/gmock/gmock.h"
+
+namespace chromecast {
+namespace bluetooth_v2_shlib {
+
+class MockLeScanner : public LeScannerImpl {
+ public:
+  MockLeScanner();
+  ~MockLeScanner() override;
+  MOCK_METHOD0(IsSupported, bool());
+  MOCK_METHOD1(SetDelegate, void(LeScanner::Delegate* delegate));
+  MOCK_METHOD0(StartScan, bool());
+  MOCK_METHOD0(StopScan, bool());
+};
+
+inline MockLeScanner::MockLeScanner() = default;
+inline MockLeScanner::~MockLeScanner() = default;
+
+}  // namespace bluetooth_v2_shlib
+}  // namespace chromecast
+
+#endif  // CHROMECAST_DEVICE_BLUETOOTH_SHLIB_MOCK_LE_SCANNER_H_
