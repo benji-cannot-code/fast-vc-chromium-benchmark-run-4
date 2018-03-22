8219666c09da50b8193c00b62c1748a90b3770e3
Adam Rice <ricea@chromium.org>
2018-03-22 11:37:54 +0000

    Remove stray semicolon from RecordResponseTypeForAdd
    git cl lint found a stray semicolon in Cache.cpp. Remove it.
    
    Change-Id: Ifb6535cf163b6febac0ee9da66bb671d7c827a0c
    Reviewed-on: https://chromium-review.googlesource.com/975085
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545040}

--- third_party/WebKit/Source/modules/cachestorage/Cache.cpp
+++ third_party/WebKit/Source/modules/cachestorage/Cache.cpp
@@ -182,7 +182,7 @@ void RecordResponseTypeForAdd(const Member<Response>& response) {
        static_cast<int>(network::mojom::FetchResponseType::kLast) + 1));
   response_type_histogram.Count(
       static_cast<int>(response->GetResponse()->GetType()));
-};
+}
 
 bool VaryHeaderContainsAsterisk(const Response* response) {
   const FetchHeaderList* headers = response->headers()->HeaderList();
