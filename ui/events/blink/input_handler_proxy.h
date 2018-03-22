bbcef6e5ac5f2d497a361fcb43bdbfa84ba188c2
Sandra Sun <sunyunjia@chromium.org>
2018-03-22 22:35:16 +0000

    Snap for GestureFling at cc side.
    This patch implements snapping for gesture fling.
    
    After receiving an inertial GSU, we create a SnapFlingCurve if the
    currently scrolling node has some snap positions. We then animate
    the SnapFlingCurve and ignore the following GSUs and GSE so that the
    fling would land on an valid snap position.
    
    Bug: 778259
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: Ib7f9a191adf02af96b9c986e016f5c75cdc591d9
    Reviewed-on: https://chromium-review.googlesource.com/901022
    Commit-Queue: Sandra Sun <sunyunjia@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Majid Valipour <majidvp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545272}

--- ui/events/blink/input_handler_proxy.h
+++ ui/events/blink/input_handler_proxy.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/web/WebActiveFlingParameters.h"
 #include "ui/events/blink/blink_features.h"
 #include "ui/events/blink/input_scroll_elasticity_controller.h"
+#include "ui/events/blink/snap_fling_controller.h"
 #include "ui/events/blink/synchronous_input_handler_proxy.h"
 #include "ui/events/blink/web_input_event_traits.h"
 
@@ -52,7 +53,8 @@ struct DidOverscrollParams;
 // events intended for a specific WebWidget.
 class InputHandlerProxy : public cc::InputHandlerClient,
                           public SynchronousInputHandlerProxy,
-                          public blink::WebGestureCurveTarget {
+                          public blink::WebGestureCurveTarget,
+                          public SnapFlingClient {
  public:
   InputHandlerProxy(cc::InputHandler* input_handler,
                     InputHandlerProxyClient* client,
@@ -114,6 +116,14 @@ class InputHandlerProxy : public cc::InputHandlerClient,
   // blink::WebGestureCurveTarget implementation.
   bool ScrollBy(const blink::WebFloatSize& offset,
                 const blink::WebFloatSize& velocity) override;
+
+  // SnapFlingClient implementation.
+  bool GetSnapFlingInfo(const gfx::Vector2dF& natural_displacement,
+                        gfx::Vector2dF* initial_offset,
+                        gfx::Vector2dF* target_offset) const override;
+  gfx::Vector2dF ScrollByForSnapFling(const gfx::Vector2dF& delta) override;
+  void ScrollEndForSnapFling() override;
+  void RequestAnimationForSnapFling() override;
 
   bool gesture_scroll_on_impl_thread_for_testing() const {
     return gesture_scroll_on_impl_thread_;
@@ -264,6 +274,8 @@ class InputHandlerProxy : public cc::InputHandlerClient,
   base::TickClock* tick_clock_;
 
   std::unique_ptr<FlingBooster> fling_booster_;
+
+  std::unique_ptr<SnapFlingController> snap_fling_controller_;
 
   DISALLOW_COPY_AND_ASSIGN(InputHandlerProxy);
 };
