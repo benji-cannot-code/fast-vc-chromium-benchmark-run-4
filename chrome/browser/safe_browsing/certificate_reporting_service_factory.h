cf3565deeac9a09d81f11a9903aed67305750e41
Matt Mueller <mattm@chromium.org>
2018-03-23 23:53:31 +0000

    c/b/safe_browsing/certificate_reporting_service_factory.h: add forward declaration for base::Clock
    Change-Id: If6d42457b8d40040ef09695eaf8197b06fd99575
    Reviewed-on: https://chromium-review.googlesource.com/967322
    Commit-Queue: Nathan Parker <nparker@chromium.org>
    Reviewed-by: Nathan Parker <nparker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545631}

--- chrome/browser/safe_browsing/certificate_reporting_service_factory.h
+++ chrome/browser/safe_browsing/certificate_reporting_service_factory.h
@@ -9,6 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/singleton.h"
 #include "components/keyed_service/content/browser_context_keyed_service_factory.h"
 
+namespace base {
+class Clock;
+}
+
 namespace content {
 class BrowserContext;
 }
