1e83fee6fd6bd340285f66d75ad6aac88ec1b471
Kent Tamura <tkent@chromium.org>
2018-03-23 04:25:01 +0000

    Remove DeprecatedDataLogF() usage in WTFString.cpp.
    String::Show() uses operator<< instead.
    
    Bug: 720442
    Change-Id: I2053f3c12f81bd0f30c164cda3b409f0cb1fd593
    Reviewed-on: https://chromium-review.googlesource.com/977185
    Commit-Queue: Yuta Kitamura <yutak@chromium.org>
    Reviewed-by: Yuta Kitamura <yutak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545367}

--- third_party/WebKit/Source/platform/wtf/text/WTFString.cpp
+++ third_party/WebKit/Source/platform/wtf/text/WTFString.cpp
@@ -25,8 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/string_util.h"
 #include "platform/wtf/ASCIICType.h"
-#include "platform/wtf/DataLog.h"
-#include "platform/wtf/HexNumber.h"
 #include "platform/wtf/MathExtras.h"
 #include "platform/wtf/StringExtras.h"
 #include "platform/wtf/Vector.h"
@@ -42,31 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WTF {
 
 using namespace Unicode;
-
-namespace {
-
-Vector<char> AsciiDebug(StringImpl* impl) {
-  if (!impl)
-    return AsciiDebug(String("[null]").Impl());
-
-  Vector<char> buffer;
-  for (unsigned i = 0; i < impl->length(); ++i) {
-    UChar ch = (*impl)[i];
-    if (IsASCIIPrintable(ch)) {
-      if (ch == '\\')
-        buffer.push_back('\\');
-      buffer.push_back(static_cast<char>(ch));
-    } else {
-      buffer.push_back('\\');
-      buffer.push_back('u');
-      HexNumber::AppendUnsignedAsHexFixedSize(ch, buffer, 4);
-    }
-  }
-  buffer.push_back('\0');
-  return buffer;
-}
-
-}  // namespace
 
 // Construct a string with UTF-16 data.
 String::String(const UChar* characters, unsigned length)
@@ -826,7 +799,7 @@ std::ostream& operator<<(std::ostream& out, const String& string) {
 
 #ifndef NDEBUG
 void String::Show() const {
-  DeprecatedDataLogF("%s\n", AsciiDebug(Impl()).data());
+  DLOG(INFO) << *this;
 }
 #endif
 
