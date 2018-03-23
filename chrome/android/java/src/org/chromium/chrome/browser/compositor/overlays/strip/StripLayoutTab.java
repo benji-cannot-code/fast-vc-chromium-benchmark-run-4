779f1b983283ec0c1ff06b91892f278c46933af3
Siddhartha <ssid@chromium.org>
2018-03-23 20:40:53 +0000

    Android: Release resources when tab is invisible in strip layout
    The bitmap resources held by LayerTitleCache for tabs are never cleared
    by tab strip model. Release resources held for a tab if the tab is
    marked invisible in tab strip layout.
    
    BUG=701976
    
    Change-Id: I3539d5aa546d5f002cb2b1a670de5dea52336722
    Reviewed-on: https://chromium-review.googlesource.com/977324
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545569}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutTab.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutTab.java
@@ -267,6 +267,9 @@ public class StripLayoutTab implements VirtualView {
      */
     public void setVisible(boolean visible) {
         mVisible = visible;
+        if (!visible) {
+            mUpdateHost.releaseResourcesForTab(mId);
+        }
         for (Observer observer : mObservers) {
             observer.onVisibilityChanged(mVisible);
         }
