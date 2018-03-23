7cf660f1768b3ed138bebb2886e84748c8b48c7a
yoshiki iguchi <yoshiki@chromium.org>
2018-03-23 05:44:53 +0000

    Close Message Center on screen rotation
    This CL adds the code to close the message center when the
    root window bounds is changed. When the bound is changed,
    the message center may be hidden by the edge of display
    partially. This CL prevents such a bad state.
    
    Bug: 813008
    Change-Id: Ia2a2bbc319f759a8dd078a91f406e88f11235a17
    Reviewed-on: https://chromium-review.googlesource.com/958049
    Commit-Queue: Yoshiki Iguchi <yoshiki@chromium.org>
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545373}

--- ash/system/tray/tray_bubble_wrapper.h
+++ ash/system/tray/tray_bubble_wrapper.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "base/macros.h"
+#include "ui/aura/window_observer.h"
 #include "ui/views/widget/widget_observer.h"
 #include "ui/wm/public/activation_change_observer.h"
 
@@ -21,6 +22,7 @@ class TrayBackgroundView;
 // Creates and manages the Widget and EventFilter components of a bubble.
 
 class ASH_EXPORT TrayBubbleWrapper : public views::WidgetObserver,
+                                     public aura::WindowObserver,
                                      public ::wm::ActivationChangeObserver {
  public:
   TrayBubbleWrapper(TrayBackgroundView* tray,
@@ -29,9 +31,16 @@ class ASH_EXPORT TrayBubbleWrapper : public views::WidgetObserver,
   ~TrayBubbleWrapper() override;
 
   // views::WidgetObserver overrides:
+  void OnWidgetClosing(views::Widget* widget) override;
   void OnWidgetDestroying(views::Widget* widget) override;
   void OnWidgetBoundsChanged(views::Widget* widget,
                              const gfx::Rect& new_bounds) override;
+
+  // aura::WindowObserver:
+  void OnWindowBoundsChanged(aura::Window* window,
+                             const gfx::Rect& old_bounds,
+                             const gfx::Rect& new_bounds,
+                             ui::PropertyChangeReason reason) override;
 
   // ::wm::ActivationChangeObserver overrides:
   void OnWindowActivated(ActivationReason reason,
