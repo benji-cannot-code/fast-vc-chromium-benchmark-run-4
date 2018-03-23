45d466469ceb9d3d4bdb20e2538fc6338a0154bc
Rune Lillesveen <futhark@chromium.org>
2018-03-23 18:53:25 +0000

    Don't store ComputedStyle for display:none elements.
    We stored ComputedStyle for option, optgroup, and svg:stop even when
    their computed display was none. That caused descendants to get
    ComputedStyle objects and confused the WhitespaceAttacher walking down
    display:none subtrees for layout tree rebuilds.
    
    Bug: 822581
    Change-Id: I604033d96ff194119f328ac5ced71a9e04563ccb
    Reviewed-on: https://chromium-review.googlesource.com/973962
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545540}

--- third_party/WebKit/Source/core/dom/ElementTest.cpp
+++ third_party/WebKit/Source/core/dom/ElementTest.cpp
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include "core/dom/Document.h"
+#include "core/dom/NodeComputedStyle.h"
 #include "core/editing/testing/EditingTestBase.h"
 #include "core/frame/LocalFrameView.h"
 #include "core/geometry/DOMRect.h"
@@ -389,6 +390,23 @@ TEST_F(ElementTest, PartAttribute) {
     ASSERT_EQ(1UL, part_names->size());
     ASSERT_EQ("partname", (*part_names)[0].Ascii());
   }
+}
+
+TEST_F(ElementTest, OptionElementDisplayNoneComputedStyle) {
+  Document& document = GetDocument();
+  SetBodyContent(R"HTML(
+    <optgroup id=group style='display:none'></optgroup>
+    <option id=option style='display:none'></option>
+    <div style='display:none'>
+      <optgroup id=inner-group></optgroup>
+      <option id=inner-option></option>
+    </div>
+  )HTML");
+
+  EXPECT_FALSE(document.getElementById("group")->GetComputedStyle());
+  EXPECT_FALSE(document.getElementById("option")->GetComputedStyle());
+  EXPECT_FALSE(document.getElementById("inner-group")->GetComputedStyle());
+  EXPECT_FALSE(document.getElementById("inner-option")->GetComputedStyle());
 }
 
 }  // namespace blink
--- third_party/WebKit/Source/core/html/forms/HTMLSelectElement.cpp
+++ third_party/WebKit/Source/core/html/forms/HTMLSelectElement.cpp
@@ -1862,9 +1862,8 @@ String HTMLSelectElement::ItemText(const Element& element) const {
 bool HTMLSelectElement::ItemIsDisplayNone(Element& element) const {
   if (auto* option = ToHTMLOptionElementOrNull(element))
     return option->IsDisplayNone();
-  if (const ComputedStyle* style = ItemComputedStyle(element))
-    return style->Display() == EDisplay::kNone;
-  return false;
+  const ComputedStyle* style = ItemComputedStyle(element);
+  return !style || style->Display() == EDisplay::kNone;
 }
 
 const ComputedStyle* HTMLSelectElement::ItemComputedStyle(
