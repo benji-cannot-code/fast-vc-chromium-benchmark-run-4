b4f9cbe8b14296ae39a1f7fdb6d99ef10d0b5bd1
Gayane Petrosyan <gayane@chromium.org>
2018-03-23 01:45:24 +0000

    Do not record expired histograms.
    For expired histograms return DummyHistogram instead of real histogram object
    that will absorb the logged data.
    
    This is based on https://chromium-review.googlesource.com/c/chromium/src/+/677086
    which corresponds to the first patch here.
    
    Change-Id: I9296cf461c35a522ecc957ebfc2ee1bd33712538
    Reviewed-on: https://chromium-review.googlesource.com/957437
    Commit-Queue: Gayane Petrosyan <gayane@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545333}

--- base/metrics/histogram_base.cc
+++ base/metrics/histogram_base.cc
@@ -40,6 +40,8 @@ std::string HistogramTypeToString(HistogramType type) {
       return "CUSTOM_HISTOGRAM";
     case SPARSE_HISTOGRAM:
       return "SPARSE_HISTOGRAM";
+    case DUMMY_HISTOGRAM:
+      return "DUMMY_HISTOGRAM";
   }
   NOTREACHED();
   return "UNKNOWN";
--- base/metrics/sparse_histogram.cc
+++ base/metrics/sparse_histogram.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/memory/ptr_util.h"
+#include "base/metrics/dummy_histogram.h"
 #include "base/metrics/metrics_hashes.h"
 #include "base/metrics/persistent_histogram_allocator.h"
 #include "base/metrics/persistent_sample_map.h"
@@ -27,6 +28,12 @@ HistogramBase* SparseHistogram::FactoryGet(const std::string& name,
                                            int32_t flags) {
   HistogramBase* histogram = StatisticsRecorder::FindHistogram(name);
   if (!histogram) {
+    // TODO(gayane): |HashMetricName| is called again in Histogram constructor.
+    // Refactor code to avoid the additional call.
+    bool should_record =
+        StatisticsRecorder::ShouldRecordHistogram(HashMetricName(name));
+    if (!should_record)
+      return DummyHistogram::GetInstance();
     // Try to create the histogram using a "persistent" allocator. As of
     // 2016-02-25, the availability of such is controlled by a base::Feature
     // that is off by default. If the allocator doesn't exist or if
