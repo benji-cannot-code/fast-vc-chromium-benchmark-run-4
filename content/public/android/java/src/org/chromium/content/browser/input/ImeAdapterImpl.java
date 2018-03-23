1434806e923238946a04fa7eea95740e7ef131ad
Jinsuk Kim <jinsukkim@chromium.org>
2018-03-23 00:59:20 +0000

    Android: Move post-touchdown event tasks to RWHVA
    This CL moves some post-tasks for MotionEvent touch down events
    flow from CVC to RWHVA-ImeAdapter/GestureListenerManager where we
    already have the access to the objects. The change adds 2 more
    JNI (native -> Java) calls per touch down events. I verified that
    there is no impact on user interaction.
    
    The motivation is to remove all the dependencies on the native content
    view core, except its ownership management by WebContentsViewAndroid.
    
    Bug: 598880
    Change-Id: If0a96b90da74049e96932bfe6ec78e7a76431c15
    Reviewed-on: https://chromium-review.googlesource.com/974481
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545318}

--- content/public/android/java/src/org/chromium/content/browser/input/ImeAdapterImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/input/ImeAdapterImpl.java
@@ -536,10 +536,6 @@ public class ImeAdapterImpl implements ImeAdapter, WindowEventObserver {
         }
     }
 
-    public Rect getFocusPreOSKViewportRect() {
-        return mFocusPreOSKViewportRect;
-    }
-
     @CalledByNative
     private void updateAfterViewSizeChanged() {
         // Execute a delayed form focus operation because the OSK was brought up earlier.
@@ -554,9 +550,18 @@ public class ImeAdapterImpl implements ImeAdapter, WindowEventObserver {
                 }
                 // Zero the rect to prevent the above operation from issuing the delayed
                 // form focus event.
-                mFocusPreOSKViewportRect.setEmpty();
+                cancelRequestToScrollFocusedEditableNodeIntoView();
             }
         }
+    }
+
+    @CalledByNative
+    private void updateOnTouchDown() {
+        cancelRequestToScrollFocusedEditableNodeIntoView();
+    }
+
+    public void cancelRequestToScrollFocusedEditableNodeIntoView() {
+        mFocusPreOSKViewportRect.setEmpty();
     }
 
     @Override
