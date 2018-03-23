3d55f2333399cc3da37e2fd7aec1de0ecff2c3ef
Conley Owens <cco3@chromium.org>
2018-03-23 23:34:00 +0000

    Report was_cached in SubresourceLoadInfo
    This change adds a boolean `was_cached` to the SubresourceLoadInfo
    struct. This lets consumers know whether or not the response was
    fetched from the network cache. Notably, this is important for
    recording page load metrics when the Network Service is enabled.
    
    This reverts commit 0b2b1a92072144890a713700be568c9fc3e520c4.
    
    Issues in the previous commit are fixed by retaining the exists_in_cache
    field.
    
    In a many cases when NavigationURLLoaderDelegate::OnRequestFailed() is
    called, NavigationURLLoaderNetworkService::OnResponseStarted() has not
    been called. In these cases, we cannot rely on using a stored
    was_cached value, since it may not be properly updated.
    
    BUG=816684,822237
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I0beb72b825ae0a12122170cec2e4f4358dda58af
    Reviewed-on: https://chromium-review.googlesource.com/966792
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Jay Civelli <jcivelli@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Conley Owens <cco3@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545626}

--- content/browser/web_contents/web_contents_impl_browsertest.cc
+++ content/browser/web_contents/web_contents_impl_browsertest.cc
@@ -595,6 +595,7 @@ IN_PROC_BROWSER_TEST_F(WebContentsImplBrowserTest,
   GURL subresource_url = embedded_test_server()->GetURL("/cachetime");
   ASSERT_TRUE(subresource_load_info);
   EXPECT_EQ(subresource_url, subresource_load_info->url);
+  EXPECT_FALSE(subresource_load_info->was_cached);
   observer.Reset();
 
   // Loading again should serve the request out of the in-memory cache.
@@ -612,6 +613,7 @@ IN_PROC_BROWSER_TEST_F(WebContentsImplBrowserTest,
   subresource_load_info = observer.last_subresource_load_info();
   ASSERT_TRUE(subresource_load_info);
   EXPECT_EQ(subresource_url, subresource_load_info->url);
+  EXPECT_TRUE(subresource_load_info->was_cached);
   EXPECT_FALSE(observer.last_memory_cached_loaded_url().is_valid());
 }
 
