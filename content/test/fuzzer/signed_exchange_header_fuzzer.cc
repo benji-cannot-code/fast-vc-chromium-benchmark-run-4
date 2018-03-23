176337953566832adb5488f6fef0724c9f7d481f
Kouhei Ueno <kouhei@chromium.org>
2018-03-23 04:10:22 +0000

    Add fuzzer for SignedExchangeHeader
    Bug: 803774
    Change-Id: I2a5e3d4479238473a86bb5bd8dffe748fabc1940
    Reviewed-on: https://chromium-review.googlesource.com/977183
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545363}

---
+++ content/test/fuzzer/signed_exchange_header_fuzzer.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/at_exit.h"
+#include "base/containers/span.h"
+#include "base/i18n/icu_util.h"
+#include "content/browser/web_package/signed_exchange_header.h"  // nogncheck
+
+namespace content {
+
+struct IcuEnvironment {
+  IcuEnvironment() { CHECK(base::i18n::InitializeICU()); }
+  // used by ICU integration.
+  base::AtExitManager at_exit_manager;
+};
+
+IcuEnvironment* env = new IcuEnvironment();
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  SignedExchangeHeader::Parse(base::make_span(data, size));
+  return 0;
+}
+
+}  // namespace content
