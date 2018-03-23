5dc725eb6f8141e3d5b54259f5c1f93d41d62d8a
Jared Saul <jsaul@google.com>
2018-03-23 19:04:39 +0000

    Revert "Clean up kAutofillCreditCardBankNameDisplay flag"
    This reverts commit e363dc219abb57a818406fffcad3bb30555dab4c.
    
    Reason for revert: Not convinced metrics are good; performing flag holdback
    
    Original change's description:
    > Clean up kAutofillCreditCardBankNameDisplay flag
    >
    > Bug: 812710
    > Change-Id: I4344c258668c5017a0a145e487ca2eb526bd49bc
    > Reviewed-on: https://chromium-review.googlesource.com/923281
    > Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    > Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    > Commit-Queue: Jared Saul <jsaul@google.com>
    > Cr-Commit-Position: refs/heads/master@{#537368}
    
    TBR=mathp@chromium.org,rkaplow@chromium.org,sebsg@chromium.org,jsaul@google.com
    
    Bug: 812710
    Change-Id: I0f15c86af76e686ef4ab55a0313831461503d758
    Reviewed-on: https://chromium-review.googlesource.com/974342
    Reviewed-by: Mathieu Perreault <mathp@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Commit-Queue: Jared Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/master@{#545544}

--- components/autofill/core/browser/autofill_experiments.cc
+++ components/autofill/core/browser/autofill_experiments.cc
@@ -35,6 +35,8 @@ const base::Feature kAutofillCreditCardAssist{
     "AutofillCreditCardAssist", base::FEATURE_DISABLED_BY_DEFAULT};
 const base::Feature kAutofillScanCardholderName{
     "AutofillScanCardholderName", base::FEATURE_DISABLED_BY_DEFAULT};
+const base::Feature kAutofillCreditCardBankNameDisplay{
+    "AutofillCreditCardBankNameDisplay", base::FEATURE_DISABLED_BY_DEFAULT};
 const base::Feature kAutofillCreditCardAblationExperiment{
     "AutofillCreditCardAblationExperiment", base::FEATURE_DISABLED_BY_DEFAULT};
 const base::Feature kAutofillCreditCardPopupLayout{
@@ -128,6 +130,10 @@ bool IsAutofillAutoDismissableUpstreamBubbleExperimentEnabled() {
 
 bool IsAutofillCreditCardLastUsedDateDisplayExperimentEnabled() {
   return base::FeatureList::IsEnabled(kAutofillCreditCardLastUsedDateDisplay);
+}
+
+bool IsAutofillCreditCardBankNameDisplayExperimentEnabled() {
+  return base::FeatureList::IsEnabled(kAutofillCreditCardBankNameDisplay);
 }
 
 // |GetCreditCardPopupParameterUintValue| returns 0 if experiment parameter is
