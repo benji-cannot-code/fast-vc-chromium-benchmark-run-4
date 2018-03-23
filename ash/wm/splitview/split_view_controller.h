3b814721644d19860b7b63e5c84d602a10f391ad
Xiaoqian Dai <xdai@chromium.org>
2018-03-23 21:59:56 +0000

    Cros splitscreen: Animate the overview window to its snapped position.
    Define SPLITVIEW_ANIMATION_RESTORE_OVERVIEW_WINDOW. And also animate the
    overview window from its position in overview grid to its snapped postion.
    
    Bug: 822486
    Change-Id: I64a48b33012c428a5529dfea58f048f30c043e73
    Reviewed-on: https://chromium-review.googlesource.com/972562
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545580}

--- ash/wm/splitview/split_view_controller.h
+++ ash/wm/splitview/split_view_controller.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell_observer.h"
 #include "ash/wm/tablet_mode/tablet_mode_observer.h"
 #include "ash/wm/window_state_observer.h"
+#include "base/containers/flat_map.h"
 #include "base/macros.h"
 #include "base/observer_list.h"
 #include "base/time/time.h"
@@ -91,8 +92,12 @@ class ASH_EXPORT SplitViewController : public mojom::SplitViewController,
   // primary orientation.
   bool IsCurrentScreenOrientationPrimary() const;
 
-  // Snaps window to left/right.
-  void SnapWindow(aura::Window* window, SnapPosition snap_position);
+  // Snaps window to left/right. |window_item_bounds| is the bounds of the
+  // overview window item in overview. It's empty if the snapped window doesn't
+  // come from overview grid.
+  void SnapWindow(aura::Window* window,
+                  SnapPosition snap_position,
+                  const gfx::Rect& window_item_bounds = gfx::Rect());
 
   // Swaps the left and right windows. This will do nothing if one of the
   // windows is not snapped.
@@ -350,6 +355,10 @@ class ASH_EXPORT SplitViewController : public mojom::SplitViewController,
   // the one who stacked above the other, see GetWindowForSmoothResize() for
   // details.
   aura::Window* smooth_resize_window_ = nullptr;
+
+  // The map from a to-be-snapped window to its overview item's bounds if the
+  // window comes from the overview.
+  base::flat_map<aura::Window*, gfx::Rect> overview_window_item_bounds_map_;
 
   base::ObserverList<Observer> observers_;
   mojo::InterfacePtrSet<mojom::SplitViewObserver> mojo_observers_;
