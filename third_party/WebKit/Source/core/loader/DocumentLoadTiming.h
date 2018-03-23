37746b412ab9e9c535f84646d084a632e872bcc4
Deepanjan Roy <dproy@chromium.org>
2018-03-23 13:49:59 +0000

    Dump more information in navigation_start event
    Today for metric calculations, we determine the URL of a
    navigation_start event by finding a FrameLoader snapshot that precedes
    it. However, we're seeing cases where we don't have a snapshot of the
    FrameLoader with the new URL before the nav start event. This CL embeds
    two essential piece of information for metric computation in the
    navigationStart trace event itself so we don't have to look for
    FrameLoader snapshots for them.
    
    Bug: 824761
    Change-Id: I41ee17a58810a0d7b1fb918ceff215454d11c157
    Reviewed-on: https://chromium-review.googlesource.com/975986
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Deepanjan Roy <dproy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545421}

--- third_party/WebKit/Source/core/loader/DocumentLoadTiming.h
+++ third_party/WebKit/Source/core/loader/DocumentLoadTiming.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/CoreExport.h"
 #include "platform/heap/Handle.h"
+#include "platform/instrumentation/tracing/TracedValue.h"
 #include "platform/wtf/Time.h"
 
 namespace blink {
@@ -97,6 +98,7 @@ class CORE_EXPORT DocumentLoadTiming final {
   void NotifyDocumentTimingChanged();
   void EnsureReferenceTimesSet();
   LocalFrame* GetFrame() const;
+  std::unique_ptr<TracedValue> GetNavigationStartTracingData() const;
 
   TimeTicks reference_monotonic_time_;
   double reference_wall_time_;
