af5637975daeb01574d443f73b44593aa173e49d
Kouhei Ueno <kouhei@chromium.org>
2018-03-23 02:22:00 +0000

    SignedExchangeHeader: Only allow lower-cased {request,response} header keys
    Bug: 803774, WICG/webpackage:161
    Change-Id: Ibf3693682dd69df0f687d1e0fae3b0be5e126eff
    Reviewed-on: https://chromium-review.googlesource.com/970221
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545344}

--- content/browser/web_package/signed_exchange_header.h
+++ content/browser/web_package/signed_exchange_header.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/span.h"
 #include "base/optional.h"
 #include "base/strings/string_piece.h"
+#include "base/strings/string_util.h"
 #include "content/browser/web_package/signed_exchange_header_parser.h"
 #include "content/common/content_export.h"
 #include "net/http/http_response_headers.h"
@@ -31,6 +32,8 @@ class CONTENT_EXPORT SignedExchangeHeader {
   // Note: |input| must be pointing to a valid memory address that has at least
   // |kEncodedHeaderLengthInBytes|.
   static size_t ParseHeadersLength(base::span<const uint8_t> input);
+
+  using HeaderMap = std::map<std::string, std::string>;
 
   // Parse headers from the new serialization format currently being discussed.
   // 1. The first 3 bytes of the content represents the length of the CBOR
@@ -55,7 +58,9 @@ class CONTENT_EXPORT SignedExchangeHeader {
   SignedExchangeHeader& operator=(SignedExchangeHeader&&);
   ~SignedExchangeHeader();
 
-  void AddResponseHeader(base::StringPiece name, base::StringPiece value);
+  // AddResponseHeader returns false on duplicated keys. |name| must be
+  // lower-cased.
+  bool AddResponseHeader(base::StringPiece name, base::StringPiece value);
   scoped_refptr<net::HttpResponseHeaders> BuildHttpResponseHeaders() const;
 
   const GURL& request_url() const { return request_url_; };
@@ -69,9 +74,7 @@ class CONTENT_EXPORT SignedExchangeHeader {
   net::HttpStatusCode response_code() const { return response_code_; }
   void set_response_code(net::HttpStatusCode c) { response_code_ = c; }
 
-  const std::map<std::string, std::string>& response_headers() const {
-    return response_headers_;
-  }
+  const HeaderMap& response_headers() const { return response_headers_; }
 
   const SignedExchangeHeaderParser::Signature& signature() const {
     return signature_;
@@ -86,7 +89,7 @@ class CONTENT_EXPORT SignedExchangeHeader {
   std::string request_method_;
 
   net::HttpStatusCode response_code_;
-  std::map<std::string, std::string> response_headers_;
+  HeaderMap response_headers_;
   SignedExchangeHeaderParser::Signature signature_;
 };
 
--- content/browser/web_package/signed_exchange_header_unittest.cc
+++ content/browser/web_package/signed_exchange_header_unittest.cc
@@ -130,7 +130,7 @@ TEST(SignedExchangeHeaderTest, StatefulRequestHeader) {
       {
           {kUrlKey, "https://test.example.org/test/"},
           {kMethodKey, "GET"},
-          {"Authorization", "Basic Zm9vOmJhcg=="},
+          {"authorization", "Basic Zm9vOmJhcg=="},
       },
       {
           {kStatusKey, "200"}, {kSignature, kSignatureString},
@@ -146,8 +146,30 @@ TEST(SignedExchangeHeaderTest, StatefulResponseHeader) {
       {
           {kStatusKey, "200"},
           {kSignature, kSignatureString},
-          {"Set-Cookie", "foo=bar"},
+          {"set-cookie", "foo=bar"},
       });
+  ASSERT_FALSE(header.has_value());
+}
+
+TEST(SignedExchangeHeaderTest, UppercaseRequestMap) {
+  auto header = GenerateHeaderAndParse(
+      {{kUrlKey, "https://test.example.org/test/"},
+       {kMethodKey, "GET"},
+       {"Accept-Language", "en-us"}},
+      {
+          {kStatusKey, "200"}, {kSignature, kSignatureString},
+      });
+  ASSERT_FALSE(header.has_value());
+}
+
+TEST(SignedExchangeHeaderTest, UppercaseResponseMap) {
+  auto header = GenerateHeaderAndParse(
+      {
+          {kUrlKey, "https://test.example.org/test/"}, {kMethodKey, "GET"},
+      },
+      {{kStatusKey, "200"},
+       {kSignature, kSignatureString},
+       {"Content-Length", "123"}});
   ASSERT_FALSE(header.has_value());
 }
 
