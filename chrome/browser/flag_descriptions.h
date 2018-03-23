611d43e99f2270892d61275378086defb345a60d
Elly Fong-Jones <ellyjones@chromium.org>
2018-03-23 19:21:53 +0000

    polychrome: add views-browser-windows flag
    Bug: 802257
    Change-Id: I5b731977cb8ec78dc45caa3c600da0453df64ab2
    Reviewed-on: https://chromium-review.googlesource.com/978029
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545552}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1022,6 +1022,9 @@ extern const char kVideoFullscreenOrientationLockDescription[];
 extern const char kVideoRotateToFullscreenName[];
 extern const char kVideoRotateToFullscreenDescription[];
 
+extern const char kViewsBrowserWindowsName[];
+extern const char kViewsBrowserWindowsDescription[];
+
 extern const char kWalletServiceUseSandboxName[];
 extern const char kWalletServiceUseSandboxDescription[];
 
