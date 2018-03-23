548233137eb0af56f0f7e177db386e8e844324f7
Gayane Petrosyan <gayane@chromium.org>
2018-03-23 01:47:42 +0000

    Use interactive UI tests for Chrome visibility observer tests.
    Use interactive UI tests for Chrome visibility observer tests and change the logic of the test:
    - Now test class inherits both from InProcessBrowserTest and ChromeVisibilityObserver,
    Previously observer was created after first browser creation that why observer and browser states were not in sync in the beginning of the test
    - Deactivate doesn't works for mac
    - For tests only, when browser is deactivated observer is notified immediately instead of delayed task.
    
    Bug: 817172
    Change-Id: I71c0e8072210af9999e9acee7910ca435d7e1d80
    Reviewed-on: https://chromium-review.googlesource.com/951409
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Gayane Petrosyan <gayane@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545335}

--- chrome/browser/metrics/desktop_session_duration/chrome_visibility_observer.cc
+++ chrome/browser/metrics/desktop_session_duration/chrome_visibility_observer.cc
@@ -42,12 +42,16 @@ void ChromeVisibilityObserver::OnBrowserSetLastActive(Browser* browser) {
 }
 
 void ChromeVisibilityObserver::OnBrowserNoLongerActive(Browser* browser) {
-  base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
-      FROM_HERE,
-      base::BindOnce(&ChromeVisibilityObserver::SendVisibilityChangeEvent,
-                     weak_factory_.GetWeakPtr(), false,
-                     visibility_gap_timeout_),
-      visibility_gap_timeout_);
+  if (visibility_gap_timeout_.InMicroseconds() == 0) {
+    SendVisibilityChangeEvent(false, base::TimeDelta());
+  } else {
+    base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
+        FROM_HERE,
+        base::BindOnce(&ChromeVisibilityObserver::SendVisibilityChangeEvent,
+                       weak_factory_.GetWeakPtr(), false,
+                       visibility_gap_timeout_),
+        visibility_gap_timeout_);
+  }
 }
 
 void ChromeVisibilityObserver::OnBrowserRemoved(Browser* browser) {
@@ -69,6 +73,11 @@ void ChromeVisibilityObserver::InitVisibilityGapTimeout() {
     base::StringToInt(param_value, &timeout_seconds);
 
   visibility_gap_timeout_ = base::TimeDelta::FromSeconds(timeout_seconds);
+}
+
+void ChromeVisibilityObserver::SetVisibilityGapTimeoutForTesting(
+    base::TimeDelta timeout) {
+  visibility_gap_timeout_ = timeout;
 }
 
 }  // namespace metrics
