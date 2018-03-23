4c2215c2f6900a17a75cfd93faf7af25e018c49e
Gauthier Ambard <gambard@chromium.org>
2018-03-23 13:56:22 +0000

    Adjust PopupMenu popup position
    This CL changes the popup menu so it has a minimal width and a margin
    between the top and bottom edges.
    
    Bug: 821765
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I3953f4db951b8dd17090caa5b3e30074b72b1682
    Reviewed-on: https://chromium-review.googlesource.com/976001
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545422}

--- ios/chrome/browser/ui/popup_menu/popup_menu_presenter.mm
+++ ios/chrome/browser/ui/popup_menu/popup_menu_presenter.mm
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 const CGFloat kMinHeight = 200;
+const CGFloat kMinWidth = 200;
 const CGFloat kMaxWidth = 250;
 const CGFloat kMaxHeight = 400;
 const CGFloat kMinMargin = 16;
@@ -147,10 +148,12 @@ const CGFloat kMinMargin = 16;
                                  constant:-kMinMargin],
     [container.heightAnchor constraintLessThanOrEqualToConstant:kMaxHeight],
     [container.widthAnchor constraintLessThanOrEqualToConstant:kMaxWidth],
+    [container.widthAnchor constraintGreaterThanOrEqualToConstant:kMinWidth],
     [container.bottomAnchor
-        constraintLessThanOrEqualToAnchor:safeArea.bottomAnchor],
-    [container.topAnchor
-        constraintGreaterThanOrEqualToAnchor:safeArea.topAnchor],
+        constraintLessThanOrEqualToAnchor:safeArea.bottomAnchor
+                                 constant:-kMinMargin],
+    [container.topAnchor constraintGreaterThanOrEqualToAnchor:safeArea.topAnchor
+                                                     constant:kMinMargin],
   ]];
   NSLayoutConstraint* leading = [container.leadingAnchor
       constraintEqualToAnchor:namedGuide.leadingAnchor];
