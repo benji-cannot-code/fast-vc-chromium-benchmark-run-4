5d9565c50c40854186aa81c896137f0a6f312063
Nektarios Paisios <nektar@chromium.org>
2018-03-23 17:50:19 +0000

    Ensures that Web notifications are announced as soon as they appear
    Switches to using an accessibility role of alert dialog which is more appropriate for a notification toast than a window role.
    Alert dialogs are widgets that may contain interactive controls that need to be announced by a screen reader as soon as they appear.
    Also, an alert event should be fired regardless to whether the notification has been updated or has been initiallly set.
    R=aleventhal@chromium.org, dmazzoni@chromium.org, dewittj@chromium.org, peter@chromium.org
    
    Tested: Manually with Jaws and NVDA screen readers on Windows
    Change-Id: Ida3abe94b9dc32a5c95ce6b14c6fac11670348e3
    Reviewed-on: https://chromium-review.googlesource.com/968672
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545510}

--- ui/message_center/views/message_view.cc
+++ ui/message_center/views/message_view.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "base/strings/utf_string_conversions.h"
+#include "ui/accessibility/ax_enums.mojom.h"
 #include "ui/accessibility/ax_node_data.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/models/simple_menu_model.h"
@@ -95,7 +96,11 @@ MessageView::~MessageView() {}
 
 void MessageView::UpdateWithNotification(const Notification& notification) {
   pinned_ = notification.pinned();
-  accessible_name_ = CreateAccessibleName(notification);
+  base::string16 new_accessible_name = CreateAccessibleName(notification);
+  if (new_accessible_name != accessible_name_) {
+    accessible_name_ = new_accessible_name;
+    NotifyAccessibilityEvent(ax::mojom::Event::kTextChanged, true);
+  }
   slide_out_controller_.set_enabled(!GetPinned());
 }
 
