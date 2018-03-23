ff08346cebc27e8b7da85f6071ffdbb1f4214761
edchin <edchin@chromium.org>
2018-03-23 12:03:29 +0000

    [ios] Fix tab grid crash when closing last incognito tab
    Previously, the app crashes when closing the last incognito tab in the
    tab grid. When the last incognito tab is closed, the incognito TabModel
    is destroyed and a new one is created.
    
    The app crash was due to the tab grid mediator not being told about the
    destruction of the old model. In fact, the mechanism to tell the
    mediator was created. But it was not hooked up.
    
    This CL makes a one-line change that hooks up the mediator.
    
    Bug: 822515
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I9889eedb182384acb9e52ab7bbf65aeacef8b867
    Reviewed-on: https://chromium-review.googlesource.com/977046
    Reviewed-by: edchin <edchin@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545408}

--- ios/chrome/browser/ui/tab_grid/tab_grid_coordinator.mm
+++ ios/chrome/browser/ui/tab_grid/tab_grid_coordinator.mm
@@ -104,6 +104,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   self.incognitoTabsMediator = [[TabGridMediator alloc]
       initWithConsumer:mainViewController.incognitoTabsConsumer];
   self.incognitoTabsMediator.tabModel = self.incognitoTabModel;
+  self.adaptor.incognitoMediator = self.incognitoTabsMediator;
   mainViewController.regularTabsDelegate = self.regularTabsMediator;
   mainViewController.incognitoTabsDelegate = self.incognitoTabsMediator;
   mainViewController.regularTabsImageDataSource = self.regularTabsMediator;
