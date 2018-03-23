6e1c1c9da67bd104c4ff5728e429146c832a2999
Filip Gorski <fgorski@chromium.org>
2018-03-23 16:12:37 +0000

    [EoC] Add bridge for contextual suggestions
    * Adds a bridge for contextual suggestions with method signatures
    * Adds a cluster type that is going to be used to group contextual
      suggestions
    
    Bug: 824182
    Change-Id: I0b8a44cd438b8e27b378d52a15e44b8fd3060044
    Reviewed-on: https://chromium-review.googlesource.com/971306
    Commit-Queue: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@google.com>
    Cr-Commit-Position: refs/heads/master@{#545472}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/contextual_suggestions/ContextualSuggestionsCluster.java
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.contextual_suggestions;
+
+import org.chromium.chrome.browser.ntp.snippets.SnippetArticle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/** Class to group contextual suggestions in a cluser of related items. */
+public class ContextualSuggestionsCluster {
+    private final String mTitle;
+    private final List<SnippetArticle> mSuggestions = new ArrayList<>();
+
+    /** Creates a new contextual suggestions cluster with provided title. */
+    public ContextualSuggestionsCluster(String title) {
+        mTitle = title;
+    }
+
+    /** @return a title related to this cluster */
+    public String getTitle() {
+        return mTitle;
+    }
+
+    /** @return a list of suggestions in this cluster */
+    public List<SnippetArticle> getSuggestions() {
+        return mSuggestions;
+    }
+}
--- components/ntp_snippets/contextual/contextual_content_suggestions_service.h
+++ components/ntp_snippets/contextual/contextual_content_suggestions_service.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/image_fetcher/core/image_fetcher.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/ntp_snippets/callbacks.h"
+#include "components/ntp_snippets/content_suggestion.h"
 #include "components/ntp_snippets/contextual/contextual_suggestions_fetcher.h"
 
 namespace ntp_snippets {
@@ -27,6 +28,29 @@ class RemoteSuggestionsDatabase;
 // for contextual suggestion, using caching.
 class ContextualContentSuggestionsService : public KeyedService {
  public:
+  // A structure representing a suggestion cluster.
+  struct Cluster {
+    Cluster();
+    ~Cluster();
+
+    std::string title;
+    std::vector<ContentSuggestion> suggestions;
+
+    DISALLOW_COPY_AND_ASSIGN(Cluster);
+  };
+
+  // Delegate for UI to hook into contextual suggestions service.
+  class Delegate {
+   public:
+    virtual ~Delegate() = default;
+
+    // Sends the new suggestions to the delegate, when they are available.
+    virtual void OnSuggestionsAvailable(std::vector<Cluster> clusters) = 0;
+    // Informs the delegate that user has switched to another tab or clobbered
+    // the current tab, therefore the UI state should be cleared.
+    virtual void OnStateCleared() = 0;
+  };
+
   ContextualContentSuggestionsService(
       std::unique_ptr<ContextualSuggestionsFetcher>
           contextual_suggestions_fetcher,
