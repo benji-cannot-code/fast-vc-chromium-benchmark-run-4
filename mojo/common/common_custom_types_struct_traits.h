0849bb19b226ece2d8bbcca4f1af9f02eff49a44
Oksana Zhuravlova <oksamyt@chromium.org>
2018-03-23 20:21:49 +0000

    Move thread_priority.mojom from common to base
    This change moves thread_priority.mojom from //mojo/common to
    //mojo/public/mojom/base. Corresponding typemap and struct traits files
    were also moved. Unit test for ThreadPriority was added to mojo_unittests.
    
    Bug: 799482,806965
    Change-Id: I47c23a6b5eea8fffd8a6aa9c18f9e8e12045e8e7
    Reviewed-on: https://chromium-review.googlesource.com/976621
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545567}

--- mojo/common/common_custom_types_struct_traits.h
+++ mojo/common/common_custom_types_struct_traits.h
@@ -9,12 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/process/process_handle.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/trace_event/memory_allocator_dump_guid.h"
-#include "base/unguessable_token.h"
 #include "mojo/common/memory_allocator_dump_cross_process_uid.mojom-shared.h"
 #include "mojo/common/mojo_common_export.h"
 #include "mojo/common/process_id.mojom-shared.h"
-#include "mojo/common/thread_priority.mojom-shared.h"
-#include "mojo/public/mojom/base/unguessable_token.mojom-shared.h"
 
 namespace mojo {
 
@@ -29,14 +26,6 @@ struct StructTraits<common::mojom::ProcessIdDataView, base::ProcessId> {
     *process_id = static_cast<base::ProcessId>(data.pid());
     return true;
   }
-};
-
-template <>
-struct EnumTraits<common::mojom::ThreadPriority, base::ThreadPriority> {
-  static common::mojom::ThreadPriority ToMojom(
-      base::ThreadPriority thread_priority);
-  static bool FromMojom(common::mojom::ThreadPriority input,
-                        base::ThreadPriority* out);
 };
 
 template <>
