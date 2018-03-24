cf98d0e1bb8194dc67283483352102dd86cfe3bb
Nasko Oskov <nasko@chromium.org>
2018-03-24 06:13:13 +0000

    Add checks to WCOSanityChecker for consistency between navigation callbacks
    This CL tries to verify that whenever WCO::ReadyToCommitNavigation has
    been dispatched, subsequent WCO::DidFinishNavigation will return the same
    RenderFrameHost consistently.
    
    Change-Id: I3797b6c55b038941a65a92aa3439526e3ca62537
    Reviewed-on: https://chromium-review.googlesource.com/977050
    Commit-Queue: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545675}

--- content/test/web_contents_observer_sanity_checker.cc
+++ content/test/web_contents_observer_sanity_checker.cc
@@ -195,6 +195,10 @@ void WebContentsObserverSanityChecker::ReadyToCommitNavigation(
   CHECK(navigation_handle->GetRenderFrameHost());
   CHECK_EQ(navigation_handle->GetWebContents(), web_contents());
   CHECK(navigation_handle->GetRenderFrameHost() != nullptr);
+
+  ready_to_commit_hosts_.insert(
+      std::make_pair(navigation_handle->GetNavigationId(),
+                     navigation_handle->GetRenderFrameHost()));
 }
 
 void WebContentsObserverSanityChecker::DidFinishNavigation(
@@ -208,6 +212,16 @@ void WebContentsObserverSanityChecker::DidFinishNavigation(
 
   CHECK(!navigation_handle->HasCommitted() ||
         navigation_handle->GetRenderFrameHost() != nullptr);
+
+  // If ReadyToCommitNavigation was dispatched, verify that the
+  // |navigation_handle| has the same RenderFrameHost at this time as the one
+  // returned at ReadyToCommitNavigation.
+  if (base::ContainsKey(ready_to_commit_hosts_,
+                        navigation_handle->GetNavigationId())) {
+    CHECK_EQ(ready_to_commit_hosts_[navigation_handle->GetNavigationId()],
+             navigation_handle->GetRenderFrameHost());
+    ready_to_commit_hosts_.erase(navigation_handle->GetNavigationId());
+  }
 
   ongoing_navigations_.erase(navigation_handle);
 }
@@ -314,6 +328,7 @@ WebContentsObserverSanityChecker::WebContentsObserverSanityChecker(
 
 WebContentsObserverSanityChecker::~WebContentsObserverSanityChecker() {
   CHECK(web_contents_destroyed_);
+  CHECK(ready_to_commit_hosts_.empty());
 }
 
 void WebContentsObserverSanityChecker::AssertRenderFrameExists(
