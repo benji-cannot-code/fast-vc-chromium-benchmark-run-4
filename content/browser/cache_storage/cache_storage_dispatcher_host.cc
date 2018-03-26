f006789b36ecab6be274bc5e9517c255da9b38f7
Luciano Pacheco <lucmult@chromium.org>
2018-03-26 10:26:22 +0000

    Remove Cache Storage API IPC messages
    content/common/cache_storage/cache_storage_messages.h has been converted
    to Mojo thus should be removed on crrev.com/c/612287
    
    Bug: 612287
    Change-Id: I88db5d0798891d218c5f1bfdd312a758a45152ff
    Reviewed-on: https://chromium-review.googlesource.com/907769
    Commit-Queue: Luciano Pacheco (SYD) <lucmult@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Chris Mumford <cmumford@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545755}

--- content/browser/cache_storage/cache_storage_dispatcher_host.cc
+++ content/browser/cache_storage/cache_storage_dispatcher_host.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/cache_storage/cache_storage_cache_handle.h"
 #include "content/browser/cache_storage/cache_storage_context_impl.h"
 #include "content/browser/cache_storage/cache_storage_manager.h"
-#include "content/common/cache_storage/cache_storage_messages.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/content_browser_client.h"
 #include "content/public/common/origin_util.h"
--- content/renderer/cache_storage/webserviceworkercachestorage_impl.cc
+++ content/renderer/cache_storage/webserviceworkercachestorage_impl.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_local.h"
 #include "content/child/child_thread_impl.h"
 #include "content/child/thread_safe_sender.h"
-#include "content/common/cache_storage/cache_storage_messages.h"
 #include "content/public/common/content_features.h"
 #include "content/public/common/referrer.h"
 #include "content/public/common/service_names.mojom.h"
