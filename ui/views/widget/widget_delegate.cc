e45d6f8c891a1b5b07a923dd303a5450b42a9c08
Trent Apted <tapted@chromium.org>
2018-03-26 21:14:08 +0000

    CHECK() if a WidgetDelegate would be deleted before the Widget it initialized is deleted.
    WidgetDelegates must outlive their Widget.
    
    Crashes suggest situations where they may not, but can't pinpoint the actual
    client code. The crash is instead attributed to a UAF on the widget delegate
    either in a layout call (during an animation), or when
    Widget::OnNativeWidgetDestroyed() tries to delete the delegate (again).
    
    This CL adds a CHECK() in ~WidgetDelegate which should track down the
    misbehaving code.
    
    Bug: 825091, 814821, 808318
    Change-Id: I9490ff1682ee236436edf075f7c237ccf910c44c
    Reviewed-on: https://chromium-review.googlesource.com/977244
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545873}

--- ui/views/widget/widget_delegate.cc
+++ ui/views/widget/widget_delegate.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/views/widget/widget_delegate.h"
 
+#include "base/logging.h"
 #include "base/strings/utf_string_conversions.h"
 #include "services/ui/public/interfaces/window_manager_constants.mojom.h"
 #include "ui/gfx/image/image_skia.h"
@@ -18,9 +19,9 @@ namespace views {
 ////////////////////////////////////////////////////////////////////////////////
 // WidgetDelegate:
 
-WidgetDelegate::WidgetDelegate()
-    : default_contents_view_(NULL),
-      can_activate_(true) {
+WidgetDelegate::WidgetDelegate() = default;
+WidgetDelegate::~WidgetDelegate() {
+  CHECK(can_delete_this_) << "A WidgetDelegate must outlive its Widget";
 }
 
 void WidgetDelegate::OnWidgetMove() {
