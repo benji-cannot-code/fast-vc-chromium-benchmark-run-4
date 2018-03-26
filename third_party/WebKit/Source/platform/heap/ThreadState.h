badaeda60ee8ec6a60a438eb12094e54413f30db
Keishi Hattori <keishi@chromium.org>
2018-03-26 12:07:40 +0000

    Oilpan: Introduce GCPhase
    Introducing the GCPhase enum. In the future, this will be used to simplify the GCState into just handling the next scheduled GC. Right now this isn't connected to anything yet.
    
    Bug: 757440
    Change-Id: If91c6d11c2ee0f53e50633b9c74475f8dc256952
    Reviewed-on: https://chromium-review.googlesource.com/979374
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545764}

--- third_party/WebKit/Source/platform/heap/ThreadState.h
+++ third_party/WebKit/Source/platform/heap/ThreadState.h
@@ -156,6 +156,18 @@ class PLATFORM_EXPORT ThreadState {
     kSweepingAndPreciseGCScheduled,
   };
 
+  // The phase that the GC is in. The GCPhase will not return kNone for mutators
+  // running during incremental marking and lazy sweeping. See SetGCPhase() for
+  // possible state transitions.
+  enum class GCPhase {
+    // GC is doing nothing.
+    kNone,
+    // GC is in marking phase.
+    kMarking,
+    // GC is in sweeping phase.
+    kSweeping,
+  };
+
   // The NoAllocationScope class is used in debug mode to catch unwanted
   // allocations. E.g. allocations during GC.
   class NoAllocationScope final {
@@ -254,7 +266,9 @@ class PLATFORM_EXPORT ThreadState {
   void WillStartV8GC(BlinkGC::V8GCType);
   void SetGCState(GCState);
   GCState GcState() const { return gc_state_; }
+  void SetGCPhase(GCPhase);
   bool IsInGC() const { return GcState() == kGCRunning; }
+  bool IsMarkingInProgress() const { return gc_phase_ == GCPhase::kMarking; }
   bool IsSweepingInProgress() const {
     return GcState() == kSweeping ||
            GcState() == kSweepingAndPreciseGCScheduled ||
@@ -645,6 +659,7 @@ class PLATFORM_EXPORT ThreadState {
   GarbageCollectedMixinConstructorMarkerBase* gc_mixin_marker_;
 
   GCState gc_state_;
+  GCPhase gc_phase_;
 
   using PreFinalizerCallback = bool (*)(void*);
   using PreFinalizer = std::pair<void*, PreFinalizerCallback>;
