dd62d7205d20d9c6432e0d9201078eaae7293c06
Yutaka Hirano <yhirano@chromium.org>
2018-03-26 12:28:42 +0000

    Have WebSocket throttling work with Network Service
    This CL introduces WebSocket throttling on network::WebSocketFactory by
    moving some logic from content/browser/websockets to services/network.
    
    Bug: 721400
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: Ie007765411d95d1854fc0271652d599e5ccbcd0c
    Reviewed-on: https://chromium-review.googlesource.com/979872
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545766}

--- content/browser/websockets/websocket_manager.h
+++ content/browser/websockets/websocket_manager.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/url_request_context_getter.h"
 #include "net/url_request/url_request_context_getter_observer.h"
 #include "services/network/websocket.h"
+#include "services/network/websocket_throttler.h"
 
 namespace content {
 class StoragePartition;
@@ -59,20 +60,19 @@ class CONTENT_EXPORT WebSocketManager
   void DoCreateWebSocket(int frame_id,
                          url::Origin origin,
                          network::mojom::WebSocketRequest request);
-  base::TimeDelta CalculateDelay() const;
   void ThrottlingPeriodTimerCallback();
 
   // This is virtual to support testing.
   virtual std::unique_ptr<network::WebSocket> CreateWebSocket(
       std::unique_ptr<network::WebSocket::Delegate> delegate,
       network::mojom::WebSocketRequest request,
+      network::WebSocketThrottler::PendingConnection pending_connection_tracker,
       int child_id,
       int frame_id,
       url::Origin origin,
       base::TimeDelta delay);
 
   net::URLRequestContext* GetURLRequestContext();
-  void OnReceivedResponseFromServer(network::WebSocket* impl);
   virtual void OnLostConnectionToClient(network::WebSocket* impl);
 
   void ObserveURLRequestContextGetter();
@@ -86,18 +86,7 @@ class CONTENT_EXPORT WebSocketManager
   // Timer and counters for per-renderer WebSocket throttling.
   base::RepeatingTimer throttling_period_timer_;
 
-  // The current number of pending connections.
-  int num_pending_connections_;
-
-  // The number of handshakes that failed in the current and previous time
-  // period.
-  int64_t num_current_succeeded_connections_;
-  int64_t num_previous_succeeded_connections_;
-
-  // The number of handshakes that succeeded in the current and previous time
-  // period.
-  int64_t num_current_failed_connections_;
-  int64_t num_previous_failed_connections_;
+  network::WebSocketPerProcessThrottler throttler_;
 
   bool context_destroyed_;
 
--- services/network/websocket.h
+++ services/network/websocket.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/binding.h"
 #include "net/websockets/websocket_event_interface.h"
 #include "services/network/public/mojom/websocket.mojom.h"
+#include "services/network/websocket_throttler.h"
 #include "url/origin.h"
 
 class GURL;
@@ -45,7 +46,6 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) WebSocket
     virtual ~Delegate() {}
 
     virtual net::URLRequestContext* GetURLRequestContext() = 0;
-    virtual void OnReceivedResponseFromServer(WebSocket* impl) = 0;
     // This function may delete |impl|.
     virtual void OnLostConnectionToClient(WebSocket* impl) = 0;
     virtual void OnSSLCertificateError(
@@ -66,6 +66,7 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) WebSocket
 
   WebSocket(std::unique_ptr<Delegate> delegate,
             network::mojom::WebSocketRequest request,
+            WebSocketThrottler::PendingConnection pending_connection_tracker,
             int child_id,
             int frame_id,
             url::Origin origin,
@@ -89,7 +90,6 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) WebSocket
   void StartClosingHandshake(uint16_t code, const std::string& reason) override;
 
   bool handshake_succeeded() const { return handshake_succeeded_; }
-  void OnHandshakeSucceeded() { handshake_succeeded_ = true; }
 
  protected:
   class WebSocketEventHandler;
@@ -104,6 +104,8 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) WebSocket
   mojo::Binding<network::mojom::WebSocket> binding_;
 
   network::mojom::WebSocketClientPtr client_;
+
+  WebSocketThrottler::PendingConnection pending_connection_tracker_;
 
   // The channel we use to send events to the network.
   std::unique_ptr<net::WebSocketChannel> channel_;
