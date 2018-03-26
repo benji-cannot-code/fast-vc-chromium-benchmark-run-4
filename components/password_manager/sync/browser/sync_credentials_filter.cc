a678645d46d7f622d0ae441cc7eb2debec9ea317
Mihai Sardarescu <msarda@chromium.org>
2018-03-26 15:05:53 +0000

    [dice] Avoid saving passwords when user is signing in to Chrome.
    This CL avoids saving passwords when the user is signing in to Chrome
    via the Desktop Identity Consistency flow. It basically avoids saving
    password for URLs with origin "accounts.google.com" and that have
    the URL parameter "ssp=1".
    
    This CL also reverts https://chromium-review.googlesource.com/888623
    which was a poor attempt to fix the same bug.
    
    To review the code:
    * Patch 1 is the revert of https://chromium-review.googlesource.com/888623
    * Following patches contain the real fix.
    
    Bug: 805826
    Change-Id: I947b63cea5e2a875260fc9d6066eed696198e35f
    Reviewed-on: https://chromium-review.googlesource.com/979932
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545789}

--- components/password_manager/sync/browser/sync_credentials_filter.cc
+++ components/password_manager/sync/browser/sync_credentials_filter.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/password_manager_util.h"
 #include "components/password_manager/core/common/password_manager_features.h"
 #include "components/password_manager/sync/browser/password_sync_util.h"
-#include "google_apis/gaia/gaia_auth_util.h"
 #include "google_apis/gaia/gaia_urls.h"
 #include "net/base/url_util.h"
 
@@ -66,7 +65,7 @@ std::vector<std::unique_ptr<PasswordForm>> SyncCredentialsFilter::FilterResults(
   auto begin_of_removed =
       std::partition(results.begin(), results.end(),
                      [this](const std::unique_ptr<PasswordForm>& form) {
-                       return ShouldSave(*form, form->origin);
+                       return ShouldSave(*form);
                      });
 
   UMA_HISTOGRAM_BOOLEAN("PasswordManager.SyncCredentialFiltered",
@@ -77,12 +76,11 @@ std::vector<std::unique_ptr<PasswordForm>> SyncCredentialsFilter::FilterResults(
   return results;
 }
 
-bool SyncCredentialsFilter::ShouldSave(const autofill::PasswordForm& form,
-                                       const GURL& main_frame_url) const {
-  return !gaia::ShouldSkipSavePasswordForGaiaURL(main_frame_url) &&
-         !sync_util::IsSyncAccountCredential(
-             form, sync_service_factory_function_.Run(),
-             signin_manager_factory_function_.Run(), client_->GetPrefs());
+bool SyncCredentialsFilter::ShouldSave(
+    const autofill::PasswordForm& form) const {
+  return !sync_util::IsSyncAccountCredential(
+      form, sync_service_factory_function_.Run(),
+      signin_manager_factory_function_.Run(), client_->GetPrefs());
 }
 
 void SyncCredentialsFilter::ReportFormLoginSuccess(
