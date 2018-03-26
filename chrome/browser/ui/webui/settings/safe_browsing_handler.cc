01ca44729adca6a7f1fb2bf6c0b73e192698c61f
Luke Zielinski <lpz@chromium.org>
2018-03-26 15:40:00 +0000

    Return consistent data to privacy page UI for SBER.
    This fixes a regression where the SBER checkbox sometimes had to be
    clicked twice to be toggled.
    
    Bug: 819203
    Change-Id: Iad26a6eaa33e4798a086c297fd11540c7c2674e8
    Reviewed-on: https://chromium-review.googlesource.com/978629
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Luke Z <lpz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545801}

--- chrome/browser/ui/webui/settings/safe_browsing_handler.cc
+++ chrome/browser/ui/webui/settings/safe_browsing_handler.cc
@@ -14,6 +14,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace settings {
 
+namespace {
+
+base::DictionaryValue GetSberStateDictionaryValue(const PrefService& prefs) {
+  base::DictionaryValue dict;
+  dict.SetBoolean("enabled", safe_browsing::IsExtendedReportingEnabled(prefs));
+  // TODO(crbug.com/813107): SBEROIA policy is being deprecated, revisit this
+  // after it is removed.
+  dict.SetBoolean("managed",
+                  !safe_browsing::IsExtendedReportingOptInAllowed(prefs) ||
+                      safe_browsing::IsExtendedReportingPolicyManaged(prefs));
+  return dict;
+}
+
+}  // namespace
+
 SafeBrowsingHandler::SafeBrowsingHandler(PrefService* prefs) : prefs_(prefs) {}
 SafeBrowsingHandler::~SafeBrowsingHandler() {}
 
@@ -50,16 +65,7 @@ void SafeBrowsingHandler::HandleGetSafeBrowsingExtendedReporting(
   const base::Value* callback_id;
   CHECK(args->Get(0, &callback_id));
 
-  base::DictionaryValue dict;
-  dict.SetBoolean("enabled",
-                  safe_browsing::IsExtendedReportingEnabled(*prefs_));
-  // TODO(crbug.com/813107): SBEROIA policy is being deprecated, revisit this
-  // after it is removed.
-  dict.SetBoolean("managed",
-                  !safe_browsing::IsExtendedReportingOptInAllowed(*prefs_) ||
-                      safe_browsing::IsExtendedReportingPolicyManaged(*prefs_));
-
-  ResolveJavascriptCallback(*callback_id, dict);
+  ResolveJavascriptCallback(*callback_id, GetSberStateDictionaryValue(*prefs_));
 }
 
 void SafeBrowsingHandler::HandleSetSafeBrowsingExtendedReportingEnabled(
@@ -74,8 +80,8 @@ void SafeBrowsingHandler::OnPrefChanged(const std::string& pref_name) {
   DCHECK(pref_name == prefs::kSafeBrowsingExtendedReportingEnabled ||
          pref_name == prefs::kSafeBrowsingScoutReportingEnabled);
 
-  base::Value is_enabled(safe_browsing::IsExtendedReportingEnabled(*prefs_));
-  FireWebUIListener("safe-browsing-extended-reporting-change", is_enabled);
+  FireWebUIListener("safe-browsing-extended-reporting-change",
+                    GetSberStateDictionaryValue(*prefs_));
 }
 
 }  // namespace settings
