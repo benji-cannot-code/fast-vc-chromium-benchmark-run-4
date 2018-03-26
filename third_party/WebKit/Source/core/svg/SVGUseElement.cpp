65aae1e7af9676e84194c17bda977d24d6236394
Fredrik Söderquist <fs@opera.com>
2018-03-26 18:57:46 +0000

    Remove outdated DCHECK in SVGUseElement::InsertedInto
    This DCHECK was made obsolete by
    9b88406d1a87dfeef593c6d1a91beb3a2b696391, since it stopped using the
    "pending resource" flag for <use>.
    
    Bug: 825460
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Ie1202097eb2172ef9ed3866e3a6575fbe4aa09db
    Reviewed-on: https://chromium-review.googlesource.com/980252
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#545854}

--- third_party/WebKit/Source/core/svg/SVGUseElement.cpp
+++ third_party/WebKit/Source/core/svg/SVGUseElement.cpp
@@ -122,7 +122,6 @@ Node::InsertionNotificationRequest SVGUseElement::InsertedInto(
     return kInsertionDone;
 #if DCHECK_IS_ON()
   DCHECK(!target_element_instance_ || !IsWellFormedDocument(&GetDocument()));
-  DCHECK(!HasPendingResources() || !IsWellFormedDocument(&GetDocument()));
 #endif
   InvalidateShadowTree();
   return kInsertionDone;
