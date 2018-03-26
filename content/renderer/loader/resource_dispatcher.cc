b3cd711199904841784e02123fdd61c199e2331d
Conley Owens <cco3@chromium.org>
2018-03-26 22:21:52 +0000

    Report mime_type in SubresourceLoadInfo
    This change adds a string `mime_type` to the list of fields in
    SubresourceLoadInfo.  Notably, this is important for recording page
    load metrics when the Network Service is enabled.
    
    BUG=816684
    
    Change-Id: Ie95bd46074f2a0f915eb8378800afa0f5ed46ecd
    Reviewed-on: https://chromium-review.googlesource.com/963312
    Reviewed-by: Jay Civelli <jcivelli@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Conley Owens <cco3@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545877}

--- content/renderer/loader/resource_dispatcher.cc
+++ content/renderer/loader/resource_dispatcher.cc
@@ -179,6 +179,7 @@ void ResourceDispatcher::OnReceivedResponse(
   }
 
   if (!IsResourceTypeFrame(request_info->resource_type)) {
+    request_info->mime_type = response_head.mime_type;
     NotifySubresourceStarted(RenderThreadImpl::DeprecatedGetMainTaskRunner(),
                              request_info->render_frame_id,
                              request_info->response_url,
@@ -274,6 +275,7 @@ void ResourceDispatcher::OnRequestComplete(
     subresource_load_info->resource_type = request_info->resource_type;
     if (request_info->parsed_ip.IsValid())
       subresource_load_info->ip = request_info->parsed_ip;
+    subresource_load_info->mime_type = request_info->mime_type;
     subresource_load_info->was_cached = status.exists_in_cache;
     NotifySubresourceLoadComplete(
         RenderThreadImpl::DeprecatedGetMainTaskRunner(),
