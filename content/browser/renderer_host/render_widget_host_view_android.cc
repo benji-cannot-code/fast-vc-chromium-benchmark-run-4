0e5a58091ce4769a625bbf9833d25abbadc013eb
AJITH KUMAR V <ajith.v@samsung.com>
2018-03-26 18:32:58 +0000

    Hiding touch handles during stylus button selection
    While doing stylus button text selection, focus is mainly
    on the stylus button press and text selection is anchored
    by the stylus button. In that case touch handle has less
    significance (same with touch long press drag selection).
    So we can hide touch handles during stylus button selection
    and can show after stylus button selection is concluded.
    
    Bug: 771667
    Change-Id: I9dccaabfd5fafccb68c1f0bce23bba36cea9061f
    Reviewed-on: https://chromium-review.googlesource.com/685054
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: AJITH KUMAR V <ajith.v@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#545846}

--- content/browser/renderer_host/render_widget_host_view_android.cc
+++ content/browser/renderer_host/render_widget_host_view_android.cc
@@ -1789,9 +1789,14 @@ void RenderWidgetHostViewAndroid::DismissTextHandles() {
     touch_selection_controller_->HideAndDisallowShowingAutomatically();
 }
 
-void RenderWidgetHostViewAndroid::SetTextHandlesTemporarilyHidden(bool hidden) {
-  if (touch_selection_controller_)
-    touch_selection_controller_->SetTemporarilyHidden(hidden);
+void RenderWidgetHostViewAndroid::SetTextHandlesTemporarilyHidden(
+    bool hide_handles) {
+  if (!touch_selection_controller_ ||
+      handles_hidden_by_selection_ui_ == hide_handles)
+    return;
+  handles_hidden_by_selection_ui_ = hide_handles;
+  touch_selection_controller_->SetTemporarilyHidden(
+      handles_hidden_by_selection_ui_ || handles_hidden_by_stylus_);
 }
 
 SkColor RenderWidgetHostViewAndroid::GetCachedBackgroundColor() const {
@@ -2111,10 +2116,20 @@ void RenderWidgetHostViewAndroid::OnLostResources() {
   DCHECK(ack_callbacks_.empty());
 }
 
+void RenderWidgetHostViewAndroid::SetTextHandlesHiddenForStylus(
+    bool hide_handles) {
+  if (!touch_selection_controller_ || handles_hidden_by_stylus_ == hide_handles)
+    return;
+  handles_hidden_by_stylus_ = hide_handles;
+  touch_selection_controller_->SetTemporarilyHidden(
+      handles_hidden_by_stylus_ || handles_hidden_by_selection_ui_);
+}
+
 void RenderWidgetHostViewAndroid::OnStylusSelectBegin(float x0,
                                                       float y0,
                                                       float x1,
                                                       float y1) {
+  SetTextHandlesHiddenForStylus(true);
   SelectBetweenCoordinates(gfx::PointF(x0, y0), gfx::PointF(x1, y1));
 }
 
@@ -2123,6 +2138,7 @@ void RenderWidgetHostViewAndroid::OnStylusSelectUpdate(float x, float y) {
 }
 
 void RenderWidgetHostViewAndroid::OnStylusSelectEnd(float x, float y) {
+  SetTextHandlesHiddenForStylus(false);
   ShowContextMenuAtPoint(gfx::Point(x, y), ui::MENU_SOURCE_STYLUS);
 }
 
