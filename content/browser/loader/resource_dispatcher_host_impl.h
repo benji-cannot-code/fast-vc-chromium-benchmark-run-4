4613c8864a9f187c0fab4b08fe26d924ede0e57b
Ben Wells <benwells@chromium.org>
2018-03-26 08:55:24 +0000

    Revert "RDH:  Ensure there's at most one LoadInfoList in flight."
    This reverts commit d03adf5a2c53f00c4663a93917a9fb0a53f082ad.
    
    Reason for revert:
    
    This has caused some tests to fail flakily and reflect a crash that could happen in the wild. See http://crbug.com/825448
    
    Original change's description:
    > RDH:  Ensure there's at most one LoadInfoList in flight.
    > 
    > ResourceDispatcherHost pushes pending load state of all frames to the UI
    > thread once every 250 milliseconds when there's a pending load.  This
    > happens even when the UI thread is blocked.  Apparently it's causing an
    > OOM when the UI thread is blocked.  Whatever is blocking the UI thread
    > is another issue entirely, but we shouldn't use unbounded memory in the
    > browser process.  This CL prevents load state updates from being sent
    > until the previous set was ACKed by the UI thread.
    > 
    > Bug: 824869
    > TBR: jam@chromium.org
    > Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    > Change-Id: I3b68522c5281d477d3a621ac02e5df1f75153d99
    > Reviewed-on: https://chromium-review.googlesource.com/978383
    > Commit-Queue: Matt Menke <mmenke@chromium.org>
    > Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    > Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#545616}
    
    TBR=jam@chromium.org,mmenke@chromium.org,csharrison@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 824869
    Change-Id: I350dd7b357c32ffc7fb85a2417afaf779552ff77
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Reviewed-on: https://chromium-review.googlesource.com/979674
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Commit-Queue: Ben Wells <benwells@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545741}

--- content/browser/loader/resource_dispatcher_host_impl.h
+++ content/browser/loader/resource_dispatcher_host_impl.h
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
-#include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
 #include "base/optional.h"
 #include "base/single_thread_task_runner.h"
@@ -49,7 +48,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 class FilePath;
-class OneShotTimer;
 class RepeatingTimer;
 }
 
@@ -537,14 +535,6 @@ class CONTENT_EXPORT ResourceDispatcherHostImpl
   // Checks all pending requests and updates the load info if necessary.
   void UpdateLoadInfo();
 
-  // Invoked on the IO thread once load state has been updated on the UI thread,
-  // starts timer call UpdateLoadInfo() again, if needed.
-  void AckUpdateLoadInfo();
-
-  // Starts the timer to call UpdateLoadInfo(), if timer isn't already running,
-  // |waiting_on_load_state_ack_| is false, and there are live ResourceLoaders.
-  void MaybeStartUpdateLoadInfoTimer();
-
   // Records statistics about outstanding requests since the last call, and
   // reset the stats.
   void RecordOutstandingRequestsStats();
@@ -729,13 +719,9 @@ class CONTENT_EXPORT ResourceDispatcherHostImpl
       RegisteredTempFiles;  // key is child process id
   RegisteredTempFiles registered_temp_files_;
 
-  // A timer that periodically calls UpdateLoadInfo while |pending_loaders_| is
-  // not empty, at least one RenderViewHost is loading, and not waiting on an
-  // ACK from the UI thread for the last sent LoadInfoList.
-  std::unique_ptr<base::OneShotTimer> update_load_info_timer_;
-  // True if a LoadInfoList has been sent to the UI thread, but has yet to be
-  // acknowledged.
-  bool waiting_on_load_state_ack_ = false;
+  // A timer that periodically calls UpdateLoadInfo while pending_loaders_ is
+  // not empty and at least one RenderViewHost is loading.
+  std::unique_ptr<base::RepeatingTimer> update_load_states_timer_;
 
   // A timer that periodically calls RecordOutstandingRequestsStats.
   std::unique_ptr<base::RepeatingTimer>
@@ -839,8 +825,6 @@ class CONTENT_EXPORT ResourceDispatcherHostImpl
 
   // Task runner for the IO thead.
   scoped_refptr<base::SingleThreadTaskRunner> io_thread_task_runner_;
-
-  base::WeakPtrFactory<ResourceDispatcherHostImpl> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(ResourceDispatcherHostImpl);
 };
