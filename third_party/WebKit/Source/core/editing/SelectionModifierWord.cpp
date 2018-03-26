87dc9b98d6a310780576d66dadd0c7d510944d5c
Yoshifumi Inoue <yosin@chromium.org>
2018-03-26 10:39:20 +0000

    Rename HonorEditingBoundaryAtOr{Before,After}() to Adjust{Back,Forward}PositionToAvoidCrossingEditingBoundaries()
    This patch renames |HonorEditingBoundaryAtOr{Before,After}()| to
    |Adjust{Back,Forward}PositionToAvoidCrossingEditingBoundaries()| to follow
    |SelectionAfjuster::AdjustSelecitonToAvoidCrossingEditingBoundaries()| for
    improving readability.
    
    
    Bug: 824687
    Change-Id: I156c09e884ea8ff805e920cdef928e473f7cb420
    Reviewed-on: https://chromium-review.googlesource.com/977343
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545759}

--- third_party/WebKit/Source/core/editing/SelectionModifierWord.cpp
+++ third_party/WebKit/Source/core/editing/SelectionModifierWord.cpp
@@ -388,8 +388,8 @@ VisiblePosition VisualWordPosition(const VisiblePosition& visible_position,
     }
 
     if (is_word_break) {
-      return HonorEditingBoundaryAtOrBefore(adjacent_character_position,
-                                            visible_position.DeepEquivalent());
+      return AdjustBackwardPositionToAvoidCrossingEditingBoundaries(
+          adjacent_character_position, visible_position.DeepEquivalent());
     }
 
     current = adjacent_character_position;
