fa3e975022949b432e7816e8f4fd8bacc992ed17
yiyix <yiyix@chromium.org>
2018-03-27 19:18:34 +0000

    Fix Regression: Integer-overflow in viz::Display::RemoveOverdrawQuads
    I used a size_t type variable to record the total number of pixels on
    screen for some UMA metrics. However, with extreme large DrawQuads,
    size_t is not enough to store the total number of pixels.
    
    Bug: 824528
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel
    Change-Id: I1309ae31757da3b8e9ae689910575e8fa2ed2316
    Reviewed-on: https://chromium-review.googlesource.com/976626
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Yi Xu <yiyix@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546197}

--- components/viz/service/display/display.cc
+++ components/viz/service/display/display.cc
@@ -6,8 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/service/display/display.h"
 
 #include <stddef.h>
+#include <limits>
 
 #include "base/metrics/histogram_macros.h"
+#include "base/numerics/checked_math.h"
 #include "base/timer/elapsed_timer.h"
 #include "base/trace_event/trace_event.h"
 #include "cc/base/math_util.h"
@@ -583,10 +585,10 @@ void Display::RemoveOverdrawQuads(CompositorFrame* frame) {
       settings_.kMinimumDrawOcclusionSize.width() * device_scale_factor_;
 
   // Total quad area to be drawn on screen before applying draw occlusion.
-  size_t total_quad_area_shown_wo_occlusion_px = 0;
+  base::CheckedNumeric<uint64_t> total_quad_area_shown_wo_occlusion_px = 0;
 
   // Total area not draw skipped by draw occlusion.
-  size_t total_area_saved_in_px = 0;
+  base::CheckedNumeric<uint64_t> total_area_saved_in_px = 0;
 
   for (const auto& pass : frame->render_pass_list) {
     // TODO(yiyix): Add filter effects to draw occlusion calculation and perform
@@ -725,13 +727,16 @@ void Display::RemoveOverdrawQuads(CompositorFrame* frame) {
   }
   UMA_HISTOGRAM_PERCENTAGE(
       "Compositing.Display.Draw.Occlusion.Percentage.Saved",
-      total_quad_area_shown_wo_occlusion_px == 0
+      total_quad_area_shown_wo_occlusion_px.ValueOrDefault(0) == 0
           ? 0
-          : total_area_saved_in_px * 100 /
-                total_quad_area_shown_wo_occlusion_px);
+          : static_cast<uint64_t>(total_area_saved_in_px.ValueOrDie()) * 100 /
+                static_cast<uint64_t>(
+                    total_quad_area_shown_wo_occlusion_px.ValueOrDie()));
+
   UMA_HISTOGRAM_COUNTS_1M(
       "Compositing.Display.Draw.Occlusion.Drawing.Area.Saved2",
-      total_area_saved_in_px);
+      static_cast<uint64_t>(total_area_saved_in_px.ValueOrDefault(
+          std::numeric_limits<uint64_t>::max())));
 }
 
 }  // namespace viz
