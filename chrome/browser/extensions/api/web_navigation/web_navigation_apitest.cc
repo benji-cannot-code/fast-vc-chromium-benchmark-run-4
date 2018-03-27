2e9df253a9fc111a967b525fde84075997727167
Lukasz Anforowicz <lukasza@chromium.org>
2018-03-27 16:26:47 +0000

    Inline site-per-process expectations for //chrome-layer tests.
    We plan to make --site-per-process mode the default mode on ToT for
    //chrome layer.  To make this switch slightly easier, this CL inlines
    the test expectations from //testing/buildbot/filters into the source
    code of the tests - this should avoid having to keep passing the filter
    files to the default test steps after making the switch.
    
    Bug: 671734, 824962, 824966
    Change-Id: I4ce397f5a7be18cc184f081058bf57f2e7c6239f
    Reviewed-on: https://chromium-review.googlesource.com/976643
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546128}

--- chrome/browser/extensions/api/web_navigation/web_navigation_apitest.cc
+++ chrome/browser/extensions/api/web_navigation/web_navigation_apitest.cc
@@ -143,7 +143,7 @@ class DelayLoadStartAndExecuteJavascript
       : public content::NavigationThrottle,
         public base::SupportsWeakPtr<WillStartRequestObserverThrottle> {
    public:
-    WillStartRequestObserverThrottle(content::NavigationHandle* handle)
+    explicit WillStartRequestObserverThrottle(content::NavigationHandle* handle)
         : NavigationThrottle(handle) {}
     ~WillStartRequestObserverThrottle() override {}
 
@@ -245,6 +245,11 @@ IN_PROC_BROWSER_TEST_F(WebNavigationApiTest, Download) {
 }
 
 IN_PROC_BROWSER_TEST_F(WebNavigationApiTest, ServerRedirectSingleProcess) {
+  // TODO(lukasza): https://crbug.com/671734: Investigate why this test fails
+  // with --site-per-process.
+  if (content::AreAllSitesIsolatedForTesting())
+    return;
+
   ASSERT_TRUE(StartEmbeddedTestServer());
 
   // Set max renderers to 1 to force running out of processes.
--- testing/buildbot/filters/README.md
+++ testing/buildbot/filters/README.md
@@ -4,8 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 ## Summary
 
 This directory contains files that list tests that are not yet ready to run in a
-particular mode. For example - the `site-per-process.browser_tests.filter` file
-lists tests that should be excluded when running `browser_tests` in
+particular mode. For example - the `site-per-process.content_browsertests.filter` file
+lists tests that should be excluded when running `content_browsertests` in
 `--site-per-process` mode.
 
 ## File syntax
