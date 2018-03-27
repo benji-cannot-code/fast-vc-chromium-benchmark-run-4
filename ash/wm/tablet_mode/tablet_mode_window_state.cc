b634013c4aa73bef520454adc212d5c0cdca2388
Xiaoqian Dai <xdai@chromium.org>
2018-03-27 03:36:49 +0000

    Cros splitscreen: single window split view behaviors.
    * Keep overview open when closing the last window in overview (only
    during splitscreen, still exit overview when the last window in overview
    is closed outside of splitscreen)
    
    * Stay in overview after creating a split with the sole open window
    
    * Disable tapping on overview background to exit overview when there's a
    snapped window (but no windows in overview)
    
    * Disable toggling overview open/closed with overview button
    
    Bug: 825043
    Change-Id: I14027007fbe7f6a324fd61f8c08a370d093eca8c
    Reviewed-on: https://chromium-review.googlesource.com/977430
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545983}

--- ash/wm/tablet_mode/tablet_mode_window_state.cc
+++ ash/wm/tablet_mode/tablet_mode_window_state.cc
@@ -87,7 +87,6 @@ gfx::Rect GetBoundsInMaximizedMode(wm::WindowState* state_object) {
     return screen_util::GetDisplayBoundsInParent(state_object->window());
 
   if (state_object->GetStateType() == mojom::WindowStateType::LEFT_SNAPPED) {
-    DCHECK(CanSnap(state_object));
     return Shell::Get()
         ->split_view_controller()
         ->GetSnappedWindowBoundsInParent(state_object->window(),
@@ -95,7 +94,6 @@ gfx::Rect GetBoundsInMaximizedMode(wm::WindowState* state_object) {
   }
 
   if (state_object->GetStateType() == mojom::WindowStateType::RIGHT_SNAPPED) {
-    DCHECK(CanSnap(state_object));
     return Shell::Get()
         ->split_view_controller()
         ->GetSnappedWindowBoundsInParent(state_object->window(),
