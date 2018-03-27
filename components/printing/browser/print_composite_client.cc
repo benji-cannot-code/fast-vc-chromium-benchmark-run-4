8df7de40e30607a44f8fe01b9ac399d8e9469110
Wei Li <weili@chromium.org>
2018-03-27 18:28:47 +0000

    Add UMA to record PDF composition status
    Add one histogram enum for recording the PDF page or document
    composition status. And collect such status information in browser.
    This is used for monitoring the execution of PDF compositor service.
    
    BUG=824867
    
    Change-Id: I2f6496195cd4ada40f0f03366997f8aa2c9f942a
    Reviewed-on: https://chromium-review.googlesource.com/980900
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Wei Li <weili@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546178}

--- components/printing/browser/print_composite_client.cc
+++ components/printing/browser/print_composite_client.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/memory/shared_memory_handle.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/stl_util.h"
 #include "components/printing/common/print_messages.h"
 #include "content/public/browser/browser_thread.h"
@@ -202,6 +203,13 @@ void PrintCompositeClient::OnDidCompositePageToPdf(
     printing::mojom::PdfCompositor::CompositePageToPdfCallback callback,
     printing::mojom::PdfCompositor::Status status,
     mojo::ScopedSharedBufferHandle handle) {
+  // Due to https://crbug.com/742517, we can not add and use COUNT for enums in
+  // mojo.
+  UMA_HISTOGRAM_ENUMERATION(
+      "CompositePageToPdf.Status", status,
+      static_cast<int32_t>(
+          printing::mojom::PdfCompositor::Status::COMPOSTING_FAILURE) +
+          1);
   std::move(callback).Run(status, std::move(handle));
 }
 
@@ -213,6 +221,14 @@ void PrintCompositeClient::OnDidCompositeDocumentToPdf(
   RemoveCompositeRequest(document_cookie);
   // Clear all stored printed subframes.
   printed_subframes_.erase(document_cookie);
+
+  // Due to https://crbug.com/742517, we can not add and use COUNT for enums in
+  // mojo.
+  UMA_HISTOGRAM_ENUMERATION(
+      "CompositeDocToPdf.Status", status,
+      static_cast<int32_t>(
+          printing::mojom::PdfCompositor::Status::COMPOSTING_FAILURE) +
+          1);
   std::move(callback).Run(status, std::move(handle));
 }
 
