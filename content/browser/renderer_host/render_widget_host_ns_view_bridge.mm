77e5bed75e22733beb3759f7b7761e51dadd9745
Christopher Cameron <ccameron@chromium.org>
2018-03-27 02:25:54 +0000

    RemoteRWHVMac: Push display::Display from NSView to RHWVMac
    Add a cached variable for RWHVMac's display::Display.
    * Initialize it with the the current key window's display, and
      pass that to BrowserCompositorMac's constructor (and remove
      the equivalent initialization from BrowserCompositorMac).
    * Update ui::DisplayLinkMac to use the id from the cached
      display::Display, and move its update code into
      UpdateNSViewAndDisplayProperties (this will usually just be a
      std::map lookup).
    
    Move the display::DisplayObserver implementation from RWHVMac (in
    the browser process) to RHWVNSViewBridgeLocal (in the AppShim
    process).
    
    Change -[RWHVCocoa updateScreenProperties] to only push a new
    display::Display to the RWHVMac when the RWHVCocoa is attached to
    a window.
    
    Bug: 821651
    Change-Id: Ifeddf82ca9858e7e9185b068094af45425790ad0
    Reviewed-on: https://chromium-review.googlesource.com/979119
    Commit-Queue: ccameron <ccameron@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545919}

--- content/browser/renderer_host/render_widget_host_ns_view_bridge.mm
+++ content/browser/renderer_host/render_widget_host_ns_view_bridge.mm
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/renderer_host/render_widget_host_view_mac.h"
 #import "skia/ext/skia_utils_mac.h"
 #import "ui/base/cocoa/animation_utils.h"
+#include "ui/display/display_observer.h"
+#include "ui/display/screen.h"
 
 namespace content {
 
@@ -21,7 +23,8 @@ namespace {
 // process as the NSView. The caller of this interface may exist in another
 // process.
 class RenderWidgetHostViewNSViewBridgeLocal
-    : public RenderWidgetHostNSViewBridge {
+    : public RenderWidgetHostNSViewBridge,
+      public display::DisplayObserver {
  public:
   explicit RenderWidgetHostViewNSViewBridgeLocal(
       std::unique_ptr<RenderWidgetHostNSViewClient> client);
@@ -32,6 +35,10 @@ class RenderWidgetHostViewNSViewBridgeLocal
   void SetVisible(bool visible) override;
 
  private:
+  // display::DisplayObserver implementation.
+  void OnDisplayMetricsChanged(const display::Display& display,
+                               uint32_t metrics) override;
+
   // Weak, this is owned by |cocoa_view_|'s |client_|, and |cocoa_view_| owns
   // its |client_|.
   RenderWidgetHostViewCocoa* cocoa_view_ = nil;
@@ -44,6 +51,8 @@ class RenderWidgetHostViewNSViewBridgeLocal
 
 RenderWidgetHostViewNSViewBridgeLocal::RenderWidgetHostViewNSViewBridgeLocal(
     std::unique_ptr<RenderWidgetHostNSViewClient> client) {
+  display::Screen::GetScreen()->AddObserver(this);
+
   // Since we autorelease |cocoa_view|, our caller must put |GetNativeView()|
   // into the view hierarchy right after calling us.
   cocoa_view_ = [[[RenderWidgetHostViewCocoa alloc]
@@ -55,7 +64,9 @@ RenderWidgetHostViewNSViewBridgeLocal::RenderWidgetHostViewNSViewBridgeLocal(
 }
 
 RenderWidgetHostViewNSViewBridgeLocal::
-    ~RenderWidgetHostViewNSViewBridgeLocal() {}
+    ~RenderWidgetHostViewNSViewBridgeLocal() {
+  display::Screen::GetScreen()->RemoveObserver(this);
+}
 
 RenderWidgetHostViewCocoa*
 RenderWidgetHostViewNSViewBridgeLocal::GetRenderWidgetHostViewCocoa() {
@@ -72,6 +83,15 @@ void RenderWidgetHostViewNSViewBridgeLocal::SetBackgroundColor(SkColor color) {
 void RenderWidgetHostViewNSViewBridgeLocal::SetVisible(bool visible) {
   ScopedCAActionDisabler disabler;
   [cocoa_view_ setHidden:!visible];
+}
+
+void RenderWidgetHostViewNSViewBridgeLocal::OnDisplayMetricsChanged(
+    const display::Display& display,
+    uint32_t changed_metrics) {
+  // Note that -updateScreenProperties is also be called by the notification
+  // NSWindowDidChangeBackingPropertiesNotification (some of these calls
+  // will be redundant).
+  [cocoa_view_ updateScreenProperties];
 }
 
 }  // namespace
