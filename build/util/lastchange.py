24dbc07bc511f0a9ed978118e75f64c09903aefd
Xiyuan Xia <xiyuan@chromium.org>
2018-03-27 14:42:34 +0000

    Dump git command error instead of silent fail
    Bug: 747349
    Change-Id: Ied3abb6f93a391bf6581275efbd96b3763413de9
    Reviewed-on: https://chromium-review.googlesource.com/976540
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546109}

--- build/util/lastchange.py
+++ build/util/lastchange.py
@@ -9,6 +9,7 @@ lastchange.py -- Chromium revision fetching utility.
 """
 
 import re
+import logging
 import optparse
 import os
 import subprocess
@@ -43,7 +44,8 @@ def RunGitCommand(directory, command):
                             cwd=directory,
                             shell=(sys.platform=='win32'))
     return proc
-  except OSError:
+  except OSError as e:
+    logging.error('Command %r failed: %s' % (' '.join(command), e))
     return None
 
 
@@ -174,6 +176,8 @@ def main(argv=None):
                     "'^Change-Id:' to suppress local commits.",
                     default='^Change-Id:')
   opts, args = parser.parse_args(argv[1:])
+
+  logging.basicConfig(level=logging.WARNING)
 
   out_file = opts.output
   header = opts.header
