62b7b8b007a098390c6ae3a7c9f94ab137dacf38
Eugene But <eugenebut@google.com>
2018-03-27 23:46:48 +0000

    Updated MobileDownloadFileUIInstallGoogleDrive metric usage.
    1.) Removed constant to allow presubmit warnings for missing metrics
    2.) Added metric to actions.xml (this is existing metric but it was
        defined in close source internal repository).
    
    Bug: 791806
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I3a35962cbde10d12b260c7575b1a3bf97af9c76c
    Reviewed-on: https://chromium-review.googlesource.com/981329
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546320}

--- ios/chrome/browser/download/download_manager_metric_names.h
+++ ios/chrome/browser/download/download_manager_metric_names.h
@@ -6,10 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_DOWNLOAD_DOWNLOAD_MANAGER_METRIC_NAMES_H_
 #define IOS_CHROME_BROWSER_DOWNLOAD_DOWNLOAD_MANAGER_METRIC_NAMES_H_
 
-// MobileDownloadFileUIInstallGoogleDrive UMA action. Logged when Google Drive
-// app is installed after presenting Store Kit dialog from the Download Manager.
-extern const char kDownloadManagerGoogleDriveInstalled[];
-
 // Values of the UMA Download.IOSDownloadedFileAction histogram. This enum is
 // append only.
 enum class DownloadedFileAction {
