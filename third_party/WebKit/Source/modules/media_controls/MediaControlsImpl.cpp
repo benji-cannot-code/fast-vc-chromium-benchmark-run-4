5de6bc404257dff212758a22cb264d417661db95
Tommy Steimel <steimel@chromium.org>
2018-03-27 21:15:31 +0000

    Only show elements in the overflow menu if they are not disabled
    This CL prevents disabled elements from showing up in the overflow menu
    for modern media controls. This fixes a bug where the disabled
    fullscreen and mute buttons were showing up (enabled) in the overflow
    menu when the video was small enough.
    
    Bug: 823401
    Change-Id: I56731e929e1d4f700eb007348183829a3a0aa2ee
    Reviewed-on: https://chromium-review.googlesource.com/978954
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546262}

--- third_party/WebKit/Source/modules/media_controls/MediaControlsImpl.cpp
+++ third_party/WebKit/Source/modules/media_controls/MediaControlsImpl.cpp
@@ -1160,7 +1160,7 @@ void MediaControlsImpl::UpdateOverflowMenuWanted() const {
       last_element = element;
     } else {
       add_elements = false;
-      if (element->HasOverflowButton()) {
+      if (element->HasOverflowButton() && !element->IsDisabled()) {
         overflow_wanted = true;
         element->SetOverflowElementIsWanted(true);
       }
