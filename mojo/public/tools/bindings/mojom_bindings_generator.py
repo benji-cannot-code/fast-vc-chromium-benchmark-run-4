11bf0e49b3b563c5cc04dfa05f63b7e1fb44043e
Takuto Ikuta <tikuta@chromium.org>
2018-03-27 07:17:00 +0000

    Reduce the number of actions related to mojom generator using response file
    Spawning many python processes can be bottleneck of build time.
    This patch is to improve the time by replacing action_foreach to action for mojom related actions.
    
    Especially, windows buildbot sometimes uses a few minutes for mojom generator.
    https://chromium-build-stats.appspot.com/ninja_log/2018/03/22/win1357-c4/ninja_log.win1357-c4.chrome-bot.20180322-005012.7392.gz/trace.html from https://ci.chromium.org/buildbot/tryserver.chromium.win/win7_chromium_rel_ng/127904
    The trace shows there are many mojom related actions around 70s~110s and 190s~260s (Those are emphasized by typing mojom to search box.)
    
    I took build stats after touching mojom_bindings_generator.py
    On Z840 Windows 10, time to build content/common:mojo_bindings improved from 16.755s to 7.646s and the number of build target is reduced from 2342 to 600.
    On Z840 Linux, time to build content/common:mojo_bindings improved from 7.501s to 4.279s and the number of build target is reduced from 2340 to 611.
    
    
    Change-Id: I56168a233839e1386f31c0b769b263b43dc865d8
    Reviewed-on: https://chromium-review.googlesource.com/974982
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546048}

--- mojo/public/tools/bindings/mojom_bindings_generator.py
+++ mojo/public/tools/bindings/mojom_bindings_generator.py
@@ -244,6 +244,11 @@ def _Generate(args, remaining_args):
 
   processor = MojomProcessor(lambda filename: filename in args.filename)
   processor.LoadTypemaps(set(args.typemaps))
+
+  if args.filelist:
+    with open(args.filelist) as f:
+      args.filename.extend(f.read().split())
+
   for filename in args.filename:
     processor._GenerateModule(args, remaining_args, generator_modules,
                               RelativePath(filename, args.depth), [])
@@ -294,6 +299,11 @@ def _ParseFile(args, rel_filename):
 
 def _Parse(args, _):
   fileutil.EnsureDirectoryExists(args.output_dir)
+
+  if args.filelist:
+    with open(args.filelist) as f:
+      args.filename.extend(f.read().split())
+
   for filename in args.filename:
     _ParseFile(args, RelativePath(filename, args.depth))
   return 0
@@ -307,6 +317,10 @@ def _Precompile(args, _):
 
 def _VerifyImportDeps(args, __):
   fileutil.EnsureDirectoryExists(args.gen_dir)
+
+  if args.filelist:
+    with open(args.filelist) as f:
+      args.filename.extend(f.read().split())
 
   for filename in args.filename:
     rel_path = RelativePath(filename, args.depth)
@@ -353,7 +367,8 @@ def main():
   parse_parser = subparsers.add_parser(
       "parse", description="Parse mojom to AST and remove disabled definitions."
                            " Pickle pruned AST into output_dir.")
-  parse_parser.add_argument("filename", nargs="+", help="mojom input file")
+  parse_parser.add_argument("filename", nargs="*", help="mojom input file")
+  parse_parser.add_argument("--filelist", help="mojom input file list")
   parse_parser.add_argument(
       "-o",
       "--output_dir",
@@ -374,8 +389,9 @@ def main():
 
   generate_parser = subparsers.add_parser(
       "generate", description="Generate bindings from mojom files.")
-  generate_parser.add_argument("filename", nargs="+",
+  generate_parser.add_argument("filename", nargs="*",
                                help="mojom input file")
+  generate_parser.add_argument("--filelist", help="mojom input file list")
   generate_parser.add_argument("-d", "--depth", dest="depth", default=".",
                                help="depth from source root")
   generate_parser.add_argument("-o", "--output_dir", dest="output_dir",
@@ -462,8 +478,9 @@ def main():
 
   verify_parser = subparsers.add_parser("verify", description="Checks "
       "the set of imports against the set of dependencies.")
-  verify_parser.add_argument("filename", nargs="+",
+  verify_parser.add_argument("filename", nargs="*",
       help="mojom input file")
+  verify_parser.add_argument("--filelist", help="mojom input file list")
   verify_parser.add_argument("-f", "--file", dest="deps_file",
       help="file containing paths to the sources files for "
       "dependencies")
