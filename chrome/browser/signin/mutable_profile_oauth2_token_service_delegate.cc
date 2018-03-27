e250d112ef32ca531daf00966f7b7aabf2a41d5a
David Roger <droger@chromium.org>
2018-03-27 19:50:45 +0000

    [signin] Specify a reason to authentication errors
    Updates code generating INVALID_GAIA_CREDENTIALS errors to also
    set an error reason.
    Report these error reasons in the Signin.InvalidGaiaCredentialsReason
    histogram.
    
    Bug: 822728
    TBR: alemate
    Change-Id: Ie727964845f0677a31d0ab035c0dee788d2b7859
    Reviewed-on: https://chromium-review.googlesource.com/966422
    Commit-Queue: David Roger <droger@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546214}

--- chrome/browser/signin/mutable_profile_oauth2_token_service_delegate.cc
+++ chrome/browser/signin/mutable_profile_oauth2_token_service_delegate.cc
@@ -506,8 +506,9 @@ void MutableProfileOAuth2TokenServiceDelegate::OnWebDataServiceRequestDone(
   for (auto& token : refresh_tokens_) {
     if (!RefreshTokenIsAvailable(token.first)) {
       UpdateAuthError(token.first,
-                      GoogleServiceAuthError(
-                          GoogleServiceAuthError::INVALID_GAIA_CREDENTIALS));
+                      GoogleServiceAuthError::FromInvalidGaiaCredentialsReason(
+                          GoogleServiceAuthError::InvalidGaiaCredentialsReason::
+                              CREDENTIALS_MISSING));
       break;
     }
   }
@@ -694,11 +695,13 @@ void MutableProfileOAuth2TokenServiceDelegate::UpdateCredentialsInMemory(
     AddAccountStatus(account_id, refresh_token);
   }
 
-  UpdateAuthError(account_id,
-                  (refresh_token == kInvalidRefreshToken)
-                      ? GoogleServiceAuthError(
-                            GoogleServiceAuthError::INVALID_GAIA_CREDENTIALS)
-                      : GoogleServiceAuthError::AuthErrorNone());
+  GoogleServiceAuthError error =
+      (refresh_token == kInvalidRefreshToken)
+          ? GoogleServiceAuthError::FromInvalidGaiaCredentialsReason(
+                GoogleServiceAuthError::InvalidGaiaCredentialsReason::
+                    CREDENTIALS_REJECTED_BY_CLIENT)
+          : GoogleServiceAuthError::AuthErrorNone();
+  UpdateAuthError(account_id, error);
 }
 
 void MutableProfileOAuth2TokenServiceDelegate::PersistCredentials(
--- components/signin/core/browser/signin_error_controller.cc
+++ components/signin/core/browser/signin_error_controller.cc
@@ -97,7 +97,7 @@ void SigninErrorController::AuthStatusChanged() {
   }
 
   if (error_changed) {
-    signin_metrics::LogAuthError(auth_error_.state());
+    signin_metrics::LogAuthError(auth_error_);
     for (auto& observer : observer_list_)
       observer.OnErrorChanged();
   }
