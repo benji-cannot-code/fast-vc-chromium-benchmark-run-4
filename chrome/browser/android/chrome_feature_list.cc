71c7aa3bdc18fbd9027ef68ab857abe4882c69b2
Zhiqiang Zhang <zqzhang@google.com>
2018-03-27 18:19:42 +0000

    [Android MR] Initial directory layout for CAF migration
    This CL lies out the directory layout for CAF migration. The
    migrated code will reside in caf/, in parallel of cast/. The
    migrated code is guarded behind a feature flag.
    
    Bug: 711860
    Change-Id: Iddfe9e3ce3103fae876c9c151c96aba2ef99354f
    Reviewed-on: https://chromium-review.googlesource.com/977232
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Zhiqiang Zhang <zqzhang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546175}

--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/subresource_filter/core/browser/subresource_filter_features.h"
 #include "content/public/common/content_features.h"
 #include "jni/ChromeFeatureList_jni.h"
+#include "media/base/media_switches.h"
 
 using base::android::ConvertJavaStringToUTF8;
 using base::android::ConvertUTF8ToJavaString;
@@ -133,6 +134,7 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &payments::features::kWebPaymentsSingleAppUiSkip,
     &kWebVrAutopresentFromIntent,
     &kWebVrCardboardSupport,
+    &media::kCafMediaRouterImpl,
     &ntp_snippets::kArticleSuggestionsExpandableHeader,
     &ntp_snippets::kIncreasedVisibility,
     &ntp_snippets::kForeignSessionsSuggestionsFeature,
