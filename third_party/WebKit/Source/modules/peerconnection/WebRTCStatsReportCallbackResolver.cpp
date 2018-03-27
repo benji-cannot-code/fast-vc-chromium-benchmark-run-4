0deaddc0a11c6ef2c8e3dd8b6f03c5cd7134aec7
Henrik Boström <hbos@chromium.org>
2018-03-27 14:08:30 +0000

    Preparation CL for getStats() with selector argument.
    Mostly moving things around, refactoring...
    
    1. Move GetRTCStatsCallback from rtc_peer_connection_handler.cc to
       rtc_stats.[h/cc] where other blink/webrtc stats-related content-glue
       is placed, and rename it RTCStatsCollectorCallbackImpl.
    
       This allows it to be used by content's RTCRtpSender and
       RTCRtpReceiver in follow-up CLs.
    
    2. Remove unused content::RTCRtpSender-constructor.
    
    3. Move WebRTCStatsReportCallbackResolver from RTCPeerConnection.cpp
       to its own file.
    
       This allows it to be used by blink's RTCRtpSender and RTCRtpReceiver
       in follow-up CLs.
    
    4. Implement new PeerConnectionInterface::GetStats() functions in
       MockPeerConnectionImpl (need to wait for
       https://webrtc-review.googlesource.com/c/src/+/62900 to land and roll
       into chromium).
    
    Bug: 680172
    Change-Id: Ief18b2fb96ebc292149af606bd0edd87bac9e961
    Reviewed-on: https://chromium-review.googlesource.com/973368
    Commit-Queue: Henrik Boström <hbos@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546101}

---
+++ third_party/WebKit/Source/modules/peerconnection/WebRTCStatsReportCallbackResolver.cpp
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#include "modules/peerconnection/WebRTCStatsReportCallbackResolver.h"
+
+#include "core/dom/ExecutionContext.h"
+
+namespace blink {
+
+// static
+std::unique_ptr<WebRTCStatsReportCallback>
+WebRTCStatsReportCallbackResolver::Create(ScriptPromiseResolver* resolver) {
+  return std::unique_ptr<WebRTCStatsReportCallback>(
+      new WebRTCStatsReportCallbackResolver(resolver));
+}
+
+WebRTCStatsReportCallbackResolver::WebRTCStatsReportCallbackResolver(
+    ScriptPromiseResolver* resolver)
+    : resolver_(resolver) {}
+
+WebRTCStatsReportCallbackResolver::~WebRTCStatsReportCallbackResolver() {
+  DCHECK(
+      ExecutionContext::From(resolver_->GetScriptState())->IsContextThread());
+}
+
+void WebRTCStatsReportCallbackResolver::OnStatsDelivered(
+    std::unique_ptr<WebRTCStatsReport> report) {
+  DCHECK(
+      ExecutionContext::From(resolver_->GetScriptState())->IsContextThread());
+  resolver_->Resolve(new RTCStatsReport(std::move(report)));
+}
+
+}  // namespace blink
