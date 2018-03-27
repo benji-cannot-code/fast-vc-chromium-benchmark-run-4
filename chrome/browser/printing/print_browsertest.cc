236439cebab6cbc2fbb2c0a182f12ae7a920fac8
Wei Li <weili@chromium.org>
2018-03-27 15:00:07 +0000

    Add UsePdfCompositorServiceForTest for fieldtrial testing
    Add "UsePdfCompositorServiceForTest" experiment to the field trial under
    tests for all desktop platforms. It tests the new PDF generation service
    which supports OOPIF printing.
    
    BUG=824867
    
    Change-Id: I808c8ed04509e5d6a6074d35b4d569f22c6c3e77
    Reviewed-on: https://chromium-review.googlesource.com/981176
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Wei Li <weili@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546111}

--- chrome/browser/printing/print_browsertest.cc
+++ chrome/browser/printing/print_browsertest.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/prefs/pref_service.h"
+#include "components/printing/browser/features.h"
 #include "components/printing/browser/print_composite_client.h"
 #include "components/printing/browser/print_manager_utils.h"
 #include "components/printing/common/print_messages.h"
@@ -533,7 +534,9 @@ IN_PROC_BROWSER_TEST_F(PrintBrowserTest, RegularPrinting) {
   ui_test_utils::NavigateToURL(browser(), url);
 
   if (base::CommandLine::ForCurrentProcess()->HasSwitch(
-          switches::kSitePerProcess)) {
+          switches::kSitePerProcess) ||
+      base::FeatureList::IsEnabled(
+          printing::features::kUsePdfCompositorServiceForPrint)) {
     EXPECT_TRUE(IsOopifEnabled());
   } else {
     EXPECT_FALSE(IsOopifEnabled());
