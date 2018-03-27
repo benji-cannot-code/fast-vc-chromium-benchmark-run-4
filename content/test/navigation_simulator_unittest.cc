2fcf5677dbcb61dfa9167b3f532c4c4b1b1d6a20
Charlie Harrison <csharrison@chromium.org>
2018-03-27 20:52:46 +0000

    NavigationSimulator: Implement Wait() and SetAutoAdvance(false)
    This CL augments the NavigationSimulator to allow tests to drive it
    manually through its stages. This is implemented via a method
    SetAutoAdvance(bool).
    
    The CL also implements a Wait() method which spins the run loop waiting
    for throttle checks to complete, and an IsDeferred() method which returns
    whether some throttle is currently deferring the navigation.
    
    This is used to implement one test outside of //content.
    
    Bug: 822275
    Change-Id: I394e33cf89ee298c92d2c6554ca0c591e080a695
    Reviewed-on: https://chromium-review.googlesource.com/964389
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546248}

--- content/test/navigation_simulator_unittest.cc
+++ content/test/navigation_simulator_unittest.cc
@@ -5,19 +5,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/public/test/navigation_simulator.h"
 
+#include <memory>
 #include <string>
 #include <tuple>
+#include <utility>
 
 #include "base/bind.h"
 #include "base/macros.h"
+#include "base/memory/ptr_util.h"
+#include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/run_loop.h"
+#include "base/test/test_simple_task_runner.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/navigation_throttle.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/common/browser_side_navigation_policy.h"
 #include "content/public/test/test_navigation_throttle.h"
+#include "content/public/test/test_navigation_throttle_inserter.h"
 #include "content/test/test_render_frame_host.h"
 #include "content/test/test_render_view_host.h"
 #include "content/test/test_web_contents.h"
@@ -27,15 +33,61 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-class NavigationSimulatorTest
+// This class defers a navigation via a no-op async task on the provided task
+// runner.
+class TaskRunnerDeferringThrottle : public NavigationThrottle {
+ public:
+  TaskRunnerDeferringThrottle(scoped_refptr<base::TaskRunner> task_runner,
+                              NavigationHandle* handle)
+      : NavigationThrottle(handle),
+        task_runner_(std::move(task_runner)),
+        weak_factory_(this) {}
+  ~TaskRunnerDeferringThrottle() override {}
+
+  static std::unique_ptr<NavigationThrottle> Create(
+      scoped_refptr<base::TaskRunner> task_runner,
+      NavigationHandle* handle) {
+    return base::WrapUnique(
+        new TaskRunnerDeferringThrottle(std::move(task_runner), handle));
+  }
+
+  // NavigationThrottle:
+  ThrottleCheckResult WillStartRequest() override { return DeferToPostTask(); }
+  ThrottleCheckResult WillRedirectRequest() override {
+    return DeferToPostTask();
+  }
+  ThrottleCheckResult WillProcessResponse() override {
+    return DeferToPostTask();
+  }
+  const char* GetNameForLogging() override {
+    return "TaskRunnerDeferringThrottle";
+  }
+
+ private:
+  ThrottleCheckResult DeferToPostTask() {
+    task_runner_->PostTaskAndReply(
+        FROM_HERE, base::DoNothing(),
+        base::BindOnce(&TaskRunnerDeferringThrottle::Resume,
+                       weak_factory_.GetWeakPtr()));
+
+    return NavigationThrottle::DEFER;
+  }
+  scoped_refptr<base::TaskRunner> task_runner_;
+  base::WeakPtrFactory<TaskRunnerDeferringThrottle> weak_factory_;
+  DISALLOW_COPY_AND_ASSIGN(TaskRunnerDeferringThrottle);
+};
+
+class NavigationSimulatorTest : public RenderViewHostImplTestHarness {};
+
+class CancellingNavigationSimulatorTest
     : public RenderViewHostImplTestHarness,
       public WebContentsObserver,
       public testing::WithParamInterface<
           std::tuple<base::Optional<TestNavigationThrottle::ThrottleMethod>,
                      TestNavigationThrottle::ResultSynchrony>> {
  public:
-  NavigationSimulatorTest() : weak_ptr_factory_(this) {}
-  ~NavigationSimulatorTest() override {}
+  CancellingNavigationSimulatorTest() : weak_ptr_factory_(this) {}
+  ~CancellingNavigationSimulatorTest() override {}
 
   void SetUp() override {
     RenderViewHostImplTestHarness::SetUp();
@@ -55,8 +107,9 @@ class NavigationSimulatorTest
     auto throttle = std::make_unique<TestNavigationThrottle>(handle);
     throttle->SetCallback(
         TestNavigationThrottle::WILL_FAIL_REQUEST,
-        base::BindRepeating(&NavigationSimulatorTest::OnWillFailRequestCalled,
-                            base::Unretained(this)));
+        base::BindRepeating(
+            &CancellingNavigationSimulatorTest::OnWillFailRequestCalled,
+            base::Unretained(this)));
     if (cancel_time_.has_value()) {
       throttle->SetResponse(cancel_time_.value(), sync_,
                             NavigationThrottle::CANCEL);
@@ -76,16 +129,48 @@ class NavigationSimulatorTest
   std::unique_ptr<NavigationSimulator> simulator_;
   bool did_finish_navigation_ = false;
   bool will_fail_request_called_ = false;
-  base::WeakPtrFactory<NavigationSimulatorTest> weak_ptr_factory_;
+  base::WeakPtrFactory<CancellingNavigationSimulatorTest> weak_ptr_factory_;
 
  private:
-  DISALLOW_COPY_AND_ASSIGN(NavigationSimulatorTest);
+  DISALLOW_COPY_AND_ASSIGN(CancellingNavigationSimulatorTest);
 };
+
+TEST_F(NavigationSimulatorTest, AutoAdvanceOff) {
+  std::unique_ptr<NavigationSimulator> simulator =
+      NavigationSimulator::CreateRendererInitiated(
+          GURL("https://example.test/"), main_rfh());
+  simulator->SetAutoAdvance(false);
+
+  auto task_runner = base::MakeRefCounted<base::TestSimpleTaskRunner>();
+  auto* raw_runner = task_runner.get();
+  TestNavigationThrottleInserter throttle_inserter(
+      web_contents(), base::BindRepeating(&TaskRunnerDeferringThrottle::Create,
+                                          std::move(task_runner)));
+
+  simulator->Start();
+  EXPECT_EQ(1u, raw_runner->NumPendingTasks());
+  EXPECT_TRUE(simulator->IsDeferred());
+  raw_runner->RunPendingTasks();
+  simulator->Wait();
+
+  simulator->Redirect(GURL("https://example.test/redirect"));
+  EXPECT_EQ(1u, raw_runner->NumPendingTasks());
+  EXPECT_TRUE(simulator->IsDeferred());
+  raw_runner->RunPendingTasks();
+  simulator->Wait();
+
+  simulator->ReadyToCommit();
+  EXPECT_EQ(1u, raw_runner->NumPendingTasks());
+  EXPECT_TRUE(simulator->IsDeferred());
+  raw_runner->RunPendingTasks();
+  simulator->Wait();
+  simulator->Commit();
+}
 
 // Stress test the navigation simulator by having a navigation throttle cancel
 // the navigation at various points in the flow, both synchronously and
 // asynchronously.
-TEST_P(NavigationSimulatorTest, Cancel) {
+TEST_P(CancellingNavigationSimulatorTest, Cancel) {
   SCOPED_TRACE(::testing::Message()
                << "CancelTime: "
                << (cancel_time_.has_value() ? cancel_time_.value() : -1)
@@ -125,7 +210,7 @@ TEST_P(NavigationSimulatorTest, Cancel) {
 
 INSTANTIATE_TEST_CASE_P(
     CancelMethod,
-    NavigationSimulatorTest,
+    CancellingNavigationSimulatorTest,
     ::testing::Combine(
         ::testing::Values(TestNavigationThrottle::WILL_START_REQUEST,
                           TestNavigationThrottle::WILL_REDIRECT_REQUEST,
@@ -135,7 +220,7 @@ INSTANTIATE_TEST_CASE_P(
                           TestNavigationThrottle::ASYNCHRONOUS)));
 
 // Create a version of the test class for parameterized testing.
-using NavigationSimulatorTestCancelFail = NavigationSimulatorTest;
+using NavigationSimulatorTestCancelFail = CancellingNavigationSimulatorTest;
 
 // Test canceling the simulated navigation.
 TEST_P(NavigationSimulatorTestCancelFail, Fail) {
@@ -155,7 +240,8 @@ INSTANTIATE_TEST_CASE_P(
                           TestNavigationThrottle::ASYNCHRONOUS)));
 
 // Create a version of the test class for parameterized testing.
-using NavigationSimulatorTestCancelFailErrAborted = NavigationSimulatorTest;
+using NavigationSimulatorTestCancelFailErrAborted =
+    CancellingNavigationSimulatorTest;
 
 // Test canceling the simulated navigation with net::ERR_ABORTED, which should
 // not call WillFailRequest on the throttle.
