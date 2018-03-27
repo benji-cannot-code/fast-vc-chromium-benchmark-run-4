59f58d60479713d8e2a4ae23ed0551e23c3608d3
Takashi Toyoshima <toyoshim@chromium.org>
2018-03-27 08:32:58 +0000

    OOR-CORS: Simplify DocumentThreadableLoader's preflight request creation code
    Now DocumentThreadableLoader uses Blink API types to construct a
    CORS-preflight request, but this isn't necessary.
    
    Also, this change prefer using std::unique_ptr for ResourceRequest
    so to respect the direction of crbug.com/787704.
    
    Bug: 803766, 787704
    Change-Id: I897b17b7ea20e47316c05436c7d36438e03f2d7d
    Reviewed-on: https://chromium-review.googlesource.com/964074
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546062}

--- third_party/WebKit/Source/core/loader/DocumentThreadableLoader.h
+++ third_party/WebKit/Source/core/loader/DocumentThreadableLoader.h
@@ -69,8 +69,8 @@ class CORE_EXPORT DocumentThreadableLoader final : public ThreadableLoader,
                                         const ResourceLoaderOptions&);
 
   // Exposed for testing. Code outside this class should not call this function.
-  static WebURLRequest CreateAccessControlPreflightRequestForTesting(
-      const WebURLRequest&);
+  static std::unique_ptr<ResourceRequest>
+  CreateAccessControlPreflightRequestForTesting(const ResourceRequest&);
 
   static DocumentThreadableLoader* Create(ThreadableLoadingContext&,
                                           ThreadableLoaderClient*,
@@ -91,8 +91,9 @@ class CORE_EXPORT DocumentThreadableLoader final : public ThreadableLoader,
  private:
   enum BlockingBehavior { kLoadSynchronously, kLoadAsynchronously };
 
-  static WebURLRequest CreateAccessControlPreflightRequest(
-      const WebURLRequest&);
+  static std::unique_ptr<ResourceRequest> CreateAccessControlPreflightRequest(
+      const ResourceRequest&,
+      const SecurityOrigin*);
 
   DocumentThreadableLoader(ThreadableLoadingContext&,
                            ThreadableLoaderClient*,
