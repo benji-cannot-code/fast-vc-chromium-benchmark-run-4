2bc8f1ba873ee363ea0cc8a017f368472837456c
Stephen McGruer <smcgruer@chromium.org>
2018-03-27 21:10:57 +0000

    Web Animations: Use WTF::Optional for hold_time_
    Previously nullable values in the animations code were tracked by
    storing nulls as quiet_NaN() and using std::isnan as a null-detector.
    It is much more explicit to store such values using WTF::Optional, which
    forces code to consider whether or not the offset exists.
    
    This CL also combines held_ and hold_time_ into a single variable, since
    held_ was used to indicate whether hold_time_ should (or could) be read
    already - which is built into WTF::Optional.
    
    Bug: 791086
    Change-Id: I223cd07b5f6b9c15be172491512a4ab319138503
    Reviewed-on: https://chromium-review.googlesource.com/979774
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546261}

--- third_party/WebKit/Source/core/animation/Animation.h
+++ third_party/WebKit/Source/core/animation/Animation.h
@@ -280,7 +280,7 @@ class CORE_EXPORT Animation final : public EventTargetWithInlineData,
   AnimationPlayState play_state_;
   double playback_rate_;
   WTF::Optional<double> start_time_;
-  double hold_time_;
+  WTF::Optional<double> hold_time_;
 
   unsigned sequence_number_;
 
@@ -292,7 +292,6 @@ class CORE_EXPORT Animation final : public EventTargetWithInlineData,
 
   // Reflects all pausing, including via pauseForTesting().
   bool paused_;
-  bool held_;
   bool is_paused_for_testing_;
   bool is_composited_animation_disabled_for_testing_;
 
@@ -321,7 +320,7 @@ class CORE_EXPORT Animation final : public EventTargetWithInlineData,
           effect_changed(false),
           pending_action(kStart) {}
     WTF::Optional<double> start_time;
-    double hold_time;
+    WTF::Optional<double> hold_time;
     double playback_rate;
     bool effect_changed;
     CompositorAction pending_action;
