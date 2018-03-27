a83c2b1e4c4ac2c3c58dd0b49123764f06debb3e
Peter Boström <pbos@chromium.org>
2018-03-27 00:54:03 +0000

    Remove unnecessary include from omnibox results.
    Bug: None
    Change-Id: Ieac0809e2253b8676bf374e255468443699bcab0
    Reviewed-on: https://chromium-review.googlesource.com/981177
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545914}

--- chrome/browser/ui/views/omnibox/rounded_omnibox_results_frame.cc
+++ chrome/browser/ui/views/omnibox/rounded_omnibox_results_frame.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/location_bar/background_with_1_px_border.h"
 #include "chrome/browser/ui/views/location_bar/location_bar_view.h"
-#include "ui/base/material_design/material_design_controller.h"
 #include "ui/compositor/layer.h"
 #include "ui/views/painter.h"
 
