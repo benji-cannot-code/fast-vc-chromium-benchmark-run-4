a49505084a3cd5134d583f5dcde4da280558143f
Avi Drissman <avi@chromium.org>
2018-03-27 03:39:02 +0000

    Move WebContents::DownloadImage/GetManifest to use the new callbacks.
    BUG=714018
    
    Change-Id: I330346f3e752870ac1752467ee2c9b934934cb2f
    Reviewed-on: https://chromium-review.googlesource.com/974746
    Reviewed-by: Sidney San Martín <sdy@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545987}

--- components/favicon/content/content_favicon_driver.cc
+++ components/favicon/content/content_favicon_driver.cc
@@ -27,7 +27,7 @@ namespace favicon {
 namespace {
 
 void ExtractManifestIcons(
-    const ContentFaviconDriver::ManifestDownloadCallback& callback,
+    ContentFaviconDriver::ManifestDownloadCallback callback,
     const GURL& manifest_url,
     const content::Manifest& manifest) {
   std::vector<FaviconURL> candidates;
@@ -35,7 +35,7 @@ void ExtractManifestIcons(
     candidates.emplace_back(icon.src, favicon_base::IconType::kWebManifestIcon,
                             icon.sizes);
   }
-  callback.Run(candidates);
+  std::move(callback).Run(candidates);
 }
 
 }  // namespace
@@ -130,12 +130,13 @@ int ContentFaviconDriver::DownloadImage(const GURL& url,
   bypass_cache_page_url_ = GURL();
 
   return web_contents()->DownloadImage(url, true, max_image_size, bypass_cache,
-                                       callback);
+                                       std::move(callback));
 }
 
 void ContentFaviconDriver::DownloadManifest(const GURL& url,
                                             ManifestDownloadCallback callback) {
-  web_contents()->GetManifest(base::Bind(&ExtractManifestIcons, callback));
+  web_contents()->GetManifest(
+      base::BindOnce(&ExtractManifestIcons, std::move(callback)));
 }
 
 bool ContentFaviconDriver::IsOffTheRecord() {
