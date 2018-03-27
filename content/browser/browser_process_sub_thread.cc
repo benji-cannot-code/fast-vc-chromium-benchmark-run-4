2bd21f7962ddd81b0e94f71456bea4f57d222be4
Gabriel Charette <gab@chromium.org>
2018-03-27 19:48:00 +0000

    Make BrowserThreadImpl lock-less
    Sampling profiler highlights high contention on this lock:
    crbug.com/821034.
    
    Initializing/tear down was already single-threaded (tear down had to be moved
    from BrowserProcessSubThread::Cleanup() to ~BrowserProcessSubThread() to
    enforce this with a ThreadChecker however). Add dchecks to confirm/enforce
    this and remove the lock which is usuless on data structures that are now (as
    of series of changes over the last year) read-only after startup.
    
    Use atomics to protect reads across the RUNNING=>SHUTDOWN boundary.
    
    Bug: 821034
    Change-Id: I7800048bff51ad79cb10ee89fd3a0a31534c393e
    Reviewed-on: https://chromium-review.googlesource.com/973556
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546212}

--- content/browser/browser_process_sub_thread.cc
+++ content/browser/browser_process_sub_thread.cc
@@ -126,8 +126,6 @@ void BrowserProcessSubThread::CleanUp() {
 #if defined(OS_WIN)
   com_initializer_.reset();
 #endif
-
-  browser_thread_.reset();
 }
 
 void BrowserProcessSubThread::CompleteInitializationOnBrowserThread() {
