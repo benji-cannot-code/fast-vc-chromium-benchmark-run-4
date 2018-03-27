b3cbe3bcc981c3c83254cd7f504f515a3920a770
Daniel Bratell <bratell@opera.com>
2018-03-27 20:32:01 +0000

    Gave two kBubbleReshowsHistogramName unique names
    In jumbo builds many cc files are compiled together in the same
    translation unit. When that happens, those files will share the
    same anonymous namespace so if they use symbols of the same name
    they will clash.
    
    That happened with two kBubbleReshowsHistogramName constants in
    chrome/browser/ui/exclusive_access and this patch gives
    those two more unique names.
    
    Bug: 803406
    Change-Id: I2b8e3a060c7b7853a3a001317e80d32b7d6e56d4
    Reviewed-on: https://chromium-review.googlesource.com/980879
    Commit-Queue: Yuri Wiitala <miu@chromium.org>
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546239}

--- chrome/browser/ui/exclusive_access/fullscreen_controller.cc
+++ chrome/browser/ui/exclusive_access/fullscreen_controller.cc
@@ -47,7 +47,7 @@ using content::WebContents;
 
 namespace {
 
-const char kBubbleReshowsHistogramName[] =
+const char kFullscreenBubbleReshowsHistogramName[] =
     "ExclusiveAccess.BubbleReshowsPerSession.Fullscreen";
 
 }  // namespace
@@ -331,7 +331,8 @@ void FullscreenController::NotifyTabExclusiveAccessLost() {
 
 void FullscreenController::RecordBubbleReshowsHistogram(
     int bubble_reshow_count) {
-  UMA_HISTOGRAM_COUNTS_100(kBubbleReshowsHistogramName, bubble_reshow_count);
+  UMA_HISTOGRAM_COUNTS_100(kFullscreenBubbleReshowsHistogramName,
+                           bubble_reshow_count);
 }
 
 void FullscreenController::ToggleFullscreenModeInternal(
--- chrome/browser/ui/exclusive_access/mouse_lock_controller.cc
+++ chrome/browser/ui/exclusive_access/mouse_lock_controller.cc
@@ -25,7 +25,7 @@ using content::WebContents;
 
 namespace {
 
-const char kBubbleReshowsHistogramName[] =
+const char kMouseLockBubbleReshowsHistogramName[] =
     "ExclusiveAccess.BubbleReshowsPerSession.MouseLock";
 
 }  // namespace
@@ -104,7 +104,8 @@ void MouseLockController::NotifyTabExclusiveAccessLost() {
 
 void MouseLockController::RecordBubbleReshowsHistogram(
     int bubble_reshow_count) {
-  UMA_HISTOGRAM_COUNTS_100(kBubbleReshowsHistogramName, bubble_reshow_count);
+  UMA_HISTOGRAM_COUNTS_100(kMouseLockBubbleReshowsHistogramName,
+                           bubble_reshow_count);
 }
 
 bool MouseLockController::HandleUserPressedEscape() {
