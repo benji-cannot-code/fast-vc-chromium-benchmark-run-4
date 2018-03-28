45079de84ef2c6f2fc29ee039cb8b60d13c9dd07
yiyix <yiyix@chromium.org>
2018-03-28 19:30:59 +0000

    Adding bool |scroll_offset_at_top| to RenderFrameMetadata
    GetLastScrollOffset() is updated to return whether the scroll
    offset is equal to 0, i.e., whether the thumb of the scroll bar
    is at top. This is because the only usage of GetLastScrollOffset
    is in thumbnailing, and it's used to check if the scroll
    offset is 0.
    
    Added bool |scroll_offset_at_top| to RenderFrameMetadata. Since
    |scroll_offset_at_top| does not change with every frame, it's added
    to low frequency fields.
    
    Bug:771360
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: If90b776905b7b7cb65b96faca6deb78a46a399fb
    Reviewed-on: https://chromium-review.googlesource.com/980704
    Commit-Queue: Yi Xu <yiyix@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Fady Samuel <fsamuel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546565}

--- content/browser/renderer_host/render_widget_host_view_mac.mm
+++ content/browser/renderer_host/render_widget_host_view_mac.mm
@@ -604,10 +604,6 @@ void RenderWidgetHostViewMac::SetBounds(const gfx::Rect& rect) {
   }
 }
 
-gfx::Vector2dF RenderWidgetHostViewMac::GetLastScrollOffset() const {
-  return last_scroll_offset_;
-}
-
 gfx::NativeView RenderWidgetHostViewMac::GetNativeView() const {
   return cocoa_view();
 }
@@ -1188,7 +1184,6 @@ void RenderWidgetHostViewMac::SubmitCompositorFrame(
   TRACE_EVENT0("browser", "RenderWidgetHostViewMac::OnSwapCompositorFrame");
 
   last_frame_root_background_color_ = frame.metadata.root_background_color;
-  last_scroll_offset_ = frame.metadata.root_scroll_offset;
 
   page_at_minimum_scale_ =
       frame.metadata.page_scale_factor == frame.metadata.min_page_scale_factor;
