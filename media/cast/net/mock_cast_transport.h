62bd8403077db2363ac69c1cee62f40af2cbc4ed
Adam Rice <ricea@chromium.org>
2018-03-28 06:46:51 +0000

    Revert "Mirroring service: Add rtp stream implementation."
    This reverts commit d3b390cf40fa9295ebc37eb5d8f2d3e2f598264e.
    
    Reason for revert: RtpStreamTest.VideoStreaming is failing on MSAN bot.
    
    Original change's description:
    > Mirroring service: Add rtp stream implementation.
    > 
    > Add RtpStream for the Mirroring service, which receives audio/video
    > data from the capturer and sends the data to media::cast::CastSender.
    > 
    > Bug: 734672
    > Change-Id: I8bf5813681463c1110458bf055211c3a233dc939
    > Reviewed-on: https://chromium-review.googlesource.com/967548
    > Reviewed-by: Yuri Wiitala <miu@chromium.org>
    > Commit-Queue: Xiangjun Zhang <xjz@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#546271}
    
    TBR=miu@chromium.org,xjz@chromium.org
    
    Change-Id: If4cf6c22452a6c6c9bdad32f860d6f8157ccd987
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 734672
    Reviewed-on: https://chromium-review.googlesource.com/983232
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546427}

--- media/cast/net/mock_cast_transport.h
+++ media/cast/net/mock_cast_transport.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef MEDIA_CAST_TEST_MOCK_CAST_TRANSPORT_H_
-#define MEDIA_CAST_TEST_MOCK_CAST_TRANSPORT_H_
+#ifndef MEDIA_CAST_NET_MOCK_CAST_TRANSPORT_H_
+#define MEDIA_CAST_NET_MOCK_CAST_TRANSPORT_H_
 
 #include <stdint.h>
 
@@ -48,4 +48,4 @@ class MockCastTransport : public CastTransport {
 }  // namespace cast
 }  // namespace media
 
-#endif  // MEDIA_CAST_TEST_MOCK_CAST_TRANSPORT_H_
+#endif  // MEDIA_CAST_NET_MOCK_CAST_TRANSPORT_H_
