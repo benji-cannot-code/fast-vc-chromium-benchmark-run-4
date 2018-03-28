b4546e5a9ec577f68da60face07c679648ec0b34
khmel@google.com <khmel@google.com>
2018-03-28 01:17:21 +0000

    overview: Add icon from arc apps to overview mode windows.
    Test: unit_tests ChromeLauncherControllerWithArcTest.ArcCustomAppIcon
    Bug: 817880
    Bug: 654622
    Change-Id: Ibe32c0fc714625d2c4d3a40e1b04715aea7ee04b
    Reviewed-on: https://chromium-review.googlesource.com/981603
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Yury Khmel <khmel@google.com>
    Cr-Commit-Position: refs/heads/master@{#546357}

--- chrome/browser/chromeos/profiles/profile_helper.cc
+++ chrome/browser/chromeos/profiles/profile_helper.cc
@@ -305,7 +305,8 @@ Profile* ProfileHelper::GetProfileByUser(const user_manager::User* user) {
   if (!user_to_profile_for_testing_.empty()) {
     std::map<const user_manager::User*, Profile*>::const_iterator it =
         user_to_profile_for_testing_.find(user);
-    return it == user_to_profile_for_testing_.end() ? NULL : it->second;
+    if (it != user_to_profile_for_testing_.end())
+      return it->second;
   }
 
   if (!user->is_profile_created())
@@ -325,7 +326,8 @@ Profile* ProfileHelper::GetProfileByUserUnsafe(const user_manager::User* user) {
   if (!user_to_profile_for_testing_.empty()) {
     std::map<const user_manager::User*, Profile*>::const_iterator it =
         user_to_profile_for_testing_.find(user);
-    return it == user_to_profile_for_testing_.end() ? NULL : it->second;
+    if (it != user_to_profile_for_testing_.end())
+      return it->second;
   }
 
   Profile* profile = NULL;
