29ae7b666315ef399b7df173741b823f8759f9d9
Abhishek Arya <inferno@chromium.org>
2018-03-28 16:34:49 +0000

    Reduce deps dependencies in content/test/fuzzer fuzz targets.
    Use //content/test:test_support instead of :fuzzer_support.
    This shrinks them by 22 MBs.
    
    R=mmoroz@google.com
    
    Change-Id: I806a28c588f742c2aaa7982e0109c0c6afec1167
    Reviewed-on: https://chromium-review.googlesource.com/984153
    Commit-Queue: Abhishek Arya <inferno@chromium.org>
    Reviewed-by: Max Moroz <mmoroz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546518}

--- content/test/fuzzer/origin_trial_token_fuzzer.cc
+++ content/test/fuzzer/origin_trial_token_fuzzer.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/at_exit.h"
 #include "base/i18n/icu_util.h"
-#include "content/test/fuzzer/fuzzer_support.h"
 #include "url/gurl.h"
 
 struct TestCase {
