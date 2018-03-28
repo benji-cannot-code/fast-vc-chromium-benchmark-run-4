8253f92862eace2d07b77b181c28b72b392162fc
Xiyuan Xia <xiyuan@chromium.org>
2018-03-28 19:01:26 +0000

    Remove obsolete headers from ui_test_utils.cc
    Bug: None.
    Test: Everything compiles.
    Change-Id: I48280b9384b80ba48998728a7b1b265a2a6011b9
    Reviewed-on: https://chromium-review.googlesource.com/984345
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546557}

--- chrome/test/base/ui_test_utils.cc
+++ chrome/test/base/ui_test_utils.cc
@@ -78,11 +78,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <windows.h>
 #endif
 
-#if defined(USE_AURA)
-#include "ash/shell.h"
-#include "ui/aura/window_event_dispatcher.h"
-#endif
-
 using content::NavigationController;
 using content::NavigationEntry;
 using content::OpenURLParams;
