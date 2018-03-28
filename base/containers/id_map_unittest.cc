249f7a9d0e6aff051a36359ef903e0602988b4de
tzik <tzik@chromium.org>
2018-03-28 20:41:13 +0000

    Fix removed value handling in base::IDMap
    This fixes some edge case handling of base::IDMap:
     - Avoid null pointer dereference even when a null value is stored.
     - DCHECK manipulations on removed values consistently on both delayed
       removal and immediate removal cases.
     - Hide the removed value on LookUp().
     - Allow readding a value on the delayed removal case.
    
    Change-Id: I9bf167e03fcc21eb1d6ed244f9d90bf68bece21d
    Reviewed-on: https://chromium-review.googlesource.com/983199
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546588}

--- base/containers/id_map_unittest.cc
+++ base/containers/id_map_unittest.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
-#include "base/memory/ptr_util.h"
+#include "base/test/gtest_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace base {
@@ -377,6 +377,24 @@ TEST(IDMapTest, Int64KeyType) {
 
   map.Remove(kId1);
   EXPECT_TRUE(map.IsEmpty());
+}
+
+TEST(IDMapTest, RemovedValueHandling) {
+  TestObject obj;
+  IDMap<TestObject*> map;
+  int key = map.Add(&obj);
+
+  IDMap<TestObject*>::iterator itr(&map);
+  map.Clear();
+  EXPECT_DCHECK_DEATH(map.Remove(key));
+  EXPECT_DCHECK_DEATH(map.Replace(key, &obj));
+  EXPECT_FALSE(map.Lookup(key));
+  EXPECT_FALSE(itr.IsAtEnd());
+  EXPECT_FALSE(itr.GetCurrentValue());
+
+  EXPECT_TRUE(map.IsEmpty());
+  map.AddWithID(&obj, key);
+  EXPECT_EQ(1u, map.size());
 }
 
 }  // namespace base
