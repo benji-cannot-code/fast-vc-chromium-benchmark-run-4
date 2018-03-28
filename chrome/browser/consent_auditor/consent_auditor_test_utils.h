64778c9dfaa010a0f9b70e1b1fd5b4caf42acad8
Martin Sramek <msramek@chromium.org>
2018-03-28 15:54:18 +0000

    Unify the instances of FakeConsentAuditor on Desktop and Chrome OS
    There are three instances of FakeConsentAuditor on Desktop and Chrome OS
    which are fairly similar - in fact, one of them is a superset of the
    other two.
    
    We extracted this one into a separate file, provided a factory method,
    and used that in the remaining two sites.
    
    There is one more instance of FakeConsentAuditor on iOS that is
    not covered by this CL (as it requires a separate factory method). This
    will be addressed in a follow-up CL.
    
    Bug: 781765
    Change-Id: Id193291b0b98a0b92cec8564dca65464db02caef
    Reviewed-on: https://chromium-review.googlesource.com/982433
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Martin Šrámek <msramek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546503}

---
+++ chrome/browser/consent_auditor/consent_auditor_test_utils.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_CONSENT_AUDITOR_CONSENT_AUDITOR_TEST_UTILS_H_
+#define CHROME_BROWSER_CONSENT_AUDITOR_CONSENT_AUDITOR_TEST_UTILS_H_
+
+#include <memory>
+
+namespace content {
+class BrowserContext;
+}
+
+class KeyedService;
+
+std::unique_ptr<KeyedService> BuildFakeConsentAuditor(
+    content::BrowserContext* context);
+
+#endif  // CHROME_BROWSER_CONSENT_AUDITOR_CONSENT_AUDITOR_TEST_UTILS_H_
