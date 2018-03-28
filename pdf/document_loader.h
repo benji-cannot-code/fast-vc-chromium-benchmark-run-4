94815dc5d88c360135c9bde8449b67fa014c1eb0
Lei Zhang <thestig@chromium.org>
2018-03-28 16:58:56 +0000

    Fix some nits in chrome_pdf::DocumentLoader.
    Add helper functions to make code more readable as well.
    
    Change-Id: I45fce08e091ca26d9eed0289416a3cd4b8261ffd
    Reviewed-on: https://chromium-review.googlesource.com/982649
    Reviewed-by: dsinclair <dsinclair@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546528}

--- pdf/document_loader.h
+++ pdf/document_loader.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
+#include "base/macros.h"
 #include "pdf/chunk_stream.h"
 #include "ppapi/utility/completion_callback_factory.h"
 
@@ -63,8 +64,9 @@ class DocumentLoader {
   void RequestData(uint32_t position, uint32_t size);
 
   bool IsDocumentComplete() const;
+  void SetDocumentSize(uint32_t size);
   uint32_t GetDocumentSize() const;
-  uint32_t count_of_bytes_received() const { return count_of_bytes_received_; }
+  uint32_t bytes_received() const { return bytes_received_; }
 
   // Clear pending requests from the queue.
   void ClearPendingRequests();
@@ -100,6 +102,8 @@ class DocumentLoader {
 
   bool SaveChunkData(char* input, uint32_t input_size);
 
+  uint32_t EndOfCurrentChunk() const;
+
   Client* const client_;
   std::string url_;
   std::unique_ptr<URLLoaderWrapper> loader_;
@@ -113,9 +117,15 @@ class DocumentLoader {
   static constexpr uint32_t kReadBufferSize = 256 * 1024;
   char buffer_[kReadBufferSize];
 
+  // The current chunk DocumentLoader is working with.
   Chunk chunk_;
+
+  // In units of Chunks.
   RangeSet pending_requests_;
-  uint32_t count_of_bytes_received_ = 0;
+
+  uint32_t bytes_received_ = 0;
+
+  DISALLOW_COPY_AND_ASSIGN(DocumentLoader);
 };
 
 }  // namespace chrome_pdf
