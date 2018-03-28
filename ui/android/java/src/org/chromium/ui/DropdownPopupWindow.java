d0d0018bf2911d0bb005d349da67a667945825d6
Amirhossein Simjour <asimjour@chromium.org>
2018-03-28 18:07:02 +0000

    [VR] Support AnchoredPopupWindow in VR
    This CL introduces UiWidgetFactory  and replaces the creation of
    PopupWindow in AnchoredPopupWindow with call to UiWidgetFactory
    to create a new PopupWindow.
    
    UiWidgetFactory is the intermediate class that is used to
    create new instances of android popups. When browsing in VR,
    UiWidgetFactory will be replaced with a new factory. As a result
    the new factory can create VR compatible UI Widgets.
    
    This CL is the break down of
    https://chromium-review.googlesource.com/961456
    to make the review and clean up process easier.
    
    Bug: 81988
    Change-Id: Iac56c502d281bfc076ed6bca0260da1d18d67aa8
    Reviewed-on: https://chromium-review.googlesource.com/964400
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Amirhossein Simjour <asimjour@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546546}

--- ui/android/java/src/org/chromium/ui/DropdownPopupWindow.java
+++ ui/android/java/src/org/chromium/ui/DropdownPopupWindow.java
@@ -29,7 +29,6 @@ public class DropdownPopupWindow implements AnchoredPopupWindow.LayoutObserver {
     private boolean mRtl;
     private int mInitialSelection = -1;
     private OnLayoutChangeListener mLayoutChangeListener;
-    private PopupWindow.OnDismissListener mOnDismissListener;
     private CharSequence mDescription;
     private AnchoredPopupWindow mAnchoredPopupWindow;
     ListAdapter mAdapter;
@@ -61,9 +60,7 @@ public class DropdownPopupWindow implements AnchoredPopupWindow.LayoutObserver {
         PopupWindow.OnDismissListener onDismissLitener = new PopupWindow.OnDismissListener() {
             @Override
             public void onDismiss() {
-                if (mOnDismissListener != null) {
-                    mOnDismissListener.onDismiss();
-                }
+                mAnchoredPopupWindow.dismiss();
                 mAnchorView.removeOnLayoutChangeListener(mLayoutChangeListener);
                 mAnchorView.setTag(null);
             }
@@ -146,7 +143,7 @@ public class DropdownPopupWindow implements AnchoredPopupWindow.LayoutObserver {
      * @param listener Listener that will be notified when the popup is dismissed.
      */
     public void setOnDismissListener(PopupWindow.OnDismissListener listener) {
-        mOnDismissListener = listener;
+        mAnchoredPopupWindow.addOnDismissListener(listener);
     }
 
     /**
