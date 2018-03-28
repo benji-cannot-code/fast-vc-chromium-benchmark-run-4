e3400411ade58155a14e07bfbbf7ff6345c82817
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2018-03-28 08:34:25 +0000

    css: Replace call to DeprecatedEqualIgnoringCase with a non-deprecated one
    Call EqualIgnoringASCIICase() instead; we are comparing to an ASCII
    constant.
    
    Bug: 627682
    
    Change-Id: Ib7b217bd3acf023c1025ae25f55bfd9480ca7924
    Reviewed-on: https://chromium-review.googlesource.com/980979
    Commit-Queue: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Reviewed-by: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546437}

--- third_party/WebKit/Source/core/css/AbstractPropertySetCSSStyleDeclaration.cpp
+++ third_party/WebKit/Source/core/css/AbstractPropertySetCSSStyleDeclaration.cpp
@@ -126,7 +126,7 @@ void AbstractPropertySetCSSStyleDeclaration::setProperty(
   if (!property_id)
     return;
 
-  bool important = DeprecatedEqualIgnoringCase(priority, "important");
+  bool important = EqualIgnoringASCIICase(priority, "important");
   if (!important && !priority.IsEmpty())
     return;
 
