bdc5f1a5de8dda7afa3634a79f780e195c9154b0
Kim Paulhamus <kpaulhamus@chromium.org>
2018-03-28 23:48:51 +0000

    [WebAuthn] Move support for BLE authenticators behind a content switch
    Bug: 664630
    Change-Id: I33752d771686b3c935b12bf13d1962cf456a32cd
    Reviewed-on: https://chromium-review.googlesource.com/978506
    Commit-Queue: Kim Paulhamus <kpaulhamus@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546662}

--- content/browser/webauth/authenticator_impl.cc
+++ content/browser/webauth/authenticator_impl.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/common/content_client.h"
+#include "content/public/common/content_features.h"
 #include "content/public/common/origin_util.h"
 #include "content/public/common/service_manager_connection.h"
 #include "crypto/sha2.h"
@@ -283,6 +284,11 @@ AuthenticatorImpl::AuthenticatorImpl(RenderFrameHost* render_frame_host)
       weak_factory_(this) {
   DCHECK(render_frame_host_);
   DCHECK(timer_);
+
+  protocols_.insert(device::U2fTransportProtocol::kUsbHumanInterfaceDevice);
+  if (base::FeatureList::IsEnabled(features::kWebAuthBle)) {
+    protocols_.insert(device::U2fTransportProtocol::kBluetoothLowEnergy);
+  }
 }
 
 AuthenticatorImpl::AuthenticatorImpl(RenderFrameHost* render_frame_host,
