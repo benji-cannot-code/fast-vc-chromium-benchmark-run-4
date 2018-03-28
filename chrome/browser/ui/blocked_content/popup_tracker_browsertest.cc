e4f0a8cdeb3f7e8947e84c749573c1f43e0056d6
Charlie Harrison <csharrison@chromium.org>
2018-03-28 20:24:01 +0000

    Log (user-initiated) engagement time for popup WebContents
    Bug: 825875
    Change-Id: I908fa19846156db42044f147979f23e8bfd75657
    Reviewed-on: https://chromium-review.googlesource.com/980602
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546582}

--- chrome/browser/ui/blocked_content/popup_tracker_browsertest.cc
+++ chrome/browser/ui/blocked_content/popup_tracker_browsertest.cc
@@ -30,8 +30,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/keycodes/dom/dom_key.h"
 #include "ui/events/keycodes/keyboard_codes.h"
 
-const char kPopupEngagement[] =
+const char kPopupFirstDocumentEngagement[] =
     "ContentSettings.Popups.FirstDocumentEngagementTime2";
+const char kPopupEngagement[] = "ContentSettings.Popups.EngagementTime";
+const char kPopupGestureClose[] =
+    "ContentSettings.Popups.EngagementTime.GestureClose";
 
 class PopupTrackerBrowserTest : public InProcessBrowserTest {
  public:
@@ -50,10 +53,13 @@ IN_PROC_BROWSER_TEST_F(PopupTrackerBrowserTest, NoPopup_NoTracker) {
   EXPECT_FALSE(PopupTracker::FromWebContents(
       browser()->tab_strip_model()->GetActiveWebContents()));
 
+  tester.ExpectTotalCount(kPopupFirstDocumentEngagement, 0);
   tester.ExpectTotalCount(kPopupEngagement, 0);
+  tester.ExpectTotalCount(kPopupGestureClose, 0);
 }
 
-IN_PROC_BROWSER_TEST_F(PopupTrackerBrowserTest, WindowOpenPopup_HasTracker) {
+IN_PROC_BROWSER_TEST_F(PopupTrackerBrowserTest,
+                       WindowOpenPopup_HasTracker_GestureClose) {
   base::HistogramTester tester;
   ui_test_utils::NavigateToURL(browser(),
                                embedded_test_server()->GetURL("/title1.html"));
@@ -71,12 +77,16 @@ IN_PROC_BROWSER_TEST_F(PopupTrackerBrowserTest, WindowOpenPopup_HasTracker) {
       browser()->tab_strip_model()->GetActiveWebContents()));
 
   // Close the popup and check metric.
+  int active_index = browser()->tab_strip_model()->active_index();
   content::WebContentsDestroyedWatcher destroyed_watcher(
-      browser()->tab_strip_model()->GetActiveWebContents());
-  browser()->tab_strip_model()->CloseAllTabs();
+      browser()->tab_strip_model()->GetWebContentsAt(active_index));
+  browser()->tab_strip_model()->CloseWebContentsAt(
+      active_index, TabStripModel::CLOSE_USER_GESTURE);
   destroyed_watcher.Wait();
 
+  tester.ExpectTotalCount(kPopupFirstDocumentEngagement, 1);
   tester.ExpectTotalCount(kPopupEngagement, 1);
+  tester.ExpectTotalCount(kPopupGestureClose, 1);
 }
 
 // OpenURLFromTab goes through a different code path than traditional popups
@@ -113,6 +123,7 @@ IN_PROC_BROWSER_TEST_F(PopupTrackerBrowserTest, ControlClick_HasTracker) {
       Profile::FromBrowserContext(new_contents->GetBrowserContext()));
   destroyed_watcher.Wait();
 
+  tester.ExpectTotalCount(kPopupFirstDocumentEngagement, 1);
   tester.ExpectTotalCount(kPopupEngagement, 1);
 }
 
@@ -144,6 +155,7 @@ IN_PROC_BROWSER_TEST_F(PopupTrackerBrowserTest, ShiftClick_HasTracker) {
       Profile::FromBrowserContext(new_contents->GetBrowserContext()));
   destroyed_watcher.Wait();
 
+  tester.ExpectTotalCount(kPopupFirstDocumentEngagement, 1);
   tester.ExpectTotalCount(kPopupEngagement, 1);
 }
 
@@ -174,6 +186,7 @@ IN_PROC_BROWSER_TEST_F(PopupTrackerBrowserTest, WhitelistedPopup_HasTracker) {
   browser()->tab_strip_model()->CloseAllTabs();
   destroyed_watcher.Wait();
 
+  tester.ExpectTotalCount(kPopupFirstDocumentEngagement, 1);
   tester.ExpectTotalCount(kPopupEngagement, 1);
 }
 
