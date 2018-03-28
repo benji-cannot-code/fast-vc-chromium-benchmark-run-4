736db3b6e1948489b0b2b2cd9ca1ae931897d080
Shakti Sahu <shaktisahu@chromium.org>
2018-03-28 22:19:52 +0000

    Thumbnails : Modification of ThumbnailProvider for offline items
    ThumbnailProviderImpl provides a front-end cache for the thumbnails which
    relies on ThumbnailDiskStorage and ThumbnailGenerator to generate and
    provide thumbnails. For offline items, thumbnails would be provided
    by the OfflineContentProvider directly which takes care thumbnail creation
    and storage. Modified the existing ThumbnailProviderImpl class to
    accommodate the caching for thumbnails provided from OfflineContentProvider.
    
    Design doc : https://docs.google.com/document/d/1bz1PH42dN0cPWOUWswAwBtA91x_a8jRPWGIvNpG9LhQ
    
    Bug: 817853
    Change-Id: Id3d928d9c76297f453f4a25d440ce0d338141294
    Reviewed-on: https://chromium-review.googlesource.com/967449
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546620}

--- chrome/android/java/src/org/chromium/chrome/browser/download/ui/DownloadHistoryItemWrapper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/ui/DownloadHistoryItemWrapper.java
@@ -27,7 +27,6 @@ import org.chromium.components.offline_items_collection.OfflineItem;
 import org.chromium.components.offline_items_collection.OfflineItem.Progress;
 import org.chromium.components.offline_items_collection.OfflineItemFilter;
 import org.chromium.components.offline_items_collection.OfflineItemState;
-import org.chromium.components.offline_items_collection.VisualsCallback;
 import org.chromium.components.url_formatter.UrlFormatter;
 import org.chromium.ui.widget.Toast;
 
@@ -178,9 +177,6 @@ public abstract class DownloadHistoryItemWrapper extends TimedItem {
 
     /** @return The file extension type. See list at the top of the file. */
     public abstract int getFileExtensionType();
-
-    /** Requests the backend provider to provide thumbnail for this item. */
-    public abstract void requestVisualsFromProvider(VisualsCallback callback);
 
     /** @return How much of the download has completed, or null if there is no progress. */
     abstract Progress getDownloadProgress();
@@ -355,9 +351,6 @@ public abstract class DownloadHistoryItemWrapper extends TimedItem {
         }
 
         @Override
-        public void requestVisualsFromProvider(VisualsCallback callback) {}
-
-        @Override
         public Progress getDownloadProgress() {
             return mItem.getDownloadInfo().getProgress();
         }
@@ -510,6 +503,7 @@ public abstract class DownloadHistoryItemWrapper extends TimedItem {
 
         @Override
         public String getId() {
+            // TODO(shaktisahu): May be change this to mItem.id.toString().
             return mItem.id.id;
         }
 
@@ -564,11 +558,6 @@ public abstract class DownloadHistoryItemWrapper extends TimedItem {
         public int getFileExtensionType() {
             // TODO(shaktisahu): Fix this.
             return FILE_EXTENSION_OTHER;
-        }
-
-        @Override
-        public void requestVisualsFromProvider(VisualsCallback callback) {
-            getOfflineContentProvider().getVisualsForItem(mItem.id, callback);
         }
 
         @Override
