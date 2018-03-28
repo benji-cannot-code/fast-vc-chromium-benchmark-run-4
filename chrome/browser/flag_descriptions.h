c837f0610a57db1ee06467abad27a67768dc4fe6
Darren Shen <shend@chromium.org>
2018-03-28 04:45:50 +0000

    Add enable-stylus-keyboard flag definition
    This will be used to hide away ongoing stylus keyboard work.
    
    Bug: 824604
    Change-Id: Iaa3aefc170e6d3a7633a08e7a0a0cd28d7ce19fb
    Reviewed-on: https://chromium-review.googlesource.com/981572
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546407}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1623,6 +1623,9 @@ extern const char kEnableFloatingVirtualKeyboardDescription[];
 extern const char kEnableImeMenuName[];
 extern const char kEnableImeMenuDescription[];
 
+extern const char kEnableStylusVirtualKeyboardName[];
+extern const char kEnableStylusVirtualKeyboardDescription[];
+
 extern const char kEnableUnifiedMultiDeviceSettingsName[];
 extern const char kEnableUnifiedMultiDeviceSettingsDescription[];
 
