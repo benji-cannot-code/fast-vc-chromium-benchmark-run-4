30327efdb16d5aaa2aca57e1659c19030c916db5
James Hawkins <jhawkins@chromium.org>
2018-03-28 22:29:53 +0000

    SmartLock: Simplify the NotificationController.
    Replaced the interface with the only implementation (CrOS).
    
    R=khorimoto@chromium.org
    
    Bug: none
    Test: none
    Change-Id: Iea58a8dd19248e7a1b32c0b3f1d11bdae61c6c41
    Reviewed-on: https://chromium-review.googlesource.com/985192
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: James Hawkins <jhawkins@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546625}

--- chrome/browser/chromeos/login/easy_unlock/easy_unlock_service_regular.cc
+++ chrome/browser/chromeos/login/easy_unlock/easy_unlock_service_regular.cc
@@ -85,7 +85,7 @@ const char kKeyDevices[] = "devices";
 EasyUnlockServiceRegular::EasyUnlockServiceRegular(Profile* profile)
     : EasyUnlockServiceRegular(
           profile,
-          EasyUnlockNotificationController::Create(profile)) {}
+          std::make_unique<EasyUnlockNotificationController>(profile)) {}
 
 EasyUnlockServiceRegular::EasyUnlockServiceRegular(
     Profile* profile,
--- chrome/browser/chromeos/login/easy_unlock/easy_unlock_service_unittest_chromeos.cc
+++ chrome/browser/chromeos/login/easy_unlock/easy_unlock_service_unittest_chromeos.cc
@@ -57,7 +57,8 @@ const char kSecondaryGaiaId[] = "2222222222";
 class MockEasyUnlockNotificationController
     : public chromeos::EasyUnlockNotificationController {
  public:
-  MockEasyUnlockNotificationController() {}
+  MockEasyUnlockNotificationController()
+      : EasyUnlockNotificationController(nullptr) {}
   ~MockEasyUnlockNotificationController() override {}
 
   // EasyUnlockNotificationController:
