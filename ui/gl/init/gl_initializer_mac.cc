fe0651db7cf91878490889936067b6e245e2c3c8
Alexis Hetu <sugoi@google.com>
2018-03-28 01:14:32 +0000

    MacOS EGL build fix
    A recent cl fixed the module path for EGL on MacOS:
    https://chromium-review.googlesource.com/c/chromium/src/+/970721
    
    There was a missing include file in that cl, but none of the bots
    picked up the failure since none of the MacOS bots compile with
    EGL enabled.
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ie85c7bb4573a88215a285bc5a34e940fb1e39850
    Reviewed-on: https://chromium-review.googlesource.com/982256
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546355}

--- ui/gl/init/gl_initializer_mac.cc
+++ ui/gl/init/gl_initializer_mac.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base_paths.h"
 #include "base/files/file_path.h"
 #include "base/logging.h"
+#include "base/mac/bundle_locations.h"
 #include "base/mac/foundation_util.h"
 #include "base/native_library.h"
 #include "base/path_service.h"
