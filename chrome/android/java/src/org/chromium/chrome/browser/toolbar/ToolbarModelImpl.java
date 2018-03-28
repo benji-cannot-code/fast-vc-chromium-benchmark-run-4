24786e0c72aac249b77fe643f51f56b9c587b19f
Troy Hildebrandt <thildebr@chromium.org>
2018-03-28 19:35:44 +0000

    Don't display query in omnibox for CCTs.
    Change-Id: Ibab89af2abf5197d7a797a01b1304ea0ffb18907
    Reviewed-on: https://chromium-review.googlesource.com/980818
    Reviewed-by: Maria Khomenko <mariakhomenko@chromium.org>
    Commit-Queue: Troy Hildebrandt <thildebr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546566}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarModelImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarModelImpl.java
@@ -14,6 +14,7 @@ import org.chromium.base.ContextUtils;
 import org.chromium.base.VisibleForTesting;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeFeatureList;
+import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.chrome.browser.UrlConstants;
 import org.chromium.chrome.browser.dom_distiller.DomDistillerServiceFactory;
 import org.chromium.chrome.browser.dom_distiller.DomDistillerTabUtils;
@@ -412,6 +413,7 @@ public class ToolbarModelImpl
      *         display search terms in place of SRP URL.
      */
     private String extractSearchTermsFromUrlInternal(String url) {
+        if (mTab != null && !(mTab.getActivity() instanceof ChromeTabbedActivity)) return null;
         if (!mQueryInOmniboxEnabled || !securityLevelSafeForQueryInOmnibox()) return null;
 
         String searchTerms = TemplateUrlService.getInstance().extractSearchTermsFromUrl(url);
