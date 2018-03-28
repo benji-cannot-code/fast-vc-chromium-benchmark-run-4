cd8c6acf4094a5fd269b736e67350c312e18792a
Henrik Boström <hbos@chromium.org>
2018-03-28 12:18:02 +0000

    Reland "Fix deadlock issue in WebRtcMediaStreamTrackAdapterMap."
    This is a reland of 2b62de0bf01e7c71c82719ec30e19a32ca251ad6,
    but without adding the stress test.
    
    Original change's description:
    > Fix deadlock issue in WebRtcMediaStreamTrackAdapterMap.
    >
    > The fix is to reduce the scope of the |lock_| during track adapter
    > creation because Create[Local/Remote]TrackAdapter() may synchronize with
    > another thread that is busy waiting for the |lock_|. There is no reason
    > to hold the |lock_| while creating the adapter; we are ensured no other
    > adapters of the same kind (=local/remote) are created concurrently
    > because they may only be created on the appropriate thread (local =
    > main thread, remote = signaling thread) which is the thread currently
    > running the operation. In other words, at most 1
    > GetOrCreateLocalTrackAdapter() on the main thread and at most 1
    > GetOrCreateRemoteTrackAdapter() on the signaling thread can run at the
    > same time.
    >
    > This is the equivalent of the WebRtcMediaStreamAdapterMap fix
    > (https://chromium-review.googlesource.com/c/868656/) but for the track
    > adapter map.
    >
    > The deadlock is quite rare, but due to recent unrelated changes it has
    > become frequent enough that it is easily reproducible on some platforms
    > and applications, see bug.
    >
    > Bug: 813574
    > Change-Id: I2e7ba605db973221f80443267fd099c5973ab818
    > Reviewed-on: https://chromium-review.googlesource.com/980949
    > Commit-Queue: Henrik Boström <hbos@chromium.org>
    > Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#546102}
    
    Bug: 813574
    Change-Id: I8b57c9fd3df3a4dff96ef983ccfd33508ba57f25
    Reviewed-on: https://chromium-review.googlesource.com/983372
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Henrik Boström <hbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546460}

--- content/renderer/media/webrtc/webrtc_media_stream_track_adapter_map_unittest.cc
+++ content/renderer/media/webrtc/webrtc_media_stream_track_adapter_map_unittest.cc
@@ -34,6 +34,10 @@ class WebRtcMediaStreamTrackAdapterMapTest : public ::testing::Test {
 
   void TearDown() override { blink::WebHeap::CollectAllGarbageForTesting(); }
 
+  scoped_refptr<base::SingleThreadTaskRunner> signaling_thread() const {
+    return dependency_factory_->GetWebRtcSignalingThread();
+  }
+
   blink::WebMediaStreamTrack CreateLocalTrack(const std::string& id) {
     blink::WebMediaStreamSource web_source;
     web_source.Initialize(
@@ -54,7 +58,7 @@ class WebRtcMediaStreamTrackAdapterMapTest : public ::testing::Test {
       webrtc::MediaStreamTrackInterface* webrtc_track) {
     DCHECK(main_thread_->BelongsToCurrentThread());
     std::unique_ptr<WebRtcMediaStreamTrackAdapterMap::AdapterRef> adapter;
-    dependency_factory_->GetWebRtcSignalingThread()->PostTask(
+    signaling_thread()->PostTask(
         FROM_HERE,
         base::BindOnce(&WebRtcMediaStreamTrackAdapterMapTest::
                            GetOrCreateRemoteTrackAdapterOnSignalingThread,
@@ -67,8 +71,7 @@ class WebRtcMediaStreamTrackAdapterMapTest : public ::testing::Test {
   void GetOrCreateRemoteTrackAdapterOnSignalingThread(
       webrtc::MediaStreamTrackInterface* webrtc_track,
       std::unique_ptr<WebRtcMediaStreamTrackAdapterMap::AdapterRef>* adapter) {
-    DCHECK(dependency_factory_->GetWebRtcSignalingThread()
-               ->BelongsToCurrentThread());
+    DCHECK(signaling_thread()->BelongsToCurrentThread());
     *adapter = map_->GetOrCreateRemoteTrackAdapter(webrtc_track);
   }
 
@@ -79,7 +82,7 @@ class WebRtcMediaStreamTrackAdapterMapTest : public ::testing::Test {
     base::WaitableEvent waitable_event(
         base::WaitableEvent::ResetPolicy::MANUAL,
         base::WaitableEvent::InitialState::NOT_SIGNALED);
-    dependency_factory_->GetWebRtcSignalingThread()->PostTask(
+    signaling_thread()->PostTask(
         FROM_HERE, base::BindOnce(&WebRtcMediaStreamTrackAdapterMapTest::
                                       RunMessageLoopUntilIdleOnSignalingThread,
                                   base::Unretained(this), &waitable_event));
@@ -89,8 +92,7 @@ class WebRtcMediaStreamTrackAdapterMapTest : public ::testing::Test {
 
   void RunMessageLoopUntilIdleOnSignalingThread(
       base::WaitableEvent* waitable_event) {
-    DCHECK(dependency_factory_->GetWebRtcSignalingThread()
-               ->BelongsToCurrentThread());
+    DCHECK(signaling_thread()->BelongsToCurrentThread());
     base::RunLoop().RunUntilIdle();
     waitable_event->Signal();
   }
