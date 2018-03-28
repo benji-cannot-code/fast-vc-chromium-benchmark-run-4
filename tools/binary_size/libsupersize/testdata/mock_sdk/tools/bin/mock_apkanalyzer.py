ffe1a43f3e01d1a0971ac868668f6238e4d0989f
Peter Wen <wnwen@chromium.org>
2018-03-28 13:19:57 +0000

    Supersize: Add generated apk integration test
    This exercises pak file parsing, dex file parsing, and all the apk_path
    dependent code paths.
    
    Bug: 824439
    Change-Id: Idae56ee6bda7c1bd942c73979ff69e115753bcbc
    Reviewed-on: https://chromium-review.googlesource.com/980815
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Reviewed-by: agrieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546469}

---
+++ tools/binary_size/libsupersize/testdata/mock_sdk/tools/bin/mock_apkanalyzer.py
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+import os
+import sys
+
+
+_SCRIPT_DIR = os.path.dirname(__file__)
+_OUTPUT_FILE = os.path.join(_SCRIPT_DIR, 'apkanalyzer.output')
+
+
+def main():
+  # Without a proguard mapping file, the last argument is the apk_path.
+  apk_path = sys.argv[-1]
+  assert os.path.exists(apk_path), 'Apk does not exist: {}'.format(apk_path)
+  with open(_OUTPUT_FILE, 'r') as f:
+    sys.stdout.write(f.read())
+
+
+if __name__ == '__main__':
+  main()
