65d96b8cf3afb7b4462e3e55a7a26435d97dda62
Rune Lillesveen <futhark@chromium.org>
2018-03-29 01:29:46 +0000

    [Squad] Remove AnimationPropertiesLocked.
    This flag was added to try to narrow down the source of a crasher. I
    could not find any reports on DCHECKs triggered by this. Removing as
    this requires mutating the ComputedStyle after it's been set on the
    LayoutObject which we are trying to get rid of.
    
    Bug: 813068, 783598
    Change-Id: I8bd02b143c6aaf820837721072e21e2324362f90
    Reviewed-on: https://chromium-review.googlesource.com/984874
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546692}

--- third_party/WebKit/Source/core/dom/Element.cpp
+++ third_party/WebKit/Source/core/dom/Element.cpp
@@ -2120,8 +2120,6 @@ void Element::RecalcStyle(StyleRecalcChange change) {
         if (ElementAnimations* element_animations =
                 data->GetElementAnimations())
           element_animations->SetAnimationStyleChange(false);
-        if (ComputedStyle* style = MutableComputedStyle())
-          style->SetAnimationPropertiesLocked(false);
       }
     }
     if (ParentComputedStyle()) {
@@ -2460,8 +2458,6 @@ void Element::SetAnimationStyleChange(bool animation_style_change) {
   if (ElementAnimations* element_animations =
           GetElementRareData()->GetElementAnimations())
     element_animations->SetAnimationStyleChange(animation_style_change);
-  if (ComputedStyle* style = MutableComputedStyle())
-    style->SetAnimationPropertiesLocked(animation_style_change);
 }
 
 void Element::ClearAnimationStyleChange() {
@@ -2470,8 +2466,6 @@ void Element::ClearAnimationStyleChange() {
   if (ElementAnimations* element_animations =
           GetElementRareData()->GetElementAnimations())
     element_animations->SetAnimationStyleChange(false);
-  if (ComputedStyle* style = MutableComputedStyle())
-    style->SetAnimationPropertiesLocked(false);
 }
 
 void Element::SetNeedsAnimationStyleRecalc() {
