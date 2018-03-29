42c4d0b51c9122a545b0c51e200be624194a60b7
Paul Meyer <paulmeyer@chromium.org>
2018-03-29 13:29:53 +0000

    Remove unneeded |message_source| variable from TouchEvent.
    Change-Id: I6d37061773bdba7bdee70bd41f945a95794de7c3
    Reviewed-on: https://chromium-review.googlesource.com/982359
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Paul Meyer <paulmeyer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546805}

--- third_party/WebKit/Source/core/events/TouchEvent.cpp
+++ third_party/WebKit/Source/core/events/TouchEvent.cpp
@@ -264,7 +264,6 @@ void TouchEvent::preventDefault() {
   // A common developer error is to wait too long before attempting to stop
   // scrolling by consuming a touchmove event. Generate a warning if this
   // event is uncancelable.
-  MessageSource message_source = kJSMessageSource;
   String warning_message;
   switch (HandlingPassive()) {
     case PassiveMode::kNotPassive:
@@ -287,7 +286,6 @@ void TouchEvent::preventDefault() {
                 WebFeature::
                     kUncancelableTouchEventDueToMainThreadResponsivenessPreventDefaulted);
           }
-          message_source = kInterventionMessageSource;
           warning_message =
               "Ignored attempt to cancel a " + type() +
               " event with cancelable=false. This event was forced to be "
@@ -307,7 +305,6 @@ void TouchEvent::preventDefault() {
       // an author may use touch action but call preventDefault for interop with
       // browsers that don't support touch-action.
       if (current_touch_action_ == TouchAction::kTouchActionAuto) {
-        message_source = kInterventionMessageSource;
         warning_message =
             "Unable to preventDefault inside passive event listener due to "
             "target being treated as passive. See "
