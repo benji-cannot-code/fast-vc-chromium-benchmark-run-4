70a087043516ac21b689d3a3ebc075a7f60406a5
Vadym Doroshenko <dvadym@chromium.org>
2018-03-29 18:47:40 +0000

    [IOS Password Manager] Plug-in FormParser in Password form parsing.
    On CL https://chromium-review.googlesource.com/860138 FormParser was implemented.
    This CL makes use of FormParser in Password Manager. It contains:
    1.Minor fixes in FormParser
    2.Moving JSON->FormData functions from autofill_agent.mm to autofill_util (since they will
    be used in PasswordManager).
    3.Calling Autofill convert utils for converting DOM->JSON from PasswordController.js
    4.Calling Autofill JSON->FormData convert utils from PassswordController.mm.
    5.Removing all old password form parsing code.
    6.Unittests update.
    
    More details in go/ios-password-form-parsing.
    
    Bug: 807623
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I256301ae09f9bdad06522847022d155fa0d78ba7
    Reviewed-on: https://chromium-review.googlesource.com/901664
    Commit-Queue: Vadym Doroshenko <dvadym@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Vaclav Brozek <vabr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546893}

--- components/autofill/ios/fill/resources/fill.js
+++ components/autofill/ios/fill/resources/fill.js
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // This file provides methods used to fill forms in JavaScript.
 
-goog.module('__crWeb.fill');
+goog.provide('__crWeb.fill');
 
 /**
  * @typedef {{
@@ -421,6 +421,19 @@ __gCrWeb.fill.createAndDispatchHTMLEvent = function(
   element.dispatchEvent(changeEvent);
 };
 
+ /**
+  * Returns a canonical action for |formElement|. It works the same as upstream
+  * function GetCanonicalActionForForm.
+  * @param {HTMLFormElement} formElement
+  * @return {string} Canonical action.
+  */
+__gCrWeb.fill.getCanonicalActionForForm = function(formElement) {
+  var rawAction = formElement.getAttribute('action') || "";
+  var absoluteUrl = __gCrWeb.common.absoluteURL(
+     formElement.ownerDocument, rawAction);
+  return __gCrWeb.common.removeQueryAndReferenceFromURL(absoluteUrl);
+};
+
 /**
  * Extracts fields from |controlElements| with |extractMask| to |formFields|.
  * The extracted fields are also placed in |elementArray|.
@@ -606,7 +619,7 @@ function matchLabelsAndFields_(
  *     will be processed.
  * @param {number} extractMask Mask controls what data is extracted from
  *     formElement.
- * @param {AutofillFormData} form Form to fill in the AutofillFormData
+ * @param {Object} form Form to fill in the AutofillFormData
  *     information of formElement.
  * @param {?AutofillFormFieldData} field Field to fill in the form field
  *     information of formControlElement.
@@ -705,7 +718,7 @@ __gCrWeb.fill.formOrFieldsetsToFormData = function(
  *     formElment, the FormField of which will be returned in field.
  * @param {number} extractMask Mask controls what data is extracted from
  *     formElement.
- * @param {AutofillFormData} form Form to fill in the AutofillFormData
+ * @param {Object} form Form to fill in the AutofillFormData
  *     information of formElement.
  * @param {?AutofillFormFieldData} field Field to fill in the form field
  *     information of formControlElement.
@@ -721,8 +734,7 @@ __gCrWeb.fill.webFormElementToFormData = function(
   form['name'] = __gCrWeb.form.getFormIdentifier(formElement);
   form['origin'] =
       __gCrWeb.common.removeQueryAndReferenceFromURL(frame.location.href);
-  form['action'] = __gCrWeb.common.absoluteURL(
-      frame.document, formElement.getAttribute('action'));
+  form['action'] = __gCrWeb.fill.getCanonicalActionForForm(formElement);
 
   // Note different from form_autofill_util.cc version of this method, which
   // computes |form.action| using document.completeURL(form_element.action())
@@ -1884,6 +1896,5 @@ __gCrWeb.fill.webFormControlElementToFormField = function(
   }
   field['value'] = value;
 };
-
 
 }());  // End of anonymous object
