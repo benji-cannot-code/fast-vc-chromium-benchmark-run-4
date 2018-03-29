647a972b71e4b5b465c0e8ff2ab5b119d8c3fd2a
Robert Liao <robliao@chromium.org>
2018-03-29 02:50:34 +0000

    Forward BrowserView::CutCopyPaste to the NSApp
    http://crbug.com/773565 removed accelerators for textfields, breaking
    Cut, Copy, and Paste in the Chrome Menu. This fix forwards those
    calls to NSApp.
    
    BUG=788262
    
    Change-Id: I70adfe270dd672961ca04bc0e4a9ab498e7f2944
    Reviewed-on: https://chromium-review.googlesource.com/984346
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546712}

--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -155,6 +155,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/profiles/profile_chooser_view.h"
 #endif  // !defined(OS_CHROMEOS)
 
+#if defined(OS_MACOSX)
+#include "chrome/browser/ui/views/frame/browser_view_commands_mac.h"
+#endif
+
 #if defined(USE_AURA)
 #include "chrome/browser/ui/views/theme_profile_key.h"
 #include "ui/aura/client/window_parenting_client.h"
@@ -1413,6 +1417,9 @@ void BrowserView::HandleKeyboardEvent(const NativeWebKeyboardEvent& event) {
 // won't do anything. We'll need something like an overall clipboard command
 // manager to do that.
 void BrowserView::CutCopyPaste(int command_id) {
+#if defined(OS_MACOSX)
+  ForwardCutCopyPasteToNSApp(command_id);
+#else
   // If a WebContents is focused, call its member method.
   //
   // We could make WebContents register accelerators and then just use the
@@ -1450,6 +1457,7 @@ void BrowserView::CutCopyPaste(int command_id) {
   ui::Accelerator accelerator;
   GetAccelerator(command_id, &accelerator);
   GetFocusManager()->ProcessAccelerator(accelerator);
+#endif  // defined(OS_MACOSX)
 }
 
 WindowOpenDisposition BrowserView::GetDispositionForPopupBounds(
---
+++ chrome/browser/ui/views/frame/browser_view_commands_mac.mm
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "chrome/browser/ui/views/frame/browser_view_commands_mac.h"
+
+#import <AppKit/AppKit.h>
+
+#include "base/logging.h"
+#include "chrome/app/chrome_command_ids.h"
+
+void ForwardCutCopyPasteToNSApp(int command_id) {
+  if (command_id == IDC_CUT)
+    [NSApp sendAction:@selector(cut:) to:nil from:nil];
+  else if (command_id == IDC_COPY)
+    [NSApp sendAction:@selector(copy:) to:nil from:nil];
+  else if (command_id == IDC_PASTE)
+    [NSApp sendAction:@selector(paste:) to:nil from:nil];
+  else
+    NOTREACHED();
+}
