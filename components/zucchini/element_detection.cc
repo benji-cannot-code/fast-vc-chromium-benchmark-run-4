29dce6e7453f465fc1f39dfdec65e8cb17ba0a1b
Etienne Pierre-Doray <etiennep@chromium.org>
2018-03-29 13:33:46 +0000

    [Zucchini] Create dex disassembler.
    Creates Disassembler that recognises and parses DEX format. For now, it doesn't
    extract any type reference, so it is equivalent to DisassemblerNoOp. Extraction
    of various types of reference will be added in a follow-up CL.
    BufferView::covers_array() and unittests were also added.
    
    Change-Id: I08756244e9af899cf0f40dabd2b0059e1749328e
    Reviewed-on: https://chromium-review.googlesource.com/967603
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546807}

--- components/zucchini/element_detection.cc
+++ components/zucchini/element_detection.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "components/zucchini/disassembler.h"
+#include "components/zucchini/disassembler_dex.h"
 #include "components/zucchini/disassembler_no_op.h"
 #include "components/zucchini/disassembler_win32.h"
 
@@ -37,6 +38,12 @@ std::unique_ptr<Disassembler> MakeDisassemblerWithoutFallback(
       return disasm;
   }
 
+  if (DisassemblerDex::QuickDetect(image)) {
+    auto disasm = Disassembler::Make<DisassemblerDex>(image);
+    if (disasm && disasm->size() >= kMinProgramSize)
+      return disasm;
+  }
+
   return nullptr;
 }
 
@@ -47,6 +54,8 @@ std::unique_ptr<Disassembler> MakeDisassemblerOfType(ConstBufferView image,
       return Disassembler::Make<DisassemblerWin32X86>(image);
     case kExeTypeWin32X64:
       return Disassembler::Make<DisassemblerWin32X64>(image);
+    case kExeTypeDex:
+      return Disassembler::Make<DisassemblerDex>(image);
     case kExeTypeNoOp:
       return Disassembler::Make<DisassemblerNoOp>(image);
     default:
