510d6466c50ef713d9a2fe616a4f3169d33bf2a2
erikchen <erikchen@chromium.org>
2018-03-29 15:23:21 +0000

    Make sure that PeriodicallyShimNewMallocZones is called during allocator shim startup.
    The old logic was relying on the flag "--enable-heap-profiling", which meant the logic wasn't being invoked for OOP HP.
    
    Change-Id: I48c0359e2724800ef3bba369a53757e3f611f330
    Reviewed-on: https://chromium-review.googlesource.com/969242
    Reviewed-by: Primiano Tucci <primiano@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546841}

--- chrome/common/profiling/profiling_client.cc
+++ chrome/common/profiling/profiling_client.cc
@@ -5,8 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/common/profiling/profiling_client.h"
 
+#include "base/allocator/allocator_interception_mac.h"
 #include "base/files/platform_file.h"
 #include "base/trace_event/malloc_dump_provider.h"
+#include "build/build_config.h"
 #include "chrome/common/profiling/memlog_allocator_shim.h"
 #include "chrome/common/profiling/memlog_sender_pipe.h"
 #include "chrome/common/profiling/memlog_stream.h"
@@ -73,6 +75,14 @@ void ProfilingClient::StartProfiling(mojom::ProfilingParamsPtr params) {
   }
 
   base::trace_event::MallocDumpProvider::GetInstance()->DisableMetrics();
+
+#if defined(OS_MACOSX)
+  // On macOS, this call is necessary to shim malloc zones that were created
+  // after startup. This cannot be done during shim initialization because the
+  // task scheduler has not yet been initialized.
+  base::allocator::PeriodicallyShimNewMallocZones();
+#endif
+
   InitAllocatorShim(memlog_sender_pipe_.get(), std::move(params));
 }
 
