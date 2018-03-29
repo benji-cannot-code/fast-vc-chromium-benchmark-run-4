188211242086dfbed057244866b9887f37ad30ae
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-03-29 22:08:08 +0000

    Import wpt@3adbaa2470fc796c189ad98a7363f43df83b42b1
    Using wpt-import in Chromium 0ae027437e1c95625fca8eb857b9303e60250714.
    With Chromium commits locally applied on WPT:
    d04a1e5256 "Revert "bluetooth: FakeBluetoothChooser impl.""
    e28b9a9799 "Add wpt test for back/forward mouse buttons."
    
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/14873
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    hta@chromium.org:
      external/wpt/webrtc
    jsbell@chromium.org:
      external/wpt/resources
    kpaulhamus@chromium.org:
      external/wpt/webauthn
    rouslan@chromium.org, mathp@chromium.org:
      external/wpt/payment-request
    xlai@chromium.org, jinho.bang@samsung.com, hs1217.lee@samsung.com:
      external/wpt/css/geometry
    
    TBR=markdittmer
    
    No-Export: true
    Change-Id: Iabdedfe95a406a66c29fe0b0e309db3218f1f5e8
    Reviewed-on: https://chromium-review.googlesource.com/986281
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546977}

---
+++ third_party/WebKit/LayoutTests/external/wpt/webauthn/interfaces.https.any.js
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/resources/WebIDLParser.js
+// META: script=/resources/idlharness.js
+
+'use strict';
+
+if (self.importScripts) {
+  importScripts('/resources/testharness.js');
+  importScripts('/resources/WebIDLParser.js', '/resources/idlharness.js');
+}
+
+// https://w3c.github.io/webauthn/
+
+promise_test(async () => {
+  const webauthnIdl = await fetch('/interfaces/webauthn.idl').then(r => r.text());
+
+  const idlArray = new IdlArray();
+  idlArray.add_idls(webauthnIdl);
+
+  // static IDL tests
+  idlArray.add_untested_idls('interface CredentialCreationOptions {};');
+  idlArray.add_untested_idls('interface CredentialRequestOptions {};');
+  idlArray.add_untested_idls("interface Navigator { };");
+  // TODO: change to "tested" for real browsers?
+  idlArray.add_untested_idls("partial interface Navigator { readonly attribute WebAuthentication authentication; };");
+  idlArray.add_objects({
+    WebAuthentication: ["navigator.authentication"]
+  });
+  idlArray.test();
+  done();
+}, 'WebAuthn interfaces.');
