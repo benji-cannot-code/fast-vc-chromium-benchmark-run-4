e27a390ffd17d8c5654b9d8331096cf1f54d49ab
Alan Lau <alanlau@google.com>
2018-03-29 18:39:51 +0000

    Wire speech level update to assistant mojo interface
    Change-Id: Ia1c703a1031a889ece73881dafec04302e1d5b9e
    Reviewed-on: https://chromium-review.googlesource.com/977221
    Commit-Queue: Alan Lau <alanlau@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546879}

--- ash/assistant/ash_assistant_controller.cc
+++ ash/assistant/ash_assistant_controller.cc
@@ -42,6 +42,11 @@ void AshAssistantController::OnTextResponse(const std::string& response) {
   NOTIMPLEMENTED();
 }
 
+void AshAssistantController::OnSpeechLevelUpdated(float speech_level) {
+  // TODO(dmblack): Handle.
+  NOTIMPLEMENTED();
+}
+
 void AshAssistantController::OnOpenUrlResponse(const GURL& url) {
   Shell::Get()->shell_delegate()->OpenUrlFromArc(url);
 }
