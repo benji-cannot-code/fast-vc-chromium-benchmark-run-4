ba634ea9fe8b8096734359c91bb65a0043adf158
Egor Pasko <pasko@chromium.org>
2018-03-29 15:52:09 +0000

    cygprofile: switch scripts to use vpython
    This should avoid import problems from various catapult dependencies
    that are pulled via src/.vpython. One such problem was recently
    mentioned in http://crrev.com/546790.
    
    Also for consistency:
    1. make all unittests executable
    2. remove shebang and exec bit for the scripts that never produce output
       (utilities)
    
    To read more about vpython:
      https://chromium.googlesource.com/infra/infra/+/master/doc/users/vpython_one_page.md
    
    Change-Id: I167a13f03b1f4a0c95d57557055840fa35db7f41
    Reviewed-on: https://chromium-review.googlesource.com/986264
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546845}

--- tools/cygprofile/phased_orderfile_unittest.py
+++ tools/cygprofile/phased_orderfile_unittest.py
@@ -1,4 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/env vpython
 # Copyright 2018 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -78,3 +79,7 @@ class PhasedOrderfileTestCase(unittest.TestCase):
                           opo([], [], [130, 140]),
                           opo([10], [20, 30], [])],
                          phaser._GetOrderfilePhaseOffsets())
+
+
+if __name__ == "__main__":
+  unittest.main()
