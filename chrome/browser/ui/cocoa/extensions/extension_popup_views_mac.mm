59b40c2aded5411f32d34057d2baebf108489d50
Robert Liao <robliao@chromium.org>
2018-03-29 18:36:36 +0000

    Rename ExtensionPopup::OnAnchorWindowActivation() to ExtensionPopup::CloseUnlessUnderInspection()
    This name removes a policy implication from what the function does.
    
    BUG=825867
    
    Change-Id: Ida740958cf416bc917ec27e338f3eaaad180a5e1
    Reviewed-on: https://chromium-review.googlesource.com/985637
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546873}

--- chrome/browser/ui/cocoa/extensions/extension_popup_views_mac.mm
+++ chrome/browser/ui/cocoa/extensions/extension_popup_views_mac.mm
@@ -39,7 +39,7 @@ ExtensionPopupViewsMac* ExtensionPopupViewsMac::ShowPopup(
                                    object:parent_window
                                     queue:nil
                                usingBlock:^(NSNotification* notification) {
-                                 popup->OnAnchorWindowActivation();
+                                 popup->CloseUnlessUnderInspection();
                                }];
   [popup->observer_tokens_ addObject:token];
   return popup;
