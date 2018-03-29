27d3c261b82577da7cc447c6dd6ae8f8b84016c6
Bo Liu <boliu@chromium.org>
2018-03-29 06:48:16 +0000

    sync compositor: Version CompositorFrameMetadata
    CompositorFrameMetadata can arrive on the UI thread out of order:
    Metadata is sent from compositor to UI in response to synchronous or
    asynchronous draws. The synchronous draw replies is processed
    immediately, even if there are replies to asynchronous draws pending. So
    when there is a mix of sync and async draws, it's possible for metadata
    to arrive out of order.
    
    This has been a theoretical problem for a long time, but only became
    real problem when FrameSwapMessageQueue was expecting metadata to be
    ordered. The only practical fix is to simply drop out of order metadata
    instead, which FrameSwapMessageQueue is designed to handle already.
    
    Bug: 821706
    Change-Id: I493862ba6d0838097f26e824359f03017eaa2a35
    Reviewed-on: https://chromium-review.googlesource.com/985096
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546750}

--- content/browser/android/synchronous_compositor_sync_call_bridge.cc
+++ content/browser/android/synchronous_compositor_sync_call_bridge.cc
@@ -61,6 +61,7 @@ void SynchronousCompositorSyncCallBridge::RemoteClosedOnIOThread() {
 
 bool SynchronousCompositorSyncCallBridge::ReceiveFrameOnIOThread(
     int layer_tree_frame_sink_id,
+    uint32_t metadata_version,
     base::Optional<viz::CompositorFrame> compositor_frame) {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
   base::AutoLock lock(lock_);
@@ -78,7 +79,8 @@ bool SynchronousCompositorSyncCallBridge::ReceiveFrameOnIOThread(
         BrowserThread::UI, FROM_HERE,
         base::BindOnce(&SynchronousCompositorSyncCallBridge::
                            ProcessFrameMetadataOnUIThread,
-                       this, compositor_frame->metadata.Clone()));
+                       this, metadata_version,
+                       compositor_frame->metadata.Clone()));
     frame_ptr->frame.reset(new viz::CompositorFrame);
     *frame_ptr->frame = std::move(*compositor_frame);
   }
@@ -187,10 +189,11 @@ void SynchronousCompositorSyncCallBridge::VSyncCompleteOnUIThread() {
 }
 
 void SynchronousCompositorSyncCallBridge::ProcessFrameMetadataOnUIThread(
+    uint32_t metadata_version,
     viz::CompositorFrameMetadata metadata) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
   if (host_)
-    host_->UpdateFrameMetaData(std::move(metadata));
+    host_->UpdateFrameMetaData(metadata_version, std::move(metadata));
 }
 
 void SynchronousCompositorSyncCallBridge::UnregisterSyncCallBridgeOnIOThread(
