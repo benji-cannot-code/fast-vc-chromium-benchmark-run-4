5d0d9114ff5dff6a02d1df40cdb66ebb086e8909
Pavol Marko <pmarko@chromium.org>
2018-03-29 15:18:51 +0000

    Fix ODR-use of cloud_policy.pb.cc
    The targets chrome_settings_proto_generated_compile and
    cloud_policy_proto_generated_compile both compiled cloud_policy.pb.cc,
    leading to ODR errors (revealed using ASAN).
    So the situation was:
    x -+-> chrome_settings_proto_generated_compile -> cloud_policy.pb.cc
       |
       +-> cloud_policy_proto_generated_compile -> cloud_policy.pb.cc
    
    Make chrome_settings_proto_generated_compile depend on the
    cloud_policy_proto_generated_compile component instead.
    The new situation is:
    x -+-> chrome_settings_proto_generated_compile
       |           |
       |           v
       +-> cloud_policy_proto_generated_compile -> cloud_policy.pb.cc
    
    IIUC gn will make sure to only build each component once in a component
    build.
    
    Additionally, change the export macro for
    chrome_settings_proto_generated_compile, so the two components use
    different export macros.
    If this was not done, linking chrome_settings_proto_generated_compile
    fails because it attempts to export
    cloud_policy_proto_generated_compile's symbols instead of importing
    them.
    
    BUG=808413
    TEST=compile with is_asan=true,is_debug=true,is_component_build=true,
         run browser_tests
    
    Change-Id: I0c933d89ec79f73ca2a995d012882eab1471c569
    Reviewed-on: https://chromium-review.googlesource.com/918761
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546840}

--- components/policy/proto/policy_proto_export.h
+++ components/policy/proto/policy_proto_export.h
@@ -16,6 +16,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define POLICY_PROTO_EXPORT __declspec(dllimport)
 #endif  // defined(POLICY_PROTO_COMPILATION)
 
+#if defined(POLICY_CHROME_SETTINGS_PROTO_COMPILATION)
+#define POLICY_CHROME_SETTINGS_PROTO_EXPORT __declspec(dllexport)
+#else
+#define POLICY_CHROME_SETTINGS_PROTO_EXPORT __declspec(dllimport)
+#endif  // defined(POLICY_PROTO_COMPILATION)
+
 #else  // defined(WIN32)
 
 #if defined(POLICY_PROTO_COMPILATION)
@@ -24,11 +30,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define POLICY_PROTO_EXPORT
 #endif  // defined(POLICY_PROTO_COMPILATION)
 
+#if defined(POLICY_CHROME_SETTINGS_PROTO_COMPILATION)
+#define POLICY_CHROME_SETTINGS_PROTO_EXPORT \
+  __attribute__((visibility("default")))
+#else
+#define POLICY_CHROME_SETTINGS_PROTO_EXPORT
+#endif  // defined(POLICY_PROTO_COMPILATION)
+
 #endif  // defined(WIN32)
 
 #else  // defined(COMPONENT_BUILD)
 
 #define POLICY_PROTO_EXPORT
+#define POLICY_CHROME_SETTINGS_PROTO_EXPORT
 
 #endif  // defined(COMPONENT_BUILD)
 
