56dc0ed37474109f58a2768bb83346f1549ee475
Sammie Quon <sammiequon@google.com>
2018-03-29 01:26:30 +0000

    splitview: Properly show splitview indicators when rotated.
    After rotate, SplitViewIndicatorView::Layout gets called, but width()
    and height() return the wrong values. Set the widget to the correct
    bounds on rotation, which will then call Layout with the correct width()
    and height().
    
    Test: manual
    Bug: 826958
    Change-Id: Ibc243ab676fba7503370a38fe7bfb3e4edd25e4d
    Reviewed-on: https://chromium-review.googlesource.com/985497
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546691}

--- ash/wm/splitview/split_view_drag_indicators.cc
+++ ash/wm/splitview/split_view_drag_indicators.cc
@@ -572,7 +572,11 @@ void SplitViewDragIndicators::SetIndicatorState(
 }
 
 void SplitViewDragIndicators::OnDisplayBoundsChanged() {
-  indicators_view_->Layout();
+  aura::Window* root_window = widget_->GetNativeView()->GetRootWindow();
+  gfx::Rect bounds = screen_util::GetDisplayWorkAreaBoundsInParent(
+      root_window->GetChildById(kShellWindowId_OverlayContainer));
+  ::wm::ConvertRectToScreen(root_window, &bounds);
+  widget_->SetBounds(bounds);
 }
 
 bool SplitViewDragIndicators::GetIndicatorTypeVisibilityForTesting(
