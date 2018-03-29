d6291e2c80a56fe5498c31c617c5c3df89f357c9
Istiaque Ahmed <lazyboy@chromium.org>
2018-03-29 18:39:48 +0000

    Cleanup: Make alarms api use OnceCallbacks.
    All these callbacks are expected to called exactly once, so make
    these OnceCallbacks instead of RepeatingCallbacks.
    
    Bug: None
    Change-Id: If68e856eb28d8a5a2b5eec2ac0cd0beebe516fe7
    Reviewed-on: https://chromium-review.googlesource.com/984823
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546878}

--- extensions/browser/api/alarms/alarms_api.cc
+++ extensions/browser/api/alarms/alarms_api.cc
@@ -115,7 +115,7 @@ bool AlarmsCreateFunction::RunAsync() {
       new Alarm(alarm_name, params->alarm_info, granularity, clock_->Now()));
   AlarmManager::Get(browser_context())
       ->AddAlarm(extension_id(), std::move(alarm),
-                 base::Bind(&AlarmsCreateFunction::Callback, this));
+                 base::BindOnce(&AlarmsCreateFunction::Callback, this));
 
   return true;
 }
@@ -132,7 +132,7 @@ bool AlarmsGetFunction::RunAsync() {
   std::string name = params->name.get() ? *params->name : kDefaultAlarmName;
   AlarmManager::Get(browser_context())
       ->GetAlarm(extension_id(), name,
-                 base::Bind(&AlarmsGetFunction::Callback, this, name));
+                 base::BindOnce(&AlarmsGetFunction::Callback, this, name));
 
   return true;
 }
@@ -148,7 +148,7 @@ void AlarmsGetFunction::Callback(const std::string& name,
 bool AlarmsGetAllFunction::RunAsync() {
   AlarmManager::Get(browser_context())
       ->GetAllAlarms(extension_id(),
-                     base::Bind(&AlarmsGetAllFunction::Callback, this));
+                     base::BindOnce(&AlarmsGetAllFunction::Callback, this));
   return true;
 }
 
@@ -170,7 +170,7 @@ bool AlarmsClearFunction::RunAsync() {
   std::string name = params->name.get() ? *params->name : kDefaultAlarmName;
   AlarmManager::Get(browser_context())
       ->RemoveAlarm(extension_id(), name,
-                    base::Bind(&AlarmsClearFunction::Callback, this, name));
+                    base::BindOnce(&AlarmsClearFunction::Callback, this, name));
 
   return true;
 }
@@ -182,8 +182,9 @@ void AlarmsClearFunction::Callback(const std::string& name, bool success) {
 
 bool AlarmsClearAllFunction::RunAsync() {
   AlarmManager::Get(browser_context())
-      ->RemoveAllAlarms(extension_id(),
-                        base::Bind(&AlarmsClearAllFunction::Callback, this));
+      ->RemoveAllAlarms(
+          extension_id(),
+          base::BindOnce(&AlarmsClearAllFunction::Callback, this));
   return true;
 }
 
