22f2b6cc11c23352837991f64fabdc2f5864009f
AJITH KUMAR V <ajith.v@samsung.com>
2018-03-29 11:26:17 +0000

    [Stylus Text Selection] Interpretting Stylus Secondary button as HANDLE_DRAG
    Currently long press drag is treating selection drag as HANDLE_DRAG and
    appropriately handling selection popup state. This was missing in case of
    stylus drag selection. Hence flickering observed with selection popup
    when we do stylus text selection. Now notifying stylus selection as
    HANDLE_DRAG for appropriately handling the selection popup UI logic
    
    Bug: 826297
    Change-Id: I17aec4a725dd7eab98db322b31574b347b6b0d9d
    Reviewed-on: https://chromium-review.googlesource.com/981734
    Reviewed-by: Pedro Amaral <amaralp@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: AJITH KUMAR V <ajith.v@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#546789}

--- content/browser/renderer_host/render_widget_host_view_android.cc
+++ content/browser/renderer_host/render_widget_host_view_android.cc
@@ -2130,6 +2130,9 @@ void RenderWidgetHostViewAndroid::OnStylusSelectBegin(float x0,
                                                       float x1,
                                                       float y1) {
   SetTextHandlesHiddenForStylus(true);
+  // TODO(ajith.v) Refactor the event names as this is not really handle drag,
+  // but currently we use same for long press drag selection as well.
+  OnSelectionEvent(ui::SELECTION_HANDLE_DRAG_STARTED);
   SelectBetweenCoordinates(gfx::PointF(x0, y0), gfx::PointF(x1, y1));
 }
 
@@ -2139,7 +2142,9 @@ void RenderWidgetHostViewAndroid::OnStylusSelectUpdate(float x, float y) {
 
 void RenderWidgetHostViewAndroid::OnStylusSelectEnd(float x, float y) {
   SetTextHandlesHiddenForStylus(false);
-  ShowContextMenuAtPoint(gfx::Point(x, y), ui::MENU_SOURCE_STYLUS);
+  // TODO(ajith.v) Refactor the event names as this is not really handle drag,
+  // but currently we use same for long press drag selection as well.
+  OnSelectionEvent(ui::SELECTION_HANDLE_DRAG_STOPPED);
 }
 
 void RenderWidgetHostViewAndroid::OnStylusSelectTap(base::TimeTicks time,
