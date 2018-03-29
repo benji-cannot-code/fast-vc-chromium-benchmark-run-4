3c2b747e3813579247d789d4c7e0bb31ff2f5b38
Bernhard Bauer <bauerb@chromium.org>
2018-03-29 13:00:54 +0000

    Check for correct security icon on tablets in TrustedCdnPublisherUrlTest
    Bug: 814365, 827124
    Change-Id: If276cbf5e13e0e245d007920f49aa82007676431
    Reviewed-on: https://chromium-review.googlesource.com/986253
    Reviewed-by: Tobias Sargeant <tobiasjs@chromium.org>
    Commit-Queue: Bernhard Bauer <bauerb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546798}

--- chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/TrustedCdnPublisherUrlTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/TrustedCdnPublisherUrlTest.java
@@ -46,6 +46,7 @@ import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.util.browser.Features;
 import org.chromium.components.url_formatter.UrlFormatter;
 import org.chromium.net.test.util.TestWebServer;
+import org.chromium.ui.base.DeviceFormFactor;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -151,13 +152,25 @@ public class TrustedCdnPublisherUrlTest {
         mScreenShooter.shoot("trustedPublisherUrlRtl");
     }
 
+    private int getDefaultSecurityIcon() {
+        // On tablets an info icon is shown for ConnectionSecurityLevel.NONE pages,
+        // on smaller form factors nothing.
+        if (DeviceFormFactor.isNonMultiDisplayContextOnTablet(
+                    InstrumentationRegistry.getTargetContext())) {
+            return org.chromium.chrome.R.drawable.omnibox_info;
+        }
+
+        return 0;
+    }
+
     @Test
     @SmallTest
     @Feature({"UiCatalogue"})
     @Features.EnableFeatures(ChromeFeatureList.SHOW_TRUSTED_PUBLISHER_URL)
     @OverrideTrustedCdn
     public void testUntrustedClient() throws Exception {
-        runTrustedCdnPublisherUrlTest("https://example.com/test", "com.someoneelse.bla", null, 0);
+        runTrustedCdnPublisherUrlTest(
+                "https://example.com/test", "com.someoneelse.bla", null, getDefaultSecurityIcon());
     }
 
     @Test
@@ -166,7 +179,7 @@ public class TrustedCdnPublisherUrlTest {
     @Features.EnableFeatures(ChromeFeatureList.SHOW_TRUSTED_PUBLISHER_URL)
     @OverrideTrustedCdn
     public void testNoHeader() throws Exception {
-        runTrustedCdnPublisherUrlTest(null, "com.example.test", null, 0);
+        runTrustedCdnPublisherUrlTest(null, "com.example.test", null, getDefaultSecurityIcon());
     }
 
     @Test
@@ -175,7 +188,8 @@ public class TrustedCdnPublisherUrlTest {
     @Features.EnableFeatures(ChromeFeatureList.SHOW_TRUSTED_PUBLISHER_URL)
     @OverrideTrustedCdn
     public void testMalformedHeader() throws Exception {
-        runTrustedCdnPublisherUrlTest("garbage", "com.example.test", null, 0);
+        runTrustedCdnPublisherUrlTest(
+                "garbage", "com.example.test", null, getDefaultSecurityIcon());
     }
 
     @Test
@@ -184,7 +198,8 @@ public class TrustedCdnPublisherUrlTest {
     @Features.DisableFeatures(ChromeFeatureList.SHOW_TRUSTED_PUBLISHER_URL)
     @OverrideTrustedCdn
     public void testDisabled() throws Exception {
-        runTrustedCdnPublisherUrlTest("https://example.com/test", "com.example.test", null, 0);
+        runTrustedCdnPublisherUrlTest(
+                "https://example.com/test", "com.example.test", null, getDefaultSecurityIcon());
     }
 
     @Test
@@ -193,7 +208,8 @@ public class TrustedCdnPublisherUrlTest {
     @Features.EnableFeatures(ChromeFeatureList.SHOW_TRUSTED_PUBLISHER_URL)
     // No @OverrideTrustedCdn
     public void testUntrustedCdn() throws Exception {
-        runTrustedCdnPublisherUrlTest("https://example.com/test", "com.example.test", null, 0);
+        runTrustedCdnPublisherUrlTest(
+                "https://example.com/test", "com.example.test", null, getDefaultSecurityIcon());
     }
     // TODO(bauerb): Test an insecure HTTPS connection.
 
