84531001a534679bcd40aa986074199b07c7fcda
yiyix <yiyix@chromium.org>
2018-03-29 02:22:12 +0000

    VIZ: use |root_background_color| from RFM in RWHVMac
    RenderWidgetHostViewMac are used to sniff the |root_background_color|
    in SubmitCompositorFrame. After this change, both RWHVMac listen to
    RenderFrameMetadataProvider and update its
    |last_frame_root_background_color_|
    
    Bug:716911
    
    Change-Id: Iafa466919c9d29185a59994ca74206fa205335f8
    Reviewed-on: https://chromium-review.googlesource.com/982576
    Reviewed-by: Fady Samuel <fsamuel@chromium.org>
    Commit-Queue: Yi Xu <yiyix@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546703}

--- content/browser/renderer_host/render_widget_host_view_mac.mm
+++ content/browser/renderer_host/render_widget_host_view_mac.mm
@@ -644,6 +644,14 @@ void RenderWidgetHostViewMac::OnTextSelectionChanged(
   }
 }
 
+void RenderWidgetHostViewMac::OnRenderFrameMetadataChanged() {
+  last_frame_root_background_color_ = host()
+                                          ->render_frame_metadata_provider()
+                                          ->LastRenderFrameMetadata()
+                                          .root_background_color;
+  RenderWidgetHostViewBase::OnRenderFrameMetadataChanged();
+}
+
 void RenderWidgetHostViewMac::RenderProcessGone(base::TerminationStatus status,
                                                 int error_code) {
   Destroy();
@@ -1049,8 +1057,6 @@ void RenderWidgetHostViewMac::SubmitCompositorFrame(
     viz::CompositorFrame frame,
     viz::mojom::HitTestRegionListPtr hit_test_region_list) {
   TRACE_EVENT0("browser", "RenderWidgetHostViewMac::OnSwapCompositorFrame");
-
-  last_frame_root_background_color_ = frame.metadata.root_background_color;
 
   page_at_minimum_scale_ =
       frame.metadata.page_scale_factor == frame.metadata.min_page_scale_factor;
