2529f5969ffd83cf0354709d8d7a36c1541f6dff
Sebastien Seguin-Gagnon <sebsg@chromium.org>
2018-03-30 15:49:10 +0000

    Revert "Implements a custom slider for display size setting"
    This reverts commit 8926b625fd56c9b3f418bc41843f6e4b5711baba.
    
    Reason for revert: Possible culprit for the failing of
    CrSettingsDevicePageTest.DisplayTest
    CrSettingsDevicePageTest.KeyboardTest
    CrSettingsDevicePageTest.PowerTest
    CrSettingsDisplaySizeSliderTest.All
    CrSettingsDevicePageTest.DevicePageTest
    CrSettingsDevicePageTest.PointersTest
    CrSettingsDevicePageTest.StylusTest
    
    on the bots.
    
    First build with errors:https://uberchromegw.corp.google.com/i/chromium.chromiumos/builders/linux-chromeos-dbg/builds/4954
    
    9 builds failed in a row with those errors.
    
    Original change's description:
    > Implements a custom slider for display size setting
    > 
    > This patch implements a custom slider that provides the basic
    > functionality of the paper-slider but provides an additional option to
    > display a label for the current slider value. The slider has the Prefs
    > Behavior integrated with it.
    > 
    > This slider has a general implemention and is ready to be used in other
    > locations as well.
    > 
    > Functionalities implemented:
    >  - Slider works the same way as the paper-slider in terms of mouse and
    >    keyboard interactions.
    >  - Additionally The slider displays a label on mouse hover and keyboard
    >    focus.
    >  - The slider only works for discrete values or options.
    > 
    > This patch also adds web ui tests for all the slider functionalities.
    > 
    > A video of this slider in use can be found on the bug link below.
    > 
    > Bug: 798906
    > Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    > Change-Id: I55fa98eb438ed1c48b4f40c14d8c05085e7a45b4
    > Component: Display Slider, display settings, webui tests
    > Reviewed-on: https://chromium-review.googlesource.com/985127
    > Commit-Queue: Malay Keshav <malaykeshav@chromium.org>
    > Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#547101}
    
    TBR=stevenjb@chromium.org,malaykeshav@chromium.org
    
    Change-Id: I456977e7b8358e6210fc72baf66bc692514f6f27
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 798906
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Reviewed-on: https://chromium-review.googlesource.com/988092
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547172}

--- chrome/browser/resources/settings/device_page/display.js
+++ chrome/browser/resources/settings/device_page/display.js
@@ -103,7 +103,7 @@ Polymer({
     /** @private {!Array<number>} Mode index values for slider. */
     modeValues_: Array,
 
-    /** @private {SliderTicks} Display zoom slider tick values. */
+    /** @private {!Array<number>} Display zoom percentage values for slider */
     zoomValues_: Array,
 
     /** @private {!DropdownMenuOptionList} */
@@ -184,7 +184,6 @@ Polymer({
     'updateNightLightScheduleSettings_(prefs.ash.night_light.schedule_type.*,' +
         ' prefs.ash.night_light.enabled.*)',
     'onSelectedModeChange_(selectedModePref_.value)',
-    'onSelectedZoomChange_(selectedZoomPref_.value)',
   ],
 
   /** @private {number} Selected mode index received from chrome. */
@@ -317,35 +316,32 @@ Polymer({
    */
   getSelectedDisplayZoom_: function(selectedDisplay) {
     const selectedZoom = selectedDisplay.displayZoomFactor * 100;
-    let closestMatch = this.zoomValues_[0].value;
+    let closestMatch = this.zoomValues_[0];
     let minimumDiff = Math.abs(closestMatch - selectedZoom);
 
     for (let i = 0; i < this.zoomValues_.length; i++) {
-      const currentDiff = Math.abs(this.zoomValues_[i].value - selectedZoom);
+      const currentDiff = Math.abs(this.zoomValues_[i] - selectedZoom);
       if (currentDiff < minimumDiff) {
-        closestMatch = this.zoomValues_[i].value;
+        closestMatch = this.zoomValues_[i];
         minimumDiff = currentDiff;
       }
     }
 
-    return /** @type {number} */ (closestMatch);
+    return closestMatch;
   },
 
   /**
    * Given the display with the current display mode, this function lists all
-   * the display zoom values and their labels to be used by the slider.
+   * the display zoom values in percentage.
    * @param {!chrome.system.display.DisplayUnitInfo} selectedDisplay
-   * @return {SliderTicks}
+   * @return {!Array<number>}
    */
   getZoomValues_: function(selectedDisplay) {
-    /** @type {SliderTicks} */
     let zoomValues = [];
     for (let i = 0; i < selectedDisplay.availableDisplayZoomFactors.length;
          i++) {
-      const value =
-          Math.round(selectedDisplay.availableDisplayZoomFactors[i] * 100);
-      const label = this.i18n('displayZoomValue', value.toString());
-      zoomValues.push({value: value, label: label});
+      zoomValues.push(
+          Math.round(selectedDisplay.availableDisplayZoomFactors[i] * 100));
     }
     return zoomValues;
   },
@@ -570,6 +566,16 @@ Polymer({
   },
 
   /**
+   * @param {!chrome.system.display.DisplayUnitInfo} selectedDisplay
+   * @return {string}
+   * @private
+   */
+  getDisplayZoomText_: function(selectedDisplay) {
+    return this.i18n(
+        'displayZoomValue', this.selectedZoomPref_.value.toString());
+  },
+
+  /**
    * @param {!{detail: string}} e |e.detail| is the id of the selected display.
    * @private
    */
@@ -658,14 +664,10 @@ Polymer({
    * @private
    */
   onSelectedZoomChange_: function() {
-    if (this.currentSelectedModeIndex_ == -1 || !this.selectedDisplay)
-      return;
-
     /** @type {!chrome.system.display.DisplayProperties} */ const properties = {
       displayZoomFactor:
           /** @type {number} */ (this.selectedZoomPref_.value) / 100.0
     };
-
     settings.display.systemDisplayApi.setDisplayProperties(
         this.selectedDisplay.id, properties,
         this.setPropertiesCallback_.bind(this));
