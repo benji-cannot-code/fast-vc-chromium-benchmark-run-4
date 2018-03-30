8a8d48c38542abede1d210cf1cdca0807eba4799
John Rummell <jrummell@chromium.org>
2018-03-30 16:43:58 +0000

    Remove content decryptor pepper APIs
    Now that CDMs use mojo exclusively, remove the Pepper APIs that are
    now unused.
    
    BUG=772160
    TEST=compiles
    
    Change-Id: Ifa77be717e7b11ab20ae8053901dcb663b1726cc
    Reviewed-on: https://chromium-review.googlesource.com/981125
    Commit-Queue: John Rummell <jrummell@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Bill Budge <bbudge@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547187}

--- ppapi/proxy/interface_list.cc
+++ ppapi/proxy/interface_list.cc
@@ -77,7 +77,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/c/ppp_instance.h"
 #include "ppapi/c/private/ppb_camera_capabilities_private.h"
 #include "ppapi/c/private/ppb_camera_device_private.h"
-#include "ppapi/c/private/ppb_content_decryptor_private.h"
 #include "ppapi/c/private/ppb_ext_crx_file_system_private.h"
 #include "ppapi/c/private/ppb_file_io_private.h"
 #include "ppapi/c/private/ppb_file_ref_private.h"
@@ -106,7 +105,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/c/private/ppb_video_destination_private.h"
 #include "ppapi/c/private/ppb_video_source_private.h"
 #include "ppapi/c/private/ppb_x509_certificate_private.h"
-#include "ppapi/c/private/ppp_content_decryptor_private.h"
 #include "ppapi/c/trusted/ppb_broker_trusted.h"
 #include "ppapi/c/trusted/ppb_browser_font_trusted.h"
 #include "ppapi/c/trusted/ppb_char_set_trusted.h"
@@ -129,7 +127,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/proxy/ppb_video_decoder_proxy.h"
 #include "ppapi/proxy/ppb_x509_certificate_private_proxy.h"
 #include "ppapi/proxy/ppp_class_proxy.h"
-#include "ppapi/proxy/ppp_content_decryptor_private_proxy.h"
 #include "ppapi/proxy/ppp_find_proxy.h"
 #include "ppapi/proxy/ppp_graphics_3d_proxy.h"
 #include "ppapi/proxy/ppp_input_event_proxy.h"
@@ -277,13 +274,6 @@ InterfaceList::InterfaceList() {
   AddProxy(API_ID_PPB_VAR_DEPRECATED, &ProxyFactory<PPB_Var_Deprecated_Proxy>);
   AddPPB(PPB_VAR_DEPRECATED_INTERFACE,
          PPB_Var_Deprecated_Proxy::GetProxyInterface(), PERMISSION_FLASH);
-
-  // TODO(tomfinegan): Figure out where to put these once we refactor things
-  // to load the PPP interface struct from the PPB interface.
-  AddProxy(API_ID_PPP_CONTENT_DECRYPTOR_PRIVATE,
-           &ProxyFactory<PPP_ContentDecryptor_Private_Proxy>);
-  AddPPP(PPP_CONTENTDECRYPTOR_PRIVATE_INTERFACE,
-         PPP_ContentDecryptor_Private_Proxy::GetProxyInterface());
 #endif
   AddProxy(API_ID_PPB_TESTING, &ProxyFactory<PPB_Testing_Proxy>);
   AddPPB(PPB_TESTING_PRIVATE_INTERFACE,
