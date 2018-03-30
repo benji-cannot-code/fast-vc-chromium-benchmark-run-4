6f01c24122313938f947cfb0a70a4495cbdae687
Tetsui Ohkubo <tetsui@chromium.org>
2018-03-30 18:33:29 +0000

    Generate screen share notifications outside tray.
    Previously, ScreenCapture/ScreenShare notifications are generated from
    ScreenCaptureTrayItem and ScreenShareTrayItem, which are
    SystemTrayItems.
    
    We're going to get rid of SystemTrayItems as UnifiedSystemTray will
    replace them, so we should separate notification generation logic from
    ScreenTrayItem to ScreenSecurityNotificationController. It also makes
    the structure clean, as the logic that is not directly related to the
    system tray view is separated.
    
    TEST=ScreenSecurityNotificationControllerTest
    BUG=824631
    
    Change-Id: I4eb7f46a344b5b288740dbceaba6ca327c850742
    Reviewed-on: https://chromium-review.googlesource.com/977781
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547216}

--- ash/shell.cc
+++ ash/shell.cc
@@ -99,6 +99,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/power/power_status.h"
 #include "ash/system/power/video_activity_notifier.h"
 #include "ash/system/screen_layout_observer.h"
+#include "ash/system/screen_security/screen_security_notification_controller.h"
 #include "ash/system/session/logout_button_tray.h"
 #include "ash/system/session/logout_confirmation_controller.h"
 #include "ash/system/status_area_widget.h"
@@ -793,6 +794,7 @@ Shell::~Shell() {
   screen_pinning_controller_.reset();
 
   resolution_notification_controller_.reset();
+  screen_security_notification_controller_.reset();
   screenshot_controller_.reset();
   mouse_cursor_filter_.reset();
   modality_filter_.reset();
@@ -905,6 +907,8 @@ void Shell::Init(ui::ContextFactory* context_factory,
   detachable_base_notification_controller_ =
       std::make_unique<DetachableBaseNotificationController>(
           detachable_base_handler_.get());
+  screen_security_notification_controller_ =
+      std::make_unique<ScreenSecurityNotificationController>();
 
   // Connector can be null in tests.
   if (shell_delegate_->GetShellConnector()) {
--- ash/system/screen_security/screen_tray_item.h
+++ ash/system/screen_security/screen_tray_item.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/tray/system_tray_item.h"
 #include "ash/system/tray/tray_item_view.h"
 #include "base/macros.h"
-#include "ui/message_center/public/cpp/notification_delegate.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/view.h"
 
@@ -68,22 +67,6 @@ class ScreenStatusView : public views::View, public views::ButtonListener {
   DISALLOW_COPY_AND_ASSIGN(ScreenStatusView);
 };
 
-class ScreenNotificationDelegate : public message_center::NotificationDelegate {
- public:
-  explicit ScreenNotificationDelegate(ScreenTrayItem* screen_tray);
-
-  // message_center::NotificationDelegate overrides:
-  void ButtonClick(int button_index) override;
-
- protected:
-  ~ScreenNotificationDelegate() override;
-
- private:
-  ScreenTrayItem* screen_tray_;
-
-  DISALLOW_COPY_AND_ASSIGN(ScreenNotificationDelegate);
-};
-
 }  // namespace tray
 
 // The base tray item for screen capture and screen sharing. The
@@ -102,23 +85,15 @@ class ASH_EXPORT ScreenTrayItem : public SystemTrayItem {
   }
 
   bool is_started() const { return is_started_; }
-  void set_is_started(bool is_started) { is_started_ = is_started; }
+
+  void SetIsStarted(bool is_started);
 
   void Update();
-  void Start(const base::Closure& stop_callback);
+  void Start(base::OnceClosure stop_callback);
   void Stop();
-
-  // Creates or updates the notification for the tray item.
-  virtual void CreateOrUpdateNotification() = 0;
-
-  // Returns the id of the notification for the tray item.
-  virtual std::string GetNotificationId() = 0;
 
   // Called after Stop() is invoked from the default view.
   virtual void RecordStoppedFromDefaultViewMetric() = 0;
-
-  // Called after Stop() is invoked from the notification view.
-  virtual void RecordStoppedFromNotificationViewMetric() = 0;
 
   // Overridden from SystemTrayItem.
   views::View* CreateTrayView(LoginStatus status) override;
@@ -127,10 +102,10 @@ class ASH_EXPORT ScreenTrayItem : public SystemTrayItem {
   void OnDefaultViewDestroyed() override;
 
  private:
+  base::OnceClosure stop_callback_;
   tray::ScreenTrayView* tray_view_;
   tray::ScreenStatusView* default_view_;
   bool is_started_;
-  base::Closure stop_callback_;
 
   DISALLOW_COPY_AND_ASSIGN(ScreenTrayItem);
 };
