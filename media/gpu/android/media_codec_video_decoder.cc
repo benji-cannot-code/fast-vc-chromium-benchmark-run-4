abdde973635e80a06755090059392cbe13c6840a
liberato@chromium.org <liberato@chromium.org>
2018-03-30 19:34:07 +0000

    Make MCVD / D3D11 VideoDecoders return POWER_EFFICIENT.
    For media capabilities, we assume that all frames from MCVD / D3D11
    are decoded using power-efficient decoders.  While this is not
    strictly true, it's a reasonable first approximation.
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I5b5ce2096bbc48ac8c78f1c512c86c4e86dbdfcd
    Reviewed-on: https://chromium-review.googlesource.com/986646
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547242}

--- media/gpu/android/media_codec_video_decoder.cc
+++ media/gpu/android/media_codec_video_decoder.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/media_switches.h"
 #include "media/base/video_codecs.h"
 #include "media/base/video_decoder_config.h"
+#include "media/base/video_frame.h"
 #include "media/gpu/android/android_video_surface_chooser.h"
 #include "media/gpu/android/avda_codec_allocator.h"
 
@@ -670,8 +671,12 @@ void MediaCodecVideoDecoder::RunEosDecodeCb(int reset_generation) {
 void MediaCodecVideoDecoder::ForwardVideoFrame(
     int reset_generation,
     const scoped_refptr<VideoFrame>& frame) {
-  if (reset_generation == reset_generation_)
+  if (reset_generation == reset_generation_) {
+    // TODO(liberato): We might actually have a SW decoder.  Consider setting
+    // this to false if so, especially for higher bitrates.
+    frame->metadata()->SetBoolean(VideoFrameMetadata::POWER_EFFICIENT, true);
     output_cb_.Run(frame);
+  }
 }
 
 // Our Reset() provides a slightly stronger guarantee than VideoDecoder does.
