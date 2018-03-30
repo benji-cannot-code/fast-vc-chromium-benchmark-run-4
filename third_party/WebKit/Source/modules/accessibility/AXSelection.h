0ec0633800b4bb8f231e5d5b862a65514e82a373
Nektarios Paisios <nektar@chromium.org>
2018-03-30 22:50:22 +0000

    Implements selection behavior that determines how AXRange is mapped to a DOM range
    Still preliminary until patches implementing tree ordering operators on AXObject and support for aria-labels and alt text positions are in.
    R=dmazzoni@chromium.org
    
    Change-Id: Ia19131b53d8a20d299a431695641b11ddc805280
    Reviewed-on: https://chromium-review.googlesource.com/954182
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547299}

--- third_party/WebKit/Source/modules/accessibility/AXSelection.h
+++ third_party/WebKit/Source/modules/accessibility/AXSelection.h
@@ -17,6 +17,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+// If the |AXSelection| is defined by endpoints that are present in the
+// accessibility tree but not in the DOM tree, determines whether setting the
+// selection will shrink or extend the |AXSelection| to encompass endpoints that
+// are in the DOM.
+enum class AXSelectionBehavior {
+  kShrinkToValidDOMRange,
+  kExtendToValidDOMRange
+};
+
 class MODULES_EXPORT AXSelection final {
   DISALLOW_NEW_EXCEPT_PLACEMENT_NEW();
 
@@ -34,10 +43,13 @@ class MODULES_EXPORT AXSelection final {
   // invalid, or if the positions are in two separate documents.
   bool IsValid() const;
 
-  const SelectionInDOMTree AsSelection() const;
+  const SelectionInDOMTree AsSelection(
+      const AXSelectionBehavior =
+          AXSelectionBehavior::kExtendToValidDOMRange) const;
 
   // Tries to set the DOM selection to this.
-  void Select();
+  void Select(
+      const AXSelectionBehavior = AXSelectionBehavior::kExtendToValidDOMRange);
 
  private:
   AXSelection();
