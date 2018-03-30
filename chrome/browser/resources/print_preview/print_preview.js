6882731230bf70478c3ee38a77c1264f5c0c9a02
rbpotter <rbpotter@chromium.org>
2018-03-30 17:09:32 +0000

    GCP Error message: Fix bug with destination changes
    If the user selects some valid printer (A) and then selects an
    unsupported printer (B), then changes back to (A), the error message
    in the preview area does not disappear, because the PreviewGenerator
    does not detect that the print ticket has changed. Hide the message
    and display the previously generated preview if this occurs.
    
    Tests for these issues added at https://crrev.com/c/987494.
    
    Bug: 792093
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I6cd08f39d821e6ae970f230b2cc0a07008db66a4
    Reviewed-on: https://chromium-review.googlesource.com/986719
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547191}

--- chrome/browser/resources/print_preview/print_preview.js
+++ chrome/browser/resources/print_preview/print_preview.js
@@ -1027,6 +1027,7 @@ cr.define('print_preview', function() {
       this.uiState_ = PrintPreviewUiState_.ERROR;
       this.isPreviewGenerationInProgress_ = false;
       this.printHeader_.isPrintButtonEnabled = false;
+      this.previewArea_.setDestinationValid(false);
       this.updateLinks_();
     },
 
@@ -1325,6 +1326,7 @@ cr.define('print_preview', function() {
       if (this.uiState_ == PrintPreviewUiState_.ERROR) {
         this.uiState_ = PrintPreviewUiState_.READY;
         this.updateLinks_();
+        this.previewArea_.setDestinationValid(true);
       }
       if (this.destinationStore_.selectedDestination &&
           this.isInKioskAutoPrintMode_) {
