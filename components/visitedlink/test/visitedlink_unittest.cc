62a5bd81914c025e934feec0cf43bb2d2442aad8
Ken Rockot <rockot@chromium.org>
2018-03-30 16:40:09 +0000

    Convert visitedlink component to new shared memory API
    This includes moving mojom shared_buffer usage to the more
    strongly-typed base memory region types.
    
    Bug: 826213
    Change-Id: Ifb304267dc0c5a18a7a9df204b2b70020337b4e9
    Reviewed-on: https://chromium-review.googlesource.com/985420
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Ken Rockot <rockot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547186}

--- components/visitedlink/test/visitedlink_unittest.cc
+++ components/visitedlink/test/visitedlink_unittest.cc
@@ -122,12 +122,11 @@ class TrackingVisitedLinkEventListener : public VisitedLinkMaster::Listener {
         completely_reset_count_(0),
         add_count_(0) {}
 
-  void NewTable(mojo::SharedBufferHandle table) override {
-    if (table.is_valid()) {
+  void NewTable(base::ReadOnlySharedMemoryRegion* table_region) override {
+    if (table_region->IsValid()) {
       for (std::vector<VisitedLinkSlave>::size_type i = 0;
            i < g_slaves.size(); i++) {
-        g_slaves[i]->UpdateVisitedLinks(
-            table.Clone(mojo::SharedBufferHandle::AccessMode::READ_ONLY));
+        g_slaves[i]->UpdateVisitedLinks(table_region->Duplicate());
       }
     }
   }
@@ -207,8 +206,7 @@ class VisitedLinkTest : public testing::Test {
 
     // Create a slave database.
     VisitedLinkSlave slave;
-    slave.UpdateVisitedLinks(master_->shared_memory().Clone(
-        mojo::SharedBufferHandle::AccessMode::READ_ONLY));
+    slave.UpdateVisitedLinks(master_->mapped_table_memory().region.Duplicate());
     g_slaves.push_back(&slave);
 
     bool found;
@@ -336,8 +334,7 @@ TEST_F(VisitedLinkTest, DeleteAll) {
 
   {
     VisitedLinkSlave slave;
-    slave.UpdateVisitedLinks(master_->shared_memory().Clone(
-        mojo::SharedBufferHandle::AccessMode::READ_ONLY));
+    slave.UpdateVisitedLinks(master_->mapped_table_memory().region.Duplicate());
     g_slaves.push_back(&slave);
 
     // Add the test URLs.
@@ -381,8 +378,7 @@ TEST_F(VisitedLinkTest, Resizing) {
 
   // ...and a slave
   VisitedLinkSlave slave;
-  slave.UpdateVisitedLinks(master_->shared_memory().Clone(
-      mojo::SharedBufferHandle::AccessMode::READ_ONLY));
+  slave.UpdateVisitedLinks(master_->mapped_table_memory().region.Duplicate());
   g_slaves.push_back(&slave);
 
   int32_t used_count = master_->GetUsedCount();
@@ -559,7 +555,7 @@ class VisitCountingContext : public mojom::VisitedLinkNotificationSink {
   }
 
   void UpdateVisitedLinks(
-      mojo::ScopedSharedBufferHandle table_handle) override {
+      base::ReadOnlySharedMemoryRegion table_region) override {
     new_table_count_++;
     NotifyUpdate();
   }
