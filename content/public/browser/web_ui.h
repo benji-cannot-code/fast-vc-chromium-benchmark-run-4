e1959696bc3f906940ec5e5f1dd373f7904e1dbb
Daniel Cheng <dcheng@chromium.org>
2018-03-30 15:21:11 +0000

    Use StringPiece for WebUI arguments that are usually literals
    Constructs fewer strings and emits less code (~88KiB saved on an
    official Linux build).
    
    Change-Id: Ie97f4cd3ec991b4ba293735382d9f8bd2b9f3245
    Reviewed-on: https://chromium-review.googlesource.com/986504
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547162}

--- content/public/browser/web_ui.h
+++ content/public/browser/web_ui.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/strings/string16.h"
+#include "base/strings/string_piece.h"
 #include "content/common/content_export.h"
 #include "ui/base/page_transition_types.h"
 
@@ -73,7 +74,7 @@ class CONTENT_EXPORT WebUI {
   // Used by WebUIMessageHandlers. If the given message is already registered,
   // the call has no effect.
   using MessageCallback = base::RepeatingCallback<void(const base::ListValue*)>;
-  virtual void RegisterMessageCallback(const std::string& message,
+  virtual void RegisterMessageCallback(base::StringPiece message,
                                        const MessageCallback& callback) = 0;
 
   // This is only needed if an embedder overrides handling of a WebUIMessage and
