9914e3945f8a9db4f46460cb9487545c00439c94
Hector Carmona <hcarmona@chromium.org>
2018-03-30 05:26:21 +0000

    MD Extensions: Fix overly aggressive sanitation.
    Polymer already protects against XSS injection, we just need to
    combine the two strings.
    
    Bug: 822939
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I866d65419b22d63a1c5a460f6a30304e4a5d487a
    Reviewed-on: https://chromium-review.googlesource.com/981599
    Commit-Queue: Hector Carmona <hcarmona@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547113}

--- chrome/test/data/webui/extensions/cr_extensions_browsertest.js
+++ chrome/test/data/webui/extensions/cr_extensions_browsertest.js
@@ -193,6 +193,10 @@ TEST_F('CrExtensionsItemsTest', 'RemoveButton', function() {
   this.runMochaTest(extension_item_tests.TestNames.RemoveButton);
 });
 
+TEST_F('CrExtensionsItemsTest', 'HtmlInName', function() {
+  this.runMochaTest(extension_item_tests.TestNames.HtmlInName);
+});
+
 ////////////////////////////////////////////////////////////////////////////////
 // Extension Detail View Tests
 
