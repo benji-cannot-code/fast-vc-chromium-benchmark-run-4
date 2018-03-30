ff909db7f4462345fe62f202eb18bbcee504a55e
junweifu <junwei.fu@intel.com>
2018-03-30 02:37:39 +0000

    ShapeDetection: Move the implementation of AsyncOperation to the .cc file
    It's better to put the class definition in a header file and the implementation
    in a C++ source file. Then, the source file is made part of the project, meaning
    it is compiled separately.
    
    BUG=791371
    
    Cq-Include-Trybots: luci.chromium.try:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win10_chromium_x64_rel_ng
    Change-Id: Ic58d0c714b405ced8974aca22c3b20f7b479682f
    Reviewed-on: https://chromium-review.googlesource.com/982751
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Commit-Queue: Junwei Fu <junwei.fu@intel.com>
    Cr-Commit-Position: refs/heads/master@{#547078}

--- services/shape_detection/detection_utils_win.h
+++ services/shape_detection/detection_utils_win.h
@@ -9,14 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <windows.storage.streams.h>
 #include <wrl/client.h>
 #include <wrl/event.h>
-#include <wrl/implements.h>
-#include <utility>
 
-#include "base/bind.h"
 #include "base/callback.h"
-#include "base/logging.h"
 #include "base/macros.h"
-#include "base/sequenced_task_runner.h"
 
 class SkBitmap;
 
@@ -25,7 +20,6 @@ namespace shape_detection {
 namespace WRL = Microsoft::WRL;
 
 using ABI::Windows::Foundation::IAsyncOperation;
-using ABI::Windows::Foundation::IAsyncOperationCompletedHandler;
 using ABI::Windows::Graphics::Imaging::ISoftwareBitmapStatics;
 using ABI::Windows::Graphics::Imaging::ISoftwareBitmap;
 using ABI::Windows::Graphics::Imaging::BitmapPixelFormat;
@@ -47,32 +41,7 @@ class AsyncOperation {
   // Creates an AsyncOperation instance which sets |callback| to be called when
   // the asynchronous action completes.
   static HRESULT BeginAsyncOperation(Callback callback,
-                                     IAsyncOperationPtr async_op_ptr) {
-    auto instance =
-        new AsyncOperation<RuntimeType>(std::move(callback), async_op_ptr);
-
-    scoped_refptr<base::SequencedTaskRunner> task_runner =
-        base::SequencedTaskRunnerHandle::Get();
-
-    typedef WRL::Implements<WRL::RuntimeClassFlags<WRL::ClassicCom>,
-                            IAsyncOperationCompletedHandler<RuntimeType*>,
-                            WRL::FtmBase>
-        AsyncCallback;
-    auto async_callback = WRL::Callback<AsyncCallback>(
-        [instance, task_runner](IAsyncOperation<RuntimeType*>* async_op,
-                                AsyncStatus status) {
-          // A reference to |async_op| is kept in |async_op_ptr_|, safe to pass
-          // outside.  This is happening on an OS thread.
-          task_runner->PostTask(
-              FROM_HERE, base::BindOnce(&AsyncOperation::AsyncCallbackInternal,
-                                        base::Owned(instance),
-                                        base::Unretained(async_op), status));
-
-          return S_OK;
-        });
-
-    return async_op_ptr->put_Completed(async_callback.Get());
-  }
+                                     IAsyncOperationPtr async_op_ptr);
 
  private:
   AsyncOperation(Callback callback, IAsyncOperationPtr async_op_ptr)
@@ -80,13 +49,7 @@ class AsyncOperation {
         callback_(std::move(callback)) {}
 
   void AsyncCallbackInternal(IAsyncOperation<RuntimeType*>* async_op,
-                             AsyncStatus status) {
-    DCHECK_EQ(async_op, async_op_ptr_.Get());
-
-    std::move(callback_).Run((async_op && status == AsyncStatus::Completed)
-                                 ? std::move(async_op_ptr_)
-                                 : nullptr);
-  }
+                             AsyncStatus status);
 
   IAsyncOperationPtr async_op_ptr_;
   Callback callback_;
