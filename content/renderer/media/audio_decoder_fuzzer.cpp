f725eaeb77aa0ccfceccf1984436bafdb1e16b6a
Max Moroz <mmoroz@chromium.org>
2018-03-30 19:42:18 +0000

    Add content::SetUpBlinkTestEnvironment to audio_decoder_fuzzer.
    Bug: 826691
    Change-Id: I4bad266fd5f560fb056d252371ed3286d192012a
    Reviewed-on: https://chromium-review.googlesource.com/986732
    Commit-Queue: Max Moroz <mmoroz@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547244}

--- content/renderer/media/audio_decoder_fuzzer.cpp
+++ content/renderer/media/audio_decoder_fuzzer.cpp
@@ -4,18 +4,33 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "content/renderer/media/audio_decoder.h"
+
+#include "base/command_line.h"
+#include "base/logging.h"
+#include "content/public/test/blink_test_environment.h"
 #include "media/base/media.h"
 #include "third_party/WebKit/public/platform/WebAudioBus.h"
 
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-  // This is needed to suppress noisy log messages from ffmpeg.
-  media::InitializeMediaLibrary();
+struct Environment {
+  Environment() {
+    base::CommandLine::Init(0, nullptr);
+    content::SetUpBlinkTestEnvironment();
 
-  if (size > 8 * 1024) {
-    // Larger inputs are likely to trigger timeouts and OOMswhich would not be
-    // considered as valid bugs.
-    return 0;
+    // Suppress WARNING messages from the debug build.
+    logging::SetMinLogLevel(logging::LOG_FATAL);
+
+    // This is needed to suppress noisy log messages from ffmpeg.
+    media::InitializeMediaLibrary();
   }
+};
+
+Environment* env = new Environment();
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  // Larger inputs are likely to trigger timeouts and OOMs which would not be
+  // considered as valid bugs.
+  if (size > 8 * 1024)
+    return 0;
 
   blink::WebAudioBus web_audio_bus;
   bool success = content::DecodeAudioFileData(
