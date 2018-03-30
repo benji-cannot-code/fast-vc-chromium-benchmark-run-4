548ba16a16ec03bb7a7c560ff9fdeee3ba1aa918
Illia Martyniuk <illiam@google.com>
2018-03-30 00:06:05 +0000

    Aura: Passing page title instead of "DelegatedFrameHost"
    We can use more verbose page title instead of "DelegatedFrameHost" to
    pass to the SurfaceManager for debugging purposes. It is easier to track
    what FrameSink is associated with what Debug Label if these labels are
    page titles.
    
    Bug: 816802
    Change-Id: If5aa98fc7e9225d2798f85ca21d6ab221c02514b
    Reviewed-on: https://chromium-review.googlesource.com/984172
    Commit-Queue: Illia Martyniuk <illiam@google.com>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547032}

--- content/browser/renderer_host/render_widget_host_view_aura.cc
+++ content/browser/renderer_host/render_widget_host_view_aura.cc
@@ -274,6 +274,11 @@ class RenderWidgetHostViewAura::WindowObserver : public aura::WindowObserver {
     view_->ParentHierarchyChanged();
   }
 
+  void OnWindowTitleChanged(aura::Window* window) override {
+    if (window == view_->window_)
+      view_->WindowTitleChanged();
+  }
+
  private:
   RenderWidgetHostViewAura* view_;
 
@@ -733,6 +738,13 @@ void RenderWidgetHostViewAura::UpdateBackgroundColorFromRenderer(
   bool opaque = SkColorGetA(color) == SK_AlphaOPAQUE;
   window_->layer()->SetFillsBoundsOpaquely(opaque);
   window_->layer()->SetColor(color);
+}
+
+void RenderWidgetHostViewAura::WindowTitleChanged() {
+  if (delegated_frame_host_) {
+    delegated_frame_host_->WindowTitleChanged(
+        base::UTF16ToUTF8(window_->GetTitle()));
+  }
 }
 
 bool RenderWidgetHostViewAura::IsMouseLocked() {
