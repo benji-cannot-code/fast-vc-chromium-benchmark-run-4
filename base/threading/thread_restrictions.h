1e7ac0ba2a3b1cbd350d211a1cc43237e62933fa
Trent Apted <tapted@chromium.org>
2018-03-30 03:06:28 +0000

    Use TouchOptimizedUi by default on ChromeOS touch devices.
    Adds the ability to --enable_features=TouchOptimizedUi which changes
    the return value of ui::MaterialDesignController::DefaultMode() only
    on ChromeOS and only when a touch device is present.
    
    Initially, enable by default on ChromeOS.
    
    Bug: 801582
    Change-Id: I325de1c022c3e5893c059b2d774426a38232a641
    Reviewed-on: https://chromium-review.googlesource.com/981832
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547091}

--- base/threading/thread_restrictions.h
+++ base/threading/thread_restrictions.h
@@ -94,6 +94,7 @@ namespace ui {
 class CommandBufferClientImpl;
 class CommandBufferLocal;
 class GpuState;
+class MaterialDesignController;
 }
 namespace net {
 class MultiThreadedCertVerifierScopedAllowBaseSyncPrimitives;
@@ -219,6 +220,7 @@ class BASE_EXPORT ScopedAllowBlocking {
   friend class cronet::CronetURLRequestContext;
   friend class mojo::CoreLibraryInitializer;
   friend class resource_coordinator::TabManagerDelegate;  // crbug.com/778703
+  friend class ui::MaterialDesignController;
   friend class ScopedAllowBlockingForTesting;
   friend class StackSamplingProfiler;
 
