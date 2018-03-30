61ffbd6185a06f59ce93eedc8c4731ac92b10811
John Abd-El-Malek <jam@chromium.org>
2018-03-30 22:30:16 +0000

    Create SafeBrowsingNetworkContext to help convert safe browsing code to use the network service.
    This is a helper class which returns mojo interfaces for making requests. Eventually it will create
    and own the URLRequestContext in the network process. For now, it simply wraps the
    net::URLRequestContext that's in the browser. Once all calls to the safe browsing context use the
    mojo APIs then we can change the internal SafeBrowsingNetworkContext implementation to create the
    request context remotely.
    
    Bug: 825242
    Change-Id: I6032d82febc72467ae1769e65a44eb57d28bdc71
    Reviewed-on: https://chromium-review.googlesource.com/982260
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547291}

--- chrome/browser/safe_browsing/safe_browsing_service.cc
+++ chrome/browser/safe_browsing/safe_browsing_service.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/safe_browsing/file_type_policies.h"
 #include "components/prefs/pref_change_registrar.h"
 #include "components/prefs/pref_service.h"
+#include "components/safe_browsing/browser/safe_browsing_network_context.h"
 #include "components/safe_browsing/browser/safe_browsing_url_request_context_getter.h"
 #include "components/safe_browsing/common/safebrowsing_constants.h"
 #include "components/safe_browsing/db/database_manager.h"
@@ -146,6 +147,10 @@ void SafeBrowsingService::Initialize() {
   url_request_context_getter_ = new SafeBrowsingURLRequestContextGetter(
       g_browser_process->system_request_context(), user_data_dir);
 
+  network_context_ =
+      std::make_unique<safe_browsing::SafeBrowsingNetworkContext>(
+          url_request_context_getter_);
+
   ui_manager_ = CreateUIManager();
 
   if (!use_v4_only_) {
@@ -193,6 +198,7 @@ void SafeBrowsingService::ShutDown() {
       BrowserThread::IO, FROM_HERE,
       base::BindOnce(&SafeBrowsingURLRequestContextGetter::ServiceShuttingDown,
                      url_request_context_getter_));
+  network_context_->ServiceShuttingDown();
 
   // Release the URLRequestContextGetter after passing it to the IOThread.  It
   // has to be released now rather than in the destructor because it can only
@@ -217,6 +223,17 @@ scoped_refptr<net::URLRequestContextGetter>
 SafeBrowsingService::url_request_context() {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
   return url_request_context_getter_;
+}
+
+network::mojom::NetworkContext* SafeBrowsingService::GetNetworkContext() {
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
+  return network_context_->GetNetworkContext();
+}
+
+scoped_refptr<network::SharedURLLoaderFactory>
+SafeBrowsingService::GetURLLoaderFactory() {
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
+  return network_context_->GetURLLoaderFactory();
 }
 
 void SafeBrowsingService::DisableQuicOnIOThread() {
