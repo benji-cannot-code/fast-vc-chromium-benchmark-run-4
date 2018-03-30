0ec7c1a0eea59577824f5f6b8481a1aa38a18c46
Gabriel Charette <gab@chromium.org>
2018-03-30 16:05:31 +0000

    Use modern base APIs to avoid base::MessageLoop::current() calls in /chrome/browser/ui/views
    This is a precusor cleanup extracted from
    https://chromium-review.googlesource.com/c/chromium/src/+/957760/7
    in the broader effort to restrict usage of MessageLoop::current().
    
    This CL was uploaded by git cl split.
    
    R=sky@chromium.org
    
    Bug: 825327
    Change-Id: I2136d88cedccad0e0aba311e1a55524ce43c008b
    Reviewed-on: https://chromium-review.googlesource.com/986535
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547174}

--- chrome/browser/ui/views/touch_events_interactive_uitest_win.cc
+++ chrome/browser/ui/views/touch_events_interactive_uitest_win.cc
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
 #include "base/win/windows_version.h"
 #include "chrome/test/base/testing_profile.h"
@@ -36,15 +35,11 @@ class TouchEventHandler : public ui::EventHandler {
   }
 
   void WaitForIdle() {
-    base::MessageLoopForUI* loop = base::MessageLoopForUI::current();
-    base::MessageLoopForUI::ScopedNestableTaskAllower allow_nested(loop);
-    base::RunLoop run_loop;
+    base::RunLoop run_loop(base::RunLoop::Type::kNestableTasksAllowed);
     run_loop.RunUntilIdle();
   }
   void WaitForEvents() {
-    base::MessageLoopForUI* loop = base::MessageLoopForUI::current();
-    base::MessageLoopForUI::ScopedNestableTaskAllower allow_nested(loop);
-    base::RunLoop run_loop;
+    base::RunLoop run_loop(base::RunLoop::Type::kNestableTasksAllowed);
     quit_closure_ = run_loop.QuitClosure();
     run_loop.Run();
   }
