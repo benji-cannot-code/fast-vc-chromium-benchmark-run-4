d7b08a444d9f7d9f6f415d1c4998996374a01f49
Francois Doray <fdoray@chromium.org>
2018-03-30 13:21:38 +0000

    Add default implementations for LifecycleUnitObserver methods.
    These will be useful for an upcoming CL were TabManager doesn't need
    to have its own implementation of all methods.
    
    Bug: 775644
    Change-Id: Id746ab0fbf821cc7c20be731c3e84950ae470631
    Reviewed-on: https://chromium-review.googlesource.com/981263
    Reviewed-by: Sébastien Marchand <sebmarchand@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547149}

---
+++ chrome/browser/resource_coordinator/lifecycle_unit_observer.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/resource_coordinator/lifecycle_unit_observer.h"
+
+namespace resource_coordinator {
+
+LifecycleUnitObserver::~LifecycleUnitObserver() = default;
+
+void LifecycleUnitObserver::OnLifecycleUnitStateChanged(
+    LifecycleUnit* lifecycle_unit) {}
+
+void LifecycleUnitObserver::OnLifecycleUnitVisibilityChanged(
+    LifecycleUnit* lifecycle_unit,
+    content::Visibility visibility) {}
+
+void LifecycleUnitObserver::OnLifecycleUnitDestroyed(
+    LifecycleUnit* lifecycle_unit) {}
+
+}  // namespace resource_coordinator
