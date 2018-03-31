7936f8923435ffb7b8b39a023257a48db48971e9
Marijn Kruisselbrink <mek@chromium.org>
2018-03-31 00:11:33 +0000

    [Blobs] Record the result of creating a blob from a stream.
    Bug: 712693
    Change-Id: I65a7fd3086fe2e2c2e0177462eb97b7b864c1c11
    Reviewed-on: https://chromium-review.googlesource.com/988739
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547330}

--- storage/browser/blob/blob_builder_from_stream.cc
+++ storage/browser/blob/blob_builder_from_stream.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/span.h"
 #include "base/guid.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/task_scheduler/post_task.h"
 #include "storage/browser/blob/blob_data_item.h"
 #include "storage/browser/blob/blob_storage_context.h"
@@ -284,14 +285,14 @@ BlobBuilderFromStream::BlobBuilderFromStream(
 }
 
 BlobBuilderFromStream::~BlobBuilderFromStream() {
-  OnError();
+  OnError(Result::kAborted);
 }
 
 void BlobBuilderFromStream::AllocateMoreMemorySpace(
     uint64_t length_hint,
     mojo::ScopedDataPipeConsumerHandle pipe) {
   if (!context_ || !callback_) {
-    OnError();
+    OnError(Result::kAborted);
     return;
   }
   if (!pipe.is_valid()) {
@@ -311,7 +312,7 @@ void BlobBuilderFromStream::AllocateMoreMemorySpace(
 
   if (context_->memory_controller().GetAvailableMemoryForBlobs() <
       length_hint) {
-    OnError();
+    OnError(Result::kMemoryAllocationFailed);
     return;
   }
 
@@ -338,7 +339,7 @@ void BlobBuilderFromStream::MemoryQuotaAllocated(
     size_t item_to_populate,
     bool success) {
   if (!success || !context_ || !callback_) {
-    OnError();
+    OnError(success ? Result::kAborted : Result::kMemoryAllocationFailed);
     return;
   }
   DCHECK_LT(item_to_populate, chunk_items.size());
@@ -356,7 +357,7 @@ void BlobBuilderFromStream::DidWriteToMemory(
     uint64_t bytes_written,
     mojo::ScopedDataPipeConsumerHandle pipe) {
   if (!context_ || !callback_) {
-    OnError();
+    OnError(Result::kAborted);
     return;
   }
   DCHECK_LE(populated_item_index, chunk_items.size());
@@ -396,7 +397,7 @@ void BlobBuilderFromStream::AllocateMoreFileSpace(
     uint64_t length_hint,
     mojo::ScopedDataPipeConsumerHandle pipe) {
   if (!context_ || !callback_) {
-    OnError();
+    OnError(Result::kAborted);
     return;
   }
   if (!pipe.is_valid()) {
@@ -409,7 +410,7 @@ void BlobBuilderFromStream::AllocateMoreFileSpace(
 
   if (context_->memory_controller().GetAvailableFileSpaceForBlobs() <
       length_hint) {
-    OnError();
+    OnError(Result::kFileAllocationFailed);
     return;
   }
 
@@ -459,7 +460,7 @@ void BlobBuilderFromStream::FileQuotaAllocated(
     std::vector<BlobMemoryController::FileCreationInfo> info,
     bool success) {
   if (!success || !context_ || !callback_) {
-    OnError();
+    OnError(success ? Result::kAborted : Result::kFileAllocationFailed);
     return;
   }
   DCHECK_EQ(chunk_items.size(), info.size());
@@ -482,7 +483,7 @@ void BlobBuilderFromStream::DidWriteToFile(
     mojo::ScopedDataPipeConsumerHandle pipe,
     const base::Time& modification_time) {
   if (!success || !context_ || !callback_) {
-    OnError();
+    OnError(success ? Result::kAborted : Result::kFileWriteFailed);
     return;
   }
   DCHECK_EQ(chunk_items.size(), info.size());
@@ -531,7 +532,7 @@ void BlobBuilderFromStream::DidWriteToExtendedFile(
     mojo::ScopedDataPipeConsumerHandle pipe,
     const base::Time& modification_time) {
   if (!success || !context_ || !callback_) {
-    OnError();
+    OnError(success ? Result::kAborted : Result::kFileWriteFailed);
     return;
   }
   DCHECK(!items_.empty());
@@ -559,7 +560,8 @@ void BlobBuilderFromStream::DidWriteToExtendedFile(
   }
 }
 
-void BlobBuilderFromStream::OnError() {
+void BlobBuilderFromStream::OnError(Result result) {
+  RecordResult(result);
   if (pending_quota_task_)
     pending_quota_task_->Cancel();
 
@@ -574,9 +576,14 @@ void BlobBuilderFromStream::OnError() {
 void BlobBuilderFromStream::OnSuccess() {
   DCHECK(context_);
   DCHECK(callback_);
+  RecordResult(Result::kSuccess);
   std::move(callback_).Run(
       this, context_->AddFinishedBlob(base::GenerateGUID(), content_type_,
                                       content_disposition_, std::move(items_)));
+}
+
+void BlobBuilderFromStream::RecordResult(Result result) {
+  UMA_HISTOGRAM_ENUMERATION("Storage.Blob.BuildFromStreamResult", result);
 }
 
 bool BlobBuilderFromStream::ShouldStoreNextBlockOnDisk(uint64_t length_hint) {
