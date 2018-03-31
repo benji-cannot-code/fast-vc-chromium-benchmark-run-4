1b1f6b9d109a3444c5368f4b666e8d22fe790edb
David Tseng <dtseng@chromium.org>
2018-03-31 06:24:28 +0000

    Revert "Enable ChromeVox ARC support for all apps"
    This reverts commit bea322523f6dafc4a8ed689faada2346c021a008.
    
    Reason for revert: various apps breaking e.g. scrolling
    
    Original change's description:
    > Enable ChromeVox ARC support for all apps
    > 
    > Change-Id: I130db573fcc2ccbda3ce58c9d2bac845218ab1d9
    > Reviewed-on: https://chromium-review.googlesource.com/961549
    > Commit-Queue: David Tseng <dtseng@chromium.org>
    > Reviewed-by: Yuki Awano <yawano@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#544283}
    
    TBR=dtseng@chromium.org,yawano@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Change-Id: I6a3c80ac74b3101a3ab756f4e0c5e74980019980
    Reviewed-on: https://chromium-review.googlesource.com/989053
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547379}

--- chrome/browser/chromeos/arc/accessibility/arc_accessibility_helper_bridge_browsertest.cc
+++ chrome/browser/chromeos/arc/accessibility/arc_accessibility_helper_bridge_browsertest.cc
@@ -94,7 +94,7 @@ IN_PROC_BROWSER_TEST_F(ArcAccessibilityHelperBridgeBrowserTest,
               aura::client::kAccessibilityTouchExplorationPassThrough));
 
   chromeos::AccessibilityManager::Get()->EnableSpokenFeedback(true);
-  EXPECT_EQ(mojom::AccessibilityFilterType::ALL,
+  EXPECT_EQ(mojom::AccessibilityFilterType::WHITELISTED_PACKAGE_NAME,
             fake_accessibility_helper_instance_->filter_type());
 
   // Touch exploration pass through of test_window_1 (current active window)
