cce1aa65c506eceb67bd7fb412c2cbee2f5912fd
Mike Wasserman <msw@chromium.org>
2018-03-31 01:48:53 +0000

    mash: Support shelf pin and unpin in app info dialogs.
    Use ChromeLauncherController's ShelfModel to get/set shelf pinning.
    Ash's Shell is inaccessible from Chrome in Mash.
    
    Fix unit tests to construct a ChromeLauncherController and ShelfModel.
    Make BrowserStatusMonitor check for a Shell to support non-Mash tests.
    Convert AshTestBase fixture to BrowserWithTestWindowTest; disable sync.
    
    Bug: 826415
    Test: App list -> app context menu -> app info -> dialog shows [un]pin.
    Change-Id: Icdfdde84692184cf95dee83a71ff068cef5f98e9
    Reviewed-on: https://chromium-review.googlesource.com/988825
    Commit-Queue: Michael Wasserman <msw@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547356}

--- chrome/browser/ui/views/apps/app_info_dialog/app_info_dialog_views_unittest.cc
+++ chrome/browser/ui/views/apps/app_info_dialog/app_info_dialog_views_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/test_extension_environment.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
+#include "chrome/browser/ui/views/apps/app_info_dialog/app_info_footer_panel.h"
 #include "chrome/browser/ui/views/apps/app_info_dialog/app_info_header_panel.h"
 #include "chrome/common/extensions/extension_constants.h"
 #include "chrome/test/base/browser_with_test_window_test.h"
@@ -29,9 +30,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/window/dialog_delegate.h"
 
 #if defined(OS_CHROMEOS)
+#include "ash/public/cpp/shelf_model.h"  // nogncheck
 #include "chrome/browser/ui/app_list/arc/arc_app_list_prefs.h"
 #include "chrome/browser/ui/app_list/arc/arc_app_test.h"
 #include "chrome/browser/ui/app_list/arc/arc_app_utils.h"
+#include "chrome/browser/ui/ash/launcher/chrome_launcher_controller.h"
 #endif
 
 #if defined(OS_CHROMEOS)
@@ -89,6 +92,10 @@ class AppInfoDialogViewsTest : public BrowserWithTestWindowTest,
   void SetUp() override {
     BrowserWithTestWindowTest::SetUp();
 #if defined(OS_CHROMEOS)
+    shelf_model_ = std::make_unique<ash::ShelfModel>();
+    chrome_launcher_controller_ = std::make_unique<ChromeLauncherController>(
+        extension_environment_.profile(), shelf_model_.get());
+    chrome_launcher_controller_->Init();
     arc_test_.SetUp(extension_environment_.profile());
 #endif
     extension_ = extension_environment_.MakePackagedApp(kTestExtensionId, true);
@@ -102,6 +109,8 @@ class AppInfoDialogViewsTest : public BrowserWithTestWindowTest,
     chrome_app_ = nullptr;
 #if defined(OS_CHROMEOS)
     arc_test_.TearDown();
+    chrome_launcher_controller_.reset();
+    shelf_model_.reset();
 #endif
     BrowserWithTestWindowTest::TearDown();
   }
@@ -163,6 +172,8 @@ class AppInfoDialogViewsTest : public BrowserWithTestWindowTest,
       extensions::TestExtensionEnvironment::Type::
           kInheritExistingTaskEnvironment};
 #if defined(OS_CHROMEOS)
+  std::unique_ptr<ash::ShelfModel> shelf_model_;
+  std::unique_ptr<ChromeLauncherController> chrome_launcher_controller_;
   ArcAppTest arc_test_;
 #endif
 
@@ -205,6 +216,8 @@ TEST_F(AppInfoDialogViewsTest, DestroyedProfileClosesDialog) {
   browser_window.reset();
 
 #if defined(OS_CHROMEOS)
+  chrome_launcher_controller_.reset();
+  shelf_model_.reset();
   arc_test_.TearDown();
 #endif
 
@@ -305,5 +318,33 @@ TEST_F(AppInfoDialogViewsTest, ArcAppInfoLinks) {
   // The ARC App info links are not available if ARC is not allowed for
   // secondary profile.
   EXPECT_FALSE(dialog_->arc_app_info_links_for_test());
+}
+
+// Tests that the pin/unpin button is focused after unpinning/pinning. This is
+// to verify regression in crbug.com/428704 is fixed.
+TEST_F(AppInfoDialogViewsTest, PinButtonsAreFocusedAfterPinUnpin) {
+  ShowAppInfo(kTestExtensionId);
+  AppInfoFooterPanel* dialog_footer =
+      static_cast<AppInfoFooterPanel*>(dialog_->dialog_footer_);
+  views::View* pin_button = dialog_footer->pin_to_shelf_button_;
+  views::View* unpin_button = dialog_footer->unpin_from_shelf_button_;
+
+  pin_button->RequestFocus();
+  EXPECT_TRUE(pin_button->visible());
+  EXPECT_FALSE(unpin_button->visible());
+  EXPECT_TRUE(pin_button->HasFocus());
+
+  // Avoid attempting to use sync, it's not initialized in this test.
+  auto sync_disabler = chrome_launcher_controller_->GetScopedPinSyncDisabler();
+
+  dialog_footer->SetPinnedToShelf(true);
+  EXPECT_FALSE(pin_button->visible());
+  EXPECT_TRUE(unpin_button->visible());
+  EXPECT_TRUE(unpin_button->HasFocus());
+
+  dialog_footer->SetPinnedToShelf(false);
+  EXPECT_TRUE(pin_button->visible());
+  EXPECT_FALSE(unpin_button->visible());
+  EXPECT_TRUE(pin_button->HasFocus());
 }
 #endif
--- chrome/browser/ui/views/apps/app_info_dialog/app_info_footer_panel.cc
+++ chrome/browser/ui/views/apps/app_info_dialog/app_info_footer_panel.cc
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_CHROMEOS)
 // gn check complains on Linux Ozone.
 #include "ash/public/cpp/shelf_model.h"  // nogncheck
-#include "ash/shell.h"
+#include "chrome/browser/ui/ash/launcher/chrome_launcher_controller.h"
 #include "chrome/browser/ui/ash/launcher/chrome_launcher_controller_util.h"
 #endif
 
@@ -92,13 +92,15 @@ void AppInfoFooterPanel::LayoutButtons() {
 void AppInfoFooterPanel::UpdatePinButtons(bool focus_visible_button) {
 #if defined(OS_CHROMEOS)
   if (pin_to_shelf_button_ && unpin_from_shelf_button_) {
-    bool is_pinned = !ash::Shell::Get()->shelf_model()->IsAppPinned(app_->id());
-    pin_to_shelf_button_->SetVisible(is_pinned);
-    unpin_from_shelf_button_->SetVisible(!is_pinned);
+    const bool was_pinned =
+        ChromeLauncherController::instance()->shelf_model()->IsAppPinned(
+            app_->id());
+    pin_to_shelf_button_->SetVisible(!was_pinned);
+    unpin_from_shelf_button_->SetVisible(was_pinned);
 
     if (focus_visible_button) {
       views::View* button_to_focus =
-          is_pinned ? pin_to_shelf_button_ : unpin_from_shelf_button_;
+          was_pinned ? unpin_from_shelf_button_ : pin_to_shelf_button_;
       button_to_focus->RequestFocus();
     }
   }
@@ -154,7 +156,8 @@ bool AppInfoFooterPanel::CanCreateShortcuts() const {
 #if defined(OS_CHROMEOS)
 void AppInfoFooterPanel::SetPinnedToShelf(bool value) {
   DCHECK(CanSetPinnedToShelf());
-  ash::ShelfModel* shelf_model = ash::Shell::Get()->shelf_model();
+  ash::ShelfModel* shelf_model =
+      ChromeLauncherController::instance()->shelf_model();
   DCHECK(shelf_model);
   if (value)
     shelf_model->PinAppWithID(app_->id());
@@ -166,10 +169,6 @@ void AppInfoFooterPanel::SetPinnedToShelf(bool value) {
 }
 
 bool AppInfoFooterPanel::CanSetPinnedToShelf() const {
-  // Non-Ash platforms don't have a shelf.
-  if (!ash::Shell::HasInstance())
-    return false;
-
   // The Chrome app can't be unpinned, and extensions can't be pinned.
   return app_->id() != extension_misc::kChromeAppId && !app_->is_extension() &&
          (GetPinnableForAppID(app_->id(), profile_) ==
