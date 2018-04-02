63237c79777c6b12b00abbf3fd7854ea0bc61a7f
Avi Drissman <avi@chromium.org>
2018-04-02 21:44:07 +0000

    Remove notifications from infobars.
    BUG=354380
    TEST=existing tests
    
    Change-Id: Ia1075716e092ef0edd64eca4907414ac5e10fca2
    Reviewed-on: https://chromium-review.googlesource.com/985073
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547527}

--- chrome/browser/translate/translate_manager_render_view_host_unittest.cc
+++ chrome/browser/translate/translate_manager_render_view_host_unittest.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "build/build_config.h"
 #include "chrome/app/chrome_command_ids.h"
-#include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/extensions/test_extension_system.h"
 #include "chrome/browser/infobars/infobar_service.h"
 #include "chrome/browser/renderer_context_menu/render_view_context_menu_test_util.h"
@@ -35,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/testing_browser_process.h"
 #include "chrome/test/base/testing_profile.h"
 #include "components/infobars/core/infobar.h"
+#include "components/infobars/core/infobar_manager.h"
 #include "components/prefs/pref_change_registrar.h"
 #include "components/prefs/pref_service.h"
 #include "components/translate/content/browser/content_translate_driver.h"
@@ -53,6 +53,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/notification_details.h"
 #include "content/public/browser/notification_registrar.h"
+#include "content/public/browser/notification_types.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/browser_side_navigation_policy.h"
 #include "content/public/common/url_constants.h"
@@ -208,7 +209,7 @@ class NavEntryCommittedObserver : public content::NotificationObserver {
 
 class TranslateManagerRenderViewHostTest
     : public ChromeRenderViewHostTestHarness,
-      public content::NotificationObserver {
+      public infobars::InfoBarManager::Observer {
  public:
   TranslateManagerRenderViewHostTest()
       : pref_callback_(
@@ -446,13 +447,8 @@ class TranslateManagerRenderViewHostTest
                                          params);
   }
 
-  virtual void Observe(int type,
-                       const content::NotificationSource& source,
-                       const content::NotificationDetails& details) {
-    DCHECK_EQ(chrome::NOTIFICATION_TAB_CONTENTS_INFOBAR_REMOVED, type);
-    removed_infobars_.insert(
-        content::Details<infobars::InfoBar::RemovedDetails>(
-            details)->first->delegate());
+  void OnInfoBarRemoved(infobars::InfoBar* infobar, bool animate) override {
+    removed_infobars_.insert(infobar->delegate());
   }
 
   MOCK_METHOD1(OnPreferenceChanged, void(const std::string&));
@@ -481,17 +477,11 @@ class TranslateManagerRenderViewHostTest
         ->translate_driver()
         .set_translate_max_reload_attempts(0);
 
-    notification_registrar_.Add(
-        this,
-        chrome::NOTIFICATION_TAB_CONTENTS_INFOBAR_REMOVED,
-        content::Source<InfoBarService>(infobar_service()));
+    infobar_service()->AddObserver(this);
   }
 
   virtual void TearDown() {
-    notification_registrar_.Remove(
-        this,
-        chrome::NOTIFICATION_TAB_CONTENTS_INFOBAR_REMOVED,
-        content::Source<InfoBarService>(infobar_service()));
+    infobar_service()->RemoveObserver(this);
 
     ChromeRenderViewHostTestHarness::TearDown();
     TranslateService::ShutdownForTesting();
@@ -544,7 +534,6 @@ class TranslateManagerRenderViewHostTest
   PrefChangeRegistrar::NamedChangeCallback pref_callback_;
 
  private:
-  content::NotificationRegistrar notification_registrar_;
   net::TestURLFetcherFactory url_fetcher_factory_;
 
   // The infobars that have been removed.
--- chrome/test/ppapi/ppapi_test.h
+++ chrome/test/ppapi/ppapi_test.h
@@ -11,8 +11,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/macros.h"
 #include "chrome/test/base/in_process_browser_test.h"
+#include "components/infobars/core/infobar_manager.h"
 #include "content/public/test/javascript_test_observer.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
+
+class InfoBarService;
 
 class PPAPITestMessageHandler : public content::TestMessageHandler {
  public:
@@ -54,7 +57,7 @@ class PPAPITestBase : public InProcessBrowserTest {
       const std::string& test_case);
 
  protected:
-  class InfoBarObserver : public content::NotificationObserver {
+  class InfoBarObserver : public infobars::InfoBarManager::Observer {
    public:
     explicit InfoBarObserver(PPAPITestBase* test_base);
     ~InfoBarObserver();
@@ -62,14 +65,13 @@ class PPAPITestBase : public InProcessBrowserTest {
     void ExpectInfoBarAndAccept(bool should_accept);
 
    private:
-    // content::NotificationObserver:
-    void Observe(int type,
-                 const content::NotificationSource& source,
-                 const content::NotificationDetails& details) override;
+    // infobars::InfoBarManager::Observer:
+    void OnInfoBarAdded(infobars::InfoBar* infobar) override;
+
+    InfoBarService* GetInfoBarService();
 
     void VerifyInfoBarState();
 
-    content::NotificationRegistrar registrar_;
     PPAPITestBase* test_base_;
     bool expecting_infobar_;
     bool should_accept_;
