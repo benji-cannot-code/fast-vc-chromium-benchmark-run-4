923736c0e0878d03fb5474b8a897adea090c731e
Daniel Cheng <dcheng@chromium.org>
2018-04-02 10:31:59 +0000

    Reland "Enforce user gesture requirement on browser side for WebUI."
    This is a reland of f6953215ca45988400eb3cb7ab406456b72f36de
    
    Original change's description:
    > Enforce user gesture requirement on browser side for WebUI.
    >
    > Also, don't trust the renderer process to send the correct URL.
    >
    > Bug: 823864
    > Change-Id: I1f5c1c7908d8157240b5a7d5fc2df944267cf537
    > Reviewed-on: https://chromium-review.googlesource.com/979399
    > Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    > Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#547114}
    
    Bug: 823864
    Tbr: sky@chromium.org
    Change-Id: I9751f0f63524aa52bc5af326cd00e99a84dc1009
    Reviewed-on: https://chromium-review.googlesource.com/987694
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547425}

--- content/browser/webui/web_ui_impl.cc
+++ content/browser/webui/web_ui_impl.cc
@@ -12,7 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/debug/dump_without_crashing.h"
 #include "base/json/json_writer.h"
+#include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/time/time.h"
 #include "base/values.h"
 #include "content/browser/child_process_security_policy_impl.h"
 #include "content/browser/frame_host/frame_tree.h"
@@ -80,7 +82,7 @@ base::string16 WebUI::GetJavascriptCall(
   return result;
 }
 
-WebUIImpl::WebUIImpl(WebContents* contents)
+WebUIImpl::WebUIImpl(WebContentsImpl* contents)
     : bindings_(BINDINGS_POLICY_WEB_UI),
       web_contents_(contents),
       web_contents_observer_(new MainFrameNavigationObserver(this, contents)) {
@@ -106,9 +108,9 @@ bool WebUIImpl::OnMessageReceived(const IPC::Message& message,
 }
 
 void WebUIImpl::OnWebUISend(RenderFrameHost* sender,
-                            const GURL& source_url,
                             const std::string& message,
                             const base::ListValue& args) {
+  const GURL& source_url = sender->GetLastCommittedURL();
   if (!ChildProcessSecurityPolicyImpl::GetInstance()->HasWebUIBindings(
           sender->GetProcess()->GetID()) ||
       !WebUIControllerFactoryRegistry::GetInstance()->IsURLAcceptableForWebUI(
@@ -123,7 +125,14 @@ void WebUIImpl::OnWebUISend(RenderFrameHost* sender,
   if (!sender->IsCurrent())
     return;
 
-  ProcessWebUIMessage(source_url, message, args);
+  if (base::EndsWith(message, "RequiringGesture",
+                     base::CompareCase::SENSITIVE) &&
+      !web_contents_->HasRecentInteractiveInputEvent()) {
+    LOG(ERROR) << message << " received without recent user interaction";
+    return;
+  }
+
+  ProcessWebUIMessage(sender->GetLastCommittedURL(), message, args);
 }
 
 void WebUIImpl::RenderFrameCreated(RenderFrameHost* render_frame_host) {
