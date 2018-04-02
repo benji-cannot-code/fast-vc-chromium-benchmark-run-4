a14caeab63352c8583ce7e732d061e23d997c73c
Koji Ishii <kojii@chromium.org>
2018-04-02 12:17:38 +0000

    LayoutText::LocalSelectionRect should return early if range is invalid
    While working on supporting LayoutNG, it turns out that there are
    several cases where the selection range becomes start > end.
      e.g., fast/text/selection/emphasis.html
    
    Layout functions generally prefer proper checks are done in the
    callers, rather than input arguments automatically corrected in
    lower-level functions.
    
    This patch makes LayoutText::LocalSelectionRect match to it.
    
    Bug: 636993, 827925
    Change-Id: I3068e70886f7cd425a2fd5f701ac0b7d77a8a978
    Reviewed-on: https://chromium-review.googlesource.com/989474
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547430}

--- third_party/WebKit/Source/core/layout/LayoutText.cpp
+++ third_party/WebKit/Source/core/layout/LayoutText.cpp
@@ -2013,9 +2013,10 @@ LayoutRect LayoutText::LocalSelectionRect() const {
     }
   }
 
+  // TODO(yoichio): The following DCHECK should pass, but fails 14 tests.
+  // DCHECK_LE(start_pos, end_pos);
   LayoutRect rect;
-
-  if (start_pos == end_pos)
+  if (start_pos >= end_pos)
     return rect;
 
   for (InlineTextBox* box : TextBoxes()) {
