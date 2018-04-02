b004f09d8c0b32b093afdf3a4da06affe969b8cb
Joshua Bell <jsbell@chromium.org>
2018-04-02 22:48:39 +0000

    Cookie Store API: Split out and enable test cases
    Split out cases from a master "cookie-store-tests.js" suite,
    allowing them to be enabled/debugged separately.
    
    Also updates observation tests to use the new "change" event,
    and deletes "static" since we can assume a dependency on a test
    server here.
    
    Bug: 783229
    Change-Id: I9e29ea74a2f0513a033a8f9c71da2836c000ee27
    Reviewed-on: https://chromium-review.googlesource.com/986649
    Commit-Queue: Joshua Bell <jsbell@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547543}

---
+++ third_party/WebKit/LayoutTests/external/wpt/cookie-store/resources/document_cookie.js
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+'use strict';
+
+cookie_test(async t => {
+  let eventPromise = observeNextCookieChangeEvent();
+  await setCookieStringDocument('DOCUMENT-🍪=🔵; path=/');
+  assert_equals(
+      await getCookieString(),
+      'DOCUMENT-🍪=🔵',
+      'Cookie we wrote using document.cookie in cookie jar');
+  assert_equals(
+    await getCookieStringHttp(),
+    'DOCUMENT-🍪=🔵',
+    'Cookie we wrote using document.cookie in HTTP cookie jar');
+  assert_equals(
+      await getCookieStringDocument(),
+      'DOCUMENT-🍪=🔵',
+      'Cookie we wrote using document.cookie in document.cookie');
+  await verifyCookieChangeEvent(
+    eventPromise, {changed: [{name: 'DOCUMENT-🍪', value: '🔵'}]},
+      'Cookie we wrote using document.cookie is observed');
+
+  eventPromise = observeNextCookieChangeEvent();
+  await setCookieStringDocument('DOCUMENT-🍪=DELETED; path=/; max-age=0');
+  assert_equals(
+      await getCookieString(),
+      undefined,
+      'Empty cookie jar after document.cookie' +
+        ' cookie-clearing using max-age=0');
+  assert_equals(
+    await getCookieStringHttp(),
+    undefined,
+    'Empty HTTP cookie jar after document.cookie' +
+        ' cookie-clearing using max-age=0');
+  assert_equals(
+      await getCookieStringDocument(),
+      undefined,
+      'Empty document.cookie cookie jar after document.cookie' +
+        ' cookie-clearing using max-age=0');
+  await verifyCookieChangeEvent(
+    eventPromise, {deleted: [{name: 'DOCUMENT-🍪'}]},
+      'Deletion observed after document.cookie cookie-clearing' +
+        ' using max-age=0');
+}, 'Verify interoperability of document.cookie with other APIs.');
