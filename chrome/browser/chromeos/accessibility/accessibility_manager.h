3bd3ab2b17e4ab2997b173cf3b64e1afa117ff94
Katie D <katie@chromium.org>
2018-04-02 23:17:30 +0000

    Change the STS EventRewriter to an EventHandler at a11y priority.
    This makes the STS EventRewriter work with Sticky Keys mode, and
    is basically a revert of commit 0c0f37953a732bd7876d209fa81847eab29abb59
    along with porting other modifications made since that commit.
    
    Bug: 819860
    Change-Id: I340295ac0ca6e24057fcb9d4a0890d77b3cf0439
    Reviewed-on: https://chromium-review.googlesource.com/988849
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547553}

--- chrome/browser/chromeos/accessibility/accessibility_manager.h
+++ chrome/browser/chromeos/accessibility/accessibility_manager.h
@@ -42,6 +42,7 @@ class Rect;
 namespace chromeos {
 
 class AccessibilityExtensionLoader;
+class SelectToSpeakEventHandler;
 class SwitchAccessEventHandler;
 
 enum AccessibilityNotificationType {
@@ -436,6 +437,9 @@ class AccessibilityManager
   std::unique_ptr<AccessibilityExtensionLoader> chromevox_loader_;
 
   std::unique_ptr<AccessibilityExtensionLoader> select_to_speak_loader_;
+
+  std::unique_ptr<chromeos::SelectToSpeakEventHandler>
+      select_to_speak_event_handler_;
 
   std::unique_ptr<AccessibilityExtensionLoader> switch_access_loader_;
 
--- chrome/browser/chromeos/chrome_browser_main_chromeos.cc
+++ chrome/browser/chromeos/chrome_browser_main_chromeos.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
-#include "ash/root_window_controller.h"
 #include "ash/shell.h"
 #include "ash/sticky_keys/sticky_keys_controller.h"
 #include "base/bind.h"
@@ -36,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/chromeos/accessibility/accessibility_manager.h"
 #include "chrome/browser/chromeos/accessibility/magnification_manager.h"
-#include "chrome/browser/chromeos/accessibility/select_to_speak_event_rewriter.h"
 #include "chrome/browser/chromeos/accessibility/spoken_feedback_event_rewriter.h"
 #include "chrome/browser/chromeos/app_mode/arc/arc_kiosk_app_manager.h"
 #include "chrome/browser/chromeos/app_mode/kiosk_app_launch_error.h"
@@ -1042,11 +1040,6 @@ void ChromeBrowserMainPartsChromeos::PostBrowserStart() {
         std::unique_ptr<ui::EventRewriter>(new KeyboardDrivenEventRewriter()));
     keyboard_event_rewriters_->AddEventRewriter(
         std::unique_ptr<ui::EventRewriter>(new SpokenFeedbackEventRewriter()));
-    keyboard_event_rewriters_->AddEventRewriter(
-        std::unique_ptr<ui::EventRewriter>(new SelectToSpeakEventRewriter(
-            ash::Shell::Get()
-                ->GetPrimaryRootWindowController()
-                ->GetRootWindow())));
     event_rewriter_delegate_ = std::make_unique<EventRewriterDelegateImpl>();
     keyboard_event_rewriters_->AddEventRewriter(
         std::make_unique<ui::EventRewriterChromeOS>(
