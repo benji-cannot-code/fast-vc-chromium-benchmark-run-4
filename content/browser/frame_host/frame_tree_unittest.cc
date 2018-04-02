0a0aadc3c424c3647604e70be12fd45ca3a4b44c
Charlie Reis <creis@chromium.org>
2018-04-02 22:52:39 +0000

    Revert "Disable test FrameTreeTest.FindFrames"
    This reverts commit 1c854a22e9cf92fbbb86ac46ba6b56c4bac1764a.
    
    Reason for revert: Failures were not due to this test.  Passes locally.
    
    Original change's description:
    > Disable test FrameTreeTest.FindFrames
    > 
    > Test FrameTreeTest.FindFrames is flaky. Disable it.
    > 
    > TBR=creis@chromium.org
    > 
    > Bug: 826599
    > Change-Id: I3536ef001db9f8df4193ad7c709cc4f7c4246d97
    > Reviewed-on: https://chromium-review.googlesource.com/985996
    > Reviewed-by: Adam Rice <ricea@chromium.org>
    > Commit-Queue: Adam Rice <ricea@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#546808}
    
    TBR=creis@chromium.org,ricea@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 826599
    Change-Id: I1b3aeeaccb8458fc57959efdf236d126280cde5b
    Reviewed-on: https://chromium-review.googlesource.com/990752
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: Charlie Reis <creis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547545}

--- content/browser/frame_host/frame_tree_unittest.cc
+++ content/browser/frame_host/frame_tree_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2013 The Chromium Authors. All rights reserved.
+// Copyright 2013 The Chromium Authors.All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -311,8 +311,7 @@ TEST_F(FrameTreeTest, Shape) {
 }
 
 // Ensure frames can be found by frame_tree_node_id, routing ID, or name.
-// Disabled due to flakiness: http://crbug.com/826599
-TEST_F(FrameTreeTest, DISABLED_FindFrames) {
+TEST_F(FrameTreeTest, FindFrames) {
   main_test_rfh()->InitializeRenderFrameIfNeeded();
 
   // Add a few child frames to the main frame.
