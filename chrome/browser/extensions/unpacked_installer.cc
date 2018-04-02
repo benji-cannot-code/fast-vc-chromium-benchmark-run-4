37ac021b8ca4a8a0fb8155e3e192aea3e5361f7b
David Bertoni <dbertoni@chromium.org>
2018-04-02 20:38:07 +0000

    Resolve a leading ~ in extension loading path to the user's home dir.
    Bug: 815060
    Change-Id: I358fca7bfeb19ef035703f37318f7eb0c90d7015
    Reviewed-on: https://chromium-review.googlesource.com/976550
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547512}

--- chrome/browser/extensions/unpacked_installer.cc
+++ chrome/browser/extensions/unpacked_installer.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/files/file_util.h"
 #include "base/json/json_file_value_serializer.h"
+#include "base/path_service.h"
 #include "base/strings/string16.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
@@ -26,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_file_task_runner.h"
 #include "extensions/browser/extension_prefs.h"
 #include "extensions/browser/install_flag.h"
+#include "extensions/browser/path_util.h"
 #include "extensions/browser/policy_check.h"
 #include "extensions/browser/preload_check_group.h"
 #include "extensions/browser/requirements_checker.h"
@@ -108,7 +110,8 @@ bool UnpackedInstaller::LoadFromCommandLine(const base::FilePath& path_in,
   // between extension loading and loading an URL from the command line.
   base::ThreadRestrictions::ScopedAllowIO allow_io;
 
-  extension_path_ = base::MakeAbsoluteFilePath(path_in);
+  extension_path_ =
+      base::MakeAbsoluteFilePath(path_util::ResolveHomeDirectory(path_in));
 
   if (!IsLoadingUnpackedAllowed()) {
     ReportExtensionLoadError(kUnpackedExtensionsBlacklistedError);
