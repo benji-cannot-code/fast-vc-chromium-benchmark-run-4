e2809b22ebb067447257c31917e742f8e2ae9b03
Simeon Anfinrud <sanfin@chromium.org>
2018-04-02 17:44:13 +0000

    [Chromecast] Add ReactiveRecorder test util class.
    This can be used in lieu of mutating an ArrayList and using
    Hamcrest matchers to check the behavior of Observables.
    
    I have a couple of in-progress refactors that could use
    something like this to help with testing.
    
    Bug: None
    Test: cast_base_junit_tests
    Change-Id: Ieaac43cc2cb9d7f5b1577a7992b6211035fc36f9
    Reviewed-on: https://chromium-review.googlesource.com/982514
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Commit-Queue: Simeon Anfinrud <sanfin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547462}

--- chromecast/base/java/test/org/chromium/chromecast/base/Inheritance.java
+++ chromecast/base/java/test/org/chromium/chromecast/base/Inheritance.java
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chromecast.base;
 
-class TestUtils {
+class Inheritance {
     // Use to test consumption of superclasses of some generic parameter.
     public static class Base {
         @Override
