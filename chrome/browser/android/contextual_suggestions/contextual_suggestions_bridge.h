cd618e2f6445aa846ebe1fe73a89245094c7ae04
Filip Gorski <fgorski@chromium.org>
2018-04-02 23:32:10 +0000

    [EoC] Wiring the new bridge to the old prototype
    * Wires the new bridge to the old prototype
    * Tentatively takes dependency on the content suggestion service
      to simplyfy favicon fetching (this may still not work, because
      the database is not filled with contextual suggestions).
    * Next step to fetch favicons is to extract and reuse the code,
      while dropping the dependency on content suggestion service.
    
    Bug: 824183
    Change-Id: I50d6879b837a06f64ac9c376de4a1f6f6c151d2b
    Reviewed-on: https://chromium-review.googlesource.com/989612
    Commit-Queue: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547561}

--- chrome/browser/android/contextual_suggestions/contextual_suggestions_bridge.h
+++ chrome/browser/android/contextual_suggestions/contextual_suggestions_bridge.h
@@ -10,6 +10,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "components/ntp_snippets/contextual/contextual_content_suggestions_service.h"
 
+namespace gfx {
+class Image;
+}  // namespace gfx
+
+namespace ntp_snippets {
+class ContentSuggestionsService;
+}  // namespace ntp_snippets
+
 namespace contextual_suggestions {
 
 // Class implementing native side of ContextualSuggestionsBrigde.java.
@@ -61,6 +69,12 @@ class ContextualSuggestionsBridge {
       std::vector<ntp_snippets::ContextualContentSuggestionsService::Cluster>
           clusters);
 
+  void OnImageFetched(base::android::ScopedJavaGlobalRef<jobject> j_callback,
+                      const gfx::Image& image);
+
+  // Content suggestions service is necessary for the favicon fetching.
+  ntp_snippets::ContentSuggestionsService* content_suggestions_service_;
+  // Contextual content suggestions service used for fetching suggestions.
   ntp_snippets::ContextualContentSuggestionsService*
       contextual_content_suggestions_service_;
 
