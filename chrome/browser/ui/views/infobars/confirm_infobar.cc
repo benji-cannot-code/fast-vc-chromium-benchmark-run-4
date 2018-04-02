a6fcd658adf7696901ab71b240e4b0ea2c19e311
Peter Kasting <pkasting@chromium.org>
2018-04-02 21:28:08 +0000

    Remove InfoBarView::child_container_.
    We need to clip infobar child elements to the "bar" portion of the infobar.
    Previously, this couldn't be done simply by clipping to the infobar bounds,
    because the top arrow meant the infobar bounds extended up past the bar.  Now
    that arrows are dead, |child_container_| is no longer necessary.
    
    BUG=none
    TEST=none
    
    Change-Id: Ib9c2ec62638eff1043d6e7a43bdb6648e0a08289
    Reviewed-on: https://chromium-review.googlesource.com/989286
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547519}

--- chrome/browser/ui/views/infobars/confirm_infobar.cc
+++ chrome/browser/ui/views/infobars/confirm_infobar.cc
@@ -79,7 +79,7 @@ void ConfirmInfoBar::ViewHierarchyChanged(
   if (details.is_add && details.child == this && (label_ == nullptr)) {
     ConfirmInfoBarDelegate* delegate = GetDelegate();
     label_ = CreateLabel(delegate->GetMessageText());
-    AddViewToContentArea(label_);
+    AddChildView(label_);
 
     if (delegate->GetButtons() & ConfirmInfoBarDelegate::BUTTON_OK) {
       ok_button_ = CreateButton(ConfirmInfoBarDelegate::BUTTON_OK);
@@ -99,7 +99,7 @@ void ConfirmInfoBar::ViewHierarchyChanged(
 
     base::string16 link_text(delegate->GetLinkText());
     link_ = CreateLink(link_text, this);
-    AddViewToContentArea(link_);
+    AddChildView(link_);
   }
 
   // This must happen after adding all other children so InfoBarView can ensure
@@ -149,7 +149,7 @@ views::MdTextButton* ConfirmInfoBar::CreateButton(
       new gfx::Insets(ChromeLayoutProvider::Get()->GetDistanceMetric(
                           DISTANCE_TOAST_CONTROL_VERTICAL),
                       0));
-  AddViewToContentArea(button);
+  AddChildView(button);
   button->SizeToPreferredSize();
   return button;
 }
