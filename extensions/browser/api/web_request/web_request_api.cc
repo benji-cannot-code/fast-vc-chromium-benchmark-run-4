0f3a1a8680e611f9e278362fdc64337ee0ba9047
Ken Rockot <rockot@chromium.org>
2018-04-02 20:13:59 +0000

    Instrument WebRequest header modifications
    This is to gain some more understanding about how request and response
    header modifications are used in practice. See bug for more details.
    
    Bug: 827582
    Change-Id: I589483173d16988f1c0676cd0a6d768da12e1037
    Reviewed-on: https://chromium-review.googlesource.com/988237
    Commit-Queue: Ken Rockot <rockot@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547504}

--- extensions/browser/api/web_request/web_request_api.cc
+++ extensions/browser/api/web_request/web_request_api.cc
@@ -114,6 +114,16 @@ enum RequestAction {
   MAX
 };
 
+// Corresponds to the "WebRequestEventResponse" histogram enumeration type in
+// src/tools/metrics/histograms/enums.xml.
+//
+// DO NOT REORDER OR CHANGE THE MEANING OF THESE VALUES.
+enum class WebRequestEventResponse {
+  kIgnored,
+  kObserved,
+  kMaxValue = kObserved,
+};
+
 const char kWebRequestEventPrefix[] = "webRequest.";
 
 // List of all the webRequest events.
@@ -745,6 +755,11 @@ int ExtensionWebRequestEventRouter::OnBeforeSendHeaders(
                       std::move(event_details));
   }
 
+  UMA_HISTOGRAM_ENUMERATION(
+      "Extensions.WebRequest.OnBeforeSendHeadersEventResponse",
+      initialize_blocked_requests ? WebRequestEventResponse::kObserved
+                                  : WebRequestEventResponse::kIgnored);
+
   if (!initialize_blocked_requests)
     return net::OK;  // Nobody saw a reason for modifying the request.
 
@@ -823,6 +838,11 @@ int ExtensionWebRequestEventRouter::OnHeadersReceived(
         DispatchEvent(browser_context, extension_info_map, request, listeners,
                       std::move(event_details));
   }
+
+  UMA_HISTOGRAM_ENUMERATION(
+      "Extensions.WebRequest.OnHeadersReceivedEventResponse",
+      initialize_blocked_requests ? WebRequestEventResponse::kObserved
+                                  : WebRequestEventResponse::kIgnored);
 
   if (!initialize_blocked_requests)
     return net::OK;  // Nobody saw a reason for modifying the request.
--- extensions/browser/api/web_request/web_request_api_helpers.cc
+++ extensions/browser/api/web_request/web_request_api_helpers.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
@@ -51,6 +52,24 @@ namespace extension_web_request_api_helpers {
 namespace {
 
 using ParsedResponseCookies = std::vector<linked_ptr<net::ParsedCookie>>;
+
+// Mirrors the histogram enum of the same name. DO NOT REORDER THESE VALUES OR
+// CHANGE THEIR MEANING.
+enum class WebRequestSpecialHeaderRemoval {
+  kNeither,
+  kAcceptLanguage,
+  kUserAgent,
+  kBoth,
+  kMaxValue = kBoth,
+};
+
+// Mirrors the histogram enum of the same name. DO NOT REORDER THESE VALUES OR
+// CHANGE THEIR MEANING.
+enum class WebRequestResponseHeaderType {
+  kNone,
+  kSetCookie,
+  kMaxValue = kSetCookie,
+};
 
 void ClearCacheOnNavigationOnUI() {
   web_cache::WebCacheManager::GetInstance()->ClearCacheOnNavigation();
@@ -795,6 +814,19 @@ void MergeOnBeforeSendHeadersResponses(
     }
   }
 
+  // See https://crbug.com/827582
+  auto removal = WebRequestSpecialHeaderRemoval::kNeither;
+  bool removed_accept_language = removed_headers.count("Accept-Language");
+  bool removed_user_agent = removed_headers.count("User-Agent");
+  if (removed_accept_language && removed_user_agent)
+    removal = WebRequestSpecialHeaderRemoval::kBoth;
+  else if (removed_accept_language)
+    removal = WebRequestSpecialHeaderRemoval::kAcceptLanguage;
+  else if (removed_user_agent)
+    removal = WebRequestSpecialHeaderRemoval::kUserAgent;
+  UMA_HISTOGRAM_ENUMERATION("Extensions.WebRequest.SpecialHeadersRemoved",
+                            removal);
+
   MergeCookiesInOnBeforeSendHeadersResponses(deltas, request_headers,
                                              conflicting_extensions, logger);
 }
@@ -1002,6 +1034,12 @@ void MergeCookiesInOnHeadersReceivedResponses(
     cookie_modifications_exist |=
         !(*delta)->response_cookie_modifications.empty();
   }
+  // See https://crbug.com/827582
+  UMA_HISTOGRAM_ENUMERATION("Extensions.WebRequest.ModifiedResponseHeaders",
+                            cookie_modifications_exist
+                                ? WebRequestResponseHeaderType::kSetCookie
+                                : WebRequestResponseHeaderType::kNone);
+
   if (!cookie_modifications_exist)
     return;
 
