0efe4982e5b436a07451b89b1e9aa8131d4e2c36
Takashi Toyoshima <toyoshim@chromium.org>
2018-04-03 11:30:34 +0000

    OOR-CORS: use request_initiator for setting preflight Origin header
    Blink should set the request_initiator correctly to use OOR-CORS
    for security checks. Blink had several Origin variants, but now
    the right SecurityOrigin is set to the blink::ResourceRequest's
    RequestorOrigin. If something get broken, we need to modify Blink-site
    to set a suitable origin.
    
    Bug: 803766
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: Ic5e184a58009189f128082c4232526e046e743cc
    Reviewed-on: https://chromium-review.googlesource.com/964103
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547670}

--- services/network/cors/preflight_controller_unittest.cc
+++ services/network/cors/preflight_controller_unittest.cc
@@ -5,8 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/network/cors/preflight_controller.h"
 
+#include "net/http/http_request_headers.h"
 #include "services/network/public/cpp/cors/cors.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "url/origin.h"
 
 namespace network {
 
@@ -16,6 +18,7 @@ namespace {
 
 TEST(PreflightControllerCreatePreflightRequestTest, LexicographicalOrder) {
   ResourceRequest request;
+  request.request_initiator = url::Origin();
   request.headers.SetHeader("Orange", "Orange");
   request.headers.SetHeader("Apple", "Red");
   request.headers.SetHeader("Kiwifruit", "Green");
@@ -26,6 +29,10 @@ TEST(PreflightControllerCreatePreflightRequestTest, LexicographicalOrder) {
       PreflightController::CreatePreflightRequest(request);
 
   std::string header;
+  EXPECT_TRUE(
+      preflight->headers.GetHeader(net::HttpRequestHeaders::kOrigin, &header));
+  EXPECT_EQ("null", header);
+
   EXPECT_TRUE(preflight->headers.GetHeader(
       cors::header_names::kAccessControlRequestHeaders, &header));
   EXPECT_EQ("apple,content-type,kiwifruit,orange,strawberry", header);
@@ -33,6 +40,7 @@ TEST(PreflightControllerCreatePreflightRequestTest, LexicographicalOrder) {
 
 TEST(PreflightControllerCreatePreflightRequestTest, ExcludeSimpleHeaders) {
   ResourceRequest request;
+  request.request_initiator = url::Origin();
   request.headers.SetHeader("Accept", "everything");
   request.headers.SetHeader("Accept-Language", "everything");
   request.headers.SetHeader("Content-Language", "everything");
@@ -52,6 +60,7 @@ TEST(PreflightControllerCreatePreflightRequestTest, ExcludeSimpleHeaders) {
 TEST(PreflightControllerCreatePreflightRequestTest,
      ExcludeSimpleContentTypeHeader) {
   ResourceRequest request;
+  request.request_initiator = url::Origin();
   request.headers.SetHeader("Content-Type", "text/plain");
 
   std::unique_ptr<ResourceRequest> preflight =
@@ -65,6 +74,7 @@ TEST(PreflightControllerCreatePreflightRequestTest,
 
 TEST(PreflightControllerCreatePreflightRequestTest, IncludeNonSimpleHeader) {
   ResourceRequest request;
+  request.request_initiator = url::Origin();
   request.headers.SetHeader("X-Custom-Header", "foobar");
 
   std::unique_ptr<ResourceRequest> preflight =
@@ -79,6 +89,7 @@ TEST(PreflightControllerCreatePreflightRequestTest, IncludeNonSimpleHeader) {
 TEST(PreflightControllerCreatePreflightRequestTest,
      IncludeNonSimpleContentTypeHeader) {
   ResourceRequest request;
+  request.request_initiator = url::Origin();
   request.headers.SetHeader("Content-Type", "application/octet-stream");
 
   std::unique_ptr<ResourceRequest> preflight =
@@ -92,6 +103,7 @@ TEST(PreflightControllerCreatePreflightRequestTest,
 
 TEST(PreflightControllerCreatePreflightRequestTest, ExcludeForbiddenHeaders) {
   ResourceRequest request;
+  request.request_initiator = url::Origin();
   request.headers.SetHeader("referer", "https://www.google.com/");
 
   std::unique_ptr<ResourceRequest> preflight =
