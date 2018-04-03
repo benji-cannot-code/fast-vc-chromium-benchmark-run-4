d5b2451e31b27ea88f22fe2a815067a6b7a1da99
Chris Nardi <cnardi@chromium.org>
2018-04-03 16:53:51 +0000

    Serialize custom property names as identifiers
    Custom property names are defined [1] to be "any valid identifier that
    starts with two dashes." As such, they need to be serialized as
    identifiers, in order to ensure that reparsing generates a valid custom
    property name. Change our serialization of custom property names to use
    SerializeIdentifier().
    
    [1]: https://drafts.csswg.org/css-variables/#typedef-custom-property-name
    
    Bug: 828272
    Change-Id: Icd830b6b3b38f0414877c6d8dec69efa00318bb3
    Reviewed-on: https://chromium-review.googlesource.com/991608
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Chris Nardi <cnardi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547733}

--- third_party/WebKit/Source/core/css/StylePropertySerializer.cpp
+++ third_party/WebKit/Source/core/css/StylePropertySerializer.cpp
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <bitset>
 #include "core/css/CSSCustomPropertyDeclaration.h"
 #include "core/css/CSSIdentifierValue.h"
+#include "core/css/CSSMarkup.h"
 #include "core/css/CSSPendingSubstitutionValue.h"
 #include "core/css/CSSValuePool.h"
 #include "core/css/properties/css_property.h"
@@ -180,7 +181,7 @@ String StylePropertySerializer::GetCustomPropertyText(
     result.Append(' ');
   const CSSCustomPropertyDeclaration* value =
       ToCSSCustomPropertyDeclaration(property.Value());
-  result.Append(value->GetName());
+  SerializeIdentifier(value->GetName(), result, is_not_first_decl);
   result.Append(':');
   if (!value->Value())
     result.Append(' ');
