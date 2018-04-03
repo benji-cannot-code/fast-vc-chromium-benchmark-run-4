b2a120c405cb9d921f723dcca1e00605c474cfa0
Robert Liao <robliao@chromium.org>
2018-04-03 17:41:02 +0000

    Port Views MenuButton Pressed Lock Logic to Cocoa BrowserActionButton
    Before this change, when the user clicks on the button to dismiss the
    Extension Popup, the popup would dismiss and destroy. Next, the
    incoming mouse-up would note that the popup is already gone and show
    the popup.
    
    We want the popup to stay closed if it was dismissed by clicking the
    action button.
    
    The Views Browser doesn't have this problem because MenuButton tracks
    the last time the locked state was released and suppresses any
    activation if it occurs too soon.
    
    This change simply carries similar logic from Views MenuButton to
    Cocoa BrowserActionButton.
    
    BUG=825866
    
    Change-Id: I089f0ab8658277ae8cbf71e2813e62aeb3c5505f
    Reviewed-on: https://chromium-review.googlesource.com/991596
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547755}

--- chrome/browser/ui/cocoa/extensions/browser_action_button.mm
+++ chrome/browser/ui/cocoa/extensions/browser_action_button.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "base/strings/sys_string_conversions.h"
+#include "base/time/time.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_window.h"
@@ -41,6 +42,9 @@ NSString* const kBrowserActionButtonDragEndNotification =
 static const CGFloat kAnimationDuration = 0.2;
 static const CGFloat kMinimumDragDistance = 5;
 
+// Mirrors ui/views/mouse_constants.h for suppressing popup activation.
+static const int kMinimumMsBetweenCloseOpenPopup = 100;
+
 @interface BrowserActionButton ()
 - (void)endDrag;
 - (void)updateHighlightedState;
@@ -62,6 +66,13 @@ class ToolbarActionViewDelegateBridge : public ToolbarActionViewDelegate {
 
   bool user_shown_popup_visible() const { return user_shown_popup_visible_; }
 
+  bool CanShowPopup() const {
+    // If the user clicks on the browser action button to close the bubble,
+    // don't show the next popup too soon on the mouse button up.
+    base::TimeDelta delta = base::TimeTicks::Now() - popup_closed_time_;
+    return delta.InMilliseconds() >= kMinimumMsBetweenCloseOpenPopup;
+  }
+
  private:
   // ToolbarActionViewDelegate:
   content::WebContents* GetCurrentWebContents() const override;
@@ -72,6 +83,11 @@ class ToolbarActionViewDelegateBridge : public ToolbarActionViewDelegate {
 
   // A helper method to implement showing the context menu.
   void DoShowContextMenu();
+
+  // Tracks when the menu was closed so that the button can ignore the incoming
+  // mouse button up event if its too soon. This simulates the same behavior
+  // provided by the views toolkit MenuButton Pressed Locked tracking state.
+  base::TimeTicks popup_closed_time_;
 
   // The owning button. Weak.
   BrowserActionButton* owner_;
@@ -158,6 +174,7 @@ void ToolbarActionViewDelegateBridge::OnPopupShown(bool by_user) {
 }
 
 void ToolbarActionViewDelegateBridge::OnPopupClosed() {
+  popup_closed_time_ = base::TimeTicks::Now();
   user_shown_popup_visible_ = false;
   [owner_ updateHighlightedState];
 }
@@ -331,7 +348,9 @@ void ToolbarActionViewDelegateBridge::DoShowContextMenu() {
                                fromView:nil];
   // Only perform the click if we didn't drag the button.
   if (NSPointInRect(location, [self bounds]) && !isBeingDragged_) {
-    [self performClick:self];
+    if (viewControllerDelegate_->CanShowPopup()) {
+      [self performClick:self];
+    }
   } else {
     // Make sure an ESC to end a drag doesn't trigger 2 endDrags.
     if (isBeingDragged_) {
