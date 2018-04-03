3f5a83c5424b3e0d46731475103cfbaa4eb3b735
Francois Doray <fdoray@chromium.org>
2018-04-03 15:21:42 +0000

    Add LifecycleUnit::Freeze().
    A frozen LifecycleUnit can't use the CPU.
    
    Note: This feature will  be test by a browser test in
    https://chromium-review.googlesource.com/c/chromium/src/+/980742/5/chrome/browser/resource_coordinator/tab_manager_browsertest.cc#814
    
    Bug: 775644
    Change-Id: I5b5aac616e7a5d28b61f379d841da2b75420300d
    Reviewed-on: https://chromium-review.googlesource.com/981396
    Commit-Queue: François Doray <fdoray@chromium.org>
    Reviewed-by: Sébastien Marchand <sebmarchand@chromium.org>
    Reviewed-by: Fadi Meawad <fmeawad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547714}

--- chrome/browser/resource_coordinator/tab_lifecycle_unit.h
+++ chrome/browser/resource_coordinator/tab_lifecycle_unit.h
@@ -76,6 +76,7 @@ class TabLifecycleUnitSource::TabLifecycleUnit
   base::string16 GetTitle() const override;
   std::string GetIconURL() const override;
   SortKey GetSortKey() const override;
+  bool Freeze() override;
   int GetEstimatedMemoryFreedOnDiscardKB() const override;
   bool CanDiscard(DiscardReason reason) const override;
   bool Discard(DiscardReason discard_reason) override;
@@ -85,6 +86,7 @@ class TabLifecycleUnitSource::TabLifecycleUnit
   bool IsMediaTab() const override;
   bool IsAutoDiscardable() const override;
   void SetAutoDiscardable(bool auto_discardable) override;
+  bool FreezeTab() override;
   bool DiscardTab() override;
   bool IsDiscarded() const override;
   int GetDiscardCount() const override;
