f28347d1baf5074015a4327cca7973730d809a92
Lei Zhang <thestig@chromium.org>
2018-04-03 18:47:19 +0000

    PDF Viewer: Add metric for PDF file size.
    BUG=827329
    
    Change-Id: I3ae863360df1b0374e367c094ca2c0d724c615c9
    Reviewed-on: https://chromium-review.googlesource.com/986740
    Reviewed-by: dsinclair <dsinclair@chromium.org>
    Reviewed-by: Gayane Petrosyan <gayane@chromium.org>
    Commit-Queue: dsinclair <dsinclair@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547773}

--- pdf/preview_mode_client.cc
+++ pdf/preview_mode_client.cc
@@ -137,7 +137,8 @@ void PreviewModeClient::DocumentPaintOccurred() {
 }
 
 void PreviewModeClient::DocumentLoadComplete(
-    const PDFEngine::DocumentFeatures& document_features) {
+    const PDFEngine::DocumentFeatures& document_features,
+    uint32_t file_size) {
   client_->PreviewDocumentLoadComplete();
 }
 
