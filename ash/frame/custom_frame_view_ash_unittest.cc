4991e0f02e5b5425d691c76665688485dd79ae37
Mitsuru Oshima <oshima@chromium.org>
2018-04-03 16:36:09 +0000

    Add copyright to icon files, sort BUILD.gn
    BUG=822960
    TEST=None
    
    Change-Id: I63cb34bab20ba4801bed00ffd997063e6e45ef1e
    Reviewed-on: https://chromium-review.googlesource.com/981909
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547731}

--- ash/frame/custom_frame_view_ash_unittest.cc
+++ ash/frame/custom_frame_view_ash_unittest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/frame/header_view.h"
 #include "ash/public/cpp/vector_icons/vector_icons.h"
 #include "ash/public/cpp/window_properties.h"
+#include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/shell.h"
 #include "ash/test/ash_test_base.h"
 #include "ash/wm/overview/window_selector_controller.h"
--- ash/frame/default_frame_header.cc
+++ ash/frame/default_frame_header.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/frame/frame_header_util.h"
 #include "ash/public/cpp/vector_icons/vector_icons.h"
 #include "ash/resources/grit/ash_resources.h"
+#include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/shell.h"
 #include "ash/wm/tablet_mode/tablet_mode_controller.h"
 #include "base/debug/leak_annotations.h"
