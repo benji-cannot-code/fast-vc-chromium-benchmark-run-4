cc2c7a663d999d4156e2910b740968b5d8333fd3
Francois Doray <fdoray@chromium.org>
2018-04-03 18:37:11 +0000

    Use LifecycleUnits for tab discarding (reland).
    This CL previously landed as
    https://chromium-review.googlesource.com/c/chromium/src/+/842306
    It was reverted because TabLifecycleUnits were not always
    destroyed when tabs were closed, which caused invalid memory
    accesses. This was solved by
    https://chromium-review.googlesource.com/c/chromium/src/+/980587
    
    TabManager discards tabs, but also ARC processes.
    LifecycleUnit is an abstraction to help us having one
    code path for all discards.
    
    TBR=chrisha@chromium.org,thestig@chromium.org
    
    Bug: 775644
    Change-Id: Ib7f3dfd8b7e9b43ad75020c65e12d94229ba1a07
    Reviewed-on: https://chromium-review.googlesource.com/980742
    Reviewed-by: François Doray <fdoray@chromium.org>
    Reviewed-by: Sébastien Marchand <sebmarchand@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547769}

--- chrome/browser/resource_coordinator/tab_manager_browsertest.cc
+++ chrome/browser/resource_coordinator/tab_manager_browsertest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/media/webrtc/media_capture_devices_dispatcher.h"
 #include "chrome/browser/media/webrtc/media_stream_capture_indicator.h"
+#include "chrome/browser/resource_coordinator/tab_lifecycle_unit_external.h"
 #include "chrome/browser/resource_coordinator/tab_manager.h"
 #include "chrome/browser/resource_coordinator/tab_manager_web_contents_data.h"
 #include "chrome/browser/resource_coordinator/time.h"
@@ -49,11 +50,16 @@ namespace resource_coordinator {
 
 namespace {
 
-static constexpr char kBlinkPageLifecycleFeature[] = "PageLifecycle";
+constexpr char kBlinkPageLifecycleFeature[] = "PageLifecycle";
+constexpr base::TimeDelta kShortDelay = base::TimeDelta::FromSeconds(1);
 
 class TabManagerTest : public InProcessBrowserTest {
  public:
-  TabManagerTest() : scoped_set_tick_clock_for_testing_(&test_clock_) {}
+  TabManagerTest() : scoped_set_tick_clock_for_testing_(&test_clock_) {
+    // Start with a non-null TimeTicks, as there is no discard protection for
+    // a tab with a null focused timestamp.
+    test_clock_.Advance(kShortDelay);
+  }
 
   void SetUpCommandLine(base::CommandLine* command_line) override {
     command_line->AppendSwitchASCII(switches::kEnableBlinkFeatures,
@@ -112,6 +118,8 @@ IN_PROC_BROWSER_TEST_F(TabManagerTest, TabManagerBasics) {
   TabManager* tab_manager = g_browser_process->GetTabManager();
 
   // Get three tabs open.
+
+  test_clock_.Advance(kShortDelay);
   WindowedNotificationObserver load1(
       content::NOTIFICATION_NAV_ENTRY_COMMITTED,
       content::NotificationService::AllSources());
@@ -121,6 +129,7 @@ IN_PROC_BROWSER_TEST_F(TabManagerTest, TabManagerBasics) {
   browser()->OpenURL(open1);
   load1.Wait();
 
+  test_clock_.Advance(kShortDelay);
   WindowedNotificationObserver load2(
       content::NOTIFICATION_NAV_ENTRY_COMMITTED,
       content::NotificationService::AllSources());
@@ -130,6 +139,7 @@ IN_PROC_BROWSER_TEST_F(TabManagerTest, TabManagerBasics) {
   browser()->OpenURL(open2);
   load2.Wait();
 
+  test_clock_.Advance(kShortDelay);
   WindowedNotificationObserver load3(
       content::NOTIFICATION_NAV_ENTRY_COMMITTED,
       content::NotificationService::AllSources());
@@ -191,9 +201,11 @@ IN_PROC_BROWSER_TEST_F(TabManagerTest, TabManagerBasics) {
 
   // Kill the third tab after making second tab active.
   tsm->ActivateTabAt(1, true);
+
   EXPECT_EQ(1, tsm->active_index());
   EXPECT_FALSE(tab_manager->IsTabDiscarded(tsm->GetWebContentsAt(1)));
-  tab_manager->DiscardWebContentsAt(2, tsm, DiscardReason::kProactive);
+  FastForwardAfterDiscardProtectionTime();
+  tab_manager->DiscardTabImpl(DiscardReason::kProactive);
   EXPECT_TRUE(tab_manager->IsTabDiscarded(tsm->GetWebContentsAt(2)));
 
   // Force creation of the FindBarController.
@@ -798,6 +810,8 @@ IN_PROC_BROWSER_TEST_F(TabManagerTest,
       "TabManager.Discarding.DiscardedTabCouldFastShutdown", false, 1);
 }
 
+// TODO(fdoray): Move this test to |tab_lifecyle_unit_unittest.cc|.
+// https://crbug.com/775644
 IN_PROC_BROWSER_TEST_F(TabManagerTest, FreezeTab) {
   const char kMainFrameFrozenStateJS[] =
       "window.domAutomationController.send(mainFrameFreezeCount);";
@@ -863,8 +877,7 @@ IN_PROC_BROWSER_TEST_F(TabManagerTest, FreezeTab) {
   EXPECT_EQ(0, freeze_count_result);
 
   // Freeze the tab. If it fails then we might be freezing a visible tab.
-  g_browser_process->GetTabManager()->FreezeWebContentsAt(
-      freezing_index, browser()->tab_strip_model());
+  TabLifecycleUnitExternal::FromWebContents(content)->FreezeTab();
 
   // freeze_count_result should be exactly 1 for both frames. The valus is
   // incremented in the onfreeze callback. If it is >1, then the callback was
@@ -894,8 +907,9 @@ IN_PROC_BROWSER_TEST_F(TabManagerTest, TabManagerWasDiscarded) {
   EXPECT_FALSE(not_discarded_result);
 
   // Discard the tab. This simulates a tab discard.
-  g_browser_process->GetTabManager()->DiscardWebContentsAt(
-      0, browser()->tab_strip_model(), DiscardReason::kProactive);
+  TabLifecycleUnitExternal::FromWebContents(
+      browser()->tab_strip_model()->GetActiveWebContents())
+      ->DiscardTab();
 
   // Here we simulate re-focussing the tab causing reload with navigation,
   // the navigation will reload the tab.
@@ -951,8 +965,7 @@ IN_PROC_BROWSER_TEST_F(TabManagerTest,
   EXPECT_FALSE(before_discard_childframe_result);
 
   // Discard the tab. This simulates a tab discard.
-  g_browser_process->GetTabManager()->DiscardWebContentsAt(
-      0, browser()->tab_strip_model(), DiscardReason::kProactive);
+  TabLifecycleUnitExternal::FromWebContents(contents)->DiscardTab();
 
   // Here we simulate re-focussing the tab causing reload with navigation,
   // the navigation will reload the tab.
