771165988df53ac90e104eec4c63216fed5d5938
Gabriel Charette <gab@chromium.org>
2018-04-03 15:46:43 +0000

    Use modern base APIs to avoid base::MessageLoop::current() calls in /chrome/browser/sessions
    This is a precusor cleanup extracted from
    https://chromium-review.googlesource.com/c/chromium/src/+/957760/7
    in the broader effort to restrict usage of MessageLoop::current().
    
    This CL was uploaded by git cl split.
    
    R=sky@chromium.org
    
    Bug: 825327
    Change-Id: Icf533bc3f199e16156ab0ec895ff5b2684b95967
    Reviewed-on: https://chromium-review.googlesource.com/986746
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547718}

--- chrome/browser/sessions/session_service_unittest.cc
+++ chrome/browser/sessions/session_service_unittest.cc
@@ -987,12 +987,6 @@ void OnGotPreviousSession(
   FAIL() << "SessionService was destroyed, this shouldn't be reached.";
 }
 
-void PostBackToThread(base::MessageLoop* message_loop,
-                      base::RunLoop* run_loop) {
-  message_loop->task_runner()->PostTask(
-      FROM_HERE, base::Bind(&base::RunLoop::Quit, base::Unretained(run_loop)));
-}
-
 // Blocks until |keep_waiting| is false.
 void SimulateWaitForTesting(const base::AtomicFlag* flag) {
   // Ideally this code would use WaitableEvent, but that triggers a DCHECK in
@@ -1025,11 +1019,7 @@ TEST_F(SessionServiceTest, GetSessionsAndDestroy) {
       FROM_HERE, base::Bind(&SimulateWaitForTesting, base::Unretained(&flag)));
   service()->GetLastSession(base::Bind(&OnGotPreviousSession),
                             &cancelable_task_tracker);
-  helper_.RunTaskOnBackendThread(
-      FROM_HERE,
-      base::Bind(&PostBackToThread,
-                 base::Unretained(base::MessageLoop::current()),
-                 base::Unretained(&run_loop)));
+  helper_.RunTaskOnBackendThread(FROM_HERE, run_loop.QuitClosure());
   delete helper_.ReleaseService();
   flag.Set();
   run_loop.Run();
