9f404e27185210a75f4294fc94ad845eea59a8ba
Jacob Dufault <jdufault@google.com>
2018-04-03 19:09:21 +0000

    cros: Store UserContext in QuickUnlockStorage.
    Adding a key to cryptohome requires the associated UserContext. This
    adds an AuthToken to quick_unlock which stores the UserContext
    instance. The AuthToken automatically expires after a delay.
    
    Bug: 623344
    Change-Id: Ife3373599fb560dfe9f9055fcda54a9f001c6ac7
    Reviewed-on: https://chromium-review.googlesource.com/980738
    Commit-Queue: Jacob Dufault <jdufault@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547785}

--- chrome/browser/chromeos/extensions/quick_unlock_private/quick_unlock_private_api.cc
+++ chrome/browser/chromeos/extensions/quick_unlock_private/quick_unlock_private_api.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/stl_util.h"
 #include "chrome/browser/chromeos/login/easy_unlock/easy_unlock_service.h"
+#include "chrome/browser/chromeos/login/quick_unlock/auth_token.h"
 #include "chrome/browser/chromeos/login/quick_unlock/quick_unlock_factory.h"
 #include "chrome/browser/chromeos/login/quick_unlock/quick_unlock_storage.h"
 #include "chrome/browser/chromeos/login/supervised/supervised_user_authentication.h"
@@ -41,6 +42,7 @@ using CredentialCheck = quick_unlock_private::CredentialCheck;
 using CredentialRequirements = quick_unlock_private::CredentialRequirements;
 using QuickUnlockMode = quick_unlock_private::QuickUnlockMode;
 
+using AuthToken = chromeos::quick_unlock::AuthToken;
 using QuickUnlockModeList = std::vector<QuickUnlockMode>;
 using QuickUnlockStorage = chromeos::quick_unlock::QuickUnlockStorage;
 
@@ -254,8 +256,8 @@ void QuickUnlockPrivateGetAuthTokenFunction::OnAuthSuccess(
   Profile* profile = Profile::FromBrowserContext(browser_context());
   QuickUnlockStorage* quick_unlock_storage =
       chromeos::quick_unlock::QuickUnlockFactory::GetForProfile(profile);
-  result->token = quick_unlock_storage->CreateAuthToken();
-  result->lifetime_seconds = QuickUnlockStorage::kTokenExpirationSeconds;
+  result->token = quick_unlock_storage->CreateAuthToken(user_context);
+  result->lifetime_seconds = AuthToken::kTokenExpirationSeconds;
 
   Respond(ArgumentList(
       quick_unlock_private::GetAuthToken::Results::Create(*result)));
@@ -396,8 +398,8 @@ QuickUnlockPrivateGetCredentialRequirementsFunction::Run() {
 QuickUnlockPrivateSetModesFunction::QuickUnlockPrivateSetModesFunction()
     : chrome_details_(this) {}
 
-QuickUnlockPrivateSetModesFunction::~QuickUnlockPrivateSetModesFunction() {
-}
+QuickUnlockPrivateSetModesFunction::~QuickUnlockPrivateSetModesFunction() =
+    default;
 
 void QuickUnlockPrivateSetModesFunction::SetModesChangedEventHandlerForTesting(
     const ModesChangedEventHandler& handler) {
