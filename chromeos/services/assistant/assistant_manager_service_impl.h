168ae77f40931e0f6522b9c5613964a114844015
David Black <dmblack@google.com>
2018-04-03 17:38:12 +0000

    Wires up recognition state change events to AshAssistantController.
    Bug: b:77282340
    Change-Id: I88ea25128043ec69ec88357f36babfae36fe88fd
    Reviewed-on: https://chromium-review.googlesource.com/986921
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/master@{#547752}

--- chromeos/services/assistant/assistant_manager_service_impl.h
+++ chromeos/services/assistant/assistant_manager_service_impl.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/services/assistant/platform_api_impl.h"
 #include "chromeos/services/assistant/public/mojom/assistant.mojom.h"
 #include "libassistant/contrib/core/macros.h"
+#include "libassistant/shared/public/conversation_state_listener.h"
 #include "mojo/public/cpp/bindings/interface_ptr_set.h"
 
 namespace assistant_client {
@@ -31,7 +32,8 @@ namespace assistant {
 class AssistantManagerServiceImpl
     : public AssistantManagerService,
       public ::chromeos::assistant::action::AssistantActionObserver,
-      public AssistantEventObserver {
+      public AssistantEventObserver,
+      public assistant_client::ConversationStateListener {
  public:
   explicit AssistantManagerServiceImpl(mojom::AudioInputPtr audio_input);
   ~AssistantManagerServiceImpl() override;
@@ -56,6 +58,12 @@ class AssistantManagerServiceImpl
   // AssistantEventObserver overrides:
   void OnSpeechLevelUpdated(float speech_level) override;
 
+  // assistant_client::ConversationStateListener overrides:
+  void OnRecognitionStateChanged(
+      assistant_client::ConversationStateListener::RecognitionState state,
+      const assistant_client::ConversationStateListener::RecognitionResult&
+          recognition_result) override;
+
  private:
   void StartAssistantInternal(const std::string& access_token,
                               const std::string& arc_version);
@@ -66,6 +74,10 @@ class AssistantManagerServiceImpl
       const std::vector<std::string>& suggestions);
   void OnShowTextOnMainThread(const std::string& text);
   void OnOpenUrlOnMainThread(const std::string& url);
+  void OnRecognitionStateChangedOnMainThread(
+      assistant_client::ConversationStateListener::RecognitionState state,
+      const assistant_client::ConversationStateListener::RecognitionResult&
+          recognition_result);
   void OnSpeechLevelUpdatedOnMainThread(const float speech_level);
 
   bool running_ = false;
