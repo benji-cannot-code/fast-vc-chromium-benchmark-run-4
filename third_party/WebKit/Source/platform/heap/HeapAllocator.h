59d6687710b8e7e5e3ac05dbf3076d5da1f61644
Michael Lippautz <mlippautz@chromium.org>
2018-04-04 13:25:01 +0000

    [oilpan] Faster write barrier guard
    Add a general counter for threads that are currently in incremental
    marking.
    
    This guard can be checked as first reason for bailing out in any write
    barrier as it only requires a single load and does not need to go
    through TLS or page lookups.
    
    Bug: chromium:757440
    Change-Id: Ida63e560bb52ccd66da739139c0510229b05a550
    Reviewed-on: https://chromium-review.googlesource.com/995272
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548033}

--- third_party/WebKit/Source/platform/heap/HeapAllocator.h
+++ third_party/WebKit/Source/platform/heap/HeapAllocator.h
@@ -131,7 +131,7 @@ class PLATFORM_EXPORT HeapAllocator {
 
   static void BackingWriteBarrier(void* address) {
 #if BUILDFLAG(BLINK_HEAP_INCREMENTAL_MARKING)
-    if (!address)
+    if (!address || !ThreadState::IsAnyIncrementalMarking())
       return;
     ThreadState* state = PageFromObject(address)->Arena()->GetThreadState();
     state->Heap().WriteBarrier(address);
@@ -215,6 +215,8 @@ class PLATFORM_EXPORT HeapAllocator {
   template <typename T, typename Traits>
   static void NotifyNewObject(T* object) {
 #if BUILDFLAG(BLINK_HEAP_INCREMENTAL_MARKING)
+    if (!ThreadState::IsAnyIncrementalMarking())
+      return;
     // The object may have been in-place constructed as part of a large object.
     // It is not safe to retrieve the page from the object here.
     ThreadState* const thread_state = ThreadState::Current();
@@ -240,6 +242,8 @@ class PLATFORM_EXPORT HeapAllocator {
   template <typename T, typename Traits>
   static void NotifyNewObjects(T* array, size_t len) {
 #if BUILDFLAG(BLINK_HEAP_INCREMENTAL_MARKING)
+    if (!ThreadState::IsAnyIncrementalMarking())
+      return;
     // The object may have been in-place constructed as part of a large object.
     // It is not safe to retrieve the page from the object here.
     ThreadState* const thread_state = ThreadState::Current();
--- third_party/WebKit/Source/platform/heap/ThreadState.cpp
+++ third_party/WebKit/Source/platform/heap/ThreadState.cpp
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <limits>
 #include <memory>
 
+#include "base/atomicops.h"
 #include "base/location.h"
 #include "base/trace_event/process_memory_dump.h"
 #include "build/build_config.h"
@@ -1234,6 +1235,21 @@ void ThreadState::InvokePreFinalizers() {
   }
 }
 
+// static
+base::subtle::AtomicWord ThreadState::incremental_marking_counter_ = 0;
+
+void ThreadState::EnableIncrementalMarkingBarrier() {
+  CHECK(!IsIncrementalMarking());
+  base::subtle::Barrier_AtomicIncrement(&incremental_marking_counter_, 1);
+  SetIncrementalMarking(true);
+}
+
+void ThreadState::DisableIncrementalMarkingBarrier() {
+  CHECK(IsIncrementalMarking());
+  base::subtle::Barrier_AtomicIncrement(&incremental_marking_counter_, -1);
+  SetIncrementalMarking(false);
+}
+
 void ThreadState::IncrementalMarkingStart() {
   VLOG(2) << "[state:" << this << "] "
           << "IncrementalMarking: Start";
@@ -1244,7 +1260,7 @@ void ThreadState::IncrementalMarkingStart() {
   MarkPhasePrologue(BlinkGC::kNoHeapPointersOnStack,
                     BlinkGC::kIncrementalMarking, BlinkGC::kIdleGC);
   MarkPhaseVisitRoots();
-  Heap().EnableIncrementalMarkingBarrier();
+  EnableIncrementalMarkingBarrier();
   ScheduleIncrementalMarkingStep();
   DCHECK(IsMarkingInProgress());
 }
@@ -1269,6 +1285,7 @@ void ThreadState::IncrementalMarkingFinalize() {
   VLOG(2) << "[state:" << this << "] "
           << "IncrementalMarking: Finalize";
   SetGCState(kNoGCScheduled);
+  DisableIncrementalMarkingBarrier();
   AtomicPauseScope atomic_pause_scope(this);
   DCHECK(IsMarkingInProgress());
   RecursiveMutexLocker persistent_lock(
@@ -1277,7 +1294,6 @@ void ThreadState::IncrementalMarkingFinalize() {
   bool complete =
       MarkPhaseAdvanceMarking(std::numeric_limits<double>::infinity());
   CHECK(complete);
-  Heap().DisableIncrementalMarkingBarrier();
   MarkPhaseEpilogue(current_gc_data_.marking_type);
   PreSweep(current_gc_data_.marking_type, BlinkGC::kLazySweeping);
   DCHECK(IsSweepingInProgress());
