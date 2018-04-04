4ca164f626f561097690a6604844ecfc486d0c05
Anna Offenwanger <offenwanger@chromium.org>
2018-04-04 19:07:41 +0000

    Add UMA for XR PageSession StartAction
    Adding in a UMA that logs what triggered the entry into Chrome in VR.
    
    Bug: 808589
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_vr;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I6bc9e9501fd86428e37db53853fc638fa00c1e73
    Reviewed-on: https://chromium-review.googlesource.com/989055
    Reviewed-by: Yash Malik <ymalik@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Bill Orr <billorr@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Anna Offenwanger <offenwanger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548150}

--- chrome/browser/vr/metrics/session_metrics_helper.h
+++ chrome/browser/vr/metrics/session_metrics_helper.h
@@ -19,8 +19,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace vr {
 
 // This enum describes various ways a Chrome VR session started.
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
+// Ensure that this stays in sync with VRSessionStartAction in enums.xml
 // GENERATED_JAVA_ENUM_PACKAGE: org.chromium.chrome.browser.vr_shell
-enum class PageSessionStartAction : int {
+enum class VrStartAction : int {
   // The user activated a headset. For example, inserted phone in Daydream, or
   // put on an Occulus or Vive.
   kHeadsetActivation = 1,
@@ -32,6 +35,7 @@ enum class PageSessionStartAction : int {
   // Chrome VR was started by an intent from another app. Most likely the user
   // clicked the icon in Daydream home.
   kIntentLaunch = 4,
+  kVrStartActionLast = kIntentLaunch,
 };
 
 // SessionTimer will monitor the time between calls to StartSession and
@@ -138,7 +142,7 @@ class SessionMetricsHelper : public content::WebContentsObserver {
   void RecordVoiceSearchStarted();
   void RecordUrlRequested(GURL url, NavigationMethod method);
 
-  void RecordVrStartAction(PageSessionStartAction action);
+  void RecordVrStartAction(VrStartAction action);
   void ReportRequestPresent();
 
  private:
@@ -161,7 +165,7 @@ class SessionMetricsHelper : public content::WebContentsObserver {
   void SetVrMode(Mode mode);
   void UpdateMode();
 
-  void MaybeSetPageSessionStartAction(PageSessionStartAction action);
+  void MaybeSetVrStartAction(VrStartAction action);
 
   void OnEnterAnyVr();
   void OnExitAllVr();
@@ -191,7 +195,7 @@ class SessionMetricsHelper : public content::WebContentsObserver {
   GURL last_requested_url_;
   NavigationMethod last_url_request_method_;
 
-  base::Optional<PageSessionStartAction> pending_page_session_start_action_;
+  base::Optional<VrStartAction> pending_page_session_start_action_;
 
   int num_videos_playing_ = 0;
   int num_session_navigation_ = 0;
