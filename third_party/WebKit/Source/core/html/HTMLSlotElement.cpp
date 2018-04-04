3dc4ccd02269ce37e7ef541e542896cc9196f3c8
Hayato Ito <hayato@chromium.org>
2018-04-04 11:55:50 +0000

    Refactor HTMLSlotElement to clarify which parts are related to Incremental Shadow DOM
    No behavior change.
    
    Bug: 776656
    Change-Id: Ib5b7d88edd3752828f83d13b1e7a026cbd5a9aa9
    Reviewed-on: https://chromium-review.googlesource.com/989525
    Commit-Queue: Hayato Ito <hayato@chromium.org>
    Reviewed-by: Takayoshi Kochi <kochi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548023}

--- third_party/WebKit/Source/core/html/HTMLSlotElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLSlotElement.cpp
@@ -202,6 +202,7 @@ void HTMLSlotElement::AppendAssignedNode(Node& host_child) {
 }
 
 void HTMLSlotElement::RecalcDistributedNodes() {
+  DCHECK(!RuntimeEnabledFeatures::IncrementalShadowDOMEnabled());
   for (auto& node : assigned_nodes_) {
     DCHECK(node->IsSlotable());
     if (HTMLSlotElement* slot =
@@ -217,12 +218,14 @@ void HTMLSlotElement::RecalcDistributedNodes() {
 }
 
 void HTMLSlotElement::AppendDistributedNode(Node& node) {
+  DCHECK(!RuntimeEnabledFeatures::IncrementalShadowDOMEnabled());
   size_t size = distributed_nodes_.size();
   distributed_nodes_.push_back(&node);
   distributed_indices_.Set(&node, size);
 }
 
 void HTMLSlotElement::AppendDistributedNodesFrom(const HTMLSlotElement& other) {
+  DCHECK(!RuntimeEnabledFeatures::IncrementalShadowDOMEnabled());
   size_t index = distributed_nodes_.size();
   distributed_nodes_.AppendVector(other.distributed_nodes_);
   for (const auto& node : other.distributed_nodes_)
@@ -232,6 +235,12 @@ void HTMLSlotElement::AppendDistributedNodesFrom(const HTMLSlotElement& other) {
 void HTMLSlotElement::ClearAssignedNodes() {
   DCHECK(RuntimeEnabledFeatures::IncrementalShadowDOMEnabled());
   assigned_nodes_.clear();
+}
+
+void HTMLSlotElement::ClearAssignedNodesAndFlatTreeChildren() {
+  DCHECK(RuntimeEnabledFeatures::IncrementalShadowDOMEnabled());
+  assigned_nodes_.clear();
+  flat_tree_children_.clear();
 }
 
 void HTMLSlotElement::RecalcFlatTreeChildren() {
@@ -253,12 +262,14 @@ void HTMLSlotElement::RecalcFlatTreeChildren() {
 }
 
 void HTMLSlotElement::ClearDistribution() {
+  DCHECK(!RuntimeEnabledFeatures::IncrementalShadowDOMEnabled());
   assigned_nodes_.clear();
   distributed_nodes_.clear();
   distributed_indices_.clear();
 }
 
 void HTMLSlotElement::SaveAndClearDistribution() {
+  DCHECK(!RuntimeEnabledFeatures::IncrementalShadowDOMEnabled());
   old_distributed_nodes_.swap(distributed_nodes_);
   ClearDistribution();
 }
@@ -450,7 +461,11 @@ void HTMLSlotElement::RemovedFrom(ContainerNode* insertion_point) {
     // This slot was in a shadow tree and got disconnected from the shadow tree
     insertion_point->ContainingShadowRoot()->GetSlotAssignment().DidRemoveSlot(
         *this);
-    ClearDistribution();
+    if (RuntimeEnabledFeatures::IncrementalShadowDOMEnabled()) {
+      ClearAssignedNodesAndFlatTreeChildren();
+    } else {
+      ClearDistribution();
+    }
   }
 
   HTMLElement::RemovedFrom(insertion_point);
@@ -485,6 +500,7 @@ void HTMLSlotElement::DidRecalcStyle(StyleRecalcChange change) {
 }
 
 void HTMLSlotElement::UpdateDistributedNodesWithFallback() {
+  DCHECK(!RuntimeEnabledFeatures::IncrementalShadowDOMEnabled());
   if (!distributed_nodes_.IsEmpty())
     return;
   for (auto& child : NodeTraversal::ChildrenOf(*this)) {
@@ -537,6 +553,7 @@ void HTMLSlotElement::LazyReattachNodesByDynamicProgramming(
 }
 
 void HTMLSlotElement::LazyReattachDistributedNodesIfNeeded() {
+  DCHECK(!RuntimeEnabledFeatures::IncrementalShadowDOMEnabled());
   // TODO(hayato): Move this probe to a better place.
   probe::didPerformSlotDistribution(this);
 
--- third_party/WebKit/Source/core/html/HTMLSlotElement.h
+++ third_party/WebKit/Source/core/html/HTMLSlotElement.h
@@ -65,34 +65,10 @@ class CORE_EXPORT HTMLSlotElement final : public HTMLElement {
     return nodes.IsEmpty() ? nullptr : nodes.back().Get();
   }
 
-  Node* FirstDistributedNode() const {
-    DCHECK(!RuntimeEnabledFeatures::IncrementalShadowDOMEnabled());
-    DCHECK(SupportsAssignment());
-    return distributed_nodes_.IsEmpty() ? nullptr
-                                        : distributed_nodes_.front().Get();
-  }
-  Node* LastDistributedNode() const {
-    DCHECK(!RuntimeEnabledFeatures::IncrementalShadowDOMEnabled());
-    DCHECK(SupportsAssignment());
-    return distributed_nodes_.IsEmpty() ? nullptr
-                                        : distributed_nodes_.back().Get();
-  }
-
   Node* AssignedNodeNextTo(const Node&) const;
   Node* AssignedNodePreviousTo(const Node&) const;
 
-  Node* DistributedNodeNextTo(const Node&) const;
-  Node* DistributedNodePreviousTo(const Node&) const;
-
   void AppendAssignedNode(Node&);
-
-  void RecalcDistributedNodes();
-  void AppendDistributedNode(Node&);
-  void AppendDistributedNodesFrom(const HTMLSlotElement& other);
-
-  void UpdateDistributedNodesWithFallback();
-
-  void LazyReattachDistributedNodesIfNeeded();
 
   void AttachLayoutTree(AttachContext&) final;
   void DetachLayoutTree(const AttachContext& = AttachContext()) final;
@@ -109,9 +85,6 @@ class CORE_EXPORT HTMLSlotElement final : public HTMLElement {
   bool HasAssignedNodesSlow() const;
   bool FindHostChildWithSameSlotName() const;
 
-  void ClearDistribution();
-  void SaveAndClearDistribution();
-
   bool SupportsAssignment() const { return IsInV1ShadowTree(); }
 
   void CheckFallbackAfterInsertedIntoShadowTree();
@@ -123,10 +96,6 @@ class CORE_EXPORT HTMLSlotElement final : public HTMLElement {
   void DispatchSlotChangeEvent();
   void ClearSlotChangeEventEnqueued() { slotchange_event_enqueued_ = false; }
 
-  // For Incremental Shadow DOM
-  void ClearAssignedNodes();
-  void RecalcFlatTreeChildren();
-
   static AtomicString NormalizeSlotName(const AtomicString&);
 
   // For User-Agent Shadow DOM
@@ -134,6 +103,34 @@ class CORE_EXPORT HTMLSlotElement final : public HTMLElement {
   static const AtomicString& UserAgentDefaultSlotName();
 
   virtual void Trace(blink::Visitor*);
+
+  // For Incremental Shadow DOM
+  void ClearAssignedNodes();
+  void RecalcFlatTreeChildren();
+
+  // For Non-Incremental Shadow DOM
+  void RecalcDistributedNodes();
+  void AppendDistributedNode(Node&);
+  void AppendDistributedNodesFrom(const HTMLSlotElement& other);
+  void UpdateDistributedNodesWithFallback();
+  void LazyReattachDistributedNodesIfNeeded();
+  void ClearDistribution();
+  void SaveAndClearDistribution();
+  Node* DistributedNodeNextTo(const Node&) const;
+  Node* DistributedNodePreviousTo(const Node&) const;
+
+  Node* FirstDistributedNode() const {
+    DCHECK(!RuntimeEnabledFeatures::IncrementalShadowDOMEnabled());
+    DCHECK(SupportsAssignment());
+    return distributed_nodes_.IsEmpty() ? nullptr
+                                        : distributed_nodes_.front().Get();
+  }
+  Node* LastDistributedNode() const {
+    DCHECK(!RuntimeEnabledFeatures::IncrementalShadowDOMEnabled());
+    DCHECK(SupportsAssignment());
+    return distributed_nodes_.IsEmpty() ? nullptr
+                                        : distributed_nodes_.back().Get();
+  }
 
  private:
   HTMLSlotElement(Document&);
@@ -161,7 +158,10 @@ class CORE_EXPORT HTMLSlotElement final : public HTMLElement {
 
   const HeapVector<Member<Node>>& GetDistributedNodes();
 
+  void ClearAssignedNodesAndFlatTreeChildren();
+
   HeapVector<Member<Node>> assigned_nodes_;
+  bool slotchange_event_enqueued_ = false;
 
   // For IncrementalShadowDOM
   HeapVector<Member<Node>> flat_tree_children_;
@@ -170,8 +170,6 @@ class CORE_EXPORT HTMLSlotElement final : public HTMLElement {
   HeapVector<Member<Node>> distributed_nodes_;
   HeapVector<Member<Node>> old_distributed_nodes_;
   HeapHashMap<Member<const Node>, size_t> distributed_indices_;
-
-  bool slotchange_event_enqueued_ = false;
 
   // TODO(hayato): Move this to more appropriate directory (e.g. platform/wtf)
   // if there are more than one usages.
