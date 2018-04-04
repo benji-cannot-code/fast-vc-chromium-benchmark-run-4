d27471253ab0ea66293a1a76e4a650cdb8deb644
Ran Ji <ranj@chromium.org>
2018-04-04 23:16:00 +0000

    Add a fallback for load library failures in Android K-.
    Sometime Android system fails to extract library. Manually extract them to
    cache directory and load as needed.
    
    Bug: 806998
    Change-Id: I7ccff04435cb5201e410c71bde741e52687329ab
    Reviewed-on: https://chromium-review.googlesource.com/930332
    Commit-Queue: Ran Ji <ranj@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: agrieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548237}

--- base/android/java/src/org/chromium/base/library_loader/Linker.java
+++ base/android/java/src/org/chromium/base/library_loader/Linker.java
@@ -5,13 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.base.library_loader;
 
+import android.annotation.SuppressLint;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Parcel;
 import android.os.ParcelFileDescriptor;
 import android.os.Parcelable;
 
+import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
+import org.chromium.base.ResourceExtractor;
 import org.chromium.base.annotations.AccessedByNative;
 
 import java.util.HashMap;
@@ -447,13 +450,23 @@ public abstract class Linker {
     /**
      * Load the Linker JNI library. Throws UnsatisfiedLinkError on error.
      */
+    @SuppressLint({"UnsafeDynamicallyLoadedCode"})
     protected static void loadLinkerJniLibrary() {
         LibraryLoader.setEnvForNative();
-        String libName = "lib" + LINKER_JNI_LIBRARY + ".so";
         if (DEBUG) {
+            String libName = "lib" + LINKER_JNI_LIBRARY + ".so";
             Log.i(TAG, "Loading " + libName);
         }
-        System.loadLibrary(LINKER_JNI_LIBRARY);
+        try {
+            System.loadLibrary(LINKER_JNI_LIBRARY);
+            LibraryLoader.incrementRelinkerCountNotHitHistogram();
+        } catch (UnsatisfiedLinkError e) {
+            if (ResourceExtractor.PLATFORM_REQUIRES_NATIVE_FALLBACK_EXTRACTION) {
+                System.load(LibraryLoader.getExtractedLibraryPath(
+                        ContextUtils.getApplicationContext(), LINKER_JNI_LIBRARY));
+                LibraryLoader.incrementRelinkerCountHitHistogram();
+            }
+        }
     }
 
     /**
