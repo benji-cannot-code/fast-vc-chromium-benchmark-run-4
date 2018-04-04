b3001d6e6758312533293a34f64595feb93cb819
David Tseng <dtseng@chromium.org>
2018-04-04 17:16:32 +0000

    Fixes sendSyntheticKeyEvent containing modifiers
    Modifiers specified as:
    { ctrl: true }
    worked, but
    { ctrl: false }
    
    got interpreted as having ctrl down (as the code only checked the presence of the value and not its boolean state).
    
    Change-Id: Icec8959441c193d548fcaf45b163c41fdb351472
    Reviewed-on: https://chromium-review.googlesource.com/985967
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548106}

---
+++ chrome/browser/accessibility/accessibility_extension_api_browsertest.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/ash_config.h"
+#include "chrome/browser/extensions/extension_apitest.h"
+#include "chrome/browser/ui/tabs/tab_strip_model.h"
+
+namespace extensions {
+
+using AccessibilityPrivateApiTest = ExtensionApiTest;
+
+#if defined(OS_CHROMEOS)
+IN_PROC_BROWSER_TEST_F(AccessibilityPrivateApiTest, SendSyntheticKeyEvent) {
+  // Not yet supported on mash.
+  if (chromeos::GetAshConfig() == ash::Config::MASH)
+    return;
+
+  ASSERT_TRUE(RunExtensionSubtest("accessibility_private/",
+                                  "send_synthetic_key_event.html"))
+      << message_;
+}
+#endif  // defined (OS_CHROMEOS)
+
+}  // namespace extensions
