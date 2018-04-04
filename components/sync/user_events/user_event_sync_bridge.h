f6bb503556c9d23a8fe830ed8eb9d45e55aaf320
Christian Dullweber <dullweber@chromium.org>
2018-04-04 18:21:52 +0000

    Add queue for UserEventSyncBridge
    Implement an in-memory queue for UserEventSyncBridge to keep user consent
    events while the store or change processor are not ready yet. The queue
    will be processed as soon as store and change processor are initialized.
    
    Bug: 781765
    
    Change-Id: I0a7fb25d66dce548c0f1ae9cf5f99e869ea54ddc
    Reviewed-on: https://chromium-review.googlesource.com/980975
    Reviewed-by: Gayane Petrosyan <gayane@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548132}

--- components/sync/user_events/user_event_sync_bridge.h
+++ components/sync/user_events/user_event_sync_bridge.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <memory>
 #include <string>
+#include <vector>
 
 #include "base/macros.h"
 #include "base/optional.h"
@@ -47,6 +48,10 @@ class UserEventSyncBridge : public ModelTypeSyncBridge {
   void RecordUserEvent(std::unique_ptr<sync_pb::UserEventSpecifics> specifics);
 
  private:
+  void RecordUserEventImpl(
+      std::unique_ptr<sync_pb::UserEventSpecifics> specifics);
+  void ProcessQueuedEvents();
+
   void OnStoreCreated(const base::Optional<ModelError>& error,
                       std::unique_ptr<ModelTypeStore> store);
   void OnReadAllMetadata(const base::Optional<ModelError>& error,
@@ -65,6 +70,11 @@ class UserEventSyncBridge : public ModelTypeSyncBridge {
   // Persistent storage for in flight events. Should remain quite small, as we
   // delete upon commit confirmation.
   std::unique_ptr<ModelTypeStore> store_;
+
+  // Used to store important events while the store or change processor are not
+  // ready. This currently only handles user consents.
+  std::vector<std::unique_ptr<sync_pb::UserEventSpecifics>>
+      deferred_user_events_while_initializing_;
 
   // The key is the global_id of the navigation the event is linked to.
   std::multimap<int64_t, sync_pb::UserEventSpecifics>
