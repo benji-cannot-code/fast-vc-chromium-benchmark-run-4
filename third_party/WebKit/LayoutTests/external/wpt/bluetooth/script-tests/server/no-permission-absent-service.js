eb5d9c8ad2097348c806ac3b84fb674ed690d0c5
Conley Owens <cco3@chromium.org>
2018-04-04 17:54:14 +0000

    bluetooth: web: test: Use AllResponsesConsumed
    This change uses AllResponsesConsumed in the bluetooth Web Platform
    Tests in order to make sure we have consumed all the anticipatory
    responses that we've set on our fakes.
    
    BUG=569709
    
    Change-Id: I9767792fcd5dda71fabf2f8941638f744aefa2dc
    Reviewed-on: https://chromium-review.googlesource.com/988422
    Commit-Queue: Conley Owens <cco3@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548125}

--- third_party/WebKit/LayoutTests/external/wpt/bluetooth/script-tests/server/no-permission-absent-service.js
+++ third_party/WebKit/LayoutTests/external/wpt/bluetooth/script-tests/server/no-permission-absent-service.js
@@ -8,7 +8,7 @@ const expected = new DOMException(
     'https://goo.gl/HxfxSQ',
     'SecurityError');
 
-bluetooth_test(() => getHealthThermometerDevice({
+bluetooth_test(() => getConnectedHealthThermometerDevice({
       filters: [{services: ['health_thermometer']}]
     })
     .then(({device}) => Promise.all([
