5dcbb650d6a4427bb79a61dc7b9fef198f75edc4
Stephen Lanham <slan@google.com>
2018-04-04 01:26:28 +0000

    [Chromecast] Add WebBluetooth support to Cast platforms (part 2 of N).
    Introduce better lifetime management for BluetoothAdapterCast, allowing
    the Cast embedder code to inject a factory for this class. This prevents
    potential races at destruct time.
    
    Also wires up the code to make Characteristic notifications work. With
    this patch (and some hacks in content), a JS application can receive
    notifications successfully.
    
    BUG=827672
    
    Bug: b/75967216
    Test: test page
    Change-Id: Idb8a1ba2c9ad94a80940083d31e307b3506f98e6
    Reviewed-on: https://chromium-review.googlesource.com/988265
    Commit-Queue: Stephen Lanham <slan@chromium.org>
    Reviewed-by: Conley Owens <cco3@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547922}

--- device/bluetooth/cast/bluetooth_remote_gatt_service_cast.cc
+++ device/bluetooth/cast/bluetooth_remote_gatt_service_cast.cc
@@ -9,11 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/bind.h"
-#include "chromecast/device/bluetooth/bluetooth_util.h"
 #include "chromecast/device/bluetooth/le/remote_characteristic.h"
 #include "chromecast/device/bluetooth/le/remote_service.h"
 #include "device/bluetooth/cast/bluetooth_device_cast.h"
 #include "device/bluetooth/cast/bluetooth_remote_gatt_characteristic_cast.h"
+#include "device/bluetooth/cast/bluetooth_utils.h"
 
 namespace device {
 
@@ -35,12 +35,11 @@ BluetoothRemoteGattServiceCast::BluetoothRemoteGattServiceCast(
 BluetoothRemoteGattServiceCast::~BluetoothRemoteGattServiceCast() {}
 
 std::string BluetoothRemoteGattServiceCast::GetIdentifier() const {
-  return chromecast::bluetooth::util::UuidToString(remote_service_->uuid());
+  return GetUUID().canonical_value();
 }
 
 BluetoothUUID BluetoothRemoteGattServiceCast::GetUUID() const {
-  return BluetoothUUID(
-      chromecast::bluetooth::util::UuidToString(remote_service_->uuid()));
+  return UuidToBluetoothUUID(remote_service_->uuid());
 }
 
 bool BluetoothRemoteGattServiceCast::IsPrimary() const {
