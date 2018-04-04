8ec0b7be74d936af59d22472f9cd973c6cc1fe9d
Gabriel Charette <gab@chromium.org>
2018-04-04 18:48:04 +0000

    Migrate MessageLoop::ScopedNestableTaskAllower to RunLoop::Type::kNestableTasksAllowed in /base
    This pass focuses on ML::SNTA in same scope as an anonymous RunLoop().RunUntilIdle()
    Scripted via a small tweak of https://bugs.chromium.org/p/chromium/issues/detail?id=781352#c1
    which focused on RunLoops with a variable name.
    
    The script should have replaced a MessageLoop::ScopedNestableTaskAllower
    in the same scope as a RunLoop with a RunLoop of
    Type::kNestableTasksAllowed. If there was a comment above the
    MessageLoop::ScopedNestableTaskAllower, it should have been moved
    alongside the augmented RunLoop.
    
    Includes should have been stripped if the was the last usage of message_loop.h
    in that file.
    
    Please CQ if LGTY
    
    This CL was uploaded by git cl split.
    
    R=thakis@chromium.org
    
    Bug: 781352
    Change-Id: I0cb8cd0bbe6161bc706a5fc81d73c1d41ddcc775
    Reviewed-on: https://chromium-review.googlesource.com/995974
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548141}

--- base/message_loop/message_loop_unittest.cc
+++ base/message_loop/message_loop_unittest.cc
@@ -1258,10 +1258,7 @@ namespace {
 
 void FuncThatPumps(TaskList* order, int cookie) {
   order->RecordStart(PUMPS, cookie);
-  {
-    MessageLoop::ScopedNestableTaskAllower allow(MessageLoop::current());
-    RunLoop().RunUntilIdle();
-  }
+  RunLoop(RunLoop::Type::kNestableTasksAllowed).RunUntilIdle();
   order->RecordEnd(PUMPS, cookie);
 }
 
