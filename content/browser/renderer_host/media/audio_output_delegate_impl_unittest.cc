2006c0c525dfd2dd1cedd1f42eca6fffd9fecc0c
Gabriel Charette <gab@chromium.org>
2018-04-04 19:45:54 +0000

    Migrate MessageLoop::ScopedNestableTaskAllower to RunLoop::Type::kNestableTasksAllowed in /content/browser/renderer_host/media
    This pass focuses on ML::SNTA in same scope as an anonymous RunLoop().RunUntilIdle()
    Scripted via a small tweak of https://bugs.chromium.org/p/chromium/issues/detail?id=781352#c1
    which focused on RunLoops with a variable name.
    
    The script should have replaced a MessageLoop::ScopedNestableTaskAllower
    in the same scope as a RunLoop with a RunLoop of
    Type::kNestableTasksAllowed. If there was a comment above the
    MessageLoop::ScopedNestableTaskAllower, it should have been moved
    alongside the augmented RunLoop.
    
    Includes should have been stripped if the was the last usage of message_loop.h
    in that file.
    
    Please CQ if LGTY
    
    This CL was uploaded by git cl split.
    
    R=xhwang@chromium.org
    
    Bug: 781352
    Change-Id: Iae32d4a5cbb177de34391d8cf99e423e4d465cdb
    Reviewed-on: https://chromium-review.googlesource.com/995921
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548161}

--- content/browser/renderer_host/media/audio_output_delegate_impl_unittest.cc
+++ content/browser/renderer_host/media/audio_output_delegate_impl_unittest.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/command_line.h"
 #include "base/memory/ptr_util.h"
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
 #include "base/sync_socket.h"
 #include "content/browser/media/capture/audio_mirroring_manager.h"
@@ -594,11 +593,7 @@ class AudioOutputDelegateTest : public testing::Test {
     // New tasks might be posted while we are syncing, but in every iteration at
     // least one task will be run. 20 iterations should be enough for our code.
     for (int i = 0; i < 20; ++i) {
-      {
-        base::MessageLoop::ScopedNestableTaskAllower allower(
-            base::MessageLoop::current());
-        base::RunLoop().RunUntilIdle();
-      }
+      base::RunLoop(base::RunLoop::Type::kNestableTasksAllowed).RunUntilIdle();
       SyncWith(BrowserThread::GetTaskRunnerForThread(BrowserThread::UI));
       SyncWith(audio_manager_->GetWorkerTaskRunner());
     }
