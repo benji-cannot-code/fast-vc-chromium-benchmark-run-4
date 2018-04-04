06a6c9588f14c4e198a04276a507559e6739486e
Ken MacKay <kmackay@chromium.org>
2018-04-04 22:47:43 +0000

    [Chromecast] Support mono input in mixer
    Bug: internal b/77586397
    
    Change-Id: I1a40b631bc3666e38008a367458737a4564f93a7
    Reviewed-on: https://chromium-review.googlesource.com/994135
    Reviewed-by: Stephen Lanham <slan@chromium.org>
    Commit-Queue: Kenneth MacKay <kmackay@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548224}

--- chromecast/media/cma/backend/direct_mixer_source.cc
+++ chromecast/media/cma/backend/direct_mixer_source.cc
@@ -16,8 +16,6 @@ namespace media {
 
 namespace {
 
-const int kNumOutputChannels = 2;
-
 std::string AudioContentTypeToString(media::AudioContentType type) {
   switch (type) {
     case media::AudioContentType::kAlarm:
@@ -35,11 +33,9 @@ std::string AudioContentTypeToString(media::AudioContentType type) {
 DirectAudioSourceToken* CastMediaShlib::AddDirectAudioSource(
     DirectAudioSource* source,
     const MediaPipelineDeviceParams& params,
-    int source_sample_rate,
     int playout_channel) {
   DCHECK(source);
-  return new DirectMixerSource(source, params, source_sample_rate,
-                               playout_channel);
+  return new DirectMixerSource(source, params, playout_channel);
 }
 
 // static
@@ -50,11 +46,10 @@ void CastMediaShlib::RemoveDirectAudioSource(DirectAudioSourceToken* token) {
 
 DirectMixerSource::DirectMixerSource(DirectAudioSource* direct_source,
                                      const MediaPipelineDeviceParams& params,
-                                     int source_sample_rate,
                                      int playout_channel)
     : source_(direct_source),
-      num_channels_(kNumOutputChannels),
-      input_samples_per_second_(source_sample_rate),
+      num_channels_(source_->GetNumChannels()),
+      input_samples_per_second_(source_->GetSampleRate()),
       primary_(params.audio_type !=
                MediaPipelineDeviceParams::kAudioStreamSoundEffects),
       device_id_(params.device_id),
@@ -62,7 +57,6 @@ DirectMixerSource::DirectMixerSource(DirectAudioSource* direct_source,
       playout_channel_(playout_channel),
       mixer_(StreamMixer::Get()),
       channel_vector_(num_channels_) {
-  DCHECK(source_);
   LOG(INFO) << "Create " << device_id_ << " (" << this
             << "), content type = " << AudioContentTypeToString(content_type_);
   DCHECK(source_);
