f30cc4c54d165b0bdb388a515cf2bd29177b01ef
Justin Cohen <justincohen@google.com>
2018-04-04 14:25:32 +0000

    [ios] Make NTP really full screen.
    Short term solution to long term problem of pulling the NTP out of native content.
    For now, convince //ios/web to show the NTP native content as full screen (including
    under the status bar) by sending a negative offset to web when calculating the frame
    of the native content.
    
    Bug: 819460
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Ia4d2417aafad0afbc5dd62ffbcd35156deb3f8ad
    Reviewed-on: https://chromium-review.googlesource.com/981233
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548047}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_layout_handset.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_layout_handset.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_layout_handset.h"
 
 #import "ios/chrome/browser/ui/ntp/new_tab_page_header_constants.h"
+#import "ios/chrome/browser/ui/uikit_ui_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -64,7 +65,13 @@ layoutAttributesForSupplementaryViewOfKind:(NSString*)kind
     attributes.zIndex = 1000;
 
     // Prevent the fake omnibox from scrolling up off of the screen.
-    CGFloat minY = headerHeight - ntp_header::kMinHeaderHeight;
+    CGFloat topSafeArea = 0;
+    if (@available(iOS 11, *)) {
+      topSafeArea = self.collectionView.safeAreaInsets.top;
+    } else {
+      topSafeArea = StatusBarHeight();
+    }
+    CGFloat minY = headerHeight - ntp_header::kMinHeaderHeight - topSafeArea;
     if (contentOffset.y > minY)
       origin.y = contentOffset.y - minY;
     attributes.frame = {origin, attributes.frame.size};
--- ios/chrome/browser/ui/ntp/new_tab_page_header_view.mm
+++ ios/chrome/browser/ui/ntp/new_tab_page_header_view.mm
@@ -103,7 +103,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [_shadow setAlpha:0];
 }
 
-- (CGFloat)searchFieldProgressForOffset:(CGFloat)offset {
+- (CGFloat)searchFieldProgressForOffset:(CGFloat)offset
+                         safeAreaInsets:(UIEdgeInsets)safeAreaInsets {
   NOTREACHED();
   return 0;
 }
