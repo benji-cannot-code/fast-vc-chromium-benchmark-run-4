707cf7e539df31e180ffbe77913653f21c4f63ab
Sasha Morrissey <sashab@chromium.org>
2018-04-04 23:53:37 +0000

    Fix typo in files app files_metadata_box
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ie30ec32ff379dca98f964e2a0de0e786eec8026f
    Reviewed-on: https://chromium-review.googlesource.com/994498
    Reviewed-by: Naoki Fukino <fukino@chromium.org>
    Commit-Queue: Sasha Morrissey <sashab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548256}

--- ui/file_manager/file_manager/foreground/elements/files_metadata_box.js
+++ ui/file_manager/file_manager/foreground/elements/files_metadata_box.js
@@ -10,7 +10,7 @@ var FilesMetadataBox = Polymer({
     // File media type, e.g. image, video.
     type: String,
     size: String,
-    modiifcationTime: String,
+    modificationTime: String,
     filePath: String,
     mediaMimeType: String,
 
@@ -76,7 +76,7 @@ var FilesMetadataBox = Polymer({
    */
   clear: function(keepSizeFields) {
     this.type = '';
-    this.modiifcationTime = '';
+    this.modificationTime = '';
     this.mediaMimeType = '';
     this.filePath = '';
 
