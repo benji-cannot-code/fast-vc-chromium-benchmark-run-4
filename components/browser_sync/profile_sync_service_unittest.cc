8a0addf1a43368e40b06d1c7ddd6588bbe31cc6a
Marc Treib <treib@chromium.org>
2018-04-04 15:28:21 +0000

    FakeProfileOAuth2TokenService: fix auto_post_fetch_response vs manual response
    FakeProfileOAuth2TokenService has two modes of operation:
    auto_post_fetch_response mode, which means it'll automatically post
    successful responses to all access token requests to the message loop,
    or regular (manual) mode, where the user calls IssueToken*/IssueError*
    etc. This CL adds DCHECKs to make sure the two modes are not mixed.
    It also fixes some ProfileSyncService tests which did just that.
    
    Bug: 825190
    Change-Id: Ibed800c7defa09fb8efbf8cb8a56608731aaf70e
    Reviewed-on: https://chromium-review.googlesource.com/995434
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548067}

--- components/browser_sync/profile_sync_service_unittest.cc
+++ components/browser_sync/profile_sync_service_unittest.cc
@@ -650,6 +650,10 @@ TEST_F(ProfileSyncServiceTest, ClearDataOnSignOut) {
 
 // Verify that credential errors get returned from GetAuthError().
 TEST_F(ProfileSyncServiceTest, CredentialErrorReturned) {
+  // This test needs to manually send access tokens (or errors), so disable
+  // automatic replies to access token requests.
+  auth_service()->set_auto_post_fetch_response_on_message_loop(false);
+
   CreateService(ProfileSyncService::AUTO_START);
   IssueTestTokens();
   ExpectDataTypeManagerCreation(1, GetDefaultConfigureCalledCallback());
@@ -664,6 +668,8 @@ TEST_F(ProfileSyncServiceTest, CredentialErrorReturned) {
       signin_manager()->GetAuthenticatedAccountId();
   auth_service()->LoadCredentials(primary_account_id);
   base::RunLoop().RunUntilIdle();
+  auth_service()->IssueAllTokensForAccount(primary_account_id, "access token",
+                                           base::Time::Max());
   ASSERT_FALSE(service()->GetAccessTokenForTest().empty());
   ASSERT_EQ(GoogleServiceAuthError::NONE, service()->GetAuthError().state());
 
@@ -686,6 +692,10 @@ TEST_F(ProfileSyncServiceTest, CredentialErrorReturned) {
 // Verify that credential errors get cleared when a new token is fetched
 // successfully.
 TEST_F(ProfileSyncServiceTest, CredentialErrorClearsOnNewToken) {
+  // This test needs to manually send access tokens (or errors), so disable
+  // automatic replies to access token requests.
+  auth_service()->set_auto_post_fetch_response_on_message_loop(false);
+
   CreateService(ProfileSyncService::AUTO_START);
   IssueTestTokens();
   ExpectDataTypeManagerCreation(1, GetDefaultConfigureCalledCallback());
@@ -700,6 +710,8 @@ TEST_F(ProfileSyncServiceTest, CredentialErrorClearsOnNewToken) {
       signin_manager()->GetAuthenticatedAccountId();
   auth_service()->LoadCredentials(primary_account_id);
   base::RunLoop().RunUntilIdle();
+  auth_service()->IssueAllTokensForAccount(primary_account_id, "access token",
+                                           base::Time::Max());
   ASSERT_FALSE(service()->GetAccessTokenForTest().empty());
   ASSERT_EQ(GoogleServiceAuthError::NONE, service()->GetAuthError().state());
 
