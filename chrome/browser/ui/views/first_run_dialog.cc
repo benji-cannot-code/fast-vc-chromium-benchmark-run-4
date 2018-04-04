6445df679b1c38a6348c39aba8a3a9e9c1ff8ae7
Gabriel Charette <gab@chromium.org>
2018-04-04 17:46:26 +0000

    Use modern base APIs to avoid base::MessageLoop::current() calls in first_run_dialog.cc
    This is one more identical change as https://chromium-review.googlesource.com/c/chromium/src/+/986535
    in chrome/browser/ui/views which I'd missed in that CL.
    
    TBR=sky@chromium.org
    
    Bug: 825327
    Change-Id: I6e3c8a1dbf0028c842fce9db1e4e1d4e648974f2
    Reviewed-on: https://chromium-review.googlesource.com/996152
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548123}

--- chrome/browser/ui/views/first_run_dialog.cc
+++ chrome/browser/ui/views/first_run_dialog.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/bind.h"
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
 #include "chrome/browser/first_run/first_run.h"
 #include "chrome/browser/metrics/metrics_reporting_state.h"
@@ -60,9 +59,7 @@ void FirstRunDialog::Show(Profile* profile) {
   FirstRunDialog* dialog = new FirstRunDialog(profile);
   views::DialogDelegate::CreateDialogWidget(dialog, NULL, NULL)->Show();
 
-  base::MessageLoopForUI* loop = base::MessageLoopForUI::current();
-  base::MessageLoopForUI::ScopedNestableTaskAllower allow_nested(loop);
-  base::RunLoop run_loop;
+  base::RunLoop run_loop(base::RunLoop::Type::kNestableTasksAllowed);
   dialog->quit_runloop_ = run_loop.QuitClosure();
   run_loop.Run();
 }
