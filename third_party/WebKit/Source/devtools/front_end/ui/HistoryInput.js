8c904e139a5e8421477a1dbe358bac2094439b9f
Pavel Feldman <pfeldman@chromium.org>
2018-04-04 23:06:53 +0000

    DevTools: make search in sources show up in drawer again.
    Change-Id: Ib2a034a4ee2b38ced8e1dc51247a49eee244aede
    Reviewed-on: https://chromium-review.googlesource.com/991152
    Commit-Queue: Pavel Feldman <pfeldman@chromium.org>
    Reviewed-by: Joel Einbinder <einbinder@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548231}

--- third_party/WebKit/Source/devtools/front_end/ui/HistoryInput.js
+++ third_party/WebKit/Source/devtools/front_end/ui/HistoryInput.js
@@ -6,10 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @unrestricted
  */
 UI.HistoryInput = class extends HTMLInputElement {
-  constructor() {
-    super();
-  }
-
   /**
    * @return {!UI.HistoryInput}
    */
