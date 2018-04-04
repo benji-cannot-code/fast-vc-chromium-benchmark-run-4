fb0136acceff4cc79da9657c205700080f044c85
Fredrik Söderquist <fs@opera.com>
2018-04-04 14:39:58 +0000

    Revert "Serialize 'oblique' computed values for 'font-style'"
    This reverts commit 755a9214dc9116a826f8881851c07e352004a6c5.
    
    Reason for revert: Mid-flight collision with 162e9dda47d694596620b69c5fb23cc7f0f87126 - will update and reland.
    
    Original change's description:
    > Serialize 'oblique' computed values for 'font-style'
    > 
    > Handling of 'oblique' values were missing in
    > ComputedStyleUtils::ValueForFontStyle - serialize as 'oblique <angle>'.
    > Because of this the return value needs to be widened to CSSValue.
    > 
    > Bug: 809935, 809938
    > Change-Id: I1de846595ed955455d57ab9f6ea45326e502a65b
    > Reviewed-on: https://chromium-review.googlesource.com/995273
    > Reviewed-by: Dominik Röttsches <drott@chromium.org>
    > Commit-Queue: Fredrik Söderquist <fs@opera.com>
    > Cr-Commit-Position: refs/heads/master@{#548044}
    
    TBR=fs@opera.com,drott@chromium.org,futhark@chromium.org
    
    Change-Id: I379a152245f16fc792c8bfc55a2d8b127ba30820
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 809935, 809938
    Reviewed-on: https://chromium-review.googlesource.com/995695
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#548052}

--- third_party/WebKit/Source/core/css/properties/ComputedStyleUtils.cpp
+++ third_party/WebKit/Source/core/css/properties/ComputedStyleUtils.cpp
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/css/CSSCounterValue.h"
 #include "core/css/CSSCustomIdentValue.h"
 #include "core/css/CSSFontFamilyValue.h"
-#include "core/css/CSSFontStyleRangeValue.h"
 #include "core/css/CSSFunctionValue.h"
 #include "core/css/CSSGridLineNamesValue.h"
 #include "core/css/CSSInitialValue.h"
@@ -747,7 +746,8 @@ CSSPrimitiveValue* ComputedStyleUtils::ValueForFontStretch(
                                    CSSPrimitiveValue::UnitType::kPercentage);
 }
 
-CSSValue* ComputedStyleUtils::ValueForFontStyle(const ComputedStyle& style) {
+CSSIdentifierValue* ComputedStyleUtils::ValueForFontStyle(
+    const ComputedStyle& style) {
   FontSelectionValue angle = style.GetFontDescription().Style();
   if (angle == NormalSlopeValue()) {
     return CSSIdentifierValue::Create(CSSValueNormal);
@@ -757,14 +757,8 @@ CSSValue* ComputedStyleUtils::ValueForFontStyle(const ComputedStyle& style) {
     return CSSIdentifierValue::Create(CSSValueItalic);
   }
 
-  // The spec says: 'The lack of a number represents an angle of
-  // "20deg"', but since we compute that to 'italic' (handled above),
-  // we don't perform any special treatment of that value here.
-  CSSValueList* oblique_values = CSSValueList::CreateSpaceSeparated();
-  oblique_values->Append(
-      *CSSPrimitiveValue::Create(angle, CSSPrimitiveValue::UnitType::kDegrees));
-  return CSSFontStyleRangeValue::Create(
-      *CSSIdentifierValue::Create(CSSValueOblique), *oblique_values);
+  NOTREACHED();
+  return CSSIdentifierValue::Create(CSSValueNormal);
 }
 
 CSSPrimitiveValue* ComputedStyleUtils::ValueForFontWeight(
