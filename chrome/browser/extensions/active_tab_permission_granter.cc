66d99ef8fcd45cf37d061af31a31bed99ab24c95
Karan Bhatia <karandeepb@chromium.org>
2018-04-05 00:41:12 +0000

    Extensions: Gate activeTab with file urls on having explicit file access.
    When an extension is granted tab permission using activeTab (in response to say
    clicking on its browser action), the extension is granted permission to the
    tab's origin for the duration of the tab lifetime.
    
    When this happens for a tab with a file url loaded, the extension gets
    permission to the file scheme on the tab. This allows, for example, the
    extension to read the contents of the page using apis like
    chrome.tabs.executeScript. For file urls, this is not ideal since this does not
    respect the "Allow access to file URLs" extension setting.
    
    This CL changes this behavior, gating the access to the file scheme on the tab,
    on the extension having explicit file access. This CL also adds extensive test
    coverage for the behavior of tabs.executeScript on pages with file urls loaded
    into them.
    
    BUG=816685
    
    Change-Id: I9175bb1883006fe594a93262c6825a962c285037
    Reviewed-on: https://chromium-review.googlesource.com/994264
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548269}

--- chrome/browser/extensions/active_tab_permission_granter.cc
+++ chrome/browser/extensions/active_tab_permission_granter.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "chrome/browser/extensions/extension_action_runner.h"
+#include "chrome/browser/extensions/extension_util.h"
 #include "chrome/browser/profiles/profile.h"
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/navigation_handle.h"
@@ -117,7 +118,14 @@ void ActiveTabPermissionGranter::GrantIfRequested(const Extension* extension) {
   if (should_grant_active_tab &&
       (permissions_data->HasWithheldImpliedAllHosts() ||
        permissions_data->HasAPIPermission(APIPermission::kActiveTab))) {
-    new_hosts.AddOrigin(UserScript::ValidUserScriptSchemes(),
+    // Gate activeTab for file urls on extensions having explicit access to file
+    // urls.
+    int valid_schemes = UserScript::ValidUserScriptSchemes();
+    if (!util::AllowFileAccess(extension->id(),
+                               web_contents()->GetBrowserContext())) {
+      valid_schemes &= ~URLPattern::SCHEME_FILE;
+    }
+    new_hosts.AddOrigin(valid_schemes,
                         web_contents()->GetVisibleURL().GetOrigin());
     new_apis.insert(APIPermission::kTab);
   }
