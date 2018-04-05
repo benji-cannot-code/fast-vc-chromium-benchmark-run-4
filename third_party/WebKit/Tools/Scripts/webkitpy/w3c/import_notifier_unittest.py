2017c23ebd691a624c4579b9f50aa979342a5bb6
Robert Ma <robertma@chromium.org>
2018-04-05 23:19:27 +0000

    [WPT Import] Handle Unicode commit subjects correctly
    Use a Unicode template string for formatting commit lists, because the
    commit subjects can contain non-ASCII characters (and they are Unicode
    strings because the underlying host.executive automatically decodes the
    subprocess outputs by default).
    
    There are definitely other places that might break when seeing non-ASCII
    characters, but nothing can be found around here.
    
    By the way, MockExecutive is changed to match Executive more closely:
    run_command now decodes the output and returns a Unicode string by
    default. This change isn't required to fix the bug, and doesn't catch
    other bugs for now, but hopefully it might be useful in the future.
    
    Bug: 827502
    Change-Id: Ib46bfe4cc18d23c2298dfd44bb6ab907dd83e987
    Reviewed-on: https://chromium-review.googlesource.com/998676
    Reviewed-by: Quinten Yearsley <qyearsley@chromium.org>
    Commit-Queue: Robert Ma <robertma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548608}

--- third_party/WebKit/Tools/Scripts/webkitpy/w3c/import_notifier_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/w3c/import_notifier_unittest.py
@@ -1,4 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# -*- coding: utf-8 -*-
 # Copyright 2017 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -131,7 +132,9 @@ class ImportNotifierTest(unittest.TestCase):
         self.assertEqual(self.notifier.new_failures_by_directory, {})
 
     def test_format_commit_list(self):
-        imported_commits = [('SHA1', 'Subject 1'), ('SHA2', 'Subject 2')]
+        imported_commits = [('SHA1', 'Subject 1'),
+                            # Use non-ASCII chars to really test Unicode handling.
+                            ('SHA2', u'ABC~‾¥≈¤･・•∙·☼★星🌟星★☼·∙•・･¤≈¥‾~XYZ')]
 
         def _is_commit_affecting_directory(commit, directory):
             self.assertIn(commit, ('SHA1', 'SHA2'))
@@ -141,8 +144,8 @@ class ImportNotifierTest(unittest.TestCase):
         self.local_wpt.is_commit_affecting_directory = _is_commit_affecting_directory
         self.assertEqual(
             self.notifier.format_commit_list(imported_commits, '/mock-checkout/third_party/WebKit/LayoutTests/external/wpt/foo'),
-            'Subject 1: https://github.com/w3c/web-platform-tests/commit/SHA1 [affecting this directory]\n'
-            'Subject 2: https://github.com/w3c/web-platform-tests/commit/SHA2\n'
+            u'Subject 1: https://github.com/w3c/web-platform-tests/commit/SHA1 [affecting this directory]\n'
+            u'ABC~‾¥≈¤･・•∙·☼★星🌟星★☼·∙•・･¤≈¥‾~XYZ: https://github.com/w3c/web-platform-tests/commit/SHA2\n'
         )
 
     def test_find_owned_directory_non_virtual(self):
