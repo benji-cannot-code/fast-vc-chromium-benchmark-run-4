3f0b7ef190cdeee16ac77ad38df878f1fed6486a
Lei Zhang <thestig@chromium.org>
2018-04-05 00:41:08 +0000

    Use version_info::GetVersion() where appropriate.
    Instead of writing base::Version(version_info::GetVersionNumber()).
    
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I0a2c0826ffced29d22151bac51537da227b6dd95
    Reviewed-on: https://chromium-review.googlesource.com/989092
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548268}

--- ios/chrome/browser/ui/promos/signin_promo_view_controller.mm
+++ ios/chrome/browser/ui/promos/signin_promo_view_controller.mm
@@ -161,7 +161,7 @@ NSSet* GaiaIdSetWithIdentities(NSArray* identities) {
 }
 
 + (base::Version)currentVersion {
-  base::Version currentVersion(version_info::GetVersionNumber());
+  base::Version currentVersion = version_info::GetVersion();
   DCHECK(currentVersion.IsValid());
   return currentVersion;
 }
