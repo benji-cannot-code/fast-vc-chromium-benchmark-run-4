f23a622f54338bfb5eb4c02dbd2a4587c33b2e83
Dale Curtis <dalecurtis@chromium.org>
2018-04-05 21:57:04 +0000

    Add suspend/resume tests for MSE.
    This adds three tests: before metadata, after metadata, and after
    have enough. It doesn't clone the have future data test that src=
    has since has_future_data == has_enough_data when using MSE.
    
    BUG=756897
    TEST=all tests!
    
    Change-Id: I99c2cff3668c471bcd3d3d4549e53690788b0813
    Reviewed-on: https://chromium-review.googlesource.com/989073
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548575}

--- third_party/WebKit/LayoutTests/media/suspend-util.js
+++ third_party/WebKit/LayoutTests/media/suspend-util.js
@@ -1,4 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Requests that |video| suspends upon reaching or exceeding |expectedState|;
+// |callback| will be called once the suspend is detected.
 function suspendMediaElement(video, expectedState, callback) {
   var pollSuspendState = function() {
     if (!window.internals.isMediaElementSuspended(video)) {
@@ -13,10 +15,8 @@ function suspendMediaElement(video, expectedState, callback) {
   window.internals.forceStaleStateForMediaElement(video, expectedState);
 }
 
-function preloadMetadataSuspendTest(t, video, src, expectSuspend) {
-  assert_true(!!window.internals, 'This test requires windows.internals.');
-  video.onerror = t.unreached_func();
-
+// Calls play() on |video| and executes t.done() when currentTime > 0.
+function completeTestUponPlayback(t, video) {
   var timeWatcher = t.step_func(function() {
     if (video.currentTime > 0) {
       assert_false(window.internals.isMediaElementSuspended(video));
@@ -26,6 +26,14 @@ function preloadMetadataSuspendTest(t, video, src, expectSuspend) {
     }
   });
 
+  window.requestAnimationFrame(timeWatcher);
+  video.play();
+}
+
+function preloadMetadataSuspendTest(t, video, src, expectSuspend) {
+  assert_true(!!window.internals, 'This test requires windows.internals.');
+  video.onerror = t.unreached_func();
+
   var eventListener = t.step_func(function() {
     assert_equals(expectSuspend,
                   window.internals.isMediaElementSuspended(video));
@@ -34,8 +42,7 @@ function preloadMetadataSuspendTest(t, video, src, expectSuspend) {
       return;
     }
 
-    window.requestAnimationFrame(timeWatcher);
-    video.play();
+    completeTestUponPlayback(t, video);
   });
 
   video.addEventListener('loadedmetadata', eventListener, false);
@@ -46,21 +53,11 @@ function suspendTest(t, video, src, expectedState) {
   assert_true(!!window.internals, 'This test requires windows.internals.');
   video.onerror = t.unreached_func();
 
-  var timeWatcher = t.step_func(function() {
-    if (video.currentTime > 0) {
-      assert_false(window.internals.isMediaElementSuspended(video));
-      t.done();
-    } else {
-      window.requestAnimationFrame(timeWatcher);
-    }
-  });
-
   // We can't force a suspend state until loading has started.
   video.addEventListener('loadstart', t.step_func(function() {
     suspendMediaElement(video, expectedState, t.step_func(function() {
       assert_true(window.internals.isMediaElementSuspended(video));
-      window.requestAnimationFrame(timeWatcher);
-      video.play();
+      completeTestUponPlayback(t, video);
     }));
   }), false);
 
