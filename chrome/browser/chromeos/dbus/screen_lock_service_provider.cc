1a8b07b876efb0e38f3e6f718952e48f06807a45
Daniel Erat <derat@chromium.org>
2018-04-05 21:51:19 +0000

    chromeos: Add org.chromium.ScreenLockService D-Bus service.
    Add a new org.chromium.ScreenLockService D-Bus service that
    exports a single ShowLockScreen method. This uses the
    existing ScreenLockServiceProvider class that already
    exports an identical LockScreen method on
    org.chromium.LibCrosService.
    
    The org.chromium.LibCrosService method will need to continue
    to be exported until session_manager has been updated to use
    org.chromium.ScreenLockService instead.
    
    Bug: 827680
    Change-Id: I8c907f4a3304ed9f1bb3953a138b10b9ce90363e
    Reviewed-on: https://chromium-review.googlesource.com/998437
    Commit-Queue: Dan Erat <derat@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548571}

--- chrome/browser/chromeos/dbus/screen_lock_service_provider.cc
+++ chrome/browser/chromeos/dbus/screen_lock_service_provider.cc
@@ -13,21 +13,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chromeos {
 
-ScreenLockServiceProvider::ScreenLockServiceProvider()
-    : weak_ptr_factory_(this) {
-}
+ScreenLockServiceProvider::ScreenLockServiceProvider(
+    const std::string& interface_name,
+    const std::string& method_name)
+    : interface_name_(interface_name),
+      method_name_(method_name),
+      weak_ptr_factory_(this) {}
 
 ScreenLockServiceProvider::~ScreenLockServiceProvider() {}
 
 void ScreenLockServiceProvider::Start(
     scoped_refptr<dbus::ExportedObject> exported_object) {
   exported_object->ExportMethod(
-      kLibCrosServiceInterface,
-      kLockScreen,
-      base::Bind(&ScreenLockServiceProvider::LockScreen,
-                 weak_ptr_factory_.GetWeakPtr()),
-      base::Bind(&ScreenLockServiceProvider::OnExported,
-                 weak_ptr_factory_.GetWeakPtr()));
+      interface_name_, method_name_,
+      base::BindRepeating(&ScreenLockServiceProvider::ShowLockScreen,
+                          weak_ptr_factory_.GetWeakPtr()),
+      base::BindRepeating(&ScreenLockServiceProvider::OnExported,
+                          weak_ptr_factory_.GetWeakPtr()));
 }
 
 void ScreenLockServiceProvider::OnExported(const std::string& interface_name,
@@ -39,12 +41,12 @@ void ScreenLockServiceProvider::OnExported(const std::string& interface_name,
   }
 }
 
-void ScreenLockServiceProvider::LockScreen(
+void ScreenLockServiceProvider::ShowLockScreen(
     dbus::MethodCall* method_call,
     dbus::ExportedObject::ResponseSender response_sender) {
-  // Please add any additional logic to ScreenLocker::HandleLockScreenRequest()
-  // instead of placing it here.
-  ScreenLocker::HandleLockScreenRequest();
+  // Please add any additional logic to
+  // ScreenLocker::HandleShowLockScreenRequest() instead of placing it here.
+  ScreenLocker::HandleShowLockScreenRequest();
   response_sender.Run(dbus::Response::FromMethodCall(method_call));
 }
 
