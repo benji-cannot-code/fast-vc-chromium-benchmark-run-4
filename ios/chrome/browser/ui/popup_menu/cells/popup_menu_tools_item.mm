0aac4c3133a2cd1812045a42aefba2812a261e19
Gauthier Ambard <gambard@chromium.org>
2018-04-05 16:19:52 +0000

    Adjust text color of the tools menu
    This CL adjusts the color of the elements of the tools menu when they
    are enabled.
    
    Bug: 824210
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I7aaca16482f40a97dfe713a1847e25ea697103f9
    Reviewed-on: https://chromium-review.googlesource.com/997818
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548445}

--- ios/chrome/browser/ui/popup_menu/cells/popup_menu_tools_item.mm
+++ ios/chrome/browser/ui/popup_menu/cells/popup_menu_tools_item.mm
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/reading_list/number_badge_view.h"
 #import "ios/chrome/browser/ui/reading_list/text_badge_view.h"
 #import "ios/chrome/browser/ui/table_view/chrome_table_view_styler.h"
+#import "ios/chrome/browser/ui/uikit_ui_util.h"
 #import "ios/chrome/browser/ui/util/constraints_ui_util.h"
 #import "ios/chrome/common/material_timing.h"
 
@@ -19,13 +20,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 namespace {
+const int kEnabledColor = 0x1A73E8;
 const CGFloat kImageLength = 28;
 const CGFloat kCellHeight = 44;
 const CGFloat kInnerMargin = 11;
 const CGFloat kMargin = 15;
 const CGFloat kTopMargin = 8;
 const CGFloat kTopMarginBadge = 14;
-}
+}  // namespace
 
 @implementation PopupMenuToolsItem
 
@@ -237,8 +239,8 @@ const CGFloat kTopMarginBadge = 14;
 - (void)setUserInteractionEnabled:(BOOL)userInteractionEnabled {
   [super setUserInteractionEnabled:userInteractionEnabled];
   if (userInteractionEnabled) {
-    self.titleLabel.textColor = self.tintColor;
-    self.imageView.tintColor = self.tintColor;
+    self.titleLabel.textColor = UIColorFromRGB(kEnabledColor);
+    self.imageView.tintColor = UIColorFromRGB(kEnabledColor);
   } else {
     self.titleLabel.textColor = [[self class] disabledColor];
     self.imageView.tintColor = [[self class] disabledColor];
