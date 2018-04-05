d407598b2313024f02d710d9185d6d5a7fe4932a
Jennifer Apacible <apacible@chromium.org>
2018-04-05 05:33:04 +0000

    [Picture in Picture] Update LayoutTest files.
    This change removes some a duplicate function to enable Picture in
    Picture for LayoutTests. It also consolidates TestExpectations and
    moves the existing tests to the same directory.
    
    BUG: 726619
    Change-Id: I5f46bb6886ac9b88bb2bd23a2c38fe77ead00ace
    Reviewed-on: https://chromium-review.googlesource.com/994256
    Commit-Queue: apacible <apacible@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548341}

--- third_party/WebKit/LayoutTests/media/media-controls.js
+++ third_party/WebKit/LayoutTests/media/media-controls.js
@@ -33,15 +33,6 @@ function castButton(videoElement) {
     return button;
 }
 
-function pictureInPictureButton(videoElement) {
-    var controlID = '-internal-media-controls-picture-in-picture-button';
-
-    var button = mediaControlsElement(window.internals.shadowRoot(videoElement).firstChild, controlID);
-    if (!button)
-        throw 'Failed to find picture in picture button';
-    return button;
-}
-
 function downloadButton(videoElement) {
     var controlID = '-internal-media-controls-download-button';
     var button = mediaControlsElement(window.internals.shadowRoot(videoElement).firstChild, controlID);
@@ -434,17 +425,6 @@ function doubleTouchAtCoordinates(x, y, timeout, callback) {
       ]
     }
   ], callback);
-}
-
-function enablePictureInPictureForTest(t) {
-  var pictureInPictureEnabledValue =
-      internals.runtimeFlags.pictureInPictureEnabled;
-  internals.runtimeFlags.pictureInPictureEnabled = true;
-
-  t.add_cleanup(() => {
-    internals.runtimeFlags.pictureInPictureEnabled =
-        pictureInPictureEnabledValue;
-  });
 }
 
 function traverseNextNode(node, stayWithin) {
