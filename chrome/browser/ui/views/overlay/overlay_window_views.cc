17a038ffcd71561f6c510392e833fcef947d6f0d
Jennifer Apacible <apacible@chromium.org>
2018-04-05 15:29:12 +0000

    [Picture in Picture] Move generic code to content/.
    Currently, all Picture in Picture code is housed in chrome/. Since
    only some of the Picture in Picture work is Chrome specific, we move
    the more generic work into content/.
    
    This change completely moves the PictureInPictureWindowController
    and OverlaySurfaceEmbedder into content/.
    
    This change will also pave for some future work related to user
    initiated commands from the window, such as play/pause. IPC messaging
    for MediaPlayerDelegateMsg must be done through content/ rather than
    in chrome/.
    
    OverlayWindowViews continues to be the desktop Chrome implementation
    of OverlayWindow, which is now moved into content/. With the
    convergence of historically platform-dependent implementations (views
    vs cocoa) into just views, there will only be one window implementation
    for Chrome.
    
    BUG: 726619 821874
    Change-Id: I6ef73552c6729aec235f4a6d176fd4109f6674b3
    Reviewed-on: https://chromium-review.googlesource.com/972841
    Commit-Queue: apacible <apacible@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548428}

--- chrome/browser/ui/views/overlay/overlay_window_views.cc
+++ chrome/browser/ui/views/overlay/overlay_window_views.cc
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/widget/widget_delegate.h"
 
 // static
-std::unique_ptr<OverlayWindow> OverlayWindow::Create() {
+std::unique_ptr<content::OverlayWindow> content::OverlayWindow::Create() {
   return base::WrapUnique(new OverlayWindowViews());
 }
 
