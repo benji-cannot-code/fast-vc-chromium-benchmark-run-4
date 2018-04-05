d845deddf2823631cd3908f871ddfe7b794db113
Tom Anderson <thomasanderson@chromium.org>
2018-04-05 03:36:35 +0000

    Replace Times New Roman with Tinos GCS font
    BUG=787020
    TBR=dpranke
    
    Change-Id: I5d7fbbbee13a356ea7b53c577441fbf1c922ccb8
    Reviewed-on: https://chromium-review.googlesource.com/996755
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548309}

--- base/test/fontconfig_util_linux.cc
+++ base/test/fontconfig_util_linux.cc
@@ -53,18 +53,6 @@ const char kFontsConfTemplate[] = R"(<?xml version="1.0"?>
       <glob>/usr/share/fonts/truetype/msttcorefonts/Impact.ttf</glob>
     </acceptfont>
     <acceptfont>
-      <glob>/usr/share/fonts/truetype/msttcorefonts/Times_New_Roman.ttf</glob>
-    </acceptfont>
-    <acceptfont>
-      <glob>/usr/share/fonts/truetype/msttcorefonts/Times_New_Roman_Bold.ttf</glob>
-    </acceptfont>
-    <acceptfont>
-      <glob>/usr/share/fonts/truetype/msttcorefonts/Times_New_Roman_Bold_Italic.ttf</glob>
-    </acceptfont>
-    <acceptfont>
-      <glob>/usr/share/fonts/truetype/msttcorefonts/Times_New_Roman_Italic.ttf</glob>
-    </acceptfont>
-    <acceptfont>
       <glob>/usr/share/fonts/truetype/msttcorefonts/Verdana.ttf</glob>
     </acceptfont>
     <acceptfont>
@@ -85,7 +73,7 @@ const char kFontsConfTemplate[] = R"(<?xml version="1.0"?>
     </edit>
   </match>
 
-  <!-- TODO(thomasanderson): Remove once Tinos is added to GCS fonts. -->
+  <!-- TODO(thomasanderson): Figure out why this is necessary. -->
   <match target="pattern">
     <test name="family" compare="eq">
       <string>Tinos</string>
@@ -96,9 +84,6 @@ const char kFontsConfTemplate[] = R"(<?xml version="1.0"?>
     <edit name="hintstyle" mode="assign">
       <const>hintslight</const>
     </edit>
-    <edit name="family" mode="assign">
-      <string>Times New Roman</string>
-    </edit>
   </match>
 
   <match target="pattern">
@@ -106,7 +91,7 @@ const char kFontsConfTemplate[] = R"(<?xml version="1.0"?>
       <string>Times</string>
     </test>
     <edit name="family" mode="assign">
-      <string>Times New Roman</string>
+      <string>Tinos</string>
     </edit>
   </match>
 
@@ -129,7 +114,7 @@ const char kFontsConfTemplate[] = R"(<?xml version="1.0"?>
   </match>
 
   <!-- Some layout tests specify Helvetica as a family and we need to make sure
-       that we don't fallback to Times New Roman for them -->
+       that we don't fallback to Tinos for them -->
   <match target="pattern">
     <test qual="any" name="family">
       <string>Helvetica</string>
@@ -153,7 +138,7 @@ const char kFontsConfTemplate[] = R"(<?xml version="1.0"?>
       <string>serif</string>
     </test>
     <edit name="family" mode="assign">
-      <string>Times New Roman</string>
+      <string>Tinos</string>
     </edit>
   </match>
 
@@ -207,7 +192,7 @@ const char kFontsConfTemplate[] = R"(<?xml version="1.0"?>
       <string>Monaco</string>
     </test>
     <edit name="family" mode="assign">
-      <string>Times New Roman</string>
+      <string>Tinos</string>
     </edit>
   </match>
 
@@ -235,6 +220,15 @@ const char kFontsConfTemplate[] = R"(<?xml version="1.0"?>
     </test>
     <edit name="family" mode="assign">
       <string>Gelasio</string>
+    </edit>
+  </match>
+
+  <match target="pattern">
+    <test qual="any" name="family">
+      <string>Times New Roman</string>
+    </test>
+    <edit name="family" mode="assign">
+      <string>Tinos</string>
     </edit>
   </match>
 
@@ -358,7 +352,7 @@ const char kFontsConfTemplate[] = R"(<?xml version="1.0"?>
       <string>SubpixelPositioning</string>
     </test>
     <edit name="family" mode="assign">
-      <string>Times New Roman</string>
+      <string>Tinos</string>
     </edit>
   </match>
 
@@ -377,7 +371,7 @@ const char kFontsConfTemplate[] = R"(<?xml version="1.0"?>
       <string>SlightHintedTimesNewRoman</string>
     </test>
     <edit name="family" mode="assign">
-      <string>Times New Roman</string>
+      <string>Tinos</string>
     </edit>
     <edit name="hintstyle" mode="assign">
       <const>hintslight</const>
