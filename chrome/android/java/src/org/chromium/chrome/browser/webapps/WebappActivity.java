cc5c42d1bff78c701529ce49ada4b1b6dcbc06f2
Mohamed Heikal <mheikal@google.com>
2018-04-05 23:43:57 +0000

    [WebApk] Move loadurl earlier in startup
    Tab.loadurl is currently called in finishNativeInitialization in
    WebappActivity vs in initializeState in ChromeTabbedActivity.
    initializeState is called earlier in the async chain in
    ChromeBrowserInitializer. This CL moves the call to loadUrl to
    initializeState in WebappActivity.
    
    Testing shows that this change leads to a 50-70ms improvement in
    startup time:
    
    	                         improvement	 average deviation
    Startup (ms)	                  81.86214	 39.11936
    StartupToNavigation (ms)	  66.5648        34.37813714
    NavigationToCommit (ms)	          56.64914	 20.09670629
    NavigationToContentfulPaint (ms)  5.47332	 29.81943543
    StartupToContentfulPaint (ms)	  72.03812	 47.7408
    NavigationToMeaningfulPaint (ms)  5.4415	 29.80534286
    StartupToMeaningfulPaint (ms)	  72.0063	 47.73242286
    
    Bug: 825259
    Change-Id: I90aee902d9d0fcef4972fafd40453f6f02b320d1
    Reviewed-on: https://chromium-review.googlesource.com/995635
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Dmitry Skiba <dskiba@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548615}

--- chrome/android/java/src/org/chromium/chrome/browser/webapps/WebappActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/WebappActivity.java
@@ -245,6 +245,12 @@ public class WebappActivity extends SingleTabActivity {
         return (intent == null) ? WebappInfo.createEmpty() : WebappInfo.create(intent);
     }
 
+    @Override
+    public void initializeState() {
+        super.initializeState();
+        initializeUI(getSavedInstanceState());
+    }
+
     protected void initializeUI(Bundle savedInstanceState) {
         // We do not load URL when restoring from saved instance states.
         if (savedInstanceState == null) {
@@ -325,12 +331,6 @@ public class WebappActivity extends SingleTabActivity {
 
     @Override
     public void finishNativeInitialization() {
-        if (!mWebappInfo.isInitialized()) {
-            ApiCompatibilityUtils.finishAndRemoveTask(this);
-            return;
-        }
-
-        initializeUI(getSavedInstanceState());
         LayoutManager layoutDriver = new LayoutManager(getCompositorViewHolder());
         initializeCompositorContent(layoutDriver, findViewById(R.id.url_bar),
                 (ViewGroup) findViewById(android.R.id.content),
