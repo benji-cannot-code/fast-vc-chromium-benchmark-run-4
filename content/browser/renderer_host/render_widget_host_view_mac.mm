55f05d413e38a46f9cf651b51ead4eb8c2c21f97
Ria Jiang <riajiang@chromium.org>
2018-04-05 03:46:31 +0000

    Early out in RenderWidgetHostViewMac get-surface-id related functions.
    We can ask RenderWidgetHostViewMac for its FrameSinkId/SurfaceId when
    the BrowserCompositorMac is already gone there; early out in those
    cases and return invalid FrameSinkId/SurfaceId. When GetCurrentSurfaceId
    returns invalid SurfaceId, we are going to send the event to the parent
    root_view of the now-invalid view [1].
    
    [1] https://cs.chromium.org/chromium/src/content/browser/renderer_host/render_widget_host_input_event_router.cc?type=cs&l=330
    
    Bug: 828304
    Change-Id: I009e51e9c1cfd31dad8efd4a7b504ba63dd49b18
    Reviewed-on: https://chromium-review.googlesource.com/996304
    Reviewed-by: ccameron <ccameron@chromium.org>
    Commit-Queue: Ria Jiang <riajiang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548314}

--- content/browser/renderer_host/render_widget_host_view_mac.mm
+++ content/browser/renderer_host/render_widget_host_view_mac.mm
@@ -1013,10 +1013,14 @@ RenderWidgetHostViewMac::CreateSyntheticGestureTarget() {
 }
 
 viz::LocalSurfaceId RenderWidgetHostViewMac::GetLocalSurfaceId() const {
+  if (!browser_compositor_)
+    return viz::LocalSurfaceId();
   return browser_compositor_->GetRendererLocalSurfaceId();
 }
 
 viz::FrameSinkId RenderWidgetHostViewMac::GetFrameSinkId() {
+  if (!browser_compositor_)
+    return viz::FrameSinkId();
   return browser_compositor_->GetDelegatedFrameHost()->frame_sink_id();
 }
 
@@ -1074,10 +1078,14 @@ bool RenderWidgetHostViewMac::TransformPointToCoordSpaceForView(
 }
 
 viz::FrameSinkId RenderWidgetHostViewMac::GetRootFrameSinkId() {
+  if (!browser_compositor_)
+    return viz::FrameSinkId();
   return browser_compositor_->GetRootFrameSinkId();
 }
 
 viz::SurfaceId RenderWidgetHostViewMac::GetCurrentSurfaceId() const {
+  if (!browser_compositor_)
+    return viz::SurfaceId();
   return browser_compositor_->GetDelegatedFrameHost()->GetCurrentSurfaceId();
 }
 
