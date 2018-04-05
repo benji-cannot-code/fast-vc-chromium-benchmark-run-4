b64bc71673434f858b67f7979637bd2efd5b1d17
Biao She <bshe@chromium.org>
2018-04-05 17:31:14 +0000

    Force exit VR if we are about to show Search engine selection dialog
    Bug: 819875
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_vr;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I95e91504b6e28ac2f762881dde31924f05c2cf6e
    Reviewed-on: https://chromium-review.googlesource.com/987388
    Reviewed-by: Yash Malik <ymalik@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Biao She <bshe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548467}

--- chrome/android/java/src/org/chromium/chrome/browser/vr_shell/VrShell.java
+++ chrome/android/java/src/org/chromium/chrome/browser/vr_shell/VrShell.java
@@ -65,7 +65,7 @@ public interface VrShell extends VrDialogManager, VrToastManager {
     /**
      * Requests to exit VR.
      */
-    void requestToExitVr(@UiUnsupportedMode int reason);
+    void requestToExitVr(@UiUnsupportedMode int reason, boolean showExitPromptBeforeDoff);
 
     /**
      *  Triggers VrShell to navigate forward.
