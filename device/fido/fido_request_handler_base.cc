9c62133b5dede2e48288d4efea47f19ec3453313
Jun Choi <hongjunchoi@chromium.org>
2018-04-05 07:39:07 +0000

    Implement GetAssertionRequestHandler
    GetAssertionRequestHandler guarantees at most one successful response to
    be sent to the relying party.
    
    Bug: 780078
    Change-Id: Ibe8cb2374713f25fbabc73d23615349edb8589fb
    Reviewed-on: https://chromium-review.googlesource.com/942368
    Commit-Queue: Jun Choi <hongjunchoi@chromium.org>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548362}

--- device/fido/fido_request_handler_base.cc
+++ device/fido/fido_request_handler_base.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "device/fido/fido_request_handler.h"
+#include "device/fido/fido_request_handler_base.h"
 
 #include <utility>
 
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace device {
 
-FidoRequestHandler::FidoRequestHandler(
+FidoRequestHandlerBase::FidoRequestHandlerBase(
     service_manager::Connector* connector,
     const base::flat_set<U2fTransportProtocol>& transports) {
   for (const auto transport : transports) {
@@ -30,9 +30,9 @@ FidoRequestHandler::FidoRequestHandler(
   }
 }
 
-FidoRequestHandler::~FidoRequestHandler() = default;
+FidoRequestHandlerBase::~FidoRequestHandlerBase() = default;
 
-void FidoRequestHandler::CancelOngoingTasks(
+void FidoRequestHandlerBase::CancelOngoingTasks(
     base::StringPiece exclude_device_id) {
   for (auto task_it = ongoing_tasks_.begin();
        task_it != ongoing_tasks_.end();) {
@@ -47,17 +47,17 @@ void FidoRequestHandler::CancelOngoingTasks(
   }
 }
 
-void FidoRequestHandler::DiscoveryStarted(FidoDiscovery* discovery,
-                                          bool success) {}
+void FidoRequestHandlerBase::DiscoveryStarted(FidoDiscovery* discovery,
+                                              bool success) {}
 
-void FidoRequestHandler::DeviceAdded(FidoDiscovery* discovery,
-                                     FidoDevice* device) {
+void FidoRequestHandlerBase::DeviceAdded(FidoDiscovery* discovery,
+                                         FidoDevice* device) {
   DCHECK(!base::ContainsKey(ongoing_tasks(), device->GetId()));
   ongoing_tasks_.emplace(device->GetId(), CreateTaskForNewDevice(device));
 }
 
-void FidoRequestHandler::DeviceRemoved(FidoDiscovery* discovery,
-                                       FidoDevice* device) {
+void FidoRequestHandlerBase::DeviceRemoved(FidoDiscovery* discovery,
+                                           FidoDevice* device) {
   // Device connection has been lost or device has already been removed.
   // Thus, calling CancelTask() is not necessary.
   DCHECK(base::ContainsKey(ongoing_tasks_, device->GetId()));
