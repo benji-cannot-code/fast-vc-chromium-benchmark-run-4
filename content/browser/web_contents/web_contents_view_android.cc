2d374d69bafe5f8355236540f3c6a8be57440629
Jinsuk Kim <jinsukkim@chromium.org>
2018-04-05 03:36:06 +0000

    Android: Fix assertion failure in RWHVA destruction
    Assertion at RenderWidgetHostViewAndroid::StopObservingRootWindow
    called via |Destroy| failed, since WebContentsViewAndroid::SetContentViewCore
    was passing a valid ViewAndroid instead of nullptr when cvc is destroyed.
    This kept RWHVA observing root window, while the view tree was already
    torn down by WCVA dtor, leaving RWHVA in inconsistent state where it
    couldn't get WindowAndroid any more. This CL fixes the bug. Previously
    WCVA passed null CVC to RWHVA before https://crrev.com/c/958646, so
    this fix is the correct behavior.
    
    Bug: 828211
    Change-Id: I79463cd0dce94c5f8d81417208cd313871c4f5d7
    Reviewed-on: https://chromium-review.googlesource.com/991002
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548308}

--- content/browser/web_contents/web_contents_view_android.cc
+++ content/browser/web_contents/web_contents_view_android.cc
@@ -118,7 +118,7 @@ void WebContentsViewAndroid::SetContentViewCore(ContentViewCore* cvc) {
     content_view_core_.reset(cvc);
   RenderWidgetHostViewAndroid* rwhv = GetRenderWidgetHostViewAndroid();
   if (rwhv)
-    rwhv->UpdateNativeViewTree(&view_);
+    rwhv->UpdateNativeViewTree(cvc ? &view_ : nullptr);
 
   if (web_contents_->ShowingInterstitialPage()) {
     rwhv = static_cast<RenderWidgetHostViewAndroid*>(
@@ -128,7 +128,7 @@ void WebContentsViewAndroid::SetContentViewCore(ContentViewCore* cvc) {
             ->GetWidget()
             ->GetView());
     if (rwhv)
-      rwhv->UpdateNativeViewTree(&view_);
+      rwhv->UpdateNativeViewTree(cvc ? &view_ : nullptr);
   }
 }
 
