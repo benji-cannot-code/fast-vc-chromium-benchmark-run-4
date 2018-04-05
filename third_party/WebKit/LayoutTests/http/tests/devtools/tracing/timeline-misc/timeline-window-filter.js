e42fbb6c1b3fa0b3ae4089894651e7ae23385d7e
Alexei Filippov <alph@chromium.org>
2018-04-05 02:59:07 +0000

    DevTools: Fix flamechart zoom/pan glitches.
    Fixed jumping during mouse wheel zoom.
    Fixed jitter in network flamechart panning when main flamechart is panned with the mouse.
    Fixed misplaced hover/selection after zooming.
    
    Change-Id: I75bce65f8392922fa3e32f72731eebbce52b5450
    Reviewed-on: https://chromium-review.googlesource.com/994285
    Commit-Queue: Alexei Filippov <alph@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548302}

--- third_party/WebKit/LayoutTests/http/tests/devtools/tracing/timeline-misc/timeline-window-filter.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/tracing/timeline-misc/timeline-window-filter.js
@@ -12,26 +12,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   var timeline = UI.panels.timeline;
   var overviewPane = timeline._overviewPane;
 
-  PerformanceTestRunner.loadTimeline(PerformanceTestRunner.timelineData()).then(onTimelineLoaded);
+  await PerformanceTestRunner.loadTimeline(PerformanceTestRunner.timelineData());
 
-  function onTimelineLoaded() {
-    overviewPane._update();
-    TestRunner.addResult('OverviewPane:');
-    overviewPane._overviewCalculator.setDisplayWidth(450);
-    dumpDividers(overviewPane._overviewCalculator);
-    TestRunner.addResult('');
+  overviewPane._update();
+  TestRunner.addResult('OverviewPane:');
+  overviewPane._overviewCalculator.setDisplayWidth(450);
+  dumpDividers(overviewPane._overviewCalculator);
+  TestRunner.addResult('');
 
-    dumpFlameChartRecordsCountForRange(0, 1);
-    dumpFlameChartRecordsCountForRange(0.25, 0.75);
-    dumpFlameChartRecordsCountForRange(0.33, 0.66);
+  dumpFlameChartRecordsCountForRange(0, 1);
+  dumpFlameChartRecordsCountForRange(0.25, 0.75);
+  dumpFlameChartRecordsCountForRange(0.33, 0.66);
 
-    overviewPane._overviewGrid.setWindow(0.1, 0.9);
+  overviewPane._overviewGrid.setWindow(0.1, 0.9);
 
-    TestRunner.addResult('--------------------------------------------------------');
-    var selectionData = timeline._currentModelSelectionData();
-    TestRunner.addResult('time range = ' + selectionData.windowStartTime + ' - ' + selectionData.windowEndTime);
-    TestRunner.completeTest();
-  }
+  TestRunner.addResult('--------------------------------------------------------');
+  var selectionData = timeline._currentModelSelectionData();
+  TestRunner.addResult('time range = ' + selectionData.windowStartTime + ' - ' + selectionData.windowEndTime);
+  TestRunner.completeTest();
 
   function dumpFlameChartRecordsCountForRange(windowLeft, windowRight) {
     var mainView = timeline._flameChart._mainFlameChart;
@@ -45,7 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 
   function dumpDividers(calculator) {
-    var times = PerfUI.TimelineGrid.calculateDividerOffsets(calculator)
+    var times = PerfUI.TimelineGrid.calculateGridOffsets(calculator)
                     .offsets.map(offset => offset.time - calculator.zeroTime());
     TestRunner.addResult('divider offsets: [' + times.join(', ') + ']. We are expecting round numbers.');
   }
