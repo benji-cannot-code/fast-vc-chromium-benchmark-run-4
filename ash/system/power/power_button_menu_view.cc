cab83e849761209630477864306ed7a73ec95ac2
Min Chen <minch@google.com>
2018-04-06 23:35:55 +0000

    Add FocusPowerOffButton and rename menu item gets functions in PowerButtonMenuView.
    Bug: 818379
    Change-Id: I20da32dd59689ba431c01ccb41707852d8514831
    Reviewed-on: https://chromium-review.googlesource.com/1000320
    Commit-Queue: Min Chen <minch@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Qiang Xu <warx@google.com>
    Cr-Commit-Position: refs/heads/master@{#548968}

--- ash/system/power/power_button_menu_view.cc
+++ ash/system/power/power_button_menu_view.cc
@@ -49,6 +49,10 @@ PowerButtonMenuView::PowerButtonMenuView(
 
 PowerButtonMenuView::~PowerButtonMenuView() = default;
 
+void PowerButtonMenuView::FocusPowerOffButton() {
+  power_off_item_->RequestFocus();
+}
+
 void PowerButtonMenuView::ScheduleShowHideAnimation(bool show) {
   // Cancel any previous animation.
   layer()->GetAnimator()->AbortAllAnimations();
