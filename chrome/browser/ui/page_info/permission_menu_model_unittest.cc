01800a52e5e2d728a9b46ea153f98afed10f8855
Reilly Grant <reillyg@chromium.org>
2018-04-06 18:26:07 +0000

    Implement desktop UI for the WebUSB guard permission
    This patch provides UI controls for the new WebUSB guard permission in
    the page info popup and on the site settings pages.
    
    Screenshots of the new UI are included in the design doc:
    https://goo.gl/uMPMZZ
    
    Bug: 771703
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I4c9dc2cc59a589e558ceb346982d493ac66dc674
    Reviewed-on: https://chromium-review.googlesource.com/982595
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Raymes Khoury <raymes@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Dave Schuyler <dschuyler@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548866}

--- chrome/browser/ui/page_info/permission_menu_model_unittest.cc
+++ chrome/browser/ui/page_info/permission_menu_model_unittest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/ui/page_info/permission_menu_model.h"
+#include "base/strings/utf_string_conversions.h"
 #include "chrome/test/base/testing_profile.h"
 #include "content/public/test/test_browser_thread_bundle.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -43,6 +44,7 @@ TEST_F(PermissionMenuModelTest, TestDefault) {
   permission.default_setting = CONTENT_SETTING_ALLOW;
   permission.source = content_settings::SETTING_SOURCE_USER;
   permission.is_incognito = false;
+
   PermissionMenuModel model(profile(), GURL("http://www.google.com"),
                             permission, callback.callback());
   EXPECT_EQ(3, model.GetItemCount());
@@ -82,4 +84,18 @@ TEST_F(PermissionMenuModelTest, TestIncognitoNotifications) {
   PermissionMenuModel incognito_model(profile(), GURL("https://www.google.com"),
                                       permission, callback.callback());
   EXPECT_EQ(2, incognito_model.GetItemCount());
+}
+
+TEST_F(PermissionMenuModelTest, TestUsbGuard) {
+  TestCallback callback;
+  PageInfoUI::PermissionInfo permission;
+  permission.type = CONTENT_SETTINGS_TYPE_USB_GUARD;
+  permission.setting = CONTENT_SETTING_ASK;
+  permission.default_setting = CONTENT_SETTING_ASK;
+  permission.source = content_settings::SETTING_SOURCE_USER;
+  permission.is_incognito = false;
+
+  PermissionMenuModel model(profile(), GURL("http://www.google.com"),
+                            permission, callback.callback());
+  EXPECT_EQ(3, model.GetItemCount());
 }
