9c80dd8215cb5e39a7775a20ea406c0b5c85ea8b
Yash Malik <ymalik@google.com>
2018-04-06 16:21:15 +0000

    VR: DOFF if VR Services need to be updated
    Befort this CL, we would just ignore a VR intent if the VR services were out of
    date. In this CL, we show the DOFF, and show the update infobar in 2D.
    
    For DLAs some reason, intenting out to the play store sometimes results in
    Android dropping the intent, so we keep the CCT alive if the DON flow is
    successful and show the infobar.
    
    Bug: 824524
    Change-Id: I2387b270a2e9c18f941e4a433309749f6ef25f87
    Reviewed-on: https://chromium-review.googlesource.com/997018
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Yash Malik <ymalik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548798}

--- chrome/android/java/src/org/chromium/chrome/browser/vr_shell/VrIntentUtils.java
+++ chrome/android/java/src/org/chromium/chrome/browser/vr_shell/VrIntentUtils.java
@@ -56,13 +56,6 @@ public class VrIntentUtils {
          * @return Whether the intent should be allowed to auto-present.
          */
         boolean isTrustedAutopresentIntent(Intent intent);
-
-        /**
-         * Determines whether the installed version of Chrome can handle VR intents.
-         * @param context The context for the caller.
-         * @return Whether VR intents can be handled.
-         */
-        boolean canHandleVrIntent(Context context);
     }
 
     private static VrIntentHandler createInternalVrIntentHandler() {
@@ -82,16 +75,6 @@ public class VrIntentUtils {
                 // we're sure that most clients have the change.
                 return isTrustedDaydreamIntent(intent)
                         && IntentUtils.safeGetBooleanExtra(intent, AUTOPRESENT_WEVBVR_EXTRA, false);
-            }
-
-            @Override
-            public boolean canHandleVrIntent(Context context) {
-                VrClassesWrapper wrapper = VrShellDelegate.getVrClassesWrapper();
-                if (wrapper == null) return false;
-                int supportLevel =
-                        VrShellDelegate.getVrSupportLevel(wrapper.createVrDaydreamApi(context),
-                                wrapper.createVrCoreVersionChecker(), null);
-                return supportLevel == VrSupportLevel.VR_DAYDREAM;
             }
         };
     }
@@ -135,13 +118,6 @@ public class VrIntentUtils {
         if (intent == null) return false;
         return IntentHandler.getUrlFromIntent(intent) != null
                 && getHandlerInstance().isTrustedDaydreamIntent(intent);
-    }
-
-    /**
-     * @return whether the installed version of Chrome can handle VR intents.
-     */
-    public static boolean canHandleVrIntent(Context context) {
-        return getHandlerInstance().canHandleVrIntent(context);
     }
 
     /**
