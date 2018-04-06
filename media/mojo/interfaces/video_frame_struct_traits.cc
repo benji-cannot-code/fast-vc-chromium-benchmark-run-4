e1e22c78e206f19d24b230b08d61fbab6d81b2a2
John Rummell <jrummell@chromium.org>
2018-04-06 04:14:46 +0000

    Improve checking for successful Decode() without returning a frame
    There's been a bunch of crashes where it appears no video frame was returned
    even though status = success. This change adds additional logging and converts
    some of the DCHECKs to CHECK so that the problem is caught earlier.
    
    BUG=828148
    TEST=encrypted media browser_tests pass
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ib15465179f4931860b6b6fb4192c320e09a30d7e
    Reviewed-on: https://chromium-review.googlesource.com/993711
    Reviewed-by: Chris Palmer <palmer@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: John Rummell <jrummell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548665}

--- media/mojo/interfaces/video_frame_struct_traits.cc
+++ media/mojo/interfaces/video_frame_struct_traits.cc
@@ -141,6 +141,9 @@ bool StructTraits<media::mojom::VideoFrameDataView,
     return false;
   }
 
+  if (!frame)
+    return false;
+
   std::unique_ptr<base::DictionaryValue> metadata;
   if (!input.ReadMetadata(&metadata))
     return false;
