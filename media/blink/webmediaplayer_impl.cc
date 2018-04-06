4c37bb17860632082a4faf060746e6d6008d6669
Jennifer Apacible <apacible@chromium.org>
2018-04-06 23:11:04 +0000

    Remove debugging line.
    Change-Id: I102db474b58162173c07334e06b39d6e7509acc8
    Reviewed-on: https://chromium-review.googlesource.com/1000141
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: apacible <apacible@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548957}

--- media/blink/webmediaplayer_impl.cc
+++ media/blink/webmediaplayer_impl.cc
@@ -792,7 +792,6 @@ void WebMediaPlayerImpl::EnterPictureInPicture() {
 }
 
 void WebMediaPlayerImpl::ExitPictureInPicture() {
-  LOG(ERROR) << "WebMediaPlayerImpl::ExitPictureInPicture";
   // TODO(apacible): Handle ending PiP from a user gesture. This currently
   // handles ending Picture-in-Picture mode from the source.
   // https://crbug.com/823172.
