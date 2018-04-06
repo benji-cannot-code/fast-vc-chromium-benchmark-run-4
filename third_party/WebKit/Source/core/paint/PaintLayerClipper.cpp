d18ee54675693dcd13716069e38b4e42d33834db
Philip Rogers <pdr@chromium.org>
2018-04-06 23:14:43 +0000

    [RLS] Do not use scrollbar-excluded overflow clip at the root
    Clip paint property nodes store a rect for the clip excluding overlay
    scrollbars. When overlay scrollbars appear or disappear, the associated
    clip node needs to be updated. Changing a clip node is expensive because
    a full treewalk is needed to ensure descendant clip caches are cleared.
    
    This patch special-cases the topmost clip node to not store a clip
    excluding overlay scrollbars. With this patch, the common operation of
    showing/hiding overlay scrollbars does not cause a full treewalk.
    
    Bug: 823999
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I605fa3dea5c33e0cbd0de314b915fd6526d64f1a
    Reviewed-on: https://chromium-review.googlesource.com/999053
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548960}

--- third_party/WebKit/Source/core/paint/PaintLayerClipper.cpp
+++ third_party/WebKit/Source/core/paint/PaintLayerClipper.cpp
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/paint/PaintLayerClipper.h"
 
+#include "core/frame/LocalFrame.h"
 #include "core/frame/LocalFrameView.h"
 #include "core/frame/Settings.h"
 #include "core/layout/LayoutView.h"
@@ -515,10 +516,11 @@ void PaintLayerClipper::CalculateBackgroundClipRectWithGeometryMapper(
     // rect if layer_ is the same as the root.
     OverlayScrollbarClipBehavior clip_behavior =
         context.overlay_scrollbar_clip_behavior;
-    if (&layer_ == context.root_layer)
+
+    if (is_clipping_root)
       clip_behavior = kIgnorePlatformOverlayScrollbarSize;
 
-    FloatClipRect clip_rect((FloatRect(LocalVisualRect(context))));
+    FloatClipRect clip_rect(FloatRect(LocalVisualRect(context)));
     clip_rect.MoveBy(FloatPoint(fragment_data.PaintOffset()));
 
     GeometryMapper::LocalToAncestorVisualRect(source_property_tree_state,
@@ -536,6 +538,24 @@ void PaintLayerClipper::CalculateBackgroundClipRectWithGeometryMapper(
   // TODO(chrishtr): generalize to multiple fragments.
   output.MoveBy(
       -context.root_layer->GetLayoutObject().FirstFragment().PaintOffset());
+
+  // The root LayoutView clip node does not store a rect excluding scrollbars
+  // for hit testing (see FragmentPaintPropertyTreeBuilder::UpdateOverflowClip).
+  // To calculate the correct clip at the root, we need to apply the LayoutView
+  // overflow clip excluding scrollbars here.
+  auto& root = context.root_layer->GetLayoutObject();
+  bool is_root_layout_view =
+      root.IsLayoutView() && !root.GetFrame()->Tree().Parent();
+  bool root_should_clip = HasOverflowClip(*context.root_layer) &&
+                          !is_clipping_root &&
+                          context.ShouldRespectRootLayerClip();
+  if (context.overlay_scrollbar_clip_behavior ==
+          kExcludeOverlayScrollbarSizeForHitTesting &&
+      is_root_layout_view && root_should_clip) {
+    ClipRect root_overflow_clip = ToLayoutView(root).OverflowClipRect(
+        LayoutPoint(), kExcludeOverlayScrollbarSizeForHitTesting);
+    output.Intersect(root_overflow_clip);
+  }
 
   output.Move(context.sub_pixel_accumulation);
 }
