19d0e54811473c4ddef763dbc6ea96a3f664e92b
Xi Cheng <chengx@chromium.org>
2018-04-06 18:18:20 +0000

    Tweaks on persistent histogram storage's implementation and usage
    Bug: 734095
    Change-Id: Ia832d7b3b176d90d4c5d618ac169ad0cc5fe8a72
    Reviewed-on: https://chromium-review.googlesource.com/996455
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Commit-Queue: Xi Cheng <chengx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548861}

--- base/metrics/persistent_histogram_storage.cc
+++ base/metrics/persistent_histogram_storage.cc
@@ -25,8 +25,8 @@ namespace base {
 
 PersistentHistogramStorage::PersistentHistogramStorage(
     StringPiece allocator_name,
-    StorageDirCreation storage_dir_create_action)
-    : storage_dir_create_action_(storage_dir_create_action) {
+    StorageDirManagement storage_dir_management)
+    : storage_dir_management_(storage_dir_management) {
   DCHECK(!allocator_name.empty());
   DCHECK(IsStringASCII(allocator_name));
 
@@ -54,8 +54,8 @@ PersistentHistogramStorage::~PersistentHistogramStorage() {
 
   FilePath storage_dir = storage_base_dir_.AppendASCII(allocator->Name());
 
-  switch (storage_dir_create_action_) {
-    case StorageDirCreation::kEnable:
+  switch (storage_dir_management_) {
+    case StorageDirManagement::kCreate:
       if (!CreateDirectory(storage_dir)) {
         LOG(ERROR)
             << "Could not write \"" << allocator->Name()
@@ -64,11 +64,11 @@ PersistentHistogramStorage::~PersistentHistogramStorage() {
         return;
       }
       break;
-    case StorageDirCreation::kDisable:
+    case StorageDirManagement::kUseExisting:
       if (!DirectoryExists(storage_dir)) {
-        // When the consumer of this class disables storage directory creation
-        // by the class instance, it should ensure the directory's existence if
-        // it's essential.
+        // When the consumer of this class decides to use an existing storage
+        // directory, it should ensure the directory's existence if it's
+        // essential.
         LOG(ERROR)
             << "Could not write \"" << allocator->Name()
             << "\" persistent histograms to file as the storage directory "
--- base/metrics/persistent_histogram_storage.h
+++ base/metrics/persistent_histogram_storage.h
@@ -23,7 +23,7 @@ namespace base {
 // Persisted histograms will eventually be reported by Chrome.
 class BASE_EXPORT PersistentHistogramStorage {
  public:
-  enum class StorageDirCreation { kEnable, kDisable };
+  enum class StorageDirManagement { kCreate, kUseExisting };
 
   // Creates a process-wide storage location for histograms that will be written
   // to a file within a directory provided by |set_storage_base_dir()| on
@@ -31,11 +31,10 @@ class BASE_EXPORT PersistentHistogramStorage {
   // The |allocator_name| is used both as an internal name for the allocator,
   // well as the leaf directory name for the file to which the histograms are
   // persisted. The string must be ASCII.
-  // |storage_dir_create_action| determines that if this instance is
-  // responsible for creating the storage directory. If kDisable is supplied,
-  // it's the consumer's responsibility to create the storage directory.
+  // |storage_dir_management| specifies if this instance reuses an existing
+  // storage directory, or is responsible for creating one.
   PersistentHistogramStorage(StringPiece allocator_name,
-                             StorageDirCreation storage_dir_create_action);
+                             StorageDirManagement storage_dir_management);
 
   ~PersistentHistogramStorage();
 
@@ -54,9 +53,8 @@ class BASE_EXPORT PersistentHistogramStorage {
   // |storage_base_dir_|/|allocator_name| (see the ctor for allocator_name).
   FilePath storage_base_dir_;
 
-  // A flag indicating if the class instance is responsible for creating the
-  // storage directory.
-  const StorageDirCreation storage_dir_create_action_;
+  // The setting of the storage directory management.
+  const StorageDirManagement storage_dir_management_;
 
   // A flag indicating if histogram storage is disabled. It starts with false,
   // but can be set to true by the caller who decides to throw away its
