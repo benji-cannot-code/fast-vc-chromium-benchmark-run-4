fb12d52284fc6ab0e3b667419e79e5c5433988e2
Hajime Hoshi <hajimehoshi@chromium.org>
2018-04-06 09:10:10 +0000

    media: Remove SequencedTaskRunnerHandle::Get() usages in UserMediaClientImpl
    This is part of efforts to replace base::ThreadTaskRunnerHandle::Get()
    and SequencedTaskRunnerHandle::Get() with other appropriate task runners
    in the renderer.
    
    Bug: 827065
    Change-Id: I07e9d3d051c0d4b01aeeafd86e06716ed8473af7
    Reviewed-on: https://chromium-review.googlesource.com/987594
    Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548719}

--- content/renderer/media/stream/apply_constraints_processor.h
+++ content/renderer/media/stream/apply_constraints_processor.h
@@ -35,7 +35,8 @@ class CONTENT_EXPORT ApplyConstraintsProcessor {
   using MediaDevicesDispatcherCallback = base::RepeatingCallback<
       const blink::mojom::MediaDevicesDispatcherHostPtr&()>;
   ApplyConstraintsProcessor(
-      MediaDevicesDispatcherCallback media_devices_dispatcher_cb);
+      MediaDevicesDispatcherCallback media_devices_dispatcher_cb,
+      scoped_refptr<base::SingleThreadTaskRunner> task_runner);
   ~ApplyConstraintsProcessor();
 
   // Starts processing of |request|. When processing of |request| is complete,
@@ -84,6 +85,8 @@ class CONTENT_EXPORT ApplyConstraintsProcessor {
 
   MediaDevicesDispatcherCallback media_devices_dispatcher_cb_;
   SEQUENCE_CHECKER(sequence_checker_);
+
+  const scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
 
   base::WeakPtrFactory<ApplyConstraintsProcessor> weak_factory_;
 
