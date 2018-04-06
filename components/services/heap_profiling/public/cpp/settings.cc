81beaf0e21bc118f1e031310ec12c19b91be67fe
erikchen <erikchen@chromium.org>
2018-04-06 18:14:47 +0000

    Move core logic for dealing with HeapProfilingService into its own class.
    The core logic for dealing with the HeapProfiler has three major components:
    1) Interfacing with the service [depends on components/services/heap_profiling]
    2) Connecting new clients [depends on content/]
    3) Uploading heap dumps [depends on chrome/]
    
    This CL moves the logic for (1) into components/services/heap_profiling. This
    allows android_webview to reuse the logic.
    
    Change-Id: Idd307c805746cacd8760b71addc88650cd81402f
    Bug: 827545
    Reviewed-on: https://chromium-review.googlesource.com/999328
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548859}

--- components/services/heap_profiling/public/cpp/settings.cc
+++ components/services/heap_profiling/public/cpp/settings.cc
@@ -28,6 +28,16 @@ const char kOOPHeapProfilingFeatureSamplingRate[] = "sampling-rate";
 const uint32_t kDefaultSamplingRate = 10000;
 const bool kDefaultShouldSample = false;
 
+bool RecordAllAllocationsForStartup() {
+  const base::CommandLine* cmdline = base::CommandLine::ForCurrentProcess();
+  if (cmdline->HasSwitch(kMemlogSampling))
+    return false;
+
+  return !base::GetFieldTrialParamByFeatureAsBool(
+      kOOPHeapProfilingFeature, kOOPHeapProfilingFeatureSampling,
+      /*default_value=*/kDefaultShouldSample);
+}
+
 }  // namespace
 
 Mode GetModeForStartup() {
@@ -114,17 +124,10 @@ mojom::StackMode ConvertStringToStackMode(const std::string& input) {
   return mojom::StackMode::NATIVE_WITHOUT_THREAD_NAMES;
 }
 
-bool GetShouldSampleForStartup() {
-  const base::CommandLine* cmdline = base::CommandLine::ForCurrentProcess();
-  if (cmdline->HasSwitch(kMemlogSampling))
-    return true;
-
-  return base::GetFieldTrialParamByFeatureAsBool(
-      kOOPHeapProfilingFeature, kOOPHeapProfilingFeatureSampling,
-      kDefaultShouldSample /* default_value */);
-}
-
 uint32_t GetSamplingRateForStartup() {
+  if (RecordAllAllocationsForStartup())
+    return 1;
+
   const base::CommandLine* cmdline = base::CommandLine::ForCurrentProcess();
   if (cmdline->HasSwitch(kMemlogSamplingRate)) {
     std::string rate_as_string =
@@ -142,7 +145,7 @@ uint32_t GetSamplingRateForStartup() {
 
   return base::GetFieldTrialParamByFeatureAsInt(
       kOOPHeapProfilingFeature, kOOPHeapProfilingFeatureSamplingRate,
-      kDefaultSamplingRate /* default_value */);
+      /*default_value=*/kDefaultSamplingRate);
 }
 
 bool IsBackgroundHeapProfilingEnabled() {
