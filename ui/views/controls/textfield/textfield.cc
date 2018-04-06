f672d1803531a20c3add62a9897b4a5b74522af3
Tommy C. Li <tommycli@chromium.org>
2018-04-06 02:31:30 +0000

    Omnibox UI Experiments: Steady state elisions - Tests for Home and Tap
    When Steady State elisions is on, and the user focuses the Omnibox,
    and then presses the Home key, we should unelide and place the caret
    at the very beginning of the full URL.
    
    Also, when the user gesture taps the Omnibox twice to place a cursor,
    we should also unelide and display the full URL.
    
    This CL adds a unit test for each of the above two behaviors.
    
    It also updates the TextField class to add a missing nullptr check for
    GetInputMethod(). This method returns nullptr in tests, and is checked
    in the other places it's used in the file.
    
    Bug: 797354
    Change-Id: I9537028b8cb0abfdb5316efd5bae5518055f5ecf
    Reviewed-on: https://chromium-review.googlesource.com/998835
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548644}

--- ui/views/controls/textfield/textfield.cc
+++ ui/views/controls/textfield/textfield.cc
@@ -538,7 +538,8 @@ void Textfield::SetHorizontalAlignment(gfx::HorizontalAlignment alignment) {
 }
 
 void Textfield::ShowImeIfNeeded() {
-  if (enabled() && !read_only())
+  // GetInputMethod() may return nullptr in tests.
+  if (enabled() && !read_only() && GetInputMethod())
     GetInputMethod()->ShowImeIfNeeded();
 }
 
