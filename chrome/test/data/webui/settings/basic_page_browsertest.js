7972748d7e07c73d8d6263326cc6e93f950d2e77
Demetrios Papadopoulos <dpapad@chromium.org>
2018-04-06 19:14:16 +0000

    Revert "Settings WebUI: when text changes in settings, redo search"
    This reverts commit 90a0340239b22badcac1a10665d4b2a21010c408.
    
    Reason for revert:
    Caused corner cases where the UI disappears unexpectedly after the user interacts with it.
    
    Original change's description:
    > Settings WebUI: when text changes in settings, redo search
    > 
    > Bug: 711156
    > Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    > Change-Id: Ic2847a86c7ec3671db79e524a25fa15c08193455
    > Reviewed-on: https://chromium-review.googlesource.com/996768
    > Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    > Commit-Queue: Esmael El-Moslimany <aee@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#548689}
    
    TBR=hcarmona@chromium.org,scottchen@chromium.org,aee@chromium.org
    
    Change-Id: Ia5b01260673e6a781f654b11f8a49bd222a91631
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 711156
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Reviewed-on: https://chromium-review.googlesource.com/998082
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548887}

--- chrome/test/data/webui/settings/basic_page_browsertest.js
+++ chrome/test/data/webui/settings/basic_page_browsertest.js
@@ -46,7 +46,8 @@ TEST_F('SettingsBasicPageBrowserTest', 'DISABLED_Load', function() {
     /** @override */
     search(text, page) {
       if (this.searchRequest_ == null || !this.searchRequest_.isSame(text)) {
-        this.searchRequest_ = new settings.SearchRequest(text);
+        this.searchRequest_ = new settings.SearchRequest(
+            text, document.createElement('div'));
         this.searchRequest_.finished = true;
         this.searchRequest_.updateMatches(false);
 
@@ -66,9 +67,6 @@ TEST_F('SettingsBasicPageBrowserTest', 'DISABLED_Load', function() {
       }
       return this.searchRequest_.resolver.promise;
     }
-
-    /** @override */
-    registerObserver(observer) {}
   }
 
   // Register mocha tests.
