ed6600943e7e09ed121cf7d3f1d412d334f5e0cd
Leonard Grey <lgrey@chromium.org>
2018-04-06 14:13:45 +0000

    MacViews: delete entire composed glyph when deleting backward
    This matches NSTextView's behavior by creating a CFString and asking
    what Cocoa thinks should happen. This also handles UTF-16 surrogate
    pairs, so this change splits that check to the non-Mac code path.
    
    Bug: 826794
    Change-Id: I8fce5510e3cd483533699edd1ba33118ce55b44e
    Reviewed-on: https://chromium-review.googlesource.com/986475
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548769}

--- ui/views/style/platform_style.h
+++ ui/views/style/platform_style.h
@@ -12,6 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/button/button.h"
 #include "ui/views/views_export.h"
 
+namespace gfx {
+class Range;
+}  // namespace gfx
+
 namespace views {
 
 class Border;
@@ -87,6 +91,14 @@ class VIEWS_EXPORT PlatformStyle {
   // Called whenever a textfield edit fails. Gives visual/audio feedback about
   // the failed edit if platform-appropriate.
   static void OnTextfieldEditFailed();
+
+  // When deleting backwards in |string| with the cursor at index
+  // |cursor_position|, return the range of UTF-16 words to be deleted.
+  // This is to support deleting entire graphemes instead of individual
+  // characters when necessary on Mac, and code points made from surrogate
+  // pairs on other platforms.
+  static gfx::Range RangeToDeleteBackwards(const base::string16& text,
+                                           size_t cursor_position);
 
  private:
   DISALLOW_IMPLICIT_CONSTRUCTORS(PlatformStyle);
