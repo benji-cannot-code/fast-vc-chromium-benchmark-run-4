d7f485d1bbd0ba0826385b14b8ffa8210bafde11
Mikhail Pozdnyakov <mikhail.pozdnyakov@intel.com>
2018-04-06 15:30:43 +0000

    [sensor] Fix origin in SensorProviderProxyImpl::CheckPermission()
    Origin must be extracted from the URL before it is passed to
    GetPermissionStatusForFrame().
    
    Bug: 750018
    Change-Id: Iffffbf4ec516cb1a798c3604333a71d3bdac5e9e
    Reviewed-on: https://chromium-review.googlesource.com/998160
    Reviewed-by: Alexander Shalamov <alexander.shalamov@intel.com>
    Commit-Queue: Mikhail Pozdnyakov <mikhail.pozdnyakov@intel.com>
    Cr-Commit-Position: refs/heads/master@{#548781}

--- content/browser/generic_sensor/sensor_provider_proxy_impl.cc
+++ content/browser/generic_sensor/sensor_provider_proxy_impl.cc
@@ -67,7 +67,8 @@ void SensorProviderProxyImpl::GetSensor(SensorType type,
 }
 
 bool SensorProviderProxyImpl::CheckPermission() const {
-  const GURL& requesting_origin = render_frame_host_->GetLastCommittedURL();
+  const GURL& requesting_origin =
+      render_frame_host_->GetLastCommittedURL().GetOrigin();
 
   blink::mojom::PermissionStatus permission_status =
       permission_manager_->GetPermissionStatusForFrame(
