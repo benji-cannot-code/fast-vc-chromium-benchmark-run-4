6d3e68969324b7905251993bbdc83202c1756a4c
Steven Bennetts <stevenjb@chromium.org>
2018-04-06 19:13:12 +0000

    DisplayPrefs cleanup
    This removes display_pref_util.h and simplifies some code.
    
    Bug: 678949
    Change-Id: I4e6d805a2fdec4b3f05dd64dbf183ed569ae1eb4
    Reviewed-on: https://chromium-review.googlesource.com/994412
    Reviewed-by: Malay Keshav <malaykeshav@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548885}

--- ui/display/manager/json_converter.cc
+++ ui/display/manager/json_converter.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/values.h"
 #include "ui/display/display_layout.h"
-#include "ui/display/manager/display_pref_util.h"
 
 namespace display {
 
