edac64919a795d6ea2cc7c0ee23df901cd6216f7
Yuri Wiitala <miu@chromium.org>
2018-04-06 03:42:05 +0000

    Fork media::AudioOutputController → audio::OutputController
    In order to break-away from the legacy threading model and eliminate the
    need for future circular dependencies on src/media/(audio|base), this
    change forks AudioOutputController and AudioSyncReader into
    src/services/audio.
    
    In addition to the namespace and class naming changes, the following
    modifications have been made on the forked code:
    
    1. Removed the ref-counting of audio::OutputController, eliminated the
    Create() factory methods, and changed a few heap-allocated objects into
    proper class data members (i.e., composition instead of indirection).
    
    2. Removed the XYZ()→DoXYZ() thread-trampoline methods.
    
    3. Updated comments that became misleading or are not otherwise accurate
    to how things work in the new Audio Service.
    
    4. Changed the audio::OutputStream to use the forked OutputController
    and SyncReader.
    
    5. Updated unit tests.
    
    Further clean-ups and code deletions/refactorings are intentionally
    being left TODO in future patches.
    
    Bug: 824019, 803102
    Change-Id: If479c63909c252c8fca135c9a51dcac76feeec8c
    Reviewed-on: https://chromium-review.googlesource.com/982904
    Commit-Queue: Yuri Wiitala <miu@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Reviewed-by: Max Morin <maxmorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548658}

--- services/audio/output_stream.h
+++ services/audio/output_stream.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_piece.h"
 #include "base/sync_socket.h"
 #include "base/timer/timer.h"
-#include "media/audio/audio_output_controller.h"
 #include "media/mojo/interfaces/audio_data_pipe.mojom.h"
 #include "media/mojo/interfaces/audio_logging.mojom.h"
 #include "media/mojo/interfaces/audio_output_stream.mojom.h"
@@ -24,6 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/system/buffer.h"
 #include "mojo/public/cpp/system/handle.h"
 #include "mojo/public/cpp/system/platform_handle.h"
+#include "services/audio/output_controller.h"
+#include "services/audio/sync_reader.h"
 
 namespace base {
 class UnguessableToken;
@@ -32,13 +33,12 @@ class UnguessableToken;
 namespace media {
 class AudioManager;
 class AudioParameters;
-class AudioSyncReader;
 }  // namespace media
 
 namespace audio {
 
 class OutputStream final : public media::mojom::AudioOutputStream,
-                           public media::AudioOutputController::EventHandler {
+                           public OutputController::EventHandler {
  public:
   using DeleteCallback = base::OnceCallback<void(OutputStream*)>;
   using CreatedCallback =
@@ -62,31 +62,30 @@ class OutputStream final : public media::mojom::AudioOutputStream,
   void Pause() final;
   void SetVolume(double volume) final;
 
-  // AudioOutputController::EventHandler implementation.
-  void OnControllerCreated() final;
+  // OutputController::EventHandler implementation.
   void OnControllerPlaying() final;
   void OnControllerPaused() final;
   void OnControllerError() final;
   void OnLog(base::StringPiece message) final;
 
  private:
+  void CreateAudioPipe(CreatedCallback created_callback);
   void OnError();
   void CallDeleter();
   void PollAudioLevel();
-  bool IsAudible() const;
+  bool IsAudible();
 
   SEQUENCE_CHECKER(owning_sequence_);
 
   base::CancelableSyncSocket foreign_socket_;
-  CreatedCallback created_callback_;
   DeleteCallback delete_callback_;
   mojo::Binding<AudioOutputStream> binding_;
   media::mojom::AudioOutputStreamClientPtr client_;
   media::mojom::AudioOutputStreamObserverAssociatedPtr observer_;
   scoped_refptr<media::mojom::ThreadSafeAudioLogPtr> log_;
 
-  const std::unique_ptr<media::AudioSyncReader> reader_;
-  scoped_refptr<media::AudioOutputController> controller_;
+  SyncReader reader_;
+  OutputController controller_;
 
   // This flag ensures that we only send OnStreamStateChanged notifications
   // and (de)register with the stream monitor when the state actually changes.
