ddd9f9040d17e748b541f8bd07dc0759f682df6b
Ke He <ke.he@intel.com>
2018-04-07 04:52:08 +0000

    Set the PowerMonitor singleton before PowerMonitorBroadcastSource::Client::Init()
    The PowerMonitorBroadcastSource::Client::Init() connects Device Service on
    IO thread, which will invoke a power state change mojo-call back. This callback
    on the IO thread can race with PowerMonitor initialization on the main thread
    and cause a crash if the writes on the main thread are made visible to the IO
    thread in a reordered fashion.
    
    We split the Init() out and call it in main thread after setting the
    PowerMonitor singleton.In this way we can make sure there is no race between
    main thread and IO thread.
    
    BUG=821790
    
    Change-Id: I6a9b4e7d16316b0826b9e6e3355b03971d9dcb80
    Reviewed-on: https://chromium-review.googlesource.com/989838
    Commit-Queue: Ke He <ke.he@intel.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549037}

--- services/device/public/cpp/power_monitor/power_monitor_broadcast_source.cc
+++ services/device/public/cpp/power_monitor/power_monitor_broadcast_source.cc
@@ -16,27 +16,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace device {
 
 PowerMonitorBroadcastSource::PowerMonitorBroadcastSource(
-    service_manager::Connector* connector,
     scoped_refptr<base::SequencedTaskRunner> task_runner)
     : PowerMonitorBroadcastSource(std::make_unique<Client>(),
-                                  connector,
                                   task_runner) {}
 
 PowerMonitorBroadcastSource::PowerMonitorBroadcastSource(
     std::unique_ptr<Client> client,
-    service_manager::Connector* connector,
     scoped_refptr<base::SequencedTaskRunner> task_runner)
-    : client_(std::move(client)), task_runner_(task_runner) {
+    : client_(std::move(client)), task_runner_(task_runner) {}
+
+PowerMonitorBroadcastSource::~PowerMonitorBroadcastSource() {
+  task_runner_->DeleteSoon(FROM_HERE, client_.release());
+}
+
+void PowerMonitorBroadcastSource::Init(service_manager::Connector* connector) {
   if (connector) {
     task_runner_->PostTask(
         FROM_HERE, base::BindOnce(&PowerMonitorBroadcastSource::Client::Init,
                                   base::Unretained(client_.get()),
                                   base::Passed(connector->Clone())));
   }
-}
-
-PowerMonitorBroadcastSource::~PowerMonitorBroadcastSource() {
-  task_runner_->DeleteSoon(FROM_HERE, client_.release());
 }
 
 bool PowerMonitorBroadcastSource::IsOnBatteryPowerImpl() {
