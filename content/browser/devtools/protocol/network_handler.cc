89f19824582edd41f695873c776d69caa2cdf4e9
Pavel Feldman <pfeldman@chromium.org>
2018-04-07 01:17:52 +0000

    DevTools: make pptr tests pass with DCHECKs.
    Change-Id: Ibc113790395add5ee5cfe773fe33f41329cbe2bc
    Reviewed-on: https://chromium-review.googlesource.com/1000730
    Reviewed-by: Andrey Lushnikov <lushnikov@chromium.org>
    Commit-Queue: Andrey Lushnikov <lushnikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549003}

--- content/browser/devtools/protocol/network_handler.cc
+++ content/browser/devtools/protocol/network_handler.cc
@@ -341,7 +341,7 @@ void DeleteSelectedCookiesOnIO(net::URLRequestContextGetter* context_getter,
         cookie, std::move(once_callback));
   }
   if (!filtered_list.size())
-    std::move(callback).Run();
+    DeletedCookiesOnIO(std::move(callback), 0);
 }
 
 void DeleteCookiesOnIO(net::URLRequestContextGetter* context_getter,
