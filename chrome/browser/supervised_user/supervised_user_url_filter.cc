d2f85d78d47aa4e3d11921d82f244ce2d228b16a
Eduardo Cordeiro <escordeiro@google.com>
2018-04-09 10:52:56 +0000

    Enforcing that the HTTP variant of families.google.com is always accessible to CHILD accounts
    Bug: b/77234786
    Change-Id: Ieb06e82dfa9fc2a5affce9299c84db75d5b4acbb
    Reviewed-on: https://chromium-review.googlesource.com/999798
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Commit-Queue: Eduardo Cordeiro <escordeiro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549146}

--- chrome/browser/supervised_user/supervised_user_url_filter.cc
+++ chrome/browser/supervised_user/supervised_user_url_filter.cc
@@ -97,7 +97,8 @@ const char* const kCrxDownloadUrls[] = {
 #endif
 
 // Whitelisted origins:
-const char kFamiliesUrl[] = "https://families.google.com/";
+const char kFamiliesSecureUrl[] = "https://families.google.com/";
+const char kFamiliesUrl[] = "http://families.google.com/";
 
 // This class encapsulates all the state that is required during construction of
 // a new SupervisedUserURLFilter::Contents.
@@ -368,7 +369,8 @@ SupervisedUserURLFilter::GetFilteringBehaviorForURL(
 
   // Allow navigations to whitelisted origins (currently families.google.com).
   static const base::NoDestructor<base::flat_set<GURL>> kWhitelistedOrigins(
-      base::flat_set<GURL>({GURL(kFamiliesUrl).GetOrigin()}));
+      base::flat_set<GURL>({GURL(kFamiliesUrl).GetOrigin(),
+                            GURL(kFamiliesSecureUrl).GetOrigin()}));
   if (base::ContainsKey(*kWhitelistedOrigins, effective_url.GetOrigin()))
     return ALLOW;
 
