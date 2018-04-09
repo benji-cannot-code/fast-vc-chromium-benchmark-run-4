036ad3c38702775cf2e61bd8f6d29d7c9ef35cff
Dominic Battre <battre@chromium.org>
2018-04-09 10:31:11 +0000

    Record form and URL signatures in UKM
    This CL introduces low entropy hashes of password form signatures and URLs.
    This allows us to distinguish different forms on a page as well as different
    pages for which we cannot record the full URL in UKM.
    
    Bug: 732846
    Change-Id: Ie98ef254286890310f6b6b5a2e3026260b65b676
    Reviewed-on: https://chromium-review.googlesource.com/977904
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Reviewed-by: Maxim Kolosovskiy <kolos@chromium.org>
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549141}

--- components/password_manager/core/browser/password_form_metrics_recorder.h
+++ components/password_manager/core/browser/password_form_metrics_recorder.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "components/autofill/core/common/password_form.h"
+#include "components/autofill/core/common/signatures_util.h"
 #include "components/password_manager/core/browser/password_form_user_action.h"
 #include "components/password_manager/core/browser/password_manager_metrics_util.h"
 #include "services/metrics/public/cpp/ukm_builders.h"
@@ -223,6 +224,13 @@ class PasswordFormMetricsRecorder
 
   // Records a DetailedUserAction UKM metric.
   void RecordDetailedUserAction(DetailedUserAction action);
+
+  // Hash algorithm for RecordFormSignature. Public for testing.
+  static int64_t HashFormSignature(autofill::FormSignature form_signature);
+
+  // Records a low entropy hash of the form signature in order to be able to
+  // distinguish two forms on the same site.
+  void RecordFormSignature(autofill::FormSignature form_signature);
 
  private:
   friend class base::RefCounted<PasswordFormMetricsRecorder>;
