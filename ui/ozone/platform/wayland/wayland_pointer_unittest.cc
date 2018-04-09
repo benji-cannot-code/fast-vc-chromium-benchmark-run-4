c791366c0f1be390a8a602fb5ee0015b1107c13e
Scott Violet <sky@chromium.org>
2018-04-09 17:28:49 +0000

    ozone: fix some minor style issues
    BUG=none
    TEST=none
    
    Change-Id: I387c43f58eaf7bf3a1f2dd01c40c9fd6974b3413
    Reviewed-on: https://chromium-review.googlesource.com/1002953
    Reviewed-by: Michael Spang <spang@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549212}

--- ui/ozone/platform/wayland/wayland_pointer_unittest.cc
+++ ui/ozone/platform/wayland/wayland_pointer_unittest.cc
@@ -31,7 +31,7 @@ class WaylandPointerTest : public WaylandTest {
 
     Sync();
 
-    pointer_ = server_.seat()->pointer_.get();
+    pointer_ = server_.seat()->pointer();
     ASSERT_TRUE(pointer_);
   }
 
--- ui/ozone/platform/wayland/wayland_touch_unittest.cc
+++ ui/ozone/platform/wayland/wayland_touch_unittest.cc
@@ -39,7 +39,7 @@ class WaylandTouchTest : public WaylandTest {
 
     Sync();
 
-    touch_ = server_.seat()->touch_.get();
+    touch_ = server_.seat()->touch();
     ASSERT_TRUE(touch_);
   }
 
