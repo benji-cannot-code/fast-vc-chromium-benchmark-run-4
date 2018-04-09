5a5386cd100c2cba80385161a023269b545c1539
Theresa <twellington@chromium.org>
2018-04-09 22:54:48 +0000

    [EoC] Send clearState() signal to backend
    BUG=
    
    Change-Id: I458e270f5740451e4898040077474d318c0cddc2
    Reviewed-on: https://chromium-review.googlesource.com/1003280
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Commit-Queue: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549309}

--- chrome/android/java/src/org/chromium/chrome/browser/contextual_suggestions/ContextualSuggestionsMediator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextual_suggestions/ContextualSuggestionsMediator.java
@@ -198,6 +198,8 @@ class ContextualSuggestionsMediator implements EnabledStateMonitor.Observer, Fet
         mCoordinator.removeSuggestions();
         mCurrentRequestUrl = "";
 
+        if (mSuggestionsSource != null) mSuggestionsSource.getBridge().clearState();
+
         if (mSheetObserver != null) {
             mCoordinator.removeBottomSheetObserver(mSheetObserver);
         }
