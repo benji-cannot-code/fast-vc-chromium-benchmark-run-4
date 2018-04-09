535d87b5943caa7a1d0b1e80f04c577f2f84f4a5
Olga Sharonova <olka@chromium.org>
2018-04-09 20:10:10 +0000

    media::AudioSystem: disconnect from Audio service when not in use.
    Also added mojo::InterfacePtr<>::IsExpectingResponse().
    
    Bug: 672469, 792441
    Change-Id: I9c508cafae84b393e226834c4236724d59641829
    Reviewed-on: https://chromium-review.googlesource.com/998166
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Reviewed-by: Max Morin <maxmorin@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549254}

--- services/audio/public/cpp/audio_system_to_service_adapter.cc
+++ services/audio/public/cpp/audio_system_to_service_adapter.cc
@@ -14,11 +14,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace audio {
 
 AudioSystemToServiceAdapter::AudioSystemToServiceAdapter(
-    std::unique_ptr<service_manager::Connector> connector)
+    std::unique_ptr<service_manager::Connector> connector,
+    base::TimeDelta disconnect_timeout)
     : connector_(std::move(connector)) {
   DCHECK(connector_);
   DETACH_FROM_THREAD(thread_checker_);
+  if (disconnect_timeout > base::TimeDelta()) {
+    disconnect_timer_.emplace(
+        FROM_HERE, disconnect_timeout, this,
+        &AudioSystemToServiceAdapter::DisconnectOnTimeout);
+  }
 }
+
+AudioSystemToServiceAdapter::AudioSystemToServiceAdapter(
+    std::unique_ptr<service_manager::Connector> connector)
+    : AudioSystemToServiceAdapter(std::move(connector), base::TimeDelta()) {}
 
 AudioSystemToServiceAdapter::~AudioSystemToServiceAdapter() {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
@@ -92,7 +102,20 @@ mojom::SystemInfo* AudioSystemToServiceAdapter::GetSystemInfo() {
                        base::Unretained(this)));
     DCHECK(system_info_);
   }
+  if (disconnect_timer_)
+    disconnect_timer_->Reset();
   return system_info_.get();
+}
+
+void AudioSystemToServiceAdapter::DisconnectOnTimeout() {
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+  DVLOG(4) << "AudioSystemToServiceAdapter::Disconnect";
+  if (system_info_.IsExpectingResponse()) {
+    if (disconnect_timer_)
+      disconnect_timer_->Reset();
+    return;
+  }
+  system_info_.reset();
 }
 
 void AudioSystemToServiceAdapter::OnConnectionError() {
--- services/audio/public/cpp/audio_system_to_service_adapter.h
+++ services/audio/public/cpp/audio_system_to_service_adapter.h
@@ -8,7 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/optional.h"
 #include "base/threading/thread_checker.h"
+#include "base/time/time.h"
+#include "base/timer/timer.h"
 #include "media/audio/audio_system.h"
 #include "services/audio/public/mojom/system_info.mojom.h"
 
@@ -23,9 +26,14 @@ namespace audio {
 // empty optionals / false booleans.
 class AudioSystemToServiceAdapter : public media::AudioSystem {
  public:
+  // If |disconnect_timeout| is positive, the instance will disconnect from
+  // Audio service upon |disconnect_timeout| if not in use, and reconnect on the
+  // next attempt to use it.
+  AudioSystemToServiceAdapter(
+      std::unique_ptr<service_manager::Connector> connector,
+      base::TimeDelta disconnect_timeout);
   explicit AudioSystemToServiceAdapter(
       std::unique_ptr<service_manager::Connector> connector);
-
   ~AudioSystemToServiceAdapter() override;
 
   // AudioSystem implementation.
@@ -49,11 +57,15 @@ class AudioSystemToServiceAdapter : public media::AudioSystem {
 
  private:
   mojom::SystemInfo* GetSystemInfo();
+  void DisconnectOnTimeout();
   void OnConnectionError();
 
   // Will be bound to the thread AudioSystemToServiceAdapter is used on.
   const std::unique_ptr<service_manager::Connector> connector_;
   mojom::SystemInfoPtr system_info_;
+
+  // To disconnect from the audio service when not in use.
+  base::Optional<base::DelayTimer> disconnect_timer_;
 
   THREAD_CHECKER(thread_checker_);
   DISALLOW_COPY_AND_ASSIGN(AudioSystemToServiceAdapter);
