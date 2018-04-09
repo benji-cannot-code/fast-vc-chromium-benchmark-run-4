d2a2ad26095864d7030cb7e128046209e005fd73
Rayan Kanso <rayankans@chromium.org>
2018-04-09 18:10:11 +0000

    [Background Fetch] Add BackgroundFetchSettledEvent idl files and basic implementation.
    Specs are defined in https://wicg.github.io/background-fetch/#backgroundfetchsettledevent.
    
    This is the first step in replacing the BackgroundFetched* API.
    
    Bug: 822765
    Change-Id: Ib725b0c389edc32563f45bd26d72621b7031ad31
    Reviewed-on: https://chromium-review.googlesource.com/975549
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549226}

---
+++ third_party/blink/renderer/modules/background_fetch/background_fetch_settled_event.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/modules/background_fetch/background_fetch_settled_event.h"
+
+namespace blink {
+
+BackgroundFetchSettledEvent::BackgroundFetchSettledEvent(
+    const AtomicString& type,
+    const BackgroundFetchSettledEventInit& initializer)
+    : BackgroundFetchEvent(type, initializer, nullptr /* observer */),
+      fetches_(*initializer.fetches()) {}
+
+BackgroundFetchSettledEvent::~BackgroundFetchSettledEvent() = default;
+
+BackgroundFetchSettledFetches* BackgroundFetchSettledEvent::fetches() const {
+  return fetches_;
+}
+
+void BackgroundFetchSettledEvent::Trace(blink::Visitor* visitor) {
+  visitor->Trace(fetches_);
+  BackgroundFetchEvent::Trace(visitor);
+}
+
+}  // namespace blink
