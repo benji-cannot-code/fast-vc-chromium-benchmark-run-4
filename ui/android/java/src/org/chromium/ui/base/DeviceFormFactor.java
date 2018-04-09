caf2bc0bf24f12619ba0053cd5ff50a648e14742
Andrew Grieve <agrieve@chromium.org>
2018-04-09 21:14:03 +0000

    Android: Fix isTablet()-related occuring on some JB tablets
    A better fix would be fix PhysicalDisplayAndroid.java, but given this is
    fixed in JB MR1, I don't think it's worth the extra effort.
    
    Bug: 829318
    Change-Id: Ibe03c8392bf86b69322ff01f9ddf3c63396a161f
    Reviewed-on: https://chromium-review.googlesource.com/1001707
    Commit-Queue: agrieve <agrieve@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549274}

--- ui/android/java/src/org/chromium/ui/base/DeviceFormFactor.java
+++ ui/android/java/src/org/chromium/ui/base/DeviceFormFactor.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.ui.base;
 
 import android.content.Context;
+import android.os.Build;
 import android.support.annotation.UiThread;
 
 import org.chromium.base.ContextUtils;
@@ -87,9 +88,12 @@ public class DeviceFormFactor {
     }
 
     private static int detectScreenWidthBucket(Context context) {
-        // TODO(agrieve): Remove runningOnUiThread() guard and audit for background thread usages.
-        //     https://crbug.com/669974
-        if (ThreadUtils.runningOnUiThread()
+        // Pre-JB MR1, Display.getSize() is used rather than Display.getRealSize().
+        // For our query, getSize() is not always correct. https://crbug.com/829318
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1
+                // TODO(agrieve): Remove thread check and audit for background usages.
+                //     https://crbug.com/669974
+                && ThreadUtils.runningOnUiThread()
                 && !isTabletDisplay(DisplayAndroid.getNonMultiDisplay(context))) {
             // There have been no cases where tablet resources end up being used on phone-sized
             // displays. Short-circuit this common-case since checking resources is slower (and
