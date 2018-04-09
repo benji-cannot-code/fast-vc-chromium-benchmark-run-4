de8db0ec04cb2015de92db50a00c09c31e768d38
Xiyuan Xia <xiyuan@chromium.org>
2018-04-09 21:36:12 +0000

    views: Remove NativeViewHost::SetPreferredSize
    Remove NativeViewHost::SetPreferredSize since it is implemented in
    View now. The View impl notifies parent only when the preferred
    size really changes.
    
    Bug: 830093
    Change-Id: I3887b3103e45fc06771f68c469258fb4e53bf4e0
    Reviewed-on: https://chromium-review.googlesource.com/1002918
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549285}

--- ui/views/controls/native/native_view_host.cc
+++ ui/views/controls/native/native_view_host.cc
@@ -49,11 +49,6 @@ void NativeViewHost::Detach() {
   Detach(false);
 }
 
-void NativeViewHost::SetPreferredSize(const gfx::Size& size) {
-  preferred_size_ = size;
-  PreferredSizeChanged();
-}
-
 bool NativeViewHost::SetCornerRadius(int corner_radius) {
   return native_wrapper_->SetCornerRadius(corner_radius);
 }
@@ -73,10 +68,6 @@ void NativeViewHost::NativeViewDestroyed() {
 
 ////////////////////////////////////////////////////////////////////////////////
 // NativeViewHost, View overrides:
-
-gfx::Size NativeViewHost::CalculatePreferredSize() const {
-  return preferred_size_;
-}
 
 void NativeViewHost::Layout() {
   if (!native_view_ || !native_wrapper_.get())
