cf6abfe749e50de9dae83f20f77fbcc27ae65c65
Oskar Sundbom <ossu@chromium.org>
2018-04-09 12:41:08 +0000

    Ensure remote WebRTC streams aren't unintentionally audible
    Before a stream has been added to an <audio> tag, its gain is set to
    1.0. Once a stream gets attached to an <audio> tag, its volume is set
    to zero until the tag starts playing. Since remote streams are mixed in
    WebRTC before being passed on to Chrome, streams that haven't been added
    to a tag can still be audible, if only one other stream has.
    
    This CL attempts to fix this by setting the initial gain of WebRTC
    remote streams to 0.
    
    Bug: chromium:810848
    Change-Id: I08b0acbd78d3f0bd06adbb70f62d42b66146a9f5
    Reviewed-on: https://chromium-review.googlesource.com/925266
    Commit-Queue: Oskar Sundbom <ossu@chromium.org>
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Reviewed-by: Tommi <tommi@chromium.org>
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549159}

--- content/renderer/media/webrtc/mock_peer_connection_dependency_factory.cc
+++ content/renderer/media/webrtc/mock_peer_connection_dependency_factory.cc
@@ -41,6 +41,19 @@ static typename V::iterator FindTrack(V* vector,
   return it;
 };
 
+MockWebRtcAudioSource::MockWebRtcAudioSource(bool is_remote)
+    : is_remote_(is_remote) {}
+void MockWebRtcAudioSource::RegisterObserver(ObserverInterface* observer) {}
+void MockWebRtcAudioSource::UnregisterObserver(ObserverInterface* observer) {}
+
+MockWebRtcAudioSource::SourceState MockWebRtcAudioSource::state() const {
+  return SourceState::kLive;
+}
+
+bool MockWebRtcAudioSource::remote() const {
+  return is_remote_;
+}
+
 MockMediaStream::MockMediaStream(const std::string& id) : id_(id) {}
 
 bool MockMediaStream::AddTrack(AudioTrackInterface* track) {
@@ -126,6 +139,7 @@ scoped_refptr<MockWebRtcAudioTrack> MockWebRtcAudioTrack::Create(
 
 MockWebRtcAudioTrack::MockWebRtcAudioTrack(const std::string& id)
     : id_(id),
+      source_(new rtc::RefCountedObject<MockWebRtcAudioSource>(true)),
       enabled_(true),
       state_(webrtc::MediaStreamTrackInterface::kLive) {}
 
@@ -136,8 +150,7 @@ std::string MockWebRtcAudioTrack::kind() const {
 }
 
 webrtc::AudioSourceInterface* MockWebRtcAudioTrack::GetSource() const {
-  NOTREACHED();
-  return nullptr;
+  return source_.get();
 }
 
 std::string MockWebRtcAudioTrack::id() const {
