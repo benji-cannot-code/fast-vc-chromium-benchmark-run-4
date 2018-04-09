cede0a9c0691e25a870c2b33a0a5f8ef571ca65a
Avi Drissman <avi@chromium.org>
2018-04-09 18:19:43 +0000

    Remove NOTIFICATION_RENDER_VIEW_CONTEXT_MENU_SHOWN.
    BUG=268984
    
    Change-Id: I169b9811b695934e7b4ec9420c7efcd02175e2e5
    Reviewed-on: https://chromium-review.googlesource.com/996014
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549228}

--- chrome/browser/renderer_context_menu/render_view_context_menu_browsertest_util.cc
+++ chrome/browser/renderer_context_menu/render_view_context_menu_browsertest_util.cc
@@ -7,33 +7,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/location.h"
+#include "base/run_loop.h"
 #include "base/single_thread_task_runner.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "chrome/app/chrome_command_ids.h"
-#include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/renderer_context_menu/render_view_context_menu.h"
-#include "content/public/browser/notification_service.h"
 #include "content/public/test/test_utils.h"
 
 ContextMenuNotificationObserver::ContextMenuNotificationObserver(
     int command_to_execute)
     : command_to_execute_(command_to_execute) {
-  registrar_.Add(this,
-                 chrome::NOTIFICATION_RENDER_VIEW_CONTEXT_MENU_SHOWN,
-                 content::NotificationService::AllSources());
+  RenderViewContextMenu::RegisterMenuShownCallbackForTesting(base::BindOnce(
+      &ContextMenuNotificationObserver::MenuShown, base::Unretained(this)));
 }
 
 ContextMenuNotificationObserver::~ContextMenuNotificationObserver() {
 }
 
-void ContextMenuNotificationObserver::Observe(
-    int type,
-    const content::NotificationSource& source,
-    const content::NotificationDetails& details) {
-  DCHECK_EQ(chrome::NOTIFICATION_RENDER_VIEW_CONTEXT_MENU_SHOWN, type);
-
-  RenderViewContextMenu* context_menu =
-      content::Source<RenderViewContextMenu>(source).ptr();
+void ContextMenuNotificationObserver::MenuShown(
+    RenderViewContextMenu* context_menu) {
   base::ThreadTaskRunnerHandle::Get()->PostTask(
       FROM_HERE,
       base::BindOnce(&ContextMenuNotificationObserver::ExecuteCommand,
@@ -46,43 +38,22 @@ void ContextMenuNotificationObserver::ExecuteCommand(
   context_menu->Cancel();
 }
 
-ContextMenuWaiter::ContextMenuWaiter(const content::NotificationSource& source)
-    : menu_visible_(false) {
-  registrar_.Add(this, chrome::NOTIFICATION_RENDER_VIEW_CONTEXT_MENU_SHOWN,
-                 content::NotificationService::AllSources());
+ContextMenuWaiter::ContextMenuWaiter() {
+  RenderViewContextMenu::RegisterMenuShownCallbackForTesting(
+      base::BindOnce(&ContextMenuWaiter::MenuShown, base::Unretained(this)));
 }
 
 ContextMenuWaiter::~ContextMenuWaiter() {
 }
 
-void ContextMenuWaiter::Observe(int type,
-                                const content::NotificationSource& source,
-                                const content::NotificationDetails& details) {
-  switch (type) {
-    case chrome::NOTIFICATION_RENDER_VIEW_CONTEXT_MENU_SHOWN: {
-      menu_visible_ = true;
-      RenderViewContextMenu* context_menu =
-          content::Source<RenderViewContextMenu>(source).ptr();
-      base::ThreadTaskRunnerHandle::Get()->PostTask(
-          FROM_HERE, base::BindOnce(&ContextMenuWaiter::Cancel,
-                                    base::Unretained(this), context_menu));
-      break;
-    }
-
-    default:
-      NOTREACHED();
-  }
+void ContextMenuWaiter::MenuShown(RenderViewContextMenu* context_menu) {
+  base::ThreadTaskRunnerHandle::Get()->PostTask(
+      FROM_HERE, base::BindOnce(&ContextMenuWaiter::Cancel,
+                                base::Unretained(this), context_menu));
 }
 
 void ContextMenuWaiter::WaitForMenuOpenAndClose() {
-  content::WindowedNotificationObserver menu_observer(
-      chrome::NOTIFICATION_RENDER_VIEW_CONTEXT_MENU_SHOWN,
-      content::NotificationService::AllSources());
-  if (!menu_visible_)
-    menu_observer.Wait();
-
-  content::RunAllPendingInMessageLoop();
-  menu_visible_ = false;
+  run_loop_.Run();
 }
 
 content::ContextMenuParams& ContextMenuWaiter::params() {
@@ -92,4 +63,5 @@ content::ContextMenuParams& ContextMenuWaiter::params() {
 void ContextMenuWaiter::Cancel(RenderViewContextMenu* context_menu) {
   params_ = context_menu->params();
   context_menu->Cancel();
+  run_loop_.Quit();
 }
