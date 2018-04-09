cc921151d87e29a90d5f96a9d717b4b27e59cc4e
bsheedy <bsheedy@chromium.org>
2018-04-09 21:14:32 +0000

    [VR] Fix initial repositioning frame opacity
    Currently, the frame/border around the VR browsing content window starts
    off opaque, and later becomes transparent due to bindings. This makes
    it start off transparent.
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_vr;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Iaaec76ef7b7ba4c8c54a55fc5a591327bb98d9f3
    Reviewed-on: https://chromium-review.googlesource.com/1000517
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549275}

--- chrome/browser/vr/elements/rect.cc
+++ chrome/browser/vr/elements/rect.cc
@@ -64,4 +64,8 @@ void Rect::NotifyClientFloatAnimated(float value,
   }
 }
 
+float Rect::ComputedAndLocalOpacityForTest() const {
+  return computed_opacity() * local_opacity_;
+}
+
 }  // namespace vr
--- chrome/browser/vr/ui_scene_creator.cc
+++ chrome/browser/vr/ui_scene_creator.cc
@@ -1240,6 +1240,7 @@ void UiSceneCreator::CreateContentQuad() {
                      kRepositionFrameEdgePadding, kRepositionFrameEdgePadding);
   frame->set_corner_radius(kContentCornerRadius);
   frame->set_bounds_contain_padding(false);
+  frame->SetLocalOpacity(0.0f);
   frame->SetTransitionedProperties({LOCAL_OPACITY});
   frame->SetTransitionDuration(
       base::TimeDelta::FromMilliseconds(kRepositionFrameTransitionDurationMs));
