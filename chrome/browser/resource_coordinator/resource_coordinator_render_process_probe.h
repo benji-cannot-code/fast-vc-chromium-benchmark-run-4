087fe607c4401386122c61323fbfa842a33b80a4
Sigurdur Asgeirsson <siggi@chromium.org>
2018-04-09 18:41:29 +0000

    Fix a UAF in render process probe.
    Bug: 829394
    Change-Id: I40384bdd8adf9d11f8647c3c5d2dc4c8775525b4
    Reviewed-on: https://chromium-review.googlesource.com/998012
    Commit-Queue: Sigurður Ásgeirsson <siggi@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549231}

--- chrome/browser/resource_coordinator/resource_coordinator_render_process_probe.h
+++ chrome/browser/resource_coordinator/resource_coordinator_render_process_probe.h
@@ -16,17 +16,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/process/process_metrics.h"
 #include "base/timer/timer.h"
 
-namespace content {
-class RenderProcessHost;
-}
-
 namespace resource_coordinator {
 
 struct RenderProcessInfo {
   RenderProcessInfo();
   ~RenderProcessInfo();
   double cpu_usage = -1.0;
-  content::RenderProcessHost* host = nullptr;
+  // This structure bounces from the UI thread to blocking threads and back.
+  // It's therefore not safe to store RenderProcessHost pointers, so the ID is
+  // used instead.
+  int render_process_host_id = 0;
   size_t last_gather_cycle_active;
   std::unique_ptr<base::ProcessMetrics> metrics;
 };
