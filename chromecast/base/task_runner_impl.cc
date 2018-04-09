d8b07a9ae16442845442966800051e301865fcc3
Mina Almasry <almasrymina@chromium.org>
2018-04-09 15:18:35 +0000

    Add AV sync unittests
    ...and fix issues uncovered by the unittests.
    
    BUG= internal 73746352
    TEST=cast_av_sync_backend_unittests, manual on device.
    
    Change-Id: Ib499983ea2dc99914a23d844d6c9a38926b6d725
    Reviewed-on: https://chromium-review.googlesource.com/989334
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Reviewed-by: Kenneth MacKay <kmackay@chromium.org>
    Commit-Queue: Mina Almasry <almasrymina@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549179}

--- chromecast/base/task_runner_impl.cc
+++ chromecast/base/task_runner_impl.cc
@@ -13,7 +13,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromecast {
 
 TaskRunnerImpl::TaskRunnerImpl()
-    : runner_(base::ThreadTaskRunnerHandle::Get()) {
+    : TaskRunnerImpl(base::ThreadTaskRunnerHandle::Get()) {}
+
+TaskRunnerImpl::TaskRunnerImpl(
+    scoped_refptr<base::SingleThreadTaskRunner> runner)
+    : runner_(std::move(runner)) {
   DCHECK(runner_.get());
 }
 
