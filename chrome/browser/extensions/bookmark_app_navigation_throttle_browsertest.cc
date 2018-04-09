1d97c0624b7275322c36ad12448417a90cf2c6cd
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2018-04-09 07:25:05 +0000

    desktop-pwas: Forward Content-Type for out-of-scope form submissions
    Otherwise POST form submissions could lose their encoding type.
    
    There are two parts to this change:
    
    1. Save the request headers in NavigationHandle, so that we can access
       the headers in NavigationThrottles.
    
    2. Change BookmarkAppNavigationThrottleUtils::OpenNewForegroundTab to
       also include the Content-Type header in the new navigation.
    
    Bug: 826158
    Change-Id: I30a575fcdfab15442bb331c20685a988df3b4def
    Reviewed-on: https://chromium-review.googlesource.com/991616
    Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Reviewed-by: Matt Giuca <mgiuca@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549119}

--- chrome/browser/extensions/bookmark_app_navigation_throttle_browsertest.cc
+++ chrome/browser/extensions/bookmark_app_navigation_throttle_browsertest.cc
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/escape.h"
 #include "net/cert/mock_cert_verifier.h"
 #include "net/dns/mock_host_resolver.h"
+#include "net/http/http_request_headers.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
 #include "net/test/embedded_test_server/http_request.h"
 #include "net/test/embedded_test_server/http_response.h"
@@ -76,6 +77,8 @@ enum class WindowAccessResult {
 
 namespace {
 
+const char kTextPlainEncType[] = "text/plain";
+
 const char kQueryParam[] = "test=";
 const char kQueryParamName[] = "test";
 
@@ -101,6 +104,10 @@ class BookmarkAppNavigationObserver : public content::TestNavigationObserver {
 
   bool last_navigation_is_post() const { return last_navigation_is_post_; }
 
+  const net::HttpRequestHeaders& last_request_headers() const {
+    return last_request_headers_;
+  }
+
   const scoped_refptr<network::ResourceRequestBody>&
   last_resource_request_body() const {
     return last_resource_request_body_;
@@ -110,12 +117,16 @@ class BookmarkAppNavigationObserver : public content::TestNavigationObserver {
   void OnDidFinishNavigation(
       content::NavigationHandle* navigation_handle) override {
     last_navigation_is_post_ = navigation_handle->IsPost();
+    last_request_headers_ = navigation_handle->GetRequestHeaders();
     last_resource_request_body_ = navigation_handle->GetResourceRequestBody();
     content::TestNavigationObserver::OnDidFinishNavigation(navigation_handle);
   }
 
   // True if the last navigation was a post.
   bool last_navigation_is_post_;
+
+  // The request headers of the last navigation.
+  net::HttpRequestHeaders last_request_headers_;
 
   // The request body of the last navigation if it was a post request.
   scoped_refptr<network::ResourceRequestBody> last_resource_request_body_;
@@ -377,6 +388,7 @@ void SubmitFormAndWait(content::WebContents* web_contents,
       "const form = document.createElement('form');"
       "form.action = '%s';"
       "form.method = '%s';"
+      "form.enctype = '%s';"
       "const input = document.createElement('input');"
       "input.name = '%s';"
       "form.appendChild(input);"
@@ -388,17 +400,26 @@ void SubmitFormAndWait(content::WebContents* web_contents,
       "})();",
       target_url.spec().c_str(),
       method == net::URLFetcher::RequestType::POST ? "post" : "get",
-      kQueryParamName);
+      kTextPlainEncType, kQueryParamName);
   ASSERT_TRUE(content::ExecuteScript(web_contents, script));
   observer.WaitForNavigationFinished();
 
   EXPECT_EQ(is_post, observer.last_navigation_is_post());
   if (is_post) {
+    const net::HttpRequestHeaders& headers = observer.last_request_headers();
+    std::string post_content_type;
+    headers.GetHeader(net::HttpRequestHeaders::kContentType,
+                      &post_content_type);
+    EXPECT_EQ(kTextPlainEncType, post_content_type);
+
     const std::vector<network::DataElement>* elements =
         observer.last_resource_request_body()->elements();
     EXPECT_EQ(1u, elements->size());
     const auto& element = elements->front();
-    EXPECT_EQ(kQueryParam, std::string(element.bytes(), element.length()));
+
+    // The text/plain enconding algorithm appends "\r\n".
+    EXPECT_EQ(std::string(kQueryParam) + "\r\n",
+              std::string(element.bytes(), element.length()));
   }
 }
 
--- content/browser/frame_host/navigation_handle_impl.h
+++ content/browser/frame_host/navigation_handle_impl.h
@@ -70,6 +70,7 @@ class CONTENT_EXPORT NavigationHandleImpl : public NavigationHandle {
       const base::Optional<std::string>& suggested_filename,
       std::unique_ptr<NavigationUIData> navigation_ui_data,
       const std::string& method = std::string(),
+      net::HttpRequestHeaders request_headers = net::HttpRequestHeaders(),
       scoped_refptr<network::ResourceRequestBody> resource_request_body =
           nullptr,
       const Referrer& sanitized_referrer = content::Referrer(),
@@ -130,6 +131,7 @@ class CONTENT_EXPORT NavigationHandleImpl : public NavigationHandle {
   bool ShouldUpdateHistory() override;
   const GURL& GetPreviousURL() override;
   net::HostPortPair GetSocketAddress() override;
+  const net::HttpRequestHeaders& GetRequestHeaders() override;
   const net::HttpResponseHeaders* GetResponseHeaders() override;
   net::HttpResponseInfo::ConnectionInfo GetConnectionInfo() override;
   const net::SSLInfo& GetSSLInfo() override;
@@ -383,6 +385,7 @@ class CONTENT_EXPORT NavigationHandleImpl : public NavigationHandle {
       const base::Optional<std::string>& suggested_filename,
       std::unique_ptr<NavigationUIData> navigation_ui_data,
       const std::string& method,
+      net::HttpRequestHeaders request_headers,
       scoped_refptr<network::ResourceRequestBody> resource_request_body,
       const Referrer& sanitized_referrer,
       bool has_user_gesture,
@@ -457,6 +460,9 @@ class CONTENT_EXPORT NavigationHandleImpl : public NavigationHandle {
 
   // The HTTP method used for the navigation.
   std::string method_;
+
+  // The headers used for the request.
+  net::HttpRequestHeaders request_headers_;
 
   // The POST body associated with this navigation.  This will be null for GET
   // and/or other non-POST requests (or if a response to a POST request was a
