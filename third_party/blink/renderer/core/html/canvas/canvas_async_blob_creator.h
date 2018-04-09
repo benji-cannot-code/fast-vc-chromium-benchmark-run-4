a6b66dc5ecb5be340d400230627ed0bb897608e7
Hiroki Nakagawa <nhiroki@chromium.org>
2018-04-09 06:12:03 +0000

    Make CanvasAsyncBlobCreator take a bare task runner bound with the parent Document
    This is a clean-up CL and doesn't change behavior.
    
    Before this CL, CanvasAsyncBlobCreator takes ParentFrameTaskRunners that is a
    set of task runners bound with the parent Document. However, the blob creater
    uses only one task runner type and doesn't have to take the full set of the task
    runners.
    
    This CL replaces ParentFrameTaskRunners with bare base::SingleThreadTaskRunner
    for simplification.
    
    Bug: 829119
    Change-Id: I656f42abac46bf723fc9aa614bdd9d3294c0e562
    Reviewed-on: https://chromium-review.googlesource.com/1002336
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Hajime Hoshi <hajimehoshi@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549112}

--- third_party/blink/renderer/core/html/canvas/canvas_async_blob_creator.h
+++ third_party/blink/renderer/core/html/canvas/canvas_async_blob_creator.h
@@ -9,11 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/location.h"
+#include "base/single_thread_task_runner.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_blob_callback.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/typed_arrays/dom_typed_array.h"
-#include "third_party/blink/renderer/core/workers/parent_frame_task_runners.h"
 #include "third_party/blink/renderer/platform/geometry/int_size.h"
 #include "third_party/blink/renderer/platform/graphics/static_bitmap_image.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
@@ -117,7 +117,7 @@ class CORE_EXPORT CanvasAsyncBlobCreator
   ToBlobFunctionType function_type_;
 
   // Used when CanvasAsyncBlobCreator runs on main thread only
-  Member<ParentFrameTaskRunners> parent_frame_task_runner_;
+  scoped_refptr<base::SingleThreadTaskRunner> parent_frame_task_runner_;
 
   // Used for HTMLCanvasElement only
   //
