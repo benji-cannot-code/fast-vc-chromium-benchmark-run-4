3f5334a3a3e28e11419a1d2d6228fef6ef506abf
Rohit Rao <rohitrao@chromium.org>
2018-04-10 16:22:20 +0000

    [ios] Adds animations for Recent Tabs presentation and dismissal.
    Also adds a shadow to the presentation controller to visually separate the
    recent tabs content from the background views.
    
    BUG=805154
    
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Ifde4a44c0eac0157ca02fc93c7814a78b51cca12
    Reviewed-on: https://chromium-review.googlesource.com/998416
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549554}

--- ios/chrome/browser/ui/recent_tabs/recent_tabs_transitioning_delegate.mm
+++ ios/chrome/browser/ui/recent_tabs/recent_tabs_transitioning_delegate.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/recent_tabs/recent_tabs_transitioning_delegate.h"
 
+#import "ios/chrome/browser/ui/table_view/table_view_animator.h"
 #import "ios/chrome/browser/ui/table_view/table_view_presentation_controller.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -20,6 +21,36 @@ presentationControllerForPresentedViewController:(UIViewController*)presented
   return [[TableViewPresentationController alloc]
       initWithPresentedViewController:presented
              presentingViewController:presenting];
+}
+
+- (id<UIViewControllerAnimatedTransitioning>)
+animationControllerForPresentedController:(UIViewController*)presented
+                     presentingController:(UIViewController*)presenting
+                         sourceController:(UIViewController*)source {
+  UITraitCollection* traitCollection = presenting.traitCollection;
+  if (traitCollection.horizontalSizeClass == UIUserInterfaceSizeClassCompact &&
+      traitCollection.verticalSizeClass != UIUserInterfaceSizeClassCompact) {
+    // Use the default animator for fullscreen presentations.
+    return nil;
+  }
+
+  TableViewAnimator* animator = [[TableViewAnimator alloc] init];
+  animator.presenting = YES;
+  return animator;
+}
+
+- (id<UIViewControllerAnimatedTransitioning>)
+animationControllerForDismissedController:(UIViewController*)dismissed {
+  UITraitCollection* traitCollection = dismissed.traitCollection;
+  if (traitCollection.horizontalSizeClass == UIUserInterfaceSizeClassCompact &&
+      traitCollection.verticalSizeClass != UIUserInterfaceSizeClassCompact) {
+    // Use the default animator for fullscreen presentations.
+    return nil;
+  }
+
+  TableViewAnimator* animator = [[TableViewAnimator alloc] init];
+  animator.presenting = NO;
+  return animator;
 }
 
 @end
