6d58fd0e6cf537d9455396044d415dbce6da77a5
Peter Beverloo <peter@chromium.org>
2018-04-10 14:18:06 +0000

    Move the FakeGCMProfileService helper to the GCM Driver component
    TBR=jianli, finnur and jamescook for include path changes
    
    Bug: 829604
    Change-Id: Icb53714ede7a4ce40ca38ee4c79740b64c70997a
    Reviewed-on: https://chromium-review.googlesource.com/1001894
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Nicolas Zea (slow) <zea@chromium.org>
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549519}

--- chrome/browser/chromeos/net/wake_on_wifi_manager_unittest.cc
+++ chrome/browser/chromeos/net/wake_on_wifi_manager_unittest.cc
@@ -4,10 +4,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/chromeos/net/wake_on_wifi_connection_observer.h"
-#include "chrome/browser/gcm/fake_gcm_profile_service.h"
 #include "chrome/browser/gcm/gcm_profile_service_factory.h"
 #include "chrome/test/base/testing_profile.h"
 #include "chromeos/network/mock_network_device_handler.h"
+#include "components/gcm_driver/fake_gcm_profile_service.h"
 #include "content/public/test/test_browser_thread_bundle.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
