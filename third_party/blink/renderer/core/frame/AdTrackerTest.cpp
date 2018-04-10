bea845841f645dc8fb27ce20d1c08637d7c413da
Josh Karlin <jkarlin@chromium.org>
2018-04-10 13:48:20 +0000

    [AdTracking] Include the top of the stack in AdTracker detection
    Includes the script of the top stack frame in ads detection processing.
    
    Bug: 807640
    Change-Id: I8f4d0364ea088bf00abcc7310e4dccce7fec78f3
    Reviewed-on: https://chromium-review.googlesource.com/986337
    Commit-Queue: Josh Karlin <jkarlin@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549509}

--- third_party/blink/renderer/core/frame/AdTrackerTest.cpp
+++ third_party/blink/renderer/core/frame/AdTrackerTest.cpp
@@ -14,6 +14,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+namespace {
+
+class TestAdTracker : public AdTracker {
+ public:
+  explicit TestAdTracker(LocalFrame* frame) : AdTracker(frame) {}
+  void SetScriptAtTopOfStack(String url) { script_at_top_ = url; }
+  ~TestAdTracker() override {}
+
+ protected:
+  // Returns "" by default unless SetScriptAtTopOfStack is called with
+  // something else.
+  String ScriptAtTopOfStack(ExecutionContext* execution_context) override {
+    return script_at_top_;
+  }
+
+ private:
+  String script_at_top_;
+};
+
+}  // namespace
+
 class AdTrackerTest : public testing::Test {
  protected:
   void SetUp() override;
@@ -27,21 +48,22 @@ class AdTrackerTest : public testing::Test {
   }
 
   bool AnyExecutingScriptsTaggedAsAdResource() {
-    return ad_tracker_->AnyExecutingScriptsTaggedAsAdResource();
+    return ad_tracker_->AnyExecutingScriptsTaggedAsAdResource(
+        &page_holder_->GetDocument());
   }
 
   void AppendToKnownAdScripts(const KURL& url) {
     ad_tracker_->AppendToKnownAdScripts(url);
   }
 
-  Persistent<AdTracker> ad_tracker_;
+  Persistent<TestAdTracker> ad_tracker_;
   std::unique_ptr<DummyPageHolder> page_holder_;
 };
 
 void AdTrackerTest::SetUp() {
   page_holder_ = DummyPageHolder::Create(IntSize(800, 600));
   page_holder_->GetDocument().SetURL(KURL("https://example.com/foo"));
-  ad_tracker_ = new AdTracker(GetFrame());
+  ad_tracker_ = new TestAdTracker(GetFrame());
 }
 
 void AdTrackerTest::TearDown() {
@@ -63,6 +85,33 @@ TEST_F(AdTrackerTest, AnyExecutingScriptsTaggedAsAdResource_False) {
   WillExecuteScript("https://example.com/foo.js");
   WillExecuteScript("https://example.com/bar.js");
   EXPECT_FALSE(AnyExecutingScriptsTaggedAsAdResource());
+}
+
+TEST_F(AdTrackerTest, TopOfStackIncluded) {
+  KURL ad_script_url("https://example.com/ad.js");
+  AppendToKnownAdScripts(ad_script_url);
+
+  WillExecuteScript("https://example.com/foo.js");
+  WillExecuteScript("https://example.com/bar.js");
+  EXPECT_FALSE(AnyExecutingScriptsTaggedAsAdResource());
+
+  ad_tracker_->SetScriptAtTopOfStack("https://www.example.com/baz.js");
+  EXPECT_FALSE(AnyExecutingScriptsTaggedAsAdResource());
+
+  ad_tracker_->SetScriptAtTopOfStack(ad_script_url);
+  EXPECT_TRUE(AnyExecutingScriptsTaggedAsAdResource());
+
+  ad_tracker_->SetScriptAtTopOfStack("https://www.example.com/baz.js");
+  EXPECT_FALSE(AnyExecutingScriptsTaggedAsAdResource());
+
+  ad_tracker_->SetScriptAtTopOfStack("");
+  EXPECT_FALSE(AnyExecutingScriptsTaggedAsAdResource());
+
+  ad_tracker_->SetScriptAtTopOfStack(WTF::String());
+  EXPECT_FALSE(AnyExecutingScriptsTaggedAsAdResource());
+
+  WillExecuteScript(ad_script_url);
+  EXPECT_TRUE(AnyExecutingScriptsTaggedAsAdResource());
 }
 
 }  // namespace blink
