bef6d8305653d3c5c21209ca54780cda67a80fa5
Hirokazu Honda <hiroh@chromium.org>
2018-04-10 11:17:40 +0000

    Refactor UnwrapFdFromMojoHandle
    There are many declarations to create scopedFD from MojoHandle.
    Create util header file, arc_video_accelerator_util.h, to collect the duplicated
    declarations.
    
    BUG=None
    TEST=Play Youtube App
    
    Change-Id: I9252757831c5c1edd34ebc4bd82bf93c991166d0
    Reviewed-on: https://chromium-review.googlesource.com/1002715
    Reviewed-by: Kuang-che Wu <kcwu@chromium.org>
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549483}

--- components/arc/video_accelerator/gpu_arc_video_protected_buffer_allocator.cc
+++ components/arc/video_accelerator/gpu_arc_video_protected_buffer_allocator.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/platform_file.h"
 #include "base/files/scoped_file.h"
 #include "base/numerics/safe_conversions.h"
+#include "components/arc/video_accelerator/arc_video_accelerator_util.h"
 #include "components/arc/video_accelerator/protected_buffer_allocator.h"
 #include "components/arc/video_accelerator/protected_buffer_manager.h"
 #include "media/gpu/format_utils.h"
@@ -19,24 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/size.h"
 
 #define VLOGF(level) VLOG(level) << __func__ << "(): "
-
-namespace {
-// TODO(hiroh): Refactor UnwrapFdFromMojoHandle not to declare multiple times.
-base::ScopedFD UnwrapFdFromMojoHandle(mojo::ScopedHandle handle) {
-  if (!handle.is_valid()) {
-    VLOGF(1) << "Handle is invalid.";
-    return base::ScopedFD();
-  }
-  base::PlatformFile platform_file;
-  MojoResult mojo_result =
-      mojo::UnwrapPlatformFile(std::move(handle), &platform_file);
-  if (mojo_result != MOJO_RESULT_OK) {
-    VLOGF(1) << "UnwrapPlatformFile failed: " << mojo_result;
-    return base::ScopedFD();
-  }
-  return base::ScopedFD(platform_file);
-}
-}  // namespace
 
 namespace arc {
 // static
--- components/arc/video_accelerator/protected_buffer_manager_proxy.cc
+++ components/arc/video_accelerator/protected_buffer_manager_proxy.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/arc/video_accelerator/protected_buffer_manager_proxy.h"
 
+#include "components/arc/video_accelerator/arc_video_accelerator_util.h"
 #include "components/arc/video_accelerator/protected_buffer_manager.h"
 #include "mojo/public/cpp/system/platform_handle.h"
 
@@ -20,24 +21,6 @@ GpuArcProtectedBufferManagerProxy::GpuArcProtectedBufferManagerProxy(
 
 GpuArcProtectedBufferManagerProxy::~GpuArcProtectedBufferManagerProxy() {}
 
-base::ScopedFD GpuArcProtectedBufferManagerProxy::UnwrapFdFromMojoHandle(
-    mojo::ScopedHandle handle) {
-  base::PlatformFile platform_file;
-  MojoResult mojo_result =
-      mojo::UnwrapPlatformFile(std::move(handle), &platform_file);
-  if (mojo_result != MOJO_RESULT_OK) {
-    VLOGF(1) << "UnwrapPlatformFile failed: " << mojo_result;
-    return base::ScopedFD();
-  }
-
-  return base::ScopedFD(platform_file);
-}
-
-mojo::ScopedHandle GpuArcProtectedBufferManagerProxy::WrapFdInMojoHandle(
-    base::ScopedFD fd) {
-  return mojo::WrapPlatformFile(fd.release());
-}
-
 void GpuArcProtectedBufferManagerProxy::GetProtectedSharedMemoryFromHandle(
     mojo::ScopedHandle dummy_handle,
     GetProtectedSharedMemoryFromHandleCallback callback) {
@@ -48,7 +31,7 @@ void GpuArcProtectedBufferManagerProxy::GetProtectedSharedMemoryFromHandle(
           ->GetProtectedSharedMemoryHandleFor(std::move(unwrapped_fd))
           .Release());
 
-  std::move(callback).Run(WrapFdInMojoHandle(std::move(shmem_fd)));
+  std::move(callback).Run(mojo::WrapPlatformFile(shmem_fd.release()));
 }
 
 }  // namespace arc
