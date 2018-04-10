f5d673502a664d91dea5c36f370c89d22c6a419e
Anna Offenwanger <offenwanger@chromium.org>
2018-04-10 22:12:37 +0000

    Add XR Presentation Start Action UMA
    Adding a histogram to track how the user got into XR Presentation.
    
    Bug: 808589
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_vr;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Iad597f74fe22afac5709ebee1525c0b342cc24a4
    Reviewed-on: https://chromium-review.googlesource.com/999019
    Commit-Queue: Anna Offenwanger <offenwanger@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549658}

--- chrome/browser/vr/metrics/session_metrics_helper.cc
+++ chrome/browser/vr/metrics/session_metrics_helper.cc
@@ -287,7 +287,7 @@ void SessionMetricsHelper::RecordVrStartAction(VrStartAction action) {
   if (!page_session_tracker_ || mode_ == Mode::kNoVr) {
     pending_page_session_start_action_ = action;
   } else {
-    MaybeSetVrStartAction(action);
+    LogVrStartAction(action);
   }
 }
 
@@ -296,8 +296,7 @@ void SessionMetricsHelper::RecordPresentationStartAction(
   if (!presentation_session_tracker_ || mode_ != Mode::kWebXrVrPresentation) {
     pending_presentation_start_action_ = action;
   } else {
-    presentation_session_tracker_->ukm_entry()->SetStartAction(action);
-    pending_presentation_start_action_ = base::nullopt;
+    LogPresentationStartAction(action);
   }
 }
 
@@ -313,15 +312,24 @@ void SessionMetricsHelper::ReportRequestPresent() {
   }
 }
 
-void SessionMetricsHelper::MaybeSetVrStartAction(VrStartAction action) {
-  UMA_HISTOGRAM_ENUMERATION("XR.VRSession.StartAction", action,
-                            VrStartAction::kVrStartActionLast);
+void SessionMetricsHelper::LogVrStartAction(VrStartAction action) {
   DCHECK(page_session_tracker_);
+
+  UMA_HISTOGRAM_ENUMERATION("XR.VRSession.StartAction", action);
   if (action == VrStartAction::kHeadsetActivation ||
       action == VrStartAction::kPresentationRequest) {
     page_session_tracker_->ukm_entry()->SetEnteredVROnPageReason(
         static_cast<int>(action));
   }
+}
+
+void SessionMetricsHelper::LogPresentationStartAction(
+    PresentationStartAction action) {
+  DCHECK(presentation_session_tracker_);
+
+  UMA_HISTOGRAM_ENUMERATION("XR.WebXR.PresentationSession", action);
+
+  presentation_session_tracker_->ukm_entry()->SetStartAction(action);
 }
 
 void SessionMetricsHelper::SetWebVREnabled(bool is_webvr_presenting) {
@@ -424,7 +432,7 @@ void SessionMetricsHelper::OnEnterAnyVr() {
           std::make_unique<ukm::builders::XR_PageSession>(
               ukm::GetSourceIdForWebContentsDocument(web_contents())));
   if (pending_page_session_start_action_) {
-    MaybeSetVrStartAction(*pending_page_session_start_action_);
+    LogVrStartAction(*pending_page_session_start_action_);
     pending_page_session_start_action_ = base::nullopt;
   }
 }
@@ -490,8 +498,7 @@ void SessionMetricsHelper::OnEnterPresentation() {
     pending_presentation_start_action_ = PresentationStartAction::kOther;
   }
 
-  presentation_session_tracker_->ukm_entry()->SetStartAction(
-      *pending_presentation_start_action_);
+  LogPresentationStartAction(*pending_presentation_start_action_);
   pending_presentation_start_action_ = base::nullopt;
 }
 
@@ -613,7 +620,7 @@ void SessionMetricsHelper::DidFinishNavigation(
             std::make_unique<ukm::builders::XR_PageSession>(
                 ukm::GetSourceIdForWebContentsDocument(web_contents())));
     if (pending_page_session_start_action_) {
-      MaybeSetVrStartAction(*pending_page_session_start_action_);
+      LogVrStartAction(*pending_page_session_start_action_);
       pending_page_session_start_action_ = base::nullopt;
     }
 
