c77647ed850abbacbe9021730bdc05459b17da2a
Aleksei Loshkarev <lixan@yandex-team.ru>
2018-04-10 13:57:44 +0000

    Eliminates heap use after free for IdentityManager.
    We discover heap use after free in ASAN builds for our signin unit
    tests. It happens because the asynchronous processing in IdentityManager
    does not check own lifetime.
    This cl adds WeakPtr usage for PostTask in IdentityManager.
    
    R=blundell@chromium.org
    
    Change-Id: I85cbdd4d5da97c0c3d3ab92a01642523c6d0a8f0
    Reviewed-on: https://chromium-review.googlesource.com/999607
    Commit-Queue: Alexander Yashkin <a-v-y@yandex-team.ru>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549513}

--- services/identity/public/cpp/identity_manager.h
+++ services/identity/public/cpp/identity_manager.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SERVICES_IDENTITY_PUBLIC_CPP_IDENTITY_MANAGER_H_
 #define SERVICES_IDENTITY_PUBLIC_CPP_IDENTITY_MANAGER_H_
 
+#include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
 #include "components/signin/core/browser/account_info.h"
 #include "components/signin/core/browser/profile_oauth2_token_service.h"
@@ -158,6 +159,12 @@ class IdentityManager : public SigninManagerBase::Observer,
       const std::string& consumer_id,
       const OAuth2TokenService::ScopeSet& scopes) override;
 
+  // Removes synchronously token from token_service
+  void HandleRemoveAccessTokenFromCache(
+      const std::string& account_id,
+      const OAuth2TokenService::ScopeSet& scopes,
+      const std::string& access_token);
+
   // Updates |primary_account_info_| and notifies observers. Invoked
   // asynchronously from GoogleSigninSucceeded() to mimic the effect of
   // receiving this call asynchronously from the Identity Service.
@@ -189,6 +196,8 @@ class IdentityManager : public SigninManagerBase::Observer,
   // Makes sure lists are empty on destruction.
   base::ObserverList<Observer, true> observer_list_;
   base::ObserverList<DiagnosticsObserver, true> diagnostics_observer_list_;
+
+  base::WeakPtrFactory<IdentityManager> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(IdentityManager);
 };
