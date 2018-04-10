8490ea28c7f36531a9268761600817884f7c2c67
Daniel Murphy <dmurph@chromium.org>
2018-04-10 02:53:28 +0000

    [IndexedDB] Schema upgrade fix, fail for corrupt blobs, fix enums.xml
    For the last 4 years we 'saved' the new schema version number, so any v2
    databases are never marked as v3. The 'migration' always deleted all
    blob files. To correctly upgrade to v3, all v2 databases that have blob
    entries are considered corrupt and are wiped.
    
    This also fixes the enums.xml values for IDB setup errors, which have
    been missing 'DeleteIndex' and 'ClearObjectStore' (and subsequently have
    had incorrect reporting on values > 23) since 2014:
    https://crrev.com/b05831ee
    
    Bug: 756447, 829125, 829141
    Change-Id: I1d711b5ed4bfb4e7240da56a2fc78e6ebe561ca1
    Reviewed-on: https://chromium-review.googlesource.com/1000138
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549395}

--- content/browser/indexed_db/indexed_db_leveldb_operations.h
+++ content/browser/indexed_db/indexed_db_leveldb_operations.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string16.h"
 #include "base/strings/string_piece.h"
 #include "base/time/time.h"
+#include "content/common/content_export.h"
 #include "content/common/indexed_db/indexed_db_key_path.h"
 #include "third_party/leveldatabase/src/include/leveldb/status.h"
 
@@ -32,17 +33,17 @@ leveldb::Status InvalidDBKeyStatus();
 leveldb::Status IOErrorStatus();
 
 template <typename DBOrTransaction>
-leveldb::Status GetInt(DBOrTransaction* db,
-                       const base::StringPiece& key,
-                       int64_t* found_int,
-                       bool* found);
+leveldb::Status CONTENT_EXPORT GetInt(DBOrTransaction* db,
+                                      const base::StringPiece& key,
+                                      int64_t* found_int,
+                                      bool* found);
 
 void PutBool(LevelDBTransaction* transaction,
              const base::StringPiece& key,
              bool value);
-void PutInt(LevelDBTransaction* transaction,
-            const base::StringPiece& key,
-            int64_t value);
+void CONTENT_EXPORT PutInt(LevelDBTransaction* transaction,
+                           const base::StringPiece& key,
+                           int64_t value);
 
 template <typename DBOrTransaction>
 WARN_UNUSED_RESULT leveldb::Status GetVarInt(DBOrTransaction* db,
