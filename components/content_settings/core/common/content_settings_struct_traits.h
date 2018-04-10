e16ea7ab2400d8e356eebf47df1cc0ecb688bc91
Oksana Zhuravlova <oksamyt@chromium.org>
2018-04-10 18:15:01 +0000

    Convert content_settings.mojom to use mojo/public/mojom/base/values.mojom
    This change also converts the minimum necessary amount of C++ code to pass
    objects of base::Value by value instead of std::unique_ptr.
    
    TBR=jam@chromium.org
    
    Bug: 646113,799482
    Change-Id: Ib216232ea68c225882890eb74356a8a663d38f13
    Reviewed-on: https://chromium-review.googlesource.com/1000254
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549591}

--- components/content_settings/core/common/content_settings_struct_traits.h
+++ components/content_settings/core/common/content_settings_struct_traits.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "components/content_settings/core/common/content_settings.h"
 #include "components/content_settings/core/common/content_settings.mojom.h"
-#include "mojo/common/values_struct_traits.h"
+#include "mojo/public/cpp/base/values_mojom_traits.h"
 #include "mojo/public/cpp/bindings/enum_traits.h"
 #include "mojo/public/cpp/bindings/struct_traits.h"
 
@@ -100,9 +100,9 @@ struct StructTraits<
     return r.secondary_pattern;
   }
 
-  static const std::unique_ptr<base::Value>& setting_value(
+  static const base::Value& setting_value(
       const ContentSettingPatternSource& r) {
-    return r.setting_value;
+    return *r.setting_value;
   }
 
   static const std::string& source(const ContentSettingPatternSource& r) {
