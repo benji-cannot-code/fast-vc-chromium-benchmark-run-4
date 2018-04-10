e0a986259aee56bc72ecf8ffcc4858d6f87c88c2
Yuwei Huang <yuweih@chromium.org>
2018-04-10 02:05:00 +0000

    [CRD iOS] Set keyboard type to ASCII capable
    Currently our keyboard type is default, which allows complex input
    methods like Chinese IME to be selected, but we haven't implemented
    support for it yet so they will never work. This CL simply limits the
    keyboard type to ASCII capable.
    
    Bug: 830959
    Change-Id: I24b05975d2e242059d4fa94c7e42444ecf7e5bc4
    Reviewed-on: https://chromium-review.googlesource.com/1003774
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549377}

--- remoting/ios/client_keyboard.mm
+++ remoting/ios/client_keyboard.mm
@@ -39,7 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     _autocapitalizationType = UITextAutocapitalizationTypeNone;
     _autocorrectionType = UITextAutocorrectionTypeNo;
     _keyboardAppearance = UIKeyboardAppearanceDefault;
-    _keyboardType = UIKeyboardTypeDefault;
+    _keyboardType = UIKeyboardTypeASCIICapable;
     _spellCheckingType = UITextSpellCheckingTypeNo;
 
     self.showsSoftKeyboard = NO;
