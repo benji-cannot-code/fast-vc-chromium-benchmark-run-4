1eea03514014fcfd427faa17de31300ed75fa82a
Troy Hildebrandt <thildebr@chromium.org>
2018-04-10 13:59:16 +0000

    Copy URL bar text verbatim instead of URL when displaying search terms.
    When cutting/copying text in the URL bar, there's currently an
    assumption that we're copying a URL and it sends some formatted version
    of the URL to the clipboard instead of copying what's in the URL bar
    verbatim. This change allows us to copy the text as-is if we're
    displaying search terms.
    
    Bug: 830050
    Change-Id: If45ecdb68a08f2333274a785c29b2c15ce78d9bb
    Reviewed-on: https://chromium-review.googlesource.com/1003314
    Reviewed-by: Maria Khomenko <mariakhomenko@chromium.org>
    Commit-Queue: Troy Hildebrandt <thildebr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549514}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/CustomTabToolbar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/CustomTabToolbar.java
@@ -286,6 +286,11 @@ public class CustomTabToolbar extends ToolbarLayout implements LocationBar,
     }
 
     @Override
+    public boolean shouldCutCopyVerbatim() {
+        return false;
+    }
+
+    @Override
     public void setShowTitle(boolean showTitle) {
         if (showTitle) {
             mState = STATE_DOMAIN_AND_TITLE;
