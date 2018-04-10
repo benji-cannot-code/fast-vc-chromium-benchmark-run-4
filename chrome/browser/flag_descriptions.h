abcd48ba18024a802f358822473688c3a4a0162e
Xiaoqian Dai <xdai@chromium.org>
2018-04-10 21:01:57 +0000

    Revert "[reland] Cros splitscreen: Disable splitscreen on M66 stable channel."
    The CL has been merged back to M66. So revert this CL on tot.
    This reverts commit cf5e5473412073d5d52476c456eed66386483e58.
    
    TBR=xiyuan@chromium.org
    
    Bug: 827322
    Change-Id: I9d5ca5abf7399318e5bf950c5e25d01bc2ff9093
    Reviewed-on: https://chromium-review.googlesource.com/1005615
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549640}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1630,6 +1630,9 @@ extern const char kDisableLockScreenAppsDescription[];
 extern const char kDisableTabletAutohideTitlebarsName[];
 extern const char kDisableTabletAutohideTitlebarsDescription[];
 
+extern const char kDisableTabletSplitViewName[];
+extern const char kDisableTabletSplitViewDescription[];
+
 extern const char kEnablePerUserTimezoneName[];
 extern const char kEnablePerUserTimezoneDescription[];
 
@@ -1662,9 +1665,6 @@ extern const char kEnableStylusVirtualKeyboardDescription[];
 
 extern const char kEnableFullscreenHandwritingVirtualKeyboardName[];
 extern const char kEnableFullscreenHandwritingVirtualKeyboardDescription[];
-
-extern const char kEnableTabletSplitViewName[];
-extern const char kEnableTabletSplitViewDescription[];
 
 extern const char kEnableUnifiedMultiDeviceSettingsName[];
 extern const char kEnableUnifiedMultiDeviceSettingsDescription[];
