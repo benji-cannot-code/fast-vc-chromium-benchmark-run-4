3c2afc7e8d955a8469c72f0f85fbafa6ffa20469
Mounir Lamouri <mlamouri@chromium.org>
2018-04-10 10:50:27 +0000

    Media Controls: expose current time in timeline accessibility value description.
    Instead of the proper value that may have many digits and make no sense
    to a human.
    
    This is a reland of https://chromium-review.googlesource.com/c/chromium/src/+/998915
    It is fixing the test to make it more reliable and rebased the CL.
    
    Bug: 829627
    Change-Id: Ic550051859367dcd92237d527285f00210098142
    Reviewed-on: https://chromium-review.googlesource.com/1000864
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549480}

--- third_party/blink/renderer/modules/media_controls/elements/media_control_timeline_element.cc
+++ third_party/blink/renderer/modules/media_controls/elements/media_control_timeline_element.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/layout_box.h"
 #include "third_party/blink/renderer/core/page/chrome_client.h"
 #include "third_party/blink/renderer/core/style/computed_style.h"
+#include "third_party/blink/renderer/modules/media_controls/elements/media_control_current_time_display_element.h"
 #include "third_party/blink/renderer/modules/media_controls/elements/media_control_elements_helper.h"
 #include "third_party/blink/renderer/modules/media_controls/media_controls_impl.h"
 #include "third_party/blink/renderer/modules/media_controls/media_controls_resource_loader.h"
@@ -88,6 +89,9 @@ bool MediaControlTimelineElement::WillRespondToMouseClickEvents() {
 
 void MediaControlTimelineElement::SetPosition(double current_time) {
   setValue(String::Number(current_time));
+  setAttribute(
+      HTMLNames::aria_valuetextAttr,
+      AtomicString(GetMediaControls().CurrentTimeDisplay().textContent(true)));
   RenderBarSegments();
 }
 
