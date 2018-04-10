3bed9379ef8ec0584d1ae730e8b9cad3f1859579
Carlos Knippschild <carlosk@chromium.org>
2018-04-10 00:15:56 +0000

    Fixed offline page opening in CCT when not from DH.
    With the open-in-CCT flag enabled, offline pages should only open in CCT
    if they are being launched from the downlaods home. Alas they were being
    opened that way from the download finished snackbar as well.
    
    This CL fixes that by checking that the current activity is indeed the
    DH.
    
    Bug: 824807
    Change-Id: Ia5f895c7fd7bbb277f8b614c1db44ec19c2da85a
    Reviewed-on: https://chromium-review.googlesource.com/998832
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Carlos Knippschild <carlosk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549337}

--- chrome/android/java/src/org/chromium/chrome/browser/offlinepages/downloads/OfflinePageDownloadBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/offlinepages/downloads/OfflinePageDownloadBridge.java
@@ -22,6 +22,7 @@ import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.chrome.browser.IntentHandler;
 import org.chromium.chrome.browser.LaunchIntentDispatcher;
 import org.chromium.chrome.browser.customtabs.CustomTabIntentDataProvider;
+import org.chromium.chrome.browser.download.DownloadActivity;
 import org.chromium.chrome.browser.offlinepages.OfflinePageOrigin;
 import org.chromium.chrome.browser.offlinepages.OfflinePageUtils;
 import org.chromium.chrome.browser.profiles.Profile;
@@ -79,7 +80,9 @@ public class OfflinePageDownloadBridge {
     private static void openItem(final String url, final long offlineId, final boolean openInCct) {
         OfflinePageUtils.getLoadUrlParamsForOpeningOfflineVersion(url, offlineId, (params) -> {
             if (params == null) return;
-            if (openInCct) {
+            boolean openingFromDownloadsHome =
+                    ApplicationStatus.getLastTrackedFocusedActivity() instanceof DownloadActivity;
+            if (openInCct && openingFromDownloadsHome) {
                 openItemInCct(offlineId, params);
             } else {
                 openItemInNewTab(offlineId, params);
