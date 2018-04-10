313c6affc269cd65a9b96c7e519cbfce6bcb23ff
Theresa <twellington@chromium.org>
2018-04-10 21:09:52 +0000

    [EoC] Remove toasts and Java-layer clustering
    Remove the toasts and Java-layer clustering used during prototyping.
    
    BUG=822945
    
    Change-Id: I391c2eb051c448cf1c9df88a9039b25db04fffeb
    Reviewed-on: https://chromium-review.googlesource.com/1005502
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Commit-Queue: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549643}

--- chrome/android/java/src/org/chromium/chrome/browser/contextual_suggestions/FetchHelper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextual_suggestions/FetchHelper.java
@@ -8,7 +8,6 @@ package org.chromium.chrome.browser.contextual_suggestions;
 import android.support.annotation.Nullable;
 import android.webkit.URLUtil;
 
-import org.chromium.base.ContextUtils;
 import org.chromium.chrome.browser.tab.EmptyTabObserver;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabObserver;
@@ -16,7 +15,6 @@ import org.chromium.chrome.browser.tabmodel.TabModel.TabSelectionType;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 import org.chromium.chrome.browser.tabmodel.TabModelSelectorTabModelObserver;
 import org.chromium.chrome.browser.util.UrlUtilities;
-import org.chromium.ui.widget.Toast;
 
 /**
  * A helper class responsible for determining when to trigger requests for suggestions and when to
@@ -92,10 +90,6 @@ class FetchHelper {
         // Context has changed, so we want to remove any old suggestions.
         mDelegate.clearState();
         mCurrentContextUrl = newUrl;
-
-        Toast.makeText(ContextUtils.getApplicationContext(), "Fetching suggestions...",
-                     Toast.LENGTH_SHORT)
-                .show();
 
         mDelegate.requestSuggestions(newUrl);
     }
