7128eb78ceb18e351789da7ee4563522a0a0701f
Justin Cohen <justincohen@google.com>
2018-04-10 16:21:46 +0000

    Use correct toolbar button to enter tab grid for ui refresh.
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I50aab0be5fae9566c4c1c3f6148407dc03063195
    Reviewed-on: https://chromium-review.googlesource.com/1003034
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549553}

--- ios/chrome/browser/ui/tab_grid/tab_grid_egtest_util.mm
+++ ios/chrome/browser/ui/tab_grid/tab_grid_egtest_util.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/tab_grid/grid/grid_constants.h"
 #import "ios/chrome/browser/ui/tab_grid/tab_grid_constants.h"
 #import "ios/chrome/browser/ui/tools_menu/public/tools_menu_constants.h"
+#import "ios/chrome/browser/ui/uikit_ui_util.h"
 #include "ios/chrome/grit/ios_strings.h"
 #import "ios/chrome/test/app/chrome_test_util.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
@@ -29,7 +30,11 @@ NSString* IdentifierForCellAtIndex(unsigned int index) {
 namespace chrome_test_util {
 
 id<GREYMatcher> TabGridOpenButton() {
-  return ButtonWithAccessibilityLabelId(IDS_IOS_TAB_STRIP_ENTER_TAB_SWITCHER);
+  if (IsRegularXRegularSizeClass()) {
+    return ButtonWithAccessibilityLabelId(IDS_IOS_TAB_STRIP_ENTER_TAB_SWITCHER);
+  } else {
+    return ButtonWithAccessibilityLabelId(IDS_IOS_TOOLBAR_SHOW_TABS);
+  }
 }
 
 id<GREYMatcher> TabGridDoneButton() {
