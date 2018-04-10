5cc7b0d3221a9e4ed917e27f2002bf65f1937afa
Rayan Kanso <rayankans@chromium.org>
2018-04-10 21:16:53 +0000

    [Background Fetch] Update persistent storage tasks w/metadata.
    Update the database manager task wrappers to use the metadata proto directly.
    
    The following files were moved and slightly updated to make naming consistent throughout:
      * create_registration_task.(cc|h) -> create_metadata_task.(cc|h)
      * get_registration_task.(cc|h) -> get_metadata_task.(cc|h)
    The metadata to registration conversion logic was removed from get_registration_task.cc.
    
    Also added tests.
    
    Bug: 826257
    Change-Id: I7a8cedf76df3b935ebeddf6536ba5cda639420db
    Reviewed-on: https://chromium-review.googlesource.com/993054
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Reviewed-by: Dan Elphick <delphick@chromium.org>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549645}

--- content/browser/background_fetch/background_fetch_data_manager.h
+++ content/browser/background_fetch/background_fetch_data_manager.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/queue.h"
 #include "base/gtest_prod_util.h"
 #include "base/macros.h"
+#include "content/browser/background_fetch/background_fetch.pb.h"
 #include "content/browser/background_fetch/background_fetch_registration_id.h"
 #include "content/browser/background_fetch/background_fetch_scheduler.h"
 #include "content/browser/background_fetch/storage/database_task.h"
@@ -55,6 +56,9 @@ class CONTENT_EXPORT BackgroundFetchDataManager
       bool /* background_fetch_succeeded */,
       std::vector<BackgroundFetchSettledFetch>,
       std::vector<std::unique_ptr<storage::BlobDataHandle>>)>;
+  using GetMetadataCallback =
+      base::OnceCallback<void(blink::mojom::BackgroundFetchError,
+                              std::unique_ptr<proto::BackgroundFetchMetadata>)>;
   using GetRegistrationCallback =
       base::OnceCallback<void(blink::mojom::BackgroundFetchError,
                               std::unique_ptr<BackgroundFetchRegistration>)>;
@@ -77,7 +81,13 @@ class CONTENT_EXPORT BackgroundFetchDataManager
       const SkBitmap& icon,
       GetRegistrationCallback callback);
 
-  // Get the BackgroundFetchOptions for a registration.
+  // Get the BackgroundFetchMetadata.
+  void GetMetadata(int64_t service_worker_registration_id,
+                   const url::Origin& origin,
+                   const std::string& developer_id,
+                   GetMetadataCallback callback);
+
+  // Get the BackgroundFetchRegistration.
   void GetRegistration(int64_t service_worker_registration_id,
                        const url::Origin& origin,
                        const std::string& developer_id,
