7118f3e61c9dc43c94ab12d17a11aac98183fd51
tanvir.rizvi <tanvir.rizvi@samsung.com>
2018-04-10 09:16:59 +0000

    Use EphemeralRange in ApplyStyleCommand::RemoveInlineStyle
    For code health we use Ephemeral range instead of
    passsing position in RemoveInlineStyle.
    With this we catch a bug at call sites,
    as EphemeralRange construction takes care of the
    current DCHECKS in RemoveInlineStyle
    
    Bug: 826106
    Change-Id: Ibaa37789f7671aa464b4c87d30f12bd023a67a3d
    Reviewed-on: https://chromium-review.googlesource.com/1001437
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Tanvir Rizvi <tanvir.rizvi@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#549466}

--- third_party/blink/renderer/core/editing/commands/apply_style_command.cc
+++ third_party/blink/renderer/core/editing/commands/apply_style_command.cc
@@ -779,8 +779,10 @@ void ApplyStyleCommand::ApplyInlineStyle(EditingStyle* style,
           GetDocument().GetSecureContextMode());
 
       if (ComparePositions(embedding_remove_start, embedding_remove_end) <= 0) {
-        RemoveInlineStyle(embedding_style, embedding_remove_start,
-                          embedding_remove_end, editing_state);
+        RemoveInlineStyle(
+            embedding_style,
+            EphemeralRange(embedding_remove_start, embedding_remove_end),
+            editing_state);
         if (editing_state->IsAborted())
           return;
       }
@@ -788,7 +790,7 @@ void ApplyStyleCommand::ApplyInlineStyle(EditingStyle* style,
   }
 
   RemoveInlineStyle(style_without_embedding ? style_without_embedding : style,
-                    remove_start, end, editing_state);
+                    EphemeralRange(remove_start, end), editing_state);
   if (editing_state->IsAborted())
     return;
   start = StartPosition();
@@ -1419,15 +1421,11 @@ void ApplyStyleCommand::PushDownInlineStyleAroundNode(
 }
 
 void ApplyStyleCommand::RemoveInlineStyle(EditingStyle* style,
-                                          const Position& start,
-                                          const Position& end,
+                                          const EphemeralRange& range,
                                           EditingState* editing_state) {
-  DCHECK(start.IsNotNull());
-  DCHECK(end.IsNotNull());
-  DCHECK(start.IsConnected()) << start;
-  DCHECK(end.IsConnected()) << end;
+  const Position& start = range.StartPosition();
+  const Position& end = range.EndPosition();
   DCHECK(Position::CommonAncestorTreeScope(start, end)) << start << " " << end;
-  DCHECK_LE(start, end);
   // FIXME: We should assert that start/end are not in the middle of a text
   // node.
 
