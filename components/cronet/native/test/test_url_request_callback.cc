73c8f7a2e09122b9810496c98315969b5c04506b
Misha Efimov <mef@chromium.org>
2018-04-10 15:46:45 +0000

    [Cronet] Native API should properly handle request cancellation.
    Bug: 812334
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I99f120d624916af57ccbc068e5162b920299fc89
    Reviewed-on: https://chromium-review.googlesource.com/984114
    Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    Commit-Queue: Misha Efimov <mef@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549543}

--- components/cronet/native/test/test_url_request_callback.cc
+++ components/cronet/native/test/test_url_request_callback.cc
@@ -145,6 +145,7 @@ void TestUrlRequestCallback::OnReadCompleted(Cronet_UrlRequestPtr request,
   }
 
   if (MaybeCancelOrPause(request)) {
+    Cronet_Buffer_Destroy(buffer);
     return;
   }
   StartNextRead(request, buffer);
@@ -198,7 +199,6 @@ void TestUrlRequestCallback::OnCanceled(Cronet_UrlRequestPtr request,
   CHECK(!last_error_);
 
   response_step_ = ON_CANCELED;
-  response_info_ = std::make_unique<UrlResponseInfo>(info);
   on_canceled_called_ = true;
   SignalDone();
   MaybeCancelOrPause(request);
