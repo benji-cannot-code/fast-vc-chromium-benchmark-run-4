ea390197a26db2e5b3f4f19012eeaa7ee46be6f6
Yuwei Huang <yuweih@chromium.org>
2018-04-10 02:29:29 +0000

    [CRD iOS] Translate "\n" into an enter keypress
    iOS is sending a "\n" unicode text event to the host when the user taps
    the `return` key. On Windows this doesn't always generate a newline.
    
    This CL fixes this issue by translating the "\n" text event into a
    ENTER keypress before sending it to the host.
    
    Bug: 830959
    Change-Id: Ib94caf6ce2036cc5654f1b59f1e0136c81c9eec1
    Reviewed-on: https://chromium-review.googlesource.com/1003598
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549385}

--- remoting/ios/client_keyboard.mm
+++ remoting/ios/client_keyboard.mm
@@ -50,7 +50,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma mark - UIKeyInput
 
 - (void)insertText:(NSString*)text {
-  [_delegate clientKeyboardShouldSend:text];
+  if ([text isEqualToString:@"\n"]) {
+    [_delegate clientKeyboardShouldSendEnter];
+  } else {
+    [_delegate clientKeyboardShouldSend:text];
+  }
 }
 
 - (void)deleteBackward {
