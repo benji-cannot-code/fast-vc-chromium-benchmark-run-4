fe9c2870ca8326edb14baea0a62688e0dca88d0b
Blake O'Hare <blakeo@chromium.org>
2018-04-10 04:44:08 +0000

    Fix bug where floating keyboard jitters when dragging with multi-touch
    When you use multiple fingers to drag the keyboard around, the events
    for these get sent to the event filter sequentially. Check for a
    pointer ID (which is unique per touch point) and include it in the
    active drag descriptor. Ignore non-matching pointer events.
    
    Bug: 802992
    Change-Id: I5a3948719e5c103ee1aa618afc3fe36939567326
    Reviewed-on: https://chromium-review.googlesource.com/979739
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: Blake O'Hare <blakeo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549424}

--- ui/keyboard/container_floating_behavior.cc
+++ ui/keyboard/container_floating_behavior.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/keyboard/container_floating_behavior.h"
 
+#include "ui/events/event.h"
 #include "ui/gfx/geometry/point.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/size.h"
@@ -195,6 +196,12 @@ bool ContainerFloatingBehavior::HandlePointerEvent(
   if (keyboard_bounds.height() <= 0)
     return false;
 
+  ui::PointerId pointer_id = -1;
+  if (event.IsTouchEvent()) {
+    const ui::TouchEvent* te = event.AsTouchEvent();
+    pointer_id = te->pointer_details().id;
+  }
+
   bool handled = false;
 
   const ui::EventType type = event.type();
@@ -211,8 +218,9 @@ bool ContainerFloatingBehavior::HandlePointerEvent(
       } else if (!drag_descriptor_) {
         // If there is no active drag descriptor, start a new one.
         bool drag_started_by_touch = (type == ui::ET_TOUCH_PRESSED);
-        drag_descriptor_.reset(new DragDescriptor(
-            keyboard_bounds.origin(), kb_offset, drag_started_by_touch));
+        drag_descriptor_.reset(
+            new DragDescriptor(keyboard_bounds.origin(), kb_offset,
+                               drag_started_by_touch, pointer_id));
         handled = true;
       }
       break;
@@ -226,6 +234,8 @@ bool ContainerFloatingBehavior::HandlePointerEvent(
         // If the event isn't of the same type that started the drag, end the
         // drag to prevent confusion.
         drag_descriptor_ = nullptr;
+      } else if (drag_descriptor_->pointer_id() != pointer_id) {
+        // do nothing.
       } else {
         // Drag continues.
         // If there is an active drag, use it to determine the new location
--- ui/keyboard/drag_descriptor.cc
+++ ui/keyboard/drag_descriptor.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/keyboard/drag_descriptor.h"
 
+#include "ui/events/event.h"
 #include "ui/gfx/geometry/point.h"
 #include "ui/gfx/geometry/vector2d.h"
 
@@ -12,9 +13,11 @@ namespace keyboard {
 
 DragDescriptor::DragDescriptor(const gfx::Point& keyboard_location,
                                const gfx::Vector2d& click_offset,
-                               bool is_touch_drag)
+                               bool is_touch_drag,
+                               ui::PointerId pointer_id)
     : original_keyboard_location_(keyboard_location),
       original_click_offset_(click_offset),
-      is_touch_drag_(is_touch_drag) {}
+      is_touch_drag_(is_touch_drag),
+      pointer_id_(pointer_id) {}
 
 }  // namespace keyboard
