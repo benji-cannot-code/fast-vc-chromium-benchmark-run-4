297c2c88c5d15fd7f4813095c56e0f7700eba7b0
Max Morin <maxmorin@chromium.org>
2018-04-10 13:31:40 +0000

    Reland "Bind StreamFactory in audio service."
    This is a reland of 21f2ef4cb6ad89be9ca765afe7a3f1d077d726f2
    
    Per offline discussion, this code is fine.
    
    Original change's description:
    > Bind StreamFactory in audio service.
    >
    > Bug: 803102
    > Change-Id: I4d8cb112445095045c492488f7c3b8d9853b6c23
    > Reviewed-on: https://chromium-review.googlesource.com/1004634
    > Commit-Queue: Max Morin <maxmorin@chromium.org>
    > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > Reviewed-by: Olga Sharonova <olka@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#549481}
    
    Tbr: 
    Bug: 803102
    Change-Id: I3b14b633dd1419eae570d2b9ab411fc8732fad4c
    Reviewed-on: https://chromium-review.googlesource.com/1005080
    Reviewed-by: Max Morin <maxmorin@chromium.org>
    Commit-Queue: Max Morin <maxmorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549504}

--- services/audio/stream_factory.h
+++ services/audio/stream_factory.h
@@ -28,6 +28,10 @@ class AudioManager;
 class AudioParameters;
 }  // namespace media
 
+namespace service_manager {
+class ServiceContextRef;
+}
+
 namespace audio {
 
 class OutputStream;
@@ -41,7 +45,8 @@ class StreamFactory final : public mojom::StreamFactory {
   explicit StreamFactory(media::AudioManager* audio_manager);
   ~StreamFactory() final;
 
-  void BindRequest(mojom::StreamFactoryRequest request);
+  void Bind(mojom::StreamFactoryRequest request,
+            std::unique_ptr<service_manager::ServiceContextRef> context_ref);
 
   // StreamFactory implementation.
   void CreateOutputStream(
@@ -64,7 +69,10 @@ class StreamFactory final : public mojom::StreamFactory {
 
   media::AudioManager* const audio_manager_;
 
-  mojo::BindingSet<mojom::StreamFactory> bindings_;
+  mojo::BindingSet<mojom::StreamFactory,
+                   std::unique_ptr<service_manager::ServiceContextRef>>
+      bindings_;
+
   OutputStreamSet output_streams_;
 
   DISALLOW_COPY_AND_ASSIGN(StreamFactory);
