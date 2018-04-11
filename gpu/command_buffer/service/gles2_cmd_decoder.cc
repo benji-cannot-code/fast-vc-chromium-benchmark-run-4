f6a3e12b218dff253aa4e439406fa4678390464f
James Darpinian <jdarpinian@chromium.org>
2018-04-11 08:32:19 +0000

    Fix UnmapBufferHelper to use the correct binding
    UnmapBufferHelper assumed that the mapped buffer was still bound to the same
    binding point that it was first bound to, which isn't guaranteed. This
    fixes the WebGL conformance test transform_feedback/switching-objects.html
    
    Bug: 696345
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ia06b8e3b2b4a7a7f1a38c4bcdb241a039dc36a80
    Reviewed-on: https://chromium-review.googlesource.com/998845
    Commit-Queue: James Darpinian <jdarpinian@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549825}

--- gpu/command_buffer/service/gles2_cmd_decoder.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder.cc
@@ -796,7 +796,7 @@ class GLES2DecoderImpl : public GLES2Decoder, public ErrorStateClient {
                                       const volatile GLuint* client_ids);
   void DeleteSyncHelper(GLuint sync);
 
-  bool UnmapBufferHelper(Buffer* buffer);
+  bool UnmapBufferHelper(Buffer* buffer, GLenum target);
 
   // Workarounds
   void OnFboChanged() const;
@@ -4416,7 +4416,22 @@ void GLES2DecoderImpl::DeleteBuffersHelper(GLsizei n,
     GLuint client_id = client_ids[ii];
     Buffer* buffer = GetBuffer(client_id);
     if (buffer && !buffer->IsDeleted()) {
-      UnmapBufferHelper(buffer);
+      if (buffer->GetMappedRange()) {
+        // The buffer is not guaranteed to still be bound to any binding point,
+        // even though it is mapped. If it is not bound, we will need to bind
+        // it temporarily in order to unmap it.
+        GLenum target = buffer->initial_target();
+        Buffer* currently_bound =
+            buffer_manager()->GetBufferInfoForTarget(&state_, target);
+        if (currently_bound != buffer) {
+          api()->glBindBufferFn(target, buffer->service_id());
+        }
+        UnmapBufferHelper(buffer, target);
+        if (currently_bound != buffer) {
+          api()->glBindBufferFn(
+              target, currently_bound ? currently_bound->service_id() : 0);
+        }
+      }
       state_.RemoveBoundBuffer(buffer);
       buffer_manager()->RemoveBuffer(client_id);
     }
@@ -18709,7 +18724,7 @@ error::Error GLES2DecoderImpl::HandleMapBufferRange(
   return error::kNoError;
 }
 
-bool GLES2DecoderImpl::UnmapBufferHelper(Buffer* buffer) {
+bool GLES2DecoderImpl::UnmapBufferHelper(Buffer* buffer, GLenum target) {
   DCHECK(buffer);
   const Buffer::MappedRange* mapped_range = buffer->GetMappedRange();
   if (!mapped_range)
@@ -18730,7 +18745,7 @@ bool GLES2DecoderImpl::UnmapBufferHelper(Buffer* buffer) {
   buffer->RemoveMappedRange();
   if (WasContextLost())
     return true;
-  GLboolean rt = api()->glUnmapBufferFn(buffer->initial_target());
+  GLboolean rt = api()->glUnmapBufferFn(target);
   if (rt == GL_FALSE) {
     // At this point, we have already done the necessary validation, so
     // GL_FALSE indicates data corruption.
@@ -18772,7 +18787,7 @@ error::Error GLES2DecoderImpl::HandleUnmapBuffer(
     LOCAL_SET_GL_ERROR(GL_INVALID_OPERATION, func_name, "buffer is unmapped");
     return error::kNoError;
   }
-  if (!UnmapBufferHelper(buffer))
+  if (!UnmapBufferHelper(buffer, target))
     return error::kLostContext;
   return error::kNoError;
 }
