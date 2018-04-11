dee40a2aeefc0def469e6102b3844d1889194755
Donna Wu <donna.wu@intel.com>
2018-04-11 07:55:15 +0000

    Add MTP Mojo interfaces for talking to MediaTransferProtocolManager.
    This CL:
    1) Introduces 2 Mojo interfaces: device::mojom::MtpManager and
       device::mojom::MtpManagerClient. The current implementation just
       forwards MTP device access requests to the existing
       MediaTransferProtocolManager..
    2) Makes Device Service expose the above 2 Mojo interfaces.
    3) Create stubs to connect these 2 interfaces with the main user,
       storage_monitor::StorageMonitor.
    
    More Mojo methods will be added to MtpManager and all users will be
    converted to use Mojo interfaces in subsequent CLs.
    
    BUG=769630
    
    Change-Id: Ibfffb2a45d13014ec3984ae9e242f49492e8b16a
    Reviewed-on: https://chromium-review.googlesource.com/972668
    Commit-Queue: Donna Wu <donna.wu@intel.com>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549814}

--- chrome/browser/browser_process_impl.cc
+++ chrome/browser/browser_process_impl.cc
@@ -126,6 +126,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_features.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/common/network_connection_tracker.h"
+#include "content/public/common/service_manager_connection.h"
 #include "extensions/buildflags/buildflags.h"
 #include "extensions/common/constants.h"
 #include "media/media_buildflags.h"
@@ -1161,7 +1162,10 @@ void BrowserProcessImpl::PreMainMessageLoopRun() {
 #endif  // BUILDFLAG(ENABLE_PLUGINS)
 
 #if !defined(OS_ANDROID)
-  storage_monitor::StorageMonitor::Create();
+  storage_monitor::StorageMonitor::Create(
+      content::ServiceManagerConnection::GetForProcess()
+          ->GetConnector()
+          ->Clone());
 #endif
 
   child_process_watcher_ = std::make_unique<ChromeChildProcessWatcher>();
--- components/storage_monitor/storage_monitor.h
+++ components/storage_monitor/storage_monitor.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/lock.h"
 #include "build/build_config.h"
 #include "components/storage_monitor/storage_info.h"
+#include "services/service_manager/public/cpp/connector.h"
 
 class MediaFileSystemRegistryTest;
 class MediaGalleriesPlatformAppBrowserTest;
@@ -72,7 +73,7 @@ class StorageMonitor {
   // Instantiates the StorageMonitor singleton. This function does not
   // guarantee the complete initialization of the object. For that, see
   // |EnsureInitialized|.
-  static void Create();
+  static void Create(std::unique_ptr<service_manager::Connector> connector);
 
   // Destroys the StorageMonitor singleton.
   static void Destroy();
@@ -148,6 +149,9 @@ class StorageMonitor {
 
   StorageMonitor();
 
+  // Provides the connector for service access.
+  service_manager::Connector* GetConnector();
+
   virtual Receiver* receiver() const;
 
   // Called to initialize the storage monitor.
@@ -186,6 +190,8 @@ class StorageMonitor {
 
   // Map of all known storage devices,including fixed and removable storages.
   StorageMap storage_map_;
+
+  std::unique_ptr<service_manager::Connector> connector_;
 
   std::unique_ptr<TransientDeviceIds> transient_device_ids_;
 };
