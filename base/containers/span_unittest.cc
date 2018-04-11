21a0ac7bc5807c9f24be68f761447ba587634005
jdoerrie <jdoerrie@chromium.org>
2018-04-11 07:22:40 +0000

    [base] Introduce byte methods on base::span
    This change implements base::as_bytes(base::span),
    base::as_writable_bytes(base::span) and base::span::size_bytes() as
    specified in the C++20 draft [1, 2]. As opposed to the standard
    as_bytes() and as_writable_bytes() return spans of uint8_t due to the
    lack of std::byte in C++14.
    
    [1] http://eel.is/c++draft/views.span#span.obs-itemdecl:2
    [2] http://eel.is/c++draft/views.span#span.objectrep
    
    Bug: 828324
    Change-Id: Ie7c4e759e81caa6dfb2b3583b36dc8923a430b55
    Reviewed-on: https://chromium-review.googlesource.com/1004956
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549808}

--- base/containers/span_unittest.cc
+++ base/containers/span_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
+#include <algorithm>
 #include <memory>
 #include <vector>
 
@@ -309,6 +310,19 @@ TEST(SpanTest, Size) {
   }
 }
 
+TEST(SpanTest, SizeBytes) {
+  {
+    span<int> span;
+    EXPECT_EQ(0u, span.size_bytes());
+  }
+
+  {
+    int array[] = {1, 2, 3};
+    span<int> span(array);
+    EXPECT_EQ(3u * sizeof(int), span.size_bytes());
+  }
+}
+
 TEST(SpanTest, Empty) {
   {
     span<int> span;
@@ -426,6 +440,39 @@ TEST(SpanTest, GreaterEqual) {
   constexpr span<const int> span3(kArray3);
 
   EXPECT_FALSE(span1 >= span3);
+}
+
+TEST(SpanTest, AsBytes) {
+  {
+    constexpr int kArray[] = {2, 3, 5, 7, 11, 13};
+    span<const uint8_t> bytes_span = as_bytes(make_span(kArray));
+    EXPECT_EQ(reinterpret_cast<const uint8_t*>(kArray), bytes_span.data());
+    EXPECT_EQ(sizeof(kArray), bytes_span.size());
+    EXPECT_EQ(bytes_span.size(), bytes_span.size_bytes());
+  }
+
+  {
+    std::vector<int> vec = {1, 1, 2, 3, 5, 8};
+    span<int> mutable_span(vec);
+    span<const uint8_t> bytes_span = as_bytes(mutable_span);
+    EXPECT_EQ(reinterpret_cast<const uint8_t*>(vec.data()), bytes_span.data());
+    EXPECT_EQ(sizeof(int) * vec.size(), bytes_span.size());
+    EXPECT_EQ(bytes_span.size(), bytes_span.size_bytes());
+  }
+}
+
+TEST(SpanTest, AsWritableBytes) {
+  std::vector<int> vec = {1, 1, 2, 3, 5, 8};
+  span<int> mutable_span(vec);
+  span<uint8_t> writable_bytes_span = as_writable_bytes(mutable_span);
+  EXPECT_EQ(reinterpret_cast<uint8_t*>(vec.data()), writable_bytes_span.data());
+  EXPECT_EQ(sizeof(int) * vec.size(), writable_bytes_span.size());
+  EXPECT_EQ(writable_bytes_span.size(), writable_bytes_span.size_bytes());
+
+  // Set the first entry of vec to zero while writing through the span.
+  std::fill(writable_bytes_span.data(),
+            writable_bytes_span.data() + sizeof(int), 0);
+  EXPECT_EQ(0, vec[0]);
 }
 
 TEST(SpanTest, MakeSpanFromDataAndSize) {
