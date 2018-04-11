0fbe877ff84d6faf772d10fa4fdff70207262f08
Jonathan Backer <backer@chromium.org>
2018-04-11 17:07:32 +0000

    Fix virtual contexts with RasterDecoder
    Copy a few more lines from GLES2DecoderImpl. Tested by hand on Linux
    desktop build with video, webgl, and canvas 2d.
    
    Bug: 829438
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I1d1edab35a7f418a2392a3dc1fa42aef4739d1c1
    Reviewed-on: https://chromium-review.googlesource.com/1005786
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549914}

--- gpu/command_buffer/service/raster_decoder.cc
+++ gpu/command_buffer/service/raster_decoder.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/service/gles2_cmd_copy_tex_image.h"
 #include "gpu/command_buffer/service/gles2_cmd_copy_texture_chromium.h"
 #include "gpu/command_buffer/service/image_manager.h"
+#include "gpu/command_buffer/service/indexed_buffer_binding_host.h"
 #include "gpu/command_buffer/service/logger.h"
 #include "gpu/command_buffer/service/mailbox_manager.h"
 #include "gpu/command_buffer/service/query_manager.h"
@@ -237,7 +238,7 @@ class RasterDecoderImpl : public RasterDecoder, public gles2::ErrorStateClient {
       bool offscreen,
       const gles2::DisallowedFeatures& disallowed_features,
       const ContextCreationAttribs& attrib_helper) override;
-  const gles2::ContextState* GetContextState() override;
+  const ContextState* GetContextState() override { return &state_; }
   void Destroy(bool have_context) override;
   bool MakeCurrent() override;
   gl::GLContext* GetGLContext() override;
@@ -768,6 +769,15 @@ gpu::ContextResult RasterDecoderImpl::Initialize(
   }
   CHECK_GL_ERROR();
 
+  // In theory |needs_emulation| needs to be true on Desktop GL 4.1 or lower.
+  // However, we set it to true everywhere, not to trust drivers to handle
+  // out-of-bounds buffer accesses.
+  bool needs_emulation = true;
+  state_.indexed_uniform_buffer_bindings =
+      new gles2::IndexedBufferBindingHost(group_->max_uniform_buffer_bindings(),
+                                          GL_UNIFORM_BUFFER, needs_emulation);
+  state_.indexed_uniform_buffer_bindings->SetIsBound(true);
+
   state_.InitGenericAttribs(group_->max_vertex_attribs());
   vertex_array_manager_.reset(new VertexArrayManager());
 
@@ -803,11 +813,6 @@ gpu::ContextResult RasterDecoderImpl::Initialize(
   return gpu::ContextResult::kSuccess;
 }
 
-const gles2::ContextState* RasterDecoderImpl::GetContextState() {
-  NOTIMPLEMENTED();
-  return nullptr;
-}
-
 void RasterDecoderImpl::Destroy(bool have_context) {
   if (!initialized())
     return;
@@ -836,6 +841,7 @@ void RasterDecoderImpl::Destroy(bool have_context) {
   state_.sampler_units.clear();
   state_.bound_pixel_pack_buffer = nullptr;
   state_.bound_pixel_unpack_buffer = nullptr;
+  state_.indexed_uniform_buffer_bindings = nullptr;
 
   copy_tex_image_blit_.reset();
   copy_texture_chromium_.reset();
