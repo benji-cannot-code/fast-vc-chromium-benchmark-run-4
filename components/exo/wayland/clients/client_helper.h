46c2e67960175d51ef889825e3e548c09d3f6b34
Scott Violet <sky@chromium.org>
2018-04-11 05:13:56 +0000

    exo: moves aura-shell into public directory
    This way other parts of the code can have a dep on it in linux
    builds. Without this when other parts of the code (such as ozone)
    have a dep on components/exo/wayland:* gn ends up parsing the whole
    file, which tries to includes things from ash, which triggers asserts
    in the ash BUILD.gn files that they are only used on ash.
    
    BUG=none
    TEST=none
    
    Change-Id: I0062da5a35c2559fe582dce8e6ed0f910d15b404
    Reviewed-on: https://chromium-review.googlesource.com/1005976
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: David Reveman <reveman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549771}

--- components/exo/wayland/clients/client_helper.h
+++ components/exo/wayland/clients/client_helper.h
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/scoped_generic.h"
-#include "components/exo/wayland/aura-shell-client-protocol.h"
+#include "components/exo/wayland/public/aura-shell-client-protocol.h"
 
 #if defined(USE_GBM)
 #include <gbm.h>
