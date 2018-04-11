6504462d3ef8326207a06906f8bd6728a356019e
Rohit Rao <rohitrao@chromium.org>
2018-04-11 17:06:11 +0000

    [ios] Fix the GetActiveViewController() EG util.
    When the UIRefresh TabGrid is enabled, then this utility method takes a separate
    codepath that wasn't accounting for the existence of the
    BVCContainerViewController. This CL fixes that so that the BVC is properly
    returned instead of its container.
    
    BUG=None
    TEST=None
    
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I969ffc62138a189a395d46279dba722670c7ce64
    Reviewed-on: https://chromium-review.googlesource.com/1007159
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549912}

--- ios/chrome/test/app/chrome_test_util.mm
+++ ios/chrome/test/app/chrome_test_util.mm
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/metrics/previous_session_info_private.h"
 #import "ios/chrome/browser/tabs/tab.h"
 #import "ios/chrome/browser/ui/browser_view_controller.h"
+#import "ios/chrome/browser/ui/main/bvc_container_view_controller.h"
 #import "ios/chrome/browser/ui/main/view_controller_swapping.h"
 #import "ios/chrome/browser/ui/ntp/new_tab_page_controller.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_switcher.h"
@@ -125,11 +126,22 @@ UIViewController* GetActiveViewController() {
     return [static_cast<id<ViewControllerSwapping>>(main_view_controller)
         activeViewController];
   }
+
   // The active view controller is either the TabGridViewController or its
-  // presented BVC.
-  return main_view_controller.presentedViewController
-             ? main_view_controller.presentedViewController
-             : main_view_controller;
+  // presented BVC. The BVC is itself contained inside of a
+  // BVCContainerViewController.
+  UIViewController* active_view_controller =
+      main_view_controller.presentedViewController
+          ? main_view_controller.presentedViewController
+          : main_view_controller;
+  if ([active_view_controller
+          isKindOfClass:[BVCContainerViewController class]]) {
+    active_view_controller =
+        base::mac::ObjCCastStrict<BVCContainerViewController>(
+            active_view_controller)
+            .currentBVC;
+  }
+  return active_view_controller;
 }
 
 id<ApplicationCommands, BrowserCommands> DispatcherForActiveViewController() {
