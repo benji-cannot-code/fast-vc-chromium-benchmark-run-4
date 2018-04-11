2c8e4a4dc24c14415f5429729bc44839429afbae
John Z Wu <jzw@chromium.org>
2018-04-11 06:18:13 +0000

    Remove forward declaration of CWVAutofillController from CWVWebView.h.
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I6dbaea927d88345e1ae5c99d1b4376a41729c190
    Reviewed-on: https://chromium-review.googlesource.com/1006517
    Reviewed-by: Hiroshi Ichikawa <ichikawa@chromium.org>
    Commit-Queue: Hiroshi Ichikawa <ichikawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549794}

--- ios/web_view/public/cwv_web_view.h
+++ ios/web_view/public/cwv_web_view.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 NS_ASSUME_NONNULL_BEGIN
 
-@class CWVAutofillController;
 @class CWVScrollView;
 @class CWVTranslationController;
 @class CWVWebViewConfiguration;
