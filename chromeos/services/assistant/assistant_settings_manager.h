84dc147cff708b9344376c128a98aefb096468b0
Jun Mukai <mukai@google.com>
2018-04-12 23:06:11 +0000

    fix build break: BindRequest needs to be pure virtual
    Otherwise linker fails for the undefined reference to the vtable
    of AssistantSettingsManager (not -Impl).
    
    Bug: None
    Test: build succeeds
    Change-Id: I5b9bcd7ae67c8fcf8ac51f2b6bf53807ed180f8f
    Reviewed-on: https://chromium-review.googlesource.com/1011384
    Reviewed-by: Muyuan Li <muyuanli@chromium.org>
    Commit-Queue: Jun Mukai <mukai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550401}

--- chromeos/services/assistant/assistant_settings_manager.h
+++ chromeos/services/assistant/assistant_settings_manager.h
@@ -18,7 +18,7 @@ class AssistantSettingsManager : public mojom::AssistantSettingsManager {
  public:
   ~AssistantSettingsManager() override = default;
 
-  virtual void BindRequest(mojom::AssistantSettingsManagerRequest request);
+  virtual void BindRequest(mojom::AssistantSettingsManagerRequest request) = 0;
 };
 
 }  // namespace assistant
