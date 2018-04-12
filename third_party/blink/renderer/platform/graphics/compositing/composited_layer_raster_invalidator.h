0f3024d20c8acf82b2c9f400c51b4771f07ebd8c
Xianzhu Wang <wangxianzhu@chromium.org>
2018-04-12 23:31:27 +0000

    [SPv175] Count paint invalidator and subsequence memory usage
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: If60370c0af378fa1d0a4f970d3cf3fe0f2f4925b
    Reviewed-on: https://chromium-review.googlesource.com/1011234
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550410}

--- third_party/blink/renderer/platform/graphics/compositing/composited_layer_raster_invalidator.h
+++ third_party/blink/renderer/platform/graphics/compositing/composited_layer_raster_invalidator.h
@@ -47,6 +47,8 @@ class PLATFORM_EXPORT CompositedLayerRasterInvalidator {
 
   const gfx::Rect& LayerBounds() const { return layer_bounds_; }
 
+  size_t ApproximateUnsharedMemoryUsage() const;
+
  private:
   friend class CompositedLayerRasterInvalidatorTest;
 
