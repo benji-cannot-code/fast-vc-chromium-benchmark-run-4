1edd6b56b842215a1e40f98f06e2b03aef9f96ac
Rayan Kanso <rayankans@chromium.org>
2018-04-12 16:40:31 +0000

    [Background Fetch] Replace BackgroundFetchedEvent w/BackgroundFetchUpdateEvent
    Dispatch instance of BackgroundFetchUpdateEvent with a backgroundfetched event.
    Add suitable constructors to BackgroundFetchUpdateEvent and
    BackgroundFetchSettledFetches.
    Add WebKit Layout test for BackgroundFetchUpdateEvent.
    
    Bug: 822765
    Change-Id: I557bd4f599009841444cbe2cbc53c4419e37892f
    Reviewed-on: https://chromium-review.googlesource.com/1005055
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550239}

---
+++ third_party/WebKit/LayoutTests/http/tests/background_fetch/resources/background-fetch-update-event-worker.js
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+'use strict';
+
+importScripts('/resources/testharness.js');
+
+promise_test(async function() {
+  assert_own_property(self, 'BackgroundFetchUpdateEvent');
+
+  // The `id` and `fetches` are required options in the
+  // BackgroundFetchUpdateEventInit. The latter must be an instance of
+  // BackgroundFetchSettledFetches.
+  assert_throws({name: "TypeError"}, () => new BackgroundFetchUpdateEvent('BackgroundFetchUpdateEvent'));
+  assert_throws({name: "TypeError"}, () => new BackgroundFetchUpdateEvent('BackgroundFetchUpdateEvent', {}));
+  assert_throws({name: "TypeError"}, () => new BackgroundFetchUpdateEvent('BackgroundFetchUpdateEvent', { id: 'foo' }));
+
+  // TODO(rayankans): Add actual construction test to BackgroundFetchUpdateEvent after
+  // https://github.com/WICG/background-fetch/issues/64 is resolved.
+
+}, 'Verifies that the BackgroundFetchUpdateEvent can be constructed.');
