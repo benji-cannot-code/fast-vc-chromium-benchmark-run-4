697ba03bff205b18ed0cd57289bfede2406c7c71
Amirhossein Simjour <asimjour@chromium.org>
2018-04-12 22:21:51 +0000

    VR support for android.app.AlertDialogs
    This is used to support html <select> element when it uses
    Android AlertDialogs.
    
    Bug:819876
    
    Change-Id: Ibe005b8227d623430e79a563521700258cc34d69
    Reviewed-on: https://chromium-review.googlesource.com/1010479
    Commit-Queue: Amirhossein Simjour <asimjour@chromium.org>
    Reviewed-by: Biao She <bshe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550387}

--- chrome/android/java/src/org/chromium/chrome/browser/vr_shell/VrUiWidgetFactory.java
+++ chrome/android/java/src/org/chromium/chrome/browser/vr_shell/VrUiWidgetFactory.java
@@ -6,9 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.vr_shell;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.widget.PopupWindow;
 
+import org.chromium.chrome.browser.modaldialog.ModalDialogManager;
 import org.chromium.ui.widget.UiWidgetFactory;
 
 /**
@@ -16,9 +18,11 @@ import org.chromium.ui.widget.UiWidgetFactory;
  */
 public class VrUiWidgetFactory extends UiWidgetFactory {
     private VrShell mVrShell;
+    private ModalDialogManager mModalDialogManager;
 
-    public VrUiWidgetFactory(VrShell vrShell) {
+    public VrUiWidgetFactory(VrShell vrShell, ModalDialogManager modalDialogManager) {
         mVrShell = vrShell;
+        mModalDialogManager = modalDialogManager;
     }
 
     @Override
@@ -29,6 +33,11 @@ public class VrUiWidgetFactory extends UiWidgetFactory {
     @Override
     public android.widget.Toast createToast(Context context) {
         return new VrToast(context, mVrShell);
+    }
+
+    @Override
+    public AlertDialog createAlertDialog(Context context) {
+        return new VrAlertDialog(context, mVrShell, mModalDialogManager);
     }
 
     @Override
