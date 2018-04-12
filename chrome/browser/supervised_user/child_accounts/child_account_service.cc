cbe09f38189a8dfe758e210a99b3c0b6b28191f2
Eduardo Cordeiro <escordeiro@google.com>
2018-04-12 12:52:00 +0000

    Cleaning up usage of ChildAccountDetection experiment, replacing it with target OS checks to enforce that the feature is always enabled on ChromeOS and Android, and disabled elsewhere
    Bug: 795857
    Change-Id: I2cd752637bc0cb5549606062c52c3b539d08677d
    Reviewed-on: https://chromium-review.googlesource.com/1002972
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Rahul Chaturvedi <rkc@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Eduardo Cordeiro <escordeiro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550163}

--- chrome/browser/supervised_user/child_accounts/child_account_service.cc
+++ chrome/browser/supervised_user/child_accounts/child_account_service.cc
@@ -38,10 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #endif
 
-#if !defined(OS_ANDROID)
-const char kChildAccountDetectionFieldTrialName[] = "ChildAccountDetection";
-#endif
-
 const char kGaiaCookieManagerSource[] = "child_account_service";
 
 // Normally, re-check the family info once per day.
@@ -91,15 +87,13 @@ ChildAccountService::~ChildAccountService() {
 
 // static
 bool ChildAccountService::IsChildAccountDetectionEnabled() {
-  // Child account detection is always enabled on Android.
-#if !defined(OS_ANDROID)
-  const std::string group_name =
-      base::FieldTrialList::FindFullName(kChildAccountDetectionFieldTrialName);
-  if (group_name == "Disabled")
-    return false;
-#endif
-
+// Child account detection is always enabled on Android and ChromeOS, and
+// disabled in other platforms.
+#if defined(OS_ANDROID) || defined(OS_CHROMEOS)
   return true;
+#else
+  return false;
+#endif
 }
 
 void ChildAccountService::RegisterProfilePrefs(
