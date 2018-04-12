b90a4a346370e1d08bd6e4b7c8e6b8eaec171bd7
Hwanseung Lee <hs1217.lee@samsung.com>
2018-04-12 13:08:55 +0000

    add use counter for resize with auto value
    recent versions of the spec do not support 'auto' value.
    so before retire that value, we should check how many use it.
    
    Bug: 831550
    Change-Id: I3b551ea759d05cade1e8823f3dc1c68f30b65816
    Reviewed-on: https://chromium-review.googlesource.com/1007294
    Commit-Queue: Hwanseung Lee <hwanseung@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550166}

--- third_party/blink/renderer/core/css/resolver/style_builder_custom.cc
+++ third_party/blink/renderer/core/css/resolver/style_builder_custom.cc
@@ -69,6 +69,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css_value_keywords.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/frame/settings.h"
+#include "third_party/blink/renderer/core/frame/use_counter.h"
+#include "third_party/blink/renderer/core/frame/web_feature.h"
 #include "third_party/blink/renderer/core/style/computed_style.h"
 #include "third_party/blink/renderer/core/style/computed_style_constants.h"
 #include "third_party/blink/renderer/core/style/content_data.h"
@@ -374,6 +376,7 @@ void StyleBuilderFunctions::applyValueCSSPropertyResize(
       r = settings->GetTextAreasAreResizable() ? EResize::kBoth
                                                : EResize::kNone;
     }
+    UseCounter::Count(state.GetDocument(), WebFeature::kCSSResizeAuto);
   } else {
     r = identifier_value.ConvertTo<EResize>();
   }
