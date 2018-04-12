4e038b41df35aed240e012934b564472f27572a6
Brandon Jones <bajones@chromium.org>
2018-04-12 22:01:29 +0000

    Drop the default MSAA samples to 2x for WebXR on mobile
    This makes it easier to hit the 60FPS ideal while encouraging apps to
    use some antialiasing by default. Should be paired with a future CL
    that bumps the default buffer resolution slightly to help compensate
    for the quality loss.
    
    Bug: 832185
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I7f089b4cde6c2d55d3965a4927d05a2a74520e2b
    Reviewed-on: https://chromium-review.googlesource.com/1010774
    Reviewed-by: Klaus Weidner <klausw@chromium.org>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550378}

--- third_party/blink/renderer/platform/graphics/gpu/xr_webgl_drawing_buffer.cc
+++ third_party/blink/renderer/platform/graphics/gpu/xr_webgl_drawing_buffer.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/platform/graphics/gpu/xr_webgl_drawing_buffer.h"
 
+#include "build/build_config.h"
 #include "third_party/blink/renderer/platform/graphics/accelerated_static_bitmap_image.h"
 #include "third_party/blink/renderer/platform/graphics/gpu/drawing_buffer.h"
 #include "third_party/blink/renderer/platform/graphics/gpu/extensions_3d_util.h"
@@ -146,7 +147,14 @@ bool XRWebGLDrawingBuffer::Initialize(const IntSize& size,
       (drawing_buffer_->webgl_version() > DrawingBuffer::kWebGL1 ||
        extensions_util->SupportsExtension("GL_EXT_texture_storage")) &&
       anti_aliasing_mode_ == kScreenSpaceAntialiasing;
+
+#if defined(OS_ANDROID)
+  // On Android devices use a smaller numer of samples to provide more breathing
+  // room for fill-rate-bound applications.
+  sample_count_ = std::min(2, max_sample_count);
+#else
   sample_count_ = std::min(4, max_sample_count);
+#endif
 
   Resize(size);
 
