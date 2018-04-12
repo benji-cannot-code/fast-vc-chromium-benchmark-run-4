99d07fad943a684a2714eb7428cd4a8ba8dc4363
Daniel Cheng <dcheng@chromium.org>
2018-04-12 00:28:59 +0000

    Enable chrome clang plugin check for kMaxValue enumerator.
    Bug: 705169, 742517
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I105debbc2ccca76bd251d1f068a2fefb8bba3e09
    Reviewed-on: https://chromium-review.googlesource.com/1004447
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549975}

--- gpu/ipc/service/gpu_vsync_provider_win.cc
+++ gpu/ipc/service/gpu_vsync_provider_win.cc
@@ -96,7 +96,7 @@ class GpuVSyncWorker : public base::Thread,
     kGetMonitorInfo = 1,
     kOpenAdapter = 2,
     kWaitForVBlankEvent = 3,
-    kMaxValue = 4
+    kMaxValue = kWaitForVBlankEvent,
   };
 
   void Reschedule();
@@ -354,8 +354,7 @@ void GpuVSyncWorker::WaitForVSyncOnThread() {
 }
 
 void GpuVSyncWorker::ReportErrorCode(WaitForVBlankErrorCode error_code) {
-  UMA_HISTOGRAM_ENUMERATION("GPU.WaitForVBlankErrorCode", error_code,
-                            WaitForVBlankErrorCode::kMaxValue);
+  UMA_HISTOGRAM_ENUMERATION("GPU.WaitForVBlankErrorCode", error_code);
 }
 
 void GpuVSyncWorker::AddTimestamp(base::TimeTicks timestamp) {
