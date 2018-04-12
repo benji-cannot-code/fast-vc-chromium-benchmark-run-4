86663733c48ee59cf1f8d7b32b0fd032ec0d6f72
Mikel Astiz <mastiz@chromium.org>
2018-04-12 11:57:30 +0000

    Improve test coverage for PutTabInWindow()
    Most notably, the scenario where the tab is initially mapped was not
    exercised in any test. We now cover this case as well as some other
    common updates for both local and remote sessions, which also includes
    test coverage for UpdateTrackerWithSpecifics() itself.
    
    This requires some minor API changes in SyncedSessionTracker.
    
    Bug: 814423
    Change-Id: If65494ef2f505eb6b49569e4d1d1f0a132f92e08
    Reviewed-on: https://chromium-review.googlesource.com/1005176
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550150}

--- components/sync_sessions/test_matchers.h
+++ components/sync_sessions/test_matchers.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_SYNC_SESSIONS_TEST_MATCHERS_H_
 #define COMPONENTS_SYNC_SESSIONS_TEST_MATCHERS_H_
 
+#include <map>
 #include <string>
 #include <vector>
 
@@ -31,6 +32,7 @@ testing::Matcher<const sync_pb::SessionSpecifics&> MatchesTab(
     testing::Matcher<std::string> session_tag,
     testing::Matcher<int> window_id,
     testing::Matcher<int> tab_id,
+    testing::Matcher<int> tab_node_id,
     testing::Matcher<std::vector<std::string>> urls);
 
 // Convenience overload.
@@ -38,18 +40,17 @@ testing::Matcher<const sync_pb::SessionSpecifics&> MatchesTab(
     testing::Matcher<std::string> session_tag,
     testing::Matcher<int> window_id,
     testing::Matcher<int> tab_id,
+    testing::Matcher<int> tab_node_id,
     const std::vector<std::string>& urls);
 
 testing::Matcher<const SyncedSession*> MatchesSyncedSession(
     testing::Matcher<std::string> session_tag,
-    testing::Matcher<std::vector<int>> window_ids,
-    testing::Matcher<std::vector<int>> tab_ids);
+    testing::Matcher<std::map<int, std::vector<int>>> window_id_to_tabs);
 
 // Convenience overload.
 testing::Matcher<const SyncedSession*> MatchesSyncedSession(
     testing::Matcher<std::string> session_tag,
-    const std::vector<int>& window_ids,
-    const std::vector<int>& tab_ids);
+    const std::map<int, std::vector<int>>& window_id_to_tabs);
 
 }  // namespace sync_sessions
 
