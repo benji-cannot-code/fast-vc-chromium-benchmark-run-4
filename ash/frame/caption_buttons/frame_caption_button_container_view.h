26cf7ef519523e63c0ec80282fdb7e580d3f1700
Alan Cutter <alancutter@chromium.org>
2018-04-12 06:23:27 +0000

    Use Android style title bar colors for theme colors on chromeOS
    This CL updates the way hosted app windows pick title bar colors
    while a theme color is set on chromeOS.
    
    This aligns the behaviour with Android, see:
    https://codereview.chromium.org/1956803002.
    
    Before:
    https://bugs.chromium.org/p/chromium/issues/attachment?aid=329991&signed_aid=3q0FM93vRJrPJV7kMinOTw==&inline=1
    https://bugs.chromium.org/p/chromium/issues/attachment?aid=333012&signed_aid=VNXQQhjxyzYYyg36S3nnOQ==&inline=1
    https://bugs.chromium.org/p/chromium/issues/attachment?aid=330203&signed_aid=DoC1IRqCuP659D2dMoL1_Q==&inline=1
    
    After:
    https://bugs.chromium.org/p/chromium/issues/attachment?aid=333244&signed_aid=J5yNTnY4-7RqGeYu32N8OA==&inline=1
    https://bugs.chromium.org/p/chromium/issues/attachment?aid=333245&signed_aid=VOf6afQMQJbCApYu21wmyw==&inline=1
    https://bugs.chromium.org/p/chromium/issues/attachment?aid=333257&signed_aid=OJzGQyVnXzCfXF6bZ16TCg==&inline=1
    https://bugs.chromium.org/p/chromium/issues/attachment?aid=333258&signed_aid=jygsmXqxXfoPsPZXb_gqfg==&inline=1
    
    Bug: 814121
    Change-Id: I10a2e837d0c8a938072171715119a04c28e4f36a
    Reviewed-on: https://chromium-review.googlesource.com/999213
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550045}

--- ash/frame/caption_buttons/frame_caption_button_container_view.h
+++ ash/frame/caption_buttons/frame_caption_button_container_view.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 
 #include "ash/ash_export.h"
+#include "ash/frame/caption_buttons/frame_caption_button.h"
 #include "ash/frame/caption_buttons/frame_size_button_delegate.h"
 #include "base/macros.h"
 #include "ui/gfx/animation/animation_delegate.h"
@@ -84,9 +85,12 @@ class ASH_EXPORT FrameCaptionButtonContainerView
   // a repaint.
   void SetPaintAsActive(bool paint_as_active);
 
-  // Sets whether the buttons should be painted in a lighter color (for use on
-  // dark backgrounds).
-  void SetUseLightImages(bool light);
+  // Sets whether the buttons should use themed foreground color computation.
+  void SetColorMode(FrameCaptionButton::ColorMode color_mode);
+
+  // Sets the background frame color that buttons should compute their color
+  // respective to.
+  void SetBackgroundColor(SkColor background_color);
 
   // Tell the window controls to reset themselves to the normal state.
   void ResetWindowControls();
---
+++ ash/frame/caption_buttons/frame_caption_button_unittest.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/frame/caption_buttons/frame_caption_button.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/skia/include/core/SkColor.h"
+#include "ui/base/material_design/material_design_controller.h"
+#include "ui/gfx/color_utils.h"
+
+namespace {
+
+constexpr SkColor kBackgroundColors[] = {
+    SK_ColorBLACK,  SK_ColorDKGRAY,
+    SK_ColorGRAY,   SK_ColorLTGRAY,
+    SK_ColorWHITE,  SK_ColorRED,
+    SK_ColorYELLOW, SK_ColorCYAN,
+    SK_ColorBLUE,   SkColorSetRGB(230, 138, 90),
+};
+
+}  // namespace
+
+TEST(FrameCaptionButtonTest, ThemedColorContrast) {
+  for (SkColor background_color : kBackgroundColors) {
+    SkColor button_color = ash::FrameCaptionButton::GetButtonColor(
+        ash::FrameCaptionButton::ColorMode::kThemed, background_color);
+    EXPECT_GE(color_utils::GetContrastRatio(button_color, background_color), 3);
+  }
+}
