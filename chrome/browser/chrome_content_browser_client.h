019eb8bad9eb710c0462d00256d11857b7e887fc
Adam Langley <agl@chromium.org>
2018-04-12 14:58:47 +0000

    webauthn: require that requests come from a frame in a focused window.
    The spec suggests aborting an operation when focus[1] is lost, but
    that's advice for site developers. I can't find anything in the spec
    about preventing background tabs from triggering operations.
    
    The cryptotoken extension refused to start a registration request, or to
    send a registration response to, anything but the active tab in the
    focused window. But background tabs could complete an authentication
    request.
    
    This change does something similar: it rejects both authentication and
    registration requests unless the requesting frame is in a focused
    window. It also performs that check before returning responses.
    
    This is slightly different from the cryptotoken behaviour because
    cryptotoken could only use what the extensions API exposed. For example,
    if the omnibox was focused, cryptotoken would complete a registration
    from the foreground tab but this code will reject it. I think this
    behaviour is better, and it's certainly far more inline with the content
    / browser separation.
    
    This change has been split from its tests, which will come in a future
    CL.
    
    [1] https://w3c.github.io/webauthn/#abortoperation
    
    Bug: 827266
    Change-Id: If6e97dd6526e175f40718724eda21e3efd434f7f
    Reviewed-on: https://chromium-review.googlesource.com/991073
    Commit-Queue: Adam Langley <agl@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550195}

--- chrome/browser/chrome_content_browser_client.h
+++ chrome/browser/chrome_content_browser_client.h
@@ -404,6 +404,7 @@ class ChromeContentBrowserClient : public content::ContentBrowserClient {
       content::BrowserContext* browser_context,
       const GURL& url,
       base::OnceCallback<void(bool, int, int)> callback) override;
+
   bool ShouldPermitIndividualAttestationForWebauthnRPID(
       content::BrowserContext* browser_context,
       const std::string& rp_id) override;
@@ -412,6 +413,8 @@ class ChromeContentBrowserClient : public content::ContentBrowserClient {
       const std::string& rp_id,
       const url::Origin& origin,
       base::OnceCallback<void(bool)> callback) override;
+  bool ShouldEnforceFocusChecksForWebauthn() override;
+
   std::unique_ptr<net::ClientCertStore> CreateClientCertStore(
       content::ResourceContext* resource_context) override;
   scoped_refptr<content::LoginDelegate> CreateLoginDelegate(
--- content/browser/webauth/authenticator_impl.cc
+++ content/browser/webauth/authenticator_impl.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
+#include "content/public/browser/render_widget_host_view.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/content_client.h"
 #include "content/public/common/content_features.h"
@@ -427,6 +428,13 @@ void AuthenticatorImpl::MakeCredential(
     return;
   }
 
+  if (GetContentClient()->browser()->ShouldEnforceFocusChecksForWebauthn() &&
+      !render_frame_host_->GetView()->HasFocus()) {
+    std::move(callback).Run(webauth::mojom::AuthenticatorStatus::NOT_FOCUSED,
+                            nullptr);
+    return;
+  }
+
   url::Origin caller_origin = render_frame_host_->GetLastCommittedOrigin();
   relying_party_id_ = options->relying_party->id;
 
@@ -537,6 +545,13 @@ void AuthenticatorImpl::GetAssertion(
   if (u2f_request_ || ctap_request_) {
     std::move(callback).Run(
         webauth::mojom::AuthenticatorStatus::PENDING_REQUEST, nullptr);
+    return;
+  }
+
+  if (GetContentClient()->browser()->ShouldEnforceFocusChecksForWebauthn() &&
+      !render_frame_host_->GetView()->HasFocus()) {
+    std::move(callback).Run(webauth::mojom::AuthenticatorStatus::NOT_FOCUSED,
+                            nullptr);
     return;
   }
 
@@ -789,7 +804,14 @@ void AuthenticatorImpl::InvokeCallbackAndCleanup(
     MakeCredentialCallback callback,
     webauth::mojom::AuthenticatorStatus status,
     webauth::mojom::MakeCredentialAuthenticatorResponsePtr response) {
-  std::move(callback).Run(status, std::move(response));
+  if (GetContentClient()->browser()->ShouldEnforceFocusChecksForWebauthn() &&
+      !render_frame_host_->GetView()->HasFocus()) {
+    std::move(callback).Run(webauth::mojom::AuthenticatorStatus::NOT_FOCUSED,
+                            nullptr);
+  } else {
+    std::move(callback).Run(status, std::move(response));
+  }
+
   Cleanup();
 }
 
@@ -797,7 +819,14 @@ void AuthenticatorImpl::InvokeCallbackAndCleanup(
     GetAssertionCallback callback,
     webauth::mojom::AuthenticatorStatus status,
     webauth::mojom::GetAssertionAuthenticatorResponsePtr response) {
-  std::move(callback).Run(status, std::move(response));
+  if (GetContentClient()->browser()->ShouldEnforceFocusChecksForWebauthn() &&
+      !render_frame_host_->GetView()->HasFocus()) {
+    std::move(callback).Run(webauth::mojom::AuthenticatorStatus::NOT_FOCUSED,
+                            nullptr);
+  } else {
+    std::move(callback).Run(status, std::move(response));
+  }
+
   Cleanup();
 }
 
