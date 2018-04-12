66dc614d5725ec37622e9525482db00d61a44df8
Wenzhao Zang <wzang@chromium.org>
2018-04-12 05:21:10 +0000

    cros: Update |kRlzEmbargoEndDateGarbageDateThresholdDays| value
    Because the factory already adds 7 days to the actual date, the
    threshold should only add 7 days to rlz_embargo_end_date.
    
    Bug: 820783
    Change-Id: I97a599b2f893493ac91910f449051ec5a0ea7807
    Reviewed-on: https://chromium-review.googlesource.com/1007962
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Commit-Queue: Wenzhao (Colin) Zang <wzang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550016}

--- rlz/chromeos/lib/rlz_value_store_chromeos.cc
+++ rlz/chromeos/lib/rlz_value_store_chromeos.cc
@@ -119,8 +119,7 @@ bool HasRlzEmbargoEndDatePassed() {
 
 }  // namespace
 
-const int RlzValueStoreChromeOS::kRlzEmbargoEndDateGarbageDateThresholdDays =
-    14;
+const int RlzValueStoreChromeOS::kRlzEmbargoEndDateGarbageDateThresholdDays = 7;
 
 const int RlzValueStoreChromeOS::kMaxRetryCount = 3;
 
