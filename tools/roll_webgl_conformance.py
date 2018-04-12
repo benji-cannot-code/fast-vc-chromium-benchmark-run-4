0a7421b05cd87bd7c5249922536f4cf427b401e2
Corentin Wallez <cwallez@chromium.org>
2018-04-12 14:29:47 +0000

    PRESUBMITs.py, use LUCI for the optional GPU tests on Windows
    Also fixup some documentation links for GPU optional bots.
    Also change a PRESUBMIT.py's master name for win_angle_rel_ng.
    
    TBR=kbr@chromium.org
    TBR=sky@chromium.org
    
    BUG=chromium:815092
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_vr
    Change-Id: Ifda505e121f67f922f19da466a37b7f6266cea05
    Reviewed-on: https://chromium-review.googlesource.com/1008182
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550188}

--- tools/roll_webgl_conformance.py
+++ tools/roll_webgl_conformance.py
@@ -15,7 +15,7 @@ import time
 
 extra_trybots = [
   {
-    "mastername": "master.tryserver.chromium.win",
+    "mastername": "luci.chromium.try",
     "buildernames": ["win_optional_gpu_tests_rel"]
   },
   {
@@ -37,7 +37,7 @@ extra_trybots = [
     "buildernames": ["linux_angle_rel_ng"]
   },
   {
-    "mastername": "master.tryserver.chromium.angle",
+    "mastername": "luci.chromium.try",
     "buildernames": ["win_angle_rel_ng"]
   },
 ]
