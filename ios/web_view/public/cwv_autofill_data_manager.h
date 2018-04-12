d3c587bf5ec1b7c36c36fa74060ea715491c667b
John Z Wu <jzw@chromium.org>
2018-04-12 22:48:47 +0000

    Define some APIs for CWVAutofillDataManager.
    1. Get all address autofill profiles.
    2. Update an address autofill profile.
    3. Delete an address autofill profile.
    
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: If433ea349119dda0e61fb3b5963a561b234984c8
    Reviewed-on: https://chromium-review.googlesource.com/1008403
    Commit-Queue: John Wu <jzw@chromium.org>
    Reviewed-by: Hiroshi Ichikawa <ichikawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550400}

--- ios/web_view/public/cwv_autofill_data_manager.h
+++ ios/web_view/public/cwv_autofill_data_manager.h
@@ -12,11 +12,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 NS_ASSUME_NONNULL_BEGIN
 
+@class CWVAutofillProfile;
+
 CWV_EXPORT
 // Exposes saved autofill data such as address profiles and credit cards.
 @interface CWVAutofillDataManager : NSObject
 
+// Returns all saved profiles for address autofill.
+@property(nonatomic, readonly) NSArray<CWVAutofillProfile*>* profiles;
+
 - (instancetype)init NS_UNAVAILABLE;
+
+// Updates the profile.
+- (void)updateProfile:(CWVAutofillProfile*)profile;
+
+// Deletes the profile.
+- (void)deleteProfile:(CWVAutofillProfile*)profile;
 
 @end
 
