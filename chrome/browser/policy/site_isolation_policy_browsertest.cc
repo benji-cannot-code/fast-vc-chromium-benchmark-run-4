11113ae32659ec717b4841b835451438f4405e3c
Julian Pastarmov <pastarmovj@chromium.org>
2018-04-12 09:35:21 +0000

    Allow the Site Isolation policies to override related trials.
    The site isolation policies until now could only enable site isolation
    but being set to disabled was equivalent to not being set at all.
    With this change their disabled state is going to prevent trials from
    turning on site isolation per default. The user can still decide to
    enable site isolation by using the command line flags directly.
    
    Additionally fixes the problem of adding the isolated origins flag
    twice if the browser is restarted programmatically.
    
    BUG=827821, 826624
    
    Change-Id: I754e1d8837b5afb0e7a70acbdb2e823733ad9663
    Reviewed-on: https://chromium-review.googlesource.com/997737
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550124}

--- chrome/browser/policy/site_isolation_policy_browsertest.cc
+++ chrome/browser/policy/site_isolation_policy_browsertest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
+#include "chrome/common/chrome_features.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
@@ -46,6 +47,7 @@ class SiteIsolationPolicyBrowserTest : public InProcessBrowserTest {
   DISALLOW_COPY_AND_ASSIGN(SiteIsolationPolicyBrowserTest);
 };
 
+template <bool policy_value>
 class SitePerProcessPolicyBrowserTest : public SiteIsolationPolicyBrowserTest {
  protected:
   SitePerProcessPolicyBrowserTest() {}
@@ -63,7 +65,7 @@ class SitePerProcessPolicyBrowserTest : public SiteIsolationPolicyBrowserTest {
     policy::PolicyMap values;
     values.Set(policy::key::kSitePerProcess, policy::POLICY_LEVEL_MANDATORY,
                policy::POLICY_SCOPE_USER, policy::POLICY_SOURCE_CLOUD,
-               std::make_unique<base::Value>(true), nullptr);
+               std::make_unique<base::Value>(policy_value), nullptr);
     provider_.UpdateChromePolicy(values);
 
     // Append the automation switch which should disable Site Isolation when the
@@ -78,6 +80,11 @@ class SitePerProcessPolicyBrowserTest : public SiteIsolationPolicyBrowserTest {
  private:
   DISALLOW_COPY_AND_ASSIGN(SitePerProcessPolicyBrowserTest);
 };
+
+typedef SitePerProcessPolicyBrowserTest<true>
+    SitePerProcessPolicyBrowserTestEnabled;
+typedef SitePerProcessPolicyBrowserTest<false>
+    SitePerProcessPolicyBrowserTestDisabled;
 
 class IsolateOriginsPolicyBrowserTest : public SiteIsolationPolicyBrowserTest {
  protected:
@@ -107,7 +114,7 @@ class IsolateOriginsPolicyBrowserTest : public SiteIsolationPolicyBrowserTest {
 };
 
 class WebDriverSitePerProcessPolicyBrowserTest
-    : public SitePerProcessPolicyBrowserTest {
+    : public SitePerProcessPolicyBrowserTestEnabled {
  protected:
   WebDriverSitePerProcessPolicyBrowserTest()
       : are_sites_isolated_for_testing_(false) {}
@@ -143,7 +150,7 @@ class WebDriverSitePerProcessPolicyBrowserTest
 
 // Ensure that --disable-site-isolation-trials does not override policies.
 class NoOverrideSitePerProcessPolicyBrowserTest
-    : public SitePerProcessPolicyBrowserTest {
+    : public SitePerProcessPolicyBrowserTestEnabled {
  protected:
   NoOverrideSitePerProcessPolicyBrowserTest() {}
   void SetUpCommandLine(base::CommandLine* command_line) override {
@@ -154,7 +161,7 @@ class NoOverrideSitePerProcessPolicyBrowserTest
   DISALLOW_COPY_AND_ASSIGN(NoOverrideSitePerProcessPolicyBrowserTest);
 };
 
-IN_PROC_BROWSER_TEST_F(SitePerProcessPolicyBrowserTest, Simple) {
+IN_PROC_BROWSER_TEST_F(SitePerProcessPolicyBrowserTestEnabled, Simple) {
   Expectations expectations[] = {
       {"https://foo.com/noodles.html", true},
       {"http://foo.com/", true},
@@ -191,4 +198,36 @@ IN_PROC_BROWSER_TEST_F(NoOverrideSitePerProcessPolicyBrowserTest, Simple) {
       {"http://example.org/pumpkins.html", true},
   };
   CheckExpectations(expectations, arraysize(expectations));
+}
+
+class SitePerProcessPolicyBrowserTestFieldTrialTest
+    : public SitePerProcessPolicyBrowserTestDisabled {
+ public:
+  SitePerProcessPolicyBrowserTestFieldTrialTest() {
+    scoped_feature_list_.InitAndEnableFeature(features::kSitePerProcess);
+  }
+  ~SitePerProcessPolicyBrowserTestFieldTrialTest() override {}
+
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_;
+
+  DISALLOW_COPY_AND_ASSIGN(SitePerProcessPolicyBrowserTestFieldTrialTest);
+};
+
+IN_PROC_BROWSER_TEST_F(SitePerProcessPolicyBrowserTestFieldTrialTest, Simple) {
+  // Skip this test if all sites are isolated.
+  if (content::AreAllSitesIsolatedForTesting())
+    return;
+  ASSERT_TRUE(base::CommandLine::ForCurrentProcess()->HasSwitch(
+      switches::kDisableSiteIsolationTrials));
+  Expectations expectations[] = {
+      {"https://foo.com/noodles.html", false},
+      {"http://example.org/pumpkins.html", false},
+  };
+  CheckExpectations(expectations, arraysize(expectations));
+}
+
+IN_PROC_BROWSER_TEST_F(SiteIsolationPolicyBrowserTest, NoPolicyNoTrialsFlags) {
+  ASSERT_FALSE(base::CommandLine::ForCurrentProcess()->HasSwitch(
+      switches::kDisableSiteIsolationTrials));
 }
