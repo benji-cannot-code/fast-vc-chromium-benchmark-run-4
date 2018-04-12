2357d9be45cdd74697a6886edc42eb5659041361
Daniel Park <danielpark@chromium.org>
2018-04-12 18:19:00 +0000

    [Survey] Replaces ChromeHome action & histograms with Chrome
    Survey flag coming in a follow up patch
    
    Bug: 830108
    Change-Id: I442f1c7eb6bf879e0495ec219b8fe7ce91db9039
    Reviewed-on: https://chromium-review.googlesource.com/1000762
    Commit-Queue: Daniel Park <danielpark@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550276}

--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -76,7 +76,6 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &kChromeHomeMenuItemsExpandSheet,
     &kChromeHomePersistentIph,
     &kChromeHomePullToRefreshIphAtTop,
-    &kChromeHomeSurvey,
     &kChromeHomeSwipeLogic,
     &kChromeHomeSwipeLogicVelocity,
     &kChromeSmartSelection,
