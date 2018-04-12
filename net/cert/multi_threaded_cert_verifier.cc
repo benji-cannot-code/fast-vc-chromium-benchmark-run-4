ebdd879e447a28862da04797cb1e2fafd6404356
Jeremy Roman <jbroman@chromium.org>
2018-04-12 19:01:52 +0000

    Remove unnecessary includes of base/memory/ptr_util.h in net/
    It no longer defines base::MakeUnique, so it should only be included
    where base::WrapUnique is used.
    
    Generated by:
      for x in `git grep -l '#include "base/memory/ptr_util.h"' net`; do \
        grep -q WrapUnique "$x" || sed -i -e \
        '/^#include "base\/memory\/ptr_util\.h"$/d' "$x"; done
    
    One include was added to net/cert/multi_threaded_cert_verifier.cc
    which previously depended on ptr_util.h being included by one of its
    headers.
    
    Bug: 755727
    Tbr: kerrnel@chromium.org
    Change-Id: I61a99097ffab3ad39b77a6506d793c9bde54d8ef
    Reviewed-on: https://chromium-review.googlesource.com/1010402
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550293}

--- net/cert/multi_threaded_cert_verifier.cc
+++ net/cert/multi_threaded_cert_verifier.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_helpers.h"
 #include "base/compiler_specific.h"
 #include "base/containers/linked_list.h"
+#include "base/memory/ptr_util.h"
 #include "base/message_loop/message_loop.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/sha1.h"
--- net/ssl/ssl_client_session_cache_unittest.cc
+++ net/ssl/ssl_client_session_cache_unittest.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/ssl/ssl_client_session_cache.h"
 
-#include "base/memory/ptr_util.h"
 #include "base/run_loop.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/test/simple_test_clock.h"
