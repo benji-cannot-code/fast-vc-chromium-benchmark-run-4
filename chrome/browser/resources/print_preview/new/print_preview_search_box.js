c0788c8d7818c24de845b35f24604545bb6f2c28
rbpotter <rbpotter@chromium.org>
2018-04-12 08:01:48 +0000

    Print Preview componentization: Add a timeout on search fields
    Add a timeout so that the list does not immediately begin changing.
    This will better match the behavior of the old UI.
    
    Bug: 773928
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I5c9d3c3ff2ccd48b30dbb8ef34a86aceb3cbee3a
    Reviewed-on: https://chromium-review.googlesource.com/1007895
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550090}

--- chrome/browser/resources/print_preview/new/print_preview_search_box.js
+++ chrome/browser/resources/print_preview/new/print_preview_search_box.js
@@ -26,6 +26,12 @@ Polymer({
     'search-changed': 'onSearchChanged_',
   },
 
+  /**
+   * Timeout used to delay processing of the input, in ms.
+   * @private {?number}
+   */
+  timeout_: null,
+
   /** @return {!HTMLInputElement} */
   getSearchInput: function() {
     return this.$.searchInput;
@@ -33,11 +39,17 @@ Polymer({
 
   /**
    * @param {!CustomEvent} e Event containing the new search.
+   * @private
    */
   onSearchChanged_: function(e) {
-    const safeQuery = e.detail.trim().replace(SANITIZE_REGEX, '\\$&');
-    this.searchQuery =
-        safeQuery.length > 0 ? new RegExp(`(${safeQuery})`, 'i') : null;
+    let safeQuery = e.detail.trim().replace(SANITIZE_REGEX, '\\$&');
+    safeQuery = safeQuery.length > 0 ? new RegExp(`(${safeQuery})`, 'i') : null;
+    if (this.timeout_)
+      clearTimeout(this.timeout_);
+    this.timeout_ = setTimeout(() => {
+      this.searchQuery = safeQuery;
+      this.timeout_ = null;
+    }, 150);
   },
 });
 })();
