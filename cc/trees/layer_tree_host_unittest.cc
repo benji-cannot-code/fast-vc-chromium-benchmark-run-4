dc87c41b5e381ba4eefda40f658117b9bdbbda4c
Adrienne Walker <enne@chromium.org>
2018-04-12 21:10:02 +0000

    cc: Add backpressure for speculative raster work
    This adds a limit per raster type of how many outstanding speculative
    (i.e. non-required) tasks can be in flight at any given time.  Not only
    will the task graph not schedule more than this limit, but it will also
    wait for these tasks to have been completed on the gpu service side as
    well.
    
    Bug: 816571
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Iff2cbc5ecaabafa62db6a06c0e8540156bfbe22d
    Reviewed-on: https://chromium-review.googlesource.com/998682
    Commit-Queue: enne <enne@chromium.org>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550360}

--- cc/trees/layer_tree_host_unittest.cc
+++ cc/trees/layer_tree_host_unittest.cc
@@ -6983,6 +6983,8 @@ class GpuRasterizationRasterizesBorderTiles : public LayerTreeHostTest {
 
   void InitializeSettings(LayerTreeSettings* settings) override {
     settings->gpu_rasterization_forced = true;
+    settings->max_prepaint_gpu_raster_tasks =
+        std::numeric_limits<size_t>::max();
   }
 
   void SetupTree() override {
