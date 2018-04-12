1c15c6a8991061b27597c67ba534ea9c56190b3e
Daniel Cheng <dcheng@chromium.org>
2018-04-12 12:02:18 +0000

    Return null from contentDocument/getSVGDocument() for cross-origin documents.
    Change-Id: I1e66f2cdc50be889b697b54401be6352607fa649
    Reviewed-on: https://chromium-review.googlesource.com/1006528
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550152}

--- third_party/WebKit/LayoutTests/external/wpt_automation/fullscreen/auto-click.js
+++ third_party/WebKit/LayoutTests/external/wpt_automation/fullscreen/auto-click.js
@@ -31,7 +31,8 @@ const observer = new MutationObserver(mutations => {
       if (node.localName == 'button')
         click(node);
       else if (node.localName == 'iframe')
-        observe(node.contentDocument);
+        if (node.contentDocument)
+          observe(node.contentDocument);
     }
   }
 });
@@ -45,17 +46,12 @@ for (const button of document.getElementsByTagName('button')) {
   click(button);
 }
 for (const iframe of document.getElementsByTagName('iframe')) {
-  try {
+  if (iframe.contentDocument)
     observe(iframe.contentDocument);
-    iframe.addEventListener('load', () => {
+  iframe.addEventListener('load', () => {
+    if (iframe.contentDocument)
       observe(iframe.contentDocument);
-    });
-  } catch (e) {
-    // Skip cross-origin iframes, but report other errors
-    if (e.name != "SecurityError") {
-        throw e;
-    }
-  }
+  });
 }
 
 // Observe future changes.
