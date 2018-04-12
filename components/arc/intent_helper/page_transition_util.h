c3de768a6ffecc0e44a8cb922f4073f1eb777fb4
David Jacobo <djacobo@chromium.org>
2018-04-12 23:14:59 +0000

    Add dedicated flag for navigations coming from ARC
    Authentication process for a variety of apps require ARC to send some of
    the traffic to Chrome-side, where we can validate the user.
    
    Most likely after validating the credentials the tab will try to
    comunicate back to the app, using a url that makes no sense on Chrome
    side (e.g. copito_app://some_token) and thus it would be useful to:
    
    1) Bypass the intent picker when there is only one potential app to
    handle the url and we can guarante this navigation was originated within
    ARC.
    2) Close the current tab, used only for authentication.
    
    This CL addresses 1) by creating and attaching ArcWebContentsData to a
    tab generated via ChromeShellDelegate so we can uniquely label this kind
    of navigations.
    
    Bug: b/67656508
    Test: Build, also added new unit tests for the added functionality.
    Change-Id: I8e5e7537937b6f1e03f896a9f8893cb67044120f
    Reviewed-on: https://chromium-review.googlesource.com/890160
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Commit-Queue: David Jacobo <djacobo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550407}

--- components/arc/intent_helper/page_transition_util.h
+++ components/arc/intent_helper/page_transition_util.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_ARC_INTENT_HELPER_PAGE_TRANSITION_UTIL_H_
 #define COMPONENTS_ARC_INTENT_HELPER_PAGE_TRANSITION_UTIL_H_
 
+#include "base/macros.h"
 #include "ui/base/page_transition_types.h"
 
 namespace arc {
