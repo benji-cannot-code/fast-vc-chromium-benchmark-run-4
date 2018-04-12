5b97a884d4d157369a001729ca661e4a82c913b5
Tom Anderson <thomasanderson@chromium.org>
2018-04-12 06:23:12 +0000

    Replace Verdana with Arimo GCS font
    Verdana is NOT metrically-compatible with Arimo, but it is chosen as the
    replacement since they are both from the Sans family.  In all of the layout test
    deltas, it appears the choice of Verdana was purely coincidental, so it should
    be safe to switch to Arimo.
    
    BUG=787020
    
    Change-Id: Id8c82888b2e844e35200d20532b88d87066792bc
    Reviewed-on: https://chromium-review.googlesource.com/1006221
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550044}

--- ui/gfx/font_names_testing.cc
+++ ui/gfx/font_names_testing.cc
@@ -10,6 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace gfx {
 
 #if defined(OS_LINUX)
+const char kTestFontName[] = "Arimo";
+#else
+const char kTestFontName[] = "Arial";
+#endif
+
+#if defined(OS_LINUX)
 const char kSymbolFontName[] = "DejaVu Sans";
 #else
 const char kSymbolFontName[] = "Symbol";
--- ui/gfx/font_names_testing.h
+++ ui/gfx/font_names_testing.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace gfx {
 
+extern const char kTestFontName[];
 extern const char kSymbolFontName[];
 extern const char kCJKFontName[];
 
