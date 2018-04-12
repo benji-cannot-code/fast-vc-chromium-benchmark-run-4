ba86d713268a54a66716747a0f8716f64610ac00
Joe Downing <joedow@chromium.org>
2018-04-12 18:18:04 +0000

    [KeyboardLock] Simplifying press and hold ESC logic for fullscreen
    This CL simplifies the logic used for determing whether Chrome should use
    the press and hold ESC logic to exit fullscreen.  The previous mechanism
    used a combination of three values (experiment flag, lock active, esc
    required).  This can be simplified and bit and moved into a single method
    to determine which message to show the UI and whether to use the press
    and hold logic in the KeyboardLockController for exiting fullscreen.
    
    BUG=680809
    
    Change-Id: I25c80e5cfd6c3e318c815d233a51c447f7d2d8d4
    Reviewed-on: https://chromium-review.googlesource.com/1007497
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550275}

--- chrome/browser/ui/exclusive_access/keyboard_lock_controller.cc
+++ chrome/browser/ui/exclusive_access/keyboard_lock_controller.cc
@@ -78,7 +78,10 @@ bool KeyboardLockController::IsKeyboardLockActive() const {
 }
 
 bool KeyboardLockController::RequiresPressAndHoldEscToExit() const {
-  return keyboard_lock_state_ == KeyboardLockState::kLockedWithEsc;
+  DCHECK_EQ(keyboard_lock_state_ == KeyboardLockState::kUnlocked,
+            exclusive_access_tab() == nullptr);
+  return IsExperimentalKeyboardLockUIEnabled() ||
+         keyboard_lock_state_ == KeyboardLockState::kLockedWithEsc;
 }
 
 void KeyboardLockController::RequestKeyboardLock(WebContents* web_contents,
@@ -102,8 +105,7 @@ bool KeyboardLockController::HandleKeyEvent(
   // active keyboard lock request which requires press and hold, then we just
   // return as the simple 'press esc to exit' case is handled by the caller
   // (which is the ExclusiveAccessManager in this case).
-  if (!IsExperimentalKeyboardLockUIEnabled() &&
-      !(IsKeyboardLockActive() && RequiresPressAndHoldEscToExit()))
+  if (!RequiresPressAndHoldEscToExit())
     return false;
 
   // TODO(joedow): Hook up press and hold exit animation here.
