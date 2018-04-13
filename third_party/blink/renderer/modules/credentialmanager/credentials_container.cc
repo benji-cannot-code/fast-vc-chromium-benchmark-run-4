6eb2317753d2181ba8daeb25e0c7446f75c8eaaf
Kim Paulhamus <kpaulhamus@chromium.org>
2018-04-13 00:50:32 +0000

    Restrict WebAuthN to "secure and same-origin with all their ancestors"
    Bug: 828958
    Change-Id: Ie24fb55ddf5b8180036e830cd73ae58b0fb2ccd5
    Reviewed-on: https://chromium-review.googlesource.com/994177
    Commit-Queue: Kim Paulhamus <kpaulhamus@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550447}

--- third_party/blink/renderer/modules/credentialmanager/credentials_container.cc
+++ third_party/blink/renderer/modules/credentialmanager/credentials_container.cc
@@ -125,9 +125,10 @@ bool CheckSecurityRequirementsBeforeRequest(
       !IsSameOriginWithAncestors(resolver->GetFrame())) {
     resolver->Reject(DOMException::Create(
         kNotAllowedError,
-        "`PasswordCredential` and `FederatedCredential` objects may only be "
-        "stored/retrieved in a document which is same-origin with all of its "
-        "ancestors."));
+        "The following credential operations can only occur in a document which"
+        " is same-origin with all of its ancestors: "
+        "storage/retrieval of 'PasswordCredential' and 'FederatedCredential', "
+        "and creation/retrieval of 'PublicKeyCredential'"));
     return false;
   }
 
@@ -428,10 +429,7 @@ ScriptPromise CredentialsContainer::get(
   ScriptPromiseResolver* resolver = ScriptPromiseResolver::Create(script_state);
   ScriptPromise promise = resolver->Promise();
 
-  auto required_origin_type =
-      options.hasFederated() || options.hasPassword()
-          ? RequiredOriginType::kSecureAndSameWithAncestors
-          : RequiredOriginType::kSecure;
+  auto required_origin_type = RequiredOriginType::kSecureAndSameWithAncestors;
   if (!CheckSecurityRequirementsBeforeRequest(resolver, required_origin_type))
     return promise;
 
@@ -558,7 +556,10 @@ ScriptPromise CredentialsContainer::create(
   ScriptPromiseResolver* resolver = ScriptPromiseResolver::Create(script_state);
   ScriptPromise promise = resolver->Promise();
 
-  const auto required_origin_type = RequiredOriginType::kSecure;
+  auto required_origin_type =
+      options.hasPublicKey() ? RequiredOriginType::kSecureAndSameWithAncestors
+                             : RequiredOriginType::kSecure;
+
   if (!CheckSecurityRequirementsBeforeRequest(resolver, required_origin_type))
     return promise;
 
