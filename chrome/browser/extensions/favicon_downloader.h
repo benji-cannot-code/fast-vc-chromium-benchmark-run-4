94ac98099d64e5f325998cf5bf369b3e9b2cca7f
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2018-04-13 03:43:53 +0000

    desktop-pwas: Only add a scope for installable sites i.e. PWAs
    Also changes Installable to an enum class to avoid accidental
    implicit casts to bools.
    
    Bug: 801940
    Change-Id: I186d878b338375e339b3b5eb24fa5f2abd6c975a
    Reviewed-on: https://chromium-review.googlesource.com/981835
    Reviewed-by: Matt Giuca <mgiuca@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550502}

--- chrome/browser/extensions/favicon_downloader.h
+++ chrome/browser/extensions/favicon_downloader.h
@@ -23,7 +23,7 @@ struct FaviconURL;
 }
 
 namespace extensions {
-FORWARD_DECLARE_TEST(BookmarkAppHelperExtensionServiceTest,
+FORWARD_DECLARE_TEST(BookmarkAppHelperExtensionServiceInstallableSiteTest,
                      CreateBookmarkAppWithManifestIcons);
 }
 
@@ -56,8 +56,9 @@ class FaviconDownloader : public content::WebContentsObserver {
 
  private:
   friend class TestFaviconDownloader;
-  FRIEND_TEST_ALL_PREFIXES(extensions::BookmarkAppHelperExtensionServiceTest,
-                           CreateBookmarkAppWithManifestIcons);
+  FRIEND_TEST_ALL_PREFIXES(
+      extensions::BookmarkAppHelperExtensionServiceInstallableSiteTest,
+      CreateBookmarkAppWithManifestIcons);
 
   // Initiates a download of the image at |url| and returns the download id.
   // This is overridden in testing.
