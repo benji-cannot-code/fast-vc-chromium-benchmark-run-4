4c586fd75f109f3d1f2727398faef7ae84fa2c2f
Mustaq Ahmed <mustaq@google.com>
2018-04-13 18:02:47 +0000

    Make PointerEvent fromElement/toElement return null.
    Also added web-platform-tests.
    
    Blink intent thread:
    https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/G2s1Ol3qcgA
    
    Closes https://github.com/w3c/pointerevents/issues/167
    
    Bug: 827271
    Change-Id: I8429b9541f8cfc38223b3a34116b74c2aba92ee5
    Reviewed-on: https://chromium-review.googlesource.com/986877
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Mustaq Ahmed <mustaq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550683}

--- third_party/WebKit/LayoutTests/external/wpt/pointerevents/pointerevent_support.js
+++ third_party/WebKit/LayoutTests/external/wpt/pointerevents/pointerevent_support.js
@@ -40,7 +40,8 @@ function check_PointerEvent(event, testNamePrefix) {
         "long": function (v) { return typeof v === "number" && Math.round(v) === v; },
         "float": function (v) { return typeof v === "number"; },
         "string": function (v) { return typeof v === "string"; },
-        "boolean": function (v) { return typeof v === "boolean" }
+        "boolean": function (v) { return typeof v === "boolean" },
+        "object": function (v) { return typeof v === "object" }
     };
     [
         ["readonly", "long", "pointerId"],
@@ -51,7 +52,9 @@ function check_PointerEvent(event, testNamePrefix) {
         ["readonly", "long", "tiltY"],
         ["readonly", "string", "pointerType"],
         ["readonly", "boolean", "isPrimary"],
-        ["readonly", "long", "detail", 0]
+        ["readonly", "long", "detail", 0],
+        ["readonly", "object", "fromElement", null],
+        ["readonly", "object", "toElement", null]
     ].forEach(function (attr) {
         var readonly = attr[0];
         var type = attr[1];
@@ -76,7 +79,7 @@ function check_PointerEvent(event, testNamePrefix) {
         }, pointerTestName + "." + name + " IDL type " + type + " (JS type was " + typeof event[name] + ")");
 
         // value check if defined
-        if (value != undefined) {
+        if (value !== undefined) {
             test(function () {
                 assert_equals(event[name], value, name + " attribute value");
             }, pointerTestName + "." + name + " value is " + value + ".");
