573521af8d5ec791e566d8d939d9e7f36c1969e3
Bailey Berro <baileyberro@chromium.org>
2018-04-13 16:24:34 +0000

    Make SMB file_system_id compound
    - Creates a helper class that manages the creation and parsing of
    a compound file_system_id.
    - Switches to these functions.
    
    Bug: chromium:757625
    Change-Id: I611cfdf213da104f1dc63ce7ba442298bd362f41
    Reviewed-on: https://chromium-review.googlesource.com/990580
    Commit-Queue: Bailey Berro <baileyberro@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550635}

--- chrome/browser/chromeos/smb_client/smb_file_system.cc
+++ chrome/browser/chromeos/smb_client/smb_file_system.cc
@@ -10,8 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/posix/eintr_wrapper.h"
-#include "base/strings/string_number_conversions.h"
 #include "chrome/browser/chromeos/file_system_provider/service.h"
+#include "chrome/browser/chromeos/smb_client/smb_file_system_id.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/dbus/smb_provider_client.h"
 #include "components/services/filesystem/public/interfaces/types.mojom.h"
@@ -143,11 +143,7 @@ base::File::Error SmbFileSystem::TranslateError(smbprovider::ErrorType error) {
 }
 
 int32_t SmbFileSystem::GetMountId() const {
-  int32_t mount_id;
-  bool result =
-      base::StringToInt(file_system_info_.file_system_id(), &mount_id);
-  DCHECK(result);
-  return mount_id;
+  return GetMountIdFromFileSystemId(file_system_info_.file_system_id());
 }
 
 SmbProviderClient* SmbFileSystem::GetSmbProviderClient() const {
