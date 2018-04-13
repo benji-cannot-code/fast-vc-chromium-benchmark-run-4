9bc92922f4e8fc9a337d439485f87896eae3dc5a
xlai <xlai@chromium.org>
2018-04-13 20:11:13 +0000

    Add image release callback to Webgl commit() in OffscreenCanvas
    DrawingBuffer::TransferToStaticBitmapImage() is the common function shared by
    commit() and transferToImageBitmap() in OffscreenCanvas WebGL, as well as the
    VR display frame submission. The two OffscreenCanvas APIs miss a release
    callback; as a result, release_callback populated by image extraction function
    PrepareTransferableResourceInternal is invoked very early with a "lost
    resource" signal. On the other hand, VR display frame submission has correctly
    implemented a release callback that is releasing previous frame whenever there
    is a new frame coming in.
    
    This is a very likely reason to contribute to the flakiness of these two
    OffscreenCanvas APIs, while the VR display is working well.
    
    This CL adds a release callback to OffscreenCanvas commit(). I will follow-up
    with another CL that adds release callback to OffscreenCanvas
    transferToImageBitmap() and another CL that enables the relevant gpu
    pixel tests.
    
    
    
    Bug: 809227, 735228
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ia6e2c613de416f1e6e2a0f1c833d9e82e47fed4f
    Reviewed-on: https://chromium-review.googlesource.com/1010466
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Olivia Lai <xlai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550740}

--- third_party/blink/renderer/platform/graphics/gpu/drawing_buffer.cc
+++ third_party/blink/renderer/platform/graphics/gpu/drawing_buffer.cc
@@ -191,6 +191,7 @@ DrawingBuffer::DrawingBuffer(
 
 DrawingBuffer::~DrawingBuffer() {
   DCHECK(destruction_in_progress_);
+  SwapPreviousFrameCallback(nullptr);
   layer_.reset();
   context_provider_.reset();
 }
@@ -1593,6 +1594,15 @@ DrawingBuffer::ScopedStateRestorer::~ScopedStateRestorer() {
     client->DrawingBufferClientRestorePixelUnpackBufferBinding();
   if (pixel_pack_buffer_binding_dirty_)
     client->DrawingBufferClientRestorePixelPackBufferBinding();
+}
+
+void DrawingBuffer::SwapPreviousFrameCallback(
+    std::unique_ptr<viz::SingleReleaseCallback> release_callback) {
+  if (previous_image_release_callback_) {
+    previous_image_release_callback_->Run(gpu::SyncToken(), false);
+  }
+
+  previous_image_release_callback_ = std::move(release_callback);
 }
 
 bool DrawingBuffer::ShouldUseChromiumImage() {
