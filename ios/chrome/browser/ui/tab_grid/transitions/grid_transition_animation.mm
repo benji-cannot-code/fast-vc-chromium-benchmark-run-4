aeca1558073fa0c679ee358284ecc72c6ac03d5b
Mark Cogan <marq@google.com>
2018-04-13 16:51:00 +0000

    [iOS] Tweak selection animation in tab grid transition
    This CL fixes a bug where when zooming out from the tab grid, the
    selection state change wasn't animated. It also makes the slection state
    change animations longer.
    
    Bug: 804536
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Ic3dba63a1c83bb967e66e110de299ce7cb898006
    Reviewed-on: https://chromium-review.googlesource.com/1012073
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550650}

--- ios/chrome/browser/ui/tab_grid/transitions/grid_transition_animation.mm
+++ ios/chrome/browser/ui/tab_grid/transitions/grid_transition_animation.mm
@@ -111,12 +111,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // If there's only one cell, the animation has two parts:
   //   (A) Fading in the selected cell highlight indicator.
   //   (B) Zooming the selected cell into position.
-  // These parts are timed like this:
+  // These parts are timed over |duration| like this:
   //
-  //                                 |*|----[A]----{100%}
+  //           |#|----------[A]--------------------{100%}
   //  {0%}------------------[B]--------------------{100%}
   //
-  //  (|*| is <staggeredDuration>%).
+  //  (|#| is |duration| - |staggeredDuration|).
   // Animation B will call the completion handler in this case.
 
   // If there's more than once cell, the animation has three parts:
@@ -125,11 +125,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   //   (C) Zooming the unselected cells into position.
   // The timing is as follows:
   //
-  //                                 |*|----[A]----{100%}
+  //           |#|----------[A]--------------------{100%}
   //  {0%}------------------[B]------|*|
   //           |#|----------[C]--------------------{100%}
-  //  (|*| is <staggeredDuration>%).
-  //  (|#| is 1-<staggeredDuration>%).
+  //  (|*| is |staggeredDuration|).
+  //  (|#| is |duration| - |staggeredDuration|).
   // Animation C will call the completion handler in this case.
 
   // TODO(crbug.com/820410): Tune the timing, relative pacing, and curves of
@@ -137,10 +137,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   UICollectionViewCell* selectedCell = self.layout.selectedItem.cell;
 
-  // Run animation (A) starting at |staggeredDuration|.
-  [UIView animateWithDuration:duration - staggeredDuration
-                        delay:staggeredDuration
-                      options:UIViewAnimationOptionCurveEaseIn
+  // Run animation (A) starting at |1 - staggeredDuration|.
+  [UIView animateWithDuration:staggeredDuration
+                        delay:duration - staggeredDuration
+                      options:UIViewAnimationOptionCurveEaseOut
                    animations:^{
                      selectedCell.selected = YES;
                    }
@@ -194,12 +194,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // If there's only one cell, the animation has two parts:
   //   (A) Fading out the selected cell highlight indicator.
   //   (B) Zooming the selected cell out into position.
-  // These parts are timed like this:
+  // These parts are timed over |duration| like this:
   //
-  //  {0%}----[A]----|#|
+  //  {0%}-----------[A]-------------|*|
   //  {0%}------------------[B]--------------------{100%}
   //
-  //  (|#| is 1-<staggeredDuration>%).
+  //  (|#| is |duration| - |staggeredDuration|).
   // Animation B will call the completion handler in this case.
 
   // If there's more than once cell, the animation has three parts:
@@ -208,11 +208,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   //   (C) Zooming the unselected cells into position.
   // The timing is as follows:
   //
-  //  {0%}----[A]----|#|
+  //  {0%}-----------[A]-------------|*|
   //  {0%}---------- [C]-------------|*|
   //                 |#|-------------[B]-----------{100%}
-  //  (|*| is <staggeredDuration>%).
-  //  (|#| is 1-<staggeredDuration>%).
+  //  (|*| is |staggeredDuration|)
+  //  (|#| is |duration| - |staggeredDuration|).
   // Animation C will call the completion handler in this case.
 
   // TODO(crbug.com/820410): Tune the timing, relative pacing, and curves of
@@ -220,9 +220,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   UICollectionViewCell* selectedCell = self.layout.selectedItem.cell;
 
-  // Run animation (A) for |duration - staggeredDuration|.
-  [UIView animateWithDuration:0
-                        delay:duration - staggeredDuration
+  // Run animation (A) for |staggeredDuration|.
+  [UIView animateWithDuration:staggeredDuration
+                        delay:0
                       options:UIViewAnimationOptionCurveEaseOut
                    animations:^{
                      selectedCell.selected = NO;
