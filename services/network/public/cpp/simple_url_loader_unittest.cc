d7f81a2515ebe52fce30bd9c1edf063bf853c961
Matt Menke <mmenke@chromium.org>
2018-04-13 18:24:18 +0000

    Disable SimpleURLLoader exceed limits tests.
    I believe there's a bug in URLLoader that's causing them to occasionally
    crash, when the data pipe is closed before reading the entire response
    body.
    
    TBR=jam@chromium.org
    NOTRY=true
    
    Bug: 755309
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: Id3f98fdea01b56b1c0c3b0edf927701579afa38d
    Reviewed-on: https://chromium-review.googlesource.com/1012443
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550688}

--- services/network/public/cpp/simple_url_loader_unittest.cc
+++ services/network/public/cpp/simple_url_loader_unittest.cc
@@ -954,7 +954,8 @@ TEST_P(SimpleURLLoaderTest, ResponseBodyWithSizeBelowLimit) {
   EXPECT_EQ(std::string(kResponseSize, 'a'), *test_helper->response_body());
 }
 
-TEST_P(SimpleURLLoaderTest, ResponseBodyWithSizeAboveLimit) {
+// Disabled: See https://crbug.com/832600
+TEST_P(SimpleURLLoaderTest, DISABLED_ResponseBodyWithSizeAboveLimit) {
   // Download to stream doesn't support response sizes.
   if (GetParam() == SimpleLoaderTestHelper::DownloadType::AS_STREAM)
     return;
@@ -972,7 +973,9 @@ TEST_P(SimpleURLLoaderTest, ResponseBodyWithSizeAboveLimit) {
 }
 
 // Same as above, but with setting allow_partial_results to true.
-TEST_P(SimpleURLLoaderTest, ResponseBodyWithSizeAboveLimitPartialResponse) {
+// Disabled: See https://crbug.com/832600
+TEST_P(SimpleURLLoaderTest,
+       DISABLED_ResponseBodyWithSizeAboveLimitPartialResponse) {
   // Download to stream doesn't support response sizes.
   if (GetParam() == SimpleLoaderTestHelper::DownloadType::AS_STREAM)
     return;
@@ -1032,7 +1035,8 @@ TEST_P(SimpleURLLoaderTest, BigResponseBodyWithSizeBelowLimit) {
   EXPECT_EQ(std::string(kResponseSize, 'a'), *test_helper->response_body());
 }
 
-TEST_P(SimpleURLLoaderTest, BigResponseBodyWithSizeAboveLimit) {
+// Disabled: See https://crbug.com/832600
+TEST_P(SimpleURLLoaderTest, DISABLED_BigResponseBodyWithSizeAboveLimit) {
   // Download to stream doesn't support response sizes.
   if (GetParam() == SimpleLoaderTestHelper::DownloadType::AS_STREAM)
     return;
@@ -1049,7 +1053,9 @@ TEST_P(SimpleURLLoaderTest, BigResponseBodyWithSizeAboveLimit) {
   EXPECT_FALSE(test_helper->response_body());
 }
 
-TEST_P(SimpleURLLoaderTest, BigResponseBodyWithSizeAboveLimitPartialResponse) {
+// Disabled: See https://crbug.com/832600
+TEST_P(SimpleURLLoaderTest,
+       DISABLED_BigResponseBodyWithSizeAboveLimitPartialResponse) {
   // Download to stream doesn't support response sizes.
   if (GetParam() == SimpleLoaderTestHelper::DownloadType::AS_STREAM)
     return;
