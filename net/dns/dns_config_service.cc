b8fb1b60798ddf42d0ced7902aea7daf81c8262b
Rob Percival <robpercival@chromium.org>
2018-04-13 10:04:12 +0000

    Remove edns0 from DnsConfig
    This does not appear to be used by anything. When it was added in 2011,
    there was some musing about using it but it seems it never was
    (https://codereview.chromium.org/7518028/diff/12006/net/dns/dns_config_service_posix.cc#newcode52).
    
    Bug: 831665
    Change-Id: I1181594cb9f84ba406228075834c4fbfed42a21a
    Reviewed-on: https://chromium-review.googlesource.com/1007664
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Rob Percival <robpercival@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550571}

--- net/dns/dns_config_service.cc
+++ net/dns/dns_config_service.cc
@@ -25,7 +25,6 @@ DnsConfig::DnsConfig()
       timeout(base::TimeDelta::FromMilliseconds(kDnsDefaultTimeoutMs)),
       attempts(2),
       rotate(false),
-      edns0(false),
       use_local_ipv6(false) {}
 
 DnsConfig::DnsConfig(const DnsConfig& other) = default;
@@ -45,7 +44,6 @@ bool DnsConfig::EqualsIgnoreHosts(const DnsConfig& d) const {
          (timeout == d.timeout) &&
          (attempts == d.attempts) &&
          (rotate == d.rotate) &&
-         (edns0 == d.edns0) &&
          (use_local_ipv6 == d.use_local_ipv6);
 }
 
@@ -58,7 +56,6 @@ void DnsConfig::CopyIgnoreHosts(const DnsConfig& d) {
   timeout = d.timeout;
   attempts = d.attempts;
   rotate = d.rotate;
-  edns0 = d.edns0;
   use_local_ipv6 = d.use_local_ipv6;
 }
 
@@ -81,7 +78,6 @@ std::unique_ptr<base::Value> DnsConfig::ToValue() const {
   dict->SetDouble("timeout", timeout.InSecondsF());
   dict->SetInteger("attempts", attempts);
   dict->SetBoolean("rotate", rotate);
-  dict->SetBoolean("edns0", edns0);
   dict->SetBoolean("use_local_ipv6", use_local_ipv6);
   dict->SetInteger("num_hosts", hosts.size());
   list = std::make_unique<base::ListValue>();
