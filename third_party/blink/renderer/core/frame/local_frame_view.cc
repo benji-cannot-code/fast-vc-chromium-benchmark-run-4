60f1755c4a8ad4c08aaf7a3bd0bc351e2b7862c5
Chris Harrelson <chrishtr@chromium.org>
2018-04-13 04:32:42 +0000

    [IOv2] Add an UMA and UKM for IntersectionObserver observation time.
    Bug:831762,783383
    
    Change-Id: I3aea3afb2669f83f0d40674218c82a4f399b4e6f
    Reviewed-on: https://chromium-review.googlesource.com/1008270
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550518}

--- third_party/blink/renderer/core/frame/local_frame_view.cc
+++ third_party/blink/renderer/core/frame/local_frame_view.cc
@@ -159,6 +159,7 @@ constexpr int kLetterPortraitPageHeight = 792;
 // EnsureUkmTimeAggregator().
 enum class UkmMetricNames {
   kCompositing,
+  kIntersectionObservation,
   kPaint,
   kPrePaint,
   kStyleAndLayout,
@@ -167,6 +168,18 @@ enum class UkmMetricNames {
 
 }  // namespace
 
+// Defines an UMA and a UKM, recorded in microseconds equal to the duration of
+// the current lexical scope after declaration of the macro. Example usage:
+//
+// void LocalFrameView::DoExpensiveThing() {
+//   SCOPED_UMA_AND_UKM_TIMER(UmaName, kUkmEnumName);
+//   // Do computation of expensive thing
+//
+// }
+//
+// |uma_name| should be the full name of an UMA defined
+// in histograms.xml. |ukm_enum| should be an entry in UkmMetricNames
+// (which in turn come from ukm.xml).
 #define SCOPED_UMA_AND_UKM_TIMER(uma_name, ukm_enum)                    \
   DEFINE_STATIC_LOCAL_IMPL(CustomCountHistogram, scoped_uma_counter,    \
                            (uma_name, 0, 10000000, 50), false);         \
@@ -3317,7 +3330,12 @@ bool LocalFrameView::UpdateLifecyclePhasesInternal(
     });
   }
 
-  UpdateViewportIntersectionsForSubtree(target_state);
+  {
+    SCOPED_UMA_AND_UKM_TIMER("Blink.IntersectionObservation.UpdateTime",
+                             UkmMetricNames::kIntersectionObservation);
+    UpdateViewportIntersectionsForSubtree(target_state);
+  }
+
   return Lifecycle().GetState() == target_state;
 }
 
@@ -5882,7 +5900,8 @@ UkmTimeAggregator& LocalFrameView::EnsureUkmTimeAggregator() {
         frame_->GetDocument()->UkmRecorder(),
         // Note that changing the order or values of the following vector
         // requires changing the UkmMetricNames enum.
-        {"Paint", "PrePaint", "Compositing", "StyleAndLayout"},
+        {"Compositing", "IntersectionObservation", "Paint", "PrePaint",
+         "StyleAndLayout"},
         TimeDelta::FromSeconds(1)));
   }
   return *ukm_time_aggregator_;
