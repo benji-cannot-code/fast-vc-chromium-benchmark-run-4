75531c5a03f4d9dc76d662b8bedff01826780c6a
Daniel Murphy <dmurph@chromium.org>
2018-04-13 01:09:40 +0000

    [SessionStorage] Metadata parsing and writing for mojo version.
    Bug: 716490
    Change-Id: If2b0672af149abf44dd475a2196e7b727cb450a7
    Reviewed-on: https://chromium-review.googlesource.com/986880
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550454}

--- components/services/leveldb/public/cpp/util.cc
+++ components/services/leveldb/public/cpp/util.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/services/leveldb/public/cpp/util.h"
 
+#include "base/containers/span.h"
 #include "third_party/leveldatabase/env_chromium.h"
 #include "third_party/leveldatabase/src/include/leveldb/status.h"
 
@@ -89,6 +90,16 @@ std::string Uint8VectorToStdString(const std::vector<uint8_t>& input) {
 std::vector<uint8_t> StdStringToUint8Vector(const std::string& input) {
   const uint8_t* data = reinterpret_cast<const uint8_t*>(input.data());
   return std::vector<uint8_t>(data, data + input.size());
+}
+
+base::StringPiece Uint8VectorToStringPiece(const std::vector<uint8_t>& input) {
+  return base::StringPiece(reinterpret_cast<const char*>(input.data()),
+                           input.size());
+}
+
+std::vector<uint8_t> StringPieceToUint8Vector(base::StringPiece input) {
+  base::span<const uint8_t> data = base::as_bytes(base::make_span(input));
+  return std::vector<uint8_t>(data.begin(), data.end());
 }
 
 }  // namespace leveldb
