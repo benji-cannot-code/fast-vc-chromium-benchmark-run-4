fd3152f51f04e0a9b57bea5a4dc66198485fefe1
Tatiana Gornak <melandory@chromium.org>
2018-04-13 15:27:32 +0000

    Implements the topic registration request to Per-User-Topic FCM server
    This CL implements the wrapper for the POST request to the FCM server.
    
    BUG=801985
    
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Ibba578f7181920e24b39c633111d47b717eb7bb1
    Reviewed-on: https://chromium-review.googlesource.com/911108
    Commit-Queue: Tatiana Gornak <melandory@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550625}

---
+++ components/invalidation/impl/status.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/invalidation/impl/status.h"
+
+namespace syncer {
+
+Status::Status(StatusCode status_code, const std::string& message)
+    : code(status_code), message(message) {}
+
+Status Status::Success() {
+  return Status(StatusCode::SUCCESS, std::string());
+}
+
+Status::~Status() = default;
+
+}  // namespace syncer
