403ff9fc466b63e38519b523febe777365ee1d77
Malay Keshav <malaykeshav@chromium.org>
2018-04-13 07:44:22 +0000

    Differentiate between duplicate touch devices based on the port
    This patch performs multiple tasks:
     - Add EVIOCGPHYS field to EventDeviceInfo and plumbs it to InputDevice.
     - Adds a secondary identifier to TouchDeviceIdentifier that corresponds
       to the phys(or port) information of the device.
     - Add support for TouchDeviceManager to manage devices with same ID
       by using their port information.
     - Persistently stores the port mapping for each device in case of
       duplicate ID.
    
    Adds and updates unit tests.
    
    Bug: 827452
    Change-Id: Ia224cd35125615705d1b94a13c4c0afa2bd25182
    Component: TouchDeviceManager, EventDeviceInfo, InputDevice
    Reviewed-on: https://chromium-review.googlesource.com/1000548
    Commit-Queue: Malay Keshav <malaykeshav@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Michael Spang <spang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550538}

--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -223,7 +223,6 @@ extern const char kLabsMediaplayerEnabled[];
 extern const char kShow3gPromoNotification[];
 extern const char kDataSaverPromptsShown[];
 extern const char kChromeOSReleaseNotesVersion[];
-
 extern const char kNoteTakingAppId[];
 extern const char kNoteTakingAppEnabledOnLockScreen[];
 extern const char kNoteTakingAppsLockScreenWhitelist[];
