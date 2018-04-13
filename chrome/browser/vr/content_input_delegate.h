843a7b4733c697bad744eb0d946161da434f30eb
Yash Malik <ymalik@google.com>
2018-04-13 23:26:13 +0000

    VR: Fix autofill related keyboard bugs
    Autofill exposed two keyboard input handling related bugs.
    1) We weren't clearing the model capturing the web input field state after the
    field lost focus. As a result, upon gaining focus the second time, we were
    pushing the old state into the keyboard.
    2) When the web input field indices change, we ask web contents for the new
    text around the indices. This has a side effect of "OnWebInputIndicesChanged"
    beinh called again. We were ignoring this second call in this case.
    The bug was that we weren't clearing the bit that told us to ingore, so we would
    fail to request the text state when we actually should have.
    
    This CL also has a functional change that clicking on the autofill suggestion
    doesn't hide the keyboard.
    
    Bug: 831187
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_vr
    Change-Id: I04e448addf8e30a56afe98cb32630195f3065f67
    Reviewed-on: https://chromium-review.googlesource.com/1012989
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Reviewed-by: Amirhossein Simjour <asimjour@chromium.org>
    Commit-Queue: Yash Malik <ymalik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550811}

--- chrome/browser/vr/content_input_delegate.h
+++ chrome/browser/vr/content_input_delegate.h
@@ -107,6 +107,8 @@ class ContentInputDelegate {
     OnWebInputTextChanged(text);
   }
 
+  void ClearTextInputState();
+
  private:
   void UpdateGesture(const gfx::PointF& normalized_content_hit_point,
                      blink::WebGestureEvent& gesture);
