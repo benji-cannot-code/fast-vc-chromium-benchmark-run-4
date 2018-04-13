b8c7232c5806d75363a63eefc3dac74cdacc6f8b
Kent Tamura <tkent@chromium.org>
2018-04-13 04:22:25 +0000

    custom-elements: Implement customElements.upgrade().
    Intent-to-implement-and-ship: https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/zCQe7UkR07w
    
    Bug: 819482
    Change-Id: I5645d5711d7fe1323992533e6c73593ba59d1bca
    Reviewed-on: https://chromium-review.googlesource.com/1009450
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550513}

--- third_party/blink/renderer/core/html/custom/custom_element_registry.cc
+++ third_party/blink/renderer/core/html/custom/custom_element_registry.cc
@@ -12,7 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/element.h"
 #include "third_party/blink/renderer/core/dom/element_definition_options.h"
+#include "third_party/blink/renderer/core/dom/element_traversal.h"
 #include "third_party/blink/renderer/core/dom/exception_code.h"
+#include "third_party/blink/renderer/core/dom/shadow_root.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/core/html/custom/ce_reactions_scope.h"
 #include "third_party/blink/renderer/core/html/custom/custom_element.h"
@@ -30,6 +32,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <limits>
 
 namespace blink {
+
+namespace {
+
+void CollectUpgradeCandidateInNode(Node& root,
+                                   HeapVector<Member<Element>>& candidates) {
+  if (root.IsElementNode()) {
+    Element& root_element = ToElement(root);
+    if (root_element.GetCustomElementState() == CustomElementState::kUndefined)
+      candidates.push_back(root_element);
+    if (auto* shadow_root = root_element.GetShadowRoot()) {
+      if (shadow_root->GetType() != ShadowRootType::kUserAgent)
+        CollectUpgradeCandidateInNode(*shadow_root, candidates);
+    }
+  }
+  for (auto& element : Traversal<HTMLElement>::ChildrenOf(root))
+    CollectUpgradeCandidateInNode(element, candidates);
+}
+
+}  // anonymous namespace
 
 // Returns true if |name| is invalid.
 static bool ThrowIfInvalidName(const AtomicString& name,
@@ -336,6 +357,21 @@ void CustomElementRegistry::CollectCandidates(
     return;
 
   sorter.Sorted(elements, document);
+}
+
+// https://html.spec.whatwg.org/multipage/custom-elements.html#dom-customelementregistry-upgrade
+void CustomElementRegistry::upgrade(Node* root) {
+  DCHECK(root);
+
+  // 1. Let candidates be a list of all of root's shadow-including
+  // inclusive descendant elements, in tree order.
+  HeapVector<Member<Element>> candidates;
+  CollectUpgradeCandidateInNode(*root, candidates);
+
+  // 2. For each candidate of candidates, try to upgrade candidate.
+  for (auto& candidate : candidates) {
+    CustomElement::TryToUpgrade(candidate);
+  }
 }
 
 }  // namespace blink
--- third_party/blink/renderer/core/html/custom/custom_element_registry.h
+++ third_party/blink/renderer/core/html/custom/custom_element_registry.h
@@ -65,6 +65,7 @@ class CORE_EXPORT CustomElementRegistry final : public ScriptWrappable {
   ScriptPromise whenDefined(ScriptState*,
                             const AtomicString& name,
                             ExceptionState&);
+  void upgrade(Node* root);
 
   void Entangle(V0CustomElementRegistrationContext*);
 
