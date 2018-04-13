01366df1c5adb8bdcbf5ea2c4f569190cda50505
Owen Min <zmin@chromium.org>
2018-04-13 18:11:04 +0000

    Fix an issue when fetching policy without local cache of token policy.
    Because the first policy cache will be loaded synchronously before local_state
    creation. We'll miss its OnStoreLoaded/Error event as CloudPolicyService
    has to be created after |local_state|. It means we won't know the result of
    cache loading.
    
    To resolve this, check the initialization status once the CloudPolicyService
    has been created. Fetch policy from server if cache is not loaded properly.
    
    Bug: 832694
    Change-Id: Idd0b9e35ea2afded586b73c765197b5574ba3900
    Reviewed-on: https://chromium-review.googlesource.com/1011211
    Commit-Queue: Owen Min <zmin@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550684}

--- chrome/browser/policy/cloud/machine_level_user_cloud_policy_helper.h
+++ chrome/browser/policy/cloud/machine_level_user_cloud_policy_helper.h
@@ -78,6 +78,8 @@ class MachineLevelUserCloudPolicyFetcher : public CloudPolicyService::Observer {
 
  private:
   void InitializeManager(std::unique_ptr<CloudPolicyClient> client);
+  // Fetch policy if device is enrolled.
+  void TryToFetchPolicy();
 
   MachineLevelUserCloudPolicyManager* policy_manager_;
   PrefService* local_state_;
