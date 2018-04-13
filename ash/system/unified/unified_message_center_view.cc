5f05226153d3c79f7a7e737c96b11742aabefc50
Tetsui Ohkubo <tetsui@chromium.org>
2018-04-13 02:02:19 +0000

    Shrink UnifiedSystemTray when notification removed
    Different from the current notification center, UnifiedSystemTray will
    shrink its area when notifications are removed.
    
    UX spec: http://shortn/_U7QSE9nhSk
    
    TEST=manual
    BUG=828752
    
    Change-Id: I5c793d39f88aa81fb7a2fa90366e62fc5dc9af2d
    Reviewed-on: https://chromium-review.googlesource.com/1011522
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550476}

--- ash/system/unified/unified_message_center_view.cc
+++ ash/system/unified/unified_message_center_view.cc
@@ -47,6 +47,7 @@ UnifiedMessageCenterView::UnifiedMessageCenterView(
   scroller_->SetHorizontalScrollBar(new views::OverlayScrollBar(true));
   AddChildView(scroller_);
 
+  message_list_view_->set_use_fixed_height(false);
   message_list_view_->set_scroller(scroller_);
   scroller_->SetContents(message_list_view_);
 
