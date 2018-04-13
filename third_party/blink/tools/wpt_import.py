38d68ed42819f4a58ea0af52fca06aaaadb2cd5b
Kent Tamura <tkent@chromium.org>
2018-04-13 00:42:12 +0000

    Copy //third_party/WebKit/Tools/Script/wpt-* to //third_party/blink/tools/
    This CL is a preparation to change references to them in build bots.
    This CL has no behavior changes.
    
    Bug: 829697
    Change-Id: I3d69dbae6520e3b2c6ca95a7b3c05a7322eeaa0a
    Reviewed-on: https://chromium-review.googlesource.com/1009543
    Reviewed-by: Quinten Yearsley <qyearsley@chromium.org>
    Reviewed-by: Robert Ma <robertma@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550439}

---
+++ third_party/blink/tools/wpt_import.py
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/env vpython
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Pulls the latest revisions of the web-platform-tests."""
+
+import os
+import sys
+
+# Without abspath(), PathFinder can't find chromium_base correctly.
+sys.path.append(os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..', '..',
+                 'WebKit', 'Tools', 'Scripts')))
+from webkitpy.common import exit_codes
+from webkitpy.common.host import Host
+from webkitpy.w3c.test_importer import TestImporter
+
+
+def main():
+    host = Host()
+    importer = TestImporter(host)
+    try:
+        host.exit(importer.main())
+    except KeyboardInterrupt:
+        host.print_("Interrupted, exiting")
+        host.exit(exit_codes.INTERRUPTED_EXIT_STATUS)
+
+
+if __name__ == '__main__':
+    main()
