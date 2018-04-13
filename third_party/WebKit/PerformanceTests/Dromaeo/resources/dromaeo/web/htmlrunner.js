6cc18d00090fd03208c1cdd8a01ec733aa0f7089
Kent Tamura <tkent@chromium.org>
2018-04-13 11:44:02 +0000

    Remove PerformanceTests/Dromaeo
    "run_benchmark dromaeo" doesn't use this directory.
    
    Bug: 829695
    Change-Id: I4389a8cf6845c9cb02ac2fc5da0866acfbf6ea52
    Reviewed-on: https://chromium-review.googlesource.com/1009343
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550593}

--- third_party/WebKit/PerformanceTests/Dromaeo/resources/dromaeo/web/htmlrunner.js
+++
@@ -1,5 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-var startTest = top.startTest || function(){};
-var test = top.test || function(name, fn){ fn(); };
-var endTest = top.endTest || function(){};
-var prep = top.prep || function(fn){ fn(); };
