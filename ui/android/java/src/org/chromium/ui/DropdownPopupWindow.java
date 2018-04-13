c3de0e2decff950c2b7a86d416b27943a6aba245
Amirhossein Simjour <asimjour@chromium.org>
2018-04-13 23:02:22 +0000

    Update the orientation of the Dropdown when the root is changed
    Update the orientation of the dropdown when the root view is
    updated.
    
    Bug: 823262, 828759
    Change-Id: Iaa5f8bff123afaf0674e93f67f223f9a3156f840
    Reviewed-on: https://chromium-review.googlesource.com/1012471
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Amirhossein Simjour <asimjour@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550770}

--- ui/android/java/src/org/chromium/ui/DropdownPopupWindow.java
+++ ui/android/java/src/org/chromium/ui/DropdownPopupWindow.java
@@ -80,6 +80,7 @@ public class DropdownPopupWindow implements AnchoredPopupWindow.LayoutObserver {
         mHorizontalPadding = paddingRect.right + paddingRect.left;
         mAnchoredPopupWindow.setPreferredHorizontalOrientation(
                 AnchoredPopupWindow.HORIZONTAL_ORIENTATION_CENTER);
+        mAnchoredPopupWindow.setUpdateOrientationOnChange(true);
     }
 
     /**
