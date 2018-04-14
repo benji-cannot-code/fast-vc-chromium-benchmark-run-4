2de1de3bc58a5c97673475fa1739e6e8e3a95447
Stephen McGruer <smcgruer@chromium.org>
2018-04-14 15:23:06 +0000

    Reland "Web Animations: Fix bugs in procedure to process a keyframes argument"
    This is a reland of 0ade0386aa4168b48234bc7f33d30a62140b95ea
    
    Original change's description:
    > Web Animations: Fix bugs in procedure to process a keyframes argument
    > 
    > There were three minor bugs left in the implementation:
    > 
    >   - We threw on lists-in-custom-iterators instead of just ignoring them.
    >   - We returned all properties on the keyframe rather than just those
    >     defined on the keyframe itself (e.g. we would include prototype
    >     properties, against spec).
    >   - We didn't access the properties in ascending unicode order.
    > 
    > Bug: 827573
    > Change-Id: I213ae5b24e1f35d7f28d16625025122950a6ba88
    > Reviewed-on: https://chromium-review.googlesource.com/989261
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    > Reviewed-by: Robert Flack <flackr@chromium.org>
    > Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#550641}
    
    Bug: 827573
    Change-Id: I6c49fa6ca36db16ecddfb520e0964bd231565a0b
    Reviewed-on: https://chromium-review.googlesource.com/1012897
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550895}

--- third_party/blink/renderer/bindings/core/v8/dictionary.h
+++ third_party/blink/renderer/bindings/core/v8/dictionary.h
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_BINDINGS_CORE_V8_DICTIONARY_H_
 #define THIRD_PARTY_BLINK_RENDERER_BINDINGS_CORE_V8_DICTIONARY_H_
 
-#include "third_party/blink/renderer/bindings/core/v8/dictionary_iterator.h"
 #include "third_party/blink/renderer/bindings/core/v8/exception_state.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_binding_for_core.h"
 #include "third_party/blink/renderer/core/core_export.h"
@@ -37,8 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "v8/include/v8.h"
 
 namespace blink {
-
-class ExecutionContext;
 
 // Dictionary class provides ways to retrieve property values as C++ objects
 // from a V8 object. Instances of this class must not outlive V8's handle scope
@@ -74,7 +71,7 @@ class CORE_EXPORT Dictionary final {
   }
 
   bool Get(const StringView& key, v8::Local<v8::Value>& value) const {
-    return isolate_ && GetInternal(V8String(isolate_, key), value);
+    return isolate_ && Get(V8String(isolate_, key), value);
   }
   bool Get(const StringView& key,
            v8::Local<v8::Value>& value,
@@ -113,6 +110,8 @@ class CORE_EXPORT Dictionary final {
     return value;
   }
 
+  bool Get(v8::Local<v8::Value> key, v8::Local<v8::Value>& result) const;
+
   HashMap<String, String> GetOwnPropertiesAsStringHashMap(
       ExceptionState&) const;
   Vector<String> GetPropertyNames(ExceptionState&) const;
@@ -125,11 +124,7 @@ class CORE_EXPORT Dictionary final {
     return isolate_->GetCurrentContext();
   }
 
-  DictionaryIterator GetIterator(ExecutionContext*) const;
-
  private:
-  bool GetInternal(const v8::Local<v8::Value>& key,
-                   v8::Local<v8::Value>& result) const;
   bool GetInternal(const v8::Local<v8::Value>& key,
                    v8::Local<v8::Value>& result,
                    ExceptionState&) const;
