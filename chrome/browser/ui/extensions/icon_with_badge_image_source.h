127f8cda0f9ab6ccac8191457130c84bfaee5694
Ahmed Fakhry <afakhry@google.com>
2018-04-14 01:59:35 +0000

    Fix extensions badges and decorations positions
    The extensions browser actions badges and decorations should
    be positioned at the corners of the icon area, rather than the
    corners of the full button's area (i.e. ignoring the paddings).
    
    Screenshots: https://bugs.chromium.org/p/chromium/issues/detail?id=831946#c11
    
    BUG=831946
    
    Change-Id: I977883a3e6127e1f91779a7e73b4adb7fa7c6471
    Reviewed-on: https://chromium-review.googlesource.com/1013290
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550855}

--- chrome/browser/ui/extensions/icon_with_badge_image_source.h
+++ chrome/browser/ui/extensions/icon_with_badge_image_source.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "third_party/skia/include/core/SkColor.h"
+#include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/image/canvas_image_source.h"
 #include "ui/gfx/image/image.h"
 
@@ -70,6 +71,13 @@ class IconWithBadgeImageSource : public gfx::CanvasImageSource {
   // Paints a decoration over the base icon to indicate that the extension has
   // a blocked action that wants to run.
   void PaintBlockedActionDecoration(gfx::Canvas* canvas);
+
+  // The toolbar action view may have different values of paddings depending on
+  // the current material design mode (See ToolbarActionsBar::GetViewSize()). In
+  // all cases, our badges and decorations should be positions at the corners of
+  // the area where the icon exists (ignoring all the paddings).
+  // https://crbug.com/831946.
+  gfx::Rect GetIconAreaRect() const;
 
   // The base icon to draw.
   gfx::Image icon_;
