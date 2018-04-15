5ee9ba6dc762a9031d168acca28dede446ea4014
David Black <dmblack@google.com>
2018-04-15 22:00:14 +0000

    Send interaction start/finish events to ash.
    Bug: b:77866848
    Change-Id: I6462c7159233f26838fbef66328cd49af8f887af
    Reviewed-on: https://chromium-review.googlesource.com/1005565
    Commit-Queue: David Black <dmblack@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Muyuan Li <muyuanli@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550917}

--- chromeos/services/assistant/assistant_manager_service_impl.h
+++ chromeos/services/assistant/assistant_manager_service_impl.h
@@ -68,6 +68,10 @@ class AssistantManagerServiceImpl
   void OnSpeechLevelUpdated(float speech_level) override;
 
   // assistant_client::ConversationStateListener overrides:
+  void OnConversationTurnStarted() override;
+  void OnConversationTurnFinished(
+      assistant_client::ConversationStateListener::Resolution resolution)
+      override;
   void OnRecognitionStateChanged(
       assistant_client::ConversationStateListener::RecognitionState state,
       const assistant_client::ConversationStateListener::RecognitionResult&
@@ -81,6 +85,9 @@ class AssistantManagerServiceImpl
   void HandleGetSettingsResponse(GetSettingsUiResponseCallback callback,
                                  const std::string& settings);
 
+  void OnConversationTurnStartedOnMainThread();
+  void OnConversationTurnFinishedOnMainThread(
+      assistant_client::ConversationStateListener::Resolution resolution);
   void OnShowHtmlOnMainThread(const std::string& html);
   void OnShowSuggestionsOnMainThread(
       const std::vector<std::string>& suggestions);
