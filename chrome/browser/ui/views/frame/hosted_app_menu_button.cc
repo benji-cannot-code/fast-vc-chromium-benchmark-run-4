52517f8ae949f0a42d66465f2e4ad9f099f055d4
Alan Cutter <alancutter@chromium.org>
2018-04-16 06:00:25 +0000

    Remove inset from hosted app menu button
    This CL fixes undesirably large insets on the hosted app menu button
    that get set when command line flags
    "--top-chrome-md=material-touch-optimized --enable-features=SecondaryUiMd"
    are used on chromeOS causing the menu icon to get cropped.
    
    Before:
    https://bugs.chromium.org/p/chromium/issues/attachment?aid=334387&signed_aid=zaosvDPanfssI1aE7ljaww==&inline=1
    
    After:
    https://bugs.chromium.org/p/chromium/issues/attachment?aid=334388&signed_aid=sVoIaJthm75DLZgxqr06fA==&inline=1
    
    
    Bug: 831968
    Change-Id: I20bcc33aef41a0e0872170e7751502936c63ded4
    Reviewed-on: https://chromium-review.googlesource.com/1011922
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550938}

--- chrome/browser/ui/views/frame/hosted_app_menu_button.cc
+++ chrome/browser/ui/views/frame/hosted_app_menu_button.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/gfx/paint_vector_icon.h"
+#include "ui/views/border.h"
 #include "ui/views/controls/button/menu_button.h"
 
 constexpr int kMenuHighlightFadeDurationMs = 800;
@@ -26,6 +27,9 @@ HostedAppMenuButton::HostedAppMenuButton(BrowserView* browser_view)
   SetInkDropMode(InkDropMode::ON);
   // Disable focus ring for consistency with sibling buttons and AppMenuButton.
   SetFocusPainter(nullptr);
+  // We have no need for a border, native theme borders can cause our menu icon
+  // to get cropped, see https://crbug.com/831968.
+  SetBorder(nullptr);
   // This name is guaranteed not to change during the lifetime of this button.
   // Get the app name only, aka "Google Docs" instead of "My Doc - Google Docs",
   // because the menu applies to the entire app.
