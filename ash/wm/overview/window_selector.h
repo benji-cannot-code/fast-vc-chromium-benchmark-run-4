fca372df3f4ee218da1964403c1099fbd384848b
Sammie Quon <sammiequon@google.com>
2018-04-16 18:06:50 +0000

    Reland "splitview: Grab app for splitscreen only happen on long press."
    This is a reland of b9d68c49a3140aa0ae429b742d29329fab54eef5
    Left the previously disabled test disabled in the reland. Will fix in a
    seperate cl.
    
    TBR=oshima@chromium.org
    
    Original change's description:
    > splitview: Grab app for splitscreen only happen on long press.
    >
    > The indicators and the item enlarging only happen after long press, or
    > if the item has been dragged.
    >
    > This is preparation for fling to dismiss. It also removes a flicker of
    > the indidcators when you tap to open a item.
    >
    > Also renables a previous flaky test. Tested locally and it is not flaky
    > no more.
    >
    > Test: ash_unittests SplitViewWindowSelectorTest.*
    > Bug: 817863
    > Change-Id: I9c20f2fa9a3c0a9218b3072c5b3d25711ad9c655
    > Reviewed-on: https://chromium-review.googlesource.com/996528
    > Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    > Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    > Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#550884}
    
    Bug: 817863
    Change-Id: I0d6b255a89e69443af2e6509a0769622c8f64fb3
    Reviewed-on: https://chromium-review.googlesource.com/1012735
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551036}

--- ash/wm/overview/window_selector.h
+++ ash/wm/overview/window_selector.h
@@ -121,6 +121,7 @@ class ASH_EXPORT WindowSelector : public display::DisplayObserver,
   void Drag(WindowSelectorItem* item, const gfx::Point& location_in_screen);
   void CompleteDrag(WindowSelectorItem* item,
                     const gfx::Point& location_in_screen);
+  void StartSplitViewDragMode(const gfx::Point& location_in_screen);
   void ActivateDraggedWindow();
   void ResetDraggedWindowGesture();
 
