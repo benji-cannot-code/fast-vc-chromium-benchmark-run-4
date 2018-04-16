0ac4c2c7119fa52ffa7ce636243b74df329fe906
khmel@google.com <khmel@google.com>
2018-04-16 19:54:53 +0000

    arc: Prevent crash from attempt do download some document.
    This prevents crash when user tries to download pdf document from
    Play Store ToS.
    
    Test: Manually
    Bug: b:64117707
    Change-Id: I965a760b0bdef2c1167a8007822ed7c9a2b353c6
    Reviewed-on: https://chromium-review.googlesource.com/1009111
    Commit-Queue: Yury Khmel <khmel@google.com>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551086}

--- chrome/browser/ui/views/select_file_dialog_extension.cc
+++ chrome/browser/ui/views/select_file_dialog_extension.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/extension_view_host.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/sessions/session_tab_helper.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
@@ -39,6 +40,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/base_window.h"
 #include "ui/shell_dialogs/selected_file_info.h"
 #include "ui/views/widget/widget.h"
+
+#if defined(OS_CHROMEOS)
+#include "chrome/browser/chromeos/login/ui/login_display_host.h"
+#include "chrome/browser/chromeos/login/ui/webui_login_view.h"
+#include "chrome/browser/chromeos/profiles/profile_helper.h"
+#endif
 
 using extensions::AppWindow;
 using content::BrowserThread;
@@ -94,6 +101,21 @@ scoped_refptr<SelectFileDialogExtension> PendingDialog::Find(
   return it->second;
 }
 
+#if defined(OS_CHROMEOS)
+// Return the Chrome OS WebUI login WebContents, if applicable.
+content::WebContents* GetLoginWebContents() {
+  chromeos::LoginDisplayHost* const login_display_host =
+      chromeos::LoginDisplayHost::default_host();
+  if (!login_display_host)
+    return nullptr;
+  chromeos::WebUILoginView* const view =
+      login_display_host->GetWebUILoginView();
+  if (!view)
+    return nullptr;
+  return view->GetWebContents();
+}
+#endif
+
 // Given |owner_window| finds corresponding |base_window|, it's associated
 // |web_contents| and |profile|.
 void FindRuntimeContext(gfx::NativeWindow owner_window,
@@ -139,7 +161,13 @@ void FindRuntimeContext(gfx::NativeWindow owner_window,
   if (chrome::IsRunningInForcedAppMode() && !(*web_contents))
     *web_contents = chromeos::LoginWebDialog::GetCurrentWebContents();
 
-  CHECK(web_contents);
+#if defined(OS_CHROMEOS)
+  // Check for a WebContents used for the Chrome OS WebUI login flow.
+  if (!*web_contents)
+    *web_contents = GetLoginWebContents();
+#endif
+
+  CHECK(*web_contents);
 }
 
 }  // namespace
@@ -332,6 +360,14 @@ void SelectFileDialogExtension::SelectFileImpl(
   CHECK(web_contents);
   profile_ = Profile::FromBrowserContext(web_contents->GetBrowserContext());
   CHECK(profile_);
+
+#if defined(OS_CHROMEOS)
+  // Handle the case when |web_contents| is associated with Default profile.
+  if (chromeos::ProfileHelper::IsSigninProfile(profile_)) {
+    profile_ = ProfileManager::GetActiveUserProfile();
+    CHECK(profile_);
+  }
+#endif
 
   // Check if we have another dialog opened for the contents. It's unlikely, but
   // possible. In such situation, discard this request.
