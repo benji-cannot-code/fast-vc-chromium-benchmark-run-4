90c2844d2a68173a89143c562375e461380970fb
Matt Falkenhagen <falken@chromium.org>
2018-04-16 10:37:28 +0000

    shared worker: Simplify telling devtools about start and stop of worker.
    SharedWorkerHost was calling WorkedDestroyed at three callsites using
    two flags to determine when to do so. Use a RAII helper class to manage
    the state instead.
    
    One motiviation is fixing 830288 which is going to add more state to
    SharedWorkerHost (creating a host before calling start), so when to call
    WorkerDestroyed would get more complicated.
    
    Bug: 830288
    Change-Id: Ie3f135d925091518b172a4afcd8c475656aa9db4
    Reviewed-on: https://chromium-review.googlesource.com/1013360
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550958}

--- content/browser/shared_worker/shared_worker_host.h
+++ content/browser/shared_worker/shared_worker_host.h
@@ -89,6 +89,7 @@ class SharedWorkerHost : public mojom::SharedWorkerHost,
   bool IsAvailable() const;
 
  private:
+  class ScopedDevToolsHandle;
   struct ClientInfo {
     ClientInfo(mojom::SharedWorkerClientPtr client,
                int connection_request_id,
@@ -136,6 +137,7 @@ class SharedWorkerHost : public mojom::SharedWorkerHost,
   bool termination_message_sent_ = false;
   bool closed_ = false;
   const base::TimeTicks creation_time_;
+  std::unique_ptr<ScopedDevToolsHandle> devtools_handle_;
 
   // This is the set of features that this worker has used.
   std::set<blink::mojom::WebFeature> used_features_;
