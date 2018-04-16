b0511ffd16901adbdabe4018cc0f886b6e82ba53
Miguel Casas <mcasas@chromium.org>
2018-04-16 16:39:01 +0000

    VaVDA: Move BlitSurface to |decoder_thread_task_runner_|
    This CL moves the call to VaapiPicture::DownloadFromSurface,
    which in turn calls VaapiWrapper::BlitSurface(), to be executed
    in the decoder thread of VaVDA. This considerable offloads the
    Gpu main thread for more urgent tasks, and anyway the said
    decoder thread is idle most of the time (seen in chrome:trace),
    so utilizing it more doesn't impact the overall performance but
    increases responsiveness.
    
    This Chrome:tracing details shows the change (soraka vp9 720p@30)
    before: https://i.imgur.com/CkqXqTp.png (https://imgur.com/a/pYVpf)
    after: https://i.imgur.com/o0c4ksW.png (https://imgur.com/a/wblx0)
    (the actual duration is inconsequential since it depends on the
    actual frame).
    
    nit: TRACE_EVENT1 is moved to the Vaapi*Picture because it results
    in easier syntax around the PostTaskAndReplyWithResult().
    
    TEST= simplechrome+crosvideo and v_d_a_unittest h264/vp8/vp9
    on soraka. No perf regression observed in the produced numbers.
    
    Bug: 717265
    Cq-Include-Trybots: luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: Icc4af2ef96141cebf95ac8fbb89c6e386e194a30
    Reviewed-on: https://chromium-review.googlesource.com/947341
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551006}

--- media/gpu/vaapi/vaapi_picture_native_pixmap.cc
+++ media/gpu/vaapi/vaapi_picture_native_pixmap.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/gpu/vaapi/vaapi_picture_native_pixmap.h"
 
+#include "base/trace_event/trace_event.h"
 #include "media/gpu/vaapi/va_surface.h"
 #include "media/gpu/vaapi/vaapi_wrapper.h"
 #include "ui/gfx/buffer_format_util.h"
@@ -38,12 +39,13 @@ VaapiPictureNativePixmap::~VaapiPictureNativePixmap() = default;
 
 bool VaapiPictureNativePixmap::DownloadFromSurface(
     const scoped_refptr<VASurface>& va_surface) {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  TRACE_EVENT1("media,gpu", "VaapiDrmPicture::DownloadFromSurface",
+               "VASurfaceID", va_surface->id());
+  // Can be called from any thread because |vaapi_wrapper_| is thread safe.
   return vaapi_wrapper_->BlitSurface(va_surface, va_surface_);
 }
 
 bool VaapiPictureNativePixmap::AllowOverlay() const {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   return true;
 }
 
