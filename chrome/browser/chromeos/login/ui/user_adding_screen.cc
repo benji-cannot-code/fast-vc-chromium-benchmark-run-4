30b8120b0c851dc9110e566fb302856ae963e75d
Evan Stade <estade@chromium.org>
2018-04-16 21:19:58 +0000

    Remove an ash reference from the user adding screen.
          system tray still works
    
    Bug: 327885
    Test: sign into one user, start signin to a second user, cancel;
    Change-Id: I9fa93bbd87a1f4a0087746e3a42ead07ee308710
    Reviewed-on: https://chromium-review.googlesource.com/1008825
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551120}

--- chrome/browser/chromeos/login/ui/user_adding_screen.cc
+++ chrome/browser/chromeos/login/ui/user_adding_screen.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/login/ui/user_adding_screen.h"
 
-#include "ash/shell.h"
-#include "ash/system/tray/system_tray.h"
 #include "base/bind.h"
 #include "base/memory/singleton.h"
 #include "base/observer_list.h"
@@ -63,8 +61,6 @@ void UserAddingScreenImpl::Start() {
 void UserAddingScreenImpl::Cancel() {
   CHECK(IsRunning());
 
-  // Make sure that system tray is enabled after this flow.
-  ash::Shell::Get()->GetPrimarySystemTray()->SetEnabled(true);
   display_host_->CancelUserAdding();
 
   // Reset wallpaper if cancel adding user from multiple user sign in page.
