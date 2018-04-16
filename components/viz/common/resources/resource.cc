97bc0da43fbe85f61d01db8bce88e93267f6f240
Antoine Labour <piman@chromium.org>
2018-04-16 19:56:17 +0000

    Remove kGpuMemoryBuffer resource type
    They are only created in ResourceProviderTest, not used in production.
    Also remove ScopedWriteLockGpuMemoryBuffer and helper functions in
    LayerTreeResourceProvider.
    
    Bug: None
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I8ddd10fe5c0f86f994dbfeb966200154304d8432
    Reviewed-on: https://chromium-review.googlesource.com/1011558
    Commit-Queue: Antoine Labour <piman@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551087}

--- components/viz/common/resources/resource.cc
+++ components/viz/common/resources/resource.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "build/build_config.h"
 #include "components/viz/common/quads/shared_bitmap.h"
-#include "ui/gfx/gpu_memory_buffer.h"
 
 namespace viz {
 namespace internal {
@@ -81,7 +80,6 @@ void Resource::SetGenerateMipmap() {
   DCHECK(is_gpu_resource_type());
   DCHECK_EQ(target, static_cast<GLenum>(GL_TEXTURE_2D));
   DCHECK(hint & ResourceTextureHint::kMipmap);
-  DCHECK(!gpu_memory_buffer);
   mipmap_state = GENERATE;
 }
 
