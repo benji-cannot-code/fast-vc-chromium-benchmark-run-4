acfd202afe95cc5d43a24ae01d9fb05775ab1b3f
Tatsuhisa Yamaguchi <yamaguchi@google.com>
2018-04-16 01:55:22 +0000

    Allow running Zip Archiver for packing on MTP volumes.
    The feature was disabled on MTP volumes because older implementaiton
    didn't work on MTP volumes (crbug/486397). The packing function of Zip
    Archiver works on MTP volumes.
    
    Bug: 831525
    Test: Manually tested by zipping file on a connected MTP device.
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I61830c5853e7290295ed01f4cfe439f68dfb2480
    Reviewed-on: https://chromium-review.googlesource.com/1006741
    Reviewed-by: Yuki Awano <yawano@chromium.org>
    Commit-Queue: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550921}

--- ui/file_manager/file_manager/foreground/js/file_manager_commands.js
+++ ui/file_manager/file_manager/foreground/js/file_manager_commands.js
@@ -1449,11 +1449,11 @@ CommandHandler.COMMANDS_['zip-selection'] = /** @type {Command} */ ({
 
     var isOnEligibleLocation = CommandHandler.IS_ZIP_ARCHIVER_PACKER_ENABLED_ ?
         true :
-        !fileManager.directoryModel.isOnDrive();
+        !fileManager.directoryModel.isOnDrive() &&
+            !fileManager.directoryModel.isOnMTP();
 
     event.canExecute = dirEntry && !fileManager.directoryModel.isReadOnly() &&
-        !fileManager.directoryModel.isOnMTP() && isOnEligibleLocation &&
-        selection && selection.totalCount > 0;
+        isOnEligibleLocation && selection && selection.totalCount > 0;
   }
 });
 
