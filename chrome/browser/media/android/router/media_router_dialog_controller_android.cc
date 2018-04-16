d4c838fa17324c24f9bf8978f909cef21dbea268
mark a. foltz <mfoltz@chromium.org>
2018-04-16 20:42:45 +0000

    [Presentation API] Remove content::PresentationError and PresesentationErrorType
    This continues the cleanup of the Presentation API typemaps by removing two more
    content:: types and replacing them with blink::mojo types defined in
    presentation.mojom.
    
    Some refactoring of code and tests was required as Mojo callbacks converted from
    base::Optional to blink::mojom::PresentationErrorPtr, which is move-only.
    
    
    Bug: 817903
    Change-Id: I46136114a4afa4c14d989f2fbac341ccffe56740
    Reviewed-on: https://chromium-review.googlesource.com/1012984
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Derek Cheng <imcheng@chromium.org>
    Commit-Queue: mark a. foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551103}

--- chrome/browser/media/android/router/media_router_dialog_controller_android.cc
+++ chrome/browser/media/android/router/media_router_dialog_controller_android.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents_delegate.h"
 #include "device/vr/buildflags/buildflags.h"
 #include "jni/ChromeMediaRouterDialogController_jni.h"
+#include "third_party/blink/public/platform/modules/presentation/presentation.mojom.h"
 
 DEFINE_WEB_CONTENTS_USER_DATA_KEY(
     media_router::MediaRouterDialogControllerAndroid);
@@ -115,8 +116,9 @@ void MediaRouterDialogControllerAndroid::OnMediaSourceNotSupported(
   if (!request)
     return;
 
-  request->InvokeErrorCallback(content::PresentationError(
-      content::PRESENTATION_ERROR_NO_AVAILABLE_SCREENS, "No screens found."));
+  request->InvokeErrorCallback(blink::mojom::PresentationError(
+      blink::mojom::PresentationErrorType::NO_AVAILABLE_SCREENS,
+      "No screens found."));
 }
 
 void MediaRouterDialogControllerAndroid::CancelPresentationRequest() {
@@ -124,8 +126,8 @@ void MediaRouterDialogControllerAndroid::CancelPresentationRequest() {
   if (!request)
     return;
 
-  request->InvokeErrorCallback(content::PresentationError(
-      content::PRESENTATION_ERROR_PRESENTATION_REQUEST_CANCELLED,
+  request->InvokeErrorCallback(blink::mojom::PresentationError(
+      blink::mojom::PresentationErrorType::PRESENTATION_REQUEST_CANCELLED,
       "Dialog closed."));
 }
 
