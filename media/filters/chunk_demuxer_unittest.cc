d4f376faf6115d6ee460fa64204be3c0774b8f9b
Wojciech Dzierżanowski <wdzierzanowski@opera.com>
2018-04-16 22:44:52 +0000

    Avoid calling GetAllStreams() on uninitialized Demuxer
    The first time we know the Demuxer initialization is complete is when
    Pipeline::Start() completes (asynchronously).
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I7451187be2d141780d5aa514fb3a1ec4abe8fbbe
    Reviewed-on: https://chromium-review.googlesource.com/1000858
    Commit-Queue: Chrome Cunningham <chcunningham@chromium.org>
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551156}

--- media/filters/chunk_demuxer_unittest.cc
+++ media/filters/chunk_demuxer_unittest.cc
@@ -1421,6 +1421,9 @@ TEST_P(ChunkDemuxerTest, Init) {
       EXPECT_FALSE(video_stream);
     }
 
+    for (auto* stream : demuxer_->GetAllStreams())
+      EXPECT_TRUE(stream->SupportsConfigChanges());
+
     ShutdownDemuxer();
     demuxer_.reset();
   }
