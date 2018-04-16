3a2134e9df5555d101e1ade1eabc5f30745a9207
Philipp Hancke <philipp.hancke@googlemail.com>
2018-04-16 08:26:45 +0000

    webrtc-internals: only highlight currently active connection
    and remove the class from any past ones.
    Re-land of
      https://chromium-review.googlesource.com/c/chromium/src/+/615500
    (with JS error fixed)
    
    BUG=678292
    
    Change-Id: Icedc03ed30e2f4c0d36341db371ef3d12a679a94
    Reviewed-on: https://chromium-review.googlesource.com/986139
    Reviewed-by: Tommi <tommi@chromium.org>
    Commit-Queue: Harald Alvestrand <hta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550946}

--- content/browser/resources/media/stats_table.js
+++ content/browser/resources/media/stats_table.js
@@ -134,6 +134,7 @@ var StatsTable = (function(ssrcInfoManager) {
     updateStatsTableRow_: function(statsTable, rowName, value) {
       var trId = statsTable.id + '-' + rowName;
       var trElement = $(trId);
+      var activeConnectionClass = 'stats-table-active-connection';
       if (!trElement) {
         trElement = document.createElement('tr');
         trElement.id = trId;
@@ -143,8 +144,13 @@ var StatsTable = (function(ssrcInfoManager) {
       trElement.cells[1].textContent = value;
 
       // Highlights the table for the active connection.
-      if (rowName == 'googActiveConnection' && value == true)
-        statsTable.parentElement.classList.add('stats-table-active-connection');
+      if (rowName == 'googActiveConnection') {
+        if (value === 'true') {
+          statsTable.parentElement.classList.add(activeConnectionClass);
+        } else {
+          statsTable.parentElement.classList.remove(activeConnectionClass);
+        }
+      }
     }
   };
 
