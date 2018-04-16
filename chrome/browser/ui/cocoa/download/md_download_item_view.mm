2c30a03fc5013998f673d7ddb5351e92bf3375a8
Sidney San Martín <sdy@chromium.org>
2018-04-16 20:16:26 +0000

    Fix a crash when dragging items from the MD download shelf.
    This crashes consistently on 10.12 when draggingFrame isn't set.
    
    Bug: 826632
    Change-Id: I9b1aec6804c0f5205cd2a471787cc296d943ff91
    Reviewed-on: https://chromium-review.googlesource.com/1014361
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Commit-Queue: Sidney San Martín <sdy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551092}

--- chrome/browser/ui/cocoa/download/md_download_item_view.mm
+++ chrome/browser/ui/cocoa/download/md_download_item_view.mm
@@ -648,6 +648,12 @@ NSTextField* MakeLabel(
                                    fileURLWithPath:base::SysUTF8ToNSString(
                                                        downloadPath_.value())]]
       autorelease];
+
+  // draggingFrame must be set to avoid triggering an AppKit crash. See
+  // https://crbug.com/826632.
+  NSRect dragRect = NSUnionRect(imageRect, labelRect);
+  draggingItem.draggingFrame = dragRect;
+
   draggingItem.imageComponentsProvider = ^{
     // If either component is zero sized (which shouldn't generally happen, but
     // apparently can… maybe a missing icon or empty string title?), omit it,
@@ -660,7 +666,8 @@ NSTextField* MakeLabel(
               initWithKey:NSDraggingImageComponentIconKey] autorelease];
       NSImage* image = imageView_.image;
       imageComponent.contents = image;
-      imageComponent.frame = imageRect;
+      imageComponent.frame =
+          NSOffsetRect(imageRect, -dragRect.origin.x, -dragRect.origin.y);
       [components addObject:imageComponent];
     }
     if (labelRect.size.width != 0 && labelRect.size.height != 0) {
@@ -674,7 +681,8 @@ NSTextField* MakeLabel(
                                           [filename drawAtPoint:NSZeroPoint];
                                           return YES;
                                         }];
-      labelComponent.frame = labelRect;
+      labelComponent.frame =
+          NSOffsetRect(labelRect, -dragRect.origin.x, -dragRect.origin.y);
       [components addObject:labelComponent];
     }
 
