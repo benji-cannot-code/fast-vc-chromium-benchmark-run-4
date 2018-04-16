fc1fcbabb25fc4d7318a145f0440353632649d32
Elly Fong-Jones <ellyjones@chromium.org>
2018-04-16 17:59:41 +0000

    macviews: use mac-y focus rings
    This change:
    1) Uses the system focus ring color even when SecondaryUiMd is enabled
    2) Moves focus ring thickness and inset out to PlatformStyle
    3) Adds Mac overrides of the default values
    
    Bug: 832306
    Change-Id: I6c18d9d0431dc411a5cb95f9c0639e41f4aa1127
    Reviewed-on: https://chromium-review.googlesource.com/1012163
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551030}

--- ui/views/controls/focus_ring.cc
+++ ui/views/controls/focus_ring.cc
@@ -6,13 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/focus_ring.h"
 
 #include "ui/gfx/canvas.h"
+#include "ui/views/controls/focusable_border.h"
+#include "ui/views/style/platform_style.h"
 
 namespace views {
 
 namespace {
-
-// The default stroke width of the focus border in dp.
-constexpr float kFocusHaloThicknessDp = 2.f;
 
 FocusRing* GetFocusRing(View* parent) {
   for (int i = 0; i < parent->child_count(); ++i) {
@@ -77,7 +76,7 @@ void FocusRing::Layout() {
   // The focus ring handles its own sizing, which is simply to fill the parent
   // and extend a little beyond its borders.
   gfx::Rect focus_bounds = parent()->GetLocalBounds();
-  focus_bounds.Inset(gfx::Insets(-kFocusHaloThicknessDp));
+  focus_bounds.Inset(gfx::Insets(PlatformStyle::kFocusHaloInset));
   SetBoundsRect(focus_bounds);
 }
 
@@ -86,14 +85,14 @@ void FocusRing::OnPaint(gfx::Canvas* canvas) {
   flags.setAntiAlias(true);
   flags.setColor(SkColorSetA(color_, 0x66));
   flags.setStyle(cc::PaintFlags::kStroke_Style);
-  flags.setStrokeWidth(kFocusHaloThicknessDp);
+  flags.setStrokeWidth(PlatformStyle::kFocusHaloThickness);
   gfx::RectF rect(GetLocalBounds());
-  rect.Inset(gfx::InsetsF(kFocusHaloThicknessDp / 2.f));
+  rect.Inset(gfx::InsetsF(PlatformStyle::kFocusHaloThickness / 2.f));
   // The focus indicator should hug the normal border, when present (as in the
   // case of text buttons). Since it's drawn outside the parent view, increase
   // the rounding slightly by adding half the ring thickness.
-  canvas->DrawRoundRect(rect, corner_radius_ + kFocusHaloThicknessDp / 2.f,
-                        flags);
+  canvas->DrawRoundRect(
+      rect, corner_radius_ + PlatformStyle::kFocusHaloThickness / 2.f, flags);
 }
 
 void FocusRing::OnViewNativeThemeChanged(View* observed_view) {
