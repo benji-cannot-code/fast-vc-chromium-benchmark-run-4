48c332f0d243198f70e83ab756066575024a6dac
stkhapugin@chromium.org <stkhapugin@chromium.org>
2018-04-17 14:42:42 +0000

    [iOS] Use autolayout to present the omnibox popup.
    Makes the omnibox popup presenter use autolayout to display the
    popup view controller. The correct height of the popup is then
    communicated by means of intrinsicContentSize of the popup table view.
    
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I521adfbff62c3c42e7120fb7bb5f7f5b09bfd2b3
    Bug: 821817
    Reviewed-on: https://chromium-review.googlesource.com/978177
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551326}

--- ios/chrome/browser/ui/omnibox/popup/omnibox_popup_presenter.h
+++ ios/chrome/browser/ui/omnibox/popup/omnibox_popup_presenter.h
@@ -9,13 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <UIKit/UIKit.h>
 
 @protocol OmniboxPopupPositioner;
-@protocol TableViewOwning;
 
 @interface OmniboxPopupPresenter : NSObject
 
 - (instancetype)initWithPopupPositioner:(id<OmniboxPopupPositioner>)positioner
-                    popupViewController:
-                        (UIViewController<TableViewOwning>*)viewController;
+                    popupViewController:(UIViewController*)viewController;
 
 // Updates appearance depending on the content size of the presented view
 // controller by changing the visible height of the popup. When the popup was
--- ios/chrome/browser/ui/omnibox/popup/omnibox_popup_view_controller.h
+++ ios/chrome/browser/ui/omnibox/popup/omnibox_popup_view_controller.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/commands/omnibox_suggestion_commands.h"
 #import "ios/chrome/browser/ui/omnibox/autocomplete_result_consumer.h"
 #import "ios/chrome/browser/ui/omnibox/image_retriever.h"
-#import "ios/chrome/browser/ui/omnibox/popup/table_view_owning.h"
 
 @protocol ImageRetriever;
 
@@ -22,9 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // is the first responder, this view controller cannot receive these events.
 // Hence the delegation.
 @interface OmniboxPopupViewController
-    : UIViewController<AutocompleteResultConsumer,
-                       OmniboxSuggestionCommands,
-                       TableViewOwning>
+    : UIViewController<AutocompleteResultConsumer, OmniboxSuggestionCommands>
 
 @property(nonatomic, assign) BOOL incognito;
 @property(nonatomic, weak) id<AutocompleteResultConsumerDelegate> delegate;
