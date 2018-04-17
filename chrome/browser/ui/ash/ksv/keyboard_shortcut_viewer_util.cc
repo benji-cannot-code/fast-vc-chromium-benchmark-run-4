d27cceda9a61e91a5b0cfd426742889058e89be4
wutao <wutao@chromium.org>
2018-04-17 15:55:38 +0000

    cros: Add TODO to remove deps for Shortcuts Viewer
    This cl adds TODO to remove the dependency on aura::Window in Keyboard
    Shortcuts Viewer. In addition, this cl removes unused deps and include.
    
    Bug: 833673
    Test: Manual
    Change-Id: Icc4fd0726cace1971674e371bb5053b20575933b
    Reviewed-on: https://chromium-review.googlesource.com/1014663
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551348}

--- chrome/browser/ui/ash/ksv/keyboard_shortcut_viewer_util.cc
+++ chrome/browser/ui/ash/ksv/keyboard_shortcut_viewer_util.cc
@@ -7,11 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/components/shortcut_viewer/views/keyboard_shortcut_view.h"
 #include "ash/shell.h"
-#include "ash/wm/window_util.h"
 
 namespace keyboard_shortcut_viewer_util {
 
 void ShowKeyboardShortcutViewer() {
+  // TODO(https://crbug.com/833673): Remove the dependency on aura::Window.
   keyboard_shortcut_viewer::KeyboardShortcutView::Show(
       ash::Shell::GetRootWindowForNewWindows());
 }
