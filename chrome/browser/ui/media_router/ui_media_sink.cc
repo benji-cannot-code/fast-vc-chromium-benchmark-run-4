6c87dd7e8b19fdfda45fd42d91d2f369d95fb5f2
Takumi Fujimoto <takumif@chromium.org>
2018-04-17 19:44:47 +0000

    Reland "Implement sink list button for Harmony Cast dialog"
    This is a reland of d37f3a32d266f49235d0260172a6cd85398324d5
    This fixes the issue of attributes in UIMediaSink not being
    initialized at instantiation, which was breaking MSAN builds.
    
    TBR=estade@chromium.org,bsep@chromium.org
    
    Original change's description:
    > Implement sink list button for Harmony Cast dialog
    >
    > - Add CastDialogSinkButton, which will be used for sink list items in
    >   the Harmony Cast dialog. It's a subclass of HoverButton that shows a
    >   device icon and an optional status indicator (checkmark or throbber).
    > - Add UIMediaSink, a struct containing data used to populate the button
    > - Add Skia icons used by the button: Cast for EDU, input, speaker,
    >   speaker group, and TV
    >
    > Screenshot of the button (two of them) in a WIP dialog:
    > https://drive.google.com/file/d/1M1R9jO-VHEO18aA3pTpoQBI4FHMU5MEf/view?usp=sharing
    >
    > Dimensions and colors are subject to change in later CLs.
    >
    > Bug: 826089, 826393
    > Change-Id: I0eef58e1c0b9bf2cff3b3d146e911d3a18c7d23c
    > Reviewed-on: https://chromium-review.googlesource.com/996458
    > Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    > Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    > Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    > Reviewed-by: Evan Stade <estade@chromium.org>
    > Reviewed-by: Adam Parker <amp@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#550722}
    
    Bug: 826089, 826393
    Change-Id: Ib9343fc56a96f128c3b3d2bfa5e82542e94a4441
    Reviewed-on: https://chromium-review.googlesource.com/1014540
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Reviewed-by: Adam Parker <amp@chromium.org>
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551441}

---
+++ chrome/browser/ui/media_router/ui_media_sink.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/media_router/ui_media_sink.h"
+
+namespace media_router {
+
+UIMediaSink::UIMediaSink() = default;
+
+UIMediaSink::UIMediaSink(const UIMediaSink& other) = default;
+
+UIMediaSink::~UIMediaSink() = default;
+
+}  // namespace media_router
