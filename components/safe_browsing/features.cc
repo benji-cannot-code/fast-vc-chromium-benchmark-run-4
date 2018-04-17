da7e57f8d0e2719bb5fb824e6509095d0a8b85d6
Jialiu Lin <jialiul@chromium.org>
2018-04-17 00:48:56 +0000

    Add a static chrome://reset-password page
    Chrome will open a tab with chrome://reset-password if the user reuses
    their protected enterprise password on other sites that are not allowed
    by enterprise policy.
    chrome://reset-password will only show if password protection related
    enterprise policy settings are properly set. Otherwise, user will see
    ERR_INVALID_URL.
    
    The new icon in ui/webui/resources/cr_elements/icons.html is copied from
    /chrome/browser/resources/settings/icons.html
    
    Bug: 824501
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I8d648f0484e0c569ddbed048114a90471e4eea25
    Reviewed-on: https://chromium-review.googlesource.com/996565
    Commit-Queue: Jialiu Lin <jialiul@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Nathan Parker <nparker@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551195}

--- components/safe_browsing/features.cc
+++ components/safe_browsing/features.cc
@@ -69,6 +69,9 @@ const base::Feature kInspectDownloadedRarFiles{
 const base::Feature kEnterprisePasswordProtectionV1{
     "EnterprisePasswordProtectionV1", base::FEATURE_DISABLED_BY_DEFAULT};
 
+const base::Feature kForceEnableResetPasswordWebUI{
+    "ForceEnableResetPasswordWebUI", base::FEATURE_DISABLED_BY_DEFAULT};
+
 namespace {
 // List of experimental features. Boolean value for each list member should be
 // set to true if the experiment is currently running at a probability other
@@ -84,6 +87,7 @@ constexpr struct {
     {&kCheckByURLLoaderThrottle, true},
     {&kDispatchSafetyNetCheckOffThread, false},
     {&kEnterprisePasswordProtectionV1, true},
+    {&kForceEnableResetPasswordWebUI, false},
     {&kGaiaPasswordReuseReporting, true},
     {&kGoogleBrandedPhishingWarning, false},
     {&kInspectDownloadedRarFiles, true},
