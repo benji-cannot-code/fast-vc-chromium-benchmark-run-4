df3899572414304130cc1d7c428ee12ef21be6d1
Peter Boström <pbos@chromium.org>
2018-04-17 23:43:30 +0000

    Use 8dp corners for browser actions in MD refresh
    Extends 8dp corners to ToolbarActionView which was missed in a previous
    change. This makes them consistent with other toolbar buttons.
    
    Bug: chromium:822069
    Change-Id: Id894664ec698504e3ff997c1b12691821ab1062e
    Reviewed-on: https://chromium-review.googlesource.com/1015286
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551523}

--- chrome/browser/ui/views/toolbar/toolbar_action_view.cc
+++ chrome/browser/ui/views/toolbar/toolbar_action_view.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/toolbar/toolbar_action_view_controller.h"
 #include "chrome/browser/ui/toolbar/toolbar_actions_bar.h"
 #include "chrome/browser/ui/view_ids.h"
+#include "chrome/browser/ui/views/harmony/chrome_layout_provider.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_ink_drop_util.h"
 #include "content/public/browser/notification_source.h"
 #include "ui/accessibility/ax_node_data.h"
@@ -76,6 +77,12 @@ ToolbarActionView::ToolbarActionView(
 
   if (ui::MaterialDesignController::IsTouchOptimizedUiEnabled())
     set_ink_drop_visible_opacity(kTouchToolbarInkDropVisibleOpacity);
+
+  if (ui::MaterialDesignController::IsNewerMaterialUi()) {
+    const int radii =
+        ChromeLayoutProvider::Get()->GetCornerRadiusMetric(EMPHASIS_MEDIUM);
+    set_ink_drop_corner_radii(radii, radii);
+  }
 
   UpdateState();
 }
