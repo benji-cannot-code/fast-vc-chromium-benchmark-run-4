fc8e1f4aaf9386484ab27bdd31fb1f1144cbd087
Mostyn Bramley-Moore <mostynb@vewd.com>
2018-04-17 18:48:22 +0000

    [jumbo] avoid HoldRefCallback collision
    CL https://chromium-review.googlesource.com/c/chromium/src/+/1013118 added
    a HoldRefCallback function to print_job_worker.cc which conflicts with
    an identically named function in print_job.cc in jumbo builds.  Let's
    rename this one.
    
    Change-Id: I41d6c064c968bd72c736d648439a372cc4ca64a1
    Reviewed-on: https://chromium-review.googlesource.com/1013495
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Mostyn Bramley-Moore <mostynb@vewd.com>
    Cr-Commit-Position: refs/heads/master@{#551407}

--- chrome/browser/printing/print_job_worker.cc
+++ chrome/browser/printing/print_job_worker.cc
@@ -104,8 +104,8 @@ void NotificationCallback(PrintJob* print_job,
 }
 
 // Helper function to ensure |query| is valid until at least |callback| returns.
-void HoldRefCallback(scoped_refptr<PrinterQuery> query,
-                     base::OnceClosure callback) {
+void WorkerHoldRefCallback(scoped_refptr<PrinterQuery> query,
+                           base::OnceClosure callback) {
   std::move(callback).Run();
 }
 
@@ -113,7 +113,7 @@ void PostOnQueryThread(scoped_refptr<PrinterQuery> query,
                        PrintingContext::PrintSettingsCallback callback,
                        PrintingContext::Result result) {
   query->PostTask(FROM_HERE,
-                  base::BindOnce(&HoldRefCallback, query,
+                  base::BindOnce(&WorkerHoldRefCallback, query,
                                  base::BindOnce(std::move(callback), result)));
 }
 
@@ -198,14 +198,14 @@ void PrintJobWorker::GetSettings(bool ask_user_for_settings,
     BrowserThread::PostTask(
         BrowserThread::UI, FROM_HERE,
         base::BindOnce(
-            &HoldRefCallback, base::WrapRefCounted(query_),
+            &WorkerHoldRefCallback, base::WrapRefCounted(query_),
             base::BindOnce(&PrintJobWorker::GetSettingsWithUI,
                            base::Unretained(this), document_page_count,
                            has_selection, is_scripted)));
   } else {
     BrowserThread::PostTask(
         BrowserThread::UI, FROM_HERE,
-        base::BindOnce(&HoldRefCallback, base::WrapRefCounted(query_),
+        base::BindOnce(&WorkerHoldRefCallback, base::WrapRefCounted(query_),
                        base::BindOnce(&PrintJobWorker::UseDefaultSettings,
                                       base::Unretained(this))));
   }
@@ -219,7 +219,7 @@ void PrintJobWorker::SetSettings(
   BrowserThread::PostTask(
       BrowserThread::UI, FROM_HERE,
       base::BindOnce(
-          &HoldRefCallback, base::WrapRefCounted(query_),
+          &WorkerHoldRefCallback, base::WrapRefCounted(query_),
           base::BindOnce(&PrintJobWorker::UpdatePrintSettings,
                          base::Unretained(this), std::move(new_settings))));
 }
@@ -233,7 +233,7 @@ void PrintJobWorker::SetSettingsFromPOD(
   BrowserThread::PostTask(
       BrowserThread::UI, FROM_HERE,
       base::BindOnce(
-          &HoldRefCallback, base::WrapRefCounted(query_),
+          &WorkerHoldRefCallback, base::WrapRefCounted(query_),
           base::BindOnce(&PrintJobWorker::UpdatePrintSettingsFromPOD,
                          base::Unretained(this), std::move(new_settings))));
 }
