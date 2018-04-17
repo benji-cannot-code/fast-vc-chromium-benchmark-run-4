b3801d7638ca8b12eb603619f9bd974918665476
Qiang Xu <warx@google.com>
2018-04-17 18:17:51 +0000

    cros: remove braille_display_connected_
    changes:
    Follow up with crrev.com/c/1007891, |braille_display_connected_| is no
    longer needed by TrayAccessibility. We don't need it for tray
    visibility either, since when connected it always guaranteed that spoken
    feedback is enabled, so that spoken feedback enabled state could reflect
    on behalf of braille display connected state.
    
    Bug: 800270
    Test: covered by tests
    Change-Id: I33f0d7b9d53a41e7ff1ed5160207b9d5fb603188
    Reviewed-on: https://chromium-review.googlesource.com/1014746
    Commit-Queue: Qiang Xu <warx@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551398}

--- ash/accessibility/accessibility_controller.h
+++ ash/accessibility/accessibility_controller.h
@@ -90,8 +90,6 @@ class ASH_EXPORT AccessibilityController
   void SetVirtualKeyboardEnabled(bool enabled);
   bool IsVirtualKeyboardEnabled() const;
 
-  bool braille_display_connected() const { return braille_display_connected_; }
-
   // Triggers an accessibility alert to give the user feedback.
   void TriggerAccessibilityAlert(mojom::AccessibilityAlert alert);
 
@@ -195,7 +193,6 @@ class ASH_EXPORT AccessibilityController
   bool select_to_speak_enabled_ = false;
   bool sticky_keys_enabled_ = false;
   bool virtual_keyboard_enabled_ = false;
-  bool braille_display_connected_ = false;
 
   // Used to control the highlights of caret, cursor and focus.
   std::unique_ptr<AccessibilityHighlightController>
