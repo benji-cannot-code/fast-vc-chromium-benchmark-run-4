3645ce962eb5128fde4bc6b95c1151c98a9ca59b
Vaclav Brozek <vabr@chromium.org>
2018-04-17 19:27:34 +0000

    Modernize unique_ptr usage in form utils
    password_form_conversion_utils.cc used the constructor of
    std::unique_ptr explicitly. This is to be avoided in Chromium's code
    (//PRESUBMIT.py has a check against introducing new instances) and
    nullptr and std::make_unique are preferred.
    
    form_autofill_util.cc used a raw owning pointer and base::WrapUnique
    to convert it to std::unique_ptr. Instead, the owned object should be
    wrapped in unique_ptr since construction, to avoid the risk of
    introducing a leak later.
    
    This CL fixes both issues mentioned above.
    
    Bug: 833838
    Change-Id: Id62f146e69a659bd594678989310f2d212151572
    Reviewed-on: https://chromium-review.googlesource.com/1013716
    Commit-Queue: Vaclav Brozek <vabr@chromium.org>
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551433}

--- components/autofill/content/renderer/form_autofill_util.cc
+++ components/autofill/content/renderer/form_autofill_util.cc
@@ -10,12 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <memory>
 #include <set>
+#include <utility>
 #include <vector>
 
 #include "base/command_line.h"
 #include "base/i18n/case_conversion.h"
 #include "base/logging.h"
-#include "base/memory/ptr_util.h"
 #include "base/no_destructor.h"
 #include "base/stl_util.h"
 #include "base/strings/string_number_conversions.h"
@@ -1012,12 +1012,12 @@ bool ExtractFieldsFromControlElements(
       continue;
 
     // Create a new FormFieldData, fill it out and map it to the field's name.
-    FormFieldData* form_field = new FormFieldData;
+    auto form_field = std::make_unique<FormFieldData>();
     WebFormControlElementToFormField(control_element,
                                      field_value_and_properties_map,
-                                     extract_mask, form_field);
-    form_fields->push_back(base::WrapUnique(form_field));
-    (*element_map)[control_element] = form_field;
+                                     extract_mask, form_field.get());
+    (*element_map)[control_element] = form_field.get();
+    form_fields->push_back(std::move(form_field));
     (*fields_extracted)[i] = true;
 
     // To avoid overly expensive computation, we impose a maximum number of
