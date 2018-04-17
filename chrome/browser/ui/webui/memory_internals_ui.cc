722fae39d767e03ce6fcc65caa7cb7a5539df084
Erik Chen <erikchen@chromium.org>
2018-04-17 19:05:32 +0000

    Clean up components/heap_profiling.
    This CL has no intended behavior change.
    
    This CL cleans up test structure and updates some comments.
    
    Change-Id: I3b5ed07813ec416dd67098d9f93b702a115cee09
    Bug: 827545
    Reviewed-on: https://chromium-review.googlesource.com/1014371
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551418}

--- chrome/browser/ui/webui/memory_internals_ui.cc
+++ chrome/browser/ui/webui/memory_internals_ui.cc
@@ -252,6 +252,7 @@ void MemoryInternalsDOMHandler::HandleReportProcess(
 
 void MemoryInternalsDOMHandler::HandleStartProfiling(
     const base::ListValue* args) {
+  DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
   if (!args->is_list() || args->GetList().size() != 1)
     return;
 
@@ -295,6 +296,8 @@ void MemoryInternalsDOMHandler::GetProfiledPids(
   DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
   heap_profiling::Supervisor* supervisor =
       heap_profiling::Supervisor::GetInstance();
+
+  // The supervisor hasn't started, so return an empty list.
   if (!supervisor->HasStarted()) {
     content::BrowserThread::PostTask(
         content::BrowserThread::UI, FROM_HERE,
@@ -303,6 +306,7 @@ void MemoryInternalsDOMHandler::GetProfiledPids(
                        std::vector<base::ProcessId>()));
     return;
   }
+
   supervisor->GetProfiledPids(
       base::BindOnce(&MemoryInternalsDOMHandler::ReturnProcessListOnUIThread,
                      weak_factory_.GetWeakPtr(), std::move(children)));
