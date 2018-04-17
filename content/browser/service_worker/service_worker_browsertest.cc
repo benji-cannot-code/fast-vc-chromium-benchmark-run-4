1d6942e00df136d5dd841e18ce5d850b6055f22d
Tsuyoshi Horo <horo@chromium.org>
2018-04-17 05:44:17 +0000

    Revert "Enable ServiceWorkerScriptFullCodeCache by default"
    This reverts commit c4334bc5b70e16494bad3511562a045f5ccb70a7.
    
    Reason for revert: Caused crashes when storing javascript modules in cache storage. https://crbug.com/832202
    
    Original change's description:
    > Enable ServiceWorkerScriptFullCodeCache by default
    > 
    > Bug: 768705,788619
    > Change-Id: I18474261a0bb443b5ed7186adb8bc68227ece26c
    > Reviewed-on: https://chromium-review.googlesource.com/994492
    > Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#547946}
    
    TBR=horo@chromium.org,kinuko@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 768705, 788619, 832202
    Change-Id: I53d3a401dfb83fbf54184588ca889b1a4f9f0474
    Reviewed-on: https://chromium-review.googlesource.com/1014310
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551258}

--- content/browser/service_worker/service_worker_browsertest.cc
+++ content/browser/service_worker/service_worker_browsertest.cc
@@ -2625,10 +2625,6 @@ class ServiceWorkerVersionBrowserV8CacheTest
       public ServiceWorkerVersion::Listener {
  public:
   using self = ServiceWorkerVersionBrowserV8CacheTest;
-  ServiceWorkerVersionBrowserV8CacheTest() {
-    scoped_feature_list_.InitAndDisableFeature(
-        features::kServiceWorkerScriptFullCodeCache);
-  }
   ~ServiceWorkerVersionBrowserV8CacheTest() override {
     if (version_)
       version_->RemoveListener(this);
@@ -2652,9 +2648,6 @@ class ServiceWorkerVersionBrowserV8CacheTest
 
   base::Closure cache_updated_closure_;
   size_t metadata_size_ = 0;
-
- private:
-  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
 IN_PROC_BROWSER_TEST_F(ServiceWorkerVersionBrowserV8CacheTest, Restart) {
