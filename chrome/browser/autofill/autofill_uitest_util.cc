841bc309d12f7b46bf076a9f663be823e62b2da4
Yiming Zhou <uwyiming@google.com>
2018-04-17 00:42:01 +0000

    Add browser tests for autofilling the shipping address info and payment info on captured sites.
    These tests are the first step in building a test framework for testing chrome autofill on complex, real-world sites pages. Since interacting with live sites are flaky and vulnerable to updates to the sites' features, this framework interacts with captured sites.
    
    Sites are captured with the Web Page Replay (WPR) tool. Prior to running the test, the browser test will start WPR locally on the machine using a capture file. The test then navigates to the test site, interact with the site's checkout workflow, and verifies that autofill correctly completes the address and payment information.
    
    Tests for 5 sites are included in this code change: Amazon, Zappos, Apple, Ebay and Walmart.
    
    Change-Id: I4b28e645554a2912c632fcb462439cac7142cf9f
    Reviewed-on: https://chromium-review.googlesource.com/963800
    Commit-Queue: Yiming Zhou <uwyiming@google.com>
    Reviewed-by: anthonyvd <anthonyvd@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551190}

--- chrome/browser/autofill/autofill_uitest_util.cc
+++ chrome/browser/autofill/autofill_uitest_util.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill {
 
+// PdmChangeWaiter ------------------------------------------------------------
 // This class is used to wait for asynchronous updates to PersonalDataManager
 // to complete.
 class PdmChangeWaiter : public PersonalDataManagerObserver {
@@ -71,6 +72,15 @@ void AddTestProfile(Browser* browser, const AutofillProfile& profile) {
   observer.Wait();
 }
 
+void AddTestProfile(Browser* browser,
+                    const AutofillProfile& profile,
+                    const CreditCard& card) {
+  AddTestProfile(browser, profile);
+  PdmChangeWaiter observer(browser);
+  GetPersonalDataManager(browser->profile())->AddCreditCard(card);
+  observer.Wait();
+}
+
 void SetTestProfile(Browser* browser, const AutofillProfile& profile) {
   std::vector<AutofillProfile> profiles;
   profiles.push_back(profile);
@@ -89,6 +99,15 @@ void AddTestCreditCard(Browser* browser, const CreditCard& card) {
 
   // AddCreditCard is asynchronous. Wait for it to finish before continuing the
   // tests.
+  observer.Wait();
+}
+
+void AddTestAutofillData(Browser* browser,
+                         const AutofillProfile& profile,
+                         const CreditCard& card) {
+  AddTestProfile(browser, profile);
+  PdmChangeWaiter observer(browser);
+  GetPersonalDataManager(browser->profile())->AddCreditCard(card);
   observer.Wait();
 }
 
