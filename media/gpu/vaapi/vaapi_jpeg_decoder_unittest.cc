f5165172b8fc4edbae7cfdadfa3cf5e493ffc04e
Miguel Casas-Sanchez <mcasas@chromium.org>
2018-04-17 18:39:15 +0000

    vaapi cleanup: a few s/const/constexpr/
    This CL s/const/constexpr/ in a few locations in vaapi/ folder.
    No new code intended, just that substitution.
    
    TBR=dcastagna@chromium.org
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I4ec733806feff58a70e8e9ab1c0d7f4565f1733c
    Reviewed-on: https://chromium-review.googlesource.com/1012949
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551403}

--- media/gpu/vaapi/vaapi_jpeg_decoder_unittest.cc
+++ media/gpu/vaapi/vaapi_jpeg_decoder_unittest.cc
@@ -27,8 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace media {
 namespace {
 
-const char* kTestFilename = "pixel-1280x720.jpg";
-const char* kExpectedMd5Sum = "6e9e1716073c9a9a1282e3f0e0dab743";
+constexpr char* kTestFilename = "pixel-1280x720.jpg";
+constexpr char* kExpectedMd5Sum = "6e9e1716073c9a9a1282e3f0e0dab743";
 
 void LogOnError() {
   LOG(FATAL) << "Oh noes! Decoder failed";
