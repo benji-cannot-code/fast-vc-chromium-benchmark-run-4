c8979de7c32da1944032241461df68fc3f26867f
Rob Buis <rob.buis@samsung.com>
2018-04-17 13:34:44 +0000

    Serialize border-image inline style
    Serialize border-image inline style by implementing the serializing
    logic for it in StylePropertySerializer::GetPropertyValue.
    
    Bug: 825568
    
    Change-Id: Ibaa18dadc41bb7aa735ab5e1384615b5d2270a2c
    Reviewed-on: https://chromium-review.googlesource.com/990721
    Commit-Queue: Rob Buis <rob.buis@samsung.com>
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551310}

--- third_party/blink/renderer/core/css/style_property_serializer.h
+++ third_party/blink/renderer/core/css/style_property_serializer.h
@@ -47,6 +47,7 @@ class StylePropertySerializer {
   String GetCommonValue(const StylePropertyShorthand&) const;
   String GetAlignmentShorthandValue(const StylePropertyShorthand&) const;
   String BorderPropertyValue() const;
+  String BorderImagePropertyValue() const;
   String GetLayeredShorthandValue(const StylePropertyShorthand&) const;
   String Get2Values(const StylePropertyShorthand&) const;
   String Get4Values(const StylePropertyShorthand&) const;
