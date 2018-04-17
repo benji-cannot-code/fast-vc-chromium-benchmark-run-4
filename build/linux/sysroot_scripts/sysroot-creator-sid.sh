5589db6e6bb913ba3d7da18d71f1e4911533b01d
Tom Anderson <thomasanderson@chromium.org>
2018-04-17 15:04:53 +0000

    Reland "Revert "Add more unbundle packages to sysroots""
    This is a reland of 961ea628cff07b24dbd5974288f41a29bd0ab65d
    Chromium r550608 has rolled clang to r329921, which includes the build fix.
    
    Original change's description:
    > Revert "Add more unbundle packages to sysroots"
    >
    > This reverts commit b828d53cbc4e24dc6f84f25e4b83c6082e717982.
    >
    > Reason for revert: Causing a build failure on Google Chrome Linux x64
    > https://ci.chromium.org/buildbot/chromium.chrome/Google%20Chrome%20Linux%20x64/29741
    >
    > /b/c/b/Google_Chrome_Linux_x64/src/out/Release/../../third_party/llvm-build/Release+Asserts/bin/ld.lld: error: duplicate symbol: exp2f@GLIBC_2.2.5
    > >>> defined in obj/third_party/swiftshader/src/OpenGL/compiler/swiftshader_opengl_compiler/Intermediate.o
    > >>> defined in obj/third_party/swiftshader/src/Renderer/swiftshader_renderer/Context.o
    > /b/c/b/Google_Chrome_Linux_x64/src/out/Release/../../third_party/llvm-build/Release+Asserts/bin/ld.lld: error: duplicate symbol: exp2f@GLIBC_2.2.5
    > >>> defined in obj/third_party/swiftshader/src/OpenGL/compiler/swiftshader_opengl_compiler/Intermediate.o
    > >>> defined in obj/third_party/swiftshader/src/Renderer/swiftshader_renderer/Sampler.o
    > /b/c/b/Google_Chrome_Linux_x64/src/out/Release/../../third_party/llvm-build/Release+Asserts/bin/ld.lld: error: duplicate symbol: powf@GLIBC_2.2.5
    > >>> defined in obj/third_party/swiftshader/src/Renderer/swiftshader_renderer/Surface.o
    > >>> defined in obj/third_party/swiftshader/src/Shader/swiftshader_shader/Constants.o
    > /b/c/b/Google_Chrome_Linux_x64/src/out/Release/../../third_party/llvm-build/Release+Asserts/bin/ld.lld: error: duplicate symbol: powf@GLIBC_2.2.5
    > >>> defined in obj/third_party/swiftshader/src/Renderer/swiftshader_renderer/Surface.o
    > >>> defined in obj/third_party/swiftshader/src/Common/swiftshader_common/Math.o
    >
    > Original change's description:
    > > Add more unbundle packages to sysroots
    > >
    > > The new packages are necessary to test unbundling of flac, libxslt, and zlib.
    > >
    > > BUG=800977
    > > R=​thestig
    > > TBR=mark
    > >
    > > Change-Id: I2c5f88b3819ce000e906cf303fdffa30bdde84cb
    > > Reviewed-on: https://chromium-review.googlesource.com/981451
    > > Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    > > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > > Reviewed-by: Nico Weber <thakis@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#549303}
    >
    > TBR=thakis@chromium.org,thestig@chromium.org,thomasanderson@chromium.org,mark@chromium.org
    >
    > Change-Id: I90188d22140d7cdd60f8bbea5b0e88705e498988
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 800977
    > Reviewed-on: https://chromium-review.googlesource.com/1003532
    > Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    > Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#549314}
    
    Bug: 800977
    Change-Id: I6b5deb3cd3c2d6ab77dfd904eb641744b88d2231
    Reviewed-on: https://chromium-review.googlesource.com/1012903
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551339}

--- build/linux/sysroot_scripts/sysroot-creator-sid.sh
+++ build/linux/sysroot_scripts/sysroot-creator-sid.sh
@@ -101,6 +101,7 @@ DEBIAN_PACKAGES="\
   libfontconfig1-dev
   libfreetype6
   libfreetype6-dev
+  libfribidi0
   libgbm-dev
   libgbm1
   libgcc-6-dev
@@ -169,6 +170,8 @@ DEBIAN_PACKAGES="\
   liblz4-1
   liblzma5
   liblzo2-2
+  libminizip-dev
+  libminizip1
   libmount1
   libnettle6
   libnspr4
@@ -176,6 +179,7 @@ DEBIAN_PACKAGES="\
   libnss-db
   libnss3
   libnss3-dev
+  libogg-dev
   libogg0
   libopengl0
   libopus-dev
@@ -207,7 +211,7 @@ DEBIAN_PACKAGES="\
   libpulse-dev
   libpulse-mainloop-glib0
   libpulse0
-  libre2-3
+  libre2-4
   libre2-dev
   librest-0.7-0
   libselinux1
@@ -292,6 +296,7 @@ DEBIAN_PACKAGES="\
   libxkbcommon-dev
   libxkbcommon0
   libxml2
+  libxml2-dev
   libxrandr-dev
   libxrandr2
   libxrender-dev
@@ -312,8 +317,8 @@ DEBIAN_PACKAGES="\
   uuid-dev
   wayland-protocols
   x11proto-composite-dev
-  x11proto-core-dev
   x11proto-damage-dev
+  x11proto-dev
   x11proto-fixes-dev
   x11proto-input-dev
   x11proto-kb-dev
