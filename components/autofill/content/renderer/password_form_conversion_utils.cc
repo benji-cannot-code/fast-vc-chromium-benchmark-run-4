658382604b2e2d61eca226630ab3c30d6d65ec5f
Vaclav Brozek <vabr@chromium.org>
2018-04-17 19:13:03 +0000

    Replace SyntheticForm::document with origin
    SyntheticForm is an abstraction of a collection of form input elements
    for the purpose of creating PasswordForm objects based on DOM tree.
    
    It currently stores a reference to the web document, only to derive
    the form's origin from it later.
    
    This CL drops the reference to the web document and replaces it with
    the computed origin. This way, SyntheticForm does not keep an
    unnecessary amount of data alive. It does introduce one GURL copy, but
    that will be addressed in a separate CL by making GetPasswordForm
    consume a SyntheticForm instead of looking at its const reference.
    
    Bug: 833838
    Change-Id: I33011146289b60bb50898f70f8001af6668a4203
    Reviewed-on: https://chromium-review.googlesource.com/1014130
    Commit-Queue: Vaclav Brozek <vabr@chromium.org>
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551423}

--- components/autofill/content/renderer/password_form_conversion_utils.cc
+++ components/autofill/content/renderer/password_form_conversion_utils.cc
@@ -36,8 +36,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/web/web_input_element.h"
 #include "third_party/blink/public/web/web_local_frame.h"
 #include "third_party/re2/src/re2/re2.h"
+#include "url/gurl.h"
 
-using blink::WebDocument;
 using blink::WebFormControlElement;
 using blink::WebFormElement;
 using blink::WebInputElement;
@@ -72,7 +72,8 @@ struct SyntheticForm {
   // Contains control elements of the represented form, including not fillable
   // ones.
   std::vector<blink::WebFormControlElement> control_elements;
-  blink::WebDocument document;
+  // The origin of the containing document.
+  GURL origin;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(SyntheticForm);
@@ -160,7 +161,8 @@ void PopulateSyntheticFormFromWebForm(const WebFormElement& web_form,
   web_form.GetFormControlElements(web_control_elements);
   synthetic_form->control_elements.assign(web_control_elements.begin(),
                                           web_control_elements.end());
-  synthetic_form->document = web_form.GetDocument();
+  synthetic_form->origin =
+      form_util::GetCanonicalOriginForDocument(web_form.GetDocument());
 }
 
 // Helper to determine which password is the main (current) one, and which is
@@ -751,8 +753,7 @@ bool GetPasswordForm(
     password_form->username_value = username_value;
   }
 
-  password_form->origin =
-      form_util::GetCanonicalOriginForDocument(form.document);
+  password_form->origin = form.origin;
   password_form->signon_realm = GetSignOnRealm(password_form->origin);
 
   // Convert |possible_usernames| to ValueElementVector.
@@ -918,7 +919,8 @@ std::unique_ptr<PasswordForm> CreatePasswordFormFromUnownedInputElements(
   SyntheticForm synthetic_form;
   synthetic_form.control_elements = form_util::GetUnownedFormFieldElements(
       frame.GetDocument().All(), &synthetic_form.fieldsets);
-  synthetic_form.document = frame.GetDocument();
+  synthetic_form.origin =
+      form_util::GetCanonicalOriginForDocument(frame.GetDocument());
 
   if (synthetic_form.control_elements.empty())
     return std::unique_ptr<PasswordForm>();
