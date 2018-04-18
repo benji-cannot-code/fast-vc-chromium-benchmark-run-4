453f0203bc1768d4e29acd43ed412896050ca341
sebsg <sebsg@chromium.org>
2018-04-18 16:54:32 +0000

    [AF] More efficient dynamic form fill on select options change.
    The previous implementation was finding the form and field of the
    select element that was modified for each option added or removed.
    
    Since this can happen several times in a row it was not efficient.
    
    This search now happens after all the add/remove have been debounced.
    
    Bug: 832077
    Change-Id: Icf28910459163a4d92eca96e2e4adad65a147cf7
    Reviewed-on: https://chromium-review.googlesource.com/1015322
    Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: Mathieu Perreault <mathp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551719}

--- components/autofill/content/renderer/autofill_agent.h
+++ components/autofill/content/renderer/autofill_agent.h
@@ -111,7 +111,7 @@ class AutofillAgent : public content::RenderFrameObserver,
 
   FormTracker* form_tracker_for_testing() { return &form_tracker_; }
 
-  void SelectWasUpdated(FormData form);
+  void SelectWasUpdated(const blink::WebFormControlElement& element);
 
  protected:
   // blink::WebAutofillClient:
