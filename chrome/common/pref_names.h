f04cd0cac06b43fcc76b4973efff714ce53f4909
Joy Ming <jming@chromium.org>
2018-04-18 22:45:57 +0000

    [Downloads location] Missing SD card error in downloads home.
    Display an error the first time a user is visiting downloads home, has
    removed their SD card, and some files are missing as a result.
    
    This includes the following changes:
    * Creation of a preference to keep track of whether this error has been
    shown/should be shown.
    * When a download is noticed to be removed by the DownloadManagerService,
    also check to see if it was due to external removal.
    * Display the snackbar with DownloadSnackbarController.
    
    Mocks: go/downloads-location-mocks
    
    Screenshot:
    https://drive.google.com/file/d/1H-d9XlxnXEW9ZACRMJvDUvUz5PKAqiQj/view
    
    Bug: 792775
    Change-Id: Ie32075739e57568c3b9c1dea31bfb9204acd3abe
    Reviewed-on: https://chromium-review.googlesource.com/1008625
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Joy Ming <jming@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551865}

--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -467,6 +467,7 @@ extern const char kOpenPdfDownloadInSystemReader[];
 #endif
 #if defined(OS_ANDROID)
 extern const char kPromptForDownloadAndroid[];
+extern const char kShowMissingSdCardErrorAndroid[];
 #endif
 
 extern const char kSaveFileDefaultDirectory[];
