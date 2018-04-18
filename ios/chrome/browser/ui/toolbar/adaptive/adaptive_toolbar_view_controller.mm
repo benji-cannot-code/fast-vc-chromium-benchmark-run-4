bddcf0e46205f36c73389c014a5059348a84cbae
Gauthier Ambard <gambard@chromium.org>
2018-04-18 15:39:33 +0000

    Layout the toolbar when changing the loading state
    This CL force a layout of the toolbar when the loading state change, to
    prevent the button changing from "stop" to "reload" with an animation
    when there is another animation happening at the same time.
    
    Bug: 831763
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I1bb5265c308067626ea00e154f8f18fd791701e5
    Reviewed-on: https://chromium-review.googlesource.com/1016761
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551687}

--- ios/chrome/browser/ui/toolbar/adaptive/adaptive_toolbar_view_controller.mm
+++ ios/chrome/browser/ui/toolbar/adaptive/adaptive_toolbar_view_controller.mm
@@ -110,6 +110,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   self.loading = loading;
   self.view.reloadButton.hiddenInCurrentState = loading;
   self.view.stopButton.hiddenInCurrentState = !loading;
+  [self.view layoutIfNeeded];
+
   if (!loading) {
     [self stopProgressBar];
   } else if (self.view.progressBar.hidden) {
