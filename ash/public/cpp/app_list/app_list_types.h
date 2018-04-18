59b9db4288dbc347ae1bbd9cbec5f2ad41f2d463
Michael Wasserman <msw@chromium.org>
2018-04-18 17:13:01 +0000

    Revert "app_launcher: Fix OEM folder is not created in new user flow."
    This reverts commit 94fac28e13a6b72e679c09b7b534cf2043c0c4b1.
    
    Reason for revert: Broke compile: https://ci.chromium.org/buildbot/chromium.chromiumos/linux-chromeos-dbg/5265
    
    Original change's description:
    > app_launcher: Fix OEM folder is not created in new user flow.
    > 
    > This fixes the issue when OEM folder is created empty after new user
    > flow and OEM items are placed to non-named folder. This also fixes
    > various crashes when trying to manipulate these items.
    > 
    > Bug: 828209
    > Test: Manually
    > Change-Id: Ib07f0ab1d301d1e38a6ad91fca10396e9f58ccf7
    > Reviewed-on: https://chromium-review.googlesource.com/1016024
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Commit-Queue: Yury Khmel <khmel@google.com>
    > Cr-Commit-Position: refs/heads/master@{#551716}
    
    TBR=xiyuan@chromium.org,khmel@google.com
    
    Change-Id: Iaa41f7f11f9992b9d5037b4ae5dd2289aa99b4cb
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 828209
    Reviewed-on: https://chromium-review.googlesource.com/1017261
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Commit-Queue: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551725}

--- ash/public/cpp/app_list/app_list_types.h
+++ ash/public/cpp/app_list/app_list_types.h
@@ -14,9 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
-// Id of OEM folder in app list.
-extern const char kOemFolderId[];
-
 // All possible states of the app list.
 // Note: Do not change the order of these as they are used for metrics.
 enum class AppListState {
