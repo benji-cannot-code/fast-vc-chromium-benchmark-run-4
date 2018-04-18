de8f57f553d4caec5c2d7f607a5142415aac4589
Bo Liu <boliu@chromium.org>
2018-04-18 13:14:38 +0000

    android: Add ChildProcessRanking
    Add ChildProcessRanking class which keeps a ranked list of connections
    for a particular ChildConnectionAllocator. Add unit tests to test basic
    functionality.
    
    This will be used for killing the lowest ranked to free up a service
    slot when all slots are full. For now, just integrate
    ChildProcessRanking into ChildProcessLauncherHelper without using it for
    anything.
    
    Current implementation still relies on stability to for reasonable
    behavior. Ideally in the future connections will be ranked explicitly
    without relying on stability.
    
    BindingManagerImpl and ChildProcessRanking probably should be merged at
    some point in the future as well.
    
    Bug: 693484
    Change-Id: I4401a29f198a63898424d7b78c8ef2b423b23cc9
    Reviewed-on: https://chromium-review.googlesource.com/1013292
    Commit-Queue: Bo <boliu@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Maria Khomenko <mariakhomenko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551650}

--- content/public/android/java/src/org/chromium/content/browser/ChildProcessLauncherHelper.java
+++ content/public/android/java/src/org/chromium/content/browser/ChildProcessLauncherHelper.java
@@ -64,6 +64,7 @@ public class ChildProcessLauncherHelper {
 
     // Allocator used for sandboxed services.
     private static ChildConnectionAllocator sSandboxedChildConnectionAllocator;
+    private static ChildProcessRanking sSandboxedChildConnectionRanking;
 
     // Map from PID to ChildProcessLauncherHelper.
     private static final Map<Integer, ChildProcessLauncherHelper> sLauncherByPid = new HashMap<>();
@@ -81,8 +82,10 @@ public class ChildProcessLauncherHelper {
     // Whether the main application is currently brought to the foreground.
     private static boolean sApplicationInForeground = true;
 
+    // TODO(boliu): Generalize these so they work for all connections, not just sandboxed.
     // Whether the connection is managed by the BindingManager.
     private final boolean mUseBindingManager;
+    private final ChildProcessRanking mRanking;
 
     // Whether the created process should be sandboxed.
     private final boolean mSandboxed;
@@ -128,6 +131,10 @@ public class ChildProcessLauncherHelper {
                     assert pid > 0;
 
                     sLauncherByPid.put(pid, ChildProcessLauncherHelper.this);
+                    if (mRanking != null) {
+                        mRanking.addConnection(connection, false /* foreground */,
+                                1 /* frameDepth */, ChildProcessImportance.MODERATE);
+                    }
 
                     // If the connection fails and pid == 0, the Java-side cleanup was already
                     // handled by DeathCallback. We still have to call back to native for cleanup
@@ -146,6 +153,9 @@ public class ChildProcessLauncherHelper {
                     BindingManager manager = getBindingManager();
                     if (mUseBindingManager && manager != null) {
                         manager.dropRecency(connection);
+                    }
+                    if (mRanking != null) {
+                        mRanking.removeConnection(connection);
                     }
                 }
             };
@@ -354,6 +364,8 @@ public class ChildProcessLauncherHelper {
                         sSandboxedServiceFactoryForTesting);
             }
             sSandboxedChildConnectionAllocator = connectionAllocator;
+            sSandboxedChildConnectionRanking = new ChildProcessRanking(
+                    sSandboxedChildConnectionAllocator.getNumberOfServices());
 
             final ChildConnectionAllocator finalConnectionAllocator = connectionAllocator;
             connectionAllocator.addListener(new ChildConnectionAllocator.Listener() {
@@ -392,6 +404,12 @@ public class ChildProcessLauncherHelper {
                 binderCallback == null ? null : Arrays.asList(binderCallback));
         mProcessType =
                 ContentSwitches.getSwitchValue(commandLine, ContentSwitches.SWITCH_PROCESS_TYPE);
+
+        if (sandboxed) {
+            mRanking = sSandboxedChildConnectionRanking;
+        } else {
+            mRanking = null;
+        }
     }
 
     /**
@@ -428,6 +446,10 @@ public class ChildProcessLauncherHelper {
             boolean boostForPendingViews, @ChildProcessImportance int importance) {
         assert LauncherThread.runningOnLauncherThread();
         assert mLauncher.getPid() == pid;
+        if (getByPid(pid) == null) {
+            // Child already disconnected. Ignore any trailing calls.
+            return;
+        }
 
         ChildProcessConnection connection = mLauncher.getConnection();
         if (ChildProcessCreationParams.getIgnoreVisibilityForImportance()) {
@@ -467,6 +489,11 @@ public class ChildProcessLauncherHelper {
         if (mBoostPriorityForPendingViews && !boostForPendingViews) {
             connection.removeInitialBinding();
         }
+
+        if (mRanking != null) {
+            mRanking.updateConnection(connection, foreground, frameDepth, importance);
+        }
+
         if (mImportance != importance) {
             switch (mImportance) {
                 case ChildProcessImportance.NORMAL:
