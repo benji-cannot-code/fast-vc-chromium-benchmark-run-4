052a35e844f4b946627b59a33445c976cec24609
Vadym Doroshenko <dvadym@chromium.org>
2018-04-18 16:13:10 +0000

    Adding class NewPasswordFormManager.
    This CL adds class NewPasswordFormManager as part of Password Manager
    redesigning go/new-cpm-design-refactoring. Now this class has very
    basic functionality. NewPasswordFormManager is aimed to replace
    PasswordFormManager
    
    Bug: 831123
    Change-Id: Idd5e0e477ce939407c4c85dd49eb19412b098d04
    Reviewed-on: https://chromium-review.googlesource.com/1012838
    Commit-Queue: Vadym Doroshenko <dvadym@chromium.org>
    Reviewed-by: Vaclav Brozek <vabr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551707}

---
+++ components/password_manager/core/browser/new_password_form_manager_unittest.cc
@@ -0,0 +1,53 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/password_manager/core/browser/new_password_form_manager.h"
+
+#include "base/strings/utf_string_conversions.h"
+#include "components/autofill/core/common/form_data.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+using autofill::FormData;
+using autofill::FormFieldData;
+using base::ASCIIToUTF16;
+
+namespace password_manager {
+class NewPasswordFormManagerTest : public testing::Test {
+ public:
+  NewPasswordFormManagerTest() {
+    form_.name = ASCIIToUTF16("sign-in");
+
+    FormFieldData field;
+    field.name = ASCIIToUTF16("firstname");
+    field.form_control_type = "text";
+    form_.fields.push_back(field);
+
+    field.name = ASCIIToUTF16("username");
+    field.form_control_type = "text";
+    form_.fields.push_back(field);
+
+    field.name = ASCIIToUTF16("password");
+    field.form_control_type = "password";
+    form_.fields.push_back(field);
+  }
+
+ protected:
+  FormData form_;
+};
+
+TEST_F(NewPasswordFormManagerTest, DoesManage) {
+  NewPasswordFormManager form_manager(form_);
+  EXPECT_TRUE(form_manager.DoesManage(form_));
+  FormData another_form = form_;
+  another_form.name += ASCIIToUTF16("1");
+  EXPECT_FALSE(form_manager.DoesManage(another_form));
+
+  another_form = form_;
+  another_form.fields[0].name += ASCIIToUTF16("1");
+  EXPECT_FALSE(form_manager.DoesManage(another_form));
+}
+
+}  // namespace  password_manager
--- components/password_manager/core/browser/password_manager.cc
+++ components/password_manager/core/browser/password_manager.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/form_saver_impl.h"
 #include "components/password_manager/core/browser/keychain_migration_status_mac.h"
 #include "components/password_manager/core/browser/log_manager.h"
+#include "components/password_manager/core/browser/new_password_form_manager.h"
 #include "components/password_manager/core/browser/password_autofill_manager.h"
 #include "components/password_manager/core/browser/password_form_manager.h"
 #include "components/password_manager/core/browser/password_manager_client.h"
@@ -448,6 +449,7 @@ void PasswordManager::UpdateFormManagers() {
 
 void PasswordManager::DropFormManagers() {
   pending_login_managers_.clear();
+  form_managers_.clear();
   provisional_save_manager_.reset();
   all_visible_forms_.clear();
 }
@@ -483,6 +485,7 @@ void PasswordManager::RemoveObserver(LoginModelObserver* observer) {
 void PasswordManager::DidNavigateMainFrame() {
   entry_to_check_ = NavigationEntryToCheck::LAST_COMMITTED;
   pending_login_managers_.clear();
+  form_managers_.clear();
 }
 
 void PasswordManager::OnPasswordFormSubmitted(
@@ -571,6 +574,11 @@ void PasswordManager::CreatePendingLoginManagers(
     logger.reset(
         new BrowserSavePasswordProgressLogger(client_->GetLogManager()));
     logger->LogMessage(Logger::STRING_CREATE_LOGIN_MANAGERS_METHOD);
+  }
+
+  if (base::FeatureList::IsEnabled(
+          password_manager::features::kNewPasswordFormParsing)) {
+    CreateFormManagers(forms);
   }
 
   const PasswordForm::Scheme effective_form_scheme =
@@ -669,6 +677,27 @@ void PasswordManager::CreatePendingLoginManagers(
   if (logger) {
     logger->LogNumber(Logger::STRING_NEW_NUMBER_LOGIN_MANAGERS,
                       pending_login_managers_.size());
+  }
+}
+
+void PasswordManager::CreateFormManagers(
+    const std::vector<autofill::PasswordForm>& forms) {
+  // Find new forms.
+  std::vector<const autofill::FormData*> new_forms;
+  for (const autofill::PasswordForm& form : forms) {
+    bool form_manager_exists =
+        std::any_of(form_managers_.begin(), form_managers_.end(),
+                    [&form](const auto& form_manager) {
+                      return form_manager->DoesManage(form.form_data);
+                    });
+    if (!form_manager_exists)
+      new_forms.push_back(&form.form_data);
+  }
+
+  // Create form manager for new forms.
+  for (const autofill::FormData* new_form : new_forms) {
+    form_managers_.push_back(
+        std::make_unique<NewPasswordFormManager>(*new_form));
   }
 }
 
