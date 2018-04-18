ff51f74f4cd9beda3b91a715ce63adb65e20118f
Christopher Lam <calamity@chromium.org>
2018-04-18 08:59:49 +0000

    [MD Bookmarks] Fix right-click showing browser context menu.
    This CL fixes an issue where the right-click of a bookmark item wouldn't show a
    new command menu when a command menu was already open. This was caused by the
    mousedown on the original menu not closing the menu.
    
    When cr-action-menu changed to no longer be a type extension, the event was no
    longer fired by the dropdown itself, and rather a child. This CL changes the
    event expectation so that the menu will close correctly.
    
    Bug: 825119
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I1cd5494c7208b21d55f817303389bb845794458e
    Reviewed-on: https://chromium-review.googlesource.com/1013769
    Commit-Queue: calamity <calamity@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551615}

--- chrome/browser/resources/md_bookmarks/command_manager.js
+++ chrome/browser/resources/md_bookmarks/command_manager.js
@@ -796,7 +796,7 @@ cr.define('bookmarks', function() {
      * @private
      */
     onMenuMousedown_: function(e) {
-      if (e.path[0] != this.$.dropdown.getIfExists())
+      if (e.path[0].tagName != 'DIALOG')
         return;
 
       this.closeCommandMenu();
