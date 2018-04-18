aff315a6ebcf241da746cadf9bc9e599ca210ab1
Ned Nguyen <nednguyen@google.com>
2018-04-18 06:20:22 +0000

    [blink_perf] Explicitly define all blink_perf benchmark's name
    This makes it a lot easier to do code search for blink_perf benchmark
    
    Change-Id: Ib1257088c4eb99d0f61ebb8fc6dc511d748459aa
    Reviewed-on: https://chromium-review.googlesource.com/1013150
    Commit-Queue: Ned Nguyen <nednguyen@google.com>
    Reviewed-by: Emily Hanley <eyaich@chromium.org>
    Reviewed-by: Charlie Andrews <charliea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551594}

--- tools/perf/benchmarks/blink_perf.py
+++ tools/perf/benchmarks/blink_perf.py
@@ -345,10 +345,6 @@ class _BlinkPerfBenchmark(perf_benchmark.PerfBenchmark):
 
   test = _BlinkPerfMeasurement
 
-  @classmethod
-  def Name(cls):
-    return 'blink_perf.' + cls.tag
-
   def CreateStorySet(self, options):
     path = os.path.join(BLINK_PERF_BASE_DIR, self.subdir)
     return CreateStorySetFromPath(path, SKIPPED_FILE)
@@ -358,19 +354,30 @@ class _BlinkPerfBenchmark(perf_benchmark.PerfBenchmark):
                          'yukishiino@chromium.org',
                          'haraken@chromium.org'])
 class BlinkPerfBindings(_BlinkPerfBenchmark):
-  tag = 'bindings'
   subdir = 'bindings'
+
+  @classmethod
+  def Name(cls):
+    return 'blink_perf.bindings'
+
 
 @benchmark.Owner(emails=['futhark@chromium.org'])
 class BlinkPerfCSS(_BlinkPerfBenchmark):
-  tag = 'css'
   subdir = 'css'
+
+  @classmethod
+  def Name(cls):
+    return 'blink_perf.css'
+
 
 
 @benchmark.Owner(emails=['junov@chromium.org'])
 class BlinkPerfCanvas(_BlinkPerfBenchmark):
-  tag = 'canvas'
   subdir = 'canvas'
+
+  @classmethod
+  def Name(cls):
+    return 'blink_perf.canvas'
 
   def CreateStorySet(self, options):
     path = os.path.join(BLINK_PERF_BASE_DIR, self.subdir)
@@ -389,20 +396,30 @@ class BlinkPerfCanvas(_BlinkPerfBenchmark):
                          'yukishiino@chromium.org',
                          'haraken@chromium.org'])
 class BlinkPerfDOM(_BlinkPerfBenchmark):
-  tag = 'dom'
   subdir = 'dom'
+
+  @classmethod
+  def Name(cls):
+    return 'blink_perf.dom'
 
 
 @benchmark.Owner(emails=['hayato@chromium.org'])
 class BlinkPerfEvents(_BlinkPerfBenchmark):
-  tag = 'events'
   subdir = 'events'
+
+  @classmethod
+  def Name(cls):
+    return 'blink_perf.events'
 
 
 @benchmark.Owner(emails=['cblume@chromium.org'])
 class BlinkPerfImageDecoder(_BlinkPerfBenchmark):
   tag = 'image_decoder'
   subdir = 'image_decoder'
+
+  @classmethod
+  def Name(cls):
+    return 'blink_perf.image_decoder'
 
   def SetExtraBrowserOptions(self, options):
     options.AppendExtraBrowserArgs([
@@ -412,14 +429,20 @@ class BlinkPerfImageDecoder(_BlinkPerfBenchmark):
 
 @benchmark.Owner(emails=['eae@chromium.org'])
 class BlinkPerfLayout(_BlinkPerfBenchmark):
-  tag = 'layout'
   subdir = 'layout'
+
+  @classmethod
+  def Name(cls):
+    return 'blink_perf.layout'
 
 
 @benchmark.Owner(emails=['dmurph@chromium.org'])
 class BlinkPerfOWPStorage(_BlinkPerfBenchmark):
-  tag = 'owp_storage'
   subdir = 'owp_storage'
+
+  @classmethod
+  def Name(cls):
+    return 'blink_perf.owp_storage'
 
   # This ensures that all blobs >= 20MB will be transported by files.
   def SetExtraBrowserOptions(self, options):
@@ -433,25 +456,37 @@ class BlinkPerfOWPStorage(_BlinkPerfBenchmark):
 
 @benchmark.Owner(emails=['wangxianzhu@chromium.org'])
 class BlinkPerfPaint(_BlinkPerfBenchmark):
-  tag = 'paint'
   subdir = 'paint'
+
+  @classmethod
+  def Name(cls):
+    return 'blink_perf.paint'
 
 
 @benchmark.Owner(emails=['jbroman@chromium.org',
                          'yukishiino@chromium.org',
                          'haraken@chromium.org'])
 class BlinkPerfParser(_BlinkPerfBenchmark):
-  tag = 'parser'
   subdir = 'parser'
+
+  @classmethod
+  def Name(cls):
+    return 'blink_perf.parser'
 
 
 @benchmark.Owner(emails=['kouhei@chromium.org', 'fs@opera.com'])
 class BlinkPerfSVG(_BlinkPerfBenchmark):
-  tag = 'svg'
   subdir = 'svg'
+
+  @classmethod
+  def Name(cls):
+    return 'blink_perf.svg'
 
 
 @benchmark.Owner(emails=['hayato@chromium.org'])
 class BlinkPerfShadowDOM(_BlinkPerfBenchmark):
-  tag = 'shadow_dom'
   subdir = 'shadow_dom'
+
+  @classmethod
+  def Name(cls):
+    return 'blink_perf.shadow_dom'
