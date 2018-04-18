7543492cac02810f7a2e1e9607164840dc401fbd
Andrew Grieve <agrieve@chromium.org>
2018-04-18 12:21:34 +0000

    Android: Fix crash when trying to select a photo from an <input>
    Bug: 831953
    Change-Id: I9e3c697d25a0754fad18a45f7a0a19c721e54e9c
    Reviewed-on: https://chromium-review.googlesource.com/1011667
    Commit-Queue: agrieve <agrieve@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551640}

--- chrome/android/java/src/org/chromium/chrome/browser/photo_picker/DecoderService.java
+++ chrome/android/java/src/org/chromium/chrome/browser/photo_picker/DecoderService.java
@@ -14,6 +14,7 @@ import android.os.ParcelFileDescriptor;
 import android.os.RemoteException;
 import android.os.SystemClock;
 
+import org.chromium.base.CommandLine;
 import org.chromium.base.Log;
 import org.chromium.base.PathUtils;
 import org.chromium.base.ThreadUtils;
@@ -47,6 +48,12 @@ public class DecoderService extends Service {
 
     @Override
     public void onCreate() {
+        // DecoderService does not require flags, but LibraryLoader.ensureInitialized() checks for
+        // --enable-low-end-device-mode. Rather than forwarding the flags from the browser process,
+        // just assume no flags.
+        if (!CommandLine.isInitialized()) {
+            CommandLine.init(null);
+        }
         try {
             // The decoder service relies on PathUtils.
             ThreadUtils.runOnUiThreadBlocking(() -> {
