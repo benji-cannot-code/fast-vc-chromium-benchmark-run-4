2f212d0db1535c5a477a13c300dc82df1027eeb3
Eugene But <eugenebut@google.com>
2018-04-18 17:14:48 +0000

    Add web::testing::CreateTestNetError for testing error pages.
    This replaces the usage of private web API in the test.
    
    Bug=622485
    
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I848dad4fddf60bc7a5e83f5008b151f143b1f7a7
    Reviewed-on: https://chromium-review.googlesource.com/1014417
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551727}

--- ios/chrome/browser/web/error_page_generator_unittest.mm
+++ ios/chrome/browser/web/error_page_generator_unittest.mm
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/web/error_page_generator.h"
 #include "base/strings/sys_string_conversions.h"
+#import "ios/web/public/test/error_test_util.h"
 #include "ios/web/public/test/web_test.h"
-#include "ios/web/web_state/error_translation_util.h"
 #import "net/base/mac/url_conversions.h"
 #include "net/base/net_errors.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -48,7 +48,7 @@ class ErrorPageGeneratorTest : public web::WebTest {
       NSURLErrorFailingURLStringErrorKey :
           base::SysUTF8ToNSString(errorURL.spec())
     };
-    return web::NetErrorFromError(
+    return web::testing::CreateTestNetError(
         [NSError errorWithDomain:errorDomain code:errorCode userInfo:info]);
   }
 
---
+++ ios/web/public/test/error_test_util.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_WEB_PUBLIC_TEST_ERROR_TEST_UTIL_H_
+#define IOS_WEB_PUBLIC_TEST_ERROR_TEST_UTIL_H_
+
+@class NSError;
+
+namespace web {
+namespace testing {
+
+// Creates Chrome specific error from a regular NSError. Returned error has the
+// same format and structure as errors provided in ios/web callbacks.
+NSError* CreateTestNetError(NSError* error);
+
+}  // namespace testing
+}  // namespace web
+
+#endif  // IOS_WEB_PUBLIC_TEST_ERROR_TEST_UTIL_H_
