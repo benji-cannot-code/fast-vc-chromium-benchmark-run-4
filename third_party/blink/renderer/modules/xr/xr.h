60f8d26fb9f3caa6a7c7dc9a5f36516921064549
Bill Orr <billorr@chromium.org>
2018-04-18 22:06:53 +0000

    WebXR: Do not provide poses to frames without focus
    This change plumbs through focus (both blink/frame and device) to XRSessions, so we
    only call raf callbacks when focused.
    
    BUG=813230
    
    Change-Id: I0aefdce989f6dd60b4cd0acf1a1477fdb2e845b8
    Reviewed-on: https://chromium-review.googlesource.com/1011288
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Commit-Queue: Bill Orr <billorr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551842}

--- third_party/blink/renderer/modules/xr/xr.h
+++ third_party/blink/renderer/modules/xr/xr.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
 #include "third_party/blink/renderer/core/dom/context_lifecycle_observer.h"
 #include "third_party/blink/renderer/core/dom/events/event_target.h"
+#include "third_party/blink/renderer/core/page/focus_changed_observer.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
 #include "third_party/blink/renderer/platform/wtf/forward.h"
 
@@ -22,7 +23,8 @@ class XRDevice;
 
 class XR final : public EventTargetWithInlineData,
                  public ContextLifecycleObserver,
-                 public device::mojom::blink::VRServiceClient {
+                 public device::mojom::blink::VRServiceClient,
+                 public FocusChangedObserver {
   DEFINE_WRAPPERTYPEINFO();
   USING_GARBAGE_COLLECTED_MIXIN(XR);
 
@@ -45,16 +47,18 @@ class XR final : public EventTargetWithInlineData,
 
   // ContextLifecycleObserver overrides.
   void ContextDestroyed(ExecutionContext*) override;
-
-  void Dispose();
-
   void Trace(blink::Visitor*) override;
+
+  // FocusChangedObserver overrides.
+  void FocusedFrameChanged() override;
+  bool IsFrameFocused();
 
  private:
   explicit XR(LocalFrame& frame);
 
   void OnDevicesSynced();
   void ResolveRequestDevice();
+  void Dispose();
 
   bool devices_synced_;
 
