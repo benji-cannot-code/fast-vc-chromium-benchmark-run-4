f0baacb0242012e8bc5c271ed180735e80199842
Simeon Anfinrud <sanfin@chromium.org>
2018-04-18 02:12:33 +0000

    [Buildfix] Fix CastWebContentsSurfaceHelperTest.
    The tests had a hidden dependency on the order that other tests in the
    suite would run. This change fixes the hidden dependency by setting the
    application context in ContextUtils.
    
          -f *CastWebContentsSurfaceHelperTest*
          without patch (fails)
          and with patch (passes)
    
    Bug: 834034
    Test: Run cast_shell_junit_tests with
    Change-Id: I3fe2d4111a6959a58352411ff653cebf2403a437
    Reviewed-on: https://chromium-review.googlesource.com/1015978
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Commit-Queue: Luke Halliwell <halliwell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551557}

--- chromecast/browser/android/junit/src/org/chromium/chromecast/shell/CastWebContentsSurfaceHelperTest.java
+++ chromecast/browser/android/junit/src/org/chromium/chromecast/shell/CastWebContentsSurfaceHelperTest.java
@@ -31,6 +31,7 @@ import org.mockito.MockitoAnnotations;
 import org.robolectric.annotation.Config;
 import org.robolectric.shadows.ShadowLooper;
 
+import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.chromecast.base.Consumer;
 import org.chromium.chromecast.base.Scope;
 import org.chromium.chromecast.base.ScopeFactory;
@@ -38,7 +39,6 @@ import org.chromium.chromecast.shell.CastWebContentsSurfaceHelper.ContentVideoVi
 import org.chromium.chromecast.shell.CastWebContentsSurfaceHelper.StartParams;
 import org.chromium.content.browser.ContentVideoViewEmbedder;
 import org.chromium.content_public.browser.WebContents;
-import org.chromium.testing.local.LocalRobolectricTestRunner;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -46,7 +46,7 @@ import java.util.List;
 /**
  * Tests for CastWebContentsSurfaceHelper.
  */
-@RunWith(LocalRobolectricTestRunner.class)
+@RunWith(BaseRobolectricTestRunner.class)
 @Config(manifest = Config.NONE)
 public class CastWebContentsSurfaceHelperTest {
     private @Mock Activity mActivity;
