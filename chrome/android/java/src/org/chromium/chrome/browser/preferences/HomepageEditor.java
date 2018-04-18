646ab85601e4c8c8deb1bd40ec0a90b0af1df680
Daniel Park <danielpark@chromium.org>
2018-04-18 18:14:44 +0000

    [Home Page Button] Fixes "Open this page" empty field
    Bug: 831404
    Change-Id: I1b0813135c39cfd912e34e229fe98a97d8b89df4
    Reviewed-on: https://chromium-review.googlesource.com/1010417
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Daniel Park <danielpark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551745}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/HomepageEditor.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/HomepageEditor.java
@@ -17,7 +17,6 @@ import android.widget.EditText;
 
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.partnercustomizations.HomepageManager;
-import org.chromium.chrome.browser.partnercustomizations.PartnerBrowserCustomizations;
 import org.chromium.components.url_formatter.UrlFormatter;
 
 /**
@@ -38,9 +37,7 @@ public class HomepageEditor extends Fragment implements TextWatcher {
         View v = inflater.inflate(R.layout.homepage_editor, container, false);
 
         mHomepageUrlEdit = (EditText) v.findViewById(R.id.homepage_url_edit);
-        mHomepageUrlEdit.setText((mHomepageManager.getPrefHomepageUseDefaultUri()
-                ? PartnerBrowserCustomizations.getHomePageUrl()
-                : mHomepageManager.getPrefHomepageCustomUri()));
+        mHomepageUrlEdit.setText(HomepageManager.getHomepageUri());
         mHomepageUrlEdit.addTextChangedListener(this);
         mHomepageUrlEdit.requestFocus();
 
