6df37dcb0f09008770cf9d7737926f61cd0c888e
Darren Shen <shend@chromium.org>
2018-04-18 04:10:22 +0000

    [getMatchedCSSRules] Remove unused API.
    getMatchedCSSRules has been disabled by default since M63. It should be
    ok to delete the corresponding code now. We also remove some unused
    expectation files that we forgot to delete when we disabled the API.
    
    This patch should land in M68.
    
    Bug: 437569
    Change-Id: I2789fb4546380e30849de6d579cd3329cdea8d3f
    Reviewed-on: https://chromium-review.googlesource.com/1009505
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551576}

--- third_party/blink/renderer/core/frame/local_dom_window.h
+++ third_party/blink/renderer/core/frame/local_dom_window.h
@@ -45,7 +45,6 @@ namespace blink {
 
 class ApplicationCache;
 class BarProp;
-class CSSRuleList;
 class CSSStyleDeclaration;
 class CustomElementRegistry;
 class Document;
@@ -221,9 +220,6 @@ class CORE_EXPORT LocalDOMWindow final : public DOMWindow,
 
   // Acessibility Object Model
   ScriptPromise getComputedAccessibleNode(ScriptState*, Element*);
-
-  // WebKit extension
-  CSSRuleList* getMatchedCSSRules(Element*, const String& pseudo_elt) const;
 
   // WebKit animation extensions
   int requestAnimationFrame(V8FrameRequestCallback*);
