c5d5810bdc547a325d663e708e9d1489dea47ebc
Peter Boström <pbos@chromium.org>
2018-04-18 22:35:49 +0000

    Use name as tooltip for avatar toolbar button
    Replaces generic "Current user" string.
    
    Bug: chromium:822070
    Change-Id: I6a5ae45d280f5570406c29a7e01ed9a4cab39c0c
    Reviewed-on: https://chromium-review.googlesource.com/1017592
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551862}

--- chrome/browser/ui/views/profiles/avatar_toolbar_button.h
+++ chrome/browser/ui/views/profiles/avatar_toolbar_button.h
@@ -21,6 +21,7 @@ class AvatarToolbarButton : public ToolbarButton,
   ~AvatarToolbarButton() override;
 
   void UpdateIcon();
+  void UpdateTooltipText();
 
  private:
   // AvatarButtonErrorControllerDelegate:
@@ -28,6 +29,12 @@ class AvatarToolbarButton : public ToolbarButton,
 
   // ProfileAttributesStorage::Observer:
   void OnProfileAvatarChanged(const base::FilePath& profile_path) override;
+  void OnProfileHighResAvatarLoaded(
+      const base::FilePath& profile_path) override;
+  void OnProfileNameChanged(const base::FilePath& profile_path,
+                            const base::string16& old_profile_name) override;
+
+  bool IsIncognito() const;
 
   Profile* const profile_;
 
