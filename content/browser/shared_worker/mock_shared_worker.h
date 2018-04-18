743f0bf575ab3d0b71442a37afc33d1bfaefbbc1
Matt Falkenhagen <falken@chromium.org>
2018-04-18 18:38:40 +0000

    NetworkService: shared worker: Fix race when two workers start simultaneously.
    r550158 introduced a regression when NetworkService/S13nServiceWorker is
    enabled, because it hops to the IO thread during the start worker
    sequence in order to support service worker interception. When two
    clients call `new SharedWorker(url)` at the same time, the expectation
    is that only one worker thread is started, and the clients share the
    worker.  But there was a race where the first request is on the IO thread
    when the second request starts, and the second request doesn't know the
    first request is happening, resulting in the creation of two workers.
    
    The fix is to create SharedWorkerHost on the UI thread before hopping to
    the IO thread, so the second request will notice the host and use it
    instead of creating a new one.
    
    This deflakes http/tests/workers/shared-worker-shared.html when
    run with the NetworkService flag.
    
    Change-Id: Ib4d189c454fc3cdab941af00596b799e2f6dc172
    Bug: 830288
    Reviewed-on: https://chromium-review.googlesource.com/1013440
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551757}

--- content/browser/shared_worker/mock_shared_worker.h
+++ content/browser/shared_worker/mock_shared_worker.h
@@ -105,6 +105,7 @@ class MockSharedWorkerClient : public mojom::SharedWorkerClient {
       std::set<blink::mojom::WebFeature> expected_used_features);
   bool CheckReceivedOnFeatureUsed(blink::mojom::WebFeature expected_feature);
   bool CheckNotReceivedOnFeatureUsed();
+  bool CheckReceivedOnScriptLoadFailed();
 
  private:
   // mojom::SharedWorkerClient methods:
@@ -122,6 +123,7 @@ class MockSharedWorkerClient : public mojom::SharedWorkerClient {
   bool on_feature_used_received_ = false;
   blink::mojom::WebFeature on_feature_used_feature_ =
       blink::mojom::WebFeature();
+  bool on_script_load_failed_ = false;
 
   DISALLOW_COPY_AND_ASSIGN(MockSharedWorkerClient);
 };
