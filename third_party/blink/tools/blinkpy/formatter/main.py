dc776bcdb5e7768e249e40fba89d4333617560ef
Kent Tamura <tkent@chromium.org>
2018-04-19 23:36:22 +0000

    Move WebKit/Tools/Scripts/format-webkitpy to blink/tools/format_blinkpy.py
    Bug: 829697
    Change-Id: Iccf1ebcccad3f7670b65d7e52c7d1481d328b289
    Reviewed-on: https://chromium-review.googlesource.com/1018783
    Reviewed-by: Quinten Yearsley <qyearsley@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552202}

--- third_party/blink/tools/blinkpy/formatter/main.py
+++ third_party/blink/tools/blinkpy/formatter/main.py
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import argparse
 import lib2to3.refactor
 
+from blinkpy.common import add_webkitpy  # pylint: disable=unused-import
 # Put blinkpy/third_party/ in the import path for autopep8 to import pep8.
 from webkitpy.common.path_finder import add_blinkpy_thirdparty_dir_to_sys_path
 add_blinkpy_thirdparty_dir_to_sys_path()
@@ -42,7 +43,7 @@ def main(host=None, args=None):
         options.quoting = None
 
     autopep8_options = _autopep8_options_for_style(options.style)
-    fixers = ['webkitpy.formatter.fix_docstrings']
+    fixers = ['blinkpy.formatter.fix_docstrings']
     fixers.extend(_fixers_for_quoting(options.quoting))
 
     if options.files == ['-']:
@@ -82,8 +83,8 @@ def _autopep8_options_for_style(style):
 def _fixers_for_quoting(quoting):
     return {
         None: [],
-        'double': ['webkitpy.formatter.fix_double_quote_strings'],
-        'single': ['webkitpy.formatter.fix_single_quote_strings'],
+        'double': ['blinkpy.formatter.fix_double_quote_strings'],
+        'single': ['blinkpy.formatter.fix_single_quote_strings'],
     }.get(quoting)
 
 
--- third_party/blink/tools/format_blinkpy.py
+++ third_party/blink/tools/format_blinkpy.py
@@ -1,12 +1,12 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/env vpython
 # Copyright 2014 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
 import sys
 
-from webkitpy.formatter.main import main
+from blinkpy.formatter.main import main
 
 
-if __name__ == '__main__':
-    sys.exit(main())
+sys.exit(main())
