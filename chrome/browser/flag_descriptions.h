dd5fac0c8cffe947a2fe5283d30beaa2bcd7234d
wutao <wutao@chromium.org>
2018-04-19 01:30:58 +0000

    Reland "cros: Add Settings shortcuts search to launcher"
    This is a reland of 1aa4ba15b97289bfd919b06e523e07847dbe4580
    
    Original change's description:
    > cros: Add Settings shortcuts search to launcher
    > 
    > This cl adds the framework to search Settings shortcuts in launcher.
    > Changes:
    > 1. Add feature flag to enable Settings shortcut search in launcher.
    > 2. New class SettingsShortProvider.
    > 3. New class SettingsShortResult.
    > 4. New struct SettingsShortcutMetadata.
    > 
    > Bug: 834049
    > Test: SettingsShortcutProviderTest.Basic and SettingsShortcutResultTest.Basic
    > Change-Id: Ib25f2d18487b77c138b7d550ad06b6f2ac73f6c2
    > Reviewed-on: https://chromium-review.googlesource.com/1016128
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Commit-Queue: Tao Wu <wutao@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#551783}
    
    Bug: 834049
    Change-Id: I486c1520bb9e532f33d4313fd5ea22faf4bdd1fa
    Reviewed-on: https://chromium-review.googlesource.com/1018060
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551914}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1802,6 +1802,9 @@ extern const char kWakeOnPacketsDescription[];
 extern const char kEnableHomeLauncherName[];
 extern const char kEnableHomeLauncherDescription[];
 
+extern const char kEnableSettingsShortcutSearchName[];
+extern const char kEnableSettingsShortcutSearchDescription[];
+
 #endif  // #if defined(OS_CHROMEOS)
 
 // Random platform combinations -----------------------------------------------
