54cbf8374bf9216ba9b3edf6ab4dc44d9c251d5b
Mohamed Amir Yosef <mamir@chromium.org>
2018-04-19 10:20:31 +0000

    [Sync::USS] Ignore permanent nodes in Processor instead of Worker
    Before this CL:
    Worker was filtering all permanent nodes. However permanent are required
    for bookmarks.
    
    After this CL:
    Worker doesn't filter out permanent nodes.
    It is the responsibility of the process to handle them.
    For ClientTagBasedModelTypeProcessor, it will keep flitering them out as
    before
    Later, BookmarksModelTypeProcessor should handle permanent folders.
    
    
    Bug: 516866
    Change-Id: I6f68ef46947cdc9f91c15df0a8af359e0f0749c9
    Reviewed-on: https://chromium-review.googlesource.com/1012846
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551975}

--- components/sync/model_impl/client_tag_based_model_type_processor_unittest.cc
+++ components/sync/model_impl/client_tag_based_model_type_processor_unittest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
+#include "components/sync/base/model_type.h"
 #include "components/sync/base/time.h"
 #include "components/sync/engine/activation_context.h"
 #include "components/sync/model/fake_model_type_sync_bridge.h"
@@ -378,6 +379,20 @@ TEST_F(ClientTagBasedModelTypeProcessorTest, InitialSyncWithTombstone) {
   EXPECT_EQ(0U, db().metadata_count());
   EXPECT_EQ(0U, ProcessorEntityCount());
   EXPECT_EQ(0U, worker()->GetNumPendingCommits());
+}
+
+// Test that an initial sync filters out updates for root nodes in the
+// processor.
+TEST_F(ClientTagBasedModelTypeProcessorTest, InitialSyncWithRootNode) {
+  ModelReadyToSync();
+  OnSyncStarting();
+
+  UpdateResponseDataList update;
+  update.push_back(worker()->GenerateTypeRootUpdateData(ModelType::SESSIONS));
+
+  worker()->UpdateFromServer(update);
+  // Root node update should be filtered out.
+  EXPECT_EQ(0U, ProcessorEntityCount());
 }
 
 // Test that subsequent starts don't call MergeSyncData.
@@ -818,6 +833,16 @@ TEST_F(ClientTagBasedModelTypeProcessorTest, ServerCreateItem) {
   EXPECT_TRUE(metadata.has_creation_time());
   EXPECT_TRUE(metadata.has_modification_time());
   EXPECT_TRUE(metadata.has_specifics_hash());
+}
+
+TEST_F(ClientTagBasedModelTypeProcessorTest, IgnoreUpdatesForRootNodes) {
+  InitializeToReadyState();
+  UpdateResponseDataList update;
+  update.push_back(worker()->GenerateTypeRootUpdateData(ModelType::SESSIONS));
+
+  worker()->UpdateFromServer(update);
+  // Root node update should be filtered out.
+  EXPECT_EQ(0U, ProcessorEntityCount());
 }
 
 // Test that an error applying changes from a server update is
--- components/sync/test/engine/mock_model_type_worker.h
+++ components/sync/test/engine/mock_model_type_worker.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/circular_deque.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
+#include "components/sync/base/model_type.h"
 #include "components/sync/engine/commit_queue.h"
 #include "components/sync/engine/model_type_processor.h"
 #include "components/sync/engine/non_blocking_sync_common.h"
@@ -94,6 +95,10 @@ class MockModelTypeWorker : public CommitQueue {
   UpdateResponseData GenerateUpdateData(
       const std::string& tag_hash,
       const sync_pb::EntitySpecifics& specifics);
+
+  // Returns an UpdateResponseData representing an update received from
+  // the server for a type root node.
+  UpdateResponseData GenerateTypeRootUpdateData(const ModelType& model_type);
 
   // Triggers a server-side deletion of the entity with |tag_hash|; updates
   // server state accordingly.
