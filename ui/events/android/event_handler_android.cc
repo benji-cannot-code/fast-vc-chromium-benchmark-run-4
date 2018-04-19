aab141cf5ba342de3c67752753d115ffb302203b
Jinsuk Kim <jinsukkim@chromium.org>
2018-04-19 06:19:53 +0000

    Android: Defines EventHandlerAndroid
    ui::EventHandlerAndroid is modeled after ui::EventHandler used for
    Aura, to make UI event processing flow similar to how other platforms
    do it.
    
    It's renamed from ViewClient as a starting point. Unlike ViewClient
    that was required to be set at ViewAndroid constructor, it can be
    added dynamically to each ViewAndroid to receive what the impl
    of EventHandlerAndroid needs. It remains set while the view is
    attached to window.
    
    Bug: 622847
    Change-Id: I89d762b24f0257b8453b9fbd98be468f843c51a9
    Reviewed-on: https://chromium-review.googlesource.com/1015448
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551956}

---
+++ ui/events/android/event_handler_android.cc
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/events/android/event_handler_android.h"
+
+namespace ui {
+
+bool EventHandlerAndroid::OnTouchEvent(const MotionEventAndroid& event) {
+  return false;
+}
+
+bool EventHandlerAndroid::OnMouseEvent(const MotionEventAndroid& event) {
+  return false;
+}
+
+bool EventHandlerAndroid::OnMouseWheelEvent(const MotionEventAndroid& event) {
+  return false;
+}
+
+bool EventHandlerAndroid::OnDragEvent(const DragEventAndroid& event) {
+  return false;
+}
+
+bool EventHandlerAndroid::OnGestureEvent(const GestureEventAndroid& event) {
+  return false;
+}
+
+void EventHandlerAndroid::OnSizeChanged() {}
+
+void EventHandlerAndroid::OnPhysicalBackingSizeChanged() {}
+
+}  // namespace ui
