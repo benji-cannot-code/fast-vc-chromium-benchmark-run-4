c141a9a5812075efb6a11d02173f6f77b2b34a55
Alessio Bazzica <alessiob@chromium.org>
2018-04-19 08:33:34 +0000

    Adding BUILD targets for RNNoise and unit tests for KissFFT
    Bug: webrtc:9076
    Change-Id: I411a546e595d413af64c0ed53f1f3f6858145274
    Reviewed-on: https://chromium-review.googlesource.com/1012068
    Commit-Queue: Ale Bzk <alessiob@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Patrik Höglund <phoglund@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551967}

--- third_party/rnnoise/src/rnn_vad_weights.cc
+++ third_party/rnnoise/src/rnn_vad_weights.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#include "third_party/rnnoise/rnn_vad_weights.h"
+#include "third_party/rnnoise/src/rnn_vad_weights.h"
 
 namespace rnnoise {
 
--- third_party/rnnoise/src/rnn_vad_weights.h
+++ third_party/rnnoise/src/rnn_vad_weights.h
@@ -2,6 +2,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_RNNOISE_SRC_RNN_VAD_WEIGHTS_H_
 #define THIRD_PARTY_RNNOISE_SRC_RNN_VAD_WEIGHTS_H_
 
+#include <cstdint>
+
 namespace rnnoise {
 
 extern const int8_t kInputDenseWeights[1008];
