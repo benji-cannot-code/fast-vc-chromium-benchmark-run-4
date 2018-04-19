e1f7207ff4df19e1d4d97606245db3a42c62d5b1
Sylvain Defresne <sdefresne@chromium.org>
2018-04-19 04:15:59 +0000

    Remove unnecessary include of bind_objc_block.h
    The file base/mac/bind_objc_block.h only expose base::BindBlock
    (and base::BindBlockArc) templated method. If the method is not
    used, then the include is unnecessary and can be removed.
    
    Automated using the following command:
    
      for file in $(git grep -l base/mac/bind_objc_block.h); do
        if ! grep -q BindBlock $file; then
          sed -i '' -e '/base\/mac\/bind_objc_block.h/d' $file
        fi
      done
    
    Bug: none
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I2538d4437f439a811e3ee6db3e8146b8db06d885
    Reviewed-on: https://chromium-review.googlesource.com/1018482
    Reviewed-by: Misha Efimov <mef@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551944}

--- components/translate/ios/browser/language_detection_controller_unittest.mm
+++ components/translate/ios/browser/language_detection_controller_unittest.mm
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/callback.h"
-#include "base/mac/bind_objc_block.h"
 #include "base/strings/utf_string_conversions.h"
 #include "components/language/ios/browser/ios_language_detection_tab_helper.h"
 #include "components/prefs/pref_registry_simple.h"
