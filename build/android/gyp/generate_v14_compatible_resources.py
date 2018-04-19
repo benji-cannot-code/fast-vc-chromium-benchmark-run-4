b2a0b5372d0001a94e8dd41e6f956b154d682494
Peter Wen <wnwen@chromium.org>
2018-04-19 17:57:10 +0000

    Supersize: Properly attribute res/ symbols
    Create .info files for resources during jinja processing, aapt2
    compressing, and aapt2 linking, using these .info files to create a
    per-apk *.apk.res.info file that supersize then uses to properly map a
    resource file back to its source file.
    
    There is some complexity with resource overloading and android build
    tools changing the resource file or directory as part of packaging an
    apk. This is handled by a combination of storing renames and parsing
    heuristics.
    
    Bug: 827196
    Change-Id: Ic8243c218791ec048b1563604d93c7b735fdc71c
    Reviewed-on: https://chromium-review.googlesource.com/1014255
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Reviewed-by: agrieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552081}

--- build/android/gyp/generate_v14_compatible_resources.py
+++ build/android/gyp/generate_v14_compatible_resources.py
@@ -262,6 +262,7 @@ def ParseArgs():
   build_utils.CheckOptions(options, parser, required=required_options)
   return options
 
+
 def GenerateV14Resources(res_dir, res_v14_dir):
   for name in os.listdir(res_dir):
     if not os.path.isdir(os.path.join(res_dir, name)):
@@ -307,6 +308,7 @@ def GenerateV14Resources(res_dir, res_v14_dir):
       elif not api_level_qualifier:
         ErrorIfStyleResourceExistsInDir(input_dir)
 
+
 def main():
   options = ParseArgs()
 
@@ -319,6 +321,7 @@ def main():
 
   if options.stamp:
     build_utils.Touch(options.stamp)
+
 
 if __name__ == '__main__':
   sys.exit(main())
