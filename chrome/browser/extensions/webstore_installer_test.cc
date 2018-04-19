6739c9f7662a41e7c1d4cf40b6e69f618302b2d6
Devlin Cronin <rdevlin.cronin@chromium.org>
2018-04-19 01:21:44 +0000

    [Extensions] Update WebstoreInstallerTests to manifest v2
    Update WebstoreInstallerTests to manifest v2 and remove the allowance
    for manifest v1 extensions.
    
    Repackage the crx used in the webstore_inline_install API tests to be
    updated to manifest v2.
    
    Bug: 816679
    Change-Id: Id38616cf30c70d886542d5b4d8112c8831372863
    Reviewed-on: https://chromium-review.googlesource.com/1017582
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551910}

--- chrome/browser/extensions/webstore_installer_test.cc
+++ chrome/browser/extensions/webstore_installer_test.cc
@@ -55,10 +55,6 @@ WebstoreInstallerTest::WebstoreInstallerTest(
 
 WebstoreInstallerTest::~WebstoreInstallerTest() {}
 
-bool WebstoreInstallerTest::ShouldAllowLegacyExtensionManifests() {
-  return true;
-}
-
 void WebstoreInstallerTest::SetUpCommandLine(base::CommandLine* command_line) {
   ExtensionBrowserTest::SetUpCommandLine(command_line);
 
