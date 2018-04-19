f1a4c44226229ac7e8c9b2dd60129a846711567e
Friedrich Horschig <fhorschig@chromium.org>
2018-04-19 18:49:13 +0000

    Revert "Clear password search on X and close it with back button"
    This reverts commit dcb5b1f4e6fa519c5122338007ae81872f4d561b.
    
    Reason for revert:
    Regressed on some devices. Failure reason is not obvious enough for confident immediate fix.
    
    Bug: 834676
    
    Original change's description:
    > Clear password search on X and close it with back button
    > 
    > Right now, clicking the X button in the search will close it.
    > To start a new search, you have to open a new search.
    > That experience isn't very discoverable, so UX developed an improved
    > workflow - details in the bug.
    > 
    > The new flow needs these changes:
    > - the X button _only_ clears the query text field now
    > - the X button is _only_ available when a query was entered
    > - closing the search happens only via the "navigate up" button
    >   in the action bar.
    > - the three-dots overflow menu is hidden while a search is active
    >   (already the case on small screens - now large screens do the same)
    > 
    > Tests cover all of these cases.
    > 
    > Bug: 821755
    > Change-Id: Ieb2e16c096750d66874aa4c4999759629332491b
    > Reviewed-on: https://chromium-review.googlesource.com/966070
    > Reviewed-by: Theresa <twellington@chromium.org>
    > Commit-Queue: Friedrich Horschig <fhorschig@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#550955}
    
    TBR=twellington@chromium.org,fhorschig@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 821755
    Change-Id: I9035e5aed91dca19bcff94e195cf060ef61f2b29
    Reviewed-on: https://chromium-review.googlesource.com/1019150
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Commit-Queue: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552101}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/website/SingleCategoryPreferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/website/SingleCategoryPreferences.java
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.preferences.website;
 
-import static org.chromium.chrome.browser.preferences.SearchUtils.handleSearchNavigation;
-
 import android.content.DialogInterface;
 import android.content.res.Resources;
 import android.os.Build;
@@ -17,7 +15,9 @@ import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceGroup;
 import android.preference.PreferenceScreen;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.SearchView;
 import android.text.format.Formatter;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -25,6 +25,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -44,7 +45,6 @@ import org.chromium.chrome.browser.preferences.ManagedPreferencesUtils;
 import org.chromium.chrome.browser.preferences.PrefServiceBridge;
 import org.chromium.chrome.browser.preferences.PreferenceUtils;
 import org.chromium.chrome.browser.preferences.ProtectedContentResetCredentialConfirmDialogFragment;
-import org.chromium.chrome.browser.preferences.SearchUtils;
 import org.chromium.chrome.browser.preferences.website.Website.StoredDataClearedCallback;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.widget.TintedDrawable;
@@ -72,14 +72,14 @@ public class SingleCategoryPreferences extends PreferenceFragment
 
     // The view to show when the list is empty.
     private TextView mEmptyView;
-    // The item for searching the list of items.
-    private MenuItem mSearchItem;
+    // The view for searching the list of items.
+    private SearchView mSearchView;
     // The clear button displayed in the Storage view.
     private Button mClearButton;
     // The Site Settings Category we are showing.
     private SiteSettingsCategory mCategory;
     // If not blank, represents a substring to use to search for site names.
-    private String mSearch;
+    private String mSearch = "";
     // Whether to group by allowed/blocked list.
     private boolean mGroupByAllowBlock;
     // Whether the Blocked list should be shown expanded.
@@ -133,7 +133,7 @@ public class SingleCategoryPreferences extends PreferenceFragment
             // Find origins matching the current search.
             List<WebsitePreference> websites = new ArrayList<>();
             for (Website site : sites) {
-                if (mSearch == null || mSearch.isEmpty() || site.getTitle().contains(mSearch)) {
+                if (mSearch.isEmpty() || site.getTitle().contains(mSearch)) {
                     websites.add(new WebsitePreference(getActivity(), site, mCategory));
                 }
             }
@@ -379,11 +379,26 @@ public class SingleCategoryPreferences extends PreferenceFragment
         menu.clear();
         inflater.inflate(R.menu.website_preferences_menu, menu);
 
-        mSearchItem = menu.findItem(R.id.search);
-        SearchUtils.initializeSearchView(mSearchItem, mSearch, (query) -> {
-            mSearch = query;
-            getInfoForOrigins();
-        });
+        MenuItem searchItem = menu.findItem(R.id.search);
+        mSearchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        mSearchView.setImeOptions(EditorInfo.IME_FLAG_NO_FULLSCREEN);
+        SearchView.OnQueryTextListener queryTextListener =
+                new SearchView.OnQueryTextListener() {
+                    @Override
+                    public boolean onQueryTextSubmit(String query) {
+                        return true;
+                    }
+
+                    @Override
+                    public boolean onQueryTextChange(String query) {
+                        if (query.equals(mSearch)) return true;
+
+                        mSearch = query;
+                        getInfoForOrigins();
+                        return true;
+                    }
+                };
+        mSearchView.setOnQueryTextListener(queryTextListener);
 
         if (mCategory.showProtectedMediaSites()) {
             // Add a menu item to reset protected media identifier device credentials.
@@ -416,11 +431,6 @@ public class SingleCategoryPreferences extends PreferenceFragment
                     getActivity(), getString(helpContextResId), Profile.getLastUsedProfile(), null);
             return true;
         }
-        if (handleSearchNavigation(item, mSearchItem, mSearch)) {
-            mSearch = null;
-            getInfoForOrigins();
-            return true;
-        }
         return false;
     }
 
@@ -433,11 +443,11 @@ public class SingleCategoryPreferences extends PreferenceFragment
             return false;
         }
 
-        if (mSearch != null) {
+        if (!mSearch.isEmpty()) {
             // Clear out any lingering searches, so that the full list is shown
             // when coming back to this page.
-            mSearch = null;
-            SearchUtils.getSearchView(mSearchItem).setQuery("", false);
+            mSearch = "";
+            mSearchView.setQuery("", false);
         }
 
         if (preference instanceof WebsitePreference) {
