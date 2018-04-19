11939b12816cda9c8dc80c702bfeee0588c32323
Sylvain Defresne <sdefresne@chromium.org>
2018-04-19 16:58:03 +0000

    Remove unnecessary include of bind_objc_block.h
    The file base/mac/bind_objc_block.h only expose base::BindBlock
    (and base::BindBlockArc) templated method. If the method is not
    used, then the include is unnecessary and can be removed.
    
    Automated using the following command:
    
      for file in $(git grep -l base/mac/bind_objc_block.h); do
        if ! grep -q BindBlock $file; then
          sed -i '' -e '/base\/mac\/bind_objc_block.h/d' $file
        fi
      done
    
    Bug: none
    Change-Id: I867f805a9d20ed0cc2b75de5d1a21b4362e8bde3
    Reviewed-on: https://chromium-review.googlesource.com/1018481
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552052}

--- chrome/browser/ui/cocoa/browser/zoom_bubble_controller_unittest.mm
+++ chrome/browser/ui/cocoa/browser/zoom_bubble_controller_unittest.mm
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/cocoa/browser/zoom_bubble_controller.h"
 
-#include "base/mac/bind_objc_block.h"
 #include "base/mac/foundation_util.h"
 #include "base/time/time.h"
 #import "chrome/browser/ui/cocoa/info_bubble_window.h"
--- chrome/browser/ui/cocoa/browser_window_fullscreen_transition.mm
+++ chrome/browser/ui/cocoa/browser_window_fullscreen_transition.mm
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
-#include "base/mac/bind_objc_block.h"
 #include "base/mac/foundation_util.h"
 #include "base/mac/mac_util.h"
 #import "base/mac/sdk_forward_declarations.h"
