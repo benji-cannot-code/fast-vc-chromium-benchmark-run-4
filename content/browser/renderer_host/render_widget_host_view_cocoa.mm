e6f2ccc2ec0c954cba8377286cc592843ab9f7d4
Sylvain Defresne <sdefresne@chromium.org>
2018-04-19 07:46:53 +0000

    Remove unnecessary include of bind_objc_block.h
    The file base/mac/bind_objc_block.h only expose base::BindBlock
    (and base::BindBlockArc) templated method. If the method is not
    used, then the include is unnecessary and can be removed.
    
    Automated using the following command:
    
      for file in $(git grep -l base/mac/bind_objc_block.h); do
        if ! grep -q BindBlock $file; then
          sed -i '' -e '/base\/mac\/bind_objc_block.h/d' $file
        fi
      done
    
    Bug: none
    Change-Id: I553d603036bea2948948238ce120fbaa70d77421
    Reviewed-on: https://chromium-review.googlesource.com/1018483
    Commit-Queue: ccameron <ccameron@chromium.org>
    Reviewed-by: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551965}

--- content/browser/renderer_host/render_widget_host_view_cocoa.mm
+++ content/browser/renderer_host/render_widget_host_view_cocoa.mm
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/debug/crash_logging.h"
-#include "base/mac/bind_objc_block.h"
 #include "base/mac/mac_util.h"
 #include "base/stl_util.h"
 #include "base/strings/sys_string_conversions.h"
