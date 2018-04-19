33e1213dbdb5fd1ad5f188190b056b2cd2373005
Sergey Poromov <poromov@chromium.org>
2018-04-19 17:37:29 +0000

    Track and report force-installed extension load time.
    In order to investigate how long does it take to install all enterprise
    force-installed extensions, the installation is tracked by new
    ForcedExtensionsTracker class which reports the installation time as
    "Extensions.ForceInstalledLoadTime" UMA stats.
    
    BUG=787970
    TEST=Manual, check the stats reporting is triggered.
    
    Change-Id: Ibf7ec768eb7c0a6dd63461b87424e4378ac2edd8
    Reviewed-on: https://chromium-review.googlesource.com/1012117
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Drew Wilson <atwilson@chromium.org>
    Reviewed-by: Gayane Petrosyan <gayane@chromium.org>
    Commit-Queue: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552069}

--- chrome/browser/extensions/extension_service.h
+++ chrome/browser/extensions/extension_service.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string16.h"
 #include "chrome/browser/extensions/blacklist.h"
 #include "chrome/browser/extensions/extension_management.h"
+#include "chrome/browser/extensions/forced_extensions/installation_tracker.h"
 #include "chrome/browser/extensions/install_gate.h"
 #include "chrome/browser/extensions/pending_extension_manager.h"
 #include "chrome/browser/upgrade_observer.h"
@@ -696,6 +697,9 @@ class ExtensionService
 
   // Helper to register and unregister extensions.
   extensions::ExtensionRegistrar extension_registrar_;
+
+  // Tracker of enterprise policy forced installation.
+  extensions::InstallationTracker forced_extensions_tracker_;
 
   using InstallGateRegistry = std::map<extensions::ExtensionPrefs::DelayReason,
                                        extensions::InstallGate*>;
