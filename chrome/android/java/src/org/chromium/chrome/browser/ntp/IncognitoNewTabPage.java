bd45fe65613938173c1a8ed7ee2343f62534c75b
Tibor Goldschwendt <tiborg@chromium.org>
2018-04-19 16:10:20 +0000

    Add param to BasicNativePage ctor to set incognito
    Bug: 832594
    Change-Id: Ide7dfb5409f87517e4c4e1d329bd1f58998d2623
    Reviewed-on: https://chromium-review.googlesource.com/1015842
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552030}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/IncognitoNewTabPage.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/IncognitoNewTabPage.java
@@ -12,7 +12,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
 
-import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.VisibleForTesting;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.BasicNativePage;
@@ -23,8 +22,6 @@ import org.chromium.chrome.browser.compositor.layouts.content.InvalidationAwareT
 import org.chromium.chrome.browser.help.HelpAndFeedback;
 import org.chromium.chrome.browser.ntp.IncognitoNewTabPageView.IncognitoNewTabPageManager;
 import org.chromium.chrome.browser.profiles.Profile;
-import org.chromium.chrome.browser.util.ColorUtils;
-import org.chromium.chrome.browser.util.FeatureUtilities;
 import org.chromium.chrome.browser.vr_shell.VrShellDelegate;
 
 /**
@@ -35,8 +32,6 @@ public class IncognitoNewTabPage
     private Activity mActivity;
 
     private String mTitle;
-    private int mBackgroundColor;
-    private int mThemeColor;
     protected IncognitoNewTabPageView mIncognitoNewTabPageView;
 
     private boolean mIsLoaded;
@@ -54,7 +49,7 @@ public class IncognitoNewTabPage
      * @param activity The activity used to create the new tab page's View.
      */
     public IncognitoNewTabPage(Activity activity, NativePageHost host) {
-        super(activity, host);
+        super(activity, host, true);
     }
 
     @Override
@@ -82,10 +77,6 @@ public class IncognitoNewTabPage
         };
 
         mTitle = activity.getResources().getString(R.string.button_new_tab);
-        mBackgroundColor =
-                ApiCompatibilityUtils.getColor(activity.getResources(), R.color.ntp_bg_incognito);
-        mThemeColor = ColorUtils.getDefaultThemeColor(
-                activity.getResources(), FeatureUtilities.isChromeModernDesignEnabled(), true);
 
         LayoutInflater inflater = LayoutInflater.from(activity);
         mIncognitoNewTabPageView =
@@ -133,16 +124,6 @@ public class IncognitoNewTabPage
     @Override
     public String getTitle() {
         return mTitle;
-    }
-
-    @Override
-    public int getBackgroundColor() {
-        return mBackgroundColor;
-    }
-
-    @Override
-    public int getThemeColor() {
-        return mThemeColor;
     }
 
     @Override
