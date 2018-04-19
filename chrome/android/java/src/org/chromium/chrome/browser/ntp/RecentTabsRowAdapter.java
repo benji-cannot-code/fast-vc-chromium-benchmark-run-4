df03e8c77577dbb66bb9c1e735784209a9daef7b
Daniel Park <danielpark@chromium.org>
2018-04-19 00:01:31 +0000

    Moves common code to ViewUtils
    Creates createDefaultRoundedIconGenerator
        and createRoundedBitmapDrawable
    Replaces most calls to RoundedIconGenerator
        and RoundedBitmapDrawableFactory.create with new methods
    
    Bug: 829550
    Change-Id: I0e9eed22c6428f29130ff4af3e3e49dfefef5b0f
    Reviewed-on: https://chromium-review.googlesource.com/1008622
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Daniel Park <danielpark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551887}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/RecentTabsRowAdapter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/RecentTabsRowAdapter.java
@@ -11,8 +11,6 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
-import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.text.TextUtils;
 import android.util.LruCache;
 import android.view.ContextMenu;
@@ -36,6 +34,7 @@ import org.chromium.chrome.browser.signin.SigninAccessPoint;
 import org.chromium.chrome.browser.signin.SyncPromoView;
 import org.chromium.chrome.browser.util.FeatureUtilities;
 import org.chromium.chrome.browser.util.UrlUtilities;
+import org.chromium.chrome.browser.util.ViewUtils;
 import org.chromium.chrome.browser.widget.RoundedIconGenerator;
 import org.chromium.ui.base.DeviceFormFactor;
 import org.chromium.ui.mojom.WindowOpenDisposition;
@@ -657,13 +656,8 @@ public class RecentTabsRowAdapter extends BaseExpandableListAdapter {
         mDefaultFavicon = ApiCompatibilityUtils.getDrawable(resources, R.drawable.default_favicon);
         mFaviconSize = resources.getDimensionPixelSize(R.dimen.default_favicon_size);
 
-        int cornerRadius = resources.getDimensionPixelSize(R.dimen.default_favicon_corner_radius);
-        int textSize = resources.getDimensionPixelSize(R.dimen.default_favicon_icon_text_size);
-        int iconColor =
-                ApiCompatibilityUtils.getColor(resources, R.color.default_favicon_background_color);
-        mIconGenerator = new RoundedIconGenerator(mFaviconSize, mFaviconSize,
-                FeatureUtilities.isChromeModernDesignEnabled() ? mFaviconSize / 2 : cornerRadius,
-                iconColor, textSize);
+        mIconGenerator = ViewUtils.createDefaultRoundedIconGenerator(
+                FeatureUtilities.isChromeModernDesignEnabled());
 
         RecordHistogram.recordEnumeratedHistogram("HistoryPage.OtherDevicesMenu",
                 OtherSessionsActions.MENU_INITIALIZED, OtherSessionsActions.LIMIT);
@@ -695,11 +689,9 @@ public class RecentTabsRowAdapter extends BaseExpandableListAdapter {
     private Drawable getRoundedFavicon(Bitmap image, int size) {
         // TODO(injae): Move shared code between Bookmarks/History/Downloads/here to ViewUtils.java.
         // Also applies to RoundedIconGenerator. crbug.com/829550
-        RoundedBitmapDrawable roundedIcon = RoundedBitmapDrawableFactory.create(
-                mActivity.getResources(), Bitmap.createScaledBitmap(image, size, size, true));
-        roundedIcon.setCornerRadius(mActivity.getResources().getDimensionPixelSize(
-                R.dimen.default_favicon_corner_radius));
-        return roundedIcon;
+        return ViewUtils.createRoundedBitmapDrawable(
+                Bitmap.createScaledBitmap(image, size, size, true),
+                ViewUtils.DEFAULT_FAVICON_CORNER_RADIUS);
     }
 
     private void loadSyncedFavicon(final ViewHolder viewHolder, final String url) {
--- chrome/android/java/src/org/chromium/chrome/browser/preferences/privacy/ConfirmImportantSitesDialogFragment.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/privacy/ConfirmImportantSitesDialogFragment.java
@@ -17,8 +17,6 @@ import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
-import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -29,7 +27,6 @@ import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.ListView;
 
-import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.CollectionUtil;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.VisibleForTesting;
@@ -38,6 +35,7 @@ import org.chromium.chrome.browser.favicon.IconType;
 import org.chromium.chrome.browser.favicon.LargeIconBridge;
 import org.chromium.chrome.browser.favicon.LargeIconBridge.LargeIconCallback;
 import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.chrome.browser.util.ViewUtils;
 import org.chromium.chrome.browser.widget.RoundedIconGenerator;
 
 import java.util.ArrayList;
@@ -58,8 +56,6 @@ public class ConfirmImportantSitesDialogFragment extends DialogFragment {
     private class ClearBrowsingDataAdapter extends ArrayAdapter<String>
             implements AdapterView.OnItemClickListener {
         private final String[] mDomains;
-        private final String[] mFaviconURLs;
-        private final int mCornerRadius;
         private final int mFaviconSize;
         private RoundedIconGenerator mIconGenerator;
 
@@ -69,12 +65,7 @@ public class ConfirmImportantSitesDialogFragment extends DialogFragment {
             mDomains = domains;
             mFaviconURLs = faviconURLs;
             mFaviconSize = resources.getDimensionPixelSize(R.dimen.default_favicon_size);
-            mCornerRadius = resources.getDimensionPixelSize(R.dimen.default_favicon_corner_radius);
-            int textSize = resources.getDimensionPixelSize(R.dimen.default_favicon_icon_text_size);
-            int iconColor = ApiCompatibilityUtils.getColor(
-                    resources, R.color.default_favicon_background_color);
-            mIconGenerator = new RoundedIconGenerator(
-                    mFaviconSize, mFaviconSize, mCornerRadius, iconColor, textSize);
+            mIconGenerator = ViewUtils.createDefaultRoundedIconGenerator(false);
         }
 
         @Override
@@ -139,11 +130,9 @@ public class ConfirmImportantSitesDialogFragment extends DialogFragment {
                 icon = mIconGenerator.generateIconForUrl(url);
                 return new BitmapDrawable(getResources(), icon);
             } else {
-                RoundedBitmapDrawable roundedIcon =
-                        RoundedBitmapDrawableFactory.create(getResources(),
-                                Bitmap.createScaledBitmap(icon, mFaviconSize, mFaviconSize, false));
-                roundedIcon.setCornerRadius(mCornerRadius);
-                return roundedIcon;
+                return ViewUtils.createRoundedBitmapDrawable(
+                        Bitmap.createScaledBitmap(icon, mFaviconSize, mFaviconSize, false),
+                        ViewUtils.DEFAULT_FAVICON_CORNER_RADIUS);
             }
         }
     }
