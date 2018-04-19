fe83c488a17273fd96a795845dd5cc181c6bced5
Antoine Labour <piman@chromium.org>
2018-04-19 18:11:59 +0000

    Remove unneeded functionality in LayerTreeResourceProvider
    At this point, viz::internal::Resource in LayerTreeResourceProvider is
    only used for tests, so a lot of support is not needed. Many tests are
    redundant with corresponding ImportedResource functionality, or only
    test Resource support itself, so remove those.
    
    Among others:
    1- create and allocate the texture on Create*, and generate the mailbox
       there. This avoids transient states that are not useful. Note, this
       removes ETC1 support, which is not used.
    2- remove the ScopedWriteLock*
    3- remove support for mipmaps
    4- remove many calls to CreateForTesting / AllocateForTesting /
       CopyToResource in remaining tests, which were only to trigger
       creation/allocation, now covered by (1)
    5- remove unused ResourceTextureHint flags.
    
    Bug: None
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I1a27edfd676e0a4e857c2e9d3cabb00b4e1b45b7
    Reviewed-on: https://chromium-review.googlesource.com/1018480
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552086}

--- components/viz/common/resources/resource_texture_hint.h
+++ components/viz/common/resources/resource_texture_hint.h
@@ -13,9 +13,6 @@ namespace viz {
 struct ResourceTextureHint {
   enum {
     kDefault = 0,
-    kMipmap = 1 << 0,
-    kFramebuffer = 1 << 1,
-    kOverlay = 1 << 2,
   };
 
   ResourceTextureHint() : value_(kDefault) {}
