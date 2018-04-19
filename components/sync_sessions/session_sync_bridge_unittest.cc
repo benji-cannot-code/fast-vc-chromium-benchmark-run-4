b1f5003666ef5b7dbf7673ba259fa87d4c5bab04
Mikel Astiz <mastiz@chromium.org>
2018-04-19 11:13:20 +0000

    Notify UI of foreign session changes
    The bridge is expected to exercise the callback that was provided in the
    constructor when foreign sessions change, so it gets propagated to the
    UI and other observers.
    
    This simple implementation sometimes overtriggers (e.g. if the local tab
    was attempted to be deleted) but it doesn't seem worth adding extra
    complexity to prevent this from happening.
    
    Bug: 681921
    Change-Id: I5f66a46db5e84ee90819367cc915fb79c2b1fb07
    Reviewed-on: https://chromium-review.googlesource.com/1016645
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551977}

--- components/sync_sessions/session_sync_bridge_unittest.cc
+++ components/sync_sessions/session_sync_bridge_unittest.cc
@@ -147,7 +147,7 @@ class SessionSyncBridgeTest : public ::testing::Test {
         &mock_device_info_provider_,
         /*store_factory=*/
         syncer::ModelTypeStoreTestUtil::FactoryForForwardingStore(store_.get()),
-        mock_sessions_updated_callback_.Get(),
+        mock_foreign_sessions_updated_callback_.Get(),
         mock_processor_.CreateForwardingProcessor());
     real_processor_ =
         std::make_unique<syncer::ClientTagBasedModelTypeProcessor>(
@@ -259,6 +259,11 @@ class SessionSyncBridgeTest : public ::testing::Test {
     return real_processor_.get();
   }
 
+  base::MockCallback<base::RepeatingClosure>&
+  mock_foreign_sessions_updated_callback() {
+    return mock_foreign_sessions_updated_callback_;
+  }
+
  private:
   base::MessageLoop message_loop_;
   const std::unique_ptr<syncer::ModelTypeStore> store_;
@@ -268,7 +273,8 @@ class SessionSyncBridgeTest : public ::testing::Test {
   testing::NiceMock<MockSessionSyncPrefs> mock_sync_prefs_;
   syncer::LocalDeviceInfoProviderMock mock_device_info_provider_;
   testing::NiceMock<MockModelTypeChangeProcessor> mock_processor_;
-  base::MockCallback<base::RepeatingClosure> mock_sessions_updated_callback_;
+  testing::NiceMock<base::MockCallback<base::RepeatingClosure>>
+      mock_foreign_sessions_updated_callback_;
   TestSyncedWindowDelegatesGetter window_getter_;
   FaviconCache favicon_cache_;
 
@@ -403,6 +409,7 @@ TEST_F(SessionSyncBridgeTest, ShouldReportLocalTabCreation) {
   StartSyncing();
 
   ASSERT_THAT(GetAllData(), SizeIs(2));
+  EXPECT_CALL(mock_foreign_sessions_updated_callback(), Run()).Times(0);
 
   // Expectations for the processor.
   std::string header_storage_key;
@@ -594,7 +601,7 @@ TEST_F(SessionSyncBridgeTest, ShouldDisableSyncAndReenable) {
 
 // Starting sync with no local data should just store the foreign entities in
 // the store and expose them via OpenTabsUIDelegate.
-TEST_F(SessionSyncBridgeTest, MergeWithoutLocal) {
+TEST_F(SessionSyncBridgeTest, ShouldMergForeignSession) {
   const std::string kForeignSessionTag = "foreignsessiontag";
   const std::string kForeignClientName = "Foreign Client Name";
   const int kForeignWindowId = 2000001;
@@ -629,6 +636,7 @@ TEST_F(SessionSyncBridgeTest, MergeWithoutLocal) {
       mock_processor(),
       DoPut(_, EntityDataHasSpecifics(MatchesHeader(kLocalSessionTag, _, _)),
             _));
+  EXPECT_CALL(mock_foreign_sessions_updated_callback(), Run());
 
   StartSyncing({foreign_header, foreign_tab});
 
@@ -692,6 +700,8 @@ TEST_F(SessionSyncBridgeTest, ShouldHandleRemoteDeletion) {
   syncer::UpdateResponseData deletion;
   deletion.entity = CreateTombstone(SessionStore::GetClientTag(foreign_header));
   deletion.response_version = 2;
+
+  EXPECT_CALL(mock_foreign_sessions_updated_callback(), Run());
   real_processor()->OnUpdateReceived(state, {deletion});
 
   foreign_session_tab = nullptr;
