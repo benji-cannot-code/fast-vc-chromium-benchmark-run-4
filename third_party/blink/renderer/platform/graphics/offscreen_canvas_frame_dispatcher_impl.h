59cb9130e41406daaf0c7e1a9a51f441c028d02b
danakj <danakj@chromium.org>
2018-04-19 19:08:26 +0000

    Register SharedMemory through CompositorFrameSink for OffscreenCanavas
    This converts from using the SharedBitmapManager, which registers
    shared memory through its own parallel channel, to allocating
    SharedMemory directly and reporting it to the CompositorFrameSink.
    
    The SharedBitmapManager path was racey with the submission of
    CompositorFrames, unless sequence numbers were used (they were not
    in this code), and required the display compositor to live in the
    browser process.
    
    With this change, OffscreenCanvas will work with viz software
    compositing.
    
    R=kylechar@chromium.org
    
    Bug: 730660
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I0daa654112e9834a325754ade37d19325793e01e
    Reviewed-on: https://chromium-review.googlesource.com/1017316
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552106}

--- third_party/blink/renderer/platform/graphics/offscreen_canvas_frame_dispatcher_impl.h
+++ third_party/blink/renderer/platform/graphics/offscreen_canvas_frame_dispatcher_impl.h
@@ -83,9 +83,6 @@ class PLATFORM_EXPORT OffscreenCanvasFrameDispatcherImpl
 
   void SetNeedsBeginFrameInternal();
 
-  std::unique_ptr<OffscreenCanvasResourceProvider>
-      offscreen_canvas_resource_provider_;
-
   bool VerifyImageSize(const IntSize);
   void PostImageToPlaceholderIfNotBlocked(scoped_refptr<StaticBitmapImage>,
                                           unsigned resource_id);
@@ -95,6 +92,8 @@ class PLATFORM_EXPORT OffscreenCanvasFrameDispatcherImpl
 
   viz::mojom::blink::CompositorFrameSinkPtr sink_;
   mojo::Binding<viz::mojom::blink::CompositorFrameSinkClient> binding_;
+  std::unique_ptr<OffscreenCanvasResourceProvider>
+      offscreen_canvas_resource_provider_;
 
   int placeholder_canvas_id_;
 
