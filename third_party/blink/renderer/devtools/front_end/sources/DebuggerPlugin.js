4124bca34d6097f619789fb610faef5be9778409
Andrey Lushnikov <lushnikov@chromium.org>
2018-04-19 17:40:04 +0000

    DevTools: fix null-pointer exception with sourcemap infobar
    Ther sourcemap infobar might never be created if user has clicked
    "never show" button.
    
    R=dgozman, einbinder
    
    Change-Id: I5d494ca218bb3b793a0c734d5fcf92549821749d
    Reviewed-on: https://chromium-review.googlesource.com/1018530
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Andrey Lushnikov <lushnikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552072}

--- third_party/blink/renderer/devtools/front_end/sources/DebuggerPlugin.js
+++ third_party/blink/renderer/devtools/front_end/sources/DebuggerPlugin.js
@@ -1413,6 +1413,8 @@ Sources.DebuggerPlugin = class extends Sources.UISourceCodeFrame.Plugin {
     this._sourceMapInfobar = UI.Infobar.create(
         UI.Infobar.Type.Info, Common.UIString('Source Map detected.'),
         Common.settings.createSetting('sourceMapInfobarDisabled', false));
+    if (!this._sourceMapInfobar)
+      return;
     this._sourceMapInfobar.createDetailsRowMessage(Common.UIString(
         'Associated files should be added to the file tree. You can debug these resolved source files as regular JavaScript files.'));
     this._sourceMapInfobar.createDetailsRowMessage(Common.UIString(
