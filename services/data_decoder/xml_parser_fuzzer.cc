9fc2a3186f9587193e84b0c9d9b8d99e01043eab
Oksana Zhuravlova <oksamyt@chromium.org>
2018-04-19 00:28:15 +0000

    Revert "Convert xml_parser.mojom to use mojo/public/mojom/base/values.mojom"
    This reverts commit 4f376c3e7f0c51702cbf28c9ae0550907a3ab5e0.
    
    Reason for revert: Bug: 834305 (crash report)
    
    Original change's description:
    > Convert xml_parser.mojom to use mojo/public/mojom/base/values.mojom
    > 
    > This change also converts the minimum necessary amount of C++ code to pass
    > objects of base::Value by value instead of std::unique_ptr.
    > 
    > Bug: 646113,799482
    > Change-Id: I86dcd6b4cfbb061942cb73f6ffddcf9e712f355f
    > Reviewed-on: https://chromium-review.googlesource.com/1012931
    > Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Jay Civelli <jcivelli@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#551470}
    
    TBR=dcheng@chromium.org,jcivelli@chromium.org,oksamyt@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 646113, 799482
    Change-Id: I91f61145eebf97b91d19453c968adb6b9210b641
    Reviewed-on: https://chromium-review.googlesource.com/1018040
    Reviewed-by: Oksana Zhuravlova <oksamyt@chromium.org>
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551896}

--- services/data_decoder/xml_parser_fuzzer.cc
+++ services/data_decoder/xml_parser_fuzzer.cc
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 void OnParseXml(base::Closure quit_loop,
-                base::Optional<base::Value> value,
+                std::unique_ptr<base::Value> value,
                 const base::Optional<std::string>& error) {
   std::move(quit_loop).Run();
 }
--- services/data_decoder/xml_parser_unittest.cc
+++ services/data_decoder/xml_parser_unittest.cc
@@ -19,11 +19,9 @@ namespace {
 
 void TestParseXmlCallback(std::unique_ptr<base::Value>* value_out,
                           base::Optional<std::string>* error_out,
-                          base::Optional<base::Value> value,
+                          std::unique_ptr<base::Value> value,
                           const base::Optional<std::string>& error) {
-  std::unique_ptr<base::Value> value_ptr =
-      value ? base::Value::ToUniquePtrValue(std::move(value.value())) : nullptr;
-  *value_out = std::move(value_ptr);
+  *value_out = std::move(value);
   *error_out = error;
 }
 
