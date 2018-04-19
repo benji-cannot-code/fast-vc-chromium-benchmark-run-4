4f45aaa6bb46ad0609601c89e29d6331af400a2c
Sebastien Marchand <sebmarchand@chromium.org>
2018-04-19 17:35:45 +0000

    Add the SiteCharacteristicsDataReader class.
    Bug: 773382
    Change-Id: Ic0f82d85a2d92c194f495d3f3b244af8b4725236
    Reviewed-on: https://chromium-review.googlesource.com/1008168
    Commit-Queue: Sébastien Marchand <sebmarchand@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552067}

---
+++ chrome/browser/resource_coordinator/local_site_characteristics_data_reader.cc
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/resource_coordinator/local_site_characteristics_data_reader.h"
+
+#include "chrome/browser/resource_coordinator/local_site_characteristics_data_impl.h"
+
+namespace resource_coordinator {
+
+LocalSiteCharacteristicsDataReader::LocalSiteCharacteristicsDataReader(
+    scoped_refptr<internal::LocalSiteCharacteristicsDataImpl> impl)
+    : impl_(std::move(impl)) {}
+
+LocalSiteCharacteristicsDataReader::~LocalSiteCharacteristicsDataReader() {}
+
+SiteFeatureUsage
+LocalSiteCharacteristicsDataReader::UpdatesFaviconInBackground() const {
+  return impl_->UpdatesFaviconInBackground();
+}
+
+SiteFeatureUsage LocalSiteCharacteristicsDataReader::UpdatesTitleInBackground()
+    const {
+  return impl_->UpdatesTitleInBackground();
+}
+
+SiteFeatureUsage LocalSiteCharacteristicsDataReader::UsesAudioInBackground()
+    const {
+  return impl_->UsesAudioInBackground();
+}
+
+SiteFeatureUsage
+LocalSiteCharacteristicsDataReader::UsesNotificationsInBackground() const {
+  return impl_->UsesNotificationsInBackground();
+}
+
+}  // namespace resource_coordinator
