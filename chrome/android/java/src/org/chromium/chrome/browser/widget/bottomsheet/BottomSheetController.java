928395d779e0b10b34b595876f1ca07d793ed79a
Matthew Jones <mdjones@chromium.org>
2018-04-19 19:34:45 +0000

    Don't close EoC sheet until a new tab is selected
    Instead of listening to the show event for a tab to close the bottom
    sheet, this change waits for a new tab to be selected. This allows the
    sheet to remain open when the activity is paused and resumed.
    
    Bug: 832304
    Change-Id: Ifd443c5321f67129792f1a3604a3efea4de2ffca
    Reviewed-on: https://chromium-review.googlesource.com/1015771
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552117}

--- chrome/android/java/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheetController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheetController.java
@@ -21,7 +21,9 @@ import org.chromium.chrome.browser.contextualsearch.ContextualSearchObserver;
 import org.chromium.chrome.browser.gsa.GSAContextDisplaySelection;
 import org.chromium.chrome.browser.snackbar.SnackbarManager;
 import org.chromium.chrome.browser.tab.Tab;
+import org.chromium.chrome.browser.tabmodel.EmptyTabModelObserver;
 import org.chromium.chrome.browser.tabmodel.TabModel;
+import org.chromium.chrome.browser.tabmodel.TabModelObserver;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 import org.chromium.chrome.browser.tabmodel.TabModelSelectorObserver;
 import org.chromium.chrome.browser.tabmodel.TabModelSelectorTabObserver;
@@ -100,14 +102,15 @@ public class BottomSheetController implements ApplicationStatus.ActivityStateLis
         // Watch for navigation and tab switching that close the sheet.
         new TabModelSelectorTabObserver(tabModelSelector) {
             @Override
-            public void onShown(Tab tab) {
+            public void onPageLoadStarted(Tab tab, String url) {
                 if (tab != tabModelSelector.getCurrentTab()) return;
                 clearRequestsAndHide();
             }
+        };
 
+        final TabModelObserver tabSelectionObserver = new EmptyTabModelObserver() {
             @Override
-            public void onPageLoadStarted(Tab tab, String url) {
-                if (tab != tabModelSelector.getCurrentTab()) return;
+            public void didSelectTab(Tab tab, TabModel.TabSelectionType type, int lastId) {
                 clearRequestsAndHide();
             }
         };
@@ -121,6 +124,8 @@ public class BottomSheetController implements ApplicationStatus.ActivityStateLis
 
             @Override
             public void onTabModelSelected(TabModel newModel, TabModel oldModel) {
+                if (oldModel != null) oldModel.removeObserver(tabSelectionObserver);
+                newModel.addObserver(tabSelectionObserver);
                 clearRequestsAndHide();
             }
 
