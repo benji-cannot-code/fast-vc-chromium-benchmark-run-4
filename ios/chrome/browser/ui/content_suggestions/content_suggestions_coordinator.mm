a1e9be4fdf4b1f15cd94fbe8244dab49d56a2569
Justin Cohen <justincohen@google.com>
2018-04-19 19:21:18 +0000

    [ios] More NTP test fixes.
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Ie9bbfd3f8b9af31df042735e1cfbbeca2c6a7c95
    Reviewed-on: https://chromium-review.googlesource.com/1014736
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552112}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_coordinator.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_coordinator.mm
@@ -232,6 +232,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   self.NTPMediator = nil;
   self.contentSuggestionsMediator = nil;
   self.headerController = nil;
+  [self.primaryToolbarMediator disconnect];
+  self.primaryToolbarMediator = nil;
   _visible = NO;
 }
 
