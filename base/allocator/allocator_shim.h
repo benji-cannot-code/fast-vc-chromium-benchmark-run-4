8101f1d3950ec807407a21bced05e3a47ff5575f
Mirko Bonadei <mbonadei@chromium.org>
2018-04-19 17:53:50 +0000

    Fixing typo in allocator_shim.h documentation.
    Bug: None
    Change-Id: I91c5141b86826badad4d61e26c2f0ef0acf37e35
    No-Try: True
    Reviewed-on: https://chromium-review.googlesource.com/1018105
    Commit-Queue: Mirko Bonadei <mbonadei@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552079}

--- base/allocator/allocator_shim.h
+++ base/allocator/allocator_shim.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base {
 namespace allocator {
 
-// Allocator Shim API. Allows to to:
+// Allocator Shim API. Allows to:
 //  - Configure the behavior of the allocator (what to do on OOM failures).
 //  - Install new hooks (AllocatorDispatch) in the allocator chain.
 
