8de881c5c5ea60e87a0fd4b83c9bcf2a592b712b
James Wallace-Lee <jamwalla@chromium.org>
2018-04-20 16:58:46 +0000

    Deflake LayerTreeHostTestEarlyDamageCheckStops
    Deflake LayerTreeHostTestEarlyDamageCheckStops by calling
    PostSetNeedsCommitToMainThread in each frame. The failed on tsan builds
    because PostSetneedsCommitToMainThread was only called after a frame
    invalidated, and so the test would time out halfway through when an
    invalidate was skipped and no new BeginFrame occurred.
    
    Bug: 817973
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ia6469126b05da939eaa58b5fbc73558ebf686786
    Reviewed-on: https://chromium-review.googlesource.com/1020363
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: James Wallace-Lee <jamwalla@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552358}

--- cc/trees/layer_tree_host_unittest.cc
+++ cc/trees/layer_tree_host_unittest.cc
@@ -1232,9 +1232,12 @@ class LayerTreeHostTestEarlyDamageCheckStops : public LayerTreeHostTest {
     }
   }
 
+  void DidActivateTreeOnThread(LayerTreeHostImpl* impl) override {
+    PostSetNeedsCommitToMainThread();
+  }
+
   void DidInvalidateLayerTreeFrameSink(LayerTreeHostImpl* impl) override {
     int frame_number = impl->active_tree()->source_frame_number();
-
     // Frames 0 and 1 invalidate because the early damage check is not enabled
     // during this setup. But frames 1 and 2 are not damaged, so the early
     // check should prevent frame 2 from invalidating.
@@ -1251,8 +1254,6 @@ class LayerTreeHostTestEarlyDamageCheckStops : public LayerTreeHostTest {
       EndTest();
       return;
     }
-
-    PostSetNeedsCommitToMainThread();
   }
 
   void AfterTest() override {
@@ -1270,9 +1271,7 @@ class LayerTreeHostTestEarlyDamageCheckStops : public LayerTreeHostTest {
 
 // This behavior is specific to Android WebView, which only uses
 // multi-threaded compositor.
-// Flaky on Win7 Tests (dbg)(1). https://crbug.com/813578
-// Flaky on linux_chromium_tsan_rel_ng. https://crbug.com/822473
-// MULTI_THREAD_TEST_F(LayerTreeHostTestEarlyDamageCheckStops);
+MULTI_THREAD_TEST_F(LayerTreeHostTestEarlyDamageCheckStops);
 
 // Verify CanDraw() is false until first commit.
 class LayerTreeHostTestCantDrawBeforeCommit : public LayerTreeHostTest {
