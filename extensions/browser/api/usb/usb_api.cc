99096ae933194f81319ac21bbe730f9f42f51786
Devlin Cronin <rdevlin.cronin@chromium.org>
2018-04-20 03:21:55 +0000

    [Extensions] Remove GetAssociatedWebContents() calls from usb API
    The ExtensionFunction::GetAssociatedWebContents() method is
    unpredictable and can return surprising values in certain circumstances.
    It is deprecated and should be removed to ensure that callers are using
    the WebContents they expect.
    
    Update the USB API to use GetSenderWebContents() instead.
    
    Bug: 461394
    
    Change-Id: I284a4efc98d878fff0ad48714177467004510d6b
    Reviewed-on: https://chromium-review.googlesource.com/1018532
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552250}

--- extensions/browser/api/usb/usb_api.cc
+++ extensions/browser/api/usb/usb_api.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/api/extensions_api_client.h"
 #include "extensions/browser/api/usb/usb_device_resource.h"
 #include "extensions/browser/api/usb/usb_guid_map.h"
+#include "extensions/browser/extension_function_constants.h"
 #include "extensions/browser/extension_system.h"
 #include "extensions/common/api/usb.h"
 #include "extensions/common/permissions/permissions_data.h"
@@ -669,8 +670,14 @@ ExtensionFunction::ResponseAction UsbGetUserSelectedDevicesFunction::Run() {
       filters.push_back(ConvertDeviceFilter(filter));
   }
 
-  prompt_ = ExtensionsAPIClient::Get()->CreateDevicePermissionsPrompt(
-      GetAssociatedWebContentsDeprecated());
+  content::WebContents* web_contents = GetSenderWebContents();
+  if (!web_contents) {
+    return RespondNow(
+        Error(function_constants::kCouldNotFindSenderWebContents));
+  }
+
+  prompt_ =
+      ExtensionsAPIClient::Get()->CreateDevicePermissionsPrompt(web_contents);
   if (!prompt_) {
     return RespondNow(Error(kErrorNotSupported));
   }
---
+++ extensions/browser/extension_function_constants.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef EXTENSIONS_BROWSER_EXTENSION_FUNCTION_CONSTANTS_H_
+#define EXTENSIONS_BROWSER_EXTENSION_FUNCTION_CONSTANTS_H_
+
+namespace extensions {
+namespace function_constants {
+
+// TODO(devlin): Move ExtensionFunction::kUnknownErrorDoNotUse here.
+extern const char kCouldNotFindSenderWebContents[];
+
+}  // namespace function_constants
+}  // namespace extensions
+
+#endif  // EXTENSIONS_BROWSER_EXTENSION_FUNCTION_CONSTANTS_H_
