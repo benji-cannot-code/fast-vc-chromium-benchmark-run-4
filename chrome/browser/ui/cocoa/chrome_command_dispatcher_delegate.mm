3387c96a11aa19c623ea290b413cb31c3433a358
Elly Fong-Jones <ellyjones@chromium.org>
2018-04-20 23:07:34 +0000

    macviews: handle user key equivalents
    There is special logic in ChromeCommandDispatcherDelegate's
    prePerformKeyEquivalent: method that handles user key equivalents for builtin
    browser commands. This same logic needs to happen in the Views browser, so
    the Views-side logic needs to consult the BrowserView's accelerator table
    to see if the key is bound.
    
    This involves an ungainly bridge, because it's not possible to include
    BrowserView and BrowserWindowController's full definitions at the same time -
    they have conflicting C++ and Objective-C class names in their dependencies.
    
    Bug: 834754
    Change-Id: I67a62064d58cb7dba9c3bd532917ed1f90959340
    Reviewed-on: https://chromium-review.googlesource.com/1022530
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552513}

--- chrome/browser/ui/cocoa/chrome_command_dispatcher_delegate.mm
+++ chrome/browser/ui/cocoa/chrome_command_dispatcher_delegate.mm
@@ -9,9 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/global_keyboard_shortcuts_mac.h"
 #include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/browser_finder.h"
+#import "chrome/browser/ui/cocoa/browser_view_bridge.h"
 #import "chrome/browser/ui/cocoa/browser_window_controller_private.h"
 #import "chrome/browser/ui/cocoa/browser_window_views_mac.h"
 #import "chrome/browser/ui/cocoa/tabs/tab_strip_controller.h"
+#include "chrome/browser/ui/views_mode_controller.h"
 
 namespace {
 
@@ -74,6 +76,16 @@ bool HandleExtraBrowserKeyboardShortcut(NSEvent* event, NSWindow* window) {
                                      CommandForBrowserKeyboardShortcut);
 }
 
+bool IsBrowserKeyboardShortcut(NSEvent* event, NSWindow* window) {
+#if BUILDFLAG(MAC_VIEWS_BROWSER)
+  BrowserView* view = browser_view_bridge::BrowserViewForNativeWindow(window);
+  if (view)
+    return browser_view_bridge::BrowserViewHasAcceleratorForEvent(view, event);
+#endif
+  return CommandForExtraKeyboardShortcut(
+             event, window, CommandForWindowKeyboardShortcut) != -1;
+}
+
 }  // namespace
 
 @implementation ChromeCommandDispatcherDelegate
@@ -117,10 +129,8 @@ bool HandleExtraBrowserKeyboardShortcut(NSEvent* event, NSWindow* window) {
   // special handling here since they happen before normal command dispatch.
   int cmd = MenuCommandForKeyEvent(event);
   if (cmd != -1) {
-    int keyCmd = CommandForExtraKeyboardShortcut(
-        event, window, CommandForWindowKeyboardShortcut);
     Browser* browser = chrome::FindBrowserWithWindow(window);
-    if (keyCmd != -1 && browser) {
+    if (IsBrowserKeyboardShortcut(event, window) && browser) {
       chrome::ExecuteCommand(browser, cmd);
       return YES;
     }
