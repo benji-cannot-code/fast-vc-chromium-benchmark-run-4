3392d751faec8703438f4b90022beff7a29cfd79
Reilly Grant <reillyg@chromium.org>
2018-04-20 19:07:37 +0000

    Revert "Disable testDetectSucceedsOnValidBitmap everywhere"
    This reverts commit bd410037c592f5017a003706031ec0a280c72184.
    
    Reason for revert: This test failure was fixed in r552392.
    
    Original change's description:
    > Disable testDetectSucceedsOnValidBitmap everywhere
    > 
    > Disables testDetectSucceedsOnValidBitmap on all platforms due to
    > consistent bot failures.
    > 
    > TBR=reillyg@chromium.org
    > 
    > BUG=835331
    > 
    > Cq-Include-Trybots: luci.chromium.try:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win10_chromium_x64_rel_ng
    > Change-Id: I0a77529e7054673d4dcab0ce8099679f596ddde6
    > Reviewed-on: https://chromium-review.googlesource.com/1022085
    > Reviewed-by: Donn Denman <donnd@chromium.org>
    > Reviewed-by: Reilly Grant <reillyg@chromium.org>
    > Commit-Queue: Donn Denman <donnd@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#552390}
    
    TBR=donnd@chromium.org,reillyg@chromium.org
    
    Bug: 835331
    Change-Id: Ibb6398faa46ade69464506b393a987698bf88437
    Cq-Include-Trybots: luci.chromium.try:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win10_chromium_x64_rel_ng
    Reviewed-on: https://chromium-review.googlesource.com/1022252
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552421}

--- services/shape_detection/android/javatests/src/org/chromium/shape_detection/TextDetectionImplTest.java
+++ services/shape_detection/android/javatests/src/org/chromium/shape_detection/TextDetectionImplTest.java
@@ -12,7 +12,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.BaseJUnit4ClassRunner;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.gfx.mojom.RectF;
 import org.chromium.shape_detection.mojom.TextDetection;
@@ -54,7 +53,6 @@ public class TextDetectionImplTest {
     }
 
     @Test
-    @DisabledTest(message = "crbug.com/835331")
     @SmallTest
     @Feature({"ShapeDetection"})
     public void testDetectSucceedsOnValidBitmap() {
