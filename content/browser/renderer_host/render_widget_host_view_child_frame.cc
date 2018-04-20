c5b0c0c0822f539be2d812b39779f6297feb70ec
W. James MacLean <wjmaclean@chromium.org>
2018-04-20 06:31:32 +0000

    Improve tool-tip handling for OOPIFs.
    This CL changes to how tooltips are handled for OOPIFs.
    
    It uses CursorManager in the browser to only set the tooltip if
    it is received from a view that currently is controlling the mouse cursor.
    This prevents an iframe element in a parent renderer from overriding the
    tooltip sent by the child's renderer, keeping in mind this can happen
    primarily due to races in the responses to the last MouseMove in the
    parent and the first MouseMove in the child.
    
    Bug: 808074
    Change-Id: I1bede992a1e8daa8025f3f2e392d721dbe701e95
    Reviewed-on: https://chromium-review.googlesource.com/1012268
    Commit-Queue: James MacLean <wjmaclean@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552276}

--- content/browser/renderer_host/render_widget_host_view_child_frame.cc
+++ content/browser/renderer_host/render_widget_host_view_child_frame.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/browser_plugin/browser_plugin_guest.h"
 #include "content/browser/compositor/surface_utils.h"
 #include "content/browser/gpu/compositor_util.h"
+#include "content/browser/renderer_host/cursor_manager.h"
 #include "content/browser/renderer_host/display_util.h"
 #include "content/browser/renderer_host/frame_connector_delegate.h"
 #include "content/browser/renderer_host/input/touch_selection_controller_client_child_frame.h"
@@ -426,7 +427,9 @@ void RenderWidgetHostViewChildFrame::Destroy() {
 
 void RenderWidgetHostViewChildFrame::SetTooltipText(
     const base::string16& tooltip_text) {
-  frame_connector_->GetRootRenderWidgetHostView()->SetTooltipText(tooltip_text);
+  frame_connector_->GetRootRenderWidgetHostView()
+      ->GetCursorManager()
+      ->SetTooltipTextForView(this, tooltip_text);
 }
 
 RenderWidgetHostViewBase* RenderWidgetHostViewChildFrame::GetParentView() {
