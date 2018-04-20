87bc16abb2e9d30402985df51077cb89a33fbc1d
Devlin Cronin <rdevlin.cronin@chromium.org>
2018-04-20 23:25:08 +0000

    [CrOS Extensions] Update Mobile Activation extension to manifest v2
    Manifest v1 extensions will no longer be loaded in M67. We need to
    update this extension to manifest v2.
    
    Update the manifest, and add a trivial browser test to at ensure the
    extension is properly loaded on CrOS (since it looks like we don't have
    any other coverage for this).
    
    Bug: 835392
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I36734dd22b85a93639cf6603841ada430b3bdf50
    Reviewed-on: https://chromium-review.googlesource.com/1022258
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552517}

---
+++ chrome/browser/extensions/chromeos_component_extensions_browsertest.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/extensions/extension_browsertest.h"
+#include "extensions/browser/extension_registry.h"
+
+namespace extensions {
+
+using ComponentExtensionsTest = ExtensionBrowserTest;
+
+// Tests that the mobile_app component extension loads. It would be nice to get
+// rid of this (see https://crbug.com/835391), but for now let's at least make
+// sure it is added correctly.
+IN_PROC_BROWSER_TEST_F(ComponentExtensionsTest, LoadsMobileAppExtension) {
+  constexpr char kMobileActivationExtensionId[] =
+      "iadeocfgjdjdmpenejdbfeaocpbikmab";
+  EXPECT_TRUE(ExtensionRegistry::Get(profile())->enabled_extensions().Contains(
+      kMobileActivationExtensionId));
+}
+
+}  // namespace extensions
