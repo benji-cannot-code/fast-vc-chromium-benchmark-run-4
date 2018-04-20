77cb4fddf96c1dae3bfef18f8bf82163806174c5
Erik Luo <luoe@chromium.org>
2018-04-20 20:36:46 +0000

    DevTools: add frontend support for Runtime.evaluate's timeout option
    Bug: 810176
    Change-Id: I03d9cbe50625ad0dd067bbf19502ede59b412b94
    Reviewed-on: https://chromium-review.googlesource.com/1020524
    Commit-Queue: Erik Luo <luoe@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552458}

---
+++ third_party/WebKit/LayoutTests/http/tests/devtools/runtime/evaluate-timeout.js
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(async function() {
+    await TestRunner.loadModule('sources_test_runner');
+    TestRunner.addResult("Test frontend's timeout support.\n");
+
+    const executionContext = UI.context.flavor(SDK.ExecutionContext);
+    const regularExpression = '1 + 1';
+    const infiniteExpression = 'while (1){}';
+
+    await runtimeTestCase(regularExpression);
+    await runtimeTestCase(infiniteExpression);
+
+    let supports = executionContext.runtimeModel.hasSideEffectSupport();
+    TestRunner.addResult(`\nDoes the runtime support side effect checks? ${supports}`);
+    TestRunner.completeTest();
+
+    async function runtimeTestCase(expression) {
+      TestRunner.addResult(`\nTesting expression ${expression} with timeout: 0`);
+      const result = await executionContext.evaluate({expression, timeout: 0});
+      printDetails(result);
+    }
+
+    function printDetails(result) {
+      if (result.error) {
+        TestRunner.addResult(`Error occurred.`);
+      } else if (result.exceptionDetails) {
+        let exceptionDescription = result.exceptionDetails.exception.description;
+        TestRunner.addResult(`Exception: ${exceptionDescription.split("\n")[0]}`);
+      } else if (result.object) {
+        let objectDescription = result.object.description;
+        TestRunner.addResult(`Result: ${objectDescription}`);
+      }
+    }
+  })();
