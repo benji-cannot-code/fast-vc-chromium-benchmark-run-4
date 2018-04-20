0be31049a20ca27aa7caccc259fef9c9dbf9caca
Tibor Goldschwendt <tiborg@chromium.org>
2018-04-20 15:24:56 +0000

    [vr] Fade in shadow with dialog
    Prior to this change, the shadow popped in while the hosted UI faded in
    causing a weird flicker.
    
    Bug: 829081
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_vr
    Change-Id: I91faf20624219dd88637b27d1322389aafe2aa3d
    Reviewed-on: https://chromium-review.googlesource.com/1020360
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552339}

--- chrome/browser/vr/ui_scene_creator.cc
+++ chrome/browser/vr/ui_scene_creator.cc
@@ -597,11 +597,8 @@ std::unique_ptr<UiElement> CreateHostedUi(
   // suggestion doesn't hide the keyboard. We will probably need to change this
   // when we support the keyboard on native UI elements.
   hosted_ui->set_focusable(false);
-  hosted_ui->SetVisible(false);
-  hosted_ui->set_opacity_when_visible(1.0);
   hosted_ui->set_requires_layout(false);
   hosted_ui->set_corner_radius(kContentCornerRadius);
-  hosted_ui->SetTransitionedProperties({OPACITY});
   hosted_ui->SetTranslate(0, 0, kHostedUiShadowOffset);
   hosted_ui->AddBinding(VR_BIND_FUNC(
       ContentInputDelegatePtr, Model, model, model->hosted_platform_ui.delegate,
@@ -615,7 +612,6 @@ std::unique_ptr<UiElement> CreateHostedUi(
           base::Unretained(model)),
       VR_BIND_LAMBDA(
           [](ContentElement* dialog, const bool& enabled) {
-            dialog->SetVisible(enabled);
             dialog->set_requires_layout(enabled);
             dialog->set_hit_testable(enabled);
           },
@@ -650,6 +646,9 @@ std::unique_ptr<UiElement> CreateHostedUi(
   auto shadow = Create<Shadow>(kNone, kPhaseForeground);
   shadow->SetType(kTypePromptShadow);
   shadow->SetTranslate(0, 0, kHostedUiDepthOffset - kHostedUiShadowOffset);
+  shadow->SetVisible(false);
+  shadow->set_opacity_when_visible(1.0);
+  shadow->SetTransitionedProperties({OPACITY});
   shadow->AddChild(std::move(hosted_ui));
   shadow->AddBinding(std::make_unique<Binding<std::pair<bool, gfx::PointF>>>(
       base::BindRepeating(
@@ -678,6 +677,9 @@ std::unique_ptr<UiElement> CreateHostedUi(
             }
           },
           base::Unretained(shadow.get()))));
+  shadow->AddBinding(VR_BIND_FUNC(bool, Model, model,
+                                  model->hosted_platform_ui.hosted_ui_enabled,
+                                  Shadow, shadow.get(), SetVisible));
 
   auto backplane = Create<InvisibleHitTarget>(name, kPhaseForeground);
   backplane->SetType(kTypeHostedUiBackplane);
