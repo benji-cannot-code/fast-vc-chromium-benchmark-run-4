9d6247d498776f7ce31bbd141b73500d9d3408d4
Xiaohan Wang <xhwang@chromium.org>
2018-04-20 22:04:30 +0000

    media: Support CDM_11 interface
    - Roll src/media/cdm/api/ 57e20546b..163f87884 (1 commit)
        2018-04-17 xhwang cdm: Add CDM_11 interface
    
    - Add CDM_11 support. But since the interface is still in development
      the support is not enabled by default.
    
    - Update CDM_10 implementation to adapt to the interface change that
      RequestCdmProxy() has been removed. CDM_10 support is not enabled
      by default yet.
    
    - Update browser_tests to run tests using CDM_11.
    
    Bug: 733821
    Change-Id: Id0c51468f1d426bb7445398862e8f8ffedad34a7
    Reviewed-on: https://chromium-review.googlesource.com/1020881
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552489}

--- media/cdm/supported_cdm_versions.cc
+++ media/cdm/supported_cdm_versions.cc
@@ -35,6 +35,8 @@ base::Optional<int> GetSupportedCdmInterfaceVersionOverrideFromCommandLine() {
 constexpr bool IsCdmInterfaceVersionEnabledByDefault(int version) {
   switch (version) {
     // Supported versions in decreasing order.
+    case 11:
+      return CdmInterfaceTraits<11>::IsEnabledByDefault();
     case 10:
       return CdmInterfaceTraits<10>::IsEnabledByDefault();
     case 9:
@@ -45,16 +47,6 @@ constexpr bool IsCdmInterfaceVersionEnabledByDefault(int version) {
 }
 
 }  // namespace
-
-bool IsSupportedCdmModuleVersion(int version) {
-  switch (version) {
-    // Latest.
-    case CDM_MODULE_VERSION:
-      return true;
-    default:
-      return false;
-  }
-}
 
 bool IsSupportedAndEnabledCdmInterfaceVersion(int version) {
   if (!IsSupportedCdmInterfaceVersion(version))
