253d1905043f8e43e8cc7c982bf26f70c7a3729e
Donn Denman <donnd@chromium.org>
2018-04-20 20:34:52 +0000

    Revert "[TTS] Fix Tap mojo race condition and update tests."
    This reverts commit 721b860552205c51f38809c900558258ff61ff63.
    
    Reason for revert: Caused a few test failures: testTap and testImageControl.
    
    Original change's description:
    > [TTS] Fix Tap mojo race condition and update tests.
    > 
    > This CL implements a workaround for the race condition between the mojo
    > tap notification and the selection-changed notification.  Now we never enter
    > the SELECTION_CLEARED internal state unless there was a previous
    > selection that got cleared.
    > 
    > Before this CL every tap caused a transition to the SELECTION_CLEARED
    > internal state even when there was no previous selection.  This would usually
    > happen before the Tap notification, but now that's faster due to mojo so it
    > sometimes arrives beforehand.  With this change we ignore selection changes
    > from empty to empty so there's only one notification for most taps. Now we only
    > enter the SELECTION_CLEARED state when it's really needed (in the tap
    > near previous selection use-case).
    > 
    > Also does some minor test cleanup and re-enabling disabled tests that
    > were flaky.
    > 
    > BUG=818897
    > 
    > Change-Id: I269815ad087aabf93332e99a6c7d1208addabe3e
    > Reviewed-on: https://chromium-review.googlesource.com/1018300
    > Commit-Queue: Donn Denman <donnd@chromium.org>
    > Reviewed-by: Theresa <twellington@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#552439}
    
    TBR=donnd@chromium.org,twellington@chromium.org
    
    Change-Id: Ic84b636996d182fce0cf630d1255b142f8f7619b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 818897
    Reviewed-on: https://chromium-review.googlesource.com/1022670
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Commit-Queue: Donn Denman <donnd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552456}

--- chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchInternalStateController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchInternalStateController.java
@@ -5,11 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.contextualsearch;
 
-import android.support.annotation.Nullable;
-
 import org.chromium.base.Log;
 import org.chromium.base.VisibleForTesting;
 import org.chromium.chrome.browser.compositor.bottombar.OverlayPanel.StateChangeReason;
+
+import javax.annotation.Nullable;
 
 /**
  * Controls the internal state of the Contextual Search Manager.
@@ -233,9 +233,11 @@ class ContextualSearchInternalStateController {
                 assert reason != null;
                 mStateHandler.hideContextualSearchUi(reason);
                 break;
+
             case SHOWING_LONGPRESS_SEARCH:
                 mStateHandler.showContextualSearchLongpressUi();
                 break;
+
             case WAITING_FOR_POSSIBLE_TAP_NEAR_PREVIOUS:
                 mStateHandler.waitForPossibleTapNearPrevious();
                 break;
