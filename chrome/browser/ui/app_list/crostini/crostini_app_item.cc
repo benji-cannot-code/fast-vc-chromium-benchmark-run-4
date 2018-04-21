bf44231e5b23696fe48ba2cf544081a9af9b508d
Timothy Loh <timloh@chromium.org>
2018-04-21 05:31:29 +0000

    Add default icon for Crostini apps
    This patch adds the default icon for Crostini apps, in particular, to be
    used on the App List and App Shelf for apps/windows which don't have an
    actual icon. Note that as icons for the shelf and pulling icons from the
    container are not yet implemented, the patch just makes the icon default
    for all app list items except the default terminal.
    
    This also adds code to resize the icons in CrostiniAppItem so that they
    will be 48x48 when dragged, instead of 192x192.
    
    Bug: 820616
    Change-Id: Ifdec8c286073ea429bdd89e26384e89313943ea3
    Reviewed-on: https://chromium-review.googlesource.com/1013842
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552577}

--- chrome/browser/ui/app_list/crostini/crostini_app_item.cc
+++ chrome/browser/ui/app_list/crostini/crostini_app_item.cc
@@ -13,7 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/app_list/app_list_controller_delegate.h"
 #include "chrome/browser/ui/app_list/crostini/crostini_app_context_menu.h"
 #include "chrome/browser/ui/app_list/crostini/crostini_installer_view.h"
+#include "ui/app_list/app_list_constants.h"
 #include "ui/gfx/image/image_skia.h"
+#include "ui/gfx/image/image_skia_operations.h"
 
 // static
 const char CrostiniAppItem::kItemType[] = "CrostiniAppItem";
@@ -25,7 +27,9 @@ CrostiniAppItem::CrostiniAppItem(
     const std::string& name,
     const gfx::ImageSkia* image_skia)
     : ChromeAppListItem(profile, id) {
-  SetIcon(*image_skia);
+  SetIcon(gfx::ImageSkiaOperations::CreateResizedImage(
+      *image_skia, skia::ImageOperations::RESIZE_BEST,
+      gfx::Size(app_list::kTileIconSize, app_list::kTileIconSize)));
   SetName(name);
   if (sync_item && sync_item->item_ordinal.IsValid()) {
     UpdateFromSync(sync_item);
