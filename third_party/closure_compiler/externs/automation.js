25c7e15fb929d4892b9363977651c587d10d5f43
David Tseng <dtseng@chromium.org>
2018-04-21 07:24:39 +0000

    Fix doubled live region output and live status queueing
    - in order for ChromeVox's cache of a live region's descendants to work properly, we need a more reliable signal (other than time).
    This fixes an issue where an individual node update took > 20 MS triggering ChromeVox's cache to be cleared. Signal ChromeVox of when tree updates begin and end to make this well defined.
    
    - force queueing for aria-live="polite".
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I66b999e49c58ac750d094a58049be2bc5288a4ce
    Reviewed-on: https://chromium-review.googlesource.com/1017761
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552584}

--- third_party/closure_compiler/externs/automation.js
+++ third_party/closure_compiler/externs/automation.js
@@ -279,6 +279,7 @@ chrome.automation.TreeChangeType = {
   NODE_CHANGED: 'nodeChanged',
   TEXT_CHANGED: 'textChanged',
   NODE_REMOVED: 'nodeRemoved',
+  SUBTREE_UPDATE_END: 'subtreeUpdateEnd',
 };
 
 /**
