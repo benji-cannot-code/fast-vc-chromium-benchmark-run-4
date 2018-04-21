fcb7ba4874cc64ad28cab72770f79ecaa05c8784
Adrienne Walker <enne@chromium.org>
2018-04-21 01:27:35 +0000

    cc: Serialize SkPaths using the transfer cache
    Bug: 815022
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I0856659a87a07369049902f2a1adb7d82fc4bc4a
    Reviewed-on: https://chromium-review.googlesource.com/1012187
    Commit-Queue: enne <enne@chromium.org>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552542}

--- cc/paint/transfer_cache_entry.cc
+++ cc/paint/transfer_cache_entry.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/paint/color_space_transfer_cache_entry.h"
 #include "cc/paint/image_transfer_cache_entry.h"
 #include "cc/paint/paint_typeface_transfer_cache_entry.h"
+#include "cc/paint/path_transfer_cache_entry.h"
 #include "cc/paint/raw_memory_transfer_cache_entry.h"
 
 namespace cc {
@@ -26,6 +27,8 @@ std::unique_ptr<ServiceTransferCacheEntry> ServiceTransferCacheEntry::Create(
       return std::make_unique<ServicePaintTypefaceTransferCacheEntry>();
     case TransferCacheEntryType::kColorSpace:
       return std::make_unique<ServiceColorSpaceTransferCacheEntry>();
+    case TransferCacheEntryType::kPath:
+      return std::make_unique<ServicePathTransferCacheEntry>();
   }
 
   return nullptr;
