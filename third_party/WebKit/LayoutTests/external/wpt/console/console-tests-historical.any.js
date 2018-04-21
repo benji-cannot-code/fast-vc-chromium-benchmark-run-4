df781854d0e5a91bf838ebb8d7135da5ecb6aa2d
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-04-21 02:27:46 +0000

    Import wpt@0628e2e73489ddd075f273d2555e504e97e28ce6
    Using wpt-import in Chromium 46b9376f4f8247a2ca750cb2a585c4676e299742.
    With Chromium commits locally applied on WPT:
    303956b6ed "Enable WPT tests for the Generic Sensor classes"
    afb472c0d2 "Try to fix crash when terminating a worker while it is XHR-ing to a blob."
    
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/15931
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    kozyatinskiy@chromium.org:
      external/wpt/console
    
    TBR=ajuma
    
    No-Export: true
    Change-Id: I995a2a8aae1ef7e7e9b46be9fd34a3bc0296b6a2
    Reviewed-on: https://chromium-review.googlesource.com/1022992
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552556}

---
+++ third_party/WebKit/LayoutTests/external/wpt/console/console-tests-historical.any.js
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+/**
+ * These tests assert the non-existence of certain
+ * legacy Console methods that are not included in
+ * the specification: http://console.spec.whatwg.org/
+ */
+
+"use strict";
+
+test(() => {
+  assert_equals(console.timeline, undefined, "console.timeline should be undefined");
+}, "'timeline' function should not exist on the console object");
+
+test(() => {
+  assert_equals(console.timelineEnd, undefined, "console.timelineEnd should be undefined");
+}, "'timelineEnd' function should not exist on the console object");
+
+test(() => {
+  assert_equals(console.markTimeline, undefined, "console.markTimeline should be undefined");
+}, "'markTimeline' function should not exist on the console object");
