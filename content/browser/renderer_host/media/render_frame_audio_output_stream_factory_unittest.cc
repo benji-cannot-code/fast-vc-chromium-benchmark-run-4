e53a1beb15a143273148dbdaf733b39a04f8fbc4
Noel Gordon <noel@chromium.org>
2018-04-22 06:15:24 +0000

    Revert "Refactor flow of audio platform errors to renderer."
    This reverts commit 901f4b6391a52e92371668ed3f59c09ad856df26.
    
    Reason for revert: Sequence confusion about which thread a Mojo
    binding_ receives on_connect_error{with_reason} relative to the
    sequence_checker_ thread.
    
    No browser tests covers this case: the sequence_checker_ thread
    and the binding_ connection error thread might not be the same.
    The DCHECK on sequence_checker_ added in this CL can cause test
    failures, but no active tests cover the change, sadly.
    
    Browser tests that could cover this work are disabled for other
    reasons (blanket sheriff flaky test disables done badly, or not
    investigated). The root cause of those test flakes was recently
    investigated, and resolved.
    
    Apologies for the revert, Max. I think we'll come back and land
    this change after some browser test coverage on ChromeOS is re-
    enabled, so you'll have some tests to support your work. Please
    bear with this will we get those tests going again.
    
    Original change's description:
    > Refactor flow of audio platform errors to renderer.
    >
    > Currently, errors from the platform (e.g. device disconnected), are sent directly to the renderer
    > (AudioOutputStreamClient). When moving to the audio service, this becomes suboptimal. It is tricky
    > to signal an error occurring before the ClientPtr is set up, and sometimes we may not want it
    > torn down due to a platform error (in case we want to try some sort of recovery operation such as
    > restarting the audio service). This CL instead signals the error to the browser
    > (AudioOutputStreamObserver), which signals it to the client through the new
    > AudioOutputStreamProviderClient interface. That interface also gets the "stream created" callback
    > as a method rather than a mojo response, so that we will be able to call it multiple times in the
    > future.
    >
    > Bug: 787806,830493
    > Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    > Change-Id: I0fca093e2676d96f0f397c0fb59dd6c31eae1384
    > Reviewed-on: https://chromium-review.googlesource.com/973367
    > Commit-Queue: Max Morin <maxmorin@chromium.org>
    > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    > Reviewed-by: Yuri Wiitala <miu@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#551314}
    
    TBR=dalecurtis@chromium.org,kinuko@chromium.org,miu@chromium.org,maxmorin@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    No-Presubmit: true
    Bug: 787806, 830493
    Change-Id: If6607bac536f46048bdddbd24faf5180aafbd51b
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Reviewed-on: https://chromium-review.googlesource.com/1023570
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552600}

--- content/browser/renderer_host/media/render_frame_audio_output_stream_factory_unittest.cc
+++ content/browser/renderer_host/media/render_frame_audio_output_stream_factory_unittest.cc
@@ -38,12 +38,10 @@ using AudioOutputStreamFactoryRequest =
 using AudioOutputStream = media::mojom::AudioOutputStream;
 using AudioOutputStreamPtr = mojo::InterfacePtr<AudioOutputStream>;
 using AudioOutputStreamRequest = mojo::InterfaceRequest<AudioOutputStream>;
-using AudioOutputStreamProviderClient =
-    media::mojom::AudioOutputStreamProviderClient;
-using AudioOutputStreamProviderClientPtr =
-    mojo::InterfacePtr<AudioOutputStreamProviderClient>;
-using AudioOutputStreamProviderClientRequest =
-    mojo::InterfaceRequest<AudioOutputStreamProviderClient>;
+using AudioOutputStreamClient = media::mojom::AudioOutputStreamClient;
+using AudioOutputStreamClientPtr = mojo::InterfacePtr<AudioOutputStreamClient>;
+using AudioOutputStreamClientRequest =
+    mojo::InterfaceRequest<AudioOutputStreamClient>;
 using AudioOutputStreamProvider = media::mojom::AudioOutputStreamProvider;
 using AudioOutputStreamProviderPtr =
     mojo::InterfacePtr<AudioOutputStreamProvider>;
@@ -169,21 +167,13 @@ class MockContext : public RendererAudioOutputStreamFactoryContext {
   DISALLOW_COPY_AND_ASSIGN(MockContext);
 };
 
-class MockClient : public AudioOutputStreamProviderClient {
+class MockClient : public AudioOutputStreamClient {
  public:
-  MockClient() : provider_client_binding_(this) {}
+  MockClient() {}
   ~MockClient() override {}
 
-  AudioOutputStreamProviderClientPtr MakeProviderClientPtr() {
-    AudioOutputStreamProviderClientPtr p;
-    provider_client_binding_.Bind(mojo::MakeRequest(&p));
-    return p;
-  }
-
-  void Created(AudioOutputStreamPtr stream,
-               media::mojom::AudioDataPipePtr data_pipe) {
+  void StreamCreated(media::mojom::AudioDataPipePtr data_pipe) {
     was_called_ = true;
-    stream_ = std::move(stream);
   }
 
   bool was_called() { return was_called_; }
@@ -191,8 +181,6 @@ class MockClient : public AudioOutputStreamProviderClient {
   MOCK_METHOD0(OnError, void());
 
  private:
-  mojo::Binding<AudioOutputStreamProviderClient> provider_client_binding_;
-  AudioOutputStreamPtr stream_;
   bool was_called_ = false;
 
   DISALLOW_COPY_AND_ASSIGN(MockClient);
@@ -212,13 +200,17 @@ void AuthCallback(media::OutputDeviceStatus* status_out,
 }  // namespace
 
 // This test authorizes and creates a stream, and checks that
-// 1. the ProviderClient callback is called with appropriate parameters.
+// 1. the authorization callback is called with appropriate parameters.
 // 2. the AudioOutputDelegate is created.
 // 3. when the delegate calls OnStreamCreated, this is propagated to the client.
 TEST(RenderFrameAudioOutputStreamFactoryTest, CreateStream) {
   content::TestBrowserThreadBundle thread_bundle;
   AudioOutputStreamProviderPtr provider;
+  AudioOutputStreamPtr output_stream;
   MockClient client;
+  AudioOutputStreamClientPtr client_ptr;
+  mojo::Binding<AudioOutputStreamClient> client_binding(
+      &client, mojo::MakeRequest(&client_ptr));
   media::AudioOutputDelegate::EventHandler* event_handler = nullptr;
   auto factory_context = std::make_unique<MockContext>(true);
   factory_context->PrepareDelegateForCreation(
@@ -238,7 +230,9 @@ TEST(RenderFrameAudioOutputStreamFactoryTest, CreateStream) {
             GetTestAudioParameters().AsHumanReadableString());
   EXPECT_TRUE(id.empty());
 
-  provider->Acquire(params, client.MakeProviderClientPtr());
+  provider->Acquire(
+      mojo::MakeRequest(&output_stream), std::move(client_ptr), params,
+      base::BindOnce(&MockClient::StreamCreated, base::Unretained(&client)));
   base::RunLoop().RunUntilIdle();
   ASSERT_NE(event_handler, nullptr);
 
@@ -277,7 +271,11 @@ TEST(RenderFrameAudioOutputStreamFactoryTest, NotAuthorized_Denied) {
 TEST(RenderFrameAudioOutputStreamFactoryTest, ConnectionError_DeletesStream) {
   content::TestBrowserThreadBundle thread_bundle;
   AudioOutputStreamProviderPtr provider;
+  AudioOutputStreamPtr output_stream;
   MockClient client;
+  AudioOutputStreamClientPtr client_ptr;
+  mojo::Binding<AudioOutputStreamClient> client_binding(
+      &client, mojo::MakeRequest(&client_ptr));
   bool delegate_is_destructed = false;
   media::AudioOutputDelegate::EventHandler* event_handler = nullptr;
   auto factory_context = std::make_unique<MockContext>(true);
@@ -295,11 +293,14 @@ TEST(RenderFrameAudioOutputStreamFactoryTest, ConnectionError_DeletesStream) {
                         const std::string& id) {}));
   base::RunLoop().RunUntilIdle();
 
-  provider->Acquire(GetTestAudioParameters(), client.MakeProviderClientPtr());
+  provider->Acquire(
+      mojo::MakeRequest(&output_stream), std::move(client_ptr),
+      GetTestAudioParameters(),
+      base::BindOnce(&MockClient::StreamCreated, base::Unretained(&client)));
   base::RunLoop().RunUntilIdle();
   ASSERT_NE(event_handler, nullptr);
   EXPECT_FALSE(delegate_is_destructed);
-  provider.reset();
+  output_stream.reset();
   base::RunLoop().RunUntilIdle();
   EXPECT_TRUE(delegate_is_destructed);
 }
@@ -307,7 +308,11 @@ TEST(RenderFrameAudioOutputStreamFactoryTest, ConnectionError_DeletesStream) {
 TEST(RenderFrameAudioOutputStreamFactoryTest, DelegateError_DeletesStream) {
   content::TestBrowserThreadBundle thread_bundle;
   AudioOutputStreamProviderPtr provider;
+  AudioOutputStreamPtr output_stream;
   MockClient client;
+  AudioOutputStreamClientPtr client_ptr;
+  mojo::Binding<AudioOutputStreamClient> client_binding(
+      &client, mojo::MakeRequest(&client_ptr));
   bool delegate_is_destructed = false;
   media::AudioOutputDelegate::EventHandler* event_handler = nullptr;
   auto factory_context = std::make_unique<MockContext>(true);
@@ -325,7 +330,10 @@ TEST(RenderFrameAudioOutputStreamFactoryTest, DelegateError_DeletesStream) {
                         const std::string& id) {}));
   base::RunLoop().RunUntilIdle();
 
-  provider->Acquire(GetTestAudioParameters(), client.MakeProviderClientPtr());
+  provider->Acquire(
+      mojo::MakeRequest(&output_stream), std::move(client_ptr),
+      GetTestAudioParameters(),
+      base::BindOnce(&MockClient::StreamCreated, base::Unretained(&client)));
   base::RunLoop().RunUntilIdle();
   ASSERT_NE(event_handler, nullptr);
   EXPECT_FALSE(delegate_is_destructed);
--- services/audio/output_stream.cc
+++ services/audio/output_stream.cc
@@ -23,6 +23,7 @@ OutputStream::OutputStream(
     CreatedCallback created_callback,
     DeleteCallback delete_callback,
     media::mojom::AudioOutputStreamRequest stream_request,
+    media::mojom::AudioOutputStreamClientPtr client,
     media::mojom::AudioOutputStreamObserverAssociatedPtr observer,
     media::mojom::AudioLogPtr log,
     media::AudioManager* audio_manager,
@@ -33,6 +34,7 @@ OutputStream::OutputStream(
     : foreign_socket_(),
       delete_callback_(std::move(delete_callback)),
       binding_(this, std::move(stream_request)),
+      client_(std::move(client)),
       observer_(std::move(observer)),
       log_(media::mojom::ThreadSafeAudioLogPtr::Create(std::move(log))),
       coordinator_(coordinator),
@@ -49,6 +51,7 @@ OutputStream::OutputStream(
                   &reader_),
       weak_factory_(this) {
   DCHECK(binding_.is_bound());
+  DCHECK(client_.is_bound());
   DCHECK(observer_.is_bound());
   DCHECK(created_callback);
   DCHECK(delete_callback_);
@@ -58,6 +61,7 @@ OutputStream::OutputStream(
   base::RepeatingClosure error_handler =
       base::BindRepeating(&OutputStream::OnError, base::Unretained(this));
   binding_.set_connection_error_handler(error_handler);
+  client_.set_connection_error_handler(error_handler);
 
   // We allow the observer to terminate the stream by closing the message pipe.
   observer_.set_connection_error_handler(std::move(error_handler));
@@ -179,10 +183,8 @@ void OutputStream::OnControllerPaused() {
 void OutputStream::OnControllerError() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(owning_sequence_);
 
-  // Only propagate platform errors to the observer.
-  observer_.ResetWithReason(
-      media::mojom::AudioOutputStreamObserver::kPlatformErrorDisconnectReason,
-      std::string());
+  // Only propagate platform errors to the renderer.
+  client_->OnError();
   log_->get()->OnError();
   OnError();
 }
