bcca2d0dbec00eec6ffb8b12bd1f4f81ad6ba81f
Gabriel Charette <gab@chromium.org>
2018-04-23 21:42:47 +0000

    Migrate MessageLoop::current() to MessageLoopCurrent in /chrome/browser/safe_browsing/incident_reporting
    These changes were scripted as part of the effort to restrict exposure of the
    raw MessageLoop*.
    
    Note that many static methods exposed on MessageLoopCurrent are also deprecated
    so it is very well possible that this is migrating to an already deprecated
    method.
    
    The goal of this pass is to reduce usage of MessageLoop::current() (and
    ultimately make it impossible to obtain the MessageLoop* statically).
    
    As such I will not edit this CL unless the script did something logically wrong.
    I defer to owners to fix highlighted usage of already deprecated APIs.
    
    Possible script screw ups / things to look out for in this review:
     - Storing MessageLoopCurrent in a MessageLoop* variable or comparing it against
       one (I will go over that in a separate pass).
    
    Includes should have been stripped if that was the last usage of message_loop.h
    in that file.
    
    Please CQ if LGTY
    
    This CL was uploaded by git cl split.
    
    R=caitkp@chromium.org
    
    Bug: 825327
    Change-Id: Id8cdae2fc85f60f7cd66d5d88fd404128ba723ac
    Reviewed-on: https://chromium-review.googlesource.com/1024241
    Commit-Queue: Cait Phillips <caitkp@chromium.org>
    Reviewed-by: Cait Phillips <caitkp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552843}

--- chrome/browser/safe_browsing/incident_reporting/state_store_unittest.cc
+++ chrome/browser/safe_browsing/incident_reporting/state_store_unittest.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/json/json_file_value_serializer.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
-#include "base/message_loop/message_loop.h"
+#include "base/message_loop/message_loop_current.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/test_simple_task_runner.h"
 #include "base/threading/thread_task_runner_handle.h"
@@ -81,7 +81,7 @@ class StateStoreTest : public PlatformStateStoreTestBase {
 
   void SetUp() override {
     PlatformStateStoreTestBase::SetUp();
-    base::MessageLoop::current()->SetTaskRunner(task_runner_);
+    base::MessageLoopCurrent::Get()->SetTaskRunner(task_runner_);
     ASSERT_TRUE(temp_dir_.CreateUniqueTempDir());
     ASSERT_TRUE(profile_manager_.SetUp());
     CreateProfile();
