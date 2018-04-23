84d0f3a8f2ed5d029b17a36278338b3d0fdb10d1
Gabriel Charette <gab@chromium.org>
2018-04-23 19:20:19 +0000

    Migrate MessageLoop::current() to MessageLoopCurrent in /headless
    These changes were scripted as part of the effort to restrict exposure of the
    raw MessageLoop*.
    
    Note that many static methods exposed on MessageLoopCurrent are also deprecated
    so it is very well possible that this is migrating to an already deprecated
    method.
    
    The goal of this pass is to reduce usage of MessageLoop::current() (and
    ultimately make it impossible to obtain the MessageLoop* statically).
    
    As such I will not edit this CL unless the script did something logically wrong.
    I defer to owners to fix highlighted usage of already deprecated APIs.
    
    Possible script screw ups / things to look out for in this review:
     - Storing MessageLoopCurrent in a MessageLoop* variable or comparing it against
       one (I will go over that in a separate pass).
    
    Includes should have been stripped if that was the last usage of message_loop.h
    in that file.
    
    Please CQ if LGTY
    
    This CL was uploaded by git cl split.
    
    R=altimin@chromium.org
    
    Bug: 825327
    Change-Id: I87b81780f737a0be5087f319474f663a3aa89892
    Reviewed-on: https://chromium-review.googlesource.com/1024481
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552786}

--- headless/public/util/generic_url_request_job_test.cc
+++ headless/public/util/generic_url_request_job_test.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/json/json_reader.h"
 #include "base/json/json_writer.h"
 #include "base/message_loop/message_loop.h"
+#include "base/message_loop/message_loop_current.h"
 #include "base/run_loop.h"
 #include "base/single_thread_task_runner.h"
 #include "base/strings/stringprintf.h"
@@ -647,7 +648,7 @@ class ByteAtATimeUploadElementReader : public net::UploadElementReader {
     if (!BytesRemaining())
       return net::OK;
 
-    base::MessageLoop::current()->task_runner()->PostTask(
+    base::MessageLoopCurrent::Get()->task_runner()->PostTask(
         FROM_HERE,
         base::BindOnce(&ByteAtATimeUploadElementReader::ReadImpl,
                        base::Unretained(this), base::WrapRefCounted(buf),
