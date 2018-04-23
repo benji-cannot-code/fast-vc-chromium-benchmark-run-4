5dfcac7f74d086f34ef8de45482b014700ad21ea
Lei Zhang <thestig@chromium.org>
2018-04-23 16:25:06 +0000

    Disable ExtensionApiTestWithManagementPolicy.UrlProtectedByPolicy.
    The test is flaky.
    
    BUG=835155
    NOTRY=true
    
    Change-Id: I3f8f98e34aedefd198ba3c1594d35d85c262e7ce
    Reviewed-on: https://chromium-review.googlesource.com/1024195
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552724}

--- chrome/browser/extensions/api/web_request/web_request_apitest.cc
+++ chrome/browser/extensions/api/web_request/web_request_apitest.cc
@@ -1420,8 +1420,9 @@ IN_PROC_BROWSER_TEST_F(ExtensionApiTestWithManagementPolicy,
 
 // Tests that the webRequest events aren't dispatched when the URL of the
 // request is protected by policy.
+// Disabled because it is flaky. See https://crbug.com/835155
 IN_PROC_BROWSER_TEST_F(ExtensionApiTestWithManagementPolicy,
-                       UrlProtectedByPolicy) {
+                       DISABLED_UrlProtectedByPolicy) {
   // Host protected by policy.
   const std::string protected_domain = "example.com";
 
