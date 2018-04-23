708c8aefad55c545e0a76fcc061ee7e00ed8c22b
Brandon Wylie <wylieb@chromium.org>
2018-04-23 23:32:24 +0000

    [EOC] Move chrome://snippets-internals to its own directory
    Moving the backend & frontend code to their own folders. This CL contains no
    functional changes.
    
    Bug: 830931
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ia6147c93063214143c8de29ca945cc3df8892bfa
    Reviewed-on: https://chromium-review.googlesource.com/1023114
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552896}

--- chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
+++ chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
@@ -120,7 +120,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_ANDROID)
 #include "chrome/browser/ui/webui/offline/offline_internals_ui.h"
-#include "chrome/browser/ui/webui/snippets_internals_ui.h"
+#include "chrome/browser/ui/webui/snippets_internals/snippets_internals_ui.h"
 #include "chrome/browser/ui/webui/webapks_ui.h"
 #else
 #include "chrome/browser/ui/webui/devtools_ui.h"
--- chrome/browser/ui/webui/snippets_internals/snippets_internals_ui.h
+++ chrome/browser/ui/webui/snippets_internals/snippets_internals_ui.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_WEBUI_SNIPPETS_INTERNALS_UI_H_
-#define CHROME_BROWSER_UI_WEBUI_SNIPPETS_INTERNALS_UI_H_
+#ifndef CHROME_BROWSER_UI_WEBUI_SNIPPETS_INTERNALS_SNIPPETS_INTERNALS_UI_H_
+#define CHROME_BROWSER_UI_WEBUI_SNIPPETS_INTERNALS_SNIPPETS_INTERNALS_UI_H_
 
 #include "base/macros.h"
 #include "content/public/browser/web_ui_controller.h"
@@ -19,4 +19,4 @@ class SnippetsInternalsUI : public content::WebUIController {
   DISALLOW_COPY_AND_ASSIGN(SnippetsInternalsUI);
 };
 
-#endif  // CHROME_BROWSER_UI_WEBUI_SNIPPETS_INTERNALS_UI_H_
+#endif  // CHROME_BROWSER_UI_WEBUI_SNIPPETS_INTERNALS_SNIPPETS_INTERNALS_UI_H_
