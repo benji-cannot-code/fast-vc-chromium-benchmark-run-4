3e8af54e8ab3c4a0865656da0e39e7f04315883f
Sebastien Marchand <sebmarchand@chromium.org>
2018-04-23 16:06:22 +0000

    Add the SiteCharacteristicsDataStore class.
    See the (@google.com) design doc: https://docs.google.com/document/d/1MgyIJ096pQChpbqQIqXHTZ0NYRXAOxcBtAq2lJplOpE/edit?usp=sharing
    
    Bug: 773382
    Change-Id: Ieff80131e2df38867c059047467abefdd9fe06a7
    Reviewed-on: https://chromium-review.googlesource.com/1011253
    Commit-Queue: Sébastien Marchand <sebmarchand@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552718}

---
+++ chrome/browser/resource_coordinator/local_site_characteristics_data_unittest_utils.cc
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/resource_coordinator/local_site_characteristics_data_unittest_utils.h"
+
+namespace resource_coordinator {
+namespace testing {
+
+MockLocalSiteCharacteristicsDataImplOnDestroyDelegate::
+    MockLocalSiteCharacteristicsDataImplOnDestroyDelegate() = default;
+MockLocalSiteCharacteristicsDataImplOnDestroyDelegate::
+    ~MockLocalSiteCharacteristicsDataImplOnDestroyDelegate() = default;
+
+}  // namespace testing
+}  // namespace resource_coordinator
