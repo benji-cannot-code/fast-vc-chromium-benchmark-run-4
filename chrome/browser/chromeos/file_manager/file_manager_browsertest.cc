721148e793343501698baacb8068bd11bc41234b
Noel Gordon <noel@chromium.org>
2018-04-23 23:18:20 +0000

    Make FileManager browser test consistent with other browser tests
    Use public. Document the browser test classes. Order the base virtuals
    to match our other browser tests.
    
    No change in behavior, no new tests.
    
    Bug: 833834
    Change-Id: I3695fabab379d86cc6cdc38bde3c6312832c567a
    Reviewed-on: https://chromium-review.googlesource.com/1023678
    Reviewed-by: Stuart Langley <slangley@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552885}

--- chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
@@ -19,22 +19,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace file_manager {
 
-// Parameter of FileManagerBrowserTest.
-// The second value is the case name of JavaScript.
+// FileManagerBrowserTest parameters: first controls IN_GUEST_MODE or not, the
+// second is the JS test case name.
 typedef std::tuple<GuestMode, const char*> TestParameter;
 
-// Test fixture class for normal (not multi-profile related) tests.
+// Test class FileManager browser tests.
 class FileManagerBrowserTest :
       public FileManagerBrowserTestBase,
       public ::testing::WithParamInterface<TestParameter> {
+ public:
   GuestMode GetGuestModeParam() const override {
     return std::get<0>(GetParam());
   }
-  const char* GetTestManifestName() const override {
-    return "file_manager_test_manifest.json";
-  }
+
   const char* GetTestCaseNameParam() const override {
     return std::get<1>(GetParam());
+  }
+
+  const char* GetTestManifestName() const override {
+    return "file_manager_test_manifest.json";
   }
 };
 
@@ -42,10 +45,11 @@ IN_PROC_BROWSER_TEST_P(FileManagerBrowserTest, Test) {
   StartTest();
 }
 
-// Test fixture class for tests that rely on deprecated event dispatch that send
-// tests.
+// Test class for FileManager browser tests that rely on deprecated event
+// dispatch that send tests.
 class FileManagerBrowserTestWithLegacyEventDispatch
     : public FileManagerBrowserTest {
+ public:
   void SetUpCommandLine(base::CommandLine* command_line) override {
     FileManagerBrowserTest::SetUpCommandLine(command_line);
     command_line->AppendSwitchASCII("disable-blink-features",
