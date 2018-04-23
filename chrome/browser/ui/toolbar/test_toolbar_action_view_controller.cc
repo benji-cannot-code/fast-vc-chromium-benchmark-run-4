fa8a12aae80b192a6a6b262579c267ef00d41191
Aaron Leventhal <aleventhal@chromium.org>
2018-04-23 20:14:02 +0000

    Use accessible name in toolbar test
    Accessibility checks from http://crrev.com/c/981937 fail in
    ComponentToolbarActionsBrowserTest.ComponentToolbarActionsShowUpAndRespondToClicks.
    
    This adds an accessible name via the controller used in the test.
    
    Bug: 819350
    Change-Id: I64c95fbea582816c47d0215065fc8983f78a2bb7
    Reviewed-on: https://chromium-review.googlesource.com/1023211
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552806}

--- chrome/browser/ui/toolbar/test_toolbar_action_view_controller.cc
+++ chrome/browser/ui/toolbar/test_toolbar_action_view_controller.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/toolbar/test_toolbar_action_view_controller.h"
 
 #include "base/strings/string16.h"
+#include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/ui/toolbar/toolbar_action_view_delegate.h"
 #include "ui/gfx/image/image.h"
 #include "ui/gfx/image/image_skia.h"
@@ -18,6 +19,8 @@ TestToolbarActionViewController::TestToolbarActionViewController(
       wants_to_run_(false),
       disabled_click_opens_menu_(false),
       execute_action_count_(0) {
+  // Needs a non-empty accessible name to pass accessibility checks.
+  SetAccessibleName(base::ASCIIToUTF16("Default name"));
 }
 
 TestToolbarActionViewController::~TestToolbarActionViewController() {
