79c34165b3428c1e095d2a181b4f8f20483cf408
Brian Osman <brianosman@google.com>
2018-04-23 22:50:09 +0000

    Remove ICCProfile::GetParametricColorSpace()
    This was only used in unit tests. Add a simpler bool accessor to
    ColorSpace that indicates if the parametric approximation is accurate,
    and use that to continue testing specific profiles. This makes the test
    logic more accurately reflect the actual logic used at runtime around
    caching of profiles.
    
    Change-Id: I5b73f13e7912ad9c7903b83af823e8bf9ac9da29
    Reviewed-on: https://chromium-review.googlesource.com/1022633
    Reviewed-by: Mike Klein <mtklein@chromium.org>
    Reviewed-by: ccameron <ccameron@chromium.org>
    Commit-Queue: Brian Osman <brianosman@google.com>
    Cr-Commit-Position: refs/heads/master@{#552871}

--- ui/gfx/icc_profile_unittest.cc
+++ ui/gfx/icc_profile_unittest.cc
@@ -31,10 +31,6 @@ TEST(ICCProfile, SRGB) {
   EXPECT_EQ(icc_profile.GetColorSpace(), ColorSpace::CreateSRGB());
   EXPECT_EQ(icc_profile.GetColorSpace().ToSkColorSpace().get(),
             sk_color_space.get());
-  // The parametric generating code should recognize that this is SRGB.
-  EXPECT_EQ(icc_profile.GetParametricColorSpace(), ColorSpace::CreateSRGB());
-  EXPECT_EQ(icc_profile.GetParametricColorSpace().ToSkColorSpace().get(),
-            sk_color_space.get());
   // The generated color space should recognize that this is SRGB.
   EXPECT_EQ(color_space.ToSkColorSpace().get(), sk_color_space.get());
 }
@@ -75,7 +71,7 @@ TEST(ICCProfile, ParametricVersusExactInaccurate) {
   // This ICC profile has three transfer functions that differ enough that the
   // parametric color space is considered inaccurate.
   ICCProfile multi_tr_fn = ICCProfileForTestingNoAnalyticTrFn();
-  EXPECT_NE(multi_tr_fn.GetColorSpace(), multi_tr_fn.GetParametricColorSpace());
+  EXPECT_FALSE(multi_tr_fn.GetColorSpace().IsParametricAccurate());
 
   // Fails to get parametric color space because the space is not parametric.
   ICCProfile profile;
@@ -84,7 +80,8 @@ TEST(ICCProfile, ParametricVersusExactInaccurate) {
 
   // The Mac cache does not find the parametric approximation, because the cache
   // only has the original.
-  profile = ICCProfile::FromCacheMac(multi_tr_fn.GetParametricColorSpace());
+  profile = ICCProfile::FromCacheMac(
+      multi_tr_fn.GetColorSpace().GetParametricApproximation());
   EXPECT_FALSE(profile.IsValid());
 
   // The Mac cache does find the original.
@@ -94,7 +91,7 @@ TEST(ICCProfile, ParametricVersusExactInaccurate) {
 
   // We are capable of generating a parametric approximation.
   profile = ICCProfile::FromParametricColorSpace(
-      multi_tr_fn.GetParametricColorSpace());
+      multi_tr_fn.GetColorSpace().GetParametricApproximation());
   EXPECT_TRUE(profile.IsValid());
   EXPECT_NE(profile, multi_tr_fn);
 }
@@ -103,7 +100,7 @@ TEST(ICCProfile, ParametricVersusExactOvershoot) {
   // This ICC profile has a transfer function with T(1) that is greater than 1
   // in the approximation, but is still close enough to be considered accurate.
   ICCProfile overshoot = ICCProfileForTestingOvershoot();
-  EXPECT_EQ(overshoot.GetColorSpace(), overshoot.GetParametricColorSpace());
+  EXPECT_TRUE(overshoot.GetColorSpace().IsParametricAccurate());
 
   ICCProfile profile;
   profile = ICCProfile::FromCacheMac(overshoot.GetColorSpace());
@@ -118,7 +115,7 @@ TEST(ICCProfile, ParametricVersusExactOvershoot) {
 TEST(ICCProfile, ParametricVersusExactAdobe) {
   // This ICC profile is precisely represented by the parametric color space.
   ICCProfile accurate = ICCProfileForTestingAdobeRGB();
-  EXPECT_EQ(accurate.GetColorSpace(), accurate.GetParametricColorSpace());
+  EXPECT_TRUE(accurate.GetColorSpace().IsParametricAccurate());
 
   ICCProfile profile;
   profile = ICCProfile::FromCacheMac(accurate.GetColorSpace());
@@ -150,12 +147,10 @@ TEST(ICCProfile, GarbageData) {
       ICCProfile::FromData(bad_data.data(), bad_data.size());
   EXPECT_FALSE(garbage_profile.IsValid());
   EXPECT_FALSE(garbage_profile.GetColorSpace().IsValid());
-  EXPECT_FALSE(garbage_profile.GetParametricColorSpace().IsValid());
 
   ICCProfile default_ctor_profile;
   EXPECT_FALSE(default_ctor_profile.IsValid());
   EXPECT_FALSE(default_ctor_profile.GetColorSpace().IsValid());
-  EXPECT_FALSE(default_ctor_profile.GetParametricColorSpace().IsValid());
 }
 
 TEST(ICCProfile, GenericRGB) {
