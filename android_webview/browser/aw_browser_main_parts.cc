cd1b0e013260cc0eb463c9333922ef0b8efe6d80
Gabriel Charette <gab@chromium.org>
2018-04-23 19:34:03 +0000

    Migrate MessageLoop::current() to MessageLoopCurrent in /android_webview
    These changes were scripted as part of the effort to restrict exposure of the
    raw MessageLoop*.
    
    Note that many static methods exposed on MessageLoopCurrent are also deprecated
    so it is very well possible that this is migrating to an already deprecated
    method.
    
    The goal of this pass is to reduce usage of MessageLoop::current() (and
    ultimately make it impossible to obtain the MessageLoop* statically).
    
    As such I will not edit this CL unless the script did something logically wrong.
    I defer to owners to fix highlighted usage of already deprecated APIs.
    
    Possible script screw ups / things to look out for in this review:
     - Storing MessageLoopCurrent in a MessageLoop* variable or comparing it against
       one (I will go over that in a separate pass).
    
    Includes should have been stripped if that was the last usage of message_loop.h
    in that file.
    
    Please CQ if LGTY
    
    This CL was uploaded by git cl split.
    
    R=torne@chromium.org
    
    Bug: 825327
    Change-Id: Id2340299ab2a3c4e3329b66a625d49f41a2e5443
    Reviewed-on: https://chromium-review.googlesource.com/1024443
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Richard Coles <torne@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552792}

--- android_webview/browser/aw_browser_main_parts.cc
+++ android_webview/browser/aw_browser_main_parts.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/i18n/rtl.h"
 #include "base/message_loop/message_loop.h"
+#include "base/message_loop/message_loop_current.h"
 #include "base/path_service.h"
 #include "components/crash/content/browser/crash_dump_manager_android.h"
 #include "components/crash/content/browser/crash_dump_observer_android.h"
@@ -73,7 +74,7 @@ int AwBrowserMainParts::PreEarlyInitialization() {
   // Android specific MessageLoop. Also see MainMessageLoopRun.
   DCHECK(!main_message_loop_.get());
   main_message_loop_.reset(new base::MessageLoopForUI);
-  base::MessageLoopForUI::current()->Start();
+  base::MessageLoopCurrentForUI::Get()->Start();
   return content::RESULT_CODE_NORMAL_EXIT;
 }
 
