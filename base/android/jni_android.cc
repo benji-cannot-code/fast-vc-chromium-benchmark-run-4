61b63bcc3080f588fa245fa0882edfd368dfdab1
Hidehiko Abe <hidehiko@chromium.org>
2018-04-23 20:43:03 +0000

    Remove unnecessary include.
    BUG=None
    TEST=Ran trybots.
    
    Change-Id: I3450f37691347e96d2c563128974c375c904b49e
    Reviewed-on: https://chromium-review.googlesource.com/1006694
    Commit-Queue: agrieve <agrieve@chromium.org>
    Reviewed-by: Luis Hector Chavez <lhchavez@chromium.org>
    Reviewed-by: agrieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552819}

--- base/android/jni_android.cc
+++ base/android/jni_android.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/android/build_info.h"
 #include "base/android/jni_string.h"
-#include "base/android/jni_utils.h"
 #include "base/debug/debugging_buildflags.h"
 #include "base/lazy_instance.h"
 #include "base/logging.h"
