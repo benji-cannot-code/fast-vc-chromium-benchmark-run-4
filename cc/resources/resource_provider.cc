0e9007d32a61a9dc18ba0ee7dfcb7e838a69224f
Greg Daniel <egdaniel@chromium.org>
2018-04-23 15:25:56 +0000

    Move SkImage::getTextureHandle to getBackendTexture in cc.
    In skia getTextureHandle is deprecated and this change moves to using the
    new API.
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Iee5426c23daf40f181dd8a1d0bd5a181529da638
    Reviewed-on: https://chromium-review.googlesource.com/1022374
    Commit-Queue: Greg Daniel <egdaniel@chromium.org>
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552704}

--- cc/resources/resource_provider.cc
+++ cc/resources/resource_provider.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/client/context_support.h"
 #include "gpu/command_buffer/client/gles2_interface.h"
 #include "gpu/command_buffer/common/gpu_memory_buffer_support.h"
-#include "skia/ext/texture_handle.h"
 #include "third_party/khronos/GLES2/gl2.h"
 #include "third_party/khronos/GLES2/gl2ext.h"
 #include "ui/gfx/geometry/rect.h"
