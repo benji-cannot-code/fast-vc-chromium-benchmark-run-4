fa121c11f915fd623023a41c635179a5bbff6536
Sean Topping <seantopping@chromium.org>
2018-04-23 22:08:25 +0000

    [Chromecast] Move content/web interfaces to "public" GN target
    This facilitates mocking for unit tests without having to pull in all
    of chromecast/browser.
    
    Bug: None
    Test: CQ
    Change-Id: Ifeafdfeeffcdbf152a4d694f7cfc280cd5887e37
    Reviewed-on: https://chromium-review.googlesource.com/1015960
    Commit-Queue: Sean Topping <seantopping@chromium.org>
    Reviewed-by: Stephen Lanham <slan@chromium.org>
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552849}

--- chromecast/browser/cast_content_window.h
+++ chromecast/browser/cast_content_window.h
@@ -14,10 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "ui/events/event.h"
 
-namespace content {
-class WebContents;
-}
-
 namespace chromecast {
 namespace shell {
 
--- chromecast/browser/cast_web_view.h
+++ chromecast/browser/cast_web_view.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromecast/graphics/cast_window_manager.h"
 #include "content/public/browser/bluetooth_chooser.h"
 #include "content/public/browser/web_contents.h"
+#include "url/gurl.h"
 
 namespace chromecast {
 
