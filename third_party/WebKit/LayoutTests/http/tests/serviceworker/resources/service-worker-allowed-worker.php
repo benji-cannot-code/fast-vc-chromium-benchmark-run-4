401126b6d27f064f830cd2949acfc0a35cc833bc
Matt Falkenhagen <falken@chromium.org>
2018-04-23 05:18:31 +0000

    service worker: Upstream Service-Worker-Allowed test to WPT.
    Also add test cases for Service-Worker-Allowed header values that are
    absolute URLs.
    
    Currently Chrome accepts SWA header values that are cross-origin to the
    script URL since it seems to only care about the path of the URL. It
    seems strange but that seems to agree with the spec:
    https://github.com/w3c/ServiceWorker/issues/1307
    
    Bug: 688116
    Change-Id: I6dca55ea8525803efd2e55bf4166c863d62d31fa
    Reviewed-on: https://chromium-review.googlesource.com/1023672
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552639}

--- third_party/WebKit/LayoutTests/http/tests/serviceworker/resources/service-worker-allowed-worker.php
+++
@@ -1,7 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-<?php
-header('Content-Type: application/javascript');
-if (isset($_GET['ServiceWorkerAllowed'])) {
-  header('Service-Worker-Allowed: ' . $_GET['ServiceWorkerAllowed']);
-}
-?>
