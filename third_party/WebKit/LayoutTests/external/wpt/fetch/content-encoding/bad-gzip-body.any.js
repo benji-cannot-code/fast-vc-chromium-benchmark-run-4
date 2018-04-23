c2b88fdeb8a89ea13dd65f6efd019a9e7c9e5640
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-04-23 15:35:10 +0000

    Import wpt@5ac29d88c701690eabc70dea4e467a4353d27541
    Using wpt-import in Chromium 490346a1fa8e273a3cfd99c7846d24defbc0a520.
    With Chromium commits locally applied on WPT:
    303956b6ed "Enable WPT tests for the Generic Sensor classes"
    afb472c0d2 "Try to fix crash when terminating a worker while it is XHR-ing to a blob."
    
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/16182
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    yhirano@chromium.org, mkwst@chromium.org, japhet@chromium.org:
      external/wpt/fetch
    
    TBR=ajuma
    
    No-Export: true
    Change-Id: If193896ed8a0c7e88f24f02eb7f250754807cccf
    Reviewed-on: https://chromium-review.googlesource.com/1024071
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552705}

---
+++ third_party/WebKit/LayoutTests/external/wpt/fetch/content-encoding/bad-gzip-body.any.js
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+promise_test((test) => {
+    return fetch("resources/bad-gzip-body.py").then(res => {
+      assert_equals(res.status, 200);
+    });
+}, "Fetching a resource with bad gzip content should still resolve");
+
+[
+  "arrayBuffer",
+  "blob",
+  "formData",
+  "json",
+  "text"
+].forEach(method => {
+  promise_test(t => {
+    return fetch("resources/bad-gzip-body.py").then(res => {
+      assert_equals(res.status, 200);
+      return promise_rejects(t, new TypeError(), res[method]());
+    });
+  }, "Consuming the body of a resource with bad gzip content with " + method + "() should reject");
+});
