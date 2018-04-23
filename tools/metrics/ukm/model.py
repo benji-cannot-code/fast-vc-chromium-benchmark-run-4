d398cf7ad90f6fc4533a40491a39cb492dceef97
Alexei Svitkine <asvitkine@chromium.org>
2018-04-23 15:39:55 +0000

    Fix support for action suffixes in actions.xml.
    There was some previously incomplete support, but
    that support didn't really work because pretty_print.py
    would just expand all the suffixes and the processed
    file would not have the suffix entries, but just the
    expanded list. This CL properly preserves the suffixes
    in the actions.xml file.
    
    Also, adopt suffixes for some IPH actions to clean them
    up and test this functionality.
    
    It also cleans up some redundancy and edge cases in
    existing Python code (for example, actions were returned
    both in a dict and a set of names, with the latter being
    redundant and now simplified.)
    
    Bug: 726400
    Change-Id: I4c73811b9d13ff4e997f1d18a9d3ef0ab9277b06
    Reviewed-on: https://chromium-review.googlesource.com/1015293
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552706}

--- tools/metrics/ukm/model.py
+++ tools/metrics/ukm/model.py
@@ -31,7 +31,7 @@ _METRIC_TYPE =  models.ObjectNodeType(
 
 _EVENT_TYPE =  models.ObjectNodeType(
     'event',
-    alphabetization=('metric', _LOWERCASE_NAME_FN),
+    alphabetization=[('metric', _LOWERCASE_NAME_FN)],
     attributes=[('name', unicode), ('singular', bool)],
     extra_newlines=(1, 1, 1),
     children=[
