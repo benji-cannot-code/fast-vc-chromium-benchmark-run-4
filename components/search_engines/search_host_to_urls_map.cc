3f52643bc6f2c981fb62a54b5b8763ebafe6284b
Esmael El-Moslimany <aee@chromium.org>
2018-04-23 23:00:10 +0000

    Search: move member variable initializations to header
    Bug: 781703
    Change-Id: I716d1e0fcf7e6023e6b516e195171ee8c0a0feee
    Reviewed-on: https://chromium-review.googlesource.com/1022318
    Commit-Queue: Esmael El-Moslimany <aee@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552877}

--- components/search_engines/search_host_to_urls_map.cc
+++ components/search_engines/search_host_to_urls_map.cc
@@ -17,7 +17,7 @@ SearchHostToURLsMap::~SearchHostToURLsMap() {
 }
 
 void SearchHostToURLsMap::Init(
-    const TemplateURLService::OwnedTemplateURLVector& template_urls,
+    const TemplateURL::OwnedTemplateURLVector& template_urls,
     const SearchTermsData& search_terms_data) {
   DCHECK(!initialized_);
   initialized_ = true;  // Set here so Add doesn't assert.
@@ -74,7 +74,7 @@ SearchHostToURLsMap::TemplateURLSet* SearchHostToURLsMap::GetURLsForHost(
 }
 
 void SearchHostToURLsMap::Add(
-    const TemplateURLService::OwnedTemplateURLVector& template_urls,
+    const TemplateURL::OwnedTemplateURLVector& template_urls,
     const SearchTermsData& search_terms_data) {
   for (const auto& turl : template_urls)
     Add(turl.get(), search_terms_data);
