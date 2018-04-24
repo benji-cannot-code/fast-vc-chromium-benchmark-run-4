nwjs/chromium.src

commit 850001f395fe619b3add0fdb80a1cfbd7d32df01

Move notification_helper/ to chrome/notification_helper/
... plus all the necessary code migration. A follow-up CL will be used
to update relevant comments.

Bug: 734095
Change-Id: I8516c750518e10a366bdc1496e75a3c2f4f0898b
Reviewed-on: https://chromium-review.googlesource.com/1022957
Reviewed-by: John Abd-El-Malek <jam@chromium.org>
Commit-Queue: Xi Cheng <chengx@chromium.org>
Cr-Commit-Position: refs/heads/master@{#552762}

BUILD.gn
@@ -579,6 +579,7 @@ group("gn_all") {
       "//chrome/install_static:install_static_unittests",
       "//chrome/installer/gcapi",
       "//chrome/installer/setup:setup_unittests",
+      "//chrome/notification_helper:notification_helper_unittests",
       "//chrome_elf:chrome_elf_import_unittests",
       "//chrome_elf:chrome_elf_unittests",
       "//chrome_elf:dll_hash_main",
@@ -587,7 +588,6 @@ group("gn_all") {
       "//components/zucchini",
       "//net:quic_client",
       "//net:quic_server",
-      "//notification_helper:notification_helper_unittests",
       "//sandbox/win:pocdll",
       "//sandbox/win:sandbox_poc",
       "//sandbox/win:sbox_integration_tests",

WATCHLISTS
@@ -1167,7 +1167,7 @@
         'content/public/browser/platform_notification|'\
         'content/public/common/platform_notification|'\
         'content/renderer/notification_|'\
-        'notification_helper/|'\
+        'chrome/notification_helper/|'\
         'third_party/WebKit/LayoutTests/external/wpt/notifications|'\
         'third_party/WebKit/LayoutTests/http/tests/notifications|'\
         'third_party/blink/renderer/modules/notifications|'\

chrome/BUILD.gn
@@ -215,7 +215,7 @@ if (!is_android && !is_mac) {
         ]
         data_deps = [
           "//chrome/app/version_assembly:version_assembly_manifest",
-          "//notification_helper",
+          "//chrome/notification_helper",
         ]
 
         if (win_console_app) {

chrome/notification_helper/com_server_module.cc
@@ -9,14 +9,14 @@
 #define __WRL_CLASSIC_COM_STRICT__
 #endif  // __WRL_CLASSIC_COM_STRICT__
 
-#include "notification_helper/com_server_module.h"
+#include "chrome/notification_helper/com_server_module.h"
 
 #include <wrl/module.h>
 
 #include "base/metrics/histogram_macros.h"
 #include "chrome/install_static/install_util.h"
-#include "notification_helper/notification_activator.h"
-#include "notification_helper/trace_util.h"
+#include "chrome/notification_helper/notification_activator.h"
+#include "chrome/notification_helper/trace_util.h"
 
 namespace {
 

chrome/notification_helper/com_server_module.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef NOTIFICATION_HELPER_COM_SERVER_MODULE_H_
-#define NOTIFICATION_HELPER_COM_SERVER_MODULE_H_
+#ifndef CHROME_NOTIFICATION_HELPER_COM_SERVER_MODULE_H_
+#define CHROME_NOTIFICATION_HELPER_COM_SERVER_MODULE_H_
 
 #include "base/synchronization/waitable_event.h"
 #include "base/win/windows_types.h"
@@ -51,4 +51,4 @@ class ComServerModule {
 
 }  // namespace notification_helper
 
-#endif  // NOTIFICATION_HELPER_COM_SERVER_MODULE_H_
+#endif  // CHROME_NOTIFICATION_HELPER_COM_SERVER_MODULE_H_

benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 756c0f6808dc5cd27e510132550d50152a9f3d55

Reland "Move notification_helper/ to chrome/notification_helper/"
This is a reland of 850001f395fe619b3add0fdb80a1cfbd7d32df01, which caused
a pre-submit deadlock for chrome/browser/notifications/OWNERS.

This is now safe to land as the following CL has been landed:
https://chromium-review.googlesource.com/c/chromium/src/+/1024633

Original change's description:
> Move notification_helper/ to chrome/notification_helper/
>
> ... plus all the necessary code migration. A follow-up CL will be used
> to update relevant comments.
>
> Bug: 734095
> Change-Id: I8516c750518e10a366bdc1496e75a3c2f4f0898b
> Reviewed-on: https://chromium-review.googlesource.com/1022957
> Reviewed-by: John Abd-El-Malek <jam@chromium.org>
> Commit-Queue: Xi Cheng <chengx@chromium.org>
> Cr-Commit-Position: refs/heads/master@{#552762}

Bug: 734095
Change-Id: Id4e0818d7e3ea2b0ea7633e883bc5cea42705167
Reviewed-on: https://chromium-review.googlesource.com/1024744
Commit-Queue: Xi Cheng <chengx@chromium.org>
Reviewed-by: John Abd-El-Malek <jam@chromium.org>
Reviewed-by: Xi Cheng <chengx@chromium.org>
Cr-Commit-Position: refs/heads/master@{#552971}

BUILD.gn
@@ -580,6 +580,7 @@ group("gn_all") {
       "//chrome/install_static:install_static_unittests",
       "//chrome/installer/gcapi",
       "//chrome/installer/setup:setup_unittests",
+      "//chrome/notification_helper:notification_helper_unittests",
       "//chrome_elf:chrome_elf_import_unittests",
       "//chrome_elf:chrome_elf_unittests",
       "//chrome_elf:dll_hash_main",
@@ -588,7 +589,6 @@ group("gn_all") {
       "//components/zucchini",
       "//net:quic_client",
       "//net:quic_server",
-      "//notification_helper:notification_helper_unittests",
       "//sandbox/win:pocdll",
       "//sandbox/win:sandbox_poc",
       "//sandbox/win:sbox_integration_tests",

WATCHLISTS
@@ -1168,7 +1168,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'content/public/browser/platform_notification|'\
         'content/public/common/platform_notification|'\
         'content/renderer/notification_|'\
-        'notification_helper/|'\
+        'chrome/notification_helper/|'\
         'third_party/WebKit/LayoutTests/external/wpt/notifications|'\
         'third_party/WebKit/LayoutTests/http/tests/notifications|'\
         'third_party/blink/renderer/modules/notifications|'\

chrome/BUILD.gn
@@ -216,7 +216,7 @@ if (!is_android && !is_mac) {
         ]
         data_deps = [
           "//chrome/app/version_assembly:version_assembly_manifest",
-          "//notification_helper",
+          "//chrome/notification_helper",
         ]
 
         if (win_console_app) {

chrome/notification_helper/com_server_module.cc
@@ -10,14 +10,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define __WRL_CLASSIC_COM_STRICT__
 #endif  // __WRL_CLASSIC_COM_STRICT__
 
-#include "notification_helper/com_server_module.h"
+#include "chrome/notification_helper/com_server_module.h"
 
 #include <wrl/module.h>
 
 #include "base/metrics/histogram_macros.h"
 #include "chrome/install_static/install_util.h"
-#include "notification_helper/notification_activator.h"
-#include "notification_helper/trace_util.h"
+#include "chrome/notification_helper/notification_activator.h"
+#include "chrome/notification_helper/trace_util.h"
 
 namespace {
 

chrome/notification_helper/com_server_module.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef NOTIFICATION_HELPER_COM_SERVER_MODULE_H_
-#define NOTIFICATION_HELPER_COM_SERVER_MODULE_H_
+#ifndef CHROME_NOTIFICATION_HELPER_COM_SERVER_MODULE_H_
+#define CHROME_NOTIFICATION_HELPER_COM_SERVER_MODULE_H_
 
 #include "base/synchronization/waitable_event.h"
 #include "base/win/windows_types.h"
@@ -52,4 +52,4 @@ class ComServerModule {
 
 }  // namespace notification_helper
 
-#endif  // NOTIFICATION_HELPER_COM_SERVER_MODULE_H_
+#endif  // CHROME_NOTIFICATION_HELPER_COM_SERVER_MODULE_H_

chrome/notification_helper/notification_activator.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "notification_helper/notification_activator.h"
+#include "chrome/notification_helper/notification_activator.h"
 
 #include <shellapi.h>
 
@@ -11,8 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string16.h"
 #include "base/win/windows_types.h"
 #include "chrome/common/chrome_switches.h"
-#include "notification_helper/notification_helper_util.h"
-#include "notification_helper/trace_util.h"
+#include "chrome/notification_helper/notification_helper_util.h"
+#include "chrome/notification_helper/trace_util.h"
 
 namespace {
 

chrome/notification_helper/notification_activator.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef NOTIFICATION_HELPER_NOTIFICATION_ACTIVATOR_H_
-#define NOTIFICATION_HELPER_NOTIFICATION_ACTIVATOR_H_
+#ifndef CHROME_NOTIFICATION_HELPER_NOTIFICATION_ACTIVATOR_H_
+#define CHROME_NOTIFICATION_HELPER_NOTIFICATION_ACTIVATOR_H_
 
 #include <NotificationActivationCallback.h>
 #include <wrl/implements.h>
@@ -40,4 +40,4 @@ class NotificationActivator
 
 }  // namespace notification_helper
 
-#endif  // NOTIFICATION_HELPER_NOTIFICATION_ACTIVATOR_H_
+#endif  // CHROME_NOTIFICATION_HELPER_NOTIFICATION_ACTIVATOR_H_

chrome/notification_helper/notification_helper.cc
@@ -16,10 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/scoped_winrt_initializer.h"
 #include "chrome/install_static/product_install_details.h"
 #include "chrome/install_static/user_data_dir.h"
-#include "notification_helper/com_server_module.h"
-#include "notification_helper/notification_helper_crash_reporter_client.h"
-#include "notification_helper/notification_helper_util.h"
-#include "notification_helper/trace_util.h"
+#include "chrome/notification_helper/com_server_module.h"
+#include "chrome/notification_helper/notification_helper_crash_reporter_client.h"
+#include "chrome/notification_helper/notification_helper_util.h"
+#include "chrome/notification_helper/trace_util.h"
 
 extern "C" int WINAPI wWinMain(HINSTANCE instance,
                                HINSTANCE prev_instance,

chrome/notification_helper/notification_helper_crash_reporter_client.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "notification_helper/notification_helper_crash_reporter_client.h"
+#include "chrome/notification_helper/notification_helper_crash_reporter_client.h"
 
 #include <memory>
 

chrome/notification_helper/notification_helper_crash_reporter_client.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef NOTIFICATION_HELPER_NOTIFICATION_HELPER_CRASH_REPORTER_CLIENT_H_
-#define NOTIFICATION_HELPER_NOTIFICATION_HELPER_CRASH_REPORTER_CLIENT_H_
+#ifndef CHROME_NOTIFICATION_HELPER_NOTIFICATION_HELPER_CRASH_REPORTER_CLIENT_H_
+#define CHROME_NOTIFICATION_HELPER_NOTIFICATION_HELPER_CRASH_REPORTER_CLIENT_H_
 
 #include "base/files/file_path.h"
 #include "base/macros.h"
@@ -52,4 +52,4 @@ class NotificationHelperCrashReporterClient
   DISALLOW_COPY_AND_ASSIGN(NotificationHelperCrashReporterClient);
 };
 
-#endif  // NOTIFICATION_HELPER_NOTIFICATION_HELPER_CRASH_REPORTER_CLIENT_H_
+#endif  // CHROME_NOTIFICATION_HELPER_NOTIFICATION_HELPER_CRASH_REPORTER_CLIENT_H_

chrome/notification_helper/notification_helper_util.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "notification_helper/notification_helper_util.h"
+#include "chrome/notification_helper/notification_helper_util.h"
 
 #include "base/base_paths.h"
 #include "base/files/file_util.h"

chrome/notification_helper/notification_helper_util.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef NOTIFICATION_HELPER_NOTIFICATION_HELPER_UTIL_H_
-#define NOTIFICATION_HELPER_NOTIFICATION_HELPER_UTIL_H_
+#ifndef CHROME_NOTIFICATION_HELPER_NOTIFICATION_HELPER_UTIL_H_
+#define CHROME_NOTIFICATION_HELPER_NOTIFICATION_HELPER_UTIL_H_
 
 #include "base/files/file_path.h"
 
@@ -15,4 +15,4 @@ base::FilePath GetChromeExePath();
 
 }  // namespace notification_helper
 
-#endif  // NOTIFICATION_HELPER_NOTIFICATION_HELPER_UTIL_H_
+#endif  // CHROME_NOTIFICATION_HELPER_NOTIFICATION_HELPER_UTIL_H_

chrome/notification_helper/trace_util.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef NOTIFICATION_HELPER_TRACE_UTIL_H_
-#define NOTIFICATION_HELPER_TRACE_UTIL_H_
+#ifndef CHROME_NOTIFICATION_HELPER_TRACE_UTIL_H_
+#define CHROME_NOTIFICATION_HELPER_TRACE_UTIL_H_
 
 #if defined(NDEBUG)
 #define Trace(format, ...) ((void)0)
@@ -13,4 +13,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 void TraceImpl(const wchar_t* format, ...);
 #endif  // defined(NDEBUG)
 
-#endif  // NOTIFICATION_HELPER_TRACE_UTIL_H_
+#endif  // CHROME_NOTIFICATION_HELPER_TRACE_UTIL_H_

chrome/notification_helper/com_server_module_unittest.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "notification_helper/com_server_module.h"
+#include "chrome/notification_helper/com_server_module.h"
 
 #include <memory>
 

chrome/test/BUILD.gn
@@ -3185,7 +3185,7 @@ test("unit_tests") {
 
       deps += [ "//chrome/installer/util:with_no_strings" ]
 
-      data_deps += [ "//notification_helper" ]
+      data_deps += [ "//chrome/notification_helper" ]
     }
   }
 

testing/buildbot/gn_isolate_map.pyl
@@ -906,7 +906,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     "type": "additional_compile_target",
   },
   "notification_helper_unittests": {
-    "label": "//notification_helper:notification_helper_unittests",
+    "label": "//chrome/notification_helper:notification_helper_unittests",
     "type": "windowed_test_launcher",
   },
   "ocmock_support_unittests": {