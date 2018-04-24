fd07e46288f2d01a31647fbf6537e0ac1cbc4859
Xida Chen <xidachen@chromium.org>
2018-04-24 15:16:23 +0000

    Disable MusDemoTest.CheckMusDemoDraws on linux
    TBR=thestig@chromium.org
    NOTRY=true
    
    Bug: 836248
    Change-Id: I3664a670dd4b794b4a5a595988542336a8d9957b
    Reviewed-on: https://chromium-review.googlesource.com/1025643
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553122}

--- services/ui/demo/mus_demo_unittests.cc
+++ services/ui/demo/mus_demo_unittests.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/run_loop.h"
 #include "base/test/scoped_feature_list.h"
+#include "build/build_config.h"
 #include "services/service_manager/public/cpp/service_test.h"
 #include "services/ui/public/interfaces/constants.mojom.h"
 #include "services/ui/public/interfaces/window_server_test.mojom.h"
@@ -49,7 +50,13 @@ class MusDemoTest : public service_manager::test::ServiceTest {
 
 }  // namespace
 
-TEST_F(MusDemoTest, CheckMusDemoDraws) {
+// Timeout on linux: https://crbug.com/836248
+#if defined(OS_LINUX)
+#define MAYBE_CheckMusDemoDraws DISABLED_CheckMusDemoDraws
+#else
+#define MAYBE_CheckMusDemoDraws CheckMusDemoDraws
+#endif
+TEST_F(MusDemoTest, MAYBE_CheckMusDemoDraws) {
   connector()->StartService("mus_demo");
 
   ::ui::mojom::WindowServerTestPtr test_interface;
