63d909e510dc19f8c74ce682f047639a63a321d4
Xiaohui Chen <xiaohuic@chromium.org>
2018-04-24 17:13:11 +0000

    Revert "Fix build errors with upstream libassistant change"
    This reverts commit 89dbe1f811a13a818a17fa3f958b03e524190669.
    
    Reason for revert: This needs to be merged together with upstream rolldeps
    
    Original change's description:
    > Fix build errors with upstream libassistant change
    > 
    > Test: build locally with upreved libassistant
    > Bug: None
    > Change-Id: I3db67f5a77824fcf0831447f353baba6167b0801
    > Reviewed-on: https://chromium-review.googlesource.com/1025268
    > Reviewed-by: Muyuan Li <muyuanli@chromium.org>
    > Commit-Queue: Muyuan Li <muyuanli@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#552972}
    
    TBR=xiaohuic@chromium.org,muyuanli@chromium.org
    
    Change-Id: I9b3b13b11381b3100203428343251d2522b1c638
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: None
    Reviewed-on: https://chromium-review.googlesource.com/1026131
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553179}

--- chromeos/services/assistant/platform/audio_input_provider_impl.h
+++ chromeos/services/assistant/platform/audio_input_provider_impl.h
@@ -19,6 +19,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromeos {
 namespace assistant {
 
+// TODO(muyuanli): This class is currently a stub. It will need to be wired up
+// to use the real HW parameters.
+class AudioInputConfigImpl : public assistant_client::AudioInputConfig {
+ public:
+  AudioInputConfigImpl();
+  ~AudioInputConfigImpl() override;
+
+  // assistant_client::AudioInputConfigImpl overrides:
+  std::vector<int> GetSelectedChannels() const override;
+  float GetMicSensitivity() const override;
+  assistant_client::AudioInputConfig::InputType GetInputType() const override;
+  std::string GetMicManufacturer() const override;
+  std::string GetMicModel() const override;
+  std::string GetMicVersion() const override;
+  assistant_client::AudioInputConfig::MicState GetMicState() const override;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(AudioInputConfigImpl);
+};
+
 class AudioInputImpl : public assistant_client::AudioInput,
                        public mojom::AudioInputObserver {
  public:
@@ -52,10 +72,14 @@ class AudioInputProviderImpl : public assistant_client::AudioInputProvider {
   ~AudioInputProviderImpl() override;
 
   // assistant_client::AudioInputProvider overrides:
+  assistant_client::AudioInputConfig& GetAudioInputConfig() override;
   assistant_client::AudioInput& GetAudioInput() override;
+  // Assumes no config would change.
+  void RegisterConfigChangeCallback(ConfigChangeCallback callback) override {}
   int64_t GetCurrentAudioTime() override;
 
  private:
+  AudioInputConfigImpl audio_input_config_;
   AudioInputImpl audio_input_;
 
   DISALLOW_COPY_AND_ASSIGN(AudioInputProviderImpl);
