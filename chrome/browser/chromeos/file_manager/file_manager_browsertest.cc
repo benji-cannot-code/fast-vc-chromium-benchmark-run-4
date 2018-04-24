dc1f389bea923848df283d3f375cb87ef38dd186
Noel Gordon <noel@chromium.org>
2018-04-24 16:14:24 +0000

    Minor comment corrections to FileManagerBrowserTest helpers
    Comment only change.
    
    Tbr: slangley
    Bug: 833834
    Change-Id: I3e554d4fc2dd55811453601947016b49df2f63f7
    Reviewed-on: https://chromium-review.googlesource.com/1025500
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553152}

--- chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
@@ -23,7 +23,7 @@ namespace file_manager {
 // second is the JS test case name.
 typedef std::tuple<GuestMode, const char*> TestParameter;
 
-// Test class FileManager browser tests.
+// FileManager browser test class.
 class FileManagerBrowserTest :
       public FileManagerBrowserTestBase,
       public ::testing::WithParamInterface<TestParameter> {
@@ -45,7 +45,7 @@ IN_PROC_BROWSER_TEST_P(FileManagerBrowserTest, Test) {
   StartTest();
 }
 
-// Test class for FileManager browser tests that rely on deprecated event
+// FileManager browser test class for tests that rely on deprecated event
 // dispatch that send tests.
 class FileManagerBrowserTestWithLegacyEventDispatch
     : public FileManagerBrowserTest {
