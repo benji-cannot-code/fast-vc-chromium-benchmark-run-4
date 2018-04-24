126a56bb02e11bd29570c6c1a1edcb12028616f8
Misha Efimov <mef@chromium.org>
2018-04-24 17:14:16 +0000

    [Cronet] Sync Http2TestServer shutdown to fix race in BidirectionalStreamTest.
    Bug: b/78182827
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester
    Change-Id: If2a6eea9db1b80c15e05ff0e33767367e1e472e5
    Reviewed-on: https://chromium-review.googlesource.com/1025642
    Reviewed-by: Andrei Kapishnikov <kapishnikov@chromium.org>
    Commit-Queue: Misha Efimov <mef@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553181}

--- components/cronet/android/test/src/org/chromium/net/Http2TestServer.java
+++ components/cronet/android/test/src/org/chromium/net/Http2TestServer.java
@@ -48,7 +48,7 @@ public final class Http2TestServer {
 
     public static boolean shutdownHttp2TestServer() throws Exception {
         if (sServerChannel != null) {
-            sServerChannel.close();
+            sServerChannel.close().sync();
             sServerChannel = null;
             return true;
         }
