96db85b89b2aeff7a06a5bc248b7c4e6ae546f8e
Hajime Hoshi <hajimehoshi@chromium.org>
2018-04-24 07:24:25 +0000

    scheduler: Rename MainThreadTaskQueue::can_be_blocked_ to can_be_deferred_
    This CL is just for code health.
    
    Change-Id: I5bba4aa0ac3e4daedf8e5cebfc7bbdd1ca64c822
    Reviewed-on: https://chromium-review.googlesource.com/1025533
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553023}

--- third_party/blink/renderer/platform/scheduler/renderer/main_thread_task_queue.h
+++ third_party/blink/renderer/platform/scheduler/renderer/main_thread_task_queue.h
@@ -78,7 +78,7 @@ class PLATFORM_EXPORT MainThreadTaskQueue : public TaskQueue {
     explicit QueueCreationParams(QueueType queue_type)
         : queue_type(queue_type),
           spec(NameForQueueType(queue_type)),
-          can_be_blocked(false),
+          can_be_deferred(false),
           can_be_throttled(false),
           can_be_paused(false),
           can_be_stopped(false),
@@ -92,7 +92,7 @@ class PLATFORM_EXPORT MainThreadTaskQueue : public TaskQueue {
     }
 
     QueueCreationParams SetCanBeDeferred(bool value) {
-      can_be_blocked = value;
+      can_be_deferred = value;
       return *this;
     }
 
@@ -142,7 +142,7 @@ class PLATFORM_EXPORT MainThreadTaskQueue : public TaskQueue {
     TaskQueue::Spec spec;
     base::Optional<TaskQueue::QueuePriority> fixed_priority;
     FrameScheduler* frame_;
-    bool can_be_blocked;
+    bool can_be_deferred;
     bool can_be_throttled;
     bool can_be_paused;
     bool can_be_stopped;
@@ -160,7 +160,7 @@ class PLATFORM_EXPORT MainThreadTaskQueue : public TaskQueue {
     return fixed_priority_;
   }
 
-  bool CanBeDeferred() const { return can_be_blocked_; }
+  bool CanBeDeferred() const { return can_be_deferred_; }
 
   bool CanBeThrottled() const { return can_be_throttled_; }
 
@@ -204,7 +204,7 @@ class PLATFORM_EXPORT MainThreadTaskQueue : public TaskQueue {
   const QueueType queue_type_;
   const QueueClass queue_class_;
   const base::Optional<TaskQueue::QueuePriority> fixed_priority_;
-  const bool can_be_blocked_;
+  const bool can_be_deferred_;
   const bool can_be_throttled_;
   const bool can_be_paused_;
   const bool can_be_stopped_;
