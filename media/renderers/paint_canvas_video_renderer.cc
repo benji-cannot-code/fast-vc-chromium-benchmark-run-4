1af5854211b72a31530b4baaf2533f5317fbbb0b
Greg Daniel <egdaniel@chromium.org>
2018-04-24 17:05:08 +0000

    Move SkImage::getTextureHandle to getBackendTexture in media calls.
    In skia, getTextureHandle is deprecated and this change moves to using the
    new API.
    
    Change-Id: I69b507744e21187cfa9da74a960e5394acb2b524
    Reviewed-on: https://chromium-review.googlesource.com/1024370
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Greg Daniel <egdaniel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553170}

--- media/renderers/paint_canvas_video_renderer.cc
+++ media/renderers/paint_canvas_video_renderer.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/common/mailbox_holder.h"
 #include "media/base/data_buffer.h"
 #include "media/base/video_frame.h"
-#include "skia/ext/texture_handle.h"
 #include "third_party/libyuv/include/libyuv.h"
 #include "third_party/skia/include/core/SkImage.h"
 #include "third_party/skia/include/core/SkImageGenerator.h"
@@ -899,18 +898,19 @@ bool PaintCanvasVideoRenderer::CopyVideoFrameTexturesToGLTexture(
     if (!UpdateLastImage(video_frame, context_3d))
       return false;
 
-    const GrGLTextureInfo* texture_info =
-        skia::GrBackendObjectToGrGLTextureInfo(
-            last_image_.GetSkImage()->getTextureHandle(true));
-
-    if (!texture_info)
+    GrBackendTexture backend_texture =
+        last_image_.GetSkImage()->getBackendTexture(true);
+    if (!backend_texture.isValid())
+      return false;
+    GrGLTextureInfo texture_info;
+    if (!backend_texture.getGLTextureInfo(&texture_info))
       return false;
 
     gpu::gles2::GLES2Interface* canvas_gl = context_3d.gl;
     gpu::MailboxHolder mailbox_holder;
-    mailbox_holder.texture_target = texture_info->fTarget;
+    mailbox_holder.texture_target = texture_info.fTarget;
     canvas_gl->GenMailboxCHROMIUM(mailbox_holder.mailbox.name);
-    canvas_gl->ProduceTextureDirectCHROMIUM(texture_info->fID,
+    canvas_gl->ProduceTextureDirectCHROMIUM(texture_info.fID,
                                             mailbox_holder.mailbox.name);
 
     // Wait for mailbox creation on canvas context before consuming it and
@@ -1029,7 +1029,7 @@ bool PaintCanvasVideoRenderer::UpdateLastImage(
     const scoped_refptr<VideoFrame>& video_frame,
     const Context3D& context_3d) {
   if (!last_image_ || video_frame->timestamp() != last_timestamp_ ||
-      !last_image_.GetSkImage()->getTextureHandle(true)) {
+      !last_image_.GetSkImage()->getBackendTexture(true).isValid()) {
     ResetCache();
 
     auto paint_image_builder =
