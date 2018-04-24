d004142c42e21f10efd7263ea439847adb97e744
Mustafa Çamurcu <camurcu@google.com>
2018-04-24 21:51:01 +0000

    [ozone/windows] Windows platform
    This cl creates the windows platform for ozone.
    
    These changes are not sufficient for a successful Windows/Ozone build.
    See: https://chromium-review.googlesource.com/c/chromium/src/+/974285
    and https://swiftshader-review.googlesource.com/c/SwiftShader/+/17968
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: If27cd7b56d5503db6881cf1f2c563826f72f0be0
    Reviewed-on: https://chromium-review.googlesource.com/975902
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Commit-Queue: Mustafa Çamurcu <camurcu@google.com>
    Cr-Commit-Position: refs/heads/master@{#553300}

---
+++ ui/ozone/platform/windows/client_native_pixmap_factory_windows.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_OZONE_PLATFORM_WINDOWS_CLIENT_NATIVE_PIXMAP_FACTORY_WINDOWS_H_
+#define UI_OZONE_PLATFORM_WINDOWS_CLIENT_NATIVE_PIXMAP_FACTORY_WINDOWS_H_
+
+namespace gfx {
+class ClientNativePixmapFactory;
+}
+
+namespace ui {
+
+// Constructor hook for use in constructor_list.cc
+gfx::ClientNativePixmapFactory* CreateClientNativePixmapFactoryWindows();
+
+}  // namespace ui
+
+#endif  // UI_OZONE_PLATFORM_WINDOWS_CLIENT_NATIVE_PIXMAP_FACTORY_WINDOWS_H_
