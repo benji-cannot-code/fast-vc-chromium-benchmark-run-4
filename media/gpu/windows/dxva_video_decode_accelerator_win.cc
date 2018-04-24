56ef6178bbf0a03ba4f1d3178fba16d15b446971
liberato@chromium.org <liberato@chromium.org>
2018-04-24 19:45:48 +0000

    Add UMA for DXVA PictureBufferMechanism.
    To help us figure out how to prioritize the various picture buffer
    types in the new D3D11VideoDecoder, this UMA records how often
    each is used in the DXVA VDA.
    
    Bug: 835972
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I39b41000b54b932ce8cc592893bef1826f458308
    Reviewed-on: https://chromium-review.googlesource.com/1024927
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553236}

--- media/gpu/windows/dxva_video_decode_accelerator_win.cc
+++ media/gpu/windows/dxva_video_decode_accelerator_win.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/macros.h"
 #include "base/memory/shared_memory.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/path_service.h"
 #include "base/single_thread_task_runner.h"
 #include "base/stl_util.h"
@@ -879,6 +880,9 @@ bool DXVAVideoDecodeAccelerator::Initialize(const Config& config,
     config_change_detector_.reset(new VP9ConfigChangeDetector);
 
   SetState(kNormal);
+
+  UMA_HISTOGRAM_ENUMERATION("Media.DXVAVDA.PictureBufferMechanism",
+                            GetPictureBufferMechanism());
 
   return StartDecoderThread();
 }
