a0250a3123b65c8e3ad3805d89fe7128afcb8e0a
Yoichi Osato <yoichio@chromium.org>
2018-04-24 08:08:11 +0000

    Disable flaky PartnerHomepageUnitTest#testNoProvider
    TBR: newt@chromium.org
    Bug: 836110
    Change-Id: I32e287ae93a48cb62d0d9de69a5fdbc8095ad571
    Reviewed-on: https://chromium-review.googlesource.com/1025670
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553029}

--- chrome/android/javatests/src/org/chromium/chrome/browser/partnercustomizations/PartnerHomepageUnitTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/partnercustomizations/PartnerHomepageUnitTest.java
@@ -22,6 +22,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.metrics.RecordHistogram;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.chrome.browser.ChromeFeatureList;
@@ -135,10 +136,12 @@ public class PartnerHomepageUnitTest {
 
     /**
      * Everything is enabled for using partner homepage, except that there is no actual provider.
+     * Flaky : http://crbug.com/836110
      */
     @Test
     @SmallTest
     @Feature({"Homepage"})
+    @DisabledTest
     public void testNoProvider() throws InterruptedException {
         mHomepageManager.setPrefHomepageEnabled(true);
         mHomepageManager.setPrefHomepageUseDefaultUri(true);
