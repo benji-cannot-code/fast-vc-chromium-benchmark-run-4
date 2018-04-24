ddabc4112d7aad9b191f924d06192d46fdd9e912
Antonio Gomes <tonikitoo@igalia.com>
2018-04-24 16:04:02 +0000

    Minor clean ups in user_image_manager_browsertest.cc
    Driven by preparation CL for migrating ProfileDownloader
    from using URLFetcher to SimpleURLLoader.
    
    CL basically removes a parameter being unused, and a bogus
    include (gurl.h).
    
    TBR=xiyuan@chromium.org
    BUG=None
    
    Change-Id: Ia8b5d78042d1166537487b926d7479eefdecc9e2
    Reviewed-on: https://chromium-review.googlesource.com/1025910
    Reviewed-by: Antonio Gomes <tonikitoo@igalia.com>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#553144}

--- chrome/browser/profiles/profile_downloader.h
+++ chrome/browser/profiles/profile_downloader.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "google_apis/gaia/oauth2_token_service.h"
 #include "net/url_request/url_fetcher_delegate.h"
 #include "third_party/skia/include/core/SkBitmap.h"
-#include "url/gurl.h"
 
 class ProfileDownloaderDelegate;
 
