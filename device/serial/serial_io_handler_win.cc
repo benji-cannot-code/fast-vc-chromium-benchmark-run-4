29b60317fb5731fd987f894cfb666fbefc29797c
Gabriel Charette <gab@chromium.org>
2018-04-24 21:39:48 +0000

    Migrate MessageLoop::current() to MessageLoopCurrent in /device
    These changes were scripted as part of the effort to restrict exposure of the
    raw MessageLoop*.
    
    Note that many static methods exposed on MessageLoopCurrent are also deprecated
    so it is very well possible that this is migrating to an already deprecated
    method.
    
    The goal of this pass is to reduce usage of MessageLoop::current() (and
    ultimately make it impossible to obtain the MessageLoop* statically).
    
    As such I will not edit this CL unless the script did something logically wrong.
    I defer to owners to fix highlighted usage of already deprecated APIs.
    
    Possible script screw ups / things to look out for in this review:
     - Storing MessageLoopCurrent in a MessageLoop* variable or comparing it against
       one (I will go over that in a separate pass).
    
    Includes should have been stripped if that was the last usage of message_loop.h
    in that file.
    
    Please CQ if LGTY
    
    This CL was uploaded by git cl split.
    
    R=rockot@chromium.org
    
    Bug: 825327
    Change-Id: If17acb98d53b8a6a352a0c09699ddb60915393d0
    Reviewed-on: https://chromium-review.googlesource.com/1024808
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553292}

--- device/serial/serial_io_handler_win.cc
+++ device/serial/serial_io_handler_win.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/macros.h"
-#include "base/message_loop/message_loop.h"
+#include "base/message_loop/message_loop_current.h"
 #include "base/scoped_observer.h"
 #include "base/sequence_checker.h"
 #include "device/base/device_info_query_win.h"
@@ -238,8 +238,8 @@ bool SerialIoHandlerWin::PostOpen() {
   DCHECK(!read_context_);
   DCHECK(!write_context_);
 
-  base::MessageLoopForIO::current()->RegisterIOHandler(file().GetPlatformFile(),
-                                                       this);
+  base::MessageLoopCurrentForIO::Get()->RegisterIOHandler(
+      file().GetPlatformFile(), this);
 
   comm_context_.reset(new base::MessagePumpForIO::IOContext());
   read_context_.reset(new base::MessagePumpForIO::IOContext());
