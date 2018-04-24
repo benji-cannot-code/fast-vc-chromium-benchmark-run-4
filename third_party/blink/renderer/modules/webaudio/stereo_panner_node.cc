613e681039d851539b1d7e590b5babf63c7e8504
Raymond Toy <rtoy@chromium.org>
2018-04-24 02:26:20 +0000

    Implement selectable AudioParam rate
    AudioParam has a new attribute, automationRate, to allow the user to choose whether
    the AudioParam is "a-rate" or "k-rate".  Add support for setting and getting
    the attribute.
    
    Update all AudioParams to support a-rate and k-rate when allowed.
    
    Spec: https://webaudio.github.io/web-audio-api/#dom-audioparam-automationrate
    Intent: https://groups.google.com/a/chromium.org/d/msg/blink-dev/extLjVzLw7s/9XUZvQqcBQAJ
    
    Bug: 828947
    Change-Id: Ibb9fe8bc0dedf3cfdcf33727734b5d66e5a7ca2f
    Reviewed-on: https://chromium-review.googlesource.com/945076
    Commit-Queue: Raymond Toy <rtoy@chromium.org>
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552965}

--- third_party/blink/renderer/modules/webaudio/stereo_panner_node.cc
+++ third_party/blink/renderer/modules/webaudio/stereo_panner_node.cc
@@ -146,6 +146,8 @@ StereoPannerNode::StereoPannerNode(BaseAudioContext& context)
                               kParamTypeStereoPannerPan,
                               "StereoPanner.pan",
                               0,
+                              AudioParamHandler::AutomationRate::kAudio,
+                              AudioParamHandler::AutomationRateMode::kVariable,
                               -1,
                               1)) {
   SetHandler(StereoPannerHandler::Create(*this, context.sampleRate(),
