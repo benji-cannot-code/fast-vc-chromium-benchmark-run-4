2037d60f8e903ed79901194a21ce8b11b8d4a2a3
Noel Gordon <noel@chromium.org>
2018-04-24 16:07:36 +0000

    Re-enable FolderShortcuts and Traverse FileManagerBrowserTests
    Enabled in RELEASE. Run these tests again on remaining chrome-os bots:
    DEBUG/MSAN/ASAN.
    
      FolderShortcuts/FileManagerBrowserTest.Test*
      Traverse/FileManagerBrowserTest.Test*
    
    Tbr: slangley
    Bug: 836254
    Change-Id: I901cd286fdc25147e17656bb54e423756c2486f9
    Reviewed-on: https://chromium-review.googlesource.com/1025499
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553147}

--- chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
@@ -324,13 +324,8 @@ WRAPPED_INSTANTIATE_TEST_CASE_P(
                       TestParameter(NOT_IN_GUEST_MODE,
                                     "restoreGeometryMaximizedState")));
 
-#if defined(DISABLE_SLOW_FILESAPP_TESTS)
-#define MAYBE_Traverse DISABLED_Traverse
-#else
-#define MAYBE_Traverse Traverse
-#endif
 WRAPPED_INSTANTIATE_TEST_CASE_P(
-    MAYBE_Traverse,
+    Traverse,
     FileManagerBrowserTest,
     ::testing::Values(TestParameter(IN_GUEST_MODE, "traverseDownloads"),
                       TestParameter(NOT_IN_GUEST_MODE, "traverseDownloads"),
@@ -395,13 +390,8 @@ WRAPPED_INSTANTIATE_TEST_CASE_P(
         TestParameter(NOT_IN_GUEST_MODE, "genericTaskIsNotExecuted"),
         TestParameter(NOT_IN_GUEST_MODE, "genericAndNonGenericTasksAreMixed")));
 
-#if defined(DISABLE_SLOW_FILESAPP_TESTS)
-#define MAYBE_FolderShortcuts DISABLED_FolderShortcuts
-#else
-#define MAYBE_FolderShortcuts FolderShortcuts
-#endif
 WRAPPED_INSTANTIATE_TEST_CASE_P(
-    MAYBE_FolderShortcuts,
+    FolderShortcuts,
     FileManagerBrowserTest,
     ::testing::Values(
         TestParameter(NOT_IN_GUEST_MODE, "traverseFolderShortcuts"),
