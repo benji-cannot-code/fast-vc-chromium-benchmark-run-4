fc578d8b8760b86982e3fc4b50fed17969570a6b
Danyao Wang <danyao@chromium.org>
2018-04-24 15:13:38 +0000

    [Nav Experiment] Reimplement restore_session.html using URL fragment.
    The WebKit History API bug only affects push/replaceState calls that
    change the URL path and query from file:// scheme. It turns out that
    changing URL fragment is still allowed. This CL reimplements restore
    session behavior by encoding all data in URL fragment and removes the
    iOS11.3 guard for WebClient::IsSlimNavigationManagerEnabled().
    
    Bug: 814803
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Ib26734b2d62cbcfc1d8f8844e317fdb5842b6b16
    Reviewed-on: https://chromium-review.googlesource.com/1024929
    Commit-Queue: Danyao Wang <danyao@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553119}

--- ios/web/navigation/wk_navigation_util_unittest.mm
+++ ios/web/navigation/wk_navigation_util_unittest.mm
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #import "ios/web/navigation/navigation_item_impl.h"
 #include "ios/web/test/test_url_constants.h"
-#include "net/base/url_util.h"
+#include "net/base/escape.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/platform_test.h"
 
@@ -46,11 +46,13 @@ TEST_F(WKNavigationUtilTest, CreateRestoreSessionUrl) {
       CreateRestoreSessionUrl(0 /* last_committed_item_index */, items);
   ASSERT_TRUE(IsRestoreSessionUrl(restore_session_url));
 
-  std::string session_json;
-  net::GetValueForKeyInQuery(restore_session_url,
-                             kRestoreSessionSessionQueryKey, &session_json);
+  net::UnescapeRule::Type unescape_rules =
+      net::UnescapeRule::URL_SPECIAL_CHARS_EXCEPT_PATH_SEPARATORS |
+      net::UnescapeRule::SPACES | net::UnescapeRule::PATH_SEPARATORS;
+  std::string session_json =
+      net::UnescapeURLComponent(restore_session_url.ref(), unescape_rules);
   EXPECT_EQ(
-      "{\"offset\":-2,\"titles\":[\"Test Website 0\",\"\",\"\"],"
+      "session={\"offset\":-2,\"titles\":[\"Test Website 0\",\"\",\"\"],"
       "\"urls\":[\"http://www.0.com/\",\"http://www.1.com/\","
       "\"about:blank?for=testwebui%3A%2F%2Fwebui%2F\"]}",
       session_json);
