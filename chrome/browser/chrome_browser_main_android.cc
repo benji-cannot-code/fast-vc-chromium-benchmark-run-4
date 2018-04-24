2dcded55894c05db27c8f1a7ec48939393000461
Gabriel Charette <gab@chromium.org>
2018-04-24 12:21:25 +0000

    Migrate MessageLoop::current() to MessageLoopCurrent in /chrome/browser/chrome_browser_main_android.cc
    These changes were scripted as part of the effort to restrict exposure of the
    raw MessageLoop*.
    
    Note that many static methods exposed on MessageLoopCurrent are also deprecated
    so it is very well possible that this is migrating to an already deprecated
    method.
    
    The goal of this pass is to reduce usage of MessageLoop::current() (and
    ultimately make it impossible to obtain the MessageLoop* statically).
    
    As such I will not edit this CL unless the script did something logically wrong.
    I defer to owners to fix highlighted usage of already deprecated APIs.
    
    Possible script screw ups / things to look out for in this review:
     - Storing MessageLoopCurrent in a MessageLoop* variable or comparing it against
       one (I will go over that in a separate pass).
    
    Includes should have been stripped if that was the last usage of message_loop.h
    in that file.
    
    Please CQ if LGTY
    
    This CL was uploaded by git cl split.
    
    R=bauerb@chromium.org
    
    Bug: 825327
    Change-Id: Ia10a44445d3e9b765511fda5e1135afc0d32a338
    Reviewed-on: https://chromium-review.googlesource.com/1024710
    Commit-Queue: Bernhard Bauer <bauerb@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553056}

--- chrome/browser/chrome_browser_main_android.cc
+++ chrome/browser/chrome_browser_main_android.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/files/file_path.h"
 #include "base/message_loop/message_loop.h"
+#include "base/message_loop/message_loop_current.h"
 #include "base/path_service.h"
 #include "base/task_scheduler/post_task.h"
 #include "base/trace_event/trace_event.h"
@@ -115,7 +116,7 @@ int ChromeBrowserMainPartsAndroid::PreEarlyInitialization() {
   {
     TRACE_EVENT0("startup",
       "ChromeBrowserMainPartsAndroid::PreEarlyInitialization:StartUiMsgLoop");
-    base::MessageLoopForUI::current()->Start();
+    base::MessageLoopCurrentForUI::Get()->Start();
   }
 
   // In order for SetLoadSecondaryLocalePaks() to work ResourceBundle must
