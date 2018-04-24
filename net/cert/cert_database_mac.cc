2c2dffd39ae52bb38d0e5a0d77af808294ed2508
Gabriel Charette <gab@chromium.org>
2018-04-24 22:04:24 +0000

    Migrate MessageLoop::current() to MessageLoopCurrent in /net/cert
    These changes were scripted as part of the effort to restrict exposure of the
    raw MessageLoop*.
    
    Note that many static methods exposed on MessageLoopCurrent are also deprecated
    so it is very well possible that this is migrating to an already deprecated
    method.
    
    The goal of this pass is to reduce usage of MessageLoop::current() (and
    ultimately make it impossible to obtain the MessageLoop* statically).
    
    As such I will not edit this CL unless the script did something logically wrong.
    I defer to owners to fix highlighted usage of already deprecated APIs.
    
    Possible script screw ups / things to look out for in this review:
     - Storing MessageLoopCurrent in a MessageLoop* variable or comparing it against
       one (I will go over that in a separate pass).
    
    Includes should have been stripped if that was the last usage of message_loop.h
    in that file.
    
    Please CQ if LGTY
    
    This CL was uploaded by git cl split.
    
    R=eroman@chromium.org
    
    Bug: 825327
    Change-Id: I981a208163dbff6560debcbdcd2dfb70f64ef717
    Reviewed-on: https://chromium-review.googlesource.com/1024406
    Reviewed-by: Eric Roman <eroman@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553308}

--- net/cert/cert_database_mac.cc
+++ net/cert/cert_database_mac.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/mac/mac_logging.h"
 #include "base/message_loop/message_loop.h"
+#include "base/message_loop/message_loop_current.h"
 #include "base/observer_list_threadsafe.h"
 #include "base/process/process_handle.h"
 #include "base/single_thread_task_runner.h"
@@ -120,7 +121,7 @@ void CertDatabase::SetMessageLoopForKeychainEvents() {
   if (notifier_.get())
     notifier_.release()->Shutdown();
 
-  notifier_.reset(new Notifier(this, base::MessageLoopForUI::current()));
+  notifier_.reset(new Notifier(this, base::MessageLoopCurrentForUI::Get()));
 }
 
 CertDatabase::CertDatabase()
