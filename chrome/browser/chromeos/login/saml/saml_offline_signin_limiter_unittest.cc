c981590a00d69477f21425a206d3ba157d917400
Gabriel Charette <gab@chromium.org>
2018-04-24 06:38:12 +0000

    Migrate MessageLoop::current() to MessageLoopCurrent in /chrome/browser/chromeos/login
    These changes were scripted as part of the effort to restrict exposure of the
    raw MessageLoop*.
    
    Note that many static methods exposed on MessageLoopCurrent are also deprecated
    so it is very well possible that this is migrating to an already deprecated
    method.
    
    The goal of this pass is to reduce usage of MessageLoop::current() (and
    ultimately make it impossible to obtain the MessageLoop* statically).
    
    As such I will not edit this CL unless the script did something logically wrong.
    I defer to owners to fix highlighted usage of already deprecated APIs.
    
    Possible script screw ups / things to look out for in this review:
     - Storing MessageLoopCurrent in a MessageLoop* variable or comparing it against
       one (I will go over that in a separate pass).
    
    Includes should have been stripped if that was the last usage of message_loop.h
    in that file.
    
    Please CQ if LGTY
    
    This CL was uploaded by git cl split.
    
    R=alemate@chromium.org
    
    Bug: 825327
    Change-Id: I94fa0384312871ab8e645ad3174f5e380e32a77d
    Reviewed-on: https://chromium-review.googlesource.com/1024287
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553019}

--- chrome/browser/chromeos/login/saml/saml_offline_signin_limiter_unittest.cc
+++ chrome/browser/chromeos/login/saml/saml_offline_signin_limiter_unittest.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/ref_counted.h"
-#include "base/message_loop/message_loop.h"
+#include "base/message_loop/message_loop_current.h"
 #include "base/test/simple_test_clock.h"
 #include "base/test/test_simple_task_runner.h"
 #include "base/time/clock.h"
@@ -109,7 +109,7 @@ void SAMLOfflineSigninLimiterTest::SetUpUserManager() {
 }
 
 void SAMLOfflineSigninLimiterTest::SetUp() {
-  base::MessageLoop::current()->SetTaskRunner(runner_);
+  base::MessageLoopCurrent::Get()->SetTaskRunner(runner_);
   profile_.reset(new TestingProfile);
 
   SAMLOfflineSigninLimiterFactory::SetClockForTesting(&clock_);
