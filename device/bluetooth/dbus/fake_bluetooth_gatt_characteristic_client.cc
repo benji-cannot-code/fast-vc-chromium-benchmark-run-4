0f7d9224febdcaa63286879720dc2376c85aaf3f
Qiyu Hu <qiyuh@google.com>
2018-04-24 23:17:28 +0000

    Update the dbus error message for gatt in bluez
    This was updated quite a while ago in bluez.
    https://chromium.git.corp.google.com/chromiumos/third_party/bluez/+/48fe55b7610257bdb9ea075f1382440ae013be42%5E%21/#F0
    
    Roll src/third_party/cros_system_api/ dea305b1a..62d5021bb (1 commit)
    https://chromium.googlesource.com/chromiumos/platform/system_api.git/+log/dea305b1a70d..62d5021bb6d4
    
    $ git log dea305b1a..62d5021bb --date=short --no-merges --format='%ad %ae %s'
    2018-04-23 qiyuh Update gatt error message in dbus for bluez.
    
    Created with:
      roll-dep src/third_party/cros_system_api
    
    BUG=b:69965109
    TEST=Pass CtsVerifier test on reading w/o permissions
    
    Change-Id: Id0eb9122c02ca255ebbb50caf718d9020fd8d1ab
    Reviewed-on: https://chromium-review.googlesource.com/1026075
    Reviewed-by: Luis Hector Chavez <lhchavez@chromium.org>
    Reviewed-by: Rahul Chaturvedi <rkc@chromium.org>
    Commit-Queue: Qiyu Hu <qiyuh@google.com>
    Cr-Commit-Position: refs/heads/master@{#553349}

--- device/bluetooth/dbus/fake_bluetooth_gatt_characteristic_client.cc
+++ device/bluetooth/dbus/fake_bluetooth_gatt_characteristic_client.cc
@@ -149,7 +149,7 @@ void FakeBluetoothGattCharacteristicClient::ReadValue(
   }
 
   if (object_path.value() == heart_rate_control_point_path_) {
-    error_callback.Run(bluetooth_gatt_service::kErrorReadNotPermitted,
+    error_callback.Run(bluetooth_gatt_service::kErrorNotPermitted,
                        "Reads of this value are not allowed");
     return;
   }
@@ -227,7 +227,7 @@ void FakeBluetoothGattCharacteristicClient::WriteValue(
   }
 
   if (object_path.value() != heart_rate_control_point_path_) {
-    error_callback.Run(bluetooth_gatt_service::kErrorWriteNotPermitted,
+    error_callback.Run(bluetooth_gatt_service::kErrorNotPermitted,
                        "Writes of this value are not allowed");
     return;
   }
