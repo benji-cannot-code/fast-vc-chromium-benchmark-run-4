479c68ba6e1bfe24b6e0accc83bb4926f5231033
Gabriel Charette <gab@chromium.org>
2018-04-24 21:33:42 +0000

    Migrate MessageLoop::current() to MessageLoopCurrent in /net/spdy
    These changes were scripted as part of the effort to restrict exposure of the
    raw MessageLoop*.
    
    Note that many static methods exposed on MessageLoopCurrent are also deprecated
    so it is very well possible that this is migrating to an already deprecated
    method.
    
    The goal of this pass is to reduce usage of MessageLoop::current() (and
    ultimately make it impossible to obtain the MessageLoop* statically).
    
    As such I will not edit this CL unless the script did something logically wrong.
    I defer to owners to fix highlighted usage of already deprecated APIs.
    
    Possible script screw ups / things to look out for in this review:
     - Storing MessageLoopCurrent in a MessageLoop* variable or comparing it against
       one (I will go over that in a separate pass).
    
    Includes should have been stripped if that was the last usage of message_loop.h
    in that file.
    
    Please CQ if LGTY
    
    This CL was uploaded by git cl split.
    
    R=rch@chromium.org
    
    Bug: 825327
    Change-Id: I683a3fc7b20d41d4d069fb9e7fd0b4cdcf915028
    Reviewed-on: https://chromium-review.googlesource.com/1024494
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553285}

--- net/spdy/chromium/spdy_session_test_util.cc
+++ net/spdy/chromium/spdy_session_test_util.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/spdy/chromium/spdy_session_test_util.h"
 
 #include "base/location.h"
+#include "base/message_loop/message_loop_current.h"
 #include "base/strings/string_util.h"
 
 namespace net {
@@ -14,11 +15,11 @@ SpdySessionTestTaskObserver::SpdySessionTestTaskObserver(
     const SpdyString& file_name,
     const SpdyString& function_name)
     : executed_count_(0), file_name_(file_name), function_name_(function_name) {
-  base::MessageLoop::current()->AddTaskObserver(this);
+  base::MessageLoopCurrent::Get()->AddTaskObserver(this);
 }
 
 SpdySessionTestTaskObserver::~SpdySessionTestTaskObserver() {
-  base::MessageLoop::current()->RemoveTaskObserver(this);
+  base::MessageLoopCurrent::Get()->RemoveTaskObserver(this);
 }
 
 void SpdySessionTestTaskObserver::WillProcessTask(
