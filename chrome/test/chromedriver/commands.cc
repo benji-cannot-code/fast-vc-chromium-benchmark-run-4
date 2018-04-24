eda54d03cbcaaabd8307a97bb29dc61fba0f0494
Gabriel Charette <gab@chromium.org>
2018-04-24 06:13:32 +0000

    Migrate MessageLoop::current() to MessageLoopCurrent in /chrome/test/chromedriver
    These changes were scripted as part of the effort to restrict exposure of the
    raw MessageLoop*.
    
    Note that many static methods exposed on MessageLoopCurrent are also deprecated
    so it is very well possible that this is migrating to an already deprecated
    method.
    
    The goal of this pass is to reduce usage of MessageLoop::current() (and
    ultimately make it impossible to obtain the MessageLoop* statically).
    
    As such I will not edit this CL unless the script did something logically wrong.
    I defer to owners to fix highlighted usage of already deprecated APIs.
    
    Possible script screw ups / things to look out for in this review:
     - Storing MessageLoopCurrent in a MessageLoop* variable or comparing it against
       one (I will go over that in a separate pass).
    
    Includes should have been stripped if that was the last usage of message_loop.h
    in that file.
    
    Please CQ if LGTY
    
    This CL was uploaded by git cl split.
    
    R=stgao@chromium.org
    
    Bug: 825327
    Change-Id: Id0af6c00456644d29aa8553a9bafab0a2d92bb4f
    Reviewed-on: https://chromium-review.googlesource.com/1024479
    Commit-Queue: John Chen <johnchen@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553016}

--- chrome/test/chromedriver/commands.cc
+++ chrome/test/chromedriver/commands.cc
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/memory/linked_ptr.h"
 #include "base/memory/ptr_util.h"
-#include "base/message_loop/message_loop.h"
+#include "base/message_loop/message_loop_current.h"
 #include "base/run_loop.h"
 #include "base/single_thread_task_runner.h"
 #include "base/stl_util.h"
@@ -134,7 +134,7 @@ void ExecuteGetSessions(const Command& session_capabilities_command,
   }
   base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
       FROM_HERE, run_loop.QuitClosure(), base::TimeDelta::FromSeconds(10));
-  base::MessageLoop::current()->SetNestableTasksAllowed(true);
+  base::MessageLoopCurrent::Get()->SetNestableTasksAllowed(true);
   run_loop.Run();
 
   callback.Run(Status(kOk), std::move(session_list), session_id, false);
@@ -186,7 +186,7 @@ void ExecuteQuitAll(
       FROM_HERE, run_loop.QuitClosure(), base::TimeDelta::FromSeconds(10));
   // Uses a nested run loop to block this thread until all the quit
   // commands have executed, or the timeout expires.
-  base::MessageLoop::current()->SetNestableTasksAllowed(true);
+  base::MessageLoopCurrent::Get()->SetNestableTasksAllowed(true);
   run_loop.Run();
   callback.Run(Status(kOk), std::unique_ptr<base::Value>(),
                session_id, false);
