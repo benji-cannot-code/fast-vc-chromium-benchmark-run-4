43a9670180910d351c92ce9a2280b133e68b369c
Jun Mukai <mukai@google.com>
2018-04-24 01:40:46 +0000

    fix a crash on exiting tablet mode
    Bug: 831423
    Test: None
    Change-Id: If8e8a2e88d009dfea258b79f6b914444f28816ee
    Reviewed-on: https://chromium-review.googlesource.com/1024735
    Reviewed-by: Dan Erat <derat@chromium.org>
    Commit-Queue: Jun Mukai <mukai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552948}

--- ash/touch/touch_devices_controller.cc
+++ ash/touch/touch_devices_controller.cc
@@ -192,8 +192,8 @@ void TouchDevicesController::UpdateTouchpadEnabled() {
   if (!GetInputDeviceControllerClient())
     return;  // Happens in tests.
 
-  bool enabled = global_touchpad_enabled_ &&
-                 GetActivePrefService()->GetBoolean(prefs::kTouchpadEnabled);
+  bool enabled = GetTouchpadEnabled(TouchDeviceEnabledSource::GLOBAL) &&
+                 GetTouchpadEnabled(TouchDeviceEnabledSource::USER_PREF);
 
   GetInputDeviceControllerClient()->SetInternalTouchpadEnabled(
       enabled, base::BindRepeating(&OnSetTouchpadEnabledDone, enabled));
