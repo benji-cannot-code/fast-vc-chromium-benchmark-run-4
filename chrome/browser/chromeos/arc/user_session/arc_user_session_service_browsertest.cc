78961f7b3f84dfc93222258918d740261d90bc1a
Gabriel Charette <gab@chromium.org>
2018-04-24 16:17:09 +0000

    Migrate MessageLoop::current() to MessageLoopCurrent in /chrome/browser/chromeos/arc
    These changes were scripted as part of the effort to restrict exposure of the
    raw MessageLoop*.
    
    Note that many static methods exposed on MessageLoopCurrent are also deprecated
    so it is very well possible that this is migrating to an already deprecated
    method.
    
    The goal of this pass is to reduce usage of MessageLoop::current() (and
    ultimately make it impossible to obtain the MessageLoop* statically).
    
    As such I will not edit this CL unless the script did something logically wrong.
    I defer to owners to fix highlighted usage of already deprecated APIs.
    
    Possible script screw ups / things to look out for in this review:
     - Storing MessageLoopCurrent in a MessageLoop* variable or comparing it against
       one (I will go over that in a separate pass).
    
    Includes should have been stripped if that was the last usage of message_loop.h
    in that file.
    
    Please CQ if LGTY
    
    This CL was uploaded by git cl split.
    
    R=lhchavez@chromium.org
    
    Bug: 825327
    Change-Id: I40de071dcef2e6356ce03a851a36e85722b4f00a
    Reviewed-on: https://chromium-review.googlesource.com/1024317
    Commit-Queue: Luis Hector Chavez <lhchavez@chromium.org>
    Reviewed-by: Luis Hector Chavez <lhchavez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553154}

--- chrome/browser/chromeos/arc/user_session/arc_user_session_service_browsertest.cc
+++ chrome/browser/chromeos/arc/user_session/arc_user_session_service_browsertest.cc
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
-#include "base/message_loop/message_loop.h"
+#include "base/message_loop/message_loop_current.h"
 #include "base/run_loop.h"
 #include "chrome/browser/chromeos/arc/user_session/arc_user_session_service.h"
 #include "chrome/test/base/in_process_browser_test.h"
@@ -36,7 +36,7 @@ int CountBroadcasts(
 }
 
 void RunUntilIdle() {
-  DCHECK(base::MessageLoop::current());
+  DCHECK(base::MessageLoopCurrent::Get());
   base::RunLoop().RunUntilIdle();
 }
 
