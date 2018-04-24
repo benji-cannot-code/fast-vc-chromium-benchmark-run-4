77fca55688b6b443ca6d2fe0ac63cb3c7e48f73a
Noel Gordon <noel@chromium.org>
2018-04-24 00:21:52 +0000

    Make Gallery browser test consistent with other browser tests
    Use protected. Don't abbr the template parameter (use MODE).
    
    No change in behavior, no new tests.
    
    Bug: 833834
    Change-Id: I1fd84e01fbb22ed93b2951986afeb408c448e281
    Reviewed-on: https://chromium-review.googlesource.com/1023680
    Reviewed-by: Luciano Pacheco (SYD) <lucmult@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552920}

--- chrome/browser/chromeos/file_manager/gallery_browsertest.cc
+++ chrome/browser/chromeos/file_manager/gallery_browsertest.cc
@@ -7,19 +7,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace file_manager {
 
-template <GuestMode M>
+template <GuestMode MODE>
 class GalleryBrowserTestBase : public FileManagerBrowserTestBase {
  public:
-  GuestMode GetGuestModeParam() const override { return M; }
+  GuestMode GetGuestModeParam() const override { return MODE; }
+
   const char* GetTestCaseNameParam() const override {
     return test_case_name_.c_str();
   }
 
- protected:
   const char* GetTestManifestName() const override {
     return "gallery_test_manifest.json";
   }
 
+ protected:
   void set_test_case_name(const std::string& name) { test_case_name_ = name; }
 
  private:
