d0a44d4de928ba44f9c6e643a05ddf9818c85c82
Steven Valdez <svaldez@chromium.org>
2018-04-24 21:52:30 +0000

    Add BidirectionalStreamRequestInfo bit to force early data.
    Change-Id: Ib33e2722e02d84e043f4d1b962490a64d6eee464
    Reviewed-on: https://chromium-review.googlesource.com/1026210
    Commit-Queue: Steven Valdez <svaldez@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553301}

--- net/http/bidirectional_stream_request_info.cc
+++ net/http/bidirectional_stream_request_info.cc
@@ -8,7 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace net {
 
 BidirectionalStreamRequestInfo::BidirectionalStreamRequestInfo()
-    : priority(LOW), end_stream_on_headers(false) {}
+    : allow_early_data_override(false),
+      priority(LOW),
+      end_stream_on_headers(false) {}
 
 BidirectionalStreamRequestInfo::~BidirectionalStreamRequestInfo() = default;
 
