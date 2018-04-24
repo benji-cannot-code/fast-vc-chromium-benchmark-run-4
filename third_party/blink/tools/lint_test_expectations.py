2bef6d87554c428d77a396230dbd850f532ce63a
Kent Tamura <tkent@chromium.org>
2018-04-24 23:26:28 +0000

    Add //thid_party/blink/tools/lint_test_expectations.py
    It will supersedes //third_party/WebKit/Tools/Scripts/lint-test-expectations.
    lint-test-expectaions isn't removed yet due to dependency from
    build/scripts/slave/chromium/.
    
    Bug: 829697
    Change-Id: I213b413304240e2f8672a1d2209a2800331b1ade
    Reviewed-on: https://chromium-review.googlesource.com/1025437
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553357}

---
+++ third_party/blink/tools/lint_test_expectations.py
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/env vpython
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import sys
+
+from blinkpy.common import version_check  # pylint: disable=unused-import
+from blinkpy.web_tests.lint_test_expectations import main
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:], sys.stderr))
