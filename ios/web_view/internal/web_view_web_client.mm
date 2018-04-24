4b875799f1ad600f7c5dbf6cef048e40696c1564
John Z Wu <jzw@chromium.org>
2018-04-24 22:08:53 +0000

    Bundle component strings in //ios/web_view.
    Fixes this warning log:
    [0411/153122.161864:WARNING:resource_bundle.cc(565)] locale resources are not loaded
    
    Also shows "This plugin is not supported" when visiting websites with flash content.
    
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I33e7e65dcb8193a93f5208f847a39a20e8472987
    Bug: 811603
    Reviewed-on: https://chromium-review.googlesource.com/1024488
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: John Wu <jzw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553312}

--- ios/web_view/internal/web_view_web_client.mm
+++ ios/web_view/internal/web_view_web_client.mm
@@ -8,11 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/mac/bundle_locations.h"
 #include "base/strings/sys_string_conversions.h"
+#include "components/strings/grit/components_strings.h"
 #include "ios/web/public/user_agent.h"
 #include "ios/web_view/internal/web_view_browser_state.h"
 #import "ios/web_view/internal/web_view_early_page_script_provider.h"
 #import "ios/web_view/internal/web_view_web_main_parts.h"
 #include "ios/web_view/public/cwv_web_view.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -81,6 +83,10 @@ NSString* WebViewWebClient::GetDocumentStartScriptForMainFrame(
   [scripts addObject:GetPageScript(@"web_view_bundle")];
 
   return [scripts componentsJoinedByString:@";"];
+}
+
+base::string16 WebViewWebClient::GetPluginNotSupportedText() const {
+  return l10n_util::GetStringUTF16(IDS_PLUGIN_NOT_SUPPORTED);
 }
 
 }  // namespace ios_web_view
