60970529752dd76b6272fddaf7ad690b6153f92b
Gabriel Charette <gab@chromium.org>
2018-04-24 12:20:34 +0000

    Migrate MessageLoop::current() to MessageLoopCurrent in /chrome/browser/prefs
    These changes were scripted as part of the effort to restrict exposure of the
    raw MessageLoop*.
    
    Note that many static methods exposed on MessageLoopCurrent are also deprecated
    so it is very well possible that this is migrating to an already deprecated
    method.
    
    The goal of this pass is to reduce usage of MessageLoop::current() (and
    ultimately make it impossible to obtain the MessageLoop* statically).
    
    As such I will not edit this CL unless the script did something logically wrong.
    I defer to owners to fix highlighted usage of already deprecated APIs.
    
    Possible script screw ups / things to look out for in this review:
     - Storing MessageLoopCurrent in a MessageLoop* variable or comparing it against
       one (I will go over that in a separate pass).
    
    Includes should have been stripped if that was the last usage of message_loop.h
    in that file.
    
    Please CQ if LGTY
    
    This CL was uploaded by git cl split.
    
    R=bauerb@chromium.org
    
    Bug: 825327
    Change-Id: I87bfe63cc53b805a916de25b360c6db5be350358
    Reviewed-on: https://chromium-review.googlesource.com/1024507
    Commit-Queue: Bernhard Bauer <bauerb@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553055}

--- chrome/browser/prefs/synced_pref_change_registrar_browsertest.cc
+++ chrome/browser/prefs/synced_pref_change_registrar_browsertest.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/json/json_string_value_serializer.h"
-#include "base/message_loop/message_loop.h"
+#include "base/message_loop/message_loop_current.h"
 #include "base/run_loop.h"
 #include "base/time/time.h"
 #include "base/values.h"
@@ -43,7 +43,7 @@ class SyncedPrefChangeRegistrarTest : public InProcessBrowserTest {
 
   void UpdateChromePolicy(const policy::PolicyMap& policies) {
     policy_provider_.UpdateChromePolicy(policies);
-    DCHECK(base::MessageLoop::current());
+    DCHECK(base::MessageLoopCurrent::Get());
     base::RunLoop loop;
     loop.RunUntilIdle();
   }
