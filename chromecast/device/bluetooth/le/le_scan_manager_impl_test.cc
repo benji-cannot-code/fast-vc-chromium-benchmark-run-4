091e356ef282e8bc9eab203f4e063b8af539d6a9
Bailey Forrest <bcf@chromium.org>
2018-04-24 20:37:37 +0000

    [chromecast] Replace StatusCallbackChecker with MockCallback
    BUG=internal b/78359662
    TEST= cast_bluetooth_unittests
    
    Change-Id: Idd10f3edcc161b964cfda90826058564d55c6a6a
    Reviewed-on: https://chromium-review.googlesource.com/1022953
    Commit-Queue: Bailey Forrest <bcf@chromium.org>
    Reviewed-by: Stephen Lanham <slan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553258}

--- chromecast/device/bluetooth/le/le_scan_manager_impl_test.cc
+++ chromecast/device/bluetooth/le/le_scan_manager_impl_test.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromecast/device/bluetooth/le/remote_device.h"
 #include "chromecast/device/bluetooth/le/remote_service.h"
 #include "chromecast/device/bluetooth/shlib/mock_gatt_client.h"
-#include "chromecast/device/bluetooth/test_util.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
