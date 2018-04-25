b7fe8079cb1e86c8371a79bc82cc7989ec02dfe6
Fredrik Söderquist <fs@opera.com>
2018-04-25 14:12:22 +0000

    Use round() [not roundf()] in RoundedIntPoint(const DoublePoint&)
    This avoids the implicit conversion to float, which can lead to loss of
    precision.
    
    Bug: 836722
    Change-Id: I66ea0f42823347b17e41e5d823aaf766429eacbf
    Reviewed-on: https://chromium-review.googlesource.com/1027710
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#553543}

---
+++ third_party/blink/renderer/platform/geometry/double_point_test.cc
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/platform/geometry/double_point.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace blink {
+
+TEST(DoublePointTest, RoundedIntPoint) {
+  // Value not exactly representable as a float.
+  DoublePoint p1(16777217.0, -16777217.0);
+  IntPoint rounded_p1 = RoundedIntPoint(p1);
+  EXPECT_EQ(16777217, rounded_p1.X());
+  EXPECT_EQ(-16777217, rounded_p1.Y());
+}
+
+}  // namespace blink
