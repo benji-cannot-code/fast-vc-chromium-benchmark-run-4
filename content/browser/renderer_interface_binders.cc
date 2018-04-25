50d8c960bf6cd26ca07f6675374e83147cb892c4
Hiroki Nakagawa <nhiroki@chromium.org>
2018-04-25 08:30:02 +0000

    WebSocket: Clean up WebSocket creation
    This is just a cleanup and doesn't change behavior.
    
    Bug: 825740
    Change-Id: I36c4002ca29bd3ad5af1a8f63ef2b33e6b977981
    Reviewed-on: https://chromium-review.googlesource.com/1027370
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553485}

--- content/browser/renderer_interface_binders.cc
+++ content/browser/renderer_interface_binders.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/bind.h"
-#include "base/feature_list.h"
 #include "content/browser/background_fetch/background_fetch_service_impl.h"
 #include "content/browser/dedicated_worker/dedicated_worker_host.h"
 #include "content/browser/locks/lock_manager.h"
@@ -27,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_switches.h"
 #include "services/device/public/mojom/constants.mojom.h"
 #include "services/device/public/mojom/vibration_manager.mojom.h"
-#include "services/network/public/cpp/features.h"
 #include "services/network/restricted_cookie_manager.h"
 #include "services/service_manager/public/cpp/binder_registry.h"
 #include "services/service_manager/public/cpp/connector.h"
@@ -188,16 +186,8 @@ void RendererInterfaceBinders::CreateWebSocket(
     network::mojom::WebSocketRequest request,
     RenderProcessHost* host,
     const url::Origin& origin) {
-  if (base::FeatureList::IsEnabled(network::features::kNetworkService)) {
-    StoragePartition* storage_partition = host->GetStoragePartition();
-    network::mojom::NetworkContext* network_context =
-        storage_partition->GetNetworkContext();
-    network_context->CreateWebSocket(std::move(request), host->GetID(),
-                                     MSG_ROUTING_NONE, origin);
-  } else {
-    WebSocketManager::CreateWebSocketWithOrigin(
-        host->GetID(), origin, std::move(request), MSG_ROUTING_NONE);
-  }
+  WebSocketManager::CreateWebSocket(host->GetID(), MSG_ROUTING_NONE, origin,
+                                    std::move(request));
 }
 
 }  // namespace
