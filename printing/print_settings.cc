5055099e316a0ddefc0adaeb8614a9f559039518
xlou <xlou@chromium.org>
2018-04-25 02:35:23 +0000

    Add N-up feature support to pdfium_engine.
    In printing, 2-up, 3-up, or more generally N-up refers to a page
    layout strategy in which multiple pages are composited onto a
    single page.
    
    1. Added ability to pass the N-up feature UI settings (which will be
       added in the next CL) to the print preview workflow, so that the
       PDF plugin can take action based on user's input.
    2. Added N-up feature support to pdfium_engine, so that when the
       source is PDF, pdf plugin will generate the N-up output doc based
       on user's input.
    
    Bug: 775999
    Change-Id: I27933d1a7da65376e70bdcb538393bb8bb3ec688
    Reviewed-on: https://chromium-review.googlesource.com/1014628
    Commit-Queue: Shirleen Lou <xlou@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553428}

--- printing/print_settings.cc
+++ printing/print_settings.cc
@@ -182,6 +182,7 @@ void PrintSettings::Clear() {
   printer_type_ = PrintSettings::PrinterType::TYPE_NONE;
 #endif
   is_modifiable_ = true;
+  num_pages_per_sheet_ = 1;
 }
 
 void PrintSettings::SetPrinterPrintableArea(
