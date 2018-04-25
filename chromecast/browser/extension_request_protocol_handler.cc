a2667bde8b1d23969551d8960a0322eb114defc8
Albert Chaulk <achaulk@google.com>
2018-04-25 18:53:25 +0000

    [Chromecast] Update extension request handler to load files too
    If we recieve a request for an extension that doesn't have a 'cast_url'
    key present defer to the normal extension request processor to load
    the files from disk
    
    This is also required to load background pages (as it is from disk)
    
    Bug: b/73768170
    Test: Load extension that loads from disk, verify fallback occurs
    Change-Id: I7bf477f92dbebfd37077ed417e12d17a03d35308
    Reviewed-on: https://chromium-review.googlesource.com/934924
    Reviewed-by: Alex Sakhartchouk <alexst@chromium.org>
    Commit-Queue: Albert Chaulk <achaulk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553682}

--- chromecast/browser/extension_request_protocol_handler.cc
+++ chromecast/browser/extension_request_protocol_handler.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromecast/browser/extension_request_protocol_handler.h"
 
+#include "extensions/browser/extension_protocols.h"
 #include "extensions/browser/extension_system.h"
 #include "extensions/browser/info_map.h"
 #include "extensions/common/extension.h"
@@ -266,6 +267,8 @@ net::URLRequestJob* ExtensionRequestProtocolHandler::MaybeCreateJob(
     // This can't be done in the constructor as extensions::ExtensionSystem::Get
     // will fail until after this is constructed.
     info_map_ = extensions::ExtensionSystem::Get(browser_context_)->info_map();
+    default_handler_ = extensions::CreateExtensionProtocolHandler(
+        false, const_cast<extensions::InfoMap*>(info_map_));
   }
 
   const extensions::Extension* extension =
@@ -278,8 +281,8 @@ net::URLRequestJob* ExtensionRequestProtocolHandler::MaybeCreateJob(
 
   std::string cast_url;
   if (!extension->manifest()->GetString("cast_url", &cast_url)) {
-    LOG(ERROR) << "No 'cast_url' value for extension";
-    return nullptr;
+    // Defer to the default handler to load from disk.
+    return default_handler_->MaybeCreateJob(request, network_delegate);
   }
 
   // Replace chrome-extension://<id> with whatever the extension wants to go to.
