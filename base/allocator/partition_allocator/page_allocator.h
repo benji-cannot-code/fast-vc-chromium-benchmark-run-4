48817d21a86bf8c8ba4e908dd0752aa2a9bc59ec
Dan Elphick <delphick@chromium.org>
2018-04-25 11:17:52 +0000

    Allow marking pages read-only
    Add PageRead to PageAccessibilityConfiguration so pages can be marked
    read-only.
    
    Matching CL in V8:
    https://chromium-review.googlesource.com/c/v8/v8/+/1006756
    
    Bug: v8:7464
    Change-Id: I9acdd9d5c00db9b89b1a2103be83fa2dde257b37
    Reviewed-on: https://chromium-review.googlesource.com/1013564
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Commit-Queue: Dan Elphick <delphick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553513}

--- base/allocator/partition_allocator/page_allocator.h
+++ base/allocator/partition_allocator/page_allocator.h
@@ -42,6 +42,7 @@ static const size_t kSystemPageBaseMask = ~kSystemPageOffsetMask;
 
 enum PageAccessibilityConfiguration {
   PageInaccessible,
+  PageRead,
   PageReadWrite,
   PageReadExecute,
   // This flag is deprecated and will go away soon.
