64d69023ecfd1029fb44894d299232ec3af1e212
Michael Spang <spang@chromium.org>
2018-04-25 17:55:49 +0000

    ui/display: Convert display callbacks to OnceCallback
    This cleans up ui/display to use base::OnceCallback instead of
    base::Callback where appropriate.
    
    Bug: 714018
    Test: CQ & run chrome on eve
    
    Change-Id: I4231d9266f2d7df41701fe70fa8ef4a9b42b34c4
    Reviewed-on: https://chromium-review.googlesource.com/1026553
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Commit-Queue: Michael Spang <spang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553643}

--- ui/display/manager/fake_display_delegate.cc
+++ ui/display/manager/fake_display_delegate.cc
@@ -105,27 +105,26 @@ void FakeDisplayDelegate::Initialize() {
   initialized_ = true;
 }
 
-void FakeDisplayDelegate::TakeDisplayControl(
-    const DisplayControlCallback& callback) {
-  callback.Run(false);
+void FakeDisplayDelegate::TakeDisplayControl(DisplayControlCallback callback) {
+  std::move(callback).Run(false);
 }
 
 void FakeDisplayDelegate::RelinquishDisplayControl(
-    const DisplayControlCallback& callback) {
-  callback.Run(false);
+    DisplayControlCallback callback) {
+  std::move(callback).Run(false);
 }
 
-void FakeDisplayDelegate::GetDisplays(const GetDisplaysCallback& callback) {
+void FakeDisplayDelegate::GetDisplays(GetDisplaysCallback callback) {
   std::vector<DisplaySnapshot*> displays;
   for (auto& display : displays_)
     displays.push_back(display.get());
-  callback.Run(displays);
+  std::move(callback).Run(displays);
 }
 
 void FakeDisplayDelegate::Configure(const DisplaySnapshot& output,
                                     const DisplayMode* mode,
                                     const gfx::Point& origin,
-                                    const ConfigureCallback& callback) {
+                                    ConfigureCallback callback) {
   bool configure_success = false;
 
   if (!mode) {
@@ -141,7 +140,8 @@ void FakeDisplayDelegate::Configure(const DisplaySnapshot& output,
     }
   }
 
-  configure_callbacks_.push(base::Bind(callback, configure_success));
+  configure_callbacks_.push(
+      base::BindOnce(std::move(callback), configure_success));
 
   // Start the timer if it's not already running. If there are multiple queued
   // configuration requests then ConfigureDone() will handle starting the
@@ -153,14 +153,14 @@ void FakeDisplayDelegate::Configure(const DisplaySnapshot& output,
 }
 
 void FakeDisplayDelegate::GetHDCPState(const DisplaySnapshot& output,
-                                       const GetHDCPStateCallback& callback) {
-  callback.Run(false, HDCP_STATE_UNDESIRED);
+                                       GetHDCPStateCallback callback) {
+  std::move(callback).Run(false, HDCP_STATE_UNDESIRED);
 }
 
 void FakeDisplayDelegate::SetHDCPState(const DisplaySnapshot& output,
                                        HDCPState state,
-                                       const SetHDCPStateCallback& callback) {
-  callback.Run(false);
+                                       SetHDCPStateCallback callback) {
+  std::move(callback).Run(false);
 }
 
 bool FakeDisplayDelegate::SetColorCorrection(
@@ -214,7 +214,7 @@ void FakeDisplayDelegate::OnConfigurationChanged() {
 
 void FakeDisplayDelegate::ConfigureDone() {
   DCHECK(!configure_callbacks_.empty());
-  configure_callbacks_.front().Run();
+  std::move(configure_callbacks_.front()).Run();
   configure_callbacks_.pop();
 
   // If there are more configuration requests waiting then restart the timer.
