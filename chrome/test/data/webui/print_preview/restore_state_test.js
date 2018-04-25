03736bebbaea8e76b1ae7d5674acdf5d320f4c2e
Reid Kleckner <rnk@chromium.org>
2018-04-25 16:23:52 +0000

    Revert "Print Preview Componentization: Migrate preview area error tests"
    This reverts commit e8acf6974f2a495906184dfa6ddaa079edcbf279.
    
    Reason for revert: Tests do not pass in official build configuration.
    
    Google internal official bot link:
    https://uberchromegw.corp.google.com/i/official.desktop.continuous/builders/win%20trunk/builds/83286
    
    Externally visible clang ToT bot configured the same way:
    https://ci.chromium.org/buildbot/chromium.clang/ToTWin64/1359
    
    Original change's description:
    > Print Preview Componentization: Migrate preview area error tests
    > 
    > Migrate some tests for errors that appear in the Preview Area:
    > - No PDF plugin
    > - Invalid Settings
    > - Bad cloud print certificate
    > 
    > Bug: 814860
    > Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    > Change-Id: Ia87708ef2bc326d794b3be0f6aa9adeb87e5a3c2
    > Reviewed-on: https://chromium-review.googlesource.com/1015850
    > Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    > Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#553416}
    
    TBR=dpapad@chromium.org,rbpotter@chromium.org
    
    Change-Id: I1431bd7e9c3bf612ecb979893b1ea1914134c7ad
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 814860
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Reviewed-on: https://chromium-review.googlesource.com/1028331
    Reviewed-by: Reid Kleckner <rnk@chromium.org>
    Commit-Queue: Reid Kleckner <rnk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553596}

--- chrome/test/data/webui/print_preview/restore_state_test.js
+++ chrome/test/data/webui/print_preview/restore_state_test.js
@@ -78,8 +78,7 @@ cr.define('restore_state_test', function() {
 
       page = document.createElement('print-preview-app');
       const previewArea = page.$$('print-preview-preview-area');
-      previewArea.plugin_ = new print_preview.PDFPluginStub(
-          previewArea.onPluginLoad_.bind(previewArea));
+      previewArea.plugin_ = new print_preview.PDFPluginStub(previewArea);
       document.body.appendChild(page);
       return nativeLayer.whenCalled('getInitialSettings').then(function() {
         return nativeLayer.whenCalled('getPrinterCapabilities');
@@ -228,8 +227,7 @@ cr.define('restore_state_test', function() {
 
       page = document.createElement('print-preview-app');
       const previewArea = page.$$('print-preview-preview-area');
-      previewArea.plugin_ = new print_preview.PDFPluginStub(
-          previewArea.onPluginLoad_.bind(previewArea));
+      previewArea.plugin_ = new print_preview.PDFPluginStub(previewArea);
       document.body.appendChild(page);
 
       return nativeLayer.whenCalled('getInitialSettings').then(function() {
