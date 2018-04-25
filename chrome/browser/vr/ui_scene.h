56efb65fffc1de8f367263d8b802bd731fe95473
Christopher Grant <cjgrant@chromium.org>
2018-04-25 18:18:34 +0000

    VR: Clean up selection of hit-testable elements.
    This furthers previous lifecycle framework cleanup, and fixes an
    outstanding technical-debt bug in the process.
    
    BUG=768079
    R=ymalik
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_vr
    Change-Id: I6743ed68e16f437d68ccc27849f3694af347617a
    Reviewed-on: https://chromium-review.googlesource.com/1015645
    Commit-Queue: Christopher Grant <cjgrant@chromium.org>
    Reviewed-by: Yash Malik <ymalik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553664}

--- chrome/browser/vr/ui_scene.h
+++ chrome/browser/vr/ui_scene.h
@@ -62,10 +62,9 @@ class UiScene {
   typedef std::vector<UiElement*> MutableElements;
 
   std::vector<UiElement*>& GetAllElements();
-  Elements GetVisibleElements();
-  MutableElements GetVisibleElementsMutable();
-  Elements GetVisibleElementsToDraw();
-  Elements GetVisibleWebVrOverlayElementsToDraw();
+  Elements GetElementsToHitTest();
+  Elements GetElementsToDraw();
+  Elements GetWebVrOverlayElementsToDraw();
 
   float background_distance() const { return background_distance_; }
   void set_background_distance(float d) { background_distance_ = d; }
@@ -81,6 +80,8 @@ class UiScene {
 
  private:
   void InitializeElement(UiElement* element);
+
+  MutableElements GetVisibleElementsMutable();
 
   std::unique_ptr<UiElement> root_element_;
 
