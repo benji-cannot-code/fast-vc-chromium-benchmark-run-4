3772b16826e9c54be16393d66a93010dc7e29d41
Yutaka Hirano <yhirano@chromium.org>
2018-04-25 13:05:03 +0000

    Keep DocumentThreadableLoader for keepalive requests
    ...in order to make the logic work after frame detach.
    This CL also removes Resource::SetDetachable which is no longer needed.
    
    Bug: 836641
    Change-Id: I6ebc978021cafa9a6337afab5de29532fc28c4c1
    Reviewed-on: https://chromium-review.googlesource.com/1027210
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553526}

--- third_party/blink/renderer/core/loader/document_threadable_loader.h
+++ third_party/blink/renderer/core/loader/document_threadable_loader.h
@@ -89,6 +89,7 @@ class CORE_EXPORT DocumentThreadableLoader final : public ThreadableLoader,
   void Trace(blink::Visitor*) override;
 
  private:
+  class DetachedClient;
   enum BlockingBehavior { kLoadSynchronously, kLoadAsynchronously };
 
   static std::unique_ptr<ResourceRequest> CreateAccessControlPreflightRequest(
--- third_party/blink/renderer/platform/loader/fetch/resource.cc
+++ third_party/blink/renderer/platform/loader/fetch/resource.cc
@@ -705,7 +705,7 @@ void Resource::DidRemoveClientOrObserver() {
 }
 
 void Resource::AllClientsAndObserversRemoved() {
-  if (loader_ && !detachable_)
+  if (loader_)
     loader_->ScheduleCancel();
 }
 
