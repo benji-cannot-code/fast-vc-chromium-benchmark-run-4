279be20cf016bdfe8e5533acbe136fa0ac9a7b32
Daniel Cheng <dcheng@chromium.org>
2018-04-25 21:49:14 +0000

    Enforce user gesture requirement on browser side for WebUI.
    WebContentsImpl now tracks the last time it received an input event that
    could be considered user interaction. When the browser process receives
    a WebUI message that requires a user gesture, it checks the WebContents
    hosting the WebUI to make sure that the user recently interacted with
    it.
    
    This also cleans up a few incidental bits of code:
    - RenderWidgetHost no longer prefilters events before notifying the
      delegate. This exposed some broken event filtering, tracked at
      https://crbug.com/827659.
    - Since the delegate method no longer prefilters input events,
      RenderWidgetHostDelegate::OnUserInteraction() is now named
      RenderWidgetHostDelegate::DidReceiveInputEvent().
    
    TBR=isherman@chromium.org
    
    Bug: 823864
    Change-Id: I9237052dbd325d8e578bfa904a8779533b63b2f9
    Reviewed-on: https://chromium-review.googlesource.com/1028484
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553769}

--- content/browser/webui/web_ui_impl.cc
+++ content/browser/webui/web_ui_impl.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/debug/dump_without_crashing.h"
 #include "base/json/json_writer.h"
+#include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
 #include "content/browser/child_process_security_policy_impl.h"
@@ -80,7 +81,7 @@ base::string16 WebUI::GetJavascriptCall(
   return result;
 }
 
-WebUIImpl::WebUIImpl(WebContents* contents)
+WebUIImpl::WebUIImpl(WebContentsImpl* contents)
     : bindings_(BINDINGS_POLICY_WEB_UI),
       web_contents_(contents),
       web_contents_observer_(new MainFrameNavigationObserver(this, contents)) {
@@ -121,6 +122,13 @@ void WebUIImpl::OnWebUISend(RenderFrameHost* sender,
     bad_message::ReceivedBadMessage(
         sender->GetProcess(),
         bad_message::WEBUI_SEND_FROM_UNAUTHORIZED_PROCESS);
+    return;
+  }
+
+  if (base::EndsWith(message, "RequiringGesture",
+                     base::CompareCase::SENSITIVE) &&
+      !web_contents_->HasRecentInteractiveInputEvent()) {
+    LOG(ERROR) << message << " received without recent user interaction";
     return;
   }
 
--- content/renderer/web_ui_extension.cc
+++ content/renderer/web_ui_extension.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <utility>
 
-#include "base/strings/string_util.h"
 #include "base/values.h"
 #include "content/common/frame_messages.h"
 #include "content/public/common/bindings_policy.h"
@@ -24,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/web/blink.h"
 #include "third_party/blink/public/web/web_document.h"
 #include "third_party/blink/public/web/web_local_frame.h"
-#include "third_party/blink/public/web/web_user_gesture_indicator.h"
 #include "third_party/blink/public/web/web_view.h"
 #include "url/gurl.h"
 #include "v8/include/v8.h"
@@ -96,13 +94,6 @@ void WebUIExtension::Send(gin::Arguments* args) {
   std::string message;
   if (!args->GetNext(&message)) {
     args->ThrowError();
-    return;
-  }
-
-  if (base::EndsWith(message, "RequiringGesture",
-                     base::CompareCase::SENSITIVE) &&
-      !blink::WebUserGestureIndicator::IsProcessingUserGesture(frame)) {
-    NOTREACHED();
     return;
   }
 
