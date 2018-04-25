f6a134f89d2f010eadb173024232ec6a5e5f3670
Kent Tamura <tkent@chromium.org>
2018-04-25 01:58:43 +0000

    Add //thid_party/blink/tools/run_blinkpy_tests.py
    It will supercedes //third_party/WebKit/Tools/Scripts/test-webkitpy.
    test-webkitpy isn't removed yet due to dependency from
    build/scripts/{master,slave}/.
    
    Bug: 829697
    Change-Id: I5f827afc1c99693613282a328cc117ed86209251
    Reviewed-on: https://chromium-review.googlesource.com/1025434
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Quinten Yearsley <qyearsley@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553419}

--- third_party/blink/tools/blinkpy/common/message_pool.py
+++ third_party/blink/tools/blinkpy/common/message_pool.py
@@ -28,8 +28,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 """Module for handling messages and concurrency for run-webkit-tests
-and test-webkitpy. This module follows the design for multiprocessing.Pool
-and concurrency.futures.ProcessPoolExecutor, with the following differences:
+and run_blinkpy_tests.py. This module follows the design for
+multiprocessing.Pool and concurrency.futures.ProcessPoolExecutor, with the
+following differences:
 
 * Tasks are executed in stateful subprocesses via objects that implement the
   Worker interface - this allows the workers to share state across tasks.
