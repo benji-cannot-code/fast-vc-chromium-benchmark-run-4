9bf96ce76c2fdd156abf17a07bb8fbc6da99e97c
Ella Ge <eirage@chromium.org>
2018-04-25 21:04:33 +0000

    Add resample event to main thread event queue
    This CL adds input event predictor in main thread event queue
    under a flag kResampleInputEvents.
    It is the basic structure for resample events, without only an
    empty predictor. The input predictor handled events before the
    event is dispatched. And apply current time to the event.
    
    design doc:
    https://docs.google.com/document/d/1DLfibi2NkV85p7AfEtNkvy24d283VRBSd3hz1Qh24Jw/edit?usp=sharing
    
    Bug: 836352
    Change-Id: I7c17a33b2e0e5e9a51920e54a9b8ce795537f4ef
    Reviewed-on: https://chromium-review.googlesource.com/860429
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Navid Zolghadr <nzolghadr@chromium.org>
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553744}

---
+++ ui/events/blink/prediction/empty_predictor.cc
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/events/blink/prediction/empty_predictor.h"
+
+namespace ui {
+
+EmptyPredictor::EmptyPredictor() {
+  Reset();
+};
+
+EmptyPredictor::~EmptyPredictor() = default;
+
+void EmptyPredictor::Reset() {
+  last_input_.time_stamp = base::TimeTicks();
+}
+
+void EmptyPredictor::Update(const InputData& cur_input) {
+  last_input_ = cur_input;
+}
+
+bool EmptyPredictor::HasPrediction() const {
+  return false;
+}
+
+bool EmptyPredictor::GeneratePrediction(base::TimeTicks frame_time,
+                                        InputData* result) const {
+  if (!last_input_.time_stamp.is_null()) {
+    result->pos_x = last_input_.pos_x;
+    result->pos_y = last_input_.pos_y;
+    return true;
+  }
+  return false;
+}
+
+}  // namespace ui
