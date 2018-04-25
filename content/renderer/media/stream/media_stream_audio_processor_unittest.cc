aee8ab2be02bcd467bae689afd45c8434721940a
Gabriel Charette <gab@chromium.org>
2018-04-25 18:02:29 +0000

    Migrate MessageLoop::current() to MessageLoopCurrent in /content/renderer/media/stream/media_stream_audio_processor_unittest.cc
    These changes were scripted as part of the effort to restrict exposure of the
    raw MessageLoop*.
    
    Note that many static methods exposed on MessageLoopCurrent are also deprecated
    so it is very well possible that this is migrating to an already deprecated
    method.
    
    The goal of this pass is to reduce usage of MessageLoop::current() (and
    ultimately make it impossible to obtain the MessageLoop* statically).
    
    As such I will not edit this CL unless the script did something logically wrong.
    I defer to owners to fix highlighted usage of already deprecated APIs.
    
    Possible script screw ups / things to look out for in this review:
     - Storing MessageLoopCurrent in a MessageLoop* variable or comparing it against
       one (I will go over that in a separate pass).
    
    Includes should have been stripped if that was the last usage of message_loop.h
    in that file.
    
    Please CQ if LGTY
    
    This CL was uploaded by git cl split.
    
    TBR=aluebs@chromium.org
    
    Bug: 825327
    Change-Id: I9f324105a770fa05c8a0c621eaf21a58664d5447
    Reviewed-on: https://chromium-review.googlesource.com/1024796
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553651}

--- content/renderer/media/stream/media_stream_audio_processor_unittest.cc
+++ content/renderer/media/stream/media_stream_audio_processor_unittest.cc
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/macros.h"
 #include "base/memory/aligned_memory.h"
-#include "base/message_loop/message_loop.h"
+#include "base/message_loop/message_loop_current.h"
 #include "base/path_service.h"
 #include "base/test/scoped_task_environment.h"
 #include "base/threading/thread_task_runner_handle.h"
@@ -86,8 +86,8 @@ class AecDumpMessageFilterForTest : public AecDumpMessageFilter {
   // This class is only used for setting |override_aec3_|, so we simply inject
   // the current task runner.
   AecDumpMessageFilterForTest()
-      : AecDumpMessageFilter(base::MessageLoop::current()->task_runner(),
-                             base::MessageLoop::current()->task_runner()) {}
+      : AecDumpMessageFilter(base::MessageLoopCurrent::Get()->task_runner(),
+                             base::MessageLoopCurrent::Get()->task_runner()) {}
 
   void set_override_aec3(base::Optional<bool> override_aec3) {
     override_aec3_ = override_aec3;
