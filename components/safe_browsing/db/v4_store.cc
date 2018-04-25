a3385324d9ee2a1e7e8e7ad59dc3f704c49ddc43
Varun Khaneja <vakh@chromium.org>
2018-04-25 20:54:46 +0000

    Stop logging some histograms that are not being used actively.
    Bug: 833716
    Change-Id: I7295b8db2b0904ee30f78f3bc452236fefd4f3f0
    Reviewed-on: https://chromium-review.googlesource.com/1026558
    Commit-Queue: Varun Khaneja <vakh@chromium.org>
    Reviewed-by: Jialiu Lin <jialiul@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553739}

--- components/safe_browsing/db/v4_store.cc
+++ components/safe_browsing/db/v4_store.cc
@@ -38,7 +38,6 @@ const char kReadFromDisk[] = "SafeBrowsing.V4ReadFromDisk";
 const char kApplyUpdate[] = ".ApplyUpdate";
 const char kDecodeAdditions[] = ".DecodeAdditions";
 const char kDecodeRemovals[] = ".DecodeRemovals";
-const char kMergeUpdate[] = ".MergeUpdate";
 // Part 3: Represent the unit of value being measured and logged.
 const char kResult[] = ".Result";
 const char kTime[] = ".Time";
@@ -105,10 +104,6 @@ void RecordEnumWithAndWithoutSuffix(const std::string& metric,
   }
 }
 
-void RecordAddUnlumpedHashesTime(base::TimeDelta time) {
-  UMA_HISTOGRAM_LONG_TIMES("SafeBrowsing.V4AddUnlumpedHashes.Time", time);
-}
-
 void RecordApplyUpdateResult(const std::string& base_metric,
                              ApplyUpdateResult result,
                              const base::FilePath& file_path) {
@@ -148,12 +143,6 @@ void RecordDecodeRemovalsTime(const std::string& base_metric,
                               const base::FilePath& file_path) {
   RecordTimeWithAndWithoutSuffix(base_metric + kDecodeRemovals, time,
                                  file_path);
-}
-
-void RecordMergeUpdateTime(const std::string& base_metric,
-                           base::TimeDelta time,
-                           const base::FilePath& file_path) {
-  RecordTimeWithAndWithoutSuffix(base_metric + kMergeUpdate, time, file_path);
 }
 
 void RecordStoreReadResult(StoreReadResult result) {
@@ -330,7 +319,6 @@ ApplyUpdateResult V4Store::ProcessUpdate(
     expected_checksum = response->checksum().sha256();
   }
 
-  TimeTicks before = TimeTicks::Now();
   if (delay_checksum_check) {
     DCHECK(hash_prefix_map_old.empty());
     DCHECK(!raw_removals);
@@ -351,7 +339,6 @@ ApplyUpdateResult V4Store::ProcessUpdate(
       return apply_update_result;
     }
   }
-  RecordMergeUpdateTime(metric, TimeTicks::Now() - before, store_path_);
 
   state_ = response->new_client_state();
   return APPLY_UPDATE_SUCCESS;
@@ -484,11 +471,9 @@ ApplyUpdateResult V4Store::AddUnlumpedHashes(PrefixSize prefix_size,
     return ADDITIONS_SIZE_UNEXPECTED_FAILURE;
   }
 
-  TimeTicks before = TimeTicks::Now();
   // TODO(vakh): Figure out a way to avoid the following copy operation.
   (*additions_map)[prefix_size] =
       std::string(raw_hashes_begin, raw_hashes_begin + raw_hashes_length);
-  RecordAddUnlumpedHashesTime(TimeTicks::Now() - before);
   return APPLY_UPDATE_SUCCESS;
 }
 
