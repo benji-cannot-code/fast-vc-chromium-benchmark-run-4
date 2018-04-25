5f01e673cb15d853fe07eba34232121aabd9f5bb
Christian Fremerey <chfremer@chromium.org>
2018-04-25 18:19:08 +0000

    [Video Capture Android] Support facing mode and list user-facing first
    On Android, the facing mode of capture devices is currently reported only in
    form of a string appended to the device name. This was probably a legacy
    solution before facing mode has become an official field that can be queried.
    
    This CL fills the facing mode field so that it gets reported correctly.
    It then sorts device infos reported during device enumeration such that
    user-facing devices get listed first. This has the effect of making them
    preferred over back-facing cameras in case no facing-related constraints are
    given.
    
    Bug: 814298
    Change-Id: I366475903693401c64ed7ff3f65d39536d6c13ba
    Reviewed-on: https://chromium-review.googlesource.com/1025213
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Weiyong Yao <braveyao@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Christian Fremerey <chfremer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553665}

--- media/base/video_facing.h
+++ media/base/video_facing.h
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace media {
 
 // Facing mode for video capture.
+// A Java counterpart will be generated for this enum.
+// GENERATED_JAVA_ENUM_PACKAGE: org.chromium.media
 enum VideoFacingMode {
   MEDIA_VIDEO_FACING_NONE = 0,
   MEDIA_VIDEO_FACING_USER,
