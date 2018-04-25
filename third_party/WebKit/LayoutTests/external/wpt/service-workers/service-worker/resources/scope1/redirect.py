3168f5ccbd127b552c7808379edd286dcbcda6ba
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-04-25 12:39:23 +0000

    Import wpt@170bf8330e41fd6ba0af816dd6fca4816cbf8d8d
    Using wpt-import in Chromium 8a67bbaef837cbefb92a0954b2f671f1541f0a06.
    With Chromium commits locally applied on WPT:
    303956b6ed "Enable WPT tests for the Generic Sensor classes"
    afb472c0d2 "Try to fix crash when terminating a worker while it is XHR-ing to a blob."
    
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/16248
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    meade@chromium.org:
      external/wpt/web-animations
    
    TBR=markdittmer
    
    No-Export: true
    Change-Id: I85211ab928f7aa846dac4b0fa294302186fd4acf
    Reviewed-on: https://chromium-review.googlesource.com/1027790
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553522}

--- third_party/WebKit/LayoutTests/external/wpt/service-workers/service-worker/resources/scope1/redirect.py
+++ third_party/WebKit/LayoutTests/external/wpt/service-workers/service-worker/resources/scope1/redirect.py
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import os
-import sys
+import imp
 # Use the file from the parent directory.
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-from redirect import main
+mod = imp.load_source("_parent", os.path.join(os.path.dirname(os.path.dirname(__file__)),
+                                              os.path.basename(__file__)))
+main = mod.main
