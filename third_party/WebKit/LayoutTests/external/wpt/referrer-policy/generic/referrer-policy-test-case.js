cea05c142665365c38aa52ddcf71048b018fba76
Philip Jägenstedt <foolip@chromium.org>
2018-04-25 13:58:15 +0000

    Deprecate document.origin
    Intent to Deprecate and Remove:
    https://groups.google.com/a/chromium.org/d/msg/blink-dev/0D_37iuh1zc/ndyyNaxgCQAJ
    
    Bug: 692084
    Change-Id: Iae09a7d82e09c9bc3f0394dfa476370fd1efe516
    Reviewed-on: https://chromium-review.googlesource.com/1013699
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Philip Jägenstedt <foolip@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553537}

--- third_party/WebKit/LayoutTests/external/wpt/referrer-policy/generic/referrer-policy-test-case.js
+++ third_party/WebKit/LayoutTests/external/wpt/referrer-policy/generic/referrer-policy-test-case.js
@@ -30,7 +30,7 @@ function ReferrerPolicyTestCase(scenario, testDescription, sanityChecker) {
       return undefined;
     },
     "origin": function() {
-      return document.origin + "/";
+      return self.origin + "/";
     },
     "stripped-referrer": function() {
       return stripUrlForUseAsReferrer(location.toString());
