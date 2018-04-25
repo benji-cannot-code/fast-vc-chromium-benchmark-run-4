7a7aee69cdeeccd2027e51f592f2f2d6636d8511
Nicolas Pena <npm@chromium.org>
2018-04-25 18:21:08 +0000

    Move webtiming-resolution, webtiming-ssl to wpt
    This CL changes the webtiming-resolution to test that resolution is
    larger than 10 microseconds, instead of testing for a specific value.
    This allows the test to no longer be flaky and to be moved to wpt.
    
    The webtiming-ssl test is moved to WPT, using an https connection to
    ensure that secureConnectionStart is correctly defined.
    
    Bug: chromium:801341
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I4d31faa08d00251098f3dc648c794d80bbcc2f22
    Reviewed-on: https://chromium-review.googlesource.com/867310
    Commit-Queue: Nicolás Peña Moreno <npm@chromium.org>
    Reviewed-by: Robert Ma <robertma@chromium.org>
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553666}

--- third_party/WebKit/LayoutTests/http/tests/misc/webtiming-ssl.php
+++
@@ -1,5 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-<?php
-  header('Location: https://127.0.0.1:8443/misc/resources/webtiming-ssl.html');
-  header('HTTP/1.0 302 Found');
-?>
