a7db08076e51c62a357e335b5c02ba30ff8a4dbd
Gabriel Charette <gab@chromium.org>
2018-04-25 17:55:40 +0000

    Migrate MessageLoop::current() to MessageLoopCurrent in /chromecast/tracing
    These changes were scripted as part of the effort to restrict exposure of the
    raw MessageLoop*.
    
    Note that many static methods exposed on MessageLoopCurrent are also deprecated
    so it is very well possible that this is migrating to an already deprecated
    method.
    
    The goal of this pass is to reduce usage of MessageLoop::current() (and
    ultimately make it impossible to obtain the MessageLoop* statically).
    
    As such I will not edit this CL unless the script did something logically wrong.
    I defer to owners to fix highlighted usage of already deprecated APIs.
    
    Possible script screw ups / things to look out for in this review:
     - Storing MessageLoopCurrent in a MessageLoop* variable or comparing it against
       one (I will go over that in a separate pass).
    
    Includes should have been stripped if that was the last usage of message_loop.h
    in that file.
    
    Please CQ if LGTY
    
    This CL was uploaded by git cl split.
    
    TBR=spang@chromium.org
    
    Bug: 825327
    Change-Id: I8c5b885a9ceb8069ff004254848b47d5b6fd55d0
    Reviewed-on: https://chromium-review.googlesource.com/1024393
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553642}

--- chromecast/tracing/tracing_service_main.cc
+++ chromecast/tracing/tracing_service_main.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/memory/weak_ptr.h"
 #include "base/message_loop/message_loop.h"
+#include "base/message_loop/message_loop_current.h"
 #include "base/message_loop/message_pump_for_io.h"
 #include "base/posix/eintr_wrapper.h"
 #include "base/posix/unix_domain_socket.h"
@@ -98,7 +99,7 @@ class TraceCopyTask : public base::MessagePumpLibevent::FdWatcher {
   ~TraceCopyTask() override {}
 
   void Start() {
-    base::MessageLoopForIO::current()->WatchFileDescriptor(
+    base::MessageLoopCurrentForIO::Get()->WatchFileDescriptor(
         out_fd_.get(), true /* persistent */,
         base::MessagePumpForIO::WATCH_WRITE, &out_watcher_, this);
   }
@@ -183,7 +184,7 @@ class TraceConnection : public base::MessagePumpLibevent::FdWatcher {
   ~TraceConnection() override {}
 
   void Init() {
-    base::MessageLoopForIO::current()->WatchFileDescriptor(
+    base::MessageLoopCurrentForIO::Get()->WatchFileDescriptor(
         connection_fd_.get(), true /* persistent */,
         base::MessagePumpForIO::WATCH_READ, &connection_watcher_, this);
   }
@@ -345,7 +346,7 @@ class TracingService : public base::MessagePumpLibevent::FdWatcher {
     if (!server_socket_.is_valid())
       return false;
 
-    base::MessageLoopForIO::current()->WatchFileDescriptor(
+    base::MessageLoopCurrentForIO::Get()->WatchFileDescriptor(
         server_socket_.get(), true /* persistent */,
         base::MessagePumpForIO::WATCH_READ, &server_socket_watcher_, this);
 
