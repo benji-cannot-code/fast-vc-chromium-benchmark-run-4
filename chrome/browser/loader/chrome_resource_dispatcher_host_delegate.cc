7c86b0bc33546378a51ed69be6effd583dde11be
Charlie Harrison <csharrison@chromium.org>
2018-04-25 21:43:19 +0000

    Delete mod_pagespeed code and metrics
    These metrics are not tracked, and the code is not really being
    maintained.
    
    Bug: None
    Change-Id: I030919ac8a2cf4e744ed120cdbcab282dcc6aea1
    Reviewed-on: https://chromium-review.googlesource.com/1027800
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553761}

--- chrome/browser/loader/chrome_resource_dispatcher_host_delegate.cc
+++ chrome/browser/loader/chrome_resource_dispatcher_host_delegate.cc
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/loader/chrome_navigation_data.h"
 #include "chrome/browser/loader/predictor_resource_throttle.h"
 #include "chrome/browser/loader/safe_browsing_resource_throttle.h"
-#include "chrome/browser/mod_pagespeed/mod_pagespeed_metrics.h"
 #include "chrome/browser/net/loading_predictor_observer.h"
 #include "chrome/browser/page_load_metrics/metrics_web_contents_observer.h"
 #include "chrome/browser/plugins/plugin_prefs.h"
@@ -696,9 +695,6 @@ void ChromeResourceDispatcherHostDelegate::OnResponseStarted(
       LogCommittedPreviewsDecision(io_data, request->url(), previews_user_data);
     }
   }
-
-  mod_pagespeed::RecordMetrics(info->GetResourceType(), request->url(),
-                               request->response_headers());
 }
 
 void ChromeResourceDispatcherHostDelegate::OnRequestRedirected(
