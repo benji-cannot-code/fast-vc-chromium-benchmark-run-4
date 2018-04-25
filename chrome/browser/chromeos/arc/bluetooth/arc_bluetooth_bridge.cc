b4a89d70e14470ce96b7c3c3d16f6b921bc7fb27
Qiyu Hu <qiyuh@google.com>
2018-04-25 20:43:25 +0000

    arc: bluetooth: Fix incorrect bonded devices reported to android
    This was introduced in the following patch as a typo:
    https://codereview.chromium.org/1927803002/diff/320001/components/arc/bluetooth/arc_bluetooth_bridge.cc
    
    BUG=727843
    TEST=Run CtsVerifier and confirm that non-bonded devices don't show up as paired
    
    Change-Id: Id50a5564257a270b62bc78c63835e194af772cce
    Reviewed-on: https://chromium-review.googlesource.com/1028574
    Reviewed-by: Rahul Chaturvedi <rkc@chromium.org>
    Reviewed-by: Luis Hector Chavez <lhchavez@chromium.org>
    Commit-Queue: Qiyu Hu <qiyuh@google.com>
    Cr-Commit-Position: refs/heads/master@{#553728}

--- chrome/browser/chromeos/arc/bluetooth/arc_bluetooth_bridge.cc
+++ chrome/browser/chromeos/arc/bluetooth/arc_bluetooth_bridge.cc
@@ -2433,7 +2433,7 @@ ArcBluetoothBridge::GetAdapterProperties(
     std::vector<mojom::BluetoothAddressPtr> bonded_devices;
 
     for (auto* device : devices) {
-      if (device->IsPaired())
+      if (!device->IsPaired())
         continue;
 
       mojom::BluetoothAddressPtr addr =
