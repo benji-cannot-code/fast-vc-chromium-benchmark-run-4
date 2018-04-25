ccb069069dc21270b40ef31c151ebf0a5f34c732
Rakina Zata Amni <rakina@chromium.org>
2018-04-25 22:48:03 +0000

    Move functions from web_local_frame_impl_find_in_page.cc to find_in_page.cc
    We are moving find-in-page related functions out of
    web_local_frame_impl_find_in_page.cc to its own class
    (FindInPage) for easier mojoification and so that we can add
    new find-in-page related functions more easily.
    
    In this CL, we are moving functions out of
    web_local_frame_impl_find_in_page.cc. In the next CL we are going to
    entirely remove the functions from the WebLocalFrame interface.
    
    Previous CLs:
    crrev.com/c/956648
    crrev.com/c/1025242
    
    Next CLs:
    Remove find-in-page functions from WebLocalFrame
    Introduce FindInPage mojo interface
    
    Bug: 819919
    Change-Id: Ia82b5e8f1e08701b014236039d2c6e5e45410222
    Reviewed-on: https://chromium-review.googlesource.com/1027013
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Takayoshi Kochi <kochi@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553801}

--- third_party/blink/renderer/core/frame/find_in_page.cc
+++ third_party/blink/renderer/core/frame/find_in_page.cc
@@ -29,13 +29,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "third_party/blink/renderer/core/frame/web_local_frame_impl.h"
+#include "third_party/blink/renderer/core/frame/find_in_page.h"
 
 #include "third_party/blink/public/web/web_document.h"
 #include "third_party/blink/public/web/web_find_options.h"
 #include "third_party/blink/public/web/web_frame_client.h"
 #include "third_party/blink/renderer/core/editing/finder/text_finder.h"
-#include "third_party/blink/renderer/core/page/find_in_page.h"
+#include "third_party/blink/renderer/core/frame/web_local_frame_impl.h"
 
 namespace blink {
 
