e22345cc2adabf39120f5069a743bdaa3955a880
Jennifer Apacible <apacible@chromium.org>
2018-04-25 20:50:11 +0000

    [Picture in Picture] Add media controls on window.
    This change adds media controls (play/pause, close) on the
    Picture-in-Picture window for desktop (OverlayWindowViews). This adds
    three vectorized icons used for these controls. These are currently
    rendered as white when the user mouses over the window, and disappears
    when the mouse is not hovering over the window.
    
    The intended sizes of the icons are currently hardcoded. They will be
    updated when we've determined how we want them to be sized or scaled.
    
    Future work includes making the controls accessible and handling events
    triggered by keyboard or touch inputs.
    
    BUG: 836389
    Change-Id: I37987286b531c1ce1ddcf867f72510194657053d
    Reviewed-on: https://chromium-review.googlesource.com/1026519
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: apacible <apacible@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553732}

--- content/browser/picture_in_picture/picture_in_picture_window_controller_impl.h
+++ content/browser/picture_in_picture/picture_in_picture_window_controller_impl.h
@@ -40,7 +40,7 @@ class PictureInPictureWindowControllerImpl
                                    const gfx::Size& natural_size) override;
   CONTENT_EXPORT OverlayWindow* GetWindowForTesting() override;
   CONTENT_EXPORT void UpdateLayerBounds() override;
-  CONTENT_EXPORT void TogglePlayPause() override;
+  CONTENT_EXPORT bool TogglePlayPause() override;
 
  private:
   friend class WebContentsUserData<PictureInPictureWindowControllerImpl>;
