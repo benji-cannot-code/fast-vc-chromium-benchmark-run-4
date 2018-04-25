39aea841335d537b96521190a5cb38e22e794ae5
Matt Menke <mmenke@chromium.org>
2018-04-25 21:13:27 +0000

    SimpleURLLoader: Make downloaded file path accessible after destruction.
    When downloading to a file, if code deleted the SimpleURLLoader on
    completion, and then tried to access the FilePath object passed to them
    by the SimpleURLLoader, they're run into a use-after-free. This CL fixes
    that.
    
    Bug: 835963
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I8b840544be743aa979145bf46c8d35de4b25c12c
    Reviewed-on: https://chromium-review.googlesource.com/1024694
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553745}

--- chrome/browser/chromeos/customization/customization_wallpaper_downloader.cc
+++ chrome/browser/chromeos/customization/customization_wallpaper_downloader.cc
@@ -151,7 +151,7 @@ void CustomizationWallpaperDownloader::OnWallpaperDirectoryCreated(
 }
 
 void CustomizationWallpaperDownloader::OnSimpleLoaderComplete(
-    const base::FilePath& response_path) {
+    base::FilePath response_path) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
 
   const bool error = response_path.empty();
@@ -159,9 +159,6 @@ void CustomizationWallpaperDownloader::OnSimpleLoaderComplete(
   VLOG(1) << "CustomizationWallpaperDownloader::OnURLFetchComplete(): status="
           << simple_loader_->NetError();
 
-  // Save the response_path before resetting SimplerURLLoader. It gets nulled
-  // out afterwards.
-  base::FilePath copy_response_path(response_path);
   simple_loader_.reset();
 
   if (error) {
@@ -172,7 +169,7 @@ void CustomizationWallpaperDownloader::OnSimpleLoaderComplete(
   std::unique_ptr<bool> success(new bool(false));
 
   base::OnceClosure rename_closure = base::BindOnce(
-      &RenameTemporaryFile, copy_response_path, wallpaper_downloaded_file_,
+      &RenameTemporaryFile, response_path, wallpaper_downloaded_file_,
       base::Unretained(success.get()));
   base::OnceClosure on_rename_closure = base::BindOnce(
       &CustomizationWallpaperDownloader::OnTemporaryFileRenamed,
--- chrome/browser/net/file_downloader.h
+++ chrome/browser/net/file_downloader.h
@@ -51,7 +51,7 @@ class FileDownloader {
   static bool IsSuccess(Result result) { return result != FAILED; }
 
  private:
-  void OnSimpleDownloadComplete(const base::FilePath& response_path);
+  void OnSimpleDownloadComplete(base::FilePath response_path);
 
   void OnFileExistsCheckDone(bool exists);
 
