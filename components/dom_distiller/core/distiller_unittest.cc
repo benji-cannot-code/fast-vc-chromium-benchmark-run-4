16c120764a6a8312d89e171143459b36630f8d46
Gabriel Charette <gab@chromium.org>
2018-04-25 18:09:32 +0000

    Migrate MessageLoop::current() to MessageLoopCurrent in /components/dom_distiller
    These changes were scripted as part of the effort to restrict exposure of the
    raw MessageLoop*.
    
    Note that many static methods exposed on MessageLoopCurrent are also deprecated
    so it is very well possible that this is migrating to an already deprecated
    method.
    
    The goal of this pass is to reduce usage of MessageLoop::current() (and
    ultimately make it impossible to obtain the MessageLoop* statically).
    
    As such I will not edit this CL unless the script did something logically wrong.
    I defer to owners to fix highlighted usage of already deprecated APIs.
    
    Possible script screw ups / things to look out for in this review:
     - Storing MessageLoopCurrent in a MessageLoop* variable or comparing it against
       one (I will go over that in a separate pass).
    
    Includes should have been stripped if that was the last usage of message_loop.h
    in that file.
    
    Please CQ if LGTY
    
    This CL was uploaded by git cl split.
    
    TBR=wychen@chromium.org
    
    Bug: 825327
    Change-Id: I54dafd02e4631007e7ea15efff66e91f68063a3b
    Reviewed-on: https://chromium-review.googlesource.com/1024656
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553656}

--- components/dom_distiller/core/distiller_unittest.cc
+++ components/dom_distiller/core/distiller_unittest.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/macros.h"
 #include "base/message_loop/message_loop.h"
+#include "base/message_loop/message_loop_current.h"
 #include "base/run_loop.h"
 #include "base/single_thread_task_runner.h"
 #include "base/strings/string_number_conversions.h"
@@ -242,7 +243,7 @@ class TestDistillerURLFetcher : public DistillerURLFetcher {
   }
 
   void PostCallbackTask() {
-    ASSERT_TRUE(base::MessageLoop::current());
+    ASSERT_TRUE(base::MessageLoopCurrent::Get());
     ASSERT_FALSE(callback_.is_null());
     base::ThreadTaskRunnerHandle::Get()->PostTask(
         FROM_HERE, base::Bind(callback_, responses_[url_]));
