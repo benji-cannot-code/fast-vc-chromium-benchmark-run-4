177f61a96ada46ab5ebff61b7fa3f71c3c532084
Maks Orlovich <morlovich@chromium.org>
2018-04-26 03:28:16 +0000

    FileNetLogObserver: use uint64_t and not size_t for size limits
    This is so that exporting of this functionality over mojo can accurately
    express the underlying type and the kNoLimit constant. (Which doesn't
    seem doable with size_t since its size, and therefore value of its max,
    differs on different platforms).
    
    Bug: 767450
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I741bfe1c942e5ee7752f346932a26e861cefb0c8
    Reviewed-on: https://chromium-review.googlesource.com/1027905
    Reviewed-by: Eric Roman <eroman@chromium.org>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553891}

--- components/net_log/net_export_file_writer.h
+++ components/net_log/net_export_file_writer.h
@@ -57,7 +57,7 @@ class ChromeNetLog;
 class NetExportFileWriter {
  public:
   // Special value meaning "can use an unlimited number of bytes".
-  static constexpr size_t kNoLimit = net::FileNetLogObserver::kNoLimit;
+  static constexpr uint64_t kNoLimit = net::FileNetLogObserver::kNoLimit;
 
   // The observer interface to be implemented by code that wishes to be notified
   // of NetExportFileWriter's state changes.
@@ -110,7 +110,7 @@ class NetExportFileWriter {
   // thread.
   void StartNetLog(const base::FilePath& log_path,
                    net::NetLogCaptureMode capture_mode,
-                   size_t max_file_size,
+                   uint64_t max_file_size,
                    const base::CommandLine::StringType& command_line_string,
                    const std::string& channel_string,
                    const URLRequestContextGetterList& context_getters);
