35c72478e026605ea4057ecc61bbb0fafcebcfc7
Devlin Cronin <rdevlin.cronin@chromium.org>
2018-04-26 21:51:07 +0000

    [Extensions WebUI] Remove ExtensionLoaderHandler
    ExtensionLoaderHandler is no longer needed, modulo a utility method
    to help parse a manifest error. Move the utility method to the
    developerPrivate API implementation (which is now the only caller),
    and remove ExtensionLoaderHandler.
    
    Bug: 820187
    Change-Id: I631e5562adca718a157fc20df36a91e7582ddfce
    Reviewed-on: https://chromium-review.googlesource.com/1030621
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554159}

--- chrome/browser/ui/webui/extensions/extensions_ui.cc
+++ chrome/browser/ui/webui/extensions/extensions_ui.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/webui/extensions/extension_loader_handler.h"
 #include "chrome/browser/ui/webui/extensions/install_extension_handler.h"
 #include "chrome/browser/ui/webui/metrics_handler.h"
 #include "chrome/common/pref_names.h"
