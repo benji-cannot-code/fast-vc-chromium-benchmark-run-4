0a3f3ff260f452a6b3167beba2175d80bf4a8a6a
Fredrik Söderquist <fs@opera.com>
2018-04-26 14:33:26 +0000

    Factor out <clipPath> user space transform computation
    This computation (clipPath[transform] and optional oBB transform) is
    used by LayoutSVGResourceClipper::HitTestClipContent,
    LayoutSVGResourceClipper::ResourceBoundingBox and ClipPathClipper.
    
    Factor it into a new method CalculateClipTransform() on
    LayoutSVGResourceClipper and use it at the three locations.
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Icaa9707f24c11f1a45d80ff71d8e156c868898c1
    Reviewed-on: https://chromium-review.googlesource.com/1030212
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#554022}

--- third_party/blink/renderer/core/layout/svg/layout_svg_resource_clipper.h
+++ third_party/blink/renderer/core/layout/svg/layout_svg_resource_clipper.h
@@ -49,6 +49,7 @@ class LayoutSVGResourceClipper final : public LayoutSVGResourceContainer {
         ->CurrentValue()
         ->EnumValue();
   }
+  AffineTransform CalculateClipTransform(const FloatRect& reference_box) const;
 
   base::Optional<Path> AsPath();
   sk_sp<const PaintRecord> CreatePaintRecord();
