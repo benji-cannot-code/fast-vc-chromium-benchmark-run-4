f251e8439f0026db82d00df9defd85ce362259df
Peter Kasting <pkasting@chromium.org>
2018-04-26 00:31:29 +0000

    Compute NTB position dynamically from caption button position.
    This makes Refresh use the trailing NTB for high-contrast RTL on Windows, where
    we draw with the native frame and the caption buttons are on the right (i.e.
    leading) edge.
    
    BUG=822063
    TEST=none
    
    Change-Id: I92b58e1db4aef83d76e6e64abb93f9549db84067
    Reviewed-on: https://chromium-review.googlesource.com/1029197
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553848}

--- chrome/browser/ui/views/tabs/tab_strip.h
+++ chrome/browser/ui/views/tabs/tab_strip.h
@@ -63,6 +63,12 @@ class TabStrip : public views::View,
                  public views::ViewTargeterDelegate,
                  public TabController {
  public:
+  enum NewTabButtonPosition {
+    LEADING,     // Pinned to the leading edge of the tabstrip region.
+    AFTER_TABS,  // After the last tab.
+    TRAILING,    // Pinned to the trailing edge of the tabstrip region.
+  };
+
   explicit TabStrip(std::unique_ptr<TabStripController> controller);
   ~TabStrip() override;
 
@@ -72,6 +78,9 @@ class TabStrip : public views::View,
   // Add and remove observers to changes within this TabStrip.
   void AddObserver(TabStripObserver* observer);
   void RemoveObserver(TabStripObserver* observer);
+
+  // Returns the position of the new tab button within the strip.
+  NewTabButtonPosition GetNewTabButtonPosition() const;
 
   // Max x-coordinate the tabstrip draws at, which is the right edge of the new
   // tab button.
@@ -349,8 +358,12 @@ class TabStrip : public views::View,
   // currently set in ideal_bounds.
   void AnimateToIdealBounds();
 
-  // Returns whether the highlight button should be highlighted after a remove.
+  // Returns whether the close button should be highlighted after a remove.
   bool ShouldHighlightCloseButtonAfterRemove();
+
+  // Returns whether dragging tabs should ever result in the new tab button
+  // being hidden.
+  bool MayHideNewTabButtonWhileDragging() const;
 
   // Returns whether the window background behind the tabstrip is transparent.
   bool TitlebarBackgroundIsTransparent() const;
