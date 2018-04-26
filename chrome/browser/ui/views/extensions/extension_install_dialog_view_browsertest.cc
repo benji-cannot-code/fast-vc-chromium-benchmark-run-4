a193c1b6ece2b0bd95738e52c85714cc06eecd12
Devlin Cronin <rdevlin.cronin@chromium.org>
2018-04-26 16:35:06 +0000

    [Extensions WebUI] Clean up more of the old extensions page
    ExtensionSettingsHandler is no longer needed, modulo pref registration.
    Move pref registration to the ExtensionsUI class, and remove
    ExtensionSettingsHandler. Also get rid of a bunch of now-unused strings.
    
    Additionally, remove the old, obsolete pref
    kExtensionsUIDismissedADTPromo. This has been unused for years, and is
    (more than) ready to be fully removed from the codebase.
    
    Bug: 820187
    Change-Id: I3cc37d0f76ab8c1eaddc31239a653c2aab97b21d
    Reviewed-on: https://chromium-review.googlesource.com/1028765
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554048}

--- chrome/browser/ui/views/extensions/extension_install_dialog_view_browsertest.cc
+++ chrome/browser/ui/views/extensions/extension_install_dialog_view_browsertest.cc
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/test/test_browser_dialog.h"
-#include "chrome/browser/ui/webui/extensions/extension_settings_handler.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/extensions/extension_test_util.h"
 #include "chrome/grit/generated_resources.h"
