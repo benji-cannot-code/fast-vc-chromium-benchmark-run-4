ccc771aa8cf77c428017fb05a0241d8651a7070e
Mark Pilgrim <pilgrim@chromium.org>
2018-04-26 20:45:51 +0000

    Migrate BrandcodeConfigFetcher to SimpleURLLoader
    Bug: 773295
    Change-Id: I4d0db1c93527c8d07b4d71f74d5c31dbaaf7e0ce
    Reviewed-on: https://chromium-review.googlesource.com/1026290
    Commit-Queue: Mark Pilgrim <pilgrim@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Robert Shield <robertshield@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554142}

--- chrome/browser/safe_browsing/settings_reset_prompt/default_settings_fetcher.cc
+++ chrome/browser/safe_browsing/settings_reset_prompt/default_settings_fetcher.cc
@@ -11,9 +11,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind_helpers.h"
 #include "base/logging.h"
 #include "chrome/browser/google/google_brand.h"
+#include "chrome/browser/net/system_network_context_manager.h"
 #include "chrome/browser/profile_resetter/brandcode_config_fetcher.h"
 #include "chrome/browser/profile_resetter/brandcoded_default_settings.h"
 #include "content/public/browser/browser_thread.h"
+#include "services/network/public/mojom/url_loader_factory.mojom.h"
 #include "url/gurl.h"
 
 namespace safe_browsing {
@@ -60,6 +62,8 @@ void DefaultSettingsFetcher::Start() {
   std::string brandcode;
   if (google_brand::GetBrand(&brandcode) && !brandcode.empty()) {
     config_fetcher_.reset(new BrandcodeConfigFetcher(
+        g_browser_process->system_network_context_manager()
+            ->GetURLLoaderFactory(),
         base::Bind(&DefaultSettingsFetcher::OnSettingsFetched,
                    base::Unretained(this)),
         GURL(kOmahaUrl), brandcode));
