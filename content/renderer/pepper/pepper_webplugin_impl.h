422ae517f39b2179463a7794691b7d255e814eb3
Dmitry Gozman <dgozman@chromium.org>
2018-04-26 17:23:16 +0000

    Use mojom::ClipboardHost directly in PepperWebPluginImpl
    ... instead of going through blink's platform clipboard.
    
    Bug: 775830
    Change-Id: I3485409765f9d567120b991a05c66f49ac4aa441
    Reviewed-on: https://chromium-review.googlesource.com/1027997
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Bill Budge <bbudge@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554064}

--- content/renderer/pepper/pepper_webplugin_impl.h
+++ content/renderer/pepper/pepper_webplugin_impl.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/sequenced_task_runner_helpers.h"
 #include "ppapi/c/pp_var.h"
+#include "third_party/blink/public/mojom/clipboard/clipboard.mojom.h"
 #include "third_party/blink/public/web/web_plugin.h"
 #include "ui/gfx/geometry/rect.h"
 
@@ -105,6 +106,7 @@ class PepperWebPluginImpl : public blink::WebPlugin {
   gfx::Rect plugin_rect_;
   PP_Var instance_object_;
   blink::WebPluginContainer* container_;
+  blink::mojom::ClipboardHostPtr clipboard_;
 
   DISALLOW_COPY_AND_ASSIGN(PepperWebPluginImpl);
 };
