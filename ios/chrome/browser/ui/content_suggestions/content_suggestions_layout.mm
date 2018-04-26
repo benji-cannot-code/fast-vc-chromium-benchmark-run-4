b95afd4f7a8a1b0a248f59433eedc188e1463c0d
Justin Cohen <justincohen@google.com>
2018-04-26 15:56:13 +0000

    [ios] Followup to NTP contentInset.bottom changes.
    Bug: 837248
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: If01a46f95b979a5069895e6135adc4203216817b
    Reviewed-on: https://chromium-review.googlesource.com/1030060
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554042}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_layout.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_layout.mm
@@ -34,7 +34,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     }
   }
   if (!content_suggestions::IsRegularXRegularSizeClass(self.collectionView))
-    minimumHeight -= ntp_header::ToolbarHeight() + topSafeArea;
+    minimumHeight -= ntp_header::ToolbarHeight() + topSafeArea +
+                     self.collectionView.contentInset.bottom;
 
   CGSize contentSize = [super collectionViewContentSize];
   if (contentSize.height < minimumHeight) {
