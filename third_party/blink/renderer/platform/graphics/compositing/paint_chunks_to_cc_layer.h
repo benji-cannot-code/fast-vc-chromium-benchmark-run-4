9a5cccbd5634d33a73ee47b8d0ab0370198f845f
Xianzhu Wang <wangxianzhu@chromium.org>
2018-04-26 18:01:37 +0000

    Reland "[SPv175] Avoid full layer invalidation for composited animation with subpixels"
    This reverts commit da6511ca6f9d72fd403b395fb42cbaedba2468db.
    
    Add null pointer checks.
    
    Original change's description:
    > Revert "[SPv175] Avoid full layer invalidation for composited animation with subpixels"
    > 
    > This reverts commit 801d8f04920f3bf226c6aaeba93f1c9100fda3c7.
    > 
    > Reason for revert: null pointer crash (crbug.com/837226).
    > 
    > Original change's description:
    > > [SPv175] Avoid full layer invalidation for composited animation with subpixels
    > > 
    > > It's unfortunate that composited animation with subpixels is
    > > incompatible with pixel snapping. We have to accept some imperfection
    > > of pixel snapping to achieve high performance composited animation
    > > with subpixels, otherwise each step of the animation might cause
    > > full layer raster invalidation.
    > > 
    > > SPv175 lost the imperfect optimization and this CL brings it back by
    > > - excluding the subpixel accumulation of the composited layer from
    > >   the visual rects of composted layer contents, if there is no non-
    > >   translation transform between the composited layer and the object.
    > > - adding the subpixel accumulation back to the final visual rects
    > >   when generating raster invalidations and convert to cc display
    > >   item list.
    > > 
    > > Bug: 833083
    > > Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    > > Change-Id: Ie8cfb3712bf7839a60b2a3187fd792b0fa4fe4d2
    > > Reviewed-on: https://chromium-review.googlesource.com/1023606
    > > Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    > > Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#553479}
    > 
    > TBR=wangxianzhu@chromium.org,trchen@chromium.org,chrishtr@chromium.org
    > 
    > # Not skipping CQ checks because original CL landed > 1 day ago.
    > 
    > Bug: 833083, 837226
    > Change-Id: Ib4b7219eee07e3b0115b75a9be28cfe6c381f82c
    > Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    > Reviewed-on: https://chromium-review.googlesource.com/1030710
    > Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    > Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#554065}
    
    TBR=wangxianzhu@chromium.org,trchen@chromium.org,chrishtr@chromium.org
    
    Change-Id: I66042496f138a4ab4f17115051639f295a985af7
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 833083, 837226
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Reviewed-on: https://chromium-review.googlesource.com/1030783
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554079}

--- third_party/blink/renderer/platform/graphics/compositing/paint_chunks_to_cc_layer.h
+++ third_party/blink/renderer/platform/graphics/compositing/paint_chunks_to_cc_layer.h
@@ -25,6 +25,7 @@ namespace blink {
 
 class DisplayItemList;
 class PaintChunkSubset;
+class FloatSize;
 class PropertyTreeState;
 class RasterInvalidationTracking;
 
@@ -58,6 +59,7 @@ class PLATFORM_EXPORT PaintChunksToCcLayer {
   static void ConvertInto(const PaintChunkSubset&,
                           const PropertyTreeState& layer_state,
                           const gfx::Vector2dF& layer_offset,
+                          const FloatSize& visual_rect_subpixel_offset,
                           const DisplayItemList&,
                           cc::DisplayItemList&);
 
