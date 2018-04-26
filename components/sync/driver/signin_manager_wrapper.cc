3410c802270ec20d8e5407ad923bf390b9a7a22d
Marc Treib <treib@chromium.org>
2018-04-26 10:43:38 +0000

    Sync: Use IdentityManager instead of SigninManager
    This CL migrates ProfileSyncService to IdentityManager for getting
    info on the primary account. This includes switching from
    SigninManagerBase::Observer to IdentityManager::Observer.
    The only remaining use of SigninManager within PSS it to call
    SignOut() which isn't exposed by IdentityManager yet.
    
    Bug: 825190
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Icf9398d63794cab03cb559adba5c767e18c31c0a
    Reviewed-on: https://chromium-review.googlesource.com/983914
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553973}

--- components/sync/driver/signin_manager_wrapper.cc
+++ components/sync/driver/signin_manager_wrapper.cc
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/sync/driver/signin_manager_wrapper.h"
 
-#include "components/signin/core/browser/signin_manager_base.h"
+#include "services/identity/public/cpp/identity_manager.h"
 
 SigninManagerWrapper::SigninManagerWrapper(
     identity::IdentityManager* identity_manager,
@@ -23,9 +23,9 @@ SigninManagerBase* SigninManagerWrapper::GetSigninManager() {
 }
 
 std::string SigninManagerWrapper::GetEffectiveUsername() const {
-  return signin_manager_->GetAuthenticatedAccountInfo().email;
+  return identity_manager_->GetPrimaryAccountInfo().email;
 }
 
 std::string SigninManagerWrapper::GetAccountIdToUse() const {
-  return signin_manager_->GetAuthenticatedAccountId();
+  return identity_manager_->GetPrimaryAccountInfo().account_id;
 }
