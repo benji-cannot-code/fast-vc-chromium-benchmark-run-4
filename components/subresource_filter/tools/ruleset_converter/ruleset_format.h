eb1dbac6d9f5b349599ab6e6ad4f2d36155b702c
Charlie Harrison <csharrison@chromium.org>
2018-04-26 16:38:53 +0000

    Introduce the ruleset_converter
    This provides the basis of converting between rulesets in different
    formats.
    
    Bug: 833419
    Change-Id: I89f59042a19e5126624dbbac5e8cb447a47697af
    Reviewed-on: https://chromium-review.googlesource.com/1028172
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554050}

---
+++ components/subresource_filter/tools/ruleset_converter/ruleset_format.h
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_SUBRESOURCE_FILTER_TOOLS_RULESET_CONVERTER_RULESET_FORMAT_H_
+#define COMPONENTS_SUBRESOURCE_FILTER_TOOLS_RULESET_CONVERTER_RULESET_FORMAT_H_
+
+#include <string>
+
+namespace subresource_filter {
+
+// Enumerates supported ruleset serialization formats.
+enum class RulesetFormat {
+  // The format is not defined.
+  kUndefined,
+  // Text representation of FilterList rules. See
+  // //components/subresource_filter/tools/rule_parser
+  // for details.
+  kFilterList,
+  // A serialized url_pattern_index::proto::FilteringRules message.
+  kProto,
+  // UnindexedRuleset format. See
+  // //components/url_pattern_index/unindexed_ruleset.* for details.
+  kUnindexedRuleset,
+};
+
+// Functions used for compatibility with "base/commandlineflags.h".
+bool ParseFlag(const std::string& text,
+               RulesetFormat* format,
+               std::string* error);
+std::string UnparseFlag(RulesetFormat format);
+
+}  // namespace subresource_filter
+
+#endif  // COMPONENTS_SUBRESOURCE_FILTER_TOOLS_RULESET_CONVERTER_RULESET_FORMAT_H_
