cb597d0c6e7be55bfb49ed5c51543164c9289f03
Eliot Courtney <edcourtney@google.com>
2018-04-26 12:35:31 +0000

    Add PIP wayland-protocol extension and window state.
    BUG=b:69370942
    
    Change-Id: I1f0c2b8deb6f505a19185a7af5e4b0949ba6f293
    Reviewed-on: https://chromium-review.googlesource.com/1021061
    Commit-Queue: Eliot Courtney <edcourtney@chromium.org>
    Reviewed-by: David Reveman <reveman@chromium.org>
    Reviewed-by: Oliver Chang <ochang@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553993}

--- ash/wm/default_state.cc
+++ ash/wm/default_state.cc
@@ -557,6 +557,7 @@ void DefaultState::UpdateBoundsFromState(
       break;
     case mojom::WindowStateType::INACTIVE:
     case mojom::WindowStateType::AUTO_POSITIONED:
+    case mojom::WindowStateType::PIP:
       return;
   }
 
