1979d03a146e3d188d07cc057e9d1da2cc2a9e75
Tatsuhisa Yamaguchi <yamaguchi@google.com>
2018-04-26 11:41:05 +0000

    Directly write ZIP file to destination when it's not on Drive or MTP.
    Existing code always wrote zip file to temporary filesystem and then
    moved to the destination, as a workaround for crbug.com/785086.
    It is not needed for local volumes like Downloads.
    Requesting temporary file system in the guest mode caused runtime error.
    We can avoid that error by this because there is no Drive volume in the
    guest mode.
    
    Bug: 834675
    Test: manually verified by zipping files in Drive, Downloads and in guest mode
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: If920f49564309a43ddcf5ca02ee8aa6baa3e6482
    Reviewed-on: https://chromium-review.googlesource.com/1027394
    Commit-Queue: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Reviewed-by: Yuki Awano <yawano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553985}

--- ui/file_manager/file_manager/foreground/js/file_tasks.js
+++ ui/file_manager/file_manager/foreground/js/file_tasks.js
@@ -117,6 +117,15 @@ FileTasks.ZIP_ARCHIVER_ZIP_TASK_ID =
     'dmboannefpncccogfdikhmhpmdnddgoe|app|pack';
 
 /**
+ * The task id of zip action of Zip Archiver app, using temporary dir as workdir
+ * @const
+ * @type {string}
+ */
+FileTasks.ZIP_ARCHIVER_ZIP_USING_TMP_TASK_ID =
+    'dmboannefpncccogfdikhmhpmdnddgoe|app|pack_using_tmp';
+
+
+/**
  * Available tasks in task menu button.
  * @enum {string}
  */
@@ -175,7 +184,8 @@ FileTasks.create = function(
       // Filters out Pack with Zip Archiver task because it will be accessible
       // via 'Zip selection' context menu button
       taskItems = taskItems.filter(function(item) {
-        return item.taskId !== FileTasks.ZIP_ARCHIVER_ZIP_TASK_ID;
+        return item.taskId !== FileTasks.ZIP_ARCHIVER_ZIP_TASK_ID &&
+            item.taskId !== FileTasks.ZIP_ARCHIVER_ZIP_USING_TMP_TASK_ID;
       });
 
       // Filters out Unpack with Zip Archiver task if switch is not enabled.
