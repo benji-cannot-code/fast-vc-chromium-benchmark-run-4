0b05666eaf7e4aea1313f8e240d714c113252cb2
Devlin Cronin <rdevlin.cronin@chromium.org>
2018-04-26 01:08:29 +0000

    [Extensions] Update StartupHelperBrowserTests to manifest v2
    StartupHelperBrowserTests have been updated to manifest v2.
    Remove the allowance for manifest v1 extensions.
    
    Bug: 816679
    Change-Id: Ib11500da4e22cd74f60193e5fd3d3c0368fb686f
    Reviewed-on: https://chromium-review.googlesource.com/1024600
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553863}

--- chrome/browser/extensions/startup_helper_browsertest.cc
+++ chrome/browser/extensions/startup_helper_browsertest.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "extensions/common/extension.h"
-#include "extensions/common/switches.h"
 
 class StartupHelperBrowserTest : public InProcessBrowserTest {
  public:
@@ -23,10 +22,6 @@ class StartupHelperBrowserTest : public InProcessBrowserTest {
 
   void SetUpCommandLine(base::CommandLine* command_line) override {
     command_line->AppendSwitch(switches::kNoStartupWindow);
-
-    // TODO(devlin): Remove this. See https://crbug.com/816679.
-    command_line->AppendSwitch(
-        extensions::switches::kAllowLegacyExtensionManifests);
 
     PathService::Get(chrome::DIR_TEST_DATA, &test_data_dir_);
     test_data_dir_ = test_data_dir_.AppendASCII("extensions");
