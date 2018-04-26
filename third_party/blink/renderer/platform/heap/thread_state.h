8a60f57d719b4ac2e6a3109ed1993c52011bb1a0
Michael Lippautz <mlippautz@chromium.org>
2018-04-26 06:59:13 +0000

    [oilpan] Introduce incremental marking stress flag
    This frees up the regular HeapIncrementalMarking flag for implementing an
    initial incremental marking strategy.
    
    Bug: chromium:757440
    Change-Id: Iee924ef718f450b4d1e865ef6880242f501376fb
    Reviewed-on: https://chromium-review.googlesource.com/1028273
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553935}

--- third_party/blink/renderer/platform/heap/thread_state.h
+++ third_party/blink/renderer/platform/heap/thread_state.h
@@ -614,6 +614,8 @@ class PLATFORM_EXPORT ThreadState {
     safe_point_scope_marker_ = nullptr;
   }
 
+  bool ShouldVerifyMarking() const;
+
   // shouldScheduleIdleGC and shouldForceConservativeGC
   // implement the heuristics that are used to determine when to collect
   // garbage.
