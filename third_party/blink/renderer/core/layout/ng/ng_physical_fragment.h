889d69209c2fe3c9444199c840f3b1df6230eb2a
Koji Ishii <kojii@chromium.org>
2018-04-26 04:31:14 +0000

    [LayoutNG] Include frame rect into visual rect for NGPhysicalTextFragment
    This patch changes visual rect of NGPhysicalTextFragment to
    include its frame rect. Legacy does so, but NG did not when
    it was first implemented due to the lack of understanding.
    
    After reviewing test results, it turns out that by not doing
    so, background and decorations on space characters are
    affected by this difference because space characters do not
    have glyph bounding box, which results in empty visual rect.
    
    Includes rebaselines that improve the rendering.
    
    Bug: 636993, 714962
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_layout_ng;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I888f8b27680aa1ff806cca15c84919461c06fc31
    Reviewed-on: https://chromium-review.googlesource.com/1027012
    Reviewed-by: Aleks Totic <atotic@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553914}

--- third_party/blink/renderer/core/layout/ng/ng_physical_fragment.h
+++ third_party/blink/renderer/core/layout/ng/ng_physical_fragment.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/editing/forward.h"
 #include "third_party/blink/renderer/core/layout/ng/geometry/ng_physical_offset.h"
+#include "third_party/blink/renderer/core/layout/ng/geometry/ng_physical_offset_rect.h"
 #include "third_party/blink/renderer/core/layout/ng/geometry/ng_physical_size.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_break_token.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_style_variant.h"
@@ -130,6 +131,10 @@ class CORE_EXPORT NGPhysicalFragment
 
   // Returns the border-box size.
   NGPhysicalSize Size() const { return size_; }
+
+  // Returns the rect in the local coordinate of this fragment; i.e., offset is
+  // (0, 0).
+  NGPhysicalOffsetRect LocalRect() const { return {{}, size_}; }
 
   // Bitmask for border edges, see NGBorderEdges::Physical.
   unsigned BorderEdges() const { return border_edge_; }
