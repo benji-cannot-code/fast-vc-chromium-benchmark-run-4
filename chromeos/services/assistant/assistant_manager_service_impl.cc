0196c15e12969988ed2f08a618f70e4bd3231a47
Xiaohui Chen <xiaohuic@google.com>
2018-04-27 20:55:11 +0000

    Update libassistant config string
    Bug: b:78589509
    Test: locally build and run
    Change-Id: I862691a0bb5774e9761f5202cd2a23f08099061b
    Reviewed-on: https://chromium-review.googlesource.com/1031092
    Reviewed-by: Muyuan Li <muyuanli@chromium.org>
    Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554506}

--- chromeos/services/assistant/assistant_manager_service_impl.cc
+++ chromeos/services/assistant/assistant_manager_service_impl.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/assistant/internal/internal_constants.h"
 #include "chromeos/assistant/internal/internal_util.h"
 #include "chromeos/services/assistant/service.h"
+#include "chromeos/services/assistant/utils.h"
 #include "chromeos/system/version_loader.h"
 #include "libassistant/shared/internal_api/assistant_manager_internal.h"
 #include "url/gurl.h"
@@ -25,7 +26,7 @@ namespace assistant {
 
 AssistantManagerServiceImpl::AssistantManagerServiceImpl(
     mojom::AudioInputPtr audio_input)
-    : platform_api_(kDefaultConfigStr, std::move(audio_input)),
+    : platform_api_(CreateLibAssistantConfig(), std::move(audio_input)),
       action_module_(std::make_unique<action::CrosActionModule>(this)),
       display_connection_(std::make_unique<CrosDisplayConnection>(this)),
       main_thread_task_runner_(base::ThreadTaskRunnerHandle::Get()),
@@ -40,7 +41,7 @@ void AssistantManagerServiceImpl::Start(const std::string& access_token,
   base::PostTaskWithTraitsAndReplyWithResult(
       FROM_HERE, {base::MayBlock(), base::TaskPriority::USER_VISIBLE},
       base::BindOnce(&assistant_client::AssistantManager::Create,
-                     &platform_api_, kDefaultConfigStr),
+                     &platform_api_, CreateLibAssistantConfig()),
       base::BindOnce(&AssistantManagerServiceImpl::StartAssistantInternal,
                      base::Unretained(this), std::move(callback), access_token,
                      chromeos::version_loader::GetARCVersion()));
