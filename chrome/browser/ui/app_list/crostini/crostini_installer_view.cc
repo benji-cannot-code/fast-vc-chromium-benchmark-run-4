0771fee8288a6faa2042f9709a08e46f2dfbac1e
Timothy Loh <timloh@chromium.org>
2018-04-27 05:24:04 +0000

    Avoid creating Crostini UI objects on guest and ephemeral profiles
    As Crostini is not supported on guest and ephemeral profiles, we change
    IsExperimentalCrostiniUIEnabled() into IsCrostiniUIAllowedForProfile()
    so we can avoid creating the various controllers and builders, as well
    as not showing UI in the settings page. This makes us less likely to
    assume a CrostiniRegistryService exists when one actually doesn't.
    
    This is a reland of https://crrev.com/c/1029934, which happened to land
    at the same time as https://crrev.com/c/1013824 causing a compile fail.
    
    TBR=xiyuan
    
    Bug: 836187
    Change-Id: Ifefe467d1135388fa1a25c2fed22d34c09108ddb
    Reviewed-on: https://chromium-review.googlesource.com/1032131
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554316}

--- chrome/browser/ui/app_list/crostini/crostini_installer_view.cc
+++ chrome/browser/ui/app_list/crostini/crostini_installer_view.cc
@@ -46,7 +46,7 @@ constexpr int kDialogWidth = 448;
 }  // namespace
 
 void CrostiniInstallerView::Show(Profile* profile) {
-  DCHECK(IsExperimentalCrostiniUIAvailable());
+  DCHECK(IsCrostiniUIAllowedForProfile(profile));
   if (!g_crostini_installer_view) {
     g_crostini_installer_view = new CrostiniInstallerView(profile);
     views::DialogDelegate::CreateDialogWidget(g_crostini_installer_view,
--- chrome/browser/ui/webui/settings/md_settings_ui.cc
+++ chrome/browser/ui/webui/settings/md_settings_ui.cc
@@ -179,7 +179,7 @@ MdSettingsUI::MdSettingsUI(content::WebUI* web_ui)
       std::make_unique<chromeos::settings::AndroidAppsHandler>(profile));
   AddSettingsPageUIHandler(
       std::make_unique<chromeos::settings::ChangePictureHandler>());
-  if (IsExperimentalCrostiniUIAvailable()) {
+  if (IsCrostiniUIAllowedForProfile(profile)) {
     AddSettingsPageUIHandler(
         std::make_unique<chromeos::settings::CrostiniHandler>());
   }
@@ -278,7 +278,8 @@ MdSettingsUI::MdSettingsUI(content::WebUI* web_ui)
   html_source->AddBoolean("hasInternalStylus",
                           ash::stylus_utils::HasInternalStylus());
 
-  html_source->AddBoolean("showCrostini", IsExperimentalCrostiniUIAvailable());
+  html_source->AddBoolean("showCrostini",
+                          IsCrostiniUIAllowedForProfile(profile));
 
   // We have 2 variants of Android apps settings. Default case, when the Play
   // Store app exists we show expandable section that allows as to
