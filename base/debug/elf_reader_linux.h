27acb2c84fab859848b0ff4960985d9c1aad2285
Siddhartha <ssid@chromium.org>
2018-04-27 04:38:07 +0000

    Add utils to read elf binary and get Build id
    Adds code to parse elf binary to get build id from
    .note.gnu.build-id section. The build ID is needed to symbolize
    heap dumps from the users.
    
    BUG=734705
    
    Change-Id: If4365e232c060ba96071ba1e1c43618f9807e39c
    Reviewed-on: https://chromium-review.googlesource.com/1028995
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554304}

---
+++ base/debug/elf_reader_linux.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_DEBUG_ELF_READER_LINUX_H_
+#define BASE_DEBUG_ELF_READER_LINUX_H_
+
+#include <string>
+
+#include "base/base_export.h"
+#include "base/optional.h"
+
+namespace base {
+namespace debug {
+
+// Returns the ELF section .note.gnu.build-id from current executable file, if
+// present.
+Optional<std::string> BASE_EXPORT ReadElfBuildId();
+
+}  // namespace debug
+}  // namespace base
+
+#endif  // BASE_DEBUG_ELF_READER_LINUX_H_
