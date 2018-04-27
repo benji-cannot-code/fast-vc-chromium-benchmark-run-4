c091b2204b1c311d1ff8a7288dad1705c3a18a7c
vasilii <vasilii@chromium.org>
2018-04-27 16:49:35 +0000

    Rename the test for PassphraseErrorItem.
    It has nothing to do with PasswordDetailsItem and probably just a copy/paste error.
    
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Iab8bc4784141d9bef3461c987a57780d4734a99e
    Reviewed-on: https://chromium-review.googlesource.com/1032437
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554408}

--- ios/chrome/browser/ui/settings/cells/passphrase_error_item_unittest.mm
+++ ios/chrome/browser/ui/settings/cells/passphrase_error_item_unittest.mm
@@ -15,10 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-using PasswordDetailsItemTest = PlatformTest;
+using PassphraseErrorItemTest = PlatformTest;
 
 // Tests that the text label is set properly after a call to |configureCell:|.
-TEST_F(PasswordDetailsItemTest, ConfigureCell) {
+TEST_F(PassphraseErrorItemTest, ConfigureCell) {
   PassphraseErrorItem* item = [[PassphraseErrorItem alloc] initWithType:0];
   PassphraseErrorCell* cell = [[[item cellClass] alloc] init];
   EXPECT_TRUE([cell isMemberOfClass:[PassphraseErrorCell class]]);
