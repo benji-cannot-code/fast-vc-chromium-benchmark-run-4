48b1c34dd87749eae1d58ebb6d203f4a3f7a2631
Elly Fong-Jones <ellyjones@chromium.org>
2018-04-27 21:21:35 +0000

    macviews: disable PrintPreviewShouldNotBeTooSmall
    This test doesn't pass when using Views browser windows on any platform, so
    skip the body on Mac when not using Cocoa windows.
    
    TBR=tapted@chromium.org
    
    Bug: 834754
    Change-Id: I4c2d447ea5ef878eaf4e395195606be1951a0dae
    Reviewed-on: https://chromium-review.googlesource.com/1033439
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554512}

--- chrome/browser/apps/app_browsertest.cc
+++ chrome/browser/apps/app_browsertest.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/extensions/app_launch_params.h"
 #include "chrome/browser/ui/extensions/application_launch.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
+#include "chrome/browser/ui/views_mode_controller.h"
 #include "chrome/browser/ui/webui/print_preview/print_preview_ui.h"
 #include "chrome/browser/ui/zoom/chrome_zoom_level_prefs.h"
 #include "chrome/common/chrome_switches.h"
@@ -1173,7 +1174,8 @@ IN_PROC_BROWSER_TEST_F(PlatformAppBrowserTest,
 }
 
 // This test currently only passes on OS X (on other platforms the print preview
-// dialog's size is limited by the size of the window being printed).
+// dialog's size is limited by the size of the window being printed). It also
+// doesn't pass in Views mode on OS X.
 #if !defined(OS_MACOSX)
 #define MAYBE_PrintPreviewShouldNotBeTooSmall \
     DISABLED_PrintPreviewShouldNotBeTooSmall
@@ -1184,6 +1186,10 @@ IN_PROC_BROWSER_TEST_F(PlatformAppBrowserTest,
 
 IN_PROC_BROWSER_TEST_F(PlatformAppBrowserTest,
                        MAYBE_PrintPreviewShouldNotBeTooSmall) {
+#if defined(OS_MACOSX)
+  if (!views_mode_controller::IsViewsBrowserCocoa())
+    return;
+#endif
   // Print preview dialogs with widths less than 410 pixels will have preview
   // areas that are too small, and ones with heights less than 191 pixels will
   // have vertical scrollers for their controls that are too small.
