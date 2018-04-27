0780a25b5b0138fa0e0f9d9fbb256682d3d12f38
Tetsui Ohkubo <tetsui@chromium.org>
2018-04-27 01:19:14 +0000

    Split notification from TraySessionLengthLimit
    Session length limit notification was generated in
    TraySessionLengthLimit, which is a SystemTrayItem.
    
    We're going to get rid of SystemTrayItems as UnifiedSystemTray will
    replace them, so we should generate the notification outside system
    tray.
    
    TEST=SessionLimitNotificationControllerTest
    BUG=none
    
    Change-Id: I370ff7048d47cbae9ce5093ccffdf4e4685c3e97
    Reviewed-on: https://chromium-review.googlesource.com/1017520
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554252}

--- ash/system/system_notification_controller.cc
+++ ash/system/system_notification_controller.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/caps_lock_notification_controller.h"
 #include "ash/system/power/power_notification_controller.h"
 #include "ash/system/screen_security/screen_security_notification_controller.h"
+#include "ash/system/session/session_limit_notification_controller.h"
 #include "ash/system/supervised/supervised_notification_controller.h"
 #include "ui/message_center/message_center.h"
 
@@ -19,6 +20,7 @@ SystemNotificationController::SystemNotificationController()
           message_center::MessageCenter::Get())),
       screen_security_(
           std::make_unique<ScreenSecurityNotificationController>()),
+      session_limit_(std::make_unique<SessionLimitNotificationController>()),
       supervised_(std::make_unique<SupervisedNotificationController>()) {}
 
 SystemNotificationController::~SystemNotificationController() = default;
