7124316c9f85faa697ee336ccd3ede3550b88464
Kouhei Ueno <kouhei@chromium.org>
2018-04-27 09:18:18 +0000

    BackgroundHTMLParser: Make token limits unconfigurable
    I couldn't find any occurrence in tests that attempt to dynamically reconfigure this.
    This CL just makes them use constants to reduce complexity.
    
    Bug: 689702
    Change-Id: I431aaec674a439d82499a1b4099d4de64e75f5c1
    Reviewed-on: https://chromium-review.googlesource.com/1032313
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554343}

--- third_party/blink/renderer/core/html/parser/html_document_parser.cc
+++ third_party/blink/renderer/core/html/parser/html_document_parser.cc
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/document_fragment.h"
 #include "third_party/blink/renderer/core/dom/element.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
-#include "third_party/blink/renderer/core/frame/settings.h"
 #include "third_party/blink/renderer/core/html/html_document.h"
 #include "third_party/blink/renderer/core/html/parser/atomic_html_token.h"
 #include "third_party/blink/renderer/core/html/parser/background_html_parser.h"
@@ -804,24 +803,6 @@ void HTMLDocumentParser::StartBackgroundParser() {
   config->xss_auditor->Init(GetDocument(), &xss_auditor_delegate_);
 
   config->decoder = TakeDecoder();
-  if (GetDocument()->GetSettings()) {
-    if (GetDocument()
-            ->GetSettings()
-            ->GetBackgroundHtmlParserOutstandingTokenLimit()) {
-      config->outstanding_token_limit =
-          GetDocument()
-              ->GetSettings()
-              ->GetBackgroundHtmlParserOutstandingTokenLimit();
-    }
-    if (GetDocument()
-            ->GetSettings()
-            ->GetBackgroundHtmlParserPendingTokenLimit()) {
-      config->pending_token_limit =
-          GetDocument()
-              ->GetSettings()
-              ->GetBackgroundHtmlParserPendingTokenLimit();
-    }
-  }
 
   DCHECK(config->xss_auditor->IsSafeToSendToAnotherThread());
 
