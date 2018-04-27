ed1395640e7cba748efa6cb3659a5cc6f5aa1b57
Greg Kraynov <kraynov@chromium.org>
2018-04-27 18:51:33 +0000

    Simplify LazyNow and prepare it to move to //base.
    Inspired by https://chromium-review.googlesource.com/1015743
    but not moving it yet, because move will happen in one go.
    
    Bug: 783309
    Change-Id: I4ef38820accab561b73b28d9bfa9fcd2ee2f47a8
    Reviewed-on: https://chromium-review.googlesource.com/1032773
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Greg Kraynov <kraynov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554433}

--- third_party/blink/renderer/platform/scheduler/base/lazy_now.cc
+++ third_party/blink/renderer/platform/scheduler/base/lazy_now.cc
@@ -9,10 +9,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 namespace scheduler {
+
+LazyNow::LazyNow(base::TimeTicks now) : tick_clock_(nullptr), now_(now) {
+  DCHECK(!now.is_null());
+}
+
+LazyNow::LazyNow(const base::TickClock* tick_clock)
+    : tick_clock_(tick_clock), now_() {
+  DCHECK(tick_clock);
+}
+
+LazyNow::LazyNow(LazyNow&& move_from)
+    : tick_clock_(move_from.tick_clock_), now_(move_from.now_) {
+  move_from.tick_clock_ = nullptr;
+  move_from.now_ = base::TimeTicks();
+}
+
 base::TimeTicks LazyNow::Now() {
-  if (!now_)
+  // TickClock might return null values only in tests, we're okay with
+  // extra calls which might occur in that case.
+  if (now_.is_null()) {
+    DCHECK(tick_clock_);  // It can fire only on use after std::move.
     now_ = tick_clock_->NowTicks();
-  return now_.value();
+  }
+  return now_;
 }
 
 }  // namespace scheduler
