fbe3ec9a6e9822ab6c11a63e4a1ba1d933fb9ded
Timothy Loh <timloh@chromium.org>
2018-04-27 04:05:05 +0000

    Revert "Avoid creating Crostini UI objects on guest and ephemeral profiles"
    This reverts commit 90d409102910100cbcb3e3073e28fea283288944.
    
    Reason for revert: Landed at the same time as https://chromium-review.googlesource.com/c/chromium/src/+/1013824 which used the old function.
    
    Original change's description:
    > Avoid creating Crostini UI objects on guest and ephemeral profiles
    > 
    > As Crostini is not supported on guest and ephemeral profiles, we change
    > IsExperimentalCrostiniUIEnabled() into IsCrostiniUIAllowedForProfile()
    > so we can avoid creating the various controllers and builders. This
    > makes us less likely to assume a CrostiniRegistryService exists when
    > one actually doesn't.
    > 
    > Bug: 836187
    > Change-Id: I5a65ad250edcb16daf381dfd77d7a5df0a1de98f
    > Reviewed-on: https://chromium-review.googlesource.com/1029934
    > Commit-Queue: Timothy Loh <timloh@chromium.org>
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#554294}
    
    TBR=xiyuan@chromium.org,timloh@chromium.org
    
    Change-Id: I76a977c63aaffdf4de5cc54c8cb02d9a345f2e43
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 836187
    Reviewed-on: https://chromium-review.googlesource.com/1031993
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554296}

--- chrome/browser/chromeos/crostini/crostini_util.cc
+++ chrome/browser/chromeos/crostini/crostini_util.cc
@@ -70,13 +70,7 @@ bool IsCrostiniAllowed() {
          virtual_machines::AreVirtualMachinesAllowedByPolicy();
 }
 
-bool IsCrostiniUIAllowedForProfile(Profile* profile) {
-  if (!profile || profile->IsOffTheRecord() ||
-      chromeos::ProfileHelper::IsEphemeralUserProfile(profile) ||
-      chromeos::ProfileHelper::IsLockScreenAppProfile(profile)) {
-    return false;
-  }
-
+bool IsExperimentalCrostiniUIAvailable() {
   return IsCrostiniAllowed() &&
          base::FeatureList::IsEnabled(features::kExperimentalCrostiniUI);
 }
--- chrome/browser/ui/app_list/crostini/crostini_installer_view.cc
+++ chrome/browser/ui/app_list/crostini/crostini_installer_view.cc
@@ -46,7 +46,7 @@ constexpr int kDialogWidth = 448;
 }  // namespace
 
 void CrostiniInstallerView::Show(Profile* profile) {
-  DCHECK(IsCrostiniUIAllowedForProfile(profile));
+  DCHECK(IsExperimentalCrostiniUIAvailable());
   if (!g_crostini_installer_view) {
     g_crostini_installer_view = new CrostiniInstallerView(profile);
     views::DialogDelegate::CreateDialogWidget(g_crostini_installer_view,
