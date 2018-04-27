be4f0f721cfd4447c028827e9725fbbe0fe258c0
Kuo-Hsin Yang <vovoy@chromium.org>
2018-04-27 06:20:39 +0000

    [tools/perf] Option --output-deps outputs deps to a json file
    Bug: 834659
    Change-Id: I5df0bc9b923c7a780de896f71cc68443902de12b
    Reviewed-on: https://chromium-review.googlesource.com/1031991
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Commit-Queue: Vovo Yang <vovoy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554325}

--- tools/perf/fetch_benchmark_deps.py
+++ tools/perf/fetch_benchmark_deps.py
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 """This module fetches and prints the dependencies given a benchmark."""
 
 import argparse
+import json
 import optparse
 import os
 import sys
@@ -65,14 +66,7 @@ def _EnumerateDependencies(story_set):
   return [dep[prefix_len:] for dep in deps if dep]
 
 
-def _PrintDeps(mesg, output_deps):
-  if output_deps:
-    print mesg
-  else:
-    logging.info(mesg)
-
-
-def FetchDepsForBenchmark(benchmark, output_deps):
+def _FetchDepsForBenchmark(benchmark):
   # Create a dummy options object which hold default values that are expected
   # by Benchmark.CreateStorySet(options) method.
   parser = optparse.OptionParser()
@@ -84,11 +78,11 @@ def FetchDepsForBenchmark(benchmark, output_deps):
   _FetchDependenciesIfNeeded(story_set)
 
   # Log files downloaded.
-  _PrintDeps('Fetch dependencies for benchmark %s' % benchmark.Name(),
-             output_deps)
+  logging.info('Fetch dependencies for benchmark %s' % benchmark.Name())
   deps = _EnumerateDependencies(story_set)
   for dep in deps:
-    _PrintDeps("Dependency: " + dep, output_deps)
+    logging.info("Dependency: " + dep)
+  return deps
 
 
 def main(args):
@@ -99,14 +93,12 @@ def main(args):
                       help=('Force fetching all the benchmarks when '
                             'benchmark_name is not specified'),
                       action='store_true', default=False)
-  # Flag --output-deps: print the dependencies to stdout, CrOS autotest
+  # Flag --output-deps: output the dependencies to a json file, CrOS autotest
   # telemetry_runner parses the output to upload the dependencies to the DUT.
-  # Example output, fetch_benchmark_deps.py --output-deps octane:
-  # Fetch dependencies for benchmark octane
-  # Dependency: tools/perf/page_sets/data/octane_002.wprgo
+  # Example output, fetch_benchmark_deps.py --output-deps=deps octane:
+  # {'octane': ['tools/perf/page_sets/data/octane_002.wprgo']}
   parser.add_argument('--output-deps',
-                      help=('Print dependencies to stdout'),
-                      action='store_true', default=False)
+                      help=('Output dependencies to a json file'))
   parser.add_argument(
         '-v', '--verbose', action='count', dest='verbosity',
         help='Increase verbosity level (repeat as needed)')
@@ -120,6 +112,7 @@ def main(args):
   else:
     logging.getLogger().setLevel(logging.WARNING)
 
+  deps = {}
   if options.benchmark_name:
     perf_dir = path_util.GetPerfDir()
     benchmark_dirs=[os.path.join(perf_dir, 'benchmarks'),
@@ -130,14 +123,18 @@ def main(args):
         options.benchmark_name, config)
     if not benchmark:
       raise ValueError('No such benchmark: %s' % options.benchmark_name)
-    FetchDepsForBenchmark(benchmark, options.output_deps)
+    deps[benchmark.Name()] = _FetchDepsForBenchmark(benchmark)
   else:
     if not options.force:
       raw_input(
           'No benchmark name is specified. Fetching all benchmark deps. '
           'Press enter to continue...')
     for b in benchmark_finders.GetAllPerfBenchmarks():
-      FetchDepsForBenchmark(b, options.output_deps)
+      deps[b.Name()] = _FetchDepsForBenchmark(b)
+
+  if options.output_deps:
+    with open(options.output_deps, 'w') as outfile:
+      json.dump(deps, outfile)
 
 
 if __name__ == '__main__':
