4ba3af5c381262eeace6adc2df385eb6a545d249
Filip Gorski <fgorski@google.com>
2018-04-27 20:26:46 +0000

    [EoC] Adding UMA for page view time
    * This is accomplished by adding a PageViewTimer class, which uses
      a TabObserver and a TabModelObserver to measure time spent viewing
      pages
    
    Bug: 824186
    Change-Id: I14fc98112d254f44be969307a5c9b74f7969f93a
    Reviewed-on: https://chromium-review.googlesource.com/1023054
    Commit-Queue: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554494}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
@@ -69,6 +69,7 @@ import org.chromium.chrome.browser.compositor.layouts.SceneChangeObserver;
 import org.chromium.chrome.browser.compositor.layouts.content.ContentOffsetProvider;
 import org.chromium.chrome.browser.compositor.layouts.content.TabContentManager;
 import org.chromium.chrome.browser.contextual_suggestions.ContextualSuggestionsCoordinator;
+import org.chromium.chrome.browser.contextual_suggestions.PageViewTimer;
 import org.chromium.chrome.browser.contextualsearch.ContextualSearchFieldTrial;
 import org.chromium.chrome.browser.contextualsearch.ContextualSearchManager;
 import org.chromium.chrome.browser.contextualsearch.ContextualSearchManager.ContextualSearchTabPromotionDelegate;
@@ -288,6 +289,9 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
     /** Whether or not a PolicyChangeListener was added. */
     private boolean mDidAddPolicyChangeListener;
 
+    /** Adds TabObserver and TabModelObserver to measure page view times. */
+    private PageViewTimer mPageViewTimer;
+
     /**
      * @param factory The {@link AppMenuHandlerFactory} for creating {@link #mAppMenuHandler}
      */
@@ -389,6 +393,7 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
         ((BottomContainer) findViewById(R.id.bottom_container)).initialize(mFullscreenManager);
 
         mModalDialogManager = createModalDialogManager();
+        mPageViewTimer = new PageViewTimer(mTabModelSelector);
     }
 
     @Override
@@ -1135,6 +1140,11 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
     @SuppressLint("NewApi")
     @Override
     protected final void onDestroy() {
+        if (mPageViewTimer != null) {
+            mPageViewTimer.destroy();
+            mPageViewTimer = null;
+        }
+
         if (mReaderModeManager != null) {
             mReaderModeManager.destroy();
             mReaderModeManager = null;
