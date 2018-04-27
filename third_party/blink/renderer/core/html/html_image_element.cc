6d323e20c73daaa60ea24b94b16a508f072e6df3
Ian Clelland <iclelland@chromium.org>
2018-04-27 22:08:30 +0000

    Recalculate image style when inverting for policy
    When an image policy is violated, and the image is flagged for color
    inversion, recalc the style so that the change is actually rendered to
    screen.
    
    Bug: 837346
    Change-Id: I2cb4f84432d03917435294af01c5b9ecc85a0fc2
    Reviewed-on: https://chromium-review.googlesource.com/1033544
    Commit-Queue: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554525}

--- third_party/blink/renderer/core/html/html_image_element.cc
+++ third_party/blink/renderer/core/html/html_image_element.cc
@@ -23,11 +23,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "third_party/blink/renderer/core/html/html_image_element.h"
-
 #include "third_party/blink/renderer/bindings/core/v8/script_event_listener.h"
 #include "third_party/blink/renderer/core/css/media_query_matcher.h"
 #include "third_party/blink/renderer/core/css/media_values_dynamic.h"
 #include "third_party/blink/renderer/core/css/parser/sizes_attribute_parser.h"
+#include "third_party/blink/renderer/core/css/style_change_reason.h"
 #include "third_party/blink/renderer/core/css_property_names.h"
 #include "third_party/blink/renderer/core/dom/attribute.h"
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
@@ -825,7 +825,12 @@ bool HTMLImageElement::ShouldInvertColor() const {
 }
 
 void HTMLImageElement::UpdateShouldInvertColor(bool value) {
-  should_invert_color_ = value;
+  if (should_invert_color_ != value) {
+    should_invert_color_ = value;
+    SetNeedsStyleRecalc(StyleChangeType::kLocalStyleChange,
+                        StyleChangeReasonForTracing::Create(
+                            StyleChangeReason::kPolicyViolation));
+  }
 }
 
 }  // namespace blink
