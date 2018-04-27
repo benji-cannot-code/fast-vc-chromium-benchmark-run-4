04682c024b126c662e175ac9aa13462c7110031d
Eric Seckler <eseckler@chromium.org>
2018-04-27 03:17:34 +0000

    headless: Adjust browser window size to contain all WebContents
    When setting the bounds of a WebContents, we shouldn't also change the
    browser window size to the size of the WebContents. This can lead to
    the bounds of the WebContents lying outside the browser's window, in
    which case screenshots will hang.
    
    Instead, make sure that all WebContents are contained within the browser
    window size.
    
    Bug: 832138
    Change-Id: Iaeb8b36b4edceedfdf2fdd1d54c3765587c79ea7
    Reviewed-on: https://chromium-review.googlesource.com/1031655
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554285}

--- headless/lib/headless_web_contents_browsertest.cc
+++ headless/lib/headless_web_contents_browsertest.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_switches.h"
 #include "content/public/test/browser_test.h"
 #include "headless/lib/browser/headless_web_contents_impl.h"
+#include "headless/public/devtools/domains/browser.h"
 #include "headless/public/devtools/domains/dom_snapshot.h"
 #include "headless/public/devtools/domains/emulation.h"
 #include "headless/public/devtools/domains/headless_experimental.h"
@@ -437,6 +438,42 @@ HEADLESS_ASYNC_DEVTOOLED_TEST_P(HeadlessWebContentsScreenshotTest);
 // Instantiate test case for both software and gpu compositing modes.
 INSTANTIATE_TEST_CASE_P(HeadlessWebContentsScreenshotTests,
                         HeadlessWebContentsScreenshotTest,
+                        ::testing::Bool());
+
+// Regression test for crbug.com/832138.
+class HeadlessWebContentsScreenshotWindowPositionTest
+    : public HeadlessWebContentsScreenshotTest {
+ public:
+  void RunDevTooledTest() override {
+    browser_devtools_client_->GetBrowser()->GetExperimental()->SetWindowBounds(
+        browser::SetWindowBoundsParams::Builder()
+            .SetWindowId(
+                HeadlessWebContentsImpl::From(web_contents_)->window_id())
+            .SetBounds(browser::Bounds::Builder()
+                           .SetLeft(600)
+                           .SetTop(100)
+                           .SetWidth(800)
+                           .SetHeight(600)
+                           .Build())
+            .Build(),
+        base::BindOnce(
+            &HeadlessWebContentsScreenshotWindowPositionTest::OnWindowBoundsSet,
+            base::Unretained(this)));
+  }
+
+  void OnWindowBoundsSet(
+      std::unique_ptr<browser::SetWindowBoundsResult> result) {
+    EXPECT_TRUE(result);
+    HeadlessWebContentsScreenshotTest::RunDevTooledTest();
+  }
+};
+
+HEADLESS_ASYNC_DEVTOOLED_TEST_P(
+    HeadlessWebContentsScreenshotWindowPositionTest);
+
+// Instantiate test case for both software and gpu compositing modes.
+INSTANTIATE_TEST_CASE_P(HeadlessWebContentsScreenshotWindowPositionTests,
+                        HeadlessWebContentsScreenshotWindowPositionTest,
                         ::testing::Bool());
 
 #if BUILDFLAG(ENABLE_PRINTING)
