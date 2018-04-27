059cecd1d7976a1e66b34068ca6012360899cd6c
Takuto Ikuta <tikuta@chromium.org>
2018-04-27 11:56:47 +0000

    Add override for overridden functions under /url This CL was uploaded by git cl split.
    R=mkwst@chromium.org
    
    Bug: 428099
    Change-Id: Ice6a3365769fe40b81100ac216a4066424955119
    Reviewed-on: https://chromium-review.googlesource.com/1032459
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554357}

--- url/url_canon.h
+++ url/url_canon.h
@@ -157,7 +157,7 @@ class RawCanonOutputT : public CanonOutputT<T> {
     this->buffer_ = fixed_buffer_;
     this->buffer_len_ = fixed_capacity;
   }
-  virtual ~RawCanonOutputT() {
+  ~RawCanonOutputT() override {
     if (this->buffer_ != fixed_buffer_)
       delete[] this->buffer_;
   }
