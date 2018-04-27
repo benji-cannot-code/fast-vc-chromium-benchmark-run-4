fc17865810dd3b76019f2d5aff0aba9c7ce303e8
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-04-27 17:40:31 +0000

    Import wpt@e36d2be3dc7b90ecca42be620a7bac36158c06b1
    Using wpt-import in Chromium 59bde77b6a6daf119540e44b95a193f981412d95.
    With Chromium commits locally applied on WPT:
    303956b6ed "Enable WPT tests for the Generic Sensor classes"
    
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/16306
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    smcgruer@chromium.org:
      external/wpt/css/css-position
    yhirano@chromium.org, mkwst@chromium.org, japhet@chromium.org:
      external/wpt/fetch
    
    TBR=foolip
    
    No-Export: true
    Change-Id: Ib40634f3b14402635670aca7754f20d3bd8a5c4b
    Reviewed-on: https://chromium-review.googlesource.com/1032814
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554416}

---
+++ third_party/WebKit/LayoutTests/external/wpt/fetch/api/idl.any.js
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: global=window,worker
+// META: script=/resources/WebIDLParser.js
+// META: script=/resources/idlharness.js
+
+promise_test(async() => {
+  const text = await (await fetch("/interfaces/fetch.idl")).text();
+  const idl_array = new IdlArray();
+  idl_array.add_idls(text);
+  idl_array.add_untested_idls("[Exposed=(Window,Worker)] interface AbortSignal {};");
+  idl_array.add_untested_idls("[Exposed=(Window,Worker)] interface ReadableStream {};");
+  idl_array.add_untested_idls("enum ReferrerPolicy {};");
+  idl_array.add_objects({
+    Headers: ["new Headers()"],
+    Request: ["new Request('about:blank')"],
+    Response: ["new Response()"],
+  });
+  idl_array.test();
+}, "Fetch Standard IDL");
