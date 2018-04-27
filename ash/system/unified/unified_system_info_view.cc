90160f9f2fb7f541a72e63c01616574e35d4e14f
Tetsui Ohkubo <tetsui@chromium.org>
2018-04-27 07:26:51 +0000

    Make enterprise managed label clickable.
    In old SystemTray, "This device is managed by example.com" row is a
    long to learn more. In UnifiedSystemTray, enterprise management state is
    shown on the right bottom, but it was not clickable. This CL makes it
    clickable.
    
    Screenshot: http://screen/2q9GG6nqpZY
    
    TEST=manual
    BUG=b/78304937
    
    Change-Id: I893144e61473e983ab56cff1c6f379340cf51fcb
    Reviewed-on: https://chromium-review.googlesource.com/1032030
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554331}

--- ash/system/unified/unified_system_info_view.cc
+++ ash/system/unified/unified_system_info_view.cc
@@ -14,13 +14,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/model/enterprise_domain_model.h"
 #include "ash/system/model/system_tray_model.h"
 #include "ash/system/power/power_status.h"
+#include "ash/system/tray/system_tray_controller.h"
 #include "ash/system/tray/system_tray_notifier.h"
 #include "ash/system/tray/tray_constants.h"
+#include "ash/system/tray/tray_popup_utils.h"
 #include "base/i18n/time_formatting.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/time/time.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/gfx/paint_vector_icon.h"
+#include "ui/views/controls/button/button.h"
 #include "ui/views/controls/image_view.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/controls/separator.h"
@@ -183,11 +186,15 @@ void BatteryView::ConfigureLabel(views::Label* label) {
 
 // A view that shows whether the device is enterprise managed or not. It updates
 // by observing EnterpriseDomainModel.
-class EnterpriseManagedView : public views::View,
+class EnterpriseManagedView : public views::Button,
+                              public views::ButtonListener,
                               public EnterpriseDomainObserver {
  public:
   EnterpriseManagedView();
   ~EnterpriseManagedView() override;
+
+  // views::ButtonListener:
+  void ButtonPressed(views::Button* sender, const ui::Event& event) override;
 
   // EnterpriseDomainObserver:
   void OnEnterpriseDomainChanged() override;
@@ -202,7 +209,7 @@ class EnterpriseManagedView : public views::View,
   DISALLOW_COPY_AND_ASSIGN(EnterpriseManagedView);
 };
 
-EnterpriseManagedView::EnterpriseManagedView() {
+EnterpriseManagedView::EnterpriseManagedView() : Button(this) {
   DCHECK(Shell::Get());
   Shell::Get()->system_tray_model()->enterprise_domain()->AddObserver(this);
 
@@ -223,6 +230,8 @@ EnterpriseManagedView::EnterpriseManagedView() {
   image->SetPreferredSize(
       gfx::Size(kUnifiedSystemInfoHeight, kUnifiedSystemInfoHeight));
   AddChildView(image);
+
+  TrayPopupUtils::ConfigureTrayPopupButton(this);
 
   Update();
 }
@@ -246,6 +255,11 @@ bool EnterpriseManagedView::GetTooltipText(const gfx::Point& p,
   } else {
     return false;
   }
+}
+
+void EnterpriseManagedView::ButtonPressed(views::Button* sender,
+                                          const ui::Event& event) {
+  Shell::Get()->system_tray_controller()->ShowEnterpriseInfo();
 }
 
 void EnterpriseManagedView::OnEnterpriseDomainChanged() {
