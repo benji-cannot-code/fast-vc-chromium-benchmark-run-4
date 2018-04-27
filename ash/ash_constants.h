4b892f9885a871667e5eba54d6e7282ba9f3aef8
Sara Kato <sarakato@chromium.org>
2018-04-27 04:24:38 +0000

    Remove border outline when focused when tab key is used.
    Bug: b/74415372
    Test: Manually Tested
    Change-Id: I4e3e5a5b48054b6a7c59e2a02b38b0d200885fee
    Reviewed-on: https://chromium-review.googlesource.com/1009243
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Commit-Queue: Sara Kato <sarakato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554301}

--- ash/ash_constants.h
+++ ash/ash_constants.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 
+#include "third_party/skia/include/core/SkColor.h"
+
 typedef unsigned int SkColor;
 
 namespace ash {
@@ -34,8 +36,8 @@ ASH_EXPORT extern const float kInactiveFrameButtonIconAlphaRatio;
 ASH_EXPORT extern const float kInactiveFrameButtonIconAlphaRatioTouch;
 
 // The border color of keyboard focus for launcher items and system tray.
-extern const SkColor kFocusBorderColor;
-extern const int kFocusBorderThickness;
+constexpr SkColor kFocusBorderColor = SK_ColorTRANSPARENT;
+constexpr int kFocusBorderThickness = 0;
 
 ASH_EXPORT extern const int kDefaultLargeCursorSize;
 
