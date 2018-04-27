32853b474b69b0765934b51f183b4d89656b2705
Sunny Sachanandani <sunnyps@chromium.org>
2018-04-27 22:56:09 +0000

    Do not throttle BeginMainFrame on SubmitCompositorFrameAck
    The original motivation for this code was to workaround early swap ack
    on freon, but since freon isn't a thing anymore and we have a display
    compositor that actually performs the swap, it should be ok to get rid
    of it. This should improve throughput similar to main frame before
    activation.
    
    R=brianderson
    BUG=311213
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I5108c74a57ba201d25999989de8da4c7f81a5176
    Reviewed-on: https://chromium-review.googlesource.com/1031892
    Reviewed-by: Brian Anderson <brianderson@chromium.org>
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554547}

--- cc/scheduler/scheduler_unittest.cc
+++ cc/scheduler/scheduler_unittest.cc
@@ -1855,7 +1855,7 @@ void SchedulerTest::ImplFrameNotSkippedAfterLateAck() {
     scheduler_->SetNeedsBeginMainFrame();
     EXPECT_FALSE(scheduler_->MainThreadMissedLastDeadline());
     SendNextBeginFrame();
-    EXPECT_ACTIONS("WillBeginImplFrame");
+    EXPECT_ACTIONS("WillBeginImplFrame", "ScheduledActionSendBeginMainFrame");
     EXPECT_TRUE(client_->IsInsideBeginImplFrame());
     EXPECT_FALSE(scheduler_->MainThreadMissedLastDeadline());
 
@@ -1867,8 +1867,7 @@ void SchedulerTest::ImplFrameNotSkippedAfterLateAck() {
     task_runner().RunTasksWhile(client_->InsideBeginImplFrame(true));
 
     // Verify that we don't skip the actions of the BeginImplFrame
-    EXPECT_ACTIONS("ScheduledActionSendBeginMainFrame", "ScheduledActionCommit",
-                   "ScheduledActionActivateSyncTree",
+    EXPECT_ACTIONS("ScheduledActionCommit", "ScheduledActionActivateSyncTree",
                    "ScheduledActionDrawIfPossible");
   }
 }
@@ -1944,7 +1943,8 @@ TEST_F(SchedulerTest, MainFrameThenImplFrameSkippedAfterLateCommitAndLateAck) {
                  "ScheduledActionSendBeginMainFrame", "ScheduledActionCommit",
                  "ScheduledActionActivateSyncTree");
 
-  // Draw and swap for first commit, start second commit.
+  // Draw and swap for first commit, start second commit which also misses
+  // deadline.
   client_->Reset();
   scheduler_->SetNeedsBeginMainFrame();
   EXPECT_TRUE(scheduler_->MainThreadMissedLastDeadline());
@@ -1960,39 +1960,31 @@ TEST_F(SchedulerTest, MainFrameThenImplFrameSkippedAfterLateCommitAndLateAck) {
                  "ScheduledActionActivateSyncTree");
 
   // Don't call scheduler_->DidReceiveCompositorFrameAck() until after next
-  // frame
-  // to put the impl thread in a high latency mode.
+  // frame to put the impl thread in a high latency mode.
   client_->Reset();
-  scheduler_->SetNeedsBeginMainFrame();
   EXPECT_TRUE(scheduler_->MainThreadMissedLastDeadline());
   EXPECT_SCOPED(AdvanceFrame());
-  EXPECT_TRUE(scheduler_->MainThreadMissedLastDeadline());
   task_runner().RunTasksWhile(client_->InsideBeginImplFrame(true));
-
   EXPECT_ACTIONS("WillBeginImplFrame");
-  // Note: BeginMainFrame and swap are skipped here because of
-  // swap ack backpressure, not because of latency recovery.
-  EXPECT_FALSE(client_->HasAction("ScheduledActionSendBeginMainFrame"));
-  EXPECT_FALSE(client_->HasAction("ScheduledActionDrawIfPossible"));
   EXPECT_TRUE(scheduler_->MainThreadMissedLastDeadline());
 
   // Lower estimates so that the scheduler will attempt latency recovery.
   fake_compositor_timing_history_->SetAllEstimatesTo(kFastDuration);
 
-  // Now that both threads are in a high latency mode, make sure we
-  // skip the BeginMainFrame, then the BeginImplFrame, but not both
-  // at the same time.
+  // Now that both threads are in a high latency mode, make sure we skip the
+  // BeginMainFrame, then the BeginImplFrame, but not both at the same time.
 
   // Verify we skip BeginMainFrame first.
   client_->Reset();
-  // Previous commit request is still outstanding.
-  EXPECT_TRUE(scheduler_->NeedsBeginMainFrame());
+  scheduler_->SetNeedsBeginMainFrame();
   EXPECT_TRUE(scheduler_->IsDrawThrottled());
   SendNextBeginFrame();
   EXPECT_TRUE(scheduler_->MainThreadMissedLastDeadline());
   scheduler_->DidReceiveCompositorFrameAck();
   task_runner().RunTasksWhile(client_->InsideBeginImplFrame(true));
 
+  // Main thread missed last deadline state is reset after main thread's tree is
+  // drawn.
   EXPECT_FALSE(scheduler_->MainThreadMissedLastDeadline());
   EXPECT_ACTIONS("WillBeginImplFrame", "ScheduledActionDrawIfPossible");
 
@@ -2015,27 +2007,22 @@ TEST_F(SchedulerTest, MainFrameThenImplFrameSkippedAfterLateCommitAndLateAck) {
   EXPECT_TRUE(scheduler_->NeedsBeginMainFrame());
   EXPECT_FALSE(scheduler_->MainThreadMissedLastDeadline());
   SendNextBeginFrame();
-  EXPECT_FALSE(scheduler_->MainThreadMissedLastDeadline());
   scheduler_->NotifyBeginMainFrameStarted(now_src()->NowTicks());
   scheduler_->NotifyReadyToCommit();
   scheduler_->NotifyReadyToActivate();
   task_runner().RunTasksWhile(client_->InsideBeginImplFrame(true));
-  EXPECT_FALSE(scheduler_->MainThreadMissedLastDeadline());
   scheduler_->DidReceiveCompositorFrameAck();
-  EXPECT_FALSE(scheduler_->MainThreadMissedLastDeadline());
 
   EXPECT_ACTIONS("WillBeginImplFrame", "ScheduledActionSendBeginMainFrame",
                  "ScheduledActionCommit", "ScheduledActionActivateSyncTree",
                  "ScheduledActionDrawIfPossible");
 }
 
-TEST_F(
-    SchedulerTest,
-    Deadlock_CommitMakesProgressWhileSwapTrottledAndActiveTreeNeedsFirstDraw) {
+TEST_F(SchedulerTest,
+       CommitMakesProgressWhileSwapTrottledAndActiveTreeNeedsFirstDraw) {
+  // Historical note:
   // NPAPI plugins on Windows block the Browser UI thread on the Renderer main
   // thread. This prevents the scheduler from receiving any pending swap acks.
-
-  scheduler_settings_.main_frame_while_submit_frame_throttled_enabled = true;
   SetUpScheduler(EXTERNAL_BFS);
 
   // Disables automatic swap acks so this test can force swap ack throttling
@@ -2095,7 +2082,6 @@ TEST_F(
 
   // Since we are simulating a long commit, set up a client with draw duration
   // estimates that prevent skipping main frames to get to low latency mode.
-  scheduler_settings_.main_frame_while_submit_frame_throttled_enabled = true;
   scheduler_settings_.main_frame_before_activation_enabled = true;
   SetUpScheduler(EXTERNAL_BFS);
 
@@ -2247,7 +2233,7 @@ void SchedulerTest::BeginFramesNotFromClient_IsDrawThrottled(
   scheduler_->SetNeedsBeginMainFrame();
   scheduler_->SetNeedsRedraw();
   EXPECT_SCOPED(AdvanceFrame());  // Run posted BeginFrame.
-  EXPECT_ACTIONS("WillBeginImplFrame");
+  EXPECT_ACTIONS("WillBeginImplFrame", "ScheduledActionSendBeginMainFrame");
   EXPECT_TRUE(client_->IsInsideBeginImplFrame());
   client_->Reset();
 
@@ -2272,7 +2258,7 @@ void SchedulerTest::BeginFramesNotFromClient_IsDrawThrottled(
 
   // Take us out of a swap throttled state.
   scheduler_->DidReceiveCompositorFrameAck();
-  EXPECT_ACTIONS("ScheduledActionSendBeginMainFrame");
+  EXPECT_NO_ACTION();
   EXPECT_TRUE(client_->IsInsideBeginImplFrame());
   client_->Reset();
 
