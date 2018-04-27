9c5796fb27971cf32a3825bea66f42aa5ee7f38d
Nicholas Verne <nverne@chromium.org>
2018-04-27 23:52:41 +0000

    The Termial App Item now has an Uninstall in its context menu.
    This runs the uninstall flow for crostini, which now includes
    stopping the concierge service then unloading the cros-termina
    component.
    
    Bug: 822507
    Change-Id: I31cc40a3ad6732a55ff3f9ae47468751d996c6cf
    Reviewed-on: https://chromium-review.googlesource.com/1032353
    Commit-Queue: Nicholas Verne <nverne@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554567}

--- chrome/browser/chromeos/crostini/crostini_remover.cc
+++ chrome/browser/chromeos/crostini/crostini_remover.cc
@@ -5,8 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/crostini/crostini_remover.h"
 
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/chromeos/crostini/crostini_pref_names.h"
 #include "chrome/browser/chromeos/crostini/crostini_util.h"
+#include "chrome/browser/component_updater/cros_component_installer.h"
 #include "chrome/browser/profiles/profile.h"
 #include "components/prefs/pref_service.h"
 #include "content/public/browser/browser_thread.h"
@@ -65,7 +67,20 @@ void CrostiniRemover::DestroyDiskImageFinished(
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   // Only set kCrostiniEnabled to false once cleanup is completely finished.
   if (result == crostini::ConciergeClientResult::SUCCESS) {
-    profile_->GetPrefs()->SetBoolean(crostini::prefs::kCrostiniEnabled, false);
+    crostini::CrostiniManager::GetInstance()->StopVmConcierge(
+        base::BindOnce(&CrostiniRemover::StopConciergeFinished, this));
+  }
+}
+
+void CrostiniRemover::StopConciergeFinished(bool success) {
+  // The success parameter is never set by debugd.
+  auto* cros_component_manager =
+      g_browser_process->platform_part()->cros_component_manager();
+  if (cros_component_manager) {
+    if (cros_component_manager->Unload("cros-termina")) {
+      profile_->GetPrefs()->SetBoolean(crostini::prefs::kCrostiniEnabled,
+                                       false);
+    }
   }
 }
 
