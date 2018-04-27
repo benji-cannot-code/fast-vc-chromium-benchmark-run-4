c2508b1dcda90df160694e52d0b424b63ea29401
Karandeep Bhatia <karandeepb@chromium.org>
2018-04-27 23:29:33 +0000

    Network servicification: Plumb ResourceContext through WebRequestProxyingURLLoaderFactory.
    This CL plumbs the ResourceContext to the WebRequestInfo structure through
    WebRequestProxyingURLLoaderFactory. This is necessary to fix
    NTPInterceptionWebRequestAPITest.NTPRendererRequestsHidden for the network
    servicification path.
    
    BUG=797461, 721414
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I2957c1ece0f8f7b44f15156aeb5e554b3a91ff01
    Reviewed-on: https://chromium-review.googlesource.com/1029549
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554556}

--- extensions/browser/api/web_request/web_request_proxying_url_loader_factory.h
+++ extensions/browser/api/web_request/web_request_proxying_url_loader_factory.h
@@ -26,6 +26,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/mojom/url_loader_factory.mojom.h"
 #include "url/gurl.h"
 
+namespace content {
+class ResourceContext;
+}  // namespace content
+
 namespace extensions {
 
 class ExtensionNavigationUIData;
@@ -132,7 +136,9 @@ class WebRequestProxyingURLLoaderFactory
     DISALLOW_COPY_AND_ASSIGN(InProgressRequest);
   };
 
-  WebRequestProxyingURLLoaderFactory(void* browser_context, InfoMap* info_map);
+  WebRequestProxyingURLLoaderFactory(void* browser_context,
+                                     content::ResourceContext* resource_context,
+                                     InfoMap* info_map);
 
   void StartProxying(
       int render_process_id,
@@ -165,6 +171,7 @@ class WebRequestProxyingURLLoaderFactory
   void RemoveRequest(uint64_t request_id);
 
   void* const browser_context_;
+  content::ResourceContext* const resource_context_;
   int render_process_id_ = -1;
   int render_frame_id_ = -1;
   std::unique_ptr<ExtensionNavigationUIData> navigation_ui_data_;
