df182bc134f72b78977f848723245bdf83444e5e
Takashi Toyoshima <toyoshim@chromium.org>
2018-04-28 09:45:43 +0000

    OOR-CORS: store more information to CORSErrorStatus on preflight errors
    Current implementation does not store rejected method or header
    information to CORSErrorStatus on CORS-preflight checks, and
    results in missing hint parameter on CreateErrorString() in Blink.
    This patch provides required information to generate right error
    messages, and fixes some crashed layout tests.
    
    Bug: 803766, 836741
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: If4de6b916a77d204812a7b010e39080bf059bc9e
    Tbr: kinuko@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/1030670
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554642}

--- third_party/blink/renderer/platform/loader/cors/cors_error_string.h
+++ third_party/blink/renderer/platform/loader/cors/cors_error_string.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_LOADER_CORS_CORS_ERROR_STRING_H_
 
 #include "base/macros.h"
+#include "services/network/public/cpp/cors/cors_error_status.h"
 #include "services/network/public/mojom/cors.mojom-shared.h"
 #include "third_party/blink/public/platform/web_url_request.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
@@ -24,7 +25,7 @@ namespace CORS {
 struct PLATFORM_EXPORT ErrorParameter {
   // Creates an ErrorParameter for generic cases. Use this function if |error|
   // can contain any.
-  static ErrorParameter Create(const network::mojom::CORSError,
+  static ErrorParameter Create(const network::CORSErrorStatus&,
                                const KURL& first_url,
                                const KURL& second_url,
                                const int status_code,
