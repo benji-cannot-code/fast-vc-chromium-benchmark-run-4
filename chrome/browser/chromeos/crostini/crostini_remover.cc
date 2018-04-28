ab1bdf6928ccf7935e545ecb6f3371cb7ad68487
Renee Wright <rjwright@chromium.org>
2018-04-28 10:17:13 +0000

    Add method to clean up registered crostini apps
    And use it to clean up registered apps before uninstalling Crostini.
    
    Change-Id: I4331d3ca30f136f0880e9e886cad12e8fe9802ad
    Reviewed-on: https://chromium-review.googlesource.com/1032672
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Commit-Queue: Renée Wright <rjwright@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554644}

--- chrome/browser/chromeos/crostini/crostini_remover.cc
+++ chrome/browser/chromeos/crostini/crostini_remover.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chromeos/crostini/crostini_pref_names.h"
+#include "chrome/browser/chromeos/crostini/crostini_registry_service.h"
+#include "chrome/browser/chromeos/crostini/crostini_registry_service_factory.h"
 #include "chrome/browser/chromeos/crostini/crostini_util.h"
 #include "chrome/browser/component_updater/cros_component_installer.h"
 #include "chrome/browser/profiles/profile.h"
@@ -54,6 +56,9 @@ void CrostiniRemover::OnRestartCrostini(
 void CrostiniRemover::StopVmFinished(crostini::ConciergeClientResult result) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   if (result == crostini::ConciergeClientResult::SUCCESS) {
+    crostini::CrostiniRegistryServiceFactory::GetForProfile(profile_)
+        ->ClearApplicationList(kCrostiniDefaultVmName,
+                               kCrostiniDefaultContainerName);
     crostini::CrostiniManager::GetInstance()->DestroyDiskImage(
         CryptohomeIdForProfile(profile_),
         base::FilePath(kCrostiniDefaultVmName),
