456753e13ff2c4140ecc1e8dcd1ce0bc7e452798
Alexei Filippov <alph@chromium.org>
2018-04-28 01:01:13 +0000

    DevTools: Do not show screenshots overview bar if there's no screenshots
    BUG=836030
    
    Change-Id: I8c01d3186568ac162cc1be118dd8faf06fcee074
    Reviewed-on: https://chromium-review.googlesource.com/1033327
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Alexei Filippov <alph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554591}

--- third_party/blink/renderer/devtools/front_end/timeline/TimelinePanel.js
+++ third_party/blink/renderer/devtools/front_end/timeline/TimelinePanel.js
@@ -417,7 +417,8 @@ Timeline.TimelinePanel = class extends UI.Panel {
     this._overviewControls.push(new Timeline.TimelineEventOverviewFrames());
     this._overviewControls.push(new Timeline.TimelineEventOverviewCPUActivity());
     this._overviewControls.push(new Timeline.TimelineEventOverviewNetwork());
-    if (this._showScreenshotsSetting.get())
+    if (this._showScreenshotsSetting.get() && this._performanceModel &&
+        this._performanceModel.frameModel().frames().length)
       this._overviewControls.push(new Timeline.TimelineFilmStripOverview());
     if (this._showMemorySetting.get())
       this._overviewControls.push(new Timeline.TimelineEventOverviewMemory());
@@ -569,6 +570,7 @@ Timeline.TimelinePanel = class extends UI.Panel {
     this._performanceModel = model;
     this._flameChart.setModel(model);
 
+    this._updateOverviewControls();
     this._overviewPane.reset();
     if (model) {
       this._performanceModel.addEventListener(
@@ -777,7 +779,7 @@ Timeline.TimelinePanel = class extends UI.Panel {
       case Timeline.TimelineSelection.Type.Range:
         return null;
       case Timeline.TimelineSelection.Type.TraceEvent:
-        return this._performanceModel.frameModel().filteredFrames(selection._endTime, selection._endTime)[0];
+        return this._performanceModel.frameModel().frames(selection._endTime, selection._endTime)[0];
       default:
         console.assert(false, 'Should never be reached');
         return null;
