813a17e6299059e7532e03877a0a8d8a81e78139
Vaclav Brozek <vabr@chromium.org>
2018-04-29 12:37:02 +0000

    Address comments in form parsing
    This CL addresses comments raised on https://crrev.com/c/1013580 after
    it landed:
    
    * Changing name and the declaration comment of a FormFieldData method
      to reflect its effect on textarea fields.
    * Adding some std::vector::reserve calls to avoid extra heap
      allocations when calling push_back in a loop with known maximal
      iteration count.
    
    Bug: 807623
    Change-Id: Ibc9fefbf29676563bbc317e1fd8a0386d245bdd2
    Reviewed-on: https://chromium-review.googlesource.com/1032791
    Commit-Queue: Vaclav Brozek <vabr@chromium.org>
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554670}

--- components/autofill/core/common/form_field_data_unittest.cc
+++ components/autofill/core/common/form_field_data_unittest.cc
@@ -339,7 +339,7 @@ TEST(FormFieldDataTest, DeserializeBadData) {
   EXPECT_TRUE(actual.SameFieldAs(empty));
 }
 
-TEST(FormFieldDataTest, IsTextField) {
+TEST(FormFieldDataTest, IsTextInputElement) {
   struct TestData {
     const char* form_control_type;
     bool expected;
@@ -349,12 +349,13 @@ TEST(FormFieldDataTest, IsTextField) {
       {"email", true},     {"password", true},
       {"select", false},   {"", false},
       {"checkbox", false}, {"random_string", false},
+      {"textarea", false},
   };
 
   for (const auto& test_case : test_data) {
     FormFieldData data;
     data.form_control_type = test_case.form_control_type;
-    EXPECT_EQ(test_case.expected, data.IsTextField());
+    EXPECT_EQ(test_case.expected, data.IsTextInputElement());
   }
 }
 
