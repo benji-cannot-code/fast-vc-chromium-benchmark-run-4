9593a15ebd17c3b20fa377f91196511eaeae57f9
Fredrik Söderquist <fs@opera.com>
2018-04-30 15:44:45 +0000

    Remove (trivially) unneeded ImageResourceContent::IntrinsicSize calls
    Since SVGImage is handled explicitly already, and we pass
    kDoNotRespectImageOrientation to IntrinsicSize() in these functions,
    just call Image::Size() directly rather than passing through the
    ImageResourceContent method.
    
    Bug: 773272
    Change-Id: I6b7c24f5be425f04cd2f68cbc4612054b635bc2b
    Reviewed-on: https://chromium-review.googlesource.com/1032745
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#554763}

--- third_party/blink/renderer/core/style/style_fetched_image.cc
+++ third_party/blink/renderer/core/style/style_fetched_image.cc
@@ -94,7 +94,7 @@ FloatSize StyleFetchedImage::ImageSize(
   // border-image, etc.)
   //
   // https://drafts.csswg.org/css-images-3/#the-image-orientation
-  FloatSize size(image_->IntrinsicSize(kDoNotRespectImageOrientation));
+  FloatSize size(image->Size());
   return ApplyZoom(size, multiplier);
 }
 
