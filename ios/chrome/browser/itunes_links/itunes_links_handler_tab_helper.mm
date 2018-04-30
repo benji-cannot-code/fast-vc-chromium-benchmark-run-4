95f07b8112f852d33152fb19fc7d6d28bdd467b4
mrefaat <mrefaat@chromium.org>
2018-04-30 19:13:29 +0000

    Disable iTunes StoerKit handling for incognito
    Bug: 837670
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I0a45e4717bd2c974408c9247054dea6a0ee13a02
    Reviewed-on: https://chromium-review.googlesource.com/1033028
    Commit-Queue: Mohammad Refaat <mrefaat@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554837}

--- ios/chrome/browser/itunes_links/itunes_links_handler_tab_helper.mm
+++ ios/chrome/browser/itunes_links/itunes_links_handler_tab_helper.mm
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/user_metrics_action.h"
 #include "base/strings/sys_string_conversions.h"
 #import "ios/chrome/browser/store_kit/store_kit_tab_helper.h"
+#include "ios/web/public/browser_state.h"
 #import "ios/web/public/navigation_item.h"
 #import "ios/web/public/navigation_manager.h"
 #import "ios/web/public/web_state/navigation_context.h"
@@ -89,12 +90,17 @@ class ITunesLinksHandlerWebStatePolicyDecider
   bool ShouldAllowResponse(NSURLResponse* response,
                            bool for_main_frame) override {
     // Don't allow rendering responses from URLs that can be handled by
-    // iTunesLinksHandler unless it's on iframe.
-    return !for_main_frame || !CanHandleUrl(net::GURLWithNSURL(response.URL));
+    // iTunesLinksHandler unless it's on iframe or the browsing mode is off the
+    // record.
+    return web_state()->GetBrowserState()->IsOffTheRecord() ||
+           !for_main_frame || !CanHandleUrl(net::GURLWithNSURL(response.URL));
   }
 
   bool ShouldAllowRequest(NSURLRequest* request,
                           ui::PageTransition transition) override {
+    // Only consider blocking the request if it's not of the record mode.
+    if (web_state()->GetBrowserState()->IsOffTheRecord())
+      return true;
     web::NavigationItem* pending_item =
         web_state()->GetNavigationManager()->GetPendingItem();
 
@@ -149,6 +155,10 @@ ITunesLinksHandlerTabHelper::ITunesLinksHandlerTabHelper(
 void ITunesLinksHandlerTabHelper::DidFinishNavigation(
     web::WebState* web_state,
     web::NavigationContext* navigation_context) {
+  // Don't handle iTunse URL in off the record mode.
+  if (web_state->GetBrowserState()->IsOffTheRecord())
+    return;
+
   GURL url = navigation_context->GetUrl();
   // Whenever a navigation to iTunes product url is finished, launch StoreKit.
   if (ITunesLinksHandlerWebStatePolicyDecider::CanHandleUrl(url)) {
