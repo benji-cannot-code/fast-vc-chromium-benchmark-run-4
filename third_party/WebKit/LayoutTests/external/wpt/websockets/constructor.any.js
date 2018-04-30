a134f66f5da16be7fca183897743f9b7ab04e1c4
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-04-30 14:47:22 +0000

    Import wpt@be39b58a90d40029baf928bbd78e450ffa8a26c3
    Using wpt-import in Chromium 50cce18b90b688e08c7d84c64d1e11abc2521231.
    With Chromium commits locally applied on WPT:
    303956b6ed "Enable WPT tests for the Generic Sensor classes"
    
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/16549
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    foolip@chromium.org, robertma@chromium.org, smcgruer@chromium.org:
      external/wpt/infrastructure
    
    TBR=lukebjerring
    
    No-Export: true
    Change-Id: I16eb56232d372474f53c53c9f92b0c60f1228d23
    Reviewed-on: https://chromium-review.googlesource.com/1034596
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554748}

--- third_party/WebKit/LayoutTests/external/wpt/websockets/constructor.any.js
+++ third_party/WebKit/LayoutTests/external/wpt/websockets/constructor.any.js
@@ -3,6 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 test(function() {
   var ws = new WebSocket("ws://" + __SERVER__NAME + ":" + __PORT + "/" + __PATH,
-                         "echo", "Stray argument")
+    "echo", "Stray argument")
   assert_true(ws instanceof WebSocket, "Expected a WebSocket instance.")
 }, "Calling the WebSocket constructor with too many arguments should not throw.")
--- third_party/WebKit/LayoutTests/external/wpt/websockets/eventhandlers.any.js
+++ third_party/WebKit/LayoutTests/external/wpt/websockets/eventhandlers.any.js
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 function testEventHandler(name) {
   test(function() {
     var ws = new WebSocket("ws://" + __SERVER__NAME + ":" + __PORT + "/" + __PATH,
-                           "echo")
+      "echo")
     assert_equals(ws["on" + name], null);
     ws["on" + name] = function() {};
     ws["on" + name] = 2;
