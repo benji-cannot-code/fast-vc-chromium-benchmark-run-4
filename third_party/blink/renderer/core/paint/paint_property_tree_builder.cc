0cf50afe5d92aebbe3d6a4e4c270357c010489c5
Philip Rogers <pdr@chromium.org>
2018-04-30 22:25:19 +0000

    [BlinkGenPropertyTrees] Add skeleton for layout test infrastructure
    Adds a skeleton of basic layout test infrastructure. A virtual test
    suite has been added just to have some basic coverage on the commit
    queue. Flag expectations should be used for all other tests. An
    initial set of failing tests have been added.
    
    The primary way to run tests should be:
    run-webkit-tests --additional-driver-flag=--enable-blink-gen-property-trees
    
    Bug: 836902
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Ie440e26e545bdaa30a1a87ef407dbea1eca59920
    Reviewed-on: https://chromium-review.googlesource.com/1035534
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554900}

--- third_party/blink/renderer/core/paint/paint_property_tree_builder.cc
+++ third_party/blink/renderer/core/paint/paint_property_tree_builder.cc
@@ -141,7 +141,8 @@ static bool UpdateScroll(LocalFrameView& frame_view,
       context.current.scroll->GetMainThreadScrollingReasons();
   state.main_thread_scrolling_reasons =
       GetMainThreadScrollingReasons(frame_view, ancestor_reasons);
-  if (RuntimeEnabledFeatures::SlimmingPaintV2Enabled())
+  if (RuntimeEnabledFeatures::SlimmingPaintV2Enabled() ||
+      RuntimeEnabledFeatures::BlinkGenPropertyTreesEnabled())
     state.compositor_element_id = frame_view.GetCompositorElementId();
 
   if (auto* existing_scroll = frame_view.ScrollNode()) {
@@ -535,7 +536,8 @@ void FragmentPaintPropertyTreeBuilder::UpdatePaintOffsetTranslation(
                            paint_offset_translation->Y());
     state.flattens_inherited_transform =
         context_.current.should_flatten_inherited_transform;
-    if (RuntimeEnabledFeatures::SlimmingPaintV2Enabled())
+    if (RuntimeEnabledFeatures::SlimmingPaintV2Enabled() ||
+        RuntimeEnabledFeatures::BlinkGenPropertyTreesEnabled())
       state.rendering_context_id = context_.current.rendering_context_id;
     OnUpdate(properties_->UpdatePaintOffsetTranslation(
         context_.current.transform, std::move(state)));
@@ -664,7 +666,8 @@ void FragmentPaintPropertyTreeBuilder::UpdateTransform() {
             ComputedStyle::kIncludeMotionPath,
             ComputedStyle::kIncludeIndependentTransformProperties);
 
-        if (RuntimeEnabledFeatures::SlimmingPaintV2Enabled()) {
+        if (RuntimeEnabledFeatures::SlimmingPaintV2Enabled() ||
+            RuntimeEnabledFeatures::BlinkGenPropertyTreesEnabled()) {
           // TODO(trchen): transform-style should only be respected if a
           // PaintLayer is created. If a node with transform-style: preserve-3d
           // does not exist in an existing rendering context, it establishes a
@@ -682,7 +685,8 @@ void FragmentPaintPropertyTreeBuilder::UpdateTransform() {
       state.flattens_inherited_transform =
           context_.current.should_flatten_inherited_transform;
 
-      if (RuntimeEnabledFeatures::SlimmingPaintV2Enabled()) {
+      if (RuntimeEnabledFeatures::SlimmingPaintV2Enabled() ||
+          RuntimeEnabledFeatures::BlinkGenPropertyTreesEnabled()) {
         state.backface_visibility =
             object_.HasHiddenBackface()
                 ? TransformPaintPropertyNode::BackfaceVisibility::kHidden
@@ -836,7 +840,8 @@ void FragmentPaintPropertyTreeBuilder::UpdateEffect() {
         state.blend_mode = WebCoreCompositeToSkiaComposite(kCompositeSourceOver,
                                                            style.BlendMode());
       }
-      if (RuntimeEnabledFeatures::SlimmingPaintV2Enabled()) {
+      if (RuntimeEnabledFeatures::SlimmingPaintV2Enabled() ||
+          RuntimeEnabledFeatures::BlinkGenPropertyTreesEnabled()) {
         // We may begin to composite our subtree prior to an animation starts,
         // but a compositor element ID is only needed when an animation is
         // current.
@@ -857,7 +862,8 @@ void FragmentPaintPropertyTreeBuilder::UpdateEffect() {
         mask_state.output_clip = output_clip;
         mask_state.color_filter = CSSMaskPainter::MaskColorFilter(object_);
         mask_state.blend_mode = SkBlendMode::kDstIn;
-        if (RuntimeEnabledFeatures::SlimmingPaintV2Enabled()) {
+        if (RuntimeEnabledFeatures::SlimmingPaintV2Enabled() ||
+            RuntimeEnabledFeatures::BlinkGenPropertyTreesEnabled()) {
           mask_state.compositor_element_id =
               CompositorElementIdFromUniqueObjectId(
                   object_.UniqueId(),
@@ -877,7 +883,8 @@ void FragmentPaintPropertyTreeBuilder::UpdateEffect() {
         clip_path_state.local_transform_space = context_.current.transform;
         clip_path_state.output_clip = output_clip;
         clip_path_state.blend_mode = SkBlendMode::kDstIn;
-        if (RuntimeEnabledFeatures::SlimmingPaintV2Enabled()) {
+        if (RuntimeEnabledFeatures::SlimmingPaintV2Enabled() ||
+            RuntimeEnabledFeatures::BlinkGenPropertyTreesEnabled()) {
           clip_path_state.compositor_element_id =
               CompositorElementIdFromUniqueObjectId(
                   object_.UniqueId(),
@@ -952,7 +959,8 @@ void FragmentPaintPropertyTreeBuilder::UpdateFilter() {
       // output pixel may depend on an input pixel outside of the output clip.
       // We should generate a special clip node to represent this expansion.
 
-      if (RuntimeEnabledFeatures::SlimmingPaintV2Enabled()) {
+      if (RuntimeEnabledFeatures::SlimmingPaintV2Enabled() ||
+          RuntimeEnabledFeatures::BlinkGenPropertyTreesEnabled()) {
         // We may begin to composite our subtree prior to an animation starts,
         // but a compositor element ID is only needed when an animation is
         // current.
@@ -1319,7 +1327,8 @@ void FragmentPaintPropertyTreeBuilder::UpdatePerspective() {
                      ToLayoutSize(context_.current.paint_offset);
       state.flattens_inherited_transform =
           context_.current.should_flatten_inherited_transform;
-      if (RuntimeEnabledFeatures::SlimmingPaintV2Enabled())
+      if (RuntimeEnabledFeatures::SlimmingPaintV2Enabled() ||
+          RuntimeEnabledFeatures::BlinkGenPropertyTreesEnabled())
         state.rendering_context_id = context_.current.rendering_context_id;
       OnUpdate(properties_->UpdatePerspective(context_.current.transform,
                                               std::move(state)));
@@ -1419,7 +1428,8 @@ void FragmentPaintPropertyTreeBuilder::UpdateScrollAndScrollTranslation() {
           full_context_.force_subtree_update = true;
       }
 
-      if (RuntimeEnabledFeatures::SlimmingPaintV2Enabled())
+      if (RuntimeEnabledFeatures::SlimmingPaintV2Enabled() ||
+          RuntimeEnabledFeatures::BlinkGenPropertyTreesEnabled())
         state.compositor_element_id = scrollable_area->GetCompositorElementId();
 
       OnUpdate(
@@ -1437,7 +1447,8 @@ void FragmentPaintPropertyTreeBuilder::UpdateScrollAndScrollTranslation() {
       state.matrix.Translate(-scroll_offset.Width(), -scroll_offset.Height());
       state.flattens_inherited_transform =
           context_.current.should_flatten_inherited_transform;
-      if (RuntimeEnabledFeatures::SlimmingPaintV2Enabled()) {
+      if (RuntimeEnabledFeatures::SlimmingPaintV2Enabled() ||
+          RuntimeEnabledFeatures::BlinkGenPropertyTreesEnabled()) {
         state.direct_compositing_reasons = CompositingReasonsForScroll(box);
         state.rendering_context_id = context_.current.rendering_context_id;
       }
