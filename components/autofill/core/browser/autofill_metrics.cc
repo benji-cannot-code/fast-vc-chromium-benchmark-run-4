600ea3484b1a27ec0d3d30f487439fd0b1e89534
Parastoo Geranmayeh <parastoog@google.com>
2018-04-30 20:48:21 +0000

    [Autofill] UMA for Counting Reflected Synthetic Fields.
    Logs hidden or presentational 'select' fields that were
    autofilled to support reflected synthetic fields.
    
    
    Bug: 836949
    Change-Id: I5dfd0d21f96d7f95ede8632fcd092991869e7cb9
    Reviewed-on: https://chromium-review.googlesource.com/1033325
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Commit-Queue: Parastoo Geranmayeh <parastoog@google.com>
    Cr-Commit-Position: refs/heads/master@{#554867}

--- components/autofill/core/browser/autofill_metrics.cc
+++ components/autofill/core/browser/autofill_metrics.cc
@@ -1151,6 +1151,12 @@ void AutofillMetrics::LogNumberOfCreditCardsDeletedForDisuse(size_t num_cards) {
 }
 
 // static
+void AutofillMetrics::LogHiddenOrPresentationalSelectFieldsFilled() {
+  UMA_HISTOGRAM_BOOLEAN("Autofill.HiddenOrPresentationalSelectFieldsFilled",
+                        true);
+}
+
+// static
 void AutofillMetrics::LogNumberOfProfilesAtAutofillableFormSubmission(
     size_t num_profiles) {
   UMA_HISTOGRAM_COUNTS(
