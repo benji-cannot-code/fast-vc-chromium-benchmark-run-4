71ec8e8e6aad521a2e75491c1dcf040d932e6b9b
Michael Giuffrida <michaelpg@chromium.org>
2018-04-30 04:37:14 +0000

    Separate TabMetrics data from logging
    TabMetricsLogger logs the TabMetrics UKM event for a background tab.
    This event includes the state of the tab, as well as some slightly
    tricky or nullable fields.
    
    Add a new TabFeatures struct that contains all this information, and
    generate the struct in a separate function. The UKM logging function
    now simply forwards info from the TabFeatures struct to the UKM event.
    
    Later we will use the same TabFeatures to calculate tab scores with an
    ML model. Encapsulating the logic for these features in a single
    function ensures that we use the same logic for logging as we will for
    inference.
    
    Bug: 784639, 783989
    Change-Id: I11e64119ce30465f98409f54f7e97af0251ce4b3
    Reviewed-on: https://chromium-review.googlesource.com/1028562
    Commit-Queue: Michael Giuffrida <michaelpg@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554694}

---
+++ chrome/browser/resource_coordinator/tab_features.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/resource_coordinator/tab_features.h"
+
+namespace resource_coordinator {
+
+TabFeatures::TabFeatures() = default;
+
+TabFeatures::~TabFeatures() = default;
+
+TabFeatures::TabFeatures(const TabFeatures& other) = default;
+
+}  // namespace resource_coordinator
