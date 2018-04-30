7821c5e9ffd1fdc80d71c9690457fdcce3de2dd5
Jay Civelli <jcivelli@google.com>
2018-04-30 22:39:55 +0000

    Add is_main_frame to ResourceLoadInfo
    This CL adds an is_main_frame member to the
    content.mojom.ResourceLoadInfo. It is needed to switch the preconnect
    predictor to using the network service.
    
    Bug: 816684
    Change-Id: Id053130c3269923d7465ec6fc11579011b3910b5
    Reviewed-on: https://chromium-review.googlesource.com/1033730
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Jay Civelli <jcivelli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554905}

--- content/browser/frame_host/render_frame_host_impl.cc
+++ content/browser/frame_host/render_frame_host_impl.cc
@@ -3232,7 +3232,7 @@ void RenderFrameHostImpl::SubresourceResponseStarted(
 
 void RenderFrameHostImpl::ResourceLoadComplete(
     mojom::ResourceLoadInfoPtr resource_load_info) {
-  delegate_->ResourceLoadComplete(std::move(resource_load_info));
+  delegate_->ResourceLoadComplete(this, std::move(resource_load_info));
 }
 
 namespace {
