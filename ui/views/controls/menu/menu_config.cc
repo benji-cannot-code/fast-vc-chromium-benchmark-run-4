e7661079f33ed12c2c84bd3f7ecc6ae5628df77f
Elly Fong-Jones <ellyjones@chromium.org>
2018-05-01 15:30:55 +0000

    macviews: reduce combobox menu corner radius to 4pt
    It looks much better.
    
    Bug: 837780
    Change-Id: Ia68a34dae0feb4eb4de3c1813094d2c9ac2f55d3
    Reviewed-on: https://chromium-review.googlesource.com/1035023
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555043}

--- ui/views/controls/menu/menu_config.cc
+++ ui/views/controls/menu/menu_config.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/menu/menu_config.h"
 
 #include "base/macros.h"
+#include "ui/views/controls/menu/menu_controller.h"
 #include "ui/views/controls/menu/menu_image_util.h"
 #include "ui/views/round_rect_painter.h"
 namespace views {
@@ -51,6 +52,7 @@ MenuConfig::MenuConfig()
       check_selected_combobox_item(false),
       show_delay(400),
       corner_radius(0),
+      combobox_corner_radius(0),
       touchable_corner_radius(8),
       touchable_anchor_offset(8),
       touchable_menu_height(36),
@@ -61,6 +63,14 @@ MenuConfig::MenuConfig()
 }
 
 MenuConfig::~MenuConfig() {}
+
+int MenuConfig::CornerRadiusForMenu(const MenuController* controller) const {
+  if (controller && controller->use_touchable_layout())
+    return touchable_corner_radius;
+  if (controller && controller->is_combobox())
+    return combobox_corner_radius;
+  return corner_radius;
+}
 
 // static
 const MenuConfig& MenuConfig::instance() {
