5fc54aed98d7cd50104418452bf0e8c91b90a3e3
Jinglong Zuo <zuojinglong@xiaomi.com>
2018-05-01 17:47:19 +0000

    Don't show text selection popup menu if current selected text is cleared.
    If text selection clear action is just after selecting all text action in editable input box,
    then the state of text selection in renderer is not correct, which causes the selection popup
    menu will be incorrectly shown.
    Fix this issue by checking selection state before popup text selection menu.
    
    Bug: 817712
    
    Change-Id: I4dcda47d5f0fe5f20febc2e1def30574c6060724
    Reviewed-on: https://chromium-review.googlesource.com/940290
    Commit-Queue: Antoine Labour <piman@chromium.org>
    Reviewed-by: Changwan Ryu <changwan@chromium.org>
    Reviewed-by: Pedro Amaral <amaralp@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555099}

--- content/renderer/render_frame_impl.cc
+++ content/renderer/render_frame_impl.cc
@@ -4588,6 +4588,10 @@ void RenderFrameImpl::ShowContextMenu(const blink::WebContextMenuData& data) {
 }
 
 void RenderFrameImpl::ShowDeferredContextMenu(const ContextMenuParams& params) {
+  // TODO (amaralp): Remove this once selection menu race conditions are fixed.
+  if (selection_text_.empty() && !params.selection_text.empty())
+    return;
+
   Send(new FrameHostMsg_ContextMenu(routing_id_, params));
 }
 
