c691dc302e353bfe61293d9d2cac6fd5f4a6b224
Manu Cornet <manucornet@google.com>
2018-05-01 22:08:23 +0000

    Add a flag for shelf hover previews
    Change-Id: If84fcb785bdc4e95e5b218e4634912ab6a3d132c
    Reviewed-on: https://chromium-review.googlesource.com/1037779
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Manu Cornet <manucornet@google.com>
    Cr-Commit-Position: refs/heads/master@{#555197}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -862,6 +862,9 @@ extern const char kServiceWorkerScriptFullCodeCacheDescription[];
 extern const char kSettingsWindowName[];
 extern const char kSettingsWindowDescription[];
 
+extern const char kShelfHoverPreviewsName[];
+extern const char kShelfHoverPreviewsDescription[];
+
 extern const char kShowAllDialogsWithViewsToolkitName[];
 extern const char kShowAllDialogsWithViewsToolkitDescription[];
 
