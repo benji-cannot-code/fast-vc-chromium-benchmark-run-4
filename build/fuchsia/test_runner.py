055ca6dc2e39ae81bfc9968b95c2df9a5f08fb97
Kevin Marshall <kmarshall@chromium.org>
2018-05-01 18:59:19 +0000

    Fuchsia: Minor comment cleanup pass, scrub references to "bootfs".
    The revised comments more accurately reflect the current runner
    behavior.
    
    Bug: 808057
    Change-Id: I4b193241a26034d608f3e1281eea7f4ce21dd510
    Reviewed-on: https://chromium-review.googlesource.com/1037780
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Reviewed-by: Scott Graham <scottmg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555130}

--- build/fuchsia/test_runner.py
+++ build/fuchsia/test_runner.py
@@ -5,9 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""Packages a user.bootfs for a Fuchsia boot image, pulling in the runtime
-dependencies of a test binary, and then uses either QEMU from the Fuchsia SDK
-to run, or starts the bootserver to allow running on a hardware device."""
+"""Deploys and runs a test package on a Fuchsia target."""
 
 import argparse
 import json
