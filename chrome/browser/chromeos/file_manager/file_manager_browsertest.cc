684d6f54a1c30e16005c1a997fa32e66c944cb0b
Noel Gordon <noel@chromium.org>
2018-05-01 08:09:49 +0000

    FileMangerBrowserTests launch the test extension
    InstallExtension (install has other connotations): rename this helper
    to LaunchExtension() since that's what is does. Similarly, change the
    virtual helper name to GetTestExtensionManifestName().
    
    Bug: 833834
    Change-Id: Ib41febb6b2259a68ae704fd7005c2745c1a9c440
    Reviewed-on: https://chromium-review.googlesource.com/1034616
    Reviewed-by: Stuart Langley <slangley@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555016}

--- chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
@@ -45,7 +45,7 @@ class FileManagerBrowserTest :
     return std::get<1>(GetParam());
   }
 
-  const char* GetTestManifestName() const override {
+  const char* GetTestExtensionManifestName() const override {
     return "file_manager_test_manifest.json";
   }
 
@@ -640,15 +640,17 @@ class MultiProfileFileManagerBrowserTest : public FileManagerBrowserTestBase {
                                                  "refresh_token");
   }
 
- private:
   GuestMode GetGuestModeParam() const override { return NOT_IN_GUEST_MODE; }
-  const char* GetTestManifestName() const override {
-    return "file_manager_test_manifest.json";
-  }
+
   const char* GetTestCaseNameParam() const override {
     return test_case_name_.c_str();
   }
 
+  const char* GetTestExtensionManifestName() const override {
+    return "file_manager_test_manifest.json";
+  }
+
+ private:
   std::string test_case_name_;
 };
 
