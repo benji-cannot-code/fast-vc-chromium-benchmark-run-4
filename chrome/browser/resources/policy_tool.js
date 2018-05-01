db8cd5cf0c783feffdc4e23003305d8fed3ec71c
rodmartin <rodmartin@google.com>
2018-05-01 19:56:02 +0000

    Added check and try to parse the types of the values in the dictionary of policies.
    Initially all the value type of each policy obteined from the UI was a string type.
    To be consistent with the actual policy types, before process the dictionary of
    policies got it from the UI, I check the value type and try to parse it if it's not
    valid. The status of each policy will be updated to |OK| if its value type is correct
    or |Invalid| otherwise.
    
    Bug: 
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I4d1eaab0768e65080bd26d45511a9dc67c85890b
    Reviewed-on: https://chromium-review.googlesource.com/1013207
    Commit-Queue: Martin Rodriguez <rodmartin@google.com>
    Reviewed-by: Dave Schuyler <dschuyler@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555155}

--- chrome/browser/resources/policy_tool.js
+++ chrome/browser/resources/policy_tool.js
@@ -244,6 +244,8 @@ policy.Policy.prototype.setStatus_ = function(value) {
     status = loadTimeData.getString('unset');
   } else if (value.error) {
     status = value.error;
+  } else if (!value.valid) {
+    status = loadTimeData.getString('errorInvalidType');
   } else {
     status = loadTimeData.getString('ok');
   }
@@ -257,7 +259,7 @@ policy.Policy.prototype.setStatus_ = function(value) {
  */
 policy.Policy.prototype.setValue_ = function(value) {
   this.value = value;
-  if (!value) {
+  if (value === undefined) {
     value = '';
   } else if (typeof value != 'string') {
     value = JSON.stringify(value);
