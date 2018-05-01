f7e79a62a0e65beee5802cc112285364b5e372ec
Siddhartha <ssid@chromium.org>
2018-05-01 22:57:09 +0000

    Fix ElfReaderTest.ReadElfLibraryName on android component builds
    Android component builds have .cr.so in library names.
    Make the test for library name less strict by checking only for
    substring match.
    
    BUG=838544
    
    Change-Id: If7958fdc70e8429e93aff098fcb8baca2ff6c456
    Reviewed-on: https://chromium-review.googlesource.com/1038228
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555212}

--- base/debug/elf_reader_linux_unittest.cc
+++ base/debug/elf_reader_linux_unittest.cc
@@ -36,7 +36,7 @@ TEST(ElfReaderTest, ReadElfBuildId) {
 TEST(ElfReaderTest, ReadElfLibraryName) {
 #if defined(OS_ANDROID)
   // On Android the library loader memory maps the full so file.
-  const char kLibraryName[] = "lib_base_unittests__library.so";
+  const char kLibraryName[] = "lib_base_unittests__library";
   const void* addr = &__executable_start;
 #else
   // On Linux the executable does not contain soname and is not mapped till
@@ -62,7 +62,9 @@ TEST(ElfReaderTest, ReadElfLibraryName) {
 
   auto name = ReadElfLibraryName(addr);
   ASSERT_TRUE(name);
-  EXPECT_EQ(kLibraryName, *name);
+  EXPECT_NE(std::string::npos, name->find(kLibraryName))
+      << "Library name " << *name << " doesn't contain expected "
+      << kLibraryName;
 }
 
 }  // namespace debug
