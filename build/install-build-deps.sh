0dff04e8105506a100fa2e9deea9688b3c3bcbf3
Tom Anderson <thomasanderson@chromium.org>
2018-05-01 00:02:43 +0000

    Remove font packgaes from install-build-deps.sh
    There's no longer a dependency on any system fonts when running Linux tests, so
    font packages may be removed.  The CrOs fonts are kept because, presumably,
    folks might still want them when testing Linux CrOs builds, though the CrOs
    tests also no longer depend on any system fonts.
    
    BUG=787020
    R=dpranke
    
    Change-Id: I6074842eb67eba6194fa90e549e0485b529aa814
    Reviewed-on: https://chromium-review.googlesource.com/1036585
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554930}

--- build/install-build-deps.sh
+++ build/install-build-deps.sh
@@ -161,7 +161,6 @@ dev_list="\
   devscripts
   fakeroot
   flex
-  fonts-ipafont
   g++
   git-core
   git-svn
@@ -506,9 +505,6 @@ if package_exists apache2.2-bin; then
 else
   dev_list="${dev_list} apache2-bin"
 fi
-if package_exists xfonts-mathml; then
-  dev_list="${dev_list} xfonts-mathml"
-fi
 if package_exists libav-tools; then
   dev_list="${dev_list} libav-tools"
 fi
@@ -520,14 +516,6 @@ elif package_exists php7.0-cgi; then
   dev_list="${dev_list} php7.0-cgi libapache2-mod-php7.0"
 else
   dev_list="${dev_list} php5-cgi libapache2-mod-php5"
-fi
-# ttf-mscorefonts-installer is in the Debian contrib repo, which has
-# dependencies on non-free software.  Install it only if the user has already
-# enabled contrib.
-if package_exists ttf-mscorefonts-installer; then
-  dev_list="${dev_list} ttf-mscorefonts-installer"
-elif package_exists msttcorefonts; then
-  dev_list="${dev_list} msttcorefonts"
 fi
 
 # Some packages are only needed if the distribution actually supports
