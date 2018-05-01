15848dd85f98974f58238c1b80016fc37488b350
Matt Reynolds <mattreynolds@google.com>
2018-05-01 22:56:58 +0000

    Add a GamepadVibration runtime feature flag
    In previous versions, the vibration feature was enabled with the
    GamepadExtensions runtime feature flag. This CL splits the vibration
    functionality into a separate flag that is enabled by default. This
    allows the vibration feature to be enabled independently of other
    features that use the same runtime flag.
    
    BUG=749295
    
    Change-Id: Ia8017e8b0ecd9df4490e8b205ecd1f49509c687b
    Reviewed-on: https://chromium-review.googlesource.com/1027996
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555211}

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -305,6 +305,9 @@ void SetRuntimeFeaturesDefaultsAndUpdateFromArgs(
   if (base::FeatureList::IsEnabled(features::kGamepadExtensions))
     WebRuntimeFeatures::EnableGamepadExtensions(true);
 
+  if (base::FeatureList::IsEnabled(features::kGamepadVibration))
+    WebRuntimeFeatures::EnableGamepadVibration(true);
+
   if (base::FeatureList::IsEnabled(features::kCompositeOpaqueFixedPosition))
     WebRuntimeFeatures::EnableFeatureFromString("CompositeOpaqueFixedPosition",
                                                 true);
