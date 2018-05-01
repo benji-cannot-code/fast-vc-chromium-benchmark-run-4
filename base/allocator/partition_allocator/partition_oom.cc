0ddc4db5d319b1c7970c8deae56522a67e91e640
Albert J. Wong <ajwong@chromium.org>
2018-05-01 18:29:21 +0000

    Shatter partition_alloc.h into smaller files.
    partition_alloc.h has been a dumping ground for all objects related to
    PartitonAlloc. The original code used many inlined top-level functions
    making the implementation very coupled. This splits out most of the
    critical clusters of functionality into separate files with roughly
    one struct/class per file.
    
    The split makes the module layering and interaction boundaries much clearer.
    Because of the heavy use of inlining, and the highly coupled nature of the
    original code, it was not possible to make a clean split yet between
    declaration and implementation. Therefore, this splitting required introducing
    "-inl.h" files because too many of these objects reached direclty into each
    others's fields. These "-inl.h" files can likely be reduced in future
    refactors.
    
    Bug: 766882
    Change-Id: Iceeccc64a1d810a68cedfb28599dfbcf66642b98
    Reviewed-on: https://chromium-review.googlesource.com/1006154
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Chris Palmer <palmer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555118}

---
+++ base/allocator/partition_allocator/partition_oom.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/allocator/partition_allocator/partition_oom.h"
+
+#include "base/allocator/partition_allocator/oom.h"
+#include "build/build_config.h"
+
+namespace base {
+namespace internal {
+
+void NOINLINE PartitionExcessiveAllocationSize() {
+  OOM_CRASH();
+}
+
+#if !defined(ARCH_CPU_64_BITS)
+NOINLINE void PartitionOutOfMemoryWithLotsOfUncommitedPages() {
+  OOM_CRASH();
+}
+#endif
+
+}  // namespace internal
+}  // namespace base
