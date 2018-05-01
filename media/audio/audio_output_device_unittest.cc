7ba3c1f68db997c787213be11f027efc3189300d
Kevin Marshall <kmarshall@chromium.org>
2018-05-01 18:58:15 +0000

    Raise time limits for AudioSyncReader::Read() for test on Fuchsia.
    Fuchsia, running on the test bots, was sometimes taking too long to
    return data to AudioSyncReader (observed 21ms, max 20ms), which
    lead to test flakes.
    
    
    Bug: 838367
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: If2e25357acd70eaa28c6e618e696cc39ea9ba372
    Reviewed-on: https://chromium-review.googlesource.com/1036196
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Reviewed-by: Max Morin <maxmorin@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555128}

--- media/audio/audio_output_device_unittest.cc
+++ media/audio/audio_output_device_unittest.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task_runner.h"
 #include "base/test/scoped_task_environment.h"
 #include "base/threading/thread_task_runner_handle.h"
+#include "build/build_config.h"
 #include "media/audio/audio_sync_reader.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -343,6 +344,15 @@ struct TestEnvironment {
         /*log callback*/ base::DoNothing(), params, std::move(shared_memory),
         std::move(browser_socket));
     time_stamp = base::TimeTicks::Now();
+
+#if defined(OS_FUCHSIA)
+    // Raise the timeout limits to reduce bot flakiness.
+    // Fuchsia's task scheduler suffers from bad jitter on systems running many
+    // tests simultaneously on nested virtualized deployments (e.g. test bots),
+    // leading some read operations to randomly timeout.
+    reader->set_max_wait_timeout_for_test(
+        base::TimeDelta::FromMilliseconds(50));
+#endif
   }
 
   base::CancelableSyncSocket renderer_socket;
