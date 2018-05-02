f6047e00b08ae55ebc997460cb5f431b0bc129db
Nate Fischer <ntfschr@chromium.org>
2018-05-02 19:11:37 +0000

    AW: add a systrace for support library startup
    No change to logic, this only adds a systrace event to log startup
    performance in the support library. This doesn't appear to be an issue
    in local benchmarks, but the bot may perform differently.
    
    Bug: 838702
    Test: N/A
    Change-Id: Idde301b3e620e7ca13e779ac35f08338c58910df
    Reviewed-on: https://chromium-review.googlesource.com/1039154
    Reviewed-by: Changwan Ryu <changwan@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555498}

--- android_webview/support_library/callback/java/src/org/chromium/support_lib_callback_glue/SupportLibWebViewContentsClientAdapter.java
+++ android_webview/support_library/callback/java/src/org/chromium/support_lib_callback_glue/SupportLibWebViewContentsClientAdapter.java
@@ -13,6 +13,7 @@ import android.webkit.WebViewClient;
 
 import org.chromium.android_webview.AwContentsClient.AwWebResourceError;
 import org.chromium.android_webview.AwSafeBrowsingResponse;
+import org.chromium.android_webview.ScopedSysTraceEvent;
 import org.chromium.base.Callback;
 import org.chromium.support_lib_boundary.SafeBrowsingResponseBoundaryInterface;
 import org.chromium.support_lib_boundary.WebResourceErrorBoundaryInterface;
@@ -41,9 +42,13 @@ public class SupportLibWebViewContentsClientAdapter {
     }
 
     public void setWebViewClient(WebViewClient possiblyCompatClient) {
-        mWebViewClient = convertCompatClient(possiblyCompatClient);
-        mWebViewClientSupportedFeatures =
-                mWebViewClient == null ? EMPTY_FEATURE_LIST : mWebViewClient.getSupportedFeatures();
+        try (ScopedSysTraceEvent event = ScopedSysTraceEvent.scoped(
+                     "SupportLibWebViewContentsClientAdapter.setWebViewClient")) {
+            mWebViewClient = convertCompatClient(possiblyCompatClient);
+            mWebViewClientSupportedFeatures = mWebViewClient == null
+                    ? EMPTY_FEATURE_LIST
+                    : mWebViewClient.getSupportedFeatures();
+        }
     }
 
     @Nullable
