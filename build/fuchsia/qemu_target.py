8d023e5f8fcd58be5308ab4d1348f58f53fee825
Kevin Marshall <kmarshall@chromium.org>
2018-05-02 21:13:10 +0000

    Reland "Fuchsia: Explicitly check that all file dependencies exist."
    This is a reland of 7891d057259d6df8483de1f03a39a6c202cca241
    
    The underlying issue in crbug.com/838703 was fixed and
    "lint_test_expectations.py" Works On My Box(TM).
    
    TBR=scottmg@chromium.org
    
    Original change's description:
    > Fuchsia: Explicitly check that all file dependencies exist.
    >
    > Wrap file path references with "EnsureFileExists()" which acts as a
    > simple identity function, except for invalid paths for which it
    > raises IOErrors.
    >
    > Bug: 837200
    > Change-Id: Ie718cd5517415b375ed3bf5d5112d59b32074c04
    > Reviewed-on: https://chromium-review.googlesource.com/1037778
    > Reviewed-by: Scott Graham <scottmg@chromium.org>
    > Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#555137}
    
    Bug: 837200
    Change-Id: I21713adc9e8d9fcaab06957a25255de3ee6e596e
    Reviewed-on: https://chromium-review.googlesource.com/1038963
    Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555534}

--- build/fuchsia/qemu_target.py
+++ build/fuchsia/qemu_target.py
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 """Implements commands for running and interacting with Fuchsia on QEMU."""
 
 import boot_data
-import common
 import logging
 import target
 import os
@@ -14,6 +13,8 @@ import platform
 import socket
 import subprocess
 import time
+
+from common import SDK_ROOT, EnsurePathExists
 
 
 # Virtual networking configuration data for QEMU.
@@ -52,9 +53,8 @@ class QemuTarget(target.Target):
       self.Shutdown()
 
   def Start(self):
-    qemu_path = os.path.join(
-        common.SDK_ROOT, 'qemu', 'bin',
-        'qemu-system-' + self._GetTargetSdkArch())
+    qemu_path = os.path.join(SDK_ROOT, 'qemu', 'bin',
+                             'qemu-system-' + self._GetTargetSdkArch())
     kernel_args = boot_data.GetKernelArgs(self._output_dir)
 
     # TERM=dumb tells the guest OS to not emit ANSI commands that trigger
@@ -68,20 +68,24 @@ class QemuTarget(target.Target):
     qemu_command = [qemu_path,
         '-m', str(self._ram_size_mb),
         '-nographic',
-        '-kernel', boot_data.GetTargetFile(self._GetTargetSdkArch(),
-                                           'zircon.bin'),
-        '-initrd', boot_data.GetTargetFile(self._GetTargetSdkArch(),
-                                           'bootdata-blob.bin'),
+        '-kernel', EnsurePathExists(
+            boot_data.GetTargetFile(self._GetTargetSdkArch(),
+                                    'zircon.bin')),
+        '-initrd', EnsurePathExists(
+            boot_data.GetTargetFile(self._GetTargetSdkArch(),
+                                    'bootdata-blob.bin')),
         '-smp', '4',
 
         # Attach the blobstore and data volumes. Use snapshot mode to discard
         # any changes.
         '-snapshot',
         '-drive', 'file=%s,format=qcow2,if=none,id=data,snapshot=on' %
-            os.path.join(self._output_dir, 'fvm.blk.qcow2'),
+                    EnsurePathExists(os.path.join(self._output_dir,
+                                                  'fvm.blk.qcow2')),
         '-drive', 'file=%s,format=qcow2,if=none,id=blobstore,snapshot=on' %
-            boot_data.ConfigureDataFVM(self._output_dir,
-                                       boot_data.FVM_TYPE_QCOW),
+            EnsurePathExists(
+                boot_data.ConfigureDataFVM(self._output_dir,
+                                           boot_data.FVM_TYPE_QCOW)),
         '-device', 'virtio-blk-pci,drive=data',
         '-device', 'virtio-blk-pci,drive=blobstore',
 
