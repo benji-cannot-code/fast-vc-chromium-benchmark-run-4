75c7fab84a316ec3fdf1eb22a9960d3acb8cd72d
Amirhossein Simjour <asimjour@chromium.org>
2018-05-02 17:30:21 +0000

    Scroll support for hosted dialogs in VR
    Now that VR supports html <select>, we should send scroll events
    to dialogs.
    
    Bug: 836292
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_vr
    Change-Id: I45d44af2069e21bedd0662d3f26ab6c89ff82288
    Reviewed-on: https://chromium-review.googlesource.com/1028584
    Commit-Queue: Amirhossein Simjour <asimjour@chromium.org>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555454}

--- chrome/browser/android/vr/vr_dialog.h
+++ chrome/browser/android/vr/vr_dialog.h
@@ -25,6 +25,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using base::android::JavaParamRef;
 
 namespace vr {
+// TODO(bshe): refactor content_input_delegate and vr_dialog so the common
+// code can be shared in a more clear way (https://crbug.com/838925).
 class VrDialog : public ContentInputDelegate {
  public:
   VrDialog(int width, int height);
@@ -37,24 +39,14 @@ class VrDialog : public ContentInputDelegate {
   ContentInputForwarder* dialog_ = nullptr;
 
   // ContentInputDelegate.
-  void OnContentEnter(const gfx::PointF& normalized_hit_point) override;
-  void OnContentLeave() override;
-  void OnContentMove(const gfx::PointF& normalized_hit_point) override;
-  void OnContentDown(const gfx::PointF& normalized_hit_point) override;
-  void OnContentUp(const gfx::PointF& normalized_hit_point) override;
-  void OnContentFlingCancel(std::unique_ptr<blink::WebGestureEvent> gesture,
-                            const gfx::PointF& normalized_hit_point) override;
-  void OnContentScrollBegin(std::unique_ptr<blink::WebGestureEvent> gesture,
-                            const gfx::PointF& normalized_hit_point) override;
-  void OnContentScrollUpdate(std::unique_ptr<blink::WebGestureEvent> gesture,
-                             const gfx::PointF& normalized_hit_point) override;
-  void OnContentScrollEnd(std::unique_ptr<blink::WebGestureEvent> gesture,
-                          const gfx::PointF& normalized_hit_point) override;
-  void SendGestureToDialog(std::unique_ptr<blink::WebInputEvent> event);
-
+  void SendGestureToTarget(
+      std::unique_ptr<blink::WebInputEvent> event) override;
+  void UpdateGesture(const gfx::PointF& normalized_content_hit_point,
+                     blink::WebGestureEvent& gesture) override;
   std::unique_ptr<blink::WebMouseEvent> MakeMouseEvent(
       blink::WebInputEvent::Type type,
-      const gfx::PointF& normalized_web_content_location);
+      const gfx::PointF& normalized_web_content_location) override;
+
   int width_;
   int height_;
 
