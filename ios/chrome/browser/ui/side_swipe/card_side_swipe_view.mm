79b254886b76e7a32dbd27368123d24f180f4f1b
Gauthier Ambard <gambard@chromium.org>
2018-05-02 08:04:21 +0000

    Fix side swipe with BrowserContainer fullscreen
    This CL fixes the side swipe animation when the flag to make
    BrowserContainer fullscreen is enabled.
    
    Bug: 836730
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I3b5a70135e91dd630e9c9052957347f790cafe16
    Reviewed-on: https://chromium-review.googlesource.com/1032435
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555327}

--- ios/chrome/browser/ui/side_swipe/card_side_swipe_view.mm
+++ ios/chrome/browser/ui/side_swipe/card_side_swipe_view.mm
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/side_swipe/side_swipe_util.h"
 #import "ios/chrome/browser/ui/side_swipe_gesture_recognizer.h"
 #import "ios/chrome/browser/ui/toolbar/public/side_swipe_toolbar_snapshot_providing.h"
+#include "ios/chrome/browser/ui/ui_feature_flags.h"
 #include "ios/chrome/browser/ui/ui_util.h"
 #import "ios/chrome/browser/ui/uikit_ui_util.h"
 #import "ios/chrome/browser/ui/util/constraints_ui_util.h"
@@ -102,8 +103,11 @@ const CGFloat kResizeFactor = 4;
     }
 
     _toolbarTopConstraint = [[_topToolbarSnapshot topAnchor]
-        constraintEqualToAnchor:self.topAnchor
-                       constant:-StatusBarHeight()];
+        constraintEqualToAnchor:self.topAnchor];
+
+    if (!base::FeatureList::IsEnabled(kBrowserContainerFullscreen)) {
+      _toolbarTopConstraint.constant = -StatusBarHeight();
+    }
 
     [constraints addObjectsFromArray:@[
       [[_image topAnchor] constraintEqualToAnchor:self.topAnchor
@@ -153,8 +157,10 @@ const CGFloat kResizeFactor = 4;
 
 - (void)setTopToolbarImage:(UIImage*)image isNewTabPage:(BOOL)isNewTabPage {
   [self.topToolbarSnapshot setImage:image];
-  // Update constraints as StatusBarHeight changes depending on orientation.
-  self.toolbarTopConstraint.constant = -StatusBarHeight();
+  if (!base::FeatureList::IsEnabled(kBrowserContainerFullscreen)) {
+    // Update constraints as StatusBarHeight changes depending on orientation.
+    self.toolbarTopConstraint.constant = -StatusBarHeight();
+  }
   [self.topToolbarSnapshot setNeedsLayout];
   [_shadowView setHidden:isNewTabPage];
 }
