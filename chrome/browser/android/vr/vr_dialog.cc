10175a7e660484140979f8afee7a616a2e779ebb
Matthew Jones <mdjones@chromium.org>
2018-05-02 23:02:17 +0000

    Revert "Scroll support for hosted dialogs in VR"
    This reverts commit 480d1516022c82afad0696259a94dee364a61890.
    
    Reason for revert: Suspected cause of Nougat Phone Tester failures:
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Nougat%20Phone%20Tester/6164
    
    Original change's description:
    > Scroll support for hosted dialogs in VR
    > 
    > Now that VR supports html <select>, we should send scroll events
    > to dialogs.
    > 
    > Bug: 836292
    > Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_vr
    > Change-Id: I45d44af2069e21bedd0662d3f26ab6c89ff82288
    > Reviewed-on: https://chromium-review.googlesource.com/1028584
    > Commit-Queue: Amirhossein Simjour <asimjour@chromium.org>
    > Reviewed-by: Ian Vollick <vollick@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#555454}
    
    TBR=vollick@chromium.org,bshe@chromium.org,asimjour@chromium.org
    
    Change-Id: I09843e204345ef1d42a17320e5bc05c386fc8eaa
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 836292
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_vr
    Reviewed-on: https://chromium-review.googlesource.com/1041048
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555595}

--- chrome/browser/android/vr/vr_dialog.cc
+++ chrome/browser/android/vr/vr_dialog.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_helpers.h"
 #include "base/time/time.h"
 #include "chrome/browser/android/vr/vr_shell_delegate.h"
-#include "third_party/blink/public/platform/web_gesture_event.h"
 #include "third_party/blink/public/platform/web_mouse_event.h"
 
 using base::android::JavaParamRef;
@@ -32,8 +31,39 @@ void VrDialog::SetSize(int width, int height) {
   width_ = width;
   height_ = height;
 }
+void VrDialog::OnContentEnter(const gfx::PointF& normalized_hit_point) {}
 
-void VrDialog::SendGestureToTarget(
+void VrDialog::OnContentLeave() {}
+
+void VrDialog::OnContentMove(const gfx::PointF& normalized_hit_point) {
+  SendGestureToDialog(
+      MakeMouseEvent(blink::WebInputEvent::kMouseMove, normalized_hit_point));
+}
+
+void VrDialog::OnContentDown(const gfx::PointF& normalized_hit_point) {
+  SendGestureToDialog(
+      MakeMouseEvent(blink::WebInputEvent::kMouseDown, normalized_hit_point));
+}
+
+void VrDialog::OnContentUp(const gfx::PointF& normalized_hit_point) {
+  SendGestureToDialog(
+      MakeMouseEvent(blink::WebInputEvent::kMouseUp, normalized_hit_point));
+}
+
+void VrDialog::OnContentFlingCancel(
+    std::unique_ptr<blink::WebGestureEvent> gesture,
+    const gfx::PointF& normalized_hit_point) {}
+void VrDialog::OnContentScrollBegin(
+    std::unique_ptr<blink::WebGestureEvent> gesture,
+    const gfx::PointF& normalized_hit_point) {}
+void VrDialog::OnContentScrollUpdate(
+    std::unique_ptr<blink::WebGestureEvent> gesture,
+    const gfx::PointF& normalized_hit_point) {}
+void VrDialog::OnContentScrollEnd(
+    std::unique_ptr<blink::WebGestureEvent> gesture,
+    const gfx::PointF& normalized_hit_point) {}
+
+void VrDialog::SendGestureToDialog(
     std::unique_ptr<blink::WebInputEvent> event) {
   if (!event || !dialog_)
     return;
@@ -60,12 +90,6 @@ std::unique_ptr<blink::WebMouseEvent> VrDialog::MakeMouseEvent(
   mouse_event->SetPositionInWidget(location.x(), location.y());
   mouse_event->click_count = 1;
   return mouse_event;
-}
-
-void VrDialog::UpdateGesture(const gfx::PointF& normalized_content_hit_point,
-                             blink::WebGestureEvent& gesture) {
-  gesture.SetPositionInWidget(
-      ScalePoint(normalized_content_hit_point, width_, height_));
 }
 
 }  //  namespace vr
--- chrome/browser/android/vr/vr_dialog.h
+++ chrome/browser/android/vr/vr_dialog.h
@@ -25,8 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using base::android::JavaParamRef;
 
 namespace vr {
-// TODO(bshe): refactor content_input_delegate and vr_dialog so the common
-// code can be shared in a more clear way (https://crbug.com/838925).
 class VrDialog : public ContentInputDelegate {
  public:
   VrDialog(int width, int height);
@@ -39,14 +37,24 @@ class VrDialog : public ContentInputDelegate {
   ContentInputForwarder* dialog_ = nullptr;
 
   // ContentInputDelegate.
-  void SendGestureToTarget(
-      std::unique_ptr<blink::WebInputEvent> event) override;
-  void UpdateGesture(const gfx::PointF& normalized_content_hit_point,
-                     blink::WebGestureEvent& gesture) override;
+  void OnContentEnter(const gfx::PointF& normalized_hit_point) override;
+  void OnContentLeave() override;
+  void OnContentMove(const gfx::PointF& normalized_hit_point) override;
+  void OnContentDown(const gfx::PointF& normalized_hit_point) override;
+  void OnContentUp(const gfx::PointF& normalized_hit_point) override;
+  void OnContentFlingCancel(std::unique_ptr<blink::WebGestureEvent> gesture,
+                            const gfx::PointF& normalized_hit_point) override;
+  void OnContentScrollBegin(std::unique_ptr<blink::WebGestureEvent> gesture,
+                            const gfx::PointF& normalized_hit_point) override;
+  void OnContentScrollUpdate(std::unique_ptr<blink::WebGestureEvent> gesture,
+                             const gfx::PointF& normalized_hit_point) override;
+  void OnContentScrollEnd(std::unique_ptr<blink::WebGestureEvent> gesture,
+                          const gfx::PointF& normalized_hit_point) override;
+  void SendGestureToDialog(std::unique_ptr<blink::WebInputEvent> event);
+
   std::unique_ptr<blink::WebMouseEvent> MakeMouseEvent(
       blink::WebInputEvent::Type type,
-      const gfx::PointF& normalized_web_content_location) override;
-
+      const gfx::PointF& normalized_web_content_location);
   int width_;
   int height_;
 
