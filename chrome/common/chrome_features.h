f95016642dccbfa2e2cf62c92ab7cf9992a61248
Timothy Loh <timloh@chromium.org>
2018-05-02 04:22:36 +0000

    Remove unused kCrOSContainer feature
    Change-Id: I26bab2e43fa0599b052b0740e8b9949b12022860
    Reviewed-on: https://chromium-review.googlesource.com/1036708
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555309}

--- chrome/common/chrome_features.h
+++ chrome/common/chrome_features.h
@@ -347,8 +347,6 @@ extern const base::Feature kCrosCompUpdates;
 
 extern const base::Feature kCrOSComponent;
 
-extern const base::Feature kCrOSContainer;
-
 extern const base::Feature kInstantTethering;
 
 extern const base::Feature kEasyUnlockPromotions;
