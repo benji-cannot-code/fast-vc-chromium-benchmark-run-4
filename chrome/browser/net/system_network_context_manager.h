28838e21dc99d84962276feedf6e2857a5021ec8
Mark Pilgrim <pilgrim@chromium.org>
2018-05-02 21:02:18 +0000

    Add SystemNetworkContextManager::GetSharedURLLoaderFactory
    (split off from https://chromium-review.googlesource.com/c/chromium/src/+/1035543 )
    
    This adds GetSharedURLLoaderFactory(), which returns a SharedURLLoaderFactory
    owned by the SystemNetworkContextManager and backed by the SystemNetworkContext.
    
    Bug: 773295
    Change-Id: I5301e9785d913dd27cdc36e7ef4f759290e1ecf5
    Reviewed-on: https://chromium-review.googlesource.com/1037943
    Commit-Queue: Mark Pilgrim <pilgrim@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555515}

--- chrome/browser/net/system_network_context_manager.h
+++ chrome/browser/net/system_network_context_manager.h
@@ -9,10 +9,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/macros.h"
+#include "base/memory/ref_counted.h"
 #include "chrome/browser/net/proxy_config_monitor.h"
 #include "services/network/public/mojom/network_service.mojom.h"
 
 class ProxyConfigMonitor;
+
+namespace network {
+namespace mojom {
+class URLLoaderFactory;
+}
+class SharedURLLoaderFactory;
+}  // namespace network
 
 // Responsible for creating and managing access to the system NetworkContext.
 // Lives on the UI thread. The NetworkContext this owns is intended for requests
@@ -60,6 +68,10 @@ class SystemNetworkContextManager {
   // this method to get a URLLoaderFactory that can be used on other threads.
   network::mojom::URLLoaderFactory* GetURLLoaderFactory();
 
+  // Returns a SharedURLLoaderFactory owned by the SystemNetworkContextManager
+  // that is backed by the SystemNetworkContext.
+  scoped_refptr<network::SharedURLLoaderFactory> GetSharedURLLoaderFactory();
+
   // Permanently disables QUIC, both for NetworkContexts using the IOThread's
   // NetworkService, and for those using the network service (if enabled).
   void DisableQuic();
@@ -71,6 +83,8 @@ class SystemNetworkContextManager {
   void FlushNetworkInterfaceForTesting();
 
  private:
+  class URLLoaderFactoryForSystem;
+
   // Creates parameters for the NetworkContext. May only be called once, since
   // it initializes some class members.
   network::mojom::NetworkContextParamsPtr CreateNetworkContextParams();
@@ -88,6 +102,7 @@ class SystemNetworkContextManager {
 
   // URLLoaderFactory backed by the NetworkContext returned by GetContext(), so
   // consumers don't all need to create their own factory.
+  scoped_refptr<URLLoaderFactoryForSystem> shared_url_loader_factory_;
   network::mojom::URLLoaderFactoryPtr url_loader_factory_;
 
   bool is_quic_allowed_ = true;
