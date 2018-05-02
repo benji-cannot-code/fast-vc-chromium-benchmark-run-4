27b7bb42c2153ef6e49b3efad49b2458d5fdb5c9
Eugene But <eugenebut@google.com>
2018-05-02 05:39:02 +0000

    Replace External URL testErrorPageRedirect with hermetic test.
    Old test was loading http://mock/bad and because this URL did not
    exist the load was failing because of DNS resolution error.
    
    New test uses EmbeddedTestServer, which closes the socket to simulate
    ERR_INTERNET_DISCONNECTED error.
    
    Bug: 694662
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Iaca523e8153a2c4cca0e1f174652ed48f22cabd3
    Reviewed-on: https://chromium-review.googlesource.com/1035610
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Danyao Wang <danyao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555316}

--- ios/web/public/test/http_server/error_page_response_provider.mm
+++ ios/web/public/test/http_server/error_page_response_provider.mm
@@ -5,10 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/web/public/test/http_server/error_page_response_provider.h"
 
-#include "base/logging.h"
-#import "ios/web/public/test/http_server/http_server.h"
-#include "net/http/http_status_code.h"
-
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
@@ -16,31 +12,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // static
 GURL ErrorPageResponseProvider::GetDnsFailureUrl() {
   return GURL("http://mock/bad");
-}
-
-// static
-GURL ErrorPageResponseProvider::GetRedirectToDnsFailureUrl() {
-  return web::test::HttpServer::MakeUrl("http://mock/redirect");
-}
-
-// Returns true for |RedirectToDnsFailureUrl|.
-bool ErrorPageResponseProvider::CanHandleRequest(const Request& request) {
-  return (HtmlResponseProvider::CanHandleRequest(request) ||
-          request.url ==
-              ErrorPageResponseProvider::GetRedirectToDnsFailureUrl());
-}
-
-void ErrorPageResponseProvider::GetResponseHeadersAndBody(
-    const Request& request,
-    scoped_refptr<net::HttpResponseHeaders>* headers,
-    std::string* response_body) {
-  if (HtmlResponseProvider::CanHandleRequest(request)) {
-    HtmlResponseProvider::GetResponseHeadersAndBody(request, headers,
-                                                    response_body);
-  } else if (request.url == GetRedirectToDnsFailureUrl()) {
-    *headers = web::ResponseProvider::GetRedirectResponseHeaders(
-        ErrorPageResponseProvider::GetDnsFailureUrl().spec(), net::HTTP_FOUND);
-  } else {
-    NOTREACHED();
-  }
 }
