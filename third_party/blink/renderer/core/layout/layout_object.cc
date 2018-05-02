533c065f79d06b7a20774041df3da0a299357d2f
Xianzhu Wang <wangxianzhu@chromium.org>
2018-05-02 04:52:42 +0000

    [PE] Fix missing InvalidateClipPathCache in a corner case
    InvalidateClipPathCache should be called whenever style.ClipPath()
    changes. Previously it missed a branch.
    
    Bug: 835828
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Ifd178ac2b2b695239a7aa0b69cdbe4f41cad5af2
    Reviewed-on: https://chromium-review.googlesource.com/1038425
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Tien-Ren Chen <trchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555312}

--- third_party/blink/renderer/core/layout/layout_object.cc
+++ third_party/blink/renderer/core/layout/layout_object.cc
@@ -2041,10 +2041,12 @@ void LayoutObject::SetStyle(scoped_refptr<ComputedStyle> style) {
       SetShouldDoFullPaintInvalidation();
     else
       SetShouldDoFullPaintInvalidationWithoutGeometryChange();
-
-    if (old_style && !old_style->ClipPathDataEquivalent(*style_))
-      InvalidateClipPathCache();
   }
+
+  if ((diff.NeedsPaintInvalidationObject() ||
+       diff.NeedsPaintInvalidationSubtree()) &&
+      old_style && !old_style->ClipPathDataEquivalent(*style_))
+    InvalidateClipPathCache();
 
   if (diff.NeedsVisualRectUpdate())
     SetMayNeedPaintInvalidation();
