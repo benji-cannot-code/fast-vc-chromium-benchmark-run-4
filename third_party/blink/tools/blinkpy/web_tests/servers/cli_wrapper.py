ad47978e84bd2d3ef1adc138f691b640e3889a71
Kent Tamura <tkent@chromium.org>
2018-05-02 13:05:00 +0000

    Update references to "Tools/Scripts"
    All code in third_party/WebKit/Tools/Scripts was moved to
    third_party/blink/tools.
    This CL also fixes references to old names of scripts in Tools/Scripts.
    
    * third_party/WebKit/PRESUBMIT.py:
     Remove dead code. This PRESUBMIT.py doesn't use
     |audit_non_blink_usage|.
    
    Bug: 829697
    Change-Id: I6c482a795708fa207124cb2030a4fdaa8ceac343
    Reviewed-on: https://chromium-review.googlesource.com/1038863
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555366}

--- third_party/blink/tools/blinkpy/web_tests/servers/cli_wrapper.py
+++ third_party/blink/tools/blinkpy/web_tests/servers/cli_wrapper.py
@@ -29,8 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 """A utility module for making standalone scripts to start servers.
 
-Scripts in Tools/Scripts can use this module to start servers that
-are normally used for layout tests, outside of the layout test runner.
+Scripts in tools/ can use this module to start servers that are normally used
+for layout tests, outside of the layout test runner.
 """
 
 import argparse
