0207d6274035f20606aa3b921e3e5522161ee04c
Xiaohui Chen <xiaohuic@google.com>
2018-05-02 21:07:55 +0000

    Sync native assistant state
    Push native assistant state into voice interation controller so existing
    UI elements logic could be reused.
    
    Bug: b/78193960
    Test: locally build
    Change-Id: I6fbaf74db41263fbdd184bed37e8f29a21578c6f
    Reviewed-on: https://chromium-review.googlesource.com/1040351
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Qiang Xu <warx@google.com>
    Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555525}

--- chrome/browser/chromeos/assistant/assistant_client.cc
+++ chrome/browser/chromeos/assistant/assistant_client.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
+#include "ash/public/interfaces/voice_interaction_controller.mojom.h"
+#include "chrome/browser/chromeos/arc/voice_interaction/voice_interaction_controller_client.h"
 #include "chrome/browser/chromeos/assistant/assistant_card_renderer.h"
 #include "chromeos/services/assistant/public/mojom/constants.mojom.h"
 #include "services/service_manager/public/cpp/connector.h"
@@ -51,7 +53,9 @@ void AssistantClient::Start(service_manager::Connector* connector) {
 }
 
 void AssistantClient::OnAssistantStatusChanged(bool running) {
-  running_ = running;
+  arc::VoiceInteractionControllerClient::Get()->NotifyStatusChanged(
+      running ? ash::mojom::VoiceInteractionState::RUNNING
+              : ash::mojom::VoiceInteractionState::STOPPED);
 }
 
 }  // namespace assistant
