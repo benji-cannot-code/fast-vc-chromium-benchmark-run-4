953f0c04f4c91f8c0a9e9bc3bdd210661467284c
Ramin Halavati <rhalavati@chromium.org>
2018-05-03 04:37:20 +0000

    Expand annotation tests to all targets.
    Network traffic annotation tests on trybots are triggered only if Chrome
    or remoting targets are modified. In cases that a file out of this
    scope, like in components, is modified, the test is not triggered and
    annotation changes miss tests.
    
    The test scope is expanded to all targets. It is expected that this
    does not have any negative speed effect as traffic annotation tests
    prefilter files that do not include any annotations.
    
    Bug: 690323
    Change-Id: Iaa771443a40285c4b547b5e3c15f26f6cb61457e
    Reviewed-on: https://chromium-review.googlesource.com/1030270
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555666}

--- testing/scripts/check_network_annotations.py
+++ testing/scripts/check_network_annotations.py
@@ -36,8 +36,7 @@ def main_run(args):
 
 
 def main_compile_targets(args):
-  json.dump(['chrome', 'remoting/host:host', 'remoting/client:client'],
-            args.output)
+  json.dump(['gn_all'], args.output)
 
 
 if __name__ == '__main__':
