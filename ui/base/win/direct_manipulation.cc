7b3474612e3501dad85effad884c932b6f09133a
chaopeng <chaopeng@chromium.org>
2018-05-03 23:28:48 +0000

    Set WindowEventTarget when DirectManipulationHelper create
    Currently, we only set WindowEventTarget to DirectManipulationHelper
    when receive DM_POINTERHITTEST. But the crash shows OnContentUpdate
    may come before DM_POINTERHITTEST. To prevent the crash we set the
    WindowEventTarget when DirectManipulationHelper create.
    
    Bug: 839338
    Change-Id: I2921277178771f9124a37325a8dff47196500322
    Reviewed-on: https://chromium-review.googlesource.com/1042381
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Jianpeng Chao <chaopeng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555904}

--- ui/base/win/direct_manipulation.cc
+++ ui/base/win/direct_manipulation.cc
@@ -19,7 +19,8 @@ namespace win {
 
 // static
 std::unique_ptr<DirectManipulationHelper>
-DirectManipulationHelper::CreateInstance(HWND window) {
+DirectManipulationHelper::CreateInstance(HWND window,
+                                         WindowEventTarget* event_target) {
   if (!::IsWindow(window))
     return nullptr;
 
@@ -34,7 +35,7 @@ DirectManipulationHelper::CreateInstance(HWND window) {
       base::WrapUnique(new DirectManipulationHelper());
   instance->window_ = window;
 
-  if (instance->Initialize())
+  if (instance->Initialize(event_target))
     return instance;
 
   return nullptr;
@@ -55,9 +56,8 @@ DirectManipulationHelper::CreateInstanceForTesting(
   std::unique_ptr<DirectManipulationHelper> instance =
       base::WrapUnique(new DirectManipulationHelper());
 
-  instance->event_handler_ =
-      Microsoft::WRL::Make<DirectManipulationHandler>(instance.get());
-  instance->event_handler_->SetWindowEventTarget(event_target);
+  instance->event_handler_ = Microsoft::WRL::Make<DirectManipulationHandler>(
+      instance.get(), event_target);
 
   instance->viewport_ = viewport;
 
@@ -75,7 +75,7 @@ DirectManipulationHelper::DirectManipulationHelper() {}
 // the fake viewport.
 const RECT VIEWPORT_DEFAULT_RECT = {0, 0, 1000, 1000};
 
-bool DirectManipulationHelper::Initialize() {
+bool DirectManipulationHelper::Initialize(WindowEventTarget* event_target) {
   // IDirectManipulationUpdateManager is the first COM object created by the
   // application to retrieve other objects in the Direct Manipulation API.
   // It also serves to activate and deactivate Direct Manipulation functionality
@@ -116,7 +116,8 @@ bool DirectManipulationHelper::Initialize() {
   if (!SUCCEEDED(hr))
     return false;
 
-  event_handler_ = Microsoft::WRL::Make<DirectManipulationHandler>(this);
+  event_handler_ =
+      Microsoft::WRL::Make<DirectManipulationHandler>(this, event_target);
   if (!SUCCEEDED(hr))
     return false;
 
@@ -209,8 +210,9 @@ DirectManipulationHandler::DirectManipulationHandler() {
 }
 
 DirectManipulationHandler::DirectManipulationHandler(
-    DirectManipulationHelper* helper)
-    : helper_(helper) {}
+    DirectManipulationHelper* helper,
+    WindowEventTarget* event_target)
+    : helper_(helper), event_target_(event_target) {}
 
 DirectManipulationHandler::~DirectManipulationHandler() {}
 
