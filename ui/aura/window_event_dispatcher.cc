a4b9c1935301ef3ecd99a6098ada74135552a6cc
Bret Sepulveda <bsep@chromium.org>
2018-05-03 19:11:28 +0000

    Reland: Use the real mouse position when synthesizing mouse events.
    Aura synthesizes a mouse event at certain times, including when the
    window comes back from being minimized. Currently it uses the last known
    mouse position, which works in most cases. However, the window doesn't
    get mouse events when minimized, so the last known position isn't
    updated. When the window comes back, if the user's mouse is not over the
    window it can cause buttons to get stuck in the hovered state until the
    user moves the mouse back into the window.
    
    To fix this, this patch uses the real mouse position.
    
    This patch also updates content_shell to move the mouse to 0,0 in
    headless mode, to avoid any queries to the real mouse position from
    interfering with blink tests. This patch also adds an implementation
    for MoveCursorTo on Windows.
    
    Bug: 828591, 834166
    Change-Id: I81354c77805c1928eb0cbccb900856a8c523709d
    Reviewed-on: https://chromium-review.googlesource.com/1022959
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555843}

--- ui/aura/window_event_dispatcher.cc
+++ ui/aura/window_event_dispatcher.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/hit_test.h"
 #include "ui/base/ime/input_method.h"
 #include "ui/compositor/dip_util.h"
+#include "ui/display/screen.h"
 #include "ui/events/event.h"
 #include "ui/events/event_utils.h"
 #include "ui/events/gestures/gesture_recognizer.h"
@@ -228,10 +229,7 @@ void WindowEventDispatcher::ReleasePointerMoves() {
 
 gfx::Point WindowEventDispatcher::GetLastMouseLocationInRoot() const {
   gfx::Point location = Env::GetInstance()->last_mouse_location();
-  client::ScreenPositionClient* client =
-      client::GetScreenPositionClient(window());
-  if (client)
-    client->ConvertPointFromScreen(window(), &location);
+  ConvertPointFromScreen(&location);
   return location;
 }
 
@@ -263,6 +261,13 @@ Window* WindowEventDispatcher::window() {
 
 const Window* WindowEventDispatcher::window() const {
   return host_->window();
+}
+
+void WindowEventDispatcher::ConvertPointFromScreen(gfx::Point* point) const {
+  client::ScreenPositionClient* client =
+      client::GetScreenPositionClient(window());
+  if (client)
+    client->ConvertPointFromScreen(window(), point);
 }
 
 void WindowEventDispatcher::TransformEventForDeviceScaleFactor(
@@ -861,10 +866,14 @@ ui::EventDispatchDetails WindowEventDispatcher::SynthesizeMouseMoveEvent() {
   if (Env::GetInstance()->mouse_button_flags())
     return details;
 
-  gfx::Point root_mouse_location = GetLastMouseLocationInRoot();
-  if (!window()->bounds().Contains(root_mouse_location))
+  // Do not use GetLastMouseLocationInRoot here because it's not updated when
+  // the mouse is not over the window or when the window is minimized.
+  gfx::Point mouse_location =
+      display::Screen::GetScreen()->GetCursorScreenPoint();
+  ConvertPointFromScreen(&mouse_location);
+  if (!window()->bounds().Contains(mouse_location))
     return details;
-  gfx::Point host_mouse_location = root_mouse_location;
+  gfx::Point host_mouse_location = mouse_location;
   host_->ConvertDIPToPixels(&host_mouse_location);
   ui::MouseEvent event(ui::ET_MOUSE_MOVED, host_mouse_location,
                        host_mouse_location, ui::EventTimeForNow(),
