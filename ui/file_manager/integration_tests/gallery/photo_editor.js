69e63673a5c52b1b2a1311074506182f7068ce5e
Ahmed Fakhry <afakhry@google.com>
2018-05-03 20:52:26 +0000

    [GM2 Top bars]: Caption Buttons
    This CL implements the new GM2 styles for the non-
    client frames caption buttons:
    - Use circular inkdrop flood fill and highlight.
    - Use the new colors and opacities for the caption icons.
    - Use a height of 32dp for the non-browser frames.
    
    TODO: Implement the same styles for PWAs' frame buttons
    
    BUG=831872
    TEST=manual
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Id2c2603b67b290b5eba1970d78d5d0c395df7fe4
    Reviewed-on: https://chromium-review.googlesource.com/1041144
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555864}

--- ui/file_manager/integration_tests/gallery/photo_editor.js
+++ ui/file_manager/integration_tests/gallery/photo_editor.js
@@ -106,8 +106,8 @@ function cropImage(testVolumeName, volumeType) {
         then(function() {
           return gallery.waitForSlideImage(
               appId,
-              532,
-              398,
+              534,
+              400,
               'My Desktop Background');
         }).
         then(function() {
@@ -376,7 +376,7 @@ testcase.resizeImageOnDownloads = function() {
  */
 testcase.resizeImageOnDrive = function() {
   return resizeImage('drive', 'drive');
-}
+};
 
 /**
  * The enableDisableOverwriteOriginalCheckbox test for Downloads.
