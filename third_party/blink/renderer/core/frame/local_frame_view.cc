9d3f8ea9ed4c28a7b2349607276fee0da47cdc85
Chris Harrelson <chrishtr@chromium.org>
2018-05-03 01:05:07 +0000

    Add benchmark category to top-level Blink rendering macros.
    This means they will show up by default in telemetry trace output.
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Ia67721c41f3347e1f58d1b37ebf69032fd06ddb7
    Reviewed-on: https://chromium-review.googlesource.com/1041146
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555637}

--- third_party/blink/renderer/core/frame/local_frame_view.cc
+++ third_party/blink/renderer/core/frame/local_frame_view.cc
@@ -3329,6 +3329,8 @@ bool LocalFrameView::UpdateLifecyclePhasesInternal(
   }
 
   {
+    TRACE_EVENT0("blink,benchmark",
+                 "LocalFrameView::UpdateViewportIntersectionsForSubtree");
     SCOPED_UMA_AND_UKM_TIMER("Blink.IntersectionObservation.UpdateTime",
                              UkmMetricNames::kIntersectionObservation);
     UpdateViewportIntersectionsForSubtree(target_state);
@@ -3363,7 +3365,7 @@ void LocalFrameView::PerformScrollAnchoringAdjustments() {
 }
 
 void LocalFrameView::PrePaint() {
-  TRACE_EVENT0("blink", "LocalFrameView::prePaint");
+  TRACE_EVENT0("blink,benchmark", "LocalFrameView::prePaint");
 
   if (!paint_controller_)
     paint_controller_ = PaintController::Create();
@@ -3425,7 +3427,7 @@ static void CollectDrawableLayersForLayerListRecursively(
 }
 
 void LocalFrameView::PaintTree() {
-  TRACE_EVENT0("blink", "LocalFrameView::paintTree");
+  TRACE_EVENT0("blink,benchmark", "LocalFrameView::paintTree");
   SCOPED_UMA_AND_UKM_TIMER("Blink.Paint.UpdateTime", UkmMetricNames::kPaint);
 
   DCHECK(GetFrame() == GetPage()->MainFrame() ||
@@ -3551,7 +3553,7 @@ void LocalFrameView::UpdateStyleAndLayoutIfNeededRecursiveInternal() {
     return;
 
   ScopedFrameBlamer frame_blamer(frame_);
-  TRACE_EVENT0("blink",
+  TRACE_EVENT0("blink,benchmark",
                "LocalFrameView::updateStyleAndLayoutIfNeededRecursive");
 
   // We have to crawl our entire subtree looking for any FrameViews that need
--- third_party/blink/renderer/core/paint/compositing/paint_layer_compositor.cc
+++ third_party/blink/renderer/core/paint/compositing/paint_layer_compositor.cc
@@ -233,7 +233,8 @@ void PaintLayerCompositor::UpdateIfNeededRecursiveInternal(
     }
   }
 
-  TRACE_EVENT0("blink", "PaintLayerCompositor::updateIfNeededRecursive");
+  TRACE_EVENT0("blink,benchmark",
+               "PaintLayerCompositor::updateIfNeededRecursive");
 
   DCHECK(!layout_view_.NeedsLayout());
 
