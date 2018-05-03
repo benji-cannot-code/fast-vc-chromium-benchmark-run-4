7d78714a33fa48a76b8a2bcd069b8c68d5e0ee69
Antonio Gomes <tonikitoo@igalia.com>
2018-05-03 14:34:39 +0000

    Migrate OneGoogleBarFetcherImpl::AuthenticatedURLFetcher to SimpleURLLoader
    Of the various unittests associated with the migration that were reworked
    to work with the TestURLLoaderFactory machinery, two particularly became
    unneeded:
    
    - OneGoogleBarFetcherImplTest::SecondRequestOverridesFirst.
    The test exercised a premise of SimpleURLLoader that says once an
    instance is deleted its completion call will not be called.
    
    - OneGoogleBarFetcherImplTest, HttpErrorIsFatal
    The latest CL does not distinguish between server and common HTTP errors
    any more.
    
    CL also updates OneGoogleBarFetcherImplTests::thread_bundle_
    to initialize with IO_MAINLOOP, so that it accommodates network requests
    being made on UI threa
    
    BUG=773295
    
    Change-Id: I1a186f4067eb64f0e36d1b88ccbe9c8914a67480
    Reviewed-on: https://chromium-review.googlesource.com/1031450
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555724}

--- chrome/browser/search/one_google_bar/one_google_bar_service_factory.cc
+++ chrome/browser/search/one_google_bar/one_google_bar_service_factory.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "components/signin/core/browser/signin_header_helper.h"
 #include "content/public/browser/browser_context.h"
+#include "content/public/browser/storage_partition.h"
 
 // static
 OneGoogleBarService* OneGoogleBarServiceFactory::GetForProfile(
@@ -66,10 +67,13 @@ KeyedService* OneGoogleBarServiceFactory::BuildServiceInstanceFor(
   }
   content_settings::CookieSettings* cookie_settings =
       CookieSettingsFactory::GetForProfile(profile).get();
+  auto url_loader_factory =
+      content::BrowserContext::GetDefaultStoragePartition(context)
+          ->GetURLLoaderFactoryForBrowserProcess();
   return new OneGoogleBarService(
       cookie_service,
       std::make_unique<OneGoogleBarFetcherImpl>(
-          profile->GetRequestContext(), google_url_tracker,
+          url_loader_factory, google_url_tracker,
           g_browser_process->GetApplicationLocale(), override_api_url,
           AccountConsistencyModeManager::IsMirrorEnabledForProfile(profile) &&
               signin::SettingsAllowSigninCookies(cookie_settings)));
