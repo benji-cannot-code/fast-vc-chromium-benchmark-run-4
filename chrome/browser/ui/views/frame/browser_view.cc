46e5d241e50215f894ac86246adf1e72c7181d36
Tom Anderson <thomasanderson@chromium.org>
2018-05-03 23:49:54 +0000

    Add warn before quitting experiment on Windows and Linux
    This CL adds a warn-before-quitting flag.  When enabled, pressing Ctrl+Shift+Q
    will show a bubble asking to continue holding the shortcut to quit.  Pressing
    the shortcut a second time while the bubble is showing will also confirm the
    quit.
    
    R=sky
    BUG=243164
    
    Change-Id: I97c4dc37cb5107fde975a4162f349e1ea5337b5d
    Reviewed-on: https://chromium-review.googlesource.com/1031097
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555909}

--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -65,6 +65,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/autofill/save_card_icon_view.h"
 #include "chrome/browser/ui/views/bookmarks/bookmark_bar_view.h"
 #include "chrome/browser/ui/views/bookmarks/bookmark_bubble_view.h"
+#include "chrome/browser/ui/views/confirm_quit_bubble_controller.h"
 #include "chrome/browser/ui/views/download/download_in_progress_dialog_view.h"
 #include "chrome/browser/ui/views/download/download_shelf_view.h"
 #include "chrome/browser/ui/views/exclusive_access_bubble_views.h"
@@ -96,6 +97,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/update_recommended_message_box.h"
 #include "chrome/browser/ui/window_sizer/window_sizer.h"
 #include "chrome/common/channel_info.h"
+#include "chrome/common/chrome_features.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/extensions/command.h"
 #include "chrome/common/pref_names.h"
@@ -1324,6 +1326,14 @@ content::KeyboardEventProcessingResult BrowserView::PreHandleKeyboardEvent(
     return content::KeyboardEventProcessingResult::NOT_HANDLED;
   }
 
+#if defined(OS_WIN) || (defined(OS_LINUX) && !defined(OS_CHROMEOS))
+  if (base::FeatureList::IsEnabled(features::kWarnBeforeQuitting) &&
+      ConfirmQuitBubbleController::GetInstance()->HandleKeyboardEvent(
+          accelerator)) {
+    return content::KeyboardEventProcessingResult::HANDLED_WANTS_KEY_UP;
+  }
+#endif  // defined(OS_WIN) || (defined(OS_LINUX) && !defined(OS_CHROMEOS))
+
 #if defined(OS_CHROMEOS)
   if (ash_util::IsAcceleratorDeprecated(accelerator)) {
     return (event.GetType() == blink::WebInputEvent::kRawKeyDown)
@@ -2090,6 +2100,14 @@ void BrowserView::OnThemeChanged() {
 // BrowserView, ui::AcceleratorTarget overrides:
 
 bool BrowserView::AcceleratorPressed(const ui::Accelerator& accelerator) {
+#if defined(OS_WIN) || (defined(OS_LINUX) && !defined(OS_CHROMEOS))
+  if (base::FeatureList::IsEnabled(features::kWarnBeforeQuitting) &&
+      ConfirmQuitBubbleController::GetInstance()->HandleKeyboardEvent(
+          accelerator)) {
+    return true;
+  }
+#endif  // defined(OS_WIN) || (defined(OS_LINUX) && !defined(OS_CHROMEOS))
+
   int command_id;
   // Though AcceleratorManager should not send unknown |accelerator| to us, it's
   // still possible the command cannot be executed now.
