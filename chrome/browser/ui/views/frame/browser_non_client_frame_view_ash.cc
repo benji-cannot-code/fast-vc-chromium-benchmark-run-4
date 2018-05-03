791811b57bf50776770d058cedcae391668a94f2
Evan Stade <estade@chromium.org>
2018-05-03 00:04:51 +0000

    [Mash] fix color of Settings app frame.
    Bug: 834092
    Change-Id: I0676206a08de89cb6f4a8d0884228357b9576d60
    Reviewed-on: https://chromium-review.googlesource.com/1028982
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555618}

--- chrome/browser/ui/views/frame/browser_non_client_frame_view_ash.cc
+++ chrome/browser/ui/views/frame/browser_non_client_frame_view_ash.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/extensions/hosted_app_browser_controller.h"
 #include "chrome/browser/ui/layout_constants.h"
 #include "chrome/browser/ui/views/frame/browser_frame.h"
+#include "chrome/browser/ui/views/frame/browser_frame_ash.h"
 #include "chrome/browser/ui/views/frame/browser_frame_header_ash.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/frame/hosted_app_button_container.h"
@@ -669,11 +670,8 @@ BrowserNonClientFrameViewAsh::CreateFrameHeader() {
                        weak_factory_.GetWeakPtr()),
         kTitlebarAnimationDelay);
   } else if (!browser->is_app()) {
-    // For non app (i.e. WebUI) windows (e.g. Settings) use MD frame color.
-    constexpr SkColor kMdWebUIFrameColor =
-        SkColorSetARGB(0xff, 0x25, 0x4f, 0xae);
-    default_frame_header->SetFrameColors(kMdWebUIFrameColor,
-                                         kMdWebUIFrameColor);
+    default_frame_header->SetFrameColors(BrowserFrameAsh::kMdWebUiFrameColor,
+                                         BrowserFrameAsh::kMdWebUiFrameColor);
   }
 
   if (back_button_)
