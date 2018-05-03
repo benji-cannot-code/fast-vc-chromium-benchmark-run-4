9d504cd94ef5898859ce2e2c5aa958b3c5baa365
Victor Costan <pwnall@chromium.org>
2018-05-03 01:17:41 +0000

    ServiceWorker: Remove AdaptCallbackForRepeating.
    base::AdaptCallbackForRepeating was introduced to help migrate from
    base::Callback to base::OnceCallback / base::RepeatingCallback. However,
    the methods used by ServiceWorkerContextCore have been converted to take
    base::OnceCallback, so the adapter isn't needed anymore.
    
    Bug: 730593
    Change-Id: I6eb27da059494075d457d3a3609faf80ca7087a2
    Reviewed-on: https://chromium-review.googlesource.com/1040986
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555643}

--- content/browser/service_worker/service_worker_context_core.cc
+++ content/browser/service_worker/service_worker_context_core.cc
@@ -495,9 +495,9 @@ void ServiceWorkerContextCore::DeleteForOrigin(const GURL& origin,
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
   storage()->GetRegistrationsForOrigin(
       origin,
-      AdaptCallbackForRepeating(base::BindOnce(
+      base::BindOnce(
           &ServiceWorkerContextCore::DidGetRegistrationsForDeleteForOrigin,
-          AsWeakPtr(), std::move(callback))));
+          AsWeakPtr(), std::move(callback)));
 }
 
 void ServiceWorkerContextCore::DidGetRegistrationsForDeleteForOrigin(
@@ -525,8 +525,7 @@ void ServiceWorkerContextCore::DidGetRegistrationsForDeleteForOrigin(
     }
     UnregisterServiceWorker(
         registration->pattern(),
-        AdaptCallbackForRepeating(base::BindOnce(&SuccessCollectorCallback,
-                                                 barrier, overall_success)));
+        base::BindOnce(&SuccessCollectorCallback, barrier, overall_success));
   }
 }
 
