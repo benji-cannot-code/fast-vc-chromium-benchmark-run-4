aecfa394fb855c69cd93d4868218f8751b78323c
Sergey Volk <servolk@google.com>
2018-05-03 18:53:34 +0000

    [Chromecast] Fix a/v unit test expectations
    Bug: b/77279478
    Test: cast_media_unittests
    Change-Id: I45ede6c40cbe7fca9dc7273e1ee1e179184a43d5
    Reviewed-on: https://chromium-review.googlesource.com/1040662
    Reviewed-by: Stephen Lanham <slan@chromium.org>
    Commit-Queue: Sergey Volk <servolk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555831}

--- chromecast/media/cma/pipeline/audio_video_pipeline_impl_unittest.cc
+++ chromecast/media/cma/pipeline/audio_video_pipeline_impl_unittest.cc
@@ -173,6 +173,22 @@ class PipelineHelper {
     }
   }
 
+  void SetPipelineStartExpectations() {
+    // The pipeline will be paused first, for the initial data buffering. Then
+    // it will be resumed, once enough data is buffered to start playback.
+    EXPECT_CALL(*pipeline_backend_, GetCurrentPts());
+    EXPECT_CALL(*pipeline_backend_, Pause());
+    EXPECT_CALL(*pipeline_backend_, SetPlaybackRate(1.0f));
+    EXPECT_CALL(*pipeline_backend_, Resume());
+  }
+
+  // This is used for the Flush test case, where the pipeline start sequence is
+  // interrupted by the Flush, and the initial buffering never completes.
+  void SetPipelineStartFlushExpectations() {
+    EXPECT_CALL(*pipeline_backend_, GetCurrentPts());
+    EXPECT_CALL(*pipeline_backend_, Pause());
+  }
+
   void Start(const base::Closure& eos_cb) {
     eos_cb_ = eos_cb;
     eos_[STREAM_AUDIO] = !media_pipeline_->HasAudio();
@@ -291,6 +307,7 @@ static void VerifyPlay(PipelineHelper* pipeline_helper) {
 TEST_P(AudioVideoPipelineImplTest, Play) {
   base::Closure verify_task =
       base::Bind(&VerifyPlay, base::Unretained(pipeline_helper_.get()));
+  pipeline_helper_->SetPipelineStartExpectations();
   scoped_task_environment_.GetMainThreadTaskRunner()->PostTask(
       FROM_HERE,
       base::BindOnce(&PipelineHelper::Start,
@@ -315,6 +332,7 @@ static void VerifyNotReached() {
 TEST_P(AudioVideoPipelineImplTest, Flush) {
   base::Closure verify_task =
       base::Bind(&VerifyFlush, base::Unretained(pipeline_helper_.get()));
+  pipeline_helper_->SetPipelineStartFlushExpectations();
   scoped_task_environment_.GetMainThreadTaskRunner()->PostTask(
       FROM_HERE, base::BindOnce(&PipelineHelper::Start,
                                 base::Unretained(pipeline_helper_.get()),
@@ -334,6 +352,7 @@ TEST_P(AudioVideoPipelineImplTest, FullCycle) {
       base::Bind(&PipelineHelper::Flush,
                  base::Unretained(pipeline_helper_.get()), stop_task);
 
+  pipeline_helper_->SetPipelineStartExpectations();
   scoped_task_environment_.GetMainThreadTaskRunner()->PostTask(
       FROM_HERE,
       base::BindOnce(&PipelineHelper::Start,
@@ -377,6 +396,7 @@ TEST_F(EncryptedAVPipelineImplTest, SetCdmWithLicenseBeforeStart) {
   scoped_task_environment_.GetMainThreadTaskRunner()->PostTask(
       FROM_HERE, base::BindOnce(&PipelineHelper::SetCdmLicenseInstalled,
                                 base::Unretained(pipeline_helper_.get())));
+  pipeline_helper_->SetPipelineStartExpectations();
   scoped_task_environment_.GetMainThreadTaskRunner()->PostTask(
       FROM_HERE,
       base::BindOnce(&PipelineHelper::Start,
@@ -388,6 +408,7 @@ TEST_F(EncryptedAVPipelineImplTest, SetCdmWithLicenseBeforeStart) {
 TEST_F(EncryptedAVPipelineImplTest, SetCdmWithLicenseAfterStart) {
   base::Closure verify_task =
       base::Bind(&VerifyPlay, base::Unretained(pipeline_helper_.get()));
+  pipeline_helper_->SetPipelineStartExpectations();
   scoped_task_environment_.GetMainThreadTaskRunner()->PostTask(
       FROM_HERE,
       base::BindOnce(&PipelineHelper::Start,
@@ -407,6 +428,7 @@ TEST_F(EncryptedAVPipelineImplTest, SetCdmWithLicenseAfterStart) {
 TEST_F(EncryptedAVPipelineImplTest, SetCdmAndInstallLicenseAfterStart) {
   base::Closure verify_task =
       base::Bind(&VerifyPlay, base::Unretained(pipeline_helper_.get()));
+  pipeline_helper_->SetPipelineStartExpectations();
   scoped_task_environment_.GetMainThreadTaskRunner()->PostTask(
       FROM_HERE,
       base::BindOnce(&PipelineHelper::Start,
