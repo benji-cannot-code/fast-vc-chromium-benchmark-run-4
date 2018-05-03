05e5fb1d5b16588c09d62daa6e1b020ffa473ab8
Marc Treib <treib@chromium.org>
2018-05-03 11:50:14 +0000

    Revert "[Layered API] Implement Layered API infrastructure behind a flag"
    This reverts commit bde742c2e4758ae132847b0434d516dc91892d7d.
    
    Reason for revert: LayeredAPITest.GetSourceText is failing on Android CFI and WebKit Android (Nexus4):
    https://ci.chromium.org/buildbot/chromium.webkit/WebKit%20Android%20%28Nexus4%29/?limit=100
    https://ci.chromium.org/buildbot/chromium.memory/Android%20CFI/
    
    Original change's description:
    > [Layered API] Implement Layered API infrastructure behind a flag
    > 
    > This CL
    > 1. Maps std:x|y URLs to std:x (according to the draft spec [1]),
    > 2. Then maps to std-internal://x/index.js (not spec'ed [2])
    >    in GetInternalURL(),
    > 3. Then retrieves and serves the script contents for std-internal:
    >    URLs in GetSourceText() using ui::ResourceBundle()
    >    from blink_resources.pak.
    > 
    > The source JavaScript files for Layered APIs are placed under
    > third_party/blink/renderer/core/script/resources/layered_api
    > and bundled into blink_resources.pak.
    > 
    > This CL introduces std: and std-internal: schemes that are used in Blink side
    > (std: is user-facing while std-internal: is mostly invisible for users)
    > but not outside Blink.
    > 
    > [1] Spec:
    > https://github.com/drufball/layered-apis/blob/master/spec.md
    > [2] Implementation design doc:
    > https://docs.google.com/document/d/1V-WaCZQbBcQJRSYSYBb8Y6p0DOdDpiNDSmD41ui_73s/edit
    > 
    > Bug: 829084
    > Change-Id: I61fc0a2f17d937db6e859303ab8a84e4b2b87850
    > Reviewed-on: https://chromium-review.googlesource.com/1013321
    > Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#555524}
    
    TBR=kinuko@chromium.org,thakis@chromium.org,hiroshige@chromium.org,kouhei@chromium.org
    
    Change-Id: I6e13216a99d4ed199f9a52d370f3cad35900f408
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 829084
    Reviewed-on: https://chromium-review.googlesource.com/1042065
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555695}

--- third_party/blink/renderer/core/loader/modulescript/module_tree_linker.h
+++ third_party/blink/renderer/core/loader/modulescript/module_tree_linker.h
@@ -114,7 +114,6 @@ class CORE_EXPORT ModuleTreeLinker final : public SingleModuleClient {
   size_t num_incomplete_fetches_ = 0;
 
 #if DCHECK_IS_ON()
-  KURL original_url_;
   KURL url_;
   bool root_is_inline_;
 
