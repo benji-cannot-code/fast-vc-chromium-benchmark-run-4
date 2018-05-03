dc5f2ea4009bb0c7ff661fad2d22cfa5268e1046
Paul Miller <paulmiller@google.com>
2018-05-03 17:51:33 +0000

    WebView: Pass channel to variations
    Since b66db7c43366e0dec86670ab698a7d116ae73075, WebView can now get the
    channel. Variations uses this to filter experiments.
    
    Stand-alone WebView will report "unknown" channel. Experiments which do
    not specify any channel will apply to stand-alone WebView; see
    CheckStudyChannel in study_filtering.cc.
    
    BUG=758290
    
    Change-Id: I6174b63c6995cb950e5ec349891ad579d24e7396
    Reviewed-on: https://chromium-review.googlesource.com/1041206
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Paul Miller <paulmiller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555802}

--- android_webview/browser/aw_variations_service_client.cc
+++ android_webview/browser/aw_variations_service_client.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/threading/thread_restrictions.h"
 #include "build/build_config.h"
+#include "components/version_info/android/channel_getter.h"
 #include "components/version_info/version_info.h"
 
 namespace android_webview {
@@ -46,9 +47,7 @@ AwVariationsServiceClient::GetNetworkTimeTracker() {
 }
 
 version_info::Channel AwVariationsServiceClient::GetChannel() {
-  // TODO(kmilka): Investigate the proper value to return here so experiments
-  // are correctly filtered.
-  return version_info::Channel::UNKNOWN;
+  return version_info::GetChannel();
 }
 
 bool AwVariationsServiceClient::OverridesRestrictParameter(
