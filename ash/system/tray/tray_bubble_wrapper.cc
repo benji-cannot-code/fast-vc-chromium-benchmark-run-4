2d30e9468ad1a9237788030d5c15ccd849f30952
Sammie Quon <sammiequon@google.com>
2018-05-03 16:11:28 +0000

    system: Allow keyboard navigation on user drop down menu on tray.
    Allows user drop down menu to activatble so that it can be cycled
    through.
    Add a custom focus search so the drop down menu closes after all items
    have been cycled through.
    Migrate Closure to OnceClosure.
    Make drop down menu a transient child of system bubble.
    
    Test: manual
    Bug: 799401
    Change-Id: I67af2bb3e58c88f6926ab65ecf565d69325d00b8
    Reviewed-on: https://chromium-review.googlesource.com/1038656
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555756}

--- ash/system/tray/tray_bubble_wrapper.cc
+++ ash/system/tray/tray_bubble_wrapper.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/window.h"
 #include "ui/views/bubble/tray_bubble_view.h"
 #include "ui/views/widget/widget.h"
+#include "ui/wm/core/transient_window_manager.h"
 #include "ui/wm/core/window_util.h"
 #include "ui/wm/public/activation_client.h"
 
@@ -44,6 +45,12 @@ TrayBubbleWrapper::~TrayBubbleWrapper() {
 
   tray_->tray_event_filter()->RemoveWrapper(this);
   if (bubble_widget_) {
+    auto* transient_manager = ::wm::TransientWindowManager::GetOrCreate(
+        bubble_widget_->GetNativeWindow());
+    if (transient_manager) {
+      for (auto* window : transient_manager->transient_children())
+        transient_manager->RemoveTransientChild(window);
+    }
     bubble_widget_->GetNativeWindow()->GetRootWindow()->RemoveObserver(this);
     bubble_widget_->RemoveObserver(this);
     bubble_widget_->Close();
