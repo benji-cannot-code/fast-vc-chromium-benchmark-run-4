7f69f43d11d295b3abe83c626f70c008d64a5ee3
Alexis Hetu <sugoi@google.com>
2018-05-03 18:54:27 +0000

    Mac EGL build fix
    Changes related to libsync caused a build failure on MacOS when EGL is enabled.
    
    Credit to zmo@ for this fix, which I cherry picked from:
    https://chromium-review.googlesource.com/c/chromium/src/+/1005017/19..20
    
    Bug: chromium:757974
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: Ie4b38b3d0c782fea4e12f5d99e2f50add94ebf84
    Reviewed-on: https://chromium-review.googlesource.com/1042488
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555832}

--- ui/gl/gl_fence.cc
+++ ui/gl/gl_fence.cc
@@ -19,7 +19,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/gl_fence_apple.h"
 #endif
 
-#if defined(USE_EGL) && defined(OS_POSIX) && !defined(OS_FUCHSIA)
+#if defined(USE_EGL) && defined(OS_POSIX) && !defined(OS_FUCHSIA) && \
+    !defined(OS_MACOSX)
+#define USE_GL_FENCE_ANDROID_NATIVE_FENCE_SYNC
 #include "ui/gl/gl_fence_android_native_fence_sync.h"
 #include "ui/gl/gl_surface_egl.h"
 #endif
@@ -93,7 +95,7 @@ void GLFence::Invalidate() {
 }
 
 bool GLFence::IsGpuFenceSupported() {
-#if defined(USE_EGL) && defined(OS_POSIX) && !defined(OS_FUCHSIA)
+#if defined(USE_GL_FENCE_ANDROID_NATIVE_FENCE_SYNC)
   return gl::GLSurfaceEGL::IsAndroidNativeFenceSyncSupported();
 #else
   return false;
@@ -106,7 +108,7 @@ std::unique_ptr<GLFence> GLFence::CreateFromGpuFence(
   DCHECK(IsGpuFenceSupported());
   switch (gpu_fence.GetGpuFenceHandle().type) {
     case gfx::GpuFenceHandleType::kAndroidNativeFenceSync:
-#if defined(USE_EGL) && defined(OS_POSIX) && !defined(OS_FUCHSIA)
+#if defined(USE_GL_FENCE_ANDROID_NATIVE_FENCE_SYNC)
       return GLFenceAndroidNativeFenceSync::CreateFromGpuFence(gpu_fence);
 #else
       NOTREACHED();
@@ -121,7 +123,7 @@ std::unique_ptr<GLFence> GLFence::CreateFromGpuFence(
 // static
 std::unique_ptr<GLFence> GLFence::CreateForGpuFence() {
   DCHECK(IsGpuFenceSupported());
-#if defined(USE_EGL) && defined(OS_POSIX) && !defined(OS_FUCHSIA)
+#if defined(USE_GL_FENCE_ANDROID_NATIVE_FENCE_SYNC)
   return GLFenceAndroidNativeFenceSync::CreateForGpuFence();
 #endif
   NOTREACHED();
