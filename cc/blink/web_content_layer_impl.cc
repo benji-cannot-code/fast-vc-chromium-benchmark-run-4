709b237fb7bc1908cb46b6f80da1ee60dbfced76
danakj <danakj@chromium.org>
2018-05-04 23:32:14 +0000

    Remove SetIsDrawable() from WebContentLayerImpl constructor.
    Each caller of CreateContentLayer() calls SetDrawsContent() explicitly
    already, which sets this same bool, so there is no need to call it in
    the constructor or add any other calls.
    
    R=pdr@chromium.org
    
    Bug: 838693
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Iec4c50d3334c733ac62f6756a3e40f392e1dd983
    Reviewed-on: https://chromium-review.googlesource.com/1044897
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556249}

--- cc/blink/web_content_layer_impl.cc
+++ cc/blink/web_content_layer_impl.cc
@@ -48,7 +48,6 @@ PaintingControlToWeb(
 WebContentLayerImpl::WebContentLayerImpl(blink::WebContentLayerClient* client)
     : client_(client) {
   layer_ = std::make_unique<WebLayerImpl>(PictureLayer::Create(this));
-  layer_->layer()->SetIsDrawable(true);
 }
 
 WebContentLayerImpl::~WebContentLayerImpl() {
