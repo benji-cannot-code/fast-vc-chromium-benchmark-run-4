0f28dfe7c331f7e8bfe091d7a7f90e2213ceb345
Eric Orth <ericorth@chromium.org>
2018-05-04 20:26:48 +0000

    Add network service methods to clear reporting cache.
    Created as two separate methods, one for reports and one for clients,
    to avoid polluting the interface with a data_type_mask as is used
    internally. Also, to avoid dealing with confusing difference between
    always-true filter and null filter when they should logically be
    equivalent, cleaned up ReportingCache to explicitly have two separate
    clear data methods, one that clears everything without filter and one
    that always assumes it has a valid filter.
    
    Bug: 824997
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: If8fc3698eb61efbdee566dbd55e649723ff1e3d8
    Reviewed-on: https://chromium-review.googlesource.com/1038524
    Commit-Queue: Eric Orth <ericorth@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Helen Li <xunjieli@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556177}

--- net/url_request/url_request_unittest.cc
+++ net/url_request/url_request_unittest.cc
@@ -7449,6 +7449,8 @@ class TestReportingService : public ReportingService {
     NOTIMPLEMENTED();
   }
 
+  void RemoveAllBrowsingData(int data_type_mask) override { NOTIMPLEMENTED(); }
+
   int GetUploadDepth(const URLRequest& request) override {
     NOTIMPLEMENTED();
     return 0;
