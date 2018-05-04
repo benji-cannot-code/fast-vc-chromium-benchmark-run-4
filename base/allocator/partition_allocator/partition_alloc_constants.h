8268e93c12d7e8e38a3618286ec468e41410acb9
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2018-05-04 12:49:58 +0000

    IWYU: Include <limits.h> for CHAR_BIT
    This fixes the build with libstdc++ after a4c197977f ("Shatter
    partition_alloc.h into smaller files").
    
        In file included from ../../base/allocator/partition_allocator/partition_bucket.h:11:0,
                         from ../../base/allocator/partition_allocator/partition_bucket.cc:5:
        ../../base/allocator/partition_allocator/partition_alloc_constants.h:140:53: error: ‘CHAR_BIT’ was not declared in this scope
         static const size_t kBitsPerSizeT = sizeof(void*) * CHAR_BIT;
                                                             ^~~~~~~~
        ../../base/allocator/partition_allocator/partition_alloc_constants.h:140:53: note: suggested alternative: ‘WCHAR_MIN’
         static const size_t kBitsPerSizeT = sizeof(void*) * CHAR_BIT;
                                                             ^~~~~~~~
                                                             WCHAR_MIN
    
    Bug: 819294
    Change-Id: Ie61cd7a1b55adf77e1f584bd5447ac11b6980b02
    Reviewed-on: https://chromium-review.googlesource.com/1043748
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Cr-Commit-Position: refs/heads/master@{#556034}

--- base/allocator/partition_allocator/partition_alloc_constants.h
+++ base/allocator/partition_allocator/partition_alloc_constants.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_ALLOCATOR_PARTITION_ALLOCATOR_PARTITION_ALLOC_CONSTANTS_H_
 #define BASE_ALLOCATOR_PARTITION_ALLOCATOR_PARTITION_ALLOC_CONSTANTS_H_
 
+#include <limits.h>
+
 #include "base/allocator/partition_allocator/page_allocator_constants.h"
 #include "base/bits.h"
 #include "base/logging.h"
