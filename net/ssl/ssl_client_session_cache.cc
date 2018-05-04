3cbfbe6884d48463242388aa3748cef252ba8936
David Benjamin <davidben@chromium.org>
2018-05-04 22:37:56 +0000

    Don't access SSL_SESSION directly.
    Change-Id: Ib4569a8a28883226ed65df37d43ae92ae0337803
    Reviewed-on: https://chromium-review.googlesource.com/1044896
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Commit-Queue: Steven Valdez <svaldez@chromium.org>
    Reviewed-by: Steven Valdez <svaldez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556222}

--- net/ssl/ssl_client_session_cache.cc
+++ net/ssl/ssl_client_session_cache.cc
@@ -121,7 +121,8 @@ void SSLClientSessionCache::DumpMemoryStats(
     for (const auto& session : pair.second.sessions) {
       if (!session)
         continue;
-      undeduped_cert_count += sk_CRYPTO_BUFFER_num(session->certs);
+      undeduped_cert_count += sk_CRYPTO_BUFFER_num(
+          SSL_SESSION_get0_peer_certificates(session.get()));
     }
   }
   // Use a flat_set here to avoid malloc upon insertion.
@@ -131,7 +132,8 @@ void SSLClientSessionCache::DumpMemoryStats(
     for (const auto& session : pair.second.sessions) {
       if (!session)
         continue;
-      for (const CRYPTO_BUFFER* cert : session->certs) {
+      for (const CRYPTO_BUFFER* cert :
+           SSL_SESSION_get0_peer_certificates(session.get())) {
         undeduped_cert_size += CRYPTO_BUFFER_len(cert);
         auto result = crypto_buffer_set.insert(cert);
         if (!result.second)
