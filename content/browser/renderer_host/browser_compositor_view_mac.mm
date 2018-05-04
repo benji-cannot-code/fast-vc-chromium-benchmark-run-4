65ede6bacb10954f960d0285af9f547958e78cda
Fady Samuel <fsamuel@chromium.org>
2018-05-04 04:08:27 +0000

    Surface Synchronization: Cleanup DelegatedFrameHost
    Surface synchronization is on in M66 stable for desktop platforms and
    so we can now safely deleted the old code path.
    
    Change-Id: Idbc4859cc01342dcbeca6a9c1b7a381202fe49e6
    Reviewed-on: https://chromium-review.googlesource.com/1040866
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Fady Samuel <fsamuel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555975}

--- content/browser/renderer_host/browser_compositor_view_mac.mm
+++ content/browser/renderer_host/browser_compositor_view_mac.mm
@@ -181,7 +181,7 @@ BrowserCompositorMac::BrowserCompositorMac(
 
   root_layer_.reset(new ui::Layer(ui::LAYER_SOLID_COLOR));
   delegated_frame_host_.reset(new DelegatedFrameHost(
-      frame_sink_id, this, features::IsSurfaceSynchronizationEnabled(),
+      frame_sink_id, this,
       base::FeatureList::IsEnabled(features::kVizDisplayCompositor),
       true /* should_register_frame_sink_id */));
 
@@ -465,17 +465,6 @@ SkColor BrowserCompositorMac::DelegatedFrameHostGetGutterColor() const {
   return client_->BrowserCompositorMacGetGutterColor();
 }
 
-bool BrowserCompositorMac::DelegatedFrameCanCreateResizeLock() const {
-  // Mac uses the RenderWidgetResizeHelper instead of a resize lock.
-  return false;
-}
-
-std::unique_ptr<CompositorResizeLock>
-BrowserCompositorMac::DelegatedFrameHostCreateResizeLock() {
-  NOTREACHED();
-  return nullptr;
-}
-
 void BrowserCompositorMac::OnFirstSurfaceActivation(
     const viz::SurfaceInfo& surface_info) {
   if (!recyclable_compositor_)
@@ -511,11 +500,6 @@ void BrowserCompositorMac::OnFirstSurfaceActivation(
 
 void BrowserCompositorMac::OnBeginFrame(base::TimeTicks frame_time) {
   client_->BrowserCompositorMacOnBeginFrame(frame_time);
-}
-
-bool BrowserCompositorMac::IsAutoResizeEnabled() const {
-  NOTREACHED();
-  return false;
 }
 
 void BrowserCompositorMac::OnFrameTokenChanged(uint32_t frame_token) {
