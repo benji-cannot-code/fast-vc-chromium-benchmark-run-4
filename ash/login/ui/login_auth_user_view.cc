f98d58d90a69334ff4d4aaa4826165f40f87d4eb
Wenzhao Zang <wzang@chromium.org>
2018-05-04 20:23:04 +0000

    cros: Update re-auth message UI
    1) Update the UI to be the same with the web-ui implementation.
    
    2) Add a button to lock_debug_view.cc|h for easy debugging.
    
    Bug: 836336
    Change-Id: I89143c7db149dea31bc6848de18e6ecc6386f98b
    Reviewed-on: https://chromium-review.googlesource.com/1044746
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Wenzhao (Colin) Zang <wzang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556174}

--- ash/login/ui/login_auth_user_view.cc
+++ ash/login/ui/login_auth_user_view.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/login/ui/non_accessible_view.h"
 #include "ash/login/ui/pin_keyboard_animation.h"
 #include "ash/public/cpp/login_constants.h"
+#include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "base/strings/utf_string_conversions.h"
@@ -27,6 +28,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/compositor/layer_animator.h"
 #include "ui/compositor/scoped_layer_animation_settings.h"
 #include "ui/gfx/interpolated_transform.h"
+#include "ui/gfx/paint_vector_icon.h"
+#include "ui/views/border.h"
 #include "ui/views/controls/button/label_button.h"
 #include "ui/views/layout/box_layout.h"
 #include "ui/views/layout/fill_layout.h"
@@ -159,25 +162,25 @@ LoginAuthUserView::LoginAuthUserView(const mojom::LoginUserInfoPtr& user,
       callbacks.on_easy_unlock_icon_hovered,
       callbacks.on_easy_unlock_icon_tapped);
 
-  // TODO(crbug.com/836336): Update the spec. Currently the text must be set in
-  // |UpdateForUser| because it's dependent on the user name.
-  online_sign_in_message_ = new views::LabelButton(this, base::string16()),
-  online_sign_in_message_->SetPaintToLayer();
-  online_sign_in_message_->SetTextSubpixelRenderingEnabled(false);
-  online_sign_in_message_->SetTextColor(views::Button::STATE_NORMAL,
-                                        kOnlineSignInMessageColor);
+  online_sign_in_message_ = new views::LabelButton(
+      this, base::UTF8ToUTF16(user->basic_user_info->display_name));
+  DecorateOnlineSignInMessage();
 
   SetPaintToLayer(ui::LayerType::LAYER_NOT_DRAWN);
 
   // Build layout.
+  auto* wrapped_password_view =
+      login_layout_util::WrapViewForPreferredSize(password_view_);
+  auto* wrapped_message_view =
+      login_layout_util::WrapViewForPreferredSize(online_sign_in_message_);
   auto* wrapped_user_view =
       login_layout_util::WrapViewForPreferredSize(user_view_);
   auto* wrapped_pin_view =
       login_layout_util::WrapViewForPreferredSize(pin_view_);
 
   // Add views in tabbing order; they are rendered in a different order below.
-  AddChildView(password_view_);
-  AddChildView(online_sign_in_message_);
+  AddChildView(wrapped_password_view);
+  AddChildView(wrapped_message_view);
   AddChildView(wrapped_pin_view);
   AddChildView(wrapped_user_view);
 
@@ -201,8 +204,8 @@ LoginAuthUserView::LoginAuthUserView(const mojom::LoginUserInfoPtr& user,
   add_padding(kDistanceFromTopOfBigUserViewToUserIconDp);
   add_view(wrapped_user_view);
   add_padding(kDistanceBetweenUserViewAndPasswordDp);
-  add_view(password_view_);
-  add_view(online_sign_in_message_);
+  add_view(wrapped_password_view);
+  add_view(wrapped_message_view);
   add_padding(kDistanceBetweenPasswordFieldAndPinKeyboard);
   add_view(wrapped_pin_view);
   add_padding(kDistanceFromPinKeyboardToBigUserViewBottom);
@@ -227,6 +230,7 @@ void LoginAuthUserView::SetAuthMethods(uint32_t auth_methods) {
 
   password_view_->SetEnabled(has_password);
   password_view_->SetFocusEnabledForChildViews(has_password);
+  password_view_->SetVisible(!force_online_sign_in);
   password_view_->layer()->SetOpacity(has_password ? 1 : 0);
 
   if (!had_password && has_password)
@@ -441,6 +445,23 @@ void LoginAuthUserView::OnOnlineSignInMessageTap() {
 
 bool LoginAuthUserView::HasAuthMethod(AuthMethods auth_method) const {
   return (auth_methods_ & auth_method) != 0;
+}
+
+void LoginAuthUserView::DecorateOnlineSignInMessage() {
+  online_sign_in_message_->SetPaintToLayer();
+  online_sign_in_message_->layer()->SetFillsBoundsOpaquely(false);
+  online_sign_in_message_->SetImage(
+      views::Button::STATE_NORMAL,
+      CreateVectorIcon(kLockScreenAlertIcon, kOnlineSignInMessageColor));
+  online_sign_in_message_->SetTextSubpixelRenderingEnabled(false);
+  online_sign_in_message_->SetTextColor(views::Button::STATE_NORMAL,
+                                        kOnlineSignInMessageColor);
+  online_sign_in_message_->SetTextColor(views::Button::STATE_HOVERED,
+                                        kOnlineSignInMessageColor);
+  online_sign_in_message_->SetTextColor(views::Button::STATE_PRESSED,
+                                        kOnlineSignInMessageColor);
+  online_sign_in_message_->SetBorder(
+      views::CreateEmptyBorder(gfx::Insets(9, 0)));
 }
 
 }  // namespace ash
