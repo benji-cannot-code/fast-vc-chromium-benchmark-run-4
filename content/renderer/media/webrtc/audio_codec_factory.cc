9c790e971e5bb9caba48db3b06d22883ac8a2b8f
Karl Wiberg <kwiberg@chromium.org>
2018-05-04 12:35:38 +0000

    Call WebRTC's audio codec factory methods with an AudioCodecPairId argument
    It's about to become mandatory.
    
    Bug: webrtc:9062
    Change-Id: I430f9af008bb1e41c435ac0bcb06fd45645d26b5
    Reviewed-on: https://chromium-review.googlesource.com/1044104
    Reviewed-by: Max Morin <maxmorin@chromium.org>
    Commit-Queue: Karl Wiberg <kwiberg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556031}

--- content/renderer/media/webrtc/audio_codec_factory.cc
+++ content/renderer/media/webrtc/audio_codec_factory.cc
@@ -42,8 +42,9 @@ struct NotAdvertisedEncoder {
   }
   static std::unique_ptr<webrtc::AudioEncoder> MakeAudioEncoder(
       const Config& config,
-      int payload_type) {
-    return T::MakeAudioEncoder(config, payload_type);
+      int payload_type,
+      rtc::Optional<webrtc::AudioCodecPairId> codec_pair_id) {
+    return T::MakeAudioEncoder(config, payload_type, codec_pair_id);
   }
 };
 
@@ -60,8 +61,9 @@ struct NotAdvertisedDecoder {
     // Don't advertise support for anything.
   }
   static std::unique_ptr<webrtc::AudioDecoder> MakeAudioDecoder(
-      const Config& config) {
-    return T::MakeAudioDecoder(config);
+      const Config& config,
+      rtc::Optional<webrtc::AudioCodecPairId> codec_pair_id) {
+    return T::MakeAudioDecoder(config, codec_pair_id);
   }
 };
 
