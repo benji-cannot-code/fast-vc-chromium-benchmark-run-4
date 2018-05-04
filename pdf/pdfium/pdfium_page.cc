1e554d446eedef948764040aab4a237a20af4496
Lei Zhang <thestig@chromium.org>
2018-05-04 17:50:10 +0000

    Use PDFium scopers in more places.
    Change-Id: I042880a6b33e52f88f31f2b4c1dce44ad71677f1
    Reviewed-on: https://chromium-review.googlesource.com/1043653
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556108}

--- pdf/pdfium/pdfium_page.cc
+++ pdf/pdfium/pdfium_page.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "pdf/pdfium/pdfium_api_string_buffer_adapter.h"
 #include "pdf/pdfium/pdfium_engine.h"
 #include "printing/units.h"
+#include "third_party/pdfium/public/cpp/fpdf_scopers.h"
 #include "third_party/pdfium/public/fpdf_annot.h"
 
 using printing::ConvertUnitDouble;
@@ -603,10 +604,9 @@ const PDFEngine::PageFeatures* PDFiumPage::GetPageFeatures() {
   page_features_.index = index_;
   int annotation_count = FPDFPage_GetAnnotCount(page);
   for (int i = 0; i < annotation_count; ++i) {
-    FPDF_ANNOTATION annotation = FPDFPage_GetAnnot(page, i);
-    FPDF_ANNOTATION_SUBTYPE subtype = FPDFAnnot_GetSubtype(annotation);
+    ScopedFPDFAnnotation annotation(FPDFPage_GetAnnot(page, i));
+    FPDF_ANNOTATION_SUBTYPE subtype = FPDFAnnot_GetSubtype(annotation.get());
     page_features_.annotation_types.insert(subtype);
-    FPDFPage_CloseAnnot(annotation);
   }
 
   return &page_features_;
