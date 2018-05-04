f5fd0a65e38d2948ea44b98b294dcc0688592444
Miguel Casas <mcasas@chromium.org>
2018-05-04 15:50:13 +0000

    blink canvas: some cosmetic cleanups
    This CL does a bit of cleanup I found around while reading the code:
    - Forward-declares CanvasDrawListener.
    - Reduces a verbose comment.
    - Removes _ from a few internal class names (they landed like this
     in crrev.com/c/728340 but usually Chrome derived classes don't
     use _.
    - Uses early-break in CanvasResourceProvider::Create() to avoid deep
     indents and use {} for if statements with large conditionals.
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Ib1faf4a2d334e0a9b108c9fa02ebfbeffa0ea8c3
    Reviewed-on: https://chromium-review.googlesource.com/1042905
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556070}

--- third_party/blink/renderer/core/html/canvas/html_canvas_element.cc
+++ third_party/blink/renderer/core/html/canvas/html_canvas_element.cc
@@ -53,6 +53,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/use_counter.h"
 #include "third_party/blink/renderer/core/html/canvas/canvas_async_blob_creator.h"
 #include "third_party/blink/renderer/core/html/canvas/canvas_context_creation_attributes_core.h"
+#include "third_party/blink/renderer/core/html/canvas/canvas_draw_listener.h"
 #include "third_party/blink/renderer/core/html/canvas/canvas_font_cache.h"
 #include "third_party/blink/renderer/core/html/canvas/canvas_rendering_context.h"
 #include "third_party/blink/renderer/core/html/canvas/canvas_rendering_context_factory.h"
--- third_party/blink/renderer/core/html/canvas/html_canvas_element.h
+++ third_party/blink/renderer/core/html/canvas/html_canvas_element.h
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/dom/context_lifecycle_observer.h"
 #include "third_party/blink/renderer/core/dom/document.h"
-#include "third_party/blink/renderer/core/html/canvas/canvas_draw_listener.h"
 #include "third_party/blink/renderer/core/html/canvas/canvas_image_source.h"
 #include "third_party/blink/renderer/core/html/canvas/canvas_rendering_context_host.h"
 #include "third_party/blink/renderer/core/html/html_element.h"
@@ -63,6 +62,7 @@ namespace blink {
 class Canvas2DLayerBridge;
 class CanvasColorParams;
 class CanvasContextCreationAttributesCore;
+class CanvasDrawListener;
 class CanvasRenderingContext;
 class CanvasRenderingContextFactory;
 class GraphicsContext;
