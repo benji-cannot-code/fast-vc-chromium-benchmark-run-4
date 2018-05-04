055745e664766df84780d8904997e6d82278edda
edchin <edchin@chromium.org>
2018-05-04 16:58:49 +0000

    [ios] Replace recently added DCHECK with TODO to fix underlying issue
    A DCHECK was recently added that revealed that the
    tab grid cell is nil sometimes in |-cellForItemAtIndexPath:|.
    The cell should never be nil in this method.
    This is an issue that was previously went unnoticed, but
    adding a DCHECK unsurfaced this existing issue.
    
    This CL removes that DCHECK since it was recently added.
    It is replaced with a TODO so that the underlying issue can be fixed.
    
    Bug: 839892
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I85e486786978f992e2b67a7c67b626fbfe97d898
    Reviewed-on: https://chromium-review.googlesource.com/1044506
    Reviewed-by: edchin <edchin@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: edchin <edchin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556089}

--- ios/chrome/browser/ui/tab_grid/grid/grid_view_controller.mm
+++ ios/chrome/browser/ui/tab_grid/grid/grid_view_controller.mm
@@ -333,6 +333,7 @@ NSIndexPath* CreateIndexPath(NSInteger index) {
     return;
   GridCell* cell = base::mac::ObjCCastStrict<GridCell>(
       [self.collectionView cellForItemAtIndexPath:CreateIndexPath(index)]);
+  // TODO(crbug.com/839892) : Cell is sometimes nil, but should not be.
   [self configureCell:cell withItem:item];
 }
 
@@ -395,7 +396,7 @@ NSIndexPath* CreateIndexPath(NSInteger index) {
 // this view controller's theme. This view controller becomes the delegate for
 // the cell.
 - (void)configureCell:(GridCell*)cell withItem:(GridItem*)item {
-  DCHECK(cell);
+  // TODO(crbug.com/839892) : Cell is sometimes nil. Add DCHECK(cell) here.
   DCHECK(item);
   cell.delegate = self;
   cell.theme = self.theme;
