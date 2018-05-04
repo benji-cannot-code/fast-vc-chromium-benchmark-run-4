69d0555e7b8d7d7d737f4c6ef6ad38c2acf41204
Stefan Zager <szager@chromium.org>
2018-05-04 18:15:03 +0000

    Add profiling interface to chrome.gpuBenchmarking
    startProfiling and stopProfiling allow a developer to generate a cpu profile
    of the renderer process over an arbitrary time slice.  For example, to profile
    scrolling performance from devtools:
    
    > chrome.gpuBenchmarking.startProfiling("scrolling-profile.pb")
    > chrome.gpuBenchmarking.smoothScrollBy(
        (document.scrollingElement.scrollHeight
         - document.scrollingElement.clientHeight),
        () => { chrome.gpuBenchmarking.stopProfiling() })
    
    R=dtapuska@chromium.org,bokan@chromium.org
    
    Change-Id: I70a00f91a6b8f0241be4426bec167cfbc3fc5ec2
    Reviewed-on: https://chromium-review.googlesource.com/1042871
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Reviewed-by: enne <enne@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556121}

--- content/renderer/gpu/gpu_benchmarking_extension.cc
+++ content/renderer/gpu/gpu_benchmarking_extension.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/base64.h"
 #include "base/command_line.h"
+#include "base/debug/profiler.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/macros.h"
@@ -551,7 +552,9 @@ gin::ObjectTemplateBuilder GpuBenchmarking::GetObjectTemplateBuilder(
       .SetMethod("hasGpuChannel", &GpuBenchmarking::HasGpuChannel)
       .SetMethod("hasGpuProcess", &GpuBenchmarking::HasGpuProcess)
       .SetMethod("getGpuDriverBugWorkarounds",
-                 &GpuBenchmarking::GetGpuDriverBugWorkarounds);
+                 &GpuBenchmarking::GetGpuDriverBugWorkarounds)
+      .SetMethod("startProfiling", &GpuBenchmarking::StartProfiling)
+      .SetMethod("stopProfiling", &GpuBenchmarking::StopProfiling);
 }
 
 void GpuBenchmarking::SetNeedsDisplayOnAllLayers() {
@@ -1074,6 +1077,23 @@ void GpuBenchmarking::GetGpuDriverBugWorkarounds(gin::Arguments* args) {
   v8::Local<v8::Value> result;
   if (gin::TryConvertToV8(args->isolate(), gpu_driver_bug_workarounds, &result))
     args->Return(result);
+}
+
+void GpuBenchmarking::StartProfiling(gin::Arguments* args) {
+  if (base::debug::BeingProfiled())
+    return;
+  std::string file_name;
+  if (!GetOptionalArg(args, &file_name))
+    return;
+  if (!file_name.length())
+    file_name = "profile.pb";
+  base::debug::StartProfiling(file_name);
+  base::debug::RestartProfilingAfterFork();
+}
+
+void GpuBenchmarking::StopProfiling() {
+  if (base::debug::BeingProfiled())
+    base::debug::StopProfiling();
 }
 
 }  // namespace content
