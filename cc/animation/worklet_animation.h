63d0cd306a9ded0662905552009f9883221fad24
Majid Valipour <majidvp@chromium.org>
2018-05-04 23:44:51 +0000

    [animation-worklet] Do not tick worklet animations on main thread
    We should not tick cc:WorkletAnimation on main thread. The local
    time is actually not updated on main thread so the produced value
    is incorrect and more importantly causes superfluous commits.
    
    
    Note: We may decide to avoid ticking all animations on main thread
    (See http://crbug.com/762717) in which case we can remove this.
    
    
    TEST: cc/animation/worklet_animation_unittest.cc
    Bug: 839555
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I3335bfd452cc284a585e84a87b8912207b843813
    Reviewed-on: https://chromium-review.googlesource.com/1042875
    Commit-Queue: Majid Valipour <majidvp@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556256}

--- cc/animation/worklet_animation.h
+++ cc/animation/worklet_animation.h
@@ -24,7 +24,8 @@ class CC_ANIMATION_EXPORT WorkletAnimation final
  public:
   WorkletAnimation(int id,
                    const std::string& name,
-                   std::unique_ptr<ScrollTimeline> scroll_timeline);
+                   std::unique_ptr<ScrollTimeline> scroll_timeline,
+                   bool is_controlling_instance);
   static scoped_refptr<WorkletAnimation> Create(
       int id,
       const std::string& name,
@@ -67,6 +68,8 @@ class CC_ANIMATION_EXPORT WorkletAnimation final
   base::TimeDelta local_time_;
 
   base::Optional<double> last_current_time_;
+
+  bool is_impl_instance_;
 };
 
 }  // namespace cc
