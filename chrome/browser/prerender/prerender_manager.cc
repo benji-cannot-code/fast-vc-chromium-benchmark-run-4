da23c76c07305c6d76f5f0b54750ee8de399c1cb
erikchen <erikchen@chromium.org>
2018-05-05 00:16:23 +0000

    Use unique_ptr in PrerenderManager::old_web_contents_list_.
    This CL is a refactor with no intended behavior change.
    
    Bug: 832879
    Change-Id: I4c86d936907595a631fb9e603c55af90292ff724
    Reviewed-on: https://chromium-review.googlesource.com/1044892
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556267}

--- chrome/browser/prerender/prerender_manager.cc
+++ chrome/browser/prerender/prerender_manager.cc
@@ -1096,11 +1096,6 @@ bool PrerenderManager::DoesRateLimitAllowPrerender(Origin origin) const {
 }
 
 void PrerenderManager::DeleteOldWebContents() {
-  for (WebContents* web_contents : old_web_contents_list_) {
-    // TODO(dominich): should we use Instant Unload Handler here?
-    // Or should |old_web_contents_list_| contain unique_ptrs?
-    delete web_contents;
-  }
   old_web_contents_list_.clear();
 }
 
@@ -1150,7 +1145,7 @@ void PrerenderManager::CleanUpOldNavigations(
 void PrerenderManager::ScheduleDeleteOldWebContents(
     std::unique_ptr<WebContents> tab,
     OnCloseWebContentsDeleter* deleter) {
-  old_web_contents_list_.push_back(tab.release());
+  old_web_contents_list_.push_back(std::move(tab));
   PostCleanupTask();
 
   if (!deleter)
