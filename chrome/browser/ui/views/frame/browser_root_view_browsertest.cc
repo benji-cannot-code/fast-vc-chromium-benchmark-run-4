0365e40c3e47b1b5f1d4e1b48afea037795f6a6b
sangwoo.ko <sangwoo108@chromium.org>
2018-05-05 05:57:11 +0000

    Allow plain strings to be dropped
    As we can convert plain strings into query url,
    We shouldn't block non-urls.
    
    Bug: 838794
    Change-Id: Iaceaf0627a95c11382a4dee72d62a7af2a4e78bd
    Reviewed-on: https://chromium-review.googlesource.com/1041367
    Commit-Queue: Sang Woo Ko <sangwoo108@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556310}

--- chrome/browser/ui/views/frame/browser_root_view_browsertest.cc
+++ chrome/browser/ui/views/frame/browser_root_view_browsertest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/frame/browser_root_view.h"
 
+#include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/views/scoped_macviews_browser_mode.h"
@@ -39,4 +40,16 @@ IN_PROC_BROWSER_TEST_F(BrowserRootViewBrowserTest, ClearDropInfo) {
   root_view->OnDragUpdated(event);
   root_view->OnPerformDrop(event);
   EXPECT_FALSE(root_view->drop_info_);
+}
+
+// Make sure plain string is droppable. http://crbug.com/838794
+IN_PROC_BROWSER_TEST_F(BrowserRootViewBrowserTest, PlainString) {
+  ui::OSExchangeData data;
+  data.SetString(base::ASCIIToUTF16("Plain string"));
+  ui::DropTargetEvent event(data, gfx::Point(), gfx::Point(),
+                            ui::DragDropTypes::DRAG_COPY);
+
+  BrowserRootView* root_view = browser_root_view();
+  EXPECT_NE(ui::DragDropTypes::DRAG_NONE, root_view->OnDragUpdated(event));
+  EXPECT_NE(ui::DragDropTypes::DRAG_NONE, root_view->OnPerformDrop(event));
 }
