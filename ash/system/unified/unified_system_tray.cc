c25c96fdb438e68074d0d153b9753de338c5ed12
Tetsui Ohkubo <tetsui@chromium.org>
2018-05-07 04:49:43 +0000

    Show UnifiedSystemTray button and hide old one.
    As described in the design doc, old SystemTray class is going to be replaced by
    UnifiedSystemTray class. Model variables of SystemTrayItems will be decoupled
    into SystemTrayModel, and tray icons will be implemented in UnifiedSystemTray by
    obseving the models.
    
    This is the initial CL to replace the old SystemTray button by new
    UnifiedSystemTray button. Old SystemTray still has unrelated logic e.g.
    notification generation, so at this point we still instantiate SystemTray and
    just set the visibility to false. When we finish refactoring to move all
    unrelated logic to outside SystemTray, we can stop instantiating it.
    
    Screenshot: http://screen/jVCtkJgKCXa
    Design doc: go/cros-qs-restyling
    
    TEST=manual
    BUG=836134
    
    Change-Id: I89e708ad239fb93aee5993fd10ace7fdeaab6673
    Reviewed-on: https://chromium-review.googlesource.com/1025495
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556378}

--- ash/system/unified/unified_system_tray.cc
+++ ash/system/unified/unified_system_tray.cc
@@ -5,8 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/system/unified/unified_system_tray.h"
 
+#include "ash/shell.h"
+#include "ash/system/date/date_view.h"
+#include "ash/system/model/system_tray_model.h"
 #include "ash/system/status_area_widget.h"
 #include "ash/system/tray/system_tray.h"
+#include "ash/system/tray/tray_container.h"
 #include "ash/system/unified/unified_system_tray_bubble.h"
 #include "ash/system/unified/unified_system_tray_model.h"
 #include "ash/system/web_notification/ash_popup_alignment_delegate.h"
@@ -97,11 +101,11 @@ UnifiedSystemTray::UnifiedSystemTray(Shelf* shelf)
     : TrayBackgroundView(shelf),
       ui_delegate_(std::make_unique<UiDelegate>(this)),
       model_(std::make_unique<UnifiedSystemTrayModel>()) {
-  // On the first step, features in the status area button are still provided by
-  // TrayViews in SystemTray.
-  // TODO(tetsui): Remove SystemTray from StatusAreaWidget and provide these
-  // features from UnifiedSystemTray.
-  SetVisible(false);
+  tray_container()->AddChildView(
+      new tray::TimeView(tray::TimeView::ClockLayout::HORIZONTAL_CLOCK,
+                         Shell::Get()->system_tray_model()->clock()));
+  SetInkDropMode(InkDropMode::ON);
+  SetVisible(true);
 }
 
 UnifiedSystemTray::~UnifiedSystemTray() = default;
@@ -141,14 +145,12 @@ void UnifiedSystemTray::ClickedOutsideBubble() {}
 
 void UnifiedSystemTray::ShowBubbleInternal(bool show_by_click) {
   bubble_ = std::make_unique<UnifiedSystemTrayBubble>(this, show_by_click);
-  // TODO(tetsui): Call its own SetIsActive. See the comment in the ctor.
-  shelf()->GetStatusAreaWidget()->system_tray()->SetIsActive(true);
+  SetIsActive(true);
 }
 
 void UnifiedSystemTray::HideBubbleInternal() {
   bubble_.reset();
-  // TODO(tetsui): Call its own SetIsActive. See the comment in the ctor.
-  shelf()->GetStatusAreaWidget()->system_tray()->SetIsActive(false);
+  SetIsActive(false);
 }
 
 }  // namespace ash
