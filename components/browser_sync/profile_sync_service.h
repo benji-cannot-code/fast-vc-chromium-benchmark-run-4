08a744f20f40f7beba6dc8f935b42b6b4a7a08d4
Marc Treib <treib@chromium.org>
2018-05-07 11:06:27 +0000

    Cleanup: Remove ProfileSyncService::GetModelSafeRoutingInfo
    It's unused.
    
    Bug: 839834
    Change-Id: Ic354b16a5534aab8ce89c036b8a81a2ceeb3ae60
    Reviewed-on: https://chromium-review.googlesource.com/1046672
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556412}

--- components/browser_sync/profile_sync_service.h
+++ components/browser_sync/profile_sync_service.h
@@ -467,9 +467,6 @@ class ProfileSyncService : public syncer::SyncServiceBase,
   // Used by tests to inspect the OAuth2 access tokens used by PSS.
   std::string GetAccessTokenForTest() const;
 
-  // TODO(sync): This is only used in tests.  Can we remove it?
-  void GetModelSafeRoutingInfo(syncer::ModelSafeRoutingInfo* out) const;
-
   // SyncPrefObserver implementation.
   void OnSyncManagedPrefChange(bool is_sync_managed) override;
 
