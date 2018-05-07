693da12b7b6edef5dd8d4f506722b9e33570e336
Patti <patricialor@chromium.org>
2018-05-07 02:56:32 +0000

    Settings: Create a new element for All Site / site list items.
    Currently, All Site list entries are not reuseable. These will eventually become
    entries that represent an ETLD+1 site, with a list of origins as its children,
    so factor out the existing entries into a separate Polymer element. This is
    needed as they will also in future be used as site-list entries.
    
    This patch also fixes the UI in All Sites to have entries on a single line, when
    previously the icon, origin and arrow displayed were misaligned.
    
    Bug: 835712
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I66315496599f8c8d04309048ca897585a3bb431c
    Reviewed-on: https://chromium-review.googlesource.com/1041628
    Commit-Queue: Patti <patricialor@chromium.org>
    Reviewed-by: Dave Schuyler <dschuyler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556370}

---
+++ chrome/browser/resources/settings/site_settings/site_entry.js
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview
+ * 'site-entry' is an element representing a single ETLD+1 site entity.
+ */
+
+Polymer({
+  is: 'site-entry',
+
+  behaviors: [SiteSettingsBehavior],
+
+  properties: {
+    /**
+     * TODO(https://crbug.com/835712): This should be an object representing a
+     * ETLD+1 site.
+     */
+    site: Object,
+  },
+
+  /** @override */
+  ready: function() {},
+
+  /**
+   * A handler for selecting a site (by clicking on the origin).
+   * @param {!{model: !{item: !SiteException}}} event
+   * @private
+   */
+  onOriginTap_: function(event) {
+    settings.navigateTo(
+        settings.routes.SITE_SETTINGS_SITE_DETAILS,
+        new URLSearchParams('site=' + this.site.origin));
+  },
+
+});
