3e11ec48dd890ca8fb0f3cdb4f1dd79df2ce7384
Fady Samuel <fsamuel@chromium.org>
2018-05-07 14:19:43 +0000

    Surface Synchronization: Fix Mac regression
    After Visual Properties ACK (Resize ACK) was moved to the
    RenderFrameMetadataObserverClient interface, Mac web content no longer
    stuck to the edge of the window. This happened because WindowResizeHelper
    didn't see RenderFrameMetadata tasks and so did not schedule them until
    AFTER the resize deadline hit.
    
    This CL moves RenderFrameMetadataObserverClient tasks to the
    WindowResizeHelper's task_runner thereby fixing resize on Mac.
    
    Bug: 840197, 672962
    Change-Id: If4f8a766a2322f1208b6ffccca89c5181f96bf8f
    Reviewed-on: https://chromium-review.googlesource.com/1046003
    Reviewed-by: ccameron <ccameron@chromium.org>
    Commit-Queue: Fady Samuel <fsamuel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556432}

--- content/browser/renderer_host/render_frame_metadata_provider_impl.cc
+++ content/browser/renderer_host/render_frame_metadata_provider_impl.cc
@@ -11,8 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 RenderFrameMetadataProviderImpl::RenderFrameMetadataProviderImpl(
+    scoped_refptr<base::SingleThreadTaskRunner> task_runner,
     FrameTokenMessageQueue* frame_token_message_queue)
-    : frame_token_message_queue_(frame_token_message_queue),
+    : task_runner_(task_runner),
+      frame_token_message_queue_(frame_token_message_queue),
       render_frame_metadata_observer_client_binding_(this),
       weak_factory_(this) {}
 
@@ -31,8 +33,8 @@ void RenderFrameMetadataProviderImpl::Bind(
     mojom::RenderFrameMetadataObserverPtr observer) {
   render_frame_metadata_observer_ptr_ = std::move(observer);
   render_frame_metadata_observer_client_binding_.Close();
-  render_frame_metadata_observer_client_binding_.Bind(
-      std::move(client_request));
+  render_frame_metadata_observer_client_binding_.Bind(std::move(client_request),
+                                                      task_runner_);
 }
 
 void RenderFrameMetadataProviderImpl::ReportAllFrameSubmissionsForTesting(
