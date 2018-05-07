2444efc6774bd484aaf9375f048fcdae3a3d0359
Mustafa Çamurcu <camurcu@google.com>
2018-05-07 20:31:44 +0000

    [Windows/Ozone] enable egl/swiftshader for Windows platform
    This cl enables EGL and Swiftshader GL implementations for
    Windows/Ozone platform.
    
    Change-Id: I3f2a3527da54c76b9dee8b04114444d65286485a
    Reviewed-on: https://chromium-review.googlesource.com/1048043
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Mustafa Çamurcu <camurcu@google.com>
    Cr-Commit-Position: refs/heads/master@{#556555}

--- ui/ozone/common/egl_util.cc
+++ ui/ozone/common/egl_util.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_path.h"
 #include "base/path_service.h"
+#include "build/build_config.h"
 #include "ui/gl/egl_util.h"
 #include "ui/gl/gl_bindings.h"
 #include "ui/gl/gl_features.h"
@@ -15,17 +16,31 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ui {
 namespace {
 
+#if defined(OS_WIN)
+const base::FilePath::CharType kDefaultEglSoname[] =
+    FILE_PATH_LITERAL("libEGL.dll");
+const base::FilePath::CharType kDefaultGlesSoname[] =
+    FILE_PATH_LITERAL("libGLESv2.dll");
+#if BUILDFLAG(ENABLE_SWIFTSHADER)
+const base::FilePath::CharType kGLESv2SwiftShaderLibraryName[] =
+    FILE_PATH_LITERAL("libGLESv2.dll");
+const base::FilePath::CharType kEGLSwiftShaderLibraryName[] =
+    FILE_PATH_LITERAL("libEGL.dll");
+#endif  // BUILDFLAG(ENABLE_SWIFTSHADER)
+
+#else
 const base::FilePath::CharType kDefaultEglSoname[] =
     FILE_PATH_LITERAL("libEGL.so.1");
 const base::FilePath::CharType kDefaultGlesSoname[] =
     FILE_PATH_LITERAL("libGLESv2.so.2");
-
 #if BUILDFLAG(ENABLE_SWIFTSHADER)
 const base::FilePath::CharType kGLESv2SwiftShaderLibraryName[] =
     FILE_PATH_LITERAL("libGLESv2.so");
 const base::FilePath::CharType kEGLSwiftShaderLibraryName[] =
     FILE_PATH_LITERAL("libEGL.so");
-#endif
+#endif  // BUILDFLAG(ENABLE_SWIFTSHADER)
+
+#endif  // defined(OS_WIN)
 
 bool LoadEGLGLES2Bindings(const base::FilePath& egl_library_path,
                           const base::FilePath& gles_library_path) {
--- ui/ozone/common/gl_ozone_egl.h
+++ ui/ozone/common/gl_ozone_egl.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/macros.h"
+#include "third_party/khronos/EGL/eglplatform.h"
 #include "ui/gl/gl_implementation.h"
 #include "ui/ozone/public/gl_ozone.h"
 
@@ -43,7 +44,7 @@ class GLOzoneEGL : public GLOzone {
  protected:
   // Returns native platform display handle. This is used to obtain the EGL
   // display connection for the native display.
-  virtual intptr_t GetNativeDisplay() = 0;
+  virtual EGLNativeDisplayType GetNativeDisplay() = 0;
 
   // Sets up GL bindings for the native surface.
   virtual bool LoadGLES2Bindings(gl::GLImplementation implementation) = 0;
