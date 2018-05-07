60332aaa48c5cc567ba6211ee85e9a074fb80bdb
Luke Zielinski <lpz@chromium.org>
2018-05-07 15:11:45 +0000

    Add a new feature for suspicious site trigger quota.
    The previous approach of having a shared feature/param containing a
    csv of quota for all triggers makes it impossible to run more than
    one trigger trial at the same time (since the populations would be
    setting the same feature, even though the triggers run independently).
    More details at
    https://groups.google.com/a/google.com/forum/#!topic/chrome-safebrowsing-team/vx1yxXugwCY
    
    Bug: 817377
    Change-Id: I02903e2bb722a8d5557a684b2d6c3788338c81c5
    Reviewed-on: https://chromium-review.googlesource.com/1042377
    Reviewed-by: Jialiu Lin <jialiul@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Commit-Queue: Luke Z <lpz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556441}

--- components/safe_browsing/triggers/trigger_throttler.cc
+++ components/safe_browsing/triggers/trigger_throttler.cc
@@ -14,8 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/safe_browsing/features.h"
 
 namespace safe_browsing {
-const char kTriggerTypeAndQuotaParam[] = "trigger_type_and_quota_csv";
 const size_t kAdSamplerTriggerDefaultQuota = 10;
+const char kSuspiciousSiteTriggerQuotaParam[] = "suspicious_site_trigger_quota";
+const char kTriggerTypeAndQuotaParam[] = "trigger_type_and_quota_csv";
 
 namespace {
 const size_t kUnlimitedTriggerQuota = std::numeric_limits<size_t>::max();
@@ -36,13 +37,20 @@ class TriggerTypeIs {
 void ParseTriggerTypeAndQuotaParam(
     std::vector<TriggerTypeAndQuotaItem>* trigger_type_and_quota_list) {
   DCHECK(trigger_type_and_quota_list);
+  trigger_type_and_quota_list->clear();
+
+  // First, handle the trigger-specific features.
+  trigger_type_and_quota_list->push_back(std::make_pair(
+      TriggerType::SUSPICIOUS_SITE, base::GetFieldTrialParamByFeatureAsInt(
+                                        kSuspiciousSiteTriggerQuotaFeature,
+                                        kSuspiciousSiteTriggerQuotaParam, 0)));
+
   // If the feature is disabled we just use the default list. Otherwise the list
   // from the Finch param will be the one used.
   if (!base::FeatureList::IsEnabled(kTriggerThrottlerDailyQuotaFeature)) {
     return;
   }
 
-  trigger_type_and_quota_list->clear();
   const std::string& trigger_and_quota_csv_param =
       base::GetFieldTrialParamValueByFeature(kTriggerThrottlerDailyQuotaFeature,
                                              kTriggerTypeAndQuotaParam);
