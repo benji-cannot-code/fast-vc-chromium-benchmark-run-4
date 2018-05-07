bbc57f29e7ad31027c05568ccd931a375d6d61b3
Alex Kalugin <akalugin@yandex-team.ru>
2018-05-07 11:17:32 +0000

    Fix crash when no annotations collected.
    map is empty. xml_content string has no "<annotations>" substring
    because XmlWriter wrties annotations element as "<annotations/>",
    therefore call to
    xml_content.insert(xml_content.find("<annotations>"), kXmlComment);
    crashes.
    
    TrafficAnnotationExporter: :GenerateSerializedXML() crashes when archive_
    Change-Id: I669975e77e34965aeac02f721bc5f5f85dcfbe8c
    Reviewed-on: https://chromium-review.googlesource.com/1046673
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Commit-Queue: Kalugin Alexander <akalugin@yandex-team.ru>
    Cr-Commit-Position: refs/heads/master@{#556415}

--- tools/traffic_annotation/auditor/traffic_annotation_exporter.cc
+++ tools/traffic_annotation/auditor/traffic_annotation_exporter.cc
@@ -277,6 +277,7 @@ bool TrafficAnnotationExporter::UpdateAnnotations(
 std::string TrafficAnnotationExporter::GenerateSerializedXML() const {
   XmlWriter writer;
   writer.StartWriting();
+  writer.AppendElementContent(kXmlComment);
   writer.StartElement("annotations");
 
   for (const auto& item : archive_) {
@@ -335,11 +336,8 @@ std::string TrafficAnnotationExporter::GenerateSerializedXML() const {
   writer.EndElement();
 
   writer.StopWriting();
-  std::string xml_content = writer.GetWrittenString();
-  // Add comment before annotation tag (and after xml version).
-  xml_content.insert(xml_content.find("<annotations>"), kXmlComment);
 
-  return xml_content;
+  return writer.GetWrittenString();
 }
 
 bool TrafficAnnotationExporter::SaveAnnotationsXML() const {
