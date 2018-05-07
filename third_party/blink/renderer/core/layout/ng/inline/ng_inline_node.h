80885862f6b49334aa8a00197f86d0cb6ee9f3fc
Koji Ishii <kojii@chromium.org>
2018-05-07 13:17:50 +0000

    Remove unused class NGInlineItemRange
    This class was once used in NGLineBreaker but is no longer used.
    
    Bug: 636993
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: Ic6823fa745b2d3722c434f009b1b56cbf680dd2b
    Reviewed-on: https://chromium-review.googlesource.com/1046290
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556424}

--- third_party/blink/renderer/core/layout/ng/inline/ng_inline_node.h
+++ third_party/blink/renderer/core/layout/ng/inline/ng_inline_node.h
@@ -23,7 +23,6 @@ class LayoutBlockFlow;
 struct MinMaxSize;
 class NGConstraintSpace;
 class NGInlineItem;
-class NGInlineItemRange;
 using NGInlineItemsBuilder =
     NGInlineItemsBuilderTemplate<EmptyOffsetMappingBuilder>;
 struct NGInlineNodeData;
@@ -65,7 +64,6 @@ class CORE_EXPORT NGInlineNode : public NGLayoutInputNode {
   }
 
   const Vector<NGInlineItem>& Items(bool is_first_line = false) const;
-  NGInlineItemRange Items(unsigned start_index, unsigned end_index);
 
   // Returns the DOM to text content offset mapping of this block. If it is not
   // computed before, compute and store it in NGInlineNodeData.
