8b42dc3b45772251f106390ab6dfbbba64f25ddd
Tarun Bansal <tbansal@chromium.org>
2018-05-07 19:47:51 +0000

    Add web platform tests for the new client hints
    Add tests for "rtt", "downlink" and "ect".
    
    Bug: 835333
    Change-Id: If54c01612f10e96f7b37868e19b01a7ff3266d43
    Reviewed-on: https://chromium-review.googlesource.com/1045917
    Reviewed-by: Robert Ma <robertma@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556532}

--- third_party/WebKit/LayoutTests/external/wpt/client-hints/echo_client_hints_received.py
+++ third_party/WebKit/LayoutTests/external/wpt/client-hints/echo_client_hints_received.py
@@ -1,8 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 def main(request, response):
     """
-    Simple handler that sets a response header based on if device-memory
-    request header was received or not.
+    Simple handler that sets a response header based on if which client hint
+    request headers were received.
     """
 
     response.headers.append("Access-Control-Allow-Origin", "*")
@@ -13,3 +13,9 @@ def main(request, response):
             response.headers.set("dpr-received", "true")
     if "viewport-width" in request.headers:
             response.headers.set("viewport-width-received", "true")
+    if "rtt" in request.headers:
+            response.headers.set("rtt-received", "true")
+    if "downlink" in request.headers:
+            response.headers.set("downlink-received", "true")
+    if "ect" in request.headers:
+            response.headers.set("ect-received", "true")
