68ca635a7feb7157b2c0ad2e90cd88a93b48acde
edchin <edchin@chromium.org>
2018-05-07 16:57:32 +0000

    [ios] Add undo close all egtest
    Previously, an alert was presented as a stub to the undo close all
    action.
    
    This CL updates the egtest to check that the undo operation actually
    worked, now that the undo close all operation is fully implemented.
    There already exists more extensive unittesting on the undo close all
    operation.
    
    Bug: 804569
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: If30ec4fb457ccfad259f8b79a5a45e9db43a9a1e
    Reviewed-on: https://chromium-review.googlesource.com/1045227
    Reviewed-by: edchin <edchin@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: edchin <edchin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556473}

--- ios/chrome/browser/ui/tab_grid/tab_grid_egtest.mm
+++ ios/chrome/browser/ui/tab_grid/tab_grid_egtest.mm
@@ -69,10 +69,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [[EarlGrey
       selectElementWithMatcher:chrome_test_util::TabGridUndoCloseAllButton()]
       performAction:grey_tap()];
-  // TODO(crbug.com/804567) : Implement Undo Close All and replace this alert
-  // check with tabs that have been previously closed.
-  [[EarlGrey selectElementWithMatcher:grey_text(@"OK")]
-      performAction:grey_tap()];
+  [[EarlGrey selectElementWithMatcher:chrome_test_util::TabGridCellAtIndex(0)]
+      assertWithMatcher:grey_sufficientlyVisible()];
   [[EarlGrey selectElementWithMatcher:chrome_test_util::TabGridCloseAllButton()]
       assertWithMatcher:grey_sufficientlyVisible()];
 }
