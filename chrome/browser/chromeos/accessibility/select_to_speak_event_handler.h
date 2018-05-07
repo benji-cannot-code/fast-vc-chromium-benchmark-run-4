7de9e6aeb630f81e6f380c33ac5314d7e0997b5d
Katie D <katie@chromium.org>
2018-05-07 17:49:16 +0000

    Select-to-Speak handles multiple touch events smoothly.
    Now that Select-to-Speak selection can happen using touch events, the
    Select-to-Speak event handler must ignore extra touch events when one pointer
    is already being tracked.
    
    Bug: 753018
    Change-Id: I5d108de51d1be1ab0c4e416ae8cc314191da7c62
    Reviewed-on: https://chromium-review.googlesource.com/1043153
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556491}

--- chrome/browser/chromeos/accessibility/select_to_speak_event_handler.h
+++ chrome/browser/chromeos/accessibility/select_to_speak_event_handler.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/macros.h"
+#include "ui/events/event.h"
 #include "ui/events/event_handler.h"
 
 namespace chromeos {
@@ -109,6 +110,8 @@ class SelectToSpeakEventHandler : public ui::EventHandler {
   };
 
   State state_ = INACTIVE;
+
+  ui::PointerId touch_id_ = ui::PointerDetails::kUnknownPointerId;
 
   SelectToSpeakEventDelegateForTesting* event_delegate_for_testing_ = nullptr;
 
