55bedd8a1ec871d5a59cd59d7ee35e03c43abac3
Marina Ciocea <marinaciocea@chromium.org>
2018-05-07 07:54:23 +0000

    Add UI thread KeyboardMicRegistration implementation.
    Used by AudioInputStreamBroker (introduced in dependent CL) instead of
    AudioInputDeviceManager::KeyboardMicRegistration implementation, in order to avoid thread jumps and
    to keep broker interface simple.
    
    AudioInputDeviceManager::KeyboardMicRegistration will be removed after switching to audio service
    input streams.
    
    Bug: 828868
    Change-Id: I41aac3e01faeb0d4d47f98726bc069640fb346e3
    Reviewed-on: https://chromium-review.googlesource.com/1044216
    Commit-Queue: Marina Ciocea <marinaciocea@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Max Morin <maxmorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556390}

---
+++ content/browser/media/keyboard_mic_registration.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_BROWSER_MEDIA_KEYBOARD_MIC_REGISTRATION_H_
+#define CONTENT_BROWSER_MEDIA_KEYBOARD_MIC_REGISTRATION_H_
+
+#include "base/macros.h"
+
+namespace content {
+
+// Chrome OS keyboard mic stream registration. Used on UI thread only and owned
+// by BrowserMainLoop; instance must be obtained through
+// BrowserMainLoop::keyboard_mic_registration().
+class KeyboardMicRegistration {
+ public:
+  KeyboardMicRegistration();
+  ~KeyboardMicRegistration();
+
+  void Register();
+  void Deregister();
+
+ private:
+  int register_count_ = 0;
+
+  DISALLOW_COPY_AND_ASSIGN(KeyboardMicRegistration);
+};
+
+}  // namespace content
+
+#endif  // CONTENT_BROWSER_MEDIA_KEYBOARD_MIC_REGISTRATION_H_
