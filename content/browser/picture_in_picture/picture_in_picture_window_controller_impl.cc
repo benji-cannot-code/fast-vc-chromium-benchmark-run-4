209386f1aaf73eed8a0fc34254b5bd7f80b1da5c
Jennifer Apacible <apacible@chromium.org>
2018-05-08 23:46:23 +0000

    [Picture in Picture] Support PiPing multiple videos on the same tab.
    Currently, only one video can be gracefully sent to Picture-in-Picture
    mode from each initiator content::WebContents. Media controls do not
    work as they are only set once. This change allows us to enter
    Picture-in-Picture from more than one video on the same initiator,
    though one at a time.
    
    Another patch handles pausing the previously PiP'd video.
    
    BUG: 823172 726619
    Change-Id: Ie9eae108c5c8ce902458aa520b6553042096fb7a
    Reviewed-on: https://chromium-review.googlesource.com/1044773
    Commit-Queue: apacible <apacible@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557012}

--- content/browser/picture_in_picture/picture_in_picture_window_controller_impl.cc
+++ content/browser/picture_in_picture/picture_in_picture_window_controller_impl.cc
@@ -58,8 +58,6 @@ PictureInPictureWindowControllerImpl::PictureInPictureWindowControllerImpl(
 
   media_web_contents_observer_ = static_cast<WebContentsImpl* const>(initiator_)
                                      ->media_web_contents_observer();
-  media_player_id_ =
-      media_web_contents_observer_->GetPictureInPictureVideoMediaPlayerId();
 
   window_ =
       GetContentClient()->browser()->CreateWindowForPictureInPicture(this);
@@ -88,6 +86,13 @@ void PictureInPictureWindowControllerImpl::EmbedSurface(
   DCHECK(window_);
   DCHECK(surface_id.is_valid());
   surface_id_ = surface_id;
+
+  // Update the media player id in step with the video surface id. If the
+  // surface id was updated for the same video, this is a no-op. This could
+  // be updated for a different video if another media player on the same
+  // |initiator_| enters Picture-in-Picture mode.
+  media_player_id_ =
+      media_web_contents_observer_->GetPictureInPictureVideoMediaPlayerId();
 
   window_->UpdateVideoSize(natural_size);
 
