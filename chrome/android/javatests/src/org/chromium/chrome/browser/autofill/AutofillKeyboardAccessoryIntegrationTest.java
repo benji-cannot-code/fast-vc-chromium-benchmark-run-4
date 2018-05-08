527ebe971a46b71688ceb66c93e6869db802c988
Friedrich Horschig <fhorschig@chromium.org>
2018-05-08 15:06:21 +0000

    Reactivate deflaked keyboard accessory tests
    After having landed https://crrev.com/c/1028230, the problems of the
    linked bug seem to have gone (as assumed).
    
    I removed the Disabling and RetryOnFailure annotations to test on 3
    devices (one physical, two emulator with different screen sizes and OS)
    and couldn't find a single broken test.
    Edit: I changed the font size recently (not finally) which seems to
    bother some bots - unrelated from the filed bug. Changed that back.
    
    This CL now reenables all the tests but keeps the RetryOnFailure
    annotation. That way, I can test whether the tests start failing on
    some bots again without making the sheriffs life harder.
    If the bots don't notice any flakiness, I will remove the
    RetryOnFailure annotation as well and mark the linked bug fixed.
    
    Bug: 838922
    Change-Id: I9781e6d820ac39e43435ad1529da817601967ed4
    Reviewed-on: https://chromium-review.googlesource.com/1044371
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Commit-Queue: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556798}

--- chrome/android/javatests/src/org/chromium/chrome/browser/autofill/AutofillKeyboardAccessoryIntegrationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/autofill/AutofillKeyboardAccessoryIntegrationTest.java
@@ -27,7 +27,6 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
 import org.chromium.base.test.util.RetryOnFailure;
@@ -128,7 +127,6 @@ public class AutofillKeyboardAccessoryIntegrationTest {
     @Test
     @MediumTest
     @Feature({"keyboard-accessory"})
-    @DisabledTest(message = "crbug.com/838922")
     public void testTapInputFieldShowsKeyboardAccessory()
             throws ExecutionException, InterruptedException, TimeoutException {
         loadTestPage(false);
@@ -147,7 +145,6 @@ public class AutofillKeyboardAccessoryIntegrationTest {
     @Test
     @MediumTest
     @Feature({"keyboard-accessory"})
-    @DisabledTest(message = "crbug.com/836027")
     public void testSwitchFieldsRescrollsKeyboardAccessory()
             throws ExecutionException, InterruptedException, TimeoutException {
         loadTestPage(false);
@@ -177,7 +174,6 @@ public class AutofillKeyboardAccessoryIntegrationTest {
     @MediumTest
     @Feature({"keyboard-accessory"})
     @MinAndroidSdkLevel(Build.VERSION_CODES.JELLY_BEAN_MR1)
-    @DisabledTest(message = "crbug.com/836027")
     public void testSwitchFieldsRescrollsKeyboardAccessoryRtl()
             throws ExecutionException, InterruptedException, TimeoutException {
         loadTestPage(true);
