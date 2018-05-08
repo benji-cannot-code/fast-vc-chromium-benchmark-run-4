ca697cecbfe7281c49094b239783596cd1d9ebe0
Nicolas Pena <npm@chromium.org>
2018-05-08 21:28:02 +0000

    Fix resource-timing.https.html test
    This CL changes resource-timing-worker.js to use a generated response.
    This means Chrome no longer passes the test.
    
    Change-Id: Ib983e4fba9e81ddbee7f9572b6a06b3c1825577a
    Reviewed-on: https://chromium-review.googlesource.com/1050383
    Commit-Queue: Nicolás Peña Moreno <npm@chromium.org>
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556964}

--- third_party/WebKit/LayoutTests/external/wpt/service-workers/service-worker/resources/resource-timing-worker.js
+++ third_party/WebKit/LayoutTests/external/wpt/service-workers/service-worker/resources/resource-timing-worker.js
@@ -1,6 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 self.addEventListener('fetch', function(event) {
-    if (event.request.url.indexOf('dummy.js') != -1) {
-      event.respondWith(fetch('empty.js'));
-    }
-  });
+  if (event.request.url.indexOf('dummy.js') != -1) {
+    event.respondWith(new Promise(resolve => {
+      // Slightly delay the response so we ensure we get a non-zero
+      // duration.
+      setTimeout(_ => resolve(new Response('// Empty javascript')), 50);
+    }));
+  }
+});
