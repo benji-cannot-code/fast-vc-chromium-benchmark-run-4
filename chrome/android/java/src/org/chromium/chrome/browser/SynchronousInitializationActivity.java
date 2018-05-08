ed9dcb8b6f62c8f2ae3a1650267740618e49c17f
Boris Sazonov <bsazonov@chromium.org>
2018-05-08 23:04:50 +0000

    [Android] Extend SynchronousInitializationActivity in signin activities
    This CL modifies AccountSigninActivity and SigninActivity to extend
    SynchronousInitializationActivity instead of duplicating initialization
    code. It also annotates SynchronousInitializationActivity.onCreate with
    @CallSuper.
    
    Bug: None
    Change-Id: I0889351e2520ce5192c45d3d3393714cc225c4df
    Reviewed-on: https://chromium-review.googlesource.com/1047765
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556999}

--- chrome/android/java/src/org/chromium/chrome/browser/SynchronousInitializationActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/SynchronousInitializationActivity.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser;
 
 import android.os.Bundle;
+import android.support.annotation.CallSuper;
 import android.support.v7.app.AppCompatActivity;
 
 import org.chromium.base.Log;
@@ -23,6 +24,7 @@ import org.chromium.chrome.browser.init.ChromeBrowserInitializer;
 public abstract class SynchronousInitializationActivity extends AppCompatActivity {
     private static final String TAG = "SyncInitActivity";
 
+    @CallSuper
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
