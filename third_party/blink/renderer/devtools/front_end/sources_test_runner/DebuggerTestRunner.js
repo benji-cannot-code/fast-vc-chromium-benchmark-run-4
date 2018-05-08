92d363827677b2d9137beb53f37aab0d7fd47390
Alexey Kozyatinskiy <kozyatinskiy@chromium.org>
2018-05-08 03:23:39 +0000

    [DevTools] fixed crash on stepIntoAsync fetch
    Network should not be intsrumented with JavaScript AsyncTask
    instrumentation since it does not trigger JS execution explicitly.
    
    R=pfeldman@chromium.org
    
    Bug: chromium:840502
    Change-Id: I07a939b7f583b0330fa40afb0326a40e58918547
    Reviewed-on: https://chromium-review.googlesource.com/1049107
    Commit-Queue: Aleksey Kozyatinskiy <kozyatinskiy@chromium.org>
    Commit-Queue: Pavel Feldman <pfeldman@chromium.org>
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556673}

--- third_party/blink/renderer/devtools/front_end/sources_test_runner/DebuggerTestRunner.js
+++ third_party/blink/renderer/devtools/front_end/sources_test_runner/DebuggerTestRunner.js
@@ -220,6 +220,12 @@ SourcesTestRunner.stepInto = function() {
   });
 };
 
+SourcesTestRunner.stepIntoAsync = function() {
+  Promise.resolve().then(function() {
+    UI.panels.sources._stepIntoAsync();
+  });
+};
+
 SourcesTestRunner.stepOut = function() {
   Promise.resolve().then(function() {
     UI.panels.sources._stepOut();
