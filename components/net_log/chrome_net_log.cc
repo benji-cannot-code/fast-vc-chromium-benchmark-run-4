8cc18cc0cc7a28bf6e5819826c07087a7e6322dd
Maks Orlovich <morlovich@chromium.org>
2018-05-08 22:28:46 +0000

    S13n: Port net-export to use network service rather than direct access to netlog
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo;master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Ia0dec7ee73750261b1e19a425ad2d00742962770
    Reviewed-on: https://chromium-review.googlesource.com/1016669
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Helen Li <xunjieli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556985}

--- components/net_log/chrome_net_log.cc
+++ components/net_log/chrome_net_log.cc
@@ -50,7 +50,7 @@ void ChromeNetLog::StartWritingToFile(
 
 NetExportFileWriter* ChromeNetLog::net_export_file_writer() {
   if (!net_export_file_writer_)
-    net_export_file_writer_ = base::WrapUnique(new NetExportFileWriter(this));
+    net_export_file_writer_ = base::WrapUnique(new NetExportFileWriter());
   return net_export_file_writer_.get();
 }
 
@@ -61,6 +61,18 @@ std::unique_ptr<base::Value> ChromeNetLog::GetConstants(
   std::unique_ptr<base::DictionaryValue> constants_dict =
       net::GetNetConstants();
   DCHECK(constants_dict);
+
+  auto platform_dict =
+      GetPlatformConstants(command_line_string, channel_string);
+  if (platform_dict)
+    constants_dict->MergeDictionary(platform_dict.get());
+  return constants_dict;
+}
+
+std::unique_ptr<base::DictionaryValue> ChromeNetLog::GetPlatformConstants(
+    const base::CommandLine::StringType& command_line_string,
+    const std::string& channel_string) {
+  auto constants_dict = std::make_unique<base::DictionaryValue>();
 
   // Add a dictionary with the version of the client and its command line
   // arguments.
@@ -85,7 +97,7 @@ std::unique_ptr<base::Value> ChromeNetLog::GetConstants(
   data_reduction_proxy::DataReductionProxyEventStore::AddConstants(
       constants_dict.get());
 
-  return std::move(constants_dict);
+  return constants_dict;
 }
 
 void ChromeNetLog::ShutDownBeforeTaskScheduler() {
