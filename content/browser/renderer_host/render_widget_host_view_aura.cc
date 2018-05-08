07d68d3b812106e6b8a5fd1364e2553bcb5367db
Blake O'Hare <blakeo@chromium.org>
2018-05-08 05:59:47 +0000

    Add a null check to GetClientSourceInfo()
    The result of GetFocusedFrame() can be null.
    I discovered this while using this value in a debug log statement, not
    from actual production code, so there is no known user-facing crash or
    bug at this point.
    
    Change-Id: Ie272aa67d2c0b9d60f62f670091e6ee3ac08b82d
    Reviewed-on: https://chromium-review.googlesource.com/1046505
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Blake O'Hare <blakeo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556705}

--- content/browser/renderer_host/render_widget_host_view_aura.cc
+++ content/browser/renderer_host/render_widget_host_view_aura.cc
@@ -1477,7 +1477,11 @@ void RenderWidgetHostViewAura::SetTextEditCommandForNextKeyEvent(
     ui::TextEditCommand command) {}
 
 const std::string& RenderWidgetHostViewAura::GetClientSourceInfo() const {
-  return GetFocusedFrame()->GetLastCommittedURL().spec();
+  RenderFrameHostImpl* frame = GetFocusedFrame();
+  if (frame) {
+    return frame->GetLastCommittedURL().spec();
+  }
+  return base::EmptyString();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
