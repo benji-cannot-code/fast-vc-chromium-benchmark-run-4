47208a7d85fe060175dfb877a628412bc4b194d9
Guido Urdaneta <guidou@chromium.org>
2018-05-08 11:57:59 +0000

    Disable flaky WebVrTransitionTest#testWindowRafStopsFiringWhilePresenting on Android
    TBR=bsheedy@chromium.org
    
    Bug: 840725
    Change-Id: Id47604a849bcfcbb51e33ddf94ffa7a539ecb1df
    Reviewed-on: https://chromium-review.googlesource.com/1049668
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556762}

--- chrome/android/javatests/src/org/chromium/chrome/browser/vr_shell/WebVrTransitionTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/vr_shell/WebVrTransitionTest.java
@@ -407,6 +407,7 @@ public class WebVrTransitionTest {
      */
     @Test
     @MediumTest
+    @DisabledTest(message = "crbug.com/840725")
     @VrActivityRestriction({VrActivityRestriction.SupportedActivity.ALL})
     @RetryOnFailure
     public void testWindowRafStopsFiringWhilePresenting() throws InterruptedException {
