07a6cea4ba40c74af8d7a4871a0e78977db06214
John Abd-El-Malek <jam@chromium.org>
2018-05-08 18:08:09 +0000

    Fix WebViewNewWindowInteractiveTest.NewWindow_WebRequestRemoveElement flake on Windows with network service.
    This passes all the time on Linux with network service, but fails most of the time on Windows. The problem is that due to timing differences with network service, |webview| is torn down while it still has its navigation in-progress. WebViewGuest::DidFinishNavigation then synthesizes a LoadAbort called and (I'm not sure why) onNewWindow gets called a second time.
    
    Bug: 598073
    Change-Id: I7ca2acd5e4ec7ddbfc25f94184d83e1abdb25545
    Reviewed-on: https://chromium-review.googlesource.com/1049011
    Reviewed-by: Fady Samuel <fsamuel@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556876}

--- chrome/test/data/extensions/platform_apps/web_view/newwindow/embedder.js
+++ chrome/test/data/extensions/platform_apps/web_view/newwindow/embedder.js
@@ -548,7 +548,7 @@ function testNewWindowWebRequestRemoveElement() {
       newwindow.contentWindow.onload = function(evt) {
         var newwebview =
             newwindow.contentWindow.document.querySelector('webview');
-        newwebview.request.onBeforeRequest.addListener(function(e) {
+        newwebview.request.onCompleted.addListener(function(e) {
           if (!calledWebRequestEvent) {
             calledWebRequestEvent = true;
             newwebview.parentElement.removeChild(newwebview);
