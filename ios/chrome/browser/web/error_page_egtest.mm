dac6a6f70a76b89d23fc5cc3341391e14af41392
Eugene But <eugenebut@google.com>
2018-05-08 19:17:00 +0000

    Ported ErrorPageTestCase to ios_web_inttest.
    This CL adds ErrorPageTest ios_web_inttest for error pages. These tests
    are faster than EG tests and can test is_post and is_off_the_record
    arguments passed to TestWebClient. ErrorPageTestCase exists, because
    it's not possible to test old implementation with ios_web_inttest
    infrastructure. ErrorPageTestCase will be removed when old error pages
    code is no longer supported. Tests for is_post and is_off_the_record
    arguments will be added in a separate CL.
    
    Bug: 725241
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Ic06e9613c6e69a488ac734922ffc691f124bd24b
    Reviewed-on: https://chromium-review.googlesource.com/1047951
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Danyao Wang <danyao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556915}

--- ios/chrome/browser/web/error_page_egtest.mm
+++ ios/chrome/browser/web/error_page_egtest.mm
@@ -94,6 +94,7 @@ NSString* GetNSErrorMessage() {
 
 // Sucessfully loads the page, goes back, stops the server, goes forward and
 // reloads.
+// TODO(crbug.com/840489): Remove this test.
 - (void)testGoForwardAfterServerIsDownAndReload {
   // First page loads sucessfully.
   [ChromeEarlGrey loadURL:self.testServer->GetURL("/echo")];
@@ -128,6 +129,7 @@ NSString* GetNSErrorMessage() {
 
 // Sucessfully loads the page, then loads the URL which fails to load, then
 // sucessfully goes back to the first page.
+// TODO(crbug.com/840489): Remove this test.
 - (void)testGoBackFromErrorPage {
   // First page loads sucessfully.
   [ChromeEarlGrey loadURL:self.testServer->GetURL("/echo")];
@@ -147,6 +149,7 @@ NSString* GetNSErrorMessage() {
 }
 
 // Loads the URL which redirects to unresponsive server.
+// TODO(crbug.com/840489): Remove this test.
 - (void)testRedirectToFailingURL {
   // No response leads to ERR_INTERNET_DISCONNECTED error.
   self.serverRespondsWithContent = NO;
@@ -161,6 +164,7 @@ NSString* GetNSErrorMessage() {
 
 // Loads the page with iframe, and that iframe fails to load. There should be no
 // error page if the main frame has sucessfully loaded.
+// TODO(crbug.com/840489): Remove this test.
 - (void)testErrorPageInIFrame {
   [ChromeEarlGrey loadURL:self.testServer->GetURL("/iframe?echo-query")];
   [ChromeEarlGrey
