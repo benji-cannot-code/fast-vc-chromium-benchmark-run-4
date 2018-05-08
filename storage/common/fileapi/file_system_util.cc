5cca65235395aaf3cab4c33ab01ab18211a1e825
Fabrice de Gans-Riberi <fdegans@chromium.org>
2018-05-08 20:41:50 +0000

    Fuchsia: Prepare for OS_POSIX removal in storage/
    This cleans up multi-platform code paths selection and prepares for
    the removal of OS_POSIX for the Fuchsia build.
    
    Bug: 836416
    Change-Id: I45af4ff9525a3129c077101ad4e365c79490299e
    Reviewed-on: https://chromium-review.googlesource.com/1033969
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Fabrice de Gans-Riberi <fdegans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556950}

--- storage/common/fileapi/file_system_util.cc
+++ storage/common/fileapi/file_system_util.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/strings/utf_string_conversions.h"
+#include "build/build_config.h"
 #include "net/base/escape.h"
 #include "net/base/net_errors.h"
 #include "storage/common/database/database_identifier.h"
@@ -325,7 +326,7 @@ std::string GetFileSystemTypeString(FileSystemType type) {
 std::string FilePathToString(const base::FilePath& file_path) {
 #if defined(OS_WIN)
   return base::UTF16ToUTF8(file_path.value());
-#elif defined(OS_POSIX)
+#elif defined(OS_POSIX) || defined(OS_FUCHSIA)
   return file_path.value();
 #endif
 }
@@ -333,7 +334,7 @@ std::string FilePathToString(const base::FilePath& file_path) {
 base::FilePath StringToFilePath(const std::string& file_path_string) {
 #if defined(OS_WIN)
   return base::FilePath(base::UTF8ToUTF16(file_path_string));
-#elif defined(OS_POSIX)
+#elif defined(OS_POSIX) || defined(OS_FUCHSIA)
   return base::FilePath(file_path_string);
 #endif
 }
