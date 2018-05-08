8ef5aa9ab85798344ab18e79a55a88577422aa0b
Renee Wright <rjwright@chromium.org>
2018-05-08 05:19:52 +0000

    Rename *VmConcierge methods to *Concierge
    Method names such as StartVmConcierge are confusing. Rename to
    StartConcierge etc.
    
    third_party/cros_system_api/dbus/debugd/dbus-constants.h still has
    
    const char kStartVmConcierge[] = "StartVmConcierge";
    const char kStopVmConcierge[] = "StopVmConcierge";
    
    as these need to match the method names in the service.
    
    Bug: 839206
    Change-Id: I48800e595a2756fd8bc8d35ca136dd73a825b08e
    Reviewed-on: https://chromium-review.googlesource.com/1041365
    Commit-Queue: Renée Wright <rjwright@chromium.org>
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556696}

--- chrome/browser/ui/app_list/crostini/crostini_installer_view.cc
+++ chrome/browser/ui/app_list/crostini/crostini_installer_view.cc
@@ -230,7 +230,7 @@ void CrostiniInstallerView::OnConciergeStarted(ConciergeClientResult result) {
         IDS_CROSTINI_INSTALLER_START_CONCIERGE_ERROR));
     return;
   }
-  VLOG(1) << "VmConcierge service started";
+  VLOG(1) << "Concierge service started";
   StepProgress();
 }
 
