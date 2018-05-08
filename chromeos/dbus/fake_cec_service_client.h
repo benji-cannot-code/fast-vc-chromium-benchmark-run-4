fb874804d1abd9c171fd4c9fb4529b1cc6499091
Felix Ekblom <felixe@chromium.org>
2018-05-08 08:31:28 +0000

    Add power state query method to CecServiceClient
    The new method will offer a crude way to query about the power state of
    connected HDMI CEC capable displays.
    
    BUG=b:78171027
    
    Change-Id: Ic37abea8f300d1d22a80368acbd3f129cc5e0187
    Reviewed-on: https://chromium-review.googlesource.com/1042575
    Reviewed-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Felix Ekblom <felixe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556727}

--- chromeos/dbus/fake_cec_service_client.h
+++ chromeos/dbus/fake_cec_service_client.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROMEOS_DBUS_FAKE_CEC_SERVICE_CLIENT_H_
 #define CHROMEOS_DBUS_FAKE_CEC_SERVICE_CLIENT_H_
 
+#include <vector>
+
 #include "base/macros.h"
 #include "chromeos/dbus/cec_service_client.h"
 
@@ -21,11 +23,20 @@ class CHROMEOS_EXPORT FakeCecServiceClient : public CecServiceClient {
   // CecServiceClient
   void SendStandBy() override;
   void SendWakeUp() override;
+  void QueryDisplayCecPowerState(
+      CecServiceClient::PowerStateCallback callback) override;
 
-  int stand_by_call_count();
-  int wake_up_call_count();
+  int stand_by_call_count() const { return stand_by_call_count_; }
+  int wake_up_call_count() const { return wake_up_call_count_; }
 
-  CurrentState current_state();
+  CurrentState last_set_state() const { return last_set_state_; }
+
+  void set_tv_power_states(const std::vector<PowerState>& power_states) {
+    tv_power_states_ = power_states;
+  }
+  const std::vector<PowerState>& tv_power_states() const {
+    return tv_power_states_;
+  }
 
  protected:
   void Init(dbus::Bus* bus) override;
@@ -33,7 +44,9 @@ class CHROMEOS_EXPORT FakeCecServiceClient : public CecServiceClient {
  private:
   int stand_by_call_count_ = 0;
   int wake_up_call_count_ = 0;
-  CurrentState current_state_ = kUndefined;
+  CurrentState last_set_state_ = kUndefined;
+
+  std::vector<PowerState> tv_power_states_;
 
   DISALLOW_COPY_AND_ASSIGN(FakeCecServiceClient);
 };
