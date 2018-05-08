6b4f9ec26a1b0a613998516231a03b03a45e86fb
Renee Wright <rjwright@chromium.org>
2018-05-08 05:31:53 +0000

    Remove dialog part of CrostiniPageTests & re-enable
    This test was failing in debug.
    
      onRemoveTap_: function(event) {
        this.showRemoveDialog_ = true;
        this.async(() => this.$$('#removeDialog').showModal());
      },
    
    in
    
    cs.chromium.org/chromium/src/chrome/browser/resources/settings/crostini_page/crostini_subpage.js
    
    always comes up with
    
    this.$$('#removeDialog').showModal == undefined
    
    in debug. I do not know why. But this dialog is being
    replace with a View dialog very soon anyway.
    
    Bug: 838097
    Change-Id: Id78b498ee9ce54849793eb9518bbb513a426f13d
    Reviewed-on: https://chromium-review.googlesource.com/1036710
    Commit-Queue: Renée Wright <rjwright@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556698}

--- chrome/test/data/webui/settings/cr_settings_browsertest.js
+++ chrome/test/data/webui/settings/cr_settings_browsertest.js
@@ -1777,13 +1777,7 @@ CrSettingsCrostiniPageTest.prototype = {
   ]),
 };
 
-GEN('#if defined(OS_CHROMEOS) && !defined(NDEBUG)');
-GEN('#define MAYBE_CROSTINI_All DISABLED_All');
-GEN('#else');
-GEN('#define MAYBE_CROSTINI_All All');
-GEN('#endif');
-
-TEST_F('CrSettingsCrostiniPageTest', 'MAYBE_CROSTINI_All', function() {
+TEST_F('CrSettingsCrostiniPageTest', 'All', function() {
   mocha.run();
 });
 
