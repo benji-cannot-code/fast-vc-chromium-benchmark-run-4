291edb5645efd10378b334ab4c17b32d58f9bb39
Fady Samuel <fsamuel@chromium.org>
2018-05-08 05:46:04 +0000

    Surface Synchronization DID_COMPLETE_RESIZE_OR_REPAINT => DID_UPDATE_VISUAL_PROPERTIES
    The name ResizeOrRepaint is a misnomer. This CL renames it to something
    more appropriate. This CL also renames the resize_ack_pending_ to
    visual_properties_ack_pending_.
    
    Bug: 672962
    Change-Id: Ibee6540617d0bcd4e5bf2f2492beb618aff19255
    TBR: sky@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/1048113
    Commit-Queue: Fady Samuel <fsamuel@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556701}

--- chrome/browser/sessions/session_restore_stats_collector.cc
+++ chrome/browser/sessions/session_restore_stats_collector.cc
@@ -138,7 +138,7 @@ void SessionRestoreStatsCollector::TrackTabs(
   if (tab_loader_stats_.tab_count == 0) {
     registrar_.Add(
         this,
-        content::NOTIFICATION_RENDER_WIDGET_HOST_DID_COMPLETE_RESIZE_OR_REPAINT,
+        content::NOTIFICATION_RENDER_WIDGET_HOST_DID_UPDATE_VISUAL_PROPERTIES,
         content::NotificationService::AllSources());
   }
 
@@ -273,7 +273,7 @@ void SessionRestoreStatsCollector::Observe(
       break;
     }
     case content::
-        NOTIFICATION_RENDER_WIDGET_HOST_DID_COMPLETE_RESIZE_OR_REPAINT: {
+        NOTIFICATION_RENDER_WIDGET_HOST_DID_UPDATE_VISUAL_PROPERTIES: {
       // This notification is across all tabs in the browser so notifications
       // will arrive for tabs that the collector is not explicitly tracking.
 
@@ -304,7 +304,7 @@ void SessionRestoreStatsCollector::Observe(
         registrar_.Remove(
             this,
             content::
-                NOTIFICATION_RENDER_WIDGET_HOST_DID_COMPLETE_RESIZE_OR_REPAINT,
+                NOTIFICATION_RENDER_WIDGET_HOST_DID_UPDATE_VISUAL_PROPERTIES,
             content::NotificationService::AllSources());
 
         // Remove any tabs that have loaded. These were only being kept around
@@ -371,7 +371,7 @@ void SessionRestoreStatsCollector::RemoveTab(NavigationController* tab) {
     got_first_paint_ = true;
     registrar_.Remove(
         this,
-        content::NOTIFICATION_RENDER_WIDGET_HOST_DID_COMPLETE_RESIZE_OR_REPAINT,
+        content::NOTIFICATION_RENDER_WIDGET_HOST_DID_UPDATE_VISUAL_PROPERTIES,
         content::NotificationService::AllSources());
   }
 }
