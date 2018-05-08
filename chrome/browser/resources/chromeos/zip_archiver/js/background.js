fe6b1a7a578ad9374b3b68961a58e830f6594710
Tatsuhisa Yamaguchi <yamaguchi@google.com>
2018-05-08 11:59:09 +0000

    Prevent runtime errors on suspend events.
    unpacker.app.onSuspend was removed at f878141 and no longer exists.
    https://chromium-review.googlesource.com/940742
    
    Bug: 839158,789073,803752
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I973b7728095cddcceab2e58c06ca07986a6bd9e6
    Reviewed-on: https://chromium-review.googlesource.com/1049417
    Commit-Queue: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Reviewed-by: Yuki Awano <yawano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556763}

--- chrome/browser/resources/chromeos/zip_archiver/js/background.js
+++ chrome/browser/resources/chromeos/zip_archiver/js/background.js
@@ -6,10 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 'use strict';
 
 function setupZipArchiver() {
-  // Save the state before suspending the event page, so we can resume it
-  // once new events arrive.
-  chrome.runtime.onSuspend.addListener(unpacker.app.onSuspend);
-
   chrome.fileSystemProvider.onUnmountRequested.addListener(
       unpacker.app.onUnmountRequested);
   chrome.fileSystemProvider.onGetMetadataRequested.addListener(
