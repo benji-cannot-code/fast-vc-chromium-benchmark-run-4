ae8254435fafb773da1ba75fb5e4b2e6bd21a6a8
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-05-08 04:57:50 +0000

    Import wpt@d9587de4ff827d75fefbd72f42e834078094202d
    Using wpt-import in Chromium 77db7fb7c874c5d8e4c3e49e03593fe7f186ad1a.
    With Chromium commits locally applied on WPT:
    8d1dda318f "Revert "Adds a test for basic WebRTC video codec conformance.""
    
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/17145
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    yhirano@chromium.org, mkwst@chromium.org, japhet@chromium.org:
      external/wpt/fetch
    
    TBR=raphael.kubo.da.costa@intel.com
    
    No-Export: true
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: I0aaea7e106de989a660cd820d618d4a7bb774486
    Reviewed-on: https://chromium-review.googlesource.com/1049029
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556691}

--- third_party/WebKit/LayoutTests/external/wpt/fetch/http-cache/resources/securedimage.py
+++ third_party/WebKit/LayoutTests/external/wpt/fetch/http-cache/resources/securedimage.py
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # -*- coding: utf-8 -
 
 def main(request, response):
-    image_url = str.replace(request.url, "http/resources/securedimage.py", "images/green.png")
+    image_url = str.replace(request.url, "fetch/http-cache/resources/securedimage.py", "images/green.png")
 
     if "authorization" not in request.headers:
         response.status = 401
--- third_party/WebKit/LayoutTests/external/wpt/http/refresh/README.md
+++
@@ -1,2 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-See `../../html/semantics/document-metadata/the-meta-element/pragma-directives/attr-meta-http-equiv-refresh/parsing.html` for more detailed parsing tests (shared with `<meta http-equiv=refresh>`).
