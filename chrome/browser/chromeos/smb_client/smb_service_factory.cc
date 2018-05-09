c186b9922f5c494712dd7783f87fc2e7a2d613ab
Bailey Berro <baileyberro@chromium.org>
2018-05-09 15:37:48 +0000

    Setup Kerberos in SmbService
    This change updates the SmbService setup flow so that setup is not run
    when SmbService is created on the LockScreen and adds a call to
    SetupKerberos for ChromAD users.
    
    Bug: chromium:757625
    Test: verified on local ActivceDirectory test network
    Change-Id: I36ca8f48368be33e740ee1128e8cc5cc23ca8e5d
    Reviewed-on: https://chromium-review.googlesource.com/1045130
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Bailey Berro <baileyberro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557193}

--- chrome/browser/chromeos/smb_client/smb_service_factory.cc
+++ chrome/browser/chromeos/smb_client/smb_service_factory.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/smb_client/smb_service_factory.h"
 
+#include "chrome/browser/chromeos/authpolicy/auth_policy_credentials_manager.h"
 #include "chrome/browser/chromeos/file_system_provider/service_factory.h"
 #include "chrome/browser/chromeos/smb_client/smb_service.h"
 #include "chrome/browser/profiles/incognito_helpers.h"
@@ -32,6 +33,7 @@ SmbServiceFactory::SmbServiceFactory()
           "SmbService",
           BrowserContextDependencyManager::GetInstance()) {
   DependsOn(file_system_provider::ServiceFactory::GetInstance());
+  DependsOn(AuthPolicyCredentialsManagerFactory::GetInstance());
 }
 
 SmbServiceFactory::~SmbServiceFactory() {}
