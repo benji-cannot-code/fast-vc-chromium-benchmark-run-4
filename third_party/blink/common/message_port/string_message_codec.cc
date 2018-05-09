b599985b9fed73473f34a8ca39e1190cb9e850b8
Marijn Kruisselbrink <mek@chromium.org>
2018-05-09 22:59:08 +0000

    Get rid of FrameMsg_PostMessage_Params.is_data_raw_string.
    Instead use the helper method we already have to just convert a raw
    string to a serialized string value encoded byte array on the browser
    side. This gets rid of some special casing and possible confusion in
    this codepath.
    
    Does requre moving string_message_codec to some non-android specific
    location (even though this code is only really used on android), and for
    that blink/common seemed like a sensible place.
    
    Bug: none
    Change-Id: I4ebc70cb68a1c12ad57d3949a1ac606392f7c908
    Reviewed-on: https://chromium-review.googlesource.com/1053169
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557363}

--- third_party/blink/common/message_port/string_message_codec.cc
+++ third_party/blink/common/message_port/string_message_codec.cc
@@ -3,13 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "content/browser/android/string_message_codec.h"
+#include "third_party/blink/public/common/message_port/string_message_codec.h"
 
 #include <vector>
 
 #include "base/logging.h"
 
-namespace content {
+namespace blink {
 namespace {
 
 const uint32_t kVarIntShift = 7;
@@ -47,7 +47,8 @@ void WriteUint32(uint32_t value, std::vector<uint8_t>* buffer) {
   }
 }
 
-void WriteBytes(const char* bytes, size_t num_bytes,
+void WriteBytes(const char* bytes,
+                size_t num_bytes,
                 std::vector<uint8_t>* buffer) {
   buffer->insert(buffer->end(), bytes, bytes + num_bytes);
 }
@@ -80,7 +81,7 @@ bool ContainsOnlyLatin1(const base::string16& data) {
   return !(x & 0xFF00);
 }
 
-} // namespace
+}  // namespace
 
 std::vector<uint8_t> EncodeStringMessage(const base::string16& data) {
   std::vector<uint8_t> buffer;
@@ -143,4 +144,4 @@ bool DecodeStringMessage(const std::vector<uint8_t>& encoded_data,
   return false;
 }
 
-}  // namespace content
+}  // namespace blink
