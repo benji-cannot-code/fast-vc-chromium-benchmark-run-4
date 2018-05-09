b97c61bcda5c0dca830b5e67edefa4130629d249
Tetsui Ohkubo <tetsui@chromium.org>
2018-05-09 03:44:29 +0000

    Add click or tap system tray metrics
    This CL adds ChromeOS.SystemTray.Interaction enum metrics to
    both old SystemTray and UnifiedSystemTray.
    The value records if the region inside system tray bubble is tapped,
    or clicked.
    
    TEST=manual(chrome://histograms)
    BUG=none
    
    Change-Id: I3592ef60c39c3e71962a81e931b8e2b9c570a6cc
    Reviewed-on: https://chromium-review.googlesource.com/1032315
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557082}

--- ash/system/tray/system_tray_view.cc
+++ ash/system/tray/system_tray_view.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/tray/system_tray_view.h"
 
 #include "ash/shell.h"
+#include "ash/system/tray/interacted_by_tap_recorder.h"
 #include "ash/system/tray/system_tray.h"
 #include "ash/system/tray/system_tray_item.h"
 #include "base/metrics/histogram_macros.h"
@@ -53,6 +54,8 @@ SystemTrayView::SystemTrayView(SystemTray* system_tray,
                                const std::vector<ash::SystemTrayItem*>& items)
     : time_to_click_recorder_(
           std::make_unique<TimeToClickRecorder>(system_tray, this)),
+      interacted_by_tap_recorder_(
+          std::make_unique<InteractedByTapRecorder>(this)),
       items_(items),
       system_tray_type_(system_tray_type) {
   SetLayoutManager(std::make_unique<BottomAlignedBoxLayout>());
--- ash/system/unified/unified_system_tray_view.cc
+++ ash/system/unified/unified_system_tray_view.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/unified/unified_system_tray_view.h"
 
 #include "ash/public/cpp/app_list/app_list_features.h"
+#include "ash/system/tray/interacted_by_tap_recorder.h"
 #include "ash/system/tray/tray_constants.h"
 #include "ash/system/unified/feature_pod_button.h"
 #include "ash/system/unified/feature_pods_container_view.h"
@@ -107,7 +108,9 @@ UnifiedSystemTrayView::UnifiedSystemTrayView(
       sliders_container_(new UnifiedSlidersContainerView(initially_expanded)),
       system_info_view_(new UnifiedSystemInfoView()),
       system_tray_container_(new views::View()),
-      detailed_view_container_(new DetailedViewContainer()) {
+      detailed_view_container_(new DetailedViewContainer()),
+      interacted_by_tap_recorder_(
+          std::make_unique<InteractedByTapRecorder>(this)) {
   DCHECK(controller_);
 
   auto* layout = SetLayoutManager(std::make_unique<views::BoxLayout>(
