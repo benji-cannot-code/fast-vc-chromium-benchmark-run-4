a50ec31c3d31dafe1c78acb7c8a711565ef50adc
Hajime Hoshi <hajimehoshi@chromium.org>
2018-05-09 06:11:19 +0000

    scheduler: Replace kInternalAccessibility with kInternalDefault and deprectate kInternalAccessibility
    Following the guideline https://bit.ly/2vMAsQ4, we determined to use
    kInternalDefault as long as the tasks can run in the default way, that
    might be throttled or paused. This CL deprecates kInternalAccessibility
    whose task runner is a default task runner.
    
    Bug: 836410
    Change-Id: Icaa3532e3ef2563b8baa6436566cc5f1ac036b86
    Reviewed-on: https://chromium-review.googlesource.com/1032318
    Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557109}

--- third_party/blink/renderer/platform/scheduler/main_thread/main_thread_scheduler_impl.cc
+++ third_party/blink/renderer/platform/scheduler/main_thread/main_thread_scheduler_impl.cc
@@ -181,8 +181,6 @@ const char* TaskTypeToString(TaskType task_type) {
       return "InternalInspector";
     case TaskType::kInternalAnimation:
       return "InternalAnimation";
-    case TaskType::kInternalAccessibility:
-      return "InternalAccessibility";
     case TaskType::kCount:
       return "Count";
   }
