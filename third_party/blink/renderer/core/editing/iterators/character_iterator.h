b9c19cf99d922f4793c89a4894897c5d71dfaadb
Yoshifumi Inoue <yosin@chromium.org>
2018-05-09 11:32:15 +0000

    Make CharacterIterator::CurrentContainer() to return const Node&
    This patch changes |CharacterIterator::CurrentContainer()| to return
    |const Node&| instead of |const Node*| since it always non-null for improving
    code health.
    
    This patch is a follow-up of the patch[1].
    
    [1] http://crrev.com/c/1050053 Make TextIterator::CurrentContainer() to return
    const Node&.
    
    Change-Id: If2dc187244e4e8a489b56f9855b786c191579d9b
    Reviewed-on: https://chromium-review.googlesource.com/1051138
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557151}

--- third_party/blink/renderer/core/editing/iterators/character_iterator.h
+++ third_party/blink/renderer/core/editing/iterators/character_iterator.h
@@ -66,7 +66,7 @@ class CORE_EXPORT CharacterIteratorAlgorithm {
   int CharacterOffset() const { return offset_; }
 
   const Document& OwnerDocument() const;
-  const Node* CurrentContainer() const;
+  const Node& CurrentContainer() const;
   int StartOffset() const;
   int EndOffset() const;
 
