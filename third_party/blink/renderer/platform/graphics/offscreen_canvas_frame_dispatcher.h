efd0da6ae1bc73c2c0fe96671d06016306c7d73f
Justin Novosad <junov@chromium.org>
2018-05-09 21:35:21 +0000

    Use IntSize for size in OffscreenCanvasFrameDispatcher
    This change is on the path to replacing OffscreenCanvasResourceProvider
    with CanvasResourceProvider
    
    BUG=788439
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I34637343dbd7693cbbfe099061b18a8cfd2094b7
    Reviewed-on: https://chromium-review.googlesource.com/1052923
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Commit-Queue: Justin Novosad <junov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557324}

--- third_party/blink/renderer/platform/graphics/offscreen_canvas_frame_dispatcher.h
+++ third_party/blink/renderer/platform/graphics/offscreen_canvas_frame_dispatcher.h
@@ -38,8 +38,7 @@ class PLATFORM_EXPORT OffscreenCanvasFrameDispatcher
                                  uint32_t client_id,
                                  uint32_t sink_id,
                                  int placeholder_canvas_id,
-                                 int width,
-                                 int height);
+                                 const IntSize&);
 
   // OffscreenCanvasFrameDispatcher implementation.
   ~OffscreenCanvasFrameDispatcher() override;
@@ -51,7 +50,7 @@ class PLATFORM_EXPORT OffscreenCanvasFrameDispatcher
                      double commit_start_time,
                      const SkIRect& damage_rect);
   void ReclaimResource(unsigned resource_id);
-  void Reshape(int width, int height);
+  void Reshape(const IntSize&);
 
   // viz::mojom::blink::CompositorFrameSinkClient implementation.
   void DidReceiveCompositorFrameAck(
@@ -82,8 +81,7 @@ class PLATFORM_EXPORT OffscreenCanvasFrameDispatcher
   viz::ParentLocalSurfaceIdAllocator parent_local_surface_id_allocator_;
   const viz::FrameSinkId frame_sink_id_;
 
-  int width_;
-  int height_;
+  IntSize size_;
   bool change_size_for_next_commit_;
   bool suspend_animation_ = false;
   bool needs_begin_frame_ = false;
