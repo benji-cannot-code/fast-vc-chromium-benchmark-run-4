a012db60dbc42dd5924f182779fbb89e58c13c34
Kent Tamura <tkent@chromium.org>
2018-05-09 01:19:47 +0000

    blinkpy: Introduce diff prettifier written in Python
    It replaces prettifier written in Ruby.
    
    This CL moves the remaining content of prettypatch.py to tools/
    commands/pretty_diff.py because it's very small and there are
    no other callsites.
    
    Bug: 672651
    Change-Id: I91e94356a5cfd73cf714b09f3b47bb4cab738454
    Reviewed-on: https://chromium-review.googlesource.com/1039121
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Quinten Yearsley <qyearsley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557047}

--- third_party/blink/tools/blinkpy/tool/commands/pretty_diff.py
+++ third_party/blink/tools/blinkpy/tool/commands/pretty_diff.py
@@ -30,9 +30,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import logging
 import optparse
 import sys
+import tempfile
 import urllib
 
-from blinkpy.common.prettypatch import PrettyPatch
+from blinkpy.common.pretty_diff import prettify_diff
 from blinkpy.common.system.executive import ScriptError
 from blinkpy.tool.commands.command import Command
 
@@ -67,9 +68,8 @@ class PrettyDiff(Command):
         if not self._tool.user.can_open_url():
             return None
         try:
-            pretty_patch = PrettyPatch(self._tool.executive)
             patch = self._diff(options)
-            pretty_diff_file = pretty_patch.pretty_diff_file(patch)
+            pretty_diff_file = PrettyDiff._pretty_diff_file(patch)
             self._open_pretty_diff(pretty_diff_file.name)
             # We return the pretty_diff_file here because we need to keep the
             # file alive until the user has had a chance to confirm the diff.
@@ -85,6 +85,16 @@ class PrettyDiff(Command):
         changed_files = self._tool.git().changed_files(options.git_commit)
         return self._tool.git().create_patch(options.git_commit,
                                              changed_files=changed_files)
+
+    @staticmethod
+    def _pretty_diff_file(diff):
+        # Diffs can contain multiple text files of different encodings
+        # so we always deal with them as byte arrays, not unicode strings.
+        assert isinstance(diff, str)
+        diff_file = tempfile.NamedTemporaryFile(suffix='.html')
+        diff_file.write(prettify_diff(diff))
+        diff_file.flush()
+        return diff_file
 
     def _open_pretty_diff(self, file_path):
         url = 'file://%s' % urllib.quote(file_path)
