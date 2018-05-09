9620625da85d659ad54e5f6db008a74392fb8087
Scott Violet <sky@chromium.org>
2018-05-09 04:19:12 +0000

    Renames WindowData to ClientWindow
    In hopes of better describinging what this class is for.
    
    BUG=none
    TEST=none
    
    Change-Id: I9611dfa0d74b3ef788381436e6f18cce167a41c9
    Reviewed-on: https://chromium-review.googlesource.com/1050878
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557089}

--- services/ui/ws2/window_host_frame_sink_client.cc
+++ services/ui/ws2/window_host_frame_sink_client.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/ui/ws2/window_host_frame_sink_client.h"
 
 #include "base/logging.h"
-#include "services/ui/ws2/window_data.h"
 #include "ui/aura/mus/client_surface_embedder.h"
 
 namespace ui {
