7ffdea11f9621d893fa9541590a0c85e25511f79
Emily Stark <estark@google.com>
2018-05-09 03:38:11 +0000

    Add HasSeenRecurrentErrors() to ChromeSSLHostStateDelegate
    In a follow-up CL, this will be used to add a message to the interstitial when
    users have seen multiple errors in a browsing session.
    
    Bug: 839969
    Change-Id: I6e7f9c7e73f93b29c73f8fafa504acbfdd182e4c
    Reviewed-on: https://chromium-review.googlesource.com/1048173
    Commit-Queue: Emily Stark <estark@chromium.org>
    Reviewed-by: Christopher Thompson <cthomp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557081}

--- chrome/browser/ssl/chrome_ssl_host_state_delegate.h
+++ chrome/browser/ssl/chrome_ssl_host_state_delegate.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <set>
 
+#include "base/feature_list.h"
 #include "base/gtest_prod_util.h"
 #include "base/macros.h"
 #include "base/time/time.h"
@@ -21,10 +22,13 @@ class Clock;
 class DictionaryValue;
 }  //  namespace base
 
-// Tracks whether the user has allowed a certificate error exception for a
-// specific site, SSL fingerprint, and error. Based on command-line flags and
-// experimental group, remembers this decision either until end-of-session or
-// for a particular length of time.
+extern const base::Feature kRecurrentInterstitialFeature;
+
+// Tracks state related to certificate and SSL errors. This state includes:
+// - certificate error exceptions (which are remembered for a particular length
+//   of time depending on experimental groups)
+// - mixed content exceptions
+// - when errors have recurred multiple times
 class ChromeSSLHostStateDelegate : public content::SSLHostStateDelegate {
  public:
   explicit ChromeSSLHostStateDelegate(Profile* profile);
@@ -63,6 +67,18 @@ class ChromeSSLHostStateDelegate : public content::SSLHostStateDelegate {
   // that there exists an exception. To see if a particular certificate and
   // error combination exception is allowed, use QueryPolicy().
   bool HasAllowException(const std::string& host) const override;
+
+  // Called when an error page is displayed for a given error code |error|.
+  // Tracks whether an error of interest has recurred over a threshold number of
+  // times.
+  void DidDisplayErrorPage(int error);
+
+  // Returns true if DidDisplayErrorPage() has been called over a threshold
+  // number of times for a particular error. Always returns false if
+  // |kRecurrentInterstitialFeature| is not enabled. Only certain error codes of
+  // interest are tracked, so this may return false for an error code that has
+  // recurred.
+  bool HasSeenRecurrentErrors(int error) const;
 
  protected:
   // SetClock takes ownership of the passed in clock.
@@ -148,6 +164,10 @@ class ChromeSSLHostStateDelegate : public content::SSLHostStateDelegate {
   // FORGET_SSL_EXCEPTION_DECISIONS_AT_SESSION_END groups. See
   // https://crbug.com/418631 for more details.
   const std::string current_expiration_guid_;
+
+  // Tracks how many times an error page has been shown for a given error, up
+  // to a certain threshold value.
+  std::map<int /* error code */, int /* count */> recurrent_errors_;
 
   DISALLOW_COPY_AND_ASSIGN(ChromeSSLHostStateDelegate);
 };
--- chrome/browser/ssl/ssl_blocking_page.cc
+++ chrome/browser/ssl/ssl_blocking_page.cc
@@ -19,6 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/renderer_preferences_util.h"
 #include "chrome/browser/ssl/cert_report_helper.h"
+#include "chrome/browser/ssl/chrome_ssl_host_state_delegate.h"
+#include "chrome/browser/ssl/chrome_ssl_host_state_delegate_factory.h"
 #include "chrome/browser/ssl/ssl_cert_reporter.h"
 #include "chrome/browser/ssl/ssl_error_controller_client.h"
 #include "chrome/common/chrome_switches.h"
@@ -85,6 +87,11 @@ SSLBlockingPage* SSLBlockingPage::Create(
       CreateSslProblemMetricsHelper(web_contents, cert_error, request_url,
                                     overridable, is_superfish));
   metrics_helper.get()->StartRecordingCaptivePortalMetrics(overridable);
+
+  ChromeSSLHostStateDelegate* state =
+      ChromeSSLHostStateDelegateFactory::GetForProfile(
+          Profile::FromBrowserContext(web_contents->GetBrowserContext()));
+  state->DidDisplayErrorPage(cert_error);
 
   return new SSLBlockingPage(web_contents, cert_error, ssl_info, request_url,
                              options_mask, time_triggered, support_url,
