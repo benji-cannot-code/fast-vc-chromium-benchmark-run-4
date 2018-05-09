b7d05f6740ec524a5d220e70fb5c29ae5cf16bb9
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-05-09 00:03:29 +0000

    Import wpt@b0edea178f845aefc880cd85b3c74c16ea7e835c
    Using wpt-import in Chromium 494ded10da7bb167b622590d2ab7b90b13bcc1fd.
    With Chromium commits locally applied on WPT:
    9180e0b3bb "Move AudioContext tests to WPT"
    a1bc65506b "Fix resource-timing.https.html test"
    
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/17163
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    alexander.shalamov@intel.com, mikhail.pozdnyakov@intel.com, rijubrata.bhaumik@intel.com, timvolodine@chromium.org:
      external/wpt/generic-sensor
    jsbell@chromium.org:
      external/wpt/resources
    
    TBR=danyao
    
    No-Export: true
    Change-Id: I6d529ae1d2dbd5bb3a62eaaad2105efbe610ff9b
    Reviewed-on: https://chromium-review.googlesource.com/1050826
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557017}

--- third_party/WebKit/LayoutTests/external/wpt/orientation-sensor/orientation-sensor-tests.js
+++ third_party/WebKit/LayoutTests/external/wpt/orientation-sensor/orientation-sensor-tests.js
@@ -1,17 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
- <!DOCTYPE html>
-<meta charset="utf-8">
-<title>OrientationSensor Test</title>
-<link rel="author" title="Intel" href="http://www.intel.com">
-<link rel="help" href="https://w3c.github.io/orientation-sensor/">
-<link rel="help" href="https://w3c.github.io/sensors/">
-<script src="/resources/testharness.js"></script>
-<script src="/resources/testharnessreport.js"></script>
-<script src="/generic-sensor/generic-sensor-tests.js"></script>
-<div id="log"></div>
-
-<script>
-
 //IEEE 754: single precision retricts to 7 decimal digits
 const float_precision = 1e-7;
 
@@ -81,23 +68,17 @@ async function checkPopulateMatrix(t, sensorType) {
   sensor.stop();
 }
 
-promise_test(t => {
-  return checkQuaternion(t, AbsoluteOrientationSensor);
-}, "Test AbsoluteOrientationSensor.quaternion return a four-element FrozenArray.");
+function runOrienationSensorTests(sensorName) {
+  const sensorType = self[sensorName];
 
-promise_test(t => {
-  return checkQuaternion(t, RelativeOrientationSensor);
-}, "Test RelativeOrientationSensor.quaternion return a four-element FrozenArray.");
+  sensor_test(async t => {
+    assert_true(sensorName in self);
+    return checkQuaternion(t, sensorType);
+  }, `${sensorName}.quaternion return a four-element FrozenArray.`);
 
-promise_test(t => {
-  return checkPopulateMatrix(t, AbsoluteOrientationSensor);
-}, "Test AbsoluteOrientationSensor.populateMatrix() method works correctly.");
+  sensor_test(async t => {
+    assert_true(sensorName in self);
+    return checkPopulateMatrix(t, sensorType);
+  }, `${sensorName}.populateMatrix() method works correctly.`);
+}
 
-promise_test(t => {
-  return checkPopulateMatrix(t, RelativeOrientationSensor);
-}, "Test RelativeOrientationSensor.populateMatrix() method works correctly.");
-
-runGenericSensorTests(AbsoluteOrientationSensor);
-runGenericSensorTests(RelativeOrientationSensor);
-
-</script>
