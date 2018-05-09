5ca855dcc57c142819ae12d26fc4f532a54b324a
Kurt Horimoto <kkhorimoto@chromium.org>
2018-05-09 21:36:46 +0000

    [iOS] Don't reset scroll state for same document navigations.
    When the browser navigates to a new page, in-progress scrolls are
    cancelled.  However, same-document navigations can occur mid-scroll.
    
    Bug: none
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I3dd0c8d6e8b570052ffe43676708b46643e52c41
    Reviewed-on: https://chromium-review.googlesource.com/1053051
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557325}

--- ios/chrome/browser/ui/main_content/web_scroll_view_main_content_ui_forwarder.mm
+++ ios/chrome/browser/ui/main_content/web_scroll_view_main_content_ui_forwarder.mm
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/main_content/main_content_ui_state.h"
 #import "ios/chrome/browser/web_state_list/web_state_list.h"
 #import "ios/chrome/browser/web_state_list/web_state_list_observer_bridge.h"
+#import "ios/web/public/web_state/navigation_context.h"
 #import "ios/web/public/web_state/ui/crw_web_view_proxy.h"
 #import "ios/web/public/web_state/ui/crw_web_view_scroll_view_proxy.h"
 #import "ios/web/public/web_state/web_state.h"
@@ -123,7 +124,8 @@ void UpdateStateWithProxy(MainContentUIStateUpdater* updater,
 
 - (void)webState:(web::WebState*)webState
     didFinishNavigation:(web::NavigationContext*)navigation {
-  [self.updater scrollWasInterrupted];
+  if (!navigation->IsSameDocument())
+    [self.updater scrollWasInterrupted];
 }
 
 #pragma mark CRWWebViewScrollViewObserver
