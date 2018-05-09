095b55cb1a7733aa6c148502c1f28bf75847e47f
Andrey Lushnikov <lushnikov@chromium.org>
2018-05-09 04:05:10 +0000

    Headless: Target.disposeBrowserContext() should close context
    The https://crrev.com/555640 regressed headless implementation of
    Target.disposeBrowserContext() method and omitted the actual context
    closing.
    
    This patch brings back the missing method.
    
    R=dgozman, alexclarke
    TBR=alexclarke
    
    Change-Id: I28677381be9fbb86daa5445978e9c0ce4bf388f7
    Reviewed-on: https://chromium-review.googlesource.com/1050983
    Commit-Queue: Andrey Lushnikov <lushnikov@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557087}

--- headless/lib/browser/protocol/target_handler.cc
+++ headless/lib/browser/protocol/target_handler.cc
@@ -99,6 +99,7 @@ Response TargetHandler::DisposeBrowserContext(const std::string& context_id) {
     // close them too.
     web_contents = context->GetAllWebContents();
   }
+  context->Close();
   return Response::OK();
 }
 
