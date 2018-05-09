b4f9a08905028352859449582be08724ec09db7c
Findit <findit-for-me@appspot.gserviceaccount.com>
2018-05-09 23:44:58 +0000

    Revert "Get rid of FrameMsg_PostMessage_Params.is_data_raw_string."
    This reverts commit 6a1c56b9e2b3c792e6f98a6b7f76a28953b164b3.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 557363 as the
    culprit for failures in the build cycles as shown on:
    https://findit-for-me.appspot.com/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtLzZhMWM1NmI5ZTJiM2M3OTJlNmY5OGE2YjdmNzZhMjg5NTNiMTY0YjMM
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium/Win%20x64/22265
    
    Sample Failed Step: compile
    
    Original change's description:
    > Get rid of FrameMsg_PostMessage_Params.is_data_raw_string.
    > 
    > Instead use the helper method we already have to just convert a raw
    > string to a serialized string value encoded byte array on the browser
    > side. This gets rid of some special casing and possible confusion in
    > this codepath.
    > 
    > Does requre moving string_message_codec to some non-android specific
    > location (even though this code is only really used on android), and for
    > that blink/common seemed like a sensible place.
    > 
    > Bug: none
    > Change-Id: I4ebc70cb68a1c12ad57d3949a1ac606392f7c908
    > Reviewed-on: https://chromium-review.googlesource.com/1053169
    > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#557363}
    
    Change-Id: If283428dc28570f24876a661315f9e61d9787fa6
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: none
    Reviewed-on: https://chromium-review.googlesource.com/1053249
    Cr-Commit-Position: refs/heads/master@{#557379}

--- content/browser/android/app_web_message_port.cc
+++ content/browser/android/app_web_message_port.cc
@@ -10,8 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_string.h"
 #include "base/bind.h"
 #include "base/threading/thread_task_runner_handle.h"
+#include "content/browser/android/string_message_codec.h"
 #include "jni/AppWebMessagePort_jni.h"
-#include "third_party/blink/public/common/message_port/string_message_codec.h"
 
 using blink::MessagePortChannel;
 
@@ -45,7 +45,7 @@ JNI_AppWebMessagePort_DecodeStringMessage(
                                            &encoded_message);
 
   base::string16 message;
-  if (!blink::DecodeStringMessage(encoded_message, &message))
+  if (!DecodeStringMessage(encoded_message, &message))
     return nullptr;
 
   base::android::ScopedJavaLocalRef<jstring> jmessage =
@@ -58,8 +58,8 @@ JNI_AppWebMessagePort_EncodeStringMessage(
     JNIEnv* env,
     const base::android::JavaParamRef<jclass>& jcaller,
     const base::android::JavaParamRef<jstring>& jmessage) {
-  std::vector<uint8_t> encoded_message = blink::EncodeStringMessage(
-      base::android::ConvertJavaStringToUTF16(jmessage));
+  std::vector<uint8_t> encoded_message =
+      EncodeStringMessage(base::android::ConvertJavaStringToUTF16(jmessage));
   return base::android::ToJavaByteArray(env, encoded_message);
 }
 
--- third_party/blink/common/test/run_all_unittests.cc
+++ third_party/blink/common/test/run_all_unittests.cc
@@ -9,17 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/test/test_suite.h"
-#include "v8/include/libplatform/libplatform.h"
-#include "v8/include/v8.h"
 
 int main(int argc, char** argv) {
   base::TestSuite test_suite(argc, argv);
-
-  v8::V8::InitializeICUDefaultLocation(argv[0]);
-  v8::V8::InitializeExternalStartupData(argv[0]);
-  auto platform = base::WrapUnique(v8::platform::CreateDefaultPlatform());
-  v8::V8::InitializePlatform(platform.get());
-  v8::V8::Initialize();
 
   return base::LaunchUnitTests(
       argc, argv,
