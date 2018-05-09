c3684a48b12ad0363ac9cb2a922279cb6fac9321
David 'Digit' Turner <digit@google.com>
2018-05-09 14:34:52 +0000

    android: crazy_linker: Read library DT_SONAME entry.
    This CL slightly modifies the crazy linker to extract the DT_SONAME
    of a given shared library. The goal is to use the soname as the unique
    identifier for a library during dependency resolution, as this matches
    the behaviour of the system linker.
    
    More precisely:
    
    - Before this CL, each library loaded by the linker is identified
      by a "name" that was the library path (with any directory sub-path)
      used to load the library.
    
      This prevents properly resolving DT_NEEDED entries, which explains
      one of the reasons why the crazy linker doesn't support the
      component build at the moment.
    
    - After this CL, system libraries are identified by their basename
      only (which was already the case when they were loaded through
      DT_NEEDED entries, but changes what happens when they are loaded
      through the dlopen() wrapper).
    
      Ideally, non-system libraries should be identified by the soname,
      but this currently doesn't work, because it breaks RELRO sharing.
      The reason for this is that, when loading libraries directly from
      a zip file, the current client code (chromium_android_linker)
      passes and relies on the path to the host APK directly
      (e.g. something like /data/data/<package-name>-<number>/base.apk
      and a file offset. This will be addressed in a future CL.
    
    BUG=802068
    R=lizeb@chromium.org, pasko@chromium.org, agrieve@chromium.org
    
    Change-Id: Ia282c29e80e8729c4079a80de7acf66975b7d1ee
    Reviewed-on: https://chromium-review.googlesource.com/1042406
    Commit-Queue: David Turner <digit@chromium.org>
    Reviewed-by: agrieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557175}

--- third_party/android_crazy_linker/src/src/crazy_linker_library_view.cpp
+++ third_party/android_crazy_linker/src/src/crazy_linker_library_view.cpp
@@ -12,6 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace crazy {
 
+LibraryView::LibraryView(SharedLibrary* crazy_lib)
+    : type_(TYPE_CRAZY), crazy_(crazy_lib), name_(crazy_lib->soname()) {}
+
 LibraryView::~LibraryView() {
   LOG("Destroying %s", name_.c_str());
   if (type_ == TYPE_SYSTEM) {
