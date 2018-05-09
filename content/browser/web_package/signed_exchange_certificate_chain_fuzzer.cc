04553ded59d3da778afc990e3d209be34373aeb6
Kunihiko Sakamoto <ksakamoto@chromium.org>
2018-05-09 09:02:05 +0000

    Switch SignedExchangeCertificateChain's input to base::span
    This is a preparation for the CBOR certificate chain support, in which
    the CBOR parser requires input of type base::span<const uint8_t>.
    
    Bug: 803774
    Change-Id: I16f96544b3accbf365a826d5c767e479aa7ef11b
    Reviewed-on: https://chromium-review.googlesource.com/1051187
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557126}

--- content/browser/web_package/signed_exchange_certificate_chain_fuzzer.cc
+++ content/browser/web_package/signed_exchange_certificate_chain_fuzzer.cc
@@ -10,8 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-  base::StringPiece input(reinterpret_cast<const char*>(data), size);
-  SignedExchangeCertificateChain::GetCertChainFromMessage(input);
+  SignedExchangeCertificateChain::GetCertChainFromMessage(
+      base::make_span(data, size));
   return 0;
 }
 
