221080a5bb5835cfad0b56fc8c46ef1436b73692
Ehsan Karamad <ekaramad@chromium.org>
2018-05-09 22:42:06 +0000

    Reworking the test "LookUpStringForRangeRoutesToFocusedWidget"
    This CL reworks the test to make the logic more tractable. The new
    version relies on swizzling the method
    [NSView showDefinitionForAttributedString] which is the method for
    showing the MacOS dictionary. The previous version relied on counting
    the number of windows which seems incorrect.
    
    Bug: 710842
    Change-Id: I483f849882ced00bc603f1b2417c69dfebd0f315
    Reviewed-on: https://chromium-review.googlesource.com/1050886
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Ehsan Karamad <ekaramad@chromium.org>
    Commit-Queue: Ehsan Karamad <ekaramad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557352}

--- content/public/test/content_browser_test_utils_mac.mm
+++ content/public/test/content_browser_test_utils_mac.mm
@@ -29,6 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // RenderWidgetHostViewCocoa methods for tests.
 @interface RenderWidgetHostViewCocoaSwizzler : NSObject
 - (void)didAddSubview:(NSView*)view;
+- (void)showDefinitionForAttributedString:(NSAttributedString*)attrString
+                                  atPoint:(NSPoint)textBaselineOrigin;
 @end
 
 namespace content {
@@ -36,7 +38,9 @@ namespace content {
 using base::mac::ScopedObjCClassSwizzler;
 
 // static
-const char* RenderWidgetHostViewCocoaObserver::kDidAddSubview = "didAddSubview";
+constexpr char RenderWidgetHostViewCocoaObserver::kDidAddSubview[];
+constexpr char
+    RenderWidgetHostViewCocoaObserver::kShowDefinitionForAttributedString[];
 
 // static
 std::map<std::string, std::unique_ptr<base::mac::ScopedObjCClassSwizzler>>
@@ -98,20 +102,25 @@ void RenderWidgetHostViewCocoaObserver::SetUpSwizzlers() {
     return;
 
   // [RenderWidgetHostViewCocoa didAddSubview:NSView*].
-  SEL selector = NSSelectorFromString([NSString
-      stringWithUTF8String:base::StringPrintf("%s:", kDidAddSubview).c_str()]);
   rwhvcocoa_swizzlers_[kDidAddSubview] =
       std::make_unique<ScopedObjCClassSwizzler>(
           GetRenderWidgetHostViewCocoaClassForTesting(),
-          [RenderWidgetHostViewCocoaSwizzler class], selector);
+          [RenderWidgetHostViewCocoaSwizzler class],
+          NSSelectorFromString(@(kDidAddSubview)));
+
+  // [RenderWidgetHostViewCocoa showDefinitionForAttributedString:atPoint].
+  rwhvcocoa_swizzlers_[kShowDefinitionForAttributedString] =
+      std::make_unique<ScopedObjCClassSwizzler>(
+          GetRenderWidgetHostViewCocoaClassForTesting(),
+          [RenderWidgetHostViewCocoaSwizzler class],
+          NSSelectorFromString(@(kShowDefinitionForAttributedString)));
 }
 
 void SetWindowBounds(gfx::NativeWindow window, const gfx::Rect& bounds) {
   NSRect new_bounds = NSRectFromCGRect(bounds.ToCGRect());
   if ([[NSScreen screens] count] > 0) {
-    new_bounds.origin.y =
-        [[[NSScreen screens] firstObject] frame].size.height -
-        new_bounds.origin.y - new_bounds.size.height;
+    new_bounds.origin.y = [[[NSScreen screens] firstObject] frame].size.height -
+                          new_bounds.origin.y - new_bounds.size.height;
   }
 
   [window setFrame:new_bounds display:NO];
@@ -191,5 +200,23 @@ void GetStringFromRangeForRenderWidget(
                        clickCount:1
                          pressure:1.0];
   [[NSApplication sharedApplication] postEvent:dismissal_event atStart:false];
+}
+
+- (void)showDefinitionForAttributedString:(NSAttributedString*)attrString
+                                  atPoint:(NSPoint)textBaselineOrigin {
+  content::RenderWidgetHostViewCocoaObserver::GetSwizzler(
+      content::RenderWidgetHostViewCocoaObserver::
+          kShowDefinitionForAttributedString)
+      ->GetOriginalImplementation()(self, _cmd, attrString, textBaselineOrigin);
+
+  auto* rwhv_mac = content::GetRenderWidgetHostViewMac(self);
+
+  auto* observer = content::RenderWidgetHostViewCocoaObserver::GetObserver(
+      rwhv_mac->GetWebContents());
+
+  if (!observer)
+    return;
+  observer->OnShowDefinitionForAttributedString(
+      base::SysNSStringToUTF8([attrString string]));
 }
 @end
