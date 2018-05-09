60f5d6abc24dc58fa603d9224e91df6aa241496c
Joel Einbinder <einbinder@chromium.org>
2018-05-09 01:28:19 +0000

    DevTools: Slim down UI.Fragment
    This removes some features from UI.Fragment:
        x-shadow
        states
        inline styles
    
    It adds a simple helper method, UI.html which returns an element.
    
    As a driveby, also fixes the jsdoc validator to parse spread operators.
    
    Change-Id: I569f697bbe29530b7a494c75727dcf37769f8f05
    Reviewed-on: https://chromium-review.googlesource.com/1050617
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Joel Einbinder <einbinder@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557049}

--- third_party/WebKit/LayoutTests/http/tests/devtools/components/fragment.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/components/fragment.js
@@ -12,11 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   var inner = document.createElement('div');
   var f1 = UI.Fragment.build`
-    <div-a $=name-a attr=val s-state1-attr=val-state1 s-state2-attr=val-state2>
-      <x-shadow $=name-shadow>
-        <div-b $=name-b foo1=bar1 foo${'2'}=${'b'}ar${'2'} ${''} ${element => element.divb = true} s-state1-attr=val-state1>
-        </div-b>
-      </x-shadow>
+    <div-a $=name-a attr=val>
+      <div-b $=name-b foo1=bar1 foo${'2'}=${'b'}ar${'2'} ${''} ${element => element.divb = true} s-state1-attr=val-state1>
+      </div-b>
       <div-c $=name-c class='${'my-class-1'} my-class-2' ${'foo'}=bar>${'Some text here'} ${'And more text'}</div-c>
       ${inner}
     </div-a>
@@ -28,16 +26,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   var diva = f1.$('name-a');
   var divb = f1.$('name-b');
   var divc = f1.$('name-c');
-  var shadow = f1.$('name-shadow');
 
   check(() => diva === f1.element());
   check(() => diva.tagName === 'DIV-A');
   check(() => divb.tagName === 'DIV-B');
   check(() => divc.tagName === 'DIV-C');
-  check(() => shadow.nodeType === Node.DOCUMENT_FRAGMENT_NODE);
-  check(() => shadow.parentElementOrShadowHost() === diva);
   check(() => divc.parentNode === diva);
-  check(() => divb.parentElementOrShadowHost() === diva);
+  check(() => divb.parentNode === diva);
   check(() => diva.lastChild === inner);
 
   check(() => divb.getAttribute('foo1') === 'bar1');
@@ -50,18 +45,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   check(() => divc.getAttribute('foo') === 'bar');
 
   check(() => diva.getAttribute('attr') === 'val');
-  check(() => divb.getAttribute('attr') === null);
-  f1.setState('state1', true);
-  check(() => diva.getAttribute('attr') === 'val-state1');
-  check(() => divb.getAttribute('attr') === 'val-state1');
-  f1.setState('state1', true);
-  check(() => diva.getAttribute('attr') === 'val-state1');
-  check(() => divb.getAttribute('attr') === 'val-state1');
-  f1.setState('state1', false);
-  check(() => diva.getAttribute('attr') === 'val');
-  check(() => divb.getAttribute('attr') === null);
-  f1.setState('state2', true);
-  check(() => diva.getAttribute('attr') === 'val-state2');
   check(() => divb.getAttribute('attr') === null);
 
   TestRunner.addResult('');
--- third_party/blink/renderer/devtools/scripts/jsdoc_validator/tests/function.js
+++ third_party/blink/renderer/devtools/scripts/jsdoc_validator/tests/function.js
@@ -379,3 +379,9 @@ FuncInterface.prototype = {
      */
     returnNumber: function() { }
 }
+
+/**
+ * @param {string} a
+ * @param {...number} abc
+ */
+function funcParamsOK3(a, ...abc) {}
