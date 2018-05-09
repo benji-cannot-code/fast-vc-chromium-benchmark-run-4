2a1d042820435c0c082e387f72051dcfbbd944fc
Eugene Ostroukhov <eostroukhov@chromium.org>
2018-05-09 00:33:02 +0000

    DevTools: fix prompt overlapping the button
    Bug: 828811
    Change-Id: I575834b95ff2c5587933601e1f46f53221f65fe0
    Reviewed-on: https://chromium-review.googlesource.com/1047956
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Commit-Queue: Eugene Ostroukhov <eostroukhov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557026}

--- third_party/blink/renderer/devtools/front_end/network/NetworkLogView.js
+++ third_party/blink/renderer/devtools/front_end/network/NetworkLogView.js
@@ -160,7 +160,7 @@ Network.NetworkLogView = class extends UI.VBox {
   }
 
   _createSearchPrompt() {
-    const text = this._searchHint.contentElement.createChild('div', 'search-button');
+    const text = this._searchHint.contentElement.createChild('div', 'search-suggestion');
     text.createChild('span').textContent = ls`Search headers and response bodies for `;
     const filterString = text.createChild('strong');
     const button = UI.createTextButton('Find All', () => this._openSearchView());
