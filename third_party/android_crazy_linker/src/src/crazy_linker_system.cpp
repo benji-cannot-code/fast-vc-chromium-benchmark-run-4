06a29a6175716321cd00eb888f6b3ebd557c8205
David 'Digit' Turner <digit@google.com>
2018-05-09 09:58:41 +0000

    android: crazy_linker: Allow searching libraries inside zip archives.
    This modifies the internal crazy::SearchPathList class to support
    looking up libraries inside zip archives, in two ways:
    
    - First, by supporting paths with an exclamation mark, used
      as a delimiter between zip archive paths, and file sub-paths
      within it. For example, looking for:
    
        /path/to/archive.zip!libs/libfoo.so
    
      Will search the zip archive at /path/to/archive.zip for a
      file named libs/libfoo.so.
    
      Note that path items added through AddPaths() can also
      contain an exclamation mark.
    
    - Second, automatically support zip files stored with a
      'crazy.' prefix (e.g. /path/to/archive.zip!libs/libfoo.so
      will match a file named 'libs/crazy.libfoo.so' in the
      archive too.
    
      This is useful to store uncompressed libraries inside
      Android APKs while preventing the system from extracting
      them when the application is installed or updated.
    
    Note that this does not modify the rest of the crazy linker to
    use this for now (e.g. to implement LoadLibraryFromZipFile()),
    since this will require more work (to be provided in a future CL).
    
    
    BUG=802068
    R=agrieve@chromium.org,pasko@chromium.org,lizeb@chromium.org
    
    Change-Id: Ic8661332e4e4537a472e325b74c8c001ea2bc113
    Reviewed-on: https://chromium-review.googlesource.com/1042394
    Commit-Queue: David Turner <digit@chromium.org>
    Reviewed-by: agrieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557131}

--- third_party/android_crazy_linker/src/src/crazy_linker_system.cpp
+++ third_party/android_crazy_linker/src/src/crazy_linker_system.cpp
@@ -19,6 +19,41 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Note: unit-testing support files are in crazy_linker_files_mock.cpp
 
+namespace crazy {
+
+String MakeDirectoryPath(const char* parent) {
+  return MakeDirectoryPath(parent, ::strlen(parent));
+}
+
+String MakeDirectoryPath(const char* parent, size_t parent_len) {
+  if (parent_len == 0) {
+    // Special case for empty inputs.
+    return String("./");
+  }
+  String result(parent);
+  if (parent_len > 0 && parent[parent_len - 1] != '/') {
+    result += '/';
+  }
+  return result;
+}
+
+String MakeAbsolutePathFrom(const char* path) {
+  return MakeAbsolutePathFrom(path, ::strlen(path));
+}
+
+String MakeAbsolutePathFrom(const char* path, size_t path_len) {
+  if (path[0] == '/') {
+    return String(path, path_len);
+  } else {
+    String cur_dir = GetCurrentDirectory();
+    String result = MakeDirectoryPath(cur_dir.c_str(), cur_dir.size());
+    result.Append(path, path_len);
+    return result;
+  }
+}
+
+}  // namespace crazy
+
 #ifndef UNIT_TESTS
 
 namespace crazy {
