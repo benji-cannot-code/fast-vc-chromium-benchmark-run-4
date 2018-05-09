becb1c4c054eb92e6b08705f7453c24b2b8f8a0d
Karandeep Bhatia <karandeepb@chromium.org>
2018-05-09 19:31:57 +0000

    Extensions: Disable ExtensionWebRequestApiTest.WebRequestClientsGoogleComProtection with network service.
    r548673 enabled the WebRequestApiTest.WebRequestClientsGoogleComProtection test
    for network service. To make a network request from the browser, the test
    replaced the usage of URLFetcher with SimpleURLLoader. However this network
    request never reaches the WebRequest API layer, hence defeating the purpose of
    the test. To sanity check that a browser request is intercepted by extension,
    this CL makes another browser request. This causes the test to fail with the
    network service.
    
    BUG=721414
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I6b18ed906b3f01c10521fd89a24414e9a4351edd
    Reviewed-on: https://chromium-review.googlesource.com/1049276
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557274}

--- chrome/test/data/extensions/api_test/webrequest_clients_google_com/background.js
+++ chrome/test/data/extensions/api_test/webrequest_clients_google_com/background.js
@@ -3,10 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-window.webRequestCount = 0;
+window.clientsGoogleWebRequestCount = 0;
+window.yahooWebRequestCount = 0;
 
 chrome.webRequest.onBeforeRequest.addListener(function(details) {
-  ++window.webRequestCount;
-}, {urls: ['http://clients1.google.com/']});
+  if (details.url.includes('http://clients1.google.com/'))
+    ++window.clientsGoogleWebRequestCount;
+  if (details.url.includes('http://yahoo.com'))
+    ++window.yahooWebRequestCount;
+}, {urls: ['<all_urls>']});
 
 chrome.test.sendMessage('ready');
