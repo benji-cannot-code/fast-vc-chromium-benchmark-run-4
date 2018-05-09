a49a54034e81f826a49b1ed4da45c07061ad8fd9
Kouhei Ueno <kouhei@chromium.org>
2018-05-09 16:05:51 +0000

    ScriptLoader: Refactor ScriptLoader::ExecuteAsyncTask for its readability.
    No change in behavior expected.
    
    - ScriptRunner::GetPendingScriptIfAsync is now renamed to GetPendingScriptOfAsyncScript() and DCHECKs if the script loader is actually async.
    - ScriptRunner::IsAsync() is introduced for an existing call-site which called the func just to see if the ScriptLoader is async.
    
    Bug: 838761
    Change-Id: I0cbe73c36706e1e4ae053dd3c438d2eb77c62699
    Reviewed-on: https://chromium-review.googlesource.com/1036712
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Daniel Vogelheim <vogelheim@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557206}

--- third_party/blink/renderer/core/script/script_loader.cc
+++ third_party/blink/renderer/core/script/script_loader.cc
@@ -965,10 +965,9 @@ bool ScriptLoader::IsScriptForEventSupported() const {
          DeprecatedEqualIgnoringCase(event_attribute, "onload()");
 }
 
-PendingScript* ScriptLoader::GetPendingScriptIfScriptIsAsync() {
-  if (pending_script_ && async_exec_type_ == ScriptRunner::kAsync)
-    return pending_script_;
-  return nullptr;
+PendingScript* ScriptLoader::GetPendingScriptIfScriptOfAsyncScript() {
+  DCHECK(IsAsync());
+  return pending_script_;
 }
 
 }  // namespace blink
