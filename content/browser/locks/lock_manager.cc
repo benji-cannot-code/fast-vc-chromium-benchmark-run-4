da2667a67de6f6f939b78dc73e973a5efb571be0
Joshua Bell <jsbell@chromium.org>
2018-05-09 18:46:36 +0000

    Web Locks API: Ensure shared/exclusive sets are rebuilt after a release
    The code contained an optimization to skip rebuilding sets of held
    shared/granted locks if there was no pending request. This was not
    valid, since a request could come in later and rely on the sets to
    correctly determine if the lock was grantable.
    
    Bug: 840994
    Change-Id: I64218cfaebe4ff7b46f133665a94378c66d0e738
    Reviewed-on: https://chromium-review.googlesource.com/1050807
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Joshua Bell <jsbell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557257}

--- content/browser/locks/lock_manager.cc
+++ content/browser/locks/lock_manager.cc
@@ -207,9 +207,6 @@ class LockManager::OriginState {
   }
 
   void ProcessRequests(LockManager* lock_manager, const url::Origin& origin) {
-    if (requested_.empty())
-      return;
-
     shared_.clear();
     exclusive_.clear();
     for (const auto& id_lock_pair : held_) {
