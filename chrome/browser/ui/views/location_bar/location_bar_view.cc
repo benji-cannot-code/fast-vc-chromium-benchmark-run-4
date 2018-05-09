578eac85848742c1614fb53e02dabab7f4d2bbfe
Tommy C. Li <tommycli@chromium.org>
2018-05-09 22:55:50 +0000

    Omnibox Material Refresh: Clean LocationBarView::OnLocationIconFetched
    Clean up an unnecessary call within:
    LocationBarView::OnLocationIconFetched.
    
    Previously we also called LocationIconView::Update, which is
    unnecessary.
    
    Bug: 823535
    Change-Id: I87e9a83ef2d831ead9d674c3ea7f98bd06830cf4
    Reviewed-on: https://chromium-review.googlesource.com/1052917
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557360}

--- chrome/browser/ui/views/location_bar/location_bar_view.cc
+++ chrome/browser/ui/views/location_bar/location_bar_view.cc
@@ -857,7 +857,6 @@ void LocationBarView::RefreshLocationIcon() {
 
 void LocationBarView::OnLocationIconFetched(const gfx::Image& image) {
   location_icon_view_->SetImage(image.AsImageSkia());
-  location_icon_view_->Update();
 }
 
 bool LocationBarView::RefreshContentSettingViews() {
