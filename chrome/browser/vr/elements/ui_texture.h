b51a5650647a19500edda6d1976e54ff8efb010d
Christopher Grant <cjgrant@chromium.org>
2018-05-09 01:08:06 +0000

    VR: Move all text code to the Text class implementation
    The Text element is now the only handler of text in the VR UI.
    
    BUG=
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_vr
    Change-Id: I16501feb6274aebad33426c53798eec21bb6d832
    Reviewed-on: https://chromium-review.googlesource.com/1048400
    Commit-Queue: Christopher Grant <cjgrant@chromium.org>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557041}

--- chrome/browser/vr/elements/ui_texture.h
+++ chrome/browser/vr/elements/ui_texture.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/optional.h"
-#include "base/strings/string16.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/size.h"
@@ -21,9 +20,7 @@ class SkCanvas;
 
 namespace gfx {
 
-class FontList;
 class PointF;
-class RenderText;
 
 }  // namespace gfx
 
@@ -54,45 +51,6 @@ class UiTexture {
   void SetForegroundColor(SkColor color);
   void SetBackgroundColor(SkColor color);
 
-  // This function sets |font_list| to a list of available fonts for |text|. If
-  // no font supports |text|, it returns false and leave |font_list| untouched.
-  static bool GetDefaultFontList(int font_size,
-                                 base::string16 text,
-                                 gfx::FontList* font_list);
-
-  // This function sets |font_list| to a list of available fonts for |text|. If
-  // the font with |preferred_font_name| is available and supports |text|,
-  // |font_list| will be configured to use the preferred font instead of default
-  // font. If no font supports |text|, it returns false and leave |font_list|
-  // untouched.
-  static bool GetFontList(const std::string& preferred_font_name,
-                          int font_size,
-                          base::string16 text,
-                          gfx::FontList* font_list);
-
-  enum TextAlignment {
-    kTextAlignmentNone,
-    kTextAlignmentLeft,
-    kTextAlignmentCenter,
-    kTextAlignmentRight,
-  };
-
-  enum WrappingBehavior {
-    kWrappingBehaviorWrap,
-    kWrappingBehaviorNoWrap,
-  };
-
-  struct TextRenderParameters {
-    SkColor color = SK_ColorBLACK;
-    TextAlignment text_alignment = kTextAlignmentNone;
-    WrappingBehavior wrapping_behavior = kWrappingBehaviorNoWrap;
-    bool cursor_enabled = false;
-    int cursor_position = 0;
-    bool shadows_enabled = false;
-    SkColor shadow_color = SK_ColorBLACK;
-    float shadow_size = 10.0f;
-  };
-
  protected:
   template <typename T>
   void SetAndDirty(T* target, const T& value) {
@@ -100,42 +58,6 @@ class UiTexture {
       set_dirty();
     *target = value;
   }
-
-  // Prepares a set of RenderText objects with the given parameters.
-  // Attempts to fit the text within the provided size. |flags| specifies how
-  // the text should be rendered. If multiline is requested and provided height
-  // is 0, it will be set to the minimum needed to fit the whole text. If
-  // multiline is not requested and provided width is 0, it will be set to the
-  // minimum needed to fit the whole text.
-  static std::vector<std::unique_ptr<gfx::RenderText>> PrepareDrawStringRect(
-      const base::string16& text,
-      const gfx::FontList& font_list,
-      gfx::Rect* bounds,
-      const TextRenderParameters& parameters);
-
-  // Deprecated legacy text prep function. UI elements that use this routine
-  // should migrate to use Text elements, rather than drawing text directly.
-  static std::vector<std::unique_ptr<gfx::RenderText>> PrepareDrawStringRect(
-      const base::string16& text,
-      const gfx::FontList& font_list,
-      SkColor color,
-      gfx::Rect* bounds,
-      TextAlignment text_alignment,
-      WrappingBehavior wrapping_behavior);
-
-  static std::unique_ptr<gfx::RenderText> CreateRenderText();
-
-  static std::unique_ptr<gfx::RenderText> CreateConfiguredRenderText(
-      const base::string16& text,
-      const gfx::FontList& font_list,
-      SkColor color,
-      TextAlignment text_alignment,
-      bool shadows_enabled,
-      SkColor shadow_color,
-      float shadow_size);
-
-  static bool IsRTL();
-  static void SetForceFontFallbackFailureForTesting(bool force);
 
   void set_dirty() {
     measured_ = false;
