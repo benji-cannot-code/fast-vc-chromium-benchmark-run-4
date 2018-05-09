43ac63e6921d062bf521e50e7bef1aff0829f507
Tetsui Ohkubo <tetsui@chromium.org>
2018-05-09 01:29:22 +0000

    Revert "Add UMA event for entering Android tab switcher by toolbar swipe"
    This reverts commit fd312950a41bf5f9c0cd4f2ef9efcc6ab65393cb.
    
    Reason for revert: tools/metrics/actions/actions.xml presubmit check fails on another CL.
    Please run tools/metrics/actions/extract_actions.py and add the description. Sorry for inconvenience.
    
    
    Original change's description:
    > Add UMA event for entering Android tab switcher by toolbar swipe
    > 
    > We currently don't have any UMA events for entering the Android tab switcher by
    > swiping down on the toolbar. In
    > https://chromium-review.googlesource.com/c/chromium/src/+/1048042, I am updating
    > the MobileToolbarShowStackView logging to include this case. In this CL, I am
    > adding a new MobileToolbarSwipeOpenStackView event specifically for this case so
    > we can measure how many people are entering the tab switcher by swiping on the
    > toolbar.
    > 
    > Bug: 828224
    > Change-Id: I2b2f7e4dafd08192355618273d7069a03b4d9e4e
    > Reviewed-on: https://chromium-review.googlesource.com/1048308
    > Reviewed-by: David Trainor <dtrainor@chromium.org>
    > Commit-Queue: Ryan Landay <rlanday@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#556954}
    
    TBR=dtrainor@chromium.org,rlanday@chromium.org
    
    Change-Id: I1f26c5b544e759b76b68db6478ae147b5475c40a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 828224
    Reviewed-on: https://chromium-review.googlesource.com/1051065
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557050}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/LayoutManagerChrome.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/LayoutManagerChrome.java
@@ -10,7 +10,6 @@ import android.view.ViewGroup;
 
 import org.chromium.base.ObserverList;
 import org.chromium.base.VisibleForTesting;
-import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.chrome.browser.compositor.TitleCache;
 import org.chromium.chrome.browser.compositor.layouts.components.LayoutTab;
 import org.chromium.chrome.browser.compositor.layouts.components.VirtualView;
@@ -416,7 +415,6 @@ public class LayoutManagerChrome extends LayoutManager implements OverviewModeBe
             if (mScrollDirection == ScrollDirection.UNKNOWN) return;
 
             if (mOverviewLayout != null && mScrollDirection == ScrollDirection.DOWN) {
-                RecordUserAction.record("MobileToolbarSwipeOpenStackView");
                 startShowing(mOverviewLayout, true);
             } else if (mToolbarSwipeLayout != null
                     && (mScrollDirection == ScrollDirection.LEFT
