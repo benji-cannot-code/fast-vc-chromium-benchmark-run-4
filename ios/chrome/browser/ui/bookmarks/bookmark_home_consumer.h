1eab90b222616e7014e9753710fa77ea68928482
Rohit Rao <rohitrao@chromium.org>
2018-05-09 01:18:35 +0000

    [ios] Adds BookmarkHomeMediator and BookmarkHomeConsumer.
    Adds skeleton implementation of a mediator and consumer interface for
    the BookmarksHomeViewController. Business logic will be added in a
    future CL.
    
    The ChromeTableViewConsumer protocol is added to serve as a base
    protocol for all tableview-based mediators. Individual subclasses can
    extend this protocol and add feature-specific methods.
    
    BUG=840381
    
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I42ff91edfaa6c85a3e534ce515bb9fc82ae1e1fc
    Reviewed-on: https://chromium-review.googlesource.com/1050196
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557046}

---
+++ ios/chrome/browser/ui/bookmarks/bookmark_home_consumer.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_BOOKMARKS_BOOKMARK_HOME_CONSUMER_H_
+#define IOS_CHROME_BROWSER_UI_BOOKMARKS_BOOKMARK_HOME_CONSUMER_H_
+
+#import "ios/chrome/browser/ui/table_view/chrome_table_view_consumer.h"
+
+// BookmarkHomeConsumer provides methods that allow mediators to update the UI.
+@protocol BookmarkHomeConsumer<ChromeTableViewConsumer>
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_BOOKMARKS_BOOKMARK_HOME_CONSUMER_H_
---
+++ ios/chrome/browser/ui/bookmarks/bookmark_home_mediator.mm
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/bookmarks/bookmark_home_mediator.h"
+
+#include "base/logging.h"
+#import "ios/chrome/browser/ui/bookmarks/bookmark_home_shared_state.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+@interface BookmarkHomeMediator ()
+
+@property(nonatomic, strong) BookmarkHomeSharedState* sharedState;
+
+@end
+
+@implementation BookmarkHomeMediator
+@synthesize consumer = _consumer;
+@synthesize sharedState = _sharedState;
+
+- (instancetype)initWithSharedState:(BookmarkHomeSharedState*)sharedState {
+  if ((self = [super init])) {
+    _sharedState = sharedState;
+  }
+  return self;
+}
+
+- (void)startMediating {
+  DCHECK(self.consumer);
+  DCHECK(self.sharedState);
+}
+
+- (void)disconnect {
+  self.consumer = nil;
+  self.sharedState = nil;
+}
+
+@end
