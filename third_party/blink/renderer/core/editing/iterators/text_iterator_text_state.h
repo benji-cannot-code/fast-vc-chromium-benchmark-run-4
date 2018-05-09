b35d7e338205ff4856b65a9428a92e90f4f1a2ac
Yoshifumi Inoue <yosin@chromium.org>
2018-05-09 05:34:29 +0000

    Make CharacterIterator::GetPosition{Before,After}() not to crash with block content
    This patch introduces |TextIterator::GetPosition{Before,After}()| as
    implementation of |CharacterIterator::GetPosition{Before,After}()| not to crash
    with block content.
    
    This is follow-up of the patch[1].
    
    [1] http://crrev.com/c/1021430 Make CharacterIterator::GetPosition{After,Before}()
    to work with BR
    
    Change-Id: I0e5aae4cef69d6c1d902cbeb86e8e3552968c280
    Reviewed-on: https://chromium-review.googlesource.com/1023502
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557103}

--- third_party/blink/renderer/core/editing/iterators/text_iterator_text_state.h
+++ third_party/blink/renderer/core/editing/iterators/text_iterator_text_state.h
@@ -92,6 +92,21 @@ class CORE_EXPORT TextIteratorTextState {
   unsigned PositionEndOffset() const;
   const Node* PositionNode() const { return position_node_; }
   const Node* PositionContainerNode() const { return position_container_node_; }
+  bool IsAfterPositionNode() const {
+    return position_node_type_ == PositionNodeType::kAfterNode;
+  }
+  bool IsBeforePositionNode() const {
+    return position_node_type_ == PositionNodeType::kBeforeNode;
+  }
+  bool IsBeforeCharacter() const {
+    return position_node_type_ == PositionNodeType::kBeforeCharacter;
+  }
+  bool IsBeforeChildren() const {
+    return position_node_type_ == PositionNodeType::kBeforeChildren;
+  }
+  bool IsInTextNode() const {
+    return position_node_type_ == PositionNodeType::kInText;
+  }
 
   bool HasEmitted() const { return has_emitted_; }
   UChar LastCharacter() const { return last_character_; }
@@ -107,6 +122,7 @@ class CORE_EXPORT TextIteratorTextState {
     kAfterNode,
     kAltText,
     kAsNode,
+    kBeforeCharacter,
     kBeforeChildren,
     kBeforeNode,
     kInText,
