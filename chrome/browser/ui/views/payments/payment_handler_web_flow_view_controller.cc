bacec560d32a000891add4c37d0ca3b43e6a8cd0
gogerald <gogerald@google.com>
2018-05-09 15:04:44 +0000

    [Payments] Display origin for initial loading and navigating
    Baug: 840759
    Change-Id: Ibade40328413f8dbd9a7518f7af692322d172b49
    Reviewed-on: https://chromium-review.googlesource.com/1048982
    Commit-Queue: Mathieu Perreault <mathp@chromium.org>
    Reviewed-by: Mathieu Perreault <mathp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557187}

--- chrome/browser/ui/views/payments/payment_handler_web_flow_view_controller.cc
+++ chrome/browser/ui/views/payments/payment_handler_web_flow_view_controller.cc
@@ -195,8 +195,9 @@ bool PaymentHandlerWebFlowViewController::ShouldShowSecondaryButton() {
 
 std::unique_ptr<views::View>
 PaymentHandlerWebFlowViewController::CreateHeaderContentView() {
-  const GURL origin =
-      web_contents() ? web_contents()->GetVisibleURL().GetOrigin() : GURL();
+  const GURL origin = web_contents()
+                          ? web_contents()->GetVisibleURL().GetOrigin()
+                          : target_.GetOrigin();
   std::unique_ptr<views::Background> background = GetHeaderBackground();
   return std::make_unique<ReadOnlyOriginView>(
       GetSheetTitle(), origin,
@@ -223,6 +224,11 @@ void PaymentHandlerWebFlowViewController::ButtonPressed(
   } else {
     PaymentRequestSheetController::ButtonPressed(sender, event);
   }
+}
+
+void PaymentHandlerWebFlowViewController::DidStartNavigation(
+    content::NavigationHandle* navigation_handle) {
+  UpdateHeaderView();
 }
 
 void PaymentHandlerWebFlowViewController::DidFinishNavigation(
