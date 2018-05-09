1eb715d4df17ce4c33705e02973ab6c588dbfeda
David Black <dmblack@google.com>
2018-05-09 02:35:17 +0000

    Hide DialogPlate when using stylus.
    This also requires modifying layout padding on visibility change.
    
    Bug: b:79387198
    Change-Id: I6b7b734badd3bf054b63d6517d82b2c976a9ddf2
    Reviewed-on: https://chromium-review.googlesource.com/1050734
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/master@{#557068}

--- ash/assistant/ui/assistant_bubble_view.h
+++ ash/assistant/ui/assistant_bubble_view.h
@@ -16,6 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/app_list/views/suggestion_chip_view.h"
 #include "ui/views/view.h"
 
+namespace views {
+class BoxLayout;
+}  // namespace views
+
 namespace ash {
 
 class AshAssistantController;
@@ -77,6 +81,8 @@ class AssistantBubbleView : public views::View,
   UiElementContainer* ui_element_container_;     // Owned by view hierarchy.
   SuggestionsContainer* suggestions_container_;  // Owned by view hierarchy.
   DialogPlate* dialog_plate_;                    // Owned by view hierarchy.
+
+  views::BoxLayout* layout_manager_ = nullptr;  // Owned by view hierarchy.
 
   // Uniquely identifies cards owned by AssistantCardRenderer.
   std::vector<base::UnguessableToken> id_token_list_;
