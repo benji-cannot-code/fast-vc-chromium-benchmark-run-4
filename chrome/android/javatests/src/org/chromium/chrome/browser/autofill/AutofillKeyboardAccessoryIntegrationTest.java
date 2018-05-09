cfc4bf57d0483031285209fe7490e9f3bd20cae7
Friedrich Horschig <fhorschig@chromium.org>
2018-05-09 16:08:40 +0000

    Reland #1 "Reactivate deflaked keyboard accessory tests"
    This is a reland of dc92f9dbb51a25f4e85f65b38b9b99218294c3a8
    
    With this reland, only one of the tests is reenabled again.
    The one that doesn't fail and is unrelated to the other two.
    (Therefore, the font changes are not needed anymore - this
    will follow in a second reland CL).
    
    Original change's description:
    > Reactivate deflaked keyboard accessory tests
    >
    > After having landed https://crrev.com/c/1028230, the problems of the
    > linked bug seem to have gone (as assumed).
    >
    > I removed the Disabling and RetryOnFailure annotations to test on 3
    > devices (one physical, two emulator with different screen sizes and OS)
    > and couldn't find a single broken test.
    > Edit: I changed the font size recently (not finally) which seems to
    > bother some bots - unrelated from the filed bug. Changed that back.
    >
    > This CL now reenables all the tests but keeps the RetryOnFailure
    > annotation. That way, I can test whether the tests start failing on
    > some bots again without making the sheriffs life harder.
    > If the bots don't notice any flakiness, I will remove the
    > RetryOnFailure annotation as well and mark the linked bug fixed.
    >
    > Bug: 838922
    > Change-Id: I9781e6d820ac39e43435ad1529da817601967ed4
    > Reviewed-on: https://chromium-review.googlesource.com/1044371
    > Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    > Commit-Queue: Friedrich Horschig <fhorschig@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#556798}
    
    Bug: 838922
    Change-Id: I18dda9ed9df416a796fa1748032f37adb0224dd0
    Reviewed-on: https://chromium-review.googlesource.com/1051831
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Commit-Queue: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557208}

--- chrome/android/javatests/src/org/chromium/chrome/browser/autofill/AutofillKeyboardAccessoryIntegrationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/autofill/AutofillKeyboardAccessoryIntegrationTest.java
@@ -128,7 +128,6 @@ public class AutofillKeyboardAccessoryIntegrationTest {
     @Test
     @MediumTest
     @Feature({"keyboard-accessory"})
-    @DisabledTest(message = "crbug.com/838922")
     public void testTapInputFieldShowsKeyboardAccessory()
             throws ExecutionException, InterruptedException, TimeoutException {
         loadTestPage(false);
