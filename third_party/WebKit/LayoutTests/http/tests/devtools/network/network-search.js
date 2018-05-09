e7b2976b1258693de37b530f540b4fdbcf5d3a24
Eugene Ostroukhov <eostroukhov@chromium.org>
2018-05-09 00:11:47 +0000

    Devtools: match request URL in network search.
    Bug: 836887
    Change-Id: Id13521c9f15f0f3a3e5c1fd4047b40b8883dcbcf
    Reviewed-on: https://chromium-review.googlesource.com/1048045
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Commit-Queue: Eugene Ostroukhov <eostroukhov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557021}

--- third_party/WebKit/LayoutTests/http/tests/devtools/network/network-search.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/network/network-search.js
@@ -28,9 +28,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     return treeElement;
   }
 
-  async function search(label, isRegex, ignoreCase) {
+  async function search(label, isRegex, ignoreCase, query = 'd.search') {
     TestRunner.addResult(label);
-    const view = await Network.SearchNetworkView.openSearch('d.search');
+    const view = await Network.SearchNetworkView.openSearch(query);
     view._matchCaseButton.setToggled(!ignoreCase);
     view._regexButton.setToggled(isRegex);
     const promise = TestRunner.addSnifferPromise(view, '_searchFinished');
@@ -73,6 +73,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       ConsoleTestRunner.addConsoleSniffer(step2);
       return;
     }
+    await search('URL search', true, true, '8000/devtools');
     await search('Ignore case, regexp', true, true);
     await search('Ignore case, No regexp', false, true);
     const lastResult = await search('Case sensitive, regexp', true, false);
