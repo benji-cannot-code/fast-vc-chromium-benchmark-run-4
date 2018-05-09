ca9e58b7d512ebf7c49c6dd0275df2ad5303f598
agrieve <agrieve@chromium.org>
2018-05-09 01:28:16 +0000

    Revert "Reactivate deflaked keyboard accessory tests"
    This reverts commit dc92f9dbb51a25f4e85f65b38b9b99218294c3a8.
    
    Reason for revert: Tests consistently fail on 
    https://ci.chromium.org/buildbot/chromium.android/Marshmallow%20Tablet%20Tester/12646
    
    Original change's description:
    > Reactivate deflaked keyboard accessory tests
    > 
    > After having landed https://crrev.com/c/1028230, the problems of the
    > linked bug seem to have gone (as assumed).
    > 
    > I removed the Disabling and RetryOnFailure annotations to test on 3
    > devices (one physical, two emulator with different screen sizes and OS)
    > and couldn't find a single broken test.
    > Edit: I changed the font size recently (not finally) which seems to
    > bother some bots - unrelated from the filed bug. Changed that back.
    > 
    > This CL now reenables all the tests but keeps the RetryOnFailure
    > annotation. That way, I can test whether the tests start failing on
    > some bots again without making the sheriffs life harder.
    > If the bots don't notice any flakiness, I will remove the
    > RetryOnFailure annotation as well and mark the linked bug fixed.
    > 
    > Bug: 838922
    > Change-Id: I9781e6d820ac39e43435ad1529da817601967ed4
    > Reviewed-on: https://chromium-review.googlesource.com/1044371
    > Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    > Commit-Queue: Friedrich Horschig <fhorschig@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#556798}
    
    TBR=bauerb@chromium.org,fhorschig@chromium.org
    
    Change-Id: I60198d2fbcd36c4687b379d07995e1b873112d51
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 838922
    Reviewed-on: https://chromium-review.googlesource.com/1051026
    Reviewed-by: agrieve <agrieve@chromium.org>
    Commit-Queue: agrieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557048}

--- chrome/android/javatests/src/org/chromium/chrome/browser/autofill/AutofillKeyboardAccessoryIntegrationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/autofill/AutofillKeyboardAccessoryIntegrationTest.java
@@ -27,6 +27,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
 import org.chromium.base.test.util.RetryOnFailure;
@@ -127,6 +128,7 @@ public class AutofillKeyboardAccessoryIntegrationTest {
     @Test
     @MediumTest
     @Feature({"keyboard-accessory"})
+    @DisabledTest(message = "crbug.com/838922")
     public void testTapInputFieldShowsKeyboardAccessory()
             throws ExecutionException, InterruptedException, TimeoutException {
         loadTestPage(false);
@@ -145,6 +147,7 @@ public class AutofillKeyboardAccessoryIntegrationTest {
     @Test
     @MediumTest
     @Feature({"keyboard-accessory"})
+    @DisabledTest(message = "crbug.com/836027")
     public void testSwitchFieldsRescrollsKeyboardAccessory()
             throws ExecutionException, InterruptedException, TimeoutException {
         loadTestPage(false);
@@ -174,6 +177,7 @@ public class AutofillKeyboardAccessoryIntegrationTest {
     @MediumTest
     @Feature({"keyboard-accessory"})
     @MinAndroidSdkLevel(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    @DisabledTest(message = "crbug.com/836027")
     public void testSwitchFieldsRescrollsKeyboardAccessoryRtl()
             throws ExecutionException, InterruptedException, TimeoutException {
         loadTestPage(true);
