4a41dc906e8d9d9da887b231c4c60801bccbc090
Eric Karl <ericrk@chromium.org>
2018-05-10 20:42:56 +0000

    Revert "Disable flaky WebVrTransitionTest#testWindowRafStopsFiringWhilePresenting on Android"
    This reverts commit 1f41d1dafcaadacf666b20b0d0aa32228295a9e0.
    
    Reason for revert: These flakes are addressed with  https://chromium.googlesource.com/chromium/src.git/+/044cca4ad8bc7a80c6edbd4e8dd4d078fd5f7659
    
    Original change's description:
    > Disable flaky WebVrTransitionTest#testWindowRafStopsFiringWhilePresenting on Android
    > 
    > TBR=bsheedy@chromium.org
    > 
    > Bug: 840725
    > Change-Id: Id47604a849bcfcbb51e33ddf94ffa7a539ecb1df
    > Reviewed-on: https://chromium-review.googlesource.com/1049668
    > Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    > Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#556762}
    
    TBR=guidou@chromium.org,bsheedy@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 840725
    Change-Id: Ie558ae22bc52438a919ccf0a49d15bea6d2ab424
    Reviewed-on: https://chromium-review.googlesource.com/1054350
    Commit-Queue: Eric Karl <ericrk@chromium.org>
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557661}

--- chrome/android/javatests/src/org/chromium/chrome/browser/vr_shell/WebVrTransitionTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/vr_shell/WebVrTransitionTest.java
@@ -407,7 +407,6 @@ public class WebVrTransitionTest {
      */
     @Test
     @MediumTest
-    @DisabledTest(message = "crbug.com/840725")
     @VrActivityRestriction({VrActivityRestriction.SupportedActivity.ALL})
     @RetryOnFailure
     public void testWindowRafStopsFiringWhilePresenting() throws InterruptedException {
