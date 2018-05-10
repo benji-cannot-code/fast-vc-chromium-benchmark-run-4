3ee740976806e84d43eb932bf5265fada55dea99
Sharon Yang <yangsharon@google.com>
2018-05-10 15:35:09 +0000

    [Android Notifications] Remove use of reflection for versions N+
    NotificationManager.areNotificationsEnabled() was introduced in N, removing
    the need for reflection.
    
    Bug: 800904
    Change-Id: I0e7acb495f5882fd84a6446f983c04a73417840c
    Reviewed-on: https://chromium-review.googlesource.com/1052228
    Commit-Queue: Sharon Yang <yangsharon@google.com>
    Reviewed-by: Anita Woodruff <awdf@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557525}

--- chrome/android/javatests/src/org/chromium/chrome/browser/notifications/NotificationPlatformBridgeTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/notifications/NotificationPlatformBridgeTest.java
@@ -730,6 +730,9 @@ public class NotificationPlatformBridgeTest {
         assertThat(actionTester.toString(), getNotificationActions(actionTester),
                 Matchers.contains(
                         "Notifications.Persistent.Shown", "Notifications.Persistent.Clicked"));
+        Assert.assertEquals(1,
+                RecordHistogram.getHistogramTotalCountForTesting(
+                        "Notifications.AppNotificationStatus"));
     }
 
     /**
