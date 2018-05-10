8e36d37fc7b0c5731e7d22e9442417225b839252
Sam McNally <sammc@chromium.org>
2018-05-10 00:47:25 +0000

    Create D-Bus service provider for DriveFS
    This is a blank implementation of a D-Bus service for chromeos providing
    interface for DriveFS UI and extended operations IPC connection. It's a
    bridge allowing to integrate ChromeOS session (which is in the browser
    context) with the new implementation of Drive support (which is a system
    service).
    
    This is adapted from https://crrev.com/c/981816, moving the delegate
    implementation into chromeos/components/drivefs and completing its
    implementation.
    
    BUG=chromium:823603
    TEST=Tested by manually calling dbus method from a python script.
    
    Change-Id: I91e5652c7a03de051cbe68baff8c9533598e5de7
    Reviewed-on: https://chromium-review.googlesource.com/1039167
    Commit-Queue: Sam McNally <sammc@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557396}

---
+++ chromeos/components/drivefs/drive_file_stream_service_provider_delegate.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/components/drivefs/drive_file_stream_service_provider_delegate.h"
+
+#include <utility>
+
+#include "chromeos/components/drivefs/pending_connection_manager.h"
+
+namespace drivefs {
+
+DriveFileStreamServiceProviderDelegate::
+    DriveFileStreamServiceProviderDelegate() = default;
+DriveFileStreamServiceProviderDelegate::
+    ~DriveFileStreamServiceProviderDelegate() = default;
+
+bool DriveFileStreamServiceProviderDelegate::OpenIpcChannel(
+    const std::string& identity,
+    base::ScopedFD ipc_channel) {
+  return PendingConnectionManager::Get().OpenIpcChannel(identity,
+                                                        std::move(ipc_channel));
+}
+
+}  // namespace drivefs
