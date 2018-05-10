1ed22bfddfe6c6f7c103fbb264bc8b861f32573e
Chris Blume <cblume@chromium.org>
2018-05-10 22:58:24 +0000

    Fix aura::WindowPortLocal assumption on embedding
    When ParentWindowAllocator changed to default generate a valid
    LocalSurfaceId, it broke an implicit assumption that Aura's
    WindowPortLocal was relying on.
    
    aura::WindowPortLocal used the default invalid LocalSurfaceId to know it
    had not generated a valid ID as the result of embedding external
    content.
    
    This patch fixes that by keeping the LocalSurfaceId allocator in a
    base::Optional and checking that to determine external content
    embedding.
    
    BUG=840246
    
    Change-Id: Ie5475e239cc237caaf3a68b149f92812746a7129
    Reviewed-on: https://chromium-review.googlesource.com/1046378
    Commit-Queue: Chris Blume <cblume@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557717}

--- ui/aura/local/window_port_local.h
+++ ui/aura/local/window_port_local.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
+#include "base/optional.h"
 #include "components/viz/common/surfaces/frame_sink_id.h"
 #include "components/viz/common/surfaces/parent_local_surface_id_allocator.h"
 #include "ui/aura/local/layer_tree_frame_sink_local.h"
@@ -61,11 +62,13 @@ class AURA_EXPORT WindowPortLocal : public WindowPort {
   void OnSurfaceChanged(const viz::SurfaceInfo& surface_info);
   void UpdateLocalSurfaceId();
   const viz::LocalSurfaceId& GetCurrentLocalSurfaceId() const;
+  bool IsEmbeddingExternalContent() const;
 
   Window* const window_;
   gfx::Size last_size_;
   float last_device_scale_factor_ = 1.0f;
-  viz::ParentLocalSurfaceIdAllocator parent_local_surface_id_allocator_;
+  base::Optional<viz::ParentLocalSurfaceIdAllocator>
+      parent_local_surface_id_allocator_;
   base::WeakPtr<cc::LayerTreeFrameSink> frame_sink_;
 
   base::WeakPtrFactory<WindowPortLocal> weak_factory_;
