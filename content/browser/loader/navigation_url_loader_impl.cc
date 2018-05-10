4652737f8e70bc176e343f43eb60f745f4e6726e
Jian Li <jianli@chromium.org>
2018-05-10 22:25:12 +0000

    Adds URLLoaderRequestInterceptor for network servicification
    This allows the embedder to create a URLLoader and ntercept a
    navigation request.
    
    Bug: 841617
    Change-Id: Id6d8c795805413962151be5fbdce91e52322af5d
    Reviewed-on: https://chromium-review.googlesource.com/1053352
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Jian Li <jianli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557699}

--- content/browser/loader/navigation_url_loader_impl.cc
+++ content/browser/loader/navigation_url_loader_impl.cc
@@ -51,6 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/plugin_service.h"
 #include "content/public/browser/resource_dispatcher_host_delegate.h"
 #include "content/public/browser/ssl_status.h"
+#include "content/public/browser/url_loader_request_interceptor.h"
 #include "content/public/common/content_features.h"
 #include "content/public/common/referrer.h"
 #include "content/public/common/url_constants.h"
@@ -76,6 +77,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 namespace {
+
+class NavigationLoaderInterceptorBrowserContainer
+    : public NavigationLoaderInterceptor {
+ public:
+  explicit NavigationLoaderInterceptorBrowserContainer(
+      std::unique_ptr<URLLoaderRequestInterceptor> browser_interceptor)
+      : browser_interceptor_(std::move(browser_interceptor)) {}
+
+  ~NavigationLoaderInterceptorBrowserContainer() override = default;
+
+  void MaybeCreateLoader(const network::ResourceRequest& resource_request,
+                         ResourceContext* resource_context,
+                         LoaderCallback callback) override {
+    browser_interceptor_->MaybeCreateLoader(resource_request, resource_context,
+                                            std::move(callback));
+  }
+
+ private:
+  std::unique_ptr<URLLoaderRequestInterceptor> browser_interceptor_;
+};
 
 // Only used on the IO thread.
 base::LazyInstance<NavigationURLLoaderImpl::BeginNavigationInterceptor>::Leaky
@@ -564,6 +585,20 @@ class NavigationURLLoaderImpl::URLLoaderRequestController
               &URLLoaderRequestController::CreateURLLoaderThrottles,
               base::Unretained(this)),
           url_request_context_getter));
+    }
+
+    std::vector<std::unique_ptr<URLLoaderRequestInterceptor>>
+        browser_interceptors = GetContentClient()
+                                   ->browser()
+                                   ->WillCreateURLLoaderRequestInterceptors(
+                                       navigation_ui_data_.get(),
+                                       request_info->frame_tree_node_id);
+    if (!browser_interceptors.empty()) {
+      for (auto& browser_interceptor : browser_interceptors) {
+        interceptors_.push_back(
+            std::make_unique<NavigationLoaderInterceptorBrowserContainer>(
+                std::move(browser_interceptor)));
+      }
     }
 
     Restart();
--- content/public/browser/content_browser_client.cc
+++ content/public/browser/content_browser_client.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/navigation_ui_data.h"
 #include "content/public/browser/network_service_instance.h"
 #include "content/public/browser/page_navigator.h"
+#include "content/public/browser/url_loader_request_interceptor.h"
 #include "content/public/browser/vpn_service_proxy.h"
 #include "content/public/common/url_loader_throttle.h"
 #include "device/geolocation/public/cpp/location_provider.h"
@@ -722,6 +723,13 @@ std::unique_ptr<OverlayWindow>
 ContentBrowserClient::CreateWindowForPictureInPicture(
     PictureInPictureWindowController* controller) {
   return nullptr;
+}
+
+std::vector<std::unique_ptr<URLLoaderRequestInterceptor>>
+ContentBrowserClient::WillCreateURLLoaderRequestInterceptors(
+    NavigationUIData* navigation_ui_data,
+    int frame_tree_node_id) {
+  return std::vector<std::unique_ptr<URLLoaderRequestInterceptor>>();
 }
 
 }  // namespace content
