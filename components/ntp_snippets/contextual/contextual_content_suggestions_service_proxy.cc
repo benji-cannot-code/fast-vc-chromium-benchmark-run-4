59f14546dd2d5f7028092d36d002d0af44d21bb8
Theresa <twellington@chromium.org>
2018-05-10 19:46:12 +0000

    [EoC] Use full favicon URL instead of encrypted docid
    BUG=833873
    
    Change-Id: I3f00d9258e7b2167c0cadaa41fed2506409b9496
    Reviewed-on: https://chromium-review.googlesource.com/1053794
    Reviewed-by: Nicolas Zea <zea@chromium.org>
    Commit-Queue: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557631}

--- components/ntp_snippets/contextual/contextual_content_suggestions_service_proxy.cc
+++ components/ntp_snippets/contextual/contextual_content_suggestions_service_proxy.cc
@@ -61,7 +61,10 @@ void ContextualContentSuggestionsServiceProxy::FetchContextualSuggestionImage(
     return;
   }
 
-  FetchImageImpl(suggestion_iter->second.image_id, std::move(callback));
+  std::string& image_id = suggestion_iter->second.image_id;
+  GURL image_url = ImageUrlFromId(image_id);
+
+  FetchImageImpl(image_url, image_id, std::move(callback));
 }
 
 void ContextualContentSuggestionsServiceProxy::FetchContextualSuggestionFavicon(
@@ -75,7 +78,8 @@ void ContextualContentSuggestionsServiceProxy::FetchContextualSuggestionFavicon(
     return;
   }
 
-  FetchImageImpl(suggestion_iter->second.favicon_image_id, std::move(callback));
+  FetchImageImpl(GURL(suggestion_iter->second.favicon_image_url),
+                 suggestion_iter->second.favicon_image_id, std::move(callback));
 }
 
 void ContextualContentSuggestionsServiceProxy::ClearState() {
@@ -112,10 +116,9 @@ void ContextualContentSuggestionsServiceProxy::FlushMetrics() {
 }
 
 void ContextualContentSuggestionsServiceProxy::FetchImageImpl(
+    const GURL& image_url,
     const std::string& image_id,
     ntp_snippets::ImageFetchedCallback callback) {
-  GURL image_url = ImageUrlFromId(image_id);
-
   ntp_snippets::ContentSuggestion::ID synthetic_cache_id(
       ntp_snippets::Category::FromKnownCategory(
           ntp_snippets::KnownCategories::CONTEXTUAL),
--- components/ntp_snippets/contextual/contextual_suggestion.h
+++ components/ntp_snippets/contextual/contextual_suggestion.h
@@ -39,6 +39,9 @@ struct ContextualSuggestion {
   // As above, but for identifying the favicon for the site the suggestion
   // resides on.
   std::string favicon_image_id;
+
+  // The favicon URL for the suggestion.
+  std::string favicon_image_url;
 };
 
 // Allows compact, precise construction of a ContextualSuggestion. Its main
@@ -53,6 +56,7 @@ class SuggestionBuilder {
   SuggestionBuilder& Snippet(const std::string& snippet);
   SuggestionBuilder& ImageId(const std::string& image_id);
   SuggestionBuilder& FaviconImageId(const std::string& favicon_image_id);
+  SuggestionBuilder& FaviconImageUrl(const std::string& favicon_image_url);
   ContextualSuggestion Build();
 
  private:
