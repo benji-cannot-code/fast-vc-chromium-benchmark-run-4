880542bc89fbd6a7fc49289bbd039fa9907efa7c
Sebastien Marchand <sebmarchand@chromium.org>
2018-05-10 19:04:53 +0000

    Add the leveldb site characteristic DB
    This is an initial implementation that currently requires that all
    tests run on a thread allowed to do IO operations. A follow-up CL will
    focus on moving all the blocking operations to a separate thread.
    
    BUG=773382
    
    Change-Id: Iea5ee2ede75f4fff168f6546b515018f0fd2a0ca
    Reviewed-on: https://chromium-review.googlesource.com/1005444
    Commit-Queue: Sébastien Marchand <sebmarchand@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557611}

--- chrome/browser/resource_coordinator/local_site_characteristics_data_store.cc
+++ chrome/browser/resource_coordinator/local_site_characteristics_data_store.cc
@@ -9,15 +9,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stl_util.h"
 #include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/resource_coordinator/leveldb_site_characteristics_database.h"
 #include "chrome/browser/resource_coordinator/local_site_characteristics_data_reader.h"
 #include "chrome/browser/resource_coordinator/local_site_characteristics_data_writer.h"
 #include "components/history/core/browser/history_service.h"
 
 namespace resource_coordinator {
 
+namespace {
+constexpr char kSiteCharacteristicsDirectoryName[] =
+    "Site Characteristics Database";
+}
+
 LocalSiteCharacteristicsDataStore::LocalSiteCharacteristicsDataStore(
     Profile* profile)
     : history_observer_(this) {
+  database_ = LevelDBSiteCharacteristicsDatabase::OpenOrCreateDatabase(
+      profile->GetPath().AppendASCII(kSiteCharacteristicsDirectoryName));
+
   history::HistoryService* history =
       HistoryServiceFactory::GetForProfileWithoutCreating(profile);
   if (history)
@@ -59,7 +68,8 @@ LocalSiteCharacteristicsDataStore::GetOrCreateFeatureImpl(
 
   // If not create a new one and add it to the map.
   internal::LocalSiteCharacteristicsDataImpl* site_characteristic_data =
-      new internal::LocalSiteCharacteristicsDataImpl(origin_str, this);
+      new internal::LocalSiteCharacteristicsDataImpl(origin_str, this,
+                                                     database_.get());
   // internal::LocalSiteCharacteristicsDataImpl is a ref-counted object, it's
   // safe to store a raw pointer to it here as this class will get notified when
   // it's about to be destroyed and it'll be removed from the map.
@@ -92,21 +102,25 @@ LocalSiteCharacteristicsDataStore::ResetLocalSiteCharacteristicsEntry(
 void LocalSiteCharacteristicsDataStore::OnURLsDeleted(
     history::HistoryService* history_service,
     const history::DeletionInfo& deletion_info) {
-  // TODO(sebmarchand): Removes these entry from the on-disk database once it's
-  // implemented.
+  // TODO(sebmarchand): Invalidates all the pending read/write operations to the
+  // database once it's asynchronous.
   if (deletion_info.IsAllHistory()) {
     for (auto iter = origin_data_map_.begin();
          iter != origin_data_map_.end();) {
       iter = ResetLocalSiteCharacteristicsEntry(iter);
     }
+    database_->ClearDatabase();
   } else {
-    for (const auto& deleted_row : deletion_info.deleted_rows()) {
+    std::vector<std::string> entries_to_remove;
+    for (auto deleted_row : deletion_info.deleted_rows()) {
       auto map_iter =
           origin_data_map_.find(deleted_row.url().GetOrigin().GetContent());
       if (map_iter != origin_data_map_.end()) {
         ResetLocalSiteCharacteristicsEntry(map_iter);
+        entries_to_remove.emplace_back(map_iter->first);
       }
     }
+    database_->RemoveSiteCharacteristicsFromDB(entries_to_remove);
   }
 }
 
--- chrome/browser/resource_coordinator/local_site_characteristics_data_unittest_utils.h
+++ chrome/browser/resource_coordinator/local_site_characteristics_data_unittest_utils.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "chrome/browser/resource_coordinator/local_site_characteristics_data_impl.h"
+#include "chrome/browser/resource_coordinator/local_site_characteristics_database.h"
 #include "testing/gmock/include/gmock/gmock.h"
 
 namespace resource_coordinator {
@@ -25,6 +26,29 @@ class MockLocalSiteCharacteristicsDataImplOnDestroyDelegate
  private:
   DISALLOW_COPY_AND_ASSIGN(
       MockLocalSiteCharacteristicsDataImplOnDestroyDelegate);
+};
+
+// An implementation of a LocalSiteCharacteristicsDatabase that doesn't record
+// anything.
+class NoopLocalSiteCharacteristicsDatabase
+    : public LocalSiteCharacteristicsDatabase {
+ public:
+  NoopLocalSiteCharacteristicsDatabase();
+  ~NoopLocalSiteCharacteristicsDatabase() override;
+
+  // LocalSiteCharacteristicsDatabase:
+  void ReadSiteCharacteristicsFromDB(
+      const std::string& site_origin,
+      ReadSiteCharacteristicsFromDBCallback callback) override;
+  void WriteSiteCharacteristicsIntoDB(
+      const std::string& site_origin,
+      const SiteCharacteristicsProto& site_characteristic_proto) override;
+  void RemoveSiteCharacteristicsFromDB(
+      const std::vector<std::string>& site_origins) override;
+  void ClearDatabase() override;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NoopLocalSiteCharacteristicsDatabase);
 };
 
 }  // namespace testing
