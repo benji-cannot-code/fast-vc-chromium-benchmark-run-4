4d1c92c0d44872a4aedfc39b097bb65206c57263
Katie D <katie@chromium.org>
2018-05-10 00:01:39 +0000

    Enable Select-to-Speak tray button without experimental a11y flag.
    This makes the button visible to anyone with Select-to-Speak on.
    
    Bug: 753018
    Change-Id: I0154b95d43bdea8645872a3bbb09d41578f3a356
    Reviewed-on: https://chromium-review.googlesource.com/1048055
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Jenny Zhang <jennyz@chromium.org>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557383}

--- chrome/browser/chromeos/accessibility/select_to_speak_browsertest.cc
+++ chrome/browser/chromeos/accessibility/select_to_speak_browsertest.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "ash/system/tray/system_tray.h"
 #include "base/bind.h"
-#include "base/command_line.h"
 #include "base/memory/weak_ptr.h"
 #include "base/strings/pattern.h"
 #include "chrome/browser/chromeos/accessibility/accessibility_manager.h"
@@ -53,13 +52,6 @@ class SelectToSpeakTest : public InProcessBrowserTest {
  protected:
   SelectToSpeakTest() : weak_ptr_factory_(this) {}
   ~SelectToSpeakTest() override {}
-
-  void SetUpCommandLine(base::CommandLine* command_line) override {
-    // TODO(katie): Remove this flag once the feature to add a STS button
-    // in the shelf is launched.
-    command_line->AppendSwitch(
-        chromeos::switches::kEnableExperimentalAccessibilityFeatures);
-  }
 
   void SetUpOnMainThread() override {
     ASSERT_FALSE(AccessibilityManager::Get()->IsSelectToSpeakEnabled());
