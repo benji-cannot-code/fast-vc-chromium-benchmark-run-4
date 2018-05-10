95f37bbd424adc8c0f11e7db1cfd4693b0426436
Bence Béky <bnc@chromium.org>
2018-05-10 15:31:09 +0000

    Lint //net/spdy/chromium
    Run git cl lint on //net/spdy/chromium and fix all complaints (except
    for one about rand_r and thread safety).
    
    Change-Id: Ie51085fb03f640a11dd2c7681d571e7bc6163ace
    Reviewed-on: https://chromium-review.googlesource.com/1050189
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Bence Béky <bnc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557522}

--- net/spdy/chromium/http2_push_promise_index.cc
+++ net/spdy/chromium/http2_push_promise_index.cc
@@ -4,10 +4,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "net/spdy/chromium/http2_push_promise_index.h"
-#include "base/trace_event/memory_usage_estimator.h"
 
 #include <algorithm>
 #include <utility>
+
+#include "base/trace_event/memory_usage_estimator.h"
 
 namespace net {
 
--- net/spdy/chromium/spdy_log_util.cc
+++ net/spdy/chromium/spdy_log_util.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/spdy/chromium/spdy_log_util.h"
 
+#include <utility>
+
 #include "base/strings/string_number_conversions.h"
 #include "base/values.h"
 #include "net/http/http_log_util.h"
