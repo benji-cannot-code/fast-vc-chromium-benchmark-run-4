4da6c3f0b10205917aeb9a2ef13dd4d2e27b72d2
Fady Samuel <fsamuel@chromium.org>
2018-05-10 05:09:33 +0000

    Surface Synchronization: Delete CompositorResizeLock.
    This is no longer used.
    
    Bug: 672962
    Change-Id: Ib97ca682e2c5eb1252972af9068da5afbff9fb81
    TBR: piman@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/1053588
    Reviewed-by: Fady Samuel <fsamuel@chromium.org>
    Commit-Queue: Fady Samuel <fsamuel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557453}

--- content/browser/renderer_host/delegated_frame_host_client_aura.h
+++ content/browser/renderer_host/delegated_frame_host_client_aura.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_BROWSER_RENDERER_HOST_DELEGATED_FRAME_HOST_CLIENT_AURA_H_
 
 #include "base/macros.h"
-#include "content/browser/renderer_host/compositor_resize_lock.h"
 #include "content/browser/renderer_host/delegated_frame_host.h"
 #include "content/common/content_export.h"
 
@@ -17,8 +16,7 @@ class RenderWidgetHostViewAura;
 
 // DelegatedFrameHostClient implementation for aura, not used in mus.
 class CONTENT_EXPORT DelegatedFrameHostClientAura
-    : public DelegatedFrameHostClient,
-      public CompositorResizeLockClient {
+    : public DelegatedFrameHostClient {
  public:
   explicit DelegatedFrameHostClientAura(
       RenderWidgetHostViewAura* render_widget_host_view);
@@ -37,11 +35,6 @@ class CONTENT_EXPORT DelegatedFrameHostClientAura
   void OnBeginFrame(base::TimeTicks frame_time) override;
   void OnFrameTokenChanged(uint32_t frame_token) override;
   void DidReceiveFirstFrameAfterNavigation() override;
-
-  // CompositorResizeLockClient implementation.
-  std::unique_ptr<ui::CompositorLock> GetCompositorLock(
-      ui::CompositorLockClient* client) override;
-  void CompositorResizeLockEnded() override;
 
  private:
   RenderWidgetHostViewAura* render_widget_host_view_;
