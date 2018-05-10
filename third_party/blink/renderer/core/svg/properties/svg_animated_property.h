08ef83597fd9c4562978c52dd043fe399fa49ee6
Hitoshi Yoshida <peria@chromium.org>
2018-05-10 09:37:32 +0000

    oilpan: Move two members from UntracedMember to TraceWrapperMember
    SVGAnimatedProperty::context_element_ and SVGPropertyTearOff::context_element_
    were stored as UntracedMember, because they were thought to have a
    reference cycle involving Persistent.
    
    However, SVGInterpolation was removed and the cycle was cut.
    https://codereview.chromium.org/1607633003
    And hence we no longer need to store them as UntracedMember.
    
    
    Bug: 528275
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Iefdda45d9d56a7e814be43a871cbaa37d55e3b2a
    Reviewed-on: https://chromium-review.googlesource.com/1053619
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Hitoshi Yoshida <peria@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557482}

--- third_party/blink/renderer/core/svg/properties/svg_animated_property.h
+++ third_party/blink/renderer/core/svg/properties/svg_animated_property.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *
  *     * Redistributions of source code must retain the above copyright
  * notice, this list of conditions and the following disclaimer.
-G*     * Redistributions in binary form must reproduce the above
+ *     * Redistributions in binary form must reproduce the above
  * copyright notice, this list of conditions and the following disclaimer
  * in the documentation and/or other materials provided with the
  * distribution.
@@ -77,9 +77,11 @@ class SVGAnimatedPropertyBase : public GarbageCollectedMixin {
 
   bool IsSpecified() const;
 
-  void Trace(blink::Visitor* visitor) override {}
+  void Trace(blink::Visitor* visitor) override {
+    visitor->Trace(context_element_);
+  }
   virtual void TraceWrappers(ScriptWrappableVisitor* visitor) const {
-    visitor->TraceWrappersWithManualWriteBarrier(context_element_.Get());
+    visitor->TraceWrappers(context_element_);
   }
 
  protected:
@@ -97,12 +99,7 @@ class SVGAnimatedPropertyBase : public GarbageCollectedMixin {
 
   const unsigned type_ : 5;
   const unsigned css_property_id_ : kCssPropertyBits;
-
-  // This raw pointer is safe since the SVG element is guaranteed to be kept
-  // alive by a V8 wrapper.
-  // See http://crbug.com/528275 for the detail.
-  UntracedMember<SVGElement> context_element_;
-
+  TraceWrapperMember<SVGElement> context_element_;
   const QualifiedName& attribute_name_;
   DISALLOW_COPY_AND_ASSIGN(SVGAnimatedPropertyBase);
 };
