16369c78537c4b7e0cd25c36c8c2346f8c53043b
Noel Gordon <noel@chromium.org>
2018-05-10 14:06:07 +0000

    FileManagerBrowserTestBase: fix include style after clang-tidy
    Tbr: slangley
    Bug: 841659
    Change-Id: I2f7e55e103f91c563600a29632c59fade44fba3b
    Reviewed-on: https://chromium-review.googlesource.com/1053676
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557504}

--- chrome/browser/chromeos/file_manager/file_manager_browsertest_base.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest_base.cc
@@ -5,9 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/file_manager/file_manager_browsertest_base.h"
 
-#include <stddef.h>
-
 #include <memory>
+#include <stddef.h>
 
 #include "base/containers/circular_deque.h"
 #include "base/json/json_reader.h"
