1ab828c10718f93df6018adf25261d55f509b760
Roger McFarlane <rogerm@chromium.org>
2018-05-10 14:45:56 +0000

    [autofill] Suppress votes and metrics for invalid profile data.
    This CL adds feature flags that causes Chrome to suppress suggestions,
    votes and quality metrics based on profile data which has been flagged
    as likely invalid.
    
    This will allow Chrome Team to evaluate the utility of attempting to
    filter out this data.
    
    Bug: 835984
    Change-Id: I986020fcdc1319ffcab9a8fd998862c27ba645f7
    Reviewed-on: https://chromium-review.googlesource.com/1053597
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Commit-Queue: Roger McFarlane <rogerm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557515}

--- components/autofill/core/browser/autofill_experiments.cc
+++ components/autofill/core/browser/autofill_experiments.cc
@@ -56,6 +56,8 @@ const base::Feature kAutofillPreferServerNamePredictions{
 const base::Feature kAutofillRationalizeFieldTypePredictions{
     "AutofillRationalizeFieldTypePredictions",
     base::FEATURE_ENABLED_BY_DEFAULT};
+const base::Feature kAutofillSuggestInvalidProfileData{
+    "AutofillSuggestInvalidProfileData", base::FEATURE_ENABLED_BY_DEFAULT};
 const base::Feature kAutofillSuppressDisusedAddresses{
     "AutofillSuppressDisusedAddresses", base::FEATURE_ENABLED_BY_DEFAULT};
 const base::Feature kAutofillSuppressDisusedCreditCards{
@@ -69,6 +71,8 @@ const base::Feature kAutofillUpstreamSendPanFirstSix{
 const base::Feature kAutofillUpstreamUpdatePromptExplanation{
     "AutofillUpstreamUpdatePromptExplanation",
     base::FEATURE_DISABLED_BY_DEFAULT};
+const base::Feature kAutofillVoteUsingInvalidProfileData{
+    "AutofillVoteUsingInvalidProfileData", base::FEATURE_ENABLED_BY_DEFAULT};
 
 const char kCreditCardSigninPromoImpressionLimitParamKey[] = "impression_limit";
 const char kAutofillCreditCardPopupBackgroundColorKey[] = "background_color";
