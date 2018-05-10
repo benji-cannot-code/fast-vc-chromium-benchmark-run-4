8788c260a885078255b6fbdee2580eda7089ea62
Xiaohan Wang <xhwang@chromium.org>
2018-05-10 06:47:28 +0000

    media: Add Media.EME.MediaCryptoAvailable UMA
    Bug: 840586
    Change-Id: I0d91bde150d6e3755df3a3577862149298eaa61f
    Reviewed-on: https://chromium-review.googlesource.com/1050934
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557469}

--- media/base/android/media_drm_bridge.cc
+++ media/base/android/media_drm_bridge.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/logging.h"
 #include "base/macros.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/single_thread_task_runner.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
@@ -948,6 +949,9 @@ void MediaDrmBridge::NotifyMediaCryptoReady(JavaObjectPtr j_media_crypto) {
   DCHECK(!j_media_crypto_);
 
   j_media_crypto_ = std::move(j_media_crypto);
+
+  UMA_HISTOGRAM_BOOLEAN("Media.EME.MediaCryptoAvailable",
+                        !j_media_crypto_->is_null());
 
   if (media_crypto_ready_cb_.is_null())
     return;
