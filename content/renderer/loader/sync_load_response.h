fcb0ad8768cb21ea36838acf7d8e4a2715d360e1
Nate Chapin <japhet@chromium.org>
2018-05-10 17:37:05 +0000

    Revert "Do partial redirect handling in the renderer process for sync requests"
    This reverts commit 0817c1db4b405492ea24c6f9fda733480002a1ec.
    
    Reason for revert: Canceling and redirecting causes a variety of small, strange behaviors, revert while landing a better solution (https://chromium-review.googlesource.com/c/chromium/src/+/1053237)
    
    Original change's description:
    > Do partial redirect handling in the renderer process for sync requests
    > 
    > Historically, blink has not received any intermediate resource loading
    > callbacks for sync requests. It simply blocked until the load finished
    > or failed, which meant that redirect security checks performed by blink
    > would be skipped (and in some cases we would try to reconstruct or do
    > a partial security check after the fact). SyncLoadContext allows us to
    > do more of these security checks before the redirect is sent. This
    > allows us to come closer to doing the correct thing for sync request
    > in general, and allows us to move some worker requests off-main-thread,
    > where before they had to proxy sync requests through the main thread in
    > order to get both proper redirect handling and the correct synchronous
    > behavior.
    > 
    > This CL "cancels" sync requests on redirect. WebURLLoaderImpl then
    > notifies blink of the redirect, which sends FetchContext callbacks (but
    > not ResourceClient callbacks). This allows us to do things CSP checking
    > on redirect. If blink permits the redirect, WebURLLoaderImpl manually
    > follows the redirect by issuing a new sync request.
    > 
    > ResourceClient callbacks aren't performed becuase sync requests have
    > historically not been associated with a ResourceClient. Doing so would
    > be a much large refactor, and would require careful handling to make sure
    > we didn't enter JS inside what is supposed to be a synchronous blocking
    > function.
    > 
    > We therefore need to continue to perform a rudimentary CORS check in
    > SyncLoadContext, which blocks cross-origin redirects if the
    > FetchRequestMode was anything other than kNoCors. This revealed a couple
    > of XML/XSLT callsites that should have been setting a FetchRequestMode but
    > weren't.
    > 
    > Bug: 706331
    > 
    > Change-Id: I4ed54cb3ccd119b8a5d192b077bef8d03745e67e
    > Reviewed-on: https://chromium-review.googlesource.com/1015237
    > Commit-Queue: Nate Chapin <japhet@chromium.org>
    > Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#556546}
    
    TBR=falken@chromium.org,horo@chromium.org,kinuko@chromium.org,yhirano@chromium.org,japhet@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 706331
    Change-Id: Id42378ac08f67861aac524ab618b782010970577
    Reviewed-on: https://chromium-review.googlesource.com/1054108
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557568}

--- content/renderer/loader/sync_load_response.h
+++ content/renderer/loader/sync_load_response.h
@@ -27,8 +27,6 @@ struct CONTENT_EXPORT SyncLoadResponse {
 
   SyncLoadResponse& operator=(SyncLoadResponse&& other);
 
-  base::Optional<net::RedirectInfo> redirect_info;
-
   network::ResourceResponseInfo info;
 
   // The response error code.
