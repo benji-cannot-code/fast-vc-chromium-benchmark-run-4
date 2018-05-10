00ff27602efcc366ad8f0ed1c8a25de9d5018a97
xlai <xlai@chromium.org>
2018-05-10 20:39:51 +0000

    Check gpu acceleration before using CanvasResourceProvider for webgl in canvas
    Bug: 802053
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I95833e58c0b7aed7244918869af159325bee75c1
    Reviewed-on: https://chromium-review.googlesource.com/1050618
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Commit-Queue: Olivia Lai <xlai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557658}

--- third_party/blink/renderer/core/html/canvas/html_canvas_element.cc
+++ third_party/blink/renderer/core/html/canvas/html_canvas_element.cc
@@ -1142,7 +1142,10 @@ HTMLCanvasElement::GetOrCreateCanvasResourceProviderForWebGL() {
     resource_provider_is_clear_ = true;
     if (IsValidImageSize(Size())) {
       webgl_resource_provider_ = CanvasResourceProvider::Create(
-          size_, CanvasResourceProvider::kAcceleratedResourceUsage,
+          size_,
+          Platform::Current()->IsGpuCompositingDisabled()
+              ? CanvasResourceProvider::kSoftwareResourceUsage
+              : CanvasResourceProvider::kAcceleratedResourceUsage,
           SharedGpuContext::ContextProviderWrapper(), 0, ColorParams());
     }
     if (!webgl_resource_provider_) {
@@ -1512,7 +1515,9 @@ void HTMLCanvasElement::UpdateMemoryUsage() {
   if (Is3d()) {
     if (webgl_resource_provider_) {
       non_gpu_buffer_count++;
-      gpu_buffer_count += 2;
+      if (webgl_resource_provider_->IsAccelerated()) {
+        gpu_buffer_count += 2;
+      }
     }
     non_gpu_buffer_count += context_->ExternallyAllocatedBufferCountPerPixel();
   }
