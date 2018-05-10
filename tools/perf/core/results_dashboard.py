c1e1594dbdee90f1e9a17242096a72ad4ef3d301
Ethan Kuefner <eakuefner@chromium.org>
2018-05-10 13:03:08 +0000

    [Perf] Compress histogram data before uploading (OBBS)
    This is the OBBS-side version of
    https://chromium-review.googlesource.com/c/chromium/tools/build/+/1052920
    
    Bug: 841351
    Change-Id: Ie863c647ecd93ebeda326b00ebcad1543db0f7ef
    Reviewed-on: https://chromium-review.googlesource.com/1053048
    Reviewed-by: Simon Hatch <simonhatch@chromium.org>
    Reviewed-by: Emily Hanley <eyaich@chromium.org>
    Commit-Queue: Simon Hatch <simonhatch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557499}

--- tools/perf/core/results_dashboard.py
+++ tools/perf/core/results_dashboard.py
@@ -20,6 +20,7 @@ import sys
 import traceback
 import urllib
 import urllib2
+import zlib
 
 from core import path_util
 
@@ -493,7 +494,7 @@ def _SendResultsJson(url, results_json):
     raise SendResultsRetryException()
 
 def _Httplib2Request(url, data, oauth_token):
-  data = urllib.urlencode({'data': data})
+  data = zlib.compress(data)
   headers = {
       'Authorization': 'Bearer %s' % oauth_token,
       'User-Agent': 'perf-uploader/1.0'
