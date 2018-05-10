e77bed10c74ba0b8acf868ec4dd889489aaf3218
Sebastien Marchand <sebmarchand@chromium.org>
2018-05-10 16:57:58 +0000

    Add a SiteCharacteristicsDataStore that doesn't record anything.
    For use in incognito sessions.
    
    Bug: 773382
    Change-Id: I5222eeb2de25c4b5a87e5a41b261d99cbea6de54
    Reviewed-on: https://chromium-review.googlesource.com/1053180
    Commit-Queue: Sébastien Marchand <sebmarchand@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557559}

--- chrome/browser/resource_coordinator/site_characteristics_data_store.h
+++ chrome/browser/resource_coordinator/site_characteristics_data_store.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "chrome/browser/resource_coordinator/site_characteristics_data_reader.h"
+#include "chrome/browser/resource_coordinator/site_characteristics_data_writer.h"
 
 namespace resource_coordinator {
 
@@ -23,6 +24,10 @@ class SiteCharacteristicsDataStore {
   // Returns a SiteCharacteristicsDataReader for the given origin.
   virtual std::unique_ptr<SiteCharacteristicsDataReader> GetReaderForOrigin(
       const std::string& origin) = 0;
+
+  // Returns a SiteCharacteristicsDataWriter for the given origin.
+  virtual std::unique_ptr<SiteCharacteristicsDataWriter> GetWriterForOrigin(
+      const std::string& origin_str) = 0;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(SiteCharacteristicsDataStore);
