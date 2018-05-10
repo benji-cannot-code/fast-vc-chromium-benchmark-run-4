5a3dbc1427ab07f1e1c7054d8e948195d64b93a8
Felix Ekblom <felixe@chromium.org>
2018-05-10 08:03:09 +0000

    Extend cecPrivate with power state query API
    BUG=b:78171027
    
    Change-Id: I08a32e347c103f790e399264d8d045b36848d492
    Reviewed-on: https://chromium-review.googlesource.com/1042576
    Commit-Queue: Felix Ekblom <felixe@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557476}

--- extensions/browser/api/cec_private/cec_private_api.h
+++ extensions/browser/api/cec_private/cec_private_api.h
@@ -6,10 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef EXTENSIONS_BROWSER_API_CEC_PRIVATE_CEC_PRIVATE_API_H_
 #define EXTENSIONS_BROWSER_API_CEC_PRIVATE_CEC_PRIVATE_API_H_
 
+#include <vector>
+
+#include "chromeos/dbus/cec_service_client.h"
 #include "extensions/browser/extension_function.h"
 #include "extensions/browser/extension_function_histogram_value.h"
 
 namespace extensions {
+namespace api {
 
 class CecPrivateFunction : public UIThreadExtensionFunction {
  public:
@@ -49,6 +53,24 @@ class CecPrivateSendWakeUpFunction : public CecPrivateFunction {
   DISALLOW_COPY_AND_ASSIGN(CecPrivateSendWakeUpFunction);
 };
 
+class CecPrivateQueryDisplayCecPowerStateFunction : public CecPrivateFunction {
+ public:
+  CecPrivateQueryDisplayCecPowerStateFunction();
+  DECLARE_EXTENSION_FUNCTION("cecPrivate.queryDisplayCecPowerState",
+                             CECPRIVATE_QUERYDISPLAYCECPOWERSTATE)
+
+ protected:
+  ~CecPrivateQueryDisplayCecPowerStateFunction() override;
+  ResponseAction Run() override;
+
+ private:
+  void HandlePowerStates(
+      const std::vector<chromeos::CecServiceClient::PowerState>& power_states);
+
+  DISALLOW_COPY_AND_ASSIGN(CecPrivateQueryDisplayCecPowerStateFunction);
+};
+
+}  // namespace api
 }  // namespace extensions
 
 #endif  // EXTENSIONS_BROWSER_API_CEC_PRIVATE_CEC_PRIVATE_API_H_
