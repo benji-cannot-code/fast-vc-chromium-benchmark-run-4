cbda6ab55affe97f9d73367fe211cd8fb4406266
Ahmed Fakhry <afakhry@google.com>
2018-05-10 22:20:51 +0000

    Fix caption buttons ink drop highlight issue
    The ink drop highlight lingers behind when the window and the
    caption buttons are growing as a result of maximizing the window.
    Sometimes it even sticks in the wrong place forever until the mouse
    is moved.
    
    This CL fixes the issue by painting the hover highlight in such
    a way that simulates the AutoHighlightMode::SHOW_ON_RIPPLE mode.
    This guarantees the highlight is always painted at the center of
    button.
    
    BUG=840901
    
    Change-Id: I6096fef6605c262ca81c8526fea029e7dc459179
    Reviewed-on: https://chromium-review.googlesource.com/1053805
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557692}

--- ash/frame/caption_buttons/frame_caption_button.h
+++ ash/frame/caption_buttons/frame_caption_button.h
@@ -63,8 +63,6 @@ class ASH_EXPORT FrameCaptionButton : public views::Button {
   // views::InkDropHostView:
   std::unique_ptr<views::InkDrop> CreateInkDrop() override;
   std::unique_ptr<views::InkDropRipple> CreateInkDropRipple() const override;
-  std::unique_ptr<views::InkDropHighlight> CreateInkDropHighlight()
-      const override;
   std::unique_ptr<views::InkDropMask> CreateInkDropMask() const override;
 
   void SetBackgroundColor(SkColor background_color);
