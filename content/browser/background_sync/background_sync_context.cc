281435bf416905fcc663b691579c078fd9ac30a0
codeimpl <codeimpl@gmail.com>
2018-05-10 03:06:59 +0000

    std::make_unique should be preferred than base::WrapUnique
    std::make_unique<Type>(...) and base::WrapUnique<new Type(...)) are
    equivalent but std::make_unique should be preferred, because it is
    harder to use unsafely than base::WrapUnique in background_sync.
    
    Ref:
     https://www.chromium.org/developers/coding-style/cpp-dos-and-donts#TOC-Prefer-MakeUnique-to-WrapUnique
    
    Bug: None
    Change-Id: Ibc1f9455206ffb469f63a7e2529a71e678b54757
    Reviewed-on: https://chromium-review.googlesource.com/1034050
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Commit-Queue: Byoungkwon Ko <codeimpl@gmail.com>
    Cr-Commit-Position: refs/heads/master@{#557439}

--- content/browser/background_sync/background_sync_context.cc
+++ content/browser/background_sync/background_sync_context.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/bind.h"
-#include "base/memory/ptr_util.h"
 #include "base/stl_util.h"
 #include "content/browser/background_sync/background_sync_manager.h"
 #include "content/browser/background_sync/background_sync_service_impl.h"
@@ -87,9 +86,9 @@ void BackgroundSyncContext::CreateServiceOnIOThread(
     mojo::InterfaceRequest<blink::mojom::BackgroundSyncService> request) {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
   DCHECK(background_sync_manager_);
-  BackgroundSyncServiceImpl* service =
-      new BackgroundSyncServiceImpl(this, std::move(request));
-  services_[service] = base::WrapUnique(service);
+  auto service =
+      std::make_unique<BackgroundSyncServiceImpl>(this, std::move(request));
+  services_[service.get()] = std::move(service);
 }
 
 void BackgroundSyncContext::ShutdownOnIO() {
