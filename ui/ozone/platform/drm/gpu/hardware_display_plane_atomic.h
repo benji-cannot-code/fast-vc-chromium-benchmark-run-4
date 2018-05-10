88c7ee907d249011ae0670829788cec790c195c2
Daniel Nicoara <dnicoara@chromium.org>
2018-05-10 14:35:27 +0000

    [Ozone-DRM] Read CTM and GAMMA properties on initialize
    Have DrmDevice read the properties and cache them during initialization.
    Update SetColorCorrection() to use the cached properties rather than
    query them every time the properties are set.
    
    BUG=839487
    TEST=Ran on eve and verified the CTM is set correctly.
    
    Change-Id: I5efd4f4d83d0458a29dc1e7b588f708e0b04912a
    Reviewed-on: https://chromium-review.googlesource.com/1042277
    Commit-Queue: Daniel Nicoara <dnicoara@chromium.org>
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557511}

--- ui/ozone/platform/drm/gpu/hardware_display_plane_atomic.h
+++ ui/ozone/platform/drm/gpu/hardware_display_plane_atomic.h
@@ -42,6 +42,7 @@ class HardwareDisplayPlaneAtomic : public HardwareDisplayPlane {
       DrmDevice* drm,
       const ScopedDrmObjectPropertyPtr& plane_props) override;
 
+  // TODO(dnicoara): Merge this with DrmDevice::Property.
   struct Property {
     Property();
     bool Initialize(DrmDevice* drm,
