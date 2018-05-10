e6636e4b9842a79bc58159359c2639c37016ccb4
Daniel Park <danielpark@chromium.org>
2018-05-10 01:49:39 +0000

    [Home Button] Tracks partner enabled homepage presses
    Tracks presses for clear tab experiment
    Only tracks the event (no IPH shown)
    
    Bug: 836445
    Change-Id: I2db14cd4a1ca93f8d500912effc2aea13c584184
    Reviewed-on: https://chromium-review.googlesource.com/1028718
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Daniel Park <danielpark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557417}

--- components/feature_engagement/public/feature_list.h
+++ components/feature_engagement/public/feature_list.h
@@ -68,6 +68,7 @@ DEFINE_VARIATION_PARAM(kIPHContextualSearchOptInFeature,
 DEFINE_VARIATION_PARAM(kIPHContextualSuggestionsFeature,
                        "IPH_ContextualSuggestions");
 DEFINE_VARIATION_PARAM(kIPHDownloadSettingsFeature, "IPH_DownloadSettings");
+DEFINE_VARIATION_PARAM(kIPHHomePageButtonFeature, "IPH_HomePageButton");
 #endif  // defined(OS_ANDROID)
 #if BUILDFLAG(ENABLE_DESKTOP_IN_PRODUCT_HELP)
 DEFINE_VARIATION_PARAM(kIPHBookmarkFeature, "IPH_Bookmark");
@@ -102,6 +103,7 @@ constexpr flags_ui::FeatureEntry::FeatureVariation
         VARIATION_ENTRY(kIPHContextualSearchOptInFeature),
         VARIATION_ENTRY(kIPHContextualSuggestionsFeature),
         VARIATION_ENTRY(kIPHDownloadSettingsFeature),
+        VARIATION_ENTRY(kIPHHomePageButtonFeature),
 #elif BUILDFLAG(ENABLE_DESKTOP_IN_PRODUCT_HELP)
         VARIATION_ENTRY(kIPHBookmarkFeature),
         VARIATION_ENTRY(kIPHIncognitoWindowFeature),
