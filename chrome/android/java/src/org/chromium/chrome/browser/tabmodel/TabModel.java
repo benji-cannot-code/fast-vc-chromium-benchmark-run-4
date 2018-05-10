db82696584c3e0ec63368b7b2cb1b54e9469eba7
Chris Hamilton <chrisha@chromium.org>
2018-05-10 01:14:28 +0000

    Create native TabModelObserver.
    This adds a native TabModelObserver to
    chrome/browser/ui/android/tab_model, along with the necessary plumbing
    and JNI bridge.
    
    BUG=836409
    
    Change-Id: I0b9bff5146d163705756ff5cbe4c6ed1f211cc35
    Reviewed-on: https://chromium-review.googlesource.com/1038611
    Commit-Queue: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557405}

--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabModel.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabModel.java
@@ -15,6 +15,8 @@ import org.chromium.chrome.browser.tab.Tab;
 public interface TabModel extends TabList {
     /**
      * A list of the various ways tabs can be launched.
+     *
+     * This must be kept in sync with chrome/browser/ui/android/tab_model/tab_model.h.
      */
     public enum TabLaunchType {
         /**
@@ -69,9 +71,13 @@ public interface TabModel extends TabList {
         /** Opened in the background from Browser Actions context menu. */
         FROM_BROWSER_ACTIONS,
     }
+    // For validating that enums are synchronized between Java and corresponding C++.
+    public static final int TabLaunchTypeSize = TabLaunchType.values().length;
 
     /**
      * A list of the various ways tabs can be selected.
+     *
+     * This must be kept in sync with chrome/browser/ui/android/tab_model/tab_model.h.
      */
     public enum TabSelectionType {
         /** Selection of adjacent tab when the active tab is closed in foreground. */
@@ -86,6 +92,8 @@ public interface TabModel extends TabList {
         /** User-originated switch to existing tab or selection of main tab on app startup. */
         FROM_USER
     }
+    // For validating that enums are synchronized between Java and corresponding C++.
+    public static final int TabSelectionTypeSize = TabSelectionType.values().length;
 
     /**
      * @return The profile associated with the current model.
--- chrome/browser/ui/android/tab_model/tab_model_jni_bridge.h
+++ chrome/browser/ui/android/tab_model/tab_model_jni_bridge.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/android/tab_model/tab_model.h"
 
 class TabAndroid;
+class TabModelObserverJniBridge;
 
 namespace content {
 class WebContents;
@@ -64,6 +65,9 @@ class TabModelJniBridge : public TabModel {
   // tab model selector.
   bool IsCurrentModel() const override;
 
+  void AddObserver(TabModelObserver* observer) override;
+  void RemoveObserver(TabModelObserver* observer) override;
+
   // Instructs the TabModel to broadcast a notification that all tabs are now
   // loaded from storage.
   void BroadcastSessionRestoreComplete(
@@ -72,6 +76,11 @@ class TabModelJniBridge : public TabModel {
 
  protected:
   JavaObjectWeakGlobalRef java_object_;
+
+  // The observer bridge. This exists as long as there are registered observers.
+  // It corresponds to a Java observer that is registered with the corresponding
+  // Java TabModelJniBridge.
+  std::unique_ptr<TabModelObserverJniBridge> observer_bridge_;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(TabModelJniBridge);
