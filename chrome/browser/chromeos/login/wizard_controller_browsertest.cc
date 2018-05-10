a50b0cdd1601f448d9afda2f50ecfadcacf1f0f2
Thiemo Nagel <tnagel@chromium.org>
2018-05-10 15:55:25 +0000

    StatisticsProvider: Update comments, rename constant
    Having already enrolled the device is no reason for failing to obtain
    the serial number anymore (https://crrev.com/c/442464).
    
    At that occasion, also fix inconsistencies in the description of the
    serial number logic and append ForTest to the name of the
    kSerialNumberKey constant.
    
    TBR=emaxx
    
    Bug: 823724
    Change-Id: Ic129f4e1141843b695e6f079d684bfd53bdb3810
    Reviewed-on: https://chromium-review.googlesource.com/1051770
    Commit-Queue: Thiemo Nagel <tnagel@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557536}

--- chrome/browser/chromeos/login/wizard_controller_browsertest.cc
+++ chrome/browser/chromeos/login/wizard_controller_browsertest.cc
@@ -815,8 +815,8 @@ class WizardControllerDeviceStateTest : public WizardControllerFlowTest {
  protected:
   WizardControllerDeviceStateTest()
       : install_attributes_(ScopedStubInstallAttributes::CreateUnset()) {
-    fake_statistics_provider_.SetMachineStatistic(system::kSerialNumberKey,
-                                                  "test");
+    fake_statistics_provider_.SetMachineStatistic(
+        system::kSerialNumberKeyForTest, "test");
     fake_statistics_provider_.SetMachineStatistic(system::kActivateDateKey,
                                                   "2000-01");
   }
