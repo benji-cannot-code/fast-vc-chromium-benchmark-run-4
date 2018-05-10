56b2512ddd6dd3fec0e0be47708a1e18b0f76ed3
dpapad <dpapad@chromium.org>
2018-05-10 23:56:53 +0000

    Cleanup: Remove unused constant kLanguageSettingsLearnMoreUrl for non-CrOS.
    Also doing the same for kEasyUnlockLearnMoreUrl, which is only used in CrOS.
    
    Bug: 621723
    Change-Id: Ieada2f363c886e9321016de45a2157a3d35ac1fb
    Reviewed-on: https://chromium-review.googlesource.com/1053155
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557728}

--- chrome/common/url_constants.cc
+++ chrome/common/url_constants.cc
@@ -127,9 +127,6 @@ const char kDownloadInterruptedLearnMoreURL[] =
 const char kDownloadScanningLearnMoreURL[] =
     "https://support.google.com/chrome/?p=ib_download_blocked";
 
-const char kEasyUnlockLearnMoreUrl[] =
-    "https://support.google.com/chromebook/?p=smart_lock";
-
 const char kExtensionControlledSettingLearnMoreURL[] =
     "https://support.google.com/chrome/?p=ui_settings_api_extension";
 
@@ -137,13 +134,6 @@ const char kExtensionInvalidRequestURL[] = "chrome-extension://invalid/";
 
 const char kGoogleAccountActivityControlsURL[] =
     "https://myaccount.google.com/activitycontrols/search";
-
-const char kLanguageSettingsLearnMoreUrl[] =
-#if defined(OS_CHROMEOS)
-    "https://support.google.com/chromebook/answer/1059490";
-#else
-    "https://support.google.com/chrome/topic/1678461";
-#endif
 
 const char kLearnMoreReportingURL[] =
     "https://support.google.com/chrome/?p=ui_usagestat";
@@ -258,6 +248,9 @@ const char kCrosScheme[] = "cros";
 const char kCupsPrintLearnMoreURL[] =
     "https://support.google.com/chromebook?p=chromebook_printing";
 
+const char kEasyUnlockLearnMoreUrl[] =
+    "https://support.google.com/chromebook/?p=smart_lock";
+
 const char kEULAPathFormat[] = "/usr/share/chromeos-assets/eula/%s/eula.html";
 
 const char kEolNotificationURL[] = "https://www.google.com/chromebook/older/";
@@ -267,6 +260,9 @@ const char kGoogleNameserversLearnMoreURL[] =
 
 const char kInstantTetheringLearnMoreURL[] =
     "https://support.google.com/chromebook?p=instant_tethering";
+
+const char kLanguageSettingsLearnMoreUrl[] =
+    "https://support.google.com/chromebook/answer/1059490";
 
 const char kLearnMoreEnterpriseURL[] =
     "https://support.google.com/chromebook/?p=managed";
