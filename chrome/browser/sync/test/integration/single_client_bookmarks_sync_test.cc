787b90dfa78baf53d34af95c57d22481490fc51b
Mohamed Amir Yosef <mamir@chromium.org>
2018-05-10 09:58:55 +0000

    [Sync::USS] Enable DownloadBookmarkFolder test for USS
    The functionality of handling remote creation of bookmarks has
    been introduced in this patch
    https://chromium-review.googlesource.com/c/chromium/src/+/1017201
    
    which supports remote creation of bookmarks as well as folders in the
    bookmark tree.
    
    This CL
    1- Enables DownloadBookmarkFolder test for USS.
    2- Removes the redudant DownloadBookmark test. It has been
    recently introduced however it tests the same behavior as
    InjectedBookmark test case.
    3- Enables InjectedBookmark test for USS instead of the removed
    DownloadBookmark test.
    
    
    Bug: 516866
    Change-Id: Ida235e4817fab122448b88f063dbabdbe7ef7712
    Reviewed-on: https://chromium-review.googlesource.com/1052247
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557487}

--- chrome/browser/sync/test/integration/single_client_bookmarks_sync_test.cc
+++ chrome/browser/sync/test/integration/single_client_bookmarks_sync_test.cc
@@ -243,7 +243,8 @@ IN_PROC_BROWSER_TEST_F(SingleClientBookmarksSyncTest, Sanity) {
     VerifyBookmarkModelMatchesFakeServer(kSingleProfileIndex);
 }
 
-IN_PROC_BROWSER_TEST_F(SingleClientBookmarksSyncTest, InjectedBookmark) {
+IN_PROC_BROWSER_TEST_P(SingleClientBookmarksSyncTestIncludingUssTests,
+                       InjectedBookmark) {
   std::string title = "Montreal Canadiens";
   fake_server::EntityBuilderFactory entity_builder_factory;
   fake_server::BookmarkEntityBuilder bookmark_builder =
@@ -255,7 +256,7 @@ IN_PROC_BROWSER_TEST_F(SingleClientBookmarksSyncTest, InjectedBookmark) {
   ASSERT_TRUE(SetupClients());
   ASSERT_TRUE(SetupSync());
 
-  ASSERT_EQ(1, CountBookmarksWithTitlesMatching(kSingleProfileIndex, title));
+  EXPECT_EQ(1, CountBookmarksWithTitlesMatching(kSingleProfileIndex, title));
 }
 
 // Test that a client doesn't mutate the favicon data in the process
@@ -393,21 +394,6 @@ IN_PROC_BROWSER_TEST_F(SingleClientBookmarksSyncTest,
   ASSERT_TRUE(ModelMatchesVerifier(kSingleProfileIndex));
 }
 
-IN_PROC_BROWSER_TEST_P(SingleClientBookmarksSyncTestIncludingUssTests,
-                       DownloadBookmark) {
-  std::string title = "Patrick Star";
-  fake_server::EntityBuilderFactory entity_builder_factory;
-  fake_server::BookmarkEntityBuilder bookmark_builder =
-      entity_builder_factory.NewBookmarkEntityBuilder(title);
-  fake_server_->InjectEntity(bookmark_builder.BuildBookmark(
-      GURL("http://en.wikipedia.org/wiki/Patrick_Star")));
-
-  DisableVerifier();
-  ASSERT_TRUE(SetupSync());
-
-  EXPECT_EQ(1, CountBookmarksWithTitlesMatching(kSingleProfileIndex, title));
-}
-
 IN_PROC_BROWSER_TEST_F(SingleClientBookmarksSyncTest,
                        DownloadDeletedBookmark) {
   std::string title = "Patrick Star";
@@ -478,7 +464,8 @@ IN_PROC_BROWSER_TEST_F(SingleClientBookmarksSyncTest,
   ASSERT_EQ(1, CountBookmarksWithTitlesMatching(kSingleProfileIndex, title));
 }
 
-IN_PROC_BROWSER_TEST_F(SingleClientBookmarksSyncTest, DownloadBookmarkFolder) {
+IN_PROC_BROWSER_TEST_P(SingleClientBookmarksSyncTestIncludingUssTests,
+                       DownloadBookmarkFolder) {
   const std::string title = "Seattle Sounders FC";
   fake_server::EntityBuilderFactory entity_builder_factory;
   fake_server::BookmarkEntityBuilder bookmark_builder =
