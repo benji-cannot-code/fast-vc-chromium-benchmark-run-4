b2aa0e66e3a269cc9e50162deaf5466b03867619
spqchan <spqchan@chromium.org>
2018-05-11 20:42:49 +0000

    [Mac] Potential AutofillPopupControllerImplMac crash fix
    A crash in the AutofillPopupControllerImplMac might
    be caused by checking the layout model to see if the popup
    is for a credit card. This CL potentially fix the issue by
    storing that value.
    
    Bug: 838660
    Change-Id: I3bbe0b53139d4582313e4eabe91a42e6f3816f3e
    Reviewed-on: https://chromium-review.googlesource.com/1048969
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Sarah Chan <spqchan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558008}

--- chrome/browser/ui/autofill/autofill_popup_controller_impl_mac.mm
+++ chrome/browser/ui/autofill/autofill_popup_controller_impl_mac.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/ui/cocoa/tab_contents/tab_contents_controller.h"
 #import "chrome/browser/ui/cocoa/web_textfield_touch_bar_controller.h"
 #include "components/autofill/core/browser/autofill_experiments.h"
+#include "components/autofill/core/browser/autofill_popup_delegate.h"
 #include "components/autofill/core/browser/popup_item_ids.h"
 
 using base::WeakPtr;
@@ -50,7 +51,8 @@ AutofillPopupControllerImplMac::AutofillPopupControllerImplMac(
                                   container_view,
                                   element_bounds,
                                   text_direction),
-      touchBarController_(nil) {}
+      touch_bar_controller_(nil),
+      is_credit_card_popup_(delegate->IsCreditCardPopup()) {}
 
 AutofillPopupControllerImplMac::~AutofillPopupControllerImplMac() {}
 
@@ -61,7 +63,7 @@ void AutofillPopupControllerImplMac::Show(
   if (!autofill::IsCreditCardAutofillTouchBarExperimentEnabled())
     return;
 
-  if (!GetLineCount() || !layout_model().is_credit_card_popup())
+  if (!GetLineCount() || !is_credit_card_popup_)
     return;
 
   if (@available(macOS 10.12.2, *)) {
@@ -69,10 +71,10 @@ void AutofillPopupControllerImplMac::Show(
         browserWindowControllerForWindow:[container_view() window]];
     TabContentsController* tabContentsController =
         [[bwc tabStripController] activeTabContentsController];
-    touchBarController_ =
+    touch_bar_controller_ =
         [tabContentsController webTextfieldTouchBarController];
 
-    [touchBarController_ showCreditCardAutofillWithController:this];
+    [touch_bar_controller_ showCreditCardAutofillWithController:this];
   }
 }
 
@@ -80,15 +82,15 @@ void AutofillPopupControllerImplMac::UpdateDataListValues(
     const std::vector<base::string16>& values,
     const std::vector<base::string16>& labels) {
   AutofillPopupControllerImpl::UpdateDataListValues(values, labels);
-  if (touchBarController_) {
-    [touchBarController_ invalidateTouchBar];
+  if (touch_bar_controller_) {
+    [touch_bar_controller_ invalidateTouchBar];
   }
 }
 
 void AutofillPopupControllerImplMac::Hide() {
-  if (touchBarController_) {
-    [touchBarController_ hideCreditCardAutofillTouchBar];
-    touchBarController_ = nil;
+  if (touch_bar_controller_) {
+    [touch_bar_controller_ hideCreditCardAutofillTouchBar];
+    touch_bar_controller_ = nil;
   }
 
   AutofillPopupControllerImpl::Hide();
