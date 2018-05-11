cfd98e064fe4ac9d48eb0c16e7e7b7ebf10acb02
Eric Karl <ericrk@chromium.org>
2018-05-11 17:21:11 +0000

    Add WeakPtr to GpuBrowserCompositorOutputSurface for callbacks
    Unlike AndroidOutputSurface and GLOutputSurface,
    GpuBrowserCompositorOutputSurface did not use a weak pointer for
    callbacks, instead de-registering them in its destructor. This
    de-registration was missed in my recent callback refactoring.
    
    Rather than give a way to de-register the new style of swap callbacks
    it seems easier to bring GpuBrowserCompositorOutputSurface in line with
    the other output surfaces and have it handle this via a weak pointer.
    
    Bug: 840788
    Change-Id: I50e38e0b065b857e575aba6a07ca7e3ed716be46
    Reviewed-on: https://chromium-review.googlesource.com/1053108
    Commit-Queue: Eric Karl <ericrk@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557920}

--- content/browser/compositor/gpu_browser_compositor_output_surface.h
+++ content/browser/compositor/gpu_browser_compositor_output_surface.h
@@ -85,6 +85,8 @@ class GpuBrowserCompositorOutputSurface : public BrowserCompositorOutputSurface,
  protected:
   void OnPresentation(uint64_t swap_id,
                       const gfx::PresentationFeedback& feedback);
+  void OnUpdateVSyncParameters(base::TimeTicks timebase,
+                               base::TimeDelta interval);
   gpu::CommandBufferProxyImpl* GetCommandBufferProxy();
 
   viz::OutputSurfaceClient* client_ = nullptr;
@@ -97,6 +99,8 @@ class GpuBrowserCompositorOutputSurface : public BrowserCompositorOutputSurface,
   ui::LatencyTracker latency_tracker_;
 
  private:
+  base::WeakPtrFactory<GpuBrowserCompositorOutputSurface> weak_ptr_factory_;
+
   DISALLOW_COPY_AND_ASSIGN(GpuBrowserCompositorOutputSurface);
 };
 
