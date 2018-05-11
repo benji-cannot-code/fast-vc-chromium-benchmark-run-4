5835bdba0a23fcb46b51ab0d5795ac021dfe214d
Amirhossein Simjour <asimjour@chromium.org>
2018-05-11 17:57:10 +0000

    Fix autofill content size
    When the size of the autofill changes, the container should update
    its size accordingly.
    
    Bug: 834096
    Change-Id: I9fdf7cc04b4572d51ffd2e1ab9f6e7fcc596ae8c
    Reviewed-on: https://chromium-review.googlesource.com/1055617
    Reviewed-by: Biao She <bshe@chromium.org>
    Commit-Queue: Amirhossein Simjour <asimjour@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557928}

--- chrome/android/java/src/org/chromium/chrome/browser/vr_shell/VrShellImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/vr_shell/VrShellImpl.java
@@ -795,7 +795,6 @@ public class VrShellImpl
     @Override
     public void setDialogSize(int width, int height) {
         nativeSetDialogBufferSize(mNativeVrShell, width, height);
-        float scale = mContentVrWindowAndroid.getDisplay().getAndroidUIScaling();
         nativeSetAlertDialogSize(mNativeVrShell, width, height);
     }
 
