43845ea0cd40dc93ba0268423df0e9789ce46668
Tommy Martino <tmartino@chromium.org>
2018-05-11 14:13:41 +0000

    [Autofill Views] Update dropdown footer strings
    This conditionally replaces "Autofill settings" with "Manage cards" or
    "Manage addresses" when using the new Autofill UI.
    
    Bug: 768881
    Change-Id: I9257385a0af87d8208e54db5f56daf78f54b770f
    Reviewed-on: https://chromium-review.googlesource.com/1054102
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Commit-Queue: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557866}

--- components/autofill/core/browser/autofill_external_delegate.cc
+++ components/autofill/core/browser/autofill_external_delegate.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/command_line.h"
+#include "base/feature_list.h"
 #include "base/i18n/case_conversion.h"
 #include "base/stl_util.h"
 #include "base/strings/string_util.h"
@@ -275,7 +276,7 @@ void AutofillExternalDelegate::ClearPreviewedForm() {
   driver_->RendererShouldClearPreviewedForm();
 }
 
-bool AutofillExternalDelegate::IsCreditCardPopup() {
+bool AutofillExternalDelegate::IsCreditCardPopup() const {
   return is_credit_card_popup_;
 }
 
@@ -408,6 +409,12 @@ void AutofillExternalDelegate::InsertDataListValues(
 
 base::string16 AutofillExternalDelegate::GetSettingsSuggestionValue()
     const {
+  if (base::FeatureList::IsEnabled(autofill::kAutofillExpandedPopupViews)) {
+    return l10n_util::GetStringUTF16(IsCreditCardPopup()
+                                         ? IDS_AUTOFILL_MANAGE_PAYMENT_METHODS
+                                         : IDS_AUTOFILL_MANAGE_ADDRESSES);
+  }
+
   return l10n_util::GetStringUTF16(
       IsKeyboardAccessoryEnabled() ? IDS_AUTOFILL_OPTIONS_CONTENT_DESCRIPTION
                                    : IDS_AUTOFILL_SETTINGS_POPUP);
