7ded70f7b48b0f461f58406b0c65810891dbe3e9
Yoshisato Yanagisawa <yyanagisawa@chromium.org>
2018-05-11 04:18:43 +0000

    Import build/third_party when executing runtest.py.
    Since runtest.py depends on modules listed in build/third_party,
    we need to set --with-third-party-lib when we execute runtest.py
    via runit.py.
    
    Bug: 825290
    Change-Id: Ic780388a9b9086364c1e4dcbd4a70a2cd2ed2425
    Reviewed-on: https://chromium-review.googlesource.com/1053674
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557794}

--- testing/scripts/common.py
+++ testing/scripts/common.py
@@ -90,6 +90,7 @@ def run_runtest(cmd_args, runtest_args):
       sys.executable,
       cmd_args.paths['runit.py'],
       '--show-path',
+      '--with-third-party-lib',
       sys.executable,
       cmd_args.paths['runtest.py'],
     ]
