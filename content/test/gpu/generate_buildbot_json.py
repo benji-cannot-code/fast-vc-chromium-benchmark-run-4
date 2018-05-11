f7b60012ec2ee7dfb430e0dd0854c5cf1a14143a
Yuly Novikov <ynovikov@chromium.org>
2018-05-11 21:02:52 +0000

    Shard angle_deqp_gles2_vulkan_tests on Android
    Bug: 839893
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: Ib50865f70c7038bd6ba69aae675212b9f571b6c3
    Tbr: kbr@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/1055856
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>
    Commit-Queue: Yuly Novikov <ynovikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558015}

--- content/test/gpu/generate_buildbot_json.py
+++ content/test/gpu/generate_buildbot_json.py
@@ -1294,6 +1294,7 @@ COMMON_GTESTS = {
             'gpu': LINUX_QUADRO_P400_STABLE_DRIVER,
             'os': 'Ubuntu'
           },
+          # Qualcomm Android Adreno 418
           {
             'device_type': 'bullhead',
             'device_os': 'O',
@@ -1310,7 +1311,7 @@ COMMON_GTESTS = {
         ],
       },
     ],
-    'desktop_swarming': {
+    'swarming': {
       'shards': 4,
     },
     'test': 'angle_deqp_gles2_tests',
