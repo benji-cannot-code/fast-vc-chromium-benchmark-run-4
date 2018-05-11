7525c299ed4cfdcfd9372a8f297e77a030c8915b
Xiaocheng Hu <xiaochengh@chromium.org>
2018-05-11 03:38:09 +0000

    Unify RenderedPosition::PositionAtLeft/RightBoundaryOfBiDiRun
    The above two functions are the same if the RenderedPosition is always
    located in the InlineBox with a higher bidi level.
    
    With crrev.com/c/1052988, the condition is already satisfied. Hence,
    this patch merges the two functions into one.
    
    Bug: 811502
    Change-Id: I0040af1d33d5105cb4504cbbdab2723041505d66
    Reviewed-on: https://chromium-review.googlesource.com/1054583
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557780}

--- third_party/blink/renderer/core/editing/selection_controller.cc
+++ third_party/blink/renderer/core/editing/selection_controller.cc
@@ -788,8 +788,7 @@ static SelectionInFlatTree AdjustEndpointsAtBidiBoundary(
         base == extent.LeftBoundaryOfBidiRun(base.BidiLevelOnRight())) {
       return SelectionInFlatTree::Builder()
           .SetBaseAndExtent(
-              CreateVisiblePosition(base.PositionAtLeftBoundaryOfBiDiRun())
-                  .DeepEquivalent(),
+              CreateVisiblePosition(base.GetPosition()).DeepEquivalent(),
               visible_extent.DeepEquivalent())
           .Build();
     }
@@ -801,8 +800,7 @@ static SelectionInFlatTree AdjustEndpointsAtBidiBoundary(
         base == extent.RightBoundaryOfBidiRun(base.BidiLevelOnLeft())) {
       return SelectionInFlatTree::Builder()
           .SetBaseAndExtent(
-              CreateVisiblePosition(base.PositionAtRightBoundaryOfBiDiRun())
-                  .DeepEquivalent(),
+              CreateVisiblePosition(base.GetPosition()).DeepEquivalent(),
               visible_extent.DeepEquivalent())
           .Build();
     }
@@ -814,8 +812,7 @@ static SelectionInFlatTree AdjustEndpointsAtBidiBoundary(
     return SelectionInFlatTree::Builder()
         .SetBaseAndExtent(
             visible_base.DeepEquivalent(),
-            CreateVisiblePosition(extent.PositionAtLeftBoundaryOfBiDiRun())
-                .DeepEquivalent())
+            CreateVisiblePosition(extent.GetPosition()).DeepEquivalent())
         .Build();
   }
 
@@ -824,8 +821,7 @@ static SelectionInFlatTree AdjustEndpointsAtBidiBoundary(
     return SelectionInFlatTree::Builder()
         .SetBaseAndExtent(
             visible_base.DeepEquivalent(),
-            CreateVisiblePosition(extent.PositionAtRightBoundaryOfBiDiRun())
-                .DeepEquivalent())
+            CreateVisiblePosition(extent.GetPosition()).DeepEquivalent())
         .Build();
   }
   return unchanged_selection;
