f4c124d89f516cc14ca2591ab17742d9fa4148cc
Peter Boström <pbos@chromium.org>
2018-05-11 00:46:34 +0000

    Use circular ink-drops for toolbar and bookmarks
    Adds circular inkdrops for the toolbar and pill-shaped ones for entries
    in the bookmarks bar for Refresh. This removes the recent move to 8dp
    squircles for Refresh and Touch and instead unifies them as rounded.
    
    Also tones down EMPHASIS_HIGH for non-touchable and non-refresh as they
    are not ready for rounded corners.
    
    Bug: chromium:822069, chromium:822072
    Change-Id: Ie619f40834127ebfde59621a86fae42e7d8c4f23
    Reviewed-on: https://chromium-review.googlesource.com/1053379
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557743}

--- chrome/browser/ui/views/harmony/chrome_layout_provider.h
+++ chrome/browser/ui/views/harmony/chrome_layout_provider.h
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 enum ChromeInsetsMetric {
   // Padding applied around the text in the omnibox's editable area.
   INSETS_OMNIBOX = views::VIEWS_INSETS_END,
+  // Padding around buttons on the bookmarks bar.
+  INSETS_BOOKMARKS_BAR_BUTTON,
   // Margins used by toasts.
   INSETS_TOAST,
 };
