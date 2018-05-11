1e27c35eb251bc7c8546263400753cbe54fe8a33
Emily Stark <estark@google.com>
2018-05-11 19:03:19 +0000

    Implement a pref mode for the recurrent error message
    When a certificate error recurs multiple times within a browsing session, we add
    a special message to the interstitial. This CL adds a Finch-controlled mode
    where the count of certificate errors is stored in a pref instead of in memory,
    so that we can trigger the recurrent error message when an error recurs within a
    certain period of time (even spanning browsing sessions). We might want to roll
    out this pref mode on certain platforms (like Android) where browsing sessions
    might be significantly shorter than desktop.
    
    Bug: 839969
    Change-Id: I04f1af1aa30043821859f4459602627b9ba7af77
    Reviewed-on: https://chromium-review.googlesource.com/1053563
    Commit-Queue: Emily Stark <estark@chromium.org>
    Reviewed-by: Christopher Thompson <cthomp@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557966}

--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -55,6 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/rlz/chrome_rlz_tracker_delegate.h"
 #include "chrome/browser/search/search.h"
 #include "chrome/browser/signin/signin_manager_factory.h"
+#include "chrome/browser/ssl/chrome_ssl_host_state_delegate.h"
 #include "chrome/browser/ssl/ssl_config_service_manager.h"
 #include "chrome/browser/task_manager/task_manager_interface.h"
 #include "chrome/browser/tracing/chrome_tracing_delegate.h"
@@ -482,6 +483,7 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
   browsing_data::prefs::RegisterBrowserUserPrefs(registry);
   certificate_transparency::prefs::RegisterPrefs(registry);
   ChromeContentBrowserClient::RegisterProfilePrefs(registry);
+  ChromeSSLHostStateDelegate::RegisterProfilePrefs(registry);
   ChromeVersionService::RegisterProfilePrefs(registry);
   chrome_browser_net::Predictor::RegisterProfilePrefs(registry);
   chrome_browser_net::RegisterPredictionOptionsProfilePrefs(registry);
