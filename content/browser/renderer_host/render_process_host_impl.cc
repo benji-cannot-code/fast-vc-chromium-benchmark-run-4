73fbb5bb75596cba9d73c4ba1c3133b88f61c37e
Max Morin <maxmorin@chromium.org>
2018-05-11 10:02:07 +0000

    Add a new RenderFrameAudioInputStreamFactory
    It checks that the stream is allowed and forwards the request to the
    relevant ForwardingAudioStreamFactory if so. This will cause
    the stream to be served by the audio service.
    The old RenderFrameAudioInputStreamFactory which creates streams
    living in content/ is renamed to
    OldRenderFrameAudioInputStreamFactory. Since the class was renamed,
    the files were moved (by adding "old_" to the beginning). No need to
    review those files. Also note that replacement is diffed against
    the previous implementation. It's probably best to just ignore the diff
    and review render_frame_audio_input_stream_factory{.cc,.h,_unittest.cc}
    as new files.
    
    The same flag as for output is used to switch between the old factory
    and the new one.
    
    Approximate diagram of stuff:
    https://docs.google.com/drawings/d/1_ZIKj6lihGKRjq4Mflduitmkn_REqpHFeqVNelBGHHk/edit
    
    Bug: 830493, 836226
    Change-Id: Ib0f58a52d849a48ba79405f0d499444c66029b58
    Reviewed-on: https://chromium-review.googlesource.com/1050245
    Commit-Queue: Max Morin <maxmorin@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557832}

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -113,7 +113,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/renderer_host/file_utilities_host_impl.h"
 #include "content/browser/renderer_host/media/media_stream_manager.h"
 #include "content/browser/renderer_host/media/peer_connection_tracker_host.h"
-#include "content/browser/renderer_host/media/render_frame_audio_input_stream_factory.h"
 #include "content/browser/renderer_host/media/video_capture_host.h"
 #include "content/browser/renderer_host/pepper/pepper_message_filter.h"
 #include "content/browser/renderer_host/pepper/pepper_renderer_connection.h"
