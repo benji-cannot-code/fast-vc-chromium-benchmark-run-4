fa7d5b150abcf95721fb3b2bfc370181c2d8de37
Tetsui Ohkubo <tetsui@chromium.org>
2018-05-11 09:53:16 +0000

    Show supervised state in UnifiedSystemTray.
    In UnifiedSystemTray, information that was shown in TraySupervisedUser
    default view will be shown at the right bottom of the system tray.
    The style is same as one for enterprise managed devices.
    
    TEST=UnifiedSystemInfoViewNoSessionTest
    BUG=none
    
    Change-Id: I7cb1512a41894a2005a4d4a76a2a667c231ea0ad
    Reviewed-on: https://chromium-review.googlesource.com/1051927
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557829}

--- ash/system/supervised/supervised_notification_controller.h
+++ ash/system/supervised/supervised_notification_controller.h
@@ -20,8 +20,6 @@ class ASH_EXPORT SupervisedNotificationController : public SessionObserver {
   SupervisedNotificationController();
   ~SupervisedNotificationController() override;
 
-  static base::string16 GetSupervisedUserMessage();
-
   // SessionObserver:
   void OnActiveUserSessionChanged(const AccountId& account_id) override;
   void OnUserSessionAdded(const AccountId& account_id) override;
--- ash/system/supervised/tray_supervised_user.cc
+++ ash/system/supervised/tray_supervised_user.cc
@@ -7,11 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
-#include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/session/session_controller.h"
 #include "ash/shell.h"
-#include "ash/strings/grit/ash_strings.h"
-#include "ash/system/supervised/supervised_notification_controller.h"
+#include "ash/system/supervised/supervised_icon_string.h"
 #include "ash/system/tray/label_tray_view.h"
 #include "ash/system/tray/tray_constants.h"
 #include "base/callback.h"
@@ -19,19 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/paint_vector_icon.h"
 
 namespace ash {
-namespace {
-
-const gfx::VectorIcon& GetSupervisedUserIcon() {
-  SessionController* session_controller = Shell::Get()->session_controller();
-  DCHECK(session_controller->IsUserSupervised());
-
-  if (session_controller->IsUserChild())
-    return kSystemMenuChildUserIcon;
-
-  return kSystemMenuSupervisedUserIcon;
-}
-
-}  // namespace
 
 TraySupervisedUser::TraySupervisedUser(SystemTray* system_tray)
     : SystemTrayItem(system_tray, UMA_SUPERVISED_USER) {}
@@ -46,8 +31,7 @@ views::View* TraySupervisedUser::CreateDefaultView(LoginStatus status) {
       new LabelTrayView(nullptr, GetSupervisedUserIcon());
   // The message almost never changes during a session, so we compute it when
   // the menu is shown. We don't update it while the menu is open.
-  tray_view->SetMessage(
-      SupervisedNotificationController::GetSupervisedUserMessage());
+  tray_view->SetMessage(GetSupervisedUserMessage());
   return tray_view;
 }
 
