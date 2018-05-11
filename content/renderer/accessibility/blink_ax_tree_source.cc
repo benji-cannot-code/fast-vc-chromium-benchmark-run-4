0f065154bbc9cd04ea3701377f64619667995ba7
Aaron Leventhal <aleventhal@chromium.org>
2018-05-11 20:43:50 +0000

    Follow all AAM role mappings: DPUB, Graphics, CORE, HTML
    Fixes incorrect platform role mappings for IA2 and ATK.
    
    Adds the following new roles to Chrome:
    New landmark roles: docAcknowledgments, docAfterword, docAppendix, docBibliography,
    docChapter, docConclusion, docCredits, docEndnotes, docEpilogue, docErrata, docForeword,
    docGlossary, docIndex, docIntroduction, docPageList, docPart, docPreface, docPrologue, docToc
    Acts like a heading: docSubtitle
    Acts like a note: docTip, docNotice, docFootnote
    Acts like a section/region: docAbstract, docColophon, docCredit, docDedication, docEpigraph,
    docExample, docQna
    Acts like an image: docCover, graphicsSymbol
    Acts like a link: docBackLink, docBiblioRef, docNoteRef, docGlossRef
    Acts like a list item: docBiblioEntry, docEndnote, docFootnote
    Acts like a separator: docPageBreak
    Acts like a document: graphicsDocument
    Acts like a pane: graphicsObject
    
    Note: these are the internal names. In the role attribute used in HTML, the role names are lowercase and
    hyphenated with "doc-" or "graphics-" prefixes, e.g. “doc-endnotes”.
    
    More DPub info:
    Spec: https://www.w3.org/TR/dpub-aria-1.0/
    Platform implementation notes: https://www.w3.org/TR/dpub-aam-1.0/#mapping_role_table
    
    More Graphics ARIA info:
    Spec: https://www.w3.org/TR/graphics-aria-1.0/
    Platform implementation notes: https://rawgit.com/w3c/graphics-aam/master/#mapping_role_table
    
    NOTRY=true
    
    Bug: 812243, 839871
    TBR: jcivelli@chromium.org,stevenjb@chromium.org
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: If2d5ec8aca551cb1fb611cc3614f4f4617f958a9
    Reviewed-on: https://chromium-review.googlesource.com/1044347
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Alex Sakhartchouk <alexst@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558009}

--- content/renderer/accessibility/blink_ax_tree_source.cc
+++ content/renderer/accessibility/blink_ax_tree_source.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/web/web_plugin_container.h"
 #include "third_party/blink/public/web/web_view.h"
 #include "ui/accessibility/ax_enum_util.h"
+#include "ui/accessibility/ax_role_properties.h"
 
 using base::ASCIIToUTF16;
 using base::UTF16ToUTF8;
@@ -657,7 +658,7 @@ void BlinkAXTreeSource::SerializeNode(WebAXObject src,
                            src.AriaActiveDescendant().AxID());
     }
 
-    if (dst->role == ax::mojom::Role::kHeading && src.HeadingLevel()) {
+    if (ui::IsHeading(dst->role) && src.HeadingLevel()) {
       dst->AddIntAttribute(ax::mojom::IntAttribute::kHierarchicalLevel,
                            src.HeadingLevel());
     } else if ((dst->role == ax::mojom::Role::kTreeItem ||
