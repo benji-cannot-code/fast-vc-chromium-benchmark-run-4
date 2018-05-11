ccab380739bd48b138e12daa88c065c1a5c3e259
kylechar <kylechar@chromium.org>
2018-05-11 20:49:05 +0000

    Add SharedBitmapReporter interface.
    VideoResourceUpdater currently uses LayerTreeFrameSink as the interface
    through which to notify the display compositor about shared bitmaps.
    This doesn't work UseSurfaceLayerForVideo where there is no
    LayerTreeFrameSink.
    
    Pull the two required functions out of LayerTreeFrameSink and into
    SharedBitmapReporter. LayerTreeFrameSink has SharedBitmapReporter as a
    subclass and doesn't implement the functions so nothing changes with
    regards to it. VideoResourceUpdater now takes a SharedBitmapReporter
    instead of a LayerTreeFrameSink.
    
    An implementation of SharedBitmapReporter that calls into
    mojom::CompositorFrameSinkPtr can be added trivially to support
    UseSurfaceLayerInVideo after this patch.
    
    Bug: 807840
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I9a5a07140f8aa1614d1b647f3af1dd5d7aab2cd2
    Reviewed-on: https://chromium-review.googlesource.com/1053054
    Commit-Queue: kylechar <kylechar@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558011}

--- cc/layers/video_layer_impl.cc
+++ cc/layers/video_layer_impl.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "cc/layers/video_frame_provider_client_impl.h"
 #include "cc/resources/resource_provider.h"
+#include "cc/trees/layer_tree_frame_sink.h"
 #include "cc/trees/layer_tree_impl.h"
 #include "cc/trees/occlusion.h"
 #include "cc/trees/task_runner_provider.h"
