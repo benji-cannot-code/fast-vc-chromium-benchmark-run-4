02209a4523de8a5138863936672607bf59e135d8
Bence Béky <bnc@chromium.org>
2018-05-11 00:54:05 +0000

    Validate HTTP/2 push headers even if they are late.
    Validate response headers of an HTTP/2 pushed stream against the client
    request headers even if the response headers only arrive after the
    pushed stream has been claimed by a request.  Cancel stream and retry
    request in HttpNetworkTransaction if headers do not match.
    
    Bug: 554220
    Change-Id: I5e1a42583b941faef5529b50178fe00d8c864976
    Reviewed-on: https://chromium-review.googlesource.com/1053429
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Bence Béky <bnc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557745}

--- net/spdy/chromium/bidirectional_stream_spdy_impl.cc
+++ net/spdy/chromium/bidirectional_stream_spdy_impl.cc
@@ -208,7 +208,8 @@ void BidirectionalStreamSpdyImpl::OnHeadersSent() {
 }
 
 void BidirectionalStreamSpdyImpl::OnHeadersReceived(
-    const SpdyHeaderBlock& response_headers) {
+    const SpdyHeaderBlock& response_headers,
+    const SpdyHeaderBlock* pushed_request_headers) {
   DCHECK(stream_);
 
   if (delegate_)
--- net/spdy/chromium/spdy_stream_test_util.h
+++ net/spdy/chromium/spdy_stream_test_util.h
@@ -30,7 +30,9 @@ class ClosingDelegate : public SpdyStream::Delegate {
 
   // SpdyStream::Delegate implementation.
   void OnHeadersSent() override;
-  void OnHeadersReceived(const SpdyHeaderBlock& response_headers) override;
+  void OnHeadersReceived(
+      const SpdyHeaderBlock& response_headers,
+      const SpdyHeaderBlock* pushed_request_headers) override;
   void OnDataReceived(std::unique_ptr<SpdyBuffer> buffer) override;
   void OnDataSent() override;
   void OnTrailers(const SpdyHeaderBlock& trailers) override;
@@ -52,7 +54,9 @@ class StreamDelegateBase : public SpdyStream::Delegate {
   ~StreamDelegateBase() override;
 
   void OnHeadersSent() override;
-  void OnHeadersReceived(const SpdyHeaderBlock& response_headers) override;
+  void OnHeadersReceived(
+      const SpdyHeaderBlock& response_headers,
+      const SpdyHeaderBlock* pushed_request_headers) override;
   void OnDataReceived(std::unique_ptr<SpdyBuffer> buffer) override;
   void OnDataSent() override;
   void OnTrailers(const SpdyHeaderBlock& trailers) override;
@@ -105,7 +109,9 @@ class StreamDelegateSendImmediate : public StreamDelegateBase {
                               SpdyStringPiece data);
   ~StreamDelegateSendImmediate() override;
 
-  void OnHeadersReceived(const SpdyHeaderBlock& response_headers) override;
+  void OnHeadersReceived(
+      const SpdyHeaderBlock& response_headers,
+      const SpdyHeaderBlock* pushed_request_headers) override;
 
  private:
   SpdyStringPiece data_;
@@ -131,7 +137,9 @@ class StreamDelegateCloseOnHeaders : public StreamDelegateBase {
       const base::WeakPtr<SpdyStream>& stream);
   ~StreamDelegateCloseOnHeaders() override;
 
-  void OnHeadersReceived(const SpdyHeaderBlock& response_headers) override;
+  void OnHeadersReceived(
+      const SpdyHeaderBlock& response_headers,
+      const SpdyHeaderBlock* pushed_request_headers) override;
 };
 
 }  // namespace test
