d80be32ade5d3c8f71973545d39f24adc601fa1e
Emil A Eklund <eae@chromium.org>
2018-05-11 00:34:08 +0000

    Change BoxModelObjectPainter to not store flow size
    Refactor BoxPainterBase and BoxModelObjectPainter to supply flowbox size
    as parameter to PaintFillLayer instead of storing it as a private field.
    
    This will in turn help with code re-use between InlineFlowBoxPainter and
    the LayoutNG equivalent.
    
    Bug: 714962
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I342c51c87eb253e2d6420f2b11440b77c564f67d
    Reviewed-on: https://chromium-review.googlesource.com/1054133
    Reviewed-by: Aleks Totic <atotic@chromium.org>
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557738}

--- third_party/blink/renderer/core/paint/box_model_object_painter.h
+++ third_party/blink/renderer/core/paint/box_model_object_painter.h
@@ -26,8 +26,7 @@ class BoxModelObjectPainter : public BoxPainterBase {
 
  public:
   BoxModelObjectPainter(const LayoutBoxModelObject&,
-                        const InlineFlowBox* = nullptr,
-                        const LayoutSize& flow_box_size = LayoutSize());
+                        const InlineFlowBox* = nullptr);
 
   static bool IsPaintingBackgroundOfPaintContainerIntoScrollingContentsLayer(
       const LayoutBoxModelObject*,
@@ -46,14 +45,14 @@ class BoxModelObjectPainter : public BoxPainterBase {
                                       const BoxPainterBase::FillLayerInfo&,
                                       const LayoutRect&) override;
   FloatRoundedRect GetBackgroundRoundedRect(
-      const LayoutRect&,
+      const LayoutRect& border_rect,
+      const LayoutSize& flow_box_size,
       bool include_logical_left_edge,
       bool include_logical_right_edge) const override;
 
  private:
   const LayoutBoxModelObject& box_model_;
   const InlineFlowBox* flow_box_;
-  const LayoutSize flow_box_size_;
 };
 
 }  // namespace blink
--- third_party/blink/renderer/core/paint/box_painter_base.h
+++ third_party/blink/renderer/core/paint/box_painter_base.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_PAINT_BOX_PAINTER_BASE_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_PAINT_BOX_PAINTER_BASE_H_
 
+#include "base/optional.h"
 #include "third_party/blink/renderer/core/layout/background_bleed_avoidance.h"
 #include "third_party/blink/renderer/core/style/style_image.h"
 #include "third_party/blink/renderer/platform/geometry/layout_rect_outsets.h"
@@ -63,7 +64,8 @@ class BoxPainterBase {
                       const LayoutRect&,
                       BackgroundBleedAvoidance,
                       BackgroundImageGeometry&,
-                      SkBlendMode = SkBlendMode::kSrcOver);
+                      SkBlendMode = SkBlendMode::kSrcOver,
+                      const LayoutSize flow_box_size = LayoutSize());
 
   void PaintMaskImages(const PaintInfo&,
                        const LayoutRect&,
@@ -143,6 +145,7 @@ class BoxPainterBase {
  protected:
   FloatRoundedRect BackgroundRoundedRectAdjustedForBleedAvoidance(
       const LayoutRect& border_rect,
+      const LayoutSize& flow_box_size,
       BackgroundBleedAvoidance,
       bool include_logical_left_edge,
       bool include_logical_right_edge) const;
@@ -150,6 +153,7 @@ class BoxPainterBase {
       const FillLayerInfo&,
       const FillLayer&,
       const LayoutRect&,
+      const LayoutSize& flow_box_size,
       BackgroundBleedAvoidance,
       LayoutRectOutsets border_padding_insets) const;
 
@@ -180,6 +184,7 @@ class BoxPainterBase {
                                          BackgroundBleedAvoidance) const = 0;
   virtual FloatRoundedRect GetBackgroundRoundedRect(
       const LayoutRect& border_rect,
+      const LayoutSize& flow_box_size,
       bool include_logical_left_edge,
       bool include_logical_right_edge) const;
 
