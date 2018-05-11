d763c5ad2e88dce751bd6c51228353d6d5d138ee
Chris Harrelson <chrishtr@chromium.org>
2018-05-11 23:01:36 +0000

    [ForeignObject PaintLayer] Fix <foreignObject> hit testing under simple SVG clip-path.
    Now that <foreignObject> gets a PaintLayer, hit testing no longer works correctly
    in the presence of a non-rectangular SVG clip path. This is because SVG children elements
    which are not <foreignObject> cannot have a PaintLayer, and PaintLayer is the only way
    hit testing currently knows how to account for clip-path.
    
    Fix this for "simple" clip-paths by adding a method to perform hit test clipping
    on the clip paint property tree. It works by checking all clips and clip-paths
    stored on the clip paint property tree between the local and ancestor points
    (in this use-case the ancestor is the containing PaintLayer), applying transforms
    as necessary.
    
    Non-"simple" clip paths (defined as SVG clip paths which have more than 42 edges
    or having text) are not fixed by this CL. The former condition could be handled in the
    future by adding a ClipPathForHitTesting to ClipPaintPropertyNode which stores the
    clip-path beyond 42 edges.
    
    Bug:833076
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Iabbce97d412fe38a4b775739517d28628ef67964
    Reviewed-on: https://chromium-review.googlesource.com/1055788
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Tien-Ren Chen <trchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558049}

--- third_party/blink/renderer/core/paint/paint_layer.cc
+++ third_party/blink/renderer/core/paint/paint_layer.cc
@@ -86,6 +86,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/geometry/transform_state.h"
 #include "third_party/blink/renderer/platform/graphics/compositor_filter_operations.h"
 #include "third_party/blink/renderer/platform/graphics/filters/filter.h"
+#include "third_party/blink/renderer/platform/graphics/paint/geometry_mapper.h"
 #include "third_party/blink/renderer/platform/instrumentation/tracing/trace_event.h"
 #include "third_party/blink/renderer/platform/length_functions.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
@@ -1978,6 +1979,17 @@ PaintLayer* PaintLayer::HitTestLayer(
   }
 
   if (HitTestClippedOutByClipPath(root_layer, hit_test_location))
+    return nullptr;
+
+  // TODO(chrishtr): this can have incorrect results for rects that are not
+  // unit-sized due to use of Center().
+  if (GetLayoutObject().IsSVGForeignObject() &&
+      !GeometryMapper::PointVisibleInAncestorSpace(
+          GetLayoutObject().FirstFragment().LocalBorderBoxProperties(),
+          container_layer->GetLayoutObject()
+              .FirstFragment()
+              .LocalBorderBoxProperties(),
+          FloatPoint(hit_test_location.BoundingBox().Center())))
     return nullptr;
 
   // The natural thing would be to keep HitTestingTransformState on the stack,
