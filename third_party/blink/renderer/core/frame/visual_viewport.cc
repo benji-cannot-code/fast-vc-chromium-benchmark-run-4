ae104adcbfb21ba6ea44a6232e81cae278f0e083
Jeremy Roman <jbroman@chromium.org>
2018-05-12 03:25:37 +0000

    Replace remaining uses of WebScrollbar::Orientation with ScrollbarOrientation.
    Bug: 841751
    Change-Id: I965b442ef21d0f199574d8d4ff04fc03af7158b9
    Reviewed-on: https://chromium-review.googlesource.com/1054248
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558104}

--- third_party/blink/renderer/core/frame/visual_viewport.cc
+++ third_party/blink/renderer/core/frame/visual_viewport.cc
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/layers/scrollbar_layer_interface.h"
 #include "third_party/blink/public/platform/task_type.h"
 #include "third_party/blink/public/platform/web_layer.h"
-#include "third_party/blink/public/platform/web_scrollbar.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/frame/local_frame_client.h"
 #include "third_party/blink/renderer/core/frame/local_frame_view.h"
@@ -426,8 +425,8 @@ void VisualViewport::InitializeScrollbars() {
       inner_viewport_container_layer_->AddChild(
           overlay_scrollbar_vertical_.get());
 
-    SetupScrollbar(WebScrollbar::kHorizontal);
-    SetupScrollbar(WebScrollbar::kVertical);
+    SetupScrollbar(kHorizontalScrollbar);
+    SetupScrollbar(kVerticalScrollbar);
   } else {
     overlay_scrollbar_horizontal_->RemoveFromParent();
     overlay_scrollbar_vertical_->RemoveFromParent();
@@ -441,8 +440,8 @@ void VisualViewport::InitializeScrollbars() {
     frame->View()->VisualViewportScrollbarsChanged();
 }
 
-void VisualViewport::SetupScrollbar(WebScrollbar::Orientation orientation) {
-  bool is_horizontal = orientation == WebScrollbar::kHorizontal;
+void VisualViewport::SetupScrollbar(ScrollbarOrientation orientation) {
+  bool is_horizontal = orientation == kHorizontalScrollbar;
   GraphicsLayer* scrollbar_graphics_layer =
       is_horizontal ? overlay_scrollbar_horizontal_.get()
                     : overlay_scrollbar_vertical_.get();
@@ -464,10 +463,8 @@ void VisualViewport::SetupScrollbar(WebScrollbar::Orientation orientation) {
   if (!scrollbar_layer_group) {
     ScrollingCoordinator* coordinator = GetPage().GetScrollingCoordinator();
     DCHECK(coordinator);
-    ScrollbarOrientation webcore_orientation =
-        is_horizontal ? kHorizontalScrollbar : kVerticalScrollbar;
     scrollbar_layer_group = coordinator->CreateSolidColorScrollbarLayer(
-        webcore_orientation, thumb_thickness, scrollbar_margin, false);
+        orientation, thumb_thickness, scrollbar_margin, false);
 
     // The compositor will control the scrollbar's visibility. Set to invisible
     // by default so scrollbars don't show up in layout tests.
