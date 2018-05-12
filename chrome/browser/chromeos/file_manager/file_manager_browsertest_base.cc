76eb6924c68d6f0861b9d046a5f0ccb93978c7ad
Noel Gordon <noel@chromium.org>
2018-05-12 07:17:48 +0000

    Add #include <utility> for std::move
    Code is using std::move, defined in <utility>. Maybe clang-tidy has a
    feature for this, but anyho.
    
    Tbr: slangley
    Bug: 841659
    Change-Id: I01d81203a77e96bc0115980eda43a442944abb31
    Reviewed-on: https://chromium-review.googlesource.com/1056508
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558123}

--- chrome/browser/chromeos/file_manager/file_manager_browsertest_base.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest_base.cc
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/file_manager/file_manager_browsertest_base.h"
 
 #include <stddef.h>
+
 #include <memory>
+#include <utility>
 
 #include "base/containers/circular_deque.h"
 #include "base/json/json_reader.h"
