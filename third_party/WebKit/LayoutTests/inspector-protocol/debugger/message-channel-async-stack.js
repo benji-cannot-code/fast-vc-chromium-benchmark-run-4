9636248e4afe71357bca76ec7d78936da4cec932
Alexey Kozyatinskiy <kozyatinskiy@chromium.org>
2018-05-12 18:28:17 +0000

    [DevTools] prepare tests for better async breakpoints locations
    With V8 side CL [1] we will start using proper break location instead
    of stack check location almost all the time, e.g. for async step-into
    and for a lot of DOM breakpoints.
    
    [1] https://chromium-review.googlesource.com/c/v8/v8/+/1054155
    
    R=dgozman@chromium.org
    
    Bug: v8:7753
    Change-Id: I550d4d43494df7abe8c5fcd0fbb1d08ca88364f6
    Reviewed-on: https://chromium-review.googlesource.com/1055791
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Aleksey Kozyatinskiy <kozyatinskiy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558138}

--- third_party/WebKit/LayoutTests/inspector-protocol/debugger/message-channel-async-stack.js
+++ third_party/WebKit/LayoutTests/inspector-protocol/debugger/message-channel-async-stack.js
@@ -43,10 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       {callFrames, parent: asyncStackTrace, parentId: asyncStackTraceId},
       debuggerId);
 
-  dp.Debugger.stepInto({breakOnAsyncCall: true});
-  ({params: {asyncCallStackTraceId, callFrames}} =
-       await dp.Debugger.oncePaused());
-
   testRunner.log('\nrequested stepInto with breakOnAsyncCall flag');
   dp.Debugger.stepInto({breakOnAsyncCall: true});
   ({params: {asyncCallStackTraceId, callFrames}} =
