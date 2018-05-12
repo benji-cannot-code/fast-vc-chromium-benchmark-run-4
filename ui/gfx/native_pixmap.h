d3a97255aefd2f81c8efaf70e8f239f354ff6104
Alexandros Frantzis <alexandros.frantzis@collabora.com>
2018-05-12 16:31:36 +0000

    gfx: Prepare NativePixmap to use overlay plane fences
    Add a parameter for an unowned GpuFence to the
    NativePixmap::ScheduleOverlayPlane() method. The parameter is unused at
    the moment, but will be used in upcoming commits to wire the overlay
    plane GpuFences in Ozone-DRM.
    
    Bug: 828393
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I4697764c88a307b307af0ca5c1305e2d83cceec4
    Reviewed-on: https://chromium-review.googlesource.com/1021737
    Commit-Queue: Daniele Castagna <dcastagna@chromium.org>
    Reviewed-by: David Reveman <reveman@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558136}

--- ui/gfx/native_pixmap.h
+++ ui/gfx/native_pixmap.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace gfx {
 class Rect;
 class RectF;
+class GpuFence;
 
 // This represents a buffer that can be directly imported via GL for
 // rendering, or exported via dma-buf fds.
@@ -55,12 +56,15 @@ class NativePixmap : public base::RefCountedThreadSafe<NativePixmap> {
   // range of [0,1].
   // |enable_blend| specifies if the plane should be alpha blended, with premul
   // apha, when scanned out.
+  // |gpu_fence| specifies a gpu fence to wait on before the pixmap is ready
+  // to be displayed.
   virtual bool ScheduleOverlayPlane(gfx::AcceleratedWidget widget,
                                     int plane_z_order,
                                     gfx::OverlayTransform plane_transform,
                                     const gfx::Rect& display_bounds,
                                     const gfx::RectF& crop_rect,
-                                    bool enable_blend) = 0;
+                                    bool enable_blend,
+                                    gfx::GpuFence* gpu_fence) = 0;
 
   // Export the buffer for sharing across processes.
   // Any file descriptors in the exported handle are owned by the caller.
