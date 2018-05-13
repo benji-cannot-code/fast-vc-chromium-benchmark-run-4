8a59862dd3cf2e62ad827c772990f38c50e5a987
Jinsuk Kim <jinsukkim@chromium.org>
2018-05-13 01:05:51 +0000

    Android: Move cipher lib to Chrome
    ByteArrayGenerator, CipherFactory are used by Chrome only, and
    don't have to be in content layer. This CL moves it to chrome
    directory, and deletes the corresponding DEPS exception rules.
    
    Bug: 617324
    Change-Id: Iee5d8effe2148ef3e50375e8848048a58a0cc046
    Reviewed-on: https://chromium-review.googlesource.com/1049769
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Chris Palmer <palmer@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558149}

--- chrome/android/javatests/src/org/chromium/chrome/browser/crypto/CipherFactoryTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/crypto/CipherFactoryTest.java
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.content.browser.crypto;
+package org.chromium.chrome.browser.crypto;
 
 import android.os.Bundle;
 import android.support.test.filters.MediumTest;
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.BaseJUnit4ClassRunner;
-import org.chromium.content.browser.crypto.CipherFactory.CipherDataObserver;
+import org.chromium.chrome.browser.crypto.CipherFactory.CipherDataObserver;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
