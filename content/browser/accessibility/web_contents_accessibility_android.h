d47447e10a178dfe2e7be8f96b0b10c7f3ec22c2
Jinsuk Kim <jinsukkim@chromium.org>
2018-05-13 23:22:46 +0000

    Android: Fix wrong tap postion in a11y mode
    https://crrev.com/c/517683 introduced a bug in accessibility mode
    when page scale factor is not 1. Before the patch, the hover event
    coordinate is converted to CSS (https://goo.gl/ER8wu5) but after
    the patch the unit was DIP by mistake. (https://goo.gl/ER8wu5).
    This CL fixes it by applying the page scale factor to set the unit
    right to CSS.
    
    Bug: 840609
    Change-Id: Iaa17882e2ffb2d5637e59df1ebe7cf677f4a76a3
    Reviewed-on: https://chromium-review.googlesource.com/1055067
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558165}

--- content/browser/accessibility/web_contents_accessibility_android.h
+++ content/browser/accessibility/web_contents_accessibility_android.h
@@ -214,7 +214,7 @@ class CONTENT_EXPORT WebContentsAccessibilityAndroid
       jint start,
       jint len);
 
-  void UpdateFrameInfo();
+  void UpdateFrameInfo(float page_scale);
 
   void set_root_manager(BrowserAccessibilityManagerAndroid* manager) {
     root_manager_ = manager;
@@ -253,6 +253,8 @@ class CONTENT_EXPORT WebContentsAccessibilityAndroid
   WebContentsImpl* const web_contents_;
 
   bool frame_info_initialized_;
+
+  float page_scale_ = 1.f;
 
   BrowserAccessibilityManagerAndroid* root_manager_;
 
