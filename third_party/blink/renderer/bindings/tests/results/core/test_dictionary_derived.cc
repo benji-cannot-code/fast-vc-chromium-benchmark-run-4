ecd3bcf190275c83b861277dd7a0a7f964e1c6d4
Rayan Kanso <rayankans@chromium.org>
2018-05-14 14:04:16 +0000

    Fix IDL compiler dictionary ImplementedAs bug
    The dependencies in the build rule use the name of the idl file
    containing the dictionary defintion. For the cases where ImplementedAs
    is used, the build rule will fail since the generated file matches the
    ImplementedAs value.
    
    This CL changes the file name of the generated dictionary implementation
    to match the idl name rather than the ImplementedAs name.
    
    Bug: 839847
    Change-Id: Ic5d9dfc82ba90188e26f1206465eb790dc8a193b
    Reviewed-on: https://chromium-review.googlesource.com/1044217
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558291}

--- third_party/blink/renderer/bindings/tests/results/core/test_dictionary_derived.cc
+++ third_party/blink/renderer/bindings/tests/results/core/test_dictionary_derived.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // DO NOT MODIFY!
 
 // clang-format off
-#include "test_dictionary_derived_implemented_as.h"
+#include "test_dictionary_derived.h"
 
 namespace blink {
 
