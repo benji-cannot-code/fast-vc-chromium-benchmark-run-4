3df792d145d4c6f15022b310a465bab0c7cf2f02
sunxd <sunxd@chromium.org>
2018-05-14 20:48:32 +0000

    Rename SurfaceLayer::hit_testable_ to surface_hit_testable_
    We want to propagate pointer-events: none information to cc to generate
    correct hit testing data for viz. The plan is to have blink set the hit
    testable bit on cc::Layers. In order to disambiguate the bit in surface
    layer that represents the origin of the surface, we want to rename the
    variable.
    
    Bug: 841358
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I207815dcfad0f29b222b0a39c350f1e926aedac9
    Reviewed-on: https://chromium-review.googlesource.com/1052506
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Xianda Sun <sunxd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558450}

--- ui/android/delegated_frame_host_android.cc
+++ ui/android/delegated_frame_host_android.cc
@@ -51,7 +51,7 @@ scoped_refptr<cc::SurfaceLayer> CreateSurfaceLayer(
   layer->SetBounds(size_in_pixels);
   layer->SetIsDrawable(true);
   layer->SetContentsOpaque(surface_opaque);
-  layer->SetHitTestable(true);
+  layer->SetSurfaceHitTestable(true);
 
   return layer;
 }
