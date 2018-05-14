bf8355ef2c289fc3e313d7f8d01d7e07f0acde8e
Wenzhao Zang <wzang@chromium.org>
2018-05-14 18:58:44 +0000

    cros: Additional UI polishing for new wallpaper picker
    1) Add animation for showing preview wallpaper/wallpaper thumbnails.
    
    2) Set min height/width for the picker window.
    
    3) Add "Preset category" for the new picker.
    
    Bug: 837485, 837329, 836848, 835352, 835196
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ifddc5e2af900b7c9b9f8e0001b605f70d2f9b079
    Reviewed-on: https://chromium-review.googlesource.com/1054141
    Commit-Queue: Wenzhao (Colin) Zang <wzang@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558396}

--- chrome/browser/resources/chromeos/wallpaper_manager/js/event_page.js
+++ chrome/browser/resources/chromeos/wallpaper_manager/js/event_page.js
@@ -324,10 +324,10 @@ chrome.app.runtime.onLaunched.addListener(function() {
     return;
   }
 
-  chrome.commandLinePrivate.hasSwitch('new-wallpaper-picker', (result) => {
+  chrome.commandLinePrivate.hasSwitch('new-wallpaper-picker', result => {
     var options = result ? {
       frame: 'none',
-      innerBounds: {width: 768, height: 512},
+      innerBounds: {width: 768, height: 512, minWidth: 480, minHeight: 480},
       resizable: true,
       alphaEnabled: true
     } :
@@ -417,6 +417,17 @@ chrome.storage.onChanged.addListener(function(changes, namespace) {
         if (!wallpaperPickerWindow)
           return;
         var wpDocument = wallpaperPickerWindow.contentWindow.document;
+        var hideCheckMarkIfNeeded = () => {
+          chrome.commandLinePrivate.hasSwitch(
+              'new-wallpaper-picker', result => {
+                // Do not hide the check mark on the new picker.
+                if (!result && wpDocument.querySelector('.check')) {
+                  wpDocument.querySelector('.check').style.visibility =
+                      'hidden';
+                }
+              });
+        };
+
         if (!!appName) {
           chrome.wallpaperPrivate.getStrings(function(strings) {
             var message =
@@ -424,8 +435,7 @@ chrome.storage.onChanged.addListener(function(changes, namespace) {
             wpDocument.querySelector('#wallpaper-set-by-message').textContent =
                 message;
             wpDocument.querySelector('#wallpaper-grid').classList.add('small');
-            if (wpDocument.querySelector('.check'))
-              wpDocument.querySelector('.check').style.visibility = 'hidden';
+            hideCheckMarkIfNeeded();
             wpDocument.querySelector('#checkbox').classList.remove('checked');
             wpDocument.querySelector('#categories-list').disabled = false;
             wpDocument.querySelector('#wallpaper-grid').disabled = false;
@@ -443,9 +453,7 @@ chrome.storage.onChanged.addListener(function(changes, namespace) {
                 if (enable) {
                   wpDocument.querySelector('#checkbox')
                       .classList.add('checked');
-                  if (wpDocument.querySelector('.check'))
-                    wpDocument.querySelector('.check').style.visibility =
-                        'hidden';
+                  hideCheckMarkIfNeeded();
                 } else {
                   wpDocument.querySelector('#checkbox')
                       .classList.remove('checked');
