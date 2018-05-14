3b719a723fb6468d62406f5b73030dbbfabd132e
Noel Gordon <noel@chromium.org>
2018-05-14 02:23:26 +0000

    FileManagerBrowserTest: change test name tabindexSearchBoxFocus
    Allows use to group "tabindex*" tests together (and eventually get rid
    of the FileManagerBrowserTestWithLegacyEventDisapatch class).
    
    Bug: 836254
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ifd4c81458e853799e8a59e1185f8745fb27f8da8
    Reviewed-on: https://chromium-review.googlesource.com/1056768
    Reviewed-by: Stuart Langley <slangley@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558176}

--- ui/file_manager/integration_tests/file_manager/tab_index.js
+++ ui/file_manager/integration_tests/file_manager/tab_index.js
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /**
  * Tests the focus behavior of the search box.
  */
-testcase.searchBoxFocus = function() {
+testcase.tabindexSearchBoxFocus = function() {
   var appId;
   StepsRunner.run([
     // Set up File Manager.
