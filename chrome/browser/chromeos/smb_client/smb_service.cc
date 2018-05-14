68381b7fefafbc33ef6d446f13aa783714cba3a0
Bailey Berro <baileyberro@chromium.org>
2018-05-14 16:49:16 +0000

    Pass ChromAD user credentials to SmbProvider Mount
    The username and workgroup of ChromAD users will now be passed to
    the SmbProvider Mount function in the smbprovider Dameon.
    
    The ParseUserPrincipalName function and corresponding tests are adapted
    from AuthPolicy's SambaHelper.
    
    BUG=chromium:757625
    TEST=unittests
    
    Change-Id: Ia25396224d9c873161adb2da3741c54cdd1209cf
    Reviewed-on: https://chromium-review.googlesource.com/1052914
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Bailey Berro <baileyberro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558339}

--- chrome/browser/chromeos/smb_client/smb_service.cc
+++ chrome/browser/chromeos/smb_client/smb_service.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/smb_client/smb_file_system_id.h"
 #include "chrome/browser/chromeos/smb_client/smb_provider.h"
 #include "chrome/browser/chromeos/smb_client/smb_service_factory.h"
+#include "chrome/browser/chromeos/smb_client/smb_service_helper.h"
 #include "chrome/common/chrome_features.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/dbus/smb_provider_client.h"
@@ -70,10 +71,19 @@ void SmbService::InitTempFileManagerAndMount(
 void SmbService::CallMount(const file_system_provider::MountOptions& options,
                            const base::FilePath& share_path,
                            MountResponse callback) {
+  std::string workgroup;
+  std::string username;
+
+  user_manager::User* user =
+      chromeos::ProfileHelper::Get()->GetUserByProfile(profile_);
+  if (user && user->IsActiveDirectoryUser()) {
+    ParseUserPrincipalName(user->GetDisplayEmail(), &username, &workgroup);
+  }
+
   // TODO(allenvic): Implement passing of credentials. This currently passes
-  // empty credentials to SmbProvider.
+  // empty credentials to SmbProvider for non-ChromAD users.
   GetSmbProviderClient()->Mount(
-      share_path, "" /* workgroup */, "" /* username */,
+      share_path, workgroup, username,
       temp_file_manager_->WritePasswordToFile("" /* password */),
       base::BindOnce(&SmbService::OnMountResponse, AsWeakPtr(),
                      base::Passed(&callback), options, share_path));
---
+++ chrome/browser/chromeos/smb_client/smb_service_helper.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_CHROMEOS_SMB_CLIENT_SMB_SERVICE_HELPER_H_
+#define CHROME_BROWSER_CHROMEOS_SMB_CLIENT_SMB_SERVICE_HELPER_H_
+
+#include <string>
+#include <vector>
+
+#include <base/logging.h>
+
+namespace chromeos {
+namespace smb_client {
+
+bool ParseUserPrincipalName(const std::string& user_principal_name,
+                            std::string* user_name,
+                            std::string* workgroup);
+
+}  // namespace smb_client
+}  // namespace chromeos
+
+#endif  // CHROME_BROWSER_CHROMEOS_SMB_CLIENT_SMB_SERVICE_HELPER_H_
