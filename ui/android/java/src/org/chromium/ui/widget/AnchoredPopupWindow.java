8652813d65418d425ac7b1892e8e5a40d9504d7a
Amirhossein Simjour <asimjour@chromium.org>
2018-05-14 22:22:48 +0000

    Fix minor typo in AnchoredPopupWindow.setFocusable
    Before this patch, setFocusable was ignoring its input params.
    There is no place that we call setFocusable(false), so this doesn't
    affect anything at the moment.
    
    Bug: None
    Change-Id: Id3cc348e1d5dba144ee44e08976785dd903c840d
    Reviewed-on: https://chromium-review.googlesource.com/1055655
    Commit-Queue: Amirhossein Simjour <asimjour@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558492}

--- ui/android/java/src/org/chromium/ui/widget/AnchoredPopupWindow.java
+++ ui/android/java/src/org/chromium/ui/widget/AnchoredPopupWindow.java
@@ -300,7 +300,7 @@ public class AnchoredPopupWindow implements OnTouchListener, RectProvider.Observ
      * @param focusable True if the popup is focusable, false otherwise.
      */
     public void setFocusable(boolean focusable) {
-        mPopupWindow.setFocusable(true);
+        mPopupWindow.setFocusable(focusable);
     }
 
     /**
