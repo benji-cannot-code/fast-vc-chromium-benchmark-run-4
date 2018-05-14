4f4846e48b57f499c228dbcd388f638571b3deb7
Sahel Sharify <sahel@chromium.org>
2018-05-14 15:33:29 +0000

    New tracing for browser side fling.
    This cl adds the equivalent of fling traces in InputHandlerProxy to the
    FlingController class.
    
    Bug: 249063
    Change-Id: I6a5478d6e02fe47f729b8310659997eb4b4dc485
    Reviewed-on: https://chromium-review.googlesource.com/1052900
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Navid Zolghadr <nzolghadr@chromium.org>
    Commit-Queue: Sahel Sharifymoghaddam <sahel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558318}

--- content/browser/renderer_host/input/fling_controller.cc
+++ content/browser/renderer_host/input/fling_controller.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/renderer_host/input/fling_controller.h"
 
+#include "base/trace_event/trace_event.h"
 #include "content/browser/renderer_host/input/gesture_event_queue.h"
 #include "content/public/common/content_features.h"
 #include "ui/compositor/compositor.h"
@@ -22,6 +23,8 @@ namespace {
 // progress.
 constexpr base::TimeDelta kMaxMicrosecondsFromFlingTimestampToFirstProgress =
     base::TimeDelta::FromMicroseconds(33333);
+
+const char* kFlingTraceName = "FlingController::HandlingGestureFling";
 }  // namespace
 
 namespace content {
@@ -117,10 +120,30 @@ bool FlingController::FilterGestureEventForFlingBoosting(
     CancelCurrentFling();
   }
 
-  if (should_filter_event &&
-      gesture_event.event.GetType() == WebInputEvent::kGestureFlingStart) {
-    UpdateCurrentFlingState(gesture_event.event,
-                            fling_booster_->current_fling_velocity());
+  if (should_filter_event) {
+    if (gesture_event.event.GetType() == WebInputEvent::kGestureFlingStart) {
+      UpdateCurrentFlingState(gesture_event.event,
+                              fling_booster_->current_fling_velocity());
+      TRACE_EVENT_INSTANT2("input",
+                           fling_booster_->fling_boosted()
+                               ? "FlingController::FlingBoosted"
+                               : "FlingController::FlingReplaced",
+                           TRACE_EVENT_SCOPE_THREAD, "vx",
+                           fling_booster_->current_fling_velocity().x(), "vy",
+                           fling_booster_->current_fling_velocity().y());
+    } else if (gesture_event.event.GetType() ==
+               WebInputEvent::kGestureFlingCancel) {
+      DCHECK(fling_booster_->fling_cancellation_is_deferred());
+      TRACE_EVENT_INSTANT0("input", "FlingController::FlingBoostStart",
+                           TRACE_EVENT_SCOPE_THREAD);
+    } else if (gesture_event.event.GetType() ==
+                   WebInputEvent::kGestureScrollBegin ||
+               gesture_event.event.GetType() ==
+                   WebInputEvent::kGestureScrollUpdate) {
+      TRACE_EVENT_INSTANT0("input",
+                           "FlingController::ExtendBoostedFlingTimeout",
+                           TRACE_EVENT_SCOPE_THREAD);
+    }
   }
 
   return should_filter_event;
@@ -159,6 +182,8 @@ void FlingController::ProcessGestureFlingStart(
   if (!UpdateCurrentFlingState(gesture_event.event, gfx::Vector2dF(vx, vy)))
     return;
 
+  TRACE_EVENT_ASYNC_BEGIN2("input", kFlingTraceName, this, "vx", vx, "vy", vy);
+
   has_fling_animation_started_ = false;
   fling_in_progress_ = true;
   fling_booster_ = std::make_unique<ui::FlingBooster>(
@@ -184,6 +209,7 @@ void FlingController::ProgressFling(base::TimeTicks current_time) {
   if (!fling_curve_)
     return;
 
+  TRACE_EVENT_ASYNC_STEP_INTO0("input", kFlingTraceName, this, "ProgressFling");
   DCHECK(fling_booster_);
   fling_booster_->set_last_fling_animation_time(
       (current_time - base::TimeTicks()).InSecondsF());
@@ -379,8 +405,10 @@ void FlingController::CancelCurrentFling() {
             scroll_begin_event, ui::LatencyInfo(latency_source_event_type)));
   }
 
-  if (had_active_fling)
+  if (had_active_fling) {
     scheduler_client_->DidStopFlingingOnBrowser(weak_ptr_factory_.GetWeakPtr());
+    TRACE_EVENT_ASYNC_END0("input", kFlingTraceName, this);
+  }
 }
 
 bool FlingController::UpdateCurrentFlingState(
