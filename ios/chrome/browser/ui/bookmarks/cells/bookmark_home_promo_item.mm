60327870ad55807e870a7c0374225629bb585104
Rohit Rao <rohitrao@chromium.org>
2018-05-14 22:02:56 +0000

    [ios] Deletes BookmarkTableView.
    Makes BookmarkHomeViewController subclass UITableViewController instead of
    UIViewController. Moves the loading spinner and empty background view into
    BookmarkHomeViewController.
    
    
    BUG=840381,839439
    
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I935a99313eb3aa7f8701d46d803bea575e5bc89a
    Reviewed-on: https://chromium-review.googlesource.com/1056328
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558484}

--- ios/chrome/browser/ui/bookmarks/cells/bookmark_home_promo_item.mm
+++ ios/chrome/browser/ui/bookmarks/cells/bookmark_home_promo_item.mm
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/foundation_util.h"
 #import "ios/chrome/browser/ui/authentication/signin_promo_view_configurator.h"
 #import "ios/chrome/browser/ui/authentication/signin_promo_view_mediator.h"
-#import "ios/chrome/browser/ui/bookmarks/bookmark_table_view.h"
 #import "ios/chrome/browser/ui/bookmarks/cells/bookmark_table_signin_promo_cell.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
