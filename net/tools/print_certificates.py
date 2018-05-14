4eb65c61488eab031e5b853b9317ea19b08685ca
Matt Mueller <mattm@chromium.org>
2018-05-14 21:43:54 +0000

    net/tools/print_certificates.py: include cert SHA256 hash in header
    Change-Id: I5480084ee5818a3ad813b75427262a266a699d23
    Reviewed-on: https://chromium-review.googlesource.com/1058042
    Reviewed-by: Eric Roman <eroman@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558478}

--- net/tools/print_certificates.py
+++ net/tools/print_certificates.py
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import argparse
 import base64
 import errno
+import hashlib
 import os
 import re
 import subprocess
@@ -259,10 +260,11 @@ def der2ascii_pretty_printer(certificate_der, unused_certificate_number):
   return process_data_with_command(["der2ascii"], certificate_der)
 
 
-def header_pretty_printer(unused_certificate_der, certificate_number):
+def header_pretty_printer(certificate_der, certificate_number):
+  cert_hash = hashlib.sha256(certificate_der).hexdigest()
   return """===========================================
-Certificate%d
-===========================================""" % certificate_number
+Certificate%d: %s
+===========================================""" % (certificate_number, cert_hash)
 
 
 # This is actually just used as a magic value, since pretty_print_certificates
