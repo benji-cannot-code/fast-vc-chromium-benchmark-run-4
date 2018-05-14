323f023e005fcc550472a40b9e07898b08a95c7d
François Beaufort <beaufort.francois@gmail.com>
2018-05-14 15:50:45 +0000

    [Picture-in-Picture] Add UseCounter.
    As part of the origin trial, we should record usage with UseCounter, as that
    can be automatically monitored by the origin trials infrastructure.
    
    Bug: 806249
    Change-Id: I7978d33f57c9d31ecc2b18a410c66475f3971ddb
    Reviewed-on: https://chromium-review.googlesource.com/1055396
    Commit-Queue: François Beaufort <beaufort.francois@gmail.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558324}

--- third_party/blink/renderer/core/html/media/html_video_element.cc
+++ third_party/blink/renderer/core/html/media/html_video_element.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/core/frame/picture_in_picture_controller.h"
 #include "third_party/blink/renderer/core/frame/settings.h"
+#include "third_party/blink/renderer/core/frame/use_counter.h"
 #include "third_party/blink/renderer/core/fullscreen/fullscreen.h"
 #include "third_party/blink/renderer/core/fullscreen/fullscreen_options.h"
 #include "third_party/blink/renderer/core/html/media/media_custom_controls_fullscreen_detector.h"
@@ -573,6 +574,20 @@ WebMediaPlayer::DisplayType HTMLVideoElement::DisplayType() const {
   if (is_picture_in_picture_)
     return WebMediaPlayer::DisplayType::kPictureInPicture;
   return HTMLMediaElement::DisplayType();
+}
+
+void HTMLVideoElement::AddedEventListener(
+    const AtomicString& event_type,
+    RegisteredEventListener& registered_listener) {
+  if (event_type == EventTypeNames::enterpictureinpicture) {
+    UseCounter::Count(GetExecutionContext(),
+                      WebFeature::kEnterPictureInPictureEventListener);
+  } else if (event_type == EventTypeNames::leavepictureinpicture) {
+    UseCounter::Count(GetExecutionContext(),
+                      WebFeature::kLeavePictureInPictureEventListener);
+  }
+
+  HTMLMediaElement::AddedEventListener(event_type, registered_listener);
 }
 
 bool HTMLVideoElement::IsRemotingInterstitialVisible() const {
