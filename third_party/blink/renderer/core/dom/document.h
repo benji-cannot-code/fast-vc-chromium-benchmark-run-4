942c1427dc769a3f7ed91070bcc3041ea3baad84
Patrick Meenan <pmeenan@chromium.org>
2018-05-14 13:50:16 +0000

    Fixed the paint supression to track only render-blocking stylesheets.
    This fixes the logic that updates style and layout to not consider
    in-body CSS as render-blocking when the
    CSSInBodyDoesNotBlockPaintEnabled experiment is enabled.
    
    It also fixes the logic to also look at pending imports (which are
    always render-blocking wether in body or head) and treat them
    the same as render-blocking stylesheets.
    
    It also removes the logic that would ignore pending stylesheets
    deep in the style engine when HaveScriptBlockingStylesheetsLoaded
    was called. It wasn't necessary because IsRenderingReady already
    checks for the ignore flag and led to a lot of inconsistent logic.
    
    This is an update of the unsbmitted CL from last year:
    https://codereview.chromium.org/2823873002/
    
    BUG=707747
    
    Change-Id: Id88b945334d8d029a654e2619004b077b47055ab
    Reviewed-on: https://chromium-review.googlesource.com/1053878
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Patrick Meenan <pmeenan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558286}

--- third_party/blink/renderer/core/dom/document.h
+++ third_party/blink/renderer/core/dom/document.h
@@ -1504,7 +1504,7 @@ class CORE_EXPORT Document : public ContainerNode,
   void ClearFocusedElementTimerFired(TimerBase*);
 
   bool HaveScriptBlockingStylesheetsLoaded() const;
-  bool HaveRenderBlockingStylesheetsLoaded() const;
+  bool HaveRenderBlockingResourcesLoaded() const;
   void StyleResolverMayHaveChanged();
 
   void SetHoverElement(Element*);
