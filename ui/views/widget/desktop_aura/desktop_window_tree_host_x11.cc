347a1216653f2b0b5c2662d130c469487d6bef7f
Joe Downing <joedow@chromium.org>
2018-05-14 17:37:07 +0000

    [KeyboardLock] Adding a SystemKeyboardLock feature
    This feature controls whether the low-level keyboard hook is enabled
    when keyboard lock is requested.  I wanted to separate browser and
    system level keyboard lock functionality as I get closer to launching
    the blink api for keyboard lock.  This will allow us to release
    browser-level keyboard lock w/o the system hooks if needed and will
    also give us the ability to target a finch kill switch for the
    system-level functionality if needed.
    
    BUG=680809
    
    Change-Id: I2d522046c90fd8776bfb1d9c53978f2c41e1c7cc
    Reviewed-on: https://chromium-review.googlesource.com/1046154
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558367}

--- ui/views/widget/desktop_aura/desktop_window_tree_host_x11.cc
+++ ui/views/widget/desktop_aura/desktop_window_tree_host_x11.cc
@@ -1312,6 +1312,7 @@ bool DesktopWindowTreeHostX11::CaptureSystemKeyEventsImpl(
       std::move(dom_codes), GetAcceleratedWidget(),
       base::BindRepeating(&DesktopWindowTreeHostX11::DispatchKeyEvent,
                           base::Unretained(this)));
+
   return keyboard_hook_ != nullptr;
 }
 
