e47c7b5b88a480856daab8bd610a9568b8bbf6f5
Bailey Berro <baileyberro@chromium.org>
2018-05-14 22:50:33 +0000

    Add username and password to SmbHandler
    This change adds username and password as arguments that are passed from
    the Add Smb Share Dialog to Smb Browser Proxy to Smb Handler. This enables
    users to mount shares that require authentication.
    
    Bug: chromium:757625
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I1cbc94e45a85cb6fe048a7456a8ff340c6fcd220
    Reviewed-on: https://chromium-review.googlesource.com/1054694
    Commit-Queue: Bailey Berro <baileyberro@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: Scott Chen <scottchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558501}

--- chrome/test/data/webui/settings/smb_shares_page_tests.js
+++ chrome/test/data/webui/settings/smb_shares_page_tests.js
@@ -12,8 +12,8 @@ class TestSmbBrowserProxy extends TestBrowserProxy {
   }
 
   /** @override */
-  smbMount(smbUrl) {
-    this.methodCalled('smbMount', smbUrl);
+  smbMount(smbUrl, username, password) {
+    this.methodCalled('smbMount', [smbUrl, username, password]);
   }
 }
 
@@ -63,19 +63,29 @@ suite('AddSmbShareDialogTests', function() {
 
   test('ClickAdd', function() {
     const expectedSmbUrl = 'smb://192.168.1.1/testshare';
+    const expectedUsername = 'username';
+    const expectedPassword = 'password';
 
     const url = addDialog.$$('#address');
     expectTrue(!!url);
-
     url.value = expectedSmbUrl;
-    expectTrue(!!addDialog);
+
+    const un = addDialog.$$('#username');
+    expectTrue(!!un);
+    un.value = expectedUsername;
+
+    const pw = addDialog.$$('#password');
+    expectTrue(!!pw);
+    pw.value = expectedPassword;
 
     const addButton = addDialog.$$('.action-button');
     expectTrue(!!addButton);
 
     addButton.click();
-    return smbBrowserProxy.whenCalled('smbMount').then(function(actualSmbUrl) {
-      expectEquals(expectedSmbUrl, actualSmbUrl);
+    return smbBrowserProxy.whenCalled('smbMount').then(function(args) {
+      expectEquals(expectedSmbUrl, args[0]);
+      expectEquals(expectedUsername, args[1]);
+      expectEquals(expectedPassword, args[2]);
     });
   });
 });
