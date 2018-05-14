7d669dfb5892a962253f24f0938e1572b7baf137
Greg Kerr <kerrnel@chromium.org>
2018-05-14 19:31:31 +0000

    Fix non-signal-safe calls in BrokerClient
    The current BrokerClient is not signal-safe because of its use of base::Pickle
    which uses new and delete, and also its use of std::vector, which is not
    technically signal safe either. These are used in messaging IPCs with the
    BrokerHost when a syscall is trapped, thus BrokerClient must be made signal
    safe. This is discussed in https://crbug.com/255063.
    
    This CL creates a new BrokerSimpleMessage class to handle simple IPC messaging
    with strings and ints only. It uses fixed sized messages to simplify logic and
    allow it to allocate all its memory on the stack. It creates send, receive, and
    a synchronous send-and-receive method. It also adds a basic set of unit tests
    to sanity check behavior.
    
    Bug: 255063
    Change-Id: I8077a515921b62969a1b8b173d903f2a118ed186
    Reviewed-on: https://chromium-review.googlesource.com/553400
    Commit-Queue: Greg Kerr <kerrnel@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558415}

--- sandbox/linux/syscall_broker/broker_channel.h
+++ sandbox/linux/syscall_broker/broker_channel.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/scoped_file.h"
 #include "base/macros.h"
+#include "sandbox/sandbox_export.h"
 
 namespace sandbox {
 
@@ -16,7 +17,7 @@ namespace syscall_broker {
 // A small class to create a pipe-like communication channel. It is based on a
 // SOCK_SEQPACKET unix socket, which is connection-based and guaranteed to
 // preserve message boundaries.
-class BrokerChannel {
+class SANDBOX_EXPORT BrokerChannel {
  public:
   typedef base::ScopedFD EndPoint;
   static void CreatePair(EndPoint* reader, EndPoint* writer);
