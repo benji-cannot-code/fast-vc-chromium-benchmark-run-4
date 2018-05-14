eaa50465bde968340db2e2184baa613570d0300a
Hajime Hoshi <hajimehoshi@chromium.org>
2018-05-14 12:47:51 +0000

    Replace TaskType::kUnthrottled with kInternalMedia where media is related
    We plan to eliminate kUnthrottled, kUnspecedTimer and kUnspecedLoading
    and replace them with appropriate kInternal* task types.
    
    Task type guideline: https://bit.ly/2vMAsQ4
    
    Bug: 836410
    Change-Id: Iefcfd45fd251b0931d958b6b9d77a69d42930b1e
    Reviewed-on: https://chromium-review.googlesource.com/1002793
    Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558273}

--- media/blink/webmediaplayer_impl.cc
+++ media/blink/webmediaplayer_impl.cc
@@ -286,7 +286,7 @@ WebMediaPlayerImpl::WebMediaPlayerImpl(
     observer_->SetClient(this);
 
   memory_usage_reporting_timer_.SetTaskRunner(
-      frame_->GetTaskRunner(blink::TaskType::kUnthrottled));
+      frame_->GetTaskRunner(blink::TaskType::kInternalMedia));
 }
 
 WebMediaPlayerImpl::~WebMediaPlayerImpl() {
@@ -1675,7 +1675,7 @@ void WebMediaPlayerImpl::CreateVideoDecodeStatsReporter() {
       base::Bind(&WebMediaPlayerImpl::GetPipelineStatistics,
                  base::Unretained(this)),
       pipeline_metadata_.video_decoder_config,
-      frame_->GetTaskRunner(blink::TaskType::kUnthrottled)));
+      frame_->GetTaskRunner(blink::TaskType::kInternalMedia)));
 
   if (delegate_->IsFrameHidden())
     video_decode_stats_reporter_->OnHidden();
@@ -2846,7 +2846,7 @@ void WebMediaPlayerImpl::CreateWatchTimeReporter() {
       base::BindRepeating(&WebMediaPlayerImpl::GetCurrentTimeInternal,
                           base::Unretained(this)),
       media_metrics_provider_.get(),
-      frame_->GetTaskRunner(blink::TaskType::kUnthrottled)));
+      frame_->GetTaskRunner(blink::TaskType::kInternalMedia)));
   watch_time_reporter_->OnVolumeChange(volume_);
 
   if (delegate_->IsFrameHidden())
