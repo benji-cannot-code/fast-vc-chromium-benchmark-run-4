1c968f8f38b78380eeccf22a3a64f8636d00cd80
Kunihiko Sakamoto <ksakamoto@chromium.org>
2018-05-14 03:08:08 +0000

    Add support for new signed-exchange cert chain format
    This adds a parser for the CBOR certificate chain format defined in [1].
    SignedExchangeCertificateChain::Parse() takes a version enum and selects
    a parser to use. For now, the new parser is used only by tests.
    
    [1] https://wicg.github.io/webpackage/draft-yasskin-http-origin-signed-responses.html#cert-chain-format
    
    Bug: 815024,815025
    Change-Id: Ia554ad3d086dbecd20294bdb6db03f37b60d67d9
    Reviewed-on: https://chromium-review.googlesource.com/1002412
    Commit-Queue: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558182}

--- content/browser/web_package/signed_exchange_certificate_chain_fuzzer.cc
+++ content/browser/web_package/signed_exchange_certificate_chain_fuzzer.cc
@@ -10,8 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-  SignedExchangeCertificateChain::GetCertChainFromMessage(
-      base::make_span(data, size));
+  SignedExchangeCertificateChain::Parse(SignedExchangeVersion::kB0,
+                                        base::make_span(data, size), nullptr);
+  SignedExchangeCertificateChain::Parse(SignedExchangeVersion::kB1,
+                                        base::make_span(data, size), nullptr);
   return 0;
 }
 
--- content/browser/web_package/signed_exchange_consts.h
+++ content/browser/web_package/signed_exchange_consts.h
@@ -11,6 +11,8 @@ namespace content {
 constexpr char kAcceptHeaderSignedExchangeSuffix[] =
     ",application/signed-exchange;v=b0";
 
+enum class SignedExchangeVersion { kB0, kB1 };
+
 // Field names defined in the application/signed-exchange content type:
 // https://wicg.github.io/webpackage/draft-yasskin-httpbis-origin-signed-exchanges-impl.html#application-signed-exchange
 
@@ -23,6 +25,10 @@ constexpr char kSignature[] = "signature";
 constexpr char kStatusKey[] = ":status";
 constexpr char kUrlKey[] = ":url";
 constexpr char kValidityUrlKey[] = "validityUrl";
+constexpr char kCertChainCborMagic[] = u8"\U0001F4DC\u26D3";  // "📜⛓"
+constexpr char kCertKey[] = "cert";
+constexpr char kOcspKey[] = "ocsp";
+constexpr char kSctKey[] = "sct";
 
 }  // namespace content
 
