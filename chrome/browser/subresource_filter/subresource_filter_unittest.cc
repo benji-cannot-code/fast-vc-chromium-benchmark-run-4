93faf2c45d41b0ccf8e3e16aedc6572da57c8be8
Charlie Harrison <csharrison@chromium.org>
2018-05-14 18:39:17 +0000

    Remove SubresourceFilter Rappor
    We have a suitable UKM replacement now.
    
    Bug: None
    Change-Id: Ic628dbc19b69f56d0ee5a4184f8bcb786348c594
    Reviewed-on: https://chromium-review.googlesource.com/1056330
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558384}

--- chrome/browser/subresource_filter/subresource_filter_unittest.cc
+++ chrome/browser/subresource_filter/subresource_filter_unittest.cc
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/subresource_filter/subresource_filter_content_settings_manager.h"
 #include "chrome/browser/subresource_filter/subresource_filter_test_harness.h"
 #include "chrome/test/base/testing_browser_process.h"
-#include "components/rappor/public/rappor_parameters.h"
-#include "components/rappor/test_rappor_service.h"
 #include "components/safe_browsing/db/util.h"
 #include "components/safe_browsing/db/v4_protocol_manager_util.h"
 #include "components/subresource_filter/content/browser/content_activation_list_utils.h"
@@ -234,26 +232,6 @@ TEST_F(SubresourceFilterTest, ToggleOffForceActivation_AfterCommit) {
   // UI should not have shown though.
   histogram_tester.ExpectBucketCount("SubresourceFilter.Actions",
                                      kActionUIShown, 0);
-}
-
-TEST_F(SubresourceFilterTest, UIShown_LogsRappor) {
-  rappor::TestRapporServiceImpl rappor_tester;
-  TestingBrowserProcess::GetGlobal()->SetRapporServiceImpl(&rappor_tester);
-  const char kRapporMetric[] = "SubresourceFilter.UIShown";
-  const GURL url("https://example.test");
-
-  ConfigureAsSubresourceFilterOnlyURL(url);
-  SimulateNavigateAndCommit(url, main_rfh());
-  EXPECT_FALSE(CreateAndNavigateDisallowedSubframe(main_rfh()));
-  EXPECT_TRUE(GetClient()->did_show_ui_for_navigation());
-
-  std::string sample_string;
-  rappor::RapporType type;
-  EXPECT_TRUE(rappor_tester.GetRecordedSampleForMetric(kRapporMetric,
-                                                       &sample_string, &type));
-  EXPECT_EQ(rappor::RapporType::UMA_RAPPOR_TYPE, type);
-  // The host is the same as the etld+1 in this case.
-  EXPECT_EQ(url.host(), sample_string);
 }
 
 TEST_F(SubresourceFilterTest, NotifySafeBrowsing) {
