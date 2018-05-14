87851c70588be5b5707867b26c2f80f904eb8012
Alexandros Frantzis <alexandros.frantzis@collabora.com>
2018-05-14 16:03:02 +0000

    gl: Prepare GLImage to use overlay plane fences
    Add an unowned GpuFence parameter to the GLImage::ScheduleOverlayPlane
    method and forward it to internal consumers (e.g., NativePixmap) where
    needed.
    
    Bug: 828393
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I195f4ee9d0a9af886cb12ce27d2ce26ebd47684a
    Reviewed-on: https://chromium-review.googlesource.com/1021511
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: David Reveman <reveman@chromium.org>
    Commit-Queue: Daniele Castagna <dcastagna@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558331}

--- gpu/command_buffer/service/texture_definition.cc
+++ gpu/command_buffer/service/texture_definition.cc
@@ -46,7 +46,8 @@ class GLImageSync : public gl::GLImage {
                             gfx::OverlayTransform transform,
                             const gfx::Rect& bounds_rect,
                             const gfx::RectF& crop_rect,
-                            bool enable_blend) override;
+                            bool enable_blend,
+                            gfx::GpuFence* gpu_fence) override;
   void SetColorSpace(const gfx::ColorSpace& color_space) override {}
   void Flush() override {}
   void OnMemoryDump(base::trace_event::ProcessMemoryDump* pmd,
@@ -107,7 +108,8 @@ bool GLImageSync::ScheduleOverlayPlane(gfx::AcceleratedWidget widget,
                                        gfx::OverlayTransform transform,
                                        const gfx::Rect& bounds_rect,
                                        const gfx::RectF& crop_rect,
-                                       bool enable_blend) {
+                                       bool enable_blend,
+                                       gfx::GpuFence* gpu_fence) {
   NOTREACHED();
   return false;
 }
--- ui/gl/gl_image_ahardwarebuffer.cc
+++ ui/gl/gl_image_ahardwarebuffer.cc
@@ -43,7 +43,8 @@ bool GLImageAHardwareBuffer::ScheduleOverlayPlane(
     gfx::OverlayTransform transform,
     const gfx::Rect& bounds_rect,
     const gfx::RectF& crop_rect,
-    bool enable_blend) {
+    bool enable_blend,
+    gfx::GpuFence* gpu_fence) {
   return false;
 }
 
