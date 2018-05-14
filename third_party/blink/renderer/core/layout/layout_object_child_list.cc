938761790ab7bf827f59e44b57308229a8359e63
Koji Ishii <kojii@chromium.org>
2018-05-14 17:46:27 +0000

    [LayoutNG] Invalidate inline items when inserted and moved
    Following CL:986982 that optimizes re-layout for inline by
    re-using previous items, this patch invalidates the re-use
    when LayoutText is moved.
    
    LayoutObject::InsertedIntoTree() catches the most cases,
    but we have an optimization to skip the notification when
    LayoutObject is moved within the same tree. The inline
    items need to be invalidated in this case as well, because
    they depend on context and in which inline formatting
    context (LayoutBlockFlow) it belongs to.
    
    Bug: 636993
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: Ia65d3e1e948c4eb0079cf81027e84ac5a90b339e
    Reviewed-on: https://chromium-review.googlesource.com/1056647
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558369}

--- third_party/blink/renderer/core/layout/layout_object_child_list.cc
+++ third_party/blink/renderer/core/layout/layout_object_child_list.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/layout_counter.h"
 #include "third_party/blink/renderer/core/layout/layout_object.h"
 #include "third_party/blink/renderer/core/layout/layout_view.h"
+#include "third_party/blink/renderer/core/layout/ng/inline/layout_ng_text.h"
 #include "third_party/blink/renderer/core/paint/object_paint_invalidator.h"
 
 namespace blink {
@@ -168,9 +169,16 @@ void LayoutObjectChildList::InsertChildNode(LayoutObject* owner,
     last_child_ = new_child;
   }
 
-  if (!owner->DocumentBeingDestroyed() && notify_layout_object) {
-    new_child->InsertedIntoTree();
-    LayoutCounter::LayoutObjectSubtreeAttached(new_child);
+  if (!owner->DocumentBeingDestroyed()) {
+    if (notify_layout_object) {
+      new_child->InsertedIntoTree();
+      LayoutCounter::LayoutObjectSubtreeAttached(new_child);
+    } else if (new_child->IsLayoutNGText()) {
+      // |notify_layout_object| is an optimization to skip notifications when
+      // moving within the same tree. Inline items need to be invalidated even
+      // when moving.
+      ToLayoutNGText(new_child)->InvalidateInlineItems();
+    }
   }
 
   // Propagate the need to notify ancestors down into any
