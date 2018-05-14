47114e74188eb4db0e6e4d47f0f495e9ddd7df03
Biao She <bshe@chromium.org>
2018-05-14 13:03:54 +0000

    Improve webvr frame estimate time
    Previously, we use a heuristic to estimate the time a fence finished.
    We now have  the ability to get the status change time from a fence.
    This CL removed the heuristic and use the status change time. It is more
    accurate.
    
    Bug: NONE
    Change-Id: Iff2f45aa84078301bf3029b31b689936f41d4f4f
    Reviewed-on: https://chromium-review.googlesource.com/1053788
    Commit-Queue: Biao She <bshe@chromium.org>
    Reviewed-by: Klaus Weidner <klausw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558276}

--- chrome/browser/android/vr/vr_shell_gl.h
+++ chrome/browser/android/vr/vr_shell_gl.h
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace gl {
 class GLContext;
 class GLFence;
+class GLFenceAndroidNativeFenceSync;
 class GLFenceEGL;
 class GLImageEGL;
 class GLSurface;
@@ -372,7 +373,8 @@ class VrShellGl : public device::mojom::VRPresentationProvider {
   void OnWebVrFrameTimedOut();
 
   base::TimeDelta GetPredictedFrameTime();
-  void AddWebVrRenderTimeEstimate(int16_t frame_index, bool did_wait);
+  void AddWebVrRenderTimeEstimate(int16_t frame_index,
+                                  const base::TimeTicks& fence_complete_time);
 
   void OnVSync(base::TimeTicks frame_time);
 
@@ -481,7 +483,8 @@ class VrShellGl : public device::mojom::VRPresentationProvider {
   bool mailbox_bridge_ready_ = false;
 
   // A fence used to avoid overstuffed GVR buffers in WebVR mode.
-  std::unique_ptr<gl::GLFenceEGL> webvr_prev_frame_completion_fence_;
+  std::unique_ptr<gl::GLFenceAndroidNativeFenceSync>
+      webvr_prev_frame_completion_fence_;
   std::unique_ptr<ScopedGpuTrace> gpu_trace_;
 
   // The default size for the render buffers.
