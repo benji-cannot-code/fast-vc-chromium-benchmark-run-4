0c3493dba81a341b6f9a3858ae6537e69dc39366
Sam McNally <sammc@chromium.org>
2018-05-14 00:12:39 +0000

    Add a MOUNT_TYPE_NETWORK_SOURCE to match MOUNT_SOURCE_NETWORK_SOURCE.
    The cros-disks equivalent for MountType (MountSourceType) has a third
    option (MOUNT_SOURCE_NETWORK_STORAGE) without a matching enumerator in
    MountType; this value is now used for generic fuse mounts. VolumeManager
    falls-through switches due to the unexpected value.
    
    Add MOUNT_TYPE_NETWORK_SOURCE on the chrome side to avoid this problem.
    Since this is currently a catch-all value for all generic fuse mount
    types, VolumeManager isn't capable of handling their mounts reasonably.
    Instead, ignore mount events with type MOUNT_TYPE_NETWORK_SOURCE in
    VolumeManager; the mounters will handle their mount events individually.
    
    Bug: 829274,832507
    Change-Id: I18dc0b5b68120c94da2ebbe05ee863a6f471b81c
    Reviewed-on: https://chromium-review.googlesource.com/1053621
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Commit-Queue: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558169}

--- chromeos/components/drivefs/drivefs_host_unittest.cc
+++ chromeos/components/drivefs/drivefs_host_unittest.cc
@@ -276,7 +276,7 @@ class DriveFsHostTest : public ::testing::Test,
                        {base::StrCat({"drivefs://", token,
                                       "@/path/to/profile/GCache/v2/g-ID"}),
                         "/media/drivefsroot/g-ID",
-                        chromeos::MOUNT_TYPE_ARCHIVE,
+                        chromeos::MOUNT_TYPE_NETWORK_STORAGE,
                         {}});
   }
 
@@ -419,7 +419,7 @@ TEST_F(DriveFsHostTest, ObserveOtherMount) {
       chromeos::disks::DiskMountManager::UNMOUNTING, chromeos::MOUNT_ERROR_NONE,
       {base::StrCat({"drivefs://", token, "@/path/to/profile/GCache/v2/g-ID"}),
        "/media/drivefsroot/g-ID",
-       chromeos::MOUNT_TYPE_ARCHIVE,
+       chromeos::MOUNT_TYPE_NETWORK_STORAGE,
        {}});
   EXPECT_FALSE(host_->IsMounted());
   host_->Unmount();
@@ -434,7 +434,7 @@ TEST_F(DriveFsHostTest, MountError) {
       chromeos::MOUNT_ERROR_DIRECTORY_CREATION_FAILED,
       {base::StrCat({"drivefs://", token, "@/path/to/profile/GCache/v2/g-ID"}),
        "/media/drivefsroot/g-ID",
-       chromeos::MOUNT_TYPE_ARCHIVE,
+       chromeos::MOUNT_TYPE_NETWORK_STORAGE,
        {}});
   EXPECT_FALSE(host_->IsMounted());
   EXPECT_FALSE(PendingConnectionManager::Get().OpenIpcChannel(token, {}));
