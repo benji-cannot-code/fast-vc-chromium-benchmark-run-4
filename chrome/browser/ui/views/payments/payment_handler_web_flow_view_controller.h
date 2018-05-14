ec043be196541a87ffc8b80e879722e7db1d29c8
gogerald <gogerald@google.com>
2018-05-14 13:26:19 +0000

    [Payment] Abort payment request when opened window has mixed content
    Test:
    1, open 'https://rsolomakhin.github.io/pr/bob/'
    2, clock 'Buy'
    3, choose 'Just-in-time Bobpay'
    4, click 'Pay'
    5, click 'Navigate to mixed content page', which opens 'https://mixed.badssl.com'
    6, Payment request should abort.
    
    Bug: 828431
    Change-Id: I38a1725b6265b6450b0cfa88d8234eed2949bb2c
    Reviewed-on: https://chromium-review.googlesource.com/1056012
    Commit-Queue: Ganggui Tang <gogerald@chromium.org>
    Reviewed-by: Mathieu Perreault <mathp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558280}

--- chrome/browser/ui/views/payments/payment_handler_web_flow_view_controller.h
+++ chrome/browser/ui/views/payments/payment_handler_web_flow_view_controller.h
@@ -54,6 +54,7 @@ class PaymentHandlerWebFlowViewController
   // content::WebContentsDelegate:
   void LoadProgressChanged(content::WebContents* source,
                            double progress) override;
+  void VisibleSecurityStateChanged(content::WebContents* source) override;
 
   // content::WebContentsObserver:
   void DidStartNavigation(
