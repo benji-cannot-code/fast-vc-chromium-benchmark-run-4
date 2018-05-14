f90f03260effa2d386d06ba2ddca48aa11a352be
Matt Mueller <mattm@chromium.org>
2018-05-14 21:42:35 +0000

    Fix cert_verify_tool dump filename conflict between CertVerifyProc impls.
    Change-Id: Ia496ac5f5311871ebb234f8d725d3d902ae3f597
    Reviewed-on: https://chromium-review.googlesource.com/1058045
    Reviewed-by: Eric Roman <eroman@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558475}

--- net/tools/cert_verify_tool/verify_using_cert_verify_proc.cc
+++ net/tools/cert_verify_tool/verify_using_cert_verify_proc.cc
@@ -116,7 +116,7 @@ bool VerifyUsingCertVerifyProc(
     const std::vector<CertInput>& intermediate_der_certs,
     const std::vector<CertInput>& root_der_certs,
     net::CRLSet* crl_set,
-    const base::FilePath& dump_prefix_path) {
+    const base::FilePath& dump_path) {
   std::cout
       << "NOTE: CertVerifyProc always uses OS trust settings (--roots are in "
          "addition).\n";
@@ -166,10 +166,8 @@ bool VerifyUsingCertVerifyProc(
 
   std::cout << "CertVerifyProc result: " << net::ErrorToShortString(rv) << "\n";
   PrintCertVerifyResult(result);
-  if (!dump_prefix_path.empty() && result.verified_cert) {
-    if (!DumpX509CertificateChain(dump_prefix_path.AddExtension(
-                                      FILE_PATH_LITERAL(".CertVerifyProc.pem")),
-                                  result.verified_cert.get())) {
+  if (!dump_path.empty() && result.verified_cert) {
+    if (!DumpX509CertificateChain(dump_path, result.verified_cert.get())) {
       return false;
     }
   }
