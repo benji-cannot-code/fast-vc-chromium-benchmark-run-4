20aff7ae216c8bda9a124d5a9a6c832d0db05d03
Henrik Boström <hbos@chromium.org>
2018-05-14 12:28:51 +0000

    Revert "Remove the extra layers of abstraction around QuicClock and QuicAlarm in Quartc."
    This reverts commit 3b6de80079c3eb0ce362e85546af9042871d397c.
    
    Reason for revert: Win10 Tests x64 are failing after this CL,
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Win10%20Tests%20x64%20%28dbg%29/540, 541, 542, 543.
    
    Speculative revert based based on CL changing "Quartc" and the failing tests are:
    QuartcSessionTest.ClientRejection
    QuartcSessionTest.GetStats
    QuartcSessionTest.CloseConnection
    QuartcSessionTest.WriterGivesPacketNumberToTransport
    QuartcSessionTest.CloseQuartcStream
    QuartcSessionTest.BundleWrites
    QuartcSessionTest.ServerRejection
    QuartcSessionTest.StreamConnection
    QuartcSessionTest.StopBundlingOnIncomingData
    QuartcSessionTest.CancelQuartcStream
    
    Original change's description:
    > Remove the extra layers of abstraction around QuicClock and QuicAlarm in Quartc.
    > 
    > QuicClock is already part of the QUIC platform API.  It's an abstraction for a
    > clock.  We don't gain anything by creating another clock abstraction and
    > wrapping that up in a QuicClock.
    > 
    > QuicAlarms are currently implemented using a task runner interface.  There's no
    > reason to change the abstraction from alarms to a task runner.
    > 
    > Eliminating the task runner interface and using the QUIC alarm abstraction
    > directly makes it easier to use the Quartc QUIC wrappers in a setting with an
    > existing alarm implementation (eg. chromium).  This might be useful in
    > implementing RTCQuicStream.
    > 
    > Quartc itself would like to use at least one alarm (see b/78013844).
    > Implementing QuicAlarms directly in Quartc makes this easy.
    > 
    > Merge internal change: 195335187
    > 
    > Change-Id: I2a0a60044e03c1d1893714ebe13492932ed87baf
    > Reviewed-on: https://chromium-review.googlesource.com/1055938
    > Commit-Queue: Zhongyi Shi <zhongyi@chromium.org>
    > Reviewed-by: Zhongyi Shi <zhongyi@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#558191}
    
    TBR=rch@chromium.org,zhongyi@chromium.org
    
    Change-Id: Ic819dc26330a12cfbba26f192aee207926ec3130
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/1057470
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Commit-Queue: Henrik Boström <hbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558268}

--- net/third_party/quic/quartc/quartc_session.h
+++ net/third_party/quic/quartc/quartc_session.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/third_party/quic/core/quic_error_codes.h"
 #include "net/third_party/quic/core/quic_session.h"
 #include "net/third_party/quic/platform/api/quic_export.h"
+#include "net/third_party/quic/quartc/quartc_clock_interface.h"
 #include "net/third_party/quic/quartc/quartc_packet_writer.h"
 #include "net/third_party/quic/quartc/quartc_session_interface.h"
 #include "net/third_party/quic/quartc/quartc_stream.h"
