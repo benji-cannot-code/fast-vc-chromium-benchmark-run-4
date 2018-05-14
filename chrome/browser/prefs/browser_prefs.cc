14437a645bb1ec2035c7bf62dd22483d62c9c46e
Charlie Harrison <csharrison@chromium.org>
2018-05-14 14:09:27 +0000

    Remove tab-under enterprise policy
    Now that tab-unders can be controlled via the popup setting, admins can
    simply use the popup enterprise policy to also control tab-unders.
    
    Since tab-under blocking was never actually launched in versions <M68,
    this CL also drops policy support for them completely.
    
    Bug: 842306
    Change-Id: Ie7b7d41b2caeaf1e26bc931777a25ec113c03dc4
    Reviewed-on: https://chromium-review.googlesource.com/1056170
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Drew Wilson <atwilson@chromium.org>
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558294}

--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -60,7 +60,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/task_manager/task_manager_interface.h"
 #include "chrome/browser/tracing/chrome_tracing_delegate.h"
 #include "chrome/browser/ui/blocked_content/safe_browsing_triggered_popup_blocker.h"
-#include "chrome/browser/ui/blocked_content/tab_under_navigation_throttle.h"
 #include "chrome/browser/ui/browser_ui_prefs.h"
 #include "chrome/browser/ui/navigation_correction_tab_observer.h"
 #include "chrome/browser/ui/network_profile_bubble.h"
@@ -527,7 +526,6 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
   SessionStartupPref::RegisterProfilePrefs(registry);
   signin::RegisterAccountConsistencyProfilePrefs(registry);
   syncer::SyncPrefs::RegisterProfilePrefs(registry);
-  TabUnderNavigationThrottle::RegisterProfilePrefs(registry);
   TemplateURLPrepopulateData::RegisterProfilePrefs(registry);
   translate::TranslatePrefs::RegisterProfilePrefs(registry);
   UINetworkQualityEstimatorService::RegisterProfilePrefs(registry);
--- chrome/browser/ui/blocked_content/tab_under_navigation_throttle.cc
+++ chrome/browser/ui/blocked_content/tab_under_navigation_throttle.cc
@@ -29,9 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/common/content_settings.h"
 #include "components/content_settings/core/common/content_settings_types.h"
 #include "components/pref_registry/pref_registry_syncable.h"
-#include "components/prefs/pref_service.h"
 #include "components/ukm/content/source_url_recorder.h"
-#include "components/user_prefs/user_prefs.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/render_frame_host.h"
@@ -120,13 +118,6 @@ const base::Feature TabUnderNavigationThrottle::kBlockTabUnders{
     "BlockTabUnders", base::FEATURE_DISABLED_BY_DEFAULT};
 
 // static
-void TabUnderNavigationThrottle::RegisterProfilePrefs(
-    user_prefs::PrefRegistrySyncable* registry) {
-  registry->RegisterBooleanPref(prefs::kTabUnderAllowed,
-                                false /* default_value */);
-}
-
-// static
 std::unique_ptr<content::NavigationThrottle>
 TabUnderNavigationThrottle::MaybeCreate(content::NavigationHandle* handle) {
   if (handle->IsInMainFrame())
@@ -145,10 +136,7 @@ TabUnderNavigationThrottle::TabUnderNavigationThrottle(
                                                  0 /* default_value */)),
       off_the_record_(
           handle->GetWebContents()->GetBrowserContext()->IsOffTheRecord()),
-      block_(base::FeatureList::IsEnabled(kBlockTabUnders) &&
-             !user_prefs::UserPrefs::Get(
-                  handle->GetWebContents()->GetBrowserContext())
-                  ->GetBoolean(prefs::kTabUnderAllowed)),
+      block_(base::FeatureList::IsEnabled(kBlockTabUnders)),
       has_opened_popup_since_last_user_gesture_at_start_(
           HasOpenedPopupSinceLastUserGesture()),
       started_in_foreground_(handle->GetWebContents()->GetVisibility() ==
