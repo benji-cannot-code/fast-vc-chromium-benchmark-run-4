f9a04b4faba517b4054fe48fb0590c37648a06f0
Olivier Robin <olivierrobin@chromium.org>
2018-05-14 11:25:54 +0000

    Always unfocus omnibox when displaying QRScanner.
    On iOS10, when dismissing the keyboard, there is a short time when the
    scroll view containing the suggestion is offset.
    This cause the omnibox to not be unfocused automatically because of the
    test in
    https://cs.chromium.org/chromium/src/ios/chrome/browser/ui/content_suggestions/content_suggestions_header_view_controller.mm?l=516
    Fix the issue by always unfocus the omnibox when displaying QR scanner.
    
    Bug: 842072
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Ie94884a18335753e919c0cd974c26e4a53abac8b
    Reviewed-on: https://chromium-review.googlesource.com/1055394
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558259}

--- ios/chrome/browser/ui/qr_scanner/qr_scanner_legacy_coordinator.mm
+++ ios/chrome/browser/ui/qr_scanner/qr_scanner_legacy_coordinator.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/commands/command_dispatcher.h"
 #import "ios/chrome/browser/ui/qr_scanner/qr_scanner_view_controller.h"
 #import "ios/chrome/browser/ui/qr_scanner/requirements/qr_scanner_presenting.h"
+#import "ios/chrome/browser/ui/toolbar/public/omnibox_focuser.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -46,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)showQRScanner {
+  [static_cast<id<OmniboxFocuser>>(self.dispatcher) cancelOmniboxEdit];
   self.viewController = [[QRScannerViewController alloc]
       initWithPresentationProvider:self.presentationProvider
                       loadProvider:self.loadProvider];
