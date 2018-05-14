4a7df0d0e5ad4a65e4ad8d6292712e8388de3445
Bailey Berro <baileyberro@chromium.org>
2018-05-14 23:17:15 +0000

    Pop Network File Shares settings from Files App
    Clicking 'SMB Shares' in the 'Add New Services' pane of the Files app
    opens the Network File Shares settings page in a new window.
    
    BUG=chromium:757625
    TEST=verified on test device.
    
    Change-Id: I579b2ac34b269104118de478f521596af32fd7bb
    Reviewed-on: https://chromium-review.googlesource.com/1052613
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Bailey Berro <baileyberro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558518}

--- chrome/common/webui_url_constants.cc
+++ chrome/common/webui_url_constants.cc
@@ -291,6 +291,7 @@ const char kHelpSubPage[] = "help";
 const char kInternetSubPage[] = "internet";
 const char kNetworkDetailSubPage[] = "networkDetail";
 const char kPowerSubPage[] = "power";
+const char kSmbSharesPage[] = "smbShares";
 const char kStylusSubPage[] = "stylus";
 #else
 const char kCreateProfileSubPage[] = "createProfile";
