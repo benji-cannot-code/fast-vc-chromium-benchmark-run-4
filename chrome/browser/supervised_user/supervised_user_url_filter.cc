3bc86e2cc3d5b4126e57a4427d8d50d49d7272c8
Mark Pilgrim <pilgrim@chromium.org>
2018-05-15 15:59:47 +0000

    Migrate SafeSearchURLChecker to SimpleURLLoader
    Bug: 773295
    Change-Id: I6be6adefab29b5d4e67013f45ba5ffe4e058dbdc
    Reviewed-on: https://chromium-review.googlesource.com/1030062
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Mark Pilgrim <pilgrim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558725}

--- chrome/browser/supervised_user/supervised_user_url_filter.cc
+++ chrome/browser/supervised_user/supervised_user_url_filter.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/escape.h"
 #include "net/base/registry_controlled_domains/registry_controlled_domain.h"
 #include "net/base/url_util.h"
+#include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "url/gurl.h"
 #include "url/url_constants.h"
 
@@ -531,7 +532,7 @@ void SupervisedUserURLFilter::SetManualURLs(std::map<GURL, bool> url_map) {
 }
 
 void SupervisedUserURLFilter::InitAsyncURLChecker(
-    net::URLRequestContextGetter* context) {
+    scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory) {
   net::NetworkTrafficAnnotationTag traffic_annotation =
       net::DefineNetworkTrafficAnnotation("supervised_user_url_filter", R"(
         semantics {
@@ -553,8 +554,8 @@ void SupervisedUserURLFilter::InitAsyncURLChecker(
             "family dashboard."
           policy_exception_justification: "Not implemented."
         })");
-  async_url_checker_.reset(
-      new SafeSearchURLChecker(context, traffic_annotation));
+  async_url_checker_.reset(new SafeSearchURLChecker(
+      std::move(url_loader_factory), traffic_annotation));
 }
 
 void SupervisedUserURLFilter::ClearAsyncURLChecker() {
--- chrome/browser/ui/webui/supervised_user_internals_message_handler.cc
+++ chrome/browser/ui/webui/supervised_user_internals_message_handler.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/supervised_user_error_page/supervised_user_error_page.h"
 #include "components/url_formatter/url_fixer.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/browser/storage_partition.h"
 #include "content/public/browser/web_ui.h"
 
 using content::BrowserThread;
