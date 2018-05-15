01736bf46f73a7db837217bf0f8d006ef7a8e2e9
Rune Lillesveen <futhark@chromium.org>
2018-05-15 21:09:36 +0000

    [Squad] Make kPseudoIdFirstLineInherited an internal pseudo.
    Gets rid of MutableStyleRef() access where we called SetHasPseudo() for
    the inherited first-line styles in order to not return early from
    GetCachedPseudoStyle.
    
    TEST=fast/css/invalidation/first-line-change-inline-color-inherited.html
    
    Bug: 813068
    Change-Id: Ib34d8ae23e6203ccb27511fb3f5580d8d3002500
    Reviewed-on: https://chromium-review.googlesource.com/1059249
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558831}

--- third_party/blink/renderer/core/style/computed_style_constants.h
+++ third_party/blink/renderer/core/style/computed_style_constants.h
@@ -64,8 +64,8 @@ enum StyleRecalcChange {
 // Static pseudo styles. Dynamic ones are produced on the fly.
 enum PseudoId {
   // The order must be NOP ID, public IDs, and then internal IDs.
-  // If you add or remove a public ID, you must update _pseudoBits in
-  // ComputedStyle.
+  // If you add or remove a public ID, you must update the field_size of
+  // "PseudoBits" in ComputedStyleExtraFields.json5.
   kPseudoIdNone,
   kPseudoIdFirstLine,
   kPseudoIdFirstLetter,
@@ -73,9 +73,9 @@ enum PseudoId {
   kPseudoIdAfter,
   kPseudoIdBackdrop,
   kPseudoIdSelection,
-  kPseudoIdFirstLineInherited,
   kPseudoIdScrollbar,
   // Internal IDs follow:
+  kPseudoIdFirstLineInherited,
   kPseudoIdScrollbarThumb,
   kPseudoIdScrollbarButton,
   kPseudoIdScrollbarTrack,
@@ -86,7 +86,7 @@ enum PseudoId {
   // Special values follow:
   kAfterLastInternalPseudoId,
   kFirstPublicPseudoId = kPseudoIdFirstLine,
-  kFirstInternalPseudoId = kPseudoIdScrollbarThumb,
+  kFirstInternalPseudoId = kPseudoIdFirstLineInherited,
   kElementPseudoIdMask = (1 << (kPseudoIdBefore - kFirstPublicPseudoId)) |
                          (1 << (kPseudoIdAfter - kFirstPublicPseudoId)) |
                          (1 << (kPseudoIdBackdrop - kFirstPublicPseudoId))
