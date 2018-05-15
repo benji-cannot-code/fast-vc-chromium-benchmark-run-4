1484a6e7b9e6c7613563cf40558514aa5e9b4eaf
Lei Zhang <thestig@chromium.org>
2018-05-15 05:57:15 +0000

    Revert "media: implement TakePhoto in Chrome OS camera HAL VideoCaptureDevice"
    This reverts commit 640eb0c2bcfc516b7b8882a4e12fd38434cc8416.
    
    Reason for revert: Broke the build.
    
    https://ci.chromium.org/buildbot/chromium.chromiumos/linux-chromeos-dbg/5678
    
    Original change's description:
    > media: implement TakePhoto in Chrome OS camera HAL VideoCaptureDevice
    > 
    > This CL implements the TakePhoto() API for the Chrome OS camera HAL
    > VideoCaptureDevice, including the following features:
    > 
    >   - Full size still capture
    >   - Auto-focus, auto-exposure, and auto-white-balancing stabilization
    > 
    > BUG=chromium:832772
    > TEST=Run capture_unittests on DUT
    > TEST=Make sure TakePhoto() API works on
    > https://googlechrome.github.io/samples/image-capture/grab-frame-take-photo.html
    > 
    > Change-Id: I99e44087006cac28eac7e73c6764f522dac57cfa
    > Reviewed-on: https://chromium-review.googlesource.com/1018641
    > Reviewed-by: Emircan Uysaler <emircan@chromium.org>
    > Reviewed-by: Wu-Cheng Li <wuchengli@chromium.org>
    > Commit-Queue: Ricky Liang <jcliang@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#558603}
    
    TBR=jcliang@chromium.org,wuchengli@chromium.org,emircan@chromium.org,shenghao@google.com,hywu@chromium.org,chfremer@chromium.org
    
    Change-Id: Ibc2b4547f33d4a505b546079dd118e1d4401a9cf
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: chromium:832772
    Reviewed-on: https://chromium-review.googlesource.com/1058568
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558622}

--- media/capture/video/chromeos/camera_metadata_utils.h
+++ media/capture/video/chromeos/camera_metadata_utils.h
@@ -6,17 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MEDIA_CAPTURE_VIDEO_CHROMEOS_CAMERA_METADATA_UTILS_H_
 #define MEDIA_CAPTURE_VIDEO_CHROMEOS_CAMERA_METADATA_UTILS_H_
 
-#include "media/base/media_export.h"
 #include "media/capture/video/chromeos/mojo/camera_metadata.mojom.h"
 
 namespace media {
 
-cros::mojom::CameraMetadataEntryPtr* GetMetadataEntry(
+const cros::mojom::CameraMetadataEntryPtr* GetMetadataEntry(
     const cros::mojom::CameraMetadataPtr& camera_metadata,
     cros::mojom::CameraMetadataTag tag);
-
-// Sort the camera metadata entries using the metadata tags.
-void SortCameraMetadata(cros::mojom::CameraMetadataPtr* camera_metadata);
 
 void MergeMetadata(cros::mojom::CameraMetadataPtr* to,
                    const cros::mojom::CameraMetadataPtr& from);
