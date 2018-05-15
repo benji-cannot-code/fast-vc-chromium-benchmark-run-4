fdc51659a29f739d0a35f741c0b12d979c7c225e
Boris Sazonov <bsazonov@chromium.org>
2018-05-15 10:25:58 +0000

    [Android] Extract helper methods from ManagedPreferenceDelegate
    A follow-up to https://crrev.com/c/1047870.
    
    This CL removes initPreference, onBindViewToPreference and
    onClickPreference helper methods from ManagedPreferenceDelegate
    interface, adding corresponding static methods to
    ManagedPreferencesUtils instead. It also moves null checks into these
    methods, making ManagedPreferenceDelegate param @Nullable.
    
    Bug: None
    Change-Id: I53e3669962c21e68a00b83dcde38b229d0b44364
    Reviewed-on: https://chromium-review.googlesource.com/1057472
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558657}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/ChromeSwitchPreference.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/ChromeSwitchPreference.java
@@ -48,7 +48,7 @@ public class ChromeSwitchPreference extends SwitchPreference {
      */
     public void setManagedPreferenceDelegate(ManagedPreferenceDelegate delegate) {
         mManagedPrefDelegate = delegate;
-        if (mManagedPrefDelegate != null) mManagedPrefDelegate.initPreference(this);
+        ManagedPreferencesUtils.initPreference(mManagedPrefDelegate, this);
     }
 
     /**
@@ -91,12 +91,12 @@ public class ChromeSwitchPreference extends SwitchPreference {
             summary.setVisibility(View.GONE);
         }
 
-        if (mManagedPrefDelegate != null) mManagedPrefDelegate.onBindViewToPreference(this, view);
+        ManagedPreferencesUtils.onBindViewToPreference(mManagedPrefDelegate, this, view);
     }
 
     @Override
     protected void onClick() {
-        if (mManagedPrefDelegate != null && mManagedPrefDelegate.onClickPreference(this)) return;
+        if (ManagedPreferencesUtils.onClickPreference(mManagedPrefDelegate, this)) return;
         super.onClick();
     }
 }
