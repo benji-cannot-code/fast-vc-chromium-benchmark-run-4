4182db9492a9d086289c8a43ab38ebd2a34de18a
Michael Lippautz <mlippautz@chromium.org>
2018-05-15 15:33:20 +0000

    [oilpan,wtf] Rework HashTable write barriers
    Instead of emitting write barriers for single writes in HashTable,
    conservatively emit a barrier for the backing store during rehashing.
    
    This moves us in the tradeoff space:
    - Removes 40KiB of binary bloat
    - Avoids emitting barriers when re-inserting single elements which
      improves throughput for the case where incremental marking is off
    - Emits a single conservative barrier at the end of RehashTo. This
      potentially means that we (re-)scan more objects when incremental
      marking is on
    
    Bug: chromium:839514, chromium:757440
    Change-Id: Ib5495c9e2210836424d7b26f2138d852f8dafec7
    Reviewed-on: https://chromium-review.googlesource.com/1055400
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558713}

--- third_party/blink/renderer/platform/wtf/hash_table.h
+++ third_party/blink/renderer/platform/wtf/hash_table.h
@@ -535,8 +535,7 @@ struct Mover {
   STATIC_ONLY(Mover);
   static void Move(T&& from, T& to) {
     to.~T();
-    ConstructTraits<T, Traits, Allocator>::ConstructAndNotifyElement(
-        &to, std::move(from));
+    new (NotNull, &to) T(std::move(from));
   }
 };
 
@@ -544,10 +543,9 @@ template <typename T, typename Allocator, typename Traits>
 struct Mover<T, Allocator, Traits, true> {
   STATIC_ONLY(Mover);
   static void Move(T&& from, T& to) {
-    to.~T();
     Allocator::EnterGCForbiddenScope();
-    ConstructTraits<T, Traits, Allocator>::ConstructAndNotifyElement(
-        &to, std::move(from));
+    to.~T();
+    new (NotNull, &to) T(std::move(from));
     Allocator::LeaveGCForbiddenScope();
   }
 };
@@ -1759,6 +1757,10 @@ HashTable<Key, Value, Extractor, HashFunctions, Traits, KeyTraits, Allocator>::
       new_entry = reinserted_entry;
     }
   }
+  // Rescan the contents of the backing store as no write barriers were emitted
+  // during re-insertion. Traits::NeedsToForbidGCOnMove ensures that no
+  // garbage collection is triggered during moving.
+  Allocator::TraceMarkedBackingStore(table_);
 
   deleted_count_ = 0;
 
