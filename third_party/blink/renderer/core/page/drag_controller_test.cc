057e914d77f282078d3753a8fc3e56ae11d0eb1f
Dmitry Gozman <dgozman@chromium.org>
2018-05-15 21:49:20 +0000

    Remove blink::DragSession
    It's not really used, we can use DragOperation instead.
    
    Change-Id: I85b997c9534dc7ce6522a9806a4d7bc4e8f82642
    Reviewed-on: https://chromium-review.googlesource.com/1058589
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558844}

--- third_party/blink/renderer/core/page/drag_controller_test.cc
+++ third_party/blink/renderer/core/page/drag_controller_test.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/visual_viewport.h"
 #include "third_party/blink/renderer/core/page/autoscroll_controller.h"
 #include "third_party/blink/renderer/core/page/drag_data.h"
-#include "third_party/blink/renderer/core/page/drag_session.h"
 #include "third_party/blink/renderer/core/page/drag_state.h"
 #include "third_party/blink/renderer/core/testing/core_unit_test_helper.h"
 #include "third_party/blink/renderer/core/testing/sim/sim_request.h"
