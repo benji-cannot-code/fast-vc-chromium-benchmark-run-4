b8354123531320f329e83a36ef11f7cddca14a0b
Lukasz Anforowicz <lukasza@chromium.org>
2018-05-15 18:46:17 +0000

    Disable flaky testRegularPlusIncognitoCheck test.
    Bug: 842999
    Change-Id: I97a64b1aa77c2991e0b20e80e985cbd4f7012e02
    Tbr: tedchoc@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/1060108
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Bryan McQuade <bmcquade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558790}

--- chrome/android/javatests/src/org/chromium/chrome/browser/metrics/UkmTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/metrics/UkmTest.java
@@ -17,6 +17,7 @@ import org.junit.runner.RunWith;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.chrome.browser.ChromeSwitches;
 import org.chromium.chrome.browser.browsing_data.BrowsingDataType;
@@ -96,7 +97,7 @@ public class UkmTest {
 
     @Test
     @SmallTest
-    @RetryOnFailure
+    @DisabledTest // TODO(https://crbug.com/842999): Reenable after unflaking.
     public void testRegularPlusIncognitoCheck() throws Exception {
         // Keep in sync with UkmBrowserTest.RegularPlusIncognitoCheck in
         // chrome/browser/metrics/ukm_browsertest.cc.
