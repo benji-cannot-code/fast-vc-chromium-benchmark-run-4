f2f4401bcda82131551585ebf7d54b788e34bf7f
Yoshifumi Inoue <yosin@chromium.org>
2018-05-15 09:03:56 +0000

    Get rid of unused declaration of Element::RecalcOwnStyleForReattach()
    This patch gets rid of unused declaration |Element::RecalcOwnStyleForReattach()|
    for improving code health.
    
    Change-Id: I9c9b1331a59614d8bdf8f1ced03e4e8348db0da0
    Reviewed-on: https://chromium-review.googlesource.com/1058877
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558644}

--- third_party/blink/renderer/core/dom/element.h
+++ third_party/blink/renderer/core/dom/element.h
@@ -967,7 +967,6 @@ class CORE_EXPORT Element : public ContainerNode {
   scoped_refptr<ComputedStyle> PropagateInheritedProperties(StyleRecalcChange);
 
   StyleRecalcChange RecalcOwnStyle(StyleRecalcChange);
-  void RecalcOwnStyleForReattach();
   void RecalcShadowIncludingDescendantStylesForReattach();
   void RecalcShadowRootStylesForReattach();
 
