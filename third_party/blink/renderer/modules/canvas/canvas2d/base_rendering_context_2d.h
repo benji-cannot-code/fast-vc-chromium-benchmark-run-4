2cb816f21298194b12c935ce29888e33a08b3173
Fernando Serboncini <fserb@chromium.org>
2018-05-15 18:26:46 +0000

    Adds getTransform/setTransform using DOMMatrix
    Bug: 742360
    Change-Id: I49c21ea926e8c228986dc041108ca7b9449966c5
    Reviewed-on: https://chromium-review.googlesource.com/676088
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Commit-Queue: Fernando Serboncini <fserb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558783}

--- third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.h
+++ third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/bindings/modules/v8/canvas_image_source.h"
 #include "third_party/blink/renderer/bindings/modules/v8/string_or_canvas_gradient_or_canvas_pattern.h"
+#include "third_party/blink/renderer/core/geometry/dom_matrix.h"
+#include "third_party/blink/renderer/core/geometry/dom_matrix_2d_init.h"
 #include "third_party/blink/renderer/core/html/canvas/image_data.h"
 #include "third_party/blink/renderer/modules/canvas/canvas2d/canvas_gradient.h"
 #include "third_party/blink/renderer/modules/canvas/canvas2d/canvas_path.h"
@@ -106,6 +108,8 @@ class MODULES_EXPORT BaseRenderingContext2D : public GarbageCollectedMixin,
                     double m22,
                     double dx,
                     double dy);
+  void setTransform(DOMMatrix2DInit&, ExceptionState&);
+  DOMMatrix* getTransform();
   void resetTransform();
 
   void beginPath();
