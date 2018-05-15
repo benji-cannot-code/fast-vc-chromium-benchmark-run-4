7c6fef55c745c552b2953d337efa744c8f89721c
Koji Ishii <kojii@chromium.org>
2018-05-15 22:00:42 +0000

    Reuse RunSegmenter when shaping multiple ranges of a string
    Because RunSegmenter needs pre-context but is forwad-only, it
    scans the input string from the beginning for each shaping.
    This characteristics does not work well with LayoutNG, where
    it constructs a string for each block, segment it, and use
    range shaping for each segment.
    
    This patch keeps a cache of RunSegmenter in HarfBuzzShaper
    instance, and re-use if caller shapes multiple ranges
    incrementally.
    
    This change turns 90 Timeouts in LayoutNG, 6 out of it to
    Failure and others to Pass.
    
    56 Timeouts are still left, probably from other reasons.
    Most looks like testharness tests, probably from large
    text files. I will look into them after this.
    
    Bug: 636993
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: I09afa9c9bd38de09189b0242cc00e5d8dc739042
    Reviewed-on: https://chromium-review.googlesource.com/1053346
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558852}

--- third_party/blink/renderer/platform/fonts/shaping/harf_buzz_shaper.h
+++ third_party/blink/renderer/platform/fonts/shaping/harf_buzz_shaper.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_FONTS_SHAPING_HARF_BUZZ_SHAPER_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_FONTS_SHAPING_HARF_BUZZ_SHAPER_H_
 
+#include "base/optional.h"
 #include "third_party/blink/renderer/platform/fonts/shaping/run_segmenter.h"
 #include "third_party/blink/renderer/platform/fonts/shaping/shape_result.h"
 #include "third_party/blink/renderer/platform/wtf/allocator.h"
@@ -45,6 +46,8 @@ class HarfBuzzShaper;
 struct ReshapeQueueItem;
 struct RangeData;
 struct BufferSlice;
+
+enum class FontOrientation;
 
 class PLATFORM_EXPORT HarfBuzzShaper final {
  public:
@@ -101,8 +104,15 @@ class PLATFORM_EXPORT HarfBuzzShaper final {
                     const BufferSlice&,
                     ShapeResult*) const;
 
+  RunSegmenter* CachedRunSegmenter(unsigned start,
+                                   unsigned end,
+                                   FontOrientation) const;
+
   const UChar* text_;
   unsigned text_length_;
+
+  // Cache an instnace of |RunSegmenter|. See |CachedRunSegmenter()|.
+  mutable base::Optional<RunSegmenter> run_segmenter_;
 };
 
 }  // namespace blink
