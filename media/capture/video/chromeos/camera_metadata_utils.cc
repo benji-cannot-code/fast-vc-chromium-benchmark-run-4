c62ae56d32b5486461dfc87437140405b332c725
Ricky Liang <jcliang@chromium.org>
2018-05-15 03:48:44 +0000

    media: implement TakePhoto in Chrome OS camera HAL VideoCaptureDevice
    This CL implements the TakePhoto() API for the Chrome OS camera HAL
    VideoCaptureDevice, including the following features:
    
      - Full size still capture
      - Auto-focus, auto-exposure, and auto-white-balancing stabilization
    
    BUG=chromium:832772
    TEST=Run capture_unittests on DUT
    TEST=Make sure TakePhoto() API works on
    https://googlechrome.github.io/samples/image-capture/grab-frame-take-photo.html
    
    Change-Id: I99e44087006cac28eac7e73c6764f522dac57cfa
    Reviewed-on: https://chromium-review.googlesource.com/1018641
    Reviewed-by: Emircan Uysaler <emircan@chromium.org>
    Reviewed-by: Wu-Cheng Li <wuchengli@chromium.org>
    Commit-Queue: Ricky Liang <jcliang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558603}

--- media/capture/video/chromeos/camera_metadata_utils.cc
+++ media/capture/video/chromeos/camera_metadata_utils.cc
@@ -5,22 +5,43 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/capture/video/chromeos/camera_metadata_utils.h"
 
-#include <set>
+#include <algorithm>
+#include <unordered_set>
 
 namespace media {
 
-const cros::mojom::CameraMetadataEntryPtr* GetMetadataEntry(
+cros::mojom::CameraMetadataEntryPtr* GetMetadataEntry(
     const cros::mojom::CameraMetadataPtr& camera_metadata,
     cros::mojom::CameraMetadataTag tag) {
-  if (!camera_metadata->entries.has_value()) {
+  if (!camera_metadata || !camera_metadata->entries.has_value()) {
     return nullptr;
   }
-  for (const auto& entry : camera_metadata->entries.value()) {
-    if (entry->tag == tag) {
-      return &entry;
-    }
+  // We assume the metadata entries are sorted.
+  auto iter = std::find_if(camera_metadata->entries.value().begin(),
+                           camera_metadata->entries.value().end(),
+                           [tag](const cros::mojom::CameraMetadataEntryPtr& e) {
+                             return e->tag == tag;
+                           });
+  if (iter == camera_metadata->entries.value().end()) {
+    return nullptr;
   }
-  return nullptr;
+  return &(camera_metadata->entries.value()[(*iter)->index]);
+}
+
+void SortCameraMetadata(cros::mojom::CameraMetadataPtr* camera_metadata) {
+  if (!camera_metadata || !(*camera_metadata) ||
+      !(*camera_metadata)->entries.has_value()) {
+    return;
+  }
+  std::sort((*camera_metadata)->entries.value().begin(),
+            (*camera_metadata)->entries.value().end(),
+            [](const cros::mojom::CameraMetadataEntryPtr& a,
+               const cros::mojom::CameraMetadataEntryPtr& b) {
+              return a->tag < b->tag;
+            });
+  for (size_t i = 0; i < (*camera_metadata)->entries.value().size(); ++i) {
+    (*camera_metadata)->entries.value()[i]->index = i;
+  }
 }
 
 void MergeMetadata(cros::mojom::CameraMetadataPtr* to,
@@ -35,7 +56,7 @@ void MergeMetadata(cros::mojom::CameraMetadataPtr* to,
     return;
   }
 
-  std::set<cros::mojom::CameraMetadataTag> tags;
+  std::unordered_set<cros::mojom::CameraMetadataTag> tags;
   if ((*to)->entries) {
     for (const auto& entry : (*to)->entries.value()) {
       tags.insert(entry->tag);
