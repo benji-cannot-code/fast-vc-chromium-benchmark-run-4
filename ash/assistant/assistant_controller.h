7e1ea7105dffe9b0fe002e047cbc4af5cce4e41e
David Black <dmblack@google.com>
2018-05-15 20:52:32 +0000

    Handle DialogPlate action press event.
    - Adds ActionView to DialogPlate.
    - Handles ActionView press event in Assistant controller.
    
    Press event is handled in the following way:
    - If using keyboard input modality, action press == commit.
    - If using voice input modality, action press == toggle voice
      interaction.
    
    ActionView will be a wrapper around LogoView which is still under
    development (b/78191547).
    
    See bug for details.
    
    Bug: b:79473532
    Change-Id: I9ee4d465b29b3812caceb9d4804504d63b90df29
    Reviewed-on: https://chromium-review.googlesource.com/1053160
    Commit-Queue: David Black <dmblack@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558827}

--- ash/assistant/assistant_controller.h
+++ ash/assistant/assistant_controller.h
@@ -77,6 +77,9 @@ class AssistantController
   void StopInteraction();
   void ToggleInteraction();
 
+  // Invoked on dialog plate action pressed event.
+  void OnDialogPlateActionPressed(const std::string& text);
+
   // Invoked on dialog plate contents changed event.
   void OnDialogPlateContentsChanged(const std::string& text);
 
