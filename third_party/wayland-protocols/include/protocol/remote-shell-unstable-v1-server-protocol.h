8ba96ea78a1d6f9e5858dfb2495a423c03701f75
Yuichiro Hanada <yhanada@chromium.org>
2018-05-15 22:31:10 +0000

    Increase zcr_remote_shell_v1 version and zcr_notification_surface_v1 version to 16.
    The version of the global interface should be increased when the
    protocol is modified, as it is how we detect a version incompatibility
    immediately on bootup.
    For consistency, the version of zcr_notification_surface_v1 is also
    increated in this CL.
    
    Bug: 829383, 834027
    Change-Id: I33e8fbdbe0798e70743ad2af83e4ada80e7797dc
    Reviewed-on: https://chromium-review.googlesource.com/1055030
    Reviewed-by: David Reveman <reveman@chromium.org>
    Commit-Queue: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558862}

--- third_party/wayland-protocols/include/protocol/remote-shell-unstable-v1-server-protocol.h
+++ third_party/wayland-protocols/include/protocol/remote-shell-unstable-v1-server-protocol.h
@@ -1499,7 +1499,7 @@ struct zcr_notification_surface_v1_interface {
 	 * set application ID
 	 *
 	 * Set an application identifier for the notification surface.
-	 * @since 2
+	 * @since 16
 	 */
 	void (*set_app_id)(struct wl_client *client,
 			   struct wl_resource *resource,
@@ -1514,7 +1514,7 @@ struct zcr_notification_surface_v1_interface {
 /**
  * @ingroup iface_zcr_notification_surface_v1
  */
-#define ZCR_NOTIFICATION_SURFACE_V1_SET_APP_ID_SINCE_VERSION 2
+#define ZCR_NOTIFICATION_SURFACE_V1_SET_APP_ID_SINCE_VERSION 16
 
 #ifdef  __cplusplus
 }
