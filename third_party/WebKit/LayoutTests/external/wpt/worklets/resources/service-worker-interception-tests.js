b55fbc9898cbdd237aa7920618083727a75c69bf
Hiroki Nakagawa <nhiroki@chromium.org>
2018-05-15 08:00:10 +0000

    Worklet: Remove outdated comment from WPT
    Worklets don't support dynamic import (see the issue)
    
    Bug: 782538
    Change-Id: Iea9b4352122e78d1ff671ae09a9947f6a7f1ef3b
    TBR: nhiroki@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/1058883
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558638}

--- third_party/WebKit/LayoutTests/external/wpt/worklets/resources/service-worker-interception-tests.js
+++ third_party/WebKit/LayoutTests/external/wpt/worklets/resources/service-worker-interception-tests.js
@@ -121,6 +121,4 @@ function runServiceWorkerInterceptionTests(worklet_type) {
             })
           .then(msg_event => assert_equals(msg_event.data, 'RESOLVED'));
     }, 'Static import should be intercepted by a service worker.');
-
-    // TODO(nhiroki): Add tests for dynamic import.
 }
