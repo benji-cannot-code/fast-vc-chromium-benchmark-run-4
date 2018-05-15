5efebeb1b4a8913ec01242eb655554725c0be187
Qiyu Hu <qiyuh@google.com>
2018-05-15 16:20:10 +0000

    arc: bluetooth: Forward the MTU received to android
    Two relevant patches are ag/4013275 and crosreview.com/1043496.
    
    Bug: b:78593133
    Test: Pass test CtsVerifier test on MTU exchange
    Change-Id: I527dee3d105d3b3db744f550c2c7003789d65c57
    CQ-DEPEND: CL:1050621
    Reviewed-on: https://chromium-review.googlesource.com/1058328
    Reviewed-by: Miao-chen Chou <mcchou@chromium.org>
    Reviewed-by: Greg Kerr <kerrnel@chromium.org>
    Reviewed-by: Luis Hector Chavez <lhchavez@chromium.org>
    Commit-Queue: Qiyu Hu <qiyuh@google.com>
    Cr-Commit-Position: refs/heads/master@{#558735}

--- chrome/browser/chromeos/arc/bluetooth/arc_bluetooth_bridge.h
+++ chrome/browser/chromeos/arc/bluetooth/arc_bluetooth_bridge.h
@@ -95,6 +95,10 @@ class ArcBluetoothBridge
                            device::BluetoothDevice* device,
                            bool new_paired_status) override;
 
+  void DeviceMTUChanged(device::BluetoothAdapter* adapter,
+                        device::BluetoothDevice* device,
+                        uint16_t mtu) override;
+
   void DeviceRemoved(device::BluetoothAdapter* adapter,
                      device::BluetoothDevice* device) override;
 
