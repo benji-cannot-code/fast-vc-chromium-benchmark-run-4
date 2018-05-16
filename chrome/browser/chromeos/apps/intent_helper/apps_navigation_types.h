9861de72be02c28487e90e9b9aa8bb3c7e42c92e
David Jacobo <djacobo@chromium.org>
2018-05-16 19:13:56 +0000

    Fix regression for intent picker UI
    IntentPickerBubbleView is not saving the user's preference when they
    decide to "Stay in Chrome" + "Remember my choice". This is a regression
    that appeared in M67.
    
    This CL specials case AppType::INVALID to handle saving user's
    preference, previously proposed to add a new AppType::NATIVE_CHROME but
    that sounds wrong given this enum lists non-cros app sources.
    
    Besides that we need to make sure that the preference is communicated
    to ARC via AddPreferredPackage and honored within ArcNavigationThrottle,
    this can be achieved by adding a new enum PreferredPlatform that will
    help DidLaunchPreferredArcApp to be more explicit.
    
    Bug: 841971
    Test: Build.
    Change-Id: I383a926ae613ffe48f4dcd3a478ac124aefe4947
    Reviewed-on: https://chromium-review.googlesource.com/1054584
    Commit-Queue: David Jacobo <djacobo@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559221}

--- chrome/browser/chromeos/apps/intent_helper/apps_navigation_types.h
+++ chrome/browser/chromeos/apps/intent_helper/apps_navigation_types.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_CHROMEOS_APPS_INTENT_HELPER_APPS_NAVIGATION_TYPES_H_
 
 #include <string>
+#include <vector>
 
 #include "base/callback_forward.h"
 #include "base/macros.h"
@@ -42,6 +43,22 @@ enum class IntentPickerCloseReason {
 
   // The user chose to open an app.
   OPEN_APP,
+};
+
+// Describes what's the preferred platform for this navigation, if any.
+enum class PreferredPlatform {
+  // Either there was an error or there is no preferred app at all.
+  NONE,
+
+  // The preferred app is Chrome.
+  NATIVE_CHROME,
+
+  // The preferred app is an ARC app.
+  ARC,
+
+  // TODO(crbug.com/826982) Not needed until app registry is in use.
+  // The preferred app is a PWA app.
+  PWA,
 };
 
 enum class AppsNavigationAction {
