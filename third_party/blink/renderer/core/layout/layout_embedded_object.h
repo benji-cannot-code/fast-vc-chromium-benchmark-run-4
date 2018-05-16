2d8d1db0697285e5fa1e9fcac00982d696cca260
Fredrik Söderquist <fs@opera.com>
2018-05-16 13:42:07 +0000

    Refactor computation of intrinsic size for "nested" objects
    Sink the update of intrinsic_size_ out of the if-else in
    ComputeIntrinsicSizingInfoForReplacedContent, and then push
    GetNestedIntrinsicSizingInfo down into subclasses, folding it away to use the
    "local" constructs of the relevant subclasses.
    
    This leaves subclasses to deal with issues such as if whether to apply zoom
    or not et.c, and will with some additional tweaks allow for more succinct
    interfaces in the "nested" cases. It also makes LayoutReplaced much simpler.
    
    Bug: 722055
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: Ie8493ffa89ffb34d2591d6a7ecdc8ebc20c6264f
    Reviewed-on: https://chromium-review.googlesource.com/1059776
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#559069}

--- third_party/blink/renderer/core/layout/layout_embedded_object.h
+++ third_party/blink/renderer/core/layout/layout_embedded_object.h
@@ -63,8 +63,8 @@ class LayoutEmbeddedObject final : public LayoutEmbeddedContent {
     return type == kLayoutObjectEmbeddedObject ||
            LayoutEmbeddedContent::IsOfType(type);
   }
+  void ComputeIntrinsicSizingInfo(IntrinsicSizingInfo&) const override;
   bool NeedsPreferredWidthsRecalculation() const override;
-  bool GetNestedIntrinsicSizingInfo(IntrinsicSizingInfo&) const override;
 
   PaintLayerType LayerTypeRequired() const final;
 
