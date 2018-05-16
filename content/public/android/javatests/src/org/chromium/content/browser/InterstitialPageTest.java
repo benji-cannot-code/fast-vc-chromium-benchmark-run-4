74552c563a924ac570d796fabdde76f00d9faa73
Jinsuk Kim <jinsukkim@chromium.org>
2018-05-16 03:03:05 +0000

    Reland "Android: Moves InterstitialPageDelegateAndroid to test"
    The new dependency content_java_test_support added to
    chrome_public_apk_for_test inadvertently pushed
    BaseChromiumAndroidJUnitRunner to secondary dex, which
    made some buildbots fail to work. This CL relands the culprit
    CL after annotating the class with @MainDex to make it stay
    in the main dex.
    
    Locally tested with Nexus 5 with the build version KTU84P.
    
    TBR=boliu@chromium.org,tedchoc@chromium.org
    
    Bug: 617324
    
    This reverts commit a4b7fec80296a5e142fd99503ae8715ad7589ba0.
    
    Change-Id: I483fa72dd7fa76a73fc2d9994243102cb06c6a46
    Reviewed-on: https://chromium-review.googlesource.com/1053046
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558949}

--- content/public/android/javatests/src/org/chromium/content/browser/InterstitialPageTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/InterstitialPageTest.java
@@ -18,6 +18,7 @@ import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.base.test.util.UrlUtils;
+import org.chromium.content.browser.test.InterstitialPageDelegateAndroid;
 import org.chromium.content.browser.test.util.Criteria;
 import org.chromium.content.browser.test.util.CriteriaHelper;
 import org.chromium.content.browser.test.util.TouchCommon;
@@ -118,8 +119,7 @@ public class InterstitialPageTest {
                 new Callable<TestWebContentsObserver>() {
                     @Override
                     public TestWebContentsObserver call() throws Exception {
-                        mActivityTestRule.getWebContents().showInterstitialPage(
-                                URL, delegate.getNative());
+                        delegate.showInterstitialPage(URL, mActivityTestRule.getWebContents());
                         return new TestWebContentsObserver(mActivityTestRule.getWebContents());
                     }
                 });
--- content/public/test/android/interstitial_page_delegate_android.cc
+++ content/public/test/android/interstitial_page_delegate_android.cc
@@ -3,12 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "content/browser/android/interstitial_page_delegate_android.h"
+#include "content/public/test/android/interstitial_page_delegate_android.h"
 
 #include "base/android/jni_android.h"
 #include "base/android/jni_string.h"
 #include "base/android/scoped_java_ref.h"
 #include "content/public/browser/interstitial_page.h"
+#include "content/public/browser/web_contents.h"
 #include "jni/InterstitialPageDelegateAndroid_jni.h"
 
 using base::android::AttachCurrentThread;
@@ -21,10 +22,7 @@ InterstitialPageDelegateAndroid::InterstitialPageDelegateAndroid(
     JNIEnv* env,
     jobject obj,
     const std::string& html_content)
-    : weak_java_obj_(env, obj),
-      html_content_(html_content),
-      page_(NULL) {
-}
+    : weak_java_obj_(env, obj), html_content_(html_content), page_(NULL) {}
 
 InterstitialPageDelegateAndroid::~InterstitialPageDelegateAndroid() {
   JNIEnv* env = AttachCurrentThread();
@@ -45,6 +43,20 @@ void InterstitialPageDelegateAndroid::DontProceed(
     const JavaParamRef<jobject>& obj) {
   if (page_)
     page_->DontProceed();
+}
+
+void InterstitialPageDelegateAndroid::ShowInterstitialPage(
+    JNIEnv* env,
+    const JavaParamRef<jobject>& obj,
+    const JavaParamRef<jstring>& jurl,
+    const JavaParamRef<jobject>& jweb_contents) {
+  WebContents* web_contents = WebContents::FromJavaWebContents(jweb_contents);
+  DCHECK(web_contents);
+  GURL url(base::android::ConvertJavaStringToUTF8(env, jurl));
+  InterstitialPage* interstitial =
+      InterstitialPage::Create(web_contents, false, url, this);
+  set_interstitial_page(interstitial);
+  interstitial->Show();
 }
 
 std::string InterstitialPageDelegateAndroid::GetHTMLContents() {
@@ -73,8 +85,8 @@ void InterstitialPageDelegateAndroid::CommandReceived(
     std::string sanitized_command(command);
     // The JSONified response has quotes, remove them.
     if (sanitized_command.length() > 1 && sanitized_command[0] == '"') {
-      sanitized_command = sanitized_command.substr(
-          1, sanitized_command.length() - 2);
+      sanitized_command =
+          sanitized_command.substr(1, sanitized_command.length() - 2);
     }
 
     Java_InterstitialPageDelegateAndroid_commandReceived(
