0f8b9097377ed9e50401cb68f4361b9d584dab0f
Sebastien SG <sebsg@chromium.org>
2018-05-16 20:35:30 +0000

    Revert "Settings[DICE]: changing sync-everything doesn't impact on-disk prefs"
    This reverts commit 46a6d04b50d8f4d7a03089a065a3066d36c37b32.
    
    Reason for revert: Introduced and inconsistency in how the Sync Payments cards toggle. See crbug.com/839643
    
    Original change's description:
    > Settings[DICE]: changing sync-everything doesn't impact on-disk prefs
    >
    > This CL stops toggling "sync everything" from overriding all individual
    > prefs' on-disk value with "true" (see reasoning in bug report).
    >
    > Bug: 815018
    > Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    > Change-Id: If6287376009b200355426113618933af9ad78243
    > Reviewed-on: https://chromium-review.googlesource.com/935836
    > Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    > Reviewed-by: Tommy Li <tommycli@chromium.org>
    > Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    > Commit-Queue: Scott Chen <scottchen@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#542929}
    
    TBR=tommycli@chromium.org,dpapad@chromium.org,mastiz@chromium.org,scottchen@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 815018,839643
    Change-Id: Iba41940bdfde775048553dd4e0abd6c52e85f7bf
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Reviewed-on: https://chromium-review.googlesource.com/1061893
    Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: Scott Chen <scottchen@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559272}

--- chrome/test/data/webui/settings/test_sync_browser_proxy.js
+++ chrome/test/data/webui/settings/test_sync_browser_proxy.js
@@ -18,7 +18,6 @@ class TestSyncBrowserProxy extends TestBrowserProxy {
       'signOut',
       'startSignIn',
       'startSyncingWithEmail',
-      'setSyncEverything',
     ]);
 
     /** @private {number} */
@@ -90,11 +89,5 @@ class TestSyncBrowserProxy extends TestBrowserProxy {
   setSyncEncryption(syncPrefs) {
     this.methodCalled('setSyncEncryption', syncPrefs);
     return Promise.resolve(this.encryptionResponse);
-  }
-
-  /** @override */
-  setSyncEverything(syncEverything) {
-    this.methodCalled('setSyncEverything', syncEverything);
-    return Promise.resolve(settings.PageStatus.CONFIGURE);
   }
 }
