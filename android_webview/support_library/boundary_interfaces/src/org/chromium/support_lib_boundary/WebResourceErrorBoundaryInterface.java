14f1b45c6e53a5e8bfe6dcbe71a22c2842555a48
Nate Fischer <ntfschr@chromium.org>
2018-05-16 23:18:12 +0000

    AW: remove redundant public modifier from interfaces
    No change to logic.
    
    This removes the 'public' visibility modifier from methods on public
    interfaces, as these methods are implicitly public (and this is a lint
    warning on the Android side).
    
    Bug: 843420
    Test: N/A
    Change-Id: I17ea506f8cb7965c80e2489d985d1a3f2476fb50
    Reviewed-on: https://chromium-review.googlesource.com/1062965
    Reviewed-by: Paul Miller <paulmiller@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559326}

--- android_webview/support_library/boundary_interfaces/src/org/chromium/support_lib_boundary/WebResourceErrorBoundaryInterface.java
+++ android_webview/support_library/boundary_interfaces/src/org/chromium/support_lib_boundary/WebResourceErrorBoundaryInterface.java
@@ -9,6 +9,6 @@ package org.chromium.support_lib_boundary;
  * Boundary interface for WebResourceErrorCompat.
  */
 public interface WebResourceErrorBoundaryInterface {
-    public int getErrorCode();
-    public CharSequence getDescription();
+    int getErrorCode();
+    CharSequence getDescription();
 }
--- android_webview/support_library/boundary_interfaces/src/org/chromium/support_lib_boundary/WebResourceRequestBoundaryInterface.java
+++ android_webview/support_library/boundary_interfaces/src/org/chromium/support_lib_boundary/WebResourceRequestBoundaryInterface.java
@@ -8,4 +8,4 @@ package org.chromium.support_lib_boundary;
 /**
  * Boundary interface for WebResourceRequest.
  */
-public interface WebResourceRequestBoundaryInterface { public boolean isRedirect(); }
+public interface WebResourceRequestBoundaryInterface { boolean isRedirect(); }
