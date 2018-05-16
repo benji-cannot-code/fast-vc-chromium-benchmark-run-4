6fa43539906a1793fdf75c0816ce51cdabd5e872
Francois Doray <fdoray@chromium.org>
2018-05-16 01:40:19 +0000

    Remove TabManager::SetTabAutoDiscardableState.
    TabLifecycleUnitExternal::SetAutoDiscardable should be used instead.
    Removing TabManager::SetTabAutoDiscardableState reduces bloating of
    TabManager.
    
    TBR=rdevlin.cronin@chromium.org
    
    Bug: 775644
    Change-Id: Ic3923f7d076ba0d7338b433c5d6c9dd197402f1a
    Reviewed-on: https://chromium-review.googlesource.com/1057694
    Commit-Queue: François Doray <fdoray@chromium.org>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558916}

--- chrome/browser/extensions/api/tabs/tabs_api.cc
+++ chrome/browser/extensions/api/tabs/tabs_api.cc
@@ -1368,8 +1368,9 @@ bool TabsUpdateFunction::RunAsync() {
 
   if (params->update_properties.auto_discardable.get()) {
     bool state = *params->update_properties.auto_discardable;
-    g_browser_process->GetTabManager()->SetTabAutoDiscardableState(contents,
-                                                                   state);
+    resource_coordinator::TabLifecycleUnitExternal::FromWebContents(
+        web_contents_)
+        ->SetAutoDiscardable(state);
   }
 
   if (!is_async) {
