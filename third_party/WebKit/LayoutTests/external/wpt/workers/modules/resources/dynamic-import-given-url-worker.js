8633e470f558d9d24ca674eb5a2e7913a7200673
Hiroki Nakagawa <nhiroki@chromium.org>
2018-05-16 00:55:22 +0000

    Worker: Throw an ErrorEvent on a failure of module loading
    This behavior is defined in the HTML spec:
    
      Step 13: "If the algorithm asynchronously completes with null, queue a task to
      fire an event named error at worker, and return."
      https://html.spec.whatwg.org/multipage/workers.html#worker-processing-model
    
    An ErrorEvent is created on a worker thread, and passed to the main thread to
    dispatch it on a worker object in a document context.
    
    Bug: 843031
    Change-Id: I1fd82cba1e02b1658bb2b556742f7a206ee754a7
    Reviewed-on: https://chromium-review.googlesource.com/1058998
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558903}

--- third_party/WebKit/LayoutTests/external/wpt/workers/modules/resources/dynamic-import-given-url-worker.js
+++ third_party/WebKit/LayoutTests/external/wpt/workers/modules/resources/dynamic-import-given-url-worker.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Dynamically import the script URL sent by postMessage().
 self.addEventListener('message', e => {
-  import(e.data);
+  import(e.data).catch(error_event => postMessage('ERROR'));
 });
