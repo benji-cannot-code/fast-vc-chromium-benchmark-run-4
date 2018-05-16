1c76249dae15dd0b159acc7eb495ca74fe60a7bd
Troy Hildebrandt <thildebr@chromium.org>
2018-05-16 22:25:45 +0000

    Add test for queries that look like URLs for Query in Omnibox.
    Change-Id: Ibe077e38a97226b8bbf8af9c974a3ef2e4472c8a
    Reviewed-on: https://chromium-review.googlesource.com/1062657
    Reviewed-by: Maria Khomenko <mariakhomenko@chromium.org>
    Commit-Queue: Troy Hildebrandt <thildebr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559311}

--- chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/LocationBarLayoutTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/LocationBarLayoutTest.java
@@ -56,7 +56,10 @@ public class LocationBarLayoutTest {
     private static final int SEARCH_ICON_RESOURCE = R.drawable.omnibox_search;
 
     private static final String SEARCH_TERMS = "machine learning";
+    private static final String SEARCH_TERMS_URL = "testing.com";
     private static final String GOOGLE_SRP_URL = "https://www.google.com/search?q=machine+learning";
+    private static final String GOOGLE_SRP_URL_LIKE_URL =
+            "https://www.google.com/search?q=" + SEARCH_TERMS_URL;
     private static final String BING_SRP_URL = "https://www.bing.com/search?q=machine+learning";
 
     private static final String VERBOSE_URL = "https://www.suchwowveryyes.edu";
@@ -250,7 +253,7 @@ public class LocationBarLayoutTest {
         mTestToolbarModel.setSecurityLevel(ConnectionSecurityLevel.SECURE);
         setUrlToPageUrl(locationBar);
 
-        Assert.assertEquals(SEARCH_TERMS, urlBar.getText().toString());
+        Assert.assertEquals(SEARCH_TERMS, getUrlText(urlBar));
     }
 
     @Test
@@ -267,7 +270,7 @@ public class LocationBarLayoutTest {
         mTestToolbarModel.setSecurityLevel(ConnectionSecurityLevel.SECURE);
         setUrlToPageUrl(locationBar);
 
-        Assert.assertEquals(SEARCH_TERMS, urlBar.getText().toString());
+        Assert.assertEquals(SEARCH_TERMS, getUrlText(urlBar));
     }
 
     @Test
@@ -284,7 +287,7 @@ public class LocationBarLayoutTest {
         mTestToolbarModel.setSecurityLevel(ConnectionSecurityLevel.SECURE);
         setUrlToPageUrl(locationBar);
 
-        Assert.assertNotEquals(SEARCH_TERMS, urlBar.getText().toString());
+        Assert.assertNotEquals(SEARCH_TERMS, getUrlText(urlBar));
     }
 
     @Test
@@ -330,9 +333,25 @@ public class LocationBarLayoutTest {
 
     @Test
     @SmallTest
+    @EnableFeatures(ChromeFeatureList.QUERY_IN_OMNIBOX)
+    @Feature({"QueryInOmnibox"})
+    public void testNotShowingSearchTermsIfLooksLikeUrl() throws ExecutionException {
+        final UrlBar urlBar = getUrlBar();
+        final LocationBarLayout locationBar = getLocationBar();
+
+        mTestToolbarModel.setCurrentUrl(GOOGLE_SRP_URL_LIKE_URL);
+        mTestToolbarModel.setSecurityLevel(ConnectionSecurityLevel.SECURE);
+        setUrlToPageUrl(locationBar);
+
+        Assert.assertNotEquals(SEARCH_TERMS_URL, getUrlText(urlBar));
+    }
+
+    @Test
+    @SmallTest
     @DisableFeatures(ChromeFeatureList.QUERY_IN_OMNIBOX)
     @Feature({"QueryInOmnibox"})
-    public void testNotShowingSearchTermsIfSrpIsGoogleAndFlagIsDisabled() {
+    public void testNotShowingSearchTermsIfSrpIsGoogleAndFlagIsDisabled()
+            throws ExecutionException {
         final UrlBar urlBar = getUrlBar();
         final LocationBarLayout locationBar = getLocationBar();
 
