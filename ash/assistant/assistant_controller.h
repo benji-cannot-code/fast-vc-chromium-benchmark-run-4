ae1c972d66aaf3c48a27dbf6869ff7f6700212a0
Muyuan Li <muyuanli@chromium.org>
2018-05-16 02:57:23 +0000

    add RequestScreenshot in assistant_controller.
    Bug: 826454
    Test: Manual
    Change-Id: I60b0adcec966cb23fe5e54b2fba6090d06c89916
    Reviewed-on: https://chromium-review.googlesource.com/1058670
    Commit-Queue: Muyuan Li <muyuanli@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558945}

--- ash/assistant/assistant_controller.h
+++ ash/assistant/assistant_controller.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "chromeos/services/assistant/public/mojom/assistant.mojom.h"
 #include "mojo/public/cpp/bindings/binding.h"
+#include "ui/gfx/geometry/rect.h"
 
 namespace base {
 class UnguessableToken;
@@ -118,6 +119,8 @@ class AssistantController
       chromeos::assistant::mojom::AssistantPtr assistant) override;
   void SetAssistantCardRenderer(
       mojom::AssistantCardRendererPtr assistant_card_renderer) override;
+  void RequestScreenshot(const gfx::Rect& rect,
+                         RequestScreenshotCallback callback) override;
 
  private:
   mojo::Binding<mojom::AssistantController> assistant_controller_binding_;
