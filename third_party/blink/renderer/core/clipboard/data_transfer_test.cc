fc640ec0cdb83f2cb5e67f0c55752f66d461f2a6
Steve Kobes <skobes@chromium.org>
2018-05-16 18:48:28 +0000

    RLS: Deparameterize core/{dom,editing,clipboard,loader} unit tests.
    Bug: 823365
    Change-Id: I34bd09b180ed8c7403217d0931cf2e765dc951d8
    Reviewed-on: https://chromium-review.googlesource.com/1062288
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559205}

--- third_party/blink/renderer/core/clipboard/data_transfer_test.cc
+++ third_party/blink/renderer/core/clipboard/data_transfer_test.cc
@@ -13,18 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/layout_object.h"
 #include "third_party/blink/renderer/core/testing/core_unit_test_helper.h"
 #include "third_party/blink/renderer/platform/drag_image.h"
-#include "third_party/blink/renderer/platform/testing/runtime_enabled_features_test_helpers.h"
 
 namespace blink {
 
-typedef bool TestParamRootLayerScrolling;
-class DataTransferTest
-    : public RenderingTest,
-      public testing::WithParamInterface<TestParamRootLayerScrolling>,
-      private ScopedRootLayerScrollingForTest {
- public:
-  DataTransferTest() : ScopedRootLayerScrollingForTest(GetParam()) {}
-
+class DataTransferTest : public RenderingTest {
  protected:
   Page& GetPage() const { return *GetDocument().GetPage(); }
   LocalFrame& GetFrame() const { return *GetDocument().GetFrame(); }
@@ -33,9 +25,7 @@ class DataTransferTest
   }
 };
 
-INSTANTIATE_TEST_CASE_P(All, DataTransferTest, testing::Bool());
-
-TEST_P(DataTransferTest, NodeImage) {
+TEST_F(DataTransferTest, NodeImage) {
   SetBodyInnerHTML(R"HTML(
     <style>
       #sample { width: 100px; height: 100px; }
@@ -48,7 +38,7 @@ TEST_P(DataTransferTest, NodeImage) {
   EXPECT_EQ(IntSize(100, 100), image->Size());
 }
 
-TEST_P(DataTransferTest, NodeImageWithNestedElement) {
+TEST_F(DataTransferTest, NodeImageWithNestedElement) {
   SetBodyInnerHTML(R"HTML(
     <style>
       div { -webkit-user-drag: element }
@@ -65,7 +55,7 @@ TEST_P(DataTransferTest, NodeImageWithNestedElement) {
       << "Descendants node should have :-webkit-drag.";
 }
 
-TEST_P(DataTransferTest, NodeImageWithPsuedoClassWebKitDrag) {
+TEST_F(DataTransferTest, NodeImageWithPsuedoClassWebKitDrag) {
   SetBodyInnerHTML(R"HTML(
     <style>
       #sample { width: 100px; height: 100px; }
@@ -80,7 +70,7 @@ TEST_P(DataTransferTest, NodeImageWithPsuedoClassWebKitDrag) {
       << ":-webkit-drag should affect dragged image.";
 }
 
-TEST_P(DataTransferTest, NodeImageWithoutDraggedLayoutObject) {
+TEST_F(DataTransferTest, NodeImageWithoutDraggedLayoutObject) {
   SetBodyInnerHTML(R"HTML(
     <style>
       #sample { width: 100px; height: 100px; }
@@ -94,7 +84,7 @@ TEST_P(DataTransferTest, NodeImageWithoutDraggedLayoutObject) {
   EXPECT_EQ(nullptr, image.get()) << ":-webkit-drag blows away layout object";
 }
 
-TEST_P(DataTransferTest, NodeImageWithChangingLayoutObject) {
+TEST_F(DataTransferTest, NodeImageWithChangingLayoutObject) {
   SetBodyInnerHTML(R"HTML(
     <style>
       #sample { color: blue; }
@@ -122,7 +112,7 @@ TEST_P(DataTransferTest, NodeImageWithChangingLayoutObject) {
       << "#sample doesn't have :-webkit-drag.";
 }
 
-TEST_P(DataTransferTest, NodeImageExceedsViewportBounds) {
+TEST_F(DataTransferTest, NodeImageExceedsViewportBounds) {
   SetBodyInnerHTML(R"HTML(
     <style>
       * { margin: 0; }
@@ -135,7 +125,7 @@ TEST_P(DataTransferTest, NodeImageExceedsViewportBounds) {
   EXPECT_EQ(IntSize(800, 600), image->Size());
 }
 
-TEST_P(DataTransferTest, NodeImageUnderScrollOffset) {
+TEST_F(DataTransferTest, NodeImageUnderScrollOffset) {
   SetBodyInnerHTML(R"HTML(
     <style>
       * { margin: 0; }
@@ -166,7 +156,7 @@ TEST_P(DataTransferTest, NodeImageUnderScrollOffset) {
             second_image->Size());
 }
 
-TEST_P(DataTransferTest, NodeImageSizeWithPageScaleFactor) {
+TEST_F(DataTransferTest, NodeImageSizeWithPageScaleFactor) {
   SetBodyInnerHTML(R"HTML(
     <style>
       * { margin: 0; }
@@ -197,7 +187,7 @@ TEST_P(DataTransferTest, NodeImageSizeWithPageScaleFactor) {
       image_with_offset->Size());
 }
 
-TEST_P(DataTransferTest, NodeImageSizeWithPageScaleFactorTooLarge) {
+TEST_F(DataTransferTest, NodeImageSizeWithPageScaleFactorTooLarge) {
   SetBodyInnerHTML(R"HTML(
     <style>
       * { margin: 0; }
@@ -228,7 +218,7 @@ TEST_P(DataTransferTest, NodeImageSizeWithPageScaleFactorTooLarge) {
             image_with_offset->Size());
 }
 
-TEST_P(DataTransferTest, NodeImageWithPageScaleFactor) {
+TEST_F(DataTransferTest, NodeImageWithPageScaleFactor) {
   // #bluegreen is a 2x1 rectangle where the left pixel is blue and the right
   // pixel is green. The element is offset by a margin of 1px.
   SetBodyInnerHTML(R"HTML(
@@ -267,7 +257,7 @@ TEST_P(DataTransferTest, NodeImageWithPageScaleFactor) {
       EXPECT_EQ(expected_bitmap.getColor(x, y), bitmap.getColor(x, y));
 }
 
-TEST_P(DataTransferTest, NodeImageFullyOffscreen) {
+TEST_F(DataTransferTest, NodeImageFullyOffscreen) {
   SetBodyInnerHTML(R"HTML(
     <style>
     #target {
@@ -294,7 +284,7 @@ TEST_P(DataTransferTest, NodeImageFullyOffscreen) {
   EXPECT_EQ(IntSize(200, 100), image->Size());
 }
 
-TEST_P(DataTransferTest, NodeImageWithScrolling) {
+TEST_F(DataTransferTest, NodeImageWithScrolling) {
   SetBodyInnerHTML(R"HTML(
     <style>
     #target {
@@ -316,7 +306,7 @@ TEST_P(DataTransferTest, NodeImageWithScrolling) {
   EXPECT_EQ(IntSize(200, 100), image->Size());
 }
 
-TEST_P(DataTransferTest, NodeImageInOffsetStackingContext) {
+TEST_F(DataTransferTest, NodeImageInOffsetStackingContext) {
   SetBodyInnerHTML(R"HTML(
     <style>
       * { margin: 0; }
@@ -350,7 +340,7 @@ TEST_P(DataTransferTest, NodeImageInOffsetStackingContext) {
   }
 }
 
-TEST_P(DataTransferTest, NodeImageWithLargerPositionedDescendant) {
+TEST_F(DataTransferTest, NodeImageWithLargerPositionedDescendant) {
   SetBodyInnerHTML(R"HTML(
     <style>
       * { margin: 0; }
--- third_party/blink/renderer/core/editing/rendered_position_test.cc
+++ third_party/blink/renderer/core/editing/rendered_position_test.cc
@@ -15,16 +15,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/layout_box.h"
 #include "third_party/blink/renderer/core/paint/compositing/composited_selection.h"
 #include "third_party/blink/renderer/core/paint/paint_layer_scrollable_area.h"
-#include "third_party/blink/renderer/platform/testing/runtime_enabled_features_test_helpers.h"
 #include "third_party/blink/renderer/platform/testing/use_mock_scrollbar_settings.h"
 
 namespace blink {
 
-class RenderedPositionTest : public testing::WithParamInterface<bool>,
-                             private ScopedRootLayerScrollingForTest,
-                             public EditingTestBase {
+class RenderedPositionTest : public EditingTestBase {
  public:
-  RenderedPositionTest() : ScopedRootLayerScrollingForTest(GetParam()) {}
   void SetUp() override {
     EditingTestBase::SetUp();
     GetPage().GetSettings().SetAcceleratedCompositingEnabled(true);
@@ -50,9 +46,7 @@ class RenderedPositionTest : public testing::WithParamInterface<bool>,
   UseMockScrollbarSettings mock_scrollbars_;
 };
 
-INSTANTIATE_TEST_CASE_P(All, RenderedPositionTest, testing::Bool());
-
-TEST_P(RenderedPositionTest, ComputeCompositedSelection) {
+TEST_F(RenderedPositionTest, ComputeCompositedSelection) {
   SetBodyContent(R"HTML(
       <!DOCTYPE html>
       input {
@@ -72,7 +66,7 @@ TEST_P(RenderedPositionTest, ComputeCompositedSelection) {
   EXPECT_TRUE(composited_selection.end.hidden);
 }
 
-TEST_P(RenderedPositionTest, PositionInScrollableRoot) {
+TEST_F(RenderedPositionTest, PositionInScrollableRoot) {
   SetBodyContent(R"HTML(
       <!DOCTYPE html>
       <style>
@@ -117,7 +111,7 @@ TEST_P(RenderedPositionTest, PositionInScrollableRoot) {
             composited_selection.end.edge_bottom_in_layer);
 }
 
-TEST_P(RenderedPositionTest, PositionInScroller) {
+TEST_F(RenderedPositionTest, PositionInScroller) {
   SetBodyContent(R"HTML(
       <!DOCTYPE html>
       <style>
@@ -184,7 +178,7 @@ TEST_P(RenderedPositionTest, PositionInScroller) {
 }
 
 // crbug.com/807930
-TEST_P(RenderedPositionTest, ContentEditableLinebreak) {
+TEST_F(RenderedPositionTest, ContentEditableLinebreak) {
   SetBodyContent(
       "<div style='font: 10px/10px Ahem;' contenteditable>"
       "test<br><br></div>");
@@ -203,7 +197,7 @@ TEST_P(RenderedPositionTest, ContentEditableLinebreak) {
 }
 
 // crbug.com/807930
-TEST_P(RenderedPositionTest, TextAreaLinebreak) {
+TEST_F(RenderedPositionTest, TextAreaLinebreak) {
   SetBodyContent(
       "<textarea style='font: 10px/10px Ahem;'>"
       "test\n</textarea>");
@@ -221,7 +215,7 @@ TEST_P(RenderedPositionTest, TextAreaLinebreak) {
 }
 
 // crbug.com/815099
-TEST_P(RenderedPositionTest, CaretBeforeSoftWrap) {
+TEST_F(RenderedPositionTest, CaretBeforeSoftWrap) {
   SetBodyContent(
       "<div style='font: 10px/10px Ahem; width:20px;' "
       "contenteditable>foo</div>");
@@ -247,7 +241,7 @@ TEST_P(RenderedPositionTest, CaretBeforeSoftWrap) {
             FloatPoint(27.0f, 18.0f));
 }
 
-TEST_P(RenderedPositionTest, CaretAfterSoftWrap) {
+TEST_F(RenderedPositionTest, CaretAfterSoftWrap) {
   SetBodyContent(
       "<div style='font: 10px/10px Ahem; width:20px;' "
       "contenteditable>foo</div>");
@@ -274,7 +268,7 @@ TEST_P(RenderedPositionTest, CaretAfterSoftWrap) {
 }
 
 // crbug.com/834686
-TEST_P(RenderedPositionTest, RangeBeginAtBlockEnd) {
+TEST_F(RenderedPositionTest, RangeBeginAtBlockEnd) {
   const SelectionInDOMTree& selection = SetSelectionTextToBody(
       "<div style='font: 10px/10px Ahem;'>"
       "<div>foo\n^</div><div>ba|r</div></div>");
