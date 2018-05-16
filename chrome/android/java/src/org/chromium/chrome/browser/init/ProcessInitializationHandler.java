1552383341d7106a3f3c01adf4bd6b09ca118836
Conley Owens <cco3@chromium.org>
2018-05-16 18:18:04 +0000

    physicalweb: Remove vestiges of the Physical Web
    This change removes all code related to the Physical Web.
    
    BUG=826540
    
    Change-Id: Ifda5d80c5cebeedf2e2fdf48142e6dd6c1ef73b6
    Reviewed-on: https://chromium-review.googlesource.com/1054335
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Commit-Queue: Conley Owens <cco3@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559187}

--- chrome/android/java/src/org/chromium/chrome/browser/init/ProcessInitializationHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/init/ProcessInitializationHandler.java
@@ -61,7 +61,6 @@ import org.chromium.chrome.browser.ntp.NewTabPage;
 import org.chromium.chrome.browser.partnercustomizations.HomepageManager;
 import org.chromium.chrome.browser.partnercustomizations.PartnerBrowserCustomizations;
 import org.chromium.chrome.browser.photo_picker.PhotoPickerDialog;
-import org.chromium.chrome.browser.physicalweb.PhysicalWeb;
 import org.chromium.chrome.browser.preferences.ChromePreferenceManager;
 import org.chromium.chrome.browser.preferences.PrefServiceBridge;
 import org.chromium.chrome.browser.profiles.ProfileManagerUtils;
@@ -303,14 +302,6 @@ public class ProcessInitializationHandler {
                 startModerateBindingManagementIfNeeded(application);
 
                 recordKeyboardLocaleUma(application);
-            }
-        });
-
-        deferredStartupHandler.addDeferredTask(new Runnable() {
-            @Override
-            public void run() {
-                // Start or stop Physical Web
-                PhysicalWeb.onChromeStart();
             }
         });
 
--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -106,8 +106,6 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &kOmniboxSpareRenderer,
     &kOmniboxVoiceSearchAlwaysVisible,
     &kPayWithGoogleV1,
-    &kPhysicalWebFeature,
-    &kPhysicalWebSharing,
     &kProgressBarThrottleFeature,
     &kPwaImprovedSplashScreen,
     &kPwaPersistentNotification,
@@ -311,12 +309,6 @@ const base::Feature kOmniboxVoiceSearchAlwaysVisible{
 
 const base::Feature kPayWithGoogleV1{"PayWithGoogleV1",
                                      base::FEATURE_ENABLED_BY_DEFAULT};
-
-const base::Feature kPhysicalWebFeature{"PhysicalWeb",
-                                        base::FEATURE_DISABLED_BY_DEFAULT};
-
-const base::Feature kPhysicalWebSharing{"PhysicalWebSharing",
-                                        base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kProgressBarThrottleFeature{
     "ProgressBarThrottle", base::FEATURE_DISABLED_BY_DEFAULT};
