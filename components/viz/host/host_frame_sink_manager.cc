501707ae14144417823e7630164c6685729a8e79
kylechar <kylechar@chromium.org>
2018-05-16 15:13:38 +0000

    viz: Fix null callbacks.
    Empty once callbacks can't be run, so pass DoNothing callback instead.
    
    Bug: 843458
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel
    Change-Id: I657e750efba2d12fea9f89d3617cfeebbaed78e3
    Reviewed-on: https://chromium-review.googlesource.com/1061576
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559103}

--- components/viz/host/host_frame_sink_manager.cc
+++ components/viz/host/host_frame_sink_manager.cc
@@ -138,7 +138,7 @@ void HostFrameSinkManager::CreateRootCompositorFrameSink(
   // the old CompositorFrameSink first.
   if (data.has_created_compositor_frame_sink) {
     frame_sink_manager_->DestroyCompositorFrameSink(frame_sink_id,
-                                                    base::OnceClosure());
+                                                    base::DoNothing());
   }
 
   data.is_root = true;
@@ -161,7 +161,7 @@ void HostFrameSinkManager::CreateCompositorFrameSink(
   // the old CompositorFrameSink first.
   if (data.has_created_compositor_frame_sink) {
     frame_sink_manager_->DestroyCompositorFrameSink(frame_sink_id,
-                                                    base::OnceClosure());
+                                                    base::DoNothing());
   }
 
   data.is_root = false;
