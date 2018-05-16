1ab641f0d5f22a3490ae152ed2f2475ca0f935ea
Joe Downing <joedow@chromium.org>
2018-05-16 22:28:50 +0000

    [KeyboardLock] Add debug console message for invalid DOMStrings
    This CL adds a debugging message to the console window if the caller
    supplies one or more invalid DOMStrings.  We currently reject the
    promise if all strings are invalid, but if a subset are invalid,
    there was no good way to debug that issue before this change.  Now
    it is obvious which DOMString is not a valid DomCode.  It also helps
    prevent silent failures if a single invalid key was passed in.
    
    BUG=680809
    
    Change-Id: If978952e53e35e128528aad28ff1e0bdc969ba14
    Reviewed-on: https://chromium-review.googlesource.com/1062367
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559314}

--- content/browser/keyboard_lock/keyboard_lock_service_impl.cc
+++ content/browser/keyboard_lock/keyboard_lock_service_impl.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/renderer_host/render_widget_host_impl.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_widget_host.h"
+#include "content/public/common/console_message_level.h"
 #include "content/public/common/content_features.h"
 #include "ui/events/keycodes/dom/dom_code.h"
 #include "ui/events/keycodes/dom/keycode_converter.h"
@@ -82,8 +83,13 @@ void KeyboardLockServiceImpl::RequestKeyboardLock(
   std::vector<ui::DomCode> dom_codes;
   for (const std::string& code : key_codes) {
     ui::DomCode dom_code = ui::KeycodeConverter::CodeStringToDomCode(code);
-    if (dom_code != ui::DomCode::NONE)
+    if (dom_code != ui::DomCode::NONE) {
       dom_codes.push_back(dom_code);
+    } else {
+      render_frame_host_->AddMessageToConsole(
+          ConsoleMessageLevel::CONSOLE_MESSAGE_LEVEL_WARNING,
+          "Invalid DOMString passed into keyboard.lock(): '" + code + "'");
+    }
   }
 
   // If we are provided with a vector containing only invalid keycodes, then
