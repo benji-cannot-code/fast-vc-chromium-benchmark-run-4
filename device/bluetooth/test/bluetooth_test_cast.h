08587e48032763bde8ecb6aec554f1d6176eb630
Bailey Forrest <bcf@chromium.org>
2018-05-16 00:30:44 +0000

    [chromecast][Bluetooth] Fix connect/services logic
    This allows WebBT to work if we were connected before or after the WebBT
    application starts.
    
    Remove BluetoothAdapterCast::OnServicesUpdated because Connect now
    implies services are discovered. Dynamic service updating is also
    deprecated:
    https://cs.chromium.org/chromium/src/device/bluetooth/bluetooth_adapter.h?rcl=21e8d8c8dc11cd4b8f77c47b7f37098ebde13d3a&l=142
    
    BUG=internal b/79542125
    TEST=device_unittests. Manual
    
    Change-Id: I99ed5dc61942cb310ed76581648fdab6c31316a1
    Reviewed-on: https://chromium-review.googlesource.com/1058605
    Commit-Queue: Bailey Forrest <bcf@chromium.org>
    Reviewed-by: Stephen Lanham <slan@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558898}

--- device/bluetooth/test/bluetooth_test_cast.h
+++ device/bluetooth/test/bluetooth_test_cast.h
@@ -7,22 +7,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define DEVICE_BLUETOOTH_TEST_BLUETOOTH_TEST_CAST_H_
 
 #include <cstdint>
+#include <map>
+#include <string>
 #include <vector>
 
 #include "base/callback_forward.h"
 #include "base/macros.h"
 #include "base/optional.h"
-#include "chromecast/device/bluetooth/shlib/mock_gatt_client.h"
+#include "chromecast/device/bluetooth/le/mock_le_scan_manager.h"
+#include "chromecast/public/bluetooth/bluetooth_types.h"
 #include "device/bluetooth/bluetooth_device.h"
 #include "device/bluetooth/bluetooth_local_gatt_characteristic.h"
 #include "device/bluetooth/bluetooth_local_gatt_descriptor.h"
 #include "device/bluetooth/test/bluetooth_test.h"
-
-namespace chromecast {
-namespace bluetooth {
-class GattClientManagerImpl;
-}
-}  // namespace chromecast
 
 namespace device {
 
@@ -38,8 +35,7 @@ class BluetoothTestCast : public BluetoothTestBase {
   BluetoothDevice* SimulateLowEnergyDevice(int device_ordinal) override;
 
  private:
-  class FakeGattClientManager;
-  class FakeLeScanManager;
+  class GattClientManager;
 
   void UpdateAdapter(
       const std::string& address,
@@ -48,11 +44,8 @@ class BluetoothTestCast : public BluetoothTestBase {
       const std::map<std::string, std::vector<uint8_t>>& service_data,
       const std::map<uint16_t, std::vector<uint8_t>>& manufacturer_data);
 
-  scoped_refptr<base::SingleThreadTaskRunner> io_task_runner_;
-  chromecast::bluetooth_v2_shlib::MockGattClient mock_gatt_client_;
-  std::unique_ptr<chromecast::bluetooth::GattClientManagerImpl>
-      gatt_client_manager_;
-  std::unique_ptr<FakeLeScanManager> fake_le_scan_manager_;
+  const std::unique_ptr<GattClientManager> gatt_client_manager_;
+  ::chromecast::bluetooth::MockLeScanManager le_scan_manager_;
 
   DISALLOW_COPY_AND_ASSIGN(BluetoothTestCast);
 };
