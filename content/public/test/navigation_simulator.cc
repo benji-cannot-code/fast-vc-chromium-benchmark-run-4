9ae9a336d059d6a425d0562780c65144e302b2e2
Alexandr Ilin <alexilin@chromium.org>
2018-05-16 15:05:49 +0000

    Fix a typo in a NavigationSimulator log message
    Change-Id: I4c7aed23d680d70283f68124a7b7883b246cd0d9
    Reviewed-on: https://chromium-review.googlesource.com/1061465
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Commit-Queue: Alexandr Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559098}

--- content/public/test/navigation_simulator.cc
+++ content/public/test/navigation_simulator.cc
@@ -584,7 +584,7 @@ void NavigationSimulator::CommitErrorPage() {
 void NavigationSimulator::CommitSameDocument() {
   if (!browser_initiated_) {
     CHECK_EQ(INITIALIZATION, state_)
-        << "NavigationSimulator::CommitErrorPage should be the only "
+        << "NavigationSimulator::CommitSameDocument should be the only "
            "navigation event function called on the NavigationSimulator";
   } else {
     CHECK(same_document_);
