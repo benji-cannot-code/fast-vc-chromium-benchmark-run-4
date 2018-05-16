2f4b09573765884e1b6417d73e917265e8be2f94
Sadrul Habib Chowdhury <sadrul@chromium.org>
2018-05-16 16:56:23 +0000

    mac: Fix presentation-callbacks.
    ImageTransportSurfaceOverlayMac needs to notify its delegate when the
    presentation-callback is run, because the delegate (implemented by
    GLES2CommandBufferStub in regular chrome) is what ultimately sends the
    IPC for presentation-callback back to the gpu/viz-host.
    
    BUG=811961, 776877
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: Ic0538eae3dbe2bfbd92f1ffdc1a4d5bdf4b248a0
    Reviewed-on: https://chromium-review.googlesource.com/1060741
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559143}

--- gpu/ipc/service/image_transport_surface_overlay_mac.mm
+++ gpu/ipc/service/image_transport_surface_overlay_mac.mm
@@ -43,7 +43,8 @@ ImageTransportSurfaceOverlayMac::ImageTransportSurfaceOverlayMac(
     : delegate_(delegate),
       use_remote_layer_api_(ui::RemoteLayerAPISupported()),
       scale_factor_(1),
-      gl_renderer_id_(0) {
+      gl_renderer_id_(0),
+      weak_ptr_factory_(this) {
   ui::GpuSwitchingManager::GetInstance()->AddObserver(this);
 
   static bool av_disabled_at_command_line =
@@ -183,6 +184,15 @@ void ImageTransportSurfaceOverlayMac::ApplyBackpressure(
   }
 }
 
+void ImageTransportSurfaceOverlayMac::BufferPresented(
+    const PresentationCallback& callback,
+    const gfx::PresentationFeedback& feedback) {
+  DCHECK(!callback.is_null());
+  callback.Run(feedback);
+  if (delegate_)
+    delegate_->BufferPresented(feedback);
+}
+
 gfx::SwapResult ImageTransportSurfaceOverlayMac::SwapBuffersInternal(
     const gfx::Rect& pixel_damage_rect,
     const PresentationCallback& callback) {
@@ -251,7 +261,9 @@ gfx::SwapResult ImageTransportSurfaceOverlayMac::SwapBuffersInternal(
       base::TimeDelta::FromMicroseconds(kRefreshIntervalInMicroseconds),
       0 /* flags */);
   base::ThreadTaskRunnerHandle::Get()->PostTask(
-      FROM_HERE, base::BindOnce(callback, std::move(feedback)));
+      FROM_HERE,
+      base::BindOnce(&ImageTransportSurfaceOverlayMac::BufferPresented,
+                     weak_ptr_factory_.GetWeakPtr(), callback, feedback));
   return gfx::SwapResult::SWAP_ACK;
 }
 
