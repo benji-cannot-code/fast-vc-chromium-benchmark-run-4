3612b6e06590b92c1c550aa6edea6197c2b86ed6
Ryan Hansberry <hansberry@chromium.org>
2018-05-16 19:38:34 +0000

    [CrOS MultiDevice] Create RemoteDeviceRef, a wrapper around RemoteDevice.
    This object will replace the direct usage of the RemoteDevice in future CLs.
    It serves as a reference to a RemoteDevice for clients to pass around amongst
    themselves, while providing the guarantee that the RemoteDevice object is
    not copied (it's important that its BeaconSeeds are not copied because of
    their size), and has an explicit lifetime (which will be tied to lifetime
    of the RemoteDeviceCache, introduced in a future CL).
    
    Bug: 824568, 752273
    Change-Id: Ic2c0f15d5edf893e5c1d9e01c91a95f3e6894340
    Reviewed-on: https://chromium-review.googlesource.com/1060090
    Commit-Queue: Ryan Hansberry <hansberry@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559229}

---
+++ components/cryptauth/remote_device_ref.cc
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/cryptauth/remote_device_ref.h"
+
+namespace cryptauth {
+
+RemoteDeviceRef::RemoteDeviceRef(std::shared_ptr<RemoteDevice> remote_device)
+    : remote_device_(std::move(remote_device)) {}
+
+RemoteDeviceRef::RemoteDeviceRef(RemoteDeviceRef& other) = default;
+
+RemoteDeviceRef::~RemoteDeviceRef() = default;
+
+SoftwareFeatureState RemoteDeviceRef::GetSoftwareFeatureState(
+    const SoftwareFeature& software_feature) const {
+  return remote_device_->GetSoftwareFeatureState(software_feature);
+}
+
+std::string RemoteDeviceRef::GetDeviceId() const {
+  return remote_device_->GetDeviceId();
+}
+
+std::string RemoteDeviceRef::GetTruncatedDeviceIdForLogs() const {
+  return remote_device_->GetTruncatedDeviceIdForLogs();
+}
+
+bool RemoteDeviceRef::operator==(const RemoteDeviceRef& other) const {
+  return *remote_device_ == *other.remote_device_;
+}
+
+bool RemoteDeviceRef::operator<(const RemoteDeviceRef& other) const {
+  return *remote_device_ < *other.remote_device_;
+}
+
+}  // namespace cryptauth
