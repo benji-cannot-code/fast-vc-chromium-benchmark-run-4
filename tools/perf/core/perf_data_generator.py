05627e467cb465a11cfdb03b051712d4c7530185
Emily Hanley <eyaich@google.com>
2018-05-16 00:35:24 +0000

    Implement smoke test mode for performance_test_suite
    1) Updates the script that executes the tests on the swarming bot by
    accepting a flag for the benchmark to shard map.  We now have at least
    three use cases for this: main perf waterfall android and desktop,
    smoke testing, testing on our trybot and potentially android go.
    
    2) Updates the custom merge script to not upload perf results when
    we are in smoke test mode.
    
    Bug:840427
    Change-Id: Idec8f45cc2d1b5a3cd336ef3d1698d3d86161ba3
    
    NOTRY=true  # win_7_chromium_rel_ng flaky
    
    Change-Id: Idec8f45cc2d1b5a3cd336ef3d1698d3d86161ba3
    Reviewed-on: https://chromium-review.googlesource.com/1055614
    Commit-Queue: Ned Nguyen <nednguyen@google.com>
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Cr-Commit-Position: refs/heads/master@{#558900}

--- tools/perf/core/perf_data_generator.py
+++ tools/perf/core/perf_data_generator.py
@@ -971,7 +971,10 @@ NEW_PERF_RECIPE_FYI_TESTERS = {
       'tests': [
         {
           'isolate': 'performance_test_suite',
-          'extra_args': ['--run-ref-build'],
+          'extra_args': [
+            '--run-ref-build',
+            '--test-shard-map-filename=benchmark_desktop_bot_map.json',
+          ],
           'num_shards': 26
         },
         {
@@ -998,8 +1001,11 @@ NEW_PERF_RECIPE_FYI_TESTERS = {
       'tests': [
         {
           'isolate': 'telemetry_perf_tests_without_chrome',
-          'extra_args': ['--xvfb',
-                         '--run-ref-build'],
+          'extra_args': [
+            '--xvfb',
+            '--run-ref-build',
+            '--test-shard-map-filename=benchmark_bot_map.json'
+          ],
           'num_shards': 3
         },
         {
@@ -1023,7 +1029,10 @@ NEW_PERF_RECIPE_FYI_TESTERS = {
         {
           'name': 'performance_test_suite',
           'isolate': 'performance_test_suite',
-          'extra_args': ['--run-ref-build'],
+          'extra_args': [
+            '--run-ref-build',
+            '--test-shard-map-filename=benchmark_android_bot_map.json',
+          ],
           'num_shards': 14
         }
       ],
@@ -1041,6 +1050,9 @@ NEW_PERF_RECIPE_FYI_TESTERS = {
       'tests': [
         {
           'isolate': 'performance_webview_test_suite',
+          'extra_args': [
+            '--test-shard-map-filename=benchmark_android_bot_map.json',
+          ],
           'num_shards': 7
         }
       ],
@@ -1058,7 +1070,10 @@ NEW_PERF_RECIPE_FYI_TESTERS = {
       'tests': [
         {
           'isolate': 'performance_test_suite',
-          'extra_args': ['--run-ref-build'],
+          'extra_args': [
+            '--run-ref-build',
+            '--test-shard-map-filename=benchmark_android_bot_map.json',
+          ],
           'num_shards': 7
         }
       ],
@@ -1082,7 +1097,10 @@ NEW_PERF_RECIPE_MIGRATED_TESTERS = {
       'tests': [
         {
           'isolate': 'performance_test_suite',
-          'extra_args': ['--run-ref-build'],
+          'extra_args': [
+              '--run-ref-build',
+              '--test-shard-map-filename=benchmark_desktop_bot_map.json',
+          ],
         },
         {
           'isolate': 'load_library_perf_tests',
@@ -1112,7 +1130,10 @@ NEW_PERF_RECIPE_MIGRATED_TESTERS = {
         # Add views_perftests, crbug.com/811766
         {
           'isolate': 'performance_test_suite',
-          'extra_args': ['--run-ref-build'],
+          'extra_args': [
+              '--run-ref-build',
+              '--test-shard-map-filename=benchmark_desktop_bot_map.json',
+          ],
         },
         {
           'isolate': 'load_library_perf_tests',
@@ -1213,11 +1234,6 @@ def generate_telemetry_args(tester_config):
   if browser_name == 'android-webview':
     test_args.append(
         '--webview-embedder-apk=../../out/Release/apks/SystemWebViewShell.apk')
-
-  # Appending testing=true if we only want to run a subset of benchmarks
-  # for quicker testing
-  if tester_config.get('testing', False):
-    test_args.append('--testing=true')
 
   return test_args
 
