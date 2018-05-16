ec44a34124ac6a1f0c83faebe7dcab8b74692399
Christopher Thompson <cthomp@chromium.org>
2018-05-16 15:13:50 +0000

    Add incremented_omnibox_typed_score to visits db
    This implements the first phase of https://crbug.com/542484, to start
    having the omnibox suggest HTTPS URLs directly if there is a redirect
    from HTTP to HTTPS.
    
    This handles initial setup and db migration. A follow up CL will make the
    user-visible changes about scoring. A second follow up CL will change the
    terminology of "typed_count" to "omnibox_typed_score" throughout the rest
    of the codebase.
    
    This CL does the following:
    (1) Move the logic that determines the typed_increment based on
    transition type from HistoryBackend::AddPageVisit() to a helper.
    (2) Adds a migration for the visits database to add a new
    |incremented_omnibox_typed_score| boolean column, using that helper
    function.
    (3) Updates the history expiration logic to use the new column to decide
    when/how to decrement typed counts.
    
    Bug: 542484
    Change-Id: I8b42c5b0e234703ad73537aa63373f240e66cc13
    Reviewed-on: https://chromium-review.googlesource.com/1002890
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Christopher Thompson <cthomp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559104}

--- components/history/core/browser/history_database.cc
+++ components/history/core/browser/history_database.cc
@@ -39,7 +39,7 @@ namespace {
 // Current version number. We write databases at the "current" version number,
 // but any previous version that can read the "compatible" one can make do with
 // our database without *too* many bad effects.
-const int kCurrentVersionNumber = 39;
+const int kCurrentVersionNumber = 40;
 const int kCompatibleVersionNumber = 16;
 const char kEarlyExpirationThresholdKey[] = "early_expiration_threshold";
 const int kMaxHostsInMemory = 10000;
@@ -597,6 +597,13 @@ sql::InitStatus HistoryDatabase::EnsureCurrentVersion() {
   if (cur_version == 38) {
     if (!MigrateDownloadSliceFinished())
       return LogMigrationFailure(38);
+    cur_version++;
+    meta_table_.SetVersionNumber(cur_version);
+  }
+
+  if (cur_version == 39) {
+    if (!MigrateVisitsWithoutIncrementedOmniboxTypedScore())
+      return LogMigrationFailure(39);
     cur_version++;
     meta_table_.SetVersionNumber(cur_version);
   }
--- components/history/core/browser/typed_url_sync_bridge_unittest.cc
+++ components/history/core/browser/typed_url_sync_bridge_unittest.cc
@@ -71,7 +71,9 @@ void AddNewestVisit(ui::PageTransition transition,
                     URLRow* url,
                     VisitVector* visits) {
   base::Time time = base::Time::FromInternalValue(visit_time);
-  visits->insert(visits->begin(), VisitRow(url->id(), time, 0, transition, 0));
+  visits->insert(visits->begin(),
+                 VisitRow(url->id(), time, 0, transition, 0,
+                          HistoryBackend::IsTypedIncrement(transition)));
 
   if (ui::PageTransitionCoreTypeIs(transition, ui::PAGE_TRANSITION_TYPED)) {
     url->set_typed_count(url->typed_count() + 1);
@@ -86,7 +88,8 @@ void AddOldestVisit(ui::PageTransition transition,
                     URLRow* url,
                     VisitVector* visits) {
   base::Time time = base::Time::FromInternalValue(visit_time);
-  visits->push_back(VisitRow(url->id(), time, 0, transition, 0));
+  visits->push_back(VisitRow(url->id(), time, 0, transition, 0,
+                             HistoryBackend::IsTypedIncrement(transition)));
 
   if (ui::PageTransitionCoreTypeIs(transition, ui::PAGE_TRANSITION_TYPED)) {
     url->set_typed_count(url->typed_count() + 1);
@@ -116,11 +119,11 @@ URLRow MakeTypedUrlRow(const std::string& url,
   if (typed_count > 0) {
     // Add a typed visit for time |last_visit|.
     visits->push_back(VisitRow(history_url.id(), last_visit_time, 0,
-                               ui::PAGE_TRANSITION_TYPED, 0));
+                               ui::PAGE_TRANSITION_TYPED, 0, true));
   } else {
     // Add a non-typed visit for time |last_visit|.
     visits->push_back(VisitRow(history_url.id(), last_visit_time, 0,
-                               ui::PAGE_TRANSITION_RELOAD, 0));
+                               ui::PAGE_TRANSITION_RELOAD, 0, false));
   }
 
   history_url.set_visit_count(visits->size());
@@ -414,7 +417,8 @@ class TypedURLSyncBridgeTest : public testing::Test {
   }
 
   static VisitRow CreateVisit(ui::PageTransition type, int64_t timestamp) {
-    return VisitRow(0, base::Time::FromInternalValue(timestamp), 0, type, 0);
+    return VisitRow(0, base::Time::FromInternalValue(timestamp), 0, type, 0,
+                    HistoryBackend::IsTypedIncrement(type));
   }
 
   static TypedURLSyncBridge::MergeResult MergeUrls(
@@ -1239,7 +1243,7 @@ TEST_F(TypedURLSyncBridgeTest, DiffVisitsSame) {
 
   for (int64_t visit : visits) {
     old_visits.push_back(VisitRow(0, base::Time::FromInternalValue(visit), 0,
-                                  ui::PAGE_TRANSITION_TYPED, 0));
+                                  ui::PAGE_TRANSITION_TYPED, 0, true));
     new_url.add_visits(visit);
     new_url.add_visit_transitions(ui::PAGE_TRANSITION_TYPED);
   }
@@ -1269,7 +1273,7 @@ TEST_F(TypedURLSyncBridgeTest, DiffVisitsRemove) {
 
   for (int64_t visit : visits_left) {
     old_visits.push_back(VisitRow(0, base::Time::FromInternalValue(visit), 0,
-                                  ui::PAGE_TRANSITION_TYPED, 0));
+                                  ui::PAGE_TRANSITION_TYPED, 0, true));
   }
 
   for (int64_t visit : visits_right) {
@@ -1303,7 +1307,7 @@ TEST_F(TypedURLSyncBridgeTest, DiffVisitsAdd) {
 
   for (int64_t visit : visits_left) {
     old_visits.push_back(VisitRow(0, base::Time::FromInternalValue(visit), 0,
-                                  ui::PAGE_TRANSITION_TYPED, 0));
+                                  ui::PAGE_TRANSITION_TYPED, 0, true));
   }
 
   for (int64_t visit : visits_right) {
@@ -1493,7 +1497,7 @@ TEST_F(TypedURLSyncBridgeTest, MergeUrlsAfterExpiration) {
   // First, create a history row that has two visits, with timestamps 2 and 3.
   VisitVector(history_visits);
   history_visits.push_back(VisitRow(0, base::Time::FromInternalValue(2), 0,
-                                    ui::PAGE_TRANSITION_TYPED, 0));
+                                    ui::PAGE_TRANSITION_TYPED, 0, true));
   URLRow history_url(
       MakeTypedUrlRow(kURL, kTitle, 2, 3, false, &history_visits));
 
