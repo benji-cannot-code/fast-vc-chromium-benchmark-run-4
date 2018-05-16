258e65869ca909b79152559cb7e5e91babc30e7f
Lily Houghton <lilyhoughton@chromium.org>
2018-05-16 16:41:28 +0000

    Add check to HttpServer::OnReadable to make sure that the pipe is still open.
    Bug: 821000
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: Ic11a4756101fa597c2779e5b82c93e62738c657d
    Reviewed-on: https://chromium-review.googlesource.com/1055742
    Reviewed-by: Helen Li <xunjieli@chromium.org>
    Commit-Queue: Lily Houghton <lilyhoughton@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559137}

--- services/network/public/cpp/server/http_server_unittest.cc
+++ services/network/public/cpp/server/http_server_unittest.cc
@@ -123,6 +123,8 @@ class TestHttpClient {
     ASSERT_TRUE(response.empty());
   }
 
+  void Close() { socket_.reset(); }
+
  private:
   bool IsCompleteResponse(const std::string& response) {
     // Check end of headers first.
@@ -296,8 +298,9 @@ class WebSocketTest : public HttpServerTest {
     NOTREACHED();
   }
 
-  void OnWebSocketRequest(int connection_id,
-                          const HttpServerRequestInfo& info) override {
+  void OnWebSocketRequest(
+      int connection_id,
+      const network::server::HttpServerRequestInfo& info) override {
     HttpServerTest::OnHttpRequest(connection_id, info);
   }
 
@@ -446,6 +449,23 @@ TEST_F(HttpServerTest, RequestWithBody) {
   ASSERT_EQ(body.length(), GetRequest(0).data.length());
   ASSERT_EQ('a', body[0]);
   ASSERT_EQ('c', *body.rbegin());
+}
+
+// Tests that |HttpServer:OnReadable()| will notice the closure of the connected
+// socket and not try to read from an invalid pipe.
+TEST_F(WebSocketTest, PipeClosed) {
+  TestHttpClient client;
+  ASSERT_THAT(client.ConnectAndWait(server_address_), IsOk());
+  client.Send(
+      "GET /test HTTP/1.1\r\n"
+      "Upgrade: WebSocket\r\n"
+      "Connection: SomethingElse, Upgrade\r\n"
+      "Sec-WebSocket-Version: 8\r\n"
+      "Sec-WebSocket-Key: key\r\n"
+      "\r\n");
+  RunUntilRequestsReceived(1);
+  client.Close();
+  RunUntilConnectionIdClosed(1);
 }
 
 TEST_F(WebSocketTest, RequestWebSocket) {
