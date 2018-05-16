a1be35e9c6ed3756d2e77d13d100198ef26acfda
CJ DiMeglio <lethalantidote@chromium.org>
2018-05-16 20:32:21 +0000

    Allows cc::Surfaces for video to work with software compositing.
    This CL makes software compositing for the cc::Surface for video case
    by making VideoFrameSubmitter a SharedBitmapReporter. Before we would
    see blue boxes instead of the video frames. Now we see video frames as
    they should be.
    
    Bug: 807840
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I486456f4d4e96b05317089557084242eeda04455
    Reviewed-on: https://chromium-review.googlesource.com/1057962
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: CJ DiMeglio <lethalantidote@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559267}

--- third_party/blink/renderer/platform/graphics/video_frame_submitter.cc
+++ third_party/blink/renderer/platform/graphics/video_frame_submitter.cc
@@ -22,8 +22,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 namespace {
+
+// TODO(danakj): One day the gpu::mojom::Mailbox type should be shared with
+// blink directly and we won't need to use gpu::mojom::blink::Mailbox, nor the
+// conversion through WTF::Vector.
+gpu::mojom::blink::MailboxPtr SharedBitmapIdToGpuMailboxPtr(
+    const viz::SharedBitmapId& id) {
+  WTF::Vector<int8_t> name(GL_MAILBOX_SIZE_CHROMIUM);
+  for (int i = 0; i < GL_MAILBOX_SIZE_CHROMIUM; ++i)
+    name[i] = id.name[i];
+  return {base::in_place, name};
+}
+
 // Delay to retry getting the context_provider.
 constexpr int kGetContextProviderRetryMS = 150;
+
 }  // namespace
 
 VideoFrameSubmitter::VideoFrameSubmitter(
@@ -133,10 +146,12 @@ void VideoFrameSubmitter::OnReceivedContextProvider(
   }
 
   context_provider_ = context_provider;
-  resource_provider_->Initialize(context_provider);
-
-  if (context_provider_)
+  if (use_gpu_compositing) {
     context_provider_->AddObserver(this);
+    resource_provider_->Initialize(context_provider, nullptr);
+  } else {
+    resource_provider_->Initialize(nullptr, this);
+  }
 
   if (frame_sink_id_.is_valid())
     StartSubmitting();
@@ -276,4 +291,19 @@ void VideoFrameSubmitter::DidPresentCompositorFrame(
 
 void VideoFrameSubmitter::DidDiscardCompositorFrame(
     uint32_t presentation_token) {}
+
+void VideoFrameSubmitter::DidAllocateSharedBitmap(
+    mojo::ScopedSharedBufferHandle buffer,
+    const viz::SharedBitmapId& id) {
+  DCHECK(compositor_frame_sink_);
+  compositor_frame_sink_->DidAllocateSharedBitmap(
+      std::move(buffer), SharedBitmapIdToGpuMailboxPtr(id));
+}
+
+void VideoFrameSubmitter::DidDeleteSharedBitmap(const viz::SharedBitmapId& id) {
+  DCHECK(compositor_frame_sink_);
+  compositor_frame_sink_->DidDeleteSharedBitmap(
+      SharedBitmapIdToGpuMailboxPtr(id));
+}
+
 }  // namespace blink
