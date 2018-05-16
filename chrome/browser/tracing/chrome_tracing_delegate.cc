3799bda29c0b3bd9a19272b6badf96f39118a27e
Siddhartha <ssid@chromium.org>
2018-05-16 03:41:55 +0000

    Fix incognito check for uploading slow reports on Android
    The tracing agent observes for tab model changes to find if incognito
    profile was active.
    
    BUG=720187
    
    Change-Id: I6f06327c8fb7d804ace70096c40c221930394518
    Reviewed-on: https://chromium-review.googlesource.com/1060564
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Reviewed-by: oysteine <oysteine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558961}

--- chrome/browser/tracing/chrome_tracing_delegate.cc
+++ chrome/browser/tracing/chrome_tracing_delegate.cc
@@ -30,6 +30,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/background_tracing_config.h"
 #include "content/public/browser/browser_thread.h"
 
+#if defined(OS_ANDROID)
+#include "chrome/browser/ui/android/tab_model/tab_model.h"
+#include "chrome/browser/ui/android/tab_model/tab_model_list.h"
+#endif
+
 namespace {
 
 const int kMinDaysUntilNextUpload = 7;
@@ -44,6 +49,8 @@ ChromeTracingDelegate::ChromeTracingDelegate() : incognito_launched_(false) {
   CHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
 #if !defined(OS_ANDROID)
   BrowserList::AddObserver(this);
+#else
+  TabModelList::AddObserver(this);
 #endif
 }
 
@@ -51,15 +58,29 @@ ChromeTracingDelegate::~ChromeTracingDelegate() {
   CHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
 #if !defined(OS_ANDROID)
   BrowserList::RemoveObserver(this);
+#else
+  TabModelList::RemoveObserver(this);
 #endif
 }
 
+#if defined(OS_ANDROID)
+void ChromeTracingDelegate::OnTabModelAdded() {
+  for (TabModelList::const_iterator i = TabModelList::begin();
+       i != TabModelList::end(); i++) {
+    if ((*i)->IsOffTheRecord())
+      incognito_launched_ = true;
+  }
+}
+
+void ChromeTracingDelegate::OnTabModelRemoved() {}
+
+#else
+
 void ChromeTracingDelegate::OnBrowserAdded(Browser* browser) {
-#if !defined(OS_ANDROID)
   if (browser->profile()->IsOffTheRecord())
     incognito_launched_ = true;
-#endif
 }
+#endif  // defined(OS_ANDROID)
 
 std::unique_ptr<content::TraceUploader> ChromeTracingDelegate::GetTraceUploader(
     net::URLRequestContextGetter* request_context) {
