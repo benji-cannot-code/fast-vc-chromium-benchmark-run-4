682813e38c6a1ead69825d6b37521921d2ae61c8
Xiaohan Wang <xhwang@chromium.org>
2018-05-16 21:01:38 +0000

    media: Adjust DVLOG levels in MediaCodecAudioDecoder
    This CL makes it easier to filter MCAD logs.
    
    See media logging guidance at
    https://chromium.googlesource.com/chromium/src/+/master/media/README.md#logging
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I5f510999273af202e0f28dc33a8e1b51b126f0d4
    Reviewed-on: https://chromium-review.googlesource.com/1062648
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559282}

--- media/filters/android/media_codec_audio_decoder.cc
+++ media/filters/android/media_codec_audio_decoder.cc
@@ -179,7 +179,7 @@ void MediaCodecAudioDecoder::Decode(scoped_refptr<DecoderBuffer> buffer,
 
   DCHECK(codec_loop_);
 
-  DVLOG(2) << __func__ << " " << buffer->AsHumanReadableString();
+  DVLOG(3) << __func__ << " " << buffer->AsHumanReadableString();
 
   DCHECK_EQ(state_, STATE_READY) << " unexpected state " << AsString(state_);
 
@@ -193,7 +193,7 @@ void MediaCodecAudioDecoder::Decode(scoped_refptr<DecoderBuffer> buffer,
 }
 
 void MediaCodecAudioDecoder::Reset(const base::Closure& closure) {
-  DVLOG(1) << __func__;
+  DVLOG(2) << __func__;
 
   ClearInputQueue(DecodeStatus::ABORTED);
 
@@ -288,7 +288,7 @@ bool MediaCodecAudioDecoder::IsAnyInputPending() const {
 }
 
 MediaCodecLoop::InputData MediaCodecAudioDecoder::ProvideInputData() {
-  DVLOG(2) << __func__;
+  DVLOG(3) << __func__;
 
   const DecoderBuffer* decoder_buffer = input_queue_.front().first.get();
 
@@ -339,7 +339,7 @@ void MediaCodecAudioDecoder::ClearInputQueue(DecodeStatus decode_status) {
 }
 
 void MediaCodecAudioDecoder::SetState(State new_state) {
-  DVLOG(1) << __func__ << ": " << AsString(state_) << "->"
+  DVLOG(3) << __func__ << ": " << AsString(state_) << "->"
            << AsString(new_state);
   state_ = new_state;
 }
@@ -377,7 +377,7 @@ bool MediaCodecAudioDecoder::OnDecodedEos(
 
 bool MediaCodecAudioDecoder::OnDecodedFrame(
     const MediaCodecLoop::OutputBuffer& out) {
-  DVLOG(2) << __func__ << " pts:" << out.pts;
+  DVLOG(3) << __func__ << " pts:" << out.pts;
 
   DCHECK_NE(out.size, 0U);
   DCHECK_NE(out.index, MediaCodecLoop::kInvalidBufferIndex);
