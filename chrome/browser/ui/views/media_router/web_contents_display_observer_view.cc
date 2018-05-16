9fb3f179fc1c3db7f17f987ba6e553ae75f5e502
Elly Fong-Jones <ellyjones@chromium.org>
2018-05-16 22:18:41 +0000

    views: remove MediaRouter WidgetObserver before nulling it
    Failure to do this causes a UAF later.
    
    Bug: 840895
    Change-Id: I4f4a62721b6c67c7d3e5ab4d137b4bd64d8a8f14
    Reviewed-on: https://chromium-review.googlesource.com/1062511
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559309}

--- chrome/browser/ui/views/media_router/web_contents_display_observer_view.cc
+++ chrome/browser/ui/views/media_router/web_contents_display_observer_view.cc
@@ -61,6 +61,8 @@ void WebContentsDisplayObserverView::OnBrowserSetLastActive(Browser* browser) {
 }
 
 void WebContentsDisplayObserverView::OnWidgetClosing(views::Widget* widget) {
+  if (widget_)
+    widget_->RemoveObserver(this);
   widget_ = nullptr;
 }
 
