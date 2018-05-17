45273f502287b89d61f2d650c996be01de7987d2
liberato@chromium.org <liberato@chromium.org>
2018-05-17 21:05:23 +0000

    Skip opacity check for underlays on Android.
    We cannot cancel an overlay on android since there is no fallback.
    Also, the resource's idea of opacity is unrelated to what the
    overlay actually contains; it's just a dummy resource.
    
    This CL skips the opacity check on Android.
    
    Bug: 842931
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel
    Change-Id: I8223ca35f78548e3150af05efa715fdd801e115e
    Reviewed-on: https://chromium-review.googlesource.com/1062017
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559675}

--- components/viz/service/display/overlay_strategy_underlay.cc
+++ components/viz/service/display/overlay_strategy_underlay.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/viz/service/display/overlay_strategy_underlay.h"
 
+#include "build/build_config.h"
 #include "components/viz/common/quads/draw_quad.h"
 #include "components/viz/common/quads/solid_color_draw_quad.h"
 #include "components/viz/service/display/overlay_candidate_validator.h"
@@ -12,8 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace viz {
 
 OverlayStrategyUnderlay::OverlayStrategyUnderlay(
-    OverlayCandidateValidator* capability_checker)
-    : capability_checker_(capability_checker) {
+    OverlayCandidateValidator* capability_checker,
+    OpaqueMode opaque_mode)
+    : capability_checker_(capability_checker), opaque_mode_(opaque_mode) {
   DCHECK(capability_checker);
 }
 
@@ -30,7 +32,8 @@ bool OverlayStrategyUnderlay::Attempt(
     cc::OverlayCandidate candidate;
     if (!cc::OverlayCandidate::FromDrawQuad(
             resource_provider, output_color_matrix, *it, &candidate) ||
-      !candidate.is_opaque) {
+        (opaque_mode_ == OpaqueMode::RequireOpaqueCandidates &&
+         !candidate.is_opaque)) {
       continue;
     }
 
