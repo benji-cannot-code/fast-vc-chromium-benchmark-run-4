d576d50369ea9dc561a6064ef6eadfbf522913aa
Christopher Lam <calamity@chromium.org>
2018-05-17 09:07:55 +0000

    Convert remaining non-CrOS Closure Compiles to GN.
    See go/closure-compile-gn for details.
    
    This CL converts ntp4 and offline_pages to GN Closure compilation. It
    also deletes the webstore_widget GYP file which is no longer used.
    
    A local closure compile can be run by setting closure_compile = true in
    args.gn, and then running ninja -C out/Default webui_closure_compile.
    A more localized compile may look like:
    ninja -C out/Default chrome/browser/resources/ntp4:closure_compile
    
    TBR=fukino@chromium.org
    
    Bug: 632206
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Id6f4c248eaf19a63084e1734bc987b5f8a7c11fc
    Reviewed-on: https://chromium-review.googlesource.com/1049209
    Reviewed-by: calamity <calamity@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Scott Graham <scottmg@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Commit-Queue: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559464}

--- chrome/browser/resources/ntp4/apps_page.js
+++ chrome/browser/resources/ntp4/apps_page.js
@@ -570,7 +570,7 @@ cr.define('ntp', function() {
   /**
    * Creates a new AppsPage object.
    * @constructor
-   * @extends {TilePage}
+   * @extends {ntp.TilePage}
    */
   function AppsPage() {
     var el = new TilePage(appsPageGridValues);
