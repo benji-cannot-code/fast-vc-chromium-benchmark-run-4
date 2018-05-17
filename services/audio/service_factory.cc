14c963b95834a990da20ebd21dfd91a5c81d1e31
Olga Sharonova <olka@chromium.org>
2018-05-17 12:34:06 +0000

    Retrieve Audio service teardown timeout from experiment parameters.
    Bug: 832666
    Change-Id: I2f8104042ba7320998b306f7fdda05af9cf67086
    Reviewed-on: https://chromium-review.googlesource.com/1064090
    Reviewed-by: Max Morin <maxmorin@chromium.org>
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559500}

--- services/audio/service_factory.cc
+++ services/audio/service_factory.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/command_line.h"
+#include "base/metrics/field_trial_params.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/time/time.h"
 #include "media/audio/audio_manager.h"
@@ -21,20 +22,35 @@ namespace audio {
 
 namespace {
 
-base::TimeDelta GetQuitTimeout() {
-  constexpr base::TimeDelta kDefaultTimeout = base::TimeDelta::FromSeconds(10);
+base::Optional<base::TimeDelta> GetExperimentalQuitTimeout() {
+  std::string timeout_str(
+      base::GetFieldTrialParamValue("AudioService", "teardown_timeout_s"));
+  int timeout_s = 0;
+  if (!base::StringToInt(timeout_str, &timeout_s) || timeout_s < 0)
+    return base::nullopt;  // Ill-formed value provided, fall back to default.
+  return base::TimeDelta::FromSeconds(timeout_s);
+}
 
+base::Optional<base::TimeDelta> GetCommandLineQuitTimeout() {
   const base::CommandLine* cmd_line = base::CommandLine::ForCurrentProcess();
   if (!cmd_line->HasSwitch(switches::kAudioServiceQuitTimeoutMs))
-    return kDefaultTimeout;
-
+    return base::nullopt;
   std::string timeout_str(
       cmd_line->GetSwitchValueASCII(switches::kAudioServiceQuitTimeoutMs));
   int timeout_ms = 0;
   if (!base::StringToInt(timeout_str, &timeout_ms) || timeout_ms < 0)
-    return kDefaultTimeout;  // Ill-formed value provided, fall back to default.
-
+    return base::nullopt;  // Ill-formed value provided, fall back to default.
   return base::TimeDelta::FromMilliseconds(timeout_ms);
+}
+
+base::TimeDelta GetQuitTimeout() {
+  if (auto timeout = GetCommandLineQuitTimeout())
+    return *timeout;
+
+  if (auto timeout = GetExperimentalQuitTimeout())
+    return *timeout;
+
+  return base::TimeDelta();
 }
 
 }  // namespace
