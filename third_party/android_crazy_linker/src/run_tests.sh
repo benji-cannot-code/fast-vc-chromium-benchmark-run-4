a0c42cbcd5a42bace860b89c7f21e2090e992af6
David 'Digit' Turner <digit@google.com>
2018-05-17 14:58:21 +0000

    android: crazy-linker: Support recursive dlopen() operations.
    This CL turns back the global crazy linker mutex to recursive,
    in order to support cases where a library's ELF initializer
    calls dlopen() recursively.
    
    This prevents crashes when Chrome is loaded on some devices,
    due to the fact that a system library is loaded (by mistake)
    through the crazy linker, and that its code actually dlopen()
    another system library that has such a rare ELF initializer
    (see bug below for details).
    
    Another CL will be used to better select which libraries
    are loaded with the system linker though.
    
    + Add a regression tests where three recursive dlopens()
      are performed (see test_dl_wrappers_recursive.cpp and
      related sources).
    
    + run_tests.sh: Make regression tests less noisy by default,
      and add a ---verbose option to get back to the old chatty
      output during development.
    
    BUG=843804
    R=pasko@chromium.org, lizeb@chromium.org, agrieve@chromium.org
    
    Change-Id: Ife05f4bf314ca168b06402a144de96f1c946e546
    Reviewed-on: https://chromium-review.googlesource.com/1064112
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Reviewed-by: agrieve <agrieve@chromium.org>
    Commit-Queue: David Turner <digit@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559535}

--- third_party/android_crazy_linker/src/run_tests.sh
+++ third_party/android_crazy_linker/src/run_tests.sh
@@ -17,6 +17,14 @@ panic () {
   exit 1
 }
 
+run () {
+  if [ "$VERBOSE" -ge 1 ]; then
+    "$@"
+  else
+    "$@" 2>&1 >/dev/null
+  fi
+}
+
 # Run a command through adb shell, strip the extra \r from the output
 # and return the correct status code to detect failures. This assumes
 # that the adb shell command prints a final \n to stdout.
@@ -55,6 +63,7 @@ adb_shell () {
 }
 
 # Command-line parsing.
+VERBOSE=0
 DO_HELP=
 DO_TEST=
 for OPT; do
@@ -64,6 +73,9 @@ for OPT; do
       ;;
     --help|-?)
       DO_HELP=true
+      ;;
+    --verbose)
+      VERBOSE=$(( $VERBOSE + 1 ))
       ;;
     -*)
       panic "Invalid option $OPT, see --help."
@@ -98,6 +110,7 @@ Possible options:
 
    --help|-?           Print this message.
    --output-dir=<dir>  Manually set the Chromium output directory.
+   --verbose           Increment verbosity.
 
 EOF
   exit 0
@@ -133,6 +146,8 @@ libcrazy_linker_tests_libfoo_with_relro.so \
 libcrazy_linker_tests_libfoo_with_static_constructor.so \
 libcrazy_linker_tests_libfoo2.so \
 libcrazy_linker_tests_libzoo.so \
+libcrazy_linker_tests_libzoo_dlopen_in_initializer.so \
+libcrazy_linker_tests_libzoo_dlopen_in_initializer_inner.so \
 libcrazy_linker_tests_libzoo_with_dlopen_handle.so \
 "
 
@@ -142,6 +157,7 @@ TEST_FILES="\
 crazy_linker_bench_load_library \
 crazy_linker_test_constructors_destructors \
 crazy_linker_test_dl_wrappers \
+crazy_linker_test_dl_wrappers_recursive \
 crazy_linker_test_dl_wrappers_with_system_handle \
 crazy_linker_test_dl_wrappers_valid_handles \
 crazy_linker_test_load_library \
@@ -166,14 +182,14 @@ if [ -n "$MISSING_FILES" ]; then
   panic "Please rebuild all crazy linker tests before using this script"
 fi
 
-(cd $CHROMIUM_OUTPUT_DIR && adb push $ALL_FILES $RUN_DIR/) ||
+(cd $CHROMIUM_OUTPUT_DIR && run adb push $ALL_FILES $RUN_DIR/) ||
     panic "Could not copy files to Android device"
 
 # Run a single test on the device.
 run_test () {
   local TEST_NAME=$1
   shift
-  adb_shell LD_LIBRARY_PATH=$RUN_DIR $RUN_DIR/$TEST_NAME "$@"
+  run adb_shell LD_LIBRARY_PATH=$RUN_DIR $RUN_DIR/$TEST_NAME "$@"
 }
 
 if [ -n "$DO_TEST" ]; then
