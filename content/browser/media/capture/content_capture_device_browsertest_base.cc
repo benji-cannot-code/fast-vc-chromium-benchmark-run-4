a11c64a45050cbf0e5ab86f10ea1b535de2760ba
Yuri Wiitala <miu@chromium.org>
2018-05-17 00:49:48 +0000

    New Desktop/Browser Window screen capture impl, based on VIZ services.
    Adds content::AuraWindowVideoCaptureDevice, which provides screen
    capture of aura::Windows that have compositor frame sinks associated
    with them. Then, for Chrome OS browser window capture, where a CFS is
    not available, use a fall-back window capturer impl instead. The
    intention is for the LameWindowCapturerChromeOS to be a temporary
    solution until more work is complete on services/ui/ws (the new Window
    Service).
    
    Note: This change does not activate the new impl yet. A follow-up
    change will switch from the legacy impl to the new impl.
    
    Bug: 806366
    Change-Id: Ia6aa81e4addde603b120a6ab61c5e35a0041b418
    Reviewed-on: https://chromium-review.googlesource.com/1006366
    Commit-Queue: Yuri Wiitala <miu@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Adam Parker <amp@chromium.org>
    Reviewed-by: Xiangjun Zhang <xjz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559371}

--- content/browser/media/capture/content_capture_device_browsertest_base.cc
+++ content/browser/media/capture/content_capture_device_browsertest_base.cc
@@ -39,7 +39,7 @@ ContentCaptureDeviceBrowserTestBase::~ContentCaptureDeviceBrowserTestBase() =
     default;
 
 void ContentCaptureDeviceBrowserTestBase::ChangePageContentColor(
-    std::string css_color_hex) {
+    SkColor color) {
   // See the HandleRequest() method for the original documents being modified
   // here.
   std::string script;
@@ -52,7 +52,9 @@ void ContentCaptureDeviceBrowserTestBase::ChangePageContentColor(
   } else {
     script = "document.body.style.backgroundColor = '#123456';";
   }
-  script.replace(script.find("123456"), 6, css_color_hex);
+  script.replace(script.find("123456"), 6,
+                 base::StringPrintf("%02x%02x%02x", SkColorGetR(color),
+                                    SkColorGetG(color), SkColorGetB(color)));
   CHECK(ExecuteScript(shell()->web_contents(), script));
 }
 
