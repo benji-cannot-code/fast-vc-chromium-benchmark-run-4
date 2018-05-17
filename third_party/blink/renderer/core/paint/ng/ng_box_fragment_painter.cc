261d8c7001ec69ad393b0a84ad6f83d99f8e8d9b
Koji Ishii <kojii@chromium.org>
2018-05-17 18:07:25 +0000

    [LayoutNG] Apply clipping to atomic inlines when needed
    In the current paint system, ObjectPainter::PaintAllPhasesAtomically
    calls LayoutObject::Paint, which creates a painter on each phase.
    This goes through BlockPainter::Paint, which instantiate BoxClipper.
    
    Since NGBoxFragmentPainter::PaintAllPhasesAtomically calls
    PaintObject directly, it needs to instantiate NGBoxClipper.
    
    I'm not confident it is the right thing to instantiate NGBoxClipper
    here yet, but at least for now, only PaintAtomicInline calls this
    function, and NGBoxClipper in NGBoxFragmentPainter::Paint doesn't
    work due to the different requirements for atomic inlines. We can
    revisit this when we have better idea how to handle atomic inlines
    in NG painters, or when other cases need to call
    PaintAllPhasesAtomically.
    
    Bug: 714962
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_layout_ng;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I3db08a9ca1b5a8dc8b37fd42c3d9f55d3832cdb0
    Reviewed-on: https://chromium-review.googlesource.com/1062666
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Aleks Totic <atotic@chromium.org>
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559605}

--- third_party/blink/renderer/core/paint/ng/ng_box_fragment_painter.cc
+++ third_party/blink/renderer/core/paint/ng/ng_box_fragment_painter.cc
@@ -511,6 +511,9 @@ void NGBoxFragmentPainter::PaintAllPhasesAtomically(
     return;
 
   PaintInfo info(paint_info);
+
+  NGBoxClipper box_clipper(box_fragment_, info);
+
   info.phase = PaintPhase::kBlockBackground;
   PaintObject(info, paint_offset);
 
