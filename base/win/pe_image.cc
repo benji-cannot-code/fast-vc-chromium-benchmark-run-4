8c5b027b2b24abdda40ea0a4805e164b722d65de
Etienne Bergeron <etienneb@chromium.org>
2018-05-17 16:41:40 +0000

    Add pdb-path parsing to GetDebugID
    This CL is adding the field pdb-path to the GetDebugID signature.
    
      1) Make optional GetDebugID fields (they can be null)
      2) Add a pdb-file parameter
      3) Add the pdb-path to chrome-trace (vmmaps dumps)
    
    Each PDB has a unique DebugID and a PDB-Path used to retrieve the debug
    information. The crash servers required both field to fetch the breakpad
    debug file.
    
    Change-Id: I739c4c2dbdb6f38e5c4a86b982dde296db86aa4c
    Reviewed-on: https://chromium-review.googlesource.com/1061591
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: Primiano Tucci <primiano@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559565}

--- base/win/pe_image.cc
+++ base/win/pe_image.cc
@@ -153,11 +153,7 @@ PIMAGE_SECTION_HEADER PEImage::GetImageSectionHeaderByName(
   return ret;
 }
 
-bool PEImage::GetDebugId(LPGUID guid, LPDWORD age) const {
-  if (NULL == guid || NULL == age) {
-    return false;
-  }
-
+bool PEImage::GetDebugId(LPGUID guid, LPDWORD age, LPCSTR* pdb_filename) const {
   DWORD debug_directory_size =
       GetImageDirectoryEntrySize(IMAGE_DIRECTORY_ENTRY_DEBUG);
   PIMAGE_DEBUG_DIRECTORY debug_directory =
@@ -175,8 +171,13 @@ bool PEImage::GetDebugId(LPGUID guid, LPDWORD age) const {
         // Unsupported PdbInfo signature
         return false;
       }
-      *guid = pdb_info->Guid;
-      *age = pdb_info->Age;
+
+      if (guid)
+        *guid = pdb_info->Guid;
+      if (age)
+        *age = pdb_info->Age;
+      if (pdb_filename)
+        *pdb_filename = pdb_info->PdbFileName;
       return true;
     }
   }
