a260f6b2a3657d4cae0474cd9c8cadceb039560b
Mark Pilgrim <pilgrim@chromium.org>
2018-05-17 18:55:23 +0000

    Migrate ByteStreamReader and ByteStreamWriter to BindRepeating
    Bug: 714018
    Change-Id: I0e1a9f6b8646b414664dbb7a7a7254f0135a166e
    Reviewed-on: https://chromium-review.googlesource.com/1061814
    Commit-Queue: Mark Pilgrim <pilgrim@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559628}

--- content/browser/byte_stream_unittest.cc
+++ content/browser/byte_stream_unittest.cc
@@ -332,7 +332,7 @@ TEST_F(ByteStreamTest, ByteStream_SinkCallback) {
   // Setup callback
   int num_callbacks = 0;
   byte_stream_output->RegisterCallback(
-      base::Bind(CountCallbacks, &num_callbacks));
+      base::BindRepeating(CountCallbacks, &num_callbacks));
 
   EXPECT_TRUE(Write(byte_stream_input.get(), 4000));
   base::RunLoop().RunUntilIdle();
@@ -384,7 +384,7 @@ TEST_F(ByteStreamTest, ByteStream_SourceCallback) {
   // Add data.
   int num_callbacks = 0;
   byte_stream_input->RegisterCallback(
-      base::Bind(CountCallbacks, &num_callbacks));
+      base::BindRepeating(CountCallbacks, &num_callbacks));
   EXPECT_TRUE(Write(byte_stream_input.get(), 2000));
   EXPECT_TRUE(Write(byte_stream_input.get(), 2001));
   EXPECT_FALSE(Write(byte_stream_input.get(), 6000));
@@ -438,7 +438,7 @@ TEST_F(ByteStreamTest, ByteStream_SinkInterrupt) {
   // Record initial state.
   int num_callbacks = 0;
   byte_stream_output->RegisterCallback(
-      base::Bind(CountCallbacks, &num_callbacks));
+      base::BindRepeating(CountCallbacks, &num_callbacks));
 
   // Add data, and pass it across.
   EXPECT_TRUE(Write(byte_stream_input.get(), 4000));
@@ -452,7 +452,7 @@ TEST_F(ByteStreamTest, ByteStream_SinkInterrupt) {
   // (simulates race with post task).
   int num_alt_callbacks = 0;
   byte_stream_output->RegisterCallback(
-      base::Bind(CountCallbacks, &num_alt_callbacks));
+      base::BindRepeating(CountCallbacks, &num_alt_callbacks));
   task_runner->RunUntilIdle();
   EXPECT_EQ(0, num_callbacks);
   EXPECT_EQ(1, num_alt_callbacks);
@@ -484,7 +484,7 @@ TEST_F(ByteStreamTest, ByteStream_SourceInterrupt) {
   // Setup state for test.
   int num_callbacks = 0;
   byte_stream_input->RegisterCallback(
-      base::Bind(CountCallbacks, &num_callbacks));
+      base::BindRepeating(CountCallbacks, &num_callbacks));
   EXPECT_TRUE(Write(byte_stream_input.get(), 2000));
   EXPECT_TRUE(Write(byte_stream_input.get(), 2001));
   EXPECT_FALSE(Write(byte_stream_input.get(), 6000));
@@ -504,7 +504,7 @@ TEST_F(ByteStreamTest, ByteStream_SourceInterrupt) {
   // Which should do the right thing when it's run.
   int num_alt_callbacks = 0;
   byte_stream_input->RegisterCallback(
-      base::Bind(CountCallbacks, &num_alt_callbacks));
+      base::BindRepeating(CountCallbacks, &num_alt_callbacks));
   task_runner->RunUntilIdle();
   EXPECT_EQ(0, num_callbacks);
   EXPECT_EQ(1, num_alt_callbacks);
@@ -533,7 +533,7 @@ TEST_F(ByteStreamTest, ByteStream_ZeroCallback) {
   // Record initial state.
   int num_callbacks = 0;
   byte_stream_output->RegisterCallback(
-      base::Bind(CountCallbacks, &num_callbacks));
+      base::BindRepeating(CountCallbacks, &num_callbacks));
 
   // Immediately close the stream.
   byte_stream_input->Close(0);
