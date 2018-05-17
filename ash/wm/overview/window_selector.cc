f6182e8835cd502cd9dc6fa6a19ef460478078b3
Sammie Quon <sammiequon@google.com>
2018-05-17 15:41:51 +0000

    overview: Shield widget bounds now correct after rotation.
    Shield widget gets updated in PositionWindows, which has an early out if
    no windows. Move update before early return.
    
    Also fix a big with the no recents message not disappearing if an window
    item is added back to the grid via split view.
    
    Test: manual
    Bug: 842395, 842394
    Change-Id: I52dcdf9c012edc32c7d47bbdd978b6f59376a289
    Reviewed-on: https://chromium-review.googlesource.com/1063171
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559545}

--- ash/wm/overview/window_selector.cc
+++ ash/wm/overview/window_selector.cc
@@ -535,11 +535,6 @@ void WindowSelector::WindowClosing(WindowSelectorItem* window) {
   grid_list_[selected_grid_index_]->WindowClosing(window);
 }
 
-void WindowSelector::SetBoundsForWindowGridsInScreen(const gfx::Rect& bounds) {
-  for (std::unique_ptr<WindowGrid>& grid : grid_list_)
-    grid->SetBoundsAndUpdatePositions(bounds);
-}
-
 void WindowSelector::SetBoundsForWindowGridsInScreenIgnoringWindow(
     const gfx::Rect& bounds,
     WindowSelectorItem* ignored_item) {
@@ -1006,7 +1001,7 @@ void WindowSelector::Move(Direction direction, bool animate) {
 void WindowSelector::OnDisplayBoundsChanged() {
   // Re-calculate the bounds for the window grids and position all the windows.
   for (std::unique_ptr<WindowGrid>& grid : grid_list_) {
-    SetBoundsForWindowGridsInScreen(
+    grid->SetBoundsAndUpdatePositions(
         GetGridBoundsInScreen(const_cast<aura::Window*>(grid->root_window())));
   }
   PositionWindows(/*animate=*/false);
