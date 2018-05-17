2a617b43e93535fcbea2cd3a67d19ead1a9137a8
Matt Menke <mmenke@chromium.org>
2018-05-17 22:33:51 +0000

    Add an experimental "feature" to disable the media cache.
    When enabled, a Profile's media URLRequestContext will just use the
    same disk cache as the main one. Also affect App Media
    URLRequestContexts.
    
    Bug: 789657
    Change-Id: I4150324ad84c1679495bd512dd0231edb30c733b
    Reviewed-on: https://chromium-review.googlesource.com/1064858
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559712}

--- chrome/browser/profiles/profile_impl_io_data.cc
+++ chrome/browser/profiles/profile_impl_io_data.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/command_line.h"
+#include "base/feature_list.h"
 #include "base/logging.h"
 #include "base/macros.h"
 #include "base/metrics/field_trial.h"
@@ -37,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/previews/previews_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/chrome_constants.h"
+#include "chrome/common/chrome_features.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
@@ -675,6 +677,8 @@ net::URLRequestContext* ProfileImplIOData::InitializeMediaRequestContext(
   // Copy most state from the original context.
   MediaRequestContext* context = new MediaRequestContext(name);
   context->CopyFrom(original_context);
+  if (base::FeatureList::IsEnabled(features::kUseSameCacheForMedia))
+    return context;
 
   // For in-memory context, return immediately after creating the new
   // context before attaching a separate cache. It is important to return
