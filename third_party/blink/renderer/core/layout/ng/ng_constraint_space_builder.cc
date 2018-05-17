fb40545fdfc844ef0d8a83e5c79730dfc3f1f8cf
Christian Biesinger <cbiesinger@chromium.org>
2018-05-17 22:56:08 +0000

    [layoutng] Don't copy data to legacy for intermediate layouts
    In some places, notably the min/max content calculation, we produce layout
    results that are just for intermediate use, they are not final fragments
    for a box. We should avoid creating paint fragments for them or copying
    data to the legacy tree.
    
    This becomes an issue in practice with fragment caching, because we may never
    reach the code to produce the final fragment and fix up the paint fragment,
    etc.
    
    R=kojii@chromium.org,eae@chromium.org
    
    Bug: 635619
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: I012b599685a0f67060e6225c195e0e8205afad8e
    Reviewed-on: https://chromium-review.googlesource.com/1064855
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559718}

--- third_party/blink/renderer/core/layout/ng/ng_constraint_space_builder.cc
+++ third_party/blink/renderer/core/layout/ng/ng_constraint_space_builder.cc
@@ -14,6 +14,7 @@ NGConstraintSpaceBuilder::NGConstraintSpaceBuilder(
     : NGConstraintSpaceBuilder(parent_space.GetWritingMode(),
                                parent_space.InitialContainingBlockSize()) {
   parent_percentage_resolution_size_ = parent_space.PercentageResolutionSize();
+  is_intermediate_layout_ = parent_space.IsIntermediateLayout();
 }
 
 NGConstraintSpaceBuilder::NGConstraintSpaceBuilder(WritingMode writing_mode,
@@ -26,6 +27,7 @@ NGConstraintSpaceBuilder::NGConstraintSpaceBuilder(WritingMode writing_mode,
       is_fixed_size_block_(false),
       fixed_size_block_is_definite_(true),
       is_shrink_to_fit_(false),
+      is_intermediate_layout_(false),
       fragmentation_type_(kFragmentNone),
       separate_leading_fragmentainer_margins_(false),
       is_new_fc_(false),
@@ -104,6 +106,12 @@ NGConstraintSpaceBuilder& NGConstraintSpaceBuilder::SetFixedSizeBlockIsDefinite(
 NGConstraintSpaceBuilder& NGConstraintSpaceBuilder::SetIsShrinkToFit(
     bool shrink_to_fit) {
   is_shrink_to_fit_ = shrink_to_fit;
+  return *this;
+}
+
+NGConstraintSpaceBuilder& NGConstraintSpaceBuilder::SetIsIntermediateLayout(
+    bool is_intermediate_layout) {
+  is_intermediate_layout_ = is_intermediate_layout;
   return *this;
 }
 
@@ -213,6 +221,7 @@ scoped_refptr<NGConstraintSpace> NGConstraintSpaceBuilder::ToConstraintSpace(
         initial_containing_block_size_, fragmentainer_block_size_,
         fragmentainer_space_at_bfc_start_, is_fixed_size_inline_,
         is_fixed_size_block_, fixed_size_block_is_definite_, is_shrink_to_fit_,
+        is_intermediate_layout_,
         static_cast<NGFragmentationType>(fragmentation_type_),
         separate_leading_fragmentainer_margins_, is_new_fc_, is_anonymous_,
         use_first_line_style_, adjoining_floats_, margin_strut, bfc_offset,
@@ -225,7 +234,7 @@ scoped_refptr<NGConstraintSpace> NGConstraintSpaceBuilder::ToConstraintSpace(
       parent_percentage_resolution_size.inline_size,
       initial_containing_block_size_, fragmentainer_block_size_,
       fragmentainer_space_at_bfc_start_, is_fixed_size_block_,
-      is_fixed_size_inline_, true, is_shrink_to_fit_,
+      is_fixed_size_inline_, true, is_shrink_to_fit_, is_intermediate_layout_,
       static_cast<NGFragmentationType>(fragmentation_type_),
       separate_leading_fragmentainer_margins_, is_new_fc_, is_anonymous_,
       use_first_line_style_, adjoining_floats_, margin_strut, bfc_offset,
