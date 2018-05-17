9c2771deb07ca2fcbfdeba0be211bbb0f3fa366a
David Black <dmblack@google.com>
2018-05-17 18:42:23 +0000

    Pull SuggestionContainerView out of AssistantBubbleView.
    In anticipation of motion spec complexity, we want to pull some of the
    child view classes in AssistantBubbleView out of the anonymous namespace
    and into their own files. This should simplify AssistantBubbleView such
    that it can be concerned only with choreography between the various
    UI elements.
    
    Follow up CL will migrate the other child views to proper class files.
    
    This will also help for unit testing.
    
    Bug: b:79875532
    Change-Id: I7ff54772ba010fa7c5b074fb3f22264f46412c49
    Reviewed-on: https://chromium-review.googlesource.com/1062787
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/master@{#559624}

--- ash/assistant/ui/assistant_bubble_view.h
+++ ash/assistant/ui/assistant_bubble_view.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/assistant/model/assistant_interaction_model_observer.h"
 #include "base/macros.h"
-#include "ui/app_list/views/suggestion_chip_view.h"
 #include "ui/views/view.h"
 
 namespace views {
@@ -27,19 +26,16 @@ class AssistantController;
 class AssistantTextElement;
 class AssistantUiElement;
 class DialogPlate;
+class SuggestionContainerView;
 
 namespace {
 class InteractionContainer;
-class SuggestionsContainer;
 class UiElementContainer;
 }  // namespace
 
 class AssistantBubbleView : public views::View,
-                            public AssistantInteractionModelObserver,
-                            public app_list::SuggestionChipListener {
+                            public AssistantInteractionModelObserver {
  public:
-  using AssistantSuggestion = chromeos::assistant::mojom::AssistantSuggestion;
-
   explicit AssistantBubbleView(AssistantController* assistant_controller);
   ~AssistantBubbleView() override;
 
@@ -55,13 +51,6 @@ class AssistantBubbleView : public views::View,
   void OnUiElementsCleared() override;
   void OnQueryChanged(const AssistantQuery& query) override;
   void OnQueryCleared() override;
-  void OnSuggestionsAdded(
-      const std::map<int, AssistantSuggestion*>& suggestions) override;
-  void OnSuggestionsCleared() override;
-
-  // app_list::SuggestionChipListener:
-  void OnSuggestionChipPressed(
-      app_list::SuggestionChipView* suggestion_chip_view) override;
 
  private:
   void InitLayout();
@@ -83,7 +72,7 @@ class AssistantBubbleView : public views::View,
   views::View* caption_bar_;                     // Owned by view hierarchy.
   InteractionContainer* interaction_container_;  // Owned by view hierarchy.
   UiElementContainer* ui_element_container_;     // Owned by view hierarchy.
-  SuggestionsContainer* suggestions_container_;  // Owned by view hierarchy.
+  SuggestionContainerView* suggestions_container_;  // Owned by view hierarchy.
   DialogPlate* dialog_plate_;                    // Owned by view hierarchy.
 
   views::BoxLayout* layout_manager_ = nullptr;  // Owned by view hierarchy.
