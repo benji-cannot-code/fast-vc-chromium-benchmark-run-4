f189fac9427cea1b91df3f71fb3276755e46c59d
Olga Sharonova <olka@chromium.org>
2018-05-17 11:08:00 +0000

    Do not instantiate AudioManager when running Audio service out of process
    Do not instantiate AudioManager in BrowserMainLoop when running
    Audio service out of process.
    
    Audio service DD: https://docs.google.com/document/d/1s_Fd1WRDdpb5n6C2MSJjeC3fis6hULZwfKMeDd4K5tI/edit#
    
    Dry runs with out of process audio service are here:
    https://chromium-review.googlesource.com/c/chromium/src/+/1057807
    
    Bug: 834666
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: Ia957b6209007413faad575d2410bdbba3ded5728
    Reviewed-on: https://chromium-review.googlesource.com/1057624
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Sergey Volk <servolk@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559490}

--- content/public/browser/content_browser_client.cc
+++ content/public/browser/content_browser_client.cc
@@ -177,6 +177,10 @@ std::unique_ptr<media::AudioManager> ContentBrowserClient::CreateAudioManager(
   return nullptr;
 }
 
+bool ContentBrowserClient::OverridesAudioManager() {
+  return false;
+}
+
 std::unique_ptr<media::CdmFactory> ContentBrowserClient::CreateCdmFactory() {
   return nullptr;
 }
--- media/audio/audio_thread_impl.cc
+++ media/audio/audio_thread_impl.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/audio/audio_thread_impl.h"
 
 #include "base/threading/thread_task_runner_handle.h"
+#include "build/build_config.h"
 
 namespace media {
 
