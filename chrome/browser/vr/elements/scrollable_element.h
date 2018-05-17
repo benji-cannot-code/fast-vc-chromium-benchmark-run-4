88583eac13a0306f216f666d2744c3c1f6b8299e
Aldo Culquicondor <acondor@chromium.org>
2018-05-17 19:10:34 +0000

    VR: Always update world transforms
    UpdateWorldTransform is already optimized to skip invisible subtrees and
    to skip recalculating local transforms when they haven't changed. This is
    not a final fix for dirtiness handling.
    
    Bug: 843326
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_vr
    Change-Id: I70a45d1d602474f8b8439f16f9680186b08f8679
    Reviewed-on: https://chromium-review.googlesource.com/1064451
    Reviewed-by: Christopher Grant <cjgrant@chromium.org>
    Commit-Queue: Aldo Culquicondor <acondor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559633}

--- chrome/browser/vr/elements/scrollable_element.h
+++ chrome/browser/vr/elements/scrollable_element.h
@@ -33,7 +33,6 @@ class ScrollableElement : public UiElement {
   // UiElement overrides.
   gfx::RectF ComputeContributingChildrenBounds() final;
   void LayOutNonContributingChildren() final;
-  bool OnBeginFrame(const gfx::Transform&) final;
 
   void AddScrollingChild(std::unique_ptr<UiElement> child);
 
@@ -64,7 +63,6 @@ class ScrollableElement : public UiElement {
   gfx::SizeF inner_size_;
 
   float scroll_offset_ = 0.0f;
-  float last_scroll_offset_ = 0.0f;
 
   Transition cached_transition_;
 
