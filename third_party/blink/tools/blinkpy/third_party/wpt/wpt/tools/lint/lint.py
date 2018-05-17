a934312523873091a44edcab7388680cc18a96a2
Philip Jägenstedt <foolip@chromium.org>
2018-05-17 12:42:08 +0000

    Roll in new version of WPT tools
    https://github.com/w3c/web-platform-tests/pull/10985 changed lint.py
    and wpt-importer is now failing because of it:
    https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/17652
    
    chromium.patch did not apply cleanly, so the changes had to be rebased
    on the new wpt version.	This was much easier within the	wpt repo
    itself, so the patch is	changed	to apply within	the repo.
    
    Bug: 843799, 843945
    Change-Id: I1ed9895b2461243c38fea552706960a3b99db2b6
    Reviewed-on: https://chromium-review.googlesource.com/1061458
    Commit-Queue: Philip Jägenstedt <foolip@chromium.org>
    Reviewed-by: Robert Ma <robertma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559502}

--- third_party/blink/tools/blinkpy/third_party/wpt/wpt/tools/lint/lint.py
+++ third_party/blink/tools/blinkpy/third_party/wpt/wpt/tools/lint/lint.py
@@ -51,7 +51,7 @@ setup_logging()
 
 
 ERROR_MSG = """You must fix all errors; for details on how to fix them, see
-http://web-platform-tests.org/writing-tests/lint-tool.html
+https://web-platform-tests.org/writing-tests/lint-tool.html
 
 However, instead of fixing a particular error, it's sometimes
 OK to add a line to the lint.whitelist file in the root of the
@@ -444,7 +444,7 @@ def check_parsed(repo_root, path, f):
             not source_file.spec_links):
             return [("MISSING-LINK", "Testcase file must have a link to a spec", path, None)]
 
-    if source_file.name_is_non_test or source_file.name_is_manual:
+    if source_file.name_is_non_test:
         return []
 
     if source_file.markup_type is None:
@@ -454,10 +454,10 @@ def check_parsed(repo_root, path, f):
         return [("PARSE-FAILED", "Unable to parse file", path, None)]
 
     if source_file.type == "manual" and not source_file.name_is_manual:
-        return [("CONTENT-MANUAL", "Manual test whose filename doesn't end in '-manual'", path, None)]
+        errors.append(("CONTENT-MANUAL", "Manual test whose filename doesn't end in '-manual'", path, None))
 
     if source_file.type == "visual" and not source_file.name_is_visual:
-        return [("CONTENT-VISUAL", "Visual test whose filename doesn't end in '-visual'", path, None)]
+        errors.append(("CONTENT-VISUAL", "Visual test whose filename doesn't end in '-visual'", path, None))
 
     for reftest_node in source_file.reftest_nodes:
         href = reftest_node.attrib.get("href", "").strip(space_chars)
@@ -667,6 +667,8 @@ def check_script_metadata(repo_root, path, f):
                     errors.append(("UNKNOWN-TIMEOUT-METADATA", "Unexpected value for timeout metadata", path, idx + 1))
             elif key == b"script":
                 pass
+            elif key == b"variant":
+                pass
             else:
                 errors.append(("UNKNOWN-METADATA", "Unexpected kind of metadata", path, idx + 1))
         else:
@@ -814,8 +816,6 @@ def create_parser():
                         help="Output machine-readable JSON format")
     parser.add_argument("--markdown", action="store_true",
                         help="Output markdown")
-    parser.add_argument("--css-mode", action="store_true",
-                        help="Run CSS testsuite specific lints")
     parser.add_argument("--repo-root", help="The WPT directory. Use this"
                         "option if the lint script exists outside the repository")
     parser.add_argument("--ignore-glob", help="Additional file glob to ignore.")
