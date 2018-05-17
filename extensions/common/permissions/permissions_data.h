ab37bb7cccfcb243fd04b9995cad1c6192901f4a
Devlin Cronin <rdevlin.cronin@chromium.org>
2018-05-17 21:48:50 +0000

    [Extensions] Finish factoring Extension out of PermissionsData
    Fully factor the Extension class out of PermissionsData, breaking the
    dependency cycle. Instead, pass in the necessary members directly to the
    constructor.
    
    Also clean up other unnecessary includes.
    
    Bug: 842270
    Change-Id: Id73c549ff39b51ef4a457612853b5ea0d62aab69
    Reviewed-on: https://chromium-review.googlesource.com/1064873
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559692}

--- extensions/common/permissions/permissions_data.h
+++ extensions/common/permissions/permissions_data.h
@@ -9,11 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <memory>
 #include <string>
-#include <vector>
 
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
-#include "base/strings/string16.h"
 #include "base/synchronization/lock.h"
 #include "base/threading/thread_checker.h"
 #include "extensions/common/extension_id.h"
@@ -25,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class GURL;
 
 namespace extensions {
-class Extension;
 class URLPatternSet;
 
 // A container for the permissions state of an extension, including active,
@@ -68,7 +65,10 @@ class PermissionsData {
 
   static void SetPolicyDelegate(PolicyDelegate* delegate);
 
-  explicit PermissionsData(const Extension* extension);
+  PermissionsData(const ExtensionId& extension_id,
+                  Manifest::Type manifest_type,
+                  Manifest::Location location,
+                  std::unique_ptr<const PermissionSet> initial_permissions);
   virtual ~PermissionsData();
 
   // Returns true if the extension is a COMPONENT extension or is on the
