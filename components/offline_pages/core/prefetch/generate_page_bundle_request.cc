5f246e81f3f23204d6c6b177720a7aa6ffeb09d5
Dan Harrington <harringtond@chromium.org>
2018-05-17 17:36:08 +0000

    Reland "Add PrefetchItemFlow unittest"
    Add PrefetchItemFlow unittest
    
    This helps test the integration of tasks that make up the prefetch
    pipeline. I found that this coverage was lacking when making a change
    to a task.
    
    This CL also improves output of PrefetchItem and prefetch enums for tests.
    
    Note, this is a revert of e1156c6ba0e8e168eeeddce8c20f7677d23c0643. The only
    change is a swap of DCHECK -> CHECK in test_download_service.cc, which allows
    the temporary directory to be created in the release build.
    
    Bug: 843947
    Change-Id: Ia5e9d3455dd4ae85c75bd0ae4a7590f2011532d0
    Reviewed-on: https://chromium-review.googlesource.com/1064003
    Commit-Queue: Dan H <harringtond@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559592}

--- components/offline_pages/core/prefetch/generate_page_bundle_request.cc
+++ components/offline_pages/core/prefetch/generate_page_bundle_request.cc
@@ -69,4 +69,4 @@ void GeneratePageBundleRequest::OnCompleted(PrefetchRequestStatus status,
   callback_.Run(PrefetchRequestStatus::SUCCESS, operation_name, pages);
 }
 
-}  // offline_pages
+}  // namespace offline_pages
