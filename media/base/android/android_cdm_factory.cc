fdeab2a9aa4a3edfeef1b5f14d963acdb4aa6a75
Xiaohan Wang <xhwang@chromium.org>
2018-05-17 00:17:37 +0000

    media: Remove NOTREACHED() in AndroidCdmFactory
    Current we have a NOTREACHED() which essentially DCHECK that
    MediaDrmBridge::IsKeySystemSupported(key_system) must return true. This
    DCHECK is based on the fact that we called the same function when
    deciding whether the key system is supported [1]. However, that is
    called in the browser process and the DCHECK is in the GPU process.
    Also, the DCHECK could happen much later. Further, the implemenation of
    MediaDrmBridge::IsKeySystemSupported() actually calls into Java
    MediaDrm, where it's hard to assume what it'll return.
    
    This CL removes the NOTREACHED() so that if that happens we'll just
    fail. UMA is also added to track how often this happens in the field.
    
    [1] See CdmMessageFilterAndroid::OnQueryKeySystemSupport()
    
    Bug: 843784
    Change-Id: I6ce743033811e4d2bfbde75ccb715697e6205063
    Reviewed-on: https://chromium-review.googlesource.com/1062803
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559358}

--- media/base/android/android_cdm_factory.cc
+++ media/base/android/android_cdm_factory.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "base/logging.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/strings/string_number_conversions.h"
 #include "media/base/android/media_drm_bridge.h"
 #include "media/base/bind_to_current_loop.h"
@@ -18,6 +19,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/origin.h"
 
 namespace media {
+
+namespace {
+
+void ReportMediaDrmBridgeKeySystemSupport(bool supported) {
+  UMA_HISTOGRAM_BOOLEAN("Media.EME.MediaDrmBridge.KeySystemSupport", supported);
+}
+
+}  // namespace
 
 AndroidCdmFactory::AndroidCdmFactory(const CreateFetcherCB& create_fetcher_cb,
                                      const CreateStorageCB& create_storage_cb)
@@ -66,11 +75,13 @@ void AndroidCdmFactory::Create(
   std::string error_message;
 
   if (!MediaDrmBridge::IsKeySystemSupported(key_system)) {
-    error_message = "Key system not supported unexpectedly: " + key_system;
-    NOTREACHED() << error_message;
-    bound_cdm_created_cb.Run(nullptr, error_message);
+    ReportMediaDrmBridgeKeySystemSupport(false);
+    bound_cdm_created_cb.Run(
+        nullptr, "Key system not supported unexpectedly: " + key_system);
     return;
   }
+
+  ReportMediaDrmBridgeKeySystemSupport(true);
 
   auto factory = std::make_unique<MediaDrmBridgeFactory>(create_fetcher_cb_,
                                                          create_storage_cb_);
