f5f31d34678ca125c04e289535d5c8d16a00d6c8
Erik Luo <luoe@chromium.org>
2018-05-17 22:06:12 +0000

    DevTools: fix console style formatter regressions
    This CL addresses vertical alignment, horizontal overflow, and the
    creation of newlines in Console logs that use the style formatter.
    
    Very long fragments still break into their own block, due to the
    `contain: paint` rule.
    
    Screenshot: https://imgur.com/a/QiNbvIT
    
    Bug: 810581
    Change-Id: Iefb5533d5696a7b91e3ff9863af67740a8e07225
    Reviewed-on: https://chromium-review.googlesource.com/1058394
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Erik Luo <luoe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559700}

--- third_party/WebKit/LayoutTests/http/tests/devtools/console/console-format-style.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/console/console-format-style.js
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     {
         console.log("%cBlue!.", "color: blue;");
         console.log("%cBlue! %cRed!", "color: blue;", "color: red;");
+        console.log("%cBlue!\\n%cRed!", "color: blue;", "color: red;");
         console.log("%cwww.google.com", "color: blue");
         console.warn("%cwww.google.com", "color: blue; background: blue");
         console.error("%cwww.google.com", "color: blue; background: blue");
