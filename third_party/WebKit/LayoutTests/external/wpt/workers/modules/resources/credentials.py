e623d650d67b7aa604d93940d7add39b4310e07c
Hiroki Nakagawa <nhiroki@chromium.org>
2018-05-18 14:44:40 +0000

    Worker: Add credentials tests for dedicated workers
    This CL adds web-platform-tests for the "credentials" option of WorkerOptions:
    https://html.spec.whatwg.org/multipage/workers.html#workeroptions
    
    The current spec defines that the default value of this option is "omit", but
    there is an ongoing spec discussion to change it to "same-origin":
    https://github.com/whatwg/html/pull/3656
    
    This CL adds the tests based on the current spec, and a subsequent CL will
    update them based on the decision.
    
    Bug: 843875
    Change-Id: I50eb0c7971587b9d84865498d67abef8ed2d8fc6
    Reviewed-on: https://chromium-review.googlesource.com/1063524
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559888}

---
+++ third_party/WebKit/LayoutTests/external/wpt/workers/modules/resources/credentials.py
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+def main(request, response):
+    cookie = request.cookies.first("COOKIE_NAME", None)
+
+    response_headers = [("Content-Type", "text/javascript"),
+                        ("Access-Control-Allow-Credentials", "true")]
+
+    cookie_value = '';
+    if cookie:
+        cookie_value = cookie.value;
+    return (200, response_headers, "postMessage('"+cookie_value+"');")
