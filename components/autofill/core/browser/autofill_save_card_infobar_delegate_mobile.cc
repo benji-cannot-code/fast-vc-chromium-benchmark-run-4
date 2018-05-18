03b218db6b1931a43b963ce0ccf41adf5413139f
Moe Ahmadi <mahmadi@chromium.org>
2018-05-18 17:13:58 +0000

    [AF] Updates the save CC prompt message on mobile
    Updates the prompt message on mobile to take into account the
    AutofillUpstreamUpdatePromptExplanation experiment similar to Desktop
    
    Bug: 844472
    Change-Id: I24bbabbb1af5f1d559196b6dccca5e600aff13ad
    Reviewed-on: https://chromium-review.googlesource.com/1066212
    Reviewed-by: Mathieu Perreault <mathp@chromium.org>
    Commit-Queue: Mathieu Perreault <mathp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559933}

--- components/autofill/core/browser/autofill_save_card_infobar_delegate_mobile.cc
+++ components/autofill/core/browser/autofill_save_card_infobar_delegate_mobile.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
+#include "components/autofill/core/browser/autofill_experiments.h"
 #include "components/autofill/core/browser/credit_card.h"
 #include "components/autofill/core/browser/legal_message_line.h"
 #include "components/autofill/core/common/autofill_constants.h"
@@ -96,8 +97,11 @@ base::string16 AutofillSaveCardInfoBarDelegateMobile::GetTitleText() const {
 
 base::string16 AutofillSaveCardInfoBarDelegateMobile::GetDescriptionText()
     const {
-  return l10n_util::GetStringUTF16(
-      IDS_AUTOFILL_SAVE_CARD_PROMPT_UPLOAD_EXPLANATION_V2);
+  return IsAutofillUpstreamUpdatePromptExplanationExperimentEnabled()
+             ? l10n_util::GetStringUTF16(
+                   IDS_AUTOFILL_SAVE_CARD_PROMPT_UPLOAD_EXPLANATION_V3)
+             : l10n_util::GetStringUTF16(
+                   IDS_AUTOFILL_SAVE_CARD_PROMPT_UPLOAD_EXPLANATION_V2);
 }
 
 int AutofillSaveCardInfoBarDelegateMobile::GetIconId() const {
