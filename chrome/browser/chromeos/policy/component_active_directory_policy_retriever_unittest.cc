dc546f84ed967bceef5838af89b22ee2a224e11f
Lutz Justen <ljusten@chromium.org>
2018-05-18 09:33:42 +0000

    Add ComponentActiveDirectoryPolicyService
    Part 2 of Active Directory extension policy support. This CL adds a
    ComponentActiveDirectoryPolicyService class that is responsible for
    fetching and parsing component policy, performing type conversions and
    validating schema. To fetch policy it uses the ComponentActiveDirectory-
    PolicyRetriever class introduced in part 1. In part 3 this class will be
    hooked up with the ActiveDirectoryPolicyManager instances.
    
    BUG=chromium:784595
    TEST=unit_tests --gtest_filter=ComponentActiveDirectoryPolicyService*
    
    Change-Id: Ib601706c6efdb2017108384687e36fbff7fae98c
    Reviewed-on: https://chromium-review.googlesource.com/1057620
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Lutz Justen <ljusten@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559845}

--- chrome/browser/chromeos/policy/component_active_directory_policy_retriever_unittest.cc
+++ chrome/browser/chromeos/policy/component_active_directory_policy_retriever_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/dbus/fake_session_manager_client.h"
 #include "chromeos/dbus/session_manager_client.h"
 #include "components/policy/core/common/cloud/policy_builder.h"
+#include "components/policy/core/common/policy_test_utils.h"
 #include "components/policy/proto/device_management_backend.pb.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -36,10 +37,7 @@ using ResponseType = ComponentActiveDirectoryPolicyRetriever::ResponseType;
 
 class ComponentActiveDirectoryPolicyRetrieverTest : public testing::Test {
  protected:
-  ComponentActiveDirectoryPolicyRetrieverTest() {}
-
-  // testing::Test:
-  void SetUp() override {
+  ComponentActiveDirectoryPolicyRetrieverTest() {
     auto session_manager_client =
         std::make_unique<chromeos::FakeSessionManagerClient>();
     session_manager_client_ = session_manager_client.get();
