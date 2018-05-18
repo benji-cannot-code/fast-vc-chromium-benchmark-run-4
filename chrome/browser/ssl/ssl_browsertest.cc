3378b8daac7c88c7e3ff9324e0f65244d398d9ac
Alexander Hendrich <hendrich@chromium.org>
2018-05-18 08:21:10 +0000

    Support multiple filters for the same pattern in 'AutoSelectCertificateForUrls'
    Update the client certificate auto-selection content settings to be a
    list of filters instead of just a single filter to support
    multiple filters for the same patterns. The filters are processed in
    the order they are specified, and the first matched certificate is
    selected.
    
    Bug: 824940
    Change-Id: I91e6193a263a8ebfe114679cd032e7997286c179
    Reviewed-on: https://chromium-review.googlesource.com/1050250
    Commit-Queue: Alexander Hendrich <hendrich@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559838}

--- chrome/browser/ssl/ssl_browsertest.cc
+++ chrome/browser/ssl/ssl_browsertest.cc
@@ -2350,12 +2350,16 @@ IN_PROC_BROWSER_TEST_F(SSLUITestWithClientCert, TestWSSClientCert) {
   // cert selection.
   Profile* profile = Profile::FromBrowserContext(tab->GetBrowserContext());
   DCHECK(profile);
-  std::unique_ptr<base::DictionaryValue> dict(new base::DictionaryValue());
-  dict->SetString("ISSUER.CN", "pywebsocket");
+  std::unique_ptr<base::DictionaryValue> setting =
+      std::make_unique<base::DictionaryValue>();
+  base::Value* filters = setting->SetKey("filters", base::ListValue());
+  base::DictionaryValue filter = base::DictionaryValue();
+  filter.SetString("ISSUER.CN", "pywebsocket");
+  filters->GetList().push_back(std::move(filter));
   HostContentSettingsMapFactory::GetForProfile(profile)
       ->SetWebsiteSettingDefaultScope(
           url, GURL(), CONTENT_SETTINGS_TYPE_AUTO_SELECT_CERTIFICATE,
-          std::string(), std::move(dict));
+          std::string(), std::move(setting));
 
   // Visit a HTTPS page which requires client certs.
   ui_test_utils::NavigateToURL(browser(), url);
@@ -2426,11 +2430,14 @@ IN_PROC_BROWSER_TEST_P(SSLUITest, TestBrowserUseClientCertStore) {
   WebContents* tab = browser()->tab_strip_model()->GetActiveWebContents();
   Profile* profile = Profile::FromBrowserContext(tab->GetBrowserContext());
   DCHECK(profile);
-  std::unique_ptr<base::DictionaryValue> dict(new base::DictionaryValue());
+  std::unique_ptr<base::DictionaryValue> setting =
+      std::make_unique<base::DictionaryValue>();
+  base::Value* filters = setting->SetKey("filters", base::ListValue());
+  filters->GetList().push_back(base::DictionaryValue());
   HostContentSettingsMapFactory::GetForProfile(profile)
       ->SetWebsiteSettingDefaultScope(
           https_url, GURL(), CONTENT_SETTINGS_TYPE_AUTO_SELECT_CERTIFICATE,
-          std::string(), std::move(dict));
+          std::string(), std::move(setting));
 
   // Visit a HTTPS page which requires client certs.
   ui_test_utils::NavigateToURLBlockUntilNavigationsComplete(browser(),
