dfdf5aac2a9eeb1d8df2d16e69310db1dde497ed
David Trainor <dtrainor@chromium.org>
2018-05-18 19:17:21 +0000

    Initial implementation of the tab selector in downloads home
    Adds the initial tab selection implementation for downloads home.
    This rolls up to a TabContentCoordinator that is responsible for
    determining which of two tabs is selected and setting a sub-child
    content view based on that selection.
    
    BUG=841562
    
    Sample Screenshot: https://drive.google.com/open?id=1dgH9olqO1g4om_Ou_JkpmCDYJu90PrLH
    
    Change-Id: Ie828db8ec98bc7b5babbfee3591deffb22758d34
    Reviewed-on: https://chromium-review.googlesource.com/1053214
    Commit-Queue: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559985}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/download/home/filter/FilterViewBinder.java
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.download.home.filter;
+
+import org.chromium.chrome.browser.download.home.filter.FilterModel.PropertyKey;
+import org.chromium.chrome.browser.modelutil.PropertyModelChangeProcessor.ViewBinder;
+
+/**
+ * A helper {@link ViewBinder} responsible for gluing {@link FilterModel} to
+ * {@link FilterView}.
+ */
+class FilterViewBinder implements ViewBinder<FilterModel, FilterView, FilterModel.PropertyKey> {
+    @Override
+    public void bind(FilterModel model, FilterView view, PropertyKey propertyKey) {
+        if (propertyKey == FilterModel.PropertyKey.CONTENT_VIEW) {
+            view.setContentView(model.getContentView());
+        } else if (propertyKey == FilterModel.PropertyKey.SELECTED_TAB) {
+            view.setTabSelected(model.getSelectedTab());
+        } else if (propertyKey == FilterModel.PropertyKey.CHANGE_LISTENER) {
+            view.setTabSelectedCallback(model.getChangeListener());
+        }
+    }
+}
--- chrome/android/java/src/org/chromium/chrome/browser/download/home/filter/Filters.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/home/filter/Filters.java
@@ -13,7 +13,7 @@ import java.lang.annotation.RetentionPolicy;
 /** A list of possible filter types on offlined items. */
 public interface Filters {
     @Retention(RetentionPolicy.SOURCE)
-    @IntDef({NONE, VIDEOS, MUSIC, IMAGES, SITES, OTHER})
+    @IntDef({NONE, VIDEOS, MUSIC, IMAGES, SITES, OTHER, PREFETCHED})
     public @interface FilterType {}
     public static final int NONE = 0;
     public static final int VIDEOS = 1;
@@ -21,4 +21,5 @@ public interface Filters {
     public static final int IMAGES = 3;
     public static final int SITES = 4;
     public static final int OTHER = 5;
+    public static final int PREFETCHED = 6;
 }
