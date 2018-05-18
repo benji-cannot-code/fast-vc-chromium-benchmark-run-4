5172796bb595f3cab5b68b754ad8637b45b6c95d
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-05-18 00:48:56 +0000

    Import wpt@b47939b757ede1c742c76b3127497d715fc61b6b
    Using wpt-import in Chromium 5c3de04ee307a86b1f54f41ec3a3351aea33baee.
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/17853
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    jsbell@chromium.org:
      external/wpt/resources
    meade@chromium.org:
      external/wpt/web-animations
    
    TBR=foolip
    
    No-Export: true
    Change-Id: I5da69d829bdbd00eb1d75bbe71ccb0027a8f5389
    Reviewed-on: https://chromium-review.googlesource.com/1065092
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559757}

--- third_party/WebKit/LayoutTests/external/wpt/BackgroundSync/interfaces.any.js
+++ third_party/WebKit/LayoutTests/external/wpt/BackgroundSync/interfaces.any.js
@@ -7,24 +7,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // https://wicg.github.io/BackgroundSync/spec/
 
 promise_test(async () => {
-  const dom = await fetch('/interfaces/dom.idl').then(r => r.text());
-  const html = await fetch('/interfaces/html.idl').then(r => r.text());
+  const idl = await fetch('/interfaces/BackgroundSync.idl').then(r => r.text());
   const sw = await fetch('/interfaces/ServiceWorker.idl').then(r => r.text());
-  const idl = await fetch('/interfaces/BackgroundSync.idl').then(response => response.text());
+  const html = await fetch('/interfaces/html.idl').then(r => r.text());
+  const dom = await fetch('/interfaces/dom.idl').then(r => r.text());
 
   const idlArray = new IdlArray();
-  idlArray.add_untested_idls(dom, { only: ['Event', 'EventInit', 'EventTarget'] });
-  idlArray.add_untested_idls(html, { only: [
-    'WorkerGlobalScope',
-    'WindowOrWorkerGlobalScope'
-  ] });
-  idlArray.add_untested_idls(sw, { only: [
-    'ServiceWorkerRegistration',
-    'ServiceWorkerGlobalScope',
-    'ExtendableEvent',
-    'ExtendableEventInit',
-  ] });
   idlArray.add_idls(idl);
+  idlArray.add_dependency_idls(sw);
+  idlArray.add_dependency_idls(html);
+  idlArray.add_dependency_idls(dom);
   idlArray.test();
   done();
 }, 'Background Sync interfaces.');
