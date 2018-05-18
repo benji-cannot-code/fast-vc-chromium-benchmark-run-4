67cd4f01b3596036de479104bbf3a2e4f9e3bef7
Mike Wasserman <msw@chromium.org>
2018-05-18 21:01:41 +0000

    ws: Add POINTER_ENTERED and POINTER_CAPTURE_CHANGED mojo event types.
    Change one-off conversion functions TypeConverters for tracking.
    Make enum naming and ordering match ui::EventType.
    
    This fixes some crashes observed running KSV as a mojo app with ws2:
      https://chromium-review.googlesource.com/c/chromium/src/+/1060163
    
    Bug: 841020, 837692
    Change-Id: If3e33ac6a35d5fb09beb4317140c1c04d18b4eb5
    Reviewed-on: https://chromium-review.googlesource.com/1065199
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560032}

--- ui/events/mojo/event_struct_traits.h
+++ ui/events/mojo/event_struct_traits.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_EVENTS_MOJO_EVENT_STRUCT_TRAITS_H_
 #define UI_EVENTS_MOJO_EVENT_STRUCT_TRAITS_H_
 
+#include "mojo/public/cpp/bindings/type_converter.h"
 #include "ui/events/event_constants.h"
 #include "ui/events/mojo/event.mojom.h"
 #include "ui/events/mojo/event_constants.mojom.h"
@@ -18,6 +19,16 @@ class LatencyInfo;
 namespace mojo {
 
 using EventUniquePtr = std::unique_ptr<ui::Event>;
+
+template <>
+struct TypeConverter<ui::mojom::EventType, ui::EventType> {
+  static ui::mojom::EventType Convert(ui::EventType type);
+};
+
+template <>
+struct TypeConverter<ui::EventType, ui::mojom::EventType> {
+  static ui::EventType Convert(ui::mojom::EventType type);
+};
 
 template <>
 struct StructTraits<ui::mojom::EventDataView, EventUniquePtr> {
--- ui/events/mojo/struct_traits_unittest.cc
+++ ui/events/mojo/struct_traits_unittest.cc
@@ -103,11 +103,24 @@ TEST_F(StructTraitsTest, PointerEvent) {
        PointerDetails(EventPointerType::POINTER_TYPE_MOUSE,
                       MouseEvent::kMousePointerId),
        base::TimeTicks() + base::TimeDelta::FromMicroseconds(203)},
+      {ET_POINTER_CANCELLED, gfx::Point(0, 1), gfx::Point(2, 3), EF_ALT_DOWN, 0,
+       PointerDetails(EventPointerType::POINTER_TYPE_MOUSE,
+                      MouseEvent::kMousePointerId),
+       base::TimeTicks() + base::TimeDelta::FromMicroseconds(204)},
+      {ET_POINTER_ENTERED, gfx::Point(6, 7), gfx::Point(8, 9), EF_NONE, 0,
+       PointerDetails(EventPointerType::POINTER_TYPE_MOUSE,
+                      MouseEvent::kMousePointerId),
+       base::TimeTicks() + base::TimeDelta::FromMicroseconds(205)},
       {ET_POINTER_EXITED, gfx::Point(10, 10), gfx::Point(20, 30),
        EF_BACK_MOUSE_BUTTON, 0,
        PointerDetails(EventPointerType::POINTER_TYPE_MOUSE,
                       MouseEvent::kMousePointerId),
-       base::TimeTicks() + base::TimeDelta::FromMicroseconds(204)},
+       base::TimeTicks() + base::TimeDelta::FromMicroseconds(206)},
+      {ET_POINTER_CAPTURE_CHANGED, gfx::Point(99, 99), gfx::Point(99, 99),
+       EF_CONTROL_DOWN, 0,
+       PointerDetails(EventPointerType::POINTER_TYPE_MOUSE,
+                      MouseEvent::kMousePointerId),
+       base::TimeTicks() + base::TimeDelta::FromMicroseconds(207)},
 
       // Touch pointer events:
       {ET_POINTER_DOWN, gfx::Point(10, 10), gfx::Point(20, 30), EF_NONE, 0,
@@ -119,7 +132,7 @@ TEST_F(StructTraitsTest, PointerEvent) {
                       /* twist */ 0,
                       /* tilt_x */ 4.0f,
                       /* tilt_y */ 5.0f),
-       base::TimeTicks() + base::TimeDelta::FromMicroseconds(205)},
+       base::TimeTicks() + base::TimeDelta::FromMicroseconds(208)},
       {ET_POINTER_CANCELLED, gfx::Point(120, 120), gfx::Point(2, 3), EF_NONE, 0,
        PointerDetails(EventPointerType::POINTER_TYPE_TOUCH,
                       /* pointer_id */ 2,
@@ -129,7 +142,7 @@ TEST_F(StructTraitsTest, PointerEvent) {
                       /* twist */ 0,
                       /* tilt_x */ 2.5f,
                       /* tilt_y */ 0.5f),
-       base::TimeTicks() + base::TimeDelta::FromMicroseconds(206)},
+       base::TimeTicks() + base::TimeDelta::FromMicroseconds(209)},
 
       // Pen pointer events:
       {ET_POINTER_DOWN, gfx::Point(1, 2), gfx::Point(3, 4), EF_NONE, 0,
@@ -142,7 +155,7 @@ TEST_F(StructTraitsTest, PointerEvent) {
                       /* tilt_x */ 4.0f,
                       /* tilt_y */ 5.0f,
                       /* tangential_pressure */ -1.f),
-       base::TimeTicks() + base::TimeDelta::FromMicroseconds(207)},
+       base::TimeTicks() + base::TimeDelta::FromMicroseconds(210)},
       {ET_POINTER_UP, gfx::Point(5, 6), gfx::Point(7, 8), EF_NONE, 0,
        PointerDetails(EventPointerType::POINTER_TYPE_PEN,
                       /* pointer_id */ 3,
@@ -153,7 +166,7 @@ TEST_F(StructTraitsTest, PointerEvent) {
                       /* tilt_x */ 4.0f,
                       /* tilt_y */ 5.0f,
                       /* tangential_pressure */ 1.f),
-       base::TimeTicks() + base::TimeDelta::FromMicroseconds(208)},
+       base::TimeTicks() + base::TimeDelta::FromMicroseconds(211)},
   };
 
   mojom::TraitsTestServicePtr proxy = GetTraitsTestProxy();
