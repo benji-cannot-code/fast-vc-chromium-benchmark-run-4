d358c460ed2b69def8dbc9081722bc5dab8b1d09
stkhapugin@chromium.org <stkhapugin@chromium.org>
2018-05-18 10:07:01 +0000

    Move OmniboxContainerView to i/c/b/ui/omnibox.
    Moves the omnibox container view to the folder where it should be.
    
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Id56806892fef9d1ce5d26bc71469f72736140272
    Reviewed-on: https://chromium-review.googlesource.com/1061193
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559852}

--- ios/chrome/browser/ui/omnibox/omnibox_view_controller.mm
+++ ios/chrome/browser/ui/omnibox/omnibox_view_controller.mm
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/omnibox/omnibox_view_controller.h"
 
-#import "ios/chrome/browser/ui/location_bar/omnibox_container_view.h"
+#import "ios/chrome/browser/ui/omnibox/omnibox_container_view.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
