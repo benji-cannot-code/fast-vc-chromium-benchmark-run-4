daeeffedb306968b5010ea34139557fb471e9ba9
Majid Valipour <majidvp@chromium.org>
2018-05-18 19:48:28 +0000

    Reland "[animation-worklet] Lazily register animation worklet global scope"
    Fix jumbo build issue by s/TestAnimationWorkletProxyClient/MockAnimationWorkletProxyClient/
    
    
    This is a reland of 0ce9ac1f4475b3ffa835230f2152bca9890bda10
    
    Original change's description:
    > [animation-worklet] Lazily register animation worklet global scope
    >
    > Register Animation Worklet Global Scope (AWGS) with its proxy client only when
    > it has at least one animator definition.
    >
    > Previously AWGS was being registered with its proxy client as soon as it was
    > constructed.
    >
    > Turns out worklet threads may be indefinitely blocked to receive debugger tasks.
    > In practice this would happen when devtools is open and page is reloaded.
    >
    > The was leading to deadlock because it was creating a cycle where compositor
    > thread could get indirectly blocked on main via the newly register animation
    > worklet global scope. Moreover, registering the global scope too eagerly when it
    > does not have any work to do is wasteful.
    >
    > Lazily registering AWGS addresses the initialization process deadlock and
    > it is more efficient.
    >
    > TEST: modules/animationworklet/animation_worklet_global_scope_test.cc
    >      and manual testing based on the instruction in the bug
    >
    > BUG: 828425
    > Change-Id: Iac8e1653d8a79bd3e27d9d9c5bfaf19174a59664
    > Reviewed-on: https://chromium-review.googlesource.com/1062947
    > Reviewed-by: Majid Valipour <majidvp@chromium.org>
    > Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    > Commit-Queue: Majid Valipour <majidvp@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#559936}
    
    TBR: smcgruer@chromium.org
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Change-Id: I64843615f4af78418682500bb75053bf3f00ce1f
    Reviewed-on: https://chromium-review.googlesource.com/1065431
    Reviewed-by: Majid Valipour <majidvp@chromium.org>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559997}

--- third_party/blink/renderer/modules/animationworklet/animation_worklet_global_scope.h
+++ third_party/blink/renderer/modules/animationworklet/animation_worklet_global_scope.h
@@ -60,6 +60,7 @@ class MODULES_EXPORT AnimationWorkletGlobalScope
                               v8::Isolate*,
                               WorkerThread*);
 
+  void RegisterWithProxyClientIfNeeded();
   Animator* GetAnimatorFor(int animation_id, const String& name);
   typedef HeapHashMap<String, TraceWrapperMember<AnimatorDefinition>>
       DefinitionMap;
@@ -67,6 +68,8 @@ class MODULES_EXPORT AnimationWorkletGlobalScope
 
   typedef HeapHashMap<int, TraceWrapperMember<Animator>> AnimatorMap;
   AnimatorMap animators_;
+
+  bool registered_ = false;
 };
 
 DEFINE_TYPE_CASTS(AnimationWorkletGlobalScope,
