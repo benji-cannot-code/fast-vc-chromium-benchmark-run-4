892426b3d79fbb177726bc97bd25f32d330072c3
Gabriel Charette <gab@chromium.org>
2018-05-18 00:36:12 +0000

    [MessageLoop cleanup] Get rid of very last(?) caller to static QuitWhenIdleClosure()
    This is the ForUI equivalent of https://chromium-review.googlesource.com/c/chromium/src/+/1064811
    
    And cleanup a few includes I missed in that previous CL.
    
    TBR=danakj@chromium.org, kylechar@chromium.org
    
    Bug: 825327
    Change-Id: I0e947c9d0441101b24ee8f21c77cd24f0ec78fa1
    Reviewed-on: https://chromium-review.googlesource.com/1065017
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559753}

--- content/browser/download/drag_download_file_browsertest.cc
+++ content/browser/download/drag_download_file_browsertest.cc
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/scoped_temp_dir.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
-#include "base/message_loop/message_loop_current.h"
 #include "base/path_service.h"
+#include "base/run_loop.h"
 #include "content/browser/download/download_manager_impl.h"
 #include "content/browser/download/drag_download_file.h"
 #include "content/browser/download/drag_download_util.h"
@@ -55,7 +55,7 @@ class DragDownloadFileTest : public ContentBrowserTest {
   void Succeed() {
     BrowserThread::PostTask(
         BrowserThread::UI, FROM_HERE,
-        base::MessageLoopCurrentForUI::Get()->QuitWhenIdleClosure());
+        base::RunLoop::QuitCurrentWhenIdleClosureDeprecated());
   }
 
   void FailFast() {
