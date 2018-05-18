4fcd045b79ce317dafcdf8b82a0fe3eb8ccac6f7
Dmitry Gozman <dgozman@chromium.org>
2018-05-18 19:18:12 +0000

    Remove WindRule from FloatPolygon, which was only used in tests
    In fact, we can remove around 80% of FloatPolygon code since it's
    unused, I'll try to make a patch and see how it looks.
    
    Change-Id: Ieec162fe43090f14b0db073030729366b496a434
    Reviewed-on: https://chromium-review.googlesource.com/1066211
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559986}

--- third_party/blink/renderer/core/layout/shapes/polygon_shape.h
+++ third_party/blink/renderer/core/layout/shapes/polygon_shape.h
@@ -66,7 +66,7 @@ class OffsetPolygonEdge final : public VertexPair {
 class PolygonShape final : public Shape {
  public:
   PolygonShape(std::unique_ptr<Vector<FloatPoint>> vertices, WindRule fill_rule)
-      : Shape(), polygon_(std::move(vertices), fill_rule) {}
+      : Shape(), polygon_(std::move(vertices)) {}
 
   LayoutRect ShapeMarginLogicalBoundingBox() const override;
   bool IsEmpty() const override { return polygon_.IsEmpty(); }
--- third_party/blink/renderer/platform/geometry/float_polygon.cc
+++ third_party/blink/renderer/platform/geometry/float_polygon.cc
@@ -86,9 +86,8 @@ static unsigned FindNextEdgeVertexIndex(const FloatPolygon& polygon,
   return vertex_index2;
 }
 
-FloatPolygon::FloatPolygon(std::unique_ptr<Vector<FloatPoint>> vertices,
-                           WindRule fill_rule)
-    : vertices_(std::move(vertices)), fill_rule_(fill_rule) {
+FloatPolygon::FloatPolygon(std::unique_ptr<Vector<FloatPoint>> vertices)
+    : vertices_(std::move(vertices)) {
   unsigned n_vertices = NumberOfVertices();
   edges_.resize(n_vertices);
   empty_ = n_vertices < 3;
@@ -175,6 +174,8 @@ static inline float LeftSide(const FloatPoint& vertex1,
 }
 
 bool FloatPolygon::ContainsEvenOdd(const FloatPoint& point) const {
+  if (!bounding_box_.Contains(point))
+    return false;
   unsigned crossing_count = 0;
   for (unsigned i = 0; i < NumberOfEdges(); ++i) {
     const FloatPoint& vertex1 = EdgeAt(i).Vertex1();
@@ -192,6 +193,8 @@ bool FloatPolygon::ContainsEvenOdd(const FloatPoint& point) const {
 }
 
 bool FloatPolygon::ContainsNonZero(const FloatPoint& point) const {
+  if (!bounding_box_.Contains(point))
+    return false;
   int winding_number = 0;
   for (unsigned i = 0; i < NumberOfEdges(); ++i) {
     const FloatPoint& vertex1 = EdgeAt(i).Vertex1();
@@ -207,13 +210,6 @@ bool FloatPolygon::ContainsNonZero(const FloatPoint& point) const {
     }
   }
   return winding_number;
-}
-
-bool FloatPolygon::Contains(const FloatPoint& point) const {
-  if (!bounding_box_.Contains(point))
-    return false;
-  return (FillRule() == RULE_NONZERO) ? ContainsNonZero(point)
-                                      : ContainsEvenOdd(point);
 }
 
 bool VertexPair::Intersection(const VertexPair& other,
