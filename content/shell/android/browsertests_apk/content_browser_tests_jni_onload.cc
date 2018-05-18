d9f196f8afb4e3bda3b211312120e0429e76eb25
John Abd-El-Malek <jam@chromium.org>
2018-05-18 20:29:50 +0000

    Fix content_shell with network service enabled not loading pages.
    This regressed in my earlier cl r528763.
    
    This is a reland of r547221.
    
    Bug: 833612
    
    Change-Id: I4c2649414d42773f2530e1abe5912a04fcd0ed9b
    Reviewed-on: https://chromium-review.googlesource.com/1064702
    Reviewed-by: Jay Civelli <jcivelli@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560011}

--- content/shell/android/browsertests_apk/content_browser_tests_jni_onload.cc
+++ content/shell/android/browsertests_apk/content_browser_tests_jni_onload.cc
@@ -16,6 +16,6 @@ JNI_EXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
   base::android::InitVM(vm);
   if (!content::android::OnJNIOnLoadInit())
     return -1;
-  content::SetContentMainDelegate(new content::ShellMainDelegate());
+  content::SetContentMainDelegate(new content::ShellMainDelegate(true));
   return JNI_VERSION_1_4;
 }
--- content/shell/utility/shell_content_utility_client.h
+++ content/shell/utility/shell_content_utility_client.h
@@ -14,7 +14,7 @@ namespace content {
 
 class ShellContentUtilityClient : public ContentUtilityClient {
  public:
-  ShellContentUtilityClient();
+  explicit ShellContentUtilityClient(bool is_browsertest = false);
   ~ShellContentUtilityClient() override;
 
   // ContentUtilityClient:
