aa81b16c4b698b5f54ca7d1d943cd325a7319058
Ovidio Henriquez <odejesush@chromium.org>
2018-05-18 20:56:35 +0000

    Allow WebUSB to work in a Dedicated Worker
    This change updates the USB class to allow the exposed APIs to work
    correctly in the context of a Dedicated Worker. In order to do this, USB
    was reworked to use an ExecutionContext object to check the context and
    the FeaturePolicy, rather than the LocalFrame that it was using
    previously. The usb layout test for workers was also updated to use the
    WebUSB test API to check that getDevices and event listeners work in
    Dedicated Workers.
    
    Additionally, the UsbDeviceManager Mojo interface used by WebUSB is
    added to the DedicatedWorkerHost BinderRegistry so that the request is
    handled correctly.
    
    Bug: 837414,837422
    Change-Id: I1ad95e288312851e9ec39ce6ec917bf38b12d4d6
    Reviewed-on: https://chromium-review.googlesource.com/1045309
    Commit-Queue: Ovidio Henriquez <odejesush@chromium.org>
    Reviewed-by: Greg Kerr <kerrnel@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Chong Zhang <chongz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560026}

--- content/browser/dedicated_worker/dedicated_worker_host.cc
+++ content/browser/dedicated_worker/dedicated_worker_host.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/dedicated_worker/dedicated_worker_host.h"
 
+#include "content/browser/frame_host/render_frame_host_impl.h"
 #include "content/browser/interface_provider_filtering.h"
 #include "content/browser/renderer_interface_binders.h"
 #include "content/browser/websockets/websocket_manager.h"
@@ -56,6 +57,15 @@ class DedicatedWorkerHost : public service_manager::mojom::InterfaceProvider {
     registry_.AddInterface(
         base::BindRepeating(&WebSocketManager::CreateWebSocket, process_id_,
                             parent_render_frame_id_, origin_));
+    registry_.AddInterface(base::BindRepeating(
+        &DedicatedWorkerHost::CreateUsbDeviceManager, base::Unretained(this)));
+  }
+
+  void CreateUsbDeviceManager(device::mojom::UsbDeviceManagerRequest request) {
+    auto* host =
+        RenderFrameHostImpl::FromID(process_id_, parent_render_frame_id_);
+    GetContentClient()->browser()->CreateUsbDeviceManager(host,
+                                                          std::move(request));
   }
 
   const int process_id_;
