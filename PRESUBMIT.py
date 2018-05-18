4f6e6e76260158af1d46620347f7aa1bf60ee4a0
Andrew Grieve <agrieve@chromium.org>
2018-05-18 16:17:22 +0000

    GN: Add "python_library" template for group()s that use ".pydeps"
    Also adds two new usages of it:
      //build/android:resource_sizes_py
      //tools/binary_size:binary_size_trybot_py
    
    Bug: 702625
    Change-Id: Idc8113ca5e95ec9b5f5ff0cf22c14eeab95b78f8
    Reviewed-on: https://chromium-review.googlesource.com/1058836
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Reviewed-by: Helen Li <xunjieli@chromium.org>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Commit-Queue: agrieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559916}

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -629,6 +629,7 @@ _VALID_OS_MACROS = (
 
 
 _ANDROID_SPECIFIC_PYDEPS_FILES = [
+    'build/android/resource_sizes.pydeps',
     'build/android/test_runner.pydeps',
     'build/android/test_wrapper/logdog_wrapper.pydeps',
     'build/secondary/third_party/android_platform/'
@@ -639,6 +640,7 @@ _ANDROID_SPECIFIC_PYDEPS_FILES = [
 
 _GENERIC_PYDEPS_FILES = [
     'chrome/test/chromedriver/test/run_py_tests.pydeps',
+    'tools/binary_size/supersize.pydeps',
 ]
 
 
