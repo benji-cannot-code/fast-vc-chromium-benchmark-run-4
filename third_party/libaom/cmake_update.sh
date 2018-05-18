cff89417bf9f763b497ec74e76ad51d707da7648
David Michael Barr <david.barr@samsung.com>
2018-05-18 02:36:14 +0000

    libaom: enable generation of linux/arm-neon config
    Use run_perl.py from third_party/libvpx.
    
    Document cmake_update.sh requirements, particularly:
    * gcc-arm-linux-gnueabihf
    * g++-arm-linux-gnueabihf
    
    Bug: 783519
    Change-Id: I091a60cd306e1bea63d4a8ab0b0caacb65ade464
    Reviewed-on: https://chromium-review.googlesource.com/1062986
    Reviewed-by: Johann Koenig <johannkoenig@google.com>
    Commit-Queue: Johann Koenig <johannkoenig@google.com>
    Cr-Commit-Position: refs/heads/master@{#559781}

--- third_party/libaom/cmake_update.sh
+++ third_party/libaom/cmake_update.sh
@@ -13,6 +13,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Usage:
 # $ ./cmake_update.sh
 # Requirements:
+# - cmake3
+# - yasm or nasm
+# Toolchain for armv7:
+#  -gcc-arm-linux-gnueabihf
+#  -g++-arm-linux-gnueabihf
 # 32bit build environment for cmake. Including but potentially not limited to:
 #  -lib32gcc-7-dev
 #  -lib32stdc++-7-dev
@@ -184,5 +189,18 @@ sed -i.bak \
 rm "${CFG}/win/x64/aom_config.h.bak"
 egrep "#define [A-Z0-9_]+ [01]" "${CFG}/win/x64/aom_config.h" \
   | awk '{print "%define " $2 " " $3}' > "${CFG}/win/x64/aom_config.asm"
+
+cd ..
+rm -rf "${TMP}"
+mkdir "${TMP}"
+cd "${TMP}"
+
+echo "Generate linux/arm-neon config files."
+gen_config_files linux/arm-neon "${toolchain}/armv7-linux-gcc.cmake ${all_platforms}"
+rm -f "${CFG}/linux/arm-neon"/*
+# mkdir required only for initial commit
+mkdir -p "${CFG}/linux/arm-neon"
+cp aom_config.h aom_config.c aom_config.asm "${CFG}/linux/arm-neon/"
+gen_rtcd_header linux/arm-neon armv7
 
 clean
