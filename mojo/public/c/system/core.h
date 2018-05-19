b7651512be2a25312c989274f4fb05eda9c3032f
Ken Rockot <rockot@chromium.org>
2018-05-19 21:55:51 +0000

    Revert "Mojo: Invitation API"
    This reverts commit a523cf48c629d7327cde4a5f36098f47ad50a696.
    
    Reason for revert: Bad rebase, conflicted with another change
    
    Original change's description:
    > Mojo: Invitation API
    > 
    > This is a public C API replacement for the EDK invitation API, i.e.
    > Incoming/OutgoingBrokerClientInvitation et al. Here the new C calls
    > are introduced and covered by unit tests.
    > 
    > See the associated bug and the bugs it's blocking for more context
    > and follow-up work.
    > 
    > Bug: 844231
    > Change-Id: Iffa2e5b08eefbe3476430eb420efe2f52f4ad195
    > Reviewed-on: https://chromium-review.googlesource.com/1066807
    > Commit-Queue: Ken Rockot <rockot@chromium.org>
    > Reviewed-by: Jay Civelli <jcivelli@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#560164}
    
    TBR=jcivelli@chromium.org,rockot@chromium.org
    
    Change-Id: I798545dc19a146fe40a9e1ad7ff95a41d0d5dd25
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 844231
    Reviewed-on: https://chromium-review.googlesource.com/1067053
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Ken Rockot <rockot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560175}

--- mojo/public/c/system/core.h
+++ mojo/public/c/system/core.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/c/system/buffer.h"
 #include "mojo/public/c/system/data_pipe.h"
 #include "mojo/public/c/system/functions.h"
-#include "mojo/public/c/system/invitation.h"
 #include "mojo/public/c/system/macros.h"
 #include "mojo/public/c/system/message_pipe.h"
 #include "mojo/public/c/system/platform_handle.h"
