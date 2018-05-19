f8c49ed40db557e5838b218f5be4b6315345ef61
David Black <dmblack@google.com>
2018-05-19 01:11:17 +0000

    Stubs in LogoView.
    Actual LogoView is forthcoming (b/78191547).
    This will tide us over until it arrives and make it easy to drop in
    the replacement.
    
    See bug for demo.
    
    Bug: b:79999539
    Change-Id: Ib2c7b7b866823c207bcdd990d8cba839e17080b9
    Reviewed-on: https://chromium-review.googlesource.com/1066815
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/master@{#560129}

---
+++ ash/assistant/ui/dialog_plate/logo_view.h
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_ASSISTANT_UI_DIALOG_PLATE_LOGO_VIEW_H_
+#define ASH_ASSISTANT_UI_DIALOG_PLATE_LOGO_VIEW_H_
+
+#include "base/macros.h"
+#include "ui/views/controls/image_view.h"
+
+namespace ash {
+
+// TODO(b/78191547): Remove this class once the actual LogoView library port
+// has been completed. This class is only a temporary placeholder.
+class LogoView : public views::ImageView {
+ public:
+  enum class State {
+    kUndefined,
+    kListening,
+    kMic,
+  };
+
+  LogoView();
+  ~LogoView() override;
+
+  void SetState(State state);
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(LogoView);
+};
+
+}  // namespace ash
+
+#endif  // ASH_ASSISTANT_UI_DIALOG_PLATE_LOGO_VIEW_H_
