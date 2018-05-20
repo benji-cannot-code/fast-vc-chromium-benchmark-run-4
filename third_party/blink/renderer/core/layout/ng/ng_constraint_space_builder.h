32f45b9829ce22f653d1a3f88b13d159c7ac3bf0
Morten Stenshorne <mstensho@chromium.org>
2018-05-20 23:31:20 +0000

    [LayoutNG] Improve support for negative clearance.
    When there are adjoining floats that need to be separated from a cleared
    block, clearance is predetermined, and we have to force it on the block.
    Any large top margin on the child will just be eaten by negative
    clearance. There's nothing that can be done to prevent clearance in such
    cases. So make sure that we don't try to determine whether to apply
    clearance or not based on the clearance offset set in the constraint
    space. When clearance has been predetermined (which may have triggered a
    BFC offset resolution and what not), refusing to apply it to the child is
    a bug.
    
    This aligns the implementation with the "alternative" way of calculating
    clearance in the spec [1]. Everyone but Edge does this. I think what
    Edge does here is problematic, because it requires us to use a
    hypothetical position that was calculated before clearance got applied
    (clearance causes margins that would otherwise collapse to be
    separated). We'd end up using a hypothetical position not based on the
    actual layout situation.[2]
    
    [1] https://www.w3.org/TR/CSS22/visuren.html#flow-control
    [2] https://github.com/w3c/csswg-drafts/issues/2608
    
    This fixes one existing test. Added a few new ones as well. Not all of
    them failed prior to this CL, but they serve as regression tests for
    things I found to lack coverage while working on this.
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: Ia45e9048c75c338477eee4923ff16eea3245bc6a
    Reviewed-on: https://chromium-review.googlesource.com/1061470
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560208}

--- third_party/blink/renderer/core/layout/ng/ng_constraint_space_builder.h
+++ third_party/blink/renderer/core/layout/ng/ng_constraint_space_builder.h
@@ -77,6 +77,11 @@ class CORE_EXPORT NGConstraintSpaceBuilder final {
 
   NGConstraintSpaceBuilder& SetClearanceOffset(LayoutUnit clearance_offset);
 
+  NGConstraintSpaceBuilder& SetShouldForceClearance() {
+    should_force_clearance_ = true;
+    return *this;
+  }
+
   NGConstraintSpaceBuilder& SetExclusionSpace(
       const NGExclusionSpace& exclusion_space);
 
@@ -114,6 +119,7 @@ class CORE_EXPORT NGConstraintSpaceBuilder final {
   unsigned is_new_fc_ : 1;
   unsigned is_anonymous_ : 1;
   unsigned use_first_line_style_ : 1;
+  unsigned should_force_clearance_ : 1;
   unsigned adjoining_floats_ : 2;  // NGFloatTypes
   unsigned text_direction_ : 1;
 
