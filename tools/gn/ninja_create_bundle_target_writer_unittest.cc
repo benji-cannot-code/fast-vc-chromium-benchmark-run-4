9420c1796878d4ff449f740cdc26ad34e040ceaf
Takuto Ikuta <tikuta@chromium.org>
2018-05-20 22:32:54 +0000

    GN: Handle deps always in create_bundle target
    Stop to use explicit dependency for indirect dependency in the CL broke
    Mac builder.
    https://chromium-review.googlesource.com/1041506
    
    This CL uses deps/public_deps in create_bundle target always not to
    miss the dependency.
    
    Bug: 843470
    Change-Id: Iae66f2a3aec20f67ea2e880bb5fd33d6c2aa2f91
    Reviewed-on: https://chromium-review.googlesource.com/1065591
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560203}

--- tools/gn/ninja_create_bundle_target_writer_unittest.cc
+++ tools/gn/ninja_create_bundle_target_writer_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "tools/gn/ninja_create_bundle_target_writer.h"
 
 #include <algorithm>
+#include <memory>
 #include <sstream>
 
 #include "testing/gtest/include/gtest/gtest.h"
@@ -26,12 +27,30 @@ void SetupBundleDataDir(BundleData* bundle_data, const std::string& root_dir) {
       SourceDir(bundle_data->contents_dir().value() + "/Plug Ins");
 }
 
+std::unique_ptr<Target> NewAction(const TestWithScope& setup) {
+  Err err;
+  auto action = std::make_unique<Target>(setup.settings(),
+                                         Label(SourceDir("//foo/"), "bar"));
+  action->set_output_type(Target::ACTION);
+  action->visibility().SetPublic();
+  action->action_values().set_script(SourceFile("//foo/script.py"));
+
+  action->action_values().outputs() =
+      SubstitutionList::MakeForTest("//out/Debug/foo.out");
+
+  action->SetToolchain(setup.toolchain());
+  return action;
+}
+
 }  // namespace
 
 // Tests multiple files with an output pattern.
 TEST(NinjaCreateBundleTargetWriter, Run) {
   Err err;
   TestWithScope setup;
+
+  std::unique_ptr<Target> action = NewAction(setup);
+  ASSERT_TRUE(action->OnResolved(&err)) << err.message();
 
   Target bundle_data(setup.settings(), Label(SourceDir("//foo/"), "data"));
   bundle_data.set_output_type(Target::BUNDLE_DATA);
@@ -50,6 +69,7 @@ TEST(NinjaCreateBundleTargetWriter, Run) {
   SetupBundleDataDir(&create_bundle.bundle_data(), "//out/Debug");
   create_bundle.set_output_type(Target::CREATE_BUNDLE);
   create_bundle.private_deps().push_back(LabelTargetPair(&bundle_data));
+  create_bundle.private_deps().push_back(LabelTargetPair(action.get()));
   create_bundle.SetToolchain(setup.toolchain());
   ASSERT_TRUE(create_bundle.OnResolved(&err));
 
@@ -59,12 +79,13 @@ TEST(NinjaCreateBundleTargetWriter, Run) {
 
   const char expected[] =
       "build bar.bundle/Contents/Resources/input1.txt: copy_bundle_data "
-          "../../foo/input1.txt\n"
+      "../../foo/input1.txt || obj/foo/bar.stamp\n"
       "build bar.bundle/Contents/Resources/input2.txt: copy_bundle_data "
-          "../../foo/input2.txt\n"
+      "../../foo/input2.txt || obj/foo/bar.stamp\n"
       "build obj/baz/bar.stamp: stamp "
-          "bar.bundle/Contents/Resources/input1.txt "
-          "bar.bundle/Contents/Resources/input2.txt\n"
+      "bar.bundle/Contents/Resources/input1.txt "
+      "bar.bundle/Contents/Resources/input2.txt"
+      " || obj/foo/bar.stamp\n"
       "build bar.bundle: phony obj/baz/bar.stamp\n";
   std::string out_str = out.str();
   EXPECT_EQ(expected, out_str);
@@ -74,6 +95,9 @@ TEST(NinjaCreateBundleTargetWriter, Run) {
 TEST(NinjaCreateBundleTargetWriter, InSubDirectory) {
   Err err;
   TestWithScope setup;
+
+  std::unique_ptr<Target> action = NewAction(setup);
+  ASSERT_TRUE(action->OnResolved(&err)) << err.message();
 
   Target bundle_data(setup.settings(), Label(SourceDir("//foo/"), "data"));
   bundle_data.set_output_type(Target::BUNDLE_DATA);
@@ -92,6 +116,7 @@ TEST(NinjaCreateBundleTargetWriter, InSubDirectory) {
   SetupBundleDataDir(&create_bundle.bundle_data(), "//out/Debug/gen");
   create_bundle.set_output_type(Target::CREATE_BUNDLE);
   create_bundle.private_deps().push_back(LabelTargetPair(&bundle_data));
+  create_bundle.private_deps().push_back(LabelTargetPair(action.get()));
   create_bundle.SetToolchain(setup.toolchain());
   ASSERT_TRUE(create_bundle.OnResolved(&err));
 
@@ -101,12 +126,12 @@ TEST(NinjaCreateBundleTargetWriter, InSubDirectory) {
 
   const char expected[] =
       "build gen/bar.bundle/Contents/Resources/input1.txt: copy_bundle_data "
-          "../../foo/input1.txt\n"
+      "../../foo/input1.txt || obj/foo/bar.stamp\n"
       "build gen/bar.bundle/Contents/Resources/input2.txt: copy_bundle_data "
-          "../../foo/input2.txt\n"
+      "../../foo/input2.txt || obj/foo/bar.stamp\n"
       "build obj/baz/bar.stamp: stamp "
-          "gen/bar.bundle/Contents/Resources/input1.txt "
-          "gen/bar.bundle/Contents/Resources/input2.txt\n"
+      "gen/bar.bundle/Contents/Resources/input1.txt "
+      "gen/bar.bundle/Contents/Resources/input2.txt || obj/foo/bar.stamp\n"
       "build gen/bar.bundle: phony obj/baz/bar.stamp\n";
   std::string out_str = out.str();
   EXPECT_EQ(expected, out_str);
@@ -117,12 +142,16 @@ TEST(NinjaCreateBundleTargetWriter, JustPartialInfoPlist) {
   Err err;
   TestWithScope setup;
 
+  std::unique_ptr<Target> action = NewAction(setup);
+  ASSERT_TRUE(action->OnResolved(&err)) << err.message();
+
   Target create_bundle(
       setup.settings(),
       Label(SourceDir("//baz/"), "bar", setup.toolchain()->label().dir(),
             setup.toolchain()->label().name()));
   SetupBundleDataDir(&create_bundle.bundle_data(), "//out/Debug");
   create_bundle.set_output_type(Target::CREATE_BUNDLE);
+  create_bundle.private_deps().push_back(LabelTargetPair(action.get()));
   create_bundle.bundle_data().product_type().assign("com.apple.product-type");
   create_bundle.bundle_data().set_partial_info_plist(
       SourceFile("//out/Debug/baz/bar/bar_partial_info.plist"));
@@ -134,9 +163,9 @@ TEST(NinjaCreateBundleTargetWriter, JustPartialInfoPlist) {
   writer.Run();
 
   const char expected[] =
-      "build baz/bar/bar_partial_info.plist: stamp\n"
+      "build baz/bar/bar_partial_info.plist: stamp || obj/foo/bar.stamp\n"
       "build obj/baz/bar.stamp: stamp "
-          "baz/bar/bar_partial_info.plist\n"
+      "baz/bar/bar_partial_info.plist || obj/foo/bar.stamp\n"
       "build bar.bundle: phony obj/baz/bar.stamp\n";
   std::string out_str = out.str();
   EXPECT_EQ(expected, out_str);
@@ -147,6 +176,9 @@ TEST(NinjaCreateBundleTargetWriter, JustPartialInfoPlist) {
 TEST(NinjaCreateBundleTargetWriter, AssetCatalog) {
   Err err;
   TestWithScope setup;
+
+  std::unique_ptr<Target> action = NewAction(setup);
+  ASSERT_TRUE(action->OnResolved(&err)) << err.message();
 
   Target bundle_data(setup.settings(), Label(SourceDir("//foo/"), "data"));
   bundle_data.set_output_type(Target::BUNDLE_DATA);
@@ -173,6 +205,7 @@ TEST(NinjaCreateBundleTargetWriter, AssetCatalog) {
   SetupBundleDataDir(&create_bundle.bundle_data(), "//out/Debug");
   create_bundle.set_output_type(Target::CREATE_BUNDLE);
   create_bundle.private_deps().push_back(LabelTargetPair(&bundle_data));
+  create_bundle.private_deps().push_back(LabelTargetPair(action.get()));
   create_bundle.bundle_data().product_type().assign("com.apple.product-type");
   create_bundle.SetToolchain(setup.toolchain());
   ASSERT_TRUE(create_bundle.OnResolved(&err));
@@ -183,10 +216,10 @@ TEST(NinjaCreateBundleTargetWriter, AssetCatalog) {
 
   const char expected[] =
       "build bar.bundle/Contents/Resources/Assets.car: compile_xcassets "
-          "../../foo/Foo.xcassets | obj/foo/data.stamp\n"
+      "../../foo/Foo.xcassets | obj/foo/data.stamp || obj/foo/bar.stamp\n"
       "  product_type = com.apple.product-type\n"
       "build obj/baz/bar.stamp: stamp "
-          "bar.bundle/Contents/Resources/Assets.car\n"
+      "bar.bundle/Contents/Resources/Assets.car || obj/foo/bar.stamp\n"
       "build bar.bundle: phony obj/baz/bar.stamp\n";
   std::string out_str = out.str();
   EXPECT_EQ(expected, out_str);
@@ -223,6 +256,9 @@ TEST(NinjaCreateBundleTargetWriter, PhonyTarget) {
 TEST(NinjaCreateBundleTargetWriter, Complex) {
   Err err;
   TestWithScope setup;
+
+  std::unique_ptr<Target> action = NewAction(setup);
+  ASSERT_TRUE(action->OnResolved(&err)) << err.message();
 
   Target bundle_data0(setup.settings(),
                       Label(SourceDir("//qux/"), "info_plist"));
@@ -290,6 +326,7 @@ TEST(NinjaCreateBundleTargetWriter, Complex) {
   create_bundle.private_deps().push_back(LabelTargetPair(&bundle_data1));
   create_bundle.private_deps().push_back(LabelTargetPair(&bundle_data2));
   create_bundle.private_deps().push_back(LabelTargetPair(&bundle_data3));
+  create_bundle.private_deps().push_back(LabelTargetPair(action.get()));
   create_bundle.bundle_data().product_type().assign("com.apple.product-type");
   create_bundle.bundle_data().set_partial_info_plist(
       SourceFile("//out/Debug/baz/bar/bar_partial_info.plist"));
@@ -302,26 +339,27 @@ TEST(NinjaCreateBundleTargetWriter, Complex) {
 
   const char expected[] =
       "build bar.bundle/Contents/Info.plist: copy_bundle_data "
-          "../../qux/qux-Info.plist\n"
+      "../../qux/qux-Info.plist || obj/foo/bar.stamp\n"
       "build bar.bundle/Contents/Resources/input1.txt: copy_bundle_data "
-          "../../foo/input1.txt\n"
+      "../../foo/input1.txt || obj/foo/bar.stamp\n"
       "build bar.bundle/Contents/Resources/input2.txt: copy_bundle_data "
-          "../../foo/input2.txt\n"
+      "../../foo/input2.txt || obj/foo/bar.stamp\n"
       "build obj/baz/bar.xcassets.inputdeps.stamp: stamp "
-          "obj/foo/assets.stamp "
-          "obj/quz/assets.stamp\n"
+      "obj/foo/assets.stamp "
+      "obj/quz/assets.stamp\n"
       "build bar.bundle/Contents/Resources/Assets.car | "
-        "baz/bar/bar_partial_info.plist: compile_xcassets "
-          "../../foo/Foo.xcassets "
-          "../../quz/Quz.xcassets | obj/baz/bar.xcassets.inputdeps.stamp\n"
+      "baz/bar/bar_partial_info.plist: compile_xcassets "
+      "../../foo/Foo.xcassets "
+      "../../quz/Quz.xcassets | obj/baz/bar.xcassets.inputdeps.stamp || "
+      "obj/foo/bar.stamp\n"
       "  product_type = com.apple.product-type\n"
       "  partial_info_plist = baz/bar/bar_partial_info.plist\n"
       "build obj/baz/bar.stamp: stamp "
-          "bar.bundle/Contents/Info.plist "
-          "bar.bundle/Contents/Resources/input1.txt "
-          "bar.bundle/Contents/Resources/input2.txt "
-          "bar.bundle/Contents/Resources/Assets.car "
-          "baz/bar/bar_partial_info.plist\n"
+      "bar.bundle/Contents/Info.plist "
+      "bar.bundle/Contents/Resources/input1.txt "
+      "bar.bundle/Contents/Resources/input2.txt "
+      "bar.bundle/Contents/Resources/Assets.car "
+      "baz/bar/bar_partial_info.plist || obj/foo/bar.stamp\n"
       "build bar.bundle: phony obj/baz/bar.stamp\n";
   std::string out_str = out.str();
   EXPECT_EQ(expected, out_str);
@@ -331,6 +369,9 @@ TEST(NinjaCreateBundleTargetWriter, Complex) {
 TEST(NinjaCreateBundleTargetWriter, CodeSigning) {
   Err err;
   TestWithScope setup;
+
+  std::unique_ptr<Target> action = NewAction(setup);
+  ASSERT_TRUE(action->OnResolved(&err)) << err.message();
 
   Target executable(setup.settings(), Label(SourceDir("//baz/"), "quz"));
   executable.set_output_type(Target::EXECUTABLE);
@@ -367,6 +408,7 @@ TEST(NinjaCreateBundleTargetWriter, CodeSigning) {
       SubstitutionList::MakeForTest("-b=quz", "bar.bundle");
   create_bundle.public_deps().push_back(LabelTargetPair(&executable));
   create_bundle.private_deps().push_back(LabelTargetPair(&bundle_data));
+  create_bundle.private_deps().push_back(LabelTargetPair(action.get()));
   create_bundle.SetToolchain(setup.toolchain());
   ASSERT_TRUE(create_bundle.OnResolved(&err));
 
@@ -381,21 +423,20 @@ TEST(NinjaCreateBundleTargetWriter, CodeSigning) {
       "  restat = 1\n"
       "\n"
       "build bar.bundle/Contents/Resources/input1.txt: copy_bundle_data "
-          "../../foo/input1.txt\n"
+      "../../foo/input1.txt || obj/foo/bar.stamp\n"
       "build bar.bundle/Contents/Resources/input2.txt: copy_bundle_data "
-          "../../foo/input2.txt\n"
+      "../../foo/input2.txt || obj/foo/bar.stamp\n"
       "build obj/baz/bar.codesigning.inputdeps.stamp: stamp "
-          "../../build/codesign.py "
-          "quz "
-          "bar.bundle/Contents/Resources/input1.txt "
-          "bar.bundle/Contents/Resources/input2.txt "
-          "./quz\n"
+      "../../build/codesign.py "
+      "quz "
+      "bar.bundle/Contents/Resources/input1.txt "
+      "bar.bundle/Contents/Resources/input2.txt || obj/foo/bar.stamp\n"
       "build bar.bundle/Contents/quz bar.bundle/_CodeSignature/CodeResources: "
-          "__baz_bar___toolchain_default__code_signing_rule "
-          "| obj/baz/bar.codesigning.inputdeps.stamp\n"
+      "__baz_bar___toolchain_default__code_signing_rule "
+      "| obj/baz/bar.codesigning.inputdeps.stamp\n"
       "build obj/baz/bar.stamp: stamp "
-          "bar.bundle/Contents/quz "
-          "bar.bundle/_CodeSignature/CodeResources\n"
+      "bar.bundle/Contents/quz "
+      "bar.bundle/_CodeSignature/CodeResources || obj/foo/bar.stamp\n"
       "build bar.bundle: phony obj/baz/bar.stamp\n";
   std::string out_str = out.str();
   EXPECT_EQ(expected, out_str);
