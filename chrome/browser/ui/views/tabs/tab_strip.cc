d0639419c4380375050052afda4a6c8b0bdfa123
Malay Keshav <malaykeshav@chromium.org>
2018-05-21 17:45:36 +0000

    Remove force stacking when touchable chrome is enabled
    This patch removes the force stacking of tabs when touch optimized
    chrome is enabled and restores the old approach.
    
    Bug: 844193
    Change-Id: Icf591860d06afe4698817291f70d619343237a82
    Component: Touchable chrome, tabstrip, tab stacking
    Reviewed-on: https://chromium-review.googlesource.com/1066864
    Commit-Queue: Malay Keshav <malaykeshav@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560292}

--- chrome/browser/ui/views/tabs/tab_strip.cc
+++ chrome/browser/ui/views/tabs/tab_strip.cc
@@ -2390,9 +2390,8 @@ bool TabStrip::NeedsTouchLayout() const {
 }
 
 void TabStrip::SetResetToShrinkOnExit(bool value) {
-  if (!adjust_layout_ || MD::IsTouchOptimizedUiEnabled()) {
+  if (!adjust_layout_)
     return;
-  }
 
   // We have to be using stacked layout to reset out of it.
   value &= stacked_layout_;
