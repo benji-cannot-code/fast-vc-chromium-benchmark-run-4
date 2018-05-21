8b92c87e478085ca1ee3b0a0ccdee0a8027a3625
Eugene But <eugenebut@google.com>
2018-05-21 16:35:54 +0000

    Add Download.IOSDownloadMimeType histogram.
    This histogram will help us to understand if Chrome for iOS should
    support application/x-apple-aspen-config downloads.
    
    Bug: 781770
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I4507c19ea7a11aa6bcb014228c7ed94781393acd
    Reviewed-on: https://chromium-review.googlesource.com/1058276
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560282}

--- ios/chrome/browser/download/browser_download_service.mm
+++ ios/chrome/browser/download/browser_download_service.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/download/browser_download_service.h"
 
 #include "base/feature_list.h"
+#include "base/metrics/histogram_macros.h"
 #import "ios/chrome/browser/download/download_manager_tab_helper.h"
 #include "ios/chrome/browser/download/pass_kit_mime_type.h"
 #import "ios/chrome/browser/download/pass_kit_tab_helper.h"
@@ -16,6 +17,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
+
+namespace {
+// Returns DownloadMimeTypeResult for the given MIME type.
+DownloadMimeTypeResult GetUmaResult(const std::string& mime_type) {
+  if (mime_type == kPkPassMimeType)
+    return DownloadMimeTypeResult::PkPass;
+
+  if (mime_type == "application/x-apple-aspen-config")
+    return DownloadMimeTypeResult::iOSMobileConfig;
+
+  return DownloadMimeTypeResult::Other;
+}
+}  // namespace
 
 BrowserDownloadService::BrowserDownloadService(
     web::DownloadController* download_controller)
@@ -35,6 +49,9 @@ void BrowserDownloadService::OnDownloadCreated(
     web::DownloadController* download_controller,
     web::WebState* web_state,
     std::unique_ptr<web::DownloadTask> task) {
+  UMA_HISTOGRAM_ENUMERATION("Download.IOSDownloadMimeType",
+                            GetUmaResult(task->GetMimeType()));
+
   if (task->GetMimeType() == kPkPassMimeType) {
     PassKitTabHelper* tab_helper = PassKitTabHelper::FromWebState(web_state);
     if (tab_helper) {
--- ios/chrome/browser/download/browser_download_service_unittest.mm
+++ ios/chrome/browser/download/browser_download_service_unittest.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/macros.h"
+#include "base/test/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "ios/chrome/browser/browser_state/test_chrome_browser_state.h"
 #import "ios/chrome/browser/download/download_manager_tab_helper.h"
@@ -105,6 +106,7 @@ class BrowserDownloadServiceTest : public PlatformTest {
   std::unique_ptr<BrowserDownloadService> service_;
   web::TestWebState web_state_;
   base::test::ScopedFeatureList feature_list_;
+  base::HistogramTester histogram_tester_;
 };
 
 // Tests that BrowserDownloadService downloads the task using
@@ -119,6 +121,10 @@ TEST_F(BrowserDownloadServiceTest, PkPassMimeType) {
   ASSERT_EQ(1U, pass_kit_tab_helper()->tasks().size());
   EXPECT_EQ(task_ptr, pass_kit_tab_helper()->tasks()[0].get());
   ASSERT_TRUE(download_manager_tab_helper()->tasks().empty());
+  histogram_tester_.ExpectUniqueSample(
+      "Download.IOSDownloadMimeType",
+      static_cast<base::HistogramBase::Sample>(DownloadMimeTypeResult::PkPass),
+      1);
 }
 
 // Tests that BrowserDownloadService uses DownloadManagerTabHelper for PDF Mime
@@ -133,4 +139,27 @@ TEST_F(BrowserDownloadServiceTest, PdfMimeType) {
   ASSERT_TRUE(pass_kit_tab_helper()->tasks().empty());
   ASSERT_EQ(1U, download_manager_tab_helper()->tasks().size());
   EXPECT_EQ(task_ptr, download_manager_tab_helper()->tasks()[0].get());
+  histogram_tester_.ExpectUniqueSample(
+      "Download.IOSDownloadMimeType",
+      static_cast<base::HistogramBase::Sample>(DownloadMimeTypeResult::Other),
+      1);
+}
+
+// Tests that BrowserDownloadService uses DownloadManagerTabHelper for Mobile
+// Config Mime Type.
+TEST_F(BrowserDownloadServiceTest, iOSMobileConfigMimeType) {
+  ASSERT_TRUE(download_controller()->GetDelegate());
+  auto task = std::make_unique<web::FakeDownloadTask>(
+      GURL(kUrl), "application/x-apple-aspen-config");
+  web::DownloadTask* task_ptr = task.get();
+  download_controller()->GetDelegate()->OnDownloadCreated(
+      download_controller(), &web_state_, std::move(task));
+  ASSERT_TRUE(pass_kit_tab_helper()->tasks().empty());
+  ASSERT_EQ(1U, download_manager_tab_helper()->tasks().size());
+  EXPECT_EQ(task_ptr, download_manager_tab_helper()->tasks()[0].get());
+  histogram_tester_.ExpectUniqueSample(
+      "Download.IOSDownloadMimeType",
+      static_cast<base::HistogramBase::Sample>(
+          DownloadMimeTypeResult::iOSMobileConfig),
+      1);
 }
