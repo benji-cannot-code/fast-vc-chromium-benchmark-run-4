3bc0019779c8c4dc721f665f55eccf6521e84087
John Abd-El-Malek <jam@chromium.org>
2018-05-21 23:47:33 +0000

    Fix BlockedSchemeNavigationBrowserTest.*/1 flake with network service on Windows.
    The file system writing would race with the reading, leading to tests timing out on Win Mojo bot. The fix is to wait on the file being written first.
    
    Bug: 729848
    Change-Id: If24fbe0b47ca8d62e54769374904bbc5e574ea6c
    Reviewed-on: https://chromium-review.googlesource.com/1068061
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560411}

--- content/browser/frame_host/blocked_scheme_navigation_browsertest.cc
+++ content/browser/frame_host/blocked_scheme_navigation_browsertest.cc
@@ -224,8 +224,10 @@ class BlockedSchemeNavigationBrowserTest
         "  fs.root.getFile('%s', {create: true}, entry => {"
         "    entry.createWriter(w => {"
         "      w.write(new Blob([contents], {type: '%s'}));"
+        "      w.onwrite = function(evt) {"
+        "        domAutomationController.send(entry.toURL());"
+        "      }"
         "    });"
-        "    domAutomationController.send(entry.toURL());"
         "  });"
         "});";
     std::string filesystem_url_string;
