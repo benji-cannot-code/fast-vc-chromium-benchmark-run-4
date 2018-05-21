54e6a087443c63f4eb9cc9348a58a56e889cac24
Peter Boström <pbos@chromium.org>
2018-05-21 23:06:52 +0000

    Narrow extension-uninstall dialog as a bubble
    This dialog can be rendered both as a modal dialog and a popover, the
    size of it needs to reflect both visual styles and use the appropriate
    width constant.
    
    Bug: chromium:843624
    Change-Id: I2c69a1704bb6502d55115078486691930bc9d74e
    Reviewed-on: https://chromium-review.googlesource.com/1062910
    Commit-Queue: Bret Sepulveda <bsep@chromium.org>
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560395}

--- chrome/browser/ui/views/extensions/extension_uninstall_dialog_view.cc
+++ chrome/browser/ui/views/extensions/extension_uninstall_dialog_view.cc
@@ -118,6 +118,7 @@ class ExtensionUninstallDialogDelegateView
 
   ExtensionUninstallDialogViews* dialog_;
   const base::string16 extension_name_;
+  const bool is_bubble_;
 
   views::Label* heading_;
   views::Checkbox* report_abuse_checkbox_;
@@ -188,6 +189,7 @@ ExtensionUninstallDialogDelegateView::ExtensionUninstallDialogDelegateView(
                                            : views::BubbleBorder::NONE),
       dialog_(dialog_view),
       extension_name_(base::UTF8ToUTF16(extension->name())),
+      is_bubble_(anchor_view != nullptr),
       report_abuse_checkbox_(nullptr),
       image_(gfx::ImageSkiaOperations::CreateResizedImage(
           *image,
@@ -278,7 +280,8 @@ bool ExtensionUninstallDialogDelegateView::Cancel() {
 
 gfx::Size ExtensionUninstallDialogDelegateView::CalculatePreferredSize() const {
   const int width = ChromeLayoutProvider::Get()->GetDistanceMetric(
-                        DISTANCE_MODAL_DIALOG_PREFERRED_WIDTH) -
+                        is_bubble_ ? DISTANCE_BUBBLE_PREFERRED_WIDTH
+                                   : DISTANCE_MODAL_DIALOG_PREFERRED_WIDTH) -
                     margins().width();
   return gfx::Size(width, GetHeightForWidth(width));
 }
