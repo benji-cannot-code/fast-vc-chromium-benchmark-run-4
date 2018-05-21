0bb58881d48e3abd906193dd1d150d8f85d6c588
Steve Kobes <skobes@chromium.org>
2018-05-21 14:22:42 +0000

    Add Region::Area.
    This will be used to measure layout jank regions.
    
    Also add some documentation of Region::Shape, and move comments about
    Region into the header.
    
    Bug: 581518
    Change-Id: I54365b4fa544210060cc8fa59f4d4af820bfcb09
    Reviewed-on: https://chromium-review.googlesource.com/1055597
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560265}

--- third_party/blink/renderer/platform/geometry/region.cc
+++ third_party/blink/renderer/platform/geometry/region.cc
@@ -28,12 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdio.h>
 
-// A region class based on the paper "Scanline Coherent Shape Algebra"
-// by Jonathan E. Steinhart from the book "Graphics Gems II".
-//
-// This implementation uses two vectors instead of linked list, and
-// also compresses regions when possible.
-
 namespace blink {
 
 Region::Region() = default;
@@ -105,6 +99,22 @@ bool Region::Intersects(const Region& region) const {
 
   return Shape::CompareShapes<Shape::CompareIntersectsOperation>(shape_,
                                                                  region.shape_);
+}
+
+double Region::Area() const {
+  double area = 0.0;
+  for (Shape::SpanIterator span = shape_.SpansBegin(), end = shape_.SpansEnd();
+       span != end && span + 1 != end; ++span) {
+    int height = (span + 1)->y - span->y;
+
+    for (Shape::SegmentIterator segment = shape_.SegmentsBegin(span),
+                                end = shape_.SegmentsEnd(span);
+         segment != end && segment + 1 != end; segment += 2) {
+      int width = *(segment + 1) - *segment;
+      area += height * width;
+    }
+  }
+  return area;
 }
 
 template <typename CompareOperation>
