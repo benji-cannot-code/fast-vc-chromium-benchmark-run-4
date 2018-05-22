a5679087cd4ae6300daf18f73eb237cba80af542
Noel Gordon <noel@chromium.org>
2018-05-22 04:21:00 +0000

    Re-enable CopyBetweenWindows browser test in RELEASE and DEBUG
    Test failed since it clicked the USB volume but did not check that the
    correct entries were displayed (often the Drive entries were) [1].
    
    Use the new "mountFakeUsbEmpty" command to better control the USB file
    volume entries during test. Verify the expected entries are shown when
    the USB volume is clicked, prior to a "copy between windows".
    
    In the C++, add LOG(FATAL) to detect if a USB addEntries request would
    be dropped on the floor (JS test called AddEntries before Mount).
    
    Also, spin the run loop when creating or mounting removable entries to
    mitigate (not fix) the potential for bot TIMEOUT under high load.
    
    Start running these tests in RELEASE/DEBUG again to gather new data on
    their speed and robustness on these bots.
    
    [1] So these test got disabled even in RELEASE, sadly.
    
    Test: browser_test --gtest_filter="CopyBetweenWindows/FilesApp*"
    Bug: 845087
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I6aea094c597a5595477b8801bc9641f5b4744694
    Reviewed-on: https://chromium-review.googlesource.com/1066255
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560479}

--- chrome/browser/chromeos/file_manager/file_manager_browsertest_base.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest_base.cc
@@ -373,6 +373,7 @@ class FakeTestVolume : public LocalTestVolume {
                               SharedOption::NONE, base::Time::Now()));
     CreateEntry(TestEntryInfo(DIRECTORY, std::string(), "A", std::string(),
                               SharedOption::NONE, base::Time::Now()));
+    base::RunLoop().RunUntilIdle();
     return true;
   }
 
@@ -391,6 +392,7 @@ class FakeTestVolume : public LocalTestVolume {
     // Expose the mount point with the given volume and device type.
     VolumeManager::Get(profile)->AddVolumeForTesting(root_path(), volume_type_,
                                                      device_type_, read_only_);
+    base::RunLoop().RunUntilIdle();
     return true;
   }
 
@@ -753,12 +755,18 @@ void FileManagerBrowserTestBase::OnCommand(const std::string& name,
           local_volume_->CreateEntry(*message.entries[i]);
           break;
         case DRIVE_VOLUME:
-          if (drive_volume_)
+          if (drive_volume_) {
             drive_volume_->CreateEntry(*message.entries[i]);
+          } else if (!IsGuestModeTest()) {
+            LOG(FATAL) << "Add entry: but no Drive volume.";
+          }
           break;
         case USB_VOLUME:
-          if (usb_volume_)
+          if (usb_volume_) {
             usb_volume_->CreateEntry(*message.entries[i]);
+          } else {
+            LOG(FATAL) << "Add entry: but no USB volume.";
+          }
           break;
       }
     }
