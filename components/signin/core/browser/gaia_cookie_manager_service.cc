8b87dc45003716c87626e67c27252ad1b36d1357
Kush Sinha <sinhak@chromium.org>
2018-05-22 11:58:34 +0000

    Dead code removal: Remove (anon)::GaiaCookieRequestType
    There is another enum with the same name at
    |GaiaCookieManagerService::GaiaCookieRequestType| which is actually
    used everywhere. Presence of the unused anon namespaced enum causes
    confusion.
    
    Change-Id: I230e86346fed5f59a20d6349c2e9b72746bfca9c
    Reviewed-on: https://chromium-review.googlesource.com/1068936
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560538}

--- components/signin/core/browser/gaia_cookie_manager_service.cc
+++ components/signin/core/browser/gaia_cookie_manager_service.cc
@@ -67,13 +67,6 @@ const int kMaxFetcherRetries = 8;
 // accounts have changed in the content-area.
 const char* const kGaiaCookieName = "APISID";
 
-enum GaiaCookieRequestType {
-  ADD_ACCOUNT,
-  LOG_OUT_ALL_ACCOUNTS,
-  LOG_OUT_ONE_ACCOUNT,
-  LIST_ACCOUNTS
-};
-
 }  // namespace
 
 GaiaCookieManagerService::GaiaCookieRequest::GaiaCookieRequest(
@@ -91,8 +84,7 @@ GaiaCookieManagerService::GaiaCookieRequest::CreateAddAccountRequest(
     const std::string& account_id,
     const std::string& source) {
   return GaiaCookieManagerService::GaiaCookieRequest(
-      GaiaCookieManagerService::GaiaCookieRequestType::ADD_ACCOUNT, account_id,
-      source);
+      GaiaCookieRequestType::ADD_ACCOUNT, account_id, source);
 }
 
 // static
@@ -100,8 +92,7 @@ GaiaCookieManagerService::GaiaCookieRequest
 GaiaCookieManagerService::GaiaCookieRequest::CreateLogOutRequest(
     const std::string& source) {
   return GaiaCookieManagerService::GaiaCookieRequest(
-      GaiaCookieManagerService::GaiaCookieRequestType::LOG_OUT, std::string(),
-      source);
+      GaiaCookieRequestType::LOG_OUT, std::string(), source);
 }
 
 // static
@@ -109,8 +100,7 @@ GaiaCookieManagerService::GaiaCookieRequest
 GaiaCookieManagerService::GaiaCookieRequest::CreateListAccountsRequest(
     const std::string& source) {
   return GaiaCookieManagerService::GaiaCookieRequest(
-      GaiaCookieManagerService::GaiaCookieRequestType::LIST_ACCOUNTS,
-      std::string(), source);
+      GaiaCookieRequestType::LIST_ACCOUNTS, std::string(), source);
 }
 
 GaiaCookieManagerService::ExternalCcResultFetcher::ExternalCcResultFetcher(
