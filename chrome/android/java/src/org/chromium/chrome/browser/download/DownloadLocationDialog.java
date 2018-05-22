8b11b3764355f9b76424783d4cc9c5b1183a0873
Xing Liu <xingliu@chromium.org>
2018-05-22 19:53:02 +0000

    Download location: Change download location preference UI.
    Previously we reused the spinner code in download location dialog, this
    CL implements new UI in download location preference to use a custom
    layout with radio button and text views.
    
    We reuses the adapter to provide the underlying data used by the UI.
    
    Bug: 842464,792775
    Change-Id: Iaf68fa3e75d259c9c6e6fd6d79caa1845aaf18b3
    Reviewed-on: https://chromium-review.googlesource.com/1062785
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560737}

--- chrome/android/java/src/org/chromium/chrome/browser/download/DownloadLocationDialog.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/DownloadLocationDialog.java
@@ -129,9 +129,8 @@ public class DownloadLocationDialog extends ModalDialogView {
     @Nullable
     File getFileLocation() {
         if (mFileLocation == null) return null;
-        DownloadDirectoryAdapter.DirectoryOption selected =
-                (DownloadDirectoryAdapter.DirectoryOption) mFileLocation.getSelectedItem();
-        return selected.getLocation();
+        DirectoryOption selected = (DirectoryOption) mFileLocation.getSelectedItem();
+        return selected.location;
     }
 
     /**
--- chrome/android/java/src/org/chromium/chrome/browser/preferences/download/DownloadDirectoryAdapter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/download/DownloadDirectoryAdapter.java
@@ -19,6 +19,7 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.download.DirectoryOption;
 import org.chromium.chrome.browser.download.DownloadUtils;
 import org.chromium.chrome.browser.preferences.PrefServiceBridge;
 import org.chromium.chrome.browser.widget.TintedImageView;
@@ -33,42 +34,6 @@ import java.util.List;
  */
 public class DownloadDirectoryAdapter extends ArrayAdapter<Object> {
     public static int NO_SELECTED_ITEM_ID = -1;
-
-    /**
-     * Denotes a given option for directory selection; includes name, location, and space.
-     */
-    public static class DirectoryOption {
-        private final String mName;
-        private final File mLocation;
-        private final long mAvailableSpace;
-
-        DirectoryOption(String directoryName, File directoryLocation, long availableSpace) {
-            mName = directoryName;
-            mLocation = directoryLocation;
-            mAvailableSpace = availableSpace;
-        }
-
-        /**
-         * @return File location associated with this directory option.
-         */
-        public File getLocation() {
-            return mLocation;
-        }
-
-        /**
-         * @return Name associated with this directory option.
-         */
-        public String getName() {
-            return mName;
-        }
-
-        /**
-         * @return Amount of available space in this directory option.
-         */
-        long getAvailableSpace() {
-            return mAvailableSpace;
-        }
-    }
 
     private Context mContext;
     private LayoutInflater mLayoutInflater;
@@ -126,7 +91,7 @@ public class DownloadDirectoryAdapter extends ArrayAdapter<Object> {
         if (directoryOption == null) return view;
 
         TextView titleText = (TextView) view.findViewById(R.id.text);
-        titleText.setText(directoryOption.getName());
+        titleText.setText(directoryOption.name);
 
         // ModalDialogView may do a measure pass on the view hierarchy to limit the layout inside
         // certain area, where LayoutParams cannot be null.
@@ -151,14 +116,14 @@ public class DownloadDirectoryAdapter extends ArrayAdapter<Object> {
         if (directoryOption == null) return view;
 
         TextView titleText = (TextView) view.findViewById(R.id.title);
-        titleText.setText(directoryOption.getName());
+        titleText.setText(directoryOption.name);
 
         TextView summaryText = (TextView) view.findViewById(R.id.description);
         if (isEnabled(position)) {
             TextViewCompat.setTextAppearance(titleText, R.style.BlackTitle1);
             TextViewCompat.setTextAppearance(summaryText, R.style.BlackBody);
             summaryText.setText(DownloadUtils.getStringForAvailableBytes(
-                    mContext, directoryOption.getAvailableSpace()));
+                    mContext, directoryOption.availableSpace));
         } else {
             TextViewCompat.setTextAppearance(titleText, R.style.BlackDisabledText1);
             TextViewCompat.setTextAppearance(summaryText, R.style.BlackDisabledText3);
@@ -178,7 +143,7 @@ public class DownloadDirectoryAdapter extends ArrayAdapter<Object> {
     @Override
     public boolean isEnabled(int position) {
         DirectoryOption directoryOption = (DirectoryOption) getItem(position);
-        return directoryOption != null && directoryOption.getAvailableSpace() != 0;
+        return directoryOption != null && directoryOption.availableSpace != 0;
     }
 
     /**
@@ -191,7 +156,7 @@ public class DownloadDirectoryAdapter extends ArrayAdapter<Object> {
         for (int i = 0; i < getCount(); i++) {
             DirectoryOption option = (DirectoryOption) getItem(i);
             if (option == null) continue;
-            if (defaultLocation.equals(option.getLocation().getAbsolutePath())) return i;
+            if (defaultLocation.equals(option.location.getAbsolutePath())) return i;
         }
         return NO_SELECTED_ITEM_ID;
     }
@@ -207,9 +172,9 @@ public class DownloadDirectoryAdapter extends ArrayAdapter<Object> {
         for (int i = 0; i < getCount(); i++) {
             DirectoryOption option = (DirectoryOption) getItem(i);
             if (option == null) continue;
-            if (option.getAvailableSpace() > 0) {
+            if (option.availableSpace > 0) {
                 PrefServiceBridge.getInstance().setDownloadAndSaveFileDefaultDirectory(
-                        option.getLocation().getAbsolutePath());
+                        option.location.getAbsolutePath());
                 return i;
             }
         }
