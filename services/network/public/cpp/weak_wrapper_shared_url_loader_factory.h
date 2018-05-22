c4e44210811a396a70924d3756dfdfd90f949c5a
Maks Orlovich <morlovich@chromium.org>
2018-05-22 00:06:51 +0000

    Add a lazy mode to WeakWrapperSharedURLLoaderFactory
    This aids in porting code that passes around URLRequestContextGetter a lot,
    as that has implementations that don't do any work until things are actually
    needed, so an eager wrapper (very relevant to iOS) around those may end up
    requiring a bunch of extra things to be set up in a test, or delaying look
    up of the SharedURLLoaderFactory, which requires more reasoning about
    lifetime than seems like a good idea for API migration work.
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I57ce1fb42881fc7fae12f922cdeddb614116ba56
    Reviewed-on: https://chromium-review.googlesource.com/1066572
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560424}

--- services/network/public/cpp/weak_wrapper_shared_url_loader_factory.h
+++ services/network/public/cpp/weak_wrapper_shared_url_loader_factory.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SERVICES_NETWORK_PUBLIC_CPP_WEAK_WRAPPER_SHARED_URL_LOADER_FACTORY_H_
 #define SERVICES_NETWORK_PUBLIC_CPP_WEAK_WRAPPER_SHARED_URL_LOADER_FACTORY_H_
 
+#include "base/callback.h"
 #include "base/component_export.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "services/network/public/mojom/url_loader_factory.mojom.h"
@@ -19,6 +20,12 @@ class COMPONENT_EXPORT(NETWORK_CPP) WeakWrapperSharedURLLoaderFactory
  public:
   explicit WeakWrapperSharedURLLoaderFactory(
       network::mojom::URLLoaderFactory* factory_ptr);
+
+  // A lazy variant. This is useful when transitionning code that sets up
+  // heavy-weight infrastructure, injects a shared_ptr<SharedURLLoaderFactory>
+  // into lots of places, but doesn't actually use it.
+  explicit WeakWrapperSharedURLLoaderFactory(
+      base::OnceCallback<network::mojom::URLLoaderFactory*()> make_factory_ptr);
 
   // Detaches from the raw mojom::URLLoaderFactory pointer. All subsequent calls
   // to CreateLoaderAndStart() will fail silently.
@@ -38,6 +45,11 @@ class COMPONENT_EXPORT(NETWORK_CPP) WeakWrapperSharedURLLoaderFactory
 
  private:
   ~WeakWrapperSharedURLLoaderFactory() override;
+
+  // Uses whichever of make_factory_ptr_ or factory_ptr_ is relevant.
+  network::mojom::URLLoaderFactory* factory();
+
+  base::OnceCallback<network::mojom::URLLoaderFactory*()> make_factory_ptr_;
 
   // Not owned.
   network::mojom::URLLoaderFactory* factory_ptr_ = nullptr;
