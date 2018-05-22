9a8411dc431042880e2231ffc389cfcf2601fe8d
Patrick Monette <pmonette@chromium.org>
2018-05-22 19:28:46 +0000

    Remove WeakPtrFactory from ModuleDatabase
    That factory was only used for the thread-bouncing in OnModuleLoad(),
    which is unnecessary given that the ModuleDatabase is a singleton that
    never gets freed.
    
    This fixes a crash where WeakPtrFactory::GetWeakPtr() was called from
    multiple threads concurrently, even though it isn't thread-safe.
    
    Bug: 845110
    Change-Id: I071ab52d8938922c93eeac6adc4d592669d15ae7
    Reviewed-on: https://chromium-review.googlesource.com/1069328
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Commit-Queue: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560729}

--- chrome/browser/conflicts/module_database_win.h
+++ chrome/browser/conflicts/module_database_win.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/memory/ref_counted.h"
-#include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
 #include "base/sequenced_task_runner.h"
 #include "base/time/time.h"
@@ -212,10 +211,6 @@ class ModuleDatabase {
 
   // Records metrics on third-party modules.
   ThirdPartyMetricsRecorder third_party_metrics_;
-
-  // Weak pointer factory for this object. This is used when bouncing
-  // incoming events to |task_runner_|.
-  base::WeakPtrFactory<ModuleDatabase> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(ModuleDatabase);
 };
