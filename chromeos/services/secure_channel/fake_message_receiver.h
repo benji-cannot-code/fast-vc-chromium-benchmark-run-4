83667e729d21935993255d7ab28e56af2d216140
Kyle Horimoto <khorimoto@google.com>
2018-05-22 00:24:08 +0000

    [CrOS MultiDevice] Add SecureChannel message routing.
    This CL creates SingleClientMessageProxy, which proxies both incoming
    and outgoing messages to/from clients, filtering by feature.
    
    Bug: 824568, 752273
    Change-Id: I3f97c40299245956028b5e7e412863e53fc2305f
    Reviewed-on: https://chromium-review.googlesource.com/1063254
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560431}

---
+++ chromeos/services/secure_channel/fake_message_receiver.h
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_SERVICES_SECURE_CHANNEL_FAKE_MESSAGE_RECEIVER_H_
+#define CHROMEOS_SERVICES_SECURE_CHANNEL_FAKE_MESSAGE_RECEIVER_H_
+
+#include <string>
+#include <vector>
+
+#include "base/macros.h"
+#include "chromeos/services/secure_channel/public/mojom/secure_channel.mojom.h"
+
+namespace chromeos {
+
+namespace secure_channel {
+
+// Test MessageReceiver implementation.
+class FakeMessageReceiver : public mojom::MessageReceiver {
+ public:
+  FakeMessageReceiver();
+  ~FakeMessageReceiver() override;
+
+  const std::vector<std::string>& received_messages() {
+    return received_messages_;
+  }
+
+ private:
+  // mojom::MessageReceiver:
+  void OnMessageReceived(const std::string& message) override;
+
+  std::vector<std::string> received_messages_;
+
+  DISALLOW_COPY_AND_ASSIGN(FakeMessageReceiver);
+};
+
+}  // namespace secure_channel
+
+}  // namespace chromeos
+
+#endif  // CHROMEOS_SERVICES_SECURE_CHANNEL_FAKE_MESSAGE_RECEIVER_H_
