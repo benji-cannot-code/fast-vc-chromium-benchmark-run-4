f881259a9dabfe21ff1f08047cf18a51752e96bd
Scott Violet <sky@chromium.org>
2018-05-22 17:38:51 +0000

    chromeos: make sure state is reset at appropriate times
    The EventHandler used to forward events to the right client maintains
    state that needs to be reset in certain cases, as otherwise it won't
    properly clean up the state. In particular if capture or visibility
    changes then EventHandler no longer gets the events.
    
    BUG=837692
    TEST=covered by test
    
    Change-Id: I560960ea93462f68c2e252dbd1b5dc9c581ff886
    Reviewed-on: https://chromium-review.googlesource.com/1067883
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560685}

---
+++ services/ui/ws2/client_window_test_helper.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/ui/ws2/client_window_test_helper.h"
+
+#include "services/ui/ws2/client_window.h"
+
+namespace ui {
+namespace ws2 {
+
+ClientWindowTestHelper::ClientWindowTestHelper(ClientWindow* client_window)
+    : client_window_(client_window) {}
+
+ClientWindowTestHelper::~ClientWindowTestHelper() = default;
+
+bool ClientWindowTestHelper::IsInPointerPressed() {
+  return client_window_->IsInPointerPressedForTesting();
+}
+
+}  // namespace ws2
+}  // namespace ui
