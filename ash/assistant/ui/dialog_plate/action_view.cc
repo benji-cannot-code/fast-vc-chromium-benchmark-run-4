802f08fd0c63d185428f24dde532958235eeb8a1
wutao <wutao@chromium.org>
2018-05-22 17:36:59 +0000

    LogoView: Rename stub LogoView class to StubLogoView
    This cl renames the stub LogoView class to StubLogoView in order to add
    the real LogoView class.
    
    Bug: b/79999539
    Test: manual.
    Change-Id: Ie606143eb73e20f4c44e67f43b5cd132a055f39b
    Reviewed-on: https://chromium-review.googlesource.com/1067879
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560683}

--- ash/assistant/ui/dialog_plate/action_view.cc
+++ ash/assistant/ui/dialog_plate/action_view.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "ash/assistant/assistant_controller.h"
-#include "ash/assistant/ui/dialog_plate/logo_view.h"
+#include "ash/assistant/ui/dialog_plate/stub_logo_view.h"
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ui/gfx/color_palette.h"
 #include "ui/gfx/paint_vector_icon.h"
@@ -29,7 +29,7 @@ ActionView::ActionView(AssistantController* assistant_controller,
     : assistant_controller_(assistant_controller),
       listener_(listener),
       keyboard_action_view_(new views::ImageView()),
-      voice_action_view_(new LogoView()) {
+      voice_action_view_(new StubLogoView()) {
   InitLayout();
   UpdateState();
 
@@ -106,10 +106,10 @@ void ActionView::UpdateState() {
 
   switch (interaction_model->mic_state()) {
     case MicState::kClosed:
-      voice_action_view_->SetState(LogoView::State::kMic);
+      voice_action_view_->SetState(StubLogoView::State::kMic);
       break;
     case MicState::kOpen:
-      voice_action_view_->SetState(LogoView::State::kListening);
+      voice_action_view_->SetState(StubLogoView::State::kListening);
       break;
   }
 
--- ash/assistant/ui/dialog_plate/stub_logo_view.h
+++ ash/assistant/ui/dialog_plate/stub_logo_view.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef ASH_ASSISTANT_UI_DIALOG_PLATE_LOGO_VIEW_H_
-#define ASH_ASSISTANT_UI_DIALOG_PLATE_LOGO_VIEW_H_
+#ifndef ASH_ASSISTANT_UI_DIALOG_PLATE_STUB_LOGO_VIEW_H_
+#define ASH_ASSISTANT_UI_DIALOG_PLATE_STUB_LOGO_VIEW_H_
 
 #include "base/macros.h"
 #include "ui/views/controls/image_view.h"
@@ -13,7 +13,7 @@ namespace ash {
 
 // TODO(b/78191547): Remove this class once the actual LogoView library port
 // has been completed. This class is only a temporary placeholder.
-class LogoView : public views::ImageView {
+class StubLogoView : public views::ImageView {
  public:
   enum class State {
     kUndefined,
@@ -21,15 +21,16 @@ class LogoView : public views::ImageView {
     kMic,
   };
 
-  LogoView();
-  ~LogoView() override;
+  StubLogoView() = default;
+
+  ~StubLogoView() override = default;
 
   void SetState(State state);
 
  private:
-  DISALLOW_COPY_AND_ASSIGN(LogoView);
+  DISALLOW_COPY_AND_ASSIGN(StubLogoView);
 };
 
 }  // namespace ash
 
-#endif  // ASH_ASSISTANT_UI_DIALOG_PLATE_LOGO_VIEW_H_
+#endif  // ASH_ASSISTANT_UI_DIALOG_PLATE_STUB_LOGO_VIEW_H_
