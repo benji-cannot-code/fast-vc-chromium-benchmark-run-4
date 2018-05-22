b65c4fa58c3528ef729357d7d221e57f0a2fde9a
Chris Morin <cmtm@google.com>
2018-05-22 21:01:37 +0000

    cros: remove code duplication between ArcProcess and ArcProcessTask
    ArcProcessTask contains a subset of the members in ArcProcess. It
    also contains similar logic with regards to determining which processes
    are killable from the task manager. This change has ArcProcessTask
    contain an ArcProcess instead of duplicating some of it's members, and
    delegates the logic of whether a process is killable to the ArcProcess
    instance. This causes a small change in which processes can be killed in
    the in the task manager: processes protected by the function
    ArcProcess::IsArcProtected can no longer be killed. This change is
    considered an improvement by the author of the IsArcProtected function.
    
    BUG=none
    TEST=Ensure task manager can still kill right processes.
    
    Change-Id: Id483b22e3a25f6fe8bbeeee8cddddcf0b5dbe298
    Reviewed-on: https://chromium-review.googlesource.com/1068615
    Commit-Queue: Christopher Morin <cmtm@google.com>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560771}

--- chrome/browser/task_manager/providers/arc/arc_process_task.h
+++ chrome/browser/task_manager/providers/arc/arc_process_task.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
+#include "chrome/browser/chromeos/arc/process/arc_process.h"
 #include "chrome/browser/task_manager/providers/task.h"
 #include "components/arc/common/intent_helper.mojom.h"
 #include "components/arc/common/process.mojom.h"
@@ -25,11 +26,7 @@ class ArcProcessTask
     : public Task,
       public arc::ConnectionObserver<arc::mojom::IntentHelperInstance> {
  public:
-  ArcProcessTask(base::ProcessId pid,
-                 base::ProcessId nspid,
-                 const std::string& process_name,
-                 arc::mojom::ProcessState process_state,
-                 const std::vector<std::string>& packages);
+  explicit ArcProcessTask(arc::ArcProcess arc_process);
   ~ArcProcessTask() override;
 
   // task_manager::Task:
@@ -43,18 +40,17 @@ class ArcProcessTask
 
   void SetProcessState(arc::mojom::ProcessState process_state);
 
-  base::ProcessId nspid() const { return nspid_; }
-  const std::string& process_name() const { return process_name_; }
+  base::ProcessId nspid() const { return arc_process_.nspid(); }
+  const std::string& process_name() const {
+    return arc_process_.process_name();
+  }
 
  private:
   void StartIconLoading();
   void OnIconLoaded(
       std::unique_ptr<arc::ArcIntentHelperBridge::ActivityToIconsMap> icons);
 
-  const base::ProcessId nspid_;
-  const std::string process_name_;
-  arc::mojom::ProcessState process_state_;
-  const std::string package_name_;
+  arc::ArcProcess arc_process_;
 
   // Note: This should remain the last member so it'll be destroyed and
   // invalidate the weak pointers before any other members are destroyed.
