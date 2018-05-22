18372e8f300b983ae897df7ac2b7cd48b07f740a
Rayan Kanso <rayankans@chromium.org>
2018-05-22 15:01:37 +0000

    [Background Fetch] Provide num completed requests to Job Controller
    Bug: 843683
    Change-Id: I20e9aa4e379d5fbc8ec50bb50b7919e4944d8c8c
    Reviewed-on: https://chromium-review.googlesource.com/1062266
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560587}

--- content/browser/background_fetch/background_fetch_data_manager.h
+++ content/browser/background_fetch/background_fetch_data_manager.h
@@ -64,6 +64,7 @@ class CONTENT_EXPORT BackgroundFetchDataManager
                               std::unique_ptr<BackgroundFetchRegistration>)>;
   using NextRequestCallback =
       base::OnceCallback<void(scoped_refptr<BackgroundFetchRequestInfo>)>;
+  using NumRequestsCallback = base::OnceCallback<void(size_t)>;
 
   BackgroundFetchDataManager(
       BrowserContext* browser_context,
@@ -134,9 +135,16 @@ class CONTENT_EXPORT BackgroundFetchDataManager
       const url::Origin& origin,
       blink::mojom::BackgroundFetchService::GetDeveloperIdsCallback callback);
 
+  // Gets the number of fetch requests that have been completed for a given
+  // registration.
+  void GetNumCompletedRequests(
+      const BackgroundFetchRegistrationId& registration_id,
+      NumRequestsCallback callback);
+
   // BackgroundFetchScheduler::RequestProvider implementation:
   void PopNextRequest(const BackgroundFetchRegistrationId& registration_id,
                       NextRequestCallback callback) override;
+
   void MarkRequestAsComplete(
       const BackgroundFetchRegistrationId& registration_id,
       BackgroundFetchRequestInfo* request,
