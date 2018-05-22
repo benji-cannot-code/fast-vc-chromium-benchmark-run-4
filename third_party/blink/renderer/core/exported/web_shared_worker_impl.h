2f144e2e4d19baffc40b8c7c824836ab5910c491
Matt Falkenhagen <falken@chromium.org>
2018-05-22 04:26:40 +0000

    Remove unused WebSharedWorkerImpl::network_provider_.
    Change-Id: Ic6b66b9ad0a3c8a27d20d4126c0c367c8eee819a
    Reviewed-on: https://chromium-review.googlesource.com/1067558
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560481}

--- third_party/blink/renderer/core/exported/web_shared_worker_impl.h
+++ third_party/blink/renderer/core/exported/web_shared_worker_impl.h
@@ -53,7 +53,6 @@ namespace blink {
 
 class WebApplicationCacheHost;
 class WebApplicationCacheHostClient;
-class WebServiceWorkerNetworkProvider;
 class WebSharedWorkerClient;
 class WebString;
 class WebURL;
@@ -120,8 +119,6 @@ class CORE_EXPORT WebSharedWorkerImpl final : public WebSharedWorker,
   // Unique worker token used by DevTools to attribute different instrumentation
   // to the same worker.
   base::UnguessableToken devtools_worker_token_;
-
-  std::unique_ptr<WebServiceWorkerNetworkProvider> network_provider_;
 
   Persistent<WorkerInspectorProxy> worker_inspector_proxy_;
 
