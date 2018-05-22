a7fe018931aec92ab581d52b08be3882c3e1d505
Dave Schuyler <dschuyler@chromium.org>
2018-05-22 21:54:12 +0000

    [Omnibox] show alternate answers images
    This CL shows answer images that are delivered through the alternate json property
    
    Bug: 843370
    Change-Id: Ic0a2088b16b5f2d00944719c46c5ff1724d7cc90
    Reviewed-on: https://chromium-review.googlesource.com/1066973
    Commit-Queue: Dave Schuyler <dschuyler@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560796}

--- components/omnibox/browser/suggestion_answer_unittest.cc
+++ components/omnibox/browser/suggestion_answer_unittest.cc
@@ -10,7 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/json/json_reader.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/values.h"
+#include "components/omnibox/browser/omnibox_field_trial.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace {
@@ -262,6 +264,23 @@ TEST(SuggestionAnswerTest, AddImageURLsTo) {
   answer->AddImageURLsTo(&urls);
   ASSERT_EQ(0U, urls.size());
 
+  {
+    base::test::ScopedFeatureList feature_list;
+    feature_list.InitAndEnableFeature(omnibox::kOmniboxNewAnswerLayout);
+    json =
+        "{ \"i\": { \"d\": \"https://gstatic.com/foo.png\", \"t\": 3 },"
+        "  \"l\" : ["
+        "    { \"il\": { \"t\": [{ \"t\": \"some text\", \"tt\": 5 }] } },"
+        "    { \"il\": { \"t\": [{ \"t\": \"other text\", \"tt\": 8 }] } }"
+        "  ]}";
+    answer = ParseAnswer(json);
+    ASSERT_TRUE(answer);
+    answer->AddImageURLsTo(&urls);
+    ASSERT_EQ(1U, urls.size());
+    EXPECT_EQ(GURL("https://gstatic.com/foo.png"), urls[0]);
+    urls.clear();
+  }
+
   json =
       "{ \"l\" : ["
       "  { \"il\": { \"t\": [{ \"t\": \"some text\", \"tt\": 5 }] } },"
@@ -272,6 +291,21 @@ TEST(SuggestionAnswerTest, AddImageURLsTo) {
   answer->AddImageURLsTo(&urls);
   ASSERT_EQ(1U, urls.size());
   EXPECT_EQ(GURL("https://gstatic.com/foo.png"), urls[0]);
+  urls.clear();
+
+  json =
+      "{ \"i\": { \"d\": \"https://gstatic.com/foo.png\", \"t\": 3 },"
+      "  \"l\" : ["
+      "    { \"il\": { \"t\": [{ \"t\": \"some text\", \"tt\": 5 }] } },"
+      "    { \"il\": { \"t\": [{ \"t\": \"other text\", \"tt\": 8 }],"
+      "              \"i\": { \"d\": \"//gstatic.com/bar.png\", \"t\": 3 }}}"
+      "  ]}";
+  answer = ParseAnswer(json);
+  ASSERT_TRUE(answer);
+  answer->AddImageURLsTo(&urls);
+  ASSERT_EQ(1U, urls.size());
+  EXPECT_EQ(GURL("https://gstatic.com/bar.png"), urls[0]);
+  urls.clear();
 
   json =
       "{ \"l\" : ["
@@ -282,7 +316,7 @@ TEST(SuggestionAnswerTest, AddImageURLsTo) {
   answer = ParseAnswer(json);
   ASSERT_TRUE(answer);
   answer->AddImageURLsTo(&urls);
-  ASSERT_EQ(3U, urls.size());
-  EXPECT_EQ(GURL("https://gstatic.com/foo.png"), urls[1]);
-  EXPECT_EQ(GURL("https://gstatic.com/bar.jpg"), urls[2]);
+  ASSERT_EQ(1U, urls.size());
+  // Note: first_line_.image_url() is not used in practice (so it's ignored).
+  EXPECT_EQ(GURL("https://gstatic.com/bar.jpg"), urls[0]);
 }
