846d252e0c52d3721a26ac1803233e741be242e1
Mounir Lamouri <mlamouri@chromium.org>
2018-05-22 23:19:08 +0000

    Picture-in-Picture: move UpdatePictureInPictureSurfaceId to //content and IPC.
    Before this change, the logic was in //chrome but would only rely on //content
    concepts. Keeping it in //content reduces the level of plumbing. It's also
    moving from frame.mojom to the media IPCs to be around the other PIP calls.
    
    Bug: None
    Change-Id: I401126871b48feaf551001ebdfc2f0f319ec75e0
    Reviewed-on: https://chromium-review.googlesource.com/1065823
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Reviewed-by: apacible <apacible@chromium.org>
    Commit-Queue: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560830}

--- content/browser/media/media_web_contents_observer.cc
+++ content/browser/media/media_web_contents_observer.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "content/browser/media/audible_metrics.h"
 #include "content/browser/media/audio_stream_monitor.h"
+#include "content/browser/picture_in_picture/picture_in_picture_window_controller_impl.h"
 #include "content/browser/web_contents/web_contents_impl.h"
 #include "content/common/media/media_player_delegate_messages.h"
 #include "content/public/browser/render_frame_host.h"
@@ -139,6 +140,9 @@ bool MediaWebContentsObserver::OnMessageReceived(
         OnPictureInPictureModeStarted)
     IPC_MESSAGE_HANDLER(MediaPlayerDelegateHostMsg_OnPictureInPictureModeEnded,
                         OnPictureInPictureModeEnded)
+    IPC_MESSAGE_HANDLER(
+        MediaPlayerDelegateHostMsg_OnPictureInPictureSurfaceChanged,
+        OnPictureInPictureSurfaceChanged)
     IPC_MESSAGE_UNHANDLED(handled = false)
   IPC_END_MESSAGE_MAP()
   return handled;
@@ -314,6 +318,22 @@ void MediaWebContentsObserver::OnPictureInPictureModeEnded(
   render_frame_host->Send(
       new MediaPlayerDelegateMsg_OnPictureInPictureModeEnded_ACK(
           render_frame_host->GetRoutingID(), delegate_id, request_id));
+}
+
+void MediaWebContentsObserver::OnPictureInPictureSurfaceChanged(
+    RenderFrameHost* render_frame_host,
+    int delegate_id,
+    const viz::SurfaceId& surface_id,
+    const gfx::Size& natural_size) {
+  DCHECK(surface_id.is_valid());
+  DCHECK(pip_player_);
+
+  PictureInPictureWindowControllerImpl* pip_controller =
+      PictureInPictureWindowControllerImpl::FromWebContents(
+          web_contents_impl());
+  DCHECK(pip_controller);
+
+  pip_controller->EmbedSurface(surface_id, natural_size);
 }
 
 void MediaWebContentsObserver::ClearWakeLocks(
