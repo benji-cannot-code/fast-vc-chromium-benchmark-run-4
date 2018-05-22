cb432d590d2f919aedd41d0f86913cb1bb945c7c
Christian Biesinger <cbiesinger@chromium.org>
2018-05-22 23:56:34 +0000

    [layoutng] Refactor min/max content contribution a bit
    Provide a helper function in ng_length_utils that calls
    node.ComputeMinMaxContentSizes for you.
    
    R=eae@chromium.org,mstensho@chromium.org
    
    Bug: 635619
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: Id5e559aa1d9991f1e39d74fd84578ca2b7641883
    Reviewed-on: https://chromium-review.googlesource.com/1069628
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560840}

--- third_party/blink/renderer/core/layout/ng/ng_length_utils.h
+++ third_party/blink/renderer/core/layout/ng/ng_length_utils.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 class ComputedStyle;
 class Length;
+struct MinMaxSizeInput;
 class NGConstraintSpace;
 class NGBlockNode;
 class NGLayoutInputNode;
@@ -87,6 +88,18 @@ CORE_EXPORT MinMaxSize
 ComputeMinAndMaxContentContribution(WritingMode writing_mode,
                                     const ComputedStyle&,
                                     const base::Optional<MinMaxSize>&);
+
+// A version of ComputeMinAndMaxContentContribution that does not require you
+// to compute the min/max content size of the node. Instead, this function
+// will compute it if necessary.
+// writing_mode is the desired output writing mode (ie. often the writing mode
+// of the parent); node is the node of which to compute the min/max content
+// contribution.
+MinMaxSize ComputeMinAndMaxContentContribution(
+    WritingMode writing_mode,
+    NGLayoutInputNode node,
+    const MinMaxSizeInput& input,
+    const NGConstraintSpace* space = nullptr);
 
 // Resolves the given length to a layout unit, constraining it by the min
 // logical width and max logical width properties from the ComputedStyle
