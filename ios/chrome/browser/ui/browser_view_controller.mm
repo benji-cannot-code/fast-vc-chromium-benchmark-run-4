e73c0b1c8404959464151bb676f8af359e3370ee
Eugene But <eugenebut@google.com>
2018-05-22 16:19:28 +0000

    Use Injection Receiver instead of Web Controller to implement View Source menu item.
    Bug: 620489
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Id4ec24e4413e74493c80a1df26da15d2a36fc2c3
    Reviewed-on: https://chromium-review.googlesource.com/1068036
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560633}

--- ios/chrome/browser/ui/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view_controller.mm
@@ -262,6 +262,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/web/public/user_agent.h"
 #include "ios/web/public/web_client.h"
 #import "ios/web/public/web_state/context_menu_params.h"
+#import "ios/web/public/web_state/js/crw_js_injection_receiver.h"
 #import "ios/web/public/web_state/ui/crw_native_content_provider.h"
 #import "ios/web/public/web_state/ui/crw_web_view_proxy.h"
 #import "ios/web/public/web_state/web_state.h"
@@ -4739,7 +4740,6 @@ bubblePresenterForFeature:(const base::Feature&)feature
 - (void)viewSource {
   Tab* tab = [_model currentTab];
   DCHECK(tab);
-  CRWWebController* webController = tab.webController;
   NSString* script = @"document.documentElement.outerHTML;";
   __weak Tab* weakTab = tab;
   __weak BrowserViewController* weakSelf = self;
@@ -4764,8 +4764,9 @@ bubblePresenterForFeature:(const base::Feature&)feature
                  atIndex:TabModelConstants::kTabPositionAutomatically
             inBackground:NO];
   };
-  [webController executeJavaScript:script
-                 completionHandler:completionHandlerBlock];
+  [tab.webState->GetJSInjectionReceiver()
+      executeJavaScript:script
+      completionHandler:completionHandlerBlock];
 }
 #endif  // !defined(NDEBUG)
 
