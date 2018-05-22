f12ae057968f8d9b7362c83f90838baf3d378635
tzik <tzik@chromium.org>
2018-05-22 11:40:15 +0000

    Remove stray std::move()s in gzip_source_stream.cc
    Some of std::move()s around replay_data_ in gzip_source_stream.cc are
    warned for their ignored result by C++17 mode of clang.
    
    Bug: 752720
    Change-Id: I8e3f799ec523114e9c5b01ea57b028c2db997485
    Reviewed-on: https://chromium-review.googlesource.com/1067559
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Taiju Tsuiki <tzik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560536}

--- net/filter/gzip_source_stream.cc
+++ net/filter/gzip_source_stream.cc
@@ -160,7 +160,7 @@ int GzipSourceStream::FilterData(IOBuffer* output_buffer,
         if (bytes_out > 0 ||
             bytes_used + replay_data_.size() >= kMaxZlibHeaderSniffBytes ||
             ret == Z_STREAM_END) {
-          std::move(replay_data_);
+          replay_data_.clear();
           if (ret == Z_STREAM_END) {
             input_state_ = STATE_GZIP_FOOTER;
           } else {
@@ -178,7 +178,6 @@ int GzipSourceStream::FilterData(IOBuffer* output_buffer,
         DCHECK_EQ(TYPE_DEFLATE, type());
 
         if (replay_data_.empty()) {
-          std::move(replay_data_);
           input_state_ = replay_state_;
           break;
         }
