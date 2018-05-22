0b1a819a1acf2fff4bb28172632cad2ed45fe92d
Siddhartha <ssid@chromium.org>
2018-05-22 21:16:21 +0000

    Tracing: Add a metadata event to track chrome library start address
    On Android for trace symbolization the start address of the library
    cannot be obtained by offsets from memory maps since it is mapped from
    apk. So, add explicit metadata for each process to mark the start
    address of the executable. The only library mapped from apk is
    chrome.so.
    
    Change-Id: Ib6f3ec81fd1c34c92974fce7a73debd4f9e8dad5
    Reviewed-on: https://chromium-review.googlesource.com/1068239
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Reviewed-by: oysteine <oysteine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560777}

--- base/trace_event/trace_log.cc
+++ base/trace_event/trace_log.cc
@@ -50,6 +50,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/trace_event_etw_export_win.h"
 #endif
 
+#if defined(OS_ANDROID)
+// The linker assigns the virtual address of the start of current library to
+// this symbol.
+extern char __executable_start;
+#endif
+
 namespace base {
 namespace trace_event {
 
@@ -1549,6 +1555,13 @@ void TraceLog::AddMetadataEventsWhileLocked() {
   InitializeMetadataEvent(
       AddEventToThreadSharedChunkWhileLocked(nullptr, false), current_thread_id,
       "process_uptime_seconds", "uptime", process_uptime.InSeconds());
+
+#if defined(OS_ANDROID)
+  InitializeMetadataEvent(
+      AddEventToThreadSharedChunkWhileLocked(nullptr, false), current_thread_id,
+      "chrome_library_address", "start_address",
+      base::StringPrintf("%p", &__executable_start));
+#endif
 
   if (!process_labels_.empty()) {
     std::vector<base::StringPiece> labels;
