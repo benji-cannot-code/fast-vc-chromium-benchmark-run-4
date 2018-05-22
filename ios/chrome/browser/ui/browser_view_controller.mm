e9bd04698bd1a4748be2e71898e69009cbd0afb5
Gauthier Ambard <gambard@chromium.org>
2018-05-22 16:14:22 +0000

    Reland "Add blink effect to the PopupMenu"
    This reverts commit 33c637fd83f41e7050e7fb33f92f745aae02e7fa.
    
    Original change's description:
    > Revert "Add blink effect to the PopupMenu"
    >
    > This reverts commit 20a94e7f7494f90da6be4593ef85ae482c58e89f.
    >
    > Reason for revert:
    > https://bugs.chromium.org/p/chromium/issues/detail?id=844753
    >
    > Bug: 844753
    >
    > Original change's description:
    > > Add blink effect to the PopupMenu
    > >
    > > This CL adds an effect to make the "New Incognito Tab" row of the tools
    > > menu blink a selected state when the in product help requires it.
    > >
    > > Bug: 829344
    > > Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    > > Change-Id: Ib2345a98f8ec284369b342d111cadf7f6a55adef
    > > Reviewed-on: https://chromium-review.googlesource.com/1057708
    > > Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    > > Reviewed-by: edchin <edchin@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#559481}
    >
    > TBR=edchin@chromium.org,gambard@chromium.org
    >
    > # Not skipping CQ checks because original CL landed > 1 day ago.
    >
    > Bug: 829344
    > Change-Id: I7135501ca78396a46d23ec53a538352d77e5c29d
    > Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    > Reviewed-on: https://chromium-review.googlesource.com/1066334
    > Commit-Queue: Eugene But <eugenebut@chromium.org>
    > Reviewed-by: edchin <edchin@chromium.org>
    > Reviewed-by: Eugene But <eugenebut@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#560106}
    
    Bug: 844753, 829344
    Change-Id: Ibaf0e5cb3938a58021105d14b8d9d60e08372e06
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Reviewed-on: https://chromium-review.googlesource.com/1068727
    Reviewed-by: edchin <edchin@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560632}

--- ios/chrome/browser/ui/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view_controller.mm
@@ -2292,6 +2292,8 @@ applicationCommandEndpoint:(id<ApplicationCommands>)applicationCommandEndpoint {
     self.popupMenuCoordinator = [[PopupMenuCoordinator alloc]
         initWithBaseViewController:self
                       browserState:self.browserState];
+    self.popupMenuCoordinator.incognitoTabTipPresenter =
+        self.incognitoTabTipBubblePresenter;
     self.popupMenuCoordinator.dispatcher = _dispatcher;
     self.popupMenuCoordinator.webStateList = [_model webStateList];
     self.popupMenuCoordinator.UIUpdater = _toolbarCoordinatorAdaptor;
@@ -2818,6 +2820,7 @@ bubblePresenterForFeature:(const base::Feature&)feature
     return;
 
   self.incognitoTabTipBubblePresenter = presenter;
+  self.popupMenuCoordinator.incognitoTabTipPresenter = presenter;
 
   [self.incognitoTabTipBubblePresenter
       presentInViewController:self
