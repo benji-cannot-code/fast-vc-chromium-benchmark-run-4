fcdd0ac3c11211141f198f67dc76dfa784656561
Reid Kleckner <rnk@chromium.org>
2018-05-22 20:13:49 +0000

    Revert "Use policy to control Chrome Cleanup reporting."
    This reverts commit 8e57e0101bec785ef9798a99e829290d7433e17d.
    
    Reason for revert: It breaks official builds:
    https://uberchromegw.corp.google.com/i/official.desktop.continuous/builders/win%20trunk/builds/84546
    
    Original change's description:
    > Use policy to control Chrome Cleanup reporting.
    > 
    > Bug: 834939
    > Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    > Change-Id: I603d1d8d43d5317f8a8ef24f4fdbb537e0a21541
    > Reviewed-on: https://chromium-review.googlesource.com/1053763
    > Commit-Queue: Roger Tawa <rogerta@chromium.org>
    > Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    > Reviewed-by: Joe Mason <joenotcharles@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#559685}
    
    TBR=rogerta@chromium.org,dpapad@chromium.org,joenotcharles@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 834939
    Change-Id: I1b6e1a4007935abf0f6e8f9b1adf05804c431a1f
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Reviewed-on: https://chromium-review.googlesource.com/1069387
    Reviewed-by: Reid Kleckner <rnk@chromium.org>
    Commit-Queue: Reid Kleckner <rnk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560748}

--- chrome/browser/ui/webui/settings/chrome_cleanup_handler.h
+++ chrome/browser/ui/webui/settings/chrome_cleanup_handler.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/safe_browsing/chrome_cleaner/chrome_cleaner_controller_win.h"
 #include "chrome/browser/safe_browsing/chrome_cleaner/chrome_cleaner_scanner_results.h"
 #include "chrome/browser/ui/webui/settings/settings_page_ui_handler.h"
-#include "components/prefs/pref_change_registrar.h"
 
 class Profile;
 
@@ -47,9 +46,6 @@ class ChromeCleanupHandler
   void OnLogsEnabledChanged(bool logs_enabled) override;
 
  private:
-  // Called when prefs::kSwReporterReportingEnabled changes.
-  void OnLogsEnabledPrefChanged();
-
   // Callback for the "registerChromeCleanerObserver" message. This registers
   // this object as an observer of the Chrome Cleanup global state and
   // and retrieves the current cleanup state.
@@ -93,7 +89,6 @@ class ChromeCleanupHandler
   safe_browsing::ChromeCleanerController* controller_;
 
   Profile* profile_;
-  PrefChangeRegistrar logs_enabled_pref_;
 
   DISALLOW_COPY_AND_ASSIGN(ChromeCleanupHandler);
 };
