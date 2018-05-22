a5ee7608aae90e4f1a9a500814ba70134b8fec09
John Z Wu <jzw@chromium.org>
2018-05-22 20:20:22 +0000

    Define CWVAutofillDataManagerDelegate.
    This delegate will provide change notifications so the client knows
    when data has changed.
    
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: If9fd061ef1ecded33acf4847582d027435509f9a
    Reviewed-on: https://chromium-review.googlesource.com/1064544
    Commit-Queue: John Wu <jzw@chromium.org>
    Reviewed-by: Hiroshi Ichikawa <ichikawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560751}

--- ios/web_view/internal/autofill/cwv_autofill_data_manager.mm
+++ ios/web_view/internal/autofill/cwv_autofill_data_manager.mm
@@ -5,25 +5,72 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/web_view/internal/autofill/cwv_autofill_data_manager_internal.h"
 
+#include <memory>
+
 #include "components/autofill/core/browser/personal_data_manager.h"
+#include "components/autofill/core/browser/personal_data_manager_observer.h"
 #import "ios/web_view/internal/autofill/cwv_autofill_profile_internal.h"
 #import "ios/web_view/internal/autofill/cwv_credit_card_internal.h"
+#import "ios/web_view/public/cwv_autofill_data_manager_delegate.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
 
+@interface CWVAutofillDataManager ()
+// Called when WebViewPersonalDataManagerObserverBridge's
+// |OnPersonalDataChanged| is invoked.
+- (void)personalDataDidChange;
+
+@end
+
+namespace ios_web_view {
+// C++ to ObjC bridge for PersonalDataManagerObserver.
+class WebViewPersonalDataManagerObserverBridge
+    : public autofill::PersonalDataManagerObserver {
+ public:
+  explicit WebViewPersonalDataManagerObserverBridge(
+      CWVAutofillDataManager* data_manager)
+      : data_manager_(data_manager){};
+  ~WebViewPersonalDataManagerObserverBridge() override = default;
+
+  // autofill::PersonalDataManagerObserver implementation.
+  void OnPersonalDataChanged() override {
+    [data_manager_ personalDataDidChange];
+  }
+
+  void OnInsufficientFormData() override {
+    // Nop.
+  }
+
+ private:
+  __weak CWVAutofillDataManager* data_manager_;
+};
+}  // namespace ios_web_view
+
 @implementation CWVAutofillDataManager {
   autofill::PersonalDataManager* _personalDataManager;
+  std::unique_ptr<ios_web_view::WebViewPersonalDataManagerObserverBridge>
+      _personalDataManagerObserverBridge;
 }
+
+@synthesize delegate = _delegate;
 
 - (instancetype)initWithPersonalDataManager:
     (autofill::PersonalDataManager*)personalDataManager {
   self = [super init];
   if (self) {
     _personalDataManager = personalDataManager;
+    _personalDataManagerObserverBridge = std::make_unique<
+        ios_web_view::WebViewPersonalDataManagerObserverBridge>(self);
+    _personalDataManager->AddObserver(_personalDataManagerObserverBridge.get());
   }
   return self;
+}
+
+- (void)dealloc {
+  _personalDataManager->RemoveObserver(
+      _personalDataManagerObserverBridge.get());
 }
 
 #pragma mark - Public Methods
@@ -64,6 +111,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)deleteCreditCard:(CWVCreditCard*)creditCard {
   _personalDataManager->RemoveByGUID(creditCard.internalCard->guid());
+}
+
+#pragma mark - Private Methods
+
+- (void)personalDataDidChange {
+  [_delegate autofillDataManagerDataDidChange:self];
 }
 
 @end
