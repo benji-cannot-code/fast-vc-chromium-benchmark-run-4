12c7ba939489bd9863fe148410c0cbb1deb6ed58
Boris Sazonov <bsazonov@chromium.org>
2018-05-22 15:23:29 +0000

    [Android] Make NoUnderlineClickableSpan non-abstract
    This CL revises NoUnderlineClickableSpan and it's subclasses. Now
    NoUnderlineClickableSpan is a non-abstract class that accepts callback
    in the constructor. This allows instantiating it using lambdas,
    removing boilerplate. This CL also removes some subclasses of
    NoUnderlineClickableSpan that can be implemented without subclassing.
    
    Change-Id: I2a2a2092c9b1ab42db38390a6715da57087af21a
    Bug: None
    Reviewed-on: https://chromium-review.googlesource.com/1064415
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560596}

--- chrome/android/java/src/org/chromium/chrome/browser/infobar/InfoBarLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/infobar/InfoBarLayout.java
@@ -502,12 +502,7 @@ public final class InfoBarLayout extends ViewGroup implements View.OnClickListen
     }
 
     private NoUnderlineClickableSpan createClickableSpan() {
-        return new NoUnderlineClickableSpan() {
-            @Override
-            public void onClick(View view) {
-                mInfoBarView.onLinkClicked();
-            }
-        };
+        return new NoUnderlineClickableSpan((view) -> mInfoBarView.onLinkClicked());
     }
 
     /**
--- ui/android/java/src/org/chromium/ui/text/NoUnderlineClickableSpan.java
+++ ui/android/java/src/org/chromium/ui/text/NoUnderlineClickableSpan.java
@@ -5,22 +5,36 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.ui.text;
 
+import android.support.annotation.ColorRes;
 import android.text.TextPaint;
 import android.text.style.ClickableSpan;
+import android.view.View;
 
 import org.chromium.base.ApiCompatibilityUtils;
+import org.chromium.base.Callback;
 import org.chromium.base.ContextUtils;
 import org.chromium.ui.R;
 
 /**
  * Shows a blue clickable link with underlines turned off.
  */
-public abstract class NoUnderlineClickableSpan extends ClickableSpan {
+public class NoUnderlineClickableSpan extends ClickableSpan {
     private final int mColor;
+    private final Callback<View> mOnClick;
 
-    public NoUnderlineClickableSpan() {
+    public NoUnderlineClickableSpan(Callback<View> onClickCallback) {
+        this(R.color.google_blue_700, onClickCallback);
+    }
+
+    public NoUnderlineClickableSpan(@ColorRes int colorResId, Callback<View> onClickCallback) {
         mColor = ApiCompatibilityUtils.getColor(
-                ContextUtils.getApplicationContext().getResources(), R.color.google_blue_700);
+                ContextUtils.getApplicationContext().getResources(), colorResId);
+        mOnClick = onClickCallback;
+    }
+
+    @Override
+    public final void onClick(View view) {
+        mOnClick.onResult(view);
     }
 
     // Disable underline on the link text.
