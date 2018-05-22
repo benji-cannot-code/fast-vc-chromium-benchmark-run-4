b1773803252b192539b77d0a3c64028d1928ffd1
Luke Sorenson <lasoren@chromium.org>
2018-05-22 19:45:24 +0000

    Add setComponentProcessState function to chrome.mediaPerceptionPrivate
    This provides an alternate API for starting the underlying
    media analytics process. Eventually this will be used instead of
    setState in production when most of the communication to the
    service is replaced with a Mojo channel.
    
    Bug: 842718, 799739
    Change-Id: Ia0cb6c74d580bf2f9b442e2fff8a12d4878f214a
    Reviewed-on: https://chromium-review.googlesource.com/1042363
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Luke Sorenson <lasoren@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560734}

--- extensions/browser/api/media_perception_private/media_perception_private_api.cc
+++ extensions/browser/api/media_perception_private/media_perception_private_api.cc
@@ -131,4 +131,15 @@ void MediaPerceptionPrivateSetAnalyticsComponentFunction::
   Respond(OneArgument(component_state.ToValue()));
 }
 
+MediaPerceptionPrivateSetComponentProcessStateFunction::
+    MediaPerceptionPrivateSetComponentProcessStateFunction() = default;
+
+MediaPerceptionPrivateSetComponentProcessStateFunction::
+    ~MediaPerceptionPrivateSetComponentProcessStateFunction() = default;
+
+ExtensionFunction::ResponseAction
+MediaPerceptionPrivateSetComponentProcessStateFunction::Run() {
+  return RespondNow(Error("Not implemented."));
+}
+
 }  // namespace extensions
