edd4ec9d01660813f267443abf6665c247bda931
Marc Treib <treib@chromium.org>
2018-05-22 15:59:43 +0000

    SyncAuthManager: don't do read-before-write for prefs
    PrefService checks the previous value anyway, no need for us to do it
    manually too.
    
    Bug: 842697
    Change-Id: I32027065bb4eea9bb18dfb0837c6d053b09defd4
    Reviewed-on: https://chromium-review.googlesource.com/1068744
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560615}

--- components/browser_sync/sync_auth_manager.cc
+++ components/browser_sync/sync_auth_manager.cc
@@ -330,11 +330,7 @@ void SyncAuthManager::AccessTokenFetched(const GoogleServiceAuthError& error,
   switch (error.state()) {
     case GoogleServiceAuthError::NONE:
       token_status_.token_receive_time = base::Time::Now();
-      // TODO(treib): Can we get rid of the read-before-write? PrefService
-      // should only notify if the value actually changed anyway.
-      if (sync_prefs_->SyncHasAuthError()) {
-        sync_prefs_->SetSyncAuthError(false);
-      }
+      sync_prefs_->SetSyncAuthError(false);
       ClearAuthError();
       break;
     case GoogleServiceAuthError::CONNECTION_FAILED:
@@ -352,11 +348,7 @@ void SyncAuthManager::AccessTokenFetched(const GoogleServiceAuthError& error,
                               weak_ptr_factory_.GetWeakPtr()));
       break;
     case GoogleServiceAuthError::INVALID_GAIA_CREDENTIALS:
-      // TODO(treib): Can we get rid of the read-before-write? PrefService
-      // should only notify if the value actually changed anyway.
-      if (!sync_prefs_->SyncHasAuthError()) {
-        sync_prefs_->SetSyncAuthError(true);
-      }
+      sync_prefs_->SetSyncAuthError(true);
       UpdateAuthErrorState(error);
       break;
     default:
