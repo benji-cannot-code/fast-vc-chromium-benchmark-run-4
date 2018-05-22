fbd095876b534132433db9fcd0444379543863fb
Gabriel Charette <gab@chromium.org>
2018-05-22 15:31:48 +0000

    [Flakiness] Fix RunLoopTest death-tests to fail without hanging.
    This test is exercising a DCHECK in RunLoop::BeforeRun() which is
    exercised the same way by RunUntilIdle(). RunUntileIdle() has the
    benefit of not hanging the test on failure.
    
    R=danakj@chromium.org, wez@chromium.org
    
    Bug: 832882
    Change-Id: I905e7f3940130b5648dcb16eab4b55a17d811169
    Reviewed-on: https://chromium-review.googlesource.com/1066401
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560602}

--- base/run_loop_unittest.cc
+++ base/run_loop_unittest.cc
@@ -639,7 +639,7 @@ TEST_P(RunLoopTest, NestingObservers) {
 
 TEST_P(RunLoopTest, DisallowRunningForTesting) {
   RunLoop::ScopedDisallowRunningForTesting disallow_running;
-  EXPECT_DCHECK_DEATH({ run_loop_.Run(); });
+  EXPECT_DCHECK_DEATH({ run_loop_.RunUntilIdle(); });
 }
 
 TEST_P(RunLoopTest, ExpiredDisallowRunningForTesting) {
