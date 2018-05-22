e7545d7d35e172093f0fd96f69f2cafdfd44ec0f
Darren Shen <shend@chromium.org>
2018-05-22 05:51:21 +0000

    [VK] Change ToggleTouchEventLogging to SetTouchEventLogging.
    Toggle usually means to switch between two modes. SetTouchEventLogging
    is perhaps a more accurate name.
    
    Change-Id: I9e5090567270af9826c3cb7f8b141067b264654d
    Reviewed-on: https://chromium-review.googlesource.com/1067520
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560491}

--- ui/keyboard/keyboard_controller.cc
+++ ui/keyboard/keyboard_controller.cc
@@ -140,7 +140,7 @@ class KeyboardWindowDelegate : public aura::WindowDelegate {
   DISALLOW_COPY_AND_ASSIGN(KeyboardWindowDelegate);
 };
 
-void ToggleTouchEventLogging(bool enable) {
+void SetTouchEventLogging(bool enable) {
 #if defined(OS_CHROMEOS)
   // TODO(moshayedi): crbug.com/642863. Revisit when we have mojo interface for
   // InputController for processes that aren't mus-ws.
@@ -361,7 +361,7 @@ void KeyboardController::HideKeyboard(HideReason reason) {
 
     case KeyboardControllerState::WILL_HIDE:
     case KeyboardControllerState::SHOWN: {
-      ToggleTouchEventLogging(true);
+      SetTouchEventLogging(true /* enable */);
 
       keyboard::LogKeyboardControlEvent(
           reason == HIDE_REASON_AUTOMATIC
@@ -652,7 +652,7 @@ void KeyboardController::PopulateKeyboardContent(
     return;
   }
 
-  ToggleTouchEventLogging(!show_keyboard);
+  SetTouchEventLogging(!show_keyboard /* enable */);
 
   switch (state_) {
     case KeyboardControllerState::INITIAL:
