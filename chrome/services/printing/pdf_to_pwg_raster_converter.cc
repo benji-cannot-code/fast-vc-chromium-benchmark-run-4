af1d5d5f3963eecd0c657237ecd7e4202de4da80
Lei Zhang <thestig@chromium.org>
2018-05-22 21:30:02 +0000

    Add printing::CreateReadOnlySharedMemoryRegionWithData().
    Consolidate non-trivial, repeated code that creates shared memory
    regions inside sandboxed environments.
    
    Change-Id: I29e0715a9d43fdde030ee0a2286f872b34576c8f
    Reviewed-on: https://chromium-review.googlesource.com/1056393
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560783}

--- chrome/services/printing/pdf_to_pwg_raster_converter.cc
+++ chrome/services/printing/pdf_to_pwg_raster_converter.cc
@@ -9,10 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <utility>
 
+#include "components/printing/service/public/cpp/pdf_service_mojo_utils.h"
 #include "components/pwg_encoder/bitmap_image.h"
 #include "components/pwg_encoder/pwg_encoder.h"
 #include "mojo/public/cpp/bindings/strong_binding.h"
-#include "mojo/public/cpp/system/platform_handle.h"
 #include "pdf/pdf.h"
 #include "printing/pdf_render_settings.h"
 
@@ -97,22 +97,13 @@ base::ReadOnlySharedMemoryRegion RenderPdfPagesToPwgRaster(
     pwg_data += pwg_page;
   }
 
-  mojo::ScopedSharedBufferHandle pwg_handle =
-      mojo::SharedBufferHandle::Create(pwg_data.size());
-  mojo::ScopedSharedBufferHandle readonly_handle;
-  if (pwg_handle.is_valid()) {
-    mojo::ScopedSharedBufferMapping pwg_mapping =
-        pwg_handle->Map(pwg_data.size());
-    if (pwg_mapping) {
-      memcpy(pwg_mapping.get(), pwg_data.data(), pwg_data.size());
-      readonly_handle =
-          pwg_handle->Clone(mojo::SharedBufferHandle::AccessMode::READ_ONLY);
-    }
-  }
-  if (!readonly_handle.is_valid())
+  base::MappedReadOnlyRegion region_mapping =
+      CreateReadOnlySharedMemoryRegion(pwg_data.size());
+  if (!region_mapping.region.IsValid() || !region_mapping.mapping.IsValid())
     return invalid_pwg_region;
 
-  return mojo::UnwrapReadOnlySharedMemoryRegion(std::move(readonly_handle));
+  memcpy(region_mapping.mapping.memory(), pwg_data.data(), pwg_data.size());
+  return std::move(region_mapping.region);
 }
 
 }  // namespace
