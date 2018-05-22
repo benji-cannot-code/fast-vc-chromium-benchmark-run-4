2b7f4a47f6b8441b2bed322d18d320de49c70964
Jeremy Roman <jbroman@chromium.org>
2018-05-22 14:29:38 +0000

    Disable CrSettingsAndroidAppsPageTest.All due to flakiness on linux-chromeos-rel.
    Bug: 825304
    Change-Id: I367136f909e2a71c3c19a7d28ad2c4c60cacc311
    Tbr: stevenjb@chromium.org
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/1067673
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560569}

--- chrome/test/data/webui/settings/cr_settings_browsertest.js
+++ chrome/test/data/webui/settings/cr_settings_browsertest.js
@@ -1823,7 +1823,7 @@ CrSettingsAndroidAppsPageTest.prototype = {
   ]),
 };
 
-TEST_F('CrSettingsAndroidAppsPageTest', 'All', function() {
+TEST_F('CrSettingsAndroidAppsPageTest', 'DISABLED_All', function() {
   mocha.run();
 });
 
