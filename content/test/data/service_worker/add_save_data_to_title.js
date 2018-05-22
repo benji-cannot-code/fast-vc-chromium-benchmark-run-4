311d69857c8139476524099b5515ca614bbb7d6c
Kenichi Ishibashi <bashi@chromium.org>
2018-05-22 01:34:35 +0000

    S13nSW: Set Content-Type for synthesized responses in content_browsertests
    Some of content_browsertests are timing out because we haven't supported
    mime-sniffing in S13nSW code path yet (issue 771118). These tests wait
    for <title> being updated but it doesn't happen when a synthesized
    response doesn't have 'Content-Type: text/html'. Probably a right fix
    would be to support mime-sniffing but it's better to make these tests
    independent from mime-sniffing as these tests do nothing with
    mime-sniffing. This CL changes results of following tests:
    
    - ServiceWorkerBrowserTest.FetchPageWithSaveData -> PASS
    - ServiceWorkerBrowserTest.ResponseFromHTTPSServiceWorkerIsMarkedAsSecure -> CRASH
    - ServiceWorkerBrowserTest.ResponseFromHTTPServiceWorkerIsNotMarkedAsSecure -> PASS
    
    Following tests are still timing out because of unexpected behavior:
    
    - ServiceWorkerBrowserTest.ImportsBustMemcache
    - ServiceWorkerBrowserTest.Reload
    
    Bug: 715640, 771118
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I14819f2b39bfa83edf9eb5fcbf9950d72fa5b292
    Reviewed-on: https://chromium-review.googlesource.com/1067220
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560448}

--- content/test/data/service_worker/add_save_data_to_title.js
+++ content/test/data/service_worker/add_save_data_to_title.js
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 this.onfetch = function(event) {
     var value = event.request.headers.get('Save-Data');
     var blob = new Blob(['<title>save-data='+ value + '</title>']);
-    var response = new Response(blob);
+    var response = new Response(blob, {
+        headers: { 'Content-Type': 'text/html' }
+    });
     event.respondWith(response);
 };
--- content/test/data/service_worker/fetch_event_blob.js
+++ content/test/data/service_worker/fetch_event_blob.js
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 this.onfetch = function(event) {
     var blob = new Blob(['<title>Title</title>']);
-    var response = new Response(blob);
+    var response = new Response(blob, {
+        headers: { 'Content-Type': 'text/html' }
+    });
     event.respondWith(response);
 };
