4db65130170119960460feb3e725fc0a0a9e2246
Fredrik Söderquist <fs@opera.com>
2018-05-22 16:13:15 +0000

    Fix SVGImage filtering in JankTracker
    The condition claiming to filter out documents (frames) originating from
    SVGImages was in fact filtering out all SVG documents. Adjust.
    
    Move the unit test to SVGImageTest because it already has usable
    infrastructure to test this properly.
    
    Also add DISALLOW_NEW to the JankTracker definition to indicate its
    relation to the GC.
    
    Bug: 581518
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I53ae6100c1538430c97c8b0fd6cda2f5c1ee9a6c
    Reviewed-on: https://chromium-review.googlesource.com/1068914
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560630}

--- third_party/blink/renderer/core/layout/jank_tracker.h
+++ third_party/blink/renderer/core/layout/jank_tracker.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/platform/geometry/region.h"
 #include "third_party/blink/renderer/platform/timer.h"
+#include "third_party/blink/renderer/platform/wtf/allocator.h"
 
 namespace blink {
 
@@ -20,6 +21,8 @@ class PaintLayer;
 // Tracks "jank" from layout objects changing their visual location between
 // animation frames.
 class CORE_EXPORT JankTracker {
+  DISALLOW_NEW();
+
  public:
   JankTracker(LocalFrameView*);
   ~JankTracker() {}
