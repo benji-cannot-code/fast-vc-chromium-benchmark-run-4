338940e532be990d76f197eda1d7fbb4ca9cf7e7
Xiaocheng Hu <xiaochengh@chromium.org>
2018-05-23 00:21:29 +0000

    [LayoutNG] Move PositionForPoint() implementation to paint fragments
    This patch moves the NG implementation of PositionForPoint() from
    physical fragments to paint fragments as a preparation for
    implementing NG hit-testing bidi adjustment (*), which requires
    traversal among paint fragments.
    
    (*) crrev.com/c/1064480
    
    Bug: 811502
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_layout_ng;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I449ac545e5ccefff5c5b6cbe7988b2f3c9280c4a
    Reviewed-on: https://chromium-review.googlesource.com/1067699
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560849}

--- third_party/blink/renderer/core/layout/ng/ng_physical_box_fragment.h
+++ third_party/blink/renderer/core/layout/ng/ng_physical_box_fragment.h
@@ -61,8 +61,6 @@ class CORE_EXPORT NGPhysicalBoxFragment final
   void AddSelfOutlineRects(Vector<LayoutRect>*,
                            const LayoutPoint& additional_offset) const;
 
-  PositionWithAffinity PositionForPoint(const NGPhysicalOffset&) const override;
-
   UBiDiLevel BidiLevel() const override;
 
   scoped_refptr<NGPhysicalFragment> CloneWithoutOffset() const;
