4a40bcc88d144a2b3c4498e299454b1873150efb
Yoshifumi Inoue <yosin@chromium.org>
2018-05-23 07:30:44 +0000

    Make WebElementTest to use Element::AttachShadowRootInternal() and CreateV0ShadowRootForTesting()
    This patch changes |FocusControllerTest| to use |AttachShadowRootInternal()|
    and CreateV0ShadowRootForTesting() instead of |Element::attachShadow()| and
    |Element::createShadowRoot()|to avoid using script only functions for
    improving code health.
    
    Change-Id: Id362106ff3f1b1f577da7ec3bce97e60b0b490f4
    Reviewed-on: https://chromium-review.googlesource.com/1070011
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560991}

--- third_party/blink/renderer/core/exported/web_element_test.cc
+++ third_party/blink/renderer/core/exported/web_element_test.cc
@@ -206,13 +206,12 @@ TEST_F(WebElementTest, ShadowRoot) {
   EXPECT_TRUE(TestElement().ShadowRoot().IsNull())
       << "ShadowRoot() should not return a UA ShadowRoot.";
 
-  auto* script_state = ToScriptStateForMainWorld(GetDocument().GetFrame());
   {
     InsertHTML("<div id=testElement></div>");
     EXPECT_TRUE(TestElement().ShadowRoot().IsNull())
         << "No ShadowRoot initially.";
     auto* element = GetDocument().getElementById("testElement");
-    element->createShadowRoot(script_state, ASSERT_NO_EXCEPTION);
+    element->CreateV0ShadowRootForTesting();
     EXPECT_FALSE(TestElement().ShadowRoot().IsNull())
         << "Should return V0 ShadowRoot.";
   }
@@ -222,9 +221,7 @@ TEST_F(WebElementTest, ShadowRoot) {
     EXPECT_TRUE(TestElement().ShadowRoot().IsNull())
         << "No ShadowRoot initially.";
     auto* element = GetDocument().getElementById("testElement");
-    ShadowRootInit init;
-    init.setMode("open");
-    element->attachShadow(script_state, init, ASSERT_NO_EXCEPTION);
+    element->AttachShadowRootInternal(ShadowRootType::kOpen);
     EXPECT_FALSE(TestElement().ShadowRoot().IsNull())
         << "Should return V1 open ShadowRoot.";
   }
@@ -234,9 +231,7 @@ TEST_F(WebElementTest, ShadowRoot) {
     EXPECT_TRUE(TestElement().ShadowRoot().IsNull())
         << "No ShadowRoot initially.";
     auto* element = GetDocument().getElementById("testElement");
-    ShadowRootInit init;
-    init.setMode("closed");
-    element->attachShadow(script_state, init, ASSERT_NO_EXCEPTION);
+    element->AttachShadowRootInternal(ShadowRootType::kClosed);
     EXPECT_FALSE(TestElement().ShadowRoot().IsNull())
         << "Should return V1 closed ShadowRoot.";
   }
