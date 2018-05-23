44dedad8ca695593c081c73e9ef71a917869d6bc
Darren Shen <shend@chromium.org>
2018-05-23 07:41:16 +0000

    [VK] Simplify KeyboardController::SetDraggableArea.
    |SetDraggableArea| returns checks if the container behavior is null,
    but KeyboardController always has a container behavior (initialised in
    the constructor).
    
    This patch removes the container behavior null check.
    
    Bug: 845780
    Change-Id: Iaa21bd8179a7fb88a0dd277f4888ae778c624d99
    Reviewed-on: https://chromium-review.googlesource.com/1069950
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560995}

--- ui/keyboard/keyboard_controller.cc
+++ ui/keyboard/keyboard_controller.cc
@@ -849,10 +849,7 @@ void KeyboardController::SetContainerType(
 }
 
 bool KeyboardController::SetDraggableArea(const gfx::Rect& rect) {
-  if (container_behavior_) {
-    return container_behavior_->SetDraggableArea(rect);
-  }
-  return false;
+  return container_behavior_->SetDraggableArea(rect);
 }
 
 bool KeyboardController::DisplayVirtualKeyboard() {
