fdc699e57d1a004e7c2d24d0660971831ecc2d1d
Theresa <twellington@chromium.org>
2018-05-23 23:56:11 +0000

    Revert "Clean up scrim logic in LocationBarLayout"
    This reverts commit b422141ffc7ae1220a00945ddf7cbed3510a1217.
    
    Reason for revert: Suspect that this broke ui automator tests
    
    Original change's description:
    > Clean up scrim logic in LocationBarLayout
    > 
    > Scrim visibility in the location bar is now completely controlled by
    > a single function. The scrim observer is now added and removed based
    > on the scrim visibility to avoid conflict with other observers (like
    > the bottom sheet).
    > 
    > Bug: 834927
    > Change-Id: I172ca637dde5bc026b1b7f629bc34c6d939dff2d
    > Reviewed-on: https://chromium-review.googlesource.com/1026168
    > Commit-Queue: Matthew Jones <mdjones@chromium.org>
    > Reviewed-by: Ted Choc <tedchoc@chromium.org>
    > Reviewed-by: Theresa <twellington@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#558785}
    
    TBR=tedchoc@chromium.org,twellington@chromium.org,mdjones@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 834927
    Change-Id: I46401b29a91f67cfa6aa6611b7d9bf854d1fd610
    Reviewed-on: https://chromium-review.googlesource.com/1070890
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561317}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarPhone.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarPhone.java
@@ -189,7 +189,8 @@ public class LocationBarPhone extends LocationBarLayout {
 
         NewTabPage ntp = getToolbarDataProvider().getNewTabPageForCurrentTab();
         if (hasFocus && ntp != null && ntp.isLocationBarShownInNTP() && mBottomSheet == null) {
-            updateFadingBackgroundView(true, true);
+            if (mFadingView == null) initFadingOverlayView();
+            mFadingView.showFadingOverlay();
         }
     }
 
