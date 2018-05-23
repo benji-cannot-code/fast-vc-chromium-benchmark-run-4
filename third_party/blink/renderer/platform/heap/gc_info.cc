f37b7ec6b4f069ba767f2fe8808cee9a38c8e9a9
Michael Lippautz <mlippautz@chromium.org>
2018-05-23 07:09:53 +0000

    [oilpan] Separate out finalizer traits into own file
    Bug: chromium:841830
    Change-Id: Ica721b07df8b50b22fd801e83abf7403653a9474
    Reviewed-on: https://chromium-review.googlesource.com/1068576
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560984}

--- third_party/blink/renderer/platform/heap/gc_info.cc
+++ third_party/blink/renderer/platform/heap/gc_info.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/heap/gc_info.h"
 
 #include "third_party/blink/renderer/platform/heap/heap.h"
+#include "third_party/blink/renderer/platform/wtf/std_lib_extras.h"
 
 namespace blink {
 
