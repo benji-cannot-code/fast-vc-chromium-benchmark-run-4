7cb2d08af3f17d65087d90077efe373987d9e2b1
Calder Kitagawa <ckitagawa@google.com>
2018-05-23 17:30:51 +0000

    [Zucchini] ZTF (text) disassembler
    A Zucchini text format (ZTF) disassembler for validating and
    debugging Zucchini using special text files.
    
    This is intended to be primarily used for easier validation of the
    core Zucchini algorithm. It is also useful for fuzzing reference
    projection as it is a lighter weight disassembler than other
    executable formats. References are encoded in an ASCII plain-text
    format to be human-readable and easier to debug.
    
    See the disassembler_ztf.h file for a complete outline of the file
    format. Which is referred to as Zucchini Text Format (ZTF) throughout
    the code.
    
    The design is meant to be highly flexible and non-redundant so a fair
    amount of templating/overloading is used. The overall design is:
    
    Shared:
    - ZtfConfig: To handle metadata for different types of references.
    - ZtfTranslator: Translate text references into offsets and back.
    
    Read:
    - ZtfParser: Find references in the file.
    - ZtfReferenceReader: Drive ZtfParser through the file and use it
      with ZtfTranslator to translate between offsets and references.
    
    Write:
    - ZtfWriter: The counterpart to parser for writing a reference to a
      location.
    - ZtfReferenceWriter: Compose ZtfWriter and ZtfTranslator
      together and write references to an image.
    
    Bug: 834904
    Change-Id: Iec62c67eab6bd3e7e95b79798417035a4873360a
    Reviewed-on: https://chromium-review.googlesource.com/1056147
    Commit-Queue: Calder Kitagawa <ckitagawa@google.com>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561140}

--- components/zucchini/element_detection.cc
+++ components/zucchini/element_detection.cc
@@ -10,9 +10,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "components/zucchini/buildflags.h"
 #include "components/zucchini/disassembler.h"
-#include "components/zucchini/disassembler_dex.h"
 #include "components/zucchini/disassembler_no_op.h"
+
+#if BUILDFLAG(ENABLE_DEX)
+#include "components/zucchini/disassembler_dex.h"
+#endif  // BUILDFLAG(ENABLE_DEX)
+
+#if BUILDFLAG(ENABLE_WIN)
 #include "components/zucchini/disassembler_win32.h"
+#endif  // BUILDFLAG(ENABLE_WIN)
+
+#if BUILDFLAG(ENABLE_ZTF)
+#include "components/zucchini/disassembler_ztf.h"
+#endif  // BUILDFLAG(ENABLE_ZTF)
 
 namespace zucchini {
 
@@ -49,6 +59,15 @@ std::unique_ptr<Disassembler> MakeDisassemblerWithoutFallback(
   }
 #endif  // BUILDFLAG(ENABLE_DEX)
 
+#if BUILDFLAG(ENABLE_ZTF)
+  if (DisassemblerZtf::QuickDetect(image)) {
+    // This disallows very short examples like "ZTxtxtZ\n" in ensemble patching.
+    auto disasm = Disassembler::Make<DisassemblerZtf>(image);
+    if (disasm && disasm->size() >= kMinProgramSize)
+      return disasm;
+  }
+#endif  // BUILDFLAG(ENABLE_ZTF)
+
   return nullptr;
 }
 
@@ -65,6 +84,10 @@ std::unique_ptr<Disassembler> MakeDisassemblerOfType(ConstBufferView image,
     case kExeTypeDex:
       return Disassembler::Make<DisassemblerDex>(image);
 #endif  // BUILDFLAG(ENABLE_DEX)
+#if BUILDFLAG(ENABLE_ZTF)
+    case kExeTypeZtf:
+      return Disassembler::Make<DisassemblerZtf>(image);
+#endif  // BUILDFLAG(ENABLE_ZTF)
     case kExeTypeNoOp:
       return Disassembler::Make<DisassemblerNoOp>(image);
     default:
