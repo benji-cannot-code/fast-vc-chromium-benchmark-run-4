34549e9a8f31398fa77bdfb4a4bd2615f590c4c3
Tsuyoshi Horo <horo@chromium.org>
2018-05-23 04:00:32 +0000

    Use net::ErrorToShortString() in SignedExchangeHandler::OnCertVerifyComplete()
    Currently when faild to verify the certificate while loading signed exchanges,
    only the error number is shown in DevTools.
    To show the error string (example: ERR_CERT_DATE_INVALID), this CL uses
    net::ErrorToShortString().
    
    Bug: 830505
    Change-Id: Ic93c6c9f532c67ca2ab0bfe6fbd32f0fb2359893
    Reviewed-on: https://chromium-review.googlesource.com/1068950
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560927}

--- content/browser/web_package/signed_exchange_handler.cc
+++ content/browser/web_package/signed_exchange_handler.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/url_loader_throttle.h"
 #include "mojo/public/cpp/system/string_data_pipe_producer.h"
 #include "net/base/io_buffer.h"
+#include "net/base/net_errors.h"
 #include "net/cert/cert_status_flags.h"
 #include "net/cert/cert_verifier.h"
 #include "net/cert/x509_certificate.h"
@@ -332,7 +333,8 @@ void SignedExchangeHandler::OnCertVerifyComplete(int result) {
   if (result != net::OK) {
     signed_exchange_utils::ReportErrorAndEndTraceEvent(
         devtools_proxy_.get(), "SignedExchangeHandler::OnCertVerifyComplete",
-        base::StringPrintf("Certificate verification error: %d", result));
+        base::StringPrintf("Certificate verification error: %s",
+                           net::ErrorToShortString(result).c_str()));
     RunErrorCallback(static_cast<net::Error>(result));
     return;
   }
