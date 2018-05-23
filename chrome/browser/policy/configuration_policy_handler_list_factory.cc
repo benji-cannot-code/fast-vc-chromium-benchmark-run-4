bc7ef0da886b4075d179dca76886ae828d0a58e7
Luciana Maroun <maroun@chromium.org>
2018-05-23 19:31:44 +0000

    Adding a policy handler for usage time limits and storing into prefs.
    Bug: 823536
    Change-Id: If7512cca762dafcb83e86619de86fe70701ec2d6
    Reviewed-on: https://chromium-review.googlesource.com/1069481
    Reviewed-by: Bartosz Fabianowski <bartfab@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Commit-Queue: Luciana Maroun <maroun@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561204}

--- chrome/browser/policy/configuration_policy_handler_list_factory.cc
+++ chrome/browser/policy/configuration_policy_handler_list_factory.cc
@@ -1220,6 +1220,11 @@ std::unique_ptr<ConfigurationPolicyHandlerList> BuildHandlerList(
       prefs::kNoteTakingAppsLockScreenWhitelist, false /*allow_wildcards*/));
   handlers->AddHandler(
       std::make_unique<SecondaryGoogleAccountSigninPolicyHandler>());
+  handlers->AddHandler(std::make_unique<SimpleSchemaValidatingPolicyHandler>(
+      key::kUsageTimeLimit, prefs::kUsageTimeLimit, chrome_schema,
+      SCHEMA_STRICT,
+      SimpleSchemaValidatingPolicyHandler::RECOMMENDED_PROHIBITED,
+      SimpleSchemaValidatingPolicyHandler::MANDATORY_ALLOWED));
 #endif  // defined(OS_CHROMEOS)
 
 #if BUILDFLAG(ENABLE_PLUGINS)
