757bca9904211a2f2356ae417cf8cdfe383c9f8d
Pawel Osciak <posciak@chromium.org>
2018-05-23 23:20:47 +0000

    Add a feature flag for VA-API VP8 video encoder.
    Add a feature flag to enable VA-API-based VP8 hardware video encode
    acceleration.
    
    BUG=794608
    TEST=run Chrome with feature disabled/enabled, verify availability
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I5866d7b158a071ea94b06d49453af1ec85ba3e2e
    Reviewed-on: https://chromium-review.googlesource.com/1068905
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Pawel Osciak <posciak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561303}

--- media/base/media_switches.h
+++ media/base/media_switches.h
@@ -132,6 +132,7 @@ MEDIA_EXPORT extern const base::Feature kUseAndroidOverlay;
 MEDIA_EXPORT extern const base::Feature kUseAndroidOverlayAggressively;
 MEDIA_EXPORT extern const base::Feature kUseNewMediaCache;
 MEDIA_EXPORT extern const base::Feature kUseR16Texture;
+MEDIA_EXPORT extern const base::Feature kVaapiVP8Encoder;
 MEDIA_EXPORT extern const base::Feature kVideoBlitColorAccuracy;
 MEDIA_EXPORT extern const base::Feature kUnifiedAutoplay;
 MEDIA_EXPORT extern const base::Feature kUseSurfaceLayerForVideo;
--- media/gpu/vaapi/vaapi_wrapper.cc
+++ media/gpu/vaapi/vaapi_wrapper.cc
@@ -25,6 +25,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sys_info.h"
 #include "build/build_config.h"
 
+#include "media/base/media_switches.h"
+
 // Auto-generated for dlopen libva libraries
 #include "media/gpu/vaapi/va_stubs.h"
 
@@ -177,6 +179,14 @@ bool IsBlackListedDriver(const std::string& va_vendor_string,
       return true;
     }
   }
+
+  // TODO(posciak): Remove once VP8 encoding is to be enabled by default.
+  if (mode == VaapiWrapper::CodecMode::kEncode &&
+      va_profile == VAProfileVP8Version0_3 &&
+      !base::FeatureList::IsEnabled(kVaapiVP8Encoder)) {
+    return true;
+  }
+
   return false;
 }
 
