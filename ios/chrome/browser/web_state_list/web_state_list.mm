e8ad8f4f079bc6e86bd4012117779fc06072b2c7
Mark Cogan <marq@google.com>
2018-05-23 12:31:43 +0000

    [iOS] Add mutation lock to WebStateList.
    This CL adds a mutation lock to WebStateList, to prevent observer
    callbacks from changing or deleting the WSL while a change to the WSL
    is in-flight.
    
    Another approach would have been to make the WebStateList passed back to
    observers a const WSL, but in many cases WSL observers already have
    another pointer to the WSL which they use for mutations.
    
    The lock implementation is straightforward, using a scoped lock object
    to set/unset lock state on the WebStateList. This isn't threadsafe, but
    WSL isn't, either. WSL will CHECK in the case of a double-lock, or of
    destruction while locked.
    
    Bug: 834263
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I3bf3512cec3297e7580cfc67c71bb12c05bc1cf5
    Reviewed-on: https://chromium-review.googlesource.com/1065632
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561045}

--- ios/chrome/browser/web_state_list/web_state_list.mm
+++ ios/chrome/browser/web_state_list/web_state_list.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <utility>
 
+#include "base/auto_reset.h"
 #include "base/logging.h"
 #import "ios/chrome/browser/web_state_list/web_state_list_delegate.h"
 #import "ios/chrome/browser/web_state_list/web_state_list_observer.h"
@@ -102,6 +103,7 @@ WebStateList::WebStateList(WebStateListDelegate* delegate)
 }
 
 WebStateList::~WebStateList() {
+  CHECK(!locked_);
   CloseAllWebStates(CLOSE_NO_FLAGS);
 }
 
@@ -155,32 +157,40 @@ int WebStateList::InsertWebState(int index,
                                  std::unique_ptr<web::WebState> web_state,
                                  int insertion_flags,
                                  WebStateOpener opener) {
-  if (IsInsertionFlagSet(insertion_flags, INSERT_INHERIT_OPENER))
-    opener = WebStateOpener(GetActiveWebState());
-
-  if (!IsInsertionFlagSet(insertion_flags, INSERT_FORCE_INDEX)) {
-    index = order_controller_->DetermineInsertionIndex(opener.opener);
-    if (index < 0 || count() < index)
-      index = count();
-  }
-
-  DCHECK(ContainsIndex(index) || index == count());
-  delegate_->WillAddWebState(web_state.get());
-
-  web::WebState* web_state_ptr = web_state.get();
-  web_state_wrappers_.insert(
-      web_state_wrappers_.begin() + index,
-      std::make_unique<WebStateWrapper>(std::move(web_state)));
-
-  if (active_index_ >= index)
-    ++active_index_;
-
   const bool activating = IsInsertionFlagSet(insertion_flags, INSERT_ACTIVATE);
-  for (auto& observer : observers_)
-    observer.WebStateInsertedAt(this, web_state_ptr, index, activating);
 
-  if (opener.opener)
-    SetOpenerOfWebStateAt(index, opener);
+  {
+    // Inner block for the mutation lock, because ActivateWebState might need to
+    // be called (if |activating| is true), and that method has its own mutation
+    // lock.
+    CHECK(!locked_);
+    base::AutoReset<bool> scoped_lock(&locked_, /* locked */ true);
+    if (IsInsertionFlagSet(insertion_flags, INSERT_INHERIT_OPENER))
+      opener = WebStateOpener(GetActiveWebState());
+
+    if (!IsInsertionFlagSet(insertion_flags, INSERT_FORCE_INDEX)) {
+      index = order_controller_->DetermineInsertionIndex(opener.opener);
+      if (index < 0 || count() < index)
+        index = count();
+    }
+
+    DCHECK(ContainsIndex(index) || index == count());
+    delegate_->WillAddWebState(web_state.get());
+
+    web::WebState* web_state_ptr = web_state.get();
+    web_state_wrappers_.insert(
+        web_state_wrappers_.begin() + index,
+        std::make_unique<WebStateWrapper>(std::move(web_state)));
+
+    if (active_index_ >= index)
+      ++active_index_;
+
+    for (auto& observer : observers_)
+      observer.WebStateInsertedAt(this, web_state_ptr, index, activating);
+
+    if (opener.opener)
+      SetOpenerOfWebStateAt(index, opener);
+  }
 
   if (activating)
     ActivateWebStateAt(index);
@@ -189,6 +199,8 @@ int WebStateList::InsertWebState(int index,
 }
 
 void WebStateList::MoveWebStateAt(int from_index, int to_index) {
+  CHECK(!locked_);
+  base::AutoReset<bool> scoped_lock(&locked_, /* locked */ true);
   DCHECK(ContainsIndex(from_index));
   DCHECK(ContainsIndex(to_index));
   if (from_index == to_index)
@@ -242,6 +254,8 @@ std::unique_ptr<web::WebState> WebStateList::ReplaceWebStateAt(
 }
 
 std::unique_ptr<web::WebState> WebStateList::DetachWebStateAt(int index) {
+  CHECK(!locked_);
+  base::AutoReset<bool> scoped_lock(&locked_, /* locked */ true);
   DCHECK(ContainsIndex(index));
   int new_active_index = order_controller_->DetermineNewActiveIndex(index);
 
@@ -276,8 +290,11 @@ std::unique_ptr<web::WebState> WebStateList::DetachWebStateAt(int index) {
 }
 
 void WebStateList::CloseWebStateAt(int index, int close_flags) {
+  // Lock after detaching, since that has its own lock.
   auto detached_web_state = DetachWebStateAt(index);
 
+  CHECK(!locked_);
+  base::AutoReset<bool> scoped_lock(&locked_, /* locked */ true);
   const bool user_action = IsClosingFlagSet(close_flags, CLOSE_USER_ACTION);
   for (auto& observer : observers_) {
     observer.WillCloseWebStateAt(this, detached_web_state.get(), index,
