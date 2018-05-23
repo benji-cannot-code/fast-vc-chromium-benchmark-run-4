5ad4e9d34eb9f3496951664365783f28be6b6d31
Sasha Morrissey <sashab@chromium.org>
2018-05-23 04:27:44 +0000

    Store can_share capability
    Store can_share capability from Google Drive API. This is pre-work to
    grey-out the 'Share' button for files without Sharing permission in
    the Files app.
    
    Bug: 719959
    Change-Id: I37281af81aeb5dfcb1a01a503ba4edb10686bf4d
    Reviewed-on: https://chromium-review.googlesource.com/1068551
    Reviewed-by: Stuart Langley <slangley@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Commit-Queue: Sasha Morrissey <sashab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560940}

--- components/drive/resource_entry_conversion.cc
+++ components/drive/resource_entry_conversion.cc
@@ -105,6 +105,8 @@ bool ConvertFileResourceToResourceEntry(
       capabilities.can_rename());
   converted.mutable_capabilities_info()->set_can_add_children(
       capabilities.can_add_children());
+  converted.mutable_capabilities_info()->set_can_share(
+      capabilities.can_share());
 
   if (input.IsDirectory()) {
     file_info->set_is_directory(true);
@@ -171,6 +173,8 @@ void ConvertTeamDriveResourceToResourceEntry(
       capabilities.can_rename_team_drive());
   out_entry->mutable_capabilities_info()->set_can_add_children(
       capabilities.can_add_children());
+  out_entry->mutable_capabilities_info()->set_can_share(
+      capabilities.can_share());
 }
 
 void ConvertResourceEntryToFileInfo(const ResourceEntry& entry,
