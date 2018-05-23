413a51c992fcc05939bbdc9a906ff19ae6788bb7
Yuichiro Hanada <yhanada@chromium.org>
2018-05-23 06:19:59 +0000

    Add arc::mojom::InputMethodManager{Host, Instance} interfaces.
    This CL adds input_method_manager.mojom file and stub implementation
    for InputMethodManagerHost interface.
    input_method_manager.mojom file defines the bridge interface
    between Chrome OS's InputMethodManager and Android's InputMethodManager.
    
    Bug: 845079
    Test: unit_tests
    Change-Id: I3d5a25c7774a1258fc0e1494fc351571327a89dd
    Reviewed-on: https://chromium-review.googlesource.com/1065560
    Commit-Queue: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Kazuhiro Inaba <kinaba@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560961}

--- components/arc/arc_bridge_service.h
+++ components/arc/arc_bridge_service.h
@@ -41,6 +41,8 @@ class FileSystemHost;
 class FileSystemInstance;
 class ImeHost;
 class ImeInstance;
+class InputMethodManagerHost;
+class InputMethodManagerInstance;
 class IntentHelperHost;
 class IntentHelperInstance;
 class KioskHost;
@@ -145,6 +147,11 @@ class ArcBridgeService {
     return &file_system_;
   }
   ConnectionHolder<mojom::ImeInstance, mojom::ImeHost>* ime() { return &ime_; }
+  ConnectionHolder<mojom::InputMethodManagerInstance,
+                   mojom::InputMethodManagerHost>*
+  input_method_manager() {
+    return &input_method_manager_;
+  }
   ConnectionHolder<mojom::IntentHelperInstance, mojom::IntentHelperHost>*
   intent_helper() {
     return &intent_helper_;
@@ -245,6 +252,9 @@ class ArcBridgeService {
   ConnectionHolder<mojom::FileSystemInstance, mojom::FileSystemHost>
       file_system_;
   ConnectionHolder<mojom::ImeInstance, mojom::ImeHost> ime_;
+  ConnectionHolder<mojom::InputMethodManagerInstance,
+                   mojom::InputMethodManagerHost>
+      input_method_manager_;
   ConnectionHolder<mojom::IntentHelperInstance, mojom::IntentHelperHost>
       intent_helper_;
   ConnectionHolder<mojom::KioskInstance, mojom::KioskHost> kiosk_;
--- components/arc/test/fake_arc_bridge_host.cc
+++ components/arc/test/fake_arc_bridge_host.cc
@@ -50,6 +50,9 @@ void FakeArcBridgeHost::OnFileSystemInstanceReady(
 
 void FakeArcBridgeHost::OnImeInstanceReady(mojom::ImeInstancePtr ime_ptr) {}
 
+void FakeArcBridgeHost::OnInputMethodManagerInstanceReady(
+    mojom::InputMethodManagerInstancePtr input_method_manager_ptr) {}
+
 void FakeArcBridgeHost::OnIntentHelperInstanceReady(
     mojom::IntentHelperInstancePtr intent_helper_ptr) {}
 
