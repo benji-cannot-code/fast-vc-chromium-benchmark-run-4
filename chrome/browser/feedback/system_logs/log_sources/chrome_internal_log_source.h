dc45f8783cc9b26e0ed51cf7eb624b10062c87ee
Steven Bennetts <stevenjb@chromium.org>
2018-05-23 20:14:32 +0000

    ChromeInternalLogSource: Get display info using mojo
    Test: Visit chrome://system, search for 'monitor_info', ensure
    information there is correct/consistent. (Test with multiple
    displays).
    
    Bug: 839265
    Change-Id: I9b11d5d9dba31ab498954aee7d08a328c3457203
    Reviewed-on: https://chromium-review.googlesource.com/1070315
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561229}

--- chrome/browser/feedback/system_logs/log_sources/chrome_internal_log_source.h
+++ chrome/browser/feedback/system_logs/log_sources/chrome_internal_log_source.h
@@ -10,6 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "components/feedback/system_logs/system_logs_source.h"
 
+#if defined(OS_CHROMEOS)
+#include "ash/public/interfaces/cros_display_config.mojom.h"
+#endif
+
 namespace system_logs {
 
 // Fetches internal Chrome logs.
@@ -29,13 +33,16 @@ class ChromeInternalLogSource : public SystemLogsSource {
 
 #if defined(OS_CHROMEOS)
   void PopulateLocalStateSettings(SystemLogsResponse* response);
-  void PopulateMonitorInfo(SystemLogsResponse* response);
 #endif  // defined(OS_CHROMEOS)
 
 #if defined(OS_WIN)
   void PopulateUsbKeyboardDetected(SystemLogsResponse* response);
   void PopulateEnrolledToDomain(SystemLogsResponse* response);
   void PopulateInstallerBrandCode(SystemLogsResponse* response);
+#endif
+
+#if defined(OS_CHROMEOS)
+  ash::mojom::CrosDisplayConfigControllerPtr cros_display_config_ptr_;
 #endif
 
   DISALLOW_COPY_AND_ASSIGN(ChromeInternalLogSource);
