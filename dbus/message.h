9fe97af8992d722de15628794b080693ffeea9bc
Abhishek Bhardwaj <abhishekbh@chromium.org>
2018-05-23 07:22:19 +0000

    dbus: Clang format files
    Clang format message logic files.
    
    BUG=None
    TEST=Compile.
    
    Change-Id: Ia13728381fd16705fba8091236622a42e7e59f0e
    Reviewed-on: https://chromium-review.googlesource.com/1069685
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Commit-Queue: Abhishek Bhardwaj <abhishekbh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560989}

--- dbus/message.h
+++ dbus/message.h
@@ -27,7 +27,6 @@ class MessageLite;
 }  // namespace protobuf
 }  // namespace google
 
-
 namespace dbus {
 
 class MessageWriter;
@@ -35,7 +34,7 @@ class MessageReader;
 
 // DBUS_TYPE_UNIX_FD was added in D-Bus version 1.4
 #if !defined(DBUS_TYPE_UNIX_FD)
-#define DBUS_TYPE_UNIX_FD      ((int) 'h')
+#define DBUS_TYPE_UNIX_FD ((int)'h')
 #endif
 
 // Returns true if Unix FD passing is supported in libdbus.
@@ -62,7 +61,7 @@ class CHROME_DBUS_EXPORT Message {
     MESSAGE_METHOD_RETURN = DBUS_MESSAGE_TYPE_METHOD_RETURN,
     MESSAGE_SIGNAL = DBUS_MESSAGE_TYPE_SIGNAL,
     MESSAGE_ERROR = DBUS_MESSAGE_TYPE_ERROR,
- };
+  };
 
   // The data type used in the D-Bus type system.  See the comment at
   // MessageType for why we are redefining data types here.
@@ -155,8 +154,7 @@ class CHROME_DBUS_EXPORT MethodCall : public Message {
   //   MethodCall method_call(DBUS_INTERFACE_INTROSPECTABLE, "Get");
   //
   // The constructor creates the internal raw message.
-  MethodCall(const std::string& interface_name,
-             const std::string& method_name);
+  MethodCall(const std::string& interface_name, const std::string& method_name);
 
   // Returns a newly created MethodCall from the given raw message of the
   // type DBUS_MESSAGE_TYPE_METHOD_CALL. Takes the ownership of |raw_message|.
@@ -183,8 +181,7 @@ class CHROME_DBUS_EXPORT Signal : public Message {
   //   Signal signal(DBUS_INTERFACE_INTROSPECTABLE, "PropertiesChanged");
   //
   // The constructor creates the internal raw_message_.
-  Signal(const std::string& interface_name,
-         const std::string& method_name);
+  Signal(const std::string& interface_name, const std::string& method_name);
 
   // Returns a newly created SIGNAL from the given raw message of the type
   // DBUS_MESSAGE_TYPE_SIGNAL. Takes the ownership of |raw_message|.
@@ -225,7 +222,7 @@ class CHROME_DBUS_EXPORT Response : public Message {
 
 // ErrorResponse is a type of message used to return an error to the
 // caller of a method.
-class CHROME_DBUS_EXPORT ErrorResponse: public Response {
+class CHROME_DBUS_EXPORT ErrorResponse : public Response {
  public:
   // Returns a newly created Response from the given raw message of the
   // type DBUS_MESSAGE_TYPE_METHOD_RETURN. Takes the ownership of |raw_message|.
@@ -486,7 +483,7 @@ class CHROME_DBUS_EXPORT MessageReader {
   bool CheckDataType(int dbus_type);
 
   // Helper function used to implement PopByte() etc.
-  bool PopBasic(int dbus_type, void *value);
+  bool PopBasic(int dbus_type, void* value);
 
   // Helper function used to implement PopArray() etc.
   bool PopContainer(int dbus_type, MessageReader* sub_reader);
--- dbus/message_unittest.cc
+++ dbus/message_unittest.cc
@@ -36,7 +36,7 @@ TEST(MessageTest, AppendAndPopByte) {
 
   uint8_t byte_value = 0;
   ASSERT_TRUE(reader.PopByte(&byte_value));
-  EXPECT_EQ(123, byte_value);  // Should match with the input.
+  EXPECT_EQ(123, byte_value);          // Should match with the input.
   ASSERT_FALSE(reader.HasMoreData());  // Should not have more data to read.
 
   // Try to get another byte. Should fail.
@@ -336,7 +336,6 @@ TEST(MessageTest, ProtoBuf) {
   EXPECT_EQ(receive_message.number(), send_message.number());
 }
 
-
 // Test that an array can be properly written and read. We only have this
 // test for array, as repeating this for other container types is too
 // redundant.
@@ -513,15 +512,16 @@ TEST(MessageTest, MethodCall) {
   MessageWriter writer(&method_call);
   writer.AppendString("payload");
 
-  EXPECT_EQ("message_type: MESSAGE_METHOD_CALL\n"
-            "destination: com.example.Service\n"
-            "path: /com/example/Object\n"
-            "interface: com.example.Interface\n"
-            "member: SomeMethod\n"
-            "signature: s\n"
-            "\n"
-            "string \"payload\"\n",
-            method_call.ToString());
+  EXPECT_EQ(
+      "message_type: MESSAGE_METHOD_CALL\n"
+      "destination: com.example.Service\n"
+      "path: /com/example/Object\n"
+      "interface: com.example.Interface\n"
+      "member: SomeMethod\n"
+      "signature: s\n"
+      "\n"
+      "string \"payload\"\n",
+      method_call.ToString());
 }
 
 TEST(MessageTest, MethodCall_FromRawMessage) {
@@ -545,14 +545,15 @@ TEST(MessageTest, Signal) {
   MessageWriter writer(&signal);
   writer.AppendString("payload");
 
-  EXPECT_EQ("message_type: MESSAGE_SIGNAL\n"
-            "path: /com/example/Object\n"
-            "interface: com.example.Interface\n"
-            "member: SomeSignal\n"
-            "signature: s\n"
-            "\n"
-            "string \"payload\"\n",
-            signal.ToString());
+  EXPECT_EQ(
+      "message_type: MESSAGE_SIGNAL\n"
+      "path: /com/example/Object\n"
+      "interface: com.example.Interface\n"
+      "member: SomeSignal\n"
+      "signature: s\n"
+      "\n"
+      "string \"payload\"\n",
+      signal.ToString());
 }
 
 TEST(MessageTest, Signal_FromRawMessage) {
@@ -586,7 +587,7 @@ TEST(MessageTest, Response_FromMethodCall) {
 
 TEST(MessageTest, ErrorResponse_FromMethodCall) {
   const uint32_t kSerial = 123;
-const char kErrorMessage[] = "error message";
+  const char kErrorMessage[] = "error message";
 
   MethodCall method_call("com.example.Interface", "SomeMethod");
   method_call.SetSerial(kSerial);
@@ -673,12 +674,13 @@ TEST(MessageTest, ToString_LongString) {
   MessageWriter writer(message.get());
   writer.AppendString(kLongString);
 
-  ASSERT_EQ("message_type: MESSAGE_METHOD_RETURN\n"
-            "signature: s\n\n"
-            "string \"oooooooooooooooooooooooooooooooooooooooooooooooo"
-            "oooooooooooooooooooooooooooooooooooooooooooooooooooo... "
-            "(1000 bytes in total)\"\n",
-            message->ToString());
+  ASSERT_EQ(
+      "message_type: MESSAGE_METHOD_RETURN\n"
+      "signature: s\n\n"
+      "string \"oooooooooooooooooooooooooooooooooooooooooooooooo"
+      "oooooooooooooooooooooooooooooooooooooooooooooooooooo... "
+      "(1000 bytes in total)\"\n",
+      message->ToString());
 }
 
 }  // namespace dbus
