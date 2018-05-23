1475be1b1448c3f5b2cd5667e9f8c3775595272b
Alan Cutter <alancutter@chromium.org>
2018-05-23 02:29:43 +0000

    Notify accessibility of origin string when PWA window opened
    The origin string is displayed in the title bar when PWA windows
    are opened as a security precaution. This CL ensures it is also
    spoken by ChromeVox for accessibility.
    
    Bug: 839177
    Change-Id: I622c45517ecddd2d85f680ae0fcae225e5be3ed9
    Reviewed-on: https://chromium-review.googlesource.com/1068891
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560902}

--- ash/frame/frame_header_origin_text.cc
+++ ash/frame/frame_header_origin_text.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/frame/frame_header_origin_text.h"
 
 #include "base/i18n/rtl.h"
+#include "ui/accessibility/ax_node_data.h"
 #include "ui/compositor/layer_animation_element.h"
 #include "ui/compositor/layer_animation_sequence.h"
 #include "ui/compositor/scoped_layer_animation_settings.h"
@@ -100,11 +101,18 @@ void FrameHeaderOriginText::StartSlideAnimation() {
 
   label_layer->GetAnimator()->StartTogether(
       {opacity_sequence.release(), transform_sequence.release()});
+
+  NotifyAccessibilityEvent(ax::mojom::Event::kValueChanged, true);
 }
 
 base::TimeDelta FrameHeaderOriginText::AnimationDuration() {
   return kOriginSlideInDuration + kOriginPauseDuration +
          kOriginSlideOutDuration;
+}
+
+void FrameHeaderOriginText::GetAccessibleNodeData(ui::AXNodeData* node_data) {
+  node_data->role = ax::mojom::Role::kApplication;
+  node_data->SetName(label_->text());
 }
 
 }  // namespace ash
