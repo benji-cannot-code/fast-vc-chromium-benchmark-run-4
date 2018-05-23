8f83978592d8697b38a9453b0f5797c7b3015f38
Rob Buis <rbuis@igalia.com>
2018-05-23 20:15:02 +0000

    Remove unneeded includes in SignatureVerifier
    Change-Id: I39cc066ce361060d1ca76b88e40d6e41d22fb808
    Reviewed-on: https://chromium-review.googlesource.com/1063725
    Commit-Queue: Rob Buis <rbuis@igalia.com>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561230}

--- crypto/signature_verifier.cc
+++ crypto/signature_verifier.cc
@@ -5,11 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "crypto/signature_verifier.h"
 
-#include <stdint.h>
-
-#include <memory>
-#include <vector>
-
 #include "base/logging.h"
 #include "crypto/openssl_util.h"
 #include "third_party/boringssl/src/include/openssl/bytestring.h"
