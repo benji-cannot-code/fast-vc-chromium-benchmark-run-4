4e63b95b5ea6d2d5c172bc209ad1230a586b3bd4
Rayan Kanso <rayankans@chromium.org>
2018-05-23 12:44:25 +0000

    [Background Fetch] Create DatabaseTask to get num requests by type
    TBR=avi@chromium.org
    
    Bug: 826257
    Change-Id: I91ea969e3de8630365a115802ff6ed93901a1d00
    Reviewed-on: https://chromium-review.googlesource.com/1066132
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561046}

--- content/browser/background_fetch/background_fetch_data_manager.cc
+++ content/browser/background_fetch/background_fetch_data_manager.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/background_fetch/storage/delete_registration_task.h"
 #include "content/browser/background_fetch/storage/get_developer_ids_task.h"
 #include "content/browser/background_fetch/storage/get_metadata_task.h"
+#include "content/browser/background_fetch/storage/get_num_requests_task.h"
 #include "content/browser/background_fetch/storage/get_settled_fetches_task.h"
 #include "content/browser/background_fetch/storage/mark_registration_for_deletion_task.h"
 #include "content/browser/background_fetch/storage/mark_request_complete_task.h"
@@ -609,7 +610,9 @@ void BackgroundFetchDataManager::GetNumCompletedRequests(
 
   if (base::CommandLine::ForCurrentProcess()->HasSwitch(
           switches::kEnableBackgroundFetchPersistence)) {
-    // TODO(crbug.com/826257): Create database task to get number of requests.
+    AddDatabaseTask(std::make_unique<background_fetch::GetNumRequestsTask>(
+        this, registration_id, background_fetch::RequestType::kCompleted,
+        std::move(callback)));
     return;
   }
 
