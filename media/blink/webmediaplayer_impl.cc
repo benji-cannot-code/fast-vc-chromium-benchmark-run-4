c028c3d3de0f1fb8398b542ab7dc011ea1cc25fa
Fredrik Hubinette <hubbe@google.com>
2018-05-24 01:00:51 +0000

    media: Avoid closing connection if we're going to play soon
    Bug: 836952
    Change-Id: I381042fd107b954b5d47f1dae7e392e5bd3752dc
    Reviewed-on: https://chromium-review.googlesource.com/1069804
    Commit-Queue: Fredrik Hubinette <hubbe@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561345}

--- media/blink/webmediaplayer_impl.cc
+++ media/blink/webmediaplayer_impl.cc
@@ -1771,7 +1771,7 @@ void WebMediaPlayerImpl::OnBufferingStateChangeInternal(
 
     // Let the DataSource know we have enough data. It may use this information
     // to release unused network connections.
-    if (data_source_)
+    if (data_source_ && !client_->CouldPlayIfEnoughData())
       data_source_->OnBufferingHaveEnough(false);
 
     // Blink expects a timeChanged() in response to a seek().
