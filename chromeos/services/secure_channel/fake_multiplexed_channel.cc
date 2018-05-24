23f26bdbe7c98f18678336bb62902bf060ab5e25
Kyle Horimoto <khorimoto@google.com>
2018-05-24 22:45:34 +0000

    [CrOS MultiDevice] Update FakeMultiplexedChannel.
    Now, when a FakeMultiplexedChannel is disconnected, it notifies its
    delegate.
    
    Bug: 824568, 752273
    Change-Id: I52a5368b374c7633a09a5c154a37f972d7a207c7
    Reviewed-on: https://chromium-review.googlesource.com/1072729
    Reviewed-by: James Hawkins <jhawkins@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561665}

--- chromeos/services/secure_channel/fake_multiplexed_channel.cc
+++ chromeos/services/secure_channel/fake_multiplexed_channel.cc
@@ -31,6 +31,8 @@ void FakeMultiplexedChannel::SetDisconnected() {
   DCHECK(!is_disconnected_);
   is_disconnecting_ = false;
   is_disconnected_ = true;
+
+  NotifyDisconnected();
 }
 
 bool FakeMultiplexedChannel::IsDisconnecting() const {
