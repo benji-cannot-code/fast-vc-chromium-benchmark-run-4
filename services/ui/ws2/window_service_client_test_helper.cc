613e2e688fbff6210370b2122018d81c7344ce29
Scott Violet <sky@chromium.org>
2018-05-24 16:33:26 +0000

    chromeos: make KeyEvents go to the embedder if the embedder intercepts events
    This is needed as KeyEvents are sent directly to the focused window, where as
    LocatedEvents consider the WindowTargeter of each window.
    
    BUG=837692
    TEST=covered by tests
    
    Change-Id: I935aaa1aaea6a8ea0339e99cc076ee7d459d9034
    Reviewed-on: https://chromium-review.googlesource.com/1070857
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561521}

--- services/ui/ws2/window_service_client_test_helper.cc
+++ services/ui/ws2/window_service_client_test_helper.cc
@@ -93,7 +93,7 @@ void WindowServiceClientTestHelper::SetWindowProperty(
       change_id, TransportIdForWindow(window), name, value);
 }
 
-WindowServiceClient* WindowServiceClientTestHelper::Embed(
+WindowServiceClientBinding* WindowServiceClientTestHelper::Embed(
     aura::Window* window,
     mojom::WindowTreeClientPtr client_ptr,
     mojom::WindowTreeClient* client,
@@ -104,8 +104,7 @@ WindowServiceClient* WindowServiceClientTestHelper::Embed(
           std::move(client_ptr), client, embed_flags)) {
     return nullptr;
   }
-  return window_service_client_->embedded_client_bindings_.back()
-      ->window_service_client();
+  return window_service_client_->embedded_client_bindings_.back().get();
 }
 
 void WindowServiceClientTestHelper::SetEventTargetingPolicy(
@@ -129,6 +128,12 @@ void WindowServiceClientTestHelper::SetCanFocus(aura::Window* window,
                                                 bool can_focus) {
   window_service_client_->SetCanFocus(
       window_service_client_->TransportIdForWindow(window), can_focus);
+}
+
+void WindowServiceClientTestHelper::DestroyBinding(
+    WindowServiceClientBinding* binding) {
+  // Triggers WindowServiceClient to delete WindowServiceClientBinding.
+  window_service_client_->OnChildBindingConnectionLost(binding);
 }
 
 ClientWindowId WindowServiceClientTestHelper::ClientWindowIdForWindow(
