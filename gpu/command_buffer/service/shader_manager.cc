ceda4fb13cdaaa80b88fbf32b559a42315509d73
Andre Goddard Rosa <andre.rosa@lge.com>
2018-05-24 18:36:10 +0000

    Don't hide releasing shader resources in usage count
    - Makes implementation symmetric to program manager
    - DecUseCount() was not only doing what's named after
    - Also decreases binary size:
              text    data     bss     dec     hex filename
    before 6883184  193376    2012 7078572  6c02ac out/Release/libgles2.so
    after  6882668  193360    2012 7078040  6c0098 out/Release/libgles2.so
    
    R=piman@chromium.org
    
    Bug: 846385
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I51fc0b37ab1f59e4fc54fb11b43cc480209a0479
    Reviewed-on: https://chromium-review.googlesource.com/1071009
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Andre Goddard Rosa <andre.rosa@lge.com>
    Cr-Commit-Position: refs/heads/master@{#561564}

--- gpu/command_buffer/service/shader_manager.cc
+++ gpu/command_buffer/service/shader_manager.cc
@@ -42,9 +42,7 @@ Shader::Shader(GLuint service_id, GLenum shader_type)
 Shader::~Shader() = default;
 
 void Shader::Destroy() {
-  if (service_id_) {
-    DeleteServiceID();
-  }
+  DeleteServiceID();
 }
 
 void Shader::RequestCompile(scoped_refptr<ShaderTranslatorInterface> translator,
@@ -147,32 +145,18 @@ void Shader::RefreshTranslatedShaderSource() {
   }
 }
 
-void Shader::IncUseCount() {
-  ++use_count_;
-}
-
-void Shader::DecUseCount() {
-  --use_count_;
-  DCHECK_GE(use_count_, 0);
-  if (service_id_ && use_count_ == 0 && marked_for_deletion_) {
-    DeleteServiceID();
-  }
-}
-
 void Shader::MarkForDeletion() {
   DCHECK(!marked_for_deletion_);
   DCHECK_NE(service_id_, 0u);
 
   marked_for_deletion_ = true;
-  if (use_count_ == 0) {
-    DeleteServiceID();
-  }
 }
 
 void Shader::DeleteServiceID() {
-  DCHECK_NE(service_id_, 0u);
-  glDeleteShader(service_id_);
-  service_id_ = 0;
+  if (service_id_) {
+    glDeleteShader(service_id_);
+    service_id_ = 0;
+  }
 }
 
 const sh::Attribute* Shader::GetAttribInfo(const std::string& name) const {
@@ -336,10 +320,11 @@ bool ShaderManager::IsOwned(Shader* shader) {
   return false;
 }
 
-void ShaderManager::RemoveShader(Shader* shader) {
+void ShaderManager::RemoveShaderIfUnused(Shader* shader) {
   DCHECK(shader);
   DCHECK(IsOwned(shader));
   if (shader->IsDeleted() && !shader->InUse()) {
+    shader->DeleteServiceID();
     for (ShaderMap::iterator it = shaders_.begin();
          it != shaders_.end(); ++it) {
       if (it->second.get() == shader) {
@@ -355,7 +340,7 @@ void ShaderManager::Delete(Shader* shader) {
   DCHECK(shader);
   DCHECK(IsOwned(shader));
   shader->MarkForDeletion();
-  RemoveShader(shader);
+  RemoveShaderIfUnused(shader);
 }
 
 void ShaderManager::UseShader(Shader* shader) {
@@ -368,7 +353,7 @@ void ShaderManager::UnuseShader(Shader* shader) {
   DCHECK(shader);
   DCHECK(IsOwned(shader));
   shader->DecUseCount();
-  RemoveShader(shader);
+  RemoveShaderIfUnused(shader);
 }
 
 }  // namespace gles2
