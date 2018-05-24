bd7ae1e35ad31c4a7e2ec13e053fc0dff6369320
Chase Phillips <cmp@chromium.org>
2018-05-24 21:16:15 +0000

    IndexedDB: include tx id in tracing output
    Bug: 846408
    Change-Id: Ib2b7fe4adcc948b88a6fbc46f9a2a678ce016aa3
    Reviewed-on: https://chromium-review.googlesource.com/1072187
    Commit-Queue: Chase Phillips <cmp@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561630}

--- third_party/blink/renderer/modules/indexeddb/idb_transaction.cc
+++ third_party/blink/renderer/modules/indexeddb/idb_transaction.cc
@@ -391,7 +391,7 @@ void IDBTransaction::OnResultReady() {
 }
 
 void IDBTransaction::OnAbort(DOMException* error) {
-  IDB_TRACE("IDBTransaction::onAbort");
+  IDB_TRACE1("IDBTransaction::onAbort", "txn.id", id_);
   if (!GetExecutionContext()) {
     Finished();
     return;
@@ -419,7 +419,7 @@ void IDBTransaction::OnAbort(DOMException* error) {
 }
 
 void IDBTransaction::OnComplete() {
-  IDB_TRACE("IDBTransaction::onComplete");
+  IDB_TRACE1("IDBTransaction::onComplete", "txn.id", id_);
   if (!GetExecutionContext()) {
     Finished();
     return;
@@ -509,7 +509,7 @@ const char* IDBTransaction::InactiveErrorMessage() const {
 }
 
 DispatchEventResult IDBTransaction::DispatchEventInternal(Event* event) {
-  IDB_TRACE("IDBTransaction::dispatchEvent");
+  IDB_TRACE1("IDBTransaction::dispatchEvent", "txn.id", id_);
   if (!GetExecutionContext()) {
     state_ = kFinished;
     return DispatchEventResult::kCanceledBeforeDispatch;
