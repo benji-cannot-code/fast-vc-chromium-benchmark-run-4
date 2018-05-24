d41d92dab9b2fb601c32b243f4267b8b8e662dae
Aaron Leventhal <aleventhal@chromium.org>
2018-05-24 16:41:21 +0000

    Remove errant code
    This code does not have tests and is likely unnecessary.
    
    HandleActiveDecendant is called on the item with @aria-activedescendant,
    therefore to invalidate something, followingthe forward relation is
    correct. Using the reverse relation would make sense if something was
    pointing at the element with @aria-activedescendant that needed
    updating.
    
    TBR=nektar@chromium.org
    NOTRY=true
    
    Bug: None
    Change-Id: Ifc2e3d71a2089e5fbeb58c1bc6e020811ad9b1d6
    Reviewed-on: https://chromium-review.googlesource.com/1070806
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561522}

--- third_party/blink/renderer/modules/accessibility/ax_layout_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_layout_object.cc
@@ -2228,11 +2228,6 @@ void AXLayoutObject::HandleActiveDescendantChanged() {
   if (!GetLayoutObject())
     return;
 
-  Vector<String> ids;
-  TokenVectorFromAttribute(ids, aria_activedescendantAttr);
-  if (!ids.IsEmpty())
-    AXObjectCache().UpdateReverseRelations(this, ids);
-
   AXObject* focused_object = AXObjectCache().FocusedObject();
   if (focused_object == this && SupportsARIAActiveDescendant()) {
     AXObject* active_descendant = ActiveDescendant();
