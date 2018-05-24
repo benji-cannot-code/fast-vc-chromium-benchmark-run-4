26051096bb019530336d5805e2b1f9d1cda5b166
Brandon Jones <bajones@chromium.org>
2018-05-24 23:32:13 +0000

    Adjust VRShell floor grid to align with users physical floor.
    If GVR reports a floor offset from the current tracking origin the floor
    grid will be adjusted to render at the same offset. This should help
    user comfort when using standalone devices.
    
    Bug: 845225
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_vr
    Change-Id: I08bd081087f4f6528ec8200ee424d08f9bf9f059
    Reviewed-on: https://chromium-review.googlesource.com/1067541
    Commit-Queue: Brandon Jones <bajones@chromium.org>
    Reviewed-by: Christopher Grant <cjgrant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561690}

--- chrome/browser/vr/ui.h
+++ chrome/browser/vr/ui.h
@@ -160,6 +160,7 @@ class Ui : public BrowserUiInterface, public KeyboardUiInterface {
   void OnContentBoundsChanged(int width, int height);
   void OnPlatformControllerInitialized(PlatformController* controller);
   void OnUiRequestedNavigation();
+  void SetFloorHeight(float floor_height);
 
   Model* model_for_test() { return model_.get(); }
 
