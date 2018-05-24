1060711f9ba5798fdc6f8979c3eab7e91494202c
Jinsuk Kim <jinsukkim@chromium.org>
2018-05-24 02:19:46 +0000

    Android: Make MotionEventSynthesizer a public interface
    MotionEventSynthesizer class is used by Chrome (VR) and content
    internally. This CL splits it into public interface with create
    method and implementation class. Helps remove the exception rule
    in DEPS.
    
    Bug: 617324
    Change-Id: I99fa8c01089ede078a7c2b2929897fdbbd7b40aa
    Reviewed-on: https://chromium-review.googlesource.com/1058873
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Maria Khomenko <mariakhomenko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561360}

--- chrome/android/java/src/org/chromium/chrome/browser/vr_shell/AndroidUiGestureTarget.java
+++ chrome/android/java/src/org/chromium/chrome/browser/vr_shell/AndroidUiGestureTarget.java
@@ -10,7 +10,7 @@ import android.view.View;
 
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
-import org.chromium.content.browser.MotionEventSynthesizer;
+import org.chromium.content_public.browser.MotionEventSynthesizer;
 
 /**
  * Forwards events for Java native UI pages to MotionEventSynthesizer.
@@ -22,7 +22,7 @@ public class AndroidUiGestureTarget {
 
     public AndroidUiGestureTarget(
             View target, float scaleFactor, float scrollRatio, int touchSlop) {
-        mMotionEventSynthesizer = new MotionEventSynthesizer(target);
+        mMotionEventSynthesizer = MotionEventSynthesizer.create(target);
         mNativePointer = nativeInit(scaleFactor, scrollRatio, touchSlop);
     }
 
--- content/public/android/java/src/org/chromium/content/browser/MotionEventSynthesizerImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/MotionEventSynthesizerImpl.java
@@ -11,10 +11,12 @@ import android.view.MotionEvent.PointerCoords;
 import android.view.MotionEvent.PointerProperties;
 import android.view.View;
 
+import org.chromium.content_public.browser.MotionEventSynthesizer;
+
 /**
  * Injects synthetic touch events. All the coordinates are of physical unit.
  */
-public class MotionEventSynthesizer {
+public class MotionEventSynthesizerImpl implements MotionEventSynthesizer {
     private static final int MAX_NUM_POINTERS = 16;
 
     private final PointerProperties[] mPointerProperties;
@@ -22,7 +24,11 @@ public class MotionEventSynthesizer {
     private final View mTarget;
     private long mDownTimeInMs;
 
-    public MotionEventSynthesizer(View target) {
+    public static MotionEventSynthesizerImpl create(View target) {
+        return new MotionEventSynthesizerImpl(target);
+    }
+
+    private MotionEventSynthesizerImpl(View target) {
         assert target != null;
         mTarget = target;
         mPointerProperties = new PointerProperties[MAX_NUM_POINTERS];
@@ -38,6 +44,7 @@ public class MotionEventSynthesizer {
      * @param id Id property of the point.
      * @param toolType ToolType property of the point.
      */
+    @Override
     public void setPointer(int index, int x, int y, int id, int toolType) {
         assert (0 <= index && index < MAX_NUM_POINTERS);
 
@@ -86,6 +93,7 @@ public class MotionEventSynthesizer {
      * @param pointerCount The number of points associated with the event.
      * @param timeInMs Timestamp for the event.
      */
+    @Override
     public void inject(int action, int pointerCount, long timeInMs) {
         switch (action) {
             case MotionEventAction.START: {
