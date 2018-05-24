240592ecbac642cafe69f93fcf2c17fc9000b898
Keishi Hattori <keishi@chromium.org>
2018-05-24 08:06:49 +0000

    Oilpan: Clear all Persistents on thread termination
    Clear all persistents on thread termination, because when we have a bug and it leaves a Persistent behind, it will not be a stale pointer and just causes null dereference.
    
    Since we disabled PersistentHeapCollections on non-main threads we can assume all PersistentNode::Self() can be cast to Persistent<DummyGCBase>
    
    Bug: 831117
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: Ib55b280f8e7c5d966ce43d3f60f2af873cd50739
    Reviewed-on: https://chromium-review.googlesource.com/1025547
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561418}

--- third_party/blink/renderer/platform/heap/persistent_node.h
+++ third_party/blink/renderer/platform/heap/persistent_node.h
@@ -152,6 +152,7 @@ class PLATFORM_EXPORT PersistentRegion final {
       Visitor*,
       ShouldTraceCallback = PersistentRegion::ShouldTracePersistentNode);
   int NumberOfPersistents();
+  void PrepareForThreadStateTermination();
 
  private:
   friend CrossThreadPersistentRegion;
