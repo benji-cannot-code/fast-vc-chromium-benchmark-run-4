eb94dadc21873d4b77c8da33ba3f6544e5b4d778
John Chen <johnchen@chromium.org>
2018-05-24 18:26:28 +0000

    [ChromeDriver] Fix race conditions in websocket
    Fixes two race conditions in ChromeDriver's websocket code (for
    connecting to DevTools):
    
    * If a connection is made after SyncWebSocketImpl::Core::Connect has
      timedout and returned, lower level code can access stack variables
      that have already gone out of scope.
    
    * If a connection is closed right after it was opened, ChromeDriver
      might try to read from a closed socket.
    
    Bug: chromedriver:2431
    Change-Id: I6895856256f610615f63220bc33c34bacab2eeda
    Reviewed-on: https://chromium-review.googlesource.com/1070571
    Commit-Queue: John Chen <johnchen@chromium.org>
    Reviewed-by: Jonathon Kereliuk <kereliuk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561558}

--- chrome/test/chromedriver/net/sync_websocket_impl.h
+++ chrome/test/chromedriver/net/sync_websocket_impl.h
@@ -77,6 +77,7 @@ class SyncWebSocketImpl : public SyncWebSocket {
     void SendOnIO(const std::string& message,
                   bool* result,
                   base::WaitableEvent* event);
+    void CloseOnIO(base::WaitableEvent* event);
 
     // OnDestruct is meant to ensure deletion on the IO thread.
     void OnDestruct() const;
