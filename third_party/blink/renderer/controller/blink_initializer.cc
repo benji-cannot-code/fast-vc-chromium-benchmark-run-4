9df947c20b25cc7e544f798066071c57ccecd7b1
Takashi SAKAMOTO <tasak@google.com>
2018-05-24 11:43:16 +0000

    Add RendererMemoryAblation features for renderer memory ablation experiment.
    Bug: 845371
    Change-Id: I6ccd560af4739f472035b6ff357346e7d6cc4cf8
    Reviewed-on: https://chromium-review.googlesource.com/1071048
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561465}

--- third_party/blink/renderer/controller/blink_initializer.cc
+++ third_party/blink/renderer/controller/blink_initializer.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "build/build_config.h"
+#include "third_party/blink/public/common/experiments/memory_ablation_experiment.h"
 #include "third_party/blink/public/platform/interface_registry.h"
 #include "third_party/blink/public/platform/platform.h"
 #include "third_party/blink/public/platform/web_thread.h"
@@ -127,6 +128,13 @@ void Initialize(Platform* platform, service_manager::BinderRegistry* registry) {
     DCHECK(!g_end_of_task_runner);
     g_end_of_task_runner = new EndOfTaskRunner;
     current_thread->AddTaskObserver(g_end_of_task_runner);
+  }
+
+  if (WebThread* main_thread = Platform::Current()->MainThread()) {
+    scoped_refptr<base::SequencedTaskRunner> task_runner =
+        main_thread->GetTaskRunner();
+    if (task_runner)
+      MemoryAblationExperiment::MaybeStartForRenderer(task_runner);
   }
 }
 
