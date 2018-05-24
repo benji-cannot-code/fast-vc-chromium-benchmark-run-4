a31b35b146f9e9ba6478df201292c2d4644dc94f
Fredrik Söderquist <fs@opera.com>
2018-05-24 15:46:33 +0000

    Remove stray, unneeded (and incorrect) forward declaration
    WebContentsObserver is in the 'content' namespace, not in the global.
    This causes a collision with a 'using' directive for the same class,
    which breaks Jumbo builds.
    
    TBR=avi@chromium.org,siggi@chromium.org
    
    Bug: 755840, 846328
    Change-Id: I977106611b374c955bf3c5c161c8a52eea927698
    Reviewed-on: https://chromium-review.googlesource.com/1071663
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Sigurður Ásgeirsson <siggi@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#561512}

--- chrome/browser/resource_coordinator/tab_load_tracker.h
+++ chrome/browser/resource_coordinator/tab_load_tracker.h
@@ -14,8 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sequence_checker.h"
 #include "base/strings/string16.h"
 
-class WebContentsObserver;
-
 namespace content {
 class WebContents;
 }  // namespace content
