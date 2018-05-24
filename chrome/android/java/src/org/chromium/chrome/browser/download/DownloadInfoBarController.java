894ab267ea007e8c637d5bf61a2a2750fdefbe74
Shakti Sahu <shaktisahu@chromium.org>
2018-05-24 15:10:00 +0000

    Download infobar : Adding tests
    This CL sets up the test class for the download infobar controller and
    adds a few tests. More extensive tests will be added in the subsequent CLs.
    
    Bug: 845730
    Change-Id: I41e64887bd2da1330743ec4a32cdd08766bccb90
    Reviewed-on: https://chromium-review.googlesource.com/1069787
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561506}

--- chrome/android/java/src/org/chromium/chrome/browser/download/DownloadInfoBarController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/DownloadInfoBarController.java
@@ -14,6 +14,7 @@ import android.text.format.Formatter;
 
 import org.chromium.base.Callback;
 import org.chromium.base.ContextUtils;
+import org.chromium.base.VisibleForTesting;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.chrome.R;
@@ -75,7 +76,8 @@ public class DownloadInfoBarController implements OfflineContentProvider.Observe
      * Note: This enum is append-only and the values must match the DownloadInfoBarState enum in
      * enums.xml.
      */
-    private enum DownloadInfoBarState {
+    @VisibleForTesting
+    protected enum DownloadInfoBarState {
         // Default initial state. It is also the final state after all the downloads are paused or
         // removed. No InfoBar is shown in this state.
         INITIAL,
@@ -384,6 +386,7 @@ public class DownloadInfoBarController implements OfflineContentProvider.Observe
         boolean isNewDownload = forceShowDownloadStarted
                 || (updatedItem != null && updatedItem.state == OfflineItemState.IN_PROGRESS
                            && !mSeenItems.contains(updatedItem.id));
+        boolean itemResumedFromPending = itemResumedFromPending(updatedItem);
 
         if (updatedItem != null) {
             mTrackedItems.put(updatedItem.id, updatedItem);
@@ -442,7 +445,7 @@ public class DownloadInfoBarController implements OfflineContentProvider.Observe
                     boolean currentlyShowingPending = mCurrentInfo != null
                             && mCurrentInfo.resultState != null
                             && mCurrentInfo.resultState == OfflineItemState.PENDING;
-                    if (currentlyShowingPending && itemResumedFromPending(updatedItem)) {
+                    if (currentlyShowingPending && itemResumedFromPending) {
                         nextState = DownloadInfoBarState.DOWNLOADING;
                     }
                     if (itemWasRemoved && mTrackedItems.size() == 0) {
@@ -612,8 +615,8 @@ public class DownloadInfoBarController implements OfflineContentProvider.Observe
         clearEndTimerRunnable();
 
         if (startTimer) {
-            long delay = showAccelerating ? DURATION_ACCELERATED_INFOBAR_IN_MS
-                                          : DURATION_SHOW_RESULT_IN_MS;
+            long delay =
+                    showAccelerating ? getDurationAcceleratedInfoBar() : getDurationShowResult();
             mEndTimerRunnable = () -> {
                 mEndTimerRunnable = null;
                 if (mCurrentInfo != null) mCurrentInfo.resultState = null;
@@ -663,6 +666,16 @@ public class DownloadInfoBarController implements OfflineContentProvider.Observe
                 && updatedItem.state == OfflineItemState.IN_PROGRESS;
     }
 
+    @VisibleForTesting
+    protected long getDurationAcceleratedInfoBar() {
+        return DURATION_ACCELERATED_INFOBAR_IN_MS;
+    }
+
+    @VisibleForTesting
+    protected long getDurationShowResult() {
+        return DURATION_SHOW_RESULT_IN_MS;
+    }
+
     /**
      * Central function called to show an InfoBar. If the previous InfoBar was on a different
      * tab which is currently not active, based on the value of |info.forceReparent|, it is
@@ -670,7 +683,8 @@ public class DownloadInfoBarController implements OfflineContentProvider.Observe
      * @param state The state of the infobar to be shown.
      * @param info Contains the information to be displayed in the UI.
      */
-    private void showInfoBar(DownloadInfoBarState state, DownloadProgressInfoBarData info) {
+    @VisibleForTesting
+    protected void showInfoBar(DownloadInfoBarState state, DownloadProgressInfoBarData info) {
         if (!FeatureUtilities.isDownloadProgressInfoBarEnabled()) return;
 
         mCurrentInfo = info;
@@ -715,7 +729,8 @@ public class DownloadInfoBarController implements OfflineContentProvider.Observe
         mCurrentInfoBar.updateInfoBar(info);
     }
 
-    private void closePreviousInfoBar() {
+    @VisibleForTesting
+    protected void closePreviousInfoBar() {
         if (mCurrentInfoBar == null) return;
 
         Tab prevTab = mCurrentInfoBar.getTab();
