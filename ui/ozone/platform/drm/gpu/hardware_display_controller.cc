ed6a486d2b338bddfb9caacc02bb1ec2f43453f0
Findit <findit-for-me@appspot.gserviceaccount.com>
2018-05-24 21:42:26 +0000

    Revert "[Ozone-DRM] Refactor plane management and add tests"
    This reverts commit ce7674644d13e37b274716876a3867e1b1dd8b8d.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 561616 as the
    culprit for failures in the build cycles as shown on:
    https://findit-for-me.appspot.com/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtL2NlNzY3NDY0NGQxM2UzN2IyNzQ3MTY4NzZhMzg2N2UxYjFkZDhiOGQM
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium.chromiumos/linux-chromeos-rel/8468
    
    Sample Failed Step: ozone_unittests
    
    Original change's description:
    > [Ozone-DRM] Refactor plane management and add tests
    > 
    > * We use plane properties on legacy DRM to get some details (plane
    > type, in_formats). Read the properties in HardwareDisplayPlane and
    > check that the required properties are available in DRM atomic.
    > * Split dummy plane into its own class to clean up code.
    > * Remove test plane since we can now create the real objects via
    > injected state in DrmDevice.
    > * Cleanup plane initialization.
    > * Remove MockHardwareDisplayPlaneManager and rely on MockDrmDevice to
    > inject the necessary state.
    > 
    > BUG=839487
    > TEST=Ran unittests
    > 
    > Change-Id: I03c91620557dcf7426629cf97bb989d8d7e83c68
    > Reviewed-on: https://chromium-review.googlesource.com/1048585
    > Commit-Queue: Daniel Nicoara <dnicoara@chromium.org>
    > Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#561616}
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    BUG=839487
    
    Change-Id: Ie87a4ecea2b6c31aa25acd3633cda1b7547b0151
    Reviewed-on: https://chromium-review.googlesource.com/1072667
    Cr-Commit-Position: refs/heads/master@{#561644}

--- ui/ozone/platform/drm/gpu/hardware_display_controller.cc
+++ ui/ozone/platform/drm/gpu/hardware_display_controller.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/ozone/platform/drm/gpu/crtc_controller.h"
 #include "ui/ozone/platform/drm/gpu/drm_buffer.h"
 #include "ui/ozone/platform/drm/gpu/drm_device.h"
-#include "ui/ozone/platform/drm/gpu/hardware_display_plane.h"
 #include "ui/ozone/platform/drm/gpu/page_flip_request.h"
 
 namespace ui {
--- ui/ozone/platform/drm/gpu/hardware_display_plane_manager.h
+++ ui/ozone/platform/drm/gpu/hardware_display_plane_manager.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/display/types/gamma_ramp_rgb_entry.h"
 #include "ui/ozone/platform/drm/common/scoped_drm_types.h"
 #include "ui/ozone/platform/drm/gpu/drm_device.h"
+#include "ui/ozone/platform/drm/gpu/hardware_display_plane.h"
 #include "ui/ozone/platform/drm/gpu/overlay_plane.h"
 
 namespace gfx {
@@ -24,7 +25,6 @@ class Rect;
 namespace ui {
 
 class CrtcController;
-class HardwareDisplayPlane;
 
 // This contains the list of planes controlled by one HDC on a given DRM fd.
 // It is owned by the HDC and filled by the CrtcController.
@@ -151,7 +151,9 @@ class HardwareDisplayPlaneManager {
                             const gfx::Rect& src_rect,
                             CrtcController* crtc) = 0;
 
-  virtual std::unique_ptr<HardwareDisplayPlane> CreatePlane(uint32_t plane_id);
+  virtual std::unique_ptr<HardwareDisplayPlane> CreatePlane(
+      uint32_t plane_id,
+      uint32_t possible_crtcs);
 
   // Finds the plane located at or after |*index| that is not in use and can
   // be used with |crtc_index|.
