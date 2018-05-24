29bbb5a89f85fb92b733fd04a38124b0b63fbb5e
Fady Samuel <fsamuel@chromium.org>
2018-05-24 20:42:36 +0000

    RELAND: Surface synchronization: Remove content_source_id in VisualProperties
    This CL removes content_source_id in VisualProperties which is no longer
    used. It also renames |local_surface_id_| to |local_surface_id_from_parent_|
    in RenderWidget to clarify where the viz::LocalSurfaceId is coming from.
    
    A previous version of this CL also
    
    1. Did not embed renderer that have not navigated yet.
    2. Always allocated a new LocalSurfaceId for navigation.
    
    1. Broke combobox popups because they're renderers that never navigate
    2. Caused new tab page flickering because the browser would submit a
       CompositorFrame that refers to no content until navigation which
       can cause SurfaceAggregator to flash the background color.
    
    The fix to 1. and 2. is to embed LocalSurfaceIds even if they did not navigate,
    and to not allocate a new LocalSurfaceId on the first navigation as before (so
    we block on new tab page to show the new content).
    
    This CL will likely cause an improvement to the timeToFirstPaint metric (and
    other navigation to paint metrics) because it will not block frame submission on
    allocating a new LocalSurfaceId anymore. However, the browser will still
    allocate a new LocalSurfaceId on DidNavigate. There is a race here which might
    cause Chrome to do a bit more work than it would otherwise (generate one more
    frame than it would otherwise) but that should be pretty small.
    
    Bug: 672962
    TBR: kenrb@chromium.org, piman@chromium.org
    Change-Id: I9754ff7b9330c99a0a1106b8d02aef98a26b1318
    Reviewed-on: https://chromium-review.googlesource.com/1070476
    Commit-Queue: Fady Samuel <fsamuel@chromium.org>
    Reviewed-by: Saman Sami <samans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561615}

--- content/browser/renderer_host/render_widget_host_view_child_frame.cc
+++ content/browser/renderer_host/render_widget_host_view_child_frame.cc
@@ -1182,6 +1182,7 @@ void RenderWidgetHostViewChildFrame::OnDidUpdateVisualPropertiesComplete(
 }
 
 void RenderWidgetHostViewChildFrame::DidNavigate() {
+  host()->ResetSentVisualProperties();
   host()->SynchronizeVisualProperties();
 }
 
