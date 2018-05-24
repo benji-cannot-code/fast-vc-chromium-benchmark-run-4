08b59b3bc4ebac182594971be5ad2e25d1813f27
Michael Lippautz <mlippautz@chromium.org>
2018-05-24 00:37:28 +0000

    [wrapper-tracing] Use name for missed write barriers
    This avoids writing a pointer for each traced wrapper on dispatch. It
    also brings the wrapper tracing descriptor closer to the regular trace
    descriptor.
    
    Bug: chromium:841830
    Change-Id: I6be7566a465184b12d9021926b3a61cd66934bc6
    Reviewed-on: https://chromium-review.googlesource.com/1067572
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561335}

--- third_party/blink/renderer/platform/heap/garbage_collected.h
+++ third_party/blink/renderer/platform/heap/garbage_collected.h
@@ -59,7 +59,6 @@ struct TraceWrapperDescriptor {
   STACK_ALLOCATED();
   void* base_object_payload;
   TraceWrappersCallback trace_wrappers_callback;
-  MissedWriteBarrierCallback missed_write_barrier_callback;
 };
 
 // The GarbageCollectedMixin interface and helper macro
@@ -115,7 +114,7 @@ class PLATFORM_EXPORT GarbageCollectedMixin {
     return {BlinkGC::kNotFullyConstructedObject, nullptr, false};
   }
   virtual TraceWrapperDescriptor GetTraceWrapperDescriptor() const {
-    return {BlinkGC::kNotFullyConstructedObject, nullptr, nullptr};
+    return {BlinkGC::kNotFullyConstructedObject, nullptr};
   }
 };
 
@@ -136,8 +135,7 @@ class PLATFORM_EXPORT GarbageCollectedMixin {
                                                                              \
   TraceWrapperDescriptor GetTraceWrapperDescriptor() const override {        \
     return {const_cast<TYPE*>(static_cast<const TYPE*>(this)),               \
-            TraceTrait<TYPE>::TraceWrappers,                                 \
-            ScriptWrappableVisitor::MissedWriteBarrier<TYPE>};               \
+            TraceTrait<TYPE>::TraceWrappers};                                \
   }                                                                          \
                                                                              \
  private:
