387a0ea5073afdb715d4b9b5639cf14dd4d02841
Yutaka Hirano <yhirano@chromium.org>
2018-05-24 09:22:12 +0000

    Remove status_code and status_text from WebSocketHandshakeResponseInfo
    They are contained in net::HttpResponseHeaders which is also contained
    in the struct.
    
    Bug: 721400
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I11b438790fc351ba9097bb44415c5c5c4ca923c3
    Reviewed-on: https://chromium-review.googlesource.com/1071327
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561436}

--- net/websockets/websocket_stream.cc
+++ net/websockets/websocket_stream.cc
@@ -468,9 +468,8 @@ void WebSocketDispatchOnFinishOpeningHandshake(
   DCHECK(connect_delegate);
   if (headers.get()) {
     connect_delegate->OnFinishOpeningHandshake(
-        std::make_unique<WebSocketHandshakeResponseInfo>(
-            url, headers->response_code(), headers->GetStatusText(), headers,
-            response_time));
+        std::make_unique<WebSocketHandshakeResponseInfo>(url, headers,
+                                                         response_time));
   }
 }
 
--- net/websockets/websocket_stream_test.cc
+++ net/websockets/websocket_stream_test.cc
@@ -610,8 +610,8 @@ TEST_P(WebSocketStreamCreateTest, HandshakeInfo) {
   // mainly only in this test case.
   EXPECT_EQ(GURL("ws://www.example.org/"), request_info_->url);
   EXPECT_EQ(GURL("ws://www.example.org/"), response_info_->url);
-  EXPECT_EQ(101, response_info_->status_code);
-  EXPECT_EQ("Switching Protocols", response_info_->status_text);
+  EXPECT_EQ(101, response_info_->headers->response_code());
+  EXPECT_EQ("Switching Protocols", response_info_->headers->GetStatusText());
   ASSERT_EQ(12u, request_headers.size());
   EXPECT_EQ(HeaderKeyValuePair("Host", "www.example.org"), request_headers[0]);
   EXPECT_EQ(HeaderKeyValuePair("Connection", "Upgrade"), request_headers[1]);
@@ -1460,7 +1460,7 @@ TEST_P(WebSocketStreamCreateBasicAuthTest, SuccessPasswordInUrl) {
   EXPECT_FALSE(has_failed());
   EXPECT_TRUE(stream_);
   ASSERT_TRUE(response_info_);
-  EXPECT_EQ(101, response_info_->status_code);
+  EXPECT_EQ(101, response_info_->headers->response_code());
 }
 
 TEST_P(WebSocketStreamCreateBasicAuthTest, FailureIncorrectPasswordInUrl) {
@@ -1487,7 +1487,7 @@ TEST_P(WebSocketStreamCreateDigestAuthTest, DigestPasswordInUrl) {
   EXPECT_FALSE(has_failed());
   EXPECT_TRUE(stream_);
   ASSERT_TRUE(response_info_);
-  EXPECT_EQ(101, response_info_->status_code);
+  EXPECT_EQ(101, response_info_->headers->response_code());
 }
 
 TEST_P(WebSocketMultiProtocolStreamCreateTest, Incomplete) {
