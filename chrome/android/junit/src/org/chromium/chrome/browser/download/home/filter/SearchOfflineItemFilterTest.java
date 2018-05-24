5ee342a55d3ba48f6256597a07ab333d956bc3ca
David Trainor <dtrainor@chromium.org>
2018-05-24 17:24:13 +0000

    Add an OfflineItemList to add date headers
    Add an OfflineItemList to wrap an OfflineItemFilterSource.  This list
    will (1) sort the items and (2) add date headers for each day items are
    seen.
    
    BUG=842345
    
    Change-Id: Ib1b0ee15b7431416934ee8e037f58ed055d090b5
    Reviewed-on: https://chromium-review.googlesource.com/1069677
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561538}

--- chrome/android/junit/src/org/chromium/chrome/browser/download/home/filter/SearchOfflineItemFilterTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/download/home/filter/SearchOfflineItemFilterTest.java
@@ -53,7 +53,7 @@ public class SearchOfflineItemFilterTest {
         // Test a query that doesn't match.
         filter.onQueryChanged("dogs");
         verify(mObserver, times(1)).onItemsRemoved(CollectionUtil.newHashSet(item1, item2, item3));
-        Assert.assertEquals(Collections.EMPTY_SET, filter.getItems());
+        Assert.assertEquals(Collections.emptySet(), filter.getItems());
 
         // Test undoing the query.
         filter.onQueryChanged("");
@@ -103,7 +103,7 @@ public class SearchOfflineItemFilterTest {
         // Test a query that doesn't match.
         filter.onQueryChanged("cows");
         verify(mObserver, times(1)).onItemsRemoved(CollectionUtil.newHashSet(item1, item2, item3));
-        Assert.assertEquals(Collections.EMPTY_SET, filter.getItems());
+        Assert.assertEquals(Collections.emptySet(), filter.getItems());
 
         // Test undoing the query.
         filter.onQueryChanged("");
