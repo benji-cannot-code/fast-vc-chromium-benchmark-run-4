e75b27e2d38aa7a25bde3ed2c1962acc30373372
John Z Wu <jzw@chromium.org>
2018-05-24 04:14:44 +0000

    Fix comment in CWVAutofillDataManager.
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ic5bb4506dccaa78871f025f235592a6401240141
    Reviewed-on: https://chromium-review.googlesource.com/1067689
    Reviewed-by: Hiroshi Ichikawa <ichikawa@chromium.org>
    Commit-Queue: Hiroshi Ichikawa <ichikawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561385}

--- ios/web_view/internal/autofill/cwv_autofill_data_manager.mm
+++ ios/web_view/internal/autofill/cwv_autofill_data_manager.mm
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 // Typedefs of |completionHandler| in |fetchProfilesWithCompletionHandler:|
-// and |fetchProfilesWithCompletionHandler:|.
+// and |fetchCreditCardsWithCompletionHandler:|.
 typedef void (^CWVFetchProfilesCompletionHandler)(
     NSArray<CWVAutofillProfile*>* profiles);
 typedef void (^CWVFetchCreditCardsCompletionHandler)(
