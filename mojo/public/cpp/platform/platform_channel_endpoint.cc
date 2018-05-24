3ce539b4f9262ac708af2f57f15b347070c3adb9
Ken Rockot <rockot@chromium.org>
2018-05-24 20:58:49 +0000

    Introduce public PlatformChannel and NamedPlatformChannel
    These are cleaned-up versions of the EDK's PlatformChannelPair and
    NamedPlatformChannelPair, which will be removed but remain untouched for
    the moment. Most of the logic is duplicated verbatim, modulo cleanup.
    
    For test coverage, since actual I/O operations are not being ported to
    the public library, the new types are used in place of EDK types within
    the invitation API tests.
    
    Bug: 844764
    Change-Id: Ieeea396370e37d4fa3a0e9a65b6f9b71855b85fc
    Reviewed-on: https://chromium-review.googlesource.com/1068652
    Commit-Queue: Ken Rockot <rockot@chromium.org>
    Reviewed-by: Jay Civelli <jcivelli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561624}

---
+++ mojo/public/cpp/platform/platform_channel_endpoint.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "mojo/public/cpp/platform/platform_channel_endpoint.h"
+
+namespace mojo {
+
+PlatformChannelEndpoint::PlatformChannelEndpoint() = default;
+
+PlatformChannelEndpoint::PlatformChannelEndpoint(
+    PlatformChannelEndpoint&& other) = default;
+
+PlatformChannelEndpoint::PlatformChannelEndpoint(PlatformHandle handle)
+    : handle_(std::move(handle)) {}
+
+PlatformChannelEndpoint::~PlatformChannelEndpoint() = default;
+
+PlatformChannelEndpoint& PlatformChannelEndpoint::operator=(
+    PlatformChannelEndpoint&& other) = default;
+
+void PlatformChannelEndpoint::reset() {
+  handle_.reset();
+}
+
+PlatformChannelEndpoint PlatformChannelEndpoint::Clone() const {
+  return PlatformChannelEndpoint(handle_.Clone());
+}
+
+}  // namespace mojo
