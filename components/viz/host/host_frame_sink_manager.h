6a507d386ed9e1f435f83ae38013a9c245959397
Saman Sami <samans@chromium.org>
2018-05-24 20:41:07 +0000

    Use ClientFrameSinkVideoCapturer in DevToolsEyeDropper
    ClientFrameSinkVideoCapturer will survive gpu crashes.
    
    Bug: 806377
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I53492851e1092729a3e331fddabf69602e1a0dc7
    Reviewed-on: https://chromium-review.googlesource.com/1071769
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Commit-Queue: Saman Sami <samans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561613}

--- components/viz/host/host_frame_sink_manager.h
+++ components/viz/host/host_frame_sink_manager.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list.h"
 #include "base/optional.h"
 #include "components/viz/common/surfaces/frame_sink_id.h"
+#include "components/viz/host/client_frame_sink_video_capturer.h"
 #include "components/viz/host/hit_test/hit_test_query.h"
 #include "components/viz/host/host_frame_sink_client.h"
 #include "components/viz/host/viz_host_export.h"
@@ -141,8 +142,14 @@ class VIZ_HOST_EXPORT HostFrameSinkManager
   // Asks viz to send updates regarding video activity to |observer|.
   void AddVideoDetectorObserver(mojom::VideoDetectorObserverPtr observer);
 
-  // Creates a FrameSinkVideoCapturer instance.
+  // Creates a FrameSinkVideoCapturer instance in viz.
   void CreateVideoCapturer(mojom::FrameSinkVideoCapturerRequest request);
+
+  // Creates a FrameSinkVideoCapturer instance in viz and returns a
+  // ClientFrameSinkVideoCapturer that's connected to it. Clients should prefer
+  // this version because ClientFrameSinkVideoCapturer is resilient to viz
+  // crashes.
+  std::unique_ptr<ClientFrameSinkVideoCapturer> CreateVideoCapturer();
 
   // Marks the given SurfaceIds for destruction.
   void EvictSurfaces(const std::vector<SurfaceId>& surface_ids);
