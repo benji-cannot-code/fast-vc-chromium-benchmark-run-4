1d4fccc1a5bbf98a538f271ed4c753b9aecb813c
Alexandr Ilin <alexilin@chromium.org>
2018-05-24 07:47:11 +0000

    Return a shmem region from media::AudioSyncReader by value
    AudioSyncReader::shared_memory_region() is never called more than once. If we
    return a region from this function by value, we don't have to duplicate the
    region.
    
    This is a follow-up to https://crrev.com/c/1068928
    
    Bug: 844508
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I22ac7f9bae926ed418c025ac0a8487b98659817a
    Reviewed-on: https://chromium-review.googlesource.com/1069352
    Reviewed-by: Max Morin <maxmorin@chromium.org>
    Commit-Queue: Alexandr Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561411}

--- media/audio/audio_output_device_unittest.cc
+++ media/audio/audio_output_device_unittest.cc
@@ -414,7 +414,7 @@ TEST_F(AudioOutputDeviceTest, VerifyDataFlow) {
   audio_device->OnDeviceAuthorized(OUTPUT_DEVICE_STATUS_OK, params,
                                    kDefaultDeviceId);
   audio_device->OnStreamCreated(
-      ToSharedMemoryHandle(env.reader->shared_memory_region()->Duplicate()),
+      ToSharedMemoryHandle(env.reader->TakeSharedMemoryRegion()),
       env.renderer_socket.Release(), /*playing_automatically*/ false);
 
   task_env_.RunUntilIdle();
@@ -475,7 +475,7 @@ TEST_F(AudioOutputDeviceTest, CreateNondefaultDevice) {
   audio_device->OnDeviceAuthorized(OUTPUT_DEVICE_STATUS_OK, params,
                                    kNonDefaultDeviceId);
   audio_device->OnStreamCreated(
-      ToSharedMemoryHandle(env.reader->shared_memory_region()->Duplicate()),
+      ToSharedMemoryHandle(env.reader->TakeSharedMemoryRegion()),
       env.renderer_socket.Release(), /*playing_automatically*/ false);
 
   audio_device->Stop();
@@ -511,7 +511,7 @@ TEST_F(AudioOutputDeviceTest, CreateBitStreamStream) {
   audio_device->OnDeviceAuthorized(OUTPUT_DEVICE_STATUS_OK, params,
                                    kNonDefaultDeviceId);
   audio_device->OnStreamCreated(
-      ToSharedMemoryHandle(env.reader->shared_memory_region()->Duplicate()),
+      ToSharedMemoryHandle(env.reader->TakeSharedMemoryRegion()),
       env.renderer_socket.Release(), /*playing_automatically*/ false);
 
   task_env_.RunUntilIdle();
--- media/mojo/services/mojo_audio_output_stream_unittest.cc
+++ media/mojo/services/mojo_audio_output_stream_unittest.cc
@@ -207,7 +207,7 @@ TEST_F(MojoAudioOutputStreamTest, Play_Plays) {
   EXPECT_CALL(client_, GotNotification());
   EXPECT_CALL(*delegate_, OnPlayStream());
 
-  delegate_event_handler_->OnStreamCreated(kStreamId, &mem_,
+  delegate_event_handler_->OnStreamCreated(kStreamId, std::move(mem_),
                                            std::move(foreign_socket_));
   audio_output_ptr->Play();
   base::RunLoop().RunUntilIdle();
@@ -219,7 +219,7 @@ TEST_F(MojoAudioOutputStreamTest, Pause_Pauses) {
   EXPECT_CALL(client_, GotNotification());
   EXPECT_CALL(*delegate_, OnPauseStream());
 
-  delegate_event_handler_->OnStreamCreated(kStreamId, &mem_,
+  delegate_event_handler_->OnStreamCreated(kStreamId, std::move(mem_),
                                            std::move(foreign_socket_));
   audio_output_ptr->Pause();
   base::RunLoop().RunUntilIdle();
@@ -231,7 +231,7 @@ TEST_F(MojoAudioOutputStreamTest, SetVolume_SetsVolume) {
   EXPECT_CALL(client_, GotNotification());
   EXPECT_CALL(*delegate_, OnSetVolume(kNewVolume));
 
-  delegate_event_handler_->OnStreamCreated(kStreamId, &mem_,
+  delegate_event_handler_->OnStreamCreated(kStreamId, std::move(mem_),
                                            std::move(foreign_socket_));
   audio_output_ptr->SetVolume(kNewVolume);
   base::RunLoop().RunUntilIdle();
@@ -244,7 +244,7 @@ TEST_F(MojoAudioOutputStreamTest, DestructWithCallPending_Safe) {
 
   ASSERT_NE(nullptr, delegate_event_handler_);
   foreign_socket_->ExpectOwnershipTransfer();
-  delegate_event_handler_->OnStreamCreated(kStreamId, &mem_,
+  delegate_event_handler_->OnStreamCreated(kStreamId, std::move(mem_),
                                            std::move(foreign_socket_));
   audio_output_ptr->Play();
   impl_.reset();
@@ -259,7 +259,7 @@ TEST_F(MojoAudioOutputStreamTest, Created_NotifiesClient) {
 
   ASSERT_NE(nullptr, delegate_event_handler_);
   foreign_socket_->ExpectOwnershipTransfer();
-  delegate_event_handler_->OnStreamCreated(kStreamId, &mem_,
+  delegate_event_handler_->OnStreamCreated(kStreamId, std::move(mem_),
                                            std::move(foreign_socket_));
 
   base::RunLoop().RunUntilIdle();
@@ -270,7 +270,7 @@ TEST_F(MojoAudioOutputStreamTest, SetVolumeTooLarge_Error) {
   EXPECT_CALL(deleter_, Finished(true));
   EXPECT_CALL(client_, GotNotification());
 
-  delegate_event_handler_->OnStreamCreated(kStreamId, &mem_,
+  delegate_event_handler_->OnStreamCreated(kStreamId, std::move(mem_),
                                            std::move(foreign_socket_));
   audio_output_ptr->SetVolume(15);
   base::RunLoop().RunUntilIdle();
@@ -282,7 +282,7 @@ TEST_F(MojoAudioOutputStreamTest, SetVolumeNegative_Error) {
   EXPECT_CALL(deleter_, Finished(true));
   EXPECT_CALL(client_, GotNotification());
 
-  delegate_event_handler_->OnStreamCreated(kStreamId, &mem_,
+  delegate_event_handler_->OnStreamCreated(kStreamId, std::move(mem_),
                                            std::move(foreign_socket_));
   audio_output_ptr->SetVolume(-0.5);
   base::RunLoop().RunUntilIdle();
@@ -308,7 +308,7 @@ TEST_F(MojoAudioOutputStreamTest, DelegateErrorAfterCreated_PropagatesError) {
 
   ASSERT_NE(nullptr, delegate_event_handler_);
   foreign_socket_->ExpectOwnershipTransfer();
-  delegate_event_handler_->OnStreamCreated(kStreamId, &mem_,
+  delegate_event_handler_->OnStreamCreated(kStreamId, std::move(mem_),
                                            std::move(foreign_socket_));
   delegate_event_handler_->OnStreamError(kStreamId);
 
@@ -322,7 +322,7 @@ TEST_F(MojoAudioOutputStreamTest, RemoteEndGone_CallsDeleter) {
   EXPECT_CALL(client_, GotNotification());
   EXPECT_CALL(deleter_, Finished(false));
 
-  delegate_event_handler_->OnStreamCreated(kStreamId, &mem_,
+  delegate_event_handler_->OnStreamCreated(kStreamId, std::move(mem_),
                                            std::move(foreign_socket_));
   audio_output_ptr.reset();
   base::RunLoop().RunUntilIdle();
