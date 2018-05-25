e2ebb692732ca1d5a3293dbbb07f98a724e4dddb
Kunihiko Sakamoto <ksakamoto@chromium.org>
2018-05-25 00:42:46 +0000

    Add OCSP check for Signed Exchange
    After this patch, SignedExchangeHandler starts accepting
    "application/signed-exchange;v=b1" content-type in addition to v=b0.
    (But Accept-Header still advertises only v=b0.)
    
    For b1 signed exchanges, OCSP response from cert chain is passed to
    CertVerifier::Verify(), and signed exchange without valid OCSP response
    is rejected.
    
    For now, b1 has minimal test coverage, but when we drop b0 support after
    M68 branch cut, we'll be able to convert existing tests to b1.
    
    This also makes IgnoreErrorsCertVerifier set OCSP results if request has
    a non-empty ocsp response. This allows LayoutTests to work.
    
    Bug: 815024
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I611d68c1d4f26b1f97ea81f8f9f9b89ba3ad0d84
    Reviewed-on: https://chromium-review.googlesource.com/1060933
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561709}

--- content/browser/web_package/signed_exchange_handler.h
+++ content/browser/web_package/signed_exchange_handler.h
@@ -29,6 +29,7 @@ class CertVerifyResult;
 class DrainableIOBuffer;
 class SourceStream;
 class URLRequestContextGetter;
+struct OCSPVerifyResult;
 }  // namespace net
 
 namespace network {
@@ -96,6 +97,7 @@ class CONTENT_EXPORT SignedExchangeHandler {
   void OnCertReceived(
       std::unique_ptr<SignedExchangeCertificateChain> cert_chain);
   void OnCertVerifyComplete(int result);
+  bool CheckOCSPStatus(const net::OCSPVerifyResult& ocsp_result);
 
   ExchangeHeadersCallback headers_callback_;
   base::Optional<SignedExchangeVersion> version_;
