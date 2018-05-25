0591ef8fc1ab8a2c06d3723bc7fe63e4de2625e1
Kyle Horimoto <khorimoto@google.com>
2018-05-25 19:33:21 +0000

    [CrOS MultiDevice] Fix broken PendingBle*ConnectionRequest tests.
    The tests were broken on debug builds because they assumed that the
    address of a ClientConnectionParameters object would remain unchanged
    throughout the life of the test, but this was not true since the object
    was std::move()'ed. In release builds, this did not trigger a failure.
    
    Bug: 824568, 752273
    Change-Id: Ic03cbde9d5c74030bdccfc87adf1041a038fed0c
    Reviewed-on: https://chromium-review.googlesource.com/1073653
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561969}

--- chromeos/services/secure_channel/pending_ble_initiator_connection_request_unittest.cc
+++ chromeos/services/secure_channel/pending_ble_initiator_connection_request_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/run_loop.h"
 #include "base/test/scoped_task_environment.h"
+#include "base/unguessable_token.h"
 #include "chromeos/services/secure_channel/client_connection_parameters.h"
 #include "chromeos/services/secure_channel/fake_pending_connection_request_delegate.h"
 #include "chromeos/services/secure_channel/test_client_connection_parameters_factory.h"
@@ -34,7 +35,7 @@ class SecureChannelPendingBleInitiatorConnectionRequestTest
         std::make_unique<FakePendingConnectionRequestDelegate>();
     auto client_connection_parameters =
         TestClientConnectionParametersFactory::Get()->Create(kTestFeature);
-    client_connection_parameters_ = &client_connection_parameters;
+    client_connection_parameters_id_ = client_connection_parameters.id();
 
     pending_ble_initiator_request_ =
         PendingBleInitiatorConnectionRequest::Factory::Get()->BuildInstance(
@@ -71,7 +72,7 @@ class SecureChannelPendingBleInitiatorConnectionRequestTest
 
   FakeConnectionDelegate* fake_connection_delegate() {
     return TestClientConnectionParametersFactory::Get()
-        ->GetDelegateForParameters(*client_connection_parameters_);
+        ->GetDelegateForParameters(client_connection_parameters_id_);
   }
 
  private:
@@ -79,7 +80,7 @@ class SecureChannelPendingBleInitiatorConnectionRequestTest
 
   std::unique_ptr<FakePendingConnectionRequestDelegate>
       fake_pending_connection_request_delegate_;
-  ClientConnectionParameters* client_connection_parameters_;
+  base::UnguessableToken client_connection_parameters_id_;
 
   std::unique_ptr<PendingConnectionRequest<BleInitiatorFailureType>>
       pending_ble_initiator_request_;
