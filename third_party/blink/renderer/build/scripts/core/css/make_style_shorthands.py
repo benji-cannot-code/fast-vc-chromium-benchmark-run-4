32f9d8a3bc6c8d1b3ccf11efcac933b294426a01
Kent Tamura <tkent@chromium.org>
2018-05-25 05:46:49 +0000

    json5_generator: Make 'name' field a NameStyleConverter object
    The 'name' field of entries produced by Json5File.load_from_files() was
    a str, and this CL makes it a NameStyleConverter object.  The original
    string is accessible via |entry['name'].original|, and
    |str(entry['name'])|.  This will make filter setup simpler because we
    don't need to provide filters for naming style conversion.
    
    * name_style_converter.py:
     - Provide |original| getter, and __str__().
     - Provide __lt__() for sort().
     - Provide __eq__() for groupby().
      Some Jinja2 templates use built-in sort and groupby filters, and it's
      hard to specify key object in templates.
    
    * json5_generator.py
      Sets entry['name'] to a NameStyleConverter object, and remove
      entry['tokenized_name'].
    
    * DEPS
      Clean stale *.pyc in blink/renderer/build/scripts too. Some buildbots
      has media_feature_symbol.pyc in build/scripts/core/css/.
    
    * Other files
     - If code requires a string name, access entry['name'].original.
     - Use entry.name instead of entry.tokenized_name
    
    
    Bug: 843927
    Change-Id: Ic47c90c51c8e88f08307a41b052ecfce971a4545
    Reviewed-on: https://chromium-review.googlesource.com/1068575
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561780}

--- third_party/blink/renderer/build/scripts/core/css/make_style_shorthands.py
+++ third_party/blink/renderer/build/scripts/core/css/make_style_shorthands.py
@@ -66,7 +66,7 @@ class StylePropertyShorthandWriter(json5_generator.Writer):
             # alphabetically
             longhands.sort(
                 key=lambda property_: (
-                    -len(property_['longhand_property_ids']), property_['name'])
+                    -len(property_['longhand_property_ids']), property_['name'].original)
             )
 
     @template_expander.use_jinja(
--- third_party/blink/renderer/build/scripts/media_feature_symbol.py
+++ third_party/blink/renderer/build/scripts/media_feature_symbol.py
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 
 def mediaFeatureSymbol(entry, suffix):
-    name = entry['name']
+    name = entry['name'].original
     if name.startswith('-webkit-'):
         name = name[8:]
 
