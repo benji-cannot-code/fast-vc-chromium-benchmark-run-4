50cb247888309798322b6c5a24b625c471fd603a
Matt Falkenhagen <falken@chromium.org>
2018-05-25 13:12:48 +0000

    Reland "shared worker: Give the renderer a factory bundle when NetworkService is enabled."
    This relands r561423 and r561793.
    Original code review: https://chromium-review.googlesource.com/1069956
    Second code review: https://chromium-review.googlesource.com/1073037
    
    This collided with the landing of the S13nSW virtual test suite. I'm relanding
    with an expectation for the S13nSW failure. Probably we're using
    NetworkService when S13nSW is on and NetworkService is off.
    
    Bug: 839982, 846545, 846637
    Change-Id: Ib1c3122ee532cce56a040356433002a24edc5553
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    TBR: nhiroki, kinuko, dcheng
    Reviewed-on: https://chromium-review.googlesource.com/1072991
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561844}

--- third_party/blink/renderer/core/exported/worker_shadow_page.cc
+++ third_party/blink/renderer/core/exported/worker_shadow_page.cc
@@ -16,17 +16,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-WorkerShadowPage::WorkerShadowPage(Client* client)
+WorkerShadowPage::WorkerShadowPage(
+    Client* client,
+    scoped_refptr<network::SharedURLLoaderFactory> loader_factory)
     : client_(client),
       web_view_(WebViewImpl::Create(nullptr,
                                     mojom::PageVisibilityState::kVisible,
                                     nullptr)),
-      main_frame_(WebLocalFrameImpl::CreateMainFrame(web_view_,
-                                                     this,
-                                                     nullptr,
-                                                     nullptr,
-                                                     g_empty_atom,
-                                                     WebSandboxFlags::kNone)) {
+      main_frame_(
+          WebLocalFrameImpl::CreateMainFrame(web_view_,
+                                             this,
+                                             nullptr /* interface_registry */,
+                                             nullptr /* opener */,
+                                             g_empty_atom,
+                                             WebSandboxFlags::kNone)),
+      loader_factory_(std::move(loader_factory)) {
   DCHECK(IsMainThread());
 
   // TODO(http://crbug.com/363843): This needs to find a better way to
@@ -88,6 +92,8 @@ WorkerShadowPage::CreateApplicationCacheHost(
 std::unique_ptr<blink::WebURLLoaderFactory>
 WorkerShadowPage::CreateURLLoaderFactory() {
   DCHECK(IsMainThread());
+  if (loader_factory_)
+    return Platform::Current()->WrapSharedURLLoaderFactory(loader_factory_);
   return Platform::Current()->CreateDefaultURLLoaderFactory();
 }
 
