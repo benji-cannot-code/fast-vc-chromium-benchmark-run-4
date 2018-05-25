e4d65dd186c6940eabc47a4c1db21afb4bda6ef3
Tsuyoshi Horo <horo@chromium.org>
2018-05-25 03:59:58 +0000

    Shows the signature and CertSHA256 of signed exchange in DevTools
    The signature header was removed from the response headers in the spec.
    https://github.com/WICG/webpackage/commit/f4ca17d1377bed966fdba85c03adbd798cfb5a96
    (Chromium implementation is not updated yet.)
    So we should show the all "signature" information.
    
    Bug: 830505
    Change-Id: I1e922806e2febedb66c3f4e1614d5277a6012e3d
    Reviewed-on: https://chromium-review.googlesource.com/1069948
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561761}

--- third_party/blink/renderer/devtools/front_end/network/SignedExchangeInfoView.js
+++ third_party/blink/renderer/devtools/front_end/network/SignedExchangeInfoView.js
@@ -65,6 +65,7 @@ Network.SignedExchangeInfoView = class extends UI.VBox {
       for (const signature of header.signatures) {
         const signatureCategory = new Network.SignedExchangeInfoView.Category(root, Common.UIString('Signature'));
         signatureCategory.createLeaf(this._formatHeader(Common.UIString('Label'), signature.label));
+        signatureCategory.createLeaf(this._formatHeaderForHexData(Common.UIString('Signature'), signature.signature));
         signatureCategory.createLeaf(this._formatHeader(Common.UIString('Integrity'), signature.integrity));
 
         if (signature.certUrl) {
@@ -76,6 +77,10 @@ Network.SignedExchangeInfoView = class extends UI.VBox {
                 'click', InspectorFrontendHost.showCertificateViewer.bind(null, signature.certificates), false);
           }
           signatureCategory.createLeaf(certURLElement);
+        }
+        if (signature.certSha256) {
+          signatureCategory.createLeaf(
+              this._formatHeaderForHexData(Common.UIString('Certificate SHA256'), signature.certSha256));
         }
         signatureCategory.createLeaf(this._formatHeader(Common.UIString('Validity URL'), signature.validityUrl));
         signatureCategory.createLeaf().title =
@@ -106,6 +111,19 @@ Network.SignedExchangeInfoView = class extends UI.VBox {
     fragment.createChild('div', 'header-name').textContent = name + ': ';
     fragment.createChild('span', 'header-separator');
     fragment.createChild('div', 'header-value source-code').textContent = value;
+    return fragment;
+  }
+
+  /**
+   * @param {string} name
+   * @param {string} value
+   * @return {!DocumentFragment}
+   */
+  _formatHeaderForHexData(name, value) {
+    const fragment = createDocumentFragment();
+    fragment.createChild('div', 'header-name').textContent = name + ': ';
+    fragment.createChild('span', 'header-separator');
+    fragment.createChild('div', 'header-value source-code hex-data').textContent = value.replace(/(.{2})/g, '$1 ');
     return fragment;
   }
 };
