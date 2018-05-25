a2ac7f421b71f52e003087b5a708d0a9c627c0ba
Devlin Cronin <rdevlin.cronin@chromium.org>
2018-05-25 19:41:42 +0000

    [Extensions + Metrics] Make it harder to mess up histogram enum values
    Take steps to make it harder to mess up
    extension_function_histogram_value (which can happen if the enum is ever
    reordered). Do this in two ways:
    
    * Add explicit values to the enum values. This makes it much more
      difficult to accidentally insert an enum in the middle, because it is
      clear that they are all increasing. What's more, adding one with a
      duplicate value will cause
      ExtensionFunctionRegistrationTest.CheckForDuplicateEntries to fail.
    * Add a unittest that compares the values in the C++ enum to the values
      specified in the enums.xml file. To do this, hijack the code to read
      and parse the enums.xml file from about_flags_unittest.cc and put it
      in a common place in base/test/enum_test_util.
    
    Between the two of these, it should hopefully be very difficult to
    accidentally reorder the histogram.
    
    Bug: None
    
    Change-Id: I8932c7da32af8121ec94ef89bf591365e93ce575
    Reviewed-on: https://chromium-review.googlesource.com/1062201
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561975}

---
+++ base/test/metrics/histogram_enum_reader_unittest.cc
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/test/metrics/histogram_enum_reader.h"
+
+#include "base/optional.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace base {
+
+TEST(HistogramEnumReaderTest, SanityChecks) {
+  {
+    // NOTE: This results in a dependency on the enums.xml file, but to
+    // otherwise inject content would circumvent a lot of the logic of the
+    // method and add additional complexity. "Boolean" is hopefully a pretty
+    // stable enum.
+    Optional<HistogramEnumEntryMap> results = ReadEnumFromEnumsXml("Boolean");
+    ASSERT_TRUE(results);
+    EXPECT_EQ("False", results->at(0));
+    EXPECT_EQ("True", results->at(1));
+  }
+
+  {
+    Optional<HistogramEnumEntryMap> results =
+        ReadEnumFromEnumsXml("TheWorstNameForAnEnum");
+    ASSERT_FALSE(results);
+  }
+}
+
+}  // namespace base
