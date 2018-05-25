7654f3f4eadaae39befeb7a0f30cdde0c25a6dda
David Jacobo <djacobo@chromium.org>
2018-05-25 00:37:41 +0000

    s/tab/web_contents in ArcExternalProtocolDialog
    Making var names consistent accross ArcExternalProtocolDialog. Also a
    couple of style fixes for params comments.
    
    Bug: None
    Change-Id: Iae821c8d3a30e9e6886cda9f380dc050ceba9ef5
    Tets: Build
    Reviewed-on: https://chromium-review.googlesource.com/1069715
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Commit-Queue: David Jacobo <djacobo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561707}

--- chrome/browser/chromeos/arc/intent_helper/arc_external_protocol_dialog.h
+++ chrome/browser/chromeos/arc/intent_helper/arc_external_protocol_dialog.h
@@ -63,7 +63,7 @@ GURL GetUrlToNavigateOnDeactivateForTesting(
     const std::vector<mojom::IntentHandlerInfoPtr>& handlers);
 
 bool GetAndResetSafeToRedirectToArcWithoutUserConfirmationFlagForTesting(
-    content::WebContents* tab);
+    content::WebContents* web_contents);
 
 bool IsChromeAnAppCandidateForTesting(
     const std::vector<mojom::IntentHandlerInfoPtr>& handlers);
