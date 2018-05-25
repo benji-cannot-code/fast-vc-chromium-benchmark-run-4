dddd71447d19e658afc38714d1bbd1d1fa942ac4
Weidong Guo <weidongg@chromium.org>
2018-05-25 21:21:03 +0000

    Implement Apps Grid Gap
    Background:
    New specs require that an item can be dragged to next page without
    having to fill up the current page. However, all items in launcher
    currently are laid out linearly and no gap is allowed between items. And
    the item's visual index (e.g. (i, j) means ith page and jth slot.) can
    be calculated by it’s model index (index of the item view in
    |view_model_|) via formula:
    
    visual_index.page = model_index / max_items_per_page
    visual_index.slot = model_index % max_items_per_page
    
    However, this formula does not work for new specs, so we need a new view
    structure to map from model index to visual index.
    
    Changes:
    1. Add a view structure to represent the item view's structure we show
       to users.
    2. Add functions to operate on the view structure: load, save, sanitize,
       move, remove, add, etc.
    3. Create a new page if the user drag an item to the end of last page.
    4. Put everything behind a home launcher flag because it is part of home
       launcher feature.
    
    Design doc: go/applist-gap
    
    BUG=837871
    
    Change-Id: I9718bfca0ebf31da5a5d63aa73c4a6f1fb20459a
    Reviewed-on: https://chromium-review.googlesource.com/1058715
    Commit-Queue: Weidong Guo <weidongg@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562012}

--- ui/app_list/views/app_list_view_unittest.cc
+++ ui/app_list/views/app_list_view_unittest.cc
@@ -457,7 +457,7 @@ class AppListViewFocusTest : public views::ViewsTestBase,
   SearchBoxView* search_box_view() { return main_view()->search_box_view(); }
 
   AppListItemView* folder_item_view() {
-    return apps_grid_view()->view_model_for_test()->view_at(0);
+    return apps_grid_view()->view_model()->view_at(0);
   }
 
   views::View* focused_view() {
@@ -533,7 +533,7 @@ TEST_P(AppListViewFocusTest, LinearFocusTraversalInFullscreenAllAppsState) {
   for (int i = 0; i < suggestions_container_view()->num_results(); ++i)
     forward_view_list.push_back(suggestions_container_view()->tile_views()[i]);
   const views::ViewModelT<AppListItemView>* view_model =
-      apps_grid_view()->view_model_for_test();
+      apps_grid_view()->view_model();
   for (int i = 0; i < view_model->view_size(); ++i)
     forward_view_list.push_back(view_model->view_at(i));
   forward_view_list.push_back(search_box_view()->search_box());
@@ -622,7 +622,7 @@ TEST_P(AppListViewFocusTest, LinearFocusTraversalInFolder) {
   std::vector<views::View*> forward_view_list;
   forward_view_list.push_back(search_box_view()->search_box());
   const views::ViewModelT<AppListItemView>* view_model =
-      app_list_folder_view()->items_grid_view()->view_model_for_test();
+      app_list_folder_view()->items_grid_view()->view_model();
   for (int i = 0; i < view_model->view_size(); ++i)
     forward_view_list.push_back(view_model->view_at(i));
   forward_view_list.push_back(
@@ -687,7 +687,7 @@ TEST_F(AppListViewFocusTest, VerticalFocusTraversalInFullscreenAllAppsState) {
       suggestions_container_view()->num_results();
   forward_view_list.push_back(tile_views.front());
   const views::ViewModelT<AppListItemView>* view_model =
-      apps_grid_view()->view_model_for_test();
+      apps_grid_view()->view_model();
   for (int i = 0; i < view_model->view_size(); i += apps_grid_view()->cols())
     forward_view_list.push_back(view_model->view_at(i));
   forward_view_list.push_back(search_box_view()->search_box());
@@ -769,7 +769,7 @@ TEST_F(AppListViewFocusTest, VerticalFocusTraversalInFirstPageOfFolder) {
   std::vector<views::View*> forward_view_list;
   forward_view_list.push_back(search_box_view()->search_box());
   const views::ViewModelT<AppListItemView>* view_model =
-      app_list_folder_view()->items_grid_view()->view_model_for_test();
+      app_list_folder_view()->items_grid_view()->view_model();
   for (size_t i = 0; i < kMaxFolderItemsPerPage;
        i += app_list_folder_view()->items_grid_view()->cols())
     forward_view_list.push_back(view_model->view_at(i));
@@ -811,7 +811,7 @@ TEST_F(AppListViewFocusTest, VerticalFocusTraversalInSecondPageOfFolder) {
   std::vector<views::View*> forward_view_list;
   forward_view_list.push_back(search_box_view()->search_box());
   const views::ViewModelT<AppListItemView>* view_model =
-      app_list_folder_view()->items_grid_view()->view_model_for_test();
+      app_list_folder_view()->items_grid_view()->view_model();
   for (int i = kMaxFolderItemsPerPage; i < view_model->view_size();
        i += app_list_folder_view()->items_grid_view()->cols()) {
     forward_view_list.push_back(view_model->view_at(i));
