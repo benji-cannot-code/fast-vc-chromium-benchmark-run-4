76a415b6a40b771091ba1a838e9ba4575f6d8372
Morten Stenshorne <mstensho@chromium.org>
2018-05-25 14:27:34 +0000

    [LayoutNG] Move ng_exclusion.* into exclusions/
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: I579692440fb153d7886e37482410e1c0ecde9e94
    Reviewed-on: https://chromium-review.googlesource.com/1073285
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561864}

--- third_party/blink/renderer/core/layout/ng/exclusions/ng_exclusion.h
+++ third_party/blink/renderer/core/layout/ng/exclusions/ng_exclusion.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef NGExclusion_h
-#define NGExclusion_h
+#ifndef THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_NG_EXCLUSIONS_NG_EXCLUSION_H_
+#define THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_NG_EXCLUSIONS_NG_EXCLUSION_H_
 
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/layout/ng/geometry/ng_bfc_rect.h"
@@ -52,4 +52,4 @@ struct CORE_EXPORT NGExclusion : public RefCounted<NGExclusion> {
 
 }  // namespace blink
 
-#endif  // NGExclusion_h
+#endif  // THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_NG_EXCLUSIONS_NG_EXCLUSION_H_
