f3803e46d1f97a9af9b8c78d4444c0ae41d9e95a
Hajime Hoshi <hajimehoshi@chromium.org>
2018-05-25 12:02:18 +0000

    De-couple MediaElementEventQueue and EventQueue
    This CL also removes MediaElementEventQueue::CancelEvent, which is not
    called anywhere.
    
    This is one of the steps to eliminate EventQueue.
    
    Bug: 846618
    Change-Id: I6e67a84d7bbd5344a5758e58d1db7be0192ac53f
    Reviewed-on: https://chromium-review.googlesource.com/1072992
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561835}

--- third_party/blink/renderer/core/dom/events/media_element_event_queue.h
+++ third_party/blink/renderer/core/dom/events/media_element_event_queue.h
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/memory/scoped_refptr.h"
 #include "third_party/blink/renderer/core/core_export.h"
-#include "third_party/blink/renderer/core/dom/events/event_queue.h"
 #include "third_party/blink/renderer/core/dom/events/event_target.h"
 #include "third_party/blink/renderer/platform/timer.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
@@ -39,16 +38,15 @@ namespace blink {
 
 // Queue for events originating in MediaElement and having
 // "media element event" task type according to the spec.
-class CORE_EXPORT MediaElementEventQueue final : public EventQueue {
+class CORE_EXPORT MediaElementEventQueue final
+    : public GarbageCollectedFinalized<MediaElementEventQueue> {
  public:
   static MediaElementEventQueue* Create(EventTarget*, ExecutionContext*);
-  ~MediaElementEventQueue() override;
+  ~MediaElementEventQueue();
 
-  // EventQueue
-  void Trace(blink::Visitor*) override;
-  bool EnqueueEvent(const base::Location&, Event*) override;
-  bool CancelEvent(Event*) override;
-  void Close() override;
+  void Trace(blink::Visitor*);
+  bool EnqueueEvent(const base::Location&, Event*);
+  void Close();
 
   void CancelAllEvents();
   bool HasPendingEvents() const;
