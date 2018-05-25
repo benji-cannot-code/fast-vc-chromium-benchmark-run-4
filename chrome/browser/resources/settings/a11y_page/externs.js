3f276be1931db42bab62037eee60b0b5dd124dcf
Katie D <katie@chromium.org>
2018-05-25 00:27:59 +0000

    Set default Text-to-Speech voices on the TTS settings page.
    Finds the voice that best matches the app locale so that the default voice
    is as close as possible to the app locale in the user's preferred language.
    
    Bug: 823359
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I3374e2d38051cd7dbc089da2a9483887befc9fe4
    Reviewed-on: https://chromium-review.googlesource.com/1067540
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561705}

--- chrome/browser/resources/settings/a11y_page/externs.js
+++ chrome/browser/resources/settings/a11y_page/externs.js
@@ -16,8 +16,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * is generated in tts_subpage.js and not passed from tts_handler.cc).
  * |displayLanguage| is the user-facing display string, i.e. 'English'.
  * |fullLanguageCode| is the code with locale, i.e. 'en-us' or 'en-gb'.
+ * |languageScore| is a relative measure of how closely the voice's language
+ * matches the app language, and can be used to set a default voice.
  * @typedef {{languageCode: string, name: string, displayLanguage: string,
- *   extensionId: string, id: string, fullLanguageCode: string}}
+ *   extensionId: string, id: string, fullLanguageCode: string,
+ *   languageScore: number}}
  */
 let TtsHandlerVoice;
 
