978ad7f79a4676039d9902824345a651a141734b
Gauthier Ambard <gambard@chromium.org>
2018-05-25 13:49:55 +0000

    Remove Close All Incognito Tab from TabGrid popup
    This CL removes the Close All Incognito Tabs action from the TabGrid
    popup menu in incognito, making it the same as the non-incognito.
    
    Bug: 846614
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ib19301aa9582c7bb2eb94512c50082c40c6dc510
    Reviewed-on: https://chromium-review.googlesource.com/1073150
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561851}

--- ios/chrome/browser/ui/popup_menu/cells/popup_menu_item.h
+++ ios/chrome/browser/ui/popup_menu/cells/popup_menu_item.h
@@ -31,7 +31,6 @@ typedef NS_ENUM(NSInteger, PopupMenuAction) {
   PopupMenuActionHistory,
   PopupMenuActionSettings,
   PopupMenuActionCloseTab,
-  PopupMenuActionCloseAllIncognitoTabs,
   PopupMenuActionNavigate,
   PopupMenuActionPasteAndGo,
   PopupMenuActionVoiceSearch,
