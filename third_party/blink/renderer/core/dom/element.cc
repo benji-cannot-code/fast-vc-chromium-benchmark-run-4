aecad36925716c40acfb9acc05321137c30bf7f9
Michael Lippautz <mlippautz@chromium.org>
2018-05-25 11:20:43 +0000

    [oilpan] Convert manual wrapper tracing to Trace methods
    Wrapper tracing uses TraceWrappersWithManualWriteBarrier when dealing
    with raw pointers. These uses need to use TraceWithWrappers in an Oilpan
    world as they would not be properly forwarded otherwise.
    
    Bug: chromium:841830
    Change-Id: I725d2fbc8a8ac1f8387e4efeae5b74f02a294ed7
    Reviewed-on: https://chromium-review.googlesource.com/1073171
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561827}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -4894,7 +4894,7 @@ void Element::LogUpdateAttributeIfIsolatedWorldAndInDocument(
 
 void Element::Trace(blink::Visitor* visitor) {
   if (HasRareData())
-    visitor->Trace(GetElementRareData());
+    visitor->TraceWithWrappers(GetElementRareData());
   visitor->Trace(element_data_);
   ContainerNode::Trace(visitor);
 }
