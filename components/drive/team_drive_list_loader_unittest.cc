4b6841cd673249f79a35353c6c2fe9d1c896407d
Stuart Langley <slangley@chromium.org>
2018-05-25 02:34:19 +0000

    Add support to remove team drives from resource metadata.
    Upon reading the full list of team drives that a user has access to, if we
    discover any drives that are still in resource metadata but no longer in the
    list returned from the server we will recursively delete them.
    
    Bug: 715355
    Change-Id: I1e7d4c8f9de95413a2b62c5ce07fb7672342e41c
    Reviewed-on: https://chromium-review.googlesource.com/1071277
    Commit-Queue: Stuart Langley <slangley@chromium.org>
    Reviewed-by: Sasha Morrissey <sashab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561744}

--- components/drive/team_drive_list_loader_unittest.cc
+++ components/drive/team_drive_list_loader_unittest.cc
@@ -23,6 +23,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace drive {
 namespace internal {
 
+namespace {
+
+constexpr char kTestStartPageToken[] = "123456";
+
+}  // namespace
+
 class TeamDriveListLoaderTest : public testing::Test {
  protected:
   void SetUp() override {
@@ -62,6 +68,21 @@ class TeamDriveListLoaderTest : public testing::Test {
         metadata_.get(), scheduler_.get(), loader_controller_.get()));
   }
 
+  // Creates a ResourceEntry for a directory with explicitly set resource_id.
+  ResourceEntry CreateDirectoryEntryWithResourceId(
+      const std::string& title,
+      const std::string& resource_id,
+      const std::string& parent_local_id) {
+    ResourceEntry entry;
+    entry.set_title(title);
+    entry.set_resource_id(resource_id);
+    entry.set_parent_local_id(parent_local_id);
+    entry.mutable_file_info()->set_is_directory(true);
+    entry.mutable_directory_specific_info()->set_start_page_token(
+        kTestStartPageToken);
+    return entry;
+  }
+
   content::TestBrowserThreadBundle thread_bundle_;
   base::ScopedTempDir temp_dir_;
   std::unique_ptr<TestingPrefServiceSimple> pref_service_;
@@ -76,6 +97,8 @@ class TeamDriveListLoaderTest : public testing::Test {
   std::unique_ptr<TeamDriveListLoader> team_drive_list_loader_;
 };
 
+// Tests that if there are no team drives on the server, we will not add
+// and team drives to local metadata.
 TEST_F(TeamDriveListLoaderTest, NoTeamDrives) {
   // Tests the response if there are no team drives loaded.
   FileError error = FILE_ERROR_FAILED;
@@ -91,6 +114,8 @@ TEST_F(TeamDriveListLoaderTest, NoTeamDrives) {
   EXPECT_TRUE(entries.empty());
 }
 
+// Tests if there is one team drive on the server we will add it to the local
+// metadata.
 TEST_F(TeamDriveListLoaderTest, OneTeamDrive) {
   constexpr char kTeamDriveId1[] = "the1stTeamDriveId";
   constexpr char kTeamDriveName1[] = "The First Team Drive";
@@ -115,6 +140,8 @@ TEST_F(TeamDriveListLoaderTest, OneTeamDrive) {
                 &entry));
 }
 
+// Tests if there are multiple team drives on the server, we will add them all
+// to local metadata.
 TEST_F(TeamDriveListLoaderTest, MultipleTeamDrive) {
   const std::vector<std::pair<std::string, std::string>> team_drives = {
       {"the1stTeamDriveId", "The First Team Drive"},
@@ -150,6 +177,115 @@ TEST_F(TeamDriveListLoaderTest, MultipleTeamDrive) {
     EXPECT_EQ(drive.second, entry.base_name());
     EXPECT_TRUE(entry.file_info().is_directory());
     EXPECT_EQ("", entry.directory_specific_info().start_page_token());
+  }
+}
+
+// Tests that is we have existing team drives in local metadata, and we add
+// new team drives from the server, that we retain the pre-existing local
+// metadata.
+TEST_F(TeamDriveListLoaderTest, RetainExistingMetadata) {
+  const std::vector<std::pair<std::string, std::string>> new_team_drives = {
+      {"the3rdTeamDriveId", "The Third Team Drive"},
+      {"the4thTeamDriveId", "The Forth Team Drive"},
+  };
+
+  const std::vector<std::pair<std::string, std::string>> old_team_drives = {
+      {"the1stTeamDriveId", "The First Team Drive"},
+      {"the2ndTeamDriveId", "The Second Team Drive"},
+  };
+
+  for (const auto& drive : old_team_drives) {
+    drive_service_->AddTeamDrive(drive.first, drive.second);
+  }
+
+  for (const auto& drive : new_team_drives) {
+    drive_service_->AddTeamDrive(drive.first, drive.second);
+  }
+
+  std::string root_local_id;
+  ASSERT_EQ(FILE_ERROR_OK,
+            metadata_->GetIdByPath(util::GetDriveTeamDrivesRootPath(),
+                                   &root_local_id));
+
+  for (const auto& drive : old_team_drives) {
+    std::string local_id;
+    ASSERT_EQ(FILE_ERROR_OK,
+              metadata_->AddEntry(CreateDirectoryEntryWithResourceId(
+                                      drive.second, drive.first, root_local_id),
+                                  &local_id));
+  }
+
+  FileError error = FILE_ERROR_FAILED;
+  team_drive_list_loader_->CheckForUpdates(
+      google_apis::test_util::CreateCopyResultCallback(&error));
+  base::RunLoop().RunUntilIdle();
+  EXPECT_EQ(FILE_ERROR_OK, error);
+  EXPECT_EQ(1, drive_service_->team_drive_list_load_count());
+
+  ResourceEntryVector entries;
+  EXPECT_EQ(FILE_ERROR_OK, metadata_->ReadDirectoryByPath(
+                               util::GetDriveTeamDrivesRootPath(), &entries));
+  EXPECT_EQ(new_team_drives.size() + old_team_drives.size(), entries.size());
+
+  ResourceEntry entry;
+  for (const auto& drive : new_team_drives) {
+    EXPECT_EQ(FILE_ERROR_OK,
+              metadata_->GetResourceEntryByPath(
+                  util::GetDriveTeamDrivesRootPath().AppendASCII(drive.second),
+                  &entry));
+    EXPECT_EQ(drive.first, entry.resource_id());
+    EXPECT_EQ(drive.second, entry.base_name());
+    EXPECT_TRUE(entry.file_info().is_directory());
+    EXPECT_EQ("", entry.directory_specific_info().start_page_token());
+  }
+
+  for (const auto& drive : old_team_drives) {
+    EXPECT_EQ(FILE_ERROR_OK,
+              metadata_->GetResourceEntryByPath(
+                  util::GetDriveTeamDrivesRootPath().AppendASCII(drive.second),
+                  &entry));
+    EXPECT_EQ(drive.first, entry.resource_id());
+    EXPECT_EQ(drive.second, entry.base_name());
+    EXPECT_TRUE(entry.file_info().is_directory());
+    EXPECT_EQ(kTestStartPageToken,
+              entry.directory_specific_info().start_page_token());
+  }
+}
+
+// Tests that if we we had team drives stored locally that are no longer on the
+// server that we deleted them correctly.
+TEST_F(TeamDriveListLoaderTest, RemoveMissingTeamDriveFromMetadata) {
+  const std::vector<std::pair<std::string, std::string>> old_team_drives = {
+      {"the1stTeamDriveId", "The First Team Drive"},
+      {"the2ndTeamDriveId", "The Second Team Drive"},
+  };
+
+  std::string root_local_id;
+  ASSERT_EQ(FILE_ERROR_OK,
+            metadata_->GetIdByPath(util::GetDriveTeamDrivesRootPath(),
+                                   &root_local_id));
+
+  for (const auto& drive : old_team_drives) {
+    std::string local_id;
+    ASSERT_EQ(FILE_ERROR_OK,
+              metadata_->AddEntry(CreateDirectoryEntryWithResourceId(
+                                      drive.second, drive.first, root_local_id),
+                                  &local_id));
+  }
+
+  FileError error = FILE_ERROR_FAILED;
+  team_drive_list_loader_->CheckForUpdates(
+      google_apis::test_util::CreateCopyResultCallback(&error));
+  base::RunLoop().RunUntilIdle();
+  EXPECT_EQ(FILE_ERROR_OK, error);
+  EXPECT_EQ(1, drive_service_->team_drive_list_load_count());
+
+  ResourceEntry entry;
+  for (const auto& drive : old_team_drives) {
+    EXPECT_EQ(FILE_ERROR_NOT_FOUND,
+              metadata_->GetResourceEntryByPath(
+                  util::GetDriveTeamDrivesRootPath().AppendASCII(drive.second),
+                  &entry));
   }
 }
 
