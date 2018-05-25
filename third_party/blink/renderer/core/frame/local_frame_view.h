a2819b475c397cf17df2d661c45fa04c544910ee
Chris Harrelson <chrishtr@chromium.org>
2018-05-25 03:16:26 +0000

    [RLS] Remove RootLayerScrolling references in LocalFrameView.
    Bug: 823365
    Change-Id: If472892ad0baae4aa6352b8ce37eb61fb84e5fdc
    Reviewed-on: https://chromium-review.googlesource.com/1072412
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561753}

--- third_party/blink/renderer/core/frame/local_frame_view.h
+++ third_party/blink/renderer/core/frame/local_frame_view.h
@@ -489,8 +489,10 @@ class CORE_EXPORT LocalFrameView final
   int ScrollSize(ScrollbarOrientation) const override;
   bool IsScrollCornerVisible() const override;
   bool UpdateAfterCompositingChange() override;
-  bool UserInputScrollable(ScrollbarOrientation) const override;
-  bool ShouldPlaceVerticalScrollbarOnLeft() const override;
+  bool UserInputScrollable(ScrollbarOrientation) const override {
+    return false;
+  }
+  bool ShouldPlaceVerticalScrollbarOnLeft() const override { return false; }
   bool ScheduleAnimation() override;
   CompositorAnimationHost* GetCompositorAnimationHost() const override;
   CompositorAnimationTimeline* GetCompositorAnimationTimeline() const override;
@@ -582,10 +584,8 @@ class CORE_EXPORT LocalFrameView final
   // (e.g. DevTool's viewport override).
   void ClipPaintRect(FloatRect*) const;
 
-  // Functions for getting/setting the size of the document contained inside the
-  // LocalFrameView (as an IntSize or as individual width and height values).
-  // Always at least as big as the visibleWidth()/visibleHeight().
-  IntSize ContentsSize() const override;
+  // TODO(chrishtr) these methods are wrong, fix all callsites.
+  IntSize ContentsSize() const override { return Size(); }
   int ContentsWidth() const { return ContentsSize().Width(); }
   int ContentsHeight() const { return ContentsSize().Height(); }
 
@@ -816,7 +816,7 @@ class CORE_EXPORT LocalFrameView final
   bool RestoreScrollAnchor(const SerializedAnchor&) override;
   ScrollAnchor* GetScrollAnchor() override { return &scroll_anchor_; }
   void ClearScrollAnchor();
-  bool ShouldPerformScrollAnchoring() const override;
+  bool ShouldPerformScrollAnchoring() const override { return false; }
   void EnqueueScrollAnchoringAdjustment(ScrollableArea*);
   void DequeueScrollAnchoringAdjustment(ScrollableArea*);
   void PerformScrollAnchoringAdjustments();
@@ -939,11 +939,6 @@ class CORE_EXPORT LocalFrameView final
                                  ComputeScrollbarExistenceOption = kFirstPass);
   void UpdateScrollbarGeometry();
 
-  // Called to update the scrollbars to accurately reflect the state of the
-  // view.
-  void UpdateScrollbars();
-  void UpdateScrollbarsIfNeeded();
-
   class InUpdateScrollbarsScope {
     STACK_ALLOCATED();
 
@@ -997,7 +992,6 @@ class CORE_EXPORT LocalFrameView final
   bool UpdateLifecyclePhasesInternal(
       DocumentLifecycle::LifecycleState target_state);
 
-  void ScrollContentsIfNeededRecursive();
   void NotifyFrameRectsChangedIfNeededRecursive();
   void UpdateStyleAndLayoutIfNeededRecursive();
   void PrePaint();
@@ -1082,9 +1076,7 @@ class CORE_EXPORT LocalFrameView final
 
   void SetLayoutSizeInternal(const IntSize&);
 
-  bool AdjustScrollbarExistence(ComputeScrollbarExistenceOption = kFirstPass);
   void AdjustScrollbarOpacity();
-  void AdjustScrollOffsetFromUpdateScrollbars();
   bool VisualViewportSuppliesScrollbars();
 
   ScrollingCoordinator* GetScrollingCoordinator() const;
@@ -1251,7 +1243,6 @@ class CORE_EXPORT LocalFrameView final
   // ScrollbarManager holds the Scrollbar instances.
   ScrollbarManager scrollbar_manager_;
 
-  bool needs_scrollbars_update_;
   bool suppress_adjust_view_size_;
   bool allows_layout_invalidation_after_layout_clean_;
   bool needs_intersection_observation_;
