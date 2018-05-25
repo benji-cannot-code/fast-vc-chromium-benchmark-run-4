aa00c79bafd169b4d6e852ff1ea430e52b4a6750
Aldo Culquicondor <acondor@chromium.org>
2018-05-25 14:42:36 +0000

    VR: Renaming OnMove to OnHoverMove
    Bug: 846478
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_vr
    Change-Id: Id24c9d4e9123b888a2228cdf0043e51e2850e97b
    Reviewed-on: https://chromium-review.googlesource.com/1073189
    Reviewed-by: Biao She <bshe@chromium.org>
    Commit-Queue: Aldo Culquicondor <acondor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561874}

--- chrome/browser/vr/elements/ui_element_unittest.cc
+++ chrome/browser/vr/elements/ui_element_unittest.cc
@@ -500,7 +500,7 @@ TEST(UiElement, EventBubbling) {
 
   // Events on grand_child don't bubble up the parent chain.
   grand_child_ptr->OnHoverEnter(gfx::PointF());
-  grand_child_ptr->OnMove(gfx::PointF());
+  grand_child_ptr->OnHoverMove(gfx::PointF());
   grand_child_ptr->OnHoverLeave();
   grand_child_ptr->OnButtonDown(gfx::PointF());
   grand_child_ptr->OnButtonUp(gfx::PointF());
@@ -510,7 +510,7 @@ TEST(UiElement, EventBubbling) {
   // Events on grand_child bubble up the parent chain.
   grand_child_ptr->set_bubble_events(true);
   grand_child_ptr->OnHoverEnter(gfx::PointF());
-  grand_child_ptr->OnMove(gfx::PointF());
+  grand_child_ptr->OnHoverMove(gfx::PointF());
   grand_child_ptr->OnHoverLeave();
   grand_child_ptr->OnButtonDown(gfx::PointF());
   grand_child_ptr->OnButtonUp(gfx::PointF());
