cf87811aa09bc12a1702b5ccea2dc33b1266e0b9
Mike Wasserman <msw@chromium.org>
2018-05-25 17:17:12 +0000

    ws: Enable the Window Service clipboard backend for mojo apps
    Have ws2::WindowService own a ClipboardImpl, like ws::Service.
    Enables copy/paste within the ksv app, but not to/from ash/chrome.
    (run ksv app via --keyboard-shortcut-viewer-app, press Ctrl+Alt+/).
    
    TODO: Support copy-paste between mojo apps and Chrome/Ash.
    
    Bug: 839591
    Change-Id: I02b05cb9f830b0053dc0f1ad9e8e702505ad10a4
    Reviewed-on: https://chromium-review.googlesource.com/1073572
    Commit-Queue: Michael Wasserman <msw@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561923}

--- services/ui/ws2/window_service.h
+++ services/ui/ws2/window_service.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "services/service_manager/public/cpp/binder_registry.h"
 #include "services/service_manager/public/cpp/service.h"
+#include "services/ui/clipboard/clipboard_impl.h"
 #include "services/ui/ime/ime_driver_bridge.h"
 #include "services/ui/ime/ime_registrar_impl.h"
 #include "services/ui/input_devices/input_device_server.h"
@@ -27,6 +28,10 @@ class Window;
 namespace client {
 class FocusClient;
 }
+}
+
+namespace clipboard {
+class ClipboardImpl;
 }
 
 namespace gfx {
@@ -112,6 +117,8 @@ class COMPONENT_EXPORT(WINDOW_SERVICE) WindowService
 
   // Provides info to InputDeviceClient users, via InputDeviceManager.
   ui::InputDeviceServer input_device_server_;
+
+  std::unique_ptr<clipboard::ClipboardImpl> clipboard_;
 
   // Id for the next WindowServiceClient.
   ClientSpecificId next_client_id_ = kWindowServerClientId + 1;
