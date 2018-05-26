8d2c4a434f35a026f16e31935401f8e0239907de
Mounir Lamouri <mlamouri@chromium.org>
2018-05-26 18:49:57 +0000

    VideoSurfaceLayer: disable feature when Mash in enabled.
    Mash needs to implement some changes server side for this feature to
    work. Instead of crashing when a video is playing, this simply forces
    the feature off is Mash in on.
    
    Bug: 843760, 831357
    Change-Id: Ia0b95fecb50ba316f8037bf835aa5fab2e8f201f
    Reviewed-on: https://chromium-review.googlesource.com/1073671
    Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Fady Samuel <fsamuel@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562127}

--- content/renderer/media/media_factory.cc
+++ content/renderer/media/media_factory.cc
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/web_video_frame_submitter.h"
 #include "third_party/blink/public/web/blink.h"
 #include "third_party/blink/public/web/web_local_frame.h"
+#include "ui/base/ui_base_features.h"
 #include "url/origin.h"
 
 #if defined(OS_ANDROID)
@@ -132,6 +133,12 @@ void PostMediaContextProviderToCallback(
       }),
       base::BindOnce(&ObtainAndSetContextProvider,
                      std::move(set_context_provider_callback)));
+}
+
+// Whether videos should use SurfaceLayers.
+bool VideoSurfaceLayerEnabled() {
+  return base::FeatureList::IsEnabled(media::kUseSurfaceLayerForVideo) &&
+         !base::FeatureList::IsEnabled(features::kMash);
 }
 
 }  // namespace
@@ -277,8 +284,7 @@ blink::WebMediaPlayer* MediaFactory::CreateMediaPlayer(
   scoped_refptr<base::SingleThreadTaskRunner>
       video_frame_compositor_task_runner;
   std::unique_ptr<blink::WebVideoFrameSubmitter> submitter;
-  bool use_surface_layer_for_video =
-      base::FeatureList::IsEnabled(media::kUseSurfaceLayerForVideo);
+  bool use_surface_layer_for_video = VideoSurfaceLayerEnabled();
   if (use_surface_layer_for_video) {
     // TODO(lethalantidote): Use a separate task_runner. https://crbug/753605.
     video_frame_compositor_task_runner =
