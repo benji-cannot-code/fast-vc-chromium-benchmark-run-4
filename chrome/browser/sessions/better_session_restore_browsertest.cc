909e5d6913913425ace3927bec958f40b4a13904
John Abd-El-Malek <jam@chromium.org>
2018-05-26 00:14:16 +0000

    Add a helper method to URLLoaderInterceptor to write data from files on disk.
    Bug: 776589
    Change-Id: I3a445e80806d16904454b6f8eb7ed9d424c29e48
    Reviewed-on: https://chromium-review.googlesource.com/1073758
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Jay Civelli <jcivelli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562078}

--- chrome/browser/sessions/better_session_restore_browsertest.cc
+++ chrome/browser/sessions/better_session_restore_browsertest.cc
@@ -113,10 +113,6 @@ class BetterSessionRestoreTest : public InProcessBrowserTest {
     test_files_.push_back("session_storage.html");
     test_files_.push_back("subdomain_cookies.html");
 
-    CHECK(base::PathService::Get(base::DIR_SOURCE_ROOT, &test_file_dir_));
-    test_file_dir_ =
-        test_file_dir_.AppendASCII("chrome/test/data").AppendASCII(test_path_);
-
     // We are adding a URLLoaderInterceptor here, instead of in
     // SetUpOnMainThread(), because during a session restore the restored tab
     // comes up before SetUpOnMainThread().  Note that at this point, we do not
@@ -129,13 +125,12 @@ class BetterSessionRestoreTest : public InProcessBrowserTest {
               for (auto& it : test_files_) {
                 std::string file = path_prefix + it;
                 if (path == file) {
-                  base::ScopedAllowBlockingForTesting allow_io;
-                  base::FilePath file_path = test_file_dir_.AppendASCII(it);
-                  std::string contents;
-                  CHECK(base::ReadFileToString(file_path, &contents));
-
+                  std::string relative_path(
+                      "chrome/test/data/session_restore/");
+                  relative_path += it;
+                  std::string headers(kTestHeaders);
                   content::URLLoaderInterceptor::WriteResponse(
-                      kTestHeaders, contents, params->client.get());
+                      relative_path, params->client.get(), &headers);
 
                   return true;
                 }
@@ -340,7 +335,6 @@ class BetterSessionRestoreTest : public InProcessBrowserTest {
   std::string last_upload_bytes_;
   const std::string fake_server_address_;
   std::vector<std::string> test_files_;
-  base::FilePath test_file_dir_;
   const std::string test_path_;
   const base::string16 title_pass_;
   const base::string16 title_storing_;
