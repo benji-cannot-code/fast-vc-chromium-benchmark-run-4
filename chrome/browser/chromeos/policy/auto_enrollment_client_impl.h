5c3996a53093fd12731686f09fa649ffc4c178a9
Pavol Marko <pmarko@chromium.org>
2018-05-27 06:31:26 +0000

    Enable Initial Enrollment for official builds and detect outdated server
    This enables Initial Enrollment by default for official builds.
    Also, this adds functionality to detect a server which does not support
    Initial Enrollment by comparing against a maximal expected modulus
    value.
    
    Bug: 839353
    Test: unit_tests --gtest_filter=AutoEnrollmentClient*
    Change-Id: I1b64ab2875c7317dc92c74276173e27b1c58bbe2
    Reviewed-on: https://chromium-review.googlesource.com/1073309
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562143}

--- chrome/browser/chromeos/policy/auto_enrollment_client_impl.h
+++ chrome/browser/chromeos/policy/auto_enrollment_client_impl.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/macros.h"
 #include "base/memory/scoped_refptr.h"
+#include "base/optional.h"
 #include "base/time/time.h"
 #include "chrome/browser/chromeos/policy/auto_enrollment_client.h"
 #include "components/policy/core/common/cloud/cloud_policy_constants.h"
@@ -72,7 +73,8 @@ class AutoEnrollmentClientImpl
         const std::string& device_serial_number,
         const std::string& device_brand_code,
         int power_initial,
-        int power_limit) override;
+        int power_limit,
+        int power_outdated_server_detect) override;
 
    private:
     DISALLOW_COPY_AND_ASSIGN(FactoryImpl);
@@ -109,6 +111,7 @@ class AutoEnrollmentClientImpl
           state_download_message_processor,
       int power_initial,
       int power_limit,
+      base::Optional<int> power_outdated_server_detect,
       std::string uma_suffix);
 
   // Tries to load the result of a previous execution of the protocol from
@@ -183,6 +186,11 @@ class AutoEnrollmentClientImpl
   // Power of the maximum power-of-2 modulus that this client will accept from
   // a retry response from the server.
   int power_limit_;
+
+  // If set and the modulus requested by the server is higher than
+  // |1<<power_outdated_server_detect|, this client will assume that the server
+  // is outdated.
+  base::Optional<int> power_outdated_server_detect_;
 
   // Number of requests for a different modulus received from the server.
   // Used to determine if the server keeps asking for different moduli.
