6cd961b67f316466da31d1cfe6404c5b3155a0e0
Trent Apted <tapted@chromium.org>
2018-05-28 02:38:58 +0000

    Revert "Media Controls: disable double-tap to scrub in immersive mode."
    This reverts commit 75f5cc7272391b1d22d40d2668f4063ec55a1899.
    
    Reason for revert: Suspect for WebKit Linux Trusty Leak failures since
    https://ci.chromium.org/buildbot/chromium.webkit/WebKit%20Linux%20Trusty%20Leak/19394
    
    Unexpected Failures:
    * media/controls/immersive-doubletap.html
    * virtual/exotic-color-space/images/feature-policy-max-downscaling-image-styles.html
    * virtual/new-remote-playback-pipeline/media/controls/immersive-doubletap.html
    * virtual/video-surface-layer/media/controls/immersive-doubletap.html
    
    Original change's description:
    > Media Controls: disable double-tap to scrub in immersive mode.
    > 
    > It is to easy to accidentally trigger the scrubbing otherwise according
    > to the VR team.
    > 
    > Bug: 827569
    > Change-Id: Ia4bf75e389303628f521caf39166e409feb3b66a
    > Reviewed-on: https://chromium-review.googlesource.com/1071714
    > Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    > Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#562131}
    
    TBR=mlamouri@chromium.org,beccahughes@chromium.org,steimel@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 827569
    Change-Id: I23086a535747900aad1237b0c3dcd34fcf3b1ac9
    Reviewed-on: https://chromium-review.googlesource.com/1074568
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562166}

--- third_party/blink/renderer/modules/media_controls/elements/media_control_overlay_play_button_element.cc
+++ third_party/blink/renderer/modules/media_controls/elements/media_control_overlay_play_button_element.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/events/event.h"
 #include "third_party/blink/renderer/core/dom/shadow_root.h"
 #include "third_party/blink/renderer/core/events/mouse_event.h"
-#include "third_party/blink/renderer/core/frame/settings.h"
 #include "third_party/blink/renderer/core/geometry/dom_rect.h"
 #include "third_party/blink/renderer/core/html/html_style_element.h"
 #include "third_party/blink/renderer/core/html/media/html_media_element.h"
@@ -280,12 +279,6 @@ bool MediaControlOverlayPlayButtonElement::ShouldCausePlayPause(
   // Double tap to navigate should only be available on modern controls.
   if (!MediaControlsImpl::IsModern() || !event->IsMouseEvent())
     return true;
-
-  // Double tap to navigate shouldn't be available in immersive mode.
-  if (GetDocument().GetSettings() &&
-      GetDocument().GetSettings()->GetImmersiveModeEnabled()) {
-    return true;
-  }
 
   // If the event doesn't have position data we should just default to
   // play/pause.
