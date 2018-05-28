c03e2bab625c8d087a7227a448c8d3c64a085b5c
Gauthier Ambard <gambard@chromium.org>
2018-05-28 16:10:14 +0000

    Reland "Move activities to their own folder"
    This is a reland of 115e3e443a5340e0cf5755be48054721772dff71
    
    Original change's description:
    > Move activities to their own folder
    >
    > This CL moves the activities classes to their own folder.
    > This is a preliminary step before adding new activities to have the
    > folder correctly organized.
    >
    > Bug: 845497
    > Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    > Change-Id: Ia3156702233044881ae54154b26951c136e63480
    > Reviewed-on: https://chromium-review.googlesource.com/1073411
    > Reviewed-by: Mark Cogan <marq@chromium.org>
    > Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#562245}
    
    Bug: 845497
    Change-Id: I6bdcc635445ccf13715cddfbca8be08a30793b25
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    
    TBR=marq@chromium.org
    
    Change-Id: I6bdcc635445ccf13715cddfbca8be08a30793b25
    Reviewed-on: https://chromium-review.googlesource.com/1075527
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562272}

--- ios/chrome/browser/ui/activity_services/activities/print_activity.mm
+++ ios/chrome/browser/ui/activity_services/activities/print_activity.mm
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/ui/activity_services/print_activity.h"
+#import "ios/chrome/browser/ui/activity_services/activities/print_activity.h"
 
 #include "base/logging.h"
 #include "ios/chrome/browser/ui/commands/browser_commands.h"
