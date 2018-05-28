059668ce8cd894343fe02e83fb8183aa53feedf9
Gauthier Ambard <gambard@chromium.org>
2018-05-28 13:18:35 +0000

    Move activities to their own folder
    This CL moves the activities classes to their own folder.
    This is a preliminary step before adding new activities to have the
    folder correctly organized.
    
    Bug: 845497
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ia3156702233044881ae54154b26951c136e63480
    Reviewed-on: https://chromium-review.googlesource.com/1073411
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562245}

--- ios/chrome/browser/ui/activity_services/activity_type_util_unittest.mm
+++ ios/chrome/browser/ui/activity_services/activity_type_util_unittest.mm
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/activity_services/activity_type_util.h"
 
+#include "ios/chrome/browser/ui/activity_services/activities/print_activity.h"
 #include "ios/chrome/browser/ui/activity_services/appex_constants.h"
-#include "ios/chrome/browser/ui/activity_services/print_activity.h"
 #include "ios/chrome/grit/ios_strings.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/gtest_mac.h"
