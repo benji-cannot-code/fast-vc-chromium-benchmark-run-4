d60c9f32052ba30599c6c4b71d54dc3682de2661
Saman Sami <samans@chromium.org>
2018-05-28 19:05:37 +0000

    viz/android: Fix DCHECK failure in SingleThreadProxy::SetNeedsAnimate
    Calling DidSwapBuffers can result in a call into SetNeedsAnimate in
    the same call stack. DidSubmitCompositorFrame is in a scope of a
    DebugScopedSetImplThread object so calling DidSwapBuffers inside of it
    triggers the DCHECK in SetNeedsAnimate that expects us to be on the
    main thread, so move the DidSwapBuffers call into
    DidReceiveCompositorFrameAck that doesn't have the same problem.
    DidReceiveCompositorFrameAck is also a closer signal to DidSwapBuffers
    than DidSubmitCompositorFrame is.
    
    Bug: 847258
    Change-Id: Ie02c28e0063022b7f404b91067b7805b8c1efb6b
    Reviewed-on: https://chromium-review.googlesource.com/1075669
    Reviewed-by: Fady Samuel <fsamuel@chromium.org>
    Commit-Queue: Saman Sami <samans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562296}

--- content/browser/renderer_host/compositor_impl_android.cc
+++ content/browser/renderer_host/compositor_impl_android.cc
@@ -1050,12 +1050,6 @@ void CompositorImpl::DidSubmitCompositorFrame() {
   TRACE_EVENT0("compositor", "CompositorImpl::DidSubmitCompositorFrame");
   pending_frames_++;
   has_submitted_frame_since_became_visible_ = true;
-
-  if (enable_viz_) {
-    // TODO(ericrk): Viz should use the actual swap callback from the Viz
-    // process. This is just a workaround until we wire that up.
-    DidSwapBuffers(size_);
-  }
 }
 
 void CompositorImpl::DidReceiveCompositorFrameAck() {
@@ -1063,6 +1057,12 @@ void CompositorImpl::DidReceiveCompositorFrameAck() {
   DCHECK_GT(pending_frames_, 0U);
   pending_frames_--;
   client_->DidSwapFrame(pending_frames_);
+
+  if (enable_viz_) {
+    // TODO(ericrk): Viz should use the actual swap callback from the Viz
+    // process. This is just a workaround until we wire that up.
+    DidSwapBuffers(size_);
+  }
 }
 
 void CompositorImpl::DidLoseLayerTreeFrameSink() {
