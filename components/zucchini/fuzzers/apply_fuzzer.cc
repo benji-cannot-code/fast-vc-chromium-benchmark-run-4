7fe07ce6fe5b26b883279a04494c966c51a7a975
Calder Kitagawa <ckitagawa@chromium.org>
2018-05-28 14:30:21 +0000

    [Zucchini] ZTF Apply Fuzzer
    This is part of a series of Fuzzers to be added to Zucchini for
    security review. This tests the full patch application logic
    exercising the patch reader and apply process. It covers ~33% of code
    in 1000000 runs. The bulk of remaining code ~40% is covered by ZTF Gen
    Fuzzer. With the remainder (~30%) being for DEX Disassembly (not in
    launch scope), patch serialization (trusted input), and other
    testing/debugging/error handling code which isn't triggered.
    
    This already found a couple bugs fixed in
    https://chromium-review.googlesource.com/c/chromium/src/+/1072272
    
    With the supplied seed corpus the fuzzer reaches approximately 12000
    execs/s.
    
    The file format for the seed is a FilePair proto of a ZTF base file
    and a patch file as used in Raw Apply. This reuses the same generator
    and fuzzer as Raw Apply as the type of application is encoded in the
    patch itself.
    
    Bug: 835341
    Change-Id: I00f28c768a6e1c7b8c5e95979b279d64785ef515
    Reviewed-on: https://chromium-review.googlesource.com/1072231
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Max Moroz <mmoroz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562260}

--- components/zucchini/fuzzers/apply_fuzzer.cc
+++ components/zucchini/fuzzers/apply_fuzzer.cc
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 struct Environment {
   Environment() {
-    logging::SetMinLogLevel(3);  // Disable console spamming.
+    logging::SetMinLogLevel(logging::LOG_FATAL);  // Disable console spamming.
   }
 };
 
