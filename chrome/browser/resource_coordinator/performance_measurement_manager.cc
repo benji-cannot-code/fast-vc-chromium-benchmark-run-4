675011486123a3a2b893368195d3860d792e7f31
Sigurdur Asgeirsson <siggi@chromium.org>
2018-05-28 14:20:53 +0000

    Start on PerformanceMeasurementManager.
    This will glue together the measurement cycle from event notifications
    from the TabLoadTracker to the measurements in RenderProcessProbe.
    Eventually this class will also persist complete measurements issued
    from an observer in the RC service by sinking notifications and
    persisting to the feature database.
    
    Bug: 755840
    Change-Id: I8c6673fcf205709ad96435295f9010a6aceb3017
    Reviewed-on: https://chromium-review.googlesource.com/1072232
    Commit-Queue: Sigurður Ásgeirsson <siggi@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562258}

---
+++ chrome/browser/resource_coordinator/performance_measurement_manager.cc
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/resource_coordinator/performance_measurement_manager.h"
+
+#include "chrome/browser/resource_coordinator/render_process_probe.h"
+#include "chrome/browser/resource_coordinator/tab_load_tracker.h"
+
+namespace resource_coordinator {
+
+PerformanceMeasurementManager::PerformanceMeasurementManager(
+    TabLoadTracker* tab_load_tracker,
+    RenderProcessProbe* render_process_probe)
+    : scoped_observer_(this), render_process_probe_(render_process_probe) {
+  scoped_observer_.Add(tab_load_tracker);
+}
+
+PerformanceMeasurementManager::~PerformanceMeasurementManager() = default;
+
+void PerformanceMeasurementManager::OnStartTracking(
+    content::WebContents* web_contents,
+    LoadingState loading_state) {
+  if (loading_state == TabLoadTracker::LOADED)
+    render_process_probe_->StartSingleGather();
+}
+
+void PerformanceMeasurementManager::OnLoadingStateChange(
+    content::WebContents* web_contents,
+    LoadingState loading_state) {
+  if (loading_state == TabLoadTracker::LOADED)
+    render_process_probe_->StartSingleGather();
+}
+
+}  // namespace resource_coordinator
