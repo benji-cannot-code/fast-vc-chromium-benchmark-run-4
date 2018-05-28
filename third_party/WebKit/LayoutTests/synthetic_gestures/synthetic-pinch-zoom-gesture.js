9d1dac98addd806475f030c29f53d60622148af9
David Bokan <bokan@chromium.org>
2018-05-28 16:32:20 +0000

    Fix flaky synthetic_gesture pinch zoom tests
    The -desktop versions of these tests were using large error bounds since
    the offset in a synthetic pinch zoom doesn't work today. Turns out it's
    not consistent either. Remove the offset check altogether for these
    tests to deflake them.
    
    Also lower the epsilon on scale for the rest of the tests since this was
    causing some mild flakiness.
    
    Bug: 846494
    Change-Id: Ic032da120ba9abf25a0c62c0935c713720f2898e
    Reviewed-on: https://chromium-review.googlesource.com/1074271
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562278}

--- third_party/WebKit/LayoutTests/synthetic_gestures/synthetic-pinch-zoom-gesture.js
+++ third_party/WebKit/LayoutTests/synthetic_gestures/synthetic-pinch-zoom-gesture.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-const kScaleEpsilon = 0.0001;
+const kScaleEpsilon = 0.1;
 const kOffsetEpsilon = 2;
 
 const centerX = window.innerWidth / 2;
@@ -49,10 +49,10 @@ async function runAllTestCases(t, testCases) {
 
 // TODO(bokan): Added temporarily to allow testing the pinch-zoom on desktop in
 // a less strict way. Currently, touchscreen pinch-zoom on desktop doesn't zoom
-// to the expected location. Until that's fixed we'll run tests with a larger
-// error bound so that we prevent further regression but once it's fixed we can
-// remove this version and run the strict tests. https://crbug.com/787615
-const kOffsetEpsilonDesktop = 170;
+// to the expected location. Until that's fixed we'll run tests without
+// checking the location so we guard regressing the scale but once it's fixed
+// we can remove this version and run the strict tests.
+// https://crbug.com/787615
 function runTestDesktop(t, testCase) {
   return new Promise(t.step_func((resolve, reject) => {
     internals.setPageScaleFactor(testCase.startingScale);
@@ -70,18 +70,6 @@ function runTestDesktop(t, testCase) {
             expectedScale,
             kScaleEpsilon,
             testCase.msg + " has correct page scale factor.");
-        assert_approx_equals(
-            window.visualViewport.offsetLeft,
-            window.innerWidth * (1 - (1 / expectedScale)) / 2,
-            kOffsetEpsilonDesktop,
-            testCase.msg +
-                " has approximately correct visual viewport offsetLeft.");
-        assert_approx_equals(
-            window.visualViewport.offsetTop,
-            window.innerHeight * (1 - (1 / expectedScale)) / 2,
-            kOffsetEpsilonDesktop,
-            testCase.msg +
-                " has approximately correct visual viewport offsetTop.");
         resolve();
       }), testCase.speed, testCase.gestureSource);
     }));
