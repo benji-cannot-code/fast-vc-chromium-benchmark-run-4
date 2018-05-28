124d2d8f3bb7bf32d8e789dd4da36243e71a4986
Roger McFarlane <rogerm@chromium.org>
2018-05-28 20:38:47 +0000

    [autofill] Add UMA counter for times unowned fields are not fillable.
    The CL adds histograms and associated unittests to count the number of
    times autofill is suppressed due to unowned (formless) fields being
    restricted to pages with "checkout" related signals.
    
    Bug: 841784, 806987
    Change-Id: Ied2f55234938793de3c5e7bce248e2cfbe80fb6c
    Reviewed-on: https://chromium-review.googlesource.com/1073568
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Roger McFarlane <rogerm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562308}

--- components/autofill/content/renderer/form_autofill_util.cc
+++ components/autofill/content/renderer/form_autofill_util.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/i18n/case_conversion.h"
 #include "base/logging.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/no_destructor.h"
 #include "base/stl_util.h"
 #include "base/strings/string_number_conversions.h"
@@ -821,10 +822,21 @@ void ForEachMatchingFormFieldCommon(
     bool force_override,
     const Callback& callback) {
   DCHECK(control_elements);
-  if (control_elements->size() != data.fields.size()) {
-    // This case should be reachable only for pathological websites and tests,
-    // which add or remove form fields while the user is interacting with the
-    // Autofill popup.
+
+  const bool num_elements_matches_num_fields =
+      control_elements->size() == data.fields.size();
+  UMA_HISTOGRAM_BOOLEAN("Autofill.NumElementsMatchesNumFields",
+                        num_elements_matches_num_fields);
+  if (!num_elements_matches_num_fields) {
+    // http://crbug.com/841784
+    // This pathological case was only thought to be reachable iff the fields
+    // are added/removed from the form while the user is interacting with the
+    // autofill popup.
+    //
+    // Is is also reachable for formless non-checkout forms when checkout
+    // restrictions are applied.
+    //
+    // TODO(crbug/847221): Add a UKM to capture these events.
     return;
   }
 
@@ -925,6 +937,7 @@ void FillFormField(const FormFieldData& data,
     return;
 
   WebInputElement* input_element = ToWebInputElement(field);
+
   if (IsCheckableElement(input_element)) {
     input_element->SetChecked(IsChecked(data.check_status), true);
   } else {
@@ -1728,6 +1741,15 @@ bool UnownedCheckoutFormElementsAndFieldSetsToFormData(
       elements_with_autocomplete.push_back(element);
     }
   }
+
+  // http://crbug.com/841784
+  // Capture the number of times this formless checkout logic prevents a from
+  // being autofilled (fill logic expects to receive a autofill field entry,
+  // possibly not fillable, for each control element).
+  // Note: this will be fixed by http://crbug.com/806987
+  UMA_HISTOGRAM_BOOLEAN(
+      "Autofill.UnownedFieldsWereFiltered",
+      elements_with_autocomplete.size() != control_elements.size());
 
   if (elements_with_autocomplete.empty())
     return false;
