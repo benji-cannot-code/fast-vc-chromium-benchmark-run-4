c68570bbc6d3e37011df0d8012514255ead2b80d
Charlie Harrison <csharrison@chromium.org>
2018-05-29 17:44:19 +0000

    Improve subframe filtering test coverage
    This CL adds unit tests for dryrun filtering, as well as console logging.
    
    Bug: 843304
    Change-Id: I63918590f8f2033e1289da2898c44040ffc2c223
    Reviewed-on: https://chromium-review.googlesource.com/1073198
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562494}

--- components/subresource_filter/content/browser/subframe_navigation_filtering_throttle_unittest.cc
+++ components/subresource_filter/content/browser/subframe_navigation_filtering_throttle_unittest.cc
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/subresource_filter/content/browser/subframe_navigation_filtering_throttle.h"
 
 #include <memory>
+#include <sstream>
+#include <string>
 
 #include "base/callback.h"
 #include "base/message_loop/message_loop_current.h"
@@ -15,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/subresource_filter/content/browser/async_document_subresource_filter.h"
 #include "components/subresource_filter/content/browser/async_document_subresource_filter_test_utils.h"
 #include "components/subresource_filter/content/browser/subresource_filter_observer_test_utils.h"
+#include "components/subresource_filter/core/browser/subresource_filter_constants.h"
 #include "components/subresource_filter/core/common/activation_level.h"
 #include "components/subresource_filter/core/common/activation_state.h"
 #include "components/subresource_filter/core/common/test_ruleset_creator.h"
@@ -23,6 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/navigation_simulator.h"
 #include "content/public/test/test_renderer_host.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "url/gurl.h"
+#include "url/origin.h"
 
 namespace subresource_filter {
 
@@ -72,13 +77,16 @@ class SubframeNavigationFilteringThrottleTest
     // The |parent_filter_| is the parent frame's filter. Do not register a
     // throttle if the parent is not activated with a valid filter.
     if (parent_filter_) {
-      navigation_handle->RegisterThrottleForTesting(
-          std::make_unique<SubframeNavigationFilteringThrottle>(
-              navigation_handle, parent_filter_.get(), &mock_delegate_));
+      auto throttle = std::make_unique<SubframeNavigationFilteringThrottle>(
+          navigation_handle, parent_filter_.get(), &mock_delegate_);
+      ASSERT_NE(nullptr, throttle->GetNameForLogging());
+      navigation_handle->RegisterThrottleForTesting(std::move(throttle));
     }
   }
 
-  void InitializeDocumentSubresourceFilter(const GURL& document_url) {
+  void InitializeDocumentSubresourceFilter(
+      const GURL& document_url,
+      ActivationLevel parent_level = ActivationLevel::ENABLED) {
     ASSERT_NO_FATAL_FAILURE(
         test_ruleset_creator_.CreateRulesetToDisallowURLsWithPathSuffix(
             "disallowed.html", &test_ruleset_pair_));
@@ -94,15 +102,16 @@ class SubframeNavigationFilteringThrottleTest
         std::make_unique<VerifiedRuleset::Handle>(dealer_handle_.get());
 
     testing::TestActivationStateCallbackReceiver activation_state;
+    ActivationState parent_activation_state(parent_level);
+    parent_activation_state.enable_logging = true;
     parent_filter_ = std::make_unique<AsyncDocumentSubresourceFilter>(
         ruleset_handle_.get(),
         AsyncDocumentSubresourceFilter::InitializationParams(
-            document_url, ActivationLevel::ENABLED,
-            false /* measure_performance */),
+            document_url, url::Origin::Create(document_url),
+            parent_activation_state),
         activation_state.GetCallback());
     RunUntilIdle();
-    activation_state.ExpectReceivedOnce(
-        ActivationState(ActivationLevel::ENABLED));
+    activation_state.ExpectReceivedOnce(parent_activation_state);
   }
 
   void RunUntilIdle() { base::RunLoop().RunUntilIdle(); }
@@ -143,6 +152,18 @@ class SubframeNavigationFilteringThrottleTest
     navigation_simulator_->CommitErrorPage();
   }
 
+  const std::vector<std::string>& GetConsoleMessages() {
+    return content::RenderFrameHostTester::For(main_rfh())
+        ->GetConsoleMessages();
+  }
+
+  std::string GetFilterConsoleMessage(const GURL& filtered_url) {
+    std::ostringstream oss(kDisallowSubframeConsoleMessagePrefix);
+    oss << filtered_url;
+    oss << kDisallowSubframeConsoleMessageSuffix;
+    return oss.str();
+  }
+
  private:
   testing::TestRulesetCreator test_ruleset_creator_;
   testing::TestRulesetPair test_ruleset_pair_;
@@ -160,10 +181,12 @@ class SubframeNavigationFilteringThrottleTest
 
 TEST_F(SubframeNavigationFilteringThrottleTest, FilterOnStart) {
   InitializeDocumentSubresourceFilter(GURL("https://example.test"));
-  CreateTestSubframeAndInitNavigation(
-      GURL("https://example.test/disallowed.html"), main_rfh());
+  const GURL url("https://example.test/disallowed.html");
+  CreateTestSubframeAndInitNavigation(url, main_rfh());
   SimulateStartAndExpectResult(
       content::NavigationThrottle::BLOCK_REQUEST_AND_COLLAPSE);
+  EXPECT_TRUE(
+      base::ContainsValue(GetConsoleMessages(), GetFilterConsoleMessage(url)));
 }
 
 TEST_F(SubframeNavigationFilteringThrottleTest, FilterOnRedirect) {
@@ -176,6 +199,28 @@ TEST_F(SubframeNavigationFilteringThrottleTest, FilterOnRedirect) {
       content::NavigationThrottle::BLOCK_REQUEST_AND_COLLAPSE;
   SimulateRedirectAndExpectResult(GURL("https://example.test/disallowed.html"),
                                   expected_result);
+}
+
+TEST_F(SubframeNavigationFilteringThrottleTest, DryRunOnStart) {
+  InitializeDocumentSubresourceFilter(GURL("https://example.test"),
+                                      ActivationLevel::DRYRUN);
+  const GURL url("https://example.test/disallowed.html");
+  CreateTestSubframeAndInitNavigation(url, main_rfh());
+
+  SimulateStartAndExpectResult(content::NavigationThrottle::PROCEED);
+  EXPECT_FALSE(
+      base::ContainsValue(GetConsoleMessages(), GetFilterConsoleMessage(url)));
+}
+
+TEST_F(SubframeNavigationFilteringThrottleTest, DryRunOnRedirect) {
+  InitializeDocumentSubresourceFilter(GURL("https://example.test"),
+                                      ActivationLevel::DRYRUN);
+  CreateTestSubframeAndInitNavigation(GURL("https://example.test/allowed.html"),
+                                      main_rfh());
+
+  SimulateStartAndExpectResult(content::NavigationThrottle::PROCEED);
+  SimulateRedirectAndExpectResult(GURL("https://example.test/disallowed.html"),
+                                  content::NavigationThrottle::PROCEED);
 }
 
 TEST_F(SubframeNavigationFilteringThrottleTest, FilterOnSecondRedirect) {
