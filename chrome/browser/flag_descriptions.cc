0d901f043d0514776d57c983827bb723a6970aae
Elly Fong-Jones <ellyjones@chromium.org>
2018-05-29 18:20:48 +0000

    chrome: add ui-food flag
    This flag will enable all upcoming UI features to make team testing easier.
    
    Bug: None
    Change-Id: I415933aa783b1cbd9efa71c606a3db4de18d2a33
    Reviewed-on: https://chromium-review.googlesource.com/1076492
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Rachel Blum <groby@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562512}

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -3153,6 +3153,13 @@ const char kAutomaticTabDiscardingDescription[] =
 
 #endif  // defined(OS_WIN) || defined(OS_MACOSX)
 
+#if defined(OS_WIN) || defined(OS_MACOSX) || defined(OS_LINUX)
+
+const char kUiFoodName[] = "Use all upcoming UI features";
+const char kUiFoodDescription[] = "Use all upcoming UI features.";
+
+#endif
+
 // Feature flags --------------------------------------------------------------
 
 #if BUILDFLAG(ENABLE_VR)
--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1934,6 +1934,13 @@ extern const char kWarnBeforeQuittingFlagDescription[];
 
 #endif  // defined(OS_WIN) || defined(OS_LINUX)
 
+#if defined(OS_WIN) || defined(OS_MACOSX) || defined(OS_LINUX)
+
+extern const char kUiFoodName[];
+extern const char kUiFoodDescription[];
+
+#endif  // defined(OS_WIN) || defined(OS_MACOSX) || defined(OS_LINUX)
+
 #if defined(OS_WIN) || defined(OS_MACOSX)
 
 extern const char kAutomaticTabDiscardingName[];
