c74a6dc91ac77bb94678b76c6e181e98ae49659b
Paul Jensen <pauljensen@chromium.org>
2018-05-29 21:04:05 +0000

    Disable internal DNS resolver when Android P using private DNS
    Bug: 842456
    Change-Id: I8f0c17cd50c75987c051e62344ac5e907e1b1133
    Reviewed-on: https://chromium-review.googlesource.com/1065463
    Reviewed-by: Misha Efimov <mef@chromium.org>
    Commit-Queue: Paul Jensen <pauljensen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562584}

--- net/android/network_library.h
+++ net/android/network_library.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/ip_endpoint.h"
 #include "net/base/mime_util.h"
 #include "net/base/net_export.h"
+#include "net/dns/dns_config_service_posix.h"
 #include "net/socket/socket_descriptor.h"
 
 namespace net {
@@ -87,7 +88,11 @@ NET_EXPORT_PRIVATE std::string GetWifiSSID();
 
 // Gets the DNS servers and puts them in |dns_servers|.
 // Only callable on Marshmallow and newer releases.
-NET_EXPORT_PRIVATE void GetDnsServers(std::vector<IPEndPoint>* dns_servers);
+// Returns CONFIG_PARSE_POSIX_OK upon success,
+// CONFIG_PARSE_POSIX_NO_NAMESERVERS if no DNS servers found, or
+// CONFIG_PARSE_POSIX_PRIVATE_DNS_ACTIVE if private DNS active.
+NET_EXPORT_PRIVATE internal::ConfigParsePosixResult GetDnsServers(
+    std::vector<IPEndPoint>* dns_servers);
 
 // Apply TrafficStats tag |tag| and UID |uid| to |socket|. Future network
 // traffic used by |socket| will be attributed to |uid| and |tag|.
