1efbaa4412da5c6b9857d3797d3ca507eb7f7896
Brian Anderson <brianderson@chromium.org>
2018-05-29 19:21:00 +0000

    ui: Improve FrameMetrics tracing.
    * Enable FrameMetrics to differentiate between source type,
        source thread, and compile target.
    * Report sample count for worst windows.
    * Move tracing logic from StreamAnalyzer to StreamAnalysis,
        which allows results to be stored and traced at a later
        point.
    
    Bug: 790759
    Change-Id: I763a0f354d97119c16635675a2b9fa29a947f417
    Reviewed-on: https://chromium-review.googlesource.com/1072872
    Commit-Queue: Brian Anderson <brianderson@chromium.org>
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562543}

--- ui/latency/stream_analyzer.h
+++ ui/latency/stream_analyzer.h
@@ -39,9 +39,12 @@ struct StreamAnalysis {
   std::vector<ThresholdResult> thresholds;
   PercentileResults percentiles;
 
+  size_t worst_sample_count = 0;
   FrameRegionResult worst_mean;
   FrameRegionResult worst_rms;
   FrameRegionResult worst_smr;
+
+  void AsValueInto(base::trace_event::TracedValue* state) const;
 
   DISALLOW_COPY_AND_ASSIGN(StreamAnalysis);
 };
@@ -116,8 +119,6 @@ class StreamAnalyzer {
   const WindowedAnalyzer& window() const { return windowed_analyzer_; }
 
   void ComputeSummary(StreamAnalysis* results) const;
-  std::unique_ptr<base::trace_event::ConvertableToTraceFormat> AsValue() const;
-  void AsValueInto(base::trace_event::TracedValue* state) const;
 
  protected:
   double VarianceHelper(double accum, double square_accum) const;
