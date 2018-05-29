ffeae98b6666da1e923c2a4d3a9d79dd88832cd6
Patrick Hulce <phulce@chromium.org>
2018-05-29 22:05:10 +0000

    DevTools: Deflake Audits2 tests
    BUG=846211,846551
    
    Change-Id: I26d7134ad0dbd7de87dc792265a81d293631a778
    Reviewed-on: https://chromium-review.googlesource.com/1071883
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Reviewed-by: Paul Irish <paulirish@chromium.org>
    Commit-Queue: Patrick Hulce <phulce@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562618}

--- third_party/WebKit/LayoutTests/http/tests/devtools/audits2/audits2-limited-run.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/audits2/audits2-limited-run.js
@@ -23,12 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   Audits2TestRunner.getRunButton().click();
 
   var results = await Audits2TestRunner.waitForResults();
-  TestRunner.addResult(`\n=============== Lighthouse Results ===============`);
-
-  Object.keys(results.audits).sort().forEach(auditName => {
-    var audit = results.audits[auditName];
-    TestRunner.addResult(`${audit.name}: ${Boolean(audit.rawValue)}`);
-  });
+  TestRunner.addResult(`\n=============== Audits run ===============`);
+  TestRunner.addResult(Object.keys(results.audits).sort().join('\n'));
 
   TestRunner.completeTest();
 })();
