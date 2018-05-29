33cd6f1b9d3680087acfb178ac51ed9cdb6da006
Wez <wez@chromium.org>
2018-05-29 21:13:25 +0000

    Remove some no-longer-used #includes from ChromeOS policy browser tests.
    Bug: 844016
    Change-Id: Ida401d3f0dd2b6aa6f82219f4cf3c0ca417762c5
    Reviewed-on: https://chromium-review.googlesource.com/1075725
    Reviewed-by: Bartosz Fabianowski <bartfab@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562590}

--- chrome/browser/chromeos/policy/display_rotation_default_handler_browsertest.cc
+++ chrome/browser/chromeos/policy/display_rotation_default_handler_browsertest.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/dbus/session_manager_client.h"
 #include "chromeos/settings/cros_settings_names.h"
 #include "components/policy/proto/chrome_device_policy.pb.h"
-#include "content/public/test/test_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/display/display.h"
 #include "ui/display/display_layout.h"
