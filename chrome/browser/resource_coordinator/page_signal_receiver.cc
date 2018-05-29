81e316d964a832256b49b022b612dce16b16d55a
Sebastien Marchand <sebmarchand@chromium.org>
2018-05-29 20:39:49 +0000

    Make it explicit that CU IDs passed in PageSignalReceiver are page CU IDs.
    Follow up on this comment: https://chromium-review.googlesource.com/c/chromium/src/+/1068665/13/services/resource_coordinator/public/mojom/page_signal.mojom#18
    
    Change-Id: Ib930db9c0326c9fae8cd7c2bf8b771d5a196c164
    Reviewed-on: https://chromium-review.googlesource.com/1076897
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Commit-Queue: Sébastien Marchand <sebmarchand@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562576}

--- chrome/browser/resource_coordinator/page_signal_receiver.cc
+++ chrome/browser/resource_coordinator/page_signal_receiver.cc
@@ -37,9 +37,10 @@ PageSignalReceiver::PageSignalReceiver() : binding_(this) {}
 
 PageSignalReceiver::~PageSignalReceiver() = default;
 
-void PageSignalReceiver::NotifyPageAlmostIdle(const CoordinationUnitID& cu_id) {
+void PageSignalReceiver::NotifyPageAlmostIdle(
+    const CoordinationUnitID& page_cu_id) {
   DCHECK(IsPageAlmostIdleSignalEnabled());
-  auto web_contents_iter = cu_id_web_contents_map_.find(cu_id);
+  auto web_contents_iter = cu_id_web_contents_map_.find(page_cu_id);
   if (web_contents_iter == cu_id_web_contents_map_.end())
     return;
   for (auto& observer : observers_)
@@ -47,9 +48,9 @@ void PageSignalReceiver::NotifyPageAlmostIdle(const CoordinationUnitID& cu_id) {
 }
 
 void PageSignalReceiver::SetExpectedTaskQueueingDuration(
-    const CoordinationUnitID& cu_id,
+    const CoordinationUnitID& page_cu_id,
     base::TimeDelta duration) {
-  auto web_contents_iter = cu_id_web_contents_map_.find(cu_id);
+  auto web_contents_iter = cu_id_web_contents_map_.find(page_cu_id);
   if (web_contents_iter == cu_id_web_contents_map_.end())
     return;
   for (auto& observer : observers_)
@@ -57,9 +58,9 @@ void PageSignalReceiver::SetExpectedTaskQueueingDuration(
                                                duration);
 }
 
-void PageSignalReceiver::SetLifecycleState(const CoordinationUnitID& cu_id,
+void PageSignalReceiver::SetLifecycleState(const CoordinationUnitID& page_cu_id,
                                            mojom::LifecycleState state) {
-  auto web_contents_iter = cu_id_web_contents_map_.find(cu_id);
+  auto web_contents_iter = cu_id_web_contents_map_.find(page_cu_id);
   if (web_contents_iter == cu_id_web_contents_map_.end())
     return;
   for (auto& observer : observers_)
@@ -67,8 +68,8 @@ void PageSignalReceiver::SetLifecycleState(const CoordinationUnitID& cu_id,
 }
 
 void PageSignalReceiver::NotifyNonPersistentNotificationCreated(
-    const CoordinationUnitID& cu_id) {
-  auto web_contents_iter = cu_id_web_contents_map_.find(cu_id);
+    const CoordinationUnitID& page_cu_id) {
+  auto web_contents_iter = cu_id_web_contents_map_.find(page_cu_id);
   if (web_contents_iter == cu_id_web_contents_map_.end())
     return;
   for (auto& observer : observers_)
