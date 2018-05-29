a1010f26bc0e8533ae6fe0150caa950f41d4077a
Amy Qiu <amyqiu@google.com>
2018-05-29 21:39:59 +0000

    Remove unnecessary attribute from top_real_world_mobile
    Since the 'action_on_load_complete' attribute is only used by one child
    class, move it out of the parent class.
    
    Bug: 760553
    Change-Id: I364794926e8d6b342e5b22402cefd6893dd3b5a2
    Reviewed-on: https://chromium-review.googlesource.com/1076976
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Commit-Queue: Amy Qiu <amyqiu@google.com>
    Cr-Commit-Position: refs/heads/master@{#562601}

--- tools/perf/page_sets/rendering/top_real_world_mobile.py
+++ tools/perf/page_sets/rendering/top_real_world_mobile.py
@@ -26,9 +26,6 @@ class TopRealWorldMobilePage(rendering_story.RenderingStory):
         shared_page_state_class=shared_page_state_class)
 
   def RunPageInteractions(self, action_runner):
-    if self.action_on_load_complete:
-      action_runner.WaitForJavaScriptCondition(
-          'document.readyState == "complete"', timeout=30)
     with action_runner.CreateGestureInteraction('ScrollAction'):
       action_runner.ScrollPage()
 
@@ -297,7 +294,12 @@ class WikipediaDelayedScrollMobilePage(TopRealWorldMobilePage):
         name_suffix=name_suffix,
         extra_browser_args=extra_browser_args,
         shared_page_state_class=shared_page_state_class)
-    self.action_on_load_complete = True
+
+  def RunPageInteractions(self, action_runner):
+    action_runner.WaitForJavaScriptCondition(
+      'document.readyState == "complete"', timeout=30)
+    with action_runner.CreateGestureInteraction('ScrollAction'):
+      action_runner.ScrollPage()
 
 
 class BlogspotMobilePage(TopRealWorldMobilePage):
