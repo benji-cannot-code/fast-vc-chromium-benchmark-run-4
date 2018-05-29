285c858c7d363a2be3b360e3a85b5a45deb4fc00
Penny MacNeil <pennymac@chromium.org>
2018-05-29 17:05:17 +0000

    [chrome_elf, third-party block support] full hook testing.
    Adding some full tests, including some corner cases.  These tests
    initialize the whole third_party_dlls project, to exercise the new hook.
    
    Non-test adjustments:
    - pe_image_safe: |directory| == 0 is valid!  (export dir)
    - hook:  Exposed a GetDataFromImageForTesting() function.
      Also a few fixes in the hook shim:
      * Use the PEImageSafe constructor that takes an HMODULE.
      * Use the new GetFingerprintString() api.
      * Don't hash an empty image or section name.
      * Pass a name hash instead of non-hashed into LogLoadAttempt
        if a DLL was allowed.
    - packed_list_file:
      * GetFingerprintHash() removed from this project.
      * Also, gracefully handle a blacklist file that exists, but is empty.
    - packed_list_format:
      * Added "new" GetFingerprintString() API here.
        It centralizes the format string without doing the hash.
    
    Test: chrome_elf_unittests.exe, ThirdPartyTest.*
    Bug: 769590
    Change-Id: I592f76b884312bb267eb6747a478e271fc96e689
    Reviewed-on: https://chromium-review.googlesource.com/1069694
    Commit-Queue: Penny MacNeil <pennymac@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562467}

--- chrome_elf/third_party_dlls/hook.h
+++ chrome_elf/third_party_dlls/hook.h
@@ -6,6 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_ELF_THIRD_PARTY_DLLS_HOOK_H_
 #define CHROME_ELF_THIRD_PARTY_DLLS_HOOK_H_
 
+#include <windows.h>
+
+#include <string>
+
 namespace third_party_dlls {
 
 // "static_cast<int>(HookStatus::value)" to access underlying value.
@@ -21,6 +25,15 @@ enum class HookStatus {
 // Apply hook.
 // - Ensure the rest of third_party_dlls is initialized before hooking.
 HookStatus ApplyHook();
+
+// Testing-only access to private GetDataFromImage() function.
+bool GetDataFromImageForTesting(PVOID buffer,
+                                DWORD buffer_size,
+                                DWORD* time_date_stamp,
+                                DWORD* image_size,
+                                std::string* image_name,
+                                std::string* section_path,
+                                std::string* section_basename);
 
 }  // namespace third_party_dlls
 
