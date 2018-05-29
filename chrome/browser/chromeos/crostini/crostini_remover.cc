54fd4610572bbb0e6827574c35cdab2dadb422c9
Renee Wright <rjwright@chromium.org>
2018-05-29 08:10:09 +0000

    Use specified container + vm names, not defaults, in crostini_remover
    Change-Id: Ic577a8d2b3f4feb3dbe2e6e40386e41a4487f603
    Reviewed-on: https://chromium-review.googlesource.com/1075886
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Commit-Queue: Renée Wright <rjwright@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562366}

--- chrome/browser/chromeos/crostini/crostini_remover.cc
+++ chrome/browser/chromeos/crostini/crostini_remover.cc
@@ -50,7 +50,7 @@ void CrostiniRemover::OnConciergeStarted(ConciergeClientResult result) {
     return;
   }
   CrostiniManager::GetInstance()->StopVm(
-      profile_, kCrostiniDefaultVmName,
+      profile_, vm_name_,
       base::BindOnce(&CrostiniRemover::StopVmFinished, this));
 }
 
@@ -68,9 +68,9 @@ void CrostiniRemover::StopVmFinished(ConciergeClientResult result) {
     return;
   }
   CrostiniRegistryServiceFactory::GetForProfile(profile_)->ClearApplicationList(
-      kCrostiniDefaultVmName, kCrostiniDefaultContainerName);
+      vm_name_, container_name_);
   CrostiniManager::GetInstance()->DestroyDiskImage(
-      CryptohomeIdForProfile(profile_), base::FilePath(kCrostiniDefaultVmName),
+      CryptohomeIdForProfile(profile_), base::FilePath(vm_name_),
       vm_tools::concierge::StorageLocation::STORAGE_CRYPTOHOME_ROOT,
       base::BindOnce(&CrostiniRemover::DestroyDiskImageFinished, this));
 }
