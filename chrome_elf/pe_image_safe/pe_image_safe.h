39c27518f1949c54d276016c81450c2d807bedf2
Penny MacNeil <pennymac@chromium.org>
2018-05-29 23:29:33 +0000

    Reland "[chrome_elf, third-party block support] full hook testing."
    This is a reland of 2c24de69532cb26cc199bb07f9ebfa6730bc26e2
    
    Original change's description:
    > [chrome_elf, third-party block support] full hook testing.
    > 
    > Adding some full tests, including some corner cases.  These tests
    > initialize the whole third_party_dlls project, to exercise the new hook.
    > 
    > Non-test adjustments:
    > - pe_image_safe: |directory| == 0 is valid!  (export dir)
    > - hook:  Exposed a GetDataFromImageForTesting() function.
    >   Also a few fixes in the hook shim:
    >   * Use the PEImageSafe constructor that takes an HMODULE.
    >   * Use the new GetFingerprintString() api.
    >   * Don't hash an empty image or section name.
    >   * Pass a name hash instead of non-hashed into LogLoadAttempt
    >     if a DLL was allowed.
    > - packed_list_file:
    >   * GetFingerprintHash() removed from this project.
    >   * Also, gracefully handle a blacklist file that exists, but is empty.
    > - packed_list_format:
    >   * Added "new" GetFingerprintString() API here.
    >     It centralizes the format string without doing the hash.
    > 
    > Test: chrome_elf_unittests.exe, ThirdPartyTest.*
    > Bug: 769590
    > Change-Id: I592f76b884312bb267eb6747a478e271fc96e689
    > Reviewed-on: https://chromium-review.googlesource.com/1069694
    > Commit-Queue: Penny MacNeil <pennymac@chromium.org>
    > Reviewed-by: Tom Sepez <tsepez@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#562467}
    
    Bug: 769590
    Change-Id: Ia801a05b04921a4147b980b2ab28b9c1060d27ca
    Reviewed-on: https://chromium-review.googlesource.com/1077347
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Penny MacNeil <pennymac@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562652}

--- chrome_elf/pe_image_safe/pe_image_safe.h
+++ chrome_elf/pe_image_safe/pe_image_safe.h
@@ -10,12 +10,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <windows.h>
 
+#include <limits>
+
 // https://msdn.microsoft.com/library/windows/desktop/ms684179.aspx
 #define LDR_IS_DATAFILE(handle) (((ULONG_PTR)(handle)) & (ULONG_PTR)1)
 
 namespace pe_image_safe {
 
 constexpr DWORD kPageSize = 4096;
+constexpr DWORD kImageSizeNotSet = std::numeric_limits<DWORD>::max();
 
 enum class ImageBitness { kUnknown, k32, k64 };
 
@@ -46,6 +49,11 @@ class PEImageSafe {
   // Some functions can only be used on images that have been memory mapped by
   // the NT Loader (e.g. LoadLibrary).  This constructor must be used to enable
   // that functionality.
+  // - Note: If the mapped image size is not known (e.g. via LoadLibrary), pass
+  //   kImageSizeNotSet for |buffer_size|.  The value will be mined from the PE
+  //   headers.
+  // - Note: Full load or LOAD_LIBRARY_AS_IMAGE_RESOURCE required.
+  // LOAD_LIBRARY_AS_DATAFILE* is not sufficient for some APIs.
   PEImageSafe(HMODULE buffer, DWORD buffer_size)
       : image_(buffer), image_size_(buffer_size) {
     ldr_image_mapping_ = !LDR_IS_DATAFILE(buffer);
