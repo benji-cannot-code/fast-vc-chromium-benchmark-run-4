b6b164bb0b9a2cc15fabe3b6152d9face10a97f3
Robbie McElrath <rmcelrath@chromium.org>
2018-05-29 21:29:33 +0000

    Migrate devicemotion layout tests to use mojo mocks
    This migrates all the layout tests in device_orientation/motion to use
    fake mojo services instead of testRunner.setMockDeviceMotion.
    
    Bug: 802815
    Change-Id: I2175bd9f463b4a9e0fc69fa83af3d2ff8fb821e2
    Reviewed-on: https://chromium-review.googlesource.com/1073920
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562595}

--- third_party/WebKit/LayoutTests/device_orientation/resources/device-orientation-helpers.js
+++ third_party/WebKit/LayoutTests/device_orientation/resources/device-orientation-helpers.js
@@ -1,6 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 'use strict';
 
+const MOTION_ROTATION_EPSILON = 1e-8;
+
 function assertTestRunner() {
   assert_true(window.testRunner instanceof Object,
     "This test can not be run without the window.testRunner.");
@@ -11,7 +13,7 @@ function generateMotionData(accelerationX, accelerationY, accelerationZ,
                             accelerationIncludingGravityY,
                             accelerationIncludingGravityZ,
                             rotationRateAlpha, rotationRateBeta, rotationRateGamma,
-                            interval) {
+                            interval = 16) {
   var motionData = {accelerationX: accelerationX,
                     accelerationY: accelerationY,
                     accelerationZ: accelerationZ,
@@ -69,6 +71,7 @@ function setMockOrientation(orientationData) {
 let nullToNan = x => (x === null ? NaN : x);
 
 function setMockMotionData(sensor, motionData) {
+  const degToRad = Math.PI / 180;
   return Promise.all([
       setMockSensorDataForType(sensor, device.mojom.SensorType.ACCELEROMETER, [
           nullToNan(motionData.accelerationIncludingGravityX),
@@ -81,9 +84,9 @@ function setMockMotionData(sensor, motionData) {
           nullToNan(motionData.accelerationZ),
       ]),
       setMockSensorDataForType(sensor, device.mojom.SensorType.GYROSCOPE, [
-          nullToNan(motionData.rotationRateAlpha),
-          nullToNan(motionData.rotationRateBeta),
-          nullToNan(motionData.rotationRateGamma),
+          nullToNan(motionData.rotationRateAlpha) * degToRad,
+          nullToNan(motionData.rotationRateBeta) * degToRad,
+          nullToNan(motionData.rotationRateGamma) * degToRad,
       ]),
   ]);
 }
@@ -108,9 +111,9 @@ function checkMotion(event, expectedMotionData) {
   assert_equals(event.accelerationIncludingGravity.y, expectedMotionData.accelerationIncludingGravityY, "accelerationIncludingGravity.y");
   assert_equals(event.accelerationIncludingGravity.z, expectedMotionData.accelerationIncludingGravityZ, "accelerationIncludingGravity.z");
 
-  assert_equals(event.rotationRate.alpha, expectedMotionData.rotationRateAlpha, "rotationRate.alpha");
-  assert_equals(event.rotationRate.beta, expectedMotionData.rotationRateBeta, "rotationRate.beta");
-  assert_equals(event.rotationRate.gamma, expectedMotionData.rotationRateGamma, "rotationRate.gamma");
+  assert_approx_equals(event.rotationRate.alpha, expectedMotionData.rotationRateAlpha, MOTION_ROTATION_EPSILON, "rotationRate.alpha");
+  assert_approx_equals(event.rotationRate.beta, expectedMotionData.rotationRateBeta, MOTION_ROTATION_EPSILON, "rotationRate.beta");
+  assert_approx_equals(event.rotationRate.gamma, expectedMotionData.rotationRateGamma, MOTION_ROTATION_EPSILON, "rotationRate.gamma");
 
   assert_equals(event.interval, expectedMotionData.interval, "interval");
 }
@@ -133,6 +136,29 @@ function waitForOrientation(expectedOrientationData, targetWindow = window) {
         beta: expectedOrientationData.beta,
         gamma: expectedOrientationData.gamma,
         absolute: expectedOrientationData.absolute,
+      }),
+      targetWindow);
+}
+
+function waitForMotion(expectedMotionData, targetWindow = window) {
+  return waitForEvent(
+      new DeviceMotionEvent('devicemotion', {
+        acceleration: {
+          x: expectedMotionData.accelerationX,
+          y: expectedMotionData.accelerationY,
+          z: expectedMotionData.accelerationZ,
+        },
+        accelerationIncludingGravity: {
+          x: expectedMotionData.accelerationIncludingGravityX,
+          y: expectedMotionData.accelerationIncludingGravityY,
+          z: expectedMotionData.accelerationIncludingGravityZ,
+        },
+        rotationRate: {
+          alpha: expectedMotionData.rotationRateAlpha,
+          beta: expectedMotionData.rotationRateBeta,
+          gamma: expectedMotionData.rotationRateGamma,
+        },
+        interval: expectedMotionData.interval,
       }),
       targetWindow);
 }
