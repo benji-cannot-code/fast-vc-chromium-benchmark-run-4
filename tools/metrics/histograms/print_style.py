1f043c95b4859ecb6f372f9dee757ca74db42ed6
Alexei Svitkine <asvitkine@chromium.org>
2018-05-29 17:19:14 +0000

    Fix ordering of obsolete tags and numbers in histogram suffixes.
    histograms.xml has been formatted with the new changes and is
    included in the CL.
    
    Bug: None
    Change-Id: I0884ba7035b48791c2ac5b0030d3e95105ef281f
    Reviewed-on: https://chromium-review.googlesource.com/1076375
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562477}

--- tools/metrics/histograms/print_style.py
+++ tools/metrics/histograms/print_style.py
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 """Holds the constants for pretty printing histograms.xml."""
 
 import os
+import re
 import sys
 
 # Import the metrics/common module for pretty print xml.
@@ -67,6 +68,15 @@ TAGS_THAT_ALLOW_SINGLE_LINE = ['summary', 'int', 'owner']
 
 LOWERCASE_NAME_FN = lambda n: n.attributes['name'].value.lower()
 
+
+def _NaturalSortByName(node):
+  """Sort by name, ordering numbers in the way humans expect."""
+  # See: https://blog.codinghorror.com/sorting-for-humans-natural-sort-order/
+  name = node.attributes['name'].value.lower()
+  convert = lambda text: int(text) if text.isdigit() else text
+  return [convert(c) for c in re.split('([0-9]+)', name)]
+
+
 # Tags whose children we want to alphabetize. The key is the parent tag name,
 # and the value is a list of pairs of tag name and key functions that maps each
 # child node to the desired sort key.
@@ -76,7 +86,8 @@ TAGS_ALPHABETIZATION_RULES = {
     'enum': [('int', lambda n: int(n.attributes['value'].value))],
     'histogram_suffixes_list': [('histogram_suffixes', LOWERCASE_NAME_FN)],
     'histogram_suffixes': [
-        ('suffix', LOWERCASE_NAME_FN),
+        ('obsolete', lambda n: None),
+        ('suffix', _NaturalSortByName),
         ('affected-histogram', LOWERCASE_NAME_FN),
     ],
 }
