3a9b9102ee2d5d695ec743e1936763f15a76dd36
Xiaohui Chen <xiaohuic@google.com>
2018-05-29 18:08:16 +0000

    Add libassistant resource provider
    Bug: b/77916286
    Test: locally build and test
    Change-Id: I5998c4250240361e1f0c0b69ad5b9f8ddcc9464d
    Reviewed-on: https://chromium-review.googlesource.com/1068260
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562506}

---
+++ chromeos/services/assistant/platform/resource_provider_impl.h
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_SERVICES_ASSISTANT_PLATFORM_RESOURCE_PROVIDER_IMPL_H_
+#define CHROMEOS_SERVICES_ASSISTANT_PLATFORM_RESOURCE_PROVIDER_IMPL_H_
+
+#include <string>
+#include <vector>
+
+#include "base/macros.h"
+#include "libassistant/shared/public/platform_resources.h"
+
+namespace chromeos {
+namespace assistant {
+
+class ResourceProviderImpl : public assistant_client::ResourceProvider {
+ public:
+  ResourceProviderImpl();
+  ~ResourceProviderImpl() override;
+
+  // assistant_client::ResourceProvider implementation:
+  bool GetResource(uint16_t resource_id, std::string* out) override;
+  std::vector<std::string> GetHotwordNameList() override;
+  std::string GetDefaultHotwordName() override;
+  bool GetHotwordData(const std::string& name, std::string* result) override;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(ResourceProviderImpl);
+};
+
+}  // namespace assistant
+}  // namespace chromeos
+
+#endif  // CHROMEOS_SERVICES_ASSISTANT_PLATFORM_RESOURCE_PROVIDER_IMPL_H_
