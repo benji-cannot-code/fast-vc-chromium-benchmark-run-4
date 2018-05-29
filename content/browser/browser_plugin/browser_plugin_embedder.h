e2ec6e27beaa72a91e9e47ec6e3c035aba0dedb7
Chris Hamilton <chrisha@chromium.org>
2018-05-29 16:23:53 +0000

    Fix audibility tracking for contents with guests.
    This fixes audibility tracking for contents with guests by having
    inner contents notify outer contents of their audio state changes,
    and having the outer contents track the aggregate state of all
    guests and itself.
    
    This is in preparation of removing WasRecentlyAudible from WebContents
    and moving that tracking to a UI tab helper.
    
    BUG=846374
    
    Change-Id: Ia4eea8994d09825748495de3df10eb4e77e03b79
    Reviewed-on: https://chromium-review.googlesource.com/1075553
    Commit-Queue: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562454}

--- content/browser/browser_plugin/browser_plugin_embedder.h
+++ content/browser/browser_plugin/browser_plugin_embedder.h
@@ -81,6 +81,10 @@ class CONTENT_EXPORT BrowserPluginEmbedder : public WebContentsObserver {
   BrowserPluginGuest* GetFullPageGuest();
 
   // Polls all guests for this web contents and returns true if any of them
+  // are currently audible.
+  bool AreAnyGuestsCurrentlyAudible();
+
+  // Polls all guests for this web contents and returns true if any of them
   // were audible recently.
   bool WereAnyGuestsRecentlyAudible();
 
@@ -99,6 +103,7 @@ class CONTENT_EXPORT BrowserPluginEmbedder : public WebContentsObserver {
   static bool UnlockMouseIfNecessaryCallback(bool* mouse_unlocked,
                                              WebContents* guest);
 
+  static bool GuestCurrentlyAudibleCallback(WebContents* guest);
   static bool GuestRecentlyAudibleCallback(WebContents* guest);
 
   // Message handlers.
