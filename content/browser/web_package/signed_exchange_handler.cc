2f84a5d8b63cace386b0fdf6e23176029e9b6b84
Kouhei Ueno <kouhei@chromium.org>
2018-05-29 03:09:31 +0000

    DCHECK SignedExchangeHandler::state_ transfer
    This CL adds DCHECKs for the state_ transfer inside SignedExchangeHandler::DidReadHeader,
    and adds comments where kouhei@ misunderstood the logic initially.
    
    Bug: 803774
    Change-Id: I9b65620a66e069a19424e030183826165e496ac7
    Reviewed-on: https://chromium-review.googlesource.com/1074949
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562315}

--- content/browser/web_package/signed_exchange_handler.cc
+++ content/browser/web_package/signed_exchange_handler.cc
@@ -139,6 +139,9 @@ void SignedExchangeHandler::DoHeaderLoop() {
 }
 
 void SignedExchangeHandler::DidReadHeader(bool completed_syncly, int result) {
+  DCHECK(state_ == State::kReadingHeadersLength ||
+         state_ == State::kReadingHeaders);
+
   TRACE_EVENT_BEGIN0(TRACE_DISABLED_BY_DEFAULT("loading"),
                      "SignedExchangeHandler::DidReadHeader");
   if (result < 0) {
@@ -161,20 +164,24 @@ void SignedExchangeHandler::DidReadHeader(bool completed_syncly, int result) {
   if (header_read_buf_->BytesRemaining() == 0) {
     switch (state_) {
       case State::kReadingHeadersLength:
-        if (!ParseHeadersLength())
+        if (!ParseHeadersLength()) {
           RunErrorCallback(net::ERR_INVALID_SIGNED_EXCHANGE);
+          return;
+        }
         break;
       case State::kReadingHeaders:
-        if (!ParseHeadersAndFetchCertificate())
+        if (!ParseHeadersAndFetchCertificate()) {
           RunErrorCallback(net::ERR_INVALID_SIGNED_EXCHANGE);
+          return;
+        }
         break;
       default:
         NOTREACHED();
     }
   }
 
-  if (state_ != State::kReadingHeadersLength &&
-      state_ != State::kReadingHeaders) {
+  // We have finished reading headers, so return without queueing the next read.
+  if (state_ == State::kFetchingCertificate) {
     TRACE_EVENT_END1(TRACE_DISABLED_BY_DEFAULT("loading"),
                      "SignedExchangeHandler::DidReadHeader", "state",
                      static_cast<int>(state_));
@@ -182,6 +189,8 @@ void SignedExchangeHandler::DidReadHeader(bool completed_syncly, int result) {
   }
 
   // Trigger the next read.
+  DCHECK(state_ == State::kReadingHeadersLength ||
+         state_ == State::kReadingHeaders);
   if (completed_syncly) {
     base::SequencedTaskRunnerHandle::Get()->PostTask(
         FROM_HERE, base::BindOnce(&SignedExchangeHandler::DoHeaderLoop,
