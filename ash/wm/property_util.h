d8c7daf25d42640f1182d4d6d21b1912f6c8ac89
Evan Stade <estade@chromium.org>
2018-05-29 18:10:35 +0000

    Mash: get rid of redundant frame color init properties.
    Just use ash::kFrameActiveColorKey/ash::kFrameInactiveColorKey.
    HeaderView already listens to changes in these properties and
    updates the header painter appropriately.
    
    Test: files app, settings app, and chrome web store apps all have
    correct frame colors with --enable-features=Mash
    
    Bug: 836992
    Change-Id: I3dd4636ea0654b1b66c95a300b345facb9b76927
    Reviewed-on: https://chromium-review.googlesource.com/1072426
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562507}

--- ash/wm/property_util.h
+++ ash/wm/property_util.h
@@ -12,9 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-#include "base/optional.h"
-#include "third_party/skia/include/core/SkColor.h"
-
 namespace aura {
 class PropertyConverter;
 class Window;
@@ -62,9 +59,6 @@ bool GetInitialBounds(const InitProperties& properties, gfx::Rect* bounds);
 bool GetWindowPreferredSize(const InitProperties& properties, gfx::Size* size);
 
 bool ShouldRemoveStandardFrame(const InitProperties& properties);
-
-base::Optional<SkColor> GetFrameColor(const InitProperties& properties,
-                                      bool active);
 
 bool ShouldEnableImmersive(const InitProperties& properties);
 
