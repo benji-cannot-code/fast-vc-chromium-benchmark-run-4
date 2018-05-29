0c6f303df3a753bef64e4638a33dd64f956ee41e
wutao <wutao@chromium.org>
2018-05-29 22:22:29 +0000

    LogoView: Fix setting alpha
    This cl fixes a bug to set logo view alpha value.
    
    Bug: b/80430044, 847609
    Test: manual.
    Change-Id: I25a6e3d64d062f44da48154af42caf3431e14904
    Reviewed-on: https://chromium-review.googlesource.com/1077267
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562632}

--- ash/assistant/ui/logo_view/logo_view.cc
+++ ash/assistant/ui/logo_view/logo_view.cc
@@ -25,6 +25,10 @@ int64_t TimeTicksToMs(const base::TimeTicks& timestamp) {
   return (timestamp - base::TimeTicks()).InMilliseconds();
 }
 
+int32_t GetLogoAlpha(const LogoView::Logo& logo) {
+  return logo.GetAlpha() * 255;
+}
+
 }  // namespace
 
 LogoView::LogoView()
@@ -94,8 +98,6 @@ void LogoView::DrawDots(gfx::Canvas* canvas) {
   // we need to find out why the Mic parts are overlapping in the first place.
   for (auto iter = logo_.dots().rbegin(); iter != logo_.dots().rend(); ++iter)
     DrawDot(canvas, (*iter).get());
-
-  layer()->SetOpacity(logo_.GetAlpha());
 }
 
 void LogoView::DrawDot(gfx::Canvas* canvas, Dot* dot) {
@@ -128,6 +130,7 @@ void LogoView::DrawShape(gfx::Canvas* canvas, Shape* shape, SkColor color) {
   cc::PaintFlags paint_flags;
   paint_flags.setAntiAlias(true);
   paint_flags.setColor(color);
+  paint_flags.setAlpha(GetLogoAlpha(logo_));
   paint_flags.setStyle(cc::PaintFlags::kStroke_Style);
   paint_flags.setStrokeCap(shape->cap());
 
@@ -143,6 +146,7 @@ void LogoView::DrawLine(gfx::Canvas* canvas, Dot* dot, float x, float y) {
   cc::PaintFlags paint_flags;
   paint_flags.setAntiAlias(true);
   paint_flags.setColor(dot->color());
+  paint_flags.setAlpha(GetLogoAlpha(logo_));
   paint_flags.setStrokeWidth(stroke_width);
   paint_flags.setStyle(cc::PaintFlags::kStroke_Style);
   paint_flags.setStrokeCap(cc::PaintFlags::kRound_Cap);
@@ -160,6 +164,7 @@ void LogoView::DrawCircle(gfx::Canvas* canvas, Dot* dot, float x, float y) {
   cc::PaintFlags paint_flags;
   paint_flags.setAntiAlias(true);
   paint_flags.setColor(dot->color());
+  paint_flags.setAlpha(GetLogoAlpha(logo_));
   paint_flags.setStyle(cc::PaintFlags::kFill_Style);
   canvas->DrawCircle(gfx::PointF(x * dots_scale_, y * dots_scale_),
                      radius * dots_scale_, paint_flags);
