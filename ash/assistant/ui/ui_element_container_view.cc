8bc1ece4d8e738c73d62340451b6c9988a3bc978
David Black <dmblack@google.com>
2018-05-29 21:28:34 +0000

    Impose minimum height restrictions.
    UX spec dictates that if not in stylus mode:
    - Height should be >= 200dip.
    - Height should grow to match contents.
    - Height should not shrink until Assistant UI is closed.
    
    Note that now UiElementContainerView needs to always be visible so
    that it can fill the remaining space, even when empty of children.
    
    Demo in bug.
    
    Bug: b:79432250
    Change-Id: I32372467bb8fff2b74885014b34152ce907be1bf
    Reviewed-on: https://chromium-review.googlesource.com/1074373
    Commit-Queue: David Black <dmblack@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562594}

--- ash/assistant/ui/ui_element_container_view.cc
+++ ash/assistant/ui/ui_element_container_view.cc
@@ -108,7 +108,6 @@ void UiElementContainerView::OnUiElementsCleared() {
   // Prevent any in-flight card rendering requests from returning.
   render_request_weak_factory_.InvalidateWeakPtrs();
 
-  SetVisible(false);
   RemoveAllChildViews(/*delete_children=*/true);
   PreferredSizeChanged();
 
@@ -161,7 +160,7 @@ void UiElementContainerView::OnCardReady(
   }
   // TODO(dmblack): Handle Mash case.
 
-  SetVisible(true);
+  PreferredSizeChanged();
 
   // Once the card has been rendered and embedded, we can resume processing
   // any UI elements that are in the pending queue.
@@ -192,7 +191,6 @@ void UiElementContainerView::OnTextElementAdded(
   text_container->AddChildView(text_view);
   AddChildView(text_container);
 
-  SetVisible(true);
   PreferredSizeChanged();
 }
 
