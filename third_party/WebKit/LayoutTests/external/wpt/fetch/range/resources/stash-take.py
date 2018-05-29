5c83dc48a72364bf6cc1dafe1792d962a829e04c
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-05-29 21:46:43 +0000

    Import wpt@e4b0f7a069606efdc0d633a94ae19b71b6dcd748
    Using wpt-import in Chromium 57b6b5db93e13247c7496f69a7f8760662d822e2.
    With Chromium commits locally applied on WPT:
    d78c1e54e7 "[Resource-Timing] Fix `initiatorType` values"
    6534acd9b9 "[css-grid] Baseline alignment inside the tracks sizing algorithm"
    
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/18904
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    yhirano@chromium.org, mkwst@chromium.org, japhet@chromium.org:
      external/wpt/fetch
    
    TBR=raphael.kubo.da.costa@intel.com
    
    No-Export: true
    Change-Id: I5006332f24cac497f9437dca1f1ecd374526cca0
    Reviewed-on: https://chromium-review.googlesource.com/1077089
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562605}

---
+++ third_party/WebKit/LayoutTests/external/wpt/fetch/range/resources/stash-take.py
@@ -0,0 +1,8 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+from wptserve.handlers import json_handler
+
+
+@json_handler
+def main(request, response):
+    key = request.GET.first("key")
+    return request.server.stash.take(key, '/fetch/range/')
--- third_party/WebKit/LayoutTests/external/wpt/service-workers/service-worker/resources/test-helpers.sub.js
+++ third_party/WebKit/LayoutTests/external/wpt/service-workers/service-worker/resources/test-helpers.sub.js
@@ -47,9 +47,14 @@ function unreached_rejection(test, prefix) {
     });
 }
 
-// Adds an iframe to the document and returns a promise that resolves to the
-// iframe when it finishes loading. The caller is responsible for removing the
-// iframe later if needed.
+/**
+ * Adds an iframe to the document and returns a promise that resolves to the
+ * iframe when it finishes loading. The caller is responsible for removing the
+ * iframe later if needed.
+ *
+ * @param {string} url
+ * @returns {HTMLIFrameElement}
+ */
 function with_iframe(url) {
   return new Promise(function(resolve) {
       var frame = document.createElement('iframe');
