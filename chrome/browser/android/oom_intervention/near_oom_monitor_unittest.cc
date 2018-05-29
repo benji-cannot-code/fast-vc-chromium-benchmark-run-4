86b42b82722c1ee997651dfe2730937272de199b
Siddhartha <ssid@chromium.org>
2018-05-29 07:37:14 +0000

    Reland "Reland "Use thresholds based on RAM size for OOM intervention""
    This reverts commit a21bf6f95c3698b9542d8aa3a4ed081e2771d7fd.
    
    Reason for revert: Test field trial config does not support low_end_device flag.
    So, make sure the config does not turn on intervention on high end devices
    without new param.
    
    Original change's description:
    > Revert "Reland "Use thresholds based on RAM size for OOM intervention""
    >
    > This reverts commit 9191004f57d2bbb925d1d48194036bb1230d0f1f.
    >
    > Reason for revert: The failure after the revert was a different failure.
    > Sorry for the reland.
    >
    > Original change's description:
    > > Reland "Use thresholds based on RAM size for OOM intervention"
    > >
    > > This reverts commit 50257b9eafc8ec098a9693d4740fd761ace59c35.
    > >
    > > Reason for revert: This was not the cause of failure, see
    > > https://crbug.com/845763
    > >
    > > Original change's description:
    > > > Revert "Use thresholds based on RAM size for OOM intervention"
    > > >
    > > > This reverts commit 0c2124b3c9acf283315fb56beea340d050db0acd.
    > > >
    > > > Reason for revert: Speculatively reverting to see if it fixes webgl conformance test timeouts
    > > > Bug: 845411
    > > >
    > > > Original change's description:
    > > > > Use thresholds based on RAM size for OOM intervention
    > > > >
    > > > > This CL does not change behavior of existing experiment on 512 devices.
    > > > > 1. Remove low-end device checks while enabling intervention. This can be
    > > > >    filtered in Finch config.
    > > > > 2. Set threshold based on percentage of RAM instead of absolute number.
    > > > > 3. Disable intervention if device has more than 512MB RAM and threshold
    > > > >    was set to absolute number.
    > > > >
    > > > > BUG=843419
    > > > >
    > > > >
    > > > > Change-Id: Ib40a038d9d2f111a514b3f41ce7f32a3ea3a156d
    > > > > Reviewed-on: https://chromium-review.googlesource.com/1066804
    > > > > Commit-Queue: Siddhartha S <ssid@chromium.org>
    > > > > Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    > > > > Cr-Commit-Position: refs/heads/master@{#560430}
    > > >
    > > > TBR=bashi@chromium.org,ssid@chromium.org
    > > >
    > > > Change-Id: I4b50e522dcc6c91542927a9e769e5eee24f486a5
    > > > No-Presubmit: true
    > > > No-Tree-Checks: true
    > > > No-Try: true
    > > > Bug: 843419
    > > > Reviewed-on: https://chromium-review.googlesource.com/1069567
    > > > Reviewed-by: enne <enne@chromium.org>
    > > > Commit-Queue: enne <enne@chromium.org>
    > > > Cr-Commit-Position: refs/heads/master@{#560761}
    > >
    > > TBR=bashi@chromium.org,enne@chromium.org,ssid@chromium.org
    > >
    > > Change-Id: I4d0a08cd44772dc690ac1d93b1d92b4934ed0373
    > > No-Presubmit: true
    > > No-Tree-Checks: true
    > > No-Try: true
    > > Bug: 845411, 843419
    > > Reviewed-on: https://chromium-review.googlesource.com/1069888
    > > Reviewed-by: Siddhartha S <ssid@chromium.org>
    > > Commit-Queue: Siddhartha S <ssid@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#560882}
    >
    > TBR=bashi@chromium.org,enne@chromium.org,ssid@chromium.org
    >
    > Change-Id: Ib8841dddaa2a167e674c997ec1577036df26e8cb
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 845411, 843419
    > Reviewed-on: https://chromium-review.googlesource.com/1069876
    > Reviewed-by: Siddhartha S <ssid@chromium.org>
    > Commit-Queue: Siddhartha S <ssid@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#560889}
    
    TBR=enne@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 845411, 843419
    Change-Id: I5c03b1ac806a5f4e0b1516761e3b86c242a3c200
    Reviewed-on: https://chromium-review.googlesource.com/1074151
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562362}

--- chrome/browser/android/oom_intervention/near_oom_monitor_unittest.cc
+++ chrome/browser/android/oom_intervention/near_oom_monitor_unittest.cc
@@ -13,13 +13,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 const int64_t kTestSwapTotalKB = 128 * 1024;
 const int64_t kTestSwapFreeThreshold = kTestSwapTotalKB / 4;
+const uint64_t kRendererWorkloadThreshold = 10 * 1024;
 }  // namespace
 
 class MockNearOomMonitor : public NearOomMonitor {
  public:
   explicit MockNearOomMonitor(
       scoped_refptr<base::SequencedTaskRunner> task_runner)
-      : NearOomMonitor(task_runner, kTestSwapFreeThreshold) {
+      : NearOomMonitor(task_runner,
+                       kTestSwapFreeThreshold,
+                       kRendererWorkloadThreshold) {
     // Start with 128MB swap total and 64MB swap free.
     memory_info_.swap_total = kTestSwapTotalKB;
     memory_info_.swap_free = kTestSwapTotalKB / 2;
