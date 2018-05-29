7f1dfaeb8c64321059d74439f67fed26c961bd39
Steve Kobes <skobes@chromium.org>
2018-05-29 16:19:11 +0000

    Restore LayoutView::LocalVisualRectIgnoringVisibility.
    This was removed in http://crrev.com/560172, but turns out to be
    necessary.
    
    Bug: 847220
    Change-Id: I311e35d8e5dca32e3a42f63c9389d52b4cdee7c0
    Reviewed-on: https://chromium-review.googlesource.com/1076387
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562452}

--- third_party/blink/renderer/core/layout/layout_view.cc
+++ third_party/blink/renderer/core/layout/layout_view.cc
@@ -323,6 +323,12 @@ void LayoutView::UpdateLayout() {
   ClearNeedsLayout();
 }
 
+LayoutRect LayoutView::LocalVisualRectIgnoringVisibility() const {
+  LayoutRect rect = VisualOverflowRect();
+  rect.Unite(LayoutRect(rect.Location(), ViewRect().Size()));
+  return rect;
+}
+
 void LayoutView::MapLocalToAncestor(const LayoutBoxModelObject* ancestor,
                                     TransformState& transform_state,
                                     MapCoordinatesFlags mode) const {
