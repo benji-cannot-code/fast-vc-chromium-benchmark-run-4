ca5dcfa8e982fe9f12bed2c882e0dae5e6661d44
Josh Horwich <jhorwich@chromium.org>
2018-05-29 23:41:07 +0000

    arc: Record empty ToS content for managed users.
    This change makes the in-product and new-user flows consistent for
    recording PLAY_STORE consent info via consent_auditor - both cases
    will now record the event with a zero-length ToS content as the
    Play ToS is not shown to managed users.
    
    Bug: b:79685367
    Test: Opt in on device with managed/unmanaged user, run unit_tests
    Change-Id: Ia80fbfd659dfbc10cde68951d7673f0c4012188b
    Reviewed-on: https://chromium-review.googlesource.com/1070667
    Commit-Queue: Josh Horwich <jhorwich@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562655}

--- chrome/browser/ui/webui/chromeos/login/arc_terms_of_service_screen_handler.h
+++ chrome/browser/ui/webui/chromeos/login/arc_terms_of_service_screen_handler.h
@@ -18,8 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/network/network_state_handler_observer.h"
 #include "chromeos/settings/timezone_settings.h"
 
-class Profile;
-
 namespace arc {
 class ArcOptInPreferenceHandler;
 }
@@ -87,9 +85,6 @@ class ArcTermsOfServiceScreenHandler
                       bool record_location_consent,
                       bool location_accepted);
 
-  // Sends if Arc enable status is manged to screen.
-  void SendArcManagedStatus(Profile* profile);
-
   bool NeedDispatchEventOnAction();
 
   // arc::ArcOptInPreferenceHandlerObserver:
@@ -107,6 +102,9 @@ class ArcTermsOfServiceScreenHandler
 
   // To filter out duplicate notifications from html.
   bool action_taken_ = false;
+
+  // To track if ARC preference is managed.
+  bool arc_managed_ = false;
 
   // To track if optional features are managed preferences.
   bool backup_restore_managed_ = false;
