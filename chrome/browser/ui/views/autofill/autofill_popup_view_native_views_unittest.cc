f7699a2bca736ca41aae84f29903c2c29d286d8d
Tommy Martino <tmartino@chromium.org>
2018-05-30 19:47:33 +0000

    [Autofill Views] Compensating for rounded corners
    The last footer row needs to include extra height/padding at the bottom
    equal to the corner radius, to compensate visually for the rounding.
    Currently we do include some padding at the bottom of the footer, but
    it's outside the row, so we get some weird effects when we hover over
    that row.
    
    Screenshots (Googlers only):
    https://docs.google.com/presentation/d/18jvoRnneYgYhKYCdf5u1ZN1rMrDPC129rfAej2HxJAk/edit?usp=sharing
    
    Change-Id: I1e3541041f86a89fa8b581755d5588a16b5345c9
    Bug: 847250
    Reviewed-on: https://chromium-review.googlesource.com/1075516
    Commit-Queue: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562963}

--- chrome/browser/ui/views/autofill/autofill_popup_view_native_views_unittest.cc
+++ chrome/browser/ui/views/autofill/autofill_popup_view_native_views_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/autofill/autofill_popup_controller.h"
 #include "chrome/browser/ui/autofill/autofill_popup_layout_model.h"
 #include "chrome/browser/ui/views/autofill/autofill_popup_view_native_views.h"
+#include "chrome/browser/ui/views/harmony/chrome_layout_provider.h"
 #include "components/autofill/core/browser/popup_item_ids.h"
 #include "components/autofill/core/browser/suggestion.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -120,6 +121,11 @@ class AutofillPopupViewNativeViewsTest : public views::ViewsTestBase {
   void SetUp() override {
     views::ViewsTestBase::SetUp();
 
+    // The layout provider is meant to be a singleton, but it is not initialized
+    // for unit tests. Constructing one here makes it globally available, which
+    // is later used by the view during initialization.
+    layout_provider_ = std::make_unique<ChromeLayoutProvider>();
+
     CreateWidget();
     generator_.reset(new ui::test::EventGenerator(widget_.GetNativeWindow()));
   }
@@ -158,6 +164,7 @@ class AutofillPopupViewNativeViewsTest : public views::ViewsTestBase {
   std::unique_ptr<ui::test::EventGenerator> generator_;
 
  private:
+  std::unique_ptr<ChromeLayoutProvider> layout_provider_;
   DISALLOW_COPY_AND_ASSIGN(AutofillPopupViewNativeViewsTest);
 };
 
