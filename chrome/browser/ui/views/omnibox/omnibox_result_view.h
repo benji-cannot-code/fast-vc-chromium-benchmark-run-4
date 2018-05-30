53f0fa6e9719627718c26b05e41a5c150c07cb30
Kevin Bailey <krb@chromium.org>
2018-05-30 18:40:38 +0000

    [omnibox] Animate tab switch button size changes
    The tab switch button only exists at quantized sizes, depending on
    window width. Currently it jumps between those sizes. We would prefer
    that it animate between them. This CL adds the beginning of animation,
    putting off issues like delaying animation until window shrinking
    stops, etc.
    
    The CL also changes the ellision type to fade.
    
    Bug: 780835
    Change-Id: Ic4a9997e213bae5415d73a7f83b410b788a95279
    Reviewed-on: https://chromium-review.googlesource.com/1067667
    Commit-Queue: Kevin Bailey <krb@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562928}

--- chrome/browser/ui/views/omnibox/omnibox_result_view.h
+++ chrome/browser/ui/views/omnibox/omnibox_result_view.h
@@ -72,6 +72,7 @@ class OmniboxResultView : public views::View,
   void ButtonPressed(views::Button* sender, const ui::Event& event) override;
 
   // views::View:
+  void Layout() override;
   bool OnMousePressed(const ui::MouseEvent& event) override;
   bool OnMouseDragged(const ui::MouseEvent& event) override;
   void OnMouseReleased(const ui::MouseEvent& event) override;
@@ -97,7 +98,6 @@ class OmniboxResultView : public views::View,
   void OpenMatch(WindowOpenDisposition disposition);
 
   // views::View:
-  void Layout() override;
   const char* GetClassName() const override;
   void OnBoundsChanged(const gfx::Rect& previous_bounds) override;
 
