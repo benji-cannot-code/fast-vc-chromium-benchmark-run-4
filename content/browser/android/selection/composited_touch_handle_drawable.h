1aea9b4d9f25cb0cc14d615749371d71b173bba6
Jinsuk Kim <jinsukkim@chromium.org>
2018-05-30 23:53:21 +0000

    Android: Moves HandleViewResources to ui/resources
    Moves HandleViewResources to ui/resources.
    Also removes the DEPS exception rule that becomes unnecessary.
    
    Bug: 617324
    Change-Id: Ideb9b2d858ffe8a9f5ea7f345c7a9cdd3a9c38cd
    Reviewed-on: https://chromium-review.googlesource.com/1071148
    Reviewed-by: Mohsen Izadi <mohsen@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563072}

--- content/browser/android/selection/composited_touch_handle_drawable.h
+++ content/browser/android/selection/composited_touch_handle_drawable.h
@@ -6,12 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_BROWSER_ANDROID_SELECTION_COMPOSITED_TOUCH_HANDLE_DRAWABLE_H_
 #define CONTENT_BROWSER_ANDROID_SELECTION_COMPOSITED_TOUCH_HANDLE_DRAWABLE_H_
 
-#include "ui/touch_selection/touch_handle.h"
-
-#include "base/android/jni_android.h"
 #include "base/android/scoped_java_ref.h"
 #include "base/macros.h"
-#include "cc/layers/ui_resource_layer.h"
+#include "base/memory/scoped_refptr.h"
+#include "ui/touch_selection/touch_handle.h"
+
+namespace cc {
+class Layer;
+class UIResourceLayer;
+}  // namespace cc
 
 namespace content {
 
