3d27a6b696336d5b6954553c070095e21d71e132
Karan Bhatia <karandeepb@chromium.org>
2018-05-30 23:09:17 +0000

    Declarative Web Request: Only register default web request rules registries if needed.
    Currently we create default web request rules registries for the declarative web
    request API even if the API is not available to the current environment. This is
    wasteful and detrimental to performance, since the initial registry load blocks
    the first network request (tracked via Extensions.NetworkDelayRegistryLoad UMA).
    
    This CL changes RulesRegistryService so that the default WebRequestRulesRegistry
    is registered only if the declarative web request API is avaialble to the
    current environment. This, for example, means that no default web request rule
    registries would be created on the stable channel. Rules registries
    corresponding to webviews and the default content rules registry would still be
    created.
    
    This also helps fix issue 777717 and renderer cache is not cleared redundantly
    on each extension load/unload/uninstall when the API is not available.
    
    BUG=693243, 777717
    
    Change-Id: I7384fed71a86aea3f5cc8d2eafd1445b439dd76a
    Reviewed-on: https://chromium-review.googlesource.com/1072497
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563051}

--- extensions/browser/api/declarative/rules_registry_service.cc
+++ extensions/browser/api/declarative/rules_registry_service.cc
@@ -21,6 +21,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/api/web_request/web_request_api.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/common/extension.h"
+#include "extensions/common/features/feature.h"
+#include "extensions/common/features/feature_provider.h"
 
 namespace extensions {
 
@@ -195,9 +197,17 @@ void RulesRegistryService::EnsureDefaultRulesRegistriesRegistered() {
       RulesRegistryKey(declarative_webrequest_constants::kOnRequest,
                        kDefaultRulesRegistryID)));
 
-  // Persist the cache since it pertains to regular pages (i.e. not webviews).
-  RegisterWebRequestRulesRegistry(kDefaultRulesRegistryID,
-                                  RulesCacheDelegate::Type::kPersistent);
+  // Only register the default web request rules registry if the
+  // declarativeWebRequest API is enabled. See crbug.com/693243.
+  const bool is_api_enabled =
+      FeatureProvider::GetAPIFeature("declarativeWebRequest")
+          ->IsAvailableToEnvironment()
+          .is_available();
+  if (is_api_enabled) {
+    // Persist the cache since it pertains to regular pages (i.e. not webviews).
+    RegisterWebRequestRulesRegistry(kDefaultRulesRegistryID,
+                                    RulesCacheDelegate::Type::kPersistent);
+  }
 
   // Create the ContentRulesRegistry.
   DCHECK(!content_rules_registry_);
