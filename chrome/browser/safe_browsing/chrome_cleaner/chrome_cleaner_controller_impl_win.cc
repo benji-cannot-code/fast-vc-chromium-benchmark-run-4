d0eb7cb73b50b67dcc349accf27064e869e39feb
Roger Tawa <rogerta@chromium.org>
2018-05-30 17:22:38 +0000

    Disable logs checkbox in ChromeCleanerDialog when disabled by policy.
    Bug: 834939
    Change-Id: I1951b6face63def33f0ec05162ff512477f7d4a0
    Reviewed-on: https://chromium-review.googlesource.com/1075718
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@chromium.org>
    Commit-Queue: Roger Tawa <rogerta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562886}

--- chrome/browser/safe_browsing/chrome_cleaner/chrome_cleaner_controller_impl_win.cc
+++ chrome/browser/safe_browsing/chrome_cleaner/chrome_cleaner_controller_impl_win.cc
@@ -39,6 +39,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/installer/util/scoped_token_privilege.h"
 #include "components/chrome_cleaner/public/constants/constants.h"
 #include "components/component_updater/component_updater_service.h"
+#include "components/component_updater/pref_names.h"
+#include "components/prefs/pref_service.h"
 #include "components/safe_browsing/common/safe_browsing_prefs.h"
 #include "content/public/browser/browser_thread.h"
 #include "net/http/http_status_code.h"
@@ -532,6 +534,15 @@ bool ChromeCleanerControllerImpl::IsAllowedByPolicy() {
 
 bool ChromeCleanerControllerImpl::IsReportingAllowedByPolicy() {
   return safe_browsing::SwReporterReportingIsAllowedByPolicy();
+}
+
+bool ChromeCleanerControllerImpl::IsReportingManagedByPolicy() {
+  // Logs are considered managed if the logs themselves are managed or if the
+  // entire cleanup feature is disabled by policy.
+  PrefService* local_state = g_browser_process->local_state();
+  return !IsAllowedByPolicy() ||
+         (local_state &&
+          local_state->IsManagedPreference(prefs::kSwReporterReportingEnabled));
 }
 
 ChromeCleanerControllerImpl::ChromeCleanerControllerImpl()
