990a85c4b0c8cb9c0def6ed9c223bf73288c3917
Scott Violet <sky@chromium.org>
2018-05-30 23:12:16 +0000

    chromeos: makes intercepts events work with capture
    If an embedding is created such that the embedder intercepts events, then the
    embedder should get events regardless of capture.
    
    BUG=837692,837696
    TEST=covered by test
    
    Change-Id: I8ed3e30ff7973d319bb5ac91323382c1743818c1
    Reviewed-on: https://chromium-review.googlesource.com/1080092
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563054}

--- services/ui/ws2/client_window.cc
+++ services/ui/ws2/client_window.cc
@@ -145,10 +145,7 @@ class ClientWindowEventHandler : public ui::EventHandler {
       return;
     }
 
-    if (HandleInterceptedKeyEvent(event))
-      return;
-
-    if (ShouldIgnoreEvent(*event))
+    if (HandleInterceptedEvent(event) || ShouldIgnoreEvent(*event))
       return;
 
     auto* owning = client_window_->owning_window_service_client();
@@ -185,10 +182,14 @@ class ClientWindowEventHandler : public ui::EventHandler {
       // window() is the the target.
       return true;
     }
+    return ShouldIgnoreEventType(event.type());
+  }
+
+  bool ShouldIgnoreEventType(EventType type) const {
     // WindowTreeClient takes care of sending ET_MOUSE_CAPTURE_CHANGED at the
     // right point. The enter events are effectively synthetic, and indirectly
     // generated in the client as the result of a move event.
-    switch (event.type()) {
+    switch (type) {
       case ET_MOUSE_CAPTURE_CHANGED:
       case ET_MOUSE_ENTERED:
       case ET_POINTER_CAPTURE_CHANGED:
@@ -200,16 +201,16 @@ class ClientWindowEventHandler : public ui::EventHandler {
     return false;
   }
 
- private:
-  // If |event| is a KeyEvent that needs to be handled because a client
-  // intercepts events, then true is returned and the event is handled.
-  // Otherwise returns false.
-  bool HandleInterceptedKeyEvent(Event* event) {
-    if (!event->IsKeyEvent())
+  // If |window| identifies an embedding and the owning client intercepts
+  // events, this forwards to the owner and returns true. Otherwise returns
+  // false.
+  bool HandleInterceptedEvent(Event* event) {
+    if (ShouldIgnoreEventType(event->type()))
       return false;
 
-    // KeyEvents do not go through through ClientWindowTargeter. As a result
-    // ClientWindowEventHandler has to check for a client intercepting events.
+    // KeyEvents, and events when there is capture, do not go through through
+    // ClientWindowTargeter. As a result ClientWindowEventHandler has to check
+    // for a client intercepting events.
     if (client_window_->DoesOwnerInterceptEvents()) {
       client_window_->owning_window_service_client()->SendEventToClient(
           window(), *event);
@@ -219,6 +220,7 @@ class ClientWindowEventHandler : public ui::EventHandler {
     return false;
   }
 
+ private:
   ClientWindow* const client_window_;
 
   DISALLOW_COPY_AND_ASSIGN(ClientWindowEventHandler);
@@ -304,6 +306,9 @@ class TopLevelEventHandler : public ClientWindowEventHandler {
       // continue. Early out to avoid sending the event to the client again.
       return;
     }
+
+    if (HandleInterceptedEvent(event))
+      return;
 
     if (!event->IsLocatedEvent()) {
       ClientWindowEventHandler::OnEvent(event);
