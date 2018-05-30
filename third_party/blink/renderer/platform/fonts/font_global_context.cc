78513303230b89a2b77b71f4ed53e525a8bcb3f6
Dmitry Gozman <dgozman@chromium.org>
2018-05-30 19:38:32 +0000

    Move locale-dependent bits to LayoutLocale
    This removes AcceptLanguagesResolver, which was delegating most of the work
    to FontGlobalContext anyway, and also simplifies FontGlobalContext
    a fair amount by introducing LayoutLocale::PerThreadData.
    
    Bug: 847934
    
    Change-Id: I10f8ccc682d4cdc77189fb6738872636ecd5425e
    Reviewed-on: https://chromium-review.googlesource.com/1074828
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562959}

--- third_party/blink/renderer/platform/fonts/font_global_context.cc
+++ third_party/blink/renderer/platform/fonts/font_global_context.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/fonts/font_global_context.h"
 
 #include "third_party/blink/renderer/platform/fonts/font_cache.h"
-#include "third_party/blink/renderer/platform/wtf/std_lib_extras.h"
 #include "third_party/blink/renderer/platform/wtf/thread_specific.h"
 
 namespace blink {
@@ -20,12 +19,7 @@ FontGlobalContext* FontGlobalContext::Get(CreateIfNeeded create_if_needed) {
   return *font_persistent;
 }
 
-FontGlobalContext::FontGlobalContext()
-    : harfbuzz_font_funcs_(nullptr),
-      default_locale_(nullptr),
-      system_locale_(nullptr),
-      default_locale_for_han_(nullptr),
-      has_default_locale_for_han_(false) {}
+FontGlobalContext::FontGlobalContext() : harfbuzz_font_funcs_(nullptr) {}
 
 void FontGlobalContext::ClearMemory() {
   if (!Get(kDoNotCreate))
@@ -36,11 +30,7 @@ void FontGlobalContext::ClearMemory() {
 
 void FontGlobalContext::ClearForTesting() {
   FontGlobalContext* ctx = Get();
-  ctx->default_locale_ = nullptr;
-  ctx->system_locale_ = nullptr;
-  ctx->default_locale_for_han_ = nullptr;
-  ctx->has_default_locale_for_han_ = false;
-  ctx->layout_locale_map_.clear();
+  ctx->layout_locale_data_ = LayoutLocale::PerThreadData();
   ctx->font_cache_.Invalidate();
 }
 
