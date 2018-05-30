f08e1a32a292419b15b8e646afcb5f8f60184869
Nate Chapin <japhet@chromium.org>
2018-05-30 06:35:22 +0000

    Revert "Revive WorkerThreadableLoader for sync XHR again"
    This reverts commit b1e172c8d0e85286f9025f291c3fc3e719bc30ff.
    
    Bug: 706331
    Change-Id: I6d9ab8095e18feb76ddb1f395bbf665186b97617
    Reviewed-on: https://chromium-review.googlesource.com/1077372
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562755}

--- third_party/blink/renderer/core/loader/threadable_loader.cc
+++ third_party/blink/renderer/core/loader/threadable_loader.cc
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/loader/document_threadable_loader.h"
 #include "third_party/blink/renderer/core/loader/threadable_loading_context.h"
-#include "third_party/blink/renderer/core/loader/worker_threadable_loader.h"
 #include "third_party/blink/renderer/core/workers/worker_global_scope.h"
 
 namespace blink {
@@ -58,12 +57,6 @@ void ThreadableLoader::LoadResourceSynchronously(
     ThreadableLoaderClient& client,
     const ThreadableLoaderOptions& options,
     const ResourceLoaderOptions& resource_loader_options) {
-  if (context.IsWorkerGlobalScope()) {
-    WorkerThreadableLoader::LoadResourceSynchronously(
-        ToWorkerGlobalScope(context), request, client, options,
-        resource_loader_options);
-    return;
-  }
   DocumentThreadableLoader::LoadResourceSynchronously(
       *ThreadableLoadingContext::Create(context), request, client, options,
       resource_loader_options);
