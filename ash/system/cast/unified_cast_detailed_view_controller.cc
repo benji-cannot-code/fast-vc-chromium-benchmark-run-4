d74e7f1f6bac403453d8971737ff66ca1d04a2a4
Tetsui Ohkubo <tetsui@chromium.org>
2018-05-30 02:04:34 +0000

    Add UnifiedCastDetailedViewController.
    This CL implements DetailedViewController for the cast detailed view.
    It fixes the behavior of the back button, and enables further
    UnifiedSystemTray specific tweaks to the detailed view.
    
    Design doc: go/cros-qs-restyling
    
    TEST=manual
    BUG=835733
    
    Change-Id: I94777e170ba0c4b3ada72bcf2cb3cd2ea335a7de
    Reviewed-on: https://chromium-review.googlesource.com/1073210
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562691}

---
+++ ash/system/cast/unified_cast_detailed_view_controller.cc
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/system/cast/unified_cast_detailed_view_controller.h"
+
+#include "ash/shell.h"
+#include "ash/system/cast/tray_cast.h"
+#include "ash/system/unified/unified_detailed_view_delegate.h"
+
+namespace ash {
+
+UnifiedCastDetailedViewController::UnifiedCastDetailedViewController(
+    UnifiedSystemTrayController* tray_controller)
+    : detailed_view_delegate_(
+          std::make_unique<UnifiedDetailedViewDelegate>(tray_controller)) {}
+
+UnifiedCastDetailedViewController::~UnifiedCastDetailedViewController() =
+    default;
+
+views::View* UnifiedCastDetailedViewController::CreateView() {
+  DCHECK(!view_);
+  view_ = new tray::CastDetailedView(
+      detailed_view_delegate_.get(),
+      Shell::Get()->cast_config()->sinks_and_routes());
+  return view_;
+}
+
+void UnifiedCastDetailedViewController::OnDevicesUpdated(
+    std::vector<mojom::SinkAndRoutePtr> devices) {
+  view_->UpdateReceiverList(std::move(devices));
+}
+
+}  // namespace ash
--- ash/system/unified/unified_system_tray_controller.cc
+++ ash/system/unified/unified_system_tray_controller.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/brightness/unified_brightness_slider_controller.h"
 #include "ash/system/cast/cast_feature_pod_controller.h"
 #include "ash/system/cast/tray_cast.h"
+#include "ash/system/cast/unified_cast_detailed_view_controller.h"
 #include "ash/system/ime/ime_feature_pod_controller.h"
 #include "ash/system/ime/tray_ime_chromeos.h"
 #include "ash/system/ime/unified_ime_detailed_view_controller.h"
@@ -233,8 +234,7 @@ void UnifiedSystemTrayController::ShowBluetoothDetailedView() {
 }
 
 void UnifiedSystemTrayController::ShowCastDetailedView() {
-  // TODO(tetsui): Implement Cast's own DetailedViewController.
-  ShowSystemTrayItemDetailedView(system_tray_->GetTrayCast());
+  ShowDetailedView(std::make_unique<UnifiedCastDetailedViewController>(this));
 }
 
 void UnifiedSystemTrayController::ShowAccessibilityDetailedView() {
