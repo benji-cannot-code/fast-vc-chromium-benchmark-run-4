4f0d1091c8c2a4c2a90c22bf35902e3f0bf0a6c5
Tim Schumann <tschumann@chromium.org>
2018-05-30 01:37:15 +0000

    Reland "Support late registration of syncable preferences."
    Sync integration tests where flaky on some platforms. Fixed
    those integration tests by enabling self-notifications and
    only modifying one client (and not the verifier as well).
    Inspected try-bot runs from two executions verifying no flakes.
    
    Original CL description:
    Support late registration of syncable preferences.
    
    Today, there's already a race when registering a preference
    through the mojo preference service: the asynchronous
    registration call might be slower than starting sync. As
    more services are being implemented, those scenarios are
    getting more likely -- especially as services could be
    started at arbitrary times.
    
    Before this path, the synced preferences implementation did
    not support this scenario. This patch changes the synced
    preference implementation to receive and persist remote
    data for preferences before they get registered. Currently,
    this only applies for a whitelisted set of preferences.
    More details about the approach can be found in the design
    doc linked from the crbug/840332.
    
    Bug: 840332
    
    Change-Id: I5fd2883d403b961a6332d7ee7bcdbad72f5cab8f
    Reviewed-on: https://chromium-review.googlesource.com/1074688
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Tim Schumann <tschumann@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562689}

--- chrome/browser/sync/test/integration/sync_test.h
+++ chrome/browser/sync/test/integration/sync_test.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_SYNC_TEST_INTEGRATION_SYNC_TEST_H_
 #define CHROME_BROWSER_SYNC_TEST_INTEGRATION_SYNC_TEST_H_
 
+#include <map>
 #include <memory>
 #include <string>
 #include <vector>
@@ -275,9 +276,10 @@ class SyncTest : public InProcessBrowserTest {
   void DisableNotificationsImpl();
   void EnableNotificationsImpl();
 
-  // If non-empty, |contents| will be written to a profile's Preferences file
-  // before the Profile object is created.
-  void SetPreexistingPreferencesFileContents(const std::string& contents);
+  // If non-empty, |contents| will be written to the Preferences file of the
+  // profile at |index| before that Profile object is created.
+  void SetPreexistingPreferencesFileContents(int index,
+                                             const std::string& contents);
 
   // Helper to ProfileManager::CreateProfileAsync that creates a new profile
   // used for UI Signin. Blocks until profile is created.
@@ -311,8 +313,9 @@ class SyncTest : public InProcessBrowserTest {
                                     Profile* profile,
                                     Profile::CreateStatus status);
 
-  // Helper to Profile::CreateProfile that handles path creation. It creates
-  // a profile then registers it as a testing profile.
+  // Helper to Profile::CreateProfile that handles path creation, setting up
+  // preexisting pref files, and registering the created profile  as a testing
+  // profile.
   Profile* MakeTestProfile(base::FilePath profile_path, int index);
 
   // Helper method used to create a Gaia account at runtime.
@@ -469,7 +472,8 @@ class SyncTest : public InProcessBrowserTest {
 
   // The contents to be written to a profile's Preferences file before the
   // Profile object is created. If empty, no preexisting file will be written.
-  std::string preexisting_preferences_file_contents_;
+  // The map key corresponds to the profile's index.
+  std::map<int, std::string> preexisting_preferences_file_contents_;
 
   // Disable extension install verification.
   extensions::ScopedInstallVerifierBypassForTest ignore_install_verification_;
