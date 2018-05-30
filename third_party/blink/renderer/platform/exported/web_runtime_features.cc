3c3333896b070aec39861ee2d2aea804bb3c35d7
Michael Lippautz <mlippautz@chromium.org>
2018-05-30 10:41:42 +0000

    Add chrome://flags entry for Blink incremental marking
    - Adds chrome://flags entry
    - Adds a FeatureFlag
    - Make existing Blink flag dependent on the Chrome flag
    
    Bug: chromium:757440, chromium:847735
    Change-Id: Iccb63fc31b330a6bb69e95d5c0d7599cce10e2ee
    Reviewed-on: https://chromium-review.googlesource.com/1077870
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562792}

--- third_party/blink/renderer/platform/exported/web_runtime_features.cc
+++ third_party/blink/renderer/platform/exported/web_runtime_features.cc
@@ -36,6 +36,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+void WebRuntimeFeatures::EnableBlinkHeapIncrementalMarking(bool enable) {
+  RuntimeEnabledFeatures::SetHeapIncrementalMarkingEnabled(enable);
+}
+
 void WebRuntimeFeatures::EnableExperimentalFeatures(bool enable) {
   RuntimeEnabledFeatures::SetExperimentalFeaturesEnabled(enable);
 }
