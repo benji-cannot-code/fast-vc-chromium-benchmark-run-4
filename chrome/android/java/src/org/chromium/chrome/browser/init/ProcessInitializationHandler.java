5d37b68359d87a6653371dc5185473264c7687bf
Ke He <ke.he@intel.com>
2018-05-30 02:35:32 +0000

    Remove the call site of "useGmsCoreLocationProvider()" in Java side.
    Embedder like Chrome can call the useGmsCoreLocationProvider() to indicate
    using the GMS core location provider. As the Geolocation will be moved
    to //services/device/, embedder shouldn't directly do that function call.
    
    We must make sure the useGmsCoreLocationProvider() is called (when embedder
    indicates so) earlier than any clients of the Geolocation which tries to
    consume the geolocation.mojom and geolocation_context.mojom. Otherwise an
    Android system default location provider will be unexpectedly created.
    
    Mojofy the "useGmsCoreLocationProvider()" is not a good option, because clients
    use separate mojo pipes to connect, we can not guarantee the order of messages
    between different mojo pipes. It is possible that the mojo call of
    "useGmsCoreLocationProvider()" is fired first, but arrives later than other
    mojo calls.
    
    So, we get a bool value which indicates whether using the GMS Core Location
    Provider from embedder, then pass it to the Device Service when it is created.
    
    BUG=800659
    
    Change-Id: Ib8d0e20a1a91d781175171bc98a63c45c27b5b59
    Reviewed-on: https://chromium-review.googlesource.com/1068516
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Ke He <ke.he@intel.com>
    Cr-Commit-Position: refs/heads/master@{#562704}

--- chrome/android/java/src/org/chromium/chrome/browser/init/ProcessInitializationHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/init/ProcessInitializationHandler.java
@@ -77,7 +77,6 @@ import org.chromium.components.signin.AccountManagerFacade;
 import org.chromium.components.signin.AccountsChangeObserver;
 import org.chromium.content.browser.ChildProcessLauncherHelper;
 import org.chromium.content_public.common.ContentSwitches;
-import org.chromium.device.geolocation.LocationProviderFactory;
 import org.chromium.printing.PrintDocumentAdapterWrapper;
 import org.chromium.printing.PrintingControllerImpl;
 import org.chromium.ui.PhotoPickerListener;
@@ -183,9 +182,6 @@ public class ProcessInitializationHandler {
         UniqueIdentificationGeneratorFactory.registerGenerator(SyncController.GENERATOR_ID,
                 new UuidBasedUniqueIdentificationGenerator(
                         application, SESSIONS_UUID_PREF_KEY), false);
-
-        // Indicate that we can use the GMS location provider.
-        LocationProviderFactory.useGmsCoreLocationProvider();
     }
 
     /**
--- device/geolocation/android/java/src/org/chromium/device/geolocation/LocationProviderFactory.java
+++ device/geolocation/android/java/src/org/chromium/device/geolocation/LocationProviderFactory.java
@@ -7,10 +7,13 @@ package org.chromium.device.geolocation;
 
 import org.chromium.base.ContextUtils;
 import org.chromium.base.VisibleForTesting;
+import org.chromium.base.annotations.CalledByNative;
+import org.chromium.base.annotations.JNINamespace;
 
 /**
  * Factory to create a LocationProvider to allow us to inject a mock for tests.
  */
+@JNINamespace("device")
 public class LocationProviderFactory {
     private static LocationProviderFactory.LocationProvider sProviderImpl;
     private static boolean sUseGmsCoreLocationProvider;
@@ -44,6 +47,7 @@ public class LocationProviderFactory {
         sProviderImpl = provider;
     }
 
+    @CalledByNative
     public static void useGmsCoreLocationProvider() {
         sUseGmsCoreLocationProvider = true;
     }
