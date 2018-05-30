9b6e29e9ac2f5afacc1b539b0382c12c9013ba97
Colin Blundell <blundell@chromium.org>
2018-05-30 10:52:32 +0000

    Remove PrimaryAccountAccessTokenFetcher observing OnRefreshTokensLoaded
    PrimaryAccountAccessTokenFetcher currently has the following flow:
    - wait for user to sign in
    - wait for primary account refresh token to become available
    - if during that waiting refresh tokens are loaded, fail (by making an
      access token request that will be guaranteed to fail)
    
    The third step above leads to inconsistency:
    
    Assume that a user is signed in but their token is not available on
    disk (e.g., it's been corrupted). If a PrimaryAccountAccessTokenFetcher
    is created *before* the tokens are loaded from disk, that fetcher will
    return an error to its client once tokens are loaded. If a
    PrimaryAccountAccessTokenFetcher is created after the tokens are loaded,
    that fetcher will wait until the refresh token actually becomes
    available (e.g., via a reauth). It will potentially wait forever.
    
    There seems no reason to have the distinction of whether a
    PrimaryAccountAccessTokenFetcher was created before or after tokens were
    loaded from disk result in behavioral differences that are visible to
    clients of PrimaryAccountAccessTokenFetcher.
    
    Note that by definition this change can result in behavioral changes for
    the cases where PrimaryAccountAccessTokenFetchers are created before
    tokens are loaded from disk. However, as clients of
    PrimaryAccountAccessTokenFetcher do not query whether tokens have been
    loaded from disk or not, any such resulting behaviors are already
    possible in the existing codebase (in the case where the fetchers
    happen to be created after tokens were loaded from disk, i.e., any time
    after startup).
    
    From treib@chromium.org, the original author of this code:
    
    "I've been trying to remember if I had any particular reason for adding
    this logic, but I think I just copied it from whatever I used as a model
    for the very first AccessTokenFetcher implementation.
    
    I completely agree with your reasoning that the distinction (based on
    whether tokens were already loaded from disk or not) doesn't make sense."
    
    Bug: 796544
    Change-Id: I1bf855663ddee961ca68886500b92943df274f06
    Reviewed-on: https://chromium-review.googlesource.com/1076289
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562795}

--- services/identity/public/cpp/primary_account_access_token_fetcher.h
+++ services/identity/public/cpp/primary_account_access_token_fetcher.h
@@ -69,7 +69,6 @@ class PrimaryAccountAccessTokenFetcher : public SigninManagerBase::Observer,
 
   // OAuth2TokenService::Observer implementation.
   void OnRefreshTokenAvailable(const std::string& account_id) override;
-  void OnRefreshTokensLoaded() override;
 
   // OAuth2TokenService::Consumer implementation.
   void OnGetTokenSuccess(const OAuth2TokenService::Request* request,
