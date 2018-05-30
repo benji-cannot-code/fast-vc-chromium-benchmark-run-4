3c9e13702ebe35e77df74dbbf37c0ac126967368
erikchen <erikchen@chromium.org>
2018-05-30 15:20:04 +0000

    Remove unused code from BackgroundMemoryTracingObserver.
    The class only needs to trigger a memory dump for light heap profiling to work.
    
    Bug: 822843
    Change-Id: I2da1cda226458f6b8b642f045f8142d561fc3ea8
    Reviewed-on: https://chromium-review.googlesource.com/1076495
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562830}

--- content/browser/tracing/background_memory_tracing_observer.cc
+++ content/browser/tracing/background_memory_tracing_observer.cc
@@ -5,21 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/tracing/background_memory_tracing_observer.h"
 
-#include "base/trace_event/heap_profiler_allocation_context_tracker.h"
-#include "base/trace_event/heap_profiler_event_filter.h"
 #include "base/trace_event/memory_dump_request_args.h"
-#include "base/trace_event/trace_log.h"
-#include "content/browser/tracing/background_tracing_rule.h"
 #include "services/resource_coordinator/public/cpp/memory_instrumentation/memory_instrumentation.h"
 
-using base::trace_event::AllocationContextTracker;
-using base::trace_event::TraceConfig;
-using base::trace_event::TraceLog;
-
 namespace content {
-namespace {
-const char kHeapProfilerCategoryFilter[] = "heap_profiler_category_filter";
-}  // namespace
 
 // static
 BackgroundMemoryTracingObserver*
@@ -32,58 +21,9 @@ BackgroundMemoryTracingObserver::BackgroundMemoryTracingObserver() {}
 BackgroundMemoryTracingObserver::~BackgroundMemoryTracingObserver() {}
 
 void BackgroundMemoryTracingObserver::OnScenarioActivated(
-    const BackgroundTracingConfigImpl* config) {
-  if (!config) {
-    DCHECK(!enabled_);
-    return;
-  }
+    const BackgroundTracingConfigImpl* config) {}
 
-  const BackgroundTracingRule* heap_profiling_rule = nullptr;
-  for (const auto& rule : config->rules()) {
-    if (rule->category_preset() == BackgroundTracingConfigImpl::CategoryPreset::
-                                       BENCHMARK_MEMORY_LIGHT &&
-        rule->args()) {
-      heap_profiling_rule = rule.get();
-      break;
-    }
-  }
-  if (!heap_profiling_rule)
-    return;
-
-  enabled_ = true;
-
-  // TODO(ssid): Add ability to enable profiling on all processes,
-  // crbug.com/700245.
-  AllocationContextTracker::SetCaptureMode(
-      AllocationContextTracker::CaptureMode::MIXED_STACK);
-
-  std::string filter_string;
-  if ((TraceLog::GetInstance()->enabled_modes() & TraceLog::FILTERING_MODE) ||
-      !heap_profiling_rule->args()->GetString(kHeapProfilerCategoryFilter,
-                                              &filter_string)) {
-    return;
-  }
-  base::trace_event::TraceConfigCategoryFilter category_filter;
-  category_filter.InitializeFromString(filter_string);
-  TraceConfig::EventFilterConfig heap_profiler_filter_config(
-      base::trace_event::HeapProfilerEventFilter::kName);
-  heap_profiler_filter_config.SetCategoryFilter(category_filter);
-  TraceConfig::EventFilters filters;
-  filters.push_back(heap_profiler_filter_config);
-  TraceConfig filtering_trace_config;
-  filtering_trace_config.SetEventFilters(filters);
-  TraceLog::GetInstance()->SetEnabled(filtering_trace_config,
-                                      TraceLog::FILTERING_MODE);
-}
-
-void BackgroundMemoryTracingObserver::OnScenarioAborted() {
-  if (!enabled_)
-    return;
-  enabled_ = false;
-  base::trace_event::AllocationContextTracker::SetCaptureMode(
-      AllocationContextTracker::CaptureMode::DISABLED);
-  TraceLog::GetInstance()->SetDisabled(TraceLog::FILTERING_MODE);
-}
+void BackgroundMemoryTracingObserver::OnScenarioAborted() {}
 
 void BackgroundMemoryTracingObserver::OnTracingEnabled(
     BackgroundTracingConfigImpl::CategoryPreset preset) {
