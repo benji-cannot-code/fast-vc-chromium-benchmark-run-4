266764fb2e3efc800f5fff91c505bbe427c35f4a
Jeremy Roman <jbroman@chromium.org>
2018-05-30 15:22:57 +0000

    Have CrossThreadPersistentRegion hold the underlying PersistentRegion as a plain member.
    There's a comment justifying not using it as a base class, but nothing prevents
    using it as a member, which allows this class to be initialized without dynamic
    allocation.
    
    Change-Id: I37bf13c4c7c226a88ccf488f5db7f100293fdd1d
    Reviewed-on: https://chromium-review.googlesource.com/1076906
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562832}

--- third_party/blink/renderer/platform/heap/persistent_node.h
+++ third_party/blink/renderer/platform/heap/persistent_node.h
@@ -170,15 +170,12 @@ class CrossThreadPersistentRegion final {
   USING_FAST_MALLOC(CrossThreadPersistentRegion);
 
  public:
-  CrossThreadPersistentRegion()
-      : persistent_region_(std::make_unique<PersistentRegion>()) {}
-
   void AllocatePersistentNode(PersistentNode*& persistent_node,
                               void* self,
                               TraceCallback trace) {
     RecursiveMutexLocker lock(ProcessHeap::CrossThreadPersistentMutex());
     PersistentNode* node =
-        persistent_region_->AllocatePersistentNode(self, trace);
+        persistent_region_.AllocatePersistentNode(self, trace);
     ReleaseStore(reinterpret_cast<void* volatile*>(&persistent_node), node);
   }
 
@@ -196,7 +193,7 @@ class CrossThreadPersistentRegion final {
     // check for this.
     if (!persistent_node)
       return;
-    persistent_region_->FreePersistentNode(persistent_node);
+    persistent_region_.FreePersistentNode(persistent_node);
     ReleaseStore(reinterpret_cast<void* volatile*>(&persistent_node), nullptr);
   }
 
@@ -205,7 +202,7 @@ class CrossThreadPersistentRegion final {
 #if DCHECK_IS_ON()
     DCHECK(ProcessHeap::CrossThreadPersistentMutex().Locked());
 #endif
-    persistent_region_->TracePersistentNodes(
+    persistent_region_.TracePersistentNodes(
         visitor, CrossThreadPersistentRegion::ShouldTracePersistentNode);
   }
 
@@ -223,7 +220,7 @@ class CrossThreadPersistentRegion final {
   // We don't make CrossThreadPersistentRegion inherit from PersistentRegion
   // because we don't want to virtualize performance-sensitive methods
   // such as PersistentRegion::allocate/freePersistentNode.
-  std::unique_ptr<PersistentRegion> persistent_region_;
+  PersistentRegion persistent_region_;
 };
 
 }  // namespace blink
