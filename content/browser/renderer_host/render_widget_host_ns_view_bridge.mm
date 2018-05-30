514f00d0b5f5408b23a86854c31435028bc8e398
Christopher Cameron <ccameron@chromium.org>
2018-05-30 21:40:33 +0000

    MacViews: Lazily enable using a single ui::Compositor
    It is not possible to know ahead of time whether or not a
    WebContentsView will be embedded in an NSView or a views::View, even
    when MacViewsBrowser is enabled (content_shell, for instance, uses the
    NSView display path).
    
    Lazily determine if a WebContentsView is to display via its parent
    views::View's compositor. When attaching to a views::View for the first
    time, clear all content that is being displayed in the NSView (because
    it will occlude the views::View) and disable displaying through Cocoa
    for the lifetime of the WebContentsView.
    
    When all code that expects to display through Cocoa is updated or
    deleted, this lazy determination may be removed.
    
    Bug: 840173
    Change-Id: I36c7054ba7c71e6fe94e496e677a9a9be4fb790a
    Reviewed-on: https://chromium-review.googlesource.com/1075563
    Reviewed-by: Sidney San Martín <sdy@chromium.org>
    Commit-Queue: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563008}

--- content/browser/renderer_host/render_widget_host_ns_view_bridge.mm
+++ content/browser/renderer_host/render_widget_host_ns_view_bridge.mm
@@ -39,6 +39,7 @@ class RenderWidgetHostViewNSViewBridgeLocal
 
   void InitAsPopup(const gfx::Rect& content_rect,
                    blink::WebPopupType popup_type) override;
+  void DisableDisplay() override;
   void MakeFirstResponder() override;
   void SetBounds(const gfx::Rect& rect) override;
   void SetCALayerParams(const gfx::CALayerParams& ca_layer_params) override;
@@ -74,6 +75,10 @@ class RenderWidgetHostViewNSViewBridgeLocal
 
   // The NSView used for input and display.
   base::scoped_nsobject<RenderWidgetHostViewCocoa> cocoa_view_;
+
+  // Once set, all calls to set the background color or CALayer content will
+  // be ignored.
+  bool display_disabled_ = false;
 
   // The window used for popup widgets, and its helper.
   std::unique_ptr<PopupWindowMac> popup_window_;
@@ -135,6 +140,14 @@ void RenderWidgetHostViewNSViewBridgeLocal::MakeFirstResponder() {
   [[cocoa_view_ window] makeFirstResponder:cocoa_view_];
 }
 
+void RenderWidgetHostViewNSViewBridgeLocal::DisableDisplay() {
+  if (display_disabled_)
+    return;
+  SetBackgroundColor(SK_ColorTRANSPARENT);
+  display_ca_layer_tree_.reset();
+  display_disabled_ = true;
+}
+
 void RenderWidgetHostViewNSViewBridgeLocal::SetBounds(const gfx::Rect& rect) {
   // |rect.size()| is view coordinates, |rect.origin| is screen coordinates,
   // TODO(thakis): fix, http://crbug.com/73362
@@ -178,10 +191,14 @@ void RenderWidgetHostViewNSViewBridgeLocal::SetBounds(const gfx::Rect& rect) {
 
 void RenderWidgetHostViewNSViewBridgeLocal::SetCALayerParams(
     const gfx::CALayerParams& ca_layer_params) {
+  if (display_disabled_)
+    return;
   display_ca_layer_tree_->UpdateCALayerTree(ca_layer_params);
 }
 
 void RenderWidgetHostViewNSViewBridgeLocal::SetBackgroundColor(SkColor color) {
+  if (display_disabled_)
+    return;
   ScopedCAActionDisabler disabler;
   base::ScopedCFTypeRef<CGColorRef> cg_color(
       skia::CGColorCreateFromSkColor(color));
