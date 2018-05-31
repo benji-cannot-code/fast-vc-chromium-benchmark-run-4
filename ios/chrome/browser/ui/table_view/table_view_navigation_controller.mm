a03ebc8c98bdb71a70ae28325114dc67a6bb20e9
sczs <sczs@chromium.org>
2018-05-31 17:08:24 +0000

    [iOS] Styles BookmarkHomeVC for UIRefresh.
    Screenshots:
    https://drive.google.com/open?id=1Y5SrS0fznYQ1HRtiyydtYrFejtcO5qbB
    https://drive.google.com/open?id=1UeIyvQTAkmkDEQcMVAuCoh8cUWCK_d2T
    
    Bug: 839442
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I1f65dc7ce24307e82c4246b4da2776e92d8df9d8
    Reviewed-on: https://chromium-review.googlesource.com/1074557
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563286}

--- ios/chrome/browser/ui/table_view/table_view_navigation_controller.mm
+++ ios/chrome/browser/ui/table_view/table_view_navigation_controller.mm
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)viewDidLoad {
   [self.navigationBar setBackgroundImage:[UIImage new]
                            forBarMetrics:UIBarMetricsDefault];
-
   if (self.tableViewController.styler.tableViewBackgroundColor !=
       [UIColor clearColor]) {
     self.navigationBar.translucent = NO;
