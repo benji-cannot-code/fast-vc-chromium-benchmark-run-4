02a151366fe88a31373b338e5f6b7a30cd633b74
Sebastien Marchand <sebmarchand@chromium.org>
2018-05-31 17:44:49 +0000

    Makes the Local Site DB factory depend on the History service factory.
    This ensures that the history service gets created before the local
    site data store factory and so we can observe it.
    
    Bug: 773382
    Change-Id: I5ffd289056199788a41d874bfbd817184e44107c
    Reviewed-on: https://chromium-review.googlesource.com/1080731
    Reviewed-by: François Doray <fdoray@chromium.org>
    Commit-Queue: Sébastien Marchand <sebmarchand@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563302}

--- chrome/browser/resource_coordinator/local_site_characteristics_data_store_factory.cc
+++ chrome/browser/resource_coordinator/local_site_characteristics_data_store_factory.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/resource_coordinator/local_site_characteristics_data_store_factory.h"
 
 #include "base/feature_list.h"
+#include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/profiles/incognito_helpers.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/resource_coordinator/local_site_characteristics_data_store.h"
@@ -36,7 +37,9 @@ LocalSiteCharacteristicsDataStoreFactory::
     LocalSiteCharacteristicsDataStoreFactory()
     : BrowserContextKeyedServiceFactory(
           "LocalSiteCharacteristicsDataStore",
-          BrowserContextDependencyManager::GetInstance()) {}
+          BrowserContextDependencyManager::GetInstance()) {
+  DependsOn(HistoryServiceFactory::GetInstance());
+}
 
 LocalSiteCharacteristicsDataStoreFactory::
     ~LocalSiteCharacteristicsDataStoreFactory() = default;
