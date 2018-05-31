26f015ffe003781ecf66ac0d698e49cd0db01a0d
Nasko Oskov <nasko@chromium.org>
2018-05-31 23:54:40 +0000

    Replace FIXMEs with correct comments.
    As discussed in https://crbug.com/753130, there is no additional work
    needed for OOPIFs to be notified of localStorage/sessionStorage updates.
    This CL removes the FIXMEs and replaces them with a more accurate comment.
    
    Bug: 753130
    Change-Id: I7b9a0c3869168c716281837dae2572adb01a09c5
    Reviewed-on: https://chromium-review.googlesource.com/1080286
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563441}

--- third_party/blink/renderer/modules/storage/storage_area.cc
+++ third_party/blink/renderer/modules/storage/storage_area.cc
@@ -168,8 +168,8 @@ void StorageArea::DispatchLocalStorageEvent(
   for (Page* page : Page::OrdinaryPages()) {
     for (Frame* frame = page->MainFrame(); frame;
          frame = frame->Tree().TraverseNext()) {
-      // FIXME: We do not yet have a way to dispatch events to out-of-process
-      // frames.
+      // Remote frames are cross-origin and do not need to be notified of
+      // events.
       if (!frame->IsLocalFrame())
         continue;
       LocalFrame* local_frame = ToLocalFrame(frame);
@@ -220,8 +220,7 @@ void StorageArea::DispatchSessionStorageEvent(
 
   for (Frame* frame = page->MainFrame(); frame;
        frame = frame->Tree().TraverseNext()) {
-    // FIXME: We do not yet have a way to dispatch events to out-of-process
-    // frames.
+    // Remote frames are cross-origin and do not need to be notified of events.
     if (!frame->IsLocalFrame())
       continue;
     LocalFrame* local_frame = ToLocalFrame(frame);
