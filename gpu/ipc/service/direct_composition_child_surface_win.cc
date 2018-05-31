fce8b6c551bd64f0d8b0fc85e6623de2b5137c47
Sunny Sachanandani <sunnyps@chromium.org>
2018-05-31 01:44:24 +0000

    Remove debugging code for null context SetSwapInterval crash
    GLContext::SetSwapInterval was removed and replaced with
    GLSurface::SetVSyncEnabled which is not prone to this kind of crash.
    Remove debugging code from a bunch of places, none of which helped in
    finding the error.
    
    Bug: 724999
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: Ica7a99f191621be1e95a80c9197e9efbadc63464
    Reviewed-on: https://chromium-review.googlesource.com/1072962
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563115}

--- gpu/ipc/service/direct_composition_child_surface_win.cc
+++ gpu/ipc/service/direct_composition_child_surface_win.cc
@@ -77,15 +77,6 @@ bool DirectCompositionChildSurfaceWin::Initialize(gl::GLSurfaceFormat format) {
   if (!default_surface_) {
     DLOG(ERROR) << "eglCreatePbufferSurface failed with error "
                 << ui::GetLastEGLErrorString();
-    // It is likely that restoring the context will fail, so call Restore here
-    // to avoid the assert during ScopedReleaseCurrent destruction.
-    ignore_result(release_current.Restore());
-    return false;
-  }
-
-  if (!release_current.Restore()) {
-    DLOG(ERROR) << "Failed to restore context with error "
-                << ui::GetLastEGLErrorString();
     return false;
   }
 
@@ -244,6 +235,7 @@ gfx::SwapResult DirectCompositionChildSurfaceWin::SwapBuffers(
   // PresentationCallback is handled by DirectCompositionSurfaceWin. The child
   // surface doesn't need provide presentation feedback.
   DCHECK(!callback);
+  ui::ScopedReleaseCurrent release_current;
   if (!ReleaseDrawTexture(false /* will_discard */))
     return gfx::SwapResult::SWAP_FAILED;
   return gfx::SwapResult::SWAP_ACK;
@@ -303,9 +295,6 @@ bool DirectCompositionChildSurfaceWin::SetDrawRectangle(
       (!use_dcomp_surface_ && !swap_chain_)) {
     if (!InitializeSurface()) {
       DLOG(ERROR) << "InitializeSurface failed";
-      // It is likely that restoring the context will fail, so call Restore here
-      // to avoid the assert during ScopedReleaseCurrent destruction.
-      ignore_result(release_current.Restore());
       return false;
     }
   }
@@ -355,15 +344,6 @@ bool DirectCompositionChildSurfaceWin::SetDrawRectangle(
       &pbuffer_attribs[0]);
   if (!real_surface_) {
     DLOG(ERROR) << "eglCreatePbufferFromClientBuffer failed with error "
-                << ui::GetLastEGLErrorString();
-    // It is likely that restoring the context will fail, so call Restore here
-    // to avoid the assert during ScopedReleaseCurrent destruction.
-    ignore_result(release_current.Restore());
-    return false;
-  }
-
-  if (!release_current.Restore()) {
-    DLOG(ERROR) << "Failed to restore context with error "
                 << ui::GetLastEGLErrorString();
     return false;
   }
