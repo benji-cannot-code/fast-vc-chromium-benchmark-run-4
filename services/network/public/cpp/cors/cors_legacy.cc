49c6494833c525ab673bc15ae04c479296cbd427
Jose Dapena Paz <jose.dapena@lge.com>
2018-05-31 17:03:37 +0000

    CORS legacy: add missing string include
    The cors_legacy.h file includes declarations using std::string, but
    it is not declared due to missing #include <string>.
    
    Also drop unneeded declarations in .cpp file.
    
    Change-Id: I00df799f84a6c3530c2f12f1e52d24c7d9bd6bfd
    Reviewed-on: https://chromium-review.googlesource.com/1080707
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: José Dapena Paz <jose.dapena@lge.com>
    Cr-Commit-Position: refs/heads/master@{#563282}

--- services/network/public/cpp/cors/cors_legacy.cc
+++ services/network/public/cpp/cors/cors_legacy.cc
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/cors/cors_legacy.h"
 
 #include <algorithm>
-#include <string>
-#include <vector>
 
 #include "url/gurl.h"
 #include "url/url_util.h"
