4605cfdcd8ca07cd1617a42843f9929641e14011
Xi Cheng <chengx@chromium.org>
2018-05-31 17:37:27 +0000

    Update file comments after file movement
    Bug: 734095
    Change-Id: I0431a43f218010bc139f5778f8d4ae2981b61408
    Reviewed-on: https://chromium-review.googlesource.com/1079802
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Xi Cheng <chengx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563300}

--- chrome/browser/notifications/win/notification_helper_launches_chrome_unittest.cc
+++ chrome/browser/notifications/win/notification_helper_launches_chrome_unittest.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // also tests if chrome.exe can be successfully launched by
 // notification_helper.exe via the NotificationActivator::Activate function.
 //
-// This test is compiled into unit_tests.exe under chrome rather than
+// This test is compiled into unit_tests.exe rather than
 // notification_helper_unittests.exe. This is because unit_tests.exe has data
 // dependency on chrome.exe which is required by this test, and it's undesired
 // to make notification_helper_unittests.exe have data dependency on chrome.exe.
