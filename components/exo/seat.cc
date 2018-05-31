b5a382855a468a534e7257a012dbea67d0f121f7
David Reveman <reveman@chromium.org>
2018-05-31 00:54:53 +0000

    exo: Ignore synthetic key repeat events.
    Exo clients should not see these events and instead handle
    key repeat on the client side.
    
    Bug: 847500
    Test: exo_unittests
    Change-Id: I4d4c755dbe0a2fffe24f3ce85fd70a3608cfed7c
    Reviewed-on: https://chromium-review.googlesource.com/1079768
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Commit-Queue: David Reveman <reveman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563095}

--- components/exo/seat.cc
+++ components/exo/seat.cc
@@ -108,6 +108,9 @@ void Seat::OnWindowFocused(aura::Window* gained_focus,
 // ui::EventHandler overrides:
 
 void Seat::OnKeyEvent(ui::KeyEvent* event) {
+  // Ignore synthetic key repeat events.
+  if (event->is_repeat())
+    return;
   switch (event->type()) {
     case ui::ET_KEY_PRESSED:
       pressed_keys_.insert(event->code());
