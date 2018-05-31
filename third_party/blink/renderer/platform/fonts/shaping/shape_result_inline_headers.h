d5ea2b734d39c55ff77169e06256318169c1c50d
Koji Ishii <kojii@chromium.org>
2018-05-31 18:18:44 +0000

    Store safe-to-break flag in HarfBuzzRunGlyphData
    This patch changes the storage of safe-to-break from
    Vector<uint16_t> to a bit in HarfBuzzRunGlyphData.
    
    The allocation of the vector in CreateSubRun turned out to be
    hot. This patch saves the cost and also reduce the memory usage.
    
    With this change, a RunInfo can store only up to 15-bits
    character index. CommitGlyphs is changed to create multiple
    RunInfo when the limit exceeds.
    
    fast/text/long-word.html hits this condition. It actually has
    been hitting the limit by stroing 65536 to uint16_t. The new
    DCHECK and multiple RunInfo for a BufferSlice fixed this.
    
    Design doc:
    https://docs.google.com/document/d/16BI-9BovAMZ4rmpaPyG5PeEY-opiQEty_X8CMmngLSU/edit?usp=sharing
    
    Bug: 636993
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: I32e60a25b7009b0becde246f28729b5163eabcd4
    Reviewed-on: https://chromium-review.googlesource.com/1076787
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563323}

--- third_party/blink/renderer/platform/fonts/shaping/shape_result_inline_headers.h
+++ third_party/blink/renderer/platform/fonts/shaping/shape_result_inline_headers.h
@@ -45,15 +45,20 @@ class SimpleFontData;
 
 struct HarfBuzzRunGlyphData {
   DISALLOW_NEW_EXCEPT_PLACEMENT_NEW();
+
+  static constexpr unsigned kMaxCharacterIndex = (1 << 15) - 1;
+
   uint16_t glyph;
-  uint16_t character_index;
+  unsigned character_index : 15;
+  unsigned safe_to_break_before : 1;
   float advance;
   FloatSize offset;
 
   void SetGlyphAndPositions(uint16_t glyph_id,
                             uint16_t character_index,
                             float advance,
-                            const FloatSize& offset);
+                            const FloatSize& offset,
+                            bool safe_to_break_before);
 };
 
 struct ShapeResult::RunInfo {
@@ -86,6 +91,7 @@ struct ShapeResult::RunInfo {
         num_characters_(other.num_characters_),
         width_(other.width_) {}
 
+  unsigned NumGlyphs() const { return glyph_data_.size(); }
   bool Rtl() const { return HB_DIRECTION_IS_BACKWARD(direction_); }
   bool IsHorizontal() const { return HB_DIRECTION_IS_HORIZONTAL(direction_); }
   CanvasRotationInVertical CanvasRotation() const { return canvas_rotation_; }
@@ -139,6 +145,7 @@ struct ShapeResult::RunInfo {
           HarfBuzzRunGlyphData& sub_glyph = run->glyph_data_[sub_glyph_index++];
           sub_glyph.glyph = glyph_data.glyph;
           sub_glyph.character_index = glyph_data.character_index - start;
+          sub_glyph.safe_to_break_before = glyph_data.safe_to_break_before;
           sub_glyph.advance = glyph_data.advance;
           sub_glyph.offset = glyph_data.offset;
           total_advance += glyph_data.advance;
@@ -147,11 +154,6 @@ struct ShapeResult::RunInfo {
 
     run->width_ = total_advance;
     run->num_characters_ = number_of_characters;
-
-    for (unsigned i = 0; i < safe_break_offsets_.size(); i++) {
-      if (safe_break_offsets_[i] >= start && safe_break_offsets_[i] <= end)
-        run->safe_break_offsets_.push_back(safe_break_offsets_[i] - start);
-    }
 
     return run;
   }
@@ -190,7 +192,7 @@ struct ShapeResult::RunInfo {
         initial_advance,
         [&](const HarfBuzzRunGlyphData& glyph_data,
             float total_advance) -> bool {
-          const uint16_t character_index =
+          const unsigned character_index =
               start_index_ + glyph_data.character_index + index_offset;
 
           if (character_index < from) {
@@ -219,7 +221,6 @@ struct ShapeResult::RunInfo {
   Vector<HarfBuzzRunGlyphData> glyph_data_;
   // List of character indecies before which it's safe to break without
   // reshaping.
-  Vector<uint16_t> safe_break_offsets_;
   unsigned start_index_;
   unsigned num_characters_;
   float width_;
