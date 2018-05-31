3cd5c358dc78f63036e5408b65df771706031ddf
Mounir Lamouri <mlamouri@chromium.org>
2018-05-31 23:36:04 +0000

    Picture-in-Picture: Close() when the window is already closed should be a no-op.
    Bug: 848107
    Change-Id: I8ca426ee3393a40071c564c6e58564167f6df935
    Reviewed-on: https://chromium-review.googlesource.com/1080464
    Reviewed-by: apacible <apacible@chromium.org>
    Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563431}

--- content/browser/picture_in_picture/picture_in_picture_window_controller_impl.cc
+++ content/browser/picture_in_picture/picture_in_picture_window_controller_impl.cc
@@ -77,6 +77,9 @@ gfx::Size PictureInPictureWindowControllerImpl::Show() {
 void PictureInPictureWindowControllerImpl::Close() {
   DCHECK(window_);
 
+  if (!window_->IsVisible())
+    return;
+
   window_->Hide();
   initiator_->SetHasPictureInPictureVideo(false);
 
