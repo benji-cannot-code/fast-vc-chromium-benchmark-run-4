c8914311244d99e9f1be883c1bb1b31a6a78ccba
Hayato Ito <hayato@chromium.org>
2018-05-31 13:28:50 +0000

    Add more slot-assignement-recalc-forbidden scoped check
    This is a follow-up of https://crrev.com/c/1070169.
    
    Added scoped checks in:
    - InsertBefore
    - ReplaceChild
    
    The future plan: The remaining operation we should check is Element::attachShadow, which will be
    done in another CL.
    
    After that, I'll refactor so that this kind of check can be done in more better places.
    
    
    Bug: 845770
    Change-Id: I8463c38a6676fd8cee4d300423b74a88e764f12d
    Reviewed-on: https://chromium-review.googlesource.com/1074786
    Commit-Queue: Hayato Ito <hayato@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563206}

--- third_party/blink/renderer/core/dom/container_node.cc
+++ third_party/blink/renderer/core/dom/container_node.cc
@@ -405,6 +405,10 @@ Node* ContainerNode::InsertBefore(Node* new_child,
   // 5. Insert node into parent before reference child.
   NodeVector post_insertion_notification_targets;
   {
+#if DCHECK_IS_ON()
+    NestingLevelIncrementer slot_assignment_recalc_forbidden_scope(
+        GetDocument().SlotAssignmentRecalcForbiddenRecursionDepth());
+#endif
     ChildListMutationScope mutation(*this);
     InsertNodeVector(targets, ref_child, AdoptAndInsertBefore(),
                      &post_insertion_notification_targets);
@@ -568,6 +572,11 @@ Node* ContainerNode::ReplaceChild(Node* new_child,
       if (!detector.HadAtMostOneDOMMutation())
         needs_recheck = true;
     }
+
+#if DCHECK_IS_ON()
+    NestingLevelIncrementer slot_assignment_recalc_forbidden_scope(
+        GetDocument().SlotAssignmentRecalcForbiddenRecursionDepth());
+#endif
 
     // 13. Let nodes be node’s children if node is a DocumentFragment node, and
     // a list containing solely node otherwise.
