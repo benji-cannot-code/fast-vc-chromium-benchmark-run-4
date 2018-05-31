529775a7ce176629c0fe128e7485ef9a0d806688
Rayan Kanso <rayankans@chromium.org>
2018-05-31 18:17:44 +0000

    Update comments about the unused ScriptState::Scope variable.
    Bug: 836787
    Change-Id: If926389721e29f806ae9c275c61fdaa7c87b287c
    Reviewed-on: https://chromium-review.googlesource.com/1080553
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563322}

--- third_party/blink/renderer/modules/serviceworkers/service_worker_global_scope_proxy.cc
+++ third_party/blink/renderer/modules/serviceworkers/service_worker_global_scope_proxy.cc
@@ -151,7 +151,8 @@ void ServiceWorkerGlobalScopeProxy::DispatchBackgroundFetchAbortEvent(
   ScriptState* script_state =
       WorkerGlobalScope()->ScriptController()->GetScriptState();
 
-  // Do not remove this, it modifies V8 state.
+  // Do not remove this, |scope| is needed by
+  // BackgroundFetchSettledFetches::Create which eventually calls ToV8.
   ScriptState::Scope scope(script_state);
 
   BackgroundFetchSettledEventInit init;
@@ -205,7 +206,8 @@ void ServiceWorkerGlobalScopeProxy::DispatchBackgroundFetchFailEvent(
   ScriptState* script_state =
       WorkerGlobalScope()->ScriptController()->GetScriptState();
 
-  // Do not remove this, it modifies V8 state.
+  // Do not remove this, |scope| is needed by
+  // BackgroundFetchSettledFetches::Create which eventually calls ToV8.
   ScriptState::Scope scope(script_state);
 
   BackgroundFetchSettledEventInit init;
@@ -231,7 +233,8 @@ void ServiceWorkerGlobalScopeProxy::DispatchBackgroundFetchedEvent(
   ScriptState* script_state =
       WorkerGlobalScope()->ScriptController()->GetScriptState();
 
-  // Do not remove this, it modifies V8 state.
+  // Do not remove this, |scope| is needed by
+  // BackgroundFetchSettledFetches::Create which eventually calls ToV8.
   ScriptState::Scope scope(script_state);
 
   BackgroundFetchSettledEventInit init;
