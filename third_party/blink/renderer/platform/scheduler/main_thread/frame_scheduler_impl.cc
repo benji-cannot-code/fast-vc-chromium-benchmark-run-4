4a52d46fa2841849b9cbadc5e201b4e23add79d4
Hajime Hoshi <hajimehoshi@chromium.org>
2018-05-31 13:26:41 +0000

    Remove PausableTimer from DOMWindowEventQueue
    As the first step of refactoring EventQueue, remove PausableTimer usage
    from DOMWindowEventQueue.
    
    Bug: 846618
    Change-Id: I95ab541c7bcbc3bc66597eb8e40e3894bcdd6066
    Reviewed-on: https://chromium-review.googlesource.com/1076110
    Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563203}

--- third_party/blink/renderer/platform/scheduler/main_thread/frame_scheduler_impl.cc
+++ third_party/blink/renderer/platform/scheduler/main_thread/frame_scheduler_impl.cc
@@ -294,8 +294,8 @@ scoped_refptr<base::SingleThreadTaskRunner> FrameSchedulerImpl::GetTaskRunner(
     case TaskType::kInternalMedia:
     case TaskType::kInternalMediaRealTime:
     case TaskType::kInternalUserInteraction:
-      return TaskQueueWithTaskType::Create(PausableTaskQueue(), type);
     case TaskType::kUnthrottled:
+      return TaskQueueWithTaskType::Create(PausableTaskQueue(), type);
     case TaskType::kInternalTest:
     case TaskType::kInternalWebCrypto:
     case TaskType::kInternalIPC:
