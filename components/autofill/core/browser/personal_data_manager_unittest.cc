c0243bddd1a1b1057922c877b3f5d62b7861080b
Hui(Andy) Wu <wuandy@chromium.org>
2018-05-31 20:02:00 +0000

    [Autofill] Fix a unittest failure due to dependency to current time.
    A unittest fails because it is checking how long has a credit card
    expired. Fixing it by always setting the expiry date to a date one
    month earlier than current time.
    
    Bug: 848246
    Change-Id: I57d142ad49d8155241f5f04da2246fee9f54b2cb
    Reviewed-on: https://chromium-review.googlesource.com/1081229
    Commit-Queue: Hui Wu <wuandy@chromium.org>
    Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563359}

--- components/autofill/core/browser/personal_data_manager_unittest.cc
+++ components/autofill/core/browser/personal_data_manager_unittest.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/message_loop.h"
 #include "base/rand_util.h"
 #include "base/run_loop.h"
+#include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/synchronization/waitable_event.h"
 #include "base/test/histogram_tester.h"
@@ -4558,9 +4559,8 @@ TEST_F(PersonalDataManagerTest, DeleteDisusedCreditCards_OncePerVersion) {
 }
 
 // Tests that DeleteDisusedCreditCards deletes desired credit cards only.
-// Disabled see crbug.com/848227
 TEST_F(PersonalDataManagerTest,
-       DISABLED_DeleteDisusedCreditCards_OnlyDeleteExpiredDisusedLocalCards) {
+       DeleteDisusedCreditCards_OnlyDeleteExpiredDisusedLocalCards) {
   // Enable the feature.
   base::test::ScopedFeatureList scoped_features;
   scoped_features.InitAndEnableFeature(kAutofillDeleteDisusedCreditCards);
@@ -4586,8 +4586,13 @@ TEST_F(PersonalDataManagerTest,
   // Create a local card expired recently, and last used 400 days ago.
   // It is expected to remain.
   CreditCard credit_card3(base::GenerateGUID(), "https://www.example.com");
+  base::Time expiry_date = now - base::TimeDelta::FromDays(32);
+  base::Time::Exploded exploded;
+  expiry_date.UTCExplode(&exploded);
   test::SetCreditCardInfo(&credit_card3, "Clyde", "4111111111111111" /* Visa */,
-                          "04", "2017", "1");
+                          base::StringPrintf("%02d", exploded.month).c_str(),
+                          base::StringPrintf("%04d", exploded.year).c_str(),
+                          "1");
   credit_card3.set_use_date(now - base::TimeDelta::FromDays(400));
 
   // Create a local card expired 400 days ago, and last used 400 days ago.
