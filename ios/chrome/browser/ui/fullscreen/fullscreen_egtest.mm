aaf8c83aadfe8dfea77d5f9b0c35ddb77289bb80
Justin Cohen <justincohen@chromium.org>
2018-05-31 22:38:17 +0000

    Revert "[ios] Fix testLongPDFInitialState for ui refresh."
    This reverts commit a67e4c54dd11361ebd77194b5df1f14d88390c91.
    
    Reason for revert: This should have also been reverted when
    the refresh art was updated (twice) here:
    https://chromium-review.googlesource.com/c/chromium/src/+/1078912
    
    Original change's description:
    > [ios] Fix testLongPDFInitialState for ui refresh.
    >
    > Followup to tab strip refresh.
    >
    > Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    > Change-Id: Iac880a7a687e413badef031451a24c61d855175d
    > Reviewed-on: https://chromium-review.googlesource.com/1050472
    > Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    > Commit-Queue: Justin Cohen <justincohen@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#557054}
    
    TBR=rohitrao@chromium.org,justincohen@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Change-Id: Iaff68fa4d98fbe0c9046c8f988c26eed03988b4d
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Reviewed-on: https://chromium-review.googlesource.com/1080891
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563409}

--- ios/chrome/browser/ui/fullscreen/fullscreen_egtest.mm
+++ ios/chrome/browser/ui/fullscreen/fullscreen_egtest.mm
@@ -94,7 +94,7 @@ void AssertURLIs(const GURL& expectedURL) {
   CGFloat yOffset = 0;
   if (IsUIRefreshPhase1Enabled()) {
     if (IsIPadIdiom()) {
-      yOffset = -104.0;
+      yOffset = -89.0;
     } else {
       yOffset = -48.0;
     }
