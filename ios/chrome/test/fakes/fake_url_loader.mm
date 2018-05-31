39b69ce91069635f8b9f8b4ded4e4d5c9836ff85
Eugene But <eugenebut@google.com>
2018-05-31 21:55:51 +0000

    Pass variation headers for Omnibox Searches.
    Variation headers will only be added to URL requests to Google servers.
    
    Variation headers will be passed for the following navigations:
    - Text search from the Omnibox
    - Back-forward navigation to search pages loaded from omnibox text search
    - Reload of the search page
    
    Headers will *not* be passed for the following navigations:
    - QR code scanner search
    - Voice Search
    - Bookmarks
    - Search by image
    - NTP tiles and articles
    - History navigations
    - Learn More on Incognito
    - Recent Tab navigation
    - Reading List article load
    
    Bug: 831976
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I28be85db0a38b64f933d0b57746b474aeecbf546
    Reviewed-on: https://chromium-review.googlesource.com/1069335
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Peter Lee <pkl@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563386}

--- ios/chrome/test/fakes/fake_url_loader.mm
+++ ios/chrome/test/fakes/fake_url_loader.mm
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @property(nonatomic) ui::PageTransition transition;
 @property(nonatomic) BOOL rendererInitiated;
 @property(nonatomic) BOOL inIncognito;
+@property(nonatomic, copy) NSDictionary* extraHeaders;
 @end
 
 @implementation FakeURLLoader
@@ -24,12 +25,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @synthesize transition = _transition;
 @synthesize rendererInitiated = _rendererInitiated;
 @synthesize inIncognito = _inIncognito;
+@synthesize extraHeaders = _extraHeaders;
 
 - (void)loadURLWithParams:(const web::NavigationManager::WebLoadParams&)params {
   _url = params.url;
   _referrer = params.referrer;
   self.transition = params.transition_type;
   self.rendererInitiated = params.is_renderer_initiated;
+  self.extraHeaders = params.extra_headers;
 }
 
 - (void)webPageOrderedOpen:(const GURL&)url
