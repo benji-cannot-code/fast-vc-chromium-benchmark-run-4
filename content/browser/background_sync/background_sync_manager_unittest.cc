aa1d5296db3edd082b9a693b5d68946aac5eb43e
Han Leon <leon.han@intel.com>
2018-05-31 00:42:54 +0000

    [ServiceWorker] ServiceWorkerContextCore no longer needs to manage SWDispatcherHosts
    After https://chromium-review.googlesource.com/c/chromium/src/+/1068514,
    no code will try to find the specific ServiceWorkerDispatcherHost for a
    given renderer process, so this CL removes the mapping management of
    ServiceWorkerDispatcherHosts inside ServiceWorkerContextCore, in the
    meantime simplifies ServiceWorkerDispatcherHost a little.
    
    BUG=845341
    
    Change-Id: Ie7747846570f64a7a4eda960878bbc0c97042952
    Reviewed-on: https://chromium-review.googlesource.com/1075849
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Commit-Queue: Han Leon <leon.han@intel.com>
    Cr-Commit-Position: refs/heads/master@{#563090}

--- content/browser/background_sync/background_sync_manager_unittest.cc
+++ content/browser/background_sync/background_sync_manager_unittest.cc
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/service_worker/embedded_worker_test_helper.h"
 #include "content/browser/service_worker/service_worker_context_core.h"
 #include "content/browser/service_worker/service_worker_context_wrapper.h"
-#include "content/browser/service_worker/service_worker_dispatcher_host.h"
 #include "content/browser/service_worker/service_worker_registration_object_host.h"
 #include "content/browser/service_worker/service_worker_storage.h"
 #include "content/browser/storage_partition_impl.h"
