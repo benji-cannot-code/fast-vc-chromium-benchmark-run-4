b56643dd9dcc6efdc23490e5938d2d130a3a786a
Lakshmi Kumar Dabbiru <dlkumar@google.com>
2018-06-01 01:52:27 +0000

    iOS GPay changes
    iOS GPay demo -  https://drive.google.com/file/d/1t6tgHIZT-o7GCDC-xPJPyW5ZnZQwMj0u/view?usp=sharing
    
    Playing around
    
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ie1713d3bb27ee50d7b47be16548e304778e0b090
    Reviewed-on: https://chromium-review.googlesource.com/1014620
    Reviewed-by: Mathieu Perreault <mathp@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: Peter Lee <pkl@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Lakshmi Kumar Dabbiru <dlkumar@google.com>
    Cr-Commit-Position: refs/heads/master@{#563481}

--- ios/chrome/browser/autofill/form_suggestion_label.h
+++ ios/chrome/browser/autofill/form_suggestion_label.h
@@ -18,11 +18,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Designated initializer. Initializes with |proposedFrame| and |client| for
 // |suggestion|. Its width will be adjusted according to the length of
 // |suggestion| and width in |proposedFrame| is ignored.
+// |userInteractionEnabled| is a boolean that denotes whether user interaction
+// is enabled on the suggestion.
 - (id)initWithSuggestion:(FormSuggestion*)suggestion
-           proposedFrame:(CGRect)proposedFrame
-                   index:(NSUInteger)index
-          numSuggestions:(NSUInteger)numSuggestions
-                  client:(id<FormSuggestionViewClient>)client;
+             proposedFrame:(CGRect)proposedFrame
+                     index:(NSUInteger)index
+    userInteractionEnabled:(BOOL)userInteractionEnabled
+            numSuggestions:(NSUInteger)numSuggestions
+                    client:(id<FormSuggestionViewClient>)client;
 
 @end
 
