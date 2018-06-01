3baff1d3ac483a2ba3e4b4cd0803cd33bcd2ecf5
Alexei Filippov <alph@chromium.org>
2018-06-01 22:16:13 +0000

    DevTools: Fix performance monitor CPU usage always zero.
    BUG=847360
    
    Change-Id: I8c61e146e979ec8be364c9b688256ecbbdd43efb
    Reviewed-on: https://chromium-review.googlesource.com/1080173
    Commit-Queue: Alexei Filippov <alph@chromium.org>
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563827}

--- third_party/WebKit/LayoutTests/inspector-protocol/performance/perf-metrics.js
+++ third_party/WebKit/LayoutTests/inspector-protocol/performance/perf-metrics.js
@@ -7,6 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   await dumpMetrics();
   await dp.Performance.enable();
   await dumpMetrics();
+  const taskTime1 = (await retrieveMetrics()).get('TaskDuration');
+  const taskTime2 = (await retrieveMetrics()).get('TaskDuration');
+  if (taskTime1 >= taskTime2)
+    testRunner.log(`Error: Metric TaskDuration should be monotonically increasing: ${taskTime1} ${taskTime2}.`);
   await dp.Performance.disable();
   await dumpMetrics();
 
