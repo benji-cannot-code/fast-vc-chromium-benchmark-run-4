5f6c2a1ad3cc94e67e6d81385bc2a0fd21ffb36e
Mike Meade <mmeade@chromium.org>
2018-06-01 13:00:02 +0000

    Adding support for running the Windows installer tests manually.
    This cl adds an easy way to zip all python packages needed to run
    these tests manually, and offers an easy to distribute package.
    When distributing the binaries they can be placed in the zip file
    to be distributed.
    
    Note this does not zip the installers or chromedriver. Those need to be
    acquired externally and added to the zip file to pass out.
    
    Change-Id: I0129e30f3f382ee7e2e1f7cef5e951d867b8fb39
    Reviewed-on: https://chromium-review.googlesource.com/1066791
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: James Meade <mmeade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563602}

---
+++ chrome/test/mini_installer/zip_test_runner.bat
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+@echo off
+
+rem This batch file installs all Python packages needed for the test
+rem and automatically runs the test. All args passed into this batch file
+rem will be passed to test_installer.py. See
+rem chrome\test\mini_installer\test_installer.py for the args that can be
+rem passed.
+
+c:\Python27\python -m pip install --upgrade pip
+c:\Python27\Scripts\pip install psutil
+c:\Python27\Scripts\pip install pywin32
+c:\Python27\python chrome\test\mini_installer\test_installer.py {run_args} %*
+pause
