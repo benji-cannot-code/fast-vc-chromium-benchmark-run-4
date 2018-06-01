6db848ba2f5c6be1c02f5455fbc456fff515dd1c
Jeremy Roman <jbroman@chromium.org>
2018-06-01 16:01:56 +0000

    wtf: Squash wtf/text/icu/ into wtf/text/.
    Blink uses ICU everywhere, so the abstraction to allow for other Unicode
    backends is no longer needed.
    
    Change-Id: Iffa160bfbded0658f321cb781b607fe3f3af040f
    Reviewed-on: https://chromium-review.googlesource.com/1074732
    Reviewed-by: Yuta Kitamura <yutak@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563657}

--- third_party/blink/renderer/core/dom/first_letter_pseudo_element.cc
+++ third_party/blink/renderer/core/dom/first_letter_pseudo_element.cc
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/layout_text.h"
 #include "third_party/blink/renderer/core/layout/layout_text_fragment.h"
 #include "third_party/blink/renderer/platform/text/text_break_iterator.h"
-#include "third_party/blink/renderer/platform/wtf/text/icu/unicode_icu.h"
+#include "third_party/blink/renderer/platform/wtf/text/unicode.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
 namespace blink {
