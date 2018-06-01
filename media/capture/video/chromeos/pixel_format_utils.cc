fa37e1a27351f25eac48aefcd3c6c1cc8c68f7d3
Robert Kroeger <rjkroege@chromium.org>
2018-06-01 20:50:13 +0000

    [ozone] Support compiling media capture on Linux
    Media capture code does not search first in third_party for its
    dependency on libdrm. As a result, it could not compile on
    standard desktop Linux.
    
    Bug: 848856
    Change-Id: Ifd38f4e463fccc31f896024d48de2a456ec0b1eb
    Reviewed-on: https://chromium-review.googlesource.com/1082820
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Commit-Queue: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563784}

--- media/capture/video/chromeos/pixel_format_utils.cc
+++ media/capture/video/chromeos/pixel_format_utils.cc
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/capture/video/chromeos/pixel_format_utils.h"
 
-#include <libdrm/drm_fourcc.h>
+#include <drm_fourcc.h>
 
 namespace media {
 
