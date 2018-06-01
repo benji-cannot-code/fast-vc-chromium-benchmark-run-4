1f08c52796210b7095f6019b702202893425a224
Yoshifumi Inoue <yosin@chromium.org>
2018-06-01 12:49:37 +0000

    Introduce TextSegments class with Finder interface
    This patch introduces |TextSegments| class with |Finder| interface as
    replacement of |FindBoundaryForwrad()| and |FindBoundaryBackward()| along with
    call sites, to implementing sentence boundary function with state instead of
    |std::function<>|.
    
    This patch also changes |TextOffsetMapping::GetPositionAfter()| to return
    correct value.
    
    This patch is a preparation of the patch[1].
    
    [1] http://crrev.com/c/1075918 Make NextSentencePosition() to utilize
    TextOffsetMapping
    
    Bug: 778507, 810579
    Change-Id: I56c6d08589740709cfe1a4d7281274f2f56fbac6
    Reviewed-on: https://chromium-review.googlesource.com/1078167
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563599}

--- third_party/blink/renderer/core/editing/visible_units.cc
+++ third_party/blink/renderer/core/editing/visible_units.cc
@@ -46,7 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/editing/position_iterator.h"
 #include "third_party/blink/renderer/core/editing/position_with_affinity.h"
 #include "third_party/blink/renderer/core/editing/text_affinity.h"
-#include "third_party/blink/renderer/core/editing/text_offset_mapping.h"
 #include "third_party/blink/renderer/core/editing/visible_position.h"
 #include "third_party/blink/renderer/core/editing/visible_selection.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
@@ -557,26 +556,6 @@ PositionInFlatTree PreviousBoundary(
     const VisiblePositionInFlatTree& visible_position,
     BoundarySearchFunction search_function) {
   return PreviousBoundaryAlgorithm(visible_position, search_function);
-}
-
-// Note: This function is used for finding word and sentence boundary.
-PositionInFlatTree FindBoundaryForward(
-    const PositionInFlatTree& position,
-    std::function<int(const UChar*, int length, int offset)> find_forward) {
-  DCHECK(position.IsNotNull());
-  PositionInFlatTree last_position = position;
-  for (const auto& inline_contents :
-       TextOffsetMapping::ForwardRangeOf(position)) {
-    const TextOffsetMapping mapping(inline_contents);
-    const String text = mapping.GetText();
-    const int offset =
-        last_position == position ? mapping.ComputeTextOffset(position) : 0;
-    const int result = find_forward(text.Characters16(), text.length(), offset);
-    if (offset < result)
-      return mapping.GetPositionAfter(result);
-    last_position = mapping.GetRange().EndPosition();
-  }
-  return last_position;
 }
 
 // ---------
