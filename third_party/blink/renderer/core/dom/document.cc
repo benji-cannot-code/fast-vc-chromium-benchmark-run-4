0e191055f996ac67aac544af0cc2de45e7ef1d67
Ella Ge <eirage@chromium.org>
2018-06-01 02:12:50 +0000

    remove createTouchList
    This Cl removes document.createTouchList and related tests.
    
    Use count is drop to a safer value, ready to remove in M69.
    UseCounter:
    https://www.chromestatus.com/metrics/feature/timeline/popularity/2368
    
    intent to deprecate and Remove:
    https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/GLbUpUUnQzc
    
    PR to touch-events spec:
    https://github.com/w3c/touch-events/pull/96
    
    Bug: 518868
    Change-Id: Ia4451d0d32bf5bbb6d9c9c543ac77187c5161b42
    Reviewed-on: https://chromium-review.googlesource.com/1055799
    Reviewed-by: Navid Zolghadr <nzolghadr@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Rick Byers <rbyers@chromium.org>
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563490}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -6685,10 +6685,6 @@ void Document::CancelIdleCallback(int id) {
   scripted_idle_task_controller_->CancelCallback(id);
 }
 
-TouchList* Document::createTouchList(HeapVector<Member<Touch>>& touches) const {
-  return TouchList::Adopt(touches);
-}
-
 DocumentLoader* Document::Loader() const {
   if (!frame_)
     return nullptr;
