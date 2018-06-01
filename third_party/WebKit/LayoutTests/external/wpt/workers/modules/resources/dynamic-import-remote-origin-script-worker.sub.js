a937f75882603c5b5867322459f1ae8cc6df7349
Hiroki Nakagawa <nhiroki@chromium.org>
2018-06-01 04:19:06 +0000

    Worker: Add CSP tests for ES Modules on dedicated workers
    Bug: 680046
    Change-Id: I066d4b7750bbec00397466daac48e9d0a2ba70c0
    Reviewed-on: https://chromium-review.googlesource.com/1078947
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563531}

---
+++ third_party/WebKit/LayoutTests/external/wpt/workers/modules/resources/dynamic-import-remote-origin-script-worker.sub.js
@@ -0,0 +1,5 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Import a remote origin script.
+import('https://{{domains[www1]}}:{{ports[https][0]}}/workers/modules/resources/export-on-load-script.js')
+  .then(module => postMessage(module.importedModules))
+  .catch(e => postMessage(['ERROR']));
