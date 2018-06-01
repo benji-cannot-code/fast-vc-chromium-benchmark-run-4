7de600658c19479fc7cfe6286643b909f0b63dca
Esmael El-Moslimany <aee@chromium.org>
2018-06-01 05:17:37 +0000

    WebUI: cr-dialog, general fix for the body vertical scrollbars with paper-input
    Bug: 847352
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I8dc6f7bba2c5e265e8c60c85bfcdc44bad36e480
    Reviewed-on: https://chromium-review.googlesource.com/1080208
    Commit-Queue: Esmael El-Moslimany <aee@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563541}

--- chrome/browser/resources/md_bookmarks/edit_dialog.js
+++ chrome/browser/resources/md_bookmarks/edit_dialog.js
@@ -139,11 +139,4 @@ Polymer({
   onCancelButtonTap_: function() {
     this.$.dialog.cancel();
   },
-
-  /** @private */
-  getHeightOfBody_: function() {
-    const heightTwoInputs = 136;
-    const heightOneInput = 62;
-    return this.isFolder_ ? heightOneInput : heightTwoInputs;
-  },
 });
