9477dc449fbcb26f33135fdb255265204dffcfb1
Scott Chen <scottchen@chromium.org>
2018-06-01 02:07:40 +0000

    WebUI[MD-refresh]: Add [type="password"] and ["readonly"] to cr-input
    Bug: 832177
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: If05ffdfc80374c79dec5bbb1422ff44a4489818d
    Reviewed-on: https://chromium-review.googlesource.com/1070861
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Scott Chen <scottchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563488}

--- ui/webui/resources/cr_elements/cr_input/cr_input.js
+++ ui/webui/resources/cr_elements/cr_input/cr_input.js
@@ -44,7 +44,14 @@ Polymer({
       observer: 'placeholderChanged_',
     },
 
+    readonly: Boolean,
+
     tabIndex: String,
+
+    type: {
+      type: String,
+      value: 'text',  // Only 'text' and 'password' are currently supported.
+    },
 
     value: {
       type: String,
@@ -62,10 +69,16 @@ Polymer({
     'input.change': 'onInputChange_',
   },
 
+  /** @return {!HTMLInputElement} */
+  get inputElement() {
+    return this.$.input;
+  },
+
   /** @private */
   disabledChanged_: function() {
     this.setAttribute('aria-disabled', this.disabled ? 'true' : 'false');
-    this.$.input.blur();  // In case input was focused when disabled changes.
+    // In case input was focused when disabled changes.
+    this.inputElement.blur();
   },
 
   /**
@@ -76,9 +89,14 @@ Polymer({
    */
   placeholderChanged_: function() {
     if (this.placeholder || this.placeholder == '')
-      this.$.input.setAttribute('placeholder', this.placeholder);
+      this.inputElement.setAttribute('placeholder', this.placeholder);
     else
-      this.$.input.removeAttribute('placeholder');
+      this.inputElement.removeAttribute('placeholder');
+  },
+
+  focus: function() {
+    if (this.shadowRoot.activeElement != this.inputElement)
+      this.inputElement.focus();
   },
 
   /**
@@ -90,5 +108,5 @@ Polymer({
    */
   onInputChange_: function(e) {
     this.fire('change', {sourceEvent: e});
-  }
+  },
 });
