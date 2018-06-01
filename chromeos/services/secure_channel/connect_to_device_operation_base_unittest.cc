efa878d9be491a114a9bab88435c6fa8316463e7
Ryan Hansberry <hansberry@chromium.org>
2018-06-01 18:47:59 +0000

    [CrOS Multidevice] SecureChannel: Migrate AuthenticatedChannel to public/cpp.
    This move is necessary because both the service and clients of it depend on it.
    
    Bug: 824568, 752273
    Change-Id: Ie0996c6ba26fa051de4759c633744bd60d56b011
    Reviewed-on: https://chromium-review.googlesource.com/1082660
    Commit-Queue: Ryan Hansberry <hansberry@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563737}

--- chromeos/services/secure_channel/connect_to_device_operation_base_unittest.cc
+++ chromeos/services/secure_channel/connect_to_device_operation_base_unittest.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/test/scoped_task_environment.h"
 #include "base/test/test_simple_task_runner.h"
-#include "chromeos/services/secure_channel/fake_authenticated_channel.h"
+#include "chromeos/services/secure_channel/public/cpp/shared/fake_authenticated_channel.h"
 #include "components/cryptauth/remote_device_test_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
--- chromeos/services/secure_channel/connection_attempt_base_unittest.cc
+++ chromeos/services/secure_channel/connection_attempt_base_unittest.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/test_simple_task_runner.h"
 #include "chromeos/services/secure_channel/connection_details.h"
 #include "chromeos/services/secure_channel/connection_medium.h"
-#include "chromeos/services/secure_channel/fake_authenticated_channel.h"
 #include "chromeos/services/secure_channel/fake_client_connection_parameters.h"
 #include "chromeos/services/secure_channel/fake_connect_to_device_operation.h"
 #include "chromeos/services/secure_channel/fake_connect_to_device_operation_factory.h"
@@ -21,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/services/secure_channel/fake_connection_delegate.h"
 #include "chromeos/services/secure_channel/fake_pending_connection_request.h"
 #include "chromeos/services/secure_channel/pending_connection_request_delegate.h"
+#include "chromeos/services/secure_channel/public/cpp/shared/fake_authenticated_channel.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace chromeos {
