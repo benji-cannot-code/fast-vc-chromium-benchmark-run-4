06249550854f9881f0479f95cce36d05e44187eb
Xi Cheng <chengx@chromium.org>
2018-06-01 02:27:44 +0000

    Replace arraysize with base::size in native_stack_sampler_win.cc
    ... as the former is deprecated.
    
    Bug: 837308
    Change-Id: Iace3a07b4ba4d6c0e0e441ab07add5a3d701ba26
    Reviewed-on: https://chromium-review.googlesource.com/1081513
    Commit-Queue: Xi Cheng <chengx@chromium.org>
    Commit-Queue: Mike Wittman <wittman@chromium.org>
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563496}

--- base/profiler/native_stack_sampler_win.cc
+++ base/profiler/native_stack_sampler_win.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
 #include "base/profiler/win32_stack_frame_unwinder.h"
+#include "base/stl_util.h"
 #include "base/strings/string16.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
@@ -140,7 +141,7 @@ void RewritePointersToStackMemory(uintptr_t top, uintptr_t bottom,
   };
 
   // Rewrite pointers in the context.
-  for (size_t i = 0; i < arraysize(nonvolatile_registers); ++i) {
+  for (size_t i = 0; i < base::size(nonvolatile_registers); ++i) {
     DWORD64* const reg = &(context->*nonvolatile_registers[i]);
     RewritePointerIfInOriginalStack(top, bottom, stack_copy,
                                     reinterpret_cast<const void**>(reg));
@@ -483,7 +484,7 @@ bool NativeStackSamplerWin::GetModuleForHandle(
     StackSamplingProfiler::Module* module) {
   wchar_t module_name[MAX_PATH];
   DWORD result_length =
-      ::GetModuleFileName(module_handle, module_name, arraysize(module_name));
+      ::GetModuleFileName(module_handle, module_name, base::size(module_name));
   if (result_length == 0)
     return false;
 
