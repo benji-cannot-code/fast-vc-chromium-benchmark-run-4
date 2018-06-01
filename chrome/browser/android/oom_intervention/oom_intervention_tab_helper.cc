bc76f6809c3eb9e2b7e7612ccfebf204f1890c35
Siddhartha <ssid@chromium.org>
2018-06-01 02:16:24 +0000

    OomIntervantion should observe CrashDumpManager instead of OOMReporter
    The reporter does not send notifications when the tab has multiple urls
    or when sync is not enabled since ukm cannot be recorded.
    
    BUG=843419
    
    Change-Id: Ie05f642fba60a6abfa1e7a4310f8e8bb01fa7a76
    Reviewed-on: https://chromium-review.googlesource.com/1081612
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563491}

--- chrome/browser/android/oom_intervention/oom_intervention_tab_helper.cc
+++ chrome/browser/android/oom_intervention/oom_intervention_tab_helper.cc
@@ -71,8 +71,9 @@ OomInterventionTabHelper::OomInterventionTabHelper(
       decider_(OomInterventionDecider::GetForBrowserContext(
           web_contents->GetBrowserContext())),
       binding_(this),
+      scoped_observer_(this),
       weak_ptr_factory_(this) {
-  OutOfMemoryReporter::FromWebContents(web_contents)->AddObserver(this);
+  scoped_observer_.Add(breakpad::CrashDumpManager::GetInstance());
   shared_metrics_buffer_ = base::UnsafeSharedMemoryRegion::Create(
       sizeof(blink::OomInterventionMetrics));
   metrics_mapping_ = shared_metrics_buffer_.Map();
@@ -111,7 +112,6 @@ void OomInterventionTabHelper::DeclineInterventionSticky() {
 }
 
 void OomInterventionTabHelper::WebContentsDestroyed() {
-  OutOfMemoryReporter::FromWebContents(web_contents())->RemoveObserver(this);
   StopMonitoring();
 }
 
@@ -197,9 +197,14 @@ void OomInterventionTabHelper::OnVisibilityChanged(
   }
 }
 
-void OomInterventionTabHelper::OnForegroundOOMDetected(
-    const GURL& url,
-    ukm::SourceId source_id) {
+void OomInterventionTabHelper::OnCrashDumpProcessed(
+    const breakpad::CrashDumpManager::CrashDumpDetails& details) {
+  if (details.process_host_id !=
+      web_contents()->GetMainFrame()->GetProcess()->GetID())
+    return;
+  if (!breakpad::CrashDumpManager::IsForegroundOom(details))
+    return;
+
   DCHECK(IsLastVisibleWebContents(web_contents()));
   if (near_oom_detected_time_) {
     base::TimeDelta elapsed_time =
