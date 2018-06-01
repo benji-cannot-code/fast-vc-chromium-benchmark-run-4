8ea20b85a01cf2660925c1dad51af74e9509cb16
Ryan Landay <rlanday@chromium.org>
2018-06-01 22:49:56 +0000

    Fix thumbnail rendering prioritization in Android horizontal tab switcher
    StackLayoutBase and StackTab contain logic to prioritize which thumbnails are
    most important to render in the Android tab switcher. Thumbnails deemed less
    important are rendered using lower-resolution thumbnails, or dropped entirely.
    
    The logic had not been updated for the horizontal tab switcher experiment. This
    caused two problems:
    
    - The current logic for determining the currently centered tab index does not
      work properly if we're centered between two tabs (it looks up which tab the
      center of the tab switcher corresponds to, and produces incorrect behavior if
      there's no tab there). So this logic needs to be pulled out from Stack into
      its subclasses so we can implement it correctly for each one.
    
    - The VisibilityComparator logic was designed for the overlapping tab switcher
      and doesn't work properly for the non-overlapping one. In particular, the only
      tab capable of being recognized as not fully on-screen in the horizontal tab
      switcher is the last one. This means that if the last tab is not fully
      on-screen, it is treated as having the lowest priority, which is not correct.
      The fix for this is to alwyas use the OrderComparator for the non-overlapping
      tab switcher.
    
    Bug: 846205
    Change-Id: I107f6d3648a1aaec23ced4cd5f714a265f4a3ad5
    Reviewed-on: https://chromium-review.googlesource.com/1080986
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Ryan Landay <rlanday@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563842}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/phone/stack/NonOverlappingStack.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/phone/stack/NonOverlappingStack.java
@@ -206,6 +206,11 @@ public class NonOverlappingStack extends Stack {
     }
 
     @Override
+    protected int computeReferenceIndex() {
+        return -Math.round(mScrollTarget / mSpacing);
+    }
+
+    @Override
     protected boolean shouldCloseGapsBetweenTabs() {
         return false;
     }
