32d2cbf2100457b62f9148cf8f55cdd00769644a
edchin <edchin@chromium.org>
2018-06-02 01:53:42 +0000

    [ios] Make recent tabs able to hide recently closed
    Recent tabs currently displays recently closed as well as remote
    sessions. This CL makes the view controller flexible to be able to hide
    recently closed tabs.
    
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I28348fb30bd274fc1344512398541c6f6cc39503
    Reviewed-on: https://chromium-review.googlesource.com/1083931
    Commit-Queue: edchin <edchin@chromium.org>
    Reviewed-by: edchin <edchin@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563912}

--- ios/chrome/browser/ui/tab_grid/tab_grid_view_controller.mm
+++ ios/chrome/browser/ui/tab_grid/tab_grid_view_controller.mm
@@ -109,7 +109,8 @@ NSUInteger GetPageIndexFromPage(TabGridPage page) {
   if (self = [super init]) {
     _regularTabsViewController = [[GridViewController alloc] init];
     _incognitoTabsViewController = [[GridViewController alloc] init];
-    _remoteTabsViewController = [[RecentTabsTableViewController alloc] init];
+    _remoteTabsViewController = [[RecentTabsTableViewController alloc]
+        initWithoutRecentlyClosedSection];
   }
   return self;
 }
