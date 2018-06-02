df4887b305888d22f8600deae0680bcf4efe1640
Jinho Bang <jinho.bang@samsung.com>
2018-06-02 01:33:53 +0000

    PaymentHandler: Fix a crash when creating event object with empty data
    The following codes cause crashes in current implementation.
      - new CanMakePaymentEvent("canmakepaymentevent", {});
      - new PaymentRequestEvent("paymentrequestevent", {});
    
    This patch fixes the problem and fixes/adds WPT tests.
    
    Bug: none
    Change-Id: I95375f1148d69a17075d393990e3726ad2bf1cf3
    Reviewed-on: https://chromium-review.googlesource.com/1070957
    Commit-Queue: Jinho Bang <jinho.bang@samsung.com>
    Reviewed-by: Ganggui Tang <gogerald@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563903}

--- third_party/WebKit/LayoutTests/external/wpt/payment-handler/payment-request-event-constructor.https.worker.js
+++ third_party/WebKit/LayoutTests/external/wpt/payment-handler/payment-request-event-constructor.https.worker.js
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 importScripts('/resources/testharness.js');
 
 test(() => {
-  assert_false('CanMakePaymentEvent' in self);
-}, 'CanMakePaymentEvent constructor must not be exposed in worker');
+  assert_false('PaymentRequestEvent' in self);
+}, 'PaymentRequestEvent constructor must not be exposed in worker');
 
 done();
