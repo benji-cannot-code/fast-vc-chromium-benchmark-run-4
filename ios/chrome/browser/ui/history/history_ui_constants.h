ad4727a95cf68fad1f0ba9ad2f192dc7ea8469f3
Rohit Rao <rohitrao@chromium.org>
2018-06-03 22:20:20 +0000

    [ios] Fixes ui_egtests to scroll the share menu if needed.
    When the UIRefresh flag is enabled, additional actions are added to the
    share menu.  These actions end up pushing the Print action off of the
    first screenful of items, so tests must be updated to scroll the
    collection view if needed.
    
    Also converts a History egtest to use a constant for an a11y identifier
    and to avoid inadvertently dismissing the history ui.
    
    BUG=825431
    
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I1efebb04d1de54dcef7c2c7d3374eff2c61c9838
    Reviewed-on: https://chromium-review.googlesource.com/1083723
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563978}

--- ios/chrome/browser/ui/history/history_ui_constants.h
+++ ios/chrome/browser/ui/history/history_ui_constants.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 
+// Accessibility identifier of the search controller search bar.
+extern NSString* const kHistorySearchControllerSearchBarIdentifier;
 // Accessibility identifier of the navigation controller done button.
 extern NSString* const kHistoryNavigationControllerDoneButtonIdentifier;
 // Accessibility identifier of the delete toolbar button.
