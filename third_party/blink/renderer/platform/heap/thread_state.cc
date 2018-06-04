e66915e7d26c2cbef9c5fc2c292bc7cdef3ed02f
Keishi Hattori <keishi@chromium.org>
2018-06-04 13:09:20 +0000

    Oilpan: Avoid incremental idle GC getting scheduled while incremental marking
    Bug is caused because V8GCController schedules an idle GC while incremental marking is in progress.
    
    Avoid starting an idle GC while marking is in progress.
    
    Bug: 849181
    Change-Id: Ic5fc7eac2ee92e53cdfc400af056d7399c6b99fa
    Reviewed-on: https://chromium-review.googlesource.com/1084523
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564072}

--- third_party/blink/renderer/platform/heap/thread_state.cc
+++ third_party/blink/renderer/platform/heap/thread_state.cc
@@ -733,6 +733,8 @@ void ThreadState::ScheduleIncrementalMarkingFinalize() {
 }
 
 void ThreadState::ScheduleIdleGC() {
+  if (GCState() != kNoGCScheduled)
+    return;
   SetGCState(kIdleGCScheduled);
   if (IsSweepingInProgress())
     return;
@@ -816,13 +818,20 @@ void ThreadState::SetGCState(GCState gc_state) {
       // These GCs should not be scheduled while sweeping is in progress.
       DCHECK(!IsSweepingInProgress());
       FALLTHROUGH;
-    case kIdleGCScheduled:
     case kPreciseGCScheduled:
       DCHECK(CheckThread());
       VERIFY_STATE_TRANSITION(
           gc_state_ == kNoGCScheduled || gc_state_ == kIdleGCScheduled ||
           gc_state_ == kIncrementalMarkingStepScheduled ||
           gc_state_ == kIncrementalMarkingFinalizeScheduled ||
+          gc_state_ == kPreciseGCScheduled || gc_state_ == kFullGCScheduled ||
+          gc_state_ == kPageNavigationGCScheduled);
+      CompleteSweep();
+      break;
+    case kIdleGCScheduled:
+      DCHECK(CheckThread());
+      VERIFY_STATE_TRANSITION(
+          gc_state_ == kNoGCScheduled || gc_state_ == kIdleGCScheduled ||
           gc_state_ == kPreciseGCScheduled || gc_state_ == kFullGCScheduled ||
           gc_state_ == kPageNavigationGCScheduled);
       CompleteSweep();
@@ -1342,6 +1351,7 @@ void ThreadState::RunIncrementalMarkingFinalizeTask() {
 void ThreadState::IncrementalMarkingStart(BlinkGC::GCReason reason) {
   VLOG(2) << "[state:" << this << "] "
           << "IncrementalMarking: Start";
+  DCHECK(!IsMarkingInProgress());
   CompleteSweep();
   Heap().stats_collector()->Start(reason);
   {
