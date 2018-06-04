7503429e5ac22cfbd33429714bb1809581a9efc9
Elly Fong-Jones <ellyjones@chromium.org>
2018-06-04 14:31:40 +0000

    macviews: use oval focus rings for ToolbarButtons
    Bug: 848299
    Change-Id: I6921e27689b9f80d027e6f1984f9aa173d2f549a
    Reviewed-on: https://chromium-review.googlesource.com/1081049
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564083}

--- ui/views/controls/button/button.h
+++ ui/views/controls/button/button.h
@@ -248,6 +248,8 @@ class VIEWS_EXPORT Button : public InkDropHostView,
     return hover_animation_;
   }
 
+  FocusRing* focus_ring() { return focus_ring_.get(); }
+
   // The button's listener. Notified when clicked.
   ButtonListener* listener_;
 
