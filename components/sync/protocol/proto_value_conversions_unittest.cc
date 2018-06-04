65725c34333b0b7b5a33157a3920990d3266424f
vitaliii <vitaliii@chromium.org>
2018-06-04 08:09:53 +0000

    [Sync] Add USER_CONSENTS datatype without enabling it yet.
    Add USER_CONSENTS sync datatype without actually enabling and wiring it
    (i.e. nothing creates it yet).
    
    This is the second change in a series to split user consents from user
    events into a separate datatype.
    
    Bug: 840357
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I92ff38b6c7957d6a5bc366ee01e65bc4ca55c679
    Reviewed-on: https://chromium-review.googlesource.com/1078440
    Commit-Queue: vitaliii <vitaliii@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Gayane Petrosyan <gayane@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564015}

--- components/sync/protocol/proto_value_conversions_unittest.cc
+++ components/sync/protocol/proto_value_conversions_unittest.cc
@@ -56,7 +56,7 @@ class ProtoValueConversionsTest : public testing::Test {
   }
 };
 
-static_assert(41 == syncer::MODEL_TYPE_COUNT,
+static_assert(42 == syncer::MODEL_TYPE_COUNT,
               "When adding a new type, add a unit test for "
               "{NewType}SpecificsToValue below.");
 
@@ -362,7 +362,7 @@ TEST_F(ProtoValueConversionsTest, WifiCredentialSpecificsToValue) {
 // TODO(akalin): Figure out how to better test EntitySpecificsToValue.
 
 TEST_F(ProtoValueConversionsTest, EntitySpecificsToValue) {
-  static_assert(41 == syncer::MODEL_TYPE_COUNT,
+  static_assert(42 == syncer::MODEL_TYPE_COUNT,
                 "When adding a new type, add its field below.");
 
   sync_pb::EntitySpecifics specifics;
@@ -405,6 +405,7 @@ TEST_F(ProtoValueConversionsTest, EntitySpecificsToValue) {
   SET_FIELD(synced_notification_app_info);
   SET_FIELD(theme);
   SET_FIELD(typed_url);
+  SET_FIELD(user_consent);
   SET_FIELD(user_event);
   SET_FIELD(wallet_metadata);
   SET_FIELD(wifi_credential);
