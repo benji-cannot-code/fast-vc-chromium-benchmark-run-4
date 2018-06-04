36cad962f5057db9d214f0229cd8be001081752b
Devlin Cronin <rdevlin.cronin@chromium.org>
2018-06-04 20:48:47 +0000

    [Extensions Bindings] Provide a better error for StorageArea invocations
    Provide a better error when a developer invokes a storage area method
    incorrectly. Add a unittest for the same.
    
    Bug: 848904
    Change-Id: If3c27d29d35bebb710318333937deecf954c5cbc
    Reviewed-on: https://chromium-review.googlesource.com/1085486
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564224}

--- extensions/renderer/storage_area.cc
+++ extensions/renderer/storage_area.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "extensions/common/api/storage.h"
 #include "extensions/renderer/bindings/api_binding_util.h"
+#include "extensions/renderer/bindings/api_invocation_errors.h"
 #include "extensions/renderer/bindings/api_request_handler.h"
 #include "extensions/renderer/bindings/api_signature.h"
 #include "extensions/renderer/bindings/api_type_reference_map.h"
@@ -214,7 +215,8 @@ void StorageArea::HandleFunctionCall(const std::string& method_name,
   if (!signature->ParseArgumentsToJSON(context, argument_list, *type_refs_,
                                        &converted_arguments, &callback,
                                        &error)) {
-    arguments->ThrowTypeError("Invalid invocation");
+    arguments->ThrowTypeError(api_errors::InvocationError(
+        full_method_name, signature->GetExpectedSignature(), error));
     return;
   }
 
