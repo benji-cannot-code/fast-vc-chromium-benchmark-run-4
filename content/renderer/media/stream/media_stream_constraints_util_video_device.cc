e718d8c0111e4a7bf757b9421e56c42e1656d1aa
Guido Urdaneta <guidou@chromium.org>
2018-06-04 10:07:00 +0000

    Do not allow the empty string as a facingMode constraint value for MediaStreams.
    The empty string is an invalid facingMode value that was being
    interpreted to be equivalent to the internal facing-mode value of NONE.
    
    Bug: 814309
    Change-Id: I260fbc41fd17a8bdcd1ca63baffe3bed14f3c4ab
    Reviewed-on: https://chromium-review.googlesource.com/1072471
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564046}

--- content/renderer/media/stream/media_stream_constraints_util_video_device.cc
+++ content/renderer/media/stream/media_stream_constraints_util_video_device.cc
@@ -41,7 +41,7 @@ blink::WebString ToWebString(media::VideoFacingMode facing_mode) {
     case media::MEDIA_VIDEO_FACING_ENVIRONMENT:
       return blink::WebString::FromASCII("environment");
     default:
-      return blink::WebString::FromASCII("");
+      return blink::WebString();
   }
 }
 
@@ -240,6 +240,25 @@ double StringConstraintSourceDistance(const blink::WebString& value,
   if (failed_constraint_name)
     *failed_constraint_name = constraint.GetName();
   return HUGE_VAL;
+}
+
+double FacingModeConstraintSourceDistance(
+    media::VideoFacingMode value,
+    const blink::StringConstraint& facing_mode_constraint,
+    const char** failed_constraint_name) {
+  blink::WebString string_value = ToWebString(value);
+  if (string_value.IsNull()) {
+    if (facing_mode_constraint.Exact().empty())
+      return 0.0;
+
+    if (failed_constraint_name)
+      *failed_constraint_name = facing_mode_constraint.GetName();
+    return HUGE_VAL;
+  }
+
+  double ret = StringConstraintSourceDistance(
+      string_value, facing_mode_constraint, failed_constraint_name);
+  return ret;
 }
 
 // Returns a custom distance between a source screen dimension and |constraint|.
@@ -447,9 +466,8 @@ double DeviceSourceDistance(
          StringConstraintSourceDistance(blink::WebString::FromASCII(group_id),
                                         constraint_set.group_id,
                                         failed_constraint_name) +
-         StringConstraintSourceDistance(ToWebString(facing_mode),
-                                        constraint_set.facing_mode,
-                                        failed_constraint_name);
+         FacingModeConstraintSourceDistance(
+             facing_mode, constraint_set.facing_mode, failed_constraint_name);
 }
 
 // Returns a custom distance between |constraint_set| and |format| given that
