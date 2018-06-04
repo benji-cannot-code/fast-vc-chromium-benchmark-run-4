ce4d9fb45c95e77e91c20acc0de17e4bf306e7a7
dpapad <dpapad@chromium.org>
2018-06-04 20:20:38 +0000

    WebUI Polymer 2 migration: Remove settings-idle-load type extension.
    This CL updates settings-idle-load for compatibility with Polymer 2, by
    making it wrap a <template> instead of extending one.
    
    Bug: 818279
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I122675b884ef3a9bfcf0fbc053cceddbb75cbfcf
    Reviewed-on: https://chromium-review.googlesource.com/1081811
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564213}

--- chrome/browser/resources/settings/controls/settings_idle_load.js
+++ chrome/browser/resources/settings/controls/settings_idle_load.js
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 Polymer({
   is: 'settings-idle-load',
-  extends: 'template',
 
   behaviors: [Polymer.Templatizer],
 
@@ -23,7 +22,7 @@ Polymer({
     url: String,
   },
 
-  /** @private {TemplatizerNode} */
+  /** @private {?Element} */
   child_: null,
 
   /** @private {number} */
@@ -51,7 +50,7 @@ Polymer({
     this.loading_ = new Promise((resolve, reject) => {
       this.importHref(this.url, () => {
         assert(!this.ctor);
-        this.templatize(this);
+        this.templatize(this.getContentChildren()[0]);
         assert(this.ctor);
 
         const instance = this.stamp({});
