586b5d4629fa02a05278a05a2e330f019e21b638
Kent Tamura <tkent@chromium.org>
2018-06-04 08:30:16 +0000

    Move ::doubleToInteger() from math_extras.h to v8_binding_for_core.h
    Also fix style errors.
    - Rename it to DoubleToInteger
    - It returns the result value, instead of the out-argument.
    - Rename local varaibles for the current Blink coding style
    
    Change-Id: If0975d00b10d5c230cef6ecd4a0093bc6432f603
    Reviewed-on: https://chromium-review.googlesource.com/1084145
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564021}

--- third_party/blink/renderer/bindings/core/v8/v8_binding_for_core.cc
+++ third_party/blink/renderer/bindings/core/v8/v8_binding_for_core.cc
@@ -413,13 +413,7 @@ int64_t ToInt64Slow(v8::Isolate* isolate,
                         "long long", exception_state);
   }
 
-  if (std::isnan(number_value) || std::isinf(number_value))
-    return 0;
-
-  // NaNs and +/-Infinity should be 0, otherwise modulo 2^64.
-  unsigned long long integer;
-  doubleToInteger(number_value, integer);
-  return integer;
+  return DoubleToInteger(number_value);
 }
 
 uint64_t ToUInt64Slow(v8::Isolate* isolate,
@@ -463,13 +457,7 @@ uint64_t ToUInt64Slow(v8::Isolate* isolate,
   if (configuration == kClamp)
     return clampTo<uint64_t>(number_value);
 
-  if (std::isinf(number_value))
-    return 0;
-
-  // NaNs and +/-Infinity should be 0, otherwise modulo 2^64.
-  unsigned long long integer;
-  doubleToInteger(number_value, integer);
-  return integer;
+  return DoubleToInteger(number_value);
 }
 
 float ToRestrictedFloat(v8::Isolate* isolate,
