d213b3b93869ddc816802c6bd76072fddde9800b
Joel Einbinder <einbinder@chromium.org>
2018-06-04 23:45:26 +0000

    DevTools: Fix UI.Fragments for text fragment arguments
    When a fragment is not wrapped in any element, it should be
    treated as an element or text node and not an attribute value.
    
    Change-Id: Ife39f9e66dcfe27931af0f7bffaddd9a09b9bfcc
    Reviewed-on: https://chromium-review.googlesource.com/1083837
    Commit-Queue: Joel Einbinder <einbinder@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564298}

--- third_party/WebKit/LayoutTests/http/tests/devtools/components/fragment.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/components/fragment.js
@@ -68,6 +68,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   TestRunner.addResult('');
 
   check(() => UI.html`<div>${[1, 2, 3].map(x => UI.html`<span>${x}</span>`)}</div>`.childNodes.length === 3);
+  check(() => UI.html`first ${UI.html`<b>bold</b>`} second`.textContent === 'first bold second');
+  const url = 'http://example.com/';
+  check(() => UI.html`<a href='${url}'></a>`.href === url);
 
   TestRunner.completeTest();
 })();
