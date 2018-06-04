7a4e837a175b966172ad4e9064c7736d0d4c3759
Timothy Loh <timloh@chromium.org>
2018-06-04 04:46:21 +0000

    Add basic tests for Crostini App List integration
    This CL adds basic tests for integration of Crostini Apps with the App
    List. In particular, we check that enabling/disabling Crostini shows/
    hides the Terminal app, and installing/updating/removing apps is also
    appropriately reflected in the app list.
    
    This CL also adds a CrostiniTestHelper class, to aid testing Crostini
    app integration. The CrostiniRegistryService intentionally does not
    use the CrostiniTestHelper aside from the proto-building helper
    functions to keep it explicit about what it is testing.
    
    Bug: 842552
    Change-Id: I0794a2a966eb5446775f5227b1ce18046f25bfb5
    Reviewed-on: https://chromium-review.googlesource.com/1080430
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563999}

--- chrome/browser/ui/app_list/crostini/crostini_app_model_builder.cc
+++ chrome/browser/ui/app_list/crostini/crostini_app_model_builder.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/app_list/app_list_controller_delegate.h"
 #include "chrome/browser/ui/app_list/crostini/crostini_app_item.h"
-#include "components/crx_file/id_util.h"
 #include "components/prefs/pref_change_registrar.h"
 #include "ui/base/l10n/l10n_util.h"
 
