7de7b759097579d9ca0d0a775b5dbba4dc8d08a2
Keishi Hattori <keishi@chromium.org>
2018-06-04 11:39:23 +0000

    Use CrossThreadPersistent for SQLTransactionBackend::database_
    SQLTransactionBackend::database_, that points to a main thread owned Database object, was being read from SQLTransactionBackend::DoCleanup on the database thread.
    
    Bug: 846144
    Change-Id: I6433e0289460da02e04878ec25748876787d6e76
    Reviewed-on: https://chromium-review.googlesource.com/1080586
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564059}

--- third_party/blink/renderer/modules/webdatabase/sql_transaction_backend.cc
+++ third_party/blink/renderer/modules/webdatabase/sql_transaction_backend.cc
@@ -399,7 +399,6 @@ SQLTransactionBackend::~SQLTransactionBackend() {
 }
 
 void SQLTransactionBackend::Trace(blink::Visitor* visitor) {
-  visitor->Trace(database_);
   visitor->Trace(wrapper_);
 }
 
