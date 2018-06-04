582e9a460412222deaae600981d81b3c402f22e1
Justin Cohen <justincohen@google.com>
2018-06-04 00:49:55 +0000

    [ios] Correct SCContentSuggestionsTestCase under UI Refresh.
    Remove references to reading list in showcase content suggestions.
    
    Bug: 849089
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I13832f021626cd3e063087666d72a64ceb628e88
    Reviewed-on: https://chromium-review.googlesource.com/1084311
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563981}

--- ios/showcase/content_suggestions/sc_content_suggestions_data_source.mm
+++ ios/showcase/content_suggestions/sc_content_suggestions_data_source.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/content_suggestions/identifier/content_suggestion_identifier.h"
 #import "ios/chrome/browser/ui/content_suggestions/identifier/content_suggestions_section_information.h"
 #import "ios/chrome/browser/ui/favicon/favicon_attributes.h"
+#include "ios/chrome/browser/ui/ui_util.h"
 #include "ios/chrome/grit/ios_strings.h"
 #import "ios/showcase/content_suggestions/sc_content_suggestions_item.h"
 #import "ios/showcase/content_suggestions/sc_content_suggestions_most_visited_item.h"
@@ -69,6 +70,12 @@ using CSCollectionViewItem = CollectionViewItem<SuggestedContent>;
 #pragma mark - ContentSuggestionsDataSource
 
 - (NSArray<ContentSuggestionsSectionInformation*>*)sectionsInfo {
+  if (IsUIRefreshPhase1Enabled()) {
+    return @[
+      self.logoHeaderSection, self.mostVisitedSection, self.articleSection,
+      self.learnMoreSection
+    ];
+  }
   return @[
     self.logoHeaderSection, self.mostVisitedSection, self.readingListSection,
     self.articleSection, self.learnMoreSection
