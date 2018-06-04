594df2697db8ad1c208e07498ac4b3bde15ca2eb
Misha Efimov <mef@chromium.org>
2018-06-04 15:10:25 +0000

    [Cronet] Fix flaky leaks in StaleHostResolverUnittest.
    - Add WaitForNetworkResolveComplete method to wait for completion of network requests before test completion.
    
    Bug: 829097
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I5190cf02dbfad6ceee18f79d228a0c4dfaa9ae25
    Reviewed-on: https://chromium-review.googlesource.com/1067608
    Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    Commit-Queue: Misha Efimov <mef@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564094}

--- components/cronet/stale_host_resolver.cc
+++ components/cronet/stale_host_resolver.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "net/base/net_errors.h"
 #include "net/dns/dns_util.h"
-#include "net/dns/host_resolver_impl.h"
 
 namespace cronet {
 
--- components/cronet/stale_host_resolver_unittest.cc
+++ components/cronet/stale_host_resolver_unittest.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/cert/cert_verifier.h"
 #include "net/dns/dns_hosts.h"
 #include "net/dns/dns_test_util.h"
+#include "net/dns/host_resolver_impl.h"
 #include "net/dns/host_resolver_proc.h"
 #include "net/http/http_network_session.h"
 #include "net/log/net_log.h"
@@ -128,18 +129,22 @@ class StaleHostResolverTest : public testing::Test {
     options_.allow_other_network = allow_other_network;
   }
 
-  void CreateResolverWithDnsClient(std::unique_ptr<net::DnsClient> dns_client) {
-    DCHECK(!resolver_);
-
+  std::unique_ptr<net::HostResolverImpl> CreateMockInnerResolverWithDnsClient(
+      std::unique_ptr<net::DnsClient> dns_client) {
     std::unique_ptr<net::HostResolverImpl> inner_resolver(
         net::HostResolver::CreateDefaultResolverImpl(nullptr));
 
     net::HostResolverImpl::ProcTaskParams proc_params(mock_proc_.get(), 1u);
     inner_resolver->set_proc_params_for_test(proc_params);
     inner_resolver->SetDnsClient(std::move(dns_client));
+    return inner_resolver;
+  }
+
+  void CreateResolverWithDnsClient(std::unique_ptr<net::DnsClient> dns_client) {
+    DCHECK(!resolver_);
 
     stale_resolver_ = std::make_unique<StaleHostResolver>(
-        std::move(inner_resolver), options_);
+        CreateMockInnerResolverWithDnsClient(std::move(dns_client)), options_);
     stale_resolver_->SetTickClockForTesting(&tick_clock_);
     resolver_ = stale_resolver_.get();
   }
@@ -153,17 +158,11 @@ class StaleHostResolverTest : public testing::Test {
     resolver_ = nullptr;
   }
 
-  void SetResolver(net::HostResolver* resolver) {
+  void SetResolver(StaleHostResolver* stale_resolver) {
     DCHECK(!resolver_);
-
-    resolver_ = resolver;
-  }
-
-  void ClearResolver() {
-    DCHECK(resolver_);
-    DCHECK(!stale_resolver_);
-
-    resolver_ = nullptr;
+    stale_resolver->inner_resolver_ =
+        CreateMockInnerResolverWithDnsClient(nullptr);
+    resolver_ = stale_resolver;
   }
 
   // Creates a cache entry for |kHostname| that is |age_sec| seconds old.
@@ -246,6 +245,18 @@ class StaleHostResolverTest : public testing::Test {
     base::ThreadTaskRunnerHandle::Get()->PostTask(
         FROM_HERE, run_loop.QuitWhenIdleClosure());
     run_loop.Run();
+  }
+
+  void WaitForNetworkResolveComplete() {
+    // The stale host resolver cache is initially setup with |kCacheAddress|,
+    // so getting that address means that network resolve is still pending.
+    // The network resolve is guaranteed to return |kNetworkAddress| at some
+    // point because inner resolver is using MockHostResolverProc that always
+    // returns |kNetworkAddress|.
+    while (resolve_addresses()[0].ToStringWithoutPort() != kNetworkAddress) {
+      Resolve();
+      WaitForResolve();
+    }
   }
 
   void Cancel() {
@@ -413,9 +424,8 @@ TEST_F(StaleHostResolverTest, CancelWithStaleCache) {
 // synchronously.
 
 // Disallow other networks cases fail under Fuchsia (crbug.com/816143).
-// TODO(https://crbug.com/829097): Fix memory leaks and re-enable under ASAN.
 // Flaky on Win buildbots. See crbug.com/836106
-#if defined(ADDRESS_SANITIZER) || defined(OS_WIN) || defined(OS_LINUX)
+#if defined(OS_WIN)
 #define MAYBE_StaleUsability DISABLED_StaleUsability
 #else
 #define MAYBE_StaleUsability StaleUsability
@@ -518,6 +528,7 @@ TEST_F(StaleHostResolverTest, MAYBE_StaleUsability) {
     }
     // Make sure that all tasks complete so jobs are freed properly.
     AdvanceTickClock(base::TimeDelta::FromSeconds(kLongStaleDelaySec));
+    WaitForNetworkResolveComplete();
     base::RunLoop run_loop;
     run_loop.RunUntilIdle();
 
@@ -525,8 +536,6 @@ TEST_F(StaleHostResolverTest, MAYBE_StaleUsability) {
   }
 }
 
-#if !defined(ADDRESS_SANITIZER)
-// TODO(https://crbug.com/829097): Fix memory leaks and re-enable under ASAN.
 TEST_F(StaleHostResolverTest, CreatedByContext) {
   URLRequestContextConfig config(
       // Enable QUIC.
@@ -574,10 +583,9 @@ TEST_F(StaleHostResolverTest, CreatedByContext) {
           net::ProxyConfigWithAnnotation::CreateDirect())));
   std::unique_ptr<net::URLRequestContext> context(builder.Build());
 
-  // Duplicate StaleCache test case to ensure StaleHostResolver was created:
-
+  // Experimental options ensure context's resolver is a StaleHostResolver.
+  SetResolver(reinterpret_cast<StaleHostResolver*>(context->host_resolver()));
   // Note: Experimental config above sets 0ms stale delay.
-  SetResolver(context->host_resolver());
   CreateCacheEntry(kAgeExpiredSec, net::OK);
 
   Resolve();
@@ -588,8 +596,8 @@ TEST_F(StaleHostResolverTest, CreatedByContext) {
   EXPECT_EQ(net::OK, resolve_error());
   EXPECT_EQ(1u, resolve_addresses().size());
   EXPECT_EQ(kCacheAddress, resolve_addresses()[0].ToStringWithoutPort());
+  WaitForNetworkResolveComplete();
 }
-#endif  // !defined(ADDRESS_SANITIZER)
 
 }  // namespace
 
