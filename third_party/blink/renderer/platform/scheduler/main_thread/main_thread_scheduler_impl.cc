675831665b806527082051fdc53b9a3be59afb6d
Farah Charab <farahcharab@google.com>
2018-06-04 17:22:56 +0000

    Scheduler: Experiment with priorities of frame and task types.
    Set the priority of hidden frames to low during loading
    when relevant feature is enabled.
    
    Set the priority of sub-frames to low during loading when
    relevant feature is enabled.
    
    Set the priority of throttleable task queues to low during loading
    when relevant feature is enabled.
    
    Set the priority of subframe throttleable task queues to low during
    loading when relevant feature is enabled.
    
    Note that this experiment is supposed to run on Canary/Dev,
    so waterfall test configs aren't added.
    
    Bug: 848925
    Change-Id: I54f3d8049faee10f1ced891033561f91d171b79c
    Reviewed-on: https://chromium-review.googlesource.com/1083194
    Reviewed-by: Alex Clarke <alexclarke@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Farah Charab <farahcharab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564134}

--- third_party/blink/renderer/platform/scheduler/main_thread/main_thread_scheduler_impl.cc
+++ third_party/blink/renderer/platform/scheduler/main_thread/main_thread_scheduler_impl.cc
@@ -2814,6 +2814,10 @@ bool MainThreadSchedulerImpl::ShouldUpdateTaskQueuePriorities(
              main_thread_only().current_policy.compositor_priority();
 }
 
+bool MainThreadSchedulerImpl::IsLoading() const {
+  return main_thread_only().current_use_case == UseCase::kLoading;
+}
+
 // static
 const char* MainThreadSchedulerImpl::UseCaseToString(UseCase use_case) {
   switch (use_case) {
--- third_party/blink/renderer/platform/scheduler/main_thread/main_thread_scheduler_impl.h
+++ third_party/blink/renderer/platform/scheduler/main_thread/main_thread_scheduler_impl.h
@@ -324,6 +324,9 @@ class PLATFORM_EXPORT MainThreadSchedulerImpl
   void OnTraceLogEnabled() override;
   void OnTraceLogDisabled() override;
 
+  // Returns true if main thread is in loading use case.
+  bool IsLoading() const;
+
   base::WeakPtr<MainThreadSchedulerImpl> GetWeakPtr();
 
  protected:
