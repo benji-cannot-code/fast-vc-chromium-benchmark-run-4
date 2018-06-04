c009218177af1764dd21eca1ff636e559c877d9b
Bence Béky <bnc@chromium.org>
2018-06-04 19:00:27 +0000

    Remove four unnecessary set_synchronous_mode(false) calls.
    Change-Id: I7047d1fb5b688edb04e921fd83dcac4f7ffb10de
    Reviewed-on: https://chromium-review.googlesource.com/1085271
    Reviewed-by: Zhongyi Shi <zhongyi@chromium.org>
    Commit-Queue: Bence Béky <bnc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564173}

--- net/socket/transport_client_socket_pool_unittest.cc
+++ net/socket/transport_client_socket_pool_unittest.cc
@@ -808,9 +808,6 @@ TEST_F(TransportClientSocketPoolTest, BackupSocketFailAfterStall) {
               IsError(ERR_CONNECTION_FAILED));
   EXPECT_EQ(0, pool_.IdleSocketCount());
   handle.Reset();
-
-  // Reset for the next case.
-  host_resolver_->set_synchronous_mode(false);
 }
 
 // Test the case where a socket took long enough to start the creation
@@ -859,9 +856,6 @@ TEST_F(TransportClientSocketPoolTest, BackupSocketFailAfterDelay) {
   EXPECT_THAT(handle.connection_attempts()[0].result,
               IsError(ERR_CONNECTION_FAILED));
   handle.Reset();
-
-  // Reset for the next case.
-  host_resolver_->set_synchronous_mode(false);
 }
 
 // Test the case of the IPv6 address stalling, and falling back to the IPv4
