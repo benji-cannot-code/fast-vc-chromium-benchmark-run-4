02823b295294000136eb7d6da7e9b0859afd2ecd
Reilly Grant <reillyg@chromium.org>
2018-06-04 17:02:42 +0000

    Restore RSSI indicators to Web Bluetooth chooser dialog
    This partially reverts the changes made in commit 9e098bcbd1252eeda8747f
    in order to restore the RSSI indicators in the Web Bluetooth chooser
    dialog. The return value of ShouldShowIconBeforeText() cannot change
    because the views::TableView has no way of changing its layout after
    creation.
    
    Bug: 846879
    Change-Id: I81c9e90997b1a6f9143b571d0b17395653832ac1
    Reviewed-on: https://chromium-review.googlesource.com/1083219
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564119}

--- chrome/browser/chooser_controller/fake_bluetooth_chooser_controller.cc
+++ chrome/browser/chooser_controller/fake_bluetooth_chooser_controller.cc
@@ -20,7 +20,7 @@ FakeBluetoothChooserController::FakeBluetoothChooserController(
 FakeBluetoothChooserController::~FakeBluetoothChooserController() {}
 
 bool FakeBluetoothChooserController::ShouldShowIconBeforeText() const {
-  return devices_.size() > 0;
+  return true;
 }
 
 bool FakeBluetoothChooserController::ShouldShowReScanButton() const {
