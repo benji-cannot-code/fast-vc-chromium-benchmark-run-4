b961196707dd33eb3c581b90619916e7d0289999
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2018-06-04 12:43:01 +0000

    AnimationOptions: Include <memory> for std::unique_ptr
    IWYU and stop relying on the header being included implicitly, which breaks
    the libstdc++ build:
    
        In file included from ../../cc/trees/layer_tree_mutator.h:11:0,
                         from ../../cc/trees/layer_tree_mutator.cc:5:
        ../../cc/trees/animation_options.h:18:16: error: ‘unique_ptr’ in namespace ‘std’ does not name a template type
           virtual std::unique_ptr<AnimationOptions> Clone() const = 0;
                        ^~~~~~~~~~
    
    Bug: 819294
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I876921fb915c41362f4674c30e46748688a78172
    Reviewed-on: https://chromium-review.googlesource.com/1084597
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Commit-Queue: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Cr-Commit-Position: refs/heads/master@{#564069}

--- cc/trees/animation_options.h
+++ cc/trees/animation_options.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CC_TREES_ANIMATION_OPTIONS_H_
 #define CC_TREES_ANIMATION_OPTIONS_H_
 
+#include <memory>
+
 #include "cc/cc_export.h"
 
 namespace cc {
