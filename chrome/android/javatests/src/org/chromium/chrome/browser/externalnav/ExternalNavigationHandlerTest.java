38dbf741516dcfb1b33faae3dd5e56eb3a92c03a
huangxueqing <huangxueqing@xiaomi.com>
2018-06-05 14:30:29 +0000

    Ignore Intent filters with wildcard hosts when handling links.
    If the Android device has a package installed that declares
    an Intent filter with `android:host="*"`, Chrome will launch
    the Intent picker UX whenever a person taps a link.
    This is a poor user experience. (Using `android:host="*"`
    arguably doesn't make much sense; normally, Intent filters
    would use `*` to match subdomains such as *.google.com.) Thus,
    skip Intent filters with such wildcard hosts when handling
    acceptable scheme links.
    
    R=dtrainor@chromium.org
    
    Bug: 841133
    Change-Id: I3cb14309ed53e0bd623ed0da59af6635391a8718
    Reviewed-on: https://chromium-review.googlesource.com/1053847
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Chris Palmer <palmer@chromium.org>
    Reviewed-by: Maria Khomenko <mariakhomenko@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564487}

--- chrome/android/javatests/src/org/chromium/chrome/browser/externalnav/ExternalNavigationHandlerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/externalnav/ExternalNavigationHandlerTest.java
@@ -1503,7 +1503,7 @@ public class ExternalNavigationHandlerTest {
         }
 
         @Override
-        public int countSpecializedHandlers(List<ResolveInfo> infos) {
+        public int countSpecializedHandlers(List<ResolveInfo> infos, Intent intent) {
             int count = 0;
             List<IntentActivity> matchingIntentActivities = findMatchingIntentActivities(infos);
             for (IntentActivity intentActivity : matchingIntentActivities) {
