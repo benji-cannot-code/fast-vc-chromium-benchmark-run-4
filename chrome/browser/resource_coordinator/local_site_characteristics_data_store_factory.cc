0aa8b8a9fe84ff0ff547b3dc1a81963466b9e5c1
Francois Doray <fdoray@chromium.org>
2018-06-05 14:08:51 +0000

    RC: Local site DB: Check SiteCharacteristicsDatabase feature in LocalSiteCharacteristicsDataStoreFactory.
    Previously, LocalSiteCharacteristicsDataStoreFactory incorrectly
    checked the ProactiveTabFreezeAndDiscard feature.
    
    Bug: 773382
    Change-Id: I2958d923f564574f6620ef8e103a3a692f17c819
    Reviewed-on: https://chromium-review.googlesource.com/1085641
    Reviewed-by: Sébastien Marchand <sebmarchand@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564478}

--- chrome/browser/resource_coordinator/local_site_characteristics_data_store_factory.cc
+++ chrome/browser/resource_coordinator/local_site_characteristics_data_store_factory.cc
@@ -92,7 +92,7 @@ LocalSiteCharacteristicsDataStoreFactory::GetBrowserContextToUse(
 
 bool LocalSiteCharacteristicsDataStoreFactory::
     ServiceIsCreatedWithBrowserContext() const {
-  return base::FeatureList::IsEnabled(features::kProactiveTabFreezeAndDiscard);
+  return base::FeatureList::IsEnabled(features::kSiteCharacteristicsDatabase);
 }
 
 bool LocalSiteCharacteristicsDataStoreFactory::ServiceIsNULLWhileTesting()
