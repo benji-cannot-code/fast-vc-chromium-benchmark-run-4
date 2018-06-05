d26cabba29c1eb9dde048a31917d8a274cfab618
Michael Lippautz <mlippautz@chromium.org>
2018-06-05 05:21:12 +0000

    [oilpan] Update GC reason in stats collection
    This reason is reported to UMA.
    
    No-try: true
    Bug: chromium:840789, chromium:757440
    Change-Id: Ie2000aa96b34e13e8280b08c7e7c7931f3efe216
    Reviewed-on: https://chromium-review.googlesource.com/1086428
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564364}

--- third_party/blink/renderer/platform/heap/heap_stats_collector_test.cc
+++ third_party/blink/renderer/platform/heap/heap_stats_collector_test.cc
@@ -69,6 +69,14 @@ TEST(ThreadHeapStatsCollectorTest, ScopeToString) {
                    ThreadHeapStatsCollector::kIncrementalMarkingStartMarking));
 }
 
+TEST(ThreadHeapStatsCollectorTest, UpdateReason) {
+  ThreadHeapStatsCollector stats_collector;
+  stats_collector.Start(BlinkGC::kTesting);
+  stats_collector.UpdateReason(BlinkGC::kForcedGC);
+  stats_collector.Stop();
+  EXPECT_EQ(BlinkGC::kForcedGC, stats_collector.previous().reason);
+}
+
 // =============================================================================
 // ThreadHeapStatsCollector::Event. ============================================
 // =============================================================================
