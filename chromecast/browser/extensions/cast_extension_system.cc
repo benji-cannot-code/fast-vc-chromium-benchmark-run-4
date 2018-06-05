10ced3c981c9808a2a96fa5033ea2bb29095660b
Randy Rossi <rmrossi@chromium.org>
2018-06-05 17:51:31 +0000

    Loading chromevox must be done from UI thread
    The extension registrar requires extensions to be
    added from the UI thread.
    
    Bug: b/73383411
    Test: None
    Change-Id: I8d4229f6e9b7bbaee81df764a534a7d7bc10551f
    Reviewed-on: https://chromium-review.googlesource.com/1087039
    Reviewed-by: Alex Sakhartchouk <alexst@chromium.org>
    Commit-Queue: Randy Rossi <rmrossi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564569}

--- chromecast/browser/extensions/cast_extension_system.cc
+++ chromecast/browser/extensions/cast_extension_system.cc
@@ -129,7 +129,10 @@ const Extension* CastExtensionSystem::LoadExtension(
       LOG(WARNING) << warning.message;
   }
 
-  PostLoadExtension(extension);
+  content::BrowserThread::PostTask(
+      content::BrowserThread::UI, FROM_HERE,
+      base::BindOnce(&CastExtensionSystem::PostLoadExtension,
+                     base::Unretained(this), extension));
 
   return extension.get();
 }
