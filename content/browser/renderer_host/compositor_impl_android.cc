311b9be5f1483c3161135ec4080a5e531d45cc03
Saman Sami <samans@chromium.org>
2018-06-05 22:21:30 +0000

    Reland "Make Compositor::DisableSwapUntilResize work with viz"
    Relands crrev.com/3e5eaae with three fixes:
    1. In ui::Compositor always call ResizeDisplay when swap is disabled
    even if size hasn't changed.
    2. Allow resize to 0x0 in RootCompositorFrameSinkImpl::Resize.
    3. Add calls to Resize in CompositorImpl.
    
    Send a sync IPC to Viz process when size is changing in order
    to prevent frame swaps of wrong size. If a wrong size frame is
    swapped then Windows might stretch our content to fill the window.
    
    TBR=rockot@chromium.org,danakj@chromium.org,tsepez@chromium.org
    
    Bug: 811945
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel
    Change-Id: I35db926585c19ef6471284cd84b10f3e25821fb1
    Reviewed-on: https://chromium-review.googlesource.com/1087040
    Reviewed-by: kylechar <kylechar@chromium.org>
    Reviewed-by: Saman Sami <samans@chromium.org>
    Commit-Queue: Saman Sami <samans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564691}

--- content/browser/renderer_host/compositor_impl_android.cc
+++ content/browser/renderer_host/compositor_impl_android.cc
@@ -826,6 +826,10 @@ void CompositorImpl::SetWindowBounds(const gfx::Size& size) {
   }
   if (display_)
     display_->Resize(size);
+
+  if (CompositorDependencies::Get().display_private)
+    CompositorDependencies::Get().display_private->Resize(size);
+
   root_window_->GetLayer()->SetBounds(size);
 }
 
@@ -1253,6 +1257,7 @@ void CompositorImpl::InitializeVizLayerTreeFrameSink(
           std::move(context_provider), nullptr, &params);
   host_->SetLayerTreeFrameSink(std::move(layer_tree_frame_sink));
   CompositorDependencies::Get().display_private->SetDisplayVisible(true);
+  CompositorDependencies::Get().display_private->Resize(size_);
 }
 
 viz::LocalSurfaceId CompositorImpl::GenerateLocalSurfaceId() const {
