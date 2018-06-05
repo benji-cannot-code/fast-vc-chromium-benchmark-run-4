0e7c29ee8da2164b2dbb3b332d924e99feaab81b
Tetsui Ohkubo <tetsui@chromium.org>
2018-06-05 05:34:27 +0000

    Fix ShelfLayoutManagerTest for UnifiedSystemTray.
    This CL fixes ShelfLayoutManagerTest when UnifiedSystemTray is enabled.
    Some tests also need https://crrev.com/c/1082140 and
    https://crrev.com/c/1084367 to run properly.
    
    TEST=ShelfLayoutManagerTest (--enable-features=SystemTrayUnified)
    BUG=847104
    
    Change-Id: If146b0e97a00fc78b73b5894041300fa84ebb2d3
    Reviewed-on: https://chromium-review.googlesource.com/1084393
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564366}

--- ash/test/ash_test_base.cc
+++ ash/test/ash_test_base.cc
@@ -21,8 +21,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/root_window_controller.h"
 #include "ash/session/session_controller.h"
 #include "ash/session/test_session_controller_client.h"
+#include "ash/shelf/shelf.h"
 #include "ash/shell.h"
 #include "ash/shell/toplevel_window.h"
+#include "ash/system/status_area_widget.h"
 #include "ash/test/ash_test_environment.h"
 #include "ash/test/ash_test_helper.h"
 #include "ash/test_screenshot_delegate.h"
@@ -211,6 +213,11 @@ Shelf* AshTestBase::GetPrimaryShelf() {
 // static
 SystemTray* AshTestBase::GetPrimarySystemTray() {
   return Shell::Get()->GetPrimarySystemTray();
+}
+
+// static
+UnifiedSystemTray* AshTestBase::GetPrimaryUnifiedSystemTray() {
+  return GetPrimaryShelf()->GetStatusAreaWidget()->unified_system_tray();
 }
 
 ui::test::EventGenerator& AshTestBase::GetEventGenerator() {
