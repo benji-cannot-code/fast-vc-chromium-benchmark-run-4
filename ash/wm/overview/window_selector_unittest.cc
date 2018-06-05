8a8d3f8d5fc53d2c0c46dc52661a33555bb00463
Tetsui Ohkubo <tetsui@chromium.org>
2018-06-05 05:52:55 +0000

    Fix WindowSelectorTest for UnifiedSystemTray.
    This CL fixes WindowSelectorTest crash when UnifiedSystemTray is
    enabled (--enable-features=SystemTrayUnified). UnifiedSystemTray
    will replace old SystemTray.
    
    TEST=WindowSelectorTest.ShowTextFilterMenu
    BUG=847104
    
    Change-Id: Ic893c83887beac01e191399c181ce5a87514dde3
    Reviewed-on: https://chromium-review.googlesource.com/1084416
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564371}

--- ash/wm/overview/window_selector_unittest.cc
+++ ash/wm/overview/window_selector_unittest.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "ash/shell_test_api.h"
 #include "ash/system/tray/system_tray.h"
+#include "ash/system/unified/unified_system_tray.h"
 #include "ash/test/ash_test_base.h"
 #include "ash/wm/overview/overview_utils.h"
 #include "ash/wm/overview/overview_window_drag_controller.h"
@@ -1739,8 +1740,13 @@ TEST_F(WindowSelectorTest, ShowTextFilterMenu) {
   EXPECT_TRUE(showing_filter_widget());
 
   // Open system bubble shifting focus from the text filter.
-  SystemTray* tray = GetPrimarySystemTray();
-  tray->ShowDefaultView(BUBBLE_CREATE_NEW, false /* show_by_click */);
+  if (features::IsSystemTrayUnifiedEnabled()) {
+    GetPrimaryUnifiedSystemTray()->ShowBubble(false /* show_by_click */);
+  } else {
+    SystemTray* tray = GetPrimarySystemTray();
+    tray->ShowDefaultView(BUBBLE_CREATE_NEW, false /* show_by_click */);
+  }
+
   RunAllPendingInMessageLoop();
 
   // This should not cancel overview mode.
