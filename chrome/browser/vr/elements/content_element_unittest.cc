cc9a51bb594eb8e142af960d43c7fb52bde1292c
Ian Vollick <vollick@chromium.org>
2018-06-05 19:59:40 +0000

    [vr] Only mark scene as dirty if transforms have actually changed
    Previously, I'd assumed that if ShouldUpdateWorldSpaceTransform was
    true that we would update the world space transform. It may be the case,
    however, that the update results in no meaningful change (the reticle
    may not have moved, for example). With this change, we only mark the
    scene as dirty if the new transforms are not approximately equal to the
    old transforms.
    
    Bug: 848074
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_vr
    Change-Id: I9f3225b852755d8296deec58f78215838dc7b334
    Reviewed-on: https://chromium-review.googlesource.com/1087170
    Reviewed-by: Christopher Grant <cjgrant@chromium.org>
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564622}

--- chrome/browser/vr/elements/content_element_unittest.cc
+++ chrome/browser/vr/elements/content_element_unittest.cc
@@ -138,7 +138,7 @@ TEST_F(ContentElementSceneTest, WebInputFocus) {
   EXPECT_CALL(*kb_delegate, OnBeginFrame());
   EXPECT_CALL(*kb_delegate, SetTransform(_));
   content->OnFocusChanged(true);
-  EXPECT_TRUE(OnBeginFrame());
+  EXPECT_FALSE(OnBeginFrame());
 
   // Updates from the browser should update keyboard state.
   TextInputInfo info(base::ASCIIToUTF16("asdfg"));
@@ -185,7 +185,7 @@ TEST_F(ContentElementSceneTest, ClearWebInputInfoModel) {
   // Initial state gets pushed when the content element is focused.
   EXPECT_CALL(*text_input_delegate_, UpdateInput(info));
   content->OnFocusChanged(true);
-  EXPECT_TRUE(OnBeginFrame());
+  EXPECT_FALSE(OnBeginFrame());
 
   // Unfocus the content element.
   content->OnFocusChanged(false);
@@ -196,7 +196,7 @@ TEST_F(ContentElementSceneTest, ClearWebInputInfoModel) {
   // so we shouldn't be pushing the stale state.
   EXPECT_CALL(*text_input_delegate_, UpdateInput(TextInputInfo()));
   content->OnFocusChanged(true);
-  EXPECT_TRUE(OnBeginFrame());
+  EXPECT_FALSE(OnBeginFrame());
 }
 
 class ContentElementInputEditingTest : public UiTest {
