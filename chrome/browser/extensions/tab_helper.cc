5a55b62103f1be96afc34a12b7ecf9c08fd4242e
Devlin Cronin <rdevlin.cronin@chromium.org>
2018-06-05 02:16:41 +0000

    [Extensions] Move ExtensionActionAPI updates to ExtensionActionRunner
    Move updating the ExtensionActionAPI out of extensions::TabHelper and
    into the ExtensionActionRunner. Each is a WebContentsObserver per tab
    (the ExtensionActionRunner is owned by the TabHelper), but the
    ExtensionActionRunner does a lot of other interaction with the
    ExtensionActionAPI already. This eliminates the dependency of
    TabHelper on the ExtensionActionAPI, and helps group similar logic
    together.
    
    This also fixes a race condition that could manifest with runtime
    host permissions. The extension icon would be updated from the
    TabHelper call into ExtensionActionAPI on WebContents navigation, but
    the ExtensionActionRunner wouldn't have cleared its state. This resulted
    in appearing that the extension still wanted to run, even if it didn't.
    
    Add a browser test as a regression test for the above issue.
    
    Bug: 834494
    
    Change-Id: I46f81da853d06d5f9fc3a104750081d176da6e55
    Reviewed-on: https://chromium-review.googlesource.com/1079724
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564348}

--- chrome/browser/extensions/tab_helper.cc
+++ chrome/browser/extensions/tab_helper.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/activity_log/activity_log.h"
 #include "chrome/browser/extensions/api/bookmark_manager_private/bookmark_manager_private_api.h"
 #include "chrome/browser/extensions/api/declarative_content/chrome_content_rules_registry.h"
-#include "chrome/browser/extensions/api/extension_action/extension_action_api.h"
 #include "chrome/browser/extensions/bookmark_app_helper.h"
 #include "chrome/browser/extensions/chrome_extension_web_contents_observer.h"
 #include "chrome/browser/extensions/extension_action_runner.h"
@@ -314,9 +313,6 @@ void TabHelper::DidFinishNavigation(
     UpdateExtensionAppIcon(
         enabled_extensions.GetExtensionOrAppByURL(navigation_handle->GetURL()));
   }
-
-  if (!navigation_handle->IsSameDocument())
-    ExtensionActionAPI::Get(context)->ClearAllValuesForTab(web_contents());
 }
 
 bool TabHelper::OnMessageReceived(const IPC::Message& message,
@@ -341,10 +337,6 @@ void TabHelper::DidCloneToNewWebContents(WebContents* old_web_contents,
 
   new_helper->SetExtensionApp(extension_app());
   new_helper->extension_app_icon_ = extension_app_icon_;
-}
-
-void TabHelper::WebContentsDestroyed() {
-  ExtensionActionAPI::Get(profile_)->ClearAllValuesForTab(web_contents());
 }
 
 void TabHelper::OnDidGetWebApplicationInfo(
