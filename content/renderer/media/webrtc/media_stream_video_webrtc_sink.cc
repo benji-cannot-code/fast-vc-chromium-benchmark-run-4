625e180477938d3073b625ba49abd0727df488bf
Niels Möller <nisse@chromium.org>
2018-06-05 19:30:33 +0000

    Update WebRtcVideoSource for changes in base class VideoTrackSource.
    See webrtc cl https://webrtc-review.googlesource.com/78403
    
    Bug: None
    Change-Id: I842a0cad7c929074f565d72d68664554d15eb5ac
    Reviewed-on: https://chromium-review.googlesource.com/1080647
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564612}

--- content/renderer/media/webrtc/media_stream_video_webrtc_sink.cc
+++ content/renderer/media/webrtc/media_stream_video_webrtc_sink.cc
@@ -41,7 +41,7 @@ class MediaStreamVideoWebRtcSink::WebRtcVideoSource
   WebRtcVideoSource(WebRtcVideoCapturerAdapter* capture_adapter,
                     bool is_screencast,
                     rtc::Optional<bool> needs_denoising)
-      : VideoTrackSource(capture_adapter, false),
+      : VideoTrackSource(false),
         capture_adapter_(capture_adapter),
         is_screencast_(is_screencast),
         needs_denoising_(needs_denoising) {}
@@ -53,6 +53,11 @@ class MediaStreamVideoWebRtcSink::WebRtcVideoSource
   bool is_screencast() const override { return is_screencast_; }
   rtc::Optional<bool> needs_denoising() const override {
     return needs_denoising_;
+  }
+
+ protected:
+  rtc::VideoSourceInterface<webrtc::VideoFrame>* source() override {
+    return capture_adapter_.get();
   }
 
  private:
