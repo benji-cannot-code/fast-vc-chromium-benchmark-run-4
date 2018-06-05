1f94c9414c33cb02e53cd97358a032e73031c7f6
Marc Treib <treib@chromium.org>
2018-06-05 08:18:20 +0000

    Remove ProfileSyncService::OnRefreshTokenAvailable
    Instead, implement the necessary logic in SyncAuthManager.
    This also allows us to make SyncAuthManager::RequestAccessToken private,
    reducing the API between ProfileSyncService and SyncAuthManager by 2
    methods.
    
    Bug: 842697, 825190)
    Change-Id: Id2ba5dc26d8cb2a1abde8566bade8f9cca763d36
    Reviewed-on: https://chromium-review.googlesource.com/1085306
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564402}

--- components/browser_sync/profile_sync_service.cc
+++ components/browser_sync/profile_sync_service.cc
@@ -627,14 +627,6 @@ void ProfileSyncService::AccessTokenFetched(
   NotifyObservers();
 }
 
-void ProfileSyncService::OnRefreshTokenAvailable() {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-
-  if (HasSyncingEngine()) {
-    auth_manager_->RequestAccessToken();
-  }
-}
-
 void ProfileSyncService::OnRefreshTokenRevoked() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
