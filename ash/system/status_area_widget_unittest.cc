4ecf17de0ba578fe4b5b3ab3d2d4dc321711ed1b
Tetsui Ohkubo <tetsui@chromium.org>
2018-06-05 02:37:47 +0000

    Fix StatusAreaWidgetTest for UnifiedSystemTray.
    This CL fixes StatusAreaWidgetTest crash.
    
    TEST=StatusAreaWidgetTest.Basics (--enable-features=SystemTrayUnified)
    BUG=847104
    
    Change-Id: If26ae246b348df68eeec9f32fd4042b1fb3934c7
    Reviewed-on: https://chromium-review.googlesource.com/1084394
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564351}

--- ash/system/status_area_widget_unittest.cc
+++ ash/system/status_area_widget_unittest.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/system_tray_focus_observer.h"
 #include "ash/system/tray/system_tray.h"
 #include "ash/system/tray/system_tray_notifier.h"
+#include "ash/system/unified/unified_system_tray.h"
 #include "ash/system/virtual_keyboard/virtual_keyboard_tray.h"
 #include "ash/test/ash_test_base.h"
 #include "base/command_line.h"
@@ -51,8 +52,12 @@ TEST_F(StatusAreaWidgetTest, Basics) {
 
   // Default trays are constructed.
   EXPECT_TRUE(status->overview_button_tray());
-  EXPECT_TRUE(status->system_tray());
-  EXPECT_TRUE(status->notification_tray());
+  if (features::IsSystemTrayUnifiedEnabled()) {
+    EXPECT_TRUE(status->unified_system_tray());
+  } else {
+    EXPECT_TRUE(status->system_tray());
+    EXPECT_TRUE(status->notification_tray());
+  }
   EXPECT_TRUE(status->logout_button_tray_for_testing());
   EXPECT_TRUE(status->ime_menu_tray());
   EXPECT_TRUE(status->virtual_keyboard_tray_for_testing());
@@ -64,8 +69,12 @@ TEST_F(StatusAreaWidgetTest, Basics) {
 
   // Default trays are visible.
   EXPECT_FALSE(status->overview_button_tray()->visible());
-  EXPECT_TRUE(status->system_tray()->visible());
-  EXPECT_TRUE(status->notification_tray()->visible());
+  if (features::IsSystemTrayUnifiedEnabled()) {
+    EXPECT_TRUE(status->unified_system_tray()->visible());
+  } else {
+    EXPECT_TRUE(status->system_tray()->visible());
+    EXPECT_TRUE(status->notification_tray()->visible());
+  }
   EXPECT_FALSE(status->logout_button_tray_for_testing()->visible());
   EXPECT_FALSE(status->ime_menu_tray()->visible());
   EXPECT_FALSE(status->virtual_keyboard_tray_for_testing()->visible());
