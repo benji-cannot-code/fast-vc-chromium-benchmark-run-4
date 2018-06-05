52e6a9e8e67f24a2a11fb8c0e9a63124ca9920e5
Gauthier Ambard <gambard@chromium.org>
2018-06-05 15:30:33 +0000

    Do not show the progress bar with the TabStrip
    This CL makes sure the progress bar is hidden when the TabStrip is
    shown on Regular x Regular size class.
    
    Bug: 846759
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I664ab653abc7547f5416c3ec4b57b6df3c5857c7
    Reviewed-on: https://chromium-review.googlesource.com/1086803
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564513}

--- ios/chrome/browser/ui/toolbar/adaptive/adaptive_toolbar_view_controller.mm
+++ ios/chrome/browser/ui/toolbar/adaptive/adaptive_toolbar_view_controller.mm
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/toolbar/buttons/toolbar_tools_menu_button.h"
 #import "ios/chrome/browser/ui/toolbar/public/features.h"
 #import "ios/chrome/browser/ui/toolbar/public/omnibox_focuser.h"
+#import "ios/chrome/browser/ui/uikit_ui_util.h"
 #import "ios/third_party/material_components_ios/src/components/ProgressView/src/MaterialProgressView.h"
 #import "ios/third_party/material_components_ios/src/components/Typography/src/MaterialTypography.h"
 
@@ -85,6 +86,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)traitCollectionDidChange:(UITraitCollection*)previousTraitCollection {
   [super traitCollectionDidChange:previousTraitCollection];
   [self updateAllButtonsVisibility];
+  if (IsRegularXRegularSizeClass(self)) {
+    [self.view.progressBar setHidden:YES animated:NO completion:nil];
+  } else if (self.loading) {
+    [self.view.progressBar setHidden:NO animated:NO completion:nil];
+  }
 }
 
 #pragma mark - Public
@@ -114,7 +120,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   if (!loading) {
     [self stopProgressBar];
-  } else if (self.view.progressBar.hidden) {
+  } else if (self.view.progressBar.hidden &&
+             !IsRegularXRegularSizeClass(self)) {
     [self.view.progressBar setProgress:0];
     [self.view.progressBar setHidden:NO animated:YES completion:nil];
     // Layout if needed the progress bar to avoid having the progress bar going
