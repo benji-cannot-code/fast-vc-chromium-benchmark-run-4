2102aa42113ddc47ce9d49673bf82c20092f82ca
Quan Nguyen <qnnguyen@chromium.org>
2018-06-05 10:42:22 +0000

    Add an Apps button for listing kiosk apps in Login screen.
    This CL uses a debug button to load up mock kiosk app info. The code for
    loading the kiosk apps from local storage will come in a later CL.
    
    Bug: 836861
    Change-Id: Ic652a80b522b066380f593147c3f497e14082be0
    Reviewed-on: https://chromium-review.googlesource.com/1080077
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Jacob Dufault <jdufault@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564436}

--- ash/shelf/shelf_widget.h
+++ ash/shelf/shelf_widget.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "ash/public/cpp/shelf_types.h"
+#include "ash/public/interfaces/kiosk_app_info.mojom.h"
 #include "ash/session/session_observer.h"
 #include "ash/shelf/shelf_background_animator.h"
 #include "ash/shelf/shelf_background_animator_observer.h"
@@ -98,6 +99,9 @@ class ASH_EXPORT ShelfWidget : public views::Widget,
 
   // Returns the ApplicationDragAndDropHost for this shelf.
   app_list::ApplicationDragAndDropHost* GetDragAndDropHostForAppList();
+
+  // Updates the list of kiosk apps for the login shelf view.
+  void SetLoginKioskApps(std::vector<mojom::KioskAppInfoPtr> kiosk_apps);
 
   void set_default_last_focusable_child(bool default_last_focusable_child);
 
