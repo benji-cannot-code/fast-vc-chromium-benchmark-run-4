fbd8faccbfd837fbe758f831fd56e27df0779245
Koji Ishii <kojii@chromium.org>
2018-06-05 19:35:03 +0000

    [LayoutNG] Add NGLineBoxStrut, a NGBoxStrut for line-relative
    This patch adds NGLineBoxStrut, a NGBoxStrut for line-relative
    coordinate system. It has `line_over` and `line_under` instead
    of `block_start` and `block_end`.
    
    Following up the baseline fix in CL:1074989, using this struct
    enforces compiler type safety for line-relative coordinate
    system.
    
    Bug: 636993
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: I894f6c47d15d24ceed783684173405006f12cd83
    Reviewed-on: https://chromium-review.googlesource.com/1086686
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564613}

--- third_party/blink/renderer/core/layout/ng/geometry/ng_box_strut.h
+++ third_party/blink/renderer/core/layout/ng/geometry/ng_box_strut.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+struct NGLineBoxStrut;
 struct NGPhysicalBoxStrut;
 
 // This struct is used for storing margins, borders or padding of a box on all
@@ -28,6 +29,7 @@ struct CORE_EXPORT NGBoxStrut {
         inline_end(inline_end),
         block_start(block_start),
         block_end(block_end) {}
+  NGBoxStrut(const NGLineBoxStrut&, bool is_flipped_lines);
 
   LayoutUnit LineLeft(TextDirection direction) const {
     return IsLtr(direction) ? inline_start : inline_end;
@@ -44,11 +46,6 @@ struct CORE_EXPORT NGBoxStrut {
   bool IsEmpty() const;
 
   NGPhysicalBoxStrut ConvertToPhysical(WritingMode, TextDirection) const;
-
-  // Returns a new NGBoxStrut whose block-start and -end are flipped.
-  NGBoxStrut ToFlippedBlock() const {
-    return {inline_start, inline_end, block_end, block_start};
-  }
 
   // The following two operators exist primarily to have an easy way to access
   // the sum of border and padding.
@@ -91,6 +88,33 @@ struct CORE_EXPORT NGBoxStrut {
 };
 
 CORE_EXPORT std::ostream& operator<<(std::ostream&, const NGBoxStrut&);
+
+// A variant of NGBoxStrut in the line-relative coordinate system.
+//
+// 'line-over' is 'block-start' and 'line-under' is 'block-end' unless it is in
+// flipped-lines writing-mode (i.e., 'vertical-lr'), in which case they are
+// swapped.
+//
+// https://drafts.csswg.org/css-writing-modes-3/#line-mappings
+struct CORE_EXPORT NGLineBoxStrut {
+  NGLineBoxStrut() = default;
+  NGLineBoxStrut(LayoutUnit inline_start,
+                 LayoutUnit inline_end,
+                 LayoutUnit line_over,
+                 LayoutUnit line_under)
+      : inline_start(inline_start),
+        inline_end(inline_end),
+        line_over(line_over),
+        line_under(line_under) {}
+  NGLineBoxStrut(const NGBoxStrut&, bool is_flipped_lines);
+
+  LayoutUnit InlineSum() const { return inline_start + inline_end; }
+
+  LayoutUnit inline_start;
+  LayoutUnit inline_end;
+  LayoutUnit line_over;
+  LayoutUnit line_under;
+};
 
 struct NGPixelSnappedPhysicalBoxStrut;
 
