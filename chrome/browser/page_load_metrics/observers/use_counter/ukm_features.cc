d64579d5bdf5e41ba4c42a5710a3f2c34918dd69
Takayoshi Kochi <kochi@chromium.org>
2018-06-05 08:34:56 +0000

    Opt-in Shadow DOM v1 and Custom Elements v1 to UKM
    To analyze usage distribution and major users, opt-in
    Shadow DOM v1 and Custom Elements v1 API UMA into UKM.
    
    Bug: 848153
    Change-Id: I262ea5628cfc96325b1d22a41d0c3a83cbe969f3
    Reviewed-on: https://chromium-review.googlesource.com/1080432
    Commit-Queue: Takayoshi Kochi <kochi@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564408}

--- chrome/browser/page_load_metrics/observers/use_counter/ukm_features.cc
+++ chrome/browser/page_load_metrics/observers/use_counter/ukm_features.cc
@@ -36,6 +36,10 @@ bool IsAllowedUkmFeature(blink::mojom::WebFeature feature) {
           WebFeature::kCredentialManagerMakePublicKeyCredentialSuccess,
           WebFeature::kCredentialManagerGetPublicKeyCredentialSuccess,
           WebFeature::kV8AudioContext_Constructor,
+          WebFeature::kElementAttachShadow,
+          WebFeature::kElementAttachShadowOpen,
+          WebFeature::kElementAttachShadowClosed,
+          WebFeature::kCustomElementRegistryDefine,
       }));
   return opt_in_features.count(feature);
 }
