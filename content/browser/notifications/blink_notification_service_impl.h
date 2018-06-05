0d4fc2cbac7a1f5c92900458b6004ef5bd461c8e
Peter Beverloo <peter@chromium.org>
2018-06-05 11:58:06 +0000

    Move the `Notification.permission` checks to the UI thread
    This is the second part of a three-part change in an effort to
    canonicalize notification permission checks on the UI thread.
    
    Bug: 845514
    Change-Id: I67a776c5497b4bac8e422eaa8d5676c3ebe948fa
    Reviewed-on: https://chromium-review.googlesource.com/1084844
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Anita Woodruff <awdf@chromium.org>
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564457}

--- content/browser/notifications/blink_notification_service_impl.h
+++ content/browser/notifications/blink_notification_service_impl.h
@@ -61,6 +61,8 @@ class CONTENT_EXPORT BlinkNotificationServiceImpl
   // Called when an error is detected on binding_.
   void OnConnectionError();
 
+  void GetPermissionStatusOnUIThread(GetPermissionStatusCallback callback);
+
   void DisplayNonPersistentNotificationOnUIThread(
       const std::string& notification_id,
       const GURL& origin,
