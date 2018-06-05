b1e444a927fb2045528f6b741c22fd5ac6314ea4
Josh Karlin <jkarlin@chromium.org>
2018-06-05 16:04:48 +0000

    Rename LocalFrame::ComputeIsAdSubframe to SetIsAdSubframeIfNecessary
    This is followup from a previous CL
    
    Change-Id: If2685b2726c19bbc9f09a926ee282f1bd864d33b
    Reviewed-on: https://chromium-review.googlesource.com/1086975
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Josh Karlin <jkarlin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564521}

--- third_party/blink/renderer/core/frame/local_frame.h
+++ third_party/blink/renderer/core/frame/local_frame.h
@@ -370,7 +370,7 @@ class CORE_EXPORT LocalFrame final : public Frame,
 
   bool CanNavigateWithoutFramebusting(const Frame&, String& error_reason);
 
-  bool ComputeIsAdSubFrame() const;
+  void SetIsAdSubframeIfNecessary();
 
   void PropagateInertToChildFrames();
 
