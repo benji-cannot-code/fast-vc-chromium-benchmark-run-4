3f332b84f758065a3437fe0a88cfa27f6d11732e
dpapad <dpapad@chromium.org>
2018-06-05 01:20:19 +0000

    WebUI Polymer 2: Update cr-drawer to work with Shadow DOM v1.
     - Ensure 'close' event is bubbling.
     - Change event target for 'transitionend' to be the underlying <dialog>
       directly, since 'transitionend' no longer bubbles across shadow roots.
    
    Bug: 849465
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I66028eca97595df32637adc94fa48dfdb6510cec
    Reviewed-on: https://chromium-review.googlesource.com/1086267
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564328}

--- chrome/test/data/webui/cr_elements/cr_drawer_tests.js
+++ chrome/test/data/webui/cr_elements/cr_drawer_tests.js
@@ -23,7 +23,7 @@ suite('cr-drawer', function() {
     const drawer = createDrawer('ltr');
     drawer.openDrawer();
 
-    return test_util.eventToPromise('transitionend', drawer)
+    return test_util.eventToPromise('cr-drawer-opened', drawer)
         .then(() => {
           assertTrue(drawer.open);
 
@@ -46,16 +46,9 @@ suite('cr-drawer', function() {
         });
   });
 
-  test('opened event', function() {
-    const drawer = createDrawer('ltr');
-    const whenOpen = test_util.eventToPromise('cr-drawer-opened', drawer);
-    drawer.openDrawer();
-    return whenOpen;
-  });
-
   test('align=ltr', function() {
     createDrawer('ltr').openDrawer();
-    return test_util.eventToPromise('transitionend', drawer).then(() => {
+    return test_util.eventToPromise('cr-drawer-opened', drawer).then(() => {
       const rect = drawer.$.dialog.getBoundingClientRect();
       assertEquals(0, rect.left);
       assertNotEquals(0, rect.right);
@@ -64,7 +57,7 @@ suite('cr-drawer', function() {
 
   test('align=rtl', function() {
     createDrawer('rtl').openDrawer();
-    return test_util.eventToPromise('transitionend', drawer).then(() => {
+    return test_util.eventToPromise('cr-drawer-opened', drawer).then(() => {
       const rect = drawer.$.dialog.getBoundingClientRect();
       assertNotEquals(0, rect.left);
       assertEquals(window.innerWidth, rect.right);
