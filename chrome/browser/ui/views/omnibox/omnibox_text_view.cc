cc3d2324cad49cb26ce03af87eea000c358f16d3
Dave Schuyler <dschuyler@chromium.org>
2018-06-05 19:09:46 +0000

    [Omnibox] update item layout for MD refresh (using margins)
    This CL changes the layout (position and size) of suggestion results.
    
    Bug: None
    Change-Id: I72cbecf3a303c03f2fce434acd6483e452c301ce
    Reviewed-on: https://chromium-review.googlesource.com/1072877
    Commit-Queue: Dave Schuyler <dschuyler@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564605}

--- chrome/browser/ui/views/omnibox/omnibox_text_view.cc
+++ chrome/browser/ui/views/omnibox/omnibox_text_view.cc
@@ -34,6 +34,13 @@ constexpr int kTextStyle = views::style::STYLE_PRIMARY;
 // code paths.
 constexpr int kInherit = INT_MIN;
 
+// The vertical padding to provide each RenderText in addition to the height
+// of the font. Where possible, RenderText uses this additional space to
+// vertically center the cap height of the font instead of centering the
+// entire font.
+static constexpr int kVerticalPadding = 4;
+static constexpr int kRefreshVerticalPadding = 3;
+
 struct TextStyle {
   OmniboxPart part;
 
@@ -158,7 +165,9 @@ int OmniboxTextView::GetHeightForWidth(int width) const {
   }
   render_text_->SetDisplayRect(gfx::Rect(width, 0));
   gfx::Size string_size = render_text_->GetStringSize();
-  return string_size.height();
+  return string_size.height() + (ui::MaterialDesignController::IsRefreshUi()
+                                     ? kRefreshVerticalPadding
+                                     : kVerticalPadding);
 }
 
 std::unique_ptr<gfx::RenderText> OmniboxTextView::CreateRenderText(
@@ -348,4 +357,7 @@ void OmniboxTextView::UpdateLineHeight() {
                           render_text_->font_list()
                               .DeriveWithWeight(gfx::Font::Weight::BOLD)
                               .GetHeight());
+  font_height_ += ui::MaterialDesignController::IsRefreshUi()
+                      ? kRefreshVerticalPadding
+                      : kVerticalPadding;
 }
