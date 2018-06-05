b79d7338705f52b6d20c8616c4c8d401ed062dfd
Dominic Farolino <domfarolino@gmail.com>
2018-06-05 08:39:59 +0000

    Experimental Priority Hints support on fetch() API
    Adding support for Priority Hints on the fetch() API under an
    experimental flag. This adds the notion of importance to core/fetch
    code, and adds a new member to the RequestInit dictionary to communicate
    developer-specified "importance" downstream.
    
    R=kinuko@chromium.org, kouhei@chromium.org, yhirano@chromium.org, yoav@yoav.ws
    
    Bug: 821464
    Change-Id: Ib96a128d77004029e86122c07552250c697dea5f
    Reviewed-on: https://chromium-review.googlesource.com/1084063
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Yoav Weiss <yoav@yoav.ws>
    Commit-Queue: Dominic Farolino <domfarolino@gmail.com>
    Cr-Commit-Position: refs/heads/master@{#564410}

--- third_party/blink/renderer/core/fetch/request_init.h
+++ third_party/blink/renderer/core/fetch/request_init.h
@@ -40,6 +40,7 @@ class RequestInit {
   const String& CacheMode() const { return cache_; }
   const String& Redirect() const { return redirect_; }
   const String& Integrity() const { return integrity_; }
+  const String& Importance() const { return importance_; }
   const base::Optional<bool>& Keepalive() const { return keepalive_; }
   base::Optional<AbortSignal*> Signal() const;
   bool AreAnyMembersSet() const { return are_any_members_set_; }
@@ -69,6 +70,7 @@ class RequestInit {
   String cache_;
   String redirect_;
   String integrity_;
+  String importance_;
   base::Optional<bool> keepalive_;
   base::Optional<Member<AbortSignal>> signal_;
   // True if any members in RequestInit are set and hence the referrer member
