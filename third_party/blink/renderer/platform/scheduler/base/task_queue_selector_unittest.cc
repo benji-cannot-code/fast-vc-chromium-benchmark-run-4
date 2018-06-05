0c66d3adb944a982b45777133462a4ed5da0435c
Devlin Cronin <rdevlin.cronin@chromium.org>
2018-06-05 18:03:09 +0000

    [Cleanup] Update histogram_tester.h includes in //third_party/blink
    Update all histogram_tester.h includes in //third_party/blink to
    include //base/test/metrics/histogram_tester.h
    
    TBR=mkwst@chromium.org
    
    Bug: 846421
    Change-Id: Ibea5864f20eee1d67d454fe575309126664b71ab
    Reviewed-on: https://chromium-review.googlesource.com/1087293
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564574}

--- third_party/blink/renderer/platform/scheduler/base/task_queue_selector_unittest.cc
+++ third_party/blink/renderer/platform/scheduler/base/task_queue_selector_unittest.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
 #include "base/pending_task.h"
-#include "base/test/histogram_tester.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/renderer/platform/scheduler/base/task_queue_impl.h"
