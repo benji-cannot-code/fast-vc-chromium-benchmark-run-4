c41322ab08d6d6ffb980238e31bb292242c01981
James Wallace-Lee <jamwalla@chromium.org>
2018-06-05 22:59:26 +0000

    Avoid class verification for SafeBrowsingResponse in onSafeBrowsingHit
    This adds the utility class WebViewCompatUtilsForOMR1. It moves references to
    SafeBrowsingResponseAdapter, which was added in OMR1, into a separate class.
    On pre-OMR1 devices, this reduces log spam caused by a NoClassDefFoundError.
    It also improves startup time.
    
    Bug: 791099, 838702
    Test: adb logcat | grep 'Failed resolution of'
    Test: run cts -m CtsWebkitTestCases -t android.webkit.cts.WebViewClientTest#testOnSafeBrowsingHit
    Change-Id: Icf52a70925be506f9b7c46ed592f5b64f43ab14c
    Reviewed-on: https://chromium-review.googlesource.com/1081081
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: James Wallace-Lee <jamwalla@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564706}

---
+++ android_webview/glue/java/src/com/android/webview/chromium/ApiHelperForOMR1.java
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package com.android.webview.chromium;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import org.chromium.android_webview.AwContentsClient;
+import org.chromium.android_webview.AwSafeBrowsingResponse;
+import org.chromium.base.Callback;
+
+/**
+ * Utility class to use new APIs that were added in OMR1 (API level 27). These need to exist in a
+ * separate class so that Android framework can successfully verify WebView classes without
+ * encountering the new APIs.
+ */
+@TargetApi(Build.VERSION_CODES.O_MR1)
+public class ApiHelperForOMR1 {
+    private ApiHelperForOMR1() {}
+
+    /**
+     * See {@link WebViewClient#onSafeBrowsingHit(WebView, WebResourceRequest, int,
+     * SafeBrowsingResponse)}, which was added in OMR1.
+     */
+    public static void onSafeBrowsingHit(WebViewClient webViewClient, WebView webView,
+            AwContentsClient.AwWebResourceRequest request, int threatType,
+            final Callback<AwSafeBrowsingResponse> callback) {
+        webViewClient.onSafeBrowsingHit(webView, new WebResourceRequestAdapter(request), threatType,
+                new SafeBrowsingResponseAdapter(callback));
+    }
+}
