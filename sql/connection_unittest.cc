abd162f7e5d3e10e71f01bcd82a2659acaa6dfca
Devlin Cronin <rdevlin.cronin@chromium.org>
2018-06-05 18:03:56 +0000

    [Cleanup] Update histogram_tester.h includes in //sql
    Update all histogram_tester.h includes in //sql to include
    //base/test/metrics/histogram_tester.h
    
    TBR=pwnall@chromium.org
    
    Bug: 846421
    Change-Id: Ie5d14e3ecec8626003e4d74b87bb8404247dc6e7
    Reviewed-on: https://chromium-review.googlesource.com/1087291
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564575}

--- sql/connection_unittest.cc
+++ sql/connection_unittest.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/macros.h"
 #include "base/strings/string_number_conversions.h"
-#include "base/test/histogram_tester.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/trace_event/process_memory_dump.h"
 #include "build/build_config.h"
 #include "sql/connection.h"
--- sql/recovery_unittest.cc
+++ sql/recovery_unittest.cc
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/scoped_temp_dir.h"
 #include "base/path_service.h"
 #include "base/strings/string_number_conversions.h"
-#include "base/test/histogram_tester.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "sql/connection.h"
 #include "sql/meta_table.h"
 #include "sql/statement.h"
