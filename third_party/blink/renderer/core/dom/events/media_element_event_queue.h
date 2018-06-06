4f9a93a82157f15ed076a56141822e6b95d0650d
Hajime Hoshi <hajimehoshi@chromium.org>
2018-06-06 09:03:44 +0000

    Always use GetExecutionContext() instead of event targets in EventQueueImpl and MediaElementEventQueue
    This is a preparation for merging EventQueueImpl and
    MediaElementEventQueue. This CL resuces the dependencies on usages of
    |event->target()|.
    
    Design Doc: https://docs.google.com/document/d/1BBtBPTarOF4NeVKSWZe3XaDHo4yTGhdlqYm35yVzPs4/edit#
    
    Bug: 846618
    Change-Id: Ib17a49226ea24a33b053f31a70ae3fe050dccb36
    Reviewed-on: https://chromium-review.googlesource.com/1088356
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564829}

--- third_party/blink/renderer/core/dom/events/media_element_event_queue.h
+++ third_party/blink/renderer/core/dom/events/media_element_event_queue.h
@@ -61,6 +61,8 @@ class CORE_EXPORT MediaElementEventQueue final
   void DispatchEvent(Event* event);
 
   void ContextDestroyed(ExecutionContext*) override;
+  void DoClose(ExecutionContext*);
+  void DoCancelAllEvents(ExecutionContext*);
 
   Member<EventTarget> owner_;
   HeapHashSet<Member<Event>> pending_events_;
