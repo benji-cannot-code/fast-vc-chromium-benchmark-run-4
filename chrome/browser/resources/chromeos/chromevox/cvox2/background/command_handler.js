0711ad28a611cee1c9eeb58ea5eb7ec27924d250
David Tseng <dtseng@chromium.org>
2018-06-06 16:18:21 +0000

    Scope ChromeVox navigation within content editables only when it has content
    Bug: 849726
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ie758887410ce95bd4650e46c31a3d81c9b450930
    Reviewed-on: https://chromium-review.googlesource.com/1087499
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564915}

--- chrome/browser/resources/chromeos/chromevox/cvox2/background/command_handler.js
+++ chrome/browser/resources/chromeos/chromevox/cvox2/background/command_handler.js
@@ -251,7 +251,7 @@ CommandHandler.onCommand = function(command) {
   var dir = Dir.FORWARD;
   var pred = null;
   var predErrorMsg = undefined;
-  var rootPred = AutomationPredicate.editableRoot;
+  var rootPred = AutomationPredicate.rootOrEditableRoot;
   var shouldWrap = true;
   var speechProps = {};
   var skipSync = false;
@@ -757,7 +757,7 @@ CommandHandler.onCommand = function(command) {
         }
 
         var root = bound;
-        while (root && !AutomationPredicate.editableRoot(root))
+        while (root && !AutomationPredicate.rootOrEditableRoot(root))
           root = root.parent;
 
         if (!root)
