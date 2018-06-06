96eab94ebcdf72c488ae032328cb579d16faac86
Greg Thompson <grt@chromium.org>
2018-06-06 20:42:41 +0000

    Fix Layout() of RelaunchRequiredDialogView.
    It was reporting a preferred size inconsistent with its Layout(),
    causing potential clipping.
    
    To fix, anticipate the required alignment using constants.
    
    BUG=847904
    R=bsep@chromium.org
    CC=tapted@chromium.org
    
    Change-Id: I775dbe8a346839914b6dda4965a9c0e97c852661
    Reviewed-on: https://chromium-review.googlesource.com/1087965
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565026}

--- chrome/browser/ui/views/relaunch_notification/relaunch_required_dialog_view.cc
+++ chrome/browser/ui/views/relaunch_notification/relaunch_required_dialog_view.cc
@@ -32,6 +32,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/widget/widget.h"
 #include "ui/views/window/non_client_view.h"
 
+namespace {
+
+constexpr int kTitleIconSize = 20;
+
+}  // namespace
+
 // static
 views::Widget* RelaunchRequiredDialogView::Show(
     Browser* browser,
@@ -123,8 +129,8 @@ bool RelaunchRequiredDialogView::ShouldShowCloseButton() const {
 
 gfx::ImageSkia RelaunchRequiredDialogView::GetWindowIcon() {
   return gfx::CreateVectorIcon(gfx::IconDescription(
-      vector_icons::kBusinessIcon, 20, gfx::kChromeIconGrey, base::TimeDelta(),
-      gfx::kNoneIcon));
+      vector_icons::kBusinessIcon, kTitleIconSize, gfx::kChromeIconGrey,
+      base::TimeDelta(), gfx::kNoneIcon));
 }
 
 bool RelaunchRequiredDialogView::ShouldShowWindowIcon() const {
@@ -138,15 +144,7 @@ int RelaunchRequiredDialogView::GetHeightForWidth(int width) const {
 }
 
 void RelaunchRequiredDialogView::Layout() {
-  // Align the body label with the left edge of the dialog's title.
-  // TODO(bsep): Remove this when fixing https://crbug.com/810970.
-  gfx::Point origin;
-  views::View::ConvertPointToWidget(GetBubbleFrameView()->title(), &origin);
-  views::View::ConvertPointFromWidget(this, &origin);
-
-  gfx::Rect bounds = GetContentsBounds();
-  bounds.Inset(origin.x(), 0, 0, 0);
-  body_label_->SetBoundsRect(bounds);
+  body_label_->SetBoundsRect(GetContentsBounds());
 }
 
 // static
@@ -244,6 +242,16 @@ RelaunchRequiredDialogView::RelaunchRequiredDialogView(
                        views::style::CONTEXT_MESSAGE_BOX_BODY_TEXT);
   body_label_->SetMultiLine(true);
   body_label_->SetHorizontalAlignment(gfx::ALIGN_LEFT);
+
+  // Align the body label with the left edge of the dialog's title.
+  // TODO(bsep): Remove this when fixing https://crbug.com/810970.
+  int title_offset = 2 * views::LayoutProvider::Get()
+                             ->GetInsetsMetric(views::INSETS_DIALOG_TITLE)
+                             .left() +
+                     kTitleIconSize;
+  body_label_->SetBorder(views::CreateEmptyBorder(
+      gfx::Insets(0, title_offset - margins().left(), 0, 0)));
+
   AddChildView(body_label_);
 
   base::RecordAction(base::UserMetricsAction("RelaunchRequiredShown"));
