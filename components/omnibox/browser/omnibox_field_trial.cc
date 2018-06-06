0e6f46e97e7df60bcb1078e7d41c23b7149efe9c
Justin Donnelly <jdonnelly@chromium.org>
2018-06-06 19:18:44 +0000

    [omnibox] Enable experimental UI features with the kExperimentalUi flag.
    Enables the following features:
    - New answer layout
    - Tab switch suggestions
    - Steady-state elision of scheme and trivial subdomains
    - Favicons
    - Increased vertical margins (10px)
    - Swap title and URL
    
    This new flag was introduced to make it easier for everyone to enable
    all of the UI features under development.
    
    Change-Id: I551d75053fb1aa12ca9f76fb7e38f7c85683b8aa
    Reviewed-on: https://chromium-review.googlesource.com/1083215
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564997}

--- components/omnibox/browser/omnibox_field_trial.cc
+++ components/omnibox/browser/omnibox_field_trial.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/variations/variations_associated_data.h"
 #include "third_party/metrics_proto/omnibox_event.pb.h"
 #include "ui/base/material_design/material_design_controller.h"
+#include "ui/base/ui_base_features.h"
 
 using metrics::OmniboxEventProto;
 
@@ -640,8 +641,10 @@ int OmniboxFieldTrial::KeywordScoreForSufficientlyCompleteMatch() {
 OmniboxFieldTrial::EmphasizeTitlesCondition
 OmniboxFieldTrial::GetEmphasizeTitlesConditionForInput(
     const AutocompleteInput& input) {
-  if (base::FeatureList::IsEnabled(omnibox::kUIExperimentSwapTitleAndUrl))
+  if (base::FeatureList::IsEnabled(omnibox::kUIExperimentSwapTitleAndUrl) ||
+      base::FeatureList::IsEnabled(features::kExperimentalUi)) {
     return EMPHASIZE_WHEN_NONEMPTY;
+  }
 
   // Touch-optimized UI and MD Refresh also always swap title and URL.
   if (ui::MaterialDesignController::is_mode_initialized() &&
@@ -677,9 +680,36 @@ OmniboxFieldTrial::GetEmphasizeTitlesConditionForInput(
   return static_cast<EmphasizeTitlesCondition>(value);
 }
 
-// static
-bool OmniboxFieldTrial::InTabSwitchSuggestionTrial() {
-  return base::FeatureList::IsEnabled(omnibox::kOmniboxTabSwitchSuggestions);
+bool OmniboxFieldTrial::IsNewAnswerLayoutEnabled() {
+  return base::FeatureList::IsEnabled(omnibox::kOmniboxNewAnswerLayout) ||
+         base::FeatureList::IsEnabled(features::kExperimentalUi);
+}
+
+bool OmniboxFieldTrial::IsTabSwitchSuggestionsEnabled() {
+  return base::FeatureList::IsEnabled(omnibox::kOmniboxTabSwitchSuggestions) ||
+         base::FeatureList::IsEnabled(features::kExperimentalUi);
+}
+
+bool OmniboxFieldTrial::IsHideSteadyStateUrlSchemeAndSubdomainsEnabled() {
+  return base::FeatureList::IsEnabled(
+             omnibox::kUIExperimentHideSteadyStateUrlSchemeAndSubdomains) ||
+         base::FeatureList::IsEnabled(features::kExperimentalUi);
+}
+
+bool OmniboxFieldTrial::IsShowSuggestionFaviconsEnabled() {
+  return base::FeatureList::IsEnabled(
+             omnibox::kUIExperimentShowSuggestionFavicons) ||
+         base::FeatureList::IsEnabled(features::kExperimentalUi);
+}
+
+int OmniboxFieldTrial::GetSuggestionVerticalMargin() {
+  if (base::FeatureList::IsEnabled(features::kExperimentalUi))
+    return 10;
+
+  using Md = ui::MaterialDesignController;
+  return base::GetFieldTrialParamByFeatureAsInt(
+      omnibox::kUIExperimentVerticalMargin, kUIVerticalMarginParam,
+      Md::GetMode() == Md::MATERIAL_HYBRID ? 8 : 4);
 }
 
 const char OmniboxFieldTrial::kBundledExperimentFieldTrialName[] =
