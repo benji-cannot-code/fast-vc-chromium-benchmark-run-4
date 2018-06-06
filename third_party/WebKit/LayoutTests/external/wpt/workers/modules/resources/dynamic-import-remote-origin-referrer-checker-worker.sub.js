2ebdd97260aa1a128a39e06b7173b35eead14192
Hiroki Nakagawa <nhiroki@chromium.org>
2018-06-06 07:04:48 +0000

    Worker: Add referrer tests for ES Modules on dedicated workers
    Referrer handling spans multiple algorithms in multiple specs, so I leave only
    the entry points of them.
    
    For top-level module script loading and static imports, referrer is determined
    based on the "outside settings":
    
      "Fetch a module worker script graph given url, outside settings, destination,
      the value of the credentials member of options, and inside settings."
    
    https://html.spec.whatwg.org/multipage/workers.html#worker-processing-model
    
    For dynamic imports, referrer is determined based on the "referencing script's
    settings object". In this case, it's "inside settings":
    
      Step 2.4: "Fetch a module script graph given url, referencing script's settings object,
      "script", and options. Wait until the algorithm asynchronously completes with
      result."
    
    https://html.spec.whatwg.org/multipage/webappapis.html#hostimportmoduledynamically(referencingscriptormodule,-specifier,-promisecapability)
    
    These tests are still failing. Subsequent CLs will fix them.
    
    Bug: 842553
    Change-Id: I3855b91c7e6ba93f5c99fb63b24b6274e6b43211
    Reviewed-on: https://chromium-review.googlesource.com/1088339
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564811}

---
+++ third_party/WebKit/LayoutTests/external/wpt/workers/modules/resources/dynamic-import-remote-origin-referrer-checker-worker.sub.js
@@ -0,0 +1,3 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Import a remote origin script.
+import('https://{{domains[www1]}}:{{ports[https][0]}}/workers/modules/resources/referrer-checker.py');
