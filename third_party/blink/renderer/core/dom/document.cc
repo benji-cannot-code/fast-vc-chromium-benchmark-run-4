ac5072583ec11a2b4b31c3c2b390d13d0d96c240
Adithya Srinivasan <adithyas@chromium.org>
2018-06-06 16:18:18 +0000

    Revert "Crash when SetFocusedElement is called during layout"
    This reverts commit 75d3f6c9afd79454604ccd505f16dd540e0623a3.
    
    Reason for revert: Causes crashes: https://crash.corp.google.com/browse?q=expanded_custom_data.ChromeCrashProto.magic_signature_1.name%3D%27blink%3A%3ADocument%3A%3ASetFocusedElement%27+AND+expanded_custom_data.ChromeCrashProto.ptype%3D%27renderer%27+AND+product.Version%3D%2769.0.3451.0%27
    
    Original change's description:
    > Crash when SetFocusedElement is called during layout
    > 
    > I think the crash in https://crbug.com/848212 is caused due to
    > Document::focused_element_ being cleared out during the
    > UpdateStyleAndLayoutIgnorePendingStylesheetsForNode call in
    > Document::SetFocusedElement, but I'm unable to reproduce a situtation
    > where this happens. This CL adds a CHECK that crashes when
    > SetFocusedElement is called during UpdateStyleAndLayout, just so I can
    > get some stack traces in the wild and confirm this theory.
    > 
    > Bug: 848212
    > Change-Id: Ia9293ea155e4ae976592166e7ab4f936f25f3f38
    > Reviewed-on: https://chromium-review.googlesource.com/1085607
    > Commit-Queue: Kent Tamura <tkent@chromium.org>
    > Reviewed-by: Kent Tamura <tkent@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#564337}
    
    TBR=tkent@chromium.org,adithyas@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 848212
    Change-Id: Ie0ee4b7e3fa8e6dd94a8f8a04e6866743f38440d
    Reviewed-on: https://chromium-review.googlesource.com/1088950
    Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564914}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -351,18 +351,6 @@ class DocumentOutliveTimeReporter : public BlinkGCObserver {
   int gc_age_when_document_detached_ = 0;
 };
 
-class SetFocusedElementForbiddenScope {
- public:
-  SetFocusedElementForbiddenScope() { count_++; }
-  ~SetFocusedElementForbiddenScope() { count_--; }
-  static bool IsSetFocusedElementForbidden() { return count_ > 0; }
-
- private:
-  static int count_;
-};
-
-int SetFocusedElementForbiddenScope::count_ = 0;
-
 static const unsigned kCMaxWriteRecursionDepth = 21;
 
 // This amount of time must have elapsed before we will even consider scheduling
@@ -2372,7 +2360,6 @@ void Document::UpdateStyleAndLayoutTreeForNode(const Node* node) {
 
 void Document::UpdateStyleAndLayoutIgnorePendingStylesheetsForNode(Node* node) {
   DCHECK(node);
-  SetFocusedElementForbiddenScope set_focused_element_scope;
   if (!node->InActiveDocument())
     return;
   UpdateStyleAndLayoutIgnorePendingStylesheets();
@@ -4537,10 +4524,6 @@ void Document::SetLastFocusType(WebFocusType last_focus_type) {
 bool Document::SetFocusedElement(Element* new_focused_element,
                                  const FocusParams& params) {
   DCHECK(!lifecycle_.InDetach());
-
-  // TODO(adithyas): This CHECK has been added to investigate
-  // https://crbug.com/848212, remove this after bug is resolved.
-  CHECK(!SetFocusedElementForbiddenScope::IsSetFocusedElementForbidden());
 
   clear_focused_element_timer_.Stop();
 
