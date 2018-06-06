bd5d32e56272efee296241a8ba4232a9c40b0da2
Evan Stade <estade@chromium.org>
2018-06-06 16:59:06 +0000

    Chrome OS: remove non-native notifications codepaths.
    Only support native notifications, i.e. those passed over mojo.
    
    Some tests are permanently removed on Chrome OS. Some tests are
    migrated to message_center_unittests.
    
    Bug: 578868
    Change-Id: Ibd4edcd1968c94af2d24b15ed3c1329ea7bf49b1
    Reviewed-on: https://chromium-review.googlesource.com/1083937
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564928}

--- chrome/browser/chromeos/chrome_browser_main_chromeos.cc
+++ chrome/browser/chromeos/chrome_browser_main_chromeos.cc
@@ -99,7 +99,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/lifetime/application_lifetime.h"
 #include "chrome/browser/net/chrome_network_delegate.h"
 #include "chrome/browser/net/system_network_context_manager.h"
-#include "chrome/browser/notifications/notification_platform_bridge.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/task_manager/task_manager_interface.h"
@@ -677,11 +676,6 @@ void ChromeBrowserMainPartsChromeos::PreMainMessageLoopRun() {
   chromeos::ResourceReporter::GetInstance()->StartMonitoring(
       task_manager::TaskManagerInterface::GetTaskManager());
 
-  if (!base::FeatureList::IsEnabled(features::kNativeNotifications) &&
-      !base::FeatureList::IsEnabled(features::kMash)) {
-    notification_client_.reset(NotificationPlatformBridge::Create());
-  }
-
   ChromeBrowserMainPartsLinux::PreMainMessageLoopRun();
 }
 
@@ -1116,9 +1110,6 @@ void ChromeBrowserMainPartsChromeos::PostMainMessageLoopRun() {
   g_browser_process->platform_part()
       ->browser_policy_connector_chromeos()
       ->PreShutdown();
-
-  // Close the notification client before destroying the profile manager.
-  notification_client_.reset();
 
   // NOTE: Closes ash and destroys ash::Shell.
   ChromeBrowserMainPartsLinux::PostMainMessageLoopRun();
