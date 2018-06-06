76ce93890ec848bdcab49001fa06e13ce8132340
Andrew Grieve <agrieve@chromium.org>
2018-06-06 19:33:00 +0000

    Android: Switch crazy linker to use implicit JNI registration
    Hoping that this will resolve kitkat crashes where the JNI is
    looking for a LegacyLinker method on the Linker class (see bug).
    
    Bug: 849554
    Change-Id: I11389b76142ead6281194cf6f363c8783d3ddbbc
    Reviewed-on: https://chromium-review.googlesource.com/1088875
    Commit-Queue: agrieve <agrieve@chromium.org>
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565001}

--- base/android/linker/linker_jni.h
+++ base/android/linker/linker_jni.h
@@ -22,6 +22,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdlib.h>
 
+#include "build/build_config.h"
+
 // Set this to 1 to enable debug traces to the Android log.
 // Note that LOG() from "base/logging.h" cannot be used, since it is
 // in base/ which hasn't been loaded yet.
@@ -48,6 +50,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // For more, see:
 //   https://crbug.com/504410
 #define RESERVE_BREAKPAD_GUARD_REGION 1
+
+#if defined(ARCH_CPU_X86)
+// Dalvik JIT generated code doesn't guarantee 16-byte stack alignment on
+// x86 - use force_align_arg_pointer to realign the stack at the JNI
+// boundary. https://crbug.com/655248
+#define JNI_GENERATOR_EXPORT \
+  extern "C" __attribute__((visibility("default"), force_align_arg_pointer))
+#else
+#define JNI_GENERATOR_EXPORT extern "C" __attribute__((visibility("default")))
+#endif
 
 namespace chromium_android_linker {
 
