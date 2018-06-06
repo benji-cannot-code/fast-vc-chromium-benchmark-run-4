ec3616243ff385992eab493e61ceb7e700944de4
Bill Orr <billorr@chromium.org>
2018-06-06 21:27:01 +0000

    Make Exclusive sessions use XrSessionController to stop/ExitPresent.
    As part of Mojoification of VRDevice, this change removes ExitPresent,
    and moves exclusive session management out of the VRDevice interface.
    
    BUG=795510
    
    Change-Id: I60c7dca54b62eb518e1895d7a2d0df628fbd84d8
    Reviewed-on: https://chromium-review.googlesource.com/1087718
    Commit-Queue: Bill Orr <billorr@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565039}

--- device/vr/openvr/openvr_device.cc
+++ device/vr/openvr/openvr_device.cc
@@ -174,12 +174,12 @@ void OpenVRDevice::RequestPresent(
     mojom::VRSubmitFrameClientPtr submit_client,
     mojom::VRPresentationProviderRequest request,
     mojom::VRRequestPresentOptionsPtr present_options,
-    mojom::VRDisplayHost::RequestPresentCallback callback) {
+    RequestExclusiveSessionCallback callback) {
   if (!render_loop_->IsRunning())
     render_loop_->Start();
 
   if (!render_loop_->IsRunning()) {
-    std::move(callback).Run(false, nullptr);
+    std::move(callback).Run(false, nullptr, nullptr);
     return;
   }
 
@@ -195,11 +195,11 @@ void OpenVRDevice::RequestPresent(
 }
 
 void OpenVRDevice::OnRequestPresentResult(
-    mojom::VRDisplayHost::RequestPresentCallback callback,
+    RequestExclusiveSessionCallback callback,
     bool result,
     mojom::VRDisplayFrameTransportOptionsPtr transport_options) {
   OnStartPresenting();
-  std::move(callback).Run(result, std::move(transport_options));
+  std::move(callback).Run(result, std::move(transport_options), this);
 
   if (result) {
     using ViewerMap = std::map<std::string, VrViewerType>;
@@ -221,7 +221,13 @@ void OpenVRDevice::OnRequestPresentResult(
   }
 }
 
-void OpenVRDevice::ExitPresent() {
+// XrSessionController
+void OpenVRDevice::SetFrameDataRestricted(bool restricted) {
+  // Presentation sessions can not currently be restricted.
+  DCHECK(false);
+}
+
+void OpenVRDevice::StopSession() {
   render_loop_->task_runner()->PostTask(
       FROM_HERE,
       base::Bind(&OpenVRRenderLoop::ExitPresent, render_loop_->GetWeakPtr()));
