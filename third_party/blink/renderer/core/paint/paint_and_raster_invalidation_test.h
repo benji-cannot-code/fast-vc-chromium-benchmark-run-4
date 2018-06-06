49e255fc08ef64654368ec0443b9d95ca6928a50
danakj <danakj@chromium.org>
2018-06-06 18:28:18 +0000

    Add documentation to cc::Layer API methods
    Adds lots of comments explaining how to hold cc::Layer and what various
    methods do or values mean.
    
    Some cleanup while there, removing redundant child_at() and unused
    SetChildren().
    
    Also noticed SafeOpaqueBackgroundColor() is not safe if the root color
    is non-opaque since that is what it inherits from. That occurs on the
    ChromeOS lock screen, but there are no opaque layers there since
    everything is trying to appear like widgets over the transparent space
    so we're lucky right now. It would be a bug to try use non-opaque
    colors to clear an "opaque" PictureLayer though, so I fixed it to
    ensure the color is opaque even coming from the root layer.
    
    R=enne@chromium.org
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I325e3b03c4b4090068bbd1f453bf99a8cbfcd91c
    Reviewed-on: https://chromium-review.googlesource.com/1087419
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: enne <enne@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564975}

--- third_party/blink/renderer/core/paint/paint_and_raster_invalidation_test.h
+++ third_party/blink/renderer/core/paint/paint_and_raster_invalidation_test.h
@@ -26,7 +26,8 @@ class PaintAndRasterInvalidationTest : public PaintControllerPaintTest {
           .View()
           ->GetPaintArtifactCompositorForTesting()
           ->RootLayer()
-          ->child_at(index);
+          ->children()[index]
+          .get();
     }
     return GetLayoutView().Layer()->GraphicsLayerBacking()->ContentLayer();
   }
