cffd3609b0ba63d2ae55cab2724f13f8cfc87e04
Amy Qiu <amyqiu@google.com>
2018-06-06 17:22:01 +0000

    Merge tough_pinch_zoom_cases into rendering page sets
    Move tough_pinch_zoom_cases into the rendering folder and refactor to
    inherit from RenderingStory
    
    Bug: 849392
    Change-Id: I6496aacc50aa730843d677e73c89f3025b0799ba
    Reviewed-on: https://chromium-review.googlesource.com/1087726
    Commit-Queue: Amy Qiu <amyqiu@google.com>
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Cr-Commit-Position: refs/heads/master@{#564941}

--- tools/perf/page_sets/rendering/story_tags.py
+++ tools/perf/page_sets/rendering/story_tags.py
@@ -20,6 +20,8 @@ FASTPATH = Tag(
     'fastpath', 'Fast path stories.')
 REQUIRED_WEBGL = Tag(
     'required_webgl', 'Stories that are skipped if no webgl support')
+PINCH_ZOOM = Tag(
+    'pinch_zoom', 'Stories that only run on Mac for desktop cases')
 
 
 def _ExtractAllTags():
