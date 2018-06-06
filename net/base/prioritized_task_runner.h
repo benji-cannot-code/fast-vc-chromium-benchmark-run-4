753c9681edd33421cfbd0a457d6b92e81176a890
Findit <findit-for-me@appspot.gserviceaccount.com>
2018-06-06 23:35:05 +0000

    Revert "Run PrioritizedTaskRunner tasks with the same priority in posting order"
    This reverts commit 413675e9b667e07ae84a115268abedfdb7430740.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 564933 as the
    culprit for failures in the build cycles as shown on:
    https://findit-for-me.appspot.com/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtLzQxMzY3NWU5YjY2N2UwN2FlODRhMTE1MjY4YWJlZGZkYjc0MzA3NDAM
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium.memory/Linux%20ChromiumOS%20MSan%20Tests/7480
    
    Sample Failed Step: browser_tests
    
    Original change's description:
    > Run PrioritizedTaskRunner tasks with the same priority in posting order
    > 
    > What:
    > If two tasks have the same priority, they should run in posting order.
    > 
    > A second thing this CL does is fix a race in the unittests. I meant to block
    > the task runner before queueing up requests but failed to. Fixed here.
    > 
    > Why:
    > This can prevent hypothetical task starvation. It's also necessary for the
    > SimpleCachePrioritization control group to ensure that we maintain old
    > behavior.
    > 
    > Bug: 850085
    > Change-Id: I1f259296604bf04ea33ecbee9ad3180f064c4266
    > Reviewed-on: https://chromium-review.googlesource.com/1088759
    > Commit-Queue: Josh Karlin <jkarlin@chromium.org>
    > Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#564933}
    
    Change-Id: Id4930090c0026aefa9d92bb8871e809a3fcfa124
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 850085
    Reviewed-on: https://chromium-review.googlesource.com/1089892
    Cr-Commit-Position: refs/heads/master@{#565096}

--- net/base/prioritized_task_runner.h
+++ net/base/prioritized_task_runner.h
@@ -49,7 +49,7 @@ class NET_EXPORT_PRIVATE PrioritizedTaskRunner
   // Similar to TaskRunner::PostTaskAndReply, except that the task runs at
   // |priority|. Priority 0 is the highest priority and will run before other
   // priority values. Multiple tasks with the same |priority| value are run in
-  // order of posting.
+  // arbitrary order.
   void PostTaskAndReply(const base::Location& from_here,
                         base::OnceClosure task,
                         base::OnceClosure reply,
@@ -81,8 +81,7 @@ class NET_EXPORT_PRIVATE PrioritizedTaskRunner
     Job(const base::Location& from_here,
         base::OnceClosure task,
         base::OnceClosure reply,
-        uint32_t priority,
-        uint32_t task_count);
+        uint32_t priority);
     ~Job();
 
     Job(Job&& other);
@@ -92,7 +91,6 @@ class NET_EXPORT_PRIVATE PrioritizedTaskRunner
     base::OnceClosure task;
     base::OnceClosure reply;
     uint32_t priority;
-    uint32_t task_count;
     scoped_refptr<base::TaskRunner> reply_task_runner =
         base::ThreadTaskRunnerHandle::Get();
 
@@ -102,8 +100,6 @@ class NET_EXPORT_PRIVATE PrioritizedTaskRunner
 
   struct JobComparer {
     bool operator()(const Job& left, const Job& right) {
-      if (left.priority == right.priority)
-        return left.task_count > right.task_count;
       return left.priority > right.priority;
     }
   };
@@ -124,11 +120,6 @@ class NET_EXPORT_PRIVATE PrioritizedTaskRunner
 
   // Accessed on Job::reply_task_runner.
   scoped_refptr<base::TaskRunner> task_runner_;
-
-  // Used to preserve order of jobs of equal priority. This can overflow and
-  // cause periodic priority inversion. This should be infrequent enough to be
-  // of negligible impact.
-  uint32_t task_count_;
 
   DISALLOW_COPY_AND_ASSIGN(PrioritizedTaskRunner);
 };
