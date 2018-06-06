1d42ebdbabaf7ce92e2c060bedae13112f2fd36f
Kyle Horimoto <khorimoto@google.com>
2018-06-06 02:11:02 +0000

    [CrOS MultiDevice] Create SecureChannelService.
    SecureChannelService holds one instance of SecureChannelImpl and brokers
    all API calls to that instance.
    
    Bug: 824568, 752273
    Change-Id: Iaa3c0cde59fca0382682d3bf34b6458b75712b15
    Reviewed-on: https://chromium-review.googlesource.com/1079643
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564768}

---
+++ chromeos/services/secure_channel/secure_channel_base.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/services/secure_channel/secure_channel_base.h"
+
+namespace chromeos {
+
+namespace secure_channel {
+
+SecureChannelBase::SecureChannelBase() = default;
+
+SecureChannelBase::~SecureChannelBase() = default;
+
+void SecureChannelBase::BindRequest(mojom::SecureChannelRequest request) {
+  bindings_.AddBinding(this, std::move(request));
+}
+
+}  // namespace secure_channel
+
+}  // namespace chromeos
