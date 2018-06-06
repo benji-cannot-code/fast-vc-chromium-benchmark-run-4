e65b5e64a60dd1a02449011443eba373d1da0c9b
Bruno Kim Medeiros Cesar <brunokim@chromium.org>
2018-06-06 18:11:52 +0000

    Surface enabling ARC for child accounts in chrome://flags.
    This does not enable the whole feature, which also requires being in a
    server-side whitelist.
    
    Bug: 836246
    Change-Id: I6be61a5a18ba1e102d9dceaf801ecdf24931dd9d
    Reviewed-on: https://chromium-review.googlesource.com/1088930
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Commit-Queue: Bruno Kim Medeiros Cesar <brunokim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564967}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1650,6 +1650,9 @@ extern const char kAcceleratedMjpegDecodeDescription[];
 extern const char kAllowTouchpadThreeFingerClickName[];
 extern const char kAllowTouchpadThreeFingerClickDescription[];
 
+extern const char kArcAvailableForChildName[];
+extern const char kArcAvailableForChildDescription[];
+
 extern const char kArcBootCompleted[];
 extern const char kArcBootCompletedDescription[];
 
