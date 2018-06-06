2a434540b058a1fc654080c5461003728a313a7a
Tetsui Ohkubo <tetsui@chromium.org>
2018-06-06 17:39:54 +0000

    Remove reference to flags from MessageListView.
    This CL removes reference to runtime flags from MessageListView,
    and moved the option to the constructor.
    Direct reference to flag led to unit test failure in
    MessageCenterViewTest.SizeAfterUpdateOfRepositionTarget.
    
    TEST=out/Release/ash_unittests '--gtest_filter=*MessageCenter*' \
    --enable-features=SystemTrayUnified
    BUG=847104
    
    Change-Id: I82d33c827d79bf5fc53d30453e8ac91b18396356
    Reviewed-on: https://chromium-review.googlesource.com/1088329
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564948}

--- ash/message_center/message_list_view.cc
+++ ash/message_center/message_list_view.cc
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/message_center/message_center_style.h"
 #include "ash/message_center/message_center_view.h"
-#include "ash/public/cpp/ash_features.h"
-#include "ash/public/cpp/ash_switches.h"
 #include "base/command_line.h"
 #include "base/location.h"
 #include "base/single_thread_task_runner.h"
@@ -31,16 +29,6 @@ namespace ash {
 
 namespace {
 const int kAnimateClearingNextNotificationDelayMS = 40;
-
-bool HasBorderPadding() {
-  return !switches::IsSidebarEnabled() &&
-         !features::IsSystemTrayUnifiedEnabled();
-}
-
-int GetMarginBetweenItems() {
-  return HasBorderPadding() ? message_center::kMarginBetweenItemsInList : 0;
-}
-
 }  // namespace
 
 MessageListView::MessageListView()
@@ -49,6 +37,7 @@ MessageListView::MessageListView()
       has_deferred_task_(false),
       clear_all_started_(false),
       use_fixed_height_(true),
+      has_border_padding_(false),
       animator_(this),
       weak_ptr_factory_(this) {
   auto layout = std::make_unique<views::BoxLayout>(views::BoxLayout::kVertical,
@@ -56,10 +45,6 @@ MessageListView::MessageListView()
   layout->SetDefaultFlex(1);
   SetLayoutManager(std::move(layout));
 
-  if (HasBorderPadding()) {
-    SetBorder(views::CreateEmptyBorder(
-        gfx::Insets(message_center::kMarginBetweenItemsInList)));
-  }
   animator_.AddObserver(this);
 }
 
@@ -338,6 +323,12 @@ void MessageListView::RemoveObserver(MessageListView::Observer* observer) {
   observers_.RemoveObserver(observer);
 }
 
+void MessageListView::SetBorderPadding() {
+  has_border_padding_ = true;
+  SetBorder(views::CreateEmptyBorder(
+      gfx::Insets(message_center::kMarginBetweenItemsInList)));
+}
+
 void MessageListView::OnBoundsAnimatorProgressed(
     views::BoundsAnimator* animator) {
   DCHECK_EQ(&animator_, animator);
@@ -391,6 +382,10 @@ void MessageListView::OnBoundsAnimatorDone(views::BoundsAnimator* animator) {
 
   if (GetWidget())
     GetWidget()->SynthesizeMouseMoveEvent();
+}
+
+int MessageListView::GetMarginBetweenItems() const {
+  return has_border_padding_ ? message_center::kMarginBetweenItemsInList : 0;
 }
 
 bool MessageListView::IsValidChild(const views::View* child) const {
