ad6216d8c3e91e3d44905552361d78634b9f946c
Tetsui Ohkubo <tetsui@chromium.org>
2018-06-06 01:19:31 +0000

    Add UnifiedSystemTrayTestApi.
    This CL adds UnifiedSystemTrayTestApi, which implements
    mojom::SystemTrayTestApi for UnifiedSystemTray.
    
    TEST=browser_tests --enable-features=SystemTrayUnified
    BUG=847104
    
    Change-Id: I278ec8d381aa9f7c13121088a892419dc627b655
    Reviewed-on: https://chromium-review.googlesource.com/1086674
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564759}

--- ash/system/unified/unified_system_tray.cc
+++ ash/system/unified/unified_system_tray.cc
@@ -143,7 +143,10 @@ UnifiedSystemTray::UnifiedSystemTray(Shelf* shelf)
       slider_bubble_controller_(
           std::make_unique<UnifiedSliderBubbleController>(this)),
       notification_counter_item_(new NotificationCounterView()),
-      quiet_mode_view_(new QuietModeView()) {
+      quiet_mode_view_(new QuietModeView()),
+      time_view_(
+          new tray::TimeView(tray::TimeView::ClockLayout::HORIZONTAL_CLOCK,
+                             Shell::Get()->system_tray_model()->clock())) {
   tray_container()->AddChildView(notification_counter_item_);
   tray_container()->AddChildView(quiet_mode_view_);
 
@@ -158,9 +161,7 @@ UnifiedSystemTray::UnifiedSystemTray(Shelf* shelf)
   tray_container()->AddChildView(new tray::PowerTrayView(nullptr));
 
   TrayItemView* time_item = new TrayItemView(nullptr);
-  time_item->AddChildView(
-      new tray::TimeView(tray::TimeView::ClockLayout::HORIZONTAL_CLOCK,
-                         Shell::Get()->system_tray_model()->clock()));
+  time_item->AddChildView(time_view_);
   tray_container()->AddChildView(time_item);
 
   SetInkDropMode(InkDropMode::ON);
--- ash/system/unified/unified_system_tray.h
+++ ash/system/unified/unified_system_tray.h
@@ -13,6 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
+namespace tray {
+class TimeView;
+}  // namespace tray
+
 class NotificationCounterView;
 class QuietModeView;
 class UnifiedSliderBubbleController;
@@ -30,9 +34,6 @@ class UnifiedSystemTrayModel;
 //
 // UnifiedSystemTray is alternative implementation of SystemTray that is going
 // to replace the original one. Eventually, SystemTray will be removed.
-// During the transition, we hide UnifiedSystemTray button and forward
-// PerformAction() from SystemTray button, as we still don't have implementation
-// of status icons on the UnifiedSystemTray button.
 class ASH_EXPORT UnifiedSystemTray : public TrayBackgroundView {
  public:
   explicit UnifiedSystemTray(Shelf* shelf);
@@ -58,6 +59,8 @@ class ASH_EXPORT UnifiedSystemTray : public TrayBackgroundView {
   UnifiedSystemTrayModel* model() { return model_.get(); }
 
  private:
+  friend class UnifiedSystemTrayTestApi;
+
   // Private class implements message_center::UiDelegate.
   class UiDelegate;
 
@@ -83,6 +86,7 @@ class ASH_EXPORT UnifiedSystemTray : public TrayBackgroundView {
 
   NotificationCounterView* const notification_counter_item_;
   QuietModeView* const quiet_mode_view_;
+  tray::TimeView* const time_view_;
 
   DISALLOW_COPY_AND_ASSIGN(UnifiedSystemTray);
 };
