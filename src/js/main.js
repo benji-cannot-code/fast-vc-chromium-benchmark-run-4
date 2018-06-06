90f0479824bb9cff76b91465b9a11881cbf561c5
Yuli Huang <yuli@chromium.org>
2018-06-06 14:20:50 +0800

    Don't dispatch key-press events when no current-view.
    BUG=None
    TEST=Tested manually on elm.
    
    Change-Id: I9fa31c44d9eb6c32f76d31989163434ea51a1e7d
    Reviewed-on: https://chromium-review.googlesource.com/1088351
    Reviewed-by: Sheng-hao Tsao <shenghao@google.com>
    Tested-by: yuli <yuli@chromium.org>

--- src/js/main.js
+++ src/js/main.js
@@ -316,7 +316,9 @@ camera.Camera.prototype.onErrorIconClicked_ = function() {
 camera.Camera.prototype.onKeyPressed_ = function(event) {
   if (this.context_.hasError)
     return;
-  this.currentView.onKeyPressed(event);
+
+  if (this.currentView)
+    this.currentView.onKeyPressed(event);
 };
 
 /**
