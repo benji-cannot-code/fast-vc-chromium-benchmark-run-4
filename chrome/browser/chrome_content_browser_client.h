11e73146686ee74e965a6e8e3ea38d5098b4b0ce
Ken Rockot <rockot@chromium.org>
2018-06-06 17:12:21 +0000

    Proxy browser requests through WebRequest API
    This enables browser-initiated requests to be proxied through the
    WebRequest API when the Network Service is enabled. Note that this
    only applies to requests made on via a profile's context, not the
    system context.
    
    This mirrors the behavior in the non-Network Service path today.
    
    Bug: 841002
    Test: see buildbot filter changes
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I1830959e1ba089974c546d933b4a6b51fb038779
    Reviewed-on: https://chromium-review.googlesource.com/1083952
    Commit-Queue: Ken Rockot <rockot@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564937}

--- chrome/browser/chrome_content_browser_client.h
+++ chrome/browser/chrome_content_browser_client.h
@@ -393,6 +393,7 @@ class ChromeContentBrowserClient : public content::ContentBrowserClient {
       int render_frame_id,
       NonNetworkURLLoaderFactoryMap* factories) override;
   bool WillCreateURLLoaderFactory(
+      content::BrowserContext* browser_context,
       content::RenderFrameHost* frame,
       bool is_navigation,
       network::mojom::URLLoaderFactoryRequest* factory_request) override;
