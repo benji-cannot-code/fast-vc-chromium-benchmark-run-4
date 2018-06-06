732aab8e61b112bd14a32188fd791800315594fe
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-06-06 00:51:56 +0000

    Import wpt@a7683a8516a1af5b4047a4bbaef7aaba575ad774
    Using wpt-import in Chromium 5316b1cb8fc215ecf9bae61ad243275b1efe91d8.
    With Chromium commits locally applied on WPT:
    e8832df879 "webrtc wpt: remove test_state_change_event"
    a558107e39 "[LayoutNG] Fix flex BODY in quirks mode"
    be0ac4b3ec "Reland "WaveShaper must output non-zero values even if input is silent""
    b9e5f3f7bc "[css-contain] Disable layout containment for certain elements"
    
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/19096
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    jsbell@chromium.org:
      external/wpt/IndexedDB
    kozyatinskiy@chromium.org:
      external/wpt/console
    meade@chromium.org:
      external/wpt/web-animations
    mkwst@chromium.org, andypaicu@chromium.org:
      external/wpt/content-security-policy
    mstensho@chromium.org:
      external/wpt/css/css-multicol
    worker-dev@chromium.org:
      external/wpt/workers
    yhirano@chromium.org, mkwst@chromium.org, japhet@chromium.org:
      external/wpt/fetch
    
    TBR=foolip
    
    No-Export: true
    Change-Id: I1f55148a4bd1008c33abc33271fe21abafe17787
    Reviewed-on: https://chromium-review.googlesource.com/1087824
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564753}

---
+++ third_party/WebKit/LayoutTests/external/wpt/html/browsers/history/the-location-interface/resources/reload_post_1-1.py
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+def main(request, response):
+    headers = [("Content-Type", "text/html")]
+    return headers, '''
+        <script>
+            onload = function() {opener.next()}
+            document.write(Math.random());
+        </script>
+        <form method="POST" action="">
+            <input type=hidden name=test value=test>
+            <input type=submit>
+        </form>
+        <button onclick="location.reload()">Reload</button>
+    '''
