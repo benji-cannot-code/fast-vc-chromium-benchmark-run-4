dd790603dec0faa533a997310e84f7236f640119
Jordy Greenblatt <jordynass@chromium.org>
2018-06-06 21:37:29 +0000

    [CrOS MultiDevice]: Setup completion recorder
    Change-Id: I81c5655e357b6211ccb32f1b82b829bb8d402f0b
    Reviewed-on: https://chromium-review.googlesource.com/1088230
    Commit-Queue: Jordy Greenblatt <jordynass@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565044}

---
+++ chromeos/services/multidevice_setup/fake_setup_flow_completion_recorder.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/services/multidevice_setup/fake_setup_flow_completion_recorder.h"
+
+namespace chromeos {
+
+namespace multidevice_setup {
+
+FakeSetupFlowCompletionRecorder::FakeSetupFlowCompletionRecorder() = default;
+
+base::Optional<base::Time>
+FakeSetupFlowCompletionRecorder::GetCompletionTimestamp() {
+  return completion_time_;
+}
+
+void FakeSetupFlowCompletionRecorder::RecordCompletion() {
+  completion_time_ = current_time_;
+}
+
+}  // namespace multidevice_setup
+
+}  // namespace chromeos
