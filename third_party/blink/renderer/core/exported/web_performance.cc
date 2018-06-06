59e4ac73625002e50a45086c92cada0f08874562
Gyuyoung Kim <gyuyoung.kim@lge.com>
2018-06-06 10:03:06 +0000

    Export FirstMeaningfulPaintCandidate() through WebPerformance
    FMP signal has sometimes been used for dismissing a splash screen in some
    downstream Web Application platforms. But, FMP signal couldn't occasionally
    be generated or happened a little bit late, then it could cause that the
    splash screen dismissed a little bit late in the Web Application platforms.
    This CL is to export FirstMeaningfulPaintCandidate() in order to be used when
    FirstMeaningfulPaint() doesn't work or generates a signal a little bit late.
    
    Bug: 848639
    Test: Covered by FirstMeaningfulPaintDetectorTest.FirstMeaningfulPaintCandidate
    Change-Id: I492d98ef4998e772d293f81b9991a066cb346233
    Reviewed-on: https://chromium-review.googlesource.com/1080499
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung.kim@lge.com>
    Cr-Commit-Position: refs/heads/master@{#564841}

--- third_party/blink/renderer/core/exported/web_performance.cc
+++ third_party/blink/renderer/core/exported/web_performance.cc
@@ -167,6 +167,11 @@ double WebPerformance::FirstMeaningfulPaint() const {
   return MillisecondsToSeconds(private_->timing()->FirstMeaningfulPaint());
 }
 
+double WebPerformance::FirstMeaningfulPaintCandidate() const {
+  return MillisecondsToSeconds(
+      private_->timing()->FirstMeaningfulPaintCandidate());
+}
+
 double WebPerformance::PageInteractive() const {
   return MillisecondsToSeconds(private_->timing()->PageInteractive());
 }
