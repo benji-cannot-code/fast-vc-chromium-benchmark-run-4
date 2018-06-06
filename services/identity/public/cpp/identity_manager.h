dc2973c447f38478a94152b64b68693b2d50a9a2
Colin Blundell <blundell@chromium.org>
2018-06-06 11:51:42 +0000

    Remove PSSStartupTest as friend of IdentityManager
    APIs are now in place that allow ProfileSyncServiceStartupTest to use
    identity_test_utils.h, which is preferable as that has a direct
    conversion path to the long-term IdentityTestEnvironment test
    infrastructure.
    
    Bug: 796544
    Change-Id: I2850f950353849a50638195bd2c4276089b011a3
    Reviewed-on: https://chromium-review.googlesource.com/1085463
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564860}

--- services/identity/public/cpp/identity_manager.h
+++ services/identity/public/cpp/identity_manager.h
@@ -22,11 +22,6 @@ namespace arc {
 class ArcTermsOfServiceDefaultNegotiatorTest;
 }
 
-// Necessary to declare this class as a friend.
-namespace browser_sync {
-class ProfileSyncServiceStartupTest;
-}
-
 // Necessary to declare these classes as friends.
 namespace chromeos {
 class ChromeSessionManager;
@@ -161,7 +156,6 @@ class IdentityManager : public SigninManagerBase::Observer,
   friend arc::ArcTermsOfServiceDefaultNegotiatorTest;
   friend chromeos::ChromeSessionManager;
   friend chromeos::UserSessionManager;
-  friend browser_sync::ProfileSyncServiceStartupTest;
 
   // Sets the primary account info synchronously with both the IdentityManager
   // and its backing SigninManager/ProfileOAuth2TokenService instances.
