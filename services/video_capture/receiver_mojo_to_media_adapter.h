c41157cc1d7a19a7beabccb2ad4c6b65f695c1f2
Christian Fremerey <chfremer@chromium.org>
2018-06-06 17:23:51 +0000

    Reland 2 [Video Capture Service] Support accelerated jpeg decoding
    This had been reverted because of a test failure on webrtc Win10 Tester for a
    test that uses a real webcam.
    
    Patchset 1 is the code as previously reviewed and reverted.
    Patchset 2 adds a fix for the test failure.
    
    Fix description:
    
    The test failure revealed a mistake in the new code. Instead of operating the
    Mojo connection to the jpeg decoder on the main task runner of the video capture
    service, it would operate it from the thread the delivers the video frames from
    the camera. On Windows, this thread is not a Chromium thread and as such does
    not allow base::ThreadTaskRunnerHandle::Get(). The fix is to pass the correct
    task runner to the factory code, see Patchset 2.
    
    Original CL description:
    
    * Move/rename class content::VideoCaptureGpuJpegDecoder to
      media::VideoCaptureJpegDecoderImpl and break dependencies to |content| in order
      to make it reusable for the video capture service. (I was unable to convince
      Gerrit that this is a move/rename. Sorry for this making it harder to review.)
    * Split Gpu memory buffer factory methods from interface ui.mojom.Gpu to new interface
      ui.mojom.GpuMemoryBufferFactory
    * Update class ui::ClientGpuMemoryBufferManager to depend on only the new interface
      ui.mojom.GpuMemoryBufferFactory.
    * Inject dependencies on ui.mojom.GpuMemoryBufferFactory and viz.mojom.GpuService
      from Browser into video capture service when starting up the service.
    * Enable the existing-but-disabled test cases for accelerated jpeg decoding in
      combination with video capture service
    
    Design Doc: https://docs.google.com/document/d/169LS3U0DD7hHpqZSli0gpCaCqE1eWN4QXFSIGoruPwQ/edit?usp=sharing
    
    Test: content_browsertests --gtest_filter=VideoCaptureBrowserTest.*
    Bug: 820608, 720604
    Change-Id: I6c77ce570aa8f0c3a54a12e0d3f14648c58005ac
    
    TBR=avi@chromium.org,sadrul@chromium.org,jcliang@chromium.org,tsepez@chromium.org,sandersd@chromium.org,emircan@chromium.org,mojahsu@chromium.org,piman@chromium.org,chfremer@chromium.org
    
    Change-Id: Id3493742537fa8a016dad391ee12e2869febf080
    Reviewed-on: https://chromium-review.googlesource.com/1087867
    Commit-Queue: Christian Fremerey <chfremer@chromium.org>
    Reviewed-by: Christian Fremerey <chfremer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564942}

--- services/video_capture/receiver_mojo_to_media_adapter.h
+++ services/video_capture/receiver_mojo_to_media_adapter.h
@@ -12,39 +12,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace video_capture {
 
-class ReceiverOnTaskRunner : public media::VideoFrameReceiver {
- public:
-  ReceiverOnTaskRunner(std::unique_ptr<media::VideoFrameReceiver> receiver,
-                       scoped_refptr<base::SingleThreadTaskRunner> task_runner);
-  ~ReceiverOnTaskRunner() override;
-
-  // media::VideoFrameReceiver implementation.
-  void OnNewBuffer(int buffer_id,
-                   media::mojom::VideoBufferHandlePtr buffer_handle) override;
-  void OnFrameReadyInBuffer(
-      int buffer_id,
-      int frame_feedback_id,
-      std::unique_ptr<
-          media::VideoCaptureDevice::Client::Buffer::ScopedAccessPermission>
-          buffer_read_permission,
-      media::mojom::VideoFrameInfoPtr frame_info) override;
-  void OnBufferRetired(int buffer_id) override;
-  void OnError() override;
-  void OnLog(const std::string& message) override;
-  void OnStarted() override;
-  void OnStartedUsingGpuDecode() override;
-
- private:
-  std::unique_ptr<media::VideoFrameReceiver> receiver_;
-  const scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
-};
-
 // Adapter that allows a mojom::VideoFrameReceiver to be used in place of
 // a media::VideoFrameReceiver.
 class ReceiverMojoToMediaAdapter : public media::VideoFrameReceiver {
  public:
   ReceiverMojoToMediaAdapter(mojom::ReceiverPtr receiver);
   ~ReceiverMojoToMediaAdapter() override;
+
+  base::WeakPtr<media::VideoFrameReceiver> GetWeakPtr();
 
   // media::VideoFrameReceiver implementation.
   void OnNewBuffer(int buffer_id,
@@ -64,6 +39,7 @@ class ReceiverMojoToMediaAdapter : public media::VideoFrameReceiver {
 
  private:
   mojom::ReceiverPtr receiver_;
+  base::WeakPtrFactory<ReceiverMojoToMediaAdapter> weak_factory_;
 };
 
 }  // namespace video_capture
