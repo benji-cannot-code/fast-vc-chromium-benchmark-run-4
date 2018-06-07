c46ffb432d6e6cbc83febcb9aa87cd7cfda5afb2
David 'Digit' Turner <digit@google.com>
2018-06-07 21:08:13 +0000

    android: crazy-linker: Simplify zip library loading.
    Use SearchPathList::Find()'s ability to look directly inside
    a zip archive if the path contains a ! separator to simplify
    LibraryList::LoadLibraryFromZipFile().
    
    + LibraryList::LoadLibraryWithSystemLinker() is introduced to
      greatly simplify the code flow and the LibraryList::LoadLibrary()
      parameter list.
    
    This is part of an ongoing effort to support the component build,
    cleanup the crazy linker, and better detect system libraries.
    
    NOTE: The LegacyLinker.java change is necessary to ensure
          RELRO sharing works properly. The issue was that the
          RELRO was associated with the file path of the APK itself,
          instead of the library SO_NAME.
    
    BUG=802068,843987
    R=pasko@chromium.org, agrieve@chromium.org, rcmilroy@chromium.org
    
    Change-Id: I833b02635f71d0bf63794681ba78e9a33647e204
    Reviewed-on: https://chromium-review.googlesource.com/1091058
    Reviewed-by: agrieve <agrieve@chromium.org>
    Commit-Queue: David Turner <digit@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565407}

--- base/android/java/src/org/chromium/base/library_loader/LegacyLinker.java
+++ base/android/java/src/org/chromium/base/library_loader/LegacyLinker.java
@@ -491,7 +491,7 @@ class LegacyLinker extends Linker {
                 }
             }
 
-            String sharedRelRoName = libFilePath;
+            final String sharedRelRoName = libFilePath;
             if (zipFilePath != null) {
                 if (!nativeLoadLibraryInZipFile(zipFilePath, libFilePath, loadAddress, libInfo)) {
                     String errorMessage = "Unable to load library: " + libFilePath
@@ -499,7 +499,6 @@ class LegacyLinker extends Linker {
                     Log.e(TAG, errorMessage);
                     throw new UnsatisfiedLinkError(errorMessage);
                 }
-                sharedRelRoName = zipFilePath;
             } else {
                 if (!nativeLoadLibrary(libFilePath, loadAddress, libInfo)) {
                     String errorMessage = "Unable to load library: " + libFilePath;
