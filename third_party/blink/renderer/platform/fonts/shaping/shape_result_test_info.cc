6a614c033b7c2ff5c22792c8d97f33ec66edd398
Koji Ishii <kojii@chromium.org>
2018-06-07 17:39:18 +0000

    Change CreateSubRun to copy entire array
    This patch changes RunInfo::CreateSubRun() to copy the entire
    array, then modify character_index. LayoutNG uses CreateSubRun
    heavily, with much longer ShapeResult than the current layout
    engine, and this function turned out to be hot.
    
    perf_tests/layout/word-break-break-all.html gets ~10% faster,
    ~700ms to ~630ms.
    
    Bug: 636993
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: I869d1b90b1afe5cd84ad3b809ce8453f1f8feba4
    Reviewed-on: https://chromium-review.googlesource.com/1088527
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565326}

--- third_party/blink/renderer/platform/fonts/shaping/shape_result_test_info.cc
+++ third_party/blink/renderer/platform/fonts/shaping/shape_result_test_info.cc
@@ -14,6 +14,11 @@ unsigned ShapeResultTestInfo::NumberOfRunsForTesting() const {
   return runs_.size();
 }
 
+ShapeResult::RunInfo& ShapeResultTestInfo::RunInfoForTesting(
+    unsigned run_index) const {
+  return *runs_[run_index];
+}
+
 bool ShapeResultTestInfo::RunInfoForTesting(unsigned run_index,
                                             unsigned& start_index,
                                             unsigned& num_characters,
