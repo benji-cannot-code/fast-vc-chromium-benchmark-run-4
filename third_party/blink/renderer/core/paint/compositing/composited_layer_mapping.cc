6741ec292c4413621d6d5a9d398619233b1160fd
Xianzhu Wang <wangxianzhu@chromium.org>
2018-06-07 21:26:46 +0000

    [PE] Invalidate ChildClippingMaskLayer on resize in all paths
    Bug: 849991
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I3aee27a853c880ac5f20aca6b9a19b32bcd97d39
    Reviewed-on: https://chromium-review.googlesource.com/1091292
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565415}

--- third_party/blink/renderer/core/paint/compositing/composited_layer_mapping.cc
+++ third_party/blink/renderer/core/paint/compositing/composited_layer_mapping.cc
@@ -1620,6 +1620,7 @@ void CompositedLayerMapping::UpdateScrollingLayerGeometry(
     child_clipping_mask_layer_->SetSize(scrolling_layer_->Size());
     child_clipping_mask_layer_->SetOffsetFromLayoutObject(
         ToIntSize(overflow_clip_rect.Location()));
+    child_clipping_mask_layer_->SetNeedsDisplay();
   }
 
   bool overflow_clip_rect_offset_changed =
@@ -1688,6 +1689,7 @@ void CompositedLayerMapping::UpdateChildClippingMaskLayerGeometry() {
   child_clipping_mask_layer_->SetSize(graphics_layer_->Size());
   child_clipping_mask_layer_->SetOffsetFromLayoutObject(
       ToIntSize(padding_box.Location()));
+  child_clipping_mask_layer_->SetNeedsDisplay();
 
   // NOTE: also some stuff happening in updateChildContainmentLayerGeometry().
 }
