e8141cdb558cb1199e62d583e455b5cbc6ba7cd1
Filip Gorski <fgorski@google.com>
2018-06-07 17:26:17 +0000

    [EoC] Adding ability to parameterize fetch delay time
    * Adding ability to parameterize fetch delay time at a feature level.
    * Udpating and adding tests.
    
    Bug: 838748
    Change-Id: I87e2a33a9ee94111615b80566d6d611600271c29
    Reviewed-on: https://chromium-review.googlesource.com/1089881
    Commit-Queue: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565319}

--- chrome/android/junit/src/org/chromium/chrome/browser/contextual_suggestions/FetchHelperTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/contextual_suggestions/FetchHelperTest.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.contextual_suggestions;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
@@ -29,6 +30,7 @@ import org.robolectric.shadows.ShadowLooper;
 
 import org.chromium.base.Callback;
 import org.chromium.base.test.BaseRobolectricTestRunner;
+import org.chromium.chrome.browser.ChromeFeatureList;
 import org.chromium.chrome.browser.contextual_suggestions.FetchHelper.Delegate;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabObserver;
@@ -42,7 +44,10 @@ import org.chromium.content_public.browser.RenderFrameHost;
 import org.chromium.content_public.browser.WebContents;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
 /** Unit tests for FetchHelper. */
@@ -119,6 +124,11 @@ public final class FetchHelperTest {
         tabModels.add(mTabModel);
         doReturn(tabModels).when(mTabModelSelector).getModels();
         doReturn(mTab).when(mTabModelSelector).getCurrentTab();
+
+        // Ensure Chrome feature list does not switch to native.
+        Map<String, Boolean> testFeatures = new HashMap<>();
+        testFeatures.put(ChromeFeatureList.CONTEXTUAL_SUGGESTIONS_BOTTOM_SHEET, true);
+        ChromeFeatureList.setTestFeatures(testFeatures);
     }
 
     @Test
@@ -443,6 +453,12 @@ public final class FetchHelperTest {
         getTabObserver().onPageLoadFinished(mTab);
         runUntilFetchPossible();
         verify(mDelegate, times(0)).requestSuggestions(DIFFERENT_URL);
+    }
+
+    @Test
+    public void getMinimumFetchDelayMillis_Default() {
+        assertEquals(FetchHelper.getMinimumFetchDelayMillis(),
+                TimeUnit.SECONDS.toMillis(FetchHelper.MINIMUM_FETCH_DELAY_SECONDS));
     }
 
     private void addTab(Tab tab) {
