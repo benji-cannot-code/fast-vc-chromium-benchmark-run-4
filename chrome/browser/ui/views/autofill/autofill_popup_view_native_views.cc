19f3502d7bfeb9a58523b13acba0e6e36fa9114d
Fabio Tirelo <ftirelo@chromium.org>
2018-06-07 16:57:01 +0000

    [af] Set accessibility node for new dropdown
    The current implementation reads "autofill menu opened" for the old
    dropdown, but only "menu opened" for the new dropdown.
    
    Bug: 850094
    Change-Id: I825069866462897116dc179f6418a8f87f1c3467
    Reviewed-on: https://chromium-review.googlesource.com/1088764
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565300}

--- chrome/browser/ui/views/autofill/autofill_popup_view_native_views.cc
+++ chrome/browser/ui/views/autofill/autofill_popup_view_native_views.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/color_palette.h"
 #include "ui/gfx/font.h"
 #include "ui/gfx/geometry/rect_conversions.h"
+#include "ui/gfx/shadow_value.h"
 #include "ui/native_theme/native_theme.h"
 #include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/background.h"
@@ -35,8 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/style/typography.h"
 #include "ui/views/view.h"
 #include "ui/views/widget/widget.h"
-
-#include "ui/gfx/shadow_value.h"
 
 namespace {
 
