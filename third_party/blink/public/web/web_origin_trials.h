57e02726c308bb3897a3ed17e9d93898592536c5
Ian Clelland <iclelland@chromium.org>
2018-06-07 16:03:43 +0000

    Allow origin trials to be queried from /content/renderer
    This adds a WebOriginTrials interface, which can be used to query the state of
    an origin trial given a WebDocument.
    
    Bug: 845995
    Change-Id: Ifbaed167bcfd2588436bd32f6f03d3f57171434c
    Reviewed-on: https://chromium-review.googlesource.com/1088310
    Commit-Queue: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Jason Chase <chasej@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565283}

---
+++ third_party/blink/public/web/web_origin_trials.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_PUBLIC_WEB_WEB_ORIGIN_TRIALS_H_
+#define THIRD_PARTY_BLINK_PUBLIC_WEB_WEB_ORIGIN_TRIALS_H_
+
+#include "third_party/blink/renderer/core/core_export.h"
+
+namespace blink {
+
+class WebDocument;
+class WebString;
+
+// A namespace with tests for experimental features which can be enabled by the
+// origin trials framework via origin trial tokens.
+namespace WebOriginTrials {
+
+CORE_EXPORT bool isTrialEnabled(const WebDocument*, const WebString&);
+
+}  // namespace WebOriginTrials
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_PUBLIC_WEB_WEB_ORIGIN_TRIALS_H_
