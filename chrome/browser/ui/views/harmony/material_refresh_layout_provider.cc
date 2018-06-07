9bcb4a807c500ec1b57d594334ef6eb5269be50a
Peter Boström <pbos@chromium.org>
2018-06-07 00:21:21 +0000

    Adjust bookmark inkdrop insets for MD Refresh
    This provides circular inkdrops for favicon-only bookmarks for both
    Refresh and Touchable Refresh (the insets are different for the larger
    buttons). As this makes the ink-drop end cap start at the center of the
    favicon, this also looks significantly better for circular favicons.
    
    The change also fixes the way-too-small insets in Touchable Refresh (as
    they were overridden by Refresh in MaterialRefreshLayoutProvider).
    
    Bug: chromium:822072, chromium:834503
    Change-Id: I12586a7588a0fb5163d25b43ebd327df65da6862
    Reviewed-on: https://chromium-review.googlesource.com/1082477
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565115}

--- chrome/browser/ui/views/harmony/material_refresh_layout_provider.cc
+++ chrome/browser/ui/views/harmony/material_refresh_layout_provider.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/harmony/material_refresh_layout_provider.h"
 
+#include "ui/base/material_design/material_design_controller.h"
 #include "ui/views/layout/layout_provider.h"
 
 int MaterialRefreshLayoutProvider::GetDistanceMetric(int metric) const {
@@ -18,7 +19,11 @@ int MaterialRefreshLayoutProvider::GetDistanceMetric(int metric) const {
 gfx::Insets MaterialRefreshLayoutProvider::GetInsetsMetric(int metric) const {
   switch (metric) {
     case INSETS_BOOKMARKS_BAR_BUTTON:
-      return gfx::Insets(5, 8);
+      // The paddings here are chosen so that the resulting inkdrops are
+      // circular for favicon-only bookmarks.
+      if (ui::MaterialDesignController::IsTouchOptimizedUiEnabled())
+        return gfx::Insets(8, 10);
+      return gfx::Insets(5, 6);
   }
   return HarmonyLayoutProvider::GetInsetsMetric(metric);
 }
