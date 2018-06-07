aee893918a30d06b58eb7f27cc25024045c74300
glevin <glevin@chromium.org>
2018-06-07 18:06:55 +0000

    Harmonize CancelCastingDialog
    As per crbug.com/788014 Comment 1 (and previous dlgs):
    - Change button text "OK" -> "Continue"
    - Remove X (Close) button from upper right
    
    Bug: 788014
    Test: Cast to a screen while multi-logged in. Try switching profiles.
    Try to spot the Harmony.
    
    Change-Id: Icdb122f100d6f0b00129da7534150b7314a17686
    Reviewed-on: https://chromium-review.googlesource.com/1065021
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Greg Levin <glevin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565345}

--- ash/system/screen_security/screen_switch_check_controller.cc
+++ ash/system/screen_security/screen_switch_check_controller.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/system/tray/system_tray_notifier.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/strings/grit/ui_strings.h"
 #include "ui/views/controls/message_box_view.h"
 #include "ui/views/layout/fill_layout.h"
 #include "ui/views/window/dialog_delegate.h"
@@ -37,6 +38,14 @@ class CancelCastingDialog : public views::DialogDelegateView {
     return ui::DIALOG_BUTTON_OK | ui::DIALOG_BUTTON_CANCEL;
   }
 
+  base::string16 GetDialogButtonLabel(ui::DialogButton button) const override {
+    if (button == ui::DIALOG_BUTTON_OK)
+      return l10n_util::GetStringUTF16(IDS_DESKTOP_CASTING_ACTIVE_CONTINUE);
+    if (button == ui::DIALOG_BUTTON_CANCEL)
+      return l10n_util::GetStringUTF16(IDS_APP_CANCEL);
+    return base::string16();
+  }
+
   bool Cancel() override {
     std::move(callback_).Run(false);
     return true;
@@ -52,6 +61,8 @@ class CancelCastingDialog : public views::DialogDelegateView {
     std::move(callback_).Run(true);
     return true;
   }
+
+  bool ShouldShowCloseButton() const override { return false; }
 
  private:
   base::OnceCallback<void(bool)> callback_;
