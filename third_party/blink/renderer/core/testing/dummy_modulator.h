ed32ab745e41dc4faf8950dfd3f2999a0f1d2704
Kouhei Ueno <kouhei@chromium.org>
2018-06-07 05:05:35 +0000

    Make Modulator::ResolveModuleSpecifier virtual.
    After package-name-maps, the module specifier resolution algorithm
    will need to reference the settings object to access package name map.
    This CL makes the Modulator::ResolveModuleSpecifier(), which maps to
    https://html.spec.whatwg.org/multipage/webappapis.html#resolve-a-module-specifier
    take a settings object |this| by making it virtual.
    
    Bug: 848607
    Change-Id: I9592fff1d291e0fea3ec4c471ae76b38d4a43aa9
    Reviewed-on: https://chromium-review.googlesource.com/1081780
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565186}

--- third_party/blink/renderer/core/testing/dummy_modulator.h
+++ third_party/blink/renderer/core/testing/dummy_modulator.h
@@ -52,6 +52,7 @@ class DummyModulator : public Modulator {
       WebURLRequest::RequestContext destination,
       ModuleTreeClient*) override;
   ModuleScript* GetFetchedModuleScript(const KURL&) override;
+  KURL ResolveModuleSpecifier(const String&, const KURL&, String*) override;
   void FetchNewSingleModule(const ModuleScriptFetchRequest&,
                             const SettingsObject& fetch_client_settings_object,
                             ModuleGraphLevel,
