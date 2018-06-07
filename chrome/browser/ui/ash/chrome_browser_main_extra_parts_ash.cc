a841bcf84eb2a3e49f17d8567da9fb6dc8cffdab
Muyuan Li <muyuanli@chromium.org>
2018-06-07 21:14:10 +0000

    assistant: fix crash caused by assistant init timing.
    BUG=849760
    TEST=Manual
    
    Change-Id: I13ff6e252ab39672ed2e01d49044ed31e4d00683
    Reviewed-on: https://chromium-review.googlesource.com/1091176
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Muyuan Li <muyuanli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565410}

--- chrome/browser/ui/ash/chrome_browser_main_extra_parts_ash.cc
+++ chrome/browser/ui/ash/chrome_browser_main_extra_parts_ash.cc
@@ -198,6 +198,12 @@ void ChromeBrowserMainExtraPartsAsh::PreProfileInit() {
       chromeos::input_method::InputMethodManager::Get());
   ime_controller_client_->Init();
 
+#if BUILDFLAG(ENABLE_CROS_ASSISTANT)
+  // Assistant has to be initialized before session_controller_client to avoid
+  // race of SessionChanged event and assistant_client initialization.
+  assistant_client_ = std::make_unique<AssistantClient>();
+#endif
+
   session_controller_client_ = std::make_unique<SessionControllerClient>();
   session_controller_client_->Init();
 
@@ -264,10 +270,6 @@ void ChromeBrowserMainExtraPartsAsh::PostProfileInit() {
         chromeos::NetworkHandler::Get()->network_state_handler(),
         chromeos::NetworkHandler::Get()->auto_connect_handler());
   }
-
-#if BUILDFLAG(ENABLE_CROS_ASSISTANT)
-  assistant_client_ = std::make_unique<AssistantClient>();
-#endif
 }
 
 void ChromeBrowserMainExtraPartsAsh::PostBrowserStart() {
@@ -289,9 +291,6 @@ void ChromeBrowserMainExtraPartsAsh::PostMainMessageLoopRun() {
 
   night_light_client_.reset();
   data_promo_notification_.reset();
-#if BUILDFLAG(ENABLE_CROS_ASSISTANT)
-  assistant_client_.reset();
-#endif
   chrome_launcher_controller_initializer_.reset();
 
   wallpaper_controller_client_.reset();
@@ -301,6 +300,9 @@ void ChromeBrowserMainExtraPartsAsh::PostMainMessageLoopRun() {
   system_tray_client_.reset();
   shell_state_client_.reset();
   session_controller_client_.reset();
+#if BUILDFLAG(ENABLE_CROS_ASSISTANT)
+  assistant_client_.reset();
+#endif
   chrome_new_window_client_.reset();
   network_portal_notification_controller_.reset();
   media_client_.reset();
