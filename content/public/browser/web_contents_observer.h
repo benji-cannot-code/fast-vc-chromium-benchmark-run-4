53926058a89190e865a37109b27ec6cc3543901c
Becca Hughes <beccahughes@chromium.org>
2018-06-07 22:59:10 +0000

    Display Cutout: Track ViewportFit in WCO
    Adds a DisplayCutoutHost mojo interface that is implemented
    by WebContentsImpl. When ViewportFitChanged is called by
    the renderer we fire the ViewportFitChanged method on
    WCO. This allows the browser to change the UI based on
    the ViewportFit state.
    
    On the Blink side changes to ViewportFit bubble up through
    Document which will call the DisplayCutoutHost Mojo
    interface.
    
    BUG=838400
    
    Change-Id: I701f8c367167c5d630800f9d41004b88d95f63f3
    Reviewed-on: https://chromium-review.googlesource.com/1038786
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565449}

--- content/public/browser/web_contents_observer.h
+++ content/public/browser/web_contents_observer.h
@@ -27,6 +27,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/page_transition_types.h"
 #include "ui/base/window_open_disposition.h"
 
+namespace blink {
+namespace mojom {
+enum class ViewportFit;
+}  // namespace mojom
+}  // namespace blink
+
 namespace gfx {
 class Size;
 }  // namespace gfx
@@ -353,6 +359,9 @@ class CONTENT_EXPORT WebContentsObserver : public IPC::Listener {
   // in the DOM.
   virtual void PepperInstanceCreated() {}
   virtual void PepperInstanceDeleted() {}
+
+  // This method is called when the viewport fit of a WebContents changes.
+  virtual void ViewportFitChanged(blink::mojom::ViewportFit value) {}
 
   // Notification that a plugin has crashed.
   // |plugin_pid| is the process ID identifying the plugin process. Note that
