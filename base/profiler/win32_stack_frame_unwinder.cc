793e9b3bb6da3f634d07ee032c171b57a0bfd027
Xi Cheng <chengx@chromium.org>
2018-06-07 22:48:57 +0000

    Use global namespace for Windows APIs
    Change-Id: I8d7607fa1735d75cb1cc22c61dff5aa8aca8ed4c
    Reviewed-on: https://chromium-review.googlesource.com/1090322
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Commit-Queue: Xi Cheng <chengx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565448}

--- base/profiler/win32_stack_frame_unwinder.cc
+++ base/profiler/win32_stack_frame_unwinder.cc
@@ -68,7 +68,7 @@ PRUNTIME_FUNCTION Win32UnwindFunctions::LookupFunctionEntry(
     DWORD64 program_counter,
     PDWORD64 image_base) {
 #ifdef _WIN64
-  return RtlLookupFunctionEntry(program_counter, image_base, nullptr);
+  return ::RtlLookupFunctionEntry(program_counter, image_base, nullptr);
 #else
   NOTREACHED();
   return nullptr;
@@ -83,9 +83,9 @@ void Win32UnwindFunctions::VirtualUnwind(DWORD64 image_base,
   void* handler_data;
   ULONG64 establisher_frame;
   KNONVOLATILE_CONTEXT_POINTERS nvcontext = {};
-  RtlVirtualUnwind(UNW_FLAG_NHANDLER, image_base, program_counter,
-                   runtime_function, context, &handler_data,
-                   &establisher_frame, &nvcontext);
+  ::RtlVirtualUnwind(UNW_FLAG_NHANDLER, image_base, program_counter,
+                     runtime_function, context, &handler_data,
+                     &establisher_frame, &nvcontext);
 #else
   NOTREACHED();
 #endif
