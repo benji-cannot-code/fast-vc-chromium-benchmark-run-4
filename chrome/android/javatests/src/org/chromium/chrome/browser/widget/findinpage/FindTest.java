bdf2fe883c3e6923c3d73a60c5b7ce3572625862
Ted Choc <tedchoc@google.com>
2018-06-07 17:20:08 +0000

    Remove FlakyTest annotation from testBackKeyDoesNotDismissFindWhenImeIsPresent
    Ran locally 50 times w/o flake.  This also cleans up the logging
    and test prep to make it easier to detect systemic failures.
    
    BUG=673930
    
    Change-Id: I86349f9256ddc554c8de12d9dca80fc06458aa20
    Reviewed-on: https://chromium-review.googlesource.com/1089649
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565314}

--- chrome/android/javatests/src/org/chromium/chrome/browser/widget/findinpage/FindTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/widget/findinpage/FindTest.java
@@ -35,7 +35,6 @@ import org.junit.runner.RunWith;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Feature;
-import org.chromium.base.test.util.FlakyTest;
 import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeSwitches;
@@ -74,7 +73,7 @@ public class FindTest {
 
     @After
     public void tearDown() throws Exception {
-        mTestServer.stopAndDestroyServer();
+        if (mTestServer != null) mTestServer.stopAndDestroyServer();
     }
 
     /**
@@ -425,7 +424,7 @@ public class FindTest {
     @Test
     @MediumTest
     @Feature({"FindInPage"})
-    @FlakyTest(message = "https://crbug.com/673930")
+    @RetryOnFailure
     public void testBackKeyDoesNotDismissFindWhenImeIsPresent() throws InterruptedException {
         mActivityTestRule.loadUrl(mTestServer.getURL(FILEPATH));
         findInPageFromMenu();
@@ -462,13 +461,8 @@ public class FindTest {
 
     private void waitForIME(final boolean imePresent) {
         // Wait for IME to appear.
-        CriteriaHelper.pollUiThread(new Criteria("IME is not getting shown!") {
-            @Override
-            public boolean isSatisfied() {
-                return org.chromium.ui.UiUtils.isKeyboardShowing(
-                               mActivityTestRule.getActivity(), getFindQueryText())
-                        == imePresent;
-            }
-        });
+        CriteriaHelper.pollUiThread(Criteria.equals(imePresent,
+                () -> org.chromium.ui.UiUtils.isKeyboardShowing(
+                        mActivityTestRule.getActivity(), getFindQueryText())));
     }
 }
