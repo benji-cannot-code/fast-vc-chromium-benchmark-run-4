08112a3e7cb496d2154ac3e28aea270897dc9ff4
Rune Lillesveen <futhark@chromium.org>
2018-06-08 17:29:53 +0000

    [Squad] Make pseudo element creation not rely on layout tree.
    - Base layout parent computed style on traversing the DOM instead of the
      layout tree.
    - Moved top layer check for ::backdrop to CanGeneratePseudoElement().
    - Moved CanHaveGeneratedChildren() to layout tree building. This means
      we may generate pseudo elements which may not generate boxes for their
      pseudo elements, but the boxes will still not be created.
    
    First-letter is still relying on layout objects. Will address that
    later.
    
    Bug: 836126
    Change-Id: I4f283a09db597bc44d1b32d1007b4aa65e83f2e0
    Reviewed-on: https://chromium-review.googlesource.com/1092692
    Reviewed-by: Anders Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565668}

--- third_party/blink/renderer/core/dom/layout_tree_builder.cc
+++ third_party/blink/renderer/core/dom/layout_tree_builder.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/v0_insertion_point.h"
 #include "third_party/blink/renderer/core/dom/whitespace_attacher.h"
 #include "third_party/blink/renderer/core/html_names.h"
+#include "third_party/blink/renderer/core/layout/generated_children.h"
 #include "third_party/blink/renderer/core/layout/layout_inline.h"
 #include "third_party/blink/renderer/core/layout/layout_object.h"
 #include "third_party/blink/renderer/core/layout/layout_text.h"
@@ -123,6 +124,10 @@ bool LayoutTreeBuilderForElement::ShouldCreateLayoutObject() const {
     return false;
   if (!parent_layout_object->CanHaveChildren())
     return false;
+  if (node_->IsPseudoElement() &&
+      !CanHaveGeneratedChildren(*parent_layout_object)) {
+    return false;
+  }
   return node_->LayoutObjectIsNeeded(Style());
 }
 
