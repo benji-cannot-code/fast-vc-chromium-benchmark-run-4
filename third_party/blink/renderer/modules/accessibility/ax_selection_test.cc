312ae3ef9ca7481b7afce48ba183f44534803a16
Nektarios Paisios <nektar@chromium.org>
2018-06-08 17:00:08 +0000

    Adds a new selection test fixture that makes selections more readable in tests
    R=dmazzoni@chromium.org
    
    Change-Id: I8598961fa8007bac98164dfe4ce96cf52e655a5e
    Reviewed-on: https://chromium-review.googlesource.com/1091148
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565661}

--- third_party/blink/renderer/modules/accessibility/ax_selection_test.cc
+++ third_party/blink/renderer/modules/accessibility/ax_selection_test.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/editing/selection_template.h"
 #include "third_party/blink/renderer/modules/accessibility/ax_object.h"
 #include "third_party/blink/renderer/modules/accessibility/ax_position.h"
-#include "third_party/blink/renderer/modules/accessibility/testing/accessibility_test.h"
+#include "third_party/blink/renderer/modules/accessibility/testing/accessibility_selection_test.h"
 
 namespace blink {
 
@@ -19,13 +19,14 @@ namespace blink {
 // Basic tests.
 //
 
-TEST_F(AccessibilityTest, SetSelectionInText) {
+TEST_F(AccessibilitySelectionTest, SetSelectionInText) {
   SetBodyInnerHTML(R"HTML(<p id='paragraph'>Hello</p>)HTML");
   const Node* text = GetElementById("paragraph")->firstChild();
   ASSERT_NE(nullptr, text);
   const AXObject* ax_static_text =
       GetAXObjectByElementId("paragraph")->FirstChild();
   ASSERT_NE(nullptr, ax_static_text);
+
   const auto ax_base =
       AXPosition::CreatePositionInTextObject(*ax_static_text, 3);
   const auto ax_extent = AXPosition::CreatePositionAfterObject(*ax_static_text);
@@ -38,15 +39,18 @@ TEST_F(AccessibilityTest, SetSelectionInText) {
   EXPECT_EQ(3, dom_selection.Base().OffsetInContainerNode());
   EXPECT_EQ(text, dom_selection.Extent().AnchorNode());
   EXPECT_EQ(5, dom_selection.Extent().OffsetInContainerNode());
+  EXPECT_EQ("<Paragraph: ><StaticText: Hel^lo|>",
+            GetSelectionText(ax_selection));
 }
 
-TEST_F(AccessibilityTest, SetSelectionInTextWithWhiteSpace) {
+TEST_F(AccessibilitySelectionTest, SetSelectionInTextWithWhiteSpace) {
   SetBodyInnerHTML(R"HTML(<p id='paragraph'>     Hello</p>)HTML");
   const Node* text = GetElementById("paragraph")->firstChild();
   ASSERT_NE(nullptr, text);
   const AXObject* ax_static_text =
       GetAXObjectByElementId("paragraph")->FirstChild();
   ASSERT_NE(nullptr, ax_static_text);
+
   const auto ax_base =
       AXPosition::CreatePositionInTextObject(*ax_static_text, 3);
   const auto ax_extent = AXPosition::CreatePositionAfterObject(*ax_static_text);
@@ -59,6 +63,8 @@ TEST_F(AccessibilityTest, SetSelectionInTextWithWhiteSpace) {
   EXPECT_EQ(8, dom_selection.Base().OffsetInContainerNode());
   EXPECT_EQ(text, dom_selection.Extent().AnchorNode());
   EXPECT_EQ(10, dom_selection.Extent().OffsetInContainerNode());
+  EXPECT_EQ("<Paragraph: ><StaticText: Hel^lo|>",
+            GetSelectionText(ax_selection));
 }
 
 //
---
+++ third_party/blink/renderer/modules/accessibility/testing/accessibility_selection_test.h
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_ACCESSIBILITY_TESTING_ACCESSIBILITY_SELECTION_TEST_H_
+#define THIRD_PARTY_BLINK_RENDERER_MODULES_ACCESSIBILITY_TESTING_ACCESSIBILITY_SELECTION_TEST_H_
+
+#include <string>
+
+#include "third_party/blink/renderer/modules/accessibility/testing/accessibility_test.h"
+#include "third_party/blink/renderer/platform/wtf/allocator.h"
+
+namespace blink {
+
+class AXObject;
+class AXSelection;
+class LocalFrameClient;
+
+// Makes writing and debugging selection tests easier.
+class AccessibilitySelectionTest : public AccessibilityTest {
+  USING_FAST_MALLOC(AccessibilitySelectionTest);
+
+ public:
+  AccessibilitySelectionTest(LocalFrameClient* local_frame_client = nullptr);
+
+ protected:
+  std::string GetSelectionText(const AXSelection& selection) const;
+  std::string GetSelectionText(const AXSelection& selection,
+                               const AXObject& subtree) const;
+};
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_RENDERER_MODULES_ACCESSIBILITY_TESTING_ACCESSIBILITY_SELECTION_TEST_H_
