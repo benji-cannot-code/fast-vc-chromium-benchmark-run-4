9b7531234e73d5249551ec64e183323ae296b617
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-06-08 19:28:39 +0000

    Import wpt@ede8a377b16bae4482b312b30db328154fdba0db
    Using wpt-import in Chromium 6bfc16d6f7c1738fdb9141756520dab51283da1e.
    With Chromium commits locally applied on WPT:
    e8832df879 "webrtc wpt: remove test_state_change_event"
    fbdd17fbdb "Improve RTCPeerConnection-setRemoteDescription-tracks.https.html tests."
    c220be7c00 "Fire RTCPeerConnection.onsignalingstatechange in the correct order."
    4049f63911 "[LayoutNG] Orthogonal flows need available inline size for min/max calculation."
    69af6f380c "webrtc wpt: add cleanup to close peerconnections"
    ef28de4746 "service worker: Add long timeout to registration-updateviacache.https.html."
    
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/19209
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    igrigorik@chromium.org, panicker@chromium.org:
      external/wpt/performance-timeline
    jsbell@chromium.org:
      external/wpt/resources
    panicker@chromium.org:
      external/wpt/user-timing
    
    TBR=foolip
    
    No-Export: true
    Change-Id: Icbf1e91867e9cc114a7dedaaa8b7b9eba52a053f
    Reviewed-on: https://chromium-review.googlesource.com/1093116
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565714}

---
+++ third_party/WebKit/LayoutTests/external/wpt/user-timing/idlharness.any.js
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: global=window,worker
+// META: script=/resources/WebIDLParser.js
+// META: script=/resources/idlharness.js
+
+// https://w3c.github.io/user-timing/
+
+'use strict';
+
+promise_test(async () => {
+  const idl_array = new IdlArray();
+  const idl = await fetch("/interfaces/user-timing.idl").then(r => r.text());
+  const hrtime = await fetch("/interfaces/hr-time.idl").then(r => r.text());
+  const perf = await fetch("/interfaces/performance-timeline.idl").then(r => r.text());
+  const dom = await fetch("/interfaces/dom.idl").then(r => r.text());
+
+  idl_array.add_idls(idl);
+  idl_array.add_dependency_idls(hrtime);
+  idl_array.add_dependency_idls(perf);
+  idl_array.add_dependency_idls(dom);
+  idl_array.add_objects({
+    Performance: ["performance"]
+  });
+  idl_array.test();
+}, "Test IDL implementation of user-timing API");
