0d593e176fa9e437cfa280ded9b8e605b40a8093
manuk <manukh@chromium.org>
2018-06-08 13:56:00 +0000

    Omnibox: Disable Paste & Go for clipboard contents over 32KB
    Opening omnibox context menu when clipboard contains over 32K characters caused chrome to freeze up for ~1 minute.
    Other performance issues remain outside of the 'is paste & go allowed' checking and large clipboard sizes (.5 - 1 million characters) still cause freezing for ~10 seconds.
    
    Bug: 277732
    Change-Id: I7d130ef4e1668f6e73b2c670143ed69f7309cc6d
    Reviewed-on: https://chromium-review.googlesource.com/1089193
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565622}

--- components/omnibox/browser/omnibox_edit_model_unittest.cc
+++ components/omnibox/browser/omnibox_edit_model_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 
 #include <memory>
+#include <string>
 
 #include "base/test/scoped_feature_list.h"
 #include "base/test/scoped_task_environment.h"
@@ -260,4 +261,17 @@ TEST_F(OmniboxEditModelTest, GenerateMatchesFromFullFormattedUrl) {
   // Bypass the test class's mock method to test the real behavior.
   AutocompleteMatch match = model()->OmniboxEditModel::CurrentMatch(nullptr);
   EXPECT_EQ(AutocompleteMatchType::URL_WHAT_YOU_TYPED, match.type);
+}
+
+TEST_F(OmniboxEditModelTest, DisablePasteAndGoForLongTexts) {
+  EXPECT_TRUE(model()->OmniboxEditModel::CanPasteAndGo(
+      base::ASCIIToUTF16("short text")));
+
+  base::string16 almost_long_text = base::ASCIIToUTF16(
+      std::string(OmniboxEditModel::kMaxPasteAndGoTextLength, '.'));
+  EXPECT_TRUE(model()->OmniboxEditModel::CanPasteAndGo(almost_long_text));
+
+  base::string16 long_text = base::ASCIIToUTF16(
+      std::string(OmniboxEditModel::kMaxPasteAndGoTextLength + 1, '.'));
+  EXPECT_FALSE(model()->OmniboxEditModel::CanPasteAndGo(long_text));
 }
