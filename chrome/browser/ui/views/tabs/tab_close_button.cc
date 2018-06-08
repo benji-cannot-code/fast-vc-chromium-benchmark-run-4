220428b0393cf58e7b5dc0e524bafa4bdea168c7
Elly Fong-Jones <ellyjones@chromium.org>
2018-06-08 17:43:06 +0000

    views: support focus rings on tab-close buttons
    This change:
    1) Adds focus rings to the tab-close button
    2) Moves the new tab button to last in the TabStrip's focus order if it is not
       in LEADING position so that the keyboard focus order within the TabStrip is
       correct
    
    Positioning the ring for the tab-close button requires some finesse: the tab
    close button has its margins adjusted, but the focus ring only needs to draw
    around the visible part of the button.
    
    Bug: 848299
    Change-Id: I226683d3a8829f5a88e9bf102b4c8ba4ecfae459
    Reviewed-on: https://chromium-review.googlesource.com/1091131
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565673}

--- chrome/browser/ui/views/tabs/tab_close_button.cc
+++ chrome/browser/ui/views/tabs/tab_close_button.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/image/image_skia_operations.h"
 #include "ui/gfx/paint_vector_icon.h"
 #include "ui/views/rect_based_targeting_utils.h"
+#include "ui/views/style/platform_style.h"
 
 #if defined(USE_AURA)
 #include "ui/aura/env.h"
@@ -41,6 +42,10 @@ TabCloseButton::TabCloseButton(views::ButtonListener* listener,
   // Disable animation so that the red danger sign shows up immediately
   // to help avoid mis-clicks.
   SetAnimationDuration(0);
+  SetInstallFocusRingOnFocus(views::PlatformStyle::kPreferFocusRings);
+
+  if (focus_ring())
+    SetFocusPainter(nullptr);
 }
 
 TabCloseButton::~TabCloseButton() {}
@@ -105,6 +110,16 @@ void TabCloseButton::OnGestureEvent(ui::GestureEvent* event) {
 
 const char* TabCloseButton::GetClassName() const {
   return "TabCloseButton";
+}
+
+void TabCloseButton::Layout() {
+  ImageButton::Layout();
+  if (focus_ring()) {
+    focus_ring()->Layout();
+    SkPath path;
+    path.addOval(gfx::RectToSkRect(GetContentsBounds()));
+    focus_ring()->SetPath(path);
+  }
 }
 
 void TabCloseButton::PaintButtonContents(gfx::Canvas* canvas) {
