7db22495021076034977ae98b92e2dd15c0bed9f
Michael Giuffrida <michaelpg@chromium.org>
2018-06-08 00:33:11 +0000

    Move safe_search_api to //components
    The SafeSearchURLChecker didn't depend on //chrome. Move it to a new
    safe_search_api component.
    
    We will reuse safe_search_api::URLChecker in a later CL for filtering
    URLs based on the SafeSearch API (a generalization of
    SupervisedUserURLFilter).
    
    Because we're creating a namespace, we can drop the SafeSearch prefix
    and move the Classification enum out of the URLChecker class. Also clean
    up presubmit issues in changed lines.
    
    Bug: 819405
    Change-Id: Iaf411b1232eb937f93056d8e3553435fb7779bee
    Reviewed-on: https://chromium-review.googlesource.com/1081892
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Cait Phillips <caitkp@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Commit-Queue: Michael Giuffrida <michaelpg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565486}

--- components/safe_search_api/url_checker_unittest.cc
+++ components/safe_search_api/url_checker_unittest.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/safe_search_api/safe_search_url_checker.h"
+#include "components/safe_search_api/url_checker.h"
 
 #include <stddef.h>
 
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/json/json_writer.h"
 #include "base/macros.h"
 #include "base/message_loop/message_loop.h"
+#include "base/stl_util.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/values.h"
 #include "net/base/net_errors.h"
@@ -26,22 +27,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
 
-using Classification = SafeSearchURLChecker::Classification;
 using testing::_;
+
+namespace safe_search_api {
 
 namespace {
 
 const size_t kCacheSize = 2;
 
 const char* kURLs[] = {
-    "http://www.randomsite1.com",
-    "http://www.randomsite2.com",
-    "http://www.randomsite3.com",
-    "http://www.randomsite4.com",
-    "http://www.randomsite5.com",
-    "http://www.randomsite6.com",
-    "http://www.randomsite7.com",
-    "http://www.randomsite8.com",
+    "http://www.randomsite1.com", "http://www.randomsite2.com",
+    "http://www.randomsite3.com", "http://www.randomsite4.com",
+    "http://www.randomsite5.com", "http://www.randomsite6.com",
+    "http://www.randomsite7.com", "http://www.randomsite8.com",
     "http://www.randomsite9.com",
 };
 
@@ -50,8 +48,7 @@ const char kSafeSearchApiUrl[] =
 
 std::string BuildResponse(bool is_porn) {
   base::DictionaryValue dict;
-  std::unique_ptr<base::DictionaryValue> classification_dict(
-      new base::DictionaryValue);
+  auto classification_dict = std::make_unique<base::DictionaryValue>();
   if (is_porn)
     classification_dict->SetBoolean("pornography", is_porn);
   auto classifications_list = std::make_unique<base::ListValue>();
@@ -83,7 +80,7 @@ class SafeSearchURLCheckerTest : public testing::Test {
 
  protected:
   GURL GetNewURL() {
-    CHECK(next_url_ < arraysize(kURLs));
+    CHECK(next_url_ < base::size(kURLs));
     return GURL(kURLs[next_url_++]);
   }
 
@@ -125,7 +122,7 @@ class SafeSearchURLCheckerTest : public testing::Test {
   base::MessageLoop message_loop_;
   network::TestURLLoaderFactory test_url_loader_factory_;
   scoped_refptr<network::SharedURLLoaderFactory> test_shared_loader_factory_;
-  SafeSearchURLChecker checker_;
+  URLChecker checker_;
 };
 
 TEST_F(SafeSearchURLCheckerTest, Simple) {
@@ -198,3 +195,5 @@ TEST_F(SafeSearchURLCheckerTest, CacheTimeout) {
   EXPECT_CALL(*this, OnCheckDone(url, Classification::UNSAFE, false));
   ASSERT_FALSE(SendValidResponse(url, true));
 }
+
+}  // namespace safe_search_api
