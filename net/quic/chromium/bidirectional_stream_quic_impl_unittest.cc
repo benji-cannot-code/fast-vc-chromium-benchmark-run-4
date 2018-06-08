4171185be79d4cee8970cf995a2fe6c6fc7ef6f7
Zhongyi Shi <zhongyi@chromium.org>
2018-06-08 01:03:26 +0000

    Initialize default_network_ to the default network value tracked by QuicStreamFactory.
    Statistics collected in Net.QuicStreamFactory.DefaultNetworkMatche indicates that 99.66% sockets were bound to a network matched the default tracked by QuicStreamFactory.
    
    Bug: 790547
    Change-Id: If5b19a484e000c17f07082a96b5f1e3df20f6558
    Reviewed-on: https://chromium-review.googlesource.com/1089911
    Commit-Queue: Zhongyi Shi <zhongyi@chromium.org>
    Reviewed-by: Bence Béky <bnc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565495}

--- net/quic/chromium/bidirectional_stream_quic_impl_unittest.cc
+++ net/quic/chromium/bidirectional_stream_quic_impl_unittest.cc
@@ -511,6 +511,7 @@ class BidirectionalStreamQuicImplTest
         /*migrate_session_on_network_change*/ false,
         /*migrate_session_early*/ false,
         /*migrate_session_on_network_change_v2*/ false,
+        /*default_network*/ NetworkChangeNotifier::kInvalidNetworkHandle,
         base::TimeDelta::FromSeconds(kMaxTimeOnNonDefaultNetworkSecs),
         kMaxMigrationsToNonDefaultNetworkOnPathDegrading,
         kQuicYieldAfterPacketsRead,
