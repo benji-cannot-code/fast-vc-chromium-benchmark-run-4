674469cf4f7da4853e7248b364869e42a40c8fe3
Becca Hughes <beccahughes@chromium.org>
2018-06-08 01:02:48 +0000

    Display Cutout: Add controller and factory to Android
    Add a DisplayCutoutController that will decide when
    to adjust the layout based on the cutout. This depends
    on Android P APIs which are called by reflection.
    
    BUG=838400
    
    Change-Id: Iaf808709ded2f3c1b3d7a060942ea8c65396ad9f
    Reviewed-on: https://chromium-review.googlesource.com/1043248
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565494}

--- chrome/android/java/src/org/chromium/chrome/browser/tab/Tab.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/Tab.java
@@ -63,6 +63,7 @@ import org.chromium.chrome.browser.contextmenu.ContextMenuPopulator;
 import org.chromium.chrome.browser.contextualsearch.ContextualSearchTabHelper;
 import org.chromium.chrome.browser.crypto.CipherFactory;
 import org.chromium.chrome.browser.customtabs.CustomTabActivity;
+import org.chromium.chrome.browser.display_cutout.DisplayCutoutController;
 import org.chromium.chrome.browser.document.ChromeLauncherActivity;
 import org.chromium.chrome.browser.download.ChromeDownloadDelegate;
 import org.chromium.chrome.browser.feature_engagement.TrackerFactory;
@@ -560,6 +561,9 @@ public class Tab
     /** Whether or not the tab's active view is attached to the window. */
     private boolean mIsViewAttachedToWindow;
 
+    /** Controls display cutout states on the tab. */
+    private DisplayCutoutController mDisplayCutoutController;
+
     /**
      * Creates an instance of a {@link Tab}.
      *
@@ -646,6 +650,8 @@ public class Tab
                 updateInteractableState();
             }
         };
+
+        mDisplayCutoutController = new DisplayCutoutController(this);
     }
 
     private int calculateDefaultThemeColor() {
@@ -906,6 +912,13 @@ public class Tab
      */
     public final InfoBarContainer getInfoBarContainer() {
         return mInfoBarContainer;
+    }
+
+    /**
+     * @return The display cutout controller for the tab.
+     */
+    public final DisplayCutoutController getDisplayCutoutController() {
+        return mDisplayCutoutController;
     }
 
     /** @return An opaque "state" object that can be persisted to storage. */
