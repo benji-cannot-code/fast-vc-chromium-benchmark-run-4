4dfd5cbb53faf6dee7bd96492ede32e69fc0b0a4
Kyle Horimoto <khorimoto@google.com>
2018-06-08 00:10:11 +0000

    [CrOS MultiDevice] Create TimerFactory and associated test doubles.
    This is necessary for a follow-up CL which much inject a
    FakeTimerFactory.
    
    Bug: 824568, 752273
    Change-Id: I3575778517c5768074e970604f95729c1fb761ae
    Reviewed-on: https://chromium-review.googlesource.com/1091657
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565474}

---
+++ chromeos/services/secure_channel/ble_advertiser_impl_unittest.cc
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/services/secure_channel/ble_advertiser_impl.h"
+
+#include <memory>
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace chromeos {
+
+namespace secure_channel {
+
+namespace {}  // namespace
+
+class SecureChannelBleAdvertiserImplTest : public testing::Test {
+ protected:
+  SecureChannelBleAdvertiserImplTest() {}
+  ~SecureChannelBleAdvertiserImplTest() override = default;
+
+  // testing::Test:
+  void SetUp() override {}
+
+ private:
+ private:
+  DISALLOW_COPY_AND_ASSIGN(SecureChannelBleAdvertiserImplTest);
+};
+
+TEST_F(SecureChannelBleAdvertiserImplTest, UrelatedScanResults) {}
+
+}  // namespace secure_channel
+
+}  // namespace chromeos
