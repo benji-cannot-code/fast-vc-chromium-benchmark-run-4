4408e94264b02e52614b62c18e970ccdd0d37bfe
François Beaufort <beaufort.francois@gmail.com>
2018-06-08 23:53:27 +0000

    [Picture-in-Picture] Add browser test for disablePictureInPicture.
    This adds a browser test to check that setting disablePictureInPicture
    on video just after requesting Picture-in-Picture doesn't result in a
    window opened.
    
    Bug: 806249
    Change-Id: Idcc8c116f206a19ce3974c50ddff37b7bf2e8a50
    Reviewed-on: https://chromium-review.googlesource.com/1092350
    Commit-Queue: François Beaufort <beaufort.francois@gmail.com>
    Reviewed-by: apacible <apacible@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565781}

--- third_party/blink/renderer/modules/picture_in_picture/picture_in_picture_controller_impl.cc
+++ third_party/blink/renderer/modules/picture_in_picture/picture_in_picture_controller_impl.cc
@@ -97,7 +97,6 @@ void PictureInPictureControllerImpl::OnEnteredPictureInPicture(
       resolver->Reject(
           DOMException::Create(DOMExceptionCode::kInvalidStateError, ""));
     }
-    // TODO(crbug.com/806249): Test that WMPI sends the message.
     element->exitPictureInPicture(base::DoNothing());
     return;
   }
