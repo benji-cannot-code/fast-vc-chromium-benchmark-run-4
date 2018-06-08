3fc10a5338a40e56fcf5b013dda1f4af11f77b97
Tao Bai <michaelbai@chromium.org>
2018-06-08 00:04:40 +0000

    [WebView autofill] Add runtime log
    The log can be enabled by run
    setprop log.tag.AwAutofillManager DEBUG
    
    Bug: 843277
    Change-Id: I136d300b4fa11e42b3acce0f41ad832b56ff9938
    Reviewed-on: https://chromium-review.googlesource.com/1091302
    Reviewed-by: Changwan Ryu <changwan@chromium.org>
    Commit-Queue: Tao Bai <michaelbai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565472}

--- android_webview/java/src/org/chromium/android_webview/AwAutofillProvider.java
+++ android_webview/java/src/org/chromium/android_webview/AwAutofillProvider.java
@@ -256,6 +256,8 @@ public class AwAutofillProvider extends AutofillProvider {
         // return.
         if (mRequest == null) return;
         mRequest.fillViewStructure(structure);
+        AwAutofillManager.log(
+                "onProvideAutoFillVirtualStructure fields:" + structure.getChildCount());
         mAutofillUMA.onVirtualStructureProvided();
     }
 
@@ -263,6 +265,7 @@ public class AwAutofillProvider extends AutofillProvider {
     public void autofill(final SparseArray<AutofillValue> values) {
         if (mNativeAutofillProvider != 0 && mRequest != null && mRequest.autofill((values))) {
             autofill(mNativeAutofillProvider, mRequest.mFormData);
+            AwAutofillManager.log("autofill values:" + values.size());
             mAutofillUMA.onAutofill();
         }
     }
