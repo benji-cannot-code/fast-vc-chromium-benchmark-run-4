b65272fc9b2ed14a84a6edeb0d3caee65ff64726
Jiaquan He <hejq@google.com>
2018-06-08 19:54:06 +0000

    app_list: cleanups.
    This commit:
    
    - removes some TODOs;
    - uses kGridIconDimension in ash/public/cpp/app_list/app_list_constants.h;
    - passes a trimmed query string over mojom for searching, since it's smaller
      and the raw query string in Chrome was only needed by search history, which
      has been removed.
    
    Bug: 733662
    Change-Id: I2f8e7f5294079b4a9ac0141384502cc7dda1a806
    Reviewed-on: https://chromium-review.googlesource.com/1093388
    Commit-Queue: Jiaquan He <hejq@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565721}

--- ash/app_list/model/folder_image.cc
+++ ash/app_list/model/folder_image.cc
@@ -5,10 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/app_list/model/folder_image.h"
 
+#include <memory>
 #include <vector>
 
 #include "ash/app_list/model/app_list_item.h"
 #include "ash/app_list/model/app_list_item_list.h"
+#include "ash/public/cpp/app_list/app_list_constants.h"
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -28,9 +30,6 @@ namespace {
 constexpr int kItemIconDimension = 16;
 constexpr float kFolderBubbleRadius = 23;
 constexpr float kFolderBubbleOffsetY = 1;
-// Keep this consistent with |kGridIconDimension| in app_list_constants.cc.
-// TODO(hejq): Figure out a way to consolidate the two.
-constexpr int kGridIconDimension = 48;
 
 // Gets the size of a small app icon inside the folder icon.
 gfx::Size ItemIconSize() {
