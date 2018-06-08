5aacccc99c9394b9da914486a4d7cda0142d1bef
Ted Choc <tedchoc@google.com>
2018-06-08 17:21:26 +0000

    Fix ChromeBrowserProvider bookmark observer thread issue.
    The BookmarkModelObserverTask added a bookmark model observer
    on a background thread, which is not allowed.
    
    In both cases of Remove and Update, there is no need for listening
    to the observer as we can just track whether we are updating the
    bookmark node internally.  This is consistent with how
    AddBookmarkTask already worked.
    
    BUG=838988
    
    Change-Id: I43bf054b70e78907a99ba2a11fac62df0af6bc67
    Reviewed-on: https://chromium-review.googlesource.com/1089497
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565667}

---
+++ chrome/browser/android/provider/bookmark_model_task.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/android/provider/bookmark_model_task.h"
+#include "components/bookmarks/browser/bookmark_model.h"
+#include "components/bookmarks/browser/model_loader.h"
+#include "content/public/browser/browser_thread.h"
+
+using bookmarks::BookmarkModel;
+using bookmarks::BookmarkNode;
+using content::BrowserThread;
+
+BookmarkModelTask::BookmarkModelTask(BookmarkModel* model) : model_(model) {
+  // Ensure the initialization of the native bookmark model.
+  DCHECK(!BrowserThread::CurrentlyOn(BrowserThread::UI));
+  DCHECK(model_);
+  model_->model_loader()->BlockTillLoaded();
+}
+
+BookmarkModel* BookmarkModelTask::model() const {
+  return model_;
+}
