8c405d5453fb49a5075144c007bad80080ab9856
Matt Falkenhagen <falken@chromium.org>
2018-06-08 04:53:00 +0000

    Disallow access to opaque CSS responses.
    Bug: 848786
    Change-Id: Ie53fbf644afdd76d7c65649a05c939c63d89b4ec
    Reviewed-on: https://chromium-review.googlesource.com/1088335
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565537}

--- third_party/blink/renderer/core/css/parser/css_parser_context.h
+++ third_party/blink/renderer/core/css/parser/css_parser_context.h
@@ -44,6 +44,7 @@ class CORE_EXPORT CSSParserContext
 
   static CSSParserContext* Create(const CSSParserContext* other,
                                   const KURL& base_url_override,
+                                  bool is_opaque_response_from_service_worker,
                                   ReferrerPolicy referrer_policy_override,
                                   const WTF::TextEncoding& charset_override,
                                   const Document* use_counter_document);
@@ -57,6 +58,7 @@ class CORE_EXPORT CSSParserContext
   static CSSParserContext* Create(
       const Document&,
       const KURL& base_url_override,
+      bool is_opaque_response_from_service_worker,
       ReferrerPolicy referrer_policy_override,
       const WTF::TextEncoding& charset = WTF::TextEncoding(),
       SelectorProfile = kLiveProfile);
@@ -75,6 +77,9 @@ class CORE_EXPORT CSSParserContext
   const Referrer& GetReferrer() const { return referrer_; }
   bool IsHTMLDocument() const { return is_html_document_; }
   bool IsLiveProfile() const { return profile_ == kLiveProfile; }
+
+  // See documentation in StyleSheetContents for this function.
+  bool IsOpaqueResponseFromServiceWorker() const;
 
   bool IsSecureContext() const;
 
@@ -110,6 +115,7 @@ class CORE_EXPORT CSSParserContext
 
  private:
   CSSParserContext(const KURL& base_url,
+                   bool is_opaque_response_from_service_worker,
                    const WTF::TextEncoding& charset,
                    CSSParserMode,
                    CSSParserMode match_mode,
@@ -122,6 +128,7 @@ class CORE_EXPORT CSSParserContext
                    const Document* use_counter_document);
 
   KURL base_url_;
+  const bool is_opaque_response_from_service_worker_;
   WTF::TextEncoding charset_;
   CSSParserMode mode_;
   CSSParserMode match_mode_;
