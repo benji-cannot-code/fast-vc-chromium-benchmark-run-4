340703f5ea90122494242c18a3d6b7423de867d1
Kyle Horimoto <khorimoto@google.com>
2018-06-08 02:34:42 +0000

    [CrOS MultiDevice] Add an ID to FakeErrorTolerantBleAdvertisement.
    Bug: 824568, 752273
    Change-Id: I7a58651e9a137ae0c35c7a8085d931ad3cf28647
    Reviewed-on: https://chromium-review.googlesource.com/1092161
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565517}

--- chromeos/services/secure_channel/fake_error_tolerant_ble_advertisement.h
+++ chromeos/services/secure_channel/fake_error_tolerant_ble_advertisement.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/macros.h"
+#include "base/unguessable_token.h"
 #include "chromeos/services/secure_channel/device_id_pair.h"
 #include "chromeos/services/secure_channel/error_tolerant_ble_advertisement.h"
 
@@ -23,6 +24,8 @@ class FakeErrorTolerantBleAdvertisement : public ErrorTolerantBleAdvertisement {
       base::OnceCallback<void(const DeviceIdPair&)> destructor_callback);
   ~FakeErrorTolerantBleAdvertisement() override;
 
+  const base::UnguessableToken& id() const { return id_; }
+
   void InvokeStopCallback();
 
   // ErrorTolerantBleAdvertisement:
@@ -30,6 +33,7 @@ class FakeErrorTolerantBleAdvertisement : public ErrorTolerantBleAdvertisement {
   bool HasBeenStopped() override;
 
  private:
+  base::UnguessableToken id_;
   base::OnceCallback<void(const DeviceIdPair&)> destructor_callback_;
   base::Closure stop_callback_;
 
