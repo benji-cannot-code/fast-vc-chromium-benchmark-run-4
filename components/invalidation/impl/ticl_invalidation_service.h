e6db6899ecedea3fe6d07163b51bb2b6e13d36b6
Colin Blundell <blundell@chromium.org>
2018-06-08 13:06:03 +0000

    Introduce IdentityProvider interface for invalidating access tokens
    One of IdentityProvider's clients' use cases is to invalidate access
    tokens for the active account. IdentityProvider currently provides no
    explicit mechanism for this, instead letting clients directly interact
    with the underlying OAuth2TokenService instance via GetTokenService().
    However, it's necessary to decouple IdentityProvider from
    OAuth2TokenService at an interface level in order to allow for porting
    ProfileIdentityProvider to be backed by IdentityManager.
    
    This CL adds an explicit interface for clients of IdentityProvider to
    invalidate access tokens for the active account. This interface is
    currently implemented directly in IdentityProvider itself, as it is
    common to ProfileIdentityProvider and DeviceIdentityProvider. Before
    porting ProfileIdentityProvider to be backed by IdentityManager we
    will move this implementation out into those two derived classes,
    temporarily duplicating it. However, we will only do that when we are
    just at the point of porting ProfileIdentityProvider away from using
    ProfileOAuth2TokenService.
    
    This CL finishes eliminating the exposure of OAuth2TokenService to
    IdentityProvider clients. The next step is to decouple the
    IdentityProvider implementation itself from the token service.
    
    Bug: 809452
    Change-Id: Idf3d2b35fa88b5b96dfb7204268e298222032d3d
    Reviewed-on: https://chromium-review.googlesource.com/1089056
    Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565615}

--- components/invalidation/impl/ticl_invalidation_service.h
+++ components/invalidation/impl/ticl_invalidation_service.h
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/invalidation/public/invalidation_handler.h"
 #include "components/invalidation/public/invalidation_service.h"
 #include "components/keyed_service/core/keyed_service.h"
-#include "google_apis/gaia/oauth2_token_service.h"
 #include "net/base/backoff_entry.h"
 
 namespace gcm {
@@ -132,7 +131,7 @@ class TiclInvalidationService : public InvalidationService,
   std::unique_ptr<syncer::Invalidator> invalidator_;
 
   // TiclInvalidationService needs to remember access token in order to
-  // invalidate it with OAuth2TokenService.
+  // invalidate it with IdentityProvider.
   std::string access_token_;
 
   // TiclInvalidationService needs to hold reference to access_token_fetcher_
