611d5b8aca07187162ef0210f534db8288ce968b
Yoichi Osato <yoichio@chromium.org>
2018-06-09 01:49:20 +0000

    [LayoutNG] Paint single hard line break.
    This patch lets NGTextFragmentPainter paint hard line break: BR and \n on white-space:pre,
    even it is only character in a line or only selected in a line.
    
    To implement that, this patch splits existing algorithm to soft line break and
    hard line break.
    This patch almost reuse existing code for soft line break and implement new
    for hard line break:
    1. Update LayoutSelection to compute inline offset for BR element.
    2. Update NGPaintFragment::ComputeLocalSelectionRect to return space width rect for BR.
    
    LayoutNG/fast/text/selection/selection-hard-linebreak-expected.png
     This rebase is needed only for 1 px gap. Linebreak paint is compatible to old.
    
    Bug: 708452
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_layout_ng;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I7134b169120c9c152dfc366da3695f1af15e36e4
    Reviewed-on: https://chromium-review.googlesource.com/1084573
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565814}

--- third_party/blink/renderer/core/paint/ng/ng_text_fragment_painter.cc
+++ third_party/blink/renderer/core/paint/ng/ng_text_fragment_painter.cc
@@ -37,6 +37,8 @@ inline bool ShouldPaintTextFragment(const NGPhysicalTextFragment& text_fragment,
   // This code path is only called in PaintPhaseForeground whereas we would
   // expect PaintPhaseSelection. The existing haveSelection logic in paint()
   // tests for != PaintPhaseTextClip.
+  if (text_fragment.IsLineBreak())
+    return true;
   if (!text_fragment.Length() || !text_fragment.TextShapeResult())
     return false;
 
@@ -162,6 +164,10 @@ void NGTextFragmentPainter::Paint(const PaintInfo& paint_info,
     PaintSelection(context, fragment_, document, style,
                    selection_style.fill_color, box_rect, selection_status);
   }
+
+  // Line break needs only selection painting.
+  if (text_fragment.IsLineBreak())
+    return;
 
   const NGLineOrientation orientation = text_fragment.LineOrientation();
   if (orientation != NGLineOrientation::kHorizontal) {
