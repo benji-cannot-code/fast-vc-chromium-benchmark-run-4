84b70348fac4839a9b9674d20d62e3a7a8690270
Gabriel Charette <gab@chromium.org>
2018-06-09 05:45:34 +0000

    [MessageLoop] Add metrics to diagnose delayed task queue issues.
    TBR=fdoray@chromium.org
    
    Bug: 850450, 786597
    Change-Id: Iaa8a9d713c5202f371b0fd549d140bbe791e4089
    Reviewed-on: https://chromium-review.googlesource.com/1094296
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565867}

--- base/message_loop/incoming_task_queue.cc
+++ base/message_loop/incoming_task_queue.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_helpers.h"
 #include "base/location.h"
 #include "base/message_loop/message_loop.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/synchronization/waitable_event.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
@@ -85,6 +86,10 @@ bool IncomingTaskQueue::AddToIncomingQueue(const Location& from_here,
     pending_task.is_high_res = true;
   }
 #endif
+
+  if (!delay.is_zero())
+    UMA_HISTOGRAM_LONG_TIMES("MessageLoop.DelayedTaskQueue.PostedDelay", delay);
+
   return PostPendingTask(&pending_task);
 }
 
@@ -125,6 +130,12 @@ IncomingTaskQueue::~IncomingTaskQueue() {
 void IncomingTaskQueue::RunTask(PendingTask* pending_task) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   task_annotator_.RunTask("MessageLoop::PostTask", pending_task);
+}
+
+void IncomingTaskQueue::ReportMetricsOnIdle() const {
+  UMA_HISTOGRAM_COUNTS_1M(
+      "MessageLoop.DelayedTaskQueue.PendingTasksCountOnIdle",
+      delayed_tasks_.Size());
 }
 
 IncomingTaskQueue::TriageQueue::TriageQueue(IncomingTaskQueue* outer)
@@ -249,6 +260,11 @@ void IncomingTaskQueue::DelayedQueue::Clear() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(outer_->sequence_checker_);
   while (!queue_.empty())
     Pop();
+}
+
+size_t IncomingTaskQueue::DelayedQueue::Size() const {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(outer_->sequence_checker_);
+  return queue_.size();
 }
 
 IncomingTaskQueue::DeferredQueue::DeferredQueue(IncomingTaskQueue* outer)
