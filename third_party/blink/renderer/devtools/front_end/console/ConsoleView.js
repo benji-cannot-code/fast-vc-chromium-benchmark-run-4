bfe5b9677920efad394d652942d8e5a94dbfead5
Erik Luo <luoe@chromium.org>
2018-06-09 01:34:01 +0000

    DevTools: fix muted 'Store as global variable' in console
    Console assumes that every 'mousedown' event is followed by either a
    'mouseup' or 'mouseleave' event. This key assumption allows muting
    viewport updates for scrollbar dragging, wheel-up-from-bottom, and
    pageUp events.
    
    This assumption is false for right clicks that: occur too rapidly or
    mouseup outside the window. This CL prevents viewport muting on
    right clicks.
    
    Screenshot of bug: https://imgur.com/a/iC2lkgU
    
    Bug: 807682
    Change-Id: I2bb4f103eadc28950968c451b80e3186733e32ce
    Reviewed-on: https://chromium-review.googlesource.com/917982
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Joel Einbinder <einbinder@chromium.org>
    Commit-Queue: Erik Luo <luoe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565812}

--- third_party/blink/renderer/devtools/front_end/console/ConsoleView.js
+++ third_party/blink/renderer/devtools/front_end/console/ConsoleView.js
@@ -208,7 +208,8 @@ Console.ConsoleView = class extends UI.VBox {
 
     UI.context.addFlavorChangeListener(SDK.ExecutionContext, this._executionContextChanged, this);
 
-    this._messagesElement.addEventListener('mousedown', this._updateStickToBottomOnMouseDown.bind(this), false);
+    this._messagesElement.addEventListener(
+        'mousedown', event => this._updateStickToBottomOnMouseDown(event.button === 2), false);
     this._messagesElement.addEventListener('mouseup', this._updateStickToBottomOnMouseUp.bind(this), false);
     this._messagesElement.addEventListener('mouseleave', this._updateStickToBottomOnMouseUp.bind(this), false);
     this._messagesElement.addEventListener('wheel', this._updateStickToBottomOnWheel.bind(this), false);
@@ -1096,8 +1097,11 @@ Console.ConsoleView = class extends UI.VBox {
     highlightNode.scrollIntoViewIfNeeded();
   }
 
-  _updateStickToBottomOnMouseDown() {
-    this._muteViewportUpdates = true;
+  /**
+   * @param {boolean=} isRightClick
+   */
+  _updateStickToBottomOnMouseDown(isRightClick) {
+    this._muteViewportUpdates = !isRightClick;
     this._viewport.setStickToBottom(false);
     if (this._waitForScrollTimeout) {
       clearTimeout(this._waitForScrollTimeout);
