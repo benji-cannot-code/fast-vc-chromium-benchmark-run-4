ff416acacb72887603e3a0306599897424e85055
Jeremy Klein <jlklein@google.com>
2018-06-09 21:05:48 +0000

    Add a feature flag for CrOS Android Messages integration.
    R=khorimoto@chromium.org
    
    Bug: 850823
    Change-Id: I7a4b0bbfad4b553929aebd59a508ab11ce208ac1
    Reviewed-on: https://chromium-review.googlesource.com/1092192
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Jeremy Klein <jlklein@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565892}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -50,6 +50,9 @@ extern const char kAllowInsecureLocalhostDescription[];
 extern const char kAllowNaclSocketApiName[];
 extern const char kAllowNaclSocketApiDescription[];
 
+extern const char kAndroidMessagesIntegrationName[];
+extern const char kAndroidMessagesIntegrationDescription[];
+
 extern const char kAppBannersName[];
 extern const char kAppBannersDescription[];
 
