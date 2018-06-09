708db564ed898903ef1e12f8680bb99d98d0dc30
Andrey Kosyakov <caseq@chromium.org>
2018-06-09 08:08:26 +0000

    DevTools: expose URL fragment as part of network Request in protocol
    Change-Id: I26ec7afcb2f8b35970e4aadf017663f001e3ca84
    Reviewed-on: https://chromium-review.googlesource.com/1093980
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565873}

---
+++ third_party/WebKit/LayoutTests/http/tests/inspector-protocol/network/url-fragment.js
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  var {page, session, dp} = await testRunner.startBlank(
+      `Tests the we properly report url fragment in network requests`);
+
+  dp.Network.enable();
+  dp.Page.enable();
+
+  dp.Network.onRequestIntercepted(event => {
+    const request = event.params.request;
+    testRunner.log(`Intercepted URL: ${request.url} fragment: ${request.urlFragment}`);
+    dp.Network.continueInterceptedRequest({interceptionId: event.params.interceptionId});
+  });
+  dp.Network.onRequestWillBeSent(event => {
+    const request = event.params.request;
+    testRunner.log(`Request will be sent: ${request.url} fragment: ${request.urlFragment}`);
+  });
+  dp.Network.setRequestInterception({patterns: [{}]});
+  const pageURL = '/inspector-protocol/network/resources/simple.html';
+  await dp.Page.navigate({url: pageURL});
+  await dp.Page.navigate({url: pageURL + '#ref'});
+  const resourceURL = '/devtools/network/resources/resource.php';
+  await session.evaluateAsync(`fetch("${resourceURL}")`);
+  await session.evaluateAsync(`fetch("${resourceURL}#ref")`);
+  await session.evaluateAsync(`fetch("${resourceURL}#")`);
+  testRunner.completeTest();
+})
