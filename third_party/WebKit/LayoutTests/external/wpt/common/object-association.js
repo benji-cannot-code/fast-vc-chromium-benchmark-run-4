918135393322e3a7d7a072bbb53ec958a68d5d81
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-06-09 13:56:06 +0000

    Import wpt@4ce3dabd577b30ca091fb217b2bed719e6794ead
    Using wpt-import in Chromium be015f13686dad8f28115b272442d18835386852.
    With Chromium commits locally applied on WPT:
    e8832df879 "webrtc wpt: remove test_state_change_event"
    fbdd17fbdb "Improve RTCPeerConnection-setRemoteDescription-tracks.https.html tests."
    c220be7c00 "Fire RTCPeerConnection.onsignalingstatechange in the correct order."
    4049f63911 "[LayoutNG] Orthogonal flows need available inline size for min/max calculation."
    69af6f380c "webrtc wpt: add cleanup to close peerconnections"
    03ef65db57 "Update existing WebUSB WPT to be multi-global"
    
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/19238
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    TBR=robertma
    
    No-Export: true
    Change-Id: I020cc126b7b724a34a62b689d718724b178928ea
    Reviewed-on: https://chromium-review.googlesource.com/1094414
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565877}

--- third_party/WebKit/LayoutTests/external/wpt/common/object-association.js
+++ third_party/WebKit/LayoutTests/external/wpt/common/object-association.js
@@ -15,7 +15,7 @@ window.testIsPerWindow = propertyName => {
     iframe.remove();
 
     const after = frame[propertyName];
-    assert_equals(after, before);
+    assert_equals(after, before, `window.${propertyName} should not change after iframe.remove()`);
   }, `Discarding the browsing context must not change window.${propertyName}`);
 
   async_test(t => {
@@ -57,6 +57,8 @@ window.testIsPerWindow = propertyName => {
 
       const after = frame[propertyName];
       assert_not_equals(after, before);
+
+      frame.document.close();
     });
 
     iframe.src = "/common/blank.html";
