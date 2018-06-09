96460f991836bd53c34644766f35cb19eb5cf1aa
Sadrul Habib Chowdhury <sadrul@chromium.org>
2018-06-09 04:30:20 +0000

    latency: Remove unused LatencyInfo::RemoveLatency().
    RemoveLatency() is no longer used, so it is removed.
    
    BUG=849719
    
    Change-Id: I1e3b9338f7e70d4e31e0e2bb2cae96966592210e
    Reviewed-on: https://chromium-review.googlesource.com/1092631
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565858}

--- ui/latency/latency_info.h
+++ ui/latency/latency_info.h
@@ -179,8 +179,6 @@ class LatencyInfo {
   // component is found.
   bool FindLatency(LatencyComponentType type, LatencyComponent* output) const;
 
-  void RemoveLatency(LatencyComponentType type);
-
   void Terminate();
 
   const LatencyMap& latency_components() const { return latency_components_; }
