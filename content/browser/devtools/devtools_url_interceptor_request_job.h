294f93c79f90715266c22bc06273c7b0723c7eb6
Pavel Feldman <pfeldman@chromium.org>
2018-06-09 03:29:28 +0000

    DevTools: allow network interceptor to access the cookie header.
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I22d22a17aac5ae5c8b564a5f446c28e1ad45e200
    Reviewed-on: https://chromium-review.googlesource.com/1093588
    Commit-Queue: Pavel Feldman <pfeldman@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565835}

--- content/browser/devtools/devtools_url_interceptor_request_job.h
+++ content/browser/devtools/devtools_url_interceptor_request_job.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/devtools/protocol/network.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/common/resource_type.h"
+#include "net/cookies/canonical_cookie.h"
 #include "net/http/http_raw_request_headers.h"
 #include "net/http/http_response_headers.h"
 #include "net/url_request/url_request.h"
@@ -105,12 +106,15 @@ class DevToolsURLInterceptorRequestJob : public net::URLRequestJob {
     GURL url;
     std::string method;
     std::unique_ptr<net::UploadDataStream> post_data;
+    std::string cookie_line;
     net::HttpRequestHeaders extra_request_headers;
     std::string referrer;
     net::URLRequest::ReferrerPolicy referrer_policy;
     net::RequestPriority priority;
     const net::URLRequestContext* url_request_context;
   };
+
+  void StartWithCookies(const net::CookieList& cookies);
 
   // Callbacks from SubRequest.
   void OnSubRequestAuthRequired(net::AuthChallengeInfo* auth_info);
