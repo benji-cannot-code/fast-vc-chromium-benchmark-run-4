9c114fd55ff57a69933e1111fd699cd19fdb3d61
Ryan Sturm <ryansturm@chromium.org>
2018-06-09 02:27:34 +0000

    Adding a capping heavy pages about:flag entry
    This allows users to turn on capping heavy pages. Users will be able to
    pause and resume resource loading on pages.
    
    Bug: 851136
    Change-Id: I2587d815f74bd46415e6065ebfe8c94d7127c06f
    Reviewed-on: https://chromium-review.googlesource.com/1093757
    Commit-Queue: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565824}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "build/build_config.h"
 #include "cc/base/switches.h"
+#include "chrome/browser/data_use_measurement/page_load_capping/chrome_page_load_capping_features.h"
 #include "chrome/browser/flag_descriptions.h"
 #include "chrome/browser/predictors/loading_predictor_config.h"
 #include "chrome/browser/predictors/resource_prefetch_common.h"
@@ -909,6 +910,14 @@ const FeatureEntry::FeatureVariation kDataReductionMainMenuFeatureVariations[] =
     {{"(persistent)", kPersistentMenuItemEnabled,
       arraysize(kPersistentMenuItemEnabled), nullptr}};
 #endif  // OS_ANDROID
+
+const FeatureEntry::FeatureParam kDetectingHeavyPagesLowThresholdEnabled[] = {
+    {"PageCapMiB", "1"},
+    {"MediaPageCapMiB", "1"}};
+
+const FeatureEntry::FeatureVariation kDetectingHeavyPagesFeatureVariations[] = {
+    {"(Low)", kDetectingHeavyPagesLowThresholdEnabled,
+     base::size(kDetectingHeavyPagesLowThresholdEnabled), nullptr}};
 
 const FeatureEntry::Choice kEnableOutOfProcessHeapProfilingChoices[] = {
     {flags_ui::kGenericExperimentChoiceDisabled, "", ""},
@@ -2057,6 +2066,13 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kEnableOptimizationHintsName,
      flag_descriptions::kEnableOptimizationHintsDescription, kOsAll,
      FEATURE_VALUE_TYPE(previews::features::kOptimizationHints)},
+    {"enable-heavy-page-capping",
+     flag_descriptions::kEnableHeavyPageCappingName,
+     flag_descriptions::kEnableHeavyPageCappingDescription, kOsAll,
+     FEATURE_WITH_PARAMS_VALUE_TYPE(data_use_measurement::page_load_capping::
+                                        features::kDetectingHeavyPages,
+                                    kDetectingHeavyPagesFeatureVariations,
+                                    "DetectingHeavyPages")},
     {"allow-insecure-localhost", flag_descriptions::kAllowInsecureLocalhostName,
      flag_descriptions::kAllowInsecureLocalhostDescription, kOsAll,
      SINGLE_VALUE_TYPE(switches::kAllowInsecureLocalhost)},
