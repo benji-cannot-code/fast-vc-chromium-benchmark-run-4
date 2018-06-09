2c498cf73a1edbe98ab38e25cb4b7ad06c1693b6
Jordy Greenblatt <jordynass@chromium.org>
2018-06-09 00:59:24 +0000

    [CrOS MultiDevice]: Notify observer for remaining events.
    Change-Id: I564c8d70a0e34a4a7eb28096a0941f7d37adcb26
    Reviewed-on: https://chromium-review.googlesource.com/1089828
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Jordy Greenblatt <jordynass@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565805}

--- chromeos/services/multidevice_setup/fake_setup_flow_completion_recorder.cc
+++ chromeos/services/multidevice_setup/fake_setup_flow_completion_recorder.cc
@@ -11,6 +11,8 @@ namespace multidevice_setup {
 
 FakeSetupFlowCompletionRecorder::FakeSetupFlowCompletionRecorder() = default;
 
+FakeSetupFlowCompletionRecorder::~FakeSetupFlowCompletionRecorder() = default;
+
 base::Optional<base::Time>
 FakeSetupFlowCompletionRecorder::GetCompletionTimestamp() {
   return completion_time_;
