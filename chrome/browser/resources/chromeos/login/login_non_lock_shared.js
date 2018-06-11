c495e46cae56876ef45e434c480530e170cd2a17
Steven Bennetts <stevenjb@chromium.org>
2018-06-11 19:07:53 +0000

    OOBE: Eliminate non MD reset screen
    Bug: 647411
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ic4fc3938481eeeb5f7f86beeacf7fdf5035b45d7
    Reviewed-on: https://chromium-review.googlesource.com/1079954
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566093}

--- chrome/browser/resources/chromeos/login/login_non_lock_shared.js
+++ chrome/browser/resources/chromeos/login/login_non_lock_shared.js
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // <include src="network_dropdown.js">
 
-// <include src="oobe_screen_reset_confirmation_overlay.js">
 // <include src="oobe_screen_reset.js">
 // <include src="oobe_screen_autolaunch.js">
 // <include src="oobe_screen_enable_kiosk.js">
