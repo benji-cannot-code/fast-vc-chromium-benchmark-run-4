74d1e9f50ccfaf1ab39c0c51a632a04b271638ee
Mohamed Amir Yosef <mamir@chromium.org>
2018-06-11 22:47:50 +0000

    [Sync::USS] Injecting only the required dependencies to BookmarkMTP
    Before this patch: sync_client has been injected as a dependency to the
    BookmarkModelTypeProcessor.
    
    After this patch: only the required dependencies are injected instead
    of the complete sync_client.
    
    This is a preparation for later patches.
    
    Bug: 516866
    Change-Id: I3a0c6e9c57874b04ec111c3cfbaa79da5d51a5a7
    Reviewed-on: https://chromium-review.googlesource.com/1095309
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566180}

--- components/sync_bookmarks/bookmark_model_type_processor.h
+++ components/sync_bookmarks/bookmark_model_type_processor.h
@@ -16,9 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync/model/model_type_controller_delegate.h"
 #include "components/sync_bookmarks/synced_bookmark_tracker.h"
 
-namespace syncer {
-class SyncClient;
-}
+class BookmarkUndoService;
 
 namespace bookmarks {
 class BookmarkModel;
@@ -29,8 +27,10 @@ namespace sync_bookmarks {
 class BookmarkModelTypeProcessor : public syncer::ModelTypeProcessor,
                                    public syncer::ModelTypeControllerDelegate {
  public:
-  // |sync_client| must not be nullptr and must outlive this object.
-  explicit BookmarkModelTypeProcessor(syncer::SyncClient* sync_client);
+  // |bookmark_undo_service| and |bookmark_undo_service| must not be nullptr and
+  // must outlive this object.
+  BookmarkModelTypeProcessor(bookmarks::BookmarkModel* bookmark_model,
+                             BookmarkUndoService* bookmark_undo_service);
   ~BookmarkModelTypeProcessor() override;
 
   // ModelTypeProcessor implementation.
@@ -108,11 +108,12 @@ class BookmarkModelTypeProcessor : public syncer::ModelTypeProcessor,
   // the initial sync cycle only.
   void AssociatePermanentFolder(const syncer::EntityData& update_data);
 
-  syncer::SyncClient* const sync_client_;
-
   // The bookmark model we are processing local changes from and forwarding
   // remote changes to.
   bookmarks::BookmarkModel* const bookmark_model_;
+
+  // Used to suspend bookmark undo when processing remote changes.
+  BookmarkUndoService* const bookmark_undo_service_;
 
   // Reference to the CommitQueue.
   //
