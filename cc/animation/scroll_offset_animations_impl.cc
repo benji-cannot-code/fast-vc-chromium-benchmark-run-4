1d5c0c95fb1e4b2660c6708588a3b36b17728e58
Sahel Sharify <sahel@chromium.org>
2018-06-11 18:33:43 +0000

    Add duration of scroll animation to cc tracing.
    This cl adds the duration of scroll animation to tracing. The new
    information helps with finding out the root cause of the potential race
    in smooth scrolling path.
    
    Bug: 797708
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ic30ad116bf8dc8047d10ab66cf4c45618d0dee27
    Reviewed-on: https://chromium-review.googlesource.com/1093539
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Sahel Sharifymoghaddam <sahel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566072}

--- cc/animation/scroll_offset_animations_impl.cc
+++ cc/animation/scroll_offset_animations_impl.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/animation/scroll_offset_animations_impl.h"
 
 #include "base/trace_event/trace_event.h"
+#include "base/trace_event/trace_event_argument.h"
 #include "cc/animation/animation_host.h"
 #include "cc/animation/animation_id_provider.h"
 #include "cc/animation/animation_timeline.h"
@@ -46,6 +47,8 @@ void ScrollOffsetAnimationsImpl::ScrollAnimationCreate(
                              CubicBezierTimingFunction::EaseType::EASE_IN_OUT),
           ScrollOffsetAnimationCurve::DurationBehavior::INVERSE_DELTA);
   curve->SetInitialValue(current_offset, delayed_by);
+  TRACE_EVENT_INSTANT1("cc", "ScrollAnimationCreate", TRACE_EVENT_SCOPE_THREAD,
+                       "Duration", curve->Duration().InMillisecondsF());
 
   std::unique_ptr<KeyframeModel> keyframe_model = KeyframeModel::Create(
       std::move(curve), AnimationIdProvider::NextKeyframeModelId(),
@@ -109,6 +112,9 @@ bool ScrollOffsetAnimationsImpl::ScrollAnimationUpdateTarget(
   trimmed -= delayed_by;
 
   curve->UpdateTarget(trimmed.InSecondsF(), new_target);
+  TRACE_EVENT_INSTANT1("cc", "ScrollAnimationUpdateTarget",
+                       TRACE_EVENT_SCOPE_THREAD, "UpdatedDuration",
+                       curve->Duration().InMillisecondsF());
 
   return true;
 }
