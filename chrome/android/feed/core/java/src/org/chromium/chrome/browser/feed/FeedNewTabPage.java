70e4c7d6cc6dff6739b309ce788e9926c4df4181
Becky Zhou <huayinz@chromium.org>
2018-06-11 18:40:21 +0000

    [Feed] Call Stream lifecycle methods in FeedNewTabPage
    Add calls to the Stream's lifecycle methods based on activity state
    change, tab state change, and FeedNewTabPage construction and
    destruction.
    
    Bug: 846467
    Change-Id: I4a34ceab140ca93e2491f4dea47bc6bfdb0a81b2
    Reviewed-on: https://chromium-review.googlesource.com/1083835
    Commit-Queue: Becky Zhou <huayinz@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566076}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedNewTabPage.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedNewTabPage.java
@@ -27,12 +27,15 @@ import org.chromium.chrome.browser.UrlConstants;
 import org.chromium.chrome.browser.feed.action.FeedActionHandler;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.suggestions.SuggestionsNavigationDelegateImpl;
+import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 
 /**
  * Provides a new tab page that displays an interest feed rendered list of content suggestions.
  */
 public class FeedNewTabPage extends BasicNativePage {
+    private final StreamLifecycleManager mStreamLifecycleManager;
+
     private FrameLayout mRootView;
     private String mTitle;
     private FeedImageLoader mImageLoader;
@@ -108,8 +111,9 @@ public class FeedNewTabPage extends BasicNativePage {
             TabModelSelector tabModelSelector) {
         super(activity, nativePageHost);
 
-        Profile profile = nativePageHost.getActiveTab().getProfile();
         FeedProcessScope feedProcessScope = FeedProcessScopeFactory.getFeedProcessScope();
+        Tab tab = nativePageHost.getActiveTab();
+        Profile profile = tab.getProfile();
         mImageLoader = new FeedImageLoader(profile, activity);
         SuggestionsNavigationDelegateImpl navigationDelegate =
                 new SuggestionsNavigationDelegateImpl(
@@ -124,14 +128,13 @@ public class FeedNewTabPage extends BasicNativePage {
                         .build();
 
         Stream stream = streamScope.getStream();
-        stream.onCreate(null);
+        mStreamLifecycleManager = new StreamLifecycleManager(stream, activity, tab);
+
         stream.getView().setBackgroundColor(Color.WHITE);
         mRootView.addView(stream.getView());
 
         // TODO(skym): This is a work around for outstanding Feed bug.
         stream.triggerRefresh();
-
-        // TODO(https://crbug.com/803317): Call appropriate lifecycle methods.
     }
 
     @Override
@@ -146,6 +149,7 @@ public class FeedNewTabPage extends BasicNativePage {
     public void destroy() {
         super.destroy();
         mImageLoader.destroy();
+        mStreamLifecycleManager.destroy();
     }
 
     @Override
