8266cf12f5c2f321603e16a259de17381f76e024
Danyao Wang <danyao@google.com>
2018-06-11 19:24:13 +0000

    [Nav Experiment] Handle back/forward failures in error retry states.
    Previously, a provisional navigation failure always adds a placeholder
    item. This is wrong if the provisional navigation failure is triggered
    on a back/forward/reload/restore_session navigation because in these
    cases, the navigation item already exists in WKBackForwardList.
    
    Adding the new cases to the error retry state machine made it
    sufficiently complex, so it is now refactored out of CRWWebController
    into its own object, owned by NavigationItemImpl.
    
    This fixes ErrorPageTestCase/testGoForwardAfterServerIsDownAndReload.
    
    Bug: 836767
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I94f5de9d92187277edb432c43899a8db4c416915
    Reviewed-on: https://chromium-review.googlesource.com/1087640
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Danyao Wang <danyao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566100}

--- ios/web/navigation/navigation_item_impl_unittest.mm
+++ ios/web/navigation/navigation_item_impl_unittest.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/strings/utf_string_conversions.h"
+#include "ios/web/navigation/wk_navigation_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #import "testing/gtest_mac.h"
 #include "testing/platform_test.h"
@@ -173,48 +174,6 @@ TEST_F(NavigationItemTest, GetDisplayTitleForURL) {
 
   title = NavigationItemImpl::GetDisplayTitleForURL(GURL("file://foo/1.gz"));
   EXPECT_EQ("1.gz", base::UTF16ToUTF8(title));
-}
-
-// Tests state transitions between ErrorRetryStates.
-TEST_F(NavigationItemTest, SetErrorRetryState) {
-  NavigationItemImpl item;
-
-  // item is not tracked in error_retry_states_ so should default to NO_ERROR.
-  EXPECT_EQ(ErrorRetryState::kNoNavigationError, item.GetErrorRetryState());
-
-  // The orders below matter because not all state transitions are valid.
-  // Invalid transitions are not tested because death tests are not supported in
-  // iOS simulator build.
-  item.SetErrorRetryState(
-      ErrorRetryState::kReadyToDisplayErrorForFailedNavigation);
-  ASSERT_EQ(ErrorRetryState::kReadyToDisplayErrorForFailedNavigation,
-            item.GetErrorRetryState());
-
-  item.SetErrorRetryState(
-      ErrorRetryState::kDisplayingNativeErrorForFailedNavigation);
-  ASSERT_EQ(ErrorRetryState::kDisplayingNativeErrorForFailedNavigation,
-            item.GetErrorRetryState());
-
-  item.SetErrorRetryState(ErrorRetryState::kNavigatingToFailedNavigationItem);
-  ASSERT_EQ(ErrorRetryState::kNavigatingToFailedNavigationItem,
-            item.GetErrorRetryState());
-
-  item.SetErrorRetryState(ErrorRetryState::kRetryFailedNavigationItem);
-  ASSERT_EQ(ErrorRetryState::kRetryFailedNavigationItem,
-            item.GetErrorRetryState());
-
-  item.SetErrorRetryState(
-      ErrorRetryState::kReadyToDisplayErrorForFailedNavigation);
-  ASSERT_EQ(ErrorRetryState::kReadyToDisplayErrorForFailedNavigation,
-            item.GetErrorRetryState());
-
-  // Cycle through again, this time, terminate in no error.
-  item.SetErrorRetryState(
-      ErrorRetryState::kDisplayingNativeErrorForFailedNavigation);
-  item.SetErrorRetryState(ErrorRetryState::kNavigatingToFailedNavigationItem);
-  item.SetErrorRetryState(ErrorRetryState::kRetryFailedNavigationItem);
-  item.SetErrorRetryState(ErrorRetryState::kNoNavigationError);
-  ASSERT_EQ(ErrorRetryState::kNoNavigationError, item.GetErrorRetryState());
 }
 
 }  // namespace
