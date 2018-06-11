83fd2a3b95de1b6c048717663449a159c5ee7357
Suproteem Sarkar <suproteem@chromium.org>
2018-06-11 18:24:31 +0000

    Implemented IScrollItemProvider, which allows for scrolling to accessibility objects
    R=nektar@chromium.org,dmazzoni@chromium.org
    
    Bug: 851518
    Change-Id: I05a3d7359e3ea1b51a4532314623d86407606e13
    Reviewed-on: https://chromium-review.googlesource.com/1079750
    Commit-Queue: Suproteem Sarkar <suproteem@chromium.org>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566065}

--- ui/accessibility/platform/ax_platform_node_win.h
+++ ui/accessibility/platform/ax_platform_node_win.h
@@ -242,6 +242,7 @@ class AX_EXPORT __declspec(uuid("26f5641a-246d-457b-a96d-07f3fae6acf2"))
                         public IAccessibleTable2,
                         public IAccessibleTableCell,
                         public IRawElementProviderSimple,
+                        public IScrollItemProvider,
                         public IServiceProvider,
                         public AXPlatformNodeBase {
  public:
@@ -257,6 +258,7 @@ class AX_EXPORT __declspec(uuid("26f5641a-246d-457b-a96d-07f3fae6acf2"))
     COM_INTERFACE_ENTRY(IAccessibleTable2)
     COM_INTERFACE_ENTRY(IAccessibleTableCell)
     COM_INTERFACE_ENTRY(IRawElementProviderSimple)
+    COM_INTERFACE_ENTRY(IScrollItemProvider)
     COM_INTERFACE_ENTRY(IServiceProvider)
   END_COM_MAP()
 
@@ -417,6 +419,12 @@ class AX_EXPORT __declspec(uuid("26f5641a-246d-457b-a96d-07f3fae6acf2"))
 
   STDMETHODIMP GetIAccessiblePair(IAccessible** accessible,
                                   LONG* child_id) override;
+
+  //
+  // IScrollItemProvider methods.
+  //
+
+  STDMETHODIMP ScrollIntoView() override;
 
   // IAccessibleEx methods not implemented.
   STDMETHODIMP GetRuntimeId(SAFEARRAY** runtime_id) override;
