92e67868c1f566e16e99b2511429c7cabc547523
Sidney San Martín <sdy@chromium.org>
2018-06-11 19:08:55 +0000

    Create views::test::WidgetDestroyedWaiter.
    Bug: 849129
    Change-Id: I2d618fb319a43d8a9727cdde3c17c85da77b6f03
    Reviewed-on: https://chromium-review.googlesource.com/1086509
    Commit-Queue: Sidney San Martín <sdy@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566094}

--- ui/views/test/widget_test.cc
+++ ui/views/test/widget_test.cc
@@ -204,5 +204,18 @@ void WidgetClosingObserver::OnWidgetClosing(Widget* widget) {
     run_loop_.Quit();
 }
 
+WidgetDestroyedWaiter::WidgetDestroyedWaiter(Widget* widget) {
+  widget->AddObserver(this);
+}
+
+void WidgetDestroyedWaiter::Wait() {
+  run_loop_.Run();
+}
+
+void WidgetDestroyedWaiter::OnWidgetDestroyed(Widget* widget) {
+  widget->RemoveObserver(this);
+  run_loop_.Quit();
+}
+
 }  // namespace test
 }  // namespace views
