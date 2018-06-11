dc004b3809deabb466b30e37f046e02c5b8be924
David 'Digit' Turner <digit@google.com>
2018-06-11 09:53:24 +0000

    android: Move native library extraction to LibraryLoader
    Some constants and methods related to extracted native libraries
    from the Chrome APK into a private application data directory
    are moved from the ResourceExtractor.java class to LibraryLoader.java
    instead.
    
    This is a more logical place, since ResourceExtractor.java is going
    to be moved out of //base and into //ui/base/ since it is now mostly
    related to UI resources.
    
    See referring bug for more context.
    
    BUG=846633
    R=agrieve@chromium.org, pasko@chromium.org
    
    Change-Id: I7ff442930ee80dfe9230021ce8e099393d51beca
    Reviewed-on: https://chromium-review.googlesource.com/1090926
    Commit-Queue: David Turner <digit@chromium.org>
    Reviewed-by: agrieve <agrieve@chromium.org>
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565951}

--- base/android/java/src/org/chromium/base/library_loader/Linker.java
+++ base/android/java/src/org/chromium/base/library_loader/Linker.java
@@ -14,7 +14,6 @@ import android.os.Parcelable;
 
 import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
-import org.chromium.base.ResourceExtractor;
 import org.chromium.base.annotations.AccessedByNative;
 
 import java.util.HashMap;
@@ -461,7 +460,7 @@ public abstract class Linker {
             System.loadLibrary(LINKER_JNI_LIBRARY);
             LibraryLoader.incrementRelinkerCountNotHitHistogram();
         } catch (UnsatisfiedLinkError e) {
-            if (ResourceExtractor.PLATFORM_REQUIRES_NATIVE_FALLBACK_EXTRACTION) {
+            if (LibraryLoader.PLATFORM_REQUIRES_NATIVE_FALLBACK_EXTRACTION) {
                 System.load(LibraryLoader.getExtractedLibraryPath(
                         ContextUtils.getApplicationContext(), LINKER_JNI_LIBRARY));
                 LibraryLoader.incrementRelinkerCountHitHistogram();
