bb99e3a50e5bf264243379b2a9c1e35046f9465d
Colin Blundell <blundell@chromium.org>
2018-06-11 16:04:39 +0000

    PrimaryAccountAccessTokenFetcher: Simplify observing of signin classes
    PrimaryAccountAccessTokenFetcher currently has a very targeted
    observance of SigninManager and ProfileOAuth2TokenService:
    - It observes SigninManager during the duration of time from which it
      determines it needs to see a signin to when that signin occurs.
    - It observes PO2TS during the duration of time from which it
      determines that the user is signed in without a refresh token to when
      the refresh token becomes available.
    
    This targeted observance allows for having nice DCHECKs around exactly
    what state of waiting the token fetcher is in when given observer
    callbacks occur. However, we will shortly be porting this class to
    talk to IdentityManager. After the porting, it will no longer be
    possible to maintain this targeted observance, as by design
    IdentityManager::Observer folds the functionality of
    SigninManagerBase::Observer and OAuth2TokenService::Observer together.
    Thus, for example, if the token fetcher is obstensibly waiting for a
    refresh token to become available, it might in fact see the user sign
    in again (if they managed to sign out and sign back in in the interim,
    for example).
    
    This CL changes PrimaryAccountAccessTokenFetcher in anticipation of
    that porting:
    - It starts observing SigninManager and PO2TS immediately when
      launching in WaitUntilAvailable mode via ScopedObserver instances if
      credentials are not immediately available.
    - When it starts an access token request, it removes these observers
      if they are present (in addition to not being present if launched
      in RequestImmediately mode, access token requests get retried once by
      the fetcher in WaitUntilAvailable mode; thus, it's not guaranteed that
      these observers will be present at the time of starting an access
      token request).
    - Otherwise, the observers are removed implicitly on destruction.
    
    A side effect of these changes is that the nice DCHECKs mentioned
    above must necessarily be removed. In fact, there could be behavioral
    changes; e.g., PrimaryAccountAccessTokenFetcher could observe a signin
    event where it hadn't before in the case mentioned above. However,
    these behavioral changes are harmless, as
    PrimaryAccountAccessTokenFetcher's observer callback implementations
    are essentially stateless: they simply wait for the moment when both
    the primary account info and the refresh token for that account are
    available, and then synchronously fire off an access token request and
    stop listening for further signin events. Observing more signin-related
    events will simply result in more checks for whether that moment has
    arrived.
    
    Bug: 796544
    Change-Id: I58738bb697e38008012f7b8bc759ed8c8382c516
    Reviewed-on: https://chromium-review.googlesource.com/1092575
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566023}

--- services/identity/public/cpp/primary_account_access_token_fetcher.h
+++ services/identity/public/cpp/primary_account_access_token_fetcher.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/macros.h"
+#include "base/scoped_observer.h"
 #include "components/signin/core/browser/signin_manager_base.h"
 #include "google_apis/gaia/google_service_auth_error.h"
 #include "google_apis/gaia/oauth2_token_service.h"
@@ -59,7 +60,10 @@ class PrimaryAccountAccessTokenFetcher : public SigninManagerBase::Observer,
  private:
   void Start();
 
-  void WaitForRefreshToken();
+  // Returns true iff there is a primary account with a refresh token. Should
+  // only be called in mode |kWaitUntilAvailable|.
+  bool AreCredentialsAvailable() const;
+
   void StartAccessTokenRequest();
 
   // SigninManagerBase::Observer implementation.
@@ -68,6 +72,10 @@ class PrimaryAccountAccessTokenFetcher : public SigninManagerBase::Observer,
 
   // OAuth2TokenService::Observer implementation.
   void OnRefreshTokenAvailable(const std::string& account_id) override;
+
+  // Checks whether credentials are now available and starts an access token
+  // request if so. Should only be called in mode |kWaitUntilAvailable|.
+  void ProcessSigninStateChange();
 
   // OAuth2TokenService::Consumer implementation.
   void OnGetTokenSuccess(const OAuth2TokenService::Request* request,
@@ -81,8 +89,10 @@ class PrimaryAccountAccessTokenFetcher : public SigninManagerBase::Observer,
   OAuth2TokenService::ScopeSet scopes_;
   TokenCallback callback_;
 
-  bool waiting_for_sign_in_;
-  bool waiting_for_refresh_token_;
+  ScopedObserver<SigninManagerBase, PrimaryAccountAccessTokenFetcher>
+      signin_manager_observer_;
+  ScopedObserver<OAuth2TokenService, PrimaryAccountAccessTokenFetcher>
+      token_service_observer_;
 
   std::unique_ptr<OAuth2TokenService::Request> access_token_request_;
 
