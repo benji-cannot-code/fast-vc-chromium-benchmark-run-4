17b6d66fb9b5776c5c33392071b420f0a6dcbbfa
Vasilii Sukhanov <vasilii@chromium.org>
2018-06-11 11:12:28 +0000

    Make password generation drop-down similar to MD2.
    This CL is the first step to make the password generation drop-down matching the updated mocks. Shadow, border and corners are to be done.
    
    TBR=rouslan@chromium.org
    
    Bug: 851021
    Change-Id: I55dafadf3d55f1670defbaa87b778408a2ebfc69
    Reviewed-on: https://chromium-review.googlesource.com/1093317
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Maxim Kolosovskiy <kolos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565961}

--- chrome/browser/ui/android/autofill/password_generation_popup_view_android.cc
+++ chrome/browser/ui/android/autofill/password_generation_popup_view_android.cc
@@ -86,6 +86,8 @@ gfx::Size PasswordGenerationPopupViewAndroid::GetPreferredSizeOfPasswordView() {
   return gfx::Size(kUnusedSize, kUnusedSize);
 }
 
+void PasswordGenerationPopupViewAndroid::UpdateState() {}
+
 void PasswordGenerationPopupViewAndroid::UpdateBoundsAndRedrawPopup() {
   if (java_object_.is_null())
     return;
