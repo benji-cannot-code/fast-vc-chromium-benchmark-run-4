78b7e7b29054552465c002cbf8c430b6adabd69c
Parastoo Geranmayeh <parastoog@google.com>
2018-06-11 15:34:29 +0000

    [Autofill] Independent Heuristics for Search Fields
    Search term fields can now only be predicted if they're adjacent to
    an address field. By moving them to an independent parser, this will
    not be the case anymore.
    
    Bug: 848413
    Change-Id: I5a35e363314b58d75fefddb65b410ddcc840df9a
    Reviewed-on: https://chromium-review.googlesource.com/1089371
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Commit-Queue: Parastoo Geranmayeh <parastoog@google.com>
    Cr-Commit-Position: refs/heads/master@{#566010}

--- components/autofill/core/browser/form_field.cc
+++ components/autofill/core/browser/form_field.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/form_structure.h"
 #include "components/autofill/core/browser/name_field.h"
 #include "components/autofill/core/browser/phone_field.h"
+#include "components/autofill/core/browser/search_field.h"
 #include "components/autofill/core/common/autofill_constants.h"
 #include "components/autofill/core/common/autofill_regexes.h"
 #include "components/autofill/core/common/autofill_util.h"
@@ -31,13 +32,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace autofill {
 
 // There's an implicit precedence determined by the values assigned here. Email
-// is currently the most important followed by Phone, Address, Credit Card and
-// finally Name.
+// is currently the most important followed by Phone, Address, Credit Card,
+// Name, and Search.
 const float FormField::kBaseEmailParserScore = 1.4f;
 const float FormField::kBasePhoneParserScore = 1.3f;
 const float FormField::kBaseAddressParserScore = 1.2f;
 const float FormField::kBaseCreditCardParserScore = 1.1f;
 const float FormField::kBaseNameParserScore = 1.0f;
+const float FormField::kBaseSearchParserScore = 0.9f;
 
 // static
 FieldCandidatesMap FormField::ParseFormFields(
@@ -77,6 +79,9 @@ FieldCandidatesMap FormField::ParseFormFields(
 
   // Name pass.
   ParseFormFieldsPass(NameField::Parse, processed_fields, &field_candidates);
+
+  // Search pass.
+  ParseFormFieldsPass(SearchField::Parse, processed_fields, &field_candidates);
 
   // Do not autofill a form if there aren't enough fields. Otherwise, it is
   // very easy to have false positives. See http://crbug.com/447332
