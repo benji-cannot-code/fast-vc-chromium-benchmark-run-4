dac8e4d6fedc7cdfc286d358a7999c199afc3a37
kylechar <kylechar@chromium.org>
2018-06-11 16:41:02 +0000

    viz: Refactor FrameSink data.
    Move registered frame sink ids and debug labels from SurfaceManager into
    FrameSinkManagerImpl. FrameSink data belongs in FrameSinkMangerImpl, it
    was only in SurfaceManager for historical reasons. Also collapse
    synchronization labels into the same data structure.
    
    Also ensure that we aren't accessing non-existant map entries. There was
    a crash reported where we were accessing debug label for FrameSinkId
    that doesn't exist.
    
    Bug: 850194
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel
    Change-Id: I3b8971c56af466d7aa10099e59ee07a5891a29b9
    Reviewed-on: https://chromium-review.googlesource.com/1090759
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Saman Sami <samans@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566031}

--- components/viz/service/frame_sinks/frame_sink_manager_impl.h
+++ components/viz/service/frame_sinks/frame_sink_manager_impl.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/optional.h"
 #include "base/single_thread_task_runner.h"
+#include "base/strings/string_piece.h"
 #include "base/threading/thread_checker.h"
 #include "components/viz/common/constants.h"
 #include "components/viz/common/surfaces/frame_sink_id.h"
@@ -172,6 +173,10 @@ class VIZ_SERVICE_EXPORT FrameSinkManagerImpl
   void AddObserver(FrameSinkObserver* obs);
   void RemoveObserver(FrameSinkObserver* obs);
 
+  // Returns the debug label associated with |frame_sink_id| if any.
+  base::StringPiece GetFrameSinkDebugLabel(
+      const FrameSinkId& frame_sink_id) const;
+
   // Returns ids of all FrameSinks that were created.
   std::vector<FrameSinkId> GetCreatedFrameSinkIds() const;
   // Returns ids of all FrameSinks that were registered.
@@ -186,6 +191,23 @@ class VIZ_SERVICE_EXPORT FrameSinkManagerImpl
 
  private:
   friend class FrameSinkManagerTest;
+
+  // Metadata for a CompositorFrameSink.
+  struct FrameSinkData {
+    FrameSinkData();
+    FrameSinkData(FrameSinkData&& other);
+    ~FrameSinkData();
+    FrameSinkData& operator=(FrameSinkData&& other);
+
+    // A label to identify frame sink.
+    std::string debug_label;
+
+    // Record synchronization events for this FrameSinkId if not empty.
+    std::string synchronization_label;
+
+   private:
+    DISALLOW_COPY_AND_ASSIGN(FrameSinkData);
+  };
 
   // BeginFrameSource routing information for a FrameSinkId.
   struct FrameSinkSourceMapping {
@@ -225,6 +247,11 @@ class VIZ_SERVICE_EXPORT FrameSinkManagerImpl
   // Provides a Display for CreateRootCompositorFrameSink().
   DisplayProvider* const display_provider_;
 
+  // Contains registered frame sink ids, debug labels and synchronization
+  // labels. Map entries will be created when frame sink is registered and
+  // destroyed when frame sink is invalidated.
+  base::flat_map<FrameSinkId, FrameSinkData> frame_sink_data_;
+
   // Set of BeginFrameSource along with associated FrameSinkIds. Any child
   // that is implicitly using this frame sink must be reachable by the
   // parent in the dag.
@@ -240,10 +267,6 @@ class VIZ_SERVICE_EXPORT FrameSinkManagerImpl
   // [Root]CompositorFrameSinkImpls are owned in this map.
   base::flat_map<FrameSinkId, std::unique_ptr<mojom::CompositorFrameSink>>
       sink_map_;
-
-  // The set of FrameSinkIds that the client wants synchronization event
-  // notifications for.
-  base::flat_map<FrameSinkId, std::string> synchronization_event_labels_;
 
   PrimaryBeginFrameSource primary_source_;
 
