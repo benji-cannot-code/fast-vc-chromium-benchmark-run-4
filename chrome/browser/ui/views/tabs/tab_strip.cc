bfc9a41bc163ce833433ce0b9f06091f10a54c0a
Allen Bauer <kylixrd@chromium.org>
2018-06-11 22:19:44 +0000

    Fixed case where a tab would not be painted when hovering in the overlap of two tabs.
    Due to the tabs overlapping, more than one tab can return that the mouse is hovered over its bounds. This would cause |hovered_tab| to be overwritten and the previously hovered tab to not get painted. This change ensures that only one tab is ever considered "hovered". For two adjacent tabs that are hovered, the one on the left is favored.
    
    The reason for all of this code in the first place is to ensure that the currently hovered background tab is always painted right before the active tab. This visually manifests as the hovered tab always painting "above" the adjacent tabs. The |last_hovered_tab_| field also ensures that when no tab is hovered anymore, the last hovered tab continues to be painted in the right order while the hover animation is still running.
    
    Bug: 846429
    Change-Id: I95723ba79f683a31f45a117bd0833a143073a46a
    Reviewed-on: https://chromium-review.googlesource.com/1093734
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566170}

--- chrome/browser/ui/views/tabs/tab_strip.cc
+++ chrome/browser/ui/views/tabs/tab_strip.cc
@@ -1159,7 +1159,7 @@ void TabStrip::PaintChildren(const views::PaintInfo& paint_info) {
         if (!tab->IsSelected()) {
           if (!stacked_layout_) {
             // In Refresh mode, defer the painting of the hovered tab to below.
-            if (MD::IsRefreshUi() && tab->IsMouseHovered()) {
+            if (MD::IsRefreshUi() && tab->IsMouseHovered() && !hovered_tab) {
               hovered_tab = tab;
             } else {
               tab->Paint(paint_info);
@@ -1207,7 +1207,7 @@ void TabStrip::PaintChildren(const views::PaintInfo& paint_info) {
   if (hovered_tab && !is_dragging)
     hovered_tab->Paint(paint_info);
 
-  // Keep track of the last tab that was hovered to that it continues to be
+  // Keep track of the last tab that was hovered so that it continues to be
   // painted right before the active tab while the animation is running.
   last_hovered_tab_ = hovered_tab;
 
