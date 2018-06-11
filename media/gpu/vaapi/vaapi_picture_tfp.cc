aae6460af0a94d56c0c010592fdf73783e52f263
Hirokazu Honda <hiroh@chromium.org>
2018-06-11 03:34:38 +0000

    Revert "VaapiPicture*: add missing DCHECK()s"
    This reverts commit 6873824cc1c2edee42ee3d0dfac8f0ba77810320.
    
    Reason for revert: crrev.com/c/1079834 breaks ARC++ video playback. This CL has dependency of the CL.
    
    Original change's description:
    > VaapiPicture*: add missing DCHECK()s
    >
    > This CL adds 3 missing DCHECK()s that I forgot after [1]
    >
    > [1] https://chromium-review.googlesource.com/c/chromium/src/+/1079834/13/media/gpu/vaapi/vaapi_picture_factory.cc#50
    >
    > TBR=dcastagna@chromium.org
    >
    > Bug: 717265
    > Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    > Change-Id: Ibfc9797bda93dd7660cc64317ab21794dbda6e10
    > Reviewed-on: https://chromium-review.googlesource.com/1086131
    > Reviewed-by: Miguel Casas <mcasas@chromium.org>
    > Commit-Queue: Miguel Casas <mcasas@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#564289}
    
    TBR=mcasas@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 717265, b:109906289
    Change-Id: Ibc20bee25cdfa127d0b646bc1a03a7e53a6129e4
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Reviewed-on: https://chromium-review.googlesource.com/1094794
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Pawel Osciak <posciak@chromium.org>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565921}

--- media/gpu/vaapi/vaapi_picture_tfp.cc
+++ media/gpu/vaapi/vaapi_picture_tfp.cc
@@ -35,7 +35,6 @@ VaapiTFPPicture::VaapiTFPPicture(
       x_pixmap_(0) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(texture_id);
-  DCHECK(client_texture_id);
 }
 
 VaapiTFPPicture::~VaapiTFPPicture() {
