5a0ce07e25dfbfab63f4c0074c8e632515ee3c4c
Tim Schumann <tschumann@chromium.org>
2018-06-11 10:31:39 +0000

    Revert "Fixes a crash when the user's install flow fails and we press Cancel."
    This reverts commit 9ec0dfc9a56f658f2c33cdd4c1918deacdf54537.
    
    Reason for revert: CrostiniInstallerViewBrowserTest.ErrorThenCancel is flaky. See https://bugs.chromium.org/p/chromium/issues/detail?id=851253
    
    Original change's description:
    > Fixes a crash when the user's install flow fails and we press Cancel.
    > 
    > Verified fix using a BrowserTest.
    > 
    > Verifying the fix properly meant fleshing out FakeConciergeClient with
    > settable response protos for all methods.
    > 
    > Found an invalid DCHECK of |state_| in CrostiniInstallerView when testing.
    > 
    > Guard CrostiniRestarterService::Abort.
    > There used to be a DCHECK, but it shouldn't be. It is a legitimate possibility
    > to cancel the install flow right when the the container startup was complete.
    > 
    > Bug: 850281
    > Change-Id: I0cf6bcef1f4f043d9c7e40f3a8baa458fb6f6ba7
    > Reviewed-on: https://chromium-review.googlesource.com/1089600
    > Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    > Reviewed-by: Timothy Loh <timloh@chromium.org>
    > Commit-Queue: Nicholas Verne <nverne@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#565603}
    
    TBR=hashimoto@chromium.org,stevenjb@chromium.org,timloh@chromium.org,nverne@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 850281
    Change-Id: Ica8ccaf48e9b0624930a8d35f11cdd0dff01734a
    Reviewed-on: https://chromium-review.googlesource.com/1095134
    Reviewed-by: Tim Schumann <tschumann@chromium.org>
    Commit-Queue: Tim Schumann <tschumann@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565955}

--- chrome/browser/ui/views/crostini/crostini_installer_view_browsertest.cc
+++ chrome/browser/ui/views/crostini/crostini_installer_view_browsertest.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
-#include "chrome/browser/chromeos/crostini/crostini_manager.h"
 #include "chrome/browser/chromeos/crostini/crostini_pref_names.h"
 #include "chrome/browser/chromeos/crostini/crostini_util.h"
 #include "chrome/browser/profiles/profile.h"
@@ -20,8 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/test/test_browser_dialog.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/test/base/in_process_browser_test.h"
-#include "chromeos/dbus/dbus_thread_manager.h"
-#include "chromeos/dbus/fake_concierge_client.h"
 #include "components/crx_file/id_util.h"
 #include "components/prefs/pref_service.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -29,12 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class CrostiniInstallerViewBrowserTest : public DialogBrowserTest {
  public:
-  CrostiniInstallerViewBrowserTest()
-      : fake_concierge_client_(new chromeos::FakeConciergeClient()) {
-    chromeos::DBusThreadManager::GetSetterForTesting()->SetConciergeClient(
-        base::WrapUnique(fake_concierge_client_));
-    crostini::CrostiniManager::GetInstance()->set_is_testing(true);
-  }
+  CrostiniInstallerViewBrowserTest() {}
 
   // DialogBrowserTest:
   void ShowUi(const std::string& name) override {
@@ -63,10 +55,6 @@ class CrostiniInstallerViewBrowserTest : public DialogBrowserTest {
   bool HasCancelButton() {
     return ActiveView()->GetDialogClientView()->cancel_button() != nullptr;
   }
-
- protected:
-  // Owned by chromeos::DBusThreadManager
-  chromeos::FakeConciergeClient* fake_concierge_client_;
 
  private:
   base::test::ScopedFeatureList scoped_feature_list_;
@@ -108,27 +96,5 @@ IN_PROC_BROWSER_TEST_F(CrostiniInstallerViewBrowserTest, Cancel) {
       "Crostini.SetupResult",
       static_cast<base::HistogramBase::Sample>(
           CrostiniInstallerView::SetupResult::kNotStarted),
-      1);
-}
-
-IN_PROC_BROWSER_TEST_F(CrostiniInstallerViewBrowserTest, ErrorThenCancel) {
-  base::HistogramTester histogram_tester;
-  ShowUi("default");
-  EXPECT_NE(nullptr, ActiveView());
-  vm_tools::concierge::StartVmResponse response;
-  response.set_success(false);
-  fake_concierge_client_->set_start_vm_response(std::move(response));
-
-  ActiveView()->GetDialogClientView()->AcceptWindow();
-  EXPECT_FALSE(ActiveView()->GetWidget()->IsClosed());
-  base::RunLoop().RunUntilIdle();
-  ActiveView()->GetDialogClientView()->CancelWindow();
-  base::RunLoop().RunUntilIdle();
-  EXPECT_EQ(nullptr, ActiveView());
-
-  histogram_tester.ExpectBucketCount(
-      "Crostini.SetupResult",
-      static_cast<base::HistogramBase::Sample>(
-          CrostiniInstallerView::SetupResult::kErrorStartingTermina),
       1);
 }
