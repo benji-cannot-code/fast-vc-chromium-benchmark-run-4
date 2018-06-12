92183f4597fed86172d1dd0b9d6b75b81611b9b0
Hayato Ito <hayato@chromium.org>
2018-06-12 04:17:43 +0000

    Add slot-assignment-recalc-forbidden scoped check to attachShadow
    This is a follow-up of https://crrev.com/c/1070169.
    
    I'll refactor added scoped checks so that this kind of check can be done in more better places.
    
    Bug: 845770
    Change-Id: I3ed963699cd42fd617f7263803ea2ddae0ff1649
    Reviewed-on: https://chromium-review.googlesource.com/1095120
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Hayato Ito <hayato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566297}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -120,6 +120,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/html/html_table_rows_collection.h"
 #include "third_party/blink/renderer/core/html/html_template_element.h"
 #include "third_party/blink/renderer/core/html/parser/html_parser_idioms.h"
+#include "third_party/blink/renderer/core/html/parser/nesting_level_incrementer.h"
 #include "third_party/blink/renderer/core/input/event_handler.h"
 #include "third_party/blink/renderer/core/intersection_observer/element_intersection_observer_data.h"
 #include "third_party/blink/renderer/core/layout/adjust_for_absolute_zoom.h"
@@ -2462,6 +2463,10 @@ void Element::RemoveCallbackSelectors() {
 }
 
 ShadowRoot& Element::CreateAndAttachShadowRoot(ShadowRootType type) {
+#if DCHECK_IS_ON()
+  NestingLevelIncrementer slot_assignment_recalc_forbidden_scope(
+      GetDocument().SlotAssignmentRecalcForbiddenRecursionDepth());
+#endif
   EventDispatchForbiddenScope assert_no_event_dispatch;
   ScriptForbiddenScope forbid_script;
 
