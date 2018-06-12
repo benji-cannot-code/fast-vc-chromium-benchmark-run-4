0ecc1a5b7d8f913d155aa38e52d167630d357cfd
Jiaquan He <hejq@google.com>
2018-06-12 16:56:15 +0000

    app_list: clean up desktop UserManager webui.
    This commit removes USER_MANAGER_SELECT_PROFILE_APP_LAUNCHER and some other
    dead codes.
    
    Bug: 821659
    Bug: 733662
    Change-Id: I72d0ecd60ff42ac25fdfaecfd5666f3664b8a223
    Reviewed-on: https://chromium-review.googlesource.com/1091929
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Commit-Queue: Jiaquan He <hejq@google.com>
    Cr-Commit-Position: refs/heads/master@{#566468}

--- chrome/browser/ui/extensions/extension_enable_flow.cc
+++ chrome/browser/ui/extensions/extension_enable_flow.cc
@@ -107,7 +107,7 @@ void ExtensionEnableFlow::CheckPermissionAndMaybePromptUser() {
   if (profiles::IsProfileLocked(profile_->GetPath())) {
 #if !defined(OS_CHROMEOS)
     UserManager::Show(base::FilePath(),
-                      profiles::USER_MANAGER_SELECT_PROFILE_APP_LAUNCHER);
+                      profiles::USER_MANAGER_SELECT_PROFILE_NO_ACTION);
 #endif  // !defined(OS_CHROMEOS)
     return;
   }
