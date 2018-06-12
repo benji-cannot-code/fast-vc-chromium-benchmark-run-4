9528f4b2d05f2e3d76dc771de9f7401470165dbb
Manuel Rego Casasnovas <rego@igalia.com>
2018-06-12 01:37:48 +0000

    [css-contain] Size containment should ignore contents in flexbox/grid
    The spec is pretty clear
    (https://drafts.csswg.org/css-contain/#containment-size):
      "1. When laying out the containing element,
      it must be treated as having no contents."
    
    The patch modifies ComputeIntrinsicLogicalWidths() and
    FirstLineBoxBaseline() in LayoutFlexibleBox and LayoutGrid,
    so the contents are ignored if the flex or grid container
    has "contain: size".
    Buttons are implemented as flexboxes internally,
    so they get fixed too thanks to this change.
    
    BUG=850169
    TEST=external/wpt/css/css-contain/contain-size-button-001.html
    TEST=external/wpt/css/css-contain/contain-size-flexbox-001.html
    TEST=external/wpt/css/css-contain/contain-size-grid-001.html
    
    Change-Id: Ia08c2c7b31126911cb4f0d1fc70aa444c22af8e3
    Reviewed-on: https://chromium-review.googlesource.com/1093854
    Commit-Queue: Manuel Rego Casasnovas <rego@igalia.com>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566228}

--- third_party/blink/renderer/core/layout/layout_grid.cc
+++ third_party/blink/renderer/core/layout/layout_grid.cc
@@ -498,6 +498,9 @@ LayoutUnit LayoutGrid::GuttersSize(
 void LayoutGrid::ComputeIntrinsicLogicalWidths(
     LayoutUnit& min_logical_width,
     LayoutUnit& max_logical_width) const {
+  if (ShouldApplySizeContainment())
+    return;
+
   std::unique_ptr<Grid> grid = Grid::Create(this);
   GridTrackSizingAlgorithm algorithm(this, *grid);
   PlaceItemsOnGrid(algorithm, base::nullopt);
@@ -1620,7 +1623,8 @@ LayoutUnit LayoutGrid::BaselinePosition(FontBaseline,
 }
 
 LayoutUnit LayoutGrid::FirstLineBoxBaseline() const {
-  if (IsWritingModeRoot() || !grid_->HasGridItems())
+  if (IsWritingModeRoot() || !grid_->HasGridItems() ||
+      ShouldApplySizeContainment())
     return LayoutUnit(-1);
   const LayoutBox* baseline_child = nullptr;
   const LayoutBox* first_child = nullptr;
