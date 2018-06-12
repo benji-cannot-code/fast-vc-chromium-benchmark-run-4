74872b42ce809696d980eaf929e358c82dcc5098
Steve Kobes <skobes@chromium.org>
2018-06-12 23:02:08 +0000

    Delete LocalFrameView::ScrollbarManager and scroll_corner_.
    This removes methods and paths in LocalFrameView that depend directly on
    scrollbar existence.  Since RLS, LocalFrameView never has scrollbars.
    
    Bug: 823365
    Change-Id: I691480e4cad9239f12c50aa868c5d13571d45b2f
    Reviewed-on: https://chromium-review.googlesource.com/1097540
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566618}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -2044,7 +2044,6 @@ void Document::PropagateStyleToViewport() {
 
     View()->RecalculateScrollbarOverlayColorTheme(
         View()->DocumentBackgroundColor());
-    View()->RecalculateCustomScrollbarStyle();
     if (PaintLayerScrollableArea* scrollable_area =
             GetLayoutView()->GetScrollableArea()) {
       if (scrollable_area->HorizontalScrollbar() &&
