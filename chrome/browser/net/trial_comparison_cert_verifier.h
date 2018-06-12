2101d8a4b8d6ef5a9742c27fbc66fab2823aeb91
Matt Mueller <mattm@chromium.org>
2018-06-12 00:11:34 +0000

    TrialComparisonCertVerifier: Ignore difference if platform trusted leaf cert directly.
    Bug: 649026
    Change-Id: I6d7c015c10a3fe3b6b4a8dfac5beb1f00db5f454
    Reviewed-on: https://chromium-review.googlesource.com/1096132
    Reviewed-by: Eric Roman <eroman@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566218}

--- chrome/browser/net/trial_comparison_cert_verifier.h
+++ chrome/browser/net/trial_comparison_cert_verifier.h
@@ -32,7 +32,8 @@ class TrialComparisonCertVerifier : public net::CertVerifier {
     kIgnoredMacUndesiredRevocationChecking = 6,
     kIgnoredMultipleEVPoliciesAndOneMatchesRoot = 7,
     kIgnoredDifferentPathReVerifiesEquivalent = 8,
-    kMaxValue = kIgnoredDifferentPathReVerifiesEquivalent
+    kIgnoredLocallyTrustedLeaf = 9,
+    kMaxValue = kIgnoredLocallyTrustedLeaf
   };
 
   TrialComparisonCertVerifier(
