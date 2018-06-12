bf32348ac3d71d9242afce4cb03915719c085ad3
Allen Bauer <kylixrd@chromium.org>
2018-06-12 19:05:41 +0000

    Paint the tab text and separator using colors derived from frame colors.
    Bug: 848482
    Bug: 848474
    Bug: 848480
    Change-Id: Iea9528748e81529e4673a3b69f22d50135a9324d
    Reviewed-on: https://chromium-review.googlesource.com/1089723
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566522}

--- chrome/browser/ui/views/frame/browser_non_client_frame_view.h
+++ chrome/browser/ui/views/frame/browser_non_client_frame_view.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/frame/avatar_button_manager.h"
 #include "chrome/browser/ui/views/profiles/profile_indicator_icon.h"
 #include "chrome/browser/ui/views/tabs/tab_strip_observer.h"
+#include "chrome/browser/ui/views/tabs/tab_strip_types.h"
 #include "ui/views/window/non_client_view.h"
 
 class BrowserFrame;
@@ -71,6 +72,17 @@ class BrowserNonClientFrameView : public views::NonClientFrameView,
   // Returns COLOR_TOOLBAR_TOP_SEPARATOR[,_INACTIVE] depending on the activation
   // state of the window.
   SkColor GetToolbarTopSeparatorColor() const;
+
+  // Under Refresh, returns the color of the separator between the tabs.
+  SkColor GetTabSeparatorColor() const;
+
+  // Returns the tab background color based on both the |state| of the tab and
+  // the activation state of the window.
+  SkColor GetTabBackgroundColor(TabState state) const;
+
+  // Returns the tab foreground color of the for the text based on both the
+  // |state| of the tab and the activation state of the window.
+  SkColor GetTabForegroundColor(TabState state) const;
 
   // Updates the throbber.
   virtual void UpdateThrobber(bool running) = 0;
@@ -166,6 +178,13 @@ class BrowserNonClientFrameView : public views::NonClientFrameView,
 
   // Returns true if |profile_indicator_icon_| should be shown.
   bool ShouldShowProfileIndicatorIcon() const;
+
+  // Returns the color of the given |color_id| from the theme provider or the
+  // default theme properties.
+  SkColor GetThemeOrDefaultColor(int color_id) const;
+
+  // Returns a color that contrasts well with the frame.
+  SkColor GetContrastingColorAgainstFrame(SkAlpha alpha) const;
 
   // The frame that hosts this view.
   BrowserFrame* frame_;
--- chrome/browser/ui/views/tabs/fake_base_tab_strip_controller.cc
+++ chrome/browser/ui/views/tabs/fake_base_tab_strip_controller.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/tabs/tab_renderer_data.h"
 #include "chrome/browser/ui/views/tabs/tab_strip.h"
+#include "ui/gfx/color_palette.h"
 
 FakeBaseTabStripController::FakeBaseTabStripController() {}
 
@@ -146,6 +147,20 @@ void FakeBaseTabStripController::OnStoppedDraggingTabs() {
 
 SkColor FakeBaseTabStripController::GetToolbarTopSeparatorColor() const {
   return SK_ColorBLACK;
+}
+
+SkColor FakeBaseTabStripController::GetTabSeparatorColor() const {
+  return SK_ColorBLACK;
+}
+
+SkColor FakeBaseTabStripController::GetTabBackgroundColor(
+    TabState state) const {
+  return gfx::kPlaceholderColor;
+}
+
+SkColor FakeBaseTabStripController::GetTabForegroundColor(
+    TabState state) const {
+  return gfx::kPlaceholderColor;
 }
 
 base::string16 FakeBaseTabStripController::GetAccessibleTabName(
