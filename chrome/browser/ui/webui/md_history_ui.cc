b3870a1b6db61c41dbf5a9733582677d8cbd4ce3
Sylvain Defresne <sdefresne@chromium.org>
2018-06-12 10:06:36 +0000

    Convert MdHistoryUI to use IdentityManager
    Bug: 798408
    Change-Id: I53b9d7f84ab95aece369e4ec37db687ce70b3a4c
    Reviewed-on: https://chromium-review.googlesource.com/1095016
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566373}

--- chrome/browser/ui/webui/md_history_ui.cc
+++ chrome/browser/ui/webui/md_history_ui.cc
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "build/build_config.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/signin/signin_manager_factory.h"
+#include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/ui/webui/browsing_history_handler.h"
 #include "chrome/browser/ui/webui/foreign_session_handler.h"
 #include "chrome/browser/ui/webui/history_login_handler.h"
@@ -28,10 +28,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/locale_settings.h"
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/pref_service.h"
-#include "components/signin/core/browser/signin_manager.h"
 #include "components/strings/grit/components_strings.h"
 #include "content/public/browser/web_ui.h"
 #include "content/public/browser/web_ui_data_source.h"
+#include "services/identity/public/cpp/identity_manager.h"
 #include "ui/base/l10n/l10n_util.h"
 
 namespace {
@@ -40,9 +40,9 @@ constexpr char kIsUserSignedInKey[] = "isUserSignedIn";
 constexpr char kShowMenuPromoKey[] = "showMenuPromo";
 
 bool IsUserSignedIn(Profile* profile) {
-  SigninManagerBase* signin_manager =
-      SigninManagerFactory::GetForProfile(profile);
-  return signin_manager && signin_manager->IsAuthenticated();
+  identity::IdentityManager* identity_manager =
+      IdentityManagerFactory::GetForProfile(profile);
+  return identity_manager && identity_manager->HasPrimaryAccount();
 }
 
 bool MenuPromoShown(Profile* profile) {
