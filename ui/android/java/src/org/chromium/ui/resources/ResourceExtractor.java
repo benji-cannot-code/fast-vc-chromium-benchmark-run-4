7028c09f96600cffdcbdf3942c796df9c87cfcdf
David 'Digit' Turner <digit@google.com>
2018-06-12 12:44:02 +0000

    android: Move ResourceExtractor from base to ui
    This prepares for a future CL that will ensure the set of PAK
    files extracted by this class match the UI language used by the
    Android framework to display localized strings.
    
    BUG=846633
    R=agrieve@chromium.org, astevenson@chromium.org
    
    Change-Id: I316dc53b23f3fc9139af845c1f9fad7e5b4138a6
    Reviewed-on: https://chromium-review.googlesource.com/1091310
    Reviewed-by: agrieve <agrieve@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: David Turner <digit@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566391}

--- ui/android/java/src/org/chromium/ui/resources/ResourceExtractor.java
+++ ui/android/java/src/org/chromium/ui/resources/ResourceExtractor.java
@@ -3,12 +3,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.base;
+package org.chromium.ui.resources;
 
 import android.content.res.AssetManager;
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Looper;
+
+import org.chromium.base.BuildConfig;
+import org.chromium.base.BuildInfo;
+import org.chromium.base.ContextUtils;
+import org.chromium.base.FileUtils;
+import org.chromium.base.LocaleUtils;
+import org.chromium.base.Log;
+import org.chromium.base.PathUtils;
+import org.chromium.base.ThreadUtils;
+import org.chromium.base.TraceEvent;
 
 import java.io.File;
 import java.io.IOException;
@@ -23,7 +33,7 @@ import java.util.Locale;
  * the file system accessible from the native code.
  */
 public class ResourceExtractor {
-    private static final String TAG = "base";
+    private static final String TAG = "ui";
     private static final String ICU_DATA_FILENAME = "icudtl.dat";
     private static final String V8_NATIVES_DATA_FILENAME = "natives_blob.bin";
     private static final String V8_SNAPSHOT_DATA_FILENAME = "snapshot_blob.bin";
@@ -32,7 +42,6 @@ public class ResourceExtractor {
     private static final int BUFFER_SIZE = 16 * 1024;
 
     private class ExtractTask extends AsyncTask<Void, Void, Void> {
-
         private final List<Runnable> mCompletionCallbacks = new ArrayList<Runnable>();
 
         private void doInBackgroundImpl() {
@@ -243,6 +252,8 @@ public class ResourceExtractor {
      * Pak extraction not necessarily required by the embedder.
      */
     private static boolean shouldSkipPakExtraction() {
-        return get().mAssetsToExtract.length == 0;
+        // Certain apks like ContentShell.apk don't have any compressed locale
+        // assets however, so skip extraction entirely for them.
+        return BuildConfig.COMPRESSED_LOCALES.length == 0;
     }
 }
