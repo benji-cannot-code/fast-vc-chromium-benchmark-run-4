abf101f64a3e2b3fdf858f4f773bd5ceac9ddb0e
Brian Anderson <brianderson@chromium.org>
2018-06-12 01:44:34 +0000

    Finch experiment that enables PresentationFeedback for FrameMetrics.
    Bug: 851088
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ia519ec6d5122a4d4383c35eb8441ba9fd614cb73
    Reviewed-on: https://chromium-review.googlesource.com/1093471
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Brian Anderson <brianderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566240}

--- ui/latency/frame_metrics.h
+++ ui/latency/frame_metrics.h
@@ -121,6 +121,8 @@ class FrameMetrics : public SkippedFrameTracker::Client {
   void AddFrameDisplayed(base::TimeTicks source_timestamp,
                          base::TimeTicks display_timestamp);
 
+  static bool RequestPresentationFeedbackEveryFrame();
+
  protected:
   void TraceStats() const;
 
