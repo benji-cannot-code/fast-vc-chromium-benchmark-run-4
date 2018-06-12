f21e5d67464c0c08cdc0d7416ed6fbbfb17a1cab
Xiaohan Wang <xhwang@chromium.org>
2018-06-12 21:52:28 +0000

    media: Rename "SecureCodecs" to "HwSecureCodecs"
    These codecs are used to support HW_SECURE* robustness levels, and in
    KeySystemConfigSelector we already use "HwSecure" in many places.
    
    Note that "secure_codecs" on Android is not renamed because these
    correponds to ".secure" decoders. But a comment is added that all
    ".secure" decoders are hardware-based and support HW_SECURE*
    robustness levels. So they are translated to "HwSecureCodecs".
    
    Bug: 848532
    Test: No functionality change.
    Change-Id: I7a73ff8de27e75bd3128b9e55e025db7d32e072e
    Reviewed-on: https://chromium-review.googlesource.com/1096681
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Stephen Lanham <slan@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566587}

--- components/cdm/renderer/widevine_key_system_properties.h
+++ components/cdm/renderer/widevine_key_system_properties.h
@@ -32,7 +32,7 @@ class WidevineKeySystemProperties : public media::KeySystemProperties {
   WidevineKeySystemProperties(
       base::flat_set<media::EncryptionMode> supported_encryption_schemes,
       media::SupportedCodecs supported_codecs,
-      media::SupportedCodecs supported_secure_codecs,
+      media::SupportedCodecs supported_hw_secure_codecs,
       Robustness max_audio_robustness,
       Robustness max_video_robustness,
       media::EmeSessionTypeSupport persistent_license_support,
@@ -48,7 +48,7 @@ class WidevineKeySystemProperties : public media::KeySystemProperties {
       media::EncryptionMode encryption_scheme) const override;
 
   media::SupportedCodecs GetSupportedCodecs() const override;
-  media::SupportedCodecs GetSupportedSecureCodecs() const override;
+  media::SupportedCodecs GetSupportedHwSecureCodecs() const override;
 
   media::EmeConfigRule GetRobustnessConfigRule(
       media::EmeMediaType media_type,
@@ -63,7 +63,7 @@ class WidevineKeySystemProperties : public media::KeySystemProperties {
  private:
   const base::flat_set<media::EncryptionMode> supported_encryption_schemes_;
   const media::SupportedCodecs supported_codecs_;
-  const media::SupportedCodecs supported_secure_codecs_;
+  const media::SupportedCodecs supported_hw_secure_codecs_;
   const Robustness max_audio_robustness_;
   const Robustness max_video_robustness_;
   const media::EmeSessionTypeSupport persistent_license_support_;
--- media/base/key_system_properties.cc
+++ media/base/key_system_properties.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media {
 
-SupportedCodecs KeySystemProperties::GetSupportedSecureCodecs() const {
+SupportedCodecs KeySystemProperties::GetSupportedHwSecureCodecs() const {
   return EME_CODEC_NONE;
 }
 
