5fe1eea8fe03719380ed74ece467f7b341d96231
Scott Violet <sky@chromium.org>
2018-06-12 18:22:21 +0000

    chromeos: Removes unnecessary code from ShellMainDelegate
    BUG=none
    TEST=none
    
    Change-Id: I61ad7b7fb7656619be687ec28d25aff2a827b5b7
    Reviewed-on: https://chromium-review.googlesource.com/1097573
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566506}

--- ash/shell/content/client/shell_main_delegate.cc
+++ ash/shell/content/client/shell_main_delegate.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "components/services/font/font_service_app.h"
 #include "components/services/font/public/interfaces/constants.mojom.h"
-#include "content/public/common/content_switches.h"
 #include "content/public/utility/content_utility_client.h"
 #include "services/service_manager/public/cpp/service.h"
 #include "services/ui/ime/test_ime_driver/public/mojom/constants.mojom.h"
@@ -95,13 +94,7 @@ ShellMainDelegate::ShellMainDelegate() = default;
 ShellMainDelegate::~ShellMainDelegate() = default;
 
 bool ShellMainDelegate::BasicStartupComplete(int* exit_code) {
-  const base::CommandLine& command_line =
-      *base::CommandLine::ForCurrentProcess();
-  std::string process_type =
-      command_line.GetSwitchValueASCII(switches::kProcessType);
-
   content::SetContentClient(&content_client_);
-
   return false;
 }
 
