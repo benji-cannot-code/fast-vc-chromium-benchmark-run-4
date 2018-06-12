35ab462e02f90a5fea4e7329e03a4e99d75d9ae7
Findit <findit-for-me@appspot.gserviceaccount.com>
2018-06-12 09:37:30 +0000

    Revert "[scheduler] Add a perftest to measure basic scheduling costs."
    This reverts commit 586e21ec4fd9a5d47b66628bbca27282420391a3.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 566362 as the
    culprit for failures in the build cycles as shown on:
    https://findit-for-me.appspot.com/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtLzU4NmUyMWVjNGZkOWE1ZDQ3YjY2NjI4YmJjYTI3MjgyNDIwMzkxYTMM
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium/Linux%20x64/66085
    
    Sample Failed Step: compile
    
    Original change's description:
    > [scheduler] Add a perftest to measure basic scheduling costs.
    > 
    > Change-Id: Idda9f71ae005250848fd2a118e5c4344e36be452
    > Reviewed-on: https://chromium-review.googlesource.com/1085295
    > Reviewed-by: Alex Clarke <alexclarke@chromium.org>
    > Commit-Queue: Alexander Timin <altimin@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#566362}
    
    Change-Id: Ie33113aec2652806e27fb627d93392b0fcb7d2f1
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/1096775
    Cr-Commit-Position: refs/heads/master@{#566364}

--- third_party/blink/renderer/platform/scheduler/main_thread/frame_scheduler_impl.cc
+++ third_party/blink/renderer/platform/scheduler/main_thread/frame_scheduler_impl.cc
@@ -351,7 +351,7 @@ scoped_refptr<base::SingleThreadTaskRunner> FrameSchedulerImpl::GetTaskRunner(
   return nullptr;
 }
 
-scoped_refptr<MainThreadTaskQueue> FrameSchedulerImpl::LoadingTaskQueue() {
+scoped_refptr<TaskQueue> FrameSchedulerImpl::LoadingTaskQueue() {
   DCHECK(parent_page_scheduler_);
   if (!loading_task_queue_) {
     // TODO(panicker): Avoid adding this queue in RS task_runners_.
@@ -365,8 +365,7 @@ scoped_refptr<MainThreadTaskQueue> FrameSchedulerImpl::LoadingTaskQueue() {
   return loading_task_queue_;
 }
 
-scoped_refptr<MainThreadTaskQueue>
-FrameSchedulerImpl::LoadingControlTaskQueue() {
+scoped_refptr<TaskQueue> FrameSchedulerImpl::LoadingControlTaskQueue() {
   DCHECK(parent_page_scheduler_);
   if (!loading_control_task_queue_) {
     loading_control_task_queue_ = main_thread_scheduler_->NewLoadingTaskQueue(
@@ -379,7 +378,7 @@ FrameSchedulerImpl::LoadingControlTaskQueue() {
   return loading_control_task_queue_;
 }
 
-scoped_refptr<MainThreadTaskQueue> FrameSchedulerImpl::ThrottleableTaskQueue() {
+scoped_refptr<TaskQueue> FrameSchedulerImpl::ThrottleableTaskQueue() {
   DCHECK(parent_page_scheduler_);
   if (!throttleable_task_queue_) {
     // TODO(panicker): Avoid adding this queue in RS task_runners_.
@@ -409,7 +408,7 @@ scoped_refptr<MainThreadTaskQueue> FrameSchedulerImpl::ThrottleableTaskQueue() {
   return throttleable_task_queue_;
 }
 
-scoped_refptr<MainThreadTaskQueue> FrameSchedulerImpl::DeferrableTaskQueue() {
+scoped_refptr<TaskQueue> FrameSchedulerImpl::DeferrableTaskQueue() {
   DCHECK(parent_page_scheduler_);
   if (!deferrable_task_queue_) {
     deferrable_task_queue_ = main_thread_scheduler_->NewTaskQueue(
@@ -428,7 +427,7 @@ scoped_refptr<MainThreadTaskQueue> FrameSchedulerImpl::DeferrableTaskQueue() {
   return deferrable_task_queue_;
 }
 
-scoped_refptr<MainThreadTaskQueue> FrameSchedulerImpl::PausableTaskQueue() {
+scoped_refptr<TaskQueue> FrameSchedulerImpl::PausableTaskQueue() {
   DCHECK(parent_page_scheduler_);
   if (!pausable_task_queue_) {
     pausable_task_queue_ = main_thread_scheduler_->NewTaskQueue(
@@ -446,7 +445,7 @@ scoped_refptr<MainThreadTaskQueue> FrameSchedulerImpl::PausableTaskQueue() {
   return pausable_task_queue_;
 }
 
-scoped_refptr<MainThreadTaskQueue> FrameSchedulerImpl::UnpausableTaskQueue() {
+scoped_refptr<TaskQueue> FrameSchedulerImpl::UnpausableTaskQueue() {
   DCHECK(parent_page_scheduler_);
   if (!unpausable_task_queue_) {
     unpausable_task_queue_ = main_thread_scheduler_->NewTaskQueue(
@@ -776,11 +775,6 @@ void FrameSchedulerImpl::RemovePauseSubresourceLoadingHandle() {
     subresource_loading_paused_ = false;
     UpdatePolicy();
   }
-}
-
-scoped_refptr<MainThreadTaskQueue>
-FrameSchedulerImpl::GetTaskQueueForTesting() {
-  return UnpausableTaskQueue();
 }
 
 }  // namespace scheduler
