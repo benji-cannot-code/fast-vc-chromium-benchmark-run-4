2bcaf67c2114ccb166f83669aa7794321759739d
Colin Blundell <blundell@chromium.org>
2018-06-12 13:43:08 +0000

    PrimaryAccountAccessTokenFetcher: Fold Start() into constructor
    As promised on
    https://chromium-review.googlesource.com/c/chromium/src/+/1092575.
    
    Change-Id: Ibd9946dda44bc9ec2a8f2594cbc1a27930f1bbb7
    Reviewed-on: https://chromium-review.googlesource.com/1097084
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566405}

--- services/identity/public/cpp/primary_account_access_token_fetcher.cc
+++ services/identity/public/cpp/primary_account_access_token_fetcher.cc
@@ -27,13 +27,6 @@ PrimaryAccountAccessTokenFetcher::PrimaryAccountAccessTokenFetcher(
       token_service_observer_(this),
       access_token_retried_(false),
       mode_(mode) {
-  Start();
-}
-
-PrimaryAccountAccessTokenFetcher::~PrimaryAccountAccessTokenFetcher() {
-}
-
-void PrimaryAccountAccessTokenFetcher::Start() {
   if (mode_ == Mode::kImmediate || AreCredentialsAvailable()) {
     StartAccessTokenRequest();
     return;
@@ -45,6 +38,8 @@ void PrimaryAccountAccessTokenFetcher::Start() {
   signin_manager_observer_.Add(signin_manager_);
   token_service_observer_.Add(token_service_);
 }
+
+PrimaryAccountAccessTokenFetcher::~PrimaryAccountAccessTokenFetcher() {}
 
 bool PrimaryAccountAccessTokenFetcher::AreCredentialsAvailable() const {
   DCHECK_EQ(Mode::kWaitUntilAvailable, mode_);
