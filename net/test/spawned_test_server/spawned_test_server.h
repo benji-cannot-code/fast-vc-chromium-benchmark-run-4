d9777aaa0f9ac16e9b928651aad2d4a7b4e19444
Sergey Volk <servolk@google.com>
2018-06-12 01:45:48 +0000

    Set //net:enable_python_utils=false for Chromecast
    Cast platform has no python runtime, and because of that PythonRunTime
    test is failing on our internal test bots (most other net_unittests
    pass, because they don't depend on python). So let's disable python
    utils for Cast.
    
    Bug: b/78913742
    Change-Id: I073f9b1414d26d33b147121cec3951d6a52ca19d
    Reviewed-on: https://chromium-review.googlesource.com/1081004
    Reviewed-by: Eric Roman <eroman@chromium.org>
    Reviewed-by: Alex Clarke <alexclarke@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Sergey Volk <servolk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566243}

--- net/test/spawned_test_server/spawned_test_server.h
+++ net/test/spawned_test_server/spawned_test_server.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "build/build_config.h"
 
-#if defined(OS_ANDROID) || defined(OS_FUCHSIA)
+#if defined(USE_REMOTE_TEST_SERVER)
 #include "net/test/spawned_test_server/remote_test_server.h"
 #else
 #include "net/test/spawned_test_server/local_test_server.h"
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace net {
 
-#if defined(OS_ANDROID) || defined(OS_FUCHSIA)
+#if defined(USE_REMOTE_TEST_SERVER)
 typedef RemoteTestServer SpawnedTestServer;
 #else
 typedef LocalTestServer SpawnedTestServer;
