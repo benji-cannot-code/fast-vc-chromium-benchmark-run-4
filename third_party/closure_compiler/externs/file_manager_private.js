1c28e45e95878afc35150554364c78107f125908
Sasha Morrissey <sashab@chromium.org>
2018-06-12 07:21:16 +0000

    Return capabilities in the FileManagerPrivate API
    Update FileManagerPrivate.getEntryProperties() call to return
    capabilities (permissions) for entries as well.
    
    Bug: 719959
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I40fbb9665afb4cd0a3c1d6124a2543db55ac89d0
    Reviewed-on: https://chromium-review.googlesource.com/1068767
    Commit-Queue: Sasha Morrissey <sashab@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566350}

--- third_party/closure_compiler/externs/file_manager_private.js
+++ third_party/closure_compiler/externs/file_manager_private.js
@@ -251,7 +251,12 @@ chrome.fileManagerPrivate.FileTask;
  *   starred: (boolean|undefined),
  *   externalFileUrl: (string|undefined),
  *   alternateUrl: (string|undefined),
- *   shareUrl: (string|undefined)
+ *   shareUrl: (string|undefined),
+ *   canCopy: (boolean|undefined),
+ *   canDelete: (boolean|undefined),
+ *   canRename: (boolean|undefined),
+ *   canAddChildren: (boolean|undefined),
+ *   canShare: (boolean|undefined)
  * }}
  */
 chrome.fileManagerPrivate.EntryProperties;
