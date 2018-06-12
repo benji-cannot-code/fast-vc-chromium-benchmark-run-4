60a9ac89a24c1210890b76266fa1a1916c0c5461
Chris Hamilton <chrisha@chromium.org>
2018-06-12 01:51:19 +0000

    Add policy to TabLoaderDelegate and integrate with TabLoader.
    This integrates the resource_coordinator::SessionRestorePolicy into
    TabLoaderDelegate, and performs the necessary plumbing in TabLoader.
    This enables fine-grained changes to session restore logic in support
    of extremely large session restores.
    
    BUG=749785
    
    Change-Id: I86682d199e0b4f1a2d57622bf98a572a9f13eba3
    Reviewed-on: https://chromium-review.googlesource.com/1093020
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Chris Hamilton <chrisha@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566252}

--- chrome/browser/sessions/tab_loader_delegate.h
+++ chrome/browser/sessions/tab_loader_delegate.h
@@ -10,6 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/time/time.h"
 
+namespace content {
+class WebContents;
+}
+
 class TabLoaderCallback {
  public:
   // This function will get called to suppress and to allow tab loading. Tab
@@ -21,7 +25,6 @@ class TabLoaderCallback {
 // the loading of hidden tabs starts.
 class TabLoaderDelegate {
  public:
-  TabLoaderDelegate() {}
   virtual ~TabLoaderDelegate() {}
 
   // Create a tab loader delegate. |TabLoaderCallback::SetTabLoadingEnabled| can
@@ -36,6 +39,26 @@ class TabLoaderDelegate {
   // Returns the default timeout time after which the next tab gets loaded if
   // the previous tab did not finish loading.
   virtual base::TimeDelta GetTimeoutBeforeLoadingNextTab() const = 0;
+
+  // Returns the maximum number of tabs that should be loading simultaneously.
+  virtual size_t GetMaxSimultaneousTabLoads() const = 0;
+
+  // Determines whether or not the given tab should be loaded. If this returns
+  // false, then the TabLoader no longer attempts to load |contents| and removes
+  // it from TabLoaders internal state. This is called immediately prior to
+  // trying to load the tab and allows the TabLoader to respond to changing
+  // conditions.
+  virtual bool ShouldLoad(content::WebContents* contents) const = 0;
+
+  // Notifies the delegate that a tab load has been initiated.
+  virtual void NotifyTabLoadStarted() = 0;
+
+ protected:
+  // The delegate should only be created via the factory function.
+  TabLoaderDelegate() {}
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(TabLoaderDelegate);
 };
 
 #endif  // CHROME_BROWSER_SESSIONS_TAB_LOADER_DELEGATE_H_
