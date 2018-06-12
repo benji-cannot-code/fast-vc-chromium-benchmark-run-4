b442a7a3b196b70df5de115ca2d6216d1009437e
Xing Liu <xingliu@chromium.org>
2018-06-12 03:18:45 +0000

    Download location: Update layout for storage summary in download home.
    This CL updates the layout for storage summary string in download home
    that the current SpannableString is not flexible enough.
    
    Now we use a dedicated TextView and the "Download" title will be in
    the center of a relative layout. Also move storage summary code to a
    single class.
    
    Bug: 849973
    Change-Id: Ic05c16d85f2f04e0bedb93ec3e6c48812ecc2cfb
    Reviewed-on: https://chromium-review.googlesource.com/1093385
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566292}

--- chrome/android/java/src/org/chromium/chrome/browser/download/ui/DownloadManagerUi.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/ui/DownloadManagerUi.java
@@ -277,7 +277,7 @@ public class DownloadManagerUi
                 true);
         mToolbar.getMenu().setGroupVisible(normalGroupId, true);
         mToolbar.setManager(this);
-        mToolbar.initializeFilterSpinner(mFilterAdapter);
+        mToolbar.initialize(mFilterAdapter);
 
         mToolbar.initializeSearchView(this, R.string.download_manager_search, mSearchMenuId);
 
