45a4a53da191226fcd8831edf44e16b5d1bc2a70
Marijn Kruisselbrink <mek@chromium.org>
2018-06-12 16:45:19 +0000

    Fix test flakiness around multi-threaded blob URL revocation.
    Registering a new blob URL in the worker will ensure (at least in chrome)
    that the revocation has been fully processed before the main thread
    tries to fetch the now revoked URL.
    
    Bug: 851942
    Change-Id: I12e794682dbb5b34dc08fe2a7d398e3ba02aaffa
    Reviewed-on: https://chromium-review.googlesource.com/1097230
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566460}

--- third_party/WebKit/LayoutTests/external/wpt/FileAPI/url/resources/revoke-helper.js
+++ third_party/WebKit/LayoutTests/external/wpt/FileAPI/url/resources/revoke-helper.js
@@ -1,5 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 self.addEventListener('message', e => {
   URL.revokeObjectURL(e.data.url);
+  // Registering a new object URL will make absolutely sure that the revocation
+  // has propagated. Without this at least in chrome it is possible for the
+  // below postMessage to arrive at its destination before the revocation has
+  // been fully processed.
+  URL.createObjectURL(new Blob([]));
   self.postMessage('revoked');
 });
