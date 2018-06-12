2f4483c1a385f91a580519eac3c7e8dc432040bb
Sasha Morrissey <sashab@chromium.org>
2018-06-12 21:08:22 +0000

    Cache capabilities for entries in the FileManager File List view
    Added capabilities (canCopy, canDelete, canRename, canAddChildren,
    canShare) to the metadata cache for Files App.
    This is pre-work to grey-out drag/drop and context-menu operations
    based on file permissions.
    
    Bug: 719959
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: If86cc407e1355aa01bd0fb1120c24da6eedee9b7
    Reviewed-on: https://chromium-review.googlesource.com/1068768
    Commit-Queue: Sasha Morrissey <sashab@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566569}

--- ui/file_manager/file_manager/foreground/js/metadata/external_metadata_provider.js
+++ ui/file_manager/file_manager/foreground/js/metadata/external_metadata_provider.js
@@ -19,11 +19,30 @@ function ExternalMetadataProvider() {
  * @const {!Array<string>}
  */
 ExternalMetadataProvider.PROPERTY_NAMES = [
-  'availableOffline', 'availableWhenMetered', 'contentMimeType',
-  'croppedThumbnailUrl', 'customIconUrl', 'dirty', 'externalFileUrl', 'hosted',
-  'imageHeight', 'imageRotation', 'imageWidth', 'modificationTime',
-  'modificationByMeTime', 'pinned', 'present', 'shared', 'sharedWithMe', 'size',
-  'thumbnailUrl'
+  'availableOffline',
+  'availableWhenMetered',
+  'contentMimeType',
+  'croppedThumbnailUrl',
+  'customIconUrl',
+  'dirty',
+  'externalFileUrl',
+  'hosted',
+  'imageHeight',
+  'imageRotation',
+  'imageWidth',
+  'modificationTime',
+  'modificationByMeTime',
+  'pinned',
+  'present',
+  'shared',
+  'sharedWithMe',
+  'size',
+  'thumbnailUrl',
+  'canCopy',
+  'canDelete',
+  'canRename',
+  'canAddChildren',
+  'canShare'
 ];
 
 ExternalMetadataProvider.prototype.__proto__ = MetadataProvider.prototype;
@@ -58,7 +77,8 @@ ExternalMetadataProvider.prototype.get = function(requests) {
 
 /**
  * @param {!Array<!MetadataRequest>} requests
- * @param {!Object<boolean>} nameMap
+ * @param {!Object<boolean>} nameMap A map of property names that will be used
+ *     to copy the value from |propertiesList|.
  * @param {!Array<!chrome.fileManagerPrivate.EntryProperties>} propertiesList
  * @return {!Array<!MetadataItem>}
  */
@@ -109,6 +129,14 @@ ExternalMetadataProvider.prototype.convertResults_ =
       item.size = requests[i].entry.isFile ? (prop.size || 0) : -1;
     if (prop.thumbnailUrl !== undefined || nameMap['thumbnailUrl'])
       item.thumbnailUrl = prop.thumbnailUrl;
+    if (prop.canCopy !== undefined || nameMap['canCopy'])
+      item.canCopy = prop.canCopy;
+    if (prop.canDelete !== undefined || nameMap['canDelete'])
+      item.canDelete = prop.canDelete;
+    if (prop.canRename !== undefined || nameMap['canRename'])
+      item.canRename = prop.canRename;
+    if (prop.canAddChildren !== undefined || nameMap['canAddChildren'])
+      item.canAddChildren = prop.canAddChildren;
     results.push(item);
   }
   return results;
