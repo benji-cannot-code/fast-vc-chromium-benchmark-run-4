3ea7d13069b36f4153f0b6c728d6e43126f2fb10
Dave Tapuska <dtapuska@chromium.org>
2018-06-12 14:28:04 +0000

    Silence 64 to 32 bit truncation errors on WTF strings.
    strlen returns size_t which can be 64 bits yet all of the WTF strings
    use 32 storage lengths. Add a helper function string_unsigned which
    returns the strlen with a SafeCast to unsigned. Place it in an
    implementation file to avoid increase in binary size due to inlining.
    
    BUG=588506
    
    Change-Id: I3b5025c1fee9b1eda9cd017af6bdfc66d808e802
    Reviewed-on: https://chromium-review.googlesource.com/1096133
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Yuta Kitamura <yutak@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566415}

--- third_party/blink/renderer/platform/wtf/text/string_concatenate.h
+++ third_party/blink/renderer/platform/wtf/text/string_concatenate.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string.h>
 #include "third_party/blink/renderer/platform/wtf/allocator.h"
+#include "third_party/blink/renderer/platform/wtf/string_extras.h"
 
 #ifndef WTFString_h
 #include "third_party/blink/renderer/platform/wtf/text/atomic_string.h"
@@ -102,7 +103,7 @@ class WTF_EXPORT StringTypeAdapter<char*> {
 
  public:
   explicit StringTypeAdapter<char*>(char* buffer)
-      : buffer_(buffer), length_(strlen(buffer)) {}
+      : buffer_(buffer), length_(strlen_unsigned(buffer)) {}
 
   unsigned length() const { return length_; }
   bool Is8Bit() const { return true; }
--- third_party/blink/renderer/platform/wtf/text/string_hash.h
+++ third_party/blink/renderer/platform/wtf/text/string_hash.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/platform/wtf/allocator.h"
 #include "third_party/blink/renderer/platform/wtf/hash_traits.h"
+#include "third_party/blink/renderer/platform/wtf/string_extras.h"
 #include "third_party/blink/renderer/platform/wtf/string_hasher.h"
 #include "third_party/blink/renderer/platform/wtf/text/atomic_string.h"
 
@@ -103,7 +104,7 @@ class CaseFoldingHash {
 
   static inline unsigned GetHash(const char* data) {
     return CaseFoldingHash::GetHash(reinterpret_cast<const LChar*>(data),
-                                    strlen(data));
+                                    strlen_unsigned(data));
   }
 
   static inline bool Equal(const StringImpl* a, const StringImpl* b) {
