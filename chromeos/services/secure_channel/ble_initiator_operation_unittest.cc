7e0940199d2cd7398d47ea443d0ac2a0a8784f62
Kyle Horimoto <khorimoto@google.com>
2018-06-12 19:50:21 +0000

    [CrOS MultiDevice] Add BleInitiatorOperation and BleListenerOperation.
    These files are currently still stubs, but they are needed for a
    follow-up CL. Implementation will come once the other CLs are finished.
    
    Bug: 824568, 752273
    Change-Id: I7456f713471f1608d99ba250bd3e001114c66c79
    Reviewed-on: https://chromium-review.googlesource.com/1097586
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566535}

---
+++ chromeos/services/secure_channel/ble_initiator_operation_unittest.cc
@@ -0,0 +1,37 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/services/secure_channel/ble_initiator_operation.h"
+
+#include <memory>
+
+#include "base/macros.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace chromeos {
+
+namespace secure_channel {
+
+class SecureChannelBleInitiatorOperationTest : public testing::Test {
+ protected:
+  SecureChannelBleInitiatorOperationTest() = default;
+  ~SecureChannelBleInitiatorOperationTest() override = default;
+
+  // testing::Test:
+  void SetUp() override {}
+
+ private:
+  std::unique_ptr<BleInitiatorOperation> operation_;
+
+  DISALLOW_COPY_AND_ASSIGN(SecureChannelBleInitiatorOperationTest);
+};
+
+TEST_F(SecureChannelBleInitiatorOperationTest, Test) {
+  // TODO(khorimoto): Add tests once implementation is complete.
+}
+
+}  // namespace secure_channel
+
+}  // namespace chromeos
---
+++ chromeos/services/secure_channel/ble_listener_operation_unittest.cc
@@ -0,0 +1,37 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/services/secure_channel/ble_listener_operation.h"
+
+#include <memory>
+
+#include "base/macros.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace chromeos {
+
+namespace secure_channel {
+
+class SecureChannelBleListenerOperationTest : public testing::Test {
+ protected:
+  SecureChannelBleListenerOperationTest() = default;
+  ~SecureChannelBleListenerOperationTest() override = default;
+
+  // testing::Test:
+  void SetUp() override {}
+
+ private:
+  std::unique_ptr<BleListenerOperation> operation_;
+
+  DISALLOW_COPY_AND_ASSIGN(SecureChannelBleListenerOperationTest);
+};
+
+TEST_F(SecureChannelBleListenerOperationTest, Test) {
+  // TODO(khorimoto): Add tests once implementation is complete.
+}
+
+}  // namespace secure_channel
+
+}  // namespace chromeos
