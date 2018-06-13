e64a410d6a93bb3dc54a750a36928cf9981a949e
Alexandre Courbot <acourbot@chromium.org>
2018-06-13 07:46:35 +0000

    media: use ScopedFD with VideoFrame
    DMA-BUF support in VideoFrame was suffering from several issues:
    
    * The number of DMA-BUF FDs provided to back a frame was assumed to be
    equal to the number of planes of the format of the frame. This is not
    always correct, since several planes can be backed by the same buffer,
    
    * FDs were always duplicated when creating a new VideoFrame - there was
    no option to make it take ownership of the passed FDs.
    
    * The class' public interface was taking FDs as ints instead of the more
    secure ScopedFD.
    
    This patch fixes these issues by doing the following:
    
    * Make VideoFrame::WrapExternalDmabufs() take a vector of ScopedFDs
    instead of a vector of integers, make it take ownership of the
    passed FDs, and make it accept a smaller number of FDs than there are
    planes in the frame's format,
    
    * Turn VideoFrame::DmabufFd() into VideoFrame::DmabufFds(), which
    returns all the FDs associated to a frame.
    
    * Introduce the media::DuplicateFDs() function which can be used by
    callers to explicitly duplicate a vector of FDs (a rather common thing
    to do).
    
    * Update users of VideoFrame::WrapExternalDmabufs() (concentrated in
    media/gpu/v4l2) to make use of this updated interface.
    
    BUG=850375
    BUG=b:73752373
    TEST=Tested VDA unittest and video playback on Hana (decoder + IP), and
    VEA unittest and resulting video playback on pit (IP + encoder).
    
    Change-Id: I9318fb3be82e3ff50c13c17dfd5faf8f4b5fd419
    Reviewed-on: https://chromium-review.googlesource.com/1090416
    Commit-Queue: Alexandre Courbot <acourbot@chromium.org>
    Reviewed-by: Pawel Osciak <posciak@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566752}

---
+++ media/base/scopedfd_helper.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <unistd.h>
+
+#include <vector>
+
+#include "base/posix/eintr_wrapper.h"
+#include "media/base/scopedfd_helper.h"
+
+namespace media {
+
+std::vector<base::ScopedFD> DuplicateFDs(
+    const std::vector<base::ScopedFD>& fds) {
+  std::vector<base::ScopedFD> ret;
+
+  for (auto& fd : fds) {
+    base::ScopedFD dup_fd = base::ScopedFD(HANDLE_EINTR(dup(fd.get())));
+    if (!dup_fd.is_valid()) {
+      DPLOG(ERROR) << "Failed to duplicate ScopedFD's file descriptor";
+      return std::vector<base::ScopedFD>();
+    }
+    ret.push_back(std::move(dup_fd));
+  }
+
+  return ret;
+}
+
+}  // namespace media
