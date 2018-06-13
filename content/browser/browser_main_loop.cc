e181ee8f01749feec8e6db6e8ab4674ada63c0f1
Scott Violet <sky@chromium.org>
2018-06-13 00:30:37 +0000

    chromeos: removes creation of ImageCursorsSet from content
    It's never used.
    
    BUG=none
    TEST=none
    
    Change-Id: If4677c56c51f33fe58eb97a4f03a72c087af8297
    Reviewed-on: https://chromium-review.googlesource.com/1097949
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566650}

--- content/browser/browser_main_loop.cc
+++ content/browser/browser_main_loop.cc
@@ -232,10 +232,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "crypto/nss_util.h"
 #endif
 
-#if BUILDFLAG(ENABLE_MUS)
-#include "services/ui/common/image_cursors_set.h"
-#endif
-
 // One of the linux specific headers defines this as a macro.
 #ifdef DestroyAll
 #undef DestroyAll
@@ -1011,12 +1007,6 @@ void BrowserMainLoop::ShutdownThreadsAndCleanUp() {
   if (RenderProcessHost::run_renderer_in_process())
     RenderProcessHostImpl::ShutDownInProcessRenderer();
 
-#if BUILDFLAG(ENABLE_MUS)
-  // NOTE: because of dependencies this has to happen before
-  // PostMainMessageLoopRun().
-  image_cursors_set_.reset();
-#endif
-
   if (parts_) {
     TRACE_EVENT0("shutdown",
                  "BrowserMainLoop::Subsystem:PostMainMessageLoopRun");
@@ -1516,11 +1506,6 @@ bool BrowserMainLoop::InitializeToolkit() {
                                        ? aura::Env::Mode::MUS
                                        : aura::Env::Mode::LOCAL);
 #endif  // defined(USE_AURA)
-
-#if BUILDFLAG(ENABLE_MUS)
-  if (features::IsMashEnabled())
-    image_cursors_set_ = std::make_unique<ui::ImageCursorsSet>();
-#endif
 
   if (parts_)
     parts_->ToolkitInitialized();
