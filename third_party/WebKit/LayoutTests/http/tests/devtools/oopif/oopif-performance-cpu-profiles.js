1f360944710d53e52e2b1e5bdf1e2f51edf5caa7
Alexei Filippov <alph@chromium.org>
2018-06-13 21:52:07 +0000

    Fix http/tests/devtools/oopif/oopif-performance-cpu-profiles.js" is flaky
    Make it sorted for real. Fixes one source of flakiness.
    
    BUG=848398
    
    Change-Id: I14e40f509e6363e06eeb5ed21bc239b59269bff8
    Reviewed-on: https://chromium-review.googlesource.com/1095479
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Alexei Filippov <alph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567005}

--- third_party/WebKit/LayoutTests/http/tests/devtools/oopif/oopif-performance-cpu-profiles.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/oopif/oopif-performance-cpu-profiles.js
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   await TestRunner.navigatePromise('resources/page.html');
   await PerformanceTestRunner.stopTimeline();
 
-  for (const track of PerformanceTestRunner.timelineModel().tracks().sort((a, b) => a.url > b.url)) {
+  for (const track of PerformanceTestRunner.timelineModel().tracks().sort((a, b) => a.url.compareTo(b.url))) {
     if (track.type !== TimelineModel.TimelineModel.TrackType.MainThread)
       continue;
     TestRunner.addResult(`name: ${track.name}`);
