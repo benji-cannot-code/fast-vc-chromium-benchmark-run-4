5f6c29950156827e5114211dcc46f79ba502f612
Alex Clarke <alexclarke@chromium.org>
2018-06-13 09:51:57 +0000

    Stop using LazilyDeallocatedDeque for now
    Seems there where some hard to understand crashes associated with it.
    I aim to re-land if possible.
    
    Change-Id: Iaf6a179ef186c1b0d027c17fa9276fafef23f55e
    Bug: 851381
    Reviewed-on: https://chromium-review.googlesource.com/1097489
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Alex Clarke <alexclarke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566779}

--- third_party/blink/renderer/platform/scheduler/base/task_queue_impl.h
+++ third_party/blink/renderer/platform/scheduler/base/task_queue_impl.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 
 #include "base/callback.h"
+#include "base/containers/circular_deque.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "base/message_loop/message_loop.h"
@@ -23,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/scheduler/base/enqueue_order.h"
 #include "third_party/blink/renderer/platform/scheduler/base/graceful_queue_shutdown_helper.h"
 #include "third_party/blink/renderer/platform/scheduler/base/intrusive_heap.h"
-#include "third_party/blink/renderer/platform/scheduler/base/lazily_deallocated_deque.h"
 #include "third_party/blink/renderer/platform/scheduler/base/task_queue_forward.h"
 
 namespace base {
@@ -390,9 +390,7 @@ class PLATFORM_EXPORT TaskQueueImpl {
   // empty.
   void PushOntoImmediateIncomingQueueLocked(Task task);
 
-  // We reserve an inline capacity of 8 tasks to try and reduce the load on
-  // PartitionAlloc.
-  using TaskDeque = sequence_manager::LazilyDeallocatedDeque<Task>;
+  using TaskDeque = circular_deque<Task>;
 
   // Extracts all the tasks from the immediate incoming queue and swaps it with
   // |queue| which must be empty.
