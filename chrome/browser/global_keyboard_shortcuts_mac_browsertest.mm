7e61cffd9870cc2c53fd2740a828b22d10e7f604
Tatiana Gornak <melandory@chromium.org>
2018-06-13 10:09:53 +0000

    Disable flacky GlobalKeyboardShortcutsTest.SwitchTabsMac on Mac
    TBR=avi@chromium.org
    
    Bug: 852232
    Change-Id: I45351c0c7f919a28d75a481bd1130d6682435a6b
    Reviewed-on: https://chromium-review.googlesource.com/1098667
    Reviewed-by: Tatiana Gornak <melandory@chromium.org>
    Commit-Queue: Tatiana Gornak <melandory@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566783}

--- chrome/browser/global_keyboard_shortcuts_mac_browsertest.mm
+++ chrome/browser/global_keyboard_shortcuts_mac_browsertest.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Cocoa/Cocoa.h>
 
 #include "base/run_loop.h"
+#include "build/build_config.h"
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/ui/browser.h"
@@ -49,6 +50,12 @@ void SendEvent(NSEvent* ns_event) {
 }  // namespace
 
 // Test that global keyboard shortcuts are handled by the native window.
+// https://crbug.com/852232
+#if defined(OS_MACOSX)
+#define MAYBE_SwitchTabsMac DISABLED_SwitchTabsMac
+#else
+#define MAYBE_SwitchTabsMac
+#endif
 IN_PROC_BROWSER_TEST_F(GlobalKeyboardShortcutsTest, SwitchTabsMac) {
   NSWindow* ns_window = browser()->window()->GetNativeWindow();
   TabStripModel* tab_strip = browser()->tab_strip_model();
