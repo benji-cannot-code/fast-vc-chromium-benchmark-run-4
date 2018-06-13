42489832575761c8a12e4735c0a927e853746023
Ben Wagner <bungeman@chromium.org>
2018-06-13 18:34:28 +0000

    Some iwyu for font_custom_platform_data.h.
    FontCustomPlatformData::FamilyNameForInspector returns an SkString so
    this adds an include for SkString.h. Previously this code compiled
    because SkTypes.h has a forward declaration of SkString which was pulled
    in though the SkRefCnt.h include. Skia is looking to remove this forward
    declaration in SkTypes.h.
    
    Change-Id: I638e13a0db4fba02e9caf73b448aa70ae2011100
    Reviewed-on: https://chromium-review.googlesource.com/1099416
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Commit-Queue: Ben Wagner <bungeman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566930}

--- third_party/blink/renderer/platform/fonts/font_custom_platform_data.h
+++ third_party/blink/renderer/platform/fonts/font_custom_platform_data.h
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/ref_counted.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 #include "third_party/skia/include/core/SkRefCnt.h"
+#include "third_party/skia/include/core/SkString.h"
 
 class SkTypeface;
 
