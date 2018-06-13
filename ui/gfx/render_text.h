ec2d8fa19a5173981ece9e0e89723579492415d8
Orin Jaworski <orinj@chromium.org>
2018-06-13 00:55:27 +0000

    [omnibox] Replaced insertion drop cursor with replacement outline.
    The drop cursor at a particular text location implies dropped text will
    be inserted there, and that remains true when dragging selected text
    from within omnibox itself (existing edit behavior remains intact).
    But when dragging text from elsewhere (no omnibox text selected), the
    new behavior will outline omnibox text indicating that the drop will
    replace current text.  Drop cursor logic remains the same, only the
    meaning of the drop is indicated with a more intuitive render effect.
    
    Bug: 329319
    Change-Id: I8aa76a497f857c0c418b9bdd7a350a597169165c
    Reviewed-on: https://chromium-review.googlesource.com/1091297
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566664}

--- ui/gfx/render_text.h
+++ ui/gfx/render_text.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/font_render_params.h"
 #include "ui/gfx/geometry/point.h"
 #include "ui/gfx/geometry/rect.h"
+#include "ui/gfx/geometry/rect_f.h"
 #include "ui/gfx/geometry/size_f.h"
 #include "ui/gfx/geometry/vector2d.h"
 #include "ui/gfx/range/range.h"
@@ -490,6 +491,9 @@ class GFX_EXPORT RenderText {
   // specifies the character range for which the corresponding font has been
   // chosen.
   virtual std::vector<FontSpan> GetFontSpansForTesting() = 0;
+
+  // Returns rectangle surrounding the current string (from origin to size)
+  RectF GetStringRect();
 
   // Get the visual bounds containing the logical substring within the |range|.
   // If |range| is empty, the result is empty. These bounds could be visually
