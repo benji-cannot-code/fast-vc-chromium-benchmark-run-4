001e9a0c166c4974447a9dfe54facbf16f938b0f
Zhiheng Vincent Li <vincentli@chromium.org>
2018-06-13 23:26:06 +0000

    Move CastCommandLineHelper.java from internal to upstream and let CastBrowserHelper use it to initialize command line.
    Bug: b/110021629
    
    Test: cast_shell_junit_tests
    
    Change-Id: I9940e95e26be072bc1762e60f6c6ae541277d34c
    Reviewed-on: https://chromium-review.googlesource.com/1097548
    Reviewed-by: Simeon Anfinrud <sanfin@chromium.org>
    Commit-Queue: Zhiheng(Vincent) Li <vincentli@google.com>
    Cr-Commit-Position: refs/heads/master@{#567041}

--- chromecast/browser/android/apk/src/org/chromium/chromecast/shell/CastBrowserHelper.java
+++ chromecast/browser/android/apk/src/org/chromium/chromecast/shell/CastBrowserHelper.java
@@ -5,11 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chromecast.shell;
 
-import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 
-import org.chromium.base.CommandLine;
 import org.chromium.base.Log;
 import org.chromium.base.library_loader.LibraryLoader;
 import org.chromium.base.library_loader.LibraryProcessType;
@@ -24,8 +21,6 @@ import org.chromium.net.NetworkChangeNotifier;
  */
 public class CastBrowserHelper {
     private static final String TAG = "CastBrowserHelper";
-
-    public static final String COMMAND_LINE_ARGS_KEY = "commandLineArgs";
 
     private static boolean sIsBrowserInitialized = false;
 
@@ -44,17 +39,8 @@ public class CastBrowserHelper {
         ChromecastConfigAndroid.initializeForBrowser(context);
 
         // Initializing the command line must occur before loading the library.
-        if (!CommandLine.isInitialized()) {
-            ((CastApplication) context.getApplicationContext()).initCommandLine();
-
-            if (context instanceof Activity) {
-                Intent launchingIntent = ((Activity) context).getIntent();
-                String[] commandLineParams = getCommandLineParamsFromIntent(launchingIntent);
-                if (commandLineParams != null) {
-                    CommandLine.getInstance().appendSwitchesAndArguments(commandLineParams);
-                }
-            }
-        }
+        CastCommandLineHelper.initCommandLineWithSavedArgs(
+                () -> { ((CastApplication) context.getApplicationContext()).initCommandLine(); });
 
         DeviceUtils.addDeviceSpecificUserAgentSwitch(context);
 
@@ -73,9 +59,5 @@ public class CastBrowserHelper {
             Log.e(TAG, "Unable to launch browser process.", e);
             return false;
         }
-    }
-
-    private static String[] getCommandLineParamsFromIntent(Intent intent) {
-        return intent != null ? intent.getStringArrayExtra(COMMAND_LINE_ARGS_KEY) : null;
     }
 }
