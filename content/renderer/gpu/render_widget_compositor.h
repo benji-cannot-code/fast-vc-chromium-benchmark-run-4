109ce4828f261c120a39db6ca7476f6175288b7f
Findit <findit-for-me@appspot.gserviceaccount.com>
2018-06-13 07:14:13 +0000

    Revert "paint timing: Use presentation-time instead of swap-time."
    This reverts commit bf746c55adf49d2f22cc308185a16d12c4e15b04.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 566724 as the
    culprit for failures in the build cycles as shown on:
    https://findit-for-me.appspot.com/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtL2JmNzQ2YzU1YWRmNDlkMmYyMmNjMzA4MTg1YTE2ZDEyYzRlMTViMDQM
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium.linux/Linux%20Tests%20%28dbg%29%281%29/72596
    
    Sample Failed Step: not_site_per_process_browser_tests
    
    Original change's description:
    > paint timing: Use presentation-time instead of swap-time.
    > 
    > The presentation-time from gpu gives a more correct view of what the
    > user experiences, rather than the swap-time in the renderer process.
    > So switch to using the presentation-time to measure the various
    > paint-time metrics.
    > 
    > The various paint-time metrics in telemetry tests (loading benchmark)
    > will go up after this change, but it is expected, and it is not a
    > regression. Results on various platforms with this change:
    > 
    > Win 7: https://pinpoint-dot-chromeperf.appspot.com/job/14d09714240000
    > Win 10: https://pinpoint-dot-chromeperf.appspot.com/job/149921e4240000
    > Mac low-end: https://pinpoint-dot-chromeperf.appspot.com/job/12eeeba5240000
    > Android one: https://pinpoint-dot-chromeperf.appspot.com/job/16ab13e9240000
    > Android N5x: https://pinpoint-dot-chromeperf.appspot.com/job/12d45da5240000
    > 
    > BUG=811961
    > 
    > Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    > Change-Id: I70a86eb85e84670942fe3fe6341926b5bc6255b9
    > Reviewed-on: https://chromium-review.googlesource.com/1050693
    > Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    > Reviewed-by: Antoine Labour <piman@chromium.org>
    > Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    > Reviewed-by: Brian Anderson <brianderson@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#566724}
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    BUG=811961
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Change-Id: Ia66f54dd90c3010d1103c7ddc657e2765f67a1e3
    Reviewed-on: https://chromium-review.googlesource.com/1098775
    Cr-Commit-Position: refs/heads/master@{#566743}

--- content/renderer/gpu/render_widget_compositor.h
+++ content/renderer/gpu/render_widget_compositor.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include "base/callback.h"
-#include "base/containers/circular_deque.h"
 #include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
 #include "base/values.h"
@@ -210,10 +209,10 @@ class CONTENT_EXPORT RenderWidgetCompositor
   void DidCommitAndDrawFrame() override;
   void DidReceiveCompositorFrameAck() override;
   void DidCompletePageScaleAnimation() override;
-  bool IsForSubframe() override;
   void DidPresentCompositorFrame(
       uint32_t frame_token,
-      const gfx::PresentationFeedback& feedback) override;
+      const gfx::PresentationFeedback& feedback) override {}
+  bool IsForSubframe() override;
 
   // cc::LayerTreeHostSingleThreadClient implementation.
   void RequestScheduleAnimation() override;
@@ -230,10 +229,6 @@ class CONTENT_EXPORT RenderWidgetCompositor
   void CreateRenderFrameObserver(
       mojom::RenderFrameMetadataObserverRequest request,
       mojom::RenderFrameMetadataObserverClientPtrInfo client_info);
-
-  void AddPresentationCallback(
-      uint32_t frame_token,
-      base::OnceCallback<void(base::TimeTicks)> callback);
 
   cc::LayerTreeHost* layer_tree_host() { return layer_tree_host_.get(); }
 
@@ -265,10 +260,6 @@ class CONTENT_EXPORT RenderWidgetCompositor
   base::OnceClosure layout_and_paint_async_callback_;
 
   viz::FrameSinkId frame_sink_id_;
-  base::circular_deque<
-      std::pair<uint32_t,
-                std::vector<base::OnceCallback<void(base::TimeTicks)>>>>
-      presentation_callbacks_;
 
   base::WeakPtrFactory<RenderWidgetCompositor> weak_factory_;
 
