fa4a25a1b7b6dc3c48cf982b7886677b722d82fa
Sunny <ratsunny@gmail.com>
2018-06-13 04:51:57 +0000

    Align FormData CRLF normalization behavior with standards
    According to xhr spec[1], no CRLF normalize operation need to be
    performed when invoking FormData methods, so all normalization
    call is removed in this commit.
    
    But in form sepc[2], when appending data from form controls to
    FormData, both name and value should be normalized. New methods
    |FormData::AppendFromElement| will perform the normalize operation.
    
    [1] https://xhr.spec.whatwg.org/#dom-formdata-append
    [2] https://html.spec.whatwg.org/multipage/form-control-infrastructure.html#append-an-entry
    
    BUG=798030
    
    Change-Id: If90a8e500705013c2935e5290624d4414dd32a72
    Reviewed-on: https://chromium-review.googlesource.com/1091238
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566723}

--- third_party/blink/renderer/core/html/forms/form_data.cc
+++ third_party/blink/renderer/core/html/forms/form_data.cc
@@ -99,7 +99,7 @@ void FormData::Trace(blink::Visitor* visitor) {
 }
 
 void FormData::append(const String& name, const String& value) {
-  entries_.push_back(new Entry(Normalize(name), Normalize(value)));
+  entries_.push_back(new Entry(name, value));
 }
 
 void FormData::append(ScriptState* script_state,
@@ -114,10 +114,9 @@ void FormData::append(ScriptState* script_state,
 }
 
 void FormData::deleteEntry(const String& name) {
-  const String normalized_name = Normalize(name);
   size_t i = 0;
   while (i < entries_.size()) {
-    if (entries_[i]->name() == normalized_name) {
+    if (entries_[i]->name() == name) {
       entries_.EraseAt(i);
     } else {
       ++i;
@@ -126,9 +125,8 @@ void FormData::deleteEntry(const String& name) {
 }
 
 void FormData::get(const String& name, FormDataEntryValue& result) {
-  const String normalized_name = Normalize(name);
   for (const auto& entry : Entries()) {
-    if (entry->name() == normalized_name) {
+    if (entry->name() == name) {
       if (entry->IsString()) {
         result.SetUSVString(entry->Value());
       } else {
@@ -143,9 +141,8 @@ void FormData::get(const String& name, FormDataEntryValue& result) {
 HeapVector<FormDataEntryValue> FormData::getAll(const String& name) {
   HeapVector<FormDataEntryValue> results;
 
-  const String normalized_name = Normalize(name);
   for (const auto& entry : Entries()) {
-    if (entry->name() != normalized_name)
+    if (entry->name() != name)
       continue;
     FormDataEntryValue value;
     if (entry->IsString()) {
@@ -160,29 +157,27 @@ HeapVector<FormDataEntryValue> FormData::getAll(const String& name) {
 }
 
 bool FormData::has(const String& name) {
-  const String normalized_name = Normalize(name);
   for (const auto& entry : Entries()) {
-    if (entry->name() == normalized_name)
+    if (entry->name() == name)
       return true;
   }
   return false;
 }
 
 void FormData::set(const String& name, const String& value) {
-  SetEntry(new Entry(Normalize(name), Normalize(value)));
+  SetEntry(new Entry(name, value));
 }
 
 void FormData::set(const String& name, Blob* blob, const String& filename) {
-  SetEntry(new Entry(Normalize(name), blob, filename));
+  SetEntry(new Entry(name, blob, filename));
 }
 
 void FormData::SetEntry(const Entry* entry) {
   DCHECK(entry);
-  const String normalized_name = entry->name();
   bool found = false;
   size_t i = 0;
   while (i < entries_.size()) {
-    if (entries_[i]->name() != normalized_name) {
+    if (entries_[i]->name() != entry->name()) {
       ++i;
     } else if (found) {
       entries_.EraseAt(i);
@@ -196,12 +191,20 @@ void FormData::SetEntry(const Entry* entry) {
     entries_.push_back(entry);
 }
 
-void FormData::append(const String& name, int value) {
-  append(name, String::Number(value));
+void FormData::append(const String& name, Blob* blob, const String& filename) {
+  entries_.push_back(new Entry(name, blob, filename));
 }
 
-void FormData::append(const String& name, Blob* blob, const String& filename) {
-  entries_.push_back(new Entry(Normalize(name), blob, filename));
+void FormData::AppendFromElement(const String& name, int value) {
+  append(Normalize(name), String::Number(value));
+}
+
+void FormData::AppendFromElement(const String& name, File* file) {
+  entries_.push_back(new Entry(Normalize(name), file, String()));
+}
+
+void FormData::AppendFromElement(const String& name, const String& value) {
+  entries_.push_back(new Entry(Normalize(name), Normalize(value)));
 }
 
 CString FormData::Encode(const String& string) const {
