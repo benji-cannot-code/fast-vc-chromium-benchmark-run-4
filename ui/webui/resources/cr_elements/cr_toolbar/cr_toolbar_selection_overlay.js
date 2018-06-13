7a54a4c2157df80116cc58397ecc870c3cd1c5ae
Christopher Lam <calamity@chromium.org>
2018-06-13 08:50:23 +0000

    [cr-toolbar-selection-overlay] Hide toolbar when showing overlay.
    This CL fixes an issue where the overlay didn't hide the toolbar properly
    so it could still be tabbed to, and a corresponding problem where overlay
    buttons could be tabbed to when not visible.
    
    Bug: 851835, 851864
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I1562211745c2373ee64a85936953137f2541ae48
    Reviewed-on: https://chromium-review.googlesource.com/1098420
    Commit-Queue: calamity <calamity@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566763}

--- ui/webui/resources/cr_elements/cr_toolbar/cr_toolbar_selection_overlay.js
+++ ui/webui/resources/cr_elements/cr_toolbar/cr_toolbar_selection_overlay.js
@@ -9,7 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * <cr-toolbar>. See <history-toolbar> for an example usage.
  *
  * Note that the embedder is expected to set position: relative to make the
- * absolute positioning of this element work.
+ * absolute positioning of this element work, and the cr-toolbar should have the
+ * has-overlay attribute set when its overlay is shown to prevent access through
+ * tab-traversal.
  */
 
 Polymer({
