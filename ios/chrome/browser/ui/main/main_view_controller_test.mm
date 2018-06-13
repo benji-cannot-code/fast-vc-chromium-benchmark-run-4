ccba7df93abfdcc1812579553938dbe53076d54b
edchin <edchin@chromium.org>
2018-06-13 06:06:50 +0000

    [ios] Add test to update offscreen cells in tab grid
    A crash will occur when an scrolled offscreen cell is updated.
    This is because UICollectionView's |-cellForItemAtIndexPath:| returns
    nil if the cell is offscreen.
    
    This CL adds a unittest that fails without the fix, which is also
    in this CL. The fix is to skip cell configuration if the cell is nil.
    
    Bug: 851352
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Iccdb53e3a67b58f225a09ce32ac70f48d42431ef
    Reviewed-on: https://chromium-review.googlesource.com/1094210
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: edchin <edchin@chromium.org>
    Commit-Queue: edchin <edchin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566733}

--- ios/chrome/browser/ui/main/main_view_controller_test.mm
+++ ios/chrome/browser/ui/main/main_view_controller_test.mm
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <UIKit/UIKit.h>
 
 #import "ios/chrome/browser/ui/tab_switcher/tab_switcher.h"
-#include "testing/gtest_mac.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -55,22 +54,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 id<TabSwitcher> MainViewControllerTest::CreateTestTabSwitcher() {
   return [[TestTabSwitcherViewController alloc] init];
-}
-
-// Sets the current key window's rootViewController and saves a pointer to
-// the original VC to allow restoring it at the end of the test.
-void MainViewControllerTest::SetRootViewController(
-    UIViewController* new_root_view_controller) {
-  original_root_view_controller_ =
-      [[[UIApplication sharedApplication] keyWindow] rootViewController];
-  [[UIApplication sharedApplication] keyWindow].rootViewController =
-      new_root_view_controller;
-}
-
-void MainViewControllerTest::TearDown() {
-  if (original_root_view_controller_) {
-    [[UIApplication sharedApplication] keyWindow].rootViewController =
-        original_root_view_controller_;
-    original_root_view_controller_ = nil;
-  }
 }
