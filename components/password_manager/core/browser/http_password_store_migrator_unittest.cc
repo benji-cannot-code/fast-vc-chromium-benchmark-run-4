ca11fe935476179f33372eb1c4d5010a8c856266
Vasilii Sukhanov <vasilii@chromium.org>
2018-06-13 15:38:49 +0000

    Fix the format of signon_realm when migrating a credential HTTP -> HTTPS.
    HttpPasswordStoreMigrator copies/moves http saved credentials to https. However, the format of the signon_realm was wrong. It should not contain the whole URL path. That is, instead of "https://google.com/something/" it should save "https://google.com/".
    
    Bug: 852356
    Change-Id: I25999f938a5ace9378c3ec0fb614491e28a71e88
    Reviewed-on: https://chromium-review.googlesource.com/1098967
    Reviewed-by: Vaclav Brozek <vabr@chromium.org>
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566842}

--- components/password_manager/core/browser/http_password_store_migrator_unittest.cc
+++ components/password_manager/core/browser/http_password_store_migrator_unittest.cc
@@ -24,15 +24,15 @@ using testing::SaveArg;
 using testing::Unused;
 using testing::_;
 
-constexpr char kTestHttpsURL[] = "https://example.org/";
-constexpr char kTestHttpURL[] = "http://example.org/";
-constexpr char kTestSubdomainHttpURL[] = "http://login.example.org/";
+constexpr char kTestHttpsURL[] = "https://example.org/path";
+constexpr char kTestHttpURL[] = "http://example.org/path";
+constexpr char kTestSubdomainHttpURL[] = "http://login.example.org/path2";
 
 // Creates a dummy http form with some basic arbitrary values.
 PasswordForm CreateTestForm() {
   PasswordForm form;
   form.origin = GURL(kTestHttpURL);
-  form.signon_realm = form.origin.spec();
+  form.signon_realm = form.origin.GetOrigin().spec();
   form.action = GURL("https://example.org/action.html");
   form.username_value = base::ASCIIToUTF16("user");
   form.password_value = base::ASCIIToUTF16("password");
@@ -43,7 +43,7 @@ PasswordForm CreateTestForm() {
 PasswordForm CreateTestPSLForm() {
   PasswordForm form;
   form.origin = GURL(kTestSubdomainHttpURL);
-  form.signon_realm = form.origin.spec();
+  form.signon_realm = form.origin.GetOrigin().spec();
   form.action = GURL(kTestSubdomainHttpURL);
   form.username_value = base::ASCIIToUTF16("user2");
   form.password_value = base::ASCIIToUTF16("password2");
@@ -128,8 +128,7 @@ class HttpPasswordStoreMigratorTest : public testing::Test {
 };
 
 void HttpPasswordStoreMigratorTest::TestEmptyStore(bool is_hsts) {
-  PasswordStore::FormDigest form(autofill::PasswordForm::SCHEME_HTML,
-                                 kTestHttpURL, GURL(kTestHttpURL));
+  PasswordStore::FormDigest form(CreateTestForm());
   EXPECT_CALL(store(), GetLogins(form, _));
   PasswordManagerClient::HSTSCallback callback;
   EXPECT_CALL(client(), PostHSTSQueryForHost(GURL(kTestHttpsURL), _))
@@ -140,7 +139,8 @@ void HttpPasswordStoreMigratorTest::TestEmptyStore(bool is_hsts) {
   // We expect a potential call to |RemoveSiteStatsImpl| which is a async task
   // posted from |PasswordStore::RemoveSiteStats|. Hence the following lines are
   // necessary to ensure |RemoveSiteStatsImpl| gets called when expected.
-  EXPECT_CALL(store(), RemoveSiteStatsImpl(GURL(kTestHttpURL))).Times(is_hsts);
+  EXPECT_CALL(store(), RemoveSiteStatsImpl(GURL(kTestHttpURL).GetOrigin()))
+      .Times(is_hsts);
   WaitForPasswordStore();
 
   EXPECT_CALL(consumer(), ProcessForms(std::vector<autofill::PasswordForm*>()));
@@ -149,8 +149,7 @@ void HttpPasswordStoreMigratorTest::TestEmptyStore(bool is_hsts) {
 }
 
 void HttpPasswordStoreMigratorTest::TestFullStore(bool is_hsts) {
-  PasswordStore::FormDigest form_digest(autofill::PasswordForm::SCHEME_HTML,
-                                        kTestHttpURL, GURL(kTestHttpURL));
+  PasswordStore::FormDigest form_digest(CreateTestForm());
   EXPECT_CALL(store(), GetLogins(form_digest, _));
   PasswordManagerClient::HSTSCallback callback;
   EXPECT_CALL(client(), PostHSTSQueryForHost(GURL(kTestHttpsURL), _))
@@ -161,7 +160,8 @@ void HttpPasswordStoreMigratorTest::TestFullStore(bool is_hsts) {
   // We expect a potential call to |RemoveSiteStatsImpl| which is a async task
   // posted from |PasswordStore::RemoveSiteStats|. Hence the following lines are
   // necessary to ensure |RemoveSiteStatsImpl| gets called when expected.
-  EXPECT_CALL(store(), RemoveSiteStatsImpl(GURL(kTestHttpURL))).Times(is_hsts);
+  EXPECT_CALL(store(), RemoveSiteStatsImpl(GURL(kTestHttpURL).GetOrigin()))
+      .Times(is_hsts);
   WaitForPasswordStore();
 
   PasswordForm form = CreateTestForm();
@@ -169,7 +169,7 @@ void HttpPasswordStoreMigratorTest::TestFullStore(bool is_hsts) {
   PasswordForm android_form = CreateAndroidCredential();
   PasswordForm expected_form = form;
   expected_form.origin = GURL(kTestHttpsURL);
-  expected_form.signon_realm = expected_form.origin.spec();
+  expected_form.signon_realm = expected_form.origin.GetOrigin().spec();
 
   EXPECT_CALL(store(), AddLogin(expected_form));
   EXPECT_CALL(store(), RemoveLogin(form)).Times(is_hsts);
@@ -206,7 +206,8 @@ void HttpPasswordStoreMigratorTest::TestMigratorDeletionByConsumer(
   // We expect a potential call to |RemoveSiteStatsImpl| which is a async task
   // posted from |PasswordStore::RemoveSiteStats|. Hence the following lines are
   // necessary to ensure |RemoveSiteStatsImpl| gets called when expected.
-  EXPECT_CALL(store(), RemoveSiteStatsImpl(GURL(kTestHttpURL))).Times(is_hsts);
+  EXPECT_CALL(store(), RemoveSiteStatsImpl(GURL(kTestHttpURL).GetOrigin()))
+      .Times(is_hsts);
   WaitForPasswordStore();
 }
 
