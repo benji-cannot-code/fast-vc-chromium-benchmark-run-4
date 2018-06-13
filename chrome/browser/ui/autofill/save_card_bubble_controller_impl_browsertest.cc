11c569332233ba860e3c032824f142010311e924
Fabio Tirelo <ftirelo@chromium.org>
2018-06-13 15:35:45 +0000

    Enable experimental UI for CC dialog tests
    There is no reason to add complexity to the tests to also run with
    experimental UI feature disabled, since we are mainly using it for
    quickly iterating on UI improvements.
    
    Change-Id: Id9674eec7b3714629aaf045b7ba15c232c589de7
    Reviewed-on: https://chromium-review.googlesource.com/1099046
    Reviewed-by: Mathieu Perreault <mathp@chromium.org>
    Commit-Queue: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566841}

--- chrome/browser/ui/autofill/save_card_bubble_controller_impl_browsertest.cc
+++ chrome/browser/ui/autofill/save_card_bubble_controller_impl_browsertest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/json/json_reader.h"
 #include "base/macros.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/values.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
@@ -18,12 +19,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/autofill/core/browser/autofill_test_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "ui/base/ui_base_features.h"
 
 namespace autofill {
 
 class SaveCardBubbleControllerImplTest : public DialogBrowserTest {
  public:
   SaveCardBubbleControllerImplTest() {}
+
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    DialogBrowserTest::SetUpCommandLine(command_line);
+    scoped_feature_list_.InitAndEnableFeature(features::kExperimentalUi);
+  }
 
   std::unique_ptr<base::DictionaryValue> GetTestLegalMessage() {
     std::unique_ptr<base::Value> value(base::JSONReader::Read(
@@ -64,6 +71,7 @@ class SaveCardBubbleControllerImplTest : public DialogBrowserTest {
 
  private:
   SaveCardBubbleControllerImpl* controller_ = nullptr;
+  base::test::ScopedFeatureList scoped_feature_list_;
 
   DISALLOW_COPY_AND_ASSIGN(SaveCardBubbleControllerImplTest);
 };
