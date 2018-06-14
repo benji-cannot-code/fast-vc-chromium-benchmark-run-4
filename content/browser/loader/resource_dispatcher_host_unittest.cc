b9db27429da382fdee8808f7623165cb942e2ee1
Yutaka Hirano <yhirano@chromium.org>
2018-06-14 05:46:46 +0000

    Enable RendererSideResourceScheduler
    It has been enabled on the finch config. This CL enables the switch by
    default on the chromium src.
    
    resource_scheduler_unittests.cc is largely affected by the change. This
    CL removes some cases and modifies some cases to keep it green.
    
    Bug: 794073, 754918
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: Ie49e5f0261a70f581ca766c3eeeae8a8f0e4e66b
    Reviewed-on: https://chromium-review.googlesource.com/1075916
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567159}

--- content/browser/loader/resource_dispatcher_host_unittest.cc
+++ content/browser/loader/resource_dispatcher_host_unittest.cc
@@ -78,6 +78,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/url_request_test_job.h"
 #include "net/url_request/url_request_test_util.h"
 #include "services/network/public/cpp/resource_request.h"
+#include "services/network/resource_scheduler.h"
+#include "services/network/resource_scheduler_params_manager.h"
 #include "services/network/test/test_url_loader_client.h"
 #include "storage/browser/blob/shareable_file_reference.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -838,6 +840,18 @@ class ResourceDispatcherHostTest : public testing::Test {
            request_info->detachable_handler()->is_detached();
   }
 
+  void SetMaxDelayableRequests(size_t max_delayable_requests) {
+    network::ResourceSchedulerParamsManager::ParamsForNetworkQualityContainer c;
+    for (int i = 0; i != net::EFFECTIVE_CONNECTION_TYPE_LAST; ++i) {
+      auto type = static_cast<net::EffectiveConnectionType>(i);
+      c[type] =
+          network::ResourceSchedulerParamsManager::ParamsForNetworkQuality(
+              max_delayable_requests, 0.0, false);
+    }
+    host_.scheduler()->SetResourceSchedulerParamsManagerForTests(
+        network::ResourceSchedulerParamsManager(c));
+  }
+
   content::TestBrowserThreadBundle thread_bundle_;
   std::unique_ptr<TestBrowserContext> browser_context_;
   std::unique_ptr<TestURLRequestJobFactory> job_factory_;
@@ -1467,6 +1481,7 @@ TEST_F(ResourceDispatcherHostTest, CancelRequestsOnRenderFrameDeleted) {
   host_.SetDelegate(&delegate);
   host_.OnRenderViewHostCreated(filter_->child_id(), 0,
                                 request_context_getter_.get());
+  SetMaxDelayableRequests(1);
 
   // One RenderView issues a high priority request and a low priority one. Both
   // should be started.
