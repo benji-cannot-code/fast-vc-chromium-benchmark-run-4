a6a869ee9bdb9964034944338ba548e10b404955
Ted Choc <tedchoc@google.com>
2018-06-14 23:14:31 +0000

    Migrate TabSwitcherButton to PropertyModel
    BUG=
    
    Change-Id: I88dd7bada07824769b07d71b8669d7ba2ebe6e69
    Reviewed-on: https://chromium-review.googlesource.com/1101702
    Reviewed-by: Pedro Amaral <amaralp@chromium.org>
    Commit-Queue: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567468}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/TabSwitcherButtonViewBinder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/TabSwitcherButtonViewBinder.java
@@ -5,8 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.toolbar;
 
+import org.chromium.chrome.browser.modelutil.PropertyKey;
+import org.chromium.chrome.browser.modelutil.PropertyModel;
 import org.chromium.chrome.browser.modelutil.PropertyModelChangeProcessor;
-import org.chromium.chrome.browser.toolbar.TabSwitcherButtonModel.PropertyKey;
 
 /**
  * This class is responsible for pushing updates to the Android view of the tab switcher. These
@@ -14,8 +15,8 @@ import org.chromium.chrome.browser.toolbar.TabSwitcherButtonModel.PropertyKey;
  * received.
  */
 public class TabSwitcherButtonViewBinder
-        implements PropertyModelChangeProcessor.ViewBinder<TabSwitcherButtonModel,
-                TabSwitcherButtonView, TabSwitcherButtonModel.PropertyKey> {
+        implements PropertyModelChangeProcessor
+                           .ViewBinder<PropertyModel, TabSwitcherButtonView, PropertyKey> {
     /**
      * Build a binder that handles interaction between the model and the views that make up the
      * tab switcher.
@@ -24,13 +25,14 @@ public class TabSwitcherButtonViewBinder
 
     @Override
     public final void bind(
-            TabSwitcherButtonModel model, TabSwitcherButtonView view, PropertyKey propertyKey) {
-        if (PropertyKey.NUMBER_OF_TABS == propertyKey) {
-            view.updateTabCountVisuals(model.getNumberOfTabs());
-        } else if (PropertyKey.ON_CLICK_LISTENER == propertyKey) {
-            view.setOnClickListener(model.getOnClickListener());
-        } else if (PropertyKey.ON_LONG_CLICK_LISTENER == propertyKey) {
-            view.setOnLongClickListener(model.getOnLongClickListener());
+            PropertyModel model, TabSwitcherButtonView view, PropertyKey propertyKey) {
+        if (TabSwitcherButtonProperties.NUMBER_OF_TABS == propertyKey) {
+            view.updateTabCountVisuals(model.getValue(TabSwitcherButtonProperties.NUMBER_OF_TABS));
+        } else if (TabSwitcherButtonProperties.ON_CLICK_LISTENER == propertyKey) {
+            view.setOnClickListener(model.getValue(TabSwitcherButtonProperties.ON_CLICK_LISTENER));
+        } else if (TabSwitcherButtonProperties.ON_LONG_CLICK_LISTENER == propertyKey) {
+            view.setOnLongClickListener(
+                    model.getValue(TabSwitcherButtonProperties.ON_LONG_CLICK_LISTENER));
         } else {
             assert false : "Unhandled property detected in TabSwitcherViewBinder!";
         }
