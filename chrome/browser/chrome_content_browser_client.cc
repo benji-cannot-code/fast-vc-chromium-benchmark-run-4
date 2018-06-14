b9ebc4d3b7ac7885fefb28886625e06cc9efb7e5
Ryan Hansberry <hansberry@chromium.org>
2018-06-14 21:28:05 +0000

    [CrOS Multidevice] Register SecureChannelService.
    SecureChannelService is created in ChromeContentBrowserClient, instead of
    ProfileImpl, because it is intended to be global, i.e. not profile-specific.
    
    Also note that the Connector object provided to SecureChannelClient is tied
    to the browser process, not the active Profile.
    
    Bug: 824568, 752273
    Change-Id: Ic99e22cc320b4e92e11bf48acd0592d7c1ef0eac
    Reviewed-on: https://chromium-review.googlesource.com/1097513
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567427}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -318,7 +318,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/ash/chrome_browser_main_extra_parts_ash.h"
 #include "chrome/browser/ui/browser_dialogs.h"
 #include "chromeos/chromeos_constants.h"
+#include "chromeos/chromeos_features.h"
 #include "chromeos/chromeos_switches.h"
+#include "chromeos/services/secure_channel/public/mojom/constants.mojom.h"
+#include "chromeos/services/secure_channel/secure_channel_service.h"
 #include "components/user_manager/user_manager.h"
 #include "services/service_manager/public/mojom/interface_provider_spec.mojom.h"
 #elif defined(OS_LINUX)
@@ -3476,6 +3479,16 @@ void ChromeContentBrowserClient::RegisterInProcessServices(
         base::Bind(&proxy_resolver::ProxyResolverService::CreateService);
     services->insert(
         std::make_pair(proxy_resolver::mojom::kProxyResolverServiceName, info));
+  }
+#endif
+
+#if defined(OS_CHROMEOS)
+  if (base::FeatureList::IsEnabled(chromeos::features::kMultiDeviceApi)) {
+    service_manager::EmbeddedServiceInfo info;
+    info.factory = base::Bind(
+        &chromeos::secure_channel::SecureChannelService::CreateService);
+    info.task_runner = base::ThreadTaskRunnerHandle::Get();
+    services->emplace(chromeos::secure_channel::mojom::kServiceName, info);
   }
 #endif
   g_browser_process->platform_part()->RegisterInProcessServices(services,
