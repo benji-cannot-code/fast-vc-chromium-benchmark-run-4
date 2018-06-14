ee1f49dab1a3a359c5e4b713b5ba6d6c9ee783f1
Anna Offenwanger <offenwanger@chromium.org>
2018-06-14 21:36:32 +0000

    Merging WebVR 1.1 RequestPresent into WebXR RequestSession
    In the WebVR 1.1 spec, the blink code was informed about devices then sent requests
    for pipelines to those devices via the mojo interfaces. In the WebXR spec, the
    decision about which device to use is left up to the browser, and not decided
    until the pipline request has been made. This CL moves the blink<->device interface
    pointers from being call parameters to being return parameters to better align
    with WebXR.
    As WebVR is being deprecated, as much of the old VR code as is possible
    has been cleaned out.
    
    Bug: 842042
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_layout_tests_slimming_paint_v2;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2;master.tryserver.chromium.linux:linux_vr
    Change-Id: I67da418829c05479eb3b2a6c5ef1633d8da92564
    Reviewed-on: https://chromium-review.googlesource.com/1080967
    Commit-Queue: Bill Orr <billorr@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Reviewed-by: Klaus Weidner <klausw@chromium.org>
    Reviewed-by: Bill Orr <billorr@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567430}

--- chrome/browser/android/vr/arcore_device/arcore_device.cc
+++ chrome/browser/android/vr/arcore_device/arcore_device.cc
@@ -132,11 +132,8 @@ void ARCoreDevice::OnARCoreGlThreadInitialized() {
   is_arcore_gl_thread_initialized_ = true;
 }
 
-void ARCoreDevice::RequestSession(
-    int render_process_id,
-    int render_frame_id,
-    bool has_user_activation,
-    mojom::VRDisplayHost::RequestSessionCallback callback) {
+void ARCoreDevice::RequestSession(const XRDeviceRuntimeSessionOptions& options,
+                                  VRDeviceRequestSessionCallback callback) {
   DCHECK(IsOnMainThread());
 
   // TODO(https://crbug.com/849568): Instead of splitting the initialization
@@ -147,7 +144,7 @@ void ARCoreDevice::RequestSession(
   // the arcore gl thread is initialized, the resolution of the request should
   // be delayed.
   if (!is_arcore_gl_thread_initialized_) {
-    std::move(callback).Run(false);
+    std::move(callback).Run(nullptr, nullptr);
     return;
   }
 
@@ -162,17 +159,20 @@ void ARCoreDevice::RequestSession(
   // appropriate permissions.
   // ARCore sessions require camera permission.
   RequestCameraPermission(
-      render_process_id, render_frame_id, has_user_activation,
+      options.render_process_id, options.render_frame_id,
+      options.has_user_activation,
       base::BindOnce(&ARCoreDevice::OnRequestCameraPermissionComplete,
                      GetWeakPtr(), std::move(preconditions_complete_callback)));
 }
 
 void ARCoreDevice::OnRequestCameraPermissionComplete(
-    mojom::VRDisplayHost::RequestSessionCallback callback,
+    base::OnceCallback<void(bool)> callback,
     bool success) {
   DCHECK(IsOnMainThread());
   DCHECK(is_arcore_gl_thread_initialized_);
 
+  // By this point ARCore has already been set up, so just return whether the
+  // permission request was a success.
   std::move(callback).Run(success);
 }
 
@@ -226,7 +226,7 @@ void ARCoreDevice::RequestCameraPermission(
     int render_process_id,
     int render_frame_id,
     bool has_user_activation,
-    mojom::VRDisplayHost::RequestSessionCallback callback) {
+    base::OnceCallback<void(bool)> callback) {
   DCHECK(IsOnMainThread());
   DCHECK(is_arcore_gl_thread_initialized_);
 
@@ -254,7 +254,7 @@ void ARCoreDevice::RequestCameraPermission(
 
 void ARCoreDevice::OnRequestCameraPermissionResult(
     content::WebContents* web_contents,
-    mojom::VRDisplayHost::RequestSessionCallback callback,
+    base::OnceCallback<void(bool)> callback,
     ContentSetting content_setting) {
   DCHECK(IsOnMainThread());
   DCHECK(is_arcore_gl_thread_initialized_);
@@ -294,13 +294,13 @@ void ARCoreDevice::OnRequestCameraPermissionResult(
 }
 
 void ARCoreDevice::OnRequestSessionPreconditionsComplete(
-    mojom::VRDisplayHost::RequestSessionCallback callback,
+    VRDeviceRequestSessionCallback callback,
     bool success) {
   DCHECK(IsOnMainThread());
   DCHECK(is_arcore_gl_thread_initialized_);
 
   if (!success) {
-    std::move(callback).Run(false);
+    std::move(callback).Run(nullptr, nullptr);
     return;
   }
 
@@ -317,13 +317,13 @@ void ARCoreDevice::OnRequestSessionPreconditionsComplete(
 }
 
 void ARCoreDevice::OnARCoreGlInitializationComplete(
-    mojom::VRDisplayHost::RequestSessionCallback callback,
+    VRDeviceRequestSessionCallback callback,
     bool success) {
   DCHECK(IsOnMainThread());
   DCHECK(is_arcore_gl_thread_initialized_);
 
   if (!success) {
-    std::move(callback).Run(false);
+    std::move(callback).Run(nullptr, nullptr);
     return;
   }
 
@@ -334,11 +334,20 @@ void ARCoreDevice::OnARCoreGlInitializationComplete(
         &ARCoreGl::Resume, arcore_gl_thread_->GetARCoreGl()->GetWeakPtr()));
   }
 
-  std::move(callback).Run(true);
+  // TODO(offenwanger) When the XRMagicWindowProvider or equivalent is returned
+  // here, clean out this dummy code.
+  auto connection = mojom::XRPresentationConnection::New();
+  mojom::VRSubmitFrameClientPtr submit_client;
+  connection->client_request = mojo::MakeRequest(&submit_client);
+  mojom::VRPresentationProviderPtr provider;
+  mojo::MakeRequest(&provider);
+  connection->provider = provider.PassInterface();
+  connection->transport_options = mojom::VRDisplayFrameTransportOptions::New();
+  std::move(callback).Run(std::move(connection), nullptr);
 }
 
 void ARCoreDevice::OnRequestAndroidCameraPermissionResult(
-    mojom::VRDisplayHost::RequestSessionCallback callback,
+    base::OnceCallback<void(bool)> callback,
     bool was_android_camera_permission_granted) {
   DCHECK(IsOnMainThread());
   DCHECK(is_arcore_gl_thread_initialized_);
--- third_party/blink/renderer/modules/xr/xr_frame_provider.h
+++ third_party/blink/renderer/modules/xr/xr_frame_provider.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class ScriptPromiseResolver;
 class XRDevice;
 class XRSession;
 class XRFrameTransport;
@@ -29,8 +28,11 @@ class XRFrameProvider final
   explicit XRFrameProvider(XRDevice*);
 
   XRSession* exclusive_session() const { return exclusive_session_; }
+  device::mojom::blink::VRSubmitFrameClientPtr GetSubmitFrameClient();
 
-  void BeginExclusiveSession(XRSession*, ScriptPromiseResolver*);
+  void BeginExclusiveSession(
+      XRSession* session,
+      device::mojom::blink::XRPresentationConnectionPtr connection);
   void OnExclusiveSessionEnded();
 
   void RequestFrame(XRSession*);
@@ -58,9 +60,6 @@ class XRFrameProvider final
   void ScheduleExclusiveFrame();
   void ScheduleNonExclusiveFrame();
 
-  void OnPresentComplete(
-      bool success,
-      device::mojom::blink::VRDisplayFrameTransportOptionsPtr);
   void OnPresentationProviderConnectionError();
   void ProcessScheduledFrame(
       device::mojom::blink::VRMagicWindowFrameDataPtr frame_data,
@@ -68,15 +67,7 @@ class XRFrameProvider final
 
   const Member<XRDevice> device_;
   Member<XRSession> exclusive_session_;
-  Member<ScriptPromiseResolver> pending_exclusive_session_resolver_;
   Member<XRFrameTransport> frame_transport_;
-
-  // Careful, exclusive_session_ being true does not mean it's OK to send
-  // frames. The initialization handshake may not be complete yet. This boolean
-  // starts out false at the start of a session, becomes true after a
-  // successful OnPresentComplete(), and remains true for the lifetime of the
-  // exclusive session.
-  bool exclusive_session_can_send_frames_ = false;
 
   // Non-exclusive Sessions which have requested a frame update.
   HeapVector<Member<XRSession>> requesting_sessions_;
