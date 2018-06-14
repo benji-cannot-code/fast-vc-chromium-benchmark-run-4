bfee8380e73a1e7af22754d096e01c148228b69e
sczs@chromium.org <sczs@chromium.org>
2018-06-14 04:24:54 +0000

    [ios] Bookmarks UI Refresh polishing.
    - Hides the toolbar border for Edit Folder and Edit Bookmark.
    - Removes the Done Button on the Folder Picker.
    - Adds separators to BookmarksHomeTableView.
    - Removes white background from FolderPicker.
    
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I26e58dcdb1c65761cf10c7a6956778b02df0b16a
    Reviewed-on: https://chromium-review.googlesource.com/1098516
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: edchin <edchin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567132}

--- ios/chrome/browser/ui/bookmarks/bookmark_folder_editor_view_controller.mm
+++ ios/chrome/browser/ui/bookmarks/bookmark_folder_editor_view_controller.mm
@@ -489,6 +489,8 @@ folderEditorWithBookmarkModel:(bookmarks::BookmarkModel*)bookmarkModel
 
   if (experimental_flags::IsBookmarksUIRebootEnabled()) {
     deleteButton.tintColor = [UIColor redColor];
+    [self.navigationController.toolbar setShadowImage:[UIImage new]
+                                   forToolbarPosition:UIBarPositionAny];
     [self setToolbarItems:@[ spaceButton, deleteButton, spaceButton ]
                  animated:NO];
   } else {
