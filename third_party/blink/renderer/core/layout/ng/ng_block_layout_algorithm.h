c4d14071859d8203ca06121cff71bce7ea260f02
Koji Ishii <kojii@chromium.org>
2018-06-14 02:16:58 +0000

    [LayoutNG] Fix block-level baseline for flipped-lines (vertical-lr)
    Following CL:1074989 that fixed inline-level baseline for
    flipped-lines (vertical-lr), this patch fixes block-level
    baselines.
    
    When NGBlockLayoutAlgorithm propagates baselines from its
    child line boxes, line boxes and font metrics are in the
    [line-relative] coordinates. This needs to be converted to
    [flow-relative] coordinates when adding to box fragments.
    
    [flow-relative]: https://drafts.csswg.org/css-writing-modes-3/#logical-directions
    [line-relative]: https://drafts.csswg.org/css-writing-modes-3/#line-directions
    
    Bug: 636993
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng;master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: If4ddc2374cc0351120ae672614a32f38bb1f3bc8
    Reviewed-on: https://chromium-review.googlesource.com/1087192
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567092}

--- third_party/blink/renderer/core/layout/ng/ng_block_layout_algorithm.h
+++ third_party/blink/renderer/core/layout/ng/ng_block_layout_algorithm.h
@@ -19,6 +19,7 @@ namespace blink {
 class NGConstraintSpace;
 class NGFragment;
 class NGLayoutResult;
+class NGPhysicalLineBoxFragment;
 
 // This struct is used for communicating to a child the position of the previous
 // inflow child. This will be used to calculate the position of the next child.
@@ -184,6 +185,13 @@ class CORE_EXPORT NGBlockLayoutAlgorithm
   bool AddBaseline(const NGBaselineRequest&,
                    const NGPhysicalFragment*,
                    LayoutUnit child_offset);
+
+  // Compute the baseline offset of a line box from the content box.
+  // Line boxes are in line-relative coordinates. This function returns the
+  // offset in flow-relative coordinates.
+  LayoutUnit ComputeLineBoxBaselineOffset(
+      const NGBaselineRequest&,
+      const NGPhysicalLineBoxFragment&) const;
 
   // If still unresolved, resolve the fragment's BFC offset.
   //
