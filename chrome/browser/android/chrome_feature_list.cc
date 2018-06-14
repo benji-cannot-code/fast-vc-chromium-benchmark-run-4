ab231659d87d20bb997780ef90473d3c5f6dae50
Michael Thiessen <mthiesse@chromium.org>
2018-06-14 01:04:11 +0000

    Remove feature flag for Daydream Home icon.
    The icon has been on by default since M67, and the updating of the
    component is causing bizarre crashes.
    
    Bug: 851983
    Change-Id: I41267744e87861fdb889ec4f6bdca67327539a25
    Reviewed-on: https://chromium-review.googlesource.com/1099875
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567073}

--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -130,7 +130,6 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &kVrBrowsingFeedback,
     &kVrBrowsingInCustomTab,
     &kVrBrowsingNativeAndroidUi,
-    &kVrIconInDaydreamHome,
     &payments::features::kWebPaymentsMethodSectionOrderV2,
     &payments::features::kWebPaymentsModifiers,
     &payments::features::kWebPaymentsSingleAppUiSkip,
@@ -371,9 +370,6 @@ const base::Feature kVrBrowsingNativeAndroidUi{
 
 const base::Feature kVrBrowsingTabsView{"VrBrowsingTabsView",
                                         base::FEATURE_DISABLED_BY_DEFAULT};
-
-const base::Feature kVrIconInDaydreamHome{"VrIconInDaydreamHome",
-                                          base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kWebVrAutopresentFromIntent{
     "WebVrAutopresentFromIntent", base::FEATURE_ENABLED_BY_DEFAULT};
