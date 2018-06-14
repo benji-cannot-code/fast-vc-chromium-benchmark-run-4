b4234bd234584b4e815d9196565247b9800cea28
Wez <wez@chromium.org>
2018-06-14 19:52:21 +0000

    Make base::ScopedZxHandle a zx::handle.
    - Add the ScopedZxHandle APIs to the base zx::handle.
    - Add missing includes.
    
    This allows ScopedZxHandle to be removed incrementally from call-sites.
    
    Bug: 852541
    Change-Id: Idc452a450ce2bbe1266e9e0ee266115e2ea38f97
    Reviewed-on: https://chromium-review.googlesource.com/1100132
    Commit-Queue: Wez <wez@chromium.org>
    Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567382}

--- base/test/launcher/test_launcher.cc
+++ base/test/launcher/test_launcher.cc
@@ -71,6 +71,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <zircon/syscalls.h>
 #include <zircon/syscalls/object.h>
 #include "base/fuchsia/default_job.h"
+#include "base/fuchsia/fuchsia_logging.h"
 #endif
 
 namespace base {
@@ -323,7 +324,7 @@ int LaunchChildTestProcessWithOptions(const CommandLine& command_line,
 
   ScopedZxHandle job_handle;
   zx_status_t result = zx_job_create(GetDefaultJob(), 0, job_handle.receive());
-  CHECK_EQ(ZX_OK, result) << "zx_job_create: " << zx_status_get_string(result);
+  ZX_CHECK(ZX_OK == result, result) << "zx_job_create";
   new_options.job_handle = job_handle.get();
 #endif  // defined(OS_FUCHSIA)
 
--- content/browser/browser_main_loop.cc
+++ content/browser/browser_main_loop.cc
@@ -207,6 +207,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <zircon/syscalls.h>
 
 #include "base/fuchsia/default_job.h"
+#include "base/fuchsia/fuchsia_logging.h"
 #endif  // defined(OS_FUCHSIA)
 
 #if defined(OS_POSIX) && !defined(OS_MACOSX)
@@ -397,8 +398,7 @@ constexpr base::TimeDelta kSwapMetricsInterval =
 void InitDefaultJob() {
   base::ScopedZxHandle handle;
   zx_status_t result = zx_job_create(zx_job_default(), 0, handle.receive());
-  CHECK_EQ(ZX_OK, result) << "zx_job_create(job): "
-                          << zx_status_get_string(result);
+  ZX_CHECK(ZX_OK == result, result) << "zx_job_create";
   base::SetDefaultJob(std::move(handle));
 }
 #endif  // defined(OS_FUCHSIA)
