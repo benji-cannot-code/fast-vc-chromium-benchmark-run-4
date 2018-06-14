f592d764410fbc0794e89d1bd2ca30eface6ff18
Istiaque Ahmed <lazyboy@chromium.org>
2018-06-14 23:51:17 +0000

    Explicitly mark LazyBackgroundTaskQueue non-copyable.
    We neither copy LazyBackgroundTaskQueue instances, nor we need to.
    Delete its copy operators.
    
    Bug: None
    Change-Id: I759fe181e9c1d561769922b299043463fe7e5cd0
    Reviewed-on: https://chromium-review.googlesource.com/1036613
    Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567487}

--- extensions/browser/lazy_background_task_queue.h
+++ extensions/browser/lazy_background_task_queue.h
@@ -123,6 +123,8 @@ class LazyBackgroundTaskQueue : public KeyedService,
 
   ScopedObserver<ExtensionRegistry, ExtensionRegistryObserver>
       extension_registry_observer_;
+
+  DISALLOW_COPY_AND_ASSIGN(LazyBackgroundTaskQueue);
 };
 
 }  // namespace extensions
