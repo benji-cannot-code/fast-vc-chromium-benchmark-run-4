116ea039f8476b1a8f038f5687390be7fffc4698
dpapad <dpapad@chromium.org>
2018-06-14 22:02:40 +0000

    WebUI Polymer 2: Fix a few Bookmarks tests.
    Specifically fixing the following tests:
    MaterialBookmarksActionsTest.All
    MaterialBookmarksAppTest.All
    MaterialBookmarksCommandManagerTest.All
    MaterialBookmarksFolderNodeTest.All
    MaterialBookmarksItemTest.All
    MaterialBookmarksListTest.All
    MaterialBookmarksPolicyTest.All
    MaterialBookmarksReducersTest.All
    MaterialBookmarksToastManagerTest.All
    MaterialBookmarksToolbarTest.All
    MaterialBookmarksUtilTest.All
    
    Remaining bookmarks tests will be fixed in a follow up.
    
    Bug: 738611
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I62bd58f80208e024953b7f44a110a28db07d7cb4
    Reviewed-on: https://chromium-review.googlesource.com/1100108
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567437}

--- chrome/browser/resources/md_bookmarks/command_manager.js
+++ chrome/browser/resources/md_bookmarks/command_manager.js
@@ -24,9 +24,7 @@ cr.define('bookmarks', function() {
       },
 
       /** @private {Set<string>} */
-      menuIds_: {
-        type: Object,
-      },
+      menuIds_: Object,
 
       /** @private */
       hasAnySublabel_: {
@@ -41,7 +39,10 @@ cr.define('bookmarks', function() {
        * or elsewhere in the UI.
        * @private {MenuSource}
        */
-      menuSource_: MenuSource.NONE,
+      menuSource_: {
+        type: Number,
+        value: MenuSource.NONE,
+      },
 
       /** @private */
       globalCanEdit_: Boolean,
@@ -703,7 +704,7 @@ cr.define('bookmarks', function() {
      * @private
      */
     computeHasAnySublabel_: function() {
-      if (!this.menuIds_)
+      if (this.menuIds_ == undefined || this.menuCommands_ == undefined)
         return false;
 
       return this.menuCommands_.some(
--- chrome/test/data/webui/md_bookmarks/test_util.js
+++ chrome/test/data/webui/md_bookmarks/test_util.js
@@ -114,6 +114,7 @@ function customClick(element, config, eventName) {
   const props = {
     bubbles: true,
     cancelable: true,
+    composed: true,
     button: 0,
     buttons: 1,
     shiftKey: false,
