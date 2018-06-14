fdb13ffdaa472f1dd20758589091531cdb827005
Maks Orlovich <morlovich@chromium.org>
2018-06-14 23:21:36 +0000

    Add a standardized way of making SharedURLLoaderFactory's multi-thread aware.
    Use it to implement StoragePartitionImpl::URLLoaderFactoryForBrowserProcess::Clone()
    
    Bug: 850089
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Change-Id: I53798149efc69bd2566175bb248c462a2e5f4486
    Reviewed-on: https://chromium-review.googlesource.com/1097214
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Helen Li <xunjieli@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567471}

--- content/browser/storage_partition_impl.cc
+++ content/browser/storage_partition_impl.cc
@@ -54,6 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/cookie_manager.h"
 #include "services/network/network_context.h"
 #include "services/network/network_service.h"
+#include "services/network/public/cpp/cross_thread_shared_url_loader_factory_info.h"
 #include "services/network/public/cpp/features.h"
 #include "services/service_manager/public/cpp/connector.h"
 #include "storage/browser/blob/blob_registry_impl.h"
@@ -297,9 +298,9 @@ class StoragePartitionImpl::URLLoaderFactoryForBrowserProcess
 
   // SharedURLLoaderFactory implementation:
   std::unique_ptr<network::SharedURLLoaderFactoryInfo> Clone() override {
-    NOTREACHED() << "This isn't supported. If you need a SharedURLLoaderFactory"
-                    " on the IO thread, get it from URLLoaderFactoryGetter.";
-    return nullptr;
+    DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+    return std::make_unique<network::CrossThreadSharedURLLoaderFactoryInfo>(
+        this);
   }
 
   void Shutdown() { storage_partition_ = nullptr; }
