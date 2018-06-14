3a9b039c28d6e05fc0e058bbf7adf2183b1639b3
Takumi Fujimoto <takumif@chromium.org>
2018-06-14 03:23:30 +0000

    [Harmony Cast Dialog] Implement the no devices view
    This is the view that is shown when there are no devices found.
    For the first three seconds, it will show a throbber. After that, it
    will show a help icon that links to a help center article.
    
    Whenever the dialog receives a model update containing sinks, the "no
    devices" view is replaced with the sink list view.
    
    Video:
    https://drive.google.com/open?id=18zr2x3B30HsRc0j9hYry8_dZWXAlW5e_
    
    Bug: 846164
    Change-Id: I203197b5300c604a062deb303c18d5c98631c761
    Reviewed-on: https://chromium-review.googlesource.com/1089580
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567113}

--- chrome/browser/ui/views/media_router/media_router_dialog_controller_views.cc
+++ chrome/browser/ui/views/media_router/media_router_dialog_controller_views.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/media_router/media_router_dialog_controller_views.h"
 
+#include <memory>
+
 #include "base/feature_list.h"
 #include "build/build_config.h"
 #include "chrome/browser/ui/browser_finder.h"
@@ -91,7 +93,8 @@ void MediaRouterDialogControllerViews::CreateMediaRouterDialog() {
     return;
   views::View* action_view = browser_actions->GetViewForId(
       ComponentToolbarActionsFactory::kMediaRouterActionId);
-  CastDialogView::ShowDialog(action_view, ui_.get());
+  CastDialogView::ShowDialog(action_view, ui_.get(),
+                             chrome::FindBrowserWithWebContents(initiator()));
   CastDialogView::GetCurrentDialogWidget()->AddObserver(this);
 }
 
