2a025f8b76c5054df010947b685ee0e7ea5b89ca
Christian Dullweber <dullweber@chromium.org>
2018-06-14 15:48:07 +0000

    Pass LocalDataContainer as unique_ptr instead of raw ptr
    LocalDataContainer is owned by a unique_ptr in CookiesTreeModel but
    it is created from the outside and passed as a raw pointer.
    Fix this by directly creating it as a unique_ptr.
    
    Change-Id: I11dadcd91359f2b07f8d021fa3daa62dde80711f
    Reviewed-on: https://chromium-review.googlesource.com/1100832
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567286}

--- chrome/browser/android/preferences/website_preference_bridge.cc
+++ chrome/browser/android/preferences/website_preference_bridge.cc
@@ -640,7 +640,7 @@ class SiteDataDeleteHelper : public CookiesTreeModel::Observer {
         nullptr);
 
     cookies_tree_model_ = std::make_unique<CookiesTreeModel>(
-        container.release(), profile_->GetExtensionSpecialStoragePolicy());
+        std::move(container), profile_->GetExtensionSpecialStoragePolicy());
     cookies_tree_model_->AddCookiesTreeObserver(this);
   }
 
