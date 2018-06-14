b7c4434c2930a8654583f3a90fa809e279a31a35
Allen Vicencio <allenvic@chromium.org>
2018-06-14 01:43:25 +0000

    Implement share discovery for SmbHandler and SmbService
    Implements share discovery for SmbHandler and SmbService using
    SmbShareFinder
    
    Bug: 757625
    Change-Id: Ib3baa0b307ab95c17f38b47339f264231831752e
    Reviewed-on: https://chromium-review.googlesource.com/1089855
    Commit-Queue: Allen Vicencio <allenvic@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: Dave Schuyler <dschuyler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567083}

--- chrome/browser/chromeos/smb_client/smb_service.cc
+++ chrome/browser/chromeos/smb_client/smb_service.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task_scheduler/post_task.h"
 #include "chrome/browser/chromeos/file_system_provider/provided_file_system_info.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
-#include "chrome/browser/chromeos/smb_client/smb_errors.h"
+#include "chrome/browser/chromeos/smb_client/discovery/mdns_host_locator.h"
 #include "chrome/browser/chromeos/smb_client/smb_file_system.h"
 #include "chrome/browser/chromeos/smb_client/smb_file_system_id.h"
 #include "chrome/browser/chromeos/smb_client/smb_provider.h"
@@ -70,6 +70,10 @@ void SmbService::Mount(const file_system_provider::MountOptions& options,
   }
 
   CallMount(options, share_path, username, password, std::move(callback));
+}
+
+void SmbService::GatherSharesInNetwork(GatherSharesResponse callback) {
+  share_finder_->GatherSharesInNetwork(std::move(callback));
 }
 
 void SmbService::InitTempFileManagerAndMount(
@@ -240,6 +244,9 @@ void SmbService::OnSetupKerberosResponse(bool success) {
 }
 
 void SmbService::CompleteSetup() {
+  share_finder_ = std::make_unique<SmbShareFinder>(GetSmbProviderClient());
+  RegisterHostLocators();
+
   GetProviderService()->RegisterProvider(std::make_unique<SmbProvider>(
       base::BindRepeating(&SmbService::Unmount, base::Unretained(this))));
   RestoreMounts();
@@ -257,6 +264,14 @@ void SmbService::RecordMountCount() const {
       GetProviderService()->GetProvidedFileSystemInfoList(provider_id_);
   UMA_HISTOGRAM_COUNTS_100("NativeSmbFileShare.MountCount",
                            file_systems.size());
+}
+
+void SmbService::RegisterHostLocators() {
+  SetUpMdnsHostLocator();
+}
+
+void SmbService::SetUpMdnsHostLocator() {
+  share_finder_->RegisterHostLocator(std::make_unique<MDnsHostLocator>());
 }
 
 }  // namespace smb_client
--- chrome/browser/chromeos/smb_client/smb_service.h
+++ chrome/browser/chromeos/smb_client/smb_service.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/file_system_provider/provider_interface.h"
 #include "chrome/browser/chromeos/file_system_provider/service.h"
 #include "chrome/browser/chromeos/smb_client/smb_errors.h"
+#include "chrome/browser/chromeos/smb_client/smb_share_finder.h"
 #include "chrome/browser/chromeos/smb_client/temp_file_manager.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chromeos/dbus/smb_provider_client.h"
@@ -63,6 +64,11 @@ class SmbService : public KeyedService,
                        const base::FilePath& share_path,
                        smbprovider::ErrorType error,
                        int32_t mount_id);
+
+  // Gathers the hosts in the network using |share_finder_| and gets the shares
+  // for each of the hosts found. |callback| will be called once per host and
+  // will contain the URLs to the shares found.
+  void GatherSharesInNetwork(GatherSharesResponse callback);
 
  private:
   // Initializes temp_file_manager_.
@@ -120,9 +126,16 @@ class SmbService : public KeyedService,
 
   void RecordMountCount() const;
 
+  // Registers host locators for |share_finder_|.
+  void RegisterHostLocators();
+
+  // Set up Multicast DNS host locator.
+  void SetUpMdnsHostLocator();
+
   const ProviderId provider_id_;
   Profile* profile_;
   std::unique_ptr<TempFileManager> temp_file_manager_;
+  std::unique_ptr<SmbShareFinder> share_finder_;
 
   DISALLOW_COPY_AND_ASSIGN(SmbService);
 };
