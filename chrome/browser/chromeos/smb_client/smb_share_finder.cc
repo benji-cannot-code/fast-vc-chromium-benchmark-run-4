e5dd0b13c5da3ba3efb88a69db7bdcc46111ee13
Allen Vicencio <allenvic@chromium.org>
2018-06-14 00:09:40 +0000

    Implement SmbShareFinder functions
    Implements the functions of SmbShareFinder and adds tests
    
    Bug: 757625
    Change-Id: I072390d39ff060e36ce748dcf28ed98a090b7096
    Reviewed-on: https://chromium-review.googlesource.com/1089852
    Commit-Queue: Allen Vicencio <allenvic@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567056}

--- chrome/browser/chromeos/smb_client/smb_share_finder.cc
+++ chrome/browser/chromeos/smb_client/smb_share_finder.cc
@@ -5,21 +5,50 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/smb_client/smb_share_finder.h"
 
+#include <vector>
+
+#include "base/bind.h"
+#include "chrome/browser/chromeos/smb_client/discovery/mdns_host_locator.h"
+#include "chrome/browser/chromeos/smb_client/smb_constants.h"
+
 namespace chromeos {
 namespace smb_client {
 
+SmbShareFinder::SmbShareFinder(SmbProviderClient* client) : client_(client) {}
+SmbShareFinder::~SmbShareFinder() = default;
+
 void SmbShareFinder::GatherSharesInNetwork(GatherSharesResponse callback) {
-  NOTREACHED();
+  scanner_.FindHostsInNetwork(base::BindOnce(&SmbShareFinder::OnHostsFound,
+                                             AsWeakPtr(), std::move(callback)));
 }
 
 void SmbShareFinder::RegisterHostLocator(std::unique_ptr<HostLocator> locator) {
-  NOTREACHED();
+  scanner_.RegisterHostLocator(std::move(locator));
 }
 
 void SmbShareFinder::OnHostsFound(GatherSharesResponse callback,
                                   bool success,
                                   const HostMap& hosts) {
-  NOTREACHED();
+  if (!success) {
+    LOG(ERROR) << "SmbShareFinder failed to find hosts";
+    callback.Run(std::vector<SmbUrl>());
+    return;
+  }
+
+  if (hosts.empty()) {
+    callback.Run(std::vector<SmbUrl>());
+    return;
+  }
+
+  for (const auto& host : hosts) {
+    const std::string& host_name = host.first;
+    const std::string& resolved_address = host.second;
+    const base::FilePath server_url(kSmbSchemePrefix + resolved_address);
+
+    client_->GetShares(
+        server_url, base::BindOnce(&SmbShareFinder::OnSharesFound, AsWeakPtr(),
+                                   host_name, callback));
+  }
 }
 
 void SmbShareFinder::OnSharesFound(
@@ -27,7 +56,23 @@ void SmbShareFinder::OnSharesFound(
     GatherSharesResponse callback,
     smbprovider::ErrorType error,
     const smbprovider::DirectoryEntryListProto& entries) {
-  NOTREACHED();
+  if (error != smbprovider::ErrorType::ERROR_OK) {
+    LOG(ERROR) << "Error finding shares: " << error;
+    callback.Run(std::vector<SmbUrl>());
+    return;
+  }
+
+  std::vector<SmbUrl> shares;
+  for (const smbprovider::DirectoryEntryProto& entry : entries.entries()) {
+    SmbUrl url(kSmbSchemePrefix + host_name + "/" + entry.name());
+    if (url.IsValid()) {
+      shares.push_back(std::move(url));
+    } else {
+      LOG(WARNING) << "URL found is not valid";
+    }
+  }
+
+  callback.Run(shares);
 }
 
 }  // namespace smb_client
