c78677315f67f97a4454165b1c64be67fd4c3003
Sylvain Defresne <sdefresne@chromium.org>
2018-06-14 09:57:15 +0000

    Remove unused fields of ChromeBrowserStateIOData
    Those fields are never accessed after initialization (except for
    destruction). Remove the fields also track preferences that are
    unused on iOS (they are related to managed accounts which is not
    supported on iOS).
    
    Bug: none
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I13f86b143d34fd0ba07d4372ff0e37133e0e2213
    Reviewed-on: https://chromium-review.googlesource.com/1097334
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567211}

--- ios/chrome/browser/browser_state/chrome_browser_state_io_data.mm
+++ ios/chrome/browser/browser_state/chrome_browser_state_io_data.mm
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service.h"
 #include "components/proxy_config/ios/proxy_service_factory.h"
 #include "components/signin/core/browser/signin_pref_names.h"
-#include "components/sync/base/pref_names.h"
 #include "ios/chrome/browser/application_context.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #include "ios/chrome/browser/chrome_url_constants.h"
@@ -127,12 +126,6 @@ void ChromeBrowserStateIOData::InitializeOnUIThread(
     google_services_user_account_id_.Init(prefs::kGoogleServicesUserAccountId,
                                           pref_service);
     google_services_user_account_id_.MoveToThread(io_task_runner);
-
-    sync_disabled_.Init(syncer::prefs::kSyncManaged, pref_service);
-    sync_disabled_.MoveToThread(io_task_runner);
-
-    signin_allowed_.Init(prefs::kSigninAllowed, pref_service);
-    signin_allowed_.MoveToThread(io_task_runner);
   }
 }
 
@@ -461,8 +454,6 @@ void ChromeBrowserStateIOData::ShutdownOnUIThread(
   enable_referrers_.Destroy();
   enable_do_not_track_.Destroy();
   enable_metrics_.Destroy();
-  sync_disabled_.Destroy();
-  signin_allowed_.Destroy();
   if (chrome_http_user_agent_settings_)
     chrome_http_user_agent_settings_->CleanupOnUIThread();
 
