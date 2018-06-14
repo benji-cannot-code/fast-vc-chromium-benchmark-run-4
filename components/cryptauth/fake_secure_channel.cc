d381230e624b3fb70adf6b5db4ce014f20bc674a
Kyle Horimoto <khorimoto@google.com>
2018-06-14 23:26:23 +0000

    [CrOS MultiDevice] Create SecureChannelDisconnector.
    This class disconnects cryptauth::SecureChannel objects, which have an
    asynchronous disconnection flow. It is an error to delete
    cryptauth::SecureChannel objects before they are fully disconnected, as
    this can cause the underlying connection to remain open.
    
    Bug: 824568, 752273
    Change-Id: I42ebf3814f27f9f45c6d90173cb3cb8513bc800b
    Reviewed-on: https://chromium-review.googlesource.com/1101515
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567474}

--- components/cryptauth/fake_secure_channel.cc
+++ components/cryptauth/fake_secure_channel.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/cryptauth/fake_secure_channel.h"
 
+#include <utility>
+
 #include "base/logging.h"
 
 namespace cryptauth {
@@ -16,7 +18,10 @@ FakeSecureChannel::SentMessage::SentMessage(const std::string& feature,
 FakeSecureChannel::FakeSecureChannel(std::unique_ptr<Connection> connection)
     : SecureChannel(std::move(connection)) {}
 
-FakeSecureChannel::~FakeSecureChannel() {}
+FakeSecureChannel::~FakeSecureChannel() {
+  if (destructor_callback_)
+    std::move(destructor_callback_).Run();
+}
 
 void FakeSecureChannel::ChangeStatus(const Status& new_status) {
   Status old_status = status_;
