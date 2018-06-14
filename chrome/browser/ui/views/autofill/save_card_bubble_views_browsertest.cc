adebd4e8c1f023c2b8dd74c53760a1ac4a257c3a
Bret Sepulveda <bsep@chromium.org>
2018-06-14 22:44:57 +0000

    Skip payment tests that rely on pre-Harmony behavior when Refresh is on.
    Refresh implies that Harmony is enabled, so these tests began failing.
    They can be safely skipped, as there are equivalent tests for Harmony.
    
    Bug: 846410
    Change-Id: I6c6211d4ea99b6df0a4ba9620217574760ba723c
    Reviewed-on: https://chromium-review.googlesource.com/1100390
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567450}

--- chrome/browser/ui/views/autofill/save_card_bubble_views_browsertest.cc
+++ chrome/browser/ui/views/autofill/save_card_bubble_views_browsertest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/autofill_experiments.h"
 #include "content/public/test/browser_test_utils.h"
 #include "net/url_request/test_url_fetcher_factory.h"
+#include "ui/base/material_design/material_design_controller.h"
 #include "ui/base/ui_base_features.h"
 #include "ui/views/bubble/bubble_frame_view.h"
 #include "ui/views/controls/button/label_button.h"
@@ -103,6 +104,10 @@ IN_PROC_BROWSER_TEST_F(SaveCardBubbleViewsFullFormBrowserTest,
 IN_PROC_BROWSER_TEST_F(
     SaveCardBubbleViewsFullFormBrowserTest,
     Local_ClickingNoThanksClosesBubbleIfSecondaryUiMdExpOff) {
+  // Pre-Harmony tests are not applicable to Refresh.
+  if (ui::MaterialDesignController::IsRefreshUi())
+    return;
+
   // Disable the SecondaryUiMd experiment.
   scoped_feature_list_.InitAndDisableFeature(features::kSecondaryUiMd);
 
@@ -230,6 +235,10 @@ IN_PROC_BROWSER_TEST_F(SaveCardBubbleViewsFullFormBrowserTest,
 IN_PROC_BROWSER_TEST_F(
     SaveCardBubbleViewsFullFormBrowserTest,
     Upload_ClickingNoThanksClosesBubbleIfSecondaryUiMdExpOff) {
+  // Pre-Harmony tests are not applicable to Refresh.
+  if (ui::MaterialDesignController::IsRefreshUi())
+    return;
+
   // Disable the SecondaryUiMd experiment.
   scoped_feature_list_.InitAndDisableFeature(features::kSecondaryUiMd);
 
