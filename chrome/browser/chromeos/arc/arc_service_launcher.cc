01686917a1ec0848db58500b64bfce748b5d4d8e
Yuichiro Hanada <yhanada@chromium.org>
2018-06-14 08:57:45 +0000

    Separate mojo interface from ArcInputMethodManagerService.
    To make writing unit-tests easy, this CL separates mojo interfaces
    from ArcInputMethodManager.
    This CL does the following:
    - Rename ArcInputMethodManagerBridge to ArcInputMethodManagerService.
    - Introduce new ArcInputMethodManagerBridge interface for injection
      point.
    
    Bug: 845079
    Test: unit tests
    Change-Id: I2e16b0bb9eba2c6bf9717813dff1640bd1f54b1d
    Reviewed-on: https://chromium-review.googlesource.com/1098738
    Commit-Queue: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567198}

--- chrome/browser/chromeos/arc/arc_service_launcher.cc
+++ chrome/browser/chromeos/arc/arc_service_launcher.cc
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/arc/enterprise/arc_enterprise_reporting_service.h"
 #include "chrome/browser/chromeos/arc/fileapi/arc_file_system_bridge.h"
 #include "chrome/browser/chromeos/arc/fileapi/arc_file_system_mounter.h"
-#include "chrome/browser/chromeos/arc/input_method_manager/arc_input_method_manager_bridge.h"
+#include "chrome/browser/chromeos/arc/input_method_manager/arc_input_method_manager_service.h"
 #include "chrome/browser/chromeos/arc/intent_helper/arc_settings_service.h"
 #include "chrome/browser/chromeos/arc/kiosk/arc_kiosk_bridge.h"
 #include "chrome/browser/chromeos/arc/notification/arc_boot_error_notification.h"
@@ -153,7 +153,7 @@ void ArcServiceLauncher::OnPrimaryUserProfilePrepared(Profile* profile) {
   ArcFileSystemBridge::GetForBrowserContext(profile);
   ArcFileSystemMounter::GetForBrowserContext(profile);
   ArcImeService::GetForBrowserContext(profile);
-  ArcInputMethodManagerBridge::GetForBrowserContext(profile);
+  ArcInputMethodManagerService::GetForBrowserContext(profile);
   ArcIntentHelperBridge::GetForBrowserContext(profile);
   ArcKioskBridge::GetForBrowserContext(profile);
   ArcLockScreenBridge::GetForBrowserContext(profile);
