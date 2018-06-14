1f648d4a1cf8dd54b721435a6ca0e177db83e3d7
Tim Schumann <tschumann@chromium.org>
2018-06-14 14:10:33 +0000

    Properly distinguish between regular and priority preferences in sync.
    This fixes a bug that was introduced with 13ef1e88dccf70d608587e641d472a040f371ba8
    While the first patch properly distinguished between the two when processing
    local changes it failed doing so when processing remote changes (either
    incremental or initial sync changes).
    
    The issue surfaced when a preference got moved between regular and priority
    sync preferences. In that scenario, sync has values for that pref in each
    flavour and one can overwrite the other.
    
    Bug: 849523, 840332
    Change-Id: Ib458c5863bc036dbb2a735ccacd52c6df2c118bb
    Reviewed-on: https://chromium-review.googlesource.com/1099381
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Tim Schumann <tschumann@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567255}

--- components/sync_preferences/unknown_user_pref_accessor.h
+++ components/sync_preferences/unknown_user_pref_accessor.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/compiler_specific.h"
 #include "base/macros.h"
+#include "components/sync/base/model_type.h"
 
 class PersistentPrefStore;
 class PrefService;
@@ -60,7 +61,8 @@ class UnknownUserPrefAccessor {
 
   // Computes the state of a preference with name |pref_name| which gives
   // information about whether it's registered and the locally persisted value.
-  PreferenceState GetPreferenceState(const std::string& pref_name) const;
+  PreferenceState GetPreferenceState(syncer::ModelType type,
+                                     const std::string& pref_name) const;
 
   // Removes the value of the preference |pref_name| from the user prefstore.
   // Must not be called for preferences having RegistrationState::kUnknown.
@@ -87,7 +89,8 @@ class UnknownUserPrefAccessor {
   int GetNumberOfSyncingUnknownPrefs() const;
 
  private:
-  RegistrationState GetRegistrationState(const std::string& pref_name) const;
+  RegistrationState GetRegistrationState(syncer::ModelType type,
+                                         const std::string& pref_name) const;
 
   std::set<std::string> synced_unknown_prefs_;
   PrefService* const pref_service_;
