fb7d4217a53a3bb6338b9f34a56ad77c38ab71ef
codeimpl <codeimpl@gmail.com>
2018-06-14 17:55:44 +0000

    Rename XRFrameOfReferenceType enums
    The values of the enum was updated in the spec[1].
    
    [1] https://github.com/immersive-web/webxr/pull/370
    
    Bug: 852520
    Change-Id: I916943bee355936e7430aa4e36d0eeb6b76607d5
    Reviewed-on: https://chromium-review.googlesource.com/1101118
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Byoungkwon Ko <codeimpl@gmail.com>
    Cr-Commit-Position: refs/heads/master@{#567340}

--- chrome/test/data/vr/e2e_test_files/resources/webxr_boilerplate.js
+++ chrome/test/data/vr/e2e_test_files/resources/webxr_boilerplate.js
@@ -55,7 +55,7 @@ function onSessionStarted(session) {
   }
 
   session.baseLayer = new XRWebGLLayer(session, gl);
-  session.requestFrameOfReference('eyeLevel').then( (frameOfRef) => {
+  session.requestFrameOfReference('eye-level').then( (frameOfRef) => {
     if (session.exclusive) {
       exclusiveFrameOfRef = frameOfRef;
     } else {
