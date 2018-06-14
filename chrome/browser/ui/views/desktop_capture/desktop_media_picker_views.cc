bbe75de6d9e735f9dd496e555f7f57100c63bb61
Robert Liao <robliao@chromium.org>
2018-06-14 17:30:55 +0000

    Revert "Add Views DesktopMediaPicker into the Overall Views Build"
    This reverts commit 170f8510730d63a686f53944c62d750eadc532f8.
    
    Reason for revert: Causing some unit_test failures on Mac 10.10
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Mac10.10%20Tests
    starting around 33081
    
    
    Original change's description:
    > Add Views DesktopMediaPicker into the Overall Views Build
    > 
    > This moves the code into the toolkit_views source list and gates Aura
    > specific code behind build flags.
    > 
    > BUG=726005,657883
    > 
    > Change-Id: I5e64ee0b16f2bfcb03da3bbc7c61486a786ee88d
    > Reviewed-on: https://chromium-review.googlesource.com/1097843
    > Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    > Commit-Queue: Robert Liao <robliao@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#566809}
    
    TBR=ellyjones@chromium.org,robliao@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 726005, 657883
    Change-Id: I4b8c6d38c1df4453ae1ee404f88a57046a908f35
    Reviewed-on: https://chromium-review.googlesource.com/1101098
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567327}

--- chrome/browser/ui/views/desktop_capture/desktop_media_picker_views.cc
+++ chrome/browser/ui/views/desktop_capture/desktop_media_picker_views.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents_delegate.h"
+#include "ui/aura/window_tree_host.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/events/keycodes/keyboard_codes.h"
 #include "ui/gfx/canvas.h"
@@ -33,15 +34,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/window/dialog_client_view.h"
 #include "ui/wm/core/shadow_types.h"
 
-#if defined(USE_AURA)
-#include "ui/aura/window_tree_host.h"
-#endif
-
 using content::DesktopMediaID;
 
 namespace {
 
-#if !defined(OS_CHROMEOS) && defined(USE_AURA)
+#if !defined(OS_CHROMEOS)
 DesktopMediaID::Id AcceleratedWidgetToDesktopMediaId(
     gfx::AcceleratedWidget accelerated_widget) {
 #if defined(OS_WIN)
@@ -227,7 +224,6 @@ DesktopMediaPickerDialogView::DesktopMediaPickerDialogView(
   // the Id is passed to DesktopMediaList.
   DesktopMediaID dialog_window_id;
   if (!modal_dialog) {
-#if defined(USE_AURA)
     dialog_window_id = DesktopMediaID::RegisterAuraWindow(
         DesktopMediaID::TYPE_WINDOW, widget->GetNativeWindow());
 
@@ -235,8 +231,7 @@ DesktopMediaPickerDialogView::DesktopMediaPickerDialogView(
 #if !defined(OS_CHROMEOS)
     dialog_window_id.id = AcceleratedWidgetToDesktopMediaId(
         widget->GetNativeWindow()->GetHost()->GetAcceleratedWidget());
-#endif  // !defined(OS_CHROMEOS)
-#endif  // defined(USE_AURA)
+#endif
   }
 
   for (auto* list_view : list_views_)
