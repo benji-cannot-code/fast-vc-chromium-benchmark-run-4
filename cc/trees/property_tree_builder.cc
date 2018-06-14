414057ba4ade4eb40ede3a7e6ef40e8b7dee41a9
danakj <danakj@chromium.org>
2018-06-14 19:54:35 +0000

    cc: Additional comments and cleanup of cc::Layer APIs.
    The GetTransformNode() method is removed, as it is only safe to use
    in certain cases, and just wraps a call to get the node from the
    property tree, just do it directly in the callers.
    
    R=weiliangc@chromium.org
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I6f242f60209c6d9e3d603cfcf60f59ecafc8f523
    Reviewed-on: https://chromium-review.googlesource.com/1099765
    Reviewed-by: weiliangc <weiliangc@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567386}

--- cc/trees/property_tree_builder.cc
+++ cc/trees/property_tree_builder.cc
@@ -297,7 +297,7 @@ static inline int SortingContextId(LayerImpl* layer) {
 }
 
 static inline bool Is3dSorted(Layer* layer) {
-  return layer->Is3dSorted();
+  return layer->sorting_context_id() != 0;
 }
 
 static inline bool Is3dSorted(LayerImpl* layer) {
