a5622d25962e8f8b5f2e4e5c3a958b1a7ffdc2a0
Helen Li <xunjieli@chromium.org>
2018-06-15 16:10:26 +0000

    Make GCM's ConnectionFactoryImpl uses ProxyResolvingClientSocketFactory
    ProxyResolvingClientSocketFactory takes care of creating a new
    HttpNetworkSession that is based on a given net::URLRequestContext.
    
    This CL switches GCM's ConnectionFactoryImpl to using
    ProxyResolvingClientSocketFactory, so there is less code duplication. This
    will makes it easier to standardize usage of ProxyResolvingClientSocket to
    prepare for servicifying it.
    
    Bug: 844187
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Change-Id: I06b1f6b7c6fc7e8c458b24f2ee3dcc5664bd4c21
    Reviewed-on: https://chromium-review.googlesource.com/1101058
    Commit-Queue: Helen Li <xunjieli@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Nicolas Zea <zea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567674}

--- components/gcm_driver/gcm_client_impl_unittest.cc
+++ components/gcm_driver/gcm_client_impl_unittest.cc
@@ -227,8 +227,7 @@ class FakeGCMInternalsBuilder : public GCMInternalsBuilder {
   std::unique_ptr<ConnectionFactory> BuildConnectionFactory(
       const std::vector<GURL>& endpoints,
       const net::BackoffEntry::Policy& backoff_policy,
-      net::HttpNetworkSession* gcm_network_session,
-      net::HttpNetworkSession* http_network_session,
+      net::URLRequestContext* url_request_context,
       GCMStatsRecorder* recorder) override;
 
  private:
@@ -258,8 +257,7 @@ std::unique_ptr<ConnectionFactory>
 FakeGCMInternalsBuilder::BuildConnectionFactory(
     const std::vector<GURL>& endpoints,
     const net::BackoffEntry::Policy& backoff_policy,
-    net::HttpNetworkSession* gcm_network_session,
-    net::HttpNetworkSession* http_network_session,
+    net::URLRequestContext* url_request_context,
     GCMStatsRecorder* recorder) {
   return base::WrapUnique<ConnectionFactory>(new FakeConnectionFactory());
 }
--- google_apis/gcm/engine/connection_factory_impl_unittest.cc
+++ google_apis/gcm/engine/connection_factory_impl_unittest.cc
@@ -17,8 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "google_apis/gcm/engine/fake_connection_handler.h"
 #include "google_apis/gcm/monitoring/fake_gcm_stats_recorder.h"
 #include "net/base/backoff_entry.h"
-#include "net/http/http_network_session.h"
 #include "net/traffic_annotation/network_traffic_annotation_test_helper.h"
+#include "net/url_request/url_request_test_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 class Policy;
@@ -87,7 +87,8 @@ void WriteContinuation() {
 // backoff policy.
 class TestConnectionFactoryImpl : public ConnectionFactoryImpl {
  public:
-  TestConnectionFactoryImpl(const base::Closure& finished_callback);
+  TestConnectionFactoryImpl(net::URLRequestContext* request_context,
+                            const base::Closure& finished_callback);
   ~TestConnectionFactoryImpl() override;
 
   void InitializeFactory();
@@ -142,12 +143,11 @@ class TestConnectionFactoryImpl : public ConnectionFactoryImpl {
 };
 
 TestConnectionFactoryImpl::TestConnectionFactoryImpl(
+    net::URLRequestContext* request_context,
     const base::Closure& finished_callback)
     : ConnectionFactoryImpl(BuildEndpoints(),
                             net::BackoffEntry::Policy(),
-                            NULL,
-                            NULL,
-                            NULL,
+                            request_context,
                             &dummy_recorder_),
       connect_result_(net::ERR_UNEXPECTED),
       num_expected_attempts_(0),
@@ -275,16 +275,23 @@ class ConnectionFactoryImplTest
  private:
   void ConnectionsComplete();
 
-  TestConnectionFactoryImpl factory_;
   base::MessageLoop message_loop_;
+
+  // Dummy request context that is not used to make network requests, and is
+  // added to make ProxyResolvingClientSocketFactory to not DCHECK on a null
+  // context.
+  net::TestURLRequestContext request_context_;
+
+  TestConnectionFactoryImpl factory_;
   std::unique_ptr<base::RunLoop> run_loop_;
 
   GURL connected_server_;
 };
 
 ConnectionFactoryImplTest::ConnectionFactoryImplTest()
-    : factory_(base::Bind(&ConnectionFactoryImplTest::ConnectionsComplete,
-                         base::Unretained(this))),
+    : factory_(&request_context_,
+               base::Bind(&ConnectionFactoryImplTest::ConnectionsComplete,
+                          base::Unretained(this))),
       run_loop_(new base::RunLoop()) {
   factory()->SetConnectionListener(this);
   factory()->Initialize(
