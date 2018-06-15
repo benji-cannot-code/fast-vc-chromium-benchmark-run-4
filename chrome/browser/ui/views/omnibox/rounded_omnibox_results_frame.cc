645d93e88039d480f352ec47e80de8e3d193b829
manuk <manukh@chromium.org>
2018-06-15 18:27:42 +0000

    omnibox: decrease bottom inset of omnibox results frame for material design refresh by 1
    Previously, the insets were 5 vertically and 6 horizontally. Now, the bottom inset has been decreased to 4 in order to align the bottom of the results frame's top section with the bottom of the location bar
    
    Bug: 852994
    Change-Id: If3a7d85c3edc3a8d055ff53d910c688ced9171ec
    Reviewed-on: https://chromium-review.googlesource.com/1102661
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567736}

--- chrome/browser/ui/views/omnibox/rounded_omnibox_results_frame.cc
+++ chrome/browser/ui/views/omnibox/rounded_omnibox_results_frame.cc
@@ -94,7 +94,7 @@ int RoundedOmniboxResultsFrame::GetNonResultSectionHeight() {
 
 // static
 gfx::Insets RoundedOmniboxResultsFrame::GetLocationBarAlignmentInsets() {
-  return ui::MaterialDesignController::IsRefreshUi() ? gfx::Insets(5, 6)
+  return ui::MaterialDesignController::IsRefreshUi() ? gfx::Insets(5, 6, 4, 6)
                                                      : gfx::Insets(4);
 }
 
