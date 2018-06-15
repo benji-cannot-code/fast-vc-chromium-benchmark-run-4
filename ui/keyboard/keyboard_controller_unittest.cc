ac6428dbdfa0965427f5db8c6c12e40e110ac0b0
Yuichiro Hanada <yhanada@chromium.org>
2018-06-15 03:24:16 +0000

    Fix KeyboardControllerTest.SetOcculudedBoundsChangesFullscreenBonnds.
    Test: keyboard_unittests.
    Change-Id: Ia69ef5a33cdec9a82ffac84d54205321f592b82e
    Reviewed-on: https://chromium-review.googlesource.com/1101607
    Commit-Queue: Blake O'Hare <blakeo@chromium.org>
    Reviewed-by: Blake O'Hare <blakeo@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567536}

--- ui/keyboard/keyboard_controller_unittest.cc
+++ ui/keyboard/keyboard_controller_unittest.cc
@@ -603,8 +603,6 @@ TEST_F(KeyboardControllerTest, CloseKeyboard) {
 }
 
 TEST_F(KeyboardControllerTest, SetOccludedBoundsChangesFullscreenBounds) {
-  aura::Window* keyboard_container(controller().GetContainerWindow());
-  root_window()->AddChild(keyboard_container);
   controller().LoadKeyboardUiInBackground();
 
   // Keyboard is hidden, so SetContainerType should be synchronous.
