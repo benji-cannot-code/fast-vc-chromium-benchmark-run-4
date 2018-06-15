7baec9cebca2847e4a733ed241e0ea2190d5c700
Niels Möller <nisse@chromium.org>
2018-06-15 17:22:00 +0000

    Delete setting of picture id and tl0 picture index.
    These members of CodingSpecificInfo are unused since cl
    https://webrtc-review.googlesource.com/61640,
    and will be deleted in webrtc.
    
    Bug: webrtc:8830
    Change-Id: I4502fc317c4e275c2890870e6a1b8f28d9c5dd88
    Reviewed-on: https://chromium-review.googlesource.com/1102330
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567707}

--- remoting/protocol/webrtc_dummy_video_encoder.cc
+++ remoting/protocol/webrtc_dummy_video_encoder.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
-#include "base/rand_util.h"
 #include "base/stl_util.h"
 #include "base/synchronization/lock.h"
 #include "base/threading/thread_task_runner_handle.h"
@@ -61,9 +60,6 @@ WebrtcDummyVideoEncoder::WebrtcDummyVideoEncoder(
     : main_task_runner_(main_task_runner),
       state_(kUninitialized),
       video_channel_state_observer_(video_channel_state_observer) {
-  // Initialize randomly to avoid replay attacks.
-  base::RandBytes(&picture_id_, sizeof(picture_id_));
-  picture_id_ &= 0x7fff;
 }
 
 WebrtcDummyVideoEncoder::~WebrtcDummyVideoEncoder() = default;
@@ -177,9 +173,6 @@ webrtc::EncodedImageCallback::Result WebrtcDummyVideoEncoder::SendEncodedFrame(
         &codec_specific_info.codecSpecific.VP8;
     vp8_info->simulcastIdx = 0;
     vp8_info->temporalIdx = webrtc::kNoTemporalIdx;
-    vp8_info->tl0PicIdx = webrtc::kNoTl0PicIdx;
-    vp8_info->pictureId = picture_id_;
-    picture_id_ = (picture_id_ + 1) & 0x7fff;
   } else if (frame.codec == webrtc::kVideoCodecVP9) {
     webrtc::CodecSpecificInfoVP9* vp9_info =
         &codec_specific_info.codecSpecific.VP9;
@@ -194,9 +187,6 @@ webrtc::EncodedImageCallback::Result WebrtcDummyVideoEncoder::SendEncodedFrame(
     vp9_info->gof_idx = webrtc::kNoGofIdx;
     vp9_info->temporal_idx = webrtc::kNoTemporalIdx;
     vp9_info->spatial_idx = webrtc::kNoSpatialIdx;
-    vp9_info->tl0_pic_idx = webrtc::kNoTl0PicIdx;
-    vp9_info->picture_id = picture_id_;
-    picture_id_ = (picture_id_ + 1) & 0x7fff;
   } else if (frame.codec == webrtc::kVideoCodecH264) {
 #if defined(USE_H264_ENCODER)
     webrtc::CodecSpecificInfoH264* h264_info =
