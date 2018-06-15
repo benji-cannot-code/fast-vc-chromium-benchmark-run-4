7763fee223d20106f53b09b692d88ecad164b348
Yuichiro Hanada <yhanada@chromium.org>
2018-06-15 08:19:19 +0000

    Fix condition to include "ui/keyboard/keyboard_resource_util.h"
    //ui/keyboard is available only on Chrome OS.
    The code using the module is guarded by the correct conditions,
    so it has not caused any build error so far.
    
    Bug: 847109
    Test: trybot
    Change-Id: I551d78e02d0b17b6d7cf6075e87189173d79844f
    Reviewed-on: https://chromium-review.googlesource.com/1098575
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Commit-Queue: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567575}

--- chrome/common/extensions/chrome_manifest_url_handlers.cc
+++ chrome/common/extensions/chrome_manifest_url_handlers.cc
@@ -25,8 +25,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/manifest_url_handlers.h"
 #include "extensions/common/permissions/api_permission.h"
 
-#if defined(USE_AURA)
-#include "ui/keyboard/keyboard_resource_util.h"  // nogncheck
+#if defined(OS_CHROMEOS)
+#include "ui/keyboard/keyboard_resource_util.h"
 #endif
 
 namespace extensions {
