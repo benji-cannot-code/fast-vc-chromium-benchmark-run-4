ef174bf24a8174ad052b5c237489ccc568f589aa
Xiaoqian Dai <xdai@chromium.org>
2018-06-15 22:02:10 +0000

    Tab dragging: Do not always update window's bounds when adding it to overview.
    When adding an Window to an existing overview grid through
    WindowSelector::AddItem(), we should only update its bounds when
    necessary. Otherwise we may see a flash and the animation might not look
    right. This is because the to-be-added window's bounds might not
    consistent with its window state (which might be caused by several
    reasons, e.g., tab dragging, tapping on the divider to resize snapped
    windows, etc), so updating its window bounds before adding to overview
    window grid may cause the window do the bounds change animation first,
    and the followed by the overview transform animation.
    
    Bug: 823769
    Change-Id: I4a56349991663a20974c7bdc5c800b919241fcd4
    Reviewed-on: https://chromium-review.googlesource.com/1102033
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567807}

--- ash/wm/overview/window_selector.cc
+++ ash/wm/overview/window_selector.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/panels/panel_layout_manager.h"
 #include "ash/wm/splitview/split_view_drag_indicators.h"
 #include "ash/wm/switchable_windows.h"
-#include "ash/wm/tablet_mode/tablet_mode_window_state.h"
 #include "ash/wm/window_state.h"
 #include "ash/wm/window_util.h"
 #include "base/auto_reset.h"
@@ -534,11 +533,6 @@ void WindowSelector::AddItem(aura::Window* window) {
   if (!grid || grid->GetWindowSelectorItemContaining(window))
     return;
 
-  // The dimensions of |window| will be very slim because of dragging the
-  // divider to the edge. Change the window dimensions to its tablet mode
-  // dimensions. Note: if split view is no longer constrained to tablet mode
-  // this will be need to updated.
-  TabletModeWindowState::UpdateWindowPosition(wm::GetWindowState(window));
   grid->AddItem(window);
   ++num_items_;
 
