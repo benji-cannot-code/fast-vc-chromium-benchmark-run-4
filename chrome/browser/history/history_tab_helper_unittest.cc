cac544a75d3b3109da9450213fccd0d78bb1fad8
clamy <clamy@chromium.org>
2018-06-15 16:24:44 +0000

    Remove usage of GetActiveEntry [5/n]
    This CL is part of a serie of CL to remove usage of the deprecated
    method NavigationController::GetActiveEntry.
    
    Bug: 273710
    Change-Id: I68a14673811f5e2854828ec8aaa218e86f9bef87
    Reviewed-on: https://chromium-review.googlesource.com/1102461
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Camille Lamy <clamy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567680}

--- chrome/browser/history/history_tab_helper_unittest.cc
+++ chrome/browser/history/history_tab_helper_unittest.cc
@@ -80,7 +80,7 @@ TEST_F(HistoryTabHelperTest, ShouldUpdateTitleInHistory) {
   web_contents_tester()->NavigateAndCommit(page_url_);
 
   content::NavigationEntry* entry =
-      web_contents()->GetController().GetActiveEntry();
+      web_contents()->GetController().GetLastCommittedEntry();
   ASSERT_NE(nullptr, entry);
   ASSERT_TRUE(web_contents()->IsLoading());
 
@@ -92,7 +92,7 @@ TEST_F(HistoryTabHelperTest, ShouldLimitTitleUpdatesPerPage) {
   web_contents_tester()->NavigateAndCommit(page_url_);
 
   content::NavigationEntry* entry =
-      web_contents()->GetController().GetActiveEntry();
+      web_contents()->GetController().GetLastCommittedEntry();
   ASSERT_NE(nullptr, entry);
   ASSERT_TRUE(web_contents()->IsLoading());
 
