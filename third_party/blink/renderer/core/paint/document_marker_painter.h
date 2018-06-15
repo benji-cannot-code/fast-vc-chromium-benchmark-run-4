c2ce45a2b06b9980ab444b324eb717ac604ea9d6
Yoichi Osato <yoichio@chromium.org>
2018-06-15 09:33:22 +0000

    [LayoutNG] Paint spelling/grammar markers
    This patch implements painting spelling/grammar markers by
    sharing logic from InlineTextBoxPainter.
    
    This patch fixes:
    - paint/markers/marker-early-break-bug.html
    
    Bug: 850448
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng;luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I4ebb3c788c614fde25c5fc28d461b089edb57fa9
    Reviewed-on: https://chromium-review.googlesource.com/1100669
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567591}

--- third_party/blink/renderer/core/paint/document_marker_painter.h
+++ third_party/blink/renderer/core/paint/document_marker_painter.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_PAINT_DOCUMENT_MARKER_PAINTER_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_PAINT_DOCUMENT_MARKER_PAINTER_H_
 
+#include "third_party/blink/renderer/core/editing/markers/document_marker.h"
 #include "third_party/blink/renderer/platform/wtf/allocator.h"
 
 namespace blink {
@@ -29,6 +30,11 @@ class DocumentMarkerPainter {
                                             const ComputedStyle& style,
                                             const FloatRect& marker_rect,
                                             LayoutUnit logical_height);
+  static void PaintDocumentMarker(GraphicsContext& context,
+                                  const LayoutPoint& box_origin,
+                                  const ComputedStyle& style,
+                                  DocumentMarker::MarkerType marker_type,
+                                  const LayoutRect& local_rect);
 };
 
 }  // namespace blink
