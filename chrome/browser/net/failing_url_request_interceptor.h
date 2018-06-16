ce5d0e019a73f3524eded2a91be3d0caf0325a3c
Matt Menke <mmenke@chromium.org>
2018-06-16 05:38:49 +0000

    Simplify in-process URLRequestContext creation with the NetworkService.
    The old code hooked up things to the URLRequestContext that hadn't been
    ported over to work with the NetworkService yet, when the NetworkService
    was enabled. This CL just does the minimum setup that's needed to not
    crash. It also makes requests made with the in-process URLRequestContext
    fail when the network service is enabled. These changes will help
    identify code that still depends on the legacy path, and allow for some
    cleanup of URLRequestContextBuilderMojo and NetworkContext.
    
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Change-Id: I4c3f40e6dc3c235844846bff7d1d43d0b1c986d0
    Bug: 825242
    Reviewed-on: https://chromium-review.googlesource.com/1096075
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567879}

---
+++ chrome/browser/net/failing_url_request_interceptor.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_NET_FAILING_URL_REQUEST_INTERCEPTOR_H_
+#define CHROME_BROWSER_NET_FAILING_URL_REQUEST_INTERCEPTOR_H_
+
+#include "net/url_request/url_request_interceptor.h"
+
+// A URLRequestInterceptor that fails all network requests.
+class FailingURLRequestInterceptor : public net::URLRequestInterceptor {
+ public:
+  FailingURLRequestInterceptor();
+  ~FailingURLRequestInterceptor() override;
+
+  net::URLRequestJob* MaybeInterceptRequest(
+      net::URLRequest* request,
+      net::NetworkDelegate* network_delegate) const override;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(FailingURLRequestInterceptor);
+};
+
+#endif  // CHROME_BROWSER_NET_FAILING_URL_REQUEST_INTERCEPTOR_H_
