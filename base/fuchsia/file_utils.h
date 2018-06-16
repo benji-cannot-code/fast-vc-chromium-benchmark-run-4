5901bca8b8296375e182552551e26be4c0f70051
Kevin Marshall <kmarshall@chromium.org>
2018-06-16 05:40:53 +0000

    Fuchsia: Enable mounting handles as dirs on child processess.
    This functionality will be used by the ContextProvider service to
    bind an arbitrary, client-supplied directory handle to the "/data"
    directory of the child Context processes.
    
    * Adds a helper function for extracting file handles from File objects.
    * Adds unit tests.
    * Migrates some uses of ScopedZxHandle to zx::handle.
    
    Bug: 850743, 852541
    Change-Id: Iada1775485745124822e32b9ac81aefaff6b9059
    Reviewed-on: https://chromium-review.googlesource.com/1091803
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567880}

---
+++ base/fuchsia/file_utils.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_FUCHSIA_FILE_UTILS_H_
+#define BASE_FUCHSIA_FILE_UTILS_H_
+
+#include <lib/zx/handle.h>
+
+#include "base/base_export.h"
+
+namespace base {
+
+class File;
+
+namespace fuchsia {
+
+// Gets a Zircon handle from a file or directory |path| in the process'
+// namespace.
+BASE_EXPORT zx::handle GetHandleFromFile(base::File file);
+
+}  // namespace fuchsia
+}  // namespace base
+
+#endif  // BASE_FUCHSIA_FILE_UTILS_H_
