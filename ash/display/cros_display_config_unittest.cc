a9233685c05a365f6dca9cbf87beb9cff1a091bc
Malay Keshav <malaykeshav@chromium.org>
2018-06-16 05:33:29 +0000

    Modify the string format to replace ui scale with zoom factor
    This patch performs multiple tasks:
      - Modify the protocol that generates a ManagedDisplayInfo to use zoom
        factor instead of ui scale. When display zoom feature is disabled,
        it reverts back to the old string format.
      - Add a DCHECK in computing the effective Ui scale to ensure that it
        is 1 when display zoom is enabled.
      - Update all tests that fail due to this assertion. This is done
        mostly by creating a new test class with the display zoom feature
        disabled. OTOH, the tests that we want to keep in display zoom
        mode are modified to fit the assertion. We want to make sure that
        both ui scale and display zoom works for a few milestones before we
        completely deprecate and remove ui scale.
    
    Bug: 823922
    Change-Id: I9bab4f6cb0884242d5f9427ba004f4c677835b64
    Component: Managed Display Info, ui scale, display zoom
    Reviewed-on: https://chromium-review.googlesource.com/1099476
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Malay Keshav <malaykeshav@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567878}

--- ash/display/cros_display_config_unittest.cc
+++ ash/display/cros_display_config_unittest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/stl_util.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/test/scoped_feature_list.h"
 #include "mojo/public/cpp/bindings/associated_binding.h"
 #include "services/ui/public/cpp/input_devices/input_device_client_test_api.h"
 #include "ui/display/display_switches.h"
@@ -198,6 +199,25 @@ class CrosDisplayConfigTest : public AshTestBase {
   DISALLOW_COPY_AND_ASSIGN(CrosDisplayConfigTest);
 };
 
+// For tests involving ui scale we need to disable the display zoom feature.
+class CrosDisplayConfigTestWithUiScale : public CrosDisplayConfigTest {
+ public:
+  CrosDisplayConfigTestWithUiScale() = default;
+  ~CrosDisplayConfigTestWithUiScale() override = default;
+
+  // CrosDisplayConfigTest
+  void SetUp() override {
+    scoped_feature_list_.InitAndDisableFeature(
+        features::kEnableDisplayZoomSetting);
+    CrosDisplayConfigTest::SetUp();
+  }
+
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_;
+
+  DISALLOW_COPY_AND_ASSIGN(CrosDisplayConfigTestWithUiScale);
+};
+
 }  // namespace
 
 TEST_F(CrosDisplayConfigTest, OnDisplayConfigChanged) {
@@ -354,7 +374,7 @@ TEST_F(CrosDisplayConfigTest, GetDisplayUnitInfoListBasic) {
   EXPECT_EQ(96, info_1.dpi_y);
 }
 
-TEST_F(CrosDisplayConfigTest, GetDisplayUnitInfoListModes) {
+TEST_F(CrosDisplayConfigTestWithUiScale, GetDisplayUnitInfoListModes) {
   UpdateDisplay("1024x512,1024x512");
   std::vector<mojom::DisplayUnitInfoPtr> result = GetDisplayUnitInfoList();
   ASSERT_EQ(2u, result.size());
@@ -573,7 +593,7 @@ TEST_F(CrosDisplayConfigTest, SetDisplayPropertiesDisplayZoomFactor) {
       display_manager()->GetDisplayInfo(display_id_list[1]).zoom_factor());
 }
 
-TEST_F(CrosDisplayConfigTest, SetDisplayMode) {
+TEST_F(CrosDisplayConfigTestWithUiScale, SetDisplayMode) {
   UpdateDisplay("1024x512,1024x512");
   std::vector<mojom::DisplayUnitInfoPtr> result = GetDisplayUnitInfoList();
   ASSERT_EQ(2u, result.size());
