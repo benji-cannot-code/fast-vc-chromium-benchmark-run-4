85e9416cd5981b2b42cba454c3c4b5ba23227646
Francois Doray <fdoray@chromium.org>
2018-06-18 16:37:15 +0000

    RC: Remove TabLifecycleUnitExternal::FreezeTab/IsFrozen().
    Freezing shouldn't be exposed to external components.
    
    Bug: 775644
    Change-Id: I60014f3a39ab241677d678d49ab1ffb2f99689c6
    Reviewed-on: https://chromium-review.googlesource.com/1099650
    Reviewed-by: Sébastien Marchand <sebmarchand@chromium.org>
    Reviewed-by: Fadi Meawad <fmeawad@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568033}

--- chrome/browser/resource_coordinator/tab_manager_unittest.cc
+++ chrome/browser/resource_coordinator/tab_manager_unittest.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/resource_coordinator/background_tab_navigation_throttle.h"
 #include "chrome/browser/resource_coordinator/tab_helper.h"
+#include "chrome/browser/resource_coordinator/tab_lifecycle_unit.h"
 #include "chrome/browser/resource_coordinator/tab_lifecycle_unit_external.h"
 #include "chrome/browser/resource_coordinator/tab_manager_features.h"
 #include "chrome/browser/resource_coordinator/tab_manager_resource_coordinator_signal_observer.h"
@@ -109,14 +110,6 @@ enum TestIndicies {
   kOldButPinned,
   kInternalPage,
 };
-
-bool IsTabDiscarded(content::WebContents* web_contents) {
-  return TabLifecycleUnitExternal::FromWebContents(web_contents)->IsDiscarded();
-}
-
-bool IsTabFrozen(content::WebContents* web_contents) {
-  return TabLifecycleUnitExternal::FromWebContents(web_contents)->IsFrozen();
-}
 
 }  // namespace
 
@@ -210,6 +203,19 @@ class TabManagerTest : public ChromeRenderViewHostTestHarness {
         tab_manager->MaybeThrottleNavigation(throttle3_.get());
 
     CheckThrottleResults(result1, result2, result3, loading_slots);
+  }
+
+  bool IsTabDiscarded(content::WebContents* content) {
+    return TabLifecycleUnitExternal::FromWebContents(content)->IsDiscarded();
+  }
+
+  bool IsTabFrozen(content::WebContents* content) {
+    const LifecycleUnitState state =
+        static_cast<TabLifecycleUnitSource::TabLifecycleUnit*>(
+            TabLifecycleUnitExternal::FromWebContents(content))
+            ->GetState();
+    return state == LifecycleUnitState::PENDING_FREEZE ||
+           state == LifecycleUnitState::FROZEN;
   }
 
   virtual void CheckThrottleResults(
