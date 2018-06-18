a8df1157b0d7f351f46b03bab4d96192ea14e01a
Hiroki Nakagawa <nhiroki@chromium.org>
2018-06-18 06:24:33 +0000

    ES Modules: Set referrer for top-level module script in ModuleScriptLoader
    Before this CL, referrer for top-level module script is set in
    BaseFetchContext::AddAdditionalRequestHeaders() using
    ExecutionContext::GetOutgoingReferrer(). This works for documents, but doesn't
    for workers because this execution context is corresponding to "module map
    settings object", not "fetch client settings object".
    
    To fix this, this CL sets the referrer for top-level module script using "fetch
    client settings object" in ModuleScriptLoader as the spec defines.
    
    Change-Id: I0878b13febdae4ee2dc56c998e7f2c100e9c1ba7
    Bug: 842553
    Reviewed-on: https://chromium-review.googlesource.com/1102236
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567937}

--- third_party/blink/renderer/core/loader/modulescript/module_tree_linker.cc
+++ third_party/blink/renderer/core/loader/modulescript/module_tree_linker.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/bindings/v8_throw_exception.h"
 #include "third_party/blink/renderer/platform/loader/fetch/resource_loading_log.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
+#include "third_party/blink/renderer/platform/weborigin/security_policy.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 #include "v8/include/v8.h"
 
@@ -183,8 +184,10 @@ void ModuleTreeLinker::FetchRoot(const KURL& original_url,
   // Step 2. Perform the internal module script graph fetching procedure given
   // ... with the top-level module fetch flag set. ...
   ModuleScriptFetchRequest request(
-      url, destination_, options, Referrer::NoReferrer(),
-      fetch_client_settings_object_.GetReferrerPolicy(),
+      url, destination_, options,
+      SecurityPolicy::GenerateReferrer(
+          fetch_client_settings_object_.GetReferrerPolicy(), url,
+          fetch_client_settings_object_.GetOutgoingReferrer()),
       TextPosition::MinimumPosition());
 
   InitiateInternalModuleScriptGraphFetching(
@@ -374,8 +377,11 @@ void ModuleTreeLinker::FetchDescendants(ModuleScript* module_script) {
     // [FD] Step 7. ... perform the internal module script graph fetching
     // procedure given ... with the top-level module fetch flag unset. ...
     ModuleScriptFetchRequest request(
-        urls[i], destination_, options, module_script->BaseURL().GetString(),
-        fetch_client_settings_object_.GetReferrerPolicy(), positions[i]);
+        urls[i], destination_, options,
+        SecurityPolicy::GenerateReferrer(
+            fetch_client_settings_object_.GetReferrerPolicy(), urls[i],
+            module_script->BaseURL().GetString()),
+        positions[i]);
     InitiateInternalModuleScriptGraphFetching(
         request, ModuleGraphLevel::kDependentModuleFetch);
   }
