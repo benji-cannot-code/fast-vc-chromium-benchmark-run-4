c909a3586e0a39278ec48ee363984d2c63975d97
Carlos Knippschild <carlosk@chromium.org>
2018-06-18 23:29:26 +0000

    Enable Offline Prefetch by default.
    Bug: 687364
    Change-Id: I4727334dcf0060b5a8202a9694e7bd60d31029ec
    Reviewed-on: https://chromium-review.googlesource.com/1105031
    Reviewed-by: Dmitry Titov <dimich@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Commit-Queue: Carlos Knippschild <carlosk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568230}

--- components/offline_pages/core/client_policy_controller_unittest.cc
+++ components/offline_pages/core/client_policy_controller_unittest.cc
@@ -238,7 +238,7 @@ TEST_F(ClientPolicyControllerTest, CheckSuggestedArticlesDefined) {
   EXPECT_TRUE(isTemporary(policy));
   EXPECT_TRUE(controller()->IsRemovedOnCacheReset(kSuggestedArticlesNamespace));
   ExpectRemovedOnCacheReset(kSuggestedArticlesNamespace, true);
-  ExpectDownloadSupport(kSuggestedArticlesNamespace, false);
+  ExpectDownloadSupport(kSuggestedArticlesNamespace, true);
   ExpectUserRequestedDownloadSupport(kSuggestedArticlesNamespace, false);
   ExpectRecentTab(kSuggestedArticlesNamespace, false);
   ExpectOnlyOriginalTab(kSuggestedArticlesNamespace, false);
