76211daca3f821b936fe6c4ac0adc87772920f71
Dominick Ng <dominickn@chromium.org>
2018-06-18 23:41:43 +0000

    Improve the accessibility of the intent picker bubble.
    This CL makes several improvements to the accessibility of the intent
    picker bubble:
    
    1. Reducing the opacity applied to the ink drop highlight for focused
       items (increasing text contrast slightly)
    2. Adding a role override to the bubble to avoid the screen reader
       saying "Unknown" for the picker purpose
    3. Making each label button focusable so they can be tabbed through.
    
    BUG=760200,825700
    
    Change-Id: Ib2477b786350b07325a615f77ba4b168b1bb5cc7
    Reviewed-on: https://chromium-review.googlesource.com/1102228
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568234}

--- chrome/browser/ui/views/intent_picker_bubble_view.cc
+++ chrome/browser/ui/views/intent_picker_bubble_view.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_dialogs.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
+#include "chrome/browser/ui/views/toolbar/toolbar_ink_drop_util.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_view.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/arc/intent_helper/arc_intent_helper_bridge.h"
@@ -83,9 +84,10 @@ class IntentPickerLabelButton : public views::LabelButton {
     if (!icon->IsEmpty())
       SetImage(views::ImageButton::STATE_NORMAL, *icon->ToImageSkia());
     SetBorder(views::CreateEmptyBorder(8, 16, 8, 0));
+    SetFocusForPlatform();
+    set_ink_drop_base_color(SK_ColorGRAY);
+    set_ink_drop_visible_opacity(kToolbarInkDropVisibleOpacity);
   }
-
-  SkColor GetInkDropBaseColor() const override { return SK_ColorGRAY; }
 
   void MarkAsUnselected(const ui::Event* event) {
     AnimateInkDrop(views::InkDropState::HIDDEN,
@@ -159,7 +161,10 @@ views::Widget* IntentPickerBubbleView::ShowBubble(
   // TODO(aleventhal) Should not need to be focusable as only descendant widgets
   // are interactive; however, it does call RequestFocus(). If it is going to be
   // focusable, it needs an accessible name so that it can pass accessibility
-  // checks. Use the same accessible name as the icon.
+  // checks. Use the same accessible name as the icon. Set the role as kDialog
+  // to ensure screen readers immediately announce the text of this view.
+  intent_picker_bubble_->GetViewAccessibility().OverrideRole(
+      ax::mojom::Role::kDialog);
   intent_picker_bubble_->GetViewAccessibility().OverrideName(
       l10n_util::GetStringUTF16(IDS_TOOLTIP_INTENT_PICKER_ICON));
   intent_picker_bubble_->SetFocusBehavior(View::FocusBehavior::ALWAYS);
