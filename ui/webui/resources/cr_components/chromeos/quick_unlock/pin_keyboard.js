ee4b9a5d3b8aa423c5bac52365ab13841d049947
Scott Chen <scottchen@chromium.org>
2018-06-18 23:57:57 +0000

    WebUI[MD-refresh]: replace most paper-input with cr-inputs
    This CL replaces most paper-inputs with cr-inputs within the code-base.
    After this CL, the 3 places that still has paper-input remaining are:
    - chrome/browser/resources/chromeos/login/
    - chrome/browser/resources/pdf/
    - ui/file_manager/
    
    There are planned follow-up CLs to replace paper-input in the above 3
    areas as well.
    
    Bug: 832177
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: I7acc7e0a26a6bd3d40ec6828608717fd97bd0c73
    Reviewed-on: https://chromium-review.googlesource.com/1086357
    Commit-Queue: Scott Chen <scottchen@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568241}

--- ui/webui/resources/cr_components/chromeos/quick_unlock/pin_keyboard.js
+++ ui/webui/resources/cr_components/chromeos/quick_unlock/pin_keyboard.js
@@ -66,10 +66,12 @@ Polymer({
       value: false,
     },
 
+    hasError: Boolean,
+
     /**
      * The password element the pin keyboard is associated with. If this is not
      * set, then a default input element is shown and used.
-     * @type {?Element}
+     * @type {?HTMLElement}
      * @private
      */
     passwordElement: Object,
@@ -396,7 +398,8 @@ Polymer({
   passwordElement_: function() {
     // |passwordElement| is null by default. It can be set to override the
     // input field that will be populated with the keypad.
-    return this.passwordElement || this.$.pinInput.inputElement.inputElement;
+    return this.passwordElement ||
+        (/** @type {CrInputElement} */ (this.$.pinInput)).inputElement;
   },
 });
 })();
