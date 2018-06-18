6024b00d03841ebe3e58cb4d190f706bc1f752f7
Tommy Martino <tmartino@chromium.org>
2018-06-18 20:42:22 +0000

    [Autofill Views] Reimplement Scrolling
    This CL adds the missing scroll functionality to the Views Autofill
    implementation. Previously, dropdowns that were too tall for their
    window would extend beyond its boundaries (Win + Mac) or even be cut
    off entirely (Linux). The major changes include:
    - Changes to PopupViewCommon to use window bounds, rather than display
      bounds
    - The creation of a mock version of PVC for contexts where we cannot
      safely get the needed window object
    - Exposing the x and y portions of the calculation logic in PVC
    - Changes to the dropdown, including the addition of a scroll view,
      and new logic which determines the needed size. The new size logic
      first uses the new PVC function to determine the height, then adjusts
      for the scroll bar if present, then applies existing rules (rounding,
      minimum, etc.), and finally uses the other new PVC function to get
      the final width.
    
    Change-Id: I4434fa045bfe010d51d582272fbc011b813d3ab0
    Reviewed-on: https://chromium-review.googlesource.com/1072427
    Commit-Queue: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568145}

--- chrome/browser/ui/autofill/autofill_popup_layout_model.h
+++ chrome/browser/ui/autofill/autofill_popup_layout_model.h
@@ -100,6 +100,10 @@ class AutofillPopupLayoutModel {
 
   bool is_credit_card_popup() const { return is_credit_card_popup_; }
 
+  // Allows the provision of another implementation of view_common, for use in
+  // unit tests where using the real thing could cause crashes.
+  void SetUpForTesting(std::unique_ptr<PopupViewCommon> view_common);
+
  private:
   // Returns the enclosing rectangle for the element_bounds.
   const gfx::Rect RoundedElementBounds() const;
@@ -117,7 +121,7 @@ class AutofillPopupLayoutModel {
   // The bounds of the Autofill popup.
   gfx::Rect popup_bounds_;
 
-  PopupViewCommon view_common_;
+  std::unique_ptr<PopupViewCommon> view_common_;
 
   AutofillPopupViewDelegate* delegate_;  // Weak reference.
 
