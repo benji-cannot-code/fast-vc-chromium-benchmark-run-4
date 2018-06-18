69bf7062b328a49f4d634ba729a88e7cd03d32fb
Kyle Horimoto <khorimoto@google.com>
2018-06-18 23:12:53 +0000

    [CrOS MultiDevice] Implement BleListenerOperation.
    This operation attempts to connect to a remote device over BLE via the
    listener role via BleConnectionManager.
    
    Bug: 824568, 752273
    Change-Id: Ie6be7b139b8234049df4d1f1c2535254a1b962a0
    Reviewed-on: https://chromium-review.googlesource.com/1103009
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568220}

--- chromeos/services/secure_channel/ble_listener_operation.h
+++ chromeos/services/secure_channel/ble_listener_operation.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/macros.h"
+#include "base/memory/weak_ptr.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "chromeos/services/secure_channel/ble_listener_failure_type.h"
 #include "chromeos/services/secure_channel/connect_to_device_operation.h"
@@ -18,6 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromeos {
 
 namespace secure_channel {
+
+class BleConnectionManager;
 
 // Attempts to connect to a remote device over BLE via the listener role.
 class BleListenerOperation
@@ -30,6 +33,7 @@ class BleListenerOperation
     virtual ~Factory();
     virtual std::unique_ptr<ConnectToDeviceOperation<BleListenerFailureType>>
     BuildInstance(
+        BleConnectionManager* ble_connection_manager,
         ConnectToDeviceOperation<
             BleListenerFailureType>::ConnectionSuccessCallback success_callback,
         const ConnectToDeviceOperation<
@@ -47,6 +51,7 @@ class BleListenerOperation
 
  private:
   BleListenerOperation(
+      BleConnectionManager* ble_connection_manager,
       ConnectToDeviceOperation<
           BleListenerFailureType>::ConnectionSuccessCallback success_callback,
       const ConnectToDeviceOperation<
@@ -61,6 +66,15 @@ class BleListenerOperation
   void PerformCancellation() override;
   void PerformUpdateConnectionPriority(
       ConnectionPriority connection_priority) override;
+
+  void OnSuccessfulConnection(
+      std::unique_ptr<AuthenticatedChannel> authenticated_channel);
+  void OnConnectionFailure(BleListenerFailureType failure_type);
+
+  BleConnectionManager* ble_connection_manager_;
+
+  bool is_attempt_active_ = false;
+  base::WeakPtrFactory<BleListenerOperation> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(BleListenerOperation);
 };
