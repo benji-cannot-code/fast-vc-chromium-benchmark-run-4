a175ef2f94f717b72cb80f129a7a4abc1824a542
Jan Wilken Doerrie <jdoerrie@chromium.org>
2018-06-18 17:35:43 +0000

    [bluetooth] Skeletons for WinRT GATT Service Discovery
    This change adds class skeletons required for the implementation and
    testing of GATT service discovery on WinRT. No functionality is added,
    this CL is simply meant to reduce the size and boilerplate of the
    following change.
    
    Bug: 821766
    Change-Id: Ie20629235f08dff9e5ffcb38273cc8ddb2a8e5e0
    Reviewed-on: https://chromium-review.googlesource.com/1102023
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568056}

---
+++ device/bluetooth/test/fake_gatt_device_service_winrt.cc
@@ -0,0 +1,48 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "device/bluetooth/test/fake_gatt_device_service_winrt.h"
+
+namespace device {
+
+namespace {
+
+using ABI::Windows::Devices::Bluetooth::GenericAttributeProfile::
+    GattCharacteristic;
+using ABI::Windows::Devices::Bluetooth::GenericAttributeProfile::
+    GattDeviceService;
+using ABI::Windows::Foundation::Collections::IVectorView;
+
+}  // namespace
+
+FakeGattDeviceServiceWinrt::FakeGattDeviceServiceWinrt() = default;
+
+FakeGattDeviceServiceWinrt::~FakeGattDeviceServiceWinrt() = default;
+
+HRESULT FakeGattDeviceServiceWinrt::GetCharacteristics(
+    GUID characteristic_uuid,
+    IVectorView<GattCharacteristic*>** value) {
+  return E_NOTIMPL;
+}
+
+HRESULT FakeGattDeviceServiceWinrt::GetIncludedServices(
+    GUID service_uuid,
+    IVectorView<GattDeviceService*>** value) {
+  return E_NOTIMPL;
+}
+
+HRESULT FakeGattDeviceServiceWinrt::get_DeviceId(HSTRING* value) {
+  return E_NOTIMPL;
+}
+
+HRESULT FakeGattDeviceServiceWinrt::get_Uuid(GUID* value) {
+  return E_NOTIMPL;
+}
+
+HRESULT FakeGattDeviceServiceWinrt::get_AttributeHandle(uint16_t* value) {
+  return E_NOTIMPL;
+}
+
+}  // namespace device
