d539f65ef0231b27e3063795353bd730b5bc79b2
Pavel Feldman <pfeldman@chromium.org>
2018-06-18 23:04:16 +0000

    Headless: don't run unload hooks upon web contents close.
    Change-Id: I6e391fadbbf47aa6427a16495bdca3c88d456b05
    Reviewed-on: https://chromium-review.googlesource.com/1105041
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Pavel Feldman <pfeldman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568213}

--- headless/lib/browser/headless_web_contents_impl.h
+++ headless/lib/browser/headless_web_contents_impl.h
@@ -100,8 +100,6 @@ class HEADLESS_EXPORT HeadlessWebContentsImpl
 
   void Close() override;
 
-  void DelegateRequestsClose();
-
   std::string GetDevToolsAgentHostId();
 
   HeadlessBrowserImpl* browser() const;
@@ -178,8 +176,6 @@ class HEADLESS_EXPORT HeadlessWebContentsImpl
   content::RenderProcessHost* render_process_host_;  // Not owned.
 
   base::ObserverList<HeadlessWebContents::Observer> observers_;
-
-  base::Closure quit_closure_;
 
   base::WeakPtrFactory<HeadlessWebContentsImpl> weak_ptr_factory_;
 
