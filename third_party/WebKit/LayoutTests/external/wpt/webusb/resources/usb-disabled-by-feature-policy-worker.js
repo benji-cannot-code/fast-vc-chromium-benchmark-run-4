4cebdac48f6fd3c1187b4ec33ab916d6a574cd5b
Ovidio Henriquez <odejesush@chromium.org>
2018-06-18 21:33:45 +0000

    Add WebUSB FeaturePolicy WPT for Workers
    This change adds Feature Policy tests for Dedicated Workers.
    
    Bug: 841510
    Change-Id: I117f8a5bb61d5783b6867e7edcc9d99aff41a497
    Reviewed-on: https://chromium-review.googlesource.com/1093379
    Commit-Queue: Ovidio Henriquez <odejesush@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568170}

---
+++ third_party/WebKit/LayoutTests/external/wpt/webusb/resources/usb-disabled-by-feature-policy-worker.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+'use strict';
+
+importScripts('/resources/testharness.js');
+
+const header = 'Feature-Policy header {"usb" : []}';
+let workerType;
+
+if (typeof postMessage === 'function') {
+  workerType = 'dedicated';
+}
+
+promise_test(() => navigator.usb.getDevices().then(
+        () => assert_unreached('expected promise to reject with SecurityError'),
+        error => assert_equals(error.name, 'SecurityError')),
+    `Inherited ${header} disallows ${workerType} workers.`);
+
+done();
