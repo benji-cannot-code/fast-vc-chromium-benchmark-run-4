c6b1e5ecc5f11289f4790766ba667852dadc0057
crystallambert@chromium.org <crystallambert@chromium.org>
2018-06-18 23:48:15 +0000

    [Extension Docs]Sample Extension
    Sample extension that uses a content script to find images on pages,
    then displays images in popup.
    If user clicks image it will download.
    Extension has options page where users can choose to save downloaded images,
    download them again, or display them as thumbnails in popup.
    
    This extension may be used in updated debugging tutorial.
    
    Bug: None
    Change-Id: I38a2bd5af135ad74b889e8366ae36a08d6d53cb0
    Reviewed-on: https://chromium-review.googlesource.com/1053958
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Crystal Lambert <crystallambert@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568236}

---
+++ chrome/common/extensions/docs/examples/extensions/download_images/background.js
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+// Declare extension default properties
+let downloadsArray = [];
+let initialState = {
+  'savedImages': downloadsArray,
+  'thumbnails': false,
+  'saveImages': true
+};
+
+// Set extension setting on installation
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: { hostEquals: 'developer.chrome.com', schemes: ['https'] },
+          css: ['img']
+        })
+      ],
+      actions: [ new chrome.declarativeContent.ShowPageAction() ]
+    }]);
+  });
+  chrome.storage.local.set(initialState);
+});
