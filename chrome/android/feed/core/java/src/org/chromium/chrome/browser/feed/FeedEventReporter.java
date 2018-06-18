f481e0587b32c3aded121ecae49143770b84027c
Sky Malice <skym@google.com>
2018-06-18 23:17:35 +0000

    [Feed] Notify Feed when Android foregrounds.
    To facilitate notifying feed of foregrounds, added FeedEventReporter
    to act as the entry point. This class has a dummy clone because it is
    called outside of Feed code.
    
    Bug: 831648
    Change-Id: Id25205e33b03d20cd63c6b97920d2cb90ac3d4c2
    Reviewed-on: https://chromium-review.googlesource.com/1095774
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568225}

---
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedEventReporter.java
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.feed;
+
+/**
+ * Provides static entry points to send notifications to Feed.
+ */
+public final class FeedEventReporter {
+    // Not meant to be instantiated.
+    private FeedEventReporter() {}
+
+    /*
+     * Should be called when the browser is foregrounded.
+     */
+    public static void onBrowserForegrounded() {
+        FeedProcessScopeFactory.getFeedSchedulerBridge().onForegrounded();
+    }
+}
