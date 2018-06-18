e73fb84cda5131f8a34c400a5faf751e9864afee
Xi Cheng <chengx@chromium.org>
2018-06-18 23:53:19 +0000

    Introduce StackProfileBuilder to generate a CallStackProfile
    This CL introduces a new class StackProfileBuilder owned by StackSamplingProfiler.
    
    This CL also refactors both Windows and Mac profile construction into the
    StackProfileBuilder class to generate a StackSamplingProfiler::CallStackProfile.
    
    Bug: 851163
    Change-Id: I6acde44fc1e38ae04d1d747fe2d3843faba3a218
    Reviewed-on: https://chromium-review.googlesource.com/1101462
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Commit-Queue: Xi Cheng <chengx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568239}

--- base/profiler/native_stack_sampler_posix.cc
+++ base/profiler/native_stack_sampler_posix.cc
@@ -9,7 +9,6 @@ namespace base {
 
 std::unique_ptr<NativeStackSampler> NativeStackSampler::Create(
     PlatformThreadId thread_id,
-    AnnotateCallback annotator,
     NativeStackSamplerTestDelegate* test_delegate) {
   return std::unique_ptr<NativeStackSampler>();
 }
