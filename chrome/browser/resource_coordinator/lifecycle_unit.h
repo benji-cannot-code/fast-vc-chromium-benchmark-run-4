7156b4bb6cbd7cbae5f342e97eca587dcf44d1f8
Chris Hamilton <chrisha@chromium.org>
2018-06-18 19:12:27 +0000

    Add loading state / actions to chrome://discards web UI.
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: I3c25b29dfbb00edbb179003d17cfe061a32ebce3
    Reviewed-on: https://chromium-review.googlesource.com/1100086
    Commit-Queue: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568099}

--- chrome/browser/resource_coordinator/lifecycle_unit.h
+++ chrome/browser/resource_coordinator/lifecycle_unit.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace resource_coordinator {
 
+using ::mojom::LifecycleUnitLoadingState;
 using ::mojom::LifecycleUnitState;
 
 class DecisionDetails;
@@ -92,6 +93,9 @@ class LifecycleUnit {
   // base::TimeTicks::Max() if the LifecycleUnit is currently visible.
   virtual base::TimeTicks GetLastVisibleTime() const = 0;
 
+  // Returns the loading state associated with a LifecycleUnit.
+  virtual LifecycleUnitLoadingState GetLoadingState() const = 0;
+
   // Returns the process hosting this LifecycleUnit. Used to distribute OOM
   // scores.
   //
@@ -112,6 +116,10 @@ class LifecycleUnit {
 
   // Returns the current state of this LifecycleUnit.
   virtual LifecycleUnitState GetState() const = 0;
+
+  // Request that the LifecycleUnit be loaded, return true if the request is
+  // successful.
+  virtual bool Load() = 0;
 
   // Request that the LifecycleUnit be frozen, return true if the request is
   // successfully sent.
