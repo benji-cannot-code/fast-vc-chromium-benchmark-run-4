6ce91d5cde8236e11ebb83a182395e844433406f
clamy <clamy@chromium.org>
2018-06-18 12:00:58 +0000

    Remove usage of GetActiveEntry [16/n]
    This CL is part of a serie of CLs to remove usage of the deprecated
    method NavigationController::GetActiveEntry.
    
    Bug: 273710
    Change-Id: I98d76530c98b62f9c1a5cdd729aea52d39b13575
    Reviewed-on: https://chromium-review.googlesource.com/1102679
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Camille Lamy <clamy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567980}

--- chrome/browser/captive_portal/captive_portal_tab_reloader.cc
+++ chrome/browser/captive_portal/captive_portal_tab_reloader.cc
@@ -267,7 +267,7 @@ void CaptivePortalTabReloader::ReloadTabIfNeeded() {
 
 void CaptivePortalTabReloader::ReloadTab() {
   content::NavigationController* controller = &web_contents_->GetController();
-  if (!controller->GetActiveEntry()->GetHasPostData())
+  if (!controller->GetLastCommittedEntry()->GetHasPostData())
     controller->Reload(content::ReloadType::NORMAL, true);
 }
 
