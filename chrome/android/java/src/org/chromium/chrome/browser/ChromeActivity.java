6e9a7e1708c88bca6bc2e6a6564279639d687450
Friedrich Horschig <fhorschig@chromium.org>
2018-06-18 17:32:48 +0000

    [Android] Manual filling is specific for Browser Tabs
    With this CL, the manual filling component ensures that the keyboard
    accessory and its sheet display only data that is relevant to the active
    tab.
    
    It introduces a mediator controlled and wired up by the already existing
    ManualFillingCoordinator. It is responsible to propagate visibility
    changes to all subcomponents and update their state depending on the
    active browser tab.
    
    Bug: 828832, 811747
    Change-Id: I247fcbbc554c06624c12ffe42597d6f671042336
    Reviewed-on: https://chromium-review.googlesource.com/1100760
    Commit-Queue: Friedrich Horschig <fhorschig@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568053}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
@@ -413,10 +413,6 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
             getWindowAndroid().setAnimationPlaceholderView(
                     mCompositorViewHolder.getCompositorView());
 
-            mManualFillingController = new ManualFillingCoordinator(getWindowAndroid(),
-                    findViewById(R.id.keyboard_accessory_stub),
-                    findViewById(R.id.keyboard_accessory_sheet_stub));
-
             initializeToolbar();
             initializeTabModels();
             if (!isFinishing() && getFullscreenManager() != null) {
@@ -1423,6 +1419,10 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
             mStatusBarScrimFraction = fraction;
             setStatusBarColor(null, mBaseStatusBarColor);
         }, coordinator);
+
+        mManualFillingController = new ManualFillingCoordinator(getWindowAndroid(),
+                findViewById(R.id.keyboard_accessory_stub),
+                findViewById(R.id.keyboard_accessory_sheet_stub));
 
         if (supportsContextualSuggestionsBottomSheet()
                 && FeatureUtilities.isContextualSuggestionsBottomSheetEnabled(isTablet())) {
--- chrome/android/junit/src/org/chromium/chrome/browser/autofill/keyboard_accessory/PasswordAccessorySheetControllerTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/autofill/keyboard_accessory/PasswordAccessorySheetControllerTest.java
@@ -23,7 +23,6 @@ import org.mockito.MockitoAnnotations;
 import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 
-import org.chromium.base.Callback;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.chrome.browser.modelutil.ListObservable;
 import org.chromium.chrome.browser.modelutil.SimpleListObservable;
@@ -52,7 +51,7 @@ public class PasswordAccessorySheetControllerTest {
 
     @Test
     public void testCreatesValidTab() {
-        KeyboardAccessoryData.Tab tab = mCoordinator.createTab();
+        KeyboardAccessoryData.Tab tab = mCoordinator.getTab();
         assertNotNull(tab);
         assertNotNull(tab.getIcon());
         assertNotNull(tab.getListener());
@@ -60,7 +59,7 @@ public class PasswordAccessorySheetControllerTest {
 
     @Test
     public void testSetsViewAdapterOnTabCreation() {
-        KeyboardAccessoryData.Tab tab = mCoordinator.createTab();
+        KeyboardAccessoryData.Tab tab = mCoordinator.getTab();
         assertNotNull(tab);
         assertNotNull(tab.getListener());
         tab.getListener().onTabCreated(mMockView);
@@ -71,29 +70,8 @@ public class PasswordAccessorySheetControllerTest {
     public void testModelNotifiesAboutActionsChangedByProvider() {
         final KeyboardAccessoryData.PropertyProvider<KeyboardAccessoryData.Item> testProvider =
                 new KeyboardAccessoryData.PropertyProvider<>();
-        final KeyboardAccessoryData.Item testItem = new KeyboardAccessoryData.Item() {
-            @Override
-            public int getType() {
-                return KeyboardAccessoryData.Item.TYPE_LABEL;
-            }
-            @Override
-            public String getCaption() {
-                return "Test Item";
-            }
-            @Override
-            public String getContentDescription() {
-                return null;
-            }
-            @Override
-            public boolean isPassword() {
-                return false;
-            }
-
-            @Override
-            public Callback<KeyboardAccessoryData.Item> getItemSelectedCallback() {
-                return null;
-            }
-        };
+        final KeyboardAccessoryData.Item testItem = new KeyboardAccessoryData.Item(
+                KeyboardAccessoryData.Item.TYPE_LABEL, "Test Item", null, false, null);
 
         mModel.addObserver(mMockItemListObserver);
         mCoordinator.registerItemProvider(testProvider);
