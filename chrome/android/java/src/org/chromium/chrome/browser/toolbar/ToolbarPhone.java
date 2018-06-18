7e005e1634b7eab075b22ae7fa936725dbd5efd1
Matthew Jones <mdjones@chromium.org>
2018-06-18 19:27:02 +0000

    Set flag for light drawables in ToolbarPhone
    This patch sets a flag for using light toolbar drawables when the
    visual state is update. This was incorrectly excluded with 227a972f.
    
    Bug: 852682
    Change-Id: I77a2e9f742720b48045a9f02ac1ab0a8ebb77429
    Reviewed-on: https://chromium-review.googlesource.com/1104708
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568104}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarPhone.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarPhone.java
@@ -2562,6 +2562,7 @@ public class ToolbarPhone extends ToolbarLayout
         // Refresh the toolbar texture.
         if ((mVisualState == VisualState.BRAND_COLOR || visualStateChanged)
                 && mLayoutUpdateHost != null) {
+            setUseLightDrawablesForTextureCapture();
             mLayoutUpdateHost.requestUpdate();
         }
         updateShadowVisibility();
