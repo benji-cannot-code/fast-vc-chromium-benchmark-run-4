3eff13916465504dbfe31abb8273048418622e17
Sigurdur Asgeirsson <siggi@chromium.org>
2018-06-18 19:05:40 +0000

    RC: Add a start_time to global memory dumps.
    This will allow a narrower guestimate at the validity of performance
    measurements relating to load events.
    
    Bug: 755840
    Change-Id: I7087c5840106102844ef7518c0ad42648f4abe3e
    Reviewed-on: https://chromium-review.googlesource.com/1101304
    Commit-Queue: Sigurður Ásgeirsson <siggi@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Primiano Tucci <primiano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568096}

--- services/resource_coordinator/memory_instrumentation/queued_request_dispatcher.cc
+++ services/resource_coordinator/memory_instrumentation/queued_request_dispatcher.cc
@@ -188,7 +188,7 @@ void QueuedRequestDispatcher::SetUpAndDispatch(
   DCHECK(!request->dump_in_progress);
   request->dump_in_progress = true;
 
-  request->start_time = base::Time::Now();
+  request->start_time = base::TimeTicks::Now();
 
   TRACE_EVENT_NESTABLE_ASYNC_BEGIN2(
       base::trace_event::MemoryDumpManager::kTraceCategory, "GlobalMemoryDump",
@@ -440,6 +440,7 @@ void QueuedRequestDispatcher::Finalize(QueuedRequest* request,
 
   // Build up the global dump by iterating on the |valid| process dumps.
   mojom::GlobalMemoryDumpPtr global_dump(mojom::GlobalMemoryDump::New());
+  global_dump->start_time = request->start_time;
   global_dump->process_dumps.reserve(request->responses.size());
   for (const auto& response : request->responses) {
     base::ProcessId pid = response.second.process_id;
@@ -553,7 +554,7 @@ void QueuedRequestDispatcher::Finalize(QueuedRequest* request,
   std::move(callback).Run(global_success, request->dump_guid,
                           std::move(global_dump));
   UMA_HISTOGRAM_MEDIUM_TIMES("Memory.Experimental.Debug.GlobalDumpDuration",
-                             base::Time::Now() - request->start_time);
+                             base::TimeTicks::Now() - request->start_time);
   UMA_HISTOGRAM_COUNTS_1000(
       "Memory.Experimental.Debug.FailedProcessDumpsPerGlobalDump",
       request->failed_memory_dump_count);
