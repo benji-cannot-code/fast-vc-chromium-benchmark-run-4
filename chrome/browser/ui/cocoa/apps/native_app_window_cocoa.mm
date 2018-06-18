f953c38f49d3e231fe067062dd40fde3dad98a63
erikchen <erikchen@chromium.org>
2018-06-18 17:32:35 +0000

    Make accelerators work on Signin modal dialog.
    Previously, only some accelerators worked in Windows/Linux [e.g. ctr + V works,
    ctr + T did not]. All accelerators were busted on macOS Views. Some accelerators
    were busted on macOS Cocoa.
    
    The root cause is that SigninViewControllerDelegateViews::HandleKeyboardEvent
    was not implemented on Views, and
    SigninViewControllerDelegateMac::HandleKeyboardEvent was incorrectly implemented
    on macOS.
    
    This CL fixes both implementations to use the now standard logic for
    redispatching key events. This CL also fixes the implementation of
    CommandDispatcher on macOS to correctly detect whether an event is being
    redispatched.
    
    Change-Id: I76b1ef263fce171ae182d316f541f836ab6388ee
    Bug: 776304
    Reviewed-on: https://chromium-review.googlesource.com/1097206
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568052}

--- chrome/browser/ui/cocoa/apps/native_app_window_cocoa.mm
+++ chrome/browser/ui/cocoa/apps/native_app_window_cocoa.mm
@@ -569,7 +569,7 @@ void NativeAppWindowCocoa::HandleKeyboardEvent(
       event.GetType() == content::NativeWebKeyboardEvent::kChar) {
     return;
   }
-  [window() redispatchKeyEvent:event.os_event];
+  [[window() commandDispatcher] redispatchKeyEvent:event.os_event];
 }
 
 void NativeAppWindowCocoa::UpdateDraggableRegionViews() {
--- ui/views/cocoa/native_widget_mac_nswindow.mm
+++ ui/views/cocoa/native_widget_mac_nswindow.mm
@@ -190,8 +190,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   commandHandler_.reset([commandHandler retain]);
 }
 
-- (BOOL)redispatchKeyEvent:(NSEvent*)event {
-  return [commandDispatcher_ redispatchKeyEvent:event];
+- (CommandDispatcher*)commandDispatcher {
+  return commandDispatcher_.get();
 }
 
 - (BOOL)defaultPerformKeyEquivalent:(NSEvent*)event {
