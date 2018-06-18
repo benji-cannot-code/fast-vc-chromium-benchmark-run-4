44642906ffc1a6d70a72ced5bc4dc0d476323fe3
Oscar Johansson <oscarj@opera.com>
2018-06-18 08:04:58 +0000

    Rename kDialogWidth constants to make them unique
    When building with Jumbo, kDialog in
    webui/signin/signin_email_confirmation_dialog.cc and
    views/webshare/webshare_target_picker_view.cc conflict.
    This commits solves the issue by renaming the constant
    to something more file specific.
    
    Bug: 773275
    Change-Id: Iea327ab1948297fac97d3e7aaab9cb27a3631a30
    Reviewed-on: https://chromium-review.googlesource.com/1102474
    Commit-Queue: Oscar Johansson <oscarj@opera.com>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567945}

--- chrome/browser/ui/views/webshare/webshare_target_picker_view.cc
+++ chrome/browser/ui/views/webshare/webshare_target_picker_view.cc
@@ -23,9 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-int kDialogWidth = 500;
-int kDialogHeight = 400;
-
+int kWebShareTargetPickerDialogWidth = 500;
+int kWebShareTargetPickerDialogHeight = 400;
 }
 
 // Supplies data to the table view.
@@ -123,7 +122,8 @@ WebShareTargetPickerView::~WebShareTargetPickerView() {
 }
 
 gfx::Size WebShareTargetPickerView::CalculatePreferredSize() const {
-  return gfx::Size(kDialogWidth, kDialogHeight);
+  return gfx::Size(kWebShareTargetPickerDialogWidth,
+                   kWebShareTargetPickerDialogHeight);
 }
 
 ui::ModalType WebShareTargetPickerView::GetModalType() const {
