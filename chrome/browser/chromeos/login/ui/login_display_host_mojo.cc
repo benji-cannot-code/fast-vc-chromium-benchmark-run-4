e6c801f7408613f3dc5982c181ced6861cdb8de1
Sarah Hu <xiaoyinh@chromium.org>
2018-06-19 22:38:47 +0000

    Display version info in views login screen and hide it in gaia dialog
    Bug: 840013
    Change-Id: I1e1e8a21144d2b1cdf62b40b33f7b16f8d157d98
    Reviewed-on: https://chromium-review.googlesource.com/1106707
    Commit-Queue: Xiaoyin Hu <xiaoyinh@chromium.org>
    Reviewed-by: Jacob Dufault <jdufault@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568622}

--- chrome/browser/chromeos/login/ui/login_display_host_mojo.cc
+++ chrome/browser/chromeos/login/ui/login_display_host_mojo.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/threading/thread_task_runner_handle.h"
 #include "chrome/browser/chromeos/login/existing_user_controller.h"
+#include "chrome/browser/chromeos/login/mojo_version_info_dispatcher.h"
 #include "chrome/browser/chromeos/login/screens/chrome_user_selection_screen.h"
 #include "chrome/browser/chromeos/login/screens/gaia_view.h"
 #include "chrome/browser/chromeos/login/ui/login_display.h"
@@ -37,6 +38,7 @@ LoginDisplayHostMojo::LoginDisplayHostMojo()
       user_board_view_mojo_(std::make_unique<UserBoardViewMojo>()),
       user_selection_screen_(
           std::make_unique<ChromeUserSelectionScreen>(kLoginDisplay)),
+      version_info_updater_(std::make_unique<MojoVersionInfoDispatcher>()),
       weak_factory_(this) {
   user_selection_screen_->SetView(user_board_view_mojo_.get());
 
@@ -184,6 +186,9 @@ void LoginDisplayHostMojo::OnStartSignInScreen(
   user_selection_screen_->InitEasyUnlock();
 
   kiosk_updater_.SendKioskApps();
+
+  // Start to request version info.
+  version_info_updater_->StartUpdate();
 }
 
 void LoginDisplayHostMojo::OnPreferencesChanged() {
