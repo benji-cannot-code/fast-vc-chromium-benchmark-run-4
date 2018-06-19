3d6b7773283c2212dfe693b6cebfe65fe5f86a28
Francois Doray <fdoray@chromium.org>
2018-06-19 15:04:54 +0000

    RC: Add LifecycleUnit::Unfreeze().
    This will be used to periodically unfreeze LifecycleUnits, to allow
    them to update their state.
    
    Bug: 775644
    Change-Id: I498930c8a555ddf3581099e61972aaeac6a36d91
    Reviewed-on: https://chromium-review.googlesource.com/1099973
    Commit-Queue: François Doray <fdoray@chromium.org>
    Reviewed-by: Fadi Meawad <fmeawad@chromium.org>
    Reviewed-by: Sébastien Marchand <sebmarchand@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568451}

--- chrome/browser/resource_coordinator/test_lifecycle_unit.cc
+++ chrome/browser/resource_coordinator/test_lifecycle_unit.cc
@@ -53,10 +53,6 @@ bool TestLifecycleUnit::Load() {
   return false;
 }
 
-bool TestLifecycleUnit::Freeze() {
-  return false;
-}
-
 int TestLifecycleUnit::GetEstimatedMemoryFreedOnDiscardKB() const {
   return 0;
 }
@@ -72,6 +68,14 @@ bool TestLifecycleUnit::CanFreeze(DecisionDetails* decision_details) const {
 bool TestLifecycleUnit::CanDiscard(DiscardReason reason,
                                    DecisionDetails* decision_details) const {
   return can_discard_;
+}
+
+bool TestLifecycleUnit::Freeze() {
+  return false;
+}
+
+bool TestLifecycleUnit::Unfreeze() {
+  return false;
 }
 
 bool TestLifecycleUnit::Discard(DiscardReason discard_reason) {
