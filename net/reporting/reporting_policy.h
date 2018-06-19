02a78c9e6767363cdd9f5ec763e7db06b7e53ea2
Douglas Creager <dcreager@chromium.org>
2018-06-19 16:39:32 +0000

    Reporting: Verify NetworkService ignores headers
    The NetworkService currently ignores all Reporting headers, since we
    haven't implemented the BACKGROUND_SYNC permissions check in a way that
    works across service/process boundaries.  This adds a browser test that
    verifies this (and while we're at it, verifies that we _don't_ ignore
    Reporting headers when we're using the legacy non-service network
    stack).
    
    Bug: 845559
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester;luci.chromium.try:linux_mojo;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I3e7c1ab446e067e7f8af6fd80d3abc9b2d5187ad
    Reviewed-on: https://chromium-review.googlesource.com/1067465
    Commit-Queue: Douglas Creager <dcreager@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568488}

--- net/reporting/reporting_policy.h
+++ net/reporting/reporting_policy.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NET_REPORTING_REPORTING_POLICY_H_
 #define NET_REPORTING_REPORTING_POLICY_H_
 
+#include <memory>
+
 #include "base/time/time.h"
 #include "net/base/backoff_entry.h"
 #include "net/base/net_export.h"
@@ -15,6 +17,13 @@ namespace net {
 // Various policy knobs for the Reporting system.
 struct NET_EXPORT ReportingPolicy {
   // Provides a reasonable default for use in a browser embedder.
+  static std::unique_ptr<ReportingPolicy> Create();
+
+  // Lets you override the default policy returned by |Create|.  Use this in
+  // browser tests, where there isn't any other way to pass in a specific test
+  // policy to use.
+  static void UsePolicyForTesting(const ReportingPolicy& policy);
+
   ReportingPolicy();
   ReportingPolicy(const ReportingPolicy& other);
   ~ReportingPolicy();
