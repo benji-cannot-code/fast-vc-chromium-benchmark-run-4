17fa542efbdf5309e41c55c70ea989706619b511
Allen Vicencio <allenvic@chromium.org>
2018-06-19 17:05:00 +0000

    Add ReadDirectory duration metrics
    Adds metrics for duration of ReadDirectory
    
    Bug: 757625
    Change-Id: I03a30296a01db87fdc2c1133c512385d6a6bc967
    Reviewed-on: https://chromium-review.googlesource.com/1099692
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Commit-Queue: Allen Vicencio <allenvic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568501}

--- chrome/browser/chromeos/smb_client/smb_file_system.h
+++ chrome/browser/chromeos/smb_client/smb_file_system.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/linked_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
+#include "base/timer/elapsed_timer.h"
 #include "chrome/browser/chromeos/file_system_provider/abort_callback.h"
 #include "chrome/browser/chromeos/file_system_provider/provided_file_system_info.h"
 #include "chrome/browser/chromeos/file_system_provider/provided_file_system_interface.h"
@@ -199,6 +200,7 @@ class SmbFileSystem : public file_system_provider::ProvidedFileSystemInterface,
 
   void HandleRequestReadDirectoryCallback(
       storage::AsyncFileUtil::ReadDirectoryCallback callback,
+      const base::ElapsedTimer& metrics_timer,
       smbprovider::ErrorType error,
       const smbprovider::DirectoryEntryListProto& entries) const;
 
