bca5e72e0dcc1bea69903840daf50a861e5f42ab
Boris Sazonov <bsazonov@chromium.org>
2018-06-19 08:47:08 +0000

    [Android] Implement StateListDrawableBuilder
    This CL adds StateListDrawableBuilder, a helper class that simplifies
    StateListDrawable and AnimatedListDrawable creation.
    
    Bug: 814728
    Change-Id: If2f07e1dab0cd7f5a6d46c81e37f4153b6259caa
    Reviewed-on: https://chromium-review.googlesource.com/1101325
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568374}

---
+++ ui/android/junit/src/org/chromium/ui/shadows/ShadowAppCompatResources.java
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.ui.shadows;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.content.res.AppCompatResources;
+
+import org.robolectric.annotation.Implementation;
+import org.robolectric.annotation.Implements;
+import org.robolectric.shadows.ShadowDrawable;
+
+@Implements(AppCompatResources.class)
+public class ShadowAppCompatResources {
+    @Implementation
+    @Nullable
+    public static Drawable getDrawable(@NonNull Context context, @DrawableRes int resId) {
+        return ShadowDrawable.createFromResourceId(resId);
+    }
+}
