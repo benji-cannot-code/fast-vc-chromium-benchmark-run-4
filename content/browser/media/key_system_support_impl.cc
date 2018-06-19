73710c35351551e0afaa1084a11d77587a276d04
Xiaohan Wang <xhwang@chromium.org>
2018-06-19 02:48:18 +0000

    media: Add kHardwareSecureDecryption feature
    For now this is used as the flag to enable hardware secure decryption
    support on Windows while it's still under development.
    
    Added a TODO to apply this flag to Android and ChromeOS where hardware
    secure decryption is already supported.
    
    Bug: 848532
    Test: No functionality change.
    Change-Id: Ie263008ace5f8da5da1ab94acf22485046a79b7d
    Reviewed-on: https://chromium-review.googlesource.com/1103283
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568316}

--- content/browser/media/key_system_support_impl.cc
+++ content/browser/media/key_system_support_impl.cc
@@ -8,12 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/containers/flat_set.h"
+#include "base/feature_list.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_functions.h"
 #include "content/public/browser/cdm_registry.h"
 #include "content/public/common/cdm_info.h"
 #include "media/base/key_system_names.h"
 #include "media/base/key_systems.h"
+#include "media/base/media_switches.h"
 #include "mojo/public/cpp/bindings/strong_binding.h"
 
 namespace content {
@@ -78,7 +80,11 @@ void KeySystemSupportImpl::IsKeySystemSupported(
   capability->encryption_schemes =
       std::vector<media::EncryptionMode>(schemes.begin(), schemes.end());
 
-  // TODO(xhwang): Populate hw_secure* fields on Windows.
+  if (base::FeatureList::IsEnabled(media::kHardwareSecureDecryption)) {
+    // TODO(xhwang): Call into GetContentClient()->browser() to get key system
+    // specific hardware secure decryption capability on Windows.
+    NOTIMPLEMENTED();
+  }
 
   // Temporary session is always supported.
   // TODO(xhwang): Populate this from CdmInfo.
