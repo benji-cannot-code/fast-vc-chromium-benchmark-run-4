6b3405f807b47becb95d9dce177274671b063916
Clark DuVall <cduvall@chromium.org>
2018-06-19 21:21:23 +0000

    Network Service: Implement OnCanEnablePrivacyMode in network delegate
    This matches the implementation in ChromeNetworkDelegate.
    
    Bug: 789632
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Change-Id: I68c00d635fd1eb527aa2e80851acf8a3fd7325af
    Reviewed-on: https://chromium-review.googlesource.com/1104937
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568606}

--- services/network/network_service_network_delegate.h
+++ services/network/network_service_network_delegate.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SERVICES_NETWORK_NETWORK_SERVICE_NETWORK_DELEGATE_H_
 #define SERVICES_NETWORK_NETWORK_SERVICE_NETWORK_DELEGATE_H_
 
+#include "base/component_export.h"
 #include "base/macros.h"
 #include "net/base/network_delegate_impl.h"
 
@@ -13,7 +14,8 @@ namespace network {
 
 class NetworkContext;
 
-class NetworkServiceNetworkDelegate : public net::NetworkDelegateImpl {
+class COMPONENT_EXPORT(NETWORK_SERVICE) NetworkServiceNetworkDelegate
+    : public net::NetworkDelegateImpl {
  public:
   // |network_context| is guaranteed to outlive this class.
   explicit NetworkServiceNetworkDelegate(NetworkContext* network_context);
@@ -28,6 +30,8 @@ class NetworkServiceNetworkDelegate : public net::NetworkDelegateImpl {
   bool OnCanAccessFile(const net::URLRequest& request,
                        const base::FilePath& original_path,
                        const base::FilePath& absolute_path) const override;
+  bool OnCanEnablePrivacyMode(const GURL& url,
+                              const GURL& site_for_cookies) const override;
 
   NetworkContext* network_context_;
 
