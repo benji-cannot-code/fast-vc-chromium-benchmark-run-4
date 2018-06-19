164ceff4c8a054a76d74b7d92b88c099cafe6c3b
Ken Rockot <rockot@chromium.org>
2018-06-19 01:30:09 +0000

    Mojo bindings: Fix data race in Connector
    Fixes a data race on the boolean |error_| field. This is almost
    certainly innocuous in practice but is technically UB, and TSan
    complains.
    
    Bug: 850091
    Change-Id: Icc6bfbc0a2e2bda6a87c455856ff5f3153568a02
    Reviewed-on: https://chromium-review.googlesource.com/1105340
    Reviewed-by: Jay Civelli <jcivelli@chromium.org>
    Commit-Queue: Ken Rockot <rockot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568283}

--- mojo/public/cpp/bindings/connector.h
+++ mojo/public/cpp/bindings/connector.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MOJO_PUBLIC_CPP_BINDINGS_CONNECTOR_H_
 #define MOJO_PUBLIC_CPP_BINDINGS_CONNECTOR_H_
 
+#include <atomic>
 #include <memory>
 #include <utility>
 
@@ -235,7 +236,7 @@ class MOJO_CPP_BINDINGS_EXPORT Connector : public MessageReceiver {
   std::unique_ptr<SimpleWatcher> handle_watcher_;
   base::Optional<HandleSignalTracker> peer_remoteness_tracker_;
 
-  bool error_ = false;
+  std::atomic<bool> error_;
   bool drop_writes_ = false;
   bool enforce_errors_from_incoming_receiver_ = true;
 
