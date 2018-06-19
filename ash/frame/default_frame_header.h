94694a40d4a1165338f23eb1a3bda3fc8d0a09a1
Mitsuru Oshima <oshima@chromium.org>
2018-06-19 16:08:14 +0000

    Animate frame color change
    ARC++ app may update the color programatically, which
    can result in noticable flashing.
    
    This will make the color change transition smooth by
    animating the color from currently used color".
    
    TBR=wutao@chromium.org
    BUG=b/88534690
    TEST=manual. also covered by unit test
    
    Change-Id: Ia15976cb1ec425ddb4e23ec6a253667fa6f774d9
    Reviewed-on: https://chromium-review.googlesource.com/1101504
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568474}

--- ash/frame/default_frame_header.h
+++ ash/frame/default_frame_header.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "ash/frame/frame_header.h"
+#include "ash/public/cpp/ash_constants.h"
 #include "base/compiler_specific.h"  // override
 #include "base/gtest_prod_util.h"
 #include "base/macros.h"
@@ -28,8 +29,12 @@ class ASH_EXPORT DefaultFrameHeader : public FrameHeader {
 
   void SetThemeColor(SkColor theme_color);
 
-  SkColor active_frame_color_for_testing() { return active_frame_color_; }
-  SkColor inactive_frame_color_for_testing() { return inactive_frame_color_; }
+  SkColor active_frame_color_for_testing() {
+    return active_frame_color_.target_color();
+  }
+  SkColor inactive_frame_color_for_testing() {
+    return inactive_frame_color_.target_color();
+  }
 
  protected:
   // FrameHeader:
@@ -48,8 +53,34 @@ class ASH_EXPORT DefaultFrameHeader : public FrameHeader {
   void SetFrameColorsImpl(SkColor active_frame_color,
                           SkColor inactive_frame_color);
 
-  SkColor active_frame_color_;
-  SkColor inactive_frame_color_;
+  gfx::SlideAnimation* GetAnimationForActiveFrameColorForTest();
+  SkColor GetActiveFrameColorForPaintForTest();
+
+  // A utility class to animate color value.
+  class ColorAnimator {
+   public:
+    explicit ColorAnimator(gfx::AnimationDelegate* delegate);
+    ~ColorAnimator();
+
+    void SetTargetColor(SkColor target);
+    SkColor target_color() const { return target_color_; };
+    SkColor GetCurrentColor();
+    float get_value() const { return animation_.GetCurrentValue(); }
+
+    gfx::SlideAnimation* animation() { return &animation_; }
+
+   private:
+    gfx::SlideAnimation animation_;
+    SkColor start_color_ = kDefaultFrameColor;
+    SkColor target_color_ = kDefaultFrameColor;
+    ;
+    SkColor current_color_ = kDefaultFrameColor;
+
+    DISALLOW_COPY_AND_ASSIGN(ColorAnimator);
+  };
+
+  ColorAnimator active_frame_color_;
+  ColorAnimator inactive_frame_color_;
 
   int width_in_pixels_ = -1;
 
