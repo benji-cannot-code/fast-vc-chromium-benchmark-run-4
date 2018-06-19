98b6e21cbcde1f594d033b1dce2015c4e6deebd0
Sorin Jianu <sorin@chromium.org>
2018-06-19 14:42:55 +0000

    Provide a component updater command line argument to control the initial delay for update checks.
    Bug: 854029
    Change-Id: I82f2f12bcca6b5af48127307f1662324bdcda9ce
    Reviewed-on: https://chromium-review.googlesource.com/1105600
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568443}

--- components/component_updater/component_updater_command_line_config_policy.cc
+++ components/component_updater/component_updater_command_line_config_policy.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/stl_util.h"
+#include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
 #include "base/strings/sys_string_conversions.h"
 #include "build/build_config.h"
@@ -37,6 +38,10 @@ const char kSwitchUrlSource[] = "url-source";
 
 // Disables differential updates.
 const char kSwitchDisableDeltaUpdates[] = "disable-delta-updates";
+
+// Configures the initial delay before the first component update check. The
+// value is in seconds.
+const char kInitialDelay[] = "initial-delay";
 
 #if defined(OS_WIN)
 // Disables background downloads.
@@ -92,6 +97,12 @@ ComponentUpdaterCommandLineConfigPolicy::
     url_source_override_ = GURL(switch_url_source);
     DCHECK(url_source_override_.is_valid());
   }
+
+  const std::string initial_delay =
+      GetSwitchArgument(switch_values, kInitialDelay);
+  int initial_delay_seconds = 0;
+  if (base::StringToInt(initial_delay, &initial_delay_seconds))
+    initial_delay_ = initial_delay_seconds;
 }
 
 bool ComponentUpdaterCommandLineConfigPolicy::BackgroundDownloadsEnabled()
@@ -117,6 +128,10 @@ bool ComponentUpdaterCommandLineConfigPolicy::TestRequest() const {
 
 GURL ComponentUpdaterCommandLineConfigPolicy::UrlSourceOverride() const {
   return url_source_override_;
+}
+
+int ComponentUpdaterCommandLineConfigPolicy::InitialDelay() const {
+  return initial_delay_;
 }
 
 }  // namespace component_updater
