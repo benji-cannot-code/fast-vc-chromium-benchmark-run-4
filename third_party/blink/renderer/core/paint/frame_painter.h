c2a5a13d28b56be04fe8ae43be0299473d035dcd
Philip Rogers <pdr@chromium.org>
2018-06-19 00:20:25 +0000

    [RLS] Remove scrollbar painting from FramePainter
    This is all dead code.
    
    Bug: 823365
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I2b7e8281099435dc937ddf5f8e2e77749c5cbad7
    Reviewed-on: https://chromium-review.googlesource.com/1103779
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568252}

--- third_party/blink/renderer/core/paint/frame_painter.h
+++ third_party/blink/renderer/core/paint/frame_painter.h
@@ -16,7 +16,6 @@ class CullRect;
 class GraphicsContext;
 class IntRect;
 class LocalFrameView;
-class Scrollbar;
 
 class FramePainter {
   STACK_ALLOCATED();
@@ -26,13 +25,9 @@ class FramePainter {
       : frame_view_(&frame_view) {}
 
   void Paint(GraphicsContext&, const GlobalPaintFlags, const CullRect&);
-  void PaintScrollbars(GraphicsContext&, const IntRect&);
   void PaintContents(GraphicsContext&, const GlobalPaintFlags, const IntRect&);
-  void PaintScrollCorner(GraphicsContext&, const IntRect& corner_rect);
 
  private:
-  void PaintScrollbar(GraphicsContext&, Scrollbar&, const IntRect&);
-
   const LocalFrameView& GetFrameView();
 
   Member<const LocalFrameView> frame_view_;
