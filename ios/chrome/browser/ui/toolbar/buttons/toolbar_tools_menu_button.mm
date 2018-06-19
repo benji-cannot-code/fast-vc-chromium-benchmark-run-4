84b3a30f1e72b4e09643d41d371a48924d0fd5cb
Gauthier Ambard <gambard@chromium.org>
2018-06-19 08:14:09 +0000

    Fix spotlight state of the tools menu button
    The spotlighted view wasn't created for the tools menu button.
    This bug was introduced in the CL moving the creation of the spotlight
    view from a lazy accessor to a protected method.
    
    Bug: 850513
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I9bdf63ab0ed7095203a0dad2ce34642912814710
    Reviewed-on: https://chromium-review.googlesource.com/1104690
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568372}

--- ios/chrome/browser/ui/toolbar/buttons/toolbar_tools_menu_button.mm
+++ ios/chrome/browser/ui/toolbar/buttons/toolbar_tools_menu_button.mm
@@ -100,6 +100,8 @@ const CGFloat kStrokeEndAtApogee = 1;
                 forState:UIControlStateNormal];
       [self setTintColor:toolbar::HighlighButtonTint(style_)
                 forState:UIControlStateHighlighted];
+    } else {
+      [self configureSpotlightView];
     }
   }
   return self;
