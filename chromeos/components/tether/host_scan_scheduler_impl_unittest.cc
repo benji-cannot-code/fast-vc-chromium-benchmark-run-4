44c91e36f2472fc12a116da315ff1d714ce79bf5
Ryan Hansberry <hansberry@chromium.org>
2018-06-19 17:33:12 +0000

    [Cros Multidevice] tether::HostScanScheduler: Remove restrictions on host scans if SecureChannel API is present.
    This conditionally (behind the chromeos::features::kMultiDeviceApi flag) removes the
    following restrictions introduced by the following CLs:
    1) "Stop scans when device is locked": https://chromium-review.googlesource.com/c/chromium/src/+/940609
    2) "Do not perform host scans while the device is locked": https://chromium-review.googlesource.com/c/chromium/src/+/1045254
    3) "Delay host scans slightly after unlock": https://chromium-review.googlesource.com/c/chromium/src/+/941583
    
    These restricitons were necessary because Tether and Smart Lock used different BLE comminication stacks. Now that the
    SecureChannel API is integrated into Tether (the BLE communication scheme that Tether and Smart Lock share), these
    restrictions are no longer necessary.
    
    Bug: 824568, 752273, 817554
    Change-Id: Id63ba851c702502420ae42b99efcd5f18102aa34
    Reviewed-on: https://chromium-review.googlesource.com/1105618
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568513}

--- chromeos/components/tether/host_scan_scheduler_impl_unittest.cc
+++ chromeos/components/tether/host_scan_scheduler_impl_unittest.cc
@@ -10,10 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/run_loop.h"
 #include "base/test/metrics/histogram_tester.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/test/scoped_task_environment.h"
 #include "base/test/simple_test_clock.h"
 #include "base/test/test_simple_task_runner.h"
 #include "base/timer/mock_timer.h"
+#include "chromeos/chromeos_features.h"
 #include "chromeos/components/tether/fake_host_scanner.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/dbus/power_manager_client.h"
@@ -97,6 +99,10 @@ class HostScanSchedulerImplTest : public NetworkStateTest {
     DBusThreadManager::Shutdown();
   }
 
+  void SetMultiDeviceApiEnabled() {
+    scoped_feature_list_.InitAndEnableFeature(features::kMultiDeviceApi);
+  }
+
   void RequestScan() { host_scan_scheduler_->ScanRequested(); }
 
   // Disconnects the Ethernet network and manually sets the default network to
@@ -173,6 +179,8 @@ class HostScanSchedulerImplTest : public NetworkStateTest {
   std::unique_ptr<base::HistogramTester> histogram_tester_;
 
   std::unique_ptr<HostScanSchedulerImpl> host_scan_scheduler_;
+
+  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
 TEST_F(HostScanSchedulerImplTest, ScheduleScan) {
@@ -213,6 +221,30 @@ TEST_F(HostScanSchedulerImplTest, TestDeviceLockAndUnlock) {
   // Fire the timer; this should start the scan.
   mock_delay_scan_after_unlock_timer_->Fire();
   EXPECT_EQ(1u, fake_host_scanner_->num_scans_started());
+}
+
+TEST_F(HostScanSchedulerImplTest,
+       TestDeviceLockAndUnlock_MultiDeviceApiEnabled) {
+  SetMultiDeviceApiEnabled();
+
+  // Lock the screen. This should not trigger a scan.
+  SetScreenLockedState(true /* is_locked */);
+  EXPECT_EQ(0u, fake_host_scanner_->num_scans_started());
+  EXPECT_FALSE(mock_delay_scan_after_unlock_timer_->IsRunning());
+
+  // Try to start a scan. Even thought the screen is locked, this should cause a
+  // scan to be started.
+  host_scan_scheduler_->ScheduleScan();
+  EXPECT_EQ(1u, fake_host_scanner_->num_scans_started());
+  EXPECT_FALSE(mock_delay_scan_after_unlock_timer_->IsRunning());
+
+  fake_host_scanner_->StopScan();
+
+  // Unlock the screen. A new scan should have started, and the timer should be
+  // untouched.
+  SetScreenLockedState(false /* is_locked */);
+  EXPECT_EQ(2u, fake_host_scanner_->num_scans_started());
+  EXPECT_FALSE(mock_delay_scan_after_unlock_timer_->IsRunning());
 }
 
 TEST_F(HostScanSchedulerImplTest, ScanRequested) {
