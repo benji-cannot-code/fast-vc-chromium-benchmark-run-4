87bb9780de2691bbb086b65f7eda6102da04079e
Arthur Sonzogni <arthursonzogni@chromium.org>
2018-06-19 09:13:24 +0000

    Fix potentially flaky inspector-protocol tests.
    Update the blink layout test:
    http/tests/inspector-protocol/frameScheduledNavigation.js
    
    This CL is similar to:
     * https://chromium-review.googlesource.com/c/chromium/src/+/1071630
     * https://chromium-review.googlesource.com/c/chromium/src/+/1071791
    
    Depending on timing and the size of the main resource, new navigations
    initiated in <script> may starts before the document is fully loaded.
    In this case, frameStartedLoading is not fired because the load didn't
    stop in between.
    
    Bug: 831155
    Change-Id: I872b7ff9b332893c0b57f921f992e348276e9d42
    Reviewed-on: https://chromium-review.googlesource.com/1102681
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568376}

--- third_party/WebKit/LayoutTests/http/tests/inspector-protocol/page/frameScheduledNavigation.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector-protocol/page/frameScheduledNavigation.js
@@ -2,6 +2,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 (async function(testRunner) {
   var {page, session, dp} = await testRunner.startBlank('Tests frameScheduledNavigation events when navigation is initiated in JS followed by other navigations.');
 
+  let isLoading = false;
+  dp.Page.onFrameStartedLoading(() => isLoading = true);
+  dp.Page.onFrameStoppedLoading(() => isLoading = false);
+
   dp.Page.enable();
   session.evaluate(`
     var frame = document.createElement('iframe');
@@ -14,9 +18,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     testRunner.log('Scheduled navigation with delay ' + msg.params.delay +
                    ' and reason ' + msg.params.reason + ' to url ' +
                    msg.params.url.split('/').pop());
-    await dp.Page.onceFrameStartedLoading();
-    // This event should be received before the scheduled navigation is cleared.
-    testRunner.log('Started loading');
+
+    // This event should be received before the scheduled navigation is cleared,
+    // unless the frame was already loading.
+    if (!isLoading)
+      await dp.Page.onceFrameStartedLoading();
 
     await dp.Page.onceFrameClearedScheduledNavigation();
     testRunner.log('Cleared scheduled navigation');
