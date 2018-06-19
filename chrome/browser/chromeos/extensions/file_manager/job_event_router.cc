69ac5e2283139114d8709beecd9225d485ba5da0
Stuart Langley <slangley@chromium.org>
2018-06-19 23:26:41 +0000

    Use correct Threadhecker macros in chrome/browser/chromeos/extensions/file_manager/*
    ThreadChecker has a set of boilerplate macros that should be used so that it is
    compiled out correctly on non DCHECK enabled builds.
    
    No Logic Changed, No New Tests.
    
    Bug: 841659
    Change-Id: I93369ee67285ace07b630cd05b11b9e1d1cc9cfc
    Reviewed-on: https://chromium-review.googlesource.com/1107017
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Commit-Queue: Stuart Langley <slangley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568638}

--- chrome/browser/chromeos/extensions/file_manager/job_event_router.cc
+++ chrome/browser/chromeos/extensions/file_manager/job_event_router.cc
@@ -33,7 +33,7 @@ void JobEventRouter::OnJobAdded(const drive::JobInfo& job_info) {
 }
 
 void JobEventRouter::OnJobUpdated(const drive::JobInfo& job_info) {
-  DCHECK(thread_checker_.CalledOnValidThread());
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
   if (!drive::IsActiveFileTransferJobInfo(job_info))
     return;
 
@@ -48,7 +48,7 @@ void JobEventRouter::OnJobUpdated(const drive::JobInfo& job_info) {
 
 void JobEventRouter::OnJobDone(const drive::JobInfo& job_info,
                                drive::FileError error) {
-  DCHECK(thread_checker_.CalledOnValidThread());
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
   if (!drive::IsActiveFileTransferJobInfo(job_info))
     return;
 
