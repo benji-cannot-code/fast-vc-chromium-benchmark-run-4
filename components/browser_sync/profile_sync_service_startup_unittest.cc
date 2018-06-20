81e90642130d66ccdaadb0bcd1f43747ca11c17b
Marc Treib <treib@chromium.org>
2018-06-20 13:25:03 +0000

    Make ProfileSyncServiceStartupCrosTest.StartCrosNoCredentials match reality
    Turns out the test was completely out of date and only passing by
    accident.
    While we're here, also replace a few EXPECT_CALLs with ON_CALLs.
    
    Bug: none
    Change-Id: Ie2250a7ea58332e6bf08b6df80e33b72eb5a0018
    Reviewed-on: https://chromium-review.googlesource.com/1107811
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568817}

--- components/browser_sync/profile_sync_service_startup_unittest.cc
+++ components/browser_sync/profile_sync_service_startup_unittest.cc
@@ -160,8 +160,8 @@ TEST_F(ProfileSyncServiceStartupTest, StartFirstTime) {
   SetUpFakeSyncEngine();
   DataTypeManagerMock* data_type_manager = SetUpDataTypeManagerMock();
   EXPECT_CALL(*data_type_manager, Configure(_, _)).Times(0);
-  EXPECT_CALL(*data_type_manager, state())
-      .WillRepeatedly(Return(DataTypeManager::STOPPED));
+  ON_CALL(*data_type_manager, state())
+      .WillByDefault(Return(DataTypeManager::STOPPED));
 
   // Should not actually start, rather just clean things up and wait
   // to be enabled.
@@ -204,8 +204,8 @@ TEST_F(ProfileSyncServiceStartupTest, StartFirstTime) {
   // Releasing the sync blocker will let ProfileSyncService configure the
   // DataTypeManager.
   EXPECT_CALL(*data_type_manager, Configure(_, _));
-  EXPECT_CALL(*data_type_manager, state())
-      .WillRepeatedly(Return(DataTypeManager::CONFIGURED));
+  ON_CALL(*data_type_manager, state())
+      .WillByDefault(Return(DataTypeManager::CONFIGURED));
 
   // Simulate the UI telling sync it has finished setting up.
   sync_blocker.reset();
@@ -229,8 +229,8 @@ TEST_F(ProfileSyncServiceStartupTest, StartNoCredentials) {
   SetUpFakeSyncEngine();
   DataTypeManagerMock* data_type_manager = SetUpDataTypeManagerMock();
   EXPECT_CALL(*data_type_manager, Configure(_, _));
-  EXPECT_CALL(*data_type_manager, state())
-      .WillRepeatedly(Return(DataTypeManager::CONFIGURED));
+  ON_CALL(*data_type_manager, state())
+      .WillByDefault(Return(DataTypeManager::CONFIGURED));
 
   sync_service()->Initialize();
 
@@ -269,15 +269,17 @@ TEST_F(ProfileSyncServiceStartupCrosTest, StartCrosNoCredentials) {
   pref_service()->ClearPref(syncer::prefs::kSyncFirstSetupComplete);
 
   SetUpFakeSyncEngine();
-  SetUpDataTypeManagerMock();
+  DataTypeManagerMock* data_type_manager = SetUpDataTypeManagerMock();
 
+  EXPECT_CALL(*data_type_manager, Configure(_, _));
   sync_service()->Initialize();
-  // Sync should not start because there are no tokens yet.
-  EXPECT_FALSE(sync_service()->IsSyncActive());
-  sync_service()->SetFirstSetupComplete();
+  ON_CALL(*data_type_manager, state())
+      .WillByDefault(Return(DataTypeManager::CONFIGURED));
 
-  // Sync should not start because there are still no tokens.
-  EXPECT_FALSE(sync_service()->IsSyncActive());
+  // Sync should start up the engine, even though there is no refresh token yet.
+  EXPECT_TRUE(sync_service()->IsSyncActive());
+  // Since we're in AUTO_START mode, FirstSetupComplete gets set automatically.
+  EXPECT_TRUE(sync_service()->IsFirstSetupComplete());
 }
 
 TEST_F(ProfileSyncServiceStartupCrosTest, StartFirstTime) {
@@ -285,8 +287,8 @@ TEST_F(ProfileSyncServiceStartupCrosTest, StartFirstTime) {
   DataTypeManagerMock* data_type_manager = SetUpDataTypeManagerMock();
   pref_service()->ClearPref(syncer::prefs::kSyncFirstSetupComplete);
   EXPECT_CALL(*data_type_manager, Configure(_, _));
-  EXPECT_CALL(*data_type_manager, state())
-      .WillRepeatedly(Return(DataTypeManager::CONFIGURED));
+  ON_CALL(*data_type_manager, state())
+      .WillByDefault(Return(DataTypeManager::CONFIGURED));
 
   // The primary account is already populated, all that's left to do is provide
   // a refresh token.
@@ -303,8 +305,8 @@ TEST_F(ProfileSyncServiceStartupTest, StartNormal) {
   SetUpFakeSyncEngine();
   DataTypeManagerMock* data_type_manager = SetUpDataTypeManagerMock();
   EXPECT_CALL(*data_type_manager, Configure(_, _));
-  EXPECT_CALL(*data_type_manager, state())
-      .WillRepeatedly(Return(DataTypeManager::CONFIGURED));
+  ON_CALL(*data_type_manager, state())
+      .WillByDefault(Return(DataTypeManager::CONFIGURED));
   ON_CALL(*data_type_manager, IsNigoriEnabled()).WillByDefault(Return(true));
 
   sync_service()->Initialize();
@@ -362,8 +364,8 @@ TEST_F(ProfileSyncServiceStartupTest, StartRecoverDatatypePrefs) {
   SetUpFakeSyncEngine();
   DataTypeManagerMock* data_type_manager = SetUpDataTypeManagerMock();
   EXPECT_CALL(*data_type_manager, Configure(_, _));
-  EXPECT_CALL(*data_type_manager, state())
-      .WillRepeatedly(Return(DataTypeManager::CONFIGURED));
+  ON_CALL(*data_type_manager, state())
+      .WillByDefault(Return(DataTypeManager::CONFIGURED));
   ON_CALL(*data_type_manager, IsNigoriEnabled()).WillByDefault(Return(true));
 
   sync_service()->Initialize();
@@ -385,8 +387,8 @@ TEST_F(ProfileSyncServiceStartupTest, StartDontRecoverDatatypePrefs) {
   SetUpFakeSyncEngine();
   DataTypeManagerMock* data_type_manager = SetUpDataTypeManagerMock();
   EXPECT_CALL(*data_type_manager, Configure(_, _));
-  EXPECT_CALL(*data_type_manager, state())
-      .WillRepeatedly(Return(DataTypeManager::CONFIGURED));
+  ON_CALL(*data_type_manager, state())
+      .WillByDefault(Return(DataTypeManager::CONFIGURED));
   ON_CALL(*data_type_manager, IsNigoriEnabled()).WillByDefault(Return(true));
   sync_service()->Initialize();
 
@@ -415,8 +417,8 @@ TEST_F(ProfileSyncServiceStartupTest, SwitchManaged) {
   SetUpFakeSyncEngine();
   DataTypeManagerMock* data_type_manager = SetUpDataTypeManagerMock();
   EXPECT_CALL(*data_type_manager, Configure(_, _));
-  EXPECT_CALL(*data_type_manager, state())
-      .WillRepeatedly(Return(DataTypeManager::CONFIGURED));
+  ON_CALL(*data_type_manager, state())
+      .WillByDefault(Return(DataTypeManager::CONFIGURED));
   ON_CALL(*data_type_manager, IsNigoriEnabled()).WillByDefault(Return(true));
   sync_service()->Initialize();
   EXPECT_TRUE(sync_service()->IsEngineInitialized());
