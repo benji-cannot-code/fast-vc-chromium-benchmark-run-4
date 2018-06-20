1246e0f00d666599a1b2248504823e177de2f644
Gauthier Ambard <gambard@chromium.org>
2018-06-20 08:48:46 +0000

    Change vertical alignment of the collapsed toolbar
    This CL changes the vertical constraints of the collapsed toolbar to
    have the location bar displayed in the middle of the collapsed toolbar
    by changing the constraint to position its bottom anchor.
    
    Bug: 836817
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I35ad32681a820b8480ecec76e825defc4c0aa2ef
    Reviewed-on: https://chromium-review.googlesource.com/1107061
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568768}

--- ios/chrome/browser/ui/toolbar/adaptive/primary_toolbar_view_controller.mm
+++ ios/chrome/browser/ui/toolbar/adaptive/primary_toolbar_view_controller.mm
@@ -136,6 +136,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       (kAdaptiveToolbarHeight - 2 * kAdaptiveLocationBarVerticalMargin -
        kToolbarHeightFullscreen) *
           progress);
+  self.view.locationBarBottomConstraint.constant =
+      -AlignValueToPixel(kAdaptiveLocationBarVerticalMargin * progress);
   self.view.locationBarContainer.backgroundColor =
       [self.buttonFactory.toolbarConfiguration
           locationBarBackgroundColorWithVisibility:alphaValue];
