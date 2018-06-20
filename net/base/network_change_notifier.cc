d43cda0873989f7b5fdf486d21fd1acbca24c15b
Matt Menke <mmenke@chromium.org>
2018-06-20 12:53:03 +0000

    NetworkService: Hook up NetworkChangeNotifier's HistogramWatcher.
    Unfortunately, the old patch to set it up in IOThread is still needed,
    since the in-process NetworkService is leaked when the OOP network
    service is disabled.
    
    Bug=853211
    
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Change-Id: Ic216b2d83d703cff776a77aaea6a2244a9eca367
    Reviewed-on: https://chromium-review.googlesource.com/1105068
    Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568806}

--- net/base/network_change_notifier.cc
+++ net/base/network_change_notifier.cc
@@ -745,6 +745,7 @@ void NetworkChangeNotifier::NotifyDataReceived(const URLRequest& request,
 void NetworkChangeNotifier::InitHistogramWatcher() {
   if (!g_network_change_notifier)
     return;
+  DCHECK(!g_network_change_notifier->histogram_watcher_);
   g_network_change_notifier->histogram_watcher_.reset(new HistogramWatcher());
   g_network_change_notifier->histogram_watcher_->Init();
 }
@@ -753,6 +754,7 @@ void NetworkChangeNotifier::InitHistogramWatcher() {
 void NetworkChangeNotifier::ShutdownHistogramWatcher() {
   if (!g_network_change_notifier)
     return;
+  DCHECK(g_network_change_notifier->histogram_watcher_);
   g_network_change_notifier->histogram_watcher_.reset();
 }
 
