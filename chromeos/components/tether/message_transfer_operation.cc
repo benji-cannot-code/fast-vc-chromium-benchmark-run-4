50281db6db2673eaf00f755ebb18aa349a8f5154
Ryan Hansberry <hansberry@chromium.org>
2018-06-20 03:16:12 +0000

    [CrOS Multidevice] tether::MessageTransferOperation: use the correct Tether feature constant.
    This is the correct constant that is expected by host devices.
    
    Bug: 824568, 752273
    Change-Id: I7f29159de6eb66f92f4534cd558fe82027175914
    Reviewed-on: https://chromium-review.googlesource.com/1105621
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568713}

--- chromeos/components/tether/message_transfer_operation.cc
+++ chromeos/components/tether/message_transfer_operation.cc
@@ -19,6 +19,8 @@ namespace tether {
 
 namespace {
 
+const char kTetherFeature[] = "magic_tether";
+
 cryptauth::RemoteDeviceRefList RemoveDuplicatesFromVector(
     const cryptauth::RemoteDeviceRefList& remote_devices) {
   cryptauth::RemoteDeviceRefList updated_remote_devices;
@@ -149,7 +151,7 @@ void MessageTransferOperation::Initialize() {
               this, remote_device,
               secure_channel_client_->ListenForConnectionFromDevice(
                   remote_device, *device_sync_client_->GetLocalDeviceMetadata(),
-                  "tether", connection_priority_));
+                  kTetherFeature, connection_priority_));
     } else {
       connection_manager_->RegisterRemoteDevice(
           remote_device.GetDeviceId(), request_id_, connection_priority_);
