02feee87692e710bf650948adf63a9215a1c6166
Mohamed Amir Yosef <mamir@chromium.org>
2018-06-20 10:48:46 +0000

    [Sync::USS] Implement SyncedBookmarkTracker::HasLocalChanges()
    Bug: 516866
    Change-Id: Iee653b8470c8aabd7eb14ed81ddf48e6411c472f
    Reviewed-on: https://chromium-review.googlesource.com/1106345
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568786}

--- components/sync_bookmarks/synced_bookmark_tracker.cc
+++ components/sync_bookmarks/synced_bookmark_tracker.cc
@@ -38,6 +38,10 @@ SyncedBookmarkTracker::Entity::Entity(
 
 SyncedBookmarkTracker::Entity::~Entity() = default;
 
+bool SyncedBookmarkTracker::Entity::IsUnsynced() const {
+  return metadata_->sequence_number() > metadata_->acked_sequence_number();
+}
+
 bool SyncedBookmarkTracker::Entity::MatchesData(
     const syncer::EntityData& data) const {
   if (metadata_->is_deleted() || data.is_deleted()) {
@@ -54,10 +58,6 @@ bool SyncedBookmarkTracker::Entity::MatchesSpecificsHash(
   std::string hash;
   HashSpecifics(specifics, &hash);
   return hash == metadata_->specifics_hash();
-}
-
-bool SyncedBookmarkTracker::Entity::IsUnsynced() const {
-  return metadata_->sequence_number() > metadata_->acked_sequence_number();
 }
 
 SyncedBookmarkTracker::SyncedBookmarkTracker(
@@ -90,6 +90,8 @@ void SyncedBookmarkTracker::Add(const std::string& sync_id,
   metadata->set_server_id(sync_id);
   metadata->set_server_version(server_version);
   metadata->set_creation_time(syncer::TimeToProtoTime(creation_time));
+  metadata->set_sequence_number(0);
+  metadata->set_acked_sequence_number(0);
   HashSpecifics(specifics, metadata->mutable_specifics_hash());
   sync_id_to_entities_map_[sync_id] =
       std::make_unique<Entity>(bookmark_node, std::move(metadata));
@@ -114,6 +116,17 @@ void SyncedBookmarkTracker::Remove(const std::string& sync_id) {
   sync_id_to_entities_map_.erase(sync_id);
 }
 
+void SyncedBookmarkTracker::IncrementSequenceNumber(
+    const std::string& sync_id) {
+  Entity* entity = sync_id_to_entities_map_.find(sync_id)->second.get();
+  DCHECK(entity);
+  DCHECK(!entity->metadata()->is_deleted());
+  // TODO(crbug.com/516866): Update base hash specifics here if the entity is
+  // not already out of sync.
+  entity->metadata()->set_sequence_number(
+      entity->metadata()->sequence_number() + 1);
+}
+
 sync_pb::BookmarkModelMetadata
 SyncedBookmarkTracker::BuildBookmarkModelMetadata() const {
   sync_pb::BookmarkModelMetadata model_metadata;
@@ -131,7 +144,13 @@ SyncedBookmarkTracker::BuildBookmarkModelMetadata() const {
 }
 
 bool SyncedBookmarkTracker::HasLocalChanges() const {
-  NOTIMPLEMENTED();
+  for (const std::pair<const std::string, std::unique_ptr<Entity>>& pair :
+       sync_id_to_entities_map_) {
+    Entity* entity = pair.second.get();
+    if (entity->IsUnsynced()) {
+      return true;
+    }
+  }
   return false;
 }
 
