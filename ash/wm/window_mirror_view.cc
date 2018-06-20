76b40709e8ca138b53d25e480fd66452033ba8dd
Sammie Quon <sammiequon@google.com>
2018-06-20 23:54:01 +0000

    overview: Fix corrupt minimized mirror views after rotation.
    Test: manual
    Bug: 842394
    Change-Id: Iaf1bb8bb94da5b6d2e60009a21b02bde894f1326
    Reviewed-on: https://chromium-review.googlesource.com/1105196
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569092}

--- ash/wm/window_mirror_view.cc
+++ ash/wm/window_mirror_view.cc
@@ -45,6 +45,13 @@ WindowMirrorView::~WindowMirrorView() {
     target_->ClearProperty(aura::client::kMirroringEnabledKey);
 }
 
+void WindowMirrorView::RecreateMirrorLayers() {
+  if (layer_owner_)
+    layer_owner_.reset();
+
+  InitLayerOwner();
+}
+
 gfx::Size WindowMirrorView::CalculatePreferredSize() const {
   return GetClientAreaBounds().size();
 }
