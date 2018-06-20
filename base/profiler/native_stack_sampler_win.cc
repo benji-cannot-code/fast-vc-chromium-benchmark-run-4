555d9c0d8e496bff55691096cdeda2f3916a4e88
Xi Cheng <chengx@chromium.org>
2018-06-20 16:24:34 +0000

    Format windows.h inclusion
    Change-Id: Ib57fa2a98d6270721df67ff890b7961d09e209b9
    Reviewed-on: https://chromium-review.googlesource.com/1107386
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Commit-Queue: Xi Cheng <chengx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568865}

--- base/profiler/native_stack_sampler_win.cc
+++ base/profiler/native_stack_sampler_win.cc
@@ -5,8 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/profiler/native_stack_sampler.h"
 
-#include <objbase.h>
 #include <windows.h>
+
+#include <objbase.h>
 #include <stddef.h>
 #include <winternl.h>
 
