4a3d72472d25c0cdfd712d203bf723c0796cf438
Hayato Ito <hayato@chromium.org>
2018-06-20 09:46:03 +0000

    Move trustedtypes from core/dom/trustedtypes to core/trustedtypes
    A part of crbug.com/738794. trustedtypes are not related to DOM
    Standard.
    
    BUG=777761,738794
    
    Change-Id: I56906a4ca41a3a3318b26cc52579661ed25f3c50
    Reviewed-on: https://chromium-review.googlesource.com/1107519
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Hayato Ito <hayato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568777}

--- third_party/blink/renderer/core/trustedtypes/trusted_script_url.h
+++ third_party/blink/renderer/core/trustedtypes/trusted_script_url.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef THIRD_PARTY_BLINK_RENDERER_CORE_DOM_TRUSTEDTYPES_TRUSTED_SCRIPT_URL_H_
-#define THIRD_PARTY_BLINK_RENDERER_CORE_DOM_TRUSTEDTYPES_TRUSTED_SCRIPT_URL_H_
+#ifndef THIRD_PARTY_BLINK_RENDERER_CORE_TRUSTEDTYPES_TRUSTED_SCRIPT_URL_H_
+#define THIRD_PARTY_BLINK_RENDERER_CORE_TRUSTEDTYPES_TRUSTED_SCRIPT_URL_H_
 
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
@@ -36,4 +36,4 @@ class CORE_EXPORT TrustedScriptURL final : public ScriptWrappable {
 
 }  // namespace blink
 
-#endif  // THIRD_PARTY_BLINK_RENDERER_CORE_DOM_TRUSTEDTYPES_TRUSTED_SCRIPT_URL_H_
+#endif  // THIRD_PARTY_BLINK_RENDERER_CORE_TRUSTEDTYPES_TRUSTED_SCRIPT_URL_H_
