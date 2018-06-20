c1569612143d3f3fe3df130586718cf2b86783e6
Nigel Tao <nigeltao@chromium.org>
2018-06-20 00:41:38 +0000

    Avoid "duplicate FILE_MANAGER_HOST" warnings
    Also fix a "missing semicolon" JS presubmit failure.
    
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: Ib607e0e25c74ff85cc9d5ba2f23c596e0f509723
    Reviewed-on: https://chromium-review.googlesource.com/1100660
    Commit-Queue: Nigel Tao <nigeltao@chromium.org>
    Reviewed-by: Sasha Morrissey <sashab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568667}

--- ui/file_manager/file_manager/foreground/js/metadata/metadata_dispatcher.js
+++ ui/file_manager/file_manager/foreground/js/metadata/metadata_dispatcher.js
@@ -5,16 +5,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /**
  * Protocol + host parts of extension URL.
+ *
+ * The __FILE_NAME suffix is because the same string constant is used in
+ * multiple JS files, and JavaScript doesn't have C's #define mechanism (which
+ * only affects the file its in). Without the suffix, we'd have "constant
+ * FILE_MANAGER_HOST assigned a value more than once" compiler warnings.
+ *
  * @type {string}
  * @const
  */
-var FILE_MANAGER_HOST = 'chrome-extension://hhaomjibdihmijegdhdafkllkbggdgoj';
+var FILE_MANAGER_HOST__METADATA_DISPATCHER =
+    'chrome-extension://hhaomjibdihmijegdhdafkllkbggdgoj';
 
 // All of these scripts could be imported with a single call to importScripts,
 // but then load and compile time errors would all be reported from the same
 // line.
-importScripts(FILE_MANAGER_HOST + '/foreground/js/metadata/metadata_parser.js');
-importScripts(FILE_MANAGER_HOST + '/foreground/js/metadata/byte_reader.js');
+importScripts(
+    FILE_MANAGER_HOST__METADATA_DISPATCHER +
+    '/foreground/js/metadata/metadata_parser.js');
+importScripts(
+    FILE_MANAGER_HOST__METADATA_DISPATCHER +
+    '/foreground/js/metadata/byte_reader.js');
 
 /**
  * Dispatches metadata requests to the correct parser.
@@ -30,10 +41,18 @@ function MetadataDispatcher(port) {
 
   // Make sure to update component_extension_resources.grd
   // when adding new parsers.
-  importScripts(FILE_MANAGER_HOST + '/foreground/js/metadata/exif_parser.js');
-  importScripts(FILE_MANAGER_HOST + '/foreground/js/metadata/image_parsers.js');
-  importScripts(FILE_MANAGER_HOST + '/foreground/js/metadata/mpeg_parser.js');
-  importScripts(FILE_MANAGER_HOST + '/foreground/js/metadata/id3_parser.js');
+  importScripts(
+      FILE_MANAGER_HOST__METADATA_DISPATCHER +
+      '/foreground/js/metadata/exif_parser.js');
+  importScripts(
+      FILE_MANAGER_HOST__METADATA_DISPATCHER +
+      '/foreground/js/metadata/image_parsers.js');
+  importScripts(
+      FILE_MANAGER_HOST__METADATA_DISPATCHER +
+      '/foreground/js/metadata/mpeg_parser.js');
+  importScripts(
+      FILE_MANAGER_HOST__METADATA_DISPATCHER +
+      '/foreground/js/metadata/id3_parser.js');
 
   var patterns = [];
 
