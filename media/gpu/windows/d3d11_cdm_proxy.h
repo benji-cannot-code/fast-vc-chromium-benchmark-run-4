cef3615cb06b5650b7d9df8f12d41a7895450d5a
Rintaro Kuroiwa <rkuroiwa@chromium.org>
2018-06-20 20:23:50 +0000

    Handle hardware content protection teardown events
    - Watch for events from
      IDXGIAdapter3::RegisterHardwareContentProtectionTeardownStatusEvent().
    - Watch for power-suspend events.
    - Both notifies the client that there has been a content protection teardown.
    
    Bug: 787657
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I531abb2c13f9f635989624d9c624da5227bd5bfa
    Reviewed-on: https://chromium-review.googlesource.com/1093536
    Commit-Queue: Rintaro Kuroiwa <rkuroiwa@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568978}

--- media/gpu/windows/d3d11_cdm_proxy.h
+++ media/gpu/windows/d3d11_cdm_proxy.h
@@ -9,12 +9,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/cdm/cdm_proxy.h"
 
 #include <d3d11_1.h>
+#include <dxgi1_4.h>
 #include <wrl/client.h>
 
 #include <map>
 #include <vector>
 
 #include "base/callback.h"
+#include "base/memory/weak_ptr.h"
 #include "media/gpu/media_gpu_export.h"
 
 namespace media {
@@ -80,6 +82,10 @@ class MEDIA_GPU_EXPORT D3D11CdmProxy : public CdmProxy {
   template <typename T>
   using ComPtr = Microsoft::WRL::ComPtr<T>;
 
+  class HardwareEventWatcher;
+
+  void NotifyHardwareContentProtectionTeardown();
+
   const GUID crypto_type_;
   const CdmProxy::Protocol protocol_;
   const FunctionIdMap function_id_map_;
@@ -109,6 +115,8 @@ class MEDIA_GPU_EXPORT D3D11CdmProxy : public CdmProxy {
   ComPtr<ID3D11VideoContext> video_context_;
   ComPtr<ID3D11VideoContext1> video_context1_;
 
+  std::unique_ptr<HardwareEventWatcher> hardware_event_watcher_;
+
   // Crypto session ID -> actual crypto session.
   std::map<uint32_t, ComPtr<ID3D11CryptoSession>> crypto_session_map_;
 
@@ -116,6 +124,8 @@ class MEDIA_GPU_EXPORT D3D11CdmProxy : public CdmProxy {
   // Used when calling NegotiateCryptoSessionKeyExchange.
   UINT private_input_size_ = 0;
   UINT private_output_size_ = 0;
+
+  base::WeakPtrFactory<D3D11CdmProxy> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(D3D11CdmProxy);
 };
