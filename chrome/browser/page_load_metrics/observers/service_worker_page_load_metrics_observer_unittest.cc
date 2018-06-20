be3c729efd4603dc1f3796ef44fd9050a88ed3f4
Makoto Shimazu <shimazu@chromium.org>
2018-06-20 15:41:39 +0000

    Add PageLoad.Clients.ServiceWorker.PaintTiming.NavigationToFirstPaint
    PaintTiming.NavigationToFirstPaint is one of key loading metrics, and we'd add
    it before starting ServceWorkerServicification (a.k.a NetS13nServiceWorker)
    experiement.
    
    Bug: 852698
    Change-Id: I2d7b7ad9ef3df94d428e1fbcbc1148de27e45c6a
    Reviewed-on: https://chromium-review.googlesource.com/1107418
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568851}

--- chrome/browser/page_load_metrics/observers/service_worker_page_load_metrics_observer_unittest.cc
+++ chrome/browser/page_load_metrics/observers/service_worker_page_load_metrics_observer_unittest.cc
@@ -38,6 +38,8 @@ class ServiceWorkerPageLoadMetricsObserverTest
 
   void AssertNoServiceWorkerHistogramsLogged() {
     histogram_tester().ExpectTotalCount(
+        internal::kHistogramServiceWorkerFirstPaint, 0);
+    histogram_tester().ExpectTotalCount(
         internal::kHistogramServiceWorkerFirstContentfulPaint, 0);
     histogram_tester().ExpectTotalCount(
         internal::kBackgroundHistogramServiceWorkerFirstContentfulPaint, 0);
@@ -124,6 +126,7 @@ class ServiceWorkerPageLoadMetricsObserverTest
     page_load_metrics::InitPageLoadTimingForTest(timing);
     timing->navigation_start = base::Time::FromDoubleT(1);
     timing->parse_timing->parse_start = base::TimeDelta::FromMilliseconds(100);
+    timing->paint_timing->first_paint = base::TimeDelta::FromMilliseconds(200);
     timing->paint_timing->first_contentful_paint =
         base::TimeDelta::FromMilliseconds(300);
     timing->paint_timing->first_meaningful_paint =
@@ -167,6 +170,12 @@ TEST_F(ServiceWorkerPageLoadMetricsObserverTest, WithServiceWorker) {
   metadata.behavior_flags |=
       blink::WebLoadingBehaviorFlag::kWebLoadingBehaviorServiceWorkerControlled;
   SimulateTimingAndMetadataUpdate(timing, metadata);
+
+  histogram_tester().ExpectTotalCount(
+      internal::kHistogramServiceWorkerFirstPaint, 1);
+  histogram_tester().ExpectBucketCount(
+      internal::kHistogramServiceWorkerFirstPaint,
+      timing.paint_timing->first_paint.value().InMilliseconds(), 1);
 
   histogram_tester().ExpectTotalCount(
       internal::kHistogramServiceWorkerFirstContentfulPaint, 1);
@@ -241,6 +250,8 @@ TEST_F(ServiceWorkerPageLoadMetricsObserverTest, WithServiceWorkerBackground) {
   SimulateTimingAndMetadataUpdate(timing, metadata);
 
   histogram_tester().ExpectTotalCount(
+      internal::kHistogramServiceWorkerFirstPaint, 0);
+  histogram_tester().ExpectTotalCount(
       internal::kHistogramServiceWorkerFirstContentfulPaint, 0);
   histogram_tester().ExpectTotalCount(
       internal::kBackgroundHistogramServiceWorkerFirstContentfulPaint, 1);
@@ -293,6 +304,12 @@ TEST_F(ServiceWorkerPageLoadMetricsObserverTest, InboxSite) {
   histogram_tester().ExpectBucketCount(
       internal::kHistogramServiceWorkerParseStartInbox,
       timing.parse_timing->parse_start.value().InMilliseconds(), 1);
+
+  histogram_tester().ExpectTotalCount(
+      internal::kHistogramServiceWorkerFirstPaint, 1);
+  histogram_tester().ExpectBucketCount(
+      internal::kHistogramServiceWorkerFirstPaint,
+      timing.paint_timing->first_paint.value().InMilliseconds(), 1);
 
   histogram_tester().ExpectTotalCount(
       internal::kHistogramServiceWorkerFirstContentfulPaint, 1);
@@ -409,6 +426,12 @@ TEST_F(ServiceWorkerPageLoadMetricsObserverTest, SearchSite) {
   histogram_tester().ExpectBucketCount(
       internal::kHistogramServiceWorkerParseStartSearch,
       timing.parse_timing->parse_start.value().InMilliseconds(), 1);
+
+  histogram_tester().ExpectTotalCount(
+      internal::kHistogramServiceWorkerFirstPaint, 1);
+  histogram_tester().ExpectBucketCount(
+      internal::kHistogramServiceWorkerFirstPaint,
+      timing.paint_timing->first_paint.value().InMilliseconds(), 1);
 
   histogram_tester().ExpectTotalCount(
       internal::kHistogramServiceWorkerFirstContentfulPaint, 1);
@@ -581,6 +604,12 @@ TEST_F(ServiceWorkerPageLoadMetricsObserverTest,
   metadata.behavior_flags |=
       blink::WebLoadingBehaviorFlag::kWebLoadingBehaviorServiceWorkerControlled;
   SimulateTimingAndMetadataUpdate(timing, metadata);
+
+  histogram_tester().ExpectTotalCount(
+      internal::kHistogramServiceWorkerFirstPaint, 1);
+  histogram_tester().ExpectBucketCount(
+      internal::kHistogramServiceWorkerFirstPaint,
+      timing.paint_timing->first_paint.value().InMilliseconds(), 1);
 
   histogram_tester().ExpectTotalCount(
       internal::kHistogramServiceWorkerFirstContentfulPaint, 1);
