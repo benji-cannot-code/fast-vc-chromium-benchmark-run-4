5212bb6f7248caf809109f71c9d260d902cfa523
Clark DuVall <cduvall@chromium.org>
2018-06-20 02:41:35 +0000

    Fix flaky SessionCleanupCookieStoreTest.TestDeleteSessionCookies
    This should ensure the delete tasks get run.
    
    Bug: 854032
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Change-Id: I73304bee8ee59e3e30ecb060cd3288c2630c8191
    Reviewed-on: https://chromium-review.googlesource.com/1106479
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568701}

--- services/network/session_cleanup_cookie_store_unittest.cc
+++ services/network/session_cleanup_cookie_store_unittest.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task_scheduler/task_scheduler.h"
 #include "base/test/scoped_task_environment.h"
 #include "base/time/time.h"
-#include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
 
@@ -128,11 +127,6 @@ TEST_F(SessionCleanupCookieStoreTest, TestPersistence) {
   cookies.clear();
 }
 
-#if defined(OS_LINUX) || defined(OS_CHROMEOS) || defined(OS_ANDROID)
-#define MAYBE_TestDeleteSessionCookies DISABLED_TestDeleteSessionCookies
-#else
-#define MAYBE_TestDeleteSessionCookies TestDeleteSessionCookies
-#endif
 TEST_F(SessionCleanupCookieStoreTest, TestDeleteSessionCookies) {
   CanonicalCookieVector cookies = CreateAndLoad();
   ASSERT_EQ(0u, cookies.size());
@@ -162,6 +156,7 @@ TEST_F(SessionCleanupCookieStoreTest, TestDeleteSessionCookies) {
       base::BindRepeating([](const std::string& domain, bool is_https) {
         return domain == "nonpersistent.com";
       }));
+  scoped_task_environment_.RunUntilIdle();
   DestroyStore();
   cookies = CreateAndLoad();
 
@@ -199,6 +194,7 @@ TEST_F(SessionCleanupCookieStoreTest, ForceKeepSessionState) {
       base::BindRepeating([](const std::string& domain, bool is_https) {
         return domain == "nonpersistent.com";
       }));
+  scoped_task_environment_.RunUntilIdle();
   DestroyStore();
   cookies = CreateAndLoad();
 
