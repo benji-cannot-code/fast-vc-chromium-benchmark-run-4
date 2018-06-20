541c5d3bd35ed954b09903cf18220720ee8fe2a9
Matt Falkenhagen <falken@chromium.org>
2018-06-20 11:58:32 +0000

    service worker: Teach Blink more about no fetch handler controllers.
    When the network service or NetS13nSW is enabled, requests
    skip service worker loaders entirely when the controller has
    no fetch event handler. This was broke assumptions in
    DocumentThreadableLoader, since it expected service worker
    machinery to return "fallback for CORS" responses for
    certain requests when there was a controller.
    
    Bug: 850839
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Change-Id: I32b058caacb14cba2e53dd8667fe9fc930a776e8
    Reviewed-on: https://chromium-review.googlesource.com/1107435
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568796}

--- content/child/blink_platform_impl.cc
+++ content/child/blink_platform_impl.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/app/resources/grit/content_resources.h"
 #include "content/app/strings/grit/content_strings.h"
 #include "content/child/child_thread_impl.h"
+#include "content/common/service_worker/service_worker_utils.h"
 #include "content/public/common/content_client.h"
 #include "content/public/common/content_features.h"
 #include "content/public/common/content_switches.h"
@@ -679,6 +680,10 @@ BlinkPlatformImpl::CreateFlingAnimationCurve(
 bool BlinkPlatformImpl::AllowScriptExtensionForServiceWorker(
     const blink::WebURL& scriptUrl) {
   return GetContentClient()->AllowScriptExtensionForServiceWorker(scriptUrl);
+}
+
+bool BlinkPlatformImpl::IsServiceWorkerNetServicificationEnabled() {
+  return ServiceWorkerUtils::IsServicificationEnabled();
 }
 
 blink::WebCrypto* BlinkPlatformImpl::Crypto() {
