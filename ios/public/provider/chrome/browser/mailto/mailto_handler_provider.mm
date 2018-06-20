e65d8c533f93bf2e48ae4a1e8f74769ea62da768
Sylvain Defresne <sdefresne@chromium.org>
2018-06-20 13:41:34 +0000

    Remove obsolete method from MailtoHandlerProvider
    The overload of PrepareMailtoHandling is no longer overridden
    in the downstream implementation and not called from upstream
    code so it can be safely removed.
    
    Bug: none
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I5bde07b17f1c5293488b0d65d284fa0e0ecea5f6
    Reviewed-on: https://chromium-review.googlesource.com/1106164
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568819}

--- ios/public/provider/chrome/browser/mailto/mailto_handler_provider.mm
+++ ios/public/provider/chrome/browser/mailto/mailto_handler_provider.mm
@@ -18,10 +18,6 @@ void MailtoHandlerProvider::PrepareMailtoHandling(
 
 void MailtoHandlerProvider::RemoveMailtoHandling() {}
 
-void MailtoHandlerProvider::PrepareMailtoHandling(
-    SignedInIdentityBlock signed_in_identity_block,
-    SignedInIdentitiesBlock signed_in_identities_block) {}
-
 NSString* MailtoHandlerProvider::MailtoHandlerSettingsTitle() const {
   return nil;
 }
