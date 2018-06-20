f90b1fec184027f49ef53e022dcfa468a3d3776e
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-06-20 20:26:29 +0000

    Import wpt@b71b2c58623ee15f0918b7388bf9cd11976b4b1b
    Using wpt-import in Chromium dba81d7721a8304c4629c394edebbde71d052db8.
    With Chromium commits locally applied on WPT:
    03ef65db57 "Update existing WebUSB WPT to be multi-global"
    8910242145 "Reland "Multi-global WebUSB WPT for window and worker""
    
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/19939
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    mkwst@chromium.org:
      external/wpt/secure-contexts
    
    TBR=robertma
    
    No-Export: true
    Change-Id: I6d9f72e41dd7bcc79f48621ecd248c33456507f5
    Reviewed-on: https://chromium-review.googlesource.com/1108384
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568983}

---
+++ third_party/WebKit/LayoutTests/external/wpt/html/semantics/embedded-content/the-img-element/sizes/support/parse-a-sizes-attribute.js
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+setup({explicit_done:true});
+
+function check(p, iframe) {
+  var current = p.firstElementChild;
+  var ref_sizes = current.getAttribute('sizes');
+  var expect = current.currentSrc;
+  if (expect) {
+    expect = expect.split('?')[0];
+  }
+  while (current = current.nextElementSibling) {
+    test(function() {
+      if (expect === '' || expect === null || expect === undefined) {
+        assert_unreached('ref currentSrc was ' + format_value(expect));
+      }
+      var got = current.currentSrc;
+      assert_greater_than(got.indexOf('?'), -1, 'expected a "?" in currentSrc');
+      got = got.split('?')[0];
+      assert_equals(got, expect);
+    }, current.outerHTML + ' ref sizes=' + format_value(ref_sizes) + ' (' + iframe.getAttribute('data-desc') + ')');
+  }
+}
+
+onload = function() {
+  var iframe = document.querySelector('iframe');
+  [].forEach.call(iframe.contentDocument.querySelectorAll('p'), function(p) {
+    check(p, iframe);
+  });
+  done();
+}
---
+++ third_party/WebKit/LayoutTests/external/wpt/secure-contexts/idlharness.any.js
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: global=window,worker
+// META: script=/resources/WebIDLParser.js
+// META: script=/resources/idlharness.js
+
+// https://w3c.github.io/webappsec-secure-contexts/
+
+'use strict';
+
+promise_test(async () => {
+  const idl = await fetch("/interfaces/secure-contexts.idl").then(r => r.text());
+  const workers = await fetch("/interfaces/dedicated-workers.idl").then(r => r.text());
+
+  const idl_array = new IdlArray();
+  idl_array.add_idls(idl);
+  idl_array.add_dependency_idls(workers);
+  idl_array.add_objects({
+    WindowOrWorkerGlobalScope: ["self"],
+  });
+  idl_array.test();
+}, "Test IDL implementation of Secure Contexts");
