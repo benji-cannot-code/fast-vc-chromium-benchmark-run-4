0e36036915862126ef11175387abb63d0a595096
chcunningham <chcunningham@chromium.org>
2018-06-20 22:19:14 +0000

    InMemory MediaCapabilities DB for Guest/Incognito profiles
    Prior to this change, Guest mode has an empty read-only DB and Incongito
    has a read-only DB. This CL allows both profiles to write to an "in
    memory" database that disapears when the profile is destroyed.
    
    This improves the experience for the user and avoids the possibility
    of using MediaCapabilites to detect incognito/guest profiles.
    
    R=jam@chromium.org
    
    Bug: 796280
    Change-Id: I37e25478ce6efe8c25bf49f77929f94c9ca27655
    Reviewed-on: https://chromium-review.googlesource.com/1092156
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Chrome Cunningham <chcunningham@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569046}

--- content/browser/frame_host/render_frame_host_impl.cc
+++ content/browser/frame_host/render_frame_host_impl.cc
@@ -3334,12 +3334,7 @@ void RenderFrameHostImpl::RegisterMojoInterfaces() {
 
   registry_->AddInterface(base::BindRepeating(
       &media::MediaMetricsProvider::Create,
-      // Only save decode stats when on-the-record.
-      GetSiteInstance()->GetBrowserContext()->IsOffTheRecord()
-          ? nullptr
-          : GetSiteInstance()
-                ->GetBrowserContext()
-                ->GetVideoDecodePerfHistory()));
+      GetSiteInstance()->GetBrowserContext()->GetVideoDecodePerfHistory()));
 
   if (base::CommandLine::ForCurrentProcess()->HasSwitch(
           cc::switches::kEnableGpuBenchmarking)) {
