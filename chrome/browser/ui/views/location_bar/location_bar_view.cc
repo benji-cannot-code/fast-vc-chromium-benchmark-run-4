e28f4929b0e7e98ed3f6a206e2fb849be27d5d56
Orin Jaworski <orinj@chromium.org>
2018-06-20 17:38:02 +0000

    [omnibox] Add a hover effect to the steady omnibox
    In Refresh, the steady state (unfocused) omnibox gets a hover
    effect with a darker shaded background for the LocationBarView.
    
    Bug: 853241
    Change-Id: I64859b5395c5a9eee6115c727bceec160b52dc39
    Reviewed-on: https://chromium-review.googlesource.com/1103547
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568905}

--- chrome/browser/ui/views/location_bar/location_bar_view.cc
+++ chrome/browser/ui/views/location_bar/location_bar_view.cc
@@ -781,7 +781,13 @@ int LocationBarView::GetHorizontalEdgeThickness() const {
 }
 
 void LocationBarView::RefreshBackground() {
-  SkColor background_color = GetColor(OmniboxPart::LOCATION_BAR_BACKGROUND);
+  // Note we are not using hover state color when focused; only in steady state.
+  const OmniboxPartState state =
+      (omnibox_view_->HasFocus() || !omnibox_view_->IsHovered())
+          ? OmniboxPartState::NORMAL
+          : OmniboxPartState::HOVERED;
+  SkColor background_color =
+      GetOmniboxColor(OmniboxPart::LOCATION_BAR_BACKGROUND, tint(), state);
   SkColor border_color = GetBorderColor();
 
   if (IsRounded()) {
@@ -1256,6 +1262,11 @@ void LocationBarView::OnOmniboxBlurred() {
 
   if (IsRounded())
     RefreshBackground();
+}
+
+void LocationBarView::OnOmniboxHoverChanged() {
+  RefreshBackground();
+  SchedulePaint();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
