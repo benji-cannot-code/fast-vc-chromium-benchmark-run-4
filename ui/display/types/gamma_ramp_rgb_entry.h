76382fea25d134a1ccc942d7072a00f0050667f6
Daniel Nicoara <dnicoara@chromium.org>
2018-06-20 20:39:40 +0000

    Add documentation for the values for GammaRampRGBEntry
    Defines the expected values and the valid range of the values.
    
    Bug: None
    Test: None
    Change-Id: I32de9b7dca313f34f157f83e1558425f75b7c60f
    Reviewed-on: https://chromium-review.googlesource.com/1099680
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Commit-Queue: Daniel Nicoara <dnicoara@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569000}

--- ui/display/types/gamma_ramp_rgb_entry.h
+++ ui/display/types/gamma_ramp_rgb_entry.h
@@ -13,6 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace display {
 
 // Provides a single entry for a gamma correction table in a GPU.
+// Each color component is UNORM16 fixed point format. This means each component
+// must be in the range [0, 2^16) and 0xffff represents the maximum value for
+// the color component.
 struct DISPLAY_TYPES_EXPORT GammaRampRGBEntry {
   uint16_t r;
   uint16_t g;
