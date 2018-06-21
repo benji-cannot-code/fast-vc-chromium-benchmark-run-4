96373341f44fa1a6ecf277a39bb3c13f3f00fa8f
Mathieu Perreault <mathp@chromium.org>
2018-06-21 23:06:26 +0000

    [New Tab Page] Create a source for NTP icons.
    For privacy reasons, the native code will have to choose which icon
    is served within a single source (so the implementer cannot deduct
    history). The priority order is the local favicon, the server favicon
    or a letter+circle fallback. This CL implements the first and the
    last of those, and further CLs will improve the logic.
    
    Bug: 853780
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: I6f56d02f3c953964e4968f191b8fb3177998c962
    Reviewed-on: https://chromium-review.googlesource.com/1105508
    Commit-Queue: Mathieu Perreault <mathp@chromium.org>
    Reviewed-by: Scott Chen <scottchen@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569414}

--- chrome/browser/search/instant_service.cc
+++ chrome/browser/search/instant_service.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/search/instant_service_observer.h"
 #include "chrome/browser/search/local_ntp_source.h"
 #include "chrome/browser/search/most_visited_iframe_source.h"
+#include "chrome/browser/search/ntp_icon_source.h"
 #include "chrome/browser/search/search.h"
 #include "chrome/browser/search/thumbnail_source.h"
 #include "chrome/browser/themes/theme_properties.h"
@@ -89,6 +90,7 @@ InstantService::InstantService(Profile* profile) : profile_(profile) {
   // Set up the data sources that Instant uses on the NTP.
   content::URLDataSource::Add(profile_, new ThemeSource(profile_));
   content::URLDataSource::Add(profile_, new LocalNtpSource(profile_));
+  content::URLDataSource::Add(profile_, new NtpIconSource(profile_));
   content::URLDataSource::Add(profile_, new ThumbnailSource(profile_, false));
   content::URLDataSource::Add(profile_, new ThumbnailSource(profile_, true));
   content::URLDataSource::Add(profile_, new ThumbnailListSource(profile_));
