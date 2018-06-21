6e40148613511195d85bf928377c265d38547f20
Qiyu Hu <qiyuh@google.com>
2018-06-21 18:09:27 +0000

    arc: bluetooth: Exclude GATT service UUIDs from advertisement data
    According to "Supplement to the Bluetooth Core Specification" (CSS 7.0,
    https://www.bluetooth.com/specifications/bluetooth-core-specification),
    Part A, 1.1, "GAP and GATT service UUIDs should not be included in a
    Service UUIDs AD type, for either a complete or incomplete list."
    
    present in scan result
    
    Bug: 78593133
    Test: Do nRF scan and confirm that only non GATT service UUIDs are
    Change-Id: Ia1d069a4356b1de90c19667fa28db167a5e3a7bb
    Reviewed-on: https://chromium-review.googlesource.com/1108882
    Commit-Queue: Qiyu Hu <qiyuh@google.com>
    Reviewed-by: Luis Hector Chavez <lhchavez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569323}

--- chrome/browser/chromeos/arc/bluetooth/arc_bluetooth_bridge_unittest.cc
+++ chrome/browser/chromeos/arc/bluetooth/arc_bluetooth_bridge_unittest.cc
@@ -25,11 +25,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "device/bluetooth/dbus/fake_bluetooth_gatt_descriptor_client.h"
 #include "device/bluetooth/dbus/fake_bluetooth_gatt_service_client.h"
 #include "device/bluetooth/dbus/fake_bluetooth_le_advertising_manager_client.h"
+#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace {
 constexpr int16_t kTestRssi = -50;
 constexpr int16_t kTestRssi2 = -70;
+constexpr char kTestServiceUUID[] = "00001357-0000-1000-8000-00805f9b34fb";
 }  // namespace
 
 namespace arc {
@@ -53,6 +55,10 @@ class ArcBluetoothBridgeTest : public testing::Test {
     fake_bluetooth_device_client->CreateDevice(
         dbus::ObjectPath(bluez::FakeBluetoothAdapterClient::kAdapterPath),
         dbus::ObjectPath(bluez::FakeBluetoothDeviceClient::kLowEnergyPath));
+    fake_bluetooth_device_client->UpdateServiceAndManufacturerData(
+        dbus::ObjectPath(bluez::FakeBluetoothDeviceClient::kLowEnergyPath),
+        {kTestServiceUUID}, /* service_data = */ {},
+        /* manufacture_data = */ {});
     fake_bluetooth_gatt_service_client->ExposeHeartRateService(
         dbus::ObjectPath(bluez::FakeBluetoothDeviceClient::kLowEnergyPath));
     fake_bluetooth_gatt_characteristic_client->ExposeHeartRateCharacteristics(
@@ -206,7 +212,7 @@ class ArcBluetoothBridgeTest : public testing::Test {
 TEST_F(ArcBluetoothBridgeTest, DeviceFound) {
   EXPECT_EQ(0u, fake_bluetooth_instance_->device_found_data().size());
   AddTestDevice();
-  EXPECT_EQ(2u, fake_bluetooth_instance_->device_found_data().size());
+  EXPECT_EQ(5u, fake_bluetooth_instance_->device_found_data().size());
   const std::vector<mojom::BluetoothPropertyPtr>& prop =
       fake_bluetooth_instance_->device_found_data().back();
 
@@ -218,9 +224,12 @@ TEST_F(ArcBluetoothBridgeTest, DeviceFound) {
   EXPECT_EQ(std::string(bluez::FakeBluetoothDeviceClient::kLowEnergyAddress),
             prop[1]->get_bdaddr()->To<std::string>());
   EXPECT_TRUE(prop[2]->is_uuids());
-  EXPECT_EQ(1u, prop[2]->get_uuids().size());
-  EXPECT_EQ(bluez::FakeBluetoothGattServiceClient::kHeartRateServiceUUID,
-            prop[2]->get_uuids()[0].value());
+  EXPECT_THAT(
+      prop[2]->get_uuids(),
+      testing::UnorderedElementsAre(
+          device::BluetoothUUID(
+              bluez::FakeBluetoothGattServiceClient::kHeartRateServiceUUID),
+          device::BluetoothUUID(kTestServiceUUID)));
   EXPECT_TRUE(prop[3]->is_device_class());
   EXPECT_EQ(bluez::FakeBluetoothDeviceClient::kLowEnergyClass,
             prop[3]->get_device_class());
@@ -233,7 +242,7 @@ TEST_F(ArcBluetoothBridgeTest, DeviceFound) {
   EXPECT_EQ(kTestRssi, prop[6]->get_remote_rssi());
 
   ChangeTestDeviceRssi(kTestRssi2);
-  EXPECT_EQ(3u, fake_bluetooth_instance_->device_found_data().size());
+  EXPECT_EQ(6u, fake_bluetooth_instance_->device_found_data().size());
   const std::vector<mojom::BluetoothPropertyPtr>& prop2 =
       fake_bluetooth_instance_->device_found_data().back();
   EXPECT_EQ(7u, prop2.size());
@@ -264,7 +273,7 @@ TEST_F(ArcBluetoothBridgeTest, LEDeviceFound) {
 
   EXPECT_TRUE(adv_data[1]->is_service_uuids());
   EXPECT_EQ(1u, adv_data[1]->get_service_uuids().size());
-  EXPECT_EQ(bluez::FakeBluetoothGattServiceClient::kHeartRateServiceUUID,
+  EXPECT_EQ(kTestServiceUUID,
             adv_data[1]->get_service_uuids()[0].canonical_value());
 
   EXPECT_EQ(kTestRssi, le_device_found_data->rssi());
