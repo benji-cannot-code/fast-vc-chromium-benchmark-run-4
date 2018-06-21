2e0fb512d5aee87840c762d9d3b6a491fb87ac96
Dominic Mazzoni <dmazzoni@chromium.org>
2018-06-21 23:11:09 +0000

    Free up accessibility tree resources from hidden frames.
    When a frame is hidden, replace its accessibility tree with just the root,
    and suppress all accessibility events in that tree while it's hidden.
    By keeping the root in the tree, this allows us to propagate the change
    to the browser process and free up resources there too.
    
    The goal is that having lots of tabs open shouldn't significantly impact
    your experience.
    
    It may add a slight delay when switching to a tab.
    
    Bug: 854411
    Change-Id: I78b45e8f5772f0c9745ee59952f27f6ba3557bbc
    Reviewed-on: https://chromium-review.googlesource.com/1107207
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569422}

--- content/renderer/accessibility/blink_ax_tree_source.h
+++ content/renderer/accessibility/blink_ax_tree_source.h
@@ -43,6 +43,11 @@ class BlinkAXTreeSource
   BlinkAXTreeSource(RenderFrameImpl* render_frame, ui::AXMode mode);
   ~BlinkAXTreeSource() override;
 
+  // If the frame is hidden, expose only the root node and skip the
+  // rest of the tree.
+  void WasHidden();
+  void WasShown();
+
   // Freeze caches the document, accessibility root, and current focused
   // object for fast retrieval during a batch of operations. Use
   // ScopedFreezeBlinkAXTreeSource on the stack rather than calling
@@ -131,6 +136,10 @@ class BlinkAXTreeSource
   blink::WebDocument document_;
   blink::WebAXObject root_;
   blink::WebAXObject focus_;
+
+  // If the page is hidden, we eliminate everything other than the
+  // root node.
+  bool hidden_;
 };
 
 }  // namespace content
--- content/renderer/accessibility/render_accessibility_impl.cc
+++ content/renderer/accessibility/render_accessibility_impl.cc
@@ -126,6 +126,9 @@ RenderAccessibilityImpl::RenderAccessibilityImpl(RenderFrameImpl* render_frame,
     settings->SetInlineTextBoxAccessibilityEnabled(true);
 #endif
 
+  if (render_frame_->IsHidden())
+    tree_source_.WasHidden();
+
   const WebDocument& document = GetMainDocument();
   if (!document.IsNull()) {
     // It's possible that the webview has already loaded a webpage without
@@ -195,8 +198,22 @@ bool RenderAccessibilityImpl::OnMessageReceived(const IPC::Message& message) {
   return handled;
 }
 
+void RenderAccessibilityImpl::WasHidden() {
+  pending_events_.clear();
+  tree_source_.WasHidden();
+  HandleAXEvent(tree_source_.GetRoot(), ax::mojom::Event::kNone);
+}
+
+void RenderAccessibilityImpl::WasShown() {
+  pending_events_.clear();
+  tree_source_.WasShown();
+  HandleAXEvent(tree_source_.GetRoot(), ax::mojom::Event::kNone);
+}
+
 void RenderAccessibilityImpl::HandleWebAccessibilityEvent(
     const blink::WebAXObject& obj, blink::WebAXEvent event) {
+  if (render_frame_->IsHidden())
+    return;
   HandleAXEvent(obj, AXEventFromBlink(event));
 }
 
