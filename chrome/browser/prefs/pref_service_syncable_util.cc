38a69d571ce53ac4c43c017110b7101937cd5b99
Nicolas Ouellet-payeur <nicolaso@chromium.org>
2018-06-21 19:08:08 +0000

    Don't persist "Never translate this site" blacklist in incognito.
    For privacy reasons, we don't want the blacklist to be persisted after the user
    leaves incognito mode (as this info could reveal a website that was visited in
    incognito).
    
    With this patch, adding a website to the blacklist makes it work for the
    incognito browsing session, but only saves those websites in memory. Closing the
    incognito discards the changes to the blacklist, and the main browsing session
    is not affected.
    
    Bug: 793925
    Change-Id: Icdd35e86b8b807c6a639c5c6ba86786f912e1bd0
    Reviewed-on: https://chromium-review.googlesource.com/1110259
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Reviewed-by: anthonyvd <anthonyvd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569349}

--- chrome/browser/prefs/pref_service_syncable_util.cc
+++ chrome/browser/prefs/pref_service_syncable_util.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/pref_names.h"
 #include "components/sync_preferences/pref_service_syncable.h"
+#include "components/translate/core/browser/translate_prefs.h"
 
 #if defined(OS_ANDROID)
 #include "components/proxy_config/proxy_config_pref_names.h"
@@ -41,6 +42,8 @@ CreateIncognitoPrefServiceSyncable(
   overlay_pref_names.push_back(prefs::kBrowserWindowPlacement);
   overlay_pref_names.push_back(prefs::kMediaRouterTabMirroringSources);
   overlay_pref_names.push_back(prefs::kSaveFileDefaultDirectory);
+  overlay_pref_names.push_back(
+      translate::TranslatePrefs::kPrefTranslateSiteBlacklist);
 #if defined(OS_ANDROID)
   overlay_pref_names.push_back(proxy_config::prefs::kProxy);
 #endif
