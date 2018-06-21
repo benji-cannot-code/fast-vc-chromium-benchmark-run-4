97d1f1055b27f15055e5aea1e00c5653a5c05286
Koji Ishii <kojii@chromium.org>
2018-06-21 09:07:23 +0000

    [LayoutNG] Fix dialog element positioning when it is out-of-flow
    LayoutBlockFlow::UpdateBlockLayout() has a specialized
    positioning for HTML <dialog> element when it is out-of-flow.
    
    This patch implements the logic by sharing the code in
    LayoutBlockFlow.
    
    Bug: 635619
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng
    Change-Id: I16870f926dc0ad6fbb6bee4787f3f6d5e30b31c7
    Reviewed-on: https://chromium-review.googlesource.com/1108240
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Aleks Totic <atotic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569195}

--- third_party/blink/renderer/core/layout/layout_block_flow.h
+++ third_party/blink/renderer/core/layout/layout_block_flow.h
@@ -809,6 +809,8 @@ class CORE_EXPORT LayoutBlockFlow : public LayoutBlock {
 
   bool ShouldTruncateOverflowingText() const;
 
+  base::Optional<LayoutUnit> ComputeDialogYPosition(LayoutUnit height) const;
+
   int GetLayoutPassCountForTesting();
 
  protected:
