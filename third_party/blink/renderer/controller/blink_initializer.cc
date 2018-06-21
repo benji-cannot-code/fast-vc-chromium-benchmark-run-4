697d0f2d239a3f0f0e41585094b04590daf901db
Becca Hughes <beccahughes@chromium.org>
2018-06-21 23:13:44 +0000

    [Display Cutout] Expose safe area through CSS
    Add a DisplayCutoutClient mojo interface that is bound to a
    frame and called from the browser. This will set safe-area*
    CSS env() variables that websites can use to layout their
    content based on the safe area.
    
    DisplayCutoutClient will be called from Android in a future
    CL.
    
    BUG=847652
    
    Change-Id: I6180aa5337e182fd1794586c47caef0f19fab718
    Reviewed-on: https://chromium-review.googlesource.com/1083291
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569427}

--- third_party/blink/renderer/controller/blink_initializer.cc
+++ third_party/blink/renderer/controller/blink_initializer.cc
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/controller/bloated_renderer_detector.h"
 #include "third_party/blink/renderer/controller/dev_tools_frontend_impl.h"
 #include "third_party/blink/renderer/core/animation/animation_clock.h"
+#include "third_party/blink/renderer/core/frame/display_cutout_client_impl.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/platform/bindings/microtask.h"
 #include "third_party/blink/renderer/platform/bindings/v8_per_isolate_data.h"
@@ -159,6 +160,11 @@ void BlinkInitializer::RegisterInterfaces(
 }
 
 void BlinkInitializer::InitLocalFrame(LocalFrame& frame) const {
+  if (frame.IsMainFrame() &&
+      RuntimeEnabledFeatures::DisplayCutoutAPIEnabled()) {
+    frame.GetInterfaceRegistry()->AddAssociatedInterface(WTF::BindRepeating(
+        &DisplayCutoutClientImpl::BindMojoRequest, WrapWeakPersistent(&frame)));
+  }
   frame.GetInterfaceRegistry()->AddAssociatedInterface(WTF::BindRepeating(
       &DevToolsFrontendImpl::BindMojoRequest, WrapWeakPersistent(&frame)));
   frame.GetInterfaceRegistry()->AddInterface(WTF::BindRepeating(
