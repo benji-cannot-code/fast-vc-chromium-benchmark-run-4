c3ba9f8627925d54bc2f7eb10231e0b1bbbc0072
Jared Saul <jsaul@google.com>
2018-06-21 03:09:24 +0000

    [Autofill cleanup] Reorder autofill_table.cc functions to match .h order
    The ordering of functions in autofill_table.cc is *completely* out of
    order with what's listed in autofill_table.h, and keeping them
    consistent seems to be our standard.  This CL makes only two changes:
     1) Moves functions in autofill_table.cc around (no changes; don't let
        the diff tell you otherwise!)
     2) Removes InitDatesTable() from the .h file, which apparently doesn't
        exist in the .cc file (or anywhere else in the repo)
    
    Change-Id: If091c89fc41bd53bd5c2aaccece0da175ff4937e
    Reviewed-on: https://chromium-review.googlesource.com/1109402
    Commit-Queue: Jared Saul <jsaul@google.com>
    Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569128}

--- components/autofill/core/browser/webdata/autofill_table.h
+++ components/autofill/core/browser/webdata/autofill_table.h
@@ -582,7 +582,6 @@ class AutofillTable : public WebDatabaseTable,
 
   bool InitMainTable();
   bool InitCreditCardsTable();
-  bool InitDatesTable();
   bool InitProfilesTable();
   bool InitProfileNamesTable();
   bool InitProfileEmailsTable();
