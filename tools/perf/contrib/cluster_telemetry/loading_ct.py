c13940af958edadb690f318b3f62b117ec917337
Deepanjan Roy <dproy@chromium.org>
2018-06-21 14:53:12 +0000

    [loading_ct] Cache temperature flag and default COLD
    Allows specifying cache temperature with --cache-temperature.
    Values are as defined in telemetry/page/cache_temperature.py.
    For example: --cache-temperature=warm.
    
    Also, changes the default cache temperature for loading_ct to
    COLD.
    
    Change-Id: Ib0210bf2a72d28902bfe653cc01646aae4ccbcff
    Reviewed-on: https://chromium-review.googlesource.com/1106643
    Commit-Queue: Deepanjan Roy <dproy@chromium.org>
    Reviewed-by: Ravi Mistry <rmistry@chromium.org>
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569260}

--- tools/perf/contrib/cluster_telemetry/loading_ct.py
+++ tools/perf/contrib/cluster_telemetry/loading_ct.py
@@ -7,6 +7,7 @@ from benchmarks import loading
 from contrib.cluster_telemetry import ct_benchmarks_util
 from contrib.cluster_telemetry import page_set
 
+from telemetry.page import cache_temperature as cache_temperature_module
 from telemetry.page import traffic_setting
 
 
@@ -16,6 +17,7 @@ class LoadingClusterTelemetry(loading._LoadingBase):
   options = {'upload_results': True}
 
   _ALL_NET_CONFIGS = traffic_setting.NETWORK_CONFIGS.keys()
+  _ALL_CACHE_TEMPERATURES = cache_temperature_module.ALL_CACHE_TEMPERATURES
 
   @classmethod
   def AddBenchmarkCommandLineArgs(cls, parser):
@@ -29,6 +31,11 @@ class LoadingClusterTelemetry(loading._LoadingBase):
         default=traffic_setting.REGULAR_4G,
         help='Traffic condition (string). Default to "%%default". Can be: %s' %
          ', '.join(cls._ALL_NET_CONFIGS))
+    parser.add_option(
+        '--cache-temperature',  choices=cls._ALL_CACHE_TEMPERATURES,
+        default=cache_temperature_module.COLD,
+        help='Cache temperature (string). Default to "%%default". Can be: %s' %
+         ', '.join(cls._ALL_CACHE_TEMPERATURES))
 
   def CreateStorySet(self, options):
     def Wait(action_runner):
@@ -36,6 +43,7 @@ class LoadingClusterTelemetry(loading._LoadingBase):
     return page_set.CTPageSet(
       options.urls_list, options.user_agent, options.archive_data_file,
       traffic_setting=options.traffic_setting,
+      cache_temperature=options.cache_temperature,
       run_page_interaction_callback=Wait)
 
   @classmethod
