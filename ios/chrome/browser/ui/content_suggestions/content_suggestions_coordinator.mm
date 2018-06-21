e8104763095c769b8e3816e89830afa234b6021c
Gauthier Ambard <gambard@chromium.org>
2018-06-21 15:57:52 +0000

    Fix NTP toolbar's dispatcher
    The dispatcher wasn't set for the primary toolbar used by the NTP,
    leading to bugs where the commands weren't sent to the rest of the
    application.
    
    Bug: 851930
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I17b2e015a307e993e548c4dfcbca599472503d58
    Reviewed-on: https://chromium-review.googlesource.com/1110123
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569279}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_coordinator.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_coordinator.mm
@@ -157,6 +157,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     self.primaryToolbarViewController =
         [[PrimaryToolbarViewController alloc] init];
     self.primaryToolbarViewController.buttonFactory = buttonFactory;
+    self.primaryToolbarViewController.dispatcher = self.dispatcher;
     [self.primaryToolbarViewController updateForSideSwipeSnapshotOnNTP:YES];
     self.headerController.toolbarViewController =
         self.primaryToolbarViewController;
