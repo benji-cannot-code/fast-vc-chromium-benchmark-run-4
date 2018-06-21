aebc5ae5a8f88a0c2686e558460cf7b0ede084a3
David Roger <droger@chromium.org>
2018-06-21 16:17:51 +0000

    [signin] Fix regression in MergeSigninCredentialIntoCookieJar
    The CL https://chromium-review.googlesource.com/c/chromium/src/+/1104682/9/components/signin/core/browser/signin_manager.cc
    introduced a regression, leading to cookies not being merged correctly.
    The cookies should be merged in all cases except Mirror, but that CL
    did the opposite (only merging cookies when Mirror is enabled)
    
    Change-Id: Ib3c5454259ff6b7ec82bb2411bcff011bc63f3e1
    Bug: 777774
    Reviewed-on: https://chromium-review.googlesource.com/1109976
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569287}

--- components/signin/core/browser/signin_manager.cc
+++ components/signin/core/browser/signin_manager.cc
@@ -394,8 +394,7 @@ void SigninManager::DisableOneClickSignIn(PrefService* prefs) {
 }
 
 void SigninManager::MergeSigninCredentialIntoCookieJar() {
-#if !defined(OS_IOS)
-  if (account_consistency_ != signin::AccountConsistencyMethod::kMirror)
+  if (account_consistency_ == signin::AccountConsistencyMethod::kMirror)
     return;
 
   if (!IsAuthenticated())
@@ -403,7 +402,6 @@ void SigninManager::MergeSigninCredentialIntoCookieJar() {
 
   cookie_manager_service_->AddAccountToCookie(GetAuthenticatedAccountId(),
                                               "ChromiumSigninManager");
-#endif  // !defined(OS_IOS)
 }
 
 void SigninManager::CompletePendingSignin() {
