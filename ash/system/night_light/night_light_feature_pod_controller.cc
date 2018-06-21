daea9624011308e2bab11ccff26b1c94a3622bee
Tetsui Ohkubo <tetsui@chromium.org>
2018-06-21 01:19:18 +0000

    Hide Night Light button when not logged in.
    Night Light button is no-op when not logged in. There's no harm but it's
    better to hide the button to avoid confusion.
    
    TEST=manual
    BUG=819957
    
    Change-Id: I28fa1629dea1c19340d6eabcb1bca51b1fea28f0
    Reviewed-on: https://chromium-review.googlesource.com/1107533
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569122}

--- ash/system/night_light/night_light_feature_pod_controller.cc
+++ ash/system/night_light/night_light_feature_pod_controller.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/public/cpp/ash_features.h"
 #include "ash/resources/vector_icons/vector_icons.h"
+#include "ash/session/session_controller.h"
 #include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/system/night_light/night_light_controller.h"
@@ -22,7 +23,9 @@ NightLightFeaturePodController::~NightLightFeaturePodController() = default;
 FeaturePodButton* NightLightFeaturePodController::CreateButton() {
   DCHECK(!button_);
   button_ = new FeaturePodButton(this);
-  button_->SetVisible(features::IsNightLightEnabled());
+  button_->SetVisible(
+      features::IsNightLightEnabled() &&
+      Shell::Get()->session_controller()->ShouldEnableSettings());
   button_->SetLabel(
       l10n_util::GetStringUTF16(IDS_ASH_STATUS_TRAY_NIGHT_LIGHT_BUTTON_LABEL));
   UpdateButton();
