3ea484aa56101cd00c99d4d3752da7d749fa54e5
Ben Wagner <bungeman@chromium.org>
2018-06-21 18:33:16 +0000

    Some iwyu for Skia roll.
    Skia is cleaning up a few headers. Unfortunately a few source files in
    Chromium relied on a few transitive includes.
    
    TBR=sadrul@chromium.org
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel
    Change-Id: I8bb15183908958eb066fb0ff4d20026ae9f311a4
    Reviewed-on: https://chromium-review.googlesource.com/1110143
    Commit-Queue: Ben Wagner <bungeman@chromium.org>
    Reviewed-by: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569332}

--- components/viz/service/display/scoped_render_pass_texture.cc
+++ components/viz/service/display/scoped_render_pass_texture.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/viz/service/display/scoped_render_pass_texture.h"
 
+#include <algorithm>
+
 #include "base/bits.h"
 #include "base/logging.h"
 #include "components/viz/common/gpu/context_provider.h"
--- ui/display/manager/display_utils_unittest.cc
+++ ui/display/manager/display_utils_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/display/manager/display_util.h"
 
+#include <array>
 #include <vector>
 
 #include "base/macros.h"
