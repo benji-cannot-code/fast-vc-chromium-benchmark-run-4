2aea0ed7765f9dd47d55024ad9571f172f132c34
Robert Liao <robliao@chromium.org>
2018-06-21 21:17:20 +0000

    Revert "Add Views DesktopMediaPicker into the Overall Views Build"
    This reverts commit 59da623a9597b60252f1c657add234eecbb30918.
    
    Reason for revert: This work was just deprioritized, so the investigation into the bot only failures is now on the backburner.
    
    Original change's description:
    > Add Views DesktopMediaPicker into the Overall Views Build
    > 
    > This moves the code into the toolkit_views source list and gates Aura
    > specific code behind build flags.
    > 
    > Additionally, this links up DesktopMediaPicker::Create in a manner
    > similar to ScreenCaptureNotificationUI::Create[Cocoa].
    > 
    > BUG=726005,657883
    > NOAUTOREVERT=true
    > Need to investigate any failure on the Mac Bot. Not reproducible locally.
    > 
    > Change-Id: I9ece0b875268495d70154378c7608c3e1bae2938
    > Reviewed-on: https://chromium-review.googlesource.com/1102042
    > Commit-Queue: Robert Liao <robliao@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#569095}
    
    TBR=ellyjones@chromium.org,sky@chromium.org,robliao@chromium.org
    
    Change-Id: Ice2800851d19895e687525bc63b8afa9d1746517
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 726005, 657883
    Reviewed-on: https://chromium-review.googlesource.com/1110897
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569377}

--- chrome/browser/ui/cocoa/media_picker/desktop_media_picker_cocoa.mm
+++ chrome/browser/ui/cocoa/media_picker/desktop_media_picker_cocoa.mm
@@ -28,3 +28,8 @@ void DesktopMediaPickerCocoa::Show(
              requestAudio:params.request_audio]);
   [controller_ showWindow:nil];
 }
+
+// static
+std::unique_ptr<DesktopMediaPicker> DesktopMediaPicker::Create() {
+  return std::unique_ptr<DesktopMediaPicker>(new DesktopMediaPickerCocoa());
+}
