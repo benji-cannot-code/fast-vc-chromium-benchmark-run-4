6a39e74da40711dd13426dfd0d72963908e190fe
Sebastien SG <sebsg@chromium.org>
2018-06-21 03:14:12 +0000

    [AF] Add CTR and save rate metrics split out by security level.
    This Cl adds autofill metrics based on the security level. The
    goal is to see whether the security level impacts the CTR of
    autofill suggestions or the card save rate.
    
    Bug: 853347
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I75ce8c6b924d7fd90e80e518af46509bb6bccde2
    Reviewed-on: https://chromium-review.googlesource.com/1103218
    Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Christopher Thompson <cthomp@chromium.org>
    Reviewed-by: Tao Bai <michaelbai@chromium.org>
    Reviewed-by: Mathieu Perreault <mathp@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569136}

--- chrome/browser/ui/autofill/save_card_bubble_controller_impl.h
+++ chrome/browser/ui/autofill/save_card_bubble_controller_impl.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/timer/elapsed_timer.h"
 #include "components/autofill/core/browser/credit_card.h"
 #include "components/autofill/core/browser/ui/save_card_bubble_controller.h"
+#include "components/security_state/core/security_state.h"
 #include "components/signin/core/browser/account_info.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
@@ -82,6 +83,9 @@ class SaveCardBubbleControllerImpl
   void OnVisibilityChanged(content::Visibility visibility) override;
   void WebContentsDestroyed() override;
 
+  // Gets the security level of the page.
+  virtual security_state::SecurityLevel GetSecurityLevel() const;
+
  private:
   friend class content::WebContentsUserData<SaveCardBubbleControllerImpl>;
 
@@ -136,6 +140,9 @@ class SaveCardBubbleControllerImpl
   // Used to measure the amount of time on a page; if it's less than some
   // reasonable limit, then don't close the bubble upon navigation.
   std::unique_ptr<base::ElapsedTimer> timer_;
+
+  // The security level for the current context.
+  security_state::SecurityLevel security_level_;
 
   DISALLOW_COPY_AND_ASSIGN(SaveCardBubbleControllerImpl);
 };
--- components/autofill/core/browser/test_autofill_client.cc
+++ components/autofill/core/browser/test_autofill_client.cc
@@ -57,6 +57,11 @@ AddressNormalizer* TestAutofillClient::GetAddressNormalizer() {
   return nullptr;
 }
 
+security_state::SecurityLevel
+TestAutofillClient::GetSecurityLevelForUmaHistograms() {
+  return security_state::SecurityLevel::NONE;
+}
+
 void TestAutofillClient::ShowAutofillSettings() {
 }
 
