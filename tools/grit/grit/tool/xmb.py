b8d242b4f25eee110bc3efc8f7ba689a2159d1bc
Tom Anderson <thomasanderson@chromium.org>
2018-06-21 00:43:09 +0000

    Add missing cwd env var to grit_info and grit xmb
    This fixes the build with compute_grit_inputs_for_analyze=true which regressed
    after CL:
    https://chromium.googlesource.com/chromium/src/+/93e774da116cc1bce515883fc28f012ccd905635
    
    BUG=853961
    R=dpranke
    
    Change-Id: I48e9297107cc0e543f00f1d6fa7e347bc39e5de3
    Reviewed-on: https://chromium-review.googlesource.com/1105485
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569099}

--- tools/grit/grit/tool/xmb.py
+++ tools/grit/grit/tool/xmb.py
@@ -166,6 +166,8 @@ Other options:
     return 'Exports all translateable messages into an XMB file.'
 
   def Run(self, opts, args):
+    os.environ['cwd'] = os.getcwd()
+
     self.SetOptions(opts)
 
     limit_file = None
