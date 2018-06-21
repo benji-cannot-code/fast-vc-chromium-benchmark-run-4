23146e83c9db53a0c7aea8591e21a72ddc77b58b
Emily Stark <estark@google.com>
2018-06-21 16:04:00 +0000

    Add UseCounters for Symantec certs to be distrusted in M70
    The old Symantec PKI will be fully distrusted in M70
    (https://security.googleblog.com/2018/03/distrust-of-symantec-pki-immediate.html). We
    already have DevTools console messages in place for affected resources, and this
    CL adds UseCounters so that we know how many page loads will be affected.
    
    The counters give us:
    - % of page visits with an affected main frame main resource
    - % of page visits with an afected subframe main resource
    - % of page visits with an affected subresource
    
    Note that these buckets can overlap, for example a single page visit can
    increment all 3 counters.
    
    Bug: 853868
    Change-Id: I7ae103efcc47a3e4e381b35c6803ef2d8ed4c7fb
    Reviewed-on: https://chromium-review.googlesource.com/1107841
    Commit-Queue: Emily Stark <estark@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569282}

--- third_party/blink/renderer/core/loader/frame_fetch_context.cc
+++ third_party/blink/renderer/core/loader/frame_fetch_context.cc
@@ -139,6 +139,15 @@ void MaybeRecordCTPolicyComplianceUseCounter(
                 kCertificateTransparencyNonCompliantSubresourceInMainFrame);
 }
 
+void RecordLegacySymantecCertUseCounter(LocalFrame* frame,
+                                        Resource::Type resource_type) {
+  // Main resources are counted in DocumentLoader.
+  if (resource_type == Resource::kMainResource) {
+    return;
+  }
+  UseCounter::Count(frame, WebFeature::kLegacySymantecCertInSubresource);
+}
+
 // Determines FetchCacheMode for a main resource, or FetchCacheMode that is
 // corresponding to WebFrameLoadType.
 // TODO(toyoshim): Probably, we should split WebFrameLoadType to FetchCacheMode
@@ -597,6 +606,7 @@ void FrameFetchContext::DispatchDidReceiveResponse(
   }
 
   if (response.IsLegacySymantecCert()) {
+    RecordLegacySymantecCertUseCounter(GetFrame(), resource->GetType());
     GetLocalFrameClient()->ReportLegacySymantecCert(response.Url(),
                                                     false /* did_fail */);
   }
