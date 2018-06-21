3eae8576432d1fd6e52b3a46cc5b2e3e8d0aa606
Jungshik Shin <jshin@chromium.org>
2018-06-21 15:01:49 +0000

    Roll ICU to 172d331 and use icu tz in wtf
    This roll has just one change to fix the timezone
    detection on Japanese/Korean and other non-English
    Windows.
    
      https://chromium-review.googlesource.com/1094322
    
    In addition, switch to ICU to convert UTC to local time
    in blink. This is to deal with a cryptic failure of
    external/wpt/html/dom/doc*/resource*/document-lastModified-01.html
    
    parens.
    
    Bug: 849724
    Test: Set the timezone to Tokyo on Japanese Windows
    Test: In JS console, 'new Date()' outputs 'Japan Standard Time' in
    Test: Layout test external/wpt/html/dom/doc*/resource*/document-lastModified-01.html
    Change-Id: I3037252ff9044a114ae831de2b151df7c652603c
    Reviewed-on: https://chromium-review.googlesource.com/1100739
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Jungshik Shin <jshin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569262}

--- third_party/blink/renderer/platform/wtf/date_math.cc
+++ third_party/blink/renderer/platform/wtf/date_math.cc
@@ -78,6 +78,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <time.h>
 #include <algorithm>
 #include <limits>
+#include <memory>
 #include "build/build_config.h"
 #include "third_party/blink/renderer/platform/wtf/ascii_ctype.h"
 #include "third_party/blink/renderer/platform/wtf/assertions.h"
@@ -86,6 +87,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/string_extras.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_builder.h"
 #include "third_party/blink/renderer/platform/wtf/time.h"
+
+#include <unicode/timezone.h>
 
 #if defined(OS_WIN)
 #include <windows.h>
@@ -836,9 +839,11 @@ String MakeRFC2822DateString(const Time date, int utc_offset) {
 }
 
 double ConvertToLocalTime(double ms) {
-  double utc_offset = CalculateUTCOffset();
-  double dst_offset = CalculateDSTOffset(ms, utc_offset);
-  return (ms + utc_offset + dst_offset);
+  std::unique_ptr<icu::TimeZone> timezone(icu::TimeZone::createDefault());
+  int32_t raw_offset, dst_offset;
+  UErrorCode status = U_ZERO_ERROR;
+  timezone->getOffset(ms, false, raw_offset, dst_offset, status);
+  return (ms + static_cast<double>(raw_offset + dst_offset));
 }
 
 }  // namespace WTF
