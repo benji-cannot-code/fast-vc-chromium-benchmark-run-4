d44c3588be711768e26dfedb30587a18916e987a
Hongchan Choi <hongchan@chromium.org>
2018-06-21 23:54:20 +0000

    Tidy up redundant call chain of FramesPerBuffer
    FramesPerBuffer() were scattered around many classes for no specific
    reason. This CL cleans them up and contains it to
    DefaultAudioDestinationNode.
    
    TBR=andrewmacp@soundtrap.com
    
    Bug: 854229
    Change-Id: I9caa0e53aadf23fac9fda19f3b0b85f5b20c4483
    Reviewed-on: https://chromium-review.googlesource.com/1110482
    Commit-Queue: Hongchan Choi <hongchan@chromium.org>
    Reviewed-by: Raymond Toy <rtoy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569459}

--- third_party/blink/renderer/modules/webaudio/default_audio_destination_node.h
+++ third_party/blink/renderer/modules/webaudio/default_audio_destination_node.h
@@ -59,7 +59,9 @@ class DefaultAudioDestinationHandler final : public AudioDestinationHandler {
   // Returns the rendering callback buffer size.
   size_t CallbackBufferSize() const override;
   double SampleRate() const override;
-  int FramesPerBuffer() const override;
+
+  // Returns a given frames-per-buffer size from audio infra.
+  int GetFramesPerBuffer() const;
 
   double TailTime() const override { return 0; }
   double LatencyTime() const override { return 0; }
@@ -88,7 +90,7 @@ class DefaultAudioDestinationNode final : public AudioDestinationNode {
   static DefaultAudioDestinationNode* Create(BaseAudioContext*,
                                              const WebAudioLatencyHint&);
 
-  size_t CallbackBufferSize() const { return Handler().CallbackBufferSize(); };
+  size_t CallbackBufferSize() const { return Handler().CallbackBufferSize(); }
 
  private:
   explicit DefaultAudioDestinationNode(BaseAudioContext&,
