ab139a22e984e16699481c08e503c30db2b5b023
Hongchan Choi <hongchan@chromium.org>
2018-06-21 18:00:39 +0000

    Removing unused members in DefaultAudioDestinationNode
    This CL removes unused 'input'-related members from
    DefaultAudioDestinationNode.
    
    Bug: 854229
    Change-Id: I8495ac736d4cb1f33c61fec243510c42a88e3fc2
    Reviewed-on: https://chromium-review.googlesource.com/1110035
    Commit-Queue: Hongchan Choi <hongchan@chromium.org>
    Reviewed-by: Raymond Toy <rtoy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569319}

--- third_party/blink/renderer/modules/webaudio/default_audio_destination_node.cc
+++ third_party/blink/renderer/modules/webaudio/default_audio_destination_node.cc
@@ -38,7 +38,6 @@ DefaultAudioDestinationHandler::DefaultAudioDestinationHandler(
     AudioNode& node,
     const WebAudioLatencyHint& latency_hint)
     : AudioDestinationHandler(node),
-      number_of_input_channels_(0),
       latency_hint_(latency_hint) {
   // Node-specific default mixing rules.
   channel_count_ = 2;
@@ -79,7 +78,6 @@ void DefaultAudioDestinationHandler::Uninitialize() {
   if (destination_->IsPlaying())
     StopDestination();
 
-  number_of_input_channels_ = 0;
   AudioHandler::Uninitialize();
 }
 
