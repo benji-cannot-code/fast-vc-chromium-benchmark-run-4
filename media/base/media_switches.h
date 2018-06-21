8f62996dd712b416d74dece1bef7b9185fa6e5fa
Xiaohan Wang <xhwang@chromium.org>
2018-06-21 00:48:17 +0000

    media: Fix hardware secure codecs support
    - On Linux/Mac/Win, require hardware secure codecs for HW_SECURE_CRYPTO
      and above. This is different than Android where we require hardware
      secure codecs for SW_SECURE_DECODE and above.
    - When there's no hardware secure video codecs supported, hardware
      secure decrypt-only (for audio) should also not be supported. Hence we
      should not add any "sucure audio decoder" in this case.
    - Add command line switch kEnableHardwareSecureCodecsForTesting.
    - Add EncryptedMediaSupportedTypesWidevineHwSecureTest to cover the
      fixes and the feature.
    
    Bug: 848532
    Test: New tests added.
    Change-Id: I21f94709da5f0352ee8a6abbfa2f9a12f50b97f6
    Reviewed-on: https://chromium-review.googlesource.com/1106712
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569107}

--- media/base/media_switches.h
+++ media/base/media_switches.h
@@ -82,6 +82,7 @@ MEDIA_EXPORT extern const char kMSEAudioBufferSizeLimitMb[];
 MEDIA_EXPORT extern const char kMSEVideoBufferSizeLimitMb[];
 
 MEDIA_EXPORT extern const char kClearKeyCdmPathForTesting[];
+MEDIA_EXPORT extern const char kEnableHardwareSecureCodecsForTesting[];
 MEDIA_EXPORT extern const char kOverrideEnabledCdmInterfaceVersion[];
 
 #if !defined(OS_ANDROID)
