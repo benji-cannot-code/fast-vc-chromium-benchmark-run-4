708eecf9ca44b0f7c654af1538c10aafe04b549b
Pedro Amaral <amaralp@chromium.org>
2018-06-21 16:49:30 +0000

    Moving ResourceManager to the Model
    The coordinator should only have references to sub coordinators and to
    the mediator.
    
    Change-Id: I6269e25f51b17f29c14730451ef67a82c2f59356
    Reviewed-on: https://chromium-review.googlesource.com/1107124
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Pedro Amaral <amaralp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569303}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/BottomToolbarCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/BottomToolbarCoordinator.java
@@ -20,7 +20,6 @@ import org.chromium.chrome.browser.modelutil.PropertyModelChangeProcessor;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 import org.chromium.chrome.browser.toolbar.BottomToolbarViewBinder.ViewHolder;
 import org.chromium.ui.resources.ResourceManager;
-import org.chromium.ui.resources.dynamics.ViewResourceAdapter;
 
 /**
  * The coordinator for the bottom toolbar. This class handles all interactions that the bottom
@@ -34,12 +33,6 @@ public class BottomToolbarCoordinator {
 
     /** The tab switcher button component that lives in the bottom toolbar. */
     private final TabSwitcherButtonCoordinator mTabSwitcherButtonCoordinator;
-
-    /** The resource ID used to reference the view bitmap in native. */
-    private final int mResourceId;
-
-    /** The resource adapter for the composited bottom toolbar view. */
-    private final ViewResourceAdapter mViewResourceAdapter;
 
     /**
      * Build the coordinator that manages the bottom toolbar.
@@ -59,9 +52,6 @@ public class BottomToolbarCoordinator {
         final ScrollingBottomViewResourceFrameLayout toolbarRoot =
                 (ScrollingBottomViewResourceFrameLayout) inflatedView;
         toolbarRoot.setTopShadowHeight(shadowHeight);
-
-        mResourceId = toolbarRoot.getId();
-        mViewResourceAdapter = toolbarRoot.getResourceAdapter();
 
         PropertyModelChangeProcessor<BottomToolbarModel, ViewHolder, PropertyKey> processor =
                 new PropertyModelChangeProcessor<>(
@@ -96,14 +86,12 @@ public class BottomToolbarCoordinator {
         mMediator.setButtonListeners(
                 searchAcceleratorListener, homeButtonListener, menuButtonListener);
         mMediator.setLayoutManager(layoutManager);
+        mMediator.setResourceManager(resourceManager);
         mMediator.setOverviewModeBehavior(overviewModeBehavior);
         mMediator.setToolbarSwipeHandler(layoutManager.getToolbarSwipeHandler());
 
         mTabSwitcherButtonCoordinator.setTabSwitcherListener(tabSwitcherListener);
         mTabSwitcherButtonCoordinator.setTabModelSelector(tabModelSelector);
-
-        resourceManager.getDynamicResourceLoader().registerResource(
-                mResourceId, mViewResourceAdapter);
     }
 
     /**
