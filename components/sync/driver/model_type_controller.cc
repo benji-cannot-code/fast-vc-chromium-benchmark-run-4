07b4a0f5eb6b7b462c9d0cd0ccf7d02271aff993
Mikel Astiz <mastiz@chromium.org>
2018-06-21 12:18:29 +0000

    Add DVLOG traces to ModelTypeController
    They are motivated by an ongoing investigation for test
    flakiness on iOS, with the suspicion that some of the codepaths
    (namely, where calls to OnSyncStopping() could be dropped) can
    cause issues (e.g. sync metadata being leaked).
    
    Bug: 854446
    Change-Id: I6fe7da40c36d00e01afa176334f59f9dc66a74e1
    Reviewed-on: https://chromium-review.googlesource.com/1108200
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569225}

--- components/sync/driver/model_type_controller.cc
+++ components/sync/driver/model_type_controller.cc
@@ -118,6 +118,7 @@ void ModelTypeController::LoadModels(
     return;
   }
 
+  DVLOG(1) << "Sync starting for " << ModelTypeToString(type());
   state_ = MODEL_STARTING;
 
   // Callback that posts back to the UI thread.
@@ -155,6 +156,10 @@ void ModelTypeController::LoadModelsDone(ConfigureResult result,
   if (state_ == NOT_RUNNING) {
     // The callback arrived on the UI thread after the type has been already
     // stopped.
+    DVLOG(1) << "Sync start completion received late for "
+             << ModelTypeToString(type()) << ", it has been stopped meanwhile";
+    // TODO(mastiz): Call to Stop() here, but think through if that's enough,
+    // because perhaps the datatype was reenabled.
     RecordStartFailure(ABORTED);
     return;
   }
@@ -162,6 +167,7 @@ void ModelTypeController::LoadModelsDone(ConfigureResult result,
   if (IsSuccessfulResult(result)) {
     DCHECK_EQ(MODEL_STARTING, state_);
     state_ = MODEL_LOADED;
+    DVLOG(1) << "Sync start completed for " << ModelTypeToString(type());
   } else {
     RecordStartFailure(result);
   }
@@ -207,6 +213,7 @@ void ModelTypeController::StartAssociating(
   DCHECK_EQ(MODEL_LOADED, state_);
 
   state_ = RUNNING;
+  DVLOG(1) << "Sync running for " << ModelTypeToString(type());
 
   // There is no association, just call back promptly.
   SyncMergeResult merge_result(type());
@@ -241,8 +248,15 @@ void ModelTypeController::Stop(SyncStopMetadataFate metadata_fate) {
   // Only call StopSync if the delegate is ready to handle it (controller is
   // in loaded state).
   if (state() == MODEL_LOADED || state() == RUNNING) {
+    DVLOG(1) << "Stopping sync for " << ModelTypeToString(type());
     PostModelTask(FROM_HERE,
                   base::BindOnce(&StopSyncHelperOnModelThread, metadata_fate));
+  } else {
+    DCHECK_EQ(MODEL_STARTING, state_);
+    DVLOG(1) << "Shortcutting stop for " << ModelTypeToString(type())
+             << " because it's still starting";
+    // TODO(mastiz): Enter STOPPING state here and/or queue pending stops,
+    // together with |metadata_fate|.
   }
 
   state_ = NOT_RUNNING;
