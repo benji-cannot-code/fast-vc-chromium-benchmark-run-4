d9210dce4715015718998e8bc030308bee9127ad
Yi Su <mrsuyi@google.com>
2018-06-21 19:35:25 +0000

    Add web page text accessibility functionality
    Inject javascript into web pages and adjust font size.
    
    Turn on flag "kWebPageTextAccessibility", then change font size from
    "Setting=>Accessibility=>Larger Text", refresh page and check it.
    
    Bug: 836962
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Iaccf85649ad992a2b99ee2b10b86c4f19ad2d419
    Reviewed-on: https://chromium-review.googlesource.com/1088704
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Yi Su <mrsuyi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569356}

--- ios/chrome/browser/web/resources/chrome_bundle_all_frames.js
+++ ios/chrome/browser/web/resources/chrome_bundle_all_frames.js
@@ -6,4 +6,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // The set of scripts to be injected into the web view as early as possible.
 goog.provide('__crWeb.chromeBundleAllFrames');
 
+goog.require('__crWeb.accessibility');
 goog.require('__crWeb.form');
