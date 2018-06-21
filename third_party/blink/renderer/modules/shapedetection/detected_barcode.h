fdec21f530bb99179e2eb7207b406b54cd284b4d
codeimpl <codeimpl@gmail.com>
2018-06-21 03:15:30 +0000

    [ShapeDetection] Change DetectedBarcode.boundingBox to DOMRectReadOnly
    The spec[1] says that the boundingBox attribute should be
    DOMRectReadOnly instead of DOMRect.
    
    [1] https://wicg.github.io/shape-detection-api/#detectedbarcode
    
    Bug: 852838
    Change-Id: Ic38665031b6e91b77f6251613b1e7ffdf072945c
    Reviewed-on: https://chromium-review.googlesource.com/1102444
    Commit-Queue: Byoungkwon Ko <codeimpl@gmail.com>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569138}

--- third_party/blink/renderer/modules/shapedetection/detected_barcode.h
+++ third_party/blink/renderer/modules/shapedetection/detected_barcode.h
@@ -13,25 +13,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class DOMRect;
+class DOMRectReadOnly;
 
 class MODULES_EXPORT DetectedBarcode final : public ScriptWrappable {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
   static DetectedBarcode* Create();
-  static DetectedBarcode* Create(String, DOMRect*, HeapVector<Point2D>);
+  static DetectedBarcode* Create(String, DOMRectReadOnly*, HeapVector<Point2D>);
 
   const String& rawValue() const;
-  DOMRect* boundingBox() const;
+  DOMRectReadOnly* boundingBox() const;
   const HeapVector<Point2D>& cornerPoints() const;
   void Trace(blink::Visitor*) override;
 
  private:
-  DetectedBarcode(String, DOMRect*, HeapVector<Point2D>);
+  DetectedBarcode(String, DOMRectReadOnly*, HeapVector<Point2D>);
 
   const String raw_value_;
-  const Member<DOMRect> bounding_box_;
+  const Member<DOMRectReadOnly> bounding_box_;
   const HeapVector<Point2D> corner_points_;
 };
 
