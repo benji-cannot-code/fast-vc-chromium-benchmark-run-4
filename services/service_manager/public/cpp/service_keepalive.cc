5dae6ec7bfb9c6ba0ca07a26b13228d18b89d36f
Christian Fremerey <chfremer@chromium.org>
2018-06-22 17:01:07 +0000

    [Video capture service] Improve delayed service quit logic
    The video capture service shuts down when not in use. The current logic
    triggers a 5 second timer when the last client disconnects. When the
    timer expires, it either stays alive if there now is a client connect or
    it shuts down if there still is no client connected.
    
    This logic is not very suitable for usage patterns where clients may
    connect frequently but only for very brief periods of time. If that
    happens, the service may end up shutting down and starting back up again
    frequently.
    
    This CL changes the logic such that when a new client connects, any previous
    timers are cancelled.
    
    Also part of this CL:
    * Move tests that are concerned with verifying when the service does or
      does not quit closer to the implementation. This allows eliminating the
      need for dynamically configuring a timeout for testing, and instead
      looking for ref counts.
    
    Bug: 829581
    Test: services_unittests --gtest_filter=DeviceFactoryProviderConnectorTest.*
    Change-Id: I788775aaf6e77159a9142994d4772200ab42a14d
    Reviewed-on: https://chromium-review.googlesource.com/1089921
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Christian Fremerey <chfremer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569666}

--- services/service_manager/public/cpp/service_keepalive.cc
+++ services/service_manager/public/cpp/service_keepalive.cc
@@ -12,9 +12,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace service_manager {
 
 ServiceKeepalive::ServiceKeepalive(ServiceContext* context,
-                                   base::TimeDelta idle_timeout)
+                                   base::TimeDelta idle_timeout,
+                                   TimeoutObserver* timeout_observer)
     : context_(context),
       idle_timeout_(idle_timeout),
+      timeout_observer_(timeout_observer),
       ref_factory_(base::BindRepeating(&ServiceKeepalive::OnRefCountZero,
                                        base::Unretained(this))),
       weak_ptr_factory_(this) {
@@ -28,12 +30,26 @@ std::unique_ptr<ServiceContextRef> ServiceKeepalive::CreateRef() {
   return ref_factory_.CreateRef();
 }
 
+bool ServiceKeepalive::HasNoRefs() {
+  return ref_factory_.HasNoRefs();
+}
+
 void ServiceKeepalive::OnRefAdded() {
+  if (idle_timer_.IsRunning() && timeout_observer_)
+    timeout_observer_->OnTimeoutCancelled();
   idle_timer_.Stop();
 }
 
 void ServiceKeepalive::OnRefCountZero() {
-  idle_timer_.Start(FROM_HERE, idle_timeout_, context_->CreateQuitClosure());
+  idle_timer_.Start(FROM_HERE, idle_timeout_,
+                    base::BindRepeating(&ServiceKeepalive::OnTimerExpired,
+                                        weak_ptr_factory_.GetWeakPtr()));
+}
+
+void ServiceKeepalive::OnTimerExpired() {
+  if (timeout_observer_)
+    timeout_observer_->OnTimeoutExpired();
+  context_->CreateQuitClosure().Run();
 }
 
 }  // namespace service_manager
--- services/video_capture/service_impl.h
+++ services/video_capture/service_impl.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/binding_set.h"
 #include "services/service_manager/public/cpp/binder_registry.h"
 #include "services/service_manager/public/cpp/service.h"
-#include "services/service_manager/public/cpp/service_context_ref.h"
+#include "services/service_manager/public/cpp/service_keepalive.h"
 #include "services/video_capture/device_factory_provider_impl.h"
 #include "services/video_capture/public/mojom/device_factory_provider.mojom.h"
 #include "services/video_capture/public/mojom/testing_controls.mojom.h"
@@ -23,12 +23,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace video_capture {
 
-class ServiceImpl : public service_manager::Service {
+class ServiceImpl : public service_manager::Service,
+                    public service_manager::ServiceKeepalive::TimeoutObserver {
  public:
-  ServiceImpl();
+  ServiceImpl(float shutdown_delay_in_seconds = 5.0f);
   ~ServiceImpl() override;
 
   static std::unique_ptr<service_manager::Service> Create();
+
+  void SetDestructionObserver(base::OnceClosure observer_cb);
+  void SetFactoryProviderClientConnectedObserver(
+      base::RepeatingClosure observer_cb);
+  void SetFactoryProviderClientDisconnectedObserver(
+      base::RepeatingClosure observer_cb);
+  void SetShutdownTimeoutCancelledObserver(base::RepeatingClosure observer_cb);
+  bool HasNoContextRefs();
 
   // service_manager::Service implementation.
   void OnStart() override;
@@ -37,31 +46,35 @@ class ServiceImpl : public service_manager::Service {
                        mojo::ScopedMessagePipeHandle interface_pipe) override;
   bool OnServiceManagerConnectionLost() override;
 
-  void SetFactoryProviderClientDisconnectedObserver(
-      const base::RepeatingClosure& observer_cb);
+  // service_manager::ServiceKeepalive::TimeoutObserver implementation.
+  void OnTimeoutExpired() override;
+  void OnTimeoutCancelled() override;
 
  private:
   void OnDeviceFactoryProviderRequest(
       mojom::DeviceFactoryProviderRequest request);
   void OnTestingControlsRequest(mojom::TestingControlsRequest request);
-  void SetShutdownDelayInSeconds(float seconds);
   void MaybeRequestQuitDelayed();
   void MaybeRequestQuit();
   void LazyInitializeDeviceFactoryProvider();
   void OnProviderClientDisconnected();
 
+  const float shutdown_delay_in_seconds_;
 #if defined(OS_WIN)
   // COM must be initialized in order to access the video capture devices.
   base::win::ScopedCOMInitializer com_initializer_;
 #endif
-  float shutdown_delay_in_seconds_;
   service_manager::BinderRegistry registry_;
   mojo::BindingSet<mojom::DeviceFactoryProvider> factory_provider_bindings_;
   std::unique_ptr<DeviceFactoryProviderImpl> device_factory_provider_;
-  std::unique_ptr<service_manager::ServiceContextRefFactory> ref_factory_;
-  // Callback to be invoked when a provider client is disconnected. Mainly used
-  // for testing.
+  std::unique_ptr<service_manager::ServiceKeepalive> ref_factory_;
+
+  // Callbacks that can optionally be set by clients.
+  base::OnceClosure destruction_cb_;
+  base::RepeatingClosure factory_provider_client_connected_cb_;
   base::RepeatingClosure factory_provider_client_disconnected_cb_;
+  base::RepeatingClosure shutdown_timeout_cancelled_cb_;
+
   base::ThreadChecker thread_checker_;
   base::WeakPtrFactory<ServiceImpl> weak_factory_;
 
