76d8b5cce34359200e1644fb52278a17955549ab
Friedrich Horschig <fhorschig@chromium.org>
2018-06-22 12:53:04 +0000

    [Android] Linking from accessory sheet to passwords
    This CL provides the native code to the "Manage passwords..." link in
    the passwords accessory bottom sheet.
    
    Bug: 853747
    Change-Id: I275171b1889d3ea85e3d8c3eda7feab8eed06495
    Reviewed-on: https://chromium-review.googlesource.com/1110131
    Commit-Queue: Friedrich Horschig <fhorschig@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569587}

--- chrome/browser/password_manager/password_accessory_controller.cc
+++ chrome/browser/password_manager/password_accessory_controller.cc
@@ -19,6 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/strings/grit/components_strings.h"
 #include "ui/base/l10n/l10n_util.h"
 
+#include "chrome/browser/android/preferences/preferences_launcher.h"
+
 using autofill::PasswordForm;
 using Item = PasswordAccessoryViewInterface::AccessoryItem;
 
@@ -102,6 +104,15 @@ void PasswordAccessoryController::OnFillingTriggered(
     return;
   }  // |driver| can be NULL if the tab is being closed.
   driver->FillIntoFocusedField(is_password, textToFill);
+}
+
+void PasswordAccessoryController::OnOptionSelected(
+    const base::string16& selectedOption) const {
+  if (selectedOption ==
+      l10n_util::GetStringUTF16(
+          IDS_PASSWORD_MANAGER_ACCESSORY_ALL_PASSWORDS_LINK)) {
+    chrome::android::PreferencesLauncher::ShowPasswordSettings();
+  }
 }
 
 gfx::NativeView PasswordAccessoryController::container_view() const {
