ac3eb8e95c5c095e40e861d052c8c9b4c7ba465a
Ulan Degenbaev <ulan@chromium.org>
2018-06-22 13:00:54 +0000

    Move bloated renderer reloading logic to BloatedRendererTabHelper.
    Currently resource_coordinator::TabLifecycleUnit controls reloading
    of a bloated renderer process and notifies the tab helper.
    
    This CL moves the renderer process reloading to the tab helper, so
    that all code related to bloated rendere is in one place.
    
    Bug: 835806,808143
    Change-Id: I1577872a1adc043a87bb2f599b211d3be9708f5c
    Reviewed-on: https://chromium-review.googlesource.com/1097481
    Commit-Queue: Ulan Degenbaev <ulan@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569588}

--- chrome/browser/resource_coordinator/tab_lifecycle_unit.h
+++ chrome/browser/resource_coordinator/tab_lifecycle_unit.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_RESOURCE_COORDINATOR_TAB_LIFECYCLE_UNIT_H_
 #define CHROME_BROWSER_RESOURCE_COORDINATOR_TAB_LIFECYCLE_UNIT_H_
 
-#include "base/gtest_prod_util.h"
 #include "base/macros.h"
 #include "base/observer_list.h"
 #include "base/time/time.h"
@@ -79,10 +78,6 @@ class TabLifecycleUnitSource::TabLifecycleUnit
   // unit.
   void UpdateLifecycleState(mojom::LifecycleState state);
 
-  // Reloads the tab because its renderer is bloated and shows an infobar
-  // explaining that it was reloaded because it ran out of memory.
-  void ReloadBloatedTab();
-
   // LifecycleUnit:
   TabLifecycleUnitExternal* AsTabLifecycleUnitExternal() override;
   base::string16 GetTitle() const override;
@@ -119,13 +114,6 @@ class TabLifecycleUnitSource::TabLifecycleUnit
   friend class TabLifecycleUnitSource;
 
  private:
-  FRIEND_TEST_ALL_PREFIXES(TabLifecycleUnitTest, CanReloadBloatedTab);
-  FRIEND_TEST_ALL_PREFIXES(TabLifecycleUnitTest, CannotReloadBloatedTabCrashed);
-  FRIEND_TEST_ALL_PREFIXES(TabLifecycleUnitTest,
-                           CannotReloadBloatedTabInvalidURL);
-  FRIEND_TEST_ALL_PREFIXES(TabLifecycleUnitTest,
-                           CannotReloadBloatedTabPendingUserInteraction);
-
   // Determines if the tab is a media tab, and populates an optional
   // |decision_details| with full details.
   bool IsMediaTabImpl(DecisionDetails* decision_details) const;
