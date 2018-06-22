230eadaa1f1e805d3906e374608fc2aa4663ab1c
Kurt Horimoto <kkhorimoto@chromium.org>
2018-06-22 21:14:35 +0000

    [iOS] Created ReadingListContextMenuCoordinator.
    This encapsulates the context menu functionality that will be shared
    across the legacy and new implementation of the reading list UI.
    
    Bug: 805209
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ie17832216d81a1b243ea45ba0cb7cb98c6805115
    Reviewed-on: https://chromium-review.googlesource.com/1101914
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569777}

---
+++ ios/chrome/browser/ui/reading_list/context_menu/reading_list_context_menu_params.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_READING_LIST_CONTEXT_MENU_READING_LIST_CONTEXT_MENU_PARAMS_H_
+#define IOS_CHROME_BROWSER_UI_READING_LIST_CONTEXT_MENU_READING_LIST_CONTEXT_MENU_PARAMS_H_
+
+#import <UIKit/UIKit.h>
+
+class GURL;
+
+// Parameter object used to construct ReadingListContextMenuCoordinators.
+@interface ReadingListContextMenuParams : NSObject
+
+// The context menu title.
+@property(nonatomic, copy) NSString* title;
+// The context menu message.
+@property(nonatomic, copy) NSString* message;
+// The location within |view| from which to display the context menu.
+@property(nonatomic, assign) CGRect rect;
+// The view from which the context menu should be displayed.
+@property(nonatomic, weak) UIView* view;
+// The URL of the reading list entry.
+@property(nonatomic, assign) GURL entryURL;
+// The offline URL of the reading list entry.
+@property(nonatomic, assign) GURL offlineURL;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_READING_LIST_CONTEXT_MENU_READING_LIST_CONTEXT_MENU_PARAMS_H_
