d9ad65d8048186c13960deaae9b8148a888b4afd
Scott Violet <sky@chromium.org>
2018-06-22 20:43:31 +0000

    chromeos: adds support for sending CANCEL_MODE events
    And this also changes ws2 to only stop cancelable events. To do otherwise
    triggers a CHECK in Event.
    
    BUG=837686
    TEST=covered by tests
    
    Change-Id: I6bc3d8b6a02eda1ecf7b7aecec6729a9ef7b1a2e
    Reviewed-on: https://chromium-review.googlesource.com/1111268
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569751}

--- services/ui/ws2/server_window.cc
+++ services/ui/ws2/server_window.cc
@@ -173,7 +173,8 @@ class ServerWindowEventHandler : public ui::EventHandler {
 
     // The event was forwarded to the remote client. We don't want it handled
     // locally too.
-    event->StopPropagation();
+    if (event->cancelable())
+      event->StopPropagation();
   }
 
  protected:
@@ -216,7 +217,8 @@ class ServerWindowEventHandler : public ui::EventHandler {
     // for a client intercepting events.
     if (server_window_->DoesOwnerInterceptEvents()) {
       server_window_->owning_window_tree()->SendEventToClient(window(), *event);
-      event->StopPropagation();
+      if (event->cancelable())
+        event->StopPropagation();
       return true;
     }
     return false;
@@ -326,7 +328,8 @@ class TopLevelEventHandler : public ServerWindowEventHandler {
     if (wm::CaptureController::Get()->GetCaptureWindow()) {
       if (server_window()->capture_owner()) {
         server_window()->capture_owner()->SendEventToClient(window(), *event);
-        event->StopPropagation();
+        if (event->cancelable())
+          event->StopPropagation();
         return;
       }
       return;
@@ -364,7 +367,7 @@ class TopLevelEventHandler : public ServerWindowEventHandler {
       }
     }
     server_window()->owning_window_tree()->SendEventToClient(window(), *event);
-    if (stop_propagation)
+    if (stop_propagation && event->cancelable())
       event->StopPropagation();
   }
 
