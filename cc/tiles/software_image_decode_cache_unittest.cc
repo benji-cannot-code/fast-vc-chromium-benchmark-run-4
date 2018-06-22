11bc9ff81e1e1d3d72e24ab5d775706f388f85f0
Khushal <khushalsagar@chromium.org>
2018-06-22 14:56:44 +0000

    cc/blink: Use same rounding for cc and ImageDecoder mip level sizes.
    The JPEGDecoder ceils when rounding floats for mip level sizes while the
    cc code floors. This results in 1 pixel differences in the scaled sizes
    when using decode to scale. Avoid this by switching the cc mip size
    calculations to match the decoder.
    
    R=ericrk@chromium.org, chrishtr@chromium.org
    
    Bug: 761565,558070
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I07069a4eff582ffbb621388ccae32b18aedebd3a
    Reviewed-on: https://chromium-review.googlesource.com/1107049
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569626}

--- cc/tiles/software_image_decode_cache_unittest.cc
+++ cc/tiles/software_image_decode_cache_unittest.cc
@@ -397,9 +397,9 @@ TEST(SoftwareImageDecodeCacheTest, ImageKeyMediumQualityAt0_1Scale) {
       draw_image, kN32_SkColorType);
   EXPECT_EQ(draw_image.frame_key(), key.frame_key());
   EXPECT_EQ(key.type(), SoftwareImageDecodeCache::CacheKey::kSubrectAndScale);
-  EXPECT_EQ(62, key.target_size().width());
+  EXPECT_EQ(63, key.target_size().width());
   EXPECT_EQ(25, key.target_size().height());
-  EXPECT_EQ(62u * 25u * 4u, key.locked_bytes());
+  EXPECT_EQ(63u * 25u * 4u, key.locked_bytes());
 }
 
 TEST(SoftwareImageDecodeCacheTest, ImageKeyMediumQualityAt0_01Scale) {
@@ -416,9 +416,9 @@ TEST(SoftwareImageDecodeCacheTest, ImageKeyMediumQualityAt0_01Scale) {
       draw_image, kN32_SkColorType);
   EXPECT_EQ(draw_image.frame_key(), key.frame_key());
   EXPECT_EQ(key.type(), SoftwareImageDecodeCache::CacheKey::kSubrectAndScale);
-  EXPECT_EQ(7, key.target_size().width());
-  EXPECT_EQ(3, key.target_size().height());
-  EXPECT_EQ(7u * 3u * 4u, key.locked_bytes());
+  EXPECT_EQ(8, key.target_size().width());
+  EXPECT_EQ(4, key.target_size().height());
+  EXPECT_EQ(8u * 4u * 4u, key.locked_bytes());
 }
 
 TEST(SoftwareImageDecodeCacheTest,
@@ -478,9 +478,9 @@ TEST(SoftwareImageDecodeCacheTest, ImageKeyHighQualityDropToMediumIfTooLarge) {
       draw_image, kN32_SkColorType);
   EXPECT_EQ(draw_image.frame_key(), key.frame_key());
   EXPECT_EQ(key.type(), SoftwareImageDecodeCache::CacheKey::kSubrectAndScale);
-  EXPECT_EQ(2277, key.target_size().width());
+  EXPECT_EQ(2278, key.target_size().width());
   EXPECT_EQ(1024, key.target_size().height());
-  EXPECT_EQ(2277u * 1024u * 4u, key.locked_bytes());
+  EXPECT_EQ(2278u * 1024u * 4u, key.locked_bytes());
 }
 
 TEST(SoftwareImageDecodeCacheTest,
@@ -1446,7 +1446,7 @@ TEST(SoftwareImageDecodeCacheTest, MediumQualityAt0_1ScaleIsHandled) {
   // Decoded image should not be lazy generated.
   EXPECT_FALSE(decoded_draw_image.image()->isLazyGenerated());
   EXPECT_EQ(kLow_SkFilterQuality, decoded_draw_image.filter_quality());
-  EXPECT_EQ(62, decoded_draw_image.image()->width());
+  EXPECT_EQ(63, decoded_draw_image.image()->width());
   EXPECT_EQ(25, decoded_draw_image.image()->height());
 
   cache.DrawWithImageFinished(draw_image, decoded_draw_image);
@@ -1477,8 +1477,8 @@ TEST(SoftwareImageDecodeCacheTest, MediumQualityAt0_01ScaleIsHandled) {
   // Decoded image should not be lazy generated.
   EXPECT_FALSE(decoded_draw_image.image()->isLazyGenerated());
   EXPECT_EQ(kLow_SkFilterQuality, decoded_draw_image.filter_quality());
-  EXPECT_EQ(7, decoded_draw_image.image()->width());
-  EXPECT_EQ(3, decoded_draw_image.image()->height());
+  EXPECT_EQ(8, decoded_draw_image.image()->width());
+  EXPECT_EQ(4, decoded_draw_image.image()->height());
 
   cache.DrawWithImageFinished(draw_image, decoded_draw_image);
   cache.UnrefImage(draw_image);
