e9cef365418438cf514dd9ddd9daefc84991ee5f
Wei Li <weili@chromium.org>
2018-06-22 23:27:07 +0000

    Reland "[MacViews] Show/hide top UI in fullscreen mode correctly"
    The original CL https://crrev.com/c/1102026 got reverted due to failed
    test BrowserViewTest.BrowserFullscreenShowTopView on
    "Mac ASan 64 Tests (1)" bot. The failure is most likely due to
    that the system preference for "always show toolbar in full screen"
    may be different on different bots. This CL explicitly set up the
    preference at the beginning of the test.
    
    Original change's description:
    > [MacViews] Show/hide top UI in fullscreen mode correctly
    >
    > Unlike Windows and Linux, Mac allows top UI to show or hide in browser
    > fullscreen mode. Also this option can be toggled at runtime to make the
    > change. But in tab fullscreen mode, top UI should always hide. These
    > require propagating fullscreen and user preference changes, recording
    > the toolbar styles and applying the layout accordingly.
    >
    > This CL makes sure the top chrome UI status is correct across different
    > fullscreen modes, and responds to user changes as well. Some tests are
    > also added.
    >
    > BUG=851261,831219,852779
    > TEST=Pls check the toolbar/tabstrip show or hide correctly when
    > entering and exiting various fullscreen modes. Bookmark bar, slide-down
    > menu and traffic lights will be fixed later.
    >
    > Change-Id: I78a2b68821d5e633ac5c2823f65390809a57dad5
    > Reviewed-on: https://chromium-review.googlesource.com/1102026
    > Commit-Queue: Wei Li <weili@chromium.org>
    > Reviewed-by: Trent Apted <tapted@chromium.org>
    > Reviewed-by: Sarah Chan <spqchan@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#569301}
    
    Change-Id: I37a2641b5a578dabcf922792158c0f13b2e197e8
    Reviewed-on: https://chromium-review.googlesource.com/1111015
    Commit-Queue: Wei Li <weili@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Reviewed-by: Sarah Chan <spqchan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569836}

--- chrome/browser/ui/views/frame/browser_view.h
+++ chrome/browser/ui/views/frame/browser_view.h
@@ -257,7 +257,6 @@ class BrowserView : public BrowserWindow,
 
   // Called after the widget's fullscreen state is changed without going through
   // FullscreenController. This method does any processing which was skipped.
-  // Only exiting fullscreen in this way is currently supported.
   void FullscreenStateChanged();
 
   // Sets the button provider for this BrowserView. Must be called before
@@ -471,6 +470,7 @@ class BrowserView : public BrowserWindow,
 
   // ExclusiveAccessContext:
   Profile* GetProfile() override;
+  void UpdateUIForTabFullscreen(TabFullscreenState state) override;
   content::WebContents* GetActiveWebContents() override;
   void HideDownloadShelf() override;
   void UnhideDownloadShelf() override;
