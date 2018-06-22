a23ab973e08ced8a409ac8f4344acd56a15f341e
Jérôme Lebel <jlebel@chromium.org>
2018-06-22 14:46:06 +0000

    [iOS] Disabling Google services settings eg_tests without RefreshUI
    Google services settings will be released with M70. There is no use
    to test it without RefreshUI flag.
    
    Bug: 827072
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ib6a39f4ecae6c79f5c0729d8f347eb457d85da94
    Reviewed-on: https://chromium-review.googlesource.com/1111612
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569620}

--- ios/chrome/browser/ui/settings/google_services_settings_egtest.mm
+++ ios/chrome/browser/ui/settings/google_services_settings_egtest.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <EarlGrey/EarlGrey.h>
 #import <XCTest/XCTest.h>
 
+#include "ios/chrome/browser/ui/ui_util.h"
 #include "ios/chrome/grit/ios_strings.h"
 #include "ios/chrome/test/earl_grey/accessibility_util.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
@@ -28,6 +29,8 @@ using chrome_test_util::SettingsDoneButton;
 
 // Opens the Google services settings view, and closes it.
 - (void)testOpenGoogleServicesSettings {
+  if (!IsUIRefreshPhase1Enabled())
+    EARL_GREY_TEST_SKIPPED(@"This test is UIRefresh only.");
   [ChromeEarlGreyUI openSettingsMenu];
   [ChromeEarlGreyUI tapSettingsMenuButton:GoogleServicesSettingsButton()];
 
