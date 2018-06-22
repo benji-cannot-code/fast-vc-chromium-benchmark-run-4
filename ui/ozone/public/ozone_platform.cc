cac85d3cf276826bf958f6c59b13318cc4977a7a
Sergey Ulanov <sergeyu@chromium.org>
2018-06-22 18:39:29 +0000

    [Ozone, Fuchsia] Add OzonePlatform::GetPlatformProperties()
    Scenic Ozone platform requires view_owner_request field to be set in
    PlatformWindowInitProperties when creating a window. Previously
    there was no mechanism for upper layers to discover that the current
    Ozone platform expects that field. To solve this problem this CL adds
    GetPlatformProperties() method in OzonePlatform. The returned
    PlatformProperties struct contains just one field, but in future it
    can be extended as needed for other platforms.
    
    Bug: 829980
    Change-Id: Idcd8c1b645cdeca36975c7feb5a1cf720fd592dd
    Reviewed-on: https://chromium-review.googlesource.com/1110877
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569706}

--- ui/ozone/public/ozone_platform.cc
+++ ui/ozone/public/ozone_platform.cc
@@ -21,6 +21,10 @@ bool g_platform_initialized_gpu = false;
 base::LazyInstance<base::OnceCallback<void(OzonePlatform*)>>::Leaky
     instance_callback = LAZY_INSTANCE_INITIALIZER;
 
+constexpr OzonePlatform::PlatformProperties kDefaultPlatformProperties = {
+    /*needs_view_owner_request=*/false,
+};
+
 base::Lock& GetOzoneInstanceLock() {
   static base::Lock lock;
   return lock;
@@ -116,6 +120,11 @@ OzonePlatform* OzonePlatform::instance_ = nullptr;
 
 IPC::MessageFilter* OzonePlatform::GetGpuMessageFilter() {
   return nullptr;
+}
+
+const OzonePlatform::PlatformProperties&
+OzonePlatform::GetPlatformProperties() {
+  return kDefaultPlatformProperties;
 }
 
 base::MessageLoop::Type OzonePlatform::GetMessageLoopTypeForGpu() {
