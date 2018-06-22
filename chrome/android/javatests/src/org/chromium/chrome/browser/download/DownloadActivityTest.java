49f3388363ce5dab44ee259c68d15d5d9fee84d1
Darren Shen <shend@chromium.org>
2018-06-22 03:37:50 +0000

    Disable flaky chrome.browser.download.DownloadActivityTest tests
    TBR=dtrainor@chromium.org
    
    Bug: 855219, 855168, 855167, 854241
    Change-Id: I555f125d6d581e4f7d0361e61bc6a3d2963d37a1
    Reviewed-on: https://chromium-review.googlesource.com/1111483
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569525}

--- chrome/android/javatests/src/org/chromium/chrome/browser/download/DownloadActivityTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/download/DownloadActivityTest.java
@@ -29,6 +29,7 @@ import org.junit.runner.RunWith;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CallbackHelper;
+import org.chromium.base.test.util.FlakyTest;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.chrome.R;
@@ -159,6 +160,7 @@ public class DownloadActivityTest {
 
     @Test
     @MediumTest
+    @FlakyTest(message = "crbug.com/855168")
     public void testSpaceDisplay() throws Exception {
         // This first check is a Criteria because initialization of the Adapter is asynchronous.
         CriteriaHelper.pollUiThread(new Criteria() {
@@ -252,6 +254,7 @@ public class DownloadActivityTest {
     @Test
     @MediumTest
     @RetryOnFailure
+    @FlakyTest(message = "crbug.com/854241")
     public void testDeleteFiles() throws Exception {
         SnackbarManager.setDurationForTesting(1);
 
@@ -291,6 +294,7 @@ public class DownloadActivityTest {
     @Test
     @MediumTest
     @RetryOnFailure
+    @FlakyTest(message = "crbug.com/855219")
     public void testDeleteFileFromMenu() throws Exception {
         SnackbarManager.setDurationForTesting(1);
 
@@ -522,6 +526,7 @@ public class DownloadActivityTest {
     @Test
     @MediumTest
     @DisableFeatures("OfflinePagesSharing")
+    @FlakyTest(message = "crbug.com/855167")
     public void testShareFiles() throws Exception {
         // Adapter positions:
         // 0 = space display
