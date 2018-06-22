f155f002de07e6b89c70c9f7e73bd8094d4df446
Jialiu Lin <jialiul@chromium.org>
2018-06-22 03:46:42 +0000

    Enable PhishGuard enterprise feature by default
    Since this feature requires configuring enterprise policy settings to
    function, Finch roll out is not necessary. We'll roll it out in M69.
    This CL enables this feature by default. The feature const and feature
    control code will be removed shortly after we get launch approval.
    
    Bug: 853286
    Change-Id: I15d15cf03a4010b67009bcf951119fb1f7f30f86
    Reviewed-on: https://chromium-review.googlesource.com/1110340
    Commit-Queue: Jialiu Lin <jialiul@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569527}

--- components/safe_browsing/features.cc
+++ components/safe_browsing/features.cc
@@ -48,7 +48,7 @@ const base::Feature kInspectDownloadedRarFiles{
     "InspectDownloadedRarFiles", base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kEnterprisePasswordProtectionV1{
-    "EnterprisePasswordProtectionV1", base::FEATURE_DISABLED_BY_DEFAULT};
+    "EnterprisePasswordProtectionV1", base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kForceEnableResetPasswordWebUI{
     "ForceEnableResetPasswordWebUI", base::FEATURE_DISABLED_BY_DEFAULT};
@@ -64,7 +64,6 @@ constexpr struct {
 } kExperimentalFeatures[]{
     {&kAdSamplerTriggerFeature, false},
     {&kCheckByURLLoaderThrottle, true},
-    {&kEnterprisePasswordProtectionV1, true},
     {&kForceEnableResetPasswordWebUI, true},
     {&kInspectDownloadedRarFiles, true},
     {&kSuspiciousSiteTriggerQuotaFeature, false},
