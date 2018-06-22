1130ecbe6954b767ea795a19569de055bf5fcc9b
Kim Paulhamus <kpaulhamus@chromium.org>
2018-06-22 19:33:25 +0000

    Make the call to Fido2ApiHandlerInternal.getAssertion.
    Removes the NOT_IMPLEMENTED placeholder.
    
    Bug: 678885
    Change-Id: I4572dc2e6aad8019b383b93173b7962cc97ad60d
    Reviewed-on: https://chromium-review.googlesource.com/1109498
    Commit-Queue: Kim Paulhamus <kpaulhamus@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569734}

--- chrome/android/java/src/org/chromium/chrome/browser/webauth/AuthenticatorImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webauth/AuthenticatorImpl.java
@@ -61,7 +61,7 @@ public class AuthenticatorImpl implements Authenticator, HandlerResponseCallback
         }
 
         mIsOperationPending = true;
-        onError(AuthenticatorStatus.NOT_IMPLEMENTED);
+        Fido2ApiHandler.getInstance().getAssertion(options, mRenderFrameHost, this);
     }
 
     @Override
@@ -89,7 +89,8 @@ public class AuthenticatorImpl implements Authenticator, HandlerResponseCallback
 
     @Override
     public void onError(Integer status) {
-        assert(mMakeCredentialCallback != null || mGetAssertionCallback != null);
+        assert((mMakeCredentialCallback != null && mGetAssertionCallback == null)
+                || (mMakeCredentialCallback == null && mGetAssertionCallback != null));
         if (mMakeCredentialCallback != null) {
             mMakeCredentialCallback.call(status, null);
         } else if (mGetAssertionCallback != null) {
