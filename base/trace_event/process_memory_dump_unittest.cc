b7957f78cadb39d16eacb7541eedca8ec97bf07d
Siddhartha <ssid@chromium.org>
2018-06-22 13:50:34 +0000

    GetAllocatorDump should not return black hold dump
    GetAllocatorDump is used to check if dump was created. Do not return
    black hole dump from here.
    
    BUG=854668
    
    Change-Id: Ife41be2622e9476d8e45586ace3ea316012341c3
    Reviewed-on: https://chromium-review.googlesource.com/1111017
    Commit-Queue: Helen Li <xunjieli@chromium.org>
    Reviewed-by: Helen Li <xunjieli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569596}

--- base/trace_event/process_memory_dump_unittest.cc
+++ base/trace_event/process_memory_dump_unittest.cc
@@ -387,6 +387,10 @@ TEST(ProcessMemoryDumpTest, BackgroundModeTest) {
   SetAllocatorDumpNameWhitelistForTesting(kTestDumpNameWhitelist);
   MemoryAllocatorDump* black_hole_mad = pmd->GetBlackHoleMad();
 
+  // GetAllocatorDump works for uncreated dumps.
+  EXPECT_EQ(nullptr, pmd->GetAllocatorDump("NotWhitelisted/TestName"));
+  EXPECT_EQ(nullptr, pmd->GetAllocatorDump("Whitelisted/TestName"));
+
   // Invalid dump names.
   EXPECT_EQ(black_hole_mad,
             pmd->CreateAllocatorDump("NotWhitelisted/TestName"));
@@ -420,7 +424,7 @@ TEST(ProcessMemoryDumpTest, BackgroundModeTest) {
             pmd->CreateAllocatorDump("Whitelisted/0xaB/TestName"));
 
   // GetAllocatorDump is consistent.
-  EXPECT_EQ(black_hole_mad, pmd->GetAllocatorDump("NotWhitelisted/TestName"));
+  EXPECT_EQ(nullptr, pmd->GetAllocatorDump("NotWhitelisted/TestName"));
   EXPECT_NE(black_hole_mad, pmd->GetAllocatorDump("Whitelisted/TestName"));
 
   // Test whitelisted entries.
