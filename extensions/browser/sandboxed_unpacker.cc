c080d9e09b577adb51dcbd13df19902bb247be2c
Karan Bhatia <karandeepb@chromium.org>
2018-06-22 21:28:07 +0000

    SandboxedUnpacker: Remove redundant PostTask usages.
    This CL removes two redundant PostTask usages from SandboxedUnpacker. In both
    the cases, we are already on the correct sequence, hence we can call the
    function synchronously.
    
    BUG=None
    
    Change-Id: I42deaad16bce1db2e2e1905aa13253251791c8b6
    Reviewed-on: https://chromium-review.googlesource.com/1110839
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569784}

--- extensions/browser/sandboxed_unpacker.cc
+++ extensions/browser/sandboxed_unpacker.cc
@@ -352,6 +352,10 @@ void SandboxedUnpacker::StartWithCrx(const CRXFileInfo& crx_info) {
 void SandboxedUnpacker::StartWithDirectory(const std::string& extension_id,
                                            const std::string& public_key,
                                            const base::FilePath& directory) {
+  // We assume that we are started on the thread that the client wants us
+  // to do file IO on.
+  DCHECK(unpacker_io_task_runner_->RunsTasksInCurrentSequence());
+
   extension_id_ = extension_id;
   public_key_ = public_key;
   if (!CreateTempDirectory())
@@ -369,9 +373,7 @@ void SandboxedUnpacker::StartWithDirectory(const std::string& extension_id,
     return;
   }
 
-  unpacker_io_task_runner_->PostTask(
-      FROM_HERE,
-      base::BindOnce(&SandboxedUnpacker::Unpack, this, extension_root_));
+  Unpack(extension_root_);
 }
 
 SandboxedUnpacker::~SandboxedUnpacker() {
@@ -411,11 +413,8 @@ void SandboxedUnpacker::UnzipDone(const base::FilePath& zip_file,
   DCHECK(unpacker_io_task_runner_->RunsTasksInCurrentSequence());
 
   if (!error.empty()) {
-    unpacker_io_task_runner_->PostTask(
-        FROM_HERE, base::BindOnce(&SandboxedUnpacker::ReportFailure, this,
-                                  SandboxedUnpackerFailureReason::UNZIP_FAILED,
-                                  l10n_util::GetStringUTF16(
-                                      IDS_EXTENSION_PACKAGE_UNZIP_ERROR)));
+    ReportFailure(SandboxedUnpackerFailureReason::UNZIP_FAILED,
+                  l10n_util::GetStringUTF16(IDS_EXTENSION_PACKAGE_UNZIP_ERROR));
     return;
   }
 
