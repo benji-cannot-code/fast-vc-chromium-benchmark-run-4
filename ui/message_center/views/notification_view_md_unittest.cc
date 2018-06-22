bd613f393478143aa3c70d86a7ab398e7d772744
Evan Stade <estade@chromium.org>
2018-06-22 22:03:38 +0000

    NotificationViewMd clean up.
    Change-Id: I74290b96dbfe84bf180c8809ad349d4ae81cb125
    Reviewed-on: https://chromium-review.googlesource.com/1103596
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569801}

--- ui/message_center/views/notification_view_md_unittest.cc
+++ ui/message_center/views/notification_view_md_unittest.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/button/image_button.h"
 #include "ui/views/controls/button/label_button.h"
 #include "ui/views/controls/button/radio_button.h"
+#include "ui/views/controls/textfield/textfield.h"
 #include "ui/views/test/views_test_base.h"
 #include "ui/views/test/widget_test.h"
 
