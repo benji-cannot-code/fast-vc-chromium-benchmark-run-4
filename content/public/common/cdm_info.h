4160bc081b3728c2e9e54cdff0302163561f8c3b
Xiaohan Wang <xhwang@chromium.org>
2018-06-23 21:01:25 +0000

    media: Add supported CdmProxy::Protocol to CdmCapability
    Also parse supported CdmProxy::Protocol from CDM manifest. For CDMs
    supporting hardware secure decryption, the supported CdmProxy protocol
    should be specified in CDM manifest, e.g. for kIntel protocol:
    
    "x-cdm-supported-cdm-proxy-protocols": ["intel"],
    
    In the next CL, we'll check whether the supported CdmProxy protocol
    is also supported by the system.
    
    Bug: 848532
    Test: Updated unit tests.
    Change-Id: I59567943620e672d72f72fdadd80cb3ce6c5bcfd
    Reviewed-on: https://chromium-review.googlesource.com/1111109
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569889}

--- content/public/common/cdm_info.h
+++ content/public/common/cdm_info.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // rename it to EncryptionScheme.
 #include "media/base/decrypt_config.h"
 #include "media/base/video_codecs.h"
+#include "media/cdm/cdm_proxy.h"
 
 namespace content {
 
@@ -26,7 +27,8 @@ struct CONTENT_EXPORT CdmCapability {
   CdmCapability();
   CdmCapability(std::vector<media::VideoCodec> video_codecs,
                 base::flat_set<media::EncryptionMode> encryption_schemes,
-                base::flat_set<media::CdmSessionType> session_types);
+                base::flat_set<media::CdmSessionType> session_types,
+                base::flat_set<media::CdmProxy::Protocol> cdm_proxy_protocols);
   CdmCapability(const CdmCapability& other);
   ~CdmCapability();
 
@@ -42,6 +44,10 @@ struct CONTENT_EXPORT CdmCapability {
 
   // List of session types supported by the CDM.
   base::flat_set<media::CdmSessionType> session_types;
+
+  // List of CdmProxy protocols supported by the CDM. These protocols should
+  // also be supported by the system to support hardware secure decryption.
+  base::flat_set<media::CdmProxy::Protocol> cdm_proxy_protocols;
 };
 
 // Represents a Content Decryption Module implementation and its capabilities.
