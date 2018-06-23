29ec58e14a0ac4417b9c589dbb6e875fc0c86592
Sergey Ulanov <sergeyu@chromium.org>
2018-06-23 22:48:20 +0000

    [Fuchsia] Enable ozone_demo and ozone_gl_unittests targets
    Also enabled gl_image_native_pixmap.cc on Fuchisa. ExportHandle() is
    currently stubbed as NOTIMPLEMENTED.
    
    Bug: 852011, 766360
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I2978a320fef928f32b3ff27473c593293a8168d6
    Reviewed-on: https://chromium-review.googlesource.com/1112592
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Michael Spang <spang@chromium.org>
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569912}

--- ui/gl/gl_image_native_pixmap.cc
+++ ui/gl/gl_image_native_pixmap.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#include "base/files/scoped_file.h"
+#include "build/build_config.h"
 #include "ui/gfx/buffer_format_util.h"
 #include "ui/gl/egl_util.h"
 #include "ui/gl/gl_context.h"
@@ -283,6 +285,11 @@ gfx::NativePixmapHandle GLImageNativePixmap::ExportHandle() {
     }
   }
 
+#if defined(OS_FUCHSIA)
+  // TODO(crbug.com/852011): Implement image handle export on Fuchsia.
+  NOTIMPLEMENTED();
+  return gfx::NativePixmapHandle();
+#else   // defined(OS_FUCHSIA)
   std::vector<int> fds(num_planes);
   std::vector<EGLint> strides(num_planes);
   std::vector<EGLint> offsets(num_planes);
@@ -318,6 +325,7 @@ gfx::NativePixmapHandle GLImageNativePixmap::ExportHandle() {
   }
 
   return handle;
+#endif  // !defined(OS_FUCHSIA)
 }
 
 unsigned GLImageNativePixmap::GetInternalFormat() {
