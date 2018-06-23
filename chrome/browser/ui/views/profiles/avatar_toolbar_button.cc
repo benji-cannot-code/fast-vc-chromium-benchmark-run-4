bf00d27b323a943380cda7a6e64ddc38787716c6
Bret Sepulveda <bsep@chromium.org>
2018-06-23 01:23:41 +0000

    Revert "Show promo account icon in toolbar when user is signed out"
    This reverts commit c5604690c12b5b998c137d6e67c46b5d1d7c626b.
    
    Reason for revert: Causes BrowserViewTest suite to fail under Refresh on Linux. See crbug.com/855797
    
    Original change's description:
    > Show promo account icon in toolbar when user is signed out
    > 
    > When the user is signed out of Chrome and the profile icon
    > has not been explicitly changed, AvatarToolbarButton now
    > uses the account icon of the first sync promo account.
    > 
    > Screenshots:
    > https://drive.google.com/file/d/1a7kr12KtA11Wt7MQ9MLnSf-M3D9nlLdg/view?usp=sharing
    > https://drive.google.com/file/d/1nKLnoD1sbcZOvwGQY3YtHVTUv32DQNFn/view?usp=sharing
    > 
    > Bug: 853363
    > Change-Id: I11ed80e2250bc4eb9202e4a1c4cabf242954f726
    > Reviewed-on: https://chromium-review.googlesource.com/1105772
    > Commit-Queue: Thomas Tangl <tangltom@chromium.org>
    > Reviewed-by: David Roger <droger@chromium.org>
    > Reviewed-by: Trent Apted <tapted@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#569213}
    
    TBR=droger@chromium.org,tapted@chromium.org,tangltom@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 853363
    Change-Id: Ib8d9a5350277146050677a2676577d29b315efda
    Reviewed-on: https://chromium-review.googlesource.com/1112599
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Commit-Queue: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569870}

--- chrome/browser/ui/views/profiles/avatar_toolbar_button.cc
+++ chrome/browser/ui/views/profiles/avatar_toolbar_button.cc
@@ -15,10 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/profiles/profiles_state.h"
 #include "chrome/browser/signin/account_consistency_mode_manager.h"
-#include "chrome/browser/signin/account_tracker_service_factory.h"
-#include "chrome/browser/signin/gaia_cookie_manager_service_factory.h"
 #include "chrome/browser/signin/signin_manager_factory.h"
-#include "chrome/browser/signin/signin_ui_util.h"
 #include "chrome/browser/sync/sync_ui_util.h"
 #include "chrome/browser/themes/theme_properties.h"
 #include "chrome/browser/ui/layout_constants.h"
@@ -41,18 +38,9 @@ AvatarToolbarButton::AvatarToolbarButton(Profile* profile,
 #if !defined(OS_CHROMEOS)
       error_controller_(this, profile_),
 #endif  // !defined(OS_CHROMEOS)
-      profile_observer_(this),
-      cookie_manager_service_observer_(this),
-      account_tracker_service_observer_(this) {
+      profile_observer_(this) {
   profile_observer_.Add(
       &g_browser_process->profile_manager()->GetProfileAttributesStorage());
-
-  if (!IsIncognito() && !profile_->IsGuestSession()) {
-    cookie_manager_service_observer_.Add(
-        GaiaCookieManagerServiceFactory::GetForProfile(profile_));
-    account_tracker_service_observer_.Add(
-        AccountTrackerServiceFactory::GetForProfile(profile_));
-  }
 
   SetImageAlignment(HorizontalAlignment::ALIGN_CENTER,
                     VerticalAlignment::ALIGN_MIDDLE);
@@ -135,18 +123,6 @@ void AvatarToolbarButton::OnProfileNameChanged(
   UpdateTooltipText();
 }
 
-void AvatarToolbarButton::OnGaiaAccountsInCookieUpdated(
-    const std::vector<gaia::ListedAccount>& accounts,
-    const std::vector<gaia::ListedAccount>& signed_out_accounts,
-    const GoogleServiceAuthError& error) {
-  UpdateIcon();
-}
-
-void AvatarToolbarButton::OnAccountImageUpdated(const std::string& account_id,
-                                                const gfx::Image& image) {
-  UpdateIcon();
-}
-
 bool AvatarToolbarButton::IsIncognito() const {
   return profile_->IsOffTheRecord() && !profile_->IsGuestSession();
 }
@@ -156,10 +132,6 @@ bool AvatarToolbarButton::ShouldShowGenericIcon() const {
   // sessions should be handled separately and never call this function.
   DCHECK(!profile_->IsGuestSession());
   DCHECK(!profile_->IsOffTheRecord());
-#if !defined(OS_CHROMEOS)
-  if (!signin_ui_util::GetAccountsForDicePromos(profile_).empty())
-    return false;
-#endif  // !defined(OS_CHROMEOS)
   return g_browser_process->profile_manager()
                  ->GetProfileAttributesStorage()
                  .GetNumberOfProfiles() == 1 &&
@@ -251,20 +223,6 @@ gfx::Image AvatarToolbarButton::GetIconImageFromProfile() const {
       return *gaia_image;
     return gfx::Image();
   }
-
-#if !defined(OS_CHROMEOS)
-  // If the user isn't signed in and the profile icon wasn't changed explicitly,
-  // try to use the first account icon of the sync promo.
-  if (!SigninManagerFactory::GetForProfile(profile_)->IsAuthenticated() &&
-      entry->GetAvatarIconIndex() == 0) {
-    std::vector<AccountInfo> promo_accounts =
-        signin_ui_util::GetAccountsForDicePromos(profile_);
-    if (!promo_accounts.empty()) {
-      return AccountTrackerServiceFactory::GetForProfile(profile_)
-          ->GetAccountImage(promo_accounts[0].account_id);
-    }
-  }
-#endif  // !defined(OS_CHROMEOS)
 
   return entry->GetAvatarIcon();
 }
