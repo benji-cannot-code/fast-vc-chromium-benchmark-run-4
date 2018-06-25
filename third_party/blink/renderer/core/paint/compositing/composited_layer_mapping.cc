db9d3abbd5d903f3afa230e14b1b96c42a5109a4
Philip Rogers <pdr@chromium.org>
2018-06-25 16:38:21 +0000

    Always invalidate layers when the offset from layout object changes
    This patch removes an optimization added in [1] to not invalidate
    graphics layers when the offset from layout object changes. This
    offset is no longer used for scroll offset so the original intent
    no longer applies [2]. Removing this optimization causes an additional
    invalidation in a rare case (vertical writing mode scroll origin
    changes) but reduces complexity in a difficult area of code.
    
    This patch also removes a SetNeedsDisplay of the scrolling contents when
    the scroll layer's offset changes. The scrolling contents layer is now
    only invalidated if the offset actually changes. No tests were affected
    by this change.
    
    In addition, all callsites were setting the offset_from_layout_object_
    to an IntSize, so the type has been updated from DoubleSize to IntSize.
    
    [1] https://trac.webkit.org/changeset/134628/webkit
    [2] https://chromium.googlesource.com/chromium/src/+/0241ab94255bdc1fcb0c84b6cd2a0937ecf9fb90
    
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ib359ad5e3c8dda61a06b5050625f99f204b558cc
    Reviewed-on: https://chromium-review.googlesource.com/1112916
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570064}

--- third_party/blink/renderer/core/paint/compositing/composited_layer_mapping.cc
+++ third_party/blink/renderer/core/paint/compositing/composited_layer_mapping.cc
@@ -1608,8 +1608,6 @@ void CompositedLayerMapping::UpdateScrollingLayerGeometry(
   bool scroll_container_size_changed =
       old_scroll_container_size != scrolling_layer_->Size();
 
-  IntSize old_scrolling_layer_offset =
-      scrolling_layer_->OffsetFromLayoutObject();
   scrolling_layer_->SetOffsetFromLayoutObject(
       ToIntSize(overflow_clip_rect.Location()));
 
@@ -1623,9 +1621,6 @@ void CompositedLayerMapping::UpdateScrollingLayerGeometry(
         ToIntSize(overflow_clip_rect.Location()));
   }
 
-  bool overflow_clip_rect_offset_changed =
-      old_scrolling_layer_offset != scrolling_layer_->OffsetFromLayoutObject();
-
   IntSize scroll_size =
       PixelSnappedIntRect(
           LayoutRect(
@@ -1634,9 +1629,6 @@ void CompositedLayerMapping::UpdateScrollingLayerGeometry(
           .Size();
   // Ensure scrolling contents are at least as large as the scroll clip
   scroll_size = scroll_size.ExpandedTo(overflow_clip_rect.Size());
-
-  if (overflow_clip_rect_offset_changed)
-    scrolling_contents_layer_->SetNeedsDisplay();
 
   FloatPoint scroll_position =
       owning_layer_.GetScrollableArea()->ScrollPosition();
@@ -1672,11 +1664,8 @@ void CompositedLayerMapping::UpdateScrollingLayerGeometry(
   }
   scrolling_contents_layer_offset_from_layout_object.MoveBy(
       overflow_clip_rect.Location());
-
-  scrolling_contents_layer_->SetOffsetDoubleFromLayoutObject(
-      DoubleSize(scrolling_contents_layer_offset_from_layout_object.X(),
-                 scrolling_contents_layer_offset_from_layout_object.Y()),
-      GraphicsLayer::kDontSetNeedsDisplay);
+  scrolling_contents_layer_->SetOffsetFromLayoutObject(
+      ToIntSize(scrolling_contents_layer_offset_from_layout_object));
 }
 
 void CompositedLayerMapping::UpdateChildClippingMaskLayerGeometry() {
--- third_party/blink/renderer/platform/graphics/graphics_layer.cc
+++ third_party/blink/renderer/platform/graphics/graphics_layer.cc
@@ -265,24 +265,16 @@ void GraphicsLayer::RemoveFromParent() {
   CcLayer()->RemoveFromParent();
 }
 
-void GraphicsLayer::SetOffsetFromLayoutObject(
-    const IntSize& offset,
-    ShouldSetNeedsDisplay should_set_needs_display) {
-  SetOffsetDoubleFromLayoutObject(DoubleSize(offset));
-}
-
-void GraphicsLayer::SetOffsetDoubleFromLayoutObject(
-    const DoubleSize& offset,
-    ShouldSetNeedsDisplay should_set_needs_display) {
+void GraphicsLayer::SetOffsetFromLayoutObject(const IntSize& offset) {
   if (offset == offset_from_layout_object_)
     return;
 
   offset_from_layout_object_ = offset;
-  CcLayer()->SetFiltersOrigin(FloatPoint() - ToFloatSize(offset));
+  CcLayer()->SetFiltersOrigin(FloatPoint() -
+                              FloatSize(offset_from_layout_object_));
 
   // If the compositing layer offset changes, we need to repaint.
-  if (should_set_needs_display == kSetNeedsDisplay)
-    SetNeedsDisplay();
+  SetNeedsDisplay();
 }
 
 LayoutSize GraphicsLayer::OffsetFromLayoutObjectWithSubpixelAccumulation()
@@ -773,7 +765,7 @@ std::unique_ptr<JSONObject> GraphicsLayer::LayerAsJSONInternal(
     json->SetArray("position", PointAsJSONArray(position));
 
   if (flags & kLayerTreeIncludesDebugInfo &&
-      offset_from_layout_object_ != DoubleSize()) {
+      offset_from_layout_object_ != IntSize()) {
     json->SetArray("offsetFromLayoutObject",
                    SizeAsJSONArray(offset_from_layout_object_));
   }
