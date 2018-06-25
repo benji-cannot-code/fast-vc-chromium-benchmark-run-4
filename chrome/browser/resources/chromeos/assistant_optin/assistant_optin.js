19ac01fb4f38a2fc23168c6d9d7b69ddd4c61438
Yue Li <updowndota@chromium.org>
2018-06-25 18:53:53 +0000

    Migrate methods from AssitantOptInUI into Handler
    - migrate many methods into the handler
    - update loading/error screen specs
    
    Bug: b/78190629
    Test: Manual
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: I7a590f547cc85309f982c58421f0984721a4f04b
    Reviewed-on: https://chromium-review.googlesource.com/1112716
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570113}

--- chrome/browser/resources/chromeos/assistant_optin/assistant_optin.js
+++ chrome/browser/resources/chromeos/assistant_optin/assistant_optin.js
@@ -96,6 +96,10 @@ cr.define('assistantOptin', function() {
       }
     },
 
+    closeDialog: function() {
+      chrome.send('dialogClose');
+    },
+
     /**
      * Show the given screen.
      *
