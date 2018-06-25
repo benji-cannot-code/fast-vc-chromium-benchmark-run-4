1f5132f98350d5b7e44cd2cc4b26ce71bd365e2a
Peng Huang <penghuang@chromium.org>
2018-06-25 15:50:31 +0000

    Revert "Fork AudioInputController to the audio service."
    This reverts commit 040f2ad0aed73dba73467bc500e01c50a3adb628.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Original change's description:
    > Fork AudioInputController to the audio service.
    > 
    > Also migrate the audio service to it. We want to add audio processing
    > capabilities to the audio service, and the media/ code cannot depend
    > on services/audio/, so we need the AudioInputController code in the
    > audio service. There's a very sketched design doc for processing in the
    > audio service at
    > https://docs.google.com/document/d/1HJnii4kuBXshVM202TVPJt_9iDmlaCX-VhatjRxRbC4/edit#.
    > 
    > Bug: 851959
    > Change-Id: Icabef727e6d309f86472d2fb401d7383051fa0ae
    > Reviewed-on: https://chromium-review.googlesource.com/1104464
    > Commit-Queue: Max Morin <maxmorin@chromium.org>
    > Reviewed-by: Olga Sharonova <olka@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#570014}
    
    TBR=olka@chromium.org,maxmorin@chromium.org
    
    Change-Id: I3727f7454ec28718e327335baec3960365afbe93
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 851959,856215
    Reviewed-on: https://chromium-review.googlesource.com/1113837
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570050}

--- services/audio/input_stream.cc
+++ services/audio/input_stream.cc
@@ -10,13 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind_helpers.h"
 #include "base/trace_event/trace_event.h"
+#include "media/audio/audio_input_sync_writer.h"
 #include "media/audio/audio_manager.h"
 #include "media/base/audio_parameters.h"
 #include "media/base/user_input_monitor.h"
 #include "mojo/public/cpp/system/buffer.h"
 #include "mojo/public/cpp/system/handle.h"
 #include "mojo/public/cpp/system/platform_handle.h"
-#include "services/audio/input_sync_writer.h"
 #include "services/audio/user_input_monitor.h"
 
 namespace audio {
@@ -46,7 +46,7 @@ InputStream::InputStream(CreatedCallback created_callback,
       created_callback_(std::move(created_callback)),
       delete_callback_(std::move(delete_callback)),
       foreign_socket_(),
-      writer_(InputSyncWriter::Create(
+      writer_(media::AudioInputSyncWriter::Create(
           log_ ? base::BindRepeating(&media::mojom::AudioLog::OnLogMessage,
                                      base::Unretained(log_->get()))
                : base::DoNothing(),
@@ -89,9 +89,9 @@ InputStream::InputStream(CreatedCallback created_callback,
     return;
   }
 
-  controller_ = InputController::Create(audio_manager, this, writer_.get(),
-                                        user_input_monitor_.get(), params,
-                                        device_id, enable_agc);
+  controller_ = media::AudioInputController::Create(
+      audio_manager, this, writer_.get(), user_input_monitor_.get(), params,
+      device_id, enable_agc);
 }
 
 InputStream::~InputStream() {
@@ -117,7 +117,7 @@ InputStream::~InputStream() {
     return;
   }
 
-  // TODO(https://crbug.com/803102): remove InputController::Close() after
+  // TODO(https://crbug.com/803102): remove AudioInputController::Close() after
   // content/ streams are removed, destructor should suffice.
   controller_->Close(base::OnceClosure());
 
@@ -184,7 +184,7 @@ void InputStream::OnCreated(bool initially_muted) {
            initially_muted, id_);
 }
 
-void InputStream::OnError(InputController::ErrorCode error_code) {
+void InputStream::OnError(media::AudioInputController::ErrorCode error_code) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(owning_sequence_);
   TRACE_EVENT_NESTABLE_ASYNC_INSTANT0("audio", "Error", this);
 
