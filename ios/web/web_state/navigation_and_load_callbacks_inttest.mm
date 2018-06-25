8139899f9caa7a1940d128f3346e171a0e53a345
Eugene But <eugenebut@chromium.org>
2018-06-25 15:40:38 +0000

    Fix NavigationAndLoadCallbacksTest.ForwardPostNavigation for iOS 12.
    WebStatePolicyDecider::ShouldAllowResponse is now called when going back
    after the form submission. Updated test expectation to match new
    behavior.
    
    Bug: 854615
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Icd3357750f56e9f4cac5a158fdd8aeb54dbf6c4b
    Reviewed-on: https://chromium-review.googlesource.com/1113449
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570048}

--- ios/web/web_state/navigation_and_load_callbacks_inttest.mm
+++ ios/web/web_state/navigation_and_load_callbacks_inttest.mm
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
-#include "base/ios/ios_util.h"
 #include "base/scoped_observer.h"
 #include "base/strings/stringprintf.h"
 #include "ios/testing/embedded_test_server_handlers.h"
@@ -1211,10 +1210,6 @@ TEST_F(NavigationAndLoadCallbacksTest, ReloadPostNavigation) {
 
 // Tests going forward to a page rendered from post response.
 TEST_F(NavigationAndLoadCallbacksTest, ForwardPostNavigation) {
-  // TODO(crbug.com/854615): Test fails on iOS12.
-  if (base::ios::IsRunningOnIOS12OrLater()) {
-    return;
-  }
   const GURL url = test_server_->GetURL("/form?echo");
   const GURL action = test_server_->GetURL("/echo");
 
@@ -1277,8 +1272,13 @@ TEST_F(NavigationAndLoadCallbacksTest, ForwardPostNavigation) {
   }
 
   EXPECT_CALL(observer_, DidStartNavigation(web_state(), _));
+  if (@available(iOS 12, *)) {
+    // On iOS 11 and earlier, ShouldAllowResponse is not called when going back
+    // after form submission.
+    EXPECT_CALL(*decider_, ShouldAllowResponse(_, /*for_main_frame=*/true))
+        .WillOnce(Return(true));
+  }
   EXPECT_CALL(observer_, DidFinishNavigation(web_state(), _));
-  // ShouldAllowResponse is not called when going back after form submission.
   EXPECT_CALL(observer_, DidStopLoading(web_state()));
   EXPECT_CALL(observer_,
               PageLoaded(web_state(), PageLoadCompletionStatus::SUCCESS));
