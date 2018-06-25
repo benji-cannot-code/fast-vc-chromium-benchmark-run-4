22b30fb4c0b5e5e9dc44ed86f26ffeea8f695d14
Xianzhu Wang <wangxianzhu@chromium.org>
2018-06-25 17:01:20 +0000

    [rm-for-spv175] Fold the only function in LayerClipRecorder into the only caller
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ic96ed128f4925cf32c3028932dca369033eec300
    Reviewed-on: https://chromium-review.googlesource.com/1112952
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570071}

--- third_party/blink/renderer/core/paint/paint_layer_painter.cc
+++ third_party/blink/renderer/core/paint/paint_layer_painter.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/layout_view.h"
 #include "third_party/blink/renderer/core/paint/clip_path_clipper.h"
 #include "third_party/blink/renderer/core/paint/compositing/composited_layer_mapping.h"
-#include "third_party/blink/renderer/core/paint/layer_clip_recorder.h"
 #include "third_party/blink/renderer/core/paint/object_paint_properties.h"
 #include "third_party/blink/renderer/core/paint/paint_info.h"
 #include "third_party/blink/renderer/core/paint/paint_layer.h"
