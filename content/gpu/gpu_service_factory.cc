ab529556cb124fbeec67f2056b1990a46ba72218
Frank Liberato <liberato@chromium.org>
2018-06-25 23:48:08 +0000

    Move GpuMediaService to gpu main thread on Windows.
    Since the CDM runs on the main thread, and we can't decode on a
    separate thread anyway, just start the whole GpuMediaService on
    the GPU main thread.
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I69e41919f96348c86289844231fe2506822a0ebd
    Reviewed-on: https://chromium-review.googlesource.com/1112593
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570242}

--- content/gpu/gpu_service_factory.cc
+++ content/gpu/gpu_service_factory.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/task_scheduler/post_task.h"
 #include "base/threading/thread_task_runner_handle.h"
+#include "build/build_config.h"
 #include "services/shape_detection/public/mojom/constants.mojom.h"
 #include "services/shape_detection/shape_detection_service.h"
 
@@ -55,9 +56,14 @@ void GpuServiceFactory::RegisterServices(ServiceMap* services) {
   // This service will host audio/video decoders, and if these decoding
   // operations are blocked, user may hear audio glitch or see video freezing,
   // hence "user blocking".
+#if defined(OS_WIN)
+  // Run everything on the gpu main thread, since that's where the CDM runs.
+  info.task_runner = task_runner_;
+#else
   // TODO(crbug.com/786169): Check whether this needs to be single threaded.
   info.task_runner = base::CreateSingleThreadTaskRunnerWithTraits(
       {base::TaskPriority::USER_BLOCKING});
+#endif  // defined(OS_WIN)
   services->insert(std::make_pair("media", info));
 #endif  // BUILDFLAG(ENABLE_MOJO_MEDIA_IN_GPU_PROCESS)
 
