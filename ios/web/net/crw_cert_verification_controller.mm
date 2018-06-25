db8caf991df640ce176faede0f1760b08e7c08ab
Eugene But <eugenebut@chromium.org>
2018-06-25 14:10:30 +0000

    Use WebThread::PostTask in CRWCertVerificationController.
    GCD API was used to make sure that completion callbacks are called
    during the app shutdown. Calling completion handler is required by
    WKWebView, otherwise web view throws an exception. This CL uses
    WebThread::PostTask instead of GCD behind feature flag. This will allow
    to run an experiment to see if using WebThread::PostTask leads to crashes
    during app shutdown.
    
    WebThread::PostTask is preferable for testing, because there is no
    good way to wait until GCD tasks are completed in a unit test.
    
    Bug: 853774
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ie9eefa7f7cef0c7432a2f733086fc5d630d8a969
    Reviewed-on: https://chromium-review.googlesource.com/1113182
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570028}

--- ios/web/net/crw_cert_verification_controller.mm
+++ ios/web/net/crw_cert_verification_controller.mm
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task_scheduler/post_task.h"
 #include "ios/web/public/browser_state.h"
 #include "ios/web/public/certificate_policy_cache.h"
+#include "ios/web/public/features.h"
 #include "ios/web/public/web_thread.h"
 #import "ios/web/web_state/wk_web_view_security_util.h"
 #include "net/cert/cert_verify_proc_ios.h"
@@ -198,9 +199,18 @@ decideLoadPolicyForRejectedTrustResult:(SecTrustResultType)trustResult
           trustResult = kSecTrustResultInvalid;
         }
         // Use GCD API, which is guaranteed to be called during shutdown.
-        dispatch_async(dispatch_get_main_queue(), ^{
-          completionHandler(trustResult);
-        });
+
+        if (base::FeatureList::IsEnabled(
+                web::features::kUseWebThreadInCertVerificationController)) {
+          web::WebThread::PostTask(web::WebThread::UI, FROM_HERE,
+                                   base::BindOnce(^{
+                                     completionHandler(trustResult);
+                                   }));
+        } else {
+          dispatch_async(dispatch_get_main_queue(), ^{
+            completionHandler(trustResult);
+          });
+        }
       }));
 }
 
