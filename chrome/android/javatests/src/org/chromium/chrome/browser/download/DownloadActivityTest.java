ded91ba3df455820f4932fab2a8bdc64e1f259ff
Theresa <twellington@chromium.org>
2018-06-25 22:54:43 +0000

    Revert "Download location: enable flaky tests in DownloadActivityTest."
    This reverts commit ce3de2d33c08d47444e6f7798cea176654e642a5.
    
    Reason for revert: Some of the tests are still failing, triggering a try flakes alert. Will you please re-land with the tests we know are passing conditionally re-enabled?
    
    
    Original change's description:
    > Download location: enable flaky tests in DownloadActivityTest.
    > 
    > The CL that causes the flakiness has been reverted. See:
    > https://chromium-review.googlesource.com/c/chromium/src/+/1112777
    > Landed as e4144ed1a4845edb13e3afce024a8cb60588b38f.
    > 
    > This CL enables flaky tests in DownloadActivityTest.
    > 
    > TBR=dtrainor@chromium.org
    > 
    > Bug: 855389,855219,855168,855167
    > Change-Id: Id53085ed71c5188cd1e7588998bda6f00c7b89a5
    > Reviewed-on: https://chromium-review.googlesource.com/1113910
    > Reviewed-by: Xing Liu <xingliu@chromium.org>
    > Commit-Queue: Xing Liu <xingliu@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#570107}
    
    TBR=dtrainor@chromium.org,xingliu@chromium.org
    
    Change-Id: I7299f6a85b0acd59050f1f26c0f6c8a8fb3626c6
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 855389, 855219, 855168, 855167
    Reviewed-on: https://chromium-review.googlesource.com/1114044
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570215}

--- chrome/android/javatests/src/org/chromium/chrome/browser/download/DownloadActivityTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/download/DownloadActivityTest.java
@@ -29,6 +29,8 @@ import org.junit.runner.RunWith;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CallbackHelper;
+import org.chromium.base.test.util.DisabledTest;
+import org.chromium.base.test.util.FlakyTest;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.chrome.R;
@@ -159,6 +161,7 @@ public class DownloadActivityTest {
 
     @Test
     @MediumTest
+    @FlakyTest(message = "crbug.com/855168")
     public void testSpaceDisplay() throws Exception {
         // This first check is a Criteria because initialization of the Adapter is asynchronous.
         CriteriaHelper.pollUiThread(new Criteria() {
@@ -218,6 +221,7 @@ public class DownloadActivityTest {
     }
 
     /** Clicking on filters affects various things in the UI. */
+    @DisabledTest(message = "crbug.com/855389")
     @Test
     @MediumTest
     public void testFilters() throws Exception {
@@ -252,6 +256,7 @@ public class DownloadActivityTest {
     @Test
     @MediumTest
     @RetryOnFailure
+    @FlakyTest(message = "crbug.com/854241")
     public void testDeleteFiles() throws Exception {
         SnackbarManager.setDurationForTesting(1);
 
@@ -291,6 +296,7 @@ public class DownloadActivityTest {
     @Test
     @MediumTest
     @RetryOnFailure
+    @FlakyTest(message = "crbug.com/855219")
     public void testDeleteFileFromMenu() throws Exception {
         SnackbarManager.setDurationForTesting(1);
 
@@ -322,6 +328,7 @@ public class DownloadActivityTest {
         Assert.assertEquals("5.00 GB downloaded", mSpaceUsedDisplay.getText());
     }
 
+    @DisabledTest(message = "crbug.com/855389")
     @Test
     @MediumTest
     @RetryOnFailure
@@ -392,6 +399,7 @@ public class DownloadActivityTest {
         Assert.assertEquals("6.50 GB downloaded", mSpaceUsedDisplay.getText());
     }
 
+    @DisabledTest(message = "crbug.com/855389")
     @Test
     @MediumTest
     @RetryOnFailure
@@ -457,6 +465,7 @@ public class DownloadActivityTest {
         Assert.assertEquals("6.50 GB downloaded", mSpaceUsedDisplay.getText());
     }
 
+    @DisabledTest(message = "crbug.com/855389")
     @Test
     @MediumTest
     @RetryOnFailure
@@ -522,6 +531,7 @@ public class DownloadActivityTest {
     @Test
     @MediumTest
     @DisableFeatures("OfflinePagesSharing")
+    @FlakyTest(message = "crbug.com/855167")
     public void testShareFiles() throws Exception {
         // Adapter positions:
         // 0 = space display
@@ -596,6 +606,7 @@ public class DownloadActivityTest {
 
     // TODO(carlosk): OfflineItems used here come from StubbedProvider so this might not be the best
     // place to test peer-2-peer sharing.
+    @DisabledTest(message = "crbug.com/855389")
     @Test
     @MediumTest
     @EnableFeatures("OfflinePagesSharing")
