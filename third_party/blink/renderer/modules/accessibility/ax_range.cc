3cc1989dfdac6b2aeefb0714ca9789a4a2289866
Nektarios Paisios <nektar@chromium.org>
2018-06-25 00:14:56 +0000

    Creates an invalid AXRange if start and end positions are invalid instead of DCHECKing
    This creates safer code overall.
    If start and end AXPositions are invalid for any reason we didn't contemplate, the browser doesn't crash but returns an AXRange that is explicitly marked invalid.
    It's also a nice workaround until CSS decorations are supported in AXPosition.
    
    TBR=dmazzoni@chromium.org, aleventhal@chromium.org
    
    Bug: 852266
    Change-Id: I94db8aeda296690a842b674ea27743dc573205d7
    Reviewed-on: https://chromium-review.googlesource.com/1112968
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569941}

--- third_party/blink/renderer/modules/accessibility/ax_range.cc
+++ third_party/blink/renderer/modules/accessibility/ax_range.cc
@@ -11,16 +11,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 AXRange::AXRange(const AXPosition& start, const AXPosition& end)
-    : start_(start), end_(end) {
-  DCHECK(start.IsValid());
-  DCHECK(end.IsValid());
-  DCHECK_LE(start, end);
+    : start_(), end_() {
+  if (!start.IsValid() || !end.IsValid() || start > end)
+    return;
 
   const Document* document = start.ContainerObject()->GetDocument();
   DCHECK(document);
   DCHECK(document->IsActive());
   DCHECK(!document->NeedsLayoutTreeUpdate());
-  DCHECK_EQ(end.ContainerObject()->GetDocument(), document);
+  // We don't support ranges that span across documents.
+  if (end.ContainerObject()->GetDocument() != document)
+    return;
+
+  start_ = start;
+  end_ = end;
+
 #if DCHECK_IS_ON()
   dom_tree_version_ = document->DomTreeVersion();
   style_version_ = document->StyleVersion();
