164586c80711ef170e7d93a7cce6d681440ca1fe
Takumi Fujimoto <takumif@chromium.org>
2018-06-25 21:07:50 +0000

    Add a dialog for enabling cloud services for Cast
    This CL adds a dialog that allows users to enable cloud services in Cast
    (Hangouts/Meet, Cast for EDU), and notifies that those services are
    governed by the Google Privacy Policy.
    
    The dialog is shown when the user tries to enable cloud services via the
    context menu of the Cast toolbar icon ("Enable cloud services") if
    the user has not previously acknowledged this dialog or the first run
    flow of the old WebUI dialog.
    
    If the user clicks on the "Enable" button in the dialog, we enable the
    preferences kMediaRouterEnableCloudServices and
    kMediaRouterCloudServicesPrefSet. If the dialog is closed in another
    way, we make no pref changes.
    
    Screenshot:
    https://drive.google.com/open?id=1MSonPoMLYt0lfLeq9NfP5yoH15EeVZ6c
    
    Bug: 848891
    
    Change-Id: I9f565e328ea0fbc84131eef5e0fa3cb179284886
    Reviewed-on: https://chromium-review.googlesource.com/1096403
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570179}

---
+++ chrome/browser/ui/media_router/cloud_services_dialog.cc
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/media_router/cloud_services_dialog.h"
+
+namespace media_router {
+
+void ShowCloudServicesDialog(Browser* browser) {}
+
+}  // namespace media_router
