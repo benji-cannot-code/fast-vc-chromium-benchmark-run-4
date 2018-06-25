874f332006b6a4b40e62ffeb0a6f52d46a72b04d
Matt Falkenhagen <falken@chromium.org>
2018-06-25 23:32:50 +0000

    service worker: Deprecate some UMAs about script load timing.
    This removes:
    (a) EmbeddedWorkerInstance.Start.TimeToURLJob
        This was recorded only for installed workers. But since script
        streaming launched in M64, a URL job is not created for installed
        workers.
    (b) EmbeddedWorkerInstance.Start.TimeToLoad.InstalledScript
        We already haven't been recording this since M64, due to script
        streaming.
    (c) EmbeddedWorkerInstance.Start.TimeToLoad.HttpCache
    (d) EmbeddedWorkerInstance.Start.TimeToLoad.Network
        These were recorded for new workers. But they rely on URLRequestJob
        to be recorded, and the path isn't taken in NetS13nServiceWorker or
        NetworkService. If we want these, we can re-add them later. It'd be
        more general to record them directly in Blink using Resource Timing.
    
    Note that this removed a call to UpdateStepTime() which would appear
    to change UMA timings. However it's OK because:
    * for installed service workers: we weren't updating step time anyway
    * for non-installed service workers: the only UMA logged using step
    time was RecordTimeToLoad, which is being removed in this CL.
    
    Bug: 855952
    Change-Id: Ia7e610565e2c50fc220212dda8aa23f2f15dc23b
    Reviewed-on: https://chromium-review.googlesource.com/1113080
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570238}

--- content/browser/service_worker/service_worker_context_request_handler.cc
+++ content/browser/service_worker/service_worker_context_request_handler.cc
@@ -159,8 +159,6 @@ net::URLRequestJob* ServiceWorkerContextRequestHandler::MaybeCreateJobImpl(
   if (resource_id != kInvalidServiceWorkerResourceId) {
     if (ServiceWorkerVersion::IsInstalled(version_->status())) {
       // An installed worker is loading a stored script.
-      if (is_main_script)
-        version_->embedded_worker()->OnURLJobCreatedForMainScript();
       *out_status = CreateJobStatus::READ_JOB;
     } else {
       // A new worker is loading a stored script. The script was already
@@ -213,7 +211,6 @@ net::URLRequestJob* ServiceWorkerContextRequestHandler::MaybeCreateJobImpl(
       incumbent_resource_id =
           stored_version->script_cache_map()->LookupResourceId(request->url());
     }
-    version_->embedded_worker()->OnURLJobCreatedForMainScript();
   }
   *out_status = incumbent_resource_id == kInvalidServiceWorkerResourceId
                     ? CreateJobStatus::WRITE_JOB
