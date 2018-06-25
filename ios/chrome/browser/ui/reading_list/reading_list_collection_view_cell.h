ce8414fbbb691d8c8b4804ec6829f27bba7872cc
Kurt Horimoto <kkhorimoto@chromium.org>
2018-06-25 19:18:23 +0000

    [iOS] Move ReadingListUIDistillationStatus to a separate file.
    This is used for both the collection view and table view implementaitons
    of the Reading List UI, so needs to be moved to a shared location.
    
    Bug: 805209
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ia01403e6d318b4ea331ed85ba622c4ce8ce041cf
    Reviewed-on: https://chromium-review.googlesource.com/1102119
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570126}

--- ios/chrome/browser/ui/reading_list/reading_list_collection_view_cell.h
+++ ios/chrome/browser/ui/reading_list/reading_list_collection_view_cell.h
@@ -6,15 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_UI_READING_LIST_READING_LIST_COLLECTION_VIEW_CELL_H_
 #define IOS_CHROME_BROWSER_UI_READING_LIST_READING_LIST_COLLECTION_VIEW_CELL_H_
 
+#import "ios/chrome/browser/ui/reading_list/reading_list_ui_distillation_status.h"
 #import "ios/third_party/material_components_ios/src/components/CollectionCells/src/MaterialCollectionCells.h"
 
 @class FaviconViewNew;
-
-typedef NS_ENUM(NSInteger, ReadingListUIDistillationStatus) {
-  ReadingListUIDistillationStatusPending,
-  ReadingListUIDistillationStatusSuccess,
-  ReadingListUIDistillationStatusFailure
-};
 
 // Cell for ReadingListCollectionViewItem.
 @interface ReadingListCell : MDCCollectionViewCell
---
+++ ios/chrome/browser/ui/reading_list/reading_list_ui_distillation_status.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_READING_LIST_READING_LIST_UI_DISTILLATION_STATUS_H_
+#define IOS_CHROME_BROWSER_UI_READING_LIST_READING_LIST_UI_DISTILLATION_STATUS_H_
+
+#import <Foundation/Foundation.h>
+
+// Enum describing the distillation state for reading list UI.
+typedef NS_ENUM(NSInteger, ReadingListUIDistillationStatus) {
+  ReadingListUIDistillationStatusPending,
+  ReadingListUIDistillationStatusSuccess,
+  ReadingListUIDistillationStatusFailure
+};
+
+#endif  // IOS_CHROME_BROWSER_UI_READING_LIST_READING_LIST_UI_DISTILLATION_STATUS_H_
