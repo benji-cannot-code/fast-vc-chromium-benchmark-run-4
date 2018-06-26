8692557202eb0fd25ccd2d4ee409063409d08888
Lukasz Anforowicz <lukasza@chromium.org>
2018-06-26 18:54:28 +0000

    Handle null callback in HungPagesTableModel::RestartHangMonitorTimeout.
    HungPagesTableModel::InitForWebContents is not called when Chrome is not
    the currently active window (e.g. when another application like Finder
    on Mac is active).  This means that
    HungPagesTableModel::hang_monitor_restarter_ may be uninitialized when
    the renderer wakes up leading to a crash when
    HungPagesTableModel::RestartHangMonitorTimeout is called.
    
    Bug: 855369
    Change-Id: Iaf2ccf3385e51bdca62d33915afcf162d99663fc
    Reviewed-on: https://chromium-review.googlesource.com/1112315
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570477}

--- chrome/browser/ui/views/hung_renderer_view.cc
+++ chrome/browser/ui/views/hung_renderer_view.cc
@@ -111,7 +111,8 @@ void HungPagesTableModel::Reset() {
 }
 
 void HungPagesTableModel::RestartHangMonitorTimeout() {
-  hang_monitor_restarter_.Run();
+  if (hang_monitor_restarter_)
+    hang_monitor_restarter_.Run();
 }
 
 ///////////////////////////////////////////////////////////////////////////////
