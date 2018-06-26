a0a8d28099008049da60bec1c0cdf67279d65f29
Mihai Sardarescu <msarda@chromium.org>
2018-06-26 07:49:15 +0000

    [unity] Remove dependency on safe_browsing from //components/unified_consent
    //components/safe_browsing depends on //content that is not build on iOS.
    This means that //components/unified_consent cannot depend on
    //components/safe_browsing as it needs to be build on iOS.
    
    This CL removes the usage of safe_browsing from //components/unified_consent
    and adds the corresponding method in the client.
    
    
    Bug: NONE
    Change-Id: I2c32629524d5621e36717dcb801fd8be00732a55
    Reviewed-on: https://chromium-review.googlesource.com/1113753
    Reviewed-by: Thomas Tangl <tangltom@chromium.org>
    Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570345}

--- components/unified_consent/unified_consent_service.cc
+++ components/unified_consent/unified_consent_service.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/pref_change_registrar.h"
 #include "components/prefs/pref_service.h"
-#include "components/safe_browsing/common/safe_browsing_prefs.h"
 #include "components/sync/base/model_type.h"
 #include "components/unified_consent/pref_names.h"
 #include "components/unified_consent/unified_consent_service_client.h"
@@ -84,11 +83,11 @@ void UnifiedConsentService::OnUnifiedConsentGivenPrefChanged() {
                                               syncer::UserSelectableTypes());
 
   // Enable all non-personalized services.
-  pref_service_->SetBoolean(prefs::kSafeBrowsingEnabled, true);
   service_client_->SetAlternateErrorPagesEnabled(true);
   service_client_->SetMetricsReportingEnabled(true);
   service_client_->SetSafeBrowsingExtendedReportingEnabled(true);
   service_client_->SetSearchSuggestEnabled(true);
+  service_client_->SetSafeBrowsingEnabled(true);
   service_client_->SetSafeBrowsingExtendedReportingEnabled(true);
   service_client_->SetNetworkPredictionEnabled(true);
 }
