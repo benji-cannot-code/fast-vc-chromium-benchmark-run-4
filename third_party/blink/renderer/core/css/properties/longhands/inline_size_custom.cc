7e0aacef62a33d7b84f37de7a5b2679b98daa217
Oriol Brufau <obrufau@igalia.com>
2018-06-26 20:55:48 +0000

    [css-logical] Fix computed style of logical size properties
    Unlike 'width' and 'height', 'inline-size' and 'block-size' were not
    considered to be layout dependent, and thus getComputedStyle could
    provide a wrong value.
    
    BUG=849855
    
    TEST=external/wpt/css/css-logical/logical-box-size.html
    
    The test still has some failures because sideways writing modes have not
    been implemented yet (http://crbug.com/680331).
    
    Change-Id: I4ee8ed8b2cd575fb3ddd5226b23c6a4bb843b81f
    Reviewed-on: https://chromium-review.googlesource.com/1114966
    Commit-Queue: Oriol Brufau <obrufau@igalia.com>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570527}

--- third_party/blink/renderer/core/css/properties/longhands/inline_size_custom.cc
+++ third_party/blink/renderer/core/css/properties/longhands/inline_size_custom.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/css/parser/css_property_parser_helpers.h"
 #include "third_party/blink/renderer/core/css/properties/css_parsing_utils.h"
+#include "third_party/blink/renderer/core/layout/layout_object.h"
 
 namespace blink {
 namespace CSSLonghand {
@@ -16,6 +17,11 @@ const CSSValue* InlineSize::ParseSingleValue(
     const CSSParserContext& context,
     const CSSParserLocalContext&) const {
   return CSSParsingUtils::ConsumeWidthOrHeight(range, context);
+}
+
+bool InlineSize::IsLayoutDependent(const ComputedStyle* style,
+                                   LayoutObject* layout_object) const {
+  return layout_object && layout_object->IsBox();
 }
 
 }  // namespace CSSLonghand
