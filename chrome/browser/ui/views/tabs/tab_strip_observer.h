2d6f8f9e285f6c302c0f131dbc394ad07192d76a
Bret Sepulveda <bsep@chromium.org>
2018-06-26 01:33:37 +0000

    Refresh: disallow single-tab mode when the only tab is pinned.
    Bug: 855286
    Change-Id: I35b3fcce765c41f7b845ec3e5ec7924750b8a1c2
    Reviewed-on: https://chromium-review.googlesource.com/1114334
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570287}

--- chrome/browser/ui/views/tabs/tab_strip_observer.h
+++ chrome/browser/ui/views/tabs/tab_strip_observer.h
@@ -35,6 +35,9 @@ class CHROME_VIEWS_EXPORT TabStripObserver {
   // Sent when the trailing edge of the last tab has changed.
   virtual void OnTabsMaxXChanged();
 
+  // Sent when the tabstrip enters or exits the special single-tab mode.
+  virtual void OnSingleTabModeChanged();
+
  protected:
   virtual ~TabStripObserver() {}
 };
