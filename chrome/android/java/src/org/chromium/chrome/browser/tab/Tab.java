79818814c4f42353d48292a4361eb90ad5947b21
Becca Hughes <beccahughes@chromium.org>
2018-06-26 02:15:31 +0000

    [Display Cutout] Send safe areas from Android
    This sends the safe areas from Android to Blink using the
    DisplayCutoutClient Mojo interface.
    
    This CL also includes instrumentation tests for end to end
    testing of Display Cutout on Android.
    
    BUG=847652
    
    Change-Id: Ide2bed7878a22d649914fde66083c28374f4d768
    Reviewed-on: https://chromium-review.googlesource.com/1097802
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570293}

--- chrome/android/java/src/org/chromium/chrome/browser/tab/Tab.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/Tab.java
@@ -3494,6 +3494,14 @@ public class Tab
         mTrustedCdnPublisherUrl = url;
     }
 
+    /**
+     * Sets a custom {@link DisplayCutoutController} for testing.
+     */
+    @VisibleForTesting
+    public void setDisplayCutoutController(DisplayCutoutController controller) {
+        mDisplayCutoutController = controller;
+    }
+
     private native void nativeInit();
     private native void nativeDestroy(long nativeTabAndroid);
     private native void nativeInitWebContents(long nativeTabAndroid, boolean incognito,
