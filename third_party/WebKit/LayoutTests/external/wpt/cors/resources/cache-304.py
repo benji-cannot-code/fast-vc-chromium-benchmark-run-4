189e4b64199fd9ecbb28038c24bb7db20f4eb9fc
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-06-26 02:48:29 +0000

    Import wpt@72048cdc2d47d5d1d0a9a2fd97485c87fc89c1b2
    Using wpt-import in Chromium 1143433fcf8b0ddd6058a01904bc24ce89ae6aa8.
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/20250
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    TBR=raphael.kubo.da.costa@intel.com
    
    No-Export: true
    Change-Id: Iec31eac2efc77eee9b9a6df6a640f8881a9340d6
    Reviewed-on: https://chromium-review.googlesource.com/1114319
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570307}

---
+++ third_party/WebKit/LayoutTests/external/wpt/cors/resources/cache-304.py
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+def main(request, response):
+    match = request.headers.get("If-None-Match", None)
+    if match is not None and match == "mybestscript-v1":
+        response.status = (304, "YEP")
+        return ""
+    response.headers.set("Access-Control-Allow-Origin", "*")
+    response.headers.set("Cache-Control", "must-revalidate")
+    response.headers.set("ETag", "mybestscript-v1")
+    response.headers.set("Content-Type", "text/javascript")
+    return "function hep() { }"
