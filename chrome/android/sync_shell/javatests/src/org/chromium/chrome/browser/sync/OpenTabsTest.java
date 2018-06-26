3a326639c5fe6164b25e7619179781291336ec88
Mikel Astiz <mastiz@chromium.org>
2018-06-26 10:07:17 +0000

    Remove FlakyTest annotation for OpenTabsTest and and related
    The underlying implementations have changed substantially due to
    migrations to USS, and we're not aware of the flakes still being an
    issue.
    
    Bug: 592437
    Change-Id: I0472379a3fa35742863f1c862d632dea902663d5
    Reviewed-on: https://chromium-review.googlesource.com/1113184
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570362}

--- chrome/android/sync_shell/javatests/src/org/chromium/chrome/browser/sync/OpenTabsTest.java
+++ chrome/android/sync_shell/javatests/src/org/chromium/chrome/browser/sync/OpenTabsTest.java
@@ -20,7 +20,6 @@ import org.junit.runner.RunWith;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Feature;
-import org.chromium.base.test.util.FlakyTest;
 import org.chromium.chrome.browser.ChromeSwitches;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 import org.chromium.chrome.browser.tabmodel.TabModelUtils;
@@ -98,26 +97,20 @@ public class OpenTabsTest {
         mSessionTagCounter = 0;
     }
 
-    /*
     // Test syncing an open tab from client to server.
+    @Test
     @LargeTest
     @Feature({"Sync"})
-    */
-    @Test
-    @FlakyTest(message = "https://crbug.com/592437")
     public void testUploadOpenTab() throws Exception {
         mSyncTestRule.loadUrl(URL);
         waitForLocalTabsForClient(mClientName, URL);
         waitForServerTabs(URL);
     }
 
-    /*
     // Test syncing multiple open tabs from client to server.
+    @Test
     @LargeTest
     @Feature({"Sync"})
-    */
-    @Test
-    @FlakyTest(message = "https://crbug.com/592437")
     public void testUploadMultipleOpenTabs() throws Exception {
         mSyncTestRule.loadUrl(URL);
         mSyncTestRule.loadUrlInNewTab(URL2);
@@ -126,13 +119,10 @@ public class OpenTabsTest {
         waitForServerTabs(URL, URL2, URL3);
     }
 
-    /*
     // Test syncing an open tab from client to server.
+    @Test
     @LargeTest
     @Feature({"Sync"})
-    */
-    @Test
-    @FlakyTest(message = "https://crbug.com/592437")
     public void testUploadAndCloseOpenTab() throws Exception {
         mSyncTestRule.loadUrl(URL);
         // Can't have zero tabs, so we have to open two to test closing one.
