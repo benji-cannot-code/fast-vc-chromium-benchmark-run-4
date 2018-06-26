681e030a248265b99573f817739047dc18f9d01b
Shakti Sahu <shaktisahu@chromium.org>
2018-06-26 00:07:23 +0000

    Moved thumbnail generation code from chrome/browser/android to chrome/browser
    This will be required for supplying thumbnails to download OfflineContentProvider.
    Since the invoking code lives in chrome/browser, we are moving the thumbnail
    generation part to platform-independent code as well
    
    Bug: 855330
    Change-Id: Ic397e50faa511c83f1241d4c961c5e888ea8d272
    Reviewed-on: https://chromium-review.googlesource.com/1111652
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570252}

--- chrome/browser/android/widget/thumbnail_generator.h
+++ chrome/browser/android/widget/thumbnail_generator.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/android/jni_android.h"
 #include "base/memory/weak_ptr.h"
-#include "chrome/browser/image_decoder.h"
+#include "chrome/browser/download/image_thumbnail_request.h"
 
 // Kicks off asynchronous pipelines for creating thumbnails for local files.
 // The native-side ThumbnailGenerator is owned by the Java-side and can be
