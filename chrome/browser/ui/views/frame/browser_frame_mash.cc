7552c7fe3d0d216e1d572a8019b9422071a09b02
Evan Stade <estade@chromium.org>
2018-06-26 20:51:56 +0000

    Mash browser frame: use default frame for V1/packaged apps.
    Extension apps already use DefaultFrameHeader and packaged apps
    should do the same (and already do in classic Ash).
    
    This patch also fixes layout of non-app, non-tabbed browser windows,
    i.e. popup windows, in Mash.
    
    Bug: 854704
    Change-Id: Id80a06cbd6d0489efd0761421909c9b4ec06c263
    Reviewed-on: https://chromium-review.googlesource.com/1115344
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570523}

--- chrome/browser/ui/views/frame/browser_frame_mash.cc
+++ chrome/browser/ui/views/frame/browser_frame_mash.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window_state.h"
 #include "chrome/browser/ui/views/frame/browser_frame.h"
 #include "chrome/browser/ui/views/frame/browser_frame_ash.h"
+#include "chrome/browser/ui/views/frame/browser_non_client_frame_view_ash.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/common/extensions/extension_constants.h"
 #include "services/ui/public/cpp/property_type_converters.h"
@@ -56,8 +57,10 @@ views::Widget::InitParams BrowserFrameMash::GetWidgetParams() {
 
   Browser* browser = browser_view_->browser();
   properties[ash::mojom::kAshWindowStyle_InitProperty] =
-      mojo::ConvertTo<std::vector<uint8_t>>(
-          static_cast<int32_t>(ash::mojom::WindowStyle::BROWSER));
+      mojo::ConvertTo<std::vector<uint8_t>>(static_cast<int32_t>(
+          BrowserNonClientFrameViewAsh::UsePackagedAppHeaderStyle(browser)
+              ? ash::mojom::WindowStyle::DEFAULT
+              : ash::mojom::WindowStyle::BROWSER));
   // ChromeLauncherController manages the browser shortcut shelf item; set the
   // window's shelf item type property to be ignored by ash::ShelfWindowWatcher.
   properties[ui::mojom::WindowManager::kShelfItemType_Property] =
--- chrome/browser/ui/views/frame/browser_non_client_frame_view_ash.h
+++ chrome/browser/ui/views/frame/browser_non_client_frame_view_ash.h
@@ -22,6 +22,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/binding.h"
 #include "ui/aura/window_observer.h"
 
+class Browser;
+
 namespace {
 class HostedAppNonClientFrameViewAshTest;
 }
@@ -125,6 +127,10 @@ class BrowserNonClientFrameViewAsh
 
   HostedAppButtonContainer* GetHostedAppButtonContainerForTesting() const;
 
+  // Returns true if the header should be painted so that it looks the same as
+  // the header used for packaged apps.
+  static bool UsePackagedAppHeaderStyle(const Browser* browser);
+
  protected:
   // BrowserNonClientFrameView:
   AvatarButtonStyle GetAvatarButtonStyle() const override;
@@ -159,11 +165,6 @@ class BrowserNonClientFrameViewAsh
   // Distance between the right edge of the NonClientFrameView and the tab
   // strip.
   int GetTabStripRightInset() const;
-
-  // Returns true if the header should be painted so that it looks the same as
-  // the header used for packaged apps. Packaged apps use a different color
-  // scheme than browser windows.
-  bool UsePackagedAppHeaderStyle() const;
 
   // Returns true if there is anything to paint. Some fullscreen windows do
   // not need their frames painted.
