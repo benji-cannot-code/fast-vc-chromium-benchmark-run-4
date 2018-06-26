898b01f8616cb98440743d3e278c35e905a1dcb9
Allen Bauer <kylixrd@chromium.org>
2018-06-26 14:52:15 +0000

    Tabs can still be made to disappear while dragging other tabs.
    It is a little embarrassing to keep churning on this issue. See the referenced
    bug for my analysis as to what is happening.
    
    Bug: 853240
    Change-Id: I923f5cd09ee709915091878e2e99eacc074c8662
    Reviewed-on: https://chromium-review.googlesource.com/1114065
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570404}

--- chrome/browser/ui/views/tabs/tab.cc
+++ chrome/browser/ui/views/tabs/tab.cc
@@ -798,6 +798,7 @@ void Tab::OnMouseMoved(const ui::MouseEvent& event) {
 }
 
 void Tab::OnMouseEntered(const ui::MouseEvent& event) {
+  mouse_hovered_ = true;
   hover_controller_.Show(GlowHoverController::SUBTLE);
   if (MD::IsRefreshUi())
     RepaintSubsequentTab();
@@ -805,6 +806,7 @@ void Tab::OnMouseEntered(const ui::MouseEvent& event) {
 }
 
 void Tab::OnMouseExited(const ui::MouseEvent& event) {
+  mouse_hovered_ = false;
   hover_controller_.Hide();
   if (MD::IsRefreshUi())
     RepaintSubsequentTab();
