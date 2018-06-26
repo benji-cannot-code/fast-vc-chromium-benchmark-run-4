d268fb06ae536c1c2dbc3454ff0c67b33c65beeb
Kyle Horimoto <khorimoto@google.com>
2018-06-26 23:39:07 +0000

    [CrOS MultiDevice] Add cryptauth::RemoteDeviceRef::GetRemoteDevice().
    I'm about to add another usage of a friend class accessing the raw
    cryptauth::RemoteDevice, so I cleaned this up by adding an explicit
    getter function for this case before continuing.
    
    Bug: 824568
    Change-Id: Ifb752ad72d18ba42f59e6bce82e63096c6251a53
    Reviewed-on: https://chromium-review.googlesource.com/1115720
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570584}

--- components/cryptauth/remote_device_ref.h
+++ components/cryptauth/remote_device_ref.h
@@ -33,8 +33,6 @@ class ProximityAuthWebUIHandler;
 
 namespace cryptauth {
 
-class RemoteDeviceCache;
-
 // Contains metadata specific to a device associated with a user's account.
 // Because this metadata contains large and expensive data types, and that data
 // can become stale if a Device Sync occurs during a client application's
@@ -108,6 +106,11 @@ class RemoteDeviceRef {
   friend class proximity_auth::BluetoothLowEnergySetupConnectionFinder;
 
   explicit RemoteDeviceRef(std::shared_ptr<RemoteDevice> remote_device);
+
+  // Returns the raw RemoteDevice object. Should only be used when passing
+  // RemoteDevice objects through a Mojo API, which requires that the raw type
+  // is passed instead of the RemoteDeviceRef wrapper object.
+  const RemoteDevice& GetRemoteDevice() const;
 
   std::shared_ptr<const RemoteDevice> remote_device_;
 };
