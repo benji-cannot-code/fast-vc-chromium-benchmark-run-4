8169d226ff2eb06da10e31e6cbc3b121b6ec4792
Matt Falkenhagen <falken@chromium.org>
2018-06-26 03:10:35 +0000

    service worker: Partially remove OnScriptLoaded/Failed IPC and UMA.
    This CL:
    - Removes ServiceWorker.ScriptLoadSuccess UMA. It was only recorded
      on success, since script streaming launched in M64. We were
      recording it in DidLoadInstalledScript (the success case for
      installed script loads). There was a callsite of script load failed in
      ServiceWorkerContextClient::WorkerContextFailedToStart, but we seem
      to abort start in the browser process before reaching that point,
      when script streaming fails to load from storage.
    - Removes OnScriptLoadFailed() IPC. It was only used for the UMA,
      removed above.
    - Removes OnScriptLoaded() IPC for installed worker scripts. For
      installed worker scripts, it was only used for the UMA removed above.
      We still need it for non-installed worker scripts for
      ResumeAfterDownload() after the byte-to-byte update check.
    
    Bug: 855952
    Change-Id: I51c9da88eb8265fd1e6ce89972578408ec74d364
    Reviewed-on: https://chromium-review.googlesource.com/1113089
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570319}

--- content/browser/service_worker/service_worker_version.cc
+++ content/browser/service_worker/service_worker_version.cc
@@ -999,17 +999,6 @@ void ServiceWorkerVersion::OnDetached(EmbeddedWorkerStatus old_status) {
   OnStoppedInternal(old_status);
 }
 
-void ServiceWorkerVersion::OnScriptLoaded() {
-  DCHECK(GetMainScriptHttpResponseInfo());
-  if (IsInstalled(status()))
-    UMA_HISTOGRAM_BOOLEAN("ServiceWorker.ScriptLoadSuccess", true);
-}
-
-void ServiceWorkerVersion::OnScriptLoadFailed() {
-  if (IsInstalled(status()))
-    UMA_HISTOGRAM_BOOLEAN("ServiceWorker.ScriptLoadSuccess", false);
-}
-
 void ServiceWorkerVersion::OnRegisteredToDevToolsManager() {
   for (auto& observer : listeners_)
     observer.OnDevToolsRoutingIdChanged(this);
@@ -1575,7 +1564,7 @@ void ServiceWorkerVersion::StartTimeoutTimer() {
   // The worker is starting up and not yet idle.
   ClearTick(&idle_time_);
 
-  // Ping will be activated in OnScriptLoaded.
+  // Ping will be activated in OnThreadStarted.
   ping_controller_->Deactivate();
 
   timeout_timer_.Start(FROM_HERE, kTimeoutTimerDelay, this,
