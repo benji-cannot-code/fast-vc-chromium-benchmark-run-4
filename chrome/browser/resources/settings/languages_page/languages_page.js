dd8370158d55cd58d59534810aa89b49ed327ba2
dpapad <dpapad@chromium.org>
2018-06-26 22:46:33 +0000

    WebUI Polymer 2: Fixing some Settings tests to work with Polymer 2, part 2.
    Specifically fixing:
    CrSettingsCertificateManagerTest.All
    CrSettingsLanguagesPageTest.Spellcheck
    CrSettingsLanguagesPageTest.InputMethods
    CrSettingsLanguagesPageTest.LanguageMenu
    
    Bug: 738611
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: Ie79c53113577172fe6f55cecda9a22562606c9a5
    Reviewed-on: https://chromium-review.googlesource.com/1114283
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570563}

--- chrome/browser/resources/settings/languages_page/languages_page.js
+++ chrome/browser/resources/settings/languages_page/languages_page.js
@@ -159,37 +159,37 @@ Polymer({
    * Used to determine which "Move" buttons to show for ordering enabled
    * languages.
    * @param {number} n
-   * @param {!LanguageState} language
    * @return {boolean} True if |language| is at the |n|th index in the list of
    *     enabled languages.
    * @private
    */
-  isNthLanguage_: function(n, language) {
+  isNthLanguage_: function(n) {
+    if (this.languages == undefined || this.detailLanguage_ == undefined)
+      return false;
+
     const compareLanguage = assert(this.languages.enabled[n]);
-    return language.language == compareLanguage.language;
+    return this.detailLanguage_.language == compareLanguage.language;
   },
 
   /**
-   * @param {!LanguageState} language
    * @return {boolean} True if the "Move to top" option for |language| should be
    *     visible.
    * @private
    */
-  showMoveUp_: function(language) {
+  showMoveUp_: function() {
     // "Move up" is a no-op for the top language, and redundant with
     // "Move to top" for the 2nd language.
-    return !this.isNthLanguage_(0, language) &&
-        !this.isNthLanguage_(1, language);
+    return !this.isNthLanguage_(0) && !this.isNthLanguage_(1);
   },
 
   /**
-   * @param {!LanguageState} language
    * @return {boolean} True if the "Move down" option for |language| should be
    *     visible.
    * @private
    */
-  showMoveDown_: function(language) {
-    return !this.isNthLanguage_(this.languages.enabled.length - 1, language);
+  showMoveDown_: function() {
+    return this.languages != undefined &&
+        !this.isNthLanguage_(this.languages.enabled.length - 1);
   },
 
   /**
@@ -197,7 +197,7 @@ Polymer({
    * @return {boolean} True if there are less than 2 languages.
    */
   isHelpTextHidden_: function(change) {
-    return this.languages.enabled.length <= 1;
+    return this.languages != undefined && this.languages.enabled.length <= 1;
   },
 
   // <if expr="chromeos">
@@ -334,7 +334,7 @@ Polymer({
    * @private
    */
   disableTranslateCheckbox_: function(language, targetLanguageCode) {
-    if (!language.supportsTranslate)
+    if (language == undefined || !language.supportsTranslate)
       return true;
 
     return this.languageHelper.convertLanguageCodeForTranslate(language.code) ==
@@ -448,6 +448,9 @@ Polymer({
    * @private
    */
   getSpellCheckSecondaryText_: function() {
+    if (this.languages == undefined || this.prefs == undefined)
+      return '';
+
     if (this.getSpellCheckDisabledByPolicy_())
       return loadTimeData.getString('spellCheckDisabled');
     const enabledSpellCheckLanguages =
@@ -506,6 +509,9 @@ Polymer({
 
   /** @private */
   updateSpellcheckLanguages_: function() {
+    if (this.languages == undefined)
+      return;
+
     this.set('spellCheckLanguages_', this.getSpellCheckLanguages_());
 
     // Notify Polymer of subproperties that might have changed on the items in
@@ -525,6 +531,9 @@ Polymer({
 
   /** @private */
   updateSpellcheckEnabled_: function() {
+    if (this.prefs == undefined)
+      return;
+
     this.set('spellCheckDisabled_', this.getSpellCheckDisabledByPolicy_());
 
     // If the spellcheck section was expanded, close it.
