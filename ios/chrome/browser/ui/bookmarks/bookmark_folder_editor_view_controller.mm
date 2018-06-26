80b1b00883a68dd81ef4a7bce322ebd53bad2312
Chris Lu <thegreenfrog@chromium.org>
2018-06-26 00:25:00 +0000

    [ios] Remove excess TableViewTextItem import from BookmarkFolderEditorViewController
    Change-Id: I9da70ff3b1f1199e46d536196057d6de76cfe804
    Reviewed-on: https://chromium-review.googlesource.com/1114303
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570260}

--- ios/chrome/browser/ui/bookmarks/bookmark_folder_editor_view_controller.mm
+++ ios/chrome/browser/ui/bookmarks/bookmark_folder_editor_view_controller.mm
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/icons/chrome_icon.h"
 #import "ios/chrome/browser/ui/material_components/utils.h"
 #include "ios/chrome/browser/ui/rtl_geometry.h"
-#import "ios/chrome/browser/ui/table_view/cells/table_view_text_item.h"
 #import "ios/chrome/browser/ui/table_view/chrome_table_view_styler.h"
 #import "ios/chrome/browser/ui/util/constraints_ui_util.h"
 #include "ios/chrome/grit/ios_strings.h"
