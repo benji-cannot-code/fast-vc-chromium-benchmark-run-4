c5d76d4a0d2dbeb8a15071fa96d4ebed28976065
Pedro Amaral <amaralp@chromium.org>
2018-06-26 23:19:27 +0000

    Don't show top toolbar items when bottom toolbar is enabled
    Remove the top toolbar buttons (home, tab switcher, and menu buttons)
    from their parent view and set them to null. Also add a bunch of
    null checks since now things can be null.
    
    Bug: 852117
    Change-Id: I620f35f81388d1f4ac8791a9eb45576566474604
    Reviewed-on: https://chromium-review.googlesource.com/1105569
    Commit-Queue: Pedro Amaral <amaralp@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570578}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
@@ -1241,7 +1241,9 @@ public class ToolbarManager implements ToolbarTabController, UrlFocusChangeListe
         updateReloadState(tabCrashed);
         updateBookmarkButtonStatus();
 
-        mToolbar.getMenuButtonWrapper().setVisibility(View.VISIBLE);
+        if (mToolbar.getMenuButtonWrapper() != null) {
+            mToolbar.getMenuButtonWrapper().setVisibility(View.VISIBLE);
+        }
     }
 
     private void updateBookmarkButtonStatus() {
