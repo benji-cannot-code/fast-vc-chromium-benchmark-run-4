3019bd976712c1a8909fee09b4ba183bd5d80a0c
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-06-26 14:48:44 +0000

    Import wpt@d7d545ab69e304b992ac1b3e0a78785779f57e3b
    Using wpt-import in Chromium e70e652d516c7d14d50e547aae2da1690c4ae54c.
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/20255
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    TBR=raphael.kubo.da.costa@intel.com
    
    No-Export: true
    Change-Id: If754f32b74a7da1714abe13814cf29b72439a755
    Reviewed-on: https://chromium-review.googlesource.com/1114685
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570402}

--- third_party/WebKit/LayoutTests/external/wpt/css/css-logical/resources/test-box-properties.js
+++ third_party/WebKit/LayoutTests/external/wpt/css/css-logical/resources/test-box-properties.js
@@ -91,14 +91,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
    *   }, logical: {
    *     inlineStart: "margin-inline-start", inlineEnd: "margin-inline-end",
    *     blockStart: "margin-block-start", blockEnd: "margin-block-end",
-   *   }, type: "length", prerequisites: "...", property: "'margin-*'" }
+   *   }, type: ["length"], prerequisites: "...", property: "'margin-*'" }
    *
    * @param {string} property
    *        A string representing the property names, like "margin-*".
    * @param {Object} descriptor
-   * @param {string} descriptor.type
+   * @param {string|string[]} descriptor.type
    *        Describes the kind of values accepted by the property, like "length".
-   *        Must be a key from the `testValues` object.
+   *        Must be a key or a collection of keys from the `testValues` object.
    * @param {Object={}} descriptor.prerequisites
    *        Represents property declarations that are needed by `property` to work.
    *        For example, border-width properties require a border style.
@@ -116,7 +116,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       physical[physicalSide] = isInset ? physicalSide : property.replace("*", physicalSide);
       prerequisites += makeDeclaration(descriptor.prerequisites, physicalSide);
     }
-    return {name, logical, physical, type: descriptor.type, prerequisites, property};
+    const type = [].concat(descriptor.type);
+    return {name, logical, physical, type, prerequisites, property};
   };
 
   /**
@@ -135,7 +136,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         horizontal: `${prefix}width`,
         vertical: `${prefix}height`,
       },
-      type: "length",
+      type: ["length"],
       prerequisites: makeDeclaration({display: "block"}),
       property: (prefix ? prefix.slice(0, -1) + " " : "") + "sizing",
     };
@@ -148,7 +149,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
    *        An object returned by createBoxPropertyGroup or createSizingPropertyGroup.
    */
   exports.runTests = function(group) {
-    const values = testValues[group.type];
+    const values = testValues[group.type[0]].map(function(_, i) {
+      return group.type.map(type => testValues[type][i]).join(" ");
+    });
     const logicals = Object.values(group.logical);
     const physicals = Object.values(group.physical);
 
@@ -159,8 +162,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         expected.push([logicalProp, values[i]]);
       }
       testCSSValues("logical properties in inline style", testElement.style, expected);
-      testElement.style.cssText = "";
     }, `Test that logical ${group.property} properties are supported.`);
+    testElement.style.cssText = "";
 
     for (const writingMode of writingModes) {
       for (const style of writingMode.styles) {
