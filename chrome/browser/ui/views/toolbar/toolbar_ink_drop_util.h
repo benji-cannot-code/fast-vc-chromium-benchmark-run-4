d225c7cfc84c4b701e8c23c258537185ee78e3ff
Peter Boström <pbos@chromium.org>
2018-06-26 22:36:44 +0000

    Show InkDrops on toolbar-button focus outside Mac
    Highlights the ToolbarButton inkdrop on platforms where focus rings are
    not used. Before this change toolbar buttons were not highlighted to
    reflect their focused state (outside of Mac where focus rings are used).
    
    Bug: chromium:856505
    Change-Id: Iab3578ff75a12477afa11ce34a09836acc3514d9
    Reviewed-on: https://chromium-review.googlesource.com/1115737
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570561}

--- chrome/browser/ui/views/toolbar/toolbar_ink_drop_util.h
+++ chrome/browser/ui/views/toolbar/toolbar_ink_drop_util.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/animation/ink_drop_impl.h"
 #include "ui/views/animation/ink_drop_mask.h"
 #include "ui/views/animation/ink_drop_ripple.h"
+#include "ui/views/style/platform_style.h"
 
 constexpr float kToolbarInkDropVisibleOpacity = 0.06f;
 constexpr float kToolbarInkDropHighlightVisibleOpacity = 0.08f;
@@ -73,6 +74,8 @@ std::unique_ptr<views::InkDrop> CreateToolbarInkDrop(
   ink_drop->SetAutoHighlightMode(
       views::InkDropImpl::AutoHighlightMode::SHOW_ON_RIPPLE);
   ink_drop->SetShowHighlightOnHover(true);
+  ink_drop->SetShowHighlightOnFocus(!views::PlatformStyle::kPreferFocusRings);
+
   return ink_drop;
 }
 
