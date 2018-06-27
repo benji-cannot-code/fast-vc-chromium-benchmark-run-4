4589ed5934df56b57313dc9fcb8ddd8c8ce6690f
Alexander Timin <altimin@chromium.org>
2018-06-27 11:27:53 +0000

    [scheduler] Do not use base::Thread in CompositorThreadScheduler.
    Do not save base::Thread reference in the CompositorThreadScheduler and
    post tasks to the scheduler default queue instead of using
    Thread::task_runner.
    
    R=alexclarke@chromium.org
    
    Change-Id: Idc05e3547316a5aaf6de487ec62e1be430b2499b
    Reviewed-on: https://chromium-review.googlesource.com/1115120
    Reviewed-by: Alex Clarke <alexclarke@chromium.org>
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570729}

--- third_party/blink/renderer/platform/scheduler/worker/compositor_thread_scheduler.h
+++ third_party/blink/renderer/platform/scheduler/worker/compositor_thread_scheduler.h
@@ -16,10 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/scheduler/worker/compositor_metrics_helper.h"
 #include "third_party/blink/renderer/platform/scheduler/worker/non_main_thread_scheduler_impl.h"
 
-namespace base {
-class Thread;
-}
-
 namespace blink {
 namespace scheduler {
 
@@ -27,8 +23,7 @@ class PLATFORM_EXPORT CompositorThreadScheduler
     : public NonMainThreadSchedulerImpl,
       public SingleThreadIdleTaskRunner::Delegate {
  public:
-  CompositorThreadScheduler(
-      base::Thread* thread,
+  explicit CompositorThreadScheduler(
       std::unique_ptr<base::sequence_manager::SequenceManager>
           task_queue_manager);
 
@@ -65,8 +60,6 @@ class PLATFORM_EXPORT CompositorThreadScheduler
   void InitImpl() override;
 
  private:
-  base::Thread* thread_;
-
   CompositorMetricsHelper compositor_metrics_helper_;
 
   DISALLOW_COPY_AND_ASSIGN(CompositorThreadScheduler);
