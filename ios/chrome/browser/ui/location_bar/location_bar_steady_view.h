714a02693055a87414e87ad3b44bc32dba6cd801
stkhapugin@chromium.org <stkhapugin@chromium.org>
2018-06-27 10:16:38 +0000

    [iOS] Fix broken constraints in location bar on NTP.
    Currently the NTP steady location bar constraints can be broken because
    the location image view is set to nil, making the intrinsic size of the
    image view zero. Use a distinct set of constraints in this case.
    
    Bug: 854057
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I67c21c9482e7def487308470e494539a24dfe9f1
    Reviewed-on: https://chromium-review.googlesource.com/1115126
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570721}

--- ios/chrome/browser/ui/location_bar/location_bar_steady_view.h
+++ ios/chrome/browser/ui/location_bar/location_bar_steady_view.h
@@ -32,10 +32,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)setColorScheme:(LocationBarSteadyViewColorScheme*)colorScheme;
 
+// Sets the location image. If |locationImage| is nil, hides the image view.
+- (void)setLocationImage:(UIImage*)locationImage;
+
 // The label displaying the current location URL.
 @property(nonatomic, strong) UILabel* locationLabel;
-// The image view displaying the current location icon (i.e. http[s] status).
-@property(nonatomic, strong) UIImageView* locationIconImageView;
 // The button displayed in the trailing corner of the view, i.e. share button.
 @property(nonatomic, strong) UIButton* trailingButton;
 
