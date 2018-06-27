fd3461a2e6c9175afe58deedb525075a16bda8ea
Antonio Gomes <tonikitoo@igalia.com>
2018-06-27 11:58:33 +0000

    [ozone/wayland] Implement support to Tooltips using sub-surfaces
    CL makes use of the sub-surface functionality to implement support for
    tooltips in Ozone/Wayland.
    A sub-surface has one parent surface, but its size and position are not
    limited to that of the parent. This means that, the content of a sub-surface
    is not automatically clipped to its parent's area.
    
    Additionally, by default, the input region of a sub-surface is empty.
    This causes all input events to be reported on the main surface, instead.
    
    TEST=Very basic UI feature: simply hover over any item that shows a tooltip.
    
    TBR=sadrul@chromium.org
    
    BUG=578890
    
    Change-Id: I09a0eb7c79516a995cc3a4994f4959cf15b37736
    Reviewed-on: https://chromium-review.googlesource.com/1110098
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570734}

--- ui/views/widget/desktop_aura/desktop_window_tree_host_platform.cc
+++ ui/views/widget/desktop_aura/desktop_window_tree_host_platform.cc
@@ -35,6 +35,10 @@ ui::PlatformWindowInitProperties ConvertWidgetInitParamsToInitProperties(
       properties.type = ui::PlatformWindowType::kMenu;
       break;
 
+    case Widget::InitParams::TYPE_TOOLTIP:
+      properties.type = ui::PlatformWindowType::kTooltip;
+      break;
+
     default:
       properties.type = ui::PlatformWindowType::kPopup;
       break;
