344952fd07c67e1017cb629120201cdc8a5c0eef
Adithya Srinivasan <adithyas@chromium.org>
2018-06-27 14:25:26 +0000

    Make speech layout tests use testharness.js
    Makes the tests simpler in many cases and also removes the -expected
    files.
    
    Bug: 781655
    Change-Id: I15957ca318c56f4904bb68bc085880e70823d202
    Reviewed-on: https://chromium-review.googlesource.com/1111910
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570760}

--- third_party/WebKit/LayoutTests/fast/speech/scripted/mock-speechrecognizer.js
+++ third_party/WebKit/LayoutTests/fast/speech/scripted/mock-speechrecognizer.js
@@ -2,8 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 'use strict';
 
 // MockSpeechRecognizer is a mock implementation of mojom::SpeechRecognizer and
-// the browser speech recognition service. It currently only supports running
-// one recognition session at a time. Mock results can be set using
+// the browser speech recognition service. Mock results can be set using
 // addMockSpeechRecognitionResult, and setMockSpeechRecognitionError can be used
 // to simulate an error state. If no mock results are set, a NoMatch error is
 // sent to the client.
@@ -117,7 +116,5 @@ class MockSpeechRecognitionSession {
 
   stop() {}
 
-  abort() {
-    this.recognizer_.session_client_.ended();
-  }
+  abort() {}
 }
