252aa729690c32cd3418bc92ba4766932c3c62d6
Koji Ishii <kojii@chromium.org>
2018-06-27 05:01:25 +0000

    [LayoutNG] Fix trailing spaces to be removed in some cases
    This patch fixes some cases where collapsible trailing spaces
    were not removed properly.
    
    Normally, ShapingLineBreaker can detect trailing spaces and
    they are easy to remove. This is more efficient when the
    removal may require re-shaping.
    
    However, in some cases, such as across element boundaries,
    non-text objects (borders etc.) overflow the line, or when
    out-of-flow objects follow, trailing spaces need to be
    removed as an additional step.
    
    Instead of patching each such case, this patch adds a flag
    that indicates trailing spaces removal was done, and call
    RemoveTrailingCollapsibleSpace() at the end of the line
    breaking unless the flag is set.
    
    The rebaselines revealed that justification was incorrect
    when e.g.:
      ...WORD <float><end-of-line>
    In this case, the algorithm needs to use the end of "WORD",
    not the Object Replacement Character that represents the
    float. This patch includes the fix too.
    
    Bug: 636993
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng
    Change-Id: Ic685c3f045b6b8dd2d8b292a6a0fdc32a208d1d2
    Reviewed-on: https://chromium-review.googlesource.com/1113159
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570663}

--- third_party/blink/renderer/core/layout/ng/inline/ng_line_breaker.h
+++ third_party/blink/renderer/core/layout/ng/inline/ng_line_breaker.h
@@ -92,6 +92,9 @@ class CORE_EXPORT NGLineBreaker {
     // the next line.
     bool is_after_forced_break = false;
 
+    // True if trailing collapsible spaces have been collapsed.
+    bool trailing_spaces_collapsed = false;
+
     LayoutUnit AvailableWidth() const {
       return line_opportunity.AvailableInlineSize();
     }
