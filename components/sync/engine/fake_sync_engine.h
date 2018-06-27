3777da54cbc212c8a181f829394b6352212f1ca8
Marc Treib <treib@chromium.org>
2018-06-27 16:49:53 +0000

    Small cleanups/improvements in and around ProfileSyncServiceStartupTest
    In particular, this gets rid of ProfileSyncServiceStartupCrosTest, which
    was not meaningfully different from the non-Cros version.
    Also adds a few extra assertions and comments.
    
    Bug: none
    Change-Id: Iabba34647d084b1518a2cea239c9f25d463aa27b
    Reviewed-on: https://chromium-review.googlesource.com/1117073
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570807}

--- components/sync/engine/fake_sync_engine.h
+++ components/sync/engine/fake_sync_engine.h
@@ -16,10 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace syncer {
 
-// A mock of the SyncEngine.
+// A fake of the SyncEngine.
 //
 // This class implements the bare minimum required for the ProfileSyncService to
-// get through initialization.  It often returns null pointers or nonesense
+// get through initialization. It often returns null pointers or nonsense
 // values; it is not intended to be used in tests that depend on SyncEngine
 // behavior.
 class FakeSyncEngine : public SyncEngine {
@@ -27,6 +27,7 @@ class FakeSyncEngine : public SyncEngine {
   FakeSyncEngine();
   ~FakeSyncEngine() override;
 
+  // Immediately calls params.host->OnEngineInitialized.
   void Initialize(InitParams params) override;
 
   void TriggerRefresh(const ModelTypeSet& types) override;
