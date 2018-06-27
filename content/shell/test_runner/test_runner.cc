814e4f23dacf9269843bd4e83445df95b525f8a7
Kent Tamura <tkent@chromium.org>
2018-06-27 09:32:00 +0000

    test_runner: Support file selection with RunFileChooser().
    Add a mock implementation of RunFileChooser(), with a testRunner
    function setFileChooserPaths().
    
    Bug: 637639
    Change-Id: Ic5536524b2a5381556605b79e1907429d985e245
    Reviewed-on: https://chromium-review.googlesource.com/1114573
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570711}

--- content/shell/test_runner/test_runner.cc
+++ content/shell/test_runner/test_runner.cc
@@ -208,6 +208,7 @@ class TestRunnerBindings : public gin::Wrappable<TestRunnerBindings> {
   void SetDumpConsoleMessages(bool value);
   void SetDumpJavaScriptDialogs(bool value);
   void SetEffectiveConnectionType(const std::string& connection_type);
+  void SetFileChooserPaths(const std::vector<std::string>& paths);
   void SetMockSpellCheckerEnabled(bool enabled);
   void SetImagesAllowed(bool allowed);
   void SetIsolatedWorldContentSecurityPolicy(int world_id,
@@ -523,6 +524,8 @@ gin::ObjectTemplateBuilder TestRunnerBindings::GetObjectTemplateBuilder(
                  &TestRunnerBindings::SetDumpJavaScriptDialogs)
       .SetMethod("setEffectiveConnectionType",
                  &TestRunnerBindings::SetEffectiveConnectionType)
+      .SetMethod("setFileChooserPaths",
+                 &TestRunnerBindings::SetFileChooserPaths)
       .SetMethod("setMockSpellCheckerEnabled",
                  &TestRunnerBindings::SetMockSpellCheckerEnabled)
       .SetMethod("setIconDatabaseEnabled", &TestRunnerBindings::NotImplemented)
@@ -745,6 +748,12 @@ void TestRunnerBindings::SetEffectiveConnectionType(
 
   if (runner_)
     runner_->SetEffectiveConnectionType(web_type);
+}
+
+void TestRunnerBindings::SetFileChooserPaths(
+    const std::vector<std::string>& paths) {
+  if (runner_)
+    runner_->SetFileChooserPaths(paths);
 }
 
 void TestRunnerBindings::SetMockSpellCheckerEnabled(bool enabled) {
@@ -1559,6 +1568,7 @@ void TestRunner::Reset() {
     close_remaining_windows_ = true;
 
   spellcheck_->Reset();
+  file_chooser_paths_ = std::vector<std::string>();
 }
 
 void TestRunner::SetTestIsRunning(bool running) {
@@ -2530,6 +2540,10 @@ void TestRunner::OnLayoutTestRuntimeFlagsChanged() {
   delegate_->OnLayoutTestRuntimeFlagsChanged(
       layout_test_runtime_flags_.tracked_dictionary().changed_values());
   layout_test_runtime_flags_.tracked_dictionary().ResetChangeTracking();
+}
+
+void TestRunner::SetFileChooserPaths(const std::vector<std::string>& paths) {
+  file_chooser_paths_ = paths;
 }
 
 void TestRunner::LocationChangeDone() {
