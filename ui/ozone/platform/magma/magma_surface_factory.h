6f6f47827f47f82831b7029fcad60ff7d141144d
Mustafa Çamurcu <camurcu@google.com>
2018-06-27 21:45:37 +0000

    [Ozone/Magma] Ozone Magma Platform
    Ozone Fuchsia platform built on top of Zircon.
    
    This platform will enable Chrome to run on Fuchsia
    using Chrome's own Display Compositor and event handling,
    independent from Scenic. This is a stub implementation
    of the platform. The parts that connect to Zircon
    will be implemented in future cls.
    
    
    Change-Id: Ie6e43946e5a38b58220f5455a2a7b89fcd8ba15a
    Reviewed-on: https://chromium-review.googlesource.com/1091178
    Commit-Queue: Mustafa Çamurcu <camurcu@google.com>
    Reviewed-by: Michael Spang <spang@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570909}

---
+++ ui/ozone/platform/magma/magma_surface_factory.h
@@ -0,0 +1,37 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_OZONE_PLATFORM_MAGMA_MAGMA_SURFACE_FACTORY_H_
+#define UI_OZONE_PLATFORM_MAGMA_MAGMA_SURFACE_FACTORY_H_
+
+#include <memory>
+#include <vector>
+
+#include "base/files/file_path.h"
+#include "base/macros.h"
+#include "ui/ozone/public/gl_ozone.h"
+#include "ui/ozone/public/surface_factory_ozone.h"
+
+namespace ui {
+
+// Configure Magma platform to indicate that it does not support GL
+class MagmaSurfaceFactory : public SurfaceFactoryOzone {
+ public:
+  MagmaSurfaceFactory();
+  ~MagmaSurfaceFactory() override;
+
+  // SurfaceFactoryOzone:
+  std::vector<gl::GLImplementation> GetAllowedGLImplementations() override;
+  GLOzone* GetGLOzone(gl::GLImplementation implementation) override;
+
+ private:
+  std::unique_ptr<GLOzone> egl_implementation_;
+
+  DISALLOW_COPY_AND_ASSIGN(MagmaSurfaceFactory);
+};
+
+}  // namespace ui
+
+#endif  // UI_OZONE_PLATFORM_MAGMA_MAGMA_SURFACE_FACTORY_H_
