f1e5c5a5f3a52e8fc2649b02a23fa0d1a713101d
Joshua Pawlicki <waffles@chromium.org>
2018-06-27 15:32:04 +0000

    Add UMA to record update client error codes.
    Bug: 855724
    Change-Id: Ib0a7ccc2f2fe272d53d19ef833b47dcf3994b0ba
    Reviewed-on: https://chromium-review.googlesource.com/1114096
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570783}

--- components/update_client/update_client_errors.h
+++ components/update_client/update_client_errors.h
@@ -11,7 +11,6 @@ namespace update_client {
 // Errors generated as a result of calling UpdateClient member functions.
 // These errors are not sent in pings.
 enum class Error {
-  INVALID_ARGUMENT = -1,
   NONE = 0,
   UPDATE_IN_PROGRESS = 1,
   UPDATE_CANCELED = 2,
@@ -19,6 +18,8 @@ enum class Error {
   SERVICE_ERROR = 4,
   UPDATE_CHECK_ERROR = 5,
   CRX_NOT_FOUND = 6,
+  INVALID_ARGUMENT = 7,
+  MAX_VALUE,
 };
 
 // These errors are sent in pings. Add new values only to the bottom of
