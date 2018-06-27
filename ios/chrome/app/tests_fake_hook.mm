062469fbc51d74128ad8434106db9c1407fc6aa3
Olivier Robin <olivierrobin@chromium.org>
2018-06-27 09:46:22 +0000

    Create DisableAppGroupAccess test hook
    EGtests bundle don't have access to application groups.
    Create a test hook to prevent app group access.
    
    Bug: 855625
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I3c773f92edfdaafb9207d69a51ba6fdee823868b
    Reviewed-on: https://chromium-review.googlesource.com/1113540
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570715}

--- ios/chrome/app/tests_fake_hook.mm
+++ ios/chrome/app/tests_fake_hook.mm
@@ -11,6 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace tests_hook {
 
+bool DisableAppGroupAccess() {
+  return false;
+}
 bool DisableContentSuggestions() {
   return false;
 }
--- ios/chrome/app/tests_hook.h
+++ ios/chrome/app/tests_hook.h
@@ -8,6 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace tests_hook {
 
+// Returns true if app group access should be disabled as tests don't have the
+// required entitlements.
+// This is used by internal code.
+bool DisableAppGroupAccess();
+
 // Returns true if ContentSuggestions should be disabled to allow other tests to
 // run unimpeded.
 bool DisableContentSuggestions();
