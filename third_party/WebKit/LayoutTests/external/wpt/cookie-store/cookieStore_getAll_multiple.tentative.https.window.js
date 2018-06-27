d506ac428c3dd4e266118bb82db25525c26ac862
Victor Costan <pwnall@chromium.org>
2018-06-27 01:58:12 +0000

    Async Cookies: The API is only available on secure origins.
    Bug: 856362
    Change-Id: Icaddfb82b2c41924cca7f058dea2e8da03183ae9
    Reviewed-on: https://chromium-review.googlesource.com/1114379
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570628}

--- third_party/WebKit/LayoutTests/external/wpt/cookie-store/cookieStore_getAll_multiple.tentative.https.window.js
+++ third_party/WebKit/LayoutTests/external/wpt/cookie-store/cookieStore_getAll_multiple.tentative.https.window.js
@@ -1,12 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-<!doctype html>
-<meta charset="utf-8">
-<title>Async Cookies: cookieStore.getAll() sees cookieStore.set() cookie</title>
-<link rel="help" href="https://github.com/WICG/cookie-store">
-<link rel="author" href="pwnall@chromium.org" title="Victor Costan">
-<script src="/resources/testharness.js"></script>
-<script src="/resources/testharnessreport.js"></script>
-<script>
 'use strict';
 
 // Workaround because add_cleanup doesn't support async functions yet.
@@ -38,5 +30,3 @@ promise_test(async testCase => {
   await async_cleanup(() => cookieStore.delete('cookie-name-2'));
   await async_cleanup(() => cookieStore.delete('cookie-name-3'));
 }, 'cookieStore.getAll returns multiple cookies written by cookieStore.set');
-
-</script>
--- third_party/WebKit/LayoutTests/external/wpt/cookie-store/cookieStore_special_names.tentative.https.window.js
+++ third_party/WebKit/LayoutTests/external/wpt/cookie-store/cookieStore_special_names.tentative.https.window.js
@@ -1,12 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-<!doctype html>
-<meta charset="utf-8">
-<title>Cookie Store: cookieStore handles special cookie names correctly (secure context)</title>
-<link rel="help" href="https://github.com/WICG/cookie-store">
-<link rel="author" href="pwnall@chromium.org" title="Victor Costan">
-<script src="/resources/testharness.js"></script>
-<script src="/resources/testharnessreport.js"></script>
-<script>
 'use strict';
 
 ['__Secure-', '__Host-'].forEach(prefix => {
@@ -38,5 +30,3 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       `Deleting ${prefix} cookies should not fail in secure context`);
   }, `cookieStore.delete with ${prefix} name on secure origin`);
 });
-
-</script>
