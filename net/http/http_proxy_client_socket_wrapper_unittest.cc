48aa0dc6d4f61a9fab7ab4bd6773f7162735a338
Zhongyi Shi <zhongyi@chromium.org>
2018-06-27 22:00:43 +0000

    Some more cleanup in QUIC Connection Migration V2, no behavior change.
    This change changes ShouldMigrateSession(renamed as IsSessionMigratable) to take a boolean to close session if not migratable. It also removes MigrateToAlternate, which was only called in MigrateSessionOnWriteError by directly calling IsSessionMigratable.
    
    Bug: 818259
    Change-Id: I699675b43fcdcde91b079021a48a77eff6b6bf32
    Reviewed-on: https://chromium-review.googlesource.com/1115763
    Commit-Queue: Zhongyi Shi <zhongyi@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570914}

--- net/http/http_proxy_client_socket_wrapper_unittest.cc
+++ net/http/http_proxy_client_socket_wrapper_unittest.cc
@@ -100,8 +100,6 @@ class HttpProxyClientSocketWrapperTest
         store_server_configs_in_properties_(false),
         idle_connection_timeout_seconds_(kIdleConnectionTimeoutSeconds),
         reduced_ping_timeout_seconds_(quic::kPingTimeoutSecs),
-        migrate_sessions_on_network_change_v2_(false),
-        migrate_sessions_early_v2_(false),
         allow_server_migration_(false),
         race_cert_verification_(false),
         estimate_initial_rtt_(false),
@@ -132,8 +130,9 @@ class HttpProxyClientSocketWrapperTest
         /*max_time_before_crypto_handshake_seconds=*/
         quic::kMaxTimeForCryptoHandshakeSecs,
         /*max_idle_time_before_crypto_handshake_seconds=*/
-        quic::kInitialIdleTimeoutSecs, migrate_sessions_on_network_change_v2_,
-        migrate_sessions_early_v2_,
+        quic::kInitialIdleTimeoutSecs,
+        /*migrate_sessions_on_network_change_v2=*/false,
+        /*migrate_sessions_early_v2=*/false,
         base::TimeDelta::FromSeconds(kMaxTimeOnNonDefaultNetworkSecs),
         kMaxMigrationsToNonDefaultNetworkOnPathDegrading,
         allow_server_migration_, race_cert_verification_, estimate_initial_rtt_,
@@ -229,8 +228,6 @@ class HttpProxyClientSocketWrapperTest
   bool store_server_configs_in_properties_;
   int idle_connection_timeout_seconds_;
   int reduced_ping_timeout_seconds_;
-  bool migrate_sessions_on_network_change_v2_;
-  bool migrate_sessions_early_v2_;
   bool allow_server_migration_;
   bool race_cert_verification_;
   bool estimate_initial_rtt_;
