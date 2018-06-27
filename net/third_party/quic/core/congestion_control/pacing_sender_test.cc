5241b9d74f80396ba97d9d3244c9e0e55fc02031
Ryan Hamilton <rch@chromium.org>
2018-06-27 00:55:23 +0000

    Clean up files in QUIC's core/congestion_control/ directory to remove code divergence with internal version.
    Change-Id: Icc7ea78ba632b066224074cec64022e8517ca09c
    Reviewed-on: https://chromium-review.googlesource.com/1114148
    Commit-Queue: Ryan Hamilton <rch@chromium.org>
    Reviewed-by: Zhongyi Shi <zhongyi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570599}

--- net/third_party/quic/core/congestion_control/pacing_sender_test.cc
+++ net/third_party/quic/core/congestion_control/pacing_sender_test.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/third_party/quic/platform/api/quic_test.h"
 #include "net/third_party/quic/test_tools/mock_clock.h"
 #include "net/third_party/quic/test_tools/quic_test_utils.h"
-#include "testing/gtest/include/gtest/gtest.h"
 
 using testing::_;
 using testing::AtMost;
