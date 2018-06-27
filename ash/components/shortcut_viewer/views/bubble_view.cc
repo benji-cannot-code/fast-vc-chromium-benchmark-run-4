833044840a95c87682fd1d31af29b74f4051a327
wutao <wutao@chromium.org>
2018-06-27 01:40:14 +0000

    cros: Change keyboard shortcut viewer colors.
    This cl updates shortcuts' colors to match Chrome's.
    Changes:
    1. All Grey 800 #3C4043 should be changed to Grey 900 #202124.
    2. All Grey 600 #80868B should be changed to Grey 700 #5F6368.
    These changes apply everywhere, including searchbox, side nav and keys.
    
    Bug: 835374
    Test: manual.
    Change-Id: I772a54012c031540e8a3476d97bff15805b322e6
    Reviewed-on: https://chromium-review.googlesource.com/1115801
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570618}

--- ash/components/shortcut_viewer/views/bubble_view.cc
+++ ash/components/shortcut_viewer/views/bubble_view.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/default_style.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/gfx/canvas.h"
+#include "ui/gfx/color_palette.h"
 #include "ui/gfx/paint_vector_icon.h"
 #include "ui/gfx/shadow_value.h"
 #include "ui/gfx/skia_paint_util.h"
@@ -70,8 +71,7 @@ void BubbleView::SetIcon(const gfx::VectorIcon& icon) {
 void BubbleView::SetText(const base::string16& text) {
   if (!text_) {
     text_ = new views::Label();
-    constexpr SkColor kTextColor = SkColorSetARGB(0xFF, 0x5F, 0x63, 0x68);
-    text_->SetEnabledColor(kTextColor);
+    text_->SetEnabledColor(gfx::kGoogleGrey700);
     text_->SetElideBehavior(gfx::NO_ELIDE);
     constexpr int kLabelFontSizeDelta = 1;
     text_->SetFontList(
