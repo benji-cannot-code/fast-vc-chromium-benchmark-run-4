e2c4ae49bfe926f97cbdfe9ec5404676204b1c32
Shik Chen <shik@chromium.org>
2018-06-27 06:58:54 +0000

    media: implement CameraDeviceStatusChange for external camera
    BUG=b:77833131,b:64996728,chromium:832816
    TEST=Verify CCA works with and without external camera on Nautilus with
         some patches in cros_camera_service and usb hal.
    TEST=Verify CCA works with unmodified cros_camera_service and usb hal
         on Nautilus.
    
    Change-Id: Ie759846a199e41778aa3c5d97ecfa2f630515544
    Reviewed-on: https://chromium-review.googlesource.com/1105648
    Commit-Queue: Shik Chen <shik@chromium.org>
    Reviewed-by: Ricky Liang <jcliang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570675}

--- media/capture/video/chromeos/camera_hal_delegate.h
+++ media/capture/video/chromeos/camera_hal_delegate.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/sequence_checker.h"
 #include "base/single_thread_task_runner.h"
+#include "base/synchronization/lock.h"
 #include "base/synchronization/waitable_event.h"
 #include "base/threading/thread.h"
 #include "media/capture/video/chromeos/mojo/camera3.mojom.h"
@@ -130,9 +131,11 @@ class CAPTURE_EXPORT CameraHalDelegate final
   // reported by the camera HAL, and |camera_info_| stores the camera info of
   // each camera device. They are modified only on |ipc_task_runner_|. They
   // are also read in GetSupportedFormats and GetDeviceDescriptors, in which the
-  // access is sequenced through UpdateBuiltInCameraInfo and
-  // |builtin_camera_info_updated_| to avoid race conditions.
+  // access is protected by |camera_info_lock_| and sequenced through
+  // UpdateBuiltInCameraInfo and |builtin_camera_info_updated_| to avoid race
+  // conditions.
   size_t num_builtin_cameras_;
+  base::Lock camera_info_lock_;
   std::unordered_map<std::string, cros::mojom::CameraInfoPtr> camera_info_;
 
   SEQUENCE_CHECKER(sequence_checker_);
