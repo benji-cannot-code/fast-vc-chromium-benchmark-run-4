c9d773e048e053f68aa1c99fefa2c4c8af26a15c
Ahmed Fakhry <afakhry@google.com>
2018-06-27 21:33:03 +0000

    Hide Browser's window controls in tablet mode
    In tablet mode, to prevent accidental clicks on the browser's
    window controls and to give more horizontal space for tabs
    and the new tab button in split screen, we hide the caption
    buttons container. We only do this for non-app browser windows
    and only when the Home Launcher feature is enabled.
    
    This is part of the Touch MLP project.
    
    BUG=822455
    TEST=Added test coverage.
    
    Change-Id: Idda77cf5b87afdd32cca586cbfb39b49c398a520
    Reviewed-on: https://chromium-review.googlesource.com/1115460
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570905}

--- ash/frame/header_view.cc
+++ ash/frame/header_view.cc
@@ -52,7 +52,7 @@ class WindowPropertyAppearanceProvider
                                          : kFrameImageOverlayInactiveKey);
   }
 
-  bool IsTabletMode() override {
+  bool IsTabletMode() const override {
     return Shell::Get()
         ->tablet_mode_controller()
         ->IsTabletModeWindowManagerEnabled();
