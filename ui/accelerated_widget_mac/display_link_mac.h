2531a5b004f23c062fdc1900d58ff93a5cb4c3a1
Avi Drissman <avi@chromium.org>
2018-06-28 20:21:20 +0000

    Fix ui::DisplayLinkMac lifetime management.
    Use a base::NoDestructor rather than a LazyInstance.
    
    Connect the display link callback via display id rather than
    DisplayLinkMac to ensure that no stale pointer issues can
    cause crashes.
    
    In addition, "<= 0" was used on uints. Fixed.
    
    This reverts the speculative fix that was committed in
    119ecfdddd45db9e5e74d0d6a6eb4d9423cec8f2 as it did not
    target the actual issue.
    
    BUG=564780
    TEST=crashes go bye?
    
    Change-Id: I779bd50fb3ffd1a96c5e8bd1e89ac18b19b7bef8
    Reviewed-on: https://chromium-review.googlesource.com/1117615
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571241}

--- ui/accelerated_widget_mac/display_link_mac.h
+++ ui/accelerated_widget_mac/display_link_mac.h
@@ -6,15 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_ACCELERATED_WIDGET_MAC_DISPLAY_LINK_MAC_H_
 #define UI_ACCELERATED_WIDGET_MAC_DISPLAY_LINK_MAC_H_
 
-#include <map>
 #include <QuartzCore/CVDisplayLink.h>
 
-#include "base/lazy_instance.h"
+#include <map>
+
 #include "base/mac/scoped_typeref.h"
 #include "base/memory/ref_counted.h"
-#include "base/synchronization/lock.h"
 #include "base/time/time.h"
-#include "base/timer/timer.h"
 #include "ui/accelerated_widget_mac/accelerated_widget_mac_export.h"
 
 namespace base {
@@ -26,6 +24,7 @@ namespace ui {
 class ACCELERATED_WIDGET_MAC_EXPORT DisplayLinkMac
     : public base::RefCountedThreadSafe<DisplayLinkMac> {
  public:
+  // This must only be called from the main thread.
   static scoped_refptr<DisplayLinkMac> GetForDisplay(
       CGDirectDisplayID display_id);
 
@@ -51,10 +50,31 @@ class ACCELERATED_WIDGET_MAC_EXPORT DisplayLinkMac
 
   void StartOrContinueDisplayLink();
   void StopDisplayLink();
-  void Tick(const CVTimeStamp& time);
 
-  // Called by the system on the display link thread, and posts a call to Tick
-  // to the UI thread.
+  // Looks up the display and calls UpdateVSyncParameters() on the corresponding
+  // DisplayLinkMac.
+  static void DoUpdateVSyncParameters(CGDirectDisplayID display,
+                                      const CVTimeStamp& time);
+
+  // Processes the display link callback.
+  void UpdateVSyncParameters(const CVTimeStamp& time);
+
+  // Each display link instance consumes a non-negligible number of cycles, so
+  // make all display links on the same screen share the same object. This map
+  // must only be changed from the main thread.
+  //
+  // Note that this is a weak map, holding non-owning pointers to the
+  // DisplayLinkMac objects. DisplayLinkMac is a ref-counted class, and is
+  // jointly owned by the various callers that got a copy by calling
+  // GetForDisplay().
+  using DisplayLinkMap = std::map<CGDirectDisplayID, DisplayLinkMac*>;
+  static DisplayLinkMap& GetAllDisplayLinks();
+
+  // The task runner to post tasks to from the display link thread.
+  static scoped_refptr<base::SingleThreadTaskRunner> GetMainThreadTaskRunner();
+
+  // Called by the system on the display link thread, and posts a call to
+  // DoUpdateVSyncParameters() to the UI thread.
   static CVReturn DisplayLinkCallback(
       CVDisplayLinkRef display_link,
       const CVTimeStamp* now,
@@ -70,28 +90,20 @@ class ACCELERATED_WIDGET_MAC_EXPORT DisplayLinkMac
       CGDisplayChangeSummaryFlags flags,
       void* user_info);
 
-  // The task runner to post tasks to from the display link thread.
-  scoped_refptr<base::SingleThreadTaskRunner> main_thread_task_runner_;
-
   // The display that this display link is attached to.
   CGDirectDisplayID display_id_;
 
   // CVDisplayLink for querying VSync timing info.
   base::ScopedTypeRef<CVDisplayLinkRef> display_link_;
 
-  // VSync parameters computed during Tick.
-  bool timebase_and_interval_valid_;
+  // VSync parameters computed during UpdateVSyncParameters().
+  bool timebase_and_interval_valid_ = false;
   base::TimeTicks timebase_;
   base::TimeDelta interval_;
 
   // The time after which we should re-start the display link to get fresh
   // parameters.
   base::TimeTicks recalculate_time_;
-
-  // Each display link instance consumes a non-negligible number of cycles, so
-  // make all display links on the same screen share the same object.
-  typedef std::map<CGDirectDisplayID, DisplayLinkMac*> DisplayMap;
-  static base::LazyInstance<DisplayMap>::DestructorAtExit display_map_;
 };
 
 }  // ui
