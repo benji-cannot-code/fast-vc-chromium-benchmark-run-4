32f1d8d22ef98835fc1f810f50ebb1876274f9e6
Ryan Cui <rcui@chromium.org>
2018-06-28 21:11:28 +0000

    Device actions interface for enabling WiFi
    Interface is implemented by Chrome and a handle is passed to the assistant
    service during initialization.
    
    Design doc: go/croissant-device-actions
    
    Bug: b/78189672
    Change-Id: I3c5bfe94772d0a6d058e4736d481325318d0b461
    Reviewed-on: https://chromium-review.googlesource.com/1110452
    Commit-Queue: Ryan Cui <rcui@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571259}

--- chrome/browser/ui/ash/assistant/assistant_client.h
+++ chrome/browser/ui/ash/assistant/assistant_client.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/macros.h"
+#include "chrome/browser/ui/ash/assistant/device_actions.h"
 #include "chromeos/services/assistant/public/mojom/assistant.mojom.h"
 #include "mojo/public/cpp/bindings/binding.h"
 
@@ -37,7 +38,11 @@ class AssistantClient : chromeos::assistant::mojom::Client {
 
  private:
   mojo::Binding<chromeos::assistant::mojom::Client> client_binding_;
+  mojo::Binding<chromeos::assistant::mojom::DeviceActions>
+      device_actions_binding_;
   chromeos::assistant::mojom::AssistantPlatformPtr assistant_connection_;
+
+  DeviceActions device_actions_;
 
   std::unique_ptr<AssistantImageDownloader> assistant_image_downloader_;
   std::unique_ptr<AssistantSetup> assistant_setup_;
---
+++ chrome/browser/ui/ash/assistant/device_actions.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/ash/assistant/device_actions.h"
+#include "chromeos/network/network_state_handler.h"
+
+using chromeos::NetworkHandler;
+using chromeos::NetworkStateHandler;
+using chromeos::NetworkTypePattern;
+
+DeviceActions::DeviceActions() {}
+
+DeviceActions::~DeviceActions() = default;
+
+void DeviceActions::SetWifiEnabled(bool enabled) {
+  NetworkHandler::Get()->network_state_handler()->SetTechnologyEnabled(
+      NetworkTypePattern::WiFi(), enabled,
+      chromeos::network_handler::ErrorCallback());
+}
