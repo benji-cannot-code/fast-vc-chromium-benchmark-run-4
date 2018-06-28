7b71e4e7cd5dd6eb56dbd85bcf6753cc3d50fe5f
Bret Sepulveda <bsep@chromium.org>
2018-06-28 23:59:31 +0000

    Fix first profile being highlighted when the avatar button is clicked.
    When the avatar button is activated using the keyboard the first profile
    should be highlighted. But the new Refresh avatar button was using a
    code path that always indicated the keyboard was used.
    
    As a short-term fix, this patch avoids that code path entirely.
    
    Bug: 849810
    Change-Id: I87c32cf76870d92911f227afd74a9dd1e201a4b2
    Reviewed-on: https://chromium-review.googlesource.com/1119385
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571333}

--- chrome/browser/ui/views/profiles/avatar_toolbar_button.h
+++ chrome/browser/ui/views/profiles/avatar_toolbar_button.h
@@ -15,6 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/toolbar/toolbar_button.h"
 #include "components/signin/core/browser/account_tracker_service.h"
 #include "components/signin/core/browser/gaia_cookie_manager_service.h"
+#include "ui/events/event.h"
+
+class Browser;
 
 class AvatarToolbarButton : public ToolbarButton,
                             public AvatarButtonErrorControllerDelegate,
@@ -22,7 +25,7 @@ class AvatarToolbarButton : public ToolbarButton,
                             public GaiaCookieManagerService::Observer,
                             public AccountTrackerService::Observer {
  public:
-  AvatarToolbarButton(Profile* profile, views::ButtonListener* listener);
+  explicit AvatarToolbarButton(Browser* browser);
   ~AvatarToolbarButton() override;
 
   void UpdateIcon();
@@ -30,6 +33,9 @@ class AvatarToolbarButton : public ToolbarButton,
 
  private:
   enum class SyncState { kNormal, kPaused, kError };
+
+  // ToolbarButton:
+  void NotifyClick(const ui::Event& event) override;
 
   // AvatarButtonErrorControllerDelegate:
   void OnAvatarErrorChanged() override;
@@ -63,6 +69,7 @@ class AvatarToolbarButton : public ToolbarButton,
   gfx::Image GetIconImageFromProfile() const;
   SyncState GetSyncState();
 
+  Browser* const browser_;
   Profile* const profile_;
 
 #if !defined(OS_CHROMEOS)
