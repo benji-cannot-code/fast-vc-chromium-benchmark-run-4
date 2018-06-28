2a840ab34eea97f8aa1a26f7241a476d837051af
Christopher Cameron <ccameron@chromium.org>
2018-06-28 02:26:15 +0000

    RemoteRWHVMac: Mojo-ify RenderWidgetHostNSViewBridge
    Replace the C++ interface RenderWidgetHostNSViewBridge with an
    equivalent mojo implementation.
    
    Add required native types WebCursor and EncodedAttributedString.
    
    Touch up RenderWidgetHostNSViewBridgeLocal's functions to match new
    signatures.
    
    Bug: 821651
    Change-Id: I8e1ed2e842571ffdb28a08b1546ef5996120266a
    Reviewed-on: https://chromium-review.googlesource.com/1110755
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571005}

--- content/browser/renderer_host/render_widget_host_ns_view_bridge_local.h
+++ content/browser/renderer_host/render_widget_host_ns_view_bridge_local.h
@@ -10,8 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/mac/scoped_nsobject.h"
 #import "content/browser/renderer_host/popup_window_mac.h"
-#import "content/browser/renderer_host/render_widget_host_ns_view_bridge.h"
 #import "content/browser/renderer_host/render_widget_host_view_cocoa.h"
+#include "content/common/render_widget_host_ns_view.mojom.h"
 #include "ui/accelerated_widget_mac/display_ca_layer_tree.h"
 #include "ui/display/display_observer.h"
 
@@ -20,15 +20,21 @@ namespace content {
 // Bridge to a locally-hosted NSView -- this is always instantiated in the same
 // process as the NSView. The caller of this interface may exist in another
 // process.
-class RenderWidgetHostNSViewBridgeLocal : public RenderWidgetHostNSViewBridge,
-                                          public display::DisplayObserver {
+class RenderWidgetHostNSViewBridgeLocal
+    : public mojom::RenderWidgetHostNSViewBridge,
+      public display::DisplayObserver {
  public:
   explicit RenderWidgetHostNSViewBridgeLocal(
       RenderWidgetHostNSViewClient* client);
   ~RenderWidgetHostNSViewBridgeLocal() override;
 
-  // RenderWidgetHostNSViewBridge implementation.
-  RenderWidgetHostViewCocoa* GetRenderWidgetHostViewCocoa() override;
+  // TODO(ccameron): RenderWidgetHostViewMac and other functions currently use
+  // this method to communicate directly with RenderWidgetHostViewCocoa. The
+  // goal of this class is to eliminate this direct communication (so this
+  // method is expected to go away).
+  RenderWidgetHostViewCocoa* GetRenderWidgetHostViewCocoa();
+
+  // mojom::RenderWidgetHostNSViewBridge implementation.
   void InitAsPopup(const gfx::Rect& content_rect,
                    blink::WebPopupType popup_type) override;
   void DisableDisplay() override;
@@ -40,7 +46,7 @@ class RenderWidgetHostNSViewBridgeLocal : public RenderWidgetHostNSViewBridge,
   void SetTooltipText(const base::string16& display_text) override;
   void SetTextInputType(ui::TextInputType text_input_type) override;
   void SetTextSelection(const base::string16& text,
-                        size_t offset,
+                        uint64_t offset,
                         const gfx::Range& range) override;
   void SetCompositionRangeInfo(const gfx::Range& range) override;
   void CancelComposition() override;
@@ -50,8 +56,9 @@ class RenderWidgetHostNSViewBridgeLocal : public RenderWidgetHostNSViewBridge,
   void ShowDictionaryOverlayForSelection() override;
   void ShowDictionaryOverlay(
       const mac::AttributedStringCoder::EncodedString& encoded_string,
-      gfx::Point baseline_point) override;
-  void LockKeyboard(base::Optional<base::flat_set<ui::DomCode>> codes) override;
+      const gfx::Point& baseline_point) override;
+  void LockKeyboard(
+      const base::Optional<std::vector<uint32_t>>& uint_dom_codes) override;
   void UnlockKeyboard() override;
 
  private:
