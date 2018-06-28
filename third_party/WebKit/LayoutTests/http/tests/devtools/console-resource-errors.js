4aae9f7d909cde8c2ebf8bacebb1a1dd081cbe8e
Erik Luo <luoe@chromium.org>
2018-06-28 16:20:32 +0000

    DevTools: show localized error message for Network errors in Console
    When migrating [1] formatting logic from ConsoleViewMessage.js to
    BrowserConsole.js, we forgot to add the line that tries to show a
    localizedFailDescription.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/920541
    
    Bug: 816743
    Change-Id: Ic3e6cc49b8634a12fe8667c19fdc4623780075db
    Reviewed-on: https://chromium-review.googlesource.com/1115872
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Erik Luo <luoe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571153}

--- third_party/WebKit/LayoutTests/http/tests/devtools/console-resource-errors.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/console-resource-errors.js
@@ -10,8 +10,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   await TestRunner.evaluateInPagePromise(`
       function performActions()
       {
-          loadXHR();
-          loadIframe();
+          var image = document.createElement('img');
+          image.onerror = step2;
+          image.src = "unknown-scheme://foo";
+      }
+
+      function step2() {
+        loadXHR();
+        loadIframe();
       }
 
       function loadXHR()
