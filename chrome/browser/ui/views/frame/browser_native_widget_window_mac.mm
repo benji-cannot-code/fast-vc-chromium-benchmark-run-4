5d5a3e5c88a434b31e80d62039c2c845e433ada8
Sidney San Martín <sdy@chromium.org>
2018-06-28 14:55:50 +0000

    Fix a crash on early releases of macOS 10.10.
    So far, all instances of this missing selector crash linked in the bug have
    come from 10.10.0, but my copy of 10.10 has it. This change checks
    `respondsToSelector:`, instead of OS version, before calling SPI.
    
    Bug: 856211
    Change-Id: Ib2aa07e0e24b83f1766a2f6e16e19b2b123970a6
    Reviewed-on: https://chromium-review.googlesource.com/1115781
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Sidney San Martín <sdy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571123}

--- chrome/browser/ui/views/frame/browser_native_widget_window_mac.mm
+++ chrome/browser/ui/views/frame/browser_native_widget_window_mac.mm
@@ -16,8 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @interface NSWindow (PrivateAPI)
 + (Class)frameViewClassForStyleMask:(NSUInteger)windowStyle;
-- (void)beginWindowDragWithEvent:(NSEvent*)event
-    NS_DEPRECATED_MAC(10_10, 10_11, "Use performWindowDragWithEvent: instead.");
+
+// Available in later point releases of 10.10. On 10.11+, use the public
+// -performWindowDragWithEvent: instead.
+- (void)beginWindowDragWithEvent:(NSEvent*)event;
 @end
 
 // Weak lets Chrome launch even if a future macOS doesn't have NSThemeFrame.
@@ -83,7 +85,8 @@ WEAK_IMPORT_ATTRIBUTE
     ;  // Not needed on 10.12 and up.
   else if (@available(macOS 10.11, *))
     [self.window performWindowDragWithEvent:event];
-  else if (@available(macOS 10.10, *))
+  else if ([self.window
+               respondsToSelector:@selector(beginWindowDragWithEvent:)])
     [self.window beginWindowDragWithEvent:event];
   else
     NOTREACHED();
