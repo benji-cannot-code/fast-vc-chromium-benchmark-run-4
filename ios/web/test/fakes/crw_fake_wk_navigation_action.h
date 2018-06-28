96c950bd7f370bb3ca453e5bef5d8007fdcd5553
mrefaat <mrefaat@chromium.org>
2018-06-28 07:46:48 +0000

    Move logic out of CRWWebDelegate ShouldOpenURL in tab.mm
    Move the logic from shouldOpenURL in tab.mm to ShouldOpenURL in crw_web_controller.mm
    ShouldOpenURL method should be entirley removed from tab.mm and preload_controller.mm
    once crbug.com/546402 is addressed.
    
    Bug: 850760
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ia7f017942c2095e9ed12e58fe3a790aa53551d35
    Reviewed-on: https://chromium-review.googlesource.com/1112646
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Mohammad Refaat <mrefaat@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571044}

---
+++ ios/web/test/fakes/crw_fake_wk_navigation_action.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_WEB_TEST_FAKES_CRW_FAKE_WK_NAVIGATION_ACTION_H_
+#define IOS_WEB_TEST_FAKES_CRW_FAKE_WK_NAVIGATION_ACTION_H_
+
+#import <WebKit/WebKit.h>
+
+// Fake WKNavigationAction class which can be used for testing.
+@interface CRWFakeWKNavigationAction : WKNavigationAction
+// Redefined WKNavigationAction properties as readwrite.
+@property(nullable, nonatomic, copy) WKFrameInfo* sourceFrame;
+@property(nullable, nonatomic, copy) WKFrameInfo* targetFrame;
+@property(nonatomic) WKNavigationType navigationType;
+@property(nullable, nonatomic, copy) NSURLRequest* request;
+@end
+
+#endif  // IOS_WEB_TEST_FAKES_CRW_FAKE_WK_NAVIGATION_ACTION_H_
