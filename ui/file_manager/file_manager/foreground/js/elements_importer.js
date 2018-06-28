1fa8ff35d1f829b3a233a3c71594f6c587a67ce6
Scott Chen <scottchen@chromium.org>
2018-06-28 18:54:54 +0000

    WebUI[MD-refresh]: switch file-manager paper-inputs to cr-inputs
    Bug: 832177
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: I552d8ba9107b82aca3a24d24299181b788fc54d8
    Reviewed-on: https://chromium-review.googlesource.com/1108918
    Commit-Queue: Scott Chen <scottchen@chromium.org>
    Reviewed-by: Sasha Morrissey <sashab@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571214}

--- ui/file_manager/file_manager/foreground/js/elements_importer.js
+++ ui/file_manager/file_manager/foreground/js/elements_importer.js
@@ -3,9 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-window.Polymer = window.Polymer || {};
-window.Polymer.dom = 'shadow';
-
 /**
  * @type {Promise} A promise which is fulfilled when HTML imports for custom
  *   elements for file manager UI are loaded.
