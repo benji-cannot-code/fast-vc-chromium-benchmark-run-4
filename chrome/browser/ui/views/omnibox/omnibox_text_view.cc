22ccdfe59accfcdd8462168bd565e11a2558a0b5
Orin Jaworski <orinj@chromium.org>
2018-06-28 17:22:51 +0000

    [omnibox] Vertically center single line entity suggestions
    Omnibox entity suggestions with two lines of text will layout
    as before, but now if the second line description is absent,
    the primary content text line will be vertically centered.
    
    Bug: 856703
    Change-Id: Ie672ccece28be79d18bf6f7f07f9937af434178d
    Reviewed-on: https://chromium-review.googlesource.com/1115760
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571174}

--- chrome/browser/ui/views/omnibox/omnibox_text_view.cc
+++ chrome/browser/ui/views/omnibox/omnibox_text_view.cc
@@ -41,6 +41,8 @@ constexpr int kInherit = INT_MIN;
 static constexpr int kVerticalPadding = 4;
 static constexpr int kRefreshVerticalPadding = 3;
 
+static const base::string16 kEmptyString;
+
 struct TextStyle {
   OmniboxPart part;
 
@@ -183,6 +185,12 @@ void OmniboxTextView::OnPaint(gfx::Canvas* canvas) {
 void OmniboxTextView::Dim() {
   render_text_->SetColor(
       result_view_->GetColor(OmniboxPart::RESULTS_TEXT_DIMMED));
+}
+
+const base::string16& OmniboxTextView::text() const {
+  if (!render_text_)
+    return kEmptyString;
+  return render_text_->text();
 }
 
 void OmniboxTextView::SetText(const base::string16& text) {
