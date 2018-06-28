aa5bb19ad418288f74ea48e3730de035ef4c3c58
Daniel Rubery <drubery@chromium.org>
2018-06-28 16:43:26 +0000

    Add state of saved password hashes to chrome://safe-browsing
    Render: https://screenshot.googleplex.com/nvBOZqHXENq
    
    Bug: 849351
    Change-Id: I8004424ed05d0b424fd9918c0e36d49d954bcabc
    Reviewed-on: https://chromium-review.googlesource.com/1117443
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Reviewed-by: Jialiu Lin <jialiul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571161}

--- components/safe_browsing/web_ui/safe_browsing_ui.cc
+++ components/safe_browsing/web_ui/safe_browsing_ui.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "components/grit/components_resources.h"
 #include "components/grit/components_scaled_resources.h"
+#include "components/password_manager/core/browser/hash_password_manager.h"
 #include "components/safe_browsing/common/safe_browsing_prefs.h"
 #include "components/safe_browsing/features.h"
 #include "components/safe_browsing/web_ui/constants.h"
@@ -519,6 +520,23 @@ void SafeBrowsingUIHandler::GetPrefs(const base::ListValue* args) {
                                 user_prefs::UserPrefs::Get(browser_context_)));
 }
 
+void SafeBrowsingUIHandler::GetSavedPasswords(const base::ListValue* args) {
+  password_manager::HashPasswordManager hash_manager(
+      user_prefs::UserPrefs::Get(browser_context_));
+
+  base::ListValue saved_passwords;
+  for (const password_manager::PasswordHashData& hash_data :
+       hash_manager.RetrieveAllPasswordHashes()) {
+    saved_passwords.AppendString(hash_data.username);
+    saved_passwords.AppendBoolean(hash_data.is_gaia_password);
+  }
+
+  AllowJavascript();
+  std::string callback_id;
+  args->GetString(0, &callback_id);
+  ResolveJavascriptCallback(base::Value(callback_id), saved_passwords);
+}
+
 void SafeBrowsingUIHandler::GetDatabaseManagerInfo(
     const base::ListValue* args) {
   base::ListValue database_manager_info;
@@ -631,6 +649,10 @@ void SafeBrowsingUIHandler::RegisterMessages() {
   web_ui()->RegisterMessageCallback(
       "getPrefs", base::BindRepeating(&SafeBrowsingUIHandler::GetPrefs,
                                       base::Unretained(this)));
+  web_ui()->RegisterMessageCallback(
+      "getSavedPasswords",
+      base::BindRepeating(&SafeBrowsingUIHandler::GetSavedPasswords,
+                          base::Unretained(this)));
   web_ui()->RegisterMessageCallback(
       "getDatabaseManagerInfo",
       base::BindRepeating(&SafeBrowsingUIHandler::GetDatabaseManagerInfo,
