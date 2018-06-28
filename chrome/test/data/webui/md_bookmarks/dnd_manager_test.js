93ae976d4cbf2cd63d2264157db7ea7f7f90196a
rbpotter <rbpotter@chromium.org>
2018-06-28 23:14:23 +0000

    WebUI Polymer2 Migration: fix 2 MD Bookmarks tests
    Fixing MaterialBookmarksDNDManagerTest.All
    and MaterialBookmarksStoreTest.All
    
    Bug: 738611
    Change-Id: I651e9ad57dbd0bd6bf3b453ee2e5dc5a656773ec
    Reviewed-on: https://chromium-review.googlesource.com/1117891
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571317}

--- chrome/test/data/webui/md_bookmarks/dnd_manager_test.js
+++ chrome/test/data/webui/md_bookmarks/dnd_manager_test.js
@@ -34,6 +34,7 @@ suite('drag and drop', function() {
     const props = {
       bubbles: true,
       cancelable: true,
+      composed: true,
       clientX: xy.x,
       clientY: xy.y,
       // Make this a primary input.
