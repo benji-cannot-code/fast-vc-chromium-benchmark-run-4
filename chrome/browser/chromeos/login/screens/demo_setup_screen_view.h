fb751abcca1de39ae13619910c9022c577efb8f3
Aga Wronska <agawronska@chromium.org>
2018-06-28 15:38:18 +0000

    Add skeleton of Demo Mode setup UI.
    Mocks:
    https://gallery.googleplex.com/projects/MCHbtQVoQ2HCZeqwFvuvKaZQ/files/MCGxzIhEyEZFkDOfw1G1Tu4Z
    
    Bug: 827376
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: I1206687fff441ec6be5a3bb9e6dddba7469f292b
    Reviewed-on: https://chromium-review.googlesource.com/1100237
    Commit-Queue: Aga Wronska <agawronska@chromium.org>
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571137}

--- chrome/browser/chromeos/login/screens/demo_setup_screen_view.h
+++ chrome/browser/chromeos/login/screens/demo_setup_screen_view.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_CHROMEOS_LOGIN_SCREENS_DEMO_SETUP_SCREEN_VIEW_H_
 #define CHROME_BROWSER_CHROMEOS_LOGIN_SCREENS_DEMO_SETUP_SCREEN_VIEW_H_
 
+#include <string>
+
 #include "chrome/browser/chromeos/login/oobe_screen.h"
 
 namespace chromeos {
@@ -27,6 +29,9 @@ class DemoSetupScreenView {
 
   // Sets view and screen.
   virtual void Bind(DemoSetupScreen* screen) = 0;
+
+  // Handles setup result.
+  virtual void OnSetupFinished(bool is_success, const std::string& message) = 0;
 };
 
 }  // namespace chromeos
