76a9fb036893c8bc4013d43fd3c1b7861ccab423
Dmitry Gozman <dgozman@chromium.org>
2018-06-28 00:21:26 +0000

    Extract FrameLoader::CancelProvisionalLoaderForNewNavigation
    This is a common part to be shared between StartNavigation and
    CommitNavigation paths.
    
    Bug: 789577
    Change-Id: I3e90d158b411b9134bcd8d1a333c81875cff8753
    Reviewed-on: https://chromium-review.googlesource.com/1107240
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570969}

--- third_party/blink/renderer/core/loader/frame_loader.h
+++ third_party/blink/renderer/core/loader/frame_loader.h
@@ -250,6 +250,9 @@ class CORE_EXPORT FrameLoader final {
                                        const KURL&);
   void ProcessFragment(const KURL&, WebFrameLoadType, LoadStartType);
 
+  // Returns whether we should continue with new navigation.
+  bool CancelProvisionalLoaderForNewNavigation(NavigationPolicy);
+
   void StartLoad(FrameLoadRequest&,
                  WebFrameLoadType,
                  NavigationPolicy,
