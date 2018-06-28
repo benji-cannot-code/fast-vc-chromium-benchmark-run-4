7347a5913498c844c1517e3ec2585b3a5943e1a6
Yi Gu <yigu@chromium.org>
2018-06-28 13:29:51 +0000

    Revert "Add a test helper method in services/network to get the body from a ResourceRequest."
    This reverts commit 30184c71c6875b2457958cb8dac401d3d761147c.
    
    Reason for revert: <suspecting that this patch caused unit test SSLErrorHandlerDateInvalidTest.TimeQueryHangs flaky, crbug.com/857446>
    
    Original change's description:
    > Add a test helper method in services/network to get the body from a ResourceRequest.
    > 
    > Also remove the custom URLLoaderFactory in content_hash_fetcher_unittest. This isn't needed anymore as TestURLLoaderFactory now supports Clone.
    > 
    > Cq-Include-Trybots: luci.chromium.try:linux_mojo
    > Change-Id: Id70ec605886a6b0f2cb9bf7392fac4eff85fc1b3
    > Reviewed-on: https://chromium-review.googlesource.com/1115346
    > Reviewed-by: Doug Turner <dougt@chromium.org>
    > Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#570813}
    
    TBR=jam@chromium.org,dougt@chromium.org
    
    Change-Id: I2bd852fbf746203e325e6a859ad2b5cc2919b7e8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Reviewed-on: https://chromium-review.googlesource.com/1117521
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571100}

--- chrome/browser/ssl/certificate_error_reporter_unittest.cc
+++ chrome/browser/ssl/certificate_error_reporter_unittest.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/resource_request.h"
 #include "services/network/public/cpp/weak_wrapper_shared_url_loader_factory.h"
 #include "services/network/test/test_url_loader_factory.h"
-#include "services/network/test/test_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/boringssl/src/include/openssl/curve25519.h"
 
@@ -72,7 +71,11 @@ TEST_F(ErrorReporterTest, ExtendedReportingSendReport) {
         latest_report_uri = request.url;
         request.headers.GetHeader(net::HttpRequestHeaders::kContentType,
                                   &latest_content_type);
-        latest_report = network::GetUploadData(request);
+        auto body = request.request_body;
+        CHECK_EQ(1u, body->elements()->size());
+        auto& element = body->elements()->at(0);
+        CHECK_EQ(network::DataElement::TYPE_BYTES, element.type());
+        latest_report = std::string(element.bytes(), element.length());
       }));
 
   // Data should not be encrypted when sent to an HTTPS URL.
--- components/sync/driver/sync_stopped_reporter_unittest.cc
+++ components/sync/driver/sync_stopped_reporter_unittest.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "services/network/public/cpp/weak_wrapper_shared_url_loader_factory.h"
 #include "services/network/test/test_url_loader_factory.h"
-#include "services/network/test/test_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace syncer {
@@ -64,6 +63,17 @@ class SyncStoppedReporterTest : public testing::Test {
 
   const SyncStoppedReporter::Result& request_result() const {
     return request_result_;
+  }
+
+  std::string GetBodyFromRequest(const network::ResourceRequest& request) {
+    auto body = request.request_body;
+    if (!body)
+      return std::string();
+
+    CHECK_EQ(1u, body->elements()->size());
+    auto& element = body->elements()->at(0);
+    CHECK_EQ(network::DataElement::TYPE_BYTES, element.type());
+    return std::string(element.bytes(), element.length());
   }
 
   network::TestURLLoaderFactory* url_loader_factory() {
@@ -123,7 +133,7 @@ TEST_F(SyncStoppedReporterTest, FetcherConfiguration) {
       base::BindLambdaForTesting([&](const network::ResourceRequest& request) {
         intercepted_url = request.url;
         intercepted_headers = request.headers;
-        intercepted_body = network::GetUploadData(request);
+        intercepted_body = GetBodyFromRequest(request);
       }));
   SyncStoppedReporter ssr(test_url(), user_agent(), shared_url_loader_factory(),
                           callback());
