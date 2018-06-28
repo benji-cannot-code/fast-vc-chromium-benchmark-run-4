858d885b607cfefb445144bdc3b133ad6c1ae00c
Dave Schuyler <dschuyler@chromium.org>
2018-06-28 23:54:51 +0000

    [Omnibox] Pretend that Calculator results are a rich response
    This CL fabricates a rich suggestion/new answer layout for calculator
    responses by creating a mimicking a suggestiondetails entry.
    
    Change-Id: I5fdc83b2f31d703e0ed8421e9be65233c9be2cb1
    Reviewed-on: https://chromium-review.googlesource.com/1111460
    Commit-Queue: Dave Schuyler <dschuyler@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Reviewed-by: Mitsuru Oshima (OOO 6/27) <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571332}

--- components/omnibox/browser/autocomplete_controller.cc
+++ components/omnibox/browser/autocomplete_controller.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/search_engines/template_url.h"
 #include "components/search_engines/template_url_service.h"
 #include "components/strings/grit/components_strings.h"
+#include "ui/base/device_form_factor.h"
 #include "ui/base/l10n/l10n_util.h"
 
 #if !defined(OS_IOS)
@@ -183,8 +184,13 @@ bool IsTrivialAutocompletion(const AutocompleteMatch& match) {
 
 // Whether this autocomplete match type supports custom descriptions.
 bool AutocompleteMatchHasCustomDescription(const AutocompleteMatch& match) {
+  if (ui::GetDeviceFormFactor() == ui::DEVICE_FORM_FACTOR_DESKTOP &&
+      OmniboxFieldTrial::IsNewAnswerLayoutEnabled() &&
+      match.type == AutocompleteMatchType::CALCULATOR) {
+    return true;
+  }
   return match.type == AutocompleteMatchType::SEARCH_SUGGEST_ENTITY ||
-      match.type == AutocompleteMatchType::SEARCH_SUGGEST_PROFILE;
+         match.type == AutocompleteMatchType::SEARCH_SUGGEST_PROFILE;
 }
 
 }  // namespace
--- components/omnibox/browser/search_suggestion_parser.cc
+++ components/omnibox/browser/search_suggestion_parser.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "components/omnibox/browser/autocomplete_i18n.h"
 #include "components/omnibox/browser/autocomplete_input.h"
+#include "components/omnibox/browser/omnibox_field_trial.h"
 #include "components/omnibox/browser/url_prefix.h"
 #include "components/url_formatter/url_fixer.h"
 #include "components/url_formatter/url_formatter.h"
@@ -31,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/resource_response.h"
 #include "services/network/public/cpp/simple_url_loader.h"
 #include "ui/base/device_form_factor.h"
+#include "ui/base/material_design/material_design_controller.h"
 #include "url/url_constants.h"
 
 namespace {
@@ -532,21 +534,27 @@ bool SearchSuggestionParser::ParseSuggestResults(
             input.text()));
       }
     } else {
+      base::string16 annotation;
       base::string16 match_contents = suggestion;
-      if ((match_type == AutocompleteMatchType::CALCULATOR) &&
-          !suggestion.compare(0, 2, base::UTF8ToUTF16("= "))) {
-        // Calculator results include a "= " prefix but we don't want to include
-        // this in the search terms.
-        suggestion.erase(0, 2);
-        // Additionally, on larger (non-phone) form factors, we don't want to
-        // display it in the suggestion contents either, because those devices
-        // display a suggestion type icon that looks like a '='.
-        if (ui::GetDeviceFormFactor() != ui::DEVICE_FORM_FACTOR_PHONE)
-          match_contents.erase(0, 2);
+      if (match_type == AutocompleteMatchType::CALCULATOR) {
+        if (!suggestion.compare(0, 2, base::UTF8ToUTF16("= "))) {
+          // Calculator results include a "= " prefix but we don't want to
+          // include this in the search terms.
+          suggestion.erase(0, 2);
+          // Additionally, on larger (non-phone) form factors, we don't want to
+          // display it in the suggestion contents either, because those devices
+          // display a suggestion type icon that looks like a '='.
+          if (ui::GetDeviceFormFactor() != ui::DEVICE_FORM_FACTOR_PHONE)
+            match_contents.erase(0, 2);
+        }
+        if (ui::GetDeviceFormFactor() == ui::DEVICE_FORM_FACTOR_DESKTOP &&
+            OmniboxFieldTrial::IsNewAnswerLayoutEnabled()) {
+          annotation = match_contents;
+          match_contents = query;
+        }
       }
 
       base::string16 match_contents_prefix;
-      base::string16 annotation;
       base::string16 answer_contents;
       base::string16 answer_type_str;
       std::unique_ptr<SuggestionAnswer> answer;
