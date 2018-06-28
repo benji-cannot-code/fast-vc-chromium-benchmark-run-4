ab2ed1d37cef8c59d215ba9edb20bd5aa145df5f
Becca Hughes <beccahughes@chromium.org>
2018-06-28 17:51:40 +0000

    [Display Cutout] Make media controls use the safe area
    Inset media contols by the safe area to avoid overlapping
    the display cutout.
    
    BUG=857252
    
    Change-Id: I38c25a07f880cd93caf6c89662e541f52096e638
    Reviewed-on: https://chromium-review.googlesource.com/1117835
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571195}

--- third_party/blink/renderer/modules/media_controls/media_controls_impl_test.cc
+++ third_party/blink/renderer/modules/media_controls/media_controls_impl_test.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/web_screen_info.h"
 #include "third_party/blink/public/platform/web_size.h"
 #include "third_party/blink/renderer/core/css/css_property_value_set.h"
+#include "third_party/blink/renderer/core/css/document_style_environment_variables.h"
 #include "third_party/blink/renderer/core/css/style_engine.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/element_traversal.h"
@@ -1333,6 +1334,44 @@ TEST_F(MediaControlsImplTest, CastOverlayShowsOnSomeEvents) {
 
     SimulateHideMediaControlsTimerFired();
     EXPECT_FALSE(IsElementVisible(*cast_overlay_button));
+  }
+}
+
+class ModernMediaControlsImplTest : public MediaControlsImplTest {
+ public:
+  void SetUp() override {
+    RuntimeEnabledFeatures::SetModernMediaControlsEnabled(true);
+    MediaControlsImplTest::SetUp();
+  }
+};
+
+TEST_F(ModernMediaControlsImplTest, ControlsShouldUseSafeAreaInsets) {
+  {
+    const ComputedStyle* style = MediaControls().EnsureComputedStyle();
+    EXPECT_EQ(0.0, style->MarginTop().Pixels());
+    EXPECT_EQ(0.0, style->MarginLeft().Pixels());
+    EXPECT_EQ(0.0, style->MarginBottom().Pixels());
+    EXPECT_EQ(0.0, style->MarginRight().Pixels());
+  }
+
+  GetStyleEngine().EnsureEnvironmentVariables().SetVariable(
+      "safe-area-inset-top", "1px");
+  GetStyleEngine().EnsureEnvironmentVariables().SetVariable(
+      "safe-area-inset-left", "2px");
+  GetStyleEngine().EnsureEnvironmentVariables().SetVariable(
+      "safe-area-inset-bottom", "3px");
+  GetStyleEngine().EnsureEnvironmentVariables().SetVariable(
+      "safe-area-inset-right", "4px");
+
+  EXPECT_TRUE(GetDocument().NeedsLayoutTreeUpdate());
+  GetDocument().View()->UpdateAllLifecyclePhases();
+
+  {
+    const ComputedStyle* style = MediaControls().EnsureComputedStyle();
+    EXPECT_EQ(1.0, style->MarginTop().Pixels());
+    EXPECT_EQ(2.0, style->MarginLeft().Pixels());
+    EXPECT_EQ(3.0, style->MarginBottom().Pixels());
+    EXPECT_EQ(4.0, style->MarginRight().Pixels());
   }
 }
 
