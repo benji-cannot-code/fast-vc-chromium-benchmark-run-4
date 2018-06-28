5043f1636ed3ba389f647a2b4dc51c7fac9d0c0d
Yi Gu <yigu@chromium.org>
2018-06-28 17:49:53 +0000

    Revert "Layout table roles should provide name from contents when used recursively."
    This reverts commit 4ffc8d34a79469cba7e63d7cf176ec6a9caf136b.
    
    Reason for revert: Consistently failed the bot https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Cast%20Audio%20Linux
    
    Original change's description:
    > Layout table roles should provide name from contents when used recursively.
    > 
    > Recent table refactoring means we expose more layout tables. This is
    > generally a good thing.
    > 
    > It broke some accessibility in Hangouts, where there was a layout table
    > inside of an ARIA button and the accessible text of the button came from
    > inside the layout table. Previously the layout table was just exposed as
    > generic elements, and layout table roles should behave the same way with
    > regards to how text is exposed to ancestors.
    > 
    > Bug: 856277
    > Change-Id: I9b30931b306fda09a5408df4dc3b3dd1c54e619e
    > Reviewed-on: https://chromium-review.googlesource.com/1117628
    > Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    > Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#571139}
    
    TBR=dmazzoni@chromium.org,aleventhal@chromium.org
    
    Change-Id: I496fb5987f7155064193ae475d45a00c7dbbb3b4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 856277
    Reviewed-on: https://chromium-review.googlesource.com/1118945
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571193}

--- third_party/blink/renderer/modules/accessibility/ax_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_object.cc
@@ -3166,6 +3166,9 @@ bool AXObject::NameFromContents(bool recursive) const {
     case kIframeRole:
     case kImageRole:
     case kInputTimeRole:
+    case kLayoutTableRole:
+    case kLayoutTableColumnRole:
+    case kLayoutTableRowRole:
     case kListBoxRole:
     case kLogRole:
     case kMainRole:
@@ -3225,9 +3228,6 @@ bool AXObject::NameFromContents(bool recursive) const {
     case kImageMapRole:
     case kInlineTextBoxRole:
     case kLabelRole:
-    case kLayoutTableRole:
-    case kLayoutTableColumnRole:
-    case kLayoutTableRowRole:
     case kLegendRole:
     case kListRole:
     case kListItemRole:
