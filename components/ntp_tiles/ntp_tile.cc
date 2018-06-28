032ee6a98c7119e333b16d85fceba23c4984aedf
kristipark <kristipark@chromium.org>
2018-06-28 21:53:09 +0000

    Revert "Return fallback colors for MV tiles with missing or small icons"
    This reverts commit 7c7778765808728c7d25b95fc7d6e1fc2a28c36d.
    
    Reason for revert: Due to http://crbug/853780, this code is no longer used.Feature flag changes are not reverted since they will be needed later.
    
    Original change's description:
    > Return fallback colors for MV tiles with missing or small icons
    >
    > Fallback style (background and text colors) will be returned for tiles with missing or small icons (i.e. where only a 1x icon is available when 2x is needed). This will be used to display a fallback monogram in the frontend.
    >
    > Bug: 837798
    > Change-Id: I9e80d763085ea5b095787f83e5d1e38b801c83eb
    > Reviewed-on: https://chromium-review.googlesource.com/1096670
    > Commit-Queue: Kristi Park <kristipark@chromium.org>
    > Reviewed-by: Marc Treib <treib@chromium.org>
    > Reviewed-by: Nico Weber <thakis@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#567402}
    
    TBR=thakis@chromium.org,treib@chromium.org,kristipark@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 837798
    Change-Id: I1101c99e3dfde87f77d0111ac52c2dd2f90b37d4
    Reviewed-on: https://chromium-review.googlesource.com/1117500
    Commit-Queue: Kristi Park <kristipark@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571281}

--- components/ntp_tiles/ntp_tile.cc
+++ components/ntp_tiles/ntp_tile.cc
@@ -8,11 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ntp_tiles {
 
 NTPTile::NTPTile()
-    : title_source(TileTitleSource::UNKNOWN),
-      source(TileSource::TOP_SITES),
-      has_fallback_style(false),
-      fallback_background_color(SK_ColorBLACK),
-      fallback_text_color(SK_ColorBLACK) {}
+    : title_source(TileTitleSource::UNKNOWN), source(TileSource::TOP_SITES) {}
 
 NTPTile::NTPTile(const NTPTile&) = default;
 
@@ -23,10 +19,7 @@ bool operator==(const NTPTile& a, const NTPTile& b) {
          (a.title_source == b.title_source) &&
          (a.whitelist_icon_path == b.whitelist_icon_path) &&
          (a.thumbnail_url == b.thumbnail_url) &&
-         (a.favicon_url == b.favicon_url) &&
-         (a.has_fallback_style == b.has_fallback_style) &&
-         (a.fallback_background_color == b.fallback_background_color) &&
-         (a.fallback_text_color == b.fallback_text_color);
+         (a.favicon_url == b.favicon_url);
 }
 
 bool operator!=(const NTPTile& a, const NTPTile& b) {
