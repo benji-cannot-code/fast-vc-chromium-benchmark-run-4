b40fb9c78f7347f0d11b598c05dcf3c5e27910e8
Max Morin <maxmorin@chromium.org>
2018-06-28 11:34:48 +0000

    Remove overeager WebRTCAudioSink thread check.
    The callback thread thread check may trigger if the
    SilentSinkSuspender sets in.
    
    Drive-by update the other thread check of this class to use a macro
    and add missing includes.
    
    Bug: 854774
    Change-Id: I79f7c56886acd7f1ad56c466eb0c2825d1d8ec16
    Reviewed-on: https://chromium-review.googlesource.com/1114973
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Max Morin <maxmorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571081}

--- content/renderer/media/webrtc/webrtc_audio_sink.h
+++ content/renderer/media/webrtc/webrtc_audio_sink.h
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include <memory>
+#include <string>
+#include <utility>
 #include <vector>
 
 #include "base/macros.h"
@@ -167,11 +169,7 @@ class CONTENT_EXPORT WebRtcAudioSink : public MediaStreamAudioSink {
 
   // In debug builds, check that WebRtcAudioSink's public methods are all being
   // called on the main render thread.
-  base::ThreadChecker thread_checker_;
-
-  // Used to DCHECK that OnSetFormat() and OnData() are called on the same
-  // thread.
-  base::ThreadChecker audio_thread_checker_;
+  THREAD_CHECKER(thread_checker_);
 
   DISALLOW_COPY_AND_ASSIGN(WebRtcAudioSink);
 };
