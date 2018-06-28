360dec13b0aa72a1bf9b9f734cfc056f29c77053
Xiaohan Wang <xhwang@chromium.org>
2018-06-28 21:37:34 +0000

    media: Support hardware secure encryption schemes
    We could have different encryption scheme support when using hardware
    secure codecs. For example, when not using hardware secure codecs, we
    support both "cenc" and "cbcs", but when using hardware secure codecs,
    we may only support "cenc".
    
    This CL updates KeySystemProperties::IsEncryptionSchemeSupported() and
    KeySystemConfigSelector so that we can differentiate encryption schemes
    supported in normal mode and in hardware secure mode.
    
    WidevineKeySystemProperties is also updated to take
    "hw_secure_encryption_schemes" returned by KeySystemSupport.
    
    TBR=slan@chromium.org
    
    Bug: 853261
    Test: Added new test cases
    Change-Id: I3ca9eba3e01adb5395a700969d2f985765d2544d
    Reviewed-on: https://chromium-review.googlesource.com/1116426
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571272}

--- components/cdm/renderer/widevine_key_system_properties.h
+++ components/cdm/renderer/widevine_key_system_properties.h
@@ -30,9 +30,10 @@ class WidevineKeySystemProperties : public media::KeySystemProperties {
   };
 
   WidevineKeySystemProperties(
-      base::flat_set<media::EncryptionMode> supported_encryption_schemes,
-      media::SupportedCodecs supported_codecs,
-      media::SupportedCodecs supported_hw_secure_codecs,
+      media::SupportedCodecs codecs,
+      base::flat_set<media::EncryptionMode> encryption_schemes,
+      media::SupportedCodecs hw_secure_codecs,
+      base::flat_set<media::EncryptionMode> hw_secure_encryption_schemes,
       Robustness max_audio_robustness,
       Robustness max_video_robustness,
       media::EmeSessionTypeSupport persistent_license_support,
@@ -44,12 +45,10 @@ class WidevineKeySystemProperties : public media::KeySystemProperties {
   std::string GetKeySystemName() const override;
   bool IsSupportedInitDataType(
       media::EmeInitDataType init_data_type) const override;
-  bool IsEncryptionSchemeSupported(
+  media::EmeConfigRule GetEncryptionSchemeConfigRule(
       media::EncryptionMode encryption_scheme) const override;
-
   media::SupportedCodecs GetSupportedCodecs() const override;
   media::SupportedCodecs GetSupportedHwSecureCodecs() const override;
-
   media::EmeConfigRule GetRobustnessConfigRule(
       media::EmeMediaType media_type,
       const std::string& requested_robustness) const override;
@@ -61,9 +60,10 @@ class WidevineKeySystemProperties : public media::KeySystemProperties {
   media::EmeFeatureSupport GetDistinctiveIdentifierSupport() const override;
 
  private:
-  const base::flat_set<media::EncryptionMode> supported_encryption_schemes_;
-  const media::SupportedCodecs supported_codecs_;
-  const media::SupportedCodecs supported_hw_secure_codecs_;
+  const media::SupportedCodecs codecs_;
+  const base::flat_set<media::EncryptionMode> encryption_schemes_;
+  const media::SupportedCodecs hw_secure_codecs_;
+  const base::flat_set<media::EncryptionMode> hw_secure_encryption_schemes_;
   const Robustness max_audio_robustness_;
   const Robustness max_video_robustness_;
   const media::EmeSessionTypeSupport persistent_license_support_;
