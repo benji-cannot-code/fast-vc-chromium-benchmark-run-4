e3f1f84aa1831dc13817a4e143e61de0dc782a73
Tiger Oakes <tigero@google.com>
2018-06-28 17:50:03 +0000

    Added diff mode to supersize ui
    Created new flag, --diff-with, which will diff two .size files and display their
    relative sizes instead of the absolute sizes in a single .size file.
    
    Preview at:
    https://notwoods.github.io/chrome-supersize-reports/monochrome-diff-2018-06-27/
    
    Reports can be compiled by running:
    ```
    tools/binary_size/supersize html_report after.size --diff-with before.size --tree-view-ui --report-dir diff-report
    xdg-open diff-report/index.html
    ```
    
    Bug: 847599
    Change-Id: I860ce6cd70c7261d3053d6a259f0208d74f0b35e
    Reviewed-on: https://chromium-review.googlesource.com/1117758
    Commit-Queue: Tiger Oakes <tigero@google.com>
    Reviewed-by: agrieve <agrieve@chromium.org>
    Reviewed-by: Eric Stevenson <estevenson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571194}

--- tools/binary_size/libsupersize/template_tree_view/ui.js
+++ tools/binary_size/libsupersize/template_tree_view/ui.js
@@ -87,6 +87,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     dom.replace(sizeElement, dom.createFragment([textNode, suffixElement]));
     sizeElement.title =
       bytes.toLocaleString(undefined, {useGrouping: true}) + ' bytes';
+
+    if (bytes < 0) {
+      sizeElement.classList.add('negative');
+    } else {
+      sizeElement.classList.remove('negative');
+    }
   }
 
   /**
