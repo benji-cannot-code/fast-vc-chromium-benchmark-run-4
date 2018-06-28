df6d6d5456e291429a0c0f60bcf3e3ef2d2cc2c8
Carlos Knippschild <carlosk@chromium.org>
2018-06-28 21:57:24 +0000

    Revert "Implement content of progress and error dialogs according to mocks."
    This reverts commit 1f7e073e5b0bf2f6846f3c35a87ac813631983a5.
    
    Reason for revert: Revert 1 of 2 - FindIt flagged https://crrev.com/c/1100237 with 100% confidence of breaking "Linux ChromiumOS MSan Tests" bot (DemoSetupTest.ProceedThroughSetupFlowSetupError); revering this CL is needed to be able to revert that one.
    
    Original change's description:
    > Implement content of progress and error dialogs according to mocks.
    > 
    > Bug: 827376
    > Cq-Include-Trybots: luci.chromium.try:closure_compilation
    > Change-Id: I16781ef2ad241f28da0eef3126d9b6a66840f828
    > Reviewed-on: https://chromium-review.googlesource.com/1105568
    > Commit-Queue: Aga Wronska <agawronska@chromium.org>
    > Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    > Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    > Reviewed-by: Jun Mukai <mukai@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#571202}
    
    TBR=mukai@chromium.org,alemate@chromium.org,michaelpg@chromium.org,agawronska@chromium.org
    
    Change-Id: I86eba9fb27bc78941b9caab8c69bf983f17b19f0
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 827376, 858817
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Reviewed-on: https://chromium-review.googlesource.com/1119267
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Commit-Queue: Carlos Knippschild <carlosk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571283}

--- chrome/browser/resources/chromeos/login/demo_setup.js
+++ chrome/browser/resources/chromeos/login/demo_setup.js
@@ -11,8 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 Polymer({
   is: 'demo-setup-md',
 
-  behaviors: [I18nBehavior, OobeDialogHostBehavior],
-
   properties: {
     /**
      * Whether offline demo setup was selected. Available setup types: online
