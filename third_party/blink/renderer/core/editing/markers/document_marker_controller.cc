f42e4ce1d25285d77eed2d8508de0409d306ffb1
Konstantin Ganenko <ganenkokb@yandex-team.ru>
2018-06-28 11:03:14 +0000

    Fixed cleaning of spelling markers if all markers were removed
     from the text.
    
    Previously RemoveSpellingMarkersUnderWords could clean markers totally.
    After this action RepaintMarkers did not triggeren on text nodes that
     had empty markers list, or  there were no possible markers of
     spellcheck type at all.
    
    Now we invalidate each node we remove marker from.
    
    R=yosin@chromium.org
    
    Change-Id: I57b86134198b2cb40ab04c52859db4164d6e8555
    Reviewed-on: https://chromium-review.googlesource.com/1114961
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571072}

--- third_party/blink/renderer/core/editing/markers/document_marker_controller.cc
+++ third_party/blink/renderer/core/editing/markers/document_marker_controller.cc
@@ -689,8 +689,10 @@ void DocumentMarkerController::RemoveSpellingMarkersUnderWords(
       DocumentMarkerList* const list = ListForType(markers, type);
       if (!list)
         continue;
-      ToSpellCheckMarkerListImpl(list)->RemoveMarkersUnderWords(
-          ToText(node).data(), words);
+      if (ToSpellCheckMarkerListImpl(list)->RemoveMarkersUnderWords(
+              ToText(node).data(), words)) {
+        InvalidatePaintForNode(node);
+      }
     }
   }
 }
