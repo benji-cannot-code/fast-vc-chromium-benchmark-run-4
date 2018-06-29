3b400ede34465389097732196fe5d36a5f22e496
Tiger Oakes <tigero@google.com>
2018-06-29 19:16:40 +0000

    Added word break hints & removed source_paths
    Zero-width spaces  (https://en.wikipedia.org/wiki/Zero-width_space) have been
    added to improve usage of supersize on small screens. They hint at where line
    breaks should be placed when the screen width is too small for the entire text
    to be shown on one line.
    
    These characters are added after ":" (for C symbol names), "#" (for Java
    symbol names), and "/" (for paths in symbol names or compressed nodes).
    
    Before: https://screenshot.googleplex.com/3cmsuPO0A97.png
    After: https://screenshot.googleplex.com/4Sfsj1RrGRW.png
    
    Additionally, as source_paths of files are always unique, I removed the
    source_paths IndexedSet and store the paths inside the symbol object in the
    JSON data.
    
    Finally, the byte representation in JSON has been truncated 2 digits after
    the decimal point, which is the max depth used by the UI. If a float is a
    whole number, it is written as an int instead.
    (214.0000003 -> 214)
    (2435.454353 -> 2435.45)
    (12.0 -> 12)
    
    Bug: 847599
    Change-Id: Ifc388f19fe0c5402c0093af0d6c1d30bea20bb5e
    Reviewed-on: https://chromium-review.googlesource.com/1120736
    Commit-Queue: Tiger Oakes <tigero@google.com>
    Reviewed-by: Eric Stevenson <estevenson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571583}

--- tools/binary_size/libsupersize/template_tree_view/tree-worker.js
+++ tools/binary_size/libsupersize/template_tree_view/tree-worker.js
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /**
  * @typedef {object} FileEntry JSON object representing a single file and its
  * symbols.
- * @prop {number} p - source_path_index
+ * @prop {number} p - source_path
  * @prop {number} c - component_index
  * @prop {Array<{n:string,b:number,t:string}>} s - Symbols belonging to this
  * node. Array of objects.
@@ -52,7 +52,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @enum {string}
  */
 const _KEYS = {
-  SOURCE_PATH_INDEX: 'p',
+  SOURCE_PATH: 'p',
   COMPONENT_INDEX: 'c',
   FILE_SYMBOLS: 's',
   SYMBOL_NAME: 'n',
@@ -100,7 +100,7 @@ function combineSingleChildNodes(node, sep) {
     // If there is only 1 child and its the same type, merge it in.
     if (node.children.length === 1 && node.type === child.type) {
       // size & type should be the same, so don't bother copying them.
-      node.shortName += sep + child.shortName;
+      node.shortName += sep + '\u200b' + child.shortName;
       node.idPath = child.idPath;
       node.children = child.children;
       // Search children of this node.
@@ -293,7 +293,7 @@ self.onmessage = event => {
     symbols: tree.file_nodes,
     sep,
     methodCountMode,
-    getPath: s => tree.source_paths[s[_KEYS.SOURCE_PATH_INDEX]],
+    getPath: s => s[_KEYS.SOURCE_PATH],
     filterTest: s => typeFilter.has(s.type),
   });
 
