3e6056ad4cabec4759176ab7fda9a2c8256cefe5
Tetsui Ohkubo <tetsui@chromium.org>
2018-06-29 05:46:44 +0000

    Add IME mode icon to UnifiedSystemTray.
    This CL restores IME mode icon to UnifiedSystemTray. The icon was
    removed as a part of UnifiedSystemTray & Shelf spec, but it turned out
    the icon is important especially for CJK users with laptop mode.
    We still hide the icon for tablet mode.
    
    TEST=manual
    BUG=854948
    
    Change-Id: I55bdd88627f91c9e285242b725c4a55451d0c60a
    Reviewed-on: https://chromium-review.googlesource.com/1116412
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571395}

--- ash/system/unified/unified_system_tray.cc
+++ ash/system/unified/unified_system_tray.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/status_area_widget.h"
 #include "ash/system/tray/system_tray.h"
 #include "ash/system/tray/tray_container.h"
+#include "ash/system/unified/ime_mode_view.h"
 #include "ash/system/unified/notification_counter_view.h"
 #include "ash/system/unified/unified_slider_bubble_controller.h"
 #include "ash/system/unified/unified_system_tray_bubble.h"
@@ -142,11 +143,13 @@ UnifiedSystemTray::UnifiedSystemTray(Shelf* shelf)
       model_(std::make_unique<UnifiedSystemTrayModel>()),
       slider_bubble_controller_(
           std::make_unique<UnifiedSliderBubbleController>(this)),
+      ime_mode_view_(new ImeModeView()),
       notification_counter_item_(new NotificationCounterView()),
       quiet_mode_view_(new QuietModeView()),
       time_view_(
           new tray::TimeView(tray::TimeView::ClockLayout::HORIZONTAL_CLOCK,
                              Shell::Get()->system_tray_model()->clock())) {
+  tray_container()->AddChildView(ime_mode_view_);
   tray_container()->AddChildView(notification_counter_item_);
   tray_container()->AddChildView(quiet_mode_view_);
 
