b6f12540f6d48f1da4417f57ceca069751276ba0
Findit <findit-for-me@appspot.gserviceaccount.com>
2018-06-29 05:28:53 +0000

    Revert "assistant: filter out incognito windows in assistant screenshot."
    This reverts commit 9f6cd533db14bff679bd8cd6714b732455e44c16.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 571385 as the
    culprit for failures in the build cycles as shown on:
    https://findit-for-me.appspot.com/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtLzlmNmNkNTMzZGIxNGJmZjY3OWJkOGNkNjcxNGI3MzI0NTVlNDRjMTYM
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium.chromiumos/linux-chromeos-dbg/6550
    
    Sample Failed Step: compile
    
    Original change's description:
    > assistant: filter out incognito windows in assistant screenshot.
    > 
    > Per privacy requirement, incognito windows should not be included
    > in screenshot sent to assistant server.
    > 
    >   ash_unittests --gtest_filter=AssistantControllerTest.Screenshot
    >   browser_tests --gtest_filter=BrowserNonClientFrameViewAshTest.IncognitoMarkedAsAssistantBlocked*
    > 
    > Bug: b/78193324
    > Test: 
    > Change-Id: Idf55135959721d69414a1ec6fae396f9763ebe49
    > Reviewed-on: https://chromium-review.googlesource.com/1114293
    > Commit-Queue: Muyuan Li <muyuanli@chromium.org>
    > Reviewed-by: James Cook <jamescook@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    > Reviewed-by: Michael Wasserman <msw@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#571385}
    
    Change-Id: Ieb8299a17fca3e3033e2c5a67933ed5ef642335b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: b/78193324
    Reviewed-on: https://chromium-review.googlesource.com/1119745
    Cr-Commit-Position: refs/heads/master@{#571394}

--- ash/assistant/assistant_controller.h
+++ ash/assistant/assistant_controller.h
@@ -25,10 +25,6 @@ namespace base {
 class UnguessableToken;
 }  // namespace base
 
-namespace ui {
-class LayerTreeOwner;
-}  // namespace ui
-
 namespace ash {
 
 class AssistantControllerObserver;
@@ -103,8 +99,6 @@ class AssistantController : public mojom::AssistantController,
     DCHECK(assistant_ui_controller_);
     return assistant_ui_controller_.get();
   }
-
-  std::unique_ptr<ui::LayerTreeOwner> CreateLayerForAssistantSnapshotForTest();
 
  private:
   // The observer list should be initialized early so that sub-controllers may
