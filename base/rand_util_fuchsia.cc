cd3d13679a6501b77c2c6bc48913027fbd8c3bc2
Wez <wez@chromium.org>
2018-06-29 05:46:49 +0000

    [fuchsia] Reduce RandBytes() to a single zx_cprng_draw() call.
    Bug: 858194
    Change-Id: I051084347da2f395e25b090ba54439c866b0c044
    Reviewed-on: https://chromium-review.googlesource.com/1119357
    Commit-Queue: Wez <wez@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571396}

--- base/rand_util_fuchsia.cc
+++ base/rand_util_fuchsia.cc
@@ -7,22 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <zircon/syscalls.h>
 
-#include <algorithm>
-
 namespace base {
 
 void RandBytes(void* output, size_t output_length) {
-  size_t remaining = output_length;
-  unsigned char* cur = reinterpret_cast<unsigned char*>(output);
-  while (remaining > 0) {
-    // The syscall has a maximum number of bytes that can be read at once.
-    size_t read_len =
-        std::min(remaining, static_cast<size_t>(ZX_CPRNG_DRAW_MAX_LEN));
-    zx_cprng_draw(cur, read_len);
-
-    remaining -= read_len;
-    cur += read_len;
-  }
+  zx_cprng_draw(output, output_length);
 }
 
 }  // namespace base
