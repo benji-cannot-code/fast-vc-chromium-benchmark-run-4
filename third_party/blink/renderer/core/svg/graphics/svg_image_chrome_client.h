f6cb6ad238c7fc127d51c72eb17792c8d08df6fc
Fredrik Söderquist <fs@opera.com>
2018-06-29 19:21:07 +0000

    Revert "Suspend SVG image animations when the page is hidden"
    This reverts commit b56e2510d08c0a8fd0be69ad29a2558c6ce63523.
    
    Reason for revert: Test flaky on cast_shell_linux
    
    Original change's description:
    > Suspend SVG image animations when the page is hidden
    > 
    > Check Document::hidden() in LayoutObject::WillRenderImage,
    > thus pausing animations on pages that are not visible.
    > To resume animations, we may need to explicitly resume the
    > timeline, so a hook is added in Page::SetVisibilityState to
    > allow calling StartAnimation() on the relevant SVGImages.
    > 
    > Bug: 856082
    > Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    > Change-Id: I4a0b008730ec3e6c2af26e15d52eebb26020ad32
    > Reviewed-on: https://chromium-review.googlesource.com/1115130
    > Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    > Commit-Queue: Fredrik Söderquist <fs@opera.com>
    > Cr-Commit-Position: refs/heads/master@{#571543}
    
    TBR=fs@opera.com,chrishtr@chromium.org
    
    Change-Id: I53a07f59bf48095c21ccf6eb357162786012ad1c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 856082
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Reviewed-on: https://chromium-review.googlesource.com/1120976
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#571587}

--- third_party/blink/renderer/core/svg/graphics/svg_image_chrome_client.h
+++ third_party/blink/renderer/core/svg/graphics/svg_image_chrome_client.h
@@ -50,8 +50,6 @@ class CORE_EXPORT SVGImageChromeClient final : public EmptyChromeClient {
 
   void SuspendAnimation();
   void ResumeAnimation();
-  void RestoreAnimationIfNeeded();
-
   bool IsSuspended() const { return timeline_state_ >= kSuspended; }
 
  private:
@@ -62,7 +60,6 @@ class CORE_EXPORT SVGImageChromeClient final : public EmptyChromeClient {
   void ScheduleAnimation(const LocalFrameView*) override;
 
   void SetTimer(std::unique_ptr<TimerBase>);
-  TimerBase* GetTimerForTesting() const { return animation_timer_.get(); }
   void AnimationTimerFired(TimerBase*);
 
   SVGImage* image_;
@@ -75,8 +72,6 @@ class CORE_EXPORT SVGImageChromeClient final : public EmptyChromeClient {
 
   FRIEND_TEST_ALL_PREFIXES(SVGImageTest, TimelineSuspendAndResume);
   FRIEND_TEST_ALL_PREFIXES(SVGImageTest, ResetAnimation);
-  FRIEND_TEST_ALL_PREFIXES(SVGImagePageVisibilityTest,
-                           PageVisibilityHiddenToVisible);
 };
 
 DEFINE_TYPE_CASTS(SVGImageChromeClient,
