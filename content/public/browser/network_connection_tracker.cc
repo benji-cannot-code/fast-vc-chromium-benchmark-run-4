222506598774fec92e9e1605e941f5dc01c0dcc3
Elad Alon <eladalon@chromium.org>
2018-06-29 15:38:41 +0000

    NetworkConnectionTracker replies to a SequencedTaskRunner
    NetworkConnectionTracker::GetConnectionType replies back to the
    thread from which the thread from which the call originated. This
    CL changes this to make it reply back to a SequencedTaskRunner,
    which is a more lax requirement on the caller.
    
    TBR=jam@chromium.org
    
    Bug: 775415
    Change-Id: Icd9f8d8f2e8587e7fc1b0127b4cc1621629b82f9
    Reviewed-on: https://chromium-review.googlesource.com/1119920
    Commit-Queue: Elad Alon <eladalon@chromium.org>
    Reviewed-by: Helen Li <xunjieli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571489}

--- content/public/browser/network_connection_tracker.cc
+++ content/public/browser/network_connection_tracker.cc
@@ -87,7 +87,7 @@ bool NetworkConnectionTracker::GetConnectionType(
   }
   if (!task_runner_->RunsTasksInCurrentSequence()) {
     connection_type_callbacks_.push_back(base::BindOnce(
-        &OnGetConnectionType, base::ThreadTaskRunnerHandle::Get(),
+        &OnGetConnectionType, base::SequencedTaskRunnerHandle::Get(),
         std::move(callback)));
   } else {
     connection_type_callbacks_.push_back(std::move(callback));
