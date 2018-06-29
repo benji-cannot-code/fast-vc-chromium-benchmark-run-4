3a636e06df13468722a0e2524c6cea63672f14fe
Shakti Sahu <shaktisahu@chromium.org>
2018-06-29 22:21:39 +0000

    Downloads OCP : Implemented GetVisualsForItem
    Completed the implementation for DownloadsOfflineContentProvider::GetVisualsForItem.
    
    Bug: 855330
    Change-Id: If7b9018c7758e3d857b6c34bc709ae517e182e2e
    Reviewed-on: https://chromium-review.googlesource.com/1113941
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571674}

--- chrome/browser/download/download_offline_content_provider.cc
+++ chrome/browser/download/download_offline_content_provider.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/time/time.h"
+#include "chrome/browser/download/image_thumbnail_request.h"
 #include "chrome/browser/download/offline_item_utils.h"
 #include "chrome/browser/offline_items_collection/offline_content_aggregator_factory.h"
 #include "chrome/browser/profiles/profile.h"
@@ -16,13 +17,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/offline_items_collection/core/offline_content_aggregator.h"
 #include "components/offline_items_collection/core/offline_item.h"
 #include "content/public/browser/browser_context.h"
+#include "third_party/skia/include/core/SkBitmap.h"
+#include "ui/display/display.h"
+#include "ui/display/screen.h"
 
 using OfflineItemFilter = offline_items_collection::OfflineItemFilter;
 using OfflineItemState = offline_items_collection::OfflineItemState;
 using OfflineItemProgressUnit =
     offline_items_collection::OfflineItemProgressUnit;
+using OfflineItemVisuals = offline_items_collection::OfflineItemVisuals;
 
 namespace {
+
+// Thumbnail size used for generating thumbnails for image files.
+const int kThumbnailSizeInDP = 64;
 
 bool ShouldShowDownloadItem(const download::DownloadItem* item) {
   return !item->IsTemporary() && !item->IsTransient() && !item->IsDangerous();
@@ -115,8 +123,32 @@ void DownloadOfflineContentProvider::GetVisualsForItem(
     const ContentId& id,
     const VisualsCallback& callback) {
   // TODO(crbug.com/855330) Supply thumbnail if item is visible.
+  DownloadItem* item = manager_->GetDownloadByGuid(id.id);
+  if (!item)
+    return;
+
+  display::Display display = display::Screen::GetScreen()->GetPrimaryDisplay();
+  int icon_size = kThumbnailSizeInDP * display.device_scale_factor();
+
+  auto request = std::make_unique<ImageThumbnailRequest>(
+      icon_size,
+      base::BindOnce(&DownloadOfflineContentProvider::OnThumbnailRetrieved,
+                     weak_ptr_factory_.GetWeakPtr(), id, callback));
+  request->Start(item->GetTargetFilePath());
+
+  // Dropping ownership of |request| here because it will clean itself up once
+  // the started request finishes.
+  request.release();
+}
+
+void DownloadOfflineContentProvider::OnThumbnailRetrieved(
+    const ContentId& id,
+    const VisualsCallback& callback,
+    const SkBitmap& bitmap) {
+  auto visuals = std::make_unique<OfflineItemVisuals>();
+  visuals->icon = gfx::Image::CreateFrom1xBitmap(bitmap);
   base::ThreadTaskRunnerHandle::Get()->PostTask(
-      FROM_HERE, base::BindOnce(callback, id, nullptr));
+      FROM_HERE, base::BindOnce(std::move(callback), id, std::move(visuals)));
 }
 
 void DownloadOfflineContentProvider::AddObserver(
