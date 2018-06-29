4b3c915655b3e3e33aee054cb24e33c5b6ed9bf4
manuk <manukh@chromium.org>
2018-06-29 15:13:05 +0000

    omnibox: reset display url when re-navigating to the current page by clicking a link
    Previously, if navigating a link did not cause a url change (e.g. clicking on the `stackoverflow`
    logo on the `stackoverflow.com` homepage reloads the same page), then the display url remained
    unchanged. If the user had deleted or edited the url, his changes would remain. Now, the url is
    reset.
    
    Bug: 830491
    Change-Id: I9819fa29ca1d86467a660e0e40ddb149cf210f1d
    Reviewed-on: https://chromium-review.googlesource.com/1114337
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571479}

--- chrome/browser/ui/views/toolbar/toolbar_view.h
+++ chrome/browser/ui/views/toolbar/toolbar_view.h
@@ -76,6 +76,10 @@ class ToolbarView : public views::AccessiblePaneView,
   // as well.
   void Update(content::WebContents* tab);
 
+  // Reverts user modifications to the omnibox text. Delegates to
+  // LocationBarView.
+  void RevertUrl();
+
   // Clears the current state for |tab|.
   void ResetTabState(content::WebContents* tab);
 
