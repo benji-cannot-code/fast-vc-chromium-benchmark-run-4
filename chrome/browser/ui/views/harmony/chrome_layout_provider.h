6cc14a412c88479267bfc4c5fed189e58803ff5a
Tommy C. Li <tommycli@chromium.org>
2018-06-29 18:05:29 +0000

    Omnibox UI Refresh: Animate the text jog when suggestion popup opens.
    Many devs have complained that the text moves unexpectedly when the
    popup opens. After consulting with UX, we are embracing the text jogging
    to make it line up with the suggestions text.
    
    This CL adds an animation to slide the text over, making the text jog
    hopefully less jarring.
    
    Bug: 853355
    Change-Id: If272e4076e30c967c0211c988e5e9f10300366bf
    Reviewed-on: https://chromium-review.googlesource.com/1119400
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571544}

--- chrome/browser/ui/views/harmony/chrome_layout_provider.h
+++ chrome/browser/ui/views/harmony/chrome_layout_provider.h
@@ -15,10 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/layout/layout_provider.h"
 
 enum ChromeInsetsMetric {
-  // Padding applied around the text in the omnibox's editable area.
-  INSETS_OMNIBOX = views::VIEWS_INSETS_END,
   // Padding around buttons on the bookmarks bar.
-  INSETS_BOOKMARKS_BAR_BUTTON,
+  INSETS_BOOKMARKS_BAR_BUTTON = views::VIEWS_INSETS_END,
   // Margins used by toasts.
   INSETS_TOAST,
 };
--- chrome/browser/ui/views/omnibox/omnibox_view_views.cc
+++ chrome/browser/ui/views/omnibox/omnibox_view_views.cc
@@ -56,6 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/event.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/font_list.h"
+#include "ui/gfx/geometry/insets.h"
 #include "ui/gfx/selection_model.h"
 #include "ui/strings/grit/ui_strings.h"
 #include "ui/views/accessibility/view_accessibility.h"
@@ -175,8 +176,8 @@ void OmniboxViewViews::Init() {
 
   // Override the default FocusableBorder from Textfield, since the
   // LocationBarView will indicate the focus state.
-  SetBorder(views::CreateEmptyBorder(
-      ChromeLayoutProvider::Get()->GetInsetsMetric(INSETS_OMNIBOX)));
+  constexpr gfx::Insets kTextfieldInsets(3);
+  SetBorder(views::CreateEmptyBorder(kTextfieldInsets));
 
 #if defined(OS_CHROMEOS)
   chromeos::input_method::InputMethodManager::Get()->
@@ -238,20 +239,6 @@ void OmniboxViewViews::InstallPlaceholderText() {
   } else {
     set_placeholder_text(base::string16());
   }
-}
-
-void OmniboxViewViews::UpdateTextIndent() {
-  DCHECK(ui::MaterialDesignController::IsRefreshUi());
-
-  gfx::Insets insets =
-      ChromeLayoutProvider::Get()->GetInsetsMetric(INSETS_OMNIBOX);
-  if (model()->popup_model()->IsOpen())
-    insets += gfx::Insets(0, 8 /* left */, 0, 0);
-
-  SetBorder(views::CreateEmptyBorder(insets));
-
-  // This is necessary to reposition the internal RenderText.
-  OnBoundsChanged(gfx::Rect());
 }
 
 bool OmniboxViewViews::IsHovered() const {
