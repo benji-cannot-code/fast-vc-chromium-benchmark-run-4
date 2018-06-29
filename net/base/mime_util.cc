ec54764411f0527f4f2b3ee50a57036e5d8d42b2
Devlin Cronin <rdevlin.cronin@chromium.org>
2018-06-29 14:52:43 +0000

    [Net] Update MIME type for .js files
    According to https://html.spec.whatwg.org/multipage/scripting.html#scriptingLanguages,
    JS files should have a MIME type of text/javascript. Update our mappings.
    
    Bug: 857588
    
    Change-Id: I6c9ce8c52a3e6afcb8768392b59e766a0db3b5c5
    Reviewed-on: https://chromium-review.googlesource.com/1117564
    Commit-Queue: Asanka Herath <asanka@chromium.org>
    Reviewed-by: Asanka Herath <asanka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571472}

--- net/base/mime_util.cc
+++ net/base/mime_util.cc
@@ -81,7 +81,6 @@ static const MimeInfo kPrimaryMappings[] = {
     // Must precede audio/webm .
     {"video/webm", "webm"},
 
-    {"application/javascript", "js"},
     {"application/wasm", "wasm"},
     {"application/x-chrome-extension", "crx"},
     {"application/xhtml+xml", "xhtml,xht,xhtm"},
@@ -99,6 +98,7 @@ static const MimeInfo kPrimaryMappings[] = {
     {"multipart/related", "mht,mhtml"},
     {"text/css", "css"},
     {"text/html", "html,htm,shtml,shtm"},
+    {"text/javascript", "js"},
     {"text/xml", "xml"},
     {"video/mp4", "mp4,m4v"},
     {"video/ogg", "ogv,ogm"},
@@ -111,6 +111,7 @@ static const MimeInfo kSecondaryMappings[] = {
     {"application/epub+zip", "epub"},
     {"application/font-woff", "woff"},
     {"application/gzip", "gz,tgz"},
+    {"application/javascript", "js"},
     {"application/json", "json"},  // Per http://www.ietf.org/rfc/rfc4627.txt.
     {"application/octet-stream", "bin,exe,com"},
     {"application/pdf", "pdf"},
