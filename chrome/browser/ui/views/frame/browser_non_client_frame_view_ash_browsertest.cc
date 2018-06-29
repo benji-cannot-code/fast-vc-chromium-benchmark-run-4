a42a54e8f33d3da3f51f445c8a860a613e78b05b
Muyuan Li <muyuanli@google.com>
2018-06-29 04:22:43 +0000

    assistant: filter out incognito windows in assistant screenshot.
    Per privacy requirement, incognito windows should not be included
    in screenshot sent to assistant server.
    
      ash_unittests --gtest_filter=AssistantControllerTest.Screenshot
      browser_tests --gtest_filter=BrowserNonClientFrameViewAshTest.IncognitoMarkedAsAssistantBlocked*
    
    Bug: b/78193324
    Test: 
    Change-Id: Idf55135959721d69414a1ec6fae396f9763ebe49
    Reviewed-on: https://chromium-review.googlesource.com/1114293
    Commit-Queue: Muyuan Li <muyuanli@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571385}

--- chrome/browser/ui/views/frame/browser_non_client_frame_view_ash_browsertest.cc
+++ chrome/browser/ui/views/frame/browser_non_client_frame_view_ash_browsertest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/ash_switches.h"
 #include "ash/public/cpp/immersive/immersive_fullscreen_controller_test_api.h"
 #include "ash/public/cpp/vector_icons/vector_icons.h"
+#include "ash/public/cpp/window_properties.h"
 #include "ash/shell.h"
 #include "ash/wm/overview/window_selector_controller.h"
 #include "ash/wm/splitview/split_view_controller.h"
@@ -372,6 +373,13 @@ IN_PROC_BROWSER_TEST_P(BrowserNonClientFrameViewAshTest, IncognitoAvatar) {
   const bool should_have_avatar = GetParam() == switches::kTopChromeMDMaterial;
   const bool has_avatar = !!frame_view->profile_indicator_icon();
   EXPECT_EQ(should_have_avatar, has_avatar);
+}
+
+IN_PROC_BROWSER_TEST_P(BrowserNonClientFrameViewAshTest,
+                       IncognitoMarkedAsAssistantBlocked) {
+  Browser* incognito_browser = CreateIncognitoBrowser();
+  EXPECT_TRUE(incognito_browser->window()->GetNativeWindow()->GetProperty(
+      ash::kBlockedForAssistantSnapshotKey));
 }
 
 // Tests that FrameCaptionButtonContainer has been relaid out in response to
