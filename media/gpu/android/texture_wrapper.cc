1943861d5446ef5e4e83d1de594dee9114c35851
Frank Liberato <liberato@chromium.org>
2018-06-29 22:12:11 +0000

    Revert "Reland "Convert various gpu video decoders to use AbstractTexture.""
    This reverts commit cb054ea1d84d927c7360bf8588345040089f2e17.
    
    Reason for revert: it definitely breaks some tests.
    
    Original change's description:
    > Reland "Convert various gpu video decoders to use AbstractTexture."
    > 
    > This is a reland of 7baa2dc10e393f3ca4d052555f00d12680ca5366
    > 
    > Original change's description:
    > > Convert various gpu video decoders to use AbstractTexture.
    > > 
    > > Previously, VDAVideoDecoder, MCVD and D3D11VideoDecoder used
    > > the validating GLES decoder's TextureRef (possibly indirectly, via
    > > GLES2DecoderHelper and CommandBufferHelper) to retain textures.
    > > 
    > > This CL moves them all over to use AbstractTexture, in preparation
    > > for supporting the passthrough GLES decoder.
    > > 
    > > Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    > > Change-Id: I13985223443b8e0897c1508f052d0f35d77746fe
    > > Reviewed-on: https://chromium-review.googlesource.com/1091649
    > > Commit-Queue: Frank Liberato <liberato@chromium.org>
    > > Reviewed-by: Antoine Labour <piman@chromium.org>
    > > Reviewed-by: Dan Sanders <sandersd@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#571223}
    > 
    > Change-Id: I69b842f2c08cbac9e9a1dfa12ea08133fff56c75
    > Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    > Reviewed-on: https://chromium-review.googlesource.com/1120470
    > Reviewed-by: Dan Sanders <sandersd@chromium.org>
    > Reviewed-by: Antoine Labour <piman@chromium.org>
    > Commit-Queue: Frank Liberato <liberato@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#571627}
    
    TBR=sandersd@chromium.org,liberato@chromium.org,piman@chromium.org
    
    Change-Id: I6376a7a565d705ef1d35ba0b3c7eb98463ecd967
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Reviewed-on: https://chromium-review.googlesource.com/1121297
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571668}

---
+++ media/gpu/android/texture_wrapper.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "media/gpu/android/texture_wrapper.h"
+
+#include "gpu/command_buffer/service/texture_manager.h"
+
+namespace media {
+
+TextureWrapperImpl::TextureWrapperImpl(
+    scoped_refptr<gpu::gles2::TextureRef> texture_ref)
+    : texture_ref_(std::move(texture_ref)) {}
+
+TextureWrapperImpl::~TextureWrapperImpl() {}
+
+void TextureWrapperImpl::ForceContextLost() {
+  if (texture_ref_)
+    texture_ref_->ForceContextLost();
+}
+
+}  // namespace media
--- media/gpu/android/video_frame_factory_impl.h
+++ media/gpu/android/video_frame_factory_impl.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/optional.h"
 #include "base/single_thread_task_runner.h"
-#include "gpu/command_buffer/service/abstract_texture.h"
 #include "gpu/command_buffer/service/gles2_cmd_decoder.h"
 #include "gpu/command_buffer/service/texture_manager.h"
 #include "gpu/ipc/service/command_buffer_stub.h"
@@ -90,7 +89,7 @@ class GpuVideoFrameFactory
   void SetImageGroup(scoped_refptr<CodecImageGroup> image_group);
 
  private:
-  // Creates an AbstractTexture and VideoFrame.
+  // Creates a TextureRef and VideoFrame.
   void CreateVideoFrameInternal(
       std::unique_ptr<CodecOutputBuffer> output_buffer,
       scoped_refptr<TextureOwner> texture_owner,
@@ -98,7 +97,7 @@ class GpuVideoFrameFactory
       gfx::Size natural_size,
       PromotionHintAggregator::NotifyPromotionHintCB promotion_hint_cb,
       scoped_refptr<VideoFrame>* video_frame_out,
-      std::unique_ptr<gpu::gles2::AbstractTexture>* texture_out);
+      scoped_refptr<gpu::gles2::TextureRef>* texture_ref_out);
 
   void OnWillDestroyStub(bool have_context) override;
 
