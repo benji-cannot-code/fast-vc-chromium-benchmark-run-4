a95949d0260cb3a7b90a479c502983386e7245a7
Henrik Grunell <grunell@chromium.org>
2018-06-29 14:06:15 +0000

    Revert "cc: Post task on mojo begin frame to mitigate begin frame flood"
    This reverts commit 89b548a0bae43ef770c8e82a0e396d08c0cc53d4.
    
    Reason for revert: Likely to cause flakiness of test WebViewTest.SelectShowHide.
    
    Original change's description:
    > cc: Post task on mojo begin frame to mitigate begin frame flood
    > 
    > Unlike PostTask from IO thread to compositor thread in Chrome IPC, mojo
    > polls for messages on the compositor thread which means it can dequeue a
    > large number of begin frame messages after the compositor thread has
    > been busy for some time. All but the last begin frame cancels the
    > previous begin frame, and is essentially a nop, but it still ticks
    > animations. When a page has a large number of animations each begin
    > frame can take a long time and push out other tasks such as tile manager
    > callbacks stalling the pipeline.
    > 
    > Throttling the begin frames in viz doesn't fully solve the problem
    > because we have to allow at least two begin frames in flight for
    > pipelining, and so the client can still process two begin frames back to
    > back.
    > 
    > Post a task to issue the begin frame to the source and its observers.
    > The task is reset on every begin frame mojo message so only the last
    > message is propagated to the source.
    > 
    > TEST=AsyncLayerTreeFrameSinkTest.MultipleBeginFrames
    > R=​danakj
    > BUG=782002
    > 
    > Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    > Change-Id: Ie98930a3078dde5ff99b42148596cf564c079220
    > Reviewed-on: https://chromium-review.googlesource.com/1111474
    > Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    > Reviewed-by: Antoine Labour <piman@chromium.org>
    > Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    > Reviewed-by: danakj <danakj@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#571282}
    
    TBR=sadrul@chromium.org,danakj@chromium.org,fsamuel@chromium.org,sunnyps@chromium.org,piman@chromium.org
    
    Change-Id: I5f184c5bde6384aa8b40feb2941e259ace7d7ac3
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 782002
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Reviewed-on: https://chromium-review.googlesource.com/1120305
    Reviewed-by: Henrik Grunell <grunell@chromium.org>
    Commit-Queue: Henrik Grunell <grunell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571462}

--- ui/aura/mus/window_port_mus.cc
+++ ui/aura/mus/window_port_mus.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/aura/mus/window_port_mus.h"
 
-#include "base/threading/thread_task_runner_handle.h"
 #include "cc/mojo_embedder/async_layer_tree_frame_sink.h"
 #include "components/viz/client/local_surface_id_provider.h"
 #include "components/viz/host/host_frame_sink_manager.h"
@@ -20,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/window.h"
 #include "ui/aura/window_delegate.h"
 #include "ui/aura/window_observer.h"
-#include "ui/aura/window_tree_host.h"
 #include "ui/base/class_property.h"
 #include "ui/base/ui_base_features.h"
 #include "ui/base/ui_base_switches_util.h"
@@ -117,7 +115,6 @@ void WindowPortMus::EmbedUsingToken(
 std::unique_ptr<cc::mojo_embedder::AsyncLayerTreeFrameSink>
 WindowPortMus::RequestLayerTreeFrameSink(
     scoped_refptr<viz::ContextProvider> context_provider,
-    scoped_refptr<base::SingleThreadTaskRunner> compositor_task_runner,
     gpu::GpuMemoryBufferManager* gpu_memory_buffer_manager) {
   viz::mojom::CompositorFrameSinkPtrInfo sink_info;
   viz::mojom::CompositorFrameSinkRequest sink_request =
@@ -127,8 +124,6 @@ WindowPortMus::RequestLayerTreeFrameSink(
       mojo::MakeRequest(&client);
 
   cc::mojo_embedder::AsyncLayerTreeFrameSink::InitParams params;
-  params.context_provider = std::move(context_provider);
-  params.compositor_task_runner = std::move(compositor_task_runner);
   params.gpu_memory_buffer_manager = gpu_memory_buffer_manager;
   params.pipes.compositor_frame_sink_info = std::move(sink_info);
   params.pipes.client_request = std::move(client_request);
@@ -140,7 +135,8 @@ WindowPortMus::RequestLayerTreeFrameSink(
 
   auto layer_tree_frame_sink =
       std::make_unique<cc::mojo_embedder::AsyncLayerTreeFrameSink>(
-          std::move(params));
+          std::move(context_provider), nullptr /* worker_context_provider */,
+          &params);
   window_tree_client_->AttachCompositorFrameSink(
       server_id(), std::move(sink_request), std::move(client));
   return layer_tree_frame_sink;
@@ -590,15 +586,9 @@ WindowPortMus::CreateLayerTreeFrameSink() {
   DCHECK_EQ(window_mus_type(), WindowMusType::LOCAL);
   DCHECK(!local_layer_tree_frame_sink_);
 
-  // Tests do not necessarily have an associated WindowTreeHost.
-  scoped_refptr<base::SingleThreadTaskRunner> compositor_task_runner =
-      base::ThreadTaskRunnerHandle::Get();
-  if (window_->GetHost())
-    compositor_task_runner = window_->GetHost()->compositor()->task_runner();
-
   std::unique_ptr<cc::LayerTreeFrameSink> frame_sink;
   auto client_layer_tree_frame_sink = RequestLayerTreeFrameSink(
-      nullptr /* context_provider */, compositor_task_runner,
+      nullptr,
       aura::Env::GetInstance()->context_factory()->GetGpuMemoryBufferManager());
   local_layer_tree_frame_sink_ = client_layer_tree_frame_sink->GetWeakPtr();
   frame_sink = std::move(client_layer_tree_frame_sink);
