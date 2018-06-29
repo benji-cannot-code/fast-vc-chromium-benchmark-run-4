5b14540de924fe0241dd06ebcbef1413f308b028
Cole Winstanley <cwinstanley@google.com>
2018-06-29 22:12:37 +0000

    documentation for old adb bug in run_py_tests
    Bug: 857239
    Change-Id: Ib55ad681b3cb87899cfdb9701542fff5290fe796
    Reviewed-on: https://chromium-review.googlesource.com/1118913
    Commit-Queue: Cole Winstanley <cwinstanley@google.com>
    Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571669}

--- chrome/test/chromedriver/test/run_py_tests.py
+++ chrome/test/chromedriver/test/run_py_tests.py
@@ -6,6 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 """End to end tests for ChromeDriver."""
 
+# Note that to run Android tests you must have the following line in
+# .gclient (in the parent directory of src):  target_os = [ 'android' ]
+# to get the appropriate adb version for ChromeDriver.
+# TODO (crbug.com/857239): Remove above comment when adb version
+# is updated in Devil.
+
 import base64
 import json
 import math
