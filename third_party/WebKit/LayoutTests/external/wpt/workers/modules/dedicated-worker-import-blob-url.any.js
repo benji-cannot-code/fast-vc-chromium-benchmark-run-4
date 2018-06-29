f1bf212df15456ec1f197fcd964160a50b211ab1
Hiroki Nakagawa <nhiroki@chromium.org>
2018-06-29 03:54:58 +0000

    Worker: Add WPTs for module loading with blob, data, and file URLs
    Bug: 680046
    Change-Id: I8b3894bfbc8df999952fa82e8f2b3abb4088f434
    Reviewed-on: https://chromium-review.googlesource.com/1114566
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571378}

---
+++ third_party/WebKit/LayoutTests/external/wpt/workers/modules/dedicated-worker-import-blob-url.any.js
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/workers/modules/resources/import-test-cases.js
+
+// Imports |testCase.scriptURL| on a dedicated worker loaded from a blob URL,
+// and waits until the list of imported modules is sent from the worker. Passes
+// if the list is equal to |testCase.expectation|.
+function import_blob_url_test(testCase) {
+  promise_test(async () => {
+    const importURL = new URL(testCase.scriptURL, location.href);
+    const blob = new Blob([`import "${importURL}";`],
+                          { type: 'text/javascript' });
+    const blobURL = URL.createObjectURL(blob);
+
+    const worker = new Worker(blobURL, { type: 'module'});
+    const msgEvent = await new Promise(resolve => worker.onmessage = resolve);
+    assert_array_equals(msgEvent.data, testCase.expectation);
+  }, testCase.description);
+}
+
+testCases.forEach(import_blob_url_test);
