51f01ddcda6e01d5ad29013ac000b6b5a8e4d438
Peter K. Lee <pkl@chromium.org>
2018-06-29 18:04:23 +0000

    Force FeedbackKit to use V2 implementation.
    FeedbackKit V2 launched in M67. In order to deprecate the V1
    implementation, the first step needs to force the chrome://flags to
    true and then implementation code can be removed.
    
    Bug: 847329
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I9451fa04453584d6e056e82f8c9f323d0a87c06c
    Reviewed-on: https://chromium-review.googlesource.com/1119355
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Peter Lee <pkl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571542}

--- ios/chrome/browser/experimental_flags.mm
+++ ios/chrome/browser/experimental_flags.mm
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/browsing_data/browsing_data_features.h"
 #include "ios/chrome/browser/chrome_switches.h"
 #include "ios/chrome/browser/ui/ui_feature_flags.h"
-#include "ios/chrome/browser/ui/user_feedback_features.h"
 #include "ios/web/public/web_view_creation_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -115,11 +114,11 @@ bool IsNewClearBrowsingDataUIEnabled() {
 }
 
 bool IsNewFeedbackKitEnabled() {
-  return base::FeatureList::IsEnabled(kFeedbackKitV2);
+  return true;
 }
 
 bool IsNewFeedbackKitEnabledWithSSOService() {
-  return base::FeatureList::IsEnabled(kFeedbackKitV2WithSSOService);
+  return true;
 }
 
 bool IsThirdPartyKeyboardWorkaroundEnabled() {
