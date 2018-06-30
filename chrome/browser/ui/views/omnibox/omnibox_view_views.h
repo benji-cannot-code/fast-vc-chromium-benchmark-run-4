bca43b05f03ec52010bb3ba3f62c2fcded724f04
Orin Jaworski <orinj@chromium.org>
2018-06-30 01:24:55 +0000

    [omnibox] Animate background color on hover
    When the steady omnibox is hovered, the location bar
    smoothly animates to a slightly different shade
    of gray instead of switching instantly.
    
    Bug: 853241
    Change-Id: I5c51607a602f7e7245dddd46060d6dc25f4f4581
    Reviewed-on: https://chromium-review.googlesource.com/1119494
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571733}

--- chrome/browser/ui/views/omnibox/omnibox_view_views.h
+++ chrome/browser/ui/views/omnibox/omnibox_view_views.h
@@ -92,9 +92,6 @@ class OmniboxViewViews : public OmniboxView,
   // "Search Google or type a URL" when the Omnibox is empty and unfocused.
   void InstallPlaceholderText();
 
-  // Returns true if the omnibox is currently hovered.
-  bool IsHovered() const;
-
   // OmniboxView:
   void EmphasizeURLComponents() override;
   void Update() override;
@@ -187,9 +184,6 @@ class OmniboxViewViews : public OmniboxView,
 
   void ClearAccessibilityLabel();
 
-  // Sets the hovered state for omnibox.
-  void SetHovered(bool hovered);
-
   // Returns true if the user text was updated with the full URL (without
   // steady-state elisions).  |gesture| is the user gesture causing unelision.
   bool UnapplySteadyStateElisions(UnelisionGesture gesture);
@@ -243,10 +237,6 @@ class OmniboxViewViews : public OmniboxView,
   bool IsTextEditCommandEnabled(ui::TextEditCommand command) const override;
   void ExecuteTextEditCommand(ui::TextEditCommand command) override;
 
-  // views::View:
-  void OnMouseMoved(const ui::MouseEvent& event) override;
-  void OnMouseExited(const ui::MouseEvent& event) override;
-
   // chromeos::input_method::InputMethodManager::CandidateWindowObserver:
 #if defined(OS_CHROMEOS)
   void CandidateWindowOpened(
@@ -282,9 +272,6 @@ class OmniboxViewViews : public OmniboxView,
 
   // TemplateURLServiceObserver:
   void OnTemplateURLServiceChanged() override;
-
-  // Holds hover state for visual hinting.
-  bool hovered_;
 
   // When true, the location bar view is read only and also is has a slightly
   // different presentation (smaller font size). This is used for popups.
