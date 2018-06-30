2b15ab76f4a11a231a6fde4e38785be12b5f554c
Gyuyoung Kim <gyuyoung.kim@lge.com>
2018-06-30 03:11:31 +0000

    CC: Support the memory pressure handler in StagingBufferPool
    StagingBufferPool has been only supporting the MemoryCoordinator.
    However, it is not enabled by default. So it would be good if we
    support the memory pressure handler until the MemoryCoordinator
    is enabled by default. Especially, it will help low-end devices to
    reduce memory usage under the out-of-memory situation.
    
    Bug: 856527
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I52be2b732199c66ddaac015fb29aae274ff2fb1d
    Reviewed-on: https://chromium-review.googlesource.com/1114559
    Commit-Queue: Gyuyoung Kim <gyuyoung.kim@lge.com>
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571750}

--- cc/raster/staging_buffer_pool.h
+++ cc/raster/staging_buffer_pool.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/circular_deque.h"
 #include "base/macros.h"
 #include "base/memory/memory_coordinator_client.h"
+#include "base/memory/memory_pressure_listener.h"
 #include "base/memory/weak_ptr.h"
 #include "base/sequenced_task_runner.h"
 #include "base/synchronization/lock.h"
@@ -101,6 +102,11 @@ class CC_EXPORT StagingBufferPool
   // Overriden from base::MemoryCoordinatorClient.
   void OnPurgeMemory() override;
 
+  // TODO(gyuyoung): OnMemoryPressure is deprecated. So this should be removed
+  // when the memory coordinator is enabled by default.
+  void OnMemoryPressure(
+      base::MemoryPressureListener::MemoryPressureLevel level);
+
   scoped_refptr<base::SequencedTaskRunner> task_runner_;
   viz::RasterContextProvider* const worker_context_provider_;
   const bool use_partial_raster_;
@@ -119,6 +125,8 @@ class CC_EXPORT StagingBufferPool
   const base::TimeDelta staging_buffer_expiration_delay_;
   bool reduce_memory_usage_pending_;
   base::Closure reduce_memory_usage_callback_;
+
+  std::unique_ptr<base::MemoryPressureListener> memory_pressure_listener_;
 
   base::WeakPtrFactory<StagingBufferPool> weak_ptr_factory_;
 
