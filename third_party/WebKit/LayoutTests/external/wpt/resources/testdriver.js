502a1eb18d7172c1798f8002d5b4a2718a2587b1
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-06-30 13:15:39 +0000

    Import wpt@f83787eec8e9cea98eaa0ee760effd1cb3126d56
    Using wpt-import in Chromium b0f7ee7dfea5ecef6850808aefd4beebc753bd06.
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/20388
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    jsbell@chromium.org:
      external/wpt/resources
    
    TBR=kereliuk
    
    No-Export: true
    Change-Id: If1beb971d3d8bec8eabb3aff40204336495233d3
    Reviewed-on: https://chromium-review.googlesource.com/1121689
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571773}

--- third_party/WebKit/LayoutTests/external/wpt/resources/testdriver.js
+++ third_party/WebKit/LayoutTests/external/wpt/resources/testdriver.js
@@ -81,7 +81,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                         return action();
                     }
                 });
-        };
+        },
 
         /**
          * Triggers a user-initiated click
