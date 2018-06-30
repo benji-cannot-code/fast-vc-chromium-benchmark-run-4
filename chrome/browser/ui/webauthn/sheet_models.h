9914871e6883149a49a5adda0eb7a24f8ca09cea
Balazs Engedy <engedy@chromium.org>
2018-06-30 00:34:27 +0000

    Advance to `Insert USB` sheet.
    Implement advancing from transport selection to the step where the user
    is asked to insert a USB security key.
    
    Bug: 849323
    Change-Id: Idd7ee863d8c5a826e156e8b9fea1bfdd1ee2347c
    Reviewed-on: https://chromium-review.googlesource.com/1104687
    Commit-Queue: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Kim Paulhamus <kpaulhamus@chromium.org>
    Reviewed-by: Jun Choi <hongjunchoi@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571721}

--- chrome/browser/ui/webauthn/sheet_models.h
+++ chrome/browser/ui/webauthn/sheet_models.h
@@ -74,4 +74,14 @@ class AuthenticatorTransportSelectorSheetModel
   base::string16 GetStepDescription() const override;
 };
 
+class AuthenticatorInsertUsbSheetModel : public AuthenticatorSheetModelBase {
+ public:
+  using AuthenticatorSheetModelBase::AuthenticatorSheetModelBase;
+
+ private:
+  // AuthenticatorSheetModelBase:
+  base::string16 GetStepTitle() const override;
+  base::string16 GetStepDescription() const override;
+};
+
 #endif  // CHROME_BROWSER_UI_WEBAUTHN_SHEET_MODELS_H_
--- chrome/browser/webauthn/authenticator_request_dialog_model.cc
+++ chrome/browser/webauthn/authenticator_request_dialog_model.cc
@@ -20,7 +20,13 @@ void AuthenticatorRequestDialogModel::SetCurrentStep(Step step) {
 void AuthenticatorRequestDialogModel::StartGuidedFlowForTransport(
     AuthenticatorTransport transport) {
   DCHECK_EQ(current_step(), Step::kTransportSelection);
-  // TODO(engedy): Use transport here.
+  switch (transport) {
+    case AuthenticatorTransport::kUsb:
+      SetCurrentStep(Step::kUsbInsert);
+      break;
+    default:
+      break;
+  }
 }
 
 void AuthenticatorRequestDialogModel::TryIfBleAdapterIsPowered() {
@@ -51,7 +57,10 @@ void AuthenticatorRequestDialogModel::TryUsbDevice() {
 
 void AuthenticatorRequestDialogModel::Cancel() {}
 
-void AuthenticatorRequestDialogModel::Back() {}
+void AuthenticatorRequestDialogModel::Back() {
+  // For now, return to the initial step all the time.
+  SetCurrentStep(Step::kInitial);
+}
 
 void AuthenticatorRequestDialogModel::AddObserver(Observer* observer) {
   observers_.AddObserver(observer);
