9fd87a689139e25a3ab77e055802c249e5c46cde
Yuli Huang <yuli@chromium.org>
2018-07-01 12:25:52 +0800

    Update recording MIME type and revise MediaRecorder usages.
    1. Use mkv MIME type and use .mkv file extension.
    2. Remove event-listeners after stopping the recorder.
    3. Remove unnecessasry enableAudio calls.
    
    BUG=b:110948780
    TEST=Tested by recording videos and exporting/saving files.
    
    Change-Id: I9439f5d89db0058b2366c2e25b3a758e75e693cc
    Reviewed-on: https://chromium-review.googlesource.com/1122072
    Reviewed-by: Sheng-hao Tsao <shenghao@google.com>
    Tested-by: yuli <yuli@chromium.org>

--- src/js/views/gallery_base.js
+++ src/js/views/gallery_base.js
@@ -153,6 +153,7 @@ camera.views.GalleryBase.prototype.exportSelection = function() {
         function() { onError(fileEntry.name); });
   }.bind(this);
 
+  // TODO(yuli): Move this into file_system.js and handle filename conflicts.
   chrome.fileSystem.chooseEntry({
     type: 'openDirectory'
   }, function(dirEntry) {
@@ -160,7 +161,8 @@ camera.views.GalleryBase.prototype.exportSelection = function() {
       return;
 
     var savePictureAsFile = function(picture) {
-      dirEntry.getFile(picture.pictureEntry.name, {
+      dirEntry.getFile(
+          camera.models.FileSystem.regulatePictureName(picture.pictureEntry), {
         create: true, exclusive: false
       }, function(fileEntry) {
           exportPicture(fileEntry, picture);
