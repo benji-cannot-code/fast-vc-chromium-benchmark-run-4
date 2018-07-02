269065cd10175dea7ef617a695d6c5e9dd738668
Oscar Johansson <oscarj@opera.com>
2018-07-02 04:17:15 +0000

    Move kMaxSegmentSize to constant file (net/)
    When building using Jumbo unnamed namespaces gets merged
    and variables with the same name conflict. This happens
    for the variables kMaxSegmentsSize in:
    net/third_party/quic/core/congestion_control/bbr_sender.cc
    net/third_party/quic/core/congestion_control/prr_sender.cc
    
    This commit solves the issue by moving the constant to
    net/third_party/quic/core/quic_constants.h.
    
    Bug: 772146
    Change-Id: I07da21c2bc8c96f6ed5cc83e2724ae948d80a9be
    Reviewed-on: https://chromium-review.googlesource.com/1119689
    Commit-Queue: Ryan Hamilton <rch@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571814}

--- net/third_party/quic/core/congestion_control/prr_sender.cc
+++ net/third_party/quic/core/congestion_control/prr_sender.cc
@@ -9,11 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace quic {
 
-namespace {
-// Constant based on TCP defaults.
-const QuicByteCount kMaxSegmentSize = kDefaultTCPMSS;
-}  // namespace
-
 PrrSender::PrrSender()
     : bytes_sent_since_loss_(0),
       bytes_delivered_since_loss_(0),
