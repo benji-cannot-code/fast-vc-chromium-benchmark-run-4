e84f1a6720714b14a4ac1ab7ba78ce7c410c9733
Marc Treib <treib@chromium.org>
2018-07-02 15:18:03 +0000

    Cleanup: remove components/browser_sync_strings.grdp
    The strings in there were referenced in only two places:
    1) ForeignSessionHandler (which powers chrome://history/syncedTabs) sent
       them to JS, but the JS side never used them.
    2) about_sync_util.cc (which powers chrome://sync-internals) used them,
       but nothing else on chrome://sync-internals is localized, so it's
       better to just inline non-localized strings there.
    
    Bug: none
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: Ide4a2efe563f57c9d0afaa51722dece0dac50d7e
    Reviewed-on: https://chromium-review.googlesource.com/1118233
    Reviewed-by: calamity <calamity@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571896}

--- chrome/browser/ui/webui/foreign_session_handler.cc
+++ chrome/browser/ui/webui/foreign_session_handler.cc
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/url_data_source.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
-#include "ui/base/l10n/l10n_util.h"
 #include "ui/base/l10n/time_format.h"
 #include "ui/base/webui/web_ui_util.h"
 
@@ -112,15 +111,6 @@ std::unique_ptr<base::DictionaryValue> BuildWindowData(
   // Please update it whenever you add or remove any keys here.
   dictionary->SetString("type", "window");
   dictionary->SetDouble("timestamp", modification_time.ToInternalValue());
-  const base::TimeDelta last_synced = base::Time::Now() - modification_time;
-  // If clock skew leads to a future time, or we last synced less than a minute
-  // ago, output "Just now".
-  dictionary->SetString(
-      "userVisibleTimestamp",
-      last_synced < base::TimeDelta::FromMinutes(1)
-          ? l10n_util::GetStringUTF16(IDS_SYNC_TIME_JUST_NOW)
-          : ui::TimeFormat::Simple(ui::TimeFormat::FORMAT_ELAPSED,
-                                   ui::TimeFormat::LENGTH_SHORT, last_synced));
 
   dictionary->SetInteger("sessionId", window_id.id());
   return dictionary;
