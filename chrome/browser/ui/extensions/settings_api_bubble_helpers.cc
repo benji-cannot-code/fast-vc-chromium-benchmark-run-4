85097fba3b69020de26c66427185d3d4dd60e523
clamy <clamy@chromium.org>
2018-07-02 12:30:32 +0000

    Remove usage of GetActiveEntry [13/n]
    This CL is part of a serie of CLs to remove usage of the deprecated
    method NavigationController::GetActiveEntry.
    
    Bug: 273710
    Change-Id: Id79dff30a1718ffc18a6470efbd308ed02fdfe18
    Reviewed-on: https://chromium-review.googlesource.com/1102515
    Commit-Queue: Camille Lamy <clamy@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571871}

--- chrome/browser/ui/extensions/settings_api_bubble_helpers.cc
+++ chrome/browser/ui/extensions/settings_api_bubble_helpers.cc
@@ -93,7 +93,7 @@ void MaybeShowExtensionControlledNewTabPage(
       browser->profile());
 
   content::NavigationEntry* entry =
-      web_contents->GetController().GetActiveEntry();
+      web_contents->GetController().GetVisibleEntry();
   if (!entry)
     return;
   GURL active_url = entry->GetURL();
--- chrome/browser/ui/views/media_router/media_router_ui_browsertest.cc
+++ chrome/browser/ui/views/media_router/media_router_ui_browsertest.cc
@@ -137,7 +137,8 @@ class MediaRouterUIBrowserTest : public InProcessBrowserTest {
         MediaRouterDialogController::GetOrCreateForWebContents(
             browser()->tab_strip_model()->GetActiveWebContents());
     content::ContextMenuParams params;
-    params.page_url = web_contents->GetController().GetActiveEntry()->GetURL();
+    params.page_url =
+        web_contents->GetController().GetLastCommittedEntry()->GetURL();
     TestRenderViewContextMenu menu(web_contents->GetMainFrame(), params);
     menu.Init();
 
