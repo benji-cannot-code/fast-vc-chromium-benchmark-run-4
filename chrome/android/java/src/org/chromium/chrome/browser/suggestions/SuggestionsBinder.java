d7d0a968f9520c73d309edfe946e5c6486e1fe63
Theresa <twellington@chromium.org>
2018-07-02 17:15:20 +0000

    [EoC] Remove use of TintedImageView in card layout
    The use of a TintedImageView was causing crashes on older versions of
    Android when trying to mutate the state of a LayerDrawable containing a
    RoundedBitmapDrawable on older versions of Android.
    
    BUG=857408
    
    Change-Id: I81326ab571c34e10a739d5f185c9179e41faf94e
    Reviewed-on: https://chromium-review.googlesource.com/1122853
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571929}

--- chrome/android/java/src/org/chromium/chrome/browser/suggestions/SuggestionsBinder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/suggestions/SuggestionsBinder.java
@@ -60,7 +60,10 @@ public class SuggestionsBinder {
     private final @Nullable TextView mSnippetTextView;
     private final TextView mPublisherTextView;
     private final TextView mAgeTextView;
-    private final TintedImageView mThumbnailView;
+    // TODO(twellington): Try to change this back to a TintedImageView. This was changed for a crash
+    // in contextual suggestions that occurs when trying to mutate state when tinting a
+    // LayerDrawable that contains a RoundedBitmapDrawable on older versions of Android.
+    private final ImageView mThumbnailView;
     private final @Nullable ImageView mVideoBadge;
     private final ImageView mOfflineBadge;
     private final @Nullable ImageView mOfflineBadgePublisherRow;
@@ -253,7 +256,7 @@ public class SuggestionsBinder {
         } else {
             mThumbnailView.setImageResource(R.drawable.ic_snippet_thumbnail_placeholder);
         }
-        mThumbnailView.setTint(null);
+        if (!mIsContextual) ((TintedImageView) mThumbnailView).setTint(null);
 
         // Fetch thumbnail for the current article.
         mImageFetcher.makeArticleThumbnailRequest(
@@ -301,7 +304,7 @@ public class SuggestionsBinder {
         mThumbnailView.setScaleType(ImageView.ScaleType.CENTER_CROP);
         mThumbnailView.setBackground(null);
         mThumbnailView.setImageDrawable(thumbnail);
-        mThumbnailView.setTint(null);
+        if (!mIsContextual) ((TintedImageView) mThumbnailView).setTint(null);
     }
 
     private void setThumbnailFromFileType(@DownloadFilter.Type int fileType) {
@@ -313,7 +316,7 @@ public class SuggestionsBinder {
         mThumbnailView.setBackgroundColor(iconBackgroundColor);
         mThumbnailView.setImageResource(
                 DownloadUtils.getIconResId(fileType, DownloadUtils.ICON_SIZE_36_DP));
-        mThumbnailView.setTint(iconForegroundColorList);
+        if (!mIsContextual) ((TintedImageView) mThumbnailView).setTint(iconForegroundColorList);
     }
 
     private void setDefaultFaviconOnView(int faviconSizePx) {
@@ -342,7 +345,7 @@ public class SuggestionsBinder {
 
         mThumbnailView.setScaleType(ImageView.ScaleType.CENTER_CROP);
         mThumbnailView.setBackground(null);
-        mThumbnailView.setTint(null);
+        if (!mIsContextual) ((TintedImageView) mThumbnailView).setTint(null);
         int duration = (int) (FADE_IN_ANIMATION_TIME_MS
                 * ChromeAnimation.Animation.getAnimationMultiplier());
         if (duration == 0) {
