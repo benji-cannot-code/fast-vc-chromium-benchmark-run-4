0d8ae9321d208f8807145d48509cff9f4703dd22
Yi Su <mrsuyi@google.com>
2018-07-02 08:10:13 +0000

    Use more precise scaling numbers for text size accessibility feature.
    Initial version of text size accessibility feature use some arbitrary
    scaling numbers for adjusting text size. This CL substitutes those with
    more precise numbers calculated by fonts' point sizes of text categories.
    
    Bug: 836962
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Id444031cbccfea40c2269792aeef1a3ee8df8670
    Reviewed-on: https://chromium-review.googlesource.com/1121236
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Yi Su <mrsuyi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571836}

--- ios/chrome/browser/web/font_size_tab_helper_unittest.mm
+++ ios/chrome/browser/web/font_size_tab_helper_unittest.mm
@@ -70,7 +70,7 @@ TEST_F(FontSizeTabHelperTest, PageLoadedWithDefaultFontSize) {
   preferred_content_size_category_ = UIContentSizeCategoryExtraLarge;
   SendUIContentSizeCategoryDidChangeNotification();
   last_executed_js = base::UTF16ToUTF8(web_state_.GetLastExecutedJavascript());
-  EXPECT_EQ("__gCrWeb.accessibility.adjustFontSize(110)", last_executed_js);
+  EXPECT_EQ("__gCrWeb.accessibility.adjustFontSize(112)", last_executed_js);
   histogram_tester_.ExpectUniqueSample(
       "Accessibility.iOS.NewLargerTextCategory", false, 2);
   web_state_.ClearLastExecutedJavascript();
@@ -78,7 +78,7 @@ TEST_F(FontSizeTabHelperTest, PageLoadedWithDefaultFontSize) {
   // Reload web page.
   web_state_.OnPageLoaded(web::PageLoadCompletionStatus::SUCCESS);
   last_executed_js = base::UTF16ToUTF8(web_state_.GetLastExecutedJavascript());
-  EXPECT_EQ("__gCrWeb.accessibility.adjustFontSize(110)", last_executed_js);
+  EXPECT_EQ("__gCrWeb.accessibility.adjustFontSize(112)", last_executed_js);
   histogram_tester_.ExpectUniqueSample(
       "Accessibility.iOS.NewLargerTextCategory", false, 3);
 }
@@ -92,7 +92,7 @@ TEST_F(FontSizeTabHelperTest, PageLoadedWithExtraLargeFontSize) {
   // Load web page.
   web_state_.OnPageLoaded(web::PageLoadCompletionStatus::SUCCESS);
   last_executed_js = base::UTF16ToUTF8(web_state_.GetLastExecutedJavascript());
-  EXPECT_EQ("__gCrWeb.accessibility.adjustFontSize(110)", last_executed_js);
+  EXPECT_EQ("__gCrWeb.accessibility.adjustFontSize(112)", last_executed_js);
   histogram_tester_.ExpectUniqueSample(
       "Accessibility.iOS.NewLargerTextCategory", false, 1);
   web_state_.ClearLastExecutedJavascript();
@@ -102,7 +102,7 @@ TEST_F(FontSizeTabHelperTest, PageLoadedWithExtraLargeFontSize) {
   preferred_content_size_category_ = UIContentSizeCategoryExtraExtraLarge;
   SendUIContentSizeCategoryDidChangeNotification();
   last_executed_js = base::UTF16ToUTF8(web_state_.GetLastExecutedJavascript());
-  EXPECT_EQ("__gCrWeb.accessibility.adjustFontSize(120)", last_executed_js);
+  EXPECT_EQ("__gCrWeb.accessibility.adjustFontSize(124)", last_executed_js);
   histogram_tester_.ExpectUniqueSample(
       "Accessibility.iOS.NewLargerTextCategory", false, 2);
   web_state_.ClearLastExecutedJavascript();
@@ -110,7 +110,7 @@ TEST_F(FontSizeTabHelperTest, PageLoadedWithExtraLargeFontSize) {
   // Reload web page.
   web_state_.OnPageLoaded(web::PageLoadCompletionStatus::SUCCESS);
   last_executed_js = base::UTF16ToUTF8(web_state_.GetLastExecutedJavascript());
-  EXPECT_EQ("__gCrWeb.accessibility.adjustFontSize(120)", last_executed_js);
+  EXPECT_EQ("__gCrWeb.accessibility.adjustFontSize(124)", last_executed_js);
   histogram_tester_.ExpectUniqueSample(
       "Accessibility.iOS.NewLargerTextCategory", false, 3);
 }
@@ -134,7 +134,7 @@ TEST_F(FontSizeTabHelperTest, PageLoadedWithUnrecognizableFontSize) {
   preferred_content_size_category_ = UIContentSizeCategoryExtraExtraLarge;
   SendUIContentSizeCategoryDidChangeNotification();
   last_executed_js = base::UTF16ToUTF8(web_state_.GetLastExecutedJavascript());
-  EXPECT_EQ("__gCrWeb.accessibility.adjustFontSize(120)", last_executed_js);
+  EXPECT_EQ("__gCrWeb.accessibility.adjustFontSize(124)", last_executed_js);
   histogram_tester_.ExpectBucketCount("Accessibility.iOS.NewLargerTextCategory",
                                       false, 1);
   web_state_.ClearLastExecutedJavascript();
@@ -142,7 +142,7 @@ TEST_F(FontSizeTabHelperTest, PageLoadedWithUnrecognizableFontSize) {
   // Reload web page.
   web_state_.OnPageLoaded(web::PageLoadCompletionStatus::SUCCESS);
   last_executed_js = base::UTF16ToUTF8(web_state_.GetLastExecutedJavascript());
-  EXPECT_EQ("__gCrWeb.accessibility.adjustFontSize(120)", last_executed_js);
+  EXPECT_EQ("__gCrWeb.accessibility.adjustFontSize(124)", last_executed_js);
   histogram_tester_.ExpectBucketCount("Accessibility.iOS.NewLargerTextCategory",
                                       false, 2);
 }
