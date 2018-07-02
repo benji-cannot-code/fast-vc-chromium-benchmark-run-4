65610d239303a5712d0a260164e798fce02ce848
junweifu <junwei.fu@intel.com>
2018-07-02 07:05:35 +0000

    Reland "ShapeDetection: Start the minimum implementation of mojom::BarcodeDetection"
    Original CL got reverted because the Vision.Framework isn't loaded in services
    unit tests.
    
    This is a reland of 4db64ed1349007e15544cd855659c91d7147a2ad
    
    Original change's description:
    > ShapeDetection: Start the minimum implementation of mojom::BarcodeDetection
    >
    > The new implementation is based on Mac OS Vision Framework[1] which recognizes
    > more barcode symbologies, then use the @available(macOS 10.13, *) in the barcode
    > detection provider to choose different implementation.
    >
    > Split original large CL[2] up in smaller subpatches including this CL.
    >
    > [1] https://developer.apple.com/documentation/vision
    > [2] https://chromium-review.googlesource.com/c/chromium/src/+/1088466
    >
    > BUG=848182
    >
    > Cq-Include-Trybots: luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win10_chromium_x64_rel_ng
    > Change-Id: I5cb8634240a35e52b3c0b696c1ccab9c24cd023e
    > Reviewed-on: https://chromium-review.googlesource.com/1107493
    > Commit-Queue: Junwei Fu <junwei.fu@intel.com>
    > Reviewed-by: Miguel Casas <mcasas@chromium.org>
    > Reviewed-by: Reilly Grant <reillyg@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#570336}
    
    Bug: 848182
    Change-Id: I143f1c2b1977690d50aa4bf4b31d5eda1cb92ece
    Cq-Include-Trybots: luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win10_chromium_x64_rel_ng
    Reviewed-on: https://chromium-review.googlesource.com/1116258
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Junwei Fu <junwei.fu@intel.com>
    Cr-Commit-Position: refs/heads/master@{#571830}

--- services/shape_detection/face_detection_impl_mac_unittest.mm
+++ services/shape_detection/face_detection_impl_mac_unittest.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/shape_detection/face_detection_impl_mac.h"
 
+#include <dlfcn.h>
 #include <memory>
 #include <utility>
 
@@ -85,6 +86,20 @@ class FaceDetectionImplMacTest : public TestWithParam<struct TestParams> {
  public:
   ~FaceDetectionImplMacTest() override {}
 
+  void SetUp() override {
+    if (@available(macOS 10.13, *)) {
+      vision_framework_ = dlopen(
+          "/System/Library/Frameworks/Vision.framework/Vision", RTLD_LAZY);
+    }
+  }
+
+  void TearDown() override {
+    if (@available(macOS 10.13, *)) {
+      if (vision_framework_)
+        dlclose(vision_framework_);
+    }
+  }
+
   void DetectCallback(size_t num_faces,
                       size_t num_landmarks,
                       size_t num_mouth_points,
@@ -103,6 +118,7 @@ class FaceDetectionImplMacTest : public TestWithParam<struct TestParams> {
 
   std::unique_ptr<mojom::FaceDetection> impl_;
   const base::MessageLoop message_loop_;
+  void* vision_framework_;
 };
 
 TEST_P(FaceDetectionImplMacTest, CreateAndDestroy) {
