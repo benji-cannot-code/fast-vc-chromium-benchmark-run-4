e4c542760193375b9697856541d916b8b6e21c12
Victor Vasiliev <vasilvv@chromium.org>
2018-07-02 18:47:41 +0000

    Replace quic_trace.proto with a copy from GitHub
    The copy on GitHub is a canonical source of truth.  Originally, that
    file was in Chromium source, but I was aware of third parties that were
    interested in embedding it, so I moved it out to a separate repository.
    It's currently only used in the unit tests, though it's possible it will
    be used in stand-alone tool binaries in the future.
    
    Change-Id: I74be47f2d317c949732eec44c432a36610650938
    Reviewed-on: https://chromium-review.googlesource.com/1086967
    Commit-Queue: Victor Vasiliev <vasilvv@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Zhongyi Shi <zhongyi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571954}

--- net/third_party/quic/core/quic_trace_visitor.h
+++ net/third_party/quic/core/quic_trace_visitor.h
@@ -6,9 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NET_THIRD_PARTY_QUIC_CORE_QUIC_TRACE_VISITOR_H_
 #define NET_THIRD_PARTY_QUIC_CORE_QUIC_TRACE_VISITOR_H_
 
-#include "net/third_party/quic/core/proto/quic_trace.pb.h"
 #include "net/third_party/quic/core/quic_connection.h"
 #include "net/third_party/quic/core/quic_types.h"
+#include "third_party/quic_trace/lib/quic_trace.pb.h"
 
 namespace quic {
 
---
+++ third_party/quic_trace/append_lite_runtime.py
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/env python
+# Copyright (c) 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""
+Appends the LITE_RUNTIME annotation to a proto2 protocol buffer file.
+"""
+
+import sys
+
+if len(sys.argv) != 3:
+  sys.stderr.write("Usage: append_lite_runtime.py in.proto out.proto\n")
+  sys.exit(1)
+
+with open(sys.argv[1], "rb") as source:
+  with open(sys.argv[2], "wb") as output:
+    for line in source:
+      output.write(line)
+      if line.strip() == 'syntax = "proto2";':
+        output.write("option optimize_for = LITE_RUNTIME;\n")
