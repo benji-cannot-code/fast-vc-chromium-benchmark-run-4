4edf92112cfbd27c4d995f5a88cd0378740967c8
Alex Newcomer <newcomer@chromium.org>
2018-07-02 21:01:06 +0000

    Cros: Make notifications in context menus swipe/click-able
    Add a NotificationItemView::NotificationItemViewDelegate, which:
     - Activates notifications.
     - Acts as the SlideOutController::Delegate.
     - Allows for easier testing of NotificationMenuView.
    
    Make NotificationMenuController a NotificationItemViewDelegate.
    
    Pass NotificationMenuController AppMenuModelAdapter:
     - Allows NotificationMenuController to close the menu.
    
    Also added tests.
    
    Bug: 843761
    Change-Id: I20474838a0fb5360196a3d0585b7e84620b86e9f
    Reviewed-on: https://chromium-review.googlesource.com/1120842
    Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571993}

--- ash/app_menu/notification_item_view.cc
+++ ash/app_menu/notification_item_view.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/image/image.h"
 #include "ui/gfx/text_elider.h"
 #include "ui/message_center/views/proportional_image_view.h"
+#include "ui/views/background.h"
 #include "ui/views/border.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/controls/menu/menu_config.h"
@@ -39,14 +40,31 @@ constexpr SkColor kNotificationTitleTextColor =
 
 }  // namespace
 
-NotificationItemView::NotificationItemView(const base::string16& title,
-                                           const base::string16& message,
-                                           const gfx::Image& icon,
-                                           const std::string notification_id)
-    : title_(title), message_(message), notification_id_(notification_id) {
+NotificationItemView::NotificationItemView(
+    NotificationItemView::Delegate* delegate,
+    message_center::SlideOutController::Delegate* slide_out_controller_delegate,
+    const base::string16& title,
+    const base::string16& message,
+    const gfx::Image& icon,
+    const std::string& notification_id)
+    : delegate_(delegate),
+      slide_out_controller_(
+          std::make_unique<message_center::SlideOutController>(
+              this,
+              slide_out_controller_delegate)),
+      title_(title),
+      message_(message),
+      notification_id_(notification_id) {
+  DCHECK(delegate_);
+  DCHECK(slide_out_controller_delegate);
+
+  // Paint to a new layer so |slide_out_controller_| can control the opacity.
+  SetPaintToLayer();
+  layer()->SetFillsBoundsOpaquely(true);
   SetBorder(views::CreateEmptyBorder(
       gfx::Insets(kNotificationVerticalPadding, kNotificationHorizontalPadding,
                   kNotificationVerticalPadding, kIconHorizontalPadding)));
+  SetBackground(views::CreateSolidBackground(SK_ColorWHITE));
 
   text_container_ = new views::View();
   text_container_->SetLayoutManager(std::make_unique<views::BoxLayout>(
@@ -99,6 +117,37 @@ void NotificationItemView::Layout() {
       width() - insets.right() - kProportionalIconViewSize.width(),
       insets.top() + kIconVerticalPadding, kProportionalIconViewSize.width(),
       kProportionalIconViewSize.height());
+}
+
+bool NotificationItemView::OnMousePressed(const ui::MouseEvent& event) {
+  return true;
+}
+
+bool NotificationItemView::OnMouseDragged(const ui::MouseEvent& event) {
+  return true;
+}
+
+void NotificationItemView::OnMouseReleased(const ui::MouseEvent& event) {
+  gfx::Point location(event.location());
+  views::View::ConvertPointToScreen(this, &location);
+  if (!event.IsOnlyLeftMouseButton() ||
+      !GetBoundsInScreen().Contains(location)) {
+    return;
+  }
+
+  delegate_->ActivateNotificationAndClose(notification_id_);
+}
+
+void NotificationItemView::OnGestureEvent(ui::GestureEvent* event) {
+  // Drag gestures are handled by |slide_out_controller_|.
+  switch (event->type()) {
+    case ui::ET_GESTURE_TAP:
+      event->SetHandled();
+      delegate_->ActivateNotificationAndClose(notification_id_);
+      return;
+    default:
+      return;
+  }
 }
 
 }  // namespace ash
--- ash/app_menu/notification_menu_view.cc
+++ ash/app_menu/notification_menu_view.cc
@@ -16,10 +16,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
-NotificationMenuView::NotificationMenuView(const std::string& app_id)
-    : app_id_(app_id) {
+NotificationMenuView::NotificationMenuView(
+
+    NotificationItemView::Delegate* notification_item_view_delegate,
+    message_center::SlideOutController::Delegate* slide_out_controller_delegate,
+    const std::string& app_id)
+    : app_id_(app_id),
+      notification_item_view_delegate_(notification_item_view_delegate),
+      slide_out_controller_delegate_(slide_out_controller_delegate) {
+  DCHECK(notification_item_view_delegate_);
+  DCHECK(slide_out_controller_delegate_);
   DCHECK(!app_id_.empty())
       << "Only context menus for applications can show notifications.";
+
   SetLayoutManager(std::make_unique<views::BoxLayout>(
       views::BoxLayout::Orientation::kVertical));
 
@@ -47,6 +56,7 @@ void NotificationMenuView::AddNotificationItemView(
     RemoveChildView(notification_item_views_.front().get());
 
   notification_item_views_.push_front(std::make_unique<NotificationItemView>(
+      notification_item_view_delegate_, slide_out_controller_delegate_,
       notification.title(), notification.message(), notification.icon(),
       notification.id()));
   notification_item_views_.front()->set_owned_by_client();
@@ -73,6 +83,17 @@ void NotificationMenuView::RemoveNotificationItemView(
   // Replace the displayed view, if it is already being shown this is a no-op.
   if (!notification_item_views_.empty())
     AddChildView(notification_item_views_.front().get());
+}
+
+ui::Layer* NotificationMenuView::GetSlideOutLayer() {
+  if (notification_item_views_.empty())
+    return nullptr;
+  return notification_item_views_.front()->layer();
+}
+
+const std::string& NotificationMenuView::GetDisplayedNotificationID() {
+  DCHECK(!notification_item_views_.empty());
+  return notification_item_views_.front()->notification_id();
 }
 
 }  // namespace ash
