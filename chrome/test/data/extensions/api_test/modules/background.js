9f32c5fb3799b858230f3d09f5b3f9797ac9af63
Devlin Cronin <rdevlin.cronin@chromium.org>
2018-07-02 16:41:51 +0000

    [Extensions + Net] Add .mjs to MIME type mappings
    Add .mjs to type mappings for the javascript MIME type. This is
    necessary for .mjs to be recognized as a valid for use in modules
    when served by the extension protocols, where the mime type is
    determined by our net code (as opposed to a server).
    
    Update the extension API test for modules to include a .mjs file test.
    
    Bug: 857076
    Change-Id: Ib14cabbc52057cfb57c933de5a9c3ed4b13b87d2
    Reviewed-on: https://chromium-review.googlesource.com/1117203
    Reviewed-by: Asanka Herath <asanka@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571921}

--- chrome/test/data/extensions/api_test/modules/background.js
+++ chrome/test/data/extensions/api_test/modules/background.js
@@ -6,6 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // TODO(devlin): Sadly, there's no good way of checking that the import
 // succeeds, so in the failure case the test just times out, rather than
 // giving a useful error.
-import {pass} from '/module.js';
+import {pass as jsPass} from '/module.js';
+import {pass as mjsPass} from '/module.mjs';
 
-pass();
+chrome.test.runTests([
+  function jsModule() {
+    jsPass();
+  },
+  function mjsModule() {
+    mjsPass();
+  },
+]);
