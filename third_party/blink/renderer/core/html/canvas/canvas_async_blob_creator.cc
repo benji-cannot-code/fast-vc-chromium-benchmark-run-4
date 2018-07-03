f306dd4f01049f6c3da18d0f15dc9ae2c3fed7c3
Brian Osman <brianosman@google.com>
2018-07-03 18:03:00 +0000

    Remove all usage of SkTransferFunctionBehavior::kRespect
    This is going away in Skia (no one wants linear blending), so this lets
    us land some Skia changes that alter image encode/decode/conversion
    behavior without breaking webkit unit tests.
    
    Some canvas color space layout tests do break, because they exercise
    multiple code paths in Skia. The paths that depend on image encode
    and decode are now using nonlinear blending, but the paths that just
    draw (to SW canvas) are hitting code that isn't updated yet. To make
    it easier to finish that migration, this change suppresses all of the
    transparent pixel tests within those five layout tests.
    
    Bug: chromium:859102 chromium:844145
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I331b5dfc08e53d498fdc1449519b42f67de6d1af
    Reviewed-on: https://chromium-review.googlesource.com/1117026
    Reviewed-by: Mike Klein <mtklein@chromium.org>
    Reviewed-by: Mohammad Reza Zakerinasab <zakerinasab@chromium.org>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Commit-Queue: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572292}

--- third_party/blink/renderer/core/html/canvas/canvas_async_blob_creator.cc
+++ third_party/blink/renderer/core/html/canvas/canvas_async_blob_creator.cc
@@ -252,7 +252,7 @@ CanvasAsyncBlobCreator::CanvasAsyncBlobCreator(
       }
       DCHECK(skia_image->colorSpace());
       skia_image = skia_image->makeColorSpace(
-          blob_color_space, SkTransferFunctionBehavior::kRespect);
+          blob_color_space, SkTransferFunctionBehavior::kIgnore);
       image_ = StaticBitmapImage::Create(skia_image);
     }
 
@@ -336,7 +336,7 @@ bool CanvasAsyncBlobCreator::EncodeImage(const double& quality) {
   SkTransferFunctionBehavior transfer_fn_behavior =
       SkTransferFunctionBehavior::kIgnore;
   if (function_type_ == kHTMLCanvasConvertToBlobPromise)
-    transfer_fn_behavior = SkTransferFunctionBehavior::kRespect;
+    transfer_fn_behavior = SkTransferFunctionBehavior::kIgnore;
   return buffer->EncodeImage("image/webp", quality, &encoded_image_,
                              transfer_fn_behavior);
 }
@@ -659,7 +659,7 @@ bool CanvasAsyncBlobCreator::EncodeImageForConvertToBlobTest() {
     return false;
   return buffer->EncodeImage(encode_options_.type(), encode_options_.quality(),
                              &encoded_image_,
-                             SkTransferFunctionBehavior::kRespect);
+                             SkTransferFunctionBehavior::kIgnore);
 }
 
 }  // namespace blink
