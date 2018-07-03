3b72ee18c6a2a9c8740e971163796a09572cc117
Gyuyoung Kim <gyuyoung.kim@lge.com>
2018-07-03 07:40:34 +0000

    CC: Support the memory pressure handler in ResourcePool
    As StagingbufferPool, ResourcePool has been only supporting the
    MemoryCoordinator. However, it is not enabled by default. So it
    would be good if we support the memory pressure handler until the
    MemoryCoordinator is enabled by default. Especially, it will help
    low-end devices to reduce memory usage under the out-of-memory situation.
    
    Bug: 856527
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I070e71c3d39da61b56f2b11d51bf5efdf1ac3c7c
    Reviewed-on: https://chromium-review.googlesource.com/1121949
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung.kim@lge.com>
    Cr-Commit-Position: refs/heads/master@{#572137}

--- cc/resources/resource_pool.h
+++ cc/resources/resource_pool.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/circular_deque.h"
 #include "base/macros.h"
 #include "base/memory/memory_coordinator_client.h"
+#include "base/memory/memory_pressure_listener.h"
 #include "base/memory/weak_ptr.h"
 #include "base/optional.h"
 #include "base/trace_event/memory_allocator_dump_guid.h"
@@ -227,6 +228,11 @@ class CC_EXPORT ResourcePool : public base::trace_event::MemoryDumpProvider,
   void OnPurgeMemory() override;
   void OnMemoryStateChange(base::MemoryState state) override;
 
+  // TODO(gyuyoung): OnMemoryPressure is deprecated. So this should be removed
+  // when the memory coordinator is enabled by default.
+  void OnMemoryPressure(
+      base::MemoryPressureListener::MemoryPressureLevel level);
+
   size_t GetTotalMemoryUsageForTesting() const {
     return total_memory_usage_bytes_;
   }
@@ -370,6 +376,8 @@ class CC_EXPORT ResourcePool : public base::trace_event::MemoryDumpProvider,
 
   // Map from the PoolResource |unique_id| to the PoolResource.
   std::map<size_t, std::unique_ptr<PoolResource>> in_use_resources_;
+
+  std::unique_ptr<base::MemoryPressureListener> memory_pressure_listener_;
 
   base::WeakPtrFactory<ResourcePool> weak_ptr_factory_;
 
