671df341ab1a123a24aa6f7c6def92ed8bad553f
Yoichi Osato <yoichio@chromium.org>
2018-07-03 07:44:18 +0000

    [LayoutNG]: Paint document markers on vertical-lr.
    This patch fixes document marker painting on writing-mode:vertical-lr.
    
    We paint document markers at 2 phases: background and foreground.
    At the background phase, we paint find-in-page highlight or so on. This is done
    before graphics context rotation following writing-mode.
    At the foreground phase, we paint spellcheck markers. This is done
    after the rotation.
    
    Since the background phase painting needs only physical rect of target range on
    the text fragment. It has worked.
    However foreground painting needs a rotated rect because it is after the rotation.
    
    This patch fixes the foreground painting by adding MarkerRectForForeground
    function that returns layout rect.
    
    Bug: 850448
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng;luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: If6e401028536cc5412be420f6600b978677af98a
    Reviewed-on: https://chromium-review.googlesource.com/1109663
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572139}

--- third_party/blink/renderer/core/paint/ng/ng_text_fragment_painter.cc
+++ third_party/blink/renderer/core/paint/ng/ng_text_fragment_painter.cc
@@ -11,8 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/editing/markers/document_marker_controller.h"
 #include "third_party/blink/renderer/core/editing/markers/text_match_marker.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
+#include "third_party/blink/renderer/core/layout/ng/geometry/ng_logical_rect.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_offset_mapping.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_physical_text_fragment.h"
+#include "third_party/blink/renderer/core/layout/ng/inline/ng_text_fragment.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_physical_box_fragment.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_text_decoration_offset.h"
 #include "third_party/blink/renderer/core/paint/document_marker_painter.h"
@@ -84,18 +86,6 @@ DocumentMarkerVector ComputeMarkersToPaint(
   return document_marker_controller.ComputeMarkersToPaint(*node);
 }
 
-void PaintStyleableMarkerUnderline(GraphicsContext& context,
-                                   const LayoutPoint& box_origin,
-                                   const StyleableMarker& marker,
-                                   const ComputedStyle& style,
-                                   const NGPhysicalOffsetRect& local_rect) {
-  const SimpleFontData* font_data = style.GetFont().PrimaryFont();
-  DCHECK(font_data);
-  DocumentMarkerPainter::PaintStyleableMarkerUnderline(
-      context, box_origin, marker, style, local_rect.ToFloatRect(),
-      LayoutUnit(font_data->GetFontMetrics().Height()));
-}
-
 unsigned GetTextContentOffset(const Text& text, unsigned offset) {
   const Position position(text, offset);
   const NGOffsetMapping* const offset_mapping =
@@ -136,6 +126,20 @@ void PaintRect(GraphicsContext& context,
   context.FillRect(global_rect.ToFloatRect(), color);
 }
 
+LayoutRect MarkerRectForForeground(const NGPhysicalTextFragment& text_fragment,
+                                   unsigned start_offset,
+                                   unsigned end_offset) {
+  LayoutUnit start_position, end_position;
+  std::tie(start_position, end_position) =
+      text_fragment.LineLeftAndRightForOffsets(start_offset, end_offset);
+
+  const LayoutUnit height = text_fragment.Size()
+                                .ConvertToLogical(static_cast<WritingMode>(
+                                    text_fragment.Style().GetWritingMode()))
+                                .block_size;
+  return {start_position, LayoutUnit(), end_position - start_position, height};
+}
+
 // Copied from InlineTextBoxPainter
 void PaintDocumentMarkers(GraphicsContext& context,
                           const NGPaintFragment& paint_fragment,
@@ -172,8 +176,8 @@ void PaintDocumentMarkers(GraphicsContext& context,
           continue;
         DocumentMarkerPainter::PaintDocumentMarker(
             context, box_origin, style, marker->GetType(),
-            text_fragment.LocalRect(paint_start_offset, paint_end_offset)
-                .ToLayoutRect());
+            MarkerRectForForeground(text_fragment, paint_start_offset,
+                                    paint_end_offset));
       } break;
 
       case DocumentMarker::kTextMatch: {
@@ -216,12 +220,12 @@ void PaintDocumentMarkers(GraphicsContext& context,
               styleable_marker.BackgroundColor());
           break;
         }
-        // TODO(yoichio) We call this on vertical/horizontal flipped context.
-        // Since NGPhysicalTextFragment::LocalRect returns physical rect,
-        // we need to adapt it.
-        PaintStyleableMarkerUnderline(
+        const SimpleFontData* font_data = style.GetFont().PrimaryFont();
+        DocumentMarkerPainter::PaintStyleableMarkerUnderline(
             context, box_origin, styleable_marker, style,
-            text_fragment.LocalRect(paint_start_offset, paint_end_offset));
+            FloatRect(MarkerRectForForeground(text_fragment, paint_start_offset,
+                                              paint_end_offset)),
+            LayoutUnit(font_data->GetFontMetrics().Height()));
       } break;
 
       default:
