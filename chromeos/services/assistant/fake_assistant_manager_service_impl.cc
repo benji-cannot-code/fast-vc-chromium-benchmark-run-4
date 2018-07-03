a44c0ff586de1fd6a41b889f57926b264b71da5b
Yue Li <updowndota@chromium.org>
2018-07-03 22:14:37 +0000

    Refactor constructor of AssistantManagerService
    Pass a Service pointer with Getters instead of pointers for each interface
    
    Bug: None
    Test: Manual Test
    Change-Id: I4b32aabbabda94b74f699cb7093708ead1bc6fa6
    Reviewed-on: https://chromium-review.googlesource.com/1123371
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572369}

--- chromeos/services/assistant/fake_assistant_manager_service_impl.cc
+++ chromeos/services/assistant/fake_assistant_manager_service_impl.cc
@@ -56,8 +56,5 @@ void FakeAssistantManagerServiceImpl::SendTextQuery(const std::string& query) {}
 void FakeAssistantManagerServiceImpl::AddAssistantEventSubscriber(
     mojom::AssistantEventSubscriberPtr subscriber) {}
 
-void FakeAssistantManagerServiceImpl::SetAssistantController(
-    ash::mojom::AssistantController* controller) {}
-
 }  // namespace assistant
 }  // namespace chromeos
