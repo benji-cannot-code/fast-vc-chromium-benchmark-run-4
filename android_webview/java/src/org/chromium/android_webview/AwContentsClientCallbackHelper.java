defdb3517c457d975a39e8f8ffb727f34be777cd
Nate Fischer <ntfschr@chromium.org>
2018-07-03 20:43:55 +0000

    AW: remove unnecessary trace events
    No change to logic.
    
    These trace events were added to help investigate the 8 ms block which
    wound up caused by class verification. This CL removes these events,
    because other trace events (which we intentionally leave in the code)
    are more helpful at diagnosing the issue.
    
    R=torne@chromium.org
    
    Bug: 838702
    Test: N/A
    Change-Id: Ib0e405fb78df12b159e3bbbf242820650648ebfd
    Reviewed-on: https://chromium-review.googlesource.com/1123464
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572343}

--- android_webview/java/src/org/chromium/android_webview/AwContentsClientCallbackHelper.java
+++ android_webview/java/src/org/chromium/android_webview/AwContentsClientCallbackHelper.java
@@ -256,11 +256,8 @@ public class AwContentsClientCallbackHelper {
     }
 
     public AwContentsClientCallbackHelper(Looper looper, AwContentsClient contentsClient) {
-        try (ScopedSysTraceEvent e =
-                        ScopedSysTraceEvent.scoped("AwContentsClientCallbackHelper.constructor")) {
-            mHandler = new MyHandler(looper);
-            mContentsClient = contentsClient;
-        }
+        mHandler = new MyHandler(looper);
+        mContentsClient = contentsClient;
     }
 
     // Public for tests.
