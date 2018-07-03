233ebc41a0d3b6dd0037451c414383b1c02dd04f
GauthamBanasandra <gautham.bangalore@gmail.com>
2018-07-03 22:31:13 +0000

    DevTools: Implement a reliable check for isNodeJS()
    The isNodeJS() method validates whether target
    has only SDK.Target.Capability.JS set. This
    patch makes it more explicit and resolves the
    existing TODO in the isNodeJS() method.
    
    The creator of the target must now explicitly
    mark the target as Node.js by calling
    markAsNodeJS() on the target object, thus
    avoiding the dependency on SDK.Target.Capability.JS.
    
    Change-Id: I4da9af58ddc42eba2b6b91f7b1b81b411180256b
    Reviewed-on: https://chromium-review.googlesource.com/1116761
    Commit-Queue: Andrey Lushnikov <lushnikov@chromium.org>
    Reviewed-by: Andrey Lushnikov <lushnikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572375}

--- third_party/WebKit/LayoutTests/http/tests/devtools/persistence/persistence-sync-content-nodejs.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/persistence/persistence-sync-content-nodejs.js
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   var testMapping = BindingsTestRunner.initializeTestMapping();
   // Pretend we are running under V8 front-end.
-  SDK.targetManager.mainTarget().setIsNodeJSForTest();
+  SDK.targetManager.mainTarget().markAsNodeJS();
 
   var content = ['', '', 'var express = require("express");', '//TODO'].join('\n');
 
