ef923d4b6387d3a0615f6e591bd9dcf46ba06e16
Peter E Conn <peconn@chromium.org>
2018-07-03 14:04:16 +0000

    Revert "Don't show top toolbar items when bottom toolbar is enabled"
    This reverts commit e83d1f12d67bfacbc9a6bde53593302326176817.
    
    Reason for revert: https://crbug.com/857437
    
    Essentially, as well as adding if statements to all usages of mMenuButton within the class, you need to add if statements to all other classes calling 'getMenuButton()' (and ToolbarManager.getMenuButton() transitively).
    
    Unfortunately this isn't so trivial for all the cases (eg, I'm guessing we still want to show the data saver when this feature is enabled, or the download text bubble?), so I'm reverting instead of attempting and TBRing a fix myself. Sorry!
    
    Original change's description:
    > Don't show top toolbar items when bottom toolbar is enabled
    > 
    > Remove the top toolbar buttons (home, tab switcher, and menu buttons)
    > from their parent view and set them to null. Also add a bunch of
    > null checks since now things can be null.
    > 
    > Bug: 852117
    > Change-Id: I620f35f81388d1f4ac8791a9eb45576566474604
    > Reviewed-on: https://chromium-review.googlesource.com/1105569
    > Commit-Queue: Pedro Amaral <amaralp@chromium.org>
    > Reviewed-by: Ted Choc <tedchoc@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#570578}
    
    TBR=tedchoc@chromium.org,amaralp@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 852117
    Change-Id: I2c16533f8dec11c5d11b920693157735e7438e89
    Reviewed-on: https://chromium-review.googlesource.com/1124579
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572193}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarLayout.java
@@ -24,7 +24,6 @@ import android.util.AttributeSet;
 import android.view.InputDevice;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
@@ -43,7 +42,6 @@ import org.chromium.chrome.browser.omnibox.UrlBarData;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
-import org.chromium.chrome.browser.util.FeatureUtilities;
 import org.chromium.chrome.browser.util.ViewUtils;
 import org.chromium.chrome.browser.widget.PulseDrawable;
 import org.chromium.chrome.browser.widget.ScrimView;
@@ -158,12 +156,6 @@ public abstract class ToolbarLayout extends FrameLayout implements Toolbar {
         mMenuButton = (TintedImageButton) findViewById(R.id.menu_button);
         mMenuBadge = (ImageView) findViewById(R.id.menu_badge);
         mMenuButtonWrapper = findViewById(R.id.menu_button_wrapper);
-        if (FeatureUtilities.isBottomToolbarEnabled()) {
-            if (mMenuButtonWrapper != null) UiUtils.removeViewFromParent(mMenuButtonWrapper);
-            mMenuButtonWrapper = null;
-            mMenuButton = null;
-            mMenuBadge = null;
-        }
 
         // Initialize the provider to an empty version to avoid null checking everywhere.
         mToolbarDataProvider = new ToolbarDataProvider() {
@@ -280,10 +272,8 @@ public abstract class ToolbarLayout extends FrameLayout implements Toolbar {
 
         mAppMenuButtonHelper = appMenuButtonHelper;
 
-        if (mMenuButton != null) {
-            mMenuButton.setOnTouchListener(mAppMenuButtonHelper);
-            mMenuButton.setAccessibilityDelegate(mAppMenuButtonHelper);
-        }
+        mMenuButton.setOnTouchListener(mAppMenuButtonHelper);
+        mMenuButton.setAccessibilityDelegate(mAppMenuButtonHelper);
     }
 
     /** Notified that the menu was shown. */
@@ -829,7 +819,6 @@ public abstract class ToolbarLayout extends FrameLayout implements Toolbar {
 
     @Override
     public void removeAppMenuUpdateBadge(boolean animate) {
-        if (mMenuBadge == null) return;
         boolean wasShowingMenuBadge = mShowMenuBadge;
         mShowMenuBadge = false;
         setMenuButtonContentDescription(false);
@@ -888,7 +877,6 @@ public abstract class ToolbarLayout extends FrameLayout implements Toolbar {
      * bitmap.
      */
     protected void setAppMenuUpdateBadgeToVisible(boolean animate) {
-        if (mMenuBadge == null || mMenuButton == null) return;
         setMenuButtonContentDescription(true);
         if (!animate || mIsMenuBadgeAnimationRunning) {
             mMenuBadge.setVisibility(View.VISIBLE);
@@ -933,7 +921,6 @@ public abstract class ToolbarLayout extends FrameLayout implements Toolbar {
      * @param useLightDrawable Whether the light drawable should be used.
      */
     protected void setAppMenuUpdateBadgeDrawable(boolean useLightDrawable) {
-        if (mMenuBadge == null) return;
         mMenuBadge.setImageResource(useLightDrawable ? R.drawable.badge_update_light
                 : R.drawable.badge_update_dark);
     }
@@ -945,7 +932,7 @@ public abstract class ToolbarLayout extends FrameLayout implements Toolbar {
      */
     protected void setMenuButtonHighlightDrawable(boolean highlighting) {
         // Return if onFinishInflate didn't finish
-        if (mMenuButtonWrapper == null || mMenuButton == null) return;
+        if (mMenuButtonWrapper == null) return;
 
         if (highlighting) {
             if (mHighlightDrawable == null) {
@@ -967,7 +954,6 @@ public abstract class ToolbarLayout extends FrameLayout implements Toolbar {
      * @param isUpdateBadgeVisible Whether the update menu badge is visible.
      */
     protected void setMenuButtonContentDescription(boolean isUpdateBadgeVisible) {
-        if (mMenuButton == null) return;
         if (isUpdateBadgeVisible) {
             mMenuButton.setContentDescription(getResources().getString(
                     R.string.accessibility_toolbar_btn_menu_update));
