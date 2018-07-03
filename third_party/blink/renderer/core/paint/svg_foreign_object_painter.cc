521376375dc07344df961e65c98f475bc1621a4a
Chris Harrelson <chrishtr@chromium.org>
2018-07-03 14:14:11 +0000

    Don't allow subsequence caching for foreignObject under multi-col
    SVG content can't fragment (see LayoutFlowThread::LocateFlowThreadContainingBlockOf)
    but since it is painted as a replaced-normal-flow stacking context, multiple
    calls into its paint can happen anyway.
    
    Bug: 859586
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ied0b869d37373499e6a784dee8bd620d7b535fa9
    Reviewed-on: https://chromium-review.googlesource.com/1123765
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572200}

--- third_party/blink/renderer/core/paint/svg_foreign_object_painter.cc
+++ third_party/blink/renderer/core/paint/svg_foreign_object_painter.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/paint/paint_layer.h"
 #include "third_party/blink/renderer/core/paint/paint_layer_painter.h"
 #include "third_party/blink/renderer/core/paint/svg_paint_context.h"
+#include "third_party/blink/renderer/platform/graphics/paint/display_item_cache_skipper.h"
 
 namespace blink {
 
@@ -26,6 +27,14 @@ void SVGForeignObjectPainter::PaintLayer(const PaintInfo& paint_info) {
   // pre-paint has finished.
   if (!layout_svg_foreign_object_.FirstFragment().HasLocalBorderBoxProperties())
     return;
+
+  // TODO(crbug.com/797779): For now foreign object contents don't know whether
+  // they are painted in a fragmented context and may do something bad in a
+  // fragmented context, e.g. creating subsequences. Skip cache to avoid that.
+  // This will be unnecessary when the contents are fragment aware.
+  base::Optional<DisplayItemCacheSkipper> cache_skipper;
+  if (layout_svg_foreign_object_.Layer()->Parent()->EnclosingPaginationLayer())
+    cache_skipper.emplace(paint_info.context);
 
   // <foreignObject> is a replaced normal-flow stacking element.
   // See IsReplacedNormalFlowStacking in paint_layer_painter.cc.
