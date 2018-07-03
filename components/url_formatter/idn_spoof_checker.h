dd0d20a630ede942eca2b87891e36773a5365c76
git-meacer.google.com <meacer@google.com>
2018-07-03 00:33:05 +0000

    Use a huffman trie for top domain storage in url formatter
    UrlFormatter uses ICU's spoof checker to determine lookalike domains that contain unicode confusables. It does this by extracting a skeleton string from the given domain representing its visual appearance. For example, google.com and googlé[.]com have the same skeleton string (google.corn).
    
    In addition to this, we want to display a "Did you mean to go to..." UI for navigations involving IDN if the domain name matches a top 10K domain. In order to do that, we need to store the domains associated with ICU skeletons.
    
    UrlFormatter currently uses a DAFSA to store the list of the skeletons of the top 10K domains. It doesn't and cannot store the actual domain in this list. To support this, this CL changes the underlying storage from DAFSA to the Huffman Trie used by net's preload list code.
    
    It
    - Generates the huffman trie from top domain list during compile time.
    - Decodes the huffman trie during runtime in IDNSpoofChecker::SimilarToTopDomains.
    
    The design doc for the preload list migration is here: https://docs.google.com/document/d/11rqIozUDaK6DvNeu436SL3Coj65J5vhD9HVftOi-RrA/edit
    
    As mentioned in the doc, micro benchmarks indicate that the binary size and speed is minimally impacted by this change (51KB additional size, 4 microseconds of additional time for each lookup).
    
    Bug: 843361
    Change-Id: If98b8161bf836fec6ba74e68587bd2159f4eb3d5
    Reviewed-on: https://chromium-review.googlesource.com/1106539
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Nick Harper <nharper@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572055}

--- components/url_formatter/idn_spoof_checker.h
+++ components/url_formatter/idn_spoof_checker.h
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "base/strings/string16.h"
 #include "base/strings/string_piece_forward.h"
+#include "net/extras/preload_data/decoder.h"
+
 #include "third_party/icu/source/common/unicode/uniset.h"
 #include "third_party/icu/source/common/unicode/utypes.h"
 #include "third_party/icu/source/common/unicode/uversion.h"
@@ -36,6 +38,13 @@ FORWARD_DECLARE_TEST(UrlFormatterTest, IDNToUnicode);
 
 class IDNSpoofChecker {
  public:
+  struct HuffmanTrieParams {
+    const uint8_t* huffman_tree;
+    size_t huffman_tree_size;
+    const uint8_t* trie;
+    size_t trie_bits;
+    size_t trie_root_position;
+  };
   IDNSpoofChecker();
   ~IDNSpoofChecker();
 
@@ -63,8 +72,8 @@ class IDNSpoofChecker {
   bool IsMadeOfLatinAlikeCyrillic(const icu::UnicodeString& label);
 
   // Used for unit tests.
-  static void RestoreTopDomainGraphToDefault();
-  static void SetTopDomainGraph(base::StringPiece domain_graph);
+  static void SetTrieParamsForTesting(const HuffmanTrieParams& trie_params);
+  static void RestoreTrieParamsForTesting();
 
   USpoofChecker* checker_;
   icu::UnicodeSet deviation_characters_;
