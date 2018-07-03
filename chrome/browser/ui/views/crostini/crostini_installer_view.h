eb3bd66888dbaa8e024098528f2bd8aa3b93744f
Nicholas Verne <nverne@chromium.org>
2018-07-03 01:10:41 +0000

    CrostiniInstallerView:: Change magic numbers to meaningful names.
    Addresses comments from tapted after
    https://chromium-review.googlesource.com/c/chromium/src/+/1114408 was submitted.
    
    Bug: 848118
    Change-Id: I66059b005de6ede632f81ec8fa6c9c103a892051
    Reviewed-on: https://chromium-review.googlesource.com/1121937
    Commit-Queue: Nicholas Verne <nverne@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572065}

--- chrome/browser/ui/views/crostini/crostini_installer_view.h
+++ chrome/browser/ui/views/crostini/crostini_installer_view.h
@@ -51,6 +51,7 @@ class CrostiniInstallerView
   int GetDialogButtons() const override;
   base::string16 GetDialogButtonLabel(ui::DialogButton button) const override;
   bool ShouldShowCloseButton() const override;
+  bool ShouldShowWindowTitle() const override;
   bool Accept() override;
   bool Cancel() override;
   gfx::Size CalculatePreferredSize() const override;
