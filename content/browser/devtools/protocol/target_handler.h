1877dc56a5e2bc78c5cfd7a41b33c3208d172d4e
Joel Einbinder <einbinder@chromium.org>
2018-07-03 23:38:18 +0000

    DevTools: Make targetId optional for Target.getTargetInfo
    If the targetId is not specified, the info for the target of the current
    session is returned.
    
    DevTools receives a connection to a target, but does not know the
    targetId. Certain commands, particularly Target.attachToTarget, take
    a targetId. Using Target.attachToTarget will allow DevTools to create
    parallel connections to the page for a better Lighthouse experience.
    
    Change-Id: I777350d6fb486c4e5ed6256ebc0bcc1d738d0ab2
    Reviewed-on: https://chromium-review.googlesource.com/1096434
    Commit-Queue: Joel Einbinder <einbinder@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572395}

--- content/browser/devtools/protocol/target_handler.h
+++ content/browser/devtools/protocol/target_handler.h
@@ -29,7 +29,7 @@ class TargetHandler : public DevToolsDomainHandler,
                       public Target::Backend,
                       public DevToolsAgentHostObserver {
  public:
-  explicit TargetHandler(bool browser_only);
+  TargetHandler(bool browser_only, const std::string& owner_target_id);
   ~TargetHandler() override;
 
   static std::vector<TargetHandler*> ForAgentHost(DevToolsAgentHostImpl* host);
@@ -58,7 +58,7 @@ class TargetHandler : public DevToolsDomainHandler,
                                Maybe<std::string> session_id,
                                Maybe<std::string> target_id) override;
   Response GetTargetInfo(
-      const std::string& target_id,
+      Maybe<std::string> target_id,
       std::unique_ptr<Target::TargetInfo>* target_info) override;
   Response ActivateTarget(const std::string& target_id) override;
   Response CloseTarget(const std::string& target_id,
@@ -109,6 +109,7 @@ class TargetHandler : public DevToolsDomainHandler,
   std::set<DevToolsAgentHost*> reported_hosts_;
   int last_session_id_ = 0;
   bool browser_only_;
+  std::string owner_target_id_;
   base::flat_set<Throttle*> throttles_;
   base::WeakPtrFactory<TargetHandler> weak_factory_;
 
