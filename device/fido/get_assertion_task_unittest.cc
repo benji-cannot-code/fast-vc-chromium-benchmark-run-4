b754bec9cf5df360569bd0d49de2f7cdbb75d1fb
Jun Choi <hongjunchoi@chromium.org>
2018-07-03 18:22:58 +0000

    Implement sign for VirtualCtap2Device
    Implement GetAssertion operation for CTAP2 software key. The implement
    GetAssertionOperation does not support multiple accounts per device and
    never returns UserEntity as a response.
    
    Bug: 829413
    Change-Id: I58a43a032fc520494ea8fdbfb1c64959a04c6b82
    Reviewed-on: https://chromium-review.googlesource.com/1117369
    Commit-Queue: Jun Choi <hongjunchoi@chromium.org>
    Reviewed-by: Kim Paulhamus <kpaulhamus@chromium.org>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572296}

--- device/fido/get_assertion_task_unittest.cc
+++ device/fido/get_assertion_task_unittest.cc
@@ -7,12 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <iterator>
 #include <memory>
+#include <string>
 #include <utility>
 #include <vector>
 
 #include "base/bind.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/scoped_task_environment.h"
+#include "crypto/ec_private_key.h"
 #include "device/base/features.h"
 #include "device/fido/authenticator_get_assertion_response.h"
 #include "device/fido/ctap_get_assertion_request.h"
@@ -21,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "device/fido/fido_test_data.h"
 #include "device/fido/mock_fido_device.h"
 #include "device/fido/test_callback_receiver.h"
+#include "device/fido/virtual_ctap2_device.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -112,6 +115,51 @@ TEST_F(FidoGetAssertionTaskTest, TestU2fSignSuccess) {
   EXPECT_TRUE(get_assertion_callback_receiver().value());
   EXPECT_EQ(device->supported_protocol(), ProtocolVersion::kU2f);
   EXPECT_FALSE(device->device_info());
+}
+
+TEST_F(FidoGetAssertionTaskTest, TestSignSuccessWithFake) {
+  auto private_key = crypto::ECPrivateKey::Create();
+  std::string public_key;
+  private_key->ExportRawPublicKey(&public_key);
+  auto hash = fido_parsing_utils::CreateSHA256Hash(public_key);
+  std::vector<uint8_t> key_handle(hash.begin(), hash.end());
+  CtapGetAssertionRequest request_param(test_data::kRelyingPartyId,
+                                        test_data::kClientDataHash);
+  request_param.SetAllowList({{CredentialType::kPublicKey, key_handle}});
+
+  auto device = std::make_unique<VirtualCtap2Device>();
+  device->mutable_state()->registrations.emplace(
+      key_handle,
+      VirtualFidoDevice::RegistrationData(
+          std::move(private_key),
+          fido_parsing_utils::CreateSHA256Hash(test_data::kRelyingPartyId),
+          42 /* counter */));
+
+  auto task = std::make_unique<GetAssertionTask>(
+      device.get(), std::move(request_param),
+      get_assertion_callback_receiver().callback());
+
+  get_assertion_callback_receiver().WaitForCallback();
+  EXPECT_EQ(CtapDeviceResponseCode::kSuccess,
+            get_assertion_callback_receiver().status());
+
+  // Just a sanity check, we don't verify the actual signature.
+  ASSERT_GE(32u + 1u + 4u + 8u,  // Minimal ECDSA signature is 8 bytes
+            get_assertion_callback_receiver()
+                .value()
+                ->auth_data()
+                .SerializeToByteArray()
+                .size());
+  EXPECT_EQ(0x01,
+            get_assertion_callback_receiver()
+                .value()
+                ->auth_data()
+                .SerializeToByteArray()[32]);  // UP flag
+  // Counter is incremented for every sign request.
+  EXPECT_EQ(43, get_assertion_callback_receiver()
+                    .value()
+                    ->auth_data()
+                    .SerializeToByteArray()[36]);  // counter
 }
 
 TEST_F(FidoGetAssertionTaskTest, TestU2fSignWithoutFlag) {
