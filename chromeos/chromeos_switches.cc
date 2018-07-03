c21f159e95ceaf852a9a0c92a98d3cfcac4479f3
Manu Cornet <manucornet@chromium.org>
2018-07-03 23:41:05 +0000

    Shelf: start new UI with centered items
    * Add flag for the shelf new UI
    * When it's on, center some shelf items (apps, pinned apps)
    * Minor dimension and opacity tweaks when the flag is on
    * Fix some minor typos here and there
    
    Change-Id: I87b7fa819bac0fff77f5007c4a0cee760a630239
    Bug: 805612
    Reviewed-on: https://chromium-review.googlesource.com/1116540
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Manu Cornet <manucornet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572397}

--- chromeos/chromeos_switches.cc
+++ chromeos/chromeos_switches.cc
@@ -546,6 +546,9 @@ const char kRlzPingDelay[] = "rlz-ping-delay";
 // App window previews when hovering over the shelf.
 const char kShelfHoverPreviews[] = "shelf-hover-previews";
 
+// Resdesigned shelf UI.
+const char kShelfNewUi[] = "shelf-new-ui";
+
 // Overrides network stub behavior. By default, ethernet, wifi and vpn are
 // enabled, and transitions occur instantaneously. Multiple options can be
 // comma separated (no spaces). Note: all options are in the format 'foo=x'.
@@ -750,6 +753,10 @@ bool ShouldHideActiveAppsFromShelf() {
 
 bool ShouldShowShelfHoverPreviews() {
   return base::CommandLine::ForCurrentProcess()->HasSwitch(kShelfHoverPreviews);
+}
+
+bool ShouldUseShelfNewUi() {
+  return base::CommandLine::ForCurrentProcess()->HasSwitch(kShelfNewUi);
 }
 
 bool IsInstantTetheringBackgroundAdvertisingSupported() {
