7b1ccf845026a7aea38164abf34d683ae140796f
Findit <findit-for-me@appspot.gserviceaccount.com>
2018-07-03 13:15:47 +0000

    Revert "Reland "[scheduler] Add a perftest to measure basic scheduling costs.""
    This reverts commit deb4b5a5f4039e08d5a5d31d5f7ca1437b68a5fd.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 572182 as the
    culprit for failures in the build cycles as shown on:
    https://findit-for-me.appspot.com/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtL2RlYjRiNWE1ZjQwMzllMDhkNWE1ZDMxZDVmN2NhMTQzN2I2OGE1ZmQM
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium/Linux%20x64/68083
    
    Sample Failed Step: compile
    
    Original change's description:
    > Reland "[scheduler] Add a perftest to measure basic scheduling costs."
    > 
    > This is a reland of 586e21ec4fd9a5d47b66628bbca27282420391a3
    > 
    > TBR=skyostil@chromium.org
    > 
    > Original change's description:
    > > [scheduler] Add a perftest to measure basic scheduling costs.
    > >
    > > Change-Id: Idda9f71ae005250848fd2a118e5c4344e36be452
    > > Reviewed-on: https://chromium-review.googlesource.com/1085295
    > > Reviewed-by: Alex Clarke <alexclarke@chromium.org>
    > > Commit-Queue: Alexander Timin <altimin@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#566362}
    > 
    > Reviewed-on: https://chromium-review.googlesource.com/1097295
    > Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    > Commit-Queue: Alexander Timin <altimin@chromium.org>
    > Cr-Original-Commit-Position: refs/heads/master@{#567645}
    > Change-Id: Ia52d89c4321cf4a6d00c51f83ee5b52ea06769de
    > Reviewed-on: https://chromium-review.googlesource.com/1122980
    > Reviewed-by: Alexander Timin <altimin@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#572182}
    
    Change-Id: Ia1c1d4413878bb5101cd696412e630ff3c90b343
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/1124540
    Cr-Commit-Position: refs/heads/master@{#572186}

--- third_party/blink/renderer/platform/scheduler/main_thread/frame_scheduler_impl.cc
+++ third_party/blink/renderer/platform/scheduler/main_thread/frame_scheduler_impl.cc
@@ -371,7 +371,7 @@ scoped_refptr<base::SingleThreadTaskRunner> FrameSchedulerImpl::GetTaskRunner(
   return nullptr;
 }
 
-scoped_refptr<MainThreadTaskQueue> FrameSchedulerImpl::LoadingTaskQueue() {
+scoped_refptr<TaskQueue> FrameSchedulerImpl::LoadingTaskQueue() {
   DCHECK(parent_page_scheduler_);
   if (!loading_task_queue_) {
     // TODO(panicker): Avoid adding this queue in RS task_runners_.
@@ -385,8 +385,7 @@ scoped_refptr<MainThreadTaskQueue> FrameSchedulerImpl::LoadingTaskQueue() {
   return loading_task_queue_;
 }
 
-scoped_refptr<MainThreadTaskQueue>
-FrameSchedulerImpl::LoadingControlTaskQueue() {
+scoped_refptr<TaskQueue> FrameSchedulerImpl::LoadingControlTaskQueue() {
   DCHECK(parent_page_scheduler_);
   if (!loading_control_task_queue_) {
     loading_control_task_queue_ = main_thread_scheduler_->NewLoadingTaskQueue(
@@ -399,7 +398,7 @@ FrameSchedulerImpl::LoadingControlTaskQueue() {
   return loading_control_task_queue_;
 }
 
-scoped_refptr<MainThreadTaskQueue> FrameSchedulerImpl::ThrottleableTaskQueue() {
+scoped_refptr<TaskQueue> FrameSchedulerImpl::ThrottleableTaskQueue() {
   DCHECK(parent_page_scheduler_);
   if (!throttleable_task_queue_) {
     // TODO(panicker): Avoid adding this queue in RS task_runners_.
@@ -429,7 +428,7 @@ scoped_refptr<MainThreadTaskQueue> FrameSchedulerImpl::ThrottleableTaskQueue() {
   return throttleable_task_queue_;
 }
 
-scoped_refptr<MainThreadTaskQueue> FrameSchedulerImpl::DeferrableTaskQueue() {
+scoped_refptr<TaskQueue> FrameSchedulerImpl::DeferrableTaskQueue() {
   DCHECK(parent_page_scheduler_);
   if (!deferrable_task_queue_) {
     deferrable_task_queue_ = main_thread_scheduler_->NewTaskQueue(
@@ -448,7 +447,7 @@ scoped_refptr<MainThreadTaskQueue> FrameSchedulerImpl::DeferrableTaskQueue() {
   return deferrable_task_queue_;
 }
 
-scoped_refptr<MainThreadTaskQueue> FrameSchedulerImpl::PausableTaskQueue() {
+scoped_refptr<TaskQueue> FrameSchedulerImpl::PausableTaskQueue() {
   DCHECK(parent_page_scheduler_);
   if (!pausable_task_queue_) {
     pausable_task_queue_ = main_thread_scheduler_->NewTaskQueue(
@@ -466,7 +465,7 @@ scoped_refptr<MainThreadTaskQueue> FrameSchedulerImpl::PausableTaskQueue() {
   return pausable_task_queue_;
 }
 
-scoped_refptr<MainThreadTaskQueue> FrameSchedulerImpl::UnpausableTaskQueue() {
+scoped_refptr<TaskQueue> FrameSchedulerImpl::UnpausableTaskQueue() {
   DCHECK(parent_page_scheduler_);
   if (!unpausable_task_queue_) {
     unpausable_task_queue_ = main_thread_scheduler_->NewTaskQueue(
@@ -814,11 +813,6 @@ void FrameSchedulerImpl::RemovePauseSubresourceLoadingHandle() {
     subresource_loading_paused_ = false;
     UpdatePolicy();
   }
-}
-
-scoped_refptr<MainThreadTaskQueue>
-FrameSchedulerImpl::GetTaskQueueForTesting() {
-  return UnpausableTaskQueue();
 }
 
 }  // namespace scheduler
