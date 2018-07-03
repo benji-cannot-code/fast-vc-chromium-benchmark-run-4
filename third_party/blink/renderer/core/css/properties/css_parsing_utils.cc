83b8e79bc0c41c51705a54bcb7b6cc1633d9e6fa
Zhuoyu Qian <zhuoyu.qian@samsung.com>
2018-07-03 08:47:40 +0000

    Background-size should not accept negative values.
    Follow the spec[1], negative values are not allowed.
    We should reject the negative values of background-size during parsing.
    
    [1]: https://drafts.csswg.org/css-backgrounds/#background-size
    
    Bug: 825549
    
    Signed-off-by: Zhuoyu Qian <zhuoyu.qian@samsung.com>
    Change-Id: Ib260bafa421249aed471ff801c8b49a4b97aba1e
    Reviewed-on: https://chromium-review.googlesource.com/989529
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572149}

--- third_party/blink/renderer/core/css/properties/css_parsing_utils.cc
+++ third_party/blink/renderer/core/css/properties/css_parsing_utils.cc
@@ -751,14 +751,7 @@ CSSPrimitiveValue* ConsumeLengthOrPercentCountNegative(
   CSSPrimitiveValue* result =
       ConsumeLengthOrPercent(range, context.Mode(), kValueRangeNonNegative,
                              CSSPropertyParserHelpers::UnitlessQuirk::kForbid);
-  if (result || !negative_size)
-    return result;
-
-  result =
-      ConsumeLengthOrPercent(range, context.Mode(), kValueRangeAll,
-                             CSSPropertyParserHelpers::UnitlessQuirk::kForbid);
-
-  if (result)
+  if (!result && negative_size)
     context.Count(*negative_size);
   return result;
 }
@@ -778,6 +771,8 @@ CSSValue* ConsumeBackgroundSize(CSSParserTokenRange& range,
     horizontal =
         ConsumeLengthOrPercentCountNegative(range, context, negative_size);
   }
+  if (!horizontal)
+    return nullptr;
 
   CSSValue* vertical = nullptr;
   if (!range.AtEnd()) {
