4c0374872d5d7093a6d0d7ca9a9408bebe596c6d
Darren Shen <shend@chromium.org>
2018-07-03 06:20:59 +0000

    [VK] Follow up on naming cleanup.
    In 1119610, there were some comments that were addressed, but the
    updated patch was not uploaded by mistake. This patch just addresses
    those comments.
    
    TBR=yhanada@chromium.org, jamescook@chromium.org
    
    Bug: 849980
    Change-Id: Ida53707785789fda4b8ee687198932dd266477fb
    Reviewed-on: https://chromium-review.googlesource.com/1124074
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572125}

--- ui/keyboard/keyboard_ui.h
+++ ui/keyboard/keyboard_ui.h
@@ -25,7 +25,6 @@ namespace keyboard {
 class KeyboardController;
 
 // Interface representing a window containing virtual keyboard UI.
-// The window can load asynchronously.... blah.
 class KEYBOARD_EXPORT KeyboardUI {
  public:
   KeyboardUI();
