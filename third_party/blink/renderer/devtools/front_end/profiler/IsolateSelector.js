e3debab3c0cfdfacab880df1909785b5303702a7
Alexei Filippov <alph@chromium.org>
2018-07-03 18:46:32 +0000

    DevTools: Do not update memory usage when Memory landing page is not visible.
    Change-Id: I0a07c50c28c4719a18f01305511d63574094f604
    Reviewed-on: https://chromium-review.googlesource.com/1115818
    Commit-Queue: Alexei Filippov <alph@chromium.org>
    Reviewed-by: Aleksey Kozyatinskiy <kozyatinskiy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572308}

--- third_party/blink/renderer/devtools/front_end/profiler/IsolateSelector.js
+++ third_party/blink/renderer/devtools/front_end/profiler/IsolateSelector.js
@@ -22,10 +22,25 @@ Profiler.IsolateSelector = class extends UI.VBox {
     this._isolateByModel = new Map();
     /** @type {!Map<string, !Profiler.IsolateSelector.ListItem>} */
     this._itemByIsolate = new Map();
+    this._updateTimer = null;
 
     SDK.targetManager.observeModels(SDK.RuntimeModel, this);
     SDK.targetManager.addEventListener(SDK.TargetManager.Events.NameChanged, this._targetChanged, this);
     SDK.targetManager.addEventListener(SDK.TargetManager.Events.InspectedURLChanged, this._targetChanged, this);
+  }
+
+  /**
+   * @override
+   */
+  wasShown() {
+    this._updateStats();
+  }
+
+  /**
+   * @override
+   */
+  willHide() {
+    clearTimeout(this._updateTimer);
   }
 
   /**
@@ -138,6 +153,13 @@ Profiler.IsolateSelector = class extends UI.VBox {
   _update() {
     this._list.invalidateRange(0, this._items.length);
   }
+
+  _updateStats() {
+    for (const item of this._itemByIsolate.values())
+      item.updateStats();
+    const heapStatsUpdateIntervalMs = 2000;
+    this._updateTimer = setTimeout(() => this._updateStats(), heapStatsUpdateIntervalMs);
+  }
 };
 
 Profiler.IsolateSelector.ListItem = class {
@@ -150,9 +172,9 @@ Profiler.IsolateSelector.ListItem = class {
     this.element = createElementWithClass('div', 'profile-isolate-item hbox');
     this._heapDiv = this.element.createChild('div', 'profile-isolate-item-heap');
     this._nameDiv = this.element.createChild('div', 'profile-isolate-item-name');
-    this._updateTimer = null;
+    this._updatesDisabled = false;
     this.updateTitle();
-    this._updateStats();
+    this.updateStats();
   }
 
   /**
@@ -169,9 +191,6 @@ Profiler.IsolateSelector.ListItem = class {
   removeModel(model) {
     this._models.delete(model);
     this.updateTitle();
-    if (this._models.size)
-      return;
-    clearTimeout(this._updateTimer);
   }
 
   /**
@@ -181,10 +200,14 @@ Profiler.IsolateSelector.ListItem = class {
     return Array.from(this._models);
   }
 
-  async _updateStats() {
-    const heapStats = await this._models.values().next().value.heapUsage();
-    if (!heapStats)
+  async updateStats() {
+    if (this._updatesDisabled)
       return;
+    const heapStats = await this._models.values().next().value.heapUsage();
+    if (!heapStats) {
+      this._updatesDisabled = true;
+      return;
+    }
     const usedTitle = ls`Heap size in use by live JS objects.`;
     const totalTitle = ls`Total JS heap size including live objects, garbage, and reserved space.`;
     this._heapDiv.removeChildren();
@@ -207,10 +230,8 @@ Profiler.IsolateSelector.ListItem = class {
     }
     this._nameDiv.removeChildren();
     for (const [name, count] of modelCountByName) {
-      const lineDiv = this._nameDiv.createChild('div');
       const title = count > 1 ? `${name} (${count})` : name;
-      lineDiv.textContent = title;
-      lineDiv.setAttribute('title', title);
+      this._nameDiv.appendChild(UI.html`<div title="${title}">${title}</div>`);
     }
   }
 };
