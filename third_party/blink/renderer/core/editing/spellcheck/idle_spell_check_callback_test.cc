f1458a33498fce5f84bc78f166caa55fd600cbae
Xiaocheng Hu <xiaochengh@chromium.org>
2018-07-03 02:15:51 +0000

    Revert "Remove cold mode idle time spellchecker flag"
    This reverts commit d1c99618c03544f0c8734c7340471b30ac67b1f1.
    
    Reason for revert: Observed many crashes in M68 & M69. Not ready
    for shipping.
    
    Original change's description:
    > Remove cold mode idle time spellchecker flag
    >
    > As cold mode idle time spellchecker is shipped into M68
    > and no regression is obversed, this patch removes the flag
    > from M69 as cleanup.
    >
    > Bug: 716642
    > Change-Id: I8f850c52469590ee9d5e542c14f4dae842a6163a
    > Reviewed-on: https://chromium-review.googlesource.com/1072953
    > Reviewed-by: Kent Tamura <tkent@chromium.org>
    > Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    > Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#561951}
    
    TBR=yosin@chromium.org,tkent@chromium.org,xiaochengh@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 716642, 856376, 859056, 859443
    Change-Id: I91426509a315a07bf4f548ebafe825c2ef856999
    Reviewed-on: https://chromium-review.googlesource.com/1123120
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572086}

--- third_party/blink/renderer/core/editing/spellcheck/idle_spell_check_callback_test.cc
+++ third_party/blink/renderer/core/editing/spellcheck/idle_spell_check_callback_test.cc
@@ -38,8 +38,10 @@ class IdleSpellCheckCallbackTest : public SpellCheckTestBase {
         IdleChecker().SetNeedsInvocation();
         break;
       case State::kColdModeTimerStarted:
+        DCHECK(RuntimeEnabledFeatures::IdleTimeColdModeSpellCheckingEnabled());
         break;
       case State::kColdModeRequested:
+        DCHECK(RuntimeEnabledFeatures::IdleTimeColdModeSpellCheckingEnabled());
         IdleChecker().SkipColdModeTimerForTesting();
         break;
       case State::kInHotModeInvocation:
@@ -52,7 +54,17 @@ class IdleSpellCheckCallbackTest : public SpellCheckTestBase {
 // Test cases for lifecycle state transitions.
 
 TEST_F(IdleSpellCheckCallbackTest, InitializationWithColdMode) {
+  if (!RuntimeEnabledFeatures::IdleTimeColdModeSpellCheckingEnabled())
+    return;
+
   EXPECT_EQ(State::kColdModeTimerStarted, IdleChecker().GetState());
+}
+
+TEST_F(IdleSpellCheckCallbackTest, InitializationWithoutColdMode) {
+  if (RuntimeEnabledFeatures::IdleTimeColdModeSpellCheckingEnabled())
+    return;
+
+  EXPECT_EQ(State::kInactive, IdleChecker().GetState());
 }
 
 TEST_F(IdleSpellCheckCallbackTest, RequestWhenInactive) {
@@ -72,6 +84,9 @@ TEST_F(IdleSpellCheckCallbackTest, RequestWhenHotModeRequested) {
 }
 
 TEST_F(IdleSpellCheckCallbackTest, RequestWhenColdModeTimerStarted) {
+  if (!RuntimeEnabledFeatures::IdleTimeColdModeSpellCheckingEnabled())
+    return;
+
   TransitTo(State::kColdModeTimerStarted);
   IdleChecker().SetNeedsInvocation();
   EXPECT_EQ(State::kHotModeRequested, IdleChecker().GetState());
@@ -79,6 +94,9 @@ TEST_F(IdleSpellCheckCallbackTest, RequestWhenColdModeTimerStarted) {
 }
 
 TEST_F(IdleSpellCheckCallbackTest, RequestWhenColdModeRequested) {
+  if (!RuntimeEnabledFeatures::IdleTimeColdModeSpellCheckingEnabled())
+    return;
+
   TransitTo(State::kColdModeRequested);
   int handle = IdleChecker().IdleCallbackHandle();
   IdleChecker().SetNeedsInvocation();
@@ -87,13 +105,28 @@ TEST_F(IdleSpellCheckCallbackTest, RequestWhenColdModeRequested) {
   EXPECT_NE(-1, IdleChecker().IdleCallbackHandle());
 }
 
+TEST_F(IdleSpellCheckCallbackTest, HotModeTransitToInactive) {
+  if (RuntimeEnabledFeatures::IdleTimeColdModeSpellCheckingEnabled())
+    return;
+
+  TransitTo(State::kHotModeRequested);
+  IdleChecker().ForceInvocationForTesting();
+  EXPECT_EQ(State::kInactive, IdleChecker().GetState());
+}
+
 TEST_F(IdleSpellCheckCallbackTest, HotModeTransitToColdMode) {
+  if (!RuntimeEnabledFeatures::IdleTimeColdModeSpellCheckingEnabled())
+    return;
+
   TransitTo(State::kHotModeRequested);
   IdleChecker().ForceInvocationForTesting();
   EXPECT_EQ(State::kColdModeTimerStarted, IdleChecker().GetState());
 }
 
 TEST_F(IdleSpellCheckCallbackTest, ColdModeTimerStartedToRequested) {
+  if (!RuntimeEnabledFeatures::IdleTimeColdModeSpellCheckingEnabled())
+    return;
+
   TransitTo(State::kColdModeTimerStarted);
   IdleChecker().SkipColdModeTimerForTesting();
   EXPECT_EQ(State::kColdModeRequested, IdleChecker().GetState());
@@ -101,6 +134,9 @@ TEST_F(IdleSpellCheckCallbackTest, ColdModeTimerStartedToRequested) {
 }
 
 TEST_F(IdleSpellCheckCallbackTest, ColdModeStayAtColdMode) {
+  if (!RuntimeEnabledFeatures::IdleTimeColdModeSpellCheckingEnabled())
+    return;
+
   TransitTo(State::kColdModeRequested);
   IdleChecker().SetNeedsMoreColdModeInvocationForTesting();
   IdleChecker().ForceInvocationForTesting();
@@ -108,6 +144,9 @@ TEST_F(IdleSpellCheckCallbackTest, ColdModeStayAtColdMode) {
 }
 
 TEST_F(IdleSpellCheckCallbackTest, ColdModeToInactive) {
+  if (!RuntimeEnabledFeatures::IdleTimeColdModeSpellCheckingEnabled())
+    return;
+
   TransitTo(State::kColdModeRequested);
   IdleChecker().ForceInvocationForTesting();
   EXPECT_EQ(State::kInactive, IdleChecker().GetState());
@@ -126,12 +165,18 @@ TEST_F(IdleSpellCheckCallbackTest, DetachWhenHotModeRequested) {
 }
 
 TEST_F(IdleSpellCheckCallbackTest, DetachWhenColdModeTimerStarted) {
+  if (!RuntimeEnabledFeatures::IdleTimeColdModeSpellCheckingEnabled())
+    return;
+
   TransitTo(State::kColdModeTimerStarted);
   GetDocument().Shutdown();
   EXPECT_EQ(State::kInactive, IdleChecker().GetState());
 }
 
 TEST_F(IdleSpellCheckCallbackTest, DetachWhenColdModeRequested) {
+  if (!RuntimeEnabledFeatures::IdleTimeColdModeSpellCheckingEnabled())
+    return;
+
   TransitTo(State::kColdModeRequested);
   GetDocument().Shutdown();
   EXPECT_EQ(State::kInactive, IdleChecker().GetState());
