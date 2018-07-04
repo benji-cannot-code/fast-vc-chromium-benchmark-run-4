b8b6a9b4f442c235c84906c0e2a45a9c55fab482
Luciano Pacheco <lucmult@chromium.org>
2018-07-04 07:05:21 +0000

    Add files_app_entry_types as dependency
    This is a preparation for crrev.com/c/1113258 which uses this deps.
    
    Bug: 846587
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: I9fc23fa6a137df4256420464384ac3366d260b3c
    Reviewed-on: https://chromium-review.googlesource.com/1116400
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Sasha Morrissey <sashab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572499}

--- ui/file_manager/file_manager/background/js/background_common_scripts.js
+++ ui/file_manager/file_manager/background/js/background_common_scripts.js
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // <include src="../../common/js/metrics_base.js">
 // <include src="../../common/js/metrics_events.js">
 // <include src="../../common/js/metrics.js">
+// <include src="../../common/js/files_app_entry_types.js">
 // <include src="../../common/js/util.js">
 // <include src="../../common/js/volume_manager_common.js">
 // <include src="app_window_wrapper.js">
