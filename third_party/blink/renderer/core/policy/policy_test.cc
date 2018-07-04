bac171112e32438be92dbd017ab89959d579be70
Ian Clelland <iclelland@chromium.org>
2018-07-04 18:11:32 +0000

    Add policy test for default-allowed features.
    Code coverage showed that we were missing a test case for the
    document.policy object where the state of a feature which is
    on-by-default but not mentioned in any policies is queried.
    
    Change-Id: Icd77f4763e80ea0746692f6bc660a11b4d444263
    Reviewed-on: https://chromium-review.googlesource.com/1126203
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Ian Clelland <iclelland@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572620}

--- third_party/blink/renderer/core/policy/policy_test.cc
+++ third_party/blink/renderer/core/policy/policy_test.cc
@@ -70,6 +70,8 @@ TEST_F(DocumentPolicyTest, TestAllowsFeature) {
   EXPECT_TRUE(GetPolicy()->allowsFeature("camera", kOriginB));
   EXPECT_FALSE(GetPolicy()->allowsFeature("camera", "https://badorigin.com"));
   EXPECT_TRUE(GetPolicy()->allowsFeature("geolocation", kSelfOrigin));
+  EXPECT_TRUE(GetPolicy()->allowsFeature("sync-xhr"));
+  EXPECT_TRUE(GetPolicy()->allowsFeature("sync-xhr", kOriginA));
 }
 
 TEST_F(DocumentPolicyTest, TestGetAllowList) {
@@ -83,6 +85,8 @@ TEST_F(DocumentPolicyTest, TestGetAllowList) {
               UnorderedElementsAre("*"));
   EXPECT_TRUE(GetPolicy()->getAllowlistForFeature("badfeature").IsEmpty());
   EXPECT_TRUE(GetPolicy()->getAllowlistForFeature("midi").IsEmpty());
+  EXPECT_THAT(GetPolicy()->getAllowlistForFeature("sync-xhr"),
+              UnorderedElementsAre("*"));
 }
 
 TEST_F(DocumentPolicyTest, TestAllowedFeatures) {
@@ -94,6 +98,8 @@ TEST_F(DocumentPolicyTest, TestAllowedFeatures) {
   EXPECT_TRUE(allowed_features.Contains("geolocation"));
   EXPECT_FALSE(allowed_features.Contains("badfeature"));
   EXPECT_FALSE(allowed_features.Contains("midi"));
+  // "sync-xhr" is allowed on all origins
+  EXPECT_TRUE(allowed_features.Contains("sync-xhr"));
 }
 
 TEST_F(IFramePolicyTest, TestAllowsFeature) {
@@ -111,6 +117,8 @@ TEST_F(IFramePolicyTest, TestAllowsFeature) {
   EXPECT_FALSE(GetPolicy()->allowsFeature("camera", kOriginB));
   EXPECT_FALSE(GetPolicy()->allowsFeature("camera", "https://badorigin.com"));
   EXPECT_TRUE(GetPolicy()->allowsFeature("geolocation", kSelfOrigin));
+  EXPECT_TRUE(GetPolicy()->allowsFeature("sync-xhr"));
+  EXPECT_TRUE(GetPolicy()->allowsFeature("sync-xhr", kOriginA));
 }
 
 TEST_F(IFramePolicyTest, TestGetAllowList) {
@@ -124,6 +132,8 @@ TEST_F(IFramePolicyTest, TestGetAllowList) {
               UnorderedElementsAre(kSelfOrigin));
   EXPECT_TRUE(GetPolicy()->getAllowlistForFeature("badfeature").IsEmpty());
   EXPECT_TRUE(GetPolicy()->getAllowlistForFeature("midi").IsEmpty());
+  EXPECT_THAT(GetPolicy()->getAllowlistForFeature("sync-xhr"),
+              UnorderedElementsAre("*"));
 }
 
 TEST_F(IFramePolicyTest, TestAllowedFeatures) {
@@ -135,6 +145,8 @@ TEST_F(IFramePolicyTest, TestAllowedFeatures) {
   EXPECT_TRUE(allowed_features.Contains("geolocation"));
   EXPECT_FALSE(allowed_features.Contains("badfeature"));
   EXPECT_FALSE(allowed_features.Contains("midi"));
+  // "sync-xhr" is allowed on all origins
+  EXPECT_TRUE(allowed_features.Contains("sync-xhr"));
 }
 
 TEST_F(IFramePolicyTest, TestCombinedPolicy) {
@@ -152,6 +164,8 @@ TEST_F(IFramePolicyTest, TestCombinedPolicy) {
   EXPECT_TRUE(allowed_features.Contains("camera"));
   // "geolocation" has default policy as allowed on self origin.
   EXPECT_FALSE(allowed_features.Contains("badfeature"));
+  // "sync-xhr" is still implicitly allowed on all origins
+  EXPECT_TRUE(allowed_features.Contains("sync-xhr"));
 }
 
 }  // namespace blink
