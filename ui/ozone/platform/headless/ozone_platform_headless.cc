e6eba616fca2071e0a052e6a443085c9486003b7
Julie Jeongeun Kim <jkim@igalia.com>
2018-07-04 05:53:29 +0000

    [ozone/headless] Add initial display fetch support for headless
    Each Ozone platform creates its own NativeDisplayDelegate to get
    display information. Otherwise, 'display::FakeDisplayDelegate' is
    used for NativeDisplayDelegate.
    
    Per bug, ozone headless is crashing because it uses
    FakeDisplayDelegate and it doesn't invoke OnConfigurationChanged
    before initialization completes so ScreenBase can't add the display.
    Once NativeDisplayDelegate is initialized, DesktopScreenOzone
    requires DisplayMode from DisplaySnapshot but FakeDisplayDelegate has
    FakeDisplaySnapshot and it doesn't have DisplayMode.
    
    This CL creates HeadlessNativeDisplayDelegate which owns
    DisplaySnapshot and DisplayMode for ozone/headless.
    DesktopScreenOzone keeps it in delegate_ and communicates with it.
    
    To try this, compile with the following gn args
    use_ozone = true
    ozone_platform_headless = true
    
    TEST=./out/Debug/chrome --ozone-platform=headless
    
    Bug: 851375
    Change-Id: I2c097bf54fafb2d9311919647801e401c59ba929
    Reviewed-on: https://chromium-review.googlesource.com/1120447
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Reviewed-by: Michael Spang <spang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572488}

--- ui/ozone/platform/headless/ozone_platform_headless.cc
+++ ui/ozone/platform/headless/ozone_platform_headless.cc
@@ -9,12 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/macros.h"
 #include "ui/base/cursor/ozone/bitmap_cursor_factory_ozone.h"
-#include "ui/display/manager/fake_display_delegate.h"
 #include "ui/events/ozone/layout/keyboard_layout_engine_manager.h"
 #include "ui/events/ozone/layout/stub/stub_keyboard_layout_engine.h"
 #include "ui/events/platform/platform_event_source.h"
 #include "ui/events/system_input_injector.h"
 #include "ui/ozone/common/stub_overlay_manager.h"
+#include "ui/ozone/platform/headless/headless_native_display_delegate.h"
 #include "ui/ozone/platform/headless/headless_surface_factory.h"
 #include "ui/ozone/platform/headless/headless_window.h"
 #include "ui/ozone/platform/headless/headless_window_manager.h"
@@ -75,7 +75,7 @@ class OzonePlatformHeadless : public OzonePlatform {
   }
   std::unique_ptr<display::NativeDisplayDelegate> CreateNativeDisplayDelegate()
       override {
-    return std::make_unique<display::FakeDisplayDelegate>();
+    return std::make_unique<HeadlessNativeDisplayDelegate>();
   }
 
   void InitializeUI(const InitParams& params) override {
