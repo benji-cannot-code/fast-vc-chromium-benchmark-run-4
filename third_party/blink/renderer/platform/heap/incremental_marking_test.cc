3c5fc12f599c31bb30094eb4de51e9c88c6b966f
Haruka Matsumura <harukamt@google.com>
2018-07-04 04:05:31 +0000

    incremental_marking_test: HeapLinkedHashSetSwapTest should be called Swap() function
    This unit test is named SwapTest. But, in actual, Move() function is called.
    
    Change-Id: I929047b9a04dd407534c769c5b4edc97f466c0b4
    Reviewed-on: https://chromium-review.googlesource.com/1124341
    Commit-Queue: Haruka Matsumura <harukamt@google.com>
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572468}

--- third_party/blink/renderer/platform/heap/incremental_marking_test.cc
+++ third_party/blink/renderer/platform/heap/incremental_marking_test.cc
@@ -1060,7 +1060,7 @@ TEST(IncrementalMarkingTest, HeapLinkedHashSetMove) {
 TEST(IncrementalMarkingTest, HeapLinkedHashSetSwap) {
   Swap<HeapLinkedHashSet<Member<Object>>>();
   // Weak references are strongified for the current cycle.
-  Move<HeapLinkedHashSet<WeakMember<Object>>>();
+  Swap<HeapLinkedHashSet<WeakMember<Object>>>();
 }
 
 // =============================================================================
