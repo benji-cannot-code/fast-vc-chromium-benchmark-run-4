ff5989e9e7dfec3fd2c521d55fea644ab161f766
stkhapugin@chromium.org <stkhapugin@chromium.org>
2018-07-04 16:20:22 +0000

    Use correct tint color for placeholder in non-incognito.
    Use the correct color scheme instead of always using incognito color
    scheme when setting the placeholder color.
    
    Bug: 859553
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: If6146f5b9f0c0c16833d058e9a5b12534314a09b
    Reviewed-on: https://chromium-review.googlesource.com/1126100
    Reviewed-by: Elodie Banel <lod@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572601}

--- ios/chrome/browser/ui/location_bar/location_bar_steady_view.mm
+++ ios/chrome/browser/ui/location_bar/location_bar_steady_view.mm
@@ -287,7 +287,7 @@ const CGFloat kFontSize = 17.0f;
   [self updateAccessibility];
 }
 
-#pragma mark - UIAccessibilityContainer]
+#pragma mark - UIAccessibilityContainer
 
 - (NSArray*)accessibilityElements {
   return self.accessibleElements;
