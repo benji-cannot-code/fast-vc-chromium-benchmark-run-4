d20bfa3c914c1b60f88d5bd75293339b1b4d5a7e
kylechar <kylechar@chromium.org>
2018-07-04 22:51:31 +0000

    Add reset() to scoped_refptr.
    Add reset() to make chromium smart pointer APIs more consistent. Both
    std::unique_ptr and std::shared_ptr (which we don't use) have reset() as
    part of their API.
    
    In order to reset a scoped_refptr you would typically do the following:
    
    scoped_refptr<Foo> foo;
    ...
    foo = nullptr;
    
    If you can't see the definition of |foo| it could easily be mistaken for
    a raw pointer. However, calling foo.reset() makes it clear |foo| is not
    a raw pointer.
    
    This CL does not add reset(T*) for scoped_refptr<T>. Assigning the
    result of base::MakeRefCounted<T> is the preferred way of creating and
    assigning a new reference counted object to a scoped_refptr.
    
    Bug: none
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel
    Change-Id: I1db7cccefe61a8dc4e03ff600d8539f565e9425f
    Reviewed-on: https://chromium-review.googlesource.com/1125216
    Commit-Queue: kylechar <kylechar@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572669}

--- base/memory/scoped_refptr.h
+++ base/memory/scoped_refptr.h
@@ -124,7 +124,7 @@ scoped_refptr<T> WrapRefCounted(T* t) {
 //   void some_other_function() {
 //     scoped_refptr<MyFoo> foo = MakeRefCounted<MyFoo>();
 //     ...
-//     foo = nullptr;  // explicitly releases |foo|
+//     foo.reset();  // explicitly releases |foo|
 //     ...
 //     if (foo)
 //       foo->Method(param);
@@ -228,6 +228,10 @@ class scoped_refptr {
     swap(r);
     return *this;
   }
+
+  // Sets managed object to null and releases reference to the previous managed
+  // object, if it existed.
+  void reset() { scoped_refptr().swap(*this); }
 
   void swap(scoped_refptr& r) noexcept { std::swap(ptr_, r.ptr_); }
 
