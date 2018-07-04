1d4e824f7170fed9cc14c160052bc87ada8c02bc
Oliver Chang <ochang@chromium.org>
2018-07-04 01:26:46 +0000

    Add --file-url-path-alias.
    --file-url-path-alias=/alias=/replacements will result in  all file://
    URLs with root paths beginning with /alias to be replaced with
    /replacement.
    
    For IPC fuzzer builds, this is automatically populated with
    /gen=BUILD_DIR/gen.
    
    Bug: 839228
    Change-Id: I935278d8b11369efc9b98281a5a8056cee8e579d
    Reviewed-on: https://chromium-review.googlesource.com/1119727
    Commit-Queue: Oliver Chang <ochang@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572434}

--- content/renderer/render_frame_impl_browsertest.cc
+++ content/renderer/render_frame_impl_browsertest.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/frame_owner_properties.h"
 #include "content/common/renderer.mojom.h"
 #include "content/common/view_messages.h"
+#include "content/public/common/content_switches.h"
 #include "content/public/common/previews_state.h"
 #include "content/public/renderer/content_renderer_client.h"
 #include "content/public/renderer/document_state.h"
@@ -620,6 +621,29 @@ TEST_F(RenderFrameImplTest, AutoplayFlags_WrongOrigin) {
   // Check the flags have been not been set.
   EXPECT_EQ(blink::mojom::kAutoplayFlagNone,
             AutoplayFlagsForFrame(GetMainRenderFrame()));
+}
+
+TEST_F(RenderFrameImplTest, FileUrlPathAlias) {
+  const struct {
+    const char* original;
+    const char* transformed;
+  } kTestCases[] = {
+      {"file:///alias", "file:///replacement"},
+      {"file:///alias/path/to/file", "file:///replacement/path/to/file"},
+      {"file://alias/path/to/file", "file://alias/path/to/file"},
+      {"file:///notalias/path/to/file", "file:///notalias/path/to/file"},
+      {"file:///root/alias/path/to/file", "file:///root/alias/path/to/file"},
+      {"file:///", "file:///"},
+  };
+  base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
+      switches::kFileUrlPathAlias, "/alias=/replacement");
+
+  for (const auto& test_case : kTestCases) {
+    WebURLRequest request;
+    request.SetURL(GURL(test_case.original));
+    GetMainRenderFrame()->WillSendRequest(request);
+    EXPECT_EQ(test_case.transformed, request.Url().GetString().Utf8());
+  }
 }
 
 // RenderFrameRemoteInterfacesTest ------------------------------------
