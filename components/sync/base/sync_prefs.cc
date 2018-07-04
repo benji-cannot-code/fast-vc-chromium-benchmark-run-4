21a0f5eb74bb191c854ef9f29049150b635661f2
vitaliii <vitaliii@chromium.org>
2018-07-04 14:16:53 +0000

    [Sync::Cleanup] Do not handle NOTREACHED failures in SyncPrefs.
    Change-Id: I1a8ef527472d65b5f24b7357fe9ac6b153879d03
    Reviewed-on: https://chromium-review.googlesource.com/1124848
    Commit-Queue: vitaliii <vitaliii@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572557}

--- components/sync/base/sync_prefs.cc
+++ components/sync/base/sync_prefs.cc
@@ -434,20 +434,14 @@ void SyncPrefs::RegisterDataTypePreferredPref(
     ModelType type,
     bool is_preferred) {
   const char* pref_name = GetPrefNameForDataType(type);
-  if (!pref_name) {
-    NOTREACHED();
-    return;
-  }
+  DCHECK(pref_name);
   registry->RegisterBooleanPref(pref_name, is_preferred);
 }
 
 bool SyncPrefs::GetDataTypePreferred(ModelType type) const {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   const char* pref_name = GetPrefNameForDataType(type);
-  if (!pref_name) {
-    NOTREACHED();
-    return false;
-  }
+  DCHECK(pref_name);
 
   if (AlwaysPreferredUserTypes().Has(type))
     return true;
@@ -466,10 +460,7 @@ bool SyncPrefs::GetDataTypePreferred(ModelType type) const {
 void SyncPrefs::SetDataTypePreferred(ModelType type, bool is_preferred) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   const char* pref_name = GetPrefNameForDataType(type);
-  if (!pref_name) {
-    NOTREACHED();
-    return;
-  }
+  DCHECK(pref_name);
 
   if (AlwaysPreferredUserTypes().Has(type))
     return;
