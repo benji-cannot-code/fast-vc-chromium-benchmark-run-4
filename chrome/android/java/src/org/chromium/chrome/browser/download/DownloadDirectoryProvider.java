c8b577ba367a6106bbbaae89c96783b60f9cad68
Marcin Wiacek <marcin@mwiacek.com>
2018-07-04 15:43:13 +0000

    Improve DownloadDirectoryLocationTYpe @IntDef and reformat code
    Change-Id: Iae40af3d5ab3ecb3a9f41443359a069424ddacb6
    Reviewed-on: https://chromium-review.googlesource.com/1126253
    Commit-Queue: Marcin Wiącek <marcin@mwiacek.com>
    Commit-Queue: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572588}

--- chrome/android/java/src/org/chromium/chrome/browser/download/DownloadDirectoryProvider.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/DownloadDirectoryProvider.java
@@ -53,12 +53,13 @@ public class DownloadDirectoryProvider {
 
             // If no default directory, return an error option.
             if (defaultDirectory == null) {
-                dirs.add(new DirectoryOption(null, 0, 0, DirectoryOption.ERROR_OPTION));
+                dirs.add(new DirectoryOption(
+                        null, 0, 0, DirectoryOption.DownloadLocationDirectoryType.ERROR));
                 return dirs;
             }
 
-            DirectoryOption defaultOption =
-                    toDirectoryOption(defaultDirectory, DirectoryOption.DEFAULT_OPTION);
+            DirectoryOption defaultOption = toDirectoryOption(
+                    defaultDirectory, DirectoryOption.DownloadLocationDirectoryType.DEFAULT);
             dirs.add(defaultOption);
 
             // Retrieve additional directories, i.e. the external SD card directory.
@@ -79,7 +80,8 @@ public class DownloadDirectoryProvider {
 
                 // Skip primary storage directory.
                 if (files[i].getAbsolutePath().contains(mExternalStorageDirectory)) continue;
-                dirs.add(toDirectoryOption(files[i], DirectoryOption.ADDITIONAL_OPTION));
+                dirs.add(toDirectoryOption(
+                        files[i], DirectoryOption.DownloadLocationDirectoryType.ADDITIONAL));
             }
             return dirs;
         }
