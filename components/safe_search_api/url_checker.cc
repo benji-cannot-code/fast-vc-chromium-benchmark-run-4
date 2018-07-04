52f3376791f66fd2b5954ce46bdf6e14776e9fb7
Bruno Kim Medeiros Cesar <brunokim@google.com>
2018-07-04 14:02:04 +0000

    Disable Safe Search whitelist for Google properties with feature flag.
    We plan to remove this code in the next release, but for now we'll keep
    it behind a "live switch" so that we may re-enable in the server if
    necessary.
    
    Bug: 859097
    Change-Id: I6c86e92de4b79c1a622a559b30840b9442f76716
    Reviewed-on: https://chromium-review.googlesource.com/1122636
    Commit-Queue: Bruno Kim Medeiros Cesar <brunokim@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572554}

--- components/safe_search_api/url_checker.cc
+++ components/safe_search_api/url_checker.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/callback.h"
+#include "base/feature_list.h"
 #include "base/json/json_reader.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/stl_util.h"
@@ -74,6 +75,10 @@ bool ParseResponse(const std::string& response, bool* is_porn) {
 
 }  // namespace
 
+// Consider all URLs within a google domain to be safe.
+const base::Feature kAllowAllGoogleUrls{"SafeSearchAllowAllGoogleURLs",
+                                        base::FEATURE_DISABLED_BY_DEFAULT};
+
 struct URLChecker::Check {
   Check(const GURL& url,
         std::unique_ptr<network::SimpleURLLoader> simple_url_loader,
@@ -128,19 +133,21 @@ URLChecker::URLChecker(
 URLChecker::~URLChecker() = default;
 
 bool URLChecker::CheckURL(const GURL& url, CheckCallback callback) {
-  // TODO(treib): Hack: For now, allow all Google URLs to save QPS. If we ever
-  // remove this, we should find a way to allow at least the NTP.
-  if (google_util::IsGoogleDomainUrl(url, google_util::ALLOW_SUBDOMAIN,
-                                     google_util::ALLOW_NON_STANDARD_PORTS)) {
-    std::move(callback).Run(url, Classification::SAFE, false);
-    return true;
-  }
-  // TODO(treib): Hack: For now, allow all YouTube URLs since YouTube has its
-  // own Safety Mode anyway.
-  if (google_util::IsYoutubeDomainUrl(url, google_util::ALLOW_SUBDOMAIN,
-                                      google_util::ALLOW_NON_STANDARD_PORTS)) {
-    std::move(callback).Run(url, Classification::SAFE, false);
-    return true;
+  if (base::FeatureList::IsEnabled(kAllowAllGoogleUrls)) {
+    // TODO(treib): Hack: For now, allow all Google URLs to save QPS.
+    if (google_util::IsGoogleDomainUrl(url, google_util::ALLOW_SUBDOMAIN,
+                                       google_util::ALLOW_NON_STANDARD_PORTS)) {
+      std::move(callback).Run(url, Classification::SAFE, false);
+      return true;
+    }
+    // TODO(treib): Hack: For now, allow all YouTube URLs since YouTube has its
+    // own Safety Mode anyway.
+    if (google_util::IsYoutubeDomainUrl(
+            url, google_util::ALLOW_SUBDOMAIN,
+            google_util::ALLOW_NON_STANDARD_PORTS)) {
+      std::move(callback).Run(url, Classification::SAFE, false);
+      return true;
+    }
   }
 
   auto cache_it = cache_.Get(url);
