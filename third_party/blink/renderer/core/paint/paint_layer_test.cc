a6cf5fa810324a185c79cfbbdf62a3e9e84807b0
Chris Harrelson <chrishtr@chromium.org>
2018-07-04 16:08:39 +0000

    Fix root cull rect for hit testing under <foreignObject>.
    It was incorrectly set to the view rect of the containing LayoutView.
    Also fixed a similar accessibility bug along the way.
    
    Bug: 859461
    
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I443bb56b2e8fac06a6af2efe4e9d3ec3ff6a9556
    Reviewed-on: https://chromium-review.googlesource.com/1125510
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#572596}

--- third_party/blink/renderer/core/paint/paint_layer_test.cc
+++ third_party/blink/renderer/core/paint/paint_layer_test.cc
@@ -1376,21 +1376,21 @@ TEST_P(PaintLayerTest, HitTestWithStopNode) {
   HitTestRequest request(HitTestRequest::kReadOnly | HitTestRequest::kActive);
   HitTestLocation location((LayoutPoint(50, 25)));
   HitTestResult result(request, location);
-  GetDocument().GetLayoutView()->Layer()->HitTest(location, result);
+  GetDocument().GetLayoutView()->HitTest(location, result);
   EXPECT_EQ(child, result.InnerNode());
 
   // Same hit test, with stop node.
   request = HitTestRequest(HitTestRequest::kReadOnly | HitTestRequest::kActive,
                            hit->GetLayoutObject());
   result = HitTestResult(request, location);
-  GetDocument().GetLayoutView()->Layer()->HitTest(location, result);
+  GetDocument().GetLayoutView()->HitTest(location, result);
   EXPECT_EQ(hit, result.InnerNode());
 
   // Regular hit test over 'overlap'
   request = HitTestRequest(HitTestRequest::kReadOnly | HitTestRequest::kActive);
   location = HitTestLocation((LayoutPoint(50, 75)));
   result = HitTestResult(request, location);
-  GetDocument().GetLayoutView()->Layer()->HitTest(location, result);
+  GetDocument().GetLayoutView()->HitTest(location, result);
   EXPECT_EQ(overlap, result.InnerNode());
 
   // Same hit test, with stop node, should still hit 'overlap' because it's not
@@ -1398,7 +1398,7 @@ TEST_P(PaintLayerTest, HitTestWithStopNode) {
   request = HitTestRequest(HitTestRequest::kReadOnly | HitTestRequest::kActive,
                            hit->GetLayoutObject());
   result = HitTestResult(request, location);
-  GetDocument().GetLayoutView()->Layer()->HitTest(location, result);
+  GetDocument().GetLayoutView()->HitTest(location, result);
   EXPECT_EQ(overlap, result.InnerNode());
 
   // List-based hit test with stop node
@@ -1407,7 +1407,7 @@ TEST_P(PaintLayerTest, HitTestWithStopNode) {
                            hit->GetLayoutObject());
   location = HitTestLocation((LayoutRect(40, 15, 20, 20)));
   result = HitTestResult(request, location);
-  GetDocument().GetLayoutView()->Layer()->HitTest(location, result);
+  GetDocument().GetLayoutView()->HitTest(location, result);
   EXPECT_EQ(1u, result.ListBasedTestResult().size());
   EXPECT_EQ(hit, *result.ListBasedTestResult().begin());
 }
@@ -1436,13 +1436,13 @@ TEST_P(PaintLayerTest, HitTestTableWithStopNode) {
   HitTestRequest request(HitTestRequest::kReadOnly | HitTestRequest::kActive);
   HitTestLocation location((LayoutPoint(50, 50)));
   HitTestResult result(request, location);
-  GetDocument().GetLayoutView()->Layer()->HitTest(location, result);
+  GetDocument().GetLayoutView()->HitTest(location, result);
   EXPECT_EQ(cell11, result.InnerNode());
 
   request = HitTestRequest(HitTestRequest::kReadOnly | HitTestRequest::kActive,
                            table->GetLayoutObject());
   result = HitTestResult(request, location);
-  GetDocument().GetLayoutView()->Layer()->HitTest(location, result);
+  GetDocument().GetLayoutView()->HitTest(location, result);
   EXPECT_EQ(table, result.InnerNode());
 }
 
@@ -1457,13 +1457,13 @@ TEST_P(PaintLayerTest, HitTestSVGWithStopNode) {
   HitTestRequest request(HitTestRequest::kReadOnly | HitTestRequest::kActive);
   HitTestLocation location((LayoutPoint(50, 50)));
   HitTestResult result(request, location);
-  GetDocument().GetLayoutView()->Layer()->HitTest(location, result);
+  GetDocument().GetLayoutView()->HitTest(location, result);
   EXPECT_EQ(circle, result.InnerNode());
 
   request = HitTestRequest(HitTestRequest::kReadOnly | HitTestRequest::kActive,
                            svg->GetLayoutObject());
   result = HitTestResult(request, location);
-  GetDocument().GetLayoutView()->Layer()->HitTest(location, result);
+  GetDocument().GetLayoutView()->HitTest(location, result);
   EXPECT_EQ(svg, result.InnerNode());
 }
 
