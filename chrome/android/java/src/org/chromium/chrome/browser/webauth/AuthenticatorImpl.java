25025b695bb319b5dbf66c51dbcfada1f6acd938
Dongjing He <dongjing@chromium.org>
2018-07-04 01:45:28 +0000

    Implement isUserVerifyingPlatformAuthenticatorAvailable.
    Bug: 828216
    Change-Id: I9913d26070b69d3d52e35d6f9763e674e7fb9ea6
    Reviewed-on: https://chromium-review.googlesource.com/1103394
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Kim Paulhamus <kpaulhamus@chromium.org>
    Commit-Queue: Kim Paulhamus <kpaulhamus@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572441}

--- chrome/android/java/src/org/chromium/chrome/browser/webauth/AuthenticatorImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webauth/AuthenticatorImpl.java
@@ -5,7 +5,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.webauth;
 
+import android.annotation.TargetApi;
+import android.app.KeyguardManager;
+import android.content.Context;
+import android.os.Build;
+
+import org.chromium.base.PackageUtils;
+import org.chromium.chrome.browser.ChromeActivity;
+import org.chromium.chrome.browser.ChromeFeatureList;
 import org.chromium.content_public.browser.RenderFrameHost;
+import org.chromium.content_public.browser.WebContents;
+import org.chromium.content_public.browser.WebContentsStatics;
 import org.chromium.mojo.system.MojoException;
 import org.chromium.webauth.mojom.Authenticator;
 import org.chromium.webauth.mojom.AuthenticatorStatus;
@@ -19,6 +29,10 @@ import org.chromium.webauth.mojom.PublicKeyCredentialRequestOptions;
  */
 public class AuthenticatorImpl implements Authenticator, HandlerResponseCallback {
     private final RenderFrameHost mRenderFrameHost;
+    private final WebContents mWebContents;
+
+    private static final String GMSCORE_PACKAGE_NAME = "com.google.android.gms";
+    private static final int GMSCORE_MIN_VERSION = 12800000;
 
     /** Ensures only one request is processed at a time. */
     boolean mIsOperationPending = false;
@@ -36,6 +50,7 @@ public class AuthenticatorImpl implements Authenticator, HandlerResponseCallback
     public AuthenticatorImpl(RenderFrameHost renderFrameHost) {
         assert renderFrameHost != null;
         mRenderFrameHost = renderFrameHost;
+        mWebContents = WebContentsStatics.fromRenderFrameHost(renderFrameHost);
     }
 
     @Override
@@ -65,9 +80,23 @@ public class AuthenticatorImpl implements Authenticator, HandlerResponseCallback
     }
 
     @Override
+    @TargetApi(Build.VERSION_CODES.M)
     public void isUserVerifyingPlatformAuthenticatorAvailable(
             IsUserVerifyingPlatformAuthenticatorAvailableResponse callback) {
-        callback.call(false);
+        Context context = ChromeActivity.fromWebContents(mWebContents);
+        if (PackageUtils.getPackageVersion(context, GMSCORE_PACKAGE_NAME)
+            < GMSCORE_MIN_VERSION) {
+            callback.call(false);
+            return;
+        }
+        if (!ChromeFeatureList.isEnabled(ChromeFeatureList.WEB_AUTH)) {
+            callback.call(false);
+            return;
+        }
+
+        KeyguardManager keyguardManager =
+                (KeyguardManager) context.getSystemService(Context.KEYGUARD_SERVICE);
+        callback.call(keyguardManager != null && keyguardManager.isDeviceSecure());
     }
 
     /**
