e91bc0ebf7a680a238c46f4a99315c85af6048c4
Mohamed Heikal <mheikal@google.com>
2018-07-04 02:30:33 +0000

    [Android] Moves Splashscreen showing to WebappActivity#preInflationStartup
    Since now layout inflation is delayed for WebappActivity, the
    splashscreen should be shown in preInflationStartup instead of
    postInflationStartup.
    
    This should fix flickering that may occur on some android devices.
    
    Bug: 855888
    Change-Id: Ica4ef8431b67a15ded1c769a50501323dc545bf3
    Reviewed-on: https://chromium-review.googlesource.com/1119260
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572453}

--- chrome/android/java/src/org/chromium/chrome/browser/webapps/WebappActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/WebappActivity.java
@@ -294,11 +294,7 @@ public class WebappActivity extends SingleTabActivity {
     }
 
     private void onLayoutInflated(ViewGroup mainView) {
-        View placeHolderView = new View(this);
-        setContentView(placeHolderView);
-        ViewGroup contentParent = (ViewGroup) placeHolderView.getParent();
-        WarmupManager.transferViewHeirarchy(mainView, contentParent);
-        contentParent.removeView(placeHolderView);
+        mSplashController.setViewHierarchyBelowSplashscreen(mainView);
         onInitialLayoutInflationComplete();
     }
 
@@ -386,6 +382,7 @@ public class WebappActivity extends SingleTabActivity {
         setTitle(mWebappInfo.shortName());
 
         super.preInflationStartup();
+        initializeWebappData();
     }
 
     @Override
@@ -614,7 +611,6 @@ public class WebappActivity extends SingleTabActivity {
 
     @Override
     public void postInflationStartup() {
-        initializeWebappData();
         if (getBrowserSession() != null) mTrustedWebContentProvider.verifyRelationship();
 
         super.postInflationStartup();
