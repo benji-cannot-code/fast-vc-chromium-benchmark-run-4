f6386f4c85d9f60bab6839c9c0cfaa39aef21902
Matt Falkenhagen <falken@chromium.org>
2018-07-04 08:26:38 +0000

    Add more instrumentation for AppCache crash when S13nSW is enabled.
    We expect the precreated AppCacheHost to exist in
    AppCacheNavigationHandleCore once navigation starts on the IO thread.
    But we know it doesn't exist at some point.  This CL add more logging to
    try to narrow down possibilities of where the host was lost, if it was
    ever created.
    
    Bug: 857005
    Change-Id: I0804b6dd972daf6e28e6ee5bf403e30b94df0b1a
    Reviewed-on: https://chromium-review.googlesource.com/1125649
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572511}

--- content/browser/appcache/appcache_navigation_handle_core.h
+++ content/browser/appcache/appcache_navigation_handle_core.h
@@ -49,6 +49,9 @@ class AppCacheNavigationHandleCore : public AppCacheFrontend {
   AppCacheServiceImpl* GetAppCacheService();
 
   void AddRequestToDebugLog(const GURL& url);
+  void AddDefaultFactoryRunToDebugLog(bool was_request_intercepted);
+  void AddCreateURLLoaderToDebugLog();
+  void AddNavigationStartToDebugLog();
   std::string GetDebugLog();
 
  protected:
@@ -75,6 +78,8 @@ class AppCacheNavigationHandleCore : public AppCacheFrontend {
       network::mojom::URLLoaderFactoryPtr url_loader_factory) override;
 
  private:
+  std::string HostToString();
+
   std::unique_ptr<AppCacheHost> precreated_host_;
   scoped_refptr<ChromeAppCacheService> appcache_service_;
   int appcache_host_id_;
--- content/browser/loader/navigation_url_loader_impl.cc
+++ content/browser/loader/navigation_url_loader_impl.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/trace_event.h"
 #include "components/download/public/common/download_stats.h"
 #include "content/browser/appcache/appcache_navigation_handle.h"
+#include "content/browser/appcache/appcache_navigation_handle_core.h"
 #include "content/browser/appcache/appcache_request_handler.h"
 #include "content/browser/blob_storage/chrome_blob_storage_context.h"
 #include "content/browser/devtools/render_frame_devtools_agent_host.h"
@@ -359,6 +360,9 @@ class NavigationURLLoaderImpl::URLLoaderRequestController
       ServiceWorkerNavigationHandleCore* service_worker_navigation_handle_core,
       AppCacheNavigationHandleCore* appcache_handle_core,
       bool was_request_intercepted) const {
+    if (appcache_handle_core)
+      appcache_handle_core->AddDefaultFactoryRunToDebugLog(
+          was_request_intercepted);
     return base::BindOnce(
         &URLLoaderRequestController::CreateNonNetworkServiceURLLoader,
         weak_factory_.GetWeakPtr(),
@@ -388,6 +392,9 @@ class NavigationURLLoaderImpl::URLLoaderRequestController
       network::mojom::URLLoaderClientPtr url_loader_client) {
     DCHECK(!base::FeatureList::IsEnabled(network::features::kNetworkService));
     DCHECK_CURRENTLY_ON(BrowserThread::IO);
+
+    if (appcache_handle_core)
+      appcache_handle_core->AddCreateURLLoaderToDebugLog();
 
     default_loader_used_ = true;
     if (signed_exchange_utils::IsSignedExchangeHandlingEnabled()) {
@@ -453,6 +460,9 @@ class NavigationURLLoaderImpl::URLLoaderRequestController
     DCHECK(!base::FeatureList::IsEnabled(network::features::kNetworkService));
     DCHECK_CURRENTLY_ON(BrowserThread::IO);
     DCHECK(!started_);
+    if (appcache_handle_core)
+      appcache_handle_core->AddNavigationStartToDebugLog();
+
     started_ = true;
     request_info_ = std::move(request_info);
     frame_tree_node_id_ = request_info_->frame_tree_node_id;
