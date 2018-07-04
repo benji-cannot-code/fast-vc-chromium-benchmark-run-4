26a389ad255af0f5756b08af381190e06d241c6d
Makoto Shimazu <shimazu@chromium.org>
2018-07-04 05:21:29 +0000

    Revert "Switching all ui/gl dependent tests to SwiftShader"
    This reverts commit 9f1eb0bad804bf4ebfe8202fe91413450f9cf062.
    
    Reason for revert: speculative revert due to cc_unittests failures
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Linux%20Tests%20%28dbg%29%281%29%2832%29/51119
    
    Original change's description:
    > Switching all ui/gl dependent tests to SwiftShader
    > 
    > This cl's switches cc_unittests and viz_unittests from OSMesa
    > to SwiftShader and fixes other issues related to that change.
    > 
    > Bug chromium:848035
    > 
    > Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    > Change-Id: Ib1b58e02d4df4a687ef7be57284c06097bdeca94
    > Reviewed-on: https://chromium-review.googlesource.com/1070490
    > Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    > Reviewed-by: danakj <danakj@chromium.org>
    > Reviewed-by: Antoine Labour <piman@chromium.org>
    > Reviewed-by: Eric Karl <ericrk@chromium.org>
    > Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#572442}
    
    TBR=danakj@chromium.org,sugoi@chromium.org,dcastagna@chromium.org,piman@chromium.org,ericrk@chromium.org
    
    Change-Id: I5957fd458b172fd58d4d1fe866e4b07be600c6cb
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Reviewed-on: https://chromium-review.googlesource.com/1125599
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Commit-Queue: Makoto Shimazu <shimazu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572485}

--- ui/gl/gl_implementation.cc
+++ ui/gl/gl_implementation.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "build/build_config.h"
 #include "ui/gl/gl_bindings.h"
-#include "ui/gl/gl_features.h"
 #include "ui/gl/gl_gl_api_implementation.h"
 #include "ui/gl/gl_version_info.h"
 
@@ -123,9 +122,9 @@ GLImplementation GetNamedGLImplementation(const std::string& name) {
 }
 
 GLImplementation GetSoftwareGLImplementation() {
-#if (BUILDFLAG(ENABLE_SWIFTSHADER) &&         \
-     (defined(OS_WIN) || defined(OS_LINUX) || \
-      (defined(OS_MACOSX) && defined(USE_EGL))))
+#if (defined(OS_WIN) ||                                                     \
+     (defined(OS_LINUX) && !defined(OS_CHROMEOS) && !defined(USE_OZONE)) || \
+     (defined(OS_MACOSX) && defined(USE_EGL)))
   return kGLImplementationSwiftShaderGL;
 #else
   return kGLImplementationOSMesaGL;
