0f6e7df1805d1f1663e1e68f4a6f1d1a40545aab
Chris Harrelson <chrishtr@chromium.org>
2018-07-04 01:20:53 +0000

    Fix CompositingContainer for replaced normal-flow stacking contexts.
    For such PaintLayers, the CompositingContainer is the parent, not the
    containing stacking context. This is because such stacking contexts
    paint during the normal-flow of the ancestor layout object.
    
    This also allows us to enable subsequence caching; the previous attempt
    last week was still suffering from the under-invalidation reported
    in issue 859520.
    
    The testcases that crash on ASAN from the referenced bugs are fixed;
    one such example is included in this CL.
    
    Bug:859294,719835,723076,859520
    
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I6ab5271d70bd834482c22b89f09b93907788ed0c
    Reviewed-on: https://chromium-review.googlesource.com/1125269
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572428}

--- third_party/blink/renderer/core/paint/paint_layer.cc
+++ third_party/blink/renderer/core/paint/paint_layer.cc
@@ -1069,6 +1069,8 @@ LayoutPoint PaintLayer::ComputeOffsetFromAncestor(
 }
 
 PaintLayer* PaintLayer::CompositingContainer() const {
+  if (IsReplacedNormalFlowStacking())
+    return Parent();
   if (!GetLayoutObject().StyleRef().IsStacked())
     return IsSelfPaintingLayer() ? Parent() : ContainingLayer();
   if (PaintLayerStackingNode* ancestor_stacking_node =
@@ -2355,7 +2357,7 @@ bool PaintLayer::HitTestContents(HitTestResult& result,
   return true;
 }
 
-bool PaintLayer::IsReplacedNormalFlowStacking() {
+bool PaintLayer::IsReplacedNormalFlowStacking() const {
   if (!GetLayoutObject().IsSVGForeignObject())
     return false;
   if (!GetLayoutObject().StyleRef().HasAutoZIndex())
@@ -2852,9 +2854,8 @@ bool PaintLayer::SupportsSubsequenceCaching() const {
   if (EnclosingPaginationLayer())
     return false;
 
-  // SVG documents paint atomically.
-  if (GetLayoutObject().IsSVGRoot() &&
-      GetLayoutObject().GetDocument().IsSVGDocument())
+  // SVG paints atomically.
+  if (GetLayoutObject().IsSVGRoot())
     return true;
 
   // Create subsequence for only stacking contexts whose painting are atomic.
