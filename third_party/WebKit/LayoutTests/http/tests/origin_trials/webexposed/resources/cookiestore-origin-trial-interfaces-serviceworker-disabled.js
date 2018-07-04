b827e9068fa65796c01b5d388635db99a67c06cc
Victor Costan <pwnall@chromium.org>
2018-07-04 01:43:39 +0000

    Async Cookies: Enable in Origin Trials as CookieStore.
    Intent to experiment:
    https://groups.google.com/a/chromium.org/d/topic/blink-dev/pdxkBoURmaA/discussion
    
    Notification on experimentation-dev:
    https://groups.google.com/a/chromium.org/d/topic/experimentation-dev/xwL0PmF3rq4/discussion
    
    Bug: 729800
    Change-Id: I88aa295f164c68b0700713a143821300724d081f
    Reviewed-on: https://chromium-review.googlesource.com/1123411
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Jason Chase <chasej@chromium.org>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572440}

---
+++ third_party/WebKit/LayoutTests/http/tests/origin_trials/webexposed/resources/cookiestore-origin-trial-interfaces-serviceworker-disabled.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+importScripts('/resources/testharness.js',
+              '/resources/origin-trials-helper.js');
+
+test(t => {
+  OriginTrialsHelper.check_interfaces_missing(
+    self,
+    ['CookieStore', 'ExtendableCookieChangeEvent']);
+}, 'Cookie Store API interfaces in Origin-Trial disabled worker.');
+
+test(t => {
+  assert_false('cookieStore' in self,
+               'cookieStore property does not exist on global scope');
+  assert_false('oncookiechange' in self,
+               'oncookiechange property does not exist on global scope');
+}, 'Cookie Store API entry points in Origin-Trial disabled worker.');
+
+done();
