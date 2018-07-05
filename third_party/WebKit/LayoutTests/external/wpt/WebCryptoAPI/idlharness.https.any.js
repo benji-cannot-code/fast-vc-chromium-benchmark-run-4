a79472035ec503223e04ada01b97fecb1f77a10b
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-07-05 00:11:43 +0000

    Import wpt@7997e96e5a24c24dd6300880f1e9ba80c61886d3
    Using wpt-import in Chromium dacbdbe8413f065d4dbe280a48e63094b35afb43.
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/20774
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    eroman@chromium.org, rsleevi@chromium.org:
      external/wpt/WebCryptoAPI
    
    TBR=markdittmer
    
    No-Export: true
    Change-Id: I7caae2ea2607bc6dcda2389576e842d5fefdc14d
    Reviewed-on: https://chromium-review.googlesource.com/1126493
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572680}

---
+++ third_party/WebKit/LayoutTests/external/wpt/WebCryptoAPI/idlharness.https.any.js
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/resources/WebIDLParser.js
+// META: script=/resources/idlharness.js
+
+// https://w3c.github.io/webcrypto/Overview.html
+
+promise_test(async () => {
+  const idl = await fetch(`/interfaces/WebCryptoAPI.idl`).then(r => r.text());
+
+  const idl_array = new IdlArray();
+  idl_array.add_idls(idl);
+  idl_array.add_objects({
+    Crypto: ['crypto'],
+    SubtleCrypto: ['crypto.subtle']
+  });
+  idl_array.test();
+}, 'WebCryptoAPI interfaces');
