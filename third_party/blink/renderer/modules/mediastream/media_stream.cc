7f96b1f8786063f707f1ae09668351a193aa3f07
Henrik Boström <hbos@chromium.org>
2018-07-05 17:12:11 +0000

    Rename MediaStream add/remove track methods to make things more clear.
    The MediaStream, MediaStreamDescriptorClient and MediaStreamDescriptor
    have some confusingly similar names: addTrack(), AddRemoteTrack(),
    AddTrackByComponent(), etc. The "-ByComponent()" methods are the same
    thing as the "-RemoteTrack()" methods except a MediaStreamTrack has to
    be created for the components. The difference between adding a track
    and adding a "remote track" is the "remote tracks" fire events. This is
    not clear from the naming and other C++ code might want to call these
    methods without the track necessarily being "remote". Generally
    speaking, if the track was added by JavaScript application (addTrack)
    then onaddtrack shouldn't fire, but if it was created by other means it
    should.
    
    The main difference between the different versions is one fires
    onaddtrack/onremovetrack events and the other doesn't. The new names
    are:
    - addTrack() - the JavaScript method, same as before.
    - AddTrackAndFireEvents() - previously AddRemoteTrack().
    - AddTrackByComponentAndFireEvents() - previously AddTrackByComponent().
    
    Bug: 777617
    Change-Id: Ic31a891ca44d9b33b4a15be9cd7d8f728fe3e6d2
    Reviewed-on: https://chromium-review.googlesource.com/1126387
    Commit-Queue: Henrik Boström <hbos@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572819}

--- third_party/blink/renderer/modules/mediastream/media_stream.cc
+++ third_party/blink/renderer/modules/mediastream/media_stream.cc
@@ -400,16 +400,18 @@ const AtomicString& MediaStream::InterfaceName() const {
   return EventTargetNames::MediaStream;
 }
 
-void MediaStream::AddTrackByComponent(MediaStreamComponent* component) {
+void MediaStream::AddTrackByComponentAndFireEvents(
+    MediaStreamComponent* component) {
   DCHECK(component);
   if (!GetExecutionContext())
     return;
   MediaStreamTrack* track =
       MediaStreamTrack::Create(GetExecutionContext(), component);
-  AddRemoteTrack(track);
+  AddTrackAndFireEvents(track);
 }
 
-void MediaStream::RemoveTrackByComponent(MediaStreamComponent* component) {
+void MediaStream::RemoveTrackByComponentAndFireEvents(
+    MediaStreamComponent* component) {
   DCHECK(component);
   if (!GetExecutionContext())
     return;
@@ -448,7 +450,7 @@ void MediaStream::RemoveTrackByComponent(MediaStreamComponent* component) {
   }
 }
 
-void MediaStream::AddRemoteTrack(MediaStreamTrack* track) {
+void MediaStream::AddTrackAndFireEvents(MediaStreamTrack* track) {
   DCHECK(track);
   switch (track->Component()->Source()->GetType()) {
     case MediaStreamSource::kTypeAudio:
@@ -470,9 +472,9 @@ void MediaStream::AddRemoteTrack(MediaStreamTrack* track) {
   }
 }
 
-void MediaStream::RemoveRemoteTrack(MediaStreamTrack* track) {
+void MediaStream::RemoveTrackAndFireEvents(MediaStreamTrack* track) {
   DCHECK(track);
-  RemoveTrackByComponent(track->Component());
+  RemoveTrackByComponentAndFireEvents(track->Component());
 }
 
 void MediaStream::ScheduleDispatchEvent(Event* event) {
