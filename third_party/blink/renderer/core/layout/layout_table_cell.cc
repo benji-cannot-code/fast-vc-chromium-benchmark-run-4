291d5ecc6bf8fda0419800c2200f51aa055612cb
Morten Stenshorne <mstensho@chromium.org>
2018-07-05 10:30:01 +0000

    [LayoutNG] Support anonymous table cells natively.
    Use LayoutObjectFactory when creating anonymous table cells, just
    like we already do for non-anonymous cells.
    We used to create legacy LayoutTableCell objects for anonymous cells,
    but allowed children to be NG. This was bad, since we should only
    switch engines at formatting context boundaries.
    
    One NG change was necessary here: Only anonymous *blocks* should
    inherit % resolution size. Anonymous table cells should NOT. This
    prevents fast/table/percent-height-inside-anonymous-cell.html from
    regressing.
    
    This made one existing test pass. Also added a new one.
    Two tests not related to tables regressed. Separate bugs reported.
    Some rebaselining was also necessary, since NG handles text runs and
    whitespace slightly differently.
    
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng
    Change-Id: Ibbb77073f1fbe1841ebf102c8bce440046f73319
    Reviewed-on: https://chromium-review.googlesource.com/1122218
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: David Grogan <dgrogan@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572753}

--- third_party/blink/renderer/core/layout/layout_table_cell.cc
+++ third_party/blink/renderer/core/layout/layout_table_cell.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/html_names.h"
 #include "third_party/blink/renderer/core/layout/collapsed_border_value.h"
 #include "third_party/blink/renderer/core/layout/layout_analyzer.h"
+#include "third_party/blink/renderer/core/layout/layout_object_factory.h"
 #include "third_party/blink/renderer/core/layout/layout_table_col.h"
 #include "third_party/blink/renderer/core/layout/subtree_layout_scope.h"
 #include "third_party/blink/renderer/core/paint/object_paint_invalidator.h"
@@ -1158,20 +1159,23 @@ void LayoutTableCell::ScrollbarsChanged(bool horizontal_scrollbar_changed,
   }
 }
 
-LayoutTableCell* LayoutTableCell::CreateAnonymous(Document* document) {
-  LayoutTableCell* layout_object = new LayoutTableCell(nullptr);
+LayoutTableCell* LayoutTableCell::CreateAnonymous(
+    Document* document,
+    scoped_refptr<ComputedStyle> style) {
+  LayoutTableCell* layout_object =
+      LayoutObjectFactory::CreateTableCell(*document, *style);
   layout_object->SetDocumentForAnonymous(document);
+  layout_object->SetStyle(std::move(style));
   return layout_object;
 }
 
 LayoutTableCell* LayoutTableCell::CreateAnonymousWithParent(
     const LayoutObject* parent) {
-  LayoutTableCell* new_cell =
-      LayoutTableCell::CreateAnonymous(&parent->GetDocument());
   scoped_refptr<ComputedStyle> new_style =
       ComputedStyle::CreateAnonymousStyleWithDisplay(parent->StyleRef(),
                                                      EDisplay::kTableCell);
-  new_cell->SetStyle(std::move(new_style));
+  LayoutTableCell* new_cell = LayoutTableCell::CreateAnonymous(
+      &parent->GetDocument(), std::move(new_style));
   return new_cell;
 }
 
