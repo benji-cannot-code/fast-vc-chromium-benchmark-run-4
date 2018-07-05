0a4df040bb09dc678702b43af2ec855d546b5f29
Peter Mayo <petermayo@chromium.org>
2018-07-05 22:45:43 +0000

    [BGPT] Create scroll nodes for ScrollTimeline scroll-sources
    AnimationWorklet currently relies on property trees having a ScrollNode
    for composited scrollers even when they don't actually scroll overflow,
    so that it can have a cc::ScrollTimeline based off of them. This CL
    alters the BlinkGenPropertyTrees scroll-node creation logic to create
    such a node.
    
    Bug: 855718
    Cq-Include-Trybots: luci.chromium.try:linux-blink-gen-property-trees;luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I135cc1ace6511315a863638b5ee824ccac88ecb3
    Reviewed-on: https://chromium-review.googlesource.com/1114121
    Commit-Queue: Peter Mayo <petermayo@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572865}

--- third_party/blink/renderer/core/animation/scroll_timeline.cc
+++ third_party/blink/renderer/core/animation/scroll_timeline.cc
@@ -170,6 +170,8 @@ void ScrollTimeline::AttachAnimation() {
   LayoutBoxModelObject* object = scroll_source_->GetLayoutBoxModelObject();
   if (object && object->HasLayer())
     object->Layer()->SetNeedsCompositingInputsUpdate();
+  if (object)
+    object->SetNeedsPaintPropertyUpdate();
 }
 
 void ScrollTimeline::DetachAnimation() {
@@ -185,6 +187,8 @@ void ScrollTimeline::DetachAnimation() {
     LayoutBoxModelObject* object = scroll_source_->GetLayoutBoxModelObject();
     if (object && object->HasLayer())
       object->Layer()->SetNeedsCompositingInputsUpdate();
+    if (object)
+      object->SetNeedsPaintPropertyUpdate();
   }
 }
 
--- third_party/blink/renderer/core/paint/paint_property_tree_builder.cc
+++ third_party/blink/renderer/core/paint/paint_property_tree_builder.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/paint/paint_property_tree_builder.h"
 
 #include <memory>
+#include "third_party/blink/renderer/core/animation/scroll_timeline.h"
 #include "third_party/blink/renderer/core/dom/dom_node_ids.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/frame/local_frame_view.h"
@@ -188,7 +189,10 @@ class FragmentPaintPropertyTreeBuilder {
 static bool NeedsScrollNode(const LayoutObject& object) {
   if (!object.HasOverflowClip())
     return false;
-  return ToLayoutBox(object).GetScrollableArea()->ScrollsOverflow();
+  // TODO(crbug.com/839341): Remove ScrollTimeline check once we support
+  // main-thread AnimationWorklet and don't need to promote the scroll-source.
+  return ToLayoutBox(object).GetScrollableArea()->ScrollsOverflow() ||
+         ScrollTimeline::HasActiveScrollTimeline(object.GetNode());
 }
 
 static CompositingReasons CompositingReasonsForScroll(const LayoutBox& box) {
