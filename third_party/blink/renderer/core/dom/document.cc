d1a4201b1ddf0e0e2021e0a4fd244fd12ddba759
Alice Boxhall <aboxhall@chromium.org>
2018-07-05 05:37:38 +0000

    Match :focus-visible after programmatic focus depending on previous user interaction
    Bug: 817199
    Change-Id: I930a0711153039b6fd7712371c346771c2a749a9
    Reviewed-on: https://chromium-review.googlesource.com/1101310
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Alice Boxhall <aboxhall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572712}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -606,6 +606,8 @@ Document::Document(const DocumentInit& initializer,
       compatibility_mode_(kNoQuirksMode),
       compatibility_mode_locked_(false),
       has_autofocused_(false),
+      last_focus_type_(kWebFocusTypeNone),
+      had_keyboard_event_(false),
       clear_focused_element_timer_(
           GetTaskRunner(TaskType::kInternalUserInteraction),
           this,
@@ -4653,7 +4655,10 @@ bool Document::SetFocusedElement(Element* new_focused_element,
     focused_element_ = new_focused_element;
     SetSequentialFocusNavigationStartingPoint(focused_element_.Get());
 
-    last_focus_type_ = params.type;
+    // Keep track of last focus from user interaction, ignoring focus from code.
+    if (params.type != kWebFocusTypeNone)
+      last_focus_type_ = params.type;
+
     focused_element_->SetFocused(true, params.type);
     focused_element_->SetHasFocusWithinUpToAncestor(true, ancestor);
 
