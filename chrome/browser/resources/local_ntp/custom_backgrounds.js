e1ed559f3d704715c7515e633b615ef1cc839d95
Kyle Milka <kmilka@chromium.org>
2018-07-05 19:25:59 +0000

    [NTP] Allow double-click to select a background
    Bug: 859372
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: I76361b2d7c8eac1e2659bf21a59eab6006d92cc0
    Reviewed-on: https://chromium-review.googlesource.com/1123217
    Commit-Queue: Kyle Milka <kmilka@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572840}

--- chrome/browser/resources/local_ntp/custom_backgrounds.js
+++ chrome/browser/resources/local_ntp/custom_backgrounds.js
@@ -105,6 +105,14 @@ customBackgrounds.closeCollectionDialog = function(menu) {
   customBackgrounds.resetSelectionDialog();
 };
 
+/* Close and reset the dialog, and set the background.
+ * @param {string} url The url of the selected background.
+ */
+customBackgrounds.setBackground = function(url) {
+  customBackgrounds.closeCollectionDialog($(customBackgrounds.IDS.MENU));
+  window.chrome.embeddedSearch.newTabPage.setBackgroundURL(url);
+};
+
 /**
  * Show dialog for selecting a collection. Populates the dialog
  * with data from |coll|.
@@ -252,7 +260,14 @@ customBackgrounds.showImageSelectionDialog = function(dialogTitle) {
           .classList.add(customBackgrounds.CLASSES.DONE_AVAILABLE);
     };
 
-    tile.onclick = tileInteraction;
+    tile.onclick = function(event) {
+      let clickCount = event.detail;
+      if (clickCount == 1) {
+        tileInteraction(event);
+      } else if (clickCount == 2) {
+        customBackgrounds.setBackground(this.dataset.url);
+      }
+    };
     tile.onkeyup = function(event) {
       if (event.keyCode === customBackgrounds.KEYCODES.ENTER) {
         tileInteraction(event);
@@ -439,10 +454,7 @@ customBackgrounds.initCustomBackgrounds = function() {
       return;
     }
 
-    menu.close();
-    window.chrome.embeddedSearch.newTabPage.setBackgroundURL(
-        customBackgrounds.selectedTile.dataset.url);
-    customBackgrounds.resetSelectionDialog();
+    customBackgrounds.setBackground(customBackgrounds.selectedTile.dataset.url);
   };
   $(customBackgrounds.IDS.DONE).onclick = doneInteraction;
   $(customBackgrounds.IDS.DONE).onkeyup = function(event) {
