e30588d1b13737e291468b39fff2ab2a19b44066
Ella Ge <eirage@chromium.org>
2018-07-06 18:58:14 +0000

    content_offset should be in css pixel(dip)
    ContentViewCore::UpdateFrameInfo() must receive positions and sizes in
    CSS pixel except top_shown_pix. When --use-zoom-for-dsf is enabled,
    value given to ContentViewCore::UpdateFrameInfo() should be in dip.
    
    
    TEST= pixel_integration_test with use-zoom-for-dsf enabled.
    
    Bug: 737777
    Change-Id: Ib19f76331bc46e94c99549213e7955ff263f92f6
    Reviewed-on: https://chromium-review.googlesource.com/1115472
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573028}

--- content/browser/renderer_host/render_widget_host_view_android.cc
+++ content/browser/renderer_host/render_widget_host_view_android.cc
@@ -382,7 +382,7 @@ void RenderWidgetHostViewAndroid::OnRenderFrameMetadataChangedBeforeActivation(
                                  metadata.top_controls_shown_ratio,
                                  scrollable_viewport_size_dip);
 
-  // ViewAndroid::content_offset() must be in CSS scale
+  // ViewAndroid::content_offset() must be in dip.
   float top_content_offset_dip = IsUseZoomForDSFEnabled()
                                      ? top_content_offset / dip_scale
                                      : top_content_offset;
@@ -404,7 +404,7 @@ void RenderWidgetHostViewAndroid::OnRenderFrameMetadataChangedBeforeActivation(
         metadata.root_overflow_y_hidden);
   }
 
-  // All offsets and sizes except |top_shown_pix| are in CSS pixels.
+  // All offsets and sizes except |top_shown_pix| are in dip.
   gesture_listener_manager_->UpdateScrollInfo(
       root_scroll_offset_dip, metadata.page_scale_factor,
       metadata.min_page_scale_factor, metadata.max_page_scale_factor,
@@ -1266,17 +1266,17 @@ void RenderWidgetHostViewAndroid::OnFrameMetadataUpdated(
                                  metadata.top_controls_height,
                                  metadata.top_controls_shown_ratio,
                                  scrollable_viewport_size_dip);
-  // ViewAndroid::content_offset() must be in CSS scale
+  // ViewAndroid::content_offset() must be in dip
   float top_content_offset_dip = IsUseZoomForDSFEnabled()
                                      ? top_content_offset / dip_scale
                                      : top_content_offset;
-  view_.UpdateFrameInfo({scrollable_viewport_size_dip, top_content_offset});
+  view_.UpdateFrameInfo({scrollable_viewport_size_dip, top_content_offset_dip});
   bool controls_changed = UpdateControls(
       view_.GetDipScale(), metadata.top_controls_height,
       metadata.top_controls_shown_ratio, metadata.bottom_controls_height,
       metadata.bottom_controls_shown_ratio);
 
-  // All offsets and sizes except |top_shown_pix| are in CSS pixels.
+  // All offsets and sizes except |top_shown_pix| are in dip.
   gesture_listener_manager_->UpdateScrollInfo(
       root_scroll_offset_dip, metadata.page_scale_factor,
       metadata.min_page_scale_factor, metadata.max_page_scale_factor,
