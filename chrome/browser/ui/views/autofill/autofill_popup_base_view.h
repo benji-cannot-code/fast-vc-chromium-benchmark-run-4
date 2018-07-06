f1214d1f3b70fe0adc5d6381bc6edfd95427ef78
Tommy Martino <tmartino@chromium.org>
2018-07-06 18:39:36 +0000

    [Autofill Views] Re-reimplement Scrolling
    This CL:
    - Relands the implementation of scrolling from
      https://chromium-review.googlesource.com/c/chromium/src/+/1072427
    
    - Adjusts the calculation of bounds in that CL to not depend on a
      browser window, which can be null and was causing crashes in Canary,
      and to provide a fallback value.
    
      This CL also deduplicates substantially similar code in
      AutofillPopupBaseView.
    
    FYI: The reland of the prior CL was uploaded as patchset 1, while the
    proposed fix was uploaded as patchset 2. Thus, diffing between patchset
    #1 and the latest patchset should show only those changes proposed
    beyond the code that was previously landed.
    
    Bug: 854183
    Change-Id: Ie41695bbe89a4a7b9645f536cb6fbda75258176a
    Reviewed-on: https://chromium-review.googlesource.com/1110838
    Commit-Queue: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573024}

--- chrome/browser/ui/views/autofill/autofill_popup_base_view.h
+++ chrome/browser/ui/views/autofill/autofill_popup_base_view.h
@@ -77,6 +77,7 @@ class AutofillPopupBaseView : public views::WidgetDelegateView,
   // views::WidgetObserver implementation.
   void OnWidgetBoundsChanged(views::Widget* widget,
                              const gfx::Rect& new_bounds) override;
+  void OnWidgetDestroying(views::Widget* widget) override;
 
   // Stop observing the widget.
   void RemoveObserver();
