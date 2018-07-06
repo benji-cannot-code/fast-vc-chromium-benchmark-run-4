161b32d98b86dcfd68f97bfe01a5b4e4ddc22bd8
Alexei Filippov <alph@chromium.org>
2018-07-06 19:08:17 +0000

    DevTools: Fix flaky test http/tests/devtools/tracing/timeline-js/compile-script.js
    BUG=860668
    
    Change-Id: Ie479bb305a32f1f43ef32920c326dcd2f21ceaf7
    Reviewed-on: https://chromium-review.googlesource.com/1128162
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Alexei Filippov <alph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573033}

--- third_party/WebKit/LayoutTests/http/tests/devtools/tracing/timeline-js/compile-script.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/tracing/timeline-js/compile-script.js
@@ -8,8 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   await TestRunner.loadModule('performance_test_runner');
   await TestRunner.showPanel('timeline');
   await TestRunner.evaluateInPagePromise(`
-      function performActions()
-      {
+      function performActions() {
+        return new Promise(resolve => {
           var script = document.createElement("script");
           script.textContent = "function noop1() {} \\n//# sourceURL=script-content.js";
           document.body.appendChild(script);
@@ -17,10 +17,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
           script = document.createElement("script");
           script.src = "resources/timeline-script-tag-2.js";
+          script.onload = resolve;
           document.body.appendChild(script);
+        });
       }
   `);
 
-  PerformanceTestRunner.performActionsAndPrint(
-      'performActions()', TimelineModel.TimelineModel.RecordType.CompileScript);
+  await PerformanceTestRunner.invokeAsyncWithTimeline('performActions');
+  PerformanceTestRunner.printTimelineRecordsWithDetails(TimelineModel.TimelineModel.RecordType.CompileScript);
+  TestRunner.completeTest();
 })();
