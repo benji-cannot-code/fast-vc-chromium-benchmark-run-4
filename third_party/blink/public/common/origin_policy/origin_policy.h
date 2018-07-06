401aca2c4c2e1800a853fc1b6701c9937dda4ec7
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2018-07-06 13:55:34 +0000

    origin policy: Include <memory> for std::unique_ptr
    This fixes the build with libstdc++ after 00446508ac0 ("Origin Policy:
    Implement Origin Policy object and parser"):
    
        In file included from ../../third_party/blink/common/origin_policy/origin_policy_unittest.cc:5:0:
        ../../third_party/blink/public/common/origin_policy/origin_policy.h:22:15: error: ‘unique_ptr’ in namespace ‘std’ does not name a template type
           static std::unique_ptr<OriginPolicy> From(base::StringPiece);
                       ^~~~~~~~~~
    
    Bug: 819294
    Change-Id: I56864efb91b405b39eb28cdace87c83159454e34
    Reviewed-on: https://chromium-review.googlesource.com/1127897
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Daniel Vogelheim <vogelheim@chromium.org>
    Commit-Queue: Raphael Kubo da Costa (CET) <raphael.kubo.da.costa@intel.com>
    Cr-Commit-Position: refs/heads/master@{#572958}

--- third_party/blink/public/common/origin_policy/origin_policy.h
+++ third_party/blink/public/common/origin_policy/origin_policy.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_PUBLIC_COMMON_ORIGIN_POLICY_ORIGIN_POLICY_H_
 #define THIRD_PARTY_BLINK_PUBLIC_COMMON_ORIGIN_POLICY_ORIGIN_POLICY_H_
 
+#include <memory>
 #include <string>
 
 #include "base/strings/string_piece.h"
