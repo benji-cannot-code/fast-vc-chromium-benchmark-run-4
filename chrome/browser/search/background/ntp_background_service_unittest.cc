c5c417545991dcc0f332be7d3d25a960c2c3f747
Ramya Nagarajan <ramyan@chromium.org>
2018-07-06 16:20:23 +0000

    [NTP] Added action_url for background image attributions.
    The action_url provides a way to find out more information about the image,
    and will be used on the NTP background in a subsequent CL.
    
    Bug: 850309
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: I8db5fde15a3041a873e83599a905b0eb1cc9e83c
    Reviewed-on: https://chromium-review.googlesource.com/1127479
    Commit-Queue: Ramya Nagarajan <ramyan@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572986}

--- chrome/browser/search/background/ntp_background_service_unittest.cc
+++ chrome/browser/search/background/ntp_background_service_unittest.cc
@@ -159,6 +159,7 @@ TEST_F(NtpBackgroundServiceTest, GoodCollectionImagesResponse) {
   image.set_asset_id(12345);
   image.set_image_url("https://wallpapers.co/some_image");
   image.add_attribution()->set_text("attribution text");
+  image.set_action_url("https://wallpapers.co/some_image/learn_more");
   ntp::background::GetImagesInCollectionResponse response;
   *response.add_images() = image;
   std::string response_string;
@@ -178,6 +179,7 @@ TEST_F(NtpBackgroundServiceTest, GoodCollectionImagesResponse) {
   collection_image.thumbnail_image_url = GURL(image.image_url());
   collection_image.image_url = GURL(image.image_url() + kImageOptions);
   collection_image.attribution.push_back(image.attribution(0).text());
+  collection_image.attribution_action_url = GURL(image.action_url());
 
   EXPECT_FALSE(service()->collection_images().empty());
   EXPECT_THAT(service()->collection_images().at(0), Eq(collection_image));
