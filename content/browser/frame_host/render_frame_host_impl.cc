38553aec4379b0fd4487fc823881823275497770
Rakina Zata Amni <rakina@chromium.org>
2018-07-06 08:17:25 +0000

    Rebind find_in_page_ pointer in RFH after error
    After a renderer crash, a reused RFH might still have a disconnected
    find_in_page_ pointer. This CL rebinds the RFH to the new renderer if
    that happens.
    
    Bug: 819919
    Change-Id: Iaec9fdc5eec4a46cc5e81dd98a5492d090d8c12b
    Reviewed-on: https://chromium-review.googlesource.com/1075951
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572919}

--- content/browser/frame_host/render_frame_host_impl.cc
+++ content/browser/frame_host/render_frame_host_impl.cc
@@ -4068,7 +4068,8 @@ RenderFrameHostImpl::GetMojoImageDownloader() {
 
 const blink::mojom::FindInPageAssociatedPtr&
 RenderFrameHostImpl::GetFindInPage() {
-  if (!find_in_page_)
+  if (!find_in_page_ || !find_in_page_.is_bound() ||
+      find_in_page_.encountered_error())
     GetRemoteAssociatedInterfaces()->GetInterface(&find_in_page_);
   return find_in_page_;
 }
