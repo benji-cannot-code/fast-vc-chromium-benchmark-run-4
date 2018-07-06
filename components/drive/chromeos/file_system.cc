ba7c9403422ec7d0fd81c0b20f4919986e223478
Stuart Langley <slangley@chromium.org>
2018-07-06 06:03:58 +0000

    Record update time, error for team drives and report them on debug page.
    Bug: 849813
    Change-Id: I2c2d86b79d26c0647ac4ee1244e8f3b5ac7a8fba
    Reviewed-on: https://chromium-review.googlesource.com/1127506
    Reviewed-by: Sasha Morrissey <sashab@chromium.org>
    Commit-Queue: Stuart Langley <slangley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572907}

--- components/drive/chromeos/file_system.cc
+++ components/drive/chromeos/file_system.cc
@@ -295,7 +295,6 @@ FileSystem::FileSystem(EventLogger* logger,
       cache_(cache),
       scheduler_(scheduler),
       resource_metadata_(resource_metadata),
-      last_update_check_error_(FILE_ERROR_OK),
       blocking_task_runner_(blocking_task_runner),
       temporary_file_directory_(temporary_file_directory),
       weak_ptr_factory_(this) {
@@ -404,18 +403,17 @@ void FileSystem::CheckForUpdates() {
 void FileSystem::OnUpdateChecked(const std::string& team_drive_id,
                                  const base::RepeatingClosure& closure,
                                  FileError error) {
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
   DVLOG(1) << "CheckForUpdates finished: " << FileErrorToString(error);
-  // TODO(slangley): Store the error reasons for team drives and show them on
-  // chrome://drive-internals.
-  if (team_drive_id.empty()) {
-    last_update_check_error_ = error;
-  }
+
+  last_update_metadata_[team_drive_id].last_update_check_error = error;
+  last_update_metadata_[team_drive_id].last_update_check_time =
+      base::Time::Now();
   closure.Run();
 }
 
 void FileSystem::OnUpdateCompleted() {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
-  last_update_check_time_ = base::Time::Now();
 }
 
 void FileSystem::AddObserver(FileSystemObserver* observer) {
@@ -886,6 +884,8 @@ void FileSystem::OnTeamDrivesChanged(const FileChange& changed_team_drives) {
             team_drive_change_list_loaders_.find(change.team_drive_id());
         DCHECK(it != team_drive_change_list_loaders_.end());
         team_drive_change_list_loaders_.erase(it);
+        // If we were tracking the update status we can remove that as well.
+        last_update_metadata_.erase(change.team_drive_id());
       } else if (change.IsAddOrUpdate()) {
         DCHECK(team_drive_change_list_loaders_.count(change.team_drive_id()) ==
                0);
@@ -957,8 +957,12 @@ void FileSystem::GetMetadata(
                      base::Owned(team_drive_metadata)));
 
   metadata->refreshing = default_corpus_change_list_loader_->IsRefreshing();
-  metadata->last_update_check_time = last_update_check_time_;
-  metadata->last_update_check_error = last_update_check_error_;
+  metadata->last_update_check_time =
+      last_update_metadata_[util::kTeamDriveIdDefaultCorpus]
+          .last_update_check_time;
+  metadata->last_update_check_error =
+      last_update_metadata_[util::kTeamDriveIdDefaultCorpus]
+          .last_update_check_error;
 
   base::PostTaskAndReplyWithResult(
       blocking_task_runner_.get(), FROM_HERE,
@@ -969,8 +973,13 @@ void FileSystem::GetMetadata(
       base::Bind(&OnGetStartPageToken, closure));
 
   for (auto& team_drive : team_drive_change_list_loaders_) {
+    const FileSystemMetadata& last_update_metadata =
+        last_update_metadata_[team_drive.first];
     FileSystemMetadata& md = (*team_drive_metadata)[team_drive.first];
+
     md.refreshing = team_drive.second->IsRefreshing();
+    md.last_update_check_time = last_update_metadata.last_update_check_time;
+    md.last_update_check_error = last_update_metadata.last_update_check_error;
 
     base::PostTaskAndReplyWithResult(
         blocking_task_runner_.get(), FROM_HERE,
