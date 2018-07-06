3eefaa998c3402baf76a35d3be429bcb67d94654
Robbie McElrath <rmcelrath@chromium.org>
2018-07-06 19:22:11 +0000

    Migrate MutableProfileOAuth2TokenServiceDelegate to NetworkConnectionTracker
    This migrates MutableProfileOAuth2TokenServiceDelegate from using
    net::NetworkChangeNotifier to content::NetworkConnectionTracker, which
    works with the network service enabled.
    
    Bug: 859138
    Change-Id: Ief59738e2f9a955d36fc61e345e47cc9c82017c3
    Reviewed-on: https://chromium-review.googlesource.com/1123707
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573034}

--- chrome/browser/signin/mutable_profile_oauth2_token_service_delegate.cc
+++ chrome/browser/signin/mutable_profile_oauth2_token_service_delegate.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/metrics/histogram_macros.h"
 #include "build/build_config.h"
+#include "chrome/browser/browser_process.h"
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/pref_service.h"
 #include "components/signin/core/browser/signin_client.h"
@@ -360,7 +361,8 @@ MutableProfileOAuth2TokenServiceDelegate::
   backoff_policy_.maximum_backoff_ms = 15 * 60 * 1000;
   backoff_policy_.entry_lifetime_ms = -1;
   backoff_policy_.always_use_initial_delay = false;
-  net::NetworkChangeNotifier::AddNetworkChangeObserver(this);
+  g_browser_process->network_connection_tracker()->AddNetworkConnectionObserver(
+      this);
 
 #if !defined(OS_CHROMEOS)
   // Ensure the device ID is not empty.
@@ -373,7 +375,8 @@ MutableProfileOAuth2TokenServiceDelegate::
     ~MutableProfileOAuth2TokenServiceDelegate() {
   VLOG(1) << "MutablePO2TS::~MutablePO2TS";
   DCHECK(server_revokes_.empty());
-  net::NetworkChangeNotifier::RemoveNetworkChangeObserver(this);
+  g_browser_process->network_connection_tracker()
+      ->RemoveNetworkConnectionObserver(this);
 }
 
 // static
@@ -887,8 +890,8 @@ void MutableProfileOAuth2TokenServiceDelegate::Shutdown() {
   refresh_tokens_.clear();
 }
 
-void MutableProfileOAuth2TokenServiceDelegate::OnNetworkChanged(
-    net::NetworkChangeNotifier::ConnectionType type) {
+void MutableProfileOAuth2TokenServiceDelegate::OnConnectionChanged(
+    network::mojom::ConnectionType type) {
   // If our network has changed, reset the backoff timer so that errors caused
   // by a previous lack of network connectivity don't prevent new requests.
   backoff_entry_.Reset();
