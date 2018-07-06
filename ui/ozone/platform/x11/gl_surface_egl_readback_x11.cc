d3a5d9ff3e5827971c1c9be4d3151ed309d8b82a
kylechar <kylechar@chromium.org>
2018-07-06 19:23:17 +0000

    Fix SwiftShader memory leak with Ozone.
    GLSurfaceEglReadbackX11::Initialize() can be called more than once, for
    example on Resize(), and we would always recreate the window GC. Check
    if window GC already exists and don't recreate in that case. Also ensure
    that Destroy() also destroys the EGLSurface by calling parents
    Destroy().
    
    Bug: swiftshader:67
    Change-Id: I855b622e4e7180be60da410df3051d9174efb77f
    Reviewed-on: https://chromium-review.googlesource.com/1128170
    Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    Commit-Queue: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573036}

--- ui/ozone/platform/x11/gl_surface_egl_readback_x11.cc
+++ ui/ozone/platform/x11/gl_surface_egl_readback_x11.cc
@@ -16,6 +16,10 @@ bool GLSurfaceEglReadbackX11::Initialize(gl::GLSurfaceFormat format) {
   if (!GLSurfaceEglReadback::Initialize(format))
     return false;
 
+  // We don't need to reinitialize |window_graphics_context_|.
+  if (window_graphics_context_)
+    return true;
+
   window_graphics_context_ = XCreateGC(xdisplay_, window_, 0, nullptr);
   if (!window_graphics_context_) {
     DLOG(ERROR) << "XCreateGC failed";
@@ -43,6 +47,8 @@ void GLSurfaceEglReadbackX11::Destroy() {
   }
 
   XSync(xdisplay_, x11::False);
+
+  GLSurfaceEglReadback::Destroy();
 }
 
 bool GLSurfaceEglReadbackX11::Resize(const gfx::Size& size,
