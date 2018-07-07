814a33e63d1b511d7be9aa37559561b9425981ad
Jan Wilken Doerrie <jdoerrie@chromium.org>
2018-07-07 10:48:19 +0000

    [bluetooth] Add WinRT Skeletons for GATT Characteristics
    Similarly to r568056 this change adds skeletons required for the
    implementation and testing of GATT Characteristic discovery on WinRT.
    No functionality is added, this will follow in a future CL.
    
    Bug: 821766
    Change-Id: I68ea08d15036159fedb75cc322ccaabe833aff02
    Reviewed-on: https://chromium-review.googlesource.com/1127945
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573161}

---
+++ device/bluetooth/test/fake_gatt_characteristics_result_winrt.cc
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "device/bluetooth/test/fake_gatt_characteristics_result_winrt.h"
+
+namespace device {
+
+namespace {
+
+using ABI::Windows::Foundation::Collections::IVectorView;
+using ABI::Windows::Foundation::IReference;
+using ABI::Windows::Devices::Bluetooth::GenericAttributeProfile::
+    GattCharacteristic;
+using ABI::Windows::Devices::Bluetooth::GenericAttributeProfile::
+    GattCommunicationStatus;
+
+}  // namespace
+
+FakeGattCharacteristicsResultWinrt::FakeGattCharacteristicsResultWinrt() =
+    default;
+
+FakeGattCharacteristicsResultWinrt::~FakeGattCharacteristicsResultWinrt() =
+    default;
+
+HRESULT FakeGattCharacteristicsResultWinrt::get_Status(
+    GattCommunicationStatus* value) {
+  return E_NOTIMPL;
+}
+
+HRESULT FakeGattCharacteristicsResultWinrt::get_ProtocolError(
+    IReference<uint8_t>** value) {
+  return E_NOTIMPL;
+}
+
+HRESULT FakeGattCharacteristicsResultWinrt::get_Characteristics(
+    IVectorView<GattCharacteristic*>** value) {
+  return E_NOTIMPL;
+}
+
+}  // namespace device
