8823399595d3b0dab81d6ab0a7f8fd0a981074df
Devlin Cronin <rdevlin.cronin@chromium.org>
2018-07-07 00:52:20 +0000

    [Extensions Cleanup] Streamline Extension::GetBaseURLFromExtensionId
    Extension::GetBaseURLFromExtensionId constructs a GURL object from a
    given extension id. Streamline this to be faster by
    - using base::StrCat instead of std::string::operator+
    - not constructing an extra std::string for the scheme
    - omitting the unnecessary trailing "/" addition
    
    Bug: None
    Change-Id: Id710c1821860f7c811b25fd79dae247613375a92
    Reviewed-on: https://chromium-review.googlesource.com/1128270
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573130}

--- extensions/common/extension.cc
+++ extensions/common/extension.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/singleton.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/stl_util.h"
+#include "base/strings/strcat.h"
 #include "base/strings/string16.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_piece.h"
@@ -323,8 +324,8 @@ bool Extension::FormatPEMForFileOutput(const std::string& input,
 
 // static
 GURL Extension::GetBaseURLFromExtensionId(const std::string& extension_id) {
-  return GURL(std::string(extensions::kExtensionScheme) +
-              url::kStandardSchemeSeparator + extension_id + "/");
+  return GURL(base::StrCat({extensions::kExtensionScheme,
+                            url::kStandardSchemeSeparator, extension_id}));
 }
 
 bool Extension::OverlapsWithOrigin(const GURL& origin) const {
