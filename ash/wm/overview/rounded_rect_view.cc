d2ca0bec5b4f02a813e04cc1c74ce77394455ea9
Manu Cornet <manucornet@chromium.org>
2018-07-09 23:40:17 +0000

    Support rounded rects that don't have the same radius on all 4 corners
    Bug: 805612
    Change-Id: I86af86a62fe2e46691db6bfb2b24da404c93ec1d
    Reviewed-on: https://chromium-review.googlesource.com/1130059
    Commit-Queue: Manu Cornet <manucornet@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573528}

--- ash/wm/overview/rounded_rect_view.cc
+++ ash/wm/overview/rounded_rect_view.cc
@@ -11,16 +11,33 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 
 RoundedRectView::RoundedRectView(int corner_radius, SkColor background_color)
-    : corner_radius_(corner_radius), background_color_(background_color) {}
+    : RoundedRectView(corner_radius,
+                      corner_radius,
+                      corner_radius,
+                      corner_radius,
+                      background_color) {}
+
+RoundedRectView::RoundedRectView(int top_left_radius,
+                                 int top_right_radius,
+                                 int bottom_right_radius,
+                                 int bottom_left_radius,
+                                 SkColor background_color)
+    : top_left_radius_(top_left_radius),
+      top_right_radius_(top_right_radius),
+      bottom_right_radius_(bottom_right_radius),
+      bottom_left_radius_(bottom_left_radius),
+      background_color_(background_color) {}
 
 RoundedRectView::~RoundedRectView() = default;
 
 void RoundedRectView::OnPaint(gfx::Canvas* canvas) {
   views::View::OnPaint(canvas);
 
-  SkScalar radius = SkIntToScalar(corner_radius_);
-  const SkScalar kRadius[8] = {radius, radius, radius, radius,
-                               radius, radius, radius, radius};
+  const SkScalar kRadius[8] = {
+      SkIntToScalar(top_left_radius_),     SkIntToScalar(top_left_radius_),
+      SkIntToScalar(top_right_radius_),    SkIntToScalar(top_right_radius_),
+      SkIntToScalar(bottom_right_radius_), SkIntToScalar(bottom_right_radius_),
+      SkIntToScalar(bottom_left_radius_),  SkIntToScalar(bottom_left_radius_)};
   SkPath path;
   gfx::Rect bounds(size());
   path.addRoundRect(gfx::RectToSkRect(bounds), kRadius);
@@ -37,12 +54,26 @@ void RoundedRectView::SetBackgroundColor(SkColor background_color) {
   SchedulePaint();
 }
 
-void RoundedRectView::SetCornerRadius(int radius) {
-  if (corner_radius_ == radius)
+void RoundedRectView::SetCornerRadius(int top_left_radius,
+                                      int top_right_radius,
+                                      int bottom_right_radius,
+                                      int bottom_left_radius) {
+  if (top_left_radius_ == top_left_radius &&
+      top_right_radius_ == top_right_radius &&
+      bottom_right_radius_ == bottom_right_radius &&
+      bottom_left_radius_ == bottom_left_radius) {
     return;
+  }
 
-  corner_radius_ = radius;
+  top_left_radius_ = top_left_radius;
+  top_right_radius_ = top_right_radius;
+  bottom_right_radius_ = bottom_right_radius;
+  bottom_left_radius_ = bottom_left_radius;
   SchedulePaint();
+}
+
+void RoundedRectView::SetCornerRadius(int radius) {
+  SetCornerRadius(radius, radius, radius, radius);
 }
 
 }  // namespace ash
