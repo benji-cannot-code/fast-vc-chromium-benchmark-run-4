31bd1b57ef65ba994c34f9e97fae10a9b8e6fa88
Sylvain Defresne <sdefresne@chromium.org>
2018-07-09 10:11:35 +0000

    Convert SyncConfirmationUI to use IdentityManager
    IdentityManager is the recommended API to access the
    Google account used by the user and SigninManager is
    deprecated.
    
    Convert SyncConfirmationUI to the new API.
    
    Bug: 798408
    Change-Id: I7d44e3b547bf694cefe57488fd8983bba064f0bd
    Reviewed-on: https://chromium-review.googlesource.com/1124465
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573261}

--- chrome/browser/ui/webui/signin/sync_confirmation_ui.cc
+++ chrome/browser/ui/webui/signin/sync_confirmation_ui.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_avatar_icon_util.h"
 #include "chrome/browser/signin/account_consistency_mode_manager.h"
-#include "chrome/browser/signin/signin_manager_factory.h"
+#include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/signin/unified_consent_helper.h"
 #include "chrome/browser/sync/profile_sync_service_factory.h"
 #include "chrome/browser/ui/webui/signin/sync_confirmation_handler.h"
@@ -19,10 +19,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/browser_resources.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/signin/core/browser/avatar_icon_util.h"
-#include "components/signin/core/browser/signin_manager.h"
 #include "components/strings/grit/components_strings.h"
 #include "content/public/browser/web_ui.h"
 #include "content/public/browser/web_ui_data_source.h"
+#include "services/identity/public/cpp/identity_manager.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/webui/web_ui_util.h"
 
@@ -93,8 +93,8 @@ SyncConfirmationUI::SyncConfirmationUI(content::WebUI* web_ui)
 
     constexpr int kAccountPictureSize = 68;
     std::string custom_picture_url = profiles::GetPlaceholderAvatarIconUrl();
-    GURL account_picture_url(SigninManagerFactory::GetForProfile(profile)
-                                 ->GetAuthenticatedAccountInfo()
+    GURL account_picture_url(IdentityManagerFactory::GetForProfile(profile)
+                                 ->GetPrimaryAccountInfo()
                                  .picture_url);
     if (account_picture_url.is_valid()) {
       custom_picture_url = signin::GetAvatarImageURLWithOptions(
