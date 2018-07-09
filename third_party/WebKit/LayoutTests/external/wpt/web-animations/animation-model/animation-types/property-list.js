570e42b2f85bed47ef55e90c9972952206fc9ecd
Majid Valipour <majidvp@chromium.org>
2018-07-09 16:25:32 +0000

    [testing] Remove per platform expectations for wpt animation-types tests
    The only difference that required per platform expectation files was
    the fact that 'hyphens: auto' is only supported in Chromium on Mac.
    
    These tests don't care about the actual css property value but how one
    value is animated to another in web animation interpolation model. So instead of
    using 'auto' for hyphens, we use 'none' which is supported on all platforms.
    This help remove three 500+ lines per platform expectation files and reduce
    unnecessary maintenance burden.
    
    Change-Id: I5f3b716ce11a72a5235e7899d2e90f4bbea77ebc
    Bug: 860359
    Reviewed-on: https://chromium-review.googlesource.com/1129201
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Majid Valipour <majidvp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573328}

--- third_party/WebKit/LayoutTests/external/wpt/web-animations/animation-model/animation-types/property-list.js
+++ third_party/WebKit/LayoutTests/external/wpt/web-animations/animation-model/animation-types/property-list.js
@@ -706,7 +706,7 @@ const gCSSProperties = {
   'hyphens': {
     // https://drafts.csswg.org/css-text-3/#propdef-hyphens
     types: [
-      { type: 'discrete', options: [ [ 'manual', 'auto' ] ] }
+      { type: 'discrete', options: [ [ 'manual', 'none' ] ] }
     ]
   },
   'image-orientation': {
