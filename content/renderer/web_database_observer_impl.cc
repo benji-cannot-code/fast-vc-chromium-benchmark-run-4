d31f95f1ff397275d79bf07a23f8115fec1a2efd
Jeremy Roman <jbroman@chromium.org>
2018-07-09 16:02:50 +0000

    webdatabase: Remove unused WebDatabaseObserverImpl::open_connections_.
    Change-Id: I1e14b3451a221931ad5dd55576e0cd476121e6cc
    Reviewed-on: https://chromium-review.googlesource.com/1127855
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573322}

--- content/renderer/web_database_observer_impl.cc
+++ content/renderer/web_database_observer_impl.cc
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/single_thread_task_runner.h"
 #include "base/strings/string16.h"
 #include "base/threading/thread_task_runner_handle.h"
-#include "content/renderer/storage_util.h"
-#include "storage/common/database/database_identifier.h"
 #include "third_party/blink/public/platform/web_security_origin.h"
 #include "third_party/blink/public/platform/web_string.h"
 #include "third_party/sqlite/sqlite3.h"
@@ -63,7 +61,6 @@ int DetermineHistogramResult(int websql_error, int sqlite_error) {
 WebDatabaseObserverImpl::WebDatabaseObserverImpl(
     scoped_refptr<blink::mojom::ThreadSafeWebDatabaseHostPtr> web_database_host)
     : web_database_host_(std::move(web_database_host)),
-      open_connections_(new storage::DatabaseConnectionsWrapper),
       main_thread_task_runner_(base::ThreadTaskRunnerHandle::Get()) {
   DCHECK(main_thread_task_runner_);
 }
@@ -75,8 +72,6 @@ void WebDatabaseObserverImpl::DatabaseOpened(
     const WebString& database_name,
     const WebString& database_display_name,
     unsigned long estimated_size) {
-  open_connections_->AddOpenConnection(storage::GetIdentifierFromOrigin(origin),
-                                       database_name.Utf16());
   GetWebDatabaseHost().Opened(origin, database_name.Utf16(),
                               database_display_name.Utf16(), estimated_size);
 }
@@ -90,8 +85,6 @@ void WebDatabaseObserverImpl::DatabaseClosed(const WebSecurityOrigin& origin,
                                              const WebString& database_name) {
   DCHECK(!main_thread_task_runner_->RunsTasksInCurrentSequence());
   GetWebDatabaseHost().Closed(origin, database_name.Utf16());
-  open_connections_->RemoveOpenConnection(
-      storage::GetIdentifierFromOrigin(origin), database_name.Utf16());
 }
 
 void WebDatabaseObserverImpl::ReportOpenDatabaseResult(
@@ -164,12 +157,6 @@ void WebDatabaseObserverImpl::ReportVacuumDatabaseResult(
   UMA_HISTOGRAM_ENUMERATION("websql.Async.VacuumResult",
                               result, kResultHistogramSize);
   HandleSqliteError(origin, database_name, sqlite_error);
-}
-
-bool WebDatabaseObserverImpl::WaitForAllDatabasesToClose(
-    base::TimeDelta timeout) {
-  DCHECK(main_thread_task_runner_->RunsTasksInCurrentSequence());
-  return open_connections_->WaitForAllDatabasesToClose(timeout);
 }
 
 void WebDatabaseObserverImpl::HandleSqliteError(const WebSecurityOrigin& origin,
