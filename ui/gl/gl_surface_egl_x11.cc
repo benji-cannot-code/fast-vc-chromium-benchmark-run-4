0c143aadd45ac3dd50c7c719feb3bda7e968637c
Nicolas Capens <capn@chromium.org>
2018-07-09 13:34:12 +0000

    Destroy EGL surface before X11 window.
    EGL can still be using the window, so we first need to destroy the
    surface, then the window.
    
    BUG=824522
    
    Change-Id: Ifa4e51cef3f625e9fc6abedfce6336f7bf835e7c
    Reviewed-on: https://chromium-review.googlesource.com/1128465
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nicolas Capens <capn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573288}

--- ui/gl/gl_surface_egl_x11.cc
+++ ui/gl/gl_surface_egl_x11.cc
@@ -53,6 +53,8 @@ bool NativeViewGLSurfaceEGLX11::InitializeNativeWindow() {
 }
 
 void NativeViewGLSurfaceEGLX11::Destroy() {
+  NativeViewGLSurfaceEGL::Destroy();
+
   if (window_) {
     if (PlatformEventSource* source = PlatformEventSource::GetInstance())
       source->RemovePlatformEventDispatcher(this);
@@ -62,8 +64,6 @@ void NativeViewGLSurfaceEGLX11::Destroy() {
     window_ = 0;
     XFlush(x11_display);
   }
-
-  NativeViewGLSurfaceEGL::Destroy();
 }
 
 EGLConfig NativeViewGLSurfaceEGLX11::GetConfig() {
