83c72b5c13d080cc7f72d4ee73d8e2bbc985de78
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-07-09 19:56:46 +0000

    Import wpt@028ff8d15432d595df85c2becf66cce686659de7
    Using wpt-import in Chromium 7b9a39fe9a449fbf02b013254be8ec6f45103b2c.
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/21128
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    jochen@chromium.org, estark@chromium.org:
      external/wpt/referrer-policy
    mkwst@chromium.org:
      external/wpt/mixed-content
    rouslan@chromium.org, mathp@chromium.org:
      external/wpt/payment-method-basic-card
      external/wpt/payment-request
    
    TBR=kereliuk
    
    No-Export: true
    Change-Id: Ibcc9ba57d01f2ef4bc11e36f6d92eff08c22855f
    Reviewed-on: https://chromium-review.googlesource.com/1129371
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573415}

---
+++ third_party/WebKit/LayoutTests/external/wpt/netinfo/idlharness.any.js
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/resources/WebIDLParser.js
+// META: script=/resources/idlharness.js
+
+'use strict';
+
+// https://wicg.github.io/netinfo/
+
+idl_test(
+  ['netinfo'],
+  ['html', 'dom'],
+  idl_array => {
+    idl_array.add_objects({ NetworkInformation: ['navigator.connection'] });
+    if (self.isWorker) {
+      idl_array.add_objects({ WorkerNavigator: ['navigator'] });
+    } else {
+      idl_array.add_objects({ Navigator: ['navigator'] });
+    }
+  },
+  'netinfo interfaces.');
---
+++ third_party/WebKit/LayoutTests/external/wpt/payment-method-basic-card/idlharness.window.js
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/resources/WebIDLParser.js
+// META: script=/resources/idlharness.js
+
+'use strict';
+
+// https://w3c.github.io/payment-method-basic-card/
+
+idl_test(
+  ['payment-method-basic-card'],
+  [], // No deps
+  null, // No objects
+  'payment-method-basic-card interfaces.');
