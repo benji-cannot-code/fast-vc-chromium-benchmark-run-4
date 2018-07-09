2f04c3c39bafe4c628dfa3b8965941036beb79e8
Dominic Mazzoni <dmazzoni@chromium.org>
2018-07-09 07:24:28 +0000

    Remove AXTableHeaderContainer and AXTableColumn from blink.
    The previous patch (crrev.com/c/1100553) refactored the codebase to no
    longer require these classes. This change removes them, rebases a couple
    of layout tests, and deletes one layout test that's completely
    superfluous now.
    
    Bug: 832289
    Change-Id: Ia9942113b4b740f72205d788328bdd70bc21080c
    Reviewed-on: https://chromium-review.googlesource.com/1121111
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573238}

--- third_party/blink/renderer/modules/accessibility/ax_layout_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_layout_object.cc
@@ -97,9 +97,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/svg/svg_svg_element.h"
 #include "third_party/blink/renderer/modules/accessibility/ax_image_map_link.h"
 #include "third_party/blink/renderer/modules/accessibility/ax_inline_text_box.h"
+#include "third_party/blink/renderer/modules/accessibility/ax_mock_object.h"
 #include "third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.h"
 #include "third_party/blink/renderer/modules/accessibility/ax_svg_root.h"
-#include "third_party/blink/renderer/modules/accessibility/ax_table_column.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/blink/renderer/platform/graphics/image_data_buffer.h"
 #include "third_party/blink/renderer/platform/text/platform_locale.h"
@@ -3102,18 +3102,6 @@ void AXLayoutObject::RowHeaders(AXObjectVector& headers) const {
     AXNodeObject::RowHeaders(headers);
 }
 
-AXObject* AXLayoutObject::HeaderContainer() {
-  for (const auto& child : Children()) {
-    if (child->RoleValue() == kTableHeaderContainerRole)
-      return child;
-  }
-
-  AXMockObject* header_container =
-      ToAXMockObject(AXObjectCache().GetOrCreate(kTableHeaderContainerRole));
-  header_container->SetParent(this);
-  return header_container;
-}
-
 AXObject* AXLayoutObject::HeaderObject() const {
   LayoutObject* layout_object = GetLayoutObject();
   if (!layout_object || !layout_object->IsTableRow())
@@ -3413,21 +3401,6 @@ void AXLayoutObject::AddTableChildren() {
           children_.push_front(caption_object);
       }
     }
-  }
-
-  for (unsigned i = 0; i < ColumnCount(); i++) {
-    AXTableColumn* column = ToAXTableColumn(ax_cache.GetOrCreate(kColumnRole));
-    column->SetColumnIndex(i);
-    column->SetParent(this);
-    if (!column->AccessibilityIsIgnored())
-      children_.push_back(column);
-  }
-
-  AXObject* header_container_object = HeaderContainer();
-  if (header_container_object &&
-      !header_container_object->AccessibilityIsIgnored()) {
-    children_.push_back(header_container_object);
-    header_container_object->SetParent(this);
   }
 }
 
