8cee835930593cc381f33c7e7d29001508e36a5c
Yuki Awano <yawano@chromium.org>
2018-07-09 07:10:39 +0000

    Observe TouchExplorationPassThrough property change
    - ArcAccessibilityHelperBridge updates TouchExplorationPassThrough
      property after TouchExplorationManager reads it. It was the cause of
      the issue. But, in general, it can be updated any time. This CL
      changes the manager to observe property change and update state every
      time when it's changed.
    
    Bug: 854919
    Test: None
    Change-Id: I3413b87e818b6043d9ac0167dd1eba176f100022
    Reviewed-on: https://chromium-review.googlesource.com/1114577
    Commit-Queue: Yuki Awano <yawano@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573236}

--- ash/accessibility/touch_exploration_manager.h
+++ ash/accessibility/touch_exploration_manager.h
@@ -35,6 +35,7 @@ class RootWindowController;
 // on Windows, which we don't ship anymore.
 class ASH_EXPORT TouchExplorationManager
     : public AccessibilityObserver,
+      public aura::WindowObserver,
       public TouchExplorationControllerDelegate,
       public TouchAccessibilityEnablerDelegate,
       public display::DisplayObserver,
@@ -48,6 +49,11 @@ class ASH_EXPORT TouchExplorationManager
 
   // AccessibilityObserver overrides:
   void OnAccessibilityStatusChanged() override;
+
+  // aura::WindowObserver overrides:
+  void OnWindowPropertyChanged(aura::Window* window,
+                               const void* key,
+                               intptr_t old) override;
 
   // TouchExplorationControllerDelegate overrides:
   void SetOutputLevel(int volume) override;
@@ -91,6 +97,7 @@ class ASH_EXPORT TouchExplorationManager
   std::unique_ptr<TouchAccessibilityEnabler> touch_accessibility_enabler_;
   RootWindowController* root_window_controller_;
   chromeos::CrasAudioHandler* audio_handler_;
+  aura::Window* observing_window_;
 
   DISALLOW_COPY_AND_ASSIGN(TouchExplorationManager);
 };
