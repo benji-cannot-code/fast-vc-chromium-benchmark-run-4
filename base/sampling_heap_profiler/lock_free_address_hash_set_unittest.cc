f6996fa2dfa49f820252abec479787cd306f851b
Martijn Croonen <martijnc@chromium.org>
2018-07-09 23:17:04 +0000

    Include <memory> in lock_free_address_hash_set_unittest.cc
    This fixes a compiler error.
    
    ../../base/sampling_heap_profiler/lock_free_address_hash_set_unittest.cc(148,22):  error: no member named 'make_unique' in namespace 'std'
      auto thread = std::make_unique<WriterThread>(&set, &cancel);
                    ~~~~~^
    ../../base/sampling_heap_profiler/lock_free_address_hash_set_unittest.cc(148,34):  error: 'WriterThread' does not refer to a value
      auto thread = std::make_unique<WriterThread>(&set, &cancel);
                                     ^
    ../../base/sampling_heap_profiler/lock_free_address_hash_set_unittest.cc(116,7):  note: declared here
    class WriterThread : public SimpleThread {
          ^
    ../../base/sampling_heap_profiler/lock_free_address_hash_set_unittest.cc(148,48):  error: expression result unused [-Werror,-Wunused-value]
      auto thread = std::make_unique<WriterThread>(&set, &cancel);
                                                   ^~~~
    3 errors generated.
    
    ninja: build stopped: subcommand failed.
    Change-Id: I72d5bddf448c091417f9497b6cb751df5a75dff1
    Reviewed-on: https://chromium-review.googlesource.com/1129759
    Reviewed-by: Alexei Filippov <alph@chromium.org>
    Commit-Queue: Alexei Filippov <alph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573513}

--- base/sampling_heap_profiler/lock_free_address_hash_set_unittest.cc
+++ base/sampling_heap_profiler/lock_free_address_hash_set_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdlib.h>
 #include <cinttypes>
+#include <memory>
 
 #include "base/allocator/allocator_shim.h"
 #include "base/debug/alias.h"
