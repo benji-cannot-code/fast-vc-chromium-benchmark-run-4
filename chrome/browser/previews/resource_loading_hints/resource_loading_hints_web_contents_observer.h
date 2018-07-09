47147a514b508ad5e711f330fefac712c55b9b1d
Tarun Bansal <tbansal@chromium.org>
2018-07-09 22:19:44 +0000

    Add mojom call to send resource loading hints from browser to renderer.
    The mojom call is send only when resource loading hints feature is
    enabled and the connection is slow.
    
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Change-Id: Ib57d656406db6f63200ffbf5dbbe83c356d464ca
    Bug: 854666
    Reviewed-on: https://chromium-review.googlesource.com/1112925
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Doug Arnett <dougarnett@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573485}

---
+++ chrome/browser/previews/resource_loading_hints/resource_loading_hints_web_contents_observer.h
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_PREVIEWS_RESOURCE_LOADING_HINTS_RESOURCE_LOADING_HINTS_WEB_CONTENTS_OBSERVER_H_
+#define CHROME_BROWSER_PREVIEWS_RESOURCE_LOADING_HINTS_RESOURCE_LOADING_HINTS_WEB_CONTENTS_OBSERVER_H_
+
+#include "base/macros.h"
+#include "content/public/browser/web_contents_observer.h"
+#include "content/public/browser/web_contents_user_data.h"
+
+// Observes navigation events and sends the resource loading hints mojo message
+// to the renderer.
+class ResourceLoadingHintsWebContentsObserver
+    : public content::WebContentsObserver,
+      public content::WebContentsUserData<
+          ResourceLoadingHintsWebContentsObserver> {
+ public:
+  ~ResourceLoadingHintsWebContentsObserver() override;
+
+ private:
+  friend class content::WebContentsUserData<
+      ResourceLoadingHintsWebContentsObserver>;
+
+  explicit ResourceLoadingHintsWebContentsObserver(
+      content::WebContents* web_contents);
+
+  // Overridden from content::WebContentsObserver. If the navigation is of type
+  // resource loading hints preview, then this method sends the resource loading
+  // hints mojo message to the renderer.
+  void DidFinishNavigation(
+      content::NavigationHandle* navigation_handle) override;
+
+  // Sends resource loading hints to the renderer.
+  void SendResourceLoadingHints(
+      content::NavigationHandle* navigation_handle) const;
+
+  DISALLOW_COPY_AND_ASSIGN(ResourceLoadingHintsWebContentsObserver);
+};
+
+#endif  // CHROME_BROWSER_PREVIEWS_RESOURCE_LOADING_HINTS_RESOURCE_LOADING_HINTS_WEB_CONTENTS_OBSERVER_H_
