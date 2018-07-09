7efda467a5da5198146ed9894d09a16aabb0f34b
Khushal <khushalsagar@chromium.org>
2018-07-09 22:11:46 +0000

    gpu: Add memory tracing for GrContext in GPU process.
    Include memory dump from GrContext used for OOP raster in the GPU
    process in traces.
    
    R=bsalomon@chromium.org, ericrk@chromium.org, sadrul@chromium.org
    TBR=bsalomon@google.com
    
    Bug: 859419
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: Ie010e1c567626343d5698b02c45776d215640da0
    Reviewed-on: https://chromium-review.googlesource.com/1123894
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Brian Salomon <bsalomon@google.com>
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573479}

--- gpu/command_buffer/service/raster_decoder_context_state.cc
+++ gpu/command_buffer/service/raster_decoder_context_state.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "gpu/command_buffer/service/raster_decoder_context_state.h"
 
+#include "base/threading/thread_task_runner_handle.h"
+#include "base/trace_event/memory_dump_manager.h"
 #include "gpu/config/gpu_driver_bug_workarounds.h"
 #include "ui/gl/gl_context.h"
 #include "ui/gl/gl_share_group.h"
@@ -22,11 +24,18 @@ RasterDecoderContextState::RasterDecoderContextState(
     : share_group(std::move(share_group)),
       surface(std::move(surface)),
       context(std::move(context)),
-      use_virtualized_gl_contexts(use_virtualized_gl_contexts) {}
+      use_virtualized_gl_contexts(use_virtualized_gl_contexts) {
+  if (base::ThreadTaskRunnerHandle::IsSet()) {
+    base::trace_event::MemoryDumpManager::GetInstance()->RegisterDumpProvider(
+        this, "RasterDecoderContextState", base::ThreadTaskRunnerHandle::Get());
+  }
+}
 
 RasterDecoderContextState::~RasterDecoderContextState() {
   if (gr_context)
     gr_context->abandonContext();
+  base::trace_event::MemoryDumpManager::GetInstance()->UnregisterDumpProvider(
+      this);
 }
 
 void RasterDecoderContextState::InitializeGrContext(
@@ -61,6 +70,14 @@ void RasterDecoderContextState::InitializeGrContext(
     gr_context->setResourceCacheLimits(kMaxGaneshResourceCacheCount,
                                        max_resource_cache_bytes);
   }
+}
+
+bool RasterDecoderContextState::OnMemoryDump(
+    const base::trace_event::MemoryDumpArgs& args,
+    base::trace_event::ProcessMemoryDump* pmd) {
+  if (gr_context)
+    DumpGrMemoryStatistics(gr_context.get(), pmd, base::nullopt);
+  return true;
 }
 
 }  // namespace raster
