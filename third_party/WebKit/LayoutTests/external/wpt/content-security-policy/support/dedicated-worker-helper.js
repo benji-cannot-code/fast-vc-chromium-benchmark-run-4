e42724367ac032dfe678957f61c51cb0d2f46570
Andy Paicu <andypaicu@chromium.org>
2018-07-09 12:46:46 +0000

    Un-deprecated 'child-src'
    'child-src' is now part of the fallback chain for 'worker-src'.
    This means that 'child-src' always takes precedence over 'script-src'
    when checking worker requests.
    Added extra tests to ensure that a worker request blocked by
    'child-src' and allowed by 'script-src' is blocked.
    
    Removed previous logic that considered 'script-src' to be the
    fallback for 'worker-src' and amended tests. Removed "temporary"
    logic put in place to not break sites using "child-src".
    
    Refactored the OperativeDirective logic to ensure that the caller
    does not need to be aware of precise fallback chain of the directive,
    otherwise the way to get the 'worker-src' operative directive would be:
    
    OperativeDirective(worker_src_.Get(),
          OperativeDirective(child_src_.Get(),
              OperativeDirective(script_src_.Get())));
    
    To be submitted with the spec PR as it includes tests.
    Spec: https://github.com/w3c/webappsec-csp/pull/313
    
    Bug: 669496
    Change-Id: I7ca9552df1d0ce203a604b0e469a268f6b112e49
    Reviewed-on: https://chromium-review.googlesource.com/1128087
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573278}

--- third_party/WebKit/LayoutTests/external/wpt/content-security-policy/support/dedicated-worker-helper.js
+++ third_party/WebKit/LayoutTests/external/wpt/content-security-policy/support/dedicated-worker-helper.js
@@ -1,3 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 var url = new URL("../support/ping.js", document.baseURI).toString();
-assert_worker_is_loaded(url, document.getElementById("foo").getAttribute("data-desc-fallback"));
+if (document.getElementById("foo").hasAttribute("blocked-worker"))
+  assert_worker_is_blocked(url, document.getElementById("foo").getAttribute("data-desc-fallback"));
+else
+  assert_worker_is_loaded(url, document.getElementById("foo").getAttribute("data-desc-fallback"));
