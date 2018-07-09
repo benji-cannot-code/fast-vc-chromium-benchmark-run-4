82ebc0512a2df7707f15bd366a609956eb2a44e8
Khushal <khushalsagar@chromium.org>
2018-07-09 20:21:16 +0000

    Reland "gpu: Share GrContext between RasterDecoders for OOP raster."
    This reverts commit 2200b6d949dc8b19a65c3e1a0753479ee68d3d65. The change
    inadvertantly disabled an optimization for low-end Android devices that
    releases all GPU memory when the application has been backgrounded. The
    optimization works by releasing all non-WebGL GpuChannels. Since this
    change moved the GrContext ownership to a shared object cached in the
    GpuChannelManager instead of per GpuChannel, it is necessary to
    explicitly destroy it in the channel manager during cleanup.
    
    R=ericrk@chromium.org, piman@chromium.org
    
    Bug: 860506, 854416
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I3f6754ce939c3f815b13a3f96cd4414f5c8a3449
    Reviewed-on: https://chromium-review.googlesource.com/1129548
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573428}

--- gpu/ipc/service/gpu_channel_manager.h
+++ gpu/ipc/service/gpu_channel_manager.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "gpu/command_buffer/common/activity_flags.h"
 #include "gpu/command_buffer/common/constants.h"
+#include "gpu/command_buffer/service/raster_decoder_context_state.h"
 #include "gpu/command_buffer/service/service_discardable_manager.h"
 #include "gpu/command_buffer/service/shader_translator_cache.h"
 #include "gpu/config/gpu_driver_bug_workarounds.h"
@@ -136,6 +137,10 @@ class GPU_IPC_SERVICE_EXPORT GpuChannelManager {
   void GetVideoMemoryUsageStats(
       VideoMemoryUsageStats* video_memory_usage_stats) const;
 
+  scoped_refptr<raster::RasterDecoderContextState> GetRasterDecoderContextState(
+      const ContextCreationAttribs& attribs,
+      ContextResult* result);
+
  private:
   void InternalDestroyGpuMemoryBuffer(gfx::GpuMemoryBufferId id, int client_id);
   void InternalDestroyGpuMemoryBufferOnIO(gfx::GpuMemoryBufferId id,
@@ -192,6 +197,19 @@ class GPU_IPC_SERVICE_EXPORT GpuChannelManager {
   GpuProcessActivityFlags activity_flags_;
 
   base::MemoryPressureListener memory_pressure_listener_;
+
+  // The RasterDecoderContextState is shared across all RasterDecoders. Note
+  // that this class needs to be ref-counted to conveniently manage the lifetime
+  // of the shared context in the case of a context loss. While the
+  // RasterDecoders strictly outlive the GpuChannelManager, in the event of a
+  // context loss the clients need to re-create the GpuChannel and command
+  // buffers once notified. In this interim state we can have multiple instances
+  // of the RasterDecoderContextState, for the lost and recovered clients. In
+  // order to avoid having the GpuChannelManager keep the lost context state
+  // alive until all clients have recovered, we use a ref-counted object and
+  // allow the decoders to manage its lifetime.
+  scoped_refptr<raster::RasterDecoderContextState>
+      raster_decoder_context_state_;
 
   // Member variables should appear before the WeakPtrFactory, to ensure
   // that any WeakPtrs to Controller are invalidated before its members
