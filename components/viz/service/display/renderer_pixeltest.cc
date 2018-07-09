e9ed5b33447e7f0703d7cb041daafd8dd9f9adad
Robert Phillips <robertphillips@google.com>
2018-07-09 14:39:29 +0000

    Reenable RendererPixelTestWithBackgroundFilter.InvertFilter test
    The fix for the flakiness of this test landed in:
    
    https://skia-review.googlesource.com/c/skia/+/138923 (Fix race condition in GrGLCaps)
    
    and has rolled into Chrome
    
    Bug: 854778
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel
    Change-Id: Ib02f84fab623e6ef2047ce432b02c94f8e637734
    Reviewed-on: https://chromium-review.googlesource.com/1128607
    Reviewed-by: weiliangc <weiliangc@chromium.org>
    Commit-Queue: Robert Phillips <robertphillips@google.com>
    Cr-Commit-Position: refs/heads/master@{#573301}

--- components/viz/service/display/renderer_pixeltest.cc
+++ components/viz/service/display/renderer_pixeltest.cc
@@ -2592,7 +2592,7 @@ using BackgroundFilterRendererTypes =
 TYPED_TEST_CASE(RendererPixelTestWithBackgroundFilter,
                 BackgroundFilterRendererTypes);
 
-TYPED_TEST(RendererPixelTestWithBackgroundFilter, DISABLED_InvertFilter) {
+TYPED_TEST(RendererPixelTestWithBackgroundFilter, InvertFilter) {
   this->background_filters_.Append(
       cc::FilterOperation::CreateInvertFilter(1.f));
 
