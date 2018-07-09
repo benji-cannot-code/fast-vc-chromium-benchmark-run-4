fa9eea5c7c324ec487095a2c8fbae67e0e65cbde
Greg Kraynov <kraynov@chromium.org>
2018-07-09 16:31:08 +0000

    Use NullTaskRunner in BudgetPool tests.
    Bug: 841735
    Change-Id: I5a38422269e6635c6cc7b4880c1a9108b0bd3761
    Reviewed-on: https://chromium-review.googlesource.com/1129231
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Greg Kraynov <kraynov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573330}

--- third_party/blink/renderer/platform/scheduler/common/throttling/budget_pool_unittest.cc
+++ third_party/blink/renderer/platform/scheduler/common/throttling/budget_pool_unittest.cc
@@ -12,8 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/macros.h"
 #include "base/task/sequence_manager/task_queue_impl.h"
+#include "base/test/null_task_runner.h"
 #include "base/test/simple_test_tick_clock.h"
-#include "components/viz/test/ordered_simple_task_runner.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/renderer/platform/scheduler/base/test/sequence_manager_for_test.h"
@@ -32,11 +32,10 @@ class BudgetPoolTest : public testing::Test {
 
   void SetUp() override {
     clock_.Advance(base::TimeDelta::FromMicroseconds(5000));
-    mock_task_runner_ =
-        base::MakeRefCounted<cc::OrderedSimpleTaskRunner>(&clock_, true);
+    null_task_runner_ = base::MakeRefCounted<base::NullTaskRunner>();
     scheduler_.reset(new MainThreadSchedulerImpl(
         base::sequence_manager::SequenceManagerForTest::Create(
-            nullptr, mock_task_runner_, &clock_),
+            nullptr, null_task_runner_, &clock_),
         base::nullopt));
     task_queue_throttler_ = scheduler_->task_queue_throttler();
     start_time_ = clock_.NowTicks();
@@ -57,7 +56,7 @@ class BudgetPoolTest : public testing::Test {
 
  protected:
   base::SimpleTestTickClock clock_;
-  scoped_refptr<cc::OrderedSimpleTaskRunner> mock_task_runner_;
+  scoped_refptr<base::NullTaskRunner> null_task_runner_;
   std::unique_ptr<MainThreadSchedulerImpl> scheduler_;
   TaskQueueThrottler* task_queue_throttler_;  // NOT OWNED
   base::TimeTicks start_time_;
