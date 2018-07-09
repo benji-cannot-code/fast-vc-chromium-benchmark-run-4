ed1993f482ca397e63d6ed6b1896bae3ace40dbd
tzik <tzik@chromium.org>
2018-07-09 02:16:39 +0000

    Replace base::Timer with behavior-specific timers in media/
    This CL replaces base::Timer with its subclasses.
    As base::Timer changes its behavior subject to its construction time
    flags, that makes hard to see the actual timer behavior, especially
    it's unclear when a timer is injected from other components.
    Also, that OnceCallback support of base::Timer is hard to implement on
    the dynamically determined invocation pattern.
    
    Bug: 850247
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I8699c5bd22fb7acb51e1091c5f7d87d57bb8e3db
    Reviewed-on: https://chromium-review.googlesource.com/1125708
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Taiju Tsuiki <tzik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573201}

--- chrome/browser/media/media_engagement_contents_observer.h
+++ chrome/browser/media/media_engagement_contents_observer.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_MEDIA_MEDIA_ENGAGEMENT_CONTENTS_OBSERVER_H_
 #define CHROME_BROWSER_MEDIA_MEDIA_ENGAGEMENT_CONTENTS_OBSERVER_H_
 
+#include "base/timer/timer.h"
 #include "content/public/browser/web_contents_observer.h"
 
 namespace base {
@@ -107,7 +108,7 @@ class MediaEngagementContentsObserver : public content::WebContentsObserver {
 
   // Timer that will fire when the playback time reaches the minimum for
   // significant media playback.
-  std::unique_ptr<base::Timer> playback_timer_;
+  base::OneShotTimer playback_timer_;
 
   // Set of active players that can produce a significant playback. In other
   // words, whether this set is empty can be used to know if there is a
@@ -217,7 +218,7 @@ class MediaEngagementContentsObserver : public content::WebContentsObserver {
 
   // Stores the ids of the players that were audible. The boolean will be true
   // if the player was significant.
-  using AudiblePlayerRow = std::pair<bool, std::unique_ptr<base::Timer>>;
+  using AudiblePlayerRow = std::pair<bool, std::unique_ptr<base::OneShotTimer>>;
   std::map<MediaPlayerId, AudiblePlayerRow> audible_players_;
 
   // The task runner to use when creating timers. It is used only for testing.
--- chrome/browser/media/media_engagement_contents_observer_unittest.cc
+++ chrome/browser/media/media_engagement_contents_observer_unittest.cc
@@ -79,7 +79,7 @@ class MediaEngagementContentsObserverTest
   }
 
   bool IsTimerRunning() const {
-    return contents_observer_->playback_timer_->IsRunning();
+    return contents_observer_->playback_timer_.IsRunning();
   }
 
   bool IsTimerRunningForPlayer(int id) const {
