ace62f009b68d9e5675921c8f4f42b50c2021f6a
Oscar Johansson <oscarj@opera.com>
2018-07-10 08:05:55 +0000

    Fix line endings when writing bookmark URLs to clipboard.
    Add "\r\n" instead of "\n" when copying multiple bookmark
    URLs on Windows.
    
    Bug: 861717
    Change-Id: I0d15a8662bbb27ee67b899ea805ddd5b147be71f
    Reviewed-on: https://chromium-review.googlesource.com/1128879
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Oscar Johansson <oscarj@opera.com>
    Cr-Commit-Position: refs/heads/master@{#573646}

--- components/bookmarks/browser/bookmark_node_data_unittest.cc
+++ components/bookmarks/browser/bookmark_node_data_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string16.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/scoped_task_environment.h"
+#include "build/build_config.h"
 #include "components/bookmarks/browser/bookmark_model.h"
 #include "components/bookmarks/test/bookmark_test_helpers.h"
 #include "components/bookmarks/test/test_bookmark_client.h"
@@ -311,7 +312,11 @@ TEST_F(BookmarkNodeDataTest, WriteToClipboardMultipleURLs) {
 
   // Now read the data back in.
   base::string16 combined_text;
+#if defined(OS_WIN)
+  base::string16 new_line = base::ASCIIToUTF16("\r\n");
+#else
   base::string16 new_line = base::ASCIIToUTF16("\n");
+#endif
   combined_text = base::UTF8ToUTF16(url.spec()) + new_line
     + base::UTF8ToUTF16(url2.spec());
   base::string16 clipboard_result;
@@ -370,7 +375,11 @@ TEST_F(BookmarkNodeDataTest, WriteToClipboardFolderAndURL) {
 
   // Now read the data back in.
   base::string16 combined_text;
+#if defined(OS_WIN)
+  base::string16 new_line = base::ASCIIToUTF16("\r\n");
+#else
   base::string16 new_line = base::ASCIIToUTF16("\n");
+#endif
   base::string16 folder_title = ASCIIToUTF16("g1");
   combined_text = base::ASCIIToUTF16(url.spec()) + new_line + folder_title;
   base::string16 clipboard_result;
