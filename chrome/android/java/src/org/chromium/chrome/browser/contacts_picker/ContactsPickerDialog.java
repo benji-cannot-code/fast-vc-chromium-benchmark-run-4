2e472686946f5afed8cae85822780e9b128449dd
Finnur Thorarinsson <finnur@chromium.org>
2018-07-10 10:48:07 +0000

    Contacts Picker part 2: Show blank dialog instead of toast.
    Bug: 860467
    Change-Id: I67bde8b8bd947f37531fedca780d7d023dfadd74
    Reviewed-on: https://chromium-review.googlesource.com/1127993
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573679}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/contacts_picker/ContactsPickerDialog.java
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.contacts_picker;
+
+import android.content.Context;
+import android.support.v7.app.AlertDialog;
+
+import org.chromium.chrome.R;
+import org.chromium.ui.ContactsPickerListener;
+
+import java.util.List;
+
+/**
+ * UI for the contacts picker that shows on the Android platform as a result of
+ * &lt;input type=file accept=contacts &gt; form element.
+ */
+public class ContactsPickerDialog extends AlertDialog {
+    /**
+     * The ContactsPickerDialog constructor.
+     * @param context The context to use.
+     * @param listener The listener object that gets notified when an action is taken.
+     * @param allowMultiple Whether the contacts picker should allow multiple items to be selected.
+     * @param mimeTypes A list of mime types to show in the dialog.
+     */
+    public ContactsPickerDialog(Context context, ContactsPickerListener listener,
+            boolean allowMultiple, List<String> mimeTypes) {
+        super(context, R.style.FullscreenWhite);
+    }
+}
