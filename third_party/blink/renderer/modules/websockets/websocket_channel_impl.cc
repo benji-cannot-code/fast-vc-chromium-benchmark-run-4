e240ccb6da198a0f403ea2a77051290c53f7c1b9
Adam Rice <ricea@chromium.org>
2018-07-10 08:08:20 +0000

    WS: Delete FileReaderLoader in BlobLoader
    Delete the FileReaderLoader member of WebSocketChannelImpl::BlobLoader
    as soon as we are done with it. This moves the lifetime semantics closer
    to what they were before Oilpan, and saves a bit of memory.
    
    Also remove some obsolete pre-Oilpan comments, since
    WebSocketChannelImpl and BlobLoader are garbage collected rather than
    explicitly deleted now.
    
    BUG=788890
    
    Change-Id: I815e53a3271a91dbeb779b6f386004b454d1db92
    Reviewed-on: https://chromium-review.googlesource.com/1128837
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573647}

--- third_party/blink/renderer/modules/websockets/websocket_channel_impl.cc
+++ third_party/blink/renderer/modules/websockets/websocket_channel_impl.cc
@@ -130,19 +130,18 @@ WebSocketChannelImpl::BlobLoader::BlobLoader(
 
 void WebSocketChannelImpl::BlobLoader::Cancel() {
   loader_->Cancel();
-  // DidFail will be called immediately.
-  // |this| is deleted here.
+  loader_ = nullptr;
 }
 
 void WebSocketChannelImpl::BlobLoader::DidFinishLoading() {
   channel_->DidFinishLoadingBlob(loader_->ArrayBufferResult());
-  // |this| is deleted here.
+  loader_ = nullptr;
 }
 
 void WebSocketChannelImpl::BlobLoader::DidFail(
     FileError::ErrorCode error_code) {
   channel_->DidFailLoadingBlob(error_code);
-  // |this| is deleted here.
+  loader_ = nullptr;
 }
 
 struct WebSocketChannelImpl::ConnectInfo {
@@ -547,7 +546,6 @@ void WebSocketChannelImpl::HandleDidClose(bool was_clean,
       was_clean ? WebSocketChannelClient::kClosingHandshakeComplete
                 : WebSocketChannelClient::kClosingHandshakeIncomplete;
   client->DidClose(status, code, reason);
-  // client->DidClose may delete this object.
 }
 
 ExecutionContext* WebSocketChannelImpl::GetExecutionContext() const {
@@ -627,7 +625,6 @@ void WebSocketChannelImpl::DidFail(WebSocketHandle* handle,
   // WebSocketConnection. Hence we fail this channel by calling
   // |this->failAsError| function.
   FailAsError(message);
-  // |this| may be deleted.
 }
 
 void WebSocketChannelImpl::DidReceiveData(WebSocketHandle* handle,
@@ -681,7 +678,6 @@ void WebSocketChannelImpl::DidReceiveData(WebSocketHandle* handle,
     receiving_message_data_.clear();
     if (message.IsNull()) {
       FailAsError("Could not decode a text frame as UTF-8.");
-      // failAsError may delete this object.
     } else {
       client_->DidReceiveTextMessage(message);
     }
@@ -717,7 +713,6 @@ void WebSocketChannelImpl::DidClose(WebSocketHandle* handle,
   }
 
   HandleDidClose(was_clean, code, reason);
-  // HandleDidClose may delete this object.
 }
 
 void WebSocketChannelImpl::DidReceiveFlowControl(WebSocketHandle* handle,
@@ -786,7 +781,6 @@ void WebSocketChannelImpl::DidFailLoadingBlob(FileError::ErrorCode error_code) {
   // FIXME: Generate human-friendly reason message.
   FailAsError("Failed to load Blob: error code = " +
               String::Number(error_code));
-  // |this| can be deleted here.
 }
 
 void WebSocketChannelImpl::TearDownFailedConnection() {
@@ -798,7 +792,6 @@ void WebSocketChannelImpl::TearDownFailedConnection() {
     client_->DidError();
 
   HandleDidClose(false, kCloseEventCodeAbnormalClosure, String());
-  // HandleDidClose may delete this object.
 }
 
 bool WebSocketChannelImpl::ShouldDisallowConnection(const KURL& url) {
