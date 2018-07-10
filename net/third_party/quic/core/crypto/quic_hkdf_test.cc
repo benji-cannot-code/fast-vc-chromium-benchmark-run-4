6426a988fddec7e3cc6081c3bd03293b096abc29
Victor Vasiliev <vasilvv@chromium.org>
2018-07-10 18:03:29 +0000

    Move QuicHKDF into core/crypto, since it's not platform-specific
    Merge internal change: 203847584
    
    R=rch@chromium.org
    
    Change-Id: Ia2fc9015fbbdc260f28ef3dc788e302c86ba04ec
    Reviewed-on: https://chromium-review.googlesource.com/1130169
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Ryan Hamilton <rch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573806}

--- net/third_party/quic/core/crypto/quic_hkdf_test.cc
+++ net/third_party/quic/core/crypto/quic_hkdf_test.cc
@@ -3,7 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "net/third_party/quic/platform/api/quic_hkdf.h"
+#include "net/third_party/quic/core/crypto/quic_hkdf.h"
+
 #include "net/third_party/quic/platform/api/quic_arraysize.h"
 #include "net/third_party/quic/platform/api/quic_string.h"
 #include "net/third_party/quic/platform/api/quic_test.h"
@@ -74,7 +75,7 @@ TEST_F(QuicHKDFTest, HKDF) {
 
     // We set the key_length to the length of the expected output and then take
     // the result from the first key, which is the client write key.
-    QuicHKDFImpl hkdf(key, salt, info, expected.size(), 0, 0);
+    QuicHKDF hkdf(key, salt, info, expected.size(), 0, 0);
 
     ASSERT_EQ(expected.size(), hkdf.client_write_key().size());
     EXPECT_EQ(0, memcmp(expected.data(), hkdf.client_write_key().data(),
