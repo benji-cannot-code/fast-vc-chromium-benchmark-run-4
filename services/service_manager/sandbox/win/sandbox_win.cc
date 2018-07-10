5cf5f689808724450b57c0d25849e16b18cc597b
Chris Palmer <palmer@chromium.org>
2018-07-10 00:38:48 +0000

    Harmonize the sandbox memory limit across platforms.
    ...to the extent possible. This involves lowering 64-bit Linux' limit from 8
    GiB to 4, to match Windows.
    
    Note that in the process of writing a previous version of this CL, we learned
    that macOS apparently does not enforce RLIMIT_DATA, so we continue to not have a
    limit on that platform. (The same goes for RLIMIT_AS, which we previously knew
    about.)
    
    Bug: 853873,459158
    Change-Id: I5a2a76b4c98a0862c16ce121572c4b2b0f910b44
    Reviewed-on: https://chromium-review.googlesource.com/1130235
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573557}

--- services/service_manager/sandbox/win/sandbox_win.cc
+++ services/service_manager/sandbox/win/sandbox_win.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/scoped_handle.h"
 #include "base/win/win_util.h"
 #include "base/win/windows_version.h"
+#include "sandbox/constants.h"
 #include "sandbox/win/src/app_container_profile.h"
 #include "sandbox/win/src/job.h"
 #include "sandbox/win/src/process_mitigations.h"
@@ -566,13 +567,13 @@ sandbox::ResultCode SetJobMemoryLimit(const base::CommandLine& cmd_line,
   DCHECK_NE(policy->GetJobLevel(), sandbox::JOB_NONE);
 
 #ifdef _WIN64
-  int64_t GB = 1024 * 1024 * 1024;
-  size_t memory_limit = 4 * GB;
+  size_t memory_limit = static_cast<size_t>(sandbox::kDataSizeLimit);
 
   // Note that this command line flag hasn't been fetched by all
   // callers of SetJobLevel, only those in this file.
   if (service_manager::SandboxTypeFromCommandLine(cmd_line) ==
       service_manager::SANDBOX_TYPE_GPU) {
+    int64_t GB = 1024 * 1024 * 1024;
     // Allow the GPU process's sandbox to access more physical memory if
     // it's available on the system.
     int64_t physical_memory = base::SysInfo::AmountOfPhysicalMemory();
