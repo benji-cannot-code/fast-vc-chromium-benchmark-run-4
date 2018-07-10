031050d5f1d43c47600d1477ca907d90f85afb23
Francois Doray <fdoray@chromium.org>
2018-07-10 20:26:40 +0000

    RC: Add LifecycleUnitState::PENDING_UNFREEZE.
    A LifeycleUnit transitions to that state after a call to Unfreeze().
    It stays in that state until the unfreeze actually happens.
    
    Having this intermediate state instead of transitionning directly
    to ACTIVE is important to be able to track when the renderer never
    notifies the browser that it ran the "onresume" callback.
    
    Bug: 775644, 855874
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: Icf578cd12d113e569b017b604e4e835beeb331bf
    Reviewed-on: https://chromium-review.googlesource.com/1131845
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Fadi Meawad <fmeawad@chromium.org>
    Reviewed-by: Sébastien Marchand <sebmarchand@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573885}

--- chrome/browser/resources/discards/discards.js
+++ chrome/browser/resources/discards/discards.js
@@ -239,6 +239,8 @@ cr.define('discards', function() {
         return 'pending discard';
       case mojom.LifecycleUnitState.DISCARDED:
         return 'discarded';
+      case mojom.LifecycleUnitState.PENDING_UNFREEZE:
+        return 'pending unfreeze';
     }
     assertNotReached('Unsupported lifecycle state: ' + state);
   }
