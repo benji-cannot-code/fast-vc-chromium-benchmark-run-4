b50076ff44df87672bdc0691078cf7681afb14b9
Sebastien Marchand <sebmarchand@chromium.org>
2018-07-10 17:52:07 +0000

    RC - LocalDB: Check the NavID when receiving a non-persistent notif signal
    Bug: 857471, 773382
    Change-Id: I6591e41f75c1f6bc0ba339725cc3b55d94781743
    Reviewed-on: https://chromium-review.googlesource.com/1131556
    Commit-Queue: Sébastien Marchand <sebmarchand@chromium.org>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573802}

--- chrome/browser/resource_coordinator/local_site_characteristics_webcontents_observer_unittest.cc
+++ chrome/browser/resource_coordinator/local_site_characteristics_webcontents_observer_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "chrome/browser/resource_coordinator/local_site_characteristics_data_store_factory.h"
 #include "chrome/browser/resource_coordinator/local_site_characteristics_data_unittest_utils.h"
+#include "chrome/browser/resource_coordinator/page_signal_receiver.h"
 #include "chrome/browser/resource_coordinator/site_characteristics_data_store.h"
 #include "chrome/browser/resource_coordinator/tab_manager_features.h"
 #include "content/public/browser/navigation_handle.h"
@@ -92,6 +93,7 @@ class LocalSiteCharacteristicsWebContentsObserverTest
         SkipObserverRegistrationForTesting();
     observer_ = std::make_unique<LocalSiteCharacteristicsWebContentsObserver>(
         web_contents());
+    observer()->SetPageSignalReceiverForTesting(&receiver_);
   }
 
   void TearDown() override {
@@ -111,14 +113,20 @@ class LocalSiteCharacteristicsWebContentsObserverTest
 
   const GURL kTestUrl1 = GURL("http://foo.com");
   const GURL kTestUrl2 = GURL("http://bar.com");
-  const PageNavigationIdentity kNavId = {CoordinationUnitID(), 0, ""};
 
   LocalSiteCharacteristicsWebContentsObserver* observer() {
     return observer_.get();
   }
 
+  PageNavigationIdentity GetNavIdForWebContents() {
+    return {CoordinationUnitID(),
+            receiver_.GetNavigationIDForWebContents(web_contents()), ""};
+  }
+
  private:
   std::unique_ptr<LocalSiteCharacteristicsWebContentsObserver> observer_;
+  PageSignalReceiver receiver_;
+
   DISALLOW_COPY_AND_ASSIGN(LocalSiteCharacteristicsWebContentsObserverTest);
 };
 
@@ -187,7 +195,8 @@ TEST_F(LocalSiteCharacteristicsWebContentsObserverTest,
   ::testing::Mock::VerifyAndClear(mock_writer);
   observer()->OnAudioStateChanged(true);
   ::testing::Mock::VerifyAndClear(mock_writer);
-  observer()->OnNonPersistentNotificationCreated(web_contents(), kNavId);
+  observer()->OnNonPersistentNotificationCreated(web_contents(),
+                                                 GetNavIdForWebContents());
   ::testing::Mock::VerifyAndClear(mock_writer);
 
   EXPECT_CALL(*mock_writer,
@@ -205,7 +214,8 @@ TEST_F(LocalSiteCharacteristicsWebContentsObserverTest,
   observer()->OnAudioStateChanged(true);
   ::testing::Mock::VerifyAndClear(mock_writer);
   EXPECT_CALL(*mock_writer, NotifyUsesNotificationsInBackground());
-  observer()->OnNonPersistentNotificationCreated(web_contents(), kNavId);
+  observer()->OnNonPersistentNotificationCreated(web_contents(),
+                                                 GetNavIdForWebContents());
   ::testing::Mock::VerifyAndClear(mock_writer);
 
   EXPECT_CALL(*mock_writer, OnDestroy());
@@ -234,7 +244,8 @@ TEST_F(LocalSiteCharacteristicsWebContentsObserverTest,
   ::testing::Mock::VerifyAndClear(mock_writer);
   observer()->OnAudioStateChanged(true);
   ::testing::Mock::VerifyAndClear(mock_writer);
-  observer()->OnNonPersistentNotificationCreated(web_contents(), kNavId);
+  observer()->OnNonPersistentNotificationCreated(web_contents(),
+                                                 GetNavIdForWebContents());
   ::testing::Mock::VerifyAndClear(mock_writer);
 
   EXPECT_CALL(*mock_writer, OnDestroy());
@@ -287,6 +298,39 @@ TEST_F(LocalSiteCharacteristicsWebContentsObserverTest, LoadEvent) {
   observer()->OnLoadingStateChange(web_contents(),
                                    TabLoadTracker::LoadingState::LOADING,
                                    TabLoadTracker::LoadingState::UNLOADED);
+
+  EXPECT_CALL(*mock_writer, OnDestroy());
+}
+
+TEST_F(LocalSiteCharacteristicsWebContentsObserverTest,
+       LateNotificationUsageSignalIsIgnored) {
+  MockDataWriter* mock_writer = NavigateAndReturnMockWriter(kTestUrl1);
+  TabLoadTracker::Get()->TransitionStateForTesting(web_contents(),
+                                                   LoadingState::LOADED);
+
+  EXPECT_CALL(*mock_writer,
+              NotifySiteVisibilityChanged(TabVisibility::kBackground));
+  web_contents()->WasHidden();
+  ::testing::Mock::VerifyAndClear(mock_writer);
+
+  auto nav_id = GetNavIdForWebContents();
+  EXPECT_CALL(*mock_writer, NotifyUsesNotificationsInBackground());
+  observer()->OnNonPersistentNotificationCreated(web_contents(), nav_id);
+  ::testing::Mock::VerifyAndClear(mock_writer);
+
+  // Invalidate the navigation ID but keep the same origin, the notification
+  // should get forwarded to the writer.
+  nav_id.navigation_id++;
+  nav_id.url = web_contents()->GetLastCommittedURL().spec();
+  EXPECT_CALL(*mock_writer, NotifyUsesNotificationsInBackground());
+  observer()->OnNonPersistentNotificationCreated(web_contents(), nav_id);
+  ::testing::Mock::VerifyAndClear(mock_writer);
+
+  // Make the URL of the navigation ID point to a different origin, the writer
+  // shouldn't get notified about this event.
+  nav_id.url = "https://not-the-same-url.com";
+  observer()->OnNonPersistentNotificationCreated(web_contents(), nav_id);
+  ::testing::Mock::VerifyAndClear(mock_writer);
 
   EXPECT_CALL(*mock_writer, OnDestroy());
 }
