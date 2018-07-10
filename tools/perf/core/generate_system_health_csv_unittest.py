80a490fe00ff67f7c268cbd8d6cbc72d2ef7a267
Juan Antonio Navarro Perez <perezju@chromium.org>
2018-07-10 17:21:16 +0000

    [tools/perf] Remove play:media:pandora user story
    Story no longer works due to flash requirements.
    
    Also updated system_health_stories.csv
    
    Bug: 862096
    Cq-Include-Trybots: master.tryserver.chromium.perf:obbs_fyi
    Change-Id: Ie44f249607147585ec90b2ea71f4864c816bd1a7
    Reviewed-on: https://chromium-review.googlesource.com/1131496
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Commit-Queue: Juan Antonio Navarro Pérez <perezju@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573787}

--- tools/perf/core/generate_system_health_csv_unittest.py
+++ tools/perf/core/generate_system_health_csv_unittest.py
@@ -21,7 +21,6 @@ class GenerateSystemHealthCSVTest(unittest.TestCase):
         'browse:tools:earth': 'All Platforms',
         'browse:tools:maps': 'All Platforms',
         'play:media:google_play_music': 'All Platforms',
-        'play:media:pandora': 'All Platforms',
         'play:media:soundcloud': 'Win Platforms'}
     all_expects = [{
         'browse:news:cnn': [(
@@ -41,8 +40,6 @@ class GenerateSystemHealthCSVTest(unittest.TestCase):
             ['Win Platforms'], 'win:crbug.com/673775, mac:crbug.com/756027')],
         'browse:media:tumblr': [(
             ['Mac 10.11'], 'crbug.com/760966')],
-        'play:media:pandora': [(
-            ['All Platforms'], 'crbug.com/649392')],
     }]
     self.assertEquals(
         expected_result,
