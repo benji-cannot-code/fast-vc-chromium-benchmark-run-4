7c84ed3e45b68a3e74d5c1c4a5736c9a44108afa
David Tseng <dtseng@chromium.org>
2018-07-10 13:30:20 +0000

    Migrate SpokenFeedbackEventRewriterDelegate capture to ash::SpokenFeedbackRewriter
    Key events, for accessibility extensions, flow through the ash, chrome, and extension processes.
    
    When keys first go through ash, the keys are all discarded, and sent to the chrome process.
    
    In the chrome process, SpokenFeedbackEventRewriterDelegate sometimes captured the key events, sometimes didn't. When captured, the keys would not be reposted to ash.
    
    This change moves the logic from SpokenFeedbackEventRewriterDelegate to the ash process (into SpokenFeedbackEventRewriter) that decides whether a key is supposed to be captured. We therefore can decide whether a key should be discarded or continue propagation earlier without needing to repost/reinject the key event.
    
    This has some benefits and fixes the below bug.
    
    Bug: 859797
    Change-Id: If6c107cef6ccf4d2751bf70a2b28c0c0e0e7a056
    Reviewed-on: https://chromium-review.googlesource.com/1124674
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573706}

--- chrome/browser/chromeos/accessibility/accessibility_manager.cc
+++ chrome/browser/chromeos/accessibility/accessibility_manager.cc
@@ -1161,10 +1161,8 @@ void AccessibilityManager::OnExtensionUnloaded(
     content::BrowserContext* browser_context,
     const extensions::Extension* extension,
     extensions::UnloadedExtensionReason reason) {
-  if (extension->id() == keyboard_listener_extension_id_) {
+  if (extension->id() == keyboard_listener_extension_id_)
     keyboard_listener_extension_id_ = std::string();
-    keyboard_listener_capture_ = false;
-  }
 }
 
 void AccessibilityManager::OnShutdown(extensions::ExtensionRegistry* registry) {
