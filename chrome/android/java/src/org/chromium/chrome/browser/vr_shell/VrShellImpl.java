e37153a818457eaceecb99d1daac34a4f0da9699
Amirhossein Simjour <asimjour@chromium.org>
2018-07-10 17:49:15 +0000

    Hide search results after entering VR
    FindInPage result should be hidden in VR.
    
    Bug: 820650
    Change-Id: I54a23ef246242eaf565a698774efe2162fe88cc4
    Reviewed-on: https://chromium-review.googlesource.com/1131676
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Amirhossein Simjour <asimjour@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573801}

--- chrome/android/java/src/org/chromium/chrome/browser/vr_shell/VrShellImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/vr_shell/VrShellImpl.java
@@ -156,7 +156,12 @@ public class VrShellImpl
         mCompositorView.replaceSurfaceManagerForVr(
                 mVrCompositorSurfaceManager, mContentVrWindowAndroid);
 
-        if (mVrBrowsingEnabled) injectVrRootView();
+        if (mVrBrowsingEnabled) {
+            injectVrRootView();
+
+            // Hide FindInPage toolbar.
+            mActivity.getFindToolbarManager().hideToolbar();
+        }
 
         // This overrides the default intent created by GVR to return to Chrome when the DON flow
         // is triggered by resuming the GvrLayout, which is the usual way Daydream apps enter VR.
