c09edc91a3646afd5be0f25fe1afd32cffada77d
David Tseng <dtseng@chromium.org>
2018-07-10 21:27:40 +0000

    Fix braille routing
    A braille routing key within editable text works by invoking a document selection action. A recent change made it so we ignore our own actions. In this particular case, we don't want to ignore the result of the action, which updates the braille display.
    
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: I57503ac02c925f1173e4229da861a5aec949b61f
    Reviewed-on: https://chromium-review.googlesource.com/1132120
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573916}

--- chrome/browser/resources/chromeos/chromevox/cvox2/background/editing.js
+++ chrome/browser/resources/chromeos/chromevox/cvox2/background/editing.js
@@ -313,7 +313,8 @@ AutomationRichEditableText.prototype = {
     // Also, skip speech based on the predicate.
     if (ChromeVoxState.isReadingContinuously ||
         AutomationPredicate.shouldOnlyOutputSelectionChangeInBraille(
-            this.node_)) {
+            this.node_) ||
+        (!DesktopAutomationHandler.announceActions && eventFrom == 'action')) {
       this.brailleCurrentRichLine_();
       this.updateIntraLineState_(cur);
       return;
