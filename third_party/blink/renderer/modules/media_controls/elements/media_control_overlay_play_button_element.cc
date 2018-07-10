47b071ae9d7d0bf072f751547a0fd75a7563ac1e
Tommy Steimel <steimel@chromium.org>
2018-07-10 20:43:41 +0000

    Media Controls: Keep more overlay play button events in node
    This CL undoes some of crrev.com/c/1067514, which kept fewer events in
    node for the overlay play button to fix a menu issue. Instead of
    allowing all non-play/pause-causing events through, we are only going
    to allow non-play/pause-causing "click" events through. This keeps the
    original issue fixed while also fixing a new issue where outer divs
    could preventDefault on certain touch events to prevent the overlay
    play button from receiving a "click" event.
    
    Bug: 858846
    Change-Id: I1c10e188529edd1219cee42ded5865442becb8f8
    Reviewed-on: https://chromium-review.googlesource.com/1130826
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573896}

--- third_party/blink/renderer/modules/media_controls/elements/media_control_overlay_play_button_element.cc
+++ third_party/blink/renderer/modules/media_controls/elements/media_control_overlay_play_button_element.cc
@@ -267,7 +267,9 @@ void MediaControlOverlayPlayButtonElement::DefaultEventHandler(Event* event) {
 }
 
 bool MediaControlOverlayPlayButtonElement::KeepEventInNode(Event* event) {
-  return ShouldCausePlayPause(event);
+  return ShouldCausePlayPause(event) ||
+         (MediaControlElementsHelper::IsUserInteractionEvent(event) &&
+          event->type() != EventTypeNames::click);
 }
 
 bool MediaControlOverlayPlayButtonElement::ShouldCausePlayPause(
