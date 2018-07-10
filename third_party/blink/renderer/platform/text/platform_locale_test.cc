9e55c29d52e37866a59987bf082dd551450933fb
Jungshik Shin <jshin@chromium.org>
2018-07-10 08:28:18 +0000

    Update ICU to 62.1
    For the actual changes, see
      https://chromium-review.googlesource.com/c/chromium/deps/icu/+/1128503
    
    Update a few tests per CLDR 33-1/ICU 62/Unicode 11 data changes.
    
    * Arabic number format expected results are also updated because Arabic
    locales now use "European" digits by default in Chromium's ICU to match
    the behavior of other Google products. The CLDR upstream is expcted to
    follow suit soon.
    
    * Blink's homegrown grapheme cluster breakiterator is partly updated
    per UAX 29 (text segmentation) in Unicode 11. (see crbug.com/856947)
    
    * Whitespaces are kept together by a new word breaking rule (
     http://unicode.org/reports/tr29/#WB3d ). Update the test to reflect
     that change.
    
    * Fix the currency formatting code in payment to handle better cases
    with a space (U+00A0 / U+0020) between a currency code and a numeric
    value (e.g. "-USD 47.37" in en-AU for USD).  A test was added to
    CurrencyAmounts/PaymentsCurrencyFormatterTest.IsValidCurrencyFormat for
    a negative value.
    
    Cq_Include_Trybots: master.tryserver.blink:linux_trusty_blink_rel,mac10.12_blink_rel,win10_blink_rel;master.tryserver.chromium.android:android_blink_rel
    
    Bug: 850334,856947
    Test: base_unittests, and all other tests
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng
    Change-Id: I6e7e76df358fc1e04c9eb95422b3ab7bfb8f78fd
    Reviewed-on: https://chromium-review.googlesource.com/1111818
    Commit-Queue: Jungshik Shin <jshin@chromium.org>
    Reviewed-by: Seigo Nonaka <nona@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Mathieu Perreault <mathp@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573650}

--- third_party/blink/renderer/platform/text/platform_locale_test.cc
+++ third_party/blink/renderer/platform/text/platform_locale_test.cc
@@ -10,10 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 TEST(PlatformLocaleTest, StripInvalidNumberCharacters) {
-  std::unique_ptr<Locale> locale = Locale::Create("ar");
+  // Arabic does not use Arabic-Indic digit any more per
+  // Google's customized CLDR 33.1, but ar-EG does. Persian
+  // uses Extened Arabic-Indic digits.
+  std::unique_ptr<Locale> locale = Locale::Create("fa");
   String result = locale->StripInvalidNumberCharacters(
-      String::FromUTF8("abc\xD9\xA0ghi"), "0123456789");
-  // ARABIC-INDIC DIGIT ZERO U+0660 = \xD9\xA0 in UTF-8
-  EXPECT_EQ(String::FromUTF8("\xD9\xA0"), result);
+      String::FromUTF8(u8"abc\u06F0ghi"), "0123456789");
+  // EXTENDED ARABIC-INDIC DIGIT ZERO U+06F0
+  EXPECT_EQ(String::FromUTF8(u8"\u06F0"), result);
 }
 }
