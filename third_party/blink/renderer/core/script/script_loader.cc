dd407145b3fa5651d513d398ba29ebc8fe50ce80
Hiroshige Hayashizaki <hiroshige@chromium.org>
2018-07-10 18:20:44 +0000

    Add UMA for script scheduling type
    Bug: 856922
    Change-Id: Ia294d6c2a1b418b9a7f46ab41c574a4eace8c626
    Reviewed-on: https://chromium-review.googlesource.com/1116508
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573816}

--- third_party/blink/renderer/core/script/script_loader.cc
+++ third_party/blink/renderer/core/script/script_loader.cc
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/script/script_element_base.h"
 #include "third_party/blink/renderer/core/script/script_runner.h"
 #include "third_party/blink/renderer/core/svg_names.h"
+#include "third_party/blink/renderer/platform/histogram.h"
 #include "third_party/blink/renderer/platform/loader/fetch/access_control_status.h"
 #include "third_party/blink/renderer/platform/loader/fetch/fetch_parameters.h"
 #include "third_party/blink/renderer/platform/loader/fetch/resource_fetcher.h"
@@ -730,6 +731,11 @@ void ScriptLoader::FetchModuleScriptTree(
 PendingScript* ScriptLoader::TakePendingScript(
     ScriptSchedulingType scheduling_type) {
   CHECK(prepared_pending_script_);
+
+  DEFINE_STATIC_LOCAL(
+      EnumerationHistogram, scheduling_type_histogram,
+      ("Blink.Script.SchedulingType", kLastScriptSchedulingType + 1));
+  scheduling_type_histogram.Count(static_cast<int>(scheduling_type));
 
   switch (scheduling_type) {
     case ScriptSchedulingType::kAsync:
