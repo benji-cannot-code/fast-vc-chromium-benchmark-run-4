217d4966f79a430b7fa2291721f97af06711bc72
Sam Maier <smaier@chromium.org>
2018-07-10 17:42:41 +0000

    Changed webview usages of android.os.AsyncTask to our own
    Bug: 843745
    Change-Id: I347cd5fff2c7efb911c2b3aa3852e63a4ac99a72
    Reviewed-on: https://chromium-review.googlesource.com/1126172
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573798}

--- android_webview/java/src/org/chromium/android_webview/DefaultVideoPosterRequestHandler.java
+++ android_webview/java/src/org/chromium/android_webview/DefaultVideoPosterRequestHandler.java
@@ -6,9 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.android_webview;
 
 import android.graphics.Bitmap;
-import android.os.AsyncTask;
 import android.util.Log;
 
+import org.chromium.base.AsyncTask;
 import org.chromium.base.ThreadUtils;
 
 import java.io.IOException;
