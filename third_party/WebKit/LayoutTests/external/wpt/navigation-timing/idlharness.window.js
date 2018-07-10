537bd00e063da65661ae6f37e2de442664641a7b
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-07-10 19:13:13 +0000

    Import wpt@98848dff9fe765e3719484392fa27458f5a21c54
    Using wpt-import in Chromium 2e1b2236f037610edad12ebad4d1ce20431b7db2.
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/21159
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    guidou@chromium.org:
      external/wpt/mediacapture-streams
    hongchan@chromium.org, rtoy@chromium.org:
      external/wpt/webaudio
    hta@chromium.org:
      external/wpt/webrtc
    jsbell@chromium.org:
      external/wpt/resources
    mcasas@chromium.org:
      external/wpt/mediacapture-record
    tdresser@chromium.org:
      external/wpt/navigation-timing
    
    TBR=kereliuk
    
    No-Export: true
    Change-Id: I554f0e464b24ff92c5147547e433a3ba3e46aa6a
    Reviewed-on: https://chromium-review.googlesource.com/1131689
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573850}

---
+++ third_party/WebKit/LayoutTests/external/wpt/navigation-timing/idlharness.window.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/resources/WebIDLParser.js
+// META: script=/resources/idlharness.js
+
+'use strict';
+
+// http://www.w3.org/TR/navigation-timing/
+
+idl_test(
+  ['navigation-timing'],
+  ['resource-timing', 'performance-timeline', 'hr-time', 'dom'],
+  idl_array => {
+    idl_array.add_objects({
+      Performance: ['performance'],
+      PerformanceNavigation: ['performance.navigation'],
+      PerformanceTiming: ['performance.timing'],
+      PerformanceNavigationTiming: [
+        'performance.getEntriesByType("navigation")[0]'
+      ]
+    });
+  },
+  'navigation-timing interfaces'
+);
