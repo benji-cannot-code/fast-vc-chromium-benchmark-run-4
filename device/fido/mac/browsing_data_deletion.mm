6d72846cde2bb0f376216864994005abf2d40b3c
Martin Kreichgauer <martinkr@google.com>
2018-07-10 18:46:09 +0000

    device/fido/mac: hide DeleteWebAuthnCredentials behind flag
    This changes device::fido::mac::DeleteWebAuthnCredentials to have no
    effect when the feature flag for the Touch ID authenticator
    (WebAuthenticationTouchId) is turned off.
    
    Bug: 678128
    Change-Id: I2fef6fe53903dad3329a075aa00d5152365af133
    Reviewed-on: https://chromium-review.googlesource.com/1129779
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/master@{#573832}

--- device/fido/mac/browsing_data_deletion.mm
+++ device/fido/mac/browsing_data_deletion.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Foundation/Foundation.h>
 #import <Security/Security.h>
 
+#include "base/feature_list.h"
 #include "base/logging.h"
 #include "base/mac/foundation_util.h"
 #include "base/mac/mac_logging.h"
@@ -17,6 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/optional.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/time/time.h"
+#include "build/build_config.h"
+#include "device/base/features.h"
 #include "device/fido/mac/credential_metadata.h"
 #include "device/fido/mac/keychain.h"
 
@@ -142,12 +145,16 @@ bool DeleteWebAuthnCredentials(const std::string& keychain_access_group,
                                const std::string& profile_metadata_secret,
                                base::Time begin,
                                base::Time end) {
-  // Touch ID uses macOS APIs available in 10.12.2 or newer. No need to check
-  // for credentials in lower OS versions.
-  if (__builtin_available(macos 10.12.2, *)) {
-    return DoDeleteWebAuthnCredentials(keychain_access_group,
-                                       profile_metadata_secret, begin, end);
+#if defined(OS_MACOSX)
+  if (base::FeatureList::IsEnabled(device::kWebAuthTouchId)) {
+    // Touch ID uses macOS APIs available in 10.12.2 or newer. No need to check
+    // for credentials in lower OS versions.
+    if (__builtin_available(macos 10.12.2, *)) {
+      return DoDeleteWebAuthnCredentials(keychain_access_group,
+                                         profile_metadata_secret, begin, end);
+    }
   }
+#endif  // defined(OS_MACOSX)
   return true;
 }
 
