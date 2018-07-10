1cfd7ed2b8a19d57bce388ede589f8f07f49eb53
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-07-10 06:57:23 +0000

    Import wpt@afc3a452f2020dc10c51f467a435ba5b32dc0589
    Using wpt-import in Chromium 10326802c7d4a7a2d752f18bfcbfb55c4329c22a.
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/21154
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    alexander.shalamov@intel.com, rijubrata.bhaumik@intel.com, timvolodine@chromium.org:
      external/wpt/generic-sensor
    meade@chromium.org:
      external/wpt/web-animations
    yhirano@chromium.org, mkwst@chromium.org, japhet@chromium.org:
      external/wpt/fetch
    
    TBR=kereliuk
    
    No-Export: true
    Change-Id: I7c46758f19e525de0c2e0c613f5265731ee200a8
    Reviewed-on: https://chromium-review.googlesource.com/1130952
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573638}

---
+++ third_party/WebKit/LayoutTests/external/wpt/generic-sensor/idlharness.https.window.js
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/resources/WebIDLParser.js
+// META: script=/resources/idlharness.js
+
+// https://w3c.github.io/sensors/
+
+'use strict';
+
+function cast(i, t) {
+  return Object.assign(Object.create(t.prototype), i);
+}
+
+idl_test(
+  ['generic-sensor'],
+  ['dom', 'html', 'WebIDL'],
+  idl_array => {
+    idl_array.add_objects({
+      Sensor: ['cast(new Accelerometer(), Sensor)'],
+      SensorErrorEvent: [
+        'new SensorErrorEvent("error", { error: new DOMException });'
+      ],
+    });
+  },
+  'Test IDL implementation of Generic Sensor');
