776cf4379b0bb407224b88a4df223e35da4639fa
Kenneth Russell <kbr@chromium.org>
2018-07-10 01:12:54 +0000

    Mark a context_lost test flaky on Mac ASAN.
    GpuCrash_GPUProcessCrashesExactlyOncePerVisitToAboutGpuCrash
    occasionally crashes the renderer process while navigating to
    about:gpucrash. The test's main tab appears to crash too.
    
    Required passing an --is-asan argument to the test, similarly to the
    WebGL conformance tests.
    
    Bug: 861956
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: Ic18876e88a0d99c9152be1a67599f2cae3cd4881
    Tbr: jmadill@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/1130507
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573577}

--- content/test/gpu/gpu_tests/context_lost_integration_test.py
+++ content/test/gpu/gpu_tests/context_lost_integration_test.py
@@ -53,9 +53,17 @@ harness_script = r"""
 
 class ContextLostIntegrationTest(gpu_integration_test.GpuIntegrationTest):
 
+  _is_asan = False
+
   @classmethod
   def Name(cls):
     return 'context_lost'
+
+  @classmethod
+  def AddCommandlineArgs(cls, parser):
+    parser.add_option('--is-asan',
+        help='Indicates whether currently running an ASAN build',
+        action='store_true')
 
   @staticmethod
   def _AddDefaultArgs(browser_args):
@@ -67,6 +75,7 @@ class ContextLostIntegrationTest(gpu_integration_test.GpuIntegrationTest):
 
   @classmethod
   def GenerateGpuTests(cls, options):
+    cls._is_asan = options.is_asan
     tests = (('GpuCrash_GPUProcessCrashesExactlyOncePerVisitToAboutGpuCrash',
               'gpu_process_crash.html'),
              ('ContextLost_WebGLContextLostFromGPUProcessExit',
@@ -95,7 +104,8 @@ class ContextLostIntegrationTest(gpu_integration_test.GpuIntegrationTest):
 
   @classmethod
   def _CreateExpectations(cls):
-    return context_lost_expectations.ContextLostExpectations()
+    return context_lost_expectations.ContextLostExpectations(
+      is_asan=cls._is_asan)
 
   @classmethod
   def SetUpProcess(cls):
