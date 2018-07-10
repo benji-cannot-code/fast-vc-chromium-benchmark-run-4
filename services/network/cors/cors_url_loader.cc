790bd9693c221c4c45cdaf600cbd0c3b0918a9b8
Yutaka Hirano <yhirano@chromium.org>
2018-07-10 16:14:44 +0000

    [OOR-CORS] Omit credentials on cross-origin redirects
    When request's credentials mode is "same-origin" and the request goes to
    cross-origin, we should stop sending credentials data. This is
    implemented in renderers (when making a request) and in net/
    (on redirect), but CORSURLLoader sometimes creates a new network loader
    on redirect, and in such a case the net/ logic doesn't work. This CL
    adds the logic in CORSURLLoader.
    
    Bug: 736308
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Change-Id: I79b287159233f8924fb2b459e0dee62ef31a0fe9
    Reviewed-on: https://chromium-review.googlesource.com/1126805
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573760}

--- services/network/cors/cors_url_loader.cc
+++ services/network/cors/cors_url_loader.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/cors/cors_url_loader.h"
 
 #include "base/stl_util.h"
+#include "net/base/load_flags.h"
 #include "services/network/cors/preflight_controller.h"
 #include "services/network/public/cpp/cors/cors.h"
 #include "url/url_util.h"
@@ -148,6 +149,16 @@ void CORSURLLoader::FollowRedirect(
   if (request_finalizer_)
     request_finalizer_.Run(request_id_);
   network_client_binding_.Unbind();
+
+  if (request_.fetch_credentials_mode ==
+      mojom::FetchCredentialsMode::kSameOrigin) {
+    // If the credentials mode is "same-origin" and CORS flag is set, we must
+    // not send credentials. As network::URLLoaderImpl doesn't understand
+    // |fetch_credentials_mode|, we need to set the load flags here.
+    request_.load_flags |= net::LOAD_DO_NOT_SAVE_COOKIES;
+    request_.load_flags |= net::LOAD_DO_NOT_SEND_COOKIES;
+    request_.load_flags |= net::LOAD_DO_NOT_SEND_AUTH_DATA;
+  }
   StartRequest();
 }
 
