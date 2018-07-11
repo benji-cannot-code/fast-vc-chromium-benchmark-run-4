1ba0934152784bca6ae41774bc6f3cd31e2851f6
Jinsuk Kim <jinsukkim@chromium.org>
2018-07-11 02:22:06 +0000

    RenderViewHostChanged in WebContentsView
    https://crrev.com/c/1112950 didn't quite fix one of the reported bugs
    https://crbug.com/854499 about webview not getting key input. This CL
    aims to address it by replacing WebContentsView::RenderFrameSwappedIn
    with WCV::RenderViewHostChanged, and letting WebContentsImpl call it
    upon NotifyViewSwapped.
    
    Verified, with the change above, that updating focus/native view tree
    takes effect as expected:
     - Build/install webview apk: ninja -C out/Default webview_system_apk
     - Open Gmail or Amazon app
     - Go to account creation page
     - Verify that click on input form brings up keyboard, and letters
       can be typed in
    
    Bug: 860173, 860235
    Change-Id: Iaf2013bcb2990b70ba8197eee86f0ababca54088
    Reviewed-on: https://chromium-review.googlesource.com/1126580
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574040}

--- content/browser/web_contents/web_contents_view.h
+++ content/browser/web_contents/web_contents_view.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/native_widget_types.h"
 
 namespace content {
-class RenderFrameHost;
 class RenderViewHost;
 class RenderWidgetHost;
 class RenderWidgetHostViewBase;
@@ -115,11 +114,10 @@ class WebContentsView {
   // Invoked when the WebContents is notified that the RenderView is ready.
   virtual void RenderViewReady() = 0;
 
-  // Invoked when the WebContents is notified that the RenderFrame has been
-  // swapped in.
-  virtual void RenderFrameSwappedIn(RenderFrameHost* old_host,
-                                    RenderFrameHost* new_host,
-                                    bool is_main_frame) = 0;
+  // Invoked when the WebContents is notified that the RenderViewHost has been
+  // changed.
+  virtual void RenderViewHostChanged(RenderViewHost* old_host,
+                                     RenderViewHost* new_host) = 0;
 
   // Invoked to enable/disable overscroll gesture navigation.
   virtual void SetOverscrollControllerEnabled(bool enabled) = 0;
--- content/browser/web_contents/web_contents_view_mac.mm
+++ content/browser/web_contents/web_contents_view_mac.mm
@@ -426,9 +426,8 @@ void WebContentsViewMac::RenderViewCreated(RenderViewHost* host) {
 
 void WebContentsViewMac::RenderViewReady() {}
 
-void WebContentsViewMac::RenderFrameSwappedIn(RenderFrameHost* old_host,
-                                              RenderFrameHost* new_host,
-                                              bool is_main_frame) {}
+void WebContentsViewMac::RenderViewHostChanged(RenderViewHost* old_host,
+                                               RenderViewHost* new_host) {}
 
 void WebContentsViewMac::SetOverscrollControllerEnabled(bool enabled) {
 }
