550643b54039dbc2000654eef1b31382c689be21
Katie D <katie@chromium.org>
2018-07-11 17:49:55 +0000

    Support multiple focus rects from different sources at once.
    This refactors AccessibilityFocusRingController components needed
    for tracking an individual group of focus rings into
    AccessibilityFocusRingGroup. The Controller maps from caller_ids
    to AccessibilityFocusRingGroups. Unit tests are updated to ensure
    coverage.
    
    See go/cros-multiple-focus-rings for background.
    
    Bug: 830967
    Change-Id: Ic04c883b2445fc74747cc93e907d373fba65949f
    Reviewed-on: https://chromium-review.googlesource.com/1120838
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Reviewed-by: Martin Barbella <mbarbella@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574234}

--- ash/accessibility/touch_exploration_manager.cc
+++ ash/accessibility/touch_exploration_manager.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/audio/chromeos_sounds.h"
 #include "chromeos/audio/cras_audio_handler.h"
 #include "chromeos/chromeos_switches.h"
+#include "extensions/common/constants.h"
 #include "ui/aura/client/aura_constants.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/keyboard/keyboard_controller.h"
@@ -227,7 +228,8 @@ void TouchExplorationManager::UpdateTouchExplorationState() {
       // Clear the focus highlight.
       Shell::Get()->accessibility_focus_ring_controller()->SetFocusRing(
           std::vector<gfx::Rect>(),
-          mojom::FocusRingBehavior::PERSIST_FOCUS_RING);
+          mojom::FocusRingBehavior::PERSIST_FOCUS_RING,
+          extension_misc::kChromeVoxExtensionId);
     } else {
       touch_exploration_controller_->SetExcludeBounds(gfx::Rect());
     }
