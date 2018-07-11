2b65c215b92fb1959421f1f81f4c533eda92546d
Alice Boxhall <aboxhall@chromium.org>
2018-07-11 05:07:16 +0000

    Revert "Migrate //content unit tests off of QuitCurrent*Deprecated()."
    This reverts commit 1afbb0e4b7797a74e6713c4a4ab9de045c0db342.
    
    Reason for revert: Causing test failures on Android CFI:
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Android%20CFI/2026
    https://logs.chromium.org/v/?s=chromium%2Fbuildbucket%2Fcr-buildbucket.appspot.com%2F8941340307860291344%2F%2B%2Fsteps%2Fcontent_unittests_on_Android_device_Nexus_5X%2F0%2Flogs%2FDevToolsManagerTest.NoUnresponsiveDialogInInspectedContents%2F0
    
    Original change's description:
    > Migrate //content unit tests off of QuitCurrent*Deprecated().
    > 
    > Cq-Include-Trybots: luci.chromium.try:linux_mojo
    > Change-Id: If5cf7d3dcda8dc411c6c90de31885dca683354d8
    > Reviewed-on: https://chromium-review.googlesource.com/1130656
    > Reviewed-by: Bo <boliu@chromium.org>
    > Commit-Queue: Wez <wez@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#574006}
    
    TBR=wez@chromium.org,boliu@chromium.org
    
    Change-Id: Icb42050ab3fcbf283098bb36943a8c9dfbefccf8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Reviewed-on: https://chromium-review.googlesource.com/1132636
    Reviewed-by: Alice Boxhall <aboxhall@chromium.org>
    Commit-Queue: Alice Boxhall <aboxhall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574082}

--- content/browser/renderer_host/render_widget_host_unittest.cc
+++ content/browser/renderer_host/render_widget_host_unittest.cc
@@ -682,13 +682,6 @@ class RenderWidgetHostTest : public testing::Test {
     return handle_mouse_event_;
   }
 
-  void RunLoopFor(base::TimeDelta duration) {
-    base::RunLoop run_loop;
-    base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
-        FROM_HERE, run_loop.QuitClosure(), duration);
-    run_loop.Run();
-  }
-
  protected:
   // testing::Test
   void SetUp() override {
@@ -1503,7 +1496,10 @@ TEST_F(RenderWidgetHostTest, DontPostponeInputEventAckTimeout) {
   host_->StartInputEventAckTimeout(TimeDelta::FromSeconds(30));
 
   // Wait long enough for first timeout and see if it fired.
-  RunLoopFor(TimeDelta::FromMilliseconds(10));
+  base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
+      FROM_HERE, base::RunLoop::QuitCurrentWhenIdleClosureDeprecated(),
+      TimeDelta::FromMilliseconds(10));
+  base::RunLoop().Run();
   EXPECT_TRUE(delegate_->unresponsive_timer_fired());
 }
 
@@ -1519,7 +1515,10 @@ TEST_F(RenderWidgetHostTest, StopAndStartInputEventAckTimeout) {
   host_->StartInputEventAckTimeout(TimeDelta::FromMilliseconds(10));
 
   // Wait long enough for first timeout and see if it fired.
-  RunLoopFor(TimeDelta::FromMilliseconds(40));
+  base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
+      FROM_HERE, base::RunLoop::QuitCurrentWhenIdleClosureDeprecated(),
+      TimeDelta::FromMilliseconds(40));
+  base::RunLoop().Run();
   EXPECT_TRUE(delegate_->unresponsive_timer_fired());
 }
 
@@ -1534,7 +1533,10 @@ TEST_F(RenderWidgetHostTest, ShorterDelayInputEventAckTimeout) {
   host_->StartInputEventAckTimeout(TimeDelta::FromMilliseconds(20));
 
   // Wait long enough for the second timeout and see if it fired.
-  RunLoopFor(TimeDelta::FromMilliseconds(25));
+  base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
+      FROM_HERE, base::RunLoop::QuitCurrentWhenIdleClosureDeprecated(),
+      TimeDelta::FromMilliseconds(25));
+  base::RunLoop().Run();
   EXPECT_TRUE(delegate_->unresponsive_timer_fired());
 }
 
@@ -1549,18 +1551,27 @@ TEST_F(RenderWidgetHostTest, InputEventAckTimeoutDisabledForInputWhenHidden) {
 
   // The timeout should not fire.
   EXPECT_FALSE(delegate_->unresponsive_timer_fired());
-  RunLoopFor(TimeDelta::FromMicroseconds(2));
+  base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
+      FROM_HERE, base::RunLoop::QuitCurrentWhenIdleClosureDeprecated(),
+      TimeDelta::FromMicroseconds(2));
+  base::RunLoop().Run();
   EXPECT_FALSE(delegate_->unresponsive_timer_fired());
 
   // The timeout should never reactivate while hidden.
   SimulateMouseEvent(WebInputEvent::kMouseMove, 10, 10, 0, false);
-  RunLoopFor(TimeDelta::FromMicroseconds(2));
+  base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
+      FROM_HERE, base::RunLoop::QuitCurrentWhenIdleClosureDeprecated(),
+      TimeDelta::FromMicroseconds(2));
+  base::RunLoop().Run();
   EXPECT_FALSE(delegate_->unresponsive_timer_fired());
 
   // Showing the widget should restore the timeout, as the events have
   // not yet been ack'ed.
   host_->WasShown(false /* record_presentation_time */);
-  RunLoopFor(TimeDelta::FromMicroseconds(2));
+  base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
+      FROM_HERE, base::RunLoop::QuitCurrentWhenIdleClosureDeprecated(),
+      TimeDelta::FromMicroseconds(2));
+  base::RunLoop().Run();
   EXPECT_TRUE(delegate_->unresponsive_timer_fired());
 }
 
@@ -1585,7 +1596,10 @@ TEST_F(RenderWidgetHostTest, MultipleInputEvents) {
   dispatched_events[0]->ToEvent()->CallCallback(INPUT_EVENT_ACK_STATE_CONSUMED);
 
   // Wait long enough for first timeout and see if it fired.
-  RunLoopFor(TimeDelta::FromMicroseconds(20));
+  base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
+      FROM_HERE, base::RunLoop::QuitCurrentWhenIdleClosureDeprecated(),
+      TimeDelta::FromMicroseconds(20));
+  base::RunLoop().Run();
   EXPECT_TRUE(delegate_->unresponsive_timer_fired());
 }
 
@@ -1621,7 +1635,10 @@ TEST_F(RenderWidgetHostTest, NewContentRenderingTimeoutWithoutSurfaceSync) {
                    .Build();
   host_->SubmitCompositorFrame(local_surface_id, std::move(frame),
                                base::nullopt, 0);
-  RunLoopFor(TimeDelta::FromMicroseconds(20));
+  base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
+      FROM_HERE, base::RunLoop::QuitCurrentWhenIdleClosureDeprecated(),
+      TimeDelta::FromMicroseconds(20));
+  base::RunLoop().Run();
 
   EXPECT_FALSE(host_->new_content_rendering_timeout_fired());
   host_->reset_new_content_rendering_timeout_fired();
@@ -1635,7 +1652,10 @@ TEST_F(RenderWidgetHostTest, NewContentRenderingTimeoutWithoutSurfaceSync) {
               .Build();
   host_->SubmitCompositorFrame(local_surface_id, std::move(frame),
                                base::nullopt, 0);
-  RunLoopFor(TimeDelta::FromMicroseconds(20));
+  base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
+      FROM_HERE, base::RunLoop::QuitCurrentWhenIdleClosureDeprecated(),
+      TimeDelta::FromMicroseconds(20));
+  base::RunLoop().Run();
 
   EXPECT_TRUE(host_->new_content_rendering_timeout_fired());
   host_->reset_new_content_rendering_timeout_fired();
@@ -1649,14 +1669,20 @@ TEST_F(RenderWidgetHostTest, NewContentRenderingTimeoutWithoutSurfaceSync) {
   host_->SubmitCompositorFrame(local_surface_id, std::move(frame),
                                base::nullopt, 0);
   host_->DidNavigate(7);
-  RunLoopFor(TimeDelta::FromMicroseconds(20));
+  base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
+      FROM_HERE, base::RunLoop::QuitCurrentWhenIdleClosureDeprecated(),
+      TimeDelta::FromMicroseconds(20));
+  base::RunLoop().Run();
 
   EXPECT_FALSE(host_->new_content_rendering_timeout_fired());
   host_->reset_new_content_rendering_timeout_fired();
 
   // Don't send any frames after the timer starts. The timer should fire.
   host_->DidNavigate(20);
-  RunLoopFor(TimeDelta::FromMicroseconds(20));
+  base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
+      FROM_HERE, base::RunLoop::QuitCurrentWhenIdleClosureDeprecated(),
+      TimeDelta::FromMicroseconds(20));
+  base::RunLoop().Run();
   EXPECT_TRUE(host_->new_content_rendering_timeout_fired());
   host_->reset_new_content_rendering_timeout_fired();
 }
