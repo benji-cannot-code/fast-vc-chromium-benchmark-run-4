28bdded123e959b9df2dac680fc754ec942a560e
Justin Donnelly <jdonnelly@chromium.org>
2018-07-11 15:05:31 +0000

    Update features::IsViewsBrowserCocoa() to respect kExperimentalUi.
    When we added kExperimentalUi, I added it to
    views_mode_controller::IsViewsBrowserCocoa() but I didn't notice
    that there was this other implementation.
    
    The omnibox has a feature that uses features::IsViewsBrowserCocoa() and
    I'd like it to trigger on kExperimentalUi. Let me know if there's some
    reason why we'd expect these to give different answers. There are only a
    handful of places that depend on the features:: version:
    
    https://cs.chromium.org/search/?q=features::IsViewsBrowserCocoa
    
    Bug: 862336
    Change-Id: Ieffe5d13bb08c5c36994cef9ccc850ca4552c2b2
    Reviewed-on: https://chromium-review.googlesource.com/1132430
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574162}

--- chrome/browser/ui/views_mode_controller.cc
+++ chrome/browser/ui/views_mode_controller.cc
@@ -13,8 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace views_mode_controller {
 
 bool IsViewsBrowserCocoa() {
-  return features::IsViewsBrowserCocoa() &&
-         !base::FeatureList::IsEnabled(features::kExperimentalUi);
+  return features::IsViewsBrowserCocoa();
 }
 
 }  // namespace views_mode_controller
