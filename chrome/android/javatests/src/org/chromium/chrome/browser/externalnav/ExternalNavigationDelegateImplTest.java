4ac173dc85f37ea891d8c08c018381a443fa2461
huangxueqing <huangxueqing@xiaomi.com>
2018-07-11 14:34:22 +0000

    Reland "Ignore Intent filters with wildcard hosts when handling links."
    This is a reland of 6d48958d7cd5f2d958a846e35d410f4ed5b85934
    
    Don't check acceptable scheme links since UrlUtilities.isAcceptedScheme()
    was native function, libchrome.so had not loaded when handle intent.
    
    Original change's description:
    > Ignore Intent filters with wildcard hosts when handling links.
    >
    > If the Android device has a package installed that declares
    > an Intent filter with `android:host="*"`, Chrome will launch
    > the Intent picker UX whenever a person taps a link.
    > This is a poor user experience. (Using `android:host="*"`
    > arguably doesn't make much sense; normally, Intent filters
    > would use `*` to match subdomains such as *.google.com.) Thus,
    > skip Intent filters with such wildcard hosts when handling
    > acceptable scheme links.
    >
    > R=dtrainor@chromium.org
    >
    > Bug: 841133
    > Change-Id: I3cb14309ed53e0bd623ed0da59af6635391a8718
    > Reviewed-on: https://chromium-review.googlesource.com/1053847
    > Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    > Reviewed-by: Chris Palmer <palmer@chromium.org>
    > Reviewed-by: Maria Khomenko <mariakhomenko@chromium.org>
    > Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#564487}
    
    Bug: 841133
    Change-Id: Id2fd7f7364eb6c70280b29a1f71783a2ee89747b
    Reviewed-on: https://chromium-review.googlesource.com/1096554
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Chris Palmer <palmer@chromium.org>
    Reviewed-by: Maria Khomenko <mariakhomenko@chromium.org>
    Commit-Queue: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574155}

--- chrome/android/javatests/src/org/chromium/chrome/browser/externalnav/ExternalNavigationDelegateImplTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/externalnav/ExternalNavigationDelegateImplTest.java
@@ -5,9 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.externalnav;
 
+import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.ActivityInfo;
 import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.os.Build;
 import android.support.test.filters.SmallTest;
 
 import org.junit.Assert;
@@ -48,7 +51,7 @@ public class ExternalNavigationDelegateImplTest {
         List<ResolveInfo> resolveInfos = new ArrayList<ResolveInfo>();
         Assert.assertEquals(0,
                 ExternalNavigationDelegateImpl
-                        .getSpecializedHandlersWithFilter(resolveInfos, packageName)
+                        .getSpecializedHandlersWithFilter(resolveInfos, packageName, null)
                         .size());
     }
 
@@ -61,7 +64,7 @@ public class ExternalNavigationDelegateImplTest {
         List<ResolveInfo> resolveInfos = makeResolveInfos(info);
         Assert.assertEquals(0,
                 ExternalNavigationDelegateImpl
-                        .getSpecializedHandlersWithFilter(resolveInfos, packageName)
+                        .getSpecializedHandlersWithFilter(resolveInfos, packageName, null)
                         .size());
     }
 
@@ -75,7 +78,7 @@ public class ExternalNavigationDelegateImplTest {
         List<ResolveInfo> resolveInfos = makeResolveInfos(info);
         Assert.assertEquals(1,
                 ExternalNavigationDelegateImpl
-                        .getSpecializedHandlersWithFilter(resolveInfos, packageName)
+                        .getSpecializedHandlersWithFilter(resolveInfos, packageName, null)
                         .size());
     }
 
@@ -89,7 +92,49 @@ public class ExternalNavigationDelegateImplTest {
         List<ResolveInfo> resolveInfos = makeResolveInfos(info);
         Assert.assertEquals(1,
                 ExternalNavigationDelegateImpl
-                        .getSpecializedHandlersWithFilter(resolveInfos, packageName)
+                        .getSpecializedHandlersWithFilter(resolveInfos, packageName, null)
+                        .size());
+    }
+
+    @Test
+    @SmallTest
+    public void testIsPackageSpecializedHandler_WithAuthority_Wildcard_Host() {
+        String packageName = "";
+        ResolveInfo info = new ResolveInfo();
+        info.filter = new IntentFilter();
+        info.filter.addDataAuthority("*", null);
+        List<ResolveInfo> resolveInfos = makeResolveInfos(info);
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com"));
+        Assert.assertEquals(0,
+                ExternalNavigationDelegateImpl
+                        .getSpecializedHandlersWithFilter(resolveInfos, packageName, intent)
+                        .size());
+
+        Intent intentWildcardHost =
+                new Intent(Intent.ACTION_VIEW, Uri.parse("https://*.google.com"));
+        Assert.assertEquals(0,
+                ExternalNavigationDelegateImpl
+                        .getSpecializedHandlersWithFilter(
+                                resolveInfos, packageName, intentWildcardHost)
+                        .size());
+
+        ResolveInfo infoWildcardSubDomain = new ResolveInfo();
+        infoWildcardSubDomain.filter = new IntentFilter();
+        infoWildcardSubDomain.filter.addDataAuthority("http://*.google.com", "80");
+        List<ResolveInfo> resolveInfosWildcardSubDomain = makeResolveInfos(infoWildcardSubDomain);
+        Intent intentSubDomain1 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://google.com"));
+        Assert.assertEquals(1,
+                ExternalNavigationDelegateImpl
+                        .getSpecializedHandlersWithFilter(
+                                resolveInfosWildcardSubDomain, packageName, intentSubDomain1)
+                        .size());
+
+        Intent intentSubDomain2 =
+                new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com"));
+        Assert.assertEquals(1,
+                ExternalNavigationDelegateImpl
+                        .getSpecializedHandlersWithFilter(
+                                resolveInfosWildcardSubDomain, packageName, intentSubDomain2)
                         .size());
     }
 
@@ -105,7 +150,7 @@ public class ExternalNavigationDelegateImplTest {
         List<ResolveInfo> resolveInfos = makeResolveInfos(info);
         Assert.assertEquals(1,
                 ExternalNavigationDelegateImpl
-                        .getSpecializedHandlersWithFilter(resolveInfos, packageName)
+                        .getSpecializedHandlersWithFilter(resolveInfos, packageName, null)
                         .size());
     }
 
@@ -121,7 +166,7 @@ public class ExternalNavigationDelegateImplTest {
         List<ResolveInfo> resolveInfos = makeResolveInfos(info);
         Assert.assertEquals(0,
                 ExternalNavigationDelegateImpl
-                        .getSpecializedHandlersWithFilter(resolveInfos, packageName)
+                        .getSpecializedHandlersWithFilter(resolveInfos, packageName, null)
                         .size());
     }
 
@@ -133,13 +178,19 @@ public class ExternalNavigationDelegateImplTest {
         info.filter = new IntentFilter();
         info.filter.addDataPath("somepath", 2);
         info.activityInfo = new ActivityInfo();
-        info.activityInfo.name = InstantAppsHandler.EPHEMERAL_INSTALLER_CLASS;
+
+        // See InstantAppsHandler.isInstantAppResolveInfo
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            info.isInstantAppAvailable = true;
+        } else {
+            info.activityInfo.name = InstantAppsHandler.EPHEMERAL_INSTALLER_CLASS;
+        }
         info.activityInfo.packageName = "com.google.android.gms";
         List<ResolveInfo> resolveInfos = makeResolveInfos(info);
         // Ephemeral resolver is not counted as a specialized handler.
         Assert.assertEquals(0,
                 ExternalNavigationDelegateImpl
-                        .getSpecializedHandlersWithFilter(resolveInfos, packageName)
+                        .getSpecializedHandlersWithFilter(resolveInfos, packageName, null)
                         .size());
     }
 
