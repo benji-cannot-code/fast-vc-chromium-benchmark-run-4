d3a66816efb5d513c0c47c35c57beb17d01c5cb5
Francois Doray <fdoray@chromium.org>
2018-07-11 18:35:00 +0000

    Build chrome/browser/metrics/desktop_session_duration/ on ChromeOS.
    This directory was previously excluded from the ChromeOS build. It
    forced #ifdefs to be added to code using DesktopSessionDurationTracker
    (e.g. https://cs.chromium.org/chromium/src/chrome/browser/resource_coordinator/usage_clock.cc?rcl=ae0af1511d081f85641f82ac650919dafffa1ee6).
    With this CL, the directory is built and the #ifdefs are not longer
    required.
    
    Note that for now, DesktopSessionDurationTracker is still not
    initialized on ChromeOS.
    
    Bug: 775644
    Change-Id: Ic3fcd2646f7577805b986888bc22f79579c9d736
    Reviewed-on: https://chromium-review.googlesource.com/1133280
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574256}

--- chrome/browser/metrics/desktop_session_duration/desktop_profile_session_durations_service_factory.cc
+++ chrome/browser/metrics/desktop_session_duration/desktop_profile_session_durations_service_factory.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/signin/profile_oauth2_token_service_factory.h"
 #include "chrome/browser/sync/profile_sync_service_factory.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
+#include "components/signin/core/browser/profile_oauth2_token_service.h"
 #include "content/public/browser/browser_context.h"
 
 namespace metrics {
