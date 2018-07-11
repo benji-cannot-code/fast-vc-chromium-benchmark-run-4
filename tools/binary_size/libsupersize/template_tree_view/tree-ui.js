1002a20b7b30b62fd5b9cda07a5d4e9a03ba64e9
Tiger Oakes <tigero@google.com>
2018-07-11 16:45:35 +0000

    Supersize: Adds small quality of life improvements
    Changes so far:
    - Improved reading of a node in a screen reader
    - Added checkboxes and radios into tab flow
    - Settings starts expanded instead of hidden
    - Arrow immediately rotates when expanding a row
    - Added "Select all" and "Select none" buttons for types
    - Query parameters equal to the defaults are hidden
    - Types are shown as a single string in the query
    - .bss is no longer checked by default
    - Fetch requests are cancelled when a new one is started
    
    Demo:
    https://notwoods.github.io/chrome-supersize-reports/monochrome-2018-07-10/
    
    Bug: 847599
    Change-Id: I366ff2746a331e846dc0c683757a2a5bfa43d96a
    Reviewed-on: https://chromium-review.googlesource.com/1132132
    Commit-Queue: Tiger Oakes <tigero@google.com>
    Reviewed-by: Eric Stevenson <estevenson@chromium.org>
    Reviewed-by: agrieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574194}

--- tools/binary_size/libsupersize/template_tree_view/tree-ui.js
+++ tools/binary_size/libsupersize/template_tree_view/tree-ui.js
@@ -147,6 +147,8 @@ const worker = new TreeWorker('tree-worker.js');
       element.setAttribute('aria-expanded', 'false');
       dom.replace(group, null);
     } else {
+      element.setAttribute('aria-expanded', 'true');
+
       let data = _uiNodeData.get(link);
       if (data == null || data.children == null) {
         const idPath = link.querySelector('.symbol-name').title;
@@ -165,7 +167,6 @@ const worker = new TreeWorker('tree-worker.js');
       // Update DOM
       requestAnimationFrame(() => {
         group.appendChild(newElementsFragment);
-        element.setAttribute('aria-expanded', 'true');
       });
     }
   }
@@ -315,9 +316,6 @@ const worker = new TreeWorker('tree-worker.js');
     if (_CONTAINER_TYPE_SET.has(type)) {
       const symbolStyle = getIconStyle(data.type[1]);
       icon.setAttribute('fill', symbolStyle.color);
-      icon.querySelector('title').textContent += ` - Mostly ${
-        symbolStyle.description
-      }`;
     }
     // Insert an SVG icon at the start of the link to represent type
     link.insertBefore(icon, link.firstElementChild);
@@ -360,11 +358,15 @@ const worker = new TreeWorker('tree-worker.js');
   });
 
   _symbolTree.addEventListener('keydown', _handleKeyNavigation);
-  _symbolTree.addEventListener('focusin', event =>
+  _symbolTree.addEventListener('focusin', event => {
     displayInfocard(
       _uiNodeData.get(event.target),
       state.has('method_count') ? _getMethodCountContents : _getSizeContents
-    )
+    );
+    event.currentTarget.parentElement.classList.add('focused');
+  });
+  _symbolTree.addEventListener('focusout', event =>
+    event.currentTarget.parentElement.classList.remove('focused')
   );
 
   self.newTreeElement = newTreeElement;
