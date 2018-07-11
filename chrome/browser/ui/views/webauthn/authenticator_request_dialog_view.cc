4a85592e7d0ec6c4f7b8ace6d1bd49001d807189
Tomasz Moniuszko <tmoniuszko@opera.com>
2018-07-11 06:55:34 +0000

    Add missing include for views::CreateEmptyBorder
    Change-Id: I5558e93ca31e5ab546457b0e07a73e5f1a60c614
    Reviewed-on: https://chromium-review.googlesource.com/1125074
    Reviewed-by: Kim Paulhamus <kpaulhamus@chromium.org>
    Commit-Queue: Tomasz Moniuszko <tmoniuszko@opera.com>
    Cr-Commit-Position: refs/heads/master@{#574095}

--- chrome/browser/ui/views/webauthn/authenticator_request_dialog_view.cc
+++ chrome/browser/ui/views/webauthn/authenticator_request_dialog_view.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/web_modal/web_contents_modal_dialog_manager.h"
 #include "components/web_modal/web_contents_modal_dialog_manager_delegate.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/views/border.h"
 #include "ui/views/layout/fill_layout.h"
 
 // static
