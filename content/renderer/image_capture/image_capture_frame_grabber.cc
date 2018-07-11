ed69d1b42081556adc47012f962c8f08dfd919f1
Miguel Casas <mcasas@chromium.org>
2018-07-11 12:42:41 +0000

    Roll src/third_party/libyuv/ 4d67b3e85..55f5d91f1 with type fixes
    This CL is a spin off of crrev.com/c/1130662/4 with the old data type
    uses (e.g. int8, int16, int32) corrected (e.g. int8_t, int16_t, int32_t)
    etc.
    
    https://chromium.googlesource.com/libyuv/libyuv.git/+log/4d67b3e851b1..55f5d91f11f9
    
    $ git log 4d67b3e85..55f5d91f1 --date=short --no-merges --format='%ad %ae %s'
    2018-07-09 fbarchard Disable old int types by default.
    2018-07-03 fbarchard msa use void * for loads
    2018-06-29 johannkoenig xcode: disable avx512 support
    
    Created with:
      roll-dep src/third_party/libyuv
    
    Bug: libyuv:807
    Test: local builds and try bots for libyuv pass.
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel
    Change-Id: I60d62b047b04c6c0f821df15a5f5b4be85cf331c
    Reviewed-on: https://chromium-review.googlesource.com/1131560
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Frank Barchard <fbarchard@chromium.org>
    Reviewed-by: Emircan Uysaler <emircan@chromium.org>
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574133}

--- content/renderer/image_capture/image_capture_frame_grabber.cc
+++ content/renderer/image_capture/image_capture_frame_grabber.cc
@@ -80,7 +80,7 @@ void ImageCaptureFrameGrabber::SingleShotFrameHandler::OnVideoFrameOnIOThread(
     return;
   }
 
-  const uint32 destination_pixel_format =
+  const uint32_t destination_pixel_format =
       (kN32_SkColorType == kRGBA_8888_SkColorType) ? libyuv::FOURCC_ABGR
                                                    : libyuv::FOURCC_ARGB;
 
@@ -90,7 +90,7 @@ void ImageCaptureFrameGrabber::SingleShotFrameHandler::OnVideoFrameOnIOThread(
                           frame->stride(media::VideoFrame::kUPlane),
                           frame->visible_data(media::VideoFrame::kVPlane),
                           frame->stride(media::VideoFrame::kVPlane),
-                          static_cast<uint8*>(pixmap.writable_addr()),
+                          static_cast<uint8_t*>(pixmap.writable_addr()),
                           pixmap.width() * 4, pixmap.width(), pixmap.height(),
                           destination_pixel_format);
 
@@ -99,7 +99,7 @@ void ImageCaptureFrameGrabber::SingleShotFrameHandler::OnVideoFrameOnIOThread(
     // This function copies any plane into the alpha channel of an ARGB image.
     libyuv::ARGBCopyYToAlpha(frame->visible_data(media::VideoFrame::kAPlane),
                              frame->stride(media::VideoFrame::kAPlane),
-                             static_cast<uint8*>(pixmap.writable_addr()),
+                             static_cast<uint8_t*>(pixmap.writable_addr()),
                              pixmap.width() * 4, pixmap.width(),
                              pixmap.height());
   }
