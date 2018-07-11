32bf106ade46c4857aa59739026db79cd2eb509a
Tanja Gornak <melandory@chromium.org>
2018-07-11 10:15:19 +0000

    Ditch the dependency to some part of cacheinvalidation library.
    TBR=pavely@chromium.org
    
    Bug: 799905
    Change-Id: Ib736c2f4e74bd025c1f3a70fac59c4c8cbf78a4a
    Reviewed-on: https://chromium-review.googlesource.com/1125923
    Commit-Queue: Tatiana Gornak <melandory@chromium.org>
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574121}

---
+++ components/invalidation/impl/network_channel.h
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_INVALIDATION_IMPL_NETWORK_CHANNEL_H_
+#define COMPONENTS_INVALIDATION_IMPL_NETWORK_CHANNEL_H_
+
+#include "base/callback.h"
+
+namespace syncer {
+
+using MessageCallback =
+    base::RepeatingCallback<void(const std::string& message)>;
+using TokenCallback = base::RepeatingCallback<void(const std::string& message)>;
+
+/*
+ * Interface specifying the functionality of the network, required by
+ * invalidation client.
+ */
+class NetworkChannel {
+ public:
+  virtual ~NetworkChannel() {}
+
+  /* Sets the receiver to which messages from the data center will be delivered.
+   */
+  virtual void SetMessageReceiver(MessageCallback incoming_receiver) = 0;
+
+  /* Sets the receiver to which FCM registration token will be delivered.
+   */
+  virtual void SetTokenReceiver(TokenCallback incoming_receiver) = 0;
+};
+
+}  // namespace syncer
+
+#endif  // COMPONENTS_INVALIDATION_IMPL_NETWORK_CHANNEL_H_
