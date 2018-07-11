72f6c9ed148b6894f7f9f58c9219fa113a23bee2
Jeremy Roman <jbroman@chromium.org>
2018-07-11 15:19:33 +0000

    content: Remove web_frame_utils.*.
    They are unused.
    
    Change-Id: I5857cac266ddbe296188490817edc1aeb70a45d6
    Reviewed-on: https://chromium-review.googlesource.com/1132294
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574168}

--- content/renderer/render_frame_impl.cc
+++ content/renderer/render_frame_impl.cc
@@ -152,7 +152,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/skia_benchmarking_extension.h"
 #include "content/renderer/stats_collection_controller.h"
 #include "content/renderer/v8_value_converter_impl.h"
-#include "content/renderer/web_frame_utils.h"
 #include "content/renderer/web_ui_extension.h"
 #include "content/renderer/web_ui_extension_data.h"
 #include "crypto/sha2.h"
