09eb0fa3d75b2a009e21581a9c5b4ae3bdccc78e
Nicolas Pena <npm@chromium.org>
2018-07-11 19:06:30 +0000

    Fix oveflow in TimeDelta to Sample conversion in HistogramBase
    This CL changes the static_cast to base::saturated_cast in the AddTime*
    methods since the TimeDelta methods return int64_t whereas Sample is an
    int32_t.
    
    Change-Id: I54e98657c3d32201a0eb0fcd5208236e8b568d7b
    Reviewed-on: https://chromium-review.googlesource.com/1132152
    Commit-Queue: Nicolás Peña Moreno <npm@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574274}

--- base/metrics/histogram_base.cc
+++ base/metrics/histogram_base.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_samples.h"
 #include "base/metrics/sparse_histogram.h"
 #include "base/metrics/statistics_recorder.h"
+#include "base/numerics/safe_conversions.h"
 #include "base/pickle.h"
 #include "base/process/process_handle.h"
 #include "base/rand_util.h"
@@ -114,7 +115,7 @@ void HistogramBase::AddKiB(Sample value, int count) {
 }
 
 void HistogramBase::AddTimeMillisecondsGranularity(const TimeDelta& time) {
-  Add(static_cast<Sample>(time.InMilliseconds()));
+  Add(saturated_cast<Sample>(time.InMilliseconds()));
 }
 
 void HistogramBase::AddTimeMicrosecondsGranularity(const TimeDelta& time) {
@@ -122,7 +123,7 @@ void HistogramBase::AddTimeMicrosecondsGranularity(const TimeDelta& time) {
   // clocks. High-resolution metrics cannot make use of low-resolution data and
   // reporting it merely adds noise to the metric. https://crbug.com/807615#c16
   if (TimeTicks::IsHighResolution())
-    Add(static_cast<Sample>(time.InMicroseconds()));
+    Add(saturated_cast<Sample>(time.InMicroseconds()));
 }
 
 void HistogramBase::AddBoolean(bool value) {
