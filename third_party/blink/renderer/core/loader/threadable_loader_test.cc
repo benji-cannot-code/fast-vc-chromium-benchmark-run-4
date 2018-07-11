617f885362f641752c057df9553f2a852e4f2676
Hiroshige Hayashizaki <hiroshige@chromium.org>
2018-07-11 00:41:07 +0000

    Use non-null origin in threadable_loader_test
    In order to assert |WorkerGlobalScope::url_| is non-empty
    when referenced (which will be needed when Issue 861564 is
    applied for module workers),
    this CL removes cases where |url_| can be empty in unit tests.
    
    Bug: 861564
    Change-Id: I983f334ab866495a13a15e42805998359748b907
    Reviewed-on: https://chromium-review.googlesource.com/1128518
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573997}

--- third_party/blink/renderer/core/loader/threadable_loader_test.cc
+++ third_party/blink/renderer/core/loader/threadable_loader_test.cc
@@ -136,7 +136,7 @@ void SetUpRedirectURL() {
   response.SetHTTPStatusCode(301);
   response.SetLoadTiming(timing);
   response.AddHTTPHeaderField("Location", SuccessURL().GetString());
-  response.AddHTTPHeaderField("Access-Control-Allow-Origin", "null");
+  response.AddHTTPHeaderField("Access-Control-Allow-Origin", "http://fake.url");
 
   URLTestHelpers::RegisterMockedURLLoadWithCustomResponse(
       url, test::CoreTestDataPath(kFileName), response);
@@ -153,7 +153,7 @@ void SetUpRedirectLoopURL() {
   response.SetHTTPStatusCode(301);
   response.SetLoadTiming(timing);
   response.AddHTTPHeaderField("Location", RedirectLoopURL().GetString());
-  response.AddHTTPHeaderField("Access-Control-Allow-Origin", "null");
+  response.AddHTTPHeaderField("Access-Control-Allow-Origin", "http://fake.url");
 
   URLTestHelpers::RegisterMockedURLLoadWithCustomResponse(
       url, test::CoreTestDataPath(kFileName), response);
@@ -190,7 +190,11 @@ class ThreadableLoaderTestHelper {
 class DocumentThreadableLoaderTestHelper : public ThreadableLoaderTestHelper {
  public:
   DocumentThreadableLoaderTestHelper()
-      : dummy_page_holder_(DummyPageHolder::Create(IntSize(1, 1))) {}
+      : dummy_page_holder_(DummyPageHolder::Create(IntSize(1, 1))) {
+    GetDocument().SetURL(KURL("http://fake.url/"));
+    GetDocument().SetSecurityOrigin(
+        SecurityOrigin::Create(KURL("http://fake.url/")));
+  }
 
   void CreateLoader(ThreadableLoaderClient* client) override {
     ThreadableLoaderOptions options;
@@ -266,7 +270,9 @@ class WebWorkerFetchContextForTest : public WebWorkerFetchContext {
 class WorkerThreadableLoaderTestHelper : public ThreadableLoaderTestHelper {
  public:
   WorkerThreadableLoaderTestHelper()
-      : dummy_page_holder_(DummyPageHolder::Create(IntSize(1, 1))) {}
+      : dummy_page_holder_(DummyPageHolder::Create(IntSize(1, 1))) {
+    GetDocument().SetURL(KURL("http://fake.url/"));
+  }
 
   void CreateLoader(ThreadableLoaderClient* client) override {
     std::unique_ptr<WaitableEvent> completion_event =
@@ -774,7 +780,8 @@ TEST_P(ThreadableLoaderTest, DidFailAccessControlCheck) {
       DidFail(ResourceError::CancelledDueToAccessCheckError(
           SuccessURL(), ResourceRequestBlockedReason::kOther,
           "No 'Access-Control-Allow-Origin' header is present on the requested "
-          "resource. Origin 'null' is therefore not allowed access.")));
+          "resource. Origin 'http://fake.url' is therefore not allowed "
+          "access.")));
 
   StartLoader(SuccessURL(), network::mojom::FetchRequestMode::kCORS);
   CallCheckpoint(2);
