b3b6eedc66f87a9cab5f33246377bf65b4a938b7
Dominik Röttsches <drott@chromium.org>
2018-07-11 10:04:58 +0000

    Fix Mimehandler out-of-process FontConfig access
    r572930 moved FontConfig out of process access from a global file
    descriptor to Mojo messages. In one instance, PepperFlashFontFileHost,
    an IPC performing FontConfig method was replaced with one that tries to
    access FontConfig inside the sandboxed process, which fails. The
    mimehandler process needs access to FontConfig as well for providing
    fonts matching to PDFium.
    
    This CL fixes mimehandler out of process font access and moves back to
    using the MatchFontWithFallback FontConfig logic which was previously
    used in this file, before it was moved to using Skia font matching like
    on Windows. This implies that on Linux additional font fallback logic is
    used again, which finds replacements for non-existing fonts such as
    SimSun.
    
    Bug: 862051
    Change-Id: I67136d51b45f72db0ae74201353369e34885f2ef
    Reviewed-on: https://chromium-review.googlesource.com/1131938
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574119}

--- chrome/renderer/pepper/pepper_flash_font_file_host.h
+++ chrome/renderer/pepper/pepper_flash_font_file_host.h
@@ -15,8 +15,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/c/private/pp_private_font_charset.h"
 #include "ppapi/host/resource_host.h"
 
+#if defined(OS_LINUX) || defined(OS_OPENBSD)
+#include "base/files/file.h"
+#elif defined(OS_WIN)
 #include "third_party/skia/include/core/SkRefCnt.h"
 #include "third_party/skia/include/core/SkTypeface.h"
+#endif
 
 namespace content {
 class RendererPpapiHost;
@@ -47,7 +51,11 @@ class PepperFlashFontFileHost : public ppapi::host::ResourceHost {
                          uint32_t table);
   bool GetFontData(uint32_t table, void* buffer, size_t* length);
 
+#if defined(OS_LINUX)
+  base::File font_file_;
+#elif defined(OS_WIN)
   sk_sp<SkTypeface> typeface_;
+#endif
 
   DISALLOW_COPY_AND_ASSIGN(PepperFlashFontFileHost);
 };
