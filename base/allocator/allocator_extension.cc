64b4159e8d440d75646f3334ee8690df95befc66
Gabriel Marin <gmx@chromium.org>
2018-07-11 21:38:42 +0000

    tcmalloc: Move existing tcmalloc files under gperftools-2.0.
    Move existing tcmalloc files, both vendor and chromium, under gperftools-2.0
    in preparation for upreving tcmalloc to the most recent gperftools version.
    
    This CL did:
    
      # Create a gerftools-2.0 folder.
      mkdir gperftools-2.0
    
      # Move vendor/ and chromium/ under the new folder.
      git mv vendor gperftools-2.0/vendor
      git mv chromium gperftools-2.0/chromium
    
      # Update //base/allocator/BUILD.gn to have tcmalloc_dir point to
      # //third_party/tcmalloc/gperftools-2.0/chromium/src.
      # Update addr2line-pdb sources in tcmalloc/BUILD.gn.
      # Update include paths in files under //base/process and //base/allocator.
    
    This is old code that uses currently banned functions.
    NOPRESUBMIT=true
    
    BUG=724399,b:70905156
    
    Change-Id: I498a6edda48e178bbacfb41e30f75e9e96e336fc
    Reviewed-on: https://chromium-review.googlesource.com/1130670
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Gabriel Marin <gmx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574346}

--- base/allocator/allocator_extension.cc
+++ base/allocator/allocator_extension.cc
@@ -8,9 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 
 #if defined(USE_TCMALLOC)
-#include "third_party/tcmalloc/chromium/src/gperftools/heap-profiler.h"
-#include "third_party/tcmalloc/chromium/src/gperftools/malloc_extension.h"
-#include "third_party/tcmalloc/chromium/src/gperftools/malloc_hook.h"
+#include "third_party/tcmalloc/gperftools-2.0/chromium/src/gperftools/heap-profiler.h"
+#include "third_party/tcmalloc/gperftools-2.0/chromium/src/gperftools/malloc_extension.h"
+#include "third_party/tcmalloc/gperftools-2.0/chromium/src/gperftools/malloc_hook.h"
 #endif
 
 namespace base {
