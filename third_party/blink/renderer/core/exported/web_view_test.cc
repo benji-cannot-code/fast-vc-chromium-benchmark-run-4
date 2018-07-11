fe5c6101d8ce38c5c0b10483f2bea0ce49ab4374
danakj <danakj@chromium.org>
2018-07-11 22:11:25 +0000

    Fix the type of the end text selection handle
    Due to copy paste mistakes, the end handle ended up being the same type
    as the start handle, but it should be the opposite type.
    
    R=chrishtr@chromium.org
    
    Bug: 862510
    Change-Id: I2e6609d1e4083ced5f92b563925ba8c8401964fa
    Reviewed-on: https://chromium-review.googlesource.com/1133681
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574363}

--- third_party/blink/renderer/core/exported/web_view_test.cc
+++ third_party/blink/renderer/core/exported/web_view_test.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "build/build_config.h"
+#include "cc/trees/layer_tree_host.h"
 #include "gin/handle.h"
 #include "gin/object_template_builder.h"
 #include "gin/wrappable.h"
@@ -257,7 +258,11 @@ class WebViewTest : public testing::Test {
 
  protected:
   void SetViewportSize(const WebSize& size) {
-    web_view_helper_.SetViewportSize(size);
+    content::LayerTreeView* layer_tree_view =
+        web_view_helper_.GetLayerTreeView();
+    layer_tree_view->SetViewportSizeAndScale(
+        static_cast<gfx::Size>(size), /*device_scale_factor=*/1.f,
+        layer_tree_view->layer_tree_host()->local_surface_id_from_parent());
   }
 
   std::string RegisterMockedHttpURLLoad(const std::string& file_name) {
