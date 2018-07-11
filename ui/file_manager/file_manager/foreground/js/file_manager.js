82802039c1dd33930140c29acd234edf9f6ce208
Luciano Pacheco <lucmult@chromium.org>
2018-07-11 03:41:18 +0000

    Fix My Files to display its children on right hand side
    Also fix to be able to open Downloads from RHS.
    
    Add |name| attribute for crostini FakeEntry so it can display it on
    RHS.
    
    Add |file-name| attribute on FileTable rows to make it easier to select
    a single row on tests.
    
    Fix crash due to DCHECK on
    getFileTasksInternal::FileManagerPrivateInternalGetFileTasksFunction
    where it would fail with entries being empty when we send an array with
    only FilesAppEntry instances, because the API binding discards objects
    that aren't native Entry. So we remove non-native entries before
    calling |FileManagerPrivate.getFileTasks|.
    
    Change DirectoryContentScanner to not check for FakeEntry but instead
    check for the availability of the desired method |createReader| since
    FilesAppEntry also implements it even though they're non-native (aka
    FakeEntry). This fixes My Files to display its children on RHS.
    
    Change FileFilter to check for attributes existence (filesystem and
    fullPath) before operating on it. This fixes crostini FakeEntry to be
    able to be displayed on RHS.
    
    Change DirectoryModel to check for non-null VolumeInfo. This fixes
    this function running for My Files and crostini entries.
    
    Bug: 846587, 857335
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: Id3cfd1e957db57114c97de1d34a10494dcaaeedb
    Reviewed-on: https://chromium-review.googlesource.com/1130559
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Sasha Morrissey <sashab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574060}

--- ui/file_manager/file_manager/foreground/js/file_manager.js
+++ ui/file_manager/file_manager/foreground/js/file_manager.js
@@ -1237,6 +1237,7 @@ FileManager.prototype = /** @struct */ {
               str('LINUX_FILES_ROOT_LABEL'), NavigationModelItemType.CROSTINI, {
                 isDirectory: true,
                 rootType: VolumeManagerCommon.RootType.CROSTINI,
+                name: str('LINUX_FILES_ROOT_LABEL'),
                 toURL: function() {
                   return 'fake-entry://linux-files';
                 },
