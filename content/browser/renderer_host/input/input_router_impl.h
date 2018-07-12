323a96cf18bdd59d010e6e4d24aa66f5606746ac
Ehsan Karamad <ekaramad@chromium.org>
2018-07-12 20:25:25 +0000

    Reset listener properties after provisional load
    When provisional load is committed in FrameLoader and the corresponding
    frame is a LocalRoot, the EventListenerProperties are reset to none for
    touch and wheel listeners. This update is then propagate through render
    widget and LayterTreeHost to the browser and the CC correspondingly.
    
    However, if the local root is provisional the call is dropped in
    ChromeClientImpl. This would include all cross-process navigations which
    end up in a provisional frame.
    
    This change will make sure the initial updates for
    EventListenerProperties for a local root are sent after
    WebLocalFrameClient is notified about committing provisional load.
    
    Bug: 850994
    Change-Id: I686ab51277bf675d93bd744d6d0208977ac06b5c
    Reviewed-on: https://chromium-review.googlesource.com/1126329
    Commit-Queue: Ehsan Karamad <ekaramad@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574701}

--- content/browser/renderer_host/input/input_router_impl.h
+++ content/browser/renderer_host/input/input_router_impl.h
@@ -114,6 +114,7 @@ class CONTENT_EXPORT InputRouterImpl : public InputRouter,
  private:
   friend class InputRouterImplTest;
   friend class MockRenderWidgetHost;
+  friend class RenderWidgetHostBrowserTest;
 
   // Keeps track of last position of touch points and sets MovementXY for them.
   void SetMovementXYForTouchPoints(blink::WebTouchEvent* event);
