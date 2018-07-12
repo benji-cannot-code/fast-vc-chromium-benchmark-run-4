3a346cad71e6f55d52660856df8b3f35576bec3d
Alexandr Ilin <alexilin@chromium.org>
2018-07-12 17:08:22 +0000

    Allow WritableSharedMemoryRegion to Unsafe conversion
    This CL adds a new ConvertToUnsafe() method to the WritableSharedMemoryRegion
    class. This new method is similar to ConvertToReadOnly() in structure. The
    conversion does not modify an underlying platform handle (except it closes a
    read-only handle on POSIX) but changes region's type.
    
    Bug: 795291
    Change-Id: Ie0a6b4c9850c0e223ee9299655f9bfd6c79c383b
    Reviewed-on: https://chromium-review.googlesource.com/985981
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Alexandr Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574610}

--- base/memory/writable_shared_memory_region.h
+++ base/memory/writable_shared_memory_region.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/platform_shared_memory_region.h"
 #include "base/memory/read_only_shared_memory_region.h"
 #include "base/memory/shared_memory_mapping.h"
+#include "base/memory/unsafe_shared_memory_region.h"
 
 namespace base {
 
@@ -49,6 +50,11 @@ class BASE_EXPORT WritableSharedMemoryRegion {
   // Makes the region read-only. No new writable mappings of the region can be
   // created after this call. Returns an invalid region on failure.
   static ReadOnlySharedMemoryRegion ConvertToReadOnly(
+      WritableSharedMemoryRegion region);
+
+  // Makes the region unsafe. The region cannot be converted to read-only after
+  // this call. Returns an invalid region on failure.
+  static UnsafeSharedMemoryRegion ConvertToUnsafe(
       WritableSharedMemoryRegion region);
 
   // Default constructor initializes an invalid instance.
