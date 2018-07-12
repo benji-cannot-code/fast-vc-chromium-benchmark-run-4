667bf01cd2abb995dbf1ff8458982023df32360d
Tetsui Ohkubo <tetsui@chromium.org>
2018-07-12 02:06:55 +0000

    Unified: Get default focus on detailed view.
    In UnifiedSystemTray, detailed view did not get focus when it is
    transitioned by keyboard.
    
    TEST=manual
    BUG=862504
    
    Change-Id: I9f39359d3fa72be4bff5d42e5a9f98144b1a1bad
    Reviewed-on: https://chromium-review.googlesource.com/1132899
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574461}

--- ash/system/tray/tray_detailed_view.h
+++ ash/system/tray/tray_detailed_view.h
@@ -53,6 +53,7 @@ class ASH_EXPORT TrayDetailedView : public views::View,
   // views::View:
   void Layout() override;
   int GetHeightForWidth(int width) const override;
+  void RequestFocus() override;
 
   // Exposes the layout manager of this view to give control to subclasses.
   views::BoxLayout* box_layout() { return box_layout_; }
--- ash/system/unified/unified_system_tray_view.cc
+++ ash/system/unified/unified_system_tray_view.cc
@@ -200,6 +200,8 @@ void UnifiedSystemTrayView::SetDetailedView(views::View* detailed_view) {
   detailed_view_container_->SetPreferredSize(system_tray_size);
   detailed_view->InvalidateLayout();
   Layout();
+
+  detailed_view->RequestFocus();
 }
 
 void UnifiedSystemTrayView::ResetDetailedView() {
