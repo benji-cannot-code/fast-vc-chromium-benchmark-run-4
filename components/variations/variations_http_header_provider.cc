86ff8b8b2791c8fbeb04864b84745eddff050fcd
Jun Cai <juncai@chromium.org>
2018-07-12 16:52:40 +0000

    Network Service: Document how adding/removing http headers for web content requests are implemented
    This is a follow-up CL per comments at:
    https://chromium-review.googlesource.com/c/chromium/src/+/1120955
    
    This CL adds comments to //components/variations/variations_http_header_provider.cc
    to document how adding/removing http headers for web content requests are
    implemented differently when Network Service is enabled or not enabled.
    
    Bug: 841309
    Change-Id: I6b1e7ac776415131f6f549253f8a69a4d0992117
    Reviewed-on: https://chromium-review.googlesource.com/1134408
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Jun Cai <juncai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574602}

--- components/variations/variations_http_header_provider.cc
+++ components/variations/variations_http_header_provider.cc
@@ -22,6 +22,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace variations {
 
+// The following documents how adding/removing http headers for web content
+// requests are implemented when Network Service is enabled or not enabled.
+//
+// When Network Service is not enabled, adding headers is implemented in
+// ChromeResourceDispatcherHostDelegate::RequestBeginning() by calling
+// variations::AppendVariationHeaders(), and removing headers is implemented in
+// ChromeNetworkDelegate::OnBeforeRedirect() by calling
+// variations::StripVariationHeaderIfNeeded().
+//
+// When Network Service is enabled, adding/removing headers is implemented by
+// request consumers, and how it is implemented depends on the request type.
+// There are three cases:
+// 1. Subresources request in renderer, it is implemented
+// in URLLoaderThrottleProviderImpl::CreateThrottles() by adding a
+// VariationsHeaderURLLoaderThrottle to a content::URLLoaderThrottle vector.
+// 2. Navigations/Downloads request in browser, it is implemented in
+// ChromeContentBrowserClient::CreateURLLoaderThrottles() by also adding a
+// VariationsHeaderURLLoaderThrottle to a content::URLLoaderThrottle vector.
+// 3. SimpleURLLoader in browser, it is implemented in a SimpleURLLoader wrapper
+// function variations::CreateSimpleURLLoaderWithVariationsHeaders().
+
 // static
 VariationsHttpHeaderProvider* VariationsHttpHeaderProvider::GetInstance() {
   return base::Singleton<VariationsHttpHeaderProvider>::get();
