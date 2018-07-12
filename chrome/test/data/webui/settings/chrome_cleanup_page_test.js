72b3baf8007419ba6dc4db0d425e42b6c46e1a5a
Nick Burris <nburris@google.com>
2018-07-12 16:33:17 +0000

    Settings WebUI: Fix extensions url on cleanup result page
    The cleaner success string now has HTML in it, and the JS i18n
    function strips it off. i18nAdvanced allows us to let the <a> through to
    the string, but the way the cleanup result page works is it simply sets
    the card text variable ("title") to the result message string. This
    would result in the raw string (with HTML) getting displayed, so the
    title variable needs to also be set to be the inner-h-t-m-l.
    
    Bug: 853339
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: I8ea98e9868f83fc14a674841072cc2915c7b4a48
    Reviewed-on: https://chromium-review.googlesource.com/1134191
    Commit-Queue: Nick Burris <nburris@google.com>
    Reviewed-by: proberge <proberge@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574594}

--- chrome/test/data/webui/settings/chrome_cleanup_page_test.js
+++ chrome/test/data/webui/settings/chrome_cleanup_page_test.js
@@ -438,6 +438,10 @@ suite('ChromeCleanupHandler', function() {
 
     const actionButton = chromeCleanupPage.$$('#action-button');
     assertFalse(!!actionButton);
+
+    const title = chromeCleanupPage.$$('#status-title');
+    assertTrue(!!title);
+    assertTrue(!!title.querySelector('a'));
   });
 
   test('logsUploadingOnScanOffered', function() {
