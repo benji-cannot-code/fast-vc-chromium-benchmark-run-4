768a0e45b16f92028207e10c7bfecd272c89f662
Lei Zhang <thestig@chromium.org>
2018-07-12 05:29:49 +0000

    Remove debugging code in base::MessagePumpWin.
    BUG=596190
    
    Change-Id: Ie9b6ee2cc1db8399247be767cabba1b39df50544
    Reviewed-on: https://chromium-review.googlesource.com/1128274
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574491}

--- base/message_loop/message_pump_win.cc
+++ base/message_loop/message_pump_win.cc
@@ -47,10 +47,6 @@ void MessagePumpWin::Run(Delegate* delegate) {
   s.should_quit = false;
   s.run_depth = state_ ? state_->run_depth + 1 : 1;
 
-  // TODO(stanisc): crbug.com/596190: Remove this code once the bug is fixed.
-  s.schedule_work_error_count = 0;
-  s.last_schedule_work_error_time = Time();
-
   RunState* previous_state = state_;
   state_ = &s;
 
@@ -119,8 +115,6 @@ void MessagePumpForUI::ScheduleWork() {
   InterlockedExchange(&work_state_, READY);
   UMA_HISTOGRAM_ENUMERATION("Chrome.MessageLoopProblem", MESSAGE_POST_ERROR,
                             MESSAGE_LOOP_PROBLEM_MAX);
-  state_->schedule_work_error_count++;
-  state_->last_schedule_work_error_time = Time::Now();
 }
 
 void MessagePumpForUI::ScheduleDelayedWork(const TimeTicks& delayed_work_time) {
@@ -445,8 +439,6 @@ void MessagePumpForIO::ScheduleWork() {
   InterlockedExchange(&work_state_, READY);  // Clarify that we didn't succeed.
   UMA_HISTOGRAM_ENUMERATION("Chrome.MessageLoopProblem", COMPLETION_POST_ERROR,
                             MESSAGE_LOOP_PROBLEM_MAX);
-  state_->schedule_work_error_count++;
-  state_->last_schedule_work_error_time = Time::Now();
 }
 
 void MessagePumpForIO::ScheduleDelayedWork(const TimeTicks& delayed_work_time) {
