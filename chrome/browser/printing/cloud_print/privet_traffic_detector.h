ff90347fec3d573a8bb6b1a4843231057fe4f2fa
Robbie McElrath <rmcelrath@chromium.org>
2018-07-12 20:20:54 +0000

    Migrate PrivetTrafficDetector to use NetworkConnectionTracker.
    This migrates PrivetTrafficDetector from using
    net::NetworkChangeNotifier to content::NetworkConnectionTracker, which
    works with the network service enabled.
    
    Bug: 859134
    Change-Id: I06f2ecbe142bbed9a8772f69ab109b0c0dc32581
    Reviewed-on: https://chromium-review.googlesource.com/1123681
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574700}

--- chrome/browser/printing/cloud_print/privet_traffic_detector.h
+++ chrome/browser/printing/cloud_print/privet_traffic_detector.h
@@ -12,9 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/cancelable_callback.h"
 #include "base/macros.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/browser/network_connection_tracker.h"
 #include "net/base/address_family.h"
 #include "net/base/ip_endpoint.h"
-#include "net/base/network_change_notifier.h"
 
 namespace net {
 class DatagramServerSocket;
@@ -29,8 +29,9 @@ namespace cloud_print {
 // When traffic is detected, class fires callback and shutdowns itself.
 class PrivetTrafficDetector
     : public base::RefCountedThreadSafe<
-          PrivetTrafficDetector, content::BrowserThread::DeleteOnIOThread>,
-      private net::NetworkChangeNotifier::NetworkChangeObserver {
+          PrivetTrafficDetector,
+          content::BrowserThread::DeleteOnIOThread>,
+      private content::NetworkConnectionTracker::NetworkConnectionObserver {
  public:
   PrivetTrafficDetector(net::AddressFamily address_family,
                         const base::Closure& on_traffic_detected);
@@ -43,9 +44,8 @@ class PrivetTrafficDetector
   friend class base::DeleteHelper<PrivetTrafficDetector>;
   ~PrivetTrafficDetector() override;
 
-    // net::NetworkChangeNotifier::NetworkChangeObserver implementation.
-  void OnNetworkChanged(
-      net::NetworkChangeNotifier::ConnectionType type) override;
+  // content::NetworkConnectionTracker::NetworkConnectionObserver:
+  void OnConnectionChanged(network::mojom::ConnectionType type) override;
 
   void StartOnIOThread();
   void ScheduleRestart();
