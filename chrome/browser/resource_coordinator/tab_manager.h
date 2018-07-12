1495a4121c4c473ba39e2afbc85142543730166a
Francois Doray <fdoray@chromium.org>
2018-07-12 21:11:50 +0000

    RC: Use Chrome usage time since hidden to decide if tab can be proactively discarded.
    Previously, a tab was proactively discarded when it had spent T time hidden,
    where T depends on the number of non-discarded tabs and the amount of
    physical memory. Unfortunately, that meant that if a user kept Chrome
    open during the night without touching it, all hidden tabs were discarded
    in the morning.
    
    With this CL, we use the "Chrome usage time since hidden" to determine
    if a tab should be proactively discarded. That will prevent the unwanted
    behavior of finding all tabs discarded after a long period of inactivity.
    
    Bug: 775644
    Change-Id: Ie45ddfc07df782adeb32e3334a97bacaec429d45
    Reviewed-on: https://chromium-review.googlesource.com/1130470
    Commit-Queue: François Doray <fdoray@chromium.org>
    Reviewed-by: Sébastien Marchand <sebmarchand@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574720}

--- chrome/browser/resource_coordinator/tab_manager.h
+++ chrome/browser/resource_coordinator/tab_manager.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string16.h"
 #include "base/timer/timer.h"
 #include "build/build_config.h"
+#include "chrome/browser/metrics/desktop_session_duration/desktop_session_duration_tracker.h"
 #include "chrome/browser/resource_coordinator/discard_reason.h"
 #include "chrome/browser/resource_coordinator/intervention_policy_database.h"
 #include "chrome/browser/resource_coordinator/lifecycle_unit.h"
@@ -83,7 +84,8 @@ class TabManagerStatsCollector;
 class TabManager : public LifecycleUnitObserver,
                    public LifecycleUnitSourceObserver,
                    public TabLoadTracker::Observer,
-                   public TabStripModelObserver {
+                   public TabStripModelObserver,
+                   public metrics::DesktopSessionDurationTracker::Observer {
  public:
   // Forward declaration of resource coordinator signal observer.
   class ResourceCoordinatorSignalObserver;
@@ -310,7 +312,7 @@ class TabManager : public LifecycleUnitObserver,
   void OnMemoryPressure(
       base::MemoryPressureListener::MemoryPressureLevel memory_pressure_level);
 
-  // TabStripModelObserver overrides.
+  // TabStripModelObserver:
   void ActiveTabChanged(content::WebContents* old_contents,
                         content::WebContents* new_contents,
                         int index,
@@ -324,7 +326,7 @@ class TabManager : public LifecycleUnitObserver,
                      content::WebContents* new_contents,
                      int index) override;
 
-  // TabLoadTracker::Observer implementation:
+  // TabLoadTracker::Observer:
   void OnStartTracking(content::WebContents* web_contents,
                        LoadingState loading_state) override;
   void OnLoadingStateChange(content::WebContents* web_contents,
@@ -332,6 +334,9 @@ class TabManager : public LifecycleUnitObserver,
                             LoadingState new_loading_state) override;
   void OnStopTracking(content::WebContents* web_contents,
                       LoadingState loading_state) override;
+
+  // DesktopSessionDurationTracker::Observer:
+  void OnSessionStarted(base::TimeTicks session_start) override;
 
   // Returns the WebContentsData associated with |contents|. Also takes care of
   // creating one if needed.
