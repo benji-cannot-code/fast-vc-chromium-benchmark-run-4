aebeddcdc09718a8099902b1a4bcecf635b60669
Scott Violet <sky@chromium.org>
2018-07-12 01:13:35 +0000

    chromeos: fixs bugs where top-levels were never deleted
    Requests from a client to delete a top-level where not actually
    deleting the aura::Window.
    
    This also makes printing the window hierarchy include the remote id of the
    window, which is useful for debugging.
    
    BUG=none
    TEST=covered by tests
    
    Change-Id: Icd3eae0839558d6db277bfeaf5fd3b7dd2fde377
    Reviewed-on: https://chromium-review.googlesource.com/1134510
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574440}

--- services/ui/ws2/server_window.h
+++ services/ui/ws2/server_window.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SERVICES_UI_WS2_SERVER_WINDOW_H_
 #define SERVICES_UI_WS2_SERVER_WINDOW_H_
 
+#include <string>
 #include <vector>
 
 #include "base/component_export.h"
@@ -118,6 +119,10 @@ class COMPONENT_EXPORT(WINDOW_SERVICE) ServerWindow {
   }
   void SetDragDropDelegate(
       std::unique_ptr<DragDropDelegate> drag_drop_delegate);
+
+  // Returns an id useful for debugging. This returns the id from the client
+  // that created the window, otherwise |frame_sink_id_|.
+  std::string GetIdForDebugging();
 
  private:
   friend class ServerWindowTestHelper;
--- services/ui/ws2/window_service.h
+++ services/ui/ws2/window_service.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include <set>
+#include <string>
 
 #include "base/component_export.h"
 #include "base/macros.h"
@@ -78,7 +79,7 @@ class COMPONENT_EXPORT(WINDOW_SERVICE) WindowService
                                 int max_title_bar_button_width);
 
   // Whether |window| hosts a remote client.
-  static bool HasRemoteClient(aura::Window* window);
+  static bool HasRemoteClient(const aura::Window* window);
 
   WindowServiceDelegate* delegate() { return delegate_; }
 
@@ -96,6 +97,10 @@ class COMPONENT_EXPORT(WINDOW_SERVICE) WindowService
   // Asks the client that created |window| to close |window|. |window| must be
   // a top-level window.
   void RequestClose(aura::Window* window);
+
+  // Returns an id useful for debugging. See ServerWindow::GetIdForDebugging()
+  // for details.
+  std::string GetIdForDebugging(aura::Window* window);
 
   // service_manager::Service:
   void OnStart() override;
