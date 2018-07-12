73444147b2425e377c581ac148402c94f7bfd556
dpapad <dpapad@chromium.org>
2018-07-12 23:41:15 +0000

    Settings WebUI: Fix focus when exiting syncSetup subpage.
    This was throwing a runtime error when the unified-consent flag was
    enabled.
    
    Bug: 860854
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: I6e521b813ca160e5434ac066e47f73ecf61b7ba1
    Reviewed-on: https://chromium-review.googlesource.com/1134515
    Reviewed-by: Scott Chen <scottchen@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574774}

--- chrome/browser/resources/settings/people_page/people_page.js
+++ chrome/browser/resources/settings/people_page/people_page.js
@@ -138,8 +138,10 @@ Polymer({
       value: function() {
         const map = new Map();
         if (settings.routes.SYNC) {
-          map.set(
-              settings.routes.SYNC.path, '#sync-status .subpage-arrow button');
+          const syncId = loadTimeData.getBoolean('unifiedConsentEnabled') ?
+              '#sync-setup' :
+              '#sync-status';
+          map.set(settings.routes.SYNC.path, `${syncId} .subpage-arrow button`);
         }
         if (settings.routes.MANAGE_PASSWORDS) {
           map.set(
