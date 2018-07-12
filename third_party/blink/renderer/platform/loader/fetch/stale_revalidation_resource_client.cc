90e49f36bc1aa3cf1b3e52837d44c06c1381505f
Dave Tapuska <dtapuska@chromium.org>
2018-07-12 15:15:56 +0000

    Add Blink.ResourceFetcher.StaleWhileRevalidateDuration histogram.
    This histogram tracks the duration of completed stale revalidation
    attempts. This value will be used as a rough approximation of
    the benefit received by using Stale While Revalidate.
    
    BUG=348877
    
    Change-Id: I47601e667394c401ccdc96afc4d73d6c464faa3b
    Reviewed-on: https://chromium-review.googlesource.com/1133961
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574564}

--- third_party/blink/renderer/platform/loader/fetch/stale_revalidation_resource_client.cc
+++ third_party/blink/renderer/platform/loader/fetch/stale_revalidation_resource_client.cc
@@ -5,13 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/platform/loader/fetch/stale_revalidation_resource_client.h"
 
+#include "base/metrics/histogram_macros.h"
 #include "third_party/blink/renderer/platform/loader/fetch/memory_cache.h"
 
 namespace blink {
 
 StaleRevalidationResourceClient::StaleRevalidationResourceClient(
     Resource* stale_resource)
-    : stale_resource_(stale_resource) {}
+    : start_time_(CurrentTimeTicks()), stale_resource_(stale_resource) {}
 
 StaleRevalidationResourceClient::~StaleRevalidationResourceClient() = default;
 
@@ -20,6 +21,13 @@ void StaleRevalidationResourceClient::NotifyFinished(Resource* resource) {
   if (stale_resource_ && IsMainThread())
     GetMemoryCache()->Remove(stale_resource_);
   ClearResource();
+
+  TimeTicks finish_time = resource->LoadFinishTime();
+  if (!finish_time.is_null()) {
+    UMA_HISTOGRAM_LONG_TIMES(
+        "Blink.ResourceFetcher.StaleWhileRevalidateDuration",
+        finish_time - start_time_);
+  }
 }
 
 void StaleRevalidationResourceClient::Trace(blink::Visitor* visitor) {
