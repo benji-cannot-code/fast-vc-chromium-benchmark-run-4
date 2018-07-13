76d95e585f7a4440bd2443ae5024b537fbfaa2f5
Tiger Oakes <tigero@google.com>
2018-07-13 17:52:40 +0000

    Added processor for @UiThread & @WorkerThread annotations
    When a method has the @UiThread annotation, a call to
    ThreadUtils.assertOnUiThread will be injected at the start of the method.
    Similarily, @WorkerThread adds a call to ThreadUtils.assertOnBackgroundThread.
    This allows for easily asserting that a method can only be used on the UI or
    worker threads.
    
    Bug: 842695
    Change-Id: Ic53b5d06d6b0581a81f1dcce72b46e7ab42313c0
    Reviewed-on: https://chromium-review.googlesource.com/1070592
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: agrieve <agrieve@chromium.org>
    Reviewed-by: Eric Stevenson <estevenson@chromium.org>
    Commit-Queue: Tiger Oakes <tigero@google.com>
    Cr-Commit-Position: refs/heads/master@{#574971}

--- build/android/bytecode/java/org/chromium/bytecode/ByteCodeProcessor.java
+++ build/android/bytecode/java/org/chromium/bytecode/ByteCodeProcessor.java
@@ -61,7 +61,8 @@ class ByteCodeProcessor {
     }
 
     private static void process(String inputJarPath, String outputJarPath, boolean shouldAssert,
-            boolean shouldUseCustomResources, ClassLoader classPathJarsClassLoader) {
+            boolean shouldUseCustomResources, boolean shouldUseThreadAnnotations,
+            ClassLoader classPathJarsClassLoader) {
         String tempJarPath = outputJarPath + TEMPORARY_FILE_SUFFIX;
         try (ZipInputStream inputStream = new ZipInputStream(
                      new BufferedInputStream(new FileInputStream(inputJarPath)));
@@ -101,6 +102,9 @@ class ByteCodeProcessor {
                        "asm-util-5.0.1.jar:out/Debug/lib.java/jar_containing_yourclass.jar" \
                        org.objectweb.asm.util.ASMifier org.package.YourClassName
                 */
+                if (shouldUseThreadAnnotations) {
+                    chain = new ThreadAssertionClassAdapter(chain);
+                }
                 if (shouldAssert) {
                     chain = new AssertionEnablerClassAdapter(chain);
                 }
@@ -156,6 +160,7 @@ class ByteCodeProcessor {
         String outputJarPath = args[1];
         boolean shouldAssert = args[2].equals("--enable-assert");
         boolean shouldUseCustomResources = args[3].equals("--enable-custom-resources");
+        boolean shouldUseThreadAnnotations = args[4].equals("--enable-thread-annotations");
 
         // Load all jars that are on the classpath for the input jar for analyzing class hierarchy.
         ClassLoader classPathJarsClassLoader = null;
@@ -166,6 +171,6 @@ class ByteCodeProcessor {
             classPathJarsClassLoader = loadJars(classPathJarsPaths);
         }
         process(inputJarPath, outputJarPath, shouldAssert, shouldUseCustomResources,
-                classPathJarsClassLoader);
+                shouldUseThreadAnnotations, classPathJarsClassLoader);
     }
 }
