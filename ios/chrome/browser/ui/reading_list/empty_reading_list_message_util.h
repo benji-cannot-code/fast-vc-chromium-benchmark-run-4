bb1a11cd0352dc505a0d058af8deb7b17c90c482
Kurt Horimoto <kkhorimoto@chromium.org>
2018-07-13 19:28:00 +0000

    [iOS] Move Reading List attributed text logic to utility functions.
    This will allow the icon insertion logic to be shared across different
    implementations of the Reading List empty state.
    
    Bug: 851962
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ibd4abe71be34ae019786620ba88ba6f226b75456
    Reviewed-on: https://chromium-review.googlesource.com/1135983
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575017}

---
+++ ios/chrome/browser/ui/reading_list/empty_reading_list_message_util.h
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_READING_LIST_EMPTY_READING_LIST_MESSAGE_UTIL_H_
+#define IOS_CHROME_BROWSER_UI_READING_LIST_EMPTY_READING_LIST_MESSAGE_UTIL_H_
+
+#import <UIKit/UIKit.h>
+
+// Returns the attributed message to use for the empty Reading List background
+// view.
+NSAttributedString* GetReadingListEmptyMessage();
+
+// Returns the accessibility label to use for the label displaying the text
+// returned by GetReadingListEmptyMessage().
+NSString* GetReadingListEmptyMessageA11yLabel();
+
+#endif  // IOS_CHROME_BROWSER_UI_READING_LIST_EMPTY_READING_LIST_MESSAGE_UTIL_H_
