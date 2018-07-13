eadcf7619fbc6560b38f9374808198b5219f67c8
Kim Paulhamus <kpaulhamus@chromium.org>
2018-07-13 16:35:21 +0000

    Disable flaky LifecycleTest (Exe_TerminateProcess) on Fuchsia
    TBR: jam@chromium.org
    Bug: 863166
    Change-Id: I9e31154c0ba8cdc5fff7af5130c8a06029a4bdaa
    Reviewed-on: https://chromium-review.googlesource.com/1136029
    Commit-Queue: Kim Paulhamus <kpaulhamus@chromium.org>
    Reviewed-by: Kim Paulhamus <kpaulhamus@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574942}

--- services/service_manager/tests/lifecycle/lifecycle_unittest.cc
+++ services/service_manager/tests/lifecycle/lifecycle_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/process/process.h"
 #include "base/run_loop.h"
+#include "build/build_config.h"
 #include "mojo/public/cpp/bindings/binding.h"
 #include "services/service_manager/public/cpp/identity.h"
 #include "services/service_manager/public/cpp/service_test.h"
@@ -417,7 +418,12 @@ TEST_F(LifecycleTest, Exe_GracefulQuit) {
   process.Terminate(9, true);
 }
 
-TEST_F(LifecycleTest, Exe_TerminateProcess) {
+#if defined(OS_FUCHSIA)
+#define MAYBE_Exe_TerminateProcess DISABLED_Exe_TerminateProcess
+#else
+#define MAYBE_Exe_TerminateProcess Exe_TerminateProcess
+#endif
+TEST_F(LifecycleTest, MAYBE_Exe_TerminateProcess) {
   base::Process process = LaunchProcess();
 
   test::mojom::LifecycleControlPtr lifecycle = ConnectTo(kTestExeName);
