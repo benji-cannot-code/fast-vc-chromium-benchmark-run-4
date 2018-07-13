de6bcbe4b55017a94f14bc7376a8208033e52ea4
Scott Chen <scottchen@chromium.org>
2018-07-13 21:34:04 +0000

    Settings: fix issue with add-language dialog check-boxes lose focus.
    <iron-list> introduced a bug that causes a focused item to lose focus
    when the user scrolls away then tab back. A fix was landed in the iron-list
    repository but a new version had not been released yet, so this CL patches
    the fix locally first. The local patch should be removed later once
    iron-list releases the new version and is patched into our code-base.
    
    Bug: 854571
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: I5b70ce4188b882839e7a322270847fde85690011
    Reviewed-on: https://chromium-review.googlesource.com/1135652
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Scott Chen <scottchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575077}

--- third_party/polymer/v1_0/components-chromium/iron-list/iron-list-extracted.js
+++ third_party/polymer/v1_0/components-chromium/iron-list/iron-list-extracted.js
@@ -1552,7 +1552,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       // Assign models to the focused index.
       this._assignModels();
       // Get the new physical index for the focused index.
-      var fpidx = this._focusedPhysicalIndex;
+      // TODO(scottchen): remove this patch after rolling past the fix for
+      // this issue: https://github.com/PolymerElements/iron-list/issues/537.
+      var fpidx = this._focusedPhysicalIndex = this._getPhysicalIndex(this._focusedVirtualIndex);
 
       var onScreenItem = this._physicalItems[fpidx];
       if (!onScreenItem) {
