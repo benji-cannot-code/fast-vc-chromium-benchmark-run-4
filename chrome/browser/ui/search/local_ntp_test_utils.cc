78586f083e08e284102c896743dc442c8517ed17
Mathieu Perreault <mathp@chromium.org>
2018-07-13 13:25:58 +0000

    [New Tab] Create a render browsertest for the local NTP
    Currently in its own target, and simply dumps the output to a file without
    checking
    
    Bug: 863174
    Test: out/Default/ntp_render_browsertests
    Change-Id: I61ca7714005c3b1d40f354d9f2ca519750843108
    Reviewed-on: https://chromium-review.googlesource.com/1135756
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Commit-Queue: Mathieu Perreault <mathp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574897}

--- chrome/browser/ui/search/local_ntp_test_utils.cc
+++ chrome/browser/ui/search/local_ntp_test_utils.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/search/local_ntp_test_utils.h"
 
+#include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/threading/thread_restrictions.h"
 #include "chrome/browser/browser_process.h"
@@ -37,7 +38,7 @@ content::WebContents* OpenNewTab(Browser* browser, const GURL& url) {
   return browser->tab_strip_model()->GetActiveWebContents();
 }
 
-void NavigateToNTPAndWaitUntilLoaded(Browser* browser) {
+void NavigateToNTPAndWaitUntilLoaded(Browser* browser, int delay) {
   content::WebContents* active_tab =
       browser->tab_strip_model()->GetActiveWebContents();
 
@@ -59,21 +60,22 @@ void NavigateToNTPAndWaitUntilLoaded(Browser* browser) {
   // important that these two things happen in the same JS invocation, since
   // otherwise we might miss the message.
   bool mv_tiles_loaded = false;
-  ASSERT_TRUE(instant_test_utils::GetBoolFromJS(active_tab,
-                                                R"js(
+  ASSERT_TRUE(instant_test_utils::GetBoolFromJS(
+      active_tab, base::StringPrintf(R"js(
       (function() {
         if (tilesAreLoaded) {
           return true;
         }
         window.addEventListener('message', function(event) {
           if (event.data.cmd == 'loaded') {
-            domAutomationController.send('NavigateToNTPAndWaitUntilLoaded');
+            setTimeout(() => {
+             domAutomationController.send('NavigateToNTPAndWaitUntilLoaded');
+            }, %d);
           }
         });
         return false;
       })()
-                                                )js",
-                                                &mv_tiles_loaded));
+      )js", delay), &mv_tiles_loaded));
 
   std::string message;
   // Get rid of the message that the GetBoolFromJS call produces.
