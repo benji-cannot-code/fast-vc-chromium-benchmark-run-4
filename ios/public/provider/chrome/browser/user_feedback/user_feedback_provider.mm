096ee3e68ed7a5402bb8b7673011d2f7a4c00fad
Peter K. Lee <pkl@chromium.org>
2018-07-13 19:07:13 +0000

    Removes 2-param CreateViewController from Provider API (5/5)
    Finally, the last step of removing from UserFeedback Provider API.
    Submit this after all other downstream CLs have been submitted.
    
    Bug: 847329
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I1c8fb9fdf5e9b3f65e959577a563e9f7311d93e6
    Reviewed-on: https://chromium-review.googlesource.com/1132542
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Peter Lee <pkl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575006}

--- ios/public/provider/chrome/browser/user_feedback/user_feedback_provider.mm
+++ ios/public/provider/chrome/browser/user_feedback/user_feedback_provider.mm
@@ -22,10 +22,4 @@ UIViewController* UserFeedbackProvider::CreateViewController(
   return nil;
 }
 
-UIViewController* UserFeedbackProvider::CreateViewController(
-    id<UserFeedbackDataSource> dataSource,
-    id<ApplicationCommands> dispatcher) {
-  return nil;
-}
-
 void UserFeedbackProvider::Synchronize() {}
