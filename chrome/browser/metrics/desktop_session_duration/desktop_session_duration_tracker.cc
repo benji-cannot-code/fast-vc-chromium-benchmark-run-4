9c9a13eb10609b35b0a4b331fd3bac7968ea7071
Francois Doray <fdoray@chromium.org>
2018-07-13 19:36:43 +0000

    Prevent double end session in DesktopSessionDurationTracker.
    Previously, this scenario could cause the same session to end twice:
     - Chrome becomes visible.
     - User event -> Session starts.
     - Chrome becomes hidden -> Session ends.
     - Inactivity timeout expires -> Session ends again.
    
    This CL fixes the issue by stopping the inactivity timer when a
    session ends.
    
    Bug: 863243
    Change-Id: I3d903844ab07775ebc5d653f34fa6b91a3623021
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Reviewed-on: https://chromium-review.googlesource.com/1136725
    Reviewed-by: Robert Kaplow (slow) <rkaplow@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575021}

--- chrome/browser/metrics/desktop_session_duration/desktop_session_duration_tracker.cc
+++ chrome/browser/metrics/desktop_session_duration/desktop_session_duration_tracker.cc
@@ -130,6 +130,7 @@ void DesktopSessionDurationTracker::OnTimerFired() {
 }
 
 void DesktopSessionDurationTracker::StartSession() {
+  DCHECK(!in_session_);
   in_session_ = true;
   is_first_session_ = false;
   session_start_ = base::TimeTicks::Now();
@@ -141,7 +142,12 @@ void DesktopSessionDurationTracker::StartSession() {
 
 void DesktopSessionDurationTracker::EndSession(
     base::TimeDelta time_to_discount) {
+  DCHECK(in_session_);
   in_session_ = false;
+
+  // Cancel the inactivity timer, to prevent the session from ending a second
+  // time when it expires.
+  timer_.Stop();
 
   base::TimeDelta delta = base::TimeTicks::Now() - session_start_;
 
