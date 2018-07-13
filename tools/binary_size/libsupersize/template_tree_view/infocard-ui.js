c8e7e3607d39c4a6717a3b91e105679cb93b23a4
Tiger Oakes <tigero@google.com>
2018-07-13 18:14:43 +0000

    Supersize: Fix percentages and counts in diff mode
    Previously percentages and symbol counts were misreported in diff mode,
    as the values did not match up with the total size. Percentages are now
    calculated based on the total delta of bytes changed, and the pie chart
    has an outer ring to indicate if that type of symbol reduced or
    increased.
    
    Counts have been updated to be more accurate now that we have small
    symbol buckets. A new property has been added to the data file that
    indicated how many symbols a symbol represents. Most symbols omit this
    key and default to 1 instead. Buckets indicate how many symbols they
    represent. Removed dex methods have a negative count to indicate they
    were removed, fixing the method count mode in diff mode.
    
    Bug: 847599
    Change-Id: I20073dc20b89481733aaaa319f3b4966217c6096
    Reviewed-on: https://chromium-review.googlesource.com/1135698
    Commit-Queue: Tiger Oakes <tigero@google.com>
    Reviewed-by: agrieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574981}

--- tools/binary_size/libsupersize/template_tree_view/infocard-ui.js
+++ tools/binary_size/libsupersize/template_tree_view/infocard-ui.js
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * symbols as the user hovers or focuses on them.
  */
 
-{
+const displayInfocard = (() => {
   const _CANVAS_RADIUS = 40;
 
   class Infocard {
@@ -44,13 +44,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
      *
      * Example: "1,234 bytes (1.23 KiB)"
      * @param {TreeNode} node
-     * @param {GetSize} getSizeLabel
      */
-    _updateSize(node, getSizeLabel) {
-      const {description, element} = getSizeLabel(
-        node,
-        state.get('byteunit', {default: 'MiB', valid: _BYTE_UNITS_SET})
-      );
+    _updateSize(node) {
+      const {description, element, value} = getSizeContents(node);
       const sizeFragment = dom.createFragment([
         document.createTextNode(`${description} (`),
         element,
@@ -58,11 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       ]);
 
       // Update DOM
-      if (node.size < 0) {
-        this._sizeInfo.classList.add('negative');
-      } else {
-        this._sizeInfo.classList.remove('negative');
-      }
+      setSizeClasses(this._sizeInfo, value);
 
       dom.replace(this._sizeInfo, sizeFragment);
     }
@@ -117,13 +109,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     /**
      * Updates the DOM for the info card.
      * @param {TreeNode} node
-     * @param {GetSize} getSizeLabel
      */
-    _updateInfocard(node, getSizeLabel) {
+    _updateInfocard(node) {
       const type = node.type[0];
 
       // Update DOM
-      this._updateSize(node, getSizeLabel);
+      this._updateSize(node);
       this._updatePath(node);
       if (type !== this._lastType) {
         // No need to create a new icon if it is identical.
@@ -136,12 +127,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     /**
      * Updates the card on the next animation frame.
      * @param {TreeNode} node
-     * @param {GetSize} getSizeLabel
      */
-    updateInfocard(node, getSizeLabel) {
+    updateInfocard(node) {
       cancelAnimationFrame(Infocard._pendingFrame);
       Infocard._pendingFrame = requestAnimationFrame(() =>
-        this._updateInfocard(node, getSizeLabel)
+        this._updateInfocard(node)
       );
     }
   }
@@ -206,16 +196,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
      * Draw a slice of a pie chart.
      * @param {number} angleStart Starting angle, in radians.
      * @param {number} percentage Percentage of circle to draw.
-     * @param {string} color Color of the pie slice.
+     * @param {string} fillColor Color of the pie slice.
+     * @param {string} strokeColor Color of the pie slice border.
      * @returns {number} Ending angle, in radians.
      */
-    _drawSlice(angleStart, percentage, color) {
-      const arcLength = percentage * 2 * Math.PI;
+    _drawSlice(angleStart, percentage, fillColor, strokeColor) {
+      const arcLength = Math.abs(percentage) * 2 * Math.PI;
       const angleEnd = angleStart + arcLength;
       if (arcLength === 0) return angleEnd;
 
       // Update DOM
-      this._ctx.fillStyle = color;
+      this._ctx.fillStyle = fillColor;
       // Move cursor to center, where line will start
       this._ctx.beginPath();
       this._ctx.moveTo(40, 40);
@@ -224,6 +215,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       // Move cursor back to center
       this._ctx.closePath();
       this._ctx.fill();
+
+      if (strokeColor) {
+        this._ctx.strokeStyle = strokeColor;
+        this._ctx.lineWidth = 16;
+        this._ctx.beginPath();
+        this._ctx.arc(40, 40, _CANVAS_RADIUS, angleStart, angleEnd);
+        this._ctx.stroke();
+      }
 
       return angleEnd;
     }
@@ -272,23 +271,31 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     /**
      * Update DOM for the container infocard
      * @param {TreeNode} containerNode
-     * @param {GetSize} getSizeLabel
      */
-    _updateInfocard(containerNode, getSizeLabel) {
+    _updateInfocard(containerNode) {
       const extraRows = {...this._infoRows};
       const statsEntries = Object.entries(containerNode.childStats).sort(
         (a, b) => b[1].size - a[1].size
       );
+      const diffMode = state.has('diff_mode');
+      let totalSize = 0;
+      for (const [, stats] of statsEntries) {
+        totalSize += Math.abs(stats.size);
+      }
 
       // Update DOM
-      super._updateInfocard(containerNode, getSizeLabel);
+      super._updateInfocard(containerNode);
       let angleStart = 0;
       for (const [type, stats] of statsEntries) {
         delete extraRows[type];
         const {color} = getIconStyle(type);
-        const percentage = stats.size / containerNode.size;
+        const percentage = stats.size / totalSize;
+        let stroke = '';
+        if (diffMode) {
+          stroke = stats.size > 0 ? '#ea4335' : '#34a853';
+        }
 
-        angleStart = this._drawSlice(angleStart, percentage, color);
+        angleStart = this._drawSlice(angleStart, percentage, color, stroke);
         this._updateBreakdownRow(this._infoRows[type], stats, percentage);
       }
 
@@ -305,19 +312,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   /**
    * Displays an infocard for the given symbol on the next frame.
    * @param {TreeNode} node
-   * @param {GetSize} getSizeLabel
    */
-  function displayInfocard(node, getSizeLabel) {
+  function displayInfocard(node) {
     if (_CONTAINER_TYPE_SET.has(node.type[0])) {
-      _containerInfo.updateInfocard(node, getSizeLabel);
+      _containerInfo.updateInfocard(node);
       _containerInfo.setHidden(false);
       _symbolInfo.setHidden(true);
     } else {
-      _symbolInfo.updateInfocard(node, getSizeLabel);
+      _symbolInfo.updateInfocard(node);
       _symbolInfo.setHidden(false);
       _containerInfo.setHidden(true);
     }
   }
 
-  self.displayInfocard = displayInfocard;
-}
+  return displayInfocard;
+})();
