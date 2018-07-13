33ae8c96ae53dfdf61c1fdcf40668aa918eaeb63
Patrick Monette <pmonette@chromium.org>
2018-07-13 15:22:40 +0000

    Save the warning and blocking decision of each module.
    This will be used in an upcoming update to the chrome://conflicts page
    to show why a particular module was blocked or caused an incompatibility
    warning to be shown.
    
    Bug: 846953
    Change-Id: I6e6eab9f4f9805b5fb67a00eb9f5521c006f11ff
    Reviewed-on: https://chromium-review.googlesource.com/1135658
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574923}

--- chrome/browser/conflicts/module_blacklist_cache_updater_win_unittest.cc
+++ chrome/browser/conflicts/module_blacklist_cache_updater_win_unittest.cc
@@ -199,8 +199,9 @@ TEST_F(ModuleBlacklistCacheUpdaterTest, OneThirdPartyModule) {
   auto module_blacklist_cache_updater = CreateModuleBlacklistCacheUpdater();
 
   // Simulate some arbitrary module loading into the process.
+  ModuleInfoKey module_key(dll1_, 0, 0, 0);
   module_blacklist_cache_updater->OnNewModuleFound(
-      ModuleInfoKey(dll1_, 0, 0, 0), CreateLoadedModuleInfoData());
+      module_key, CreateLoadedModuleInfoData());
   module_blacklist_cache_updater->OnModuleDatabaseIdle();
 
   RunUntilIdle();
@@ -217,6 +218,9 @@ TEST_F(ModuleBlacklistCacheUpdaterTest, OneThirdPartyModule) {
                                      &blacklisted_modules, &md5_digest));
 
   EXPECT_EQ(1u, blacklisted_modules.size());
+  ASSERT_EQ(
+      ModuleBlacklistCacheUpdater::ModuleBlockingDecision::kBlacklisted,
+      module_blacklist_cache_updater->GetModuleBlockingDecision(module_key));
 }
 
 TEST_F(ModuleBlacklistCacheUpdaterTest, IgnoreMicrosoftModules) {
@@ -258,6 +262,9 @@ TEST_F(ModuleBlacklistCacheUpdaterTest, IgnoreMicrosoftModules) {
                                      &blacklisted_modules, &md5_digest));
 
   EXPECT_EQ(0u, blacklisted_modules.size());
+  ASSERT_EQ(
+      ModuleBlacklistCacheUpdater::ModuleBlockingDecision::kAllowedMicrosoft,
+      module_blacklist_cache_updater->GetModuleBlockingDecision(module_key));
 }
 
 // Tests that modules with a matching certificate subject are whitelisted.
@@ -267,8 +274,9 @@ TEST_F(ModuleBlacklistCacheUpdaterTest, WhitelistMatchingCertificateSubject) {
   auto module_blacklist_cache_updater = CreateModuleBlacklistCacheUpdater();
 
   // Simulate the module loading into the process.
+  ModuleInfoKey module_key(dll1_, 0, 0, 0);
   module_blacklist_cache_updater->OnNewModuleFound(
-      ModuleInfoKey(dll1_, 0, 0, 0), CreateSignedLoadedModuleInfoData());
+      module_key, CreateSignedLoadedModuleInfoData());
   module_blacklist_cache_updater->OnModuleDatabaseIdle();
 
   RunUntilIdle();
@@ -285,6 +293,10 @@ TEST_F(ModuleBlacklistCacheUpdaterTest, WhitelistMatchingCertificateSubject) {
                                      &blacklisted_modules, &md5_digest));
 
   EXPECT_EQ(0u, blacklisted_modules.size());
+  ASSERT_EQ(
+      ModuleBlacklistCacheUpdater::ModuleBlockingDecision::
+          kAllowedSameCertificate,
+      module_blacklist_cache_updater->GetModuleBlockingDecision(module_key));
 }
 
 // Make sure IMEs are allowed while shell extensions are blacklisted.
@@ -298,7 +310,7 @@ TEST_F(ModuleBlacklistCacheUpdaterTest, RegisteredModules) {
   ModuleInfoData module_data1 = CreateLoadedModuleInfoData();
   module_data1.module_properties |= ModuleInfoData::kPropertyIme;
 
-  ModuleInfoKey module_key2(dll2_, 456u, 789u, 0);
+  ModuleInfoKey module_key2(dll2_, 456u, 789u, 1);
   ModuleInfoData module_data2 = CreateLoadedModuleInfoData();
   module_data2.module_properties |= ModuleInfoData::kPropertyShellExtension;
 
@@ -322,6 +334,12 @@ TEST_F(ModuleBlacklistCacheUpdaterTest, RegisteredModules) {
 
   // Make sure the only blacklisted module is the shell extension.
   ASSERT_EQ(1u, blacklisted_modules.size());
+  ASSERT_EQ(
+      ModuleBlacklistCacheUpdater::ModuleBlockingDecision::kAllowedIME,
+      module_blacklist_cache_updater->GetModuleBlockingDecision(module_key1));
+  ASSERT_EQ(
+      ModuleBlacklistCacheUpdater::ModuleBlockingDecision::kBlacklisted,
+      module_blacklist_cache_updater->GetModuleBlockingDecision(module_key2));
 
   third_party_dlls::PackedListModule expected;
   const std::string module_basename = base::UTF16ToUTF8(
