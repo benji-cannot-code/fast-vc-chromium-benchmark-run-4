a5946e48f45b4e1209e727ad76d374dec2b78cc1
Daniel Murphy <dmurph@chromium.org>
2018-07-13 01:43:14 +0000

    [SessionStorageS13N] Graceful downgrade of session storage database
    This allows the graceful downgrade of the session storage database from
    the new servicified database to the pre-servicification database.
    
    R=mek@chromium.org
    
    Bug: 859464,857464
    Change-Id: I7049444a9c11b394c704b6a523c51c5b1ce47735
    Reviewed-on: https://chromium-review.googlesource.com/1135894
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574809}

--- content/browser/dom_storage/session_storage_database_unittest.cc
+++ content/browser/dom_storage/session_storage_database_unittest.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/scoped_task_environment.h"
 #include "base/threading/thread_task_runner_handle.h"
+#include "content/browser/dom_storage/session_storage_metadata.h"
 #include "content/common/dom_storage/dom_storage_types.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/leveldatabase/src/include/leveldb/db.h"
@@ -826,5 +827,24 @@ TEST_F(SessionStorageDatabaseTest, DeleteAllOrigins) {
   CheckDatabaseConsistency();
 }
 
+TEST_F(SessionStorageDatabaseTest, WipeDBOnNewVersion) {
+  // Write data for a namespace.
+  DOMStorageValuesMap data;
+  data[kKey1] = kValue1;
+  ASSERT_TRUE(db_->CommitAreaChanges(kNamespace1, kOrigin1, false, data));
+  CheckDatabaseConsistency();
+
+  leveldb::Slice version_key =
+      leveldb::Slice(reinterpret_cast<const char*>(
+                         SessionStorageMetadata::kDatabaseVersionBytes),
+                     sizeof(SessionStorageMetadata::kDatabaseVersionBytes));
+  db_->db()->Put(leveldb::WriteOptions(), version_key, "something");
+  ResetDatabase();
+
+  CheckDatabaseConsistency();
+  CheckEmptyDatabase();
+  data.clear();
+  CheckAreaData(kNamespace1, kOrigin1, data);
+}
 
 }  // namespace content
