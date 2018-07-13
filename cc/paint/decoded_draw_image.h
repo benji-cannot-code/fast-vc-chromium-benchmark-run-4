9a6c385277e34a5b5aa1aa0bd96772e88b80430d
Eric Karl <ericrk@chromium.org>
2018-07-13 08:26:44 +0000

    Explicitly manage mips in GPU Image Decode Cache.
    Changes in Skia require that Chrome explicitly specify whether an SkImage needs mips.
    
    With this change, we now request mips in two places - at image upload time, if needed immediately,
    or at image use time, if we need to add them to an existing upload. As mips use additional space
    and aren't needed for exactly-sized images, we try to avoid adding them unless necessary.
    
    To simplify handling, we abstract texture upload, colorspace conversion, and mip generation behind
    a new MakeTextureImage function. This function provides similar functionality to Skia's
    SkImage::makeTextureImage, but works around colorspace limitations (Skia's fn ignores colorspaces
    for now).
    
    Bug: 834837
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ib616c79c800f1b2f32011c225d6e5a6eff00c36c
    Reviewed-on: https://chromium-review.googlesource.com/1125304
    Commit-Queue: Eric Karl <ericrk@chromium.org>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574861}

--- cc/paint/decoded_draw_image.h
+++ cc/paint/decoded_draw_image.h
@@ -29,6 +29,7 @@ class CC_PAINT_EXPORT DecodedDrawImage {
                    const SkSize& src_rect_offset,
                    const SkSize& scale_adjustment,
                    SkFilterQuality filter_quality,
+                   bool needs_mips,
                    bool is_budgeted);
   DecodedDrawImage(const DecodedDrawImage& other);
   DecodedDrawImage();
@@ -45,6 +46,9 @@ class CC_PAINT_EXPORT DecodedDrawImage {
     return std::abs(scale_adjustment_.width() - 1.f) < FLT_EPSILON &&
            std::abs(scale_adjustment_.height() - 1.f) < FLT_EPSILON;
   }
+  bool transfer_cache_entry_needs_mips() const {
+    return transfer_cache_entry_needs_mips_;
+  }
   bool is_budgeted() const { return is_budgeted_; }
 
  private:
@@ -53,6 +57,7 @@ class CC_PAINT_EXPORT DecodedDrawImage {
   SkSize src_rect_offset_;
   SkSize scale_adjustment_;
   SkFilterQuality filter_quality_;
+  bool transfer_cache_entry_needs_mips_ = false;
   bool is_budgeted_;
 };
 
--- cc/tiles/gpu_image_decode_cache.h
+++ cc/tiles/gpu_image_decode_cache.h
@@ -295,7 +295,8 @@ class CC_EXPORT GpuImageDecodeCache
               size_t size,
               const gfx::ColorSpace& target_color_space,
               SkFilterQuality quality,
-              int mip_level,
+              int upload_scale_mip_level,
+              bool needs_mips,
               bool is_bitmap_backed);
 
     bool IsGpuOrTransferCache() const;
@@ -306,7 +307,8 @@ class CC_EXPORT GpuImageDecodeCache
     const size_t size;
     gfx::ColorSpace target_color_space;
     SkFilterQuality quality;
-    int mip_level;
+    int upload_scale_mip_level;
+    bool needs_mips = false;
     bool is_bitmap_backed;
     bool is_budgeted = false;
 
@@ -346,7 +348,7 @@ class CC_EXPORT GpuImageDecodeCache
     explicit InUseCacheKey(const DrawImage& draw_image);
 
     PaintImage::FrameKey frame_key;
-    int mip_level;
+    int upload_scale_mip_level;
     SkFilterQuality filter_quality;
     gfx::ColorSpace target_color_space;
   };
@@ -443,6 +445,9 @@ class CC_EXPORT GpuImageDecodeCache
                                                 PaintImage::FrameKeyHash>;
   PersistentCache::iterator RemoveFromPersistentCache(
       PersistentCache::iterator it);
+
+  // Adds mips to an image if required.
+  void UpdateMipsIfNeeded(const DrawImage& draw_image, ImageData* image_data);
 
   const SkColorType color_type_;
   const bool use_transfer_cache_ = false;
