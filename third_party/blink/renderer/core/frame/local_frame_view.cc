327cb17425df0d6de0f59ccdc70024272ad5bf0e
Chris Harrelson <chrishtr@chromium.org>
2018-07-13 18:55:20 +0000

    Don't notify throttled frames of frame rect changes.
    This is unnecessary, because once these frames become unthrottled
    they will update their layout, including frame rects.
    
    Change-Id: I9f58575b58d5e6e7ced6d11391a3d3f628365feb
    Reviewed-on: https://chromium-review.googlesource.com/1134505
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575002}

--- third_party/blink/renderer/core/frame/local_frame_view.cc
+++ third_party/blink/renderer/core/frame/local_frame_view.cc
@@ -3529,7 +3529,9 @@ void LocalFrameView::FrameRectsChanged() {
     SetLayoutSizeInternal(Size());
 
   ForAllChildViewsAndPlugins([](EmbeddedContentView& embedded_content_view) {
-    embedded_content_view.FrameRectsChanged();
+    if (!embedded_content_view.IsLocalFrameView() ||
+        !ToLocalFrameView(embedded_content_view).ShouldThrottleRendering())
+      embedded_content_view.FrameRectsChanged();
   });
 
   GetFrame().Client()->FrameRectsChanged(FrameRect());
