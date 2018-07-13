6f4792fd178778125657946d1e8bdfd2ebb56a3d
Sidney San Martín <sdy@chromium.org>
2018-07-13 20:21:07 +0000

    Fix missing rounded corners on MacViews app windows.
    Two changes:
    
    - Make all Views windows layer backed (they mostly were already).
    
    - Use _titlebarHeight to hide the titlebar for app windows. _hidingTitlebar
      resulted in the top corners not being clipped (as if the window were a
      sheet).
    
    Bug: 863264, 862672
    Change-Id: I9647afdcba464cc17b88cc2d752bf273a0f6ab89
    Reviewed-on: https://chromium-review.googlesource.com/1136520
    Commit-Queue: Sidney San Martín <sdy@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575047}

--- chrome/browser/ui/views/frame/native_widget_mac_frameless_nswindow.mm
+++ chrome/browser/ui/views/frame/native_widget_mac_frameless_nswindow.mm
@@ -14,8 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @end
 
 @implementation NativeWidgetMacFramelessNSWindowFrame
-- (BOOL)_hidingTitlebar {
-  return YES;
+- (CGFloat)_titlebarHeight {
+  return 0;
 }
 @end
 
