17ae43b166f5b16fac285024091f99ba9e8410ab
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-07-13 00:29:38 +0000

    Import wpt@ceba83a56edd838cca0aacd35549f979292a5995
    Using wpt-import in Chromium ed5e651e10cd158bf786b33c0dcd77d714244d2e.
    With Chromium commits locally applied on WPT:
    a9045946af "Add a fail-fast for longtask layouttests"
    
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/21191
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    jsbell@chromium.org:
      external/wpt/IndexedDB
    yhirano@chromium.org, mkwst@chromium.org, japhet@chromium.org:
      external/wpt/fetch
    
    TBR=foolip
    
    No-Export: true
    Change-Id: I8cbb857220afec37734ab39cca07d888d7d3c941
    Reviewed-on: https://chromium-review.googlesource.com/1135754
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574788}

---
+++ third_party/WebKit/LayoutTests/external/wpt/IndexedDB/idlharness.any.js
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: global=window,worker
+// META: script=/resources/WebIDLParser.js
+// META: script=/resources/idlharness.js
+
+idl_test(
+  ['IndexedDB'],
+  ['html', 'dom'],
+  idl_array => {
+    idl_array.add_objects({
+      IDBCursor: [],
+      IDBCursorWithValue: [],
+      IDBDatabase: [],
+      IDBFactory: [self.indexedDB],
+      IDBIndex: [],
+      IDBKeyRange: [IDBKeyRange.only(0)],
+      IDBObjectStore: [],
+      IDBOpenDBRequest: [],
+      IDBRequest: [],
+      IDBTransaction: [],
+      IDBVersionChangeEvent: ['new IDBVersionChangeEvent("type")'],
+      DOMStringList: [],
+    });
+  },
+  'IndexedDB interfaces'
+);
