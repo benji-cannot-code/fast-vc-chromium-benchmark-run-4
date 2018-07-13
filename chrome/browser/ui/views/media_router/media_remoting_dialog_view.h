9ea739baa41b1dbbdf3b14861a916ed85fea7ccc
Xiangjun Zhang <xjz@chromium.org>
2018-07-13 17:04:01 +0000

    Media remoting dialog: Check pref before showing dialog.
    This CL adds the implementation to check the preference of
    enabling/disabling media remoting. The remoting dialog is only shown
    when user has not set the preference.
    
    The preference can be set through the remoting dialog. Though this CL
    doesn't clear the preference once it is set. This will be done in a
    later CL.
    
    Bug: 849020
    Change-Id: I89c4ed6b71a2e51bd81fae919531d15b53664b18
    Reviewed-on: https://chromium-review.googlesource.com/1125355
    Commit-Queue: Xiangjun Zhang <xjz@chromium.org>
    Reviewed-by: Derek Cheng <imcheng@chromium.org>
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574954}

--- chrome/browser/ui/views/media_router/media_remoting_dialog_view.h
+++ chrome/browser/ui/views/media_router/media_remoting_dialog_view.h
@@ -8,9 +8,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/views/bubble/bubble_dialog_delegate.h"
 
+class PrefService;
+
 namespace views {
 class Checkbox;
 }  // namespace views
+
+namespace content {
+class WebContents;
+}  // namespace content
 
 namespace media_router {
 
@@ -20,11 +26,13 @@ namespace media_router {
 // then mirroring it remotely.
 class MediaRemotingDialogView : public views::BubbleDialogDelegateView {
  public:
-  // Instantiates and shows the singleton dialog. The dialog must not be
-  // currently shown.
-  // TODO(https://crbug.com/849020): This method needs to take a callback to
-  // call when the user interacts with the dialog.
-  static void ShowDialog(views::View* anchor_view);
+  using PermissionCallback = base::OnceCallback<void(bool)>;
+  // Checks the existing preference value, and if unset, instantiates and shows
+  // the singleton dialog to get user's permission. |callback| runs on the same
+  // stack if the preference was set, or asynchronously when user sets the
+  // permission through the dialog.
+  static void GetPermission(content::WebContents* web_contents,
+                            PermissionCallback callback);
 
   // No-op if the dialog is currently not shown.
   static void HideDialog();
@@ -50,16 +58,25 @@ class MediaRemotingDialogView : public views::BubbleDialogDelegateView {
   gfx::Size CalculatePreferredSize() const override;
 
  private:
-  explicit MediaRemotingDialogView(views::View* anchor_view);
+  explicit MediaRemotingDialogView(views::View* anchor_view,
+                                   PrefService* pref_service,
+                                   PermissionCallback callback);
   ~MediaRemotingDialogView() override;
 
   // views::BubbleDialogDelegateView:
   void Init() override;
   void WindowClosing() override;
 
+  // Runs |permission_callback_| to report whether remoting is allowed by user.
+  void ReportPermission(bool allowed);
+
   // The singleton dialog instance. This is a nullptr when a dialog is not
   // shown.
   static MediaRemotingDialogView* instance_;
+
+  PermissionCallback permission_callback_;
+
+  PrefService* const pref_service_;
 
   // Title shown at the top of the dialog.
   base::string16 dialog_title_;
