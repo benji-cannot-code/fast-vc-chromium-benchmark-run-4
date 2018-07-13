f2c0d23b4d34dc86cebc9f48ff373a9c0e51cd20
David Black <dmblack@google.com>
2018-07-13 21:30:02 +0000

    Pass type and params in OnDeepLinkReceived event.
    DeepLinkType enum is more easily consumed by event observers.
    DeepLinkType + params adequately represents the complete deep link.
    
    Bug: b:111437390
    Change-Id: I62618b9ae84a1240232bd2f9e50f999cb2be9055
    Reviewed-on: https://chromium-review.googlesource.com/1136855
    Commit-Queue: David Black <dmblack@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575075}

--- ash/assistant/assistant_controller_observer.h
+++ ash/assistant/assistant_controller_observer.h
@@ -6,11 +6,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_ASSISTANT_ASSISTANT_CONTROLLER_OBSERVER_H_
 #define ASH_ASSISTANT_ASSISTANT_CONTROLLER_OBSERVER_H_
 
+#include <map>
+#include <string>
+
 #include "base/macros.h"
 
 class GURL;
 
 namespace ash {
+
+namespace assistant {
+namespace util {
+enum class DeepLinkType;
+}  // namespace util
+}  // namespace assistant
 
 class AssistantControllerObserver {
  public:
@@ -20,8 +29,11 @@ class AssistantControllerObserver {
   // Invoked when the AssistantController is starting to be destroyed.
   virtual void OnAssistantControllerDestroying() {}
 
-  // Invoked when Assistant has received the specified |deep_link|.
-  virtual void OnDeepLinkReceived(const GURL& deep_link) {}
+  // Invoked when Assistant has received a deep link of the specified |type|
+  // with the given |params|.
+  virtual void OnDeepLinkReceived(
+      assistant::util::DeepLinkType type,
+      const std::map<std::string, std::string>& params) {}
 
   // Invoked when the specified |url| is opened by Assistant in a new tab.
   virtual void OnUrlOpened(const GURL& url) {}
