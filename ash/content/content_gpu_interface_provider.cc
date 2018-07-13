58a9d6af3e8d9a3b9661f43a8b83f107b1b55c24
Xu Xing <xing.xu@intel.com>
2018-07-13 00:31:20 +0000

    viz: Remove GpuClient dependency on BrowserThread
    BUG=857217
    
    Change-Id: I328529cc444ddcadad83eeb4b9aa5ff445844912
    Reviewed-on: https://chromium-review.googlesource.com/1125518
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Xing Xu <xing.xu@intel.com>
    Cr-Commit-Position: refs/heads/master@{#574789}

--- ash/content/content_gpu_interface_provider.cc
+++ ash/content/content_gpu_interface_provider.cc
@@ -15,19 +15,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
-// InterfaceBinderImpl handles the actual binding. The binding (and destruction
-// of this object) has to happen on the io-thread.
+// InterfaceBinderImpl handles the actual binding. The binding has to happen on
+// the IO thread.
 class ContentGpuInterfaceProvider::InterfaceBinderImpl
-    : public base::RefCountedThreadSafe<
-          InterfaceBinderImpl,
-          content::BrowserThread::DeleteOnIOThread> {
+    : public base::RefCountedThreadSafe<InterfaceBinderImpl> {
  public:
   InterfaceBinderImpl() = default;
 
   void BindGpuRequestOnGpuTaskRunner(ui::mojom::GpuRequest request) {
+    // The GPU task runner is bound to the IO thread.
+    DCHECK_CURRENTLY_ON(content::BrowserThread::IO);
     auto gpu_client = content::GpuClient::Create(
         std::move(request),
-        base::BindOnce(&InterfaceBinderImpl::OnGpuClientConnectionError, this));
+        base::BindOnce(&InterfaceBinderImpl::OnGpuClientConnectionError, this),
+        content::BrowserThread::GetTaskRunnerForThread(
+            content::BrowserThread::IO));
     gpu_clients_.push_back(std::move(gpu_client));
   }
 
@@ -38,22 +40,17 @@ class ContentGpuInterfaceProvider::InterfaceBinderImpl
   }
 
  private:
-  friend struct content::BrowserThread::DeleteOnThread<
-      content::BrowserThread::IO>;
-  friend class base::DeleteHelper<InterfaceBinderImpl>;
-
+  friend class base::RefCountedThreadSafe<InterfaceBinderImpl>;
   ~InterfaceBinderImpl() = default;
 
   void OnGpuClientConnectionError(content::GpuClient* client) {
     base::EraseIf(
         gpu_clients_,
-        base::UniquePtrMatcher<content::GpuClient,
-                               content::BrowserThread::DeleteOnIOThread>(
+        base::UniquePtrMatcher<content::GpuClient, base::OnTaskRunnerDeleter>(
             client));
   }
 
-  std::vector<std::unique_ptr<content::GpuClient,
-                              content::BrowserThread::DeleteOnIOThread>>
+  std::vector<std::unique_ptr<content::GpuClient, base::OnTaskRunnerDeleter>>
       gpu_clients_;
 
   DISALLOW_COPY_AND_ASSIGN(InterfaceBinderImpl);
--- content/public/browser/gpu_client.h
+++ content/public/browser/gpu_client.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback_forward.h"
 #include "content/common/content_export.h"
-#include "content/public/browser/browser_thread.h"
 #include "services/ui/public/interfaces/gpu.mojom.h"
 
 namespace content {
@@ -22,9 +21,10 @@ class CONTENT_EXPORT GpuClient {
 
   using ConnectionErrorHandlerClosure =
       base::OnceCallback<void(GpuClient* client)>;
-  static std::unique_ptr<GpuClient, BrowserThread::DeleteOnIOThread> Create(
+  static std::unique_ptr<GpuClient, base::OnTaskRunnerDeleter> Create(
       ui::mojom::GpuRequest request,
-      ConnectionErrorHandlerClosure connection_error_handler);
+      ConnectionErrorHandlerClosure connection_error_handler,
+      scoped_refptr<base::SingleThreadTaskRunner> task_runner);
 };
 
 }  // namespace content
