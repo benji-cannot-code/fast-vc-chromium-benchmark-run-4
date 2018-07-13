fc6607fd5214dc3be3cfb277ed76257a7f305523
Sadrul Habib Chowdhury <sadrul@chromium.org>
2018-07-13 19:44:59 +0000

    tab-switching: Report some additional latency times.
    When switching tabs, report the time it takes for the browser to send
    the request to the renderer to become visible.
    
    BUG=858944
    
    Change-Id: Id11569162c433a66f61a4fd8dc8ef8c8b2f70327
    Reviewed-on: https://chromium-review.googlesource.com/1136217
    Reviewed-by: Robert Kaplow (slow) <rkaplow@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575025}

--- chrome/browser/ui/tabs/tab_strip_model.cc
+++ chrome/browser/ui/tabs/tab_strip_model.cc
@@ -30,6 +30,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/feature_engagement/buildflags.h"
 #include "components/web_modal/web_contents_modal_dialog_manager.h"
 #include "content/public/browser/render_process_host.h"
+#include "content/public/browser/render_widget_host.h"
+#include "content/public/browser/render_widget_host_observer.h"
+#include "content/public/browser/render_widget_host_view.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_observer.h"
 
@@ -59,6 +62,49 @@ bool ShouldForgetOpenersForTransition(ui::PageTransition transition) {
          ui::PageTransitionCoreTypeIs(transition,
                                       ui::PAGE_TRANSITION_AUTO_TOPLEVEL);
 }
+
+// This tracks (and reports via UMA and tracing) how long it takes before a
+// RenderWidgetHost is requested to become visible.
+class RenderWidgetHostVisibilityTracker
+    : public content::RenderWidgetHostObserver {
+ public:
+  explicit RenderWidgetHostVisibilityTracker(content::RenderWidgetHost* host)
+      : host_(host) {
+    if (!host_ || host_->GetView()->IsShowing())
+      return;
+    host_->AddObserver(this);
+    TRACE_EVENT_ASYNC_BEGIN0("ui,latency", "TabSwitchVisibilityRequest", this);
+  }
+
+  ~RenderWidgetHostVisibilityTracker() override {
+    if (host_)
+      host_->RemoveObserver(this);
+  }
+
+ private:
+  // content::RenderWidgetHostObserver:
+  void RenderWidgetHostVisibilityChanged(content::RenderWidgetHost* host,
+                                         bool became_visible) override {
+    DCHECK_EQ(host_, host);
+    DCHECK(became_visible);
+    UMA_HISTOGRAM_CUSTOM_MICROSECONDS_TIMES(
+        "Browser.Tabs.SelectionToVisibilityRequestTime", timer_.Elapsed(),
+        base::TimeDelta::FromMicroseconds(1), base::TimeDelta::FromSeconds(3),
+        50);
+    TRACE_EVENT_ASYNC_END0("ui,latency", "TabSwitchVisibilityRequest", this);
+  }
+
+  void RenderWidgetHostDestroyed(content::RenderWidgetHost* host) override {
+    DCHECK_EQ(host_, host);
+    host_->RemoveObserver(this);
+    host_ = nullptr;
+  }
+
+  content::RenderWidgetHost* host_ = nullptr;
+  base::ElapsedTimer timer_;
+
+  DISALLOW_COPY_AND_ASSIGN(RenderWidgetHostVisibilityTracker);
+};
 
 }  // namespace
 
@@ -1397,6 +1443,13 @@ void TabStripModel::NotifyIfActiveTabChanged(WebContents* old_contents,
   WebContents* new_contents = GetWebContentsAtImpl(active_index());
   if (old_contents == new_contents)
     return;
+
+  content::RenderWidgetHost* track_host = nullptr;
+  if (notify_types == Notify::kUserGesture &&
+      new_contents->GetRenderWidgetHostView()) {
+    track_host = new_contents->GetRenderWidgetHostView()->GetRenderWidgetHost();
+  }
+  RenderWidgetHostVisibilityTracker tracker(track_host);
 
   int reason = notify_types == Notify::kUserGesture
                    ? TabStripModelObserver::CHANGE_REASON_USER_GESTURE
