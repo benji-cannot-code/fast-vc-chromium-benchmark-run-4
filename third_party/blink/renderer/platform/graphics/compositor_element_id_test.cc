f87977dc350fb52d87d537e8d25e896a54ea679c
Stefan Zager <szager@chromium.org>
2018-07-13 20:20:35 +0000

    CompositorElementId: s/ToInternalValue/GetInternalValue/
    This is only necessary to eliminate a presubmit error about the use
    of the banned method Time::ToInternalValue. No functional change.
    
    BUG=752327
    R=chrishtr@chromium.org,vmpstr@chromium.org
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Id0ecab92b4605da1c25398a539be7dc87a250547
    Reviewed-on: https://chromium-review.googlesource.com/1135988
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575046}

--- third_party/blink/renderer/platform/graphics/compositor_element_id_test.cc
+++ third_party/blink/renderer/platform/graphics/compositor_element_id_test.cc
@@ -12,7 +12,7 @@ namespace blink {
 class CompositorElementIdTest : public testing::Test {};
 
 uint64_t IdFromCompositorElementId(CompositorElementId element_id) {
-  return element_id.ToInternalValue() >> kCompositorNamespaceBitCount;
+  return element_id.GetInternalValue() >> kCompositorNamespaceBitCount;
 }
 
 TEST_F(CompositorElementIdTest, EncodeDecode) {
