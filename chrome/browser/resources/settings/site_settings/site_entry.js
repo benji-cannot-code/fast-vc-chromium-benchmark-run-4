bd257f306b3f02cd2280ec5977a5cc100c85032c
Patti <patricialor@chromium.org>
2018-07-13 22:41:50 +0000

    Settings: Show the number of cookies set on grouped entries in All Sites.
    Show the number of cookies on a domain / eTLD+1 group of sites in All Sites.
    Since cookies at their widest scope can be set on eTLD+1s, this display will
    be restricted to sites that are grouped.
    
    Bug: 835712
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: I66d2c36b2af1ad577d4d71512f3d22541e1c926f
    Reviewed-on: https://chromium-review.googlesource.com/1111770
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Patti <patricialor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575098}

--- chrome/browser/resources/settings/site_settings/site_entry.js
+++ chrome/browser/resources/settings/site_settings/site_entry.js
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @fileoverview
  * 'site-entry' is an element representing a single eTLD+1 site entity.
  */
-
 Polymer({
   is: 'site-entry',
 
@@ -29,6 +28,24 @@ Polymer({
      * @private
      */
     displayName_: String,
+
+    /**
+     * The string to display when there is a non-zero number of cookies.
+     * @private
+     */
+    cookieString_: {
+      type: String,
+      value: '',
+    },
+  },
+
+  /** @private {?settings.LocalDataBrowserProxy} */
+  localDataBrowserProxy_: null,
+
+  /** @override */
+  created: function() {
+    this.localDataBrowserProxy_ =
+        settings.LocalDataBrowserProxyImpl.getInstance();
   },
 
   /**
@@ -76,9 +93,28 @@ Polymer({
   onSiteGroupChanged_: function(siteGroup) {
     this.displayName_ = this.siteRepresentation_(siteGroup, -1);
 
-    // Ensure ungrouped |siteGroup|s do not get stuck in an opened state.
-    if (!this.grouped_(SiteGroup) && this.$.collapseChild.opened)
-      this.toggleCollapsible_();
+    if (!this.grouped_(SiteGroup)) {
+      // Ensure ungrouped |siteGroup|s do not get stuck in an opened state.
+      if (this.$.collapseChild.opened)
+        this.toggleCollapsible_();
+      // Ungrouped site-entries should not show cookies.
+      if (this.cookieString_) {
+        this.cookieString_ = '';
+        this.fire('site-entry-resized');
+      }
+    }
+    if (!siteGroup || !this.grouped_(siteGroup))
+      return;
+
+    this.localDataBrowserProxy_.getNumCookiesString(this.displayName_)
+        .then(string => {
+          // If there was no cookie string previously and now there is, or vice
+          // versa, the height of this site-entry will have changed.
+          if ((this.cookieString_ == '') != (string == ''))
+            this.fire('site-entry-resized');
+
+          this.cookieString_ = string;
+        });
   },
 
   /**
