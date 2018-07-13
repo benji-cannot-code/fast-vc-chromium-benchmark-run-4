30f68cf0375c8b99e0eb4a6595c65cae6a366292
Tommy Steimel <steimel@chromium.org>
2018-07-13 20:24:12 +0000

    [Media Controls] Use zoom when calculating overlay play button hitbox
    This CL modifies the overlay play button ShouldCausePlayPause function
    to take CSS and Document zoom into account. This fixes a bug where a
    zoomed video had an incorrect hitbox for the overlay play button.
    
    Bug: 862063
    Change-Id: I3f51454618e5c455bba62b4b019701d67fd8886c
    Reviewed-on: https://chromium-review.googlesource.com/1135663
    Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575051}

--- third_party/blink/renderer/modules/media_controls/elements/media_control_overlay_play_button_element.cc
+++ third_party/blink/renderer/modules/media_controls/elements/media_control_overlay_play_button_element.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/task_type.h"
 #include "third_party/blink/public/platform/web_size.h"
 #include "third_party/blink/renderer/core/dom/events/event.h"
+#include "third_party/blink/renderer/core/dom/node_computed_style.h"
 #include "third_party/blink/renderer/core/dom/shadow_root.h"
 #include "third_party/blink/renderer/core/events/mouse_event.h"
 #include "third_party/blink/renderer/core/geometry/dom_rect.h"
@@ -16,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/html/media/html_media_element.h"
 #include "third_party/blink/renderer/core/html/media/html_media_source.h"
 #include "third_party/blink/renderer/core/input_type_names.h"
+#include "third_party/blink/renderer/core/layout/layout_view.h"
 #include "third_party/blink/renderer/modules/media_controls/elements/media_control_elements_helper.h"
 #include "third_party/blink/renderer/modules/media_controls/media_controls_impl.h"
 #include "third_party/blink/renderer/modules/media_controls/media_controls_resource_loader.h"
@@ -291,9 +293,15 @@ bool MediaControlOverlayPlayButtonElement::ShouldCausePlayPause(
 
   // If the click happened on the internal button or a margin around it then
   // we should play/pause.
-  return IsPointInRect(*internal_button_->getBoundingClientRect(),
-                       kInnerButtonTouchPaddingSize, mouse_event->clientX(),
-                       mouse_event->clientY());
+  DOMRect* box = internal_button_->getBoundingClientRect();
+  float zoom = ComputedStyleRef().EffectiveZoom() /
+               GetDocument().GetLayoutView()->ZoomFactor();
+  box->setX(box->x() * zoom);
+  box->setY(box->y() * zoom);
+  box->setWidth(box->width() * zoom);
+  box->setHeight(box->height() * zoom);
+  return IsPointInRect(*box, kInnerButtonTouchPaddingSize,
+                       mouse_event->clientX(), mouse_event->clientY());
 }
 
 WebSize MediaControlOverlayPlayButtonElement::GetSizeOrDefault() const {
