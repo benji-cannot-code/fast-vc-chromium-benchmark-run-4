fac5ce62f7dff65f4c03f8c13f4962445bfbdaf9
Douglas Creager <dcreager@chromium.org>
2018-07-15 10:34:06 +0000

    Network Error Logging: Only report DNS errors for include_subdomains policies
    Per https://github.com/w3c/network-error-logging/issues/74, NEL policies
    that have include_subdomains set can only be used to report errors
    during DNS resolution.  They cannot be used to report on successes, or
    on errors in other phases, since once we contact a server, that server
    has to explicitly consent to NEL reporting by delivering its own policy.
    
    Bug: 862188
    Change-Id: If051f6787344b9385af3e46384370636a6bafc5d
    Reviewed-on: https://chromium-review.googlesource.com/1135599
    Commit-Queue: Douglas Creager <dcreager@chromium.org>
    Reviewed-by: Misha Efimov <mef@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575180}

--- net/network_error_logging/network_error_logging_service_unittest.cc
+++ net/network_error_logging/network_error_logging_service_unittest.cc
@@ -269,6 +269,8 @@ TEST_F(NetworkErrorLoggingServiceTest, SuccessReportQueued) {
                                NetworkErrorLoggingService::kStatusCodeKey);
   base::ExpectDictIntegerValue(1000, *body,
                                NetworkErrorLoggingService::kElapsedTimeKey);
+  base::ExpectDictStringValue("application", *body,
+                              NetworkErrorLoggingService::kPhaseKey);
   base::ExpectDictStringValue("ok", *body,
                               NetworkErrorLoggingService::kTypeKey);
 }
@@ -303,6 +305,8 @@ TEST_F(NetworkErrorLoggingServiceTest, FailureReportQueued) {
                                NetworkErrorLoggingService::kStatusCodeKey);
   base::ExpectDictIntegerValue(1000, *body,
                                NetworkErrorLoggingService::kElapsedTimeKey);
+  base::ExpectDictStringValue("connection", *body,
+                              NetworkErrorLoggingService::kPhaseKey);
   base::ExpectDictStringValue("tcp.refused", *body,
                               NetworkErrorLoggingService::kTypeKey);
 }
@@ -337,6 +341,8 @@ TEST_F(NetworkErrorLoggingServiceTest, HttpErrorReportQueued) {
                                NetworkErrorLoggingService::kStatusCodeKey);
   base::ExpectDictIntegerValue(1000, *body,
                                NetworkErrorLoggingService::kElapsedTimeKey);
+  base::ExpectDictStringValue("application", *body,
+                              NetworkErrorLoggingService::kPhaseKey);
   base::ExpectDictStringValue("http.error", *body,
                               NetworkErrorLoggingService::kTypeKey);
 }
@@ -468,7 +474,7 @@ TEST_F(NetworkErrorLoggingServiceTest, IncludeSubdomainsMatchesDifferentPort) {
   service()->OnHeader(kOrigin_, kHeaderIncludeSubdomains_);
 
   service()->OnRequest(
-      MakeRequestDetails(kUrlDifferentPort_, ERR_CONNECTION_REFUSED));
+      MakeRequestDetails(kUrlDifferentPort_, ERR_NAME_NOT_RESOLVED));
 
   ASSERT_EQ(1u, reports().size());
   EXPECT_EQ(kUrlDifferentPort_, reports()[0].url);
@@ -478,7 +484,7 @@ TEST_F(NetworkErrorLoggingServiceTest, IncludeSubdomainsMatchesSubdomain) {
   service()->OnHeader(kOrigin_, kHeaderIncludeSubdomains_);
 
   service()->OnRequest(
-      MakeRequestDetails(kUrlSubdomain_, ERR_CONNECTION_REFUSED));
+      MakeRequestDetails(kUrlSubdomain_, ERR_NAME_NOT_RESOLVED));
 
   ASSERT_EQ(1u, reports().size());
 }
@@ -487,7 +493,35 @@ TEST_F(NetworkErrorLoggingServiceTest,
        IncludeSubdomainsDoesntMatchSuperdomain) {
   service()->OnHeader(kOriginSubdomain_, kHeaderIncludeSubdomains_);
 
-  service()->OnRequest(MakeRequestDetails(kUrl_, ERR_CONNECTION_REFUSED));
+  service()->OnRequest(MakeRequestDetails(kUrl_, ERR_NAME_NOT_RESOLVED));
+
+  EXPECT_TRUE(reports().empty());
+}
+
+TEST_F(NetworkErrorLoggingServiceTest,
+       IncludeSubdomainsDoesntReportConnectionError) {
+  service()->OnHeader(kOrigin_, kHeaderIncludeSubdomains_);
+
+  service()->OnRequest(
+      MakeRequestDetails(kUrlSubdomain_, ERR_CONNECTION_REFUSED));
+
+  EXPECT_TRUE(reports().empty());
+}
+
+TEST_F(NetworkErrorLoggingServiceTest,
+       IncludeSubdomainsDoesntReportApplicationError) {
+  service()->OnHeader(kOrigin_, kHeaderIncludeSubdomains_);
+
+  service()->OnRequest(
+      MakeRequestDetails(kUrlSubdomain_, ERR_INVALID_HTTP_RESPONSE));
+
+  EXPECT_TRUE(reports().empty());
+}
+
+TEST_F(NetworkErrorLoggingServiceTest, IncludeSubdomainsDoesntReportSuccess) {
+  service()->OnHeader(kOrigin_, kHeaderIncludeSubdomains_);
+
+  service()->OnRequest(MakeRequestDetails(kUrlSubdomain_, OK));
 
   EXPECT_TRUE(reports().empty());
 }
