db24414f0835a7ef27d6996e23b1b0411d1f6115
Nicolas Pena <npm@chromium.org>
2018-07-16 20:04:28 +0000

    Make longtask WPT layouttests less flaky
    This CL improves tests in external/wpt/longtask-timing by:
    * Increasing the length of a longtask from ~51 to ~60 to prevent a
    coarse performance.now() granularity from affecting the tests.
    * Wrapping the long tasks or iframes containing longtasks inside onload
    handlers to ensure that the PerformanceObserver is registered by the
    time the longtask is executed.
    
    Test: third_party/blink/tools/run_web_tests external/wpt/longtask-timing
    --iterations=1000 did not produce flakes.
    
    Bug: 796668
    Change-Id: Ic2ce59f7d221566f5443da9e9c7d36c7bd7bbe6f
    Reviewed-on: https://chromium-review.googlesource.com/1138725
    Commit-Queue: Nicolás Peña Moreno <npm@chromium.org>
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575396}

--- third_party/WebKit/LayoutTests/external/wpt/longtask-timing/resources/makelongtask.js
+++ third_party/WebKit/LayoutTests/external/wpt/longtask-timing/resources/makelongtask.js
@@ -1,4 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* Generate a slow task. */
 const begin = window.performance.now();
-while (window.performance.now() < begin + 51);
+while (window.performance.now() < begin + 60);
--- third_party/WebKit/LayoutTests/external/wpt/longtask-timing/resources/raflongtask.js
+++ third_party/WebKit/LayoutTests/external/wpt/longtask-timing/resources/raflongtask.js
@@ -2,5 +2,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 window.requestAnimationFrame(function() {
   /* Generate a slow task. */
   const begin = window.performance.now();
-  while (window.performance.now() < begin + 51);
+  while (window.performance.now() < begin + 60);
 });
