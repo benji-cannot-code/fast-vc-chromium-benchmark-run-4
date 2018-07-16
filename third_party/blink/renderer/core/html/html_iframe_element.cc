37de1db41a8b149eb84c3c7d1148db8c71bb3fdf
Orsi Batiz <orsibatiz@google.com>
2018-07-16 13:43:21 +0000

    Changing srcdoc from DOMString to HTMLString
    A setter and a getter have been added to element.cc to support StringOrTrustedHTML
    
    
    Bug: 739170
    Change-Id: I2472d1920a08834e9bd4b1cb532f72f5305a699b
    Reviewed-on: https://chromium-review.googlesource.com/1134994
    Commit-Queue: Orsolya Bernadett Batiz <orsibatiz@google.com>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575240}

--- third_party/blink/renderer/core/html/html_iframe_element.cc
+++ third_party/blink/renderer/core/html/html_iframe_element.cc
@@ -25,7 +25,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/html/html_iframe_element.h"
 
+#include "third_party/blink/renderer/bindings/core/v8/string_or_trusted_html.h"
+#include "third_party/blink/renderer/bindings/core/v8/v8_html_iframe_element.h"
 #include "third_party/blink/renderer/core/css_property_names.h"
+#include "third_party/blink/renderer/core/dom/element.h"
 #include "third_party/blink/renderer/core/frame/csp/content_security_policy.h"
 #include "third_party/blink/renderer/core/frame/use_counter.h"
 #include "third_party/blink/renderer/core/html/html_document.h"
