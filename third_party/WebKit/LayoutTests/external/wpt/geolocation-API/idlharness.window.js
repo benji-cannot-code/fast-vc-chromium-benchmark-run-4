8693d484d5320b3558d59737a3681f70493e63f1
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-07-16 18:27:32 +0000

    Import wpt@41dfb946b06a725c333314484d51da1003c901d9
    Using wpt-import in Chromium d80b0273ace36654311eae16e646bfdd0dea402f.
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/21462
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    mcasas@chromium.org:
      external/wpt/geolocation-API
    
    TBR=raphael.kubo.da.costa@intel.com
    
    No-Export: true
    Change-Id: Ic10b935af281a05a087702df437dae12fd1731f3
    Reviewed-on: https://chromium-review.googlesource.com/1138541
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575353}

---
+++ third_party/WebKit/LayoutTests/external/wpt/geolocation-API/idlharness.window.js
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/resources/WebIDLParser.js
+// META: script=/resources/idlharness.js
+
+// https://www.w3.org/TR/geolocation-API/
+
+promise_test(async () => {
+  const idl = await fetch('/interfaces/geolocation-API.idl').then(r => r.text());
+  const html = await fetch('/interfaces/html.idl').then(r => r.text());
+
+  const idl_array = new IdlArray();
+  idl_array.add_idls(idl);
+  idl_array.add_dependency_idls(html);
+  idl_array.add_objects({
+    Navigator: ["navigator"],
+    Geolocation: ["navigator.geolocation"]
+  });
+  idl_array.test();
+}, 'geolocation-API interfaces');
