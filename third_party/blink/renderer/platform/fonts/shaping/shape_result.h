6f89c4fb4c5072638d5ced31d35bb25d49d9c5ea
Emil A Eklund <eae@chromium.org>
2018-07-16 15:20:31 +0000

    [LayoutNG] Optimize ShapingLineBreaker::ShapeToEnd
    Optimize ShapingLineBreaker::ShapeToEnd by handling the common case when
    an entire shape result can be reused thereby avoiding range comparisons,
    memory allocation, and the entire process of copying all the glyph data.
    
    This is especially common for complex scripts and results in a ~10% perf
    win on the layout/hindi-line-layout performance test.
    
    Test: perf_tests/layout/hindi-line-layout.html
    Bug: 636993
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng
    Change-Id: If90768aebe9007bf18e9beaa18554ef0efceab49
    Reviewed-on: https://chromium-review.googlesource.com/1137796
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575273}

--- third_party/blink/renderer/platform/fonts/shaping/shape_result.h
+++ third_party/blink/renderer/platform/fonts/shaping/shape_result.h
@@ -75,6 +75,9 @@ class PLATFORM_EXPORT ShapeResult : public RefCounted<ShapeResult> {
                                     TextDirection direction) {
     return base::AdoptRef(new ShapeResult(font, num_characters, direction));
   }
+  static scoped_refptr<ShapeResult> Create(const ShapeResult& other) {
+    return base::AdoptRef(new ShapeResult(other));
+  }
   static scoped_refptr<ShapeResult> CreateForTabulationCharacters(
       const Font*,
       const TextRun&,
@@ -211,9 +214,6 @@ class PLATFORM_EXPORT ShapeResult : public RefCounted<ShapeResult> {
                                            TextDirection direction) {
     return base::AdoptRef(
         new ShapeResult(font_data, num_characters, direction));
-  }
-  static scoped_refptr<ShapeResult> Create(const ShapeResult& other) {
-    return base::AdoptRef(new ShapeResult(other));
   }
 
   struct GlyphIndexResult {
