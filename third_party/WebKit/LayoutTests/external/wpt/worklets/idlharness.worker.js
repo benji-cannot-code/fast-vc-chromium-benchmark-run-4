593d3d5fd13a5c67ccfbd9d98cd23b576bed026a
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-07-16 14:05:20 +0000

    Import wpt@f582e7ad3b06d37fa65957aa93dc62bf7429e803
    Using wpt-import in Chromium bfcd1e5a550464c20cdf5dcb81a2998fa5e7bf7d.
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/21460
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    worker-dev@chromium.org:
      external/wpt/worklets
    
    TBR=raphael.kubo.da.costa@intel.com
    
    No-Export: true
    Change-Id: Ib412afd3619e9c72774cf9d6c7375a31466305a8
    Reviewed-on: https://chromium-review.googlesource.com/1138173
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575245}

---
+++ third_party/WebKit/LayoutTests/external/wpt/worklets/idlharness.worker.js
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+importScripts("/resources/testharness.js");
+importScripts("/resources/WebIDLParser.js", "/resources/idlharness.js");
+
+// https://drafts.css-houdini.org/worklets/
+
+promise_test(async () => {
+  const idl = await fetch('/interfaces/worklets.idl').then(r => r.text());
+
+  const idl_array = new IdlArray();
+  idl_array.add_idls(idl);
+  idl_array.test();
+}, 'worklets interfaces');
+done();
