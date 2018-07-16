77142a4da907e2a82704ce55a5a79f6b3730fc81
lanwei <lanwei@chromium.org>
2018-07-16 19:33:10 +0000

    Make sure that the corresponding pointer events are sent when pressing eraser button
    When we press an eraser button of a stylus, we should send the missing
    pointerdown, pointerup, gotpointercapture and lostpointercapture events.
    
    Bug: 851457
    Change-Id: Iff45a897a87ef03d73cc308d84e874dbc1310fa1
    Reviewed-on: https://chromium-review.googlesource.com/1129619
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Navid Zolghadr <nzolghadr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575384}

--- third_party/blink/renderer/core/events/pointer_event_factory.cc
+++ third_party/blink/renderer/core/events/pointer_event_factory.cc
@@ -193,14 +193,6 @@ PointerEvent* PointerEventFactory::Create(
   SetIdTypeButtons(pointer_event_init, web_pointer_event);
 
   AtomicString type = PointerEventNameForEventType(event_type);
-  // Make sure chorded buttons fire pointermove instead of pointerup/down.
-  if ((event_type == WebInputEvent::kPointerDown &&
-       (pointer_event_init.buttons() &
-        ~ButtonToButtonsBitfield(web_pointer_event.button)) != 0) ||
-      (event_type == WebInputEvent::kPointerUp &&
-       pointer_event_init.buttons() != 0))
-    type = EventTypeNames::pointermove;
-
   if (event_type == WebInputEvent::kPointerDown ||
       event_type == WebInputEvent::kPointerUp) {
     WebPointerProperties::Button button = web_pointer_event.button;
@@ -210,6 +202,14 @@ PointerEvent* PointerEventFactory::Create(
         button == WebPointerProperties::Button::kLeft)
       button = WebPointerProperties::Button::kEraser;
     pointer_event_init.setButton(static_cast<int>(button));
+
+    // Make sure chorded buttons fire pointermove instead of pointerup/down.
+    if ((event_type == WebInputEvent::kPointerDown &&
+         (pointer_event_init.buttons() & ~ButtonToButtonsBitfield(button)) !=
+             0) ||
+        (event_type == WebInputEvent::kPointerUp &&
+         pointer_event_init.buttons() != 0))
+      type = EventTypeNames::pointermove;
   } else {
     pointer_event_init.setButton(
         static_cast<int>(WebPointerProperties::Button::kNoButton));
