2d5d9127e43df6714f76615dd4fc158c3682cd23
Owen Min <zmin@chromium.org>
2018-07-16 17:16:01 +0000

    Disable testToolbarSwipePrevTab test.
    Tbr: skyostil@chromium.org
    Bug: 863676
    Change-Id: I00165973f5135273938f5ca23362c7964c69c1fd
    Reviewed-on: https://chromium-review.googlesource.com/1138409
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575321}

--- chrome/android/javatests/src/org/chromium/chrome/browser/TabsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/TabsTest.java
@@ -1490,6 +1490,7 @@ public class TabsTest {
     @Feature({"Android-TabSwitcher"})
     @Restriction(RESTRICTION_TYPE_NON_LOW_END_DEVICE)
     @RetryOnFailure
+    @DisabledTest(message = "crbug.com/863676")
     public void testToolbarSwipePrevTab() throws InterruptedException, TimeoutException {
         ChromeTabUtils.newTabFromMenu(
                 InstrumentationRegistry.getInstrumentation(), mActivityTestRule.getActivity());
