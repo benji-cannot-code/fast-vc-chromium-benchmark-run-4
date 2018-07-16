366ac03010c6d84a219d173e753f82d82684e5d1
Daniel Bratell <bratell@opera.com>
2018-07-16 19:22:48 +0000

    Use :: prefix to distinguish between ::media_router::prefs and ::prefs
    There are two namespaces named prefs in scope inside media_router code:
    The global ::prefs namespace and the local ::media_router::prefs namespace.
    
    If both namespaces are known to the compiler, as happens in jumbo builds,
    then the name lookup will be ambigious and the compiler will fail to
    compile the code.
    
    Bug: 803406
    Change-Id: Ib4be547ffe1ba7a790c2003fd54bd091935c378e
    Reviewed-on: https://chromium-review.googlesource.com/1138244
    Commit-Queue: mark a. foltz <mfoltz@chromium.org>
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575379}

--- chrome/browser/ui/views/media_router/media_remoting_dialog_view.cc
+++ chrome/browser/ui/views/media_router/media_remoting_dialog_view.cc
@@ -33,7 +33,7 @@ void MediaRemotingDialogView::GetPermission(content::WebContents* web_contents,
           ->GetPrefs();
   DCHECK(pref_service);
   const PrefService::Preference* pref =
-      pref_service->FindPreference(prefs::kMediaRouterMediaRemotingEnabled);
+      pref_service->FindPreference(::prefs::kMediaRouterMediaRemotingEnabled);
   if (pref && !pref->IsDefaultValue()) {
     std::move(callback).Run(pref->GetValue()->GetBool());
     return;
@@ -158,8 +158,10 @@ void MediaRemotingDialogView::WindowClosing() {
 void MediaRemotingDialogView::ReportPermission(bool allowed) {
   DCHECK(remember_choice_checkbox_);
   DCHECK(permission_callback_);
-  if (remember_choice_checkbox_->checked())
-    pref_service_->SetBoolean(prefs::kMediaRouterMediaRemotingEnabled, allowed);
+  if (remember_choice_checkbox_->checked()) {
+    pref_service_->SetBoolean(::prefs::kMediaRouterMediaRemotingEnabled,
+                              allowed);
+  }
   std::move(permission_callback_).Run(allowed);
 }
 
