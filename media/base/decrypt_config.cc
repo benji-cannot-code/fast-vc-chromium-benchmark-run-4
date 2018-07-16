451205eac8597fb083cf8581f99f0391a5391311
Rintaro Kuroiwa <rkuroiwa@chromium.org>
2018-07-16 19:38:46 +0000

    Handle encrypted stream in H264Decoder
    - Handle the decrypt config passed to SetStream().
    - The config is copied to CodecPictures and passed to the accelerator.
    
    Bug: 820192
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: Ib3337b4bc2f972d410a07bc5af6f67a7ef2a9a2d
    Reviewed-on: https://chromium-review.googlesource.com/1133974
    Commit-Queue: Rintaro Kuroiwa <rkuroiwa@chromium.org>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575385}

--- media/base/decrypt_config.cc
+++ media/base/decrypt_config.cc
@@ -73,7 +73,7 @@ DecryptConfig::DecryptConfig(
 
 DecryptConfig::~DecryptConfig() = default;
 
-std::unique_ptr<DecryptConfig> DecryptConfig::Clone() {
+std::unique_ptr<DecryptConfig> DecryptConfig::Clone() const {
   return base::WrapUnique(new DecryptConfig(*this));
 }
 
