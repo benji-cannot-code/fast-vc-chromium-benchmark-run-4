a589c8f9071615f3a8aa58fe4181436c9e7d3be3
Sandra Sun <sunyunjia@chromium.org>
2018-07-16 01:04:21 +0000

    Snaps after scrollbar clicking.
    After user clicking on the scrollbar's track or arrows, we should snap
    to the closest snap position of the container. This is done by simply
    moving SnapAfterScrollbarDragging() from
    ScrollableArea::MouseReleasedScrollbar() up to Scrollbar::MouseUp().
    
    This patch also rewrites the previous
    snaps-after-scrollbar-dragging.html test using the gesture-util.js, and
    adds the new clicking tests to it as well.
    
    Bug: 860768
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I75267c3fc0917ecf5c14673a1eb480a047d03824
    Reviewed-on: https://chromium-review.googlesource.com/1127153
    Commit-Queue: Sandra Sun <sunyunjia@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Majid Valipour <majidvp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575191}

--- third_party/blink/renderer/platform/scroll/scrollable_area.h
+++ third_party/blink/renderer/platform/scroll/scrollable_area.h
@@ -114,11 +114,11 @@ class PLATFORM_EXPORT ScrollableArea : public GarbageCollectedMixin {
   void MouseEnteredScrollbar(Scrollbar&);
   void MouseExitedScrollbar(Scrollbar&);
   void MouseCapturedScrollbar();
-  void MouseReleasedScrollbar(ScrollbarOrientation);
+  void MouseReleasedScrollbar();
   void ContentAreaDidShow() const;
   void ContentAreaDidHide() const;
 
-  virtual void SnapAfterScrollbarDragging(ScrollbarOrientation) {}
+  virtual void SnapAfterScrollbarScrolling(ScrollbarOrientation) {}
 
   void FinishCurrentScrollAnimations() const;
 
