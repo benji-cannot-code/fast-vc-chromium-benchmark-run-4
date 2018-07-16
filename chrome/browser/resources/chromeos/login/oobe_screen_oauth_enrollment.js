d6922119db9e60c7e8df95552a410923b09d80f7
Roman Sorokin <rsorokin@chromium.org>
2018-07-16 15:59:51 +0000

    Cros: Fix ESC behaviour for the Enrollment Flow
    * Restarts Chrome for Active Directory at the end of successful
    enrollment
    * ESC not to cancel enrollment during next steps: "license",
    "attribute-prompt"
    
    TEST=manual
    
    Bug: 855379
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: Icaa3dc0f87bfa75c57c8b85c1b410b246fc194e6
    Reviewed-on: https://chromium-review.googlesource.com/1118176
    Commit-Queue: Roman Sorokin <rsorokin@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575285}

--- chrome/browser/resources/chromeos/login/oobe_screen_oauth_enrollment.js
+++ chrome/browser/resources/chromeos/login/oobe_screen_oauth_enrollment.js
@@ -296,7 +296,6 @@ login.createScreen('OAuthEnrollmentScreen', 'oauth-enrollment', function() {
             'mode-' + modes[i], data.enrollment_mode == modes[i]);
       }
       this.isManualEnrollment_ = data.enrollment_mode === 'manual';
-      this.isCancelDisabled = true;
       this.navigation_.disabled = false;
 
       this.showStep(data.attestationBased ? STEP_WORKING : STEP_SIGNIN);
@@ -332,10 +331,6 @@ login.createScreen('OAuthEnrollmentScreen', 'oauth-enrollment', function() {
      * screen (either the next authentication or the login screen).
      */
     cancel: function() {
-      if (this.currentStep_ == STEP_WORKING ||
-          this.currentStep_ == STEP_AD_JOIN) {
-        return;
-      }
       if (this.isCancelDisabled)
         return;
       this.isCancelDisabled = true;
@@ -384,6 +379,9 @@ login.createScreen('OAuthEnrollmentScreen', 'oauth-enrollment', function() {
       this.classList.toggle('oauth-enroll-state-' + this.currentStep_, false);
       this.classList.toggle('oauth-enroll-state-' + step, true);
 
+      this.isCancelDisabled =
+          (step == STEP_SIGNIN && !this.isManualEnrollment_) ||
+          step == STEP_AD_JOIN || step == STEP_WORKING;
       if (step == STEP_SIGNIN) {
         $('oauth-enroll-auth-view').focus();
       } else if (step == STEP_LICENSE_TYPE) {
@@ -427,7 +425,6 @@ login.createScreen('OAuthEnrollmentScreen', 'oauth-enrollment', function() {
         this.showStep(STEP_ACTIVE_DIRECTORY_JOIN_ERROR);
         return;
       }
-      this.isCancelDisabled_ = false;  // Re-enable if called before Gaia loads.
       $('oauth-enroll-error-card').textContent = message;
       $('oauth-enroll-error-card').buttonLabel =
           retry ? loadTimeData.getString('oauthEnrollRetry') : '';
