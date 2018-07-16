3f4bbd795eb1fb7c4cd8ee6f8bed77bb20fd9330
Chris Sharp <csharp@chromium.org>
2018-07-16 20:41:44 +0000

    Move empty_dll, scoped_file and test_strings to the Chromium repo
    Bug: 830892
    Change-Id: I0621b9b69cd84e539231e535562f7c34b75fa1ca
    Reviewed-on: https://chromium-review.googlesource.com/1136941
    Commit-Queue: Chris Sharp <csharp@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575412}

---
+++ chrome/chrome_cleaner/test/scoped_file.h
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_CHROME_CLEANER_TEST_SCOPED_FILE_H_
+#define CHROME_CHROME_CLEANER_TEST_SCOPED_FILE_H_
+
+#include <memory>
+#include <string>
+
+#include "base/files/file_path.h"
+#include "base/strings/string16.h"
+
+// Holds the path for a file that gets deleted on destruction.
+class ScopedFile {
+ public:
+  static std::unique_ptr<ScopedFile> Create(const base::FilePath& dir,
+                                            const base::string16& file_name,
+                                            const std::string& contents);
+
+  explicit ScopedFile(const base::FilePath& file_path);
+
+  // Deletes the file with path |file_path_| if it exists. If the file doesn't
+  // exist on destruction (which can be the case for tests involving UwS
+  // removals), the destructor is a no-op.
+  ~ScopedFile();
+
+  const base::FilePath& file_path();
+
+ private:
+  base::FilePath file_path_;
+};
+
+#endif  // CHROME_CHROME_CLEANER_TEST_SCOPED_FILE_H_
