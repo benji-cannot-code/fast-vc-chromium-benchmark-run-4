c44145e9809420be6f4921eeb1b4cb26b50057c8
Dale Curtis <dalecurtis@chromium.org>
2018-07-16 19:19:02 +0000

    Don't use IsOnBatteryPower as initial "has controls" value :|
    Whoops. No test unfortunately since there's no good way to test
    this other than hard coding the expected default values in the
    test itself, which is generally not a good test.
    
    BUG=779192
    TEST=none
    TBR=mlamouri
    
    Change-Id: I00d26ef715fac4884017ce295639ee6aab37d777
    Reviewed-on: https://chromium-review.googlesource.com/1138648
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575376}

--- media/blink/watch_time_reporter.cc
+++ media/blink/watch_time_reporter.cc
@@ -510,7 +510,7 @@ WatchTimeReporter::CreateControlsComponent() {
                                              FOREGROUND_KEY(NativeControlsOff)};
 
   return std::make_unique<WatchTimeComponent<bool>>(
-      IsOnBatteryPower(), std::move(keys_to_finalize),
+      false, std::move(keys_to_finalize),
       base::BindRepeating(&WatchTimeReporter::GetControlsKey,
                           base::Unretained(this)),
       get_media_time_cb_, recorder_.get());
