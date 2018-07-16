8e60488c67edba2c8ad59d545105ccf5c9fbf932
Ken Rockot <rockot@chromium.org>
2018-07-16 21:32:19 +0000

    [content-service] View -> NavigableContents
    The term "View" is too overloaded and has been deemed confusing
    as the root of so many names central to the Content Service.
    It also tends to imply visual representation, and the central concept
    of a navigable-thing encapsulated by the Content Service client library
    does not in fact require any associated visual representation. Better
    to rename now than later, sooo...
    
    This renames View to NavigableContents, a name which accurately
    embodies the purpose of the thing without colliding other names or
    implying more meaning than it should.
    
    Also updates some documentation and adds a few missing class comments.
    
    No real functional changes.
    
    TBR=rsesek@chromium.org
    
    Bug: None
    Change-Id: I8576b62f622116be5effaf4963fc0ea746183931
    Reviewed-on: https://chromium-review.googlesource.com/1135915
    Commit-Queue: Ken Rockot <rockot@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575430}

---
+++ services/content/navigable_contents_factory_impl.h
@@ -0,0 +1,43 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SERVICES_CONTENT_NAVIGABLE_CONTENTS_FACTORY_IMPL_H_
+#define SERVICES_CONTENT_NAVIGABLE_CONTENTS_FACTORY_IMPL_H_
+
+#include "base/macros.h"
+#include "mojo/public/cpp/bindings/binding.h"
+#include "services/content/public/mojom/navigable_contents_factory.mojom.h"
+
+namespace content {
+
+class Service;
+
+// An implementation of the NavigableContentsFactory which backs every connected
+// NavigableContentsFactory interface in a Content Service client. This creates
+// instances of NavigableContentsImpl to fulfill |CreateContents()| requests.
+//
+// Instances of this class, and of the NavigableContentsImpls it creates, are
+// all managed by the Service instance.
+class NavigableContentsFactoryImpl : public mojom::NavigableContentsFactory {
+ public:
+  NavigableContentsFactoryImpl(Service* service,
+                               mojom::NavigableContentsFactoryRequest request);
+  ~NavigableContentsFactoryImpl() override;
+
+ private:
+  // mojom::NavigableContentsFactory:
+  void CreateContents(mojom::NavigableContentsParamsPtr params,
+                      mojom::NavigableContentsRequest request,
+                      mojom::NavigableContentsClientPtr client) override;
+
+  Service* const service_;
+  mojo::Binding<mojom::NavigableContentsFactory> binding_;
+
+  DISALLOW_COPY_AND_ASSIGN(NavigableContentsFactoryImpl);
+};
+
+}  // namespace content
+
+#endif  // SERVICES_CONTENT_NAVIGABLE_CONTENTS_FACTORY_IMPL_H_
--- services/content/service_delegate.h
+++ services/content/service_delegate.h
@@ -6,12 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SERVICES_CONTENT_SERVICE_DELEGATE_H_
 #define SERVICES_CONTENT_SERVICE_DELEGATE_H_
 
-#include "services/content/public/mojom/view.mojom.h"
+#include "services/content/public/mojom/navigable_contents.mojom.h"
 
 namespace content {
 
+class NavigableContentsDelegate;
 class Service;
-class ViewDelegate;
 
 // This is a delegate interface which allows the Content Service implementation
 // to delegate out to private src/content code without a circular dependency
@@ -29,12 +29,12 @@ class ServiceDelegate {
   // delegate) is about to be destroyed.
   virtual void WillDestroyServiceInstance(Service* service) = 0;
 
-  // Constructs a new ViewDelegate implementation to back a new ContentViewImpl
-  // instance, servicing a client's ContentView. |client| is a ViewClient
-  // interface the implementation can use to communicate with the client of this
-  // view.
-  virtual std::unique_ptr<ViewDelegate> CreateViewDelegate(
-      mojom::ViewClient* client) = 0;
+  // Constructs a new NavigableContentsDelegate implementation to back a new
+  // NavigableContentsImpl instance, servicing a client's NavigableContents.
+  // |client| is a NavigableContentsClient interface the implementation can use
+  // to communicate with the client of this contents.
+  virtual std::unique_ptr<NavigableContentsDelegate>
+  CreateNavigableContentsDelegate(mojom::NavigableContentsClient* client) = 0;
 };
 
 };  // namespace content
