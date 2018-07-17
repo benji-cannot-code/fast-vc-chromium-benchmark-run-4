95e312329fe32d536ba4a0b2c30e11b19e1439d6
Daniel Rubery <drubery@chromium.org>
2018-07-17 21:55:21 +0000

    Add timestamp to FindNavigationEvent and FindRetargetingNavigationEvent
    In order to prevent Referrer chains from looping, add a timestamp
    argument to both FindNavigationEvent and FindRetargetingNavigationEvent.
    Then only allow the next event if it precedes the previous event
    chronologically.
    
    Bug: 864234
    Change-Id: Ie5752096eb2571ecf24bdf22057ebc5c4d87adbd
    Reviewed-on: https://chromium-review.googlesource.com/1141058
    Reviewed-by: Jialiu Lin <jialiul@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575790}

--- chrome/browser/safe_browsing/safe_browsing_navigation_observer_manager.cc
+++ chrome/browser/safe_browsing/safe_browsing_navigation_observer_manager.cc
@@ -118,6 +118,7 @@ NavigationEventList::NavigationEventList(std::size_t size_limit)
 NavigationEventList::~NavigationEventList() {}
 
 NavigationEvent* NavigationEventList::FindNavigationEvent(
+    const base::Time& last_event_timestamp,
     const GURL& target_url,
     const GURL& target_main_frame_url,
     SessionID target_tab_id) {
@@ -133,6 +134,11 @@ NavigationEvent* NavigationEventList::FindNavigationEvent(
   for (auto rit = navigation_events_.rbegin(); rit != navigation_events_.rend();
        ++rit) {
     auto* nav_event = rit->get();
+
+    // The next event cannot come before the previous one.
+    if (nav_event->last_updated > last_event_timestamp)
+      continue;
+
     // If tab id is not valid, we only compare url, otherwise we compare both.
     if (nav_event->GetDestinationUrl() == search_url &&
         (!target_tab_id.is_valid() ||
@@ -148,7 +154,8 @@ NavigationEvent* NavigationEventList::FindNavigationEvent(
         // need to adjust our search url to the original request.
         if (!nav_event->server_redirect_urls.empty()) {
           NavigationEvent* retargeting_nav_event =
-              FindRetargetingNavigationEvent(nav_event->original_request_url,
+              FindRetargetingNavigationEvent(nav_event->last_updated,
+                                             nav_event->original_request_url,
                                              nav_event->target_tab_id);
           if (!retargeting_nav_event)
             return nullptr;
@@ -168,6 +175,7 @@ NavigationEvent* NavigationEventList::FindNavigationEvent(
 }
 
 NavigationEvent* NavigationEventList::FindRetargetingNavigationEvent(
+    const base::Time& last_event_timestamp,
     const GURL& target_url,
     SessionID target_tab_id) {
   if (target_url.is_empty())
@@ -178,6 +186,11 @@ NavigationEvent* NavigationEventList::FindRetargetingNavigationEvent(
   for (auto rit = navigation_events_.rbegin(); rit != navigation_events_.rend();
        ++rit) {
     auto* nav_event = rit->get();
+
+    // The next event cannot come before the previous one.
+    if (nav_event->last_updated > last_event_timestamp)
+      continue;
+
     // In addition to url and tab_id checking, we need to compare the
     // source_tab_id and target_tab_id to make sure it is a retargeting event.
     if (nav_event->original_request_url == target_url &&
@@ -275,7 +288,6 @@ void SafeBrowsingNavigationObserverManager::SanitizeReferrerChain(
 
 SafeBrowsingNavigationObserverManager::SafeBrowsingNavigationObserverManager()
     : navigation_event_list_(kNavigationRecordMaxSize) {
-
   // Schedule clean up in 2 minutes.
   ScheduleNextCleanUpAfterInterval(
       base::TimeDelta::FromSecondsD(kNavigationFootprintTTLInSecond));
@@ -358,7 +370,7 @@ SafeBrowsingNavigationObserverManager::IdentifyReferrerChainByEventURL(
     return INVALID_URL;
 
   NavigationEvent* nav_event = navigation_event_list_.FindNavigationEvent(
-      ClearURLRef(event_url), GURL(), event_tab_id);
+      base::Time::Now(), ClearURLRef(event_url), GURL(), event_tab_id);
   if (!nav_event) {
     // We cannot find a single navigation event related to this event.
     return NAVIGATION_EVENT_NOT_FOUND;
@@ -402,8 +414,8 @@ SafeBrowsingNavigationObserverManager::IdentifyReferrerChainByHostingPage(
     return INVALID_URL;
 
   NavigationEvent* nav_event = navigation_event_list_.FindNavigationEvent(
-      ClearURLRef(initiating_frame_url), ClearURLRef(initiating_main_frame_url),
-      tab_id);
+      base::Time::Now(), ClearURLRef(initiating_frame_url),
+      ClearURLRef(initiating_main_frame_url), tab_id);
   if (!nav_event) {
     // We cannot find a single navigation event related to this hosting page.
     return NAVIGATION_EVENT_NOT_FOUND;
@@ -638,6 +650,7 @@ void SafeBrowsingNavigationObserverManager::GetRemainingReferrerChain(
     // Back trace to the next nav_event that was initiated by the user.
     while (!last_nav_event_traced->IsUserInitiated()) {
       last_nav_event_traced = navigation_event_list_.FindNavigationEvent(
+          last_nav_event_traced->last_updated,
           last_nav_event_traced->source_url,
           last_nav_event_traced->source_main_frame_url,
           last_nav_event_traced->source_tab_id);
@@ -664,7 +677,7 @@ void SafeBrowsingNavigationObserverManager::GetRemainingReferrerChain(
     }
 
     last_nav_event_traced = navigation_event_list_.FindNavigationEvent(
-        last_nav_event_traced->source_url,
+        last_nav_event_traced->last_updated, last_nav_event_traced->source_url,
         last_nav_event_traced->source_main_frame_url,
         last_nav_event_traced->source_tab_id);
     if (!last_nav_event_traced)
