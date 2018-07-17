f0da6139ab92af5298500cad20b389c143559cfd
Naoki Fukino <fukino@chromium.org>
2018-07-17 13:21:26 +0000

    Files app: Show "Play files" root by default.
    Bug: 864449
    Test: Manually tested both Enable/Disable options.
    Change-Id: I57a768421e3a86e1bae63c72f1467911ddb6096f
    Reviewed-on: https://chromium-review.googlesource.com/1140110
    Reviewed-by: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Commit-Queue: Naoki Fukino <fukino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575623}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -3002,10 +3002,12 @@ const FeatureEntry kFeatureEntries[] = {
      ENABLE_DISABLE_VALUE_TYPE(
          chromeos::switches::kEnableFileManagerTouchMode,
          chromeos::switches::kDisableFileManagerTouchMode)},
-    {"show-android-files-in-files-app",
+    {"android-files-in-files-app",
      flag_descriptions::kShowAndroidFilesInFilesAppName,
      flag_descriptions::kShowAndroidFilesInFilesAppDescription, kOsCrOS,
-     SINGLE_VALUE_TYPE(chromeos::switches::kShowAndroidFilesInFilesApp)},
+     ENABLE_DISABLE_VALUE_TYPE(
+         chromeos::switches::kShowAndroidFilesInFilesApp,
+         chromeos::switches::kHideAndroidFilesInFilesApp)},
     {"disable-my-files-navigation",
      flag_descriptions::kFilesAppDisableMyFilesNavigationName,
      flag_descriptions::kFilesAppDisableMyFilesNavigationDescription, kOsCrOS,
