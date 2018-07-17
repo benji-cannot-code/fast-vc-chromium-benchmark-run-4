b3b8231631814f94a2d8991ce0d8c99b3c384fe9
Xiaocheng Hu <xiaochengh@chromium.org>
2018-07-17 23:46:26 +0000

    Revert "Return unclipped bounds for get_characterExtents"
    This reverts commit 84c9263088796f6afea44be5a0db02de8e81b572.
    
    Reason for revert: Suspected to cause compile failure at https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Android%20Release%20%28Nexus%205X%29/8033
    
    Original change's description:
    > Return unclipped bounds for get_characterExtents
    > 
    > For get_characterExtents():
    > - The x,y position should be unclipped (allowed to be negative or larger than the screen width/height).
    > - The width/height should not be changed to 1 for offscreen objects.
    > 
    > Bug: 864195
    > Change-Id: Ibea1f3ecb0b8f5f72c6fc967892ac9e6b1af135b
    > Reviewed-on: https://chromium-review.googlesource.com/1138800
    > Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    > Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#575831}
    
    TBR=dmazzoni@chromium.org,aleventhal@chromium.org
    
    Change-Id: I5efff2e628b9518039777b8317a581c4c0bc326e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 864195
    Reviewed-on: https://chromium-review.googlesource.com/1140459
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575842}

--- content/browser/accessibility/browser_accessibility.cc
+++ content/browser/accessibility/browser_accessibility.cc
@@ -344,9 +344,8 @@ gfx::Rect BrowserAccessibility::GetPageBoundsRect(bool* offscreen,
   return RelativeToAbsoluteBounds(gfx::RectF(), false, offscreen, clip_bounds);
 }
 
-gfx::Rect BrowserAccessibility::GetPageBoundsForRange(int start,
-                                                      int len,
-                                                      bool clipped) const {
+gfx::Rect BrowserAccessibility::GetPageBoundsForRange(int start, int len)
+    const {
   DCHECK_GE(start, 0);
   DCHECK_GE(len, 0);
 
@@ -466,11 +465,9 @@ gfx::Rect BrowserAccessibility::GetPageBoundsForRange(int start,
       }
     }
 
-    // Don't clip bounds. Some screen magnifiers (e.g. ZoomText) prefer to
-    // get unclipped bounds so that they can make smooth scrolling calculations.
     gfx::Rect absolute_child_rect = child->RelativeToAbsoluteBounds(
         child_overlap_rect, false /* frame_only */, nullptr /* offscreen */,
-        clipped /* clip_bounds */);
+        true /* clip_bounds */);
     if (bounds.width() == 0 && bounds.height() == 0) {
       bounds = absolute_child_rect;
     } else {
@@ -481,10 +478,9 @@ gfx::Rect BrowserAccessibility::GetPageBoundsForRange(int start,
   return bounds;
 }
 
-gfx::Rect BrowserAccessibility::GetScreenBoundsForRange(int start,
-                                                        int len,
-                                                        bool clipped) const {
-  gfx::Rect bounds = GetPageBoundsForRange(start, len, clipped);
+gfx::Rect BrowserAccessibility::GetScreenBoundsForRange(int start, int len)
+    const {
+  gfx::Rect bounds = GetPageBoundsForRange(start, len);
 
   // Adjust the bounds by the top left corner of the containing view's bounds
   // in screen coordinates.
