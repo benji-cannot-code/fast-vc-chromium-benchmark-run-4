94d7166ed820e94ac46e66350a5dd946f41463b8
Marcin Wiacek <marcin@mwiacek.com>
2018-07-17 22:48:39 +0000

    @IntDef cleanup in the chrome/java/customtabs
    @IntDef/@StringDef annotation are preferred way for declaring
    set of String/int values
    
    1. they need less space in APK than enum, see
    https://developer.android.com/topic/performance/reduce-apk-size#remove-enums
    2. they give more control over allowed values than "static final" values
    
    Main goal of patch is writing "static final" values, enum
    and some classes in one common @IntDef/@StringDef form:
    
    1. with @IntDef/@StringDef first, @Retention second
       and related @interface third
    2. with values inside @interface
    3. with NUM_ENTRIES declaring number of entries if necessary
    4. with comment about numbering from 0 without gaps when necessary
    5. with @Retention(RetentionPolicy.SOURCE)
    6. without "static final" in the @interface
    
    Change-Id: Icba90d7f019ce799fbcdd07849c685c44e0a0243
    Reviewed-on: https://chromium-review.googlesource.com/1133984
    Commit-Queue: Marcin Wiącek <marcin@mwiacek.com>
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575824}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabAppMenuPropertiesDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabAppMenuPropertiesDelegate.java
@@ -5,12 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.customtabs;
 
-import static org.chromium.chrome.browser.customtabs.CustomTabIntentDataProvider.CUSTOM_TABS_UI_TYPE_MEDIA_VIEWER;
-import static org.chromium.chrome.browser.customtabs.CustomTabIntentDataProvider.CUSTOM_TABS_UI_TYPE_MINIMAL_UI_WEBAPP;
-import static org.chromium.chrome.browser.customtabs.CustomTabIntentDataProvider.CUSTOM_TABS_UI_TYPE_OFFLINE_PAGE;
-import static org.chromium.chrome.browser.customtabs.CustomTabIntentDataProvider.CUSTOM_TABS_UI_TYPE_PAYMENT_REQUEST;
-import static org.chromium.chrome.browser.customtabs.CustomTabIntentDataProvider.CUSTOM_TABS_UI_TYPE_READER_MODE;
-
 import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -86,7 +80,7 @@ public class CustomTabAppMenuPropertiesDelegate extends AppMenuPropertiesDelegat
             boolean addToHomeScreenVisible = true;
             boolean requestDesktopSiteVisible = true;
 
-            if (mUiType == CUSTOM_TABS_UI_TYPE_MEDIA_VIEWER) {
+            if (mUiType == CustomTabsUiType.MEDIA_VIEWER) {
                 // Most of the menu items don't make sense when viewing media.
                 menu.findItem(R.id.icon_row_menu_id).setVisible(false);
                 menu.findItem(R.id.find_in_page_id).setVisible(false);
@@ -95,7 +89,7 @@ public class CustomTabAppMenuPropertiesDelegate extends AppMenuPropertiesDelegat
                 openInChromeItemVisible = false;
                 requestDesktopSiteVisible = false;
                 addToHomeScreenVisible = false;
-            } else if (mUiType == CUSTOM_TABS_UI_TYPE_PAYMENT_REQUEST) {
+            } else if (mUiType == CustomTabsUiType.PAYMENT_REQUEST) {
                 // Only the icon row and 'find in page' are shown for opening payment request UI
                 // from Chrome.
                 openInChromeItemVisible = false;
@@ -103,7 +97,7 @@ public class CustomTabAppMenuPropertiesDelegate extends AppMenuPropertiesDelegat
                 addToHomeScreenVisible = false;
                 downloadItemVisible = false;
                 bookmarkItemVisible = false;
-            } else if (mUiType == CUSTOM_TABS_UI_TYPE_READER_MODE) {
+            } else if (mUiType == CustomTabsUiType.READER_MODE) {
                 // Only 'find in page' and the reader mode preference are shown for Reader Mode UI.
                 menu.findItem(R.id.icon_row_menu_id).setVisible(false);
                 bookmarkItemVisible = false; // Set to skip initialization.
@@ -113,12 +107,12 @@ public class CustomTabAppMenuPropertiesDelegate extends AppMenuPropertiesDelegat
                 addToHomeScreenVisible = false;
 
                 menu.findItem(R.id.reader_mode_prefs_id).setVisible(true);
-            } else if (mUiType == CUSTOM_TABS_UI_TYPE_MINIMAL_UI_WEBAPP) {
+            } else if (mUiType == CustomTabsUiType.MINIMAL_UI_WEBAPP) {
                 requestDesktopSiteVisible = false;
                 addToHomeScreenVisible = false;
                 downloadItemVisible = false;
                 bookmarkItemVisible = false;
-            } else if (mUiType == CUSTOM_TABS_UI_TYPE_OFFLINE_PAGE) {
+            } else if (mUiType == CustomTabsUiType.OFFLINE_PAGE) {
                 openInChromeItemVisible = false;
                 bookmarkItemVisible = true;
                 downloadItemVisible = false;
@@ -190,8 +184,7 @@ public class CustomTabAppMenuPropertiesDelegate extends AppMenuPropertiesDelegat
     @Override
     public int getFooterResourceId() {
         // Avoid showing the branded menu footer for media and offline pages.
-        if (mUiType == CUSTOM_TABS_UI_TYPE_MEDIA_VIEWER
-                || mUiType == CUSTOM_TABS_UI_TYPE_OFFLINE_PAGE) {
+        if (mUiType == CustomTabsUiType.MEDIA_VIEWER || mUiType == CustomTabsUiType.OFFLINE_PAGE) {
             return 0;
         }
         return R.layout.powered_by_chrome_footer;
--- chrome/android/java/src/org/chromium/chrome/browser/webapps/WebappActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/WebappActivity.java
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.webapps;
 
-import static org.chromium.chrome.browser.customtabs.CustomTabIntentDataProvider.CUSTOM_TABS_UI_TYPE_MINIMAL_UI_WEBAPP;
-
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -56,6 +54,7 @@ import org.chromium.chrome.browser.browserservices.TrustedWebActivityDisclosure;
 import org.chromium.chrome.browser.browserservices.UkmRecorder;
 import org.chromium.chrome.browser.compositor.layouts.LayoutManager;
 import org.chromium.chrome.browser.customtabs.CustomTabAppMenuPropertiesDelegate;
+import org.chromium.chrome.browser.customtabs.CustomTabIntentDataProvider;
 import org.chromium.chrome.browser.customtabs.CustomTabNavigationEventObserver;
 import org.chromium.chrome.browser.customtabs.CustomTabsConnection;
 import org.chromium.chrome.browser.document.ChromeLauncherActivity;
@@ -623,7 +622,8 @@ public class WebappActivity extends SingleTabActivity {
 
     @Override
     protected AppMenuPropertiesDelegate createAppMenuPropertiesDelegate() {
-        return new CustomTabAppMenuPropertiesDelegate(this, CUSTOM_TABS_UI_TYPE_MINIMAL_UI_WEBAPP,
+        return new CustomTabAppMenuPropertiesDelegate(this,
+                CustomTabIntentDataProvider.CustomTabsUiType.MINIMAL_UI_WEBAPP,
                 new ArrayList<String>(), true /* is opened by Chrome */,
                 true /* should show share */, false /* should show star (bookmarking) */,
                 false /* should show download */);
