1395610b64effc66d6f83acf4e86ab933982136b
Ramin Halavati <rhalavati@chromium.org>
2018-07-17 12:12:58 +0000

    Change incognito preference store default to in-memory.
    The default storage for OverlayUserPrefStore which is used for
    incognito preferences is the persistent storage in the regular profile,
    and preference types that should not be stored there must be enumerated
    in CreateIncognitoPrefServiceSyncable.
    
    This is changed so that the default storage is in memory, and
    preferences that need a persistent storage would need enumeration.
    
    Bug: 861722
    Change-Id: Ia4c8f655752a07f81fb6d35685876cd252118999
    Reviewed-on: https://chromium-review.googlesource.com/1128753
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575613}

--- services/preferences/pref_store_manager_impl.h
+++ services/preferences/pref_store_manager_impl.h
@@ -47,7 +47,7 @@ class PrefStoreManagerImpl : public service_manager::Service {
                        PersistentPrefStore* incognito_user_prefs_underlay,
                        PrefStore* recommended_prefs,
                        PrefRegistry* pref_registry,
-                       std::vector<const char*> overlay_pref_names);
+                       std::vector<const char*> persistent_perf_names);
   ~PrefStoreManagerImpl() override;
 
   base::OnceClosure ShutDownClosure();
@@ -80,7 +80,7 @@ class PrefStoreManagerImpl : public service_manager::Service {
   std::unique_ptr<PersistentPrefStoreImpl> persistent_pref_store_;
   std::unique_ptr<PersistentPrefStoreImpl>
       incognito_persistent_pref_store_underlay_;
-  std::vector<const char*> overlay_pref_names_;
+  std::vector<const char*> persistent_perf_names_;
 
   const std::unique_ptr<SharedPrefRegistry> shared_pref_registry_;
 
