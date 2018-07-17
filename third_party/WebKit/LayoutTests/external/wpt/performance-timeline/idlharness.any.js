b38ecf9416039f7ef89c514706e1cd65f6bf23b1
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-07-17 20:49:48 +0000

    Import wpt@a6230eaa1bb0f58e1744b8163ea73bc09e9a814b
    Using wpt-import in Chromium d74f01b7582589eb9b2607b8881d1ba804792c48.
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/21501
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    igrigorik@chromium.org, panicker@chromium.org:
      external/wpt/performance-timeline
    
    TBR=foolip
    
    No-Export: true
    Change-Id: I3e61664c718581f984e73e916a33d5f81288a6fe
    Reviewed-on: https://chromium-review.googlesource.com/1140776
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575763}

--- third_party/WebKit/LayoutTests/external/wpt/performance-timeline/idlharness.any.js
+++ third_party/WebKit/LayoutTests/external/wpt/performance-timeline/idlharness.any.js
@@ -7,22 +7,47 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 'use strict';
 
-idl_test(
-  ['performance-timeline'],
-  ['hr-time', 'dom'],
-  idl_array => {
+promise_test(async t => {
+  const observe = new Promise((resolve, reject) => {
     try {
-      const callback = (e, o) => {};
-      self.observerEntry = new ResizeObserverEntry(callback);
+      self.observer = new PerformanceObserver((entries, observer) => {
+        self.entryList = entries;
+        self.mark = entries.getEntries()[0];
+        resolve();
+      });
+      observer.observe({ entryTypes: ['mark'] });
+      performance.mark('test');
     } catch (e) {
-      // Will be surfaced when entry is undefined below.
+      reject(e);
     }
+  });
+  const timeout = new Promise((_, reject) => {
+    t.step_timeout(() => reject('Timed out waiting for observation'), 3000);
+  });
+  const user = await fetch('/interfaces/user-timing.idl').then(r => r.text());
+  const execute_test = () => {
+    idl_test(
+      ['performance-timeline'],
+      ['hr-time', 'dom'],
+      idl_array => {
+        idl_array.add_idls(user, {only: ['PerformanceMark']});
+        idl_array.add_objects({
+          Performance: ['performance'],
+          // NOTE: PerformanceMark cascadingly tests PerformanceEntry
+          PerformanceMark: ['mark'],
+          PerformanceObserver: ['observer'],
+          PerformanceObserverEntryList: ['entryList'],
+        });
+      },
+      'Test IDL implementation of performance-timeline API'
+    );
+  };
 
-    idl_array.add_objects({
-      Performance: ['performance'],
-      // PerformanceEntry implicitly tested in user-timing.
-      ResizeObserverEntry: ['observerEntry'],
-    });
-  },
-  'Test IDL implementation of performance-timeline API'
-);
+  return Promise.race([observe, timeout]).then(
+    execute_test,
+    reason => {
+      execute_test();
+      return Promise.reject(reason);
+    }
+  );
+})
