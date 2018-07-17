5bc92b60813ededfd39c80e7a82e237dbf9ee844
nednguyen <nednguyen@google.com>
2018-07-17 20:19:57 +0000

    Add logging to measure time spent sending results to perf dashboard server
    NOTRY=true  # Flake
    
    Bug: 857283
    Cq-Include-Trybots: master.tryserver.chromium.perf:obbs_fyi
    Change-Id: I28b99bebbd705045c2f62a74d1324af480483309
    Reviewed-on: https://chromium-review.googlesource.com/1140688
    Commit-Queue: Ned Nguyen <nednguyen@google.com>
    Reviewed-by: Simon Hatch <simonhatch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575755}

--- tools/perf/core/results_dashboard.py
+++ tools/perf/core/results_dashboard.py
@@ -18,6 +18,7 @@ import os
 import subprocess
 import sys
 import traceback
+import time
 import tempfile
 import urllib
 import urllib2
@@ -72,6 +73,7 @@ def SendResults(data, url, tmp_dir,
     send_as_histograms: True if result is to be sent to /add_histograms.
     oauth_token: string; used for flushing oauth uploads from cache.
   """
+  start = time.time()
   results_json = json.dumps({
       'is_histogramset': send_as_histograms,
       'data': data
@@ -84,6 +86,8 @@ def SendResults(data, url, tmp_dir,
 
   # Send all the results from this run and the previous cache to the dashboard.
   fatal_error, errors = _SendResultsFromCache(cache_file_name, url, oauth_token)
+
+  print 'Time spent sending results to %s: %s' % (url, time.time() - start)
 
   # Print any errors; if there was a fatal error, it should be an exception.
   for error in errors:
