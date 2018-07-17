0edd39c2277a783ec2039e46a7cd9f918795e075
Peter Boström <pbos@chromium.org>
2018-07-17 15:47:11 +0000

    Use FocusRing RoundRects to match toolbar inkdrops
    Creates a SkPath that matches the inkdrop shape and use that as the
    focus ring.
    
    In the future, hopefully the inkdrop shape can be generated from the
    same path so that they are forced to stay in sync. For now though they
    are less separate as the focus ring shape is generated using the same
    insets as the inkdrop.
    
    This focus ring is installed for ToolbarButton, BrowserAppMenuButton and
    ToolbarActionView (all are visibly toolbar buttons).
    
    Bug: chromium:862925
    Change-Id: I898d2d9c6a9e058e9f84aff37615ebf3ab41cee7
    Reviewed-on: https://chromium-review.googlesource.com/1139221
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575653}

--- chrome/browser/ui/views/toolbar/browser_app_menu_button.h
+++ chrome/browser/ui/views/toolbar/browser_app_menu_button.h
@@ -47,6 +47,7 @@ class BrowserAppMenuButton : public AppMenuButton,
   void SetIsProminent(bool is_prominent);
 
   // views::MenuButton:
+  void OnBoundsChanged(const gfx::Rect& previous_bounds) override;
   gfx::Size CalculatePreferredSize() const override;
   void Layout() override;
   void OnThemeChanged() override;
