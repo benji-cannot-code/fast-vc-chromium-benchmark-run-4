dac5e2d86950f6acf2f30515ec67a262b1f48fc8
Matt Falkenhagen <falken@chromium.org>
2018-07-17 20:36:33 +0000

    Remove instrumentation added for AppCache crash with S13nSW.
    This is mostly a revert of https://crrev.com/a0cf214082,
    https://crrev.com/f03034d9a3c66, https://crrev.com/d2867e8cba3c,
    and https://crrev.com/5fe56c3ef49a6 but some new CHECKs
    added are retained as DCHECKs, and some trace instrumentation is
    kept.
    
    Bug: 857005
    Change-Id: Iaa686c040122acc3d2a376c8af8c66732c1f9307
    Reviewed-on: https://chromium-review.googlesource.com/1136458
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575758}

--- content/browser/service_worker/service_worker_controllee_request_handler.cc
+++ content/browser/service_worker/service_worker_controllee_request_handler.cc
@@ -318,11 +318,10 @@ void ServiceWorkerControlleeRequestHandler::
 
   const bool need_to_update = !force_update_started_ && registration &&
                               context_->force_update_on_page_load();
+
   if (provider_host_ && !need_to_update)
     provider_host_->SetAllowAssociation(true);
   if (status != blink::ServiceWorkerStatusCode::kOk) {
-    int fallback_reason = 0;
-    base::debug::Alias(&fallback_reason);
     url_job_->FallbackToNetwork();
     TRACE_EVENT_ASYNC_END1(
         "ServiceWorker",
@@ -333,32 +332,26 @@ void ServiceWorkerControlleeRequestHandler::
   DCHECK(registration.get());
 
   if (!provider_host_) {
-    int fallback_reason = 1;
-    base::debug::Alias(&fallback_reason);
     url_job_->FallbackToNetwork();
     TRACE_EVENT_ASYNC_END1(
         "ServiceWorker",
         "ServiceWorkerControlleeRequestHandler::PrepareForMainResource",
-        url_job_.get(), "Status", blink::ServiceWorkerStatusToString(status));
+        url_job_.get(), "Info", "No Provider");
     return;
   }
 
   if (!context_) {
-    int fallback_reason = 2;
-    base::debug::Alias(&fallback_reason);
     url_job_->FallbackToNetwork();
     TRACE_EVENT_ASYNC_END1(
         "ServiceWorker",
         "ServiceWorkerControlleeRequestHandler::PrepareForMainResource",
-        url_job_.get(), "Status", blink::ServiceWorkerStatusToString(status));
+        url_job_.get(), "Info", "No Context");
     return;
   }
 
   if (!GetContentClient()->browser()->AllowServiceWorker(
           registration->pattern(), provider_host_->topmost_frame_url(),
           resource_context_, provider_host_->web_contents_getter())) {
-    int fallback_reason = 3;
-    base::debug::Alias(&fallback_reason);
     url_job_->FallbackToNetwork();
     TRACE_EVENT_ASYNC_END2(
         "ServiceWorker",
@@ -371,8 +364,6 @@ void ServiceWorkerControlleeRequestHandler::
   if (!provider_host_->IsContextSecureForServiceWorker()) {
     // TODO(falken): Figure out a way to surface in the page's DevTools
     // console that the service worker was blocked for security.
-    int fallback_reason = 4;
-    base::debug::Alias(&fallback_reason);
     url_job_->FallbackToNetwork();
     TRACE_EVENT_ASYNC_END1(
         "ServiceWorker",
@@ -423,8 +414,6 @@ void ServiceWorkerControlleeRequestHandler::
 
   if (!active_version.get() ||
       active_version->status() != ServiceWorkerVersion::ACTIVATED) {
-    int fallback_reason = 5;
-    base::debug::Alias(&fallback_reason);
     url_job_->FallbackToNetwork();
     TRACE_EVENT_ASYNC_END2(
         "ServiceWorker",
