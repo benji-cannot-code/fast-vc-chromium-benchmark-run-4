d981179ba4e1f09dea69c85cc0e0cddf4e864656
Olivier Li <olivierli@chromium.org>
2018-07-17 21:07:41 +0000

    Add the pup_data directory
    Bug: 830892
    Change-Id: Id148e1503564e86f220a6a775aa9055294e465b1
    Reviewed-on: https://chromium-review.googlesource.com/1140198
    Reviewed-by: Joe Mason <joenotcharles@chromium.org>
    Commit-Queue: Oliver Li <olivierli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575771}

---
+++ chrome/chrome_cleaner/pup_data/dynamic_pup.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/chrome_cleaner/pup_data/dynamic_pup.h"
+
+namespace chrome_cleaner {
+
+DynamicPUP::DynamicPUP(const std::string& name, UwSId id, PUPData::Flags flags)
+    : PUPData::PUP(&stored_signature_),
+      stored_name_(name),
+      stored_signature_{id,
+                        flags,
+                        stored_name_.c_str(),
+                        /*max_files_to_remove=*/0,
+                        kNoDisk,
+                        kNoRegistry,
+                        kNoCustomMatcher} {}
+
+}  // namespace chrome_cleaner
