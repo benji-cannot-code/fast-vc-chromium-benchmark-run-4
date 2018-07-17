248b5dadb3e5f4565c1cf1db7a244b8759558614
Koji Ishii <kojii@chromium.org>
2018-07-17 23:49:54 +0000

    [LayoutNG] Move NGLineInfo to the member field of NGLineBreaker
    This patch adds NGLineInfo and NGInlineItemResults to the
    member field of NGLineBreaker.
    
    These are the output of the NGLineInfo, analogous to
    NGContainerFragmentBuilder to NGLayoutAlgorithm. Adding to
    the member fields makes NGLineBreaker closeer to
    NGLayoutAlgorithm.
    
    Note, NGInlineItemResults should be local and should
    std::move when the line is complete if we follow the existing
    pattern. It has inline buffer however, which makes std::move
    expensive. This patch keeps it as a pointer to the buffer in
    NGLineInfo.
    
    Bug: 636993
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng
    Change-Id: Ibc66fae8a8653196535dfa469b94a3255c0cd8e8
    Reviewed-on: https://chromium-review.googlesource.com/1138136
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575844}

--- third_party/blink/renderer/core/layout/ng/inline/ng_inline_item_result.h
+++ third_party/blink/renderer/core/layout/ng/inline/ng_inline_item_result.h
@@ -159,7 +159,7 @@ class CORE_EXPORT NGLineInfo {
   void SetIsEmptyLine() { is_empty_line_ = true; }
 
   // NGInlineItemResults for this line.
-  NGInlineItemResults& Results() { return results_; }
+  NGInlineItemResults* MutableResults() { return &results_; }
   const NGInlineItemResults& Results() const { return results_; }
 
   LayoutUnit TextIndent() const { return text_indent_; }
