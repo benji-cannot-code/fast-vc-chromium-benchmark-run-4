8803f5649ec34da53992928c444cf519f1202c76
Matthew Jones <mdjones@chromium.org>
2018-07-17 16:05:34 +0000

    Add data saver promo to top of menu in Chrome Duet
    This patch adds the data saver promo to the top of the overflow menu
    when Chrome Duet is enabled. The top divider for the promo has been
    removed and promo now scrolls off when the menu is scrolled (only in
    Duet).
    
    Supporting this patch is a change in how the header view is provided
    to the menu. Instead of passing the view directly, its layout ID is
    passed instead, bringing it more in line with the footer version.
    To get access to the inflated view, this patch also added
    AppMenuPropertiesDelegate#onHeaderViewInflated.
    
    Bug: 847632
    Change-Id: I1c332199f6760d1ba1f30262e122bf30ab4c835e
    Reviewed-on: https://chromium-review.googlesource.com/1136714
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575658}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -104,6 +104,7 @@ import org.chromium.chrome.browser.partnercustomizations.HomepageManager;
 import org.chromium.chrome.browser.partnercustomizations.PartnerBrowserCustomizations;
 import org.chromium.chrome.browser.preferences.ChromePreferenceManager;
 import org.chromium.chrome.browser.preferences.PrefServiceBridge;
+import org.chromium.chrome.browser.preferences.datareduction.DataReductionMainMenuItem;
 import org.chromium.chrome.browser.preferences.datareduction.DataReductionPromoScreen;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.signin.SigninPromoUtil;
@@ -1508,7 +1509,7 @@ public class ChromeTabbedActivity
     @Override
     protected AppMenuPropertiesDelegate createAppMenuPropertiesDelegate() {
         return new AppMenuPropertiesDelegate(this) {
-            private boolean showDataSaverFooter() {
+            private boolean shouldShowDataSaverMenuItem() {
                 return DataReductionProxySettings.getInstance()
                         .shouldUseDataReductionMainMenuItem();
             }
@@ -1518,7 +1519,7 @@ public class ChromeTabbedActivity
                 if (FeatureUtilities.isBottomToolbarEnabled()) {
                     return this.shouldShowPageMenu() ? R.layout.icon_row_menu_footer : 0;
                 }
-                return showDataSaverFooter() ? R.layout.data_reduction_main_menu_item : 0;
+                return shouldShowDataSaverMenuItem() ? R.layout.data_reduction_main_menu_item : 0;
             }
 
             @Override
@@ -1530,14 +1531,25 @@ public class ChromeTabbedActivity
             }
 
             @Override
-            public View getHeaderView() {
-                return null;
+            public int getHeaderResourceId() {
+                if (FeatureUtilities.isBottomToolbarEnabled()) {
+                    return shouldShowDataSaverMenuItem() ? R.layout.data_reduction_main_menu_item
+                                                         : 0;
+                }
+                return 0;
+            }
+
+            @Override
+            public void onHeaderViewInflated(AppMenu menu, View view) {
+                if (view instanceof DataReductionMainMenuItem) {
+                    view.findViewById(R.id.data_reduction_menu_divider).setVisibility(View.GONE);
+                }
             }
 
             @Override
             public boolean shouldShowFooter(int maxMenuHeight) {
                 if (FeatureUtilities.isBottomToolbarEnabled()) return true;
-                if (showDataSaverFooter()) {
+                if (shouldShowDataSaverMenuItem()) {
                     return canShowDataReductionItem(maxMenuHeight);
                 }
                 return super.shouldShowFooter(maxMenuHeight);
@@ -1545,7 +1557,9 @@ public class ChromeTabbedActivity
 
             @Override
             public boolean shouldShowHeader(int maxMenuHeight) {
-                if (getBottomSheet() == null) return super.shouldShowHeader(maxMenuHeight);
+                if (!FeatureUtilities.isBottomToolbarEnabled()) {
+                    return super.shouldShowHeader(maxMenuHeight);
+                }
 
                 if (DataReductionProxySettings.getInstance().shouldUseDataReductionMainMenuItem()) {
                     return canShowDataReductionItem(maxMenuHeight);
