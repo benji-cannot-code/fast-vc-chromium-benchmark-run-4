9992cc84164b620d49aa5f9c1c9929c74ff931de
Brian Sheedy <bsheedy@chromium.org>
2018-07-17 18:26:27 +0000

    Revert "VR: Ensure we remove black overlay if VR is exited while entering."
    This reverts commit 9f8158c12189567f8b9f4bcb44dd3cfc68ecd470.
    
    Reason for revert: Culprit of failed VR tests https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Nougat%20Phone%20Tester/6884
    
    Original change's description:
    > VR: Ensure we remove black overlay if VR is exited while entering.
    > 
    > This fixes a failure mode where we shutdownVR after handling the VR NFC
    > broadcast, and fail to properly cancel VR entry.
    > 
    > Also does some other random cleanup of comments/visibility.
    > 
    > Bug: 862829
    > Change-Id: Ic382692d7255aa5893391a602a6c949b4b4d498b
    > Reviewed-on: https://chromium-review.googlesource.com/1138726
    > Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    > Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#575388}
    
    TBR=mthiesse@chromium.org,tiborg@chromium.org
    
    Change-Id: Ibe67ee8c09dc44ac34d40c3eaf890884b400fc24
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 862829
    Reviewed-on: https://chromium-review.googlesource.com/1140813
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575725}

--- chrome/android/java/src/org/chromium/chrome/browser/vr/VrAlertDialog.java
+++ chrome/android/java/src/org/chromium/chrome/browser/vr/VrAlertDialog.java
@@ -17,15 +17,19 @@ import org.chromium.chrome.browser.modaldialog.ModalDialogView;
  * This class implements a VrAlertDialog which is similar to Android AlertDialog in VR.
  */
 public class VrAlertDialog extends AlertDialog {
+    private VrDialogManager mVrDialogManager;
     private ModalDialogManager mModalDialogManager;
     private ModalDialogView mModalDialogView;
+    private boolean mIsShowing;
     private CharSequence mMessage;
     private DialogButton mButtonPositive;
     private DialogButton mButtonNegative;
     protected View mView;
 
-    public VrAlertDialog(Context context, ModalDialogManager modalDialogManager) {
+    public VrAlertDialog(Context context, VrDialogManager vrDialogManager,
+            ModalDialogManager modalDialogManager) {
         super(context);
+        mVrDialogManager = vrDialogManager;
         mModalDialogManager = modalDialogManager;
     }
 
@@ -57,6 +61,7 @@ public class VrAlertDialog extends AlertDialog {
     public void show() {
         mModalDialogView = createView();
         mModalDialogManager.showDialog(mModalDialogView, ModalDialogManager.APP_MODAL);
+        mIsShowing = true;
     }
 
     /**
@@ -99,6 +104,7 @@ public class VrAlertDialog extends AlertDialog {
      */
     @Override
     public void dismiss() {
+        mIsShowing = false;
         mModalDialogManager.cancelDialog(mModalDialogView);
     }
 
--- chrome/android/java/src/org/chromium/chrome/browser/vr/VrShellDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/vr/VrShellDelegate.java
@@ -43,7 +43,6 @@ import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
 import org.chromium.base.PackageUtils;
 import org.chromium.base.ThreadUtils;
-import org.chromium.base.VisibleForTesting;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
 import org.chromium.base.library_loader.LibraryLoader;
@@ -208,7 +207,6 @@ public class VrShellDelegate
 
     private static final List<VrModeObserver> sVrModeObservers = new ArrayList<>();
 
-    @VisibleForTesting
     protected boolean mTestWorkaroundDontCancelVrEntryOnResume;
 
     private long mNativeVrShellDelegate;
@@ -909,7 +907,7 @@ public class VrShellDelegate
     }
 
     /**
-     *  @return Whether or not VR is enabled in this build.
+     *  @return Whether or not VR is supported on this platform.
      */
     /* package */ static boolean isVrEnabled() {
         return getVrClassesWrapper() != null;
@@ -1045,8 +1043,8 @@ public class VrShellDelegate
         // If an activity isn't resumed at the point, it must have been paused.
         mPaused = ApplicationStatus.getStateForActivity(activity) != ActivityState.RESUMED;
         mVisible = activity.hasWindowFocus();
-        mNativeVrShellDelegate = nativeInit();
         updateVrSupportLevel(null);
+        mNativeVrShellDelegate = nativeInit();
         mFeedbackFrequency = VrFeedbackStatus.getFeedbackFrequency();
         ensureLifecycleObserverInitialized();
         if (!mPaused) onResume();
@@ -1162,7 +1160,6 @@ public class VrShellDelegate
                 buttonText, null, true);
     }
 
-    @VisibleForTesting
     protected boolean isVrBrowsingEnabled() {
         return isVrBrowsingEnabled(mActivity, getVrSupportLevel());
     }
@@ -1219,11 +1216,16 @@ public class VrShellDelegate
     }
 
     private void enterVr(final boolean tentativeWebVrMode) {
-        // We should only enter VR when we're the resumed Activity or our changes to things like
-        // system UI flags might get lost.
+        // We can't enter VR before the application resumes, or we encounter bizarre crashes
+        // related to gpu surfaces.
+        // TODO(mthiesse): Is the above comment still accurate? It may have been tied to our HTML
+        // UI which is gone.
         assert !mPaused;
-        assert mNativeVrShellDelegate != 0;
         if (mInVr) return;
+        if (mNativeVrShellDelegate == 0) {
+            cancelPendingVrEntry();
+            return;
+        }
         mInVr = true;
         setVrModeEnabled(mActivity, true);
 
@@ -1243,11 +1245,11 @@ public class VrShellDelegate
         mExitedDueToUnsupportedMode = false;
 
         addVrViews();
+        boolean webVrMode = mRequestedWebVr || tentativeWebVrMode;
         // Make sure that assets component is registered when creating native VR shell.
         if (!sRegisteredVrAssetsComponent) {
             registerVrAssetsComponentIfDaydreamUser(isDaydreamCurrentViewer());
         }
-        boolean webVrMode = mRequestedWebVr || tentativeWebVrMode;
         mVrShell.initializeNative(webVrMode, getVrClassesWrapper().bootsToVr());
         mVrShell.setWebVrModeEnabled(webVrMode);
 
@@ -1289,9 +1291,6 @@ public class VrShellDelegate
         // Nothing to do if we were launched by an internal intent.
         if (mInternalIntentUsedToStartVr) {
             mInternalIntentUsedToStartVr = false;
-            // This is extremely unlikely in practice. Some code must have called shutdownVR() while
-            // we were entering VR through NFC insertion.
-            if (!mDonSucceeded) cancelPendingVrEntry();
             return;
         }
 
@@ -1546,7 +1545,6 @@ public class VrShellDelegate
         return true;
     }
 
-    @VisibleForTesting
     protected void onResume() {
         if (DEBUG_LOGS) Log.i(TAG, "onResume");
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) return;
@@ -1667,7 +1665,7 @@ public class VrShellDelegate
         if (mInVr) mVrShell.pause();
     }
 
-    private void onPause() {
+    protected void onPause() {
         if (DEBUG_LOGS) Log.i(TAG, "onPause");
         mPaused = true;
         if (mCancellingEntryAnimation) return;
@@ -1758,9 +1756,13 @@ public class VrShellDelegate
 
         mShowingDaydreamDoff = false;
 
-        if (mShowingDoffForGvrUpdate) mShowVrServicesUpdatePrompt = success;
+        if (mShowingDoffForGvrUpdate) {
+            mShowVrServicesUpdatePrompt = success;
+        }
 
-        if (success) shutdownVr(true /* disableVrMode */, true /* stayingInChrome */);
+        if (success) {
+            shutdownVr(true /* disableVrMode */, true /* stayingInChrome */);
+        }
 
         callOnExitVrRequestListener(success);
         mShowingDoffForGvrUpdate = false;
@@ -1777,7 +1779,8 @@ public class VrShellDelegate
     @CalledByNative
     private void setListeningForWebVrActivate(boolean listening) {
         if (DEBUG_LOGS) Log.i(TAG, "WebVR page listening for vrdisplayactivate: " + listening);
-        // Non-Daydream devices do not have the concept of activation.
+        // Non-Daydream devices may not have the concept of display activate. So disable
+        // mListeningForWebVrActivate for them.
         if (getVrSupportLevel() != VrSupportLevel.VR_DAYDREAM) return;
         if (mListeningForWebVrActivate == listening) return;
         mListeningForWebVrActivate = listening;
@@ -1822,7 +1825,7 @@ public class VrShellDelegate
     /**
      * Exits VR Shell, performing all necessary cleanup.
      */
-    private void shutdownVr(boolean disableVrMode, boolean stayingInChrome) {
+    protected void shutdownVr(boolean disableVrMode, boolean stayingInChrome) {
         if (DEBUG_LOGS) Log.i(TAG, "shuttdown VR");
         cancelPendingVrEntry();
 
@@ -1939,13 +1942,11 @@ public class VrShellDelegate
                 GVR_KEYBOARD_UPDATE_RESULT);
     }
 
-    @VisibleForTesting
     protected boolean canLaunch2DIntentsInternal() {
         return getVrClassesWrapper().supports2dInVr()
                 && !sVrModeEnabledActivitys.contains(sInstance.mActivity);
     }
 
-    @VisibleForTesting
     protected boolean createVrShell() {
         assert mVrShell == null;
         if (getVrClassesWrapper() == null) return false;
@@ -1972,7 +1973,6 @@ public class VrShellDelegate
         mActivity.onEnterVr();
     }
 
-    @VisibleForTesting
     protected boolean isBlackOverlayVisible() {
         View overlay = mActivity.getWindow().findViewById(R.id.vr_overlay_view);
         return overlay != null;
@@ -1998,7 +1998,6 @@ public class VrShellDelegate
     /**
      * @param api The VrDaydreamApi object this delegate will use instead of the default one
      */
-    @VisibleForTesting
     protected void overrideDaydreamApi(VrDaydreamApi api) {
         sVrDaydreamApi = api;
     }
@@ -2006,7 +2005,6 @@ public class VrShellDelegate
     /**
      * @return The VrShell for the VrShellDelegate instance
      */
-    @VisibleForTesting
     protected VrShell getVrShell() {
         return mVrShell;
     }
@@ -2014,7 +2012,6 @@ public class VrShellDelegate
     /**
      * @param versionChecker The VrCoreVersionChecker object this delegate will use
      */
-    @VisibleForTesting
     protected void overrideVrCoreVersionChecker(VrCoreVersionChecker versionChecker) {
         sVrCoreVersionChecker = versionChecker;
         updateVrSupportLevel(null);
@@ -2023,30 +2020,24 @@ public class VrShellDelegate
     /**
      * @param frequency Sets how often to show the feedback prompt.
      */
-    @VisibleForTesting
     protected void setFeedbackFrequency(int frequency) {
         mFeedbackFrequency = frequency;
     }
 
-    @VisibleForTesting
     protected boolean isListeningForWebVrActivate() {
         return mListeningForWebVrActivate;
     }
 
-    @VisibleForTesting
     protected boolean isVrEntryComplete() {
         return mInVr && !mProbablyInDon;
     }
 
-    @VisibleForTesting
     protected boolean isShowingDoff() {
         return mShowingDaydreamDoff;
     }
 
-    @VisibleForTesting
     protected void onBroadcastReceived() {}
 
-    @VisibleForTesting
     protected void setExpectingIntent(boolean expectingIntent) {}
 
     /**
