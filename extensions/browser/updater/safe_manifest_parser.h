140080180dc830ab48c1da9ce6eeb9f1500a63bc
Minh X. Nguyen <mxnguyen@chromium.org>
2018-07-17 22:40:26 +0000

    [Extensions] Re-classify extension manifest update results.
    This is to correct the way the (old) extension updater determines updates
    from the update manifest (|ExtensionDownloader::DetermineUpdates|). It
    currently treats extension update errors (e.g. extension not in the update
    manifest, invalid update version in the update manifest, ...) as no updates,
    which would cause a disparity with the new extension updater running in the
    Finch experiment.
    
    Bug: 722942
    Change-Id: I4d958fb0d313f2a9cd2456eef3035d2ef5f091fe
    Reviewed-on: https://chromium-review.googlesource.com/1123710
    Commit-Queue: Minh Nguyen <mxnguyen@chromium.org>
    Reviewed-by: Robert Kaplow (slow) <rkaplow@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575818}

--- extensions/browser/updater/safe_manifest_parser.h
+++ extensions/browser/updater/safe_manifest_parser.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef EXTENSIONS_BROWSER_UPDATER_SAFE_MANIFEST_PARSER_H_
 #define EXTENSIONS_BROWSER_UPDATER_SAFE_MANIFEST_PARSER_H_
 
+#include <map>
 #include <memory>
 #include <string>
 #include <vector>
@@ -47,6 +48,10 @@ struct UpdateManifestResults {
   UpdateManifestResults(const UpdateManifestResults& other);
   UpdateManifestResults& operator=(const UpdateManifestResults& other);
   ~UpdateManifestResults();
+
+  // Group |list| by |extension_id|.
+  std::map<std::string, std::vector<const UpdateManifestResult*>> GroupByID()
+      const;
 
   std::vector<UpdateManifestResult> list;
   // This will be >= 0, or kNoDaystart if the <daystart> tag was not present.
