74e5b6f8e252792e8abc6bddb277e97e6cdabaa2
Theresa <twellington@chromium.org>
2018-07-17 16:59:25 +0000

    Don't allow EoC to reshow until completely removed from sheet
    Don't allow Explore on Content to re-show itself in response to a tap on
    the toolbar button until any previously set content is fully removed
    from the sheet.
    
    Also, go directly to the hidden state when tapping on the scrim when the
    peek state is disabled.
    
    BUG=863568
    
    Change-Id: I5f6fea7fe637b4f346542321de6716899b6bf8fc
    Reviewed-on: https://chromium-review.googlesource.com/1139112
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575686}

--- chrome/android/java/src/org/chromium/chrome/browser/contextual_suggestions/ContextualSuggestionsMediator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextual_suggestions/ContextualSuggestionsMediator.java
@@ -25,6 +25,7 @@ import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 import org.chromium.chrome.browser.toolbar.ToolbarManager;
 import org.chromium.chrome.browser.util.MathUtils;
 import org.chromium.chrome.browser.widget.ListMenuButton;
+import org.chromium.chrome.browser.widget.bottomsheet.BottomSheet;
 import org.chromium.chrome.browser.widget.bottomsheet.BottomSheet.StateChangeReason;
 import org.chromium.chrome.browser.widget.bottomsheet.BottomSheetObserver;
 import org.chromium.chrome.browser.widget.bottomsheet.EmptyBottomSheetObserver;
@@ -72,6 +73,7 @@ class ContextualSuggestionsMediator
     private @Nullable TextBubble mHelpBubble;
     private @Nullable WebContents mCurrentWebContents;
 
+    private boolean mSuggestionsSetOnBottomSheet;
     private boolean mDidSuggestionsShowForTab;
     private boolean mHasRecordedPeekEventForTab;
     private boolean mHasRecordedButtonShownForTab;
@@ -277,6 +279,8 @@ class ContextualSuggestionsMediator
     }
 
     private void onToolbarButtonClicked() {
+        if (mSuggestionsSetOnBottomSheet) return;
+
         maybeShowContentInSheet();
         mCoordinator.showSuggestions(mSuggestionsSource);
         mCoordinator.expandBottomSheet();
@@ -366,6 +370,25 @@ class ContextualSuggestionsMediator
         }
     }
 
+    private void removeSuggestionsFromSheet() {
+        if (mSheetObserver != null) {
+            mCoordinator.removeBottomSheetObserver(mSheetObserver);
+            mSheetObserver = null;
+        }
+        mCoordinator.removeSuggestions();
+
+        // Wait until suggestions are fully removed to reset {@code mSuggestionsSetOnBottomSheet}.
+        mCoordinator.addBottomSheetObserver(new EmptyBottomSheetObserver() {
+            @Override
+            public void onSheetContentChanged(@Nullable BottomSheet.BottomSheetContent newContent) {
+                if (!(newContent instanceof ContextualSuggestionsBottomSheetContent)) {
+                    mSuggestionsSetOnBottomSheet = false;
+                    mCoordinator.removeBottomSheetObserver(this);
+                }
+            }
+        });
+    }
+
     /**
      * Called when suggestions are cleared either due to the user explicitly dismissing
      * suggestions via the close button or due to the FetchHelper signaling state should
@@ -374,13 +397,9 @@ class ContextualSuggestionsMediator
     private void clearSuggestions() {
         // TODO(twellington): Does this signal need to go back to FetchHelper?
 
-        // Call remove suggestions before clearing model state so that views don't respond to model
+        // Remove suggestions before clearing model state so that views don't respond to model
         // changes while suggestions are hiding. See https://crbug.com/840579.
-        if (mSheetObserver != null) {
-            mCoordinator.removeBottomSheetObserver(mSheetObserver);
-            mSheetObserver = null;
-        }
-        mCoordinator.removeSuggestions();
+        removeSuggestionsFromSheet();
 
         if (mToolbarButtonEnabled) {
             mToolbarManager.disableExperimentalButton();
@@ -433,7 +452,7 @@ class ContextualSuggestionsMediator
                                         : ContextualSuggestionsEvent.UI_DISMISSED_WITHOUT_OPEN;
             reportEvent(openedEvent);
             if (mToolbarButtonEnabled) {
-                mCoordinator.removeSuggestions();
+                removeSuggestionsFromSheet();
             } else {
                 clearSuggestions();
 
@@ -466,6 +485,7 @@ class ContextualSuggestionsMediator
             return;
         }
 
+        mSuggestionsSetOnBottomSheet = true;
         mDidSuggestionsShowForTab = true;
         mUpdateRemainingCountOnNextPeek = true;
 
@@ -523,7 +543,7 @@ class ContextualSuggestionsMediator
             @Override
             public void onSheetClosed(int reason) {
                 mModel.setMenuButtonVisibility(false);
-                if (mToolbarButtonEnabled) mCoordinator.removeSuggestions();
+                if (mToolbarButtonEnabled) removeSuggestionsFromSheet();
             }
 
             @Override
