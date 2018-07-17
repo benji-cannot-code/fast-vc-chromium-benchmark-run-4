a5964b968c7373895904bd3ea6c68c862e6e9bd2
Alexey Kozyatinskiy <kozyatinskiy@chromium.org>
2018-07-17 21:03:11 +0000

    [DevTools] support ansi color escape sequencies in String.format
    R=lushnikov@chromium.org
    
    Bug: none
    Change-Id: Id2192b6ca53da504540c2528121b3ee7a0dd1761
    Reviewed-on: https://chromium-review.googlesource.com/1132630
    Commit-Queue: Andrey Lushnikov <lushnikov@chromium.org>
    Reviewed-by: Andrey Lushnikov <lushnikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575767}

--- third_party/WebKit/LayoutTests/http/tests/devtools/unit/string-util.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/unit/string-util.js
@@ -1,13 +1,23 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 (async function() {
     TestRunner.runTests([
-        function testEmptyPrefixSuffix()
-        {
+        function testEmptyPrefixSuffix() {
             const tokens = String.tokenizeFormatString(`%c%s`, {
                 c: () => {},
                 s: () => {}
             });
             TestRunner.addObject(tokens);
         },
+
+        function testAnsiColors() {
+            const types = [3, 9, 4, 10];
+            const colors = [];
+            for (const type of types) {
+                for (let i = 0; i < 10; ++i)
+                    colors.push(type * 10 + i)
+            }
+            const tokens = String.tokenizeFormatString(colors.map(c => `\u001b[${c}m`).join(''), {c: _ => {}});
+            TestRunner.addArray(tokens);
+        }
     ]);
 })();
