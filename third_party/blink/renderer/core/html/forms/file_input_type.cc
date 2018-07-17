79bb5c5eeed538bd411feb9ba4ecaeb8e98a9505
Kent Tamura <tkent@chromium.org>
2018-07-17 08:08:20 +0000

    Forms: Add a UseCounter for opening popups while a file chooser is opening
    We'd like to fix a confusing UI issue, and to know the impact
    of a possible fix.
    
    Bug: 637639
    Change-Id: I7af67ac314f61723c7904cf0fee408ac2b60e774
    Reviewed-on: https://chromium-review.googlesource.com/1139940
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575575}

--- third_party/blink/renderer/core/html/forms/file_input_type.cc
+++ third_party/blink/renderer/core/html/forms/file_input_type.cc
@@ -172,6 +172,7 @@ void FileInputType::HandleDOMActivateEvent(Event* event) {
                       : WebFeature::kInputTypeFileInsecureOriginOpenChooser);
 
     chrome_client->OpenFileChooser(document.GetFrame(), NewFileChooser(params));
+    chrome_client->RegisterPopupOpeningObserver(this);
   }
   event->SetDefaultHandled();
 }
@@ -349,6 +350,11 @@ void FileInputType::SetFiles(FileList* files) {
 void FileInputType::FilesChosen(const Vector<FileChooserFileInfo>& files) {
   SetFiles(CreateFileList(files,
                           GetElement().FastHasAttribute(webkitdirectoryAttr)));
+  if (HasConnectedFileChooser()) {
+    DisconnectFileChooser();
+    if (auto* chrome_client = GetChromeClient())
+      chrome_client->UnregisterPopupOpeningObserver(this);
+  }
 }
 
 void FileInputType::SetFilesFromDirectory(const String& path) {
@@ -453,6 +459,14 @@ void FileInputType::HandleKeyupEvent(KeyboardEvent* event) {
     }
   }
   KeyboardClickableInputTypeView::HandleKeyupEvent(event);
+}
+
+void FileInputType::WillOpenPopup() {
+  // TODO(tkent): Should we disconnect the file chooser? crbug.com/637639
+  if (HasConnectedFileChooser()) {
+    UseCounter::Count(GetElement().GetDocument(),
+                      WebFeature::kPopupOpenWhileFileChooserOpened);
+  }
 }
 
 }  // namespace blink
--- third_party/blink/renderer/core/html/forms/file_input_type.h
+++ third_party/blink/renderer/core/html/forms/file_input_type.h
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/html/forms/file_chooser.h"
 #include "third_party/blink/renderer/core/html/forms/input_type.h"
 #include "third_party/blink/renderer/core/html/forms/keyboard_clickable_input_type_view.h"
+#include "third_party/blink/renderer/core/page/popup_opening_observer.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
 
 namespace blink {
@@ -47,7 +48,8 @@ class FileList;
 
 class CORE_EXPORT FileInputType final : public InputType,
                                         public KeyboardClickableInputTypeView,
-                                        private FileChooserClient {
+                                        private FileChooserClient,
+                                        private PopupOpeningObserver {
   USING_GARBAGE_COLLECTED_MIXIN(FileInputType);
 
  public:
@@ -98,6 +100,9 @@ class CORE_EXPORT FileInputType final : public InputType,
 
   // FileChooserClient implementation.
   void FilesChosen(const Vector<FileChooserFileInfo>&) override;
+
+  // PopupOpeningObserver implementation.
+  void WillOpenPopup() override;
 
   void SetFilesFromDirectory(const String&);
 
