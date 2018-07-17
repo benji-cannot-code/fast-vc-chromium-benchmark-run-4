3874873305bf0d9c974a9d958cf96a8468329ce9
Peter Collingbourne <pcc@chromium.org>
2018-07-17 20:09:00 +0000

    Remove ChromeMainDelegateAndroid::Create.
    All implementations of this function do the same thing so we can just
    call the constructor directly.
    
    Change-Id: I9f1dfb6682302bf6294666895a7ffd2b716c1e58
    Reviewed-on: https://chromium-review.googlesource.com/1139226
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Peter Collingbourne <pcc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575751}

--- chrome/app/android/chrome_main_delegate_android.h
+++ chrome/app/android/chrome_main_delegate_android.h
@@ -19,10 +19,8 @@ class SafeBrowsingApiHandler;
 // Android override of ChromeMainDelegate
 class ChromeMainDelegateAndroid : public ChromeMainDelegate {
  public:
-  static ChromeMainDelegateAndroid* Create();
   static void SecureDataDirectory();  // visible for testing
 
- protected:
   ChromeMainDelegateAndroid();
   ~ChromeMainDelegateAndroid() override;
 
