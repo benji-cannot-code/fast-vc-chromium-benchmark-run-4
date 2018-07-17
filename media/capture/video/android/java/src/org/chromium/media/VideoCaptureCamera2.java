c0556805c7b373d29880cb9233331fcc6a58d3f9
Christian Fremerey <chfremer@chromium.org>
2018-07-17 17:21:57 +0000

    [Image Capture] Add stress test that takes 10 photos in a row
    The purpose of the added test is to ensure the reliability of the feature as
    well as detecting flakiness that could be caused by race conditions.
    
    The test is marked as MANUAL_ to avoid it running on the regular commit queue
    trybots.
    
    To run the test manually on Android, increasing various timeouts is needed:
    out/android_release/bin/run_content_browsertests --gtest_filter=*WebRtcImageCaptureStressBrowserTest* --test-launcher-retry-limit=0 --ui-test-action-max-timeout=60000 --shard-timeout=60
    The above sets the timeout to 1 minute, which should be more than enough for 10 photos.
    
    Bug: 857530
    Change-Id: Ibf4f78d43dc3cbe24687710f48bf311f7bb0bfd4
    Reviewed-on: https://chromium-review.googlesource.com/1135703
    Commit-Queue: Christian Fremerey <chfremer@chromium.org>
    Reviewed-by: Emircan Uysaler <emircan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575697}

--- media/capture/video/android/java/src/org/chromium/media/VideoCaptureCamera2.java
+++ media/capture/video/android/java/src/org/chromium/media/VideoCaptureCamera2.java
@@ -65,6 +65,7 @@ public class VideoCaptureCamera2 extends VideoCapture {
 
         @Override
         public void onDisconnected(CameraDevice cameraDevice) {
+            Log.e(TAG, "cameraDevice was closed unexpectedly");
             cameraDevice.close();
             mCameraDevice = null;
             changeCameraStateAndNotify(CameraState.STOPPED);
@@ -72,6 +73,7 @@ public class VideoCaptureCamera2 extends VideoCapture {
 
         @Override
         public void onError(CameraDevice cameraDevice, int error) {
+            Log.e(TAG, "cameraDevice encountered an error");
             cameraDevice.close();
             mCameraDevice = null;
             changeCameraStateAndNotify(CameraState.STOPPED);
