98631f1d70f13e543a29963ad7a3bd438b15361c
Keishi Hattori <keishi@chromium.org>
2018-07-18 04:22:39 +0000

    compositor_will_send_main_frame_not_expected flag should not be set when no PageSchedulers exist
    When we set compositor_will_send_main_frame_not_expected flag when no PageSchedulers exist, MainThreadSchedulerImpl::BeginMainFrameNotExpectedUntil() never gets called. This means that StartIdlePeriod rarely gets called and the idle tasks may not be executed for a long time.
    
    After this CL, compositor_will_send_main_frame_not_expected will not be set when no PageSchedulers exist, so MainThreadSchedulerImpl will try calling RequestBeginMainFrameNotExpected next time.
    
    Bug: 862589
    Change-Id: I06f9c813df6cc7270269d9ffb3a01f24b9c911ce
    Reviewed-on: https://chromium-review.googlesource.com/1134630
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575938}

--- third_party/blink/renderer/platform/scheduler/public/page_scheduler.h
+++ third_party/blink/renderer/platform/scheduler/public/page_scheduler.h
@@ -27,7 +27,9 @@ class PLATFORM_EXPORT PageScheduler {
     virtual ~Delegate() = default;
 
     virtual void ReportIntervention(const WTF::String& message) = 0;
-    virtual void RequestBeginMainFrameNotExpected(bool new_state) = 0;
+    // Returns true if the request has been succcessfully relayed to the
+    // compositor.
+    virtual bool RequestBeginMainFrameNotExpected(bool new_state) = 0;
     virtual void SetLifecycleState(PageLifecycleState) = 0;
     virtual ukm::UkmRecorder* GetUkmRecorder() = 0;
     virtual int64_t GetUkmSourceId() = 0;
@@ -151,7 +153,9 @@ class PLATFORM_EXPORT PageScheduler {
 
   virtual bool HasActiveConnectionForTest() const = 0;
 
-  virtual void RequestBeginMainFrameNotExpected(bool new_state) = 0;
+  // Returns true if the request has been succcessfully relayed to the
+  // compositor.
+  virtual bool RequestBeginMainFrameNotExpected(bool new_state) = 0;
 };
 
 }  // namespace blink
