7ccbeb9c9bd187a8867f131c8d02dba515a57bef
Sky Malice <skym@google.com>
2018-07-18 17:33:38 +0000

    [Feed] Suppress scheduled refreshes on clear history.
    Bug: 831648
    Change-Id: Ibaca5b3501997d0c4538a9d8a79ae6050c10ec01
    Reviewed-on: https://chromium-review.googlesource.com/1131692
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576117}

--- chrome/browser/browsing_data/chrome_browsing_data_remover_delegate.cc
+++ chrome/browser/browsing_data/chrome_browsing_data_remover_delegate.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/user_metrics.h"
 #include "base/task_scheduler/post_task.h"
 #include "build/build_config.h"
+#include "chrome/browser/android/feed/feed_host_service_factory.h"
 #include "chrome/browser/autofill/personal_data_manager_factory.h"
 #include "chrome/browser/bookmarks/bookmark_model_factory.h"
 #include "chrome/browser/browser_process.h"
@@ -74,6 +75,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/data_reduction_proxy/core/browser/data_reduction_proxy_settings.h"
 #include "components/device_event_log/device_event_log.h"
 #include "components/domain_reliability/service.h"
+#include "components/feed/core/feed_host_service.h"
+#include "components/feed/core/feed_scheduler_host.h"
 #include "components/history/core/browser/history_service.h"
 #include "components/language/core/browser/url_language_histogram.h"
 #include "components/nacl/browser/nacl_browser.h"
@@ -408,6 +411,14 @@ void ChromeBrowsingDataRemoverDelegate::RemoveEmbedderData(
       ntp_snippets::RemoveLastVisitedDatesBetween(delete_begin_, delete_end_,
                                                   filter, bookmark_model);
     }
+
+#if defined(OS_ANDROID)
+    feed::FeedHostService* feed_host_service =
+        feed::FeedHostServiceFactory::GetForBrowserContext(profile_);
+    if (feed_host_service) {
+      feed_host_service->GetSchedulerHost()->OnHistoryCleared();
+    }
+#endif  // defined(OS_ANDROID)
 
     language::UrlLanguageHistogram* language_histogram =
         UrlLanguageHistogramFactory::GetForBrowserContext(profile_);
--- chrome/browser/browsing_data/chrome_browsing_data_remover_delegate_unittest.cc
+++ chrome/browser/browsing_data/chrome_browsing_data_remover_delegate_unittest.cc
@@ -68,6 +68,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/domain_reliability/monitor.h"
 #include "components/domain_reliability/service.h"
 #include "components/favicon/core/favicon_service.h"
+#include "components/feed/core/pref_names.h"
 #include "components/history/core/browser/history_service.h"
 #include "components/language/core/browser/url_language_histogram.h"
 #include "components/ntp_snippets/bookmarks/bookmark_last_visit_utils.h"
@@ -2958,4 +2959,14 @@ TEST_F(ChromeBrowsingDataRemoverDelegateTest, WipeOriginVerifierData) {
       customtabs::OriginVerifier::GetClearBrowsingDataCallCountForTesting());
 }
 
+TEST_F(ChromeBrowsingDataRemoverDelegateTest, FeedClearsLastFetchAttempt) {
+  PrefService* prefs = GetProfile()->GetPrefs();
+  prefs->SetTime(feed::prefs::kLastFetchAttemptTime, base::Time::Now());
+
+  BlockUntilBrowsingDataRemoved(
+      base::Time(), base::Time::Max(),
+      ChromeBrowsingDataRemoverDelegate::DATA_TYPE_HISTORY, false);
+
+  EXPECT_EQ(base::Time(), prefs->GetTime(feed::prefs::kLastFetchAttemptTime));
+}
 #endif  // defined(OS_ANDROID)
