0d3d04136f3580ddd4ad31cd51db565e790c4757
Adam Rice <ricea@chromium.org>
2018-07-18 04:44:52 +0000

    KURL::Port(): Remove unneeded constants
    Remove the constants kMaximumValidPortNumber and kInvalidPortNumber from
    kurl.cc as they are confusing and don't actually do anything.
    
    Also change remaining CHECKs in KURL::Port back to DCHECKs now that we
    have confidence the change is safe.
    
    Also make the method comment less confusing.
    
    BUG=606462
    
    Change-Id: I4719b8611cc6f3f0c5f488d8c86f3b6b8ad6d88f
    Reviewed-on: https://chromium-review.googlesource.com/1127083
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Eric Roman <eroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575942}

--- third_party/blink/renderer/platform/weborigin/kurl.cc
+++ third_party/blink/renderer/platform/weborigin/kurl.cc
@@ -46,9 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-static const int kMaximumValidPortNumber = 0xFFFE;
-static const int kInvalidPortNumber = 0xFFFF;
-
 #if DCHECK_IS_ON()
 static void AssertProtocolIsGood(const StringView protocol) {
   DCHECK(protocol != "");
@@ -360,26 +357,15 @@ String KURL::Host() const {
   return ComponentString(parsed_.host);
 }
 
-// Returns 0 when there is no port.
-//
-// We treat URL's with out-of-range port numbers as invalid URLs, and they will
-// be rejected by the canonicalizer. KURL.cpp will allow them in parsing, but
-// return invalidPortNumber from this port() function, so we mirror that
-// behavior here.
 unsigned short KURL::Port() const {
   if (!is_valid_ || parsed_.port.len <= 0)
     return 0;
-  // TODO(ricea): Change this back to DCHECK.
-  CHECK(!string_.IsNull());
+  DCHECK(!string_.IsNull());
   int port = string_.Is8Bit()
                  ? url::ParsePort(AsURLChar8Subtle(string_), parsed_.port)
                  : url::ParsePort(string_.Characters16(), parsed_.port);
-  // TODO(ricea): Change these two to DCHECK.
-  CHECK_NE(port, url::PORT_UNSPECIFIED);  // Checked port.len <= 0 already.
-  CHECK_NE(port, url::PORT_INVALID);      // Checked is_valid_ already.
-
-  // TODO(ricea): Remove this CHECK and the constants it uses.
-  CHECK(port <= kMaximumValidPortNumber || port == kInvalidPortNumber);
+  DCHECK_NE(port, url::PORT_UNSPECIFIED);  // Checked port.len <= 0 already.
+  DCHECK_NE(port, url::PORT_INVALID);      // Checked is_valid_ already.
 
   return static_cast<unsigned short>(port);
 }
