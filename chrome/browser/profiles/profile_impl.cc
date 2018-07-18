5cea934a9248b6ab55c64f6c3b9bf3280cf876f0
Dominick Ng <dominickn@chromium.org>
2018-07-18 07:24:14 +0000

    Create a skeleton App Service and App Registry.
    This CL creates the App Service as a in-process per-profile service, and
    attaches the App Registry to it.
    
    App Service is currently located in chrome/browser as many of its direct
    dependencies (which are not yet connected to the service) live there.
    Over time, we will refactor the dependencies such that:
    
    1. data which can be made independent of chrome/browser is moved within
       the App Service
    2. components which rely on that data access it asynchronously via the
       App Service
    
    This will allow us to move the App Service to chrome/services in the
    future.
    
    BUG=826982
    
    Change-Id: Ia7608fbb8c8b235d2b3e69ffa447963b32f2d5f0
    Reviewed-on: https://chromium-review.googlesource.com/1127513
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575968}

--- chrome/browser/profiles/profile_impl.cc
+++ chrome/browser/profiles/profile_impl.cc
@@ -34,6 +34,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/trace_event.h"
 #include "base/version.h"
 #include "build/build_config.h"
+#include "chrome/browser/apps/foundation/app_service/app_service.h"
+#include "chrome/browser/apps/foundation/app_service/public/mojom/constants.mojom.h"
 #include "chrome/browser/background/background_contents_service_factory.h"
 #include "chrome/browser/background_fetch/background_fetch_delegate_factory.h"
 #include "chrome/browser/background_fetch/background_fetch_delegate_impl.h"
@@ -295,6 +297,11 @@ std::string ExitTypeToSessionTypePrefValue(Profile::ExitType type) {
   }
   NOTREACHED();
   return std::string();
+}
+
+std::unique_ptr<service_manager::Service> CreateAppService(Profile* profile) {
+  // TODO(crbug.com/826982): use |profile| to fetch existing registries.
+  return std::make_unique<apps::AppService>();
 }
 
 }  // namespace
@@ -1172,6 +1179,16 @@ void ProfileImpl::RegisterInProcessServices(StaticServiceMap* services) {
     services->emplace(chromeos::multidevice_setup::mojom::kServiceName, info);
   }
 #endif
+
+  {
+    // Binding the App Service here means that its preferences will be stored in
+    // the primary Preferences file for this profile.
+    service_manager::EmbeddedServiceInfo info;
+    info.task_runner = base::ThreadTaskRunnerHandle::Get();
+    info.factory =
+        base::BindRepeating(&CreateAppService, base::Unretained(this));
+    services->emplace(apps::mojom::kServiceName, info);
+  }
 
   service_manager::EmbeddedServiceInfo identity_service_info;
 
