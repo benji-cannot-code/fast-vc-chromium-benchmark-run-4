a7ae8e6bc3c3698e76299b5ab16818fc2db1a6e5
Tiger Oakes <tigero@google.com>
2018-07-18 23:03:15 +0000

    Remove getApplicationContext calls from browserservices
    Continuation of work in bug 646094.
    
    Preparing to activate an errorprone check to flag
    context#getApplicationContext calls. The goal is to prevent storing the
    resulting context as a class property, and instead use ContextUtils.
    
    This CL replaces two others:
    https://chromium-review.googlesource.com/c/chromium/src/+/1095986
    https://chromium-review.googlesource.com/c/chromium/src/+/1095999
    
    Bug: 846456
    Change-Id: Id7ed38c318d97b7370938d32677bcf73b85c3314
    Reviewed-on: https://chromium-review.googlesource.com/1141933
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Tiger Oakes <tigero@google.com>
    Cr-Commit-Position: refs/heads/master@{#576242}

--- chrome/android/java/src/org/chromium/chrome/browser/browserservices/TrustedWebActivityClient.java
+++ chrome/android/java/src/org/chromium/chrome/browser/browserservices/TrustedWebActivityClient.java
@@ -10,6 +10,7 @@ import android.content.res.Resources;
 import android.net.Uri;
 import android.support.customtabs.trusted.TrustedWebActivityServiceConnectionManager;
 
+import org.chromium.base.ContextUtils;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.notifications.NotificationBuilderBase;
 import org.chromium.chrome.browser.notifications.NotificationUmaTracker;
@@ -19,14 +20,13 @@ import org.chromium.chrome.browser.notifications.NotificationUmaTracker;
  */
 public class TrustedWebActivityClient {
     private final TrustedWebActivityServiceConnectionManager mConnection;
-    private final Context mContext;
 
     /**
      * Creates a TrustedWebActivityService.
      */
-    public TrustedWebActivityClient(Context context) {
-        mConnection = new TrustedWebActivityServiceConnectionManager(context);
-        mContext = context.getApplicationContext();
+    public TrustedWebActivityClient() {
+        mConnection = new TrustedWebActivityServiceConnectionManager(
+                ContextUtils.getApplicationContext());
     }
 
     /**
@@ -49,7 +49,7 @@ public class TrustedWebActivityClient {
      */
     public void notifyNotification(Uri scope, String platformTag, int platformId,
             NotificationBuilderBase builder) {
-        Resources res = mContext.getResources();
+        Resources res = ContextUtils.getApplicationContext().getResources();
         String channelDisplayName = res.getString(R.string.notification_category_group_general);
 
         mConnection.execute(scope, new Origin(scope).toString(), service -> {
