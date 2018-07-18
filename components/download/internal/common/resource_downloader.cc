83226942f6eac607c821da18b068c4b2f78f28a0
Christian Dullweber <dullweber@chromium.org>
2018-07-18 09:00:57 +0000

    Revert "Fix an issue that ResourceDownloader was never deleted on download completion"
    This reverts commit e0b12de09e77d5954ace778522cf1a41c90f13aa.
    
    Reason for revert: The Cl probably introduced flakiness in multiple download related tests (https://crbug.com/864922)
    
    Original change's description:
    > Fix an issue that ResourceDownloader was never deleted on download completion
    > 
    > This CL calls the InProgressDownloadManager to delete ResourceDownloader
    > after response is completed.
    > It also fixes the issue when download is cancelled.
    > 
    > BUG=864189
    > 
    > Change-Id: I184d5faaace90f49874e45b4126a0a9821fdced7
    > Reviewed-on: https://chromium-review.googlesource.com/1138676
    > Commit-Queue: Min Qin <qinmin@chromium.org>
    > Reviewed-by: Xing Liu <xingliu@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#575882}
    
    TBR=qinmin@chromium.org,shaktisahu@chromium.org,xingliu@chromium.org
    
    Change-Id: I669a69fc135547508b44ccc8150847cc5824b2ec
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 864189
    Reviewed-on: https://chromium-review.googlesource.com/1141684
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575981}

--- components/download/internal/common/resource_downloader.cc
+++ components/download/internal/common/resource_downloader.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/download/public/common/download_url_loader_factory_getter.h"
 #include "components/download/public/common/stream_handle_input_stream.h"
-#include "components/download/public/common/url_download_request_handle.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 
 namespace network {
@@ -200,8 +199,6 @@ void ResourceDownloader::InterceptResponse(
 void ResourceDownloader::OnResponseStarted(
     std::unique_ptr<DownloadCreateInfo> download_create_info,
     mojom::DownloadStreamHandlePtr stream_handle) {
-  download_create_info->request_handle.reset(new UrlDownloadRequestHandle(
-      weak_ptr_factory_.GetWeakPtr(), base::SequencedTaskRunnerHandle::Get()));
   download_create_info->is_new_download = is_new_download_;
   download_create_info->guid = guid_;
   download_create_info->site_url = site_url_;
@@ -221,21 +218,6 @@ void ResourceDownloader::OnResponseStarted(
 
 void ResourceDownloader::OnReceiveRedirect() {
   url_loader_->FollowRedirect(base::nullopt, base::nullopt);
-}
-
-void ResourceDownloader::OnResponseCompleted() {
-  Destroy();
-}
-
-void ResourceDownloader::CancelRequest() {
-  Destroy();
-}
-
-void ResourceDownloader::Destroy() {
-  delegate_task_runner_->PostTask(
-      FROM_HERE,
-      base::BindOnce(&UrlDownloadHandler::Delegate::OnUrlDownloadStopped,
-                     delegate_, this));
 }
 
 }  // namespace download
