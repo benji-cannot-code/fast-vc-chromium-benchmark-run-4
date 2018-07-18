8a3fdd63443cbab7dc014f45343f754489e90416
Christopher Grant <cjgrant@chromium.org>
2018-07-18 20:29:03 +0000

    VR: Move UI-specific code into the UI for cleaner separation
    This change makes the future introduction of a VR UI feature module
    possible by cutting dependencies between VrShellGl and UI code.  This
    includes creating a UI support shim to help avoid UI calls directly
    to code that may not be exported to modules in the future.
    
    BUG=
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:linux_vr;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: Ib2edbe31ca8c6fcbbfbd8facc9913d6068eda208
    Reviewed-on: https://chromium-review.googlesource.com/1141934
    Commit-Queue: Christopher Grant <cjgrant@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576187}

---
+++ chrome/browser/vr/ui_initial_state.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_VR_UI_INITIAL_STATE_H_
+#define CHROME_BROWSER_VR_UI_INITIAL_STATE_H_
+
+#include "chrome/browser/vr/vr_export.h"
+
+namespace vr {
+
+// This class describes the initial state of a UI, and may be used by a UI
+// instances owner to specify a custom state on startup.
+struct VR_EXPORT UiInitialState {
+  UiInitialState();
+  UiInitialState(const UiInitialState& other);
+  bool in_web_vr = false;
+  bool web_vr_autopresentation_expected = false;
+  bool browsing_disabled = false;
+  bool has_or_can_request_audio_permission = true;
+  bool skips_redraw_when_not_dirty = false;
+  bool assets_supported = false;
+  bool supports_selection = true;
+  bool needs_keyboard_update = false;
+  bool is_standalone_vr_device = false;
+  bool create_tabs_view = false;
+};
+
+}  // namespace vr
+
+#endif  // CHROME_BROWSER_VR_UI_INITIAL_STATE_H_
