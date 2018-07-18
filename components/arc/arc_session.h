cd5c54fcc7970978002d4583d7f5f260e9065331
P H <phongo@google.com>
2018-07-18 17:01:17 +0000

    Pipe down supervision transition status to upgrade ARC instance request.
    Bug: 836246
    Change-Id: I991c886d8eeec65277c38d24f6a48b7e2ba35c98
    Reviewed-on: https://chromium-review.googlesource.com/1135876
    Commit-Queue: Patricia Hongo <phongo@google.com>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Reviewed-by: Luis Hector Chavez <lhchavez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576096}

--- components/arc/arc_session.h
+++ components/arc/arc_session.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/observer_list.h"
 #include "components/arc/arc_stop_reason.h"
+#include "components/arc/arc_supervision_transition.h"
 
 namespace base {
 class FilePath;
@@ -57,6 +58,12 @@ class ArcSession {
 
     // Whether the account is a child.
     bool is_child;
+
+    // The supervision transition state for this account. Indicates whether
+    // child account should become regular, regular account should become child
+    // or neither.
+    ArcSupervisionTransition supervision_transition =
+        ArcSupervisionTransition::NO_TRANSITION;
 
     // Define language configuration set during Android container boot.
     // |preferred_languages| may be empty.
