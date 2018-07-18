61b36a0254c8627e0920362eb6f9a0708dac46b6
Jérôme Lebel <jlebel@chromium.org>
2018-07-18 09:53:32 +0000

    [iOS] Adding UnifiedConsentServiceFactory
    Adding UnifiedConsentServiceFactory and UnifiedConsentServiceClientImpl
    for iOS.
    Those classes will be used by the Sync and Google services settings later.
    
    Bug: 827072
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I6b6e43527bba4a26fb6e3eae486ba62a900db81e
    Reviewed-on: https://chromium-review.googlesource.com/1138084
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575991}

---
+++ ios/chrome/browser/unified_consent/unified_consent_service_client_impl.cc
@@ -0,0 +1,55 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/chrome/browser/unified_consent/unified_consent_service_client_impl.h"
+
+#include "components/metrics/metrics_pref_names.h"
+#include "components/prefs/pref_member.h"
+#include "components/prefs/pref_service.h"
+#include "ios/chrome/browser/pref_names.h"
+
+UnifiedConsentServiceClientImpl::UnifiedConsentServiceClientImpl(
+    PrefService* pref_service)
+    : pref_service_(pref_service) {}
+
+void UnifiedConsentServiceClientImpl::SetAlternateErrorPagesEnabled(
+    bool enabled) {
+  // Feature not available on iOS.
+  NOTREACHED();
+}
+
+void UnifiedConsentServiceClientImpl::SetMetricsReportingEnabled(bool enabled) {
+  BooleanPrefMember basePreference;
+  basePreference.Init(metrics::prefs::kMetricsReportingEnabled, pref_service_);
+  basePreference.SetValue(enabled);
+  BooleanPrefMember wifiPreference;
+  wifiPreference.Init(prefs::kMetricsReportingWifiOnly, pref_service_);
+  wifiPreference.SetValue(enabled);
+}
+
+void UnifiedConsentServiceClientImpl::SetSearchSuggestEnabled(bool enabled) {
+  pref_service_->SetBoolean(prefs::kSearchSuggestEnabled, enabled);
+}
+
+void UnifiedConsentServiceClientImpl::SetSafeBrowsingEnabled(bool enabled) {
+  // Feature not available on iOS.
+  NOTREACHED();
+}
+
+void UnifiedConsentServiceClientImpl::SetSafeBrowsingExtendedReportingEnabled(
+    bool enabled) {
+  // Feature not available on iOS.
+  NOTREACHED();
+}
+
+void UnifiedConsentServiceClientImpl::SetNetworkPredictionEnabled(
+    bool enabled) {
+  BooleanPrefMember basePreference;
+  basePreference.Init(prefs::kNetworkPredictionEnabled, pref_service_);
+  basePreference.SetValue(enabled);
+  BooleanPrefMember wifiPreference;
+  wifiPreference.Init(prefs::kNetworkPredictionWifiOnly, pref_service_);
+  wifiPreference.SetValue(enabled);
+}
