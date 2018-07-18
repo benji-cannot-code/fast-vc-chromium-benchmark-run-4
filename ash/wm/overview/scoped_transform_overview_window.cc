c69f6c7880afa7d8ec5b2e1c80eb5221cd52103d
Sammie Quon <sammiequon@chromium.org>
2018-07-18 18:06:19 +0000

    Revert "overview: Fix crash with hiding title bars on entry."
    This reverts commit b87381369ce222524f52343c2bd4f0387574191e.
    
    Reason for revert: crbug.com/863795
    
    Original change's description:
    > overview: Fix crash with hiding title bars on entry.
    > 
    > Using OnCompositorStarted does not work for overview anymore. See stack
    > trace in bug.
    > 
    > Test: manual
    > Bug: 863795, 843851
    > Change-Id: I3dc8965cbece03ac36f1dc404c9b19b7dcac5b5e
    > Reviewed-on: https://chromium-review.googlesource.com/1138737
    > Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    > Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#575447}
    
    TBR=oshima@chromium.org,sammiequon@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 863795, 843851
    Change-Id: Ibc5965bc51a62e08b44d034a81f6954b8e799cad
    Reviewed-on: https://chromium-review.googlesource.com/1142166
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576135}

--- ash/wm/overview/scoped_transform_overview_window.cc
+++ ash/wm/overview/scoped_transform_overview_window.cc
@@ -280,8 +280,9 @@ bool ScopedTransformOverviewWindow::HideTitleBarAndAnimate(
   return true;
 }
 
-void ScopedTransformOverviewWindow::OnCompositingDidCommit(
-    ui::Compositor* compositor) {
+void ScopedTransformOverviewWindow::OnCompositingStarted(
+    ui::Compositor* compositor,
+    base::TimeTicks start_time) {
   views::Widget* widget = views::Widget::GetWidgetForNativeWindow(window_);
   DCHECK(widget);
   DCHECK_EQ(compositor, widget->GetCompositor());
