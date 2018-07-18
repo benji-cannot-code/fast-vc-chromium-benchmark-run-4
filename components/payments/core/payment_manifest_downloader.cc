68a9b7dd554d6267606a73d5f4bc8a39691b7d25
gogerald <gogerald@google.com>
2018-07-18 13:56:35 +0000

    [Payments] Prevent cross site redirects for payment method HEAD
    Bug: 863974
    Change-Id: Idfeae3e29ff93b0897e822035d2f35282b6c8ca1
    Reviewed-on: https://chromium-review.googlesource.com/1141046
    Commit-Queue: Ganggui Tang <gogerald@chromium.org>
    Reviewed-by: Mathieu Perreault <mathp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576026}

--- components/payments/core/payment_manifest_downloader.cc
+++ components/payments/core/payment_manifest_downloader.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/data_use_measurement/core/data_use_user_data.h"
 #include "components/link_header_util/link_header_util.h"
 #include "net/base/load_flags.h"
+#include "net/base/registry_controlled_domains/registry_controlled_domain.h"
 #include "net/base/url_util.h"
 #include "net/http/http_response_headers.h"
 #include "net/http/http_status_code.h"
@@ -155,7 +156,11 @@ void PaymentManifestDownloader::OnURLLoaderRedirect(
   if (download->allowed_number_of_redirects > 0) {
     DCHECK(download->method == "HEAD");
     GURL redirect_url = ParseRedirectUrl(redirect_info);
-    if (!redirect_url.is_empty()) {
+    if (!redirect_url.is_empty() &&
+        // Do not allow cross site redirects.
+        net::registry_controlled_domains::SameDomainOrHost(
+            download->original_url, redirect_url,
+            net::registry_controlled_domains::INCLUDE_PRIVATE_REGISTRIES)) {
       InitiateDownload(redirect_url, "HEAD",
                        --download->allowed_number_of_redirects,
                        std::move(download->callback));
