ec97eff2717eef212b6d3e4f5886aca95678deef
Min Qin <qinmin@chromium.org>
2018-07-18 20:35:02 +0000

    Reland "Fix an issue that ResourceDownloader was never deleted on download completion"
    This reverts commit 9faf0129433836d06b0a7113a61332b5b7090cd3.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Original change's description:
    > Revert "Fix an issue that ResourceDownloader was never deleted on download completion"
    > 
    > This reverts commit e0b12de09e77d5954ace778522cf1a41c90f13aa.
    > 
    > Reason for revert: The Cl probably introduced flakiness in multiple download related tests (https://crbug.com/864922)
    > 
    > Original change's description:
    > > Fix an issue that ResourceDownloader was never deleted on download completion
    > > 
    > > This CL calls the InProgressDownloadManager to delete ResourceDownloader
    > > after response is completed.
    > > It also fixes the issue when download is cancelled.
    > > 
    > > BUG=864189
    > > 
    > > Change-Id: I184d5faaace90f49874e45b4126a0a9821fdced7
    > > Reviewed-on: https://chromium-review.googlesource.com/1138676
    > > Commit-Queue: Min Qin <qinmin@chromium.org>
    > > Reviewed-by: Xing Liu <xingliu@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#575882}
    > 
    > TBR=qinmin@chromium.org,shaktisahu@chromium.org,xingliu@chromium.org
    > 
    > Change-Id: I669a69fc135547508b44ccc8150847cc5824b2ec
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 864189
    > Reviewed-on: https://chromium-review.googlesource.com/1141684
    > Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    > Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#575981}
    
    TBR=qinmin@chromium.org,shaktisahu@chromium.org,dullweber@chromium.org,xingliu@chromium.org
    
    Change-Id: I11012d0bf159f4944d69cc13881180db9ed98d7e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 864189
    Reviewed-on: https://chromium-review.googlesource.com/1142171
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576190}

--- components/download/internal/common/download_response_handler.cc
+++ components/download/internal/common/download_response_handler.cc
@@ -203,8 +203,10 @@ void DownloadResponseHandler::OnComplete(
         ConvertInterruptReasonToMojoNetworkRequestStatus(reason));
   }
 
-  if (started_)
+  if (started_) {
+    delegate_->OnResponseCompleted();
     return;
+  }
 
   // OnComplete() called without OnReceiveResponse(). This should only
   // happen when the request was aborted.
@@ -212,6 +214,7 @@ void DownloadResponseHandler::OnComplete(
   create_info_->result = reason;
 
   OnResponseStarted(mojom::DownloadStreamHandlePtr());
+  delegate_->OnResponseCompleted();
 }
 
 void DownloadResponseHandler::OnResponseStarted(
