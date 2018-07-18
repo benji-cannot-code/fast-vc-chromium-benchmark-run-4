248c512e2ce6f3ab5d4c65df31018069b0072399
Donn Denman <donnd@chromium.org>
2018-07-18 21:23:39 +0000

    [TTS] Update URL sending to use Unity URL helper.
    Updates the logic in Contextual Search that determines whether to send a
    URL to our servers as part of the Resolve request.  Now instead of
    checking the sync service we use the Unity helper -- the Unified
    Consent UrlKeyedDataCollectionConsentHelper.
    
    BUG=825428
    
    Change-Id: I6d294260018fc77d1eef896d75ea3b664cfba6d0
    Reviewed-on: https://chromium-review.googlesource.com/1117705
    Commit-Queue: Donn Denman <donnd@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576215}

--- chrome/browser/android/contextualsearch/contextual_search_delegate.h
+++ chrome/browser/android/contextualsearch/contextual_search_delegate.h
@@ -101,7 +101,6 @@ class ContextualSearchDelegate
                            SampleSurroundingTextSameStartEnd);
   FRIEND_TEST_ALL_PREFIXES(ContextualSearchDelegateTest,
                            DecodeSearchTermFromJsonResponse);
-  FRIEND_TEST_ALL_PREFIXES(ContextualSearchDelegateTest, DecodeStringMentions);
 
   // net::URLFetcherDelegate:
   void OnURLFetchComplete(const net::URLFetcher* source) override;
