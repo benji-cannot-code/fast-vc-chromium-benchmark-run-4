1cb929f3333add28191ffbc2fa5ea9f99567f20e
Pavol Marko <pmarko@chromium.org>
2018-07-18 10:30:32 +0000

    Introduce command-line switch to force-enable developer tools
    Since CL:1125855, the new default is that policy-installed extensions
    can not be inspected by developer tools.
    However, some autotests rely on inspecting policy-installed extensions
    and it is infeasible to change the policy the device sees there.
    Introduce the "--force-devtools-available" command-line switch which
    enforces developer tools availability.
    
    Bug: 838146
    Test: browser_tests --gtest_filter=*DevTools*
    Change-Id: I666d9785195eff53f1a1346d95177985d1573d72
    Reviewed-on: https://chromium-review.googlesource.com/1140308
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575994}

--- chrome/browser/devtools/chrome_devtools_manager_delegate.cc
+++ chrome/browser/devtools/chrome_devtools_manager_delegate.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
+#include "base/command_line.h"
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
 #include "chrome/browser/devtools/chrome_devtools_session.h"
@@ -150,6 +151,10 @@ bool ChromeDevToolsManagerDelegate::AllowInspection(
 bool ChromeDevToolsManagerDelegate::AllowInspection(
     Profile* profile,
     const extensions::Extension* extension) {
+  base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
+  if (command_line->HasSwitch(switches::kForceDevToolsAvailable))
+    return true;
+
   using Availability = policy::DeveloperToolsPolicyHandler::Availability;
   Availability availability =
       policy::DeveloperToolsPolicyHandler::GetDevToolsAvailability(
