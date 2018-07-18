c59916851ef08c198f8c2bb43448d687caf16b00
Matt Falkenhagen <falken@chromium.org>
2018-07-18 07:04:02 +0000

    AppCache: Minor code cleanups.
    Change-Id: Iaee118571685b3018bcf377391953543e73733f0
    Reviewed-on: https://chromium-review.googlesource.com/1141452
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575961}

--- content/browser/appcache/appcache_navigation_handle.h
+++ content/browser/appcache/appcache_navigation_handle.h
@@ -8,8 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include "base/macros.h"
-#include "base/memory/ref_counted.h"
-#include "base/memory/weak_ptr.h"
 
 namespace content {
 
@@ -43,7 +41,7 @@ class ChromeAppCacheService;
 //      update the RequestNavigationParams based on the id from the
 //      AppCacheNavigationHandle.
 //
-//   6. The commit leads to AppCache registrations happening from the renderer.
+//   6) The commit leads to AppCache registrations happening from the renderer.
 //      This is via the IPC message AppCacheHostMsg_RegisterHost. The
 //      AppCacheDispatcherHost class which handles these IPCs will be informed
 //      about these hosts when the navigation commits. It will ignore the
@@ -53,24 +51,19 @@ class ChromeAppCacheService;
 
 //   7) When the navigation finishes, the AppCacheNavigationHandle is
 //      destroyed. The destructor of the AppCacheNavigationHandle posts a
-//      task to destroy the AppacheNavigationHandleCore on the IO thread.
+//      task to destroy the AppCacheNavigationHandleCore on the IO thread.
 
 class AppCacheNavigationHandle {
  public:
-  AppCacheNavigationHandle(ChromeAppCacheService* appcache_service);
+  explicit AppCacheNavigationHandle(ChromeAppCacheService* appcache_service);
   ~AppCacheNavigationHandle();
 
   int appcache_host_id() const { return appcache_host_id_; }
   AppCacheNavigationHandleCore* core() const { return core_.get(); }
 
-  // Called when a navigation is committed. The |process_id| parameter is
-  // is the process id of the renderer.
-  void CommitNavigation(int process_id);
-
  private:
-  int appcache_host_id_;
+  const int appcache_host_id_;
   std::unique_ptr<AppCacheNavigationHandleCore> core_;
-  base::WeakPtrFactory<AppCacheNavigationHandle> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(AppCacheNavigationHandle);
 };
