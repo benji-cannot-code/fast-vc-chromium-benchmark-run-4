af0f1852816ace008c042cfe1384f9c3bc206f2b
Scott Chen <scottchen@chromium.org>
2018-07-18 22:13:03 +0000

    Media Router: remove iron-icons/ dependency
    Part of the effort to remove iron-icons/*.html iconsets from the repro.
    
    Screenshot showing icons unchanged from before:
    https://imgur.com/a/1iDQh03
    
    Bug: 855205
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: Iac109e42bee49be6467e40746e3e54d4ab200826
    Reviewed-on: https://chromium-review.googlesource.com/1139220
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Commit-Queue: Scott Chen <scottchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576231}

--- chrome/browser/resources/media_router/elements/route_controls/route_controls.js
+++ chrome/browser/resources/media_router/elements/route_controls/route_controls.js
@@ -255,7 +255,8 @@ Polymer({
    * @private
    */
   getMuteUnmuteIcon_: function(routeStatus) {
-    return routeStatus.isMuted ? 'av:volume-off' : 'av:volume-up';
+    return routeStatus.isMuted ? 'route-controls:volume-off' :
+                                 'route-controls:volume-up';
   },
 
   /**
@@ -275,8 +276,8 @@ Polymer({
    */
   getPlayPauseIcon_: function(routeStatus) {
     return routeStatus.playState === media_router.PlayState.PAUSED ?
-        'av:play-arrow' :
-        'av:pause';
+        'route-controls:play-arrow' :
+        'route-controls:pause';
   },
 
   /**
