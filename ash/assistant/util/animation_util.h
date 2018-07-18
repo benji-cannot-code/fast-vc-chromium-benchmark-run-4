d49aa3960873168b64b392bd52b580d27b575aa9
David Black <dmblack@google.com>
2018-07-18 01:56:08 +0000

    Implement DialogPlate motion spec (part 2).
    This builds off of DialogPlate motion spec part 1:
    https://chromium-review.googlesource.com/c/chromium/src/+/1137843
    
    This CL:
    - Fixes issue which caused settings to fade in/out. Previously we used
      two settings buttons, now we use a single button which exists outside
      of the layout containers being animated.
    - Implements translation phase of the animation.
    
    See bug for spec/demo.
    
    Bug: b:111500701
    Change-Id: I269666dafa815018e0cf65df299aa734eaf80b79
    Reviewed-on: https://chromium-review.googlesource.com/1139031
    Commit-Queue: David Black <dmblack@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575901}

--- ash/assistant/util/animation_util.h
+++ ash/assistant/util/animation_util.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "ui/gfx/animation/tween.h"
+
 namespace base {
 class TimeDelta;
 }  // namespace base
@@ -22,17 +24,31 @@ namespace assistant {
 namespace util {
 
 // Creates a LayerAnimationSequence containing the specified
-// |layer_animation_element|. The method caller assumes ownership of the
+// LayerAnimationElements. The method caller assumes ownership of the
 // returned pointer.
 ui::LayerAnimationSequence* CreateLayerAnimationSequence(
-    std::unique_ptr<ui::LayerAnimationElement> layer_animation_element);
+    std::unique_ptr<ui::LayerAnimationElement> a,
+    std::unique_ptr<ui::LayerAnimationElement> b = nullptr);
 
 // Creates a LayerAnimationSequence containing the specified
 // |layer_animation_element| to be started after the given |delay|. The method
 // caller assumes ownership of the returned pointer.
 ui::LayerAnimationSequence* CreateLayerAnimationSequenceWithDelay(
     std::unique_ptr<ui::LayerAnimationElement> layer_animation_element,
-    base::TimeDelta delay);
+    const base::TimeDelta& delay);
+
+// Creates a LayerAnimationElement to animate opacity with the given parameters.
+std::unique_ptr<ui::LayerAnimationElement> CreateOpacityElement(
+    float opacity,
+    const base::TimeDelta& duration,
+    const gfx::Tween::Type& tween = gfx::Tween::Type::LINEAR);
+
+// Creates a LayerAnimationElement to animate transform with the given
+// parameters.
+std::unique_ptr<ui::LayerAnimationElement> CreateTransformElement(
+    const gfx::Transform& transform,
+    const base::TimeDelta& duration,
+    const gfx::Tween::Type& tween = gfx::Tween::Type::LINEAR);
 
 }  // namespace util
 }  // namespace assistant
