78fa522f4572b0c94a7d0a0f724052a9691df8b1
Nate Chapin <japhet@chromium.org>
2018-07-19 21:59:35 +0000

    Implement FeaturePolicy sync-script (as an experimental feature policy)
    Spec discussion: https://github.com/WICG/feature-policy/issues/135
    Intent to implement: https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/b7f_K30MBYo
    
    Bug: 862422
    Change-Id: Icd8a5d747373a819c7073ac459b5e8823d7986a8
    Reviewed-on: https://chromium-review.googlesource.com/894137
    Reviewed-by: Ojan Vafai <ojan@chromium.org>
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576652}

--- third_party/blink/common/feature_policy/feature_policy.cc
+++ third_party/blink/common/feature_policy/feature_policy.cc
@@ -280,6 +280,8 @@ const FeaturePolicy::FeatureList& FeaturePolicy::GetDefaultFeatureList() {
                             FeaturePolicy::FeatureDefault::EnableForAll},
                            {mojom::FeaturePolicyFeature::kSpeaker,
                             FeaturePolicy::FeatureDefault::EnableForSelf},
+                           {mojom::FeaturePolicyFeature::kSyncScript,
+                            FeaturePolicy::FeatureDefault::EnableForAll},
                            {mojom::FeaturePolicyFeature::kSyncXHR,
                             FeaturePolicy::FeatureDefault::EnableForAll},
                            {mojom::FeaturePolicyFeature::kUnsizedMedia,
