7930206fe860d74aa68b4fc3429af14133fbfba4
Christian Dullweber <dullweber@chromium.org>
2018-07-19 11:53:45 +0000

    Revert "Reland "Enable powerwash shortcut in views-based login screen.""
    This reverts commit 016ff4b37121ca3c08d85971a44f8c167e588fa4.
    
    Reason for revert: LoginCursorTest.CursorHidden is still failing:
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/linux-chromeos-dbg/6897
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20%281%29/28314
    
    Original change's description:
    > Reland "Enable powerwash shortcut in views-based login screen."
    > 
    > This reverts commit 14fd238a824c6bd59c8cbd9189c8bf017cbffc48.
    > Removes the chrome.send('showAddUser') in md_login.js to avoid a race
    > between the powerwash dialog and the Gaia signin screen.
    > 
    > Bug: 837501
    > Cq-Include-Trybots: luci.chromium.try:closure_compilation
    > Change-Id: I7ab656ebe522cbd164073b73b3dd927cca7b1f13
    > Reviewed-on: https://chromium-review.googlesource.com/1129384
    > Commit-Queue: Quan Nguyen <qnnguyen@chromium.org>
    > Reviewed-by: Jacob Dufault <jdufault@chromium.org>
    > Reviewed-by: Dan Erat <derat@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#576306}
    
    TBR=derat@chromium.org,dcheng@chromium.org,jdufault@chromium.org,qnnguyen@chromium.org
    
    Change-Id: I7044802f815396b7c918f1153342d531122d8852
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 837501
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Reviewed-on: https://chromium-review.googlesource.com/1143206
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576446}

--- ash/login/ui/lock_contents_view.h
+++ ash/login/ui/lock_contents_view.h
@@ -105,7 +105,6 @@ class ASH_EXPORT LockContentsView
     kShowFeedback,
     kFocusNextUser,
     kFocusPreviousUser,
-    kShowResetScreen,
   };
 
   // Number of login attempts before a login dialog is shown. For example, if
