b6624163e099c7aafdcf54de5207eaf370b9ef78
yucliu <yucliu@chromium.org>
2018-07-19 23:21:49 +0000

    [Chromecast] Add channel layout for CastAudioDecoder
    Add OutputChannelLayout for CastAudioDecoder, so that CastAudioDecoder
    can output mono channel stream.
    
    BUG=internal b/110436402
    TEST=Play music
    
    Change-Id: I9657b1b212e142ceea90b9210a02e249a292e09d
    Reviewed-on: https://chromium-review.googlesource.com/1142616
    Reviewed-by: Kenneth MacKay <kmackay@chromium.org>
    Reviewed-by: Bailey Forrest <bcf@chromium.org>
    Commit-Queue: Yuchen Liu <yucliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576685}

--- chromecast/media/cma/decoder/cast_audio_decoder.h
+++ chromecast/media/cma/decoder/cast_audio_decoder.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
+#include "media/base/channel_layout.h"
 
 namespace base {
 class SingleThreadTaskRunner;
@@ -43,17 +44,22 @@ class CastAudioDecoder {
       const scoped_refptr<media::DecoderBufferBase>& decoded)> DecodeCallback;
 
   // Creates a CastAudioDecoder instance for the given |config|. Decoding must
-  // occur on the same thread as |task_runner|. Returns an empty scoped_ptr if
+  // occur on the same thread as |task_runner|. Returns an empty unique_ptr if
   // the decoder could not be created.
   static std::unique_ptr<CastAudioDecoder> Create(
       const scoped_refptr<base::SingleThreadTaskRunner>& task_runner,
       const media::AudioConfig& config,
       OutputFormat output_format,
+      ::media::ChannelLayout output_channel_layout,
       const InitializedCallback& initialized_callback);
 
   // Given a CastAudioDecoder::OutputFormat, return the size of each sample in
   // that OutputFormat in bytes.
   static int OutputFormatSizeInBytes(CastAudioDecoder::OutputFormat format);
+
+  // Get output channel layout to be used by CastAudioDecoder from |config|.
+  static ::media::ChannelLayout OutputChannelLayoutFromConfig(
+      const AudioConfig& config);
 
   CastAudioDecoder() {}
   virtual ~CastAudioDecoder() {}
