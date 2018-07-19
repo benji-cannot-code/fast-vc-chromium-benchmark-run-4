a30dd719728f72af742ce42bb2c694adf2eea9b9
Kyle Qian <kyleqian@google.com>
2018-07-19 23:54:37 +0000

    Implement ThreadUtils for CrOS Nearby.
    This CL includes a concrete implementation for the abstract class
    ThreadUtils defined in the Nearby library. This class currently only
    contains the method sleep(), which causes the currently executing
    thread to sleep for a specified number of milliseconds.
    
    This CL also adds missing file entries into nearby/library/BUILD.gn.
    
    Because the actual Nearby library has yet to be merged into the CrOS
    directory, this CL includes stand-in Nearby abstract classes under
    the temporary directory //chromeos/components/nearby/library.
    This directory will be removed after the Nearby library gets imported
    to //third_party (in-progress CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/1113198).
    
    Bug: 861813
    Change-Id: I8c78673bc42ee38b4667fd0fbc7118ee422b3393
    Reviewed-on: https://chromium-review.googlesource.com/1144417
    Commit-Queue: Kyle Qian <kyleqian@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576701}

---
+++ chromeos/components/nearby/thread_utils_impl.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/components/nearby/thread_utils_impl.h"
+
+#include "base/threading/platform_thread.h"
+#include "base/time/time.h"
+#include "chromeos/components/nearby/library/exception.h"
+
+namespace chromeos {
+
+namespace nearby {
+
+ThreadUtilsImpl::ThreadUtilsImpl() = default;
+
+ThreadUtilsImpl::~ThreadUtilsImpl() = default;
+
+location::nearby::Exception::Value ThreadUtilsImpl::sleep(int64_t millis) {
+  base::PlatformThread::Sleep(base::TimeDelta::FromMilliseconds(millis));
+  return location::nearby::Exception::NONE;
+}
+
+}  // namespace nearby
+
+}  // namespace chromeos
