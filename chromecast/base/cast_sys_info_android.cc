bed8f5a86935f60a547d631ef852ac0cede8a390
Jeremy Newton-Smith <jeremyns@chromium.org>
2018-07-19 18:25:36 +0000

    Updating GetBuildType to use cast_config file to determine build type.
    After this change the channel will no longer be used to determine production
    builds.  If a cast_config file doesn't exist in the APK then BUILD_PRODUCTION
    will be used, and the file will be stripped from release builds.  Debug builds
    will still contain the file and follow old logic.
    
    Only the existence of the file will be checked, but this will be updated to
    parse the text file for details.
    
    Bug: Internal b/77869986
    Change-Id: I4781a896dbd8e835cbd7e3e1bec38add43b0c121
    Reviewed-on: https://chromium-review.googlesource.com/1143896
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Commit-Queue: Jeremy Newton-Smith <jeremyns@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576564}

--- chromecast/base/cast_sys_info_android.cc
+++ chromecast/base/cast_sys_info_android.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
+#include "base/android/apk_assets.h"
 #include "base/android/build_info.h"
 #include "base/android/jni_android.h"
 #include "base/android/jni_string.h"
@@ -22,7 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromecast {
 
 namespace {
-const char kBuildTypeUser[] = "user";
+const char kCastConfigAssetPath[] = "assets/cast_config";
 
 std::string GetAndroidProperty(const std::string& key,
                                const std::string& default_value) {
@@ -35,6 +36,13 @@ std::string GetAndroidProperty(const std::string& key,
 
   return std::string(value);
 }
+
+bool DoesCastConfigFileExist() {
+  base::MemoryMappedFile::Region config_region;
+  int config_fd =
+      base::android::OpenApkAsset(kCastConfigAssetPath, &config_region);
+  return config_fd > 0;
+}
 }  // namespace
 
 CastSysInfoAndroid::CastSysInfoAndroid()
@@ -43,26 +51,17 @@ CastSysInfoAndroid::CastSysInfoAndroid()
 CastSysInfoAndroid::~CastSysInfoAndroid() {}
 
 CastSysInfo::BuildType CastSysInfoAndroid::GetBuildType() {
+  // TODO(b/110375912): Update this to parse the file contents and allow
+  // selection based on the config values.  For now this is just checking for
+  // file existence.
+  if (!DoesCastConfigFileExist())
+    return BUILD_PRODUCTION;
+
   if (CAST_IS_DEBUG_BUILD())
     return BUILD_ENG;
 
-  int build_number;
-  if (!base::StringToInt(CAST_BUILD_INCREMENTAL, &build_number))
-    build_number = 0;
-
-  const std::string channel(GetSystemReleaseChannel());
-  if (strcmp(build_info_->build_type(), kBuildTypeUser) == 0 &&
-      build_number > 0 && (channel.empty() || channel == "stable-channel")) {
-    return BUILD_PRODUCTION;
-  }
-
-  // Dogfooders without a user system build should all still have non-Debug
-  // builds of the cast receiver APK, but with valid build numbers.
-  if (build_number > 0)
-    return BUILD_BETA;
-
-  // Default to ENG build.
-  return BUILD_ENG;
+  // Default to BETA build.
+  return BUILD_BETA;
 }
 
 std::string CastSysInfoAndroid::GetSerialNumber() {
