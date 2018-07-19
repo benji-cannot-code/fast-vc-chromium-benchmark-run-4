a7bd1d990cd8572a51263bdec1cffea07716f829
Mounir Lamouri <mlamouri@chromium.org>
2018-07-19 02:22:43 +0000

    VideoSurfaceLayer: submit empty CompositorFrame before stopping frame submission.
    This leads to better memory performance as the last frame will be discarded.
    
    Bug: 829813
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I7bc753e7e8d74d80d8c8390bd0f33ed45003ed5a
    Reviewed-on: https://chromium-review.googlesource.com/1141291
    Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576331}

--- third_party/blink/renderer/platform/graphics/video_frame_submitter_test.cc
+++ third_party/blink/renderer/platform/graphics/video_frame_submitter_test.cc
@@ -288,7 +288,7 @@ TEST_F(VideoFrameSubmitterTest, ShouldSubmitPreventsSubmission) {
       .WillOnce(Return(media::VideoFrame::CreateFrame(
           media::PIXEL_FORMAT_YV12, gfx::Size(8, 8), gfx::Rect(gfx::Size(8, 8)),
           gfx::Size(8, 8), base::TimeDelta())));
-  EXPECT_CALL(*sink_, DoSubmitCompositorFrame(_, _));
+  EXPECT_CALL(*sink_, DoSubmitCompositorFrame(_, _)).Times(1);
   EXPECT_CALL(*provider_, PutCurrentFrame());
   EXPECT_CALL(*resource_provider_, AppendQuads(_, _, _));
   EXPECT_CALL(*resource_provider_, PrepareSendToParent(_, _));
@@ -297,6 +297,8 @@ TEST_F(VideoFrameSubmitterTest, ShouldSubmitPreventsSubmission) {
   scoped_task_environment_.RunUntilIdle();
 
   EXPECT_CALL(*sink_, SetNeedsBeginFrame(false));
+  EXPECT_CALL(*sink_, DoSubmitCompositorFrame(_, _)).Times(1);
+  EXPECT_CALL(*provider_, GetCurrentFrame()).Times(0);
   submitter_->UpdateSubmissionState(false);
   scoped_task_environment_.RunUntilIdle();
 
