53196f3a999dfff92deb4adcad977e815b504cc1
Wenzhao Zang <wzang@chromium.org>
2018-07-19 03:26:22 +0000

    cros: Disable daily refresh when user selects a new wallpaper
    This CL is a combination of small changes based on the meeting with PM
    and UX, It includes:
    
    1) Disable daily refresh when another wallpaper is selected.
    2) Make the entire area of daily refresh thumbnail clickable.
    3) Add shadow to preview top bar and selected thumbnails.
    4) Adjust spacing for buttons in current wallpaper info bar.
    5) Switch the colors for enabled/disabled states for the buttons.
    6) Change the color of the button text.
    7) Prevent restoring the previous active windows in tablet mode after
       the picker closes (i.e. continue to show the new wallpaper or home
       launcher).
    8) Hide scroll bar if there's no scroll event after a timeout.
    
    Bug: 863491
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: Id0aa1913f2bb0820909d7c714805cb723426a316
    Reviewed-on: https://chromium-review.googlesource.com/1137486
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Commit-Queue: Wenzhao (Colin) Zang <wzang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576346}

--- chrome/browser/resources/chromeos/wallpaper_manager/js/wallpaper_images_grid.js
+++ chrome/browser/resources/chromeos/wallpaper_manager/js/wallpaper_images_grid.js
@@ -98,6 +98,9 @@ cr.define('wallpapers', function() {
           if (e.keyCode == 13)
             this.parentNode.selectedItem = this.dataItem;
         });
+        this.addEventListener('mousedown', e => {
+          e.preventDefault();
+        });
       }
 
       imageEl.classList.add('thumbnail');
@@ -706,10 +709,9 @@ cr.define('wallpapers', function() {
       if (!this.dailyRefreshItem.querySelector('.daily-refresh-banner')) {
         var dailyRefreshBanner = document.querySelector('.daily-refresh-banner')
                                      .cloneNode(true /*deep=*/);
-        wallpaperManager.decorateDailyRefreshSlider(
-            this.collectionId,
-            dailyRefreshBanner.querySelector('.daily-refresh-slider'));
         this.dailyRefreshItem.appendChild(dailyRefreshBanner);
+        wallpaperManager.decorateDailyRefreshItem(
+            this.collectionId, this.dailyRefreshItem);
       }
 
       slideShowImage.style.opacity =
@@ -729,17 +731,12 @@ cr.define('wallpapers', function() {
       var images = this.dailyRefreshImages;
       if (images.length <= index)
         return;
-      images[index].style.opacity = 1;
-
-      if (images.length > 1) {
-        var previousIndex = (index - 1) % images.length;
-        if (previousIndex < 0)
-          previousIndex += images.length;
-        images[previousIndex].style.opacity = 0;
-        var nextIndex = (index + 1) % images.length;
-        this.dailyRefreshTimer_ =
-            window.setTimeout(this.showNextImage_.bind(this, nextIndex), 3000);
+      for (var i = 0; i < images.length; ++i) {
+        images[i].style.opacity = i === index ? 1 : 0;
       }
+      var nextIndex = (index + 1) % images.length;
+      this.dailyRefreshTimer_ =
+          window.setTimeout(this.showNextImage_.bind(this, nextIndex), 3000);
     },
 
     /**
