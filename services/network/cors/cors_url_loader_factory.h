7b2392366e90697d8d8df20212687fbd13b3050a
Yutaka Hirano <yhirano@chromium.org>
2018-07-19 10:01:41 +0000

    Reject insane requests in CORSURLLoaderFactory
    With this CL, CORSURLLoaderFactory rejects ill-configuared requests.
    
     - CORS needs a proper origin (including an opaque unique origin)
       attached to a request. Hence CORSURLLoaderFactory rejects a request
       which has a CORS-enabled mode and null request_initiator. Also,
       a request with null request_initiator won't set the CORS flag with
       this CL.
     - The relationship between fetch credentials mode and load_flags is
       a bit unclear. If a request's credentials mode is "omit" but
       one of LOAD_DO_NOT_SAVE_COOKIES, LOAD_DO_NOT_SEND_COOKIES and
       LOAD_DO_NOT_SEND_AUTH_DATA is not set on load_flags, that is likely
       a mis-configuration, so fail the request.
    
    Bug: 736308, 862184
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Change-Id: I51fb491b865de330b22b028a0eddbc30043e6b69
    Reviewed-on: https://chromium-review.googlesource.com/1136342
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576430}

--- services/network/cors/cors_url_loader_factory.h
+++ services/network/cors/cors_url_loader_factory.h
@@ -63,6 +63,8 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) CORSURLLoaderFactory final
 
   void DeleteIfNeeded();
 
+  static bool IsSane(const ResourceRequest& request);
+
   mojo::BindingSet<mojom::URLLoaderFactory> bindings_;
 
   // Used when constructed by NetworkContext.
