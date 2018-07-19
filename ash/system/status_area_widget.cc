988a98d746e5c3a9de613925c293cbc9aa6e75f8
Sammie Quon <sammiequon@google.com>
2018-07-19 16:00:59 +0000

    system: Fix tray transition on login when changing to tablet mode.
    Test: manual
    Bug: 864236
    Change-Id: I6429eb0c469ab473d5edf5db57e717c20e1d28fe
    Reviewed-on: https://chromium-review.googlesource.com/1141080
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576512}

--- ash/system/status_area_widget.cc
+++ ash/system/status_area_widget.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/tray/system_tray.h"
 #include "ash/system/unified/unified_system_tray.h"
 #include "ash/system/virtual_keyboard/virtual_keyboard_tray.h"
-#include "ash/wm/tablet_mode/tablet_mode_controller.h"
 #include "base/command_line.h"
 #include "base/i18n/time_formatting.h"
 #include "ui/base/ui_base_features.h"
@@ -190,11 +189,12 @@ void StatusAreaWidget::SetSystemTrayVisibility(bool visible) {
 }
 
 TrayBackgroundView* StatusAreaWidget::GetSystemTrayAnchor() const {
-  if (Shell::Get()
-          ->tablet_mode_controller()
-          ->IsTabletModeWindowManagerEnabled()) {
+  // Use the target visibility of the layer instead of the visibility of the
+  // view because the view is still visible when fading away, but we do not want
+  // to anchor to this element in that case.
+  if (overview_button_tray_->layer()->GetTargetVisibility())
     return overview_button_tray_.get();
-  }
+
   if (unified_system_tray_)
     return unified_system_tray_.get();
   return system_tray_.get();
