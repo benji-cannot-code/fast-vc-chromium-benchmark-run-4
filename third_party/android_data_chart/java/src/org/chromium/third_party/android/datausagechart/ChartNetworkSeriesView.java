d9ad96731677033bf22526b7ef6d43c91e1bae80
Doug Arnett <dougarnett@chromium.org>
2018-07-19 19:57:06 +0000

    Only show Data Saver usage data since enabled if less than 30 days
    Includes some refactoring in how the network stats data is reconciled
    with time of day for presenting day labels. The previous TimeZoneOffset
    approach really didn't work and that became apparent when charting
    a single day.
    
    Bug: 820286
    Change-Id: If14b456aaef6ae5729a9292842364f4115d0a563
    Reviewed-on: https://chromium-review.googlesource.com/1111080
    Reviewed-by: Ben Greenstein <bengr@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Doug Arnett <dougarnett@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576596}

--- third_party/android_data_chart/java/src/org/chromium/third_party/android/datausagechart/ChartNetworkSeriesView.java
+++ third_party/android_data_chart/java/src/org/chromium/third_party/android/datausagechart/ChartNetworkSeriesView.java
@@ -140,6 +140,9 @@ public class ChartNetworkSeriesView extends View {
     public void setBounds(long start, long end) {
         mStart = start;
         mEnd = end;
+        if (end > mEndTime) {
+            mEndTime = end;
+        }
     }
 
     /**
@@ -172,7 +175,7 @@ public class ChartNetworkSeriesView extends View {
         mPathValid = true;
 
         // bail when not enough stats to render
-        if (mStats == null || mStats.size() < 2) {
+        if (mStats == null || mStats.size() < 1) {
             return;
         }
 
@@ -275,6 +278,16 @@ public class ChartNetworkSeriesView extends View {
         } else {
             return maxVisible;
         }
+    }
+
+    /** Returns the total network byte count in the current stats. */
+    public long getMaxStats() {
+        if (mStats == null) {
+            return 0;
+        }
+        final NetworkStatsHistory.Entry entry =
+                mStats.getValues(mStats.getStart(), mStats.getEnd(), null);
+        return entry.rxBytes + entry.txBytes;
     }
 
     @Override
