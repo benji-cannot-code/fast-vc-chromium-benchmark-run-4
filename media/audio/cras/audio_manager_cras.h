1aab1627b8505c11f15381591b353b39f4f36468
Hsin-Yu Chao <hychao@chromium.org>
2018-07-19 16:06:07 +0000

    Expose experimental AEC on Chrome OS
    Bug: 710465
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I6c42f67335f9335ac3e8d2e003bfdde1d6bfa433
    Reviewed-on: https://chromium-review.googlesource.com/1086893
    Commit-Queue: Hsinyu Chao <hychao@chromium.org>
    Reviewed-by: Oskar Sundbom <ossu@chromium.org>
    Reviewed-by: Henrik Andreasson <henrika@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576514}

--- media/audio/cras/audio_manager_cras.h
+++ media/audio/cras/audio_manager_cras.h
@@ -80,6 +80,9 @@ class MEDIA_EXPORT AudioManagerCras : public AudioManagerBase {
   // Get default output buffer size for this board.
   int GetDefaultOutputBufferSizePerBoard();
 
+  // Get if system AEC is supported or not for this board.
+  bool GetSystemAecSupportedPerBoard();
+
   void GetAudioDeviceNamesImpl(bool is_input, AudioDeviceNames* device_names);
 
   std::string GetHardwareDeviceFromDeviceId(
@@ -98,6 +101,8 @@ class MEDIA_EXPORT AudioManagerCras : public AudioManagerBase {
                                               base::WaitableEvent* event);
   void GetDefaultOutputBufferSizeOnMainThread(int32_t* buffer_size,
                                               base::WaitableEvent* event);
+  void GetSystemAecSupportedOnMainThread(bool* system_aec_supported,
+                                         base::WaitableEvent* event);
 
   void WaitEventOrShutdown(base::WaitableEvent* event);
 
