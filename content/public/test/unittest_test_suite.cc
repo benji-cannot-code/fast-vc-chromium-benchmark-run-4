56a7e0812dbbf9c3eeb24f3e6ae3c1908c12ee4d
Sergey Ulanov <sergeyu@chromium.org>
2018-07-19 03:52:14 +0000

    [Fuchsia] use scenic ozone platform by default.
    Previously headless ozone platform was used on Fuchsia. Switched it to
    scenic. content_unittests fail with scenic, so this change also adds
    --ozone-platforms=headless flag in the unittests, until this issue
    is resolved.
    
    Bug: 829980, 865172
    Change-Id: I8a0b282953cf90554dc78ed1575d927b53374c58
    Reviewed-on: https://chromium-review.googlesource.com/1142492
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576360}

--- content/public/test/unittest_test_suite.cc
+++ content/public/test/unittest_test_suite.cc
@@ -22,17 +22,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/x/x11.h"
 #endif
 
+#if defined(OS_FUCHSIA)
+#include "ui/ozone/public/ozone_switches.h"
+#endif
+
 namespace content {
 
 UnitTestTestSuite::UnitTestTestSuite(base::TestSuite* test_suite)
     : test_suite_(test_suite) {
-  const base::CommandLine* command_line =
-      base::CommandLine::ForCurrentProcess();
+  base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
   std::string enabled =
       command_line->GetSwitchValueASCII(switches::kEnableFeatures);
   std::string disabled =
       command_line->GetSwitchValueASCII(switches::kDisableFeatures);
   feature_list_.InitFromCommandLine(enabled, disabled);
+
+#if defined(OS_FUCHSIA)
+  // Use headless ozone platform on Fuchsia by default.
+  // TODO(crbug.com/865172): Remove this flag.
+  if (!command_line->HasSwitch(switches::kOzonePlatform))
+    command_line->AppendSwitchASCII(switches::kOzonePlatform, "headless");
+#endif
 
 #if defined(USE_X11)
   XInitThreads();
