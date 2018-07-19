b66231a58ec2499fd6555e56e6e8521a60300cff
Thomas Guilbert <tguilbert@chromium.org>
2018-07-19 05:03:58 +0000

    Fix HasSingleSecurityOrigin for HLS
    HLS manifests can request segments from a different origin than the
    original manifest's origin. We do not inspect HLS manifests within
    Chromium, and instead delegate to Android's MediaPlayer. This means we
    need to be conservative, and always assume segments might come from a
    different origin. HasSingleSecurityOrigin should always return false
    when decoding HLS.
    
    Bug: 864283
    Change-Id: Ie16849ac6f29ae7eaa9caf342ad0509a226228ef
    Reviewed-on: https://chromium-review.googlesource.com/1142691
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576378}

--- media/blink/webmediaplayer_impl.cc
+++ media/blink/webmediaplayer_impl.cc
@@ -1127,6 +1127,12 @@ bool WebMediaPlayerImpl::DidGetOpaqueResponseFromServiceWorker() const {
 }
 
 bool WebMediaPlayerImpl::HasSingleSecurityOrigin() const {
+  if (demuxer_found_hls_) {
+    // HLS manifests might pull segments from a different origin. We can't know
+    // for sure, so we conservatively say no here.
+    return false;
+  }
+
   if (data_source_)
     return data_source_->HasSingleOrigin();
   return true;
@@ -1531,6 +1537,8 @@ void WebMediaPlayerImpl::OnError(PipelineStatus status) {
 
 #if defined(OS_ANDROID)
   if (status == PipelineStatus::DEMUXER_ERROR_DETECTED_HLS) {
+    demuxer_found_hls_ = true;
+
     renderer_factory_selector_->SetUseMediaPlayer(true);
 
     pipeline_controller_.Stop();
