39ac64bec7ec0666de8d2176eb093c24859d1f6f
Kenichi Ishibashi <bashi@chromium.org>
2018-07-19 06:05:43 +0000

    S13nSW: Skip ServiceWorkerTimeoutTimerTest.NonS13nServiceWorker
    We should just do early return when S13nServiceWorker is on
    because we want to make the flag on by default in the future.
    
    Bug: 860361
    Change-Id: I241b491f7f49e47b7cbfa5bfaeb1fab1978442db
    Reviewed-on: https://chromium-review.googlesource.com/1143050
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576389}

--- content/renderer/service_worker/service_worker_timeout_timer_unittest.cc
+++ content/renderer/service_worker/service_worker_timeout_timer_unittest.cc
@@ -274,7 +274,8 @@ TEST_F(ServiceWorkerTimeoutTimerTest, SetIdleTimerDelayToZero) {
 }
 
 TEST_F(ServiceWorkerTimeoutTimerTest, NonS13nServiceWorker) {
-  ASSERT_FALSE(blink::ServiceWorkerUtils::IsServicificationEnabled());
+  if (blink::ServiceWorkerUtils::IsServicificationEnabled())
+    return;
 
   MockEvent event;
   {
