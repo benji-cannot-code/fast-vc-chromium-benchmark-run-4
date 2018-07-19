0277c0c70c8ef0f8cd7ffe6668846dcd08835fd9
Shakti Sahu <shaktisahu@chromium.org>
2018-07-19 02:09:34 +0000

    Download Home: Menu items - search and delete
    Hooked up the menu items to their respective functionalites.
    Implemented functionalities for search and delete.
    
    Bug: 850605
    Change-Id: I89186c21481ed834f92ac4b5915a41d3e830b273
    Reviewed-on: https://chromium-review.googlesource.com/1141208
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576324}

--- chrome/android/java/src/org/chromium/chrome/browser/download/home/list/DateOrderedListMediator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/home/list/DateOrderedListMediator.java
@@ -29,6 +29,7 @@ import org.chromium.components.offline_items_collection.OfflineItem;
 import org.chromium.components.offline_items_collection.VisualsCallback;
 
 import java.io.Closeable;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -125,6 +126,11 @@ class DateOrderedListMediator {
         }
     }
 
+    /** Called to delete a list of items specified by {@code items}. */
+    public void onDeletionRequested(List<ListItem> items) {
+        onDeleteItems(getOfflineItems(items));
+    }
+
     /**
      * @return The {@link OfflineItemFilterSource} that should be used to determine which filter
      *         options are available.
@@ -169,6 +175,16 @@ class DateOrderedListMediator {
                 new ThumbnailRequestGlue(mProvider, item, iconWidthPx, iconHeightPx, callback);
         mThumbnailProvider.getThumbnail(request);
         return () -> mThumbnailProvider.cancelRetrieval(request);
+    }
+
+    private List<OfflineItem> getOfflineItems(List<ListItem> items) {
+        List<OfflineItem> offlineItems = new ArrayList<>();
+        for (ListItem item : items) {
+            if (item instanceof ListItem.OfflineItemListItem) {
+                offlineItems.add(((ListItem.OfflineItemListItem) item).item);
+            }
+        }
+        return offlineItems;
     }
 
     /** Helper class to disable animations for certain list changes. */
