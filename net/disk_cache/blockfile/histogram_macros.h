720e9a55d082afc42408318ae9b5bebf2dcbbe63
Mythri Alle <mythria@chromium.org>
2018-07-19 12:23:23 +0000

    Add a separate GeneratedCodeCache to decouple HttpCache and metadata
    Storing the generated code for javascript resources in a separate
    cache gives more flexibility in the implementation of the code
    cache.More details in the design doc [1]
    
    This cl just adds the GeneratedCodeCache class but does not
    use it yet. There will be follow up cls to actually use
    the cache.
    
    [1] https://docs.google.com/document/d/1O_PVZPn37Ev3_DWJjQLX-gid__OKxnXP1UsE8g_YkRY/edit?usp=sharing
    
    Bug: chromium:812168
    Change-Id: I04b709d8e443217f8235859731e75fdf94c62971
    Reviewed-on: https://chromium-review.googlesource.com/1107703
    Commit-Queue: Mythri Alle <mythria@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576453}

--- net/disk_cache/blockfile/histogram_macros.h
+++ net/disk_cache/blockfile/histogram_macros.h
@@ -103,6 +103,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       case net::PNACL_CACHE:                                         \
         CACHE_HISTOGRAM_##type(my_name.data(), sample);              \
         break;                                                       \
+      case net::GENERATED_CODE_CACHE:                                \
+        break;                                                       \
     }                                                                \
   }
 
--- net/disk_cache/simple/simple_histogram_macros.h
+++ net/disk_cache/simple/simple_histogram_macros.h
@@ -19,27 +19,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // TODO(pasko): add histograms for shader cache as soon as it becomes possible
 // for a user to get shader cache with the |SimpleBackendImpl| without altering
 // any flags.
-#define SIMPLE_CACHE_UMA(uma_type, uma_name, cache_type, ...)          \
-  do {                                                                 \
-    switch (cache_type) {                                              \
-      case net::DISK_CACHE:                                            \
-        SIMPLE_CACHE_THUNK(                                            \
-            uma_type, ("SimpleCache.Http." uma_name, ##__VA_ARGS__));  \
-        break;                                                         \
-      case net::APP_CACHE:                                             \
-        SIMPLE_CACHE_THUNK(                                            \
-            uma_type, ("SimpleCache.App." uma_name, ##__VA_ARGS__));   \
-        break;                                                         \
-      case net::MEDIA_CACHE:                                           \
-        SIMPLE_CACHE_THUNK(                                            \
-            uma_type, ("SimpleCache.Media." uma_name, ##__VA_ARGS__)); \
-        break;                                                         \
-      case net::SHADER_CACHE:                                          \
-        break;                                                         \
-      default:                                                         \
-        NOTREACHED();                                                  \
-        break;                                                         \
-    }                                                                  \
+// TODO(mythria): add histograms for generated_code_cache when we actually start
+// using the generated_code_cache.
+#define SIMPLE_CACHE_UMA(uma_type, uma_name, cache_type, ...)               \
+  do {                                                                      \
+    switch (cache_type) {                                                   \
+      case net::DISK_CACHE:                                                 \
+        SIMPLE_CACHE_THUNK(uma_type,                                        \
+                           ("SimpleCache.Http." uma_name, ##__VA_ARGS__));  \
+        break;                                                              \
+      case net::APP_CACHE:                                                  \
+        SIMPLE_CACHE_THUNK(uma_type,                                        \
+                           ("SimpleCache.App." uma_name, ##__VA_ARGS__));   \
+        break;                                                              \
+      case net::MEDIA_CACHE:                                                \
+        SIMPLE_CACHE_THUNK(uma_type,                                        \
+                           ("SimpleCache.Media." uma_name, ##__VA_ARGS__)); \
+        break;                                                              \
+      case net::GENERATED_CODE_CACHE:                                       \
+      case net::SHADER_CACHE:                                               \
+        break;                                                              \
+      default:                                                              \
+        NOTREACHED();                                                       \
+        break;                                                              \
+    }                                                                       \
   } while (0)
 
 #endif  // NET_DISK_CACHE_SIMPLE_SIMPLE_HISTOGRAM_MACROS_H_
