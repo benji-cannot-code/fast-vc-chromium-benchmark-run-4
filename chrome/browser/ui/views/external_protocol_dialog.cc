daea3d8d77798478b04b009b16ab1f099a23d1b2
Dominick Ng <dominickn@chromium.org>
2018-07-19 02:49:06 +0000

    Set Cancel as the default button for the external protocol dialog.
    This ensures that users holding down the Enter or Space key cannot
    accidentally trigger an external protocol launch.
    
    BUG=865202
    
    Change-Id: I2cec7b3c216b80641200c97dec2517a66b4e0b24
    Reviewed-on: https://chromium-review.googlesource.com/1142705
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576340}

--- chrome/browser/ui/views/external_protocol_dialog.cc
+++ chrome/browser/ui/views/external_protocol_dialog.cc
@@ -58,6 +58,10 @@ bool ExternalProtocolDialog::ShouldShowCloseButton() const {
   return false;
 }
 
+int ExternalProtocolDialog::GetDefaultDialogButton() const {
+  return ui::DIALOG_BUTTON_CANCEL;
+}
+
 base::string16 ExternalProtocolDialog::GetDialogButtonLabel(
     ui::DialogButton button) const {
   return delegate_->GetDialogButtonLabel(button);
