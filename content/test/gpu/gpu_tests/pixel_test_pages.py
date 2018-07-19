07b0742b3ff67f3d6719eee465ff432451f267d3
Maggie Chen <magchen@chromium.org>
2018-07-19 23:06:42 +0000

    Fix the Pixel_DirectComposition_Nonroot test failures
    In this Pixel_DirectComposition_Nonroot test (see
    \content\test\data\gpu\pixel_video_nonroot.html) -webkit-gradient
    is used and there is an issue with -gradient that it shows different
    images on different machines. See http://crbug.com/865556.
    
    The solutoin is to uppdate the solid color expectation area so only
    the areas without the gradient effect are compared. The purpose of
    this test is to exercise the nonroot path, not to test the gradient
    functionality. The area with gradient will be put back after crbug
    865556 is fixed.
    
    media/test/data/README is also updated with more info on four-colors.mp4
    
    Note:Direct Composition nonroot feature is not enabled by default except
    for the protect video. This nonroot path is not used for most cases.
    
    Bug: 865346
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I51b9cf15944bff5d26fd58b24611a560b51785e4
    Reviewed-on: https://chromium-review.googlesource.com/1144242
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576680}

--- content/test/gpu/gpu_tests/pixel_test_pages.py
+++ content/test/gpu/gpu_tests/pixel_test_pages.py
@@ -792,7 +792,7 @@ def DirectCompositionPages(base_name):
         {
           'comment': 'red top right quadrant',
           'location': [124, 4],
-          'size': [110, 60],
+          'size': [50, 60],
           'color': [255, 17, 24],
           'tolerance': 3
         },
@@ -806,7 +806,7 @@ def DirectCompositionPages(base_name):
         {
           'comment': 'green bottom right quadrant',
           'location': [124, 72],
-          'size': [110, 60],
+          'size': [50, 60],
           'color': [44, 255, 16],
           'tolerance': 3
         }
