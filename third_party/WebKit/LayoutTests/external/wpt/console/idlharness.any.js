fb01afc60a1be63599d2a40bac2732e1b821a4be
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-07-19 10:48:08 +0000

    Import wpt@a6e4d3fc416ef91ed9aa856522a2255608da2c30
    Using wpt-import in Chromium 96e636376ade5f6a8964dfd5e93a22c6cc1aebeb.
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/21544
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    kozyatinskiy@chromium.org:
      external/wpt/console
    
    TBR=foolip
    
    No-Export: true
    Change-Id: Ia010fb7467e3a5df5870eb88e74ef9d4abf03aab
    Reviewed-on: https://chromium-review.googlesource.com/1143018
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576435}

---
+++ third_party/WebKit/LayoutTests/external/wpt/console/idlharness.any.js
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/resources/WebIDLParser.js
+// META: script=/resources/idlharness.js
+
+// https://console.spec.whatwg.org/
+
+promise_test(async () => {
+  const srcs = ['console'];
+  const [idl] = await Promise.all(
+      srcs.map(i => fetch(`/interfaces/${i}.idl`).then(r => r.text())));
+
+  const idl_array = new IdlArray();
+  idl_array.add_idls(idl);
+  idl_array.test();
+}, 'console interfaces');
