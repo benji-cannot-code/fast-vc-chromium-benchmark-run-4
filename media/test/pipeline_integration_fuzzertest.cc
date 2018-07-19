cbd841090ab3654e1c737de242efe245ca6de7c6
Dale Curtis <dalecurtis@chromium.org>
2018-07-19 02:37:24 +0000

    Add profile parsing from the 'av1C' MP4 atom. Enable fuzzing.
    This implements reading of the 'seq_profile' value from the
    'av1C' atom per the spec description here:
    https://aomediacodec.github.io/av1-isobmff/#av1codecconfigurationbox
    https://aomediacodec.github.io/av1-spec/av1-spec.pdf (Section 5.2)
    
    This also re-enables previously disabled tests and adds the various
    bits necessary for av1 fuzzer tests.
    
    Bug: 757047, 784993, 784607
    Test: Updated unittests and fuzzertests.
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I05d8d42c8c022b7dc508ab8d212ad846ae1de8b6
    Reviewed-on: https://chromium-review.googlesource.com/1107260
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Chrome Cunningham (In Paris) <chcunningham@chromium.org>
    Reviewed-by: Tom Finegan <tomfinegan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576335}

--- media/test/pipeline_integration_fuzzertest.cc
+++ media/test/pipeline_integration_fuzzertest.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/pipeline_status.h"
 #include "media/test/mock_media_source.h"
 #include "media/test/pipeline_integration_test_base.h"
+#include "third_party/libaom/av1_buildflags.h"
 
 namespace {
 
@@ -32,6 +33,9 @@ enum FuzzerVariant {
   WEBM_VP8,
   WEBM_VP9,
   WEBM_OPUS_VP9,
+#if BUILDFLAG(ENABLE_AV1_DECODER)
+  MP4_AV1,
+#endif
   MP4_FLAC,
   MP3,
 #if BUILDFLAG(USE_PROPRIETARY_CODECS)
@@ -62,6 +66,10 @@ std::string MseFuzzerVariantEnumToMimeTypeString(FuzzerVariant variant) {
       return "video/webm; codecs=\"vp9\"";
     case WEBM_OPUS_VP9:
       return "video/webm; codecs=\"opus,vp9\"";
+#if BUILDFLAG(ENABLE_AV1_DECODER)
+    case MP4_AV1:
+      return "video/mp4; codecs=\"av01.0.04M.08\"";
+#endif  // BUILDFLAG(ENABLE_AV1_DECODER)
     case MP4_FLAC:
       return "audio/mp4; codecs=\"flac\"";
     case MP3:
