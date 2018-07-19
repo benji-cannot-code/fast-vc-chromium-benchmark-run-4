a67712fbff0ef49ee149e09bb72906a8b28cd2be
Henrik Boström <hbos@chromium.org>
2018-07-19 11:09:45 +0000

    Implement & ship: RTCRtpTransceiver in Unified Plan.
    Intent: https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/yC_UowCRnaY
    
    This CL implements and ships the following:
    - RTCRtpTransceiver
      Everything except setCodecPreferences(), as originally planned, and
      stop(), which recently is being up for spec discussions for
      unresolved issues. stop() is not essential, you can simply set
      transceiver.direction = "inactive".
    - RTCPeerConnection.getTransceivers().
    - RTCTrackEvent (and RTCTrackEventInit) gets the transceiver attribute
      previously missing.
    
    RTCPeerConnection.addTransceiver() will be added in the next CL.
    
    Transceivers are only supported if sdpSemantics:'unified-plan' is
    used, for more information see the intent to ship which explains the
    background.
    
    In "Unified Plan", the following methods get a change in behavior
    because of this CL:
    - RTCPeerConnection.addTrack()
    - RTCPeerConnection.removeTrack()
    - RTCPeerConnection.setLocalDescription()
    - RTCPeerConnection.setRemoteDescription()
    
    When transceivers are not supported, getTransceivers() simply return
    an empty set.
    
    external/wpt/webrtc/ runs in default "Plan B" mode,
    virtual/webrtc-wpt-unified-plan/external/wpt/webrtc/ runs in "Unified
    Plan" mode. -expected.txt files are updated accordingly. If there is
    no -expected.txt file in "virtual" then it uses the same
    -expected.txt as the non-"virtual" if there is one; PASS-only files
    are needed in "virtual" if the non-"virtual" one contained any FAIL
    (as opposed to ommiting the -expected.txt file because everything
    passed).
    
    // All the bots already passed except for one unrelated test failure
    // on a single bot. NOTRY to skip waiting for retry.
    NOTRY=True
    
    Bug: 777617
    Change-Id: Ifd4d39e4f90511ebf4871536da210c3e8a1d08d0
    Reviewed-on: https://chromium-review.googlesource.com/1141963
    Commit-Queue: Henrik Boström <hbos@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576439}

--- third_party/blink/renderer/modules/peerconnection/rtc_track_event.h
+++ third_party/blink/renderer/modules/peerconnection/rtc_track_event.h
@@ -16,6 +16,7 @@ namespace blink {
 class MediaStream;
 class MediaStreamTrack;
 class RTCRtpReceiver;
+class RTCRtpTransceiver;
 class RTCTrackEventInit;
 
 // https://w3c.github.io/webrtc-pc/#rtctrackevent
@@ -27,11 +28,13 @@ class RTCTrackEvent final : public Event {
                                const RTCTrackEventInit& eventInitDict);
   RTCTrackEvent(RTCRtpReceiver*,
                 MediaStreamTrack*,
-                const HeapVector<Member<MediaStream>>&);
+                const HeapVector<Member<MediaStream>>&,
+                RTCRtpTransceiver*);
 
   RTCRtpReceiver* receiver() const;
   MediaStreamTrack* track() const;
   HeapVector<Member<MediaStream>> streams() const;
+  RTCRtpTransceiver* transceiver() const;
 
   void Trace(blink::Visitor*) override;
 
@@ -42,6 +45,7 @@ class RTCTrackEvent final : public Event {
   Member<RTCRtpReceiver> receiver_;
   Member<MediaStreamTrack> track_;
   HeapVector<Member<MediaStream>> streams_;
+  Member<RTCRtpTransceiver> transceiver_;
 };
 
 }  // namespace blink
