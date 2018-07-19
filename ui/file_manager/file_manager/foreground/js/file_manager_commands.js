fd69a54014c5dd9636c97ee0bcf1ebc9c49136f0
Naoki Fukino <fukino@chromium.org>
2018-07-19 08:37:34 +0000

    Files app: Remember "Show all Play folders" option.
    When "Show all Play folders" is toggled, we make modification on FileFilter
    instance which filter visible folders/files.
    FileFilter dispatches 'changed' event, and we can remember the current option
    on the event handler.
    
    Bug: 863200
    Test: Manually tested on ARC-enabled devices.
    Change-Id: I517cabfea04e1c17e2e2a844fb2ca35c4becb648
    Reviewed-on: https://chromium-review.googlesource.com/1143101
    Reviewed-by: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Commit-Queue: Naoki Fukino <fukino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576421}

--- ui/file_manager/file_manager/foreground/js/file_manager_commands.js
+++ ui/file_manager/file_manager/foreground/js/file_manager_commands.js
@@ -834,6 +834,8 @@ CommandHandler.COMMANDS_['toggle-hidden-android-folders'] =
             !!fileManager.volumeManager.getCurrentProfileVolumeInfo(
                 VolumeManagerCommon.VolumeType.ANDROID_FILES);
         event.command.setHidden(!event.canExecute);
+        event.command.checked =
+            fileManager.fileFilter.isAllAndroidFoldersVisible();
       }
     });
 
