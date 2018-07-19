7898509192e8dc9e47dd4bdc910ba7d5c4d0fc7c
David Tseng <dtseng@chromium.org>
2018-07-19 01:40:33 +0000

    Do not trigger braille blinking if there is no cursor
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: I9e4eb9de5bfd5876711bde8b77367ccd7be67aba
    Reviewed-on: https://chromium-review.googlesource.com/1142577
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576307}

--- chrome/browser/resources/chromeos/chromevox/braille/pan_strategy.js
+++ chrome/browser/resources/chromeos/chromevox/braille/pan_strategy.js
@@ -320,6 +320,14 @@ cvox.PanStrategy.prototype = {
   },
 
   /**
+   * Gets the current cursor.
+   * @return {{start: (number), end: (number)}}
+   */
+  getCursor: function() {
+    return this.cursor_;
+  },
+
+  /**
    *
    */
   maybeSetWrappedCursor_: function(unwrappedIndex, wrappedIndex) {
