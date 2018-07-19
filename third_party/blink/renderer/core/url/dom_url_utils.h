e941a1f1cbba7d21c8bdac61c188a1f6dd4ff648
Maja Kabus <kabusm@google.com>
2018-07-19 16:45:51 +0000

    HTMLAnchorElement::href and HTMLAreaElement::href changed to accept Trusted Types
    Existing implementation of setHref moved from DOMURLUtils
    to DOMURL
    New implementation of setHref added to DOMURLUtils
    New implementations of href() added to DOMURLUtilsReadOnly
    to accept URLString as an argument
    
    Bug: 739170
    Change-Id: I047d6a564e65a3be78436faef8780f60b85e8f7c
    Reviewed-on: https://chromium-review.googlesource.com/1143391
    Commit-Queue: Maja Kabus <kabusm@google.com>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576525}

--- third_party/blink/renderer/core/url/dom_url_utils.h
+++ third_party/blink/renderer/core/url/dom_url_utils.h
@@ -34,7 +34,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class ExceptionState;
+class ScriptState;
 class KURL;
+class USVStringOrTrustedURL;
 
 class CORE_EXPORT DOMURLUtils : public DOMURLUtilsReadOnly {
  public:
@@ -42,7 +45,7 @@ class CORE_EXPORT DOMURLUtils : public DOMURLUtilsReadOnly {
   virtual void SetInput(const String&) = 0;
   ~DOMURLUtils() override;
 
-  void setHref(const String&);
+  void setHref(ScriptState*, const USVStringOrTrustedURL&, ExceptionState&);
 
   void setProtocol(const String&);
   void setUsername(const String&);
