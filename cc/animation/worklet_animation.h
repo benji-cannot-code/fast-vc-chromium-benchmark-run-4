d7171cb44dcccb753285f09a2a459f04a2586f4d
Majid Valipour <majidvp@chromium.org>
2018-07-19 21:16:41 +0000

    [animation-worklet] apply worklet animation local time immediately
    Fix a bug in our logic where ticking worklet animations does not ensure
    that the updated local time is actually applied to update the animation
    output.
    
    The relevant logic is in AnimationHost::SetMutationUpdate where
    we set the local time but don't tick the animations.
    
    This was not noticeable for the most part because in many situations
    we call AnimationHost::Animate twice in the same frame which masks
    the issues. Though this becomes noticeable where we have main
    thread scrolling. See [1] for more details.
    
    The fix is to first tick mutator which updates worklet animations
    output state (in particular their local time) and then tick animations
    (which includes worklet animations). This reverses the previous order.
    
    
    Other minor changes:
    - Add a test for ensuring we don't tick main side worklet animation
    - Remove unnecessary call to SetNeedsPushProperties inside SetOutputState.
      We no longer push local_time_ from main to impl so it does not
      make sense to ask for pushing props when it is set.
    - Make WorkletAnimation::local_time_ an optional value instead of
      incorrectly initializing it to base::TimeDelta(). In fact this
      was masking a bug in worklet-animation-local-time-before-start.html
    
    [1] https://bugs.chromium.org/p/chromium/issues/detail?id=844436#c1
    
    
    Bug: 844436
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I0273c2ee1ad473d5675ea83cc3fcfd2a7956b6cd
    Reviewed-on: https://chromium-review.googlesource.com/1066358
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Commit-Queue: Majid Valipour <majidvp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576632}

--- cc/animation/worklet_animation.h
+++ cc/animation/worklet_animation.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CC_ANIMATION_WORKLET_ANIMATION_H_
 #define CC_ANIMATION_WORKLET_ANIMATION_H_
 
+#include "base/gtest_prod_util.h"
 #include "base/optional.h"
 #include "base/time/time.h"
 #include "cc/animation/animation_export.h"
@@ -14,6 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/trees/property_tree.h"
 
 namespace cc {
+
+namespace {
+FORWARD_DECLARE_TEST(WorkletAnimationTest, NonImplInstanceDoesNotTickKeyframe);
+}  // namespace
 
 class AnimationOptions;
 class ScrollTimeline;
@@ -68,6 +73,16 @@ class CC_ANIMATION_EXPORT WorkletAnimation final
   void RemoveKeyframeModel(int keyframe_model_id) override;
 
  private:
+  FRIEND_TEST_ALL_PREFIXES(WorkletAnimationTest,
+                           NonImplInstanceDoesNotTickKeyframe);
+  WorkletAnimation(int cc_animation_id,
+                   WorkletAnimationId worklet_animation_id,
+                   const std::string& name,
+                   std::unique_ptr<ScrollTimeline> scroll_timeline,
+                   std::unique_ptr<AnimationOptions> options,
+                   bool is_controlling_instance,
+                   std::unique_ptr<KeyframeEffect> effect);
+
   ~WorkletAnimation() override;
 
   // Returns the current time to be passed into the underlying AnimationWorklet.
@@ -99,9 +114,14 @@ class CC_ANIMATION_EXPORT WorkletAnimation final
 
   std::unique_ptr<AnimationOptions> options_;
 
-  base::TimeDelta local_time_;
+  // Local time is used as an input to the keyframe effect of this animation.
+  // The value comes from the user script that runs inside the animation worklet
+  // global scope.
+  base::Optional<base::TimeDelta> local_time_;
 
   base::Optional<base::TimeTicks> start_time_;
+  // Last current time used for updatig. We use this to skip updating if current
+  // time has not changed since last update.
   base::Optional<double> last_current_time_;
 
   State state_;
