3497f6e17d4509632c8f5cf8fb0fd865d3ec846f
Elly Fong-Jones <ellyjones@chromium.org>
2018-07-19 19:41:56 +0000

    macviews: use cocoa task manager
    The Views task manager is not ready to go yet.
    
    Bug: 865557
    Change-Id: Ie6fbb6aa52baf5825e5b8025dd31ae2f40ac458f
    Reviewed-on: https://chromium-review.googlesource.com/1144135
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576592}

--- chrome/browser/ui/cocoa/task_manager_mac.mm
+++ chrome/browser/ui/cocoa/task_manager_mac.mm
@@ -42,10 +42,6 @@ NSString* ColumnIdentifier(int id) {
 }
 
 bool ShouldUseViewsTaskManager() {
-#if BUILDFLAG(MAC_VIEWS_BROWSER)
-  if (!views_mode_controller::IsViewsBrowserCocoa())
-    return true;
-#endif
   return base::FeatureList::IsEnabled(features::kViewsTaskManager);
 }
 
