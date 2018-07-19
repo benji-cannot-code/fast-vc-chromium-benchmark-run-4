6d5ccffce77a2b19c0b67cc276de82a2ee18bdac
stkhapugin@chromium.org <stkhapugin@chromium.org>
2018-07-19 16:19:12 +0000

    Use UI Refresh icons for suggestions in the omnibox.
    Uses new completion icons in the omnibox.
    
    Bug: 864990
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ic062601ec2b0eb665c8e80779ffc04d90bbb5723
    Reviewed-on: https://chromium-review.googlesource.com/1143274
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576516}

--- ios/chrome/browser/ui/omnibox/omnibox_mediator.mm
+++ ios/chrome/browser/ui/omnibox/omnibox_mediator.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/omnibox/omnibox_mediator.h"
 
+#include "base/strings/sys_string_conversions.h"
 #import "ios/chrome/browser/ui/omnibox/omnibox_consumer.h"
 #include "ios/chrome/browser/ui/omnibox/omnibox_util.h"
 #import "ios/chrome/browser/ui/uikit_ui_util.h"
@@ -19,9 +20,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma mark - OmniboxLeftImageConsumer
 
 - (void)setLeftImageForAutocompleteType:(AutocompleteMatchType::Type)type {
-  int imageId = GetIconForAutocompleteMatchType(type, /* is_starred */ false,
-                                                /* is_incognito */ false);
-  UIImage* image = [NativeImage(imageId)
+  std::string imageName =
+      GetResourceNameForAutocompleteMatchType(type, /* is_starred */ false);
+  UIImage* image = [[UIImage imageNamed:base::SysUTF8ToNSString(imageName)]
       imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
   [self.consumer updateAutocompleteIcon:image];
 }
