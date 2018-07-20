6fe761772b6a1c20175d6b08096ac2935a83c402
Stephen McGruer <smcgruer@chromium.org>
2018-07-20 15:05:20 +0000

    A few small code health fixes in DocumentTimeline
    This CL adds a test for EffectiveTime, and also removes unnecessary
    checks of |document_| in the DocumentTimeline code (because the
    constructor already DCHECKs it, and it is then held in a Member which
    will keep the |document_| alive).
    
    Bug: None
    Change-Id: I7ede1064280d681f881ef398214ee14872eacf2c
    Reviewed-on: https://chromium-review.googlesource.com/1138856
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Majid Valipour <majidvp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576862}

--- third_party/blink/renderer/core/animation/document_timeline.cc
+++ third_party/blink/renderer/core/animation/document_timeline.cc
@@ -96,7 +96,7 @@ DocumentTimeline::DocumentTimeline(Document* document,
 }
 
 bool DocumentTimeline::IsActive() {
-  return document_ && document_->GetPage();
+  return document_->GetPage();
 }
 
 void DocumentTimeline::AnimationAttached(Animation& animation) {
@@ -106,9 +106,6 @@ void DocumentTimeline::AnimationAttached(Animation& animation) {
 }
 
 Animation* DocumentTimeline::Play(AnimationEffect* child) {
-  if (!document_)
-    return nullptr;
-
   Animation* animation = Animation::Create(child, this);
   DCHECK(animations_.Contains(animation));
 
@@ -198,7 +195,7 @@ void DocumentTimeline::DocumentTimelineTiming::WakeAfter(double duration) {
 }
 
 void DocumentTimeline::DocumentTimelineTiming::ServiceOnNextFrame() {
-  if (timeline_->document_ && timeline_->document_->View())
+  if (timeline_->document_->View())
     timeline_->document_->View()->ScheduleAnimation();
 }
 
@@ -208,7 +205,7 @@ void DocumentTimeline::DocumentTimelineTiming::Trace(blink::Visitor* visitor) {
 }
 
 TimeTicks DocumentTimeline::ZeroTime() {
-  if (!zero_time_initialized_ && document_ && document_->Loader()) {
+  if (!zero_time_initialized_ && document_->Loader()) {
     zero_time_ = document_->Loader()->GetTiming().ReferenceMonotonicTime() +
                  origin_time_;
     zero_time_initialized_ = true;
