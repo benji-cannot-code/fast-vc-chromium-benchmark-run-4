058750d5b8302581a5d289de4b35f0639a57e9ee
Xiaoqian Dai <xdai@chromium.org>
2018-07-20 18:18:02 +0000

    TabDragging: The dragged window in preview area case.
    When dragging a window in overview grid, if the window is dragged into
    the preview area, and a left or right semitransparent preview window
    shows up indicating the dragged window will be snapped to left or right
    side of the screen after releasing the mouse/touch, we should clear the
    selection widget on the current selected overview window item and also
    clear the ash::kIsDeferredTabDraggingTargetWindowKey property to prevent
    the dragged window to merge into the selected overview window item.
    
    See before https://drive.google.com/open?id=14fwUX8yXUoAnJrOklOGDcG2ChZFMpIVJ
    and after https://drive.google.com/open?id=1Ps1VXj03UMceMDeZ3_puMSG-PluHAQG2
    
    Bug: 823769
    Change-Id: I0cc1a009dc6efc07f3baca5519cbc26e9dbb1ec3
    Reviewed-on: https://chromium-review.googlesource.com/1142483
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576927}

--- ash/wm/overview/window_grid.cc
+++ ash/wm/overview/window_grid.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/overview/window_selector.h"
 #include "ash/wm/overview/window_selector_delegate.h"
 #include "ash/wm/overview/window_selector_item.h"
+#include "ash/wm/splitview/split_view_drag_indicators.h"
 #include "ash/wm/window_state.h"
 #include "base/i18n/string_search.h"
 #include "base/numerics/ranges.h"
@@ -584,7 +585,8 @@ void WindowGrid::OnWindowDragStarted(aura::Window* dragged_window) {
 }
 
 void WindowGrid::OnWindowDragContinued(aura::Window* dragged_window,
-                                       const gfx::Point& location_in_screen) {
+                                       const gfx::Point& location_in_screen,
+                                       IndicatorState indicator_state) {
   DCHECK_EQ(dragged_window->GetRootWindow(), root_window_);
   // Find the window selector item that contains |location_in_screen|.
   auto iter = std::find_if(
@@ -595,6 +597,25 @@ void WindowGrid::OnWindowDragContinued(aura::Window* dragged_window,
 
   aura::Window* target_window =
       (iter != window_list_.end()) ? (*iter)->GetWindow() : nullptr;
+
+  if (indicator_state == IndicatorState::kPreviewAreaLeft ||
+      indicator_state == IndicatorState::kPreviewAreaRight) {
+    // If the dragged window is currently dragged into preview window area,
+    // clear the selection widget.
+    if (SelectedWindow()) {
+      SelectedWindow()->set_selected(false);
+      selection_widget_.reset();
+    }
+
+    // Also clear ash::kIsDeferredTabDraggingTargetWindowKey key on the target
+    // window selector item so that it can't merge into this window selector
+    // item if the dragged window is currently in preview window area.
+    if (target_window && !IsNewSelectorItemWindow(target_window))
+      target_window->ClearProperty(ash::kIsDeferredTabDraggingTargetWindowKey);
+
+    return;
+  }
+
   // If |location_in_screen| is contained by one of the eligible window selector
   // item in overview, show the selection widget.
   if (target_window && (IsNewSelectorItemWindow(target_window) ||
