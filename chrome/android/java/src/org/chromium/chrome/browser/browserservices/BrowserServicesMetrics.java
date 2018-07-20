a0e7e0a1d745c770512800aed7b4968fabd780e4
Peter E Conn <peconn@chromium.org>
2018-07-20 19:04:24 +0000

    🤝 Launch TWAs from notifications.
    Check whether an URL can be handled by a Trusted Web Activity when
    launched from background Android Services (such as by notifications).
    
    This change also rolls the custom_tabs_client DEP, this roll includes:
    057eae40 TrustedWebActivityService can report notifications enabled.
    dde0cb7b Improved error reporting for clients.
    e8d760cd Better support for client developers.
    81a14bc0 Expose getVerifiedPackages, required by this CL.
    
    Bug: 860104
    Change-Id: Ib901a1d4a569e382b168d2c16ea639584913f890
    Reviewed-on: https://chromium-review.googlesource.com/1127177
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576940}

--- chrome/android/java/src/org/chromium/chrome/browser/browserservices/BrowserServicesMetrics.java
+++ chrome/android/java/src/org/chromium/chrome/browser/browserservices/BrowserServicesMetrics.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.browserservices;
 
+import android.os.SystemClock;
 import android.support.annotation.IntDef;
 
 import org.chromium.base.metrics.RecordHistogram;
@@ -56,6 +57,38 @@ public class BrowserServicesMetrics {
      */
     public static void recordTwaOpenTime(long duration, TimeUnit unit) {
         RecordHistogram.recordTimesHistogram("BrowserServices.TwaOpenTime", duration, unit);
+    }
+
+    /**
+     * Returns a {@link TimingMetric} that records the amount of time spent querying the Android
+     * system for ResolveInfos that will deal with a given URL when launching from a background
+     * service.
+     */
+    public static TimingMetric getServiceTabResolveInfoTimingContext() {
+        return new TimingMetric("BrowserServices.ServiceTabResolveInfoQuery");
+    }
+
+    /**
+     * A class to be used with a try-with-resources to record the elapsed time within the try block.
+     */
+    public static class TimingMetric implements AutoCloseable {
+        private final String mMetric;
+        private final long mStart;
+
+        private static long now() {
+            return SystemClock.uptimeMillis();
+        }
+
+        public TimingMetric(String metric) {
+            mMetric = metric;
+            mStart = now();
+        }
+
+        @Override
+        public void close() {
+            RecordHistogram.recordMediumTimesHistogram(
+                    mMetric, now() - mStart, TimeUnit.MILLISECONDS);
+        }
     }
 
     // Don't let anyone instantiate.
