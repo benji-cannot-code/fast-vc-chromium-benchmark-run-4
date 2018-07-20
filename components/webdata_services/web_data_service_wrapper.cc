75a74c5ab265a4f155ac287006dcb80e2c234166
Florian Uunk <feuunk@chromium.org>
2018-07-20 15:53:36 +0000

    Hook account store up to sync.
    Plumb the account store into ChromeSyncClient, and use it when the
    feature flag is on.
    
    Add an integration test to make sure the account storage is used, and
    not the profile storage.
    
    BUG=854643
    
    Change-Id: Icae8fb8d5e15856ec81ad108502f829d320922d4
    Reviewed-on: https://chromium-review.googlesource.com/1100835
    Commit-Queue: Florian Uunk <feuunk@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576878}

--- components/webdata_services/web_data_service_wrapper.cc
+++ components/webdata_services/web_data_service_wrapper.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/webdata/autofill_wallet_metadata_syncable_service.h"
 #include "components/autofill/core/browser/webdata/autofill_wallet_syncable_service.h"
 #include "components/autofill/core/browser/webdata/autofill_webdata_service.h"
+#include "components/autofill/core/common/autofill_features.h"
 #include "components/password_manager/core/browser/webdata/logins_table.h"
 #include "components/search_engines/keyword_table.h"
 #include "components/search_engines/keyword_web_data_service.h"
@@ -45,7 +46,7 @@ namespace {
 // TODO(jkrcal): Rename this function when the last webdata sync type get
 // converted to USS, e.g. to InitSyncBridgesOnDBSequence(). Check also other
 // related functions.
-void InitSyncableServicesOnDBSequence(
+void InitSyncableProfileServicesOnDBSequence(
     scoped_refptr<base::SingleThreadTaskRunner> db_task_runner,
     const syncer::SyncableService::StartSyncFlare& sync_flare,
     const scoped_refptr<autofill::AutofillWebDataService>& autofill_web_data,
@@ -69,12 +70,22 @@ void InitSyncableServicesOnDBSequence(
         autofill_web_data.get())
         ->InjectStartSyncFlare(sync_flare);
   }
+}
 
+void InitSyncableAccountServicesOnDBSequence(
+    scoped_refptr<base::SingleThreadTaskRunner> db_task_runner,
+    const syncer::SyncableService::StartSyncFlare& sync_flare,
+    const scoped_refptr<autofill::AutofillWebDataService>& autofill_web_data,
+    const base::FilePath& context_path,
+    const std::string& app_locale,
+    autofill::AutofillWebDataBackend* autofill_backend) {
+  DCHECK(db_task_runner->RunsTasksInCurrentSequence());
   autofill::AutofillWalletSyncableService::CreateForWebDataServiceAndBackend(
       autofill_web_data.get(), autofill_backend, app_locale);
   autofill::AutofillWalletMetadataSyncableService::
       CreateForWebDataServiceAndBackend(autofill_web_data.get(),
                                         autofill_backend, app_locale);
+
   autofill::AutofillWalletSyncableService::FromWebDataService(
       autofill_web_data.get())
       ->InjectStartSyncFlare(sync_flare);
@@ -146,27 +157,40 @@ WebDataServiceWrapper::WebDataServiceWrapper(
       ui_task_runner);
 #endif
 
-  profile_autofill_web_data_->GetAutofillBackend(
-      base::Bind(&InitSyncableServicesOnDBSequence, db_task_runner, flare,
-                 profile_autofill_web_data_, context_path, application_locale));
+  profile_autofill_web_data_->GetAutofillBackend(base::Bind(
+      &InitSyncableProfileServicesOnDBSequence, db_task_runner, flare,
+      profile_autofill_web_data_, context_path, application_locale));
 
-  account_database_ =
-      new WebDatabaseService(base::FilePath(WebDatabase::kInMemoryPath),
-                             ui_task_runner, db_task_runner);
-  account_database_->AddTable(std::make_unique<autofill::AutofillTable>());
-  account_database_->LoadDatabase();
+  if (base::FeatureList::IsEnabled(
+          autofill::features::kAutofillEnableAccountWalletStorage)) {
+    account_database_ =
+        new WebDatabaseService(base::FilePath(WebDatabase::kInMemoryPath),
+                               ui_task_runner, db_task_runner);
+    account_database_->AddTable(std::make_unique<autofill::AutofillTable>());
+    account_database_->LoadDatabase();
 
-  account_autofill_web_data_ = new autofill::AutofillWebDataService(
-      account_database_, ui_task_runner, db_task_runner,
-      base::Bind(show_error_callback, ERROR_LOADING_ACCOUNT_AUTOFILL));
-  account_autofill_web_data_->Init();
+    account_autofill_web_data_ = new autofill::AutofillWebDataService(
+        account_database_, ui_task_runner, db_task_runner,
+        base::Bind(show_error_callback, ERROR_LOADING_ACCOUNT_AUTOFILL));
+    account_autofill_web_data_->Init();
+    account_autofill_web_data_->GetAutofillBackend(base::Bind(
+        &InitSyncableAccountServicesOnDBSequence, db_task_runner, flare,
+        account_autofill_web_data_, context_path, application_locale));
+
+  } else {
+    account_autofill_web_data_ = nullptr;
+    profile_autofill_web_data_->GetAutofillBackend(base::Bind(
+        &InitSyncableAccountServicesOnDBSequence, db_task_runner, flare,
+        profile_autofill_web_data_, context_path, application_locale));
+  }
 }
 
 WebDataServiceWrapper::~WebDataServiceWrapper() {}
 
 void WebDataServiceWrapper::Shutdown() {
   profile_autofill_web_data_->ShutdownOnUISequence();
-  account_autofill_web_data_->ShutdownOnUISequence();
+  if (account_autofill_web_data_)
+    account_autofill_web_data_->ShutdownOnUISequence();
   keyword_web_data_->ShutdownOnUISequence();
   token_web_data_->ShutdownOnUISequence();
 
@@ -179,7 +203,8 @@ void WebDataServiceWrapper::Shutdown() {
 #endif
 
   profile_database_->ShutdownDatabase();
-  account_database_->ShutdownDatabase();
+  if (account_database_)
+    account_database_->ShutdownDatabase();
 }
 
 scoped_refptr<autofill::AutofillWebDataService>
