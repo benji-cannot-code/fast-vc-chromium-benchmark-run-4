d631b22dd247001bc8c6234658d8f3f4e54b0d99
Danyao Wang <danyao@chromium.org>
2018-07-20 17:00:20 +0000

    [Nav Experiment] Fix "decidePolicy callback not called" crash.
    By calling the decision handler with NO when repost dialog is dismissed.
    
    WKBasedNavigationManager presents repost dialog in
    -webView:decidePolicyForNavigationAction, with a completion handler
    that calls |decisionHandler| with Allow or Cancel, depending on user
    action. However, if the webview is navigated to a new page while the
    repost dialog is still presenting, the dialog is simply dismissed and
    the policy decision handler is never called. This raises an exception
    in WebKit.
    
    Also added an egtest for this scenario. Verified that this test
    crashes without the fix in this CL.
    
    Bug: 854750
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I419906e2f3619dc017d4a9dcbbb5b124bd9b1816
    Reviewed-on: https://chromium-review.googlesource.com/1144448
    Commit-Queue: Danyao Wang <danyao@chromium.org>
    Reviewed-by: Peter Lee <pkl@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576904}

--- ios/chrome/browser/ui/alert_coordinator/repost_form_coordinator.mm
+++ ios/chrome/browser/ui/alert_coordinator/repost_form_coordinator.mm
@@ -21,6 +21,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   UIAlertController* _dialogController;
   // Number of attempts to show the repost form action sheet.
   NSUInteger _repostAttemptCount;
+  // A completion handler to be called when the dialog is dismissed.
+  void (^_dismissCompletionHandler)(void);
 }
 
 // Creates a new UIAlertController to use for the dialog.
@@ -50,6 +52,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         [[self class] newDialogControllerForSourceView:webState->GetView()
                                             sourceRect:sourceRect
                                      completionHandler:completionHandler];
+    // The dialog may be dimissed when a new navigation starts while the dialog
+    // is still presenting. This should be treated as a NO from user.
+    // See https://crbug.com/854750 for a case why this matters.
+    _dismissCompletionHandler = ^{
+      completionHandler(NO);
+    };
   }
   return self;
 }
@@ -95,8 +103,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)stop {
   [_dialogController.presentingViewController
       dismissViewControllerAnimated:YES
-                         completion:nil];
+                         completion:_dismissCompletionHandler];
   _repostAttemptCount = 0;
+  _dismissCompletionHandler = nil;
 }
 
 #pragma mark - Private
