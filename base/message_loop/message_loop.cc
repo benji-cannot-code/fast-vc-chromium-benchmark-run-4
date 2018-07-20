6cd2a9da0777fe3948471fa336be821381535ec9
Gabriel Charette <gab@chromium.org>
2018-07-20 14:31:33 +0000

    [MessageLoop] Do not schedule sleeps > 1 day
    Some platforms don't like timeouts > 100,000,000 seconds.
    
    Note: I tried just banning long delays but it didn't work
    (https://bugs.chromium.org/p/chromium/issues/detail?id=850450#c4).
    And while we could control delays posted by our codebase (and we should
    still try to address that), we can't control those set from Javascript.
    
    R=kylechar@chromium.org
    
    Bug: 850450
    Change-Id: Iaf1b983b7af1954412518ed049f6cd0a3f78ba2a
    Reviewed-on: https://chromium-review.googlesource.com/1142602
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576857}

--- base/message_loop/message_loop.cc
+++ base/message_loop/message_loop.cc
@@ -455,6 +455,10 @@ void MessageLoop::ScheduleWork() {
   pump_->ScheduleWork();
 }
 
+TimeTicks MessageLoop::CapAtOneDay(TimeTicks next_run_time) {
+  return std::min(next_run_time, recent_time_ + TimeDelta::FromDays(1));
+}
+
 bool MessageLoop::DoWork() {
   if (!task_execution_allowed_)
     return false;
@@ -503,7 +507,7 @@ bool MessageLoop::DoDelayedWork(TimeTicks* next_delayed_work_time) {
   if (next_run_time > recent_time_) {
     recent_time_ = TimeTicks::Now();  // Get a better view of Now();
     if (next_run_time > recent_time_) {
-      *next_delayed_work_time = next_run_time;
+      *next_delayed_work_time = CapAtOneDay(next_run_time);
       return false;
     }
   }
@@ -511,8 +515,8 @@ bool MessageLoop::DoDelayedWork(TimeTicks* next_delayed_work_time) {
   PendingTask pending_task = incoming_task_queue_->delayed_tasks().Pop();
 
   if (incoming_task_queue_->delayed_tasks().HasTasks()) {
-    *next_delayed_work_time =
-        incoming_task_queue_->delayed_tasks().Peek().delayed_run_time;
+    *next_delayed_work_time = CapAtOneDay(
+        incoming_task_queue_->delayed_tasks().Peek().delayed_run_time);
   }
 
   return DeferOrRunPendingTask(std::move(pending_task));
