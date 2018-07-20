43daaee6ce69aa7a957501a5d73c35726fe38670
Mitsuru Oshima <oshima@chromium.org>
2018-07-20 19:16:24 +0000

    Toggle maximize when double clicked on wide frame
    BUG=865977
    TEST=Covered by unit test
    
    
    Change-Id: I444981ec98241d2a355d23d9711a961010a292c3
    Reviewed-on: https://chromium-review.googlesource.com/1144628
    Reviewed-by: Jun Mukai <mukai@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576947}

--- ash/frame/wide_frame_view.h
+++ ash/frame/wide_frame_view.h
@@ -63,6 +63,7 @@ class ASH_EXPORT WideFrameView
 
   // views::View:
   void Layout() override;
+  void OnMouseEvent(ui::MouseEvent* event) override;
 
   // aura::WindowObserver:
   void OnWindowDestroying(aura::Window* window) override;
