5a1245c4077432e314e03d7ea874e8005cbaad5c
Bill Orr <billorr@chromium.org>
2018-07-20 00:59:24 +0000

    Remove the WebVR Origin Trial.
    As previously announced (https://groups.google.com/a/chromium.org/d/msg/blink-dev/-s4pWwshKhI/M78h2_scBwAJ), the WebVR Origin Trial is ending after M68 as WebVR is replaced by the WebXR Device API and Origin Trial.
    
    WebVR can still be manually enabled through chrome://flags.
    
    BUG=758265
    
    Cq-Include-Trybots: luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I752ac8d61c8436abd80a511cc66d4aad502d0245
    Reviewed-on: https://chromium-review.googlesource.com/1144278
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Commit-Queue: Bill Orr <billorr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576732}

--- third_party/blink/renderer/modules/modules_initializer.cc
+++ third_party/blink/renderer/modules/modules_initializer.cc
@@ -240,7 +240,7 @@ void ModulesInitializer::OnClearWindowObjectInMainWorld(
   NavigatorGamepad::From(document);
   NavigatorServiceWorker::From(document);
   DOMWindowStorageController::From(document);
-  if (OriginTrials::WebVREnabled(document.GetExecutionContext()))
+  if (RuntimeEnabledFeatures::WebVREnabled())
     NavigatorVR::From(document);
   if (RuntimeEnabledFeatures::PresentationEnabled() &&
       settings.GetPresentationReceiver()) {
