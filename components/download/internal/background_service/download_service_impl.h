bd0acc097d3d52ed114e31b05a1e0c098f1559a5
Xing Liu <xingliu@chromium.org>
2018-07-20 02:22:24 +0000

    Background download: Use weak pointer for DownloadServiceImpl.
    Uses weak pointer in DownloadServiceImpl to fix a potential memory
    corruption in |pending_tasks_|.
    
    Bug: 860771
    Change-Id: Ib84f4c27aa4d2f1e7560434d41175ecba0a622e5
    Reviewed-on: https://chromium-review.googlesource.com/1144464
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576754}

--- components/download/internal/background_service/download_service_impl.h
+++ components/download/internal/background_service/download_service_impl.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/circular_deque.h"
 #include "base/macros.h"
+#include "base/memory/weak_ptr.h"
 #include "components/download/internal/background_service/config.h"
 #include "components/download/internal/background_service/service_config_impl.h"
 #include "components/download/public/background_service/download_service.h"
@@ -60,6 +61,8 @@ class DownloadServiceImpl : public DownloadService {
   base::circular_deque<base::OnceClosure> pending_actions_;
   std::map<DownloadTaskType, base::OnceClosure> pending_tasks_;
   bool startup_completed_;
+
+  base::WeakPtrFactory<DownloadServiceImpl> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(DownloadServiceImpl);
 };
