a80c27e554d754f9da734b947f856f0e45cfc860
Ryan Harrison <rharrison@chromium.org>
2018-07-20 17:50:49 +0000

    Display warning when PDF signals an attempt to save
    This CL adds a warning dialogue that is shown when a
    signal via SaveCalled is received. Currently this
    signal is only wired up for app.execMenuItem("SaveAs")
    in XFA JS, but will be added in other locations where
    saving is attempted.
    
    Once saving of PDFs is fully implemented this warning
    can be removed.
    
    BUG=pdfium:953,chromium:61248
    
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: Ib89c394a07459692ee1302ea97f7b0b291b42b92
    Reviewed-on: https://chromium-review.googlesource.com/1115205
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Ryan Harrison <rharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576920}

--- chrome/browser/resources/pdf/pdf.js
+++ chrome/browser/resources/pdf/pdf.js
@@ -630,6 +630,7 @@ PDFViewer.prototype = {
     $('zoom-toolbar').strings = strings;
     $('password-screen').strings = strings;
     $('error-screen').strings = strings;
+    $('save-called-screen').strings = strings;
   },
 
   /**
@@ -761,6 +762,9 @@ PDFViewer.prototype = {
         break;
       case 'transformPagePointReply':
         this.coordsTransformer_.onReplyReceived(message);
+        break;
+      case 'saveCalled':
+        $('save-called-screen').show();
         break;
     }
   },
