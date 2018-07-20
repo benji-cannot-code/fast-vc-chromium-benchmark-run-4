b5d8a721f15f57d4bf3c3da51b22d9b342779d5e
Oksana Zhuravlova <oksamyt@chromium.org>
2018-07-20 23:05:06 +0000

    Remove unused PlatformEventObserver and RendererGamepadProvider
    Bug: 855308
    Change-Id: I8c8ab5d3787dda1be5e7b075eb34f35555231201
    Reviewed-on: https://chromium-review.googlesource.com/1145583
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577025}

--- content/test/layouttest_support.cc
+++ content/test/layouttest_support.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/storage_partition.h"
 #include "content/public/common/page_state.h"
 #include "content/public/common/screen_info.h"
-#include "content/public/renderer/renderer_gamepad_provider.h"
 #include "content/renderer/gpu/layer_tree_view.h"
 #include "content/renderer/input/render_widget_input_handler_delegate.h"
 #include "content/renderer/layout_test_dependencies.h"
