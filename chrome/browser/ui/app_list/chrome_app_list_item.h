21c367aae962d0561471c0b62c6461aa0414ffcb
Yury Khmel <khmel@chromium.org>
2018-07-20 20:41:52 +0000

    Reland "Create crostini app folder and add all Crostini apps to it"
    This reverts commit 348548619c27222450db9be6f1253e49394f19df.
    
    Reason for reland: Crash fixed.
    
    Original change's description:
    > Revert "Create crostini app folder and add all Crostini apps to it"
    > 
    > This reverts commit b5e1688aeb2a34754ddae9cba5bedc4f8587bbf4.
    > 
    > Reason for revert: This CL seems to cause a crash when installing Terminal on Eve.
    > 
    > Original change's description:
    > > Create crostini app folder and add all Crostini apps to it
    > > 
    > > Bug: 822488
    > > Test: Manually, simulate crositini app environment.
    > > Change-Id: I278a6ddfc709b5b48f923368aad07f047788bf40
    > > Reviewed-on: https://chromium-review.googlesource.com/1143883
    > > Commit-Queue: Renée Wright <rjwright@chromium.org>
    > > Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#576703}
    > 
    > TBR=stevenjb@chromium.org,rjwright@chromium.org,khmel@google.com
    > 
    > Change-Id: I3104b7d7abbf885f100aedb223d31a4d73bed47c
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 822488
    > Reviewed-on: https://chromium-review.googlesource.com/1144500
    > Reviewed-by: Nicholas Verne <nverne@chromium.org>
    > Commit-Queue: Nicholas Verne <nverne@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#576714}
    
    TBR=stevenjb@chromium.org,rjwright@chromium.org,khmel@google.com,nverne@chromium.org
    
    Change-Id: I000b4ddbba75f11947570f4c2470332db3d80a11
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 822488
    Reviewed-on: https://chromium-review.googlesource.com/1145563
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Commit-Queue: Yury Khmel <khmel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576974}

--- chrome/browser/ui/app_list/chrome_app_list_item.h
+++ chrome/browser/ui/app_list/chrome_app_list_item.h
@@ -113,6 +113,9 @@ class ChromeAppListItem {
 
   std::string ToDebugString() const;
 
+  // Set the default position if it exists.
+  void SetDefaultPositionIfApplicable();
+
  protected:
   ChromeAppListItem(Profile* profile, const std::string& app_id);
 
@@ -130,9 +133,6 @@ class ChromeAppListItem {
   // Updates item position and name from |sync_item|. |sync_item| must be valid.
   void UpdateFromSync(
       const app_list::AppListSyncableService::SyncItem* sync_item);
-
-  // Set the default position if it exists.
-  void SetDefaultPositionIfApplicable();
 
   // Get the context menu of a certain app. This could be different for
   // different kinds of items.
