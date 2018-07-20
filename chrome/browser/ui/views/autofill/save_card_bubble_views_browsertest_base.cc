8e0fd04b36602c6d5a0819061a5939b023ee5b45
Jared Saul <jsaul@google.com>
2018-07-20 03:28:53 +0000

    [Autofill] Show Sign-in/Sync promo after saving a card locally and Manage Cards bubble.
    A sign-in promo pops up right after a card is saved locally. This also introduces
    a "Manage cards" bubble, with a footnote of a sign-in promo.
    
    An organized list of the diffs --
        https://screenshot.googleplex.com/kMBPkpWZYsq.png
    
    A rough doc I quickly put together of the changes in this CL --
        https://docs.google.com/document/d/1DarGyLBDSBWcsumcxhpojuvugUTUU6r5j6uI0RZZAo0/edit?usp=sharing
    
    Here are different screenshots explaining why we chose to have the promo not display a title --
    Using promo's title (The string isn't updated in this screenshot):
        https://screenshot.googleplex.com/kiYnAynyedD
    Empty promo title, using main bubble title:
        https://screenshot.googleplex.com/rT3F9RETsgq
    No promo title, using main bubble title:
        https://screenshot.googleplex.com/nMXOdNvysbR
    
    This is what the mocks were like (with the old UI):
        https://screenshot.googleplex.com/AhXfxONG0sO
    
    
    Additionally, this is what the sync promo looks like:
        https://screenshot.googleplex.com/J3huQ06Y5X8
    Sign-in promo on the Manage Cards view:
        https://screenshot.googleplex.com/MJqf5Fbhnpa
    Sync promo on the Manage Cards view:
        https://screenshot.googleplex.com/cNLy78Ts80k
    
    
    Metrics, additional browser tests, and omnibox icon animation to follow.
    
    Bug: 855186
    Change-Id: I74ecdd802da960120baacfdd771c90cb126fbd10
    Reviewed-on: https://chromium-review.googlesource.com/1134514
    Commit-Queue: Jared Saul <jsaul@google.com>
    Reviewed-by: Mathieu Perreault <mathp@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/master@{#576769}

--- chrome/browser/ui/views/autofill/save_card_bubble_views_browsertest_base.cc
+++ chrome/browser/ui/views/autofill/save_card_bubble_views_browsertest_base.cc
@@ -373,6 +373,7 @@ views::View* SaveCardBubbleViewsBrowserTestBase::FindViewInBubbleById(
 
   views::View* specified_view =
       save_card_bubble_views->GetViewByID(static_cast<int>(view_id));
+
   if (!specified_view) {
     // Many of the save card bubble's inner Views are not child views but rather
     // contained by its DialogClientView. If we didn't find what we were looking
@@ -390,6 +391,7 @@ views::View* SaveCardBubbleViewsBrowserTestBase::FindViewInBubbleById(
       specified_view = footnote_view->GetViewByID(static_cast<int>(view_id));
     }
   }
+
   return specified_view;
 }
 
