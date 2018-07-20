c86be884c76ab7a95fff1965bd280ea935bf1b27
Karan Bhatia <karandeepb@chromium.org>
2018-07-20 19:36:40 +0000

    ExtensionMessageFilter: Prevent data race.
    The |browser_context_| member of ExtensionMessageFilter is only supposed to be
    read on the UI thread, but is also read in OnMessageReceived which might not be
    invoked on the UI thread (for messages not handled by the
    ExtensionMessageFilter). This is racy and can lead to accessing the variable
    simultaneously on multiple threads.
    
    Remove the check for a valid |browser_context_| in
    ExtensionMessageFilter::OnMessageReceived. It's redundant anyway since all the
    message handler functions do already check for a valid browser context.
    
    BUG=862597
    
    Change-Id: Ib34ac14b9a1ff609224a9fdf80202070098760e9
    Reviewed-on: https://chromium-review.googlesource.com/1144606
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576953}

--- extensions/browser/extension_message_filter.cc
+++ extensions/browser/extension_message_filter.cc
@@ -119,10 +119,6 @@ void ExtensionMessageFilter::OnDestruct() const {
 }
 
 bool ExtensionMessageFilter::OnMessageReceived(const IPC::Message& message) {
-  // If we have been shut down already, return.
-  if (!browser_context_)
-    return true;
-
   bool handled = true;
   IPC_BEGIN_MESSAGE_MAP(ExtensionMessageFilter, message)
     IPC_MESSAGE_HANDLER(ExtensionHostMsg_AddListener,
@@ -430,6 +426,7 @@ void ExtensionMessageFilter::OnOpenChannelToTab(
 void ExtensionMessageFilter::OnOpenMessagePort(
     int routing_id,
     const PortId& port_id) {
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
   if (!browser_context_)
     return;
 
@@ -441,6 +438,7 @@ void ExtensionMessageFilter::OnCloseMessagePort(
     int routing_id,
     const PortId& port_id,
     bool force_close) {
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
   if (!browser_context_)
     return;
 
@@ -450,6 +448,7 @@ void ExtensionMessageFilter::OnCloseMessagePort(
 
 void ExtensionMessageFilter::OnPostMessage(const PortId& port_id,
                                            const Message& message) {
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
   if (!browser_context_)
     return;
 
