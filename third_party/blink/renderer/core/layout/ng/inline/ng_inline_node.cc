8ace01841da315766a398450238e343addf0f4eb
Koji Ishii <kojii@chromium.org>
2018-07-20 06:33:29 +0000

    [LayoutNG] Change static position of inline abpos to flow relative
    This patch:
    1. Fixes out-of-flow static position to be flow relative,
       from line-relative.
    2. Fixes bidi-reordering orders out-of-flow objects
       incorrectly.
    
    The first fix has been a bug since the beginning, but most
    RTL out-of-flow tests pass due to the second problem.
    
    The second problem was introduced when we stopped inserting
    an Object Replacement Character for out-of-flow, because it
    creates a break opportunity but all 4 impls do not create
    a break opportunity for out-of-flow. It wasn't a good idea
    because bidi algorithm cannot resolve embedding levels
    without a character that represents.
    
    This fix will change break opportunities around out-of-flow
    objects. We can fix it in NGLineBreaker if this turns out
    to be a problem.
    
    Bug: 848496
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng
    Change-Id: I3f3c7b5a8ddf93cb4b9d6fce21ccc5e2b7623901
    Reviewed-on: https://chromium-review.googlesource.com/1133106
    Reviewed-by: Aleks Totic <atotic@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576799}

--- third_party/blink/renderer/core/layout/ng/inline/ng_inline_node.cc
+++ third_party/blink/renderer/core/layout/ng/inline/ng_inline_node.cc
@@ -109,7 +109,8 @@ void CollectInlinesInternal(
                             kObjectReplacementCharacter, nullptr, node);
 
     } else if (node->IsOutOfFlowPositioned()) {
-      builder->AppendOpaque(NGInlineItem::kOutOfFlowPositioned, nullptr, node);
+      builder->AppendOpaque(NGInlineItem::kOutOfFlowPositioned,
+                            kObjectReplacementCharacter, nullptr, node);
 
     } else if (node->IsAtomicInlineLevel()) {
       if (node->IsLayoutNGListMarker()) {
@@ -478,8 +479,7 @@ void NGInlineNode::ShapeText(const String& text_content,
           break;
         end_offset = item.EndOffset();
       } else if (item.Type() == NGInlineItem::kOpenTag ||
-                 item.Type() == NGInlineItem::kCloseTag ||
-                 item.Type() == NGInlineItem::kOutOfFlowPositioned) {
+                 item.Type() == NGInlineItem::kCloseTag) {
         // These items are opaque to shaping.
         // Opaque items cannot have text, such as Object Replacement Characters,
         // since such characters can affect shaping.
