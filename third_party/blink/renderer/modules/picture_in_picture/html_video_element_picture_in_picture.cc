c7839f0001c40ad3daea641aee6dd9ea354b7836
François Beaufort <beaufort.francois@gmail.com>
2018-07-21 19:51:09 +0000

    Hide Picture-in-Picture button if video src is a MediaStream.
    This CL makes sure native video controls don't show a Picture-in-Picture
    button when video src is a MediaStream as backend is not implemented
    yet.
    
    Bug: 806249
    Change-Id: I04171b2ba73e3689334809c6419bcc8ff598196f
    Reviewed-on: https://chromium-review.googlesource.com/1145076
    Reviewed-by: apacible <apacible@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: François Beaufort <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/master@{#577083}

--- third_party/blink/renderer/modules/picture_in_picture/html_video_element_picture_in_picture.cc
+++ third_party/blink/renderer/modules/picture_in_picture/html_video_element_picture_in_picture.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/modules/picture_in_picture/html_video_element_picture_in_picture.h"
 
-#include "third_party/blink/public/platform/web_media_player.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
@@ -27,6 +26,8 @@ const char kMetadataNotLoadedError[] =
     "Metadata for the video element are not loaded yet.";
 const char kVideoTrackNotAvailableError[] =
     "The video element has no video track.";
+const char kMediaStreamsNotSupportedYet[] =
+    "Media Streams are not supported yet.";
 const char kFeaturePolicyBlocked[] =
     "Access to the feature \"picture-in-picture\" is disallowed by feature "
     "policy.";
@@ -61,6 +62,11 @@ ScriptPromise HTMLVideoElementPictureInPicture::requestPictureInPicture(
           script_state,
           DOMException::Create(DOMExceptionCode::kInvalidStateError,
                                kVideoTrackNotAvailableError));
+    case Status::kMediaStreamsNotSupportedYet:
+      return ScriptPromise::RejectWithDOMException(
+          script_state,
+          DOMException::Create(DOMExceptionCode::kNotSupportedError,
+                               kMediaStreamsNotSupportedYet));
     case Status::kDisabledByFeaturePolicy:
       return ScriptPromise::RejectWithDOMException(
           script_state, DOMException::Create(DOMExceptionCode::kSecurityError,
@@ -87,14 +93,6 @@ ScriptPromise HTMLVideoElementPictureInPicture::requestPictureInPicture(
     return ScriptPromise::RejectWithDOMException(
         script_state, DOMException::Create(DOMExceptionCode::kNotAllowedError,
                                            kUserGestureRequired));
-  }
-
-  // TODO(crbug.com/806249): Remove this when MediaStreams are supported.
-  if (element.GetLoadType() == WebMediaPlayer::kLoadTypeMediaStream) {
-    return ScriptPromise::RejectWithDOMException(
-        script_state,
-        DOMException::Create(DOMExceptionCode::kNotSupportedError,
-                             "MediaStreams are not supported yet."));
   }
 
   ScriptPromiseResolver* resolver = ScriptPromiseResolver::Create(script_state);
--- third_party/blink/renderer/modules/picture_in_picture/picture_in_picture_controller_impl.cc
+++ third_party/blink/renderer/modules/picture_in_picture/picture_in_picture_controller_impl.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/modules/picture_in_picture/picture_in_picture_controller_impl.h"
 
+#include "third_party/blink/public/platform/web_media_player.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
 #include "third_party/blink/renderer/core/dom/events/event.h"
@@ -77,6 +78,10 @@ PictureInPictureControllerImpl::IsElementAllowed(
 
   if (element.FastHasAttribute(HTMLNames::disablepictureinpictureAttr))
     return Status::kDisabledByAttribute;
+
+  // TODO(crbug.com/806249): Remove this when MediaStreams are supported.
+  if (element.GetLoadType() == WebMediaPlayer::kLoadTypeMediaStream)
+    return Status::kMediaStreamsNotSupportedYet;
 
   return Status::kEnabled;
 }
