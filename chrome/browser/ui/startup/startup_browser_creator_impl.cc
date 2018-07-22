f3aa37037a43edda982b35a38ab0a5a70d045a54
Jochen Eisinger <jochen@chromium.org>
2018-07-22 09:38:28 +0000

    Show an infobar to supervised users announcing EOL in M70
    Bug: 865157
    
    Change-Id: I6d04148640812e4a2de80866fee1545bd2ef1868
    Reviewed-on: https://chromium-review.googlesource.com/1141874
    Commit-Queue: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577091}

--- chrome/browser/ui/startup/startup_browser_creator_impl.cc
+++ chrome/browser/ui/startup/startup_browser_creator_impl.cc
@@ -102,6 +102,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/rlz/rlz_tracker.h"  // nogncheck
 #endif
 
+#if BUILDFLAG(ENABLE_SUPERVISED_USERS) && !defined(OS_ANDROID) && \
+    !defined(OS_CHROMEOS)
+#include "chrome/browser/ui/startup/supervised_users_deprecated_infobar_delegate.h"
+#endif
+
 using content::ChildProcessSecurityPolicy;
 using content::WebContents;
 using extensions::Extension;
@@ -925,6 +930,11 @@ void StartupBrowserCreatorImpl::AddInfoBarsIfNecessary(
           !local_state->GetBoolean(prefs::kSuppressUnsupportedOSWarning))
         ObsoleteSystemInfoBarDelegate::Create(infobar_service);
     }
+#if BUILDFLAG(ENABLE_SUPERVISED_USERS) && !defined(OS_ANDROID) && \
+    !defined(OS_CHROMEOS)
+    if (profile_->IsLegacySupervised())
+      SupervisedUsersDeprecatedInfoBarDelegate::Create(infobar_service);
+#endif
 
 #if !defined(OS_CHROMEOS)
     if (!command_line_.HasSwitch(switches::kNoDefaultBrowserCheck)) {
--- components/infobars/core/infobar_delegate.h
+++ components/infobars/core/infobar_delegate.h
@@ -154,6 +154,7 @@ class InfoBarDelegate {
     DOWNLOAD_PROGRESS_INFOBAR_ANDROID = 82,
     AR_CORE_UPGRADE_ANDROID = 83,
     BLOATED_RENDERER_INFOBAR_DELEGATE = 84,
+    SUPERVISED_USERS_DEPRECATED_INFOBAR_DELEGATE = 85,
   };
 
   // Describes navigation events, used to decide whether infobars should be
