27a62c46109e73e2231adf20005313891530de5f
Noel Gordon <noel@chromium.org>
2018-07-23 13:40:22 +0000

    Rename MultiProfileFilesAppBrowserTest
    Rename the FileManager part of the test fixture to FilesApp since it's
    all FilesApp these days, and it simplifies --gtest-filter.
    
    Bug: 836254
    Change-Id: Ibfd29300ced82fec3029864230c4db8f1e3accee
    Reviewed-on: https://chromium-review.googlesource.com/1146416
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577162}

--- services/identity/public/cpp/identity_manager.h
+++ services/identity/public/cpp/identity_manager.h
@@ -31,7 +31,7 @@ class UserSessionManager;
 
 // Necessary to declare this class as a friend.
 namespace file_manager {
-class MultiProfileFileManagerBrowserTest;
+class MultiProfileFilesAppBrowserTest;
 }
 
 // Necessary to declare these classes as friends.
@@ -186,7 +186,7 @@ class IdentityManager : public SigninManagerBase::Observer,
                                        const std::string& email);
   friend MultiProfileDownloadNotificationTest;
   friend ProfileSyncServiceHarness;
-  friend file_manager::MultiProfileFileManagerBrowserTest;
+  friend file_manager::MultiProfileFilesAppBrowserTest;
 
   // These clients needs to call SetPrimaryAccountSynchronously().
   friend ArcSupportHostTest;
