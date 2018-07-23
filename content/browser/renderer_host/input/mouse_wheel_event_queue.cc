1dde39a3d26eba09958d537360f67c5b27baf57f
Ehsan Karamad <ekaramad@chromium.org>
2018-07-23 22:20:40 +0000

    Do not swap X & Y for for shift-scrolling on Mac
    MacOSX scrollWheel event already provides the correct behavior. If the
    event (physical) source has precise deltas and comes from a trackpad
    or apple mouse then it has bidirectional deltas already and pressing
    shift key should not swap the axis. If the source of the event is an
    ordinary single-axis mouse, then the NSEvent already packs |delta_x|
    instead of |delta_y| (for shift-scroll).
    
    Also, it turns out that rails mode should only be used for trackpad. The
    logic in MosueWheelRailsFilter uses an auto-regressive filter to smooth
    out rails latching. This state is reset for a kPhaseScrollBegan. However,
    apple mice would always have a phase of kPhaseNone. Since it never resets
    it might lead to a large value in one axis (i.e., in y-axis after a
    vertical scroll phase) and when scrolling in the other direction occurs,
    the rails mode is set incorrectly. The side effects of this are zero
    scroll deltas which also fires a DCHECK in mouse_wheel_event_queue.cc:
    DCHECK(needs_update). To repro use an apple mouse and scroll vertically
    and then horizontally using the bidirectional wheel.
    
    Bug: 862661
    
    Change-Id: I4e5210454fe7352ab0230b312f44e1a374f22f0c
    Reviewed-on: https://chromium-review.googlesource.com/1134434
    Reviewed-by: ccameron <ccameron@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Ehsan Karamad <ekaramad@chromium.org>
    Commit-Queue: Ehsan Karamad <ekaramad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577296}

--- content/browser/renderer_host/input/mouse_wheel_event_queue.cc
+++ content/browser/renderer_host/input/mouse_wheel_event_queue.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/metrics/histogram_macros.h"
 #include "base/trace_event/trace_event.h"
+#include "build/build_config.h"
 #include "content/common/input/input_event_dispatch_type.h"
 #include "content/public/common/content_features.h"
 #include "ui/events/base_event_utils.h"
@@ -127,6 +128,7 @@ void MouseWheelEventQueue::ProcessMouseWheelAck(
         event_sent_for_gesture_ack_->event.PositionInScreen());
     scroll_update.resending_plugin_id = -1;
 
+#if !defined(OS_MACOSX)
     // Swap X & Y if Shift is down and when there is no horizontal movement.
     if ((event_sent_for_gesture_ack_->event.GetModifiers() &
          WebInputEvent::kShiftKey) != 0 &&
@@ -135,7 +137,9 @@ void MouseWheelEventQueue::ProcessMouseWheelAck(
           event_sent_for_gesture_ack_->event.delta_y;
       scroll_update.data.scroll_update.delta_y =
           event_sent_for_gesture_ack_->event.delta_x;
-    } else {
+    } else
+#endif  // OS_MACOSX
+    {
       scroll_update.data.scroll_update.delta_x =
           event_sent_for_gesture_ack_->event.delta_x;
       scroll_update.data.scroll_update.delta_y =
