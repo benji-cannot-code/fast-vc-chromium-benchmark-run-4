222639e215c4f5d54f76a8b70f69dbdf1f22b400
Owen Min <zmin@chromium.org>
2018-07-23 17:10:11 +0000

    Block dock and menu when EnterpriseStartupDialog displayed on Mac.
    Run the dialog modally on mac so that the dock and main menu are
    blocked.
    
    Bug: 865760
    Change-Id: I734ba42a3110b6a71fa2aaae807ef9b1f763a90d
    Reviewed-on: https://chromium-review.googlesource.com/1135728
    Commit-Queue: Owen Min <zmin@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577194}

--- chrome/browser/ui/views/policy/enterprise_startup_dialog_view_browsertest.cc
+++ chrome/browser/ui/views/policy/enterprise_startup_dialog_view_browsertest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/strings/utf_string_conversions.h"
+#include "build/build_config.h"
 #include "chrome/browser/ui/test/test_browser_dialog.h"
 
 namespace policy {
@@ -39,6 +40,16 @@ class EnterpriseStartupDialogViewBrowserTest : public DialogBrowserTest {
                                   base::ASCIIToUTF16(kButton));
     }
   }
+
+#if defined(OS_MACOSX)
+  // On mac, we need to wait until the dialog launched modally before closing
+  // it.
+  void DismissUi() override {
+    base::ThreadTaskRunnerHandle::Get()->PostTask(
+        FROM_HERE, base::BindOnce(&EnterpriseStartupDialogView::CloseDialog,
+                                  base::Unretained(dialog)));
+  }
+#endif
 
  private:
   EnterpriseStartupDialogView* dialog;
