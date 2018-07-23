40e9292be70cb510c0d3a85e2239669c406bc55b
Chris Kuiper <ckuiper@google.com>
2018-07-23 17:07:31 +0000

    [Chromecast] Cache Android volume tables in native code
    This implements a caching logic to store the Android volume tables in native
    code. This avoids constant JNI calls that can slow things down too much leading
    to problems (see bug for more details).
    
    Bug: [internal] b/71871956
    Test: Running on a several AThings Cast-enabled speakers, as well as unit-test.
    
    Change-Id: I5c769d0d98e3d4b00f01d74a0b0ded984993431e
    Reviewed-on: https://chromium-review.googlesource.com/1134589
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Reviewed-by: Kenneth MacKay <kmackay@chromium.org>
    Commit-Queue: Chris Kuiper <ckuiper@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577193}

--- chromecast/media/cma/backend/android/volume_control_android.h
+++ chromecast/media/cma/backend/android/volume_control_android.h
@@ -16,14 +16,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread.h"
 #include "base/values.h"
 #include "chromecast/media/cma/backend/android/audio_sink_manager.h"
+#include "chromecast/media/cma/backend/android/volume_cache.h"
 
 namespace chromecast {
 namespace media {
 
-class VolumeControlAndroid {
+class VolumeControlAndroid : SystemVolumeTableAccessApi {
  public:
   VolumeControlAndroid();
-  ~VolumeControlAndroid();
+  ~VolumeControlAndroid() override;
 
   void AddVolumeObserver(VolumeObserver* observer);
   void RemoveVolumeObserver(VolumeObserver* observer);
@@ -32,6 +33,8 @@ class VolumeControlAndroid {
   bool IsMuted(AudioContentType type);
   void SetMuted(AudioContentType type, bool muted);
   void SetOutputLimit(AudioContentType type, float limit);
+  float VolumeToDbFSCached(AudioContentType type, float volume);
+  float DbFSToVolumeCached(AudioContentType type, float db);
 
   // Called from java to signal a change volume.
   void OnVolumeChange(JNIEnv* env,
@@ -44,6 +47,10 @@ class VolumeControlAndroid {
                     jint type,
                     jboolean muted);
 
+  // SystemVolumeTableAccessApi implementation.
+  int GetMaxVolumeIndex(AudioContentType type) override;
+  float VolumeToDbFS(AudioContentType type, float volume) override;
+
  private:
   void InitializeOnThread();
   void SetVolumeOnThread(AudioContentType type, float level, bool from_android);
@@ -51,7 +58,17 @@ class VolumeControlAndroid {
   void ReportVolumeChangeOnThread(AudioContentType type, float level);
   void ReportMuteChangeOnThread(AudioContentType type, bool muted);
 
+  // For the user of the VolumeControl, all volume values are in the volume
+  // table domain of kMedia (MUSIC). For volume types other than media,
+  // VolumeControl converts them internally into their proper volume table
+  // domains.
+  float MapIntoDifferentVolumeTableDomain(AudioContentType from_type,
+                                          AudioContentType to_type,
+                                          float level);
+
   base::android::ScopedJavaGlobalRef<jobject> j_volume_control_;
+
+  std::map<AudioContentType, std::unique_ptr<VolumeCache>> volume_cache_;
 
   base::Lock volume_lock_;
   std::map<AudioContentType, float> volumes_;
