f245a88652eca103d33b924f38ec4d3d568ab3ff
Khushal <khushalsagar@chromium.org>
2018-07-23 21:25:36 +0000

    gpu: Handle invalid discardable handle allocation.
    R=ericrk@chromium.org
    
    Bug: 866434
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I6c47cf25c2f15c668b50622896170886e23e6658
    Reviewed-on: https://chromium-review.googlesource.com/1147253
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577264}

--- gpu/command_buffer/client/client_font_manager.h
+++ gpu/command_buffer/client/client_font_manager.h
@@ -37,6 +37,8 @@ class RASTER_EXPORT ClientFontManager
   SkStrikeServer* strike_server() { return &strike_server_; }
 
  private:
+  static constexpr SkDiscardableHandleId kInvalidSkDiscardableHandleId = -1;
+
   Client* client_;
   CommandBuffer* command_buffer_;
 
