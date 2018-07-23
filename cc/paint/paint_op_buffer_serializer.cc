b4e754f81def4cc72f89efbc9d74b2c6ab6d4603
Adrienne Walker <enne@chromium.org>
2018-07-23 23:46:34 +0000

    cc: Don't use unnecessary colorspace conversion canvas
    Colorspace conversion is showing up in profiles, and this is unnecessary
    on the renderer side when it is SRGB.
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I9de0be7700ceffa44f072e8160ddb6fed5fd7730
    Reviewed-on: https://chromium-review.googlesource.com/1147491
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Commit-Queue: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577338}

--- cc/paint/paint_op_buffer_serializer.cc
+++ cc/paint/paint_op_buffer_serializer.cc
@@ -90,8 +90,16 @@ PaintOpBufferSerializer::PaintOpBufferSerializer(
                                            max_texture_size,
                                            max_texture_bytes)) {
   DCHECK(serialize_cb_);
-  canvas_ = SkCreateColorSpaceXformCanvas(&text_blob_canvas_,
-                                          sk_ref_sp<SkColorSpace>(color_space));
+  if (color_space->isSRGB()) {
+    // Colorspace converting every paint is not free.  Only images have a
+    // non-srgb colorspace and this serializer does not handle images.
+    // Therefore, it's correct to just ignore the conversion for srgb.
+    canvas_ = &text_blob_canvas_;
+  } else {
+    color_canvas_ = SkCreateColorSpaceXformCanvas(
+        &text_blob_canvas_, sk_ref_sp<SkColorSpace>(color_space));
+    canvas_ = color_canvas_.get();
+  }
 }
 
 PaintOpBufferSerializer::~PaintOpBufferSerializer() = default;
@@ -108,7 +116,7 @@ void PaintOpBufferSerializer::Serialize(const PaintOpBuffer* buffer,
   // save / final restore. SerializeBuffer will create its own SerializeOptions
   // and PlaybackParams based on the post-preamble canvas.
   PaintOp::SerializeOptions options = MakeSerializeOptions();
-  PlaybackParams params = MakeParams(canvas_.get());
+  PlaybackParams params = MakeParams(canvas_);
 
   Save(options, params);
   SerializePreamble(preamble, options, params);
@@ -130,7 +138,7 @@ void PaintOpBufferSerializer::Serialize(
   DCHECK(canvas_->getTotalMatrix().isIdentity());
 
   PaintOp::SerializeOptions options = MakeSerializeOptions();
-  PlaybackParams params = MakeParams(canvas_.get());
+  PlaybackParams params = MakeParams(canvas_);
 
   // TODO(khushalsagar): remove this clip rect if it's not needed.
   if (!playback_rect.IsEmpty()) {
@@ -273,7 +281,7 @@ void PaintOpBufferSerializer::SerializeBuffer(
     const std::vector<size_t>* offsets) {
   DCHECK(buffer);
   PaintOp::SerializeOptions options = MakeSerializeOptions();
-  PlaybackParams params = MakeParams(canvas_.get());
+  PlaybackParams params = MakeParams(canvas_);
 
   for (PaintOpBuffer::PlaybackFoldingIterator iter(buffer, offsets); iter;
        ++iter) {
@@ -282,7 +290,7 @@ void PaintOpBufferSerializer::SerializeBuffer(
     // Skip ops outside the current clip if they have images. This saves
     // performing an unnecessary expensive decode.
     const bool skip_op = PaintOp::OpHasDiscardableImages(op) &&
-                         PaintOp::QuickRejectDraw(op, canvas_.get());
+                         PaintOp::QuickRejectDraw(op, canvas_);
     if (skip_op)
       continue;
 
@@ -353,9 +361,9 @@ bool PaintOpBufferSerializer::SerializeOp(
 
   if (op->IsPaintOpWithFlags() && options.flags_to_serialize) {
     static_cast<const PaintOpWithFlags*>(op)->RasterWithFlags(
-        canvas_.get(), options.flags_to_serialize, params);
+        canvas_, options.flags_to_serialize, params);
   } else {
-    op->Raster(canvas_.get(), params);
+    op->Raster(canvas_, params);
   }
   return true;
 }
@@ -379,8 +387,8 @@ void PaintOpBufferSerializer::RestoreToCount(
 
 PaintOp::SerializeOptions PaintOpBufferSerializer::MakeSerializeOptions() {
   return PaintOp::SerializeOptions(
-      image_provider_, transfer_cache_, canvas_.get(), strike_server_,
-      color_space_, can_use_lcd_text_, context_supports_distance_field_text_,
+      image_provider_, transfer_cache_, canvas_, strike_server_, color_space_,
+      can_use_lcd_text_, context_supports_distance_field_text_,
       max_texture_size_, max_texture_bytes_, canvas_->getTotalMatrix());
 }
 
