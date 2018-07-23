64ccf52c171af02bded3904b35ec8541fdbb3a8e
Laís Minchillo <laisminchillo@chromium.org>
2018-07-23 10:39:56 +0000

    Ensure webview is clickable
    Originally the webview content was always set to clickable but after
    http://crrev.com/551479 it wasn't. This CL restores original behaviour.
    
    Also adds a test to ensure this remains true in the future.
    
    Bug: 857427
    Change-Id: If7a765f146290b933f57e514f26ad8a672f7695e
    Reviewed-on: https://chromium-review.googlesource.com/1127951
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Laís Minchillo <laisminchillo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577138}

--- android_webview/java/src/org/chromium/android_webview/AwWebContentsDelegateAdapter.java
+++ android_webview/java/src/org/chromium/android_webview/AwWebContentsDelegateAdapter.java
@@ -53,6 +53,7 @@ class AwWebContentsDelegateAdapter extends AwWebContentsDelegate {
 
     public void setContainerView(View containerView) {
         mContainerView = containerView;
+        mContainerView.setClickable(true);
     }
 
     @Override
