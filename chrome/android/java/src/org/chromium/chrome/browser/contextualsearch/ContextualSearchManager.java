ffbeeaf4084de75052c93f8785d34bcff2a8fba3
Tiger Oakes <tigero@google.com>
2018-07-23 17:56:58 +0000

    Remove getApplicationContext from contextualsearch
    Continuation of work in bug 646094.
    Preparing to activate an errorprone check to flag
    context#getApplicationContext calls. The goal is to prevent storing the
    resulting context as a class property, and instead use ContextUtils.
    
    Bug: 846456
    Change-Id: I2c3efdf6c0de690ee8a9a06a523c773722c07196
    Reviewed-on: https://chromium-review.googlesource.com/1095997
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Tiger Oakes <tigero@google.com>
    Cr-Commit-Position: refs/heads/master@{#577202}

--- chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManager.java
@@ -238,7 +238,7 @@ public class ContextualSearchManager
         mSelectionController = new ContextualSearchSelectionController(activity, this);
         mNetworkCommunicator = this;
         mPolicy = new ContextualSearchPolicy(mSelectionController, mNetworkCommunicator);
-        mTranslateController = new ContextualSearchTranslateController(activity, mPolicy, this);
+        mTranslateController = new ContextualSearchTranslateController(mPolicy, this);
         mInternalStateController = new ContextualSearchInternalStateController(
                 mPolicy, getContextualSearchInternalStateHandler());
         mTapSuppressionRankerLogger = new ContextualSearchRankerLoggerImpl();
