d74939be15bfebd7847ed64747bca2410196220c
Darren Shen <shend@chromium.org>
2018-07-23 01:45:20 +0000

    [VK] Remove unused friend declaration in KeyboardController.
    In a previous patch, we removed KeyboardLayoutManager's dependency
    on KeyboardController's SetContainerType. This means that the friend
    declaration in KeyboardController is no longer needed.
    
    TBR=blakeo@chromium.org
    
    Bug: 845780
    Change-Id: I56ea22d989dcefc3bc70d4ca054678fd52c5bef7
    Reviewed-on: https://chromium-review.googlesource.com/1146139
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577101}

--- ui/keyboard/keyboard_controller.h
+++ ui/keyboard/keyboard_controller.h
@@ -236,9 +236,6 @@ class KEYBOARD_EXPORT KeyboardController
   // For access to Observer methods for simulation.
   friend class KeyboardControllerTest;
 
-  // For access to SetContainerBounds.
-  friend class KeyboardLayoutManager;
-
   // For access to NotifyKeyboardConfigChanged
   friend bool keyboard::UpdateKeyboardConfig(
       const keyboard::KeyboardConfig& config);
