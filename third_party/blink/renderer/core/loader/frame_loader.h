fe729c68403c657745c739fa3b3d76926c51946b
Dmitry Gozman <dgozman@chromium.org>
2018-07-23 22:15:07 +0000

    Split FrameLoader start and commit paths part 2
    Inline StartLoad and ShouldContinueForNavigationPolicy into
    StartNavigation and CommitNavigation, dropping the parts which are not relevant.
    
    Bug: 789577
    Change-Id: I5d580bdffed64340fc2396f7a6a274d8242fd96f
    Reviewed-on: https://chromium-review.googlesource.com/1107211
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577287}

--- third_party/blink/renderer/core/loader/frame_loader.h
+++ third_party/blink/renderer/core/loader/frame_loader.h
@@ -66,7 +66,6 @@ namespace blink {
 class Document;
 class DocumentLoader;
 class ExecutionContext;
-class HTMLFormElement;
 class LocalFrame;
 class Frame;
 class LocalFrameClient;
@@ -210,23 +209,6 @@ class CORE_EXPORT FrameLoader final {
   void SaveScrollState();
   void RestoreScrollPositionAndViewState();
 
-  // The navigation should only be continued immediately in this frame if this
-  // returns NavigationPolicyCurrentTab.
-  NavigationPolicy ShouldContinueForNavigationPolicy(
-      const ResourceRequest&,
-      Document* origin_document,
-      const SubstituteData&,
-      DocumentLoader*,
-      ContentSecurityPolicyDisposition,
-      WebNavigationType,
-      NavigationPolicy,
-      WebFrameLoadType,
-      bool is_client_redirect,
-      WebTriggeringEventInfo,
-      HTMLFormElement*,
-      mojom::blink::BlobURLTokenPtr,
-      bool check_with_client);
-
   // Note: When a PlzNavigtate navigation is handled by the client, we will
   // have created a dummy provisional DocumentLoader, so this will return true
   // while the client handles the navigation.
@@ -257,14 +239,6 @@ class CORE_EXPORT FrameLoader final {
 
   // Returns whether we should continue with new navigation.
   bool CancelProvisionalLoaderForNewNavigation(NavigationPolicy);
-
-  void StartLoad(FrameLoadRequest&,
-                 WebFrameLoadType,
-                 NavigationPolicy,
-                 HistoryItem*,
-                 bool check_with_client,
-                 std::unique_ptr<WebDocumentLoader::ExtraData>,
-                 const WebNavigationTimings&);
 
   void ClearInitialScrollState();
 
