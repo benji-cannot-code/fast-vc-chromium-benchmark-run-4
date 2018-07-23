0b1db6b807aa4b269dd7ff5ae262d601ed6ccd69
Trent Apted <tapted@chromium.org>
2018-07-23 23:50:01 +0000

    MacViews: Don't animate in web-modals after the first Show().
    Implement the currently unimplemented (on mac) NativeWidget::
    SetVisibilityAnimationTransition() for this. Autofill uses this
    but not constrained windows.
    
    Constrained windows on aura platforms have bespoke animation
    suppression code, which is currently different to the desired
    behavior on Mac (i.e. the very first "show" should animate).
    
    Consolidate some of the animation logic in MacViews Widgets.
    
    Bug: 767743
    Change-Id: If49d139a66873e896e68030f69efdaa81a8dc948
    Reviewed-on: https://chromium-review.googlesource.com/1146408
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Reviewed-by: Sidney San Martín <sdy@chromium.org>
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577343}

--- ui/views/cocoa/bridged_native_widget.mm
+++ ui/views/cocoa/bridged_native_widget.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdint.h>
 
+#include "base/command_line.h"
 #include "base/logging.h"
 #import "base/mac/foundation_util.h"
 #include "base/mac/mac_util.h"
@@ -22,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/ime/input_method.h"
 #include "ui/base/ime/input_method_factory.h"
 #include "ui/base/layout.h"
+#include "ui/base/ui_base_switches.h"
 #include "ui/compositor/recyclable_compositor_mac.h"
 #include "ui/gfx/geometry/dip_util.h"
 #import "ui/gfx/mac/coordinate_conversion.h"
@@ -528,7 +530,7 @@ void BridgedNativeWidget::SetVisibilityState(WindowVisibilityState new_state) {
 
   // For non-sheet modal types, use the constrained window animations to make
   // the window appear.
-  if (GetAnimate() && native_widget_mac_->GetWidget()->IsModal()) {
+  if (ShouldRunCustomAnimationFor(Widget::ANIMATE_SHOW)) {
     show_animation_.reset(
         [[ModalShowAnimationWithLayer alloc] initWithBridgedNativeWidget:this]);
 
@@ -993,14 +995,29 @@ void BridgedNativeWidget::ReparentNativeView(NSView* native_view,
   }
 }
 
-bool BridgedNativeWidget::GetAnimate() const {
-  return [window_ animationBehavior] != NSWindowAnimationBehaviorNone;
-}
-
-void BridgedNativeWidget::SetAnimate(bool animate) {
+void BridgedNativeWidget::SetAnimationEnabled(bool animate) {
   [window_
       setAnimationBehavior:(animate ? NSWindowAnimationBehaviorDocumentWindow
                                     : NSWindowAnimationBehaviorNone)];
+}
+
+bool BridgedNativeWidget::ShouldRunCustomAnimationFor(
+    Widget::VisibilityTransition transition) const {
+  // The logic around this needs to change if new transition types are set.
+  // E.g. it would be nice to distinguish "hide" from "close". Mac currently
+  // treats "hide" only as "close". Hide (e.g. Cmd+h) should not animate on Mac.
+  constexpr int kSupported =
+      Widget::ANIMATE_SHOW | Widget::ANIMATE_HIDE | Widget::ANIMATE_NONE;
+  DCHECK_EQ(0, transitions_to_animate_ & ~kSupported);
+
+  // Custom animations are only used for tab-modals. Note this also checks the
+  // native animation property. Clearing that will also disable custom
+  // animations to ensure that the views::Widget API behaves consistently.
+  return (transitions_to_animate_ & transition) &&
+         native_widget_mac_->GetWidget()->IsModal() &&
+         [window_ animationBehavior] != NSWindowAnimationBehaviorNone &&
+         !base::CommandLine::ForCurrentProcess()->HasSwitch(
+             switches::kDisableModalAnimations);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
