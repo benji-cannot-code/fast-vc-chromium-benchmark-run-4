8b214fede9d148b5d8db77b12e6c4722f13b4673
Tom McKee <tommckee@chromium.org>
2018-07-23 23:38:45 +0000

    Adding tests against and fixes for tabs in Server-Timing headers
    According to the spec
    (https://w3c.github.io/server-timing/#the-server-timing-header-field),
    optional whitespace is allowed amongst parameter names and values. We
    should be discarding this whitespace during parsing but, when there are
    tabs, we were treating it like an error and discarding the whole value.
    
    Changed the code that tokenizes HTTP headers to skip tabs and spaces
    when consuming optional whitespace.
    
    BUG=798446
    
    Change-Id: If776761e5ea199e662ec7b2b5aa245e4581131fd
    Reviewed-on: https://chromium-review.googlesource.com/1147258
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Commit-Queue: Tom McKee <tommckee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577325}

--- third_party/blink/renderer/platform/network/header_field_tokenizer.cc
+++ third_party/blink/renderer/platform/network/header_field_tokenizer.cc
@@ -56,7 +56,7 @@ bool IsTokenCharacter(Mode mode, UChar c) {
 
 HeaderFieldTokenizer::HeaderFieldTokenizer(const String& header_field)
     : index_(0u), input_(header_field) {
-  SkipSpaces();
+  SkipOptionalWhitespace();
 }
 
 HeaderFieldTokenizer::HeaderFieldTokenizer(HeaderFieldTokenizer&&) = default;
@@ -64,12 +64,13 @@ HeaderFieldTokenizer::HeaderFieldTokenizer(HeaderFieldTokenizer&&) = default;
 bool HeaderFieldTokenizer::Consume(char c) {
   // TODO(cvazac) change this to use LChar
   DCHECK_NE(c, ' ');
+  DCHECK_NE(c, '\t');
 
   if (IsConsumed() || input_[index_] != c)
     return false;
 
   ++index_;
-  SkipSpaces();
+  SkipOptionalWhitespace();
   return true;
 }
 
@@ -83,7 +84,7 @@ bool HeaderFieldTokenizer::ConsumeQuotedString(String& output) {
     if (input_[index_] == '"') {
       output = builder.ToString();
       ++index_;
-      SkipSpaces();
+      SkipOptionalWhitespace();
       return true;
     }
     if (input_[index_] == '\\') {
@@ -108,7 +109,7 @@ bool HeaderFieldTokenizer::ConsumeToken(Mode mode, StringView& output) {
     return false;
 
   output = StringView(input_, start, index_ - start);
-  SkipSpaces();
+  SkipOptionalWhitespace();
   return true;
 }
 
@@ -127,10 +128,8 @@ bool HeaderFieldTokenizer::ConsumeTokenOrQuotedString(Mode mode,
   return true;
 }
 
-void HeaderFieldTokenizer::SkipSpaces() {
-  // TODO(cvazac) skip tabs, per:
-  // https://tools.ietf.org/html/rfc7230#section-3.2.3
-  while (!IsConsumed() && input_[index_] == ' ')
+void HeaderFieldTokenizer::SkipOptionalWhitespace() {
+  while (!IsConsumed() && (input_[index_] == ' ' || input_[index_] == '\t'))
     ++index_;
 }
 
--- third_party/blink/renderer/platform/network/header_field_tokenizer.h
+++ third_party/blink/renderer/platform/network/header_field_tokenizer.h
@@ -42,7 +42,7 @@ class PLATFORM_EXPORT HeaderFieldTokenizer final {
 
  private:
   bool ConsumeQuotedString(String& output);
-  void SkipSpaces();
+  void SkipOptionalWhitespace();
 
   unsigned index_;
   const String input_;
@@ -50,4 +50,4 @@ class PLATFORM_EXPORT HeaderFieldTokenizer final {
 
 }  // namespace blink
 
-#endif
+#endif  // THIRD_PARTY_BLINK_RENDERER_PLATFORM_NETWORK_HEADER_FIELD_TOKENIZER_H_
