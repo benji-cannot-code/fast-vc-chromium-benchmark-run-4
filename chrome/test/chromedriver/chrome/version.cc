a608e7221bd8e2c0cc6e59f805b2cfcd6a917b1a
Artur Khachatryan <khachatryan@chromium.org>
2018-07-23 17:34:21 +0000

    [ChromeDriver] Drop support of Chrome v66, add support of Chrome v69
    Branching Chrome v69.
    We are adding supporting of Chrome v69 and dropping supporting of Chrome v66
    
    Change-Id: Id9b75ef78dd14da34f5165cbc6f64b3a188a5eb9
    Reviewed-on: https://chromium-review.googlesource.com/1146993
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Artur Khachatryan <khachatryan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577198}

--- chrome/test/chromedriver/chrome/version.cc
+++ chrome/test/chromedriver/chrome/version.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // This variable must be able to be found and parsed by the upload script.
-const int kMinimumSupportedChromeVersion[] = {66, 0, 3359, 0};
+const int kMinimumSupportedChromeVersion[] = {67, 0, 3396, 0};
 
 }  // namespace
 
