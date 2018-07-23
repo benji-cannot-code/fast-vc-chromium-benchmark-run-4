d283a681028dcf1d50a0db53cf700a99f9d73448
Ricardo Quesada <ricardoq@google.com>
2018-07-23 23:35:20 +0000

    viz: use correct TexCoord precision
    Use correct TexCoord precision in GLRenderer::EnqueueTextureQuad.
    
    Under certain cases, specially when resizing ARC++ windows, the surface
    size == fullscreen size. But when EnqueueTextureSize asks for the
    TexCoord precision, it passes visible_quad_layer_rect which could be
    less than the highp_threshold_cache. So it will use a mediump qualifier
    instead of a highp.
    Probably this is only reproducible on Kevin because its mediump is only
    10 bits, and I guess other GPUs use a higher precision for mediump.
    
    * Add unit test for the fix
    * Remove unused vars from gl_renderer_unittest.cc
    * Replace bottom_right() with size() before calling TexCoordPrecisionRequired,
      since it only cares about sizes and not positions.
    * Add #include <utility>, <map>, <memory> since "git cl lint"
      complained about it
    
    BUG=862452
    TEST=viz_unittest in Linux. Passed all tests.
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel
    Change-Id: I51ec129db3369c45192f4f0c69ec38f3a2ec511d
    Reviewed-on: https://chromium-review.googlesource.com/1132607
    Commit-Queue: Ricardo Quesada <ricardoq@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577319}

--- components/viz/service/display/gl_renderer.h
+++ components/viz/service/display/gl_renderer.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_VIZ_SERVICE_DISPLAY_GL_RENDERER_H_
 #define COMPONENTS_VIZ_SERVICE_DISPLAY_GL_RENDERER_H_
 
+#include <map>
+#include <memory>
 #include <unordered_map>
 #include <vector>
 
