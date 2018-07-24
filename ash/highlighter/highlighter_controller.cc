f6231d02a1ca8d8ce9db7835a9b977a7614771d3
Muyuan Li <muyuanli@google.com>
2018-07-24 21:31:19 +0000

    assistant: fix metalayer scale and hook up selection region
    Bug: None
    Test: Manual
    Change-Id: I7a9968bb3b421ef26cc90dd68b063e514e3f4094
    Reviewed-on: https://chromium-review.googlesource.com/1147502
    Commit-Queue: Muyuan Li <muyuanli@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577692}

--- ash/highlighter/highlighter_controller.cc
+++ ash/highlighter/highlighter_controller.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "ash/system/palette/palette_utils.h"
 #include "base/metrics/histogram_macros.h"
+#include "chromeos/chromeos_switches.h"
 #include "ui/aura/window.h"
 #include "ui/aura/window_tree_host.h"
 #include "ui/events/base_event_utils.h"
@@ -223,8 +224,17 @@ void HighlighterController::RecognizeGesture() {
 
   if (!box.IsEmpty() &&
       gesture_type != HighlighterGestureType::kNotRecognized) {
-    const gfx::Rect selection_rect = gfx::ToEnclosingRect(
-        gfx::ScaleRect(box, GetScreenshotScale(current_window)));
+    // TODO(muyuanli): Delete the check when native assistant is default on.
+    // This is a temporary workaround to support both ARC-based assistant
+    // and native assistant. In ARC-based assistant, we send the rect in pixels
+    // to ARC side, where the app will crop the screenshot. In native assistant,
+    // we pass the rect directly to UI snapshot API, which assumes coordinates
+    // in DP.
+    const gfx::Rect selection_rect =
+        chromeos::switches::IsAssistantEnabled()
+            ? gfx::ToEnclosingRect(box)
+            : gfx::ToEnclosingRect(
+                  gfx::ScaleRect(box, GetScreenshotScale(current_window)));
     if (client_)
       client_->HandleSelection(selection_rect);
 
