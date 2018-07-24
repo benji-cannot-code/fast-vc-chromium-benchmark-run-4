711b3294ed18e45a31653b6198021f258310f41f
Gabriel Charette <gab@chromium.org>
2018-07-24 15:34:01 +0000

    Reland "[MessageLoop] Remove heavy diagnosis metrics and leave others only on UI thread"
    Original CL: https://chromium-review.googlesource.com/c/chromium/src/+/1142589
    Reverted for test flakiness on Fuchsia.
    Implementation is fine (this is a diagnostic metric -- this change
    definitely doesn't make it worse). The test was only added to ensure
    proper logging on UI thread only. Made the test more robust in this
    reland (see diff vs PS1). If it flakes again, please disable it on flaky
    platforms instead of reverting.
    
    
    Original change's description:
    
    Addresses crbug.com/860801 in time for the M69 branch while keeping the
    diagnosis signals which will be used to assess future improvements
    (MessageLoop.DelayedTaskQueueForUI.PendingTasksCountOnIdle that is).
    
    Removed MessageLoop.DelayedTaskQueue.PostedDelay as we've
    learnt everything we needed from this one, that is :
    We now know that while 99% of tasks are posted with a delay under 30s,
    95% of users post at least one task in every portion of the 0ms-3hours
    range at least once per day:
    https://uma.googleplex.com/histograms?endDate=20180717&dayCount=1&histograms=MessageLoop.DelayedTaskQueue.PostedDelay&fixupData=true&uniqueUsers=true&showMax=true&filters=platform%2Ceq%2CW%2Cchannel%2Ceq%2C1%2Cisofficial%2Ceq%2CTrue&implicitFilters=isofficial
    
    Removed MessageLoop.ScheduledSleep.* metrics as we've also
    learnt what we wanted from it, that is :
    The maximum sleep a MessageLoop ever completes is 1 hour (and
    even then 99% of completed sleeps are under 1 second), all
    longer delays are reliably always interrupted.
    Win : https://uma.googleplex.com/histograms?endDate=20180717&dayCount=1&histograms=MessageLoop.ScheduledSleep.Completed%2CMessageLoop.ScheduledSleep.Interrupted&fixupData=true&showMax=true&filters=platform%2Ceq%2CM%2Cchannel%2Ceq%2C1%2Cisofficial%2Ceq%2CTrue&implicitFilters=isofficial
    Mac : https://uma.googleplex.com/histograms?endDate=20180717&dayCount=1&histograms=MessageLoop.ScheduledSleep.Completed%2CMessageLoop.ScheduledSleep.Interrupted&fixupData=true&showMax=true&filters=platform%2Ceq%2CM%2Cchannel%2Ceq%2C1%2Cisofficial%2Ceq%2CTrue&implicitFilters=isofficial
    Android : https://uma.googleplex.com/histograms?endDate=20180717&dayCount=1&histograms=MessageLoop.ScheduledSleep.Completed%2CMessageLoop.ScheduledSleep.Interrupted&fixupData=true&showMax=true&filters=platform%2Ceq%2CA%2Cchannel%2Ceq%2C1%2Cisofficial%2Ceq%2CTrue&implicitFilters=isofficial
    
    Added a test for MessageLoop.DelayedTaskQueueForUI.PendingTasksCountOnIdle
    but it appears to be broken on MacOSX (prior to this CL). Added a note
    to investigate on the metric but it shouldn't block this CL which
    doesn't make it worse.
    
    TBR=wez@chromium.org
    
    Bug: 860801, 850450, 786597, 866299
    Change-Id: Ic0fb2b58780cd595e47b36f97a9ae440bb988225
    Reviewed-on: https://chromium-review.googlesource.com/1147400
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577553}

--- base/message_loop/incoming_task_queue.cc
+++ base/message_loop/incoming_task_queue.cc
@@ -73,9 +73,6 @@ bool IncomingTaskQueue::AddToIncomingQueue(const Location& from_here,
   }
 #endif
 
-  if (!delay.is_zero())
-    UMA_HISTOGRAM_LONG_TIMES("MessageLoop.DelayedTaskQueue.PostedDelay", delay);
-
   return PostPendingTask(&pending_task);
 }
 
@@ -86,7 +83,7 @@ void IncomingTaskQueue::Shutdown() {
 
 void IncomingTaskQueue::ReportMetricsOnIdle() const {
   UMA_HISTOGRAM_COUNTS_1M(
-      "MessageLoop.DelayedTaskQueue.PendingTasksCountOnIdle",
+      "MessageLoop.DelayedTaskQueueForUI.PendingTasksCountOnIdle",
       delayed_tasks_.Size());
 }
 
