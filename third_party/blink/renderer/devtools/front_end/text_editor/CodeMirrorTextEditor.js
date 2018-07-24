327226117fc3d0feef58d1bed0cb84c9f0c751c7
Erik Luo <luoe@chromium.org>
2018-07-24 02:16:04 +0000

    DevTools: allow ShiftTab to focus away from CodeMirror
    CodeMirror text editors try to reduce indentation on 'Shift+Tab'.
    However, this consumes the key event, preventing focus from leaving,
    even if nothing changed.
    
    This CL allows focus to leave when we are sure that no indentation
    should change. Shift+Tab can now move focus from Console prompt to
    toolbar and from SourceFrame's editor to the Sources TabbedPane.
    
    Bug: none
    Change-Id: I3660a48fe1c30a0972e2908514ee44fba7a51132
    Reviewed-on: https://chromium-review.googlesource.com/1142519
    Commit-Queue: Erik Luo <luoe@chromium.org>
    Reviewed-by: Andrey Lushnikov <lushnikov@chromium.org>
    Reviewed-by: Joel Einbinder <einbinder@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577406}

--- third_party/blink/renderer/devtools/front_end/text_editor/CodeMirrorTextEditor.js
+++ third_party/blink/renderer/devtools/front_end/text_editor/CodeMirrorTextEditor.js
@@ -75,7 +75,7 @@ TextEditor.CodeMirrorTextEditor = class extends UI.VBox {
       'Delete': 'delCharAfter',
       'Backspace': 'delCharBefore',
       'Tab': 'defaultTab',
-      'Shift-Tab': 'indentLess',
+      'Shift-Tab': 'indentLessOrPass',
       'Enter': 'newlineAndIndent',
       'Ctrl-Space': 'autocomplete',
       'Esc': 'dismiss',
@@ -1327,6 +1327,20 @@ CodeMirror.commands.selectCamelRight = TextEditor.CodeMirrorTextEditor.moveCamel
 
 /**
  * @param {!CodeMirror} codeMirror
+ * @return {!Object|undefined}
+ */
+CodeMirror.commands.indentLessOrPass = function(codeMirror) {
+  const selections = codeMirror.listSelections();
+  if (selections.length === 1) {
+    const range = TextEditor.CodeMirrorUtils.toRange(selections[0].anchor, selections[0].head);
+    if (range.isEmpty() && !/^\s/.test(codeMirror.getLine(range.startLine)))
+      return CodeMirror.Pass;
+  }
+  codeMirror.execCommand('indentLess');
+};
+
+/**
+ * @param {!CodeMirror} codeMirror
  */
 CodeMirror.commands.gotoMatchingBracket = function(codeMirror) {
   const updatedSelections = [];
@@ -1372,6 +1386,7 @@ CodeMirror.commands.redoAndReveal = function(codemirror) {
 };
 
 /**
+ * @param {!CodeMirror} codemirror
  * @return {!Object|undefined}
  */
 CodeMirror.commands.dismiss = function(codemirror) {
@@ -1390,6 +1405,7 @@ CodeMirror.commands.dismiss = function(codemirror) {
 };
 
 /**
+ * @param {!CodeMirror} codemirror
  * @return {!Object|undefined}
  */
 CodeMirror.commands.goSmartPageUp = function(codemirror) {
@@ -1399,6 +1415,7 @@ CodeMirror.commands.goSmartPageUp = function(codemirror) {
 };
 
 /**
+ * @param {!CodeMirror} codemirror
  * @return {!Object|undefined}
  */
 CodeMirror.commands.goSmartPageDown = function(codemirror) {
