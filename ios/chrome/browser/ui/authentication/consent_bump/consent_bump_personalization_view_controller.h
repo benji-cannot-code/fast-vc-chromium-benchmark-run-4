285c096a3d4d54d8980bc51ae62e3f2b0ca133c3
Gauthier Ambard <gambard@chromium.org>
2018-07-24 13:51:09 +0000

    [iOS] Create the ConsentBump skeleton
    This CL creates the architecture of the ConsentBump for the new sign in.
    
    Bug: 866506
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I02fd7b3f2612f9c9510f111199100ca3e88b85fb
    Reviewed-on: https://chromium-review.googlesource.com/1148320
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577513}

---
+++ ios/chrome/browser/ui/authentication/consent_bump/consent_bump_personalization_view_controller.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_AUTHENTICATION_CONSENT_BUMP_CONSENT_BUMP_PERSONALIZATION_VIEW_CONTROLLER_H_
+#define IOS_CHROME_BROWSER_UI_AUTHENTICATION_CONSENT_BUMP_CONSENT_BUMP_PERSONALIZATION_VIEW_CONTROLLER_H_
+
+#import <UIKit/UIKit.h>
+
+// View controller displaying the Personalization screen.
+@interface ConsentBumpPersonalizationViewController : UIViewController
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_AUTHENTICATION_CONSENT_BUMP_CONSENT_BUMP_PERSONALIZATION_VIEW_CONTROLLER_H_
---
+++ ios/chrome/browser/ui/authentication/consent_bump/consent_bump_personalization_view_controller.mm
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/authentication/consent_bump/consent_bump_personalization_view_controller.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+@implementation ConsentBumpPersonalizationViewController
+
+- (void)viewDidLoad {
+  self.view.backgroundColor = [UIColor blueColor];
+}
+
+@end
