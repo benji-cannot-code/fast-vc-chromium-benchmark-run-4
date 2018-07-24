9145cc2960c2020efe3bc9d25468294c93b45369
Takumi Fujimoto <takumif@chromium.org>
2018-07-24 20:30:07 +0000

    [Harmony Cast Dialog] Disable the stop button when clicked
    Disable the "Stop" button after it's clicked, so that the user doesn't
    click on it repeatedly. The button becomes enabled again when there is
    a model update, or if the user clicks on a sink.
    
    When the stop button is clicked for a sink, the sink enters DISCONNECTING
    state for that dialog, until its route is removed.
    
    Bug: 848975
    Change-Id: I0acde957053a445fce6bb24cdb54c5e30dd45587
    Reviewed-on: https://chromium-review.googlesource.com/1123326
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577665}

--- chrome/browser/ui/views/media_router/media_router_views_ui.h
+++ chrome/browser/ui/views/media_router/media_router_views_ui.h
@@ -35,6 +35,7 @@ class MediaRouterViewsUI : public MediaRouterUIBase,
   FRIEND_TEST_ALL_PREFIXES(MediaRouterViewsUITest, NotifyObserver);
   FRIEND_TEST_ALL_PREFIXES(MediaRouterViewsUITest, RemovePseudoSink);
   FRIEND_TEST_ALL_PREFIXES(MediaRouterViewsUITest, ConnectingState);
+  FRIEND_TEST_ALL_PREFIXES(MediaRouterViewsUITest, DisconnectingState);
   FRIEND_TEST_ALL_PREFIXES(MediaRouterViewsUITest, AddAndRemoveIssue);
 
   // MediaRouterUIBase:
@@ -63,6 +64,10 @@ class MediaRouterViewsUI : public MediaRouterUIBase,
   // This value is set when the user opens a file picker, and used when a file
   // is selected and casting starts.
   base::Optional<MediaSink::Id> local_file_sink_id_;
+
+  // This value is set when the UI requests a route to be terminated, and gets
+  // reset when the route is removed.
+  base::Optional<MediaRoute::Id> terminating_route_id_;
 
   // Observers for dialog model updates.
   base::ObserverList<CastDialogController::Observer> observers_;
