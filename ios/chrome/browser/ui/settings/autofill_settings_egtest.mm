d5d6d9aa2457d9f5ae952551f31f69d19ea4c08d
Justin Cohen <justincohen@google.com>
2018-07-24 00:47:32 +0000

    [ios] Disable failing autofill tests.
    TBR: sczs@chromium.org
    Bug: 866730
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ibc3828d336d5f6247f2a656d747e35847f86fd70
    Reviewed-on: https://chromium-review.googlesource.com/1147701
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577384}

--- ios/chrome/browser/ui/settings/autofill_settings_egtest.mm
+++ ios/chrome/browser/ui/settings/autofill_settings_egtest.mm
@@ -128,6 +128,9 @@ NSString* GetTextFieldForID(int categoryId) {
 
 // Test that submitting a form ensures saving the data as an autofill profile.
 - (void)testAutofillProfileSaving {
+  // TODO(crbug.com/866730): Re-enable when fixed
+  EARL_GREY_TEST_DISABLED(@"Test broken.");
+
   [self loadAndSubmitTheForm];
   [self openEditAddress:@"George Washington, 1600 Pennsylvania Ave NW"];
 
@@ -148,6 +151,9 @@ NSString* GetTextFieldForID(int categoryId) {
 // Test that editing country names is followed by validating the value and
 // replacing it with a canonical one.
 - (void)testAutofillProfileEditing {
+  // TODO(crbug.com/866730): Re-enable when fixed
+  EARL_GREY_TEST_DISABLED(@"Test broken.");
+
   [self loadAndSubmitTheForm];
   [self openEditAddress:@"George Washington, 1600 Pennsylvania Ave NW"];
 
@@ -182,6 +188,9 @@ NSString* GetTextFieldForID(int categoryId) {
 
 // Test that the page for viewing autofill profile details is accessible.
 - (void)testAccessibilityOnAutofillProfileViewPage {
+  // TODO(crbug.com/866730): Re-enable when fixed
+  EARL_GREY_TEST_DISABLED(@"Test broken.");
+
   [self loadAndSubmitTheForm];
   [self openEditAddress:@"George Washington, 1600 Pennsylvania Ave NW"];
   chrome_test_util::VerifyAccessibilityForCurrentScreen();
@@ -191,6 +200,9 @@ NSString* GetTextFieldForID(int categoryId) {
 
 // Test that the page for editing autofill profile details is accessible.
 - (void)testAccessibilityOnAutofillProfileEditPage {
+  // TODO(crbug.com/866730): Re-enable when fixed
+  EARL_GREY_TEST_DISABLED(@"Test broken.");
+
   [self loadAndSubmitTheForm];
   [self openEditAddress:@"George Washington, 1600 Pennsylvania Ave NW"];
   // Switch on edit mode.
@@ -205,6 +217,9 @@ NSString* GetTextFieldForID(int categoryId) {
 // Checks that if the autofill profiles and credit cards list view is in edit
 // mode, the "autofill" and "wallet" switch items are disabled.
 - (void)testListViewEditMode {
+  // TODO(crbug.com/866730): Re-enable when fixed
+  EARL_GREY_TEST_DISABLED(@"Test broken.");
+
   [self loadAndSubmitTheForm];
 
   [ChromeEarlGreyUI openSettingsMenu];
