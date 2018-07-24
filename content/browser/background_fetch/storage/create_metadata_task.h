061d0aa5ca8719bbe7653659da5f33cfc63ca769
Rayan Kanso <rayankans@chromium.org>
2018-07-24 12:58:30 +0000

    Reland "[Background Fetch] Move all updatable UI options to their own proto."
    This is a reland of d3981964f2499033059aae600732d656370e0ddf
    
    TBR=avi@chromium.org
    
    Original change's description:
    > [Background Fetch] Move all updatable UI options to their own proto.
    >
    > Bundle up the icon and the title since they will be accessed/modified
    > together in the updateUI call.
    >
    > It also makes sense to move the icon from the metadata proto since that
    > will speed up getting the registration info.
    >
    > TBR=avi@chromium.org
    >
    > Bug: 865063
    > Change-Id: I661062f19fddfa6b9c3bf11b5e146d5289a02593
    > Reviewed-on: https://chromium-review.googlesource.com/1146650
    > Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Reviewed-by: Peter Beverloo <peter@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#577272}
    
    Bug: 865063
    Change-Id: I77567e8e2e0ea59ede514c77b40326e7ece48a25
    Reviewed-on: https://chromium-review.googlesource.com/1148261
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577500}

--- content/browser/background_fetch/storage/create_metadata_task.h
+++ content/browser/background_fetch/storage/create_metadata_task.h
@@ -42,7 +42,7 @@ class CreateMetadataTask : public DatabaseTask {
   void DidGetUniqueId(const std::vector<std::string>& data,
                       blink::ServiceWorkerStatusCode status);
 
-  void StoreIcon(std::string serialized_icon);
+  void DidSerializeIcon(std::string serialized_icon);
 
   void StoreMetadata();
 
@@ -60,6 +60,8 @@ class CreateMetadataTask : public DatabaseTask {
   CreateMetadataCallback callback_;
 
   std::unique_ptr<proto::BackgroundFetchMetadata> metadata_proto_;
+
+  std::string serialized_icon_;
 
   base::WeakPtrFactory<CreateMetadataTask> weak_factory_;  // Keep as last.
 
