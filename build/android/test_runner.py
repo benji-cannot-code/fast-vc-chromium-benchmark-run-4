be7c892bbed6547c8b1b19151c6bef79654784f6
David 'Digit' Turner <digit@google.com>
2018-07-24 13:42:59 +0000

    build/android: Fixes to make pylint happy.
    This CL contains fixes required to ensure that the presubmit
    upload script does not fail after the Pylint update to 1.5.
    
    More specifically:
    
    - The pylintrc is modified to add wrong-order-position to
      the list of disabled warnings, since many of the scripts
      under build/android require sophisticated import logic
      due to the use of third_party/catapult/devil/
    
    - Otherwise, scripts are locally updated by either changing
      the source code, or adding local pylint comments to disable
      a specific warning.
    
    Without this CL, no Android-specific CL can pass the presubmit
    test on Gerrit :-/
    
    BUG=NONE
    R=jbudorick@chromium.org, agrieve@chromium.org, vapier@chromium.org
    
    Change-Id: I4812c76bf1b86918d56c230574c741c3c1328c5d
    Reviewed-on: https://chromium-review.googlesource.com/1148339
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Commit-Queue: David Turner <digit@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577510}

--- build/android/test_runner.py
+++ build/android/test_runner.py
@@ -26,6 +26,7 @@ import unittest
 # See http://crbug.com/724524 and https://bugs.python.org/issue7980.
 import _strptime  # pylint: disable=unused-import
 
+# pylint: disable=ungrouped-imports
 from pylib.constants import host_paths
 
 if host_paths.DEVIL_PATH not in sys.path:
@@ -49,7 +50,6 @@ from pylib.utils import logdog_helper
 from pylib.utils import logging_utils
 
 from py_utils import contextlib_ext
-
 
 _DEVIL_STATIC_CONFIG_FILE = os.path.abspath(os.path.join(
     host_paths.DIR_SOURCE_ROOT, 'build', 'android', 'devil_config.json'))
@@ -213,10 +213,12 @@ def AddCommonOptions(parser):
 def ProcessCommonOptions(args):
   """Processes and handles all common options."""
   run_tests_helper.SetLogLevel(args.verbose_count, add_handler=False)
+  # pylint: disable=redefined-variable-type
   if args.verbose_count > 0:
     handler = logging_utils.ColorStreamHandler()
   else:
     handler = logging.StreamHandler(sys.stdout)
+  # pylint: enable=redefined-variable-type
   handler.setFormatter(run_tests_helper.CustomFormatter())
   logging.getLogger().addHandler(handler)
 
@@ -430,6 +432,7 @@ def AddInstrumentationTestOptions(parser):
     split_arg = arg.split(',')
     if len(split_arg) != 2:
       raise argparse.ArgumentError(
+          arg,
           'Expected two comma-separated strings for --replace-system-package, '
           'received %d' % len(split_arg))
     PackageReplacement = collections.namedtuple('PackageReplacement',
