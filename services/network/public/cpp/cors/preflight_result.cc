6d4e1c09a10fc150a53ed89694e644eda269ec4d
Yutaka Hirano <yhirano@chromium.org>
2018-07-24 07:06:26 +0000

    [OOR-CORS] Have PreflightResult functions return CORSErrorStatus
    ...instead of mojom::CORSError. Currently they return a mojom::CORSError
    and callers need to compose a CORSErrorStatus from it. That requires
    callers to know which specific errors are returned from the function,
    which is not good.
    
    Bug: 736308
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Change-Id: I5b0dfaf5b906e0203c9a72ebaef759a8fafa4ad3
    Reviewed-on: https://chromium-review.googlesource.com/1147681
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577443}

--- services/network/public/cpp/cors/preflight_result.cc
+++ services/network/public/cpp/cors/preflight_result.cc
@@ -113,8 +113,7 @@ PreflightResult::PreflightResult(
 
 PreflightResult::~PreflightResult() = default;
 
-base::Optional<mojom::CORSError>
-PreflightResult::EnsureAllowedCrossOriginMethod(
+base::Optional<CORSErrorStatus> PreflightResult::EnsureAllowedCrossOriginMethod(
     const std::string& method) const {
   // Request method is normalized to upper case, and comparison is performed in
   // case-sensitive way, that means access control header should provide an
@@ -128,13 +127,13 @@ PreflightResult::EnsureAllowedCrossOriginMethod(
   if (!credentials_ && methods_.find("*") != methods_.end())
     return base::nullopt;
 
-  return mojom::CORSError::kMethodDisallowedByPreflightResponse;
+  return CORSErrorStatus(mojom::CORSError::kMethodDisallowedByPreflightResponse,
+                         method);
 }
 
-base::Optional<mojom::CORSError>
+base::Optional<CORSErrorStatus>
 PreflightResult::EnsureAllowedCrossOriginHeaders(
-    const net::HttpRequestHeaders& headers,
-    std::string* detected_header) const {
+    const net::HttpRequestHeaders& headers) const {
   if (!credentials_ && headers_.find("*") != headers_.end())
     return base::nullopt;
 
@@ -150,9 +149,8 @@ PreflightResult::EnsureAllowedCrossOriginHeaders(
       // fine.
       if (IsForbiddenHeader(key))
         continue;
-      if (detected_header)
-        *detected_header = header.key;
-      return mojom::CORSError::kHeaderDisallowedByPreflightResponse;
+      return CORSErrorStatus(
+          mojom::CORSError::kHeaderDisallowedByPreflightResponse, header.key);
     }
   }
   return base::nullopt;
@@ -173,7 +171,7 @@ bool PreflightResult::EnsureAllowedRequest(
   if (EnsureAllowedCrossOriginMethod(method))
     return false;
 
-  if (EnsureAllowedCrossOriginHeaders(headers, nullptr))
+  if (EnsureAllowedCrossOriginHeaders(headers))
     return false;
 
   return true;
