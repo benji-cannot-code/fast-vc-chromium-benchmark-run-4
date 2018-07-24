bf028e72d2dd0bf867bd2f1389a730a06f8e612d
Kurt Horimoto <kkhorimoto@chromium.org>
2018-07-24 02:20:09 +0000

    [iOS] Set a11y identifiers directly to UIButtons for legacy toolbar.
    LegacyReadingListToolbarButton is actually a plain UIView superview of
    the UIButton handling touch events, so setting the a11y identifier on
    the superview was not passing touches to the underlying button in
    EG tests.  This CL updates LegacyReadingListToolbarButton to take an
    a11y identifier that's applied to the underlying UIButton, allowing
    touches to be handled properly.
    
    Bug: 866012
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I1e98ae482bd21e1e913d500d53f43526fadb62af
    Reviewed-on: https://chromium-review.googlesource.com/1147310
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Peter Lee <pkl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577407}

--- ios/chrome/browser/ui/reading_list/legacy_reading_list_toolbar_button.mm
+++ ios/chrome/browser/ui/reading_list/legacy_reading_list_toolbar_button.mm
@@ -34,7 +34,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (instancetype)initWithText:(NSString*)labelText
                  destructive:(BOOL)isDestructive
-                    position:(ButtonPositioning)position {
+                    position:(ButtonPositioning)position
+                  identifier:(NSString*)identifier {
   self = [super init];
   if (!self) {
     return self;
@@ -44,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                      destructive:isDestructive
                         position:position];
   _button.translatesAutoresizingMaskIntoConstraints = NO;
+  _button.accessibilityIdentifier = identifier;
   [self addSubview:_button];
 
   NSDictionary* views = @{@"button" : _button};
