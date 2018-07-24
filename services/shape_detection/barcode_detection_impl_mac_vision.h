29b6ce3f634d13867bce7faec86f079ecc588ccf
junweifu <junwei.fu@intel.com>
2018-07-24 00:30:30 +0000

    ShapeDetection: Detect barcode informations by VNDetectBarcodesRequest
    The native request first locates all barcodes in the input image, then analyzes
    each to decode payload. The bounding box of the detected barcode are normalized
    to the dimensions of the processed image, so the coordinates need to be
    converted.
    Forward declaring the necessary symbols in sdk_forward_declarations.h.
    
    BUG=848182
    
    Cq-Include-Trybots: luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win10_chromium_x64_rel_ng
    Change-Id: I03b966a1886feaf64a87e778396ad96e3d16b85e
    Reviewed-on: https://chromium-review.googlesource.com/1142831
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Junwei Fu <junwei.fu@intel.com>
    Cr-Commit-Position: refs/heads/master@{#577368}

--- services/shape_detection/barcode_detection_impl_mac_vision.h
+++ services/shape_detection/barcode_detection_impl_mac_vision.h
@@ -6,8 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SERVICES_SHAPE_DETECTION_BARCODE_DETECTION_IMPL_MAC_VISION_H_
 #define SERVICES_SHAPE_DETECTION_BARCODE_DETECTION_IMPL_MAC_VISION_H_
 
+#include <memory>
+#include <utility>
+
 #include "base/mac/availability.h"
+#include "base/mac/sdk_forward_declarations.h"
 #include "base/macros.h"
+#include "base/memory/weak_ptr.h"
+#include "mojo/public/cpp/bindings/strong_binding.h"
+#include "services/shape_detection/detection_utils_mac.h"
 #include "services/shape_detection/public/mojom/barcodedetection.mojom.h"
 #include "services/shape_detection/public/mojom/barcodedetection_provider.mojom.h"
 
@@ -27,7 +34,19 @@ class API_AVAILABLE(macos(10.13)) BarcodeDetectionImplMacVision
   void Detect(const SkBitmap& bitmap,
               mojom::BarcodeDetection::DetectCallback callback) override;
 
+  void SetBinding(mojo::StrongBindingPtr<mojom::BarcodeDetection> binding) {
+    binding_ = std::move(binding);
+  }
+
  private:
+  void OnBarcodesDetected(VNRequest* request, NSError* error);
+
+  CGSize image_size_;
+  std::unique_ptr<VisionAPIAsyncRequestMac> barcodes_async_request_;
+  DetectCallback detected_callback_;
+  mojo::StrongBindingPtr<mojom::BarcodeDetection> binding_;
+  base::WeakPtrFactory<BarcodeDetectionImplMacVision> weak_factory_;
+
   DISALLOW_COPY_AND_ASSIGN(BarcodeDetectionImplMacVision);
 };
 
