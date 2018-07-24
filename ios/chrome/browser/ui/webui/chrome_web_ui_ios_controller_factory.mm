b2b330e0f2255c6527a9fdbc8cf833f2e0dd9be0
Moe Ahmadi <mahmadi@chromium.org>
2018-07-24 16:14:49 +0000

    [IOS][UKM] Modifies UKM internals page (chrome://ukm) to use WebUI.
    - Modifies iOS implementation of chrome://ukm to use WebUI.
    - Refactors HTML/JS resources to be used in iOS.
    - Removes method in ukm::UkmDebugDataExtractor that returns HTML debug data.
    - Adds helper methods to resolve/reject Promises to WebUIIOS.
    
    Bug: 843181
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I008ce1beb8d35dd9d4a7fc482990f06e3095f16a
    Reviewed-on: https://chromium-review.googlesource.com/1147241
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Peter Lee <pkl@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577569}

--- ios/chrome/browser/ui/webui/chrome_web_ui_ios_controller_factory.mm
+++ ios/chrome/browser/ui/webui/chrome_web_ui_ios_controller_factory.mm
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/ui/webui/suggestions_ui.h"
 #include "ios/chrome/browser/ui/webui/sync_internals/sync_internals_ui.h"
 #include "ios/chrome/browser/ui/webui/terms_ui.h"
-#include "ios/chrome/browser/ui/webui/url_keyed_metrics_ui.h"
+#include "ios/chrome/browser/ui/webui/ukm_internals_ui.h"
 #include "ios/chrome/browser/ui/webui/version_ui.h"
 #include "url/gurl.h"
 
@@ -92,7 +92,7 @@ WebUIIOSFactoryFunction GetWebUIIOSFactoryFunction(WebUIIOS* web_ui,
   if (url_host == kChromeUIFlagsHost)
     return &NewWebUIIOS<FlagsUI>;
   if (url_host == kChromeUIURLKeyedMetricsHost)
-    return &NewWebUIIOSWithHost<URLKeyedMetricsUI>;
+    return &NewWebUIIOS<UkmInternalsUI>;
 
   return nullptr;
 }
--- ios/chrome/browser/ui/webui/ukm_internals_ui.h
+++ ios/chrome/browser/ui/webui/ukm_internals_ui.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef IOS_CHROME_BROWSER_UI_WEBUI_URL_KEYED_METRICS_UI_H_
-#define IOS_CHROME_BROWSER_UI_WEBUI_URL_KEYED_METRICS_UI_H_
+#ifndef IOS_CHROME_BROWSER_UI_WEBUI_UKM_INTERNALS_UI_H_
+#define IOS_CHROME_BROWSER_UI_WEBUI_UKM_INTERNALS_UI_H_
 
 #include "base/macros.h"
 #include "ios/web/public/webui/web_ui_ios_controller.h"
@@ -14,13 +14,13 @@ class WebUIIOS;
 }
 
 // The WebUI controller for chrome://ukm.
-class URLKeyedMetricsUI : public web::WebUIIOSController {
+class UkmInternalsUI : public web::WebUIIOSController {
  public:
-  URLKeyedMetricsUI(web::WebUIIOS* web_ui, const std::string& name);
-  ~URLKeyedMetricsUI() override;
+  explicit UkmInternalsUI(web::WebUIIOS* web_ui);
+  ~UkmInternalsUI() override;
 
  private:
-  DISALLOW_COPY_AND_ASSIGN(URLKeyedMetricsUI);
+  DISALLOW_COPY_AND_ASSIGN(UkmInternalsUI);
 };
 
-#endif  // IOS_CHROME_BROWSER_UI_WEBUI_URL_KEYED_METRICS_UI_H_
+#endif  // IOS_CHROME_BROWSER_UI_WEBUI_UKM_INTERNALS_UI_H_
