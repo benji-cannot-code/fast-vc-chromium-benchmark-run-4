96b8695bfa8b22e11e0c6a82389e6fb25293dea8
Dmitry Gozman <dgozman@chromium.org>
2018-07-24 18:17:40 +0000

    Simplify RenderFrameImpl::DecidePolicyForNavigation
    Only renderer-inititated navigations go through DecidePolicyForNavigation
    now, so there should be no |pending_navigation_params_| or |extra_data|.
    
    This patch removes previous CHECKs and refactors code
    assuming there is no extra_data and pending_navigation_params_.
    
    Bug: 789577
    Change-Id: I8ac67b5735e2f4b8976347df593877e0b1d61292
    Reviewed-on: https://chromium-review.googlesource.com/1139024
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577612}

--- third_party/blink/public/web/web_local_frame_client.h
+++ third_party/blink/public/web/web_local_frame_client.h
@@ -333,8 +333,6 @@ class BLINK_EXPORT WebLocalFrameClient {
   // defaultPolicy should just be returned.
 
   struct NavigationPolicyInfo {
-    WebDocumentLoader::ExtraData* extra_data;
-
     // Note: if browser side navigations are enabled, the client may modify
     // the urlRequest. However, should this happen, the client should change
     // the WebNavigationPolicy to WebNavigationPolicyIgnore, and the load
@@ -360,8 +358,7 @@ class BLINK_EXPORT WebLocalFrameClient {
     ArchiveStatus archive_status;
 
     explicit NavigationPolicyInfo(WebURLRequest& url_request)
-        : extra_data(nullptr),
-          url_request(url_request),
+        : url_request(url_request),
           navigation_type(kWebNavigationTypeOther),
           default_policy(kWebNavigationPolicyIgnore),
           replaces_current_history_item(false),
