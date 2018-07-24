16d826f853dbe8762ce8fd6e30e1fd9dd2c5dcc8
Rayan Kanso <rayankans@chromium.org>
2018-07-24 11:57:32 +0000

    Reland "[Background Fetch] Delete the unused FetchedEvent files."
    This is a reland of f7206154fb4970f43ef7b485348cdf32c88d9266
    
    Original change's description:
    > [Background Fetch] Delete the unused FetchedEvent files.
    > 
    > TBR=jbroman@chromium.org
    > 
    > Bug: 822765
    > Change-Id: I03fa9efc6fa80814f252593a0582ff25b633580b
    > Reviewed-on: https://chromium-review.googlesource.com/1142154
    > Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    > Reviewed-by: Mugdha Lakhani <nator@chromium.org>
    > Reviewed-by: Peter Beverloo <peter@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#577271}
    
    Bug: 822765
    Change-Id: I1e8e31569874b6be9de954cc15ac01f6b134017d
    Reviewed-on: https://chromium-review.googlesource.com/1148260
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577486}

--- third_party/blink/renderer/modules/background_fetch/background_fetch_update_event.cc
+++ third_party/blink/renderer/modules/background_fetch/background_fetch_update_event.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/fetch/response.h"
 #include "third_party/blink/renderer/modules/background_fetch/background_fetch_bridge.h"
 #include "third_party/blink/renderer/modules/background_fetch/background_fetch_settled_fetch.h"
-#include "third_party/blink/renderer/modules/background_fetch/background_fetched_event_init.h"
 #include "third_party/blink/renderer/modules/event_modules_names.h"
 #include "third_party/blink/renderer/platform/bindings/script_state.h"
 
