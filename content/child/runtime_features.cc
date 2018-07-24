5544bf2b54f24a0ed6ea7f4aee5733c70dee91f3
Leszek Swirski <leszeks@chromium.org>
2018-07-24 15:09:59 +0000

    [blink] Use scheduler for script streaming
    Add a new feature flag, ScheduledScriptStreaming, which makes the script
    streamer use the task scheduler for streaming tasks, rather than using a
    dedicated ScriptStreamerThread.
    
    Bug: chromium:865098
    Bug: chromium:866868
    Change-Id: I05ca411d8d5187c4b5180011ebce2a603e3944fe
    Reviewed-on: https://chromium-review.googlesource.com/1145386
    Commit-Queue: Leszek Swirski <leszeks@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577543}

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -407,6 +407,9 @@ void SetRuntimeFeaturesDefaultsAndUpdateFromArgs(
   WebRuntimeFeatures::EnableWorkStealingInScriptRunner(
       base::FeatureList::IsEnabled(features::kWorkStealingInScriptRunner));
 
+  WebRuntimeFeatures::EnableScheduledScriptStreaming(
+      base::FeatureList::IsEnabled(features::kScheduledScriptStreaming));
+
   WebRuntimeFeatures::EnableFeatureFromString(
       "FeaturePolicyForPermissions",
       base::FeatureList::IsEnabled(features::kUseFeaturePolicyForPermissions));
