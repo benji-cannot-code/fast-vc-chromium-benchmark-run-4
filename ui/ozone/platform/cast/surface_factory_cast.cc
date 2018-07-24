6ef02cbf6a1327ea2a2ff50311d6bc700cc2effe
Daniel Nicoara <dnicoara@chromium.org>
2018-07-24 18:13:27 +0000

    Remove unnecessary overlay prints in cast GL surface
    Video overlays on the Cast platform follow a special path handled by the
    hardware via a separate API. The Cast overlay strategy was updated in
    3737028e1f309587ee995464f83b4f4bb30d9fdf to accept the overlay rather
    than relying on the Ozone platform to make the decision.
    
    The logging was moved in OverlayStrategyUnderlayCast and
    VideoPlaneController.
    
    Bug: None
    Test: Compiled cast_shell
    Change-Id: Ide14380ad626d7bcd9ba00cb7f74daab292f2f80
    Reviewed-on: https://chromium-review.googlesource.com/1134495
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Commit-Queue: Daniel Nicoara <dnicoara@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577607}

--- ui/ozone/platform/cast/surface_factory_cast.cc
+++ ui/ozone/platform/cast/surface_factory_cast.cc
@@ -48,7 +48,7 @@ class DummySurface : public SurfaceOzoneCanvas {
 
 class CastPixmap : public gfx::NativePixmap {
  public:
-  explicit CastPixmap(GLOzoneEglCast* parent) : parent_(parent) {}
+  CastPixmap() {}
 
   bool AreDmaBufFdsValid() const override { return false; }
   size_t GetDmaBufFdCount() const override { return 0; }
@@ -69,8 +69,7 @@ class CastPixmap : public gfx::NativePixmap {
                             const gfx::RectF& crop_rect,
                             bool enable_blend,
                             std::unique_ptr<gfx::GpuFence> gpu_fence) override {
-    parent_->OnOverlayScheduled(display_bounds);
-    return true;
+    return false;
   }
   gfx::NativePixmapHandle ExportHandle() override {
     return gfx::NativePixmapHandle();
@@ -78,8 +77,6 @@ class CastPixmap : public gfx::NativePixmap {
 
  private:
   ~CastPixmap() override {}
-
-  GLOzoneEglCast* parent_;
 
   DISALLOW_COPY_AND_ASSIGN(CastPixmap);
 };
@@ -132,7 +129,7 @@ scoped_refptr<gfx::NativePixmap> SurfaceFactoryCast::CreateNativePixmap(
     gfx::Size size,
     gfx::BufferFormat format,
     gfx::BufferUsage usage) {
-  return base::MakeRefCounted<CastPixmap>(egl_implementation_.get());
+  return base::MakeRefCounted<CastPixmap>();
 }
 
 }  // namespace ui
