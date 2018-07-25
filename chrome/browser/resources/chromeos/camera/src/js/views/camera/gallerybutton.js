88958cc4a107388038800674702c99bbea022716
Yuli Huang <yuli@chromium.org>
2018-07-25 15:40:27 +0800

    Extract options for new UI and fix end-take call.
    1. Extract options from camera.js for new UI.
    2. Fix UI controls may still remain disabled after end-take call (e.g.
    stream-disconnected during timer ticks.)
    3. Update shutter labels before re-enabling buttons for accessibility.
    
    BUG=None
    TEST=Tested by switching modes, and taking pictures w/ or w/o the timer.
    
    Change-Id: I65415d3525de0ef45b0452df13bf7908b81c857e
    Reviewed-on: https://chromium-review.googlesource.com/1155267
    Reviewed-by: Jasmine Chen <lnishan@chromium.org>
    Reviewed-by: Jasmine Chen <lnishan@google.com>
    Tested-by: yuli <yuli@chromium.org>

--- chrome/browser/resources/chromeos/camera/src/js/views/camera/gallerybutton.js
+++ chrome/browser/resources/chromeos/camera/src/js/views/camera/gallerybutton.js
@@ -67,9 +67,6 @@ camera.views.camera.GalleryButton = function(router, model) {
 };
 
 camera.views.camera.GalleryButton.prototype = {
-  /*
-   * @param {boolean} value Whether disabled.
-   */
   set disabled(value) {
     this.button_.disabled = value;
   }
