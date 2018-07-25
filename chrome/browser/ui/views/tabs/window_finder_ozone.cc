51636d287e4430bbdcca322fc03256370c08b59a
Jun Mukai <mukai@chromium.org>
2018-07-25 18:21:13 +0000

    consolidate window_finder_{ash,chromeos,mus}
    Those files are said to be ChromeOS only, putting all of them
    into _chromeos.cc files and removing others. _ozone, _win, and
    _x11 are actually referring to _mus, but they won't be used since
    the feature isn't enabled on those platforms.
    
    Bug: 867078
    Test: build passes
    Change-Id: I76e7285fce7aac7c229a7cbe8668c5f654276178
    Reviewed-on: https://chromium-review.googlesource.com/1149186
    Commit-Queue: Jun Mukai <mukai@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577980}

--- chrome/browser/ui/views/tabs/window_finder_ozone.cc
+++ chrome/browser/ui/views/tabs/window_finder_ozone.cc
@@ -4,15 +4,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/ui/views/tabs/window_finder.h"
-#include "chrome/browser/ui/views/tabs/window_finder_mus.h"
 
 gfx::NativeWindow WindowFinder::GetLocalProcessWindowAtPoint(
     const gfx::Point& screen_point,
     const std::set<gfx::NativeWindow>& ignore) {
-  gfx::NativeWindow mus_result = nullptr;
-  if (GetLocalProcessWindowAtPointMus(screen_point, ignore, &mus_result))
-    return mus_result;
-
-  NOTREACHED() << "For Ozone builds, only mash launch is supported for now.";
+  NOTIMPLEMENTED()
+      << "For Ozone builds, window finder is not supported for now.";
   return nullptr;
 }
--- chrome/browser/ui/views/tabs/window_finder_x11.cc
+++ chrome/browser/ui/views/tabs/window_finder_x11.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/tabs/window_finder.h"
 
-#include "chrome/browser/ui/views/tabs/window_finder_mus.h"
 #include "ui/display/display.h"
 #include "ui/display/screen.h"
 #include "ui/gfx/geometry/point_conversions.h"
@@ -28,10 +27,6 @@ gfx::Point DIPToPixelPoint(const gfx::Point& dip_point) {
 gfx::NativeWindow WindowFinder::GetLocalProcessWindowAtPoint(
     const gfx::Point& screen_point,
     const std::set<gfx::NativeWindow>& ignore) {
-  gfx::NativeWindow mus_result = nullptr;
-  if (GetLocalProcessWindowAtPointMus(screen_point, ignore, &mus_result))
-    return mus_result;
-
   // The X11 server is the canonical state of what the window stacking order is.
   views::X11TopmostWindowFinder finder;
   return finder.FindLocalProcessWindowAt(DIPToPixelPoint(screen_point), ignore);
