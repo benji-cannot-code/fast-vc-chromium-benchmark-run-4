075ea0d715e7c535b0150d2669d4bc702937d9c9
Esmael El-Moslimany <aee@chromium.org>
2018-07-25 00:26:02 +0000

    Settings: site-list, use iron-list for a11y
    Bug: 867023
    Change-Id: Iacb0e3a1a5083d6f35564f4b0e3c6c9590c576d1
    Reviewed-on: https://chromium-review.googlesource.com/1145775
    Reviewed-by: Scott Chen <scottchen@chromium.org>
    Commit-Queue: Esmael El-Moslimany <aee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577743}

--- chrome/browser/resources/settings/site_settings/site_list.js
+++ chrome/browser/resources/settings/site_settings/site_list.js
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * category.
  */
 Polymer({
-
   is: 'site-list',
 
   behaviors: [
@@ -19,14 +18,6 @@ Polymer({
   ],
 
   properties: {
-    /** @private */
-    enableSiteSettings_: {
-      type: Boolean,
-      value: function() {
-        return loadTimeData.getBoolean('enableSiteSettings');
-      },
-    },
-
     /**
      * Some content types (like Location) do not allow the user to manually
      * edit the exception list from within Settings.
@@ -109,6 +100,9 @@ Polymer({
         SESSION_ONLY: 'SessionOnly',
       }
     },
+
+    /** @private */
+    lastFocused_: Object,
   },
 
   /**
@@ -194,30 +188,6 @@ Polymer({
   },
 
   /**
-   * @param {!SiteException} exception The content setting exception.
-   * @param {boolean} readOnlyList Whether the site exception list is read-only.
-   * @return {boolean}
-   * @private
-   */
-  shouldHideResetButton_: function(exception, readOnlyList) {
-    return exception.enforcement ==
-        chrome.settingsPrivate.Enforcement.ENFORCED ||
-        !(readOnlyList || !!exception.embeddingOrigin);
-  },
-
-  /**
-   * @param {!SiteException} exception The content setting exception.
-   * @param {boolean} readOnlyList Whether the site exception list is read-only.
-   * @return {boolean}
-   * @private
-   */
-  shouldHideActionMenu_: function(exception, readOnlyList) {
-    return exception.enforcement ==
-        chrome.settingsPrivate.Enforcement.ENFORCED ||
-        readOnlyList || !!exception.embeddingOrigin;
-  },
-
-  /**
    * A handler for the Add Site button.
    * @private
    */
@@ -288,29 +258,6 @@ Polymer({
   },
 
   /**
-   * A handler for selecting a site (by clicking on the origin).
-   * @param {!{model: !{item: !SiteException}}} event
-   * @private
-   */
-  onOriginTap_: function(event) {
-    if (!this.enableSiteSettings_)
-      return;
-    settings.navigateTo(
-        settings.routes.SITE_SETTINGS_SITE_DETAILS,
-        new URLSearchParams('site=' + event.model.item.origin));
-  },
-
-  /**
-   * @param {?SiteException} site
-   * @private
-   */
-  resetPermissionForOrigin_: function(site) {
-    assert(site);
-    this.browserProxy.resetCategoryPermissionForPattern(
-        site.origin, site.embeddingOrigin, this.category, site.incognito);
-  },
-
-  /**
    * @param {!settings.ContentSetting} contentSetting
    * @private
    */
@@ -360,51 +307,20 @@ Polymer({
 
   /** @private */
   onResetTap_: function() {
-    this.resetPermissionForOrigin_(this.actionMenuSite_);
+    const site = this.actionMenuSite_;
+    assert(site);
+    this.browserProxy.resetCategoryPermissionForPattern(
+        site.origin, site.embeddingOrigin, this.category, site.incognito);
     this.closeActionMenu_();
   },
 
   /**
-   * Returns the appropriate site description to display. This can, for example,
-   * be blank, an 'embedded on <site>' or 'Current incognito session' (or a
-   * mix of the last two).
-   * @param {SiteException} item The site exception entry.
-   * @return {string} The site description.
-   */
-  computeSiteDescription_: function(item) {
-    let displayName = '';
-    if (item.embeddingOrigin) {
-      displayName = loadTimeData.getStringF(
-          'embeddedOnHost', this.sanitizePort(item.embeddingOrigin));
-    } else if (this.category == settings.ContentSettingsTypes.GEOLOCATION) {
-      displayName = loadTimeData.getString('embeddedOnAnyHost');
-    }
-
-    if (item.incognito) {
-      if (displayName.length > 0)
-        return loadTimeData.getStringF('embeddedIncognitoSite', displayName);
-      return loadTimeData.getString('incognitoSite');
-    }
-    return displayName;
-  },
-
-  /**
-   * @param {!{model: !{item: !SiteException}}} e
+   * @param {!Event} e
    * @private
    */
-  onResetButtonTap_: function(e) {
-    this.resetPermissionForOrigin_(e.model.item);
-  },
-
-  /**
-   * @param {!{model: !{item: !SiteException}}} e
-   * @private
-   */
-  onShowActionMenuTap_: function(e) {
-    this.activeDialogAnchor_ = /** @type {!HTMLElement} */ (
-        Polymer.dom(/** @type {!Event} */ (e)).localTarget);
-
-    this.actionMenuSite_ = e.model.item;
+  onShowActionMenu_: function(e) {
+    this.activeDialogAnchor_ = /** @type {!HTMLElement} */ (e.detail.anchor);
+    this.actionMenuSite_ = e.detail.model;
     /** @type {!CrActionMenuElement} */ (this.$$('cr-action-menu'))
         .showAt(this.activeDialogAnchor_);
   },
