f87fa409d687899442961376f5f998d37b463541
Oscar Johansson <oscarj@opera.com>
2018-07-25 12:47:31 +0000

    Move function to util files (media/mojo)
    When building using jumbo, files gets merged and functions
    with the same name may end up in the same scope and conflict.
    
    This commits moves such functions to shared util files,
    instead of having duplicates.
    
    Bug: 867350
    Change-Id: I9986d6d653ef71acbf9583e18aa85a8e34f6e6f0
    Reviewed-on: https://chromium-review.googlesource.com/1149781
    Reviewed-by: Chrome Cunningham (In Paris) <chcunningham@chromium.org>
    Commit-Queue: Oscar Johansson <oscarj@opera.com>
    Cr-Commit-Position: refs/heads/master@{#577850}

--- media/mojo/common/mojo_data_pipe_read_write.cc
+++ media/mojo/common/mojo_data_pipe_read_write.cc
@@ -9,16 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
+#include "media/mojo/common/mojo_pipe_read_write_util.h"
+
+using media::mojo_pipe_read_write_util::IsPipeReadWriteError;
 
 namespace media {
-
-namespace {
-
-bool IsPipeReadWriteError(MojoResult result) {
-  return result != MOJO_RESULT_OK && result != MOJO_RESULT_SHOULD_WAIT;
-}
-
-}  // namespace
 
 // MojoDataPipeReader
 
---
+++ media/mojo/common/mojo_pipe_read_write_util.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "media/mojo/common/mojo_pipe_read_write_util.h"
+
+#include "mojo/public/c/system/types.h"
+
+namespace media {
+namespace mojo_pipe_read_write_util {
+
+bool IsPipeReadWriteError(MojoResult result) {
+  return result != MOJO_RESULT_OK && result != MOJO_RESULT_SHOULD_WAIT;
+}
+
+}  // namespace mojo_pipe_read_write_util
+}  // namespace media
