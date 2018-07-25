005b7ef7a3ca0ecb7a3e4f14b357b8fc41b693cb
Kush Sinha <sinhak@chromium.org>
2018-07-25 15:48:10 +0000

    Fix race condition in Account Manager UI
    A race condition exists between Account Manager's JS components'
    initialization and AccountTrackerService callbacks.
    
    Bug: 833797
    Change-Id: I8eaf0d134aef4c08e089494b3ddd7d1bf7d0b2ae
    Reviewed-on: https://chromium-review.googlesource.com/1150171
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577907}

--- chrome/browser/ui/webui/settings/chromeos/account_manager_handler.cc
+++ chrome/browser/ui/webui/settings/chromeos/account_manager_handler.cc
@@ -158,6 +158,10 @@ void AccountManagerUIHandler::OnAccountRemoved(const AccountInfo& account_key) {
 }
 
 void AccountManagerUIHandler::RefreshUI() {
+  if (!IsJavascriptAllowed()) {
+    return;
+  }
+
   FireWebUIListener("accounts-changed");
 }
 
