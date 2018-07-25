84a53b330de78af66012c2c6bdfa5f6ab7f2da26
Yiming Zhou <uwyiming@google.com>
2018-07-25 02:03:27 +0000

    Adding fixes and features for the Action Recorder Extension.
    1. Fixed an edge case where the Recorder UI is truncated by the parent
    page's stylesheet. Added an explicit padding style to the Recorder UI.
    2. Record page load events on the main frame. I can later use the page
    load events to truncate test recipes.
    3. Add a typePassword action.
    
    Bug: 855284
    Change-Id: Ieac830addbc7dff303ab456438328ffd64e12953
    Reviewed-on: https://chromium-review.googlesource.com/1145624
    Commit-Queue: Yiming Zhou <uwyiming@google.com>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/master@{#577771}

--- components/test/data/autofill/automated_integration/action_recorder_extension/background/background.js
+++ components/test/data/autofill/automated_integration/action_recorder_extension/background/background.js
@@ -661,6 +661,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           // occur on 'about:'' pages such as the blank page. Plus, this
           // extension has no permission to access 'about:' pages.
           !details.url.startsWith('about:')) {
+
+        // If the tab's root frame loaded a new page, log the page's url.
+        // An engineer can truncate a test recipe generated for captured sites
+        // using the 'loadPage' actions. If the Web Page Replay (WPR) tool can
+        // serve a 'loadPage' action url, then the engineer can delete all the
+        // actions that precedes the 'loadPage' action.
+        if (details.frameId === 0) {
+          addActionToRecipe({
+            url: details.url,
+            context: { 'isIframe': false },
+            type: 'loadPage'
+          });
+        }
+
         startRecordingOnTabAndFrame(tabId, details.frameId)
         .then(() => getRecordingState())
         .then((state) => {
--- components/test/data/autofill/automated_integration/action_recorder_extension/content/foreground.js
+++ components/test/data/autofill/automated_integration/action_recorder_extension/content/foreground.js
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     panelUi.src = chrome.runtime.getURL('content/panel.html');
     panelUi.style.border = 'none';
     panelUi.style.height = '613px';
+    panelUi.style.padding = '0';
     panelUi.style.position = 'fixed';
     panelUi.style.right = 0;
     panelUi.style.top = '100px';
