391617c2046f3c480ab941fba5fb41c690bc0bfa
Steven Bennetts <stevenjb@chromium.org>
2018-07-25 16:43:25 +0000

    Network config: Elim bogus profile path unknown errors
    Currently calls to FindPolicyByGuidAndProfile produce an error when
    called with an empty profile path. There are several places where this
    can occur, but we recently added one to the system tray UI:
    
    https://chromium-review.googlesource.com/c/chromium/src/+/1101204/12/ash/system/network/network_list.cc#813
    
    Rather than ensure that we test for an empty profile path everywhere
    (which just indicates a non favorite/saved network), just return nullptr
    with no error when the path is empty.
    
    Bug: None
    Change-Id: I692babc10ae2472e8fa5dbfef1c8be170cc841b2
    Reviewed-on: https://chromium-review.googlesource.com/1149096
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577936}

--- chromeos/network/auto_connect_handler.cc
+++ chromeos/network/auto_connect_handler.cc
@@ -353,7 +353,6 @@ void AutoConnectHandler::DisconnectAndRemoveBlacklistedNetworks() {
       continue;
 
     const bool is_managed =
-        !network->profile_path().empty() && !network->guid().empty() &&
         managed_configuration_handler_->FindPolicyByGuidAndProfile(
             network->guid(), network->profile_path(), nullptr /* onc_source */);
     if (is_managed)
@@ -377,7 +376,6 @@ void AutoConnectHandler::DisconnectFromAllUnmanagedWiFiNetworks(
                                                false, false, 0, &networks);
   for (const NetworkState* network : networks) {
     const bool is_managed =
-        !network->profile_path().empty() && !network->guid().empty() &&
         managed_configuration_handler_->FindPolicyByGuidAndProfile(
             network->guid(), network->profile_path(), nullptr /* onc_source */);
     if (is_managed)
