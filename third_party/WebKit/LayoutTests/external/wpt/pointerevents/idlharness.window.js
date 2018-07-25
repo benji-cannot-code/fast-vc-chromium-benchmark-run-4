dc09b6b9d965a65c55dd81e2ed0e8b0f23edf99e
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-07-25 22:24:17 +0000

    Import wpt@efef417d9f9ceb9016f682c3aa523d31405e1238
    Using wpt-import in Chromium 2bce35f2af9857c3b7dc733e57870c3687b4a11b.
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/21745
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    nzolghadr@chromium.org, mustaq@chromium.org:
      external/wpt/pointerevents
    
    TBR=lukebjerring
    
    No-Export: true
    Change-Id: I54642867f4081a1547147946096e88aa1e032024
    Reviewed-on: https://chromium-review.googlesource.com/1150611
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578099}

---
+++ third_party/WebKit/LayoutTests/external/wpt/pointerevents/idlharness.window.js
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/resources/WebIDLParser.js
+// META: script=/resources/idlharness.js
+
+'use strict';
+
+// https://w3c.github.io/pointerevents/
+
+idl_test(
+  ['pointerevents'],
+  ['uievents', 'dom', 'html'],
+  idl_array => {
+    idl_array.add_objects({
+      Element: ['document'],
+      Window: ['window'],
+      Navigator: ['navigator'],
+      PointerEvent: ['new PointerEvent("type")']
+    });
+  },
+  'pointerevents interfaces'
+);
