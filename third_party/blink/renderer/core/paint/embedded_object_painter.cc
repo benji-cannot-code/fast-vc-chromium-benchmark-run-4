3b05da433ab024e152d1df415cb5f4b8ecb8b5f3
Tien-Ren Chen <trchen@chromium.org>
2018-07-25 00:23:28 +0000

    [Blink] Unify replaced element clipping
    Prior to this CL each replaced element type implemented its own clipping.
    This CL refactors it so that a clip node will be applied in the common
    path, ReplacedPainter::Paint(), so the specialized
    XyzPainter::PaintReplaced() won't need to apply their own clips.
    
    Note that ImagePainter still mutates its internal painting for changed
    clip rect. This behavior should be changed in a follow-up CL, and
    changing only the content box of a replaced element should only result
    in incremental invalidation.
    
    BUG=730284
    
    Cq-Include-Trybots: luci.chromium.try:linux-blink-gen-property-trees;luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ia5c1412b1bd3182a22849d0b3196f3e72d52248a
    Reviewed-on: https://chromium-review.googlesource.com/1147617
    Commit-Queue: Tien-Ren Chen <trchen@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577741}

--- third_party/blink/renderer/core/paint/embedded_object_painter.cc
+++ third_party/blink/renderer/core/paint/embedded_object_painter.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/settings.h"
 #include "third_party/blink/renderer/core/layout/layout_embedded_object.h"
 #include "third_party/blink/renderer/core/layout/layout_theme.h"
+#include "third_party/blink/renderer/core/paint/embedded_content_painter.h"
 #include "third_party/blink/renderer/core/paint/paint_info.h"
 #include "third_party/blink/renderer/platform/fonts/font.h"
 #include "third_party/blink/renderer/platform/fonts/font_selector.h"
@@ -38,8 +39,11 @@ static Font ReplacementTextFont() {
 
 void EmbeddedObjectPainter::PaintReplaced(const PaintInfo& paint_info,
                                           const LayoutPoint& paint_offset) {
-  if (!layout_embedded_object_.ShowsUnavailablePluginIndicator())
+  if (!layout_embedded_object_.ShowsUnavailablePluginIndicator()) {
+    EmbeddedContentPainter(layout_embedded_object_)
+        .PaintReplaced(paint_info, paint_offset);
     return;
+  }
 
   if (paint_info.phase == PaintPhase::kSelection)
     return;
