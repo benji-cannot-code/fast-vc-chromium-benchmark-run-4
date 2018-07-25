cda0edbfd6af99a57e2a41b6cf1eeae5813c3d5e
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2018-07-25 03:48:32 +0000

    desktop-pwas: Skeleton for BookmarkAppInstallationTask
    Introduces the BookmarkAppInstallationTask. This class purpose is to
    perform the necessary steps to install an BookmarkApp from a
    WebContents or a WebApplicationInfo.
    
    This class will have two subclasses BookmarkAppShortcutInstallation task
    and BookmarkAppWebAppInstallationTask. The former is included in this
    CL and installs BookmarkApp-based Shortcuts. The latter will install
    BookmarkApp-based Web Apps.
    
    Bug: 864904
    
    Change-Id: I5b5b9f44ff0984b0138663cb7c84abcba1062c77
    Reviewed-on: https://chromium-review.googlesource.com/1131024
    Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577790}

---
+++ chrome/browser/web_applications/extensions/bookmark_app_shortcut_installation_task.h
@@ -0,0 +1,43 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_WEB_APPLICATIONS_EXTENSIONS_BOOKMARK_APP_SHORTCUT_INSTALLATION_TASK_H_
+#define CHROME_BROWSER_WEB_APPLICATIONS_EXTENSIONS_BOOKMARK_APP_SHORTCUT_INSTALLATION_TASK_H_
+
+#include "base/callback.h"
+#include "base/memory/weak_ptr.h"
+#include "base/optional.h"
+#include "chrome/browser/web_applications/extensions/bookmark_app_installation_task.h"
+#include "chrome/common/web_application_info.h"
+
+namespace content {
+class WebContents;
+}
+
+namespace extensions {
+
+// Subclass of BookmarkAppInstallationTask that exclusively installs
+// BookmarkApp-based Shortcuts.
+class BookmarkAppShortcutInstallationTask : public BookmarkAppInstallationTask {
+ public:
+  BookmarkAppShortcutInstallationTask();
+  ~BookmarkAppShortcutInstallationTask() override;
+
+  void InstallFromWebContents(content::WebContents* web_contents,
+                              ResultCallback callback);
+
+ private:
+  void OnGetWebApplicationInfo(ResultCallback result_callback,
+                               base::Optional<WebApplicationInfo> web_app_info);
+
+  base::WeakPtrFactory<BookmarkAppShortcutInstallationTask> weak_ptr_factory_{
+      this};
+
+  DISALLOW_COPY_AND_ASSIGN(BookmarkAppShortcutInstallationTask);
+};
+
+}  // namespace extensions
+
+#endif  // CHROME_BROWSER_WEB_APPLICATIONS_EXTENSIONS_BOOKMARK_APP_SHORTCUT_INSTALLATION_TASK_H_
