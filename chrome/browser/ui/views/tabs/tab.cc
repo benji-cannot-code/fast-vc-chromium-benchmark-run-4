4622437afa1144bcb0d286b84ad73642f38108c9
Avi Drissman <avi@chromium.org>
2018-07-25 17:49:07 +0000

    Fix the attention indicator.
    It wasn't showing up for dialogs on background tabs (broken in
    r520759). This fixes that and adds a test.
    
    BUG=860185
    
    Signed-off-by: Avi Drissman <avi@chromium.org>
    Change-Id: I70ddec77e987dff727a5189e810aad5c3fc589de
    Reviewed-on: https://chromium-review.googlesource.com/1147705
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577966}

--- chrome/browser/ui/views/tabs/tab.cc
+++ chrome/browser/ui/views/tabs/tab.cc
@@ -1034,10 +1034,7 @@ bool Tab::IsActive() const {
 }
 
 void Tab::ActiveStateChanged() {
-  if (IsActive()) {
-    // Clear the blocked WebContents for active tabs because it's distracting.
-    icon_->SetAttention(TabIcon::AttentionType::kBlockedWebContents, false);
-  }
+  UpdateTabIconNeedsAttentionBlocked();
   OnButtonColorMaybeChanged();
   alert_indicator_button_->UpdateEnabledForMuteToggle();
   Layout();
@@ -1071,14 +1068,7 @@ void Tab::SetData(TabRendererData data) {
   icon_->SetNetworkState(data_.network_state, data_.should_hide_throbber);
   icon_->SetCanPaintToLayer(controller_->CanPaintThrobberToLayer());
   icon_->SetIsCrashed(data_.IsCrashed());
-  if (IsActive()) {
-    icon_->SetAttention(TabIcon::AttentionType::kBlockedWebContents, false);
-  } else {
-    // Only non-active WebContents get the blocked attention type because it's
-    // confusing on the active tab.
-    icon_->SetAttention(TabIcon::AttentionType::kBlockedWebContents,
-                        data_.blocked);
-  }
+  UpdateTabIconNeedsAttentionBlocked();
 
   base::string16 title = data_.title;
   if (title.empty()) {
@@ -1698,6 +1688,18 @@ void Tab::OnButtonColorMaybeChanged() {
                            ? GetCloseTabButtonColor(views::Button::STATE_NORMAL)
                            : button_color_;
   close_button_->SetIconColors(icon_color);
+}
+
+void Tab::UpdateTabIconNeedsAttentionBlocked() {
+  // Only show the blocked attention indicator on non-active tabs. For active
+  // tabs, the user sees the dialog blocking the tab, so there's no point to it
+  // and it would be distracting.
+  if (IsActive()) {
+    icon_->SetAttention(TabIcon::AttentionType::kBlockedWebContents, false);
+  } else {
+    icon_->SetAttention(TabIcon::AttentionType::kBlockedWebContents,
+                        data_.blocked);
+  }
 }
 
 Tab::BackgroundCache::BackgroundCache() = default;
