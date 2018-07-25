d77baa211e190e4705a0728803d807e907d917ea
Ned Nguyen <nednguyen@google.com>
2018-07-25 22:16:06 +0000

    Refactor results_dashboard code to be cleaner
    Bug: 864565
    Cq-Include-Trybots: master.tryserver.chromium.perf:obbs_fyi
    Change-Id: I1fbea8b4afd7f6be773db2ea5f83e0a0aa549b41
    Reviewed-on: https://chromium-review.googlesource.com/1150431
    Reviewed-by: Simon Hatch <simonhatch@chromium.org>
    Commit-Queue: Ned Nguyen <nednguyen@google.com>
    Cr-Commit-Position: refs/heads/master@{#578095}

--- tools/perf/core/results_dashboard_unittest.py
+++ tools/perf/core/results_dashboard_unittest.py
@@ -18,8 +18,10 @@ class ResultsDashboardTest(unittest.TestCase):
     self.dashboard_url = 'https://chromeperf.appspot.com'
 
   def testRetryForSendResultRetryException(self):
-    def raise_retry_exception(url, histogramset_json, service_account_file):
+    def raise_retry_exception(
+        url, histogramset_json, service_account_file, token_generator_callback):
       del url, histogramset_json, service_account_file  # unused
+      del token_generator_callback  # unused
       raise results_dashboard.SendResultsRetryException('Should retry')
 
     with mock.patch('core.results_dashboard._SendHistogramJson',
@@ -33,8 +35,10 @@ class ResultsDashboardTest(unittest.TestCase):
 
   def testNoRetryForSendResultFatalException(self):
 
-    def raise_retry_exception(url, histogramset_json, service_account_file):
+    def raise_retry_exception(
+        url, histogramset_json, service_account_file, token_generator_callback):
       del url, histogramset_json, service_account_file  # unused
+      del token_generator_callback  # unused
       raise results_dashboard.SendResultsFatalException('Do not retry')
 
     with mock.patch('core.results_dashboard._SendHistogramJson',
@@ -60,8 +64,9 @@ class ResultsDashboardTest(unittest.TestCase):
   def testNoRetryAfterSucessfulSendResult(self):
     counter = [0]
     def raise_retry_exception_first_two_times(
-        url, histogramset_json, service_account_file):
+        url, histogramset_json, service_account_file, token_generator_callback):
       del url, histogramset_json, service_account_file  # unused
+      del token_generator_callback  # unused
       counter[0] += 1
       if counter[0] <= 2:
         raise results_dashboard.SendResultsRetryException('Please retry')
