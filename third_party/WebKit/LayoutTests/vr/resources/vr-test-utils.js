ad68b5d16189bb77fd07665130c2dd1ee7e8afe6
Anna Offenwanger <offenwanger@chromium.org>
2018-07-25 23:24:32 +0000

    Removing code duplication between XR layout tests and wpt tests
    Changing all the WebXR internal tests to use the test API specified in
    https://github.com/immersive-web/webxr-test-api. This will make tests
    more maintainable as changes to the backend mocking need only be made
    once.
    
    Bug: 863557
    Change-Id: I4b8740269c0faa74073ef9294b6b27b78b18715a
    Reviewed-on: https://chromium-review.googlesource.com/1136982
    Commit-Queue: Anna Offenwanger <offenwanger@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Bill Orr <billorr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578123}

--- third_party/WebKit/LayoutTests/vr/resources/vr-test-utils.js
+++ third_party/WebKit/LayoutTests/vr/resources/vr-test-utils.js
@@ -1,10 +1,52 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 'use strict';
 
+MockDevice.prototype.setPose = function(pose) {
+  if (pose == null) {
+    this.pose_ = null;
+  } else {
+    this.pose_ = {
+      orientation: null,
+      position: null,
+      angularVelocity: null,
+      linearVelocity: null,
+      angularAcceleration: null,
+      linearAcceleration: null,
+      inputState: null,
+      poseReset: false,
+      poseIndex: 0
+    };
+    for (var field in pose) {
+      if (this.pose_.hasOwnProperty(field)) {
+        this.pose_[field] = pose[field];
+      }
+    }
+  }
+};
+
+MockDevice.prototype.forceActivate = function(reason) {
+  this.displayClient_.onActivate(reason);
+};
+
 function vr_test(func, vrDisplays, name, properties) {
-  setFakeDevices(vrDisplays);
-  let t = async_test(name, properties);
-  func(t, mockVRService);
+  let chain = Promise.resolve();
+  let firstDeviceController;
+  vrDisplays.forEach(display => {
+    return chain.then(
+        XRTest
+            .simulateDeviceConnection(
+                {supportsImmersive: display.capabilities.canPresent})
+            .then((deviceController) => {
+              deviceController.displayInfo_ = display;
+              if (!firstDeviceController) {
+                firstDeviceController = deviceController;
+              }
+            }));
+  });
+  chain.then(() => {
+    let t = async_test(name, properties);
+    func(t, firstDeviceController);
+  })
 }
 
 function fakeVRDisplays(){
