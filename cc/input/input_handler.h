94210bbf40283b68260dd4d0076bd934de1babf2
Robert Flack <flackr@chromium.org>
2018-07-25 14:54:42 +0000

    Remove use of layer ids from LayerTreeHostImpl scroll methods.
    LayerTreeHostImpl::ScrollLayerTo and
    LayerTreeHostImpl::GetScrollOffsetForLayer looked up the cc::LayerImpl
    from the active tree by layer_id, however the caller knows the element
    id and this allows us to more directly look up the scroll node and ask
    the scroll tree directly. This moves us towards not needing layer ids in
    cc.
    
    Bug: 865973
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ic279a95592071bbd93cd4c239a5a738b6041d292
    Reviewed-on: https://chromium-review.googlesource.com/1145130
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577887}

--- cc/input/input_handler.h
+++ cc/input/input_handler.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/input/scroll_state.h"
 #include "cc/input/scrollbar.h"
 #include "cc/input/touch_action.h"
+#include "cc/trees/element_id.h"
 #include "cc/trees/swap_promise_monitor.h"
 
 namespace gfx {
@@ -228,10 +229,11 @@ class CC_EXPORT InputHandler {
   virtual ScrollElasticityHelper* CreateScrollElasticityHelper() = 0;
 
   // Called by the single-threaded UI Compositor to get or set the scroll offset
-  // on the impl side. Retruns false if |layer_id| isn't in the active tree.
-  virtual bool GetScrollOffsetForLayer(int layer_id,
+  // on the impl side. Returns false if |element_id| isn't in the active tree.
+  virtual bool GetScrollOffsetForLayer(ElementId element_id,
                                        gfx::ScrollOffset* offset) = 0;
-  virtual bool ScrollLayerTo(int layer_id, const gfx::ScrollOffset& offset) = 0;
+  virtual bool ScrollLayerTo(ElementId element_id,
+                             const gfx::ScrollOffset& offset) = 0;
 
   virtual bool ScrollingShouldSwitchtoMainThread() = 0;
 
--- cc/trees/property_tree.h
+++ cc/trees/property_tree.h
@@ -461,6 +461,7 @@ class CC_EXPORT ScrollTree final : public PropertyTree<ScrollNode> {
   void CopyCompleteTreeState(const ScrollTree& other);
 #endif
 
+  ScrollNode* FindNodeFromElementId(ElementId id);
   const ScrollNode* FindNodeFromElementId(ElementId id) const;
 
  private:
