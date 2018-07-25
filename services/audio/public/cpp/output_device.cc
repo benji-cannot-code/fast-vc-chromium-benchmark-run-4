71d6d55e2a76d3c09f69115b785b81e276d63b31
Olga Sharonova <olka@chromium.org>
2018-07-25 10:19:36 +0000

    Allow blocking when audio::OutputDevice joins the audio thread.
    BUG=858987
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: Ic61830a546ffe6873fef4db18a49d03031293fe7
    Reviewed-on: https://chromium-review.googlesource.com/1119914
    Reviewed-by: Marina Ciocea <marinaciocea@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Max Morin <maxmorin@chromium.org>
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577832}

--- services/audio/public/cpp/output_device.cc
+++ services/audio/public/cpp/output_device.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
+#include "base/threading/thread_restrictions.h"
 #include "media/audio/audio_output_device_thread_callback.h"
 #include "mojo/public/cpp/system/platform_handle.h"
 #include "services/audio/public/mojom/constants.mojom.h"
@@ -28,8 +29,8 @@ OutputDevice::OutputDevice(
 
   media::mojom::AudioOutputStreamRequest stream_request =
       mojo::MakeRequest(&stream_);
-  stream_.set_connection_error_handler(
-      base::BindOnce(&OutputDevice::CleanUp, weak_factory_.GetWeakPtr()));
+  stream_.set_connection_error_handler(base::BindOnce(
+      &OutputDevice::OnConnectionError, weak_factory_.GetWeakPtr()));
   stream_factory_->CreateOutputStream(
       std::move(stream_request), nullptr, nullptr, device_id, params,
       base::UnguessableToken::Create(),
@@ -69,16 +70,25 @@ void OutputDevice::StreamCreated(
       data_pipe->shared_memory;
   DCHECK(shared_memory_region.IsValid());
 
-  audio_callback_.reset(new media::AudioOutputDeviceThreadCallback(
-      audio_parameters_, std::move(shared_memory_region), render_callback_));
-  audio_thread_.reset(new media::AudioDeviceThread(
+  DCHECK(!audio_callback_);
+  DCHECK(!audio_thread_);
+  audio_callback_ = std::make_unique<media::AudioOutputDeviceThreadCallback>(
+      audio_parameters_, std::move(shared_memory_region), render_callback_);
+  audio_thread_ = std::make_unique<media::AudioDeviceThread>(
       audio_callback_.get(), socket_handle, "audio::OutputDevice",
-      base::ThreadPriority::REALTIME_AUDIO));
+      base::ThreadPriority::REALTIME_AUDIO);
+}
+
+void OutputDevice::OnConnectionError() {
+  // Connection errors should be rare and handling them synchronously is
+  // simpler.
+  base::ScopedAllowBlocking allow_blocking;
+  CleanUp();
 }
 
 void OutputDevice::CleanUp() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  audio_thread_.reset();
+  audio_thread_.reset();  // Blocking call.
   audio_callback_.reset();
   stream_.reset();
   stream_factory_.reset();
