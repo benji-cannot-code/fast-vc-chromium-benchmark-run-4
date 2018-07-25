4be97e31b5b925e60bb5e70071e47ec43ea0099c
Xiaocheng Hu <xiaochengh@chromium.org>
2018-07-25 16:16:44 +0000

    [Find-in-Page] Ensure timeout check in TextFinder::ScopeStringMatches()
    The function's main loop is supposed to halt after timeout, but contains
    a branch that continues directly without checking timeout, which leads
    to renderer hang in some edge/buggy cases.
    
    This patch implements yoichio's idea (https://goo.gl/EQED8Y) to fix the
    issue, by rewriting loop conditions and ensuring timeout checking in
    all branches.
    
    Note: no test in this patch, because the only repro case we have hits
    a DCHECK earlier before entering the problematic branch.
    
    Bug: 862648
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng
    Change-Id: I050909bd625f122c0df7ed905184c32e133170ce
    Reviewed-on: https://chromium-review.googlesource.com/1148645
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577918}

--- third_party/blink/renderer/core/editing/finder/text_finder.cc
+++ third_party/blink/renderer/core/editing/finder/text_finder.cc
@@ -444,7 +444,7 @@ void TextFinder::ScopeStringMatches(int identifier,
   const double kMaxScopingDuration = 0.1;  // seconds
 
   int match_count = 0;
-  bool timed_out = false;
+  bool full_range_searched = false;
   double start_time = CurrentTime();
   PositionInFlatTree next_scoping_start;
   do {
@@ -458,6 +458,7 @@ void TextFinder::ScopeStringMatches(int identifier,
                       search_text, options.match_case ? 0 : kCaseInsensitive);
     if (result.IsCollapsed()) {
       // Not found.
+      full_range_searched = true;
       break;
     }
     Range* result_range = Range::Create(
@@ -517,8 +518,7 @@ void TextFinder::ScopeStringMatches(int identifier,
     search_start = result.EndPosition();
 
     next_scoping_start = search_start;
-    timed_out = (CurrentTime() - start_time) >= kMaxScopingDuration;
-  } while (!timed_out);
+  } while (CurrentTime() - start_time < kMaxScopingDuration);
 
   if (next_scoping_start.IsNotNull()) {
     resume_scoping_from_range_ =
@@ -541,7 +541,7 @@ void TextFinder::ScopeStringMatches(int identifier,
     IncreaseMatchCount(identifier, match_count);
   }
 
-  if (timed_out) {
+  if (!full_range_searched) {
     // If we found anything during this pass, we should redraw. However, we
     // don't want to spam too much if the page is extremely long, so if we
     // reach a certain point we start throttling the redraw requests.
