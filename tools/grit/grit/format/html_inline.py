b1c0650eb6150816e89e1767059f4ca6a7581be1
Alexander Alekseev <alemate@chromium.org>
2018-07-25 21:58:36 +0000

    Grit: Fix bug with generating dependency on hidden files.
    Grit previously ignored <if> conditions when generating dependency file.
    This CL makes Grit to include into dependency only files that were actually
    included in the output.
    
    Bug: 866232,852038
    Change-Id: I8d974219d00a76568263d7ecf8eab811807565b4
    Reviewed-on: https://chromium-review.googlesource.com/1149145
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578086}

--- tools/grit/grit/format/html_inline.py
+++ tools/grit/grit/format/html_inline.py
@@ -368,6 +368,7 @@ def DoInline(
     if names_only:
       inlined_files.update(GetResourceFilenames(
           filepath,
+          grd_node,
           allow_external_script,
           rewrite_function,
           filename_expansion_function=filename_expansion_function))
@@ -556,6 +557,7 @@ def InlineToFile(input_filename, output_filename, grd_node):
 
 
 def GetResourceFilenames(filename,
+                         grd_node,
                          allow_external_script=False,
                          rewrite_function=None,
                          filename_expansion_function=None):
@@ -563,7 +565,7 @@ def GetResourceFilenames(filename,
   try:
     return DoInline(
         filename,
-        None,
+        grd_node,
         names_only=True,
         preprocess_only=False,
         allow_external_script=allow_external_script,
