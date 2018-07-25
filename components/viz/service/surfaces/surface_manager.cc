60520a4c0312c14b2627e212dca449b93f9bbca5
akaba <akaba@google.com>
2018-07-25 20:31:47 +0000

    Fix Compositing.SurfaceAggregator.LatestInFlightSurface.ManhattanDistanceToPrimary
    This CL remove the extra line in histogram.xml that's preventing the UMA from working.
    
    In addition, we rename "Compositing.SurfaceManager.TemporarySurfaces" to
    "Compositing.SurfaceManager.TemporaryReferences" as the data is actually counting
    references not surfaces.
    
    Bug: 867534
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel
    Change-Id: I7c4e4fc1a399d7e0d5fd8c6cea6a166f0ffbba76
    Reviewed-on: https://chromium-review.googlesource.com/1150419
    Commit-Queue: Andre Kaba <akaba@google.com>
    Reviewed-by: Fady Samuel <fsamuel@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578041}

--- components/viz/service/surfaces/surface_manager.cc
+++ components/viz/service/surfaces/surface_manager.cc
@@ -31,8 +31,8 @@ namespace {
 
 const char kUmaAliveSurfaces[] = "Compositing.SurfaceManager.AliveSurfaces";
 
-const char kUmaTemporarySurfaces[] =
-    "Compositing.SurfaceManager.TemporarySurfaces";
+const char kUmaTemporaryReferences[] =
+    "Compositing.SurfaceManager.TemporaryReferences";
 
 constexpr base::TimeDelta kExpireInterval = base::TimeDelta::FromSeconds(10);
 
@@ -238,7 +238,7 @@ void SurfaceManager::GarbageCollectSurfaces() {
   UMA_HISTOGRAM_CUSTOM_COUNTS(kUmaAliveSurfaces, reachable_surfaces.size(), 1,
                               200, 50);
   // Log the number of temporary references after a garbage collection.
-  UMA_HISTOGRAM_CUSTOM_COUNTS(kUmaTemporarySurfaces,
+  UMA_HISTOGRAM_CUSTOM_COUNTS(kUmaTemporaryReferences,
                               temporary_references_.size(), 1, 200, 50);
 
   std::vector<SurfaceId> surfaces_to_delete;
