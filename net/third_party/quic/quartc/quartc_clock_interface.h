4c85c1c5629015f74fb1ce65e423ffc4252a0473
Henrik Boström <hbos@chromium.org>
2018-07-26 09:57:18 +0000

    Revert "Synchronize QuaRTC code up to the Wed Jul 25 20:25:29 EDT 2018"
    This reverts commit 812abd15e33ab7dd8179bca0cf783d03d361cb9a.
    
    Reason for revert: Speculative revert for making several QuartcSessionTests flaky (net_unittests). This is recent to the first flake. The flakes are frequent, but which test fail or on which device of the bot is random.
    
    First flake:
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/ios-simulator/35890
    Other flakes: 35893, 35894, 35895, 35896, 35897, 35898, 35899, 35900
    
    Includes failures in:
    QuartcSessionTest.StreamConnection
    QuartcSessionTest.CancelQuartcStream
    QuartcSessionTest.PreSharedKeyHandshake
    QuartcSessionTest.WriterGivesPacketNumberToTransport
    QuartcSessionTest.CloseConnection
    
    Original change's description:
    > Synchronize QuaRTC code up to the Wed Jul 25 20:25:29 EDT 2018
    > 
    > R=​rch@chromium.org
    > 
    > Change-Id: I93d8c963e2d40a409aab5985625659e39f3ffb87
    > Reviewed-on: https://chromium-review.googlesource.com/1150883
    > Reviewed-by: Ryan Hamilton <rch@chromium.org>
    > Commit-Queue: Victor Vasiliev <vasilvv@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#578172}
    
    TBR=rch@chromium.org,vasilvv@chromium.org
    
    Change-Id: I0b7e0ae028d8371c57e66bc9876d4aa05c10b471
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/1151148
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Commit-Queue: Henrik Boström <hbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578252}

---
+++ net/third_party/quic/quartc/quartc_clock_interface.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NET_THIRD_PARTY_QUIC_QUARTC_QUARTC_CLOCK_INTERFACE_H_
+#define NET_THIRD_PARTY_QUIC_QUARTC_QUARTC_CLOCK_INTERFACE_H_
+
+#include <stdint.h>
+
+namespace quic {
+
+// Implemented by the Quartc API user to provide a timebase.
+class QuartcClockInterface {
+ public:
+  virtual ~QuartcClockInterface() {}
+  virtual int64_t NowMicroseconds() = 0;
+};
+
+}  // namespace quic
+
+#endif  // NET_THIRD_PARTY_QUIC_QUARTC_QUARTC_CLOCK_INTERFACE_H_
