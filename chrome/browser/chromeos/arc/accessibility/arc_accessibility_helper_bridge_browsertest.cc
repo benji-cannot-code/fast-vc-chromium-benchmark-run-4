77ac7f37f9462cd039d5edd346355c17f3e44493
jonross <jonross@chromium.org>
2018-07-26 22:05:59 +0000

    Disable ArcAccessibilityHelperBridgeBrowserTest on Viz
    Currently when using VizDisplayCompositor, Arc doesn't work. There's ongoing
    work for Exo+Viz which is needed to enable it.
    
    Until then I'm adding an early exit to ArcAccessibilityHelperBridgeBrowserTest
    so that we can get ride of the filter files associated with viz_browser_tests
    
    TEST=ArcAccessibilityHelperBridgeBrowserTest
    
    Bug: 807465
    Change-Id: Iae350792c4fcedd23561e524cc7d269c50846e5e
    Reviewed-on: https://chromium-review.googlesource.com/1151760
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578456}

--- chrome/browser/chromeos/arc/accessibility/arc_accessibility_helper_bridge_browsertest.cc
+++ chrome/browser/chromeos/arc/accessibility/arc_accessibility_helper_bridge_browsertest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/arc/accessibility/arc_accessibility_helper_bridge.h"
 
 #include "ash/shell.h"
+#include "base/feature_list.h"
 #include "chrome/browser/chromeos/accessibility/accessibility_manager.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
@@ -18,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/exo/shell_surface.h"
 #include "components/exo/test/exo_test_helper.h"
 #include "components/exo/wm_helper.h"
+#include "components/viz/common/features.h"
 #include "ui/aura/client/aura_constants.h"
 #include "ui/views/widget/widget.h"
 #include "ui/wm/public/activation_client.h"
@@ -65,6 +67,11 @@ class ArcAccessibilityHelperBridgeBrowserTest : public InProcessBrowserTest {
 
 IN_PROC_BROWSER_TEST_F(ArcAccessibilityHelperBridgeBrowserTest,
                        PreferenceChange) {
+  // TODO(penghuang): Re-enable once the EXO+Viz work is done and Arc can be
+  // supported. https://crbug.com/807465
+  if (base::FeatureList::IsEnabled(features::kVizDisplayCompositor))
+    return;
+
   ASSERT_EQ(mojom::AccessibilityFilterType::OFF,
             fake_accessibility_helper_instance_->filter_type());
 
