188171de0d80dd95a2d4cfe9b9bf71a16d97be77
Noel Gordon <noel@chromium.org>
2018-07-26 04:29:42 +0000

    Rename zip archive file content rows helper
    Call it getUnzippedFileRowEntries for less confusion.
    
    Bug: 867738
    Change-Id: I0e38e8c41ec0cfc599aaa24b369738ffea3c2f0a
    Reviewed-on: https://chromium-review.googlesource.com/1150953
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578197}

--- ui/file_manager/integration_tests/file_manager/zip_files.js
+++ ui/file_manager/integration_tests/file_manager/zip_files.js
@@ -6,9 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 'use strict';
 
 /**
- * Returns the file ENTRIES.zipArchive content (2 files) as row entries.
+ * Returns the ENTRIES.zipArchive content (2 files) as row entries.
  */
-function getZipArchiveFileRowEntries() {
+function getUnzippedFileRowEntries() {
   return [
     ['image.png', '272 bytes', 'PNG image', 'Sep 2, 2013, 10:01 PM'],
     ['text.txt', '51 bytes', 'Plain text', 'Sep 2, 2013, 10:01 PM']
@@ -42,7 +42,7 @@ testcase.zipFileOpenDownloads = function() {
     // Check: the zip file content should be shown (unzip).
     function(result) {
       chrome.test.assertTrue(!!result, 'fakeKeyDown failed');
-      const files = getZipArchiveFileRowEntries();
+      const files = getUnzippedFileRowEntries();
       remoteCall.waitForFiles(appId, files).then(this.next);
     },
   ]);
@@ -75,7 +75,7 @@ testcase.zipFileOpenDrive = function() {
     // Check: the zip file content should be shown (unzip).
     function(result) {
       chrome.test.assertTrue(!!result, 'fakeKeyDown failed');
-      const files = getZipArchiveFileRowEntries();
+      const files = getUnzippedFileRowEntries();
       remoteCall.waitForFiles(appId, files).then(this.next);
     },
   ]);
@@ -133,7 +133,7 @@ testcase.zipFileOpenUsb = function() {
     // Check: the zip file content should be shown (unzip).
     function(result) {
       chrome.test.assertTrue(!!result, 'fakeKeyDown failed');
-      const files = getZipArchiveFileRowEntries();
+      const files = getUnzippedFileRowEntries();
       remoteCall.waitForFiles(appId, files).then(this.next);
     },
   ]);
