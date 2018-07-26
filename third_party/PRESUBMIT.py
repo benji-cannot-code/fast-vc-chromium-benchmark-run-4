3970b0866b79cee26d97af810636a664927a52c0
Peter Wen <wnwen@chromium.org>
2018-07-26 20:55:39 +0000

    Android: Use android_deps for play services APIs
    - Rename `repository` to `libs` in order to stop git complaining about
      previously ignored *.info files being overwitten (since we are now
      including them in the repo) when cherry picking
      - Nice side benefit of resetting to -cr0 for packages
    
    - Add gms libraries, previously defined in
      `build/secondary/third_party/android_tools/BUILD.gn`
    
    - Add `third_party/android_deps/Android_SDK_License-December_9_2016.txt`
      - This is used for all gms dependencies which require this license
      - Taken from `https://developer.android.com/studio/terms`
    
    - Pass in output file directly to `aar.py`
    
    - Add .info files to the repository and remove from `.gitignore`
    
    - Increment crX suffix for CIPD packages in order to avoid duplicate
      tags
    
    - Add OWNERS in each package's subdirectory under `libs` so that future
      version upgrades will not require full review, but new packages will
      still go through full third_party review.
    
    Bug: 865020, 865065, 863547
    Change-Id: I91cc400d02ce83e0195d8aab3f76939d6ddcb120
    Reviewed-on: https://chromium-review.googlesource.com/1142189
    Reviewed-by: Grace Kloba <klobag@chromium.org>
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578434}

--- third_party/PRESUBMIT.py
+++ third_party/PRESUBMIT.py
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 ANDROID_WHITELISTED_LICENSES = [
   'A(pple )?PSL 2(\.0)?',
+  'Android Software Development Kit License',
   'Apache( Version)? 2(\.0)?',
   '(New )?([23]-Clause )?BSD( [23]-Clause)?( with advertising clause)?',
   'L?GPL ?v?2(\.[01])?( or later)?',
