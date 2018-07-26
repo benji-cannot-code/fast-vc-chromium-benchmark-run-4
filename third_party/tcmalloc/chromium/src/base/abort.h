e43205fea1ae37f6cea059fd5efb0a80b1b55c0b
kaiwang@chromium.org <kaiwang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2018-07-26 20:51:50 +0000

    Try not to inline tcmalloc::Abort() so we can find it in stack trace.
    BUG=724399,b:70905156
    
    Review URL: https://chromiumcodereview.appspot.com/10535049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141295 0039d316-1c4b-4281-b951-d872f2087c98
    (cherry picked from commit 49e4b033aa8306df6475696017beb40f880878b9)
    Signed-off-by: Gabriel Marin <gmx@chromium.org>
    
    Change-Id: I5a22ef15a79748fb123609c793c96acbdbe903e1
    Reviewed-on: https://chromium-review.googlesource.com/1130778
    Commit-Queue: Gabriel Marin <gmx@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578433}

--- third_party/tcmalloc/chromium/src/base/abort.h
+++ third_party/tcmalloc/chromium/src/base/abort.h
@@ -7,29 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // On some platforms abort() is implemented in a way that Chrome's crash
 // reporter treats it as a normal exit. See issue:
 // http://code.google.com/p/chromium/issues/detail?id=118665
-// So we replace abort with a
-// segmentation fault, that crash reporter can always detect.
+// So we replace abort with a segmentation fault, then crash reporter can
+// always detect.
 
 #ifndef BASE_ABORT_H_
 #define BASE_ABORT_H_
 
-#if defined(TCMALLOC_USE_SYSTEM_ABORT)
-#include <stdlib.h>
-
 namespace tcmalloc {
-inline void Abort() {
-  abort();
-}
+void Abort();
 } // namespace tcmalloc
-
-#else
-namespace tcmalloc {
-inline void Abort() {
-  // Make a segmentation fault to force abort.
-  *reinterpret_cast<volatile int*>(NULL) = 0x2001;
-}
-} // namespace tcmalloc
-
-#endif
 
 #endif  // BASE_ABORT_H_
