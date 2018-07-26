022b38c2fa9a84e7de98c24ade9d6bee22c4a53b
Fady Samuel <fsamuel@chromium.org>
2018-07-26 15:13:21 +0000

    RELAND: Surface Synchronization: Simplify SurfaceDependencyTracker
    Prior to this CL, SurfaceDependencyTracker read activation_dependencies
    from the pending CompositorFrame's CompositorFrameMetadata. This meant
    that it had to look up already resolved dependencies. It's more efficient
    to use the remaining dependencies set tracked in Surface directly. The code
    is also more compact and easier to read.
    
    In the first attempt to land, the CL iterated over activation_dependencies
    as they were changing. This resulted in a crash. The solution is to
    copy over the set before iteration.
    
    Bug: 672962
    Change-Id: I3264f75b3fc71d7cd0368a63fcd38f7afcaf1cb0
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel
    Reviewed-on: https://chromium-review.googlesource.com/1148226
    Reviewed-by: Saman Sami <samans@chromium.org>
    Commit-Queue: Fady Samuel <fsamuel@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1150675
    Cr-Commit-Position: refs/heads/master@{#578307}

--- components/viz/service/surfaces/surface_dependency_tracker.cc
+++ components/viz/service/surfaces/surface_dependency_tracker.cc
@@ -20,8 +20,6 @@ SurfaceDependencyTracker::~SurfaceDependencyTracker() = default;
 void SurfaceDependencyTracker::RequestSurfaceResolution(Surface* surface) {
   DCHECK(surface->HasPendingFrame());
 
-  const CompositorFrame& pending_frame = surface->GetPendingFrame();
-
   if (IsSurfaceLate(surface)) {
     ActivateLateSurfaceSubtree(surface);
     return;
@@ -29,8 +27,7 @@ void SurfaceDependencyTracker::RequestSurfaceResolution(Surface* surface) {
 
   // Activation dependencies that aren't currently known to the surface manager
   // or do not have an active CompositorFrame block this frame.
-  for (const SurfaceId& surface_id :
-       pending_frame.metadata.activation_dependencies) {
+  for (const SurfaceId& surface_id : surface->activation_dependencies()) {
     Surface* dependency = surface_manager_->GetSurfaceForId(surface_id);
     if (!dependency || !dependency->HasActiveFrame()) {
       blocked_surfaces_from_dependency_[surface_id.frame_sink_id()].insert(
@@ -76,12 +73,7 @@ void SurfaceDependencyTracker::OnSurfaceDiscarded(Surface* surface) {
   if (!surface->HasPendingFrame())
     return;
 
-  const CompositorFrame& pending_frame = surface->GetPendingFrame();
-
-  DCHECK(!pending_frame.metadata.activation_dependencies.empty());
-
-  for (const SurfaceId& surface_id :
-       pending_frame.metadata.activation_dependencies) {
+  for (const SurfaceId& surface_id : surface->activation_dependencies()) {
     auto it =
         blocked_surfaces_from_dependency_.find(surface_id.frame_sink_id());
     if (it == blocked_surfaces_from_dependency_.end())
@@ -111,10 +103,9 @@ void SurfaceDependencyTracker::OnFrameSinkInvalidated(
 void SurfaceDependencyTracker::ActivateLateSurfaceSubtree(Surface* surface) {
   DCHECK(surface->HasPendingFrame());
 
-  const CompositorFrame& pending_frame = surface->GetPendingFrame();
-
-  for (const SurfaceId& surface_id :
-       pending_frame.metadata.activation_dependencies) {
+  base::flat_set<SurfaceId> late_dependencies(
+      surface->activation_dependencies());
+  for (const SurfaceId& surface_id : late_dependencies) {
     Surface* dependency = surface_manager_->GetSurfaceForId(surface_id);
     if (dependency && dependency->HasPendingFrame())
       ActivateLateSurfaceSubtree(dependency);
@@ -125,8 +116,6 @@ void SurfaceDependencyTracker::ActivateLateSurfaceSubtree(Surface* surface) {
 
 void SurfaceDependencyTracker::UpdateSurfaceDeadline(Surface* surface) {
   DCHECK(surface->HasPendingFrame());
-
-  const CompositorFrame& pending_frame = surface->GetPendingFrame();
 
   // Inherit the deadline from the first parent blocked on this surface.
   auto it = blocked_surfaces_from_dependency_.find(
@@ -147,8 +136,9 @@ void SurfaceDependencyTracker::UpdateSurfaceDeadline(Surface* surface) {
          surface->has_deadline());
 
   // Recursively propagate the newly set deadline to children.
-  for (const SurfaceId& surface_id :
-       pending_frame.metadata.activation_dependencies) {
+  base::flat_set<SurfaceId> activation_dependencies(
+      surface->activation_dependencies());
+  for (const SurfaceId& surface_id : activation_dependencies) {
     Surface* dependency = surface_manager_->GetSurfaceForId(surface_id);
     if (dependency && dependency->HasPendingFrame())
       UpdateSurfaceDeadline(dependency);
