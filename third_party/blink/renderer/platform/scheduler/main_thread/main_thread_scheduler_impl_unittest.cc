82e807df9a8d925f662c10bdc742e46dc4efeca3
Scott Haseley <shaseley@google.com>
2018-07-26 22:22:17 +0000

    Reland "Reland "[scheduler] Refactor FrameSchedulerImpl to use FrameTaskQueueController""
    This is a reland of fa8e9207b7ab56db8ecd5ef5be21c1944b27a6c4
    
    Original change's description:
    >Reland "[scheduler] Refactor FrameSchedulerImpl to use FrameTaskQueueController"
    >
    >This is a reland of e2d89968d57604c3121004103f8aaf6e442dd5a1
    >
    >Original change's description:
    >> [scheduler] Refactor FrameSchedulerImpl to use FrameTaskQueueController
    >>
    >> This CL moves all task queue management and creation to
    >> FrameTaskQueueController and adds the appropriate interface for
    >> accessing queues in FrameTaskQueueController. TaskType to QueueTraits
    >> management is handled by FrameSchedulerImpl since it embeds policy, and
    >> FrameTaskQueueController exposes an interface to get non-loading task
    >> queues by QueueTraits, as well as accessing loading and loading
    >> control task queues.
    >>
    >> Bug: 859963
    >> Change-Id: I7db5aaaa148d0dc4f3f70e517e088a205e502db8
    >> Reviewed-on: https://chromium-review.googlesource.com/1142390
    >> Commit-Queue: Scott Haseley <shaseley@google.com>
    >> Reviewed-by: Alexander Timin <altimin@chromium.org>
    >> Cr-Commit-Position: refs/heads/master@{#576619}
    >
    >Bug: 859963
    >Change-Id: Id2c9179b580759b815602d61b98c99d1f3f5253d
    >Reviewed-on: https://chromium-review.googlesource.com/1144608
    >Commit-Queue: Scott Haseley <shaseley@google.com>
    >Reviewed-by: Alexander Timin <altimin@chromium.org>
    >Cr-Commit-Position: refs/heads/master@{#578008}
    
    Bug: 859963
    Change-Id: I57e49ea8a7c60b2f0e33905a6701fcb96b5b28bf
    Reviewed-on: https://chromium-review.googlesource.com/1150910
    Commit-Queue: Scott Haseley <shaseley@google.com>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578464}

--- third_party/blink/renderer/platform/scheduler/main_thread/main_thread_scheduler_impl_unittest.cc
+++ third_party/blink/renderer/platform/scheduler/main_thread/main_thread_scheduler_impl_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/scheduler/main_thread/main_thread_scheduler_impl.h"
 
 #include <memory>
+#include <string>
 #include <utility>
 
 #include "base/callback.h"
@@ -31,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/scheduler/common/throttling/budget_pool.h"
 #include "third_party/blink/renderer/platform/scheduler/main_thread/auto_advancing_virtual_time_domain.h"
 #include "third_party/blink/renderer/platform/scheduler/main_thread/frame_scheduler_impl.h"
+#include "third_party/blink/renderer/platform/scheduler/main_thread/frame_task_queue_controller.h"
 #include "third_party/blink/renderer/platform/testing/runtime_enabled_features_test_helpers.h"
 
 using base::sequence_manager::TaskQueue;
@@ -355,10 +357,14 @@ class MainThreadSchedulerImplTest : public testing::Test {
         FrameSchedulerImpl::Create(page_scheduler_.get(), nullptr, nullptr,
                                    FrameScheduler::FrameType::kMainFrame);
 
-    loading_task_runner_ = main_frame_scheduler_->LoadingTaskQueue();
+    auto* frame_task_queue_controller =
+        main_frame_scheduler_->FrameTaskQueueControllerForTest();
+    loading_task_runner_ = frame_task_queue_controller->LoadingTaskQueue();
     loading_control_task_runner_ =
-        main_frame_scheduler_->LoadingControlTaskQueue();
-    timer_task_runner_ = main_frame_scheduler_->ThrottleableTaskQueue();
+        frame_task_queue_controller->LoadingControlTaskQueue();
+    auto queue_traits = main_frame_scheduler_->ThrottleableTaskQueueTraits();
+    timer_task_runner_ =
+        frame_task_queue_controller->NonLoadingTaskQueue(queue_traits);
   }
 
   void TearDown() override {
@@ -769,7 +775,10 @@ class MainThreadSchedulerImplTest : public testing::Test {
 
   static scoped_refptr<TaskQueue> ThrottleableTaskQueue(
       FrameSchedulerImpl* scheduler) {
-    return scheduler->ThrottleableTaskQueue();
+    auto* frame_task_queue_controller =
+        scheduler->FrameTaskQueueControllerForTest();
+    auto queue_traits = FrameSchedulerImpl::ThrottleableTaskQueueTraits();
+    return frame_task_queue_controller->NonLoadingTaskQueue(queue_traits);
   }
 
   QueueingTimeEstimator* queueing_time_estimator() {
@@ -3669,6 +3678,7 @@ TEST_F(MainThreadSchedulerImplTest, Tracing) {
   std::unique_ptr<base::trace_event::ConvertableToTraceFormat> value =
       scheduler_->AsValue(base::TimeTicks());
   EXPECT_TRUE(value);
+  EXPECT_FALSE(value->ToString().empty());
 }
 
 void RecordingTimeTestTask(
--- third_party/blink/renderer/platform/scheduler/main_thread/page_scheduler_impl_unittest.cc
+++ third_party/blink/renderer/platform/scheduler/main_thread/page_scheduler_impl_unittest.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/renderer/platform/scheduler/main_thread/frame_scheduler_impl.h"
+#include "third_party/blink/renderer/platform/scheduler/main_thread/frame_task_queue_controller.h"
 #include "third_party/blink/renderer/platform/scheduler/main_thread/main_thread_scheduler_impl.h"
 #include "third_party/blink/renderer/platform/scheduler/main_thread/page_visibility_state.h"
 #include "third_party/blink/renderer/platform/testing/runtime_enabled_features_test_helpers.h"
@@ -83,7 +84,10 @@ class PageSchedulerImplTest : public testing::Test {
 
   static scoped_refptr<TaskQueue> ThrottleableTaskQueueForScheduler(
       FrameSchedulerImpl* scheduler) {
-    return scheduler->ThrottleableTaskQueue();
+    auto* frame_task_queue_controller =
+        scheduler->FrameTaskQueueControllerForTest();
+    auto queue_traits = FrameSchedulerImpl::ThrottleableTaskQueueTraits();
+    return frame_task_queue_controller->NonLoadingTaskQueue(queue_traits);
   }
 
   base::TimeDelta delay_for_background_tab_freezing() const {
@@ -102,24 +106,32 @@ class PageSchedulerImplTest : public testing::Test {
     return LoadingTaskQueue()->CreateTaskRunner(TaskType::kInternalTest);
   }
 
+  scoped_refptr<MainThreadTaskQueue> NonLoadingTaskQueue(
+      MainThreadTaskQueue::QueueTraits queue_traits) {
+    return frame_scheduler_->FrameTaskQueueControllerForTest()
+        ->NonLoadingTaskQueue(queue_traits);
+  }
+
   scoped_refptr<MainThreadTaskQueue> ThrottleableTaskQueue() {
-    return frame_scheduler_->ThrottleableTaskQueue();
+    return NonLoadingTaskQueue(
+        FrameSchedulerImpl::ThrottleableTaskQueueTraits());
   }
 
   scoped_refptr<MainThreadTaskQueue> LoadingTaskQueue() {
-    return frame_scheduler_->LoadingTaskQueue();
+    return frame_scheduler_->FrameTaskQueueControllerForTest()
+        ->LoadingTaskQueue();
   }
 
   scoped_refptr<MainThreadTaskQueue> DeferrableTaskQueue() {
-    return frame_scheduler_->DeferrableTaskQueue();
+    return NonLoadingTaskQueue(FrameSchedulerImpl::DeferrableTaskQueueTraits());
   }
 
   scoped_refptr<MainThreadTaskQueue> PausableTaskQueue() {
-    return frame_scheduler_->PausableTaskQueue();
+    return NonLoadingTaskQueue(FrameSchedulerImpl::PausableTaskQueueTraits());
   }
 
   scoped_refptr<MainThreadTaskQueue> UnpausableTaskQueue() {
-    return frame_scheduler_->UnpausableTaskQueue();
+    return NonLoadingTaskQueue(FrameSchedulerImpl::UnpausableTaskQueueTraits());
   }
 
   bool ShouldFreezePage() { return page_scheduler_->ShouldFreezePage(); }
