661033cf7d8adff477959a170c57df0f7051fa08
Paul Miller <paulmiller@google.com>
2018-07-26 00:08:32 +0000

    WebView: Remove variations experiment code
    There were 4 mechanisms for enabling/disabling variations in WebView:
    - a command-line flag (mEnabledByCmd)
    - the AGSA experiment in m67 (mEnabledByExperiment)
    - the hard-coded setting (sVariationsAlwaysEnabled)
    - the hold-back experiment in m68 & m69 (sEnableState)
    
    Remove all of them, so that variations is always enabled.
    
    Set the hold-back experiment histogram, VariationsEnableState, to expire
    after m69. Don't mark it as obsolete yet, because we'll want to watch it
    for the lifetime of m68 & m69.
    
    BUG=851045
    
    Change-Id: I44bd36789019074f422ec0c25be2afadba95b8c4
    Reviewed-on: https://chromium-review.googlesource.com/1150772
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Paul Miller <paulmiller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578136}

--- android_webview/common/aw_switches.cc
+++ android_webview/common/aw_switches.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace switches {
 
-const char kEnableWebViewVariations[] = "enable-webview-variations";
 const char kWebViewSandboxedRenderer[] = "webview-sandboxed-renderer";
 
 // used to enable safebrowsing functionality in webview
--- android_webview/common/aw_switches.h
+++ android_webview/common/aw_switches.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace switches {
 
-extern const char kEnableWebViewVariations[];
 extern const char kWebViewSandboxedRenderer[];
 extern const char kWebViewEnableSafeBrowsingSupport[];
 extern const char kWebViewDisableSafeBrowsingSupport[];
