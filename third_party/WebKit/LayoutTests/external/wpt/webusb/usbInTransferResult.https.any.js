3af55729b6949f4962a3b5375d2c0796068da5f8
Ovidio Henriquez <odejesush@chromium.org>
2018-07-26 23:17:51 +0000

    Remove Shared Worker WPT for WebUSB
    This change removes the WebUSB on Shared Worker web platform tests,
    since the feature is not yet implemented in any browser.
    
    Bug: 841510
    Change-Id: I6cd09c245edcd69799b39701b82ba1510caf845e
    Reviewed-on: https://chromium-review.googlesource.com/1151752
    Reviewed-by: Chong Zhang <chongz@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Ovidio Henriquez <odejesush@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578479}

--- third_party/WebKit/LayoutTests/external/wpt/webusb/usbInTransferResult.https.any.js
+++ third_party/WebKit/LayoutTests/external/wpt/webusb/usbInTransferResult.https.any.js
@@ -1,7 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: script=/webusb/resources/fake-devices.js
 // META: script=/webusb/resources/usb-helpers.js
-// META: global=sharedworker
 'use strict';
 
 test(t => {
--- third_party/WebKit/LayoutTests/external/wpt/webusb/usbOutTransferResult.https.any.js
+++ third_party/WebKit/LayoutTests/external/wpt/webusb/usbOutTransferResult.https.any.js
@@ -1,5 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// META: global=sharedworker
 'use strict';
 
 test(t => {
