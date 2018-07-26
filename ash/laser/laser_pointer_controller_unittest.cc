5da71290fe70defd412195eee46a8b5f2bc0e24c
Scott Violet <sky@chromium.org>
2018-07-26 21:08:49 +0000

    chromeos: makes ServerWindow install pre-target handler using kDefault
    Using kSystem means it conflicts with other system level landers, such as
    the laser pointer.
    
    BUG=867483
    TEST=covered by tests
    
    Change-Id: I40581e6ae8716e333d1e64ecd2ae8ebbcbf1936d
    Reviewed-on: https://chromium-review.googlesource.com/1151546
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578438}

--- ash/laser/laser_pointer_controller_unittest.cc
+++ ash/laser/laser_pointer_controller_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/config.h"
 #include "ash/shell.h"
 #include "ash/test/ash_test_base.h"
+#include "services/ui/ws2/server_window.h"
 #include "ui/events/test/event_generator.h"
 
 namespace ash {
@@ -153,6 +154,27 @@ TEST_F(LaserPointerControllerTest, LaserPointerPrediction) {
   EXPECT_TRUE(controller_test_api_->IsFadingAway());
   EXPECT_EQ(0,
             controller_test_api_->predicted_laser_points().GetNumberOfPoints());
+}
+
+// Test to laser pointer is shown for touches over a remote app.
+TEST_F(LaserPointerControllerTest, LaserPointerWorksWithRemoteApp) {
+  std::unique_ptr<aura::Window> window =
+      CreateTestWindow(gfx::Rect(50, 50, 100, 100));
+  ui::ws2::ServerWindow* server_window =
+      ui::ws2::ServerWindow::GetMayBeNull(window.get());
+  ASSERT_TRUE(server_window);
+  // Setting the client area triggers slightly different logic.
+  server_window->SetClientArea(gfx::Insets(10), std::vector<gfx::Rect>());
+
+  // The laser pointer mode only works with stylus.
+  ui::test::EventGenerator* event_generator = GetEventGenerator();
+  event_generator->EnterPenPointerMode();
+  controller_test_api_->SetEnabled(true);
+
+  event_generator->MoveTouch(gfx::Point(100, 100));
+  EXPECT_FALSE(controller_test_api_->IsShowingLaserPointer());
+  event_generator->PressTouch();
+  EXPECT_TRUE(controller_test_api_->IsShowingLaserPointer());
 }
 
 }  // namespace ash
