a0a80d0e24045fbb2e6f72b36fb5f5e09043b652
Scott Wu <scottwu@chromium.org>
2018-07-26 01:52:01 +0000

    Move js password manager into component folder, so that it can be shared by ios web view.
    Bug: 865114
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Iaf96f48918f1a8556e3f7de94cc2f2470034245e
    Reviewed-on: https://chromium-review.googlesource.com/1149663
    Reviewed-by: John Wu <jzw@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Hiroshi Ichikawa <ichikawa@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Scott Wu <scottwu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578165}

--- components/password_manager/ios/js_password_manager.h
+++ components/password_manager/ios/js_password_manager.h
@@ -1,10 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2012 The Chromium Authors. All rights reserved.
+// Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef IOS_CHROME_BROWSER_PASSWORDS_JS_PASSWORD_MANAGER_H_
-#define IOS_CHROME_BROWSER_PASSWORDS_JS_PASSWORD_MANAGER_H_
+#ifndef COMPONENTS_PASSWORD_MANAGER_IOS_JS_PASSWORD_MANAGER_H_
+#define COMPONENTS_PASSWORD_MANAGER_IOS_JS_PASSWORD_MANAGER_H_
 
 #include "base/ios/block_types.h"
 #import "ios/web/public/web_state/js/crw_js_injection_receiver.h"
@@ -55,4 +55,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @end
 
-#endif  // IOS_CHROME_BROWSER_PASSWORDS_JS_PASSWORD_MANAGER_H_
+#endif  // COMPONENTS_PASSWORD_MANAGER_IOS_JS_PASSWORD_MANAGER_H_
--- ios/chrome/browser/passwords/password_controller_js_unittest.mm
+++ ios/chrome/browser/passwords/password_controller_js_unittest.mm
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 
-#import "ios/chrome/browser/passwords/js_password_manager.h"
+#import "components/password_manager/ios/js_password_manager.h"
 #import "ios/web/public/test/web_js_test.h"
 #import "ios/web/public/test/web_test_with_web_state.h"
 #include "testing/gtest/include/gtest/gtest.h"
