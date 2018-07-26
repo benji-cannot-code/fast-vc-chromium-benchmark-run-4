e519d26b87308d527b3b787207ea9c27ea8b3f74
Hiroshige Hayashizaki <hiroshige@chromium.org>
2018-07-26 22:42:33 +0000

    Reject cross-origin redirects for top-level module worker scripts
    This CL rejects cross-origin redirects in top-level scripts of
    module dedicated workers in WorkerModuleScriptFetcher,
    to ensure that the response and request URLs of
    a worker top-level script are same-origin in successful cases.
    
    Bug: 861965, 861564
    Change-Id: I5969020480aeb0e1f108b0393199bb04b19419b6
    Reviewed-on: https://chromium-review.googlesource.com/1147664
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578469}

--- chrome/test/data/extensions/api_test/worker/background.js
+++ chrome/test/data/extensions/api_test/worker/background.js
@@ -102,13 +102,11 @@ chrome.test.getConfig(function(config) {
     crossOriginRedirectTest.bind(
       undefined,
       () => { return new Worker(crossOriginRedirectedWorkerUrl) }),
-    // TODO(crbug.com/861965): Forbid cross-origin redirects for module
-    // dedicated workers.
-    // crossOriginRedirectTest.bind(
-    //   undefined,
-    //   () => {
-    //     return new Worker(crossOriginRedirectedWorkerUrl, {type: 'module'});
-    //   }),
+    crossOriginRedirectTest.bind(
+      undefined,
+      () => {
+        return new Worker(crossOriginRedirectedWorkerUrl, {type: 'module'});
+      }),
     crossOriginRedirectTest.bind(
       undefined,
       () => { return new SharedWorker(crossOriginRedirectedSharedWorkerUrl) }),
