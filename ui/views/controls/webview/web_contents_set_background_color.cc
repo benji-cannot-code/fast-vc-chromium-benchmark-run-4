f5ad0dc43e97e3ac0fd56f15e6348e10759783b1
Francois Doray <fdoray@chromium.org>
2018-07-26 14:32:45 +0000

    Remove DEFINE_WEB_CONTENTS_USER_DATA_KEY in //ui.
    It is no longer necessary to invoke the DEFINE_WEB_CONTENTS_USER_DATA_KEY
    macro to define the global variable from which the key of a
    WebContentsUserData is derived. The key is now derived from a static
    local variable in WebContentsUserData::UserDataKey().
    See https://chromium-review.googlesource.com/c/chromium/src/+/1093015
    
    This CL was uploaded by git cl split.
    
    R=sadrul@chromium.org
    
    Bug: 589840
    Change-Id: I26d9d7478b46c88bde21ec9ee095cebb26c0e520
    Reviewed-on: https://chromium-review.googlesource.com/1150288
    Commit-Queue: François Doray <fdoray@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578297}

--- ui/views/controls/webview/web_contents_set_background_color.cc
+++ ui/views/controls/webview/web_contents_set_background_color.cc
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_widget_host.h"
 #include "content/public/browser/render_widget_host_view.h"
 
-DEFINE_WEB_CONTENTS_USER_DATA_KEY(views::WebContentsSetBackgroundColor);
-
 namespace views {
 
 // static
