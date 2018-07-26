97a711e50c18649464686f7d3bce18abb47e9aad
Moe Ahmadi <mahmadi@chromium.org>
2018-07-26 18:56:39 +0000

    [AF][IOS] Makes the new CC save infobar the default infobar for local save
    Bug: 841758
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I1475b5ccf301eb83cdecc0b9fbd2ba5d56092416
    Reviewed-on: https://chromium-review.googlesource.com/1151488
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Mathieu Perreault <mathp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578388}

--- ios/chrome/browser/ui/autofill/chrome_autofill_client_ios.mm
+++ ios/chrome/browser/ui/autofill/chrome_autofill_client_ios.mm
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/logging.h"
 #include "components/autofill/core/browser/autofill_credit_card_filling_infobar_delegate_mobile.h"
-#include "components/autofill/core/browser/autofill_experiments.h"
 #include "components/autofill/core/browser/autofill_save_card_infobar_delegate_mobile.h"
 #include "components/autofill/core/browser/ui/card_unmask_prompt_view.h"
 #include "components/autofill/core/browser/webdata/autofill_webdata_service.h"
@@ -45,13 +44,9 @@ namespace {
 // Creates and returns an infobar for saving credit cards.
 std::unique_ptr<infobars::InfoBar> CreateSaveCardInfoBarMobile(
     std::unique_ptr<autofill::AutofillSaveCardInfoBarDelegateMobile> delegate) {
-  if (!base::FeatureList::IsEnabled(autofill::kAutofillUpstream))
-    return ::CreateConfirmInfoBar(std::move(delegate));
-
   SaveCardInfoBarController* controller = [[SaveCardInfoBarController alloc]
       initWithInfoBarDelegate:delegate.get()];
-  auto infobar = std::make_unique<InfoBarIOS>(controller, std::move(delegate));
-  return infobar;
+  return std::make_unique<InfoBarIOS>(controller, std::move(delegate));
 }
 
 }  // namespace
