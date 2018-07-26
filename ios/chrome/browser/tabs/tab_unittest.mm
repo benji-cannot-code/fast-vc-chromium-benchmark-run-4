5fffa51f8dd013ad2e2da9ab06941cc1b1750b0c
mrefaat <mrefaat@chromium.org>
2018-07-26 21:49:57 +0000

    Remove the usage of CRWWebDelegate's openExternalURL method.
    This is the last of CRWWebDelegate methods used by CRWWebController, removing it
    will allow deleting the CRWWebDelegate entirely.
    Also removed it from Tab and from PreloadController which were implementing them.
    The logic of handling external apps is now entirely moved to AppLauncherTabHelper
    which is a policy decider and is attached to the web state used by the CRWWebController.
    
    Bug: 850760, 681867, 674991
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I2df6282ae561eef2e1da8fac605f42dbfe4cb97b
    Reviewed-on: https://chromium-review.googlesource.com/1124942
    Commit-Queue: Mohammad Refaat <mrefaat@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Danyao Wang <danyao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578447}

--- ios/chrome/browser/tabs/tab_unittest.mm
+++ ios/chrome/browser/tabs/tab_unittest.mm
@@ -69,7 +69,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #undef catch
 
 namespace {
-const char kAppSettingsUrl[] = "app-settings://";
 const char kNewTabUrl[] = "chrome://newtab/";
 const char kGoogleUserUrl[] = "http://google.com";
 const char kGoogleRedirectUrl[] = "http://www.google.fr/";
@@ -316,13 +315,6 @@ TEST_F(TabTest, AddToHistoryWithRedirect) {
   EXPECT_EQ(1U, results.size());
   CheckHistoryResult(results[0], GURL(kGoogleRedirectUrl), kGoogleTitle);
   CheckCurrentItem(results[0]);
-}
-
-TEST_F(TabTest, FailToOpenAppSettings) {
-  GURL app_settings_url = GURL(kAppSettingsUrl);
-  BOOL will_open_app_settings =
-      [tab_ openExternalURL:app_settings_url sourceURL:GURL() linkClicked:YES];
-  EXPECT_FALSE(will_open_app_settings);
 }
 
 // TODO(crbug.com/378098): Disabled because forward/back is now implemented in
