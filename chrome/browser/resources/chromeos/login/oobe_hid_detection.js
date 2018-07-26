c931a5ec4eea4d2bed854113f5b61847c227fb96
Alexander Alekseev <alemate@chromium.org>
2018-07-26 22:55:53 +0000

    Chrome OS OOBE: Update HID detection screen style.
    This CL updates HID detection screen with new style.
    
    Bug: 852046
    Change-Id: Ibba68ae848efeb7518ec1942719da666f5f92f24
    Reviewed-on: https://chromium-review.googlesource.com/1150970
    Reviewed-by: Wenzhao (Colin) Zang <wzang@chromium.org>
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578470}

--- chrome/browser/resources/chromeos/login/oobe_hid_detection.js
+++ chrome/browser/resources/chromeos/login/oobe_hid_detection.js
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 Polymer({
   is: 'oobe-hid-detection-md',
 
+  behaviors: [I18nBehavior, OobeDialogHostBehavior],
+
   properties: {
     /** "Continue" button is disabled until HID devices are paired. */
     continueButtonDisabled: {
