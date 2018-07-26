11ac502523b53e7020d8006ee76a79c66f57f481
Kyle Milka <kmilka@chromium.org>
2018-07-26 22:01:32 +0000

    [NTP] Custom backgrounds style fixes
    Add a bottom border to the title bar of the selection dialog.
    
    When an attribution contains a link change the cursor on mouse-over.
    
    Bug: 867765, 867764
    Change-Id: I1357b005154c1c3d685e584422fdaa6e424057b6
    Reviewed-on: https://chromium-review.googlesource.com/1151620
    Commit-Queue: Kyle Milka <kmilka@chromium.org>
    Reviewed-by: Mathieu Perreault <mathp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578453}

--- chrome/browser/resources/local_ntp/custom_backgrounds.js
+++ chrome/browser/resources/local_ntp/custom_backgrounds.js
@@ -174,6 +174,7 @@ customBackgrounds.setAttribution = function(
     attributionBox.onclick = function() {
       window.open(attributionActionUrl, '_blank');
     };
+    attributionBox.style.cursor = 'pointer';
   }
 };
 
