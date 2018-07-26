f01f80693e6148efae56a3b4a7acb690fbe6fbe2
Francois Doray <fdoray@chromium.org>
2018-07-26 14:32:11 +0000

    Remove DEFINE_WEB_CONTENTS_USER_DATA_KEY in //extensions.
    It is no longer necessary to invoke the DEFINE_WEB_CONTENTS_USER_DATA_KEY
    macro to define the global variable from which the key of a
    WebContentsUserData is derived. The key is now derived from a static
    local variable in WebContentsUserData::UserDataKey().
    See https://chromium-review.googlesource.com/c/chromium/src/+/1093015
    
    This CL was uploaded by git cl split.
    
    R=rdevlin.cronin@chromium.org
    
    Bug: 589840
    Change-Id: I8e7bbadce2f45929143f65aa54957a703432ed76
    Reviewed-on: https://chromium-review.googlesource.com/1150286
    Commit-Queue: François Doray <fdoray@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578296}

--- extensions/shell/browser/shell_extension_web_contents_observer.cc
+++ extensions/shell/browser/shell_extension_web_contents_observer.cc
@@ -5,9 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "extensions/shell/browser/shell_extension_web_contents_observer.h"
 
-DEFINE_WEB_CONTENTS_USER_DATA_KEY(
-    extensions::ShellExtensionWebContentsObserver);
-
 namespace extensions {
 
 ShellExtensionWebContentsObserver::ShellExtensionWebContentsObserver(
