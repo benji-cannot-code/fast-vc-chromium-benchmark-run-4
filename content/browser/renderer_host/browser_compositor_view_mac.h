bb05b5b68ed8dd1326d29117cf3272ac335fdfb0
Yoichi Osato <yoichio@chromium.org>
2018-07-26 01:55:25 +0000

    Revert "Enable FlingSchedulerMac"
    This reverts commit d6ec81545fea9d795fe5655d8453c343bc61420a.
    
    Reason for revert: Findit identified the culprit r577957 with confidence 100.0% in the config "chromium.mac / Mac10.11 Tests"
    
    Original change's description:
    > Enable FlingSchedulerMac
    > 
    > Enable tests that were disabled because of this functionality missing.
    > 
    > Bug: 842325
    > Change-Id: I6f540460f4e5f81d7ff8dd36e4abf369f0bd4785
    > Reviewed-on: https://chromium-review.googlesource.com/1149612
    > Reviewed-by: Jonathan Ross <jonross@chromium.org>
    > Commit-Queue: ccameron <ccameron@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#577957}
    
    TBR=ccameron@chromium.org,jonross@chromium.org
    
    Change-Id: I4a38151ba5333ca99fb28c00f57f93f057f5fd4b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 842325, 867668
    Reviewed-on: https://chromium-review.googlesource.com/1150946
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578166}

--- content/browser/renderer_host/browser_compositor_view_mac.h
+++ content/browser/renderer_host/browser_compositor_view_mac.h
@@ -149,7 +149,7 @@ class CONTENT_EXPORT BrowserCompositorMac : public DelegatedFrameHostClient,
 
   bool ForceNewSurfaceForTesting();
 
-  ui::Compositor* GetCompositor() const;
+  ui::Compositor* GetCompositorForTesting() const;
 
  private:
   // ui::LayerObserver implementation:
