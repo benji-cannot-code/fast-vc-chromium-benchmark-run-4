edd67765c29be21e856ead9bc5e0fc59371f0cc5
Jonathan Ross <jonross@chromium.org>
2018-07-26 17:57:19 +0000

    Early Exit Failing Mac Fling Tests on Viz
    There are two remaining Mac Fling tests which fail under Viz. Work is
    ongoing to land a fix, but there was flakiness if the resolution.
    
    This change updates the failing tests to exit early when it is Viz on
    Mac. This will unblock removing our filter files.
    
    TEST=BrowserSideFlingBrowserTest.TouchpadFling
    BrowserSideFlingBrowserTest.TouchscreenFling
    
    Bug: 842325
    Change-Id: I67e7c6165d821c9df4907ebf8803fe22fbdf1663
    Reviewed-on: https://chromium-review.googlesource.com/1151392
    Reviewed-by: ccameron <ccameron@chromium.org>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578362}

--- content/browser/renderer_host/input/fling_browsertest.cc
+++ content/browser/renderer_host/input/fling_browsertest.cc
@@ -3,7 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/feature_list.h"
 #include "build/build_config.h"
+#include "components/viz/common/features.h"
 #include "content/browser/renderer_host/input/synthetic_smooth_scroll_gesture.h"
 #include "content/browser/web_contents/web_contents_impl.h"
 #include "content/public/test/browser_test_utils.h"
@@ -108,6 +110,13 @@ class BrowserSideFlingBrowserTest : public ContentBrowserTest {
 };
 
 IN_PROC_BROWSER_TEST_F(BrowserSideFlingBrowserTest, TouchscreenFling) {
+#if defined(OS_MACOSX)
+  // TODO(jonross): Re-enable once fling on Mac works with Viz.
+  // https://crbug.com/842325
+  if (base::FeatureList::IsEnabled(features::kVizDisplayCompositor))
+    return;
+#endif  // defined(OS_MACOSX)
+
   LoadURL(kBrowserFlingDataURL);
 
   // Send a GSB to start scrolling sequence.
@@ -141,6 +150,13 @@ IN_PROC_BROWSER_TEST_F(BrowserSideFlingBrowserTest, TouchscreenFling) {
 }
 
 IN_PROC_BROWSER_TEST_F(BrowserSideFlingBrowserTest, TouchpadFling) {
+#if defined(OS_MACOSX)
+  // TODO(jonross): Re-enable once fling on Mac works with Viz.
+  // https://crbug.com/842325
+  if (base::FeatureList::IsEnabled(features::kVizDisplayCompositor))
+    return;
+#endif  // defined(OS_MACOSX)
+
   LoadURL(kBrowserFlingDataURL);
 
   // Send a wheel event to start scrolling sequence.
