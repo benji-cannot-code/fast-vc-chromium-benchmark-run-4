e394ea5fc9f4b15e774a55e04e932d74971483ac
Yoichi Osato <yoichio@chromium.org>
2018-07-26 04:46:05 +0000

    Revert "Chromad: Fix kerberos files tests"
    This reverts commit 7a20ffc4e1f5cbb57336f2c5254f61eba56c231e.
    
    Reason for revert: Still flaky
    https://chromium-try-flakes.appspot.com/all_flake_occurrences?key=ahVzfmNocm9taXVtLXRyeS1mbGFrZXNyUwsSBUZsYWtlIkhFeGlzdGluZ1VzZXJDb250cm9sbGVyQWN0aXZlRGlyZWN0b3J5VGVzdC5Qb2xpY3lDaGFuZ2VUcmlnZ2Vyc0ZpbGVVcGRhdGUM
    
    Original change's description:
    > Chromad: Fix kerberos files tests
    >
    > Modify KerberosFilesChangeWaiter to be more robust callbacks called
    > several times
    >
    > BUG=chromium:865206
    >
    > Change-Id: I0c5f22fb26871607257d3b29ad3bd88d5b505c7c
    > Reviewed-on: https://chromium-review.googlesource.com/1145074
    > Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    > Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#576941}
    
    TBR=alemate@chromium.org,rsorokin@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 865206, 867386
    Change-Id: I97bc01ed73dcd0dcc7c0fa3735f40548edc92a81
    Reviewed-on: https://chromium-review.googlesource.com/1150061
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578207}

--- chrome/browser/chromeos/login/existing_user_controller_browsertest.cc
+++ chrome/browser/chromeos/login/existing_user_controller_browsertest.cc
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include <string>
-#include <unordered_set>
 #include <vector>
 
 #include "base/barrier_closure.h"
@@ -140,6 +139,16 @@ class KerberosFilesChangeWaiter {
   // If |files_must_exist| is true and a file already exists the class does not
   // wait when it changes.
   explicit KerberosFilesChangeWaiter(bool files_must_exist) {
+    barrier_closure_ = base::BarrierClosure(2, loop_.QuitClosure());
+
+    watch_callback_ = base::BindRepeating(
+        [](const base::RepeatingClosure& barrier_closure,
+           const base::FilePath& path, bool error) -> void {
+          EXPECT_FALSE(error);
+          barrier_closure.Run();
+        },
+        barrier_closure_);
+
     config_watcher_ = std::make_unique<base::FilePathWatcher>();
     MaybeStartWatch(&config_watcher_,
                     base::FilePath(GetKerberosConfigFileName()),
@@ -149,8 +158,6 @@ class KerberosFilesChangeWaiter {
     MaybeStartWatch(&creds_watcher_,
                     base::FilePath(GetKerberosCredentialsCacheFileName()),
                     files_must_exist);
-    if (watching_paths_.empty())
-      loop_.Quit();
   }
 
   // Should be called once.
@@ -164,31 +171,19 @@ class KerberosFilesChangeWaiter {
   void MaybeStartWatch(std::unique_ptr<base::FilePathWatcher>* watcher,
                        const base::FilePath& path,
                        bool files_must_exist) {
-    (*watcher)->Watch(
-        path, false /* recursive */,
-        base::BindRepeating(&KerberosFilesChangeWaiter::WatchCallback,
-                            weak_factory_.GetWeakPtr()));
+    (*watcher)->Watch(path, false /* recursive */, watch_callback_);
     if (!files_must_exist && base::PathExists(path)) {
-      // File was written so stop the watch.
+      watch_callback_.Run(path, false /* error */);
       watcher->reset();
-      return;
     }
-    watching_paths_.insert(path.value());
   }
-
-  void WatchCallback(const base::FilePath& path, bool error) {
-    EXPECT_FALSE(error);
-    watching_paths_.erase(path.value());
-    if (watching_paths_.empty())
-      loop_.Quit();
-  }
-
   base::RunLoop loop_;
-  std::unordered_set<std::string> watching_paths_;
+  base::RepeatingClosure barrier_closure_;
+
+  base::RepeatingCallback<void(const base::FilePath& path, bool error)>
+      watch_callback_;
   std::unique_ptr<base::FilePathWatcher> config_watcher_;
   std::unique_ptr<base::FilePathWatcher> creds_watcher_;
-
-  base::WeakPtrFactory<KerberosFilesChangeWaiter> weak_factory_{this};
 };
 
 }  // namespace
@@ -983,7 +978,7 @@ IN_PROC_BROWSER_TEST_F(ExistingUserControllerActiveDirectoryTest,
 // Kerberos files.
 // Disabled due to flakiness, see https://crbug.com/865206.
 IN_PROC_BROWSER_TEST_F(ExistingUserControllerActiveDirectoryTest,
-                       PolicyChangeTriggersFileUpdate) {
+                       DISABLED_PolicyChangeTriggersFileUpdate) {
   LoginAdOnline();
 
   ApplyPolicyAndWaitFilesChanged(false /* enable_dns_cname_lookup */);
@@ -996,8 +991,9 @@ IN_PROC_BROWSER_TEST_F(ExistingUserControllerActiveDirectoryTest,
 // Tests if user Kerberos files changed D-Bus signal triggers updating user
 // Kerberos files.
 // Disabled due to flakiness, see https://crbug.com/865206.
-IN_PROC_BROWSER_TEST_F(ExistingUserControllerActiveDirectoryTest,
-                       UserKerberosFilesChangedSignalTriggersFileUpdate) {
+IN_PROC_BROWSER_TEST_F(
+    ExistingUserControllerActiveDirectoryTest,
+    DISABLED_UserKerberosFilesChangedSignalTriggersFileUpdate) {
   LoginAdOnline();
   KerberosFilesChangeWaiter files_change_waiter(true /* files_must_exist */);
   fake_authpolicy_client()->SetUserKerberosFiles("new_kerberos_creds",
