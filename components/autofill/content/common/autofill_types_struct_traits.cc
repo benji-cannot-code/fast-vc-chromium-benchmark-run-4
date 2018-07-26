9c9c4b0d50b9716a49e8ce7fe8fd84fd971d2344
Vaclav Brozek <vabr@chromium.org>
2018-07-26 20:45:52 +0000

    Remove FormFieldData::is_default
    ...and also the related *password_value_is_default fields in
    PasswordForm. These are only set and never read.
    
    Bug: 845426
    Change-Id: I87e686d2cfb588cb94a50e8a6395a7b19b7b82d5
    Reviewed-on: https://chromium-review.googlesource.com/1149878
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Vaclav Brozek <vabr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578432}

--- components/autofill/content/common/autofill_types_struct_traits.cc
+++ components/autofill/content/common/autofill_types_struct_traits.cc
@@ -615,7 +615,6 @@ bool StructTraits<
 
   out->is_enabled = data.is_enabled();
   out->is_readonly = data.is_readonly();
-  out->is_default = data.is_default();
   if (!data.ReadValue(&out->typed_value))
     return false;
 
@@ -790,13 +789,11 @@ bool StructTraits<
     return false;
 
   out->form_has_autofilled_value = data.form_has_autofilled_value();
-  out->password_value_is_default = data.password_value_is_default();
 
   if (!data.ReadNewPasswordElement(&out->new_password_element) ||
       !data.ReadNewPasswordValue(&out->new_password_value))
     return false;
 
-  out->new_password_value_is_default = data.new_password_value_is_default();
   out->new_password_marked_by_site = data.new_password_marked_by_site();
 
   if (!data.ReadConfirmationPasswordElement(
