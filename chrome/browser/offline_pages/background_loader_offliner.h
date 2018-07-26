a7c1800788fb6de93c4fd4a167252b8e1ce2666a
Pete Williamson <petewil@chromium.org>
2018-07-26 17:30:51 +0000

    Split SnapshotController into online and offline versions.
    To make the Resource Percentage work easier, we are splitting the
    snapshot controller into a foreground and a background version.
    
    Bug: 699313
    Change-Id: Ideeea7d644a903290d9eeae2d7ab700ab0ff50b6
    Reviewed-on: https://chromium-review.googlesource.com/1136023
    Reviewed-by: Dmitry Titov <dimich@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Commit-Queue: Peter Williamson <petewil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578351}

--- chrome/browser/offline_pages/background_loader_offliner.h
+++ chrome/browser/offline_pages/background_loader_offliner.h
@@ -14,8 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/offline_pages/content/background_loader/background_loader_contents.h"
 #include "components/offline_pages/core/background/load_termination_listener.h"
 #include "components/offline_pages/core/background/offliner.h"
+#include "components/offline_pages/core/background_snapshot_controller.h"
 #include "components/offline_pages/core/offline_page_types.h"
-#include "components/offline_pages/core/snapshot_controller.h"
 #include "content/public/browser/web_contents_observer.h"
 
 namespace content {
@@ -44,7 +44,7 @@ class BackgroundLoaderOffliner
     : public Offliner,
       public background_loader::BackgroundLoaderContents::Delegate,
       public content::WebContentsObserver,
-      public SnapshotController::Client,
+      public BackgroundSnapshotController::Client,
       public ResourceLoadingObserver {
  public:
   BackgroundLoaderOffliner(
@@ -79,12 +79,12 @@ class BackgroundLoaderOffliner
   void DidFinishNavigation(
       content::NavigationHandle* navigation_handle) override;
 
-  // SnapshotController::Client implementation.
+  // BackgroundSnapshotController::Client implementation.
   void StartSnapshot() override;
   void RunRenovations() override;
 
-  void SetSnapshotControllerForTest(
-      std::unique_ptr<SnapshotController> controller);
+  void SetBackgroundSnapshotControllerForTest(
+      std::unique_ptr<BackgroundSnapshotController> controller);
 
   // ResourceLoadingObserver implemenation
   void ObserveResourceLoading(ResourceLoadingObserver::ResourceDataType type,
@@ -141,7 +141,7 @@ class BackgroundLoaderOffliner
   // Tracks pending request, if any.
   std::unique_ptr<SavePageRequest> pending_request_;
   // Handles determining when a page should be snapshotted.
-  std::unique_ptr<SnapshotController> snapshot_controller_;
+  std::unique_ptr<BackgroundSnapshotController> snapshot_controller_;
   // Callback when pending request completes.
   CompletionCallback completion_callback_;
   // Callback to report progress.
--- chrome/browser/offline_pages/background_loader_offliner_unittest.cc
+++ chrome/browser/offline_pages/background_loader_offliner_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/offline_pages/background_loader_offliner.h"
 
 #include "base/bind.h"
+#include "base/command_line.h"
 #include "base/run_loop.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
@@ -34,6 +35,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/net_errors.h"
 #include "net/http/http_response_headers.h"
 #include "testing/gtest/include/gtest/gtest.h"
+
+namespace {
+char kShortSnapshotDelayForTest[] =
+    "short-offline-page-snapshot-delay-for-test";
+};  // namespace
 
 namespace offline_pages {
 
@@ -210,15 +216,12 @@ class BackgroundLoaderOfflinerTest : public testing::Test {
 
   void CompleteLoading() {
     // Reset snapshot controller.
-    std::unique_ptr<SnapshotController> snapshot_controller(
-        new SnapshotController(base::ThreadTaskRunnerHandle::Get(),
-                               offliner_.get(),
-                               0L /* DelayAfterDocumentAvailable */,
-                               0L /* DelayAfterDocumentOnLoad */,
-                               0L /* DelayAfterRenovationsCompleted */,
-                               false /* DocumentAvailableTriggersSnapshot */,
-                               false /* RenovationsEnabled */));
-    offliner_->SetSnapshotControllerForTest(std::move(snapshot_controller));
+    std::unique_ptr<BackgroundSnapshotController> snapshot_controller(
+        new BackgroundSnapshotController(base::ThreadTaskRunnerHandle::Get(),
+                                         offliner_.get(),
+                                         false /* RenovationsEnabled */));
+    offliner_->SetBackgroundSnapshotControllerForTest(
+        std::move(snapshot_controller));
     // Call complete loading.
     offliner()->DocumentOnLoadCompletedInMainFrame();
     PumpLoop();
@@ -266,6 +269,10 @@ BackgroundLoaderOfflinerTest::BackgroundLoaderOfflinerTest()
 BackgroundLoaderOfflinerTest::~BackgroundLoaderOfflinerTest() {}
 
 void BackgroundLoaderOfflinerTest::SetUp() {
+  // Set the snapshot controller delay command line switch to short delays.
+  base::CommandLine* cl = base::CommandLine::ForCurrentProcess();
+  cl->AppendSwitch(kShortSnapshotDelayForTest);
+
   std::unique_ptr<TestLoadTerminationListener> listener =
       std::make_unique<TestLoadTerminationListener>();
   load_termination_listener_ = listener.get();
