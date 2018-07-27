4e618313f92751a5cd8479d1f9e0f85535499420
Mihai Sardarescu <msarda@chromium.org>
2018-07-27 11:33:33 +0000

    Revert "[unified-consent] Add shutdown information for the UrlKeyedDataCollectionConsentHelper."
    This reverts commit bfc33b7c48a995c13ddd3bc56a2c146abb773226.
    
    Reason for revert: The work for UKM is done in a separate CL, so this is no longer needed.
    
    Original change's description:
    > [unified-consent] Add shutdown information for the UrlKeyedDataCollectionConsentHelper.
    > 
    > This CL adds methods that handle the shutdown state for the
    > UrlKeyedDataCollectionConsentHelper. This is set when the dependent sync
    > service is shut down.
    > 
    > Bug: 867388
    > 
    > Change-Id: Id6f948bc0aa1a55ec2943590a5f77cd179b65e3f
    > Reviewed-on: https://chromium-review.googlesource.com/1148564
    > Reviewed-by: Brian White <bcwhite@chromium.org>
    > Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#578242}
    
    TBR=msarda@chromium.org,bcwhite@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 867388
    Change-Id: If3978f977afcb9ea1997bdde1b7220f56a271bbb
    Reviewed-on: https://chromium-review.googlesource.com/1152847
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578598}

--- components/unified_consent/url_keyed_data_collection_consent_helper.cc
+++ components/unified_consent/url_keyed_data_collection_consent_helper.cc
@@ -27,7 +27,6 @@ class PrefBasedUrlKeyedDataCollectionConsentHelper
 
   // UrlKeyedDataCollectionConsentHelper:
   bool IsEnabled() override;
-  bool IsShutDown() override;
 
  private:
   void OnPrefChanged();
@@ -48,7 +47,6 @@ class SyncBasedUrlKeyedDataCollectionConsentHelper
 
   // UrlKeyedDataCollectionConsentHelper:
   bool IsEnabled() override;
-  bool IsShutDown() override;
 
   // syncer::SyncServiceObserver:
   void OnStateChanged(syncer::SyncService* sync) override;
@@ -76,10 +74,6 @@ PrefBasedUrlKeyedDataCollectionConsentHelper::
 bool PrefBasedUrlKeyedDataCollectionConsentHelper::IsEnabled() {
   return pref_service_->GetBoolean(
       prefs::kUrlKeyedAnonymizedDataCollectionEnabled);
-}
-
-bool PrefBasedUrlKeyedDataCollectionConsentHelper::IsShutDown() {
-  return false;
 }
 
 void PrefBasedUrlKeyedDataCollectionConsentHelper::OnPrefChanged() {
@@ -110,10 +104,6 @@ bool SyncBasedUrlKeyedDataCollectionConsentHelper::IsEnabled() {
   return sync_data_type_upload_state_ == syncer::UploadState::ACTIVE;
 }
 
-bool SyncBasedUrlKeyedDataCollectionConsentHelper::IsShutDown() {
-  return !sync_service_;
-}
-
 void SyncBasedUrlKeyedDataCollectionConsentHelper::OnStateChanged(
     syncer::SyncService* sync_service) {
   DCHECK_EQ(sync_service_, sync_service);
@@ -128,11 +118,8 @@ void SyncBasedUrlKeyedDataCollectionConsentHelper::OnStateChanged(
 void SyncBasedUrlKeyedDataCollectionConsentHelper::OnSyncShutdown(
     syncer::SyncService* sync_service) {
   DCHECK_EQ(sync_service_, sync_service);
-  DCHECK_EQ(syncer::UploadState::NOT_ACTIVE, sync_data_type_upload_state_);
-
   sync_service_->RemoveObserver(this);
   sync_service_ = nullptr;
-  FireOnShutDown();
 }
 
 }  // namespace
@@ -179,11 +166,6 @@ void UrlKeyedDataCollectionConsentHelper::RemoveObserver(Observer* observer) {
 void UrlKeyedDataCollectionConsentHelper::FireOnStateChanged() {
   for (auto& observer : observer_list_)
     observer.OnUrlKeyedDataCollectionConsentStateChanged(this);
-}
-
-void UrlKeyedDataCollectionConsentHelper::FireOnShutDown() {
-  for (auto& observer : observer_list_)
-    observer.OnUrlKeyedDataCollectionConsentHelperShutDown(this);
 }
 
 }  // namespace unified_consent
