6bd221f59b0fcc9edcdf83e090eb9dd4c7748384
Weilun Shi <sweilun@chromium.org>
2018-07-27 06:26:02 +0000

    [NTP] Adds a pill under MD icons text for themes.
    Addresses the visibility of NTP icon text by adding a white pill underneath the text. Also, because we no longer need the textshadow, we use the css function to implement the ellipsis instead of writing our own function.
    
    Before:
    https://screenshot.googleplex.com/8N6m2yLinvo.png
    https://screenshot.googleplex.com/3uCW3TsFzyN.png
    After:
    https://screenshot.googleplex.com/SbwT1GmCR5W.png
    https://screenshot.googleplex.com/xrMjBTji41t.png
    
    The setting doesn't affect the older version mv-tiles:
    https://screenshot.googleplex.com/DWtC5dUPqW5.png
    
    Bug: 850718
    Change-Id: I002bc972edf2a69d4092b37193ee2fe932ab66f2
    Reviewed-on: https://chromium-review.googlesource.com/1148891
    Reviewed-by: Mathieu Perreault <mathp@chromium.org>
    Commit-Queue: Weilun Shi <sweilun@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578549}

--- chrome/browser/resources/local_ntp/local_ntp.js
+++ chrome/browser/resources/local_ntp/local_ntp.js
@@ -341,7 +341,6 @@ function sendThemeInfoToMostVisitedIframe() {
 
   var message = {cmd: 'updateTheme'};
   message.isThemeDark = isThemeDark;
-  message.hasBackgroundImage = !!info.imageUrl;
 
   var titleColor = NTP_DESIGN.titleColor;
   if (!info.usingDefaultTheme && info.textColorRgba) {
