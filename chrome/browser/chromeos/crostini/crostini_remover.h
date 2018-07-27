39a4cea51475bf9b501fe3d2bee78b79b1e9b5e8
Nicholas Verne <nverne@chromium.org>
2018-07-27 01:18:29 +0000

    Implement CrostiniRemover without using RestartCrostini
    Bug: 866752
    Change-Id: I34056730480bb7ad52b01f6ec045ee936b5f016b
    Reviewed-on: https://chromium-review.googlesource.com/1151041
    Reviewed-by: Renée Wright <rjwright@chromium.org>
    Commit-Queue: Nicholas Verne <nverne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578504}

--- chrome/browser/chromeos/crostini/crostini_remover.h
+++ chrome/browser/chromeos/crostini/crostini_remover.h
@@ -10,8 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace crostini {
 
-class CrostiniRemover : public crostini::CrostiniManager::RestartObserver,
-                        public base::RefCountedThreadSafe<CrostiniRemover> {
+class CrostiniRemover : public base::RefCountedThreadSafe<CrostiniRemover> {
  public:
   CrostiniRemover(Profile* profile,
                   std::string vm_name,
@@ -20,22 +19,17 @@ class CrostiniRemover : public crostini::CrostiniManager::RestartObserver,
 
   void RemoveCrostini();
 
-  // crostini::CrostiniManager::RestartObserver
-  void OnComponentLoaded(crostini::ConciergeClientResult result) override {}
-  void OnConciergeStarted(crostini::ConciergeClientResult result) override;
-  void OnDiskImageCreated(crostini::ConciergeClientResult result) override {}
-  void OnVmStarted(crostini::ConciergeClientResult result) override {}
-
  private:
   friend class base::RefCountedThreadSafe<CrostiniRemover>;
-  ~CrostiniRemover() override;
-  void OnRestartCrostini(crostini::ConciergeClientResult result);
+
+  ~CrostiniRemover();
+
+  void OnComponentLoaded(bool is_successful);
+  void OnConciergeStarted(bool is_successful);
   void StopVmFinished(crostini::ConciergeClientResult result);
   void DestroyDiskImageFinished(crostini::ConciergeClientResult result);
-  void StopConciergeFinished(bool success);
+  void StopConciergeFinished(bool is_successful);
 
-  crostini::CrostiniManager::RestartId restart_id_ =
-      crostini::CrostiniManager::kUninitializedRestartId;
   Profile* profile_;
   std::string vm_name_;
   std::string container_name_;
