54b46bbf1695927b33459aa6b750aa03d489d1b2
bsheedy <bsheedy@chromium.org>
2018-07-27 19:06:09 +0000

    Enable AR instrumentation tests on Oreo
    Enables monochrome_public_test_ar_apk on Oreo Phone Tester and adds in
    the necessary changes to download ArCore and include it in the isolate.
    
    Bug: 851020
    Change-Id: Id98519c43acc3ec4ee203c78e04457d8cd6f92ad
    Reviewed-on: https://chromium-review.googlesource.com/1136028
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Reviewed-by: Ben Pastene <bpastene@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578741}

---
+++ third_party/arcore-android-sdk/test-apks/update.py
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import os
+import subprocess
+import sys
+
+THIS_DIR = os.path.abspath(os.path.dirname(__file__))
+ARCORE_DIR = os.path.abspath(os.path.join(THIS_DIR, 'arcore'))
+
+def main():
+  # TODO(https://crbug.com/853898): Rename this to DOWNLOAD_XR_TEST_APKS
+  if 'DOWNLOAD_VR_TEST_APKS' not in os.environ:
+    return 0
+  subprocess.check_call(['download_from_google_storage',
+                                '--bucket', 'chromium-ar-test-apks/arcore',
+                                '-d', ARCORE_DIR])
+  return 0
+
+if __name__ == '__main__':
+  sys.exit(main())
