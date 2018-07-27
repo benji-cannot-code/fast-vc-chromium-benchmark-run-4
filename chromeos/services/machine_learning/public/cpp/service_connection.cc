8a3aac99e81193cddaa90e1d0f428ec5542e9825
Stephen McGruer <smcgruer@chromium.org>
2018-07-27 14:29:19 +0000

    Revert "Add basic ServiceConnection unit test"
    This reverts commit 453eba239f74a28e5046925a04c2378673b762fa.
    
    Reason for revert: Failing Linux ASAN/LSAN bot: https://logs.chromium.org/logs/chromium/buildbucket/cr-buildbucket.appspot.com/8939853423518476256/+/steps/chromeos_unittests/0/logs/ServiceConnectionTest.BindModelProvider/0
    
    Original change's description:
    > Add basic ServiceConnection unit test
    > 
    > The is just testing that ServiceConnection::BindModelProvider returns
    > successfully. The Mojo invitation won't go anywhere beyond the no-op
    > FakeMachineLearningClient D-Bus client.
    > 
    > To make this test compile, I had to fix an #include from an
    > overlooked file rename in an earlier CL ...
    > 
    > Bug: 863794
    > 
    > Change-Id: Ifd31fea950145561cd5954ae16e930b801bfe49b
    > Reviewed-on: https://chromium-review.googlesource.com/1139942
    > Commit-Queue: Andrew Moylan <amoylan@chromium.org>
    > Reviewed-by: Ken Rockot <rockot@chromium.org>
    > Reviewed-by: Dan Erat <derat@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#578329}
    
    TBR=derat@chromium.org,rockot@chromium.org,amoylan@chromium.org
    
    Change-Id: I9878dee8885c78f88c0af50e70e921861ab87663
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 863794
    Reviewed-on: https://chromium-review.googlesource.com/1152371
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578632}

--- chromeos/services/machine_learning/public/cpp/service_connection.cc
+++ chromeos/services/machine_learning/public/cpp/service_connection.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/no_destructor.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/dbus/machine_learning_client.h"
-#include "chromeos/services/machine_learning/public/mojom/machine_learning_service.mojom.h"
+#include "chromeos/services/machine_learning/public/mojom/interface.mojom.h"
 #include "mojo/public/cpp/platform/platform_channel.h"
 #include "mojo/public/cpp/system/invitation.h"
 #include "third_party/cros_system_api/dbus/service_constants.h"
