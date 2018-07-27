cde17720332e372fd20933df47202cac75181cd1
Yafei Duan <romax@chromium.org>
2018-07-27 21:05:19 +0000

    [NTP tiles] Don't enable homepage tile if home page url is google.com.
    Homepage tile should not be added to Most Visited tiles if the homepage
    url is google.com.
    
    Bug: 862786
    Change-Id: Icb42ce4fb5f0c26bece80b7a7dd82bfd8526bcb2
    Reviewed-on: https://chromium-review.googlesource.com/1152298
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Yafei Duan <romax@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578788}

--- chrome/android/java/src/org/chromium/chrome/browser/suggestions/MostVisitedSitesBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/suggestions/MostVisitedSitesBridge.java
@@ -13,6 +13,7 @@ import org.chromium.chrome.browser.ntp.NewTabPage;
 import org.chromium.chrome.browser.partnercustomizations.HomepageManager;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.util.FeatureUtilities;
+import org.chromium.chrome.browser.util.UrlUtilities;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -50,6 +51,7 @@ public class MostVisitedSitesBridge
                 public boolean isHomepageTileEnabled() {
                     return HomepageManager.isHomepageEnabled()
                             && !NewTabPage.isNTPUrl(getHomepageUrl())
+                            && !UrlUtilities.nativeIsGoogleHomePageUrl(getHomepageUrl())
                             && !TextUtils.isEmpty(HomepageManager.getHomepageUri());
                 }
 
