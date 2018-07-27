76d8013122fee9b2f0aed894bb9793d1a1bfd4c4
Yeol <peary2@gmail.com>
2018-07-27 02:03:29 +0000

    Uses SetHeader instead of AddHeadersFromString, for code health.
    So it replaced AddHeadersFromString with SetHeader in the unit tests.
    
    Bug: 862175
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Change-Id: Ia29ed00a158d1dd37fd0aea28cb71b50320daa8e
    Reviewed-on: https://chromium-review.googlesource.com/1146411
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Yeol Park <peary2@gmail.com>
    Cr-Commit-Position: refs/heads/master@{#578518}

--- services/network/url_loader_unittest.cc
+++ services/network/url_loader_unittest.cc
@@ -1597,7 +1597,8 @@ TEST_F(URLLoaderTest, SSLInfoOnComplete) {
 TEST_F(URLLoaderTest, RedirectModifiedHeaders) {
   ResourceRequest request = CreateResourceRequest(
       "GET", test_server()->GetURL("/redirect307-to-echo"));
-  request.headers.AddHeadersFromString("Header1: Value1\r\nHeader2: Value2");
+  request.headers.SetHeader("Header1", "Value1");
+  request.headers.SetHeader("Header2", "Value2");
 
   base::RunLoop delete_run_loop;
   mojom::URLLoaderPtr loader;
@@ -1641,7 +1642,8 @@ TEST_F(URLLoaderTest, RedirectModifiedHeaders) {
 TEST_F(URLLoaderTest, RedirectRemoveHeader) {
   ResourceRequest request = CreateResourceRequest(
       "GET", test_server()->GetURL("/redirect307-to-echo"));
-  request.headers.AddHeadersFromString("Header1: Value1\r\nHeader2: Value2");
+  request.headers.SetHeader("Header1", "Value1");
+  request.headers.SetHeader("Header2", "Value2");
 
   base::RunLoop delete_run_loop;
   mojom::URLLoaderPtr loader;
@@ -1681,7 +1683,8 @@ TEST_F(URLLoaderTest, RedirectRemoveHeader) {
 TEST_F(URLLoaderTest, RedirectRemoveHeaderAndAddItBack) {
   ResourceRequest request = CreateResourceRequest(
       "GET", test_server()->GetURL("/redirect307-to-echo"));
-  request.headers.AddHeadersFromString("Header1: Value1\r\nHeader2: Value2");
+  request.headers.SetHeader("Header1", "Value1");
+  request.headers.SetHeader("Header2", "Value2");
 
   base::RunLoop delete_run_loop;
   mojom::URLLoaderPtr loader;
