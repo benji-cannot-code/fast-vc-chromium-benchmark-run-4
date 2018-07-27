4c434b9ce19a79002c8830163ae789ecdda3658c
Joe Mason <joenotcharles@chromium.org>
2018-07-27 19:04:43 +0000

    chrome_cleaner/os unit tests, part 2: g-r
    Remove unused chrome_cleaner::RegistryOverrideManager. All tests now use
    registry_util::RegistryOverrideManager.
    
    R=csharp
    
    Bug: 830892
    Change-Id: Ife5ed1075176436e470bac0aeb02ac271409aea5
    Reviewed-on: https://chromium-review.googlesource.com/1150681
    Reviewed-by: Chris Sharp <csharp@chromium.org>
    Commit-Queue: Joe Mason <joenotcharles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578738}

---
+++ chrome/chrome_cleaner/os/pre_fetched_paths_unittest.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/chrome_cleaner/os/pre_fetched_paths.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace chrome_cleaner {
+
+namespace {
+
+TEST(PreFetchedPathsTest, PathsArePreFetched) {
+  PreFetchedPaths* pre_fetched_paths = PreFetchedPaths::GetInstance();
+  // Note: pre_fetched_paths->Initialized() is already called in test_main.cc.
+
+  EXPECT_FALSE(pre_fetched_paths->GetExecutablePath().empty());
+  EXPECT_FALSE(pre_fetched_paths->GetProgramFilesFolder().empty());
+}
+
+}  // namespace
+
+}  // namespace chrome_cleaner
