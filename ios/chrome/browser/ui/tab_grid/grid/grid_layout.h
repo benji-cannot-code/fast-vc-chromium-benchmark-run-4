c1eed764864ff9d69fa77def83141e19b05e5430
Mark Cogan <marq@google.com>
2018-07-27 08:03:32 +0000

    [iOS] Change cell appearance when reordering tabs.
    This CL introduces an appearance change for the tab grid when tabs are being reordered. It adds a second collection view layout to make all of the non-moving cells slightly transparent, and to make the moving cell slightly larger.
    
    Bug: 804593
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I6ba416844281aa7a106552c24ef1d9c854bff567
    Reviewed-on: https://chromium-review.googlesource.com/1151297
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578562}

--- ios/chrome/browser/ui/tab_grid/grid/grid_layout.h
+++ ios/chrome/browser/ui/tab_grid/grid/grid_layout.h
@@ -14,4 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @interface GridLayout : UICollectionViewFlowLayout
 @end
 
+// A specialization of GridLayout that shows the UI in its "reordering" state,
+// with the moving cell enlarged and the non-moving cells transparent.
+@interface GridReorderingLayout : GridLayout
+@end
+
 #endif  // IOS_CHROME_BROWSER_UI_TAB_GRID_GRID_GRID_LAYOUT_H_
