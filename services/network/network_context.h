16e94f851b6e3a5091a25f3243a1e59ef8447fba
Matt Menke <mmenke@chromium.org>
2018-07-27 21:36:16 +0000

    Revert "NetworkContext: Always create the CookieStore and ChannelIDStore"
    This reverts commit 41e3c57070c2c1ad1aed15c4b0be89a8df4083f7.
    
    Reason for revert: https://crbug.com/868536 - DCHECK being hit because OSCrypt isn't set up.
    
    Original change's description:
    > NetworkContext: Always create the CookieStore and ChannelIDStore
    > 
    > This CL makes NetworkContexts create CookieStores and ChannelIDStores in
    > the hybrid URLRequestContext creation path, where the embedder starts
    > configuring a URLRequestContextBuilder, and the NetworkContext finishes
    > configuring it.
    > 
    > The reduces redundant code, lets us test the new path in production
    > before we ship the network service, and will allow Isolated App
    > URLRequestContexts to need less code when they're updated to use the
    > hybrid configuration path (Which I plan to do in a followup CL).
    > 
    > The NetworkContext constructor that takes an already-initialized
    > URLRequestContext is not modified in this CL.
    > 
    > Bug: 789644
    > Cq-Include-Trybots: luci.chromium.try:linux_mojo
    > Change-Id: Ibf40ae6497e7be06b0a666bc664ea943e145cae9
    > Reviewed-on: https://chromium-review.googlesource.com/1152083
    > Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    > Reviewed-by: Clark DuVall <cduvall@chromium.org>
    > Commit-Queue: Matt Menke <mmenke@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#578740}
    
    TBR=mmenke@chromium.org,morlovich@chromium.org,cduvall@chromium.org
    
    Change-Id: Ia9f914fac8b0fa624210363dd55333a0dfa20531
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 789644
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Reviewed-on: https://chromium-review.googlesource.com/1153232
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578808}

--- services/network/network_context.h
+++ services/network/network_context.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "mojo/public/cpp/bindings/binding.h"
 #include "mojo/public/cpp/bindings/strong_binding_set.h"
+#include "services/network/cookie_manager.h"
 #include "services/network/http_cache_data_counter.h"
 #include "services/network/http_cache_data_remover.h"
 #include "services/network/public/mojom/network_context.mojom.h"
@@ -51,7 +52,6 @@ class TreeStateTracker;
 }  // namespace certificate_transparency
 
 namespace network {
-class CookieManager;
 class ExpectCTReporter;
 class NetworkService;
 class ResourceScheduler;
@@ -240,7 +240,9 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) NetworkContext
   // On connection errors the NetworkContext destroys itself.
   void OnConnectionError();
 
-  URLRequestContextOwner MakeURLRequestContext();
+  URLRequestContextOwner MakeURLRequestContext(
+      SessionCleanupCookieStore** session_cleanup_cookie_store,
+      SessionCleanupChannelIDStore** session_cleanup_channel_id_store);
 
   NetworkService* const network_service_;
 
