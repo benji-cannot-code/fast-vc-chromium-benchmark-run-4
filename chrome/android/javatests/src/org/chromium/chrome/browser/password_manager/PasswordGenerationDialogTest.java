39b061dd71012b82f9a0664b05bca2bcd9c2b82a
Ioana Pandele <ioanap@chromium.org>
2018-07-27 15:43:43 +0000

    Remove link from password generation dialog string
    The string explaining that generated passwords are saved by the Chrome
    has changed and does not include a link anymore. This CL removes the
    link logic from the modal dialog.
    
    Bug:867572,835234
    
    Change-Id: Id984e5cebbf6bf7fc6e98006ca06b997cdf2575d
    Reviewed-on: https://chromium-review.googlesource.com/1151204
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578655}

--- chrome/android/javatests/src/org/chromium/chrome/browser/password_manager/PasswordGenerationDialogTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/password_manager/PasswordGenerationDialogTest.java
@@ -11,7 +11,6 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 
 import android.support.test.filters.SmallTest;
@@ -40,7 +39,7 @@ import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 public class PasswordGenerationDialogTest {
     private PasswordGenerationDialogCoordinator mDialog;
     private String mGeneratedPassword = "generatedpassword";
-    private String mExplanationString = "This has a link.";
+    private String mExplanationString = "Explanation string.";
 
     @Mock
     private Callback<View> mExplanationTextLinkCallback;
@@ -60,8 +59,7 @@ public class PasswordGenerationDialogTest {
         mDialog = new PasswordGenerationDialogCoordinator(mActivityTestRule.getActivity());
         ThreadUtils.runOnUiThreadBlocking(
                 () -> mDialog.showDialog(mGeneratedPassword,
-                        new PasswordGenerationDialogCoordinator.SaveExplanationText(
-                                mExplanationString, 12, 16, mExplanationTextLinkCallback),
+                        mExplanationString,
                         mOnPasswordAcceptedOrRejectedCallback));
     }
 
@@ -71,13 +69,6 @@ public class PasswordGenerationDialogTest {
         onView(withId(R.id.generated_password)).check(matches(withText(mGeneratedPassword)));
         onView(withId(R.id.generation_save_explanation))
                 .check(matches(withText(mExplanationString)));
-    }
-
-    @Test
-    @SmallTest
-    public void testSavedPasswordsLinkCallback() {
-        onView(withId(R.id.generation_save_explanation)).perform(click());
-        verify(mExplanationTextLinkCallback).onResult(any());
     }
 
     @Test
