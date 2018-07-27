8ebf1bbb130e9520ab850f67a375489028e2ae6c
Shakti Sahu <shaktisahu@chromium.org>
2018-07-27 00:00:32 +0000

    Download Home : Split ListItemViewHolder into separate classes
    Moved ListItemViewHolder into separate classes into separate package
    
    Bug: 867693
    Change-Id: Ia5aa1f131882b6c8d4d695f99db92a1a42815b1a
    Reviewed-on: https://chromium-review.googlesource.com/1150852
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578491}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/download/home/list/holder/DateViewHolder.java
@@ -0,0 +1,39 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.download.home.list.holder;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.chromium.chrome.R;
+import org.chromium.chrome.browser.download.home.list.ListItem;
+import org.chromium.chrome.browser.download.home.list.ListPropertyModel;
+import org.chromium.chrome.browser.download.home.list.UiUtils;
+
+/** A {@link RecyclerView.ViewHolder} specifically meant to display a date header. */
+public class DateViewHolder extends ListItemViewHolder {
+    /** Creates a new {@link org.chromium.chrome.browser.download.home.list.holder.DateViewHolder}
+     * instance. */
+    public static org.chromium.chrome.browser.download.home.list.holder.DateViewHolder create(
+            ViewGroup parent) {
+        View view = LayoutInflater.from(parent.getContext())
+                            .inflate(R.layout.download_manager_date_item, null);
+        return new org.chromium.chrome.browser.download.home.list.holder.DateViewHolder(view);
+    }
+
+    private DateViewHolder(View view) {
+        super(view);
+    }
+
+    // ListItemViewHolder implementation.
+    @Override
+    public void bind(ListPropertyModel properties, ListItem item) {
+        ListItem.DateListItem dateItem = (ListItem.DateListItem) item;
+        ((TextView) itemView).setText(UiUtils.dateToHeaderString(dateItem.date));
+    }
+}
---
+++ chrome/android/java/src/org/chromium/chrome/browser/download/home/list/holder/VideoViewHolder.java
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.download.home.list.holder;
+
+import android.support.v7.widget.AppCompatTextView;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.chromium.chrome.browser.download.home.list.ListItem;
+import org.chromium.chrome.browser.download.home.list.ListPropertyModel;
+
+/**
+ * A {@link RecyclerView.ViewHolder} specifically meant to display a video {@code OfflineItem}.
+ */
+public class VideoViewHolder extends ListItemViewHolder {
+    public VideoViewHolder(ViewGroup parent) {
+        super(new AppCompatTextView(parent.getContext()));
+    }
+
+    // ListItemViewHolder implementation.
+    @Override
+    public void bind(ListPropertyModel properties, ListItem item) {
+        ListItem.OfflineItemListItem offlineItem = (ListItem.OfflineItemListItem) item;
+        ((TextView) itemView).setText(offlineItem.item.title);
+    }
+}
