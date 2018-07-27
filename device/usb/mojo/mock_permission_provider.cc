10f83b324d8bd5833848b3227dd359775ed63483
Donna Wu <donna.wu@intel.com>
2018-07-27 01:32:19 +0000

    Add WebUsbDeviceManager for permission moving out of device/usb.
    This is a preparing CL for USB servicification which introduced
    WebUsbDeviceManager and its unit test. As designed, all permission
    management for USB devices will be consolidated within //chrome.
    Permission checking code in //device/usb will be moved out to this
    new class WebUsbDeviceManager.
    
    
    BUG=699790
    
    Change-Id: I60580152ec31d1962dfdce616bd5b3729a3e60cc
    Reviewed-on: https://chromium-review.googlesource.com/1121971
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Donna Wu <donna.wu@intel.com>
    Cr-Commit-Position: refs/heads/master@{#578508}

--- device/usb/mojo/mock_permission_provider.cc
+++ device/usb/mojo/mock_permission_provider.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <utility>
 
+#include "base/strings/utf_string_conversions.h"
 #include "device/usb/public/mojom/device.mojom.h"
 
 using ::testing::Return;
@@ -16,11 +17,18 @@ using ::testing::_;
 namespace device {
 namespace usb {
 
-MockPermissionProvider::MockPermissionProvider() : weak_factory_(this) {
-  ON_CALL(*this, HasDevicePermission(_)).WillByDefault(Return(true));
-}
+const char MockPermissionProvider::kRestrictedSerialNumber[] = "no_permission";
+
+MockPermissionProvider::MockPermissionProvider() : weak_factory_(this) {}
 
 MockPermissionProvider::~MockPermissionProvider() = default;
+
+bool MockPermissionProvider::HasDevicePermission(
+    scoped_refptr<const UsbDevice> device) const {
+  return device->serial_number() == base::ASCIIToUTF16(kRestrictedSerialNumber)
+             ? false
+             : true;
+}
 
 base::WeakPtr<PermissionProvider> MockPermissionProvider::GetWeakPtr() {
   return weak_factory_.GetWeakPtr();
