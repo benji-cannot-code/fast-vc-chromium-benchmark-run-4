0470ff2e64a841443f836c3693e1823134005825
Adam Rice <ricea@chromium.org>
2018-07-27 02:15:53 +0000

    Remove DOMWebSocket::SubprotocolSeperator method
    It is a static method which returns a constant ", " string, and which is
    only used inside the implementation of DOMWebSocket. Replace it with a
    compile-time constant.
    
    Change-Id: I1bb9ab48421394e6b3f759536d53a56888a3af8f
    Reviewed-on: https://chromium-review.googlesource.com/1151170
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578519}

--- third_party/blink/renderer/modules/websockets/dom_websocket.cc
+++ third_party/blink/renderer/modules/websockets/dom_websocket.cc
@@ -65,6 +65,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 static const size_t kMaxByteSizeForHistogram = 100 * 1000 * 1000;
 static const int32_t kBucketCountForMessageSizeHistogram = 50;
+static const char kWebSocketSubprotocolSeparator[] = ", ";
 
 namespace blink {
 
@@ -214,10 +215,6 @@ static String JoinStrings(const Vector<String>& strings,
 static void SetInvalidStateErrorForSendMethod(ExceptionState& exception_state) {
   exception_state.ThrowDOMException(DOMExceptionCode::kInvalidStateError,
                                     "Still in CONNECTING state.");
-}
-
-const char* DOMWebSocket::SubprotocolSeperator() {
-  return ", ";
 }
 
 DOMWebSocket::DOMWebSocket(ExecutionContext* context)
@@ -376,7 +373,7 @@ void DOMWebSocket::Connect(const String& url,
 
   String protocol_string;
   if (!protocols.IsEmpty())
-    protocol_string = JoinStrings(protocols, SubprotocolSeperator());
+    protocol_string = JoinStrings(protocols, kWebSocketSubprotocolSeparator);
 
   origin_string_ = SecurityOrigin::Create(url_)->ToString();
   channel_ = CreateChannel(GetExecutionContext(), this);
