776c01c01af3ae46ba3d0608c850a4a8fb3d30a6
Pedro Amaral <amaralp@chromium.org>
2018-07-27 21:07:27 +0000

    Moving incognito toggle button down to bottom toolbar
    Moved the incognito toggle button to the bottom toolbar and removed
    the new tab button and incognito toggle button from the top toolbar.
    
    Bug: 847639
    
    Change-Id: I1a8c081251f2058f74ec7ad9b76865758fe136e4
    Reviewed-on: https://chromium-review.googlesource.com/1152541
    Commit-Queue: Pedro Amaral <amaralp@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578791}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarButtonSlotData.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarButtonSlotData.java
@@ -6,9 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.toolbar;
 
 import android.content.Context;
+import android.support.v4.content.ContextCompat;
 import android.view.View.OnClickListener;
 
-import org.chromium.chrome.browser.widget.TintedDrawable;
+import org.chromium.chrome.R;
 import org.chromium.chrome.browser.widget.TintedImageButton;
 
 /**
@@ -19,16 +20,14 @@ class ToolbarButtonSlotData {
     public final ToolbarButtonData browsingModeButtonData;
 
     /** The button to be shown when in tab switcher mode. */
-    public final ToolbarButtonData tabSwitcherModeButtonData;
+    public ToolbarButtonData tabSwitcherModeButtonData;
 
     /**
      * @param browsingModeButton The button to be shown when in browsing mode.
      * @param tabSwitcherModeButton The button to be shown when in tab switcher mode.
      */
-    ToolbarButtonSlotData(
-            ToolbarButtonData browsingModeButton, ToolbarButtonData tabSwitcherModeButton) {
+    ToolbarButtonSlotData(ToolbarButtonData browsingModeButton) {
         browsingModeButtonData = browsingModeButton;
-        tabSwitcherModeButtonData = tabSwitcherModeButton;
     }
 
     /**
@@ -36,23 +35,27 @@ class ToolbarButtonSlotData {
      * buttons when entering or leaving tab switching mode.
      */
     static class ToolbarButtonData {
-        private final TintedDrawable mTintedDrawable;
+        private final int mDrawableResId;
+        // TODO(amaralp): Add incognito accessibility string.
         private final CharSequence mAccessibilityStringResId;
         private final OnClickListener mOnClickListener;
+        private final boolean mShouldTint;
 
         /**
-         * @param tintedDrawableResId The drawable's resource id.
+         * @param drawableResId The drawable's resource id.
          * @param accessibilityStringResId The accessibility's resource id.
          * @param onClickListener An {@link OnClickListener} that is triggered when this button is
          *                        clicked.
+         * @param shouldTint Whether the button should be tinted.
          * @param context The {@link Context} used to get the drawable and accessibility string
          *                resources.
          */
-        ToolbarButtonData(int tintedDrawableResId, int accessibilityStringResId,
-                OnClickListener onClickListener, Context context) {
-            mTintedDrawable = TintedDrawable.constructTintedDrawable(context, tintedDrawableResId);
+        ToolbarButtonData(int drawableResId, int accessibilityStringResId,
+                OnClickListener onClickListener, boolean shouldTint, Context context) {
             mAccessibilityStringResId = context.getString(accessibilityStringResId);
             mOnClickListener = onClickListener;
+            mDrawableResId = drawableResId;
+            mShouldTint = shouldTint;
         }
 
         /**
@@ -60,8 +63,14 @@ class ToolbarButtonSlotData {
          */
         void updateButton(TintedImageButton imageButton) {
             imageButton.setOnClickListener(mOnClickListener);
-            imageButton.setImageDrawable(mTintedDrawable);
+            imageButton.setImageResource(mDrawableResId);
             imageButton.setContentDescription(mAccessibilityStringResId);
+            if (mShouldTint) {
+                imageButton.setImageTintList(ContextCompat.getColorStateList(
+                        imageButton.getContext(), R.color.dark_mode_tint));
+            } else {
+                imageButton.setImageTintList(null);
+            }
         }
     }
 }
