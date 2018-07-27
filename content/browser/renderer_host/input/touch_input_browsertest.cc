ff4ce4ce324dec6e6a96573333d730ea334d8569
jonross <jonross@chromium.org>
2018-07-27 21:50:52 +0000

    Update TouchInputBrowserTest Frame Synchronizing
    The TouchInputBrowserTests were flaking across OSes.
    This was due to how it was waiting for frames. It was waiting for a
    visual properties synchronization to have been started. However, while
    this leads to a frame being generated, it does not block until that
    frame is produced.
    
    This change updates the frame syncing.
    Now it will use RenderFrameSubmisisonObserver to wait for a frame from the URL load to be generated.
    For non-Android it will then set a size, which triggers a visual properties sync. Then it will wait for a frame to be generated.
    On Android, this size is set in the initial page load, so there is no synchronizing here.
    Furthermore for Viz Hit Testing, a wait for hit test data is added.
    
    TEST=TouchInputBrowserTest
    
    Bug: 346363, 514456,855532
    Change-Id: Ifa9179af9234d98c5be5575876c061763edb9f4e
    Reviewed-on: https://chromium-review.googlesource.com/1147322
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578819}

--- content/browser/renderer_host/input/touch_input_browsertest.cc
+++ content/browser/renderer_host/input/touch_input_browsertest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/single_thread_task_runner.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "build/build_config.h"
+#include "components/viz/common/features.h"
 #include "content/browser/gpu/compositor_util.h"
 #include "content/browser/renderer_host/render_widget_host_impl.h"
 #include "content/browser/web_contents/web_contents_impl.h"
@@ -23,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/content_browser_test.h"
 #include "content/public/test/content_browser_test_utils.h"
+#include "content/public/test/hit_test_region_observer.h"
 #include "content/shell/browser/shell.h"
 #include "third_party/blink/public/platform/web_input_event.h"
 #include "ui/latency/latency_info.h"
@@ -30,13 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using blink::WebInputEvent;
 
 namespace {
-
-void GiveItSomeTime() {
-  base::RunLoop run_loop;
-  base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
-      FROM_HERE, run_loop.QuitClosure(), base::TimeDelta::FromMilliseconds(10));
-  run_loop.Run();
-}
 
 const char kTouchEventDataURL[] =
     "data:text/html;charset=utf-8,"
@@ -114,11 +109,23 @@ class TouchInputBrowserTest : public ContentBrowserTest {
     NavigateToURL(shell(), data_url);
 
     RenderWidgetHostImpl* host = GetWidgetHost();
-    host->GetView()->SetSize(gfx::Size(400, 400));
+    // Wait to confirm a frame was generated from the navigation.
+    RenderFrameSubmissionObserver frame_observer(
+        host->render_frame_metadata_provider());
+    frame_observer.WaitForMetadataChange();
 
-    // The page is loaded in the renderer, wait for a new frame to arrive.
-    while (!host->RequestRepaintForTesting())
-      GiveItSomeTime();
+#if !defined(OS_ANDROID)
+    // On non-Android, set a size for the view, and wait for a new frame to be
+    // generated at that size. On Android the size is specified in
+    // kTouchEventDataURL.
+    host->GetView()->SetSize(gfx::Size(400, 400));
+    frame_observer.WaitForAnyFrameSubmission();
+#endif
+
+    if (features::IsVizHitTestingEnabled()) {
+      HitTestRegionObserver observer(host->GetFrameSinkId());
+      observer.WaitForHitTestData();
+    }
   }
 
   void SetUpCommandLine(base::CommandLine* cmd) override {
@@ -127,13 +134,7 @@ class TouchInputBrowserTest : public ContentBrowserTest {
   }
 };
 
-#if defined(OS_MACOSX)
-// TODO(ccameron): Failing on mac: crbug.com/346363
-#define MAYBE_TouchNoHandler DISABLED_TouchNoHandler
-#else
-#define MAYBE_TouchNoHandler TouchNoHandler
-#endif
-IN_PROC_BROWSER_TEST_F(TouchInputBrowserTest, MAYBE_TouchNoHandler) {
+IN_PROC_BROWSER_TEST_F(TouchInputBrowserTest, TouchNoHandler) {
   LoadURL();
   SyntheticWebTouchEvent touch;
 
@@ -151,13 +152,7 @@ IN_PROC_BROWSER_TEST_F(TouchInputBrowserTest, MAYBE_TouchNoHandler) {
   SendTouchEvent(&touch);
 }
 
-#if defined(OS_CHROMEOS)
-// crbug.com/514456
-#define MAYBE_TouchHandlerNoConsume DISABLED_TouchHandlerNoConsume
-#else
-#define MAYBE_TouchHandlerNoConsume TouchHandlerNoConsume
-#endif
-IN_PROC_BROWSER_TEST_F(TouchInputBrowserTest, MAYBE_TouchHandlerNoConsume) {
+IN_PROC_BROWSER_TEST_F(TouchInputBrowserTest, TouchHandlerNoConsume) {
   LoadURL();
   SyntheticWebTouchEvent touch;
 
@@ -174,13 +169,7 @@ IN_PROC_BROWSER_TEST_F(TouchInputBrowserTest, MAYBE_TouchHandlerNoConsume) {
   filter->WaitForAck();
 }
 
-#if defined(OS_CHROMEOS)
-// crbug.com/514456
-#define MAYBE_TouchHandlerConsume DISABLED_TouchHandlerConsume
-#else
-#define MAYBE_TouchHandlerConsume TouchHandlerConsume
-#endif
-IN_PROC_BROWSER_TEST_F(TouchInputBrowserTest, MAYBE_TouchHandlerConsume) {
+IN_PROC_BROWSER_TEST_F(TouchInputBrowserTest, TouchHandlerConsume) {
   LoadURL();
   SyntheticWebTouchEvent touch;
 
@@ -197,16 +186,7 @@ IN_PROC_BROWSER_TEST_F(TouchInputBrowserTest, MAYBE_TouchHandlerConsume) {
   filter->WaitForAck();
 }
 
-#if defined(OS_CHROMEOS)
-// crbug.com/514456
-#define MAYBE_MultiPointTouchPress DISABLED_MultiPointTouchPress
-#elif defined(OS_MACOSX)
-// TODO(ccameron): Failing on mac: crbug.com/346363
-#define MAYBE_MultiPointTouchPress DISABLED_MultiPointTouchPress
-#else
-#define MAYBE_MultiPointTouchPress MultiPointTouchPress
-#endif
-IN_PROC_BROWSER_TEST_F(TouchInputBrowserTest, MAYBE_MultiPointTouchPress) {
+IN_PROC_BROWSER_TEST_F(TouchInputBrowserTest, MultiPointTouchPress) {
   LoadURL();
   SyntheticWebTouchEvent touch;
 
