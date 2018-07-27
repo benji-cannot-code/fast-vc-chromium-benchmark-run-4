0eee5b5773041be47c6b7f16689b05a99e1d5ba1
Hiroshige Hayashizaki <hiroshige@chromium.org>
2018-07-27 20:59:53 +0000

    Use response URL for classic shared worker global scope's URL
    Bug: 861564
    Change-Id: Ia41cf740fc405269307a8c92c85d8aaf4b54c9d1
    Reviewed-on: https://chromium-review.googlesource.com/1128541
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578778}

--- chrome/test/data/extensions/api_test/worker/background.js
+++ chrome/test/data/extensions/api_test/worker/background.js
@@ -97,7 +97,7 @@ chrome.test.getConfig(function(config) {
     sameOriginRedirectTest.bind(
       undefined,
       () => { return new SharedWorker(redirectedSharedWorkerUrl) },
-      redirectedSharedWorkerUrl),
+      sharedWorkerUrl),
 
     crossOriginRedirectTest.bind(
       undefined,
