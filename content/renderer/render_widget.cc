3dcda123c5e185620a4388fbc96676e1283a34a1
Yiming Zhou <uwyiming@google.com>
2018-07-27 21:18:55 +0000

    Remove queuing the DidCommitAndDrawCompositorFrame message.
    This change removes queuing the DidCommitAndDrawCompositorFrame message
    when Chrome commits a paint update. Queuing the message causes an
    additional blink update, resulting in a serious performance regression.
    
    The original intent of queuing the DidCommitAndDrawCompositorFrame
    message is to notify the WebContents when Chrome performs a paint, which
    in turn helps Chrome's experimental capture sites automation test framework.
    Since this performance regression is particularly large, and the absence
    of this event only impacts the test framework, we decide to remove
    queuing the message for now.
    
    Bug: 866981
    Change-Id: I20e6fa6dbe1fd30f1529d6dda20e6208ae719bd1
    Reviewed-on: https://chromium-review.googlesource.com/1153594
    Reviewed-by: Fady Samuel <fsamuel@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578800}

--- content/renderer/render_widget.cc
+++ content/renderer/render_widget.cc
@@ -1097,9 +1097,6 @@ void RenderWidget::DidCommitAndDrawCompositorFrame() {
 
   // Notify subclasses that we initiated the paint operation.
   DidInitiatePaint();
-
-  QueueMessage(new ViewHostMsg_DidCommitAndDrawCompositorFrame(routing_id_),
-               MESSAGE_DELIVERY_POLICY_WITH_VISUAL_STATE);
 }
 
 void RenderWidget::DidCommitCompositorFrame() {
