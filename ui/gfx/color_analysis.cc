e803246b6c2098b0d5cf8ddbc0fae7ede3456ef8
Peter Kasting <pkasting@chromium.org>
2018-07-27 22:02:51 +0000

    Compute colors for the frame and tabs from provided images.
    If theme authors explicitly provide values here, we'll use them; but if they
    don't, but do provide images, set the colors to be the dominant colors of the
    images, using an existing K-means algorithm (similar to what we use for
    computing representative favicon colors etc.).
    
    This allows us to rely on those colors later when deciding what color to make
    tab text, the new tab button, and similar tabstrip/window frame items.
    
    This also does a bit of cleanup to the theme pack code, e.g. moving a map used
    in only one function into that function to make its provenance clearer.  I can
    try to split this apart into more CLs if desired.
    
    Bug: 862664
    Change-Id: I8c3f15893ad491a6fee8aa5f76ae162cecb2c6fd
    Reviewed-on: https://chromium-review.googlesource.com/1152517
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578823}

--- ui/gfx/color_analysis.cc
+++ ui/gfx/color_analysis.cc
@@ -544,7 +544,8 @@ SkColor CalculateKMeanColorOfBuffer(uint8_t* decoded_data,
                                     int img_height,
                                     const HSL& lower_bound,
                                     const HSL& upper_bound,
-                                    KMeanImageSampler* sampler) {
+                                    KMeanImageSampler* sampler,
+                                    bool find_closest) {
   SkColor color = kDefaultBgColor;
   if (img_width > 0 && img_height > 0) {
     std::vector<KMeanCluster> clusters;
@@ -676,9 +677,11 @@ SkColor CalculateKMeanColorOfBuffer(uint8_t* decoded_data,
     }
   }
 
-  // Find a color that actually appears in the image (the K-mean cluster center
-  // will not usually be a color that appears in the image).
-  return FindClosestColor(decoded_data, img_width, img_height, color);
+  // The K-mean cluster center will not usually be a color that appears in the
+  // image.  If desired, find a color that actually appears.
+  return find_closest
+             ? FindClosestColor(decoded_data, img_width, img_height, color)
+             : color;
 }
 
 SkColor CalculateKMeanColorOfPNG(scoped_refptr<base::RefCountedMemory> png,
@@ -691,18 +694,11 @@ SkColor CalculateKMeanColorOfPNG(scoped_refptr<base::RefCountedMemory> png,
   SkColor color = kDefaultBgColor;
 
   if (png.get() && png->size() &&
-      gfx::PNGCodec::Decode(png->front(),
-                            png->size(),
-                            gfx::PNGCodec::FORMAT_BGRA,
-                            &decoded_data,
-                            &img_width,
-                            &img_height)) {
-    return CalculateKMeanColorOfBuffer(&decoded_data[0],
-                                       img_width,
-                                       img_height,
-                                       lower_bound,
-                                       upper_bound,
-                                       sampler);
+      gfx::PNGCodec::Decode(png->front(), png->size(),
+                            gfx::PNGCodec::FORMAT_BGRA, &decoded_data,
+                            &img_width, &img_height)) {
+    return CalculateKMeanColorOfBuffer(&decoded_data[0], img_width, img_height,
+                                       lower_bound, upper_bound, sampler, true);
   }
   return color;
 }
@@ -714,25 +710,27 @@ SkColor CalculateKMeanColorOfPNG(scoped_refptr<base::RefCountedMemory> png) {
 }
 
 SkColor CalculateKMeanColorOfBitmap(const SkBitmap& bitmap,
+                                    int height,
                                     const HSL& lower_bound,
                                     const HSL& upper_bound,
-                                    KMeanImageSampler* sampler) {
+                                    bool find_closest) {
   // SkBitmap uses pre-multiplied alpha but the KMean clustering function
   // above uses non-pre-multiplied alpha. Transform the bitmap before we
   // analyze it because the function reads each pixel multiple times.
-  int pixel_count = bitmap.width() * bitmap.height();
+  int pixel_count = bitmap.width() * height;
   std::unique_ptr<uint32_t[]> image(new uint32_t[pixel_count]);
   UnPreMultiply(bitmap, image.get(), pixel_count);
 
+  GridSampler sampler;
   return CalculateKMeanColorOfBuffer(reinterpret_cast<uint8_t*>(image.get()),
-                                     bitmap.width(), bitmap.height(),
-                                     lower_bound, upper_bound, sampler);
+                                     bitmap.width(), height, lower_bound,
+                                     upper_bound, &sampler, find_closest);
 }
 
 SkColor CalculateKMeanColorOfBitmap(const SkBitmap& bitmap) {
-  GridSampler sampler;
   return CalculateKMeanColorOfBitmap(
-      bitmap, kDefaultLowerHSLBound, kDefaultUpperHSLBound, &sampler);
+      bitmap, bitmap.height(), kDefaultLowerHSLBound, kDefaultUpperHSLBound,
+      true);
 }
 
 std::vector<SkColor> CalculateProminentColorsOfBitmap(
