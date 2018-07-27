84ffcc0829d9dd5c86531236da779492a6719697
Wez <wez@chromium.org>
2018-07-27 08:21:32 +0000

    Roll Fuchsia SDK from c4a06fe7a901 to d9fde06fec56
    The AutoRoll server is located here: https://fuchsia-sdk-chromium-roll.skia.org
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+/master/autoroll/README.md
    
    If the roll is causing failures, please contact the current sheriff, who should
    be CC'd on the roll, and stop the roller if necessary.
    
    
    CQ_INCLUDE_TRYBOTS=luci.chromium.try:fuchsia_arm64_cast_audio;luci.chromium.try:fuchsia_x64_cast_audio
    TBR=cr-fuchsia+bot@chromium.org, fdegans, sergeyu
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I6499e133cd71503143426575ead473bb973be2ea
    Reviewed-on: https://chromium-review.googlesource.com/1151096
    Commit-Queue: Wez <wez@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: Fuchsia SDK Autoroller <fuchsia-sdk-chromium-autoroll@skia-buildbots.google.com.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#578565}

--- base/fuchsia/service_directory_test_base.h
+++ base/fuchsia/service_directory_test_base.h
@@ -10,14 +10,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/fuchsia/component_context.h"
 #include "base/fuchsia/scoped_service_binding.h"
-#include "base/fuchsia/test_fidl/cpp/fidl.h"
+#include "base/fuchsia/testfidl/cpp/fidl.h"
 #include "base/message_loop/message_loop.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace base {
 namespace fuchsia {
 
-class TestInterfaceImpl : public test_fidl::TestInterface {
+class TestInterfaceImpl : public testfidl::TestInterface {
  public:
   TestInterfaceImpl();
   ~TestInterfaceImpl() override;
@@ -32,7 +32,7 @@ class ServiceDirectoryTestBase : public testing::Test {
   ~ServiceDirectoryTestBase() override;
 
   void ConnectClientContextToDirectory(const char* path);
-  void VerifyTestInterface(fidl::InterfacePtr<test_fidl::TestInterface>* stub,
+  void VerifyTestInterface(fidl::InterfacePtr<testfidl::TestInterface>* stub,
                            bool expect_error);
 
  protected:
@@ -40,7 +40,7 @@ class ServiceDirectoryTestBase : public testing::Test {
   std::unique_ptr<ServiceDirectory> service_directory_;
   zx::channel service_directory_client_channel_;
   TestInterfaceImpl test_service_;
-  std::unique_ptr<ScopedServiceBinding<test_fidl::TestInterface>>
+  std::unique_ptr<ScopedServiceBinding<testfidl::TestInterface>>
       service_binding_;
   std::unique_ptr<ComponentContext> client_context_;
 };
