26509714c98bf152ffa302653f156664dcbc32b3
Avi Drissman <avi@chromium.org>
2018-07-27 19:44:37 +0000

    Mac: turn popups into new tabs while in fullscreen.
    It's platform convention to show popups as new tabs while in
    non-HTML5 fullscreen. (Popups cause tabs to lose HTML5 fullscreen.)
    
    This was implemented for Cocoa in a BrowserWindow override, but
    it makes sense to just stick it into Browser and remove a ton
    of override code put in just to support this.
    
    BUG=858929, 868416
    TEST=as in bugs
    
    Change-Id: I43471f242813ec1159d9c690bab73dab3e610b7d
    Reviewed-on: https://chromium-review.googlesource.com/1153455
    Reviewed-by: Sidney San Martín <sdy@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578755}

--- chrome/browser/ui/browser_window.h
+++ chrome/browser/ui/browser_window.h
@@ -60,7 +60,6 @@ class Extension;
 }
 
 namespace gfx {
-class Rect;
 class Size;
 }
 
@@ -323,10 +322,6 @@ class BrowserWindow : public ui::BaseWindow {
 
   // Clipboard commands applied to the whole browser window.
   virtual void CutCopyPaste(int command_id) = 0;
-
-  // Return the correct disposition for a popup window based on |bounds|.
-  virtual WindowOpenDisposition GetDispositionForPopupBounds(
-      const gfx::Rect& bounds) = 0;
 
   // Construct a FindBar implementation for the |browser|.
   virtual FindBar* CreateFindBar() = 0;
--- chrome/test/base/test_browser_window.cc
+++ chrome/test/base/test_browser_window.cc
@@ -181,11 +181,6 @@ DownloadShelf* TestBrowserWindow::GetDownloadShelf() {
   return &download_shelf_;
 }
 
-WindowOpenDisposition TestBrowserWindow::GetDispositionForPopupBounds(
-    const gfx::Rect& bounds) {
-  return WindowOpenDisposition::NEW_POPUP;
-}
-
 FindBar* TestBrowserWindow::CreateFindBar() {
   return NULL;
 }
