0c135baee795cf91a4cfc6461683c190621df0ee
Eric Karl <ericrk@chromium.org>
2018-07-28 03:14:22 +0000

    Revert "cc: Remove glFinish call during cc shutdown."
    This reverts commit b148b4788bc90804d3848878db99aff8167c5096.
    
    This change was causing hangs on Android. Was able to repro locally
    and bisect with high confidence. I agree that this logically should
    be removable, but for some reason isn't. We should re-visit in a
    future milestone.
    
    TBR=danakj,piman
    
    Bug: 846709
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I1029fa57bc3bfb919121535fc2b70f6884e88a15
    Reviewed-on: https://chromium-review.googlesource.com/1154198
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Commit-Queue: Eric Karl <ericrk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578909}

--- cc/trees/layer_tree_host_impl.cc
+++ cc/trees/layer_tree_host_impl.cc
@@ -3103,6 +3103,11 @@ void LayerTreeHostImpl::ReleaseLayerTreeFrameSink() {
   resource_pool_ = nullptr;
   ClearUIResources();
 
+  if (layer_tree_frame_sink_->context_provider()) {
+    auto* gl = layer_tree_frame_sink_->context_provider()->ContextGL();
+    gl->Finish();
+  }
+
   // Release any context visibility before we destroy the LayerTreeFrameSink.
   SetContextVisibility(false);
 
