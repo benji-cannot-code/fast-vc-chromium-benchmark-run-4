8728822373c016bd04f6ae6e127214c9ba3335ba
Christian Fremerey <chfremer@chromium.org>
2018-07-28 16:09:19 +0000

    [Video Capture, Tests] Consolidate Mock implementations of VideoCaptureDevice::Client interface
    There are several redundant mock implementations of interface
    VideoCaptureDevice::Client scattered across the test code. This CL consolidates
    them into a single implementation that is reused where needed.
    
    This CL is part of a series, see Design Doc at
    https://docs.google.com/document/d/1ihGDZloUGdDpZ5XfmiI3AcqsSxOP9kOe5GxXOTqpHW4/edit?usp=sharing
    
    Bug: 768887
    Change-Id: Iaec91f3ad4956eeb6a265631bf9029c2baeaf7fe
    Reviewed-on: https://chromium-review.googlesource.com/1124972
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Reviewed-by: Emircan Uysaler <emircan@chromium.org>
    Commit-Queue: Christian Fremerey <chfremer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578916}

---
+++ media/capture/video/mock_video_capture_device_client.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "media/capture/video/mock_video_capture_device_client.h"
+
+namespace media {
+
+MockVideoCaptureDeviceClient::MockVideoCaptureDeviceClient() = default;
+MockVideoCaptureDeviceClient::~MockVideoCaptureDeviceClient() = default;
+
+void MockVideoCaptureDeviceClient::OnIncomingCapturedBuffer(
+    Buffer buffer,
+    const media::VideoCaptureFormat& format,
+    base::TimeTicks reference_time,
+    base::TimeDelta timestamp) {
+  DoOnIncomingCapturedBuffer(buffer, format, reference_time, timestamp);
+}
+void MockVideoCaptureDeviceClient::OnIncomingCapturedBufferExt(
+    Buffer buffer,
+    const media::VideoCaptureFormat& format,
+    base::TimeTicks reference_time,
+    base::TimeDelta timestamp,
+    gfx::Rect visible_rect,
+    const media::VideoFrameMetadata& additional_metadata) {
+  DoOnIncomingCapturedBufferExt(buffer, format, reference_time, timestamp,
+                                visible_rect, additional_metadata);
+}
+
+}  // namespace media
