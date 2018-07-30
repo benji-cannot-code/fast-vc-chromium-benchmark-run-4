e059271a67c9de1c420253093a8dabccfa877a87
Han Leon <leon.han@intel.com>
2018-07-30 04:42:59 +0000

    [OnionSoup] Remove typemapping for blink.mojom.FetchAPIResponse
    This CL is the 1st step of the plan below aiming to eliminate the native
    struct content::ServiceWorkerResponse defined in
    content/common/service_worker/service_worker_types.h.
    
    1st step:
    - Remove the typemapping of blink.mojom.FetchAPIResponse.
      The mojom struct blink.mojom.FetchAPIResponse describes a Response in
      terms of the concept from the Fetch spec, currently it's defined under
      blink/public/platform/modules/fetch/ and is used only by Cache Storage
      API implementation (by both Browser and Renderer code), as this struct
      meets the 2 conditions below, it should be fine to go into
      blink/public/mojom/:
         - it is expected to be used by both Browser and Renderer code.
         - it is expected to be passed over Mojo between Browser and
           Renderer (Blink).
      But, currently for Chromium Mojo binding variant it is typemapped to
      content::ServiceWorkerResponse being used in
      content/browser/cache_storage/, this blocks it being moved to
      blink/public/mojom/. So we need to remove this typemapping first, this
      removal also helps reduce 1 user of content::ServiceWorkerResponse.
    
    2nd step:
      Now we can just move blink.mojom.FetchAPIResponse into
      blink/public/mojom/fetch/, which makes the next steps possible.
    
    3rd step:
    - Use blink.mojom.FetchAPIResponse to represent the response a service
      worker provides to FetchEvent#respondWith. Specifically, use this
      mojom struct to replace the existing [Native] mojom struct
      content.mojom.ServiceWorkerResponse in
      content/common/service_worker/service_worker_fetch_response_callback.mojom.
      Thus we can reduce another 1 user of content::ServiceWorkerResponse.
      More importantly, this makes it possible for us to move
      service_worker_fetch_response_callback.mojom into
      blink/public/mojom/service_worker/ in future.
    
    4th step:
    - Use blink.mojom.FetchAPIResponse to represent responses to background
      fetches. Specifically, use this mojom struct to replace the existing
      [Native] mojom struct content.mojom.ServiceWorkerResponse defined in
      content/common/service_worker/service_worker.mojom.
      Thus we can reduce the last 1 user of content::ServiceWorkerResponse.
    
    5th step:
    - Remove content::ServiceWorkerResponse and do any left cleanup.
    
    BUG=789854
    
    Change-Id: Ibae19bcac50688360426ef8b736a3841ca59ff39
    Reviewed-on: https://chromium-review.googlesource.com/1134731
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Commit-Queue: Han Leon <leon.han@intel.com>
    Cr-Commit-Position: refs/heads/master@{#578974}

--- content/browser/background_fetch/background_fetch_data_manager_unittest.cc
+++ content/browser/background_fetch/background_fetch_data_manager_unittest.cc
@@ -604,7 +604,7 @@ class BackgroundFetchDataManagerTest
   void DidMatchCache(base::OnceClosure quit_closure,
                      bool* out_result,
                      blink::mojom::CacheStorageError error,
-                     std::unique_ptr<ServiceWorkerResponse> response) {
+                     blink::mojom::FetchAPIResponsePtr response) {
     if (error == blink::mojom::CacheStorageError::kSuccess) {
       DCHECK(response);
       *out_result = true;
--- content/browser/cache_storage/cache_storage.h
+++ content/browser/cache_storage/cache_storage.h
@@ -122,7 +122,7 @@ class CONTENT_EXPORT CacheStorage : public CacheStorageCacheObserver {
   // Puts the request/response pair in the cache.
   void WriteToCache(const std::string& cache_name,
                     std::unique_ptr<ServiceWorkerFetchRequest> request,
-                    std::unique_ptr<ServiceWorkerResponse> response,
+                    blink::mojom::FetchAPIResponsePtr response,
                     CacheStorage::ErrorCallback callback);
 
   // Sums the sizes of each cache and closes them. Runs |callback| with the
@@ -215,18 +215,17 @@ class CONTENT_EXPORT CacheStorage : public CacheStorageCacheObserver {
   void MatchCacheDidMatch(CacheStorageCacheHandle cache_handle,
                           CacheStorageCache::ResponseCallback callback,
                           blink::mojom::CacheStorageError error,
-                          std::unique_ptr<ServiceWorkerResponse> response);
+                          blink::mojom::FetchAPIResponsePtr response);
 
   // The MatchAllCaches callbacks are below.
   void MatchAllCachesImpl(std::unique_ptr<ServiceWorkerFetchRequest> request,
                           blink::mojom::QueryParamsPtr match_params,
                           CacheStorageCache::ResponseCallback callback);
-  void MatchAllCachesDidMatch(
-      CacheStorageCacheHandle cache_handle,
-      CacheMatchResponse* out_match_response,
-      const base::RepeatingClosure& barrier_closure,
-      blink::mojom::CacheStorageError error,
-      std::unique_ptr<ServiceWorkerResponse> service_worker_response);
+  void MatchAllCachesDidMatch(CacheStorageCacheHandle cache_handle,
+                              CacheMatchResponse* out_match_response,
+                              const base::RepeatingClosure& barrier_closure,
+                              blink::mojom::CacheStorageError error,
+                              blink::mojom::FetchAPIResponsePtr response);
   void MatchAllCachesDidMatchAll(
       std::unique_ptr<std::vector<CacheMatchResponse>> match_responses,
       CacheStorageCache::ResponseCallback callback);
@@ -234,7 +233,7 @@ class CONTENT_EXPORT CacheStorage : public CacheStorageCacheObserver {
   // WriteToCache callbacks.
   void WriteToCacheImpl(const std::string& cache_name,
                         std::unique_ptr<ServiceWorkerFetchRequest> request,
-                        std::unique_ptr<ServiceWorkerResponse> response,
+                        blink::mojom::FetchAPIResponsePtr response,
                         CacheStorage::ErrorCallback callback);
 
   void GetSizeThenCloseAllCachesImpl(SizeCallback callback);
