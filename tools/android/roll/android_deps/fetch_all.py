a77f477b3bc4a510e1132d1394512202ecd912da
Peter Wen <wnwen@chromium.org>
2018-07-30 19:43:11 +0000

    Android: Fix fetch_all.py's CIPD commands
    Print CIPD commands that avoid creating new ones if existing CIPD
    packages are found with the same tags, display those with
    `cipd describe` instead.
    
    Bug: 867454
    Change-Id: I9cccdcf55d5ddb02a98f4dd8d530d9240618b817
    Reviewed-on: https://chromium-review.googlesource.com/1155480
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579126}

--- tools/android/roll/android_deps/fetch_all.py
+++ tools/android/roll/android_deps/fetch_all.py
@@ -312,9 +312,13 @@ def GenerateCipdUploadCommand(cipd_pkg_info):
   Returns:
     A string holding a shell command to upload the package through cipd.
   """
-  pkg_path, _, pkg_tag = cipd_pkg_info
-  return '(cd %s; cipd create --pkg-def cipd.yaml -tag %s)' % (
-      pkg_path, pkg_tag)
+  pkg_path, pkg_name, pkg_tag = cipd_pkg_info
+  return ('(cd {0}; '
+          # Need to skip create step if an instance already exists with the
+          # same package name and version tag (thus the use of ||).
+          'cipd describe {1} -version {2} || '
+          'cipd create --pkg-def cipd.yaml -tag {2})').format(
+              pkg_path, pkg_name, pkg_tag)
 
 
 def main():
@@ -498,6 +502,7 @@ def main():
 
     if cipd_packages_to_upload:
       print 'Run the following to upload new and updated CIPD packages:'
+      print 'Note: Duplicate instances with the same tag will break the build.'
       print '------------------------ cut here -----------------------------'
       print '\n'.join(cipd_commands)
       print '------------------------ cut here -----------------------------'
