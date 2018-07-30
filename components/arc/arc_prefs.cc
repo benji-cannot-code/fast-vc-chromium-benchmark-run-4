459ae4541f51198c4dcf88afea94aa8fc737a666
phweiss <phweiss@google.com>
2018-07-30 16:56:42 +0000

    Create new mojo API for Always-on VPN feature
    To expand the Always-on VPN feature on Android to apply
    to chrome, we need to know what VPN package to apply it to.
    Then we store that information in a pref in chrome, so
    that it is available after restart, even before ARC++
    has started.
    
    BUG=b:72861260
    TEST=unit tests
    
    Change-Id: I2096acb14f74316186a83e76547bd6e9d0c116b7
    Reviewed-on: https://chromium-review.googlesource.com/1052507
    Reviewed-by: Mattias Nissler <mnissler@chromium.org>
    Reviewed-by: Luis Hector Chavez <lhchavez@chromium.org>
    Commit-Queue: Philipp Weiß <phweiss@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579070}

--- components/arc/arc_prefs.cc
+++ components/arc/arc_prefs.cc
@@ -13,6 +13,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace arc {
 namespace prefs {
 
+// A bool preference indicating whether traffic other than the VPN connection
+// set via kAlwaysOnVpnPackage should be blackholed.
+const char kAlwaysOnVpnLockdown[] = "arc.vpn.always_on.lockdown";
+// A string preference indicating the Android app that will be used for
+// "Always On VPN". Should be empty if "Always On VPN" is not enabled.
+const char kAlwaysOnVpnPackage[] = "arc.vpn.always_on.vpn_package";
 // Stores the user id received from DM Server when enrolling a Play user on an
 // Active Directory managed device. Used to report to DM Server that the account
 // is still used.
@@ -128,6 +134,8 @@ void RegisterProfilePrefs(PrefRegistrySimple* registry) {
   // Sorted in lexicographical order.
   registry->RegisterBooleanPref(kVoiceInteractionActivityControlAccepted,
                                 false);
+  registry->RegisterBooleanPref(kAlwaysOnVpnLockdown, false);
+  registry->RegisterStringPref(kAlwaysOnVpnPackage, std::string());
   registry->RegisterBooleanPref(kArcDataRemoveRequested, false);
   registry->RegisterBooleanPref(kArcEnabled, false);
   registry->RegisterBooleanPref(kArcInitialSettingsPending, false);
--- components/arc/net/arc_net_host_impl.cc
+++ components/arc/net/arc_net_host_impl.cc
@@ -29,6 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/arc/arc_bridge_service.h"
 #include "components/arc/arc_browser_context_keyed_service_factory_base.h"
 #include "components/arc/arc_features.h"
+#include "components/arc/arc_prefs.h"
+#include "components/prefs/pref_service.h"
 #include "components/user_manager/user_manager.h"
 #include "third_party/cros_system_api/dbus/shill/dbus-constants.h"
 
@@ -360,6 +362,12 @@ ArcNetHostImpl* ArcNetHostImpl::GetForBrowserContext(
   return ArcNetHostImplFactory::GetForBrowserContext(context);
 }
 
+// static
+ArcNetHostImpl* ArcNetHostImpl::GetForBrowserContextForTesting(
+    content::BrowserContext* context) {
+  return ArcNetHostImplFactory::GetForBrowserContextForTesting(context);
+}
+
 ArcNetHostImpl::ArcNetHostImpl(content::BrowserContext* context,
                                ArcBridgeService* bridge_service)
     : arc_bridge_service_(bridge_service), weak_factory_(this) {
@@ -375,6 +383,10 @@ ArcNetHostImpl::~ArcNetHostImpl() {
   }
   arc_bridge_service_->net()->RemoveObserver(this);
   arc_bridge_service_->net()->SetHost(nullptr);
+}
+
+void ArcNetHostImpl::SetPrefService(PrefService* pref_service) {
+  pref_service_ = pref_service;
 }
 
 void ArcNetHostImpl::OnConnectionReady() {
@@ -929,6 +941,14 @@ void ArcNetHostImpl::AndroidVpnStateChanged(mojom::ConnectionStateType state) {
   GetNetworkConnectionHandler()->DisconnectNetwork(
       service_path, base::Bind(&ArcVpnSuccessCallback),
       base::Bind(&ArcVpnErrorCallback));
+}
+
+void ArcNetHostImpl::SetAlwaysOnVpn(const std::string& vpn_package,
+                                    bool lockdown) {
+  // pref_service_ should be set by ArcServiceLauncher.
+  DCHECK(pref_service_);
+  pref_service_->SetString(prefs::kAlwaysOnVpnPackage, vpn_package);
+  pref_service_->SetBoolean(prefs::kAlwaysOnVpnLockdown, lockdown);
 }
 
 void ArcNetHostImpl::DisconnectArcVpn() {
