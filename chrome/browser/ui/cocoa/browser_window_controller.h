ac8bfa1e93097b555affcc3b71738e9c518a5101
Elly Fong-Jones <ellyjones@chromium.org>
2018-07-30 13:59:26 +0000

    macviews: delete Cocoa bookmark secondary UI
    This UI was only reachable in !mac_views_browser (last shipped in M66) with
    !ShowAllDialogsUsingToolkitViews (last shipped in M65).
    
    Bug: 832676
    Change-Id: I3097f0de0b09c28e72c5e3a4c0977739cb0da2cc
    Reviewed-on: https://chromium-review.googlesource.com/1151655
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579032}

--- chrome/browser/ui/cocoa/browser_window_controller.h
+++ chrome/browser/ui/cocoa/browser_window_controller.h
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/browser_extension_window_controller.h"
 #include "chrome/browser/translate/chrome_translate_client.h"
 #import "chrome/browser/ui/cocoa/bookmarks/bookmark_bar_controller.h"
-#import "chrome/browser/ui/cocoa/bookmarks/bookmark_bubble_controller.h"
 #import "chrome/browser/ui/cocoa/tabs/tab_strip_controller.h"
 #import "chrome/browser/ui/cocoa/tabs/tab_window_controller.h"
 #import "chrome/browser/ui/cocoa/themed_window.h"
@@ -105,7 +104,6 @@ constexpr const gfx::Size kMinCocoaPopupWindowSize(100, 122);
   StatusBubbleMac* statusBubble_;
 
   std::unique_ptr<BookmarkBubbleObserverCocoa> bookmarkBubbleObserver_;
-  BookmarkBubbleController* bookmarkBubbleController_;  // Weak.
   BOOL initializing_;  // YES while we are currently in initWithBrowser:
   BOOL ownsBrowser_;  // Only ever NO when testing
 
