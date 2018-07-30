46b710a39fe4273329b7aa61d32aada50b984e13
Parastoo Geranmayeh <parastoog@google.com>
2018-07-30 15:11:58 +0000

    [Autofill] Keeping the user edit.
    Problem:
    When we Autofill, to decide if we should fill a field or leave it as
    is, we check if the last edit was user's edit. If so, we would not
    autofill (clear, etc). But sometimes, the website writes on the top
    of the autofilled value (exp: formatting a phone number). So, if the
    user adds a value on the field, the JS would edit it, and it would be
    overwritten by autofill.
    Solution:
    Instead of looking at the last edit, look if the user has
    ever edited that field.
    
    Bug: 860542, 867556
    Change-Id: I59ec9c1efab98a9a07f23a47a0354e04ba9b87aa
    Reviewed-on: https://chromium-review.googlesource.com/1150683
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Parastoo Geranmayeh <parastoog@google.com>
    Cr-Commit-Position: refs/heads/master@{#579038}

--- third_party/blink/renderer/core/html/forms/text_control_element.cc
+++ third_party/blink/renderer/core/html/forms/text_control_element.cc
@@ -69,6 +69,7 @@ TextControlElement::TextControlElement(const QualifiedName& tag_name,
                                        Document& doc)
     : HTMLFormControlElementWithState(tag_name, doc),
       last_change_was_user_edit_(false),
+      user_has_edited_the_field_(false),
       cached_selection_start_(0),
       cached_selection_end_(0) {
   cached_selection_direction_ =
@@ -108,6 +109,7 @@ void TextControlElement::DefaultEventHandler(Event* event) {
   if (event->type() == EventTypeNames::webkitEditableContentChanged &&
       GetLayoutObject() && GetLayoutObject()->IsTextControl()) {
     last_change_was_user_edit_ = !GetDocument().IsRunningExecCommand();
+    user_has_edited_the_field_ |= last_change_was_user_edit_;
 
     if (IsFocused()) {
       // Updating the cache in SelectionChanged() isn't enough because
@@ -752,6 +754,12 @@ void TextControlElement::ParseAttribute(
   }
 }
 
+bool TextControlElement::UserHasEditedTheField() const {
+  if (!IsTextControl())
+    return false;
+  return user_has_edited_the_field_;
+}
+
 bool TextControlElement::LastChangeWasUserEdit() const {
   if (!IsTextControl())
     return false;
@@ -1006,6 +1014,7 @@ void TextControlElement::CloneNonAttributePropertiesFrom(
   const TextControlElement& source_element =
       static_cast<const TextControlElement&>(source);
   last_change_was_user_edit_ = source_element.last_change_was_user_edit_;
+  user_has_edited_the_field_ = source_element.user_has_edited_the_field_;
   HTMLFormControlElement::CloneNonAttributePropertiesFrom(source, flag);
 }
 
