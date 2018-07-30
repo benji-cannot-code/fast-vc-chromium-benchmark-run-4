da777b0e1d8fe8ac4ea8ae0add9e96f03641d94c
Yao Xiao <yaoxia@chromium.org>
2018-07-30 19:09:05 +0000

    Enable UKM metrics for kFullscreenSecureOrigin/kFullscreenInsecureOrigin
    (FullscreenSecureOrigin + FullscreenInsecureOrigin) / PageVisits = 1.78%
    
    Bug: 864196
    Change-Id: I88e313288e5d960ba8bc93cef4b45b8a2e6af8ac
    Reviewed-on: https://chromium-review.googlesource.com/1151652
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Bryan McQuade <bmcquade@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Commit-Queue: Yao Xiao <yaoxia@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579111}

--- chrome/browser/page_load_metrics/page_load_metrics_browsertest.cc
+++ chrome/browser/page_load_metrics/page_load_metrics_browsertest.cc
@@ -384,6 +384,12 @@ class PageLoadMetricsBrowserTest : public InProcessBrowserTest {
     ui_test_utils::NavigateToURL(browser(), GURL(url::kAboutBlankURL));
   }
 
+  void MakeComponentFullscreen(const std::string& id) {
+    EXPECT_TRUE(content::ExecuteScript(
+        browser()->tab_strip_model()->GetActiveWebContents(),
+        "document.getElementById(\"" + id + "\").webkitRequestFullscreen();"));
+  }
+
   std::string GetRecordedPageLoadMetricNames() {
     auto entries = histogram_tester_.GetTotalCountsForPrefix("PageLoad.");
     std::vector<std::string> names;
@@ -1359,6 +1365,7 @@ IN_PROC_BROWSER_TEST_F(PageLoadMetricsBrowserTest,
       browser(),
       embedded_test_server()->GetURL(
           "non-secure.test", "/page_load_metrics/use_counter_features.html"));
+  MakeComponentFullscreen("testvideo");
   waiter->Wait();
   NavigateToUntrackedUrl();
 
@@ -1382,6 +1389,9 @@ IN_PROC_BROWSER_TEST_F(PageLoadMetricsBrowserTest,
   histogram_tester_.ExpectBucketCount(
       internal::kFeaturesHistogramName,
       static_cast<int32_t>(WebFeature::kPageVisits), 1);
+  histogram_tester_.ExpectBucketCount(
+      internal::kFeaturesHistogramName,
+      static_cast<int32_t>(WebFeature::kFullscreenInsecureOrigin), 1);
 }
 
 // Test UseCounter UKM features observed.
@@ -1394,12 +1404,13 @@ IN_PROC_BROWSER_TEST_F(PageLoadMetricsBrowserTest,
   GURL url = embedded_test_server()->GetURL(
       "/page_load_metrics/use_counter_features.html");
   ui_test_utils::NavigateToURL(browser(), url);
+  MakeComponentFullscreen("testvideo");
   waiter->Wait();
   NavigateToUntrackedUrl();
 
   const auto& entries = test_ukm_recorder_->GetEntriesByName(
       ukm::builders::Blink_UseCounter::kEntryName);
-  EXPECT_EQ(3u, entries.size());
+  EXPECT_EQ(4u, entries.size());
   std::vector<int64_t> ukm_features;
   for (const auto* entry : entries) {
     test_ukm_recorder_->ExpectEntrySourceHasUrl(entry, url);
@@ -1411,6 +1422,7 @@ IN_PROC_BROWSER_TEST_F(PageLoadMetricsBrowserTest,
   EXPECT_THAT(
       ukm_features,
       UnorderedElementsAre(
+          static_cast<int64_t>(WebFeature::kFullscreenSecureOrigin),
           static_cast<int64_t>(WebFeature::kNavigatorVibrate),
           static_cast<int64_t>(WebFeature::kDataUriHasOctothorpe),
           static_cast<int64_t>(
@@ -1432,12 +1444,13 @@ IN_PROC_BROWSER_TEST_F(PageLoadMetricsBrowserTest,
   GURL url =
       https_server.GetURL("/page_load_metrics/use_counter_features.html");
   ui_test_utils::NavigateToURL(browser(), url);
+  MakeComponentFullscreen("testvideo");
   waiter->Wait();
   NavigateToUntrackedUrl();
 
   const auto& entries = test_ukm_recorder_->GetEntriesByName(
       ukm::builders::Blink_UseCounter::kEntryName);
-  EXPECT_EQ(6u, entries.size());
+  EXPECT_EQ(7u, entries.size());
   std::vector<int64_t> ukm_features;
   for (const auto* entry : entries) {
     test_ukm_recorder_->ExpectEntrySourceHasUrl(entry, url);
@@ -1448,6 +1461,7 @@ IN_PROC_BROWSER_TEST_F(PageLoadMetricsBrowserTest,
   }
   EXPECT_THAT(ukm_features,
               UnorderedElementsAre(
+                  static_cast<int64_t>(WebFeature::kFullscreenSecureOrigin),
                   static_cast<int64_t>(WebFeature::kNavigatorVibrate),
                   static_cast<int64_t>(WebFeature::kDataUriHasOctothorpe),
                   static_cast<int64_t>(
