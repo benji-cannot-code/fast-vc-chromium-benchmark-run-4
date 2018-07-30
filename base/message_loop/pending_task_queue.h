2044372da90a35f4eff96fd807d59d1c6ef8d972
Gabriel Charette <gab@chromium.org>
2018-07-30 19:12:43 +0000

    [MessageLoop] Remove left behind TODO
    Oops hadn't saved file locally before "git cl upload -c" for last patch
    set @ https://chromium-review.googlesource.com/c/chromium/src/+/1153458
    
    TBR=kylechar@chromium.org
    
    Bug: None
    Change-Id: I4466d287a80f4472b61da2017c28c01ff31c4dbb
    Reviewed-on: https://chromium-review.googlesource.com/1155250
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579113}

--- base/message_loop/pending_task_queue.h
+++ base/message_loop/pending_task_queue.h
@@ -15,8 +15,6 @@ namespace internal {
 
 // Provides storage for tasks deferred by MessageLoop via DelayedQueue and
 // DeferredQueue.
-// TODO(gab): Rename to PendingTaskQueue after landing
-// https://crrev.com/1088762.
 class PendingTaskQueue {
  public:
   // Provides a read-write task queue.
