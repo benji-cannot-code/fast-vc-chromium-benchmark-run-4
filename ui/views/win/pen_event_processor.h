3e2a0963ab3479f8d0e40076fd273fc25db61aca
Takuto Ikuta <tikuta@chromium.org>
2018-07-30 22:17:20 +0000

    Fix to remove /wd4018 (-Wno-sign-compare) for /ui/views
    This is a part of effort to remove /wd4018 warning suppression.
    
    Master CL is
    https://chromium-review.googlesource.com/c/chromium/src/+/1152755
    
    This CL was uploaded by git cl split.
    
    R=sky@chromium.org
    
    Bug: 868248, 588506
    Change-Id: Ibeb235f73e433aa9bc34dbdb7513582cf11427c8
    Reviewed-on: https://chromium-review.googlesource.com/1152844
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579196}

--- ui/views/win/pen_event_processor.h
+++ ui/views/win/pen_event_processor.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/optional.h"
 #include "ui/events/event.h"
 #include "ui/gfx/geometry/point.h"
 #include "ui/gfx/sequential_id_generator.h"
@@ -55,7 +56,7 @@ class VIEWS_EXPORT PenEventProcessor {
   bool send_touch_for_pen_ = false;
   bool sent_mouse_down_ = false;
   bool sent_touch_start_ = false;
-  int eraser_pointer_id_ = -1;
+  base::Optional<unsigned int> eraser_pointer_id_;
 
   DISALLOW_COPY_AND_ASSIGN(PenEventProcessor);
 };
