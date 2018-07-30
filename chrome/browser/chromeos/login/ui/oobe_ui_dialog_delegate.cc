c80fd7d0e2fcf9f291e335406628ecbdef3096d9
Sarah Hu <xiaoyinh@chromium.org>
2018-07-30 23:20:43 +0000

    Revert "cros: Update visibility of guest and apps button in the shelf."
    This reverts commit fa0ab3e4d807f864ff1b808a2f27ed58370eeae9.
    
    Add user button is not showing up correctly due to this CL, revert it now
    and will investigate the cause.
    
    TBR=tsepez@chromium.org,msw@chromium.org
    
    Change-Id: I2ab8ccd357f2ef27d3f9d1d53733cad45e65b6c8
    Reviewed-on: https://chromium-review.googlesource.com/1155757
    Commit-Queue: Xiaoyin Hu <xiaoyinh@chromium.org>
    Reviewed-by: Jacob Dufault <jdufault@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579228}

--- chrome/browser/chromeos/login/ui/oobe_ui_dialog_delegate.cc
+++ chrome/browser/chromeos/login/ui/oobe_ui_dialog_delegate.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/ui/oobe_ui_dialog_delegate.h"
 
 #include "ash/public/cpp/shell_window_ids.h"
-#include "ash/public/interfaces/login_screen.mojom.h"
 #include "chrome/browser/chromeos/login/screens/gaia_view.h"
 #include "chrome/browser/chromeos/login/ui/login_display_host_mojo.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
@@ -96,8 +95,7 @@ content::WebContents* OobeUIDialogDelegate::GetWebContents() {
 }
 
 void OobeUIDialogDelegate::Show() {
-  LoginScreenClient::Get()->login_screen()->NotifyOobeDialogState(
-      ash::mojom::OobeDialogState::GAIA_SIGNIN);
+  LoginScreenClient::Get()->login_screen()->NotifyOobeDialogVisibility(true);
   dialog_widget_->Show();
 }
 
@@ -114,16 +112,14 @@ void OobeUIDialogDelegate::ShowFullScreen() {
 void OobeUIDialogDelegate::Hide() {
   if (!dialog_widget_)
     return;
-  LoginScreenClient::Get()->login_screen()->NotifyOobeDialogState(
-      ash::mojom::OobeDialogState::HIDDEN);
+  LoginScreenClient::Get()->login_screen()->NotifyOobeDialogVisibility(false);
   dialog_widget_->Hide();
 }
 
 void OobeUIDialogDelegate::Close() {
   if (!dialog_widget_)
     return;
-  LoginScreenClient::Get()->login_screen()->NotifyOobeDialogState(
-      ash::mojom::OobeDialogState::HIDDEN);
+  LoginScreenClient::Get()->login_screen()->NotifyOobeDialogVisibility(false);
   dialog_widget_->Close();
 }
 
