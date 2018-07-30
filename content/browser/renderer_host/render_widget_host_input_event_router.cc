9eff2394f06a631d0ed89b975869a59fe0f744d9
Xida Chen <xidachen@chromium.org>
2018-07-30 17:11:43 +0000

    Reland of: Handle iframe touch ack time out
    The previous CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/1102729
    was reverted. The reason is that in the test we have two gesture scroll,
    and the second gesture starts before the first TapDown finishes.
    
    This CL solve the problem. PS#1 is exactly the same as the previous CL
    which was reverted, so that it is easier for review.
    
    TBR=creis@chromium.org
    
    Bug: 851644
    Change-Id: Ife9bf8f9a37892380e6587b80bcc03366cc0e80d
    Reviewed-on: https://chromium-review.googlesource.com/1151094
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579074}

--- content/browser/renderer_host/render_widget_host_input_event_router.cc
+++ content/browser/renderer_host/render_widget_host_input_event_router.cc
@@ -1198,6 +1198,11 @@ void RenderWidgetHostInputEventRouter::DispatchTouchscreenGestureEvent(
       map_size_key,
       base::StringPrintf("%u", static_cast<int>(owner_map_.size())));
 
+  if (events_being_flushed_) {
+    touchscreen_gesture_target_.target->host()
+        ->input_router()
+        ->ForceSetTouchActionAuto();
+  }
   touchscreen_gesture_target_.target->ProcessGestureEvent(event, latency);
 
   if (gesture_event.GetType() == blink::WebInputEvent::kGestureFlingStart)
@@ -1367,6 +1372,11 @@ RenderWidgetHostInputEventRouter::FindTargetSynchronously(
   }
   NOTREACHED();
   return RenderWidgetTargetResult();
+}
+
+void RenderWidgetHostInputEventRouter::SetEventsBeingFlushed(
+        bool events_being_flushed) {
+  events_being_flushed_ = events_being_flushed;
 }
 
 void RenderWidgetHostInputEventRouter::DispatchEventToTarget(
