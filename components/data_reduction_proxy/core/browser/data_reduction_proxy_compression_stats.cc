dc3fb3146e1ecdaf4026afbeda7c97686ae60b45
rajendrant <rajendrant@chromium.org>
2018-07-30 22:56:46 +0000

    Disable weekly aggregate metrics in x86 android
    Crashes are observed in X86 Android with jellybean. So disabling those
    metrics until rootcause is analyzed.
    
    Bug: 865373
    Change-Id: I4d72bf8fc47fd917091b8b8b4e0e4dee5dca662c
    Reviewed-on: https://chromium-review.googlesource.com/1155788
    Reviewed-by: Doug Arnett <dougarnett@chromium.org>
    Commit-Queue: rajendrant <rajendrant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579215}

--- components/data_reduction_proxy/core/browser/data_reduction_proxy_compression_stats.cc
+++ components/data_reduction_proxy/core/browser/data_reduction_proxy_compression_stats.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/trace_event/trace_event.h"
 #include "base/values.h"
+#include "build/build_config.h"
 #include "components/data_reduction_proxy/core/browser/data_reduction_proxy_metrics.h"
 #include "components/data_reduction_proxy/core/browser/data_reduction_proxy_service.h"
 #include "components/data_reduction_proxy/core/browser/data_usage_store.h"
@@ -1399,6 +1400,12 @@ void DataReductionProxyCompressionStats::OnDataUsageReportingPrefChanged() {
 
 void DataReductionProxyCompressionStats::InitializeWeeklyAggregateDataUse(
     const base::Time& now) {
+#if defined(OS_ANDROID) && defined(ARCH_CPU_X86)
+  // TODO(rajendrant): Enable aggregate metrics recording in x86 Android.
+  // http://crbug.com/865373
+  return;
+#endif
+
   MaybeInitWeeklyAggregateDataUsePrefs(now, pref_service_);
   // Record the histograms that will show up in the user feedback.
   RecordDictionaryToHistogram(
@@ -1435,6 +1442,11 @@ void DataReductionProxyCompressionStats::RecordWeeklyAggregateDataUse(
     bool is_user_request,
     data_use_measurement::DataUseUserData::DataUseContentType content_type,
     int32_t service_hash_code) {
+#if defined(OS_ANDROID) && defined(ARCH_CPU_X86)
+  // TODO(rajendrant): Enable aggregate metrics recording in x86 Android.
+  // http://crbug.com/865373
+  return;
+#endif
   // Update the prefs if this is a new week. This can happen when chrome is open
   // for weeks without being closed.
   MaybeInitWeeklyAggregateDataUsePrefs(now, pref_service_);
