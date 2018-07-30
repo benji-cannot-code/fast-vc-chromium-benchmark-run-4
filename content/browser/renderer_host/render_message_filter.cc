b15e2bdad1f0e7a14f00beab5f6ee6cbf1b1f9f5
Mythri Alle <mythria@chromium.org>
2018-07-30 15:04:42 +0000

    Add support to store generated code in isolated code caches.
    We want to decouple the code cache from the HttpCache and
    recently added a separate code cache [1]. This cl is a follow up
    that actually writes the generated code into the code cache.
    There will be a followup cl that also fetches from the code cache.
    
    [1] chromium-review.googlesource.com/c/chromium/src/+/1120179
    
    Bug: chromium:812168
    Change-Id: I2866f8689b83ccba13ea8e0bcf7c60558997380d
    Reviewed-on: https://chromium-review.googlesource.com/1150237
    Commit-Queue: Mythri Alle <mythria@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579037}

--- content/browser/renderer_host/render_message_filter.cc
+++ content/browser/renderer_host/render_message_filter.cc
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/content_browser_client.h"
 #include "content/public/browser/resource_context.h"
+#include "content/public/common/content_features.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/common/context_menu_params.h"
 #include "content/public/common/url_constants.h"
@@ -215,22 +216,46 @@ void RenderMessageFilter::DidGenerateCacheableMetadata(
     return;
   }
 
-  net::HttpCache* cache = request_context_->GetURLRequestContext()->
-      http_transaction_factory()->GetCache();
-  if (!cache)
-    return;
+  DCHECK_CURRENTLY_ON(BrowserThread::IO);
 
-  // Use the same priority for the metadata write as for script
-  // resources (see defaultPriorityForResourceType() in WebKit's
-  // CachedResource.cpp). Note that WebURLRequest::PriorityMedium
-  // corresponds to net::LOW (see ConvertWebKitPriorityToNetPriority()
-  // in weburlloader_impl.cc).
-  const net::RequestPriority kPriority = net::LOW;
-  scoped_refptr<net::IOBuffer> buf(new net::IOBuffer(data.size()));
-  if (!data.empty())
-    memcpy(buf->data(), &data.front(), data.size());
-  cache->WriteMetadata(url, kPriority, expected_response_time, buf.get(),
-                       data.size());
+  if (!base::FeatureList::IsEnabled(features::kIsolatedCodeCache)) {
+    net::HttpCache* cache = request_context_->GetURLRequestContext()
+                                ->http_transaction_factory()
+                                ->GetCache();
+    if (!cache)
+      return;
+
+    // Use the same priority for the metadata write as for script
+    // resources (see defaultPriorityForResourceType() in WebKit's
+    // CachedResource.cpp). Note that WebURLRequest::PriorityMedium
+    // corresponds to net::LOW (see ConvertWebKitPriorityToNetPriority()
+    // in weburlloader_impl.cc).
+    const net::RequestPriority kPriority = net::LOW;
+    scoped_refptr<net::IOBuffer> buf(new net::IOBuffer(data.size()));
+
+    if (!data.empty())
+      memcpy(buf->data(), &data.front(), data.size());
+    cache->WriteMetadata(url, kPriority, expected_response_time, buf.get(),
+                         data.size());
+  } else {
+    GURL requesting_url =
+        ChildProcessSecurityPolicyImpl::GetInstance()->GetOriginLock(
+            render_process_id_);
+    // If the origin_url isn't valid then render process might not be locked to
+    // an origin yet so we don't allow any writes to the disk.
+    if (!requesting_url.is_valid() || !url.is_valid())
+      return;
+    url::Origin requesting_origin = url::Origin::Create(requesting_url);
+
+    // Don't cache the code corresponding to unique origins. The same-origin
+    // checks should always fail for unique origins but the serialized value of
+    // unique origins does not ensure this.
+    if (requesting_origin.unique())
+      return;
+
+    generated_code_cache_context_->generated_code_cache()->WriteData(
+        url, requesting_origin, expected_response_time, data);
+  }
 }
 
 void RenderMessageFilter::FetchCachedCode(const GURL& url,
