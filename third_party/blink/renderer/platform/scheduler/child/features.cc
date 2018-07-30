b1a33d94d82c91596caed3b2fdff3f7fdda90e3d
Scott Haseley <shaseley@google.com>
2018-07-30 20:00:48 +0000

    [scheduler] Add ThrottleAndFreezeTaskTypes experiment
    Add support for setting the throttleable and freezable task types
    through field trial parameters.
    
    BUG: 859963
    Change-Id: Icca5df00fd7a918e52f9c4c7e08b0a85515ddbb2
    Reviewed-on: https://chromium-review.googlesource.com/1143891
    Commit-Queue: Scott Haseley <shaseley@google.com>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579133}

---
+++ third_party/blink/renderer/platform/scheduler/child/features.cc
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/platform/scheduler/child/features.h"
+
+namespace blink {
+namespace scheduler {
+
+// Field trial parameters associated with |kThrottleAndFreezeTaskTypes| feature.
+// These override the throttleable and freezable QueueTraits bits for the tasks
+// specified in the parameters. The parameters are comma separated lists, of the
+// the form: "throttleable": "name1,name2", "freezable": "name1,name3". The
+// names should be those returned by TaskTypeNames::TaskTypeToString().
+const char kThrottleableTaskTypesListParam[] = "ThrottleableTasks";
+const char kFreezableTaskTypesListParam[] = "FreezableTasks";
+
+}  // namespace scheduler
+}  // namespace blink
