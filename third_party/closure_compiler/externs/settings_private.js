99ca9e482106a6e9c62cd9e944e17bd6cb89cd45
Katie D <katie@chromium.org>
2018-07-30 21:54:40 +0000

    Makes pageId optional in settings_private setPref function.
    Has to make callbacks optional too in order to continue to pass closure
    compilation.
    
    Bug: 866161
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: Ie911461b0e575771708897bca30483fa9cc9f038
    Reviewed-on: https://chromium-review.googlesource.com/1155643
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579184}

--- third_party/closure_compiler/externs/settings_private.js
+++ third_party/closure_compiler/externs/settings_private.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2016 The Chromium Authors. All rights reserved.
+// Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -64,11 +64,11 @@ chrome.settingsPrivate.Enforcement = {
 chrome.settingsPrivate.PrefObject;
 
 /**
- * Sets a settings value.
+ * Sets a pref value.
  * @param {string} name The name of the pref.
  * @param {*} value The new value of the pref.
- * @param {string} pageId The user metrics identifier or null.
- * @param {function(boolean):void} callback The callback for whether the pref
+ * @param {string=} pageId An optional user metrics identifier.
+ * @param {function(boolean):void=} callback The callback for whether the pref
  *     was set or not.
  */
 chrome.settingsPrivate.setPref = function(name, value, pageId, callback) {};
