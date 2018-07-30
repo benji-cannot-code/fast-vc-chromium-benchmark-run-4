95d6334187eb2d1d4fff14cc6ab525ec75dafd66
Manuel Rego Casasnovas <rego@igalia.com>
2018-07-30 11:40:28 +0000

    [css-grid] Add deprecation message for the change on percentage rows
    We're planning to update the behavior of percentage row tracks
    and gutters when the grid container has an indefinite size
    to match the last changes on the spec.
    
    In the intent to implement and ship thread [1] it was agreed
    to first add a deprecation message to notify web developers
    about the upcoming change.
    
    BUG=846187
    
    [1] https://groups.google.com/a/chromium.org/d/msg/blink-dev/CJgcT4hR7Rk/58WfZNbWBQAJ
    
    Change-Id: I5ca013615522d431c81bd96b6d12f61dd1846070
    Reviewed-on: https://chromium-review.googlesource.com/1154531
    Commit-Queue: Manuel Rego <rego@igalia.com>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579014}

--- third_party/blink/renderer/core/layout/grid_track_sizing_algorithm.cc
+++ third_party/blink/renderer/core/layout/grid_track_sizing_algorithm.cc
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/layout/grid_track_sizing_algorithm.h"
 
-#include "third_party/blink/renderer/core/frame/use_counter.h"
+#include "third_party/blink/renderer/core/frame/deprecation.h"
 #include "third_party/blink/renderer/core/layout/grid.h"
 #include "third_party/blink/renderer/core/layout/grid_layout_utils.h"
 #include "third_party/blink/renderer/core/layout/layout_grid.h"
@@ -828,8 +828,9 @@ GridTrackSize GridTrackSizingAlgorithm::GetGridTrackSize(
   // values are treated as <auto>.
   if (IsRelativeSizedTrackAsAuto(track_size, direction)) {
     if (direction == kForRows) {
-      UseCounter::Count(layout_grid_->GetDocument(),
-                        WebFeature::kGridRowTrackPercentIndefiniteHeight);
+      Deprecation::CountDeprecation(
+          layout_grid_->GetDocument(),
+          WebFeature::kGridRowTrackPercentIndefiniteHeight);
     }
     if (min_track_breadth.HasPercentage())
       min_track_breadth = Length(kAuto);
