f1426bdc27bab35130e9c71f304b0cd85ef9c2ed
Tanja Gornak <melandory@chromium.org>
2018-07-30 12:13:59 +0000

    Invalidation Service.
    The service to substitute TiclInvalidationService.
    
    Bug: 801985
    Change-Id: I675954b439b14ee8ccd2360fe042f31d8526bd1d
    Reviewed-on: https://chromium-review.googlesource.com/1145301
    Commit-Queue: Tatiana Gornak <melandory@chromium.org>
    Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579016}

--- components/invalidation/impl/invalidation_switches.h
+++ components/invalidation/impl/invalidation_switches.h
@@ -6,12 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_INVALIDATION_IMPL_INVALIDATION_SWITCHES_H
 #define COMPONENTS_INVALIDATION_IMPL_INVALIDATION_SWITCHES_H
 
+#include "base/feature_list.h"
+
 namespace invalidation {
 namespace switches {
 
 extern const char kInvalidationUseGCMChannel[];
 extern const char kSyncNotificationHostPort[];
 extern const char kSyncAllowInsecureXmppConnection[];
+extern const base::Feature kFCMInvalidations;
 
 }  // namespace switches
 }  // namespace invalidation
