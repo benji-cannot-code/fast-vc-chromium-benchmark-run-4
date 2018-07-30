44b1928d6dbcfed813ad368a1fe23b7dcb99f46c
Thomas Tangl <tangltom@chromium.org>
2018-07-30 11:37:06 +0000

    [unified-consent] Record unified consent bump metrics
    Metrics are recorded for the action the user takes
    when the consent bump is shown.
    
    Bug: 858870
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: I6723ed814e22f8681edab3062648cc39a2731e3b
    Reviewed-on: https://chromium-review.googlesource.com/1143185
    Commit-Queue: Thomas Tangl <tangltom@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Scott Chen <scottchen@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Markus Heintz <markusheintz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579012}

--- chrome/browser/ui/views/profiles/signin_view_controller_delegate_views.cc
+++ chrome/browser/ui/views/profiles/signin_view_controller_delegate_views.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "build/build_config.h"
+#include "chrome/browser/extensions/chrome_extension_web_contents_observer.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_avatar_icon_util.h"
 #include "chrome/browser/signin/signin_promo.h"
@@ -222,6 +223,9 @@ SigninViewControllerDelegateViews::CreateDialogWebView(
   int dialog_width = opt_width.value_or(kModalDialogWidth);
   views::WebView* web_view = new views::WebView(browser->profile());
   web_view->LoadInitialURL(GURL(url));
+  // To record metrics using javascript, extensions are needed.
+  extensions::ChromeExtensionWebContentsObserver::CreateForWebContents(
+      web_view->GetWebContents());
 
   SigninWebDialogUI* web_dialog_ui = static_cast<SigninWebDialogUI*>(
       web_view->GetWebContents()->GetWebUI()->GetController());
