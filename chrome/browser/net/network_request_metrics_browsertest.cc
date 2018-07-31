e4c26ff66498fe6bfba3dc40162886fe557f6676
Bence Béky <bnc@chromium.org>
2018-07-31 16:13:07 +0000

    Add ConnectionInfo histograms for network requests.
    As part of the network servicification effort, histograms were removed
    from ResourceLoader in https://crrev.com/c/1122712, and are now added to
    ResourceDispatcher in this CL.
    
    Bug: 859546
    Change-Id: I1c073098cdc532d067ab69ec98daa0aec1f9c910
    Reviewed-on: https://chromium-review.googlesource.com/1145129
    Commit-Queue: Bence Béky <bnc@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579423}

--- chrome/browser/net/network_request_metrics_browsertest.cc
+++ chrome/browser/net/network_request_metrics_browsertest.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/test_navigation_observer.h"
 #include "net/base/filename_util.h"
 #include "net/base/net_errors.h"
+#include "net/http/http_response_info.h"
 #include "net/test/embedded_test_server/controllable_http_response.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -53,6 +54,16 @@ enum class RequestType {
   kSubFrame,
   kImage,
   kScript,
+};
+
+enum class HeadersReceived {
+  kHeadersReceived,
+  kNoHeadersReceived,
+};
+
+enum class NetworkAccessed {
+  kNetworkAccessed,
+  kNoNetworkAccessed,
 };
 
 // Utility class to wait until the main resource load is complete. This is to
@@ -160,19 +171,35 @@ class NetworkRequestMetricsBrowserTest
 
   // Checks all relevant histograms. |expected_net_error| is the expected result
   // of the RequestType specified by the test parameter.
-  void CheckHistograms(int expected_net_error) {
+  void CheckHistograms(int expected_net_error,
+                       HeadersReceived headers_received,
+                       NetworkAccessed network_accessed) {
     // Some metrics may come from the renderer. This call ensures that those
-    // metrics are availble.
+    // metrics are available.
     SubprocessMetricsProvider::MergeHistogramDeltasForTesting();
 
     if (GetParam() == RequestType::kMainFrame) {
-      // Can't check Net.ErrorCodesForSubresources3, due to the favicon, which
-      // Chrome may or may not have attempted to load.
-      EXPECT_EQ(0u,
-                histograms_->GetAllSamples("Net.ErrorCodesForImages2").size());
+      histograms_->ExpectTotalCount("Net.ErrorCodesForImages2", 0);
 
       histograms_->ExpectUniqueSample("Net.ErrorCodesForMainFrame4",
                                       -expected_net_error, 1);
+
+      if (headers_received == HeadersReceived::kHeadersReceived) {
+        histograms_->ExpectUniqueSample(
+            "Net.ConnectionInfo.MainFrame",
+            net::HttpResponseInfo::CONNECTION_INFO_HTTP1_1, 1);
+      } else {
+        histograms_->ExpectTotalCount("Net.ConnectionInfo.MainFrame", 0);
+      }
+
+      // Favicon may or may not have been loaded.
+      EXPECT_GE(
+          1u,
+          histograms_->GetAllSamples("Net.ErrorCodesForSubresources3").size());
+      EXPECT_GE(
+          1u,
+          histograms_->GetAllSamples("Net.ConnectionInfo.SubResource").size());
+
       return;
     }
 
@@ -203,11 +230,30 @@ class NetworkRequestMetricsBrowserTest
     EXPECT_TRUE(found_expected_load);
 
     if (GetParam() != RequestType::kImage) {
-      EXPECT_EQ(0u,
-                histograms_->GetAllSamples("Net.ErrorCodesForImages2").size());
+      histograms_->ExpectTotalCount("Net.ErrorCodesForImages2", 0);
     } else {
       histograms_->ExpectUniqueSample("Net.ErrorCodesForImages2",
                                       -expected_net_error, 1);
+    }
+
+    // A subresource load requires a main frame load, which is only logged for
+    // network URLs.
+    if (network_accessed == NetworkAccessed::kNetworkAccessed) {
+      histograms_->ExpectUniqueSample(
+          "Net.ConnectionInfo.MainFrame",
+          net::HttpResponseInfo::CONNECTION_INFO_HTTP1_1, 1);
+      if (headers_received == HeadersReceived::kHeadersReceived) {
+        histograms_->ExpectUniqueSample(
+            "Net.ConnectionInfo.SubResource",
+            net::HttpResponseInfo::CONNECTION_INFO_HTTP1_1,
+            found_favicon_load ? 2 : 1);
+      } else {
+        histograms_->ExpectTotalCount("Net.ConnectionInfo.SubResource", 0);
+      }
+    } else {
+      histograms_->ExpectTotalCount("Net.ConnectionInfo.MainFrame", 0);
+      histograms_->ExpectTotalCount("Net.ConnectionInfo.SubResource",
+                                    found_favicon_load ? 1 : 0);
     }
   }
 
@@ -216,17 +262,15 @@ class NetworkRequestMetricsBrowserTest
   // to fail with net::ERR_ABORTED.
   void CheckHistogramsAfterMainFrameInterruption() {
     // Some metrics may come from the renderer. This call ensures that those
-    // metrics are availble.
+    // metrics are available.
     SubprocessMetricsProvider::MergeHistogramDeltasForTesting();
 
     if (GetParam() == RequestType::kMainFrame) {
       // Can't check Net.ErrorCodesForSubresources3, due to the favicon, which
       // Chrome may or may not have attempted to load.
-      EXPECT_EQ(0u,
-                histograms_->GetAllSamples("Net.ErrorCodesForImages2").size());
+      histograms_->ExpectTotalCount("Net.ErrorCodesForImages2", 0);
 
-      EXPECT_EQ(
-          2u, histograms_->GetAllSamples("Net.ErrorCodesForMainFrame4").size());
+      histograms_->ExpectTotalCount("Net.ErrorCodesForMainFrame4", 2);
       EXPECT_EQ(1, histograms_->GetBucketCount("Net.ErrorCodesForMainFrame4",
                                                -net::ERR_ABORTED));
       EXPECT_EQ(1, histograms_->GetBucketCount("Net.ErrorCodesForMainFrame4",
@@ -261,8 +305,7 @@ class NetworkRequestMetricsBrowserTest
     EXPECT_TRUE(found_expected_load);
 
     if (GetParam() != RequestType::kImage) {
-      EXPECT_EQ(0u,
-                histograms_->GetAllSamples("Net.ErrorCodesForImages2").size());
+      histograms_->ExpectTotalCount("Net.ErrorCodesForImages2", 0);
     } else {
       histograms_->ExpectUniqueSample("Net.ErrorCodesForImages2",
                                       -net::ERR_ABORTED, 1);
@@ -318,7 +361,8 @@ IN_PROC_BROWSER_TEST_P(NetworkRequestMetricsBrowserTest,
   interesting_http_response()->Done();
   navigation_observer.Wait();
 
-  CheckHistograms(net::ERR_EMPTY_RESPONSE);
+  CheckHistograms(net::ERR_EMPTY_RESPONSE, HeadersReceived::kNoHeadersReceived,
+                  NetworkAccessed::kNetworkAccessed);
 }
 
 IN_PROC_BROWSER_TEST_P(NetworkRequestMetricsBrowserTest, NetErrorDuringBody) {
@@ -328,7 +372,9 @@ IN_PROC_BROWSER_TEST_P(NetworkRequestMetricsBrowserTest, NetErrorDuringBody) {
   interesting_http_response()->Done();
   navigation_observer.Wait();
 
-  CheckHistograms(net::ERR_CONTENT_LENGTH_MISMATCH);
+  CheckHistograms(net::ERR_CONTENT_LENGTH_MISMATCH,
+                  HeadersReceived::kHeadersReceived,
+                  NetworkAccessed::kNetworkAccessed);
 }
 
 IN_PROC_BROWSER_TEST_P(NetworkRequestMetricsBrowserTest, CancelBeforeHeaders) {
@@ -337,7 +383,8 @@ IN_PROC_BROWSER_TEST_P(NetworkRequestMetricsBrowserTest, CancelBeforeHeaders) {
   active_web_contents()->Stop();
   navigation_observer.Wait();
 
-  CheckHistograms(net::ERR_ABORTED);
+  CheckHistograms(net::ERR_ABORTED, HeadersReceived::kNoHeadersReceived,
+                  NetworkAccessed::kNetworkAccessed);
 }
 
 IN_PROC_BROWSER_TEST_P(NetworkRequestMetricsBrowserTest, CancelDuringBody) {
@@ -356,7 +403,8 @@ IN_PROC_BROWSER_TEST_P(NetworkRequestMetricsBrowserTest, CancelDuringBody) {
   active_web_contents()->Stop();
   navigation_observer.Wait();
 
-  CheckHistograms(net::ERR_ABORTED);
+  CheckHistograms(net::ERR_ABORTED, HeadersReceived::kHeadersReceived,
+                  NetworkAccessed::kNetworkAccessed);
 }
 
 IN_PROC_BROWSER_TEST_P(NetworkRequestMetricsBrowserTest,
@@ -408,7 +456,8 @@ IN_PROC_BROWSER_TEST_P(NetworkRequestMetricsBrowserTest, SuccessWithBody) {
   interesting_http_response()->Done();
   navigation_observer.Wait();
 
-  CheckHistograms(net::OK);
+  CheckHistograms(net::OK, HeadersReceived::kHeadersReceived,
+                  NetworkAccessed::kNetworkAccessed);
 }
 
 IN_PROC_BROWSER_TEST_P(NetworkRequestMetricsBrowserTest, SuccessWithEmptyBody) {
@@ -419,7 +468,8 @@ IN_PROC_BROWSER_TEST_P(NetworkRequestMetricsBrowserTest, SuccessWithEmptyBody) {
   interesting_http_response()->Done();
   navigation_observer.Wait();
 
-  CheckHistograms(net::OK);
+  CheckHistograms(net::OK, HeadersReceived::kHeadersReceived,
+                  NetworkAccessed::kNetworkAccessed);
 }
 
 // Downloads should not be logged (Either as successes or failures).
@@ -454,17 +504,21 @@ IN_PROC_BROWSER_TEST_P(NetworkRequestMetricsBrowserTest, Download) {
   download_test_observer_terminal.WaitForFinished();
 
   // Some metrics may come from the renderer. This call ensures that those
-  // metrics are availble.
+  // metrics are available.
   SubprocessMetricsProvider::MergeHistogramDeltasForTesting();
 
   if (GetParam() == RequestType::kMainFrame) {
-    // Can't check Net.ErrorCodesForSubresources3, due to the favicon, which
-    // Chrome may or may not have attempted to load.
-    EXPECT_EQ(0u,
-              histograms()->GetAllSamples("Net.ErrorCodesForImages2").size());
+    histograms()->ExpectTotalCount("Net.ErrorCodesForImages2", 0);
+    histograms()->ExpectTotalCount("Net.ErrorCodesForMainFrame4", 0);
+    histograms()->ExpectTotalCount("Net.ConnectionInfo.MainFrame", 0);
+    // Favicon may or may not have been loaded.
+    EXPECT_GE(
+        1u,
+        histograms()->GetAllSamples("Net.ErrorCodesForSubresources3").size());
+    EXPECT_GE(
+        1u,
+        histograms()->GetAllSamples("Net.ConnectionInfo.SubResource").size());
 
-    EXPECT_EQ(
-        0u, histograms()->GetAllSamples("Net.ErrorCodesForMainFrame4").size());
     return;
   }
 
@@ -487,6 +541,17 @@ IN_PROC_BROWSER_TEST_P(NetworkRequestMetricsBrowserTest, Download) {
     EXPECT_EQ(0, bucket.count)
         << "Found unexpected load with result: " << bucket.min;
   }
+
+  histograms()->ExpectUniqueSample(
+      "Net.ConnectionInfo.MainFrame",
+      net::HttpResponseInfo::CONNECTION_INFO_HTTP1_1, 1);
+  if (found_favicon_load) {
+    histograms()->ExpectUniqueSample(
+        "Net.ConnectionInfo.SubResource",
+        net::HttpResponseInfo::CONNECTION_INFO_HTTP1_1, 1);
+  } else {
+    histograms()->ExpectTotalCount("Net.ConnectionInfo.SubResource", 0);
+  }
 }
 
 // A few tests for file:// URLs, so that URLs not handled by the network service
@@ -507,7 +572,8 @@ IN_PROC_BROWSER_TEST_P(NetworkRequestMetricsBrowserTest, FileURLError) {
 
   ui_test_utils::NavigateToURL(browser(),
                                net::FilePathToFileURL(main_frame_path));
-  CheckHistograms(net::ERR_FILE_NOT_FOUND);
+  CheckHistograms(net::ERR_FILE_NOT_FOUND, HeadersReceived::kNoHeadersReceived,
+                  NetworkAccessed::kNoNetworkAccessed);
 }
 
 IN_PROC_BROWSER_TEST_P(NetworkRequestMetricsBrowserTest, FileURLSuccess) {
@@ -534,7 +600,8 @@ IN_PROC_BROWSER_TEST_P(NetworkRequestMetricsBrowserTest, FileURLSuccess) {
 
   ui_test_utils::NavigateToURL(browser(),
                                net::FilePathToFileURL(main_frame_path));
-  CheckHistograms(net::OK);
+  CheckHistograms(net::OK, HeadersReceived::kNoHeadersReceived,
+                  NetworkAccessed::kNoNetworkAccessed);
 }
 
 INSTANTIATE_TEST_CASE_P(,
