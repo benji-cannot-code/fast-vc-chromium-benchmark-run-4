0fb0a7b4605e8c56ace243c753a9c4fdf03676b9
Lan Wei <lanwei@chromium.org>
2018-07-31 18:11:28 +0000

    Make pen inputs behave like touch inputs on MacOS
    Currently on MacOS the pen behaves like a mouse input, because we
    generate mouse events for the pen input. We agreed that the pen input
    should behave like a touch input, so we now send touch events instead.
    
    Bug: 854354
    Change-Id: Iac7910e250020a8125a57fb6055fb282db3faf0e
    Reviewed-on: https://chromium-review.googlesource.com/1136721
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Navid Zolghadr <nzolghadr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579482}

--- content/browser/renderer_host/input/web_input_event_builders_mac.h
+++ content/browser/renderer_host/input/web_input_event_builders_mac.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/web_input_event.h"
 #include "third_party/blink/public/platform/web_keyboard_event.h"
 #include "third_party/blink/public/platform/web_mouse_wheel_event.h"
+#include "third_party/blink/public/platform/web_touch_event.h"
 
 @class NSEvent;
 @class NSView;
@@ -40,6 +41,11 @@ class CONTENT_EXPORT WebMouseWheelEventBuilder {
 class CONTENT_EXPORT WebGestureEventBuilder {
  public:
   static blink::WebGestureEvent Build(NSEvent*, NSView*);
+};
+
+class CONTENT_EXPORT WebTouchEventBuilder {
+ public:
+  static blink::WebTouchEvent Build(NSEvent* event, NSView* view);
 };
 
 }  // namespace content
