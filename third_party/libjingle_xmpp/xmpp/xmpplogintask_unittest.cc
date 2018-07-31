ccb0b72eac11532c3b2ddb27d71d5b9a2b2f9bc7
Niels Möller <nisse@chromium.org>
2018-07-31 17:22:50 +0000

    Delete include of webrtc/typedefs.h
    The header file is obsolete and will be deleted in webrtc cl
    https://webrtc-review.googlesource.com/c/src/+/90410.
    
    Bug: webrtc:6854
    Change-Id: Ia3dff6686d8135ad54df8436f2c0125472c936bf
    Reviewed-on: https://chromium-review.googlesource.com/1156388
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579450}

--- third_party/libjingle_xmpp/xmpp/xmpplogintask_unittest.cc
+++ third_party/libjingle_xmpp/xmpp/xmpplogintask_unittest.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/libjingle_xmpp/xmpp/util_unittest.h"
 #include "third_party/libjingle_xmpp/xmpp/xmppengine.h"
 #include "third_party/webrtc/rtc_base/gunit.h"
-#include "third_party/webrtc/typedefs.h"
 
 // Macro to be used for switch-case fallthrough (required for enabling
 // -Wimplicit-fallthrough warning on Clang).
