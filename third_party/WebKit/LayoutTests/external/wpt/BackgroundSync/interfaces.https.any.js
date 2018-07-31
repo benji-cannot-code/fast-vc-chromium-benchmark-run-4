8e7d85b1dea88ef86283c27c7d87dddbfb3a1a6c
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-07-31 17:41:33 +0000

    Import wpt@d533edcd109bf4084608515766d7744f9957633a
    Using wpt-import in Chromium dbae05f3e05596da415445b8809c05d5ab244c38.
    With Chromium commits locally applied on WPT:
    7dad0f1723 "Added new Sec-Metadata Web Platform Tests."
    6fa8d815d9 "Add UserActivation member to MessageEvent"
    
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/22170
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    pwnall@chromium.org, jsbell@chromium.org:
      external/wpt/cookie-store
    
    TBR=raphael.kubo.da.costa@intel.com
    
    No-Export: true
    Change-Id: I19a9d6919ecdf6d5a911deafe9a4e7dc4c72ffbc
    Reviewed-on: https://chromium-review.googlesource.com/1156481
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579465}

--- third_party/WebKit/LayoutTests/external/wpt/BackgroundSync/interfaces.https.any.js
+++ third_party/WebKit/LayoutTests/external/wpt/BackgroundSync/interfaces.https.any.js
@@ -6,21 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // https://wicg.github.io/BackgroundSync/spec/
 
-promise_test(async () => {
-  const srcs = [
-    'BackgroundSync',
-    'service-workers',
-    'html',
-    'dom'
-  ];
-  const [idl, sw, html, dom] = await Promise.all(
-    srcs.map(i => fetch(`/interfaces/${i}.idl`).then(r => r.text())));
-
-  const idlArray = new IdlArray();
-  idlArray.add_idls(idl);
-  idlArray.add_dependency_idls(sw);
-  idlArray.add_dependency_idls(html);
-  idlArray.add_dependency_idls(dom);
-  idlArray.test();
-  done();
-}, 'Background Sync interfaces.');
+idl_test(
+  ['BackgroundSync'],
+  ['service-workers', 'html', 'dom'],
+  idlArray => {
+    // TODO: Objects
+  },
+  'Background Sync interfaces.'
+);
