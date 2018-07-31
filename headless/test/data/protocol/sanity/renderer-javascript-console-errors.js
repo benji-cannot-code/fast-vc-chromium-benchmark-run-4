dceb82593c293f9ee188c903b2a43bcbafd05bfa
Peter Kvitek <kvitekp@chromium.org>
2018-07-31 22:34:30 +0000

    Enabled BeginFrameControl for headless renderer tests
    Change-Id: Iec45bb977b603eef79ac91f4cb243c4e13d7e103
    Reviewed-on: https://chromium-review.googlesource.com/1157247
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579601}

--- headless/test/data/protocol/sanity/renderer-javascript-console-errors.js
+++ headless/test/data/protocol/sanity/renderer-javascript-console-errors.js
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 (async function(testRunner) {
-  let {page, session, dp} = await testRunner.startBlank(
+  let {page, session, dp} = await testRunner.startWithFrameControl(
       'Tests renderer: verify JavaScript console errors reporting.');
 
   let RendererTestHelper =
