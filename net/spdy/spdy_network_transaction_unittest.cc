2058fb460de8e8fe4f435b6f19ffa180dd0bd69f
Bence Béky <bnc@chromium.org>
2018-07-31 18:52:04 +0000

    Add histogram to track server HTTP/2 WebSocket support.
    Add Net.SpdySession.ServerSupportsWebSocket histogram to track server
    support of WebSockets over HTTP/2.
    
    Bug: 801564
    Change-Id: I1cd63e5bdca17106487c48ce6c1734e4d6591985
    Reviewed-on: https://chromium-review.googlesource.com/1152486
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Bence Béky <bnc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579500}

--- net/spdy/spdy_network_transaction_unittest.cc
+++ net/spdy/spdy_network_transaction_unittest.cc
@@ -7669,6 +7669,7 @@ TEST_F(SpdyNetworkTransactionTest, PushCanceledByServerAfterClaimed) {
 #if BUILDFLAG(ENABLE_WEBSOCKETS)
 
 TEST_F(SpdyNetworkTransactionTest, WebSocketOpensNewConnection) {
+  base::HistogramTester histogram_tester;
   NormalSpdyTransactionHelper helper(request_, DEFAULT_PRIORITY, log_, nullptr);
   helper.RunPreTestSetup();
 
@@ -7769,12 +7770,18 @@ TEST_F(SpdyNetworkTransactionTest, WebSocketOpensNewConnection) {
   // HTTP/2 connection is still open, but WebSocket request did not pool to it.
   ASSERT_TRUE(spdy_session);
 
-  base::RunLoop().RunUntilIdle();
   data1.Resume();
+  base::RunLoop().RunUntilIdle();
   helper.VerifyDataConsumed();
+
+  // Server did not advertise WebSocket support.
+  histogram_tester.ExpectUniqueSample("Net.SpdySession.ServerSupportsWebSocket",
+                                      /* support_websocket = false */ 0,
+                                      /* expected_count = */ 1);
 }
 
 TEST_F(SpdyNetworkTransactionTest, WebSocketOverHTTP2) {
+  base::HistogramTester histogram_tester;
   auto session_deps = std::make_unique<SpdySessionDependencies>();
   session_deps->enable_websocket_over_http2 = true;
   NormalSpdyTransactionHelper helper(request_, HIGHEST, log_,
@@ -7890,6 +7897,11 @@ TEST_F(SpdyNetworkTransactionTest, WebSocketOverHTTP2) {
   ASSERT_THAT(rv, IsOk());
 
   helper.VerifyDataConsumed();
+
+  // Server advertised WebSocket support.
+  histogram_tester.ExpectUniqueSample("Net.SpdySession.ServerSupportsWebSocket",
+                                      /* support_websocket = true */ 1,
+                                      /* expected_count = */ 1);
 }
 
 TEST_F(SpdyNetworkTransactionTest, WebSocketNegotiatesHttp2) {
