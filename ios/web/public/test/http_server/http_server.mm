129fbc094b51b4442c1ad6b1d53f38c793368f5c
tzik <tzik@chromium.org>
2018-07-31 04:50:36 +0000

    Keep shared web::test::HttpServer instance alive
    The web::test::HttpServer instance returned by GetSharedInstance has
    zero reference count initially, HttpServer::StartOrDie implicitly
    increments the reference count through base::Bind, and its
    embedded_test_server_ destruction decrements the reference count.
    That is, once the server stopped, the shared instance is destroyed, and
    GetSharedInstance() starts returning a stale pointer.
    
    After this CL, the resulting instance of GetSharedInstance has non-zero
    reference count, so that it keeps alive after server stop.
    
    Bug: 866456
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I1f13dff1e86d08efb28b5805ae73426b874a4f80
    Reviewed-on: https://chromium-review.googlesource.com/1146881
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Taiju Tsuiki <tzik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579314}

--- ios/web/public/test/http_server/http_server.mm
+++ ios/web/public/test/http_server/http_server.mm
@@ -57,6 +57,7 @@ HttpServer& HttpServer::GetSharedInstance() {
   static dispatch_once_t once;
   dispatch_once(&once, ^{
     shared_instance = new HttpServer();
+    shared_instance->AddRef();
   });
   return *shared_instance;
 }
