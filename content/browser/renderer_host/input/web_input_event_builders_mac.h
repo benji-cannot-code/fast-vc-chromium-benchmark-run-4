4bc2f5961d90d40186154beb2b270e7d7624ddc2
Fabrice de Gans-Riberi <fdegans@chromium.org>
2018-07-31 21:53:20 +0000

    Revert "Make pen inputs behave like touch inputs on MacOS"
    This reverts commit de31a6341849baa4ea04b8e41a2227066fee5f04.
    
    Reason for revert: This has made the Mac10.12 Tests content_unittests fail on the following tests:
    WebInputEventBuilderMacTest.TouchEventsWithPointerTypePenRotationLess180
    WebInputEventBuilderMacTest.TouchEventsWithPointerTypePenRotationGreater360
    WebInputEventBuilderMacTest.TouchEventsWithPointerTypePenRotationLess90
    WebInputEventBuilderMacTest.TouchEventsWithPointerTypePenRotationLess360
    WebInputEventBuilderMacTest.BuildWebTouchEvents
    
    See https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Mac10.12%20Tests/14711
    
    Original change's description:
    > Make pen inputs behave like touch inputs on MacOS
    > 
    > Currently on MacOS the pen behaves like a mouse input, because we
    > generate mouse events for the pen input. We agreed that the pen input
    > should behave like a touch input, so we now send touch events instead.
    > 
    > Bug: 854354
    > Change-Id: Iac7910e250020a8125a57fb6055fb282db3faf0e
    > Reviewed-on: https://chromium-review.googlesource.com/1136721
    > Commit-Queue: Lan Wei <lanwei@chromium.org>
    > Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    > Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    > Reviewed-by: Navid Zolghadr <nzolghadr@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#579482}
    
    TBR=meacer@chromium.org,tdresser@chromium.org,lanwei@chromium.org,nzolghadr@chromium.org
    
    Change-Id: I51b85daec63d39ef9aebb9dd727def7dfd7ca432
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 854354
    Reviewed-on: https://chromium-review.googlesource.com/1157304
    Reviewed-by: Fabrice de Gans-Riberi <fdegans@chromium.org>
    Commit-Queue: Fabrice de Gans-Riberi <fdegans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579584}

--- content/browser/renderer_host/input/web_input_event_builders_mac.h
+++ content/browser/renderer_host/input/web_input_event_builders_mac.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/web_input_event.h"
 #include "third_party/blink/public/platform/web_keyboard_event.h"
 #include "third_party/blink/public/platform/web_mouse_wheel_event.h"
-#include "third_party/blink/public/platform/web_touch_event.h"
 
 @class NSEvent;
 @class NSView;
@@ -41,11 +40,6 @@ class CONTENT_EXPORT WebMouseWheelEventBuilder {
 class CONTENT_EXPORT WebGestureEventBuilder {
  public:
   static blink::WebGestureEvent Build(NSEvent*, NSView*);
-};
-
-class CONTENT_EXPORT WebTouchEventBuilder {
- public:
-  static blink::WebTouchEvent Build(NSEvent* event, NSView* view);
 };
 
 }  // namespace content
