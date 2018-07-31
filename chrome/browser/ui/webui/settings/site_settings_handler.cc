58950e7375a51e88f65b0199793df078733997a8
Patti <patricialor@chromium.org>
2018-07-31 03:21:11 +0000

    Settings: All sites can now be sorted by most visited.
    The default sort order of the All Sites page is by the amount of site engagement
    each site has. Retrieve the site engagement score for each origin in All Sites
    and sort the list by the max site engagement per domain (eTLD+1) group.
    
    Bug: 835712
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: I421c41f3307a4e5b3ea35683bc4455dfc69ecd96
    Reviewed-on: https://chromium-review.googlesource.com/1141667
    Commit-Queue: Patti <patricialor@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579302}

--- chrome/browser/ui/webui/settings/site_settings_handler.cc
+++ chrome/browser/ui/webui/settings/site_settings_handler.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/content_settings/web_site_settings_uma_util.h"
+#include "chrome/browser/engagement/site_engagement_service.h"
 #include "chrome/browser/infobars/infobar_service.h"
 #include "chrome/browser/permissions/chooser_context_base.h"
 #include "chrome/browser/permissions/permission_decision_auto_blocker.h"
@@ -149,19 +150,30 @@ void CreateOrAppendSiteGroupEntry(
   }
 }
 
-// Converts a given |site_group_map| to a list of base::DictionaryValues.
+// Converts a given |site_group_map| to a list of base::DictionaryValues, adding
+// the site engagement score for each origin.
 void ConvertSiteGroupMapToListValue(
     const std::map<std::string, std::set<std::string>>& site_group_map,
-    base::Value* list_value) {
+    base::Value* list_value,
+    Profile* profile) {
   DCHECK_EQ(base::Value::Type::LIST, list_value->type());
+  DCHECK(profile);
+  SiteEngagementService* engagement_service =
+      SiteEngagementService::Get(profile);
   for (const auto& entry : site_group_map) {
     // eTLD+1 is the effective top level domain + 1.
     base::Value site_group(base::Value::Type::DICTIONARY);
     site_group.SetKey(kEffectiveTopLevelDomainPlus1Name,
                       base::Value(entry.first));
     base::Value origin_list(base::Value::Type::LIST);
-    for (const std::string& origin : entry.second)
-      origin_list.GetList().emplace_back(base::Value(origin));
+    for (const std::string& origin : entry.second) {
+      base::Value origin_object(base::Value::Type::DICTIONARY);
+      origin_object.SetKey("origin", base::Value(origin));
+      origin_object.SetKey(
+          "engagement",
+          base::Value(engagement_service->GetScore(GURL(origin))));
+      origin_list.GetList().emplace_back(std::move(origin_object));
+    }
     site_group.SetKey(kOriginList, std::move(origin_list));
     list_value->GetList().push_back(std::move(site_group));
   }
@@ -612,7 +624,7 @@ void SiteSettingsHandler::HandleGetAllSites(const base::ListValue* args) {
   }
 
   base::Value result(base::Value::Type::LIST);
-  ConvertSiteGroupMapToListValue(all_sites_map, &result);
+  ConvertSiteGroupMapToListValue(all_sites_map, &result, profile);
   ResolveJavascriptCallback(*callback_id, result);
 }
 
@@ -625,8 +637,8 @@ void SiteSettingsHandler::OnLocalStorageFetched(
     CreateOrAppendSiteGroupEntry(&all_sites_map, info.origin_url);
   }
   base::Value result(base::Value::Type::LIST);
-  ConvertSiteGroupMapToListValue(all_sites_map, &result);
-  FireWebUIListener("onLocalStorageListFetched", std::move(result));
+  ConvertSiteGroupMapToListValue(all_sites_map, &result, profile_);
+  FireWebUIListener("onLocalStorageListFetched", result);
 }
 
 void SiteSettingsHandler::HandleGetExceptionList(const base::ListValue* args) {
