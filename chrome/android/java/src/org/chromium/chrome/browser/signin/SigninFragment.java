c573353a91c57c48c190c7b9c15e4445c108e725
Boris Sazonov <bsazonov@chromium.org>
2018-07-31 18:27:55 +0000

    [Unity][Android] Move mSigninAccessPoint to SigninFragment
    This CL moves mSigninAccessPoint from SigninFragmentBase to
    SigninFragment. SigninFragmentBase will be used for as the base class
    for the consent bump fragment and for the consent bump there's no notion
    of the access point. This CL also reimplements how SigninFragmentBase
    determines the text for the cancel button, because it's been using
    mSigninAccessPoint.
    
    Bug: 869426
    Change-Id: I8361965a9c9df62d86e058b01f4a06f88e7bfbda
    Reviewed-on: https://chromium-review.googlesource.com/1156701
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579485}

--- chrome/android/java/src/org/chromium/chrome/browser/signin/SigninFragment.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/SigninFragment.java
@@ -12,6 +12,7 @@ import android.support.annotation.Nullable;
 
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.metrics.RecordUserAction;
+import org.chromium.chrome.R;
 import org.chromium.chrome.browser.preferences.PrefServiceBridge;
 import org.chromium.chrome.browser.preferences.PreferencesLauncher;
 
@@ -22,6 +23,7 @@ import java.lang.annotation.RetentionPolicy;
 public class SigninFragment extends SigninFragmentBase {
     private static final String TAG = "SigninFragment";
 
+    private static final String ARGUMENT_ACCESS_POINT = "SigninFragment.AccessPoint";
     private static final String ARGUMENT_PERSONALIZED_PROMO_ACTION =
             "SigninFragment.PersonalizedPromoAction";
 
@@ -35,6 +37,7 @@ public class SigninFragment extends SigninFragmentBase {
         int NEW_ACCOUNT = 3;
     }
 
+    private @SigninAccessPoint int mSigninAccessPoint;
     private @PromoAction int mPromoAction;
 
     /**
@@ -42,7 +45,9 @@ public class SigninFragment extends SigninFragmentBase {
      * @param accessPoint The access point for starting sign-in flow.
      */
     public static Bundle createArguments(@SigninAccessPoint int accessPoint) {
-        return SigninFragmentBase.createArguments(accessPoint, null);
+        Bundle result = SigninFragmentBase.createArguments(null);
+        result.putInt(ARGUMENT_ACCESS_POINT, accessPoint);
+        return result;
     }
 
     /**
@@ -52,7 +57,8 @@ public class SigninFragment extends SigninFragmentBase {
      */
     public static Bundle createArgumentsForPromoDefaultFlow(
             @SigninAccessPoint int accessPoint, String accountName) {
-        Bundle result = SigninFragmentBase.createArguments(accessPoint, accountName);
+        Bundle result = SigninFragmentBase.createArguments(accountName);
+        result.putInt(ARGUMENT_ACCESS_POINT, accessPoint);
         result.putInt(ARGUMENT_PERSONALIZED_PROMO_ACTION, PromoAction.WITH_DEFAULT);
         return result;
     }
@@ -65,8 +71,8 @@ public class SigninFragment extends SigninFragmentBase {
      */
     public static Bundle createArgumentsForPromoChooseAccountFlow(
             @SigninAccessPoint int accessPoint, String accountName) {
-        Bundle result =
-                SigninFragmentBase.createArgumentsForChooseAccountFlow(accessPoint, accountName);
+        Bundle result = SigninFragmentBase.createArgumentsForChooseAccountFlow(accountName);
+        result.putInt(ARGUMENT_ACCESS_POINT, accessPoint);
         result.putInt(ARGUMENT_PERSONALIZED_PROMO_ACTION, PromoAction.NOT_DEFAULT);
         return result;
     }
@@ -77,7 +83,8 @@ public class SigninFragment extends SigninFragmentBase {
      * @param accessPoint The access point for starting sign-in flow.
      */
     public static Bundle createArgumentsForPromoAddAccountFlow(@SigninAccessPoint int accessPoint) {
-        Bundle result = SigninFragmentBase.createArgumentsForAddAccountFlow(accessPoint);
+        Bundle result = SigninFragmentBase.createArgumentsForAddAccountFlow();
+        result.putInt(ARGUMENT_ACCESS_POINT, accessPoint);
         result.putInt(ARGUMENT_PERSONALIZED_PROMO_ACTION, PromoAction.NEW_ACCOUNT);
         return result;
     }
@@ -89,10 +96,20 @@ public class SigninFragment extends SigninFragmentBase {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        int accessPoint = getSigninArguments().getInt(ARGUMENT_ACCESS_POINT, -1);
+        assert accessPoint == SigninAccessPoint.AUTOFILL_DROPDOWN
+                || accessPoint == SigninAccessPoint.BOOKMARK_MANAGER
+                || accessPoint == SigninAccessPoint.NTP_CONTENT_SUGGESTIONS
+                || accessPoint == SigninAccessPoint.RECENT_TABS
+                || accessPoint == SigninAccessPoint.SETTINGS
+                || accessPoint == SigninAccessPoint.SIGNIN_PROMO
+                || accessPoint
+                        == SigninAccessPoint.START_PAGE : "invalid access point: " + accessPoint;
+        mSigninAccessPoint = accessPoint;
         mPromoAction =
                 getSigninArguments().getInt(ARGUMENT_PERSONALIZED_PROMO_ACTION, PromoAction.NONE);
 
-        SigninManager.logSigninStartAccessPoint(getSigninAccessPoint());
+        SigninManager.logSigninStartAccessPoint(mSigninAccessPoint);
         recordSigninStartedHistogramAccountInfo();
         recordSigninStartedUserAction();
     }
@@ -136,6 +153,12 @@ public class SigninFragment extends SigninFragmentBase {
         });
     }
 
+    @Override
+    protected int getNegativeButtonTextId() {
+        return mSigninAccessPoint == SigninAccessPoint.SIGNIN_PROMO ? R.string.no_thanks
+                                                                    : R.string.cancel;
+    }
+
     private void recordSigninCompletedHistogramAccountInfo() {
         final String histogram;
         switch (mPromoAction) {
@@ -156,7 +179,7 @@ public class SigninFragment extends SigninFragmentBase {
         }
 
         RecordHistogram.recordEnumeratedHistogram(
-                histogram, getSigninAccessPoint(), SigninAccessPoint.MAX);
+                histogram, mSigninAccessPoint, SigninAccessPoint.MAX);
     }
 
     private void recordSigninStartedHistogramAccountInfo() {
@@ -179,11 +202,11 @@ public class SigninFragment extends SigninFragmentBase {
         }
 
         RecordHistogram.recordEnumeratedHistogram(
-                histogram, getSigninAccessPoint(), SigninAccessPoint.MAX);
+                histogram, mSigninAccessPoint, SigninAccessPoint.MAX);
     }
 
     private void recordSigninStartedUserAction() {
-        switch (getSigninAccessPoint()) {
+        switch (mSigninAccessPoint) {
             case SigninAccessPoint.AUTOFILL_DROPDOWN:
                 RecordUserAction.record("Signin_Signin_FromAutofillDropdown");
                 break;
