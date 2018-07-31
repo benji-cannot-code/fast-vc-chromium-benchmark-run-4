860393ca0adc5af96847b0502a8826f974d4d342
Eric Orth <ericorth@chromium.org>
2018-07-31 22:01:07 +0000

    Implement new ResolveHost API in MappedHostResolver.
    Generally just applies the mapping to the input HostPortPair and passes
    on to the wrapped implementation. Some special handling for when the
    input is mapped to an error.
    
    Bug: 821021
    Change-Id: I0d108976e363993ebb36b8081948ef410894fa47
    Reviewed-on: https://chromium-review.googlesource.com/1156856
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Eric Orth <ericorth@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579587}

--- net/dns/mapped_host_resolver.cc
+++ net/dns/mapped_host_resolver.cc
@@ -7,12 +7,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
+#include "base/no_destructor.h"
 #include "base/strings/string_util.h"
 #include "base/values.h"
 #include "net/base/host_port_pair.h"
 #include "net/base/net_errors.h"
 
 namespace net {
+
+class MappedHostResolver::AlwaysErrorRequestImpl
+    : public HostResolver::ResolveHostRequest {
+ public:
+  explicit AlwaysErrorRequestImpl(int error) : error_(error) {}
+
+  int Start(CompletionOnceCallback callback) override { return error_; }
+
+  const base::Optional<AddressList>& GetAddressResults() const override {
+    static base::NoDestructor<base::Optional<AddressList>> nullopt_address_list;
+    return *nullopt_address_list;
+  }
+
+ private:
+  const int error_;
+};
 
 MappedHostResolver::MappedHostResolver(std::unique_ptr<HostResolver> impl)
     : impl_(std::move(impl)) {}
@@ -22,9 +39,13 @@ MappedHostResolver::~MappedHostResolver() = default;
 std::unique_ptr<HostResolver::ResolveHostRequest>
 MappedHostResolver::CreateRequest(const HostPortPair& host,
                                   const NetLogWithSource& source_net_log) {
-  // TODO(crbug.com/821021): Implement.
-  NOTIMPLEMENTED();
-  return nullptr;
+  HostPortPair rewritten = host;
+  rules_.RewriteHost(&rewritten);
+
+  if (rewritten.host() == "~NOTFOUND")
+    return std::make_unique<AlwaysErrorRequestImpl>(ERR_NAME_NOT_RESOLVED);
+
+  return impl_->CreateRequest(rewritten, source_net_log);
 }
 
 int MappedHostResolver::Resolve(const RequestInfo& original_info,
