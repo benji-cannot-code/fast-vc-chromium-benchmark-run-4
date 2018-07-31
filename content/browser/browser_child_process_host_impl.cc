c840495a666d75577a12ccf3aa6a43554cb48da0
Bruce Dawson <brucedawson@chromium.org>
2018-07-31 17:28:26 +0000

    Reland "Fix invalid handle errors found with App Verifier"
    This is a reland of c31de3260f48a6c197d698f8f2e295cd9f04d49b. The CHECK
    that was triggering (crbug.com/868341) has been removed but the change
    is otherwise the same. The CHECK will be readded once all of the bugs
    that it reveals are fixed (crbug.com/869154)
    
    Original change's description:
    > Fix invalid handle errors found with App Verifier
    >
    > ChildProcessData was holding a raw ProcessHandle which meant that there
    > was no guarantee that the handle would be valid when it was used. This
    > change switches to a ScopedHandle on Windows, with the associated
    > duplication of the handle, so that it is guaranteed to be valid. This
    > fixes a frequently encountered use of an invalid handle that was
    > detected by Application Verifier. This change also adds a CHECK to
    > base::GetProcId to check for use of invalid handles even when not using
    > Application Verifier.
    >
    > ChildProcessData::handle is now a private member so all references to
    > it were updated to use accessors.
    >
    > When ChildProcessData::SetHandle is called the handle is duplicated.
    > When a ChildProcessData object needs to be copied an explicit
    > Duplicate function is called.
    >
    > An IsHandleValid helper function was added.
    >
    > An invalid test was removed because it was triggering the CHECK in
    > base::GetProcId.
    >
    > Bug: 417532,821453,865805
    > Change-Id: I7e38140335c0140536919341f011f144f150c88f
    > Reviewed-on: https://chromium-review.googlesource.com/1145767
    > Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    > Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#578419}
    
    Bug: 417532, 821453, 865805, 868341, 869154
    Change-Id: I022982f447cce4649618f4413e960f5639ad750a
    Reviewed-on: https://chromium-review.googlesource.com/1155498
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579454}

--- content/browser/browser_child_process_host_impl.cc
+++ content/browser/browser_child_process_host_impl.cc
@@ -187,7 +187,7 @@ BrowserChildProcessHostImpl::~BrowserChildProcessHostImpl() {
   if (notify_child_disconnected_) {
     BrowserThread::PostTask(
         BrowserThread::UI, FROM_HERE,
-        base::BindOnce(&NotifyProcessHostDisconnected, data_));
+        base::BindOnce(&NotifyProcessHostDisconnected, data_.Duplicate()));
   }
 }
 
@@ -317,7 +317,7 @@ void BrowserChildProcessHostImpl::SetMetricsName(
 
 void BrowserChildProcessHostImpl::SetHandle(base::ProcessHandle handle) {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
-  data_.handle = handle;
+  data_.SetHandle(handle);
 }
 
 service_manager::mojom::ServiceRequest
@@ -359,7 +359,8 @@ ChildProcessTerminationInfo BrowserChildProcessHostImpl::GetTerminationInfo(
   if (!child_process_) {
     // If the delegate doesn't use Launch() helper.
     ChildProcessTerminationInfo info;
-    info.status = base::GetTerminationStatus(data_.handle, &info.exit_code);
+    info.status =
+        base::GetTerminationStatus(data_.GetHandle(), &info.exit_code);
     return info;
   }
   return child_process_->GetChildTerminationInfo(known_dead);
@@ -382,8 +383,9 @@ void BrowserChildProcessHostImpl::OnChannelConnected(int32_t peer_pid) {
   early_exit_watcher_.StopWatching();
 #endif
 
-  BrowserThread::PostTask(BrowserThread::UI, FROM_HERE,
-                          base::BindOnce(&NotifyProcessHostConnected, data_));
+  BrowserThread::PostTask(
+      BrowserThread::UI, FROM_HERE,
+      base::BindOnce(&NotifyProcessHostConnected, data_.Duplicate()));
 
   delegate_->OnChannelConnected(peer_pid);
 
@@ -391,7 +393,7 @@ void BrowserChildProcessHostImpl::OnChannelConnected(int32_t peer_pid) {
     ShareMetricsAllocatorToProcess();
     BrowserThread::PostTask(
         BrowserThread::UI, FROM_HERE,
-        base::BindOnce(&NotifyProcessLaunchedAndConnected, data_));
+        base::BindOnce(&NotifyProcessLaunchedAndConnected, data_.Duplicate()));
   }
 }
 
@@ -436,13 +438,14 @@ void BrowserChildProcessHostImpl::OnChildDisconnected() {
   // early exit watcher so GetTerminationStatus can close the process handle.
   early_exit_watcher_.StopWatching();
 #endif
-  if (child_process_.get() || data_.handle) {
+  if (child_process_.get() || data_.GetHandle()) {
     ChildProcessTerminationInfo info =
         GetTerminationInfo(true /* known_dead */);
 #if defined(OS_ANDROID)
     delegate_->OnProcessCrashed(info.exit_code);
-    BrowserThread::PostTask(BrowserThread::UI, FROM_HERE,
-                            base::BindOnce(&NotifyProcessKilled, data_, info));
+    BrowserThread::PostTask(
+        BrowserThread::UI, FROM_HERE,
+        base::BindOnce(&NotifyProcessKilled, data_.Duplicate(), info));
 #else  // OS_ANDROID
     switch (info.status) {
       case base::TERMINATION_STATUS_PROCESS_CRASHED:
@@ -450,7 +453,7 @@ void BrowserChildProcessHostImpl::OnChildDisconnected() {
         delegate_->OnProcessCrashed(info.exit_code);
         BrowserThread::PostTask(
             BrowserThread::UI, FROM_HERE,
-            base::BindOnce(&NotifyProcessCrashed, data_, info));
+            base::BindOnce(&NotifyProcessCrashed, data_.Duplicate(), info));
         UMA_HISTOGRAM_ENUMERATION("ChildProcess.Crashed2",
                                   static_cast<ProcessType>(data_.process_type),
                                   PROCESS_TYPE_MAX);
@@ -463,7 +466,7 @@ void BrowserChildProcessHostImpl::OnChildDisconnected() {
         delegate_->OnProcessCrashed(info.exit_code);
         BrowserThread::PostTask(
             BrowserThread::UI, FROM_HERE,
-            base::BindOnce(&NotifyProcessKilled, data_, info));
+            base::BindOnce(&NotifyProcessKilled, data_.Duplicate(), info));
         // Report that this child process was killed.
         UMA_HISTOGRAM_ENUMERATION("ChildProcess.Killed2",
                                   static_cast<ProcessType>(data_.process_type),
@@ -601,15 +604,14 @@ void BrowserChildProcessHostImpl::OnProcessLaunched() {
   early_exit_watcher_.StartWatchingOnce(process.Handle(), this);
 #endif
 
-  // TODO(rvargas) crbug.com/417532: Don't store a handle.
-  data_.handle = process.Handle();
+  data_.SetHandle(process.Handle());
   delegate_->OnProcessLaunched();
 
   if (is_channel_connected_) {
     ShareMetricsAllocatorToProcess();
     BrowserThread::PostTask(
         BrowserThread::UI, FROM_HERE,
-        base::BindOnce(&NotifyProcessLaunchedAndConnected, data_));
+        base::BindOnce(&NotifyProcessLaunchedAndConnected, data_.Duplicate()));
   }
 }
 
