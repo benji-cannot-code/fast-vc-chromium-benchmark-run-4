c7bd11514b704e959f3284352908643d6127f94a
Matthew Jones <mdjones@chromium.org>
2018-07-31 16:37:23 +0000

    Fix issue with scrim becoming invisible during animation
    This patch fixes an issue with the scrim that would cause it to become
    invisible if re-shown while hiding. The onAnimationEnd event would
    remove the scrim from the tree when it was still needed. This is
    resolved by making the animator listener one that is cancel-aware. The
    scrim is not removed from the tree if the animation is canceled (if
    the animation should end early, the #end() method should be called
    instead).
    
    Bug: 865109
    Change-Id: I5a83bc486a2798ddca8dca4fefc6a0a340fa1613
    Reviewed-on: https://chromium-review.googlesource.com/1156482
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579431}

--- chrome/android/java/src/org/chromium/chrome/browser/widget/ScrimView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/ScrimView.java
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.widget;
 
 import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
 import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.support.annotation.NonNull;
@@ -18,6 +17,7 @@ import android.view.ViewGroup.MarginLayoutParams;
 import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.util.MathUtils;
+import org.chromium.chrome.browser.widget.animation.CancelAwareAnimatorListener;
 import org.chromium.ui.UiUtils;
 import org.chromium.ui.interpolators.BakedBezierInterpolator;
 
@@ -261,9 +261,9 @@ public class ScrimView extends View implements View.OnClickListener {
             mOverlayFadeOutAnimator = ObjectAnimator.ofFloat(this, ALPHA, 0f);
             mOverlayFadeOutAnimator.setDuration(FADE_DURATION_MS);
             mOverlayFadeOutAnimator.setInterpolator(BakedBezierInterpolator.FADE_OUT_CURVE);
-            mOverlayFadeOutAnimator.addListener(new AnimatorListenerAdapter() {
+            mOverlayFadeOutAnimator.addListener(new CancelAwareAnimatorListener() {
                 @Override
-                public void onAnimationEnd(Animator animation) {
+                public void onEnd(Animator animation) {
                     setVisibility(View.GONE);
                     if (mActiveParams != null && mActiveParams.observer != null) {
                         mActiveParams.observer.onScrimVisibilityChanged(false);
