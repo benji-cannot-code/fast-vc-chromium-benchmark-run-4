6167cd0fb17ab51cd8feaf8fa5f84af3d7b0b9ca
kristipark <kristipark@chromium.org>
2018-07-31 20:05:29 +0000

    Increase max number of MV sites to 9 if custom links is enabled and show up to 10 custom links
    This is to balance the MV tiles when the "add custom link" button is
    present.
    
    Default MV: https://screenshot.googleplex.com/brjYoMmojgO.png
    Custom links enabled: https://screenshot.googleplex.com/C2zg6XFzEtk.png
    Max custom  links: https://screenshot.googleplex.com/x5KkYLmJ0tz.png
    
    Bug: 851194
    Change-Id: I5c487f10b9a95a4dcbbefa5a3913fdd6cedeb04d
    Reviewed-on: https://chromium-review.googlesource.com/1142570
    Commit-Queue: Kristi Park <kristipark@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579541}

--- chrome/browser/search/instant_service.cc
+++ chrome/browser/search/instant_service.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/search/instant_service_observer.h"
 #include "chrome/browser/search/local_ntp_source.h"
 #include "chrome/browser/search/most_visited_iframe_source.h"
+#include "chrome/browser/search/ntp_features.h"
 #include "chrome/browser/search/ntp_icon_source.h"
 #include "chrome/browser/search/search.h"
 #include "chrome/browser/search/thumbnail_source.h"
@@ -116,8 +117,13 @@ InstantService::InstantService(Profile* profile)
                  content::NotificationService::AllSources());
 
   most_visited_sites_ = ChromeMostVisitedSitesFactory::NewForProfile(profile_);
-  if (most_visited_sites_)
-    most_visited_sites_->SetMostVisitedURLsObserver(this, 8);
+  if (most_visited_sites_) {
+    // 9 tiles are required for the custom links feature in order to balance the
+    // Most Visited rows (this is due to an additional "Add" button). Otherwise,
+    // Most Visited should return the regular 8 tiles.
+    most_visited_sites_->SetMostVisitedURLsObserver(
+        this, features::IsCustomLinksEnabled() ? 9 : 8);
+  }
 
   if (profile_ && profile_->GetResourceContext()) {
     content::BrowserThread::PostTask(
