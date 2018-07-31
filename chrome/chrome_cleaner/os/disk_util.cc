2041a9db2bcdd8f081da0153ea9da750185f0844
Joe Mason <joenotcharles@chromium.org>
2018-07-31 19:03:48 +0000

    Add ComputeSHA256DigestOfString function to chrome_cleaner/os
    Bug: 830892
    Change-Id: I3b900e68527f31cfd52cc27d98cfcdd3a22d001b
    Reviewed-on: https://chromium-review.googlesource.com/1147497
    Reviewed-by: Chris Sharp <csharp@chromium.org>
    Commit-Queue: Joe Mason <joenotcharles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579511}

--- chrome/chrome_cleaner/os/disk_util.cc
+++ chrome/chrome_cleaner/os/disk_util.cc
@@ -543,7 +543,7 @@ bool RetrieveDetailedFileInformation(
   RetrievePathInformation(expanded_path, file_information);
 
   // Retrieve the detailed file information.
-  if (!ComputeDigestSHA256(expanded_path, &file_information->sha256)) {
+  if (!ComputeSHA256DigestOfPath(expanded_path, &file_information->sha256)) {
     LOG(ERROR) << "Unable to compute digest SHA256 for: '"
                << file_information->path << "'";
     return false;
@@ -589,7 +589,8 @@ bool RetrieveFileInformation(const base::FilePath& file_path,
   }
 }
 
-bool ComputeDigestSHA256(const base::FilePath& path, std::string* digest) {
+bool ComputeSHA256DigestOfPath(const base::FilePath& path,
+                               std::string* digest) {
   DCHECK(digest);
 
   base::File file(path, base::File::FLAG_OPEN | base::File::FLAG_READ);
@@ -604,6 +605,21 @@ bool ComputeDigestSHA256(const base::FilePath& path, std::string* digest) {
       break;
     ctx->Update(buffer, count);
   }
+
+  char digest_bytes[crypto::kSHA256Length];
+  ctx->Finish(digest_bytes, crypto::kSHA256Length);
+
+  *digest = base::HexEncode(digest_bytes, crypto::kSHA256Length);
+  return true;
+}
+
+bool ComputeSHA256DigestOfString(const std::string& content,
+                                 std::string* digest) {
+  DCHECK(digest);
+
+  std::unique_ptr<SecureHash> ctx(SecureHash::Create(SecureHash::SHA256));
+
+  ctx->Update(content.c_str(), content.length());
 
   char digest_bytes[crypto::kSHA256Length];
   ctx->Finish(digest_bytes, crypto::kSHA256Length);
