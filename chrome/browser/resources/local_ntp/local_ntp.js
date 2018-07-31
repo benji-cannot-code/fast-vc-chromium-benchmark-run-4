ff3cc6b66b012f43036753d23420da4d1a2fbd6a
Mathieu Perreault <mathp@chromium.org>
2018-07-31 20:32:22 +0000

    [Local NTP] Add config.js as top-level instead of dynamically added.
    To load the NTP faster, we include config.js data in the local ntp
    page's initial resources instead of later added by local_ntp.js
    
    To achieve this, we refactor into the SearchConfigProvider and cache
    the config data. It will be recomputed on Search provider change.
    
    We also now declare the CSP as part of a <meta> tag instead of headers,
    since in URLDataSource the headers are created on the IO thread and
    the response on the UI thread. Doing everything in the local_ntp.html
    response avoids races nicely.
    
    Bug: 868432
    Change-Id: Iab38ca2b2e032a58c2d404938065a612b08cc69a
    Reviewed-on: https://chromium-review.googlesource.com/1154687
    Reviewed-by: Chris Palmer <palmer@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Mathieu Perreault <mathp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579553}

--- chrome/browser/resources/local_ntp/local_ntp.js
+++ chrome/browser/resources/local_ntp/local_ntp.js
@@ -1093,20 +1093,11 @@ function init() {
 }
 
 
-function loadConfig() {
-  var configScript = document.createElement('script');
-  configScript.type = 'text/javascript';
-  configScript.src = 'chrome-search://local-ntp/config.js';
-  configScript.onload = init;
-  document.head.appendChild(configScript);
-}
-
-
 /**
  * Binds event listeners.
  */
 function listen() {
-  document.addEventListener('DOMContentLoaded', loadConfig);
+  document.addEventListener('DOMContentLoaded', init);
 }
 
 
--- chrome/browser/search/local_ntp_source.h
+++ chrome/browser/search/local_ntp_source.h
@@ -48,7 +48,7 @@ class LocalNtpSource : public content::URLDataSource,
   explicit LocalNtpSource(Profile* profile);
 
  private:
-  class GoogleSearchProviderTracker;
+  class SearchConfigurationProvider;
   class DesktopLogoObserver;
 
   struct NtpBackgroundRequest {
@@ -86,8 +86,10 @@ class LocalNtpSource : public content::URLDataSource,
   bool ShouldServiceRequest(const GURL& url,
                             content::ResourceContext* resource_context,
                             int render_process_id) const override;
-  std::string GetContentSecurityPolicyScriptSrc() const override;
-  std::string GetContentSecurityPolicyChildSrc() const override;
+  bool ShouldAddContentSecurityPolicy() const override;
+
+  // The Content Security Policy for the Local NTP.
+  std::string GetContentSecurityPolicy() const;
 
   // Overridden from NtpBackgroundServiceObserver:
   void OnCollectionInfoAvailable() override;
@@ -124,7 +126,7 @@ class LocalNtpSource : public content::URLDataSource,
   search_provider_logos::LogoService* logo_service_;
   std::unique_ptr<DesktopLogoObserver> logo_observer_;
 
-  std::unique_ptr<GoogleSearchProviderTracker> google_tracker_;
+  std::unique_ptr<SearchConfigurationProvider> search_config_provider_;
 
   base::WeakPtrFactory<LocalNtpSource> weak_ptr_factory_;
 
