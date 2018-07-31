e81298aa1b1a47e8d0bbe1834e18ce789a16c075
Ella Ge <eirage@chromium.org>
2018-07-31 20:06:02 +0000

    Enable use-zoom-for-dsf on Android on M70
    This CL makes use-zoom-for-dsf enabled by default on Android
    
    This marks Test NoResizeAfterIframeLoad disabled on Android
    because it's failing on Android.
    
    This also rebaseline 22 gpu_tests: they are failing on Android
    after enabling the flag because the new result are one pixel off
    the original reference image. Will test expectations in a followup
    CL.
    
    Bug: 737777, 485650
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: If06cf325a831e229fd6bb1889fe1e40ed4bbe764
    Reviewed-on: https://chromium-review.googlesource.com/1055812
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579542}

--- content/public/common/use_zoom_for_dsf_policy.cc
+++ content/public/common/use_zoom_for_dsf_policy.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "content/public/common/content_switches.h"
 
-#if defined(OS_WIN)
+#if defined(OS_WIN) || defined(OS_ANDROID)
 #include "base/feature_list.h"
 #endif
 
@@ -20,10 +20,15 @@ const base::Feature kUseZoomForDsfEnabledByDefault{
     "use-zoom-for-dsf enabled by default", base::FEATURE_ENABLED_BY_DEFAULT};
 #endif
 
+#if defined(OS_ANDROID)
+const base::Feature kUseZoomForDsfEnabledByDefault{
+    "use-zoom-for-dsf enabled by default", base::FEATURE_ENABLED_BY_DEFAULT};
+#endif
+
 bool IsUseZoomForDSFEnabledByDefault() {
 #if defined(OS_LINUX) || defined(OS_FUCHSIA)
   return true;
-#elif defined(OS_WIN)
+#elif defined(OS_WIN) || defined(OS_ANDROID)
   return base::FeatureList::IsEnabled(kUseZoomForDsfEnabledByDefault);
 #else
   return false;
