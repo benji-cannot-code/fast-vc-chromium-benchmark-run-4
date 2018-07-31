be66c76e7dc25fea97bb9f444f1005232cd7de19
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2018-07-31 02:20:20 +0000

    lint: Fix some issues in PRESUBMIT.py found by flake8
    Fixed some of the following issues:
    - E302 expected 2 blank lines, found 1
    - E703 statement ends with a semicolon
    - E999 SyntaxError: invalid syntax
    
    Bug: None
    Change-Id: Ia4c9a2c81168395331f5d2135a7bba1eafcd3640
    Reviewed-on: https://chromium-review.googlesource.com/1155625
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579285}

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -963,9 +963,10 @@ def _CheckValidHostsInDEPS(input_api, output_api):
     return []
   # Outsource work to gclient verify
   try:
-    input_api.subprocess.check_output(['gclient', 'verify'])
+    input_api.subprocess.check_output(['gclient', 'verify'],
+                                      stderr=input_api.subprocess.STDOUT)
     return []
-  except input_api.subprocess.CalledProcessError, error:
+  except input_api.subprocess.CalledProcessError as error:
     return [output_api.PresubmitError(
         'DEPS file must have only git dependencies.',
         long_text=error.output)]
@@ -1265,6 +1266,7 @@ def _CheckForVersionControlConflicts(input_api, output_api):
     results.append(output_api.PresubmitError(
       'Version control conflict markers found, please resolve.', errors))
   return results
+
 
 def _CheckGoogleSupportAnswerUrl(input_api, output_api):
   pattern = input_api.re.compile('support\.google\.com\/chrome.*/answer')
@@ -2249,6 +2251,7 @@ def _CheckAndroidTestJUnitInheritance(input_api, output_api):
       ' questions.', errors))
   return results
 
+
 def _CheckAndroidTestAnnotationUsage(input_api, output_api):
   """Checks that android.test.suitebuilder.annotation.* is no longer used."""
   deprecated_annotation_import_pattern = input_api.re.compile(
@@ -2498,6 +2501,7 @@ _DEPRECATED_CSS = [
   ( "-webkit-repeating-radial-gradient", "repeating-radial-gradient" ),
 ]
 
+
 def _CheckNoDeprecatedCss(input_api, output_api):
   """ Make sure that we don't use deprecated CSS
       properties, functions or values. Our external
@@ -2532,6 +2536,7 @@ _DEPRECATED_JS = [
   ( "__defineSetter__", "Object.defineProperty" ),
 ]
 
+
 def _CheckNoDeprecatedJs(input_api, output_api):
   """Make sure that we don't use deprecated JS in Chrome code."""
   results = []
@@ -2549,17 +2554,20 @@ def _CheckNoDeprecatedJs(input_api, output_api):
               (fpath.LocalPath(), lnum, deprecated, replacement)))
   return results
 
+
 def _CheckForRiskyJsArrowFunction(line_number, line):
   if ' => ' in line:
     return "line %d, is using an => (arrow) function\n %s\n" % (
         line_number, line)
   return ''
 
+
 def _CheckForRiskyJsConstLet(input_api, line_number, line):
   if input_api.re.match('^\s*(const|let)\s', line):
     return "line %d, is using const/let keyword\n %s\n" % (
         line_number, line)
   return ''
+
 
 def _CheckForRiskyJsFeatures(input_api, output_api):
   maybe_ios_js = (r"^(ios|components|ui\/webui\/resources)\/.+\.js$", )
@@ -2609,6 +2617,7 @@ https://chromium.googlesource.com/chromium/src/+/master/docs/es6_chromium.md#con
           ])))
 
   return results
+
 
 def _CheckForRelativeIncludes(input_api, output_api):
   # Need to set the sys.path so PRESUBMIT_test.py runs properly
@@ -3276,7 +3285,7 @@ def _CheckCrbugLinksHaveHttps(input_api, output_api):
       f, white_list=white_list, black_list=black_list)
 
   pattern = input_api.re.compile(r'//.*(?<!:\/\/)crbug[.com]*')
-  accepted_pattern = input_api.re.compile(r'//.*TODO\(crbug[.com]*');
+  accepted_pattern = input_api.re.compile(r'//.*TODO\(crbug[.com]*')
   problems = []
   for f in input_api.AffectedSourceFiles(sources):
     for line_num, line in f.ChangedContents():
