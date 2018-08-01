1b1b4a04b62dddb5395acdcbe06efeaab4413ff1
Yuri Wiitala <miu@chromium.org>
2018-08-01 20:09:23 +0000

    'Place-holder' overlay rendering for CrOS window capture.
    A minimal FrameSinkVideoCaptureOverlay implementation for browser window
    video capture on ChromeOS (i.e., not desktop capture, and not
    WebContents capture). The intention is for this code to be a temporary
    solution until more work is complete on services/ui/ws (the new Window
    Service).
    
    Bug: 810133,806366
    Change-Id: I4f929248a9620f96f2a538caaac9761a472cbe09
    Reviewed-on: https://chromium-review.googlesource.com/1155985
    Commit-Queue: Yuri Wiitala <miu@chromium.org>
    Reviewed-by: Xiangjun Zhang <xjz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579913}

--- content/browser/media/capture/lame_window_capturer_chromeos.h
+++ content/browser/media/capture/lame_window_capturer_chromeos.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/timer/timer.h"
 #include "base/unguessable_token.h"
+#include "content/browser/media/capture/lame_capture_overlay_chromeos.h"
 #include "media/base/video_frame.h"
 #include "mojo/public/cpp/system/buffer.h"
 #include "services/viz/privileged/interfaces/compositing/frame_sink_video_capture.mojom.h"
@@ -48,6 +49,7 @@ namespace content {
 //
 // TODO(crbug/806366): The goal is to remove this code by 2019.
 class LameWindowCapturerChromeOS : public viz::mojom::FrameSinkVideoCapturer,
+                                   public LameCaptureOverlayChromeOS::Owner,
                                    public aura::WindowObserver {
  public:
   explicit LameWindowCapturerChromeOS(aura::Window* target);
@@ -76,6 +78,9 @@ class LameWindowCapturerChromeOS : public viz::mojom::FrameSinkVideoCapturer,
   // delivered to the consumer. When the consumer is done with the frame, this
   // returns the buffer back to the pool.
   class InFlightFrame;
+
+  // LameWindowCapturerChromeOS::Owner implementation.
+  void OnOverlayConnectionLost(LameCaptureOverlayChromeOS* overlay) final;
 
   // Initiates capture of the next frame. This is called periodically by the
   // |timer_|.
@@ -124,6 +129,9 @@ class LameWindowCapturerChromeOS : public viz::mojom::FrameSinkVideoCapturer,
   // A value provided in the copy requests to enable VIZ to optimize around
   // video capture.
   const base::UnguessableToken copy_request_source_;
+
+  // An optional overlay to be rendered over each captured video frame.
+  std::unique_ptr<LameCaptureOverlayChromeOS> overlay_;
 
   // Used for cancelling any outstanding activities' results, once Stop() is
   // called and there is no longer a consumer to receive another frame.
