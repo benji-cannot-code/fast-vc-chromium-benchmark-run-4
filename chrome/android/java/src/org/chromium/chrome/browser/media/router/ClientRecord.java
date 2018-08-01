e85f35001fa53dbfbf6de58f180cf856556e2789
Zhiqiang Zhang <zqzhang@google.com>
2018-08-01 21:14:39 +0000

    Reland "[Android MR] Migrate session management"
    This reverts commit 5b9084f802a4976d61d9cfcce611f6b76faa251c.
    
    Reason for revert: Reland after trimming out unused SDK features in dex file
    
    Original change's description:
    > Revert "[Android MR] Migrate session management"
    > 
    > This reverts commit 40d02af15a3202690b05928fb636d4e97e7c0d47.
    > 
    > Reason for revert: Broke downstream release builders, and caused
    > large binary size regression.
    > 
    > Original change's description:
    > > [Android MR] Migrate session management
    > > 
    > > This CL migrates the session management part of MediaRouter.
    > > CreateRouteRequest is not used and we are using route selection
    > > to let CAF start sessions automatically.
    > > 
    > > Change-Id: I58d267418d9afd11f4f44520e8ca8ad94e82388b
    > > Reviewed-on: https://chromium-review.googlesource.com/1006358
    > > Reviewed-by: David Trainor <dtrainor@chromium.org>
    > > Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    > > Commit-Queue: Zhiqiang Zhang <zqzhang@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#569080}
    > 
    > TBR=dtrainor@chromium.org,zqzhang@chromium.org,tguilbert@chromium.org
    > 
    > Change-Id: I8fae7d4492f873223b5efb3fc3e5d6acef37f981
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 855081, 855025
    > Reviewed-on: https://chromium-review.googlesource.com/1109981
    > Reviewed-by: agrieve <agrieve@chromium.org>
    > Commit-Queue: agrieve <agrieve@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#569314}
    
    TBR=dtrainor@chromium.org,agrieve@chromium.org,zqzhang@chromium.org,tguilbert@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 855081, 855025
    Change-Id: I8fcdf97d32b57f6011be1dbd322365ceeaf3f442
    Reviewed-on: https://chromium-review.googlesource.com/1132356
    Commit-Queue: Zhiqiang Zhang <zqzhang@chromium.org>
    Reviewed-by: Zhiqiang Zhang <zqzhang@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579941}

--- chrome/android/java/src/org/chromium/chrome/browser/media/router/ClientRecord.java
+++ chrome/android/java/src/org/chromium/chrome/browser/media/router/ClientRecord.java
@@ -3,9 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser.media.router.cast;
-
-import org.chromium.chrome.browser.media.router.MediaRoute;
+package org.chromium.chrome.browser.media.router;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -54,13 +52,8 @@ public class ClientRecord {
      */
     public List<String> pendingMessages = new ArrayList<String>();
 
-    ClientRecord(
-            String routeId,
-            String clientId,
-            String appId,
-            String autoJoinPolicy,
-            String origin,
-            int tabId) {
+    public ClientRecord(String routeId, String clientId, String appId, String autoJoinPolicy,
+            String origin, int tabId) {
         this.routeId = routeId;
         this.clientId = clientId;
         this.appId = appId;
