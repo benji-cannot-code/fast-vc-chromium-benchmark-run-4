00425e5794802725e2c406bb0a7d7aab5be73152
Pavol Marko <pmarko@chromium.org>
2018-08-01 16:42:37 +0000

    Set SequenceToken for SequenceLocalStorageMap destructor
    Ensure that the SequenceLocalStorageMap and consequently objects still
    stored in the sequence-local storage are destroyed while the owning
    Sequence's SequenceToken is set for the current thread.
    This makes sure that objects stored in the sequence-local storage which
    expect to be destroyed on the same Sequence they've been created on do
    not (D)CHECK in their destrutor.
    
    Bug: 869272
    Test: base_unittests && browser_tests --gtest_filter=*UserAffiliation*
    Change-Id: I8c76b274a746a35c4ad038e7d8f04ea212ef1514
    Reviewed-on: https://chromium-review.googlesource.com/1156510
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579834}

--- base/task_scheduler/sequence.h
+++ base/task_scheduler/sequence.h
@@ -72,7 +72,7 @@ class BASE_EXPORT Sequence : public RefCountedThreadSafe<Sequence> {
   const SequenceToken& token() const { return token_; }
 
   SequenceLocalStorageMap* sequence_local_storage() {
-    return &sequence_local_storage_;
+    return &sequence_local_storage_.value();
   }
 
  private:
@@ -92,7 +92,9 @@ class BASE_EXPORT Sequence : public RefCountedThreadSafe<Sequence> {
       {};
 
   // Holds data stored through the SequenceLocalStorageSlot API.
-  SequenceLocalStorageMap sequence_local_storage_;
+  // Valid throughout this Sequence's lifetime (Optional so it can be released
+  // in a custom scope in the destructor).
+  base::Optional<SequenceLocalStorageMap> sequence_local_storage_;
 
   DISALLOW_COPY_AND_ASSIGN(Sequence);
 };
