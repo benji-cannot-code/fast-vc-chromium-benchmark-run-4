e7e2c785073985b4f7e09f2e7da32b315461dc43
Kevin Strohbehn <ginko@google.com>
2018-08-01 01:24:02 +0000

    Fixes stale icon during reparent drag out of a folder
    Bug: 868117
    Change-Id: I15aae661ae200ec858e6886be718eaa55ff3266a
    Reviewed-on: https://chromium-review.googlesource.com/1152231
    Commit-Queue: Kevin Strohbehn <ginko@google.com>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Reviewed-by: Weidong Guo <weidongg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579655}

--- ash/app_list/model/app_list_folder_item.cc
+++ ash/app_list/model/app_list_folder_item.cc
@@ -71,4 +71,8 @@ void AppListFolderItem::OnFolderImageUpdated() {
   SetIcon(folder_image_.icon());
 }
 
+void AppListFolderItem::NotifyOfDraggedItem(AppListItem* dragged_item) {
+  folder_image_.UpdateDraggedItem(dragged_item);
+}
+
 }  // namespace app_list
