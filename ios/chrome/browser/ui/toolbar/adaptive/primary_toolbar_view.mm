0e5a36aeb89522dd29a6506e4f2ec274b8debd5e
Gauthier Ambard <gambard@chromium.org>
2018-08-01 14:06:12 +0000

    Adjust toolbar height in fullscreen
    This CL adjusts the toolbar height in fullscreen.
    It also adjusts the vertical position of the location bar, in fullscreen
    and outside of fullscreen. This is needed to have something which looks
    visually balanced.
    
    Bug: 868901, 865820
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ib8da381737b4660554a5fb68a213b99860f117d0
    Reviewed-on: https://chromium-review.googlesource.com/1156602
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579793}

--- ios/chrome/browser/ui/toolbar/adaptive/primary_toolbar_view.mm
+++ ios/chrome/browser/ui/toolbar/adaptive/primary_toolbar_view.mm
@@ -36,6 +36,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @property(nonatomic, strong, readwrite) UIView* locationBarContainer;
 // The height of the container for the location bar, redefined as readwrite.
 @property(nonatomic, strong, readwrite) NSLayoutConstraint* locationBarHeight;
+// The layout guide used to give extra padding at the bottom for the location
+// bar. This padding is considered as "extra" as it is added to the one defined
+// in |locationBarBottomConstraint|.
+@property(nonatomic, strong) UILayoutGuide* extraPaddingGuide;
 
 // StackView containing the leading buttons (relative to the location bar). It
 // should only contain ToolbarButtons. Redefined as readwrite.
@@ -89,7 +93,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @synthesize locationBarView = _locationBarView;
 @synthesize fakeOmniboxTarget = _fakeOmniboxTarget;
 @synthesize locationBarBottomConstraint = _locationBarBottomConstraint;
+@synthesize locationBarExtraBottomPadding = _locationBarExtraBottomPadding;
 @synthesize locationBarHeight = _locationBarHeight;
+@synthesize extraPaddingGuide = _extraPaddingGuide;
 @synthesize buttonFactory = _buttonFactory;
 @synthesize allButtons = _allButtons;
 @synthesize progressBar = _progressBar;
@@ -225,6 +231,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // The location bar shouldn't have vibrancy.
   [self addSubview:self.locationBarContainer];
 
+  // Add layout guide to add extra padding for the location bar if needed.
+  self.extraPaddingGuide = [[UILayoutGuide alloc] init];
+  [self addLayoutGuide:self.extraPaddingGuide];
+
   if (self.locationBarView) {
     [self.locationBarContainer addSubview:self.locationBarView];
   }
@@ -318,11 +328,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       constraintEqualToConstant:kAdaptiveToolbarHeight -
                                 2 * kAdaptiveLocationBarVerticalMargin];
   self.locationBarBottomConstraint = [self.locationBarContainer.bottomAnchor
-      constraintEqualToAnchor:self.bottomAnchor
+      constraintEqualToAnchor:self.extraPaddingGuide.topAnchor
                      constant:-kAdaptiveLocationBarVerticalMargin];
+  self.locationBarExtraBottomPadding =
+      [self.extraPaddingGuide.heightAnchor constraintEqualToConstant:0];
+
   [NSLayoutConstraint activateConstraints:@[
     self.locationBarBottomConstraint,
     self.locationBarHeight,
+    self.locationBarExtraBottomPadding,
+    [self.extraPaddingGuide.bottomAnchor
+        constraintEqualToAnchor:self.bottomAnchor],
   ]];
   [self.contractedConstraints addObjectsFromArray:@[
     [self.locationBarContainer.trailingAnchor
