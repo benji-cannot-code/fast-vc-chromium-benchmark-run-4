7754fd55c438f31f9188873b35d4e9a29a3a4ac1
dpapad <dpapad@chromium.org>
2018-08-01 01:45:40 +0000

    WebUI: Make WebUIPolymer2 flag work for optimize_webui=true, part 3.
    In this CL updating:
     - chrome://history
     - chrome://print
     - chrome://settings
    
    This adds some additional size to the binary, since both Polymer 1 and 2 HTML
    bundles are included. This is temporary and the Polymer 1 version will be
    removed once the migration is completed
    
    Bug: 862472
    Change-Id: Ia77a01b730630ab04a34483c7f0a53358501ceed
    Reviewed-on: https://chromium-review.googlesource.com/1155834
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579663}

--- chrome/browser/ui/webui/print_preview/print_preview_ui.cc
+++ chrome/browser/ui/webui/print_preview/print_preview_ui.cc
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/url_data_source.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui_data_source.h"
+#include "content/public/common/content_features.h"
 #include "extensions/common/constants.h"
 #include "printing/buildflags/buildflags.h"
 #include "printing/page_size_margins.h"
@@ -529,6 +530,10 @@ content::WebUIDataSource* CreateNewPrintPreviewUISource(Profile* profile) {
 #if BUILDFLAG(OPTIMIZE_WEBUI)
   source->AddResourcePath("crisper.js", IDR_PRINT_PREVIEW_CRISPER_JS);
   source->SetDefaultResource(IDR_PRINT_PREVIEW_VULCANIZED_HTML);
+  source->SetDefaultResource(
+      base::FeatureList::IsEnabled(features::kWebUIPolymer2) ?
+          IDR_PRINT_PREVIEW_VULCANIZED_P2_HTML :
+          IDR_PRINT_PREVIEW_VULCANIZED_HTML);
 #else
   for (size_t i = 0; i < kPrintPreviewResourcesSize; ++i) {
     source->AddResourcePath(kPrintPreviewResources[i].name,
