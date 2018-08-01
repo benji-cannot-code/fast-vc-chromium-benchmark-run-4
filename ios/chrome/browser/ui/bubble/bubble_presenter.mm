2e7fc31a64d24af1c430c82ce05868ecc766ff39
Gauthier Ambard <gambard@chromium.org>
2018-08-01 19:48:37 +0000

    Fix bubble presentation for non-UI refresh
    The bubble presentation of UI Refresh tips in non-UI Refresh app should
    be a no-op.
    
    Bug: 869400
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I308c2a5dee2088af8b6ca56567a7089944f8bf34
    Reviewed-on: https://chromium-review.googlesource.com/1156694
    Commit-Queue: edchin <edchin@chromium.org>
    Reviewed-by: edchin <edchin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579904}

--- ios/chrome/browser/ui/bubble/bubble_presenter.mm
+++ ios/chrome/browser/ui/bubble/bubble_presenter.mm
@@ -174,6 +174,9 @@ const CGFloat kBubblePresentationDelay = 1;
 }
 
 - (void)presentLongPressBubble {
+  if (!IsUIRefreshPhase1Enabled())
+    return;
+
   if (self.longPressToolbarTipBubblePresenter.isUserEngaged)
     return;
 
@@ -232,7 +235,7 @@ presentBubbleForFeature:(const base::Feature&)feature
 // Presents a bubble associated with the bottom toolbar tip in-product help
 // promotion. This method requires that |self.browserState| is not NULL.
 - (void)presentBottomToolbarTipBubble {
-  if (!IsSplitToolbarMode())
+  if (!IsUIRefreshPhase1Enabled() || !IsSplitToolbarMode())
     return;
 
   if (![self canPresentBubble])
