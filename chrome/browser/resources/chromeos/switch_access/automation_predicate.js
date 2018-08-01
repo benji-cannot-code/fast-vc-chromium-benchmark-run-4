98a83571d77e69d251b718f9e6c065764f77bbbc
Zach Helfinstein <zhelfins@chromium.org>
2018-08-01 23:19:14 +0000

    Rename Switch Access method for clarity
    Rename "isInteresting" to "isActionable" for precision.
    
    Bug: None
    Change-Id: I40d6e2c74b80a0717f0c2b864010d5650bd69378
    Reviewed-on: https://chromium-review.googlesource.com/1159179
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Zach Helfinstein <zhelfins@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579988}

--- chrome/browser/resources/chromeos/switch_access/automation_predicate.js
+++ chrome/browser/resources/chromeos/switch_access/automation_predicate.js
@@ -20,7 +20,7 @@ function AutomationPredicate() {}
  * @return {boolean}
  */
 AutomationPredicate.isSubtreeLeaf = function(node, scope) {
-  return AutomationPredicate.isInteresting(node) ||
+  return AutomationPredicate.isActionable(node) ||
       AutomationPredicate.isGroup(node, scope);
 };
 
@@ -81,7 +81,7 @@ AutomationPredicate.hasSameLocation_ = function(node1, node2) {
  */
 AutomationPredicate.isInterestingSubtree = function(node) {
   let children = node.children || [];
-  return AutomationPredicate.isInteresting(node) ||
+  return AutomationPredicate.isActionable(node) ||
       children.some(AutomationPredicate.isInterestingSubtree);
 };
 
@@ -92,7 +92,7 @@ AutomationPredicate.isInterestingSubtree = function(node) {
  * @param {!chrome.automation.AutomationNode} node
  * @return {boolean}
  */
-AutomationPredicate.isInteresting = function(node) {
+AutomationPredicate.isActionable = function(node) {
   let loc = node.location;
   let parent = node.parent;
   let root = node.root;
