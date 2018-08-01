810da271c1fc905f9105ff2c8daf6fcc29b6254c
dpapad <dpapad@chromium.org>
2018-08-01 01:36:00 +0000

    WebUI: Make WebUIPolymer2 flag work for optimize_webui=true, part 2.
    In this CL updating:
     - chrome://bookmarks
     - chrome://downloads
     - chrome://extensions
    
    This adds some additional size to the binary, since both Polymer 1 and 2 HTML
    bundles are included. This is temporary and the Polymer 1 version will be
    removed once the migration is completed
    
    Bug: 862472
    Change-Id: I55e0edbd4e9269432e2494aadb3d0abae146b43b
    Reviewed-on: https://chromium-review.googlesource.com/1155827
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Scott Chen <scottchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579661}

--- chrome/browser/ui/webui/md_bookmarks/md_bookmarks_ui.cc
+++ chrome/browser/ui/webui/md_bookmarks/md_bookmarks_ui.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/strings/grit/components_strings.h"
 #include "content/public/browser/web_ui.h"
 #include "content/public/browser/web_ui_data_source.h"
+#include "content/public/common/content_features.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
 
@@ -136,7 +137,10 @@ content::WebUIDataSource* CreateMdBookmarksUIHTMLSource(Profile* profile) {
                           IDR_MD_BOOKMARKS_IMAGES_FOLDER_SVG);
 #if BUILDFLAG(OPTIMIZE_WEBUI)
   source->AddResourcePath("crisper.js", IDR_MD_BOOKMARKS_CRISPER_JS);
-  source->SetDefaultResource(IDR_MD_BOOKMARKS_VULCANIZED_HTML);
+  source->SetDefaultResource(
+      base::FeatureList::IsEnabled(features::kWebUIPolymer2)
+          ? IDR_MD_BOOKMARKS_VULCANIZED_P2_HTML
+          : IDR_MD_BOOKMARKS_VULCANIZED_HTML);
   source->UseGzip({"images/folder_open.svg", "images/folder.svg"});
 #else
   source->AddResourcePath("actions.html", IDR_MD_BOOKMARKS_ACTIONS_HTML);
