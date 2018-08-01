346e78612ef62a9a8927f9482858d11cccf4f0e0
Rune Lillesveen <futhark@chromium.org>
2018-08-01 12:18:33 +0000

    Don't use Bloom filter for layout tree rebuild.
    Layout tree rebuild and re-attachment now only matches selectors for
    ::first-letter. It's not worth populating the Bloom filter with
    ancestor attributes just for optimizing ::first-letter matching.
    
    Bug: 843520, 836126
    Change-Id: I22c3a4d6518314ded66ef9b214db53c3ff16aac2
    Reviewed-on: https://chromium-review.googlesource.com/1158230
    Reviewed-by: Anders Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579774}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -2115,8 +2115,6 @@ void Element::AttachLayoutTree(AttachContext& context) {
     rare_data->ClearPseudoElements();
   }
 
-  SelectorFilterParentScope filter_scope(*this);
-
   AttachContext children_context(context);
 
   LayoutObject* layout_object = GetLayoutObject();
@@ -2501,7 +2499,6 @@ void Element::RebuildLayoutTree(WhitespaceAttacher& whitespace_attacher) {
     whitespace_attacher.DidReattachElement(this,
                                            reattach_context.previous_in_flow);
   } else {
-    SelectorFilterParentScope filter_scope(*this);
     // We create a local WhitespaceAttacher when rebuilding children of an
     // element with a LayoutObject since whitespace nodes do not rely on layout
     // objects further up the tree. Also, if this Element's layout object is an
