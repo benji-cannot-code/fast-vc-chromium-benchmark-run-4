3f58d09ef076c7da82b0c84a217d1e067d92fa34
Alexander Alekseev <alemate@chromium.org>
2018-08-01 23:00:34 +0000

    ChromeOS OOBE: Make sure all oobe-dialog receive onBeforeShow event.
    This CL routes onBeforeShow event to the oobe-dialogs, that are not subnodes
    of another Polymer object.
    
    Bug: 828999
    Change-Id: Id2cee750d2364e53052133bbea7d27ea2c1ca0a2
    Reviewed-on: https://chromium-review.googlesource.com/1157912
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579977}

--- chrome/browser/resources/chromeos/assistant_optin/assistant_get_more.js
+++ chrome/browser/resources/chromeos/assistant_optin/assistant_get_more.js
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 Polymer({
   is: 'assistant-get-more',
 
+  behaviors: [OobeDialogHostBehavior],
+
   properties: {
     /**
      * Buttons are disabled when the page content is loading.
--- chrome/browser/resources/chromeos/assistant_optin/assistant_value_prop.js
+++ chrome/browser/resources/chromeos/assistant_optin/assistant_value_prop.js
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 Polymer({
   is: 'assistant-value-prop',
 
+  behaviors: [OobeDialogHostBehavior],
+
   properties: {
     /**
      * Buttons are disabled when the webview content is loading.
