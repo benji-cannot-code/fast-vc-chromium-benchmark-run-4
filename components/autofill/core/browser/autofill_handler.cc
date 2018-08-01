8eeea38e452351a537098c016e88f6609353a40d
Oscar Johansson <oscarj@opera.com>
2018-08-01 11:44:57 +0000

    Renaming conflicting variables names (components/autofill)
    When building using jumbo, files get merged and variables
    and functions with the same name may end up in the same
    namespace/scope and conflict.
    
    This commit solves the issues in components/autofill
    by renaming the conflicting functions and variables to
    something more file specific.
    
    Bug: 869381
    Change-Id: Ifccae861ca7379346fb0939113ac227b770f9640
    Reviewed-on: https://chromium-review.googlesource.com/1158235
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Commit-Queue: Oscar Johansson <oscarj@opera.com>
    Cr-Commit-Position: refs/heads/master@{#579768}

--- components/autofill/core/browser/autofill_handler.cc
+++ components/autofill/core/browser/autofill_handler.cc
@@ -17,7 +17,7 @@ namespace {
 
 // Set a conservative upper bound on the number of forms we are willing to
 // cache, simply to prevent unbounded memory consumption.
-const size_t kMaxFormCacheSize = 100;
+const size_t kAutofillHandlerMaxFormCacheSize = 100;
 
 }  // namespace
 
@@ -173,7 +173,7 @@ bool AutofillHandler::ParseForm(const FormData& form,
                                 const FormStructure* cached_form,
                                 FormStructure** parsed_form_structure) {
   DCHECK(parsed_form_structure);
-  if (form_structures_.size() >= kMaxFormCacheSize)
+  if (form_structures_.size() >= kAutofillHandlerMaxFormCacheSize)
     return false;
 
   auto form_structure = std::make_unique<FormStructure>(form);
