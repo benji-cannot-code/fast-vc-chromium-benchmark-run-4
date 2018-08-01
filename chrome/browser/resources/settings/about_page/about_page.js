ab963fbe62ebcea5c5944f2d6c2e5168a40d0089
Renee Wright <rjwright@chromium.org>
2018-08-01 04:53:18 +0000

    Add a special about-page license message for when Crostini is installed
    This message is also supposed to link to
    /run/imageloader/cros-termina/<version>/about_os_credits.html on the
    CrOS machine, but that is not present unless Termina is running. I
    think that part will be more involved, so I want to do it in a second
    CL.
    
    Bug: 858832
    Change-Id: I1ab11af69de6434ddcfacd4987951a05e50b3889
    Reviewed-on: https://chromium-review.googlesource.com/1143169
    Commit-Queue: Renée Wright <rjwright@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579703}

--- chrome/browser/resources/settings/about_page/about_page.js
+++ chrome/browser/resources/settings/about_page/about_page.js
@@ -43,6 +43,15 @@ Polymer({
 
     /** @private */
     hasEndOfLife_: Boolean,
+
+    /** @private */
+    showCrostini: Boolean,
+
+    /** @private */
+    showCrostiniLicense_: {
+      type: Boolean,
+      value: false,
+    },
     // </if>
 
     // <if expr="_google_chrome and is_macosx">
@@ -134,6 +143,7 @@ Polymer({
         'currentChannel_)',
     'updateShowButtonContainer_(' +
         'showRelaunch_, showRelaunchAndPowerwash_, showCheckUpdates_)',
+    'handleCrostiniEnabledChanged_(prefs.crostini.enabled.value)',
     // </if>
   ],
 
@@ -512,6 +522,26 @@ Polymer({
         !this.hasCheckedForUpdates_ && this.checkStatus_(UpdateStatus.UPDATED);
 
     return staleUpdatedStatus || this.checkStatus_(UpdateStatus.FAILED);
+  },
+
+  /**
+   * @param {boolean} showCrostiniLicense True if Crostini is enabled and
+   * Crostini UI is allowed.
+   * @return {string}
+   * @private
+   */
+  getAboutProductOsLicense_: function(showCrostiniLicense) {
+    return showCrostiniLicense ?
+        this.i18nAdvanced('aboutProductOsWithLinuxLicense') :
+        this.i18nAdvanced('aboutProductOsLicense');
+  },
+
+  /**
+   * @param {boolean} enabled True if Crostini is enabled.
+   * @private
+   */
+  handleCrostiniEnabledChanged_: function(enabled) {
+    this.showCrostiniLicense_ = enabled && this.showCrostini;
   },
 
   /**
