323475bccdffdf7168c66a6fc73acb25fbfea325
Adrienne Walker <enne@chromium.org>
2018-08-01 20:02:33 +0000

    gpu: Early out earlier for bad target in RasterDecoder
    GetLevelSize calls GLTargetToFaceIndex which asserts if the target is bogus, so move
    the early out before this call.
    
    Bug: 868242
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I24a6262e2f7cf3ebf70c814bfca818365ec6c242
    Reviewed-on: https://chromium-review.googlesource.com/1158801
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579909}

--- gpu/command_buffer/service/raster_decoder.cc
+++ gpu/command_buffer/service/raster_decoder.cc
@@ -3001,6 +3001,13 @@ void RasterDecoderImpl::DoBeginRasterCHROMIUM(
   DCHECK(!raster_canvas_);
   raster_decoder_context_state_->need_context_state_reset = true;
 
+  if (texture->target() != GL_TEXTURE_2D &&
+      texture->target() != GL_TEXTURE_RECTANGLE_ARB) {
+    LOCAL_SET_GL_ERROR(GL_INVALID_OPERATION, "glBeginRasterCHROMIUM",
+                       "invalid texture target");
+    return;
+  }
+
   // This function should look identical to
   // ResourceProvider::ScopedSkSurfaceProvider.
   GrGLTextureInfo texture_info;
@@ -3021,13 +3028,6 @@ void RasterDecoderImpl::DoBeginRasterCHROMIUM(
   }
   texture_info.fID = texture->service_id();
   texture_info.fTarget = texture->target();
-
-  if (texture->target() != GL_TEXTURE_2D &&
-      texture->target() != GL_TEXTURE_RECTANGLE_ARB) {
-    LOCAL_SET_GL_ERROR(GL_INVALID_OPERATION, "glBeginRasterCHROMIUM",
-                       "invalid texture target");
-    return;
-  }
 
   // GetInternalFormat may return a base internal format but Skia requires a
   // sized internal format. So this may be adjusted below.
