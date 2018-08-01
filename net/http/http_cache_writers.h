71e4d47d51f17e56d1b0b1ba1b4f2b1c5b4ca2a8
Bence Béky <bnc@chromium.org>
2018-08-01 22:07:57 +0000

    Use CompletionOnceCallback in struct WaitingForRead.
    Since CompletionOnceCallback is non-copyable, therefore WaitingForRead
    becomes non-copyable.  This Cl adds a move constructor so that
    WaitingForRead can be put in a std::map.
    
    Bug: 807724
    Change-Id: I57dce65d46626ce0c39cd3e2e738ad9d6979165c
    Reviewed-on: https://chromium-review.googlesource.com/1156862
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Bence Béky <bnc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579961}

--- net/http/http_cache_writers.h
+++ net/http/http_cache_writers.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/weak_ptr.h"
 #include "net/base/completion_callback.h"
+#include "net/base/completion_once_callback.h"
 #include "net/http/http_cache.h"
 
 namespace net {
@@ -165,12 +166,12 @@ class NET_EXPORT_PRIVATE HttpCache::Writers {
     scoped_refptr<IOBuffer> read_buf;
     int read_buf_len;
     int write_len;
-    const CompletionCallback callback;
+    CompletionOnceCallback callback;
     WaitingForRead(scoped_refptr<IOBuffer> read_buf,
                    int len,
-                   const CompletionCallback& consumer_callback);
+                   CompletionOnceCallback consumer_callback);
     ~WaitingForRead();
-    WaitingForRead(const WaitingForRead&);
+    WaitingForRead(WaitingForRead&&);
   };
   using WaitingForReadMap = std::map<Transaction*, WaitingForRead>;
 
