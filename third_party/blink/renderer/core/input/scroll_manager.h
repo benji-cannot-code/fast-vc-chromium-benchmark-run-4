8c25e43c9e87813e81deb3f8d370dbda6bf41a9f
Sandra Sun <sunyunjia@chromium.org>
2018-08-02 19:25:43 +0000

    Check nullptrs for SnapFling in ScrollManager.
    The previous code doesn't check nullptrs thoroughly, causing
    clusterfuzz pages to crash. This patch adds the checks for those cases
    to make sure they don't crash.
    
    Bug: 863338
    Change-Id: Ibed005057fa376cb65200ad51e6dbb16bafa2c6a
    Reviewed-on: https://chromium-review.googlesource.com/1142457
    Commit-Queue: Sandra Sun <sunyunjia@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Majid Valipour <majidvp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580281}

--- third_party/blink/renderer/core/input/scroll_manager.h
+++ third_party/blink/renderer/core/input/scroll_manager.h
@@ -28,7 +28,6 @@ class LocalFrame;
 class PaintLayer;
 class PaintLayerScrollableArea;
 class Page;
-class ScrollableArea;
 class Scrollbar;
 class ScrollState;
 class WebGestureEvent;
@@ -141,7 +140,6 @@ class CORE_EXPORT ScrollManager
   void NotifyScrollPhaseEndForCustomizedScroll();
 
   LayoutBox* LayoutBoxForSnapping() const;
-  ScrollableArea* ScrollableAreaForSnapping() const;
 
   // NOTE: If adding a new field to this class please ensure that it is
   // cleared in |ScrollManager::clear()|.
