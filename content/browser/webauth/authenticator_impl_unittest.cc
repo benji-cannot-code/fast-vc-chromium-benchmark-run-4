cc098b23042f22aea42c8164b9460df49ede9591
Martin Kreichgauer <martinkr@google.com>
2018-08-02 00:47:07 +0000

    device/fido: fix a crash in IsUVPAA if Touch ID is flag-enabled.
    In 9aa7c54540178d4bc52f6ffc87567d76beb99094
    AuthenticatorImpl::IsUserVerifyingPlatformAuthenticatorAvailable was
    changed to make Touch ID unavailable in Incognito and if the embedder
    does not provide a configuration for the Touch ID authenticator. The
    latter check dereferenced a nullptr in AuthenticatorImpl's
    request_delegate_.  request_delegate_ only gets initialized for the
    duration of a MakeCredential or GetAssertion request. It is
    uninitialized during IsUVPAA.
    
    This works around the issue by adding a
    IsWebAuthenticationTouchIdAuthenticatorSupported instance method to
    ContentBrowserClient, so that the feature support can be checked without
    requiring a request delegate instance.
    
    Bug: 869651
    Change-Id: Ib64a2278d63047d68876e224d44a9aab96f9cfea
    Reviewed-on: https://chromium-review.googlesource.com/1157605
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Kim Paulhamus <kpaulhamus@chromium.org>
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/master@{#580014}

--- content/browser/webauth/authenticator_impl_unittest.cc
+++ content/browser/webauth/authenticator_impl_unittest.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/test_mock_time_task_runner.h"
 #include "base/time/tick_clock.h"
 #include "base/time/time.h"
+#include "build/build_config.h"
 #include "components/cbor/cbor_reader.h"
 #include "components/cbor/cbor_values.h"
 #include "content/public/browser/authenticator_request_client_delegate.h"
@@ -196,6 +197,7 @@ constexpr OriginClaimedAuthorityPair kInvalidRelyingPartyTestCases[] = {
     {"https://com", "https://www.gstatic.com/securitykey/origins.json"},
 };
 
+using TestIsUvpaaCallback = device::test::ValueCallbackReceiver<bool>;
 using TestMakeCredentialCallback = device::test::StatusAndValueCallbackReceiver<
     AuthenticatorStatus,
     MakeCredentialAuthenticatorResponsePtr>;
@@ -1103,6 +1105,14 @@ class TestAuthenticatorContentBrowserClient : public ContentBrowserClient {
         individual_attestation, attestation_consent, is_focused);
   }
 
+#if defined(OS_MACOSX)
+  bool IsWebAuthenticationTouchIdAuthenticatorSupported() override {
+    return supports_touch_id;
+  }
+
+  bool supports_touch_id = true;
+#endif
+
   // If set, this closure will be called when the subsequently constructed
   // delegate is informed that the request has started.
   base::OnceClosure request_started_callback;
@@ -1496,6 +1506,20 @@ TEST_F(AuthenticatorContentBrowserClientTest,
     EXPECT_EQ(AuthenticatorStatus::PENDING_REQUEST, cb.status());
   }
 }
+
+#if defined(OS_MACOSX)
+TEST_F(AuthenticatorContentBrowserClientTest, IsUVPAAFalseWithoutTouchId) {
+  test_client_.supports_touch_id = false;
+
+  NavigateAndCommit(GURL(kTestOrigin1));
+  AuthenticatorPtr authenticator = ConnectToAuthenticator();
+
+  TestIsUvpaaCallback cb;
+  authenticator->IsUserVerifyingPlatformAuthenticatorAvailable(cb.callback());
+  cb.WaitForCallback();
+  EXPECT_FALSE(cb.value());
+}
+#endif
 
 class MockAuthenticatorRequestDelegateObserver
     : public TestAuthenticatorRequestDelegate {
