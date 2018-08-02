f02357de2e7335f06daa3bfc5f5fe60d985970e5
Evan Stade <estade@chromium.org>
2018-08-02 01:17:22 +0000

    OopAsh: make browser frame respond to changes in tablet mode.
    Tested via --ash-debug-shortcuts and Ctrl+Alt+Shift+T
    
    Bug: 854704,760811
    Change-Id: Ia3aadae89fcd50d95a84fdf7d275e97bc2e9e2e9
    Reviewed-on: https://chromium-review.googlesource.com/1152230
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580021}

--- ash/shell_test_api.cc
+++ ash/shell_test_api.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "ash/system/power/backlights_forced_off_setter.h"
 #include "ash/system/power/power_button_controller.h"
+#include "ash/wm/tablet_mode/tablet_mode_controller.h"
 #include "components/prefs/testing_pref_service.h"
 #include "mojo/public/cpp/bindings/strong_binding.h"
 
@@ -70,6 +71,10 @@ void ShellTestApi::SimulateModalWindowOpenForTest(bool modal_window_open) {
 
 void ShellTestApi::IsSystemModalWindowOpen(IsSystemModalWindowOpenCallback cb) {
   std::move(cb).Run(Shell::IsSystemModalWindowOpen());
+}
+
+void ShellTestApi::EnableTabletModeWindowManager(bool enable) {
+  shell_->tablet_mode_controller()->EnableTabletModeWindowManager(enable);
 }
 
 }  // namespace ash
--- chrome/browser/ui/views/frame/immersive_mode_controller_ash.cc
+++ chrome/browser/ui/views/frame/immersive_mode_controller_ash.cc
@@ -171,10 +171,6 @@ void ImmersiveModeControllerAsh::OnFindBarVisibleBoundsChanged(
 }
 
 bool ImmersiveModeControllerAsh::ShouldStayImmersiveAfterExitingFullscreen() {
-  // TODO(crbug.com/760811): Support tablet mode in mash.
-  if (!features::IsAshInBrowserProcess())
-    return false;
-
   return !browser_view_->IsBrowserTypeNormal() &&
          TabletModeClient::Get()->tablet_mode_enabled();
 }
@@ -189,11 +185,8 @@ void ImmersiveModeControllerAsh::OnWidgetActivationChanged(
   if (browser_view_->IsBrowserTypeNormal())
     return;
 
-  // TODO(crbug.com/760811): Support tablet mode in mash.
-  if (!features::IsAshInBrowserProcess() ||
-      !TabletModeClient::Get()->tablet_mode_enabled()) {
+  if (!TabletModeClient::Get()->tablet_mode_enabled())
     return;
-  }
 
   // Enable immersive mode if the widget is activated. Do not disable immersive
   // mode if the widget deactivates, but is not minimized.
