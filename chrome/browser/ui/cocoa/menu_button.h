3b442268e50a528228b5f3eabc74ea3d56695a3f
Avi Drissman <avi@chromium.org>
2018-08-02 02:41:52 +0000

    Work around Mojave crash.
    NSPopUpButtonCell in Mojave calls through the parent view at all
    times, even during destruction. Work around this by properly
    scoping the lifetime of the NSPopUpButtonCell in MenuButton, which
    is something we weren't able to do in 10.5 but that was fixed
    in 10.6 but that we never got around to updating.
    
    BUG=869844
    
    Change-Id: Iaecd978bc410810f02276f21ba13dd20ca73b195
    Reviewed-on: https://chromium-review.googlesource.com/1159191
    Commit-Queue: Sidney San Martín <sdy@chromium.org>
    Reviewed-by: Sidney San Martín <sdy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580032}

--- chrome/browser/ui/cocoa/menu_button.h
+++ chrome/browser/ui/cocoa/menu_button.h
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   BOOL openMenuOnClick_;
   BOOL openMenuOnRightClick_;
   BOOL openMenuOnClickHold_;
-  base::scoped_nsobject<NSPopUpButtonCell> popUpCell_;
 }
 
 // The menu to display. Note that it should have no (i.e., a blank) title and
