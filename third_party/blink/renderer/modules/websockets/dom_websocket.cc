30ab34666b79594cedf2f4cd5f384a7811d107a6
Adam Rice <ricea@chromium.org>
2018-08-02 05:27:18 +0000

    Update buffered amount when async callbacks are called
    If an asynchronous callback is called, it means we must have returned to
    the start of the event loop. Ensure that any consumed bufferedAmount is
    reflected in that case. Do not reflect bufferedAmount if the EventQueue
    is paused, as that means that we may be in a nested event loop.
    
    Add a unit test for this case. Also add a unit test for normal
    bufferedAmount behaviour, as there wasn't one.
    
    Add a web platform test for what happens if a sync XHR is performed
    between calling send() and looking at bufferedAmount.
    
    BUG=856651
    
    Change-Id: Iafa2d619a1eb5284b64500ac03d336fb6380193b
    Reviewed-on: https://chromium-review.googlesource.com/1151086
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580078}

--- third_party/blink/renderer/modules/websockets/dom_websocket.cc
+++ third_party/blink/renderer/modules/websockets/dom_websocket.cc
@@ -101,7 +101,7 @@ bool DOMWebSocket::EventQueue::IsEmpty() const {
 }
 
 void DOMWebSocket::EventQueue::Pause() {
-  if (state_ != kActive && state_ != kUnpausePosted)
+  if (state_ == kStopped || state_ == kPaused)
     return;
 
   state_ = kPaused;
@@ -124,6 +124,10 @@ void DOMWebSocket::EventQueue::ContextDestroyed() {
 
   state_ = kStopped;
   events_.clear();
+}
+
+bool DOMWebSocket::EventQueue::IsPaused() {
+  return state_ == kPaused || state_ == kUnpausePosted;
 }
 
 void DOMWebSocket::EventQueue::DispatchQueuedEvents() {
@@ -392,15 +396,25 @@ void DOMWebSocket::UpdateBufferedAmountAfterClose(uint64_t payload_size) {
   LogError("WebSocket is already in CLOSING or CLOSED state.");
 }
 
+void DOMWebSocket::PostBufferedAmountUpdateTask() {
+  if (buffered_amount_update_task_pending_)
+    return;
+  buffered_amount_update_task_pending_ = true;
+  GetExecutionContext()
+      ->GetTaskRunner(TaskType::kWebSocket)
+      ->PostTask(FROM_HERE, WTF::Bind(&DOMWebSocket::BufferedAmountUpdateTask,
+                                      WrapWeakPersistent(this)));
+}
+
 void DOMWebSocket::BufferedAmountUpdateTask() {
   buffered_amount_update_task_pending_ = false;
   ReflectBufferedAmountConsumption();
 }
 
 void DOMWebSocket::ReflectBufferedAmountConsumption() {
+  if (event_queue_->IsPaused())
+    return;
   DCHECK_GE(buffered_amount_, consumed_buffered_amount_);
-  // Cast to unsigned long long is required since clang doesn't accept
-  // combination of %llu and uint64_t (known as unsigned long).
   NETWORK_DVLOG(1) << "WebSocket " << this
                    << " reflectBufferedAmountConsumption() " << buffered_amount_
                    << " => " << (buffered_amount_ - consumed_buffered_amount_);
@@ -654,6 +668,11 @@ void DOMWebSocket::Pause() {
 
 void DOMWebSocket::Unpause() {
   event_queue_->Unpause();
+
+  // If |consumed_buffered_amount_| was updated while the object was paused then
+  // the changes to |buffered_amount_| will not yet have been applied. Post
+  // another task to update it.
+  PostBufferedAmountUpdateTask();
 }
 
 void DOMWebSocket::DidConnect(const String& subprotocol,
@@ -670,7 +689,7 @@ void DOMWebSocket::DidConnect(const String& subprotocol,
 void DOMWebSocket::DidReceiveTextMessage(const String& msg) {
   NETWORK_DVLOG(1) << "WebSocket " << this
                    << " DidReceiveTextMessage() Text message " << msg;
-
+  ReflectBufferedAmountConsumption();
   DCHECK_NE(state_, kConnecting);
   if (state_ != kOpen)
     return;
@@ -684,7 +703,7 @@ void DOMWebSocket::DidReceiveBinaryMessage(
     std::unique_ptr<Vector<char>> binary_data) {
   NETWORK_DVLOG(1) << "WebSocket " << this << " DidReceiveBinaryMessage() "
                    << binary_data->size() << " byte binary message";
-
+  ReflectBufferedAmountConsumption();
   DCHECK(!origin_string_.IsNull());
 
   DCHECK_NE(state_, kConnecting);
@@ -720,6 +739,7 @@ void DOMWebSocket::DidReceiveBinaryMessage(
 
 void DOMWebSocket::DidError() {
   NETWORK_DVLOG(1) << "WebSocket " << this << " DidError()";
+  ReflectBufferedAmountConsumption();
   state_ = kClosed;
   event_queue_->Dispatch(Event::Create(EventTypeNames::error));
 }
@@ -731,16 +751,12 @@ void DOMWebSocket::DidConsumeBufferedAmount(uint64_t consumed) {
   if (state_ == kClosed)
     return;
   consumed_buffered_amount_ += consumed;
-  if (buffered_amount_update_task_pending_)
-    return;
-  GetExecutionContext()
-      ->GetTaskRunner(TaskType::kWebSocket)
-      ->PostTask(FROM_HERE, WTF::Bind(&DOMWebSocket::BufferedAmountUpdateTask,
-                                      WrapWeakPersistent(this)));
+  PostBufferedAmountUpdateTask();
 }
 
 void DOMWebSocket::DidStartClosingHandshake() {
   NETWORK_DVLOG(1) << "WebSocket " << this << " DidStartClosingHandshake()";
+  ReflectBufferedAmountConsumption();
   state_ = kClosing;
 }
 
@@ -749,6 +765,7 @@ void DOMWebSocket::DidClose(
     unsigned short code,
     const String& reason) {
   NETWORK_DVLOG(1) << "WebSocket " << this << " DidClose()";
+  ReflectBufferedAmountConsumption();
   if (!channel_)
     return;
   bool all_data_has_been_consumed =
--- third_party/blink/renderer/modules/websockets/dom_websocket.h
+++ third_party/blink/renderer/modules/websockets/dom_websocket.h
@@ -169,6 +169,8 @@ class MODULES_EXPORT DOMWebSocket : public EventTargetWithInlineData,
     void Unpause();
     void ContextDestroyed();
 
+    bool IsPaused();
+
     void Trace(blink::Visitor*);
 
    private:
@@ -226,7 +228,17 @@ class MODULES_EXPORT DOMWebSocket : public EventTargetWithInlineData,
   // Updates |buffered_amount_after_close_| given the amount of data passed to
   // send() method after the state changed to CLOSING or CLOSED.
   void UpdateBufferedAmountAfterClose(uint64_t);
+
+  // Causes |buffered_amount_| to be updated asynchronously after returning to
+  // the event loop. Uses |buffered_amount_update_task_pending_| to avoid
+  // posting multiple tasks simultaneously.
+  void PostBufferedAmountUpdateTask();
+
+  // Updates |buffered_amount_| and resets
+  // |buffered_amount_update_task_pending_|.
   void BufferedAmountUpdateTask();
+
+  // Updates |buffered_amount_| provided the object is not currently paused.
   void ReflectBufferedAmountConsumption();
 
   void ReleaseChannel();
