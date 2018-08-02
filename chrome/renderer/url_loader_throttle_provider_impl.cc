66776507214a43bd6a0776fef6cc77c73c28f61b
John Abd-El-Malek <jam@chromium.org>
2018-08-02 04:28:50 +0000

    Apply Google-related request modifications for networking service path.
    This adds support for:
    1) Safe Search
    2) YouTube restricted mode
    3) restricting consumer accounts
    through Group Policy.
    
    The other supporting change is to make ThrottlingURLLoader handle a throttle changing the URL in WillStartRequest.
    
    This is a reland of https://chromium-review.googlesource.com/c/chromium/src/+/1152507 with a fix for the CFI failure in unit_tests.
    
    TBR=kinuko
    
    Bug: 841313
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Change-Id: Iedb413e4cb2837d71d380e5df74d7f80305181cd
    Reviewed-on: https://chromium-review.googlesource.com/1159821
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580063}

--- chrome/renderer/url_loader_throttle_provider_impl.cc
+++ chrome/renderer/url_loader_throttle_provider_impl.cc
@@ -10,9 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/feature_list.h"
 #include "base/message_loop/message_loop_current.h"
+#include "chrome/common/google_url_loader_throttle.h"
 #include "chrome/common/prerender.mojom.h"
 #include "chrome/common/prerender_url_loader_throttle.h"
-#include "chrome/common/variations_header_url_loader_throttle.h"
 #include "chrome/renderer/chrome_content_renderer_client.h"
 #include "chrome/renderer/chrome_render_thread_observer.h"
 #include "chrome/renderer/prerender/prerender_dispatcher.h"
@@ -244,10 +244,12 @@ URLLoaderThrottleProviderImpl::CreateThrottles(
     throttles.push_back(std::move(ad_throttle));
   }
 
-  bool is_off_the_record = ChromeRenderThreadObserver::is_incognito_process();
-  bool is_signed_in = ChromeRenderThreadObserver::is_signed_in();
-  throttles.push_back(std::make_unique<VariationsHeaderURLLoaderThrottle>(
-      is_off_the_record, is_signed_in));
+  throttles.push_back(std::make_unique<GoogleURLLoaderThrottle>(
+      ChromeRenderThreadObserver::is_incognito_process(),
+      ChromeRenderThreadObserver::is_signed_in(),
+      ChromeRenderThreadObserver::force_safe_search(),
+      ChromeRenderThreadObserver::youtube_restrict(),
+      ChromeRenderThreadObserver::allowed_domains_for_apps()));
 
   return throttles;
 }
