7d285598950c3fe65f9bc8cd256a73725a4daf2a
Cathy Li <chili@chromium.org>
2018-08-02 20:32:54 +0000

    [Explore Sites]: Add feature parameters to explore sites flag
    Bug: 867488
    Change-Id: Ibb67a81cfef555e93128ae395fe6a9f0081a45ed
    Reviewed-on: https://chromium-review.googlesource.com/1157183
    Commit-Queue: Cathy Li <chili@chromium.org>
    Reviewed-by: Dmitry Titov <dimich@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580303}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -145,6 +145,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_ANDROID)
 #include "chrome/browser/android/chrome_feature_list.h"
 #include "chrome/browser/android/download/new_net_error_page_feature.h"
+#include "chrome/browser/android/explore_sites/explore_sites_feature.h"
 #else  // OS_ANDROID
 #include "chrome/browser/media/router/media_router_feature.h"
 #include "ui/message_center/public/cpp/features.h"
@@ -1207,6 +1208,12 @@ const FeatureEntry::FeatureVariation kNewNetErrorPageUIVariations[] = {
     {"Content Preview", &kNewNetErrorPageUIContentPreview, 1, nullptr},
     {"Content Preview + Auto download", &kNewNetErrorPageUIContentPreviewAutoDL,
      1, nullptr}};
+
+const FeatureEntry::FeatureParam kExploreSitesExperimental = {
+    chrome::android::explore_sites::kExploreSitesVariationParameterName,
+    chrome::android::explore_sites::kExploreSitesVariationExperimental};
+const FeatureEntry::FeatureVariation kExploreSitesVariations[] = {
+    {"Experimental", &kExploreSitesExperimental, 1, nullptr}};
 #endif  // defined(OS_ANDROID)
 
 // RECORDING USER METRICS FOR FLAGS:
@@ -1381,7 +1388,9 @@ const FeatureEntry kFeatureEntries[] = {
      FEATURE_VALUE_TYPE(chrome::android::kContextualSearchUnityIntegration)},
     {"explore-sites", flag_descriptions::kExploreSitesName,
      flag_descriptions::kExploreSitesDescription, kOsAndroid,
-     FEATURE_VALUE_TYPE(chrome::android::kExploreSites)},
+     FEATURE_WITH_PARAMS_VALUE_TYPE(chrome::android::kExploreSites,
+                                    kExploreSitesVariations,
+                                    "ExploreSites")},
 #endif  // OS_ANDROID
     {"show-autofill-type-predictions",
      flag_descriptions::kShowAutofillTypePredictionsName,
---
+++ chrome/browser/android/explore_sites/explore_sites_feature.cc
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/android/explore_sites/explore_sites_feature.h"
+
+#include "base/feature_list.h"
+#include "base/metrics/field_trial_params.h"
+#include "chrome/browser/android/chrome_feature_list.h"
+
+namespace chrome {
+namespace android {
+namespace explore_sites {
+
+const char kExploreSitesVariationParameterName[] = "variation";
+
+const char kExploreSitesVariationExperimental[] = "experiment";
+
+ExploreSitesVariation GetExploreSitesVariation() {
+  if (base::FeatureList::IsEnabled(kExploreSites)) {
+    if (base::GetFieldTrialParamValueByFeature(
+            kExploreSites, kExploreSitesVariationParameterName) ==
+        kExploreSitesVariationExperimental) {
+      return ExploreSitesVariation::EXPERIMENT;
+    }
+    return ExploreSitesVariation::ENABLED;
+  }
+  return ExploreSitesVariation::DISABLED;
+}
+
+}  // namespace explore_sites
+}  // namespace android
+}  // namespace chrome
