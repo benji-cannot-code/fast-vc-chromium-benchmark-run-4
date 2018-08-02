53a0a605f7f8354842d76425020157e9b5fbf805
Olivier Li <olivierli@chromium.org>
2018-08-02 14:10:22 +0000

    Add logging and chrome_utils directories
    Bug: 830892
    Change-Id: I8c121dff724dea526a7cc7ce198da51cc1b8d045
    Reviewed-on: https://chromium-review.googlesource.com/1158719
    Reviewed-by: Joe Mason <joenotcharles@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Oliver Li <olivierli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580163}

---
+++ chrome/chrome_cleaner/logging/dummy_api_keys.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/chrome_cleaner/logging/api_keys.h"
+
+namespace chrome_cleaner {
+
+const char* kSafeBrowsingCleanerUrl =
+    "https://sb-ssl.google.com/safebrowsing/clientreport/chrome-cct"
+    "?key=DUMMY_KEY";
+
+const char* kSafeBrowsingReporterUrl =
+    "https://sb-ssl.google.com/safebrowsing/clientreport/chrome-sw-reporter"
+    "?key=DUMMY_KEY";
+
+}  // namespace chrome_cleaner
