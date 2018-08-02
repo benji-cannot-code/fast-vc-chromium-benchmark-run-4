a953380170ee912fc005a743ad6a0deadb9e263e
Darren Shen <shend@chromium.org>
2018-08-02 05:27:49 +0000

    [VK] Remove extraneous calls to NotifyKeyboardBoundsChanged.
    In a previous CL, we made it so that changes to the keyboard window
    bounds automatically call NotifyKeyboardBoundsChanged. Now we can
    remove these extraneous calls and make NotifyKeyboardBoundsChanged
    private.
    
    TBR=jamescook@chromium.org
    
    Bug: 845780
    Change-Id: I191a22fc50e37a3c57552169f05f609f4fba3005
    Reviewed-on: https://chromium-review.googlesource.com/1156114
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580079}

--- ui/keyboard/keyboard_controller.h
+++ ui/keyboard/keyboard_controller.h
@@ -108,10 +108,6 @@ class KEYBOARD_EXPORT KeyboardController
   // loaded yet.
   void Reload();
 
-  // Notifies observers that the visual or occluded bounds of the keyboard
-  // window are changing.
-  void NotifyKeyboardBoundsChanging(const gfx::Rect& new_bounds);
-
   // Management of the observer list.
   void AddObserver(KeyboardControllerObserver* observer);
   bool HasObserver(KeyboardControllerObserver* observer) const;
@@ -308,6 +304,10 @@ class KEYBOARD_EXPORT KeyboardController
   // Notifies keyboard config change to the observers.
   // Only called from |UpdateKeyboardConfig| in keyboard_util.
   void NotifyKeyboardConfigChanged();
+
+  // Notifies observers that the visual or occluded bounds of the keyboard
+  // window are changing.
+  void NotifyKeyboardBoundsChanging(const gfx::Rect& new_bounds);
 
   // Validates the state transition. Called from ChangeState.
   void CheckStateTransition(KeyboardControllerState prev,
