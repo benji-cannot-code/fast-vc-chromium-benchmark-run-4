73b9194f094915e3d6a27f9b448376b2900daa92
stkhapugin@chromium.org <stkhapugin@chromium.org>
2018-08-02 08:28:25 +0000

    Remove an unused header.
    Removes the omnibox_popup_row.h header left for downstream compilation.
    
    Bug: 814732
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ic7eb3c6f6d2b72496998ce58d7a81fc63614be69
    Reviewed-on: https://chromium-review.googlesource.com/1158576
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580112}

--- ios/chrome/browser/ui/omnibox/omnibox_popup_row.h
+++
@@ -1,7 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// TODO(crbug.com/814732): Remove this file once it's not included downstream.
-#import "ios/chrome/browser/ui/omnibox/popup/omnibox_popup_row.h"
