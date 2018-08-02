300d9ea653062681d12379ff5ad8e7c5772f93d8
Christopher Cameron <ccameron@chromium.org>
2018-08-02 04:31:44 +0000

    Remove need of ICC profiles in the renderer process
    Change zero-copy to specify IOSurface color profiles using
    SetColorSpaceMetadataCHROMIUM rather than calling SetColorSpace
    on the GpuMemoryBuffer directly.
    
    The SetColorSpaceMetadataCHROMIUM path ends up calling
    gl::GLImageIOSurface::SetColorSpace, which uses the
    gfx::DisplayICCProfiles structure to ensure that we use low power
    profiles when possible.
    
    This removes the need for
    - gfx::ICCProfile::FromCacheMac
    - Sending gfx::ICCProfiles from the browser to the renderer in
      the content::ScreenInfo
      - Note that content::ScreenInfo::icc_profile was not ever
        actually read
      - Sending it over IPC would result it being added to the cache
        read by gfx::ICCProfile::FromCacheMac
    - mojo and IPC support of gfx::ICCProfile
    
    Bug: 869570
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I814496a5fffc8da38b4b59d7d2cc055dc47cd52b
    Reviewed-on: https://chromium-review.googlesource.com/1159233
    Commit-Queue: ccameron <ccameron@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580066}

--- ui/gfx/icc_profile_unittest.cc
+++ ui/gfx/icc_profile_unittest.cc
@@ -50,16 +50,6 @@ TEST(ICCProfile, Equality) {
   EXPECT_FALSE(spin_space == adobe_space);
   EXPECT_TRUE(spin_space != adobe_space);
 
-  ICCProfile temp = ICCProfile::FromCacheMac(spin_space);
-  EXPECT_TRUE(temp.IsValid());
-  EXPECT_TRUE(spin_profile == temp);
-  EXPECT_FALSE(spin_profile != temp);
-
-  temp = ICCProfile::FromCacheMac(adobe_space);
-  EXPECT_TRUE(temp.IsValid());
-  EXPECT_FALSE(spin_profile == temp);
-  EXPECT_TRUE(spin_profile != temp);
-
   EXPECT_TRUE(!!spin_space.ToSkColorSpace());
   EXPECT_TRUE(!!adobe_space.ToSkColorSpace());
   EXPECT_FALSE(SkColorSpace::Equals(
@@ -75,12 +65,8 @@ TEST(ICCProfile, ParametricVersusExactInaccurate) {
   ICCProfile multi_tr_fn = ICCProfileForTestingNoAnalyticTrFn();
   EXPECT_TRUE(multi_tr_fn.GetColorSpace().IsParametricAccurate());
 
-  ICCProfile profile;
-  profile = ICCProfile::FromCacheMac(multi_tr_fn.GetColorSpace());
-  EXPECT_TRUE(profile.IsValid());
-  EXPECT_EQ(profile, multi_tr_fn);
-
   // We are capable of generating a parametric approximation.
+  ICCProfile profile;
   profile = ICCProfile::FromParametricColorSpace(multi_tr_fn.GetColorSpace());
   EXPECT_TRUE(profile.IsValid());
   EXPECT_NE(profile, multi_tr_fn);
@@ -93,10 +79,6 @@ TEST(ICCProfile, ParametricVersusExactOvershoot) {
   EXPECT_TRUE(overshoot.GetColorSpace().IsParametricAccurate());
 
   ICCProfile profile;
-  profile = ICCProfile::FromCacheMac(overshoot.GetColorSpace());
-  EXPECT_TRUE(profile.IsValid());
-  EXPECT_EQ(profile, overshoot);
-
   profile = ICCProfile::FromParametricColorSpace(overshoot.GetColorSpace());
   EXPECT_TRUE(profile.IsValid());
   EXPECT_NE(profile, overshoot);
@@ -108,10 +90,6 @@ TEST(ICCProfile, ParametricVersusExactAdobe) {
   EXPECT_TRUE(accurate.GetColorSpace().IsParametricAccurate());
 
   ICCProfile profile;
-  profile = ICCProfile::FromCacheMac(accurate.GetColorSpace());
-  EXPECT_TRUE(profile.IsValid());
-  EXPECT_EQ(profile, accurate);
-
   profile = ICCProfile::FromParametricColorSpace(accurate.GetColorSpace());
   EXPECT_TRUE(profile.IsValid());
   EXPECT_NE(profile, accurate);
