4e8537b72e6e60eec66cc1dfd19a58af9579fd34
Johannes Henkel <johannes@chromium.org>
2018-08-02 17:39:50 +0000

    Rename to agent_state_.ClearAllFields().
    I thought it was nice to name everything Clear, but when I tried
    to use it in one of the pending PRs it felt too much like action
    from a distance to clear the agent state which causes the fields
    to be cleared. So the more explicit name (like you suggested it)
    is better. Sorry about the churn.
    
    Change-Id: I33ab0b02a714dd93c2375ec28327a5eb58748135
    Reviewed-on: https://chromium-review.googlesource.com/1159996
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Johannes Henkel <johannes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580235}

--- third_party/blink/renderer/core/inspector/inspector_session_state.cc
+++ third_party/blink/renderer/core/inspector/inspector_session_state.cc
@@ -107,7 +107,7 @@ void InspectorAgentState::InitFrom(InspectorSessionState* session_state) {
     f->InitFrom(session_state);
 }
 
-void InspectorAgentState::Clear() {
+void InspectorAgentState::ClearAllFields() {
   for (Field* f : fields_)
     f->Clear();
 }
