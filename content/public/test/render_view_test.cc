faab9edacc022231bd530258e0b22e85128b537b
Ross Mcilroy <rmcilroy@chromium.org>
2018-08-02 08:08:24 +0000

    [cleanup] Avoid using deprecated APIs in render_view_test.
    BUG=v8:7786
    
    Change-Id: I2325c003da6db08a72eb442ea4f9641ca62d78ad
    Reviewed-on: https://chromium-review.googlesource.com/1159071
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Ross McIlroy <rmcilroy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580108}

--- content/public/test/render_view_test.cc
+++ content/public/test/render_view_test.cc
@@ -195,7 +195,7 @@ bool RenderViewTest::ExecuteJavaScriptAndReturnIntValue(
     return false;
 
   if (int_result)
-    *int_result = result->Int32Value();
+    *int_result = result.As<v8::Int32>()->Value();
 
   return true;
 }
@@ -210,7 +210,7 @@ bool RenderViewTest::ExecuteJavaScriptAndReturnNumberValue(
     return false;
 
   if (number_result)
-    *number_result = result->NumberValue();
+    *number_result = result.As<v8::Number>()->Value();
 
   return true;
 }
@@ -489,7 +489,7 @@ gfx::Rect RenderViewTest::GetElementBounds(const std::string& element_id) {
     v8::Local<v8::Value> value = array->Get(index);
     if (value.IsEmpty() || !value->IsInt32())
       return gfx::Rect();
-    coords.push_back(value->Int32Value());
+    coords.push_back(value.As<v8::Int32>()->Value());
   }
   return gfx::Rect(coords[0], coords[1], coords[2], coords[3]);
 }
