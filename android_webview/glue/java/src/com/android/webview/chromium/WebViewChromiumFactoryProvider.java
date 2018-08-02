2272f62d583326a5a3a2a7690996690fc401cd38
Changwan Ryu <changwan@google.com>
2018-08-02 21:56:08 +0000

    Fix startup regression and avoid logspam
    TracingController is a new API in P, and VerifyClass issue in
    http://crbug.com/791099, http://crbug.com/838702 got resurfaced when
    we rolled P SDK in
    https://chromium-review.googlesource.com/c/chromium/src/+/1112729
    
    Note that the issue might have been there in the downstream, which will
    also be fixed with this CL.
    
    Bug: 868250, 791099, 838702
    Test: I no longer see logcat message 'Rejecting re-init on ... TracingControllerAdapter'
    Change-Id: Ia7a967d9dfa206f64b0ec7ae284af120ac449534
    Reviewed-on: https://chromium-review.googlesource.com/1161003
    Commit-Queue: Changwan Ryu <changwan@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580339}

--- android_webview/glue/java/src/com/android/webview/chromium/WebViewChromiumFactoryProvider.java
+++ android_webview/glue/java/src/com/android/webview/chromium/WebViewChromiumFactoryProvider.java
@@ -546,8 +546,7 @@ public class WebViewChromiumFactoryProvider implements WebViewFactoryProvider {
             // waiting for startup. Hence check the mTracingControler here to ensure
             // the singleton property.
             if (mTracingController == null) {
-                mTracingController =
-                        new TracingControllerAdapter(this, mAwInit.getAwTracingController());
+                mTracingController = ApiHelperForP.createTracingControllerAdapter(this, mAwInit);
             }
         }
         return mTracingController;
