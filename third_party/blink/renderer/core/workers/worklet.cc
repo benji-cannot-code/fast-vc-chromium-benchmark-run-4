ff9b07f410e3bde7fc13a50c0ce7d1b5596c9219
tzik <tzik@chromium.org>
2018-08-02 23:20:19 +0000

    Avoid passing WorkletOptions as a WTF::Bind parameter
    WorkletOptions is marked as DISALLOW_NEW_EXCEPT_PLACEMENT_NEW(), and
    that required to be allocated on stack or on stack heap container.
    
    WTF::Bind() will start rejecting pass-by-value object that has Trace()
    method such as WorkletOptions as it's error prone. So, as a preparation
    for that, this CL expands necessary bit of WorkletOptions to the
    parameters of Worklet::FetchAndInvokeScript to avoid passing
    WorkletOptions directly.
    
    Change-Id: I4cbcb8e1163327c1d4ffd8aa17f3f3d719e2f523
    Reviewed-on: https://chromium-review.googlesource.com/1160682
    Commit-Queue: Taiju Tsuiki <tzik@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580372}

--- third_party/blink/renderer/core/workers/worklet.cc
+++ third_party/blink/renderer/core/workers/worklet.cc
@@ -77,9 +77,10 @@ ScriptPromise Worklet::addModule(ScriptState* script_state,
   // loading.
   GetExecutionContext()
       ->GetTaskRunner(TaskType::kInternalLoading)
-      ->PostTask(FROM_HERE, WTF::Bind(&Worklet::FetchAndInvokeScript,
-                                      WrapPersistent(this), module_url_record,
-                                      options, WrapPersistent(pending_tasks)));
+      ->PostTask(FROM_HERE,
+                 WTF::Bind(&Worklet::FetchAndInvokeScript, WrapPersistent(this),
+                           module_url_record, options.credentials(),
+                           WrapPersistent(pending_tasks)));
   return promise;
 }
 
@@ -109,7 +110,7 @@ WorkletGlobalScopeProxy* Worklet::FindAvailableGlobalScope() {
 // algorithm:
 // https://drafts.css-houdini.org/worklets/#dom-worklet-addmodule
 void Worklet::FetchAndInvokeScript(const KURL& module_url_record,
-                                   const WorkletOptions& options,
+                                   const String& credentials,
                                    WorkletPendingTasks* pending_tasks) {
   DCHECK(IsMainThread());
   if (!GetExecutionContext())
@@ -117,8 +118,7 @@ void Worklet::FetchAndInvokeScript(const KURL& module_url_record,
 
   // Step 6: "Let credentialOptions be the credentials member of options."
   network::mojom::FetchCredentialsMode credentials_mode;
-  bool result =
-      Request::ParseCredentialsMode(options.credentials(), &credentials_mode);
+  bool result = Request::ParseCredentialsMode(credentials, &credentials_mode);
   DCHECK(result);
 
   // Step 7: "Let outsideSettings be the relevant settings object of this."
