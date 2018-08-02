96275c100f9381c1649afff6c0548608df1676ed
Oscar Johansson <oscarj@opera.com>
2018-08-02 12:35:03 +0000

    Move variable to util (components/autofill/)
    When building using jumbo, files get merged and
    variables with the same name may
    end up in the same namespace and conflict.
    
    This happens for the constant kLocalGuidSize in autofill/core/browser.
    The variable is moved to a util file in
    common.
    
    Bug: 869381
    Change-Id: I038ef40acd5617f9d9d1e18c81a2fcce55cab8eb
    Reviewed-on: https://chromium-review.googlesource.com/1158544
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Commit-Queue: Oscar Johansson <oscarj@opera.com>
    Cr-Commit-Position: refs/heads/master@{#580142}

--- components/autofill/core/browser/webdata/autofill_wallet_metadata_syncable_service.cc
+++ components/autofill/core/browser/webdata/autofill_wallet_metadata_syncable_service.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/webdata/autofill_table.h"
 #include "components/autofill/core/browser/webdata/autofill_webdata_backend.h"
 #include "components/autofill/core/browser/webdata/autofill_webdata_service.h"
+#include "components/autofill/core/common/autofill_util.h"
 #include "components/sync/model/sync_change.h"
 #include "components/sync/model/sync_change_processor.h"
 #include "components/sync/model/sync_data.h"
@@ -32,10 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace autofill {
 
 namespace {
-
-// The length of the GUIDs used for local autofill data. It is different than
-// the length used for server autofill data.
-const int kLocalGuidSize = 36;
 
 void* UserDataKey() {
   // Use the address of a static so that COMDAT folding won't ever fold
