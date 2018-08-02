f7c80a602753b630b825ecbbab15d8ac3b155b09
Adithya Srinivasan <adithyas@chromium.org>
2018-08-02 22:16:29 +0000

    Mojofy FileSystem
    - Mojofies and removes all messages in file_system_messages.h.
    - Replaces FileAPIMessageFilter with FileSystemDispatcherHost.
    - Adds mojom types for FileSystemInfo and FileSystemType and adds
      type converters to convert these types to storage::FileSystemInfo
      and storage FileSystemInfo respectively. The storage types will be
      replaced with the mojom types in follow up CLs.
    
    Change-Id: I82aaf9dcb04149880e68238540766f21771d2b21
    Bug: 787281
    Reviewed-on: https://chromium-review.googlesource.com/1125316
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580346}

--- content/browser/indexed_db/indexed_db_callbacks.cc
+++ content/browser/indexed_db/indexed_db_callbacks.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/sequenced_task_runner_handle.h"
 #include "base/time/time.h"
 #include "content/browser/child_process_security_policy_impl.h"
-#include "content/browser/fileapi/fileapi_message_filter.h"
 #include "content/browser/indexed_db/cursor_impl.h"
 #include "content/browser/indexed_db/database_impl.h"
 #include "content/browser/indexed_db/indexed_db_connection.h"
--- content/renderer/pepper/url_request_info_util.cc
+++ content/renderer/pepper/url_request_info_util.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/strings/string_util.h"
-#include "content/common/fileapi/file_system_messages.h"
+#include "content/public/common/service_names.mojom.h"
 #include "content/renderer/loader/request_extra_data.h"
 #include "content/renderer/pepper/host_globals.h"
 #include "content/renderer/pepper/pepper_file_ref_renderer_host.h"
@@ -25,6 +25,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/shared_impl/url_request_info_data.h"
 #include "ppapi/shared_impl/var.h"
 #include "ppapi/thunk/enter.h"
+#include "services/service_manager/public/cpp/connector.h"
+#include "third_party/blink/public/mojom/filesystem/file_system.mojom.h"
 #include "third_party/blink/public/platform/file_path_conversion.h"
 #include "third_party/blink/public/platform/web_data.h"
 #include "third_party/blink/public/platform/web_feature.mojom.h"
@@ -49,6 +51,13 @@ using blink::WebURLRequest;
 namespace content {
 
 namespace {
+
+blink::mojom::FileSystemManagerPtr GetFileSystemManager() {
+  blink::mojom::FileSystemManagerPtr file_system_manager_ptr;
+  ChildThreadImpl::current()->GetConnector()->BindInterface(
+      mojom::kBrowserServiceName, mojo::MakeRequest(&file_system_manager_ptr));
+  return file_system_manager_ptr;
+}
 
 // Appends the file ref given the Resource pointer associated with it to the
 // given HTTP body, returning true on success.
@@ -79,9 +88,8 @@ bool AppendFileRefToBody(PP_Instance instance,
     case PP_FILESYSTEMTYPE_LOCALPERSISTENT:
       // TODO(kinuko): remove this sync IPC when we fully support
       // AppendURLRange for FileSystem URL.
-      RenderThreadImpl::current()->Send(
-          new FileSystemHostMsg_SyncGetPlatformPath(
-              file_ref_host->GetFileSystemURL(), &platform_path));
+      GetFileSystemManager()->GetPlatformPath(file_ref_host->GetFileSystemURL(),
+                                              &platform_path);
       break;
     case PP_FILESYSTEMTYPE_EXTERNAL:
       platform_path = file_ref_host->GetExternalFilePath();
