ae572e8d688f987f1a30d55182970c7cd64c80d3
Findit <findit-for-me@appspot.gserviceaccount.com>
2018-08-02 01:33:57 +0000

    Revert "Apply Google-related request modifications for networking service path."
    This reverts commit b8dd98642173f26e20338e9da9eb74d31126198c.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 579953 as the
    culprit for failures in the build cycles as shown on:
    https://findit-for-me.appspot.com/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtL2I4ZGQ5ODY0MjE3M2YyNmUyMDMzOGU5ZGE5ZWI3NGQzMTEyNjE5OGMM
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium.memory/Linux%20CFI/9356
    
    Sample Failed Step: not_site_per_process_unit_tests
    
    Original change's description:
    > Apply Google-related request modifications for networking service path.
    > 
    > This adds support for:
    > 1) Safe Search
    > 2) YouTube restricted mode
    > 3) restricting consumer accounts
    > through Group Policy.
    > 
    > The other supporting change is to make ThrottlingURLLoader handle a throttle changing the URL in WillStartRequest.
    > 
    > Bug: 841313
    > Cq-Include-Trybots: luci.chromium.try:linux_mojo
    > Change-Id: Ic98e14dd5fa7b5b00d4befd3314aca7a67e6ba5a
    > Reviewed-on: https://chromium-review.googlesource.com/1152507
    > Reviewed-by: Matt Menke <mmenke@chromium.org>
    > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#579953}
    
    Change-Id: Ic1b90a4148f1d5ebb4442ce34fa5421d879ef32f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 841313
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Reviewed-on: https://chromium-review.googlesource.com/1159801
    Cr-Commit-Position: refs/heads/master@{#580025}

--- chrome/browser/profiles/profile_impl.cc
+++ chrome/browser/profiles/profile_impl.cc
@@ -98,7 +98,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_paths_internal.h"
 #include "chrome/common/chrome_switches.h"
-#include "chrome/common/net/safe_search_util.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/grit/chromium_strings.h"
