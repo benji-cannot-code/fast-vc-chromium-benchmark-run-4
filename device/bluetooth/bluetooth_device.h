c7355bcd6801caa78100ca1914786276296bdec1
jdoerrie <jdoerrie@chromium.org>
2018-08-02 15:29:48 +0000

    [Bluetooth] Make BluetoothDevice::UUIDSet a base::flat_set
    BluetoothDevice::DeviceUUIDs::UpdateDeviceUUIDs() makes use of
    std::set_union in order to merge advertised uuids and service uuids.
    However, std::set_union requires sorted ranges as input, which wasn't
    necessarily guaranteed before this change. When using a
    std::unordered_set the order of elements is not specified and
    std::set_union is not guaranteed to work correctly. Using a
    base::flat_set for UUIDSet instead fixes this issue. In order to prevent
    future regressions this change also replaces the usage of std::set_union
    with base::STLSetUnion performing an explicit IsSorted() check in Debug
    builds.
    
    Bug: 821766, 870208
    Change-Id: Id25711e6acf77ded9eebb6c80db5bcb2ed8a63e0
    Reviewed-on: https://chromium-review.googlesource.com/1159374
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580198}

--- device/bluetooth/bluetooth_device.h
+++ device/bluetooth/bluetooth_device.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/callback.h"
+#include "base/containers/flat_set.h"
 #include "base/gtest_prod_util.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
@@ -96,7 +97,7 @@ class DEVICE_BLUETOOTH_EXPORT BluetoothDevice {
   };
 
   typedef std::vector<BluetoothUUID> UUIDList;
-  typedef std::unordered_set<BluetoothUUID, BluetoothUUIDHash> UUIDSet;
+  typedef base::flat_set<BluetoothUUID> UUIDSet;
   typedef std::unordered_map<BluetoothUUID,
                              std::vector<uint8_t>,
                              BluetoothUUIDHash>
