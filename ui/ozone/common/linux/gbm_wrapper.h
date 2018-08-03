1563bfc4260419639ed41de3b4df2a6d5747bd5b
Michael Spang <spang@chromium.org>
2018-08-03 22:13:10 +0000

    ozone: drm: Allow overriding GbmDevice & GbmBuffer in tests
    Make these polymorphic types so that we can provide a fake implementation
    of libgbm for test purposes.
    
    Bug: 869206
    Test: ozone_unittests
    Change-Id: I773cdac62bade798fbcddac54a2d5557ce1da8ce
    Reviewed-on: https://chromium-review.googlesource.com/1162655
    Commit-Queue: Michael Spang <spang@chromium.org>
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580669}

---
+++ ui/ozone/common/linux/gbm_wrapper.h
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_OZONE_COMMON_LINUX_GBM_WRAPPER_H_
+#define UI_OZONE_COMMON_LINUX_GBM_WRAPPER_H_
+
+#include <memory>
+
+#include "ui/ozone/common/linux/gbm_device.h"
+
+namespace ui {
+
+std::unique_ptr<ui::GbmDevice> CreateGbmDevice(int fd);
+
+}  // namespace ui
+
+#endif  // UI_OZONE_COMMON_LINUX_GBM_WRAPPER_H_
