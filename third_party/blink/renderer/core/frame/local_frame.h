99c5145c73c2017036337d7f7c2b5c6c64250b2d
Ehsan Karamad <ekaramad@chromium.org>
2018-08-03 22:00:32 +0000

    Move SmoothScrollSequence to LocalRoot
    The smooth-scroll animations are local frame root concepts. This move
    will avoid a nested OOPIF same process with a parent to use the same
    sequencer so their animations are independently scheduled.
    
    Bug: 865446
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ic07c22eef5d7a6ec25fe51c32d3e22e056398522
    Reviewed-on: https://chromium-review.googlesource.com/1159189
    Commit-Queue: Ehsan Karamad <ekaramad@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Sandra Sun <sunyunjia@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580663}

--- third_party/blink/renderer/core/frame/local_frame.h
+++ third_party/blink/renderer/core/frame/local_frame.h
@@ -97,6 +97,7 @@ class PerformanceMonitor;
 class PluginData;
 class ScriptController;
 class SharedBuffer;
+class SmoothScrollSequencer;
 class SpellChecker;
 class TextSuggestionController;
 class WebComputedAXTree;
@@ -367,6 +368,8 @@ class CORE_EXPORT LocalFrame final : public Frame,
   void BindPreviewsResourceLoadingHintsRequest(
       blink::mojom::blink::PreviewsResourceLoadingHintsReceiverRequest request);
 
+  SmoothScrollSequencer& GetSmoothScrollSequencer();
+
  private:
   friend class FrameNavigationDisabler;
 
@@ -450,6 +453,9 @@ class CORE_EXPORT LocalFrame final : public Frame,
   Member<AdTracker> ad_tracker_;
   Member<IdlenessDetector> idleness_detector_;
   Member<InspectorTraceEvents> inspector_trace_events_;
+  // SmoothScrollSequencer is only populated for local roots; all local frames
+  // use the instance owned by their local root.
+  Member<SmoothScrollSequencer> smooth_scroll_sequencer_;
 
   InterfaceRegistry* const interface_registry_;
 
--- third_party/blink/renderer/core/page/page.h
+++ third_party/blink/renderer/core/page/page.h
@@ -70,7 +70,6 @@ class PointerLockController;
 class ScopedPagePauser;
 class ScrollingCoordinator;
 class ScrollbarTheme;
-class SmoothScrollSequencer;
 class Settings;
 class ConsoleMessageStorage;
 class TopDocumentRootScrollerController;
@@ -187,8 +186,6 @@ class CORE_EXPORT Page final : public GarbageCollectedFinalized<Page>,
   void SetValidationMessageClient(ValidationMessageClient*);
 
   ScrollingCoordinator* GetScrollingCoordinator();
-
-  SmoothScrollSequencer* GetSmoothScrollSequencer();
 
   DOMRectList* NonFastScrollableRectsForTesting(const LocalFrame*);
 
@@ -358,7 +355,6 @@ class CORE_EXPORT Page final : public GarbageCollectedFinalized<Page>,
   const std::unique_ptr<PageScaleConstraintsSet> page_scale_constraints_set_;
   const Member<PointerLockController> pointer_lock_controller_;
   Member<ScrollingCoordinator> scrolling_coordinator_;
-  Member<SmoothScrollSequencer> smooth_scroll_sequencer_;
   const Member<BrowserControls> browser_controls_;
   const Member<ConsoleMessageStorage> console_message_storage_;
   const Member<TopDocumentRootScrollerController>
