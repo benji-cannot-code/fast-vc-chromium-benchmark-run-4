a8b8eacd5810d0bf01aec15369f23e2c66455e54
Boris Sazonov <bsazonov@chromium.org>
2018-08-03 09:25:50 +0000

    [Android] Hide description in RadioButtonWithDescription if it's empty
    This CL adds a check to hide the description text view in
    RadioButtonWithDescription if it contains an empty string.
    
    Bug: 869426
    Change-Id: I9e0b26303a0d98b2d44944818927f0acea5aaa59
    Reviewed-on: https://chromium-review.googlesource.com/1159072
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580486}

--- chrome/android/java/src/org/chromium/chrome/browser/widget/RadioButtonWithDescription.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/RadioButtonWithDescription.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.util.SparseArray;
 import android.view.LayoutInflater;
@@ -86,6 +87,7 @@ public class RadioButtonWithDescription extends RelativeLayout implements OnClic
      */
     public void setDescriptionText(CharSequence text) {
         mDescription.setText(text);
+        mDescription.setVisibility(TextUtils.isEmpty(text) ? View.GONE : View.VISIBLE);
     }
 
     /**
