ec03c838e98f5efb3b9fef3bc35bec900a1f88a2
Dominick Ng <dominickn@chromium.org>
2018-08-03 13:25:36 +0000

    Add missing unit test initialization in DeviceStatusCollectorTest.
    Unlike CloudPrintProxyPolicyStartupTest, which similarly is a unit test
    running in the browser_tests binary, DeviceStatusCollectorTest does not
    initialize a ContentClient or a BrowserContentClient. This is causing
    tests to fail while trying to land crrev.com/c/1157704, where a new
    AfterStartupTask that eventually calls into a nullptr
    BrowserContentClient while running this seemingly unrelated test.
    
    BUG=None
    
    Change-Id: Ibb4c4a9d4b8d8ecf2e243a81158341824ef4cda1
    Reviewed-on: https://chromium-review.googlesource.com/1160072
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580523}

--- chrome/browser/chromeos/policy/device_status_collector_browsertest.cc
+++ chrome/browser/chromeos/policy/device_status_collector_browsertest.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/scoped_feature_list.h"
 #include "base/test/scoped_path_override.h"
 #include "base/threading/thread_task_runner_handle.h"
+#include "chrome/browser/chrome_content_browser_client.h"
 #include "chrome/browser/chromeos/app_mode/arc/arc_kiosk_app_manager.h"
 #include "chrome/browser/chromeos/app_mode/kiosk_app_data.h"
 #include "chrome/browser/chromeos/app_mode/kiosk_app_manager.h"
@@ -35,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/chromeos/settings/scoped_cros_settings_test_helper.h"
 #include "chrome/browser/chromeos/settings/stub_install_attributes.h"
+#include "chrome/common/chrome_content_client.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/pref_names.h"
@@ -65,6 +67,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/user_manager/scoped_user_manager.h"
 #include "components/user_manager/user_type.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/common/content_client.h"
 #include "content/public/test/test_browser_thread_bundle.h"
 #include "content/public/test/test_utils.h"
 #include "storage/browser/fileapi/external_mount_points.h"
@@ -322,6 +325,9 @@ class DeviceStatusCollectorTest : public testing::Test {
     ChromeUnitTestSuite::InitializeProviders();
     ChromeUnitTestSuite::InitializeResourceBundle();
 
+    content::SetContentClient(&content_client_);
+    content::SetBrowserClientForTesting(&browser_content_client_);
+
     // Run this test with a well-known timezone so that Time::LocalMidnight()
     // returns the same values on all machines.
     std::unique_ptr<base::Environment> env(base::Environment::Create());
@@ -558,6 +564,8 @@ class DeviceStatusCollectorTest : public testing::Test {
   TestingBrowserProcessInitializer initializer_;
   content::TestBrowserThreadBundle test_browser_thread_bundle_;
 
+  ChromeContentClient content_client_;
+  ChromeContentBrowserClient browser_content_client_;
   chromeos::ScopedStubInstallAttributes install_attributes_;
   chromeos::system::ScopedFakeStatisticsProvider fake_statistics_provider_;
   DiskMountManager::MountPointMap mount_point_map_;
