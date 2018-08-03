8b196a1a67a809a0b5542e2b0112eef1b21fd24c
Renee Wright <rjwright@chromium.org>
2018-08-03 01:53:58 +0000

    Add learn more link to Crostini Settings page
    Bug: 862609
    Change-Id: I27594afaa4b128ff0ae8e6009ff5c88eb22c69f3
    Reviewed-on: https://chromium-review.googlesource.com/1160123
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Commit-Queue: Renée Wright <rjwright@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580410}

--- chrome/browser/resources/settings/crostini_page/crostini_page.js
+++ chrome/browser/resources/settings/crostini_page/crostini_page.js
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 Polymer({
   is: 'settings-crostini-page',
 
-  behaviors: [PrefsBehavior],
+  behaviors: [I18nBehavior, PrefsBehavior],
 
   properties: {
     /** Preferences state. */
