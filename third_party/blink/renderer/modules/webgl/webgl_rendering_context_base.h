f5c2b5b95fd67d13f757651862d5956a77ebf5b9
Miguel Casas <mcasas@chromium.org>
2018-08-03 02:09:16 +0000

    webgl lowLatency: don't flip vertically the DrawQuad if not needed
    This CL adds a CanvasRenderingContext::IsOriginTopLeft() method
    that is used to indicate if the canvas origin of coords -top left
    corner- and the CanvasResource origin -low left corner- differ
    and need reconciliation.
    
    IsOriginTopLeft() is usually false if the context IsAccelerated();
    for WebGL cases the framebuffer can be flipped itself instead:
    this is marked by the presence of a context flag. IsOriginTopLeft()
    is true in this case.
    
    This CL wires this info to CanvasResourceDispatcher::PrepareFrame()
    so that the DrawQuad is marked appropriately. On some platforms (intel)
    only non-flipped quads can be promoted to overlays (enabling front
    buffer rendering).
    
    Running with the patch makes the webgl demo [1] show up as overlay on
    both nautilus and scarlet (needs at least 10925 OS). Canvas2D still OK.
    
    [1] https://codepen.io/miguelao/full/WKZaqd
    
    Bug: 869922
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_layout_tests_slimming_paint_v2;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Idbe2bf4901c8f71b3fe0b596f5e7c3e3f0ae0df6
    Reviewed-on: https://chromium-review.googlesource.com/1159185
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580415}

--- third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.h
+++ third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.h
@@ -654,6 +654,7 @@ class MODULES_EXPORT WebGLRenderingContextBase : public CanvasRenderingContext,
   bool Is3d() const override { return true; }
   bool IsComposited() const override { return true; }
   bool IsAccelerated() const override { return true; }
+  bool IsOriginTopLeft() const override;
   void SetIsHidden(bool) override;
   bool PaintRenderingResultsToCanvas(SourceDrawingBuffer) override;
   cc::Layer* CcLayer() const override;
@@ -711,6 +712,8 @@ class MODULES_EXPORT WebGLRenderingContextBase : public CanvasRenderingContext,
   scoped_refptr<DrawingBuffer> drawing_buffer_;
 
   TraceWrapperMember<WebGLContextGroup> context_group_;
+
+  bool is_origin_top_left_;
 
   bool is_hidden_;
   LostContextMode context_lost_mode_;
