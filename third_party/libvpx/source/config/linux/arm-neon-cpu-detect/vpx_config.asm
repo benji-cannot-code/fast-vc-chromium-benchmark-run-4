07fb66335865fe983635afd64f6cedc82714bcd1
Jerome Jiang <jianj@google.com>
2018-08-03 23:00:42 +0000

    Roll src/third_party/libvpx/source/libvpx/ 2d79df494..6fd9d0244 (28 commits)
    https://chromium.googlesource.com/webm/libvpx.git/+log/2d79df494088..6fd9d0244c7d
    
    $ git log 2d79df494..6fd9d0244 --date=short --no-merges --format='%ad %ae %s'
    2018-08-03 marpan vp9: Add screen-content mode to overshoot detection.
    2018-08-03 marpan vp9: Adjust qp_thresh on slide change overshoot detection
    2018-08-02 marpan vp9: Add zero_temp_sad count to scene detection.
    2018-08-02 marpan vp9: Increase min_thresh for slide change detection
    2018-08-02 huisu Refactor vp9_full_pixel_search()
    2018-08-02 marpan vp9: Disable re_encode_overshoot feature for speed >= 6.
    2018-08-01 huisu Handle partition cost better in RD search
    2018-07-31 jingning Use mesh full pixel motion search to build the source ARF
    2018-08-01 jianj vp9 svc: Adjust overshoot threshold in datarate test.
    2018-08-01 jianj vp9: Refactor nonrd pickmode: new mv search
    2018-07-30 jianj vp9: Refactor nonrd pickmode: interp filter.
    2018-07-30 jianj vp9: Refactor nonrd pickmode: tx_size.
    2018-07-31 jianj vp9 svc: Fix the scaling factor in intra only test for 1 SL.
    2018-07-31 jzern vp9_encoder: make setup_tpl_stats() static
    2018-07-31 marpan vp9: Clamp tx_size in model_rd_large
    2018-07-31 jzern vp9: enable tpl model in high-bitdepth w/8-bit output
    2018-07-31 jianj vp9: Disable aq mode for some datarate tests.
    2018-07-31 marpan vp9: Remove assert from model_rd in non-rd pickmode.
    2018-07-27 jingning Add frame pointer to support recon frames in tpl model
    2018-07-27 jingning Move frame pointer assignment outside block loop in tpl model
    (...)
    
    Created with:
      roll-dep src/third_party/libvpx/source/libvpx
    R=johannkoenig@google.com
    
    BUG=b/112123926
    
    Change-Id: I5adddb62cf63654739a3dc8f8ebe398fd5c744e4
    Reviewed-on: https://chromium-review.googlesource.com/1162719
    Reviewed-by: Johann Koenig <johannkoenig@google.com>
    Commit-Queue: Jerome Jiang <jianj@google.com>
    Cr-Commit-Position: refs/heads/master@{#580688}

--- third_party/libvpx/source/config/linux/arm-neon-cpu-detect/vpx_config.asm
+++ third_party/libvpx/source/config/linux/arm-neon-cpu-detect/vpx_config.asm
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @ This file was created from a .asm file
 @  using the ads2gas.pl script.
 	.equ DO1STROUNDING, 0
+	.syntax unified
 .equ ARCH_ARM ,  1
 .equ ARCH_MIPS ,  0
 .equ ARCH_X86 ,  0
