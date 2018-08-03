ad8726a794f8716518b98812e5c7912bd0a71cf4
Benjamin Shaya <bshaya@chromium.org>
2018-08-03 22:40:09 +0000

    Add missing functions to video devices w/ mixer.
    Bug: 80048435
    Test: User EQ on video device w/ mixer
    Change-Id: Iccd7ffb26e1cc75fbc79ec0bb1db04dc6551ee08
    Reviewed-on: https://chromium-review.googlesource.com/1159694
    Commit-Queue: Benjamin Shaya <bshaya@chromium.org>
    Reviewed-by: Kenneth MacKay <kmackay@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580680}

---
+++ chromecast/media/cma/backend/cast_media_shlib_mixer_audio.cc
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Provides CastMediaShlib functions common to all devices using StreamMixer.
+
+#include "chromecast/public/cast_media_shlib.h"
+
+#include <string>
+
+#include "chromecast/media/cma/backend/stream_mixer.h"
+
+namespace chromecast {
+namespace media {
+
+void CastMediaShlib::AddLoopbackAudioObserver(LoopbackAudioObserver* observer) {
+  StreamMixer::Get()->AddLoopbackAudioObserver(observer);
+}
+
+void CastMediaShlib::RemoveLoopbackAudioObserver(
+    LoopbackAudioObserver* observer) {
+  StreamMixer::Get()->RemoveLoopbackAudioObserver(observer);
+}
+
+void CastMediaShlib::ResetPostProcessors() {
+  StreamMixer::Get()->ResetPostProcessors();
+}
+
+void CastMediaShlib::SetPostProcessorConfig(const std::string& name,
+                                            const std::string& config) {
+  StreamMixer::Get()->SetPostProcessorConfig(name, config);
+}
+
+}  // namespace media
+}  // namespace chromecast
