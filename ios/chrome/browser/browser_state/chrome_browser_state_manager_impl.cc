aa07e2a4c0724643e144be5f04a710bbe559cab1
Rohit Rao <rohitrao@chromium.org>
2018-08-03 20:09:30 +0000

    Revert "[iOS] Start unified consent service at startup"
    This reverts commit 3b42f46af25d26a932351e05f8fd12be996443a2.
    
    Reason for revert: Crashing internal bots
    
    Test Case '-[GoogleServicesSettingsTestCase testOpeningServicesWhileSignedInAndConsentGiven]' started.
    0803/040411.392060:FATAL:unified_consent_service.cc(155)] Check failed: identity_manager_->HasPrimaryAccount().
    0   ios_chrome_unified_consent_egtests  0x000000010d19df38 base::debug::StackTrace::StackTrace(unsigned long) + 72
    1   ios_chrome_unified_consent_egtests  0x000000010d19df9d base::debug::StackTrace::StackTrace(unsigned long) + 29
    2   ios_chrome_unified_consent_egtests  0x000000010ce8adac base::debug::StackTrace::StackTrace() + 28
    3   ios_chrome_unified_consent_egtests  0x000000010ced068b logging::LogMessage::~LogMessage() + 315
    4   ios_chrome_unified_consent_egtests  0x000000010cecf495 logging::LogMessage::~LogMessage() + 21
    5   ios_chrome_unified_consent_egtests  0x000000011054785e unified_consent::UnifiedConsentService::OnUnifiedConsentGivenPrefChanged() + 638
    
    Original change's description:
    > [iOS] Start unified consent service at startup
    > 
    > Bug: 827072
    > Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    > Change-Id: I2f2e14ceba73ebecbc9c0222ae87ea86e4f91a49
    > Reviewed-on: https://chromium-review.googlesource.com/1159062
    > Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    > Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    > Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#580485}
    
    TBR=msarda@chromium.org,sdefresne@chromium.org,jlebel@chromium.org
    
    Change-Id: I4d94ba1e7630c81f666d336882c17c993ac5ef02
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 827072
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Reviewed-on: https://chromium-review.googlesource.com/1162402
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580635}

--- ios/chrome/browser/browser_state/chrome_browser_state_manager_impl.cc
+++ ios/chrome/browser/browser_state/chrome_browser_state_manager_impl.cc
@@ -42,7 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/signin/gaia_cookie_manager_service_factory.h"
 #include "ios/chrome/browser/signin/signin_manager_factory.h"
 #include "ios/chrome/browser/sync/profile_sync_service_factory.h"
-#include "ios/chrome/browser/unified_consent/unified_consent_service_factory.h"
 
 namespace {
 
@@ -225,9 +224,6 @@ void ChromeBrowserStateManagerImpl::DoFinalInitForServices(
   ios::AccountFetcherServiceFactory::GetForBrowserState(browser_state)
       ->SetupInvalidationsOnProfileLoad(invalidation_service);
   ios::AccountReconcilorFactory::GetForBrowserState(browser_state);
-  // Initialization needs to happen after the browser context is available
-  // because ProfileSyncService needs the URL context getter.
-  UnifiedConsentServiceFactory::GetForBrowserState(browser_state);
   DesktopPromotionSyncServiceFactory::GetForBrowserState(browser_state);
 }
 
