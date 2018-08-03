78ebd1fb8e7a8676e49f347ff4d231ff676d7f54
David Black <dmblack@google.com>
2018-08-03 03:14:10 +0000

    Prevent crashing caused by server side contract break.
    We have a contract with the server that TTS will be after all UI
    responses. When the server breaks this contract, we currently crash.
    
    We should not crash when server breaks contract but we will ignore the
    unexpected responses.
    
    Bug: b:112158958
    Change-Id: I560c647584bee2ee3fcbce74c2f77b8124fa3d82
    Reviewed-on: https://chromium-review.googlesource.com/1161390
    Commit-Queue: David Black <dmblack@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580430}

--- ash/assistant/assistant_interaction_controller.cc
+++ ash/assistant/assistant_interaction_controller.cc
@@ -211,6 +211,13 @@ void AssistantInteractionController::OnHtmlResponse(
     return;
   }
 
+  // If this occurs, the server has broken our response ordering agreement. We
+  // should not crash but we cannot handle the response so we ignore it.
+  if (!assistant_interaction_model_.pending_response()) {
+    NOTREACHED();
+    return;
+  }
+
   assistant_interaction_model_.pending_response()->AddUiElement(
       std::make_unique<AssistantCardElement>(response));
 }
@@ -237,6 +244,13 @@ void AssistantInteractionController::OnSuggestionsResponse(
     return;
   }
 
+  // If this occurs, the server has broken our response ordering agreement. We
+  // should not crash but we cannot handle the response so we ignore it.
+  if (!assistant_interaction_model_.pending_response()) {
+    NOTREACHED();
+    return;
+  }
+
   assistant_interaction_model_.pending_response()->AddSuggestions(
       std::move(response));
 }
@@ -245,6 +259,13 @@ void AssistantInteractionController::OnTextResponse(
     const std::string& response) {
   if (assistant_interaction_model_.interaction_state() !=
       InteractionState::kActive) {
+    return;
+  }
+
+  // If this occurs, the server has broken our response ordering agreement. We
+  // should not crash but we cannot handle the response so we ignore it.
+  if (!assistant_interaction_model_.pending_response()) {
+    NOTREACHED();
     return;
   }
 
