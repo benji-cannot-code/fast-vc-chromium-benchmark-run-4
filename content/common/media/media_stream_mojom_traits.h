58b0b30ec2942a5ce8f62de2f3652461d1be050a
Daniel Cheng <dcheng@chromium.org>
2018-08-03 23:17:17 +0000

    Mojom traits: rename incorrectly named files in media code.
    Change-Id: Id1fee120b7d61cc41cd359389bf5d05f28dac674
    Reviewed-on: https://chromium-review.googlesource.com/1162430
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580694}

--- content/common/media/media_stream_mojom_traits.h
+++ content/common/media/media_stream_mojom_traits.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CONTENT_COMMON_MEDIA_MEDIA_STREAM_TYPEMAP_TRAITS_H_
-#define CONTENT_COMMON_MEDIA_MEDIA_STREAM_TYPEMAP_TRAITS_H_
+#ifndef CONTENT_COMMON_MEDIA_MEDIA_STREAM_MOJOM_TRAITS_H_
+#define CONTENT_COMMON_MEDIA_MEDIA_STREAM_MOJOM_TRAITS_H_
 
 #include "content/common/media/media_stream.mojom.h"
 #include "content/common/media/media_stream_controls.h"
@@ -77,4 +77,4 @@ struct StructTraits<content::mojom::StreamControlsDataView,
 
 }  // namespace mojo
 
-#endif  // CONTENT_COMMON_MEDIA_MEDIA_STREAM_TYPEMAP_TRAITS_H_
+#endif  // CONTENT_COMMON_MEDIA_MEDIA_STREAM_MOJOM_TRAITS_H_
