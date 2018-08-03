066b0a29ccac2b8afe275425bcb91cd735045609
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-08-03 18:11:29 +0000

    Import wpt@79f585c4b5c1391a62e94b87bfb5f89719e82e99
    Using wpt-import in Chromium 0856d23e554086fb4f3a3391c48c86e1fa75efca.
    With Chromium commits locally applied on WPT:
    7dad0f1723 "Added new Sec-Metadata Web Platform Tests."
    
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/22272
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    worker-dev@chromium.org:
      external/wpt/workers
    
    TBR=foolip
    
    No-Export: true
    Change-Id: I7c4b226374d3e80ccccef3f2a4c3a0e35b6a4476
    Reviewed-on: https://chromium-review.googlesource.com/1162273
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580600}

--- third_party/WebKit/LayoutTests/external/wpt/workers/modules/dedicated-worker-import.any.js
+++ third_party/WebKit/LayoutTests/external/wpt/workers/modules/dedicated-worker-import.any.js
@@ -7,7 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 function import_test(testCase) {
   promise_test(async () => {
     const worker = new Worker(testCase.scriptURL, { type: 'module' });
-    const msgEvent = await new Promise(resolve => worker.onmessage = resolve);
+    const msgEvent = await new Promise((resolve, reject) => {
+      worker.onmessage = resolve;
+      worker.onerror = (error) => reject(error && error.message);
+    });
     assert_array_equals(msgEvent.data, testCase.expectation);
   }, testCase.description);
 }
