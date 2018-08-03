74d5cbe2bafb1a8cc2ca6fd53297f903fcbd9c35
Xi Cheng <chengx@chromium.org>
2018-08-03 02:43:19 +0000

    Format headers inclusion for call_stack_profile_builder.h
    Bug: 851163
    Change-Id: I4655ce7f882983bfd671ec27dfb9d54c62f0e166
    Reviewed-on: https://chromium-review.googlesource.com/1161333
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Commit-Queue: Xi Cheng <chengx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580418}

--- components/metrics/call_stack_profile_builder.h
+++ components/metrics/call_stack_profile_builder.h
@@ -6,11 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_METRICS_CALL_STACK_PROFILE_BUILDER_H_
 #define COMPONENTS_METRICS_CALL_STACK_PROFILE_BUILDER_H_
 
-#include "base/profiler/stack_sampling_profiler.h"
-
 #include <map>
 
 #include "base/callback.h"
+#include "base/profiler/stack_sampling_profiler.h"
 
 namespace metrics {
 
