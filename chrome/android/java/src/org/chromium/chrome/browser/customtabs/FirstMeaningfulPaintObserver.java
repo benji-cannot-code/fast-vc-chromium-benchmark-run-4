ff1c8faa060e74deb5479679f63fe017dea438da
Peter E Conn <peconn@chromium.org>
2018-08-03 08:04:39 +0000

    🛃 Tidy up CustomTabActivitys Observers.
    Split CustomTabActivity's PageLoadMetrics.Observers according to their
    purpose. Also create the TabObserverRegistrar class that deals with
    attaching these observers to Tabs as they enter and exit the TabModel.
    
    Bug: none
    Change-Id: Ic434ead9c5167eb876f91af33b54dfd3d50493cd
    Reviewed-on: https://chromium-review.googlesource.com/1154968
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580474}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/FirstMeaningfulPaintObserver.java
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.customtabs;
+
+import org.chromium.chrome.browser.metrics.PageLoadMetrics;
+import org.chromium.chrome.browser.tab.Tab;
+import org.chromium.content_public.browser.WebContents;
+
+/**
+ * Notifies the provided {@link CustomTabObserver} when first meaningful paint occurs.
+ */
+/* package */ class FirstMeaningfulPaintObserver implements PageLoadMetrics.Observer {
+    private final Tab mTab;
+    private final CustomTabObserver mCustomTabObserver;
+
+    /* package */ FirstMeaningfulPaintObserver(CustomTabObserver tabObserver, Tab tab) {
+        mCustomTabObserver = tabObserver;
+        mTab = tab;
+    }
+
+    @Override
+    public void onFirstMeaningfulPaint(WebContents webContents, long navigationId,
+            long navigationStartTick, long firstContentfulPaintMs) {
+        if (webContents != mTab.getWebContents()) return;
+
+        mCustomTabObserver.onFirstMeaningfulPaint(mTab);
+    }
+}
