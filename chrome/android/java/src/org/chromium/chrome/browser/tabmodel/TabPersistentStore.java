22787b84ecbcb646507b4766d8476a22b9a9affa
Sam Maier <smaier@chromium.org>
2018-08-03 15:42:07 +0000

    Android: make loadNextTab AsyncTask explicit on SERIAL_EXECUTOR
    We might be changing the default execute(), so we want to make this an
    explicit call to the SERIAL_EXECUTOR.
    
    Bug: 869907
    Change-Id: Ifd7cff98b7714b664fdc6cb30bc0d0f7b1a3be4a
    Reviewed-on: https://chromium-review.googlesource.com/1162076
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580559}

--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabPersistentStore.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabPersistentStore.java
@@ -1267,7 +1267,7 @@ public class TabPersistentStore extends TabPersister {
         } else {
             TabRestoreDetails tabToRestore = mTabsToRestore.removeFirst();
             mLoadTabTask = new LoadTabTask(tabToRestore);
-            mLoadTabTask.execute();
+            mLoadTabTask.executeOnExecutor(AsyncTask.SERIAL_EXECUTOR);
         }
     }
 
