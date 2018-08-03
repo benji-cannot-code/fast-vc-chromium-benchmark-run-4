b3b6406ae4898f652dc3df93d80d2227a78640c1
Shakti Sahu <shaktisahu@chromium.org>
2018-08-03 17:30:28 +0000

    Download Home : Moved ListPropertyModel to PropertyModel
    This CL migrates the download home list property model to use a
    PropertyModel instead.
    
    Bug: 870432
    Change-Id: I6f1f2f5f523f5fb66726e3991e3d3d8dd0f760fa
    Reviewed-on: https://chromium-review.googlesource.com/1161216
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580584}

--- chrome/android/java/src/org/chromium/chrome/browser/download/home/list/ListItemModel.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/home/list/ListItemModel.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.download.home.list;
 
 import org.chromium.chrome.browser.modelutil.ListObservable;
+import org.chromium.chrome.browser.modelutil.PropertyModel;
 import org.chromium.chrome.browser.modelutil.SimpleList;
 
 import java.util.ArrayList;
@@ -14,17 +15,17 @@ import java.util.List;
 /**
  * This model represents the data required to build a list UI around a set of {@link ListItem}s.
  * This includes (1) a {@link ListObservable} implementation and (2) exposing a
- * {@link ListPropertyModel} for shared item properties and general list information.
+ * {@link PropertyModel} for shared item properties and general list information.
  */
 class ListItemModel extends BatchListObservable implements SimpleList<ListItem> {
     private final List<ListItem> mItems = new ArrayList<>();
-    private final ListPropertyModel mListProperties = new ListPropertyModel();
+    private final PropertyModel mListProperties = new PropertyModel(ListProperties.ALL_KEYS);
 
     /**
-     * @return A {@link ListPropertyModel} instance, which is a set of shared properties for the
+     * @return A {@link PropertyModel} instance, which is a set of shared properties for the
      *         list.
      */
-    public ListPropertyModel getProperties() {
+    public PropertyModel getProperties() {
         return mListProperties;
     }
 
--- chrome/android/java/src/org/chromium/chrome/browser/download/home/list/holder/ThumbnailAwareViewHolder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/home/list/holder/ThumbnailAwareViewHolder.java
@@ -12,8 +12,9 @@ import android.widget.ImageView;
 
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.download.home.list.ListItem;
-import org.chromium.chrome.browser.download.home.list.ListPropertyModel;
+import org.chromium.chrome.browser.download.home.list.ListProperties;
 import org.chromium.chrome.browser.download.home.view.SelectionView;
+import org.chromium.chrome.browser.modelutil.PropertyModel;
 import org.chromium.components.offline_items_collection.ContentId;
 import org.chromium.components.offline_items_collection.OfflineItem;
 import org.chromium.components.offline_items_collection.OfflineItemVisuals;
@@ -69,7 +70,7 @@ abstract class ThumbnailAwareViewHolder extends MoreButtonViewHolder implements 
     // MoreButtonViewHolder implementation.
     @Override
     @CallSuper
-    public void bind(ListPropertyModel properties, ListItem item) {
+    public void bind(PropertyModel properties, ListItem item) {
         super.bind(properties, item);
         // If we have no thumbnail to show just return early.
         if (mThumbnail == null) return;
@@ -82,20 +83,21 @@ abstract class ThumbnailAwareViewHolder extends MoreButtonViewHolder implements 
         }
 
         if (mSelectionView != null) {
-            mSelectionView.setSelectionState(
-                    item.selected, properties.getSelectionModeActive(), item.showSelectedAnimation);
+            mSelectionView.setSelectionState(item.selected,
+                    properties.getValue(ListProperties.SELECTION_MODE_ACTIVE),
+                    item.showSelectedAnimation);
         }
 
         itemView.setOnLongClickListener(v -> {
-            properties.getSelectionCallback().onResult(item);
+            properties.getValue(ListProperties.CALLBACK_SELECTION).onResult(item);
             return true;
         });
 
         itemView.setOnClickListener(v -> {
             if (mSelectionView != null && mSelectionView.isInSelectionMode()) {
-                properties.getSelectionCallback().onResult(item);
+                properties.getValue(ListProperties.CALLBACK_SELECTION).onResult(item);
             } else {
-                properties.getOpenCallback().onResult(offlineItem);
+                properties.getValue(ListProperties.CALLBACK_OPEN).onResult(offlineItem);
             }
         });
 
@@ -107,18 +109,19 @@ abstract class ThumbnailAwareViewHolder extends MoreButtonViewHolder implements 
 
         // Start the new request.
         mId = offlineItem.id;
-        mCancellable =
-                properties.getVisualsProvider().getVisuals(offlineItem, mWidthPx, mHeightPx, this);
+        mCancellable = properties.getValue(ListProperties.PROVIDER_VISUALS)
+                               .getVisuals(offlineItem, mWidthPx, mHeightPx, this);
 
         // Make sure to update our state properly if we got a synchronous response.
         if (!mIsRequesting) mCancellable = null;
     }
 
-    private boolean selectionStateHasChanged(ListPropertyModel properties, ListItem item) {
+    private boolean selectionStateHasChanged(PropertyModel properties, ListItem item) {
         if (mSelectionView == null) return false;
 
         return mSelectionView.isSelected() != item.selected
-                || mSelectionView.isInSelectionMode() != properties.getSelectionModeActive();
+                || mSelectionView.isInSelectionMode()
+                != properties.getValue(ListProperties.SELECTION_MODE_ACTIVE);
     }
 
     // VisualsCallback implementation.
