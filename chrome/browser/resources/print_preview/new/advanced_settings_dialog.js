ce9f4d84bd308ba8189d4e5e89b7324517983679
rbpotter <rbpotter@chromium.org>
2018-08-03 14:12:46 +0000

    Print Preview: Fix key event handling in dialogs
    If a child dialog is focused, do not pass key events to the main
    Print Preview page.
    
    Bug: 869317
    Change-Id: I0fd24ff715f92d39fc0f1f51a02688d630a8d219
    Reviewed-on: https://chromium-review.googlesource.com/1159527
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580534}

--- chrome/browser/resources/print_preview/new/advanced_settings_dialog.js
+++ chrome/browser/resources/print_preview/new/advanced_settings_dialog.js
@@ -26,6 +26,10 @@ Polymer({
     },
   },
 
+  listeners: {
+    'keydown': 'onKeydown_',
+  },
+
   /** @private {!Array<Node>} */
   highlights_: [],
 
@@ -53,7 +57,9 @@ Polymer({
    */
   onKeydown_: function(e) {
     e.stopPropagation();
-    if (e.key == 'Escape' && !this.$.searchBox.getSearchInput().value.trim()) {
+    const searchInput = this.$.searchBox.getSearchInput();
+    if (e.key == 'Escape' &&
+        (e.composedPath()[0] !== searchInput || !searchInput.value.trim())) {
       this.$.dialog.cancel();
       e.preventDefault();
     }
