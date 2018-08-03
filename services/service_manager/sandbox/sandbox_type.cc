f9b0adbb5cc584451fc882b7320ff94da8b28a96
Robert Sesek <rsesek@chromium.org>
2018-08-03 22:18:24 +0000

    [Mac] Bring up the audio service sandbox.
    This requires --enable-features=AudioServiceAudioStreams,AudioServiceOutOfProcess,AudioServiceSandbox
    
    Bug: 850878
    Change-Id: I9c759b2537483612f9b3106bdf2ec193b343f198
    Reviewed-on: https://chromium-review.googlesource.com/1152080
    Reviewed-by: Greg Kerr <kerrnel@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580672}

--- services/service_manager/sandbox/sandbox_type.cc
+++ services/service_manager/sandbox/sandbox_type.cc
@@ -8,11 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/feature_list.h"
-#include "services/service_manager/sandbox/switches.h"
-
-#if defined(OS_WIN)
 #include "services/service_manager/sandbox/features.h"
-#endif
+#include "services/service_manager/sandbox/switches.h"
 
 namespace service_manager {
 
@@ -25,7 +22,7 @@ bool IsUnsandboxedSandboxType(SandboxType sandbox_type) {
       return true;
 #endif
     case SANDBOX_TYPE_AUDIO:
-#if defined(OS_WIN)
+#if defined(OS_WIN) || defined(OS_MACOSX)
       return !base::FeatureList::IsEnabled(
           service_manager::features::kAudioServiceSandbox);
 #else
