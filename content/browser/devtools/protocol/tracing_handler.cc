e4d4b7623e7bad80bc619732158cf25f599bce96
Saman Sami <samans@chromium.org>
2018-08-04 17:14:27 +0000

    Fix performance timeline screenshots on Android WebView
    They just don't work at all right now. Video capture doesn't work on
    Android WebView. We need to call CopyFromSurface.
    
    Bug: 813929, 865430
    Change-Id: I4360d3f53f852eb7e3182ac58540a1500cc6fee3
    Reviewed-on: https://chromium-review.googlesource.com/1159199
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Saman Sami <samans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580759}

--- content/browser/devtools/protocol/tracing_handler.cc
+++ content/browser/devtools/protocol/tracing_handler.cc
@@ -43,6 +43,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/resource_coordinator/public/cpp/memory_instrumentation/memory_instrumentation.h"
 #include "services/tracing/public/mojom/constants.mojom.h"
 
+#ifdef OS_ANDROID
+#include "content/browser/renderer_host/compositor_impl_android.h"
+#endif
+
 namespace content {
 namespace protocol {
 
@@ -215,9 +219,16 @@ TracingHandler::TracingHandler(FrameTreeNode* frame_tree_node_,
       return_as_stream_(false),
       gzip_compression_(false),
       weak_factory_(this) {
-  if (base::FeatureList::IsEnabled(features::kVizDisplayCompositor) ||
+  bool use_video_capture_api =
+      base::FeatureList::IsEnabled(features::kVizDisplayCompositor) ||
       base::FeatureList::IsEnabled(
-          features::kUseVideoCaptureApiForDevToolsSnapshots)) {
+          features::kUseVideoCaptureApiForDevToolsSnapshots);
+#ifdef OS_ANDROID
+  // Video capture API cannot be used on Android WebView.
+  if (!CompositorImpl::IsInitialized())
+    use_video_capture_api = false;
+#endif
+  if (use_video_capture_api) {
     video_consumer_ =
         std::make_unique<DevToolsVideoConsumer>(base::BindRepeating(
             &TracingHandler::OnFrameFromVideoConsumer, base::Unretained(this)));
