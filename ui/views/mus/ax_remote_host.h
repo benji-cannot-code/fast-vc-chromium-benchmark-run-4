2fd43b4446121bccdad6096dfa6dfc8e176b3fb3
James Cook <jamescook@chromium.org>
2018-08-04 00:56:12 +0000

    chromeos: Speculative fix for accessibility crash in shortcut viewer app
    The crash appears to be an accessibility change event triggered by an
    aura::Window visibility change in the middle of a views::Widget close.
    I have not been able to reproduce.
    
    AXRemoteHost owns the AXTreeSource and AXTreeSerializer in the remote
    app process. Change it to stop sending AX node tree updates to the
    browser process earlier in Widget close.
    
    Also add a defensive null check for a missing widget, explicit cleanup
    of the AXRemoteHost slightly earlier during remote process shutdown,
    and some DCHECKs.
    
    Bug: 869608
    Test: added to views_mus_unittests
    Change-Id: I5a69557382f457258d619e61342ddeca8a57aaf0
    Reviewed-on: https://chromium-review.googlesource.com/1162520
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580719}

--- ui/views/mus/ax_remote_host.h
+++ ui/views/mus/ax_remote_host.h
@@ -66,6 +66,7 @@ class VIEWS_MUS_EXPORT AXRemoteHost : public ax::mojom::AXRemoteHost,
   void PerformAction(const ui::AXActionData& action) override;
 
   // WidgetObserver:
+  void OnWidgetClosing(Widget* widget) override;
   void OnWidgetDestroying(Widget* widget) override;
 
   // AXAuraObjCache::Delegate:
@@ -74,6 +75,7 @@ class VIEWS_MUS_EXPORT AXRemoteHost : public ax::mojom::AXRemoteHost,
                ax::mojom::Event event_type) override;
 
   void FlushForTesting();
+  Widget* widget_for_testing() { return widget_; }
 
  private:
   // Registers this object as a remote host for the parent AXHost.
