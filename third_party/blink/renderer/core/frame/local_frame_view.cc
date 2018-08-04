f646090db86e828dfc5330ccabc1cc64de9e0286
Philip Rogers <pdr@chromium.org>
2018-08-04 03:00:36 +0000

    Add new UMA for compositing: Blink.CompositingCommit.UpdateTime
    PaintArtifactCompositor is a new compositing algorithm (slimming paint
    v2, SPV2) and we also have a legacy algorithm. Blink gen property trees
    is an incremental step towards lunching the new algorithm, and uses
    parts of both the new and legacy systems. To disambiguate our metrics,
    the PaintArtifactCompositor metrics are being renamed to
    Blink.CompositingCommit.UpdateTime.
    
    SPV175 (current):
    Only Blink.Compositing.UpdateTime
    
    BlinkGenPropertyTrees:
    Blink.CompositingCommit.UpdateTime and Blink.Compositing.UpdateTime
    
    SPV2:
    Only Blink.CompositingCommit.UpdateTime
    
    Bug: 865168
    
    Change-Id: Ia676208a8b10cf14f5add11c09f2d7884b5d71e1
    Reviewed-on: https://chromium-review.googlesource.com/1145478
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580735}

--- third_party/blink/renderer/core/frame/local_frame_view.cc
+++ third_party/blink/renderer/core/frame/local_frame_view.cc
@@ -170,6 +170,7 @@ constexpr size_t kCssFragmentIdentifierPrefixLength =
 // EnsureUkmTimeAggregator().
 enum class UkmMetricNames {
   kCompositing,
+  kCompositingCommit,
   kIntersectionObservation,
   kPaint,
   kPrePaint,
@@ -2525,8 +2526,8 @@ bool LocalFrameView::RunCompositingLifecyclePhase(
   DCHECK(layout_view);
 
   if (!RuntimeEnabledFeatures::SlimmingPaintV2Enabled()) {
-    SCOPED_UMA_AND_UKM_TIMER("Blink.Compositing.UpdateTime",
-                             UkmMetricNames::kCompositing);
+    SCOPED_UMA_AND_UKM_TIMER("Blink.CompositingCommit.UpdateTime",
+                             UkmMetricNames::kCompositingCommit);
     layout_view->Compositor()->UpdateIfNeededRecursive(target_state);
   } else {
     ForAllNonThrottledLocalFrameViews([](LocalFrameView& frame_view) {
@@ -4440,8 +4441,8 @@ UkmTimeAggregator& LocalFrameView::EnsureUkmTimeAggregator() {
         frame_->GetDocument()->UkmRecorder(),
         // Note that changing the order or values of the following vector
         // requires changing the UkmMetricNames enum.
-        {"Compositing", "IntersectionObservation", "Paint", "PrePaint",
-         "StyleAndLayout"},
+        {"Compositing", "CompositingCommit", "IntersectionObservation", "Paint",
+         "PrePaint", "StyleAndLayout"},
         TimeDelta::FromSeconds(30)));
   }
   return *ukm_time_aggregator_;
