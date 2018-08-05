df0662f8139a587ebd5c9615035bce7f8a604d77
Gabriel Charette <gab@chromium.org>
2018-08-05 21:54:26 +0000

    [TaskScheduler] Migrate one more include committed after mass migration
    This is https://crrev.com/c/1161179 applied further up the tree
    while trying to land https://crrev.com/c/1161088
    
    TBR=skyostil@chromium.org
    
    Bug: 867421
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Change-Id: I3d9830332e9c7331faa2576e92677274bfa06273
    Reviewed-on: https://chromium-review.googlesource.com/1163285
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580786}

--- services/network/p2p/socket_manager.cc
+++ services/network/p2p/socket_manager.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 
 #include "base/bind.h"
-#include "base/task_scheduler/post_task.h"
+#include "base/task/post_task_forward.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "net/base/address_list.h"
 #include "net/base/completion_callback.h"
