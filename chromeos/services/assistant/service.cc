3d1cb8fc110d901f582633848f8708d6351531c4
Xiaohui Chen <xiaohuic@google.com>
2018-08-06 18:35:36 +0000

    Add vlog for debugging
    Added two very infreqent vlog for debug auth issues.
    
    Bug: b:111370621
    Test: locally build
    Change-Id: I77d3f6cc23a8c0cd9d5ab30c59989d1f2d80b464
    Reviewed-on: https://chromium-review.googlesource.com/1162879
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580930}

--- chromeos/services/assistant/service.cc
+++ chromeos/services/assistant/service.cc
@@ -180,6 +180,7 @@ void Service::BindAssistantSettingsManager(
 }
 
 void Service::RequestAccessToken() {
+  VLOG(1) << "Start requesting access token.";
   GetIdentityManager()->GetPrimaryAccountInfo(base::BindOnce(
       &Service::GetPrimaryAccountInfoCallback, base::Unretained(this)));
 }
