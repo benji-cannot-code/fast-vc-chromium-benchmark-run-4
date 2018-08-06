3f089045205e3736ea54a18789fb5954901744e1
Sarah Hu <xiaoyinh@chromium.org>
2018-08-06 17:50:22 +0000

    cros: Update fingerprint unlock UI per latest mock.
    Bug: 835357
    Change-Id: I61be44e86f85175a3472669aecf630ddb5bb2fa2
    Reviewed-on: https://chromium-review.googlesource.com/1159383
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Jacob Dufault <jdufault@chromium.org>
    Commit-Queue: Xiaoyin Hu <xiaoyinh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580914}

--- ash/login/ui/login_user_view.cc
+++ ash/login/ui/login_user_view.cc
@@ -140,7 +140,11 @@ class LoginUserView::UserImage : public NonAccessibleView {
     }
   }
 
-  void SetAnimationEnabled(bool enable) { image_->SetAnimationEnabled(enable); }
+  void SetAnimationEnabled(bool enable) {
+    image_->SetAnimationPlayback(
+        enable ? AnimatedRoundedImageView::Playback::kRepeat
+               : AnimatedRoundedImageView::Playback::kFirstFrameOnly);
+  }
 
  private:
   void OnImageDecoded(AnimationFrames animation) {
@@ -151,7 +155,8 @@ class LoginUserView::UserImage : public NonAccessibleView {
     }
 
     image_->SetAnimationDecoder(
-        std::make_unique<PassthroughAnimationDecoder>(animation));
+        std::make_unique<PassthroughAnimationDecoder>(animation),
+        AnimatedRoundedImageView::Playback::kRepeat);
   }
 
   AnimatedRoundedImageView* image_ = nullptr;
