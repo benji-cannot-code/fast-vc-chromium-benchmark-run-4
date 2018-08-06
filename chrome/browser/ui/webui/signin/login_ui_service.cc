276b869e7bf9a2e3e7c062f74f517178cdb9cafd
Thomas Tangl <tangltom@chromium.org>
2018-08-06 17:20:05 +0000

    [unified-consent] Add metric ConsentBump.SuppressReason
    Add metric to record the suppress reason when the
    consent bump should not be shown.
    
    Additional changes:
     - Don't show consent bump when any on-by-default privacy
     setting was off during migration.
    
    Bug: 858870, 863948
    Change-Id: Iac7bbb6b8d1d332db3439b2f96c6035ed255e6b3
    Reviewed-on: https://chromium-review.googlesource.com/1162233
    Commit-Queue: Thomas Tangl <tangltom@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580902}

--- chrome/browser/ui/webui/signin/login_ui_service.cc
+++ chrome/browser/ui/webui/signin/login_ui_service.cc
@@ -78,6 +78,10 @@ class ConsentBumpActivator : public BrowserListObserver,
     // signed out of Gmail).
     if (token_service->RefreshTokenHasError(
             signin_manager->GetAuthenticatedAccountId())) {
+      unified_consent::UnifiedConsentService* consent_service =
+          UnifiedConsentServiceFactory::GetForProfile(profile_);
+      consent_service->RecordConsentBumpSuppressReason(
+          unified_consent::ConsentBumpSuppressReason::kSyncPaused);
       return;
     }
 
@@ -113,7 +117,8 @@ class ConsentBumpActivator : public BrowserListObserver,
     unified_consent::UnifiedConsentService* consent_service =
         UnifiedConsentServiceFactory::GetForProfile(profile_);
 
-    consent_service->MarkMigrationComplete();
+    consent_service->MarkMigrationComplete(
+        unified_consent::ConsentBumpSuppressReason::kNone);
 
     switch (result) {
       case LoginUIService::CONFIGURE_SYNC_FIRST:
