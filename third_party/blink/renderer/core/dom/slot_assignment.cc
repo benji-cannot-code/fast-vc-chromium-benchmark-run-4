bcf093ec751b665e6620e96869d4fc3ba10982fa
Hayato Ito <hayato@chromium.org>
2018-08-06 06:13:55 +0000

    Remove unused headers from slot_assignment.cc
    Change-Id: I8cfd1db4b3b48b69c478a0bc5a60402e035272eb
    Reviewed-on: https://chromium-review.googlesource.com/1162085
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Hayato Ito <hayato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580810}

--- third_party/blink/renderer/core/dom/slot_assignment.cc
+++ third_party/blink/renderer/core/dom/slot_assignment.cc
@@ -10,12 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/node_traversal.h"
 #include "third_party/blink/renderer/core/dom/shadow_root.h"
 #include "third_party/blink/renderer/core/dom/slot_assignment_engine.h"
-#include "third_party/blink/renderer/core/dom/v0_insertion_point.h"
 #include "third_party/blink/renderer/core/html/forms/html_opt_group_element.h"
 #include "third_party/blink/renderer/core/html/forms/html_select_element.h"
 #include "third_party/blink/renderer/core/html/html_details_element.h"
 #include "third_party/blink/renderer/core/html/html_slot_element.h"
-#include "third_party/blink/renderer/core/html_names.h"
 
 namespace blink {
 
