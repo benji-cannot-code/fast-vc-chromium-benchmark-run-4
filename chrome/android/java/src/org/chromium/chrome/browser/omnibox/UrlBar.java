6bc38cf0fd786c81dd92b7f40da670a4b82023ee
Ted Choc <tedchoc@google.com>
2018-08-06 20:16:26 +0000

    Fix initial focus of the search widget.
    UrlBar#setAllowFocus was not updating the widget if it had
    not drawn a single time.  This internally was done because
    tabbed mode wanted to prevent he UrlBar from gaining focus
    on initial inflation, but tabbed mode never calls setAllowFocus
    except when manipulating the tab switcher and thus would
    never happen before drawn a single time.  This just updates
    setAllowFocus to immediately take action and the lazy setting
    of focus for Tabbed mode still happens in the first draw
    callback (this method can just be set before that has happened,
    which is OK for both SearchWidget and Custom Tabs as they both
    want to set that state as early as possible).
    
    BUG=871121
    
    Change-Id: I74ef57890e223a10583ecbb67179705fb2f11caa
    Reviewed-on: https://chromium-review.googlesource.com/1163754
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Commit-Queue: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580968}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/UrlBar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/UrlBar.java
@@ -299,10 +299,8 @@ public class UrlBar extends AutocompleteEditText {
      */
     public void setAllowFocus(boolean allowFocus) {
         mAllowFocus = allowFocus;
-        if (mFirstDrawComplete) {
-            setFocusable(allowFocus);
-            setFocusableInTouchMode(allowFocus);
-        }
+        setFocusable(allowFocus);
+        setFocusableInTouchMode(allowFocus);
     }
 
     /**
