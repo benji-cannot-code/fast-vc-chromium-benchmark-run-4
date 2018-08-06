eaa48f0227aab2a7b73fcfadf7323860d2f4b05a
Adam Rice <ricea@chromium.org>
2018-08-06 08:32:38 +0000

    Update WritableStream to match standard
    Update WritableStream to standard version
    https://github.com/whatwg/streams/commit/51227372cc84846bdcf68312724c4cac6a4b9e58
    
    Remove failing test expectations.
    
    Changes:
    
    The timing of access of strategy parameters has been changed to
    match the standard. This mostly only affects the tests that are looking
    for it.
    
    Algorithms are cleared out once they will no longer be called, enabling
    garbage collection.
    
    Bug: 866388
    Change-Id: I948a858c101960bd19c826f9d8764575d5360c66
    Reviewed-on: https://chromium-review.googlesource.com/1146622
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580822}

--- third_party/blink/renderer/core/streams/WritableStream.js
+++ third_party/blink/renderer/core/streams/WritableStream.js
@@ -145,9 +145,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       }
 
       InitializeWritableStream(this);
-      const type = underlyingSink.type;
       const size = strategy.size;
       let highWaterMark = strategy.highWaterMark;
+      const type = underlyingSink.type;
       if (type !== undefined) {
         throw new RangeError(streamErrors.invalidType);
       }
@@ -823,7 +823,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // or impossible, so use static dispatch for now. This will have to be fixed
   // when adding a byte controller.
   function WritableStreamDefaultControllerAbortSteps(controller, reason) {
-    return controller[_abortAlgorithm](reason);
+    const result = controller[_abortAlgorithm](reason);
+    WritableStreamDefaultControllerClearAlgorithms(controller);
+    return result;
   }
 
   function WritableStreamDefaultControllerErrorSteps(controller) {
@@ -896,6 +898,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     SetUpWritableStreamDefaultController(stream, controller, startAlgorithm,
         writeAlgorithm, closeAlgorithm, abortAlgorithm, highWaterMark,
         sizeAlgorithm);
+  }
+
+  function WritableStreamDefaultControllerClearAlgorithms(controller) {
+    controller[_writeAlgorithm] = undefined;
+    controller[_closeAlgorithm] = undefined;
+    controller[_abortAlgorithm] = undefined;
   }
 
   function WritableStreamDefaultControllerClose(controller) {
@@ -980,6 +988,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     // assert(controller[_queue].length === 0,
     //        'controller.[[queue]] is empty.');
     const sinkClosePromise = controller[_closeAlgorithm]();
+    WritableStreamDefaultControllerClearAlgorithms(controller);
     thenPromise(
         sinkClosePromise, () => WritableStreamFinishInFlightClose(stream),
         reason => WritableStreamFinishInFlightCloseWithError(stream, reason));
@@ -1006,6 +1015,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           WritableStreamDefaultControllerAdvanceQueueIfNeeded(controller);
         },
         reason => {
+          const state = stream[_stateAndFlags] & STATE_MASK;
+          if (state === WRITABLE) {
+            WritableStreamDefaultControllerClearAlgorithms(controller);
+          }
           WritableStreamFinishInFlightWriteWithError(stream, reason);
         });
   }
@@ -1020,6 +1033,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     const stream = controller[_controlledWritableStream];
     // assert((stream[_stateAndFlags] & STATE_MASK) === WRITABLE,
     //        '_stream_.[[state]] is `"writable"`.');
+    WritableStreamDefaultControllerClearAlgorithms(controller);
     WritableStreamStartErroring(stream, error);
   }
 
