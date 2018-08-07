3ccbf07b8fe0921712c2b6b5e7a4b72899a71a0e
Ella Ge <eirage@chromium.org>
2018-08-07 18:27:05 +0000

    fix video subtitle font size
    font size property is set on CSS pixel, but it's calculated from
    video size, which is in physical pixel after use-zoom-for-dsf enabled
    on Android.
    This CL applys the page zoom factor to the video track font size.
    
    Bug: 870243
    Change-Id: If597e115b8f77395bbd21ac90d0112c7a86ee62f
    Reviewed-on: https://chromium-review.googlesource.com/1160775
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581289}

--- third_party/blink/renderer/core/html/track/text_track_container.cc
+++ third_party/blink/renderer/core/html/track/text_track_container.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/html/track/text_track_container.h"
 
+#include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/html/media/html_video_element.h"
 #include "third_party/blink/renderer/core/html/track/cue_timeline.h"
 #include "third_party/blink/renderer/core/html/track/text_track.h"
@@ -113,6 +114,8 @@ void TextTrackContainer::UpdateDefaultFontSize(
   LayoutUnit smallest_dimension =
       std::min(video_size.Height(), video_size.Width());
   float font_size = smallest_dimension * 0.05f;
+  if (media_layout_object->GetFrame())
+    font_size /= media_layout_object->GetFrame()->PageZoomFactor();
 
   // Avoid excessive FP precision issue.
   // C11 5.2.4.2.2:9 requires assignment and cast to remove extra precision, but
