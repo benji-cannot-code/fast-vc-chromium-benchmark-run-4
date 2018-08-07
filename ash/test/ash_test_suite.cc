2e4b8a757312ca3f04a2ce66036b499b4cd29c81
Scott Violet <sky@chromium.org>
2018-08-07 16:32:14 +0000

    chromeos: forces kOverrideUseSoftwareGLForTests for ash_unittests
    Without this tests that try to connect to ash a service hang. I get the
    following gl errors:
    
    libGL error: No matching fbConfigs or visuals found
    libGL error: failed to load driver: swrast
    [171208:171216:0802/122357.849400:5628725490183:ERROR:gl_context_glx.cc(227)] Couldn't make context current with X drawable.
    [171208:171216:0802/122357.849448:5628725490229:ERROR:gpu_info_collector.cc(68)] gl::GLContext::MakeCurrent() failed
    
    BUG=869739
    TEST=test only change
    
    Change-Id: Ib3fa4e01c715664e17bdcafd1af0ca98c34ff2dc
    Reviewed-on: https://chromium-review.googlesource.com/1161171
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581250}

--- ash/test/ash_test_suite.cc
+++ ash/test/ash_test_suite.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/ui_base_switches.h"
 #include "ui/compositor/test/context_factories_for_test.h"
 #include "ui/gfx/gfx_paths.h"
+#include "ui/gl/gl_switches.h"
 #include "ui/gl/test/gl_surface_test_support.h"
 
 namespace ash {
@@ -27,6 +28,12 @@ AshTestSuite::~AshTestSuite() = default;
 
 void AshTestSuite::Initialize() {
   base::TestSuite::Initialize();
+
+  // Force software-gl. This is necessary for tests that trigger launching ash
+  // in its own process
+  base::CommandLine* cmd_line = base::CommandLine::ForCurrentProcess();
+  cmd_line->AppendSwitch(switches::kOverrideUseSoftwareGLForTests);
+
   gl::GLSurfaceTestSupport::InitializeOneOff();
 
   gfx::RegisterPathProvider();
