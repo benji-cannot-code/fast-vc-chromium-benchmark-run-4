be73e7ea944e6695215312fdd987a3bc44a491e9
Marc Treib <treib@chromium.org>
2018-08-07 11:44:49 +0000

    ProfileSyncService: shut down immediately on unrecoverable errors
    Previously, we'd post a task to perform the shutdown. This meant that
    there was a brief period where the unrecoverable error was already set,
    but Sync was still active, which made things hard to reason about.
    This CL removes the task-posting and instead just directly calls
    OnUnrecoverableErrorImpl (which calls ShutdownImpl), which doesn't seem
    to have any ill effects.
    
    Bug: 839834
    Change-Id: I998488332c531e817ffcd19e44df0e1273d091a5
    Reviewed-on: https://chromium-review.googlesource.com/1163707
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581196}

--- components/browser_sync/profile_sync_service.h
+++ components/browser_sync/profile_sync_service.h
@@ -527,9 +527,10 @@ class ProfileSyncService : public syncer::SyncService,
   // Helper method for managing encryption UI.
   bool IsEncryptedDatatypeEnabled() const;
 
-  // Helper for OnUnrecoverableError and OnInternalUnrecoverableError.
+  // Helper for OnUnrecoverableError.
   void OnUnrecoverableErrorImpl(const base::Location& from_here,
-                                const std::string& message);
+                                const std::string& message,
+                                UnrecoverableErrorReason reason);
 
   // Stops the sync engine. Does NOT set IsSyncRequested to false. Use
   // RequestStop for that. |data_fate| controls whether the local sync data is
@@ -566,12 +567,6 @@ class ProfileSyncService : public syncer::SyncService,
   void UpdateSelectedTypesHistogram(
       bool sync_everything,
       const syncer::ModelTypeSet chosen_types) const;
-
-  // Internal unrecoverable error handler. Used to track error reason via
-  // Sync.UnrecoverableErrors histogram.
-  void OnInternalUnrecoverableError(const base::Location& from_here,
-                                    const std::string& message,
-                                    UnrecoverableErrorReason reason);
 
   // Update UMA for syncing engine.
   void UpdateEngineInitUMA(bool success) const;
