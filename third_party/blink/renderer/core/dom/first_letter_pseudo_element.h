249fa6bdae81db1b24ec54ef7cc15fcdd5e3a67f
Rune Lillesveen <futhark@chromium.org>
2018-08-07 20:06:20 +0000

    Don't re-attach ::first-letter during style recalc.
    Update (create or destroy) the FirstLetterPseudoElement and its style
    at the point where the block level element's layout sub-tree is up-to-
    date. This means we will always attach the layout tree for the
    ::first-letter element in Element::AttachLayoutTree, but the pseudo
    element and its style may be created/updated at style recalc time,
    layout tree rebuild time, or layout tree attachment time depending on
    when we know what will be the LayoutText from which we will format the
    first letter if any.
    
    UpdateFirstLetterPseudoElement is split out from UpdatePseudoElement to
    make the code easier to read as the former case has some exceptional
    cases.
    
    We no longer use the pseudo style cache for ::first-letter as we will
    now compute the style only once per pass with the correct inheritance
    parent.
    
    Bug: 847218
    Change-Id: I7a1e2a60122891fa38998ff85e566bec0a38b513
    Reviewed-on: https://chromium-review.googlesource.com/1155591
    Reviewed-by: Anders Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581320}

--- third_party/blink/renderer/core/dom/first_letter_pseudo_element.h
+++ third_party/blink/renderer/core/dom/first_letter_pseudo_element.h
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class Element;
-class LayoutObject;
 class LayoutText;
 class LayoutTextFragment;
 
@@ -62,10 +61,10 @@ class CORE_EXPORT FirstLetterPseudoElement final : public PseudoElement {
  private:
   explicit FirstLetterPseudoElement(Element*);
 
+  scoped_refptr<ComputedStyle> CustomStyleForLayoutObject() override;
   void DidRecalcStyle(StyleRecalcChange) override;
 
   void AttachFirstLetterTextLayoutObjects(LayoutText* first_letter_text);
-  ComputedStyle* StyleForFirstLetter(LayoutObject*);
 
   LayoutTextFragment* remaining_text_layout_object_;
   DISALLOW_COPY_AND_ASSIGN(FirstLetterPseudoElement);
