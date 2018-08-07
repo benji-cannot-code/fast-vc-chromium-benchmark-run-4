9ce38274aa2ffcd606d79c5de18741bf684cde82
Gauthier Ambard <gambard@chromium.org>
2018-08-07 09:32:52 +0000

    Disable test
    This CL disables JavaScriptDialogTestCase/testShowJavaScriptAlert and
    PrintControllerTestCase/testPrintNormalPage as they are failing on
    iPad iOS 10 simulator for the multitasking test suite.
    
    Bug: 871685
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I0486941bdfcba501001e6c3e8e6e3ff0c997245a
    Reviewed-on: https://chromium-review.googlesource.com/1164944
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Eric Noyau <noyau@chromium.org>
    Reviewed-by: Eric Noyau <noyau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581182}

--- ios/chrome/browser/ui/dialogs/javascript_dialog_egtest.mm
+++ ios/chrome/browser/ui/dialogs/javascript_dialog_egtest.mm
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
 #import "ios/chrome/test/earl_grey/chrome_test_case.h"
+#include "ios/testing/earl_grey/disabled_test_macros.h"
 #import "ios/testing/earl_grey/matchers.h"
 #import "ios/web/public/test/earl_grey/web_view_matchers.h"
 #include "ios/web/public/test/element_selector.h"
@@ -339,7 +340,15 @@ void TapSuppressDialogsButton() {
 #pragma mark - Tests
 
 // Tests that an alert is shown, and that the completion block is called.
+// TODO(crbug.com/871685): Re-enable this test.
 - (void)testShowJavaScriptAlert {
+#if TARGET_IPHONE_SIMULATOR
+  if (IsIPadIdiom() && !base::ios::IsRunningOnIOS11OrLater()) {
+    EARL_GREY_TEST_DISABLED(
+        @"Failing on iOS 10 iPad simulator, for "
+        @"ios_chrome_multitasking_egtests");
+  }
+#endif  // TARGET_IPHONE_SIMULATOR
   // Load the blank test page and show an alert.
   [self loadBlankTestPage];
   ShowJavaScriptDialog(JavaScriptAlertType::ALERT);
