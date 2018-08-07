2f689008bb4c6b32868b321d2c216a40b2be8fd7
Trent Apted <tapted@chromium.org>
2018-08-07 00:42:22 +0000

    Delineate internal/external headers for ui/views/cocoa in views/BUILD.gn
    Since https://crbug.com/732993 was fixed, gn now supports a `friend`
    property which the ui/views/ test targets can use. This means "internal"
    headers only used (also) in tests no longer need to be repeated in test
    targets inside views/BUILD.gn.
    
    So, declare other targets in views/BUILD.gn as friends, remove those
    repeated headers, and mark some things internal.
    
    Start with ui/views/cocoa. Currently 3 headers are used in mac browser
    code (for OS-specific fullscreen/native frame, and touchbar things).
    
    Bug: 871123
    Change-Id: I5e5a260dd200f1bffc4f6fb8456d6e854fe19c52
    Reviewed-on: https://chromium-review.googlesource.com/1163351
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581062}

--- chrome/browser/ui/cocoa/fullscreen/fullscreen_toolbar_controller_views.mm
+++ chrome/browser/ui/cocoa/fullscreen/fullscreen_toolbar_controller_views.mm
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/ui/cocoa/fullscreen/fullscreen_toolbar_controller_views.h"
 
 #include "chrome/browser/ui/views/frame/browser_view.h"
-#include "ui/views/cocoa/bridged_content_view.h"
 #include "ui/views/cocoa/bridged_native_widget.h"
 #include "ui/views/widget/native_widget_mac.h"
 
