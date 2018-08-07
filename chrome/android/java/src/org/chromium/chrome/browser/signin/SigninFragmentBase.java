84998ab8b72b94a9afb1b6a0e3c95aa72faaa9ac
Boris Sazonov <bsazonov@chromium.org>
2018-08-07 15:32:06 +0000

    [Unity][Android] Pin account for consent bump screen
    The consent bump screen is shown for the primary signed in account and
    the account picker in the consent bump screen should not react to
    clicks. This CL adds a fragment argument to pass the account name and
    disables the account picker.
    
    Bug: 869426
    Change-Id: I1f84705d36b78d7f66eb28a546ab4bf825727e30
    Reviewed-on: https://chromium-review.googlesource.com/1165235
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581232}

--- chrome/android/java/src/org/chromium/chrome/browser/signin/SigninFragmentBase.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/SigninFragmentBase.java
@@ -158,10 +158,14 @@ public abstract class SigninFragmentBase
         return result;
     }
 
-    /** Creates an argument bundle for the consent bump screen. */
-    protected static Bundle createArgumentsForConsentBumpFlow() {
+    /**
+     * Creates an argument bundle for the consent bump screen.
+     * @param accountName The name of the signed in account.
+     */
+    protected static Bundle createArgumentsForConsentBumpFlow(String accountName) {
         Bundle result = new Bundle();
         result.putInt(ARGUMENT_SIGNIN_FLOW_TYPE, SigninFlowType.CONSENT_BUMP);
+        result.putString(ARGUMENT_ACCOUNT_NAME, accountName);
         return result;
     }
 
@@ -199,6 +203,11 @@ public abstract class SigninFragmentBase
     /** Returns whether this fragment is in "force sign-in" mode. */
     protected boolean isForcedSignin() {
         return mSigninFlowType == SigninFlowType.FORCED;
+    }
+
+    /** Returns whether this fragment is in Consent bump mode. */
+    protected boolean isConsentBump() {
+        return mSigninFlowType == SigninFlowType.CONSENT_BUMP;
     }
 
     @Override
@@ -364,7 +373,7 @@ public abstract class SigninFragmentBase
     }
 
     private void onAccountPickerClicked() {
-        if (isForcedSignin() || !areControlsEnabled()) return;
+        if (isForcedSignin() || isConsentBump() || !areControlsEnabled()) return;
         showAccountPicker();
     }
 
