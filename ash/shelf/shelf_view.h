6b4c6be9bb612f9386575c46c0b07caaf0684fa1
Avi Drissman <avi@chromium.org>
2018-08-07 14:49:01 +0000

    Revert "cros: Ignore clamshell only requests in tablet mode"
    This reverts commit c05ec33a19fc5eee16cc777f9eecf3faa78e3dc7.
    
    Reason for revert: Kills ASAN/MSAN bots; details in message
    
    Original change's description:
    > cros: Ignore clamshell only requests in tablet mode
    > 
    > There is a bug where users can keep their clamshell mode context menu
    > open by detaching the keyboard. This allows users to have a side shelf
    > in tablet mode, and they may get stuck in this state.
    > 
    > This CL prevents this from happening by ignoring the clamshell options
    > if they are somehow executed in tablet mode.
    > 
    > Bug: 870469
    > Change-Id: Ifb9638c3ae705bd58a2e785edc5ca84d698651d2
    > Reviewed-on: https://chromium-review.googlesource.com/1161387
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#581030}
    
    TBR=xiyuan@chromium.org,newcomer@chromium.org
    
    Change-Id: If3b5e2cf2b9f6012cb165412c54d1d0f15283a07
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 870469
    Reviewed-on: https://chromium-review.googlesource.com/1165422
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581223}

--- ash/shelf/shelf_view.h
+++ ash/shelf/shelf_view.h
@@ -30,10 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/view.h"
 #include "ui/views/view_model.h"
 
-namespace ash {
-class TabletModeController;
-}
-
 namespace ui {
 class SimpleMenuModel;
 }
@@ -115,8 +111,7 @@ class ASH_EXPORT ShelfView : public views::View,
                              public views::ContextMenuController,
                              public views::FocusTraversable,
                              public views::BoundsAnimatorObserver,
-                             public app_list::ApplicationDragAndDropHost,
-                             public ash::TabletModeObserver {
+                             public app_list::ApplicationDragAndDropHost {
  public:
   ShelfView(ShelfModel* model, Shelf* shelf, ShelfWidget* shelf_widget);
   ~ShelfView() override;
@@ -184,10 +179,6 @@ class ASH_EXPORT ShelfView : public views::View,
                            views::View* replaced_view,
                            const gfx::Vector2d& cursor_offset_from_center,
                            float scale_factor) override;
-
-  // Overridden from ash::TabletModeObserver:
-  void OnTabletModeStarted() override;
-  void OnTabletModeEnded() override;
 
   void CreateDragIconProxyByLocationWithNoAnimation(
       const gfx::Point& origin_in_screen_coordinates,
@@ -539,10 +530,6 @@ class ASH_EXPORT ShelfView : public views::View,
 
   // The view which gets replaced by our drag icon proxy.
   views::View* drag_replaced_view_ = nullptr;
-
-  // Observes tablet mode changing.
-  ScopedObserver<ash::TabletModeController, ash::TabletModeObserver>
-      tablet_mode_observer_;
 
   // True when the icon was dragged off the shelf.
   bool dragged_off_shelf_ = false;
