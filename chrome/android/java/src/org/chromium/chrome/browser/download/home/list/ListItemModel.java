b49eabab7231a5b086da25c4961066eccf01bd42
Shakti Sahu <shaktisahu@chromium.org>
2018-08-07 00:33:13 +0000

    Download Home : Added section titles
    1- Modified the mutator logic to maintain the items in a date/section
       ordered hierarchical form
    2- The mutator will add views for sections, section separators and
       date separators.
    
    Several other smaller details such as :
    1 - First section should have less spacing between the preceding date.
    2 - The last date/section shouldn't have a separator.
    
    
    Bug: 866307
    Change-Id: I6eca116c26f403760160d016655fad3c05278c3a
    Reviewed-on: https://chromium-review.googlesource.com/1151062
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581045}

--- chrome/android/java/src/org/chromium/chrome/browser/download/home/list/ListItemModel.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/home/list/ListItemModel.java
@@ -5,20 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.download.home.list;
 
-import org.chromium.chrome.browser.modelutil.ListObservable;
 import org.chromium.chrome.browser.modelutil.PropertyModel;
-import org.chromium.chrome.browser.modelutil.SimpleList;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * This model represents the data required to build a list UI around a set of {@link ListItem}s.
- * This includes (1) a {@link ListObservable} implementation and (2) exposing a
+ * This includes (1) a {@link BatchListObservable} implementation and (2) exposing a
  * {@link PropertyModel} for shared item properties and general list information.
  */
-class ListItemModel extends BatchListObservable implements SimpleList<ListItem> {
-    private final List<ListItem> mItems = new ArrayList<>();
+class ListItemModel extends BatchListObservable<ListItem> {
     private final PropertyModel mListProperties = new PropertyModel(ListProperties.ALL_KEYS);
 
     /**
@@ -27,34 +21,5 @@ class ListItemModel extends BatchListObservable implements SimpleList<ListItem> 
      */
     public PropertyModel getProperties() {
         return mListProperties;
-    }
-
-    /** Adds {@code item} to this list at {@code index}. */
-    public void addItem(int index, ListItem item) {
-        mItems.add(index, item);
-        notifyItemInserted(index);
-    }
-
-    /** Removes the {@link ListItem} at {@code index}. */
-    public void removeItem(int index) {
-        mItems.remove(index);
-        notifyItemRemoved(index);
-    }
-
-    /** Sets the {@link ListItem} at {@code index} to {@code item}. */
-    public void setItem(int index, ListItem item) {
-        mItems.set(index, item);
-        notifyItemChanged(index);
-    }
-
-    // SimpleList implementation.
-    @Override
-    public ListItem get(int index) {
-        return mItems.get(index);
-    }
-
-    @Override
-    public int size() {
-        return mItems.size();
     }
 }
