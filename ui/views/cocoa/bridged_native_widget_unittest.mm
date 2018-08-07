fa35c214218a52a6f6f0b694f69d7d9413cc26a1
Christopher Cameron <ccameron@chromium.org>
2018-08-07 06:07:04 +0000

    RemoteMacViews: Add BridgedNativeWidgetHost[Impl]
    Add an empty BridgedNativeWidgetHost interface and a largely-empty
    BridgedNativeWidgetHostImpl implementation. These will be the browser
    process part of BridgedNativeWidget.
    
    Update NativeWidgetMac to have a BridgedNativeWidgetHost instead of a
    BridgedNativeWidget, but still allow calls through the old interface.
    
    The next steps will be a long process of shuffling members and methods
    to these interfaces.
    
    Bug: 859152
    Change-Id: Id8be908302a32e52dc0ae1bf54fe19a285adccef
    Reviewed-on: https://chromium-review.googlesource.com/1162875
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581146}

--- ui/views/cocoa/bridged_native_widget_unittest.mm
+++ ui/views/cocoa/bridged_native_widget_unittest.mm
@@ -289,9 +289,7 @@ class MockNativeWidgetMac : public NativeWidgetMac {
  public:
   explicit MockNativeWidgetMac(internal::NativeWidgetDelegate* delegate)
       : NativeWidgetMac(delegate) {}
-
-  // Expose a reference, so that it can be reset() independently.
-  std::unique_ptr<BridgedNativeWidget>& bridge() { return bridge_; }
+  using NativeWidgetMac::bridge;
 
   // internal::NativeWidgetPrivate:
   void InitNativeWidget(const Widget::InitParams& params) override {
@@ -302,7 +300,7 @@ class MockNativeWidgetMac : public NativeWidgetMac {
     delegate()->OnNativeWidgetCreated(true);
 
     // To allow events to dispatch to a view, it needs a way to get focus.
-    bridge_->SetFocusManager(GetWidget()->GetFocusManager());
+    bridge()->SetFocusManager(GetWidget()->GetFocusManager());
   }
 
   void ReorderNativeViews() override {
@@ -325,9 +323,7 @@ class BridgedNativeWidgetTestBase : public ui::CocoaTest {
   explicit BridgedNativeWidgetTestBase(SkipInitialization tag)
       : native_widget_mac_(nullptr) {}
 
-  std::unique_ptr<BridgedNativeWidget>& bridge() {
-    return native_widget_mac_->bridge();
-  }
+  BridgedNativeWidget* bridge() { return native_widget_mac_->bridge(); }
 
   // Overridden from testing::Test:
   void SetUp() override {
