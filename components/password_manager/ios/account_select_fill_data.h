d391a15a5f9fc7d449eb26cd810b70a09ffa6537
Scott Wu <scottwu@chromium.org>
2018-08-07 09:23:17 +0000

    Move password form serialization logic into components folder
    The purpose is to share this logic with both ios chrome and ios web view
    
    Bug: 865114
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Id6f16a05eb3408238c2772b420ec408e499ed2e8
    Reviewed-on: https://chromium-review.googlesource.com/1151181
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: John Wu <jzw@chromium.org>
    Reviewed-by: Hiroshi Ichikawa <ichikawa@chromium.org>
    Commit-Queue: Scott Wu <scottwu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581180}

--- components/password_manager/ios/account_select_fill_data.h
+++ components/password_manager/ios/account_select_fill_data.h
@@ -1,10 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2017 The Chromium Authors. All rights reserved.
+// Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef IOS_CHROME_BROWSER_PASSWORDS_ACCOUNT_SELECT_FILL_DATA_H_
-#define IOS_CHROME_BROWSER_PASSWORDS_ACCOUNT_SELECT_FILL_DATA_H_
+#ifndef COMPONENTS_PASSWORD_MANAGER_IOS_ACCOUNT_SELECT_FILL_DATA_H_
+#define COMPONENTS_PASSWORD_MANAGER_IOS_ACCOUNT_SELECT_FILL_DATA_H_
 
 #include <map>
 #include <memory>
@@ -128,4 +128,4 @@ class AccountSelectFillData {
 
 }  // namespace  password_manager
 
-#endif  // IOS_CHROME_BROWSER_PASSWORDS_ACCOUNT_SELECT_FILL_DATA_H_
+#endif  // COMPONENTS_PASSWORD_MANAGER_IOS_ACCOUNT_SELECT_FILL_DATA_H_
--- components/password_manager/ios/test_helpers.h
+++ components/password_manager/ios/test_helpers.h
@@ -1,10 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2017 The Chromium Authors. All rights reserved.
+// Copyright (c) 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef IOS_CHROME_BROWSER_PASSWORDS_TEST_HELPERS_H_
-#define IOS_CHROME_BROWSER_PASSWORDS_TEST_HELPERS_H_
+#ifndef COMPONENTS_PASSWORD_MANAGER_IOS_TEST_HELPERS_H_
+#define COMPONENTS_PASSWORD_MANAGER_IOS_TEST_HELPERS_H_
 
 #include <string>
 
@@ -28,4 +28,4 @@ void SetPasswordFormFillData(autofill::PasswordFormFillData& form_data,
 
 }  // namespace  test_helpers
 
-#endif  // IOS_CHROME_BROWSER_PASSWORDS_TEST_HELPERS_H_
+#endif  // COMPONENTS_PASSWORD_MANAGER_IOS_TEST_HELPERS_H_
