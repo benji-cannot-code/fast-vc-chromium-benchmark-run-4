c927edcaf01b2a3afcf9d2010922f39e4052b2da
Min Qin <qinmin@chromium.org>
2018-08-07 22:08:07 +0000

    Handle some cases download is running without full browser
    When browser is not fully running, Profile cannot be accessed.
    This CL also disables the use of toast as it is very expensive when
    full browser is not running.
    
    BUG=695115
    
    Change-Id: I7560e3684307741e0a1ba01ec8efec0ff97fdbc4
    Reviewed-on: https://chromium-review.googlesource.com/1165849
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581358}

--- chrome/android/java/src/org/chromium/chrome/browser/download/DownloadManagerService.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/DownloadManagerService.java
@@ -1114,7 +1114,9 @@ public class DownloadManagerService
      */
     private long getNativeDownloadManagerService() {
         if (mNativeDownloadManagerService == 0) {
-            mNativeDownloadManagerService = nativeInit();
+            mNativeDownloadManagerService =
+                    nativeInit(BrowserStartupController.get(LibraryProcessType.PROCESS_BROWSER)
+                                       .isStartupSuccessfullyCompleted());
         }
         return mNativeDownloadManagerService;
     }
@@ -1888,7 +1890,7 @@ public class DownloadManagerService
     private static native boolean nativeIsSupportedMimeType(String mimeType);
     private static native int nativeGetAutoResumptionLimit();
 
-    private native long nativeInit();
+    private native long nativeInit(boolean isFullBrowserStarted);
     private native void nativeOpenDownload(long nativeDownloadManagerService, String downloadGuid,
             boolean isOffTheRecord, int source);
     private native void nativeResumeDownload(
