1c08acffc037095d1bb805fa5b3a3bfde82d227d
Moe Ahmadi <mahmadi@chromium.org>
2018-08-07 21:50:36 +0000

    [AF] Add the new Autofill prefs to the extensions prefs API
    Adds kAutofillCreditCardEnabled and kAutofillProfileEnabled to
    the extensions prefs API
    
    Bug: 870328
    Change-Id: Ic5961b3cd58c58381764f6eddf2698df72db8695
    Reviewed-on: https://chromium-review.googlesource.com/1161002
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581354}

--- chrome/test/data/extensions/api_test/preference/standard/test.js
+++ chrome/test/data/extensions/api_test/preference/standard/test.js
@@ -34,6 +34,8 @@ var preferences_to_test = [
     preferences: {
       alternateErrorPagesEnabled: false,
       autofillEnabled: false,
+      autofillAddressEnabled: false,
+      autofillCreditCardEnabled: false,
       passwordSavingEnabled: false,
       safeBrowsingEnabled: false,
       safeBrowsingExtendedReportingEnabled: false,
