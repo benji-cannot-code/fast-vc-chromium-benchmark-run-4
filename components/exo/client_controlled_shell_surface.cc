7ad25a8d0aa64e14ee10dbfd677b34dfa337c206
Mitsuru Oshima <oshima@chromium.org>
2018-08-07 02:40:42 +0000

    Fix Autohide -> None frame traisntion
    The transition didn't work correctly but no one was using
    this transition except pinned mode. This is actually odd
    transition, but this should work regardless.
    
    I will look into why pinned mode does this separately.
    
    Bug: b/112240082
    Test: covered by unittests
    Change-Id: I88db9dfb614895f19951fd822185460bf682948a
    Reviewed-on: https://chromium-review.googlesource.com/1164570
    Reviewed-by: Daichi Hirono <hirono@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581104}

--- components/exo/client_controlled_shell_surface.cc
+++ components/exo/client_controlled_shell_surface.cc
@@ -908,8 +908,9 @@ gfx::Point ClientControlledShellSurface::GetSurfaceOrigin() const {
 // ClientControlledShellSurface, private:
 
 void ClientControlledShellSurface::UpdateFrame() {
-  if (!widget_ || !GetFrameView()->visible())
+  if (!widget_)
     return;
+
   gfx::Rect work_area =
       display::Screen::GetScreen()
           ->GetDisplayNearestWindow(widget_->GetNativeWindow())
