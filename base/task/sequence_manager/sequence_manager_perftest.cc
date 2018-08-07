ea163b5d0e0d2e5f771ec9ac908b85c4347c40bd
Greg Kraynov <kraynov@chromium.org>
2018-08-07 08:43:38 +0000

    Support MessagePump in SequenceManager perf tests.
    SequenceManagerPerfTest now runs in both environments.
    
    Bug: 828835
    Change-Id: Ib06dc1bbabd4fe972c428605247f6bf86b2c9825
    Reviewed-on: https://chromium-review.googlesource.com/1163883
    Reviewed-by: Alex Clarke <alexclarke@chromium.org>
    Commit-Queue: Greg Kraynov <kraynov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581168}

--- base/task/sequence_manager/sequence_manager_perftest.cc
+++ base/task/sequence_manager/sequence_manager_perftest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/sequence_manager/test/sequence_manager_for_test.h"
 #include "base/task/sequence_manager/test/test_task_queue.h"
 #include "base/task/sequence_manager/test/test_task_time_observer.h"
+#include "base/task/sequence_manager/thread_controller_with_message_pump_impl.h"
 #include "base/threading/thread.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/time/default_tick_clock.h"
@@ -53,7 +54,12 @@ class PerfTestTimeDomain : public MockTimeDomain {
   DISALLOW_COPY_AND_ASSIGN(PerfTestTimeDomain);
 };
 
-class SequenceManagerPerfTest : public testing::Test {
+enum class PerfTestType : int {
+  kUseMessageLoop = 0,
+  kUseMessagePump = 1,
+};
+
+class SequenceManagerPerfTest : public testing::TestWithParam<PerfTestType> {
  public:
   SequenceManagerPerfTest()
       : num_queues_(0),
@@ -65,6 +71,42 @@ class SequenceManagerPerfTest : public testing::Test {
   void SetUp() override {
     if (ThreadTicks::IsSupported())
       ThreadTicks::WaitUntilInitialized();
+
+    delayed_task_closure_ = BindRepeating(
+        &SequenceManagerPerfTest::TestDelayedTask, Unretained(this));
+
+    immediate_task_closure_ = BindRepeating(
+        &SequenceManagerPerfTest::TestImmediateTask, Unretained(this));
+
+    switch (GetParam()) {
+      case PerfTestType::kUseMessageLoop:
+        CreateSequenceManagerWithMessageLoop();
+        break;
+      case PerfTestType::kUseMessagePump:
+        CreateSequenceManagerWithMessagePump();
+        break;
+    }
+
+    time_domain_ = std::make_unique<PerfTestTimeDomain>();
+    manager_->RegisterTimeDomain(time_domain_.get());
+    manager_->AddTaskTimeObserver(&test_task_time_observer_);
+  }
+
+  void CreateSequenceManagerWithMessageLoop() {
+    message_loop_ = std::make_unique<MessageLoop>();
+    manager_ = SequenceManagerForTest::Create(message_loop_.get(),
+                                              message_loop_->task_runner(),
+                                              DefaultTickClock::GetInstance());
+  }
+
+  void CreateSequenceManagerWithMessagePump() {
+    manager_ = SequenceManagerForTest::Create(
+        std::make_unique<internal::ThreadControllerWithMessagePumpImpl>(
+            DefaultTickClock::GetInstance()));
+    // ThreadControllerWithMessagePumpImpl doesn't provide a default tas runner.
+    scoped_refptr<TaskQueue> default_task_queue =
+        manager_->CreateTaskQueue<TestTaskQueue>(TaskQueue::Spec("default"));
+    manager_->SetDefaultTaskRunner(default_task_queue);
   }
 
   void TearDown() override {
@@ -75,25 +117,10 @@ class SequenceManagerPerfTest : public testing::Test {
 
   void Initialize(size_t num_queues) {
     num_queues_ = num_queues;
-    message_loop_.reset(new MessageLoop());
-    manager_ = SequenceManagerForTest::Create(message_loop_.get(),
-                                              message_loop_->task_runner(),
-                                              DefaultTickClock::GetInstance());
-    manager_->AddTaskTimeObserver(&test_task_time_observer_);
-
-    time_domain_.reset(new PerfTestTimeDomain());
-    manager_->RegisterTimeDomain(time_domain_.get());
-
     for (size_t i = 0; i < num_queues; i++) {
       queues_.push_back(manager_->CreateTaskQueue<TestTaskQueue>(
           TaskQueue::Spec("test").SetTimeDomain(time_domain_.get())));
     }
-
-    delayed_task_closure_ = BindRepeating(
-        &SequenceManagerPerfTest::TestDelayedTask, Unretained(this));
-
-    immediate_task_closure_ = BindRepeating(
-        &SequenceManagerPerfTest::TestImmediateTask, Unretained(this));
   }
 
   void TestDelayedTask() {
@@ -185,8 +212,19 @@ class SequenceManagerPerfTest : public testing::Test {
       now = ThreadTicks::Now();
       num_iterations++;
     } while (now - start < TimeDelta::FromSeconds(5));
+
+    std::string trace_suffix;
+    switch (GetParam()) {
+      case PerfTestType::kUseMessageLoop:
+        trace_suffix = " with message loop";
+        break;
+      case PerfTestType::kUseMessagePump:
+        trace_suffix = " with message pump";
+        break;
+    }
+
     perf_test::PrintResult(
-        "task", "", trace,
+        "task", "", trace + trace_suffix,
         (now - start).InMicroseconds() / static_cast<double>(num_iterations),
         "us/run", true);
   }
@@ -208,7 +246,12 @@ class SequenceManagerPerfTest : public testing::Test {
   TestTaskTimeObserver test_task_time_observer_;
 };
 
-TEST_F(SequenceManagerPerfTest, RunTenThousandDelayedTasks_OneQueue) {
+INSTANTIATE_TEST_CASE_P(,
+                        SequenceManagerPerfTest,
+                        testing::Values(PerfTestType::kUseMessageLoop,
+                                        PerfTestType::kUseMessagePump));
+
+TEST_P(SequenceManagerPerfTest, RunTenThousandDelayedTasks_OneQueue) {
   if (!ThreadTicks::IsSupported())
     return;
   Initialize(1u);
@@ -219,7 +262,7 @@ TEST_F(SequenceManagerPerfTest, RunTenThousandDelayedTasks_OneQueue) {
                           Unretained(this), 10000));
 }
 
-TEST_F(SequenceManagerPerfTest, RunTenThousandDelayedTasks_FourQueues) {
+TEST_P(SequenceManagerPerfTest, RunTenThousandDelayedTasks_FourQueues) {
   if (!ThreadTicks::IsSupported())
     return;
   Initialize(4u);
@@ -230,7 +273,7 @@ TEST_F(SequenceManagerPerfTest, RunTenThousandDelayedTasks_FourQueues) {
                           Unretained(this), 10000));
 }
 
-TEST_F(SequenceManagerPerfTest, RunTenThousandDelayedTasks_EightQueues) {
+TEST_P(SequenceManagerPerfTest, RunTenThousandDelayedTasks_EightQueues) {
   if (!ThreadTicks::IsSupported())
     return;
   Initialize(8u);
@@ -241,7 +284,7 @@ TEST_F(SequenceManagerPerfTest, RunTenThousandDelayedTasks_EightQueues) {
                           Unretained(this), 10000));
 }
 
-TEST_F(SequenceManagerPerfTest, RunTenThousandDelayedTasks_ThirtyTwoQueues) {
+TEST_P(SequenceManagerPerfTest, RunTenThousandDelayedTasks_ThirtyTwoQueues) {
   if (!ThreadTicks::IsSupported())
     return;
   Initialize(32u);
@@ -252,7 +295,7 @@ TEST_F(SequenceManagerPerfTest, RunTenThousandDelayedTasks_ThirtyTwoQueues) {
                           Unretained(this), 10000));
 }
 
-TEST_F(SequenceManagerPerfTest, RunTenThousandImmediateTasks_OneQueue) {
+TEST_P(SequenceManagerPerfTest, RunTenThousandImmediateTasks_OneQueue) {
   if (!ThreadTicks::IsSupported())
     return;
   Initialize(1u);
@@ -264,7 +307,7 @@ TEST_F(SequenceManagerPerfTest, RunTenThousandImmediateTasks_OneQueue) {
                     Unretained(this), 10000));
 }
 
-TEST_F(SequenceManagerPerfTest, RunTenThousandImmediateTasks_FourQueues) {
+TEST_P(SequenceManagerPerfTest, RunTenThousandImmediateTasks_FourQueues) {
   if (!ThreadTicks::IsSupported())
     return;
   Initialize(4u);
@@ -276,7 +319,7 @@ TEST_F(SequenceManagerPerfTest, RunTenThousandImmediateTasks_FourQueues) {
                     Unretained(this), 10000));
 }
 
-TEST_F(SequenceManagerPerfTest, RunTenThousandImmediateTasks_EightQueues) {
+TEST_P(SequenceManagerPerfTest, RunTenThousandImmediateTasks_EightQueues) {
   if (!ThreadTicks::IsSupported())
     return;
   Initialize(8u);
@@ -288,7 +331,7 @@ TEST_F(SequenceManagerPerfTest, RunTenThousandImmediateTasks_EightQueues) {
                     Unretained(this), 10000));
 }
 
-TEST_F(SequenceManagerPerfTest, RunTenThousandImmediateTasks_ThirtyTwoQueues) {
+TEST_P(SequenceManagerPerfTest, RunTenThousandImmediateTasks_ThirtyTwoQueues) {
   if (!ThreadTicks::IsSupported())
     return;
   Initialize(32u);
