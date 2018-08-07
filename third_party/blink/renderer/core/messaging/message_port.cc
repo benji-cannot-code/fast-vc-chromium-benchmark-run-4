0b206c1f6d50fe54cb34d82fa9fe85185a7292ee
Kenneth Russell <kbr@chromium.org>
2018-08-07 17:22:10 +0000

    Revert "Reland [blink] Add a time limit to message processing in MessagePort"
    This reverts commit a0c5484dfce884bf6ce702147399556faa84500e.
    
    Reason for revert: Still causes failures of maps_pixel_test, this time on Nexus 5 device:
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Android%20FYI%20Release%20%28Nexus%205%29/7545 . Previously seen in http://crbug.com/870001 .
    
    Original change's description:
    > Reland [blink] Add a time limit to message processing in MessagePort
    > 
    > In addition to existing limit of 200 messages in a single task also
    > limit it to 10 milliseconds to avoid starvation.
    > 
    > This logic will be removed when message-per-task mojo dispatching
    > will be implemented.
    > 
    > BUG=867133
    > R=​mek@chromium.org
    > 
    > Reviewed-on: https://chromium-review.googlesource.com/1152815
    > Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    > Commit-Queue: Alexander Timin <altimin@chromium.org>
    > Cr-Original-Commit-Position: refs/heads/master@{#579751}
    > Change-Id: Idb757ddbc91c3e732e34e834bf8e00cc2292ddb5
    > Reviewed-on: https://chromium-review.googlesource.com/1163782
    > Cr-Commit-Position: refs/heads/master@{#581184}
    
    TBR=mek@chromium.org,altimin@chromium.org
    
    Change-Id: I9de23aa6fa95b06e5aa47cedc30ecd5f9d6ccaed
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 867133
    Reviewed-on: https://chromium-review.googlesource.com/1165527
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581264}

--- third_party/blink/renderer/core/messaging/message_port.cc
+++ third_party/blink/renderer/core/messaging/message_port.cc
@@ -48,12 +48,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-// TODO(altimin): Remove these after per-task mojo dispatching.
 // The maximum number of MessageEvents to dispatch from one task.
-constexpr int kMaximumMessagesPerTask = 200;
-// The threshold to stop processing new tasks.
-constexpr base::TimeDelta kYieldThreshold =
-    base::TimeDelta::FromMilliseconds(50);
+static const int kMaximumMessagesPerTask = 200;
 
 MessagePort* MessagePort::Create(ExecutionContext& execution_context) {
   return new MessagePort(execution_context);
@@ -250,13 +246,12 @@ bool MessagePort::Accept(mojo::Message* mojo_message) {
   // the connector is temporarily paused after |kMaximumMessagesPerTask| have
   // been received without other tasks having had a chance to run (in particular
   // the ResetMessageCount task posted here).
-  // TODO(altimin): Remove this after per-task mojo dispatching lands[1].
-  // [1] https://chromium-review.googlesource.com/c/chromium/src/+/1145692
   if (messages_in_current_task_ == 0) {
     task_runner_->PostTask(FROM_HERE, WTF::Bind(&MessagePort::ResetMessageCount,
                                                 WrapWeakPersistent(this)));
   }
-  if (ShouldYieldAfterNewMessage()) {
+  ++messages_in_current_task_;
+  if (messages_in_current_task_ > kMaximumMessagesPerTask) {
     connector_->PauseIncomingMethodCallProcessing();
   }
 
@@ -297,20 +292,9 @@ bool MessagePort::Accept(mojo::Message* mojo_message) {
 void MessagePort::ResetMessageCount() {
   DCHECK_GT(messages_in_current_task_, 0);
   messages_in_current_task_ = 0;
-  task_start_time_ = base::nullopt;
   // No-op if not paused already.
   if (connector_)
     connector_->ResumeIncomingMethodCallProcessing();
-}
-
-bool MessagePort::ShouldYieldAfterNewMessage() {
-  ++messages_in_current_task_;
-  if (messages_in_current_task_ > kMaximumMessagesPerTask)
-    return true;
-  base::TimeTicks now = base::TimeTicks::Now();
-  if (!task_start_time_)
-    task_start_time_ = now;
-  return now - task_start_time_.value() > kYieldThreshold;
 }
 
 }  // namespace blink
