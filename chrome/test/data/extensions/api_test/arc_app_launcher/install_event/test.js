e1f2c1a3fca361f3a343d712fae769b5cca9c0a4
Wenzhao Zang <wzang@chromium.org>
2018-08-07 22:27:15 +0000

    cros: Add onInstalled event to arcAppsPrivate API
    The "installed" event is fired when
    ArcAppListPrefs::Observer::OnAppRegistered is called, which is the time
    that |launchApp| can be used to actually launch the app.
    
    Bug: 819404
    Change-Id: If874607a351fb2e0e24637c7b6b4c1013b6812ae
    Reviewed-on: https://chromium-review.googlesource.com/1152703
    Commit-Queue: Wenzhao (Colin) Zang <wzang@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581370}

---
+++ chrome/test/data/extensions/api_test/arc_app_launcher/install_event/test.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var EXPECTED_APP_ID = null;
+
+chrome.test.runTests([
+  function getConfig() {
+    chrome.test.getConfig(chrome.test.callbackPass(config => {
+      EXPECTED_APP_ID = config.customArg;
+    }));
+  },
+
+  function onInstalled() {
+    chrome.test.assertTrue(!!EXPECTED_APP_ID);
+    chrome.test.listenOnce(chrome.arcAppsPrivate.onInstalled, appInfo => {
+      chrome.test.assertEq({id: EXPECTED_APP_ID}, appInfo);
+      chrome.arcAppsPrivate.launchApp(appInfo.id, chrome.test.callbackPass());
+    });
+    chrome.test.sendMessage('ready');
+  }
+]);
