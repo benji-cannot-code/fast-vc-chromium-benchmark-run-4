4a8c75c424b55249e6fda6a1511ee7a2cdecfa8b
erikchen <erikchen@chromium.org>
2018-08-07 19:29:43 +0000

    Implement native event observer for linux.
    This CL uses ui::PlatformEventSource observer API to hook into pre and post
    native event dispatch on Linux.
    
    Timestamps for X11 events are in X11 server time. The implementation makes a
    round-trip to the X11 server to obtain a delta, and then uses that delta with
    base::TimeTicks::Now() to convert to a base::TimeTicks.
    
    The overhead from the rount-trip seems reasonable. 99th percentile X11 RTT is
    3ms. 99.9th percentile is 16ms. 99.99th percentile is 40ms. These include the
    time for syscalls to base::TimeTicks::Now().
    
    To further reduce overhead, this CL changes the UMA metric to only be computed
    with 1/1000 frequency, to avoid two calls to base::TimeTicks::Now().
    
    Note: The implementation of native event observer will be turned on via Finch
    experiment to measure overhead.
    
    Change-Id: I786537f093e862a7a19630c9150606a2eb8267d9
    Bug: 859155
    Reviewed-on: https://chromium-review.googlesource.com/1162658
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581304}

--- content/browser/scheduler/responsiveness/native_event_observer.h
+++ content/browser/scheduler/responsiveness/native_event_observer.h
@@ -15,6 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/native_event_processor_observer_mac.h"
 #endif
 
+#if defined(OS_LINUX)
+#include "ui/events/platform/platform_event_observer.h"
+#endif
+
 namespace content {
 namespace responsiveness {
 
@@ -32,6 +36,8 @@ namespace responsiveness {
 class CONTENT_EXPORT NativeEventObserver
 #if defined(OS_MACOSX)
     : public NativeEventProcessorObserver
+#elif defined(OS_LINUX)
+    : public ui::PlatformEventObserver
 #endif
 {
  public:
@@ -47,7 +53,12 @@ class CONTENT_EXPORT NativeEventObserver
   // processor. The destructor will unregister the object.
   NativeEventObserver(WillRunEventCallback will_run_event_callback,
                       DidRunEventCallback did_run_event_callback);
+
+#if defined(OS_LINUX)
+  ~NativeEventObserver() override;
+#else
   virtual ~NativeEventObserver();
+#endif
 
  protected:
 #if defined(OS_MACOSX)
@@ -56,6 +67,11 @@ class CONTENT_EXPORT NativeEventObserver
   void WillRunNativeEvent(const void* opaque_identifier) override;
   void DidRunNativeEvent(const void* opaque_identifier,
                          base::TimeTicks creation_time) override;
+#elif defined(OS_LINUX)
+  // PlatformEventObserver overrides:
+  // Exposed for tests.
+  void WillProcessEvent(const ui::PlatformEvent& event) override;
+  void DidProcessEvent(const ui::PlatformEvent& event) override;
 #endif
 
  private:
