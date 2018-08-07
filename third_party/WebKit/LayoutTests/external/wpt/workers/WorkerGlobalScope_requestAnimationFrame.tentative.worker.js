7bdd7f30d615a2e5c4b42b3f75142367ffecc6fb
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-08-07 13:08:24 +0000

    Import wpt@5a3a984492dcd1b1586576790922baf0a208dfb5
    Using wpt-import in Chromium 501f89508dc73ac4b0529c820f7577da9a685a85.
    With Chromium commits locally applied on WPT:
    7dad0f1723 "Added new Sec-Metadata Web Platform Tests."
    
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/22558
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    kojii@chromium.org:
      external/wpt/css/css-writing-modes
    mkwst@chromium.org, andypaicu@chromium.org:
      external/wpt/content-security-policy
    worker-dev@chromium.org:
      external/wpt/workers
    
    TBR=markdittmer
    
    No-Export: true
    Change-Id: Icb1c9f92c4a4e81a331a1c012242f515fbe8a778
    Reviewed-on: https://chromium-review.googlesource.com/1165103
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581207}

---
+++ third_party/WebKit/LayoutTests/external/wpt/workers/WorkerGlobalScope_requestAnimationFrame.tentative.worker.js
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+importScripts("/resources/testharness.js");
+
+async_test(t => {
+  const res = [];
+  requestAnimationFrame(t.step_func(dt => {
+    res.push(dt);
+    requestAnimationFrame(t.step_func(dt => {
+      res.push(dt);
+      requestAnimationFrame(t.step_func_done(dt => {
+        res.push(dt);
+        assert_equals(res.length, 3);
+        assert_less_than(res[0], res[1]);
+        assert_less_than(res[1], res[2]);
+      }));
+    }));
+  }));
+});
+
+done();
