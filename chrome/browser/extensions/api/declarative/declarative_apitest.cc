a6f6ab63cfaaca82bca80c85eeef04f49936d863
Rune Lillesveen <futhark@chromium.org>
2018-08-07 11:26:06 +0000

    Revert "Fix flaky webrequest related network service tests in component builds"
    This reverts commit 30d8a3b9d66558f01b026c11ffe7db2bbda6fc1e.
    
    Reason for revert: Made SetIconAPITest.Overview test flaky (871720)
    
    Original change's description:
    > Fix flaky webrequest related network service tests in component builds
    > 
    > These tests needed some extra synchronization due to the changes made in
    > http://crrev.com/c/1139048. These were flaking in component builds.
    > 
    > Bug: 871224
    > Change-Id: Ibe370f9b9c8ebb94ee1dd21ba204c06e91518057
    > Reviewed-on: https://chromium-review.googlesource.com/1163987
    > Commit-Queue: Clark DuVall <cduvall@chromium.org>
    > Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#580980}
    
    TBR=jam@chromium.org,cduvall@chromium.org
    
    Change-Id: If7bb80ef219f5aa5107ffd012981839dfaceac76
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 871224, 871720
    Reviewed-on: https://chromium-review.googlesource.com/1165122
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581195}

--- chrome/browser/extensions/api/declarative/declarative_apitest.cc
+++ chrome/browser/extensions/api/declarative/declarative_apitest.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "content/public/browser/browser_thread.h"
-#include "content/public/browser/storage_partition.h"
 #include "extensions/browser/api/declarative/rules_registry_service.h"
 #include "extensions/browser/api/declarative_webrequest/webrequest_constants.h"
 #include "extensions/browser/api/declarative_webrequest/webrequest_rules_registry.h"
@@ -188,9 +187,6 @@ IN_PROC_BROWSER_TEST_F(DeclarativeApiTest,
   const Extension* extension = InstallExtensionWithUIAutoConfirm(
       ext_dir.Pack(), 1 /*+1 installed extension*/, browser());
   ASSERT_TRUE(extension);
-  // Wait for declarative rules to be set up.
-  content::BrowserContext::GetDefaultStoragePartition(profile())
-      ->FlushNetworkInterfaceForTesting();
   std::string extension_id(extension->id());
   ASSERT_TRUE(ready.WaitUntilSatisfied());
   ui_test_utils::NavigateToURL(browser(), GURL(kArbitraryUrl));
@@ -265,9 +261,6 @@ IN_PROC_BROWSER_TEST_F(DeclarativeApiTest, MAYBE_NoTracesAfterUninstalling) {
   const Extension* extension = InstallExtensionWithUIAutoConfirm(
       ext_dir.Pack(), 1 /*+1 installed extension*/, browser());
   ASSERT_TRUE(extension);
-  // Wait for declarative rules to be set up.
-  content::BrowserContext::GetDefaultStoragePartition(profile())
-      ->FlushNetworkInterfaceForTesting();
   std::string extension_id(extension->id());
   ASSERT_TRUE(ready.WaitUntilSatisfied());
   ui_test_utils::NavigateToURL(browser(), GURL(kArbitraryUrl));
@@ -278,9 +271,6 @@ IN_PROC_BROWSER_TEST_F(DeclarativeApiTest, MAYBE_NoTracesAfterUninstalling) {
 
   // 2. Uninstall the extension. Rules are gone and preferences should be empty.
   UninstallExtension(extension_id);
-  // Wait for declarative rules to be removed.
-  content::BrowserContext::GetDefaultStoragePartition(profile())
-      ->FlushNetworkInterfaceForTesting();
   ui_test_utils::NavigateToURL(browser(), GURL(kArbitraryUrl));
   EXPECT_NE(kTestTitle, GetTitle());
   EXPECT_EQ(0u, NumberOfRegisteredRules(extension_id));
