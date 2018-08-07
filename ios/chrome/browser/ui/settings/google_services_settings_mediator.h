96d75d2f91aeff1fb2e320bffe4edca463448d5f
Jérôme Lebel <jlebel@chromium.org>
2018-08-07 17:41:14 +0000

    [iOS] Code cleanup for Google services settings
    Reorganizing GoogleServicesSettingsCommandID enum and
    <GoogleServicesSettingsCommandHandler> to match the UI layout.
    
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ie60468e0d3f2f2ad3b9ad3d515675ab4deafaaa8
    Reviewed-on: https://chromium-review.googlesource.com/1163663
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581273}

--- ios/chrome/browser/ui/settings/google_services_settings_mediator.h
+++ ios/chrome/browser/ui/settings/google_services_settings_mediator.h
@@ -27,6 +27,11 @@ class ProfileSyncService;
     : NSObject<GoogleServicesSettingsCommandHandler,
                GoogleServicesSettingsViewControllerModelDelegate>
 
+// View controller.
+@property(nonatomic, weak) id<GoogleServicesSettingsConsumer> consumer;
+// Authentication service.
+@property(nonatomic, assign) AuthenticationService* authService;
+
 // Designated initializer. |prefService|, |syncService| and |syncSetupService|
 // should not be null.
 - (instancetype)initWithPrefService:(PrefService*)prefService
@@ -36,11 +41,6 @@ class ProfileSyncService;
     NS_DESIGNATED_INITIALIZER;
 
 - (instancetype)init NS_UNAVAILABLE;
-
-// View controller.
-@property(nonatomic, weak) id<GoogleServicesSettingsConsumer> consumer;
-// Authentication service.
-@property(nonatomic, assign) AuthenticationService* authService;
 
 @end
 
