b74ee24ab5a9f06489f3d71c2da8fdd0dfb7e8a0
Yuki Shiino <yukishiino@chromium.org>
2018-08-08 08:50:01 +0000

    v8binding: Supports IDL callback function with variadic arguments.
    It turned out that we need type Function defined in Web IDL in
    order to correctly implement callback-constructor-related
    (custom elements, etc.) and also timer APIs (setTimeout,
    setInterval).
    
    This patch supports variadic arguments on callback functions
    in order to support Function type.
    https://heycam.github.io/webidl/#Function
    
    Change-Id: I435053cd770a36e3e993c5bc789f77dc51829cb8
    Bug: chromium:871208
    Reviewed-on: https://chromium-review.googlesource.com/1163559
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581504}

--- third_party/blink/renderer/bindings/tests/results/core/v8_void_callback_function_enum_arg.cc
+++ third_party/blink/renderer/bindings/tests/results/core/v8_void_callback_function_enum_arg.cc
@@ -99,7 +99,9 @@ v8::Maybe<void> V8VoidCallbackFunctionEnumArg::Invoke(ScriptWrappable* callback_
       CallbackRelevantScriptState()->GetContext()->Global();
   ALLOW_UNUSED_LOCAL(argument_creation_context);
   v8::Local<v8::Value> v8_arg = V8String(GetIsolate(), arg);
+  constexpr int argc = 1;
   v8::Local<v8::Value> argv[] = { v8_arg };
+  static_assert(static_cast<size_t>(argc) == base::size(argv), "size mismatch");
 
   // step 11. Let callResult be Call(X, thisArg, esArgs).
   v8::Local<v8::Value> call_result;
@@ -107,7 +109,7 @@ v8::Maybe<void> V8VoidCallbackFunctionEnumArg::Invoke(ScriptWrappable* callback_
           CallbackFunction(),
           ExecutionContext::From(CallbackRelevantScriptState()),
           this_arg,
-          1,
+          argc,
           argv,
           GetIsolate()).ToLocal(&call_result)) {
     // step 12. If callResult is an abrupt completion, set completion to
