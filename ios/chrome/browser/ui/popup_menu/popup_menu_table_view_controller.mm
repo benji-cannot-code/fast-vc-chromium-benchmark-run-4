a3b9bf4e0cc6c4102ce19730ee5840cf7ce250eb
Gauthier Ambard <gambard@chromium.org>
2018-08-08 10:05:09 +0000

    Remove inset at the bottom of the popup menus
    The popup menus have a bottom inset because of the style of the table
    view used. This CL prevents its by adding a dummy footer and settings
    the footer height to 0.
    This only happened on iOS 10.
    
    Bug: 867782
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ia05e0a94f136e1cfaffb9e67bbc1bd09c606a5b2
    Reviewed-on: https://chromium-review.googlesource.com/1166905
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581510}

--- ios/chrome/browser/ui/popup_menu/popup_menu_table_view_controller.mm
+++ ios/chrome/browser/ui/popup_menu/popup_menu_table_view_controller.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/popup_menu/popup_menu_table_view_controller.h"
 
+#include "base/ios/ios_util.h"
 #include "base/metrics/user_metrics.h"
 #include "base/metrics/user_metrics_action.h"
 #import "ios/chrome/browser/ui/commands/application_commands.h"
@@ -138,6 +139,15 @@ const CGFloat kScrollIndicatorVerticalInsets = 11;
   self.tableView.tableHeaderView = [[UIView alloc]
       initWithFrame:CGRectMake(0.0f, 0.0f, self.tableView.bounds.size.width,
                                0.01f)];
+
+  if (!base::ios::IsRunningOnIOS11OrLater()) {
+    // On iOS 10, a footer with a height of 0 is also needed to prevent inset at
+    // the bottom.
+    self.tableView.tableFooterView = [[UIView alloc]
+        initWithFrame:CGRectMake(0.0f, 0.0f, self.tableView.bounds.size.width,
+                                 0.01f)];
+    self.tableView.sectionFooterHeight = 0.0;
+  }
 
   self.view.layer.cornerRadius = kPopupMenuCornerRadius;
   self.view.layer.masksToBounds = YES;
