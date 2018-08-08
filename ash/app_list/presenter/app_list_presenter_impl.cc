eaadc7c567f364b6e982365f074b695250473b4c
Weidong Guo <weidongg@chromium.org>
2018-08-08 21:01:29 +0000

    Fix home launcher not shown issue
    Background:
    There are several corner cases that home launcher is not shown:
    1. Drag app list from shelf, meanwhile switch to tablet mode.
    2. Drag app list to shelf after being opened, meanwhile switch to tablet
       mode.
    3. Close app list and, during the close animation, switch to tablet
       mode.
    
    Changes:
    1. Ignore drag event from both shelf or app list and reset dragging
       state when tablet mode is on.
    2. Immediately stop close animation before reshow app list when tablet
       mode is on.
    
    Bug: 872072,871923
    Change-Id: I5bd7dae242d84e2f0a82839693fbd36930f8e613
    Reviewed-on: https://chromium-review.googlesource.com/1167658
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Weidong Guo <weidongg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581688}

--- ash/app_list/presenter/app_list_presenter_impl.cc
+++ ash/app_list/presenter/app_list_presenter_impl.cc
@@ -254,6 +254,7 @@ void AppListPresenterImpl::ScheduleAnimation() {
   gfx::Transform transform;
   transform.Translate(-offset.x(), -offset.y());
   layer->SetTransform(transform);
+  is_animating_to_close_ = true;
 
   {
     ui::ScopedLayerAnimationSettings animation(layer->GetAnimator());
@@ -319,10 +320,12 @@ void AppListPresenterImpl::OnWindowFocused(aura::Window* gained_focus,
 // AppListPresenterImpl, ui::ImplicitAnimationObserver implementation:
 
 void AppListPresenterImpl::OnImplicitAnimationsCompleted() {
-  if (is_visible_)
+  if (is_visible_) {
     view_->GetWidget()->Activate();
-  else
+  } else {
     view_->GetWidget()->Close();
+    is_animating_to_close_ = false;
+  }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
