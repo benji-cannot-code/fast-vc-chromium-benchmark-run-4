a82dfd61436379d5a995c1ee1c3340d78606c2af
Christopher Cameron <ccameron@chromium.org>
2018-08-08 20:02:01 +0000

    Correctly set DisplayICCProfiles::needs_update_
    Bug: 869570, 871948
    Change-Id: I92312b385c7e38610aa92ad2e25a1b2fa0f192ff
    Reviewed-on: https://chromium-review.googlesource.com/1166205
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581673}

--- ui/gfx/mac/display_icc_profiles.cc
+++ ui/gfx/mac/display_icc_profiles.cc
@@ -36,6 +36,7 @@ DisplayICCProfiles::~DisplayICCProfiles() {
 void DisplayICCProfiles::UpdateIfNeeded() {
   if (!needs_update_)
     return;
+  needs_update_ = false;
   map_.clear();
 
   // Always add Apple's sRGB profile.
