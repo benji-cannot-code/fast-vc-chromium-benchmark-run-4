5a4d001ea0c888135b9c5f7469ade4b478435e33
Hitoshi Yoshida <peria@chromium.org>
2018-08-08 07:41:38 +0000

    bindings: Implement ToV8 for IDL callback function
    To support more types in ToV8().
    This will be needed in defining IDL callback functions
    in IDL dictoinaries.
    
    
    Bug: 867875
    Change-Id: Id905ed1143899a3b455b5aca24e376e5ac50739c
    Reviewed-on: https://chromium-review.googlesource.com/1166618
    Commit-Queue: Hitoshi Yoshida <peria@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581496}

--- third_party/blink/renderer/platform/bindings/to_v8.h
+++ third_party/blink/renderer/platform/bindings/to_v8.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/optional.h"
+#include "third_party/blink/renderer/platform/bindings/callback_function_base.h"
 #include "third_party/blink/renderer/platform/bindings/callback_interface_base.h"
 #include "third_party/blink/renderer/platform/bindings/dom_data_store.h"
 #include "third_party/blink/renderer/platform/bindings/script_state.h"
@@ -36,6 +37,20 @@ inline v8::Local<v8::Value> ToV8(ScriptWrappable* impl,
   wrapper = impl->Wrap(isolate, creation_context);
   DCHECK(!wrapper.IsEmpty());
   return wrapper;
+}
+
+// Callback function
+
+inline v8::Local<v8::Value> ToV8(CallbackFunctionBase* callback,
+                                 v8::Local<v8::Object> creation_context,
+                                 v8::Isolate* isolate) {
+  // |creation_context| is intentionally ignored. Callback functions are not
+  // wrappers nor clonable. ToV8 on a callback function must be used only when
+  // it's the same origin-domain in the same world.
+  DCHECK(!callback || (callback->CallbackRelevantScriptState()->GetContext() ==
+                       creation_context->CreationContext()));
+  return callback ? callback->CallbackFunction().As<v8::Value>()
+                  : v8::Null(isolate).As<v8::Value>();
 }
 
 // Callback interface
