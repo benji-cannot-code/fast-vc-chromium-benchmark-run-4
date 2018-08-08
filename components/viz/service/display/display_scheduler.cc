71ec7e8bf511d41b92bb006fd7096c6fa238597f
Sadrul Habib Chowdhury <sadrul@chromium.org>
2018-08-08 03:42:51 +0000

    viz: Report the reason for not drawing in display scheduler.
    Add some metrics for why the display-scheduler aborts a draw operation.
    This metric should help explain why fewer frames are drawn when oop-d
    is turned on.
    
    BUG=865179, 778749
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel
    Change-Id: I68e9a1e9c89b42042ac1611e9b6516bf7948657f
    Reviewed-on: https://chromium-review.googlesource.com/1165485
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581463}

--- components/viz/service/display/display_scheduler.cc
+++ components/viz/service/display/display_scheduler.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/auto_reset.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/stl_util.h"
 #include "base/trace_event/trace_event.h"
 #include "components/viz/common/surfaces/surface_info.h"
@@ -292,7 +293,7 @@ void DisplayScheduler::StopObservingBeginFrames() {
   }
 }
 
-bool DisplayScheduler::ShouldDraw() {
+bool DisplayScheduler::ShouldDraw() const {
   // Note: When any of these cases becomes true, MaybeStartObservingBeginFrames
   // must be called to ensure the draw will happen.
   return needs_draw_ && !output_surface_lost_ && visible_ &&
@@ -484,6 +485,7 @@ bool DisplayScheduler::AttemptDrawAndSwap() {
     if (pending_swaps_ < max_pending_swaps_)
       return DrawAndSwap();
   } else {
+    ReportNotDrawReason();
     // We are going idle, so reset expectations.
     // TODO(eseckler): Should we avoid going idle if
     // |expecting_root_surface_damage_because_of_resize_| is true?
@@ -521,6 +523,17 @@ void DisplayScheduler::DidReceiveSwapBuffersAck() {
   pending_swaps_--;
   TRACE_EVENT_ASYNC_END0("viz", "DisplayScheduler:pending_swaps", swap_id);
   ScheduleBeginFrameDeadline();
+}
+
+void DisplayScheduler::ReportNotDrawReason() {
+  DCHECK(!ShouldDraw());
+  UMA_HISTOGRAM_BOOLEAN("DisplayScheduler.ShouldNotDraw.DrawNotNeeded",
+                        !needs_draw_);
+  UMA_HISTOGRAM_BOOLEAN("DisplayScheduler.ShouldNotDraw.OutputSurfaceLost",
+                        output_surface_lost_);
+  UMA_HISTOGRAM_BOOLEAN("DisplayScheduler.ShouldNotDraw.NotVisible", !visible_);
+  UMA_HISTOGRAM_BOOLEAN("DisplayScheduler.ShouldNotDraw.RootFrameMissing",
+                        root_frame_missing_);
 }
 
 }  // namespace viz
