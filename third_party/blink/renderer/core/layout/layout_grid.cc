cebf3859cd69ae052d9c56cfeac124433b44e4ca
Manuel Rego Casasnovas <rego@igalia.com>
2018-08-08 00:10:38 +0000

    [css-grid] Fix scroll reset when updating content of grid item
    This patch simply uses DelayScrollOffsetClampScope to avoid
    resetting scroll position when the contents of a grid item are modified.
    
    BUG=833837
    TEST=fast/css-grid-layout/grid-item-scroll-position.html
    
    Change-Id: I74dd5e67d432b7b86b756ed741c7ad24243e6b8e
    Reviewed-on: https://chromium-review.googlesource.com/1165983
    Commit-Queue: Manuel Rego <rego@igalia.com>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581404}

--- third_party/blink/renderer/core/layout/layout_grid.cc
+++ third_party/blink/renderer/core/layout/layout_grid.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/text_autosizer.h"
 #include "third_party/blink/renderer/core/paint/grid_painter.h"
 #include "third_party/blink/renderer/core/paint/paint_layer.h"
+#include "third_party/blink/renderer/core/paint/paint_layer_scrollable_area.h"
 #include "third_party/blink/renderer/core/style/computed_style.h"
 #include "third_party/blink/renderer/core/style/grid_area.h"
 #include "third_party/blink/renderer/platform/length_functions.h"
@@ -256,6 +257,8 @@ void LayoutGrid::UpdateBlockLayout(bool relayout_children) {
     return;
 
   SubtreeLayoutScope layout_scope(*this);
+
+  PaintLayerScrollableArea::DelayScrollOffsetClampScope delay_clamp_scope;
 
   {
     // LayoutState needs this deliberate scope to pop before updating scroll
