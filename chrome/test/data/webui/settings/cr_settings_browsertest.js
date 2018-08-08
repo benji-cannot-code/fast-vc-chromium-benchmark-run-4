4332b06f74a1923e3a64f72eb3bfd448e4e4e2d7
Rakina Zata Amni <rakina@chromium.org>
2018-08-08 04:44:18 +0000

    Disable Flaky CrSettings tests
    TBR=dschuyler@chromium.org
    
    Bug: 87206,872027,872028,871692
    Change-Id: I798194cdb7ee2059565940791b751856611ed34a
    Reviewed-on: https://chromium-review.googlesource.com/1166562
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581471}

--- chrome/test/data/webui/settings/cr_settings_browsertest.js
+++ chrome/test/data/webui/settings/cr_settings_browsertest.js
@@ -1013,7 +1013,8 @@ CrSettingsCategorySettingExceptionsTest.prototype = {
   ]),
 };
 
-TEST_F('CrSettingsCategorySettingExceptionsTest', 'All', function() {
+// Flaky, see https://crbug.com/872026.
+TEST_F('CrSettingsCategorySettingExceptionsTest', 'DISABLED_All', function() {
   mocha.run();
 });
 
@@ -1039,7 +1040,8 @@ CrSettingsSiteEntryTest.prototype = {
   ]),
 };
 
-TEST_F('CrSettingsSiteEntryTest', 'All', function() {
+// Flaky, see https://crbug.com/872027.
+TEST_F('CrSettingsSiteEntryTest', 'DISABLED_All', function() {
   mocha.run();
 });
 
@@ -1161,7 +1163,8 @@ TEST_F('CrSettingsSiteListTest', 'EditExceptionDialog', function() {
   mocha.grep('EditExceptionDialog').run();
 });
 
-TEST_F('CrSettingsSiteListTest', 'AddExceptionDialog', function() {
+// Flaky, see https://crbug.com/872028.
+TEST_F('CrSettingsSiteListTest', 'DISABLED_AddExceptionDialog', function() {
   mocha.grep('AddExceptionDialog').run();
 });
 
