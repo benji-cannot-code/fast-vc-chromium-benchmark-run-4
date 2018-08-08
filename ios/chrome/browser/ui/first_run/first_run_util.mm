29225de9bfb5bb50a8bc0deafb29aa0af70d3b45
Jochen Eisinger <jochen@chromium.org>
2018-08-08 09:39:05 +0000

    Use IdentityManager to get profile signin state during first run
    BUG=797948
    R=msarda@chromium.org
    
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ibf031e86d4e7a9385e01ac872566f63bb4564f9a
    Reviewed-on: https://chromium-review.googlesource.com/1163668
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581507}

--- ios/chrome/browser/ui/first_run/first_run_util.mm
+++ ios/chrome/browser/ui/first_run/first_run_util.mm
@@ -12,19 +12,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/sys_string_conversions.h"
 #include "base/task/post_task.h"
 #include "base/time/time.h"
-#include "components/signin/core/browser/signin_manager.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #import "ios/chrome/browser/crash_report/breakpad_helper.h"
 #include "ios/chrome/browser/first_run/first_run.h"
 #import "ios/chrome/browser/first_run/first_run_configuration.h"
 #include "ios/chrome/browser/first_run/first_run_metrics.h"
-#include "ios/chrome/browser/signin/signin_manager_factory.h"
+#include "ios/chrome/browser/signin/identity_manager_factory.h"
 #include "ios/chrome/browser/tabs/tab.h"
 #include "ios/chrome/browser/ui/first_run/first_run_histograms.h"
 #import "ios/chrome/browser/ui/settings/settings_utils.h"
 #import "ios/chrome/browser/ui/settings/sync_utils/sync_util.h"
 #include "ios/chrome/browser/ui/ui_util.h"
 #include "ios/web/public/web_thread.h"
+#include "services/identity/public/cpp/identity_manager.h"
 #import "ui/gfx/ios/NSString+CrStringDrawing.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -95,9 +95,8 @@ void RecordFirstRunMetricsInternal(ios::ChromeBrowserState* browserState,
                                    bool sign_in_attempted,
                                    bool has_sso_accounts) {
   first_run::SignInStatus sign_in_status;
-  bool user_signed_in =
-      ios::SigninManagerFactory::GetForBrowserState(browserState)
-          ->IsAuthenticated();
+  bool user_signed_in = IdentityManagerFactory::GetForBrowserState(browserState)
+                            ->HasPrimaryAccount();
   if (user_signed_in) {
     sign_in_status = has_sso_accounts
                          ? first_run::HAS_SSO_ACCOUNT_SIGNIN_SUCCESSFUL
