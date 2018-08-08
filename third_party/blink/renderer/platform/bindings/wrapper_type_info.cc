f7c237ed0c0b3423d2373091f889feaa5d1bc592
Michael Lippautz <mlippautz@chromium.org>
2018-08-08 13:12:08 +0000

    [bindings] Mark WrapperTypeInfo::Trace* as const
    Bug: chromium:843903
    Change-Id: Ibdb5830029ee33ab04b450d6841dcb3d4862ad41
    Reviewed-on: https://chromium-review.googlesource.com/1166915
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581541}

--- third_party/blink/renderer/platform/bindings/wrapper_type_info.cc
+++ third_party/blink/renderer/platform/bindings/wrapper_type_info.cc
@@ -28,7 +28,7 @@ void WrapperTypeInfo::WrapperDestroyed() {
   stats_collector->IncreaseCollectedWrapperCount(1);
 }
 
-void WrapperTypeInfo::Trace(Visitor* visitor, void* impl) {
+void WrapperTypeInfo::Trace(Visitor* visitor, void* impl) const {
   switch (wrapper_class_id) {
     case WrapperTypeInfo::kNodeClassId:
     case WrapperTypeInfo::kObjectClassId:
@@ -42,7 +42,7 @@ void WrapperTypeInfo::Trace(Visitor* visitor, void* impl) {
   }
 }
 
-void WrapperTypeInfo::TraceWithWrappers(Visitor* visitor, void* impl) {
+void WrapperTypeInfo::TraceWithWrappers(Visitor* visitor, void* impl) const {
   switch (wrapper_class_id) {
     case WrapperTypeInfo::kNodeClassId:
     case WrapperTypeInfo::kObjectClassId:
