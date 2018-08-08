2b87f0c6ea6108dde11e1cf4e860d774f0726ed3
Gauthier Ambard <gambard@chromium.org>
2018-08-08 13:30:06 +0000

    [iOS] Change ConsentBump buttons
    This CL updates the buttons used by the ConsentBump to have the same
    buttons as the other screens, taking example on the Infobars.
    It is also using two buttons for the secondary view as using one button
    create a visible flash.
    
    Bug: 866506
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I5b17e3891c91399ec18da6054ce1951b4c2f13e9
    Reviewed-on: https://chromium-review.googlesource.com/1165358
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581546}

--- ios/chrome/browser/ui/authentication/consent_bump/consent_bump_consumer.h
+++ ios/chrome/browser/ui/authentication/consent_bump/consent_bump_consumer.h
@@ -14,11 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Sets the title of the primary button of the ConsentBump. By default the
 // primary button isn't visible.
 - (void)setPrimaryButtonTitle:(NSString*)primaryButtonTitle;
-// Sets the title of the secondary button of the ConsentBump.
-- (void)setSecondaryButtonTitle:(NSString*)secondaryButtonTitle;
 // Shows the primary button on the ConsentBump. There are no way to hide it once
 // it is shown.
 - (void)showPrimaryButton;
+// Whether to show the button to display more options, or the back button.
+- (void)showMoreOptionsButton:(BOOL)showMoreOptionsButton;
 
 @end
 
--- ios/chrome/browser/ui/authentication/consent_bump/consent_bump_mediator.mm
+++ ios/chrome/browser/ui/authentication/consent_bump/consent_bump_mediator.mm
@@ -24,15 +24,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     case ConsentBumpScreenUnifiedConsent:
       [self.consumer setPrimaryButtonTitle:l10n_util::GetNSString(
                                                IDS_IOS_CONSENT_BUMP_IM_IN)];
-      [self.consumer setSecondaryButtonTitle:l10n_util::GetNSString(
-                                                 IDS_IOS_CONSENT_BUMP_MORE)];
+      [self.consumer showMoreOptionsButton:YES];
       break;
 
     case ConsentBumpScreenPersonalization:
       [self.consumer
           setPrimaryButtonTitle:l10n_util::GetNSString(IDS_ACCNAME_OK)];
-      [self.consumer
-          setSecondaryButtonTitle:l10n_util::GetNSString(IDS_ACCNAME_BACK)];
+      [self.consumer showMoreOptionsButton:NO];
       [self consumerCanProceed];
       break;
   }
