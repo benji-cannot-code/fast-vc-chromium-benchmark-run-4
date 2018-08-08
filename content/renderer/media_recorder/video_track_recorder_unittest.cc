0ae52935715082d39738a44e37cad4ad9e5034b3
Christian Fremerey <chfremer@chromium.org>
2018-08-08 20:15:06 +0000

    Revert "Reland [Media Recorder] Fix video freeze on short recording. Fix tab crash on initialize."
    This reverts commit 545410e67c57571be348780766ef40ead5addba9.
    
    Reason for revert: A different fix for the initialization had already landed by now, see https://chromium-review.googlesource.com/c/chromium/src/+/1158316. I need to remove the extra initialization changes from my CL.
    
    Original change's description:
    > Reland [Media Recorder] Fix video freeze on short recording. Fix tab crash on initialize.
    > 
    > Patch Set 1 is the original CL that got reverted.
    > Patch Set 2 fixes the reason for the revert.
    > 
    > Reason for the revert was a memory leak during a unit test. This was caused by
    > a newly added DeleteSoon() causing the test to exist before all objects had
    > been released. The fix is to add a ScopedTaskEnvironment::RunUntilIdle() at
    > test teardown.
    > 
    > Original CL description:
    > 
    > Fixes an issue where VideoTrackRecorder when used in combination with
    > VEAEncoder and very short recording durations (just 1 frame) would
    > occasionally hold on to video frames forever, causing frame buffers
    > from the buffer pool to become permanently blocked, eventually leading
    > to video capture freezing.
    > 
    > The cause was a circular ownership that would happen if only 1 frame
    > is recorded.
    > 
    > This CL additionally fixes a crash that could happen on initialization
    > of VEAEncoder. This issue was revealed during testing of the above fix.
    > Since it is not 100% if this crash was masked by the other issue, and
    > would only start occurring with the above fix, I am making a fix for that
    > part of this CL as well.
    > 
    > The cause for the crash issue was that class VEAEncoder, which uses
    > RefCountedThreadSafe<>, would post an asynchronous task from its
    > constructor. If this task would finish executing before the newly
    > constructed instance was assigned to a scoped_refptr<> by however
    > called the constructor, the instance would destroy itself, because
    > of the AddRef() and subsequent Release() done by the posting and
    > running of the asynchronous task.
    > 
    > The fix for this is to use a new method Initialize() instead of
    > posting a task from the constructor.
    > 
    > TBR=emircan@chromium.org
    > 
    > Bug: 859610
    > Change-Id: I120b60b4b5cdf7462e38a74b7c757af6b5e1a7d6
    > Reviewed-on: https://chromium-review.googlesource.com/1167742
    > Reviewed-by: Christian Fremerey <chfremer@chromium.org>
    > Commit-Queue: Christian Fremerey <chfremer@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#581672}
    
    TBR=emircan@chromium.org,chfremer@chromium.org
    
    Change-Id: I4794d8d76b78bd1ab86860230bc01f0f20d3bd18
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 859610
    Reviewed-on: https://chromium-review.googlesource.com/1167768
    Reviewed-by: Christian Fremerey <chfremer@chromium.org>
    Commit-Queue: Christian Fremerey <chfremer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581679}

--- content/renderer/media_recorder/video_track_recorder_unittest.cc
+++ content/renderer/media_recorder/video_track_recorder_unittest.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/single_thread_task_runner.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/scoped_task_environment.h"
-#include "base/threading/thread_task_runner_handle.h"
 #include "content/child/child_process.h"
 #include "content/renderer/media/stream/media_stream_video_track.h"
 #include "content/renderer/media/stream/mock_media_stream_video_source.h"
@@ -94,9 +93,6 @@ class VideoTrackRecorderTest
     blink_source_.Reset();
     video_track_recorder_.reset();
     blink::WebHeap::CollectAllGarbageForTesting();
-    // VideoTrackRecorder::Encoder::~Encoder may post a DeleteSoon(), which
-    // may cause ASAN to detect a memory leak if we don't wait.
-    scoped_task_environment_.RunUntilIdle();
   }
 
   void InitializeRecorder(VideoTrackRecorder::CodecId codec) {
@@ -142,7 +138,7 @@ class VideoTrackRecorderTest
   }
 
   uint32_t NumFramesInEncode() {
-    return video_track_recorder_->encoder_->num_frames_in_encode_->count();
+    return video_track_recorder_->encoder_->num_frames_in_encode_;
   }
 
   // A ChildProcess is needed to fool the Tracks and Sources into believing they
