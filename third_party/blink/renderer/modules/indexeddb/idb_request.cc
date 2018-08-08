d94b5c1ea527c045870b5ffab193354a954576ee
Hayato Ito <hayato@chromium.org>
2018-08-08 04:44:37 +0000

    Use a reference for EventQueue::EnqueueEvent's event parameter
    As part of http://crrev.com/581145.
    
    Bug: 581145
    Change-Id: I60203e3705ee84260d17cbe1c7cfcd2ebaf94dca
    Reviewed-on: https://chromium-review.googlesource.com/1166281
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Hayato Ito <hayato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581472}

--- third_party/blink/renderer/modules/indexeddb/idb_request.cc
+++ third_party/blink/renderer/modules/indexeddb/idb_request.cc
@@ -763,7 +763,7 @@ void IDBRequest::EnqueueEvent(Event* event) {
 
   event->SetTarget(this);
 
-  event_queue_->EnqueueEvent(FROM_HERE, event);
+  event_queue_->EnqueueEvent(FROM_HERE, *event);
 }
 
 }  // namespace blink
