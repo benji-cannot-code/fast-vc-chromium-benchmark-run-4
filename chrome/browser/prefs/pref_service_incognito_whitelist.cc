32b7300adb92b1acecd2619713485790ebd87572
Jordy Greenblatt <jordynass@chromium.org>
2018-08-08 17:57:12 +0000

    [CrOS MultiDevice] Delete EasyUnlock prefs from incognito whitelist
    The whitelist list irrelevant to the EasyUnlock prefs and we are about
    to move at least one of them to a new location. Rather than update this
    unecessary reference, I'd like to just get rid of it before the
    migration.
    
    Bug: 824568
    Change-Id: Id9a90fff2139f665c9696f104f45bc6590decb93
    Reviewed-on: https://chromium-review.googlesource.com/1166286
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Commit-Queue: Jordy Greenblatt <jordynass@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581626}

--- chrome/browser/prefs/pref_service_incognito_whitelist.cc
+++ chrome/browser/prefs/pref_service_incognito_whitelist.cc
@@ -379,8 +379,6 @@ const char* temporary_incognito_whitelist[] = {
 
     prefs::kGCMProductCategoryForSubtypes,
 
-    prefs::kEasyUnlockAllowed, prefs::kEasyUnlockPairing,
-
     prefs::kWebRTCMultipleRoutesEnabled, prefs::kWebRTCNonProxiedUdpEnabled,
     prefs::kWebRTCIPHandlingPolicy, prefs::kWebRTCUDPPortRange,
 
@@ -632,9 +630,6 @@ const char* temporary_incognito_whitelist[] = {
 
     prefs::kQuickCheckEnabled, prefs::kPacHttpsUrlStrippingEnabled,
     prefs::kBrowserGuestModeEnabled, prefs::kBrowserAddPersonEnabled,
-
-    prefs::kEasyUnlockHardlockState, prefs::kEasyUnlockLocalStateTpmKeys,
-    prefs::kEasyUnlockLocalStateUserPrefs,
 
     prefs::kRecoveryComponentNeedsElevation,
 #if BUILDFLAG(ENABLE_BACKGROUND_MODE)
