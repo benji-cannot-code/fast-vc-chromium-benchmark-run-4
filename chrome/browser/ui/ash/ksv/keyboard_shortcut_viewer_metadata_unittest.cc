a8210a03c33e549233854d0eb27a3b75ae4fbd9a
Daniel Erat <derat@chromium.org>
2018-08-08 22:49:47 +0000

    chromeos: Fix "Print Screen" screenshot accelerator.
    Make ash map VKEY_SNAPSHOT to the TAKE_SCREENSHOT action.
    7ab3497a updated Chrome to map the "Print Screen" key to
    VKEY_SNAPSHOT rather than VKEY_PRINT.
    
    Bug: 683097, 846919, 872094
    Change-Id: I3e8de9e8b2d110d8e06649fc0553c6d9b7cab35a
    Reviewed-on: https://chromium-review.googlesource.com/1166037
    Commit-Queue: Dan Erat <derat@chromium.org>
    Reviewed-by: Kevin Schoedel <kpschoedel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581711}

--- chrome/browser/ui/ash/ksv/keyboard_shortcut_viewer_metadata_unittest.cc
+++ chrome/browser/ui/ash/ksv/keyboard_shortcut_viewer_metadata_unittest.cc
@@ -21,9 +21,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // The total number of Ash accelerators.
-constexpr int kAshAcceleratorsTotalNum = 100;
+constexpr int kAshAcceleratorsTotalNum = 101;
 // The hash of Ash accelerators.
-constexpr char kAshAcceleratorsHash[] = "4232f3c0c55cc5e487c820b1c2813dcc";
+constexpr char kAshAcceleratorsHash[] = "48e2af8115132ccec79dc16ebe7da7fb";
 #if defined(GOOGLE_CHROME_BUILD)
 // Internal builds add an extra accelerator for the Feedback app.
 // The total number of Chrome accelerators (available on Chrome OS).
