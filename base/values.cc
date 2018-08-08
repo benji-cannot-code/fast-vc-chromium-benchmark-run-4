897140c71139fe0178d75c627a9ea91ef714307f
jdoerrie <jdoerrie@chromium.org>
2018-08-08 00:11:37 +0000

    [base] Add is_alive field to Debug Double Frees in base::Values
    This change temporarily adds an "is_alive" flag to base::Value that
    should help in debugging use-after-free crashes. This change is
    intended to be reverted after a few days in Canary.
    
    Bug: 859477
    Change-Id: I6ab51c37db18dd766677ef162fd42cc6832d1bab
    Reviewed-on: https://chromium-review.googlesource.com/1163244
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581405}

--- base/values.cc
+++ base/values.cc
@@ -77,6 +77,8 @@ std::unique_ptr<Value> CopyWithoutEmptyChildren(const Value& node) {
 
 }  // namespace
 
+constexpr uint32_t Value::kMagicIsAlive;
+
 // static
 std::unique_ptr<Value> Value::CreateWithCopiedBuffer(const char* buffer,
                                                      size_t size) {
@@ -216,6 +218,7 @@ Value Value::Clone() const {
 
 Value::~Value() {
   InternalCleanup();
+  is_alive_ = 0;
 }
 
 // static
@@ -702,6 +705,8 @@ void Value::InternalMoveConstructFrom(Value&& that) {
 }
 
 void Value::InternalCleanup() {
+  CHECK_EQ(is_alive_, kMagicIsAlive);
+
   switch (type_) {
     case Type::NONE:
     case Type::BOOLEAN:
