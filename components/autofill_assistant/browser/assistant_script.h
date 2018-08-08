57d4f86aa44bdbbb41bd1ae565b25ff1a91eb738
gogerald <gogerald@google.com>
2018-08-08 15:58:52 +0000

    [Autofill Assistant] Introduce assistant script precondition
    Bug: 806868
    Change-Id: I1fe95fa3e310320fbb5c903c0ebe3cb2b7f0fb47
    Reviewed-on: https://chromium-review.googlesource.com/1163922
    Commit-Queue: Ganggui Tang <gogerald@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581579}

--- components/autofill_assistant/browser/assistant_script.h
+++ components/autofill_assistant/browser/assistant_script.h
@@ -8,12 +8,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "components/autofill_assistant/browser/assistant_script_precondition.h"
+
 namespace autofill_assistant {
 
 // Assistant script represents a sequence of client actions.
 struct AssistantScript {
+  AssistantScript();
+  ~AssistantScript();
+
   std::string name;
   std::string path;
+  std::unique_ptr<AssistantScriptPrecondition> precondition;
 };
 
 }  // namespace autofill_assistant
