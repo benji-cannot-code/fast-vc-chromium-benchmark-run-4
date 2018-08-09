74d1e8443469fb6606bdbbc6377d1da33653fc34
Xiyuan Xia <xiyuan@chromium.org>
2018-08-09 23:04:36 +0000

    chromeos: Make test_ws bring up more services
    This is needed to make content_browsertests happy under mash.
    
    Bug: 866942
    Change-Id: I21885e4328706f83bb57805abe4cff581551120a
    Reviewed-on: https://chromium-review.googlesource.com/1168143
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581947}

--- services/ui/test_ws/test_gpu_interface_provider.h
+++ services/ui/test_ws/test_gpu_interface_provider.h
@@ -6,16 +6,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SERVICES_UI_TEST_WS_TEST_GPU_INTERFACE_PROVIDER_H_
 #define SERVICES_UI_TEST_WS_TEST_GPU_INTERFACE_PROVIDER_H_
 
+#include "components/discardable_memory/public/interfaces/discardable_shared_memory_manager.mojom.h"
+#include "services/ui/public/interfaces/gpu.mojom.h"
 #include "services/ui/ws2/gpu_interface_provider.h"
 
+namespace discardable_memory {
+class DiscardableSharedMemoryManager;
+}
+
 namespace ui {
+
+namespace gpu_host {
+class GpuHost;
+}
+
 namespace test {
 
-// TestGpuInterfaceProvider binds Gpu and DiscardableSharedMemoryManager
-// requests with no-op stub functions to avoid test crashes.
+// TestGpuInterfaceProvider used by test_ws.
 class TestGpuInterfaceProvider : public ui::ws2::GpuInterfaceProvider {
  public:
-  TestGpuInterfaceProvider();
+  TestGpuInterfaceProvider(ui::gpu_host::GpuHost* gpu_host,
+                           discardable_memory::DiscardableSharedMemoryManager*
+                               discardable_shared_memory_manager);
   ~TestGpuInterfaceProvider() override;
 
   // ui::ws2::GpuInterfaceProvider:
@@ -27,6 +39,14 @@ class TestGpuInterfaceProvider : public ui::ws2::GpuInterfaceProvider {
 #endif
 
  private:
+  void BindDiscardableSharedMemoryManagerRequest(
+      discardable_memory::mojom::DiscardableSharedMemoryManagerRequest request);
+  void BindGpuRequest(ui::mojom::GpuRequest request);
+
+  ui::gpu_host::GpuHost* const gpu_host_;
+  discardable_memory::DiscardableSharedMemoryManager* const
+      discardable_shared_memory_manager_;
+
   DISALLOW_COPY_AND_ASSIGN(TestGpuInterfaceProvider);
 };
 
