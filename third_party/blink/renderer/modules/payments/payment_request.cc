af159921fa25a0813ea28c7d1262d53b9d7f3d43
Eric Willigers <ericwilligers@chromium.org>
2018-08-09 02:07:44 +0000

    Retire PaymentRequestBasicCard flag
    Support for basic card payment method specification shipped in M57.
    https://codereview.chromium.org/2501593003
    https://www.chromestatus.com/feature/5408502604365824
    
    BUG=665190
    
    Change-Id: I29780c35a6ccf5c9d5a94b21e38d8605ba8b77f9
    Reviewed-on: https://chromium-review.googlesource.com/1166744
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581754}

--- third_party/blink/renderer/modules/payments/payment_request.cc
+++ third_party/blink/renderer/modules/payments/payment_request.cc
@@ -53,7 +53,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/bindings/script_state.h"
 #include "third_party/blink/renderer/platform/feature_policy/feature_policy.h"
 #include "third_party/blink/renderer/platform/mojo/mojo_helper.h"
-#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/uuid.h"
 #include "third_party/blink/renderer/platform/weborigin/kurl.h"
 #include "third_party/blink/renderer/platform/wtf/hash_set.h"
@@ -519,8 +518,7 @@ void StringifyAndParseMethodSpecificData(const String& supported_method,
     if (exception_state.HadException())
       exception_state.ClearException();
   }
-  if (RuntimeEnabledFeatures::PaymentRequestBasicCardEnabled() &&
-      supported_method == "basic-card") {
+  if (supported_method == "basic-card") {
     SetBasicCardMethodData(input, output, exception_state);
     if (exception_state.HadException())
       exception_state.ClearException();
