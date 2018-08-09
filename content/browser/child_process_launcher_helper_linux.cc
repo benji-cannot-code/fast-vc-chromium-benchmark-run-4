222ea280dc692ef2fc234aa8ef3b0f6ecaff7542
Siddhartha <ssid@chromium.org>
2018-08-09 06:28:10 +0000

    Android: Add feature to set moderate binding for media renderer
    This CL adds a flag to control if the renderer with media streams should
    have strong or moderate binding.
    
    BUG=868611
    
    Change-Id: I3263ffcca6076f9d38452c3e60df8fdd90634ea8
    Reviewed-on: https://chromium-review.googlesource.com/1162927
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581809}

--- content/browser/child_process_launcher_helper_linux.cc
+++ content/browser/child_process_launcher_helper_linux.cc
@@ -159,7 +159,7 @@ void ChildProcessLauncherHelper::SetProcessPriorityOnLauncherThread(
     const ChildProcessLauncherPriority& priority) {
   DCHECK(CurrentlyOnProcessLauncherTaskRunner());
   if (process.CanBackgroundProcesses())
-    process.SetProcessBackgrounded(priority.background);
+    process.SetProcessBackgrounded(priority.is_background());
 }
 
 // static
--- content/browser/child_process_launcher_helper_win.cc
+++ content/browser/child_process_launcher_helper_win.cc
@@ -116,7 +116,7 @@ void ChildProcessLauncherHelper::SetProcessPriorityOnLauncherThread(
     const ChildProcessLauncherPriority& priority) {
   DCHECK(CurrentlyOnProcessLauncherTaskRunner());
   if (process.CanBackgroundProcesses())
-    process.SetProcessBackgrounded(priority.background);
+    process.SetProcessBackgrounded(priority.is_background());
 }
 
 // static
