8b3b2f4c4f797430c00d754bbfccadf4954fdbaa
Boris Sazonov <bsazonov@chromium.org>
2018-08-09 21:28:40 +0000

    [Unity][Android] Check UnifiedConsentService and maybe show consent bump
    This CL adds JNI bridge for UnifiedConsentService::ShouldShowConsentBump
    and shows the consent bump if necessary.
    
    Bug: 869426
    Change-Id: I7f1ca38673c330a86effd376f3120dae6029bb61
    Reviewed-on: https://chromium-review.googlesource.com/1167846
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581890}

---
+++ chrome/browser/android/signin/unified_consent_service_bridge.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/android/jni_android.h"
+#include "chrome/browser/profiles/profile_android.h"
+#include "chrome/browser/unified_consent/unified_consent_service_factory.h"
+#include "components/unified_consent/unified_consent_service.h"
+#include "jni/UnifiedConsentServiceBridge_jni.h"
+
+using base::android::JavaParamRef;
+
+static jboolean JNI_UnifiedConsentServiceBridge_ShouldShowConsentBump(
+    JNIEnv* env,
+    const base::android::JavaParamRef<jclass>& jcaller,
+    const base::android::JavaParamRef<jobject>& profileAndroid) {
+  Profile* profile = ProfileAndroid::FromProfileAndroid(profileAndroid);
+  auto* unifiedConsentService =
+      UnifiedConsentServiceFactory::GetForProfile(profile);
+  return unifiedConsentService->ShouldShowConsentBump();
+}
