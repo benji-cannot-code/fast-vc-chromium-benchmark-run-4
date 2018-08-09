d854bfd0dee495d4d9dd153760dff9550d3e7cdc
Mike Wasserman <msw@chromium.org>
2018-08-09 02:11:07 +0000

    ws: Fix top-level window placement and touch gesture handling.
    This fixes most known Keyboard Shortcut Viewer app touch defects:
    
    Only target the root when touch events are outside the associated display.
    (in clients, root windows are top-level app windows, not display-sized roots)
    
    Fix placement of top-level client app windows by using screen coordinates.
    (use ScreenPositionClient and screen bounds for top-levels; add test)
    
    Use GetDisplayForNewWindows in CreateInitParamsForTopLevel.
    (place window service top-level windows on the correct display)
    
    TODO: Fix capture release from EditingHandleView gesture event handling.
    (it continues consuming events even after touch drag/scroll release)
    
    Bug: 867559
    Test: KSV (Ctrl-Alt-/) window placement and touch interaction. No regression of bezel touch gestures, etc.
    Change-Id: I4fabcb9ac787743b1e80ec54e615bd5443e580d8
    Reviewed-on: https://chromium-review.googlesource.com/1152102
    Commit-Queue: Michael Wasserman <msw@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581760}

--- ui/aura/client/screen_position_client.h
+++ ui/aura/client/screen_position_client.h
@@ -54,7 +54,7 @@ AURA_EXPORT void SetScreenPositionClient(Window* root_window,
 AURA_EXPORT ScreenPositionClient* GetScreenPositionClient(
     const Window* root_window);
 
-}  // namespace clients
+}  // namespace client
 }  // namespace aura
 
 #endif  // UI_AURA_SCREEN_POSITION_CLIENT_H_
