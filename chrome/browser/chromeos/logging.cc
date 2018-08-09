e6ba3dcdf878c1c11713b512a73a7252e3c797c6
Scott Violet <sky@chromium.org>
2018-08-09 02:05:36 +0000

    chromeos: make specifying --vmodule work with ChromeOS
    Chrome for ChromeOS builds calls to initialize logging twice. If --vmodule
    is specified this triggers a CHECK in BaseInitLoggingImpl(). Make it so we
    don't redirect logging if not on a device.
    
    BUG=none
    TEST=none
    
    Change-Id: Iee4170db4b19cbf4a12add02e2497caabdfb3d59
    Reviewed-on: https://chromium-review.googlesource.com/1168106
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581751}

--- chrome/browser/chromeos/logging.cc
+++ chrome/browser/chromeos/logging.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/files/file_path.h"
 #include "base/logging.h"
+#include "base/sys_info.h"
 #include "base/task/post_task.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/logging_chrome.h"
@@ -46,6 +47,11 @@ void SymlinkSetUp(const base::CommandLine& command_line,
 
 void RedirectChromeLogging(const base::CommandLine& command_line) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+
+  // Only redirect when on an actual device. To do otherwise conflicts with
+  // --vmodule that developers may want to use.
+  if (!base::SysInfo::IsRunningOnChromeOS())
+    return;
 
   if (chrome_logging_redirected_) {
     // TODO: Support multiple active users. http://crbug.com/230345
