19e5f9aed72a76a50af2ffc2c94e5b734df09785
Jean-denis Muys <jdmuys@chromium.org>
2018-08-09 10:40:31 +0000

    MediaRouterContextualMenu: Convert from using SigninManager to using IdentityManager
    The unittest still requires knowledge of SigninManager, as the IdentityManager testing facilities
    being used require passing in the SigninManager.
    Affects media_router_contextual_menu_unittest.cc
    
    Bug: 797897
    Change-Id: I94d3fd68cff3143e461dc9ece3ae860d28d1f3ae
    Reviewed-on: https://chromium-review.googlesource.com/1139061
    Commit-Queue: Jean-Denis Muys <jdmuys@chromium.org>
    Reviewed-by: Derek Cheng <imcheng@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581854}

--- chrome/browser/ui/toolbar/media_router_contextual_menu.cc
+++ chrome/browser/ui/toolbar/media_router_contextual_menu.cc
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/media/router/media_router_factory.h"
 #include "chrome/browser/media/router/mojo/media_router_mojo_impl.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/signin/signin_manager_factory.h"
+#include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/media_router/cloud_services_dialog.h"
 #include "chrome/browser/ui/singleton_tabs.h"
@@ -28,10 +28,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/url_constants.h"
 #include "chrome/grit/chromium_strings.h"
 #include "chrome/grit/generated_resources.h"
-#include "components/signin/core/browser/signin_manager.h"
 #include "components/strings/grit/components_strings.h"
 #include "components/vector_icons/vector_icons.h"
 #include "extensions/common/constants.h"
+#include "services/identity/public/cpp/identity_manager.h"
 #include "ui/base/models/menu_model_delegate.h"
 #include "ui/gfx/color_palette.h"
 #include "ui/gfx/paint_vector_icon.h"
@@ -137,9 +137,9 @@ bool MediaRouterContextualMenu::IsCommandIdVisible(int command_id) const {
   if (command_id == IDC_MEDIA_ROUTER_CLOUD_SERVICES_TOGGLE) {
     // Cloud services preference is not set or used if the user is not signed
     // in.
-    SigninManagerBase* signin_manager =
-        SigninManagerFactory::GetForProfile(browser_->profile());
-    return signin_manager && signin_manager->IsAuthenticated();
+    identity::IdentityManager* identity_manager =
+        IdentityManagerFactory::GetForProfile(browser_->profile());
+    return identity_manager && identity_manager->HasPrimaryAccount();
   }
   return true;
 }
