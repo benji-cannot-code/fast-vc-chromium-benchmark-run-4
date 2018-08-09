0fcde8360579ec355c9516921ed750d74adcb4c6
Mikel Astiz <mastiz@chromium.org>
2018-08-09 13:01:22 +0000

    Fix status counters in chrome://sync-internals
    The regression was introduced in
    https://chromium-review.googlesource.com/1160229 which overlooked the
    fact that the UI makes assumptions about when it's notified about the
    results (i.e. no synchronous reply expected).
    
    There are cleaner alternatives to the proposed fix, but it seems most
    conservative to go back to the previous implementation by partially
    reverting the offending patch.
    
    Bug: None
    Change-Id: I81d82d27234666d407708b1ccd366e493784e9c5
    Reviewed-on: https://chromium-review.googlesource.com/1169045
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581880}

--- components/browser_sync/profile_sync_service.cc
+++ components/browser_sync/profile_sync_service.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/core/browser/account_info.h"
 #include "components/signin/core/browser/signin_manager.h"
 #include "components/signin/core/browser/signin_metrics.h"
+#include "components/sync/base/bind_to_task_runner.h"
 #include "components/sync/base/cryptographer.h"
 #include "components/sync/base/passphrase_type.h"
 #include "components/sync/base/report_unrecoverable_error.h"
@@ -1715,9 +1716,13 @@ std::unique_ptr<base::Value> ProfileSyncService::GetTypeStatusMap() {
                                           dtc_iter->second->state()));
       if (dtc_iter->second->state() !=
           syncer::DataTypeController::NOT_RUNNING) {
-        dtc_iter->second->GetStatusCounters(base::BindRepeating(
-            &ProfileSyncService::OnDatatypeStatusCounterUpdated,
-            base::Unretained(this)));
+        // We use BindToCurrentSequence() to make sure observers (i.e.
+        // |type_debug_info_observers_|) are not notified synchronously, which
+        // the UI code (chrome://sync-internals) doesn't handle well.
+        dtc_iter->second->GetStatusCounters(
+            BindToCurrentSequence(base::BindRepeating(
+                &ProfileSyncService::OnDatatypeStatusCounterUpdated,
+                base::Unretained(this))));
       }
     }
 
