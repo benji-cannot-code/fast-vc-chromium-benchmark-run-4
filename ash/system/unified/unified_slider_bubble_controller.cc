acabe0c6dcb0924fec1c7b0f8e280b02b23cf4de
Tetsui Ohkubo <tetsui@chromium.org>
2018-08-09 06:26:26 +0000

    Modify style of volume slider bubble.
    As UX spec is now ready, this CL updates the style of slider bubbles
    e.g. volume slider and brightness slider.
    
    TEST=manual
    BUG=870473
    
    Change-Id: I8c3ca92e6fd2f8d834c494144578bf06d78276b2
    Reviewed-on: https://chromium-review.googlesource.com/1166779
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581808}

--- ash/system/unified/unified_slider_bubble_controller.cc
+++ ash/system/unified/unified_slider_bubble_controller.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/system/unified/unified_slider_bubble_controller.h"
 
+#include "ash/public/cpp/app_list/app_list_features.h"
 #include "ash/root_window_controller.h"
 #include "ash/shell.h"
 #include "ash/system/audio/unified_volume_slider_controller.h"
@@ -13,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/status_area_widget.h"
 #include "ash/system/tray/tray_constants.h"
 #include "ash/system/unified/unified_system_tray.h"
+#include "ash/system/unified/unified_system_tray_view.h"
 
 using chromeos::CrasAudioHandler;
 
@@ -27,6 +29,12 @@ bool IsAnyMainBubbleShown() {
       return true;
   }
   return false;
+}
+
+void ConfigureSliderViewStyle(views::View* slider_view) {
+  slider_view->SetBackground(UnifiedSystemTrayView::CreateBackground());
+  slider_view->SetBorder(
+      views::CreateEmptyBorder(kUnifiedTopShortcutSpacing, 0, 0, 0));
 }
 
 }  // namespace
@@ -125,6 +133,7 @@ void UnifiedSliderBubbleController::ShowBubble(SliderType slider_type) {
 
       UnifiedSliderView* slider_view =
           static_cast<UnifiedSliderView*>(slider_controller_->CreateView());
+      ConfigureSliderViewStyle(slider_view);
       bubble_view_->AddChildView(slider_view);
       bubble_view_->Layout();
     }
@@ -149,14 +158,16 @@ void UnifiedSliderBubbleController::ShowBubble(SliderType slider_type) {
   init_params.parent_window = tray_->GetBubbleWindowContainer();
   init_params.anchor_view =
       tray_->shelf()->GetSystemTrayAnchor()->GetBubbleAnchor();
+  init_params.corner_radius = kUnifiedTrayCornerRadius;
+  init_params.has_shadow = false;
 
   bubble_view_ = new views::TrayBubbleView(init_params);
   UnifiedSliderView* slider_view =
       static_cast<UnifiedSliderView*>(slider_controller_->CreateView());
+  ConfigureSliderViewStyle(slider_view);
   bubble_view_->AddChildView(slider_view);
-  bubble_view_->SetBorder(
-      views::CreateEmptyBorder(kUnifiedTopShortcutSpacing, 0, 0, 0));
-  bubble_view_->set_color(kUnifiedMenuBackgroundColor);
+  bubble_view_->set_color(SK_ColorTRANSPARENT);
+  bubble_view_->layer()->SetFillsBoundsOpaquely(false);
   bubble_view_->set_anchor_view_insets(
       tray_->shelf()->GetSystemTrayAnchor()->GetBubbleAnchorInsets());
 
@@ -164,6 +175,11 @@ void UnifiedSliderBubbleController::ShowBubble(SliderType slider_type) {
 
   TrayBackgroundView::InitializeBubbleAnimations(bubble_widget_);
   bubble_view_->InitializeAndShowBubble();
+
+  if (app_list::features::IsBackgroundBlurEnabled()) {
+    bubble_widget_->client_view()->layer()->SetBackgroundBlur(
+        kUnifiedMenuBackgroundBlur);
+  }
 
   // Notify value change accessibility event because the popup is triggered by
   // changing value using an accessor key like VolUp.
--- ash/system/unified/unified_system_tray_view.cc
+++ ash/system/unified/unified_system_tray_view.cc
@@ -61,15 +61,6 @@ views::View* CreateNotificationHiddenView() {
   return view;
 }
 
-std::unique_ptr<views::Background> CreateUnifiedBackground() {
-  return views::CreateBackgroundFromPainter(
-      views::Painter::CreateSolidRoundRectPainter(
-          app_list::features::IsBackgroundBlurEnabled()
-              ? kUnifiedMenuBackgroundColorWithBlur
-              : kUnifiedMenuBackgroundColor,
-          kUnifiedTrayCornerRadius));
-}
-
 // Border applied to SystemTrayContainer and DetailedViewContainer to iminate
 // notification list scrolling under SystemTray part of UnifiedSystemTray.
 // The border paints mock notification frame behind the top corners based on
@@ -121,7 +112,7 @@ class SystemTrayContainer : public views::View {
   SystemTrayContainer() {
     SetLayoutManager(
         std::make_unique<views::BoxLayout>(views::BoxLayout::kVertical));
-    SetBackground(CreateUnifiedBackground());
+    SetBackground(UnifiedSystemTrayView::CreateBackground());
     SetBorder(std::make_unique<TopCornerBorder>());
   }
 
@@ -139,7 +130,7 @@ class SystemTrayContainer : public views::View {
 class DetailedViewContainer : public views::View {
  public:
   DetailedViewContainer() {
-    SetBackground(CreateUnifiedBackground());
+    SetBackground(UnifiedSystemTrayView::CreateBackground());
     SetBorder(std::make_unique<TopCornerBorder>());
   }
 
@@ -268,7 +259,7 @@ UnifiedSystemTrayView::UnifiedSystemTrayView(
   auto* layout = SetLayoutManager(
       std::make_unique<views::BoxLayout>(views::BoxLayout::kVertical));
 
-  SetBackground(CreateUnifiedBackground());
+  SetBackground(CreateBackground());
   SetPaintToLayer();
   layer()->SetFillsBoundsOpaquely(false);
 
@@ -409,6 +400,16 @@ void UnifiedSystemTrayView::SetNotificationHeightBelowScroll(
   static_cast<TopCornerBorder*>(detailed_view_container_->border())
       ->set_height_below_scroll(height_below_scroll);
   SchedulePaint();
+}
+
+// static
+std::unique_ptr<views::Background> UnifiedSystemTrayView::CreateBackground() {
+  return views::CreateBackgroundFromPainter(
+      views::Painter::CreateSolidRoundRectPainter(
+          app_list::features::IsBackgroundBlurEnabled()
+              ? kUnifiedMenuBackgroundColorWithBlur
+              : kUnifiedMenuBackgroundColor,
+          kUnifiedTrayCornerRadius));
 }
 
 void UnifiedSystemTrayView::OnGestureEvent(ui::GestureEvent* event) {
