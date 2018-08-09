c9ea2b47140b6880690a8a1b1bad7d63a0a3ab9d
Trent Apted <tapted@chromium.org>
2018-08-09 05:08:31 +0000

    FileManager: Generate unit test html files from closure dependencies.
    JS files don't name their `#includes`, but for closure compilation we
    must effectively list each .js file's #includes in build dependencies.
    We can use this to generate the list of .js files, in the correct
    order, that we use for js unittests. Manually creating the
    foo_unittest.html files is fiddly, error-prone, and is not robust to
    files being moved around. However, it does require the unittest js
    file to be properly closure-compiled.
    
    Start by obsoleting the 3 foo_unittest.html files in
    gallery/image_editor, which are the only ones currently closure-
    compiled.
    
    Note that the dependency files are created _regardless_ of the
    `closure_compilation = true` gn flag. i.e. this does not introduce a
    dependency on the closure _compiler_: dependency information comes from
    the GN build files alone. The closure_compilation flag only affects the
    js_type_check(..) targets, but the approach here uses js_library(..)
    targets.
    
    Bug: 867700
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: I91b3a726b999a9f7db721fefc23b3f6512fd3c0c
    Reviewed-on: https://chromium-review.googlesource.com/1161720
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581795}

--- chrome/browser/chromeos/file_manager/gallery_jstest.cc
+++ chrome/browser/chromeos/file_manager/gallery_jstest.cc
@@ -7,13 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class GalleryJsTest : public FileManagerJsTestBase {
  protected:
-  GalleryJsTest() : FileManagerJsTestBase(
-      base::FilePath(FILE_PATH_LITERAL("ui/file_manager/gallery/js"))) {}
+  GalleryJsTest()
+      : FileManagerJsTestBase(
+            base::FilePath(FILE_PATH_LITERAL("ui/file_manager/gallery/js"))) {}
 };
 
 IN_PROC_BROWSER_TEST_F(GalleryJsTest, ImageEncoderTest) {
-  RunTest(base::FilePath(
-      FILE_PATH_LITERAL("image_editor/image_encoder_unittest.html")));
+  RunGeneratedTest("/image_editor/image_encoder_unittest.html");
 }
 
 // Disabled on ASan builds due to a consistent failure. https://crbug.com/762831
@@ -23,33 +23,29 @@ IN_PROC_BROWSER_TEST_F(GalleryJsTest, ImageEncoderTest) {
 #define MAYBE_ExifEncoderTest ExifEncoderTest
 #endif
 IN_PROC_BROWSER_TEST_F(GalleryJsTest, MAYBE_ExifEncoderTest) {
-  RunTest(base::FilePath(
-      FILE_PATH_LITERAL("image_editor/exif_encoder_unittest.html")));
+  RunGeneratedTest("/image_editor/exif_encoder_unittest.html");
 }
 
 IN_PROC_BROWSER_TEST_F(GalleryJsTest, ImageViewTest) {
-  RunTest(base::FilePath(
-      FILE_PATH_LITERAL("image_editor/image_view_unittest.html")));
+  RunGeneratedTest("/image_editor/image_view_unittest.html");
 }
 
 IN_PROC_BROWSER_TEST_F(GalleryJsTest, EntryListWatcherTest) {
-  RunTest(base::FilePath(
-      FILE_PATH_LITERAL("entry_list_watcher_unittest.html")));
+  RunTest(
+      base::FilePath(FILE_PATH_LITERAL("entry_list_watcher_unittest.html")));
 }
 
 IN_PROC_BROWSER_TEST_F(GalleryJsTest, GalleryUtilTest) {
-  RunTest(base::FilePath(
-      FILE_PATH_LITERAL("gallery_util_unittest.html")));
+  RunTest(base::FilePath(FILE_PATH_LITERAL("gallery_util_unittest.html")));
 }
 
 IN_PROC_BROWSER_TEST_F(GalleryJsTest, GalleryItemTest) {
-  RunTest(base::FilePath(
-      FILE_PATH_LITERAL("gallery_item_unittest.html")));
+  RunTest(base::FilePath(FILE_PATH_LITERAL("gallery_item_unittest.html")));
 }
 
 IN_PROC_BROWSER_TEST_F(GalleryJsTest, GalleryDataModelTest) {
-  RunTest(base::FilePath(
-      FILE_PATH_LITERAL("gallery_data_model_unittest.html")));
+  RunTest(
+      base::FilePath(FILE_PATH_LITERAL("gallery_data_model_unittest.html")));
 }
 
 IN_PROC_BROWSER_TEST_F(GalleryJsTest, RibbonTest) {
