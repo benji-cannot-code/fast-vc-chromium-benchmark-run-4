117792313880b6ef55720e2cd9f15ea306fe3437
Oscar Johansson <oscarj@opera.com>
2018-08-09 11:32:36 +0000

    Move TruncateUTF8 to util (components/autofill)
    When building using jumbo, files gets merged and
    functions with the same name may end up in the
    same namespace and conflict. This happens for
    the function TruncateUTF8.
    
    This commit solves the issue by moving the function
    to a shared util file.
    
    Bug: 869381
    Change-Id: Ia9f754138fb18aa9af575f7cd2f92bca4e8ea0c2
    Reviewed-on: https://chromium-review.googlesource.com/1168497
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Commit-Queue: Oscar Johansson <oscarj@opera.com>
    Cr-Commit-Position: refs/heads/master@{#581866}

--- components/autofill/core/browser/autofill_profile_sync_util.cc
+++ components/autofill/core/browser/autofill_profile_sync_util.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/guid.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
+#include "components/autofill/core/browser/autofill_data_util.h"
 #include "components/autofill/core/browser/autofill_profile.h"
 #include "components/autofill/core/browser/country_names.h"
 #include "components/autofill/core/browser/field_types.h"
@@ -17,18 +18,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using base::UTF16ToUTF8;
 using base::UTF8ToUTF16;
+using autofill::data_util::TruncateUTF8;
 using sync_pb::AutofillProfileSpecifics;
 using syncer::EntityData;
 
 namespace autofill {
 namespace {
-
-std::string TruncateUTF8(const std::string& data) {
-  std::string trimmed_value;
-  base::TruncateUTF8ToByteSize(data, AutofillTable::kMaxDataLength,
-                               &trimmed_value);
-  return trimmed_value;
-}
 
 bool IsAutofillProfileSpecificsValid(
     const AutofillProfileSpecifics& specifics) {
