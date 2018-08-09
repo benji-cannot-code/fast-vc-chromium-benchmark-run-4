fce58431adeaee7e7110ac6e723a8f4d994f87fc
Michael Lippautz <mlippautz@chromium.org>
2018-08-09 11:30:03 +0000

    [heap] Incremental marking: Do not promptly free HashTable backings
    Previously we checked whether a backing could be freed during
    incremental marking using the provided Traits and kWeakHandlingFlag. It
    turns out that WeakMember may also be part of otherwise strongly treated
    in-place constructed objects, e.g., std::pair<WeakMember<T>,U>.
    
    Completely turn off promptly freeing for HashTable backings during
    incremental marking.
    
    Bug: chromium:870306, chromium:757440
    Change-Id: I94aacad0dbba707a4e0fb8c8119591fd413f9314
    Reviewed-on: https://chromium-review.googlesource.com/1165353
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581865}

--- third_party/blink/renderer/platform/wtf/allocator/partition_allocator.cc
+++ third_party/blink/renderer/platform/wtf/allocator/partition_allocator.cc
@@ -18,8 +18,7 @@ void PartitionAllocator::FreeVectorBacking(void* address) {
   Partitions::BufferFree(address);
 }
 
-void PartitionAllocator::FreeHashTableBacking(void* address,
-                                              bool is_weak_table) {
+void PartitionAllocator::FreeHashTableBacking(void* address) {
   Partitions::BufferFree(address);
 }
 
