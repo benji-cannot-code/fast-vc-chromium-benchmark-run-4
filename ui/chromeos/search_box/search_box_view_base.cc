873290c86e4e0206ca70bcef07002be7b7ad2639
Kevin Strohbehn <ginko@google.com>
2018-08-09 22:30:53 +0000

    Destroy touch controller when search box isn't being interacted with
    Bug: 869638
    Change-Id: Ifea913dd4df2c94487173e87eba2bd860802c6c9
    Reviewed-on: https://chromium-review.googlesource.com/1168475
    Commit-Queue: Kevin Strohbehn <ginko@google.com>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Weidong Guo <weidongg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581926}

--- ui/chromeos/search_box/search_box_view_base.cc
+++ ui/chromeos/search_box/search_box_view_base.cc
@@ -268,6 +268,8 @@ void SearchBoxViewBase::SetSearchBoxActive(bool active,
   if (active) {
     search_box_->RequestFocus();
     RecordSearchBoxActivationHistogram(event_type);
+  } else {
+    search_box_->DestroyTouchSelection();
   }
 
   search_box_right_space_->SetVisible(!active);
@@ -310,6 +312,10 @@ void SearchBoxViewBase::OnGestureEvent(ui::GestureEvent* event) {
 
 void SearchBoxViewBase::OnMouseEvent(ui::MouseEvent* event) {
   HandleSearchBoxEvent(event);
+}
+
+void SearchBoxViewBase::NotifyGestureEvent() {
+  search_box_->DestroyTouchSelection();
 }
 
 ax::mojom::Role SearchBoxViewBase::GetAccessibleWindowRole() const {
