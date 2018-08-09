8481e48d71376f81f92f2832a6a9fa96d02283c6
Raymes Khoury <raymes@chromium.org>
2018-08-09 09:34:48 +0000

    Change SubscribePermissionStatusChange to use a RFH to query permissions
    Currently updates to permission change subscriptions in
    PermissionManager won't use the RenderFrameHost to determine the
    permission value. This results in incorrect permission updates being
    sent in some cases. This changes updates to query
    GetPermissionStatusForFrame when possible. This is only not possible
    when the request is from a worker in which case we just use the worker's
    origin.
    
    Tbr: slan@chromium.org, asanka@chromium.org
    Bug: 802945
    Change-Id: Ia69f7de8f166000661b5560a2f430b3787872b75
    Reviewed-on: https://chromium-review.googlesource.com/979735
    Commit-Queue: Raymes Khoury <raymes@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581843}

--- content/browser/permissions/permission_controller_impl.h
+++ content/browser/permissions/permission_controller_impl.h
@@ -56,8 +56,8 @@ class CONTENT_EXPORT PermissionControllerImpl : public PermissionController {
 
   int SubscribePermissionStatusChange(
       PermissionType permission,
+      RenderFrameHost* render_frame_host,
       const GURL& requesting_origin,
-      const GURL& embedding_origin,
       const base::Callback<void(blink::mojom::PermissionStatus)>& callback);
 
   void UnsubscribePermissionStatusChange(int subscription_id);
