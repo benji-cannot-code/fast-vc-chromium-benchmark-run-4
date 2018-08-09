46cdbc9376bc59a8a7b02f4fcb59d32411c47ca1
Chris Harrelson <chrishtr@chromium.org>
2018-08-09 02:19:41 +0000

    Don't force paint invalidation on non-composited scroll.
    This is no longer necessary because, in SPv175:
    a) Object visual rects are stored relative to their transform ancestor,
    which is not affected by change of that scroll transform.
    b) Raster invalidations happen on paint chunks, which takes care of
    descendants which don't scroll.
    
    We do, however need to set NeedsRepaint on the scrolling PaintLayer,
    because its descendants could have been affected by a changed cull rect.
    
    Bug: 823751
    
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I9865e336233a92e1aac9c25ea221cec039f6abb1
    Reviewed-on: https://chromium-review.googlesource.com/1159382
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581763}

--- third_party/blink/renderer/core/paint/paint_layer_scrollable_area.h
+++ third_party/blink/renderer/core/paint/paint_layer_scrollable_area.h
@@ -379,8 +379,6 @@ class CORE_EXPORT PaintLayerScrollableArea final
   void UpdateAfterStyleChange(const ComputedStyle*);
   void UpdateAfterOverflowRecalc();
 
-  void UpdateAfterCompositingChange();
-
   bool HasScrollbar() const {
     return HasHorizontalScrollbar() || HasVerticalScrollbar();
   }
