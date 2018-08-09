cba26546aa5d72534ce23d4c37e06dbb591d9e56
Tetsui Ohkubo <tetsui@chromium.org>
2018-08-09 08:40:53 +0000

    Fix glitch when creating notification during drag
    In UnifiedSystemTray, we only optimize expand/collapse transform when
    there is no notification. We should update the layer transform state
    when a notification is created during dragging system tray.
    
    TEST=manual
    BUG=863577
    
    Change-Id: Iaff5438a9e8f50b621a48cff5c9f197f48d7617e
    Reviewed-on: https://chromium-review.googlesource.com/1168709
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581826}

--- ash/system/unified/unified_message_center_view.cc
+++ ash/system/unified/unified_message_center_view.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/message_center/views/notification_control_buttons_view.h"
 #include "ui/views/controls/scroll_view.h"
 #include "ui/views/layout/box_layout.h"
+#include "ui/views/widget/widget.h"
 
 using message_center::MessageCenter;
 using message_center::MessageView;
@@ -233,6 +234,8 @@ void UnifiedMessageCenterView::NotifyHeightBelowScroll() {
 
 void UnifiedMessageCenterView::Update() {
   SetVisible(message_list_view_->GetNotificationCount() > 0);
+  if (GetWidget() && !GetWidget()->IsClosed())
+    tray_controller_->OnMessageCenterVisibilityUpdated();
 
   size_t notification_count = message_list_view_->GetNotificationCount();
   // TODO(tetsui): This is O(n^2).
