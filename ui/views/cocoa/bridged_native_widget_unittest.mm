4c5d5c6a777ae794097f0ab30d798994124c53fe
Christopher Cameron <ccameron@chromium.org>
2018-08-09 23:43:19 +0000

    RemoteMacViews: Remove most views calls from BridgedContentView
    In BridgedContentView, change most calls that refer to the views::View
    hostedView_ to be routed through the BridgedNativeWidgetHost interface.
    
    Leave keyboard events, text input, and a11y unchanged for now.
    
    Add a BridgedNativeWidgetHostImpl::SetRootLayer method which now calls
    through to BridgedNativeWidget::SetRootLayer. Once the remaining uses
    of hostedView_ are removed, the BridgedNativeWidget::SetRootLayer method
    will be removed.
    
    Bug: 859152
    Change-Id: I2a010b3149357af4f9aeaec0088eb9fcde53db13
    Reviewed-on: https://chromium-review.googlesource.com/1168459
    Commit-Queue: ccameron <ccameron@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581960}

--- ui/views/cocoa/bridged_native_widget_unittest.mm
+++ ui/views/cocoa/bridged_native_widget_unittest.mm
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/test/cocoa_test_event_utils.h"
 #import "ui/gfx/mac/coordinate_conversion.h"
 #import "ui/views/cocoa/bridged_content_view.h"
+#import "ui/views/cocoa/bridged_native_widget_host_impl.h"
 #import "ui/views/cocoa/native_widget_mac_nswindow.h"
 #import "ui/views/cocoa/views_nswindow_delegate.h"
 #include "ui/views/controls/textfield/textfield.h"
@@ -290,6 +291,7 @@ class MockNativeWidgetMac : public NativeWidgetMac {
   explicit MockNativeWidgetMac(internal::NativeWidgetDelegate* delegate)
       : NativeWidgetMac(delegate) {}
   using NativeWidgetMac::bridge;
+  using NativeWidgetMac::bridge_host_for_testing;
 
   // internal::NativeWidgetPrivate:
   void InitNativeWidget(const Widget::InitParams& params) override {
@@ -324,6 +326,9 @@ class BridgedNativeWidgetTestBase : public ui::CocoaTest {
       : native_widget_mac_(nullptr) {}
 
   BridgedNativeWidget* bridge() { return native_widget_mac_->bridge(); }
+  BridgedNativeWidgetHostImpl* bridge_host() {
+    return native_widget_mac_->bridge_host_for_testing();
+  }
 
   // Overridden from testing::Test:
   void SetUp() override {
@@ -572,7 +577,7 @@ void BridgedNativeWidgetTest::SetUp() {
 
   // The delegate should exist before setting the root view.
   EXPECT_TRUE([window delegate]);
-  bridge()->SetRootView(view_.get());
+  bridge_host()->SetRootView(view_.get());
   ns_view_ = bridge()->ns_view();
 
   // Pretend it has been shown via NativeWidgetMac::Show().
@@ -584,8 +589,8 @@ void BridgedNativeWidgetTest::TearDown() {
   // Clear kill buffer so that no state persists between tests.
   TextfieldModel::ClearKillBuffer();
 
-  if (bridge())
-    bridge()->SetRootView(nullptr);
+  if (bridge_host())
+    bridge_host()->SetRootView(nullptr);
   view_.reset();
   BridgedNativeWidgetTestBase::TearDown();
 }
