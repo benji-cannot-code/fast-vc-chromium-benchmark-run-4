02bd974abe60c5528a8873649809f4c59f9a7f8b
Kenichi Ishibashi <bashi@chromium.org>
2018-08-09 08:51:22 +0000

    S13nServiceWorker: Fix URLLoaderClientImplTests
    crrev.com/c/1160133 introduced MimeSniffingThrottle which may
    defer a request. Since URLLoaderClientImplTest has a (probably
    wrong) assumption that the request wouldn't be deferred, these
    tests were failing when S13nServiceWorker/Network is on.
    To work around the assumption this CL sets
    |fetch_request_context_type| to REQUEST_CONTEXT_TYPE_FETCH so
    that ResourceDispatcher won't install MimeSniffingThrottle.
    
    Bug: 860361
    Change-Id: I6dab15515089a46f1b1c2a7501c826b9433e46b3
    Reviewed-on: https://chromium-review.googlesource.com/1168705
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581830}

--- content/renderer/loader/url_loader_client_impl_unittest.cc
+++ content/renderer/loader/url_loader_client_impl_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 #include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
+#include "content/public/common/request_context_type.h"
 #include "content/renderer/loader/navigation_response_override_parameters.h"
 #include "content/renderer/loader/resource_dispatcher.h"
 #include "content/renderer/loader/test_request_peer.h"
@@ -26,8 +27,13 @@ class URLLoaderClientImplTest : public ::testing::Test,
                                 public network::mojom::URLLoaderFactory {
  protected:
   URLLoaderClientImplTest() : dispatcher_(new ResourceDispatcher()) {
+    auto request = std::make_unique<network::ResourceRequest>();
+    // Set request context type to fetch so that ResourceDispatcher doesn't
+    // install MimeSniffingThrottle, which makes URLLoaderThrottleLoader
+    // defer the request.
+    request->fetch_request_context_type = REQUEST_CONTEXT_TYPE_FETCH;
     request_id_ = dispatcher_->StartAsync(
-        std::make_unique<network::ResourceRequest>(), 0,
+        std::move(request), 0,
         blink::scheduler::GetSingleThreadTaskRunnerForTesting(),
         TRAFFIC_ANNOTATION_FOR_TESTS, false, false,
         std::make_unique<TestRequestPeer>(dispatcher_.get(),
