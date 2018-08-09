de035ecda4ec06f494a7e6754350930388a65430
Nicholas Verne <nverne@chromium.org>
2018-08-09 07:16:42 +0000

    MaybeUpradeCrostini happens in the correct browser session
    Previously, we would lose any CrostiniManager state set in UserSessionManager
    because it quits and restarts the browser, killing the old CrostiniManager with
    it.
    
    Also fixed a bug where newly installing Termina didn't set the CrostiniManager
    state variable correctly.
    
    Bug: 871651
    Change-Id: I2cfa3288069fccd86280e4301cd4757eb656e02d
    Reviewed-on: https://chromium-review.googlesource.com/1166271
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Commit-Queue: Nicholas Verne <nverne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581815}

--- chrome/browser/chromeos/login/session/chrome_session_manager.cc
+++ chrome/browser/chromeos/login/session/chrome_session_manager.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/boot_times_recorder.h"
 #include "chrome/browser/chromeos/child_accounts/consumer_status_reporting_service_factory.h"
 #include "chrome/browser/chromeos/child_accounts/screen_time_controller_factory.h"
+#include "chrome/browser/chromeos/crostini/crostini_manager.h"
 #include "chrome/browser/chromeos/lock_screen_apps/state_controller.h"
 #include "chrome/browser/chromeos/login/demo_mode/demo_session.h"
 #include "chrome/browser/chromeos/login/lock/webui_screen_locker.h"
@@ -143,6 +144,8 @@ void StartUserSession(Profile* user_profile, const std::string& login_user_id) {
       policy::AppInstallEventLogManagerWrapper::CreateForProfile(user_profile);
     }
     arc::ArcServiceLauncher::Get()->OnPrimaryUserProfilePrepared(user_profile);
+    crostini::CrostiniManager::GetInstance()->MaybeUpgradeCrostini(
+        user_profile);
 
     if (user->GetType() == user_manager::USER_TYPE_CHILD) {
       ScreenTimeControllerFactory::GetForBrowserContext(user_profile);
