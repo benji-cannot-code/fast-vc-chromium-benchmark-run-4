7e1eda949068496d58b8352efa76d3ab9930e1b6
Jun Mukai <mukai@chromium.org>
2018-08-10 18:28:53 +0000

    use random GUID for the device id of the offline demo mode
    Previously the device id was extracted from the offline policy,
    but that means all of the offline-enrolled device shared the same
    device id (since the offline policy data is stored on the device).
    
    This isn't a big issue as long as the device stays offline,
    but we should use the generated ID to allow making the transition
    to the online demo mode after the offline enrollment is conducted.
    
    BUG=827369
    TEST=manually walked through the offline enrollment
    
    Change-Id: I0d9713d78c08fabcd0fb49326e1acca9ee578b98
    Reviewed-on: https://chromium-review.googlesource.com/1147336
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Commit-Queue: Jun Mukai <mukai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582257}

--- chrome/browser/chromeos/policy/enrollment_handler_chromeos.cc
+++ chrome/browser/chromeos/policy/enrollment_handler_chromeos.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base64.h"
 #include "base/bind.h"
 #include "base/command_line.h"
+#include "base/guid.h"
 #include "base/location.h"
 #include "base/logging.h"
 #include "base/sequenced_task_runner.h"
@@ -491,7 +492,9 @@ void EnrollmentHandlerChromeOS::OnOfflinePolicyValidated(
     return;
   }
 
-  device_id_ = validator->policy_data()->device_id();
+  // Don't use the device ID within the validated policy -- it's common among
+  // all of the offline-enrolled devices.
+  device_id_ = base::GenerateGUID();
   policy_ = std::move(validator->policy());
 
   // The steps for OAuth2 token fetching is skipped for the OFFLINE_DEMO_MODE.
