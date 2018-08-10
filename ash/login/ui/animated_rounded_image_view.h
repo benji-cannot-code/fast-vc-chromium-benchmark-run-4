fdc513d8eabc5ab236e162cc836c64efae813f4e
Jacob Dufault <jdufault@google.com>
2018-08-10 18:07:19 +0000

    cros: Do not play animations for non-focused users.
    Bug: 872923
    Change-Id: I7071921895fcd017a65286fce4b565ad8f96de76
    Reviewed-on: https://chromium-review.googlesource.com/1170035
    Commit-Queue: Jacob Dufault <jdufault@chromium.org>
    Reviewed-by: Xiaoyin Hu <xiaoyinh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582245}

--- ash/login/ui/animated_rounded_image_view.h
+++ ash/login/ui/animated_rounded_image_view.h
@@ -25,7 +25,7 @@ class AnimatedRoundedImageView : public views::View {
   enum class Playback {
     kFirstFrameOnly,  // Only the first frame in the animation will be shown.
     kSingle,          // Play the animation only once.
-    kRepeat,          // Play the animation repeatly.
+    kRepeat,          // Play the animation repeatedly.
   };
 
   // Provides animation frames.
@@ -49,6 +49,9 @@ class AnimatedRoundedImageView : public views::View {
 
   // Set playback type of the animation.
   void SetAnimationPlayback(Playback playback);
+
+  // Get the current type of animation playback.
+  Playback playback() const { return playback_; }
 
   // views::View:
   gfx::Size CalculatePreferredSize() const override;
