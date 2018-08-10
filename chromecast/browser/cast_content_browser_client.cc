4724b0192fef166b53f5c33dbad3f802edb800fa
Lukasz Anforowicz <lukasza@chromium.org>
2018-08-10 17:23:35 +0000

    Make //content layer default to --site-per-process mode on desktop.
    After this CL site-per-process is the default in all layers on desktop
    platforms, except:
    
    - Layout Tests which still run with no isolation by default
      (see https://crbug.com/856734#c5 for the explanation why)
    
    - //content embedders that don't want to use site-per-process: //chromecast
    
    - //content embedders that don't yet support site-per-process, but will
      need to migrate eventually: //headless
    
    
    Also note that even after this CL:
    
    - //content on Android still defaults to no isolation.
      This is compatible with not_site_per_process_* test steps
      because such steps are not run on Android bots...
    
    - //chrome layer ChromeContentBrowserClient controls usage of
      site-per-process in the Chrome browser and continues to be controlled
      by a field trial (note that Android is not covered by the field trial
      or by testing/variations/fieldtrial_testing_config.json).
    
    
    Bug: 856734
    Change-Id: Iebd46e5502583f84a4b0a7c2ee8d8b0e081c6ff2
    Reviewed-on: https://chromium-review.googlesource.com/1153981
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Stephen Lanham <slan@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582223}

--- chromecast/browser/cast_content_browser_client.cc
+++ chromecast/browser/cast_content_browser_client.cc
@@ -742,6 +742,10 @@ CastContentBrowserClient::GetNavigationUIData(
   return std::make_unique<CastNavigationUIData>(session_id);
 }
 
+bool CastContentBrowserClient::ShouldEnableStrictSiteIsolation() {
+  return false;
+}
+
 scoped_refptr<net::X509Certificate> CastContentBrowserClient::DeviceCert() {
   return nullptr;
 }
