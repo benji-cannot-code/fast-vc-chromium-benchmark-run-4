bbfa27a82454237c28ff755f4f08b12d4a0f50ff
Alexander Alekseev <alemate@chromium.org>
2018-08-10 00:12:37 +0000

    Reland "ChromeOS OOBE: Add Welcome screen animations."
    This is a reland of 04b716f7608bea53d0903f821ef0aa4f59741b6d
    
    Original change's description:
    > ChromeOS OOBE: Add Welcome screen animations.
    > 
    > This Cl replaces static images with animations.
    > 
    > Bug: 852038
    > Change-Id: I8185420aed0ed23c080ac0bc7a2270d3ac4c5b7b
    > Reviewed-on: https://chromium-review.googlesource.com/1166590
    > Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    > Reviewed-by: Wenzhao (Colin) Zang <wzang@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#581735}
    
    Bug: 852038
    Change-Id: I50659b137bc2e4c1facb7a3b3b734e1a1d3a6500
    Reviewed-on: https://chromium-review.googlesource.com/1168328
    Reviewed-by: Wenzhao (Colin) Zang <wzang@chromium.org>
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581970}

--- chrome/browser/resources/chromeos/login/oobe_welcome.js
+++ chrome/browser/resources/chromeos/login/oobe_welcome.js
@@ -95,6 +95,10 @@ Polymer({
       if (behavior.onBeforeShow)
         behavior.onBeforeShow.call(this);
     });
+
+    let activeScreen = this.getActiveScreen_();
+    if (activeScreen.show)
+      activeScreen.show();
   },
 
   /**
