61306016b122c8c2c60e8c07ab61d6cedb518ac6
Michael Spang <spang@chromium.org>
2018-08-10 18:26:14 +0000

    ozone: drm: Allow importing buffers that cannot be added as framebuffers
    After 6fc38604ad77 ("ozone: drm: Move DrmFramebuffer from GbmBuffer to
    GbmPixmap"), if a buffer's format is supported for scanout but cannot
    be added as a framebuffer, we fail the import.
    
    This broke CTS tests, so relax this test and add a note.
    
    Bug: 872681
    Test: ozone_unittests
    
    Change-Id: I35e18f043abe21f5554369129da05448f990bfd4
    Reviewed-on: https://chromium-review.googlesource.com/1169595
    Commit-Queue: Michael Spang <spang@chromium.org>
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582254}

--- ui/ozone/platform/drm/gpu/drm_thread.cc
+++ ui/ozone/platform/drm/gpu/drm_thread.cc
@@ -203,9 +203,9 @@ void DrmThread::CreateBufferFromFds(
 
   scoped_refptr<DrmFramebuffer> framebuffer;
   if (buffer->GetFlags() & GBM_BO_USE_SCANOUT) {
+    // NB: This is not required to succeed; framebuffers are added for
+    // imported buffers on a best effort basis.
     framebuffer = DrmFramebuffer::AddFramebuffer(drm, buffer.get());
-    if (!framebuffer)
-      return;
   }
 
   *out_buffer = std::move(buffer);
