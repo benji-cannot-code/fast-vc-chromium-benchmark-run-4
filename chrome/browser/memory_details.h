6850d040e61aeb8a16a68ca05012bb72745c0ee1
Erik Chen <erikchen@chromium.org>
2018-08-10 18:02:12 +0000

    Move logic for site details histograms into MetricsMemoryDetails.
    This CL is a refactor with no intended behavior change.
    
    This CL is the beginning of a refactor to separate the two types of consumers of
    MemoryDetails:
      * Those that want a human readable string that shows a mapping from [process :
      memory usage].
      * Those that want to emit non-memory related metrics.
    
    Bug: 864352
    Change-Id: I0f2a968ef63b3c9fcb658898a2f3f55f68867285
    Reviewed-on: https://chromium-review.googlesource.com/1170587
    Reviewed-by: Nick Carter <nick@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582243}

--- chrome/browser/memory_details.h
+++ chrome/browser/memory_details.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string16.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
-#include "chrome/browser/site_details.h"
 #include "content/public/common/process_type.h"
 
 namespace memory_instrumentation {
@@ -86,10 +85,6 @@ struct ProcessData {
   base::string16 name;
   base::string16 process_name;
   ProcessMemoryInformationList processes;
-
-  // Track site data for predicting process counts with out-of-process iframes.
-  // See site_details.h.
-  BrowserContextSiteDataMap site_data;
 };
 
 // MemoryDetails fetches memory details about current running browsers.
