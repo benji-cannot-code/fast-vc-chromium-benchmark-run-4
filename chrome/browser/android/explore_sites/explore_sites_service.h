4958f6e8ac78002d601c0da9fa8d8a2e45e3fb27
Justin DeWitt <dewittj@chromium.org>
2018-08-10 23:32:35 +0000

    [Explore Sites] Create a keyed service.
    This will host the ExploreSites datastore as well as the downloader.
    
    BUG=867488
    
    Change-Id: Ifff84b6c8316033929ccbd52c32c2f175af117c8
    Reviewed-on: https://chromium-review.googlesource.com/1169588
    Reviewed-by: Cathy Li <chili@chromium.org>
    Commit-Queue: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582385}

---
+++ chrome/browser/android/explore_sites/explore_sites_service.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ANDROID_EXPLORE_SITES_EXPLORE_SITES_SERVICE_H_
+#define CHROME_BROWSER_ANDROID_EXPLORE_SITES_EXPLORE_SITES_SERVICE_H_
+
+#include "components/keyed_service/core/keyed_service.h"
+
+namespace explore_sites {
+
+// Main class and entry point for the Explore Sites feature, that
+// controls the lifetime of all major subcomponents.
+class ExploreSitesService : public KeyedService {
+ public:
+  ~ExploreSitesService() override = default;
+};
+
+}  // namespace explore_sites
+
+#endif  // CHROME_BROWSER_ANDROID_EXPLORE_SITES_EXPLORE_SITES_SERVICE_H_
