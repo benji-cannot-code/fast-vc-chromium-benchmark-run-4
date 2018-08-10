c45cb7d3efd9cc949b5696a708eeeccf0634dbfb
Ken Rockot <rockot@chromium.org>
2018-08-10 15:48:25 +0000

    Fix http/tests/event-timing/event-timing-onloadthenobserve.html
    Changes this test to properly wait for the first synthetic click
    event to fire before proceeding with the rest of the test and validating
    its expectations.
    
    Bug: 872081
    Change-Id: I0954ef7deff8415600effbc79cca7402a1b51583
    Reviewed-on: https://chromium-review.googlesource.com/1170173
    Commit-Queue: Ken Rockot <rockot@chromium.org>
    Reviewed-by: Nicolás Peña Moreno <npm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582184}

--- third_party/WebKit/LayoutTests/http/tests/event-timing/resources/event-timing-support.js
+++ third_party/WebKit/LayoutTests/http/tests/event-timing/resources/event-timing-support.js
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-function clickOnElement(id) {
-  const rect = document.getElementById(id).getBoundingClientRect();
+function clickOnElement(id, callback) {
+  const element = document.getElementById(id);
+  const rect = element.getBoundingClientRect();
   const xCenter = rect.x + rect.width / 2;
   const yCenter = rect.y + rect.height / 2;
   var pointerActions = [{
@@ -10,6 +11,12 @@ function clickOnElement(id) {
       { name: "pointerUp" },
     ]
   }];
+  var clickHandler = () => {
+    if (callback)
+      callback();
+    element.removeEventListener("click", clickHandler);
+  };
+  element.addEventListener("click", clickHandler);
   if (!chrome || !chrome.gpuBenchmarking) {
     reject();
   } else {
@@ -61,8 +68,7 @@ function wait() {
 
 function clickAndBlockMain(id) {
   return new Promise((resolve, reject) => {
-    clickOnElement(id);
+    clickOnElement(id, resolve);
     mainThreadBusy(300);
-    resolve();
   });
 }
