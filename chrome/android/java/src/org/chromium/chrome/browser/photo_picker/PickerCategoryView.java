801a3de706afec2e88a7ccfe72470af7a6a306c6
Sam Maier <smaier@chromium.org>
2018-08-10 21:56:46 +0000

    Reland "Android: moving FileEnumWorkerTask to thread pool"
    This reverts commit c2af33f66d4079e4306fb698828d6d9d6d4fbe95.
    
    Reason for revert: Just re landing now that gerrit is OK
    
    Original change's description:
    > Revert "Android: moving FileEnumWorkerTask to thread pool"
    >
    > This reverts commit 5eb802f9e1b7c765f77ec4a61f7f448731c36811.
    >
    > Reason for revert: Due to Gerrit outage http://crbug.com/872722, we are reverting this CL. Please, re-land it after all clear is given. If you have questions, please ask on the bug. Sorry for the inconvenience.
    >
    > Original change's description:
    > > Android: moving FileEnumWorkerTask to thread pool
    > >
    > > Currently, AsyncTask.execute() defaults to the SERIAL_EXECUTOR. This
    > > exector is good for preventing concurrency errors since it guarantees
    > > serial execution, but bad for performance since the entire app shares
    > > this single queue.
    > >
    > > It looks like this callsite can use the THREAD_POOL_EXECUTOR instead,
    > > since this use doesn't appear to rely on the concurrency guarantees that
    > > SERIAL_EXECUTOR provides.
    > >
    > > Bug: 869907
    > > Change-Id: I266476c44f16ade391dfef0e5a99d33494312598
    > > Reviewed-on: https://chromium-review.googlesource.com/1161089
    > > Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    > > Commit-Queue: Sam Maier <smaier@chromium.org>
    >
    > TBR=finnur@chromium.org,smaier@chromium.org
    >
    > Change-Id: I890bfff82b7471f54948c03dea7d2820224e978f
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 869907
    > Reviewed-on: https://chromium-review.googlesource.com/1169851
    > Reviewed-by: Andrii Shyshkalov <tandrii@chromium.org>
    > Commit-Queue: Andrii Shyshkalov <tandrii@chromium.org>
    
    TBR=finnur@chromium.org,tandrii@chromium.org,smaier@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    TBR=Gerrit reland
    
    Bug: 869907
    Change-Id: I4e07a7d43ae7f0af01a248f4c41d42699d041f30
    Reviewed-on: https://chromium-review.googlesource.com/1171427
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Reviewed-by: Andrii Shyshkalov <tandrii@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582352}

--- chrome/android/java/src/org/chromium/chrome/browser/photo_picker/PickerCategoryView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/photo_picker/PickerCategoryView.java
@@ -19,6 +19,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
+import org.chromium.base.AsyncTask;
 import org.chromium.base.DiscardableReferencePool.DiscardableReference;
 import org.chromium.base.VisibleForTesting;
 import org.chromium.base.metrics.RecordHistogram;
@@ -363,7 +364,7 @@ public class PickerCategoryView extends RelativeLayout
         mEnumStartTime = SystemClock.elapsedRealtime();
         mWorkerTask = new FileEnumWorkerTask(
                 mActivity.getWindowAndroid(), this, new MimeTypeFileFilter(mMimeTypes));
-        mWorkerTask.execute();
+        mWorkerTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
     }
 
     /**
