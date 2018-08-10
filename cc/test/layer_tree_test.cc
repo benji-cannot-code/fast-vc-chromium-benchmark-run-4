6fa60eb92c51d1d48cd5a2b6ce2114667191de8d
Adrienne Walker <enne@chromium.org>
2018-08-10 22:44:54 +0000

    cc: Increase layer tree test timeout for CFI
    Also, generally increase the timeout for layer tree tests.  Most things
    don't need this, however the GL versions of pixel tests end up spending
    quite a bit of time to compile shaders.  When run in parallel, this
    only exacerbates the problem and tests can time out.  Be more generous
    so that folks stop turning off these tests.
    
    Bug: 872733
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I7c08212d454bf0d9ae6ea445fcb46b48b73aea88
    Reviewed-on: https://chromium-review.googlesource.com/1171377
    Reviewed-by: weiliangc <weiliangc@chromium.org>
    Commit-Queue: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582367}

--- cc/test/layer_tree_test.cc
+++ cc/test/layer_tree_test.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "cc/test/layer_tree_test.h"
 
+#include "base/cfi_buildflags.h"
 #include "base/command_line.h"
 #include "base/location.h"
 #include "base/memory/ptr_util.h"
@@ -627,12 +628,16 @@ LayerTreeTest::LayerTreeTest()
 #elif defined(MEMORY_SANITIZER)
     // MSAN is slower than uninstrumented code
     timeout_seconds_ = 20;
+#elif BUILDFLAG(CFI_CAST_CHECK) || BUILDFLAG(CFI_ICALL_CHECK) || \
+    BUILDFLAG(CFI_ENFORCEMENT_DIAGNOSTIC) || BUILDFLAG(CFI_ENFORCEMENT_TRAP)
+    // CFI is slow as well.
+    timeout_seconds_ = 20;
 #elif defined(ADDRESS_SANITIZER) || defined(_DEBUG) || defined(USE_OZONE)
     // ASAN and Debug builds are slower than release builds, as expected
     // Ozone builds also go through a slower path than regular Linux builds
-    timeout_seconds_ = 12;
+    timeout_seconds_ = 15;
 #else
-    timeout_seconds_ = 6;
+    timeout_seconds_ = 10;
 #endif
   if (command_line->HasSwitch(switches::kCCLayerTreeTestLongTimeout))
     timeout_seconds_ = 5 * 60;
