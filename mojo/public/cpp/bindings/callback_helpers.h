3669ca0f528521c4c3fb4773d5b09d707facffcd
Eric Roman <eroman@chromium.org>
2018-08-10 22:56:21 +0000

    Fix a typo in documentation.
    Change-Id: Ibbde1b31fdeade51caf0cb70fefbe915bd7a9af6
    Reviewed-on: https://chromium-review.googlesource.com/1169693
    Commit-Queue: Eric Roman <eroman@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582376}

--- mojo/public/cpp/bindings/callback_helpers.h
+++ mojo/public/cpp/bindings/callback_helpers.h
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // This is a helper utility to wrap a base::OnceCallback such that if the
 // callback is destructed before it has a chance to run (e.g. the callback is
-// bound into a task and the task is dropped), it will be run the with
+// bound into a task and the task is dropped), it will be run with the
 // default arguments passed into WrapCallbackWithDefaultInvokeIfNotRun.
 // Alternatively, it will run the delete closure passed to
 // WrapCallbackWithDropHandler.
