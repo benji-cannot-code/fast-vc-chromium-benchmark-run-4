36389b90990034f42ec5f51fd1dff183e3fec921
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-08-10 09:06:21 +0000

    Import wpt@8ab7aace6a65a045a1b8823988623afc4c1e4f09
    Using wpt-import in Chromium 836ab12f9c986027cc661cdd9354f61b95ccf30d.
    With Chromium commits locally applied on WPT:
    7dad0f1723 "Added new Sec-Metadata Web Platform Tests."
    
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/22581
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    domenic@chromium.org, ricea@chromium.org:
      external/wpt/streams
    mkwst@chromium.org:
      external/wpt/secure-contexts
    
    NOAUTOREVERT=true
    TBR=robertma
    
    No-Export: true
    Change-Id: I9b98ffb1a68c9d79a96ce45414872cb43d7e0ccb
    Reviewed-on: https://chromium-review.googlesource.com/1170563
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582097}

--- third_party/WebKit/LayoutTests/external/wpt/secure-contexts/idlharness.any.js
+++ third_party/WebKit/LayoutTests/external/wpt/secure-contexts/idlharness.any.js
@@ -7,15 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 'use strict';
 
-promise_test(async () => {
-  const idl = await fetch("/interfaces/secure-contexts.idl").then(r => r.text());
-  const workers = await fetch("/interfaces/dedicated-workers.idl").then(r => r.text());
-
-  const idl_array = new IdlArray();
-  idl_array.add_idls(idl);
-  idl_array.add_dependency_idls(workers);
-  idl_array.add_objects({
-    WindowOrWorkerGlobalScope: ["self"],
-  });
-  idl_array.test();
-}, "Test IDL implementation of Secure Contexts");
+idl_test(
+  ['secure-contexts'],
+  ['html', 'dom'],
+  idl_array => {
+    if (self.Window) {
+      idl_array.add_objects({ Window: ['self'] });
+    } else {
+      idl_array.add_objects({ WorkerGlobalScope: ['self'] });
+    }
+  }
+);
