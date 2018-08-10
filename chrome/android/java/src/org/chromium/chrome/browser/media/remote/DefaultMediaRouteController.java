454540e3a3730d70f6f60b65aea81df3be25075c
Sam Maier <smaier@chromium.org>
2018-08-10 22:24:21 +0000

    Reland "Android: moving MediaUrlResolver tasks to thread pool"
    This reverts commit 108f9e6cb07bdf3327b8b62e3ad43703b854662d.
    
    Reason for revert: Just re landing now that gerrit is OK
    
    Original change's description:
    > Revert "Android: moving MediaUrlResolver tasks to thread pool"
    > 
    > This reverts commit d20f6cb34484fbd5e7ca2c651974d71c62e2f4d0.
    > 
    > Reason for revert:Due to Gerrit outage http://crbug.com/872722, we are reverting this CL. Please, re-land it after all clear is given. If you have questions, please ask on the bug. Sorry for the inconvenience.
    > 
    > Original change's description:
    > > Android: moving MediaUrlResolver tasks to thread pool
    > > 
    > > Currently, AsyncTask.execute() defaults to the SERIAL_EXECUTOR. This
    > > exector is good for preventing concurrency errors since it guarantees
    > > serial execution, but bad for performance since the entire app shares
    > > this single queue.
    > > 
    > > It looks like this callsite can use the THREAD_POOL_EXECUTOR instead,
    > > since this use doesn't appear to rely on the concurrency guarantees that
    > > SERIAL_EXECUTOR provides.
    > > 
    > > TBR= test file
    > > Bug: 869907
    > > Change-Id: I5770f3f0211d2c10b28f2290973bbfd57bee53f0
    > > Reviewed-on: https://chromium-review.googlesource.com/1159236
    > > Commit-Queue: Sam Maier <smaier@chromium.org>
    > > Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    > 
    > TBR=mlamouri@chromium.org,smaier@chromium.org
    > 
    > Change-Id: I5cd823a033bb6443007734d4faf0e1ddf98de648
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 869907
    > Reviewed-on: https://chromium-review.googlesource.com/1169854
    > Reviewed-by: Andrii Shyshkalov <tandrii@chromium.org>
    
    TBR=mlamouri@chromium.org,tandrii@chromium.org,smaier@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 869907
    Change-Id: I31b02f5b543f2fdb8b0b089f241d79ceb720a626
    Reviewed-on: https://chromium-review.googlesource.com/1171702
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582358}

--- chrome/android/java/src/org/chromium/chrome/browser/media/remote/DefaultMediaRouteController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/media/remote/DefaultMediaRouteController.java
@@ -25,6 +25,7 @@ import com.google.android.gms.cast.CastMediaControlIntent;
 
 import org.chromium.base.ApplicationState;
 import org.chromium.base.ApplicationStatus;
+import org.chromium.base.AsyncTask;
 import org.chromium.base.Log;
 import org.chromium.base.annotations.RemovableInRelease;
 import org.chromium.base.annotations.UsedByReflection;
@@ -811,7 +812,7 @@ public class DefaultMediaRouteController extends AbstractMediaRouteController {
             public void deliverResult(Uri uri, boolean playable) {
                 callback.onResult(playable, uri.toString(), frameUrl);
             }
-        }, userAgent).execute();
+        }, userAgent).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
     }
 
     @Override
