2e503c4a7f05205d7a85bcf5c05fc59c33385737
yoshiki iguchi <yoshiki@chromium.org>
2018-08-10 20:49:56 +0000

    Create a separate flag for inline reply on lock screen
    Inline reply on lock screen is not launched at the same time as
    the lock screen notifications. So this CL creates a separate feature
    flag for inline reply.
    
    Bug: b/111391270
    
    Change-Id: I585beba17b3a491ce495f1d6c35ce46b2a6b6686
    Reviewed-on: https://chromium-review.googlesource.com/1166617
    Commit-Queue: Yoshiki Iguchi <yoshiki@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582325}

--- ash/public/cpp/ash_features.cc
+++ ash/public/cpp/ash_features.cc
@@ -29,6 +29,9 @@ const base::Feature kKeyboardShortcutViewerApp{
 const base::Feature kLockScreenNotifications{"LockScreenNotifications",
                                              base::FEATURE_DISABLED_BY_DEFAULT};
 
+const base::Feature kLockScreenInlineReply{"LockScreenInlineReply",
+                                           base::FEATURE_DISABLED_BY_DEFAULT};
+
 const base::Feature kLockScreenHideSensitiveNotificationsSupport{
     "LockScreenHideSensitiveNotificationsSupport",
     base::FEATURE_ENABLED_BY_DEFAULT};
@@ -66,6 +69,10 @@ bool IsKeyboardShortcutViewerAppEnabled() {
 
 bool IsLockScreenNotificationsEnabled() {
   return base::FeatureList::IsEnabled(kLockScreenNotifications);
+}
+
+bool IsLockScreenInlineReplyEnabled() {
+  return base::FeatureList::IsEnabled(kLockScreenInlineReply);
 }
 
 bool IsLockScreenHideSensitiveNotificationsSupported() {
