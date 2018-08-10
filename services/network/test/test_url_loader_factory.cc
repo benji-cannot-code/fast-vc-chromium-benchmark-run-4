c8576a31539f46c5ceaf50353268ff3d748b8890
Maks Orlovich <morlovich@chromium.org>
2018-08-10 13:04:45 +0000

    TestURLLoaderFactory: set decoded_body_length in advanced AddResponse.
    All other methods do the same, and so it was surprising when doing AddResponse
    with same arguments that would work in SimulateResponseForPendingRequest
    produced a fetch failure (especially when many layers were involved).
    
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Change-Id: I9faf75cb3ea415f2b04d87aa7f8fcd895bfc7438
    Reviewed-on: https://chromium-review.googlesource.com/1169312
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582128}

--- services/network/test/test_url_loader_factory.cc
+++ services/network/test/test_url_loader_factory.cc
@@ -43,6 +43,7 @@ void TestURLLoaderFactory::AddResponse(const GURL& url,
   response.head = head;
   response.content = content;
   response.status = status;
+  response.status.decoded_body_length = content.size();
   response.flags = flags;
   responses_[url] = response;
 
@@ -61,7 +62,6 @@ void TestURLLoaderFactory::AddResponse(const std::string& url,
   ResourceResponseHead head = CreateResourceResponseHead(http_status);
   head.mime_type = "text/html";
   URLLoaderCompletionStatus status;
-  status.decoded_body_length = content.size();
   AddResponse(GURL(url), head, content, status);
 }
 
