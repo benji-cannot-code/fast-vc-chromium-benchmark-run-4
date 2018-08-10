88c92e0a239ea27122a80c9fde7f2c755c026191
Morten Stenshorne <mstensho@chromium.org>
2018-08-10 17:21:32 +0000

    [LayoutNG] Flip for writing mode when calculating inline bounding box.
    LayoutInline::LinesVisualOverflowBoundingBox() is expected to return
    coordinates with a logical block coordinate. So we need to flip for
    writing-mode vertical-rl.
    
    Discovered while investigating why
    wpt/css/css-position/position-sticky-writing-modes.html fails. It had
    some weird repaint issues (that I discovered when picking the test
    apart) that this CL fixes. That's not enough to fix the test, though.
    
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng
    Change-Id: I9a8930cc6de01991592282491677327e3406acfb
    Reviewed-on: https://chromium-review.googlesource.com/1170835
    Reviewed-by: Aleks Totic <atotic@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582222}

--- third_party/blink/renderer/core/layout/layout_inline.cc
+++ third_party/blink/renderer/core/layout/layout_inline.cc
@@ -1178,7 +1178,10 @@ LayoutRect LayoutInline::LinesVisualOverflowBoundingBox() const {
       child_rect.offset += child.offset_to_container_box;
       result.Unite(child_rect);
     }
-    return result.ToLayoutRect();
+    LayoutRect rect = result.ToLayoutRect();
+    if (HasFlippedBlocksWritingMode())
+      ContainingBlock()->FlipForWritingMode(rect);
+    return rect;
   }
 
   if (!AlwaysCreateLineBoxes())
