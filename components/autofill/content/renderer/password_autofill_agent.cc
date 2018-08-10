805525802da7a37268d577ef4719dc25033e29cd
Christos Froussios <cfroussios@chromium.org>
2018-08-10 12:02:58 +0000

    [Password Manager] Split AUTOFILLED field flag
    The flag currently indicates that the field was autofilled, regardless
    of the trigger for autofilling.
    
    We will now identify two triggers separately.
    1. AUTOFILLED_ON_USER_TRIGGER. This refers to Fill On Account Select in
    the context of the password manager. It also the current default
    behaviour for Autofill.
    2. AUTOFILLED_ON_PAGELOAD. Password manager determined that a password
    form is a login form and filled the credentials as soon as the page was
    loaded.
    
    Any conditions which took AUTOFILLED into account are made to work with
    either of the new flags.
    
    Bug: 867348
    Change-Id: I2eb5fea7219ce784822174de24e76b3e1e4f1f15
    Reviewed-on: https://chromium-review.googlesource.com/1169060
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Commit-Queue: Christos Froussios <cfroussios@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582121}

--- components/autofill/content/renderer/password_autofill_agent.cc
+++ components/autofill/content/renderer/password_autofill_agent.cc
@@ -898,9 +898,9 @@ void PasswordAutofillAgent::FillField(WebInputElement* input,
   DCHECK(!input->IsNull());
   input->SetAutofillValue(WebString::FromUTF16(credential));
   input->SetAutofillState(WebAutofillState::kAutofilled);
-  UpdateFieldValueAndPropertiesMaskMap(*input, &credential,
-                                       FieldPropertiesFlags::AUTOFILLED,
-                                       &field_value_and_properties_map_);
+  UpdateFieldValueAndPropertiesMaskMap(
+      *input, &credential, FieldPropertiesFlags::AUTOFILLED_ON_USER_TRIGGER,
+      &field_value_and_properties_map_);
 }
 
 void PasswordAutofillAgent::FillPasswordFieldAndSave(
@@ -1877,9 +1877,10 @@ bool PasswordAutofillAgent::FillUserNameAndPassword(
       }
     }
 
-    UpdateFieldValueAndPropertiesMaskMap(*username_element, &username,
-                                         FieldPropertiesFlags::AUTOFILLED,
-                                         field_value_and_properties_map);
+    UpdateFieldValueAndPropertiesMaskMap(
+        *username_element, &username,
+        FieldPropertiesFlags::AUTOFILLED_ON_PAGELOAD,
+        field_value_and_properties_map);
     username_element->SetAutofillState(WebAutofillState::kAutofilled);
     if (logger)
       logger->LogElementName(Logger::STRING_USERNAME_FILLED, *username_element);
@@ -1890,9 +1891,10 @@ bool PasswordAutofillAgent::FillUserNameAndPassword(
   // user is intentionally interacting with the page.
   if (password_element->Value().Utf16() != password)
     password_element->SetSuggestedValue(WebString::FromUTF16(password));
-  UpdateFieldValueAndPropertiesMaskMap(*password_element, &password,
-                                       FieldPropertiesFlags::AUTOFILLED,
-                                       field_value_and_properties_map);
+  UpdateFieldValueAndPropertiesMaskMap(
+      *password_element, &password,
+      FieldPropertiesFlags::AUTOFILLED_ON_PAGELOAD,
+      field_value_and_properties_map);
   ProvisionallySavePassword(password_element->Form(), *password_element,
                             RESTRICTION_NONE);
   gatekeeper_.RegisterElement(password_element);
