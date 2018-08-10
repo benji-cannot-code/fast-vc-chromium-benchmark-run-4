4ee20e91d4671a687c2ea88e7e766459cd826d67
Joe Mason <joenotcharles@chromium.org>
2018-08-10 19:24:16 +0000

    Re-enable pretty-printed ExecutionMode test names
    Fix ODR violation with ExecutionMode by declaring the streaming operator in the
    same header as the type, and add back the GetParamNameForTest calls that use
    that operator.
    
    Also add a streaming operator for ChromePromptValue so that all types declared
    in constants.h can be pretty-printed.
    
    R=proberge
    
    Bug: 872748
    Change-Id: Idfe3f6044f97b003f17d83c6985d442f9b4103f6
    Reviewed-on: https://chromium-review.googlesource.com/1170852
    Commit-Queue: Joe Mason <joenotcharles@chromium.org>
    Reviewed-by: proberge <proberge@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582285}

--- chrome/chrome_cleaner/test/test_name_helper.h
+++ chrome/chrome_cleaner/test/test_name_helper.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/strings/string_util.h"
-#include "components/chrome_cleaner/public/constants/constants.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace chrome_cleaner {
@@ -46,8 +45,6 @@ struct GetParamNameForTest {
     return param_name;
   }
 };
-
-std::ostream& operator<<(std::ostream& stream, ExecutionMode mode);
 
 }  // namespace chrome_cleaner
 
--- components/chrome_cleaner/public/constants/constants.h
+++ components/chrome_cleaner/public/constants/constants.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_CHROME_CLEANER_PUBLIC_CONSTANTS_CONSTANTS_H_
 #define COMPONENTS_CHROME_CLEANER_PUBLIC_CONSTANTS_CONSTANTS_H_
 
+#include <ostream>
+
 #include "components/chrome_cleaner/public/constants/result_codes.h"
 
 // Constants shared by the Chromium and the Chrome Cleanaup tool repos.
@@ -149,6 +151,15 @@ enum class ExecutionMode {
   // Auxiliary enumerator for range checking.
   kNumValues,
 };
+
+// Pretty printers for gtest. Declared here to avoid ODR violations, but only
+// defined in constants_test_support.cc to prevent their use outside tests. (See
+// https://groups.google.com/a/chromium.org/d/msg/chromium-dev/i_wOTsE5Z6g/jhtqTY6fCwAJ
+// for explanation.)
+
+std::ostream& operator<<(std::ostream& stream, ChromePromptValue mode);
+
+std::ostream& operator<<(std::ostream& stream, ExecutionMode mode);
 
 }  // namespace chrome_cleaner
 
