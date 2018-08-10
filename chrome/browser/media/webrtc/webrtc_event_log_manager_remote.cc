cdd6fd0c43896cfb00f4216f8e592952ba815e42
Elad Alon <eladalon@chromium.org>
2018-08-10 14:57:10 +0000

    Reland "Support dynamic refresh of WebRtcEventLogCollectionAllowed policy"
    This is a reland of 7dae0f6307b6ffc38b69faa9340d2ef7d298aabd
    
    The original CL caused flakiness because, in production code,
    WebRtcEventLogManager is torn down during Chrome shutdown, after
    all threads other the the UI thread have quit. For unit tests, this
    was not the case, and so the tests hit some use-after-destruction.
    This was fixed by making the tests stop the non-UI threads before
    tearing down WebRtcEventLogManager.
    
    Original change's description:
    > Support dynamic refresh of WebRtcEventLogCollectionAllowed policy
    >
    > Add suport for dynamic refresh of WebRtcEventLogCollectionAllowed:
    > 1. If the policy was disabled before, and becomes enabled,
    >    subsequent calls to StartRemoteLogging() will succeed (barring
    >    unrelated issues), even for peer connections created while the
    >    policy was disabled.
    > 2. If the policy was enabled before, and becomes disabled:
    >    a. Peer connections associated with the profile reject
    >       calls to StartRemoteLogging().
    >    b. Active logs (associated with the profile) are stopped,
    >       and those log files deleted.
    >    c. Pending log files (associated with the profile) are deleted.
    >    d. If a log file associated with the relevant profile is
    >       currently being uploaded, the upload will be cancelled,
    >       and the file deleted.
    >
    > Bug: 775415
    > Change-Id: I70a02ff04eded2926e56fa8e368715ec1ff7c34f
    > Reviewed-on: https://chromium-review.googlesource.com/1162168
    > Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    > Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Commit-Queue: Elad Alon <eladalon@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#581167}
    
    TBR=guidou@chromium.org
    
    Bug: 775415
    Change-Id: Ie6070a8843012580c9a6c069f65c01b8d63525ff
    Reviewed-on: https://chromium-review.googlesource.com/1168962
    Commit-Queue: Elad Alon <eladalon@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582162}

--- chrome/browser/media/webrtc/webrtc_event_log_manager_remote.cc
+++ chrome/browser/media/webrtc/webrtc_event_log_manager_remote.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
-#include "base/threading/sequenced_task_runner_handle.h"
 #include "base/unguessable_token.h"
 #include "chrome/common/chrome_switches.h"
 #include "content/public/browser/browser_thread.h"
@@ -218,9 +217,9 @@ void WebRtcRemoteEventLogManager::EnableForBrowserContext(
     return;
   }
 
-  AddPendingLogs(browser_context_id, remote_bound_logs_dir);
-
   enabled_browser_contexts_.insert(browser_context_id);
+
+  AddPendingLogs(browser_context_id, remote_bound_logs_dir);
 
   if (!proactive_prune_scheduling_delta_.is_zero() &&
       !proactive_prune_scheduling_started_) {
@@ -229,7 +228,6 @@ void WebRtcRemoteEventLogManager::EnableForBrowserContext(
   }
 }
 
-// TODO(crbug.com/775415): Add unit tests.
 void WebRtcRemoteEventLogManager::DisableForBrowserContext(
     BrowserContextId browser_context_id) {
   DCHECK(task_runner_->RunsTasksInCurrentSequence());
@@ -241,8 +239,14 @@ void WebRtcRemoteEventLogManager::DisableForBrowserContext(
   enabled_browser_contexts_.erase(browser_context_id);
 
 #if DCHECK_IS_ON()
-  // All of the RPHs associated with this BrowserContext must already have
-  // exited, which should have implicitly stopped all active logs.
+  // DisableForBrowserContext() is called in one of two cases:
+  // 1. If Chrome is shutting down. In that case, all the RPHs associated with
+  //    this BrowserContext must already have exited, which should have
+  //    implicitly stopped all active logs.
+  // 2. Remote-bound logging is no longer allowed for this BrowserContext.
+  //    In that case, some peer connections associated with this BrowserContext
+  //    might still be active, or become active at a later time, but all
+  //    logs must have already been stopped.
   auto pred = [browser_context_id](decltype(active_logs_)::value_type& log) {
     return log.first.browser_context_id == browser_context_id;
   };
@@ -250,7 +254,6 @@ void WebRtcRemoteEventLogManager::DisableForBrowserContext(
 #endif
 
   // Pending logs for this BrowserContext are no longer eligible for upload.
-  // (Active uploads, if any, are not affected.)
   for (auto it = pending_logs_.begin(); it != pending_logs_.end();) {
     if (it->browser_context_id == browser_context_id) {
       it = pending_logs_.erase(it);
@@ -258,6 +261,9 @@ void WebRtcRemoteEventLogManager::DisableForBrowserContext(
       ++it;
     }
   }
+
+  // Active uploads of logs associated with this BrowserContext must be stopped.
+  MaybeCancelUpload(base::Time::Min(), base::Time::Max(), browser_context_id);
 
   // Active logs may have been removed, which could remove upload suppression,
   // or pending logs which were about to be uploaded may have been removed,
@@ -418,6 +424,7 @@ void WebRtcRemoteEventLogManager::RenderProcessHostExitedDestroyed(
 
 void WebRtcRemoteEventLogManager::OnConnectionChanged(
     network::mojom::ConnectionType type) {
+  DCHECK(task_runner_->RunsTasksInCurrentSequence());
   // Even if switching from WiFi to Ethernet, or between to WiFi connections,
   // reset the timer (if running) until an upload is permissible due to stable
   // upload-supporting conditions.
@@ -474,6 +481,7 @@ bool WebRtcRemoteEventLogManager::AreLogParametersValid(
 
 bool WebRtcRemoteEventLogManager::BrowserContextEnabled(
     BrowserContextId browser_context_id) const {
+  DCHECK(task_runner_->RunsTasksInCurrentSequence());
   const auto it = enabled_browser_contexts_.find(browser_context_id);
   return it != enabled_browser_contexts_.cend();
 }
@@ -630,7 +638,7 @@ void WebRtcRemoteEventLogManager::RecurringPendingLogsPrune() {
 
   // |this| is only destroyed (on the UI thread) after |task_runner_| stops,
   // so both base::Unretained(this) is safe.
-  base::SequencedTaskRunnerHandle::Get()->PostDelayedTask(
+  task_runner_->PostDelayedTask(
       FROM_HERE,
       base::BindOnce(&WebRtcRemoteEventLogManager::RecurringPendingLogsPrune,
                      base::Unretained(this)),
@@ -667,6 +675,7 @@ void WebRtcRemoteEventLogManager::MaybeCancelActiveLogs(
     const base::Time& delete_begin,
     const base::Time& delete_end,
     BrowserContextId browser_context_id) {
+  DCHECK(task_runner_->RunsTasksInCurrentSequence());
   for (auto it = active_logs_.begin(); it != active_logs_.end();) {
     // Since the file is active, assume it's still being modified.
     if (LogFileMatchesFilter(it->first.browser_context_id, base::Time::Now(),
@@ -682,6 +691,7 @@ void WebRtcRemoteEventLogManager::MaybeCancelUpload(
     const base::Time& delete_begin,
     const base::Time& delete_end,
     base::Optional<BrowserContextId> browser_context_id) {
+  DCHECK(task_runner_->RunsTasksInCurrentSequence());
   if (uploader_) {
     const WebRtcLogFileInfo& info = uploader_->GetWebRtcLogFileInfo();
     if (LogFileMatchesFilter(info.browser_context_id, info.last_modified,
@@ -705,6 +715,7 @@ bool WebRtcRemoteEventLogManager::LogFileMatchesFilter(
     base::Optional<BrowserContextId> filter_browser_context_id,
     const base::Time& filter_range_begin,
     const base::Time& filter_range_end) const {
+  DCHECK(task_runner_->RunsTasksInCurrentSequence());
   if (filter_browser_context_id &&
       *filter_browser_context_id != log_browser_context_id) {
     return false;
@@ -743,6 +754,7 @@ bool WebRtcRemoteEventLogManager::UploadSuppressed() const {
 }
 
 bool WebRtcRemoteEventLogManager::UploadConditionsHold() const {
+  DCHECK(task_runner_->RunsTasksInCurrentSequence());
   return !uploader_ && !pending_logs_.empty() && !UploadSuppressed() &&
          uploading_supported_for_connection_type_;
 }
