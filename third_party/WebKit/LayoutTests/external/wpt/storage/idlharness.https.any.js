9a85a2617c3364d0eb151128552230fe6e81d267
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-08-10 14:29:29 +0000

    Import wpt@56d7c6101f0ae73fd87d555623dd2c608d4cfc8c
    Using wpt-import in Chromium f2f75f326f9fb7b36d8829440104d599474f3bda.
    With Chromium commits locally applied on WPT:
    7dad0f1723 "Added new Sec-Metadata Web Platform Tests."
    
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/22583
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    foolip@chromium.org:
      external/wpt/compat
      external/wpt/fullscreen
    jsbell@chromium.org:
      external/wpt/storage
    kozyatinskiy@chromium.org:
      external/wpt/console
    
    NOAUTOREVERT=true
    TBR=lukebjerring
    
    No-Export: true
    Change-Id: I5abd8b46c4e13639f2caeceee1d6b7d2d18c8636
    Reviewed-on: https://chromium-review.googlesource.com/1170581
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582151}

---
+++ third_party/WebKit/LayoutTests/external/wpt/storage/idlharness.https.any.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/resources/WebIDLParser.js
+// META: script=/resources/idlharness.js
+
+'use strict';
+
+idl_test(
+  ['storage'],
+  ['html'],
+  idl_array => {
+    idl_array.add_objects({ StorageManager: ['navigator.storage'] });
+    if (self.Window) {
+      idl_array.add_objects({ Navigator: ['navigator'] });
+    } else {
+      idl_array.add_objects({ WorkerNavigator: ['navigator'] });
+    }
+  }
+);
---
+++ third_party/WebKit/LayoutTests/external/wpt/url/idlharness.any.js
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/resources/WebIDLParser.js
+// META: script=/resources/idlharness.js
+
+idl_test(
+  ['url'],
+  [], // no deps
+  idl_array => {
+    idl_array.add_objects({
+      URL: ['new URL("http://foo")'],
+      URLSearchParams: ['new URLSearchParams("hi=there&thank=you")']
+    });
+  }
+);
