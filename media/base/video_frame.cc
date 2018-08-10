04d0044312fcce89cb9ac4dfbf723e24b55e7089
Miguel Casas <mcasas@chromium.org>
2018-08-10 13:34:58 +0000

    GMBVFPool: do not count VFs backed by DmaBufs as unsupported
    GMBVFPool counts as unsupported those VideoFrames backed by DmaBufs,
    because it only checks HasTextures().  This CL adds a new method
    VideoFrame::HasDmaBufs() and uses it to avoid logging those VFrames.
    
    V4L2* creates these VideoFrames calling WrapExternalDmabufs().
    
    Bug: 864145
    Change-Id: I340f1f8c2cbbb53de0bed13cc69f868c63b097bd
    Reviewed-on: https://chromium-review.googlesource.com/1169823
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582135}

--- media/base/video_frame.cc
+++ media/base/video_frame.cc
@@ -414,6 +414,7 @@ scoped_refptr<VideoFrame> VideoFrame::WrapExternalDmabufs(
          sizeof(frame->mailbox_holders_));
   frame->mailbox_holders_release_cb_ = ReleaseMailboxCB();
   frame->dmabuf_fds_ = std::move(dmabuf_fds);
+  DCHECK(frame->HasDmaBufs());
 
   return frame;
 }
@@ -863,6 +864,10 @@ std::vector<int> VideoFrame::DmabufFds() const {
     ret.emplace_back(fd.get());
 
   return ret;
+}
+
+bool VideoFrame::HasDmaBufs() const {
+  return !dmabuf_fds_.empty();
 }
 #endif
 
