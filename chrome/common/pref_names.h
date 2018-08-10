e2dfda909766dcba6a1c0e8f31c478bb0c2375d9
Nick Harper <nharper@chromium.org>
2018-08-10 19:57:11 +0000

    Add enterpise policy to control SSLConfig.client_cert_pooling_policy
    Bug: 855690
    Change-Id: I19d0b1ca0770c9016b0197bf435cf47affe49a94
    Reviewed-on: https://chromium-review.googlesource.com/1159840
    Commit-Queue: Nick Harper <nharper@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582300}

--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -689,6 +689,7 @@ extern const char kSSLVersionMin[];
 extern const char kSSLVersionMax[];
 extern const char kTLS13Variant[];
 extern const char kCipherSuiteBlacklist[];
+extern const char kH2ClientCertCoalescingHosts[];
 
 extern const char kBuiltInDnsClientEnabled[];
 extern const char kDnsOverHttpsServers[];
