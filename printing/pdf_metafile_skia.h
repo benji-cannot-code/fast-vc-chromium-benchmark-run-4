edc6478135a4741f69147b8043492a4bda1e463a
Wei Li <weili@chromium.org>
2018-08-10 02:52:37 +0000

    Add a test for nested oopif printing
    When an out-of-process subframe itself also has out-of-process
    subframe(s), we need to be able to embed such contents too. This
    CL adds a test to make sure our code works in such cases.
    
    BUG=853942
    
    Change-Id: I09d1fab4f10e299d3a7fb7f164bbbcfade3eebb5
    Reviewed-on: https://chromium-review.googlesource.com/1166032
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582027}

--- printing/pdf_metafile_skia.h
+++ printing/pdf_metafile_skia.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/gtest_prod_util.h"
 #include "base/macros.h"
 #include "build/build_config.h"
 #include "cc/paint/paint_canvas.h"
@@ -97,6 +98,15 @@ class PRINTING_EXPORT PdfMetafileSkia : public Metafile {
   const ContentToProxyIdMap& GetSubframeContentInfo() const;
 
  private:
+  FRIEND_TEST_ALL_PREFIXES(PdfMetafileSkiaTest, TestFrameContent);
+
+  // The following three functions are used for tests only.
+  void AppendPage(const SkSize& page_size, sk_sp<cc::PaintRecord> record);
+  void AppendSubframeInfo(uint32_t content_id,
+                          int proxy_id,
+                          sk_sp<SkPicture> subframe_pic_holder);
+  SkStreamAsset* GetPdfData() const;
+
   // Callback function used during page content drawing to replace a custom
   // data holder with corresponding place holder SkPicture.
   void CustomDataToSkPictureCallback(SkCanvas* canvas, uint32_t content_id);
