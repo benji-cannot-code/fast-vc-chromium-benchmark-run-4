306e9ab78f899817e9229e39861539d090e33113
Jinsuk Kim <jinsukkim@chromium.org>
2018-08-10 01:56:10 +0000

    Android: Fix Find in Page timing bug
    There is a timing-related bug in Find in page menu on NTP, which happens
    when the web contents was already switched to one for NTP but the menu for
    the action remained available. The actual find toolbar is designed not
    to appear by checking if the current web contents is for NTP or not.
    Previously, null check against |tab.getContentViewCore| was enough
    but after https://crrev.com/c/989515, |tab.isNativePage| should be
    called as well as |tab.getWebContents|. This CL adds the missing check.
    
    Bug: 872650
    Change-Id: Ibef4d335cf2dc2a4ceab5b507eb9f08c47e5e946
    Reviewed-on: https://chromium-review.googlesource.com/1170142
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582011}

--- chrome/android/java/src/org/chromium/chrome/browser/widget/findinpage/FindToolbar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/findinpage/FindToolbar.java
@@ -550,7 +550,8 @@ public class FindToolbar extends LinearLayout
      */
     protected boolean isWebContentAvailable() {
         Tab currentTab = mTabModelSelector.getCurrentTab();
-        return currentTab != null && currentTab.getWebContents() != null;
+        return currentTab != null && currentTab.getWebContents() != null
+                && !currentTab.isNativePage();
     }
 
     /**
