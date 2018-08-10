ef18e30db13afb0bf0e86b1f026dccc3881c943f
Yiming Zhou <uwyiming@google.com>
2018-08-10 22:35:36 +0000

    Add iframe support to the Captured Sites Automation Framework.
    This change allows the Captured Sites Automation Framework to replay
    interactions on iframes. As part of this change, I switched the
    automation framework to trigger the autofill popup by clicking on an
    input field instead of pressing the 'DOWN' key, which does not work in
    automation on some iframes.
    
    Also included in this change is a fix I made to address the issue that
    calling 'ASSERT' inside a test function does not abort the test.
    
    Change-Id: I15b30f878f04cb3ee2935879c2da4c474442ab27
    Reviewed-on: https://chromium-review.googlesource.com/1161603
    Commit-Queue: Yiming Zhou <uwyiming@google.com>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582360}

--- chrome/browser/autofill/autofill_uitest.cc
+++ chrome/browser/autofill/autofill_uitest.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/personal_data_manager.h"
 #include "components/autofill/core/browser/personal_data_manager_observer.h"
 #include "content/public/browser/render_view_host.h"
+#include "content/public/browser/render_widget_host_view.h"
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/test_utils.h"
 #include "ui/events/base_event_utils.h"
@@ -90,10 +91,11 @@ void AutofillUiTest::TearDownOnMainThread() {
   test::ReenableSystemServices();
 }
 
-void AutofillUiTest::SendKeyToPage(ui::DomKey key) {
+void AutofillUiTest::SendKeyToPage(content::WebContents* web_contents,
+                                   const ui::DomKey key) {
   ui::KeyboardCode key_code = ui::NonPrintableDomKeyToKeyboardCode(key);
   ui::DomCode code = ui::UsLayoutKeyboardCodeToDomCode(key_code);
-  content::SimulateKeyPress(GetWebContents(), key, code, key_code, false, false,
+  content::SimulateKeyPress(web_contents, key, code, key_code, false, false,
                             false, false);
 }
 
@@ -116,10 +118,12 @@ void AutofillUiTest::SendKeyToPageAndWait(
   test_delegate()->Wait(std::move(expected_events));
 }
 
-void AutofillUiTest::SendKeyToPopup(ui::DomKey key) {
+void AutofillUiTest::SendKeyToPopup(content::RenderFrameHost* render_frame_host,
+                                    const ui::DomKey key) {
   ui::KeyboardCode key_code = ui::NonPrintableDomKeyToKeyboardCode(key);
   ui::DomCode code = ui::UsLayoutKeyboardCodeToDomCode(key_code);
-  content::RenderWidgetHost* widget = GetRenderViewHost()->GetWidget();
+  content::RenderWidgetHost* widget =
+      render_frame_host->GetView()->GetRenderWidgetHost();
 
   // Route popup-targeted key presses via the render view host.
   content::NativeWebKeyboardEvent event(blink::WebKeyboardEvent::kRawKeyDown,
