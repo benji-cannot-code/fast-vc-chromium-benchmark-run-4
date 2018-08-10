7eb1ae32e28154c6342ebabc0ca38c63b1a69ff0
Min Chen <minch@google.com>
2018-08-10 00:11:27 +0000

    Put the new selector item as the first one in overview grid.
    Bug: 872052
    Test: SplitViewTabDraggingTest.ShowNewWindowItemWhenDragStarts
    Change-Id: Ib7a563d350fb5d789495518b414f7a319bc9063b
    Reviewed-on: https://chromium-review.googlesource.com/1168592
    Commit-Queue: Min Chen <minch@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581969}

--- ash/wm/splitview/split_view_controller_unittest.cc
+++ ash/wm/splitview/split_view_controller_unittest.cc
@@ -2217,7 +2217,8 @@ TEST_F(SplitViewTabDraggingTest, ShowNewWindowItemWhenDragStarts) {
   EXPECT_EQ(split_view_controller()->state(),
             SplitViewController::RIGHT_SNAPPED);
 
-  // Test that the new window item widget shows up.
+  // Test that the new window item widget shows up as the first one of the
+  // windows in the grid.
   WindowSelector* window_selector =
       Shell::Get()->window_selector_controller()->window_selector();
   WindowGrid* current_grid =
@@ -2231,6 +2232,7 @@ TEST_F(SplitViewTabDraggingTest, ShowNewWindowItemWhenDragStarts) {
       current_grid->GetWindowSelectorItemContaining(
           new_selector_widget->GetNativeWindow());
   ASSERT_TRUE(new_selector_item);
+  EXPECT_EQ(new_selector_item, current_grid->window_list().front().get());
   const gfx::Rect new_selector_bounds = new_selector_item->target_bounds();
   DragWindowTo(resizer.get(), new_selector_bounds.CenterPoint());
   CompleteDrag(std::move(resizer));
@@ -2238,8 +2240,12 @@ TEST_F(SplitViewTabDraggingTest, ShowNewWindowItemWhenDragStarts) {
   EXPECT_TRUE(Shell::Get()->window_selector_controller()->IsSelecting());
   EXPECT_EQ(split_view_controller()->state(),
             SplitViewController::RIGHT_SNAPPED);
-  // Test that the dragged window has been added to the overview mode.
+  // Test that the dragged window has been added to the overview mode, and it is
+  // added at the front of the grid.
   EXPECT_EQ(current_grid->window_list().size(), 2u);
+  WindowSelectorItem* first_selector_item =
+      current_grid->GetWindowSelectorItemContaining(window1.get());
+  EXPECT_EQ(first_selector_item, current_grid->window_list().front().get());
   EXPECT_TRUE(window_selector->IsWindowInOverview(window1.get()));
   EXPECT_TRUE(window_selector->IsWindowInOverview(window3.get()));
   // Test that the new window item widget has been destroyed.
