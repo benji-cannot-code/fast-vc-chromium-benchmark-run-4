f422da32d0cf13978f6aa0152fdd71b8b0276beb
Alexey Kozyatinskiy <kozyatinskiy@chromium.org>
2018-08-10 04:00:21 +0000

    [DevTools] migrate CallStackSidebarPane to live locations
    New CallStackSidebarPane tries to finally split model and view by using
    live locations more.
    With new implementation each item maintains own state based on live
    location updates and schedule updates of correspond list item.
    
    drive-by: removed BlackboxManager.isRawLocationBlackboxed method.
    
    R=lushnikov@chromium.org
    
    Bug: none
    Change-Id: I19adde997652754e5f90869ee41ab41899bf8ec4
    Reviewed-on: https://chromium-review.googlesource.com/1168384
    Commit-Queue: Aleksey Kozyatinskiy <kozyatinskiy@chromium.org>
    Reviewed-by: Andrey Lushnikov <lushnikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582044}

--- third_party/blink/renderer/devtools/front_end/components/JSPresentationUtils.js
+++ third_party/blink/renderer/devtools/front_end/components/JSPresentationUtils.js
@@ -44,7 +44,6 @@ Components.JSPresentationUtils.buildStackTracePreviewContents = function(
   element.style.display = 'inline-block';
   const shadowRoot = UI.createShadowRootWithCoreStyles(element, 'components/jsUtils.css');
   const contentElement = shadowRoot.createChild('table', 'stack-preview-container');
-  const debuggerModel = target ? target.model(SDK.DebuggerModel) : null;
   let totalHiddenCallFramesCount = 0;
 
   /**
@@ -60,15 +59,11 @@ Components.JSPresentationUtils.buildStackTracePreviewContents = function(
       const link = linkifier.maybeLinkifyConsoleCallFrame(target, stackFrame);
       if (link) {
         link.addEventListener('contextmenu', populateContextMenu.bind(null, link));
-        if (debuggerModel) {
-          const location = debuggerModel.createRawLocationByScriptId(
-              stackFrame.scriptId, stackFrame.lineNumber, stackFrame.columnNumber);
-          if (location && Bindings.blackboxManager.isBlackboxedRawLocation(location)) {
-            row.classList.add('blackboxed');
-            ++hiddenCallFrames;
-          }
+        const uiLocation = Components.Linkifier.uiLocation(link);
+        if (uiLocation && Bindings.blackboxManager.isBlackboxedUISourceCode(uiLocation.uiSourceCode)) {
+          row.classList.add('blackboxed');
+          ++hiddenCallFrames;
         }
-
         row.createChild('td').textContent = ' @ ';
         row.createChild('td').appendChild(link);
       }
