b5580d0d3fa9af93cbcc50209d0411939cee6846
Miguel Casas <mcasas@chromium.org>
2018-08-10 00:30:26 +0000

    RELAND: Simplify OffscreenCanvasResourceProvider
    This is just a reland of crrev.com/c/1167474; it got reverted
    due to infra issues (Gerit outage), see crbug.com/872722 and/or
    https://groups.google.com/a/chromium.org/forum/#!topic/chromium-dev/YnUEOtzCrzs
    
    TBR=fserb@chromium.org
    
    Original CL description ------------------------------------------------
    
    This CL simplifies OffscreenCanvasResourceProvider:
    
    - Removes the unused member variables |width_|, |height_|
     and |frame_provider_|.
    - Removes superfluous method Reshape().
    - Forward declares a few data types ISO including files.
    - Uses early return for easier indenting.
    
    This is inspired by junov@'s crrev.com/c/1103011 (in the follow
    up CL crrev.com/c/1167907 I'll fold OffscreenCanvasResourceProvider
    into CanvasResourceDispatcher, but it's easier to review in two
    steps).
    
    Bug: 839970
    Change-Id: Ic4d3a6002cec32c01e8914a11d3c392981dae8b5
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Reviewed-on: https://chromium-review.googlesource.com/1167474
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1169829
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581981}

--- third_party/blink/renderer/platform/graphics/offscreen_canvas_placeholder.cc
+++ third_party/blink/renderer/platform/graphics/offscreen_canvas_placeholder.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/graphics/canvas_resource_dispatcher.h"
 #include "third_party/blink/renderer/platform/web_task_runner.h"
 #include "third_party/blink/renderer/platform/wtf/hash_map.h"
+#include "third_party/blink/renderer/platform/wtf/wtf.h"
 
 namespace {
 
