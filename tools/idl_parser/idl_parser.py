a93d934d6f0a9ebf777478320e3cc81b27b9f8ad
Hitoshi Yoshida <peria@chromium.org>
2018-08-10 01:53:24 +0000

    IDL parser: Drop accepting extended attributes on enum values
    For now, IDL parser allows to put extended attributes on enum values,
    that is syntactically invalid in Web IDL spec [1].
    In Blink, other IDL compiler parts ignore them, and no IDL files have them.
    
    In order to avoid misleading developers, this CL makes IDL parser
    to disallow extended attributes on enum values.
    
    [1] https://heycam.github.io/webidl/#idl-enums
    
    
    Bug: 871867
    Change-Id: Ic57406ce3ff94a0c1a4cf38da12b217c05667eb0
    Reviewed-on: https://chromium-review.googlesource.com/1169482
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Hitoshi Yoshida <peria@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582009}

--- tools/idl_parser/idl_parser.py
+++ tools/idl_parser/idl_parser.py
@@ -465,9 +465,9 @@ class IDLParser(object):
     p[0] = self.BuildError(p, 'Enum')
 
   def p_EnumValueList(self, p):
-    """EnumValueList : ExtendedAttributeList string EnumValueListComma"""
-    enum = self.BuildNamed('EnumItem', p, 2, p[1])
-    p[0] = ListFromConcat(enum, p[3])
+    """EnumValueList : string EnumValueListComma"""
+    enum = self.BuildNamed('EnumItem', p, 1)
+    p[0] = ListFromConcat(enum, p[2])
 
   def p_EnumValueListComma(self, p):
     """EnumValueListComma : ',' EnumValueListString
@@ -476,11 +476,11 @@ class IDLParser(object):
       p[0] = p[2]
 
   def p_EnumValueListString(self, p):
-    """EnumValueListString : ExtendedAttributeList string EnumValueListComma
+    """EnumValueListString : string EnumValueListComma
                            |"""
     if len(p) > 1:
-      enum = self.BuildNamed('EnumItem', p, 2, p[1])
-      p[0] = ListFromConcat(enum, p[3])
+      enum = self.BuildNamed('EnumItem', p, 1)
+      p[0] = ListFromConcat(enum, p[2])
 
   def p_CallbackRest(self, p):
     """CallbackRest : identifier '=' ReturnType '(' ArgumentList ')' ';'"""
