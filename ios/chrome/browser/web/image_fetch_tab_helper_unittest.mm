117294ecf97c97c3f561d94a69392bce86b67237
Yi Su <mrsuyi@chromium.org>
2018-08-10 08:46:27 +0000

    Add timeout to ImageFetchTabHelper::GetImageData
    Add a timeout parameter for ImageFetchTabHelper::GetImageData in case the
    JavaScript does not response.
    
    Bug: 163201
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ibee9284d937f020ae8deed55eab9eb3aabdba5c1
    Reviewed-on: https://chromium-review.googlesource.com/1169168
    Commit-Queue: Yi Su <mrsuyi@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582092}

--- ios/chrome/browser/web/image_fetch_tab_helper_unittest.mm
+++ ios/chrome/browser/web/image_fetch_tab_helper_unittest.mm
@@ -60,6 +60,7 @@ TEST_F(ImageFetchTabHelperTest, GetImageDataJsSucceed) {
 
   image_fetch_tab_helper()->GetImageData(
       GURL("http://a.com/"),
+      base::TimeDelta::FromSeconds(kWaitForJSCompletionTimeout),
       base::BindOnce(&ImageFetchTabHelperTest::OnImageData,
                      base::Unretained(this)));
 
@@ -83,6 +84,7 @@ TEST_F(ImageFetchTabHelperTest, GetImageDataJsFail) {
 
   image_fetch_tab_helper()->GetImageData(
       GURL("http://a.com/"),
+      base::TimeDelta::FromSeconds(kWaitForJSCompletionTimeout),
       base::BindOnce(&ImageFetchTabHelperTest::OnImageData,
                      base::Unretained(this)));
 
@@ -94,10 +96,34 @@ TEST_F(ImageFetchTabHelperTest, GetImageDataJsFail) {
 }
 
 // Tests that ImageFetchTabHelper::GetImageData returns nullptr in callback when
+// JavaScript does not send a message back.
+TEST_F(ImageFetchTabHelperTest, GetImageDataJsTimeout) {
+  // Replaces __gCrWeb.imageFetch.getImageData with empty function to trigger a
+  // timeout.
+  id script_result = ExecuteJavaScript(
+      @"__gCrWeb.imageFetch = {}; __gCrWeb.imageFetch.getImageData = "
+      @"function(id, url){}; true;");
+  ASSERT_NSEQ(@YES, script_result);
+
+  image_fetch_tab_helper()->GetImageData(
+      GURL("http://a.com/"), base::TimeDelta::FromMilliseconds(100),
+      base::BindOnce(&ImageFetchTabHelperTest::OnImageData,
+                     base::Unretained(this)));
+
+  ASSERT_TRUE(WaitUntilConditionOrTimeout(kWaitForJSCompletionTimeout, ^{
+    base::RunLoop().RunUntilIdle();
+    return on_image_data_called_;
+  }));
+
+  EXPECT_FALSE(image_data_);
+}
+
+// Tests that ImageFetchTabHelper::GetImageData returns nullptr in callback when
 // WebState is destroyed.
 TEST_F(ImageFetchTabHelperTest, GetImageDataWebStateDestroy) {
   image_fetch_tab_helper()->GetImageData(
       GURL("http://a.com/"),
+      base::TimeDelta::FromSeconds(kWaitForJSCompletionTimeout),
       base::BindOnce(&ImageFetchTabHelperTest::OnImageData,
                      base::Unretained(this)));
 
@@ -115,6 +141,7 @@ TEST_F(ImageFetchTabHelperTest, GetImageDataWebStateDestroy) {
 TEST_F(ImageFetchTabHelperTest, GetImageDataWebStateNavigate) {
   image_fetch_tab_helper()->GetImageData(
       GURL("http://a.com/"),
+      base::TimeDelta::FromSeconds(kWaitForJSCompletionTimeout),
       base::BindOnce(&ImageFetchTabHelperTest::OnImageData,
                      base::Unretained(this)));
 
