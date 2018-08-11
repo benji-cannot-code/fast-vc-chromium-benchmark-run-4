fe873c035a4c986c5814ff86b53d0e1e27bfe3c9
Yafei Duan <romax@chromium.org>
2018-08-11 01:45:19 +0000

    [Offline Pages] Implement live page sharing.
    Implement live page sharing:
    - Adding a new temporary client namespace for the usage of live page
      sharing, which has a limit of one page per url and expiration period
      as one hour.
    - Adding saveAndSharePage in OfflinePageUtils, implementing the workflow
      of save page, get page and share page.
    
    Bug: 870895
    Change-Id: Iab5f074dfa955ba30f8b125a70cedb87e8610246
    Reviewed-on: https://chromium-review.googlesource.com/1162827
    Reviewed-by: Peter Williamson <petewil@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Yafei Duan <romax@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582418}

--- components/offline_pages/core/client_namespace_constants.h
+++ components/offline_pages/core/client_namespace_constants.h
@@ -25,6 +25,7 @@ extern const char kDownloadNamespace[];
 extern const char kNTPSuggestionsNamespace[];
 extern const char kSuggestedArticlesNamespace[];
 extern const char kBrowserActionsNamespace[];
+extern const char kLivePageSharingNamespace[];
 
 // Enum of namespaces used by metric collection.
 // See OfflinePagesNamespaceEnumeration in enums.xml for histogram usages.
@@ -40,6 +41,7 @@ enum class OfflinePagesNamespaceEnumeration {
   NTP_SUGGESTION = 6,
   SUGGESTED_ARTICLES = 7,
   BROWSER_ACTIONS = 8,
+  LIVE_PAGE_SHARING = 9,
   // NOTE: always keep this entry at the end. Add new result types only
   // immediately above this line. Make sure to update the corresponding
   // histogram enum accordingly.
