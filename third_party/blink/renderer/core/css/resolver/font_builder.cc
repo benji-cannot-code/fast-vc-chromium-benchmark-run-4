9d29b990bc08f26829c1471e2b09c042dbdd752a
Ella Ge <eirage@chromium.org>
2018-08-11 16:12:28 +0000

    Fix text autosizer line height with zoomed
    TextAutosizer::ComputeAutosizedFontSize are used in both font_builder and
    computed_style. The size passed in from font_builder is computedsize,
    which has zoom factor applied. But in ComputedStyle::SetTextAutosizingMultiplier,
    zoom factor doesn't.
    And also, kPleasantSize is hard-coded css pixel (without zoom factor), and it
    was used in calculation with computed_size(with zoom factor).
    The inconsistancy causes line height incorrect when multiplier applied(crbug/871124),
    and also text sometime lost scaling(crbug/871054)
    
    This CL correct the variable in ComputeAutosizedFontSize to computed_size,
    and pass in effective_zoom, so the sizes are computed in correct scale.
    
    
    Bug: 871054, 871124
    Change-Id: Icf546795663b76af3cd03e4e42da96483eb83af7
    Reviewed-on: https://chromium-review.googlesource.com/1169972
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582447}

--- third_party/blink/renderer/core/css/resolver/font_builder.cc
+++ third_party/blink/renderer/core/css/resolver/font_builder.cc
@@ -345,7 +345,7 @@ void FontBuilder::UpdateAdjustedSize(FontDescription& font_description,
       font_description, style.EffectiveZoom(), adjusted_size);
 
   adjusted_size = TextAutosizer::ComputeAutosizedFontSize(
-      adjusted_size, style.TextAutosizingMultiplier());
+      adjusted_size, style.TextAutosizingMultiplier(), style.EffectiveZoom());
   font_description.SetAdjustedSize(adjusted_size);
 }
 
@@ -355,7 +355,7 @@ void FontBuilder::UpdateComputedSize(FontDescription& font_description,
       GetComputedSizeFromSpecifiedSize(font_description, style.EffectiveZoom(),
                                        font_description.SpecifiedSize());
   computed_size = TextAutosizer::ComputeAutosizedFontSize(
-      computed_size, style.TextAutosizingMultiplier());
+      computed_size, style.TextAutosizingMultiplier(), style.EffectiveZoom());
   font_description.SetComputedSize(computed_size);
 }
 
