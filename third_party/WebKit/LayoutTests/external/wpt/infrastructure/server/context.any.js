a902c402e3851cb4f71ec679999c0dd398764ba1
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-08-12 19:37:54 +0000

    Import wpt@d31eacaff0c4d96f8c125c21faac6e0f75dd683c
    Using wpt-import in Chromium 2ef04d1e80e63e69c26893400e00a1c8b015d910.
    With Chromium commits locally applied on WPT:
    7dad0f1723 "Added new Sec-Metadata Web Platform Tests."
    
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/22604
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    foolip@chromium.org, robertma@chromium.org, smcgruer@chromium.org:
      external/wpt/infrastructure
    
    NOAUTOREVERT=true
    TBR=lukebjerring
    
    No-Export: true
    Change-Id: I13dd0e4c95a393b7ad1a09eeffeed86870b9a3b0
    Reviewed-on: https://chromium-review.googlesource.com/1172099
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582479}

---
+++ third_party/WebKit/LayoutTests/external/wpt/infrastructure/server/context.any.js
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: global=window,dedicatedworker,sharedworker,serviceworker
+test(t => {
+  // Test for object that's only exposed in serviceworker
+  if (self.clients) {
+      assert_true(self.isSecureContext);
+      assert_equals(location.protocol, "https:");
+  } else {
+      assert_false(self.isSecureContext);
+      assert_equals(location.protocol, "http:");
+  }
+});
+
+done();
--- third_party/WebKit/LayoutTests/external/wpt/infrastructure/server/secure-context.https.any.js
+++ third_party/WebKit/LayoutTests/external/wpt/infrastructure/server/secure-context.https.any.js
@@ -1,4 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: global=window,dedicatedworker,sharedworker,serviceworker
 test(() => {
   assert_true(self.isSecureContext);
 }, "Use of .https file name flag implies secure context");
