702763c371cadf68d48d6e446fe6ad3d01d236ca
Bruce Dawson <brucedawson@chromium.org>
2018-08-13 19:12:35 +0000

    Add missing include of <string>
    nt_registry.h was implicitly assuming that vector would include string.
    This change adds an explicit include of string to allow <vector> to stop
    including <string> in the VS header files.
    
    Change-Id: Id03d249b90320bc5506b20fd7481eecab58eecbc
    Reviewed-on: https://chromium-review.googlesource.com/1173065
    Reviewed-by: Penny MacNeil <pennymac@chromium.org>
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582659}

--- chrome_elf/nt_registry/nt_registry.h
+++ chrome_elf/nt_registry/nt_registry.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_ELF_NT_REGISTRY_NT_REGISTRY_H_
 #define CHROME_ELF_NT_REGISTRY_NT_REGISTRY_H_
 
+#include <string>
 #include <vector>
 
 #include "sandbox/win/src/nt_internals.h"  // NTSTATUS
