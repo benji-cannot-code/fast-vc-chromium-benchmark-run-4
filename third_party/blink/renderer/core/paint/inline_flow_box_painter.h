f5fb94e6b2214c6b9c50c774d7c69ef64903f0cf
Emil A Eklund <eae@chromium.org>
2018-08-13 04:01:16 +0000

    Compute multi-line lazily for inline painting
    Computing whether an inline block spans multiple lines is more expensive
    than expected thus causing a small paint performance regression for both
    legacy layout and LayoutNG. This patch moves the computation to use-time
    thereby avoiding it when cached drawings are used or paint isn't needed.
    
    Bug: 714962
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng;luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I97380b2e304c3c3293514ecf118fffdf6f9b9292
    Reviewed-on: https://chromium-review.googlesource.com/1170310
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582501}

--- third_party/blink/renderer/core/paint/inline_flow_box_painter.h
+++ third_party/blink/renderer/core/paint/inline_flow_box_painter.h
@@ -38,6 +38,12 @@ class InlineFlowBoxPainter : public InlineBoxPainterBase {
  protected:
   LayoutRect PaintRectForImageStrip(const LayoutRect&,
                                     TextDirection) const override;
+  void PaintNormalBoxShadow(const PaintInfo&,
+                            const ComputedStyle&,
+                            const LayoutRect& paint_rect) override;
+  void PaintInsetBoxShadow(const PaintInfo&,
+                           const ComputedStyle&,
+                           const LayoutRect& paint_rect) override;
 
  private:
   void PaintBackgroundBorderShadow(const PaintInfo&,
@@ -46,7 +52,8 @@ class InlineFlowBoxPainter : public InlineBoxPainterBase {
 
   BorderPaintingType GetBorderPaintType(
       const LayoutRect& adjusted_frame_rect,
-      IntRect& adjusted_clip_rect) const override;
+      IntRect& adjusted_clip_rect,
+      bool object_has_multiple_boxes) const override;
 
   LayoutRect AdjustedPaintRect(const LayoutPoint& paint_offset) const;
 
