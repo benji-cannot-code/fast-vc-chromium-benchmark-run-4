a20674605f869256767b66a46b798b8679a55141
Gauthier Ambard <gambard@chromium.org>
2018-08-13 08:35:21 +0000

    [iOS] Use UIModalPresentationPageSheet for ConsentBump
    The ConsentBump should be displayed modally on iPhone+ landscape and
    iPad.
    
    Bug: 873128
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I96482c930ef1b6b94509e8aafd7b1691b76b2e28
    Reviewed-on: https://chromium-review.googlesource.com/1170823
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582529}

--- ios/chrome/browser/ui/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view_controller.mm
@@ -5052,6 +5052,8 @@ applicationCommandEndpoint:(id<ApplicationCommands>)applicationCommandEndpoint {
       [[ConsentBumpCoordinator alloc] initWithBaseViewController:self];
   self.consentBumpCoordinator.delegate = self;
   [self.consentBumpCoordinator start];
+  self.consentBumpCoordinator.viewController.modalPresentationStyle =
+      UIModalPresentationPageSheet;
   [self presentViewController:self.consentBumpCoordinator.viewController
                      animated:YES
                    completion:nil];
