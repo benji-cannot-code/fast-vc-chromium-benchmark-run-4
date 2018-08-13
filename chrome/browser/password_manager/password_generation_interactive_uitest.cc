f2d7eae9c65b3d805aa8b884d9fe7e151bac8e3d
Vasilii Sukhanov <vasilii@chromium.org>
2018-08-13 09:26:55 +0000

    Reenable PasswordGenerationInteractiveTest.AutoSavingGeneratedPassword.
    Bug: 791389
    Change-Id: Iea3b6e0f2ec7899e099bd9d7e23017ac4c83e459
    Reviewed-on: https://chromium-review.googlesource.com/1170908
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582537}

--- chrome/browser/password_manager/password_generation_interactive_uitest.cc
+++ chrome/browser/password_manager/password_generation_interactive_uitest.cc
@@ -301,9 +301,8 @@ IN_PROC_BROWSER_TEST_F(PasswordGenerationInteractiveTest,
   EXPECT_TRUE(GenerationPopupShowing());
 }
 
-// https://crbug.com/791389
 IN_PROC_BROWSER_TEST_F(PasswordGenerationInteractiveTest,
-                       DISABLED_AutoSavingGeneratedPassword) {
+                       AutoSavingGeneratedPassword) {
   scoped_refptr<password_manager::TestPasswordStore> password_store =
       static_cast<password_manager::TestPasswordStore*>(
           PasswordStoreFactory::GetForProfile(
@@ -320,9 +319,7 @@ IN_PROC_BROWSER_TEST_F(PasswordGenerationInteractiveTest,
   content::SimulateKeyPress(WebContents(), ui::DomKey::FromCharacter('U'),
                             ui::DomCode::US_U, ui::VKEY_U, false, false, false,
                             false);
-  content::SimulateKeyPress(WebContents(), ui::DomKey::FromCharacter('N'),
-                            ui::DomCode::US_N, ui::VKEY_N, false, false, false,
-                            false);
+  WaitForElementValue("username_field", "U");
 
   // Submit form.
   NavigationObserver observer(WebContents());
@@ -339,6 +336,6 @@ IN_PROC_BROWSER_TEST_F(PasswordGenerationInteractiveTest,
       password_store->stored_passwords();
   EXPECT_EQ(1u, stored_passwords.size());
   EXPECT_EQ(1u, stored_passwords.begin()->second.size());
-  EXPECT_EQ(base::UTF8ToUTF16("UN"),
+  EXPECT_EQ(base::UTF8ToUTF16("U"),
             (stored_passwords.begin()->second)[0].username_value);
 }
