8d806b230b9fcff98a764acc323153f30132fad5
Ryan Daum <rdaum@chromium.org>
2018-08-13 15:07:26 +0000

    Revert "[chromecast] Detect screen enter/exit and corner holds."
    This reverts commit 64356c229ffca5a9da911891b4354eafc0d4ff8c.
    
    Reason for revert: Downstream breakage on device.
    
    Original change's description:
    > [chromecast] Detect screen enter/exit and corner holds.
    > 
    >   - Adds detection and dispatch of screen entry/exit events.
    >   - Adds detection of a finger press and hold in the corner.
    >   - Better handling of multiple-finger events on the screen.
    > 
    > Bug: internal b/112073644
    > Bug: internal b/112314035
    > Test: unit test and manual
    > Change-Id: Id2155f8e432428ee63f3457274f1507ce1edee53
    > Reviewed-on: https://chromium-review.googlesource.com/1165795
    > Reviewed-by: Alex Sakhartchouk <alexst@chromium.org>
    > Reviewed-by: Kevin Schoedel <kpschoedel@chromium.org>
    > Commit-Queue: Ryan Daum <rdaum@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#582196}
    
    TBR=alexst@chromium.org,kpschoedel@chromium.org,rdaum@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: internal b/112073644, internal b/112314035
    Change-Id: I8fc29ab7b2a7629803d1b45eff6b84bb582698c6
    Reviewed-on: https://chromium-review.googlesource.com/1172562
    Reviewed-by: Alex Sakhartchouk <alexst@chromium.org>
    Commit-Queue: Ryan Daum <rdaum@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582580}

--- chromecast/graphics/gestures/side_swipe_detector.h
+++ chromecast/graphics/gestures/side_swipe_detector.h
@@ -7,20 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROMECAST_GRAPHICS_GESTURES_SIDE_SWIPE_DETECTOR_H_
 
 #include "base/timer/elapsed_timer.h"
-#include "base/timer/timer.h"
 #include "chromecast/graphics/cast_gesture_handler.h"
 #include "ui/events/event_rewriter.h"
-#include "ui/events/gesture_detection/gesture_detector.h"
 
 namespace aura {
 class Window;
 }  // namespace aura
 
 namespace chromecast {
-
-namespace test {
-class SideSwipeDetectorTest;
-}  // namespace test
 
 // An event rewriter for detecting system-wide gestures performed on the margins
 // of the root window.
@@ -35,10 +29,8 @@ class SideSwipeDetector : public ui::EventRewriter {
 
   ~SideSwipeDetector() override;
 
-  bool GetMarginPosition(const gfx::Point& point,
-                         const gfx::Rect& screen_bounds,
-                         CastSideSwipeOrigin* side,
-                         CastGestureHandler::Corner* corner) const;
+  CastSideSwipeOrigin GetDragPosition(const gfx::Point& point,
+                                      const gfx::Rect& screen_bounds) const;
 
   // Overridden from ui::EventRewriter
   ui::EventRewriteStatus RewriteEvent(
@@ -49,16 +41,7 @@ class SideSwipeDetector : public ui::EventRewriter {
       std::unique_ptr<ui::Event>* new_event) override;
 
  private:
-  friend class test::SideSwipeDetectorTest;
-  void SetTimerForTesting(std::unique_ptr<base::OneShotTimer> mock_timer);
-
   void StashEvent(const ui::TouchEvent& event);
-  void OnCornerHoldTimerFired();
-  void EndCornerHold(const ui::TouchEvent& event);
-  void CancelCornerHoldCheck();
-
-  ui::EventRewriteStatus StartNextDispatchStashedEvents(
-      std::unique_ptr<ui::Event>* new_event);
 
   const int gesture_start_width_;
   const int gesture_start_height_;
@@ -66,18 +49,10 @@ class SideSwipeDetector : public ui::EventRewriter {
 
   CastGestureHandler* gesture_handler_;
   aura::Window* root_window_;
-  CastSideSwipeOrigin current_swipe_origin_;
-  int current_pointer_id_;
-  base::ElapsedTimer gesture_elapsed_timer_;
-  std::deque<ui::TouchEvent> stashed_events_;
+  CastSideSwipeOrigin current_swipe_;
+  base::ElapsedTimer current_swipe_time_;
 
-  // A default gesture detector config, so we can share the same
-  // longpress timeout for corner hold.
-  ui::GestureDetector::Config gesture_detector_config_;
-  std::unique_ptr<base::OneShotTimer> corner_hold_timer_;
-  std::unique_ptr<ui::Event> corner_hold_start_event_;
-  bool in_corner_hold_;
-  CastGestureHandler::Corner current_corner_origin_;
+  std::deque<ui::TouchEvent> stashed_events_;
 
   DISALLOW_COPY_AND_ASSIGN(SideSwipeDetector);
 };
