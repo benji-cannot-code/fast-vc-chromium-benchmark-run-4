caa1ebce3df6616db3f2538a0c67c6849574eba9
sczs <sczs@chromium.org>
2018-08-13 21:45:37 +0000

    [ios] Nulls historyServices when stopping HistoryCoordinator
    HistoryCoordinator owns _browsingHistoryService, which is instantiated on start
    but its never niled on stop. Since HistoryCoordinator is long lived and
    never gets deallocated, _browsingHistoryService keeps listening for notifications.
    
    When the App is being shutdown _browsingHistoryService gets notified of an
    Auth change and HistoryTableVC (of which _browsingHistoryDriver holds a reference
    of) tries to react accordingly. Since the user is no longer logged in this causes
    a background Crash.
    
    This CL nils both pointers so they stop observing once HistoryCoordinator has
    been stopped.
    
    Bug: 867452
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Id99b4ab481d90eca3a380d5b090631c263b65ef3
    Reviewed-on: https://chromium-review.googlesource.com/1171250
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582714}

--- ios/chrome/browser/ui/history/history_coordinator.mm
+++ ios/chrome/browser/ui/history/history_coordinator.mm
@@ -109,6 +109,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           dismissViewControllerAnimated:YES
                              completion:completionHandler];
       self.historyNavigationController = nil;
+      _browsingHistoryDriver = nullptr;
+      _browsingHistoryService = nullptr;
     };
     if (self.historyClearBrowsingDataCoordinator) {
       [self.historyClearBrowsingDataCoordinator stopWithCompletion:^() {
