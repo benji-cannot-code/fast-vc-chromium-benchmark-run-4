3adc3d1c6ed02c35126721dc49caf736ab75c875
Yi Su <mrsuyi@chromium.org>
2018-08-13 11:59:26 +0000

    Fix bug of inconsistent button title for tabs count in toolbar and tab-grid
    Add a helper function that returns text for tabs count:
    (-inf, 0]   => ""
    (0, 100)    => "num"
    [100, +inf) => ":)"
    
    Bug: 835801
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ica4607b10dbd1f241d4e5a07c32128a1f4607db8
    Reviewed-on: https://chromium-review.googlesource.com/1172297
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Yi Su <mrsuyi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582552}

--- ios/chrome/browser/ui/toolbar/buttons/toolbar_tab_grid_button.mm
+++ ios/chrome/browser/ui/toolbar/buttons/toolbar_tab_grid_button.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/toolbar/buttons/toolbar_constants.h"
 #import "ios/chrome/browser/ui/toolbar/public/features.h"
 #include "ios/chrome/browser/ui/ui_util.h"
+#import "ios/chrome/browser/ui/uikit_ui_util.h"
 #import "ios/chrome/common/ui_util/constraints_ui_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -38,14 +39,7 @@ const CGFloat kLabelSize = 14;
   // the button's title may be empty or contain an easter egg, but the
   // accessibility value will always be equal to |tabCount|.
   NSString* tabStripButtonValue = [NSString stringWithFormat:@"%d", tabCount];
-  NSString* tabStripButtonTitle = tabStripButtonValue;
-  if (tabCount <= 0) {
-    tabStripButtonTitle = @"";
-  } else if (tabCount > kShowTabStripButtonMaxTabCount) {
-    // As an easter egg, show a smiley face instead of the count if the user has
-    // more than 99 tabs open.
-    tabStripButtonTitle = @":)";
-  }
+  NSString* tabStripButtonTitle = TextForTabCount(tabCount);
 
   // TODO(crbug.com/799601): Delete this once its not needed.
   if (base::FeatureList::IsEnabled(kMemexTabSwitcher)) {
