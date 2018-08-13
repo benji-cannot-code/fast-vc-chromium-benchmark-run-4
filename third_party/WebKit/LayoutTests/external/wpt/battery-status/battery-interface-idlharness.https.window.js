2bc467cc06adeba967d08e48e32ec3ac15910ac1
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-08-13 16:42:49 +0000

    Import wpt@ff1ca2cdc345804c8c9a3b707479b9bcca1bdf01
    Using wpt-import in Chromium cc4cd8976eca52d966ae6c56f8252c47ad4703f8.
    With Chromium commits locally applied on WPT:
    7dad0f1723 "Added new Sec-Metadata Web Platform Tests."
    3364c23332 "[css-properties-values-api] Reify values from attributeStyleMap.get."
    
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/22692
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    jsbell@chromium.org:
      external/wpt/resources
    timvolodine@chromium.org:
      external/wpt/battery-status
    
    NOAUTOREVERT=true
    TBR=lukebjerring
    
    No-Export: true
    Change-Id: I1e2e1f2c301da8d05d4c5aad485ea3a6e7c44d18
    Reviewed-on: https://chromium-review.googlesource.com/1172444
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582610}

--- third_party/WebKit/LayoutTests/external/wpt/battery-status/battery-interface-idlharness.https.window.js
+++ third_party/WebKit/LayoutTests/external/wpt/battery-status/battery-interface-idlharness.https.window.js
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 'use strict';
 
 idl_test(
-  ['battery'],
+  ['battery-status'],
   ['dom', 'html'],
   async idl_array => {
     idl_array.add_objects({
