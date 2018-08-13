355a739807b44f0f88823e638bb15526f1dcdb75
Doug Arnett <dougarnett@chromium.org>
2018-08-13 23:29:14 +0000

    Adds new resource_loading_hints previews type to Previews UKM
    Bug: 870030
    Change-Id: If46caf16806b64ed5516b548d3080a0a734ca614
    Reviewed-on: https://chromium-review.googlesource.com/1159267
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Robert Kaplow (slow) <rkaplow@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582741}

--- chrome/browser/page_load_metrics/observers/previews_ukm_observer.cc
+++ chrome/browser/page_load_metrics/observers/previews_ukm_observer.cc
@@ -60,6 +60,10 @@ PreviewsUKMObserver::OnCommit(content::NavigationHandle* navigation_handle,
                             previews::PreviewsType::NOSCRIPT) {
     noscript_seen_ = true;
   }
+  if (previews_state && previews::GetMainFramePreviewsType(previews_state) ==
+                            previews::PreviewsType::RESOURCE_LOADING_HINTS) {
+    resource_loading_hints_seen_ = true;
+  }
   previews::PreviewsUserData* previews_user_data =
       chrome_navigation_data->previews_user_data();
   if (previews_user_data &&
@@ -109,7 +113,8 @@ void PreviewsUKMObserver::RecordPreviewsTypes(
     const page_load_metrics::PageLoadExtraInfo& info) {
   // Only record previews types when they are active.
   if (!server_lofi_seen_ && !client_lofi_seen_ && !lite_page_seen_ &&
-      !noscript_seen_ && !origin_opt_out_occurred_ && !save_data_enabled_) {
+      !noscript_seen_ && !resource_loading_hints_seen_ &&
+      !origin_opt_out_occurred_ && !save_data_enabled_) {
     return;
   }
 
@@ -122,6 +127,8 @@ void PreviewsUKMObserver::RecordPreviewsTypes(
     builder.Setlite_page(1);
   if (noscript_seen_)
     builder.Setnoscript(1);
+  if (resource_loading_hints_seen_)
+    builder.Setresource_loading_hints(1);
   if (opt_out_occurred_)
     builder.Setopt_out(1);
   if (origin_opt_out_occurred_)
