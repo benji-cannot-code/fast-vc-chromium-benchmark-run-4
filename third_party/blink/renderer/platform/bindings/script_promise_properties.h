790473967c53a929cabad483d6218112c8e68da4
Mugdha Lakhani <nator@chromium.org>
2018-08-13 14:12:02 +0000

    [Background Fetch] Implement BackgroundFetchRecord.
    This will be required to implement match() and matchAll() on
    BackgroundFetchRegistration in a follow-up CL.
    
    Bug: 869918
    Change-Id: I9ed88076488a035f313b9d16e0f41b3c168a0a09
    Reviewed-on: https://chromium-review.googlesource.com/1169465
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Mugdha Lakhani <nator@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582574}

--- third_party/blink/renderer/platform/bindings/script_promise_properties.h
+++ third_party/blink/renderer/platform/bindings/script_promise_properties.h
@@ -7,13 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_BINDINGS_SCRIPT_PROMISE_PROPERTIES_H_
 
 // See ScriptPromiseProperty.h
-#define SCRIPT_PROMISE_PROPERTIES(P, ...)    \
-  P(ScriptPromise, kReady##__VA_ARGS__)      \
-  P(ScriptPromise, kClosed##__VA_ARGS__)     \
-  P(ScriptPromise, kFinished##__VA_ARGS__)   \
-  P(ScriptPromise, kLoaded##__VA_ARGS__)     \
-  P(ScriptPromise, kReleased##__VA_ARGS__)   \
-  P(ScriptPromise, kUserChoice##__VA_ARGS__) \
+#define SCRIPT_PROMISE_PROPERTIES(P, ...)       \
+  P(ScriptPromise, kReady##__VA_ARGS__)         \
+  P(ScriptPromise, kClosed##__VA_ARGS__)        \
+  P(ScriptPromise, kFinished##__VA_ARGS__)      \
+  P(ScriptPromise, kLoaded##__VA_ARGS__)        \
+  P(ScriptPromise, kReleased##__VA_ARGS__)      \
+  P(ScriptPromise, kResponseReady##__VA_ARGS__) \
+  P(ScriptPromise, kUserChoice##__VA_ARGS__)    \
   P(ScriptPromise, kPreloadResponse##__VA_ARGS__)
 
 #endif  // THIRD_PARTY_BLINK_RENDERER_PLATFORM_BINDINGS_SCRIPT_PROMISE_PROPERTIES_H_
