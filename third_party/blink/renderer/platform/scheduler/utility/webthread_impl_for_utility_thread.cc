41f9d112828b5c8e50c0602921d49f62473e317e
Yuta Kitamura <yutak@chromium.org>
2018-08-13 13:06:11 +0000

    Remove NOTIMPLEMENTED() in WebThreadImplForUtilityThread::Scheduler().
    It's possible to reach here even if Scheduler() just returns nullptr.
    
    Bug: 873207
    Change-Id: I6664ccaa75fbcb31e4f9e75099d85404f8db8059
    Reviewed-on: https://chromium-review.googlesource.com/1172178
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Yuta Kitamura <yutak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582561}

--- third_party/blink/renderer/platform/scheduler/utility/webthread_impl_for_utility_thread.cc
+++ third_party/blink/renderer/platform/scheduler/utility/webthread_impl_for_utility_thread.cc
@@ -17,7 +17,6 @@ WebThreadImplForUtilityThread::WebThreadImplForUtilityThread()
 WebThreadImplForUtilityThread::~WebThreadImplForUtilityThread() = default;
 
 blink::ThreadScheduler* WebThreadImplForUtilityThread::Scheduler() const {
-  NOTIMPLEMENTED();
   return nullptr;
 }
 
