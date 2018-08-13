05c072739b81704ee9ed46ebdd7bc9c3e4182658
Ramin Halavati <rhalavati@chromium.org>
2018-08-13 07:07:07 +0000

    Move Rappor preferences to incognito persistent whitelist.
    Preferences related to Rapor are moved to the incognito persistent
    storage list.
    This CL does not change the current behavior.
    
    Bug: 861722
    Change-Id: Ie2212b845d2ce9a73d60a7fc24ae480b29b56420
    Reviewed-on: https://chromium-review.googlesource.com/1166829
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582517}

--- chrome/browser/prefs/pref_service_incognito_whitelist.cc
+++ chrome/browser/prefs/pref_service_incognito_whitelist.cc
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/proxy_config/proxy_config_pref_names.h"
 #include "components/proxy_config/proxy_prefs.h"
 #include "components/rappor/rappor_pref_names.h"
-#include "components/rappor/rappor_prefs.h"
 #include "components/reading_list/core/reading_list_pref_names.h"
 #include "components/search_engines/search_engines_pref_names.h"
 #include "components/startup_metric_utils/browser/pref_names.h"
@@ -137,6 +136,11 @@ const char* persistent_pref_names[] = {
     metrics::prefs::kUninstallMetricsPageLoadCount,
     metrics::prefs::kUninstallMetricsUptimeSec, metrics::prefs::kUkmCellDataUse,
     metrics::prefs::kUmaCellDataUse, metrics::prefs::kUserCellDataUse,
+
+    // Rappor preferences are not used in incognito mode, but they are written
+    // in startup if they don't exist. So if the startup would be in incognito,
+    // they need to be persisted.
+    rappor::prefs::kRapporCohortSeed, rappor::prefs::kRapporSecret,
 
     // Although UKMs are not collected in incognito, theses preferences may be
     // changed by UMA/Sync/Unity consent, and need to be the same between
@@ -800,14 +804,6 @@ const char* temporary_incognito_whitelist[] = {
     ProxyPrefs::kDirectProxyModeName, ProxyPrefs::kAutoDetectProxyModeName,
     ProxyPrefs::kPacScriptProxyModeName, ProxyPrefs::kFixedServersProxyModeName,
     ProxyPrefs::kSystemProxyModeName,
-
-    // components/rappor/rappor_pref_names.h
-    rappor::prefs::kRapporCohortDeprecated, rappor::prefs::kRapporCohortSeed,
-    rappor::prefs::kRapporLastDailySample, rappor::prefs::kRapporSecret,
-
-    // components/rappor/rappor_prefs.h
-    rappor::internal::kLoadCohortHistogramName,
-    rappor::internal::kLoadSecretHistogramName,
 
     // components/reading_list/core/reading_list_pref_names.h
     reading_list::prefs::kReadingListHasUnseenEntries,
