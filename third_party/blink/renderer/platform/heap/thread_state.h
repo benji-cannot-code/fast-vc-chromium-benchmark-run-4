0530bc66318f1b9d4c08233c68bb2051e1aa8b42
Vlad Tsyrklevich <vtsyrklevich@chromium.org>
2018-08-14 22:20:02 +0000

    SafeStack: Make oilpan unsafe stack aware
    SafeStack [1] introduces a secondary thread stack called the unsafe
    stack that also needs to be scanned for object references. Introduce
    matching logic in oilpan to scan the unsafe stack for heap references.
    
    [1] https://clang.llvm.org/docs/SafeStack.html
    
    Bug: 864705
    Change-Id: I376077bd985e2077aa3771101c1822e1570c7807
    Reviewed-on: https://chromium-review.googlesource.com/1169772
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Vlad Tsyrklevich <vtsyrklevich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583056}

--- third_party/blink/renderer/platform/heap/thread_state.h
+++ third_party/blink/renderer/platform/heap/thread_state.h
@@ -439,6 +439,11 @@ class PLATFORM_EXPORT ThreadState final
   void LeaveSafePoint();
 
   void RecordStackEnd(intptr_t* end_of_stack) { end_of_stack_ = end_of_stack; }
+#if HAS_FEATURE(safe_stack)
+  void RecordUnsafeStackEnd(intptr_t* end_of_unsafe_stack) {
+    end_of_unsafe_stack_ = end_of_unsafe_stack;
+  }
+#endif
   NO_SANITIZE_ADDRESS void CopyStackUntilSafePointScope();
 
   // A region of non-weak PersistentNodes allocated on the given thread.
@@ -622,6 +627,9 @@ class PLATFORM_EXPORT ThreadState final
   void ClearSafePointScopeMarker() {
     safe_point_stack_copy_.clear();
     safe_point_scope_marker_ = nullptr;
+#if HAS_FEATURE(safe_stack)
+    safe_point_scope_unsafe_marker_ = nullptr;
+#endif
   }
 
   bool ShouldVerifyMarking() const;
@@ -708,8 +716,14 @@ class PLATFORM_EXPORT ThreadState final
   BlinkGC::StackState stack_state_;
   intptr_t* start_of_stack_;
   intptr_t* end_of_stack_;
-
   void* safe_point_scope_marker_;
+
+#if HAS_FEATURE(safe_stack)
+  intptr_t* start_of_unsafe_stack_;
+  intptr_t* end_of_unsafe_stack_;
+  void* safe_point_scope_unsafe_marker_;
+#endif
+
   Vector<Address> safe_point_stack_copy_;
   bool sweep_forbidden_;
   size_t no_allocation_count_;
