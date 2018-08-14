f25d877f7a8109c0588ba606eb6aee242a42dbc8
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-08-14 09:58:34 +0000

    Import wpt@dba431554f1256e84dd3500f91d86b755fbced3f
    Using wpt-import in Chromium 0422626272014f175914a3f80f75c637db713bd3.
    With Chromium commits locally applied on WPT:
    7dad0f1723 "Added new Sec-Metadata Web Platform Tests."
    
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/22700
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    dtapuska@chromium.org:
      external/wpt/uievents
    
    NOAUTOREVERT=true
    TBR=raphael.kubo.da.costa@intel.com
    
    No-Export: true
    Change-Id: Ibc1fdb53ea79ae7f8809f0b21c26ab4e49e97ad6
    Reviewed-on: https://chromium-review.googlesource.com/1174099
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582879}

---
+++ third_party/WebKit/LayoutTests/external/wpt/uievents/idlharness.window.js
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/resources/WebIDLParser.js
+// META: script=/resources/idlharness.js
+
+'use strict';
+
+idl_test(
+  ['uievents'],
+  ['dom'],
+  idl_array => {
+    idl_array.add_objects({
+      FocusEvent: ['new FocusEvent("event")'],
+      MouseEvent: ['new MouseEvent("event")'],
+      WheelEvent: ['new WheelEvent("event")'],
+      KeyboardEvent: ['new KeyboardEvent("event")'],
+      CompositionEvent: ['new CompositionEvent("event")'],
+      UIEvent: ['new UIEvent("event")'],
+      InputEvent: ['new InputEvent("event")'],
+    });
+  }
+);
