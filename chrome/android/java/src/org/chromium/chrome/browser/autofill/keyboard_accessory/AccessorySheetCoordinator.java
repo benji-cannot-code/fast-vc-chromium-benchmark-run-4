a9ab9cee7c58c5788d01a8264e8d2f629bac0fd6
Friedrich Horschig <fhorschig@chromium.org>
2018-08-14 10:07:32 +0000

    [Android] Record keyboard accessory bar metrics up to once
    Before this CL, the accessory bar wouldn't add impressions to delayed
    appearing actions. This CL ensures this is done once (per new set of
    actions) while keeping the constraint that every bar impression happens
    exactly once.
    
    Bug: 873143
    Change-Id: I4de6c8f8e95321176d9bca19459d6e9d2cdae746
    Reviewed-on: https://chromium-review.googlesource.com/1171227
    Commit-Queue: Friedrich Horschig <fhorschig@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582881}

--- chrome/android/java/src/org/chromium/chrome/browser/autofill/keyboard_accessory/AccessorySheetCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill/keyboard_accessory/AccessorySheetCoordinator.java
@@ -41,7 +41,7 @@ public class AccessorySheetCoordinator {
         model.addObserver(new PropertyModelChangeProcessor<>(model, stubHolder,
                 new LazyViewBinderAdapter<>(
                         new AccessorySheetViewBinder(), this::onViewInflated)));
-        KeyboardAccessoryMetricsRecorder.recordModelChanges(model);
+        KeyboardAccessoryMetricsRecorder.registerMetricsObserver(model);
         mMediator = new AccessorySheetMediator(model);
     }
 
