20d2879c84c9f6cbdc17361ef8b3e860dcbc083f
Pedro Amaral <amaralp@chromium.org>
2018-08-14 18:21:34 +0000

    Swapping new tab and incognito toggle buttons in Bottom Toolbar
    The fixes a hole that is present in the bottom toolbar when the
    horizontal tab switcher is not enabled. It also makes it easier to
    reach the new tab button which we expect to be more used than the
    toggle button.
    
    Change-Id: I7fa5bc192516d956a78704c4d90e2d656abd856c
    Reviewed-on: https://chromium-review.googlesource.com/1170193
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Pedro Amaral <amaralp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582979}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
@@ -830,7 +830,7 @@ public class ToolbarManager implements ToolbarTabController, UrlFocusChangeListe
                     && ChromeFeatureList.isEnabled(
                                ChromeFeatureList.HORIZONTAL_TAB_SWITCHER_ANDROID)
                     && PrefServiceBridge.getInstance().isIncognitoModeEnabled();
-            final ToolbarButtonData secondSlotTabSwitcherButtonData = showIncognitoToggleButton
+            final ToolbarButtonData firstSlotTabSwitcherButtonData = showIncognitoToggleButton
                     ? createIncognitoToggleButton(
                               wrapBottomToolbarClickListenerForIPH(incognitoClickHandler),
                               mActivity)
@@ -842,9 +842,9 @@ public class ToolbarManager implements ToolbarTabController, UrlFocusChangeListe
                     wrapBottomToolbarClickListenerForIPH(tabSwitcherClickHandler),
                     mAppMenuButtonHelper, mTabModelSelector, mOverviewModeBehavior,
                     mActivity.getContextualSearchManager(), mActivity.getWindowAndroid(),
+                    firstSlotTabSwitcherButtonData,
                     createNewTabButton(
-                            wrapBottomToolbarClickListenerForIPH(newTabClickHandler), mActivity),
-                    secondSlotTabSwitcherButtonData);
+                            wrapBottomToolbarClickListenerForIPH(newTabClickHandler), mActivity));
 
             Tab currentTab = tabModelSelector.getCurrentTab();
             maybeShowDuetHelpBubble(currentTab);
