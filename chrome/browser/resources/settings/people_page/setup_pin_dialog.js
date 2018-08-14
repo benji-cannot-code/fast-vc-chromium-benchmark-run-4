a5ebd2a6f9deab514a870b5661c3c3d4e6134b45
Sammie Quon <sammiequon@google.com>
2018-08-14 21:04:21 +0000

    settings: Update ripples on setup PIN keyboard.
    Also fix a bug where continue button was still enabled after tabbing to
    and pressing backspace with the keyboard.
    
    Test: manual
    Bug: 855014, 869641
    Change-Id: I0f3e570fc87b1a2e60de112de47558885f5d4847
    Reviewed-on: https://chromium-review.googlesource.com/1173139
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583030}

--- chrome/browser/resources/settings/people_page/setup_pin_dialog.js
+++ chrome/browser/resources/settings/people_page/setup_pin_dialog.js
@@ -264,6 +264,8 @@ Polymer({
         this.quickUnlockPrivate_.checkCredential(
             chrome.quickUnlockPrivate.QuickUnlockMode.PIN,
             this.pinKeyboardValue_, this.processPinProblems_.bind(this));
+      } else {
+        this.enableSubmit_ = false;
       }
       return;
     }
