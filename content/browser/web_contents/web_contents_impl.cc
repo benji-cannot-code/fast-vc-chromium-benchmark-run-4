26a64c9e89676073b1d988a777a96ae872a68891
Chris Mumford <cmumford@chromium.org>
2018-08-14 23:13:10 +0000

    Revert "Security drop fullscreen for any nested WebContents level."
    This reverts commit 3dcaec6e30feebefc11ee6972a76d446257b28a6.
    
    Reason for revert: Failing Linux MSan tests
    
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Linux%20MSan%20Tests/11191
    
    DevTools listening on ws://127.0.0.1:34267/devtools/browser/a12caedc-549b-4d49-b9bb-a0519f91f318
    ==14126==WARNING: MemorySanitizer: use-of-uninitialized-value
        #0 0x3c2bb23 in ExitFullscreenModeForTab ./../../content/browser/web_contents/web_contents_impl_browsertest.cc:1561:9
    
    Original change's description:
    > Security drop fullscreen for any nested WebContents level.
    > 
    > BUG=873080
    > TEST=as in bug
    > 
    > Change-Id: Icd75715ac42789c84870967b11c9917a972ae086
    > Reviewed-on: https://chromium-review.googlesource.com/1173342
    > Commit-Queue: Avi Drissman <avi@chromium.org>
    > Reviewed-by: Sidney San Martín <sdy@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#582970}
    
    TBR=avi@chromium.org,sdy@chromium.org
    
    Change-Id: I52a9b29f2d17ea6e7db0128815653de624fa5b13
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 873080
    Reviewed-on: https://chromium-review.googlesource.com/1174640
    Reviewed-by: Chris Mumford <cmumford@chromium.org>
    Commit-Queue: Chris Mumford <cmumford@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583071}

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -2696,7 +2696,8 @@ void WebContentsImpl::CreateNewWindow(
 
   // Any new WebContents opened while this WebContents is in fullscreen can be
   // used to confuse the user, so drop fullscreen.
-  ForSecurityDropFullscreen();
+  if (IsFullscreenForCurrentTab())
+    ExitFullscreen(true);
 
   if (params.opener_suppressed) {
     // When the opener is suppressed, the original renderer cannot access the
@@ -4366,7 +4367,8 @@ void WebContentsImpl::ViewSource(RenderFrameHostImpl* frame) {
 
   // Any new WebContents opened while this WebContents is in fullscreen can be
   // used to confuse the user, so drop fullscreen.
-  ForSecurityDropFullscreen();
+  if (IsFullscreenForCurrentTab())
+    ExitFullscreen(true);
 
   // We intentionally don't share the SiteInstance with the original frame so
   // that view source has a consistent process model and always ends up in a new
@@ -5077,7 +5079,8 @@ void WebContentsImpl::RunJavaScriptDialog(RenderFrameHost* render_frame_host,
 
   // Running a dialog causes an exit to webpage-initiated fullscreen.
   // http://crbug.com/728276
-  ForSecurityDropFullscreen();
+  if (IsFullscreenForCurrentTab())
+    ExitFullscreen(true);
 
   auto callback =
       base::BindOnce(&WebContentsImpl::OnDialogClosed, base::Unretained(this),
@@ -5146,7 +5149,8 @@ void WebContentsImpl::RunBeforeUnloadConfirm(
 
   // Running a dialog causes an exit to webpage-initiated fullscreen.
   // http://crbug.com/728276
-  ForSecurityDropFullscreen();
+  if (IsFullscreenForCurrentTab())
+    ExitFullscreen(true);
 
   RenderFrameHostImpl* rfhi =
       static_cast<RenderFrameHostImpl*>(render_frame_host);
@@ -5728,15 +5732,6 @@ void WebContentsImpl::EnsureOpenerProxiesExist(RenderFrameHost* source_rfh) {
   }
 }
 
-void WebContentsImpl::ForSecurityDropFullscreen() {
-  WebContentsImpl* web_contents = this;
-  while (web_contents) {
-    if (web_contents->IsFullscreenForCurrentTab())
-      web_contents->ExitFullscreen(true);
-    web_contents = web_contents->GetOuterWebContents();
-  }
-}
-
 void WebContentsImpl::SetAsFocusedWebContentsIfNecessary() {
   // Only change focus if we are not currently focused.
   WebContentsImpl* old_contents = GetFocusedWebContents();
@@ -5804,7 +5799,8 @@ void WebContentsImpl::SetFocusedFrame(FrameTreeNode* node,
 void WebContentsImpl::DidCallFocus() {
   // Any explicit focusing of another window while this WebContents is in
   // fullscreen can be used to confuse the user, so drop fullscreen.
-  ForSecurityDropFullscreen();
+  if (IsFullscreenForCurrentTab())
+    ExitFullscreen(true);
 }
 
 RenderFrameHost* WebContentsImpl::GetFocusedFrameIncludingInnerWebContents() {
