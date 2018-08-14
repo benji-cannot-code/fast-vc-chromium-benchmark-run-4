d2ffb2d758f1b3c4d462e075eacf5619711c9d25
Pavol Marko <pmarko@chromium.org>
2018-08-14 12:36:08 +0000

    [Reland] Add vector of intervals range checking to Time Restrictions policy
    * Add function GetEndOfCurrent interval to obtain the last end of the
    interval which contains the current time.
    * Add function MaterializeIntervalsToLocalTimezone, which materializes
    the given intervals to the current local timezone.
    * Add function TimeIsInsideCrosSettingsIntervals, which checks if the
    current time is inside the intervals set in cros settings. It then
    returns the localized string of the end of the interval that contains
    the current time.
    
    This is a reland of CL:1132676 which fixes memory leaks that lead to a
    revert (revert was CL:1172003).
    
    BUG=chromium:852860
    TEST=added unittests
    
    Change-Id: I8ad01d4ab90d4e699e4b7377c24e4932513ebe1b
    Reviewed-on: https://chromium-review.googlesource.com/1172128
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582892}

--- chrome/browser/chromeos/policy/weekly_time/time_utils.h
+++ chrome/browser/chromeos/policy/weekly_time/time_utils.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/optional.h"
 #include "base/strings/string16.h"
 #include "third_party/icu/source/i18n/unicode/timezone.h"
 
@@ -56,6 +57,14 @@ std::vector<WeeklyTimeInterval> ConvertIntervalsToGmt(
 base::TimeDelta GetDeltaTillNextTimeInterval(
     const WeeklyTime& current_time,
     const std::vector<WeeklyTimeInterval>& weekly_time_intervals);
+
+// Takes in a vector of weekly time intervals. If |clock->Now()|
+// is inside one of the intervals, then the function returns the
+// interval that contains |clock->Now()|. Otherwise, return |base::nullopt|.
+// The intervals must have a defined
+base::Optional<WeeklyTimeInterval> GetIntervalForCurrentTime(
+    const std::vector<WeeklyTimeInterval>& intervals,
+    base::Clock* clock);
 
 }  // namespace weekly_time_utils
 }  // namespace policy
