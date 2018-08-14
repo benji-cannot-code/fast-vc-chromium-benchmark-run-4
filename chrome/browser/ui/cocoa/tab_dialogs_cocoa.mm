77076be1fcba13d198e953f9fed3a8af39e6d45d
Elly Fong-Jones <ellyjones@chromium.org>
2018-08-14 17:55:43 +0000

    cocoa: remove all Cocoa content setting UI
    This has been off by default since M67 and is actually dead in M69.
    
    Tbr: thakis@chromium.org
    Bug: 832676
    Change-Id: Iba63da4b6c0d12fea4356bc5173b19ae46d4c510
    Reviewed-on: https://chromium-review.googlesource.com/1172791
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582971}

--- chrome/browser/ui/cocoa/tab_dialogs_cocoa.mm
+++ chrome/browser/ui/cocoa/tab_dialogs_cocoa.mm
@@ -16,10 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/ui_features.h"
 
 #if !BUILDFLAG(MAC_VIEWS_BROWSER)
-#import "chrome/browser/ui/cocoa/content_settings/collected_cookies_mac.h"
-#endif
-
-#if !BUILDFLAG(MAC_VIEWS_BROWSER)
 // static
 void TabDialogs::CreateForWebContents(content::WebContents* contents) {
   DCHECK(contents);
@@ -54,12 +50,7 @@ gfx::NativeView TabDialogsCocoa::GetDialogParentView() const {
 }
 
 void TabDialogsCocoa::ShowCollectedCookies() {
-#if BUILDFLAG(MAC_VIEWS_BROWSER)
   NOTREACHED() << "MacViewsBrowser builds can't use Cocoa dialogs";
-#else
-  // Deletes itself on close.
-  new CollectedCookiesMac(web_contents_);
-#endif
 }
 
 void TabDialogsCocoa::ShowHungRendererDialog(
