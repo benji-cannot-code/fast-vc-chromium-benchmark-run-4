4cf1e8f6c29ef68deecdc9c6b979033bd46aa2a4
Oriol Brufau <obrufau@igalia.com>
2018-08-14 12:56:02 +0000

    [css-logical] Interpolate logical properties like physical ones in CSS transitions
    Spec: https://drafts.csswg.org/css-logical/#box
    
    BUG=873101
    
    TEST=external/wpt/css/css-logical/animation-004.html
    
    Change-Id: I104a8ecc746f4516e567b16a75c5d32a31d4dab8
    Reviewed-on: https://chromium-review.googlesource.com/1170769
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Oriol Brufau <obrufau@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#582895}

--- third_party/blink/renderer/core/animation/css/css_animations.cc
+++ third_party/blink/renderer/core/animation/css/css_animations.cc
@@ -867,7 +867,8 @@ void CSSAnimations::CalculateTransitionUpdateForCustomProperty(
 void CSSAnimations::CalculateTransitionUpdateForStandardProperty(
     TransitionUpdateState& state,
     const CSSTransitionData::TransitionProperty& transition_property,
-    size_t transition_index) {
+    size_t transition_index,
+    const ComputedStyle& style) {
   if (transition_property.property_type !=
       CSSTransitionData::kTransitionKnownProperty) {
     return;
@@ -886,7 +887,10 @@ void CSSAnimations::CalculateTransitionUpdateForStandardProperty(
         property_list.length() ? property_list.properties()[i]->PropertyID()
                                : resolved_id;
     DCHECK_GE(longhand_id, firstCSSProperty);
-    const CSSProperty& property = CSSProperty::Get(longhand_id);
+    const CSSProperty& property =
+        CSSProperty::Get(longhand_id)
+            .ResolveDirectionAwareProperty(style.Direction(),
+                                           style.GetWritingMode());
     PropertyHandle property_handle = PropertyHandle(property);
 
     if (!animate_all && !property.IsInterpolable()) {
@@ -941,7 +945,7 @@ void CSSAnimations::CalculateTransitionUpdate(CSSAnimationUpdate& update,
       } else {
         DCHECK_EQ(property_pass, PropertyPass::kStandard);
         CalculateTransitionUpdateForStandardProperty(state, transition_property,
-                                                     transition_index);
+                                                     transition_index, style);
       }
     }
   }
