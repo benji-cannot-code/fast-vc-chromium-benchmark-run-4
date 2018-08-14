cdd15b299f71026032f66c9bd108a79f840912e3
Yuki Yamada <yukiy@google.com>
2018-08-14 03:40:23 +0000

    Do not clear TraceWrapperV8Reference in blink::V8AbstractEventListener destructor
    This CL removed some code that clearing TraceWrapperV8Reference object
    in its destructor.
    |listener_| is cleared in the destructor of TraceWrapperV8Reference, so
    we do not have to clear it in blink::V8AbstractEventListener.
    
    Bug: 872138
    Change-Id: I4f8e8e67074ee8b2081735053b5b0f7604afadff
    Reviewed-on: https://chromium-review.googlesource.com/1173195
    Commit-Queue: Yuki Yamada <yukiy@google.com>
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582819}

--- third_party/blink/renderer/bindings/core/v8/v8_abstract_event_listener.cc
+++ third_party/blink/renderer/bindings/core/v8/v8_abstract_event_listener.cc
@@ -71,8 +71,6 @@ V8AbstractEventListener::~V8AbstractEventListener() {
     DCHECK(listener_.IsEmpty());
     InstanceCounters::DecrementCounter(
         InstanceCounters::kJSEventListenerCounter);
-  } else {
-    listener_.Clear();
   }
 }
 
