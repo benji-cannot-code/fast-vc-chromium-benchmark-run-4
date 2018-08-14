4bf25ed27b7f6666299b51a8133366f152e5cf87
Lei Zhang <thestig@chromium.org>
2018-08-14 01:03:16 +0000

    Fix DCHECK failure in PrintRenderFrameHelper::PrintPreviewContext.
    Accept the "DONE" state as a valid state for failing, since after the
    rendering is done, there is still work to do and that work can
    potentially fail.
    
    BUG=872980
    
    Change-Id: If1eb31c57057b2e6354dc4649ec7806468fa3f1d
    Reviewed-on: https://chromium-review.googlesource.com/1170199
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582775}

--- components/printing/renderer/print_render_frame_helper.cc
+++ components/printing/renderer/print_render_frame_helper.cc
@@ -2307,7 +2307,7 @@ void PrintRenderFrameHelper::PrintPreviewContext::Finished() {
 }
 
 void PrintRenderFrameHelper::PrintPreviewContext::Failed(bool report_error) {
-  DCHECK(state_ == INITIALIZED || state_ == RENDERING);
+  DCHECK(state_ != UNINITIALIZED);
   state_ = INITIALIZED;
   if (report_error) {
     DCHECK_NE(PREVIEW_ERROR_NONE, error_);
