071e8643507a1a1a570fe1bf3f27b5abb96dae23
Jian Li <jianli@chromium.org>
2018-08-14 00:05:44 +0000

    Reland: Report different UMA enums when clicking async download notification
    Currently it is reported as "Download Home". We now add the logic
    to separate it out, together with the case that clicking from progress
    bar.
    
    This was previously reviewed at
    https://chromium-review.googlesource.com/c/chromium/src/+/1157644
    
    It was landed and reverted due to test failure. I am relanding it to fix
    the test failure.
    
    Bug: 869692
    Change-Id: I9bfe0a2b1fc0e984fe258c3c28be3491fe6bca99
    Tbr: dgn@chromium.org
    Tbr: dimich@chromium.org
    Tbr: peter@chromium.org
    Tbr: holte@chromium.org
    Tbr: qinmin@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/1173268
    Reviewed-by: Jian Li <jianli@chromium.org>
    Commit-Queue: Jian Li <jianli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582759}

--- components/offline_pages/core/downloads/download_ui_adapter.h
+++ components/offline_pages/core/downloads/download_ui_adapter.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/gurl.h"
 
 using ContentId = offline_items_collection::ContentId;
+using LaunchLocation = offline_items_collection::LaunchLocation;
 using OfflineItem = offline_items_collection::OfflineItem;
 using OfflineContentProvider = offline_items_collection::OfflineContentProvider;
 using OfflineContentAggregator =
@@ -58,7 +59,9 @@ class DownloadUIAdapter : public OfflineContentProvider,
     virtual void SetUIAdapter(DownloadUIAdapter* ui_adapter) = 0;
 
     // Opens an offline item.
-    virtual void OpenItem(const OfflineItem& item, int64_t offline_id) = 0;
+    virtual void OpenItem(const OfflineItem& item,
+                          int64_t offline_id,
+                          LaunchLocation launch_location) = 0;
 
     // Suppresses the download complete notification
     // depending on flags and origin.
@@ -81,7 +84,7 @@ class DownloadUIAdapter : public OfflineContentProvider,
       OfflinePageModel* model);
 
   // OfflineContentProvider implementation.
-  void OpenItem(const ContentId& id) override;
+  void OpenItem(LaunchLocation location, const ContentId& id) override;
   void RemoveItem(const ContentId& id) override;
   void CancelDownload(const ContentId& id) override;
   void PauseDownload(const ContentId& id) override;
@@ -152,7 +155,8 @@ class DownloadUIAdapter : public OfflineContentProvider,
       OfflineContentProvider::SingleItemCallback callback,
       std::vector<std::unique_ptr<SavePageRequest>> requests);
 
-  void OnPageGetForOpenItem(const OfflinePageItem* page);
+  void OnPageGetForOpenItem(LaunchLocation location,
+                            const OfflinePageItem* page);
   void OnPageGetForThumbnailAdded(const OfflinePageItem* page);
 
   void OnDeletePagesDone(DeletePageResult result);
