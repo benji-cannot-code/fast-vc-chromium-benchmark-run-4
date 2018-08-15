865ce5535786332b1173836c0004046f69d60a3f
David Bertoni <dbertoni@chromium.org>
2018-08-15 00:07:20 +0000

    [EXTENSIONS] Use small_map instead of std::map for the manifest handler registry.
    Add a perf unit test and a unit test to make sure the small_map doesn't overflow.
    
    Bug: 847237
    Change-Id: I56bd58b75dacf42abb32b6ce091e5930ceebd203
    Reviewed-on: https://chromium-review.googlesource.com/1166190
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583094}

--- chrome/common/extensions/chrome_extensions_client_unittest.cc
+++ chrome/common/extensions/chrome_extensions_client_unittest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/extension.h"
 #include "extensions/common/file_util.h"
 #include "extensions/common/manifest.h"
+#include "extensions/common/manifest_handler.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace extensions {
@@ -79,6 +80,15 @@ TEST_F(ChromeExtensionsClientTest, CheckZeroLengthActionIconFiles) {
   EXPECT_FALSE(extension3.get());
   EXPECT_STREQ("Could not load icon 'icon.png' for page action.",
                error.c_str());
+}
+
+// Test that the ManifestHandlerRegistry handler map hasn't overflowed.
+// If this test fails, increase ManifestHandlerRegistry::kHandlerMax.
+TEST_F(ChromeExtensionsClientTest, CheckManifestHandlerRegistryForOverflow) {
+  ManifestHandlerRegistry* registry = ManifestHandlerRegistry::Get();
+  ASSERT_TRUE(registry);
+  ASSERT_LT(0u, registry->handlers_.size());
+  EXPECT_LE(registry->handlers_.size(), ManifestHandlerRegistry::kHandlerMax);
 }
 
 }  // namespace extensions
