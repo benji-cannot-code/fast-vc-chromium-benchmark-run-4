93f8c6920fd2911dedd778e0c4520ee577e931d0
Mike Wasserman <msw@chromium.org>
2018-08-15 19:38:39 +0000

    ws: Fix capture release on window destruction
    The capture window should only be cleared if it's part of the client.
    
    Fixes the case where dragging a touch edit handle closes the context menu.
    (the context menu closing shouldn't reset client-side touch handle capture)
    (otherwise, capture release isn't sent to the ws when ending the touch drag)
    
    Bug: 873743
    Test: Automated, KSV (Ctrl-Alt-/) touch text edit handles work.
    Change-Id: I4e28115de92f7b8cb0355b6a9e4c0b470634ad2b
    Reviewed-on: https://chromium-review.googlesource.com/1176073
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583358}

--- services/ui/ws2/server_window.cc
+++ services/ui/ws2/server_window.cc
@@ -472,7 +472,7 @@ void ServerWindow::SetCaptureOwner(WindowTree* owner) {
   if (!IsTopLevel())
     return;
 
-  return static_cast<TopLevelEventHandler*>(event_handler_.get())
+  static_cast<TopLevelEventHandler*>(event_handler_.get())
       ->OnCaptureOwnerChanged();
 }
 
