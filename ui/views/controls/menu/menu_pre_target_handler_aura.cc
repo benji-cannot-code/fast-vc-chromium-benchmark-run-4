3d5b0a25cc58f4c4996b12dad2508a018d4b9c10
Elly Fong-Jones <ellyjones@chromium.org>
2018-08-15 15:30:12 +0000

    views: fix Mac menu key handling
    This change:
    
    1) Adds logic to EventMonitorMac so that if an event is marked handled by the
       client of EventMonitorMac, the event is not handled further - i.e.,
       EventMonitorMac can now eat events;
    2) Renames the existing MenuPreTargetHandler to MenuPreTargetHandlerAura;
    3) Adds MenuPreTargetHandlerMac which uses EventMonitorMac;
    4) Adds MenuPreTargetHandler::Create to manufacture the platform-appropriate
       MenuPreTargetHandler instance.
    
    Bug: 867127
    Change-Id: I33f7c750598b0496fa291434098f8019e4b99c47
    Reviewed-on: https://chromium-review.googlesource.com/1174568
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583254}

--- ui/views/controls/menu/menu_pre_target_handler_aura.cc
+++ ui/views/controls/menu/menu_pre_target_handler_aura.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "ui/views/controls/menu/menu_pre_target_handler.h"
+#include "ui/views/controls/menu/menu_pre_target_handler_aura.h"
 
 #include "ui/aura/env.h"
 #include "ui/aura/window.h"
@@ -21,8 +21,8 @@ aura::Window* GetOwnerRootWindow(views::Widget* owner) {
 
 }  // namespace
 
-MenuPreTargetHandler::MenuPreTargetHandler(MenuController* controller,
-                                           Widget* owner)
+MenuPreTargetHandlerAura::MenuPreTargetHandlerAura(MenuController* controller,
+                                                   Widget* owner)
     : controller_(controller), root_(GetOwnerRootWindow(owner)) {
   aura::Env::GetInstance()->AddPreTargetHandler(
       this, ui::EventTarget::Priority::kSystem);
@@ -32,12 +32,12 @@ MenuPreTargetHandler::MenuPreTargetHandler(MenuController* controller,
   }
 }
 
-MenuPreTargetHandler::~MenuPreTargetHandler() {
+MenuPreTargetHandlerAura::~MenuPreTargetHandlerAura() {
   aura::Env::GetInstance()->RemovePreTargetHandler(this);
   Cleanup();
 }
 
-void MenuPreTargetHandler::OnWindowActivated(
+void MenuPreTargetHandlerAura::OnWindowActivated(
     wm::ActivationChangeObserver::ActivationReason reason,
     aura::Window* gained_active,
     aura::Window* lost_active) {
@@ -45,19 +45,19 @@ void MenuPreTargetHandler::OnWindowActivated(
     controller_->CancelAll();
 }
 
-void MenuPreTargetHandler::OnWindowDestroying(aura::Window* window) {
+void MenuPreTargetHandlerAura::OnWindowDestroying(aura::Window* window) {
   Cleanup();
 }
 
-void MenuPreTargetHandler::OnCancelMode(ui::CancelModeEvent* event) {
+void MenuPreTargetHandlerAura::OnCancelMode(ui::CancelModeEvent* event) {
   controller_->CancelAll();
 }
 
-void MenuPreTargetHandler::OnKeyEvent(ui::KeyEvent* event) {
+void MenuPreTargetHandlerAura::OnKeyEvent(ui::KeyEvent* event) {
   controller_->OnWillDispatchKeyEvent(event);
 }
 
-void MenuPreTargetHandler::Cleanup() {
+void MenuPreTargetHandlerAura::Cleanup() {
   if (!root_)
     return;
   // The ActivationClient may have been destroyed by the time we get here.
@@ -66,6 +66,13 @@ void MenuPreTargetHandler::Cleanup() {
     client->RemoveObserver(this);
   root_->RemoveObserver(this);
   root_ = nullptr;
+}
+
+// static
+std::unique_ptr<MenuPreTargetHandler> MenuPreTargetHandler::Create(
+    MenuController* controller,
+    Widget* owner) {
+  return std::make_unique<MenuPreTargetHandlerAura>(controller, owner);
 }
 
 }  // namespace views
---
+++ ui/views/controls/menu/menu_pre_target_handler_mac.mm
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/views/controls/menu/menu_pre_target_handler_mac.h"
+
+#include "ui/views/controls/menu/menu_controller.h"
+#include "ui/views/widget/widget.h"
+
+namespace views {
+
+MenuPreTargetHandlerMac::MenuPreTargetHandlerMac(MenuController* controller,
+                                                 Widget* widget)
+    : controller_(controller),
+      event_monitor_(
+          std::make_unique<EventMonitorMac>(this, widget->GetNativeWindow())) {}
+
+MenuPreTargetHandlerMac::~MenuPreTargetHandlerMac() {}
+
+void MenuPreTargetHandlerMac::OnKeyEvent(ui::KeyEvent* event) {
+  if (controller_->OnWillDispatchKeyEvent(event) !=
+      ui::POST_DISPATCH_PERFORM_DEFAULT) {
+    event->SetHandled();
+  }
+}
+
+// static
+std::unique_ptr<MenuPreTargetHandler> MenuPreTargetHandler::Create(
+    MenuController* controller,
+    Widget* widget) {
+  return std::make_unique<MenuPreTargetHandlerMac>(controller, widget);
+}
+
+}  // namespace views
