89574553e91896da59a6d2bef775e19c220b1a9b
Momoko Sumida <momon@google.com>
2018-08-15 07:49:22 +0000

    Revert "Revert "Delete constructor for creating empty CSSStyleSheets""
    This reverts commit 2472878c1953dba5360a183647d077cd1634db81.
    
    The following CL was reverted as a suspect for https://crbug.com/873015:
    crrev.com/c/1164876
    
    This CL reverts the revert as the failing test is not related to CSSStyleSheet.
    The flaky test has been disabled in crrev.com/c/1171681
    
    Bug: 873015
    Change-Id: I54cefa72e4f82b1075550d6efb4a0ca8efe98383
    Reviewed-on: https://chromium-review.googlesource.com/1174150
    Commit-Queue: Momoko Sumida <momon@google.com>
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583191}

--- third_party/blink/renderer/core/css/css_style_sheet.h
+++ third_party/blink/renderer/core/css/css_style_sheet.h
@@ -54,7 +54,6 @@ class CORE_EXPORT CSSStyleSheet final : public StyleSheet {
  public:
   static const Document* SingleOwnerDocument(const CSSStyleSheet*);
 
-  static CSSStyleSheet* Create(Document&, ExceptionState&);
   static CSSStyleSheet* Create(Document&,
                                const CSSStyleSheetInit&,
                                ExceptionState&);
@@ -197,6 +196,11 @@ class CORE_EXPORT CSSStyleSheet final : public StyleSheet {
   FRIEND_TEST_ALL_PREFIXES(
       CSSStyleSheetTest,
       CSSStyleSheetConstructionWithNonEmptyCSSStyleSheetInit);
+  FRIEND_TEST_ALL_PREFIXES(CSSStyleSheetTest,
+                           CreateEmptyCSSStyleSheetWithEmptyCSSStyleSheetInit);
+  FRIEND_TEST_ALL_PREFIXES(
+      CSSStyleSheetTest,
+      CreateEmptyCSSStyleSheetWithNonEmptyCSSStyleSheetInit);
   FRIEND_TEST_ALL_PREFIXES(
       CSSStyleSheetTest,
       CreateCSSStyleSheetWithEmptyCSSStyleSheetInitAndText);
