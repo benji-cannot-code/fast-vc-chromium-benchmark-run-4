509fac39b11262825d03f1f9fef806c137e181e3
Lei Zhang <thestig@chromium.org>
2018-08-15 18:15:51 +0000

    Remove chrome::FILE_NACL_PLUGIN.
    The NaCl plugin is built into the Chromium binary, so it does not have a
    real path. Instead of calling PathService::Get(chrome::FILE_NACL_PLUGIN)
    to get the fake path, just use a constant like the PDF plugin.
    
    Change-Id: I7487f6be7ddbdbecf6255b6faa5b104593e6193e
    Reviewed-on: https://chromium-review.googlesource.com/1175546
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Justin TerAvest <teravest@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583328}

--- chrome/browser/extensions/extension_nacl_browsertest.cc
+++ chrome/browser/extensions/extension_nacl_browsertest.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/files/file_path.h"
-#include "base/path_service.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/bind_test_util.h"
 #include "chrome/browser/extensions/crx_installer.h"
@@ -14,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
-#include "chrome/common/chrome_paths.h"
+#include "chrome/common/chrome_content_client.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/prefs/pref_service.h"
@@ -116,22 +115,19 @@ class NaClExtensionTest : public extensions::ExtensionBrowserTest {
   }
 
   bool IsNaClPluginLoaded() {
-    base::FilePath path;
-    if (base::PathService::Get(chrome::FILE_NACL_PLUGIN, &path)) {
-      // Make sure plugins are loaded off disk first.
-      {
-        base::RunLoop run_loop;
-        PluginService::GetInstance()->GetPlugins(base::BindLambdaForTesting(
-            [&](const std::vector<content::WebPluginInfo>&) {
-              run_loop.Quit();
-            }));
-        run_loop.Run();
-      }
-
-      content::WebPluginInfo info;
-      return PluginService::GetInstance()->GetPluginInfoByPath(path, &info);
+    // Make sure plugins are loaded off disk first.
+    {
+      base::RunLoop run_loop;
+      PluginService::GetInstance()->GetPlugins(base::BindLambdaForTesting(
+          [&](const std::vector<content::WebPluginInfo>&) {
+            run_loop.Quit();
+          }));
+      run_loop.Run();
     }
-    return false;
+
+    static const base::FilePath path(ChromeContentClient::kNaClPluginFileName);
+    content::WebPluginInfo info;
+    return PluginService::GetInstance()->GetPluginInfoByPath(path, &info);
   }
 
   void CheckPluginsCreated(const GURL& url, PluginType expected_to_succeed) {
