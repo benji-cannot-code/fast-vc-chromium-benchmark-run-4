5055ceb33050c4170da27abe3c4b78a2e744ea28
Jeffrey Cohen <jeffreycohen@chromium.org>
2018-08-15 20:49:17 +0000

    Add feature flag for deprecating company name
    Bug: 864612
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I5781f919f5903b78f5e922ad9774f9734513de97
    Reviewed-on: https://chromium-review.googlesource.com/1173154
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Jeffrey Cohen <jeffreycohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583384}

--- components/autofill/core/browser/contact_info.cc
+++ components/autofill/core/browser/contact_info.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "components/autofill/core/browser/autofill_data_util.h"
 #include "components/autofill/core/browser/autofill_type.h"
+#include "components/autofill/core/common/autofill_features.h"
 #include "components/autofill/core/common/autofill_l10n_util.h"
 
 namespace autofill {
@@ -241,7 +242,8 @@ void CompanyInfo::GetSupportedTypes(ServerFieldTypeSet* supported_types) const {
 }
 
 base::string16 CompanyInfo::GetRawInfo(ServerFieldType type) const {
-  if (type == COMPANY_NAME)
+  if (type == COMPANY_NAME &&
+      base::FeatureList::IsEnabled(features::kAutofillEnableCompanyName))
     return company_name_;
 
   return base::string16();
