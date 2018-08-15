d05a9d5d11b8841fe3a14d81c39f5fc6b1c9af53
Clark DuVall <cduvall@chromium.org>
2018-08-15 01:26:46 +0000

    Fix SecurityExploitBrowserTest to use network service path
    These tests were accessing ResourceMessageFilter directly, instead of
    using the network service path when it is enabled.
    
    Bug: 862176, 874227
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Change-Id: I3add3b588ad40c3bca265ad68adccca293ddae0d
    Reviewed-on: https://chromium-review.googlesource.com/1175105
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583117}

--- content/browser/renderer_host/render_process_host_impl.h
+++ content/browser/renderer_host/render_process_host_impl.h
@@ -445,7 +445,6 @@ class CONTENT_EXPORT RenderProcessHostImpl
   friend class ChildProcessLauncherBrowserTest_ChildSpawnFail_Test;
   friend class VisitRelayingRenderProcessHost;
   friend class StoragePartitonInterceptor;
-  friend class SecurityExploitBrowserTest;
   class ConnectionFilterController;
   class ConnectionFilterImpl;
 
