a2b302075ff3fcf718944801cd18b92b4446e639
Helen Li <xunjieli@chromium.org>
2018-08-15 14:11:35 +0000

    Improve error msg in CronetBufferedOutputStream
    Bug: 874152
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester
    Change-Id: I45441e5fdd8eaa4b0e39144cd94d610d3f98f368
    Reviewed-on: https://chromium-review.googlesource.com/1174935
    Commit-Queue: Paul Jensen <pauljensen@chromium.org>
    Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583240}

--- components/cronet/android/test/javatests/src/org/chromium/net/urlconnection/CronetBufferedOutputStreamTest.java
+++ components/cronet/android/test/javatests/src/org/chromium/net/urlconnection/CronetBufferedOutputStreamTest.java
@@ -93,7 +93,9 @@ public class CronetBufferedOutputStreamTest {
             out.write(TestUtil.UPLOAD_DATA);
             fail();
         } catch (IllegalStateException e) {
-            assertEquals("Cannot write after being connected.", e.getMessage());
+            assertEquals("Use setFixedLengthStreamingMode() or "
+                            + "setChunkedStreamingMode() for writing after connect",
+                    e.getMessage());
         }
     }
 
