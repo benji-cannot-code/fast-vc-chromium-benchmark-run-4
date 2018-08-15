29066d9abaf2c6386c048cedbd730f216905bbe5
Patrick Noland <pnoland@google.com>
2018-08-15 17:12:25 +0000

    [Feed] Import feed@a4e91c7238e329ad11e48b068fe3b1e935c0de9b
    Change-Id: I20cd1fe7eeb2eac30d7913599a8dc1285ae9023e
    
    ******************************Resource Sizes Diff******************************
    Monochrome.apk_Dex
        +1,728 entries fields
        +2,366 entries methods
          +581 entries types
        +1,193 entries strings
    Monochrome.apk_Breakdown (+169,703 bytes)
        +3,241 bytes Zip Overhead
        +1,510 bytes Non-compiled Android resources size
       +12,528 bytes Native code size
          +129 bytes Package metadata size
      +148,695 bytes Java code size
        +3,600 bytes Compiled Android resources size
    Monochrome.apk_Specifics
      +523,907 bytes normalized apk size
            +3 zip entries file count
       +12,528 bytes main lib size
      +354,204 bytes main dex size
    
    Change-Id: I20cd1fe7eeb2eac30d7913599a8dc1285ae9023e
    Reviewed-on: https://chromium-review.googlesource.com/1175218
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583291}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedNewTabPage.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedNewTabPage.java
@@ -16,6 +16,7 @@ import android.widget.FrameLayout;
 
 import com.google.android.libraries.feed.api.scope.FeedProcessScope;
 import com.google.android.libraries.feed.api.scope.FeedStreamScope;
+import com.google.android.libraries.feed.api.stream.NonDismissibleHeader;
 import com.google.android.libraries.feed.api.stream.Stream;
 import com.google.android.libraries.feed.common.functional.Consumer;
 import com.google.android.libraries.feed.host.action.ActionApi;
@@ -214,7 +215,8 @@ public class FeedNewTabPage extends NewTabPage implements TouchEnabledDelegate {
         mStream.getView().setBackgroundColor(Color.WHITE);
         mRootView.addView(mStream.getView());
 
-        mStream.setHeaderViews(Arrays.asList(mNewTabPageLayout, mSectionHeaderView));
+        mStream.setHeaderViews(Arrays.asList(new NonDismissibleHeader(mNewTabPageLayout),
+                new NonDismissibleHeader(mSectionHeaderView)));
     }
 
     @Override
