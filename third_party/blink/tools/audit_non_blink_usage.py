439bfda93155742584ca9c1611ceb1b4bd1bf5df
Marijn Kruisselbrink <mek@chromium.org>
2018-08-15 21:22:35 +0000

    [FileSystem] Add initial bits of new FileWriter implementation.
    This adds a WritableFiles RuntimeEnabledFeature, as well as a new
    FileSystemWriter interface (exact name TBD) and a mojo interface the
    implementation uses.
    
    Bug: 872460
    Change-Id: I830a44d5df75fbfb5fbb1c97048a589a4facedc5
    Reviewed-on: https://chromium-review.googlesource.com/1169773
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583400}

--- third_party/blink/tools/audit_non_blink_usage.py
+++ third_party/blink/tools/audit_non_blink_usage.py
@@ -33,6 +33,8 @@ _CONFIG = [
             # //base constructs that are allowed everywhere
             'base::AdoptRef',
             'base::AutoReset',
+            'base::File',
+            'base::FilePath',
             'base::GetUniqueIdForProcess',
             'base::Location',
             'base::MakeRefCounted',
