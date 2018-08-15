e7f1c0a290a2c47d42687a4bbe0c04307e2236e5
Ned Nguyen <nednguyen@google.com>
2018-08-15 22:39:57 +0000

    Add wait time between retry attempts to upload to perf dashboard
    This implements exponential backoff to increase the wait time after each
    retry attempt.
    
    This CL also increase the default number of retry attempts to 4.
    
    Bug:864565
    Cq-Include-Trybots: master.tryserver.chromium.perf:obbs_fyi
    Change-Id: I99c167e1329bbb83d86d310e2acf164cd3f3e7cf
    
    NOTRY=true # linux-chromeos-rel flake
    
    Change-Id: I99c167e1329bbb83d86d310e2acf164cd3f3e7cf
    Reviewed-on: https://chromium-review.googlesource.com/1176089
    Commit-Queue: Ned Nguyen <nednguyen@google.com>
    Reviewed-by: Annie Sullivan <sullivan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583422}

--- tools/perf/core/results_dashboard_unittest.py
+++ tools/perf/core/results_dashboard_unittest.py
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import unittest
 
 import mock
+from mock import call
 
 from core import results_dashboard
 
@@ -24,14 +25,19 @@ class ResultsDashboardTest(unittest.TestCase):
       del token_generator_callback  # unused
       raise results_dashboard.SendResultsRetryException('Should retry')
 
-    with mock.patch('core.results_dashboard._SendHistogramJson',
-                    side_effect=raise_retry_exception) as m:
-      upload_result = results_dashboard.SendResults(
-          self.perf_data, self.dashboard_url, send_as_histograms=True,
-          service_account_file=self.fake_service,
-          token_generator_callback=self.dummy_token_generator, num_retries=5)
-      self.assertFalse(upload_result)
-      self.assertEqual(m.call_count, 5)
+    with mock.patch('core.results_dashboard.time.sleep') as sleep_mock:
+      with mock.patch('core.results_dashboard._SendHistogramJson',
+                      side_effect=raise_retry_exception) as m:
+        upload_result = results_dashboard.SendResults(
+            self.perf_data, 'dummy_benchmark',
+            self.dashboard_url, send_as_histograms=True,
+            service_account_file=self.fake_service,
+            token_generator_callback=self.dummy_token_generator, num_retries=5)
+        self.assertFalse(upload_result)
+        self.assertEqual(m.call_count, 5)
+        self.assertEqual(
+            sleep_mock.mock_calls,
+            [call(30), call(60), call(120), call(240), call(480)])
 
   def testNoRetryForSendResultFatalException(self):
 
@@ -41,25 +47,31 @@ class ResultsDashboardTest(unittest.TestCase):
       del token_generator_callback  # unused
       raise results_dashboard.SendResultsFatalException('Do not retry')
 
-    with mock.patch('core.results_dashboard._SendHistogramJson',
-                    side_effect=raise_retry_exception) as m:
-      upload_result =  results_dashboard.SendResults(
-          self.perf_data, self.dashboard_url, send_as_histograms=True,
-          service_account_file=self.fake_service,
-          token_generator_callback=self.dummy_token_generator,
-          num_retries=5)
-      self.assertFalse(upload_result)
-      self.assertEqual(m.call_count, 1)
+    with mock.patch('core.results_dashboard.time.sleep') as sleep_mock:
+      with mock.patch('core.results_dashboard._SendHistogramJson',
+                      side_effect=raise_retry_exception) as m:
+        upload_result =  results_dashboard.SendResults(
+            self.perf_data, 'dummy_benchmark',
+            self.dashboard_url, send_as_histograms=True,
+            service_account_file=self.fake_service,
+            token_generator_callback=self.dummy_token_generator,
+            num_retries=5)
+        self.assertFalse(upload_result)
+        self.assertEqual(m.call_count, 1)
+        self.assertFalse(sleep_mock.mock_calls)
 
   def testNoRetryForSuccessfulSendResult(self):
-    with mock.patch('core.results_dashboard._SendHistogramJson') as m:
-      upload_result = results_dashboard.SendResults(
-          self.perf_data, self.dashboard_url, send_as_histograms=True,
-          service_account_file=self.fake_service,
-          token_generator_callback=self.dummy_token_generator,
-          num_retries=5)
-      self.assertTrue(upload_result)
-      self.assertEqual(m.call_count, 1)
+    with mock.patch('core.results_dashboard.time.sleep') as sleep_mock:
+      with mock.patch('core.results_dashboard._SendHistogramJson') as m:
+        upload_result = results_dashboard.SendResults(
+            self.perf_data, 'dummy_benchmark',
+            self.dashboard_url, send_as_histograms=True,
+            service_account_file=self.fake_service,
+            token_generator_callback=self.dummy_token_generator,
+            num_retries=5)
+        self.assertTrue(upload_result)
+        self.assertEqual(m.call_count, 1)
+        self.assertFalse(sleep_mock.mock_calls)
 
   def testNoRetryAfterSucessfulSendResult(self):
     counter = [0]
@@ -71,12 +83,16 @@ class ResultsDashboardTest(unittest.TestCase):
       if counter[0] <= 2:
         raise results_dashboard.SendResultsRetryException('Please retry')
 
-    with mock.patch('core.results_dashboard._SendHistogramJson',
-                    side_effect=raise_retry_exception_first_two_times) as m:
-      upload_result = results_dashboard.SendResults(
-          self.perf_data, self.dashboard_url, send_as_histograms=True,
-          service_account_file=self.fake_service,
-          token_generator_callback=self.dummy_token_generator,
-          num_retries=5)
-      self.assertTrue(upload_result)
-      self.assertEqual(m.call_count, 3)
+    with mock.patch('core.results_dashboard.time.sleep') as sleep_mock:
+      with mock.patch('core.results_dashboard._SendHistogramJson',
+                      side_effect=raise_retry_exception_first_two_times) as m:
+        upload_result = results_dashboard.SendResults(
+            self.perf_data, 'dummy_benchmark',
+            self.dashboard_url, send_as_histograms=True,
+            service_account_file=self.fake_service,
+            token_generator_callback=self.dummy_token_generator,
+            num_retries=5)
+        self.assertTrue(upload_result)
+        self.assertEqual(m.call_count, 3)
+        self.assertEqual(
+            sleep_mock.mock_calls, [call(30), call(60)])
