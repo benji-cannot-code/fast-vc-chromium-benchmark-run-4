983cfa05a7dc691f32cbf05290a3521b305dd135
Khushal <khushalsagar@chromium.org>
2018-08-15 07:19:58 +0000

    cc: Guard GrContext access for upload tasks in the GPU image cache.
    The RasterInterface should be notified when accessing the GrContext, so
    it can make sure any state tracked in the implementation is reset if it
    could be modified by using the GrContext. We are already doing this for
    raster tasks in GpuRasterBufferProvider, also do this in upload tasks
    which use the GrContext for uploading images.
    
    R=ericrk@chromium.org
    
    Bug: 870317
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I2b5039f2a2f64fb6986181639b81597c741103a5
    Reviewed-on: https://chromium-review.googlesource.com/1175415
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583188}

--- cc/tiles/gpu_image_decode_cache.cc
+++ cc/tiles/gpu_image_decode_cache.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/base/devtools_instrumentation.h"
 #include "cc/base/histograms.h"
 #include "cc/paint/image_transfer_cache_entry.h"
+#include "cc/raster/scoped_grcontext_access.h"
 #include "cc/raster/tile_task.h"
 #include "cc/tiles/mipmap_util.h"
 #include "components/viz/common/gpu/raster_context_provider.h"
@@ -1112,7 +1113,11 @@ void GpuImageDecodeCache::UploadImageInTask(const DrawImage& draw_image) {
   if (context_->GetLock())
     context_lock.emplace(context_);
 
+  base::Optional<ScopedGrContextAccess> gr_context_access;
+  if (!use_transfer_cache_)
+    gr_context_access.emplace(context_);
   base::AutoLock lock(lock_);
+
   ImageData* image_data = GetImageDataForDrawImage(
       draw_image, InUseCacheKey::FromDrawImage(draw_image));
   DCHECK(image_data);
