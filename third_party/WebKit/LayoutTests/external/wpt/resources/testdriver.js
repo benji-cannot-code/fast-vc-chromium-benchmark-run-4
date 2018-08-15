846524f07a15a4324dca5ba84916b30dd4c1c764
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-08-15 09:40:24 +0000

    Import wpt@7985ccfee21f815f20a7e8699009f76cebbcea66
    Using wpt-import in Chromium 0ecac34302d2d95059c3ea716f118c0b1cfcedd2.
    With Chromium commits locally applied on WPT:
    7dad0f1723 "Added new Sec-Metadata Web Platform Tests."
    
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/22749
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    jsbell@chromium.org:
      external/wpt/resources
    
    NOAUTOREVERT=true
    TBR=raphael.kubo.da.costa@intel.com
    
    No-Export: true
    Change-Id: I953afb89497304a427dcba8b844eb18e792dd90d
    Reviewed-on: https://chromium-review.googlesource.com/1175559
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583205}

--- third_party/WebKit/LayoutTests/external/wpt/resources/testdriver.js
+++ third_party/WebKit/LayoutTests/external/wpt/resources/testdriver.js
@@ -68,7 +68,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             button.innerHTML = "This test requires user interaction.<br />" +
                 "Please click here to allow " + intent + ".";
             button.id = "wpt-test-driver-bless-" + (idCounter += 1);
-            document.body.appendChild(button);
+            const elem = document.body || document.documentElement;
+            elem.appendChild(button);
 
             return new Promise(function(resolve, reject) {
                     button.addEventListener("click", resolve);
