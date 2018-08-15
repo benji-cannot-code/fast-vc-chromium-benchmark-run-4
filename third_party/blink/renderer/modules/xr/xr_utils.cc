e9f8553479c9c2aaff7bd743a0fe12cb33144d03
Max Rebuschatis <lincolnfrog@chromium.org>
2018-08-15 20:38:29 +0000

    Rename transformationMatrixToFloat32Array
    Rename transformationMatrixToFloat32Array
    to transformationMatrixToDOMFloat32Array
    to more accurately reflect the type it
    returns.
    
    Bug: 845296
    Change-Id: Iedea31c7b8335795657bf582215ed566861b4127
    Reviewed-on: https://chromium-review.googlesource.com/1171830
    Reviewed-by: Bill Orr <billorr@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Max Rebuschatis <lincolnfrog@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583377}

--- third_party/blink/renderer/modules/xr/xr_utils.cc
+++ third_party/blink/renderer/modules/xr/xr_utils.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-DOMFloat32Array* transformationMatrixToFloat32Array(
+DOMFloat32Array* transformationMatrixToDOMFloat32Array(
     const TransformationMatrix& matrix) {
   float array[] = {
       static_cast<float>(matrix.M11()), static_cast<float>(matrix.M12()),
