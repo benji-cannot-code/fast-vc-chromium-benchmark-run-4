e7fa4595a7c4d11d3c6bbe671e25742c4cb14477
Tom Anderson <thomasanderson@chromium.org>
2018-08-15 02:47:05 +0000

    Repurpose the confirm-quit-bubble to a quit-instruction-bubble
    It was decided in [1] that the Ctrl+Shift+Q shortcut should be removed
    altogether and replaced with a bubble giving an alternative way to quit using
    the keyboard (similar to the "Press Alt+Left to go back" bubble).
    
    [1] https://docs.google.com/document/d/1mvTBrxD58GIsqFiqgiLCZiXy1iOtDpQx_Tp5OSuYWEU/edit?usp=sharing
    
    BUG=243164
    R=sky
    
    Change-Id: I47578d6b750c45fbc5d0a6e6ffe7e5f528dbe80d
    Reviewed-on: https://chromium-review.googlesource.com/1173654
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583138}

--- chrome/common/chrome_features.h
+++ chrome/common/chrome_features.h
@@ -203,10 +203,6 @@ extern const base::Feature kMacMaterialDesignDownloadShelf;
 extern const base::Feature kAcknowledgeNtpOverrideOnDeactivate;
 #endif
 
-#if defined(OS_WIN) || (defined(OS_LINUX) && !defined(OS_CHROMEOS))
-extern const base::Feature kWarnBeforeQuitting;
-#endif
-
 extern const base::Feature kMaterialDesignIncognitoNTP;
 
 extern const base::Feature kModalPermissionPrompts;
