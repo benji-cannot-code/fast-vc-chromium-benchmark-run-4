06eac9b9dea7d2dfe24dcfd9ce887ec2766595dd
Xida Chen <xidachen@chromium.org>
2018-08-15 18:53:48 +0000

    Disable ChromeLauncherControllerWithArcTest.ArcCustomAppIcon
    It has been flaky for a while
    
    TBR=wjmaclean@chromium.org
    NOTRY=true
    
    Bug: 874538
    Change-Id: Ia598f4367f0d90ff407395ffe69fb96ed707b59c
    Reviewed-on: https://chromium-review.googlesource.com/1176208
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583343}

--- chrome/browser/ui/ash/launcher/chrome_launcher_controller_unittest.cc
+++ chrome/browser/ui/ash/launcher/chrome_launcher_controller_unittest.cc
@@ -2410,7 +2410,7 @@ TEST_P(ChromeLauncherControllerWithArcTest, ArcAppPinOptOutOptIn) {
             GetPinnedAppStatus());
 }
 
-TEST_P(ChromeLauncherControllerWithArcTest, ArcCustomAppIcon) {
+TEST_P(ChromeLauncherControllerWithArcTest, DISABLED_ArcCustomAppIcon) {
   InitLauncherController();
 
   TestShelfController* shelf_controller =
