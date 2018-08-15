2dd5cc966f7cba5b97de0f3cb43886f4d36af9c0
Nektarios Paisios <nektar@chromium.org>
2018-08-15 21:11:56 +0000

    Added a test for shrinking or extending the selection in ARIA hidden and one for list bullets
    R=dmazzoni@chromium.org
    
    Change-Id: Icecff9eece2167e4678dca0aba1f0d0e08a457b0
    Reviewed-on: https://chromium-review.googlesource.com/1175032
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583391}

--- third_party/blink/renderer/modules/accessibility/testing/accessibility_selection_test.cc
+++ third_party/blink/renderer/modules/accessibility/testing/accessibility_selection_test.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/modules/accessibility/testing/accessibility_selection_test.h"
 
+#include "third_party/blink/renderer/core/dom/character_data.h"
+#include "third_party/blink/renderer/core/dom/container_node.h"
 #include "third_party/blink/renderer/core/dom/node.h"
 #include "third_party/blink/renderer/core/html/html_element.h"
 #include "third_party/blink/renderer/modules/accessibility/ax_object.h"
@@ -145,7 +147,9 @@ class AXSelectionSerializer final {
 // Deserializes an HTML snippet with or without selection markers to an
 // accessibility tree. A '^' could be present at the selection anchor offset and
 // a '|' at the focus offset. If multiple markers are present, the deserializer
-// will DCHECK. If there are no markers, no selection will be made.
+// will DCHECK. If there are no markers, no selection will be made. We don't
+// allow '^' and '|' markers to appear in anything other than the contents of an
+// HTML node, e.g. they are not permitted in aria-labels.
 class AXSelectionDeserializer final {
   STACK_ALLOCATED();
 
@@ -157,11 +161,12 @@ class AXSelectionDeserializer final {
   // Creates an accessibility tree rooted at the given HTML element from the
   // provided HTML snippet, selects the part of the tree indicated by the
   // selection markers in the snippet if present, and returns the tree's root.
-  AXObject* Deserialize(const std::string& html_snippet, HTMLElement& element) {
+  const AXSelection Deserialize(const std::string& html_snippet,
+                                HTMLElement& element) {
     element.SetInnerHTMLFromString(String::FromUTF8(html_snippet.c_str()));
     AXObject* root = ax_object_cache_->GetOrCreate(&element);
     if (!root)
-      return nullptr;
+      return AXSelection::Builder().Build();
 
     FindSelectionMarkers(*root);
     if (base_ && extent_) {
@@ -169,32 +174,55 @@ class AXSelectionDeserializer final {
       AXSelection ax_selection =
           builder.SetBase(base_).SetExtent(extent_).Build();
       ax_selection.Select();
+      return ax_selection;
     }
 
-    return root;
+    return AXSelection::Builder().Build();
   }
 
  private:
   void HandleCharacterData(const AXObject& text_object) {
+    CharacterData* const node = ToCharacterData(text_object.GetNode());
     int base_offset = -1;
     int extent_offset = -1;
-    String name = text_object.ComputedName();
-    for (unsigned i = 0; i < name.length(); ++i) {
-      const UChar character = name[i];
+    StringBuilder builder;
+    for (unsigned i = 0; i < node->length(); ++i) {
+      const UChar character = node->data()[i];
       if (character == '^') {
-        DCHECK_EQ(base_offset, -1) << text_object;
+        DCHECK_EQ(base_offset, -1) << "Only one '^' is allowed " << text_object;
         base_offset = static_cast<int>(i);
         continue;
       }
       if (character == '|') {
-        DCHECK_EQ(extent_offset, -1) << text_object;
+        DCHECK_EQ(extent_offset, -1)
+            << "Only one '|' is allowed " << text_object;
         extent_offset = static_cast<int>(i);
         continue;
       }
+      builder.Append(character);
     }
 
+    LOG(ERROR) << "Nektar\n" << base_offset << extent_offset;
     if (base_offset == -1 && extent_offset == -1)
       return;
+
+    // Remove the markers, otherwise they would be duplicated if the AX
+    // selection is re-serialized.
+    node->setData(builder.ToString());
+
+    if (node->length() == 0) {
+      // Since the text object contains only selection markers, this indicates
+      // that this is a request for a non-text selection.
+      if (base_offset >= 0)
+        base_ = AXPosition::CreatePositionBeforeObject(text_object);
+      if (extent_offset >= 0)
+        extent_ = AXPosition::CreatePositionBeforeObject(text_object);
+
+      ContainerNode* const parent_node = node->parentNode();
+      DCHECK(parent_node);
+      parent_node->removeChild(node);
+      return;
+    }
 
     if (base_offset >= 0)
       base_ = AXPosition::CreatePositionInTextObject(text_object, base_offset);
@@ -246,16 +274,16 @@ std::string AccessibilitySelectionTest::GetSelectionText(
   return AXSelectionSerializer(selection).Serialize(subtree);
 }
 
-AXObject* AccessibilitySelectionTest::SetSelectionText(
+const AXSelection AccessibilitySelectionTest::SetSelectionText(
     const std::string& selection_text) const {
   HTMLElement* body = GetDocument().body();
   if (!body)
-    return nullptr;
+    return AXSelection::Builder().Build();
   return AXSelectionDeserializer(GetAXObjectCache())
       .Deserialize(selection_text, *body);
 }
 
-AXObject* AccessibilitySelectionTest::SetSelectionText(
+const AXSelection AccessibilitySelectionTest::SetSelectionText(
     const std::string& selection_text,
     HTMLElement& element) const {
   return AXSelectionDeserializer(GetAXObjectCache())
