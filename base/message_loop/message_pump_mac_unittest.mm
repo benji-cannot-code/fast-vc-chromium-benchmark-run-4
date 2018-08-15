7c691e9ab719ad6c7af9e8fe9dc28f752cb7c4c3
Sidney San Martín <sdy@chromium.org>
2018-08-15 19:42:17 +0000

    Whitelist the run loop mode that AppKit uses to animate the menu in fullscreen.
    This stops UI animation and video from freezing during the animation.
    Also move some constants to test-accessible location and delete the
    duplicates in the tests.
    
    Bug: 554993
    Change-Id: Ie4585fa405b32e8bdae984d5a4ffac700c1375ab
    Reviewed-on: https://chromium-review.googlesource.com/1166169
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Sidney San Martín <sdy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583361}

--- base/message_loop/message_pump_mac_unittest.mm
+++ base/message_loop/message_pump_mac_unittest.mm
@@ -17,14 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)runTestThenCloseAlert:(NSAlert*)alert;
 @end
 
-namespace {
-
-// Internal constants from message_pump_mac.mm.
-constexpr int kAllModesMask = 0xf;
-constexpr int kNSApplicationModalSafeModeMask = 0x3;
-
-}  // namespace
-
 namespace base {
 
 class TestMessagePumpCFRunLoopBase {
@@ -193,7 +185,8 @@ TEST(MessagePumpMacTest, ScopedPumpMessagesAttemptWithModalDialog) {
   {
     base::ScopedPumpMessagesInPrivateModes allow_private;
     // No modal window, so all modes should be pumped.
-    EXPECT_EQ(kAllModesMask, allow_private.GetModeMaskForTest());
+    EXPECT_EQ(MessagePumpCFRunLoopBase::kAllModesMask,
+              allow_private.GetModeMaskForTest());
   }
 
   base::scoped_nsobject<NSAlert> alert([[NSAlert alloc] init]);
@@ -217,7 +210,7 @@ TEST(MessagePumpMacTest, ScopedPumpMessagesAttemptWithModalDialog) {
   {
     base::ScopedPumpMessagesInPrivateModes allow_private;
     // With a modal window, only safe modes should be pumped.
-    EXPECT_EQ(kNSApplicationModalSafeModeMask,
+    EXPECT_EQ(base::MessagePumpCFRunLoopBase::kNSApplicationModalSafeModeMask,
               allow_private.GetModeMaskForTest());
   }
   [[alert buttons][0] performClick:nil];
