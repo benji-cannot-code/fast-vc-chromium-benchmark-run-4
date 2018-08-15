48ef43a98d62c198bcb69e8cfb1335cadb099a6d
Mike Dougherty <michaeldo@chromium.org>
2018-08-15 21:13:46 +0000

    [iOS] Remove Captive Portal interstitial flag.
    Bug: 862323
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ida6501b7c176b227be691c1120298eb563fd71bd
    Reviewed-on: https://chromium-review.googlesource.com/1176116
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583393}

--- ios/chrome/browser/ssl/captive_portal_features.h
+++ ios/chrome/browser/ssl/captive_portal_features.h
@@ -8,9 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 
-// Used to control the state of the Captive Portal Login feature.
-extern const base::Feature kCaptivePortalFeature;
-
 // Used to control the state of logging Captive Portal Metrics.
 extern const base::Feature kCaptivePortalMetrics;
 
