ceb8c3643fbfac2f37ad32117b568f29844bca30
Xi Cheng <chengx@chromium.org>
2018-08-15 01:00:28 +0000

    Replace CallStackProfileBuilder::Module with ModuleCache::Module
    Bug: 851163, 804942
    Change-Id: I4da05d9c4e3a4c12f8b3c851d074fa3cdf97679f
    Reviewed-on: https://chromium-review.googlesource.com/1174953
    Commit-Queue: Xi Cheng <chengx@chromium.org>
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583111}

--- components/metrics/call_stack_profile_builder.cc
+++ components/metrics/call_stack_profile_builder.cc
@@ -5,10 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/metrics/call_stack_profile_builder.h"
 
-#include <stddef.h>
-
-#include <cstring>
-#include <map>
 #include <string>
 #include <utility>
 
@@ -17,8 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/metrics/metrics_hashes.h"
 #include "base/stl_util.h"
-
-using base::StackSamplingProfiler;
 
 namespace metrics {
 
@@ -81,10 +75,9 @@ uint64_t HashModuleFilename(const base::FilePath& filename) {
 
 // Transcode |sample| into |proto_sample|, using base addresses in |modules| to
 // compute module instruction pointer offsets.
-void CopySampleToProto(
-    const CallStackProfileBuilder::Sample& sample,
-    const std::vector<CallStackProfileBuilder::Module>& modules,
-    CallStackProfile::Sample* proto_sample) {
+void CopySampleToProto(const CallStackProfileBuilder::Sample& sample,
+                       const std::vector<base::ModuleCache::Module>& modules,
+                       CallStackProfile::Sample* proto_sample) {
   for (const auto& frame : sample.frames) {
     CallStackProfile::Entry* entry = proto_sample->add_entry();
     // A frame may not have a valid module. If so, we can't compute the
@@ -188,17 +181,6 @@ SampledProfile::TriggerEvent ToSampledProfileTriggerEvent(
 
 }  // namespace
 
-// CallStackProfileBuilder::Module --------------------------------------------
-
-CallStackProfileBuilder::Module::Module() : base_address(0u) {}
-
-CallStackProfileBuilder::Module::Module(uintptr_t base_address,
-                                        const std::string& id,
-                                        const base::FilePath& filename)
-    : base_address(base_address), id(id), filename(filename) {}
-
-CallStackProfileBuilder::Module::~Module() = default;
-
 // CallStackProfileBuilder::Frame ---------------------------------------------
 
 CallStackProfileBuilder::Frame::Frame(uintptr_t instruction_pointer,
@@ -241,8 +223,8 @@ void CallStackProfileBuilder::RecordAnnotations() {
 }
 
 void CallStackProfileBuilder::OnSampleCompleted(
-    std::vector<StackSamplingProfiler::Frame> frames) {
-  // Assemble sample_ first.
+    std::vector<base::StackSamplingProfiler::Frame> frames) {
+  // Assemble sample_ from |frames| first.
   for (const auto& frame : frames) {
     const base::ModuleCache::Module& module(frame.module);
     if (!module.is_valid) {
@@ -251,19 +233,18 @@ void CallStackProfileBuilder::OnSampleCompleted(
       continue;
     }
 
-    // Dedup modules.
+    // Dedup modules and cache them in modules_.
     auto loc = module_index_.find(module.base_address);
     if (loc == module_index_.end()) {
-      modules_.emplace_back(module.base_address, module.id, module.filename);
+      modules_.push_back(module);
       size_t index = modules_.size() - 1;
       loc = module_index_.insert(std::make_pair(module.base_address, index))
                 .first;
     }
-    // convert Frames to Frames.
     sample_.frames.emplace_back(frame.instruction_pointer, loc->second);
   }
 
-  // Write CallStackProfile::Sample protocol buffer message.
+  // Write CallStackProfile::Sample protocol buffer message based on sample_.
   int existing_sample_index = -1;
   auto location = sample_index_.find(sample_);
   if (location != sample_index_.end())
@@ -307,7 +288,7 @@ void CallStackProfileBuilder::OnProfileCompleted(
   module_index_.clear();
   sample_index_.clear();
 
-  // Assemble SampledProfile protocol buffer message and run the associated
+  // Assemble the SampledProfile protocol buffer message and run the associated
   // callback to pass it.
   SampledProfile sampled_profile;
   CallStackProfile* proto_profile =
