a1df533da31221dddf186b1b8f2f49fc5f7e9ede
Dale Curtis <dalecurtis@chromium.org>
2018-08-15 23:32:19 +0000

    Speculative MC fix: Don't call getInputBuffers() after an error.
    See bugs for more detail; this only helps M+ where we're using
    the callback API now and can get the true error status earlier
    than we could before.
    
    This also adds a log message when onError is called so that we
    can debug these more easily in the field.
    
    BUG=873094, 610523
    TEST=none
    
    Change-Id: I3fbae95e872fe430f7dd0ddc70b01552492bd64a
    Reviewed-on: https://chromium-review.googlesource.com/1176168
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583442}

--- media/base/android/java/src/org/chromium/media/MediaCodecBridge.java
+++ media/base/android/java/src/org/chromium/media/MediaCodecBridge.java
@@ -215,6 +215,8 @@ class MediaCodecBridge {
 
         @Override
         public void onError(MediaCodec codec, MediaCodec.CodecException e) {
+            // TODO(dalecurtis): We may want to drop transient errors here.
+            Log.e(TAG, "MediaCodec.onError: " + e.getDiagnosticInfo());
             mMediaCodecBridge.onError(e);
         }
 
@@ -288,7 +290,6 @@ class MediaCodecBridge {
     }
 
     public synchronized void onError(MediaCodec.CodecException e) {
-        // TODO(dalecurtis): We may want to drop transient errors here.
         mPendingError = true;
         mPendingInputBuffers.clear();
         mPendingOutputBuffers.clear();
@@ -479,6 +480,15 @@ class MediaCodecBridge {
     @CalledByNative
     private ByteBuffer getInputBuffer(int index) {
         if (Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT) {
+            if (mUseAsyncApi) {
+                synchronized (this) {
+                    // Prior to Android N, some versions of MediaCodec will
+                    // crash internally if we call getInputBuffer() after an
+                    // error has occurred. See https://crbug.com/610523 and
+                    // https://crbug.com/873094.
+                    if (mPendingError) return null;
+                }
+            }
             try {
                 return mMediaCodec.getInputBuffer(index);
             } catch (IllegalStateException e) {
