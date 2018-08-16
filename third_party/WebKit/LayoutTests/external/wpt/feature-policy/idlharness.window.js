ff9986abcacb48a3c19fdf697ee0f7313095ef28
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-08-16 03:57:24 +0000

    Import wpt@af43e2eb32555059316b67fba4a1d7df6ea3148d
    Using wpt-import in Chromium 54738d34a08ba7be6a0bef44b2e53150396e22f9.
    With Chromium commits locally applied on WPT:
    7dad0f1723 "Added new Sec-Metadata Web Platform Tests."
    
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/22766
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    TBR=raphael.kubo.da.costa@intel.com
    
    No-Export: true
    Change-Id: I133123ddfaa3ac6c5b14bdc651bb362418a3438b
    Reviewed-on: https://chromium-review.googlesource.com/1176877
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583528}

---
+++ third_party/WebKit/LayoutTests/external/wpt/feature-policy/idlharness.window.js
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/resources/WebIDLParser.js
+// META: script=/resources/idlharness.js
+
+"use strict";
+
+// https://wicg.github.io/feature-policy/
+
+idl_test(
+  ['feature-policy'],
+  ['reporting', 'html', 'dom'],
+  idl_array => {
+    idl_array.add_objects({
+      Document: ['document'],
+      HTMLIframeElement: ['document.createElement("iframe")'],
+      Policy: ['document.policy'],
+    })
+  }
+);
