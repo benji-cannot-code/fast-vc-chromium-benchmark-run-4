4e57e69fe2a3b3d9ffc53e8182b1e667b2ec0ed0
Vadym Doroshenko <dvadym@chromium.org>
2018-08-16 14:09:40 +0000

    [Password Manager] Do not fill gaia reath and chrome sign-in forms.
    These forms should not be filled. This CL implements that nor
    PasswordFormManager nor NewPasswordFormManager is created for such forms
    so as result they are not filled.
    
    Bug:871191
    
    Change-Id: I509bb6d1ca500ac2bbbea265712001f61bec3c53
    Reviewed-on: https://chromium-review.googlesource.com/1174714
    Commit-Queue: Vadym Doroshenko <dvadym@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583637}

--- components/password_manager/core/browser/test_password_store.h
+++ components/password_manager/core/browser/test_password_store.h
@@ -36,6 +36,8 @@ class TestPasswordStore : public PasswordStore {
   // have entries of size 0.
   bool IsEmpty() const;
 
+  int fill_matching_logins_calls() const { return fill_matching_logins_calls_; }
+
  protected:
   ~TestPasswordStore() override;
 
@@ -86,6 +88,9 @@ class TestPasswordStore : public PasswordStore {
 
  private:
   PasswordMap stored_passwords_;
+
+  // Number of calls of FillMatchingLogins() method.
+  int fill_matching_logins_calls_ = 0;
 
   DISALLOW_COPY_AND_ASSIGN(TestPasswordStore);
 };
