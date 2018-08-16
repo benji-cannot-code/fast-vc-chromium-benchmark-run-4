b3ecbabbd5867d7c5e164f5e95141bbd7abf0af6
Weidong Guo <weidongg@chromium.org>
2018-08-16 18:30:19 +0000

    Fix home button behavior for home launcher
    Changes:
    1. Press home button, the opened folder or search result page will be
       closed. Press home button again, app list will go back to first page.
    2. Tapping background or outside app list view, opening an app in
       folder or opening a search result should only close the folder or
       search results page. It should not reset pagination.
    
    Bug: 869627
    Test: AppListViewHomeLauncherTest.BackAction
    Change-Id: Ic8d4d5f76c7225ce7433750c2cb1ce8d2c3d7e59
    Reviewed-on: https://chromium-review.googlesource.com/1176978
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Weidong Guo <weidongg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583749}

--- ash/app_list/app_list_controller_impl.cc
+++ ash/app_list/app_list_controller_impl.cc
@@ -548,6 +548,10 @@ bool AppListControllerImpl::IsHomeLauncherEnabledInTabletMode() const {
                                           ->IsTabletModeWindowManagerEnabled();
 }
 
+void AppListControllerImpl::Back() {
+  presenter_.GetView()->Back();
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // Methods of |client_|:
 
@@ -596,7 +600,7 @@ void AppListControllerImpl::OpenSearchResult(const std::string& result_id,
     client_->OpenSearchResult(result_id, event_flags);
 
   if (IsHomeLauncherEnabledInTabletMode() && presenter_.IsVisible())
-    presenter_.GetView()->ResetToInitialState();
+    presenter_.GetView()->CloseOpenedPage();
 }
 
 void AppListControllerImpl::InvokeSearchResultAction(
@@ -645,7 +649,7 @@ void AppListControllerImpl::ActivateItem(const std::string& id,
     client_->ActivateItem(id, event_flags);
 
   if (IsHomeLauncherEnabledInTabletMode() && presenter_.IsVisible())
-    presenter_.GetView()->ResetToInitialState();
+    presenter_.GetView()->CloseOpenedPage();
 }
 
 void AppListControllerImpl::GetContextMenuModel(
