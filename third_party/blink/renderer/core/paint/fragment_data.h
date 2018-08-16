234fcb20c785c1f3f7fa95b2fbc9354d8e9fd1a5
Fredrik Söderquist <fs@opera.com>
2018-08-16 15:20:57 +0000

    Avoid recursing in the FragmentData destructor
    Too deep recursions can cause stack overflows.
    
    Bug: 874759
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ie532e28bceee4e7f1c3ab501b80c0e2f14be083a
    Reviewed-on: https://chromium-review.googlesource.com/1106378
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#583657}

--- third_party/blink/renderer/core/paint/fragment_data.h
+++ third_party/blink/renderer/core/paint/fragment_data.h
@@ -22,7 +22,7 @@ class CORE_EXPORT FragmentData {
  public:
   FragmentData* NextFragment() const { return next_fragment_.get(); }
   FragmentData& EnsureNextFragment();
-  void ClearNextFragment() { next_fragment_.reset(); }
+  void ClearNextFragment() { DestroyTail(); }
 
   // Visual offset of this fragment's top-left position from the
   // "paint offset root" which is the containing root PaintLayer of the root
@@ -210,8 +210,15 @@ class CORE_EXPORT FragmentData {
   const EffectPaintPropertyNode* PreEffect() const;
   const EffectPaintPropertyNode* PreFilter() const;
 
+  ~FragmentData() {
+    if (next_fragment_)
+      DestroyTail();
+  }
+
  private:
   friend class FragmentDataTest;
+
+  void DestroyTail();
 
   // Contains rare data that that is not needed on all fragments.
   struct RareData {
