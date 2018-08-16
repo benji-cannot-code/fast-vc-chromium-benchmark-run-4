0acfbcd845c0c8d021ef2c6525cc1f5722104e5f
Adam Langley <agl@chromium.org>
2018-08-16 02:28:18 +0000

    Rename USB FIDO transport to “usb”.
    This matches the name in the spec[1]. However, I note that these strings
    might be saved in preferences now, so we might be too late here. If so,
    we can always create another enum for this.
    
    [1] https://w3c.github.io/webauthn/#enumdef-authenticatortransport
    
    Change-Id: Id74e1d62a72d85b0bd96c0996d4a804de5911531
    Reviewed-on: https://chromium-review.googlesource.com/1176781
    Reviewed-by: Jun Choi <hongjunchoi@chromium.org>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583506}

--- device/fido/fido_transport_protocol.cc
+++ device/fido/fido_transport_protocol.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace device {
 
-const char kUsbHumanInterfaceDevice[] = "hid";
+const char kUsbHumanInterfaceDevice[] = "usb";
 const char kNearFieldCommunication[] = "nfc";
 const char kBluetoothLowEnergy[] = "ble";
 const char kCloudAssistedBluetoothLowEnergy[] = "cable";
