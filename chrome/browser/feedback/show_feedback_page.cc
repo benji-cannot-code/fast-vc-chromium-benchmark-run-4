16ab93330856c561fd7a6b36c953b0be0ba74d9b
Wenzhao Zang <wzang@chromium.org>
2018-08-16 20:15:34 +0000

    Add Bluetooth logging option for Google internal users.
    This CL introduces a new checkbox that will be enabled only for users on the
    google.com domain. This checkbox is only shown if we detect that the google
    internal user is trying to file feedback related to bluetooth.
    
    This CL only introduces the UI changes. Once the platforms-side changes land
    to collect bluetooth logs for Google internal users, I'll send a follow on CL
    that gathers those logs and attaches them to the feedback report.
    
    R=afakhry@chromium.org
    
    Bug: 872403
    Change-Id: I937ddf8002373a861fbe5252ce37570c9f2a2f63
    Reviewed-on: https://chromium-review.googlesource.com/1166594
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Wenzhao (Colin) Zang <wzang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583790}

--- chrome/browser/feedback/show_feedback_page.cc
+++ chrome/browser/feedback/show_feedback_page.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/metrics/histogram_macros.h"
+#include "base/strings/string_util.h"
 #include "chrome/browser/feedback/feedback_dialog_utils.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
@@ -14,9 +15,36 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "extensions/browser/api/feedback_private/feedback_private_api.h"
 
+#if defined(OS_CHROMEOS)
+#include "chrome/browser/signin/signin_manager_factory.h"
+#include "components/signin/core/browser/signin_manager.h"
+#endif
+
 namespace feedback_private = extensions::api::feedback_private;
 
 namespace chrome {
+
+namespace {
+
+#if defined(OS_CHROMEOS)
+constexpr char kGoogleDotCom[] = "@google.com";
+
+// Returns if the feedback page is considered to be triggered from user
+// interaction.
+bool IsFromUserInteraction(FeedbackSource source) {
+  switch (source) {
+    case kFeedbackSourceArcApp:
+    case kFeedbackSourceAsh:
+    case kFeedbackSourceBrowserCommand:
+    case kFeedbackSourceMdSettingsAboutPage:
+    case kFeedbackSourceOldSettingsAboutPage:
+      return true;
+    default:
+      return false;
+  }
+}
+#endif
+}
 
 void ShowFeedbackPage(Browser* browser,
                       FeedbackSource source,
@@ -43,12 +71,25 @@ void ShowFeedbackPage(Browser* browser,
   extensions::FeedbackPrivateAPI* api =
       extensions::FeedbackPrivateAPI::GetFactoryInstance()->Get(profile);
 
-  api->RequestFeedbackForFlow(
-      description_template, description_placeholder_text, category_tag,
-      extra_diagnostics, page_url,
+  feedback_private::FeedbackFlow flow =
       source == kFeedbackSourceSadTabPage
           ? feedback_private::FeedbackFlow::FEEDBACK_FLOW_SADTABCRASH
-          : feedback_private::FeedbackFlow::FEEDBACK_FLOW_REGULAR);
+          : feedback_private::FeedbackFlow::FEEDBACK_FLOW_REGULAR;
+
+#if defined(OS_CHROMEOS)
+  SigninManagerBase* signin_manager =
+      SigninManagerFactory::GetForProfile(profile);
+  if (signin_manager &&
+      base::EndsWith(signin_manager->GetAuthenticatedAccountInfo().email,
+                     kGoogleDotCom, base::CompareCase::INSENSITIVE_ASCII) &&
+      IsFromUserInteraction(source)) {
+    flow = feedback_private::FeedbackFlow::FEEDBACK_FLOW_GOOGLEINTERNAL;
+  }
+#endif
+
+  api->RequestFeedbackForFlow(description_template,
+                              description_placeholder_text, category_tag,
+                              extra_diagnostics, page_url, flow);
 }
 
 }  // namespace chrome
