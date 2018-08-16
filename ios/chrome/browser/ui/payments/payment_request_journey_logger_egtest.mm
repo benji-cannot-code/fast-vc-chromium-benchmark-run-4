7ffac1e06dd5364ecdd3dae72d68bcea90083109
Danyao Wang <danyao@chromium.org>
2018-08-16 14:52:56 +0000

    [Nav Experiment] Fix PaymentRequestJourneyLoggerEGTest expectations.
    Current expectations assume a buggy behavior of LegacyNavigationManager,
    which doesn't create a pending item on reload (crbug.com/676129). The
    test initiates a navigation to the same page to abort the payment
    request. This navigation is incorrectly classified as renderer-initiated
    by LegacyNavigationManager, which causes the abort reason for payment
    request to be incorrectly logged as EVENT_OTHER_ABORTED.
    
    WKBasedNavigationManager fixes this bug, so the test should now check
    EVENT_USER_ABORTED.
    
    Bug: 874628
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I74997df64464f196cfcfb34f21b0252d2f2f6e3e
    Reviewed-on: https://chromium-review.googlesource.com/1176470
    Commit-Queue: Danyao Wang <danyao@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583648}

--- ios/chrome/browser/ui/payments/payment_request_journey_logger_egtest.mm
+++ ios/chrome/browser/ui/payments/payment_request_journey_logger_egtest.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/test/app/histogram_test_util.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
+#import "ios/web/public/web_client.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -778,8 +779,19 @@ using payments::JourneyLogger;
       buckets[0].min & JourneyLogger::EVENT_RECEIVED_INSTRUMENT_DETAILS, @"");
   GREYAssertFalse(buckets[0].min & JourneyLogger::EVENT_SKIPPED_SHOW, @"");
   GREYAssertFalse(buckets[0].min & JourneyLogger::EVENT_COMPLETED, @"");
-  GREYAssertFalse(buckets[0].min & JourneyLogger::EVENT_USER_ABORTED, @"");
-  GREYAssertTrue(buckets[0].min & JourneyLogger::EVENT_OTHER_ABORTED, @"");
+
+  // TODO(crbug.com/676129): LegacyNavigationManager has a bug that doesn't
+  // create a pending item when reloading the page. This is incorrectly causing
+  // the second navigation above to be considered a renderer-initiated
+  // navigation, and the abort reason is incorrectly logged as
+  // EVENT_OTHER_ABORTED.
+  if (!web::GetWebClient()->IsSlimNavigationManagerEnabled()) {
+    GREYAssertFalse(buckets[0].min & JourneyLogger::EVENT_USER_ABORTED, @"");
+    GREYAssertTrue(buckets[0].min & JourneyLogger::EVENT_OTHER_ABORTED, @"");
+  } else {
+    GREYAssertTrue(buckets[0].min & JourneyLogger::EVENT_USER_ABORTED, @"");
+    GREYAssertFalse(buckets[0].min & JourneyLogger::EVENT_OTHER_ABORTED, @"");
+  }
   GREYAssertTrue(
       buckets[0].min & JourneyLogger::EVENT_HAD_INITIAL_FORM_OF_PAYMENT, @"");
   GREYAssertTrue(
@@ -829,8 +841,18 @@ using payments::JourneyLogger;
       buckets[0].min & JourneyLogger::EVENT_RECEIVED_INSTRUMENT_DETAILS, @"");
   GREYAssertFalse(buckets[0].min & JourneyLogger::EVENT_SKIPPED_SHOW, @"");
   GREYAssertFalse(buckets[0].min & JourneyLogger::EVENT_COMPLETED, @"");
-  GREYAssertFalse(buckets[0].min & JourneyLogger::EVENT_USER_ABORTED, @"");
-  GREYAssertTrue(buckets[0].min & JourneyLogger::EVENT_OTHER_ABORTED, @"");
+  // TODO(crbug.com/676129): LegacyNavigationManager has a bug that doesn't
+  // create a pending item when reloading the page. This is incorrectly causing
+  // the second navigation above to be considered a renderer-initiated
+  // navigation, and the abort reason is incorrectly logged as
+  // EVENT_OTHER_ABORTED.
+  if (!web::GetWebClient()->IsSlimNavigationManagerEnabled()) {
+    GREYAssertFalse(buckets[0].min & JourneyLogger::EVENT_USER_ABORTED, @"");
+    GREYAssertTrue(buckets[0].min & JourneyLogger::EVENT_OTHER_ABORTED, @"");
+  } else {
+    GREYAssertTrue(buckets[0].min & JourneyLogger::EVENT_USER_ABORTED, @"");
+    GREYAssertFalse(buckets[0].min & JourneyLogger::EVENT_OTHER_ABORTED, @"");
+  }
   GREYAssertFalse(
       buckets[0].min & JourneyLogger::EVENT_HAD_INITIAL_FORM_OF_PAYMENT, @"");
   GREYAssertFalse(
@@ -881,8 +903,18 @@ using payments::JourneyLogger;
       buckets[0].min & JourneyLogger::EVENT_RECEIVED_INSTRUMENT_DETAILS, @"");
   GREYAssertFalse(buckets[0].min & JourneyLogger::EVENT_SKIPPED_SHOW, @"");
   GREYAssertFalse(buckets[0].min & JourneyLogger::EVENT_COMPLETED, @"");
-  GREYAssertFalse(buckets[0].min & JourneyLogger::EVENT_USER_ABORTED, @"");
-  GREYAssertTrue(buckets[0].min & JourneyLogger::EVENT_OTHER_ABORTED, @"");
+  // TODO(crbug.com/676129): LegacyNavigationManager has a bug that doesn't
+  // create a pending item when reloading the page. This is incorrectly causing
+  // the second navigation above to be considered a renderer-initiated
+  // navigation, and the abort reason is incorrectly logged as
+  // EVENT_OTHER_ABORTED.
+  if (!web::GetWebClient()->IsSlimNavigationManagerEnabled()) {
+    GREYAssertFalse(buckets[0].min & JourneyLogger::EVENT_USER_ABORTED, @"");
+    GREYAssertTrue(buckets[0].min & JourneyLogger::EVENT_OTHER_ABORTED, @"");
+  } else {
+    GREYAssertTrue(buckets[0].min & JourneyLogger::EVENT_USER_ABORTED, @"");
+    GREYAssertFalse(buckets[0].min & JourneyLogger::EVENT_OTHER_ABORTED, @"");
+  }
   GREYAssertFalse(
       buckets[0].min & JourneyLogger::EVENT_HAD_INITIAL_FORM_OF_PAYMENT, @"");
   GREYAssertFalse(
