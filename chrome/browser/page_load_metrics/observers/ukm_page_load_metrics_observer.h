ed9dc8837749734b66a8b048e52017360e402c4b
Tarun Bansal <tbansal@chromium.org>
2018-08-16 23:12:29 +0000

    NQE servicification of UKM page load metrics
    Use network quality tracker instead of network quality estimator (NQE)
    in UKM page load metrics.
    
    Network quality tracker can provide network service when network
    service is enabled as well as when network service is disabled.
    
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Change-Id: Icffe39d2ae493934456ff911290bc50d53efab49
    Bug: 819244
    Reviewed-on: https://chromium-review.googlesource.com/1177199
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Helen Li <xunjieli@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583873}

--- chrome/browser/page_load_metrics/observers/ukm_page_load_metrics_observer.h
+++ chrome/browser/page_load_metrics/observers/ukm_page_load_metrics_observer.h
@@ -11,11 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "chrome/browser/page_load_metrics/page_load_metrics_observer.h"
 #include "components/ukm/ukm_source.h"
-#include "net/nqe/network_quality_estimator.h"
 #include "ui/base/page_transition_types.h"
 
-namespace content {
-class WebContents;
+namespace network {
+class NetworkQualityTracker;
 }
 
 // If URL-Keyed-Metrics (UKM) is enabled in the system, this is used to
@@ -25,11 +24,10 @@ class UkmPageLoadMetricsObserver
  public:
   // Returns a UkmPageLoadMetricsObserver, or nullptr if it is not needed.
   static std::unique_ptr<page_load_metrics::PageLoadMetricsObserver>
-  CreateIfNeeded(content::WebContents* web_contents);
+  CreateIfNeeded();
 
   explicit UkmPageLoadMetricsObserver(
-      net::NetworkQualityEstimator::NetworkQualityProvider*
-          network_quality_provider);
+      network::NetworkQualityTracker* network_quality_tracker);
   ~UkmPageLoadMetricsObserver() override;
 
   // page_load_metrics::PageLoadMetricsObserver implementation:
@@ -72,8 +70,8 @@ class UkmPageLoadMetricsObserver
       const page_load_metrics::PageLoadExtraInfo& info,
       base::TimeTicks app_background_time);
 
-  net::NetworkQualityEstimator::NetworkQualityProvider* const
-      network_quality_provider_;
+  // Guaranteed to be non-null during the lifetime of |this|.
+  network::NetworkQualityTracker* network_quality_tracker_;
 
   // The number of body (not header) prefilter bytes consumed by requests for
   // the page.
