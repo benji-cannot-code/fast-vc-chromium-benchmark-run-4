bc22e53500f5fb59535e1b2b0ad22887edc999e0
Lei Zhang <thestig@chromium.org>
2018-08-16 18:16:49 +0000

    Simplify PrivetTrafficDetector.
    - Remove net::AddressFamily ctor parameter and |address_family_|.
      It's always IPv4.
    - Remove |callback_runner_| since it is always the UI thread. Add a
      DCHECK to make sure that is the case.
    - Use base::OnceClosure in more places.
    - Fix some nits and better explain how the class behaves.
    
    Change-Id: Idd15c40df11c39056b9e40aab6a7bee6c3805e2e
    Reviewed-on: https://chromium-review.googlesource.com/1176642
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583737}

--- chrome/browser/printing/cloud_print/privet_traffic_detector.h
+++ chrome/browser/printing/cloud_print/privet_traffic_detector.h
@@ -9,11 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/callback.h"
-#include "base/cancelable_callback.h"
 #include "base/macros.h"
 #include "content/public/browser/browser_thread.h"
 #include "mojo/public/cpp/bindings/binding.h"
-#include "net/base/address_family.h"
 #include "net/base/ip_endpoint.h"
 #include "services/network/public/cpp/network_connection_tracker.h"
 #include "services/network/public/mojom/udp_socket.mojom.h"
@@ -24,10 +22,13 @@ class BrowserContext;
 
 namespace cloud_print {
 
-// Detects mDns traffic that looks like "Privet" protocol.
-// Can produce false positives results, but main task of the class is to avoid
-// running full mDns listener if user doesn't have devices.
-// When traffic is detected, class fires callback and shutdowns itself.
+// Detects mDns traffic that looks like the "Privet" protocol. This can produce
+// false positives results, but the main task of the class is to avoid running a
+// full mDns listener if user doesn't have devices.
+// When potential "Privet" traffic has been detected, fire a callback and stop
+// listening for traffic.
+// When the network changes, restarts itself to start listening for traffic
+// again on the new network(s).
 class PrivetTrafficDetector
     : public base::RefCountedThreadSafe<
           PrivetTrafficDetector,
@@ -35,9 +36,9 @@ class PrivetTrafficDetector
       private network::NetworkConnectionTracker::NetworkConnectionObserver,
       public network::mojom::UDPSocketReceiver {
  public:
-  PrivetTrafficDetector(net::AddressFamily address_family,
-                        content::BrowserContext* profile,
-                        const base::Closure& on_traffic_detected);
+  // Called on the UI thread.
+  PrivetTrafficDetector(content::BrowserContext* profile,
+                        const base::RepeatingClosure& on_traffic_detected);
 
   // Called on the UI thread.
   void Start();
@@ -53,7 +54,7 @@ class PrivetTrafficDetector
   // Unless otherwise noted, all methods are called on the IO thread.
   void HandleConnectionChanged(network::mojom::ConnectionType type);
   void ScheduleRestart();
-  void Restart(const net::NetworkInterfaceList& networks);
+  void Restart(net::NetworkInterfaceList networks);
   void Bind();
 
   // Called on the UI thread.
@@ -79,13 +80,11 @@ class PrivetTrafficDetector
                   base::Optional<base::span<const uint8_t>> data) override;
 
   // Initialized on the UI thread, but only accessed on the IO thread.
-  base::Closure on_traffic_detected_;
-  scoped_refptr<base::TaskRunner> callback_runner_;
-  net::NetworkInterfaceList networks_;
-  net::AddressFamily address_family_;
+  base::RepeatingClosure on_traffic_detected_;
   int restart_attempts_;
 
   // Only accessed on the IO thread.
+  net::NetworkInterfaceList networks_;
   net::IPEndPoint recv_addr_;
   base::Time start_time_;
   network::mojom::UDPSocketPtr socket_;
