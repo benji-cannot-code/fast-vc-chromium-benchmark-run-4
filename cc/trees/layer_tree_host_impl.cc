878935484b7e6f369f65619f78903905db87bff0
Ned Nguyen <nednguyen@google.com>
2018-08-16 22:25:01 +0000

    Revert "Hold pre-decoded images until use or 250ms rather than 2 frames"
    This reverts commit 520ce5d7136360877309b68ab8b43a7d13c84d82.
    
    Reason for revert: breaking blink_perf.image_decoder/* tests
    
    BUG:chromium:874810
    
    Original change's description:
    > Hold pre-decoded images until use or 250ms rather than 2 frames
    > 
    > We currently unlock pre-decoded images after 2 frames of any type. This
    > doesn't always give callers a chance to use their images (if main thread
    > is a bit backed up). Instead we should wait for the caller to actually
    > use the image. Additionally, we drop images after 250ms to prevent
    > excessive growth if a user fails to use images in a timely manner.
    > 
    > See the linked bug for example traces which show the benefits.
    > 
    > R=​khushalsagar
    > 
    > Bug: 869491
    > Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    > Change-Id: Icd94709aad5c076b1ba295495eaab24dc893ebdb
    > Reviewed-on: https://chromium-review.googlesource.com/1159203
    > Commit-Queue: Eric Karl <ericrk@chromium.org>
    > Reviewed-by: Khushal <khushalsagar@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#583171}
    
    TBR=khushalsagar@chromium.org,ericrk@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 869491
    Change-Id: Ia80ab26563bf1ccc0556d5638a483337acee972d
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Reviewed-on: https://chromium-review.googlesource.com/1178841
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Commit-Queue: Ned Nguyen <nednguyen@google.com>
    Cr-Commit-Position: refs/heads/master@{#583855}

--- cc/trees/layer_tree_host_impl.cc
+++ cc/trees/layer_tree_host_impl.cc
@@ -2443,6 +2443,7 @@ void LayerTreeHostImpl::DidFinishImplFrame() {
   skipped_frame_tracker_.FinishFrame();
   impl_thread_phase_ = ImplThreadPhase::IDLE;
   current_begin_frame_tracker_.Finish();
+  tile_manager_.decoded_image_tracker().NotifyFrameFinished();
 }
 
 void LayerTreeHostImpl::DidNotProduceFrame(const viz::BeginFrameAck& ack) {
@@ -2873,7 +2874,6 @@ void LayerTreeHostImpl::OnPurgeMemory() {
   }
   if (resource_pool_)
     resource_pool_->OnPurgeMemory();
-  tile_manager_.decoded_image_tracker().UnlockAllImages();
 }
 
 void LayerTreeHostImpl::OnMemoryPressure(
@@ -2921,7 +2921,6 @@ void LayerTreeHostImpl::SetVisible(bool visible) {
     EvictAllUIResources();
     // Call PrepareTiles to evict tiles when we become invisible.
     PrepareTiles();
-    tile_manager_.decoded_image_tracker().UnlockAllImages();
   }
 }
 
