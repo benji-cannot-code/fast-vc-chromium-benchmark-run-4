751d2c21571907c9046a00f1e84c4d745017538d
Cole Winstanley <cwinstanley@google.com>
2018-08-16 16:19:06 +0000

    [ChromeDriver] DevTools log parser and unit tests
    Added a class to parse out HTTP and WebSocket DevTools entries from
    ChromeDriver log files.
    
    Bug: chromedriver:2501
    Change-Id: I0a3173b295b802c119c90c416fe1e77d7e39cdab
    Reviewed-on: https://chromium-review.googlesource.com/1166036
    Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Cole Winstanley <cwinstanley@google.com>
    Cr-Commit-Position: refs/heads/master@{#583681}

--- chrome/test/chromedriver/commands.cc
+++ chrome/test/chromedriver/commands.cc
@@ -221,6 +221,8 @@ void ExecuteSessionCommandOnSessionThread(
   if (IsVLogOn(0)) {
     if (!session->driver_log ||
         session->driver_log->min_level() != Log::Level::kOff) {
+      // Note: ChromeDriver log-replay depends on the format of this logging.
+      // see chromedriver/log_replay/client_replay.py
       VLOG(0) << "[" << session->id << "] "
               << "COMMAND " << command_name << " "
               << FormatValueForDisplay(*params);
@@ -278,6 +280,8 @@ void ExecuteSessionCommandOnSessionThread(
       }
       if (!session->driver_log ||
           session->driver_log->min_level() != Log::Level::kOff) {
+        // Note: ChromeDriver log-replay depends on the format of this logging.
+        // see chromedriver/log_replay/client_replay.py
         VLOG(0) << "[" << session->id << "] "
                 << "RESPONSE " << command_name
                 << (result.length() ? " " + result : "");
