9ad911b9a7b80df09efb71d8182d9ebbf6e351a0
Fabio Tirelo <ftirelo@chromium.org>
2018-08-16 22:14:54 +0000

    [Autofill Styles] Fix typo in flag selection color
    Bug: 874094
    Change-Id: I246b33a817bb87357acb9b9426add41323cb2711
    Reviewed-on: https://chromium-review.googlesource.com/1178748
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583848}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -796,7 +796,7 @@ const FeatureEntry::Choice kForceColorProfileChoices[] = {
 
 const FeatureEntry::FeatureParam kAutofillPreviewStyleBlackOnBlue050[] = {
     {blink::features::kAutofillPreviewStyleExperimentBgColorParameterName,
-     "#E8F0F3"},
+     "#E8F0FE"},
     {blink::features::kAutofillPreviewStyleExperimentColorParameterName,
      "#000000"},
 };
@@ -808,7 +808,7 @@ const FeatureEntry::FeatureParam kAutofillPreviewStyleBlackOnBlue100[] = {
 };
 const FeatureEntry::FeatureParam kAutofillPreviewStyleBlue900OnBlue050[] = {
     {blink::features::kAutofillPreviewStyleExperimentBgColorParameterName,
-     "#E8F0F3"},
+     "#E8F0FE"},
     {blink::features::kAutofillPreviewStyleExperimentColorParameterName,
      "#174EA6"},
 };
