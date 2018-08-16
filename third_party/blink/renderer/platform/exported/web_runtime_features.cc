bd322141d40e6d1b673ff49f4f2dba9b9e1f1246
Daniel Vogelheim <vogelheim@chromium.org>
2018-08-16 16:14:24 +0000

    Origin Policy: Cleanup: Remove runtime-enabled switch.
    The previous implementation used both a --feature-enabled and a
    --runtime-feature-enabled switch, initializing the latter based on the former,
    presumably because it had components in both browser and Blink.
    That's redundant.
    
    The current implementation uses the browser switch. All Blink-resident parts
    of the code act on whether they receive a policy or not (which would be always
    false if the browser switch isn't set).
    
    Bug: 751996
    Change-Id: I81f10cc888fd7c29c6c93e727526863216ca6f02
    Reviewed-on: https://chromium-review.googlesource.com/1165351
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Daniel Vogelheim <vogelheim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583679}

--- third_party/blink/renderer/platform/exported/web_runtime_features.cc
+++ third_party/blink/renderer/platform/exported/web_runtime_features.cc
@@ -77,10 +77,6 @@ void WebRuntimeFeatures::EnableOriginTrialControlledFeatures(bool enable) {
   RuntimeEnabledFeatures::SetOriginTrialControlledFeaturesEnabled(enable);
 }
 
-void WebRuntimeFeatures::EnableOriginPolicy(bool enable) {
-  RuntimeEnabledFeatures::SetOriginPolicyEnabled(enable);
-}
-
 void WebRuntimeFeatures::EnableOutOfBlinkCORS(bool enable) {
   RuntimeEnabledFeatures::SetOutOfBlinkCORSEnabled(enable);
 }
