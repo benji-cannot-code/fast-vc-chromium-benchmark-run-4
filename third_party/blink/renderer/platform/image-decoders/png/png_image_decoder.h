41e7868c994a01eb7fd35dde009d5b4d3aa9cc05
Reza.Zakerinasab <zakerinasab@chromium.org>
2018-08-16 18:50:39 +0000

    Decode 16 bit PNG in PNGImageDecoder
    This change adds 16 bit decoding to PNGImageDecoder.
    This change is a part of the bigger change to support 16 bit PNGs
    in the new color managed canvas API
    (chromium-review.googlesource.com/c/chromium/src/+/1079788).
    
    BUG: 839034
    Change-Id: I1bf92e1dd4fcbd79ffe7def33223fb5298649eab
    Reviewed-on: https://chromium-review.googlesource.com/1148787
    Reviewed-by: Leon Scroggins <scroggo@chromium.org>
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Commit-Queue: Mohammad Reza Zakerinasab <zakerinasab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583760}

--- third_party/blink/renderer/platform/image-decoders/png/png_image_decoder.h
+++ third_party/blink/renderer/platform/image-decoders/png/png_image_decoder.h
@@ -49,6 +49,7 @@ class PLATFORM_EXPORT PNGImageDecoder final : public ImageDecoder {
   String FilenameExtension() const override { return "png"; }
   bool SetSize(unsigned, unsigned) override;
   int RepetitionCount() const override;
+  bool ImageIsHighBitDepth() override;
   bool FrameIsReceivedAtIndex(size_t) const override;
   TimeDelta FrameDurationAtIndex(size_t) const override;
   bool SetFailed() override;
@@ -60,6 +61,7 @@ class PLATFORM_EXPORT PNGImageDecoder final : public ImageDecoder {
 
   void SetColorSpace();
   void SetRepetitionCount(int);
+  void SetBitDepth();
 
  private:
   using ParseQuery = PNGImageReader::ParseQuery;
@@ -79,6 +81,8 @@ class PLATFORM_EXPORT PNGImageDecoder final : public ImageDecoder {
   int repetition_count_;
   bool has_alpha_channel_;
   bool current_buffer_saw_alpha_;
+  bool decode_to_half_float_;
+  size_t bit_depth_;
   std::unique_ptr<ImageFrame::PixelData[]> color_transform_scanline_;
 };
 
