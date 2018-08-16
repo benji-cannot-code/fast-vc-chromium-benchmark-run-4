3a1684c8b095a94e1de48bea88a58d6575906904
Jun Cai <juncai@chromium.org>
2018-08-16 00:14:04 +0000

    Network Service: Make some checking Origin headers tests early return if network service is enabled
    Based on the comment at:
    https://bugs.chromium.org/p/chromium/issues/detail?id=862176
    This CL makes:
    SecurityExploitBrowserTest.InvalidOriginHeaders
    ExtensionUnloadBrowserTest.UnloadWithContentScripts
    early return if network service is enabled.
    
    Bug: 862176
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Change-Id: I040ac9615a6deed786825d306a9192d14925a5cc
    Reviewed-on: https://chromium-review.googlesource.com/1176479
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Jun Cai <juncai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583455}

--- chrome/browser/extensions/extension_unload_browsertest.cc
+++ chrome/browser/extensions/extension_unload_browsertest.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/feature_list.h"
 #include "chrome/browser/extensions/extension_browsertest.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
@@ -11,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_test_utils.h"
 #include "net/dns/mock_host_resolver.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
+#include "services/network/public/cpp/features.h"
 #include "ui/base/window_open_disposition.h"
 
 namespace extensions {
@@ -48,6 +50,10 @@ IN_PROC_BROWSER_TEST_F(ExtensionUnloadBrowserTest, TestUnload) {
 // After an extension is uninstalled, network requests from its content scripts
 // should fail but not kill the renderer process.
 IN_PROC_BROWSER_TEST_F(ExtensionUnloadBrowserTest, UnloadWithContentScripts) {
+  // https://crbug.com/862176
+  if (base::FeatureList::IsEnabled(network::features::kNetworkService))
+    return;
+
   ASSERT_TRUE(embedded_test_server()->Start());
 
   // Load an extension with a content script that has a button to send XHRs.
