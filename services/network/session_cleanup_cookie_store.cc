1d8e9920a65bb7da728a1bc2dbcfa8be56421a53
Helen Li <xunjieli@chromium.org>
2018-08-16 17:12:31 +0000

    Add NetLog events to SessionCleanupCookieStore
    This CL adds NetLog events to SessionCleanupCookieStore.
    
    This is based on rdsmith@'s CL at
    https://chromium-review.googlesource.com/c/chromium/src/+/925181
    
    Bug: 801910
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Change-Id: I87a89ff265d703792a492f93ce59cb57f9f02d14
    Reviewed-on: https://chromium-review.googlesource.com/1175437
    Commit-Queue: Helen Li <xunjieli@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583704}

--- services/network/session_cleanup_cookie_store.cc
+++ services/network/session_cleanup_cookie_store.cc
@@ -15,19 +15,41 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/sequenced_task_runner.h"
 #include "base/task/post_task.h"
+#include "base/values.h"
 #include "net/cookies/canonical_cookie.h"
 #include "net/cookies/cookie_constants.h"
 #include "net/cookies/cookie_util.h"
 #include "net/extras/sqlite/cookie_crypto_delegate.h"
+#include "net/log/net_log.h"
 #include "url/gurl.h"
 
 namespace network {
+
+namespace {
+
+std::unique_ptr<base::Value> CookieStoreOriginFiltered(
+    const std::string& origin,
+    bool is_https,
+    net::NetLogCaptureMode capture_mode) {
+  if (!capture_mode.include_cookies_and_credentials())
+    return nullptr;
+  auto dict = std::make_unique<base::DictionaryValue>();
+  dict->SetString("origin", origin);
+  dict->SetBoolean("is_https", is_https);
+  return dict;
+}
+
+}  // namespace
 
 SessionCleanupCookieStore::SessionCleanupCookieStore(
     const scoped_refptr<net::SQLitePersistentCookieStore>& cookie_store)
     : persistent_store_(cookie_store) {}
 
-SessionCleanupCookieStore::~SessionCleanupCookieStore() {}
+SessionCleanupCookieStore::~SessionCleanupCookieStore() {
+  net_log_.AddEvent(
+      net::NetLogEventType::COOKIE_PERSISTENT_STORE_CLOSED,
+      net::NetLog::StringCallback("type", "SessionCleanupCookieStore"));
+}
 
 void SessionCleanupCookieStore::DeleteSessionCookies(
     DeleteCookiePredicate delete_cookie_predicate) {
@@ -47,6 +69,10 @@ void SessionCleanupCookieStore::DeleteSessionCookies(
         !delete_cookie_predicate.Run(cookie.first, cookie.second)) {
       continue;
     }
+    net_log_.AddEvent(
+        net::NetLogEventType::COOKIE_PERSISTENT_STORE_ORIGIN_FILTERED,
+        base::BindRepeating(&CookieStoreOriginFiltered, cookie.first,
+                            cookie.second));
     session_only_cookies.push_back(cookie);
   }
 
@@ -55,6 +81,7 @@ void SessionCleanupCookieStore::DeleteSessionCookies(
 
 void SessionCleanupCookieStore::Load(const LoadedCallback& loaded_callback,
                                      const net::NetLogWithSource& net_log) {
+  net_log_ = net_log;
   persistent_store_->Load(
       base::BindRepeating(&SessionCleanupCookieStore::OnLoad, this,
                           loaded_callback),
