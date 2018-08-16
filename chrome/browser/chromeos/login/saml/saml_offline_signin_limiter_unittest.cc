4be627281b40762fe0cd6a079ee3049dfebed2a7
Xida Chen <xidachen@chromium.org>
2018-08-16 12:22:04 +0000

    Revert "De-flake SAMLOfflineSigninLimiterTest"
    This reverts commit 24e19835c8892d9ec23533fd9c9d3a2784c641be.
    
    Reason for revert: <INSERT REASONING HERE>
    Causes a bunch of unittest to fail:
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20%281%29/28633
    
    Original change's description:
    > De-flake SAMLOfflineSigninLimiterTest
    > 
    > Without this, its tests fail when conceptually unrelated code such as
    > https://chromium-review.googlesource.com/c/chromium/src/+/1170145 calls
    > content::BrowserThread::PostAfterStartupTask
    > 
    > Change-Id: I7433719ee9721e224db955669b3c8d079cf780dc
    > Reviewed-on: https://chromium-review.googlesource.com/1174577
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Commit-Queue: Nigel Tao <nigeltao@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#583465}
    
    TBR=xiyuan@chromium.org,nigeltao@chromium.org
    
    Change-Id: I573f27fbd7bfc5b5fe933cd8e293248d43b742ea
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/1177641
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583610}

--- chrome/browser/chromeos/login/saml/saml_offline_signin_limiter_unittest.cc
+++ chrome/browser/chromeos/login/saml/saml_offline_signin_limiter_unittest.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/memory/ref_counted.h"
 #include "base/message_loop/message_loop_current.h"
-#include "base/run_loop.h"
 #include "base/test/simple_test_clock.h"
 #include "base/test/test_simple_task_runner.h"
 #include "base/time/clock.h"
@@ -28,10 +27,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-using testing::_;
 using testing::Mock;
 using testing::Return;
 using testing::Sequence;
+using testing::_;
 
 namespace chromeos {
 
@@ -128,8 +127,6 @@ TestingPrefServiceSimple* SAMLOfflineSigninLimiterTest::GetTestingLocalState() {
 
 void SAMLOfflineSigninLimiterTest::TearDown() {
   SAMLOfflineSigninLimiterFactory::SetClockForTesting(NULL);
-  // Clear any PostAfterStartupTask's to avoid hanging.
-  base::RunLoop().RunUntilIdle();
 }
 
 TEST_F(SAMLOfflineSigninLimiterTest, NoSAMLDefaultLimit) {
