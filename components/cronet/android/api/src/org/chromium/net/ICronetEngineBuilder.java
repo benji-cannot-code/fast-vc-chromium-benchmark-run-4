d5bd2a06375a682beea6efbf94530922fbfedd47
Paul Jensen <pauljensen@chromium.org>
2018-08-16 15:40:11 +0000

    [Cronet] Remove ICronetEngineBuilder.setCertVerifierData()
    This API was entirely unused so it was allowed to be removed from the API
    previously in crrev.com/571588. This CL cleans up the ICronetEngineBuilder
    and api.txt references as they're no longer needed.  Removing an API
    isn't normally allowed, so this required temporary modification of the
    API update checks to rebuild api.txt.
    
    Bug: 857693
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester
    Change-Id: I1bffb016f781aeb5116d8507245630dedacbb14b
    Reviewed-on: https://chromium-review.googlesource.com/1148019
    Reviewed-by: Misha Efimov <mef@chromium.org>
    Commit-Queue: Paul Jensen <pauljensen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583665}

--- components/cronet/android/api/src/org/chromium/net/ICronetEngineBuilder.java
+++ components/cronet/android/api/src/org/chromium/net/ICronetEngineBuilder.java
@@ -49,10 +49,6 @@ public abstract class ICronetEngineBuilder {
         return this;
     }
 
-    public ICronetEngineBuilder setCertVerifierData(String certVerifierData) {
-        return this;
-    }
-
     public ICronetEngineBuilder setThreadPriority(int priority) {
         return this;
     }
