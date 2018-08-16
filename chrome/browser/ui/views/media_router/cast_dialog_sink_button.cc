4602d2efdeeb7468378ced7d10505b55f921466e
Takumi Fujimoto <takumif@chromium.org>
2018-08-16 01:54:48 +0000

    [Harmony Cast Dialog] Disable sinks incompatible with the selected source
    In a future CL, we will be removing the "Cast" button in the dialog
    and enabling the user to start casting just by clicking on a sink. This
    CL is in preparation for that.
    
    Currently, the source list only shows sources supported by the currently
    selected sink. This CL makes the list show all the sources. When a source
    is selected, we disable buttons for sinks that do not support the source.
    
    The visual design for disabled sinks is TBD; the icon/text are greyed out
    for now.
    
    Bug: 872509
    
    Change-Id: I5ba719d08dd85e2b8915da22d0a2b22988b4f384
    Reviewed-on: https://chromium-review.googlesource.com/1166341
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Reviewed-by: Derek Cheng <imcheng@chromium.org>
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583492}

--- chrome/browser/ui/views/media_router/cast_dialog_sink_button.cc
+++ chrome/browser/ui/views/media_router/cast_dialog_sink_button.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/color_palette.h"
 #include "ui/gfx/paint_vector_icon.h"
 #include "ui/views/animation/ink_drop_impl.h"
+#include "ui/views/controls/styled_label.h"
 #include "ui/views/controls/throbber.h"
 #include "ui/views/vector_icons.h"
 
@@ -26,7 +27,7 @@ namespace media_router {
 
 namespace {
 
-gfx::ImageSkia CreateSinkIcon(SinkIconType icon_type) {
+gfx::ImageSkia CreateSinkIcon(SinkIconType icon_type, bool enabled = true) {
   const gfx::VectorIcon* vector_icon;
   switch (icon_type) {
     case SinkIconType::CAST_AUDIO_GROUP:
@@ -57,9 +58,13 @@ gfx::ImageSkia CreateSinkIcon(SinkIconType icon_type) {
       vector_icon = &kTvIcon;
       break;
   }
-  return gfx::CreateVectorIcon(*vector_icon,
-                               CastDialogSinkButton::kPrimaryIconSize,
-                               gfx::kChromeIconGrey);
+  SkColor icon_color = enabled ? gfx::kChromeIconGrey : gfx::kGoogleGrey500;
+  return gfx::CreateVectorIcon(
+      *vector_icon, CastDialogSinkButton::kPrimaryIconSize, icon_color);
+}
+
+gfx::ImageSkia CreateDisabledSinkIcon(SinkIconType icon_type) {
+  return CreateSinkIcon(icon_type, false);
 }
 
 std::unique_ptr<views::View> CreatePrimaryIconForSink(const UIMediaSink& sink) {
@@ -159,6 +164,26 @@ bool CastDialogSinkButton::OnKeyPressed(const ui::KeyEvent& event) {
                    ui::LocatedEvent::FromIfValid(&event));
   }
   return handled_event;
+}
+
+void CastDialogSinkButton::OnEnabledChanged() {
+  HoverButton::OnEnabledChanged();
+  SkColor background_color = GetNativeTheme()->GetSystemColor(
+      ui::NativeTheme::kColorId_ProminentButtonColor);
+  if (enabled()) {
+    SetTitleTextStyle(views::style::STYLE_PRIMARY, background_color);
+    if (sink_.state == UIMediaSinkState::AVAILABLE) {
+      static_cast<views::ImageView*>(icon_view())
+          ->SetImage(CreateSinkIcon(sink_.icon_type));
+    }
+  } else {
+    SetTitleTextStyle(views::style::STYLE_DISABLED, background_color);
+    if (sink_.state == UIMediaSinkState::AVAILABLE) {
+      static_cast<views::ImageView*>(icon_view())
+          ->SetImage(CreateDisabledSinkIcon(sink_.icon_type));
+    }
+  }
+  title()->Layout();
 }
 
 std::unique_ptr<views::InkDrop> CastDialogSinkButton::CreateInkDrop() {
