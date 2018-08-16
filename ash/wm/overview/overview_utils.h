a8a429acb795a709cc21dceba5a6679c6642456c
Xiaoqian Dai <xdai@chromium.org>
2018-08-16 17:02:03 +0000

    When dragging a window around overview, reposition overview windows.
    When dragging a window from caption area in tablet mode, overview will
    open behind the dragged windows. When the dragged window is dragged into
    the preview split area, we should update the overview grid's bounds and
    also update the new selector item's visibility.
    
    See recorded video:
    https://drive.google.com/open?id=10gPVG1Wlgt4YDnZI_tPS2p6JJGYJ-ztl
    
    Bug: 873292
    Change-Id: Ibc7028820bed11d61bdbb376fdc6723c26a0c262
    Reviewed-on: https://chromium-review.googlesource.com/1176220
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583696}

--- ash/wm/overview/overview_utils.h
+++ ash/wm/overview/overview_utils.h
@@ -6,12 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_WM_OVERVIEW_OVERVIEW_UTILS_H_
 #define ASH_WM_OVERVIEW_OVERVIEW_UTILS_H_
 
+#include <memory>
+
 #include "ash/ash_export.h"
 #include "ash/wm/overview/overview_animation_type.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/compositor/layer_type.h"
-
-#include <memory>
 
 namespace aura {
 class Window;
