79646f23590e4dbe87aed717a688e1c9dcdb0959
Jordy Greenblatt <jordynass@chromium.org>
2018-08-16 17:58:30 +0000

    [CrOS MultiDevice] Prepare settings UI infrastrcture for feature data.
    This CL replaces some data types we had thought would be important for
    displaying the multidevice settings page that wound up being redundant
    with the data types we will need to receive individual feature updates
    from the multidevice service.
    
    Bug: 824568
    Change-Id: I33b70cba332dec8cf7726630ccbf57bcf62b8655
    Reviewed-on: https://chromium-review.googlesource.com/1173658
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583724}

--- chrome/browser/resources/settings/multidevice_page/multidevice_page.js
+++ chrome/browser/resources/settings/multidevice_page/multidevice_page.js
@@ -50,8 +50,7 @@ Polymer({
    * @private
    */
   getLabelText_: function() {
-    return !!this.pageContentData.hostDevice ?
-        this.pageContentData.hostDevice.name :
+    return this.pageContentData.hostDeviceName ||
         this.i18n('multideviceSetupItemHeading');
   },
 
