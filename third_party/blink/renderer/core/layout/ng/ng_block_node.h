c4c9d9d816a39584306dea0f7c02e0e847dac199
Koji Ishii <kojii@chromium.org>
2018-08-17 09:22:17 +0000

    [LayoutNG] Make NGBlockNode::CopyFragmentDataToLayoutBoxForInlineChildren private
    This patch changes
      NGBlockNode::CopyFragmentDataToLayoutBoxForInlineChildren
    to a private member function from an anonymous function, so
    that the planned changes to the function is easier to review.
    
    No changes other than moving within the same .cc file.
    
    TBR=eae@chromium.org
    
    Bug: 636993
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng
    Change-Id: I8af2cd1872e9f6c66156368c508ea4f209fc3255
    Reviewed-on: https://chromium-review.googlesource.com/1179491
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583997}

--- third_party/blink/renderer/core/layout/ng/ng_block_node.h
+++ third_party/blink/renderer/core/layout/ng/ng_block_node.h
@@ -19,6 +19,7 @@ class NGConstraintSpace;
 class NGFragmentBuilder;
 class NGLayoutResult;
 class NGPhysicalBoxFragment;
+class NGPhysicalContainerFragment;
 class NGPhysicalFragment;
 struct MinMaxSize;
 struct NGBaselineRequest;
@@ -101,6 +102,11 @@ class CORE_EXPORT NGBlockNode final : public NGLayoutInputNode {
   // data to the layout box.
   void CopyFragmentDataToLayoutBox(const NGConstraintSpace&,
                                    const NGLayoutResult&);
+  void CopyFragmentDataToLayoutBoxForInlineChildren(
+      const NGPhysicalContainerFragment& container,
+      LayoutUnit initial_container_width,
+      bool initial_container_is_flipped,
+      NGPhysicalOffset offset = {});
   void PlaceChildrenInLayoutBox(const NGConstraintSpace&,
                                 const NGPhysicalBoxFragment&,
                                 const NGPhysicalOffset& offset_from_start);
