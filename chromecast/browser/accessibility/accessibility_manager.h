432b86aa10c18d01dc625306e1c0908dc0b0c309
Ryan Daum <rdaum@chromium.org>
2018-08-17 15:37:08 +0000

    [chromecast] Make triple tap detector detect doubles, too.
    A single detector for both multiple and triple tap events on cast, as
    part of the effort so that the triple tap for the magnification
    gesture and the double tap inside the TouchExplorationController do
    not conflict.
    
    Bug: internal b/112634029
    Test: manual and unit test
    
    Change-Id: Ifd23bbc40eb9cfd710acce7ca387eaae137c7ba1
    Reviewed-on: https://chromium-review.googlesource.com/1178640
    Reviewed-by: Alex Sakhartchouk <alexst@chromium.org>
    Commit-Queue: Ryan Daum <rdaum@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584080}

--- chromecast/browser/accessibility/accessibility_manager.h
+++ chromecast/browser/accessibility/accessibility_manager.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromecast/browser/accessibility/accessibility_sound_proxy.h"
 #include "chromecast/browser/accessibility/touch_exploration_manager.h"
 #include "chromecast/graphics/accessibility/accessibility_focus_ring_controller.h"
-#include "chromecast/graphics/gestures/triple_tap_detector.h"
+#include "chromecast/graphics/gestures/multiple_tap_detector.h"
 
 namespace aura {
 class WindowTreeHost;
@@ -28,7 +28,7 @@ namespace shell {
 
 // Responsible for delegating chromecast browser process accessibility functions
 // to the responsible party.
-class AccessibilityManager : public TripleTapDetectorDelegate {
+class AccessibilityManager : public MultipleTapDetectorDelegate {
  public:
   explicit AccessibilityManager(CastWindowManagerAura* window_manager);
   ~AccessibilityManager() override;
@@ -74,7 +74,7 @@ class AccessibilityManager : public TripleTapDetectorDelegate {
   // Returns whether the magnification gesture is currently enabled.
   bool IsMagnificationGestureEnabled() const;
 
-  // TripleTapDetectorDelegate implementation
+  // MultipleTapDetectorDelegate implementation
   void OnTripleTap(const gfx::Point& tap_location) override;
 
   // Sets the player for earcons.
@@ -88,7 +88,7 @@ class AccessibilityManager : public TripleTapDetectorDelegate {
   std::unique_ptr<AccessibilityFocusRingController>
       accessibility_focus_ring_controller_;
   std::unique_ptr<TouchExplorationManager> touch_exploration_manager_;
-  std::unique_ptr<TripleTapDetector> triple_tap_detector_;
+  std::unique_ptr<MultipleTapDetector> magnify_gesture_detector_;
   std::unique_ptr<MagnificationController> magnification_controller_;
 
   AccessibilitySoundProxy accessibility_sound_proxy_;
