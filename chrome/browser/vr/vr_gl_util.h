d464ad9f865779719be3ec0425cc89dd6692f209
Christopher Grant <cjgrant@chromium.org>
2018-08-17 19:46:40 +0000

    Roll vr_gl_utils into vr_base library
    Also, move UI support source out of the UI library.  Those files are
    meant to supply capabilitie to the UI library, but not live in the
    library itself.
    
    BUG=
    R=acondor
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:linux_vr;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: Ice761aa2d0f18f32bf9600e4a202421f5f705e51
    Reviewed-on: https://chromium-review.googlesource.com/1179979
    Reviewed-by: Aldo Culquicondor <acondor@chromium.org>
    Commit-Queue: Christopher Grant <cjgrant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584154}

--- chrome/browser/vr/vr_gl_util.h
+++ chrome/browser/vr/vr_gl_util.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <array>
 #include <string>
 
+#include "chrome/browser/vr/vr_export.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/gl/gl_bindings.h"
 
@@ -23,27 +24,27 @@ class Transform;
 
 namespace vr {
 
-std::array<float, 16> MatrixToGLArray(const gfx::Transform& matrix);
+VR_EXPORT std::array<float, 16> MatrixToGLArray(const gfx::Transform& matrix);
 
 // Compile a shader.
-GLuint CompileShader(GLenum shader_type,
-                     const GLchar* shader_source,
-                     std::string& error);
+VR_EXPORT GLuint CompileShader(GLenum shader_type,
+                               const GLchar* shader_source,
+                               std::string& error);
 
 // Compile and link a program.
-GLuint CreateAndLinkProgram(GLuint vertex_shader_handle,
-                            GLuint fragment_shader_handle,
-                            std::string& error);
+VR_EXPORT GLuint CreateAndLinkProgram(GLuint vertex_shader_handle,
+                                      GLuint fragment_shader_handle,
+                                      std::string& error);
 
 // Sets default texture parameters given a texture type.
-void SetTexParameters(GLenum texture_type);
+VR_EXPORT void SetTexParameters(GLenum texture_type);
 
 // Sets color uniforms given an SkColor.
-void SetColorUniform(GLuint handle, SkColor c);
+VR_EXPORT void SetColorUniform(GLuint handle, SkColor c);
 
 // Sets color uniforms (but not alpha) given an SkColor. The alpha is assumed to
 // be 1.0 in this case.
-void SetOpaqueColorUniform(GLuint handle, SkColor c);
+VR_EXPORT void SetOpaqueColorUniform(GLuint handle, SkColor c);
 
 }  // namespace vr
 
