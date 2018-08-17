a7740dd44c216859edd297c28e1a76b6ff369fd1
Tonko Sabolčec <tsabolcec@google.com>
2018-08-17 14:35:47 +0000

    [Password Manager] Add pref to store date when undecryptable logins were deleted on Mac
    This CL includes:
    - Add a util class which sets the preference on the main thread.
    - Use utility class in LoginDatabase to set the date on successul deletion of undecryptable passwords.
    - Expand DB tests to make sure that the pref is stored.
    
    Bug: 791541
    Change-Id: I32c2afc70c6091175cae09a7120de7622b568448
    Reviewed-on: https://chromium-review.googlesource.com/1178052
    Commit-Queue: Tonko Sabolčec <tsabolcec@google.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584060}

--- chrome/browser/password_manager/password_store_factory.cc
+++ chrome/browser/password_manager/password_store_factory.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "base/rand_util.h"
 #include "build/build_config.h"
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/incognito_helpers.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/signin_manager_factory.h"
@@ -166,6 +167,13 @@ PasswordStoreFactory::BuildServiceInstanceFor(
 
   std::unique_ptr<password_manager::LoginDatabase> login_db(
       password_manager::CreateLoginDatabase(profile->GetPath()));
+#if defined(OS_MACOSX)
+  PrefService* local_state = g_browser_process->local_state();
+  DCHECK(local_state);
+  login_db->InitPasswordRecoveryUtil(
+      std::make_unique<password_manager::PasswordRecoveryUtilMac>(
+          local_state, base::ThreadTaskRunnerHandle::Get()));
+#endif
 
   scoped_refptr<PasswordStore> ps;
 #if defined(OS_WIN)
