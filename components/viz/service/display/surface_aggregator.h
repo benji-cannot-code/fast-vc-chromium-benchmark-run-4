7738e4573463f9e5bd55a17890856bd33ec731e6
akaba <akaba@google.com>
2018-08-17 01:12:58 +0000

    Fix SurfaceAggregator's tracking of damaged surfaces
    This CL allows SurfaceAggregator to track damage in all possible cases
    of child_surface. This is done by storing the whole surface range instead
    of just LocalSurfaceIds
    
    Bug: 874249
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel
    Change-Id: Ie2886ae6eda753ab8bb3bb8b168f3a7c507df187
    Reviewed-on: https://chromium-review.googlesource.com/1174984
    Commit-Queue: Andre Kaba <akaba@google.com>
    Reviewed-by: Saman Sami <samans@chromium.org>
    Reviewed-by: Fady Samuel <fsamuel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583915}

--- components/viz/service/display/surface_aggregator.h
+++ components/viz/service/display/surface_aggregator.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/quads/render_pass.h"
 #include "components/viz/common/resources/transferable_resource.h"
 #include "components/viz/common/surfaces/surface_id.h"
+#include "components/viz/common/surfaces/surface_range.h"
 #include "components/viz/service/viz_service_export.h"
 #include "ui/gfx/color_space.h"
 
@@ -277,10 +278,9 @@ class VIZ_SERVICE_EXPORT SurfaceAggregator {
   // Tracks UMA stats for SurfaceDrawQuads during a call to Aggregate().
   SurfaceDrawQuadUmaStats uma_stats_;
 
-  // For each FrameSinkId, contains a range of LocalSurfaceIds that will damage
+  // For each FrameSinkId, contains a vector of SurfaceRanges that will damage
   // the display if they're damaged.
-  base::flat_map<FrameSinkId, std::pair<LocalSurfaceId, LocalSurfaceId>>
-      damage_ranges_;
+  base::flat_map<FrameSinkId, std::vector<SurfaceRange>> damage_ranges_;
 
   int32_t display_trace_id_ = -1;
 
