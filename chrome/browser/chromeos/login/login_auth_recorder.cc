47d43a2cf3dbb0f7d147bbdf2c4a767c606f7b8a
Sarah Hu <xiaoyinh@chromium.org>
2018-08-17 21:48:11 +0000

    cros: Update Fingerprint.UnlockEnabled recording
    Fingerprint.UnlockEnabled should map to whether there's finger enrolled
    for screen unlock, instead of tracking a feature flag.
    Remove the recording in chrome and keep the definition for biod to use.
    
    Bug: 862295
    Change-Id: Ibb706d0ac8ae05240446a3758c5b2e6d5c2e6224
    Reviewed-on: https://chromium-review.googlesource.com/1180343
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Xiaoyin Hu <xiaoyinh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584206}

--- chrome/browser/chromeos/login/login_auth_recorder.cc
+++ chrome/browser/chromeos/login/login_auth_recorder.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/login_auth_recorder.h"
 
 #include "base/metrics/histogram_macros.h"
-#include "chrome/browser/chromeos/login/quick_unlock/quick_unlock_utils.h"
 #include "chrome/browser/ui/ash/tablet_mode_client.h"
 #include "components/session_manager/core/session_manager.h"
 
@@ -102,8 +101,6 @@ AuthMethodSwitchType FindSwitchType(AuthMethod previous, AuthMethod current) {
 
 LoginAuthRecorder::LoginAuthRecorder() {
   session_manager::SessionManager::Get()->AddObserver(this);
-  UMA_HISTOGRAM_BOOLEAN("Fingerprint.UnlockEnabled",
-                        chromeos::quick_unlock::IsFingerprintEnabled());
 }
 
 LoginAuthRecorder::~LoginAuthRecorder() {
