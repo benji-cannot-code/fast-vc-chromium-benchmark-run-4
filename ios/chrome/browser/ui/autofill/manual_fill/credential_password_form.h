74a0f3d06761303109a769dc0d2fecb79f3e7674
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2018-08-17 15:24:28 +0000

    [iOS][MF] Model for passwords view controller
    Obj-c model for the manual fallback passwords view controller. Also adds
    a convenience initializer from a password form.
    
    Bug: 845472
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I0bdd704eb9cfa075f7cd78bdea2eaed191cd5bd0
    Reviewed-on: https://chromium-review.googlesource.com/1172348
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584073}

---
+++ ios/chrome/browser/ui/autofill/manual_fill/credential_password_form.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_AUTOFILL_MANUAL_FILL_CREDENTIAL_PASSWORD_FORM_H_
+#define IOS_CHROME_BROWSER_UI_AUTOFILL_MANUAL_FILL_CREDENTIAL_PASSWORD_FORM_H_
+
+#import "ios/chrome/browser/ui/autofill/manual_fill/credential.h"
+
+namespace autofill {
+struct PasswordForm;
+}
+
+@interface ManualFillCredential (PasswordForm)
+
+// Convenience initializer from a PasswordForm.
+- (instancetype)initWithPasswordForm:
+    (const autofill::PasswordForm&)passwordForm;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_AUTOFILL_MANUAL_FILL_CREDENTIAL_PASSWORD_FORM_H_
