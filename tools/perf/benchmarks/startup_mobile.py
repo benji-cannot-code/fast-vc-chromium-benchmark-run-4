0f5fc90afb4dd0f7cadbb68c45fef3dcc5d359e2
Egor Pasko <pasko@chromium.org>
2018-08-17 09:51:12 +0000

    benchmarks/startup_mobile: scaling governor tweaks
    Comparing benchmark results between experimental.startup.android.coldish
    and experimental.startup.mobile revealed that the latter is a bit more
    noisy (and reports slower startup). One difference visible in the log is
    the lack of scaling governor adjustment. We do the scaling governor
    adjustment for all other benchmarks I know of, hence correcting the
    behavior of _MobileStartupSharedState to match it.
    
    In my local testing this tweak did not bring the noise levels to match.
    Looking further.
    
    Bug: 760498
    Change-Id: Ifb12f227a135d652fd068a28329887dcae35233a
    Reviewed-on: https://chromium-review.googlesource.com/1178054
    Reviewed-by: Juan Antonio Navarro Pérez <perezju@chromium.org>
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584005}

--- tools/perf/benchmarks/startup_mobile.py
+++ tools/perf/benchmarks/startup_mobile.py
@@ -68,6 +68,7 @@ class _MobileStartupSharedState(story_module.SharedState):
     wpr_mode = wpr_modes.WPR_REPLAY
     if finder_options.use_live_sites:
       wpr_mode = wpr_modes.WPR_OFF
+    self.platform.SetFullPerformanceModeEnabled(True)
     self.platform.network_controller.Open(wpr_mode)
     self._story_set = story_set
 
@@ -77,6 +78,7 @@ class _MobileStartupSharedState(story_module.SharedState):
 
   def TearDownState(self):
     self.platform.network_controller.Close()
+    self.platform.SetFullPerformanceModeEnabled(False)
 
   def LaunchBrowser(self, url):
     self.platform.FlushDnsCache()
