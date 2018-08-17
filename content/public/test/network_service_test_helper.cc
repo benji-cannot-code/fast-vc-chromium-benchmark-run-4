28871f4acb961a42e9f74087d3f22b8c7d37ae02
Kentaro Hara <haraken@chromium.org>
2018-08-17 04:08:00 +0000

    Revert "Convert dns_api.cc to mojo host resolver"
    This reverts commit dccf844abebc5b18353b40fe9a8eb09ae441bd5e.
    
    Reason for revert: Broke some browser_tests on Mac.
    
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Mac10.13%20Tests%20%28dbg%29/4738
    
    Original change's description:
    > Convert dns_api.cc to mojo host resolver
    > 
    > Bug: 874658
    > Cq-Include-Trybots: luci.chromium.try:linux_mojo
    > Change-Id: I576d2877ec71df8e489d349916f758ba6fe74263
    > Reviewed-on: https://chromium-review.googlesource.com/1176725
    > Reviewed-by: Will Harris <wfh@chromium.org>
    > Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    > Commit-Queue: Clark DuVall <cduvall@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#583878}
    
    TBR=jam@chromium.org,wfh@chromium.org,ericorth@chromium.org,cduvall@chromium.org
    
    Change-Id: Id845006ad303fca63c7c681fd259d3db379dfddd
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 874658
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Reviewed-on: https://chromium-review.googlesource.com/1179383
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583955}

--- content/public/test/network_service_test_helper.cc
+++ content/public/test/network_service_test_helper.cc
@@ -41,19 +41,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 namespace {
-
-#define STATIC_ASSERT_ENUM(a, b)                            \
-  static_assert(static_cast<int>(a) == static_cast<int>(b), \
-                "mismatching enums: " #a)
-
-STATIC_ASSERT_ENUM(network::mojom::ResolverType::kResolverTypeFail,
-                   net::RuleBasedHostResolverProc::Rule::kResolverTypeFail);
-STATIC_ASSERT_ENUM(network::mojom::ResolverType::kResolverTypeSystem,
-                   net::RuleBasedHostResolverProc::Rule::kResolverTypeSystem);
-STATIC_ASSERT_ENUM(
-    network::mojom::ResolverType::kResolverTypeIPLiteral,
-    net::RuleBasedHostResolverProc::Rule::kResolverTypeIPLiteral);
-
 void CrashResolveHost(const std::string& host_to_crash,
                       const std::string& host) {
   if (host_to_crash == host)
@@ -82,14 +69,8 @@ class NetworkServiceTestHelper::NetworkServiceTestImpl
   void AddRules(std::vector<network::mojom::RulePtr> rules,
                 AddRulesCallback callback) override {
     for (const auto& rule : rules) {
-      if (rule->resolver_type ==
-          network::mojom::ResolverType::kResolverTypeFail) {
-        test_host_resolver_.host_resolver()->AddSimulatedFailure(
-            rule->host_pattern);
-      } else {
-        test_host_resolver_.host_resolver()->AddRule(rule->host_pattern,
-                                                     rule->replacement);
-      }
+      test_host_resolver_.host_resolver()->AddRule(rule->host_pattern,
+                                                   rule->replacement);
     }
     std::move(callback).Run();
   }
