ae34f957b5befdb2773424d7fbd479c10e4c00b1
Kevin Marshall <kmarshall@chromium.org>
2018-08-17 23:37:22 +0000

    [Fuchsia] Use QEMU and QEMU host tools downloaded from CIPD.
    QEMU will soon be removed from the Fuchsia SDK tarball.
    This CL migrates our scripts to source QEMU from CIPD instead.
    
    Change-Id: I2347a5be598a1eecd2d7c9e6c431b55c3eab1189
    Reviewed-on: https://chromium-review.googlesource.com/1178975
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584238}

--- build/fuchsia/qemu_target.py
+++ build/fuchsia/qemu_target.py
@@ -15,7 +15,7 @@ import subprocess
 import sys
 import time
 
-from common import SDK_ROOT, EnsurePathExists
+from common import QEMU_ROOT, EnsurePathExists
 
 
 # Virtual networking configuration data for QEMU.
@@ -56,7 +56,7 @@ class QemuTarget(target.Target):
       self.Shutdown()
 
   def Start(self):
-    qemu_path = os.path.join(SDK_ROOT, 'qemu', 'bin',
+    qemu_path = os.path.join(QEMU_ROOT, 'bin',
                              'qemu-system-' + self._GetTargetSdkLegacyArch())
     kernel_args = boot_data.GetKernelArgs(self._output_dir)
 
