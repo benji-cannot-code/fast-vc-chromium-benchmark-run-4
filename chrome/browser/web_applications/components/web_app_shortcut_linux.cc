91ea7a000f4a2f563e1fa84e84f772daa6766a47
Alexey Baskakov <loyso@chromium.org>
2018-08-17 05:30:21 +0000

    WebApp: Add missing dependency.
    Bug: 875042
    Change-Id: I33dfb721ad6eeaa04509bb0c83ea92456cb52aaf
    Reviewed-on: https://chromium-review.googlesource.com/1179423
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Alexey Baskakov <loyso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583969}

--- chrome/browser/web_applications/components/web_app_shortcut_linux.cc
+++ chrome/browser/web_applications/components/web_app_shortcut_linux.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/shell_integration_linux.h"
 #include "chrome/browser/web_applications/components/web_app_helpers.h"
 #include "chrome/browser/web_applications/components/web_app_shortcut.h"
+#include "chrome/common/buildflags.h"
 #include "chrome/common/chrome_constants.h"
 
 namespace {
