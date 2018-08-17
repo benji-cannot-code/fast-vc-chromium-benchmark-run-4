3ad9b38556d07d0901204bf6c4ef8c1a53880596
Toni Barzic <tbarzic@google.com>
2018-08-17 20:33:07 +0000

    Reland "Remove pre-installed demo mode resources on low disk space"
    This is a reland of d6bbb56c26abc16cf9b232d14556cdac9bc4a426
    
    Inheriting from the original cl (as the files for which these lgtms
    were given have not changed)
    TBR=isherman@chromium.org, alemate@chromium.org, gab@chromium.org
    
    Original change's description:
    > Remove pre-installed demo mode resources on low disk space
    >
    > Introduces a class that will handle removing pre-installed demo mode
    > resources when needed.
    > First incarnation observes low disk space signal from cryptohome
    > service, and removes pre-installed resources when low space is detected
    > (provided that the device is not in demo mode).
    > When demo mode resources get removed from disk, this is recorded in
    > local state, to avoid future attempts to remove these resources.
    >
    > More logic for requesting demo mode resources removal to come.
    >
    > BUG=827368
    >
    > Change-Id: I721d1995fa818fc2fc1d056e33ba513380bf441e
    > Reviewed-on: https://chromium-review.googlesource.com/1173670
    > Commit-Queue: Toni Baržić <tbarzic@chromium.org>
    > Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    > Reviewed-by: Aga Wronska <agawronska@chromium.org>
    > Reviewed-by: Gabriel Charette <gab@chromium.org>
    > Reviewed-by: Ilya Sherman <isherman@chromium.org>
    > Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#583976}
    
    Bug: 827368
    Change-Id: Idd8f42f1f85d8ed0eccd17a342750a4a1150e679
    Reviewed-on: https://chromium-review.googlesource.com/1180041
    Commit-Queue: Toni Baržić <tbarzic@chromium.org>
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584179}

--- chromeos/dbus/fake_cryptohome_client.cc
+++ chromeos/dbus/fake_cryptohome_client.cc
@@ -732,6 +732,11 @@ FakeCryptohomeClient::GetTpmAttestationDeviceKeyPayload(
                                              : base::make_optional(it->second);
 }
 
+void FakeCryptohomeClient::NotifyLowDiskSpace(uint64_t disk_free_bytes) {
+  for (auto& observer : observer_list_)
+    observer.LowDiskSpace(disk_free_bytes);
+}
+
 // static
 std::vector<uint8_t> FakeCryptohomeClient::GetStubSystemSalt() {
   const char kStubSystemSalt[] = "stub_system_salt";
@@ -813,11 +818,6 @@ void FakeCryptohomeClient::NotifyAsyncCallStatusWithData(
     const std::string& data) {
   for (auto& observer : observer_list_)
     observer.AsyncCallStatusWithData(async_id, return_status, data);
-}
-
-void FakeCryptohomeClient::NotifyLowDiskSpace(uint64_t disk_free_bytes) {
-  for (auto& observer : observer_list_)
-    observer.LowDiskSpace(disk_free_bytes);
 }
 
 void FakeCryptohomeClient::NotifyDircryptoMigrationProgress(
