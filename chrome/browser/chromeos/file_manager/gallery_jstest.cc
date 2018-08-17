5c4d7829af23e97068d036626e83f611d5d1cbf5
Trent Apted <tapted@chromium.org>
2018-08-17 08:12:21 +0000

    Gallery: Closure compile slide_mode_unittest.js
    Bug: 867700
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: I7e0fbb902d969ded9c4713be217c5bff4cba3281
    Reviewed-on: https://chromium-review.googlesource.com/1177210
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583990}

--- chrome/browser/chromeos/file_manager/gallery_jstest.cc
+++ chrome/browser/chromeos/file_manager/gallery_jstest.cc
@@ -52,7 +52,7 @@ IN_PROC_BROWSER_TEST_F(GalleryJsTest, RibbonTest) {
 }
 
 IN_PROC_BROWSER_TEST_F(GalleryJsTest, SlideModeTest) {
-  RunTest(base::FilePath(FILE_PATH_LITERAL("slide_mode_unittest.html")));
+  RunGeneratedTest("/slide_mode_unittest.html");
 }
 
 IN_PROC_BROWSER_TEST_F(GalleryJsTest, DimmableUIControllerTest) {
