db540256ca90d2080046ded03feebf480b4af68a
David Davidović <davidovic@google.com>
2018-08-17 09:51:15 +0000

    [sync::cleanup] Remove old EnumSet iter uses (2/2)
    After https://chromium-review.googlesource.com/c/chromium/src/+/1145066,
    which adds an STL-compatible interface to EnumSet and EnumSet::Iterator,
    replace all uses of the previous iteration interface with a range-based
    for loop. This allows eventual removal of the old interface altogether.
    
    Also, replace existing usage of |arraysize| with |base::Size|, since the
    former was deprecated.
    
    This is CL part 2 of 2.
    
    TBR=dschuyler@chromium.org
    
    Bug: 860435
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I7839362e3b103e9a1d309f21c7b1bef17da7b8e1
    Reviewed-on: https://chromium-review.googlesource.com/1146925
    Commit-Queue: David Davidović <davidovic@google.com>
    Reviewed-by: vitaliii <vitaliii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584006}

--- components/sync/test/engine/mock_connection_manager.cc
+++ components/sync/test/engine/mock_connection_manager.cc
@@ -178,10 +178,9 @@ bool MockConnectionManager::PostBufferToPath(PostBufferParams* params,
       sync_pb::ClientToServerResponse_Error* response_error =
           response.mutable_error();
       response_error->set_error_type(SyncEnums::THROTTLED);
-      for (ModelTypeSet::Iterator it = partial_failure_type_.First(); it.Good();
-           it.Inc()) {
+      for (ModelType type : partial_failure_type_) {
         response_error->add_error_data_type_ids(
-            GetSpecificsFieldNumberFromModelType(it.Get()));
+            GetSpecificsFieldNumberFromModelType(type));
       }
       throttling_ = false;
     }
@@ -190,10 +189,9 @@ bool MockConnectionManager::PostBufferToPath(PostBufferParams* params,
       sync_pb::ClientToServerResponse_Error* response_error =
           response.mutable_error();
       response_error->set_error_type(SyncEnums::PARTIAL_FAILURE);
-      for (ModelTypeSet::Iterator it = partial_failure_type_.First(); it.Good();
-           it.Inc()) {
+      for (ModelType type : partial_failure_type_) {
         response_error->add_error_data_type_ids(
-            GetSpecificsFieldNumberFromModelType(it.Get()));
+            GetSpecificsFieldNumberFromModelType(type));
       }
       partial_failure_ = false;
     }
