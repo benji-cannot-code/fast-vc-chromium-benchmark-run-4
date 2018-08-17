05ad87b5f8190d4d0fa2930017d9fb749adba35c
Anna Offenwanger <offenwanger@chromium.org>
2018-08-17 01:40:56 +0000

    Fix error where XR will report no devices
    This happens when AR device is the only device, because we were asking for
    a non-immersive device, and if that didn't exist, default to the immersive
    device, and if that didn't exist, reporting no devices, which misses out
    any AR devices.
    
    Bug: 875099
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:linux_vr;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I94f91c007694685551db07cdafa61d4aaca4aabe
    Reviewed-on: https://chromium-review.googlesource.com/1179271
    Commit-Queue: Anna Offenwanger <offenwanger@chromium.org>
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583930}

--- chrome/browser/vr/service/xr_runtime_manager.cc
+++ chrome/browser/vr/service/xr_runtime_manager.cc
@@ -154,8 +154,7 @@ BrowserXRRuntime* XRRuntimeManager::GetRuntimeForOptions(
 }
 
 bool XRRuntimeManager::HasAnyRuntime() {
-  device::mojom::XRSessionOptions options = device::mojom::XRSessionOptions();
-  return GetRuntimeForOptions(&options) != nullptr;
+  return runtimes_.size() > 0;
 }
 
 bool XRRuntimeManager::HasInstance() {
