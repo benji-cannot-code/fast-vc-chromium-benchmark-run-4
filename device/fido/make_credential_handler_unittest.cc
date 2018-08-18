a08e5e354577e77fc14d9350c5952a9b7641039f
Martin Kreichgauer <martinkr@google.com>
2018-08-18 02:30:14 +0000

    fido: fix lifetime issue with MaybeAddPlatformAuthenticatorCallback
    Currently, |AuthenticatorImpl| is binding
    |CreatePlatformAuthenticatorIfAvailable| with |base::Unretained| for the
    |MaybeAddPlatformAuthenticatorCallback| argument to the
    |FidoRequestHandlerBase| ctor. The callback was originally invoked
    synchronously from the ctor, but then recently changed to async
    invocation to account for observer registration (crrev.com/c/1175418).
    
    This is a potential callback lifetime issue because the
    AuthenticatorImpl and its request handler can now theoretically get
    destroyed before the callback gets invoked.
    
    To work around this issue, replace the callback with a method on the
    request handler invoked by the AuthenticatorImpl.
    
    Bug: 875338
    Change-Id: I089f0783a46791ae8d9d6bac5f0e663e52075b86
    Reviewed-on: https://chromium-review.googlesource.com/1179081
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Jun Choi <hongjunchoi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584288}

--- device/fido/make_credential_handler_unittest.cc
+++ device/fido/make_credential_handler_unittest.cc
@@ -78,12 +78,12 @@ class FidoMakeCredentialHandlerTest : public ::testing::Test {
         test_data::kClientDataHash, std::move(rp), std::move(user),
         std::move(credential_params));
 
-    return std::make_unique<MakeCredentialRequestHandler>(
+    auto handler = std::make_unique<MakeCredentialRequestHandler>(
         nullptr, supported_transports_, std::move(request_parameter),
-        std::move(authenticator_selection_criteria), cb_.callback(),
-        base::BindOnce(
-            &FidoMakeCredentialHandlerTest::CreatePlatformAuthenticator,
-            base::Unretained(this)));
+        std::move(authenticator_selection_criteria), cb_.callback());
+    handler->SetPlatformAuthenticatorOrMarkUnavailable(
+        CreatePlatformAuthenticator());
+    return handler;
   }
 
   void ExpectAllowedTransportsForRequestAre(
@@ -96,8 +96,6 @@ class FidoMakeCredentialHandlerTest : public ::testing::Test {
       ble_discovery()->WaitForCallToStartAndSimulateSuccess();
     if (base::ContainsKey(transports, Transport::kNearFieldCommunication))
       nfc_discovery()->WaitForCallToStartAndSimulateSuccess();
-    if (base::ContainsKey(transports, Transport::kInternal))
-      platform_authenticator_factory().WaitForCallback();
 
     scoped_task_environment_.FastForwardUntilNoTasksRemain();
     EXPECT_FALSE(callback().was_called());
@@ -108,8 +106,6 @@ class FidoMakeCredentialHandlerTest : public ::testing::Test {
       EXPECT_FALSE(ble_discovery()->is_start_requested());
     if (!base::ContainsKey(transports, Transport::kNearFieldCommunication))
       EXPECT_FALSE(nfc_discovery()->is_start_requested());
-    if (!base::ContainsKey(transports, Transport::kInternal))
-      EXPECT_FALSE(platform_authenticator_factory().was_called());
 
     // Even with FidoTransportProtocol::kInternal allowed, unless the platform
     // authenticator factory returns a FidoAuthenticator instance (which it will
@@ -140,10 +136,6 @@ class FidoMakeCredentialHandlerTest : public ::testing::Test {
     mock_platform_device_ = std::move(device);
   }
 
-  test::TestCallbackReceiver<>& platform_authenticator_factory() {
-    return create_platform_authenticator_receiver_;
-  }
-
   void set_supported_transports(
       base::flat_set<FidoTransportProtocol> transports) {
     supported_transports_ = std::move(transports);
@@ -151,7 +143,6 @@ class FidoMakeCredentialHandlerTest : public ::testing::Test {
 
  protected:
   std::unique_ptr<FidoAuthenticator> CreatePlatformAuthenticator() {
-    create_platform_authenticator_receiver_.callback().Run();
     if (!mock_platform_device_)
       return nullptr;
     return std::make_unique<FidoDeviceAuthenticator>(
@@ -166,7 +157,6 @@ class FidoMakeCredentialHandlerTest : public ::testing::Test {
   test::FakeFidoDiscovery* ble_discovery_;
   test::FakeFidoDiscovery* nfc_discovery_;
   std::unique_ptr<MockFidoDevice> mock_platform_device_;
-  test::TestCallbackReceiver<> create_platform_authenticator_receiver_;
   TestMakeCredentialRequestCallback cb_;
   base::flat_set<FidoTransportProtocol> supported_transports_ =
       GetTestableTransportProtocols();
@@ -364,6 +354,7 @@ TEST_F(FidoMakeCredentialHandlerTest,
   set_supported_transports({FidoTransportProtocol::kInternal});
   auto platform_device = MockFidoDevice::MakeCtap(
       ReadCTAPGetInfoResponse(test_data::kTestGetInfoResponsePlatformDevice));
+  platform_device->SetDeviceTransport(FidoTransportProtocol::kInternal);
   EXPECT_CALL(*platform_device, GetId())
       .WillRepeatedly(testing::Return("device0"));
   platform_device->ExpectCtap2CommandAndRespondWith(
@@ -414,6 +405,7 @@ TEST_F(FidoMakeCredentialHandlerTest,
        PlatformAuthenticatorPretendingToBeCrossPlatform) {
   auto platform_device = MockFidoDevice::MakeCtap(
       ReadCTAPGetInfoResponse(test_data::kTestAuthenticatorGetInfoResponse));
+  platform_device->SetDeviceTransport(FidoTransportProtocol::kInternal);
   EXPECT_CALL(*platform_device, GetId())
       .WillRepeatedly(testing::Return("device0"));
   set_mock_platform_device(std::move(platform_device));
@@ -426,7 +418,6 @@ TEST_F(FidoMakeCredentialHandlerTest,
               true /* require_resident_key */,
               UserVerificationRequirement::kRequired));
 
-  platform_authenticator_factory().WaitForCallback();
   scoped_task_environment_.FastForwardUntilNoTasksRemain();
   EXPECT_FALSE(callback().was_called());
 }
