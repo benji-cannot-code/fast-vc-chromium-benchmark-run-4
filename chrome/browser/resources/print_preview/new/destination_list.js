de1a0950882b37be0a6f03428d9c9ed662042e21
rbpotter <rbpotter@chromium.org>
2018-08-18 01:15:46 +0000

    WebUI Polymer2: Fix print preview tests
    Destinations list tests broke in Polymer2 after being fixed originally.
    Add handling for undefined destinations array so that they work again.
    
    Bug: 875448
    Change-Id: Id41219554d3a7f808b9097a268044540d6d6cf40
    Reviewed-on: https://chromium-review.googlesource.com/1162266
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584274}

--- chrome/browser/resources/print_preview/new/destination_list.js
+++ chrome/browser/resources/print_preview/new/destination_list.js
@@ -57,15 +57,6 @@ Polymer({
     'matchingDestinationsChanged_(matchingDestinations_.*)',
   ],
 
-  /** @private {boolean} */
-  newDestinations_: false,
-
-  /** @private {boolean} */
-  initializedDestinations_: false,
-
-  /** @private {!Array<!Node>} */
-  highlights_: [],
-
   // This is a workaround to ensure that the iron-list correctly updates the
   // displayed destination information when the elements in the
   // |matchingDestinations_| array change, instead of using stale information
@@ -78,6 +69,9 @@ Polymer({
 
   /** @private */
   updateMatchingDestinations_: function() {
+    if (this.destinations === undefined)
+      return;
+
     this.updateList(
         'matchingDestinations_',
         destination => destination.origin + '/' + destination.id + '/' +
