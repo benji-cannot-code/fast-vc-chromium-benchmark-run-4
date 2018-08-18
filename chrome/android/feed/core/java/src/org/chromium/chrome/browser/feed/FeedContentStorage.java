038cdb5c67d80814f9a30d889875a92e77d5f090
Gang Wu <gangwu@chromium.org>
2018-08-18 06:22:19 +0000

    [Feed] Implement Journal storage API in java
    Implement Journal storage API in java
    
    Bug:875438
    
    Change-Id: Iaaede743f66182c3bec7ffc945c37fc9204c1475
    Reviewed-on: https://chromium-review.googlesource.com/1179252
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584307}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedContentStorage.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedContentStorage.java
@@ -100,8 +100,7 @@ public class FeedContentStorage implements ContentStorage {
     @Override
     public void commit(ContentMutation mutation, Consumer<CommitResult> consumer) {
         assert mFeedContentBridge != null;
-        CommitCallback callback = new CommitCallback(consumer);
-        mFeedContentBridge.commitContentMutation(mutation, callback);
+        mFeedContentBridge.commitContentMutation(mutation, new CommitCallback(consumer));
     }
 
     @Override
