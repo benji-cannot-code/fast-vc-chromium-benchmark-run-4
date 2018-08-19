4416803e7aaf2a5956668db9f24c4e5883711f8c
Nico Weber <thakis@chromium.org>
2018-08-19 00:04:42 +0000

    Revert "[Windows Host] Prevent connection hang for hosts with 3D Display Mode enabled"
    This reverts commit ee9ee6345abd0cfeb8a5b94446a278acdfe69e24.
    
    Reason for revert: Likely broke remoting tests on Win7, https://crbug.com/875619
    
    Original change's description:
    > [Windows Host] Prevent connection hang for hosts with 3D Display Mode enabled
    > 
    > This change prevents a hang when connecting to a Windows machine
    > which has curtain mode and 3d display mode enabled.  The root cause
    > of the hang is that creating a D3D11 device takes several seconds so
    > function calls which used to take a few milliseconds now take 5+
    > seconds.  I did not see a mechanism to control this display mode
    > programatically so I am adding code which can detect the condition
    > and the logic to prevent D3D usage when 3D mode is enabled.
    > 
    > Of note is the change in BasicDesktopEnvironment where I requery the
    > D3D apis.  DesktopEnvironmentOptions are queried in Session 0 and
    > then passed on to the Desktop process.  There is a non-obvious
    > problem with this as many D3D methods cannot be queried or will
    > not return reliable results.  This results in a base set of options
    > being set in Session 0 (which includes user / experiment overrides)
    > and then passed into a session where D3D will return new values.  In
    > this CL, I requery the D3D APIs and update the capturer options if
    > they should be disabled.
    > 
    > BUG=836007
    > 
    > Change-Id: Ib7a6e61fa7987bd093e93a6100b97cc49e3aff05
    > Reviewed-on: https://chromium-review.googlesource.com/1164403
    > Commit-Queue: Joe Downing <joedow@chromium.org>
    > Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#583276}
    
    TBR=jamiewalch@chromium.org,joedow@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 836007
    Change-Id: I464ba6eb37560ef6fc687516e9e33682bee4ac7a
    Reviewed-on: https://chromium-review.googlesource.com/1180681
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584325}

--- remoting/host/host_attributes.cc
+++ remoting/host/host_attributes.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/windows_version.h"
 #include "media/base/win/mf_initializer.h"
 #include "media/gpu/windows/media_foundation_video_encode_accelerator_win.h"
-#include "remoting/host/win/evaluate_3d_display_mode.h"
 #include "remoting/host/win/evaluate_d3d.h"
 #endif
 
@@ -105,7 +104,7 @@ std::string GetHostAttributes() {
   }
 #if defined(OS_WIN)
   {
-    GetD3DCapabilities(&result);
+    GetD3DCapability(&result);
 
     auto version = base::win::GetVersion();
     if (version >= base::win::VERSION_WIN8) {
