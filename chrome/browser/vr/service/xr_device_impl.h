ff3b9ca5f636b3a6d568cee67e27509e426e808c
Anna Offenwanger <offenwanger@chromium.org>
2018-08-20 20:23:37 +0000

    Move runtime management logic out of XRDeviceImpl
    Runtimes should only be handled by the XRRuntimeManager. Moving the
    logic which handles getting current display info as well as checking for
    session supports out of XRDeviceImpl is a step in that direction.
    
    Bug: 842025
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:linux_vr;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I95bd331a30c91deb665aa12853f8a80d79c0e51b
    Reviewed-on: https://chromium-review.googlesource.com/1181535
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Anna Offenwanger <offenwanger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584550}

--- chrome/browser/vr/service/xr_device_impl.h
+++ chrome/browser/vr/service/xr_device_impl.h
@@ -41,8 +41,9 @@ class XRDeviceImpl : public device::mojom::XRDevice {
       device::mojom::XRSessionOptionsPtr options,
       bool triggered_by_displayactive,
       device::mojom::XRDevice::RequestSessionCallback callback) override;
-  void SupportsSession(device::mojom::XRSessionOptionsPtr options,
-                       SupportsSessionCallback callback) override;
+  void SupportsSession(
+      device::mojom::XRSessionOptionsPtr options,
+      device::mojom::XRDevice::SupportsSessionCallback callback) override;
   void ExitPresent() override;
   // device::mojom::XRDevice WebVR compatibility functions
   void GetImmersiveVRDisplayInfo(
@@ -53,12 +54,8 @@ class XRDeviceImpl : public device::mojom::XRDevice {
 
   void SetInFocusedFrame(bool in_focused_frame);
 
-  // Notifications when devices are added/removed.
-  void OnRuntimeRemoved(BrowserXRRuntime* device);
-  void OnRuntimeAvailable(BrowserXRRuntime* device);
-
   // Notifications/calls from BrowserXRRuntime:
-  void OnChanged();
+  void RuntimesChanged();
   void OnExitPresent();
   void OnBlur();
   void OnFocus();
@@ -89,8 +86,6 @@ class XRDeviceImpl : public device::mojom::XRDevice {
   // object.
   bool IsSecureContextRequirementSatisfied();
 
-  device::mojom::VRDisplayInfoPtr GetCurrentVRDisplayInfo();
-
   bool in_focused_frame_ = false;
 
   content::RenderFrameHost* render_frame_host_;
@@ -102,12 +97,6 @@ class XRDeviceImpl : public device::mojom::XRDevice {
   mojo::InterfacePtrSet<device::mojom::XRSessionController>
       magic_window_controllers_;
   int next_key_ = 0;
-
-  // If we start an immersive session, or are listening to immersive activation,
-  // notify this device if we are destroyed.
-  BrowserXRRuntime* immersive_runtime_ = nullptr;
-  BrowserXRRuntime* non_immersive_runtime_ = nullptr;
-  BrowserXRRuntime* ar_runtime_ = nullptr;
 
   base::WeakPtrFactory<XRDeviceImpl> weak_ptr_factory_;
 
