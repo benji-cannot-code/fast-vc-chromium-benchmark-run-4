c802ad1f18659146ab4130df9e90beb8e543041f
Timothy Gu <timothygu@chromium.org>
2018-08-20 04:32:48 +0000

    Make about:blank page have a charset of UTF-8
    This brings us into alignment with the Fetch Standard as well as Firefox and
    Edge.
    
    Bug: 165083, 874536
    Change-Id: Id06d07d301aeae8bd0c48bfe94df68cf1ed68db9
    Reviewed-on: https://chromium-review.googlesource.com/1176263
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Timothy Gu <timothygu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584357}

--- third_party/blink/renderer/core/loader/document_loader.cc
+++ third_party/blink/renderer/core/loader/document_loader.cc
@@ -889,6 +889,7 @@ bool DocumentLoader::MaybeLoadEmpty() {
       !GetFrameLoader().StateMachine()->CreatingInitialEmptyDocument())
     request_.SetURL(BlankURL());
   response_ = ResourceResponse(request_.Url(), "text/html");
+  response_.SetTextEncodingName("utf-8");
   FinishedLoading(CurrentTimeTicks());
   return true;
 }
