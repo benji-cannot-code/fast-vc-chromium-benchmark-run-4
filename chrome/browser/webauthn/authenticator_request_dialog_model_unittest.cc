6e7cdd979570cd166a98710305469c18b34bfdd9
Balazs Engedy <engedy@chromium.org>
2018-08-20 20:31:22 +0000

    WebAuthn UX: `Back` button should go to transport selection with >=2 transports.
    Bug: 875903
    Change-Id: I82c0e154101e7fee7c74262eadfc1ce974ae0a67
    Reviewed-on: https://chromium-review.googlesource.com/1181566
    Reviewed-by: Jun Choi <hongjunchoi@chromium.org>
    Commit-Queue: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584552}

--- chrome/browser/webauthn/authenticator_request_dialog_model_unittest.cc
+++ chrome/browser/webauthn/authenticator_request_dialog_model_unittest.cc
@@ -12,6 +12,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
+namespace {
+
+const base::flat_set<device::FidoTransportProtocol> kAllTransports = {
+    device::FidoTransportProtocol::kUsbHumanInterfaceDevice,
+    device::FidoTransportProtocol::kNearFieldCommunication,
+    device::FidoTransportProtocol::kBluetoothLowEnergy,
+    device::FidoTransportProtocol::kInternal,
+    device::FidoTransportProtocol::kCloudAssistedBluetoothLowEnergy,
+};
+}
+
 class AuthenticatorRequestDialogModelTest : public ::testing::Test {
  public:
   AuthenticatorRequestDialogModelTest() {}
@@ -24,14 +35,6 @@ class AuthenticatorRequestDialogModelTest : public ::testing::Test {
 TEST_F(AuthenticatorRequestDialogModelTest, TransportAutoSelection) {
   using FidoTransportProtocol = ::device::FidoTransportProtocol;
   using Step = AuthenticatorRequestDialogModel::Step;
-
-  const base::flat_set<FidoTransportProtocol> kAllTransports = {
-      FidoTransportProtocol::kUsbHumanInterfaceDevice,
-      FidoTransportProtocol::kNearFieldCommunication,
-      FidoTransportProtocol::kBluetoothLowEnergy,
-      FidoTransportProtocol::kInternal,
-      FidoTransportProtocol::kCloudAssistedBluetoothLowEnergy,
-  };
 
   const struct {
     base::flat_set<FidoTransportProtocol> available_transports;
@@ -111,22 +114,23 @@ TEST_F(AuthenticatorRequestDialogModelTest, TransportAutoSelection) {
       ASSERT_EQ(Step::kWelcomeScreen, model.current_step());
       model.StartGuidedFlowForMostLikelyTransportOrShowTransportSelection();
     }
-
     EXPECT_EQ(test_case.expected_first_step, model.current_step());
+
+    if (model.current_step() == Step::kTransportSelection)
+      continue;
+
+    model.Back();
+    if (test_case.available_transports.size() >= 2u) {
+      EXPECT_EQ(Step::kTransportSelection, model.current_step());
+    } else {
+      EXPECT_EQ(Step::kWelcomeScreen, model.current_step());
+    }
   }
 }
 
 TEST_F(AuthenticatorRequestDialogModelTest, TransportList) {
-  using FidoTransportProtocol = ::device::FidoTransportProtocol;
-
   ::device::FidoRequestHandlerBase::TransportAvailabilityInfo transports_info_1;
-  transports_info_1.available_transports = {
-      FidoTransportProtocol::kUsbHumanInterfaceDevice,
-      FidoTransportProtocol::kNearFieldCommunication,
-      FidoTransportProtocol::kBluetoothLowEnergy,
-      FidoTransportProtocol::kInternal,
-      FidoTransportProtocol::kCloudAssistedBluetoothLowEnergy,
-  };
+  transports_info_1.available_transports = kAllTransports;
 
   AuthenticatorRequestDialogModel model;
   model.StartFlow(std::move(transports_info_1), base::nullopt);
