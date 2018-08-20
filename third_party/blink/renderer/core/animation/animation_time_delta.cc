278cbb1dfba2d9d6228d44af34053f7c01c2ace9
Stephen McGruer <smcgruer@chromium.org>
2018-08-20 20:12:33 +0000

    Introduce AnimationTimeDelta class
    This class is intended to be used to facilitate the transition of Blink
    animations from double-based times to TimeDelta-based times. This CL
    introduces a dual-compiled class, AnimationTimeDelta, which is compiled
    by default as double-based but can also be compiled to use
    base::TimeDelta as the underlying value.
    
    Bug: 737867
    Change-Id: Ibba92d9eb860b20659b0897c505000a91d042988
    Reviewed-on: https://chromium-review.googlesource.com/1175859
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584547}

---
+++ third_party/blink/renderer/core/animation/animation_time_delta.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/core/animation/animation_time_delta.h"
+
+namespace blink {
+
+#if !defined(BLINK_ANIMATION_USE_TIME_DELTA)
+std::ostream& operator<<(std::ostream& os, AnimationTimeDelta time) {
+  return os << time.InSecondsF() << " s";
+}
+#endif
+
+}  // namespace blink
