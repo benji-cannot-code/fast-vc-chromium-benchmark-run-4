366abd746c73e24a92f14c767b4cedf8eb2659df
Ramin Halavati <rhalavati@chromium.org>
2018-08-20 08:16:45 +0000

    Remove Language preferences from incognito whitelist.
    Language preferences are removed from the persistent storage list for
    incognito mode.
    After this CL, if this preference is changed from incognito mode,
    it is only stored in memory and won't affect user profile.
    
    Bug: 861722
    Change-Id: Ia013aaf468aa29b2efcfcb670003f09535e2a060
    Reviewed-on: https://chromium-review.googlesource.com/1174118
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584379}

--- chrome/browser/prefs/pref_service_incognito_whitelist.cc
+++ chrome/browser/prefs/pref_service_incognito_whitelist.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/flags_ui/flags_ui_pref_names.h"
 #include "components/google/core/browser/google_pref_names.h"
 #include "components/invalidation/impl/invalidation_prefs.h"
-#include "components/language/core/browser/pref_names.h"
 #include "components/metrics/metrics_pref_names.h"
 #include "components/omnibox/browser/omnibox_pref_names.h"
 #include "components/onc/onc_pref_names.h"
@@ -224,14 +223,6 @@ const char* const kTemporaryIncognitoWhitelist[] = {
     prefs::kResolveTimezoneByGeolocation,
     prefs::kResolveTimezoneByGeolocationMethod,
     prefs::kResolveTimezoneByGeolocationMigratedToMethod,
-    prefs::kLanguageCurrentInputMethod, prefs::kLanguagePreviousInputMethod,
-    prefs::kLanguageAllowedInputMethods, prefs::kLanguagePreferredLanguages,
-    prefs::kLanguagePreferredLanguagesSyncable, prefs::kLanguagePreloadEngines,
-    prefs::kLanguagePreloadEnginesSyncable, prefs::kLanguageEnabledImes,
-    prefs::kLanguageEnabledImesSyncable, prefs::kLanguageImeMenuActivated,
-    prefs::kLanguageShouldMergeInputMethods, prefs::kLanguageSendFunctionKeys,
-    prefs::kLanguageXkbAutoRepeatEnabled, prefs::kLanguageXkbAutoRepeatDelay,
-    prefs::kLanguageXkbAutoRepeatInterval,
 
     prefs::kLabsAdvancedFilesystemEnabled, prefs::kLabsMediaplayerEnabled,
     prefs::kShow3gPromoNotification, prefs::kDataSaverPromptsShown,
@@ -672,9 +663,6 @@ const char* const kTemporaryIncognitoWhitelist[] = {
     invalidation::prefs::kInvalidatorSavedInvalidations,
     invalidation::prefs::kInvalidationServiceUseGCMChannel,
 
-    // components/language/core/browser/pref_names.h
-    language::prefs::kApplicationLocale, language::prefs::kUserLanguageProfile,
-
     // components/omnibox/browser/omnibox_pref_names.h
     omnibox::kZeroSuggestCachedResults,
 
@@ -701,7 +689,6 @@ const char* const kTemporaryIncognitoWhitelist[] = {
     // TODO(https://crbug.com/861722): Check with code owners why this pref is
     // required in tests, if possible, update tests and remove.
     chromeos::extension_ime_util::kBrailleImeEngineId,
-    chromeos::extension_ime_util::kArcImeLanguage,
 #endif  // defined(OS_CHROMEOS)
 };
 
