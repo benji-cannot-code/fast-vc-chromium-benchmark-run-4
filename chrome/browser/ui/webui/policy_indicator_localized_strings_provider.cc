a93c61b0dbf734730e4d356757c7f9ceb366ebc7
Wenzhao Zang <wzang@chromium.org>
2018-08-20 04:53:52 +0000

    cros: Update controlled-setting indicator text for Demo Mode
    There are three places that use |IDS_CONTROLLED_SETTING_POLICY|, but
    AFAIK there's no need to change the other two places
    (arc_support_host.cc and extensions_ui.cc).
    
    Bug: 873946
    Change-Id: I34e9fb0a20139b0aaca6380e949248745f50ed58
    Reviewed-on: https://chromium-review.googlesource.com/1180526
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Commit-Queue: Wenzhao (Colin) Zang <wzang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584361}

--- chrome/browser/ui/webui/policy_indicator_localized_strings_provider.cc
+++ chrome/browser/ui/webui/policy_indicator_localized_strings_provider.cc
@@ -9,6 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/generated_resources.h"
 #include "content/public/browser/web_ui_data_source.h"
 
+#if defined(OS_CHROMEOS)
+#include "chrome/browser/chromeos/login/demo_mode/demo_session.h"
+#endif
+
 namespace policy_indicator {
 
 struct LocalizedString {
@@ -17,8 +21,13 @@ struct LocalizedString {
 };
 
 void AddLocalizedStrings(content::WebUIDataSource* html_source) {
+  int controlled_setting_policy_id = IDS_CONTROLLED_SETTING_POLICY;
+#if defined(OS_CHROMEOS)
+  if (chromeos::DemoSession::IsDeviceInDemoMode())
+    controlled_setting_policy_id = IDS_CONTROLLED_SETTING_DEMO_SESSION;
+#endif
   LocalizedString localized_strings[] = {
-    {"controlledSettingPolicy", IDS_CONTROLLED_SETTING_POLICY},
+    {"controlledSettingPolicy", controlled_setting_policy_id},
     {"controlledSettingRecommendedMatches", IDS_CONTROLLED_SETTING_RECOMMENDED},
     {"controlledSettingRecommendedDiffers",
      IDS_CONTROLLED_SETTING_HAS_RECOMMENDATION},
