b9e19b84590bc2c66eeacd91737f6ca292ced27c
Rayan Kanso <rayankans@chromium.org>
2018-08-20 16:49:24 +0000

    Fix broken PWA notification badge.
    The WebApkServiceClient was overwriting the small icon in the
    notification builder. This change checks whether there was already a
    provided small icon bitmap before overwriting.
    
    This is the CL that introduced the bug:
    https://chromium-review.googlesource.com/c/chromium/src/+/978126
    It will always call setSmallIconForRemoteApp on the notification builder for PWAs. For Android M+
    this will overwrite the provided small icon bitmap.
    
    Bug: 867351
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: If65970f3349e6a0738003d89e2721e3b2568afdc
    Reviewed-on: https://chromium-review.googlesource.com/1181043
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584475}

--- chrome/android/java/src/org/chromium/chrome/browser/webapps/WebApkServiceClient.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/WebApkServiceClient.java
@@ -81,8 +81,10 @@ public class WebApkServiceClient {
         final ApiUseCallback connectionCallback = new ApiUseCallback() {
             @Override
             public void useApi(IWebApkApi api) throws RemoteException {
-                int smallIconId = api.getSmallIconId();
-                notificationBuilder.setSmallIconForRemoteApp(smallIconId, webApkPackage);
+                if (!notificationBuilder.hasSmallIconBitmap()) {
+                    notificationBuilder.setSmallIconForRemoteApp(
+                            api.getSmallIconId(), webApkPackage);
+                }
 
                 boolean notificationPermissionEnabled = api.notificationPermissionEnabled();
                 if (notificationPermissionEnabled) {
