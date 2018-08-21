81945bf860fbfef260e2f9a39f6625d8076365ff
Siddhartha <ssid@chromium.org>
2018-08-21 04:53:35 +0000

    Make trace buffer size configurable
    Add a config param to limit the total trace buffer size
    
    BUG=859260
    
    Change-Id: Ic15e5aa0d381273eadcdad270d955441386cd288
    Reviewed-on: https://chromium-review.googlesource.com/1182510
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Reviewed-by: oysteine <oysteine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584654}

--- base/trace_event/trace_config_unittest.cc
+++ base/trace_event/trace_config_unittest.cc
@@ -61,7 +61,8 @@ const char kCustomTraceConfigString[] =
     "}"
     "]"
     "},"
-    "\"record_mode\":\"record-continuously\""
+    "\"record_mode\":\"record-continuously\","
+    "\"trace_buffer_size_in_events\":100"
     "}";
 
 void CheckDefaultTraceConfigBehavior(const TraceConfig& tc) {
@@ -349,6 +350,7 @@ TEST(TraceConfigTest, TraceConfigFromDict) {
   EXPECT_EQ(RECORD_CONTINUOUSLY, custom_tc.GetTraceRecordMode());
   EXPECT_TRUE(custom_tc.IsSystraceEnabled());
   EXPECT_TRUE(custom_tc.IsArgumentFilterEnabled());
+  EXPECT_EQ(100u, custom_tc.GetTraceBufferSizeInEvents());
   EXPECT_STREQ(
       "included,inc_pattern*,"
       "disabled-by-default-cc,disabled-by-default-memory-infra,"
