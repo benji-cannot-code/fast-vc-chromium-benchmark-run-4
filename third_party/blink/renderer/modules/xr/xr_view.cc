f36e995b14c824e0b29b92f3410c3248e4adcbb7
Byoungkown <codeimpl@gmail.com>
2018-08-21 05:15:29 +0000

    [WebXR] Update using XREye instead of VREye
    As following the spec[1] at XRView, The eye has changed to use XREye
    instead of VREye. And it is nullable.
    
    [1] : https://immersive-web.github.io/webxr/#xrview-interface
    
    Bug: None
    Cq-Include-Trybots: luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: Ib8e698f714ea107bc0ac9a275a6466d419a067e3
    Reviewed-on: https://chromium-review.googlesource.com/1180629
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Byoungkwon Ko <codeimpl@gmail.com>
    Cr-Commit-Position: refs/heads/master@{#584656}

--- third_party/blink/renderer/modules/xr/xr_view.cc
+++ third_party/blink/renderer/modules/xr/xr_view.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-XRView::XRView(XRSession* session, Eye eye)
+XRView::XRView(XRSession* session, XREye eye)
     : eye_(eye),
       session_(session),
       projection_matrix_(DOMFloat32Array::Create(16)) {
