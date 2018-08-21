38ad2e1f54d6c5e998f77cd07f52279b38d2e5b9
Eliot Courtney <edcourtney@google.com>
2018-08-21 08:39:46 +0000

    Only allow one PIP window between Chrome and Android.
    Bug: 841886
    Bug: b/69370942
    Test: unit test
    Test: manually tested Android PIP causing Chrome PIP to close and vice-versa
    Change-Id: Ic524db7d48b31d3351a3a669fc48b769a155d956
    Reviewed-on: https://chromium-review.googlesource.com/1145148
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Luis Hector Chavez <lhchavez@chromium.org>
    Commit-Queue: Eliot Courtney <edcourtney@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584689}

--- chrome/browser/chromeos/arc/pip/arc_pip_bridge.h
+++ chrome/browser/chromeos/arc/pip/arc_pip_bridge.h
@@ -6,17 +6,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_CHROMEOS_ARC_PIP_ARC_PIP_BRIDGE_H_
 #define CHROME_BROWSER_CHROMEOS_ARC_PIP_ARC_PIP_BRIDGE_H_
 
+#include <memory>
+
 #include "components/arc/common/pip.mojom.h"
 #include "components/arc/connection_observer.h"
 #include "components/keyed_service/core/keyed_service.h"
 
 namespace content {
+
 class BrowserContext;
+
 }  // namespace content
 
 namespace arc {
 
 class ArcBridgeService;
+class ArcPictureInPictureWindowControllerImpl;
 
 class ArcPipBridge : public KeyedService,
                      public ConnectionObserver<mojom::PipInstance>,
@@ -42,6 +47,10 @@ class ArcPipBridge : public KeyedService,
 
  private:
   ArcBridgeService* const arc_bridge_service_;
+
+  std::unique_ptr<ArcPictureInPictureWindowControllerImpl>
+      pip_window_controller_;
+  bool prevent_closing_pip_ = false;
 
   DISALLOW_COPY_AND_ASSIGN(ArcPipBridge);
 };
---
+++ components/arc/test/fake_pip_instance.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/arc/test/fake_pip_instance.h"
+
+#include <utility>
+
+namespace arc {
+
+FakePipInstance::FakePipInstance() = default;
+
+FakePipInstance::~FakePipInstance() = default;
+
+void FakePipInstance::Init(mojom::PipHostPtr host_ptr, InitCallback callback) {
+  host_ptr_ = std::move(host_ptr);
+  std::move(callback).Run();
+}
+
+void FakePipInstance::ClosePip() {
+  num_closed_++;
+}
+
+}  // namespace arc
