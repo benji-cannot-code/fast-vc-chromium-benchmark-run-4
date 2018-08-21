575fb019733243275087265eb070080b9448947c
Wez <wez@chromium.org>
2018-08-21 16:08:26 +0000

    Remove PPB_FlashFullscreen version 0.1 API.
    This early revision of the API hasn't been in use for some time, so
    remove it.
    
    Change-Id: I79df9948f1844563de7b100e19f5d4444be731dc
    Reviewed-on: https://chromium-review.googlesource.com/1161743
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Raymes Khoury <raymes@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584764}

--- ppapi/c/private/ppb_flash_fullscreen.h
+++ ppapi/c/private/ppb_flash_fullscreen.h
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * found in the LICENSE file.
  */
 
-/* From private/ppb_flash_fullscreen.idl modified Tue Sep 11 13:52:24 2012. */
+/* From private/ppb_flash_fullscreen.idl modified Thu Jul 26 06:15:01 2018. */
 
 #ifndef PPAPI_C_PRIVATE_PPB_FLASH_FULLSCREEN_H_
 #define PPAPI_C_PRIVATE_PPB_FLASH_FULLSCREEN_H_
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/c/pp_size.h"
 #include "ppapi/c/pp_stdint.h"
 
-#define PPB_FLASHFULLSCREEN_INTERFACE_0_1 "PPB_FlashFullscreen;0.1"
 #define PPB_FLASHFULLSCREEN_INTERFACE_1_0 "PPB_FlashFullscreen;1.0"
 #define PPB_FLASHFULLSCREEN_INTERFACE PPB_FLASHFULLSCREEN_INTERFACE_1_0
 
@@ -54,12 +53,6 @@ struct PPB_FlashFullscreen_1_0 {
 };
 
 typedef struct PPB_FlashFullscreen_1_0 PPB_FlashFullscreen;
-
-struct PPB_FlashFullscreen_0_1 {
-  PP_Bool (*IsFullscreen)(PP_Instance instance);
-  PP_Bool (*SetFullscreen)(PP_Instance instance, PP_Bool fullscreen);
-  PP_Bool (*GetScreenSize)(PP_Instance instance, struct PP_Size* size);
-};
 /**
  * @}
  */
--- ppapi/native_client/src/untrusted/pnacl_irt_shim/pnacl_shim.c
+++ ppapi/native_client/src/untrusted/pnacl_irt_shim/pnacl_shim.c
@@ -3787,8 +3787,6 @@ static int32_t Pnacl_M33_PPB_Flash_DRM_MonitorIsExternal(PP_Resource drm, PP_Boo
 
 /* Not generating wrapper methods for PPB_Flash_FontFile_0_2 */
 
-/* Not generating wrapper methods for PPB_FlashFullscreen_0_1 */
-
 /* Not generating wrapper methods for PPB_FlashFullscreen_1_0 */
 
 /* Begin wrapper methods for PPB_Flash_Menu_0_2 */
@@ -5584,8 +5582,6 @@ static const struct PPB_Flash_DRM_1_1 Pnacl_Wrappers_PPB_Flash_DRM_1_1 = {
 /* Not generating wrapper interface for PPB_Flash_FontFile_0_1 */
 
 /* Not generating wrapper interface for PPB_Flash_FontFile_0_2 */
-
-/* Not generating wrapper interface for PPB_FlashFullscreen_0_1 */
 
 /* Not generating wrapper interface for PPB_FlashFullscreen_1_0 */
 
