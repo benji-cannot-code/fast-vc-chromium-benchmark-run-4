9d8a887423c73ca712e8f4eed3699f93205bf256
Guido Urdaneta <guidou@chromium.org>
2018-08-21 14:14:26 +0000

    Disable flaky AppViewTest.TestAppViewRefusedDataShouldFail
    The test has flaked on Windows, Linux and Mac.
    
    Bug: 875908
    TBR: lazyboy@chromium.org
    Change-Id: I214ecaea3d96bd5d2e892556b0f92d7bb4f652d6
    Reviewed-on: https://chromium-review.googlesource.com/1183433
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584731}

--- chrome/browser/apps/guest_view/app_view_browsertest.cc
+++ chrome/browser/apps/guest_view/app_view_browsertest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/strings/stringprintf.h"
+#include "build/build_config.h"
 #include "chrome/browser/apps/platform_apps/app_browsertest_util.h"
 #include "components/guest_view/browser/guest_view_manager.h"
 #include "components/guest_view/browser/guest_view_manager_factory.h"
@@ -162,7 +163,14 @@ IN_PROC_BROWSER_TEST_F(AppViewTest, TestAppViewWithUndefinedDataShouldSucceed) {
 }
 
 // Tests that <appview> correctly processes parameters passed on connect.
-IN_PROC_BROWSER_TEST_F(AppViewTest, TestAppViewRefusedDataShouldFail) {
+// Flaky on Windows, Linux and Mac. See https://crbug.com/875908
+#if defined(OS_WIN) || defined(OS_LINUX) || defined(OS_MACOSX)
+#define MAYBE_TestAppViewRefusedDataShouldFail \
+  DISABLED_TestAppViewRefusedDataShouldFail
+#else
+#define MAYBE_TestAppViewRefusedDataShouldFail TestAppViewRefusedDataShouldFail
+#endif
+IN_PROC_BROWSER_TEST_F(AppViewTest, MAYBE_TestAppViewRefusedDataShouldFail) {
   const extensions::Extension* skeleton_app =
       InstallPlatformApp("app_view/shim/skeleton");
   TestHelper("testAppViewRefusedDataShouldFail",
