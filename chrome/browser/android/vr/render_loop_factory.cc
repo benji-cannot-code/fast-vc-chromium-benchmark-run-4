5d5a3192c3a42d6381b6fef219a73dd12ae10f39
Christopher Grant <cjgrant@chromium.org>
2018-08-21 15:28:12 +0000

    VR: Add UI module instantiation support
    This change adds the plumbing necessary to create a UI using a future native
    feature module library.  The code is currently behind a define, which will
    become a feature flag.
    
    Bug=
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:linux_vr;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I8f71a78f980bda790517d0bd7c41ed091ce66c52
    Reviewed-on: https://chromium-review.googlesource.com/1181924
    Reviewed-by: Aldo Culquicondor <acondor@chromium.org>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Commit-Queue: Christopher Grant <cjgrant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584751}

--- chrome/browser/android/vr/render_loop_factory.cc
+++ chrome/browser/android/vr/render_loop_factory.cc
@@ -39,6 +39,7 @@ RenderLoopFactory::Params::~Params() = default;
 
 std::unique_ptr<VrShellGl> RenderLoopFactory::Create(
     VrGLThread* vr_gl_thread,
+    UiFactory* ui_factory,
     std::unique_ptr<Params> params) {
   DCHECK(params);
   auto keyboard_delegate = GvrKeyboardDelegate::Create();
@@ -51,7 +52,7 @@ std::unique_ptr<VrShellGl> RenderLoopFactory::Create(
                             base::Unretained(keyboard_delegate.get())));
   }
   auto audio_delegate = std::make_unique<SoundsManagerAudioDelegate>();
-  auto ui = UiFactory::Create(
+  auto ui = ui_factory->Create(
       vr_gl_thread, vr_gl_thread, std::move(keyboard_delegate),
       std::move(text_input_delegate), std::move(audio_delegate),
       params->ui_initial_state);
