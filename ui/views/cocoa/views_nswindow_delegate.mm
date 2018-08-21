b7f8c357bc4554b7d131329f8bb15b16c03c76f0
Christopher Cameron <ccameron@chromium.org>
2018-08-21 17:23:10 +0000

    RemoteMacViews: Update visibility, miniaturize, and key window state
    Mechanically move the following functions either from
    BridgedNativeWidget to BridgedNativeWidgetPublic or from direct
    manipulation in NativeWidgetMac to BridgedNativeWidgetPublic:
      - SetVisibilityState
      - SetVisibleOnAllSpaces
      - SetMiniaturized
      - SetOpacity
      - SetContentAspectRatio
      - SetWindowTitle
      - MakeFirstResponder
    
    Add state tracking for visibility, miniaturization, and window key
    status to BridgedNativeWidgetHostImpl.
    
    Merge the new visibility tracking in with the existing compositor
    visibility tracking.
    
    Bug: 859152
    Change-Id: I750986e6732bc54ff12e90a75e9d649a34895682
    Reviewed-on: https://chromium-review.googlesource.com/1182962
    Commit-Queue: ccameron <ccameron@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584811}

--- ui/views/cocoa/views_nswindow_delegate.mm
+++ ui/views/cocoa/views_nswindow_delegate.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_task_runner_handle.h"
 #import "ui/views/cocoa/bridged_content_view.h"
 #import "ui/views/cocoa/bridged_native_widget.h"
+#include "ui/views/cocoa/bridged_native_widget_host.h"
 #include "ui/views/widget/native_widget_mac.h"
 
 @implementation ViewsNSWindowDelegate
@@ -156,10 +157,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)windowDidMiniaturize:(NSNotification*)notification {
+  parent_->host()->OnWindowMiniaturizedChanged(true);
   parent_->OnVisibilityChanged();
 }
 
 - (void)windowDidDeminiaturize:(NSNotification*)notification {
+  parent_->host()->OnWindowMiniaturizedChanged(false);
   parent_->OnVisibilityChanged();
 }
 
