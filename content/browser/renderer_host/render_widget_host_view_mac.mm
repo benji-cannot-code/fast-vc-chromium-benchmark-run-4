eb58be70a70b20cb3f7e32e19c2241d40ac7d4ee
akaba <akaba@google.com>
2018-08-21 17:11:01 +0000

    Rewrite content rendering timeout using new fallback semantics
    Content rendering timeout will now set the fallback to the first Surface
    after navigation instead of invalidating it.
    
    Bug: 861769
    TBR: piman@chromium.org
    Change-Id: I145a482cc084398d6687aa749acd563c9a07c6e2
    Reviewed-on: https://chromium-review.googlesource.com/1174707
    Commit-Queue: Andre Kaba <akaba@google.com>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Fady Samuel <fsamuel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584798}

--- content/browser/renderer_host/render_widget_host_view_mac.mm
+++ content/browser/renderer_host/render_widget_host_view_mac.mm
@@ -92,10 +92,6 @@ void RenderWidgetHostViewMac::OnFrameTokenChanged(uint32_t frame_token) {
   OnFrameTokenChangedForView(frame_token);
 }
 
-void RenderWidgetHostViewMac::DidReceiveFirstFrameAfterNavigation() {
-  host()->DidReceiveFirstFrameAfterNavigation();
-}
-
 void RenderWidgetHostViewMac::DestroyCompositorForShutdown() {
   // When RenderWidgetHostViewMac was owned by an NSView, this function was
   // necessary to ensure that the ui::Compositor did not outlive the
@@ -1039,6 +1035,11 @@ void RenderWidgetHostViewMac::OnDidNotProduceFrame(
 
 void RenderWidgetHostViewMac::ClearCompositorFrame() {
   browser_compositor_->ClearCompositorFrame();
+}
+
+void RenderWidgetHostViewMac::ResetFallbackToFirstNavigationSurface() {
+  browser_compositor_->GetDelegatedFrameHost()
+      ->ResetFallbackToFirstNavigationSurface();
 }
 
 bool RenderWidgetHostViewMac::RequestRepaintForTesting() {
