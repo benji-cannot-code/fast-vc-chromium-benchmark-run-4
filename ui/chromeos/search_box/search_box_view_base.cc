c8b753b7de928eb8e2daf2ea04e4b581e2efc4b3
Alex Newcomer <newcomer@chromium.org>
2018-08-21 16:52:05 +0000

    cros: Remove background hightlight for searchboxview in tablet mode
    Remove the background highlight in tablet mode for the AppList
    SearchBoxView.
    
    Bug: 866653
    Change-Id: Id43065944a877d8e44a123df60be2caf2a79b52a
    Reviewed-on: https://chromium-review.googlesource.com/1182281
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584787}

--- ui/chromeos/search_box/search_box_view_base.cc
+++ ui/chromeos/search_box/search_box_view_base.cc
@@ -409,6 +409,7 @@ void SearchBoxViewBase::ButtonPressed(views::Button* sender,
 void SearchBoxViewBase::OnTabletModeChanged(bool started) {
   is_tablet_mode_ = started;
   UpdateKeyboardVisibility();
+  UpdateSearchBoxBorder();
 }
 
 void SearchBoxViewBase::OnOnSearchBoxFocusedChanged() {
