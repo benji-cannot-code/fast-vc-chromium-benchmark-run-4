916dd71c1297e10dc8daade7a000fa817fcfd126
Trent Apted <tapted@chromium.org>
2018-08-21 08:00:51 +0000

    Update 3 ObserverLists introduced after r584330 to be ::Unchecked.
    r584330 updated all annotations on ObserverList declarations to be
    ::Unchecked, but a few new declarations appeared since that. They
    need to be ::Unchecked as well in order for https://crrev.com/c/1053338
    to land cleanly.
    
    TBR=gab@chromium.org
    
    Bug: 842987
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Change-Id: I1c6d7ae8e9668438afa8184fd0c3884a21e9732d
    Reviewed-on: https://chromium-review.googlesource.com/1182712
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584682}

--- chrome/browser/ui/toolbar/media_router_action_controller.h
+++ chrome/browser/ui/toolbar/media_router_action_controller.h
@@ -134,7 +134,7 @@ class MediaRouterActionController : public media_router::IssuesObserver,
 
   PrefChangeRegistrar pref_change_registrar_;
 
-  base::ObserverList<Observer> observers_;
+  base::ObserverList<Observer>::Unchecked observers_;
 
   base::WeakPtrFactory<MediaRouterActionController> weak_factory_;
 
