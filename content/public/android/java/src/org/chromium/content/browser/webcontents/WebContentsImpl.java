19e539749ebe3a4fa4e69d7d131991845948434a
Jinsuk Kim <jinsukkim@chromium.org>
2018-08-21 05:30:28 +0000

    Android: Move more methods to test-only targets
    Moved new test-only methods added to WebContents/RenderFrameHost
    in https://crrev.com/c/1163831 to targets content*test_support.
    
    Bug: 862153, 875742
    Change-Id: Id9be5f53b45e2b28861a35401bba71020cf5fc0f
    Reviewed-on: https://chromium-review.googlesource.com/1180824
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584660}

--- content/public/android/java/src/org/chromium/content/browser/webcontents/WebContentsImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/webcontents/WebContentsImpl.java
@@ -343,11 +343,6 @@ public class WebContentsImpl implements WebContents, RenderFrameHostDelegate, Wi
     }
 
     @Override
-    public RenderFrameHost getFocusedFrame() {
-        return nativeGetFocusedFrame(mNativeWebContentsAndroid);
-    }
-
-    @Override
     public String getTitle() {
         return nativeGetTitle(mNativeWebContentsAndroid);
     }
@@ -901,7 +896,6 @@ public class WebContentsImpl implements WebContents, RenderFrameHostDelegate, Wi
     private native void nativeSetTopLevelNativeWindow(
             long nativeWebContentsAndroid, WindowAndroid windowAndroid);
     private native RenderFrameHost nativeGetMainFrame(long nativeWebContentsAndroid);
-    private native RenderFrameHost nativeGetFocusedFrame(long nativeWebContentsAndroid);
     private native String nativeGetTitle(long nativeWebContentsAndroid);
     private native String nativeGetVisibleURL(long nativeWebContentsAndroid);
     private native String nativeGetEncoding(long nativeWebContentsAndroid);
--- content/public/test/android/javatests/src/org/chromium/content/browser/test/util/WebContentsUtils.java
+++ content/public/test/android/javatests/src/org/chromium/content/browser/test/util/WebContentsUtils.java
@@ -6,10 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.content.browser.test.util;
 
 import org.chromium.base.annotations.JNINamespace;
+import org.chromium.content_public.browser.RenderFrameHost;
 import org.chromium.content_public.browser.WebContents;
 
 /**
  * Collection of test-only WebContents utilities.
+ * TODO(jinsukkim): Move test utilities to content_public.
  */
 @JNINamespace("content")
 public class WebContentsUtils {
@@ -23,6 +25,15 @@ public class WebContentsUtils {
         nativeReportAllFrameSubmissions(webContents, enabled);
     }
 
+    /**
+     * Gets the currently focused {@link RenderFrameHost} instance for a given {@link WebContents}.
+     * @param webContents The WebContents in use.
+     */
+    public static RenderFrameHost getFocusedFrame(final WebContents webContents) {
+        return nativeGetFocusedFrame(webContents);
+    }
+
     private static native void nativeReportAllFrameSubmissions(
             WebContents webContents, boolean enabled);
+    private static native RenderFrameHost nativeGetFocusedFrame(WebContents webContents);
 }
