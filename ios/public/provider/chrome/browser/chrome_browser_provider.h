cac05679ef55ba7e3eafbfe5f051e97e3a52973f
Kurt Horimoto <kkhorimoto@chromium.org>
2018-08-22 18:17:04 +0000

    [iOS] Create FullscreenProvider.
    This interface can be used by chrome embedders to provide additional
    scrolling fixes for FullscreenControllerObserver callbacks.
    
    Bug: none
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ie6ce56c484ea7bc5a48a6802b626589d03ccf1cb
    Reviewed-on: https://chromium-review.googlesource.com/1184171
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585170}

--- ios/public/provider/chrome/browser/chrome_browser_provider.h
+++ ios/public/provider/chrome/browser/chrome_browser_provider.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class AppDistributionProvider;
 class BrandedImageProvider;
 class ExternalSearchProvider;
+class FullscreenProvider;
 class MailtoHandlerProvider;
 class OmahaServiceProvider;
 class SpotlightProvider;
@@ -161,6 +162,9 @@ class ChromeBrowserProvider {
 
   // Returns an instance of the External Search provider.
   virtual ExternalSearchProvider* GetExternalSearchProvider() const;
+
+  // Returns an instance of the fullscreen provider.
+  virtual FullscreenProvider* GetFullscreenProvider() const;
 
   // Checks for native iOS apps that are installed.
   virtual void CheckForFirstPartyApps() const;
---
+++ ios/public/provider/chrome/browser/ui/fullscreen_provider.mm
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/public/provider/chrome/browser/ui/fullscreen_provider.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+FullscreenProvider::FullscreenProvider() = default;
+
+FullscreenProvider::~FullscreenProvider() = default;
+
+void FullscreenProvider::InitializeFullscreen(
+    FullscreenController* controller) {}
