567b922eecf8dacc81c49ae09d20ff5a908bba21
Gauthier Ambard <gambard@chromium.org>
2018-08-22 07:16:56 +0000

    [iOS] Fix ConsentBump presentation style
    The presentation style should be UIModalPresentationFormSheet.
    
    Bug: none
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I2e2e6c20b940e2c455a7605b2bdfc874ec2e9f4e
    Reviewed-on: https://chromium-review.googlesource.com/1183670
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584971}

--- ios/chrome/browser/ui/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view_controller.mm
@@ -5060,7 +5060,7 @@ applicationCommandEndpoint:(id<ApplicationCommands>)applicationCommandEndpoint {
   self.consentBumpCoordinator.delegate = self;
   [self.consentBumpCoordinator start];
   self.consentBumpCoordinator.viewController.modalPresentationStyle =
-      UIModalPresentationPageSheet;
+      UIModalPresentationFormSheet;
   [self presentViewController:self.consentBumpCoordinator.viewController
                      animated:YES
                    completion:nil];
