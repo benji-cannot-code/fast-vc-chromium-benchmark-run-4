d884ef0889a5055a19d9112dc3a9f7e1b026154c
Weidong Guo <weidongg@chromium.org>
2018-08-22 17:56:25 +0000

    Fix homecher issue with multi profile
    Changes:
    1. Fix a crash that occurs when signing into a different account in
       multi profile.
    2. Fix homecher not shown issue when account is switched.
    
    Bug: 875913
    Change-Id: Iddd093aa93aa21247f77c9831fbc3cb8afdac9c6
    Reviewed-on: https://chromium-review.googlesource.com/1182449
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Weidong Guo <weidongg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585154}

--- ash/app_list/presenter/app_list_presenter_impl.cc
+++ ash/app_list/presenter/app_list_presenter_impl.cc
@@ -121,9 +121,7 @@ void AppListPresenterImpl::Show(int64_t display_id,
   is_visible_ = true;
   RequestPresentationTime(display_id, event_time_stamp);
 
-  if (view_) {
-    ScheduleAnimation();
-  } else {
+  if (!view_) {
     // Note |delegate_| outlives the AppListView. For Ash, the view
     // is destroyed when dismissed.
     AppListView* view = new AppListView(delegate_->GetAppListViewDelegate());
@@ -330,7 +328,6 @@ void AppListPresenterImpl::ScheduleAnimation() {
   gfx::Transform transform;
   transform.Translate(-offset.x(), -offset.y());
   layer->SetTransform(transform);
-  is_animating_to_close_ = true;
 
   {
     ui::ScopedLayerAnimationSettings animation(layer->GetAnimator());
@@ -400,7 +397,6 @@ void AppListPresenterImpl::OnImplicitAnimationsCompleted() {
     view_->GetWidget()->Activate();
   } else {
     view_->GetWidget()->Close();
-    is_animating_to_close_ = false;
   }
 }
 
