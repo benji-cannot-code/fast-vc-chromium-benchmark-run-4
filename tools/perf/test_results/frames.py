92e6a0d40c31939eb2df9cbdf76d3326ea88123b
Juan Antonio Navarro Perez <perezju@chromium.org>
2018-08-22 14:05:10 +0000

    [flakiness_cli] Convert builders data to pandas.DataFrame
    Add a function to conver the data returned by test-results server about
    known masters, builders, and test_types into a DataFrame for more
    convenient querying of the data on it.
    
    Also add a corresponding unit test.
    
    NOTRY=true
    
    Bug: 875251
    Change-Id: Ifa03250669fe3b661f093ff0e1ca172db85fade6
    Reviewed-on: https://chromium-review.googlesource.com/1179888
    Commit-Queue: Juan Antonio Navarro Pérez <perezju@chromium.org>
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Cr-Commit-Position: refs/heads/master@{#585004}

---
+++ tools/perf/test_results/frames.py
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Module to convert json responses from test-results into data frames."""
+
+try:
+  import pandas
+except ImportError:
+  pandas = None
+
+
+def BuildersDataFrame(data):
+  def iter_rows():
+    for master in data['masters']:
+      for test_type, builders in master['tests'].iteritems():
+        for builder_name in builders['builders']:
+          yield master['name'], builder_name, test_type
+
+  return pandas.DataFrame.from_records(
+      iter_rows(), columns=('master', 'builder', 'test_type'))
