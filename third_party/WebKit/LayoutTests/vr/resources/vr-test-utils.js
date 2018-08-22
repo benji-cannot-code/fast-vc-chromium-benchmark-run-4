8a7fb501487514c5a4152e55d40a9396cbf5b97e
Anna Offenwanger <offenwanger@chromium.org>
2018-08-22 16:37:43 +0000

    Move XR LayoutTests over to be WPTs
    Moving as many of the WebXR LayoutTests as possible over to be
    WebPlatformTests. All the tests that have been moved test some
    aspect of the spec (https://immersive-web.github.io/webxr/) in
    a platform agnostic way.
    
    Bug: 863557, 844772
    Change-Id: I203b4c59be0daa0ca624fb0281fc33035f1be8a1
    Reviewed-on: https://chromium-review.googlesource.com/1171857
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Anna Offenwanger <offenwanger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585064}

--- third_party/WebKit/LayoutTests/vr/resources/vr-test-utils.js
+++ third_party/WebKit/LayoutTests/vr/resources/vr-test-utils.js
@@ -2,13 +2,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 'use strict';
 
 MockVRService.prototype.setListeningForActivate = function(client) {
-  for (let i = 0; i < this.devices_.length; i++) {
-    this.devices_[i].displayClient_ = client;
+  for (let i = 0; i < this.runtimes_.length; i++) {
+    this.runtimes_[i].displayClient_ = client;
   }
 };
 
+MockVRService.prototype.getImmersiveVRDisplayInfo = function() {
+  return Promise.resolve(
+      {info: this.runtimes_[0] ? this.runtimes_[0].displayInfo_ : null});
+};
 
-MockDevice.prototype.setPose = function(pose) {
+MockRuntime.prototype.setPose = function(pose) {
   if (pose == null) {
     this.pose_ = null;
   } else {
@@ -31,12 +35,8 @@ MockDevice.prototype.setPose = function(pose) {
   }
 };
 
-MockDevice.prototype.forceActivate = function(reason) {
+MockRuntime.prototype.forceActivate = function(reason) {
   this.displayClient_.onActivate(reason);
-};
-
-MockDevice.prototype.getImmersiveVRDisplayInfo = function() {
-  return Promise.resolve({info: this.displayInfo_});
 };
 
 function vr_test(func, vrDisplays, name, properties) {
