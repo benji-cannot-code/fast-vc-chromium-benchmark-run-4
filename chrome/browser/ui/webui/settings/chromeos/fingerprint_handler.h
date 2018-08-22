561cf69164b32a18f18ead0284899720cccd1211
Sarah Hu <xiaoyinh@chromium.org>
2018-08-22 16:54:38 +0000

    cros: Fix crash in lock screen when fingerprint unlock is available.
    Make session observer to be ScopedObserver so it remove itself when out
    of scope.
    Also add AllowJavascript in some JS call hander.
    
    Bug: 876467
    Change-Id: If7cd1c2fe42024708027bee5f87855244c7a4ea9
    Reviewed-on: https://chromium-review.googlesource.com/1183590
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Xiaoyin Hu <xiaoyinh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585094}

--- chrome/browser/ui/webui/settings/chromeos/fingerprint_handler.h
+++ chrome/browser/ui/webui/settings/chromeos/fingerprint_handler.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_WEBUI_SETTINGS_CHROMEOS_FINGERPRINT_HANDLER_H_
 
 #include "base/containers/flat_map.h"
+#include "base/scoped_observer.h"
 #include "chrome/browser/ui/webui/settings/settings_page_ui_handler.h"
 #include "components/session_manager/core/session_manager_observer.h"
 #include "mojo/public/cpp/bindings/binding.h"
@@ -17,6 +18,10 @@ class Profile;
 namespace base {
 class ListValue;
 }  // namespace base
+
+namespace session_manager {
+class SessionManager;
+}  // namespace session_manager
 
 namespace chromeos {
 namespace settings {
@@ -77,6 +82,9 @@ class FingerprintHandler : public ::settings::SettingsPageUIHandler,
 
   device::mojom::FingerprintPtr fp_service_;
   mojo::Binding<device::mojom::FingerprintObserver> binding_;
+  ScopedObserver<session_manager::SessionManager,
+                 session_manager::SessionManagerObserver>
+      session_observer_;
 
   base::WeakPtrFactory<FingerprintHandler> weak_ptr_factory_;
 
