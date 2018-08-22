09dbd912e498b748a0f6104f231f98a4a4e6b6c7
Ryan Hamilton <rch@chromium.org>
2018-08-22 23:24:14 +0000

    Remove a couple of unused #includes from QUIC and SPDY code.
    Change-Id: I28f66dbd11c0a9cd6ab47019503fc4dbb17847a5
    Reviewed-on: https://chromium-review.googlesource.com/1185638
    Reviewed-by: Zhongyi Shi <zhongyi@chromium.org>
    Commit-Queue: Ryan Hamilton <rch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585303}

--- net/third_party/quic/platform/impl/quic_file_utils_impl.h
+++ net/third_party/quic/platform/impl/quic_file_utils_impl.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_enumerator.h"
 #include "base/files/file_util.h"
-#include "net/http/http_util.h"
 #include "net/third_party/quic/platform/api/quic_string.h"
 #include "net/third_party/quic/platform/impl/quic_file_utils_impl.h"
 
--- net/third_party/spdy/core/spdy_test_utils.h
+++ net/third_party/spdy/core/spdy_test_utils.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <memory>
 
-#include "net/spdy/server_push_delegate.h"
 #include "net/test/gtest_util.h"
 #include "net/third_party/spdy/core/spdy_bug_tracker.h"
 #include "net/third_party/spdy/core/spdy_header_block.h"
