720f7f6e9969b044cb822719b1ac0e8ebadcf0a7
Sujie Zhu <sujiezhu@google.com>
2018-08-22 17:44:30 +0000

    [Autofill] Add accessor function in AutofillManager and FormDataImporter
    In order to prevent the pointer leakage when we create local_card_migration_manager from the settings page, we add two accessor methods in AutofillManager and FormDataImporter.
    
    Notice that form_data_importer is a unique_ptr owned by autofill_manager (created when autofill_manager is created). Form_data_importer owns a unqiue_ptr of the local_card_migration_manager and create that when itself is created.
    
    We can add form_data_importer() accessor function in autofill_manager to get the raw pointer of the form_data_importer and add local_card_migration_manager() function to form_data_importer to get the raw pointer of local_card_migration_manager.
    
    Bug: 852904
    Change-Id: Ied951b50aaa0181cfeb97c1f6be9f2517db5e5a5
    Reviewed-on: https://chromium-review.googlesource.com/1180595
    Reviewed-by: Jared Saul <jsaul@google.com>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Commit-Queue: Sujie Zhu <sujiezhu@google.com>
    Cr-Commit-Position: refs/heads/master@{#585144}

--- components/autofill/core/browser/autofill_manager.h
+++ components/autofill/core/browser/autofill_manager.h
@@ -135,6 +135,8 @@ class AutofillManager : public AutofillHandler,
     return download_manager_.get();
   }
 
+  FormDataImporter* form_data_importer() { return form_data_importer_.get(); }
+
   payments::FullCardRequest* GetOrCreateFullCardRequest();
 
   payments::FullCardRequest* CreateFullCardRequest(
