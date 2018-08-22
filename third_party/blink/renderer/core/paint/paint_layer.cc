7c77e30a099a42a426a98a7cd273190e27d3ddbc
Chris Harrelson <chrishtr@chromium.org>
2018-08-22 17:24:09 +0000

    [PE] Don't invalidate compositing inputs on scroll.
    An earlier patch (http://crrev.com/582337) regressed this.
    
    Bug: 873883
    
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ie5fe1149ac1da2df7c1a59864339f5b5982527ff
    Reviewed-on: https://chromium-review.googlesource.com/1183596
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585128}

--- third_party/blink/renderer/core/paint/paint_layer.cc
+++ third_party/blink/renderer/core/paint/paint_layer.cc
@@ -325,7 +325,8 @@ void PaintLayer::UpdateLayerPositionsAfterLayout() {
 }
 
 void PaintLayer::UpdateLayerPositionRecursive(
-    UpdateLayerPositionBehavior behavior) {
+    UpdateLayerPositionBehavior behavior,
+    bool dirty_compositing_if_needed) {
   LayoutPoint old_location = location_;
   switch (behavior) {
     case AllLayers:
@@ -343,7 +344,7 @@ void PaintLayer::UpdateLayerPositionRecursive(
       NOTREACHED();
   }
 
-  if (location_ != old_location)
+  if (dirty_compositing_if_needed && location_ != old_location)
     SetNeedsCompositingInputsUpdate();
 
   for (PaintLayer* child = FirstChild(); child; child = child->NextSibling())
@@ -425,12 +426,14 @@ void PaintLayer::UpdateLayerPositionsAfterOverflowScroll() {
     // offset is not included in clip rects. Therefore, we do not need to clear
     // them when that PaintLayer is scrolled. We also don't need to update layer
     // positions, because they also do not depend on the root's scroll offset.
-    if (GetScrollableArea()->HasStickyDescendants())
-      UpdateLayerPositionRecursive(OnlyStickyLayers);
+    if (GetScrollableArea()->HasStickyDescendants()) {
+      UpdateLayerPositionRecursive(OnlyStickyLayers,
+                                   /* dirty_compositing */ false);
+    }
     return;
   }
   ClearClipRects();
-  UpdateLayerPositionRecursive(AllLayers);
+  UpdateLayerPositionRecursive(AllLayers, /* dirty_compositing */ false);
 }
 
 void PaintLayer::UpdateTransformationMatrix() {
