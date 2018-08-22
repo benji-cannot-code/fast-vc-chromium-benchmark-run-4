3d2ab0bc38a6f2caed14d834b80f68a62406b968
Jeremy Roman <jbroman@chromium.org>
2018-08-22 15:13:44 +0000

    WTF: Replace most uses of WTF::MutexBase::Locked with AssertAcquired.
    This matches the API of base::Lock, which we intend to eventually switch to,
    better, and doing a DCHECK is the only reasonable thing to do with the result
    of Locked anyhow.
    
    Bug: 856641
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ibd31cb7d1c0982929b6aa09135c7b331cadec984
    Reviewed-on: https://chromium-review.googlesource.com/1183633
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Yuta Kitamura <yutak@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585021}

--- third_party/blink/renderer/platform/heap/persistent.h
+++ third_party/blink/renderer/platform/heap/persistent.h
@@ -187,7 +187,7 @@ class PersistentBase {
         crossThreadnessConfiguration == kCrossThreadPersistentConfiguration,
         "This Persistent does not require the cross-thread lock.");
 #if DCHECK_IS_ON()
-    DCHECK(ProcessHeap::CrossThreadPersistentMutex().Locked());
+    ProcessHeap::CrossThreadPersistentMutex().AssertAcquired();
 #endif
     raw_ = nullptr;
     CrossThreadPersistentRegion& region =
@@ -343,7 +343,7 @@ class PersistentBase {
                      kWeakPersistentConfiguration,
                      kCrossThreadPersistentConfiguration>* persistent) {
 #if DCHECK_IS_ON()
-    DCHECK(ProcessHeap::CrossThreadPersistentMutex().Locked());
+    ProcessHeap::CrossThreadPersistentMutex().AssertAcquired();
 #endif
     persistent->ClearWithLockHeld();
   }
