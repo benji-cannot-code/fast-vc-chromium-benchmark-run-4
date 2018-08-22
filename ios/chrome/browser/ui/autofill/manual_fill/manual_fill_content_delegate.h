5e75341a029f4e9c85c16241992f2945dba61cfe
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2018-08-22 17:07:42 +0000

    [iOS][MF] Create Cells to show credentials
    Adds cells to show credentials. These support dynamic fonts and have 2
    buttons. One for password and one for username, which send the events
    to the delegate passed when setting the cell up.
    
    Mocks: https://folio.googleplex.com/ntp/Autofill/Autofill%20Manual%20Fallback
    
    Bug: 845472
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ia375e00789fb21935817801b904ec7190a516875
    Reviewed-on: https://chromium-review.googlesource.com/1183192
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585107}

---
+++ ios/chrome/browser/ui/autofill/manual_fill/manual_fill_content_delegate.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_AUTOFILL_MANUAL_FILL_MANUAL_FILL_CONTENT_DELEGATE_H_
+#define IOS_CHROME_BROWSER_UI_AUTOFILL_MANUAL_FILL_MANUAL_FILL_CONTENT_DELEGATE_H_
+
+#import <Foundation/Foundation.h>
+
+// Protocol to send Manual Fill user selections to be filled in the active web
+// state.
+@protocol ManualFillContentDelegate<NSObject>
+
+// Called after the user selects an element to be used as the input for the
+// current form field.
+//
+// @param content The selected string.
+// @param isSecure YES if the user selected a sensitive field, i.e. a password
+//                 or a credit card.
+- (void)userDidPickContent:(NSString*)content isSecure:(BOOL)isSecure;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_AUTOFILL_MANUAL_FILL_MANUAL_FILL_CONTENT_DELEGATE_H_
---
+++ ios/chrome/browser/ui/autofill/manual_fill/manual_fill_password_cell.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_AUTOFILL_MANUAL_FILL_MANUAL_FILL_PASSWORD_CELL_H_
+#define IOS_CHROME_BROWSER_UI_AUTOFILL_MANUAL_FILL_MANUAL_FILL_PASSWORD_CELL_H_
+
+#import <UIKit/UIKit.h>
+
+@protocol ManualFillContentDelegate;
+@class ManualFillCredential;
+
+// Cell to display a Credential where the username and password are interactable
+// and send the data to the delegate.
+@interface ManualFillPasswordCell : UITableViewCell
+
+// Updates the cell with the |credential|. If the user iteracts with it, the
+// |delegate| will be notified.
+- (void)setUpWithCredential:(ManualFillCredential*)credential
+                   delegate:(id<ManualFillContentDelegate>)delegate;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_AUTOFILL_MANUAL_FILL_MANUAL_FILL_PASSWORD_CELL_H_
