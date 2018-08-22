ab710d6ce565bb5b17517176075cbee80faa2919
Mustaq Ahmed <mustaq@google.com>
2018-08-22 17:07:39 +0000

    UAv2: Bring expiry time down from infinity to half a minute.
    The transient bit in UserActivationState is a proxy for the
    state that "the user is currently active with the page".
    From a logical perspective, this state should expire/reset
    after a certain period of users' inactivity.  Chrome's
    current expiry is 1sec (but can be 10secs in certain cases).
    As of mid-2017, none of Edge, Firefox and Safari had any
    expiry time, so we wanted to explore the simplest possible
    design for UAv2---w/o any expiry.  This created issues with
    APIs that never consumes the activation, and we learned
    that Firefox also added an expiry time last year.  Hence
    we are bringing down the experimental 1h expiry time here.
    
    Bug: 776404
    Change-Id: I640c22805c8d4db74812a02d24e645aa994dc20f
    Reviewed-on: https://chromium-review.googlesource.com/1178746
    Commit-Queue: Mustaq Ahmed <mustaq@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585106}

--- third_party/blink/common/frame/user_activation_state.cc
+++ third_party/blink/common/frame/user_activation_state.cc
@@ -7,12 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-// This is a tentative timespan, which should be more than the current limit of
-// 1 sec (in UGI) because we want a reasonable value that works even for a slow
-// network.  Currently we are experimenting with a vary large value (eqvt to no
-// expiry): https://crbug.com/776404.
+// The expiry time should be long enough to allow network round trips even in a
+// very slow connection (to support xhr-like calls with user activation), yet
+// not too long to make an "unattneded" page feel activated.
 constexpr base::TimeDelta kActivationLifespan =
-    base::TimeDelta::FromSeconds(3600);
+    base::TimeDelta::FromSeconds(30);
 
 void UserActivationState::Activate() {
   has_been_active_ = true;
