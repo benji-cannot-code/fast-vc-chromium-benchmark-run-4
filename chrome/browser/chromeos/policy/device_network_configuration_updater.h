aa42f9a95ccb9e2580a43d706d146faf9992020d
Pavol Marko <pmarko@chromium.org>
2018-08-22 22:35:42 +0000

    Make policy-set trust anchors available early on session start
    The goal is to make policy-set trust anchors available as early
    as possible, instead of waiting for the user's private slot to be
    initialized.
    
    The following changes were made to achieve this:
    - UserNetworkConfigurationUpdater makes server and CA certificates
      available to observers as soon as ONC policy comes in.
    - UserNetworkConfigurationUpdater does not pass server and CA
      certificates to CertificateImporter for permanent import anymore.
      Only Client certificates are permanently imported from user policy.
    - PolicyCertService (which observes for user-policy-set cert changes)
      makes all user-policy-set server and authority certificates available
      to NSS as temp certificates through TempCertsCacheNSS.
    
    Planned follow-up work:
    The policy-set certs are not displayed in the Certificates settings
    view. A follow-up CL (CL:916681) will add support for that.
    
          browser_tests --gtest_filter=*PolicyProvided*
    
    Bug: 787602
    Test: unit_tests --gtest_filter=*NetworkConfigurationUpdater* &&
    Change-Id: I71aa8f1f91849b0b0ad24eb81ce3d1863fe7959c
    Reviewed-on: https://chromium-review.googlesource.com/873810
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585286}

--- chrome/browser/chromeos/policy/device_network_configuration_updater.h
+++ chrome/browser/chromeos/policy/device_network_configuration_updater.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/chromeos/policy/network_configuration_updater.h"
 #include "components/onc/onc_constants.h"
+#include "net/cert/x509_certificate.h"
 
 namespace base {
 class DictionaryValue;
@@ -57,7 +58,7 @@ class DeviceNetworkConfigurationUpdater : public NetworkConfigurationUpdater {
 
   // Returns all authority certificates from the currently applied ONC device
   // policy.
-  std::vector<std::string> GetAuthorityCertificates();
+  net::CertificateList GetAuthorityCertificates();
 
  private:
   DeviceNetworkConfigurationUpdater(
--- chrome/browser/chromeos/policy/temp_certs_cache_nss.h
+++ chrome/browser/chromeos/policy/temp_certs_cache_nss.h
@@ -6,12 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_CHROMEOS_POLICY_TEMP_CERTS_CACHE_NSS_H_
 #define CHROME_BROWSER_CHROMEOS_POLICY_TEMP_CERTS_CACHE_NSS_H_
 
-#include <string>
-#include <vector>
-
-#include "base/callback_forward.h"
 #include "base/macros.h"
 #include "net/cert/scoped_nss_types.h"
+#include "net/cert/x509_certificate.h"
 
 namespace policy {
 
@@ -19,10 +16,8 @@ namespace policy {
 // them available e.g. for client certificate discovery.
 class TempCertsCacheNSS {
  public:
-  explicit TempCertsCacheNSS(const std::vector<std::string>& x509_certs);
+  explicit TempCertsCacheNSS(const net::CertificateList& certificates);
   ~TempCertsCacheNSS();
-
-  static std::vector<std::string> GetUntrustedAuthoritiesFromDeviceOncPolicy();
 
  private:
   // The actual cache of NSS temporary certificates.
