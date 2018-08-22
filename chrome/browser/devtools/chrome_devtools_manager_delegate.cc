4670ffaa5e39aba704c6910e374a871bf38ba9c1
Andrey Lushnikov <lushnikov@chromium.org>
2018-08-22 17:05:13 +0000

    DevTools: move browser context management to devtools_manager_delegate
    This patch:
    - moves browser context management to devtools_manager_delegate
    - implements browser context related protocol methods in content/
    
    R=dgozman
    
    Bug: 631464
    Change-Id: Ia43343d5c2208556214b41225cb05f744f2d9ccd
    Reviewed-on: https://chromium-review.googlesource.com/1183594
    Commit-Queue: Andrey Lushnikov <lushnikov@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585102}

--- chrome/browser/devtools/chrome_devtools_manager_delegate.cc
+++ chrome/browser/devtools/chrome_devtools_manager_delegate.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/devtools/chrome_devtools_session.h"
 #include "chrome/browser/devtools/device/android_device_manager.h"
 #include "chrome/browser/devtools/device/tcp_device_provider.h"
+#include "chrome/browser/devtools/devtools_browser_context_manager.h"
 #include "chrome/browser/devtools/devtools_window.h"
 #include "chrome/browser/devtools/protocol/target_handler.h"
 #include "chrome/browser/extensions/extension_tab_util.h"
@@ -220,6 +221,22 @@ std::string ChromeDevToolsManagerDelegate::GetDiscoveryPageHTML() {
   return ui::ResourceBundle::GetSharedInstance()
       .GetRawDataResource(IDR_DEVTOOLS_DISCOVERY_PAGE_HTML)
       .as_string();
+}
+
+std::vector<content::BrowserContext*>
+ChromeDevToolsManagerDelegate::GetBrowserContexts() {
+  return DevToolsBrowserContextManager::GetInstance().GetBrowserContexts();
+}
+
+content::BrowserContext* ChromeDevToolsManagerDelegate::CreateBrowserContext() {
+  return DevToolsBrowserContextManager::GetInstance().CreateBrowserContext();
+}
+
+void ChromeDevToolsManagerDelegate::DisposeBrowserContext(
+    content::BrowserContext* context,
+    DisposeCallback callback) {
+  DevToolsBrowserContextManager::GetInstance().DisposeBrowserContext(
+      context, std::move(callback));
 }
 
 bool ChromeDevToolsManagerDelegate::HasBundledFrontendResources() {
