bf2785d22eab7f4dbc27224ed02383b8743a1c24
Ramin Halavati <rhalavati@chromium.org>
2018-08-22 17:08:44 +0000

    Remove Timezone preferences from incognito whitelist.
    Timezone preferences are removed from the persistent storage list for
    incognito mode.
    After this CL, if these preferences are changed from incognito mode,
    they are only stored in memory and won't affect user profile.
    
    Bug: 861722
    Change-Id: Ia6839002befb019f46617fc373c77b9600c069f1
    Reviewed-on: https://chromium-review.googlesource.com/1180219
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585108}

--- chrome/browser/prefs/pref_service_incognito_whitelist.cc
+++ chrome/browser/prefs/pref_service_incognito_whitelist.cc
@@ -216,15 +216,10 @@ const char* const kTemporaryIncognitoWhitelist[] = {
     prefs::kEnableTouchpadThreeFingerClick, prefs::kNaturalScroll,
     prefs::kPrimaryMouseButtonRight, prefs::kMouseReverseScroll,
     prefs::kMouseSensitivity, prefs::kTouchpadSensitivity,
-    prefs::kUse24HourClock, prefs::kUserTimezone,
-    prefs::kResolveTimezoneByGeolocation,
-    prefs::kResolveTimezoneByGeolocationMethod,
-    prefs::kResolveTimezoneByGeolocationMigratedToMethod,
-
-    prefs::kLabsAdvancedFilesystemEnabled, prefs::kLabsMediaplayerEnabled,
-    prefs::kShow3gPromoNotification, prefs::kDataSaverPromptsShown,
-    prefs::kChromeOSReleaseNotesVersion, prefs::kNoteTakingAppId,
-    prefs::kNoteTakingAppEnabledOnLockScreen,
+    prefs::kUse24HourClock, prefs::kLabsAdvancedFilesystemEnabled,
+    prefs::kLabsMediaplayerEnabled, prefs::kShow3gPromoNotification,
+    prefs::kDataSaverPromptsShown, prefs::kChromeOSReleaseNotesVersion,
+    prefs::kNoteTakingAppId, prefs::kNoteTakingAppEnabledOnLockScreen,
     prefs::kNoteTakingAppsLockScreenWhitelist,
     prefs::kNoteTakingAppsLockScreenToastShown,
     prefs::kRestoreLastLockScreenNote, prefs::kSessionUserActivitySeen,
@@ -496,12 +491,6 @@ const char* const kTemporaryIncognitoWhitelist[] = {
 
     prefs::kFactoryResetRequested, prefs::kFactoryResetTPMFirmwareUpdateMode,
     prefs::kDebuggingFeaturesRequested,
-
-#if defined(OS_CHROMEOS)
-    prefs::kResolveDeviceTimezoneByGeolocation,
-    prefs::kResolveDeviceTimezoneByGeolocationMethod,
-    prefs::kSystemTimezoneAutomaticDetectionPolicy,
-#endif  // defined(OS_CHROMEOS)
 
 #if !defined(OS_ANDROID)
 #if !defined(OS_CHROMEOS)
