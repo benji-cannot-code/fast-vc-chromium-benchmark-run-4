9731ee7c680b1e34919dd4cb47032aa76834398c
Calder Kitagawa <ckitagawa@chromium.org>
2018-08-22 17:54:28 +0000

    [WebShareTarget] Update to action/params in WebShare
    Phase 3 of the work to replace url_template with action and params as
    per the updated WebShareTarget spec:
    https://wicg.github.io/web-share-target/.
    
    This work updates WebShare to use the new action and parameter model
    rather than |url_template|.
    
    See the bug for a detailed plan for performing these changes.
    
    Bug: 871788
    Change-Id: I0d2e044320085a082d4a8912de2a099fe01f9156
    Reviewed-on: https://chromium-review.googlesource.com/1168127
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Reviewed-by: Matt Giuca <mgiuca@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585152}

--- chrome/browser/webshare/webshare_target.cc
+++ chrome/browser/webshare/webshare_target.cc
@@ -12,17 +12,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 WebShareTarget::WebShareTarget(const GURL& manifest_url,
                                const std::string& name,
-                               const GURL& url_template)
-    : manifest_url_(manifest_url), name_(name), url_template_(url_template) {}
+                               const GURL& action,
+                               const std::string& text,
+                               const std::string& title,
+                               const std::string& url)
+    : manifest_url_(manifest_url),
+      name_(name),
+      action_(action),
+      text_(text),
+      title_(title),
+      url_(url) {}
 
 WebShareTarget::~WebShareTarget() {}
 
+WebShareTarget::WebShareTarget(WebShareTarget&& other) = default;
+
 bool WebShareTarget::operator==(const WebShareTarget& other) const {
-  return std::tie(manifest_url_, name_, url_template_) ==
-         std::tie(other.manifest_url_, other.name_, other.url_template_);
+  return std::tie(manifest_url_, name_, action_, text_, title_, url_) ==
+         std::tie(other.manifest_url_, other.name_, other.action_, text_,
+                  title_, url_);
 }
 
 std::ostream& operator<<(std::ostream& out, const WebShareTarget& target) {
   return out << "WebShareTarget(GURL(" << target.manifest_url().spec() << "), "
-             << target.name() << ", " << target.url_template() << ")";
+             << target.name() << ", " << target.action() << ", "
+             << target.text() << ", " << target.title() << ", " << target.url()
+             << ")";
 }
