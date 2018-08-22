ae235c1b81f61f4e03dc494dc5e91ea074cacfb6
Kevin Marshall <kmarshall@chromium.org>
2018-08-22 20:49:35 +0000

    [fuchsia] Implement Frame::GetVisibleEntry() API method.
    This method uses the NavigationController API to query the committed
    navigational state on a Frame.
    
    Bug: 852145
    Change-Id: I49f9122753c393ef33d67905353b5ba1afad157a
    Reviewed-on: https://chromium-review.googlesource.com/1182507
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585237}

--- webrunner/browser/frame_impl.cc
+++ webrunner/browser/frame_impl.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/run_loop.h"
 #include "base/strings/utf_string_conversions.h"
-#include "content/public/browser/navigation_handle.h"
+#include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/web_contents.h"
 #include "ui/aura/layout_manager.h"
 #include "ui/aura/window.h"
@@ -60,6 +60,15 @@ class LayoutManagerImpl : public aura::LayoutManager {
 
   DISALLOW_COPY_AND_ASSIGN(LayoutManagerImpl);
 };
+
+chromium::web::NavigationEntry ConvertContentNavigationEntry(
+    content::NavigationEntry* entry) {
+  DCHECK(entry);
+  chromium::web::NavigationEntry converted;
+  converted.title = base::UTF16ToUTF8(entry->GetTitle());
+  converted.url = entry->GetURL().spec();
+  return converted;
+}
 
 }  // namespace
 
@@ -147,8 +156,15 @@ void FrameImpl::Reload(chromium::web::ReloadType type) {
 }
 
 void FrameImpl::GetVisibleEntry(GetVisibleEntryCallback callback) {
-  NOTIMPLEMENTED();
-  callback(nullptr);
+  content::NavigationEntry* entry =
+      web_contents_->GetController().GetVisibleEntry();
+  if (!entry) {
+    callback(nullptr);
+    return;
+  }
+
+  chromium::web::NavigationEntry output = ConvertContentNavigationEntry(entry);
+  callback(std::make_unique<chromium::web::NavigationEntry>(std::move(output)));
 }
 
 void FrameImpl::DidFinishLoad(content::RenderFrameHost* render_frame_host,
