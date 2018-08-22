dd60bb7027e3da087317b48ce40ba5891a495e33
Vlad Tsyrklevich <vtsyrklevich@chromium.org>
2018-08-22 20:55:36 +0000

    SafeStack: Fix blink v8 initialization
    Currently, the blink v8 initialization code uses a stack address-taken
    variable to read the stack position. This fails with SafeStack because
    it reads an address on the unsafe stack. Make it match the code in v8 to
    read the stack position safely with either ASAN or SafeStack [1] by
    using a compiler intrinsic.
    
    [1] https://crrev.com/c/1162669
    
    Bug: 864705
    Change-Id: Ib972540eb0264d9f737eb8bc40c601b8b2cf1ab0
    Reviewed-on: https://chromium-review.googlesource.com/1179265
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Vlad Tsyrklevich <vtsyrklevich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585239}

--- third_party/blink/renderer/platform/wtf/compiler.h
+++ third_party/blink/renderer/platform/wtf/compiler.h
@@ -64,4 +64,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define NO_SANITIZE_CFI_ICALL
 #endif
 
+#if defined(COMPILER_MSVC)
+#define WTF_NOINLINE __declspec(noinline)
+#else
+#define WTF_NOINLINE __attribute__((noinline))
+#endif
+
 #endif /* WTF_Compiler_h */
