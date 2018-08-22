9670ecc3fac9137d862955906142b3d9000a8856
Helen Li <xunjieli@chromium.org>
2018-08-22 16:43:38 +0000

    Implement Socket::ReadIfReady for HttpProxyClientSocket
    network::SocketDataPump uses Socket::ReadIfReady instead of Socket::Read, and
    expects ReadIfReady to be implemented.
    
    This CL adds the implementation to HttpProxyClientSocket, and fixes the
    incorrect test expectation, which can serve to catch future regressions.
    
    Bug: 875855
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Change-Id: Ic9b79cf8c1290de55d102052ea5c84fcac0f4fbd
    Reviewed-on: https://chromium-review.googlesource.com/1183984
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585075}

--- net/http/http_proxy_client_socket.h
+++ net/http/http_proxy_client_socket.h
@@ -82,6 +82,10 @@ class NET_EXPORT_PRIVATE HttpProxyClientSocket : public ProxyClientSocket {
   int Read(IOBuffer* buf,
            int buf_len,
            CompletionOnceCallback callback) override;
+  int ReadIfReady(IOBuffer* buf,
+                  int buf_len,
+                  CompletionOnceCallback callback) override;
+  int CancelReadIfReady() override;
   int Write(IOBuffer* buf,
             int buf_len,
             CompletionOnceCallback callback,
@@ -125,6 +129,9 @@ class NET_EXPORT_PRIVATE HttpProxyClientSocket : public ProxyClientSocket {
   int DoReadHeadersComplete(int result);
   int DoDrainBody();
   int DoDrainBodyComplete(int result);
+
+  // Returns whether |next_state_| is STATE_DONE.
+  bool CheckDone();
 
   CompletionRepeatingCallback io_callback_;
   State next_state_;
