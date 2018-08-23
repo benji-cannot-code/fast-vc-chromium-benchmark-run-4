8dcbce22c3223c6647c591b4b5f10fe12faf6f78
Sahel Sharify <sahel@chromium.org>
2018-08-23 20:58:45 +0000

    Revert "Fix fast/frames/crash-frameset-CSS-content-property.html"
    This reverts commit 2e954351680196ce8508ce8bb7b47af62c7e77e3.
    
    Reason for revert: "fast/frames/crash-frameset-CSS-content-property.html" is flaky
    
    example failure:
    https://ci.chromium.org/p/chromium/builders/luci.chromium.try/linux_chromium_rel_ng/171496
    
    Original change's description:
    > Fix fast/frames/crash-frameset-CSS-content-property.html
    > 
    > This test switches a <frameset> to a <body>. For framesets, LayoutView::CalculateScrollbarModes
    > forces the user scrollable bit to be false. So, we need to make sure to call
    > LayoutView::SetNeedsPaintPropertyUpdate when the LayoutView's child changes to/from a frameset.
    > 
    > 
    > Bug: 875287
    > Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux-blink-gen-property-trees;luci.chromium.try:linux_layout_tests_layout_ng;luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    > 
    > Change-Id: I5ae0a7673ae8e325242465633749ffcd76ca705f
    > Reviewed-on: https://chromium-review.googlesource.com/1186181
    > Reviewed-by: Philip Rogers <pdr@chromium.org>
    > Commit-Queue: Jianpeng Chao <chaopeng@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#585493}
    
    TBR=pdr@chromium.org,chaopeng@chromium.org
    
    Change-Id: Ibbfd46388eedd04af20242e4c87413880b9d9718
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 875287, 877210
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux-blink-gen-property-trees;luci.chromium.try:linux_layout_tests_layout_ng;luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Reviewed-on: https://chromium-review.googlesource.com/1187283
    Reviewed-by: Sahel Sharify <sahel@chromium.org>
    Commit-Queue: Sahel Sharify <sahel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585612}

--- third_party/blink/renderer/core/layout/layout_frame_set.h
+++ third_party/blink/renderer/core/layout/layout_frame_set.h
@@ -154,9 +154,6 @@ class LayoutFrameSet final : public LayoutBox {
     return false;
   }
 
-  void InsertedIntoTree() override;
-  void WillBeRemovedFromTree() override;
-
   LayoutObjectChildList children_;
 
   GridAxis rows_;
