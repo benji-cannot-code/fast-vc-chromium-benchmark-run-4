4c4c76af17aa1da7ea94a2ccf2c4cefb10d158da
Joe Mason <joenotcharles@chromium.org>
2018-08-23 05:24:29 +0000

    Add chrome_cleaner/scanner dir
    R=csharp
    
    Bug: 830892
    Change-Id: I8af09ba27cbc35d7bff39f4b7b6a9c972f1afc0d
    Reviewed-on: https://chromium-review.googlesource.com/1175202
    Commit-Queue: Joe Mason <joenotcharles@chromium.org>
    Reviewed-by: Chris Sharp <csharp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585405}

---
+++ chrome/chrome_cleaner/scanner/urza_scanner_controller.h
@@ -0,0 +1,43 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_CHROME_CLEANER_SCANNER_URZA_SCANNER_CONTROLLER_H_
+#define CHROME_CHROME_CLEANER_SCANNER_URZA_SCANNER_CONTROLLER_H_
+
+#include <vector>
+
+#include "chrome/chrome_cleaner/constants/uws_id.h"
+#include "chrome/chrome_cleaner/logging/registry_logger.h"
+#include "chrome/chrome_cleaner/scanner/scanner_controller.h"
+#include "chrome/chrome_cleaner/scanner/signature_matcher_api.h"
+#include "chrome/chrome_cleaner/scanner/urza_scanner_impl.h"
+#include "chrome/chrome_cleaner/settings/matching_options.h"
+
+namespace chrome_cleaner {
+
+// The Urza (pre-ESET) implementation of the ScannerController.
+class UrzaScannerController : public ScannerController {
+ public:
+  UrzaScannerController(MatchingOptions* options,
+                        std::unique_ptr<SignatureMatcherAPI> signature_matcher,
+                        RegistryLogger* registry_logger);
+  ~UrzaScannerController() override;
+
+ protected:
+  void StartScan() override;
+
+ private:
+  void FoundUwSCallback(UwSId pup_id);
+
+  std::unique_ptr<SignatureMatcherAPI> signature_matcher_;
+  UrzaScannerImpl scanner_;
+  std::vector<UwSId> found_pups_;
+
+  DISALLOW_COPY_AND_ASSIGN(UrzaScannerController);
+};
+
+}  // namespace chrome_cleaner
+
+#endif  // CHROME_CHROME_CLEANER_SCANNER_URZA_SCANNER_CONTROLLER_H_
