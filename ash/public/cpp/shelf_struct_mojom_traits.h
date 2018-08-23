61ce3b27e4dadfa98ec36541ad020277ac69a29c
Manu Cornet <manucornet@chromium.org>
2018-08-23 00:43:55 +0000

    CrOS: differentiate between running and active apps
    * Add a simple mechanism to keep track of the active shelf item
      in the model
    * Use the existing shelf window watcher to update the active shelf
      item
    * When rendering shelf items in ShelfView, query the active shelf
      item to show the proper indicator
    * Remove the STATUS_ACTIVE value since this is now all managed within
      ash
    
    Bug: 874181
    Bug: 805612
    Change-Id: I089587646208ee03776f95a4f479c5b6d35d82f8
    Reviewed-on: https://chromium-review.googlesource.com/1180274
    Reviewed-by: Martin Barbella <mbarbella@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Manu Cornet <manucornet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585340}

--- ash/public/cpp/shelf_struct_mojom_traits.h
+++ ash/public/cpp/shelf_struct_mojom_traits.h
@@ -67,8 +67,6 @@ struct EnumTraits<ash::mojom::ShelfItemStatus, ash::ShelfItemStatus> {
         return ash::mojom::ShelfItemStatus::CLOSED;
       case ash::STATUS_RUNNING:
         return ash::mojom::ShelfItemStatus::RUNNING;
-      case ash::STATUS_ACTIVE:
-        return ash::mojom::ShelfItemStatus::ACTIVE;
       case ash::STATUS_ATTENTION:
         return ash::mojom::ShelfItemStatus::ATTENTION;
     }
@@ -85,8 +83,6 @@ struct EnumTraits<ash::mojom::ShelfItemStatus, ash::ShelfItemStatus> {
       case ash::mojom::ShelfItemStatus::RUNNING:
         *out = ash::STATUS_RUNNING;
         return true;
-      case ash::mojom::ShelfItemStatus::ACTIVE:
-        *out = ash::STATUS_ACTIVE;
         return true;
       case ash::mojom::ShelfItemStatus::ATTENTION:
         *out = ash::STATUS_ATTENTION;
