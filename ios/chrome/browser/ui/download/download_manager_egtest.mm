652a85bd17b7355e9d18a1cb6d8f167b2cff2890
Eugene But <eugenebut@google.com>
2018-08-23 16:48:27 +0000

    Added Accessibility EG Tests for Download Manager UI.
    Added the following tests:
     - testAccessibilityOnNotStartedDownloadToolbar
     - testAccessibilityOnCompletedDownloadToolbar
    
    Bug: 854232
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I2c49197f02881548cdae817a9753798507ddfc3b
    Reviewed-on: https://chromium-review.googlesource.com/1184323
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585517}

--- ios/chrome/browser/ui/download/download_manager_egtest.mm
+++ ios/chrome/browser/ui/download/download_manager_egtest.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/chrome_url_constants.h"
 #include "ios/chrome/grit/ios_strings.h"
 #import "ios/chrome/test/app/tab_test_util.h"
+#include "ios/chrome/test/earl_grey/accessibility_util.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
@@ -159,6 +160,32 @@ bool WaitForOpenInButton() {
   [[GREYUIThreadExecutor sharedInstance] drainUntilIdle];
 
   GREYAssert(WaitForOpenInButton(), @"Open in... button did not show up");
+}
+
+// Tests accessibility on Download Manager UI when download is not started.
+- (void)testAccessibilityOnNotStartedDownloadToolbar {
+  [ChromeEarlGrey loadURL:self.testServer->GetURL("/")];
+  [ChromeEarlGrey waitForWebViewContainingText:"Download"];
+  [ChromeEarlGrey tapWebViewElementWithID:@"download"];
+
+  [[EarlGrey selectElementWithMatcher:DownloadButton()]
+      assertWithMatcher:grey_notNil()];
+
+  chrome_test_util::VerifyAccessibilityForCurrentScreen();
+}
+
+// Tests accessibility on Download Manager UI when download is complete.
+- (void)testAccessibilityOnCompletedDownloadToolbar {
+  [ChromeEarlGrey loadURL:self.testServer->GetURL("/")];
+  [ChromeEarlGrey waitForWebViewContainingText:"Download"];
+  [ChromeEarlGrey tapWebViewElementWithID:@"download"];
+
+  [[EarlGrey selectElementWithMatcher:DownloadButton()]
+      performAction:grey_tap()];
+
+  GREYAssert(WaitForOpenInButton(), @"Open in... button did not show up");
+
+  chrome_test_util::VerifyAccessibilityForCurrentScreen();
 }
 
 @end
