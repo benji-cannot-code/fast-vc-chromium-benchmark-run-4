4d4668c8852c3d225488f76745c47307dfe1daa0
Trent Apted <tapted@chromium.org>
2018-08-23 00:13:22 +0000

    base::CheckedObserver: Follow-up review comments
    Follow-ups to review comments in https://crrev.com/c/1053338
    
    Bug: 842987
    Change-Id: I1cf78667887bb86555d3f9043e2afe5d8eacf2bb
    Reviewed-on: https://chromium-review.googlesource.com/1184590
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585327}

--- base/observer_list_internal.h
+++ base/observer_list_internal.h
@@ -40,8 +40,6 @@ class BASE_EXPORT UncheckedObserverAdapter {
  private:
   void* ptr_;
 
-  // Although copying works, disallow it to be consistent with
-  // CheckedObserverAdapter.
   DISALLOW_COPY_AND_ASSIGN(UncheckedObserverAdapter);
 };
 
@@ -62,7 +60,7 @@ class BASE_EXPORT CheckedObserverAdapter {
 
   void MarkForRemoval() {
     DCHECK(weak_ptr_);
-    weak_ptr_.reset();
+    weak_ptr_ = nullptr;
   }
 
   bool IsMarkedForRemoval() const {
