bf90c976eee7263e687957d6e323160f4dadcc16
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2018-08-23 08:36:05 +0000

    [iOS][AF] Add bouncing to suggestions
    Enable bouncing in autofill suggestions scrollview.
    
    Bug: 876657
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I8bbb27c6c2d567bc41e4931a85cefb97453030c3
    Reviewed-on: https://chromium-review.googlesource.com/1184716
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585430}

--- ios/chrome/browser/autofill/form_suggestion_view.mm
+++ ios/chrome/browser/autofill/form_suggestion_view.mm
@@ -70,7 +70,6 @@ const CGFloat kSuggestionHorizontalMargin = 6;
 - (void)setupSubviews {
   self.showsVerticalScrollIndicator = NO;
   self.showsHorizontalScrollIndicator = NO;
-  self.bounces = NO;
   self.canCancelContentTouches = YES;
 
   UIStackView* stackView = [[UIStackView alloc] initWithArrangedSubviews:@[]];
