5390bff4092424502c1baf98281de7ef876aef11
Gang Wu <gangwu@chromium.org>
2018-08-23 23:05:53 +0000

    [Feed] Remove old storage implementation
    Remove old storage implementation for both Java and C++.
    
    Bug: 828935
    Change-Id: Ib6144e206b513272510763559337dc6aa16e06b6
    Reviewed-on: https://chromium-review.googlesource.com/1185794
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585645}

--- components/feed/core/feed_host_service.h
+++ components/feed/core/feed_host_service.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/feed/core/feed_journal_database.h"
 #include "components/feed/core/feed_networking_host.h"
 #include "components/feed/core/feed_scheduler_host.h"
-#include "components/feed/core/feed_storage_database.h"
 #include "components/keyed_service/core/keyed_service.h"
 
 namespace feed {
@@ -29,7 +28,6 @@ class FeedHostService : public KeyedService {
   FeedHostService(std::unique_ptr<FeedImageManager> image_manager,
                   std::unique_ptr<FeedNetworkingHost> networking_host,
                   std::unique_ptr<FeedSchedulerHost> scheduler_host,
-                  std::unique_ptr<FeedStorageDatabase> storage_database,
                   std::unique_ptr<FeedContentDatabase> content_database,
                   std::unique_ptr<FeedJournalDatabase> journal_database);
   ~FeedHostService() override;
@@ -37,7 +35,6 @@ class FeedHostService : public KeyedService {
   FeedImageManager* GetImageManager();
   FeedNetworkingHost* GetNetworkingHost();
   FeedSchedulerHost* GetSchedulerHost();
-  FeedStorageDatabase* GetStorageDatabase();
   FeedContentDatabase* GetContentDatabase();
   FeedJournalDatabase* GetJournalDatabase();
 
@@ -45,7 +42,6 @@ class FeedHostService : public KeyedService {
   std::unique_ptr<FeedImageManager> image_manager_;
   std::unique_ptr<FeedNetworkingHost> networking_host_;
   std::unique_ptr<FeedSchedulerHost> scheduler_host_;
-  std::unique_ptr<FeedStorageDatabase> storage_database_;
   std::unique_ptr<FeedContentDatabase> content_database_;
   std::unique_ptr<FeedJournalDatabase> journal_database_;
 
