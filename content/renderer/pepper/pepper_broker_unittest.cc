142bc4344030d445bdb8a82af7d65581d3e1e2e5
Wez <wez@chromium.org>
2018-08-23 00:07:07 +0000

    Add missing ScopedTaskEnvironment to stop tests leaking TaskScheduler.
    Bug: 875486
    Change-Id: I448361ccdb91b83ab71c6eda34066e6e76c278de
    Reviewed-on: https://chromium-review.googlesource.com/1185490
    Reviewed-by: Raymes Khoury <raymes@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585323}

--- content/renderer/pepper/pepper_broker_unittest.cc
+++ content/renderer/pepper/pepper_broker_unittest.cc
@@ -4,13 +4,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "content/renderer/pepper/pepper_broker.h"
-#include "base/message_loop/message_loop.h"
 
 #if defined(OS_POSIX)
 #include <fcntl.h>
 #include <sys/socket.h>
 #endif  // defined(OS_POSIX)
 
+#include "base/test/scoped_task_environment.h"
 #include "build/build_config.h"
 #include "content/test/mock_render_process.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -19,7 +19,10 @@ namespace content {
 
 class PepperBrokerTest : public ::testing::Test {
  protected:
-  base::MessageLoopForIO message_loop_;
+  PepperBrokerTest()
+      : task_environment_(
+            base::test::ScopedTaskEnvironment::MainThreadType::IO) {}
+  base::test::ScopedTaskEnvironment task_environment_;
   // We need a render process for ppapi::proxy::ProxyChannel to work.
   MockRenderProcess mock_process_;
 };
