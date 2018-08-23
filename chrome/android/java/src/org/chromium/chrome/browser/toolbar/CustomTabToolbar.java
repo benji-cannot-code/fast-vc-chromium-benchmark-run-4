494ee4abe5d43050c1519f71f8c8bc16cf0ae6c0
Theresa <twellington@chromium.org>
2018-08-23 15:59:05 +0000

    [Modern cleanup] Delete some non-modern phone-only toolbar code
    BUG=871802
    
    Change-Id: Ia96c4bd872ba9d0e2e7529b5b27b91d8b8ffbc7e
    Reviewed-on: https://chromium-review.googlesource.com/1185891
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585500}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/CustomTabToolbar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/CustomTabToolbar.java
@@ -828,7 +828,7 @@ public class CustomTabToolbar extends ToolbarLayout implements LocationBar,
 
     @Override
     public boolean useModernDesign() {
-        return false;
+        return true;
     }
 
     @Override
