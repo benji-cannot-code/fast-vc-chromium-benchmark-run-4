2c0617a5ed68ddd61b5489859a63a14c04d526a7
Weidong Guo <weidongg@chromium.org>
2018-08-23 17:59:09 +0000

    Fix app list animation issue for toggling overview mode
    Background:
    When active window exists, toggling overview mode will show a
    enlarge/shrink animation instead of slide up/down animation. In this
    case, app list should stay invisible during the animation.
    
    Changes:
    Only run animation when overview mode is using slide animation.
    Otherwise, set the animation duration to 0 so that app list will
    immediately disappear when overview mode is on and will immediately
    show up after overview mode animation when overview mode is off.
    
    Bug: 876781
    Change-Id: Ifc7af8838beead1dbd1b6d70ee46eec3af19dc19
    Reviewed-on: https://chromium-review.googlesource.com/1186036
    Commit-Queue: Weidong Guo <weidongg@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585540}

--- ash/app_list/app_list_controller_impl.cc
+++ ash/app_list/app_list_controller_impl.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "ash/voice_interaction/voice_interaction_controller.h"
 #include "ash/wallpaper/wallpaper_controller.h"
+#include "ash/wm/overview/window_selector_controller.h"
 #include "ash/wm/tablet_mode/tablet_mode_controller.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_macros.h"
@@ -499,14 +500,27 @@ void AppListControllerImpl::OnOverviewModeStarting() {
     DismissAppList();
     return;
   }
-  presenter_.ScheduleOverviewModeAnimation(true /* start */);
+
+  // Only animate the app list when overview mode is using slide animation.
+  presenter_.ScheduleOverviewModeAnimation(
+      true /* start */, Shell::Get()
+                            ->window_selector_controller()
+                            ->window_selector()
+                            ->use_slide_animation() /* animate */);
 }
 
 void AppListControllerImpl::OnOverviewModeEnding() {
   in_overview_mode_ = false;
 
-  if (IsHomeLauncherEnabledInTabletMode())
-    presenter_.ScheduleOverviewModeAnimation(false /* start */);
+  if (!IsHomeLauncherEnabledInTabletMode())
+    return;
+
+  // Only animate the app list when overview mode is using slide animation.
+  presenter_.ScheduleOverviewModeAnimation(
+      false /* start */, Shell::Get()
+                             ->window_selector_controller()
+                             ->window_selector()
+                             ->use_slide_animation() /* animate */);
 }
 
 void AppListControllerImpl::OnTabletModeStarted() {
