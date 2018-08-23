bcece7f14ab247009a2965088a639beadb86df70
Anna Offenwanger <offenwanger@chromium.org>
2018-08-23 00:41:09 +0000

    Remove offenwanger from TODOs
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I911ec971475379b5627e5bfc91bdf612127f21f9
    Reviewed-on: https://chromium-review.googlesource.com/1182678
    Commit-Queue: Anna Offenwanger <offenwanger@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585339}

--- third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
+++ third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
@@ -831,7 +831,8 @@ ScriptPromise WebGLRenderingContextBase::setCompatibleXRDevice(
     compatible_xr_device_ = xr_device;
     return ScriptPromise::CastUndefined(script_state);
   } else {
-    // TODO(offenwanger): Trigger context loss and recreate on compatible GPU.
+    // TODO(http://crbug.com/876140) Trigger context loss and recreate on
+    // compatible GPU.
     return ScriptPromise::RejectWithDOMException(
         script_state,
         DOMException::Create(
@@ -1020,7 +1021,8 @@ WebGLRenderingContextBase::WebGLRenderingContextBase(
       context_type_(context_type) {
   DCHECK(context_provider);
 
-  // TODO(offenwanger) Make sure this is being created on a compatible adapter.
+  // TODO(http://crbug.com/876140) Make sure this is being created on a
+  // compatible adapter.
   compatible_xr_device_ =
       static_cast<XRDevice*>(requested_attributes.compatible_xr_device.Get());
 
@@ -1114,8 +1116,8 @@ void WebGLRenderingContextBase::InitializeNewContext() {
   DCHECK(!isContextLost());
   DCHECK(GetDrawingBuffer());
 
-  // TODO(offenwanger): Check if compatible_xr_device needs to be taken into
-  // account here.
+  // TODO(http://crbug.com/876140) Does compatible_xr_device needs to be taken
+  // into account here?
 
   marked_canvas_dirty_ = false;
   animation_frame_in_progress_ = false;
@@ -1575,7 +1577,8 @@ bool WebGLRenderingContextBase::PaintRenderingResultsToCanvas(
 
 bool WebGLRenderingContextBase::ContextCreatedOnCompatibleAdapter(
     const XRDevice* device) {
-  // TODO(offenwanger): Determine if device is compatible with current context.
+  // TODO(http://crbug.com/876140) Determine if device is compatible with
+  // current context.
   return true;
 }
 
