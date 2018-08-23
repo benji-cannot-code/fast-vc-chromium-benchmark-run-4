87317e663b7a77acd622fbddf9d6e4a0ccd87720
Quentin Fiard <qfiard@google.com>
2018-08-23 01:05:54 +0000

    Fix https://bugs.chromium.org/p/chromium/issues/detail?id=872922
    I'll request a merge into M69 when this is in so we can restart experiments quickly.
    
    Bug: 872922
    Change-Id: Icb269a5f6499a5a58a2a51f3348b23e15f20c035
    Reviewed-on: https://chromium-review.googlesource.com/1183912
    Commit-Queue: Quentin Fiard <qfiard@google.com>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585347}

--- chrome/browser/google/google_search_domain_mixing_metrics_emitter.cc
+++ chrome/browser/google/google_search_domain_mixing_metrics_emitter.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/google/google_search_domain_mixing_metrics_emitter.h"
 
+#include "base/logging.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "components/history/core/browser/domain_mixing_metrics.h"
 #include "components/history/core/browser/history_backend.h"
@@ -53,7 +54,9 @@ GoogleSearchDomainMixingMetricsEmitter::GoogleSearchDomainMixingMetricsEmitter(
     : prefs_(prefs),
       history_service_(history_service),
       ui_thread_task_runner_(content::BrowserThread::GetTaskRunnerForThread(
-          content::BrowserThread::UI)) {}
+          content::BrowserThread::UI)) {
+  DCHECK(history_service_);
+}
 
 GoogleSearchDomainMixingMetricsEmitter::
     ~GoogleSearchDomainMixingMetricsEmitter() {}
