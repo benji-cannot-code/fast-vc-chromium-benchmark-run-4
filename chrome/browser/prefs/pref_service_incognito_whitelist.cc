db3da246f0d13731f7875c74e521a7cc9d4a879f
Ramin Halavati <rhalavati@chromium.org>
2018-08-23 13:21:19 +0000

    Move Reading List preference to incognito whitelist.
    Reading List preference is moved to the persistent storage list for
    incognito mode.
    This CL does NOT make any change of behavior.
    
    Bug: 861722
    Change-Id: Ie29e7c86cedc19842f64939a1acf60e7835bffc1
    Reviewed-on: https://chromium-review.googlesource.com/1172132
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585465}

--- chrome/browser/prefs/pref_service_incognito_whitelist.cc
+++ chrome/browser/prefs/pref_service_incognito_whitelist.cc
@@ -132,6 +132,9 @@ const char* const kPersistentPrefNames[] = {
     // they need to be persisted.
     rappor::prefs::kRapporCohortSeed, rappor::prefs::kRapporSecret,
 
+    // Reading list preferences are common between incognito and regular mode.
+    reading_list::prefs::kReadingListHasUnseenEntries,
+
     // Although UKMs are not collected in incognito, theses preferences may be
     // changed by UMA/Sync/Unity consent, and need to be the same between
     // incognito and regular modes.
@@ -590,9 +593,6 @@ const char* const kTemporaryIncognitoWhitelist[] = {
     // components/onc/onc_pref_names.h
     onc::prefs::kDeviceOpenNetworkConfiguration,
     onc::prefs::kOpenNetworkConfiguration,
-
-    // components/reading_list/core/reading_list_pref_names.h
-    reading_list::prefs::kReadingListHasUnseenEntries,
 
     // components/search_engines/search_engines_pref_names.h
     prefs::kSyncedDefaultSearchProviderGUID,
