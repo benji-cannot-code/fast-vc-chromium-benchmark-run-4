657f69a9bb8e2c2b69e24c26fb00c6a44d820423
Takumi Fujimoto <takumif@chromium.org>
2018-08-24 20:30:23 +0000

    [Harmony Cast Dialog] Start and stop casting by clicking on a sink
    Before this CL, starting and stopping casting was done with the button
    at the bottom of the dialog. This CL removes the button, and allows the
    user to start/stop by clicking on a sink.
    
    A sink with a session is indicated by a stop icon replacing the device
    type icon for the sink. After this CL the user can click anywhere on an
    active sink to stop its session, but in the future we will likely make
    only the stop icon clickable. When there is an issue, an info icon
    replaces it instead.
    
    The stop icon used is a placeholder, since we don't have the vector icon
    used in the mocks yet.
    
    Bug: 872831
    
    Change-Id: I638204db4cf751060be86e701c834e0abab2050e
    Reviewed-on: https://chromium-review.googlesource.com/1168508
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Reviewed-by: Derek Cheng <imcheng@chromium.org>
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585968}

--- chrome/browser/ui/views/hover_button.h
+++ chrome/browser/ui/views/hover_button.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_VIEWS_HOVER_BUTTON_H_
 #define CHROME_BROWSER_UI_VIEWS_HOVER_BUTTON_H_
 
+#include "base/gtest_prod_util.h"
 #include "base/strings/string16.h"
 #include "ui/views/controls/button/menu_button.h"
 #include "ui/views/controls/button/menu_button_listener.h"
@@ -14,6 +15,11 @@ namespace gfx {
 enum ElideBehavior;
 class ImageSkia;
 }  // namespace gfx
+
+namespace media_router {
+FORWARD_DECLARE_TEST(CastDialogSinkButtonTest, SetTitleLabel);
+FORWARD_DECLARE_TEST(CastDialogSinkButtonTest, SetStatusLabel);
+}  // namespace media_router
 
 namespace views {
 class ButtonListener;
@@ -109,9 +115,15 @@ class HoverButton : public views::MenuButton, public views::MenuButtonListener {
   void OnBoundsChanged(const gfx::Rect& previous_bounds) override;
 
   views::StyledLabel* title() const { return title_; }
+  views::Label* subtitle() const { return subtitle_; }
   views::View* icon_view() const { return icon_view_; }
 
  private:
+  FRIEND_TEST_ALL_PREFIXES(media_router::CastDialogSinkButtonTest,
+                           SetTitleLabel);
+  FRIEND_TEST_ALL_PREFIXES(media_router::CastDialogSinkButtonTest,
+                           SetStatusLabel);
+
   views::StyledLabel* title_;
   views::Label* subtitle_;
   views::View* icon_view_;
