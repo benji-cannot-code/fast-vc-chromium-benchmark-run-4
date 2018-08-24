a267d1a1dcca23b57d5abe2cfc392d55a6cdd7bf
Yi Su <mrsuyi@google.com>
2018-08-24 22:51:57 +0000

    Add image_fetcher::IOSImageDataFetcherWrapper to ImageFetchTabHelper
    ImageFetchTabHelper will be able to get image data by both JavaScript and
    image_fetcher::IOSImageDataFetcherWrapper. The image_fetcher ivar of BVC
    can be removed once this is completed.
    
    Bug: 163201
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ie5bb9279c5fb19498d3cde4c37cdf8944baa9e98
    Reviewed-on: https://chromium-review.googlesource.com/1181571
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Yi Su <mrsuyi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586032}

--- ios/web/public/test/fakes/test_browser_state.h
+++ ios/web/public/test/fakes/test_browser_state.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/ref_counted.h"
 #include "ios/web/public/browser_state.h"
+#include "services/network/public/cpp/shared_url_loader_factory.h"
 
 namespace web {
 class TestBrowserState : public BrowserState {
@@ -19,13 +20,24 @@ class TestBrowserState : public BrowserState {
   bool IsOffTheRecord() const override;
   base::FilePath GetStatePath() const override;
   net::URLRequestContextGetter* GetRequestContext() override;
+  scoped_refptr<network::SharedURLLoaderFactory> GetSharedURLLoaderFactory()
+      override;
+
+  // Sets a SharedURLLoaderFactory for test.
+  void SetSharedURLLoaderFactory(
+      scoped_refptr<network::SharedURLLoaderFactory> shared_url_loader_factory);
 
   // Makes |IsOffTheRecord| return the given flag value.
   void SetOffTheRecord(bool flag);
 
  private:
   scoped_refptr<net::URLRequestContextGetter> request_context_;
+
   bool is_off_the_record_;
+
+  // A SharedURLLoaderFactory for test.
+  scoped_refptr<network::SharedURLLoaderFactory>
+      test_shared_url_loader_factory_;
 };
 }  // namespace web
 
--- ios/web/public/test/web_test_with_web_state.h
+++ ios/web/public/test/web_test_with_web_state.h
@@ -24,8 +24,11 @@ class WebState;
 class WebTestWithWebState : public WebTest,
                             public base::MessageLoop::TaskObserver {
  protected:
-  WebTestWithWebState();
-  explicit WebTestWithWebState(std::unique_ptr<web::WebClient> web_client);
+  explicit WebTestWithWebState(
+      TestWebThreadBundle::Options = TestWebThreadBundle::Options::DEFAULT);
+  WebTestWithWebState(
+      std::unique_ptr<web::WebClient> web_client,
+      TestWebThreadBundle::Options = TestWebThreadBundle::Options::DEFAULT);
   ~WebTestWithWebState() override;
 
   // WebTest overrides.
