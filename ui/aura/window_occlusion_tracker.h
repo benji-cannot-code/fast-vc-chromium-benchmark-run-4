d10b3d1c142902290a0a2ee817955eb08d4bce68
Francois Doray <fdoray@chromium.org>
2018-08-24 21:54:46 +0000

    aura: Do not allow occlusion by shaped windows.
    Preivously, the shape of a window was not taken into account when
    computing occlusion state. A window with a shape that didn't fully
    cover its bounds could wrongly be considered to occlude another
    window underneath it.
    
    With this CL, shaped windows can't occlude other windows (similar
    to windows that are animated or not axis-aligned).
    
    Bug: 864508
    Change-Id: I01ff1afda438982964cdc2d786f37caaa03677be
    Reviewed-on: https://chromium-review.googlesource.com/1173318
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586011}

--- ui/aura/window_occlusion_tracker.h
+++ ui/aura/window_occlusion_tracker.h
@@ -142,6 +142,10 @@ class AURA_EXPORT WindowOcclusionTracker : public ui::LayerAnimationObserver,
                                   bool assume_parent_opaque = false,
                                   bool assume_window_opaque = false) const;
 
+  // Returns true if changing the opacity or alpha state of |window| could
+  // affect the occlusion state of a tracked window.
+  bool WindowOpacityChangeMayAffectOcclusionStates(Window* window) const;
+
   // Returns true if changing the transform, bounds or stacking order of
   // |window| could affect the occlusion state of a tracked window.
   bool WindowMoveMayAffectOcclusionStates(Window* window) const;
@@ -175,6 +179,7 @@ class AURA_EXPORT WindowOcclusionTracker : public ui::LayerAnimationObserver,
                              ui::PropertyChangeReason reason) override;
   void OnWindowOpacitySet(Window* window,
                           ui::PropertyChangeReason reason) override;
+  void OnWindowAlphaShapeSet(Window* window) override;
   void OnWindowTransformed(Window* window,
                            ui::PropertyChangeReason reason) override;
   void OnWindowStackingChanged(Window* window) override;
