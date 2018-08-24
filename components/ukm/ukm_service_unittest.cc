77003fad2ffb426cf31de4073dc0a2b1cd030393
Charlie Harrison <csharrison@chromium.org>
2018-08-24 13:50:06 +0000

    Add tab_id/previous_source_id/opener_source_id to UkmSource
    This CL adds three new fields to UkmSource. A follow-up will propagate
    changes to the underlying proto fields.
    
    Bug: 873316
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ic691221c8165df7cee16b089407d8ddd346e78c3
    Reviewed-on: https://chromium-review.googlesource.com/1175033
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Robert Kaplow (slow) <rkaplow@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585824}

--- components/ukm/ukm_service_unittest.cc
+++ components/ukm/ukm_service_unittest.cc
@@ -62,6 +62,11 @@ class TestRecordingHelper {
     recorder_->UpdateSourceURL(source_id, url);
   }
 
+  void RecordNavigation(SourceId source_id,
+                        const UkmSource::NavigationData& navigation_data) {
+    recorder_->RecordNavigation(source_id, navigation_data);
+  }
+
  private:
   UkmRecorder* recorder_;
 
@@ -254,10 +259,12 @@ TEST_F(UkmServiceTest, SourceSerialization) {
   service.EnableRecording(/*extensions=*/false);
   service.EnableReporting();
 
+  UkmSource::NavigationData navigation_data;
+  navigation_data.initial_url = GURL("https://google.com/initial");
+  navigation_data.url = GURL("https://google.com/final");
+
   ukm::SourceId id = GetWhitelistedSourceId(0);
-  recorder.UpdateSourceURL(id, GURL("https://google.com/initial"));
-  recorder.UpdateSourceURL(id, GURL("https://google.com/intermediate"));
-  recorder.UpdateSourceURL(id, GURL("https://google.com/foobar"));
+  recorder.RecordNavigation(id, navigation_data);
 
   service.Flush();
   EXPECT_EQ(GetPersistedLogCount(), 1);
@@ -268,7 +275,7 @@ TEST_F(UkmServiceTest, SourceSerialization) {
   const Source& proto_source = proto_report.sources(0);
 
   EXPECT_EQ(id, proto_source.id());
-  EXPECT_EQ(GURL("https://google.com/foobar").spec(), proto_source.url());
+  EXPECT_EQ(GURL("https://google.com/final").spec(), proto_source.url());
   EXPECT_FALSE(proto_source.has_initial_url());
 }
 
@@ -443,9 +450,10 @@ TEST_F(UkmServiceTest, RecordInitialUrl) {
     service.EnableReporting();
 
     ukm::SourceId id = GetWhitelistedSourceId(0);
-    recorder.UpdateSourceURL(id, GURL("https://google.com/initial"));
-    recorder.UpdateSourceURL(id, GURL("https://google.com/intermediate"));
-    recorder.UpdateSourceURL(id, GURL("https://google.com/foobar"));
+    UkmSource::NavigationData navigation_data;
+    navigation_data.initial_url = GURL("https://google.com/initial");
+    navigation_data.url = GURL("https://google.com/final");
+    recorder.RecordNavigation(id, navigation_data);
 
     service.Flush();
     EXPECT_EQ(GetPersistedLogCount(), 1);
@@ -455,7 +463,7 @@ TEST_F(UkmServiceTest, RecordInitialUrl) {
     const Source& proto_source = proto_report.sources(0);
 
     EXPECT_EQ(id, proto_source.id());
-    EXPECT_EQ(GURL("https://google.com/foobar").spec(), proto_source.url());
+    EXPECT_EQ(GURL("https://google.com/final").spec(), proto_source.url());
     EXPECT_EQ(should_record_initial_url, proto_source.has_initial_url());
     if (should_record_initial_url) {
       EXPECT_EQ(GURL("https://google.com/initial").spec(),
