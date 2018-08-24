efab943de13fc3866c93e8557d898d47a995ada5
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-08-24 11:36:02 +0000

    Import wpt@1bc6d9975ef38c3ade2986dff737b4421c6352df
    Using wpt-import in Chromium 03168d86dfe524a9e90f38fb27f3b5f01c29eded.
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/23218
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    yhirano@chromium.org, mkwst@chromium.org, japhet@chromium.org:
      external/wpt/fetch
    
    NOAUTOREVERT=true
    TBR=lukebjerring
    
    No-Export: true
    Change-Id: If8345598e240e21411a867bc432e0df20c00ab80
    Reviewed-on: https://chromium-review.googlesource.com/1188084
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585796}

---
+++ third_party/WebKit/LayoutTests/external/wpt/fetch/api/response/response-from-stream.any.js
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: global=window,worker
+
+"use strict";
+
+test(() => {
+  const stream = new ReadableStream();
+  stream.getReader();
+  assert_throws(new TypeError(), () => new Response(stream));
+}, "Constructing a Response with a stream on which getReader() is called");
+
+test(() => {
+  const stream = new ReadableStream();
+  stream.getReader().read();
+  assert_throws(new TypeError(), () => new Response(stream));
+}, "Constructing a Response with a stream on which read() is called");
+
+promise_test(async () => {
+  const stream = new ReadableStream({ pull: c => c.enqueue(new Uint8Array()) }),
+        reader = stream.getReader();
+  await reader.read();
+  reader.releaseLock();
+  assert_throws(new TypeError(), () => new Response(stream));
+}, "Constructing a Response with a stream on which read() and releaseLock() are called");
