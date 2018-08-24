513308f72df79b630b43e2e1fa72e71f1a5472a0
Wez <wez@chromium.org>
2018-08-24 23:57:42 +0000

    Check that TaskScheduler is not leaked between tests, or test-cases.
    This is a reland of a2a4e75d2752687d91e1272450bdcc6e48b33739 with a
    work-around for cronet_tests' sharing libbase.so with libcronet.so in
    component builds.
    
    - Move some APIs off of base::TestSuite, that we unnecessarily public.
    - Rename the various internal TestEventListeners to express purpose.
    - Add CheckForLeakedGlobals check, and DisableCheckForLeakedGlobals API.
      The latter is called by the content::ContentTestSuiteBase, since the
      various browser- and ui-tests running under that suite base run more
      like the browser itself, and so are expected to let global singletons
      leak.
    
    If tests fail or flake due to this patch, then this indicates that the
    test is actually leaking global state that may break other tests if they
    happen to run in the same process, leading to hard-to-diagnose flakes.
    
    There are two main failure cases:
    If the test directly or indirectly uses TaskScheduler, but does not
    create a ScopedTaskEnvironment, then please add one to the test, or to
    the test base-class, as appropriate, to fix it.
    If the test suite cannot be fixed in this way then please add a call to
    DisableCheckForLeakedGlobals(), with a comment linking to a bug for the
    issue.
    
    TBR: gab, sky, mef
    Bug: 875486, 877355, 744567
    Change-Id: Iaea38d24ede271c248a3abb0b3f7ee931c2538f5
    Reviewed-on: https://chromium-review.googlesource.com/1187783
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586065}

--- components/cronet/run_all_unittests.cc
+++ components/cronet/run_all_unittests.cc
@@ -9,6 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 int main(int argc, char** argv) {
   base::TestSuite test_suite(argc, argv);
+#if defined(CRONET_TESTS_IMPLEMENTATION) && defined(COMPONENT_BUILD)
+  // In component builds cronet_tests and libcronet.so share various libraries,
+  // so globals initialized by libcronet.so are visible to the TestSuite.
+  test_suite.DisableCheckForLeakedGlobals();
+#endif
   return base::LaunchUnitTests(
       argc, argv,
       base::BindOnce(&base::TestSuite::Run, base::Unretained(&test_suite)));
--- extensions/shell/test/shell_test_launcher_delegate.cc
+++ extensions/shell/test/shell_test_launcher_delegate.cc
@@ -13,7 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace extensions {
 
 int AppShellTestLauncherDelegate::RunTestSuite(int argc, char** argv) {
-  return base::TestSuite(argc, argv).Run();
+  base::TestSuite test_suite(argc, argv);
+  // Browser tests are expected not to tear-down various globals.
+  test_suite.DisableCheckForLeakedGlobals();
+  return test_suite.Run();
 }
 
 bool AppShellTestLauncherDelegate::AdjustChildProcessCommandLine(
