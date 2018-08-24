2d2f6e67cabaa6815d0919e6afdfefd32d634efa
Lei Zhang <thestig@chromium.org>
2018-08-24 15:28:03 +0000

    Avoid UTF8 to UTF16 to UTF8 conversions with base::Value.
    There are a couple places that starts with UTF8 data and converts them
    to UTF16 before passing them to base::Value. base::Value turns around
    and converts it back to UTF8.
    
    Fix some nits along the way.
    
    Change-Id: I10ad741de22aecc04b37fa633ab0033a26acc688
    Reviewed-on: https://chromium-review.googlesource.com/1188033
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585848}

--- components/printing/renderer/print_render_frame_helper.cc
+++ components/printing/renderer/print_render_frame_helper.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/single_thread_task_runner.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/stringprintf.h"
-#include "base/strings/utf_string_conversions.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "build/build_config.h"
 #include "components/grit/components_resources.h"
@@ -635,9 +634,8 @@ void PrintRenderFrameHelper::PrintHeaderAndFooter(
   blink::WebWidgetClient web_widget_client;
   blink::WebFrameWidget::Create(&web_widget_client, frame);
 
-  base::Value html(base::UTF8ToUTF16(
-      ui::ResourceBundle::GetSharedInstance().GetRawDataResource(
-          IDR_PRINT_HEADER_FOOTER_TEMPLATE_PAGE)));
+  base::Value html(ui::ResourceBundle::GetSharedInstance().GetRawDataResource(
+      IDR_PRINT_HEADER_FOOTER_TEMPLATE_PAGE));
   // Load page with script to avoid async operations.
   ExecuteScript(frame, kPageLoadScriptFormat, html);
 
@@ -1851,7 +1849,6 @@ bool PrintRenderFrameHelper::UpdatePrintSettings(
   }
 
   bool source_is_html = !PrintingNodeOrPdfFrame(frame, node);
-
   if (!source_is_html) {
     modified_job_settings.MergeDictionary(job_settings);
     modified_job_settings.SetBoolean(kSettingHeaderFooterEnabled, false);
@@ -2409,7 +2406,7 @@ void PrintRenderFrameHelper::SetPrintPagesParams(
                                              settings.params.document_cookie));
 }
 
-PrintRenderFrameHelper::ScriptingThrottler::ScriptingThrottler() : count_(0) {}
+PrintRenderFrameHelper::ScriptingThrottler::ScriptingThrottler() = default;
 
 bool PrintRenderFrameHelper::ScriptingThrottler::IsAllowed(
     blink::WebLocalFrame* frame) {
@@ -2443,7 +2440,7 @@ bool PrintRenderFrameHelper::ScriptingThrottler::IsAllowed(
   }
 
   blink::WebString message(
-      blink::WebString::FromUTF8("Ignoring too frequent calls to print()."));
+      blink::WebString::FromASCII("Ignoring too frequent calls to print()."));
   frame->AddMessageToConsole(blink::WebConsoleMessage(
       blink::WebConsoleMessage::kLevelWarning, message));
   return false;
