b143446011f567f287e32075b9b9e883aa3f4462
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-08-24 06:49:07 +0000

    Import wpt@34f9b93c2749043ba68485dea92d1fb554075e60
    Using wpt-import in Chromium aa6aaad888ee4619f709de684992cac9b5d69075.
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/23216
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    TBR=foolip
    
    No-Export: true
    Change-Id: Ibc8838d43dcf7a2f26468879347785e0bfe35b64
    Reviewed-on: https://chromium-review.googlesource.com/1187896
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585727}

---
+++ third_party/WebKit/LayoutTests/external/wpt/html/webappapis/dynamic-markup-insertion/opening-the-input-stream/bailout-exception-vs-return-xml.window.js
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+async_test(t => {
+  const iframe = document.body.appendChild(document.createElement("iframe"));
+  t.add_cleanup(() => { iframe.remove(); });
+  self.testSynchronousScript = t.step_func_done(() => {
+    assert_throws("InvalidStateError", () => {
+      iframe.contentDocument.open();
+    }, "opening an XML document should throw");
+  });
+  iframe.src = "resources/bailout-order-xml-with-synchronous-script-frame.xhtml";
+}, "document.open should throw an InvalidStateError with XML document even when there is an active parser executing script");
+
+for (const ev of ["beforeunload", "pagehide", "unload"]) {
+  async_test(t => {
+    const iframe = document.body.appendChild(document.createElement("iframe"));
+    t.add_cleanup(() => { iframe.remove(); });
+    iframe.addEventListener("load", t.step_func(() => {
+      iframe.contentWindow.addEventListener(ev, t.step_func_done(() => {
+        assert_throws("InvalidStateError", () => {
+          iframe.contentDocument.open();
+        }, "opening an XML document should throw");
+      }));
+      iframe.src = "about:blank";
+    }), { once: true });
+    iframe.src = "/common/dummy.xhtml";
+  }, `document.open should throw an InvalidStateError with XML document even when the ignore-opens-during-unload counter is greater than 0 (during ${ev} event)`);
+}
---
+++ third_party/WebKit/LayoutTests/external/wpt/html/webappapis/dynamic-markup-insertion/opening-the-input-stream/history-state.window.js
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+async_test(t => {
+  const iframe = document.body.appendChild(document.createElement("iframe"));
+  t.add_cleanup(() => iframe.remove());
+  iframe.src = "/common/blank.html";
+  iframe.onload = t.step_func_done(() => {
+    const win = iframe.contentWindow;
+    const doc = iframe.contentDocument;
+    assert_equals(win.history.state, null);
+    win.history.replaceState("state", "");
+    assert_equals(win.history.state, "state");
+    assert_equals(doc.open(), doc);
+    assert_equals(win.history.state, "state");
+  });
+}, "history.state is kept by document.open()");
+
+async_test(t => {
+  const iframe = document.body.appendChild(document.createElement("iframe"));
+  t.add_cleanup(() => iframe.remove());
+  iframe.src = "/common/blank.html";
+  iframe.onload = t.step_func_done(() => {
+    const win = iframe.contentWindow;
+    const doc = iframe.contentDocument;
+    assert_equals(win.history.state, null);
+    win.history.replaceState("state", "");
+    assert_equals(win.history.state, "state");
+    assert_equals(doc.open("", "replace"), doc);
+    assert_equals(win.history.state, "state");
+  });
+}, "history.state is kept by document.open() (with historical replace parameter set)");
