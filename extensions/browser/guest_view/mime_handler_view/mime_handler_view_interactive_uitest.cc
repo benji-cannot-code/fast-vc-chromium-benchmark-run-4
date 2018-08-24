1d4463b3b89a56a42646263ab47ed80b299ae7fe
Scott Little <sclittle@chromium.org>
2018-08-24 21:51:53 +0000

    Disable flaky MimeHandlerView test on Linux.
    The MimeHandlerViewTests/MimeHandlerViewTest.Fullscreen tests are flaky
    on Linux MSan:
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Linux%20MSan%20Tests
    
    Example failure:
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Linux%20MSan%20Tests/11308
    
    TBR=lazyboy@chromium.org
    
    Bug: 877627
    Change-Id: Ie7f05125c5abf60454327ea5c5d61326f9137f93
    Reviewed-on: https://chromium-review.googlesource.com/1188874
    Reviewed-by: Scott Little <sclittle@chromium.org>
    Commit-Queue: Scott Little <sclittle@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586006}

--- extensions/browser/guest_view/mime_handler_view/mime_handler_view_interactive_uitest.cc
+++ extensions/browser/guest_view/mime_handler_view/mime_handler_view_interactive_uitest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "base/run_loop.h"
 #include "base/test/scoped_feature_list.h"
+#include "build/build_config.h"
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/guest_view/browser/test_guest_view_manager.h"
@@ -133,7 +134,13 @@ INSTANTIATE_TEST_CASE_P(MimeHandlerViewTests,
                         MimeHandlerViewTest,
                         testing::Bool());
 
-IN_PROC_BROWSER_TEST_P(MimeHandlerViewTest, Fullscreen) {
+// Test is flaky on Linux.  https://crbug.com/877627
+#if defined(OS_LINUX)
+#define MAYBE_Fullscreen DISABLED_Fullscreen
+#else
+#define MAYBE_Fullscreen Fullscreen
+#endif
+IN_PROC_BROWSER_TEST_P(MimeHandlerViewTest, MAYBE_Fullscreen) {
   RunTest("testFullscreen.csv");
 }
 
