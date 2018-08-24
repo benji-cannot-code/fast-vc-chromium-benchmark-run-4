2134016a1f105f10db829d5024b89858a331b891
Lei Zhang <thestig@chromium.org>
2018-08-24 05:49:49 +0000

    Use static_cast instead of reinterpret_cast for memory() calls.
    Fix some lint errors as well.
    
    TBR=emircan@chromium.org,kbr@chromium.org,reillyg@chromium.org,sandersd@chromium.org,thakis@chromium.org
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I6753c2e20302ce2296950d9a033bf0d0ac3cf509
    Reviewed-on: https://chromium-review.googlesource.com/1183847
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585708}

--- extensions/renderer/user_script_set.cc
+++ extensions/renderer/user_script_set.cc
@@ -109,7 +109,7 @@ bool UserScriptSet::UpdateUserScripts(base::SharedMemoryHandle shared_memory,
   if (!shared_memory_->Map(sizeof(base::Pickle::Header)))
     return false;
   base::Pickle::Header* pickle_header =
-      reinterpret_cast<base::Pickle::Header*>(shared_memory_->memory());
+      static_cast<base::Pickle::Header*>(shared_memory_->memory());
 
   // Now map in the rest of the block.
   int pickle_size = sizeof(base::Pickle::Header) + pickle_header->payload_size;
@@ -119,7 +119,7 @@ bool UserScriptSet::UpdateUserScripts(base::SharedMemoryHandle shared_memory,
 
   // Unpickle scripts.
   uint32_t num_scripts = 0;
-  base::Pickle pickle(reinterpret_cast<char*>(shared_memory_->memory()),
+  base::Pickle pickle(static_cast<char*>(shared_memory_->memory()),
                       pickle_size);
   base::PickleIterator iter(pickle);
   base::debug::Alias(&pickle_size);
