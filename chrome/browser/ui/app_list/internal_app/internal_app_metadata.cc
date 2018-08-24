6e84d5f4c224c3325606cb61fcb72f7a4afd761a
Alexander Alekseev <alemate@chromium.org>
2018-08-24 22:59:29 +0000

    Chrome OS: Add Discover window UI.
    This CL adds Chrome OS Discover UI in separate window.
    
    Window need further changes to match the specs, but this
    change mostly adds infrastructure.
    
    Bug: 864686
    
    Change-Id: I2834d8f9efb3660050a31768508992acb1f3f262
    Reviewed-on: https://chromium-review.googlesource.com/1182880
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586034}

--- chrome/browser/ui/app_list/internal_app/internal_app_metadata.cc
+++ chrome/browser/ui/app_list/internal_app/internal_app_metadata.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/app_list/internal_app_id_constants.h"
 #include "ash/public/cpp/resources/grit/ash_public_unscaled_resources.h"
 #include "base/logging.h"
+#include "base/metrics/user_metrics.h"
 #include "base/no_destructor.h"
 #include "base/strings/string16.h"
 #include "base/strings/string_util.h"
@@ -42,6 +43,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/image/image_skia_operations.h"
 #include "url/gurl.h"
 
+#if defined(OS_CHROMEOS)
+#include "chrome/browser/ui/webui/chromeos/login/discover/discover_window_manager.h"
+#endif
+
 namespace app_list {
 
 namespace {
@@ -74,7 +79,14 @@ const std::vector<InternalApp>& GetInternalAppList(bool is_guest_mode) {
             /*recommendable=*/true,
             /*searchable=*/false,
             /*show_in_launcher=*/false, InternalAppName::kContinueReading,
-            /*searchable_string_resource_id=*/0}});
+            /*searchable_string_resource_id=*/0},
+
+           {kInternalAppIdDiscover, IDS_INTERNAL_APP_DISCOVER,
+            IDR_DISCOVER_APP_192,
+            /*recommendable=*/false,
+            /*searchable=*/true,
+            /*show_in_launcher=*/true, InternalAppName::kDiscover,
+            /*searchable_string_resource_id=*/IDS_INTERNAL_APP_DISCOVER}});
 
   static base::NoDestructor<std::vector<InternalApp>> internal_app_list;
   internal_app_list->clear();
@@ -165,6 +177,12 @@ void OpenInternalApp(const std::string& app_id,
     chrome::ShowSettingsSubPageForProfile(profile, std::string());
   } else if (app_id == kInternalAppIdCamera) {
     ShowCameraApp(app_id, profile, event_flags);
+  } else if (app_id == kInternalAppIdDiscover) {
+#if defined(OS_CHROMEOS)
+    base::RecordAction(base::UserMetricsAction("ShowDiscover"));
+    chromeos::DiscoverWindowManager::GetInstance()
+        ->ShowChromeDiscoverPageForProfile(profile);
+#endif
   }
 }
 
