621fb1a6b90910b6a31f51ed241d2bfb241facd8
Christian Biesinger <cbiesinger@chromium.org>
2018-08-24 21:24:02 +0000

    [layoutng] Improve NGExclusionSpace::operator==
    If neither space has exclusions, the two spaces are equal.
    
    exclusions_ is a scoped_refptr, so operator== only compares pointer values.
    
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng
    Change-Id: Iea67f268f9a537a27f267441f2afda860d641950
    Reviewed-on: https://chromium-review.googlesource.com/1188847
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585994}

--- third_party/blink/renderer/core/layout/ng/exclusions/ng_exclusion_space.cc
+++ third_party/blink/renderer/core/layout/ng/exclusions/ng_exclusion_space.cc
@@ -586,6 +586,8 @@ const NGExclusionSpace::DerivedGeometry& NGExclusionSpace::GetDerivedGeometry()
 }
 
 bool NGExclusionSpace::operator==(const NGExclusionSpace& other) const {
+  if (num_exclusions_ == 0 && other.num_exclusions_ == 0)
+    return true;
   return num_exclusions_ == other.num_exclusions_ &&
          exclusions_ == other.exclusions_;
 }
