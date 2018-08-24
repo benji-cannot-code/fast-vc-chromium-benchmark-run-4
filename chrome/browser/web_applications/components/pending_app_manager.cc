780172256fab88476f7aca94d8bb33dd9a14ec26
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2018-08-24 10:45:29 +0000

    desktop-pwas: Add option to skip shortcut creation in PendingAppManager
    WebAppPolicyManager would like to skip shortcut creation because there
    is a separate policy to create them. So we add that option to AppInfo
    and plumb it all the way down to BookmarkAppHelper
    
    Bug: 876175
    Change-Id: I09f460efe7c3809856bde77ebe4570de6ae8845e
    Reviewed-on: https://chromium-review.googlesource.com/1186220
    Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585788}

--- chrome/browser/web_applications/components/pending_app_manager.cc
+++ chrome/browser/web_applications/components/pending_app_manager.cc
@@ -10,8 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace web_app {
 
-PendingAppManager::AppInfo::AppInfo(GURL url, LaunchContainer launch_container)
-    : url(std::move(url)), launch_container(launch_container) {}
+PendingAppManager::AppInfo::AppInfo(GURL url,
+                                    LaunchContainer launch_container,
+                                    bool create_shortcuts)
+    : url(std::move(url)),
+      launch_container(launch_container),
+      create_shortcuts(create_shortcuts) {}
 
 PendingAppManager::AppInfo::AppInfo(PendingAppManager::AppInfo&& other) =
     default;
@@ -20,15 +24,16 @@ PendingAppManager::AppInfo::~AppInfo() = default;
 
 std::unique_ptr<PendingAppManager::AppInfo> PendingAppManager::AppInfo::Clone()
     const {
-  auto other = std::make_unique<AppInfo>(url, launch_container);
+  auto other =
+      std::make_unique<AppInfo>(url, launch_container, create_shortcuts);
   DCHECK_EQ(*this, *other);
   return other;
 }
 
 bool PendingAppManager::AppInfo::operator==(
     const PendingAppManager::AppInfo& other) const {
-  return std::tie(url, launch_container) ==
-         std::tie(other.url, other.launch_container);
+  return std::tie(url, launch_container, create_shortcuts) ==
+         std::tie(other.url, other.launch_container, other.create_shortcuts);
 }
 
 PendingAppManager::PendingAppManager() = default;
@@ -38,7 +43,8 @@ PendingAppManager::~PendingAppManager() = default;
 std::ostream& operator<<(std::ostream& out,
                          const PendingAppManager::AppInfo& app_info) {
   return out << "url: " << app_info.url << "\n launch_container: "
-             << static_cast<int32_t>(app_info.launch_container);
+             << static_cast<int32_t>(app_info.launch_container)
+             << "\n create_shortcuts: " << app_info.create_shortcuts;
 }
 
 }  // namespace web_app
--- chrome/browser/web_applications/components/pending_app_manager.h
+++ chrome/browser/web_applications/components/pending_app_manager.h
@@ -37,7 +37,9 @@ class PendingAppManager {
   };
 
   struct AppInfo {
-    AppInfo(GURL url, LaunchContainer launch_container);
+    AppInfo(GURL url,
+            LaunchContainer launch_container,
+            bool create_shortcuts = true);
     AppInfo(AppInfo&& other);
     ~AppInfo();
 
@@ -45,8 +47,9 @@ class PendingAppManager {
 
     bool operator==(const AppInfo& other) const;
 
-    GURL url;
-    LaunchContainer launch_container;
+    const GURL url;
+    const LaunchContainer launch_container;
+    const bool create_shortcuts;
 
     DISALLOW_COPY_AND_ASSIGN(AppInfo);
   };
