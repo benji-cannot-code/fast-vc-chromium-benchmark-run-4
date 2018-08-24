30c27dab01d499a54f361f7a275deba659cab370
Yuwei Huang <yuweih@chromium.org>
2018-08-24 23:15:34 +0000

    [CRD iOS] Migrate MDCAppBar to MDCAppBarViewController
    MDCAppBar will soon be deprecated. This CL migrates all MDCAppBar code
    to use MDCAppBarViewController.
    
    It is basically following the migration guide here:
    https://github.com/material-components/material-components-ios/tree/develop/components/AppBar#migration-guide-mdcappbar-to-mdcappbarviewcontroller
    
    Bug: 874534
    Change-Id: Ibe3ea272763b98bd7ded513111167221f06c9e6b
    Reviewed-on: https://chromium-review.googlesource.com/1187402
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586042}

--- remoting/ios/app/remoting_view_controller.mm
+++ remoting/ios/app/remoting_view_controller.mm
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/bind.h"
 #import "ios/third_party/material_components_ios/src/components/AnimationTiming/src/MaterialAnimationTiming.h"
-#import "ios/third_party/material_components_ios/src/components/AppBar/src/MaterialAppBar.h"
+#import "ios/third_party/material_components_ios/src/components/AppBar/src/MDCAppBarViewController.h"
 #import "ios/third_party/material_components_ios/src/components/Dialogs/src/MaterialDialogs.h"
 #import "ios/third_party/material_components_ios/src/components/ShadowElevations/src/MaterialShadowElevations.h"
 #import "ios/third_party/material_components_ios/src/components/ShadowLayer/src/MaterialShadowLayer.h"
@@ -89,7 +89,7 @@ using remoting::HostListService;
                                      UIViewControllerAnimatedTransitioning,
                                      UIViewControllerTransitioningDelegate> {
   MDCDialogTransitionController* _dialogTransitionController;
-  MDCAppBar* _appBar;
+  MDCAppBarViewController* _appBarViewController;
   HostCollectionViewController* _collectionViewController;
   HostFetchingViewController* _fetchingViewController;
   HostFetchingErrorViewController* _fetchingErrorViewController;
@@ -136,8 +136,8 @@ using remoting::HostListService;
     _setupViewController = [[HostSetupViewController alloc] init];
     _setupViewController.scrollViewDelegate = self.headerViewController;
 
-    _appBar = [[MDCAppBar alloc] init];
-    [self addChildViewController:_appBar.headerViewController];
+    _appBarViewController = [[MDCAppBarViewController alloc] init];
+    [self addChildViewController:_appBarViewController];
 
     self.navigationItem.title =
         l10n_util::GetNSString(IDS_PRODUCT_NAME).lowercaseString;
@@ -150,13 +150,13 @@ using remoting::HostListService;
     remoting::SetAccessibilityInfoFromImage(menuButton);
     self.navigationItem.leftBarButtonItem = menuButton;
 
-    _appBar.headerViewController.headerView.backgroundColor =
+    _appBarViewController.headerView.backgroundColor =
         RemotingTheme.hostListBackgroundColor;
-    _appBar.navigationBar.backgroundColor =
+    _appBarViewController.navigationBar.backgroundColor =
         RemotingTheme.hostListBackgroundColor;
     MDCNavigationBarTextColorAccessibilityMutator* mutator =
         [[MDCNavigationBarTextColorAccessibilityMutator alloc] init];
-    [mutator mutate:_appBar.navigationBar];
+    [mutator mutate:_appBarViewController.navigationBar];
 
     MDCFlexibleHeaderView* headerView = self.headerViewController.headerView;
     headerView.backgroundColor = [UIColor clearColor];
@@ -203,7 +203,8 @@ using remoting::HostListService;
         constraintGreaterThanOrEqualToAnchor:[self.view heightAnchor]],
   ]];
 
-  [_appBar addSubviewsToParent];
+  [self.view addSubview:_appBarViewController.view];
+  [_appBarViewController didMoveToParentViewController:self];
 
   __weak __typeof(self) weakSelf = self;
   _hostListStateSubscription =
