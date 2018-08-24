a947f4c8cd0a6d9fb21799727faee0bc77f04938
Guido Urdaneta <guidou@chromium.org>
2018-08-24 14:04:47 +0000

    Remove dead code related to MediaStreamSource constraints.
    MediaStreamSources do not have constraints, so remove old accessors
    that provided that functionality.
    
    Bug: 877430
    Change-Id: Iee3973f5ddcb8d85608a798565784a59e474231a
    Reviewed-on: https://chromium-review.googlesource.com/1188312
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585829}

--- third_party/blink/public/platform/web_media_stream_source.h
+++ third_party/blink/public/platform/web_media_stream_source.h
@@ -45,7 +45,6 @@ namespace blink {
 
 class MediaStreamSource;
 class WebAudioDestinationConsumer;
-class WebMediaConstraints;
 class WebString;
 
 class WebMediaStreamSource {
@@ -138,8 +137,6 @@ class WebMediaStreamSource {
       EchoCancellationMode echo_cancellation_mode,
       bool auto_gain_control,
       bool noise_supression);
-
-  BLINK_PLATFORM_EXPORT WebMediaConstraints Constraints();
 
   BLINK_PLATFORM_EXPORT void SetCapabilities(const Capabilities&);
 
