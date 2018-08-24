b5b6cec0ff9c16e187a39478c46499aacf237391
Yuwei Huang <yuweih@chromium.org>
2018-08-24 00:04:29 +0000

    [CRD iOS] Improve viewport manipulation on iPhone X
    This CL allows the iOS client to display desktop that is partially
    obstructed by the notch while allowing user to pan the desktop out of
    the obstructed area, similar to what we have done on the Android
    client.
    
    This CL also changes the previous logic that handles keyboard height.
    We are now using DesktopViewport's SetSafeInsets() methods to adjust
    for the keyboard rather than just changing the surface size.
    
    Bug: 876014
    Change-Id: I428cf2552acb3bc33cf932890a9395428d969a40
    Reviewed-on: https://chromium-review.googlesource.com/1186027
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585666}

--- remoting/client/gesture_interpreter.h
+++ remoting/client/gesture_interpreter.h
@@ -83,6 +83,7 @@ class GestureInterpreter {
 
   void OnSurfaceSizeChanged(int width, int height);
   void OnDesktopSizeChanged(int width, int height);
+  void OnSafeInsetsChanged(int left, int top, int right, int bottom);
 
   base::WeakPtr<GestureInterpreter> GetWeakPtr();
 
