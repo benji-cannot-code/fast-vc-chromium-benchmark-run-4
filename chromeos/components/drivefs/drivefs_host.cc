2502cb86f3552bc22f75e69e3741598a8a27d73c
Sergei Datsenko <dats@chromium.org>
2018-08-24 06:07:38 +0000

    Present drivefs logs on drive-internals page.
    BUG=chromium:856874
    
    Change-Id: I9e7e9645d120b27868618053cffd5953908b99b1
    Reviewed-on: https://chromium-review.googlesource.com/1184801
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Commit-Queue: Sergei Datsenko <dats@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585718}

--- chromeos/components/drivefs/drivefs_host.cc
+++ chromeos/components/drivefs/drivefs_host.cc
@@ -80,10 +80,8 @@ class DriveFsHost::MountState : public mojom::DriveFsDelegate,
         pending_token_(base::UnguessableToken::Create()),
         binding_(this) {
     source_path_ = base::StrCat({kMountScheme, pending_token_.ToString()});
-    std::string datadir_option = base::StrCat(
-        {"datadir=", host_->profile_path_.Append(kDataPath)
-                         .Append(host_->delegate_->GetObfuscatedAccountId())
-                         .value()});
+    std::string datadir_option =
+        base::StrCat({"datadir=", host_->GetDataPath().value()});
     auto bootstrap =
         mojo::MakeProxy(mojo_connection_delegate_->InitializeMojoConnection());
     mojom::DriveFsDelegatePtr delegate;
@@ -364,6 +362,11 @@ bool DriveFsHost::IsMounted() const {
 const base::FilePath& DriveFsHost::GetMountPath() const {
   DCHECK(mount_state_);
   return mount_state_->mount_path();
+}
+
+base::FilePath DriveFsHost::GetDataPath() const {
+  return profile_path_.Append(kDataPath).Append(
+      delegate_->GetObfuscatedAccountId());
 }
 
 mojom::DriveFs* DriveFsHost::GetDriveFsInterface() const {
