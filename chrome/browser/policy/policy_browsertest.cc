71dc078104a0fe5d0ff19fe38e13d49435999cf5
Roger Tawa <rogerta@chromium.org>
2018-08-24 16:23:25 +0000

    Replace deprecated SetDomainStateForTesting with scoping class.
    Bug: None
    Change-Id: I9370b99599f128d7379761c77ba9195be59baf71
    Reviewed-on: https://chromium-review.googlesource.com/1187270
    Commit-Queue: Roger Tawa <rogerta@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585863}

--- chrome/browser/policy/policy_browsertest.cc
+++ chrome/browser/policy/policy_browsertest.cc
@@ -2583,7 +2583,7 @@ IN_PROC_BROWSER_TEST_F(PolicyTest, ExtensionRecommendedInstallationMode) {
 
 // Mark as enterprise managed.
 #if defined(OS_WIN)
-  base::win::SetDomainStateForTesting(true);
+  base::win::ScopedDomainStateForTesting scoped_domain(true);
 #endif
 
   extensions::ExtensionService* service = extension_service();
@@ -2876,7 +2876,7 @@ IN_PROC_BROWSER_TEST_F(PolicyTest, ExtensionMinimumVersionForceInstalled) {
 
 // Mark as enterprise managed.
 #if defined(OS_WIN)
-  base::win::SetDomainStateForTesting(true);
+  base::win::ScopedDomainStateForTesting scoped_domain(true);
 #endif
   extensions::ExtensionRegistry* registry =
       extensions::ExtensionRegistry::Get(browser()->profile());
--- components/policy/core/common/policy_loader_win_unittest.cc
+++ components/policy/core/common/policy_loader_win_unittest.cc
@@ -287,7 +287,7 @@ void RegistryTestHarness::SetUp() {
 ConfigurationPolicyProvider* RegistryTestHarness::CreateProvider(
     SchemaRegistry* registry,
     scoped_refptr<base::SequencedTaskRunner> task_runner) {
-  base::win::SetDomainStateForTesting(true);
+  base::win::ScopedDomainStateForTesting scoped_domain(true);
   std::unique_ptr<AsyncPolicyLoader> loader(
       new PolicyLoaderWin(task_runner, kTestPolicyKey));
   return new AsyncPolicyProvider(registry, std::move(loader));
@@ -407,11 +407,10 @@ class PolicyLoaderWinTest : public PolicyTestBase {
   // files in chrome/test/data/policy/gpo.
   static const base::char16 kTestPolicyKey[];
 
-  PolicyLoaderWinTest() {}
+  PolicyLoaderWinTest() : scoped_domain_(false) {}
   ~PolicyLoaderWinTest() override {}
 
   void SetUp() override {
-    base::win::SetDomainStateForTesting(false);
     PolicyTestBase::SetUp();
 
     // Activate overrides of registry keys. gtest documentation guarantees
@@ -429,6 +428,7 @@ class PolicyLoaderWinTest : public PolicyTestBase {
   }
 
   ScopedGroupPolicyRegistrySandbox registry_sandbox_;
+  base::win::ScopedDomainStateForTesting scoped_domain_;
 };
 
 const base::char16 PolicyLoaderWinTest::kTestPolicyKey[] =
