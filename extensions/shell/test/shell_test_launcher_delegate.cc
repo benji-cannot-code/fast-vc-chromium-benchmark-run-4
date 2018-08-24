c05408eaa306e51d53a3d6bded1cc21f3e3ca5c1
Hayato Ito <hayato@chromium.org>
2018-08-24 04:49:46 +0000

    Revert "Check that TaskScheduler is not leaked between tests, or test-cases."
    This reverts commit a2a4e75d2752687d91e1272450bdcc6e48b33739.
    
    Reason for revert: [sheriff] a culprit for cronet_tests failing on multiple builders
    See https://crbug.com/877355 for details.
    
    Original change's description:
    > Check that TaskScheduler is not leaked between tests, or test-cases.
    > 
    > - Move some APIs off of base::TestSuite, that we unnecessarily public.
    > - Rename the various internal TestEventListeners to express purpose.
    > - Add CheckForLeakedGlobals check, and DisableCheckForLeakedGlobals API.
    >   The latter is called by the content::ContentTestSuiteBase, since the
    >   various browser- and ui-tests running under that suite base run more
    >   like the browser itself, and so are expected to let global singletons
    >   leak.
    > 
    > Bug: 875486
    > Change-Id: If749dfe8530abc4a5c13a840bbf8258d48c377c5
    > Reviewed-on: https://chromium-review.googlesource.com/1182612
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Reviewed-by: Gabriel Charette <gab@chromium.org>
    > Commit-Queue: Wez <wez@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#585652}
    
    TBR=sky@chromium.org,wez@chromium.org,gab@chromium.org
    
    Change-Id: I906458f276100d9f924a0a78c7cca3a55bcb28b6
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 875486
    Reviewed-on: https://chromium-review.googlesource.com/1187982
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Commit-Queue: Hayato Ito <hayato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585680}

--- extensions/shell/test/shell_test_launcher_delegate.cc
+++ extensions/shell/test/shell_test_launcher_delegate.cc
@@ -13,10 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace extensions {
 
 int AppShellTestLauncherDelegate::RunTestSuite(int argc, char** argv) {
-  base::TestSuite test_suite(argc, argv);
-  // Browser tests are expected not to tear-down various globals.
-  test_suite.DisableCheckForLeakedGlobals();
-  return test_suite.Run();
+  return base::TestSuite(argc, argv).Run();
 }
 
 bool AppShellTestLauncherDelegate::AdjustChildProcessCommandLine(
