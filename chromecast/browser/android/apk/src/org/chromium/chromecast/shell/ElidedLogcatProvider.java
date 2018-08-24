362f9fc26c1037d741b1df48a6195f9edd09ab28
Alex Leung <alexleung@google.com>
2018-08-24 23:06:18 +0000

    [Chromecast] Move elideLogcat to background thread.
    BUG=b:113066827
    
    Change-Id: Ic0e1d434eadbddf38f0fc6b2a5a6b1f364e947af
    Reviewed-on: https://chromium-review.googlesource.com/1188588
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Commit-Queue: Alex Leung <alexleung@google.com>
    Cr-Commit-Position: refs/heads/master@{#586036}

--- chromecast/browser/android/apk/src/org/chromium/chromecast/shell/ElidedLogcatProvider.java
+++ chromecast/browser/android/apk/src/org/chromium/chromecast/shell/ElidedLogcatProvider.java
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chromecast.shell;
 
+import android.os.SystemClock;
+
+import org.chromium.base.AsyncTask;
 import org.chromium.base.Log;
 import org.chromium.base.VisibleForTesting;
 
@@ -18,26 +21,37 @@ import java.io.IOException;
  * Javascript console messages.
  */
 abstract class ElidedLogcatProvider {
-    private static final String TAG = "cr_ElidedLogcatProvider";
+    private static final String TAG = "cr_ElidedLogcatProv";
 
     protected abstract void getRawLogcat(RawLogcatCallback rawLogcatCallback);
 
-    protected interface RawLogcatCallback { public void onLogsDone(BufferedReader logsFileReader); }
-    public interface LogcatCallback { public void onLogsDone(String logs); }
+    protected interface RawLogcatCallback {
+        public void onLogsDone(BufferedReader logsFileReader);
+    }
+
+    public interface LogcatCallback {
+        public void onLogsDone(String logs);
+    }
 
     public void getElidedLogcat(LogcatCallback callback) {
-        getRawLogcat((BufferedReader logsFileReader)
-                             -> callback.onLogsDone(elideLogcat(logsFileReader)));
+        getRawLogcat((BufferedReader logsFileReader) -> {
+            // Run elideLogcat in background thread because it can be very slow
+            new AsyncTaskRunner(AsyncTask.THREAD_POOL_EXECUTOR).doAsync(
+                    () -> elideLogcat(logsFileReader), callback::onLogsDone);
+        });
     }
 
     @VisibleForTesting
     protected static String elideLogcat(BufferedReader logsFileReader) {
+        long startTimeMillis = SystemClock.elapsedRealtime();
         StringBuilder builder = new StringBuilder();
         try (BufferedReader autoClosableBufferedReader = logsFileReader) {
             String logLn;
             while ((logLn = autoClosableBufferedReader.readLine()) != null) {
                 builder.append(LogcatElision.elide(logLn + "\n"));
             }
+            long elapsedMillis = SystemClock.elapsedRealtime() - startTimeMillis;
+            Log.i(TAG, "elideLogcat took " + elapsedMillis + " ms");
         } catch (IOException e) {
             Log.e(TAG, "Can't read logs", e);
         } finally {
