0414059f62550bd8438ff953321c31b86dcbba2e
Khushal <khushalsagar@chromium.org>
2018-08-25 01:11:07 +0000

    blink: Use external memory allocator in tests using MockImageDecoder.
    The caller expects the decoder to populate memory in the allocator if
    set, but the MockImageDecoder was not doing this. This causes failures
    in MSAN where the memory remains unitinitialized.
    
    Fix MockImageDecoder to respect this expectation.
    
    R=chrishtr@chromium.org
    
    Bug: 877497
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I441095eb3fb89c90cf3b9bf592438cd20b126391
    Reviewed-on: https://chromium-review.googlesource.com/1188877
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586090}

--- third_party/blink/renderer/platform/graphics/test/mock_image_decoder.h
+++ third_party/blink/renderer/platform/graphics/test/mock_image_decoder.h
@@ -108,11 +108,17 @@ class MockImageDecoder : public ImageDecoder {
     return ImageDecoder::FrameBytesAtIndex(index);
   }
 
-  void SetMemoryAllocator(SkBitmap::Allocator*) override {
+  void SetMemoryAllocator(SkBitmap::Allocator* allocator) override {
+    if (frame_buffer_cache_.IsEmpty()) {
+      // Ensure that InitializeNewFrame is called, after parsing if
+      // necessary.
+      if (!FrameCount())
+        return;
+    }
+
+    frame_buffer_cache_[0].SetMemoryAllocator(allocator);
     client_->MemoryAllocatorSet();
   }
-
-  bool IsForTesting() const override { return true; }
 
  private:
   void DecodeSize() override {}
@@ -121,6 +127,8 @@ class MockImageDecoder : public ImageDecoder {
 
   void Decode(size_t index) override {
     client_->DecodeRequested();
+    frame_buffer_cache_[index].ClearPixelData();
+    InitializeNewFrame(index);
     frame_buffer_cache_[index].SetStatus(client_->GetStatus(index));
   }
 
