1e8882c75d1f23e9808a4d561cd19ed03685b13f
Eugene But <eugenebut@chromium.org>
2018-08-25 02:25:57 +0000

    Add UKM logging for Slim Navigation Manager URL mismatch.
    This metric will help to diagnose possible URL spoofing bugs in Slim
    Navigation feature. _documentURL is used as proxy for legacy navigation
    manager URL, which is generally reliable and almost always match
    last committed URL.
    
    Collection review doc:
    https://docs.google.com/document/d/14SygQ8BzLkzedTLuDfOHhakQHZOOJSU8TAJT3rxBGv4/edit#
    
    Bug: 1182320
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I6dcf5711f0fd42063b67afb0fa0a99b77ce4c7a0
    Reviewed-on: https://chromium-review.googlesource.com/1187014
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586110}

--- ios/web/web_state/ui/crw_web_controller.mm
+++ ios/web/web_state/ui/crw_web_controller.mm
@@ -111,6 +111,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/net_errors.h"
 #include "net/cert/x509_util_ios.h"
 #include "net/ssl/ssl_info.h"
+#include "services/metrics/public/cpp/ukm_builders.h"
 #include "ui/base/page_transition_types.h"
 #include "url/gurl.h"
 #include "url/url_constants.h"
@@ -1273,13 +1274,20 @@ GURL URLEscapedForHistory(const GURL& url) {
     _documentURL = newURL;
     _interactionRegisteredSinceLastURLChange = NO;
   }
-  if (web::GetWebClient()->IsSlimNavigationManagerEnabled() &&
-      (!context || !context->IsLoadingHtmlString()) &&
-      !IsWKInternalUrl(newURL) && _webView) {
+  if (web::GetWebClient()->IsSlimNavigationManagerEnabled() && context &&
+      !context->IsLoadingHtmlString() && !IsWKInternalUrl(newURL) && _webView) {
     GURL documentOrigin = newURL.GetOrigin();
     GURL committedOrigin = _webStateImpl->GetLastCommittedURL().GetOrigin();
     DCHECK_EQ(documentOrigin, committedOrigin)
         << "Old and new URL detection system have a mismatch";
+
+    ukm::SourceId sourceID = ukm::ConvertToSourceId(
+        context->GetNavigationId(), ukm::SourceIdType::NAVIGATION_ID);
+    if (sourceID != ukm::kInvalidSourceId) {
+      ukm::builders::IOS_URLMismatchInLegacyAndSlimNavigationManager(sourceID)
+          .SetHasMismatch(documentOrigin != committedOrigin)
+          .Record(ukm::UkmRecorder::Get());
+    }
   }
 }
 
