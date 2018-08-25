1a9d99ed8b7e70aa3c8e4fe9759487617d556ec0
Gabriel Charette <gab@chromium.org>
2018-08-25 04:20:08 +0000

    Reland "[RenderProcessHost/Desktop] Update process priority on process launch and fix navigation priority inversion"
    This is a reland of c492203d118a287fa628c074e59acc7989996832
    Reverted @ https://crrev.com/c/1188963 per the new test being flaky.
    I'm pretty sure it's just because 10us for the recurring check was
    too aggressive for that kernel and starved the test (which also
    explains why ASAN was flaking -- looking back it was a termination
    failure (timeout) not an ASAN failure -- so I've re-enabled both.
    
    Original change's description:
    > [RenderProcessHost/Desktop] Update process priority on process launch and fix navigation priority inversion
    >
    > This is a second take on
    > https://chromium-review.googlesource.com/c/chromium/src/+/754299
    >
    > It's under an experiment to (1) be able to see the side-effects and (2)
    > make merge to M69 easier. Intentionally kept old (and buggy) behavior
    > intact under the non-experimental branch for comparison.
    >
    > This fixes all aspects of crbug.com/560446:
    >  1) Fixes the OP better than r385608 did.
    >  2) Fixes the side-effect of r385608 which was to default all tabs
    >     created backgrounded to run at foreground priority until visiblity
    >     was toggle on/off again.
    >  3) Fixes a major priority inversion on foreground navigation which
    >     could cause them to background their associated renderer while
    >     navigation (which reintroduced the OP but worse since all background
    >     tabs were running foregrounded per r385608)...
    >     See https://crbug.com/560446#c74 for details.
    >
    > Updated ChildProcessLauncherPriority::is_background() to consistently
    > reflect backgrounding decision to all callers and renamed
    > |ChildProcessLauncherPriority::foreground| to
    > |ChildProcessLauncherPriority::visible| to better reflect that it's a
    > property but not the resulting foreground/background state.
    >
    > See in code comment on ShouldBoostPriorityForPendingViews() for more
    > details.
    >
    > TBR=haraken@chromium.org (side-effect in third_party/blink)
    >
    > Bug: 560446
    > Change-Id: I901b702506a44704b53c007bcdd498fb60824e94
    > Reviewed-on: https://chromium-review.googlesource.com/1142593
    > Commit-Queue: Gabriel Charette <gab@chromium.org>
    > Reviewed-by: Nasko Oskov <nasko@chromium.org>
    > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > Reviewed-by: Bo <boliu@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#585918}
    
    TBR=nasko@chromium.org (trivial reland)
    
    Bug: 560446
    Change-Id: Ib8daff024edabd488afda0b4b9da6f6e0fda77d4
    Reviewed-on: https://chromium-review.googlesource.com/1189204
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586119}

--- content/public/android/java/src/org/chromium/content/browser/ChildProcessLauncherHelperImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/ChildProcessLauncherHelperImpl.java
@@ -137,9 +137,8 @@ public final class ChildProcessLauncherHelperImpl {
 
                     sLauncherByPid.put(pid, ChildProcessLauncherHelperImpl.this);
                     if (mRanking != null) {
-                        mRanking.addConnection(connection, false /* foreground */,
-                                1 /* frameDepth */, false /* intersectsViewport */,
-                                ChildProcessImportance.MODERATE);
+                        mRanking.addConnection(connection, false /* visible */, 1 /* frameDepth */,
+                                false /* intersectsViewport */, ChildProcessImportance.MODERATE);
                     }
 
                     // If the connection fails and pid == 0, the Java-side cleanup was already
@@ -173,7 +172,7 @@ public final class ChildProcessLauncherHelperImpl {
     // This is the current computed importance from all the inputs from setPriority.
     // The initial value is MODERATE since a newly created connection has moderate bindings.
     private @ChildProcessImportance int mEffectiveImportance = ChildProcessImportance.MODERATE;
-    private boolean mForeground;
+    private boolean mVisible;
 
     @CalledByNative
     private static FileDescriptorInfo makeFdInfo(
@@ -436,7 +435,7 @@ public final class ChildProcessLauncherHelperImpl {
     }
 
     @CalledByNative
-    private void setPriority(int pid, boolean foreground, boolean hasMediaStream, long frameDepth,
+    private void setPriority(int pid, boolean visible, boolean hasMediaStream, long frameDepth,
             boolean intersectsViewport, boolean boostForPendingViews,
             @ChildProcessImportance int importance) {
         assert LauncherThread.runningOnLauncherThread();
@@ -448,7 +447,7 @@ public final class ChildProcessLauncherHelperImpl {
 
         ChildProcessConnection connection = mLauncher.getConnection();
         if (ChildProcessCreationParamsImpl.getIgnoreVisibilityForImportance()) {
-            foreground = false;
+            visible = false;
             boostForPendingViews = false;
         }
 
@@ -457,10 +456,10 @@ public final class ChildProcessLauncherHelperImpl {
 
         @ChildProcessImportance
         int newEffectiveImportance;
-        if ((foreground && frameDepth == 0) || importance == ChildProcessImportance.IMPORTANT
+        if ((visible && frameDepth == 0) || importance == ChildProcessImportance.IMPORTANT
                 || (hasMediaStream && !mediaRendererHasModerate)) {
             newEffectiveImportance = ChildProcessImportance.IMPORTANT;
-        } else if ((foreground && frameDepth > 0 && intersectsViewport) || boostForPendingViews
+        } else if ((visible && frameDepth > 0 && intersectsViewport) || boostForPendingViews
                 || importance == ChildProcessImportance.MODERATE
                 || (hasMediaStream && mediaRendererHasModerate)) {
             newEffectiveImportance = ChildProcessImportance.MODERATE;
@@ -469,13 +468,13 @@ public final class ChildProcessLauncherHelperImpl {
         }
 
         // Add first and remove second.
-        if (foreground && !mForeground) {
+        if (visible && !mVisible) {
             BindingManager manager = getBindingManager();
             if (mUseBindingManager && manager != null) {
                 manager.increaseRecency(connection);
             }
         }
-        mForeground = foreground;
+        mVisible = visible;
 
         if (mEffectiveImportance != newEffectiveImportance) {
             switch (newEffectiveImportance) {
@@ -498,7 +497,7 @@ public final class ChildProcessLauncherHelperImpl {
 
         if (mRanking != null) {
             mRanking.updateConnection(
-                    connection, foreground, frameDepth, intersectsViewport, importance);
+                    connection, visible, frameDepth, intersectsViewport, importance);
         }
 
         if (mEffectiveImportance != newEffectiveImportance) {
