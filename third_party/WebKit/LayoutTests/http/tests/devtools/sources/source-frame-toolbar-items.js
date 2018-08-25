eedb059871562c12649554c08ae0974909a953fa
Alexey Kozyatinskiy <kozyatinskiy@chromium.org>
2018-08-25 21:39:58 +0000

    DevTools: show origin url for anonymous scripts
    R=lushnikov@chromium.org
    
    Bug: chromium:646849
    Change-Id: I8141bcdcfef7d9734ee9f1c97bfa552160fdcd9d
    Reviewed-on: https://chromium-review.googlesource.com/1182865
    Commit-Queue: Aleksey Kozyatinskiy <kozyatinskiy@chromium.org>
    Reviewed-by: Andrey Lushnikov <lushnikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586149}

---
+++ third_party/WebKit/LayoutTests/http/tests/devtools/sources/source-frame-toolbar-items.js
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(async function() {
+  TestRunner.addResult('Checks tool bar items for scripts');
+  await TestRunner.loadModule('sources_test_runner');
+  await TestRunner.showPanel('sources');
+
+  await SourcesTestRunner.startDebuggerTestPromise();
+  TestRunner.evaluateInPagePromise(`
+    function testFunction() {
+      eval('foo()//# sourceURL=test.js');
+
+      function foo() {
+        eval('debugger;');
+      }
+    }
+    //# sourceURL=foo.js`);
+
+  SourcesTestRunner.runTestFunctionAndWaitUntilPausedPromise();
+  await TestRunner.addSnifferPromise(
+            Sources.ScriptOriginPlugin.prototype, 'rightToolbarItems');
+
+  TestRunner.addResult('Items for foo.js:');
+  dumpToolbarItems(Sources.SourcesPanel.instance().visibleView);
+  TestRunner.addResult('Items for test.js:');
+  dumpToolbarItems(await SourcesTestRunner.showScriptSourcePromise('test.js'));
+
+  SourcesTestRunner.completeDebuggerTest();
+
+  function dumpToolbarItems(sourceFrame) {
+    const items = sourceFrame.syncToolbarItems();
+    for (let item of items)
+      TestRunner.addResult(item.element.deepTextContent());
+  }
+})();
