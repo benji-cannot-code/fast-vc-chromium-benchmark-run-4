c0049e1f261ecf2a6f501815613e28ed90ce3884
Kushagra Sinha <sinhak@chromium.org>
2018-08-25 17:43:56 +0000

    Add a submenu for crOS Account Manager in Settings
    Add a submenu for Chrome OS Account Manager in the People section of
    Settings. This will link to the Chrome OS Account Manager Settings
    subpage.
    
    This will be visible only if the Chrome OS Account Manager feature is
    enabled.
    
    Bug: 833797
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: Id9e6a339ef16db6dbd14a6827df6fdf02c66d913
    Reviewed-on: https://chromium-review.googlesource.com/1186584
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586140}

--- chrome/browser/resources/settings/people_page/people_page.js
+++ chrome/browser/resources/settings/people_page/people_page.js
@@ -130,6 +130,18 @@ Polymer({
       },
       readOnly: true,
     },
+
+    /**
+     * True if Chrome OS Account Manager is enabled.
+     * @private
+     */
+    isAccountManagerEnabled_: {
+      type: Boolean,
+      value: function() {
+        return loadTimeData.getBoolean('isAccountManagerEnabled');
+      },
+      readOnly: true,
+    },
     // </if>
 
     /** @private {!Map<string, string>} */
@@ -175,6 +187,11 @@ Polymer({
           map.set(
               settings.routes.ACCOUNTS.path,
               '#manage-other-people-subpage-trigger .subpage-arrow button');
+        }
+        if (settings.routes.ACCOUNT_MANAGER) {
+          map.set(
+              settings.routes.ACCOUNT_MANAGER.path,
+              '#account-manager-subpage-trigger .subpage-arrow button');
         }
         // </if>
         return map;
@@ -463,6 +480,14 @@ Polymer({
     // it, which takes focus from the dialog.
     e.preventDefault();
     settings.navigateTo(settings.routes.LOCK_SCREEN);
+  },
+
+  /**
+   * @param {!Event} e
+   * @private
+   */
+  onAccountManagerTap_: function(e) {
+    settings.navigateTo(settings.routes.ACCOUNT_MANAGER);
   },
   // </if>
 
