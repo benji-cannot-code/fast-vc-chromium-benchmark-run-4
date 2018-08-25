1178f116dde932e2aa4e9be095605d4c7042bcb8
Xiangjun Zhang <xjz@chromium.org>
2018-08-25 01:48:56 +0000

    Refactoring: Move OffscreenTab out of TabCapture API.
    This CL keeps OffscreenTabOwner in TabCapture API, but moves
    OffscreenTab to chrome/browser/media to allow it being used by the
    mirroring service to support OffscreenTab mirroring. Implementation is
    not changed at all. No functional or behavior change either.
    
    Bug: 734672
    Change-Id: Ia8de3680853b86824809d70dc96c6180e9a0c957
    Reviewed-on: https://chromium-review.googlesource.com/1180588
    Commit-Queue: Xiangjun Zhang <xjz@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586101}

--- chrome/browser/extensions/api/tab_capture/tab_capture_api.cc
+++ chrome/browser/extensions/api/tab_capture/tab_capture_api.cc
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/values.h"
-#include "chrome/browser/extensions/api/tab_capture/offscreen_tab.h"
+#include "chrome/browser/extensions/api/tab_capture/offscreen_tabs_owner.h"
 #include "chrome/browser/extensions/api/tab_capture/tab_capture_registry.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/sessions/session_tab_helper.h"
