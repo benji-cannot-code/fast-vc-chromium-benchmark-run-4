19c802254122c0389cdb34fc11c43e55caf754f4
Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
2018-08-27 07:27:31 +0000

    Add swipe control buttons.
    Shows buttons when swipe control is opened by horizontal scroll of a
    notification box. The buttons triggers same action just like the
    existing control buttons inside MessageView for mouse.
    
    Test: ash_unittests --gtest_filter=MessageCenterViewTest.*:MessageListViewTest.*
    Test: manual
    Bug: 840497
    Change-Id: I6ab456cda8dd1f2de5c885e9c28492bf00ac8cd3
    Reviewed-on: https://chromium-review.googlesource.com/1188044
    Commit-Queue: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586203}

--- ash/message_center/message_list_view.cc
+++ ash/message_center/message_list_view.cc
@@ -139,7 +139,11 @@ void MessageListView::RemoveNotification(MessageView* message_view) {
 
 void MessageListView::UpdateNotification(MessageView* message_view,
                                          const Notification& notification) {
-  views::View* container = message_view->parent();
+  DCHECK_EQ(std::string(SlidableMessageView::kViewClassName),
+            message_view->parent()->GetClassName());
+  SlidableMessageView* container =
+      static_cast<SlidableMessageView*>(message_view->parent());
+
   // Skip updating the notification being cleared.
   if (base::ContainsValue(clearing_all_views_, container))
     return;
@@ -155,7 +159,7 @@ void MessageListView::UpdateNotification(MessageView* message_view,
     deleting_views_.erase(container);
   if (deleted_when_done_.find(container) != deleted_when_done_.end())
     deleted_when_done_.erase(container);
-  message_view->UpdateWithNotification(notification);
+  container->UpdateWithNotification(notification);
   DoUpdateIfPossible();
 }
 
