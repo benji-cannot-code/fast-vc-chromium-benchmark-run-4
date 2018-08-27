5a6fe2e033c63aaf9caf22b8bcb6ab31c59a76eb
Manu Cornet <manucornet@chromium.org>
2018-08-27 17:14:49 +0000

    CrOS shelf: Fix button highlight size
    This makes sure that the size of the highlight (which is also
    the 'small' part of the ripple animation) corresponds to the shelf
    size.
    
    This results in no behavior change in the old UI, and fixes a glitch
    in the new one.
    
    Bug: 877563
    Change-Id: I93c0fa90e22ded53e77b2b099e852851821f62a1
    Reviewed-on: https://chromium-review.googlesource.com/1189186
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Manu Cornet <manucornet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586293}

--- ash/shelf/shelf_button.cc
+++ ash/shelf/shelf_button.cc
@@ -47,8 +47,7 @@ constexpr int kNotificationIndicatorRadiusDip = 7;
 constexpr SkColor kIndicatorBorderColor = SkColorSetA(SK_ColorBLACK, 0x4D);
 constexpr SkColor kIndicatorColor = SK_ColorWHITE;
 
-// Shelf item ripple constants.
-constexpr int kInkDropSmallSize = 48;
+// Shelf item ripple size.
 constexpr int kInkDropLargeSize = 60;
 
 // The time threshold before an item can be dragged.
@@ -686,10 +685,11 @@ void ShelfButton::OnGestureEvent(ui::GestureEvent* event) {
 }
 
 std::unique_ptr<views::InkDropRipple> ShelfButton::CreateInkDropRipple() const {
+  const int ink_drop_small_size = ash::ShelfConstants::shelf_size();
   return std::make_unique<views::SquareInkDropRipple>(
       gfx::Size(kInkDropLargeSize, kInkDropLargeSize),
       ink_drop_large_corner_radius(),
-      gfx::Size(kInkDropSmallSize, kInkDropSmallSize),
+      gfx::Size(ink_drop_small_size, ink_drop_small_size),
       ink_drop_small_corner_radius(), GetLocalBounds().CenterPoint(),
       GetInkDropBaseColor(), ink_drop_visible_opacity());
 }
