b30243b9dee607e106446f97532131b281270d12
Lei Zhang <thestig@chromium.org>
2018-08-27 20:53:48 +0000

    Fix static initializers in ash/assistant/util/deep_link_util.cc.
    Fix nits as well.
    
    BUG=537099
    
    Change-Id: I55ee7d530ca73e13ae63049f07242853650687b2
    Reviewed-on: https://chromium-review.googlesource.com/1188639
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586409}

--- ash/assistant/util/deep_link_util.cc
+++ ash/assistant/util/deep_link_util.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <set>
 
+#include "base/stl_util.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
 #include "net/base/escape.h"
@@ -55,25 +56,6 @@ constexpr char kAssistantSettingsWebUrl[] = R"(data:text/html,
   </html>
 )";
 
-// Map of supported deep link types to their prefixes.
-const std::map<DeepLinkType, std::string> kSupportedDeepLinks = {
-    {DeepLinkType::kFeedback, kAssistantFeedbackPrefix},
-    {DeepLinkType::kOnboarding, kAssistantOnboardingPrefix},
-    {DeepLinkType::kQuery, kAssistantQueryPrefix},
-    {DeepLinkType::kReminders, kAssistantRemindersPrefix},
-    {DeepLinkType::kScreenshot, kAssistantScreenshotPrefix},
-    {DeepLinkType::kSettings, kAssistantSettingsPrefix},
-    {DeepLinkType::kWhatsOnMyScreen, kAssistantWhatsOnMyScreenPrefix}};
-
-// Map of supported deep link params to their keys.
-const std::map<DeepLinkParam, std::string> kDeepLinkParamKeys = {
-    {DeepLinkParam::kQuery, kQueryParamKey},
-    {DeepLinkParam::kRelaunch, kRelaunchParamKey}};
-
-// Set of deep link types which open web contents in the Assistant UI.
-const std::set<DeepLinkType> kWebDeepLinks = {DeepLinkType::kReminders,
-                                              DeepLinkType::kSettings};
-
 }  // namespace
 
 GURL CreateAssistantSettingsDeepLink() {
@@ -105,15 +87,21 @@ std::map<std::string, std::string> GetDeepLinkParams(const GURL& deep_link) {
     params[pair.first] = pair.second;
 
   return params;
-};
+}
 
 base::Optional<std::string> GetDeepLinkParam(
     const std::map<std::string, std::string>& params,
     DeepLinkParam param) {
+  // Map of supported deep link params to their keys.
+  static const std::map<DeepLinkParam, std::string> kDeepLinkParamKeys = {
+      {DeepLinkParam::kQuery, kQueryParamKey},
+      {DeepLinkParam::kRelaunch, kRelaunchParamKey}};
+
   const std::string& key = kDeepLinkParamKeys.at(param);
-  return params.count(key) == 1
+  const auto it = params.find(key);
+  return it != params.end()
              ? base::Optional<std::string>(net::UnescapeURLComponent(
-                   params.at(key), net::UnescapeRule::REPLACE_PLUS_WITH_SPACE))
+                   it->second, net::UnescapeRule::REPLACE_PLUS_WITH_SPACE))
              : base::nullopt;
 }
 
@@ -121,7 +109,6 @@ base::Optional<bool> GetDeepLinkParamAsBool(
     const std::map<std::string, std::string>& params,
     DeepLinkParam param) {
   const base::Optional<std::string>& value = GetDeepLinkParam(params, param);
-
   if (value == "true")
     return true;
 
@@ -132,6 +119,16 @@ base::Optional<bool> GetDeepLinkParamAsBool(
 }
 
 DeepLinkType GetDeepLinkType(const GURL& url) {
+  // Map of supported deep link types to their prefixes.
+  static const std::map<DeepLinkType, std::string> kSupportedDeepLinks = {
+      {DeepLinkType::kFeedback, kAssistantFeedbackPrefix},
+      {DeepLinkType::kOnboarding, kAssistantOnboardingPrefix},
+      {DeepLinkType::kQuery, kAssistantQueryPrefix},
+      {DeepLinkType::kReminders, kAssistantRemindersPrefix},
+      {DeepLinkType::kScreenshot, kAssistantScreenshotPrefix},
+      {DeepLinkType::kSettings, kAssistantSettingsPrefix},
+      {DeepLinkType::kWhatsOnMyScreen, kAssistantWhatsOnMyScreenPrefix}};
+
   for (const auto& supported_deep_link : kSupportedDeepLinks) {
     if (base::StartsWith(url.spec(), supported_deep_link.second,
                          base::CompareCase::SENSITIVE)) {
@@ -172,6 +169,7 @@ base::Optional<GURL> GetWebUrl(DeepLinkType type) {
       return base::nullopt;
   }
 
+  NOTREACHED();
   return base::nullopt;
 }
 
@@ -180,7 +178,11 @@ bool IsWebDeepLink(const GURL& deep_link) {
 }
 
 bool IsWebDeepLinkType(DeepLinkType type) {
-  return kWebDeepLinks.count(type) != 0;
+  // Set of deep link types which open web contents in the Assistant UI.
+  static const std::set<DeepLinkType> kWebDeepLinks = {DeepLinkType::kReminders,
+                                                       DeepLinkType::kSettings};
+
+  return base::ContainsKey(kWebDeepLinks, type);
 }
 
 }  // namespace util
