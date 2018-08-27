51e2b5dc3f7fd18578a9c2e93cd875d201ff88b5
James Hawkins <jhawkins@chromium.org>
2018-08-27 21:55:19 +0000

    chrome/settings/lockScreen: Remove dead code for SmartLock.
    R=tommycli@chomium.org
    
    Bug: none
    Test: none
    Change-Id: I5c46c27a977535802f15c8e89e7945327fb4ef7c
    Reviewed-on: https://chromium-review.googlesource.com/1188895
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: James Hawkins <jhawkins@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586433}

--- chrome/browser/resources/settings/people_page/lock_screen.js
+++ chrome/browser/resources/settings/people_page/lock_screen.js
@@ -418,16 +418,6 @@ Polymer({
     return enabled ? enabledStr : disabledStr;
   },
 
-  /**
-   * @param {boolean} easyUnlockEnabled
-   * @param {boolean} proximityDetectionAllowed
-   * @private
-   */
-  getShowEasyUnlockToggle_: function(
-      easyUnlockEnabled, proximityDetectionAllowed) {
-    return easyUnlockEnabled && proximityDetectionAllowed;
-  },
-
   /** @private */
   updateNumFingerprints_: function() {
     if (this.fingerprintUnlockEnabled_ && this.fingerprintBrowserProxy_) {
