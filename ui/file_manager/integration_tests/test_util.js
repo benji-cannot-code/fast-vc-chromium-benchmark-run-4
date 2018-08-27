a98aa6e913654e0cf2f3e5d02cc498bf05cd3fa0
Sam McNally <sammc@chromium.org>
2018-08-27 07:50:29 +0000

    Add tests for selecting Drive files while offline.
    The file manager integration tests currently have no coverage for
    offline Drive. Add tests to ensure the file chooser dialog allows the
    selection of Drive files that are available offline while offline.
    
    These tests only run with DriveFS enabled due to the lack of controls
    available to set up the test scenarios with the existing Drive sync
    client.
    
    Bug: 876973
    Change-Id: I81e39fba868a073fd82a457004700bb0ebb566ec
    Reviewed-on: https://chromium-review.googlesource.com/1189515
    Commit-Queue: Sam McNally <sammc@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586208}

--- ui/file_manager/integration_tests/test_util.js
+++ ui/file_manager/integration_tests/test_util.js
@@ -336,6 +336,7 @@ function TestEntryInfo(options) {
   this.sizeText = options.sizeText;
   this.typeText = options.typeText;
   this.capabilities = options.capabilities;
+  this.pinned = !!options.pinned;
   Object.freeze(this);
 }
 
@@ -479,6 +480,18 @@ var ENTRIES = {
     nameText: 'newly added file.ogg',
     sizeText: '14 KB',
     typeText: 'OGG audio'
+  }),
+
+  pinned: new TestEntryInfo({
+    type: EntryType.FILE,
+    sourceFileName: 'text.txt',
+    targetPath: 'pinned.txt',
+    mimeType: 'text/plain',
+    lastModifiedTime: 'Sep 4, 1998, 12:34 PM',
+    nameText: 'pinned.txt',
+    sizeText: '51 bytes',
+    typeText: 'Plain text',
+    pinned: true,
   }),
 
   directoryA: new TestEntryInfo({
