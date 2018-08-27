d99012ed21132d38e9d1cda46d40c556d73c489a
Jinho Bang <zino@chromium.org>
2018-08-27 18:23:00 +0000

    PaymentRequest: Only peform editors if proper PaymentOption is true
    If requestShipping is false, we should not perform the shipping address
    editor even if calling retry() with shippingAddressErrors. Similarly,
    if none of requestPayer* options is true, we should not perform the
    contact info editor even if calling retry() with payerErrors.
    
    Test:
      chrome/browser/ui/views/payments/contact_info_editor_view_controller_browsertest.cc
      chrome/browser/ui/views/payments/shipping_address_editor_view_controller_browsertest.cc
      components/payments/content/payment_request_spec_unittest.cc
    
    Bug: 861704
    Change-Id: Id388758617820d9766b0557d0f19670b1044c66e
    Reviewed-on: https://chromium-review.googlesource.com/1189245
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Jinho Bang <jinho.bang@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#586322}

--- components/payments/content/payment_request_spec_unittest.cc
+++ components/payments/content/payment_request_spec_unittest.cc
@@ -454,7 +454,9 @@ TEST_F(PaymentRequestSpecTest, MultipleCurrenciesWithTwoDisplayItem) {
 }
 
 TEST_F(PaymentRequestSpecTest, ShippingAddressErrors) {
-  RecreateSpecWithOptionsAndDetails(mojom::PaymentOptions::New(),
+  mojom::PaymentOptionsPtr options = mojom::PaymentOptions::New();
+  options->request_shipping = true;
+  RecreateSpecWithOptionsAndDetails(std::move(options),
                                     mojom::PaymentDetails::New());
 
   EXPECT_FALSE(spec()->has_shipping_address_error());
@@ -474,7 +476,11 @@ TEST_F(PaymentRequestSpecTest, ShippingAddressErrors) {
 }
 
 TEST_F(PaymentRequestSpecTest, PayerErrors) {
-  RecreateSpecWithOptionsAndDetails(mojom::PaymentOptions::New(),
+  mojom::PaymentOptionsPtr options = mojom::PaymentOptions::New();
+  options->request_payer_email = true;
+  options->request_payer_name = true;
+  options->request_payer_phone = true;
+  RecreateSpecWithOptionsAndDetails(std::move(options),
                                     mojom::PaymentDetails::New());
 
   EXPECT_FALSE(spec()->has_payer_error());
