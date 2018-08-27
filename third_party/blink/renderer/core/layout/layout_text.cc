dc2183db91dab5b8f311df1dbef87d5ed357a7dc
Koji Ishii <kojii@chromium.org>
2018-08-27 14:07:44 +0000

    [LayoutNG] Ensure associated paint fragments are cleared
    r583983 (CL:1178441) checks associated InlineBox are cleared
    when LayoutText and LayoutInline are destructed. This patch
    checks associated paint fragments are cleared too.
    
    https://chromium-review.googlesource.com/c/chromium/src/+/1180828
    
    Bug: 714962
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng;luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I83e66e44be04366fcb072a2218e674c29be497c8
    Reviewed-on: https://chromium-review.googlesource.com/1188407
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586249}

--- third_party/blink/renderer/core/layout/layout_text.cc
+++ third_party/blink/renderer/core/layout/layout_text.cc
@@ -146,7 +146,9 @@ LayoutText::LayoutText(Node* node, scoped_refptr<StringImpl> str)
 
 #if DCHECK_IS_ON()
 LayoutText::~LayoutText() {
-  if (!IsInLayoutNGInlineFormattingContext())
+  if (IsInLayoutNGInlineFormattingContext())
+    DCHECK(!first_paint_fragment_);
+  else
     text_boxes_.AssertIsEmpty();
 }
 #endif
