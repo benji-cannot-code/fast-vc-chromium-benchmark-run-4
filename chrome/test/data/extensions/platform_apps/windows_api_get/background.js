a900f6094c08e33971424c9edeb2224516cc9682
Trent Apted <tapted@chromium.org>
2018-08-27 07:34:43 +0000

    Fix AppWindowApiTest.Get#testGetAllMultipleWindows properly
    Its comparator was rubbish and always has been. Array.prototype.sort()
    takes a function that should return {-1, 0, 1}, not bool.
    
    TBR=benwells@chromium.org
    
    Bug: 877890
    Change-Id: I65162d2b261ad4ff91a028bf62c0cdcfb765df27
    Reviewed-on: https://chromium-review.googlesource.com/1188736
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586206}

--- chrome/test/data/extensions/platform_apps/windows_api_get/background.js
+++ chrome/test/data/extensions/platform_apps/windows_api_get/background.js
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 function compareId(a, b) {
-  return a.id < b.id;
+  return a.id.localeCompare(b.id);
 }
 
 chrome.app.runtime.onLaunched.addListener(function() {
