87c831d22bc242d975a5a462a6e235b866ff5e8b
Chris Harrelson <chrishtr@chromium.org>
2018-08-27 17:42:01 +0000

    [PE] Fix float painting under inline when self-painting status changes
    Previously we did not consider non-BlockFlow elements in the containing block chain
    between the float and the containing BlockFlow.
    
    The code being fixed was introduced here, as part of solving a similar situation:
    https://codereview.chromium.org/1661523002. It seems that the case of under-inline was not
    skipped on purpose.
    
    Bug: 833602
    
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I311ebb3d9f2172ed041bf0e6c8a583d89ae1a74a
    Reviewed-on: https://chromium-review.googlesource.com/1188980
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586301}

--- third_party/blink/renderer/core/paint/paint_layer.cc
+++ third_party/blink/renderer/core/paint/paint_layer.cc
@@ -2943,7 +2943,9 @@ void PaintLayer::UpdateSelfPaintingLayer() {
   SetNeedsRepaint();
   is_self_painting_layer_ = is_self_painting_layer;
   self_painting_status_changed_ = true;
-  SetNeedsRepaint();
+  // Self-painting change can change the compositing container chain;
+  // invalidate the new chain in addition to the old one.
+  MarkCompositingContainerChainForNeedsRepaint();
 
   if (PaintLayer* parent = Parent()) {
     parent->MarkAncestorChainForDescendantDependentFlagsUpdate();
