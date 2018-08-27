410df5308d78ec973711d2e2deb22e69a6d9cd67
Stuart Langley <slangley@google.com>
2018-08-27 03:45:10 +0000

    Introduce a timer to evict cached AboutResource data.
    AboutResource has an internal caching mechanism, that is only refreshed when
    UpdateAboutResource is called. With the introduction of team drives
    UpdateAboutResource is no longer called to get the largest change id, which
    means that the cached version can become stale. Here we introduce a timer that
    will periodically evict that cached version, so that calls to GetAboutResource
    will get fresh data from the server.
    
    Bug: 874551
    Change-Id: Ib0ed943b2abec95c6d32be17228dd429ffedaea8
    Reviewed-on: https://chromium-review.googlesource.com/1189684
    Commit-Queue: Stuart Langley <slangley@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586191}

--- components/drive/chromeos/about_resource_loader.h
+++ components/drive/chromeos/about_resource_loader.h
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "base/threading/thread_checker.h"
+#include "base/time/default_tick_clock.h"
+#include "base/timer/timer.h"
 #include "google_apis/drive/drive_common_callbacks.h"
 
 namespace drive {
@@ -24,7 +26,10 @@ namespace internal {
 // This class is responsible to load AboutResource from the server and cache it.
 class AboutResourceLoader {
  public:
-  explicit AboutResourceLoader(JobScheduler* scheduler);
+  // |clock| can be injected for testing.
+  explicit AboutResourceLoader(
+      JobScheduler* scheduler,
+      const base::TickClock* clock = base::DefaultTickClock::GetInstance());
   ~AboutResourceLoader();
 
   // Returns the cached about resource.
@@ -55,6 +60,8 @@ class AboutResourceLoader {
       google_apis::DriveApiErrorCode status,
       std::unique_ptr<google_apis::AboutResource> about_resource);
 
+  void EvictCachedAboutResource();
+
   JobScheduler* scheduler_;
   std::unique_ptr<google_apis::AboutResource> cached_about_resource_;
 
@@ -65,6 +72,8 @@ class AboutResourceLoader {
   // when GetAboutResource is called before the task completes.
   std::map<int, std::vector<google_apis::AboutResourceCallback>>
       pending_callbacks_;
+
+  std::unique_ptr<base::RetainingOneShotTimer> cache_eviction_timer_;
 
   THREAD_CHECKER(thread_checker_);
 
