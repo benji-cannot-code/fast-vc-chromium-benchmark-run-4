9b6bd38c7e8c09d81394b2b997b6370fc69a306a
Jun Cai <juncai@chromium.org>
2018-08-27 22:56:04 +0000

    Network Service: Add UMA for Origin header sanity check for URLLoaderFactory
    Bug: 862176
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Change-Id: I31885abe5eeb6eb56d775e2a80667d0fc30cafca
    Reviewed-on: https://chromium-review.googlesource.com/1187274
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Jun Cai <juncai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586466}

--- services/network/url_loader_factory.cc
+++ services/network/url_loader_factory.cc
@@ -6,9 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/url_loader_factory.h"
 
 #include <memory>
+#include <string>
 #include <utility>
 
 #include "base/logging.h"
+#include "base/metrics/histogram_macros.h"
+#include "base/optional.h"
 #include "services/network/cors/cors_url_loader_factory.h"
 #include "services/network/network_context.h"
 #include "services/network/network_service.h"
@@ -16,6 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/resource_request.h"
 #include "services/network/resource_scheduler_client.h"
 #include "services/network/url_loader.h"
+#include "url/gurl.h"
+#include "url/origin.h"
 
 namespace network {
 
@@ -65,6 +70,19 @@ void URLLoaderFactory::CreateLoaderAndStart(
     const ResourceRequest& url_request,
     mojom::URLLoaderClientPtr client,
     const net::MutableNetworkTrafficAnnotationTag& traffic_annotation) {
+  std::string origin_string;
+  bool has_origin = url_request.headers.GetHeader("Origin", &origin_string) &&
+                    origin_string != "null";
+  base::Optional<url::Origin> request_initiator = url_request.request_initiator;
+  if (has_origin && request_initiator.has_value()) {
+    url::Origin origin = url::Origin::Create(GURL(origin_string));
+    bool origin_head_same_as_request_origin =
+        request_initiator.value().IsSameOriginWith(origin);
+    UMA_HISTOGRAM_BOOLEAN(
+        "NetworkService.URLLoaderFactory.OriginHeaderSameAsRequestOrigin",
+        origin_head_same_as_request_origin);
+  }
+
   bool report_raw_headers = false;
   if (url_request.report_raw_headers) {
     const NetworkService* service = context_->network_service();
