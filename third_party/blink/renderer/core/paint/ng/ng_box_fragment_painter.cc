5675751fb08b7c33407cf8159ebe82f247617265
Xiaocheng Hu <xiaochengh@chromium.org>
2018-08-27 17:56:21 +0000

    [EditingNG] Paint caret in NG contenteditable blocks
    This patch makes NGBoxFragmentPainter paint caret by
    - Routing back to legacy LayoutBlock to determine if a block has
      carets to paint
    - Adding caret painting code to NGBoxFragmentPainter
    so that carets can be painted in NG contenteditable blocks.
    
    This patch makes other editing NG development/debugging easier.
    
    It also fixes some image failures with EditingNG flag enabled, e.g.
    editing/selection/editable-links.html.
    
    Bug: 876555
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng;luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I29968b24872603d999b8fd2301ffd465cac23406
    Reviewed-on: https://chromium-review.googlesource.com/1185499
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586309}

--- third_party/blink/renderer/core/paint/ng/ng_box_fragment_painter.cc
+++ third_party/blink/renderer/core/paint/ng/ng_box_fragment_painter.cc
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/paint/ng/ng_box_fragment_painter.h"
 
+#include "third_party/blink/renderer/core/editing/drag_caret.h"
+#include "third_party/blink/renderer/core/editing/frame_selection.h"
+#include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/layout/background_bleed_avoidance.h"
 #include "third_party/blink/renderer/core/layout/hit_test_location.h"
 #include "third_party/blink/renderer/core/layout/hit_test_result.h"
@@ -18,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_physical_text_fragment.h"
 #include "third_party/blink/renderer/core/layout/ng/layout_ng_mixin.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_physical_box_fragment.h"
+#include "third_party/blink/renderer/core/page/page.h"
 #include "third_party/blink/renderer/core/paint/background_image_geometry.h"
 #include "third_party/blink/renderer/core/paint/box_decoration_data.h"
 #include "third_party/blink/renderer/core/paint/list_marker_painter.h"
@@ -261,12 +265,25 @@ void NGBoxFragmentPainter::PaintObject(
 
   if (ShouldPaintSelfOutline(paint_phase))
     NGFragmentPainter(box_fragment_).PaintOutline(paint_info, paint_offset);
-  // TODO(layout-dev): Implement once we have selections in LayoutNG.
-  // If the caret's node's layout object's containing block is this block, and
+
+  // If the caret's node's fragment's containing block is this block, and
   // the paint action is PaintPhaseForeground, then paint the caret.
-  // if (paint_phase == PaintPhase::kForeground &&
-  //     box_fragment_.ShouldPaintCarets())
-  //  PaintCarets(paint_info, paint_offset);
+  if (paint_phase == PaintPhase::kForeground &&
+      box_fragment_.ShouldPaintCarets())
+    PaintCarets(paint_info, paint_offset);
+}
+
+void NGBoxFragmentPainter::PaintCarets(const PaintInfo& paint_info,
+                                       const LayoutPoint& paint_offset) {
+  LocalFrame* frame = box_fragment_.GetLayoutObject()->GetFrame();
+
+  if (box_fragment_.ShouldPaintCursorCaret())
+    frame->Selection().PaintCaret(paint_info.context, paint_offset);
+
+  if (box_fragment_.ShouldPaintDragCaret()) {
+    frame->GetPage()->GetDragCaret().PaintDragCaret(frame, paint_info.context,
+                                                    paint_offset);
+  }
 }
 
 void NGBoxFragmentPainter::PaintBlockFlowContents(
