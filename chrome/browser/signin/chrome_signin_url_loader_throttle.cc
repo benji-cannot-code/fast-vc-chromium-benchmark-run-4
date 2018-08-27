2afdad49c8e1a680285b43f1a2476712058c237b
Reilly Grant <reillyg@chromium.org>
2018-08-27 21:55:01 +0000

    Implement signin header modifications for sub-resource requests
    This change is a follow-up to r584270 and adds support for modifying
    headers sent in sub-resource requests made to the GAIA signon realm.
    This is done by inserting a proxy between the render process and network
    service when the frame's origin matches the GAIA signon realm.
    
    Bug: 789670
    Change-Id: I762a54dd6c8fec57c97f522fc6ef7de2179f3722
    Reviewed-on: https://chromium-review.googlesource.com/1187063
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586431}

--- chrome/browser/signin/chrome_signin_url_loader_throttle.cc
+++ chrome/browser/signin/chrome_signin_url_loader_throttle.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/signin/chrome_signin_url_loader_throttle.h"
 
 #include "chrome/browser/signin/chrome_signin_helper.h"
+#include "chrome/browser/signin/header_modification_delegate.h"
 #include "components/signin/core/browser/signin_header_helper.h"
 
 namespace signin {
@@ -117,16 +118,12 @@ class URLLoaderThrottle::ThrottleResponseAdapter : public ResponseAdapter {
   DISALLOW_COPY_AND_ASSIGN(ThrottleResponseAdapter);
 };
 
-URLLoaderThrottle::Delegate::Delegate() = default;
-
-URLLoaderThrottle::Delegate::~Delegate() = default;
-
 // static
 std::unique_ptr<URLLoaderThrottle> URLLoaderThrottle::MaybeCreate(
-    std::unique_ptr<Delegate> delegate,
+    std::unique_ptr<HeaderModificationDelegate> delegate,
     content::NavigationUIData* navigation_ui_data,
     content::ResourceRequestInfo::WebContentsGetter web_contents_getter) {
-  if (!delegate->ShouldIntercept(navigation_ui_data))
+  if (!delegate->ShouldInterceptNavigation(navigation_ui_data))
     return nullptr;
 
   return base::WrapUnique(new URLLoaderThrottle(
@@ -197,7 +194,7 @@ void URLLoaderThrottle::WillProcessResponse(
 }
 
 URLLoaderThrottle::URLLoaderThrottle(
-    std::unique_ptr<Delegate> delegate,
+    std::unique_ptr<HeaderModificationDelegate> delegate,
     content::ResourceRequestInfo::WebContentsGetter web_contents_getter)
     : delegate_(std::move(delegate)),
       web_contents_getter_(std::move(web_contents_getter)) {}
