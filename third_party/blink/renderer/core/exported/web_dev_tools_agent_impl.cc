f106ffb7655035752a001fc3c8403e86da8268c6
Pavel Feldman <pfeldman@chromium.org>
2018-08-27 22:02:01 +0000

    DevTools: introduce experimental domain for Testing-related methods.
    Change-Id: I1f6ae07e64193ee328ecc4ecd16f5c17e8447808
    Reviewed-on: https://chromium-review.googlesource.com/1188687
    Commit-Queue: Pavel Feldman <pfeldman@chromium.org>
    Reviewed-by: Andrey Lushnikov <lushnikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586438}

--- third_party/blink/renderer/core/exported/web_dev_tools_agent_impl.cc
+++ third_party/blink/renderer/core/exported/web_dev_tools_agent_impl.cc
@@ -77,6 +77,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/inspector/inspector_resource_content_loader.h"
 #include "third_party/blink/renderer/core/inspector/inspector_session.h"
 #include "third_party/blink/renderer/core/inspector/inspector_task_runner.h"
+#include "third_party/blink/renderer/core/inspector/inspector_testing_agent.h"
 #include "third_party/blink/renderer/core/inspector/inspector_worker_agent.h"
 #include "third_party/blink/renderer/core/inspector/main_thread_debugger.h"
 #include "third_party/blink/renderer/core/layout/layout_view.h"
@@ -470,6 +471,8 @@ void WebDevToolsAgentImpl::Session::InitializeInspectorSession(
   // during remote->local transition we cannot access mainFrameImpl() yet, so
   // we have to store the frame which will become the main frame later.
   inspector_session_->Append(new InspectorEmulationAgent(frame_.Get()));
+
+  inspector_session_->Append(new InspectorTestingAgent(inspected_frames));
 
   // Call session init callbacks registered from higher layers
   CoreInitializer::GetInstance().InitInspectorAgentSession(
