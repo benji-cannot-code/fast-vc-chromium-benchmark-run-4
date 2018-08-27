3149bc7ed7b7582423a0124cb19b4f6f0f9ecc5b
Theresa <twellington@chromium.org>
2018-08-27 20:15:48 +0000

    [Modern] Fix tint for tablet tab switcher new tab button
    BUG=877916
    
    Change-Id: I6f3d4131fa3205170d2234c892471a4bbaf2323e
    Reviewed-on: https://chromium-review.googlesource.com/1191125
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586386}

--- chrome/android/java/src/org/chromium/chrome/browser/widget/newtab/NewTabButton.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/newtab/NewTabButton.java
@@ -19,6 +19,7 @@ import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeFeatureList;
 import org.chromium.chrome.browser.device.DeviceClassManager;
+import org.chromium.ui.base.DeviceFormFactor;
 
 /**
  * Button for creating new tabs.
@@ -46,6 +47,7 @@ public class NewTabButton extends Button implements Drawable.Callback {
         mModernDrawable.setBounds(
                 0, 0, mModernDrawable.getIntrinsicWidth(), mModernDrawable.getIntrinsicHeight());
         mModernDrawable.setCallback(this);
+        updateDrawableTint();
     }
 
     /**
@@ -126,11 +128,13 @@ public class NewTabButton extends Button implements Drawable.Callback {
 
     /** Update the tint for the icon drawable for Chrome Modern. */
     private void updateDrawableTint() {
-        final boolean shouldUseLightMode = mIsNativeReady
-                && (DeviceClassManager.enableAccessibilityLayout()
-                           || ChromeFeatureList.isEnabled(
-                                      ChromeFeatureList.HORIZONTAL_TAB_SWITCHER_ANDROID))
-                && mIsIncognito;
+        final boolean shouldUseLightMode =
+                DeviceFormFactor.isNonMultiDisplayContextOnTablet(getContext())
+                || (mIsNativeReady
+                           && (DeviceClassManager.enableAccessibilityLayout()
+                                      || ChromeFeatureList.isEnabled(
+                                                 ChromeFeatureList.HORIZONTAL_TAB_SWITCHER_ANDROID))
+                           && mIsIncognito);
         mModernDrawable.setTintList(shouldUseLightMode ? mLightModeTint : mDarkModeTint);
     }
 }
