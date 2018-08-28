e86161a77fea56d2406c92af6a97bc9b676377e5
Stuart Langley <slangley@google.com>
2018-08-28 01:17:10 +0000

    Change GetFileContentInitializedCallback from RepeatingCallback to OnceCallback.
    Part of ongoing work to cleanup various file system callbacks from repeating
    to once callback, to match the actual desired semantics of the operaton flow.
    
    Introduces no logic changes.
    
    Bug: 875700
    Change-Id: Iae1ed25ce1d04a71cf0805e773b84fa618d4cf3e
    Reviewed-on: https://chromium-review.googlesource.com/1189686
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Commit-Queue: Stuart Langley <slangley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586531}

--- components/drive/chromeos/dummy_file_system.cc
+++ components/drive/chromeos/dummy_file_system.cc
@@ -8,10 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace drive {
 
 base::Closure DummyFileSystem::GetFileContent(
-      const base::FilePath& file_path,
-      const GetFileContentInitializedCallback& initialized_callback,
-      const google_apis::GetContentCallback& get_content_callback,
-      const FileOperationCallback& completion_callback) {
+    const base::FilePath& file_path,
+    GetFileContentInitializedCallback initialized_callback,
+    const google_apis::GetContentCallback& get_content_callback,
+    const FileOperationCallback& completion_callback) {
   return base::DoNothing();
 }
 
