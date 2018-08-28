d4d4514801ace06a4c93f6cf44df81497d4814a4
Josh Karlin <jkarlin@chromium.org>
2018-08-28 01:05:58 +0000

    Update Filter Tool to output in sorted order and display hit count
    Bug: 877148
    Change-Id: I06beb16416689912aec7802a87e2775f1314b620
    Reviewed-on: https://chromium-review.googlesource.com/1191746
    Commit-Queue: Josh Karlin <jkarlin@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586529}

--- components/subresource_filter/tools/filter_tool.cc
+++ components/subresource_filter/tools/filter_tool.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/subresource_filter/tools/filter_tool.h"
 
+#include <algorithm>
 #include <istream>
 #include <ostream>
 #include <string>
@@ -187,13 +188,27 @@ void FilterTool::MatchBatchImpl(std::istream* request_stream,
   if (print_each_request)
     return;
 
+  // Sort the rules in descending order by match count.
+  std::vector<std::pair<std::string, int>> vector_rules;
   for (auto rule_and_count : matched_rules) {
-    if (rule_and_count.second >= min_match_count) {
-      *output_ << url_pattern_index::FlatUrlRuleToFilterlistString(
-                      rule_and_count.first)
-                      .c_str()
-               << std::endl;
-    }
+    if (rule_and_count.second < min_match_count)
+      continue;
+
+    vector_rules.push_back(std::make_pair(
+        url_pattern_index::FlatUrlRuleToFilterlistString(rule_and_count.first)
+            .c_str(),
+        rule_and_count.second));
+  }
+
+  std::sort(vector_rules.begin(), vector_rules.end(),
+            [](const std::pair<std::string, int>& left,
+               const std::pair<std::string, int>& right) {
+              return left.second > right.second;
+            });
+
+  for (auto rule_and_count : vector_rules) {
+    *output_ << rule_and_count.second << " " << rule_and_count.first
+             << std::endl;
   }
 }
 
