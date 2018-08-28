11f9063c41f3dcc44c2bd292874c13f12db1d41a
Donn Denman <donnd@chromium.org>
2018-08-28 23:26:28 +0000

    [TTS] Unit tests for Unified Consent integration.
    Adds a native and a Java unit test to exercise the Unified Consent
    integration with Contextual Search.
    
    The native test checks that the previous-enabled state is captured
    correctly by the native preference in the CS component.
    
    The Java test checks that request throttling is done correctly based on
    the field-trial parameters and the previous-enabled state metadata.
    
    Also remove a parameter that's not needed in the component preference
    header file.
    
    BUG=825428
    
    Change-Id: I5937f8d3e8495547f33126f3118e2c77aac1d756
    Reviewed-on: https://chromium-review.googlesource.com/1186026
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Donn Denman <donnd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586925}

--- chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchPreferenceHelper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchPreferenceHelper.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.contextualsearch;
 
+import org.chromium.base.VisibleForTesting;
 import org.chromium.chrome.browser.ChromeFeatureList;
 import org.chromium.chrome.browser.preferences.ChromePreferenceManager;
 import org.chromium.chrome.browser.preferences.Pref;
@@ -66,7 +67,7 @@ class ContextualSearchPreferenceHelper {
                 && wasUndecided();
     }
 
-    // Updates our preference metadata storage based on what our native member knows.
+    /** Updates our preference metadata storage based on what our native member knows. */
     void onPreferenceChangedInternal() {
         updatePreviousPreferenceStorage(nativeGetPreferenceMetadata(mNativePointer));
     }
@@ -106,6 +107,18 @@ class ContextualSearchPreferenceHelper {
         assert setting != ContextualSearchPreviousPreferenceMetadata.UNKNOWN;
         ChromePreferenceManager.getInstance().writeInt(
                 ChromePreferenceManager.CONTEXTUAL_SEARCH_PRE_UNIFIED_CONSENT_PREF, setting);
+    }
+
+    /**
+     * Applies previous metadata as would be recorded by native Unified Consent integration, for use
+     * by tests only.
+     * @param metadata The metadata expressing the previous user's enabled-state.
+     */
+    @VisibleForTesting
+    void applyUnifiedConsentGivenMetadata(
+            @ContextualSearchPreviousPreferenceMetadata int metadata) {
+        ChromePreferenceManager.getInstance().writeInt(
+                ChromePreferenceManager.CONTEXTUAL_SEARCH_PRE_UNIFIED_CONSENT_PREF, metadata);
     }
 
     private native long nativeInit();
