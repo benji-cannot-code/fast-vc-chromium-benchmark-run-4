808641423b4b23243808f73e9577b41614c74892
Daniel Rubery <drubery@chromium.org>
2018-08-28 22:25:15 +0000

    Remove CanCheckUrl from CanCheckCsdWhitelistUrl
    Currently, PhishGuard pings for about:blank are being whitelisted, since
    the whitelist checks CanCheckUrl. This CL removes that check, so pings
    for about:blank can make it through.
    
    Bug: 865625
    Change-Id: I05137820f82527748fb757e8a2ccc73ffc60cd76
    Reviewed-on: https://chromium-review.googlesource.com/1187716
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Reviewed-by: Jialiu Lin <jialiul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586890}

--- components/safe_browsing/db/test_database_manager.cc
+++ components/safe_browsing/db/test_database_manager.cc
@@ -26,7 +26,7 @@ bool TestSafeBrowsingDatabaseManager::CanCheckResourceType(
 
 bool TestSafeBrowsingDatabaseManager::CanCheckUrl(const GURL& url) const {
   NOTIMPLEMENTED();
-  return false;
+  return (url != GURL("about:blank"));
 }
 
 bool TestSafeBrowsingDatabaseManager::ChecksAreAlwaysAsync() const {
