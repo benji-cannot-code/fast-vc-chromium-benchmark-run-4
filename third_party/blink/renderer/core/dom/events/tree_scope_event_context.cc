12922ad8a57df06875b2bc706124c1ffb15cda82
Kent Tamura <tkent@chromium.org>
2018-08-28 04:05:40 +0000

    DOM: Reduce the size of window_event_context.h
    This CL reduces the pre-processed size of window_event_context.h by
    3.63MB.
    
    Bug: 242216
    Change-Id: I7a93ad794d7adf10b5f67291e41bf550703440e5
    Reviewed-on: https://chromium-review.googlesource.com/1189308
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586575}

--- third_party/blink/renderer/core/dom/events/tree_scope_event_context.cc
+++ third_party/blink/renderer/core/dom/events/tree_scope_event_context.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/shadow_root.h"
 #include "third_party/blink/renderer/core/dom/static_node_list.h"
 #include "third_party/blink/renderer/core/events/touch_event_context.h"
+#include "third_party/blink/renderer/core/frame/local_dom_window.h"
 
 namespace blink {
 
--- third_party/blink/renderer/core/dom/events/window_event_context.h
+++ third_party/blink/renderer/core/dom/events/window_event_context.h
@@ -29,14 +29,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_DOM_EVENTS_WINDOW_EVENT_CONTEXT_H_
 
 #include "base/macros.h"
-#include "base/memory/scoped_refptr.h"
-#include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
 
 namespace blink {
 
 class EventTarget;
 class Event;
+class LocalDOMWindow;
 class NodeEventContext;
 
 class WindowEventContext : public GarbageCollected<WindowEventContext> {
