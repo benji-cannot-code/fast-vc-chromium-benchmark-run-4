716cdcda3e9c471c7de8133a62c80f0c2d81c14e
Sammie Quon <sammiequon@google.com>
2018-08-28 05:07:46 +0000

    applist: Allow using gestures to hide transient windows.
    This was skipped over in the initial patch to keep the size lower. This
    cl adds functionality to minimize windows with transient children.
    Also puts the code behind a feature.
    
    Tests: added test
    Bug: 872319
    Change-Id: I15387bbcaddeb1e891df70baa385c8a0fbc285cd
    Reviewed-on: https://chromium-review.googlesource.com/1191416
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586585}

--- ash/app_list/app_list_controller_impl.cc
+++ ash/app_list/app_list_controller_impl.cc
@@ -74,7 +74,8 @@ AppListControllerImpl::AppListControllerImpl(
   Shell::Get()->AddShellObserver(this);
   keyboard::KeyboardController::Get()->AddObserver(this);
 
-  if (IsHomeLauncherEnabledInTabletMode()) {
+  if (IsHomeLauncherEnabledInTabletMode() &&
+      app_list::features::IsHomeLauncherGesturesEnabled()) {
     home_launcher_gesture_handler_ =
         std::make_unique<HomeLauncherGestureHandler>();
   }
@@ -526,7 +527,8 @@ void AppListControllerImpl::OnOverviewModeEnding() {
 void AppListControllerImpl::OnTabletModeStarted() {
   // TODO(sammiequon): Find a better way to handle events that are not in tablet
   // mode than controlling the lifetime fo this object.
-  if (is_home_launcher_enabled_) {
+  if (IsHomeLauncherEnabledInTabletMode() &&
+      app_list::features::IsHomeLauncherGesturesEnabled()) {
     home_launcher_gesture_handler_ =
         std::make_unique<HomeLauncherGestureHandler>();
   }
