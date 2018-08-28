5da74b955934bc4a48400b424e2ef27ce5d27f80
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-08-28 09:20:14 +0000

    Import wpt@3d172bc612e03a896b5611d9e9652f860995feef
    Using wpt-import in Chromium d143f76c406803239e216b67e89b420f9a546e8a.
    With Chromium commits locally applied on WPT:
    fbb650b347 "Add WPT serialization tests for wasm modules"
    
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/23539
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    yhirano@chromium.org, mkwst@chromium.org, japhet@chromium.org:
      external/wpt/fetch
    
    NOAUTOREVERT=true
    TBR=robertma
    
    No-Export: true
    Change-Id: I784e7de8ee0142fc715e71a591236641e05169a3
    Reviewed-on: https://chromium-review.googlesource.com/1192593
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586624}

---
+++ third_party/WebKit/LayoutTests/external/wpt/fetch/api/basic/header-value-null-byte.any.js
@@ -0,0 +1,6 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: global=window,worker
+
+promise_test(t => {
+  return promise_rejects(t, new TypeError(), fetch("../../../xhr/resources/parse-headers.py?my-custom-header="+encodeURIComponent("x\0x")));
+}, "Ensure fetch() rejects null bytes in headers");
