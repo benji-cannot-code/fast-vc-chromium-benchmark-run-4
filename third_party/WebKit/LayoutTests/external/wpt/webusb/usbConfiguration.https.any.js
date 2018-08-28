bca8c3c1ad4f879002b873a30a4817f2240ec9d0
Ovidio Henriquez <odejesush@chromium.org>
2018-08-28 23:32:53 +0000

    Improve Code Coverage for WebUSB
    This change improves the code coverage for the sources files under
    //third_party/blink/renderer/modules/webusb.
    
    The affected files are:
    * usb.cc
    * usb_alternate_interface.cc
    * usb_configuration.cc
    * usb_device.cc
    * usb_endpoint.cc
    * usb_interface.cc
    
    Bug: 854831
    Change-Id: Ie67533ece27f1c944b0e9186d01a61219a30f805
    Reviewed-on: https://chromium-review.googlesource.com/1183789
    Commit-Queue: Ovidio Henriquez <odejesush@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586930}

---
+++ third_party/WebKit/LayoutTests/external/wpt/webusb/usbConfiguration.https.any.js
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/webusb/resources/fake-devices.js
+// META: script=/webusb/resources/usb-helpers.js
+'use strict';
+
+usb_test(async () => {
+  let { device } = await getFakeDevice();
+  let configuration = new USBConfiguration(
+      device, device.configurations[1].configurationValue);
+  assertDeviceInfoEquals(configuration, fakeDeviceInit.configurations[1]);
+}, 'Can construct a USBConfiguration.');
+
+usb_test(async () => {
+  let { device } = await getFakeDevice();
+  try {
+    let configuration =
+        new USBConfiguration(device, device.configurations.length + 1);
+    assert_unreached(
+        'USBConfiguration should reject an invalid configuration value');
+  } catch (error) {
+    assert_equals(error.name, 'RangeError');
+  }
+}, 'Constructing a USBConfiguration with an invalid configuration value ' +
+    'throws a range error.');
