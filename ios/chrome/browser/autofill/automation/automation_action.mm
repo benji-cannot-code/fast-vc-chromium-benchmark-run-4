c39d68f9d95824787d3c424c53762be0adaef53e
Eric Aleshire <ericale@google.com>
2018-08-28 18:19:47 +0000

    Add waitFor JS assertion action to Autofill EarlGrey automation tests.
    My first CL into Autofill iOS automation - continuing off noyau@'s
    work by expanding the capabilities of the automation suite to match work
    done on other platforms. Previously, only the "click" action was
    implemented in iOS, and this adds the "waitFor" action, which waits
    until all associated Javascript assertions are true before continuing
    with the test script.
    
    This is meant to be a fairly 1:1 port of the equivalent actions
    (composed of TestRecipeReplayer::ExecuteWaitForStateAction,
    WaitForStateChange, and AllAssertionsPassed) here:
    https://chromium.googlesource.com/chromium/src/+/831f175d0cafe05c00306104bd86dfb4c00921da/chrome/browser/autofill/captured_sites_test_utils.cc#477
    
    Next, I plan on adding the "autofill" action.
    
    Bug: None
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Iaea2e2ecd68eb29755fb4a8be6736bdaea64dcfb
    Reviewed-on: https://chromium-review.googlesource.com/1185920
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: ericale <ericale@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586790}

--- ios/chrome/browser/autofill/automation/automation_action.mm
+++ ios/chrome/browser/autofill/automation/automation_action.mm
@@ -9,11 +9,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/stringprintf.h"
 #include "base/strings/sys_string_conversions.h"
+#import "base/test/ios/wait_util.h"
 #import "ios/chrome/test/app/chrome_test_util.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
 #import "ios/web/public/test/earl_grey/web_view_actions.h"
 #import "ios/web/public/test/earl_grey/web_view_matchers.h"
 #include "ios/web/public/test/element_selector.h"
+#import "ios/web/public/test/js_test_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -41,7 +43,25 @@ using web::test::ElementSelector;
 @end
 
 // An action that simply tap on an element on the page.
+// Right now this always assumes a click event of the following format:
+//  {
+//    "selectorType": "xpath",
+//    "selector": "//*[@id=\"add-to-cart-button\"]",
+//    "context": [],
+//    "type": "click"
+//  },
 @interface AutomationActionClick : AutomationAction
+@end
+
+// An action that waits for a series of JS assertions to become true before
+// continuing. We assume this action has a format resembling:
+// {
+//   "context": [],
+//   "type": "waitFor",
+//   "assertions": ["return document.querySelector().style.display ===
+//   'none';"]
+// },
+@interface AutomationActionWaitFor : AutomationAction
 @end
 
 @implementation AutomationAction
@@ -62,6 +82,7 @@ using web::test::ElementSelector;
 + (Class)classForType:(NSString*)type {
   static NSDictionary* classForType = @{
     @"click" : [AutomationActionClick class],
+    @"waitFor" : [AutomationActionWaitFor class],
     // More to come.
   };
 
@@ -90,14 +111,6 @@ using web::test::ElementSelector;
 @implementation AutomationActionClick
 
 - (void)execute {
-  // Right now this always assumes a click event of the following format:
-  //  {
-  //    "selectorType": "xpath",
-  //    "selector": "//*[@id=\"add-to-cart-button\"]",
-  //    "context": [],
-  //    "type": "click"
-  //  },
-
   const base::Value* xpathValue(self.actionDictionary->FindKeyOfType(
       "selector", base::Value::Type::STRING));
   GREYAssert(xpathValue, @"Selector is missing in action.");
@@ -116,6 +129,62 @@ using web::test::ElementSelector;
   // Tap on the element.
   [[EarlGrey selectElementWithMatcher:web::WebViewInWebState(web_state)]
       performAction:web::WebViewTapElement(web_state, selector)];
+}
+
+@end
+
+@implementation AutomationActionWaitFor
+
+- (void)execute {
+  const base::Value* assertionsValue(self.actionDictionary->FindKeyOfType(
+      "assertions", base::Value::Type::LIST));
+  GREYAssert(assertionsValue, @"Assertions key is missing in action.");
+
+  const base::Value::ListStorage& assertionsValues(assertionsValue->GetList());
+  GREYAssert(assertionsValues.size(), @"Assertions list is empty.");
+
+  std::vector<std::string> state_assertions;
+
+  for (auto const& assertionValue : assertionsValues) {
+    const std::string assertionString(assertionValue.GetString());
+    GREYAssert(!assertionString.empty(), @"assertionString is an empty value.");
+    state_assertions.push_back(assertionString);
+  }
+
+  GREYAssert(base::test::ios::WaitUntilConditionOrTimeout(
+                 base::test::ios::kWaitForActionTimeout,
+                 ^{
+                   return [self CheckForJsAssertionFailures:state_assertions] ==
+                          nil;
+                 }),
+             @"waitFor State change hasn't completed within timeout.");
+}
+
+// Executes a vector of Javascript assertions on the webpage, returning the
+// first assertion that fails to be true, or nil if all assertions are true.
+- (NSString*)CheckForJsAssertionFailures:
+    (const std::vector<std::string>&)assertions {
+  for (std::string const& assertion : assertions) {
+    NSError* error;
+    NSString* assertionString =
+        [NSString stringWithUTF8String:assertion.c_str()];
+
+    id result = chrome_test_util::ExecuteJavaScript(
+        [NSString stringWithFormat:@""
+                                    "    (function() {"
+                                    "      try {"
+                                    "        %@"
+                                    "      } catch (ex) {}"
+                                    "      return false;"
+                                    "    })();",
+                                   assertionString],
+        &error);
+
+    if (![result boolValue] || error) {
+      return assertionString;
+    }
+  }
+  return nil;
 }
 
 @end
