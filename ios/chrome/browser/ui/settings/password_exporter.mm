d580548ec5c4097d3d0ba35646d517ca4d918ade
Etienne Pierre-doray <etiennep@chromium.org>
2018-08-28 22:16:34 +0000

    [TaskScheduler]: Use ScopedBlockingCall to mark blocking tasks.
    This CL uses ScopedBlockingCall to mark blocking calls in /ios/chrome/browser/ui/settings.
    
    This CL was created by replacing calls to AssertBlockingAllowed()
    with instantiations of ScopedBlockingCall(MAY_BLOCK).
    I kindly ask the reviewer to make sure of the following:
      - ScopedBlockingCall is instantiated in a scope with minimal CPU usage.
        If this is not the case, ScopedBlockingCall should be instantiated
        closer to the blocking call. See scoped_blocking_call.h for more
        info. Please let me know when/where the blocking call happens if this needs
        to be changed.
      - Parameter |blocking_type| matches expectation (MAY_BLOCK/WILL_BLOCK). See
        BlockingType for more info. While I assumed MAY_BLOCK by default, that might
        not be the best fit if we know that this callsite is guaranteed to block.
      - The ScopedBlockingCall's scope covers the entirety of the blocking operation
        previously asserted against by the AssertBlockingAllowed().
    
    This CL was uploaded by git cl split.
    
    R=sczs@chromium.org
    
    Bug: 874080
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: I3da7acaa1d239af5fc937b7204aa649996555758
    Reviewed-on: https://chromium-review.googlesource.com/1191196
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586887}

--- ios/chrome/browser/ui/settings/password_exporter.mm
+++ ios/chrome/browser/ui/settings/password_exporter.mm
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/sys_string_conversions.h"
 #include "base/task/post_task.h"
 #include "base/task_runner_util.h"
-#include "base/threading/thread_restrictions.h"
+#include "base/threading/scoped_blocking_call.h"
 #include "components/autofill/core/common/password_form.h"
 #include "components/password_manager/core/browser/export/password_csv_writer.h"
 #include "components/password_manager/core/browser/password_manager_metrics_util.h"
@@ -62,12 +62,13 @@ enum class ReauthenticationStatus {
             toURL:(NSURL*)fileURL
           handler:(void (^)(WriteToURLStatus))handler {
   WriteToURLStatus (^writeToFile)() = ^{
-    base::AssertBlockingAllowed();
     NSError* error = nil;
 
     NSURL* directoryURL = [fileURL URLByDeletingLastPathComponent];
     NSFileManager* fileManager = [NSFileManager defaultManager];
 
+    base::ScopedBlockingCall scoped_blocking_call(
+        base::BlockingType::WILL_BLOCK);
     if (![fileManager createDirectoryAtURL:directoryURL
                withIntermediateDirectories:YES
                                 attributes:nil
@@ -351,8 +352,9 @@ enum class ReauthenticationStatus {
   base::PostTaskWithTraits(
       FROM_HERE, {base::MayBlock(), base::TaskPriority::BEST_EFFORT},
       base::BindOnce(^{
-        base::AssertBlockingAllowed();
         NSFileManager* fileManager = [NSFileManager defaultManager];
+        base::ScopedBlockingCall scoped_blocking_call(
+            base::BlockingType::WILL_BLOCK);
         [fileManager removeItemAtURL:uniqueDirectoryURL error:nil];
       }));
 }
