cb9290af56de46ceefac8a21abf5828b54c5ff80
Stuart Langley <slangley@google.com>
2018-08-28 05:10:58 +0000

    Change OpenFileCallback to OnceCallback.
    Part of ongoing work to cleanup various file system callbacks from repeating
    to once callback, to match the actual desired semantics of the operaton flow.
    
    Introduces no logic changes, although updates function signatures to use
    pass by value for OnceCallback rather than pass by const-ref, which is the
    canonical way to do it.
    
    In a followup CL I will change the nested callback to be OnceCallback, for now
    leave it as repeating.
    
    Bug: 875700
    Change-Id: Ia889c349085772a2185e8eb553145c662d486520
    Reviewed-on: https://chromium-review.googlesource.com/1192364
    Reviewed-by: Anand Mistry <amistry@chromium.org>
    Commit-Queue: Stuart Langley <slangley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586586}

--- components/drive/chromeos/dummy_file_system.h
+++ components/drive/chromeos/dummy_file_system.h
@@ -26,7 +26,7 @@ class DummyFileSystem : public FileSystemInterface {
   void OpenFile(const base::FilePath& file_path,
                 OpenMode open_mode,
                 const std::string& mime_type,
-                const OpenFileCallback& callback) override {}
+                OpenFileCallback callback) override {}
   void Copy(const base::FilePath& src_file_path,
             const base::FilePath& dest_file_path,
             bool preserve_last_modified,
