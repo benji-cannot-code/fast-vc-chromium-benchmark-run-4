8e7b929cc78aa0ef95a5feca1eb2e32464b23771
John Rummell <jrummell@chromium.org>
2018-08-28 22:48:19 +0000

    Include Media Licenses in Clear Browsing Data basic tab on Android
    On Android, the "Cookies and site data" item on the basic tab of Clear
    Browsing Data is updated to include media licenses. If there are media
    licenses present, the message now include "You may lose access to
    protected content from <site.com> and some other sites."
    
    BUG=877999
    TEST=tested manually
    
    Change-Id: Ic17653cbc8799cf6551e6a279c314d31eb26daaf
    Reviewed-on: https://chromium-review.googlesource.com/1179308
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: John Rummell <jrummell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586900}

--- chrome/browser/browsing_data/counters/media_licenses_counter.cc
+++ chrome/browser/browsing_data/counters/media_licenses_counter.cc
@@ -101,6 +101,8 @@ class MediaLicensesCounterAndroid : public MediaLicensesCounter {
   explicit MediaLicensesCounterAndroid(Profile* profile);
   ~MediaLicensesCounterAndroid() override;
 
+  const char* GetPrefName() const override;
+
  private:
   // BrowsingDataCounter implementation.
   void Count() final;
@@ -116,6 +118,14 @@ MediaLicensesCounterAndroid::~MediaLicensesCounterAndroid() = default;
 void MediaLicensesCounterAndroid::Count() {
   ReportResult(std::make_unique<MediaLicenseResult>(
       this, cdm::MediaDrmStorageImpl::GetAllOrigins(profile_->GetPrefs())));
+}
+
+const char* MediaLicensesCounterAndroid::GetPrefName() const {
+  // On the basic tab Media Licenses are cleared via the cookies checkbox.
+  // On the advanced tab, Media Licenses is its own checkbox.
+  return GetTab() == browsing_data::ClearBrowsingDataTab::BASIC
+             ? browsing_data::prefs::kDeleteCookiesBasic
+             : browsing_data::prefs::kDeleteMediaLicenses;
 }
 
 #endif  // defined(OS_ANDROID)
