20a9431ab9d5ccc7ea6ff16d7db52b707de66a1d
Adam Rice <ricea@chromium.org>
2018-08-28 06:34:00 +0000

    Update TransformStream to fix transform algo error behaviour
    Update TransformStream to standard version
    78cfd1e22b717ce7e6d3aae4e36de0ef9101356e. This includes change
    https://github.com/whatwg/streams/pull/948 which corrects the behaviour when the
    transformAlgorithm rejects.
    
    Also enable test TransformStreamTest.ThrowFromTransform which was disabled
    pending this fix.
    
    BUG=845427
    
    Change-Id: If0eff40c5855bf662098eecc87e32256b97dc71e
    Reviewed-on: https://chromium-review.googlesource.com/1167011
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586601}

--- third_party/blink/renderer/core/streams/TransformStream.js
+++ third_party/blink/renderer/core/streams/TransformStream.js
@@ -312,14 +312,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       if (typeof transformMethod !== 'function') {
         throw new TypeError('transformer.transform is not a function');
       }
-      transformAlgorithm = chunk => {
-        const transformPromise =
-            PromiseCall2(transformMethod, transformer, chunk, controller);
-        return thenPromise(transformPromise, undefined, e => {
-          TransformStreamError(stream, e);
-          throw e;
-        });
-      };
+      transformAlgorithm = chunk =>
+          PromiseCall2(transformMethod, transformer, chunk, controller);
     } else {
       transformAlgorithm = chunk => {
         try {
@@ -370,6 +364,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     TransformStreamError(controller[_controlledTransformStream], e);
   }
 
+  function TransformStreamDefaultControllerPerformTransform(controller, chunk) {
+    const transformPromise = controller[_transformAlgorithm](chunk, controller);
+    return thenPromise(transformPromise, undefined, r => {
+      TransformStreamError(controller[_controlledTransformStream], r);
+      throw r;
+    });
+  }
+
   function TransformStreamDefaultControllerTerminate(controller) {
     const stream = controller[_controlledTransformStream];
     const readableController =
@@ -405,11 +407,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         // assert(binding.isWritableStreamWritable(writable),
         //        `state is "writable"`);
 
-        return controller[_transformAlgorithm](chunk, controller);
+        return TransformStreamDefaultControllerPerformTransform(controller,
+                                                                chunk);
       });
     }
 
-    return controller[_transformAlgorithm](chunk, controller);
+    return TransformStreamDefaultControllerPerformTransform(controller, chunk);
   }
 
   function TransformStreamDefaultSinkAbortAlgorithm(stream, reason) {
