a99648a4cb31dd95f72453941b083531e62bbb9b
Christopher Cameron <ccameron@chromium.org>
2018-08-28 21:55:14 +0000

    RemoteMacViews: Remove use of views from touchbar
    Change BridgedContentView(TouchBarAdditions) to no longer directly
    call into the hostedView_ member (a views::View) directly, and
    route these calls through the BridgedNativeWidgetHost interface
    (which will be mojo-ified).
    
    Bug: 859152
    Change-Id: I016ed2ead343e5d66900e9fc01441b77439d0208
    Reviewed-on: https://chromium-review.googlesource.com/1192546
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586870}

--- ui/views/cocoa/bridged_native_widget_host_impl.mm
+++ ui/views/cocoa/bridged_native_widget_host_impl.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/hit_test.h"
 #include "ui/base/ime/input_method.h"
 #include "ui/base/ime/input_method_factory.h"
+#include "ui/base/models/dialog_model.h"
 #include "ui/compositor/recyclable_compositor_mac.h"
 #include "ui/display/screen.h"
 #include "ui/gfx/geometry/dip_util.h"
@@ -16,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/widget/native_widget_mac.h"
 #include "ui/views/widget/widget_aura_utils.h"
 #include "ui/views/widget/widget_delegate.h"
+#include "ui/views/window/dialog_client_view.h"
 #include "ui/views/window/dialog_delegate.h"
 #include "ui/views/word_lookup_client.h"
 
@@ -475,6 +477,43 @@ void BridgedNativeWidgetHostImpl::OnWindowKeyStatusChanged(
       widget->GetFocusManager()->StoreFocusedView(true);
     }
   }
+}
+
+void BridgedNativeWidgetHostImpl::DoDialogButtonAction(
+    ui::DialogButton button) {
+  views::DialogDelegate* dialog =
+      root_view_->GetWidget()->widget_delegate()->AsDialogDelegate();
+  DCHECK(dialog);
+  views::DialogClientView* client = dialog->GetDialogClientView();
+  if (button == ui::DIALOG_BUTTON_OK) {
+    client->AcceptWindow();
+  } else {
+    DCHECK_EQ(button, ui::DIALOG_BUTTON_CANCEL);
+    client->CancelWindow();
+  }
+}
+
+void BridgedNativeWidgetHostImpl::GetDialogButtonInfo(
+    ui::DialogButton button,
+    bool* button_exists,
+    base::string16* button_label,
+    bool* is_button_enabled,
+    bool* is_button_default) {
+  *button_exists = false;
+  ui::DialogModel* model =
+      root_view_->GetWidget()->widget_delegate()->AsDialogDelegate();
+  if (!model || !(model->GetDialogButtons() & button))
+    return;
+  *button_exists = true;
+  *button_label = model->GetDialogButtonLabel(button);
+  *is_button_enabled = model->IsDialogButtonEnabled(button);
+  *is_button_default = button == model->GetDefaultDialogButton();
+}
+
+void BridgedNativeWidgetHostImpl::GetDoDialogButtonsExist(bool* buttons_exist) {
+  ui::DialogModel* model =
+      root_view_->GetWidget()->widget_delegate()->AsDialogDelegate();
+  *buttons_exist = model && model->GetDialogButtons();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
