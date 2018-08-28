7442e71801b1600fd3c2a55195c530a1bbddd0e4
Xida Chen <xidachen@chromium.org>
2018-08-28 16:31:13 +0000

    [Debugging] Remove DumpWithoutCrashing in TouchActionFilter
    Right now there are few places in TouchActionFilter where we set a
    crash key and do DumpWithoutCrashing for debugging purpose. We don't
    actually need the DumpWithoutCrashing because the crash will happen
    just one line below the DumpWithoutCrashing, which means when it crashes,
    we will have the crash key for debugging.
    
    This CL removes the DumpWithoutCrashing.
    
    TBR=dtapuska@chromium.org
    
    Bug: None
    Change-Id: I78f53cae28455e51e9ddb2ea6f20dc285967a954
    Reviewed-on: https://chromium-review.googlesource.com/1193745
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586735}

--- content/browser/renderer_host/input/touch_action_filter.cc
+++ content/browser/renderer_host/input/touch_action_filter.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <math.h>
 
 #include "base/debug/crash_logging.h"
-#include "base/debug/dump_without_crashing.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_macros.h"
 #include "third_party/blink/public/platform/web_gesture_event.h"
@@ -63,7 +62,6 @@ FilterGestureEventResult TouchActionFilter::FilterGestureEvent(
         static auto* crash_key = base::debug::AllocateCrashKeyString(
             "scrollbegin-gestures", base::debug::CrashKeySize::Size256);
         base::debug::SetCrashKeyString(crash_key, gesture_sequence_);
-        base::debug::DumpWithoutCrashing();
         gesture_sequence_.clear();
       }
       suppress_manipulation_events_ =
@@ -90,7 +88,6 @@ FilterGestureEventResult TouchActionFilter::FilterGestureEvent(
         static auto* crash_key = base::debug::AllocateCrashKeyString(
             "scrollupdate-gestures", base::debug::CrashKeySize::Size256);
         base::debug::SetCrashKeyString(crash_key, gesture_sequence_);
-        base::debug::DumpWithoutCrashing();
         gesture_sequence_.clear();
       }
       if (IsYAxisActionDisallowed(scrolling_touch_action_.value())) {
@@ -145,7 +142,6 @@ FilterGestureEventResult TouchActionFilter::FilterGestureEvent(
         static auto* crash_key = base::debug::AllocateCrashKeyString(
             "tapunconfirmed-gestures", base::debug::CrashKeySize::Size256);
         base::debug::SetCrashKeyString(crash_key, gesture_sequence_);
-        base::debug::DumpWithoutCrashing();
         gesture_sequence_.clear();
       }
       allow_current_double_tap_event_ = (scrolling_touch_action_.value() &
@@ -182,13 +178,6 @@ FilterGestureEventResult TouchActionFilter::FilterGestureEvent(
       if (gesture_event->is_source_touch_event_set_non_blocking)
         SetTouchAction(cc::kTouchActionAuto);
       scrolling_touch_action_ = allowed_touch_action_;
-      if (!scrolling_touch_action_.has_value()) {
-        static auto* crash_key = base::debug::AllocateCrashKeyString(
-            "tapdown-gestures", base::debug::CrashKeySize::Size256);
-        base::debug::SetCrashKeyString(crash_key, gesture_sequence_);
-        base::debug::DumpWithoutCrashing();
-        gesture_sequence_.clear();
-      }
       DCHECK(!drop_current_tap_ending_event_);
       break;
 
