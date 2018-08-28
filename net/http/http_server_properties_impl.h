1fdcfdb80bf8036a7f2ff03f356abf7e2abf0d55
Zhongyi Shi <zhongyi@chromium.org>
2018-08-28 21:45:15 +0000

    Add API in HttpServerProperties to persist alternative service broken until the default network changes.
    Bug: 790547
    Change-Id: I2d784037a0ab316232ad51f2f5cfceaaf6dd8c44
    Reviewed-on: https://chromium-review.googlesource.com/1192109
    Commit-Queue: Zhongyi Shi <zhongyi@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586863}

--- net/http/http_server_properties_impl.h
+++ net/http/http_server_properties_impl.h
@@ -116,6 +116,8 @@ class NET_EXPORT HttpServerPropertiesImpl
                                   alternative_service_info_vector) override;
   void MarkAlternativeServiceBroken(
       const AlternativeService& alternative_service) override;
+  void MarkAlternativeServiceBrokenUntilDefaultNetworkChanges(
+      const AlternativeService& alternative_service) override;
   void MarkAlternativeServiceRecentlyBroken(
       const AlternativeService& alternative_service) override;
   bool IsAlternativeServiceBroken(
@@ -124,6 +126,7 @@ class NET_EXPORT HttpServerPropertiesImpl
       const AlternativeService& alternative_service) override;
   void ConfirmAlternativeService(
       const AlternativeService& alternative_service) override;
+  bool OnDefaultNetworkChanged() override;
   const AlternativeServiceMap& alternative_service_map() const override;
   std::unique_ptr<base::Value> GetAlternativeServiceInfoAsValue()
       const override;
