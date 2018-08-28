f7cc0080b72844a2f2d15119c49d7d99468563c1
Azeem Arshad <azeemarshad@chromium.org>
2018-08-28 02:52:05 +0000

    [AndroidSmsService] Add unittests and logging for ConnectionEstablisherImpl.
    Bug: 850823
    Change-Id: I303f5fdc52b78c5877ea73ac13bb34dfaa29f26d
    Reviewed-on: https://chromium-review.googlesource.com/1187823
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Azeem Arshad <azeemarshad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586556}

--- content/public/test/fake_service_worker_context.h
+++ content/public/test/fake_service_worker_context.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_PUBLIC_TEST_FAKE_SERVICE_WORKER_CONTEXT_H_
 
 #include <string>
+#include <tuple>
 
 #include "base/callback_forward.h"
 #include "base/observer_list.h"
@@ -24,6 +25,9 @@ class ServiceWorkerContextObserver;
 // what you need.
 class FakeServiceWorkerContext : public ServiceWorkerContext {
  public:
+  using StartServiceWorkerAndDispatchLongRunningMessageArgs =
+      std::tuple<GURL, blink::TransferableMessage, ResultCallback>;
+
   FakeServiceWorkerContext();
   ~FakeServiceWorkerContext() override;
 
@@ -52,6 +56,10 @@ class FakeServiceWorkerContext : public ServiceWorkerContext {
       const GURL& pattern,
       ServiceWorkerContext::StartWorkerCallback info_callback,
       base::OnceClosure failure_callback) override;
+  void StartServiceWorkerAndDispatchLongRunningMessage(
+      const GURL& pattern,
+      blink::TransferableMessage message,
+      FakeServiceWorkerContext::ResultCallback result_callback) override;
   void StartServiceWorkerForNavigationHint(
       const GURL& document_url,
       StartServiceWorkerForNavigationHintCallback callback) override;
@@ -67,13 +75,16 @@ class FakeServiceWorkerContext : public ServiceWorkerContext {
     return start_service_worker_for_navigation_hint_called_;
   }
 
-  void StartServiceWorkerAndDispatchLongRunningMessage(
-      const GURL& pattern,
-      blink::TransferableMessage message,
-      FakeServiceWorkerContext::ResultCallback result_callback) override;
+  std::vector<StartServiceWorkerAndDispatchLongRunningMessageArgs>&
+  start_service_worker_and_dispatch_long_running_message_calls() {
+    return start_service_worker_and_dispatch_long_running_message_calls_;
+  };
 
  private:
   bool start_service_worker_for_navigation_hint_called_ = false;
+
+  std::vector<StartServiceWorkerAndDispatchLongRunningMessageArgs>
+      start_service_worker_and_dispatch_long_running_message_calls_;
 
   base::ObserverList<ServiceWorkerContextObserver, true>::Unchecked observers_;
 
