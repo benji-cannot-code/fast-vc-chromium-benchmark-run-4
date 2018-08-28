9e17e333e27c94dc8f11be518e58072715fcd303
Dean Liao <deanliao@chromium.org>
2018-08-28 21:42:34 +0000

    media/base: Make VideoFrameLayout copyable.
    VideoFrameLayout stores only a few small vectors to describe
    video frame's physical layout. It does not hold resources
    expensive to copy.
    
    Making VFL a copyable object simplifies its use especially
    when it is a member of a class and the value cannot be
    initialized in constructor.
    
    BUG=110815424
    TEST=Run media_unittest
    
    Change-Id: If3530cf4b0401bf06f335650304d9c6191fd8575
    Reviewed-on: https://chromium-review.googlesource.com/1189312
    Commit-Queue: Shuo-Peng Liao <deanliao@google.com>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586861}

--- media/base/video_frame_layout.h
+++ media/base/video_frame_layout.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
-#include "base/macros.h"
 #include "media/base/media_export.h"
 #include "media/base/video_types.h"
 #include "ui/gfx/geometry/size.h"
@@ -25,6 +24,7 @@ namespace media {
 // which can be used to allocate buffer(s) hardware expected.
 // Also, it stores stride (bytes per line) per color plane to calculate each
 // color plane's size (note that a buffer may contains multiple color planes.)
+// Note that it is copyable.
 class MEDIA_EXPORT VideoFrameLayout {
  public:
   // Constructor with strides and buffers' size.
@@ -35,10 +35,11 @@ class MEDIA_EXPORT VideoFrameLayout {
                    std::vector<int32_t> strides = {0, 0, 0, 0},
                    std::vector<size_t> buffer_sizes = {0, 0, 0, 0});
 
-  // Move constructor.
-  VideoFrameLayout(VideoFrameLayout&&);
-
+  VideoFrameLayout();
   ~VideoFrameLayout();
+  VideoFrameLayout(const VideoFrameLayout&);
+  VideoFrameLayout(VideoFrameLayout&&);
+  VideoFrameLayout& operator=(const VideoFrameLayout&);
 
   VideoPixelFormat format() const { return format_; }
   const gfx::Size& coded_size() const { return coded_size_; }
@@ -57,9 +58,6 @@ class MEDIA_EXPORT VideoFrameLayout {
     strides_ = std::move(strides);
   }
 
-  // Clones this as a explicitly copy constructor.
-  VideoFrameLayout Clone() const;
-
   // Returns sum of bytes of all buffers.
   size_t GetTotalBufferSize() const;
 
@@ -67,14 +65,14 @@ class MEDIA_EXPORT VideoFrameLayout {
   std::string ToString() const;
 
  private:
-  const VideoPixelFormat format_;
+  VideoPixelFormat format_;
 
   // Width and height of the video frame in pixels. This must include pixel
   // data for the whole image; i.e. for YUV formats with subsampled chroma
   // planes, in the case that the visible portion of the image does not line up
   // on a sample boundary, |coded_size_| must be rounded up appropriately and
   // the pixel data provided for the odd pixels.
-  const gfx::Size coded_size_;
+  gfx::Size coded_size_;
 
   // Vector of strides for each buffer, typically greater or equal to the
   // width of the surface divided by the horizontal sampling period. Note that
@@ -84,8 +82,6 @@ class MEDIA_EXPORT VideoFrameLayout {
   // Vector of sizes for each buffer, typically greater or equal to the area of
   // |coded_size_|.
   std::vector<size_t> buffer_sizes_;
-
-  DISALLOW_COPY_AND_ASSIGN(VideoFrameLayout);
 };
 
 }  // namespace media
