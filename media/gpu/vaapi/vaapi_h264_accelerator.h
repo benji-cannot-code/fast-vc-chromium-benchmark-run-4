c6c14cedfa9330bb2fbfecc95c85d77c7063ae7d
Miguel Casas <mcasas@chromium.org>
2018-08-28 13:58:10 +0000

    VaVDA: extract surface lifetime into an interface
    This CL extracts CreateSurface() and SurfaceReady() into a new
    interface: DecodeSurfaceHandler.  This extraction reduces the
    coupling surface (always good) and allows e.g. easier testing in
    subsequent CLs.
    
    VaapiVideoDecodeAccelerator is made to implement the said
    templatised interface and teaches the Vaapi*Accelerator classes
    to use it (instead of the whole VaVDA).
    
    Partial diagram before: https://goo.gl/LBxH3h, after: https://goo.gl/E6sDrc.
    
    Test: v_d_a_unittests and simplechrome on nautilus
    Bug: 875005
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: Id376a6a25e664b0b6669b202062ebd7179346c50
    Reviewed-on: https://chromium-review.googlesource.com/1178340
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586680}

--- media/gpu/vaapi/vaapi_h264_accelerator.h
+++ media/gpu/vaapi/vaapi_h264_accelerator.h
@@ -6,21 +6,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MEDIA_GPU_VAAPI_VAAPI_H264_ACCELERATOR_H_
 #define MEDIA_GPU_VAAPI_VAAPI_H264_ACCELERATOR_H_
 
-#include <va/va.h>
-
 #include "base/sequence_checker.h"
 #include "media/filters/vp9_parser.h"
 #include "media/gpu/h264_decoder.h"
 
+// Verbatim from va/va.h, where typedef is used.
+typedef struct _VAPictureH264 VAPictureH264;
+
 namespace media {
 
+template <class T> class DecodeSurfaceHandler;
 class H264Picture;
-class VaapiVideoDecodeAccelerator;
+class VASurface;
 class VaapiWrapper;
 
 class VaapiH264Accelerator : public H264Decoder::H264Accelerator {
  public:
-  VaapiH264Accelerator(VaapiVideoDecodeAccelerator* vaapi_dec,
+  VaapiH264Accelerator(DecodeSurfaceHandler<VASurface>* vaapi_dec,
                        const scoped_refptr<VaapiWrapper> vaapi_wrapper);
   ~VaapiH264Accelerator() override;
 
@@ -52,7 +54,7 @@ class VaapiH264Accelerator : public H264Decoder::H264Accelerator {
                              int num_pics);
 
   const scoped_refptr<VaapiWrapper> vaapi_wrapper_;
-  VaapiVideoDecodeAccelerator* vaapi_dec_;
+  DecodeSurfaceHandler<VASurface>* vaapi_dec_;
 
   SEQUENCE_CHECKER(sequence_checker_);
 
