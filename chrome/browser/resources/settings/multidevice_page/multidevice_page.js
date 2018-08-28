d0f23c4932c5240d04368d98bac1b4f983259586
Jordy Greenblatt <jordynass@chromium.org>
2018-08-28 02:07:16 +0000

    [CrOS MultiDevice] Add Suite (i.e. Phone) SVG to main Settings page.
    Screenshots for all the modes that currently show the page can be found
    in the folder
    https://drive.google.com/open?id=1aJc6zbxJW7ZAoDVfPdVRgLo_LSkSb2Lp
    
    Change-Id: Ibfe5ba4d906fa439bec0d1d0e48d36bb225cdcfe
    Bug: 876925
    Reviewed-on: https://chromium-review.googlesource.com/1191666
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Commit-Queue: Jordy Greenblatt <jordynass@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586544}

--- chrome/browser/resources/settings/multidevice_page/multidevice_page.js
+++ chrome/browser/resources/settings/multidevice_page/multidevice_page.js
@@ -69,6 +69,17 @@ Polymer({
   },
 
   /**
+   * CSS class for the <div> containing all the text in the multidevice-item
+   * <div>, i.e. the label and sublabel. If the host is set, the Better Together
+   * icon appears so before the text (i.e. text div is 'middle' class).
+   * @return {string}
+   * @private
+   */
+  getMultiDeviceItemLabelBlockCssClass_: function() {
+    return this.isHostSet ? 'middle' : 'start';
+  },
+
+  /**
    * @return {string} Translated item label.
    * @private
    */
@@ -116,8 +127,8 @@ Polymer({
    * @return {boolean}
    * @private
    */
-  showButton_: function() {
-    return this.pageContentData.mode !=
+  shouldShowButton_: function() {
+    return this.pageContentData.mode !==
         settings.MultiDeviceSettingsMode.HOST_SET_VERIFIED;
   },
 
@@ -125,26 +136,14 @@ Polymer({
    * @return {boolean}
    * @private
    */
-  showToggle_: function() {
-    return this.pageContentData.mode ==
+  shouldShowToggle_: function() {
+    return this.pageContentData.mode ===
         settings.MultiDeviceSettingsMode.HOST_SET_VERIFIED;
-  },
-
-  /**
-   * @return {boolean}
-   * @private
-   */
-  doesClickOpenSubpage_: function() {
-    return [
-      settings.MultiDeviceSettingsMode.HOST_SET_WAITING_FOR_SERVER,
-      settings.MultiDeviceSettingsMode.HOST_SET_WAITING_FOR_VERIFICATION,
-      settings.MultiDeviceSettingsMode.HOST_SET_VERIFIED,
-    ].includes(this.pageContentData.mode);
   },
 
   /** @private */
   handleItemClick_: function() {
-    if (!this.doesClickOpenSubpage_())
+    if (!this.isHostSet)
       return;
     settings.navigateTo(settings.routes.MULTIDEVICE_FEATURES);
   },
