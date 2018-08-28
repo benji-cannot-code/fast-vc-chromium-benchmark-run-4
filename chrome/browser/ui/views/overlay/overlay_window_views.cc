89f2ef3f7ff5134fc578af6359fb083ce4dafbc3
Jennifer Apacible <apacible@chromium.org>
2018-08-28 19:52:57 +0000

    [Picture in Picture] Call parent function in OnGestureEvent.
    Change-Id: I854654be22abd217c3f8ed557bc3fb9118c557c6
    Reviewed-on: https://chromium-review.googlesource.com/1192326
    Reviewed-by: CJ DiMeglio <lethalantidote@chromium.org>
    Commit-Queue: apacible <apacible@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586820}

--- chrome/browser/ui/views/overlay/overlay_window_views.cc
+++ chrome/browser/ui/views/overlay/overlay_window_views.cc
@@ -644,6 +644,8 @@ void OverlayWindowViews::OnGestureEvent(ui::GestureEvent* event) {
     TogglePlayPause();
     event->SetHandled();
   }
+
+  views::Widget::OnGestureEvent(event);
 }
 
 void OverlayWindowViews::ButtonPressed(views::Button* sender,
