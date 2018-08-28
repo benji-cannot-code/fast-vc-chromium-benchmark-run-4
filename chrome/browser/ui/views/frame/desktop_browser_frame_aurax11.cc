f6d6cf07b851b65c65c72b58d2c76877b1a85522
Matt Giuca <mgiuca@chromium.org>
2018-08-28 01:47:47 +0000

    Enable custom window frame for PWAs and bookmark apps on Linux.
    Bug: 877861
    Change-Id: I021faa5014e83c2a7ec992e59f22974fd9fdc51c
    Reviewed-on: https://chromium-review.googlesource.com/1189685
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Matt Giuca <mgiuca@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586537}

--- chrome/browser/ui/views/frame/desktop_browser_frame_aurax11.cc
+++ chrome/browser/ui/views/frame/desktop_browser_frame_aurax11.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "chrome/browser/shell_integration_linux.h"
+#include "chrome/browser/ui/extensions/hosted_app_browser_controller.h"
 #include "chrome/browser/ui/views/frame/browser_frame.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/common/chrome_switches.h"
@@ -54,8 +55,20 @@ views::Widget::InitParams DesktopBrowserFrameAuraX11::GetWidgetParams() {
 }
 
 bool DesktopBrowserFrameAuraX11::UseCustomFrame() const {
-  return use_custom_frame_pref_.GetValue() &&
-      browser_view()->IsBrowserTypeNormal();
+  // Normal browser windows get a custom frame (per the user's preference).
+  if (use_custom_frame_pref_.GetValue() &&
+      browser_view()->IsBrowserTypeNormal()) {
+    return true;
+  }
+
+  // Hosted app windows get a custom frame (if the desktop PWA experimental
+  // feature is enabled).
+  if (extensions::HostedAppBrowserController::IsForExperimentalHostedAppBrowser(
+          browser_view()->browser())) {
+    return true;
+  }
+
+  return false;
 }
 
 void DesktopBrowserFrameAuraX11::OnUseCustomChromeFrameChanged() {
