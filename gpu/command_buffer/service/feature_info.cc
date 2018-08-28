11119fb49b9caefaf7ec6267b4ce620d1631bf49
Kai Ninomiya <kainino@chromium.org>
2018-08-28 18:02:10 +0000

    Command buffer changes needed for WebGPU in Blink
    A few more changes, following up to the previous commit which added the WebGPU command buffer.
    
    Bug: 868192
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: Iade27669c2bdebb598463b579a8dfaa6b91fbb9b
    Reviewed-on: https://chromium-review.googlesource.com/1192411
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Kai Ninomiya <kainino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586784}

--- gpu/command_buffer/service/feature_info.cc
+++ gpu/command_buffer/service/feature_info.cc
@@ -1828,10 +1828,6 @@ void FeatureInfo::EnableES3Validators() {
   }
 }
 
-bool FeatureInfo::IsGLContext() const {
-  return IsGLContextType(context_type_);
-}
-
 bool FeatureInfo::IsWebGLContext() const {
   return IsWebGLContextType(context_type_);
 }
@@ -1850,10 +1846,6 @@ bool FeatureInfo::IsWebGL2OrES3OrHigherContext() const {
 
 bool FeatureInfo::IsWebGL2ComputeContext() const {
   return IsWebGL2ComputeContextType(context_type_);
-}
-
-bool FeatureInfo::IsWebGPUContext() const {
-  return IsWebGPUContextType(context_type_);
 }
 
 void FeatureInfo::AddExtensionString(const base::StringPiece& extension) {
