7825bc009678d81712cd7fe65eb2c0a2e43a2559
Lei Zhang <thestig@chromium.org>
2018-08-28 19:24:12 +0000

    Remove static initializer in user_removal_manager code.
    BUG=537099
    
    Change-Id: I630acaa06e52222392dcfeae0c06bcc27236bbc0
    Reviewed-on: https://chromium-review.googlesource.com/1188637
    Reviewed-by: Ivan Šandrk <isandrk@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586812}

--- chrome/browser/chromeos/system/user_removal_manager.cc
+++ chrome/browser/chromeos/system/user_removal_manager.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/callback.h"
 #include "base/location.h"
+#include "base/no_destructor.h"
 #include "base/task_runner.h"
 #include "base/threading/sequenced_task_runner_handle.h"
 #include "base/time/time.h"
@@ -32,7 +33,10 @@ constexpr base::TimeDelta kFailsafeTimerTimeout =
     base::TimeDelta::FromSeconds(60);
 
 // Override for the LogOut function inside of tests.
-base::OnceClosure g_log_out_override_callback;
+base::OnceClosure& GetLogOutOverrideCallbackForTest() {
+  static base::NoDestructor<base::OnceClosure> callback;
+  return *callback;
+}
 
 }  // namespace
 
@@ -57,15 +61,17 @@ bool RemoveUsersIfNeeded() {
 }
 
 void LogOut() {
-  if (!g_log_out_override_callback.is_null()) {
-    std::move(g_log_out_override_callback).Run();
+  auto& log_out_override_callback = GetLogOutOverrideCallbackForTest();
+  if (log_out_override_callback) {
+    std::move(log_out_override_callback).Run();
     return;
   }
   chrome::AttemptUserExit();
 }
 
 void OverrideLogOutForTesting(base::OnceClosure callback) {
-  g_log_out_override_callback = std::move(callback);
+  auto& log_out_override_callback = GetLogOutOverrideCallbackForTest();
+  log_out_override_callback = std::move(callback);
 }
 
 void InitiateUserRemoval(base::OnceClosure on_pref_persisted_callback) {
@@ -78,7 +84,7 @@ void InitiateUserRemoval(base::OnceClosure on_pref_persisted_callback) {
         base::SequencedTaskRunnerHandle::Get()->PostDelayedTask(
             FROM_HERE, base::BindOnce(&LogOut), kFailsafeTimerTimeout);
 
-        if (!on_pref_persisted_callback.is_null())
+        if (on_pref_persisted_callback)
           std::move(on_pref_persisted_callback).Run();
       },
       std::move(on_pref_persisted_callback)));
