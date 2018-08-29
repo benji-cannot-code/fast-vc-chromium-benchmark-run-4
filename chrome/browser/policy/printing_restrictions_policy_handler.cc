71c0e3b42518d33be95443365e8da1ba1e982364
Vladislav Kuzkokov <vkuzkokov@chromium.org>
2018-08-29 22:12:20 +0000

    Clean up printing restriction policy handlers.
    Bug: 853877
    Change-Id: Ida1f8e9c1e88d91e6867d461f364e7f5fe298c63
    Reviewed-on: https://chromium-review.googlesource.com/1194079
    Commit-Queue: Vladislav Kuzkokov <vkuzkokov@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587315}

--- chrome/browser/policy/printing_restrictions_policy_handler.cc
+++ chrome/browser/policy/printing_restrictions_policy_handler.cc
@@ -49,8 +49,8 @@ void PrintingAllowedColorModesPolicyHandler::ApplyPolicySettings(
     PrefValueMap* prefs) {
   printing::ColorModeRestriction value;
   if (GetValue(policies, nullptr, &value)) {
-    prefs->SetValue(prefs::kPrintingAllowedColorModes,
-                    std::make_unique<base::Value>(static_cast<int>(value)));
+    prefs->SetInteger(prefs::kPrintingAllowedColorModes,
+                      static_cast<int>(value));
   }
 }
 
@@ -91,8 +91,8 @@ void PrintingAllowedDuplexModesPolicyHandler::ApplyPolicySettings(
     PrefValueMap* prefs) {
   printing::DuplexModeRestriction value;
   if (GetValue(policies, nullptr, &value)) {
-    prefs->SetValue(prefs::kPrintingAllowedDuplexModes,
-                    std::make_unique<base::Value>(static_cast<int>(value)));
+    prefs->SetInteger(prefs::kPrintingAllowedDuplexModes,
+                      static_cast<int>(value));
   }
 }
 
@@ -152,10 +152,8 @@ void PrintingColorDefaultPolicyHandler::ApplyPolicySettings(
     const PolicyMap& policies,
     PrefValueMap* prefs) {
   printing::ColorModeRestriction value;
-  if (GetValue(policies, nullptr, &value)) {
-    prefs->SetValue(prefs::kPrintingColorDefault,
-                    std::make_unique<base::Value>(static_cast<int>(value)));
-  }
+  if (GetValue(policies, nullptr, &value))
+    prefs->SetInteger(prefs::kPrintingColorDefault, static_cast<int>(value));
 }
 
 PrintingDuplexDefaultPolicyHandler::PrintingDuplexDefaultPolicyHandler()
@@ -192,10 +190,8 @@ void PrintingDuplexDefaultPolicyHandler::ApplyPolicySettings(
     const PolicyMap& policies,
     PrefValueMap* prefs) {
   printing::DuplexModeRestriction value;
-  if (GetValue(policies, nullptr, &value)) {
-    prefs->SetValue(prefs::kPrintingDuplexDefault,
-                    std::make_unique<base::Value>(static_cast<int>(value)));
-  }
+  if (GetValue(policies, nullptr, &value))
+    prefs->SetInteger(prefs::kPrintingDuplexDefault, static_cast<int>(value));
 }
 
 PrintingSizeDefaultPolicyHandler::PrintingSizeDefaultPolicyHandler()
