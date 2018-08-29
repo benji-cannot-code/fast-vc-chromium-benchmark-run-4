6c51eef65cafc096deebbf6e62e059f9fb9963d2
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2018-08-29 15:52:46 +0000

    Add policy migrator for Chrome Reporting Extension
    This ExtensionPolicyMigrator uses the CRE's existing policies as a
    fallback. That way, existing setups that set those policies will be more
    compatible with the new version of Enterprise Reporting.
    
    Bug: 877120
    Change-Id: I6e8b8e46450ea8e3ed8e073e7b3a8c545864f0e3
    Reviewed-on: https://chromium-review.googlesource.com/1191743
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587131}

---
+++ chrome/browser/extensions/api/enterprise_reporting_private/enterprise_reporting_policy_migrator.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_EXTENSIONS_API_ENTERPRISE_REPORTING_PRIVATE_ENTERPRISE_REPORTING_POLICY_MIGRATOR_H_
+#define CHROME_BROWSER_EXTENSIONS_API_ENTERPRISE_REPORTING_PRIVATE_ENTERPRISE_REPORTING_POLICY_MIGRATOR_H_
+
+#include "components/policy/core/common/extension_policy_migrator.h"
+
+namespace extensions {
+namespace enterprise_reporting {
+
+class EnterpriseReportingPolicyMigrator
+    : public policy::ExtensionPolicyMigrator {
+ public:
+  EnterpriseReportingPolicyMigrator();
+  ~EnterpriseReportingPolicyMigrator() override;
+
+  void Migrate(policy::PolicyBundle* bundle) override;
+};
+
+}  // namespace enterprise_reporting
+}  // namespace extensions
+
+#endif  // CHROME_BROWSER_EXTENSIONS_API_ENTERPRISE_REPORTING_PRIVATE_ENTERPRISE_REPORTING_POLICY_MIGRATOR_H_
