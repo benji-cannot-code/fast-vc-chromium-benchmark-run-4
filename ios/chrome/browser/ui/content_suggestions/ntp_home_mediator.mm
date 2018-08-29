85afb3e7775500949c56f4e02156a6aaf6252d73
sczs <sczs@chromium.org>
2018-08-29 17:20:58 +0000

    [ios] Updates support URLs
    Bug: 863927
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;luci.chromium.try:ios-simulator-cronet
    Change-Id: I54338519ca436c176356bb9f7f50cf1f648a316f
    Reviewed-on: https://chromium-review.googlesource.com/1180432
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Peter Lee <pkl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587172}

--- ios/chrome/browser/ui/content_suggestions/ntp_home_mediator.mm
+++ ios/chrome/browser/ui/content_suggestions/ntp_home_mediator.mm
@@ -57,7 +57,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 // URL for the page displaying help for the NTP.
-const char kNTPHelpURL[] = "https://support.google.com/chrome/?p=ios_new_tab";
+const char kNTPHelpURL[] =
+    "https://support.google.com/chrome/?p=ios_new_tab&ios=1";
 
 // The What's New promo command that shows the Bookmarks Manager.
 const char kBookmarkCommand[] = "bookmark";
