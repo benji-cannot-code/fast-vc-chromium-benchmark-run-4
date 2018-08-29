143322995be23ee6f1f72effd255bcc488df3962
Will Harris <wfh@chromium.org>
2018-08-29 22:58:15 +0000

    Change kUpdateThumbnailTime to be a TimeDelta.
    Fixes a future warning with -Wshorten-64-to-32.
    
    ../../components/history/core/common/thumbnail_score_unittest.cc(86,33):  warning: implicit conversion loses integer precision: 'const int64_t' (aka 'const long long') to 'int' [-Wshorten-64-to-32]
          base::TimeDelta::FromDays(ThumbnailScore::kUpdateThumbnailTimeDays);
          ~~~~                      ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    Also, remove an unused definition and some minor cleanup.
    
    BUG=588506
    
    Change-Id: I447c758454d0868cf1d023772c05464bc6e40262
    Reviewed-on: https://chromium-review.googlesource.com/1195656
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587350}

--- components/history/core/common/thumbnail_score.cc
+++ components/history/core/common/thumbnail_score.cc
@@ -11,9 +11,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using base::Time;
 using base::TimeDelta;
 
-const int64_t ThumbnailScore::kUpdateThumbnailTimeDays = 1;
+// static
 const double ThumbnailScore::kThumbnailMaximumBoringness = 0.94;
+// static
+const TimeDelta ThumbnailScore::kUpdateThumbnailTime = TimeDelta::FromDays(1);
+// static
 const double ThumbnailScore::kThumbnailDegradePerHour = 0.01;
+// static
 const double ThumbnailScore::kTooWideAspectRatio = 2.0;
 
 // Calculates a numeric score from traits about where a snapshot was
@@ -131,8 +135,8 @@ bool ShouldReplaceThumbnailWith(const ThumbnailScore& current,
 
 bool ThumbnailScore::ShouldConsiderUpdating() {
   const TimeDelta time_elapsed = Time::Now() - time_at_snapshot;
-  if (time_elapsed < TimeDelta::FromDays(kUpdateThumbnailTimeDays) &&
-      good_clipping && at_top && load_completed) {
+  if (time_elapsed < kUpdateThumbnailTime && good_clipping && at_top &&
+      load_completed) {
     // The current thumbnail is new and has good properties.
     return false;
   }
