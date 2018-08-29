3c7f943f6847b6be85f36002dcf1caa2d61b51c5
Etienne Pierre-doray <etiennep@chromium.org>
2018-08-29 14:37:46 +0000

    [TaskScheduler]: Use ScopedBlockingCall to mark blocking tasks.
    This CL uses ScopedBlockingCall to mark blocking calls in /chrome/browser/metrics.
    
    This CL was created by replacing calls to AssertBlockingAllowed()
    with instantiations of ScopedBlockingCall(MAY_BLOCK).
    I kindly ask the reviewer to make sure of the following:
      - ScopedBlockingCall is instantiated in a scope with minimal CPU usage.
        If this is not the case, ScopedBlockingCall should be instantiated
        closer to the blocking call. See scoped_blocking_call.h for more
        info. Please let me know when/where the blocking call happens if this needs
        to be changed.
      - Parameter |blocking_type| matches expectation (MAY_BLOCK/WILL_BLOCK). See
        BlockingType for more info. While I assumed MAY_BLOCK by default, that might
        not be the best fit if we know that this callsite is guaranteed to block.
      - The ScopedBlockingCall's scope covers the entirety of the blocking operation
        previously asserted against by the AssertBlockingAllowed().
    
    This CL was uploaded by git cl split.
    
    R=rkaplow@chromium.org
    
    Bug: 874080
    Change-Id: I002dbf2568e14095e79910d01499623fa2414976
    Reviewed-on: https://chromium-review.googlesource.com/1191229
    Reviewed-by: Robert Kaplow (slow) <rkaplow@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587108}

--- chrome/browser/metrics/antivirus_metrics_provider_win.cc
+++ chrome/browser/metrics/antivirus_metrics_provider_win.cc
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/sys_string_conversions.h"
 #include "base/task/post_task.h"
 #include "base/task_runner_util.h"
-#include "base/threading/thread_restrictions.h"
+#include "base/threading/scoped_blocking_call.h"
 #include "base/version.h"
 #include "base/win/com_init_util.h"
 #include "base/win/scoped_bstr.h"
@@ -242,7 +242,7 @@ AntiVirusMetricsProvider::ResultCode
 AntiVirusMetricsProvider::FillAntiVirusProductsFromWSC(
     std::vector<AvProduct>* products) {
   std::vector<AvProduct> result_list;
-  base::AssertBlockingAllowed();
+  base::ScopedBlockingCall scoped_blocking_call(base::BlockingType::MAY_BLOCK);
 
   Microsoft::WRL::ComPtr<IWSCProductList> product_list;
   HRESULT result =
@@ -340,7 +340,7 @@ AntiVirusMetricsProvider::ResultCode
 AntiVirusMetricsProvider::FillAntiVirusProductsFromWMI(
     std::vector<AvProduct>* products) {
   std::vector<AvProduct> result_list;
-  base::AssertBlockingAllowed();
+  base::ScopedBlockingCall scoped_blocking_call(base::BlockingType::MAY_BLOCK);
 
   Microsoft::WRL::ComPtr<IWbemLocator> wmi_locator;
   HRESULT hr =
@@ -474,7 +474,7 @@ AntiVirusMetricsProvider::FillAntiVirusProductsFromWMI(
 
 void AntiVirusMetricsProvider::MaybeAddUnregisteredAntiVirusProducts(
     std::vector<AvProduct>* products) {
-  base::AssertBlockingAllowed();
+  base::ScopedBlockingCall scoped_blocking_call(base::BlockingType::MAY_BLOCK);
 
   // Trusteer Rapport does not register with WMI or Security Center so do some
   // "best efforts" detection here.
