874d254c301df6ce88fd07a026df40206d8af1f6
Donn Denman <donnd@chromium.org>
2018-08-29 00:45:20 +0000

    [TTS] Fix NPE in shouldPreviousTapResolve.
    Fixes a recently introduced NPE in shouldPreviousTapResolve.
    Evidently in some tests initialization may happen late.
    
    This change just protects checking the new Preference Helper when
    not yet initialized.
    
    BUG=878512
    
    Change-Id: Id2917f693eb47974fe4d851dfa7b3672b42c023f
    Reviewed-on: https://chromium-review.googlesource.com/1194822
    Commit-Queue: Donn Denman <donnd@chromium.org>
    Reviewed-by: Troy Hildebrandt <thildebr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586969}

--- chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchPolicy.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchPolicy.java
@@ -146,7 +146,9 @@ class ContextualSearchPolicy {
             return false;
         }
 
-        return isPromoAvailable() || mContextualSearchPreferenceHelper.canThrottle()
+        return (isPromoAvailable()
+                       || (mContextualSearchPreferenceHelper != null
+                                  && mContextualSearchPreferenceHelper.canThrottle()))
                 ? isBasePageHTTP(mNetworkCommunicator.getBasePageUrl())
                 : true;
     }
