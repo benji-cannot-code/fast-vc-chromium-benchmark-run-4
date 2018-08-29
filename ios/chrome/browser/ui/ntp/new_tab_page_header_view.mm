ee3880a12115ab06511e73570f46527f9991834e
Justin Cohen <justincohen@google.com>
2018-08-29 16:26:51 +0000

    Re-land "[ios] Add highlight animation state to fake omnibox."
    Now using KVO instead of a long press gesture recognizer to
    pass around the highlight state.
    
    Bug: 865834
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: I8562bd22aeebdd80f6db464e5ddb40e52506bd0b
    Reviewed-on: https://chromium-review.googlesource.com/1195542
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587147}

--- ios/chrome/browser/ui/ntp/new_tab_page_header_view.mm
+++ ios/chrome/browser/ui/ntp/new_tab_page_header_view.mm
@@ -109,6 +109,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return 0;
 }
 
+- (void)setFakeboxHighlighted:(BOOL)highlighted {
+  // Not implemented pre-ui-refresh.
+}
+
 - (void)updateSearchFieldWidth:(NSLayoutConstraint*)widthConstraint
                         height:(NSLayoutConstraint*)heightConstraint
                      topMargin:(NSLayoutConstraint*)topMarginConstraint
