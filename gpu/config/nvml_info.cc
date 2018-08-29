15a7264a49806e72bc7e3b9b1488b172db1313cc
Olli Etuaho <oetuaho@nvidia.com>
2018-08-29 08:34:20 +0000

    Re-enable NVML with a DLL version check
    NVML is re-enabled only on drivers that are known to not have a NVML
    crash issue (version >= 348).
    
    BUG=873095
    TEST=Run Chrome on a machine with NVIDIA 341.74 drivers
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: Ie188a0de1c4618c7e1d0ace054742d692ec19eeb
    Reviewed-on: https://chromium-review.googlesource.com/1180972
    Commit-Queue: Olli Etuaho <oetuaho@nvidia.com>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587041}

--- gpu/config/nvml_info.cc
+++ gpu/config/nvml_info.cc
@@ -7,15 +7,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <windows.h>
 
+#include "base/file_version_info_win.h"
 #include "base/files/file_path.h"
 #include "base/logging.h"
 #include "base/path_service.h"
+#include "base/version.h"
 #include "third_party/nvml/nvml.h"
 
 namespace {
 
-// TODO(crbug.com/873095): diagnose crashes inside nvml.dll
-// const unsigned int kDriverVersionCapacity = 80u;
+const unsigned int kDriverVersionCapacity = 80u;
 
 }  // anonymous namespace
 
@@ -37,14 +38,34 @@ bool GetNvmlDeviceInfo(uint32_t pci_device_id,
   *major_cuda_compute_capability = 0;
   *minor_cuda_compute_capability = 0;
 
-  // TODO(crbug.com/873095): diagnose crashes inside nvml.dll
-  return false;
-#if 0
   base::FilePath dll_path;
   if (!base::PathService::Get(base::DIR_PROGRAM_FILES6432, &dll_path)) {
     return false;
   }
   dll_path = dll_path.Append(L"NVIDIA Corporation\\NVSMI\\nvml.dll");
+
+  std::unique_ptr<FileVersionInfoWin> file_version_info(
+      static_cast<FileVersionInfoWin*>(
+          FileVersionInfoWin::CreateFileVersionInfo(dll_path)));
+
+  if (!file_version_info) {
+    return false;
+  }
+
+  std::vector<uint32_t> components = {
+      {HIWORD(file_version_info->fixed_file_info()->dwFileVersionMS),
+       LOWORD(file_version_info->fixed_file_info()->dwFileVersionMS),
+       HIWORD(file_version_info->fixed_file_info()->dwFileVersionLS),
+       LOWORD(file_version_info->fixed_file_info()->dwFileVersionLS)}};
+  base::Version dll_version(components);
+
+  // There was a bug that could cause crashes in 64-bit applications in earlier
+  // versions of NVML, so we only use it if a new enough DLL is detected.
+  // http://crbug.com/873095
+  if (dll_version < base::Version("8.17.13.4800")) {
+    return false;
+  }
+
   HINSTANCE hinstNVML = LoadLibrary(dll_path.value().data());
 
   if (!hinstNVML) {
@@ -128,5 +149,4 @@ bool GetNvmlDeviceInfo(uint32_t pci_device_id,
     return false;
   }
   return true;
-#endif
 }
