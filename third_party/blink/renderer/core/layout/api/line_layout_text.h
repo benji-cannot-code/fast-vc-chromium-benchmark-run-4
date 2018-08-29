9eac403cd8f6f378e5deca784bcca47ae5329406
Yoichi Osato <yoichio@chromium.org>
2018-08-29 12:28:15 +0000

    [LayoutNG] Unify selection start/end API into LayoutTextSelectionStatus.
    |optional<int> FrameSelection::LayoutSelectionStart()/End()| is confusing API
    because it returns the text offset where selection starts/end and the text
    node could be different like |f^oo<br>ba|r| but the fact that selection
    can wrap two or more nodes from the API is hard to catch and the API looked
    rather "selection start/end offset on a node", which is completely wrong.
    Plus, to get collect information, you should use with SelectionState.
    We have such code here and there.
    
    This patch unifies the API into
    |LayoutTextSelectionStatus ComputeLayoutSelectionStatus(
        const LayoutText& text)|
    so that users can get selected offset pair on the text w/o any confusion.
    
    Bug: 789870
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng
    Change-Id: Ib11cd358cc76d4464625a493803ea92c7032604e
    Reviewed-on: https://chromium-review.googlesource.com/1179518
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587085}

--- third_party/blink/renderer/core/layout/api/line_layout_text.h
+++ third_party/blink/renderer/core/layout/api/line_layout_text.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_API_LINE_LAYOUT_TEXT_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_API_LINE_LAYOUT_TEXT_H_
 
+#include "third_party/blink/renderer/core/editing/frame_selection.h"
+#include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/layout/api/line_layout_item.h"
 #include "third_party/blink/renderer/core/layout/layout_text.h"
 #include "third_party/blink/renderer/platform/layout_unit.h"
@@ -121,6 +123,11 @@ class LineLayoutText : public LineLayoutItem {
   float MinLogicalWidth() const { return ToText()->MinLogicalWidth(); }
 
   UChar PreviousCharacter() const { return ToText()->PreviousCharacter(); }
+
+  struct LayoutTextSelectionStatus SelectionStatus() const {
+    return ToText()->GetFrame()->Selection().ComputeLayoutSelectionStatus(
+        *ToText());
+  }
 
  private:
   LayoutText* ToText() { return ToLayoutText(GetLayoutObject()); }
