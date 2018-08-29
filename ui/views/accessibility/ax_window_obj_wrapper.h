7bbf8e2feaffe251882f2a3d4c4a022c3a2ceab4
David Tseng <dtseng@chromium.org>
2018-08-29 19:21:49 +0000

    Ensure accessibility updates get sent when a window is transformed
    When an aura Window receives a new transform, it possibly impacts its bounds. Previously, we were only observing OnWindowBoundsChanged, which does not get called for transform changes.
    
    This change makes it so we also observe OnWindowTransformed and fire a location change to push the new location to all accessibility clients.
    
    Bug: 842868
    Change-Id: I5251198da8b4af6cb96ad3197a6d37caa0a37faa
    Reviewed-on: https://chromium-review.googlesource.com/1195645
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587237}

--- ui/views/accessibility/ax_window_obj_wrapper.h
+++ ui/views/accessibility/ax_window_obj_wrapper.h
@@ -51,6 +51,8 @@ class AXWindowObjWrapper : public AXAuraObjWrapper,
                                const void* key,
                                intptr_t old) override;
   void OnWindowVisibilityChanged(aura::Window* window, bool visible) override;
+  void OnWindowTransformed(aura::Window* window,
+                           ui::PropertyChangeReason reason) override;
 
  private:
   aura::Window* window_;
