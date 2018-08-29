e528784b7d9e4dd1bfc55a5711bfef5f92bed204
Anthony Vallee-Dubois <anthonyvd@chromium.org>
2018-08-29 17:32:14 +0000

    Log the backoff state for the triggering experiment in UMA
    Bug: 840367
    Change-Id: I34fc6971149872c00ef5c79c91857311d1341e24
    Reviewed-on: https://chromium-review.googlesource.com/1178304
    Reviewed-by: Robert Kaplow (slow) <rkaplow@chromium.org>
    Reviewed-by: Michael Martis <martis@chromium.org>
    Commit-Queue: anthonyvd <anthonyvd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587177}

--- components/language/core/common/language_experiments.cc
+++ components/language/core/common/language_experiments.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/metrics/field_trial_params.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/strings/string_number_conversions.h"
 
 namespace language {
@@ -45,13 +46,21 @@ OverrideLanguageModel GetOverrideLanguageModel() {
 }
 
 bool ShouldForceTriggerTranslateOnEnglishPages(int force_trigger_count) {
-  return base::FeatureList::IsEnabled(kOverrideTranslateTriggerInIndia) &&
-         !IsForceTriggerBackoffThresholdReached(force_trigger_count);
+  if (!base::FeatureList::IsEnabled(kOverrideTranslateTriggerInIndia))
+    return false;
+
+  bool threshold_reached =
+      IsForceTriggerBackoffThresholdReached(force_trigger_count);
+  UMA_HISTOGRAM_BOOLEAN("Translate.ForceTriggerBackoffStateReached",
+                        threshold_reached);
+
+  return !threshold_reached;
 }
 
 bool ShouldPreventRankerEnforcementInIndia(int force_trigger_count) {
   std::map<std::string, std::string> params;
-  return ShouldForceTriggerTranslateOnEnglishPages(force_trigger_count) &&
+  return base::FeatureList::IsEnabled(kOverrideTranslateTriggerInIndia) &&
+         !IsForceTriggerBackoffThresholdReached(force_trigger_count) &&
          base::GetFieldTrialParamsByFeature(kOverrideTranslateTriggerInIndia,
                                             &params) &&
          params[kEnforceRankerKey] == "false";
