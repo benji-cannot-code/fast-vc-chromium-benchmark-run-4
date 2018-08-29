55baaff9e8887ca1a2e90e83ab60ee3c8abed96d
Jialiu Lin <jialiul@chromium.org>
2018-08-29 18:09:06 +0000

    Add user's advanced protection status to SB pings
    Add user's advanced protection status to download pings and password
    protection pings.
    
    Server side proto change already landed in cl/209451527
    
    
    Bug: 866620
    Change-Id: I5e0d6034bad77d512bc91ed919f90600658d0bd8
    Reviewed-on: https://chromium-review.googlesource.com/1192382
    Commit-Queue: Jialiu Lin <jialiul@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587193}

--- chrome/browser/safe_browsing/download_protection/check_client_download_request.cc
+++ chrome/browser/safe_browsing/download_protection/check_client_download_request.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/policy/chrome_browser_policy_connector.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/safe_browsing/advanced_protection_status_manager.h"
 #include "chrome/browser/safe_browsing/download_protection/download_feedback_service.h"
 #include "chrome/browser/safe_browsing/download_protection/download_protection_service.h"
 #include "chrome/browser/safe_browsing/download_protection/download_protection_util.h"
@@ -104,6 +105,7 @@ CheckClientDownloadRequest::CheckClientDownloadRequest(
       skipped_certificate_whitelist_(false),
       is_extended_reporting_(false),
       is_incognito_(false),
+      is_under_advanced_protection_(false),
       weakptr_factory_(this) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
   item_->AddObserver(this);
@@ -133,6 +135,9 @@ void CheckClientDownloadRequest::Start() {
     is_extended_reporting_ =
         profile && IsExtendedReportingEnabled(*profile->GetPrefs());
     is_incognito_ = browser_context->IsOffTheRecord();
+    is_under_advanced_protection_ =
+        profile &&
+        AdvancedProtectionStatusManager::IsUnderAdvancedProtection(profile);
   }
 
   // If whitelist check passes, PostFinishTask() will be called to avoid
@@ -912,6 +917,8 @@ void CheckClientDownloadRequest::SendRequest() {
   request->mutable_population()->set_profile_management_status(
       GetProfileManagementStatus(
           g_browser_process->browser_policy_connector()));
+  request->mutable_population()->set_is_under_advanced_protection(
+      is_under_advanced_protection_);
 
   request->set_url(SanitizeUrl(item_->GetUrlChain().back()));
   request->mutable_digests()->set_sha256(item_->GetHash());
