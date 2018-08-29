eb17140f5f2d0fc553b72909b9263df0679445e5
manuk <manukh@chromium.org>
2018-08-29 19:32:59 +0000

    Decrease width of location icon by 1 px when separator is shown.
    The location bar displays a separator between the location icon and the omnibox text when either the location icon has an attached label, or when omnibox text jog is disabled. Previously, the location icon was 1 px too wide (33 px without a label instead of 32 px) when the separator was shown. This cl decreases the location icon width by 1 px when a separator is shown, and increases the text padding by 1 px in order to retain alignment between the omnibox and results popup texts. This change applies to location icons with and without a label. This change only applies with material refresh.
    
    Bug: 865523
    Change-Id: I5205a9ee81af6e33bbcb79637c69dd7444fc9b09
    Reviewed-on: https://chromium-review.googlesource.com/1188886
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587244}

--- chrome/browser/ui/views/location_bar/selected_keyword_view.cc
+++ chrome/browser/ui/views/location_bar/selected_keyword_view.cc
@@ -103,7 +103,7 @@ int SelectedKeywordView::GetExtraInternalSpacing() const {
   // MD Refresh needs more space to align the label text with suggestion text.
   return ui::MaterialDesignController::IsRefreshUi()
              ? 11
-             : GetPrefixedSeparatorWidth();
+             : GetWidthBetweenIconAndSeparator();
 }
 
 const char* SelectedKeywordView::GetClassName() const {
