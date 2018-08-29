abf69f2c325db7fcba2933bd7c01923363727374
Sidney San Martín <sdy@chromium.org>
2018-08-29 02:07:26 +0000

    Fix up header include order.
    Change-Id: Iec3c3c68a16be98b2d1c82f51dbf1646b18c0914
    Reviewed-on: https://chromium-review.googlesource.com/1194947
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586997}

--- chrome/browser/metrics/power_metrics_provider_mac.mm
+++ chrome/browser/metrics/power_metrics_provider_mac.mm
@@ -4,6 +4,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/metrics/power_metrics_provider_mac.h"
+
+#include <IOKit/IOKitLib.h>
+#include <libkern/OSByteOrder.h>
+
 #include "base/mac/scoped_ioobject.h"
 #include "base/macros.h"
 #include "base/metrics/histogram_functions.h"
@@ -13,9 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/post_task.h"
 #include "base/time/time.h"
 #include "chrome/browser/ui/browser_finder.h"
-
-#include <IOKit/IOKitLib.h>
-#include <libkern/OSByteOrder.h>
 
 namespace {
 constexpr base::TimeDelta kStartupPowerMetricsCollectionDuration =
