82ded0bdab03eb4f0d3edc6d7c2bca21194c2dbd
Justin Cohen <justincohen@chromium.org>
2018-08-29 11:14:58 +0000

    Revert "Re-land "[ios] Add highlight animation state to fake omnibox.""
    This reverts commit bd98c9e073fe87155f291420f54ad7c0260bceca.
    
    Reason for revert: Breaks https://ci.chromium.org/buildbot/internal.bling.main/iphone10-simulator/14654
    
    Original change's description:
    > Re-land "[ios] Add highlight animation state to fake omnibox."
    > 
    > This reverts commit 167f94502dd8ecef22cd0615505c2aba608cd9ef.
    > 
    > Correct test to scroll from the middle of the screen.
    > 
    > Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    > Change-Id: Ie0d4822e3d44eb0ff2441d0e375dc82bcae97437
    > Reviewed-on: https://chromium-review.googlesource.com/1194470
    > Reviewed-by: Justin Cohen <justincohen@chromium.org>
    > Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    > Commit-Queue: Justin Cohen <justincohen@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#587009}
    
    TBR=rohitrao@chromium.org,justincohen@chromium.org
    
    Change-Id: I32afd822bde6bfe8cdca5a21e63f96d29069ddb8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Reviewed-on: https://chromium-review.googlesource.com/1194952
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587074}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_header_view.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_header_view.mm
@@ -31,12 +31,6 @@ namespace {
 // Landscape inset for fake omnibox background container
 const CGFloat kBackgroundLandscapeInset = 169;
 
-// Fakebox highlight animation duration.
-const CGFloat kFakeboxHighlightDuration = 0.4;
-
-// Fakebox highlight background alpha increase.
-const CGFloat kFakeboxHighlightIncrease = 0.06;
-
 }  // namespace
 
 @interface ContentSuggestionsHeaderView ()<ToolbarSnapshotProviding>
@@ -252,20 +246,6 @@ const CGFloat kFakeboxHighlightIncrease = 0.06;
     else
       constraint.constant = -constantDiff;
   }
-}
-
-- (void)setFakeboxHighlighted:(BOOL)highlighted {
-  [UIView animateWithDuration:kFakeboxHighlightDuration
-                        delay:0
-                      options:UIViewAnimationOptionCurveEaseOut
-                   animations:^{
-                     CGFloat alpha = kAdaptiveLocationBarBackgroundAlpha;
-                     if (highlighted)
-                       alpha += kFakeboxHighlightIncrease;
-                     self.backgroundContainer.backgroundColor =
-                         [UIColor colorWithWhite:0 alpha:alpha];
-                   }
-                   completion:nil];
 }
 
 - (void)fadeOutShadow {
