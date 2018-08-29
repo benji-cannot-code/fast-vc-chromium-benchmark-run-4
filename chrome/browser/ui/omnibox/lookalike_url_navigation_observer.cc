d064caefaf39e5e1fa179eb1addfd7ed0afdc698
Mustafa Emre Acer <meacer@chromium.org>
2018-08-29 20:38:13 +0000

    Rename IdnNavigationSuggestions feature to LookalikeUrlNavigationSuggestions
    Also rename any related classes. In the future we might show suggestions for
    non-IDN navigations as well, this is to prepare for that.
    
    Bug: 843361,847662
    Change-Id: Id614decdc1b108923d6934c66b99274a2477e0a3
    Reviewed-on: https://chromium-review.googlesource.com/1187227
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587283}

--- chrome/browser/ui/omnibox/lookalike_url_navigation_observer.cc
+++ chrome/browser/ui/omnibox/lookalike_url_navigation_observer.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/omnibox/idn_navigation_observer.h"
+#include "chrome/browser/ui/omnibox/lookalike_url_navigation_observer.h"
 
 #include "base/bind.h"
 #include "base/metrics/field_trial_params.h"
@@ -24,10 +24,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 const base::FeatureParam<std::string> kMetricsOnly{
-    &features::kIdnNavigationSuggestions, "metrics_only", ""};
+    &features::kLookalikeUrlNavigationSuggestions, "metrics_only", ""};
 
-void RecordEvent(IdnNavigationObserver::NavigationSuggestionEvent event) {
-  UMA_HISTOGRAM_ENUMERATION(IdnNavigationObserver::kHistogramName, event);
+void RecordEvent(
+    LookalikeUrlNavigationObserver::NavigationSuggestionEvent event) {
+  UMA_HISTOGRAM_ENUMERATION(LookalikeUrlNavigationObserver::kHistogramName,
+                            event);
 }
 
 bool SkeletonsMatch(const url_formatter::Skeletons& skeletons1,
@@ -44,15 +46,16 @@ bool SkeletonsMatch(const url_formatter::Skeletons& skeletons1,
 }  // namespace
 
 // static
-const char IdnNavigationObserver::kHistogramName[] =
+const char LookalikeUrlNavigationObserver::kHistogramName[] =
     "NavigationSuggestion.Event";
 
-IdnNavigationObserver::IdnNavigationObserver(content::WebContents* web_contents)
+LookalikeUrlNavigationObserver::LookalikeUrlNavigationObserver(
+    content::WebContents* web_contents)
     : WebContentsObserver(web_contents) {}
 
-IdnNavigationObserver::~IdnNavigationObserver() {}
+LookalikeUrlNavigationObserver::~LookalikeUrlNavigationObserver() {}
 
-void IdnNavigationObserver::NavigationEntryCommitted(
+void LookalikeUrlNavigationObserver::NavigationEntryCommitted(
     const content::LoadCommittedDetails& load_details) {
   const GURL url = load_details.entry->GetVirtualURL();
   // If the user has engaged with this site, don't show any lookalike
@@ -88,7 +91,7 @@ void IdnNavigationObserver::NavigationEntryCommitted(
 
   if (kMetricsOnly.Get().empty()) {
     RecordEvent(NavigationSuggestionEvent::kInfobarShown);
-    AlternateNavInfoBarDelegate::CreateForIDNNavigation(
+    AlternateNavInfoBarDelegate::CreateForLookalikeUrlNavigation(
         web_contents(), base::UTF8ToUTF16(matched_domain), suggested_url,
         load_details.entry->GetVirtualURL(),
         base::BindOnce(RecordEvent, NavigationSuggestionEvent::kLinkClicked));
@@ -96,16 +99,17 @@ void IdnNavigationObserver::NavigationEntryCommitted(
 }
 
 // static
-void IdnNavigationObserver::CreateForWebContents(
+void LookalikeUrlNavigationObserver::CreateForWebContents(
     content::WebContents* web_contents) {
   DCHECK(web_contents);
   if (!FromWebContents(web_contents)) {
     web_contents->SetUserData(
-        UserDataKey(), std::make_unique<IdnNavigationObserver>(web_contents));
+        UserDataKey(),
+        std::make_unique<LookalikeUrlNavigationObserver>(web_contents));
   }
 }
 
-std::string IdnNavigationObserver::GetMatchingSiteEngagementDomain(
+std::string LookalikeUrlNavigationObserver::GetMatchingSiteEngagementDomain(
     SiteEngagementService* service,
     const GURL& url) {
   // Compute skeletons using eTLD+1.
