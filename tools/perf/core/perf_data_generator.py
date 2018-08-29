6322563b07ca602b4e2ade002b8d239811be86bd
Dominic Mazzoni <dmazzoni@chromium.org>
2018-08-29 18:24:12 +0000

    Add blink_perf accessibility test for large textareas.
    Bug: 875344, 877669
    Cq-Include-Trybots: master.tryserver.chromium.perf:obbs_fyi
    Change-Id: I15bbd8c9f64d999f0d987af9d0b8d2b2785165be
    Reviewed-on: https://chromium-review.googlesource.com/1188777
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Cr-Commit-Position: refs/heads/master@{#587201}

--- tools/perf/core/perf_data_generator.py
+++ tools/perf/core/perf_data_generator.py
@@ -38,6 +38,7 @@ from py_utils import discover
 _UNSCHEDULED_TELEMETRY_BENCHMARKS = set([
   'experimental.startup.android.coldish',
   'experimental.startup.mobile',
+  'blink_perf.accessibility',
   ])
 
 # Additional compile targets to add to builders.
