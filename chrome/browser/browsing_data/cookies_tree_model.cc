c6917bdda2253a2f458c559b995273e6c8e40396
John Rummell <jrummell@chromium.org>
2018-08-29 18:17:46 +0000

    Add additional CookieTree nodes when updating with a filter
    When users apply a filter on chrome://settings/siteData, MediaLicenses and
    FlashLSO nodes where not added to the cookie tree, so they are not displayed.
    This CL adds them back in.
    
    BUG=808690
    TEST=tested locally with sites that save Media Licenses
    
    Change-Id: Ibef359a2c9bdf02c2a59864e8e6232d2953000a8
    Reviewed-on: https://chromium-review.googlesource.com/1171595
    Commit-Queue: John Rummell <jrummell@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587197}

--- chrome/browser/browsing_data/cookies_tree_model.cc
+++ chrome/browser/browsing_data/cookies_tree_model.cc
@@ -1024,6 +1024,9 @@ void CookieTreeFlashLSONode::DeleteStoredObjects() {
   LocalDataContainer* container = GetModel()->data_container();
   container->flash_lso_helper_->DeleteFlashLSOsForSite(
       domain_, base::Closure());
+  auto entry = std::find(container->flash_lso_domain_list_.begin(),
+                         container->flash_lso_domain_list_.end(), domain_);
+  container->flash_lso_domain_list_.erase(entry);
 }
 
 CookieTreeNode::DetailedInfo CookieTreeFlashLSONode::GetDetailedInfo() const {
@@ -1188,6 +1191,8 @@ void CookiesTreeModel::UpdateSearchResults(const base::string16& filter) {
   PopulateServiceWorkerUsageInfoWithFilter(data_container(), &notifier, filter);
   PopulateSharedWorkerInfoWithFilter(data_container(), &notifier, filter);
   PopulateCacheStorageUsageInfoWithFilter(data_container(), &notifier, filter);
+  PopulateFlashLSOInfoWithFilter(data_container(), &notifier, filter);
+  PopulateMediaLicenseInfoWithFilter(data_container(), &notifier, filter);
 }
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
