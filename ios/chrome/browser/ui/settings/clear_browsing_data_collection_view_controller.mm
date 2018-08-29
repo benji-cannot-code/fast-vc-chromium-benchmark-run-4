a28f39b4493c068b48515bd8b85138f63d4bc8d7
sczs <sczs@chromium.org>
2018-08-29 22:31:10 +0000

    [ios] Adds ClearBrowsingData metrics
    Bug: None
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: Ib69e3147dccd2ee7bbeedbd50c0f604240291bfb
    Reviewed-on: https://chromium-review.googlesource.com/1192343
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Gayane Petrosyan <gayane@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587339}

--- ios/chrome/browser/ui/settings/clear_browsing_data_collection_view_controller.mm
+++ ios/chrome/browser/ui/settings/clear_browsing_data_collection_view_controller.mm
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/mac/foundation_util.h"
+#include "base/metrics/user_metrics.h"
+#include "base/metrics/user_metrics_action.h"
 #include "base/scoped_observer.h"
 #include "base/strings/sys_string_conversions.h"
 #include "components/prefs/pref_service.h"
@@ -233,6 +235,8 @@ void BrowsingDataRemoverObserverWrapper::OnBrowsingDataRemoved(
                                timePeriod:(browsing_data::TimePeriod)timePeriod
                                removeMask:(BrowsingDataRemoveMask)removeMask
                           completionBlock:(ProceduralBlock)completionBlock {
+  base::RecordAction(
+      base::UserMetricsAction("MobileClearBrowsingDataTriggeredFromLegacyUI"));
   [self.dispatcher removeBrowsingDataForBrowserState:browserState
                                           timePeriod:timePeriod
                                           removeMask:removeMask
--- ios/chrome/browser/ui/settings/clear_browsing_data_table_view_controller.mm
+++ ios/chrome/browser/ui/settings/clear_browsing_data_table_view_controller.mm
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/settings/clear_browsing_data_table_view_controller.h"
 
 #include "base/mac/foundation_util.h"
+#include "base/metrics/user_metrics.h"
+#include "base/metrics/user_metrics_action.h"
 #include "ios/chrome/browser/browsing_data/browsing_data_remove_mask.h"
 #include "ios/chrome/browser/chrome_url_constants.h"
 #import "ios/chrome/browser/ui/alert_coordinator/action_sheet_coordinator.h"
@@ -247,6 +249,8 @@ class ChromeBrowserState;
                                timePeriod:(browsing_data::TimePeriod)timePeriod
                                removeMask:(BrowsingDataRemoveMask)removeMask
                           completionBlock:(ProceduralBlock)completionBlock {
+  base::RecordAction(
+      base::UserMetricsAction("MobileClearBrowsingDataTriggeredFromUIRefresh"));
   [self.dispatcher removeBrowsingDataForBrowserState:browserState
                                           timePeriod:timePeriod
                                           removeMask:removeMask
