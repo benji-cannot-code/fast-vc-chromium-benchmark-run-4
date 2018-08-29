475ed04c4779b65ffd3b9041d59db2e9e7ada6e4
Jun Choi <hongjunchoi@chromium.org>
2018-08-29 19:51:35 +0000

    Fix transport type for FidoCableDiscovery
    Currently FidoCableDiscovery::transport() returns
    FidoTransportProtocol::kBluetoothLowEnergy. Fix this so that it returns
    FidoTransportProtocol::kCloudAssistedBluetoothLowEnergy instead.
    
    Bug: 877344
    Change-Id: I870e57b747738b5a2608e3c18b71ca46c22818f6
    Reviewed-on: https://chromium-review.googlesource.com/1195120
    Commit-Queue: Jun Choi <hongjunchoi@chromium.org>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587257}

--- device/fido/ble/fido_ble_discovery.cc
+++ device/fido/ble/fido_ble_discovery.cc
@@ -18,7 +18,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace device {
 
-FidoBleDiscovery::FidoBleDiscovery() : weak_factory_(this) {}
+FidoBleDiscovery::FidoBleDiscovery()
+    : FidoBleDiscoveryBase(FidoTransportProtocol::kBluetoothLowEnergy),
+      weak_factory_(this) {}
 
 FidoBleDiscovery::~FidoBleDiscovery() = default;
 
--- device/fido/ble/fido_ble_discovery_base.cc
+++ device/fido/ble/fido_ble_discovery_base.cc
@@ -18,9 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace device {
 
-FidoBleDiscoveryBase::FidoBleDiscoveryBase()
-    : FidoDiscovery(FidoTransportProtocol::kBluetoothLowEnergy),
-      weak_factory_(this) {}
+FidoBleDiscoveryBase::FidoBleDiscoveryBase(FidoTransportProtocol transport)
+    : FidoDiscovery(transport), weak_factory_(this) {}
 
 FidoBleDiscoveryBase::~FidoBleDiscoveryBase() {
   if (adapter_)
