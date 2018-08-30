0f32454f9e45dbfc32e347ae8d9be9fb62516117
Weidong Guo <weidongg@chromium.org>
2018-08-30 17:17:12 +0000

    Avoid hide backdrop for homecher
    Bug: 877163
    Change-Id: I9f1b221bbbfbfb34d4a796c109ef7595e17bbc40
    Reviewed-on: https://chromium-review.googlesource.com/1195928
    Commit-Queue: Weidong Guo <weidongg@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587636}

--- ash/app_list/app_list_presenter_delegate_unittest.cc
+++ ash/app_list/app_list_presenter_delegate_unittest.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shelf/shelf_layout_manager.h"
 #include "ash/shelf/shelf_view.h"
 #include "ash/shell.h"
+#include "ash/shell_test_api.h"
 #include "ash/test/ash_test_base.h"
 #include "ash/wallpaper/wallpaper_controller_test_api.h"
 #include "ash/wm/overview/window_selector_controller.h"
@@ -33,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/tablet_mode/tablet_mode_controller.h"
 #include "ash/wm/window_state.h"
 #include "ash/wm/window_util.h"
+#include "ash/wm/workspace_controller_test_api.h"
 #include "base/command_line.h"
 #include "base/macros.h"
 #include "base/run_loop.h"
@@ -1606,6 +1608,21 @@ TEST_F(AppListPresenterDelegateHomeLauncherTest,
   generator->ReleaseTouch();
   GetAppListTestHelper()->WaitUntilIdle();
   GetAppListTestHelper()->CheckVisibility(true);
+}
+
+// Test backdrop exists for active non-fullscreen window in tablet mode.
+TEST_F(AppListPresenterDelegateHomeLauncherTest, BackdropTest) {
+  WorkspaceControllerTestApi test_helper(
+      ShellTestApi(Shell::Get()).workspace_controller());
+  EnableTabletMode(true);
+  GetAppListTestHelper()->CheckVisibility(true);
+  EXPECT_FALSE(test_helper.GetBackdropWindow());
+
+  std::unique_ptr<aura::Window> non_fullscreen_window(
+      CreateTestWindow(gfx::Rect(0, 0, 100, 100)));
+  non_fullscreen_window->Show();
+  GetAppListTestHelper()->CheckVisibility(true);
+  EXPECT_TRUE(test_helper.GetBackdropWindow());
 }
 
 }  // namespace ash
