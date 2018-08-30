c77ffccd75cd623b0e539052f887a36d89882b70
Etienne Pierre-doray <etiennep@chromium.org>
2018-08-30 20:15:11 +0000

    [TaskScheduler]: Use ScopedBlockingCall to mark blocking tasks.
    This CL uses ScopedBlockingCall to mark blocking calls in /ios/chrome/browser/ui/ntp.
    
    This CL was created by replacing calls to AssertBlockingAllowed()
    with instantiations of ScopedBlockingCall(MAY_BLOCK).
    I kindly ask the reviewer to make sure of the following:
      - ScopedBlockingCall is instantiated in a scope with minimal CPU usage.
        If this is not the case, ScopedBlockingCall should be instantiated
        closer to the blocking call. See scoped_blocking_call.h for more
        info. Please let me know when/where the blocking call happens if this needs
        to be changed.
      - Parameter |blocking_type| matches expectation (MAY_BLOCK/WILL_BLOCK). See
        BlockingType for more info. While I assumed MAY_BLOCK by default, that might
        not be the best fit if we know that this callsite is guaranteed to block.
      - The ScopedBlockingCall's scope covers the entirety of the blocking operation
        previously asserted against by the AssertBlockingAllowed().
    
    This CL was uploaded by git cl split.
    
    R=justincohen@chromium.org
    
    Bug: 874080
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: I5e564d5d5fe57d7c17d06d6e3e79a1b1f609054d
    Reviewed-on: https://chromium-review.googlesource.com/1191677
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587732}

--- ios/chrome/browser/ui/ntp/ntp_tile_saver.mm
+++ ios/chrome/browser/ui/ntp/ntp_tile_saver.mm
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/md5.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/task/post_task.h"
-#include "base/threading/thread_restrictions.h"
+#include "base/threading/scoped_blocking_call.h"
 #include "components/favicon/core/fallback_url_util.h"
 #include "components/ntp_tiles/ntp_tile.h"
 #import "ios/chrome/browser/ui/favicon/favicon_attributes_provider.h"
@@ -185,7 +185,8 @@ void UpdateSingleFavicon(const GURL& site_url,
           NSData* imageData = UIImagePNGRepresentation(attributes.faviconImage);
 
           base::OnceCallback<void()> writeImage = base::BindOnce(^{
-            base::AssertBlockingAllowed();
+            base::ScopedBlockingCall scoped_blocking_call(
+                base::BlockingType::WILL_BLOCK);
             [imageData writeToURL:fileURL atomically:YES];
           });
 
@@ -209,7 +210,8 @@ void UpdateSingleFavicon(const GURL& site_url,
           NSURL* fileURL =
               [favicons_directory URLByAppendingPathComponent:faviconFileName];
           base::OnceCallback<void()> removeImage = base::BindOnce(^{
-            base::AssertBlockingAllowed();
+            base::ScopedBlockingCall scoped_blocking_call(
+                base::BlockingType::WILL_BLOCK);
             [[NSFileManager defaultManager] removeItemAtURL:fileURL error:nil];
           });
 
