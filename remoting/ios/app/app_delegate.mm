03d91899d4924ce7d7c20bfd9bbf2ec26d69959f
Yuwei Huang <yuweih@chromium.org>
2018-08-30 01:40:04 +0000

    [CRD iOS] Change audio session category to Ambient
    AVAudioSessionCategoryAmbient works basically like
    AVAudioSessionCategoryPlayback+AVAudioSessionCategoryOptionMixWithOthers,
    except that it allows the audio to be muted by the silent switch.
    This a more proper category to use because the app can still work with no
    sound. Only apps like music player that requires audio playback when
    silent switch is on should use the AVAudioSessionCategoryPlayback
    category.
    
    Bug: 868088
    Change-Id: I56163a221d878f67363aa8cff7a0151e9602e1c1
    Reviewed-on: https://chromium-review.googlesource.com/1196020
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587393}

--- remoting/ios/app/app_delegate.mm
+++ remoting/ios/app/app_delegate.mm
@@ -51,10 +51,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     didFinishLaunchingWithOptions:(NSDictionary*)launchOptions {
   [self launchRootViewController];
   [RemotingTheme applyColorSchemes];
-  [AVAudioSession.sharedInstance
-      setCategory:AVAudioSessionCategoryPlayback
-      withOptions:AVAudioSessionCategoryOptionMixWithOthers
-            error:NULL];
+  [AVAudioSession.sharedInstance setCategory:AVAudioSessionCategoryAmbient
+                                       error:NULL];
   [AppInitializer onAppDidFinishLaunching];
 
   return YES;
