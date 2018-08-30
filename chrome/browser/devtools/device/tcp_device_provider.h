1611eed5256b08e21c23c6bfefc0ce7440f97e7c
Clark DuVall <cduvall@chromium.org>
2018-08-30 20:44:54 +0000

    Reland "Convert tcp_device_provider.cc to mojo host resolver"
    This is a reland of ac880b3ad00b2f206ff8a9fddaff004763370b03
    
    Switched to using
    ChromeDevToolsManagerDelegate::GetDefaultBrowserContext() which
    outlives TCPDeviceProvider. The original was reverted because of a
    crash when TCPDeviceProvider tried to access a destroyed
    BrowserContext, see http://crbug.com/879060.
    
    Original change's description:
    > Convert tcp_device_provider.cc to mojo host resolver
    >
    > Bug: 874653
    > Change-Id: I95b968427344a4c9821c0985ae1f89c153036bcd
    > Reviewed-on: https://chromium-review.googlesource.com/1185626
    > Commit-Queue: Clark DuVall <cduvall@chromium.org>
    > Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#586738}
    
    Bug: 874653
    Change-Id: Ia6c00ba51020473f820add8c4257f3bf16fd5b24
    Reviewed-on: https://chromium-review.googlesource.com/1197265
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587750}

--- chrome/browser/devtools/device/tcp_device_provider.h
+++ chrome/browser/devtools/device/tcp_device_provider.h
@@ -17,10 +17,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // BUILDFLAG is set.
 class TCPDeviceProvider : public AndroidDeviceManager::DeviceProvider {
  public:
-  static scoped_refptr<TCPDeviceProvider> CreateForLocalhost(uint16_t port);
+  static scoped_refptr<TCPDeviceProvider> CreateForLocalhost(
+      uint16_t port,
+      content::BrowserContext* context);
 
   using HostPortSet = std::set<net::HostPortPair>;
-  explicit TCPDeviceProvider(const HostPortSet& targets);
+  TCPDeviceProvider(const HostPortSet& targets,
+                    content::BrowserContext* context);
 
   void QueryDevices(const SerialsCallback& callback) override;
 
@@ -33,6 +36,8 @@ class TCPDeviceProvider : public AndroidDeviceManager::DeviceProvider {
 
   void ReleaseDevice(const std::string& serial) override;
 
+  void set_targets(HostPortSet targets) { targets_ = std::move(targets); }
+
   void set_release_callback_for_test(const base::Closure& callback);
 
   HostPortSet get_targets_for_test() { return targets_; }
@@ -40,8 +45,13 @@ class TCPDeviceProvider : public AndroidDeviceManager::DeviceProvider {
  private:
   ~TCPDeviceProvider() override;
 
+  void InitializeHostResolver();
+  void InitializeHostResolverOnUI(network::mojom::HostResolverRequest request);
+
   HostPortSet targets_;
   base::Closure release_callback_;
+  content::BrowserContext* context_;
+  network::mojom::HostResolverPtr host_resolver_;
 };
 
 #endif  // CHROME_BROWSER_DEVTOOLS_DEVICE_TCP_DEVICE_PROVIDER_H_
