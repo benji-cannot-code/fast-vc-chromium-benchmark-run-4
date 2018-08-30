b14eed62f33d2a372e63897b89f8e60d9d75fbc4
Sujie Zhu <sujiezhu@google.com>
2018-08-30 21:49:12 +0000

    [Local card migration] Add new autofill_private api to trigger migration
    In this CL, we create a new autofill_private api function in order to trigger migration from settings page. We have to construct autofill_client, payments_client, personal_data_manager and local_card_migration_manager in the api for triggering migration and uploading the cards.
    
    The following CL will be use the added IsMigratable, syncStatus and experimental flag to check the migration requirement and trigger this api when meets the requirements.
    
    Please refer to the design doc for more details: https://docs.google.com/document/d/1J7YyBeKZZaBhjAHnMEezvuBa5x1pk_XCh_yZvV_HMmc/edit#heading=h.1wm3blr98d8i
    
    Bug: 852904
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: I75a4a952efaf33c3f9187348a6e39f3abd4471a4
    Reviewed-on: https://chromium-review.googlesource.com/1178683
    Reviewed-by: Scott Chen <scottchen@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Commit-Queue: Sujie Zhu <sujiezhu@google.com>
    Cr-Commit-Position: refs/heads/master@{#587795}

--- extensions/browser/extension_function_histogram_value.h
+++ extensions/browser/extension_function_histogram_value.h
@@ -1329,6 +1329,7 @@ enum HistogramValue {
   ARCAPPSPRIVATE_GETLAUNCHABLEAPPS = 1265,
   ARCAPPSPRIVATE_LAUNCHAPP = 1266,
   AUTOTESTPRIVATE_RUNCROSTINIINSTALLER = 1267,
+  AUTOFILLPRIVATE_MIGRATECREDITCARDS = 1268,
   // Last entry: Add new entries above, then run:
   // python tools/metrics/histograms/update_extension_histograms.py
   ENUM_BOUNDARY
