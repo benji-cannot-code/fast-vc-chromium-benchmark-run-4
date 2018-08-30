e11c895984ec3fc90a811c999714fa63b60f001c
Francois Doray <fdoray@chromium.org>
2018-08-30 19:35:42 +0000

    aura: Make WebContents VISIBLE when shown in Alt-Tab view on ChromeOS.
    Today, WebContents::GetVisibility() returns HIDDEN for a tab that is in
    a minimized window shown in the Alt-Tab view on ChromeOS. This is
    because content::WebContents::GetVisibility() indicates the visibility
    of the aura::Window, and isn't affected by the mirroring functionnality
    used by the Alt-Tab view.
    
    Unfortunately, the fact that WebContents::GetVisibility() returns
    HIDDEN makes TabManager think that the tab is eligible for freezing and
    discarding (freezing or discarding a tab in Alt-Tab breaks the live
    view functionnality). TabManager could check by itself whether the
    WebContents IsBeingCaptured(), but instead of requiring that every
    call to WebContents::GetVisibility() be accompanied to a check to
    IsBeingCaptured(), we believe it's better to return VISIBLE from
    GetVisibility() when a tab is displayed in Alt-Tab view.
    
    Bug: 876103
    Change-Id: I01c07858bcae3038577bac1408c67eb72fe08e0c
    Reviewed-on: https://chromium-review.googlesource.com/1191862
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587711}

--- content/browser/web_contents/web_contents_view_aura.h
+++ content/browser/web_contents/web_contents_view_aura.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/buildflags.h"
 #include "content/common/content_export.h"
 #include "content/public/browser/global_routing_id.h"
+#include "content/public/browser/visibility.h"
 #include "ui/aura/client/drag_drop_delegate.h"
 #include "ui/aura/window.h"
 #include "ui/aura/window_delegate.h"
@@ -97,6 +98,12 @@ class CONTENT_EXPORT WebContentsViewAura
 
   // Called from CreateView() to create |window_|.
   void CreateAuraWindow(aura::Window* context);
+
+  // Computes the view's visibility updates the WebContents accordingly.
+  void UpdateWebContentsVisibility();
+
+  // Computes the view's visibility.
+  Visibility GetVisibility() const;
 
   // Overridden from WebContentsView:
   gfx::NativeView GetNativeView() const override;
