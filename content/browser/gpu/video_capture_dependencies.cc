d45620595f76540fe5b57b16f205692d30fa70eb
Christian Fremerey <chfremer@chromium.org>
2018-08-30 01:30:56 +0000

    [Video Capture Service] Inject GPU dependencies when starting service via extension
    The video capture service requires GPU dependencies to be injected after startup
    in order to enable accelerated MJPEG decoding. This CL adds this injection call
    where the service is started from the media_perception_private extension.
    
    The injected dependencies use classes that used to live in
    content/browser/renderer_host/media. To be able to reuse them for the
    extension, this CL moves them to a location that is accessible to both
    usage sites, i.e. to content/browser/gpu with a public interface in
    content/public/browser.
    
    Bug: 877690
    Change-Id: If43ff7ff3f24a0aa17d441af5f5f605a894f374e
    Reviewed-on: https://chromium-review.googlesource.com/1195657
    Commit-Queue: Christian Fremerey <chfremer@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587377}

--- content/browser/gpu/video_capture_dependencies.cc
+++ content/browser/gpu/video_capture_dependencies.cc
@@ -3,11 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "content/browser/renderer_host/media/video_capture_dependencies.h"
+#include "content/browser/gpu/video_capture_dependencies.h"
 
 #include "content/browser/gpu/gpu_process_host.h"
 #include "content/public/browser/browser_thread.h"
-#include "mojo/public/cpp/bindings/strong_binding.h"
 
 namespace content {
 
