ccb7cbff692529270b123664bd26e1f83f1580f8
Matt Menke <mmenke@chromium.org>
2018-08-30 07:55:35 +0000

    Move Pepper TCP and UDP annotations to the same place, update UDP ones.
    This CL moves the pepper network anntotations to the same file, to more
    easily keep them in sync, and makes the UDP one match the more complete
    TCP one.
    
    Bug: 878136
    Change-Id: I7d607aacedb9298afc3796990c5a713247c32001
    Reviewed-on: https://chromium-review.googlesource.com/1194958
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Bill Budge <bbudge@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587478}

--- content/browser/renderer_host/pepper/pepper_socket_utils.h
+++ content/browser/renderer_host/pepper/pepper_socket_utils.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "build/build_config.h"
 #include "content/public/common/socket_permission_request.h"
+#include "net/traffic_annotation/network_traffic_annotation.h"
 #include "ppapi/c/pp_stdint.h"
 
 #if defined(OS_CHROMEOS)
@@ -72,6 +73,11 @@ void OpenTCPFirewallHole(const net::IPEndPoint& address,
 void OpenUDPFirewallHole(const net::IPEndPoint& address,
                          FirewallHoleOpenCallback callback);
 #endif  // defined(OS_CHROMEOS)
+
+// Annotations for TCP and UDP network requests. Defined here to make it easier
+// to keep them in sync.
+net::MutableNetworkTrafficAnnotationTag PepperTCPNetworkAnnotationTag();
+net::MutableNetworkTrafficAnnotationTag PepperUDPNetworkAnnotationTag();
 
 }  // namespace pepper_socket_utils
 
