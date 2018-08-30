4f5c764795416565c2815ee39bf5a0a98085ba2f
Sahel Sharify <sahel@chromium.org>
2018-08-30 21:38:15 +0000

    Reland "Clean up DidStopFlinging IPC"
    This reverts commit fa22a78cac1c7d2dda45a7fb2085216eacd40290.
    
    Reason for revert: I reverted the initial cl to prove that crash reports in crbug.com/868849 are not caused by it. Relanding it now since the crashes still happened after the revert.
    
    Original change's description:
    > Revert "Clean up DidStopFlinging IPC"
    > 
    > This reverts commit 7cb247dd37dcef6c4a30374d0b30da99de5bbed3.
    > 
    > Reason for revert: Check to see if the crash report in crbug.com/868849 goes away or not.
    > 
    > Original change's description:
    > > Clean up DidStopFlinging IPC
    > > 
    > > This cl also removes TouchscreenFlingInProgress function that is no
    > > longer needed.
    > > 
    > > Bug: 249063
    > > Change-Id: Icc939d654cdfe0cf38c843933f2fd469f843b74c
    > > Reviewed-on: https://chromium-review.googlesource.com/1145784
    > > Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    > > Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    > > Reviewed-by: Philip Rogers <pdr@chromium.org>
    > > Commit-Queue: Sahel Sharify <sahel@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#578410}
    > 
    > TBR=kenrb@chromium.org,pdr@chromium.org,dtapuska@chromium.org,sahel@chromium.org
    > 
    > # Not skipping CQ checks because original CL landed > 1 day ago.
    > 
    > Bug: 249063
    > Change-Id: Iee7200d3c4e388fccbc2dc6b86997a07a3a03f0a
    > Reviewed-on: https://chromium-review.googlesource.com/1155169
    > Reviewed-by: Sahel Sharify <sahel@chromium.org>
    > Commit-Queue: Sahel Sharify <sahel@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#579166}
    
    TBR=kenrb@chromium.org,pdr@chromium.org,dtapuska@chromium.org,sahel@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 249063
    Change-Id: I47b3d00615ebb29b643aa03ef73b523381c092c6
    Reviewed-on: https://chromium-review.googlesource.com/1197334
    Reviewed-by: Sahel Sharify <sahel@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Sahel Sharify <sahel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587784}

--- content/browser/renderer_host/input/fling_controller.h
+++ content/browser/renderer_host/input/fling_controller.h
@@ -100,8 +100,6 @@ class CONTENT_EXPORT FlingController {
 
   bool FlingCancellationIsDeferred() const;
 
-  bool TouchscreenFlingInProgress() const;
-
   gfx::Vector2dF CurrentFlingVelocity() const;
 
   // Returns the |TouchpadTapSuppressionController| instance.
