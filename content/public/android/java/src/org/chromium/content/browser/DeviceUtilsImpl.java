b4d28033f42ee2d6f728375e2e85f81cfda66a54
Ted Choc <tedchoc@google.com>
2018-08-30 01:21:10 +0000

    Add strict mode exemption to DeviceUtils#addDeviceSpecificUserAgentSwitch
    BUG=878704
    
    Change-Id: I30dbc348c2a8b5e7440822993dae21c92784fa47
    Reviewed-on: https://chromium-review.googlesource.com/1195922
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587360}

--- content/public/android/java/src/org/chromium/content/browser/DeviceUtilsImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/DeviceUtilsImpl.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.content.browser;
 
 import org.chromium.base.CommandLine;
+import org.chromium.base.StrictModeContext;
 import org.chromium.content_public.common.ContentSwitches;
 import org.chromium.ui.base.DeviceFormFactor;
 
@@ -16,8 +17,10 @@ public class DeviceUtilsImpl {
     private DeviceUtilsImpl() {}
 
     public static void addDeviceSpecificUserAgentSwitch() {
-        if (!DeviceFormFactor.isTablet()) {
-            CommandLine.getInstance().appendSwitch(ContentSwitches.USE_MOBILE_UA);
+        try (StrictModeContext unused = StrictModeContext.allowDiskReads()) {
+            if (!DeviceFormFactor.isTablet()) {
+                CommandLine.getInstance().appendSwitch(ContentSwitches.USE_MOBILE_UA);
+            }
         }
     }
 }
