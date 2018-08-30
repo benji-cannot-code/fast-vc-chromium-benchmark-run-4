109b31fc99d4c67bffa98c529fcc3a9ec65a58fb
Florian Uunk <feuunk@chromium.org>
2018-08-30 12:41:11 +0000

    Tell the sync server whether full sync is enabled.
    We want to know on the sync server whether full sync is enabled or not
    so that we can separate the different types of users in dashboards and
    monitoring, and so that we can decide to hit the new payments backend
    for transport-only users.
    
    We're adding this boolean to ClientStatus, because we're already sending
    that on all ClientToServerRequest messages.
    
    Whenever we switch between full sync and transport-only sync, the sync
    engine gets reconfigured. This CL adds a boolean to that configure call,
    which gets forwarded to the sync_cycle_context. The syncer_proto_util.cc
    picks it up from there, together with other information that was already
    part of ClientStatus.
    
    Change-Id: Ie9283e83c2fa92c1d1467fcce59057ffced577d2
    Bug: 878857
    Reviewed-on: https://chromium-review.googlesource.com/1193878
    Commit-Queue: Florian Uunk <feuunk@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587533}

--- components/sync/driver/data_type_manager_impl.cc
+++ components/sync/driver/data_type_manager_impl.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/threading/sequenced_task_runner_handle.h"
 #include "base/trace_event/trace_event.h"
+#include "components/sync/driver/configure_context.h"
 #include "components/sync/driver/data_type_encryption_handler.h"
 #include "components/sync/driver/data_type_manager_observer.h"
 #include "components/sync/driver/data_type_status_table.h"
@@ -607,6 +608,8 @@ ModelTypeSet DataTypeManagerImpl::PrepareConfigureParams(
                  weak_ptr_factory_.GetWeakPtr(), download_types_queue_.front());
   params->retry_callback = base::Bind(&DataTypeManagerImpl::OnDownloadRetry,
                                       weak_ptr_factory_.GetWeakPtr());
+  params->is_sync_feature_enabled = last_requested_context_.storage_option ==
+                                    ConfigureContext::STORAGE_ON_DISK;
 
   DCHECK(Intersection(active_types, types_to_purge).Empty());
   DCHECK(Intersection(active_types, fatal_types).Empty());
--- components/sync/driver/glue/sync_backend_host_core.cc
+++ components/sync/driver/glue/sync_backend_host_core.cc
@@ -164,7 +164,7 @@ void SyncBackendHostCore::OnInitializationComplete(
   sync_manager_->PurgeDisabledTypes(types_to_purge, ModelTypeSet(),
                                     ModelTypeSet());
   sync_manager_->ConfigureSyncer(
-      reason, new_control_types,
+      reason, new_control_types, SyncManager::SyncFeatureState::INITIALIZING,
       base::Bind(&SyncBackendHostCore::DoInitialProcessControlTypes,
                  weak_ptr_factory_.GetWeakPtr()),
       base::Closure());
@@ -492,6 +492,9 @@ void SyncBackendHostCore::DoConfigureSyncer(
                  weak_ptr_factory_.GetWeakPtr(), params.retry_callback));
 
   sync_manager_->ConfigureSyncer(params.reason, params.to_download,
+                                 params.is_sync_feature_enabled
+                                     ? SyncManager::SyncFeatureState::ON
+                                     : SyncManager::SyncFeatureState::OFF,
                                  chained_ready_task, chained_retry_task);
 }
 
