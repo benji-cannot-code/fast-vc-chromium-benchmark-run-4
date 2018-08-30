43920aa7e80daab10e26c190586191f925c7fce0
Nick Harper <nharper@chromium.org>
2018-08-30 23:17:47 +0000

    Disable Channel ID
    Bug: 875053
    Change-Id: Ia0d4f40619c2d9f7a271e78857a86b9b63a3dc0b
    Reviewed-on: https://chromium-review.googlesource.com/1194876
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Commit-Queue: Nick Harper <nharper@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587833}

--- components/network_session_configurator/common/network_features.cc
+++ components/network_session_configurator/common/network_features.cc
@@ -9,8 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace features {
 
-// TODO(nharper): Disable kChannelID on all platforms in M69.
-const base::Feature kChannelID{"channel-id", base::FEATURE_ENABLED_BY_DEFAULT};
+const base::Feature kChannelID{"channel-id", base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kDnsOverHttps{"dns-over-https",
                                   base::FEATURE_DISABLED_BY_DEFAULT};
