664b7c4bdb19654f238a85ddbe32078015b39fa7
Kenneth Russell <kbr@chromium.org>
2018-08-30 23:08:14 +0000

    Rewrite Pixel_WorkerRAF_OOPD as context_lost test.
    The test seems to be timing out while taking a page screenshot after
    the test completes, and this isn't the important part of the test.
    Instead, rewrite it as a context_lost test, and include two variants:
    
      ContextLost_WorkerRAFAfterGPUCrash
      ContextLost_WorkerRAFAfterGPUCrash_OOPD
    
    Tbr: fserb@chromium.org
    Bug: 851213
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: Ib1556ad9f693003b6b216085a0b1665cad017314
    Reviewed-on: https://chromium-review.googlesource.com/1197942
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587829}

--- content/test/gpu/gpu_tests/pixel_test_pages.py
+++ content/test/gpu/gpu_tests/pixel_test_pages.py
@@ -396,9 +396,6 @@ def ExperimentalCanvasFeaturesPages(base_name):
   unaccelerated_args = [
     '--disable-accelerated-2d-canvas',
     '--disable-gpu-compositing']
-  browser_args_oopd = [
-    '--enable-viz-display-compositor',
-    '--enable-experimental-web-platform-features']
 
   return [
     PixelTestPage(
@@ -414,14 +411,6 @@ def ExperimentalCanvasFeaturesPages(base_name):
       test_rect=[0, 0, 350, 350],
       revision=8,
       browser_args=browser_args),
-
-    PixelTestPage(
-      'pixel_worker_requestAnimationFrame.html',
-      base_name + '_WorkerRAF_OOPD',
-      test_rect=[0, 0, 1, 1],
-      revision=1,
-      optional_action='CrashGpuProcess',
-      browser_args=browser_args_oopd),
 
     PixelTestPage(
       'pixel_offscreenCanvas_webgl_paint_after_resize.html',
