ef44de0d094616ac2e143fb0bedac03058f6d9b4
Yoichi Osato <yoichio@chromium.org>
2018-08-30 03:26:57 +0000

    Show Deprecation message for HTML Import.
    This patch marks it removed in M73.
    This patch also unifies deprecation message for
    HTMLImportsHasStyleSheets.
    
    Bug: 766694
    Change-Id: I7788a5500c3760fcf73e84c5badff2d4170904bc
    Reviewed-on: https://chromium-review.googlesource.com/1192866
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587433}

--- third_party/blink/renderer/core/html/imports/html_import_child.cc
+++ third_party/blink/renderer/core/html/imports/html_import_child.cc
@@ -34,8 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/style_engine.h"
 #include "third_party/blink/renderer/core/css/style_sheet_list.h"
 #include "third_party/blink/renderer/core/dom/document.h"
-#include "third_party/blink/renderer/core/frame/deprecation.h"
-#include "third_party/blink/renderer/core/frame/use_counter.h"
 #include "third_party/blink/renderer/core/html/custom/v0_custom_element.h"
 #include "third_party/blink/renderer/core/html/custom/v0_custom_element_microtask_import_step.h"
 #include "third_party/blink/renderer/core/html/custom/v0_custom_element_sync_microtask_queue.h"
@@ -82,10 +80,6 @@ void HTMLImportChild::DidFinish() {
 
 void HTMLImportChild::DidFinishLoading() {
   StateWillChange();
-  if (GetDocument() && GetDocument()->GetStyleEngine().HasStyleSheets()) {
-    Deprecation::CountDeprecation(Root()->GetDocument(),
-                                  WebFeature::kHTMLImportsHasStyleSheets);
-  }
   V0CustomElement::DidFinishLoadingImport(*(Root()->GetDocument()));
 }
 
--- third_party/blink/renderer/core/html/imports/html_imports_controller.cc
+++ third_party/blink/renderer/core/html/imports/html_imports_controller.cc
@@ -42,9 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 HTMLImportsController::HTMLImportsController(Document& master)
-    : root_(HTMLImportTreeRoot::Create(&master)) {
-  UseCounter::Count(master, WebFeature::kHTMLImports);
-}
+    : root_(HTMLImportTreeRoot::Create(&master)) {}
 
 void HTMLImportsController::Dispose() {
   for (const auto& loader : loaders_)
