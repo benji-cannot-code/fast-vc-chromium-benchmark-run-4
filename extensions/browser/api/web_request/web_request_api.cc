43f1d38c55fc202459d03a8660bc852672cb5b4d
Clark DuVall <cduvall@chromium.org>
2018-08-30 20:44:06 +0000

    Fix crash in web request proxies connection error handler
    When WebRequestAPI is destroyed, it posts a task to the IO thread to
    shutdown the proxies. This crash can happen when the WebRequestAPI is
    destroyed, and a connection error happens on one of the proxies (which
    accesses data destroyed by the profile) before the ProxySet can be fully
    shutdown on the IO thread. This change adds an atomic flag to check to
    make sure the WebRequestAPI has not been destroyed before running error
    handlers.
    
    Bug: 878366
    Change-Id: I2f7b98c1d6df7c53a5917444ac996e9b5ef4e794
    Reviewed-on: https://chromium-review.googlesource.com/1196031
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587749}

--- extensions/browser/api/web_request/web_request_api.cc
+++ extensions/browser/api/web_request/web_request_api.cc
@@ -517,6 +517,7 @@ WebRequestAPI::WebRequestAPI(content::BrowserContext* context)
 }
 
 WebRequestAPI::~WebRequestAPI() {
+  proxies_->SetAPIDestroyed();
   BrowserThread::PostTask(
       BrowserThread::IO, FROM_HERE,
       base::BindOnce(&ProxySet::Shutdown, std::move(proxies_)));
