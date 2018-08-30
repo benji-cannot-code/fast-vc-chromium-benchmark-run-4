c3f3f4f07524a9e34a214f4ecd828c0f853a6dd8
Esmael El-Moslimany <aee@chromium.org>
2018-08-30 17:52:03 +0000

    Site Settings WebUI: hide policy pref indicator when not enforced
    Bug: 877896
    Change-Id: I008e41233bf9378370b592425e5a928a0b97301f
    Reviewed-on: https://chromium-review.googlesource.com/1195745
    Reviewed-by: Scott Chen <scottchen@chromium.org>
    Commit-Queue: Esmael El-Moslimany <aee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587651}

--- chrome/browser/resources/settings/site_settings/site_list_entry.js
+++ chrome/browser/resources/settings/site_settings/site_list_entry.js
@@ -45,6 +45,12 @@ Polymer({
       type: String,
       computed: 'computeSiteDescription_(model)',
     },
+
+    /** @private */
+    showPolicyPrefIndicator_: {
+      type: Boolean,
+      computed: 'computeShowPolicyPrefIndicator_(model)',
+    },
   },
 
   /** @private */
@@ -112,6 +118,16 @@ Polymer({
       return loadTimeData.getString('incognitoSite');
     }
     return displayName;
+  },
+
+  /**
+   * @return {boolean}
+   * @private
+   */
+  computeShowPolicyPrefIndicator_: function() {
+    return this.model.enforcement ==
+        chrome.settingsPrivate.Enforcement.ENFORCED &&
+        !!this.model.controlledBy;
   },
 
   /** @private */
