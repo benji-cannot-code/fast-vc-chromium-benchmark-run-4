7da8779da8a0f6e857005e4804cb7a8ecfa754f5
Paul Irish <paulirish@chromium.org>
2018-08-30 17:04:23 +0000

    DevTools: [Audits] Minor tweak of status text
    Change-Id: I2e7443a02e3bb6f46e1065b9164af72393912344
    Reviewed-on: https://chromium-review.googlesource.com/1196103
    Reviewed-by: Patrick Hulce <phulce@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Paul Irish <paulirish@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587633}

--- third_party/blink/renderer/devtools/front_end/audits2/Audits2StatusView.js
+++ third_party/blink/renderer/devtools/front_end/audits2/Audits2StatusView.js
@@ -286,7 +286,7 @@ Audits2.StatusView.StatusPhases = [
   {
     id: 'auditing',
     progressBarClass: 'auditing',
-    message: 'Almost there! Lighthouse is now generating your own special pretty report!',
+    message: 'Almost there! Lighthouse is now generating your report.',
     statusMessagePrefix: 'Evaluating',
     order: 30,
   }
