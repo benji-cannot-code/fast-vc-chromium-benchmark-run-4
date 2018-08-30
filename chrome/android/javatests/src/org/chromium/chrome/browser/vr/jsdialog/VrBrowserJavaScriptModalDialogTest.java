2992b3eb57d37b28b62c89b30dcefc3a8148be9a
Alexandr Ilin <alexilin@chromium.org>
2018-08-30 13:07:06 +0000

    vr: Disable failing modal dialog tests
    This CL disables:
    org.chromium.chrome.browser.vr.jsdialog.VrBrowserJavaScriptModalDialogTest#testConfirmModalDialog
    org.chromium.chrome.browser.vr.jsdialog.VrBrowserJavaScriptModalDialogTest#testPromptModalDialog
    org.chromium.chrome.browser.vr.jsdialog.VrBrowserJavaScriptModalDialogTest#testAlertModalDialog
    
    These tests are constantly failing on Nougat Phone Tester.
    
    TBR: bsheedy@chromium.org
    Bug: 879130
    Change-Id: I7e9677764274d33909907ed28cfed6a6aa88e2d9
    Reviewed-on: https://chromium-review.googlesource.com/1196446
    Reviewed-by: Alexandr Ilin <alexilin@chromium.org>
    Commit-Queue: Alexandr Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587540}

--- chrome/android/javatests/src/org/chromium/chrome/browser/vr/jsdialog/VrBrowserJavaScriptModalDialogTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/vr/jsdialog/VrBrowserJavaScriptModalDialogTest.java
@@ -18,6 +18,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.ChromeSwitches;
@@ -61,6 +62,7 @@ public class VrBrowserJavaScriptModalDialogTest {
      */
     @Test
     @MediumTest
+    @DisabledTest(message = "crbug.com/879130")
     @Feature({"Browser", "RenderTest"})
     public void testAlertModalDialog() throws ExecutionException, IOException {
         testModalDialogImpl("js_modal_view_vr_alert", "alert('Hello Android!')");
@@ -71,6 +73,7 @@ public class VrBrowserJavaScriptModalDialogTest {
      */
     @Test
     @MediumTest
+    @DisabledTest(message = "crbug.com/879130")
     @Feature({"Browser", "RenderTest"})
     public void testConfirmModalDialog() throws ExecutionException, IOException {
         testModalDialogImpl("js_modal_view_vr_confirm", "confirm('Deny?')");
@@ -81,6 +84,7 @@ public class VrBrowserJavaScriptModalDialogTest {
      */
     @Test
     @MediumTest
+    @DisabledTest(message = "crbug.com/879130")
     @Feature({"Browser", "RenderTest"})
     public void testPromptModalDialog() throws ExecutionException, IOException {
         testModalDialogImpl(
