37d56a3adc5b057c7af0011c0b39d32ce9e7de68
Alexey Baskakov <loyso@chromium.org>
2018-08-30 08:16:03 +0000

    WebApp: Remove BookmarkAppHelper dependency from BookmarkAppDataRetriever.
    Remove BookmarkAppHelper and extensions_misc deps from BookmarkAppDataRetriever.
    
    Introduce web_app::IconSize size presets.
    
    Bug: 875698
    Change-Id: I460ef7cf8c2c55f02c9dfabb6fdb888a19883d3c
    Reviewed-on: https://chromium-review.googlesource.com/1192465
    Commit-Queue: Alexey Baskakov <loyso@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587481}

--- chrome/browser/web_applications/components/web_app_icon_generator.h
+++ chrome/browser/web_applications/components/web_app_icon_generator.h
@@ -16,6 +16,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace web_app {
 
+namespace icon_size {
+
+// An iteration of valid icon sizes in pixels. Allows client code to declare a
+// subset of supported sizes that are guaranteed to be valid.
+enum {
+  k512 = 512,
+  k256 = 256,
+  k128 = 128,
+  k48 = 48,
+  k32 = 32,
+  k24 = 24,
+  k16 = 16,
+  kInvalid = 0,
+};
+
+}  // namespace icon_size
+
 struct BitmapAndSource {
   BitmapAndSource();
   BitmapAndSource(const GURL& source_url_p, const SkBitmap& bitmap_p);
