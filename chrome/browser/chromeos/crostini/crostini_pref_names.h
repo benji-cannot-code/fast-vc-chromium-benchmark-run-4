26c595a29007e0c5cd5fe88cf44e5bbd92a9a83d
Jeffrey Kardatzke <jkardatzke@google.com>
2018-08-31 00:01:51 +0000

    Add alternate MIME types for files for Crostini
    This receives MIME type information from Crostini and uses that as a
    secondary lookup technique for file associations. This information is
    only referenced when a file has been detected to have one of the default
    MIME types of either text/plain or application/octet-stream. This is
    stored in a new preferences location as well with a unit test for that.
    
    Bug: 869747
    Test: Verified Open With associations on eve, unit tests pass
    Change-Id: Ie631ae609b04c72e37c205e6d5c214816ba47563
    Reviewed-on: https://chromium-review.googlesource.com/1195045
    Commit-Queue: Jeffrey Kardatzke <jkardatzke@google.com>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587863}

--- chrome/browser/chromeos/crostini/crostini_pref_names.h
+++ chrome/browser/chromeos/crostini/crostini_pref_names.h
@@ -13,6 +13,7 @@ namespace prefs {
 
 extern const char kCrostiniEnabled[];
 extern const char kCrostiniRegistry[];
+extern const char kCrostiniMimeTypes[];
 
 void RegisterProfilePrefs(PrefRegistrySimple* registry);
 
--- chrome/browser/chromeos/crostini/crostini_remover.cc
+++ chrome/browser/chromeos/crostini/crostini_remover.cc
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/chromeos/crostini/crostini_mime_types_service.h"
+#include "chrome/browser/chromeos/crostini/crostini_mime_types_service_factory.h"
 #include "chrome/browser/chromeos/crostini/crostini_pref_names.h"
 #include "chrome/browser/chromeos/crostini/crostini_registry_service.h"
 #include "chrome/browser/chromeos/crostini/crostini_registry_service_factory.h"
@@ -64,6 +66,8 @@ void CrostiniRemover::StopVmFinished(ConciergeClientResult result) {
     return;
   }
   CrostiniRegistryServiceFactory::GetForProfile(profile_)->ClearApplicationList(
+      vm_name_, container_name_);
+  CrostiniMimeTypesServiceFactory::GetForProfile(profile_)->ClearMimeTypes(
       vm_name_, container_name_);
   CrostiniManager::GetInstance()->DestroyDiskImage(
       CryptohomeIdForProfile(profile_), base::FilePath(vm_name_),
