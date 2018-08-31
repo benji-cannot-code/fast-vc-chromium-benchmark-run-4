3a93682f0358b5508103a37b7cdc46c49479f172
Ovidio Henriquez <odejesush@chromium.org>
2018-08-31 20:14:02 +0000

    Add validation to the requestDevice filters
    This change adds validation checks to the USBDeviceFilter parameters
    passed into navigator.usb.requestDevice.
    
    Bug: 854703
    Change-Id: I921a7f30a9e2ec5d46d3a74801de594565ab8a6f
    Reviewed-on: https://chromium-review.googlesource.com/1196034
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Ovidio Henriquez <odejesush@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588140}

--- third_party/WebKit/LayoutTests/external/wpt/webusb/usb.https.window.js
+++ third_party/WebKit/LayoutTests/external/wpt/webusb/usb.https.window.js
@@ -79,7 +79,7 @@ usb_test(() => {
   return callWithTrustedClick(() => {
     return navigator.usb.requestDevice({ filters: expectedFilters })
       .then(device => {
-        assert_unreachable(
+        assert_unreached(
             'requestDevice should reject because no device selected');
       })
       .catch(error => {
@@ -87,6 +87,26 @@ usb_test(() => {
       });
   });
 }, 'filters are sent correctly');
+
+usb_test(async () => {
+  const badFilters = [
+    { productId: 1234 },     // productId requires vendorId
+    { subclassCode: 5678 },  // subclassCode requires classCode
+    { protocolCode: 9012 },  // protocolCode requires subclassCode
+  ];
+
+  for (const filter of badFilters) {
+    await callWithTrustedClick(async () => {
+      try {
+        await navigator.usb.requestDevice({ filters: [filter] });
+        assert_unreached(
+            'requestDevice should reject because of invalid filters');
+      } catch (error) {
+        assert_equals(error.name, 'TypeError');
+      }
+    });
+  }
+}, 'requestDevice rejects on invalid filters');
 
 usb_test(() => {
   return getFakeDevice().then(({ device, fakeDevice }) => {
