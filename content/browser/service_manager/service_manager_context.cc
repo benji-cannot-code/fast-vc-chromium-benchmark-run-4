6610b98ab3cdb5833bb4f0b126b26c12b0c57853
Becca Hughes <beccahughes@chromium.org>
2018-08-31 18:02:07 +0000

    [Media Session] Embed media session service
    Embed the media session service in the browser process.
    
    BUG=875004
    
    Change-Id: I757988d0090dfb9bb7974c8dad920c9d758c54f4
    Reviewed-on: https://chromium-review.googlesource.com/1188766
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588089}

--- content/browser/service_manager/service_manager_context.cc
+++ content/browser/service_manager/service_manager_context.cc
@@ -60,6 +60,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/data_decoder/public/mojom/constants.mojom.h"
 #include "services/device/device_service.h"
 #include "services/device/public/mojom/constants.mojom.h"
+#include "services/media_session/media_session_service.h"
+#include "services/media_session/public/cpp/switches.h"
+#include "services/media_session/public/mojom/constants.mojom.h"
 #include "services/metrics/metrics_mojo_service.h"
 #include "services/metrics/public/mojom/constants.mojom.h"
 #include "services/network/network_service.h"
@@ -555,6 +558,14 @@ ServiceManagerContext::ServiceManagerContext(
         base::Bind(&resource_coordinator::ResourceCoordinatorService::Create);
     packaged_services_connection_->AddEmbeddedService(
         resource_coordinator::mojom::kServiceName, resource_coordinator_info);
+  }
+
+  if (media_session::IsMediaSessionEnabled()) {
+    service_manager::EmbeddedServiceInfo media_session_info;
+    media_session_info.factory =
+        base::BindRepeating(&media_session::MediaSessionService::Create);
+    packaged_services_connection_->AddEmbeddedService(
+        media_session::mojom::kServiceName, media_session_info);
   }
 
   {
