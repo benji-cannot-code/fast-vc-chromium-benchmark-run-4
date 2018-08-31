b31ac98f07b10b2ff0954f1c07e9d117fc5c86ca
Sahel Sharify <sahel@chromium.org>
2018-08-31 13:31:02 +0000

    Fixed Android WebView fling jump.
    On Android WebView ui::compositor is not available and the fling progress
    happens by setting needs begin frame and calling progressFling on beginFrame.
    In this case calling ProgressFling right after processing fling start
    casues a jump in the beginning of fling since on the first frame after GFS
    two GSUs are sent: One on beginframe and another one on ProcessGestureFlingStart.
    On all other platforms this is not an issue since the GSU generated in
    onAnimationStep of frame n gets handled in frame n+1. So during the first
    frame after GFS only the GSU generated in ProcessGestureFlingStart causes
    scrolling.
    
    This cl checks to see if SetNeedsBeginFrame is used for scheduling fling
    progress calls and if that is the case, it skips calling ProgressFling after
    processing the GSF to make sure that only one GSU is sent in the first frame
    after processing of GFS.
    
    Bug: 868867
    Change-Id: I690c066034013faa62acbe740b574c5c40fe6bfa
    Tests: FlingControllerTest.*/1
    Reviewed-on: https://chromium-review.googlesource.com/1181521
    Commit-Queue: Sahel Sharify <sahel@chromium.org>
    Reviewed-by: Navid Zolghadr <nzolghadr@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588008}

--- content/browser/renderer_host/input/fling_scheduler_android.h
+++ content/browser/renderer_host/input/fling_scheduler_android.h
@@ -26,6 +26,7 @@ class CONTENT_EXPORT FlingSchedulerAndroid : public FlingSchedulerBase,
       base::WeakPtr<FlingController> fling_controller) override;
   void DidStopFlingingOnBrowser(
       base::WeakPtr<FlingController> fling_controller) override;
+  bool NeedsBeginFrameForFlingProgress() override;
 
   // FlingSchedulerBase
   void ProgressFlingOnBeginFrameIfneeded(base::TimeTicks current_time) override;
--- content/browser/renderer_host/input/gesture_event_queue_unittest.cc
+++ content/browser/renderer_host/input/gesture_event_queue_unittest.cc
@@ -108,6 +108,7 @@ class GestureEventQueueTest : public testing::Test,
       base::WeakPtr<FlingController> fling_controller) override {}
   void DidStopFlingingOnBrowser(
       base::WeakPtr<FlingController> fling_controller) override {}
+  bool NeedsBeginFrameForFlingProgress() override { return false; }
 
  protected:
   static GestureEventQueue::Config DefaultConfig() {
