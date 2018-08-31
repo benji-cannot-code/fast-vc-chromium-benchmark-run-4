9153dd98ec7969c161029d3e510944260124aac0
Miguel Casas <mcasas@chromium.org>
2018-08-31 03:14:15 +0000

    Canvas 2d: ignore RuntimeEnabledFeatures for LowLatency() cases
    ToT looks at RuntimeEnabledFeatures::Canvas2dImageChromiumEnabled()
    (enabled by default for Mac, disabled by default elsewhere) to use
    ImageCHROMIUM for Canvas 2D contexts or not.  This is fine, but in
    the LowLatency it doesn't make sense not use that features, we
    always want to use ImageCHROMIUM (if we |want_acceleration| at all).
    
    Hence this CL:
    - ignores the RuntimeEnabledFeature if LowLatency (so all platforms
     will use ImageCHROMIUM for LowLatency -- if |want_acceleration|).
    - removes the associated code in runtime_features.cc and about_flags.cc,
     essentially undoing my previous crrev.com/c/1056168. (So that Mac
     can still control the use of ImageCHROMIUM for non-low-latency cases).
    
    Bonus cleanup: simplify |enable_web_gl_image_chromium| initialization.
    
    Bug: 878405
    Change-Id: I91c5422fad82b1146af02c193546f924bbe5448b
    Reviewed-on: https://chromium-review.googlesource.com/1194473
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Reviewed-by: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587930}

--- third_party/blink/renderer/core/html/canvas/canvas_rendering_context_host.cc
+++ third_party/blink/renderer/core/html/canvas/canvas_rendering_context_host.cc
@@ -103,7 +103,8 @@ CanvasRenderingContextHost::GetOrCreateCanvasResourceProviderImpl(
                 : CanvasResourceProvider::kSoftwareCompositedResourceUsage;
 
         const CanvasResourceProvider::PresentationMode presentation_mode =
-            RuntimeEnabledFeatures::Canvas2dImageChromiumEnabled()
+            (RuntimeEnabledFeatures::Canvas2dImageChromiumEnabled() ||
+             (LowLatencyEnabled() && want_acceleration))
                 ? CanvasResourceProvider::kAllowImageChromiumPresentationMode
                 : CanvasResourceProvider::kDefaultPresentationMode;
 
