3696f889b7fa6057c03acd57e458c0dfe2f97827
Marina Ciocea <marinaciocea@chromium.org>
2018-08-31 20:23:25 +0000

    Disable launch services on when running audio service sandboxed on Mac.
    When running audio service sandboxed on Mac, the audio process is a daemon process
    and should not connect to launchservicesd.
    
    Bug: 874785
    Change-Id: I84ffe96fea85d3d54fb9b1cca1d441b5d6f085a1
    Reviewed-on: https://chromium-review.googlesource.com/1194073
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Marina Ciocea <marinaciocea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588146}

--- content/utility/utility_service_factory.cc
+++ content/utility/utility_service_factory.cc
@@ -44,6 +44,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif  // BUILDFLAG(ENABLE_CDM_HOST_VERIFICATION)
 #endif
 
+#if defined(OS_MACOSX)
+#include "sandbox/mac/system_services.h"
+#include "services/service_manager/sandbox/features.h"
+#endif
+
 #if defined(OS_WIN)
 #include "sandbox/win/src/sandbox.h"
 
@@ -204,6 +209,15 @@ UtilityServiceFactory::CreateNetworkService() {
 
 std::unique_ptr<service_manager::Service>
 UtilityServiceFactory::CreateAudioService() {
+#if defined(OS_MACOSX)
+  // Don't connect to launch services when running sandboxed
+  // (https://crbug.com/874785).
+  if (base::FeatureList::IsEnabled(
+          service_manager::features::kAudioServiceSandbox)) {
+    sandbox::DisableLaunchServices();
+  }
+#endif
+
   return audio::CreateStandaloneService(std::move(audio_registry_));
 }
 
