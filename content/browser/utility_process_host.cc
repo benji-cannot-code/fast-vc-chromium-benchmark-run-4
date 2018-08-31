ee713573f498424ec643443e253ea057e69d51cc
Findit <findit-for-me@appspot.gserviceaccount.com>
2018-08-31 19:35:30 +0000

    Revert "Sandbox audio service on Linux."
    This reverts commit 2ea45c062c0d59ed81ec5f185a23ffb39c34ff03.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 588100 as the
    culprit for failures in the build cycles as shown on:
    https://findit-for-me.appspot.com/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtLzJlYTQ1YzA2MmMwZDU5ZWQ4MWVjNWYxODVhMjNmZmIzOWMzNGZmMDMM
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium.linux/Linux%20Builder%20%28dbg%29%2832%29/87361
    
    Sample Failed Step: compile
    
    Original change's description:
    > Sandbox audio service on Linux.
    > 
    > Run audio service process in sandboxed mode on Linux. Sandbox is enabled behind finch experiment flag:
    > --enable-features=AudioServiceStreams,AudioServiceOutOfProcess,AudioServiceSandbox
    > 
    > TBR=mpdenton@chromium.org, palmer@chromium.org, tsepez@chromium.com
    > 
    > 
    > Bug: 850878
    > Change-Id: Iba6b84adc8c4a62ac4c20d5b36aa4f21b36a1574
    > Reviewed-on: https://chromium-review.googlesource.com/1160844
    > Commit-Queue: Marina Ciocea <marinaciocea@chromium.org>
    > Reviewed-by: Tom Sepez <tsepez@chromium.org>
    > Reviewed-by: Nasko Oskov <nasko@chromium.org>
    > Reviewed-by: Robert Sesek <rsesek@chromium.org>
    > Reviewed-by: Olga Sharonova <olka@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#588100}
    
    Change-Id: I64a6f98f7b2a89e47324a37bf8e91fda6caa0e7d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 850878
    Reviewed-on: https://chromium-review.googlesource.com/1199855
    Cr-Commit-Position: refs/heads/master@{#588121}

--- content/browser/utility_process_host.cc
+++ content/browser/utility_process_host.cc
@@ -106,8 +106,7 @@ class UtilitySandboxedProcessLauncherDelegate
 #if BUILDFLAG(USE_ZYGOTE_HANDLE)
   service_manager::ZygoteHandle GetZygote() override {
     if (service_manager::IsUnsandboxedSandboxType(sandbox_type_) ||
-        sandbox_type_ == service_manager::SANDBOX_TYPE_NETWORK ||
-        sandbox_type_ == service_manager::SANDBOX_TYPE_AUDIO) {
+        sandbox_type_ == service_manager::SANDBOX_TYPE_NETWORK) {
       return nullptr;
     }
     return service_manager::GetGenericZygote();
--- content/utility/utility_main.cc
+++ content/utility/utility_main.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/service_manager/sandbox/sandbox.h"
 
 #if defined(OS_LINUX)
-#include "services/audio/audio_sandbox_hook_linux.h"
 #include "services/network/network_sandbox_hook_linux.h"
 #include "services/service_manager/sandbox/linux/sandbox_linux.h"
 #endif
@@ -73,14 +72,10 @@ int UtilityMain(const MainFunctionParams& parameters) {
   auto sandbox_type =
       service_manager::SandboxTypeFromCommandLine(parameters.command_line);
   if (parameters.zygote_child ||
-      sandbox_type == service_manager::SANDBOX_TYPE_NETWORK ||
-      sandbox_type == service_manager::SANDBOX_TYPE_AUDIO) {
+      sandbox_type == service_manager::SANDBOX_TYPE_NETWORK) {
     service_manager::SandboxLinux::PreSandboxHook pre_sandbox_hook;
     if (sandbox_type == service_manager::SANDBOX_TYPE_NETWORK)
       pre_sandbox_hook = base::BindOnce(&network::NetworkPreSandboxHook);
-    else if (sandbox_type == service_manager::SANDBOX_TYPE_AUDIO)
-      pre_sandbox_hook = base::BindOnce(&audio::AudioPreSandboxHook);
-
     service_manager::Sandbox::Initialize(
         sandbox_type, std::move(pre_sandbox_hook),
         service_manager::SandboxLinux::Options());
