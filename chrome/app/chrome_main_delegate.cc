9bea989eea2d4bc8c1e98e927faae0ea445a0c08
Jan Wilken Dörrie <jdoerrie@chromium.org>
2018-08-31 13:50:16 +0000

    Revert "Do not build chrome/service on POSIX if CUPS is disabled."
    This reverts commit af6a592750ca823d5fef05e5bc8795145ecc22ba.
    
    Reason for revert: Likely culprit of https://crbug.com/879564
    
    Original change's description:
    > Do not build chrome/service on POSIX if CUPS is disabled.
    > 
    > The code, used for the Cloud Print Proxy, is useless without CUPS.
    > 
    > BUG=793801
    > 
    > Change-Id: Icb4605ae15d758366c5c0b1e52a56afcfe5334a1
    > Reviewed-on: https://chromium-review.googlesource.com/961862
    > Commit-Queue: Lei Zhang <thestig@chromium.org>
    > Reviewed-by: Sean Kau <skau@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#587921}
    
    TBR=thestig@chromium.org,skau@chromium.org
    
    Change-Id: Ia285e5ae6b9f72d8773bea5118fcfe4bff1d922a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 793801
    Reviewed-on: https://chromium-review.googlesource.com/1199582
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588012}

--- chrome/app/chrome_main_delegate.cc
+++ chrome/app/chrome_main_delegate.cc
@@ -175,7 +175,7 @@ base::LazyInstance<ChromeCrashReporterClient>::Leaky g_chrome_crash_client =
 
 extern int NaClMain(const content::MainFunctionParams&);
 
-#if defined(OS_WIN) || (defined(USE_CUPS) && !defined(OS_CHROMEOS))
+#if !defined(OS_CHROMEOS)
 extern int CloudPrintServiceProcessMain(const content::MainFunctionParams&);
 #endif
 
@@ -951,9 +951,13 @@ void ChromeMainDelegate::SandboxInitialized(const std::string& process_type) {
 int ChromeMainDelegate::RunProcess(
     const std::string& process_type,
     const content::MainFunctionParams& main_function_params) {
+// ANDROID doesn't support "service", so no CloudPrintServiceProcessMain, and
+// arraysize doesn't support empty array. So we comment out the block for
+// Android.
+#if !defined(OS_ANDROID)
   static const MainFunction kMainFunctions[] = {
 #if BUILDFLAG(ENABLE_PRINT_PREVIEW) && !defined(CHROME_MULTIPLE_DLL_CHILD) && \
-    (defined(OS_WIN) || (defined(USE_CUPS) && !defined(OS_CHROMEOS)))
+    !defined(OS_CHROMEOS)
     {switches::kCloudPrintServiceProcess, CloudPrintServiceProcessMain},
 #endif
 
@@ -967,8 +971,8 @@ int ChromeMainDelegate::RunProcess(
     !defined(OS_LINUX)
     {switches::kNaClLoaderProcess, NaClMain},
 #else
-    {"<invalid>", nullptr},  // To avoid constant array of size 0
-                             // when NaCl disabled and CHROME_MULTIPLE_DLL_CHILD
+    {"<invalid>", NULL},  // To avoid constant array of size 0
+                          // when NaCl disabled and CHROME_MULTIPLE_DLL_CHILD
 #endif
   };
 
@@ -976,6 +980,8 @@ int ChromeMainDelegate::RunProcess(
     if (process_type == kMainFunctions[i].name)
       return kMainFunctions[i].function(main_function_params);
   }
+#endif
+
   return -1;
 }
 
