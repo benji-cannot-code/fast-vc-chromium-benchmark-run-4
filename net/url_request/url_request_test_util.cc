6bfa64cec980416515aace5cd5127dd137e4a0dd
Clark DuVall <cduvall@chromium.org>
2018-08-31 17:26:34 +0000

    Reland "Unify ChromeNetworkDelegate and NetworkServiceNetworkDelegate cookie logic"
    This reverts commit 150c7353da386029eac1513f22257a3717ebf991.
    
    Added some more tests, and support for the special casing of chrome:// URLs.
    The chrome-extension:// case is only needed when network service is disabled,
    since chrome-extension:// requests only use the URLRequestContext in that case.
    
    See diff after Patchset 1 for changes since the original.
    
    Original change's description:
    > Revert "Unify ChromeNetworkDelegate and NetworkServiceNetworkDelegate cookie logic"
    >
    > This reverts commit 8420d58c08a2b1dd5a0a414e44284491a5518331.
    >
    > Reason for revert: This breaks the sign-in on the print dialog when third party
    > cookies are blocked: http://crbug.com/878051.
    > This is because the network service does not have the custom logic to whitelist
    > chrome:// or chrome-extensions:// schemes, which used to be here:
    > https://cs.chromium.org/chromium/src/components/content_settings/core/browser/cookie_settings.cc?l=98&rcl=5edbbd319ff4010b66e07ec404a3188716a9cda2
    >
    > The print dialog issues requests from chrome://print, which was now blocked from
    > using third party cookies.
    >
    > Original change's description:
    > > Unify ChromeNetworkDelegate and NetworkServiceNetworkDelegate cookie logic
    > >
    > > This was suggested in crbug.com/789636 so this logic can run in
    > > production before the launch of the network service.
    > >
    > > Had to keep the tab notification logic in each delegate, since the
    > > way NetworkServiceNetworkDelegate maps the request back to the
    > > original tab (using the URLLoader set as user data on the request)
    > > does not work if network service is disabled.
    > >
    > > Bug: 789636, 789632
    > > Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;luci.chromium.try:linux_mojo;master.tryserver.chromium.android:android_cronet_tester;master.tryserver.chromium.mac:ios-simulator-cronet
    > > Change-Id: I22395f914639d8ce83457a63062927e876caeaa9
    > > Reviewed-on: https://chromium-review.googlesource.com/1113903
    > > Reviewed-by: Matt Menke <mmenke@chromium.org>
    > > Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    > > Commit-Queue: Clark DuVall <cduvall@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#570877}
    >
    > Bug: 878051, 789636, 789632
    > Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs;luci.chromium.try:linux_mojo
    > Change-Id: Ib023acff943f992c8b6ec8b15bbdbc1524ed3720
    > Reviewed-on: https://chromium-review.googlesource.com/1194329
    > Commit-Queue: Clark DuVall <cduvall@chromium.org>
    > Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#586944}
    
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs;luci.chromium.try:linux_mojo;master.tryserver.chromium.android:android_cronet_tester
    Change-Id: Ia2b596baeacd1d25d99104db88403846e251f806
    Bug: 878051, 789636, 789632
    Reviewed-on: https://chromium-review.googlesource.com/1195147
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588072}

--- net/url_request/url_request_test_util.cc
+++ net/url_request/url_request_test_util.cc
@@ -654,8 +654,9 @@ NetworkDelegate::AuthRequiredResponse TestNetworkDelegate::OnAuthRequired(
 }
 
 bool TestNetworkDelegate::OnCanGetCookies(const URLRequest& request,
-                                          const CookieList& cookie_list) {
-  bool allow = true;
+                                          const CookieList& cookie_list,
+                                          bool allowed_from_caller) {
+  bool allow = allowed_from_caller;
   if (cookie_options_bit_mask_ & NO_GET_COOKIES)
     allow = false;
 
@@ -668,8 +669,9 @@ bool TestNetworkDelegate::OnCanGetCookies(const URLRequest& request,
 
 bool TestNetworkDelegate::OnCanSetCookie(const URLRequest& request,
                                          const net::CanonicalCookie& cookie,
-                                         CookieOptions* options) {
-  bool allow = true;
+                                         CookieOptions* options,
+                                         bool allowed_from_caller) {
+  bool allow = allowed_from_caller;
   if (cookie_options_bit_mask_ & NO_SET_COOKIE)
     allow = false;
 
--- services/network/network_service_unittest.cc
+++ services/network/network_service_unittest.cc
@@ -1406,15 +1406,15 @@ TEST_F(NetworkServiceNetworkChangeTest, MAYBE_NetworkChangeManagerRequest) {
   manager_client.WaitForNotification(mojom::ConnectionType::CONNECTION_3G);
 }
 
-class NetworkServiceClearSiteDataTest : public NetworkServiceTest {
+class NetworkServiceNetworkDelegateTest : public NetworkServiceTest {
  public:
-  NetworkServiceClearSiteDataTest() {
+  NetworkServiceNetworkDelegateTest() {
     // |NetworkServiceNetworkDelegate::HandleClearSiteDataHeader| requires
     // Network Service.
     scoped_feature_list_.InitAndEnableFeature(
         network::features::kNetworkService);
   }
-  ~NetworkServiceClearSiteDataTest() override = default;
+  ~NetworkServiceNetworkDelegateTest() override = default;
 
   void CreateNetworkContext() {
     mojom::NetworkContextParamsPtr context_params =
@@ -1460,7 +1460,7 @@ class NetworkServiceClearSiteDataTest : public NetworkServiceTest {
         net::test_server::EmbeddedTestServer::TYPE_HTTPS));
     https_server_->SetSSLConfig(net::EmbeddedTestServer::CERT_OK);
     https_server_->RegisterRequestHandler(base::BindRepeating(
-        &NetworkServiceClearSiteDataTest::HandleHTTPSRequest,
+        &NetworkServiceNetworkDelegateTest::HandleHTTPSRequest,
         base::Unretained(this)));
     ASSERT_TRUE(https_server_->Start());
   }
@@ -1495,7 +1495,7 @@ class NetworkServiceClearSiteDataTest : public NetworkServiceTest {
   mojom::URLLoaderPtr loader_;
   base::test::ScopedFeatureList scoped_feature_list_;
 
-  DISALLOW_COPY_AND_ASSIGN(NetworkServiceClearSiteDataTest);
+  DISALLOW_COPY_AND_ASSIGN(NetworkServiceNetworkDelegateTest);
 };
 
 class ClearSiteDataNetworkServiceClient : public TestNetworkServiceClient {
@@ -1548,7 +1548,7 @@ class ClearSiteDataNetworkServiceClient : public TestNetworkServiceClient {
 
 // Check that |NetworkServiceNetworkDelegate| handles Clear-Site-Data header
 // w/ and w/o |NetworkServiceCient|.
-TEST_F(NetworkServiceClearSiteDataTest, ClearSiteDataNetworkServiceCient) {
+TEST_F(NetworkServiceNetworkDelegateTest, ClearSiteDataNetworkServiceCient) {
   const char kClearCookiesHeader[] = "Clear-Site-Data: \"cookies\"";
   CreateNetworkContext();
 
@@ -1575,7 +1575,7 @@ TEST_F(NetworkServiceClearSiteDataTest, ClearSiteDataNetworkServiceCient) {
 }
 
 // Check that headers are handled and passed to the client correctly.
-TEST_F(NetworkServiceClearSiteDataTest, HandleClearSiteDataHeaders) {
+TEST_F(NetworkServiceNetworkDelegateTest, HandleClearSiteDataHeaders) {
   const char kClearCookiesHeaderValue[] = "\"cookies\"";
   const char kClearCookiesHeader[] = "Clear-Site-Data: \"cookies\"";
   CreateNetworkContext();
