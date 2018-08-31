1a98951e79d5737477356b1b56a9dd66310e0822
Matt Falkenhagen <falken@chromium.org>
2018-08-31 10:25:46 +0000

    service worker: Simplify ServiceWorkerNavigationLoader states.
    * Remove the Cancelled state so there is only one Completed state.
      The Cancellation state was previously reached by Mojo connection
      error. But it's not needed since WasCanceled() only gets called by
      ServiceWorkerControlleeRequestHandler prior to StartRequest(), which
      means the Mojo connection is not yet established.
    * Funnel "cancellation" to CommitCompleted() so there's less transitions
      to Completed.
    * Add a unit test about Mojo connection error while the fetch dispatcher
      is alive. Note that the current code passed this test also. The
      test would fail if the InvalidateWeakPtrs() and
      fetch_dispatcher_.reset() lines are removed from the connection
      error handler.
    
    Bug: 715640
    Change-Id: Ibac22ceea5dbc2c11ae7c7d3c47e21cfb9a6d393
    Reviewed-on: https://chromium-review.googlesource.com/1198650
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587982}

--- content/browser/service_worker/service_worker_navigation_loader.h
+++ content/browser/service_worker/service_worker_navigation_loader.h
@@ -90,7 +90,6 @@ class CONTENT_EXPORT ServiceWorkerNavigationLoader
   void ForwardToServiceWorker();
   bool ShouldFallbackToNetwork();
   bool ShouldForwardToServiceWorker();
-  bool WasCanceled() const;
 
   // The navigation request that was holding this job is
   // going away. Calling this internally calls |DeleteIfNeeded()|
@@ -103,6 +102,18 @@ class CONTENT_EXPORT ServiceWorkerNavigationLoader
 
  private:
   class StreamWaiter;
+  enum class Status {
+    kNotStarted,
+    // |binding_| is bound and the fetch event is being dispatched to the
+    // service worker.
+    kStarted,
+    // The response head has been sent to |url_loader_client_|. The response
+    // body is being streamed.
+    kSentHeader,
+    // OnComplete() was called on |url_loader_client_|, or fallback to network
+    // occurred so the request was not handled.
+    kCompleted,
+  };
 
   // For FORWARD_TO_SERVICE_WORKER case.
   void StartRequest(const network::ResourceRequest& resource_request,
@@ -145,6 +156,8 @@ class CONTENT_EXPORT ServiceWorkerNavigationLoader
   void ReportDestination(
       ServiceWorkerMetrics::MainResourceRequestDestination destination);
 
+  void TransitionToStatus(Status new_status);
+
   ResponseType response_type_ = ResponseType::NOT_DETERMINED;
   NavigationLoaderInterceptor::LoaderCallback loader_callback_;
   NavigationLoaderInterceptor::FallbackCallback fallback_callback_;
@@ -172,13 +185,6 @@ class CONTENT_EXPORT ServiceWorkerNavigationLoader
   network::mojom::URLLoaderClientPtr url_loader_client_;
   mojo::Binding<network::mojom::URLLoader> binding_;
 
-  enum class Status {
-    kNotStarted,
-    kStarted,
-    kSentHeader,
-    kCompleted,
-    kCancelled
-  };
   Status status_ = Status::kNotStarted;
 
   base::WeakPtrFactory<ServiceWorkerNavigationLoader> weak_factory_;
