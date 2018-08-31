96656dff931fa9199491defbae01cfeb5fe60989
Gauthier Ambard <gambard@chromium.org>
2018-08-31 09:47:27 +0000

    [iOS] Animate constraints when exiting fullscreen
    This CL animates the toolbar constraints change when animating the
    fullscreen final position (i.e. getting out or in).
    
    Bug: 876712
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: I58dc0c200138f9cf482e9ee25a76e2653665fbfe
    Reviewed-on: https://chromium-review.googlesource.com/1199063
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587977}

--- ios/chrome/browser/ui/toolbar/adaptive/primary_toolbar_view_controller.mm
+++ ios/chrome/browser/ui/toolbar/adaptive/primary_toolbar_view_controller.mm
@@ -204,6 +204,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   CGFloat finalProgress = animator.finalProgress;
   [animator addAnimations:^{
     [self updateForFullscreenProgress:finalProgress];
+    [self.view layoutIfNeeded];
   }];
 }
 
