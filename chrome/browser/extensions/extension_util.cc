952916f6311b49ebdcd1b55add240c19851c6242
Ben Wells <benwells@chromium.org>
2018-08-31 00:22:33 +0000

    Don't treat bookmark apps that aren't locally installed as PWAs.
    This means if a PWA is installed on one machine and synced to another
    machine such that it isn't locally installed (i.e. it is synced to a
    non-Chrome OS machine), it won't be treated as an installed PWA. This
    affects features such as the navigation options available.
    
    Bug: 874841
    Change-Id: I42667edbd7d9ac7dbe2a088d4b11bda422beee8b
    Reviewed-on: https://chromium-review.googlesource.com/1195382
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Commit-Queue: Ben Wells <benwells@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587874}

--- chrome/browser/extensions/extension_util.cc
+++ chrome/browser/extensions/extension_util.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/webui/extensions/extension_icon_source.h"
+#include "chrome/browser/web_applications/extensions/bookmark_app_util.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/extensions/api/url_handlers/url_handlers_parser.h"
@@ -342,6 +343,8 @@ const Extension* GetInstalledPwaForUrl(
   for (scoped_refptr<const Extension> app :
        ExtensionRegistry::Get(context)->enabled_extensions()) {
     if (!app->from_bookmark())
+      continue;
+    if (!BookmarkAppIsLocallyInstalled(prefs, app.get()))
       continue;
     if (launch_container_filter &&
         GetLaunchContainer(prefs, app.get()) != *launch_container_filter) {
