31d1a8b1804952813548848944e95c71074de670
Lei Zhang <thestig@chromium.org>
2018-08-31 02:21:40 +0000

    Clean up some chrome/renderer DEPS.
    - Make spellcheck rules inclusive instead of exclusive.
    - Push some PPAPI rules down into the pepper/ sub-directory.
    - Remove a rendundant rule.
    
    Change-Id: I8dfee499566bb37b11912b063d16fa788127bdc4
    Reviewed-on: https://chromium-review.googlesource.com/1197353
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Raymes Khoury <raymes@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587918}

--- chrome/renderer/chrome_content_renderer_client.cc
+++ chrome/renderer/chrome_content_renderer_client.cc
@@ -109,7 +109,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/media_buildflags.h"
 #include "net/base/net_errors.h"
 #include "ppapi/buildflags/buildflags.h"
-#include "ppapi/c/private/ppb_pdf.h"
 #include "ppapi/shared_impl/ppapi_switches.h"
 #include "printing/buildflags/buildflags.h"
 #include "services/service_manager/public/cpp/connector.h"
