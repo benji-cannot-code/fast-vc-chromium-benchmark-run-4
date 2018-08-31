3939fa943732f62dc79fc7cce87fc0919dde63e4
Mohamed Amir Yosef <mamir@chromium.org>
2018-08-31 11:04:33 +0000

    [Sync::USS] Implement GetStatusCountersForDebugging for Bookmarks
    Bug: 516866
    Change-Id: Ia9155a08997d453025ec3e46fd0a6db1c2ff804f
    Reviewed-on: https://chromium-review.googlesource.com/1196374
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587987}

--- components/sync_bookmarks/bookmark_model_type_processor.cc
+++ components/sync_bookmarks/bookmark_model_type_processor.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/bookmarks/browser/bookmark_utils.h"
 #include "components/sync/base/model_type.h"
 #include "components/sync/engine/commit_queue.h"
+#include "components/sync/engine/cycle/status_counters.h"
 #include "components/sync/engine/model_type_processor_proxy.h"
 #include "components/sync/model/data_type_activation_request.h"
 #include "components/sync/protocol/bookmark_model_metadata.pb.h"
@@ -351,7 +352,13 @@ void BookmarkModelTypeProcessor::GetAllNodesForDebugging(
 void BookmarkModelTypeProcessor::GetStatusCountersForDebugging(
     StatusCountersCallback callback) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  NOTIMPLEMENTED();
+  DCHECK(bookmark_tracker_);
+  syncer::StatusCounters counters;
+  counters.num_entries = bookmark_tracker_->TrackedBookmarksCountForDebugging();
+  counters.num_entries_and_tombstones =
+      counters.num_entries +
+      bookmark_tracker_->TrackedUncommittedTombstonesCountForDebugging();
+  std::move(callback).Run(syncer::BOOKMARKS, counters);
 }
 
 void BookmarkModelTypeProcessor::RecordMemoryUsageAndCountsHistograms() {
