4b423c1ee01adb012c39cc48c8cf1145530d019d
Rakina Zata Amni <rakina@chromium.org>
2018-08-31 11:35:40 +0000

    Bring back moreStyleSheets as adoptedStyleSheets
    After discussions regarding https://github.com/WICG/construct-stylesheets,
    we are actually interested in having a way to style TreeScopes
    with constructed CSSStyleSheet. This CL brings back moreStyleSheets,
    renaming it as adoptedStylSheets (was deleted in crrev.com/c/1125741
    and crrev.com/c/1155286). No additional behavior change from previous
    implementation. Other changes to the spec will be added in separate CL.
    
    Draft spec at: https://wicg.github.io/construct-stylesheets/
    Previous implementation CLs: crrev.com/c/925763, crrev.com/c/938872
    
    Bug: 807560
    Change-Id: I65b4de496c0a70566d293374249816643456d8b9
    Reviewed-on: https://chromium-review.googlesource.com/1197002
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587993}

--- third_party/blink/renderer/core/css/document_style_sheet_collection.cc
+++ third_party/blink/renderer/core/css/document_style_sheet_collection.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/style_change_reason.h"
 #include "third_party/blink/renderer/core/css/style_engine.h"
 #include "third_party/blink/renderer/core/css/style_sheet_candidate.h"
+#include "third_party/blink/renderer/core/css/style_sheet_list.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/processing_instruction.h"
 #include "third_party/blink/renderer/core/style/computed_style.h"
@@ -85,6 +86,24 @@ void DocumentStyleSheetCollection::CollectStyleSheetsFromCandidates(
       continue;
 
     CSSStyleSheet* css_sheet = ToCSSStyleSheet(sheet);
+    collector.AppendActiveStyleSheet(
+        std::make_pair(css_sheet, master_engine.RuleSetForSheet(*css_sheet)));
+  }
+  if (!GetTreeScope().HasAdoptedStyleSheets())
+    return;
+
+  StyleSheetList& adopted_style_sheets = GetTreeScope().AdoptedStyleSheets();
+  unsigned length = adopted_style_sheets.length();
+  for (unsigned index = 0; index < length; ++index) {
+    StyleSheet* sheet = adopted_style_sheets.item(index);
+    if (!sheet)
+      continue;
+    CSSStyleSheet* css_sheet = ToCSSStyleSheet(sheet);
+    if (!css_sheet ||
+        !css_sheet->CanBeActivated(
+            GetDocument().GetStyleEngine().PreferredStylesheetSetName()))
+      continue;
+    collector.AppendSheetForList(sheet);
     collector.AppendActiveStyleSheet(
         std::make_pair(css_sheet, master_engine.RuleSetForSheet(*css_sheet)));
   }
