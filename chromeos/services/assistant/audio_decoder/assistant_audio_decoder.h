c43f82c25f8437a03bc47e2cd46ca4fcb04db59c
wutao <wutao@chromium.org>
2018-08-31 02:11:38 +0000

    Assistant: Decodes streaming data - client side
    This patch implements the decoding client side for streaming data.
    
    Bug: b/113131759, b/80315134
    Test: manual.
    Change-Id: I2ae58ef3d3c368ba02891db14b0a39f62155f8a9
    Reviewed-on: https://chromium-review.googlesource.com/1195145
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587915}

--- chromeos/services/assistant/audio_decoder/assistant_audio_decoder.h
+++ chromeos/services/assistant/audio_decoder/assistant_audio_decoder.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/macros.h"
-#include "base/synchronization/lock.h"
 #include "chromeos/services/assistant/public/mojom/assistant_audio_decoder.mojom.h"
 
 namespace media {
