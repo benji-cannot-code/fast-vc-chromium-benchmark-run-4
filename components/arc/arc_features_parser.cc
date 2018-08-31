b4ff5287c3fac58c3dc528f8e308bd1e62548d11
Etienne Pierre-doray <etiennep@chromium.org>
2018-08-31 15:27:11 +0000

    [TaskScheduler]: Use ScopedBlockingCall to mark blocking tasks.
    This CL uses ScopedBlockingCall to mark blocking calls in /components/arc.
    
    This CL was created by replacing calls to AssertBlockingAllowed()
    with instantiations of ScopedBlockingCall(MAY_BLOCK).
    I kindly ask the reviewer to make sure of the following:
      - ScopedBlockingCall is instantiated in a scope with minimal CPU usage.
        If this is not the case, ScopedBlockingCall should be instantiated
        closer to the blocking call. See scoped_blocking_call.h for more
        info. Please let me know when/where the blocking call happens if this needs
        to be changed.
      - Parameter |blocking_type| matches expectation (MAY_BLOCK/WILL_BLOCK). See
        BlockingType for more info. While I assumed MAY_BLOCK by default, that might
        not be the best fit if we know that this callsite is guaranteed to block.
      - The ScopedBlockingCall's scope covers the entirety of the blocking operation
        previously asserted against by the AssertBlockingAllowed().
    
    This CL was uploaded by git cl split.
    
    R=hidehiko@chromium.org
    
    Bug: 874080
    Change-Id: Ia1f51ac98e70110b6c405a8a56e792b6a0ff1aca
    Reviewed-on: https://chromium-review.googlesource.com/1190928
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588027}

--- components/arc/arc_features_parser.cc
+++ components/arc/arc_features_parser.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/task/post_task.h"
-#include "base/threading/thread_restrictions.h"
+#include "base/threading/scoped_blocking_call.h"
 #include "base/values.h"
 
 namespace arc {
@@ -110,12 +110,17 @@ base::Optional<ArcFeatures> ParseFeaturesJson(base::StringPiece input_json) {
 
 base::Optional<ArcFeatures> ReadOnFileThread(const base::FilePath& file_path) {
   DCHECK(!file_path.empty());
-  base::AssertBlockingAllowed();
+  base::ScopedBlockingCall scoped_blocking_call(base::BlockingType::MAY_BLOCK);
 
   std::string input_json;
-  if (!base::ReadFileToString(file_path, &input_json)) {
-    PLOG(ERROR) << "Cannot read file " << file_path.value() << " into string.";
-    return base::nullopt;
+  {
+    base::ScopedBlockingCall scoped_blocking_call(
+        base::BlockingType::MAY_BLOCK);
+    if (!base::ReadFileToString(file_path, &input_json)) {
+      PLOG(ERROR) << "Cannot read file " << file_path.value()
+                  << " into string.";
+      return base::nullopt;
+    }
   }
 
   if (input_json.empty()) {
