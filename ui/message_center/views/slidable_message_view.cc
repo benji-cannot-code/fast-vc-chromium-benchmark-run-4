b6cfd892e838f65acdf4c6193babc90dc96227c8
Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
2018-08-31 13:03:21 +0000

    Reland of "Add swipe control buttons."
    Description of the original change:
    > Shows buttons when swipe control is opened by horizontal scroll of a
    > notification box. The buttons triggers same action just like the
    > existing control buttons inside MessageView for mouse.
    > Reviewed-on: https://chromium-review.googlesource.com/1188044
    
    The original change was reverted once for reverting the breaking change
    on which this is based on.  16ab50cc3 crrev.com/c/1184724
    
    Test: ash_unittests --gtest_filter=MessageCenterViewTest.*:MessageListViewTest.*
    Test: manual
    Bug: 840497,877983
    Change-Id: I3a03f7a1386c1a7f484a2442e0e39dfdb9a8840b
    Reviewed-on: https://chromium-review.googlesource.com/1193082
    Commit-Queue: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588004}

--- ui/message_center/views/slidable_message_view.cc
+++ ui/message_center/views/slidable_message_view.cc
@@ -8,6 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/message_center/public/cpp/features.h"
 #include "ui/message_center/public/cpp/message_center_constants.h"
 #include "ui/message_center/views/message_view.h"
+#include "ui/message_center/views/notification_background_painter.h"
+#include "ui/message_center/views/notification_control_buttons_view.h"
+#include "ui/message_center/views/notification_swipe_control_view.h"
 #include "ui/views/background.h"
 #include "ui/views/layout/fill_layout.h"
 #include "ui/views/view.h"
@@ -26,7 +29,11 @@ SlidableMessageView::SlidableMessageView(MessageView* message_view)
 
   SetLayoutManager(std::make_unique<views::FillLayout>());
 
-  // TODO(crbug.com/840497): Add button container child view.
+  if (base::FeatureList::IsEnabled(message_center::kNotificationSwipeControl)) {
+    control_view_ = std::make_unique<NotificationSwipeControlView>();
+    AddChildView(control_view_.get());
+    control_view_->AddObserver(this);
+  }
 
   message_view_->AddSlideObserver(this);
   AddChildView(message_view);
@@ -35,7 +42,31 @@ SlidableMessageView::SlidableMessageView(MessageView* message_view)
 SlidableMessageView::~SlidableMessageView() = default;
 
 void SlidableMessageView::OnSlideChanged(const std::string& notification_id) {
-  // TODO(crbug.com/840497): Show/hide control buttons.
+  if (!base::FeatureList::IsEnabled(message_center::kNotificationSwipeControl))
+    return;
+  float gesture_amount = message_view_->GetSlideAmount();
+  if (gesture_amount == 0) {
+    control_view_->HideButtons();
+  } else {
+    bool on_right = gesture_amount < 0;
+    NotificationControlButtonsView* buttons =
+        message_view_->GetControlButtonsView();
+    bool has_settings_button = buttons->settings_button();
+    bool has_snooze_button = buttons->snooze_button();
+    control_view_->ShowButtons(on_right, has_settings_button,
+                               has_snooze_button);
+  }
+}
+
+void SlidableMessageView::OnSettingsButtonPressed(const ui::Event& event) {
+  message_view_->CloseSwipeControl();
+  message_view_->OnSettingsButtonPressed(event);
+  control_view_->HideButtons();
+}
+
+void SlidableMessageView::OnSnoozeButtonPressed(const ui::Event& event) {
+  message_view_->OnSnoozeButtonPressed(event);
+  control_view_->HideButtons();
 }
 
 void SlidableMessageView::CloseSwipeControl() {
@@ -63,6 +94,27 @@ gfx::Size SlidableMessageView::CalculatePreferredSize() const {
 
 int SlidableMessageView::GetHeightForWidth(int width) const {
   return message_view_->GetHeightForWidth(width);
+}
+
+void SlidableMessageView::UpdateCornerRadius(int top_radius,
+                                             int bottom_radius) {
+  // No need to update because the background is transparent when disabled.
+  if (!base::FeatureList::IsEnabled(message_center::kNotificationSwipeControl))
+    return;
+  SetBackground(views::CreateBackgroundFromPainter(
+      std::make_unique<NotificationBackgroundPainter>(
+          top_radius, bottom_radius, kSwipeControlBackgroundColor)));
+  SchedulePaint();
+}
+
+// static
+SlidableMessageView* SlidableMessageView::GetFromMessageView(
+    MessageView* message_view) {
+  DCHECK(message_view);
+  DCHECK(message_view->parent());
+  DCHECK_EQ(std::string(SlidableMessageView::kViewClassName),
+            message_view->parent()->GetClassName());
+  return static_cast<SlidableMessageView*>(message_view->parent());
 }
 
 }  // namespace message_center
