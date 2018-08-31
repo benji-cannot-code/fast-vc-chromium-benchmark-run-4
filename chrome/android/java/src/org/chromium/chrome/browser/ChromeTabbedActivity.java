d8ab06eab7937825bab017a9552e6d13c229e4d7
Brandon Wylie <wylieb@chromium.org>
2018-08-31 21:35:44 +0000

    [EOC] Pause/resume pageviewtimer on tabswitcher events
    Bug: 875088
    Change-Id: I5c09daa610bba32c8be8b411e1a9e333697e64f9
    Reviewed-on: https://chromium-review.googlesource.com/1196163
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588173}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -67,6 +67,7 @@ import org.chromium.chrome.browser.compositor.layouts.LayoutManagerChromePhone;
 import org.chromium.chrome.browser.compositor.layouts.LayoutManagerChromeTablet;
 import org.chromium.chrome.browser.compositor.layouts.OverviewModeBehavior.OverviewModeObserver;
 import org.chromium.chrome.browser.compositor.layouts.phone.StackLayout;
+import org.chromium.chrome.browser.contextual_suggestions.PageViewTimer;
 import org.chromium.chrome.browser.cookies.CookiesFetcher;
 import org.chromium.chrome.browser.crypto.CipherFactory;
 import org.chromium.chrome.browser.device.DeviceClassManager;
@@ -2430,5 +2431,10 @@ public class ChromeTabbedActivity
                         getActivityTab(), FeatureConstants.DOWNLOAD_PAGE_SCREENSHOT_FEATURE);
             }
         });
+    }
+
+    @Override
+    protected PageViewTimer createPageViewTimer() {
+        return new PageViewTimer(mTabModelSelectorImpl, mLayoutManager);
     }
 }
