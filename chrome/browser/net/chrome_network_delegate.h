65e7955b07cc6ae37a38b1055be138903c5984b2
Eric Roman <eroman@chromium.org>
2018-09-01 03:37:46 +0000

    Make chrome.proxy.onProxyError extension API work under the Network Service.
    This adds an optional "ProxyErrorClient" client to the NetworkContext which
    receives notifications of errors in the PAC script, as well as notifications
    of failed URL loads that may have been proxy related.
    
    (Reland of d576f34af2134e4205788845247d5d52063fee1f).
    
    Bug: 876568,851609,879491
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Change-Id: I08987d658879913d94c9b13b6fdc1de2a32a6d54
    Reviewed-on: https://chromium-review.googlesource.com/1200076
    Commit-Queue: Eric Roman <eroman@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588259}

--- chrome/browser/net/chrome_network_delegate.h
+++ chrome/browser/net/chrome_network_delegate.h
@@ -132,7 +132,6 @@ class ChromeNetworkDelegate : public net::NetworkDelegateImpl {
                    bool started,
                    int net_error) override;
   void OnURLRequestDestroyed(net::URLRequest* request) override;
-  void OnPACScriptError(int line_number, const base::string16& error) override;
   net::NetworkDelegate::AuthRequiredResponse OnAuthRequired(
       net::URLRequest* request,
       const net::AuthChallengeInfo& auth_info,
