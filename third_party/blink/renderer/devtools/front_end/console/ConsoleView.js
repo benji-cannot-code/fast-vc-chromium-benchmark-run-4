175e37fff11935d3887350ae93ea2d6cd35678a3
Erik Luo <luoe@chromium.org>
2018-09-01 01:08:07 +0000

    DevTools: allow Console message keyboard navigation
    This makes Consoles messages focusable and navigable using:
    ArrowUp/Down/Left/Right, Home, End
    
    Screenshot: https://imgur.com/a/6JWY0iU
    
    Bug: 865674
    Change-Id: I4166bf4d8d57856c6d1f7d51ef29e2dea0c088eb
    Reviewed-on: https://chromium-review.googlesource.com/1144453
    Commit-Queue: Erik Luo <luoe@chromium.org>
    Reviewed-by: Joel Einbinder <einbinder@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588237}

--- third_party/blink/renderer/devtools/front_end/console/ConsoleView.js
+++ third_party/blink/renderer/devtools/front_end/console/ConsoleView.js
@@ -628,6 +628,7 @@ Console.ConsoleView = class extends UI.VBox {
   }
 
   _consoleCleared() {
+    const hadFocus = this._viewport.element.hasFocus();
     this._cancelBuildHiddenCache();
     this._currentMatchRangeIndex = -1;
     this._consoleMessages = [];
@@ -640,6 +641,8 @@ Console.ConsoleView = class extends UI.VBox {
     this._linkifier.reset();
     this._badgePool.reset();
     this._filter.clear();
+    if (hadFocus)
+      this._prompt.focus();
   }
 
   _handleContextMenuEvent(event) {
--- third_party/blink/renderer/devtools/front_end/console/ConsoleViewport.js
+++ third_party/blink/renderer/devtools/front_end/console/ConsoleViewport.js
@@ -59,6 +59,14 @@ Console.ConsoleViewport = class {
     this.element.addEventListener('scroll', this._onScroll.bind(this), false);
     this.element.addEventListener('copy', this._onCopy.bind(this), false);
     this.element.addEventListener('dragstart', this._onDragStart.bind(this), false);
+    this._keyboardNavigationEnabled = Runtime.experiments.isEnabled('consoleKeyboardNavigation');
+    if (this._keyboardNavigationEnabled) {
+      this.element.addEventListener('focusin', this._onFocusIn.bind(this), false);
+      this.element.addEventListener('focusout', this._onFocusOut.bind(this), false);
+      this.element.addEventListener('keydown', this._onKeyDown.bind(this), false);
+    }
+    this._virtualSelectedIndex = -1;
+    this.element.tabIndex = -1;
 
     this._firstActiveIndex = -1;
     this._lastActiveIndex = -1;
@@ -116,6 +124,35 @@ Console.ConsoleViewport = class {
   /**
    * @param {!Event} event
    */
+  _onFocusIn(event) {
+    // Make default selection when moving from external (e.g. prompt) to the container.
+    if (this._virtualSelectedIndex === -1 && this._isOutsideViewport(/** @type {?Element} */ (event.relatedTarget)) &&
+        event.target === this.element)
+      this._virtualSelectedIndex = this._itemCount - 1;
+    this._updateFocusedItem();
+  }
+
+  /**
+   * @param {!Event} event
+   */
+  _onFocusOut(event) {
+    // Remove selection when focus moves to external location (e.g. prompt).
+    if (this._isOutsideViewport(/** @type {?Element} */ (event.relatedTarget)))
+      this._virtualSelectedIndex = -1;
+    this._updateFocusedItem();
+  }
+
+  /**
+   * @param {?Element} element
+   * @return {boolean}
+   */
+  _isOutsideViewport(element) {
+    return !!element && (element !== this.element && !element.isDescendant(this._contentElement));
+  }
+
+  /**
+   * @param {!Event} event
+   */
   _onDragStart(event) {
     const text = this._selectedText();
     if (!text)
@@ -124,6 +161,63 @@ Console.ConsoleViewport = class {
     event.dataTransfer.setData('text/plain', text);
     event.dataTransfer.effectAllowed = 'copy';
     return true;
+  }
+
+  /**
+   * @param {!Event} event
+   */
+  _onKeyDown(event) {
+    if (UI.isEditing() || !this._itemCount || this.element.hasSelection())
+      return;
+    switch (event.key) {
+      case 'ArrowUp':
+        this._virtualSelectedIndex--;
+        if (this._virtualSelectedIndex < 0)
+          this._virtualSelectedIndex = this._itemCount - 1;
+        break;
+      case 'ArrowDown':
+        this._virtualSelectedIndex++;
+        if (this._virtualSelectedIndex >= this._itemCount)
+          this._virtualSelectedIndex = 0;
+        break;
+      case 'Home':
+        this._virtualSelectedIndex = 0;
+        break;
+      case 'End':
+        this._virtualSelectedIndex = this._itemCount - 1;
+        break;
+      default:
+        return;
+    }
+    event.consume(true);
+    this.scrollItemIntoView(this._virtualSelectedIndex);
+    this._updateFocusedItem();
+  }
+
+  _updateFocusedItem() {
+    const selectedElement = this.renderedElementAt(this._virtualSelectedIndex);
+    const changed = this._lastSelectedElement !== selectedElement;
+    const containerHasFocus = this.element === this.element.ownerDocument.deepActiveElement();
+    if (this._lastSelectedElement && changed)
+      this._lastSelectedElement.classList.remove('console-selected');
+    if (selectedElement && (changed || containerHasFocus)) {
+      selectedElement.classList.add('console-selected');
+      focusWithoutScroll(selectedElement);
+    }
+    if (this._itemCount && !this._contentElement.hasFocus())
+      this.element.tabIndex = 0;
+    else
+      this.element.tabIndex = -1;
+    this._lastSelectedElement = selectedElement;
+
+    /**
+     * @suppress {checkTypes}
+     * @param {!Element} element
+     */
+    function focusWithoutScroll(element) {
+      // TODO(luoe): Closure has an outdated typedef for Element.prototype.focus.
+      element.focus({preventScroll: true});
+    }
   }
 
   /**
@@ -136,6 +230,8 @@ Console.ConsoleViewport = class {
   invalidate() {
     delete this._cachedProviderElements;
     this._itemCount = this._provider.itemCount();
+    if (this._virtualSelectedIndex > this._itemCount - 1)
+      this._virtualSelectedIndex = this._itemCount - 1;
     this._rebuildCumulativeHeights();
     this.refresh();
   }
@@ -344,6 +440,8 @@ Console.ConsoleViewport = class {
       this._bottomGapElement.style.height = '0px';
       this._firstActiveIndex = -1;
       this._lastActiveIndex = -1;
+      if (this._keyboardNavigationEnabled)
+        this._updateFocusedItem();
       return;
     }
 
@@ -405,8 +503,12 @@ Console.ConsoleViewport = class {
     for (let i = 0; i < willBeHidden.length; ++i)
       willBeHidden[i].willHide();
     prepare();
-    for (let i = 0; i < willBeHidden.length; ++i)
+    let hadFocus = false;
+    for (let i = 0; i < willBeHidden.length; ++i) {
+      if (this._keyboardNavigationEnabled)
+        hadFocus = hadFocus || willBeHidden[i].element().hasFocus();
       willBeHidden[i].element().remove();
+    }
 
     const wasShown = [];
     let anchor = this._contentElement.firstChild;
@@ -424,6 +526,12 @@ Console.ConsoleViewport = class {
     for (let i = 0; i < wasShown.length; ++i)
       wasShown[i].wasShown();
     this._renderedItems = Array.from(itemsToRender);
+
+    if (this._keyboardNavigationEnabled) {
+      if (hadFocus)
+        this.element.focus();
+      this._updateFocusedItem();
+    }
   }
 
   /**
@@ -531,9 +639,7 @@ Console.ConsoleViewport = class {
    * @return {?Element}
    */
   renderedElementAt(index) {
-    if (index < this._firstActiveIndex)
-      return null;
-    if (index > this._lastActiveIndex)
+    if (index === -1 || index < this._firstActiveIndex || index > this._lastActiveIndex)
       return null;
     return this._renderedItems[index - this._firstActiveIndex].element();
   }
