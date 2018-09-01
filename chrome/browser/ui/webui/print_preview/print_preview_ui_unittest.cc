f6e0ac93b8c58958496ddfc92ec568713705939f
Lei Zhang <thestig@chromium.org>
2018-09-01 05:30:27 +0000

    Combine calls to PrintPreviewUI methods.
    Encapsulate ClearAllPreviewData() and SetPrintPreviewDataForIndex().
    
    Change-Id: I0ad046aef0ffcf098cc783bc177a8597c1c973c5
    Reviewed-on: https://chromium-review.googlesource.com/1188778
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588269}

--- chrome/browser/ui/webui/print_preview/print_preview_ui_unittest.cc
+++ chrome/browser/ui/webui/print_preview/print_preview_ui_unittest.cc
@@ -97,9 +97,8 @@ TEST_F(PrintPreviewUIUnitTest, PrintPreviewData) {
 
   scoped_refptr<base::RefCountedBytes> dummy_data = CreateTestData();
 
-  preview_ui->SetPrintPreviewDataForIndex(
-      printing::COMPLETE_PREVIEW_DOCUMENT_INDEX,
-      dummy_data.get());
+  preview_ui->SetPrintPreviewDataForIndexForTest(
+      printing::COMPLETE_PREVIEW_DOCUMENT_INDEX, dummy_data.get());
   preview_ui->GetPrintPreviewDataForIndex(
       printing::COMPLETE_PREVIEW_DOCUMENT_INDEX,
       &data);
@@ -107,7 +106,7 @@ TEST_F(PrintPreviewUIUnitTest, PrintPreviewData) {
   EXPECT_EQ(dummy_data.get(), data.get());
 
   // Clear the preview data.
-  preview_ui->ClearAllPreviewData();
+  preview_ui->ClearAllPreviewDataForTest();
 
   preview_ui->GetPrintPreviewDataForIndex(
       printing::COMPLETE_PREVIEW_DOCUMENT_INDEX,
@@ -143,15 +142,15 @@ TEST_F(PrintPreviewUIUnitTest, PrintPreviewDraftPages) {
 
   scoped_refptr<base::RefCountedBytes> dummy_data = CreateTestData();
 
-  preview_ui->SetPrintPreviewDataForIndex(printing::FIRST_PAGE_INDEX,
-                                          dummy_data.get());
+  preview_ui->SetPrintPreviewDataForIndexForTest(printing::FIRST_PAGE_INDEX,
+                                                 dummy_data.get());
   preview_ui->GetPrintPreviewDataForIndex(printing::FIRST_PAGE_INDEX, &data);
   EXPECT_EQ(dummy_data->size(), data->size());
   EXPECT_EQ(dummy_data.get(), data.get());
 
   // Set and get the third page data.
-  preview_ui->SetPrintPreviewDataForIndex(printing::FIRST_PAGE_INDEX + 2,
-                                          dummy_data.get());
+  preview_ui->SetPrintPreviewDataForIndexForTest(printing::FIRST_PAGE_INDEX + 2,
+                                                 dummy_data.get());
   preview_ui->GetPrintPreviewDataForIndex(printing::FIRST_PAGE_INDEX + 2,
                                           &data);
   EXPECT_EQ(dummy_data->size(), data->size());
@@ -162,15 +161,15 @@ TEST_F(PrintPreviewUIUnitTest, PrintPreviewDraftPages) {
                                           &data);
   EXPECT_FALSE(data);
 
-  preview_ui->SetPrintPreviewDataForIndex(printing::FIRST_PAGE_INDEX + 1,
-                                          dummy_data.get());
+  preview_ui->SetPrintPreviewDataForIndexForTest(printing::FIRST_PAGE_INDEX + 1,
+                                                 dummy_data.get());
   preview_ui->GetPrintPreviewDataForIndex(printing::FIRST_PAGE_INDEX + 1,
                                           &data);
   EXPECT_EQ(dummy_data->size(), data->size());
   EXPECT_EQ(dummy_data.get(), data.get());
 
   // Clear the preview data.
-  preview_ui->ClearAllPreviewData();
+  preview_ui->ClearAllPreviewDataForTest();
   preview_ui->GetPrintPreviewDataForIndex(printing::FIRST_PAGE_INDEX, &data);
   EXPECT_FALSE(data);
 }
