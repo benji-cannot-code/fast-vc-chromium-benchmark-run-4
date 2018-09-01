738bb63208c037f54f591bd13a13d94851779642
Lei Zhang <thestig@chromium.org>
2018-09-01 05:45:07 +0000

    Validate page numbers sent to PrintPreviewUI.
    While generating the print preview, the renderer now must send the list
    of page numbers in the DidStartPreview message. The browser can then use
    the list to validate the DidPreviewPage messages.
    
    Also combine calls to PrintPreviewUI methods to encapsulate more.
    
    Change-Id: If50199d14a61fa60e5e657fab6459fa80a7f0d88
    Reviewed-on: https://chromium-review.googlesource.com/1188242
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588271}

--- chrome/browser/ui/webui/print_preview/print_preview_ui.h
+++ chrome/browser/ui/webui/print_preview/print_preview_ui.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include <memory>
+#include <vector>
 
 #include "base/callback_forward.h"
 #include "base/gtest_prod_util.h"
@@ -87,6 +88,11 @@ class PrintPreviewUI : public ConstrainedWebDialogUI {
                                  const gfx::Rect& printable_area,
                                  bool has_custom_page_size_style,
                                  int request_id);
+
+  // Notifies the Web UI that the 0-based page |page_number| rendering is being
+  // processed and an OnPendingPreviewPage() call is imminent. Returns whether
+  // |page_number| is the expected page.
+  bool OnPendingPreviewPage(int page_number);
 
   // Notifies the Web UI that the 0-based page |page_number| has been rendered.
   // |preview_request_id| indicates which request resulted in this response.
@@ -210,6 +216,12 @@ class PrintPreviewUI : public ConstrainedWebDialogUI {
   // Store the initiator title, used for populating the print preview dialog
   // title.
   base::string16 initiator_title_;
+
+  // The list of 0-based page numbers that will be rendered.
+  std::vector<int> pages_to_render_;
+
+  // Index into |pages_to_render_| for the page number to expect.
+  size_t pages_to_render_index_ = 0;
 
   DISALLOW_COPY_AND_ASSIGN(PrintPreviewUI);
 };
