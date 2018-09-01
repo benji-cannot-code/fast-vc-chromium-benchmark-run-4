ab65344f2971ddd10d8421765d36cabab9f87f5c
Xida Chen <xidachen@chromium.org>
2018-09-01 02:38:29 +0000

    Add RecordHitTestData in LayoutReplacedPainter
    This CL adds RecordHitTestData in LayoutReplacedPainter. Two layout
    tests are added for SVG root and HTML image element cases.
    
    Bug: 876468
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Iea6a2318bcd7029b252ebccc92d62d93ad0b6c23
    Reviewed-on: https://chromium-review.googlesource.com/1196914
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588250}

--- third_party/blink/renderer/core/paint/replaced_painter.h
+++ third_party/blink/renderer/core/paint/replaced_painter.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_PAINT_REPLACED_PAINTER_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_PAINT_REPLACED_PAINTER_H_
 
+#include "third_party/blink/renderer/platform/geometry/layout_point.h"
 #include "third_party/blink/renderer/platform/wtf/allocator.h"
 
 namespace blink {
@@ -26,6 +27,10 @@ class ReplacedPainter {
   bool ShouldPaint(const PaintInfoWithOffset&) const;
 
  private:
+  // Paint a hit test display item and record hit test data. This should be
+  // called in the background paint phase even if there is no other painted
+  // content.
+  void RecordHitTestData(const PaintInfo&, const LayoutPoint& paint_offset);
   const LayoutReplaced& layout_replaced_;
 };
 
