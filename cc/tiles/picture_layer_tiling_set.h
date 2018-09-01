d55e009d69c04938d1d5f579d36eb0eb41f87af4
Victor Miura <vmiura@chromium.org>
2018-09-01 01:12:35 +0000

    Optimize PictureLayerTilingSet::visible_rect_history_ type.
    Appending entries to visible_rect_history was a hotspot in profiling.
    Changing to std::deque, and re-ordering the update code so the deque
    only grows to 2 entries rather than 3.
    
    BUG=879668
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I576ba0ae278a04a0e7336b3b1cd589fadd25b177
    Reviewed-on: https://chromium-review.googlesource.com/1200466
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Commit-Queue: Victor Miura <vmiura@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588238}

--- cc/tiles/picture_layer_tiling_set.h
+++ cc/tiles/picture_layer_tiling_set.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 
-#include <list>
+#include <deque>
 #include <set>
 #include <vector>
 
@@ -251,8 +251,8 @@ class CC_EXPORT PictureLayerTilingSet {
   PictureLayerTilingClient* client_;
   const float max_preraster_distance_;
   // State saved for computing velocities based on finite differences.
-  // .front() of the list refers to the most recent FrameVisibleRect.
-  std::list<FrameVisibleRect> visible_rect_history_;
+  // .front() of the deque refers to the most recent FrameVisibleRect.
+  std::deque<FrameVisibleRect> visible_rect_history_;
   StateSinceLastTilePriorityUpdate state_since_last_tile_priority_update_;
 
   scoped_refptr<RasterSource> raster_source_;
