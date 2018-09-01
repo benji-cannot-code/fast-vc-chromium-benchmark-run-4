668beca312d3d3665936716ab13c86bb64386ddf
Oksana Zhuravlova <oksamyt@chromium.org>
2018-09-01 04:02:30 +0000

    Mark BookmarkTest#testOpenBookmarkManager as flaky
    Bug: 879803
    
    Change-Id: Ie3db494b151b36bc73ae9f9067c0c89d4685553b
    
    TBR=twellington@chromium.org
    
    Change-Id: Ie3db494b151b36bc73ae9f9067c0c89d4685553b
    Reviewed-on: https://chromium-review.googlesource.com/1200172
    Reviewed-by: Oksana Zhuravlova <oksamyt@chromium.org>
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588263}

--- chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkTest.java
@@ -29,6 +29,7 @@ import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.Feature;
+import org.chromium.base.test.util.FlakyTest;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.chrome.R;
@@ -209,6 +210,7 @@ public class BookmarkTest {
 
     @Test
     @SmallTest
+    @FlakyTest(message = "crbug.com/879803")
     public void testOpenBookmarkManager() throws InterruptedException {
         openBookmarkManager();
         BookmarkDelegate delegate =
