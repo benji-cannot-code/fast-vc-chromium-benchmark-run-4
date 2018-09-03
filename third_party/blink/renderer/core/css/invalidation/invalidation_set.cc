4703444ea4a94621f87af28b2dad884ed89114f1
Fredrik Söderquist <fs@opera.com>
2018-09-03 18:53:38 +0000

    Use Element::LocalNameForSelectorMatching in InvalidationSet
    To match ElementRuleCollector et al. We will lower-case tag names on
    (selector) parsing in HTML documents, and thus need to give the same
    treatment to the local name extracted from the element.
    
    Bug: 856368
    Change-Id: I07e726864de048f5948cb27fa7e5dd6eaad9b0ef
    Reviewed-on: https://chromium-review.googlesource.com/1202206
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588427}

--- third_party/blink/renderer/core/css/invalidation/invalidation_set.cc
+++ third_party/blink/renderer/core/css/invalidation/invalidation_set.cc
@@ -70,10 +70,11 @@ bool InvalidationSet::InvalidatesElement(Element& element) const {
   if (invalidation_flags_.WholeSubtreeInvalid())
     return true;
 
-  if (tag_names_ && tag_names_->Contains(element.TagQName().LocalName())) {
+  if (tag_names_ &&
+      tag_names_->Contains(element.LocalNameForSelectorMatching())) {
     TRACE_STYLE_INVALIDATOR_INVALIDATION_SELECTORPART_IF_ENABLED(
         element, kInvalidationSetMatchedTagName, *this,
-        element.TagQName().LocalName());
+        element.LocalNameForSelectorMatching());
     return true;
   }
 
@@ -116,10 +117,11 @@ bool InvalidationSet::InvalidatesElement(Element& element) const {
 }
 
 bool InvalidationSet::InvalidatesTagName(Element& element) const {
-  if (tag_names_ && tag_names_->Contains(element.TagQName().LocalName())) {
+  if (tag_names_ &&
+      tag_names_->Contains(element.LocalNameForSelectorMatching())) {
     TRACE_STYLE_INVALIDATOR_INVALIDATION_SELECTORPART_IF_ENABLED(
         element, kInvalidationSetMatchedTagName, *this,
-        element.TagQName().LocalName());
+        element.LocalNameForSelectorMatching());
     return true;
   }
 
