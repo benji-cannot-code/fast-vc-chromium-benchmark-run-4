929908f98bb8ab33542ac9136cf7d8b4f74cdd48
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-09-03 22:29:21 +0000

    Import wpt@04bc9c5667911cce7361ee33244dfc8d7ed61212
    Using wpt-import in Chromium 0a598ade72987da2e61357ec94708f9dc560a6b1.
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/24020
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    tdresser@chromium.org:
      external/wpt/hr-time
    
    NOAUTOREVERT=true
    TBR=foolip
    
    No-Export: true
    Change-Id: Iedc66e95b50ef2b475dd412e3c9dd225b4fd6c7a
    Reviewed-on: https://chromium-review.googlesource.com/1203311
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588435}

---
+++ third_party/WebKit/LayoutTests/external/wpt/hr-time/resources/unload.js
@@ -0,0 +1,52 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+const syncDelay = ms => {
+  const start = performance.now();
+  let elapsedTime;
+  do {
+    elapsedTime = performance.now() - start;
+  } while (elapsedTime < ms);
+};
+
+const markTime = (docName, lifecycleEventName) => {
+  // Calculating these values before the below `mark` invocation ensures that delays in
+  // reaching across to the other window object doesn't interfere with the correctness
+  // of the test.
+  const dateNow = Date.now();
+  const performanceNow = performance.now();
+
+  window.opener.mark({
+    docName,
+    lifecycleEventName,
+    performanceNow: performanceNow,
+    dateNow: dateNow
+  });
+};
+
+const setupUnloadPrompt = (docName, msg) => {
+  window.addEventListener("beforeunload", ev => {
+    markTime(docName, "beforeunload");
+    return ev.returnValue = msg || "Click OK to continue test."
+  });
+};
+
+const setupListeners = (docName, nextDocument) => {
+  window.addEventListener("load", () => {
+    markTime(docName, "load");
+    document.getElementById("proceed").addEventListener("click", ev => {
+      ev.preventDefault();
+      if (nextDocument) {
+        document.location = nextDocument;
+      } else {
+        window.close();
+      }
+    })
+  });
+
+  setupUnloadPrompt(docName);
+
+  window.addEventListener("unload", () => {
+    markTime(docName, "unload");
+    if (docName !== "c") { syncDelay(1000); }
+  });
+};
+
