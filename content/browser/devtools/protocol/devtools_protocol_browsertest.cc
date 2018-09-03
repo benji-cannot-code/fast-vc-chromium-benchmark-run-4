f3f6d6e34c8604367a00a5cd8859d94c5f89e69b
Andrey Lushnikov <lushnikov@chromium.org>
2018-09-03 18:03:16 +0000

    DevTools: teach request interception to work with Security.setIgnoreCertificateErrors
    This is becoming very painful bug for puppeteer given that Chromium 70
    distrusted symantec certificates. It already works with NetworkService
    codepath; this patch adds the missing logic for non-network-service
    world.
    
    R=caseq, dgozman
    
    Bug: 801426
    Change-Id: I5f9d88aa3e6c859626486965b9b1801a4728a8e0
    Reviewed-on: https://chromium-review.googlesource.com/1202662
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Commit-Queue: Andrey Lushnikov <lushnikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588420}

--- content/browser/devtools/protocol/devtools_protocol_browsertest.cc
+++ content/browser/devtools/protocol/devtools_protocol_browsertest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/bind_helpers.h"
 #include "base/command_line.h"
+#include "base/json/json_reader.h"
 #include "base/logging.h"
 #include "base/strings/stringprintf.h"
 #include "base/sys_info.h"
@@ -1350,6 +1351,48 @@ IN_PROC_BROWSER_TEST_F(DevToolsProtocolTest, CertificateError) {
   shell()->LoadURL(test_url);
   WaitForNotification("Network.loadingFinished", true);
   continue_observer2.Wait();
+  EXPECT_EQ(test_url, shell()
+                          ->web_contents()
+                          ->GetController()
+                          .GetLastCommittedEntry()
+                          ->GetURL());
+}
+
+IN_PROC_BROWSER_TEST_F(DevToolsProtocolTest,
+                       CertificateErrorRequestInterception) {
+  net::EmbeddedTestServer https_server(net::EmbeddedTestServer::TYPE_HTTPS);
+  https_server.SetSSLConfig(net::EmbeddedTestServer::CERT_EXPIRED);
+  https_server.ServeFilesFromSourceDirectory("content/test/data");
+  ASSERT_TRUE(https_server.Start());
+  GURL test_url = https_server.GetURL("/devtools/navigation.html");
+
+  shell()->LoadURL(GURL("about:blank"));
+  WaitForLoadStop(shell()->web_contents());
+
+  Attach();
+  SendCommand("Network.enable", nullptr, true);
+  SendCommand("Security.enable", nullptr, false);
+  SendCommand(
+      "Network.setRequestInterception",
+      base::JSONReader::Read("{\"patterns\": [{\"urlPattern\": \"*\"}]}"),
+      true);
+
+  SendCommand("Security.setIgnoreCertificateErrors",
+              base::JSONReader::Read("{\"ignore\": true}"), true);
+
+  SendCommand("Network.clearBrowserCache", nullptr, true);
+  SendCommand("Network.clearBrowserCookies", nullptr, true);
+  TestNavigationObserver continue_observer(shell()->web_contents(), 1);
+  shell()->LoadURL(test_url);
+  std::unique_ptr<base::DictionaryValue> params =
+      WaitForNotification("Network.requestIntercepted", false);
+  std::string interceptionId;
+  EXPECT_TRUE(params->GetString("interceptionId", &interceptionId));
+  SendCommand("Network.continueInterceptedRequest",
+              base::JSONReader::Read("{\"interceptionId\": \"" +
+                                     interceptionId + "\"}"),
+              false);
+  continue_observer.Wait();
   EXPECT_EQ(test_url, shell()
                           ->web_contents()
                           ->GetController()
