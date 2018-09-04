65768c8be3dba512192e0ed201d709266c0afe5e
Aaron Leventhal <aleventhal@chromium.org>
2018-09-04 22:41:34 +0000

    Remove public method IsCollapsed is it is covered by IsExpanded
    Bug: None
    Change-Id: I92d4a5f598d27331592817b23b17aa27e1a2f2a6
    Reviewed-on: https://chromium-review.googlesource.com/1176278
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588677}

--- third_party/blink/renderer/modules/accessibility/ax_object.h
+++ third_party/blink/renderer/modules/accessibility/ax_object.h
@@ -519,7 +519,6 @@ class MODULES_EXPORT AXObject : public GarbageCollectedFinalized<AXObject> {
   // Check object state.
   virtual bool IsAutofillAvailable() { return false; }
   virtual bool IsClickable() const;
-  virtual bool IsCollapsed() const { return false; }
   virtual AccessibilityExpanded IsExpanded() const {
     return kExpandedUndefined;
   }
