da7dc3b5a7e915a3d0a5cbb767a67d8dea414dcd
Kunihiko Sakamoto <ksakamoto@chromium.org>
2018-09-04 04:09:02 +0000

    Signed Exchange: Make content-type parsing more conformant to the spec
    Before this patch, StructuredHeaderParser was used to extract the v=
    parameter of application/signed-exchange content type. However, the
    content-type header field has slightly different syntax [1]. For
    example, parameter names are not restricted to lower case but are
    case-insensitive.
    
    This patch replaces
    SignedExchangeSignatureHeaderField::GetVersionParamFromContentType()
    with signed_exchange_utils::GetSignedExchangeVersion() function, which
    follows the "Identifying signed exchanges" algorithm [2] and uses
    HttpUtil::NameValuePairsIterator to parse parameters.
    
    [1] https://tools.ietf.org/html/rfc7231#section-3.1.1.1
    [2] https://wicg.github.io/webpackage/loading.html#signed-exchange-version
    
    Bug: 803774
    Change-Id: Ibc00d20837ed2e1f503b3734cbf015468f4bf756
    Reviewed-on: https://chromium-review.googlesource.com/1201635
    Commit-Queue: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588449}

--- content/browser/web_package/signed_exchange_utils.h
+++ content/browser/web_package/signed_exchange_utils.h
@@ -9,7 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/optional.h"
+#include "content/browser/web_package/signed_exchange_consts.h"
 #include "content/browser/web_package/signed_exchange_error.h"
+#include "content/common/content_export.h"
 
 class GURL;
 
@@ -44,7 +46,15 @@ bool ShouldHandleAsSignedHTTPExchange(
     const GURL& request_url,
     const network::ResourceResponseHead& head);
 
-}  // namespace  signed_exchange_utils
+// Extracts the signed exchange version [1] from |content_type|, and converts it
+// to SignedExchanveVersion. Returns nullopt if the mime type is not a variant
+// of application/signed-exchange. Returns SignedExchangeVersion::kUnknown if an
+// unsupported signed exchange version is found.
+// [1] https://wicg.github.io/webpackage/loading.html#signed-exchange-version
+CONTENT_EXPORT base::Optional<SignedExchangeVersion> GetSignedExchangeVersion(
+    const std::string& content_type);
+
+}  // namespace signed_exchange_utils
 }  // namespace content
 
 #endif  // CONTENT_BROWSER_WEB_PACKAGE_SIGNED_EXCHANGE_UTILS_H_
