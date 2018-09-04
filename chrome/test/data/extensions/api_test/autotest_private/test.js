f30d3fcf56ee7ad6325723455995f13ab22e7b10
Jeffrey Kardatzke <jkardatzke@google.com>
2018-09-04 23:15:05 +0000

    Add autotest private API to enable Crostini
    Bug: None
    Test: Verified with tast-test that this can enable Crostini
    Change-Id: I8682d50e3bff49e5d36384694f5eeee1f899d378
    Reviewed-on: https://chromium-review.googlesource.com/1204970
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Commit-Queue: Jeffrey Kardatzke <jkardatzke@google.com>
    Cr-Commit-Position: refs/heads/master@{#588688}

--- chrome/test/data/extensions/api_test/autotest_private/test.js
+++ chrome/test/data/extensions/api_test/autotest_private/test.js
@@ -152,6 +152,10 @@ chrome.test.runTests([
             });
         });
   },
+  function setCrostiniEnabled() {
+    chrome.autotestPrivate.setCrostiniEnabled(true, chrome.test.callbackFail(
+        'Crostini is not available for the current user'));
+  },
   function runCrostiniInstaller() {
     chrome.autotestPrivate.runCrostiniInstaller(chrome.test.callbackFail(
         'Crostini is not available for the current user'));
