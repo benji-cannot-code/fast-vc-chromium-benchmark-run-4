c61342ad9ef0ab723b399df852cb8f0fa0dc05ee
Samuel Huang <huangs@chromium.org>
2018-09-04 23:18:28 +0000

    Revert "[Cronet] Provide bidirectional stream support from native CronetEngine."
    This reverts commit 4327cf6c9069208ad77c28a8f8df3b71ff74f5ec.
    
    Reason for revert: http://crbug.com/880618: cronet_tests failing on chromium.memory/Linux CFI
    
    Original change's description:
    > [Cronet] Provide bidirectional stream support from native CronetEngine.
    > 
    > Bug: 874542
    > Change-Id: I8414e75049ee219f1c7491caf91f6282c42f23dd
    > Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;master.tryserver.chromium.android:android_cronet_tester
    > Reviewed-on: https://chromium-review.googlesource.com/1178666
    > Commit-Queue: Misha Efimov <mef@chromium.org>
    > Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#588541}
    
    TBR=pauljensen@chromium.org,mef@chromium.org
    
    Change-Id: I2e300b34b04a5a16ff3c2959bc7ae8ebfdf32b4c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 874542
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;master.tryserver.chromium.android:android_cronet_tester
    Reviewed-on: https://chromium-review.googlesource.com/1205770
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588689}

--- components/cronet/cronet_url_request_context.h
+++ components/cronet/cronet_url_request_context.h
@@ -35,7 +35,6 @@ enum EffectiveConnectionType;
 class NetLog;
 class ProxyConfigService;
 class URLRequestContext;
-class URLRequestContextGetter;
 class FileNetLogObserver;
 }  // namespace net
 
@@ -115,13 +114,7 @@ class CronetURLRequestContext {
   // Returns true if running on network thread.
   bool IsOnNetworkThread() const;
 
-  // Returns net::URLRequestContext owned by |this|.
   net::URLRequestContext* GetURLRequestContext();
-
-  // Returns a new instance of net::URLRequestContextGetter.
-  // The net::URLRequestContext and base::SingleThreadTaskRunner that
-  // net::URLRequestContextGetter returns are owned by |this|.
-  net::URLRequestContextGetter* CreateURLRequestContextGetter();
 
   // TODO(xunjieli): Keep only one version of StartNetLog().
 
@@ -158,7 +151,6 @@ class CronetURLRequestContext {
 
  private:
   friend class TestUtil;
-  class ContextGetter;
 
   // NetworkTasks performs tasks on the network thread and owns objects that
   // live on the network thread.
