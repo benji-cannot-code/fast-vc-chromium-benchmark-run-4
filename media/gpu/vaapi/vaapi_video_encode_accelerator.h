3ec5a423e3f8254e4a7e5002a6ff03061f90c048
Chih-Yu Huang <akahuang@google.com>
2018-09-04 05:50:32 +0000

    VEA: Remove "--disable_flush" argument at VEA unittest.
    Originally, we need to add "--disable_flush" argument when running
    the VEA unittest if the encoder does not support flush. This CL added
    a method IsFlushSupported() to check if the encoder support flush.
    
    BUG=820313
    TEST=Run vea unittest at eve and nyan_big without
         "--disable_flush" argument
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: Id6a59546b0a13df15ca4ff2825eb70c848eaf704
    Reviewed-on: https://chromium-review.googlesource.com/1170671
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Reviewed-by: Pawel Osciak <posciak@chromium.org>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Commit-Queue: Chih-Yu Huang <akahuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588457}

--- media/gpu/vaapi/vaapi_video_encode_accelerator.h
+++ media/gpu/vaapi/vaapi_video_encode_accelerator.h
@@ -46,6 +46,7 @@ class MEDIA_GPU_EXPORT VaapiVideoEncodeAccelerator
       uint32_t framerate) override;
   void Destroy() override;
   void Flush(FlushCallback flush_callback) override;
+  bool IsFlushSupported() override;
 
  private:
   class H264Accelerator;
