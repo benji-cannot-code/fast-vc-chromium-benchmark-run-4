9b882825a6e56e2843e3f921b59a4bef2ca5b952
Jun Choi <hongjunchoi@chromium.org>
2018-09-04 21:50:09 +0000

    Use CTAPHID_MSG command when sending U2F register request
    When we send U2F register to CTAP2 devices as a fallback when the
    connected device has client pin set, we use CTAPHID_CBOR msg. Fix this
    so that CTAPHID_MSG command is used instead.
    
    Bug: 880520
    Change-Id: I86343696c70f27867edd820b11a3ac4270051980
    Reviewed-on: https://chromium-review.googlesource.com/1204919
    Reviewed-by: Kim Paulhamus <kpaulhamus@chromium.org>
    Commit-Queue: Jun Choi <hongjunchoi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588663}

--- device/fido/make_credential_task.cc
+++ device/fido/make_credential_task.cc
@@ -59,6 +59,7 @@ void MakeCredentialTask::StartTask() {
       IsClientPinOptionCompatible(device(), request_parameter_)) {
     MakeCredential();
   } else {
+    device()->set_supported_protocol(ProtocolVersion::kU2f);
     U2fRegister();
   }
 }
@@ -78,6 +79,7 @@ void MakeCredentialTask::U2fRegister() {
     return;
   }
 
+  DCHECK_EQ(ProtocolVersion::kU2f, device()->supported_protocol());
   register_operation_ = std::make_unique<U2fRegisterOperation>(
       device(), request_parameter_, std::move(callback_));
   register_operation_->Start();
