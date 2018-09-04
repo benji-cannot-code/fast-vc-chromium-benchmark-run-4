1a72c4111fdf4947c6a83a088cedd11fb3be8ee5
Khushal <khushalsagar@chromium.org>
2018-09-04 17:15:23 +0000

    ui/gl: Set up bindings for GrGLInterface to support GL 4.1.
    Add missing function pointers and GL entry points to support using
    GL 4.1 with skia. The primary motivation for this is to enable disk
    caching of shaders on desktop with OOP raster, since skia requires
    atleast GL 4.1 to enable this.
    
    R=kbr@chromium.org
    
    Bug: 867187
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: Ibb373fc96845286e3385d611b628524c58444ea0
    Reviewed-on: https://chromium-review.googlesource.com/1198530
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588560}

--- ui/gl/gl_bindings_autogen_mock.h
+++ ui/gl/gl_bindings_autogen_mock.h
@@ -1238,6 +1238,7 @@ static void GL_BINDING_CALL Mock_glMatrixLoadfEXT(GLenum matrixMode,
 static void GL_BINDING_CALL Mock_glMemoryBarrier(GLbitfield barriers);
 static void GL_BINDING_CALL Mock_glMemoryBarrierByRegion(GLbitfield barriers);
 static void GL_BINDING_CALL Mock_glMemoryBarrierEXT(GLbitfield barriers);
+static void GL_BINDING_CALL Mock_glMinSampleShading(GLfloat value);
 static void GL_BINDING_CALL Mock_glObjectLabel(GLenum identifier,
                                                GLuint name,
                                                GLsizei length,
