532e4be6c84a0f5d38349f0573d96a6f18fb56d1
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-09-04 11:02:08 +0000

    Import wpt@70b8ec865d913a46efacd351d0ad4669f6847a91
    Using wpt-import in Chromium 5ce3f48c87ffb3dcaeae0f2dfc929f5d0b2d8a11.
    With Chromium commits locally applied on WPT:
    5428f3ffcf "[WebSocket] Fix cookies/006.html?wss wpt"
    
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/24052
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    rouslan@chromium.org, mathp@chromium.org:
      external/wpt/payment-request
    
    NOAUTOREVERT=true
    TBR=foolip
    
    No-Export: true
    Change-Id: Ieb0efa5709857df7e47322dadba3fdc59b503f63
    Reviewed-on: https://chromium-review.googlesource.com/1203871
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588485}

--- third_party/WebKit/LayoutTests/external/wpt/payment-request/idlharness.https.window.js
+++ third_party/WebKit/LayoutTests/external/wpt/payment-request/idlharness.https.window.js
@@ -26,6 +26,7 @@ idl_test(
       PaymentRequest: ['paymentRequest'],
       PaymentMethodChangeEvent: ['new PaymentMethodChangeEvent("paymentmethodchange")'],
       PaymentRequestUpdateEvent: ['new PaymentRequestUpdateEvent("paymentrequestupdate")'],
+      MerchantValidationEvent: ['new MerchantValidationEvent("merchantvalidation")'],
     });
   }
 );
