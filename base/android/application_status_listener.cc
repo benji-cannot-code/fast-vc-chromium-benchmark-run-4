5949d6f128bbf66dab61e3312b0e97df4f0eae95
Siddhartha <ssid@chromium.org>
2018-09-04 22:00:48 +0000

    Add trace event to track application state
    Useful to understand traces from users.
    
    BUG=859260
    
    Change-Id: Ieece0c5726570daf5e2981f0e26f88b17083ac6d
    Reviewed-on: https://chromium-review.googlesource.com/1199779
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588665}

--- base/android/application_status_listener.cc
+++ base/android/application_status_listener.cc
@@ -57,6 +57,7 @@ void ApplicationStatusListener::Notify(ApplicationState state) {
 // static
 void ApplicationStatusListener::NotifyApplicationStateChange(
     ApplicationState state) {
+  TRACE_COUNTER1("browser", "ApplicationState", static_cast<int>(state));
   g_observers.Get().Notify(FROM_HERE, &ApplicationStatusListener::Notify,
                            state);
 }
