c8a7ccff61bc62aae2699d5cd5b148ede7dab301
Marcin Wiacek <marcin@mwiacek.com>
2018-09-04 18:08:12 +0000

    Cleanup inside chrome/java/preferences/website.
    * Removing duplicated strings inside SingleWebsitePreference
    * Indexing preferences list with
      ContentSettingException.Type/PermissionInfo.Type
    * Adding mapping to ContentSettingTypes inside
      ContentSettingException/PermissionInfo
    * Using new mappings in other files
    
    Change-Id: I9b4c9e7a33107edd94fcb6c7d5ae2bb8102073d2
    Reviewed-on: https://chromium-review.googlesource.com/1195373
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Marcin Wiącek <marcin@mwiacek.com>
    Cr-Commit-Position: refs/heads/master@{#588586}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/website/ContentSettingException.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/website/ContentSettingException.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser.preferences.website;
 
 import android.support.annotation.IntDef;
 
+import org.chromium.chrome.browser.ContentSettingsType;
 import org.chromium.chrome.browser.preferences.PrefServiceBridge;
 
 import java.io.Serializable;
@@ -21,8 +22,8 @@ public class ContentSettingException implements Serializable {
             Type.POPUP, Type.SOUND})
     @Retention(RetentionPolicy.SOURCE)
     public @interface Type {
-        // Values used to address array index in Website. Should be enumerated from 0
-        // and can't have gaps.
+        // Values used to address array index below, inside Website and SingleWebsitePreferences.
+        // Should be enumerated from 0 and can't have gaps.
         int ADS = 0;
         int AUTOPLAY = 1;
         int BACKGROUND_SYNC = 2;
@@ -35,6 +36,17 @@ public class ContentSettingException implements Serializable {
          */
         int NUM_ENTRIES = 7;
     }
+
+    // Mapping from {@link Type} to ContentSettingType.
+    final static int[] CONTENT_TYPES = {
+            ContentSettingsType.CONTENT_SETTINGS_TYPE_ADS,
+            ContentSettingsType.CONTENT_SETTINGS_TYPE_AUTOPLAY,
+            ContentSettingsType.CONTENT_SETTINGS_TYPE_BACKGROUND_SYNC,
+            ContentSettingsType.CONTENT_SETTINGS_TYPE_COOKIES,
+            ContentSettingsType.CONTENT_SETTINGS_TYPE_JAVASCRIPT,
+            ContentSettingsType.CONTENT_SETTINGS_TYPE_POPUPS,
+            ContentSettingsType.CONTENT_SETTINGS_TYPE_SOUND,
+    };
 
     private final int mContentSettingType;
     private final String mPattern;
