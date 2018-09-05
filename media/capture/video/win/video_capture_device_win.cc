52bf75cffb4d00b801211c8f1ac90b0067278597
Christian Fremerey <chfremer@chromium.org>
2018-09-05 19:04:52 +0000

    [Video Capture] Fix missing focus distance field on Win Direct Show
    This is a fix for bug 880249.
    
    Bug: 880249
    Change-Id: I176f925cbddfff9b2fdd6127216bbdcce85cb4c4
    Reviewed-on: https://chromium-review.googlesource.com/1207093
    Commit-Queue: Christian Fremerey <chfremer@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588954}

--- media/capture/video/win/video_capture_device_win.cc
+++ media/capture/video/win/video_capture_device_win.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/scoped_variant.h"
 #include "media/base/media_switches.h"
 #include "media/base/timestamp_constants.h"
+#include "media/capture/mojom/image_capture_types.h"
 #include "media/capture/video/blob_utils.h"
 #include "media/capture/video/win/metrics.h"
 #include "media/capture/video/win/video_capture_device_utils_win.h"
@@ -654,7 +655,7 @@ void VideoCaptureDeviceWin::GetPhotoState(GetPhotoStateCallback callback) {
       return;
   }
 
-  auto photo_capabilities = mojom::PhotoState::New();
+  auto photo_capabilities = mojo::CreateEmptyPhotoState();
 
   photo_capabilities->exposure_compensation = RetrieveControlRangeAndCurrent(
       [this](auto... args) {
