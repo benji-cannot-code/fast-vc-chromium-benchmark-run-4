331e819a1d6dd83ac7a0a7fd065aec68c95d59a5
James Wallace-Lee <jamwalla@chromium.org>
2018-09-05 17:05:10 +0000

    ViewAccessibilityUtils helper class to find important Views for A11y
    Refactoring code from BrowserView and ViewAXPlatformNodeDelegate
    into a static helper.
    
    Change-Id: I247ca4c8a7da4eeb02dfd0cd7886ae82c2bc3280
    Reviewed-on: https://chromium-review.googlesource.com/1195898
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: James Wallace-Lee <jamwalla@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588897}

---
+++ ui/views/accessibility/view_accessibility_utils.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_VIEWS_ACCESSIBILITY_VIEW_ACCESSIBILITY_UTILS_H_
+#define UI_VIEWS_ACCESSIBILITY_VIEW_ACCESSIBILITY_UTILS_H_
+
+#include "ui/views/view.h"
+#include "ui/views/widget/widget.h"
+
+namespace views {
+
+class VIEWS_EXPORT ViewAccessibilityUtils {
+ public:
+  // Returns a focused child widget if the view has a child that should be
+  // treated as a special case. For example, if a tab modal dialog is visible
+  // and focused, this will return the dialog when called on the BrowserView.
+  // This helper function is used to treat such widgets as separate windows for
+  // accessibility. Returns nullptr if no such widget is present.
+  static Widget* GetFocusedChildWidgetForAccessibility(const View* view);
+
+  // Used by GetFocusedChildWidgetForAccessibility to determine if a Widget
+  // should be handled separately. When visible and focused, these include:
+  //  - tab modal dialogs
+  static bool IsFocusedChildWidget(Widget* widget, const View* focused_view);
+};
+
+}  // namespace views
+
+#endif  // UI_VIEWS_ACCESSIBILITY_VIEW_ACCESSIBILITY_UTILS_H_
