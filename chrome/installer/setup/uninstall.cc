21c7e594a05194c7547076e13711d606747c9924
Greg Thompson <grt@chromium.org>
2018-09-05 10:21:50 +0000

    Reland "Move DoPostUninstallOperations from BrowserDistribution into setup."
    This is a reland of ea06ab2f566abeef6a195863601f8a25194671e4.
    
    Original CL:
    > Showing the uninstall survey is a brand-specific behavior. This CL
    > introduces a new setup/brand_behaviors.h file that declares various
    > operations that vary by brand. DoPostUninstallOperations and
    > GetDistributionData (its helper function) both reside in brand-specific
    > implementation files that are selected at build time.
    >
    > This also allowed moving the uninstall metrics code from installer/util
    > into installer/setup where it really belongs.
    >
    > I have also taken the liberty to gently modernize some of the moved
    > code.
    >
    > BUG=879568
    >
    > Change-Id: I25d9c205d46ffb14454193fcc28ae5803502bf29
    > Reviewed-on: https://chromium-review.googlesource.com/1200862
    > Reviewed-by: Robert Sesek <rsesek@chromium.org>
    > Reviewed-by: Brian White <bcwhite@chromium.org>
    > Reviewed-by: Samuel Huang <huangs@chromium.org>
    > Commit-Queue: Greg Thompson <grt@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#588537}
    > (cherry picked from commit ea06ab2f566abeef6a195863601f8a25194671e4)
    
    BUG=879568
    TBR=rsesek@chromium.org,bcwhite@chromium.org,huangs@chromium.org
    
    Change-Id: I089f50e1a24d447936bb034bddc9cba5ee0afa7f
    Reviewed-on: https://chromium-review.googlesource.com/1206431
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588817}

--- chrome/installer/setup/uninstall.cc
+++ chrome/installer/setup/uninstall.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_result_codes.h"
 #include "chrome/install_static/install_util.h"
+#include "chrome/installer/setup/brand_behaviors.h"
 #include "chrome/installer/setup/install.h"
 #include "chrome/installer/setup/install_worker.h"
 #include "chrome/installer/setup/installer_state.h"
@@ -884,7 +885,7 @@ InstallStatus UninstallProduct(const InstallationState& original_state,
 
   // Note that we must retrieve the distribution-specific data before deleting
   // product.GetVersionKey().
-  base::string16 distribution_data(browser_dist->GetDistributionData(reg_root));
+  base::string16 distribution_data(GetDistributionData());
 
   // Remove Control Panel uninstall link.
   // Assert that this is only called with the one relevant distribution.
@@ -1021,8 +1022,8 @@ InstallStatus UninstallProduct(const InstallationState& original_state,
 
   if (!force_uninstall && product_state) {
     VLOG(1) << "Uninstallation complete. Launching post-uninstall operations.";
-    browser_dist->DoPostUninstallOperations(product_state->version(),
-        backup_state_file, distribution_data);
+    DoPostUninstallOperations(product_state->version(), backup_state_file,
+                              distribution_data);
   }
 
   // Try and delete the preserved local state once the post-install
