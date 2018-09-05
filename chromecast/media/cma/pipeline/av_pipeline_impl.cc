c539b7c554972d8d5cfadc5d69a9a7a197cad64c
Yuchen Liu <yucliu@chromium.org>
2018-09-05 00:03:07 +0000

    [Chromecast] Only push buffer if state is kPlaying.
    AvPipelineImpl::Flush will remove all the pending buffers. There's a
    small window after Flush is called but media pipeline backend isn't
    stopped. As a result, the pipeline will try to push a new buffer to the
    backend and it causes crash.
    
    BUG=internal b/113336161
    TEST=Spotify/PlayMovie seek without failure
    
    Change-Id: Ic48177b19fd55018b37440000be30a5e8b4c9235
    Reviewed-on: https://chromium-review.googlesource.com/1205674
    Reviewed-by: Sergey Volk <servolk@chromium.org>
    Reviewed-by: Kenneth MacKay <kmackay@chromium.org>
    Commit-Queue: Yuchen Liu <yucliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588707}

--- chromecast/media/cma/pipeline/av_pipeline_impl.cc
+++ chromecast/media/cma/pipeline/av_pipeline_impl.cc
@@ -257,6 +257,9 @@ void AvPipelineImpl::ProcessPendingBuffer() {
 }
 
 void AvPipelineImpl::PushAllReadyBuffers() {
+  if (state_ != kPlaying)
+    return;
+
   DCHECK(!ready_buffers_.empty());
 
   scoped_refptr<DecoderBufferBase> ready_buffer =
