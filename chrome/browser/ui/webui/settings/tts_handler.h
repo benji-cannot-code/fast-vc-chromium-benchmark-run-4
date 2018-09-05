8fae289e4bf85d41d1dedb9a7c47d4ce4d3ffe59
David Tseng <dtseng@chromium.org>
2018-09-05 21:23:57 +0000

    Fix tts engine load
    The Tts engine component extension was converted to an event page in:
    https://chromium-review.googlesource.com/c/chromiumos/platform/assets/+/447000/5/speech_synthesis/patts/manifest.json
    
    This means the extension is aggressively unloaded. In particular, on startup, it is not loaded until the extension receives a request.
    
    In code paths that require the engine to be awake, explicitly wake the event page.
    
    Bug: 880556
    Change-Id: If49f3b59440bd64a9a0af7382f25e054285a26bf
    Reviewed-on: https://chromium-review.googlesource.com/1205092
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589004}

--- chrome/browser/ui/webui/settings/tts_handler.h
+++ chrome/browser/ui/webui/settings/tts_handler.h
@@ -6,16 +6,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_WEBUI_SETTINGS_TTS_HANDLER_H_
 #define CHROME_BROWSER_UI_WEBUI_SETTINGS_TTS_HANDLER_H_
 
+#include "base/memory/weak_ptr.h"
 #include "chrome/browser/speech/tts_controller.h"
 #include "chrome/browser/ui/webui/settings/settings_page_ui_handler.h"
+
+class Profile;
 
 namespace settings {
 
 // Chrome "/manageAccessibility/tts/*" settings page UI handler.
 class TtsHandler : public SettingsPageUIHandler, public VoicesChangedDelegate {
  public:
-  TtsHandler() = default;
-  ~TtsHandler() override = default;
+  TtsHandler();
+  ~TtsHandler() override;
 
   void HandleGetAllTtsVoiceData(const base::ListValue* args);
   void HandleGetTtsExtensions(const base::ListValue* args);
@@ -30,8 +33,12 @@ class TtsHandler : public SettingsPageUIHandler, public VoicesChangedDelegate {
   void OnVoicesChanged() override;
 
  private:
+  void WakeTtsEngine(const base::ListValue* args);
+  void OnTtsEngineAwake(bool success);
   int GetVoiceLangMatchScore(const VoiceData* voice,
                              const std::string& app_locale);
+
+  base::WeakPtrFactory<TtsHandler> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(TtsHandler);
 };
