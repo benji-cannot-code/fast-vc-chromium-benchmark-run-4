c337608e2e97ba112da63dcdea51edf0bb254f75
Becca Hughes <beccahughes@chromium.org>
2018-09-05 23:05:45 +0000

    [Autoplay] Add audio context feature
    Add a feature to disable AudioContext playbacks counting
    towards the MEI score.
    
    BUG=878460
    
    Change-Id: I486f3853ebc26a703e52712bee13600c99c7be14
    Reviewed-on: https://chromium-review.googlesource.com/1204728
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589046}

--- chrome/browser/media/media_engagement_contents_observer_unittest.cc
+++ chrome/browser/media/media_engagement_contents_observer_unittest.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/test_service_manager_context.h"
 #include "content/public/test/web_contents_tester.h"
+#include "media/base/media_switches.h"
 #include "services/metrics/public/cpp/ukm_builders.h"
 #include "services/metrics/public/cpp/ukm_source.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -40,9 +41,6 @@ class MediaEngagementContentsObserverTest
       : task_runner_(new base::TestMockTimeTaskRunner()) {}
 
   void SetUp() override {
-    scoped_feature_list_.InitFromCommandLine("RecordMediaEngagementScores",
-                                             std::string());
-
     ChromeRenderViewHostTestHarness::SetUp();
 
     test_service_manager_context_ =
@@ -449,8 +447,6 @@ class MediaEngagementContentsObserverTest
   MediaEngagementContentsObserver* contents_observer_;
 
   std::unique_ptr<MediaEngagementService> service_;
-
-  base::test::ScopedFeatureList scoped_feature_list_;
 
   ukm::TestAutoSetUkmRecorder test_ukm_recorder_;
 
@@ -1373,4 +1369,20 @@ TEST_F(MediaEngagementContentsObserverTest, GetOrCreateSession_WithOpener) {
   content::WebContentsTester::For(opener.get())->NavigateAndCommit(url);
   EXPECT_EQ(GetSessionFor(other_observer),
             GetOrCreateSession(url::Origin::Create(url), opener.get()));
+}
+
+TEST_F(MediaEngagementContentsObserverTest, IgnoreAudioContextIfDisabled) {
+  base::test::ScopedFeatureList feature_list;
+  feature_list.InitAndDisableFeature(media::kRecordWebAudioEngagement);
+
+  Navigate(GURL("https://www.example.com"));
+  SimulateAudioContextStarted(0);
+  SimulateAudible();
+
+  EXPECT_FALSE(AreAudioContextConditionsMet());
+  EXPECT_FALSE(IsAudioContextTimerRunning());
+  EXPECT_FALSE(WasSignificantAudioContextPlaybackRecorded());
+
+  SimulateAudioContextPlaybackTimerFired();
+  EXPECT_FALSE(WasSignificantAudioContextPlaybackRecorded());
 }
