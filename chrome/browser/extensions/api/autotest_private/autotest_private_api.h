20d570f1de0ea5cad79e5bc6aed03ce7cfb5d2f8
Daniel Erat <derat@chromium.org>
2018-09-05 21:03:36 +0000

    chromeos: Add autotestPrivate.getHistogram function.
    Add a getHistogram function to the chrome.autotestPrivate
    that Chrome OS integration tests can call to get data about
    a given histogram. This will hopefully be more reliable than
    the current approach of parsing chrome://histograms.
    
    Bug: 878641, 844755
    Change-Id: I9077c297a69742b9e225eefed52fe4b012d796fa
    Reviewed-on: https://chromium-review.googlesource.com/1200840
    Commit-Queue: Dan Erat <derat@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588996}

--- chrome/browser/extensions/api/autotest_private/autotest_private_api.h
+++ chrome/browser/extensions/api/autotest_private/autotest_private_api.h
@@ -232,6 +232,16 @@ class AutotestPrivateSetPlayStoreEnabledFunction
   ResponseAction Run() override;
 };
 
+class AutotestPrivateGetHistogramFunction : public UIThreadExtensionFunction {
+ public:
+  DECLARE_EXTENSION_FUNCTION("autotestPrivate.getHistogram",
+                             AUTOTESTPRIVATE_GETHISTOGRAM)
+
+ private:
+  ~AutotestPrivateGetHistogramFunction() override;
+  ResponseAction Run() override;
+};
+
 class AutotestPrivateIsAppShownFunction : public UIThreadExtensionFunction {
  public:
   DECLARE_EXTENSION_FUNCTION("autotestPrivate.isAppShown",
@@ -340,7 +350,7 @@ class AutotestPrivateAPI : public BrowserContextKeyedAPI {
   static const bool kServiceIsNULLWhileTesting = true;
   static const bool kServiceRedirectedInIncognito = true;
 
-  bool test_mode_;  // true for ExtensionApiTest.AutotestPrivate browser test.
+  bool test_mode_;  // true for AutotestPrivateApiTest.AutotestPrivate test.
 };
 
 template <>
