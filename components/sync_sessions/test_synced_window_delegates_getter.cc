2ff2f1d7bfbd26018e095d84740838d26a9fce7e
Mikel Astiz <mastiz@chromium.org>
2018-09-05 11:08:29 +0000

    Fix tab nodes not marked as free during restore
    When the local session is restored from persistence, if there are orphan
    tabs, they should be marked as free, to prevent newly created tabs from
    creating new tab nodes, instead of recycling available ones.
    
    The restore flow exercises SyncedSessionTracker::CleanupSession(), which
    prior to this patch wasn't smart enough for the local session.
    
    Bug: 875671,847325
    Change-Id: I91f9f13ad4aedbbd85bfcc73c606096781b80527
    Reviewed-on: https://chromium-review.googlesource.com/1204134
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588827}

--- components/sync_sessions/test_synced_window_delegates_getter.cc
+++ components/sync_sessions/test_synced_window_delegates_getter.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/bind.h"
+#include "base/stl_util.h"
 #include "components/sessions/core/serialized_navigation_entry_test_helper.h"
 #include "components/sync_sessions/synced_session.h"
 #include "components/sync_sessions/tab_node_pool.h"
@@ -250,6 +251,13 @@ void TestSyncedWindowDelegate::OverrideTabAt(int index,
   tab_delegates_[index] = delegate;
 }
 
+void TestSyncedWindowDelegate::CloseTab(SessionID tab_id) {
+  base::EraseIf(tab_delegates_,
+                [tab_id](const std::pair<int, SyncedTabDelegate*>& entry) {
+                  return entry.second->GetSessionId() == tab_id;
+                });
+}
+
 void TestSyncedWindowDelegate::SetIsSessionRestoreInProgress(bool value) {
   is_session_restore_in_progress_ = value;
 }
@@ -345,6 +353,14 @@ TestSyncedTabDelegate* TestSyncedWindowDelegatesGetter::AddTab(
   // navigations.
   router_.NotifyNav(tabs_.back().get());
   return tabs_.back().get();
+}
+
+void TestSyncedWindowDelegatesGetter::CloseTab(SessionID tab_id) {
+  for (auto& window : windows_) {
+    // CloseTab() will only take effect with the belonging window, the rest will
+    // simply ignore the call.
+    window->CloseTab(tab_id);
+  }
 }
 
 void TestSyncedWindowDelegatesGetter::SessionRestoreComplete() {
