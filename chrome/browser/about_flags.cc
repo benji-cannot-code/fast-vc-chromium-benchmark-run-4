051ad1b45424da1b04596db612f4a860bf574978
Andres Calderon Jaramillo <andrescj@chromium.org>
2018-09-05 15:46:35 +0000

    Move features::kVaapiJpegImageDecodeAcceleration from content to gpu.
    This CL moves features::kVaapiJpegImageDecodeAcceleration so that it can
    be accessed easily by GpuServiceImpl. GpuServiceImpl will use it to
    decide whether it should instantiate an implementation of the image
    decode acceleration mojo interface when requested by a renderer.
    
    Bug: 868400
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I93aa630792adae723236c8403ff807a31469ea8c
    Reviewed-on: https://chromium-review.googlesource.com/1203430
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Andres Calderon Jaramillo <andrescj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588877}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -112,6 +112,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "device/vr/buildflags/buildflags.h"
 #include "extensions/buildflags/buildflags.h"
 #include "google_apis/drive/drive_switches.h"
+#include "gpu/config/gpu_finch_features.h"
 #include "gpu/config/gpu_switches.h"
 #include "media/audio/audio_features.h"
 #include "media/base/media_switches.h"
