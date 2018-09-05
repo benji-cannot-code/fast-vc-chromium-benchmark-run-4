e5e09c31cd7d8b6924804562489959021d3319cd
Misha Efimov <mef@chromium.org>
2018-09-05 19:30:55 +0000

    Reland "[Cronet] Provide bidirectional stream support from native CronetEngine."
    This is a reland of 4327cf6c9069208ad77c28a8f8df3b71ff74f5ec
    
    Original change's description:
    > [Cronet] Provide bidirectional stream support from native CronetEngine.
    > 
    > Bug: 874542
    > Change-Id: I8414e75049ee219f1c7491caf91f6282c42f23dd
    > Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;master.tryserver.chromium.android:android_cronet_tester
    > Reviewed-on: https://chromium-review.googlesource.com/1178666
    > Commit-Queue: Misha Efimov <mef@chromium.org>
    > Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#588541}
    
    Bug: 874542
    Change-Id: I1380e751fa00fae546c563d72da829b089458fe5
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;master.tryserver.chromium.android:android_cronet_tester
    Reviewed-on: https://chromium-review.googlesource.com/1207613
    Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    Commit-Queue: Paul Jensen <pauljensen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588967}

--- components/grpc_support/include/bidirectional_stream_c.h
+++ components/grpc_support/include/bidirectional_stream_c.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_GRPC_SUPPORT_INCLUDE_BIDIRECTIONAL_STREAM_C_H_
 
 #if defined(WIN32)
-#define GRPC_SUPPORT_EXPORT
+#define GRPC_SUPPORT_EXPORT __declspec(dllexport)
 #else
 #define GRPC_SUPPORT_EXPORT __attribute__((visibility("default")))
 #endif
@@ -243,4 +243,4 @@ bool bidirectional_stream_is_done(bidirectional_stream* stream);
 }
 #endif
 
-#endif  // COMPONENTS_GRPC_SUPPORT_INCLUDE_BIDIRECTIONAL_STREAM_H_
+#endif  // COMPONENTS_GRPC_SUPPORT_INCLUDE_BIDIRECTIONAL_STREAM_C_H_
