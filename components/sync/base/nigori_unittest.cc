6b2f860ab1ab9694b888ff082d6c119eca6026a2
David Davidović <davidovic@google.com>
2018-09-05 11:57:56 +0000

    [sync::nigori] Add metrics for custom passphrase key derivation
    Add UMA metrics to track the rollout and performance of the new scrypt
    key derivation method used for Sync custom passphrases. Will try to
    merge this change into M70.
    
    Metrics added:
    
    * Time taken to derive keys using scrypt.
    * Time taken to derive keys using PBKDF2 (old method), for comparison.
    * Key derivation method state on Sync init.
    * Key derivation method on key decryption success (when the correct
      custom passphrase is entered).
    * Key derivation method on encryption (when a new custom passphrase is
      set).
    
    Bug: 879531
    Change-Id: I3d27fa8305d74588a706fcbf8f55f8df00fceb8b
    Reviewed-on: https://chromium-review.googlesource.com/1199595
    Commit-Queue: David Davidović <davidovic@google.com>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: vitaliii <vitaliii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588835}

--- components/sync/base/nigori_unittest.cc
+++ components/sync/base/nigori_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "components/sync/base/sync_base_switches.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -243,6 +244,29 @@ TEST(SyncNigoriTest,
   Nigori nigori;
   EXPECT_FALSE(nigori.InitByDerivation(
       KeyDerivationParams::CreateWithUnsupportedMethod(), "Passphrase!"));
+}
+
+// TODO(davidovic): Use an injected clock in the following tests in order to
+// verify that the duration is computed correctly.
+TEST(SyncNigoriTest, InitByDerivationShouldReportPbkdf2DurationInHistogram) {
+  Nigori nigori;
+  base::HistogramTester histogram_tester;
+  ASSERT_TRUE(nigori.InitByDerivation(
+      KeyDerivationParams::CreateForPbkdf2("localhost", "dummy"),
+      "Passphrase!"));
+
+  histogram_tester.ExpectTotalCount(
+      "Sync.Crypto.NigoriKeyDerivationDuration.Pbkdf2", /*count=*/1);
+}
+
+TEST(SyncNigoriTest, InitByDerivationShouldReportScryptDurationInHistogram) {
+  Nigori nigori;
+  base::HistogramTester histogram_tester;
+  ASSERT_TRUE(nigori.InitByDerivation(
+      KeyDerivationParams::CreateForScrypt("somesalt"), "Passphrase!"));
+
+  histogram_tester.ExpectTotalCount(
+      "Sync.Crypto.NigoriKeyDerivationDuration.Scrypt8192", /*count=*/1);
 }
 
 TEST(SyncNigoriTest, GenerateScryptSaltShouldReturnSaltOfCorrectSize) {
