598828d8141f6cd1b9fce8bf4cb69a994aab56a9
Dan Sanders <sandersd@chromium.org>
2018-09-05 22:13:01 +0000

    [media] Add UMA for MSE changeType() duration.
    This CL adds a notification from DecoderStream to DecoderSelector when a
    config change is detected. This allows DecoderSelector to be wholly
    responsible for decoder change metrics.
    
    All cases where the config change includes a codec change (and decoder
    selection is successful) are logged to Media.MSE.CodecChangeTime.
    The only time this should currently happen is for MSE changeType().
    
    The HW and SW suffixes are mapped to the final (successful) decoder
    selection, they can be read as 'to HW' and 'to SW'.
    
    Bug: 877673
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I19fcd82775fd107b268bacf3d3dc5ed1ab6ca697
    Reviewed-on: https://chromium-review.googlesource.com/1205499
    Commit-Queue: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: Matthew Wolenetz <wolenetz@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589025}

--- media/filters/decoder_selector.h
+++ media/filters/decoder_selector.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
+#include "base/time/time.h"
 #include "media/base/demuxer_stream.h"
 #include "media/base/pipeline_status.h"
 #include "media/filters/decoder_stream_traits.h"
@@ -86,6 +87,10 @@ class MEDIA_EXPORT DecoderSelector {
   // calls to SelectDecoder() will select from the full list of decoders.
   void FinalizeDecoderSelection();
 
+  // Signals that a config change has started being processed.
+  // Currently only for metric collection.
+  void NotifyConfigChanged();
+
  private:
   void InitializeDecoder();
   void OnDecoderInitializeDone(bool success);
@@ -112,6 +117,11 @@ class MEDIA_EXPORT DecoderSelector {
   typename Decoder::OutputCB output_cb_;
   std::unique_ptr<Decoder> decoder_;
   std::unique_ptr<DecryptingDemuxerStream> decrypting_demuxer_stream_;
+
+  // Metrics.
+  bool is_platform_decoder_ = false;
+  bool is_codec_changing_ = false;
+  base::TimeTicks codec_change_start_;
 
   base::WeakPtrFactory<DecoderSelector> weak_this_factory_;
 
