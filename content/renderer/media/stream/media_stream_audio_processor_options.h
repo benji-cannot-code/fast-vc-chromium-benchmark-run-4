685673776d8d42f9953f8c051dfd163fe6e408ba
Armando Miraglia <armax@chromium.org>
2018-09-06 14:47:23 +0000

    [CLEANUP] Refactor audio_processing_sample_rate into a static header constant.
    The audio_processing_sample_rate is currently only used in
    media_stream_audio_processor.cc however this will be needed for crbug/731170 to
    define valid constraints.
    
    This CL turns the definition of such variable into a more generally
    available static constant.
    
    Bonus: I am associating a new bug to the comment written on top rather
    than keep it assigned to a specific user.
    
    BUG=731170
    
    Change-Id: Ib69fed0a44dfa7a11732221b7ef5d143c739abfd
    Reviewed-on: https://chromium-review.googlesource.com/1209702
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Armando Miraglia <armax@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589165}

--- content/renderer/media/stream/media_stream_audio_processor_options.h
+++ content/renderer/media/stream/media_stream_audio_processor_options.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file.h"
 #include "base/macros.h"
 #include "base/threading/thread_checker.h"
+#include "build/build_config.h"
 #include "content/common/content_export.h"
 #include "content/public/common/media_stream_request.h"
 #include "media/audio/audio_processing.h"
@@ -31,6 +32,13 @@ class TypingDetection;
 namespace content {
 
 using webrtc::AudioProcessing;
+
+static constexpr int kAudioProcessingSampleRate =
+#if defined(OS_ANDROID)
+    AudioProcessing::kSampleRate16kHz;
+#else
+    AudioProcessing::kSampleRate48kHz;
+#endif
 
 // Simple struct with audio-processing properties.
 struct CONTENT_EXPORT AudioProcessingProperties {
