68b54c4d04cd0b7ef0977b6e9aa38395c7bb7803
Xiaoqian Dai <xdai@chromium.org>
2018-09-06 16:03:33 +0000

    Activate an unsnappable window should end both splitview and overview.
    Bug: 880959
    Change-Id: I87d4d222ea3b748171624846bfade66cca994a1b
    Reviewed-on: https://chromium-review.googlesource.com/1208145
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589181}

--- ash/wm/splitview/split_view_controller.cc
+++ ash/wm/splitview/split_view_controller.cc
@@ -696,7 +696,7 @@ void SplitViewController::OnWindowActivated(ActivationReason reason,
   // mode and show the cannot snap toast.
   if (!CanSnap(gained_active)) {
     if (wm::GetWindowState(gained_active)->IsUserPositionable()) {
-      EndSplitView();
+      EndSplitView(EndReason::kUnsnappableWindowActivated);
       ShowAppCannotSnapToast();
     }
     return;
