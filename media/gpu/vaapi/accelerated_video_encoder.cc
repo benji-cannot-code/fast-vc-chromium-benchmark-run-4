91364890cd5c70d01a6b4e509f8e1abe602577af
Chih-Yu Huang <akahuang@google.com>
2018-09-06 09:43:46 +0000

    vaapi vea: Change the formula of output buffer size.
    Originally we calculate the output buffer size by this formula:
    (width * height + 32768), but we cannot guarantee it is enough.
    
    In this CL, the new formula is: min(2MB, width * height * 1.5)
    2MB is empirically big enough for current usage, and
    (width * height * 1.5) is uncompressed buffer size. Both are feasible
    upper bound of the output buffer, so we choose the minimum one.
    
    BUG=b:113309237
    TEST=pass testOtherH264FlexArbitraryH and testOtherH264FlexArbitraryW
         CTS at cyan device
    TEST=pass VEA unittest
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: Ie53f79a9997420e1aad57168ad16f9e9718e4a54
    Reviewed-on: https://chromium-review.googlesource.com/1195175
    Commit-Queue: Chih-Yu Huang <akahuang@chromium.org>
    Reviewed-by: Pawel Osciak <posciak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589121}

--- media/gpu/vaapi/accelerated_video_encoder.cc
+++ media/gpu/vaapi/accelerated_video_encoder.cc
@@ -5,10 +5,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/gpu/vaapi/accelerated_video_encoder.h"
 
+#include <algorithm>
+
 #include "media/base/video_frame.h"
 #include "media/video/video_encode_accelerator.h"
 
 namespace media {
+
+namespace {
+// The maximum size for bitstream buffer, which is chosen empirically for the
+// video smaller than 1080p.
+// TODO(akahuang): Refactor it when we support 4K encoding.
+constexpr size_t kMaxBitstreamBufferSizeInBytes = 2 * 1024 * 1024;  // 2MB
+}  // namespace
 
 AcceleratedVideoEncoder::EncodeJob::EncodeJob(
     scoped_refptr<VideoFrame> input_frame,
@@ -53,6 +62,14 @@ void AcceleratedVideoEncoder::EncodeJob::Execute() {
   }
 
   std::move(execute_callback_).Run();
+}
+
+size_t AcceleratedVideoEncoder::GetBitstreamBufferSize() const {
+  // The buffer size of uncompressed stream is a feasible estimation of the
+  // output buffer size. However, the estimation is loose in high resolution.
+  // Therefore we set an empirical upper bound.
+  size_t uncompressed_buffer_size = GetCodedSize().GetArea() * 3 / 2;
+  return std::min(uncompressed_buffer_size, kMaxBitstreamBufferSizeInBytes);
 }
 
 }  // namespace media
