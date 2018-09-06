e772df0d80e31985e1fe3ffed5a77a06940114af
Kevin Bailey <krb@chromium.org>
2018-09-06 03:57:18 +0000

    [omnibox] Accessibility announcements for un/focus tab switch button
    We were doing some of the work to generate accessibility
    announcements when "focusing" the tab switch button, but not all those
    necessary for MacOS. This CL attempts to fill in some of those gaps.
    
    Notably, the tab switch button will have to make its own call to the
    accessibility device on focus, and will have to manufacture a
    notification when focus returns to the result-view proper.
    
    TBR: tapted@chromium.org
    Bug: 853929
    Change-Id: I8d81a8fa6a6528bb12c5e0ba3faff716610410b6
    Reviewed-on: https://chromium-review.googlesource.com/1185300
    Commit-Queue: Kevin Bailey <krb@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589098}

--- chrome/browser/ui/views/omnibox/omnibox_tab_switch_button.h
+++ chrome/browser/ui/views/omnibox/omnibox_tab_switch_button.h
@@ -43,6 +43,7 @@ class OmniboxTabSwitchButton : public views::MdTextButton {
 
   // Called to indicate button has been focused.
   void ProvideFocusHint();
+  void GetAccessibleNodeData(ui::AXNodeData* node_data) override;
 
  private:
   // Consults the parent views to see if the button is selected.
--- components/omnibox/browser/omnibox_edit_model.h
+++ components/omnibox/browser/omnibox_edit_model.h
@@ -368,6 +368,8 @@ class OmniboxEditModel {
   // Name of the histogram tracking cut or copy omnibox commands.
   static const char kCutOrCopyAllTextHistogram[];
 
+  OmniboxView* view() { return view_; }
+
  private:
   friend class OmniboxControllerTest;
   FRIEND_TEST_ALL_PREFIXES(OmniboxEditModelTest, ConsumeCtrlKey);
