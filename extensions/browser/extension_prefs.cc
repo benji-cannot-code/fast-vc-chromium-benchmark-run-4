b03919ad72ef5cc2ab10b0d25602e6b77c36f0a7
Devlin Cronin <rdevlin.cronin@chromium.org>
2018-09-06 01:34:33 +0000

    [Extensions Click-to-Script] Add observer for runtime permission changes
    Add a method to ExtensionPrefsObserver to monitor for runtime permission
    changes, and listen for these changes in the chrome://extensions page.
    This solves an issue where adding a new runtime host permission wouldn't
    be reflected immediately in the chrome://extensions page if it wasn't a
    permission directly requested by the extension, because there wouldn't
    be an active permissions change.
    
    Also reorder setting the "withhold permissions" pref to happen before
    adding the withheld permissions so that when the observers are notified,
    the bit is properly set.
    
    Bug: 878939
    
    Change-Id: I98c70ecd951a6ebb3d3372fec797f3657215a2b1
    Reviewed-on: https://chromium-review.googlesource.com/1196107
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589081}

--- extensions/browser/extension_prefs.cc
+++ extensions/browser/extension_prefs.cc
@@ -1001,6 +1001,8 @@ void ExtensionPrefs::AddRuntimeGrantedPermissions(
     const PermissionSet& permissions) {
   AddToPrefPermissionSet(extension_id, permissions,
                          kPrefRuntimeGrantedPermissions);
+  for (auto& observer : observer_list_)
+    observer.OnExtensionRuntimePermissionsChanged(extension_id);
 }
 
 void ExtensionPrefs::RemoveRuntimeGrantedPermissions(
@@ -1008,6 +1010,8 @@ void ExtensionPrefs::RemoveRuntimeGrantedPermissions(
     const PermissionSet& permissions) {
   RemoveFromPrefPermissionSet(extension_id, permissions,
                               kPrefRuntimeGrantedPermissions);
+  for (auto& observer : observer_list_)
+    observer.OnExtensionRuntimePermissionsChanged(extension_id);
 }
 
 void ExtensionPrefs::SetExtensionRunning(const std::string& extension_id,
