a5a81210393be1f4e95267ebe809322eed23bf14
Esmael El-Moslimany <aee@chromium.org>
2018-09-06 21:07:21 +0000

    Settings WebUI: prevent settings-subpage from removing itself as a FindShortcutListener if it was not already added as one.
    Ensure |active_| has changed value and change onActiveChanged_
    observer to a compound observer that also depends on |searchLabel|
    to ensure |searchLabel| has been initialized.
    
    Bug: 881104
    Change-Id: Icf4e740c766d1069aba9da13b25879c95a1959eb
    Reviewed-on: https://chromium-review.googlesource.com/1208822
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Esmael El-Moslimany <aee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589297}

--- chrome/browser/resources/settings/settings_page/settings_subpage.js
+++ chrome/browser/resources/settings/settings_page/settings_subpage.js
@@ -55,9 +55,15 @@ Polymer({
     active_: {
       type: Boolean,
       value: false,
-      observer: 'onActiveChanged_',
     },
   },
+
+  observers: [
+    'onActiveChanged_(active_, searchLabel)',
+  ],
+
+  /** @private {boolean} */
+  lastActiveValue_: false,
 
   /** @override */
   attached: function() {
@@ -88,8 +94,9 @@ Polymer({
 
   /** @private */
   onActiveChanged_: function() {
-    if (!this.searchLabel)
+    if (!this.searchLabel || this.lastActiveValue_ == this.active_)
       return;
+    this.lastActiveValue_ = this.active_;
 
     if (this.active_)
       this.becomeActiveFindShortcutListener();
