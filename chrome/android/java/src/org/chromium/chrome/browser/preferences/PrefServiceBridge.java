f5852ee62f2dfa59c210817b0628ab2b43c828e4
Anthony Vallee-Dubois <anthonyvd@chromium.org>
2018-09-06 14:37:45 +0000

    Only show explicit language ask once
    Bug: 865500
    Change-Id: I51eec8d3cbd6bd474af8d3b5275a433899f232fc
    Reviewed-on: https://chromium-review.googlesource.com/1207131
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Michael Martis <martis@chromium.org>
    Commit-Queue: anthonyvd <anthonyvd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589160}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/PrefServiceBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/PrefServiceBridge.java
@@ -1045,6 +1045,21 @@ public class PrefServiceBridge {
         nativeSetPromptForDownloadAndroid(status);
     }
 
+    /**
+     * @return Whether the explicit language prompt was shown at least once.
+     */
+    public boolean getExplicitLanguageAskPromptShown() {
+        return nativeGetExplicitLanguageAskPromptShown();
+    }
+
+    /**
+     * @param shown The value to set the underlying pref to: whether the prompt
+     * was shown to the user at least once.
+     */
+    public void setExplicitLanguageAskPromptShown(boolean shown) {
+        nativeSetExplicitLanguageAskPromptShown(shown);
+    }
+
     @VisibleForTesting
     public static void setInstanceForTesting(@Nullable PrefServiceBridge instanceForTesting) {
         sInstance = instanceForTesting;
@@ -1166,4 +1181,6 @@ public class PrefServiceBridge {
     private native void nativeSetDownloadAndSaveFileDefaultDirectory(String directory);
     private native int nativeGetPromptForDownloadAndroid();
     private native void nativeSetPromptForDownloadAndroid(int status);
+    private native boolean nativeGetExplicitLanguageAskPromptShown();
+    private native void nativeSetExplicitLanguageAskPromptShown(boolean shown);
 }
