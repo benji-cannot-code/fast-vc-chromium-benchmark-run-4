c2b10962d9bd891f7fbe985725a3e6c11bdd3be0
Yuki Yamada <yukiy@google.com>
2018-09-06 02:52:11 +0000

    Removed unused headers
    This CL removes unused includes regarding to
    blink::V8AbstractEventHandler and blink::V8EventListenerOrEventHandler.
    
    Bug: 872138
    Change-Id: I0fec7187851165e36f1bef0508cecced1f1202b7
    Reviewed-on: https://chromium-review.googlesource.com/1206751
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Commit-Queue: Yuki Yamada <yukiy@google.com>
    Cr-Commit-Position: refs/heads/master@{#589093}

--- third_party/blink/renderer/bindings/core/v8/v8_binding_for_core.cc
+++ third_party/blink/renderer/bindings/core/v8/v8_binding_for_core.cc
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/core/v8/idl_types.h"
 #include "third_party/blink/renderer/bindings/core/v8/native_value_traits_impl.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_controller.h"
-#include "third_party/blink/renderer/bindings/core/v8/v8_abstract_event_handler.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_array_buffer_view.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_element.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_event_target.h"
--- third_party/blink/renderer/core/inspector/thread_debugger.cc
+++ third_party/blink/renderer/core/inspector/thread_debugger.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/core/v8/v8_event.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_event_listener_helper.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_event_listener_info.h"
-#include "third_party/blink/renderer/bindings/core/v8/v8_event_listener_or_event_handler.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_html_all_collection.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_html_collection.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_node.h"
