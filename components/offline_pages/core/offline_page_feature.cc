5922e21f4b1ce9821a38a67791325837688339a9
Jian Li <jianli@chromium.org>
2018-09-06 22:31:41 +0000

    Add more parameters for offline indicator experiment
    The following parameters are added:
    1) Select either top or bottom snackbar
    2) Set stable offline wait time
    3) Allow to bypass getting connectivity info from system and run our
    own http probes
    
    Bug: 881064
    Change-Id: I72bbb82f2deb9f6222d25191380c048a53a4c815
    Reviewed-on: https://chromium-review.googlesource.com/1208445
    Reviewed-by: Peter Williamson <petewil@chromium.org>
    Commit-Queue: Jian Li <jianli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589344}

--- components/offline_pages/core/offline_page_feature.cc
+++ components/offline_pages/core/offline_page_feature.cc
@@ -80,6 +80,9 @@ const base::Feature kOfflinePagesShowAlternateDinoPageFeature{
 const base::Feature kOfflineIndicatorFeature{"OfflineIndicator",
                                              base::FEATURE_DISABLED_BY_DEFAULT};
 
+const base::Feature kOfflineIndicatorAlwaysHttpProbeFeature{
+    "OfflineIndicatorAlwaysHttpProbe", base::FEATURE_DISABLED_BY_DEFAULT};
+
 const char kPrefetchingOfflinePagesExperimentsOption[] = "exp";
 
 bool IsOfflineBookmarksEnabled() {
@@ -179,6 +182,10 @@ std::string GetPrefetchingOfflinePagesExperimentTag() {
 
 bool IsOfflineIndicatorFeatureEnabled() {
   return base::FeatureList::IsEnabled(kOfflineIndicatorFeature);
+}
+
+bool IsOfflineIndicatorAlwaysHttpProbeEnabled() {
+  return base::FeatureList::IsEnabled(kOfflineIndicatorAlwaysHttpProbeFeature);
 }
 
 }  // namespace offline_pages
