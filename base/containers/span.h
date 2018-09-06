0f386473d378ec42cf8b112c1638c65b71460019
Chris Palmer <palmer@chromium.org>
2018-09-06 19:38:32 +0000

    Revert "Add `CheckedRandomAccess[Const]Iterator`."
    This reverts commit 383049f284c02d11f4026b365c8a4d902185dbb8.
    
    Reason for revert: Landed wrong patchset!
    
    Original change's description:
    > Add `CheckedRandomAccess[Const]Iterator`.
    > 
    > And add a first call site.
    > 
    > Bug: 817982
    > Cq-Include-Trybots: luci.chromium.try:linux_mojo;master.tryserver.chromium.linux:linux_mojo
    > Change-Id: I2db298494e4f562ffac1f68b87c7a617e592f196
    > Reviewed-on: https://chromium-review.googlesource.com/963726
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Matt Menke <mmenke@chromium.org>
    > Reviewed-by: Jeffrey Yasskin <jyasskin@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#589244}
    
    TBR=ajwong@chromium.org,palmer@chromium.org,dcheng@chromium.org,jyasskin@chromium.org,mmenke@chromium.org,tsepez@chromium.org,nharper@chromium.org
    
    Change-Id: Icf99f8409054d853a15b8d27cdb232ff727f8a42
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 817982
    Cq-Include-Trybots: luci.chromium.try:linux_mojo;master.tryserver.chromium.linux:linux_mojo
    Reviewed-on: https://chromium-review.googlesource.com/1211743
    Reviewed-by: Chris Palmer <palmer@chromium.org>
    Commit-Queue: Chris Palmer <palmer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589245}

--- base/containers/span.h
+++ base/containers/span.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <type_traits>
 #include <utility>
 
-#include "base/containers/checked_iterators.h"
 #include "base/logging.h"
 #include "base/stl_util.h"
 
@@ -223,8 +222,8 @@ class span : public internal::ExtentStorage<Extent> {
   using difference_type = ptrdiff_t;
   using pointer = T*;
   using reference = T&;
-  using iterator = CheckedRandomAccessIterator<T>;
-  using const_iterator = CheckedRandomAccessConstIterator<T>;
+  using iterator = T*;
+  using const_iterator = const T*;
   using reverse_iterator = std::reverse_iterator<iterator>;
   using const_reverse_iterator = std::reverse_iterator<const_iterator>;
   static constexpr index_type extent = Extent;
@@ -394,10 +393,8 @@ class span : public internal::ExtentStorage<Extent> {
   constexpr T* data() const noexcept { return data_; }
 
   // [span.iter], span iterator support
-  iterator begin() const noexcept { return iterator(data_, data_ + size()); }
-  iterator end() const noexcept {
-    return iterator(data_, data_ + size(), data_ + size());
-  }
+  constexpr iterator begin() const noexcept { return data(); }
+  constexpr iterator end() const noexcept { return data() + size(); }
 
   constexpr const_iterator cbegin() const noexcept { return begin(); }
   constexpr const_iterator cend() const noexcept { return end(); }
