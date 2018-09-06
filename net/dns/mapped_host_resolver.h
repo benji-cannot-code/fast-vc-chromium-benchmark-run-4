8622c9831a8c0f42625799dc209108b2dababeac
Wojciech Dzierżanowski <wdzierzanowski@opera.com>
2018-09-06 13:38:43 +0000

    Add missing MappedHostResolver overrides
    Save for remapping hosts according to the rules, MappedHostResolver
    should be a transparent wrapper around the "real" resolver.  Thus, it
    must forward all HostResolver methods to the real resolver.
    
    Change-Id: I34deed523b3cb690d450599c89bfd7f2184e843d
    Reviewed-on: https://chromium-review.googlesource.com/1209402
    Commit-Queue: Wojciech Dzierżanowski <wdzierzanowski@opera.com>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589150}

--- net/dns/mapped_host_resolver.h
+++ net/dns/mapped_host_resolver.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include <string>
+#include <vector>
 
 #include "net/base/completion_once_callback.h"
 #include "net/base/host_mapping_rules.h"
@@ -66,15 +67,18 @@ class NET_EXPORT MappedHostResolver : public HostResolver {
                             HostCache::EntryStaleness* stale_info,
                             const NetLogWithSource& source_net_log) override;
   void SetDnsClientEnabled(bool enabled) override;
-
   HostCache* GetHostCache() override;
   bool HasCached(base::StringPiece hostname,
                  HostCache::Entry::Source* source_out,
                  HostCache::EntryStaleness* stale_out) const override;
-
   std::unique_ptr<base::Value> GetDnsConfigAsValue() const override;
   void SetNoIPv6OnWifi(bool no_ipv6_on_wifi) override;
   bool GetNoIPv6OnWifi() override;
+  void SetRequestContext(URLRequestContext* request_context) override;
+  void AddDnsOverHttpsServer(std::string spec, bool use_post) override;
+  void ClearDnsOverHttpsServers() override;
+  const std::vector<DnsConfig::DnsOverHttpsServerConfig>*
+  GetDnsOverHttpsServersForTesting() const override;
 
  private:
   class AlwaysErrorRequestImpl;
