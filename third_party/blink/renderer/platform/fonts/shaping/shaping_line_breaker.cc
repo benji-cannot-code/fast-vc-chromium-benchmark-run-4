369815dbb688bdbf366db41ccd0bc3ccad383587
Emil A Eklund <eae@chromium.org>
2018-09-06 22:25:08 +0000

    Remove unused shape_result_inline_headers includes
    Tbr: kojii@chromium.org
    Bug: 881110
    Change-Id: Ib1007641d09eec0ad372f2641bec77bc1c8643c8
    Reviewed-on: https://chromium-review.googlesource.com/1211709
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589338}

--- third_party/blink/renderer/platform/fonts/shaping/shaping_line_breaker.cc
+++ third_party/blink/renderer/platform/fonts/shaping/shaping_line_breaker.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/fonts/font.h"
 #include "third_party/blink/renderer/platform/fonts/shaping/harfbuzz_shaper.h"
 #include "third_party/blink/renderer/platform/fonts/shaping/shape_result.h"
-#include "third_party/blink/renderer/platform/fonts/shaping/shape_result_inline_headers.h"
 #include "third_party/blink/renderer/platform/fonts/shaping/shape_result_spacing.h"
 #include "third_party/blink/renderer/platform/text/text_break_iterator.h"
 
