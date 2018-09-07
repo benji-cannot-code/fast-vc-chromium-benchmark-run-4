b9cd9c65a2010b3753085d93958d4c37485f7b70
Jinho Bang <jinho.bang@samsung.com>
2018-09-07 01:22:00 +0000

    Jumbo: Fix a build error in components/content_settings
    The build error occurs if using `use_jumbo_build=true` in GN
    configuration. The same variable name is used in the anonymous namespace
    of content_settings_registry.cc and website_settings_registry.cc. It can
    cause a name conflict during merging both files.
    
    TBR=raymes@chromium.org
    NOTRY=true
    
    Bug: none
    Change-Id: I33eeb5ff00a2ab113d28a63f0044442375b98df2
    Reviewed-on: https://chromium-review.googlesource.com/1212422
    Commit-Queue: Jinho Bang <jinho.bang@samsung.com>
    Reviewed-by: Jinho Bang <jinho.bang@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#589400}

--- components/content_settings/core/browser/website_settings_registry.cc
+++ components/content_settings/core/browser/website_settings_registry.cc
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 base::LazyInstance<content_settings::WebsiteSettingsRegistry>::DestructorAtExit
-    g_instance = LAZY_INSTANCE_INITIALIZER;
+    g_website_settings_registry_instance = LAZY_INSTANCE_INITIALIZER;
 
 }  // namespace
 
@@ -24,7 +24,7 @@ namespace content_settings {
 
 // static
 WebsiteSettingsRegistry* WebsiteSettingsRegistry::GetInstance() {
-  return g_instance.Pointer();
+  return g_website_settings_registry_instance.Pointer();
 }
 
 WebsiteSettingsRegistry::WebsiteSettingsRegistry() {
