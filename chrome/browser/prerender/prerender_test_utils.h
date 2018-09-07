f330ac0ec198664ebab6e2f23a6434853251a66e
Eric Seckler <eseckler@chromium.org>
2018-09-07 11:08:55 +0000

    chrome: Prepare browser tests for PostTask refactor.
    Updates prerender and safe browsing browser tests to initialize the
    SafeBrowsingDatabaseManager after BrowserThreads were created (but
    before they are started). This is required for the upcoming move of
    BrowserThread::PostTask to base/task/post_task.h, as construction of
    SafeBrowsingDatabaseManager obtains a TaskRunner for the IO thread.
    In the future, this has to happen after thread initialization.
    
    Bug: 878356
    Change-Id: I1d4c614690ba3aacf4887fefe57d378c23668706
    Reviewed-on: https://chromium-review.googlesource.com/1211735
    Reviewed-by: Jialiu Lin <jialiul@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589488}

--- chrome/browser/prerender/prerender_test_utils.h
+++ chrome/browser/prerender/prerender_test_utils.h
@@ -312,7 +312,8 @@ class PrerenderInProcessBrowserTest : virtual public InProcessBrowserTest {
   ~PrerenderInProcessBrowserTest() override;
 
   void SetUpCommandLine(base::CommandLine* command_line) override;
-  void SetUpInProcessBrowserTestFixture() override;
+  void CreatedBrowserMainParts(
+      content::BrowserMainParts* browser_main_parts) override;
   void TearDownInProcessBrowserTestFixture() override;
   void SetUpOnMainThread() override;
   content::SessionStorageNamespace* GetSessionStorageNamespace() const;
