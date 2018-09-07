0532bfdfbaf50a9ceb519998d607e6297465b6ad
David Tseng <dtseng@chromium.org>
2018-09-07 22:31:54 +0000

    Remove unused lineBreaks attribute
    TBR=dcheng@chromium.org
    
    Bug: 669134
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: I91a10a1f39a1a353ace08d54611f6daec35ea1de
    Reviewed-on: https://chromium-review.googlesource.com/1213879
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589691}

--- ui/accessibility/ax_node_data.cc
+++ ui/accessibility/ax_node_data.cc
@@ -175,7 +175,6 @@ bool IsNodeIdIntListAttribute(ax::mojom::IntListAttribute attr) {
     // add a new attribute without explicitly considering whether it's
     // a node id attribute or not.
     case ax::mojom::IntListAttribute::kNone:
-    case ax::mojom::IntListAttribute::kLineBreaks:
     case ax::mojom::IntListAttribute::kMarkerTypes:
     case ax::mojom::IntListAttribute::kMarkerStarts:
     case ax::mojom::IntListAttribute::kMarkerEnds:
@@ -1073,9 +1072,6 @@ std::string AXNodeData::ToString() const {
         break;
       case ax::mojom::IntListAttribute::kRadioGroupIds:
         result += " radio_group_ids=" + IntVectorToString(values);
-        break;
-      case ax::mojom::IntListAttribute::kLineBreaks:
-        result += " line_breaks=" + IntVectorToString(values);
         break;
       case ax::mojom::IntListAttribute::kMarkerTypes: {
         std::string types_str;
