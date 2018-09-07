3ef3427ef807ba130f570136f26377f5276be961
Eric Robinson <ericrobinson@chromium.org>
2018-09-07 16:49:04 +0000

    Adding a dom message to avoid flakiness in Create*Frame scripts.
    The test that checked the cross-origin status of a doc-written ad
    frame was flaky, and wasn't seeing any histogram entry at all.  The
    operating theory is that this was due to the frame itself not
    being guaranteed to load all its subresources.  This CL blocks the
    test until the subframe's load event is fired to ensure that
    subresources have been fully loaded.
    
    Bug: 874443
    Change-Id: If4aa585d1e51dcf1e2350b768d6f6e5dfd3554cd
    Reviewed-on: https://chromium-review.googlesource.com/1211085
    Commit-Queue: Eric Robinson <ericrobinson@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589551}

--- chrome/test/data/ad_tagging/ad_script.js
+++ chrome/test/data/ad_tagging/ad_script.js
@@ -1,4 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 'use strict';
 
 function createAdFrame(url, name) {
@@ -21,6 +25,9 @@ async function createDocWrittenAdFrame(name, base_url) {
   document.body.appendChild(frame);
 
   frame.contentDocument.open();
+  frame.onload = function() {
+    window.domAutomationController.send(true);
+  }
   frame.contentDocument.write(doc_text);
   frame.contentDocument.close();
 }
--- chrome/test/data/ad_tagging/create_frame.js
+++ chrome/test/data/ad_tagging/create_frame.js
@@ -26,6 +26,9 @@ async function createDocWrittenFrame(name, base_url) {
   document.body.appendChild(frame);
 
   frame.contentDocument.open();
+  frame.onload = function() {
+    window.domAutomationController.send(true);
+  }
   frame.contentDocument.write(doc_text);
   frame.contentDocument.close();
 }
