2cabfa276955503f382c35bdc609b6a812c8fb0f
Yutaka Hirano <yhirano@chromium.org>
2018-09-07 04:56:05 +0000

    Reland "Strengthen requirements on CORS-safelisted request-headers"
    This is a reland of 074455deb4bfb5b117c641f65b60f32471093878
    
    Original change's description:
    > Strengthen requirements on CORS-safelisted request-headers
    >
    > With this CL, some request headers that used to be treated as
    > CORS-safelisted are not CORS-safelisted any more. Specifically,
    >
    >  - "accept", "accept-language" and "content-language" have a stronger
    >    check on its value
    >  - All headers whose value exceeds 128 bytes are treated as not
    >    CORS-safelisted
    >  - If the sum of value length of CORS-safelisted headers exceeds 1024,
    >    then all of them are treated as not CORS-safelisted.
    >
    > This CL also implements
    > https://fetch.spec.whatwg.org/#no-cors-safelisted-request-header.
    >
    > This is for https://github.com/whatwg/fetch/pull/736.
    >
    > Bug: 824130
    > Cq-Include-Trybots: luci.chromium.try:linux_mojo
    > Change-Id: Ib12a7dbff6367717a43130ae59304dca55b7bf4e
    > Reviewed-on: https://chromium-review.googlesource.com/1196563
    > Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    > Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#589153}
    
    Bug: 824130
    Change-Id: Ia5caad12a51ee44713cf4cf11f42b1fc9ab831a9
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Tbr: toyoshim@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/1212425
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589434}

--- services/network/public/cpp/cors/cors.h
+++ services/network/public/cpp/cors/cors.h
@@ -7,9 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define SERVICES_NETWORK_PUBLIC_CPP_CORS_CORS_H_
 
 #include <string>
+#include <vector>
 
 #include "base/component_export.h"
 #include "base/optional.h"
+#include "net/http/http_request_headers.h"
 #include "services/network/public/cpp/cors/cors_error_status.h"
 #include "services/network/public/mojom/cors.mojom-shared.h"
 #include "services/network/public/mojom/fetch_api.mojom-shared.h"
@@ -118,6 +120,26 @@ COMPONENT_EXPORT(NETWORK_CPP)
 bool IsCORSSafelistedContentType(const std::string& name);
 COMPONENT_EXPORT(NETWORK_CPP)
 bool IsCORSSafelistedHeader(const std::string& name, const std::string& value);
+COMPONENT_EXPORT(NETWORK_CPP)
+bool IsNoCORSSafelistedHeader(const std::string& name,
+                              const std::string& value);
+
+// https://fetch.spec.whatwg.org/#cors-unsafe-request-header-names
+// |headers| must not contain multiple headers for the same name.
+// The returned list is NOT sorted.
+// The returned list consists of lower-cased names.
+COMPONENT_EXPORT(NETWORK_CPP)
+std::vector<std::string> CORSUnsafeRequestHeaderNames(
+    const net::HttpRequestHeaders::HeaderVector& headers);
+
+// https://fetch.spec.whatwg.org/#cors-unsafe-request-header-names
+// Returns header names which are not CORS-safelisted AND not forbidden.
+// |headers| must not contain multiple headers for the same name.
+// The returned list is NOT sorted.
+// The returned list consists of lower-cased names.
+COMPONENT_EXPORT(NETWORK_CPP)
+std::vector<std::string> CORSUnsafeNotForbiddenRequestHeaderNames(
+    const net::HttpRequestHeaders::HeaderVector& headers);
 
 // Checks forbidden method in the fetch spec.
 // See https://fetch.spec.whatwg.org/#forbidden-method.
