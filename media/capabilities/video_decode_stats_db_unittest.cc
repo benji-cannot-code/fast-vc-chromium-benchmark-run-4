4bd05cf30de2cadb44e883e83a48b47a09e1c7a8
chcunningham <chcunningham@chromium.org>
2018-09-07 01:06:50 +0000

    [RELAND] Add destruction observer to catch VideoDecodeStatsDB UAF
    This re-instates the behavior where Incognito profiles have their
    in-memory DB seeded by the on-disk DB of the original profile. This
    was temporarily disabled to fix the UAF for M69 release.
    
    This CL adds a CHECK in the destructor of seed DB which will fire
    whenever it it is destroyed while a dependent in-memory DB is still
    alive.
    
    TBR: droger@chromium.org
    Bug: 865321
    Test: new unit test, manual testing.
    Change-Id: I7c873369b1ffbd3259273c628190ee1cc581b45e
    Reviewed-on: https://chromium-review.googlesource.com/1208253
    Commit-Queue: Chrome Cunningham <chcunningham@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589393}

--- media/capabilities/video_decode_stats_db_unittest.cc
+++ media/capabilities/video_decode_stats_db_unittest.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using leveldb_proto::test::FakeDB;
 using testing::Pointee;
 using testing::Eq;
+using testing::_;
 
 namespace media {
 
