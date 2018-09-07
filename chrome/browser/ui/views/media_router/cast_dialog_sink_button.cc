c0b2d0b2abd151e14498a980d9f70cd51b611f90
Takumi Fujimoto <takumif@chromium.org>
2018-09-07 17:00:15 +0000

    [Harmony Cast Dialog] Make stop button compatible with screen readers
    When a stop button is in focus, the screen reader will say "stop, <sink
    name>, <sink status>, button".
    
    Bug: 881611
    Change-Id: If90512e79c5c47a0cbe04a8e88e49fe165fcbb2c
    Reviewed-on: https://chromium-review.googlesource.com/1211666
    Reviewed-by: Derek Cheng <imcheng@chromium.org>
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589553}

--- chrome/browser/ui/views/media_router/cast_dialog_sink_button.cc
+++ chrome/browser/ui/views/media_router/cast_dialog_sink_button.cc
@@ -35,6 +35,7 @@ namespace {
 class StopButton : public views::LabelButton {
  public:
   StopButton(views::ButtonListener* button_listener,
+             const UIMediaSink& sink,
              int button_tag,
              bool enabled)
       : views::LabelButton(button_listener, base::string16()) {
@@ -48,6 +49,9 @@ class StopButton : public views::LabelButton {
     SetEnabled(enabled);
     // Make it possible to navigate to this button by pressing the tab key.
     SetFocusBehavior(FocusBehavior::ALWAYS);
+    SetAccessibleName(l10n_util::GetStringFUTF16(
+        IDS_MEDIA_ROUTER_STOP_CASTING_BUTTON_ACCESSIBLE_NAME,
+        sink.friendly_name, sink.status_text));
   }
 
   ~StopButton() override = default;
@@ -118,7 +122,8 @@ std::unique_ptr<views::View> CreatePrimaryIconForSink(
   if (sink.state == UIMediaSinkState::CONNECTED ||
       sink.state == UIMediaSinkState::DISCONNECTING) {
     return std::make_unique<StopButton>(
-        button_listener, button_tag, sink.state == UIMediaSinkState::CONNECTED);
+        button_listener, sink, button_tag,
+        sink.state == UIMediaSinkState::CONNECTED);
   } else if (sink.issue) {
     auto icon_view = std::make_unique<views::ImageView>();
     icon_view->SetImage(CreateVectorIcon(::vector_icons::kInfoOutlineIcon,
