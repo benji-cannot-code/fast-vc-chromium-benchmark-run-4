b7bb3a1892d4c9d645eddfa204376a9fb8af8689
Paul Miller <paulmiller@google.com>
2018-09-07 21:23:34 +0000

    Create WebView's prefs earlier and use them for variations
    Google-internal design doc:
    https://docs.google.com/document/d/1X6Xoz9hl3qoAj3EhAKcvZ6hWLVcY_QpQgmUexpgDHKA/edit?usp=sharing
    
    Move prefs creation from AwBrowserContext in PreMainMessageLoopRun to
    AwBrowserMainParts in PreCreateThreads. This way, variations can use the
    same pref service as the rest of WebView, while still being initialized
    as early as possible. (See BrowserMainLoop::CreateStartupTasks() for an
    overview of the startup sequence.)
    
    Before, AwBrowserContext created and owned PrefService and
    BrowserPolicyConnector. Now, AwBrowserMainParts creates them in
    PreCreateThreads, and owns them until PreMainMessageLoopRun, at which
    point ownership is transferred to AwBrowserContext.
    
    BUG=866722
    
    Change-Id: Ie8eb6df60bd4a0f872d8c6401cc14ff0bca38368
    Reviewed-on: https://chromium-review.googlesource.com/1212268
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Paul Miller <paulmiller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589665}

--- android_webview/browser/aw_content_browser_client.cc
+++ android_webview/browser/aw_content_browser_client.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "android_webview/browser/aw_content_browser_client.h"
 
+#include <string>
 #include <utility>
 #include <vector>
 
@@ -46,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/crash/content/browser/child_exit_observer_android.h"
 #include "components/navigation_interception/intercept_navigation_delegate.h"
 #include "components/policy/content/policy_blacklist_navigation_throttle.h"
+#include "components/policy/core/browser/browser_policy_connector_base.h"
 #include "components/safe_browsing/browser/browser_url_loader_throttle.h"
 #include "components/safe_browsing/browser/mojo_safe_browsing_impl.h"
 #include "components/safe_browsing/features.h"
@@ -215,12 +217,15 @@ AwContentBrowserClient::AwContentBrowserClient() : net_log_(new net::NetLog()) {
 
 AwContentBrowserClient::~AwContentBrowserClient() {}
 
-AwBrowserContext* AwContentBrowserClient::InitBrowserContext() {
+AwBrowserContext* AwContentBrowserClient::InitBrowserContext(
+    std::unique_ptr<PrefService> pref_service,
+    std::unique_ptr<policy::BrowserPolicyConnectorBase> policy_connector) {
   base::FilePath user_data_dir;
   if (!base::PathService::Get(base::DIR_ANDROID_APP_DATA, &user_data_dir)) {
     NOTREACHED() << "Failed to get app data directory for Android WebView";
   }
-  browser_context_.reset(new AwBrowserContext(user_data_dir));
+  browser_context_ = std::make_unique<AwBrowserContext>(
+      user_data_dir, std::move(pref_service), std::move(policy_connector));
   return browser_context_.get();
 }
 
--- android_webview/browser/aw_content_browser_client.h
+++ android_webview/browser/aw_content_browser_client.h
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 
 #include <memory>
+#include <string>
+#include <vector>
 
 #include "base/compiler_specific.h"
 #include "base/macros.h"
@@ -16,12 +18,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/content_browser_client.h"
 #include "services/service_manager/public/cpp/binder_registry.h"
 
+class PrefService;
+
 namespace content {
 class RenderFrameHost;
 }
 
 namespace net {
 class NetLog;
+}
+
+namespace policy {
+class BrowserPolicyConnectorBase;
 }
 
 namespace safe_browsing {
@@ -45,7 +53,9 @@ class AwContentBrowserClient : public content::ContentBrowserClient {
 
   // Allows AwBrowserMainParts to initialize a BrowserContext at the right
   // moment during startup. AwContentBrowserClient owns the result.
-  AwBrowserContext* InitBrowserContext();
+  AwBrowserContext* InitBrowserContext(
+      std::unique_ptr<PrefService> pref_service,
+      std::unique_ptr<policy::BrowserPolicyConnectorBase> policy_connector);
 
   content::BrowserMainParts* CreateBrowserMainParts(
       const content::MainFunctionParams& parameters) override;
