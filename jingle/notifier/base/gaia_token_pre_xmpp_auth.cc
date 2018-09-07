583702c9f51dee111f5b26473070e2deb8b6621b
zx6658 <zx6658@naver.com>
2018-09-07 21:17:14 +0000

    Use base::ContainsValue() instead of std::find() in src/jingle
    This simplifies conditions around the code.
    
    Bug: 561800
    Change-Id: Iaa24aeb45df5465e88de8f48b504b98737557dbd
    Reviewed-on: https://chromium-review.googlesource.com/1180635
    Commit-Queue: Nicolas Zea <zea@chromium.org>
    Reviewed-by: Nicolas Zea <zea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589658}

--- jingle/notifier/base/gaia_token_pre_xmpp_auth.cc
+++ jingle/notifier/base/gaia_token_pre_xmpp_auth.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/macros.h"
+#include "base/stl_util.h"
 #include "third_party/libjingle_xmpp/xmpp/constants.h"
 #include "third_party/libjingle_xmpp/xmpp/saslcookiemechanism.h"
 #include "third_party/webrtc/rtc_base/socketaddress.h"
@@ -102,10 +103,8 @@ std::string GaiaTokenPreXmppAuth::GetAuthMechanism() const {
 
 std::string GaiaTokenPreXmppAuth::ChooseBestSaslMechanism(
     const std::vector<std::string> & mechanisms, bool encrypted) {
-  return (std::find(mechanisms.begin(), mechanisms.end(), auth_mechanism_) !=
-              mechanisms.end())
-             ? auth_mechanism_
-             : std::string();
+  return base::ContainsValue(mechanisms, auth_mechanism_) ? auth_mechanism_
+                                                          : std::string();
 }
 
 buzz::SaslMechanism* GaiaTokenPreXmppAuth::CreateSaslMechanism(
