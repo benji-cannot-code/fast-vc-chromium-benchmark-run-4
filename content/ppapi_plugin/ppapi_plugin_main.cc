2cb7e5d75b4f0e42afa0aa638809d117ee74ae25
Wez <wez@chromium.org>
2018-09-07 17:30:56 +0000

    Migrate non-test code in //content off of QuitCurrent*Deprecated().
    - ChildThreadImpl now takes a |quit_closure|, which is provided by all
      sub-classes when used out-of-process.
    - Sub-classes which can run in-process instead pass base::DoNothing() to
      ChildThreadImpl, to instead rely on the in-browser Thread having
      Stop() invoked on it, to shut them down.
    
    Bug: 859095
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel
    Change-Id: I0251db64063601a6a9de8883c8558e25104e5b0b
    Reviewed-on: https://chromium-review.googlesource.com/1200839
    Reviewed-by: enne <enne@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589564}

--- content/ppapi_plugin/ppapi_plugin_main.cc
+++ content/ppapi_plugin/ppapi_plugin_main.cc
@@ -130,8 +130,10 @@ int PpapiPluginMain(const MainFunctionParams& parameters) {
 #endif
 
   ChildProcess ppapi_process;
-  ppapi_process.set_main_thread(
-      new PpapiThread(parameters.command_line, false));  // Not a broker.
+  base::RunLoop run_loop;
+  ppapi_process.set_main_thread(new PpapiThread(run_loop.QuitClosure(),
+                                                parameters.command_line,
+                                                false /* Not a broker */));
 
 #if defined(OS_WIN)
   if (!base::win::IsUser32AndGdi32Available())
@@ -153,7 +155,7 @@ int PpapiPluginMain(const MainFunctionParams& parameters) {
       subpixel_rendering != gfx::FontRenderParams::SUBPIXEL_RENDERING_NONE);
 #endif
 
-  base::RunLoop().Run();
+  run_loop.Run();
 
 #if defined(OS_WIN)
   UninitializeDWriteFontProxy();
