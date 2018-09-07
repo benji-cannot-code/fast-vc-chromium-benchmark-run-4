5d30f9d6e589791a40d037f17c5960d1f925faa5
Luigi Semenzato <semenzato@chromium.org>
2018-09-07 21:06:41 +0000

    Add metrics event reporting for Chrome OS
    This adds a mechanism to report statistically-interesting
    events to Chrome OS listeners.  A Chrome OS daemon (such
    as the memory daemon "memd") can register as a client
    of the Metrics Event Service, and receive signals on the
    occurrence of related events (for instance, a tab discard).
    
    This CL adds the supporting machinery and its first use,
    signaling of a tab discard.
    
    CQ-DEPEND=CL:1199965
    BUG=chromium:729335
    TEST=ran on DUT, verified that D-Bus signal is sent as expected
    
    Change-Id: I6eddf4b0d18aa4c8a3fd4aab7f0770d87ddc7aeb
    Reviewed-on: https://chromium-review.googlesource.com/1062078
    Commit-Queue: Luigi Semenzato <semenzato@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589639}

--- chrome/browser/chromeos/chrome_browser_main_chromeos.cc
+++ chrome/browser/chromeos/chrome_browser_main_chromeos.cc
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/dbus/component_updater_service_provider.h"
 #include "chrome/browser/chromeos/dbus/drive_file_stream_service_provider.h"
 #include "chrome/browser/chromeos/dbus/kiosk_info_service_provider.h"
+#include "chrome/browser/chromeos/dbus/metrics_event_service_provider.h"
 #include "chrome/browser/chromeos/dbus/proxy_resolution_service_provider.h"
 #include "chrome/browser/chromeos/dbus/screen_lock_service_provider.h"
 #include "chrome/browser/chromeos/dbus/virtual_file_request_service_provider.h"
@@ -335,6 +336,11 @@ class DBusServices {
         CrosDBusService::CreateServiceProviderList(
             std::make_unique<KioskInfoService>()));
 
+    metrics_event_service_ = CrosDBusService::Create(
+        kMetricsEventServiceName, dbus::ObjectPath(kMetricsEventServicePath),
+        CrosDBusService::CreateServiceProviderList(
+            std::make_unique<MetricsEventServiceProvider>()));
+
     screen_lock_service_ = CrosDBusService::Create(
         kScreenLockServiceName, dbus::ObjectPath(kScreenLockServicePath),
         CrosDBusService::CreateServiceProviderList(
@@ -408,6 +414,7 @@ class DBusServices {
     TPMTokenLoader::Shutdown();
     proxy_resolution_service_.reset();
     kiosk_info_service_.reset();
+    metrics_event_service_.reset();
     virtual_file_request_service_.reset();
     component_updater_service_.reset();
     chrome_features_service_.reset();
@@ -424,6 +431,7 @@ class DBusServices {
  private:
   std::unique_ptr<CrosDBusService> proxy_resolution_service_;
   std::unique_ptr<CrosDBusService> kiosk_info_service_;
+  std::unique_ptr<CrosDBusService> metrics_event_service_;
   std::unique_ptr<CrosDBusService> screen_lock_service_;
   std::unique_ptr<CrosDBusService> virtual_file_request_service_;
   std::unique_ptr<CrosDBusService> component_updater_service_;
