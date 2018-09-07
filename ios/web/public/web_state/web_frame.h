c5943e5e5865bae11a1f012aa1540188bbcfa7bc
Mike Dougherty <michaeldo@chromium.org>
2018-09-07 04:41:12 +0000

    Cleanup WebFrame class.
    Fixes for comments on crrev.com/c/1192424 after merge.
    
    Bug: 851636
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: Id742b36e6f46a647b510f81307f371b51bac7b3f
    Reviewed-on: https://chromium-review.googlesource.com/1208463
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589433}

--- ios/web/public/web_state/web_frame.h
+++ ios/web/public/web_state/web_frame.h
@@ -10,10 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/supports_user_data.h"
+#include "base/time/time.h"
 #include "url/gurl.h"
 
 namespace base {
-class TimeDelta;
 class Value;
 }
 
--- ios/web/web_state/web_frame_impl_inttest.mm
+++ ios/web/web_state/web_frame_impl_inttest.mm
@@ -166,7 +166,7 @@ TEST_F(WebFrameImplIntTest, PreventMessageReplay) {
     return;
   }
 
-  ASSERT_TRUE(LoadHtml("<p>>"));
+  ASSERT_TRUE(LoadHtml("<p>"));
 
   WebFrame* main_frame = GetMainWebFrameForWebState(web_state());
   ASSERT_TRUE(main_frame);
@@ -221,13 +221,13 @@ TEST_F(WebFrameImplIntTest, PreventMessageReplay) {
     return called;
   }));
 
-  EXPECT_EQ(1, [ExecuteJavaScript(@"sensitiveValue") intValue]);
+  EXPECT_NSEQ(@1, ExecuteJavaScript(@"sensitiveValue"));
 
   ExecuteJavaScript(@"replayInterceptedMessage()");
 
   // Value should not increase because replaying message should not re-execute
   // the called function.
-  EXPECT_EQ(1, [ExecuteJavaScript(@"sensitiveValue") intValue]);
+  EXPECT_NSEQ(@1, ExecuteJavaScript(@"sensitiveValue"));
 }
 
 }  // namespace web
