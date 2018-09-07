49cb46d77c7239e1691676c6805082d4669b8ea5
Nick Carter <nick@chromium.org>
2018-09-07 21:39:51 +0000

    Reconcile SecurityOrigin and url::Origin nonstandard-scheme behaviors.
    Create opaque SecurityOrigins for nonstandard schemes, unless
    they are local. Allow creation of url::Origins for local,
    nonstandard schemes.
    
    The root problem here was inconsistency between url::Origin and
    SecurityOrigin -- url::SchemeHostPort (and thus url::Origin)
    rejects anything with a nonstandard protocol, but
    SecurityOrigin::Create(KURL) would allow this creation to
    proceed as a tuple origin ("protocol", "", 0).
    
    The allowance of local+nonstandard schemes seems to be
    necessary, at least temporarily, to avoid breaking the
    Android-only "content:" scheme, and the ChromeOS-only
    "externalfile:" scheme.
    
    Other nonstandard schemes shouldn't generally commit, but this does
    seem to be possible via LoadDataWithBaseUrl; for example in the test
    "NavigationControllerBrowserTest.CrossDomainResourceRequestLoadDataWithBaseUrl"
    
    BUG=862282
    
    Change-Id: I351a067521cd846d2c77cca2498e58580613b4e2
    Reviewed-on: https://chromium-review.googlesource.com/1208811
    Commit-Queue: Nick Carter <nick@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589671}

--- third_party/blink/renderer/core/dom/document_test.cc
+++ third_party/blink/renderer/core/dom/document_test.cc
@@ -590,6 +590,8 @@ TEST_F(DocumentTest, EnforceSandboxFlags) {
 
   // A unique origin does not bypass secure context checks unless it
   // is also potentially trustworthy.
+  url::AddStandardScheme("very-special-scheme",
+                         url::SchemeType::SCHEME_WITH_HOST);
   SchemeRegistry::RegisterURLSchemeBypassingSecureContextCheck(
       "very-special-scheme");
   origin =
--- url/origin.cc
+++ url/origin.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string.h>
 
 #include "base/logging.h"
+#include "base/stl_util.h"
 #include "base/strings/string_number_conversions.h"
 #include "url/gurl.h"
 #include "url/url_canon.h"
@@ -21,7 +22,7 @@ namespace url {
 Origin::Origin() {}
 
 Origin Origin::Create(const GURL& url) {
-  if (!url.is_valid() || (!url.IsStandard() && !url.SchemeIsBlob()))
+  if (!url.is_valid())
     return Origin();
 
   SchemeHostPort tuple;
@@ -36,18 +37,22 @@ Origin Origin::Create(const GURL& url) {
     tuple = SchemeHostPort(GURL(url.GetContent()));
   } else {
     tuple = SchemeHostPort(url);
+
+    // It's SchemeHostPort's responsibility to filter out unrecognized schemes;
+    // sanity check that this is happening.
+    DCHECK(tuple.IsInvalid() || url.IsStandard() ||
+           base::ContainsValue(GetLocalSchemes(), url.scheme_piece()));
   }
 
   if (tuple.IsInvalid())
     return Origin();
-
   return Origin(std::move(tuple));
 }
 
 // Note: this is very similar to Create(const GURL&), but opaque origins are
 // created with CreateUniqueOpaque() rather than the default constructor.
 Origin Origin::CreateCanonical(const GURL& url) {
-  if (!url.is_valid() || (!url.IsStandard() && !url.SchemeIsBlob()))
+  if (!url.is_valid())
     return CreateUniqueOpaque();
 
   SchemeHostPort tuple;
@@ -62,6 +67,11 @@ Origin Origin::CreateCanonical(const GURL& url) {
     tuple = SchemeHostPort(GURL(url.GetContent()));
   } else {
     tuple = SchemeHostPort(url);
+
+    // It's SchemeHostPort's responsibility to filter out unrecognized schemes;
+    // sanity check that this is happening.
+    DCHECK(tuple.IsInvalid() || url.IsStandard() ||
+           base::ContainsValue(GetLocalSchemes(), url.scheme_piece()));
   }
 
   if (tuple.IsInvalid())
