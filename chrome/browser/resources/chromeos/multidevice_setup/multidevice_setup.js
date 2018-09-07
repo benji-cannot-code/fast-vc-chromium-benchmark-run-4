e87ac95b4573de7e91adf8e9a97516ffe6dc02d0
Ryan Hansberry <hansberry@chromium.org>
2018-09-07 22:57:41 +0000

    [CrOS MultiDevice] Make the password entry page of setup flow the 2nd page.
    Improve user experience by showing the "Start" page first, and the
    "Password" page after, making for a less jarring experience.
    
    Bug: 881610
    Change-Id: I0a54c0d6c8b5ffc11e6bfdd2aba80adf6bab374d
    Reviewed-on: https://chromium-review.googlesource.com/1214169
    Reviewed-by: Jeremy Klein <jlklein@chromium.org>
    Commit-Queue: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589701}

--- chrome/browser/resources/chromeos/multidevice_setup/multidevice_setup.js
+++ chrome/browser/resources/chromeos/multidevice_setup/multidevice_setup.js
@@ -34,7 +34,7 @@ cr.define('multidevice_setup', function() {
        */
       visiblePageName_: {
         type: String,
-        value: PageName.PASSWORD,
+        value: PageName.START,
         // For testing purporses only
         notify: true,
       },
@@ -53,7 +53,6 @@ cr.define('multidevice_setup', function() {
        */
       authToken_: {
         type: String,
-        observer: 'onAuthTokenChanged_',
       },
 
       /**
@@ -139,12 +138,6 @@ cr.define('multidevice_setup', function() {
           return;
         case PageName.PASSWORD:
           this.$.passwordPage.clearPasswordTextInput();
-          this.visiblePageName_ = PageName.START;
-          return;
-        case PageName.SUCCESS:
-          this.exitSetupFlow_();
-          return;
-        case PageName.START:
           let deviceId = /** @type {string} */ (this.selectedDeviceId_);
           this.multideviceSetup.setHostDevice(deviceId, this.authToken_)
               .then((responseParams) => {
@@ -152,11 +145,6 @@ cr.define('multidevice_setup', function() {
                   console.warn(
                       'Failure setting device with device ID: ' +
                       this.selectedDeviceId_);
-
-                  // If setting the host failed, it was likely due to an expired
-                  // auth token, so go back to the password page to get a new
-                  // token.
-                  this.visiblePageName_ = PageName.PASSWORD;
                   return;
                 }
 
@@ -172,6 +160,13 @@ cr.define('multidevice_setup', function() {
               .catch((error) => {
                 console.warn('Mojo service failure: ' + error);
               });
+          return;
+        case PageName.SUCCESS:
+          this.exitSetupFlow_();
+          return;
+        case PageName.START:
+          this.visiblePageName_ = PageName.PASSWORD;
+          return;
       }
     },
 
@@ -187,16 +182,6 @@ cr.define('multidevice_setup', function() {
     shouldForwardButtonBeDisabled_: function() {
       return (this.visiblePageName_ == PageName.PASSWORD) &&
           this.passwordPageForwardButtonDisabled_;
-    },
-
-    /** @private */
-    onAuthTokenChanged_: function() {
-      // If the auth token has expired but the user is still on the "Start
-      // Setup" page (i.e., the user has not yet chosen a device to be the host
-      // device for this account), go back to the password page and require that
-      // the user enter their password again.
-      if (!this.authToken_ && this.visiblePageName_ == PageName.START)
-        this.visiblePageName_ = PageName.PASSWORD;
     },
 
     /**
--- chrome/test/data/webui/multidevice_setup/integration_test.js
+++ chrome/test/data/webui/multidevice_setup/integration_test.js
@@ -25,6 +25,7 @@ cr.define('multidevice_setup', () => {
        */
       let backwardButton;
 
+      const PASSWORD = 'password-page';
       const SUCCESS = 'setup-succeeded-page';
       const START = 'start-setup-page';
 
@@ -71,12 +72,14 @@ cr.define('multidevice_setup', () => {
       });
 
       test(
-          'StartSetupPage forward button sets host in backround and ' +
-              'continues OOBE (OOBE).',
+          'StartSetupPage forward button sets host in background and ' +
+              'goes to PasswordPage (OOBE).',
           done => {
-            multiDeviceSetupElement.addEventListener('setup-exited', () => {
-              done();
-            });
+            multiDeviceSetupElement.addEventListener(
+                'visible-page-name_-changed', () => {
+                  if (multiDeviceSetupElement.visiblePageName_ == PASSWORD)
+                    done();
+                });
 
             setVisiblePage(START);
             multiDeviceSetupElement.multideviceSetup.shouldSetHostSucceed =
@@ -98,8 +101,18 @@ cr.define('multidevice_setup', () => {
         backwardButton.click();
       });
 
+      test('PasswordPage backward button closes UI (post-OOBE)', done => {
+        multiDeviceSetupElement.addEventListener('setup-exited', () => done());
+
+        setVisiblePage(PASSWORD);
+        multiDeviceSetupElement.multideviceSetup.shouldSetHostSucceed = true;
+        multiDeviceSetupElement.uiMode = multidevice_setup.UiMode.POST_OOBE;
+
+        backwardButton.click();
+      });
+
       test(
-          'StartSetupPage forward button goes to success page if mojo works ' +
+          'PasswordPage forward button goes to success page if mojo works ' +
               '(post-OOBE)',
           done => {
             multiDeviceSetupElement.addEventListener(
@@ -108,7 +121,7 @@ cr.define('multidevice_setup', () => {
                     done();
                 });
 
-            setVisiblePage(START);
+            setVisiblePage(PASSWORD);
             multiDeviceSetupElement.multideviceSetup.shouldSetHostSucceed =
                 true;
             multiDeviceSetupElement.uiMode = multidevice_setup.UiMode.POST_OOBE;
