2bcc36d475c980ebb283a028e29eb3c1f5d7435c
James Cook <jamescook@chromium.org>
2018-09-07 15:40:42 +0000

    chromeos: Collapse NativeCursorManagerAsh with its only subclass
    NativeCursorManagerAshClassic is the only subclass.
    
    I'm going to rename the file in a follow-up CL to help git rename
    tracking preserve the blame history.
    
    Bug: 866532
    Change-Id: Icb4a0ee7211a4256d6655797393d22bb43169e77
    Reviewed-on: https://chromium-review.googlesource.com/1212143
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589526}

--- ash/wm/native_cursor_manager_ash_classic.cc
+++ ash/wm/native_cursor_manager_ash_classic.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/cursor/cursor.h"
 #include "ui/base/cursor/image_cursors.h"
 #include "ui/base/layout.h"
+#include "ui/wm/core/native_cursor_manager_delegate.h"
 
 namespace ash {
 namespace {
@@ -53,27 +54,27 @@ void NotifyMouseEventsEnableStateChange(bool enabled) {
 
 }  // namespace
 
-NativeCursorManagerAshClassic::NativeCursorManagerAshClassic()
+NativeCursorManagerAsh::NativeCursorManagerAsh()
     : native_cursor_enabled_(true), image_cursors_(new ui::ImageCursors) {}
 
-NativeCursorManagerAshClassic::~NativeCursorManagerAshClassic() = default;
+NativeCursorManagerAsh::~NativeCursorManagerAsh() = default;
 
-void NativeCursorManagerAshClassic::SetNativeCursorEnabled(bool enabled) {
+void NativeCursorManagerAsh::SetNativeCursorEnabled(bool enabled) {
   native_cursor_enabled_ = enabled;
 
   ::wm::CursorManager* cursor_manager = Shell::Get()->cursor_manager();
   SetCursor(cursor_manager->GetCursor(), cursor_manager);
 }
 
-float NativeCursorManagerAshClassic::GetScale() const {
+float NativeCursorManagerAsh::GetScale() const {
   return image_cursors_->GetScale();
 }
 
-display::Display::Rotation NativeCursorManagerAshClassic::GetRotation() const {
+display::Display::Rotation NativeCursorManagerAsh::GetRotation() const {
   return image_cursors_->GetRotation();
 }
 
-void NativeCursorManagerAshClassic::SetDisplay(
+void NativeCursorManagerAsh::SetDisplay(
     const display::Display& display,
     ::wm::NativeCursorManagerDelegate* delegate) {
   DCHECK(display.is_valid());
@@ -96,7 +97,7 @@ void NativeCursorManagerAshClassic::SetDisplay(
       ->SetDisplay(display);
 }
 
-void NativeCursorManagerAshClassic::SetCursor(
+void NativeCursorManagerAsh::SetCursor(
     gfx::NativeCursor cursor,
     ::wm::NativeCursorManagerDelegate* delegate) {
   if (native_cursor_enabled_) {
@@ -114,7 +115,7 @@ void NativeCursorManagerAshClassic::SetCursor(
     SetCursorOnAllRootWindows(cursor);
 }
 
-void NativeCursorManagerAshClassic::SetCursorSize(
+void NativeCursorManagerAsh::SetCursorSize(
     ui::CursorSize cursor_size,
     ::wm::NativeCursorManagerDelegate* delegate) {
   image_cursors_->SetCursorSize(cursor_size);
@@ -130,7 +131,7 @@ void NativeCursorManagerAshClassic::SetCursorSize(
       ->SetCursorSize(cursor_size);
 }
 
-void NativeCursorManagerAshClassic::SetVisibility(
+void NativeCursorManagerAsh::SetVisibility(
     bool visible,
     ::wm::NativeCursorManagerDelegate* delegate) {
   delegate->CommitVisibility(visible);
@@ -146,7 +147,7 @@ void NativeCursorManagerAshClassic::SetVisibility(
   NotifyCursorVisibilityChange(visible);
 }
 
-void NativeCursorManagerAshClassic::SetMouseEventsEnabled(
+void NativeCursorManagerAsh::SetMouseEventsEnabled(
     bool enabled,
     ::wm::NativeCursorManagerDelegate* delegate) {
   delegate->CommitMouseEventsEnabled(enabled);
