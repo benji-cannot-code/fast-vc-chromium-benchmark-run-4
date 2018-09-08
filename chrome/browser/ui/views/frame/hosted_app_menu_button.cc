d014a3bf365120b94c24fe5c5b8462c124d85743
Xiaoqian Dai <xdai@chromium.org>
2018-09-08 01:02:45 +0000

    Add UMA for how many times the hosted app menu button are clicked.
    Per the PM's request, add UMA to record how many times the hosted app
    menu button are clicked.
    
    Bug: None
    Change-Id: Ib52f936ce5029bf7c23ed15f14f724db665d81f9
    Reviewed-on: https://chromium-review.googlesource.com/1214084
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589739}

--- chrome/browser/ui/views/frame/hosted_app_menu_button.cc
+++ chrome/browser/ui/views/frame/hosted_app_menu_button.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/frame/hosted_app_menu_button.h"
 
+#include "base/metrics/user_metrics.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/time/time.h"
 #include "chrome/app/vector_icons/vector_icons.h"
@@ -74,6 +75,10 @@ void HostedAppMenuButton::OnMenuButtonClicked(views::MenuButton* source,
            browser, AppMenu::NO_FLAGS);
 
   menu()->RunMenu(this);
+
+  // Add UMA for how many times the hosted app menu button are clicked.
+  base::RecordAction(
+      base::UserMetricsAction("HostedAppMenuButtonButton_Clicked"));
 }
 
 void HostedAppMenuButton::FadeHighlightOff() {
