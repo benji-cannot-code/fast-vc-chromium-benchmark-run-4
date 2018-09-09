7d9ca7fea648375925c11a8e6ebe8ff3aa19a4ac
Ned Nguyen <nednguyen@google.com>
2018-09-09 13:28:10 +0000

    Fix a bug about ordering of stories in generate_perf_sharding
    ** PERF SHERIFF: this CL may cause perf graphs to regress but it's expected **
    
    
    Previously, there is a big discrepancy between the predicted timing of
    generate_perf_sharding's output & the actual timing. For details, see:
    * Predicted timing: https://logs.chromium.org/v/?s=chrome%2Fbuildbucket%2Fcr-buildbucket.appspot.com%2F8935880309586521856%2F%2B%2FBenchmarks_Shard_Map_ca71405d-0fb3-4722-a815-af11b3f08318
    * Actual timing: https://ci.chromium.org/p/chrome/builders/luci.chrome.ci/android-go-perf/182
    
    After debugging, I found the root cause of this discrepancy is due to the order
    of stories input in generate_perf_sharding isn't the same of the order of stories
    which are meant to be run. This is because we mistakenly sort the list of
    story names in benchmark_utils.GetBenchmarkStoryNames(..) method.
    
    This CL fixes it by removing the sorting logic. It also add unittest to ensure
    that the order of story names return by the method always matches with the
    order of stories to be run.
    
    Bug:863768
    Cq-Include-Trybots: master.tryserver.chromium.perf:obbs_fyi
    Change-Id: I769ff867b0ee10a9afafafebbd4cb2621e0ab93f
    
    TBR=jbudorick@chromium.org, perezju@chromium.org
    NOTRY=true # content_browsertests flake
    
    Change-Id: I769ff867b0ee10a9afafafebbd4cb2621e0ab93f
    Reviewed-on: https://chromium-review.googlesource.com/1214806
    Commit-Queue: Ned Nguyen <nednguyen@google.com>
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Cr-Commit-Position: refs/heads/master@{#589801}

---
+++ tools/perf/core/benchmark_utils_unittest.py
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+import unittest
+
+from core import benchmark_utils
+
+from telemetry import page
+from telemetry import story
+from telemetry import benchmark as benchmark_module
+
+class ThousandAndOneStoriesBenchmark(benchmark_module.Benchmark):
+  def CreateStorySet(self, options):
+    del options  # unused
+    story_set = story.StorySet()
+    for i in range(1001):
+      story_set.AddStory(
+          page.Page(
+              url='file://does-not-exist.html', name='story-number-%i' % i,
+              page_set=story_set))
+    return story_set
+
+
+class BenchmarkUtilsUnittest(unittest.TestCase):
+  def testGetBenchmarkStoryNamesOrdering(self):
+    story_names = benchmark_utils.GetBenchmarkStoryNames(
+        ThousandAndOneStoriesBenchmark())
+    self.assertEquals(story_names, ['story-number-%i' % i for i in range(1001)])
