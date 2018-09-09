4ccdc92f38b57f6ae8929dd3b2f061d5fc5324a0
Thomas Tangl <tangltom@chromium.org>
2018-09-09 10:12:43 +0000

    [Dice] Fix highlighting of sync promo dropdown arrow
    Screenshot before:
    https://drive.google.com/file/d/1gxhx2eGVfqwOi6GQFmyMTPaJkVtWIZXL/view?usp=sharing
    Screenshot after:
    https://drive.google.com/file/d/1bvwkIfvfCu0Jo3H04HRISWSp0xNmotO1/view?usp=sharing
    
    Bug: 872807
    Change-Id: Ic0f6edee667e86661245105fd8f8a159f5674773
    Reviewed-on: https://chromium-review.googlesource.com/1213176
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Thomas Tangl <tangltom@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589798}

--- chrome/browser/ui/views/hover_button.cc
+++ chrome/browser/ui/views/hover_button.cc
@@ -291,7 +291,7 @@ void HoverButton::StateChanged(ButtonState old_state) {
 }
 
 bool HoverButton::ShouldUseFloodFillInkDrop() const {
-  return views::MenuButton::ShouldUseFloodFillInkDrop() || title_ != nullptr;
+  return true;
 }
 
 SkColor HoverButton::GetInkDropBaseColor() const {
