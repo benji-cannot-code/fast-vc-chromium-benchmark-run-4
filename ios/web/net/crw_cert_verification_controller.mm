87b19cb04ee991a83ee1e0a532dd070ac66e7746
Eugene But <eugenebut@google.com>
2018-09-10 21:16:37 +0000

    Remove UseWebThreadInCertVerificationController experiment.
    Experiment results were collected in M69.
    
    Bug: 853774
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: I44d71ec5a620b8ac5b9673161dbf9172cfc67fdf
    Reviewed-on: https://chromium-review.googlesource.com/1213268
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590064}

--- ios/web/net/crw_cert_verification_controller.mm
+++ ios/web/net/crw_cert_verification_controller.mm
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/post_task.h"
 #include "ios/web/public/browser_state.h"
 #include "ios/web/public/certificate_policy_cache.h"
-#include "ios/web/public/features.h"
 #include "ios/web/public/web_thread.h"
 #import "ios/web/web_state/wk_web_view_security_util.h"
 #include "net/cert/cert_verify_proc_ios.h"
@@ -199,18 +198,10 @@ decideLoadPolicyForRejectedTrustResult:(SecTrustResultType)trustResult
           trustResult = kSecTrustResultInvalid;
         }
         // Use GCD API, which is guaranteed to be called during shutdown.
-
-        if (base::FeatureList::IsEnabled(
-                web::features::kUseWebThreadInCertVerificationController)) {
-          web::WebThread::PostTask(web::WebThread::UI, FROM_HERE,
-                                   base::BindOnce(^{
-                                     completionHandler(trustResult);
-                                   }));
-        } else {
-          dispatch_async(dispatch_get_main_queue(), ^{
-            completionHandler(trustResult);
-          });
-        }
+        // See https://crbug.com/853774 for details.
+        dispatch_async(dispatch_get_main_queue(), ^{
+          completionHandler(trustResult);
+        });
       }));
 }
 
