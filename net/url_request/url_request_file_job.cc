6b2728769b6917785529d3b36d97dd93e56be6d7
Erik Luo <luoe@chromium.org>
2018-09-10 16:44:15 +0000

    DevTools: serve MimeType as ContentType for debug_devtools
    - Adds ShouldServeMimeTypeAsContentTypeHeader() on URLRequestFileJob
    - With debug_devtools, our interceptor now serves files with
      ContentType header, allowing DevTools to importScripts() on
      locally-served bundled files.
    - Without this CL, strict MIME-type checks prevent importScripts()
      in FormatterWorker, required for pretty-print and autocomplete.
    
    Bug: 882013
    Change-Id: Ib61284cfccd430dd9a4ddae769a416147c504e05
    Reviewed-on: https://chromium-review.googlesource.com/1214365
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Erik Luo <luoe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589944}

--- net/url_request/url_request_file_job.cc
+++ net/url_request/url_request_file_job.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/files/file_util.h"
 #include "base/strings/string_util.h"
+#include "base/strings/stringprintf.h"
 #include "base/synchronization/lock.h"
 #include "base/task_runner.h"
 #include "base/threading/thread_restrictions.h"
@@ -177,6 +178,17 @@ void URLRequestFileJob::SetExtraRequestHeaders(
       }
     }
   }
+}
+
+void URLRequestFileJob::GetResponseInfo(HttpResponseInfo* info) {
+  if (!serve_mime_type_as_content_type_ || !meta_info_.mime_type_result)
+    return;
+  auto headers =
+      base::MakeRefCounted<net::HttpResponseHeaders>("HTTP/1.1 200 OK");
+  headers->AddHeader(base::StringPrintf("%s: %s",
+                                        net::HttpRequestHeaders::kContentType,
+                                        meta_info_.mime_type.c_str()));
+  info->headers = headers;
 }
 
 void URLRequestFileJob::OnOpenComplete(int result) {}
