8c1cf2083f67ee3829637937947faeaccbf7c501
Xiaohui Chen <xiaohuic@google.com>
2018-09-10 22:12:16 +0000

    assistant: Hide invisible elements to avoid triggering screenreader
    When Views are invisible by opacity 0 but still have visibility true,
    screenreader would still be able to read it. We need to explicitly
    set their visibility to false.
    
    Bug: b:113731925
    Test: locally build and not able to navigate to these elements with
    Test: screen reader
    Change-Id: Ibb1b111844dbd6021c267ef594777614cf127189
    Reviewed-on: https://chromium-review.googlesource.com/1212342
    Reviewed-by: Muyuan Li <muyuanli@chromium.org>
    Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590085}

--- ash/assistant/ui/dialog_plate/dialog_plate.h
+++ ash/assistant/ui/dialog_plate/dialog_plate.h
@@ -106,7 +106,6 @@ class DialogPlate : public views::View,
   bool OnAnimationEnded(const ui::CallbackLayerAnimationObserver& observer);
 
   void SetFocus(InputModality modality);
-  void SetFocusMode(InputModality modality);
 
   AssistantController* const assistant_controller_;  // Owned by Shell.
 
