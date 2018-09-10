eac8f15aa68c6db50ffbf6b8609daa5ee9eecd0e
Christian Fremerey <chfremer@chromium.org>
2018-09-10 18:58:22 +0000

    [Video Capture, Android] Fix more nullptr exceptions
    Method getCameraParameters() in file VideoCaptureCamera.java may return
    null on failure. But several call sites assume that this never happens.
    Crash reports show that it does happen.
    
    This CL adds null checks and performs the appropriate failure action.
    
    Bug: 879678
    Change-Id: I978f45c04696c9a5b272932a945da033bafc6a77
    Reviewed-on: https://chromium-review.googlesource.com/1214072
    Reviewed-by: Emircan Uysaler <emircan@chromium.org>
    Commit-Queue: Christian Fremerey <chfremer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590007}

--- media/capture/video/android/video_capture_device_android.cc
+++ media/capture/video/android/video_capture_device_android.cc
@@ -386,6 +386,10 @@ void VideoCaptureDeviceAndroid::OnGetPhotoCapabilitiesReply(
     NOTREACHED() << "|callback_id| not found.";
     return;
   }
+  if (result == nullptr) {
+    get_photo_state_callbacks_.erase(reference_it);
+    return;
+  }
 
   base::android::ScopedJavaLocalRef<jobject> scoped_photo_capabilities(env,
                                                                        result);
