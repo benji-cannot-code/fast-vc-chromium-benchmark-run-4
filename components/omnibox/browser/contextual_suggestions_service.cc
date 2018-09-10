4e59d0d0954a8e9bfd5475d4151a1df2fcbba464
Jenny Zhang <jennyz@chromium.org>
2018-09-10 18:50:55 +0000

    Integrate app_list zero state suggestion with omnibox.
    1. Add query params pgcl=14 and sclient=cros-launcher for launcher queries to completion server.
    2. Enable ZeroSuggestProvider to process empty queries from cros launcher.
    3. The above changes are only enabled when zero state is enabled by flag, which by default is off.
    
    Bug: 854242
    Change-Id: I9ca99a668b74ab642ff1d020eb14565fb4af12ff
    Reviewed-on: https://chromium-review.googlesource.com/1178971
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Commit-Queue: Jenny Zhang <jennyz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590003}

--- components/omnibox/browser/contextual_suggestions_service.cc
+++ components/omnibox/browser/contextual_suggestions_service.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/values.h"
 #include "components/data_use_measurement/core/data_use_user_data.h"
+#include "components/omnibox/browser/base_search_provider.h"
 #include "components/omnibox/browser/omnibox_field_trial.h"
 #include "components/search_engines/template_url_service.h"
 #include "components/sync/base/time.h"
@@ -101,6 +102,7 @@ ContextualSuggestionsService::~ContextualSuggestionsService() {}
 void ContextualSuggestionsService::CreateContextualSuggestionsRequest(
     const std::string& current_url,
     const base::Time& visit_time,
+    const AutocompleteInput& input,
     const TemplateURLService* template_url_service,
     StartCallback start_callback,
     CompletionCallback completion_callback) {
@@ -111,7 +113,7 @@ void ContextualSuggestionsService::CreateContextualSuggestionsRequest(
                               std::move(start_callback),
                               std::move(completion_callback));
   else
-    CreateDefaultRequest(current_url, template_url_service,
+    CreateDefaultRequest(current_url, input, template_url_service,
                          std::move(start_callback),
                          std::move(completion_callback));
 }
@@ -124,6 +126,7 @@ void ContextualSuggestionsService::StopCreatingContextualSuggestionsRequest() {
 // static
 GURL ContextualSuggestionsService::ContextualSuggestionsUrl(
     const std::string& current_url,
+    const AutocompleteInput& input,
     const TemplateURLService* template_url_service) {
   if (template_url_service == nullptr) {
     return GURL();
@@ -144,6 +147,13 @@ GURL ContextualSuggestionsService::ContextualSuggestionsUrl(
   if (!current_url.empty()) {
     search_term_args.current_page_url = current_url;
   }
+
+  search_term_args.page_classification = input.current_page_classification();
+
+  // Append a specific suggest client in ChromeOS app_list launcher contexts.
+  BaseSearchProvider::AppendSuggestClientToAdditionalQueryParams(
+      search_engine, search_terms_data, input.current_page_classification(),
+      &search_term_args);
   return GURL(suggestion_url_ref.ReplaceSearchTerms(search_term_args,
                                                     search_terms_data));
 }
@@ -189,11 +199,12 @@ GURL ContextualSuggestionsService::ExperimentalContextualSuggestionsUrl(
 
 void ContextualSuggestionsService::CreateDefaultRequest(
     const std::string& current_url,
+    const AutocompleteInput& input,
     const TemplateURLService* template_url_service,
     StartCallback start_callback,
     CompletionCallback completion_callback) {
   const GURL suggest_url =
-      ContextualSuggestionsUrl(current_url, template_url_service);
+      ContextualSuggestionsUrl(current_url, input, template_url_service);
   DCHECK(suggest_url.is_valid());
 
   net::NetworkTrafficAnnotationTag traffic_annotation =
