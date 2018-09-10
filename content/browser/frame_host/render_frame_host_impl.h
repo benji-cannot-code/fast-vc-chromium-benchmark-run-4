2d4721d2fc76d1c6cc63b042f9970178579235b2
Ojan Vafai <ojan@chromium.org>
2018-09-10 21:42:27 +0000

    Revert "Implement crash report prototype (behind flag)"
    This reverts commit 0962c66ceed21aafea46092143bbecd853b5818c.
    
    Reason for revert: This test also fails on some mac bots, e.g. https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Mac10.12%20Tests/15266
    
    Original change's description:
    > Implement crash report prototype (behind flag)
    > 
    > This patch implements crash reporting, as detailed in the
    > reporting spec: https://w3c.github.io/reporting/#crash-report
    > 
    > Change-Id: I209880150193910a0947608e6b08a6eeea29206c
    > Reviewed-on: https://chromium-review.googlesource.com/1174965
    > Commit-Queue: Paul Meyer <paulmeyer@chromium.org>
    > Reviewed-by: Nick Carter <nick@chromium.org>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#589941}
    
    TBR=avi@chromium.org,nick@chromium.org,paulmeyer@chromium.org
    
    Change-Id: I52ea9472a94c7c5eaa13e29dcd3569e0db3c8280
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/1217513
    Reviewed-by: Ojan Vafai <ojan@chromium.org>
    Commit-Queue: Ojan Vafai <ojan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590073}

--- content/browser/frame_host/render_frame_host_impl.h
+++ content/browser/frame_host/render_frame_host_impl.h
@@ -24,10 +24,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "base/optional.h"
-#include "base/rand_util.h"
 #include "base/strings/string16.h"
 #include "base/supports_user_data.h"
 #include "base/time/time.h"
+#include "base/unguessable_token.h"
 #include "build/build_config.h"
 #include "content/browser/accessibility/browser_accessibility_manager.h"
 #include "content/browser/bad_message.h"
@@ -1294,10 +1294,6 @@ class CONTENT_EXPORT RenderFrameHostImpl
   // navigation, so it can be logged with the tracing system.
   std::unique_ptr<base::trace_event::TracedValue> CommitAsTracedValue(
       FrameHostMsg_DidCommitProvisionalLoad_Params* validated_params) const;
-
-  // Based on the termination |status|, may generate a crash report to be routed
-  // to the Reporting API.
-  void MaybeGenerateCrashReport(int status);
 
   // For now, RenderFrameHosts indirectly keep RenderViewHosts alive via a
   // refcount that calls Shutdown when it reaches zero.  This allows each
