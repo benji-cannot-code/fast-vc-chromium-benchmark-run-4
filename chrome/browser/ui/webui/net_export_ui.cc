7ecf8a56e13378167cc9d68bf8199fefbd61f9b0
Matt Menke <mmenke@chromium.org>
2018-09-10 18:36:39 +0000

    Remove unused URLRequestContextGetter typedef in net_export_ui.cc.
    Also remove corresponding header, and add some missing headers for used
    classes.
    
    Bug: None
    Change-Id: I2439a13b7995569a50a1f7f62aa5836589988c32
    Reviewed-on: https://chromium-review.googlesource.com/1216622
    Reviewed-by: Scott Chen <scottchen@chromium.org>
    Reviewed-by: Eric Roman <eroman@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589994}

--- chrome/browser/ui/webui/net_export_ui.cc
+++ chrome/browser/ui/webui/net_export_ui.cc
@@ -5,13 +5,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/webui/net_export_ui.h"
 
+#include <stdint.h>
+
 #include <memory>
 #include <string>
 #include <vector>
 
 #include "base/bind.h"
+#include "base/command_line.h"
 #include "base/lazy_instance.h"
 #include "base/macros.h"
+#include "base/memory/ref_counted.h"
 #include "base/scoped_observer.h"
 #include "base/single_thread_task_runner.h"
 #include "base/strings/string_util.h"
@@ -39,7 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_ui_message_handler.h"
 #include "extensions/buildflags/buildflags.h"
 #include "net/log/net_log_capture_mode.h"
-#include "net/url_request/url_request_context_getter.h"
 #include "ui/shell_dialogs/select_file_dialog.h"
 
 #if defined(OS_ANDROID)
@@ -107,9 +110,6 @@ class NetExportMessageHandler
   void OnNewState(const base::DictionaryValue& state) override;
 
  private:
-  using URLRequestContextGetterList =
-      std::vector<scoped_refptr<net::URLRequestContextGetter>>;
-
   // Send NetLog data via email.
   static void SendEmail(const base::FilePath& file_to_send);
 
