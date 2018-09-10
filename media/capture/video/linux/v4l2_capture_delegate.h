ae89b9e5c4b5f0b0a2ebb61ade81c8bf2d6d687c
Lei Zhang <thestig@chromium.org>
2018-09-10 18:32:42 +0000

    Fix nits in V4L2CaptureDelegate.
    Change-Id: I3716899d74877992ad3b85a82eda82a17dcf142e
    Reviewed-on: https://chromium-review.googlesource.com/1215049
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589990}

--- media/capture/video/linux/v4l2_capture_delegate.h
+++ media/capture/video/linux/v4l2_capture_delegate.h
@@ -9,6 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdint.h>
 
+#include <memory>
+#include <string>
+#include <vector>
+
 #include "base/containers/queue.h"
 #include "base/files/scoped_file.h"
 #include "base/macros.h"
