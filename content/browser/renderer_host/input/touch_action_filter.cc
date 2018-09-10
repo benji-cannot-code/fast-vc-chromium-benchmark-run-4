61ca74609d768d744aac4dbc54f591deebc78308
Xida Chen <xidachen@chromium.org>
2018-09-10 16:44:46 +0000

    [Debugging] Don't DumpWithoutCrash in TouchActionFilter
    In the TouchActionFilter, we maintain a num_of_active_touches_ and
    increment it at TouchSequenceStart and decrement it at TouchSequenceEnd.
    From the collected data, we have seen num_of_active_touches_ goes larger
    than 1, while it never goes smaller than 0.
    
    This CL removes the DumpWithoutCrash so that it won't generate a crash
    report as we have enough data.
    
    TBR=dtapuska@chromium.org
    
    Bug: 880021
    Change-Id: I0d097a4721f3f613f2d373eca408636f761ced17
    Reviewed-on: https://chromium-review.googlesource.com/1216027
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589945}

--- content/browser/renderer_host/input/touch_action_filter.cc
+++ content/browser/renderer_host/input/touch_action_filter.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <math.h>
 
 #include "base/debug/crash_logging.h"
-#include "base/debug/dump_without_crashing.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_macros.h"
 #include "third_party/blink/public/platform/web_gesture_event.h"
@@ -243,17 +242,10 @@ void TouchActionFilter::OnSetTouchAction(cc::TouchAction touch_action) {
 }
 
 void TouchActionFilter::IncreaseActiveTouches() {
-  // The touch start and associated touch end should be acked in order. If not,
-  // dump.
-  if (num_of_active_touches_ > 0)
-    base::debug::DumpWithoutCrashing();
   num_of_active_touches_++;
 }
 
 void TouchActionFilter::DecreaseActiveTouches() {
-  // Something is seriously wrong if this is true.
-  if (num_of_active_touches_ == 0)
-    base::debug::DumpWithoutCrashing();
   num_of_active_touches_--;
 }
 
