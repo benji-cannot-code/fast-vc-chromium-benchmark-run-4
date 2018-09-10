0259545dc25df387659f9f9f613b615d08b14931
Joanmarie Diggs <jdiggs@igalia.com>
2018-09-10 17:06:16 +0000

    Emit state-changed:selected events for ATK on Aura Linux
    * Add an event notifier in AXPlatformNodeAuraLinux for browser chrome.
    * Check to see if focus should follow selection. If it should, also
      emit state-changed:focused events.
    * Ensure that the correct states (focusable and selectable) wind up
      in the state set of elements where focus should follow selection.
    * Forward events from Blink content to AXPlatformNodeAuraLinux.
    * Create an event listener so that DumpAccessibilityEventsTest tests
      can be run. Add expectations for tests for which events are now
      emitted. At the present time, that is only focus and selected.
    
    R=dmazzoni@google.com
    
    Bug: 866340
    Change-Id: I0d3334c39b064afc75ac5e051969fb43a79a0d8a
    Reviewed-on: https://chromium-review.googlesource.com/1163724
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Joanmarie Diggs <joanmarie.diggs@gmail.com>
    Cr-Commit-Position: refs/heads/master@{#589951}

--- content/browser/accessibility/browser_accessibility_manager_auralinux.h
+++ content/browser/accessibility/browser_accessibility_manager_auralinux.h
@@ -35,6 +35,8 @@ class CONTENT_EXPORT BrowserAccessibilityManagerAuraLinux
   void FireGeneratedEvent(AXEventGenerator::Event event_type,
                           BrowserAccessibility* node) override;
 
+  void FireSelectedEvent(BrowserAccessibility* node);
+
   AtkObject* parent_object() { return parent_object_; }
 
  protected:
