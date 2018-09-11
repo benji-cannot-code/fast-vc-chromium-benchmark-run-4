b0edcb44d797bae6e04ec36ae0873589958de815
Jian Li <jianli@chromium.org>
2018-09-11 23:16:05 +0000

    Reshow offline indicator when chrome activity resumes
    Also shorten the time to wait for stable offline to 20s.
    
    Bug: 883020
    Change-Id: If932810e0d4864fe00bd2fec980175086b43afbd
    Reviewed-on: https://chromium-review.googlesource.com/1220049
    Reviewed-by: Peter Williamson <petewil@chromium.org>
    Commit-Queue: Jian Li <jianli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590527}

--- chrome/android/java/src/org/chromium/chrome/browser/offlinepages/indicator/TopSnackbarManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/offlinepages/indicator/TopSnackbarManager.java
@@ -45,7 +45,7 @@ public class TopSnackbarManager
 
     @Override
     public void onActivityStateChange(Activity activity, int newState) {
-        if (newState == ActivityState.STOPPED) {
+        if (newState == ActivityState.PAUSED || newState == ActivityState.STOPPED) {
             dismissSnackbar(false);
         }
     }
@@ -71,10 +71,10 @@ public class TopSnackbarManager
      * Shows a snackbar at the top of the given activity.
      */
     public void show(Snackbar snackbar, Activity activity) {
-        if (mSnackbar != null
-                || ApplicationStatus.getStateForActivity(activity) != ActivityState.RESUMED) {
-            return;
-        }
+        if (mSnackbar != null) return;
+        @ActivityState
+        int state = ApplicationStatus.getStateForActivity(activity);
+        if (state != ActivityState.STARTED && state != ActivityState.RESUMED) return;
 
         mActivity = activity;
         mSnackbar = snackbar;
