aa3ff9ad6c91004d0892642e4943f4be3d5d9500
Victor Costan <pwnall@chromium.org>
2018-09-11 18:44:07 +0000

    Roll src/third_party/leveldatabase/src/ f7b0e1d90..7b945f200 (6 commits)
    https://chromium.googlesource.com/external/leveldb.git/+log/f7b0e1d901da..7b945f200339
    
    Following up on leveldb cleanups, this CL also removes the ssize_t
    definition from port_chromium.h, and the OS_WIN macro definition that
    was required by it.
    
    $ git log f7b0e1d90..7b945f200 --date=short --no-merges --format='%ad %ae %s'
    2018-09-04 costan Clean up posix_logger.h.
    2018-09-04 costan Remove ssize_t from code that is not POSIX-specific.
    2018-09-04 costan Simplify Limiter in env_posix.cc.
    2018-09-04 costan Clarify comments for leveldb::Env file reading methods.
    2018-09-04 costan Remove GCC on OSX from the Travis CI matrix.
    2018-08-19 costan Expose WriteBatch::Append in the C API.
    
    Created with:
      roll-dep src/third_party/leveldatabase/src
    
    Change-Id: I4f045f15cdae69e15f5a52c05cff9e7671fb2753
    Reviewed-on: https://chromium-review.googlesource.com/1205472
    Reviewed-by: Chris Mumford <cmumford@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590418}

--- third_party/leveldatabase/port/port_chromium.h
+++ third_party/leveldatabase/port/port_chromium.h
@@ -26,10 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #  include <linux/unistd.h>
 #endif
 
-#if defined(OS_WIN)
-typedef SSIZE_T ssize_t;
-#endif
-
 namespace leveldb {
 namespace port {
 
