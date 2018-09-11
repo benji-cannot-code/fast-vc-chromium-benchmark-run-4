daba7bd80d20e658064de7b8345982acb935b884
Eric Seckler <eseckler@chromium.org>
2018-09-11 17:54:34 +0000

    content: Prepare unittests for PostTask refactor.
    This adds TestBrowserThreadBundles to unittests that will need it when
    we replace BrowserThread::PostTask() and friends with base/post_task.h.
    
    The TestBrowserThreadBundle initializes globals that are required for
    things like posting a task to or obtaining a TaskRunner for a BrowserThread.
    
    I'm making this change in a separate patch to make review of the
    future automated refactoring patches easier.
    
    Bug: 878356
    Change-Id: I100eefb16afa5490c9c7dee56c9a0bdb72501524
    Reviewed-on: https://chromium-review.googlesource.com/1208170
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Christian Fremerey <chfremer@chromium.org>
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590387}

--- content/browser/browser_main_loop_unittest.cc
+++ content/browser/browser_main_loop_unittest.cc
@@ -23,6 +23,7 @@ namespace content {
 TEST(BrowserMainLoopTest, CreateThreadsInSingleProcess) {
   {
     base::TaskScheduler::Create("Browser");
+    BrowserThreadImpl::CreateTaskExecutor();
     base::test::ScopedCommandLine scoped_command_line;
     scoped_command_line.GetProcessCommandLine()->AppendSwitch(
         switches::kSingleProcess);
@@ -42,6 +43,7 @@ TEST(BrowserMainLoopTest, CreateThreadsInSingleProcess) {
     BrowserThreadImpl::ResetGlobalsForTesting(
         static_cast<BrowserThread::ID>(id));
   }
+  BrowserThreadImpl::ResetTaskExecutorForTesting();
   base::TaskScheduler::GetInstance()->JoinForTesting();
   base::TaskScheduler::SetInstance(nullptr);
 }
