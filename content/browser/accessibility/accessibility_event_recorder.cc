89e567565594f2c90548b0b4557cad1793279430
Joanmarie Diggs <jdiggs@igalia.com>
2018-09-11 05:30:05 +0000

    Re-land: Emit state-changed:selected events for ATK on Aura Linux
    Originally landed: crrev.com/c/1163724
    Reverted: crrev.com/c/1217069
    
    The revert was due to the two new tests failing on Ubuntu 16.04,
    which has a newer version of ATK and thus different expectations.
    In order to run the same tests in both environments, prune states
    which are not essential to what is being tested from the state set
    of the expectations. This modification also eliminates the need to
    hand-edit new expectations which are generated on more recent systems.
    
    Original changes preserved in this re-land:
    * Add an event notifier in AXPlatformNodeAuraLinux for browser chrome.
    * Check to see if focus should follow selection. If it should, also
      emit state-changed:focused events.
    * Ensure that the correct states (focusable and selectable) wind up
      in the state set of elements where focus should follow selection.
    * Forward events from Blink content to AXPlatformNodeAuraLinux.
    * Create an event listener so that DumpAccessibilityEventsTest tests
      can be run. Add expectations for tests for which events are now
      emitted. At the present time, that is only focus and selected.
    
    R=dmazzoni@google.com
    
    Bug: 866340
    Change-Id: I371ad0a1516aceb6c6924d0142a6f3268dfdd3ac
    Reviewed-on: https://chromium-review.googlesource.com/1217744
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590209}

--- content/browser/accessibility/accessibility_event_recorder.cc
+++ content/browser/accessibility/accessibility_event_recorder.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/accessibility/accessibility_event_recorder.h"
 
 #include "build/build_config.h"
+#include "content/browser/accessibility/accessibility_buildflags.h"
 #include "content/browser/accessibility/browser_accessibility_manager.h"
 
 namespace content {
@@ -17,7 +18,7 @@ AccessibilityEventRecorder::AccessibilityEventRecorder(
 
 AccessibilityEventRecorder::~AccessibilityEventRecorder() = default;
 
-#if !defined(OS_WIN) && !defined(OS_MACOSX)
+#if !defined(OS_WIN) && !defined(OS_MACOSX) && !BUILDFLAG(USE_ATK)
 // static
 AccessibilityEventRecorder& AccessibilityEventRecorder::GetInstance(
     BrowserAccessibilityManager* manager,
