9c691ff03220392a1a131b236ee806400c690d40
Stuart Langley <slangley@google.com>
2018-09-11 01:04:16 +0000

    Change GetAvailableSpaceCallback from RepeatingCallback to OnceCallback.
    Part of the ongoing work to cleanup the filesystem callbacks to be OnceCallback
    where possible.
    
    No logic changes.
    
    Bug: 875700
    Change-Id: I69c5518eaee657e0ce42d8bde3687696fa9047b9
    Reviewed-on: https://chromium-review.googlesource.com/1217644
    Reviewed-by: Anand Mistry <amistry@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590152}

--- components/drive/chromeos/fake_file_system.cc
+++ components/drive/chromeos/fake_file_system.cc
@@ -205,8 +205,7 @@ void FakeFileSystem::SearchByHashes(const std::set<std::string>& hashes,
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
 }
 
-void FakeFileSystem::GetAvailableSpace(
-    const GetAvailableSpaceCallback& callback) {
+void FakeFileSystem::GetAvailableSpace(GetAvailableSpaceCallback callback) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
 }
 
