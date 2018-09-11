e671a8203e41c1c8074705125612ec3618e8aaef
Juan Antonio Navarro Perez <perezju@chromium.org>
2018-09-11 17:02:13 +0000

    [tools/perf] Disable Surface Synchronization on all WebView variants
    The previous tweak did not disable all possible webview browsers like,
    e.g., android-webview-google.
    
    Bug: 879151
    Change-Id: Ic2b0e22b06893d7e7e769dd50fb86f46bbc9c936
    Reviewed-on: https://chromium-review.googlesource.com/1219614
    Commit-Queue: Ned Nguyen <nednguyen@google.com>
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Cr-Commit-Position: refs/heads/master@{#590355}

--- tools/perf/core/perf_benchmark.py
+++ tools/perf/core/perf_benchmark.py
@@ -109,7 +109,7 @@ class PerfBenchmark(benchmark.Benchmark):
 
     # TODO(crbug.com/881469): remove this once Webview support surface
     # synchronization.
-    if options.browser_type == 'android-webview':
+    if 'android-webview' in options.browser_type:
       options.AppendExtraBrowserArgs(
           '--disable-features=SurfaceSynchronization')
     self.SetExtraBrowserOptions(options)
