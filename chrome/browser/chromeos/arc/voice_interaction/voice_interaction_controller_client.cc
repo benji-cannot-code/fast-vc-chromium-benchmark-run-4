c4c9fd4ccc02675a47039ddf50760df94e11aaf4
wutao <wutao@chromium.org>
2018-09-11 17:43:59 +0000

    Fix reminder notification on the first time
    The notification pref is sent when device starts. At this time the
    kVoiceInteractionEnabled is not enabled for the first time user.
    
    Bug: 881608, b/112853995
    Test: manual.
    Change-Id: I4753dbaa79dca7aaa0dd0781bbee25abef825371
    Reviewed-on: https://chromium-review.googlesource.com/1211876
    Reviewed-by: Muyuan Li <muyuanli@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590380}

--- chrome/browser/chromeos/arc/voice_interaction/voice_interaction_controller_client.cc
+++ chrome/browser/chromeos/arc/voice_interaction/voice_interaction_controller_client.cc
@@ -131,8 +131,6 @@ void VoiceInteractionControllerClient::NotifyFeatureAllowed() {
 void VoiceInteractionControllerClient::NotifyNotificationEnabled() {
   DCHECK(profile_);
   PrefService* prefs = profile_->GetPrefs();
-  // Make sure voice interaction is enabled.
-  DCHECK(prefs->GetBoolean(prefs::kVoiceInteractionEnabled));
   bool enabled = prefs->GetBoolean(prefs::kVoiceInteractionNotificationEnabled);
   voice_interaction_controller_->NotifyNotificationEnabled(enabled);
 }
@@ -220,10 +218,9 @@ void VoiceInteractionControllerClient::SetProfile(Profile* profile) {
   NotifySettingsEnabled();
   NotifyContextEnabled();
   NotifyLocaleChanged();
-  if (prefs->GetBoolean(prefs::kVoiceInteractionEnabled)) {
+  NotifyNotificationEnabled();
+  if (prefs->GetBoolean(prefs::kVoiceInteractionEnabled))
     NotifyHotwordEnabled();
-    NotifyNotificationEnabled();
-  }
 }
 
 void VoiceInteractionControllerClient::Observe(
