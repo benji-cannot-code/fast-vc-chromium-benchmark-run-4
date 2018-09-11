98fb2361cca928cd46e7236792eb12e8420c1e26
Tetsui Ohkubo <tetsui@chromium.org>
2018-09-11 02:40:05 +0000

    Unified: Change timing of stacking counter
    This CL changes timing of stacking notification counter to appear in
    UnifiedSystemTray. Combined with https://crrev.com/c/1215507, it fixes
    top corner artifact of ARC++ notifications in UnifiedSystemTray.
    
    TEST=manual
    BUG=866777
    
    Change-Id: I57f0ca846b129c18e7500d7738375644ff82157b
    Reviewed-on: https://chromium-review.googlesource.com/1215602
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590177}

--- ash/message_center/message_list_view.cc
+++ ash/message_center/message_list_view.cc
@@ -344,7 +344,7 @@ void MessageListView::SetBorderPadding() {
       gfx::Insets(message_center::kMarginBetweenItemsInList)));
 }
 
-int MessageListView::GetCountAboveVisibleRect() const {
+int MessageListView::GetCountAboveVisibleRect(int y_offset) const {
   DCHECK(scroller_);
 
   int height = 0;
@@ -359,7 +359,7 @@ int MessageListView::GetCountAboveVisibleRect() const {
     height += child->bounds().height() + padding;
     padding = GetMarginBetweenItems();
 
-    if (height >= scroller_->GetVisibleRect().y())
+    if (height >= scroller_->GetVisibleRect().y() + y_offset)
       return i;
   }
   return child_count();
