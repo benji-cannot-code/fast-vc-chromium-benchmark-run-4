3ce8ae553015aa1e30f7d55d0e8317536300dae3
Christian Biesinger <cbiesinger@chromium.org>
2018-09-11 21:08:21 +0000

    [layoutng] Add move constructors to ng_exclusion_space.h for performance
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng
    Change-Id: I2c47b2a941163b30b3a37bd9ef2c08c9b9ccd2ed
    Reviewed-on: https://chromium-review.googlesource.com/1212382
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590472}

--- third_party/blink/renderer/core/layout/ng/exclusions/ng_exclusion_space.h
+++ third_party/blink/renderer/core/layout/ng/exclusions/ng_exclusion_space.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/ng/geometry/ng_bfc_rect.h"
 #include "third_party/blink/renderer/core/style/computed_style_constants.h"
 #include "third_party/blink/renderer/platform/layout_unit.h"
+#include "third_party/blink/renderer/platform/wtf/allocator.h"
 #include "third_party/blink/renderer/platform/wtf/ref_vector.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 
@@ -31,6 +32,8 @@ class CORE_EXPORT NGExclusionSpace {
  public:
   NGExclusionSpace();
   NGExclusionSpace(const NGExclusionSpace&);
+  NGExclusionSpace(NGExclusionSpace&&) noexcept;
+  NGExclusionSpace& operator=(const NGExclusionSpace&);
   ~NGExclusionSpace(){};
 
   void Add(scoped_refptr<const NGExclusion> exclusion);
@@ -144,6 +147,26 @@ class CORE_EXPORT NGExclusionSpace {
               base::AdoptRef(new NGShapeExclusions(*other.shape_exclusions))),
           has_shape_exclusions(other.has_shape_exclusions) {}
 
+    NGShelf(NGShelf&& other) noexcept
+        : block_offset(other.block_offset),
+          line_left(other.line_left),
+          line_right(other.line_right),
+          line_left_edges(std::move(other.line_left_edges)),
+          line_right_edges(std::move(other.line_right_edges)),
+          shape_exclusions(std::move(other.shape_exclusions)),
+          has_shape_exclusions(other.has_shape_exclusions) {}
+
+    NGShelf& operator=(NGShelf&& other) noexcept {
+      block_offset = other.block_offset;
+      line_left = other.line_left;
+      line_right = other.line_right;
+      line_left_edges = std::move(other.line_left_edges);
+      line_right_edges = std::move(other.line_right_edges);
+      shape_exclusions = std::move(other.shape_exclusions);
+      has_shape_exclusions = other.has_shape_exclusions;
+      return *this;
+    }
+
     LayoutUnit block_offset;
     LayoutUnit line_left;
     LayoutUnit line_right;
@@ -193,6 +216,12 @@ class CORE_EXPORT NGExclusionSpace {
   // derived_geometry_ data-structure.
   struct DerivedGeometry {
     DerivedGeometry();
+    DerivedGeometry(DerivedGeometry&& o) noexcept
+        : shelves_(std::move(o.shelves_)),
+          opportunities_(std::move(o.opportunities_)),
+          last_float_block_start_(o.last_float_block_start_),
+          left_float_clear_offset_(o.left_float_clear_offset_),
+          right_float_clear_offset_(o.right_float_clear_offset_) {}
 
     void Add(const NGExclusion& exclusion);
 
