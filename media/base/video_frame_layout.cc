6f58e6288a88c9784e0f077fceaf1fe7a4513beb
Hirokazu Honda <hiroh@chromium.org>
2018-09-11 08:54:27 +0000

    VideoFrameLayout: Add offsets and aggregate strides and offsets as Plane
    VideoFrameLayout should have information about offset per plane, not only
    stride. The number of stride should be the same as the number of offsets. To
    guarantee this, they are stored in the vector of struct, Plane.
    
    BUG=chromium:876986, chromium:856562
    TEST=VDA unittest and media_unittest
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: Ie9d9afa5db7dabaee2f48fbcd355c13d29be8035
    Reviewed-on: https://chromium-review.googlesource.com/1188730
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590242}

--- media/base/video_frame_layout.cc
+++ media/base/video_frame_layout.cc
@@ -8,7 +8,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <numeric>
 #include <sstream>
 
+namespace media {
+
 namespace {
+
+std::ostringstream& operator<<(std::ostringstream& ostrm,
+                               const media::VideoFrameLayout::Plane& plane) {
+  ostrm << "(" << plane.stride << ", " << plane.offset << ")";
+  return ostrm;
+}
 
 template <class T>
 std::string VectorToString(const std::vector<T>& vec) {
@@ -16,7 +24,8 @@ std::string VectorToString(const std::vector<T>& vec) {
   std::string delim;
   result << "[";
   for (auto v : vec) {
-    result << delim << v;
+    result << delim;
+    result << v;
     if (delim.size() == 0)
       delim = ", ";
   }
@@ -24,9 +33,16 @@ std::string VectorToString(const std::vector<T>& vec) {
   return result.str();
 }
 
-}  // namespace
+std::vector<VideoFrameLayout::Plane> PlanesFromStrides(
+    const std::vector<int32_t> strides) {
+  std::vector<VideoFrameLayout::Plane> planes(strides.size());
+  for (size_t i = 0; i < strides.size(); i++) {
+    planes[i].stride = strides[i];
+  }
+  return planes;
+}
 
-namespace media {
+}  // namespace
 
 VideoFrameLayout::VideoFrameLayout(VideoPixelFormat format,
                                    const gfx::Size& coded_size,
@@ -34,14 +50,22 @@ VideoFrameLayout::VideoFrameLayout(VideoPixelFormat format,
                                    std::vector<size_t> buffer_sizes)
     : format_(format),
       coded_size_(coded_size),
-      strides_(std::move(strides)),
+      planes_(PlanesFromStrides(strides)),
+      buffer_sizes_(std::move(buffer_sizes)) {}
+
+VideoFrameLayout::VideoFrameLayout(VideoPixelFormat format,
+                                   const gfx::Size& coded_size,
+                                   std::vector<Plane> planes,
+                                   std::vector<size_t> buffer_sizes)
+    : format_(format),
+      coded_size_(coded_size),
+      planes_(std::move(planes)),
       buffer_sizes_(std::move(buffer_sizes)) {}
 
 VideoFrameLayout::VideoFrameLayout()
     : format_(PIXEL_FORMAT_UNKNOWN),
-      coded_size_(),
-      strides_({0, 0, 0, 0}),
-      buffer_sizes_({0, 0, 0, 0}) {}
+      planes_(kDefaultPlaneCount),
+      buffer_sizes_(kDefaultBufferCount, 0) {}
 
 VideoFrameLayout::~VideoFrameLayout() = default;
 VideoFrameLayout::VideoFrameLayout(const VideoFrameLayout&) = default;
@@ -55,12 +79,12 @@ size_t VideoFrameLayout::GetTotalBufferSize() const {
 
 std::string VideoFrameLayout::ToString() const {
   std::ostringstream s;
-  s << "VideoFrameLayout format:" << VideoPixelFormatToString(format_)
-    << " coded_size:" << coded_size_.ToString()
-    << " num_buffers:" << num_buffers()
-    << " buffer_sizes:" << VectorToString(buffer_sizes_)
-    << " num_strides:" << num_strides()
-    << " strides:" << VectorToString(strides_);
+  s << "VideoFrameLayout format: " << VideoPixelFormatToString(format_)
+    << ", coded_size: " << coded_size_.ToString()
+    << ", num_buffers: " << num_buffers()
+    << ", buffer_sizes: " << VectorToString(buffer_sizes_)
+    << ", num_planes: " << num_planes()
+    << ", planes (stride, offset): " << VectorToString(planes_);
   return s.str();
 }
 
