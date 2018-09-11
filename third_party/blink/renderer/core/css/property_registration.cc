a666e95823e14dc3b1539dbe7bbe3f6798a5bfc2
Anders Hartvoll Ruud <andruud@chromium.org>
2018-09-11 11:59:05 +0000

    Remove PropertyRegistration::Create.
    This was added recently for testing purposes, but I wasn't aware of
    FRIEND_TEST_ALL_PREFIXES at the time. PropertyRegistration instances must
    never exist unless CSSVariables2 is enabled, and I've been worried this
    might accidentally happen ever since ::Create was added.
    
    Added bonus DCHECK for even more inner peace.
    
    R=futhark@chromium.org
    
    Bug: 641877
    Change-Id: I61780ec25fb09673a0f3325775ce1b891b0867cb
    Reviewed-on: https://chromium-review.googlesource.com/1218702
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Anders Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590267}

--- third_party/blink/renderer/core/css/property_registration.cc
+++ third_party/blink/renderer/core/css/property_registration.cc
@@ -23,16 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-PropertyRegistration* PropertyRegistration::Create(
-    const AtomicString& name,
-    const CSSSyntaxDescriptor& syntax,
-    bool inherits,
-    const CSSValue* initial,
-    scoped_refptr<CSSVariableData> initial_variable_data) {
-  return new PropertyRegistration(name, syntax, inherits, initial,
-                                  initial_variable_data);
-}
-
 const PropertyRegistration* PropertyRegistration::From(
     const ExecutionContext* execution_context,
     const AtomicString& property_name) {
@@ -57,7 +47,9 @@ PropertyRegistration::PropertyRegistration(
           CSSInterpolationTypesMap::CreateInterpolationTypesForCSSSyntax(
               name,
               syntax,
-              *this)) {}
+              *this)) {
+  DCHECK(RuntimeEnabledFeatures::CSSVariables2Enabled());
+}
 
 static bool ComputationallyIndependent(const CSSValue& value) {
   DCHECK(!value.IsCSSWideKeyword());
