d40e2cfc65be9c3e79e6f38d8c29b2cc7e43b066
Jialiu Lin <jialiul@chromium.org>
2018-09-11 19:00:22 +0000

    Update advanced protection status upon Chrome OS online sign-in
    If an OAuth2 token is fetched during Chrome OS sign-in, we update
    the advanced protection status accordingly for the primary AccountInfo
    (in AccountTrackerService).
    
    Bug: 866620, 882576
    Change-Id: I729a7c47b8b3e45589c03df0fe8b8c9ab7c4084e
    Reviewed-on: https://chromium-review.googlesource.com/1205107
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Jialiu Lin <jialiul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590427}

--- google_apis/gaia/fake_gaia.cc
+++ google_apis/gaia/fake_gaia.cc
@@ -731,6 +731,7 @@ void FakeGaia::HandleAuthToken(const HttpRequest& request,
       base::DictionaryValue response_dict;
       response_dict.SetString("access_token", token_info->token);
       response_dict.SetInteger("expires_in", 3600);
+      response_dict.SetString("id_token", token_info->id_token);
       FormatJSONResponse(response_dict, http_response);
       return;
     }
@@ -760,6 +761,7 @@ void FakeGaia::HandleTokenInfo(const HttpRequest& request,
     response_dict.SetString("scope", base::JoinString(scope_vector, " "));
     response_dict.SetInteger("expires_in", token_info->expires_in);
     response_dict.SetString("email", token_info->email);
+    response_dict.SetString("id_token", token_info->id_token);
     FormatJSONResponse(response_dict, http_response);
   } else {
     http_response->set_code(net::HTTP_BAD_REQUEST);
@@ -782,6 +784,7 @@ void FakeGaia::HandleIssueToken(const HttpRequest& request,
       response_dict.SetString("expiresIn",
                               base::IntToString(token_info->expires_in));
       response_dict.SetString("token", token_info->token);
+      response_dict.SetString("id_token", token_info->id_token);
       FormatJSONResponse(response_dict, http_response);
       return;
     }
@@ -820,6 +823,7 @@ void FakeGaia::HandleOAuthUserInfo(
     response_dict.SetString("id", GetGaiaIdOfEmail(token_info->email));
     response_dict.SetString("email", token_info->email);
     response_dict.SetString("verified_email", token_info->email);
+    response_dict.SetString("id_token", token_info->id_token);
     FormatJSONResponse(response_dict, http_response);
   } else {
     http_response->set_code(net::HTTP_BAD_REQUEST);
