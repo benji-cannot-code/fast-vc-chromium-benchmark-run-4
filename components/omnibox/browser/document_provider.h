f276b304749702a542fa9f4fa3bdaa53d421931e
Travis Skare <skare@chromium.org>
2018-09-11 23:51:05 +0000

    [Omnibox] Documents - check unity opt-in as prereq.
    Falls back to sync if unified consent is not given, or hasn't been offered.
    
    Also checks on offering search suggestions, since that seems logical.
    
    Bug: 877850
    
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: I8daab9213c3a6b71b2bd8a1dd921973b51b7db8e
    Reviewed-on: https://chromium-review.googlesource.com/1189702
    Commit-Queue: Travis Skare <skare@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Reviewed-by: Thomas Tangl <tangltom@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590535}

--- components/omnibox/browser/document_provider.h
+++ components/omnibox/browser/document_provider.h
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class AutocompleteProviderListener;
 class AutocompleteProviderClient;
-class PrefService;
 
 namespace base {
 class Value;
@@ -60,6 +59,8 @@ class DocumentProvider : public AutocompleteProvider {
   FRIEND_TEST_ALL_PREFIXES(DocumentProviderTest,
                            CheckFeaturePrerequisiteNoIncognito);
   FRIEND_TEST_ALL_PREFIXES(DocumentProviderTest,
+                           CheckFeaturePrerequisiteNoConsentBit);
+  FRIEND_TEST_ALL_PREFIXES(DocumentProviderTest,
                            CheckFeaturePrerequisiteClientSettingOff);
   FRIEND_TEST_ALL_PREFIXES(DocumentProviderTest,
                            CheckFeaturePrerequisiteDefaultSearch);
@@ -78,13 +79,9 @@ class DocumentProvider : public AutocompleteProvider {
 
   ~DocumentProvider() override;
 
-  // Using this provider requires the user is signed-in and has Google set as
-  // default search engine.
-  bool IsDocumentProviderAllowed(
-      PrefService* prefs,
-      bool is_incognito,
-      bool is_authenticated,
-      const TemplateURLService* template_url_service);
+  // Determines whether the profile/session/window meet the feature
+  // prerequisites.
+  bool IsDocumentProviderAllowed(AutocompleteProviderClient* client);
 
   // Determines if the input is a URL, or is the start of the user entering one.
   // We avoid queries for these cases for quality and scaling reasons.
--- ios/chrome/browser/autocomplete/autocomplete_provider_client_impl.cc
+++ ios/chrome/browser/autocomplete/autocomplete_provider_client_impl.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service.h"
 #include "components/signin/core/browser/signin_manager.h"
 #include "components/sync/driver/sync_service.h"
+#include "components/unified_consent/unified_consent_service.h"
 #include "components/unified_consent/url_keyed_data_collection_consent_helper.h"
 #include "ios/chrome/browser/application_context.h"
 #include "ios/chrome/browser/autocomplete/autocomplete_classifier_factory.h"
@@ -27,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/search_engines/template_url_service_factory.h"
 #include "ios/chrome/browser/signin/signin_manager_factory.h"
 #include "ios/chrome/browser/sync/profile_sync_service_factory.h"
+#include "ios/chrome/browser/unified_consent/unified_consent_service_factory.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 
 AutocompleteProviderClientImpl::AutocompleteProviderClientImpl(
@@ -168,6 +170,18 @@ bool AutocompleteProviderClientImpl::IsAuthenticated() const {
   SigninManagerBase* signin_manager =
       ios::SigninManagerFactory::GetForBrowserState(browser_state_);
   return signin_manager != nullptr && signin_manager->IsAuthenticated();
+}
+
+bool AutocompleteProviderClientImpl::IsUnifiedConsentGiven() const {
+  unified_consent::UnifiedConsentService* consent_service =
+      UnifiedConsentServiceFactory::GetForBrowserState(browser_state_);
+  return consent_service && consent_service->IsUnifiedConsentGiven();
+}
+
+bool AutocompleteProviderClientImpl::IsSyncActive() const {
+  syncer::SyncService* sync =
+      ProfileSyncServiceFactory::GetForBrowserState(browser_state_);
+  return sync && sync->IsSyncActive();
 }
 
 void AutocompleteProviderClientImpl::Classify(
