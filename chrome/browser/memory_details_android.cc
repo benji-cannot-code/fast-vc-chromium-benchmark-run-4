287f6127e2b0ef2dfed024c52078feee6f21df69
Etienne Pierre-doray <etiennep@chromium.org>
2018-09-11 21:55:46 +0000

    [TaskScheduler]: Use ScopedBlockingCall to mark blocking tasks.
    This CL uses ScopedBlockingCall to mark blocking calls in /chrome/browser/memory_details_android.cc.
    
    This CL was created by replacing calls to AssertBlockingAllowed()
    with instantiations of ScopedBlockingCall(MAY_BLOCK).
    I kindly ask the reviewer to make sure of the following:
      - ScopedBlockingCall is instantiated in a scope with minimal CPU usage.
        If this is not the case, ScopedBlockingCall should be instantiated
        closer to the blocking call. See scoped_blocking_call.h for more
        info. Please let me know when/where the blocking call happens if this needs
        to be changed.
      - Parameter |blocking_type| matches expectation (MAY_BLOCK/WILL_BLOCK). See
        BlockingType for more info. While I assumed MAY_BLOCK by default, that might
        not be the best fit if we know that this callsite is guaranteed to block.
      - The ScopedBlockingCall's scope covers the entirety of the blocking operation
        previously asserted against by the AssertBlockingAllowed().
    
    This CL was uploaded by git cl split.
    
    R=dtrainor@chromium.org
    
    Bug: 874080
    Change-Id: I26104d2e0bc7817b955152a60a8ee4c97b033079
    Reviewed-on: https://chromium-review.googlesource.com/1191418
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590497}

--- chrome/browser/memory_details_android.cc
+++ chrome/browser/memory_details_android.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/process/process_iterator.h"
 #include "base/strings/utf_string_conversions.h"
-#include "base/threading/thread_restrictions.h"
+#include "base/threading/scoped_blocking_call.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/grit/chromium_strings.h"
 #include "content/public/browser/browser_thread.h"
@@ -114,7 +114,7 @@ ProcessData* MemoryDetails::ChromeBrowser() {
 
 void MemoryDetails::CollectProcessData(
     const std::vector<ProcessMemoryInformation>& chrome_processes) {
-  base::AssertBlockingAllowed();
+  base::ScopedBlockingCall scoped_blocking_call(base::BlockingType::WILL_BLOCK);
 
   std::vector<ProcessMemoryInformation> all_processes(chrome_processes);
   AddNonChildChromeProcesses(&all_processes);
