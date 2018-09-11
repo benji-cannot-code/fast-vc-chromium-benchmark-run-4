8e06c5a1478ec7312d6d65f0de9635c81c9beec5
Aaron Leventhal <aleventhal@chromium.org>
2018-09-11 14:17:42 +0000

    Relax aria-activedescendant validation
    Removing the role check for aria-activedescendant support simplifies the
    code and decreases differences with Firefox.
    
    Bug: None
    Change-Id: Ie497d29434344152d1e51d27a9301e31e332b1d9
    Reviewed-on: https://chromium-review.googlesource.com/1213539
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590294}

--- third_party/blink/renderer/modules/accessibility/ax_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_object.cc
@@ -1291,8 +1291,7 @@ bool AXObject::AncestorExposesActiveDescendant() const {
   if (!parent)
     return false;
 
-  if (parent->SupportsARIAActiveDescendant() &&
-      parent->GetAOMPropertyOrARIAAttribute(
+  if (parent->GetAOMPropertyOrARIAAttribute(
           AOMRelationProperty::kActiveDescendant)) {
     return true;
   }
@@ -1733,34 +1732,6 @@ bool AXObject::AriaPressedIsPresent() const {
 bool AXObject::AriaCheckedIsPresent() const {
   AtomicString result;
   return HasAOMPropertyOrARIAAttribute(AOMStringProperty::kChecked, result);
-}
-
-bool AXObject::SupportsARIAActiveDescendant() const {
-  // According to the ARIA Spec, all ARIA composite widgets, ARIA text boxes,
-  // ARIA groups and ARIA application should be able to expose an active descendant.
-  // Implicitly, <input> and <textarea> elements should also have this ability.
-  switch (RoleValue()) {
-    case kComboBoxGroupingRole:
-    case kComboBoxMenuButtonRole:
-    case kGridRole:
-    case kGroupRole:
-    case kListBoxRole:
-    case kMenuRole:
-    case kMenuBarRole:
-    case kRadioGroupRole:
-    case kRowRole:
-    case kSearchBoxRole:
-    case kTabListRole:
-    case kTextFieldRole:
-    case kTextFieldWithComboBoxRole:
-    case kToolbarRole:
-    case kTreeRole:
-    case kTreeGridRole:
-    case kApplicationRole:
-      return true;
-    default:
-      return false;
-  }
 }
 
 bool AXObject::SupportsARIAExpanded() const {
