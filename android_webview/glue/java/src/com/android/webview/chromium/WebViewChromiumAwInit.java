90b6297dc140c41e8a95cb4d7be9e0217bbe118e
Changwan Ryu <changwan@google.com>
2018-09-11 21:50:14 +0000

    Fix class verification errors around field access
    There is no logic change - this just move the code around for
    optimization.
    
    This CL specifically fixes verification errors around field access.
    Also, it avoids extra type conversion which was causing another
    verification error.
    
    See crbug.com/838702 for more information about run-time verification
    issue.
    
    Bug: 868250
    Change-Id: I00c593e9526ddc3766d0c5b203ed38816c5c6da0
    Reviewed-on: https://chromium-review.googlesource.com/1218522
    Commit-Queue: Changwan Ryu <changwan@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590491}

--- android_webview/glue/java/src/com/android/webview/chromium/WebViewChromiumAwInit.java
+++ android_webview/glue/java/src/com/android/webview/chromium/WebViewChromiumAwInit.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package com.android.webview.chromium;
 
 import android.Manifest;
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
@@ -40,6 +41,7 @@ import org.chromium.base.ContextUtils;
 import org.chromium.base.PathService;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.TraceEvent;
+import org.chromium.base.annotations.DoNotInline;
 import org.chromium.base.library_loader.LibraryLoader;
 import org.chromium.base.library_loader.LibraryProcessType;
 import org.chromium.base.library_loader.ProcessInitException;
@@ -57,13 +59,27 @@ public class WebViewChromiumAwInit {
 
     private static final String HTTP_AUTH_DATABASE_FILE = "http_auth.db";
 
+    /**
+     * This holds objects of classes that are defined in N and above to ensure that run-time class
+     * verification does not occur until it is actually used for N and above.
+     */
+    @TargetApi(Build.VERSION_CODES.N)
+    @DoNotInline
+    private static class ObjectHolderForN {
+        public TokenBindingService mTokenBindingService;
+    }
+
     // TODO(gsennton): store aw-objects instead of adapters here
     // Initialization guarded by mLock.
     private AwBrowserContext mBrowserContext;
     private SharedStatics mSharedStatics;
     private GeolocationPermissionsAdapter mGeolocationPermissions;
     private CookieManagerAdapter mCookieManager;
-    private Object mTokenBindingManager;
+
+    @TargetApi(Build.VERSION_CODES.N)
+    private ObjectHolderForN mObjectHolderForN =
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.N ? new ObjectHolderForN() : null;
+
     private WebIconDatabaseAdapter mWebIconDatabase;
     private WebStorageAdapter mWebStorage;
     private WebViewDatabaseAdapter mWebViewDatabase;
@@ -372,13 +388,15 @@ public class WebViewChromiumAwInit {
         return mServiceWorkerController;
     }
 
+    @TargetApi(Build.VERSION_CODES.N)
     public TokenBindingService getTokenBindingService() {
         synchronized (mLock) {
-            if (mTokenBindingManager == null) {
-                mTokenBindingManager = GlueApiHelperForN.createTokenBindingManagerAdapter(mFactory);
+            if (mObjectHolderForN.mTokenBindingService == null) {
+                mObjectHolderForN.mTokenBindingService =
+                        GlueApiHelperForN.createTokenBindingManagerAdapter(mFactory);
             }
         }
-        return (TokenBindingService) mTokenBindingManager;
+        return mObjectHolderForN.mTokenBindingService;
     }
 
     public android.webkit.WebIconDatabase getWebIconDatabase() {
