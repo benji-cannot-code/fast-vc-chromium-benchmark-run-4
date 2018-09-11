c24443b86dea69d6631222044861d4254228b3d9
Matthew Jones <mdjones@chromium.org>
2018-09-11 21:48:29 +0000

    Remove unused independent animation from LayoutTab
    This patch reomves some obsolete code that was used to transition
    theme colors on the tab border in the tab switcher. This feature
    has since been removed.
    
    Bug: 882508
    Change-Id: Ie63fabb08631f0377721f7057cbe65a4cd3af9a2
    Reviewed-on: https://chromium-review.googlesource.com/1217405
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590488}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/components/LayoutTab.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/components/LayoutTab.java
@@ -126,11 +126,6 @@ public class LayoutTab implements ChromeAnimation.Animatable {
      */
     private boolean mInitFromHostCalled;
 
-    /** The animation set specific to this LayoutTab. */
-    private ChromeAnimation<ChromeAnimation.Animatable> mCurrentAnimations;
-    private int mInitialThemeColor;
-    private int mFinalThemeColor;
-
     // All the members bellow are initialized from the delayed initialization.
     //
     // Begin section --------------
@@ -240,15 +235,6 @@ public class LayoutTab implements ChromeAnimation.Animatable {
         mInitFromHostCalled = true;
 
         return needsUpdate;
-    }
-
-    /**
-     * Update any animation controlled by this object.
-     * @param time The current app time in ms.
-     * @return Whether the animations controlled by this LayoutTab are finished.
-     */
-    public boolean onUpdateAnimation(long time) {
-        return mCurrentAnimations == null ? true : mCurrentAnimations.update(time);
     }
 
     /**
@@ -797,7 +783,6 @@ public class LayoutTab implements ChromeAnimation.Animatable {
      * @param visible True if the {@link LayoutTab} is visible and need to be drawn.
      */
     public void setVisible(boolean visible) {
-        if (!visible && mCurrentAnimations != null) mCurrentAnimations.updateAndFinish();
         mVisible = visible;
     }
 
@@ -996,9 +981,6 @@ public class LayoutTab implements ChromeAnimation.Animatable {
 
     @Override
     public void onPropertyAnimationFinished(@Property int prop) {
-        if (mCurrentAnimations != null && mCurrentAnimations.finished()) {
-            mCurrentAnimations = null;
-        }
     }
 
     public static final FloatProperty<LayoutTab> ALPHA = new FloatProperty<LayoutTab>("ALPHA") {
