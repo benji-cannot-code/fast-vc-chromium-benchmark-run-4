d64f93cea7e70c4fdea35be47ed4b9da2e47b6e0
Stefan Zager <szager@chromium.org>
2018-09-11 01:07:28 +0000

    [IntersectionObserver] Reland: Fix the concept of "tracking document"
    Re-landing after revert:
    
    https://chromium-review.googlesource.com/c/chromium/src/+/1200388
    
    ... but leave the flaky tests disabled.
    
    R=chrishtr@chromium.org
    BUG=879798
    
    Change-Id: Ia588f9d82665a533b4e1bd2f77809692ded0cb41
    Reviewed-on: https://chromium-review.googlesource.com/1215370
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590155}

--- third_party/blink/renderer/core/dom/element.h
+++ third_party/blink/renderer/core/dom/element.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/css_primitive_value.h"
 #include "third_party/blink/renderer/core/css/css_selector.h"
 #include "third_party/blink/renderer/core/dom/container_node.h"
+#include "third_party/blink/renderer/core/dom/dom_high_res_time_stamp.h"
 #include "third_party/blink/renderer/core/dom/element_data.h"
 #include "third_party/blink/renderer/core/dom/names_map.h"
 #include "third_party/blink/renderer/core/dom/whitespace_attacher.h"
@@ -898,6 +899,7 @@ class CORE_EXPORT Element : public ContainerNode {
 
   ElementIntersectionObserverData* IntersectionObserverData() const;
   ElementIntersectionObserverData& EnsureIntersectionObserverData();
+  void ComputeIntersectionObservations(bool);
 
   HeapHashMap<TraceWrapperMember<ResizeObserver>, Member<ResizeObservation>>*
   ResizeObserverData() const;
