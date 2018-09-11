38fa3370558ccc03bf01d195048d5f75d289ad89
David 'Digit' Turner <digit@google.com>
2018-09-11 14:03:04 +0000

    android: crazy_linker: Add GNU hash table support.
    The static linker currently generates two symbol hash tables by
    default in each ELF binary (executables or shared libraries).
    One uses the old, slow and large SysV format, and the other the
    much smaller and much faster GNU format.
    
    This CL adds support for the GNU format to the crazy linker
    library (which turns out to require very little code).
    
    This will allow the removal of the SysV table whenever the
    chromium linker is being used (i.e. for ChromePublic.apk and
    ChromeModernPublic.apk and their derivatives).
    
    This will also be handy when we split libchrome.so into
    several native libraries in the future (e.g. to support
    android app feature modules like VR), which will require
    exporting a lot more symbols.
    
    Note that MonochromePublic.apk, which only runs on Android N+
    already uses GNU tables exclusively (for the record, support
    for this format was added in Android M / API level 23).
    
    + Add unit-tests. And scripts to generate the test data from
      a known list of symbols.
    
    + Add a small script to regenerate all auto-generated sources
      at once.
    
    BUG=851391
    R=pasko@chromium.org, rmcilroy@chromium.org, agrieve@chromium.org, cjgrant@chromium.org
    
    Change-Id: I42b1aac46445b5b4d4b3b4f69eaa69ea47e7ab3c
    Reviewed-on: https://chromium-review.googlesource.com/1216122
    Commit-Queue: David Turner <digit@chromium.org>
    Reviewed-by: agrieve <agrieve@chromium.org>
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590291}

--- third_party/android_crazy_linker/src/tests/test_load_library.cpp
+++ third_party/android_crazy_linker/src/tests/test_load_library.cpp
@@ -16,7 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 typedef void (*FunctionPtr)();
 
+#ifndef LIB_NAME
 #define LIB_NAME "libcrazy_linker_tests_libfoo.so"
+#endif
 
 int main() {
   crazy_context_t* context = crazy_context_create();
