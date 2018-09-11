278c9f175341435f74abd85afb9aa81a8cc94032
Joshua Bell <jsbell@chromium.org>
2018-09-11 11:43:37 +0000

    Web Locks API: Run tests in additional globals
    Use the "META: global=" annotation to run the web-locks/ tests in
    shared and service workers, as appropriate.
    
    Change-Id: I1d6acd9b4e2932bf3a60b1e41285cbe74e3cb8ff
    Reviewed-on: https://chromium-review.googlesource.com/1208447
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590265}

--- third_party/WebKit/LayoutTests/external/wpt/web-locks/mode-exclusive.tentative.https.any.js
+++ third_party/WebKit/LayoutTests/external/wpt/web-locks/mode-exclusive.tentative.https.any.js
@@ -1,5 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: title=Web Locks API: Exclusive Mode
+// META: global=window,dedicatedworker,sharedworker,serviceworker
 
 'use strict';
 
--- third_party/WebKit/LayoutTests/external/wpt/web-locks/non-secure-context.tentative.any.js
+++ third_party/WebKit/LayoutTests/external/wpt/web-locks/non-secure-context.tentative.any.js
@@ -1,5 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// META title=Web Locks API: API not available in non-secure context
+// META: title=Web Locks API: API not available in non-secure context
+// META: global=window,dedicatedworker,sharedworker
 
 'use strict';
 
