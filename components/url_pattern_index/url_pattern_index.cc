f8c612e91b115e9c689de7d7048ea6354d3b1ac7
Karan Bhatia <karandeepb@chromium.org>
2018-09-12 18:57:21 +0000

    UrlPatternIndex: Reduce redundant computations while matching.
    r590118 implemented case-insensitive matching which led to some regression in url
    matching performance. Currently, it's possible for a url to be lower-cased each
    time it's matched with a pattern in UrlPattern::MatchesUrl. This CL aims to
    reduce this redundant computation. A thin wrapper over GURL, UrlInfo is
    introduced to cache the computation performed on the GURL.
    
    BUG=767605
    
    Change-Id: Ibe977f34e649068453c5a01e3619b93a76c9ae7d
    Reviewed-on: https://chromium-review.googlesource.com/1220628
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590767}

--- components/url_pattern_index/url_pattern_index.cc
+++ components/url_pattern_index/url_pattern_index.cc
@@ -607,7 +607,7 @@ bool DoesRuleFlagsMatch(const flat::UrlRule& rule,
 // |sorted_candidates| or null if no rule matches.
 const flat::UrlRule* FindMatchAmongCandidates(
     const FlatUrlRuleList* sorted_candidates,
-    const GURL& url,
+    const UrlPattern::UrlInfo& url,
     const url::Origin& document_origin,
     flat::ElementType element_type,
     flat::ActivationType activation_type,
@@ -643,7 +643,7 @@ const flat::UrlRule* FindMatchAmongCandidates(
 // between |url| and |document_origin|.
 const flat::UrlRule* FindMatchInFlatUrlPatternIndex(
     const flat::UrlPatternIndex& index,
-    const GURL& url,
+    const UrlPattern::UrlInfo& url,
     const url::Origin& document_origin,
     flat::ElementType element_type,
     flat::ActivationType activation_type,
@@ -658,14 +658,10 @@ const flat::UrlRule* FindMatchInFlatUrlPatternIndex(
 
   NGramHashTableProber prober;
 
-  // |hash_table| contains lower-cased n-grams. Lower case the url if necessary
-  // to find possible matches.
-  base::Optional<std::string> lower_case_url;
-  if (!HasNoUpperAscii(url.spec()))
-    lower_case_url = base::ToLowerASCII(url.spec());
+  // |hash_table| contains lower-cased n-grams. Use lower-cased url to find
+  // prospective matches.
   auto ngrams = CreateNGramExtractor<kNGramSize, uint64_t>(
-      lower_case_url ? *lower_case_url : url.spec(),
-      [](char) { return false; });
+      url.lower_case_spec(), [](char) { return false; });
 
   auto get_max_priority_rule = [](const flat::UrlRule* lhs,
                                   const flat::UrlRule* rhs) {
@@ -767,8 +763,8 @@ const flat::UrlRule* UrlPatternIndexMatcher::FindMatch(
   }
 
   return FindMatchInFlatUrlPatternIndex(
-      *flat_index_, url, first_party_origin, element_type, activation_type,
-      is_third_party, disable_generic_rules, strategy);
+      *flat_index_, UrlPattern::UrlInfo(url), first_party_origin, element_type,
+      activation_type, is_third_party, disable_generic_rules, strategy);
 }
 
 }  // namespace url_pattern_index
