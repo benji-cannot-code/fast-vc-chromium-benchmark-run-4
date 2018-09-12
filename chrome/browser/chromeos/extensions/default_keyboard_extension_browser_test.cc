dd81180feebf2bc96b0911fa20482dc82901f632
Darren Shen <shend@chromium.org>
2018-09-12 00:33:49 +0000

    [VK] Remove useless ShowVirtualKeyboard call in browser test.
    There is a call to ShowVirtualKeyboard in
    DefaultKeyboardExtensionBrowserTest::GetKeyboardWebContents, which is
    quite confusing, because the virtual keyboard should be showing up from
    focus/clicking and not an explicit call.
    
    I don't really know why that code is there (maybe it's to load the
    extension). However, when --enable-virtual-keyboard flag is on, we
    begin loading the extension anyway.
    
    Bug: 849995
    Change-Id: Icae6c405ccde304ece80456190802012e487ebc5
    Reviewed-on: https://chromium-review.googlesource.com/1215432
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590552}

--- chrome/browser/chromeos/extensions/default_keyboard_extension_browser_test.cc
+++ chrome/browser/chromeos/extensions/default_keyboard_extension_browser_test.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
-#include "ash/shell.h"
 #include "base/command_line.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
@@ -88,17 +87,9 @@ void DefaultKeyboardExtensionBrowserTest::RunTest(
   EXPECT_TRUE(ExecuteWebUIResourceTest(web_contents, resource_ids));
 }
 
-void DefaultKeyboardExtensionBrowserTest::ShowVirtualKeyboard() {
-  aura::Window* window = ash::Shell::GetPrimaryRootWindow();
-  ui::InputMethod* input_method = window->GetHost()->GetInputMethod();
-  ASSERT_TRUE(input_method);
-  input_method->ShowVirtualKeyboardIfEnabled();
-}
-
 content::WebContents*
 DefaultKeyboardExtensionBrowserTest::GetKeyboardWebContents(
     const std::string& id) {
-  ShowVirtualKeyboard();
   GURL url = extensions::Extension::GetBaseURLFromExtensionId(id);
   std::unique_ptr<content::RenderWidgetHostIterator> widgets(
       content::RenderWidgetHost::GetRenderWidgetHosts());
