505e3ca721e77729a39150b53a5a561c608a3ebf
Ramin Halavati <rhalavati@chromium.org>
2018-09-12 09:55:44 +0000

    Remove several preferences from incognito whitelist (4).
    The following preferences are removed from the persistent storage list
    for incognito mode.
    
     - kAppWindowPlacement
     - kExcludedSchemes
     - kEulaAccepted
     - kGCMProductCategoryForSubtypesa
     - kHasSeenWelcomePage
     - kInvertNotificationShown
     - kLocalDiscoveryNotificationsEnabled
     - kMessageCenterDisabledExtensionIds
     - kMessageCenterDisabledSystemComponentIds
     - kSelectFileLastDirectory
     - kStabilityOtherUserCrashCount
     - kStabilityKernelCrashCount
     - kStabilitySystemUncleanShutdownCount
     - kStabilityPluginStats
     - kStabilityPluginName
     - kStabilityPluginLaunches
     - kStabilityPluginInstances
     - kStabilityPluginCrashes
     - kStabilityPluginLoadingErrors
     - kTaskManagerColumnVisibility
     - kTaskManagerEndProcessEnabled
     - kTaskManagerWindowPlacement
    
    These preferences are either never changed from incognito mode, or
    the changed value is not supposed to be written to user profile.
    
    Bug: 861722
    Change-Id: I8e002eca894871581a065b802736e980b65f55bb
    Reviewed-on: https://chromium-review.googlesource.com/1215703
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Nicolas Zea <zea@chromium.org>
    Reviewed-by: Jun Mukai <mukai@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590631}

--- chrome/browser/prefs/pref_service_incognito_whitelist.cc
+++ chrome/browser/prefs/pref_service_incognito_whitelist.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/reading_list/core/reading_list_pref_names.h"
 #include "components/ukm/ukm_pref_names.h"
 #include "components/variations/pref_names.h"
-#include "components/web_resource/web_resource_pref_names.h"
 
 #if !defined(OS_ANDROID)
 #include "chrome/browser/accessibility/animation_policy_prefs.h"
@@ -59,6 +58,9 @@ const char* const kPersistentPrefNames[] = {
     ash::prefs::kScreenMagnifierAcceleratorDialogHasBeenAccepted,
     ash::prefs::kShouldAlwaysShowAccessibilityMenu,
 #endif  // defined(OS_CHROMEOS)
+#if !defined(OS_ANDROID)
+    kAnimationPolicyAllowed, kAnimationPolicyOnce, kAnimationPolicyNone,
+#endif  // !defined(OS_ANDROID)
 
     // Bookmark preferences are common between incognito and regular mode.
     bookmarks::prefs::kBookmarkEditorExpandedNodes,
@@ -194,11 +196,6 @@ const char* const kPersistentPrefNames[] = {
 // audited, checked with owners, and removed or transfered to
 // |kPersistentPrefNames|.
 const char* const kTemporaryIncognitoWhitelist[] = {
-// chrome/browser/accessibility/animation_policy_prefs.h
-#if !defined(OS_ANDROID)
-    kAnimationPolicyAllowed, kAnimationPolicyOnce, kAnimationPolicyNone,
-#endif  // !defined(OS_ANDROID)
-
     // chrome/common/pref_names.h
     prefs::kEnableHyperlinkAuditing,
 
@@ -207,13 +204,6 @@ const char* const kTemporaryIncognitoWhitelist[] = {
     prefs::kEnableReferrers, prefs::kEnableDoNotTrack,
     prefs::kEnableEncryptedMedia,
 
-    prefs::kInvertNotificationShown,
-
-    prefs::kMessageCenterDisabledExtensionIds,
-    prefs::kMessageCenterDisabledSystemComponentIds,
-
-    prefs::kLocalDiscoveryNotificationsEnabled,
-
 #if defined(OS_ANDROID)
     prefs::kMigratedToSiteNotificationChannels,
     prefs::kClearedBlockedSiteNotificationChannels,
@@ -221,14 +211,8 @@ const char* const kTemporaryIncognitoWhitelist[] = {
 
     prefs::kPushMessagingAppIdentifierMap,
 
-    prefs::kGCMProductCategoryForSubtypes,
-
     prefs::kWebRTCMultipleRoutesEnabled, prefs::kWebRTCNonProxiedUdpEnabled,
     prefs::kWebRTCIPHandlingPolicy, prefs::kWebRTCUDPPortRange,
-
-#if !defined(OS_ANDROID)
-    prefs::kHasSeenWelcomePage,
-#endif
 
 #if defined(OS_WIN)
     prefs::kHasSeenWin10PromoPage,
@@ -237,29 +221,13 @@ const char* const kTemporaryIncognitoWhitelist[] = {
 #endif  // defined(GOOGLE_CHROME_BUILD)
 #endif  // defined(OS_WIN)
 
-    prefs::kStabilityOtherUserCrashCount, prefs::kStabilityKernelCrashCount,
-    prefs::kStabilitySystemUncleanShutdownCount,
-
-    prefs::kStabilityPluginStats, prefs::kStabilityPluginName,
-    prefs::kStabilityPluginLaunches, prefs::kStabilityPluginInstances,
-    prefs::kStabilityPluginCrashes, prefs::kStabilityPluginLoadingErrors,
-
-    // prefs::kBrowserWindowPlacement, prefs::kBrowserWindowPlacementPopup,
-    prefs::kTaskManagerWindowPlacement, prefs::kTaskManagerColumnVisibility,
-    prefs::kTaskManagerEndProcessEnabled, prefs::kAppWindowPlacement,
-
 #if defined(OS_WIN) || defined(OS_LINUX) || defined(OS_MACOSX)
     prefs::kOpenPdfDownloadInSystemReader,
 #endif
 
-    // prefs::kSaveFileDefaultDirectory, prefs::kSaveFileType,
     prefs::kTrustedDownloadSources,
 
     prefs::kDefaultTasksByMimeType, prefs::kDefaultTasksBySuffix,
-
-    prefs::kSelectFileLastDirectory,
-
-    prefs::kExcludedSchemes,
 
     prefs::kLastKnownIntranetRedirectOrigin,
 
@@ -394,9 +362,6 @@ const char* const kTemporaryIncognitoWhitelist[] = {
     prefs::kComponentUpdatesEnabled,
 
     prefs::kMediaEngagementSchemaVersion,
-
-    // components/web_resource/web_resource_pref_names.h
-    prefs::kEulaAccepted,
 };
 
 }  // namespace
