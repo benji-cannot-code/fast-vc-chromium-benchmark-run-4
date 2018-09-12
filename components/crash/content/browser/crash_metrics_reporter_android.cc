32668f2a8f5c6c00eecce8f549c3e79805e70c88
Khushal <khushalsagar@chromium.org>
2018-09-12 01:12:36 +0000

    android: Add diagnostic crash dump for GPU OOM crashes.
    R=cpu@chromium.org
    
    Bug: 879259
    Change-Id: I4ad88bcf863be355a286b6e0271a08dc470d1cda
    Reviewed-on: https://chromium-review.googlesource.com/1220769
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Carlos Pizano <cpu@chromium.org>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590562}

--- components/crash/content/browser/crash_metrics_reporter_android.cc
+++ components/crash/content/browser/crash_metrics_reporter_android.cc
@@ -5,10 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/crash/content/browser/crash_metrics_reporter_android.h"
 
+#include "base/debug/dump_without_crashing.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/metrics/user_metrics.h"
 #include "base/optional.h"
+#include "base/rand_util.h"
 #include "components/crash/content/browser/crash_dump_manager_android.h"
 
 namespace crash_reporter {
@@ -149,6 +151,12 @@ void CrashMetricsReporter::CrashDumpProcessed(
 
   if (info.process_type == content::PROCESS_TYPE_GPU && app_foreground &&
       android_oom_kill) {
+    constexpr int kCrashDumpFrequency = 20;
+    if (base::RandInt(1, kCrashDumpFrequency) == kCrashDumpFrequency) {
+      // Diagnostic crash dump to investigate crbug.com/879259.
+      base::debug::DumpWithoutCrashing();
+    }
+
     ReportCrashCount(ProcessedCrashCounts::kGpuForegroundOom, &reported_counts);
   }
 
