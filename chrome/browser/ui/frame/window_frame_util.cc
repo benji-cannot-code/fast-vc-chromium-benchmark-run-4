61bfaa7ed9882b5c10a3d20cb629885ef213de15
Ryan Meier <rameier@chromium.org>
2018-09-12 15:40:58 +0000

    Make the Windows Caption Button color contrast with its background.
    BrowserThemePack now calculates expected average background colors for control buttons, based on frame color, button background color, and a window control background image (as applicable).  Windows10CaptionButton then uses these calculated colors to determine what color to draw the button foregrounds, ensuring that there is sufficient contrast between the background and the foreground.
    
    Additionally, the theme's Caption Button BG image now appears to draw contiguously behind all of the caption buttons.
    
    Bug: 854255
    Change-Id: I4adacd4fb066b840476fcf74036b2c2d524e1899
    Reviewed-on: https://chromium-review.googlesource.com/1182412
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Ryan Meier <rameier@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590692}

---
+++ chrome/browser/ui/frame/window_frame_util.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/frame/window_frame_util.h"
+
+#include "ui/gfx/geometry/size.h"
+
+// static
+SkAlpha WindowFrameUtil::CalculateWindows10GlassCaptionButtonBackgroundAlpha(
+    SkAlpha theme_alpha) {
+  return theme_alpha == SK_AlphaOPAQUE ? 0xCC : theme_alpha;
+}
+
+// static
+gfx::Size WindowFrameUtil::GetWindows10GlassCaptionButtonAreaSize() {
+  constexpr int kNumButtons = 3;
+
+  return gfx::Size(
+      (kNumButtons * kWindows10GlassCaptionButtonWidth) +
+          ((kNumButtons - 1) * kWindows10GlassCaptionButtonVisualSpacing),
+      kWindows10GlassCaptionButtonHeightRestored);
+}
