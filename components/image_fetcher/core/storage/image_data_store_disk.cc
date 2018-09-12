f8c415976ec3d9c1481c0c88ca52d3fcdf04c169
Brandon Wylie <wylieb@chromium.org>
2018-09-12 19:26:08 +0000

    [IC] Stub out storage interfaces and classes
    Storage for the Cached Image Fetcher handled by image_storage.
    image_storage delegates the storage of meta/data to
    image_metadata_storage and image_data_storage respectively. There are
    two stubs of concrete classes included here: metadata -> leveldb and
    data -> disk. In the near future, I will want to supplement meta/data
    storage for a separate format (such as ephemeral), which is why I hid
    them behind an interface even through there's only one implementer per
    interface.
    
    Bug: 878848
    Change-Id: Ibd7052935f8a7f10d98d94122ee0b7f86489ca2a
    Reviewed-on: https://chromium-review.googlesource.com/1189051
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590783}

---
+++ components/image_fetcher/core/storage/image_data_store_disk.cc
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/image_fetcher/core/storage/image_data_store_disk.h"
+
+namespace image_fetcher {
+
+// TODO(wylieb): Implement method stubs.
+ImageDataStoreDisk::ImageDataStoreDisk(const std::string& storage_path)
+    : initialzation_status_(InitializationStatus::UNINITIALIZED),
+      storage_path_(storage_path),
+      weak_ptr_factory_(this) {}
+
+ImageDataStoreDisk::~ImageDataStoreDisk() = default;
+
+void ImageDataStoreDisk::Initialize(base::OnceClosure callback) {}
+
+bool ImageDataStoreDisk::IsInitialized() {
+  return initialzation_status_ == InitializationStatus::INITIALIZED ||
+         initialzation_status_ == InitializationStatus::INIT_FAILURE;
+}
+
+void ImageDataStoreDisk::SaveImage(const std::string& key,
+                                   const std::string& image_data) {}
+
+void ImageDataStoreDisk::LoadImage(const std::string& key,
+                                   ImageDataCallback callback) {}
+
+void ImageDataStoreDisk::DeleteImage(const std::string& key) {}
+
+void ImageDataStoreDisk::GetAllKeys(KeysCallback callback) {}
+
+}  // namespace image_fetcher
