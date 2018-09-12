8d82673f8efcb27daddff511412fc6c32b6d9ebd
Kevin Strohbehn <ginko@google.com>
2018-09-12 17:59:49 +0000

    Folder Should no longer travel with users between pages
    Bug: 880975
    Change-Id: I4a4a41833e4312c8a78ff6b74897e539bf456d1f
    Reviewed-on: https://chromium-review.googlesource.com/1220057
    Reviewed-by: Weidong Guo <weidongg@chromium.org>
    Commit-Queue: Kevin Strohbehn <ginko@google.com>
    Cr-Commit-Position: refs/heads/master@{#590751}

--- ash/app_list/views/folder_background_view.cc
+++ ash/app_list/views/folder_background_view.cc
@@ -11,7 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace app_list {
 
 FolderBackgroundView::FolderBackgroundView(AppListFolderView* folder_view)
-    : folder_view_(folder_view) {}
+    : folder_view_(folder_view) {
+  SetPaintToLayer();
+}
 
 FolderBackgroundView::~FolderBackgroundView() = default;
 
