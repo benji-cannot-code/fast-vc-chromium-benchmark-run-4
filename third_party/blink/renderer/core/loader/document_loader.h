a4f2e92e349b969e1f20fbe47fa305b15c80f59a
Daniel Cheng <dcheng@chromium.org>
2018-09-12 04:17:05 +0000

    Flush microtask queue before DocumentLoader is detached from frame
    This is a bandaid to prevent a large number of crashes from a null
    DocumentLoader: most code assumes that if a frame is not detached,
    there will be an associated DocumentLoader.
    
    Bug: 880182
    Change-Id: Ic0b88613882d4ab902950052592caa245799ad9b
    Reviewed-on: https://chromium-review.googlesource.com/1217326
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590596}

--- third_party/blink/renderer/core/loader/document_loader.h
+++ third_party/blink/renderer/core/loader/document_loader.h
@@ -102,7 +102,7 @@ class CORE_EXPORT DocumentLoader
 
   ResourceTimingInfo* GetNavigationTimingInfo() const;
 
-  virtual void DetachFromFrame();
+  virtual void DetachFromFrame(bool flush_microtask_queue);
 
   unsigned long MainResourceIdentifier() const;
 
