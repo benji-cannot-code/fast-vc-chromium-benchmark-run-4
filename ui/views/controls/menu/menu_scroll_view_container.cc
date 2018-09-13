21e7f5a7a10b7dd085b64f6ae93636c64795b378
Adithya Srinivasan <adithyas@chromium.org>
2018-09-13 15:47:40 +0000

    Revert "Under refresh, use the same menu options for Windows/Linux as under Mac"
    This reverts commit 9c5131ad426872a1c931a6e3a27bbd4c469b7721.
    
    Reason for revert: Speculative revert, might be causing flakiness in ookmarkBarViewTest24.ContextMenusKeyboardEscape. See https://crbug.com/883600.
     
    Original change's description:
    > Under refresh, use the same menu options for Windows/Linux as under Mac
    > 
    > Use the BubbleBorder for the menus since the drop-shadow under Windows
    > doesn't track the shape of the menu. We also have much more control over
    > the shape and depth of the drop-shadow.
    > 
    > Bug: 837782
    > Change-Id: I91b9340251c2cd0b60e3b3f66d450a4791113007
    > Reviewed-on: https://chromium-review.googlesource.com/1159532
    > Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    > Reviewed-by: Evan Stade <estade@chromium.org>
    > Reviewed-by: Michael Wasserman <msw@chromium.org>
    > Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#590731}
    
    TBR=ellyjones@chromium.org,msw@chromium.org,estade@chromium.org,kylixrd@chromium.org
    
    Change-Id: Ic4cc5a69d1cbd48041ae4c3305846efe414e0511
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 837782
    Reviewed-on: https://chromium-review.googlesource.com/1224950
    Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591027}

--- ui/views/controls/menu/menu_scroll_view_container.cc
+++ ui/views/controls/menu/menu_scroll_view_container.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/paint/paint_flags.h"
 #include "third_party/skia/include/core/SkPath.h"
 #include "ui/accessibility/ax_node_data.h"
-#include "ui/base/material_design/material_design_controller.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/color_palette.h"
 #include "ui/views/border.h"
@@ -21,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/round_rect_painter.h"
 
 using ui::NativeTheme;
-using MD = ui::MaterialDesignController;
 
 namespace views {
 
@@ -189,14 +187,14 @@ MenuScrollViewContainer::MenuScrollViewContainer(SubmenuView* content_view)
   arrow_ = BubbleBorderTypeFromAnchor(
       content_view_->GetMenuItem()->GetMenuController()->GetAnchorPosition());
 
-  if (HasBubbleBorder())
+  if (arrow_ != BubbleBorder::NONE)
     CreateBubbleBorder();
   else
     CreateDefaultBorder();
 }
 
 bool MenuScrollViewContainer::HasBubbleBorder() {
-  return (arrow_ != BubbleBorder::NONE) || MD::IsRefreshUi();
+  return arrow_ != BubbleBorder::NONE;
 }
 
 void MenuScrollViewContainer::SetBubbleArrowOffset(int offset) {
@@ -240,7 +238,7 @@ void MenuScrollViewContainer::Layout() {
 
 void MenuScrollViewContainer::OnNativeThemeChanged(
     const ui::NativeTheme* theme) {
-  if (!HasBubbleBorder())
+  if (arrow_ == BubbleBorder::NONE)
     CreateDefaultBorder();
 }
 
@@ -312,14 +310,13 @@ void MenuScrollViewContainer::CreateDefaultBorder() {
 void MenuScrollViewContainer::CreateBubbleBorder() {
   bubble_border_ =
       new BubbleBorder(arrow_, BubbleBorder::SMALL_SHADOW, SK_ColorWHITE);
-  MenuController* controller =
-      content_view_->GetMenuItem()->GetMenuController();
-  if (MD::IsRefreshUi() || controller->use_touchable_layout()) {
+  if (content_view_->GetMenuItem()
+          ->GetMenuController()
+          ->use_touchable_layout()) {
     const MenuConfig& menu_config = MenuConfig::instance();
-    bubble_border_->SetCornerRadius(
-        menu_config.CornerRadiusForMenu(controller));
+    bubble_border_->SetCornerRadius(menu_config.touchable_corner_radius);
     bubble_border_->set_md_shadow_elevation(
-        menu_config.ShadowElevationForMenu(controller));
+        menu_config.touchable_menu_shadow_elevation);
     scroll_view_->GetContents()->SetBorder(CreateEmptyBorder(
         gfx::Insets(menu_config.vertical_touchable_menu_item_padding, 0)));
   }
