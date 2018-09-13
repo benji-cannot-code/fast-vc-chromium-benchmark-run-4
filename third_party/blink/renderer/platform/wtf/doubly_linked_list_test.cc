6f910eed1c4e048aa52038b73c8ea68169266423
Yuta Kitamura <yutak@chromium.org>
2018-09-13 05:00:33 +0000

    Remove std::function from DoublyLinkedList.
    std::function is banned within Chromium. We should use
    base::RepeatingCallback instead.
    
    Change-Id: Ife12ee795b0f2e84511fabfd8a37b9bb4a436ca9
    Reviewed-on: https://chromium-review.googlesource.com/1221486
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Sergio Villar <svillar@igalia.com>
    Commit-Queue: Yuta Kitamura <yutak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590929}

--- third_party/blink/renderer/platform/wtf/doubly_linked_list_test.cc
+++ third_party/blink/renderer/platform/wtf/doubly_linked_list_test.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/doubly_linked_list.h"
 
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/renderer/platform/wtf/functional.h"
 #include "third_party/blink/renderer/platform/wtf/wtf_test_helper.h"
 
 namespace WTF {
@@ -71,7 +72,8 @@ DoublyLinkedList<TestNode>::AddResult DoublyLinkedListTest::CheckedInsert(
     int i) {
   size_t current_size = list_.size();
 
-  auto result = list_.Insert(std::make_unique<TestNode>(i), CompareInt);
+  auto result =
+      list_.Insert(std::make_unique<TestNode>(i), BindRepeating(CompareInt));
   EXPECT_EQ(list_.size(),
             result.is_new_entry ? current_size + 1 : current_size);
   EXPECT_EQ(test_node_counter,
@@ -99,7 +101,7 @@ TEST_F(DoublyLinkedListTest, InsertEmpty) {
   delete list_.RemoveHead();
 
   TestNode node_stack(-1);
-  list_.Insert(&node_stack, CompareInt);
+  list_.Insert(&node_stack, BindRepeating(CompareInt));
   EXPECT_EQ(1ul, list_.size());
   EXPECT_EQ(1ul, test_node_counter);
   EXPECT_EQ(list_.Head(), list_.Tail());
@@ -116,7 +118,8 @@ TEST_F(DoublyLinkedListTest, InsertRandom) {
   int items[6] = {2, -1, 3, 4, 0, 1};
 
   for (int item : items) {
-    auto result = list_.Insert(std::make_unique<TestNode>(item), CompareInt);
+    auto result = list_.Insert(std::make_unique<TestNode>(item),
+                               BindRepeating(CompareInt));
     EXPECT_TRUE(result.is_new_entry);
   }
   EXPECT_EQ(num_items, list_.size());
@@ -132,7 +135,8 @@ TEST_F(DoublyLinkedListTest, InsertSorted) {
   int items[6] = {0, 1, 2, 3, 4, 5};
 
   for (int item : items) {
-    auto result = list_.Insert(std::make_unique<TestNode>(item), CompareInt);
+    auto result = list_.Insert(std::make_unique<TestNode>(item),
+                               BindRepeating(CompareInt));
     EXPECT_TRUE(result.is_new_entry);
   }
   EXPECT_EQ(num_items, list_.size());
