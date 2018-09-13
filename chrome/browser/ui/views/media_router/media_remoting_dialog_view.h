fe050024a88fd42f5b655cc127c3cc875b094d5b
Takumi Fujimoto <takumif@chromium.org>
2018-09-13 23:10:45 +0000

    Depress the Cast toolbar icon when showing Media Remoting dialog
    Put the Cast toolbar icon in pressed state while showing the Media
    Remoting dialog.
    
    Bug: 854374
    Change-Id: Ie75f7905e7c796375ec85075a56bbdf15c73d36c
    Reviewed-on: https://chromium-review.googlesource.com/1191063
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Commit-Queue: mark a. foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591197}

--- chrome/browser/ui/views/media_router/media_remoting_dialog_view.h
+++ chrome/browser/ui/views/media_router/media_remoting_dialog_view.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/views/bubble/bubble_dialog_delegate_view.h"
 
+class MediaRouterActionController;
 class PrefService;
 
 namespace views {
@@ -58,9 +59,10 @@ class MediaRemotingDialogView : public views::BubbleDialogDelegateView {
   gfx::Size CalculatePreferredSize() const override;
 
  private:
-  explicit MediaRemotingDialogView(views::View* anchor_view,
-                                   PrefService* pref_service,
-                                   PermissionCallback callback);
+  MediaRemotingDialogView(views::View* anchor_view,
+                          PrefService* pref_service,
+                          MediaRouterActionController* action_controller,
+                          PermissionCallback callback);
   ~MediaRemotingDialogView() override;
 
   // views::BubbleDialogDelegateView:
@@ -77,6 +79,7 @@ class MediaRemotingDialogView : public views::BubbleDialogDelegateView {
   PermissionCallback permission_callback_;
 
   PrefService* const pref_service_;
+  MediaRouterActionController* const action_controller_;
 
   // Title shown at the top of the dialog.
   base::string16 dialog_title_;
