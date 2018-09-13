d0e897fde85086bd28facf9de14456a863192045
Charlie Harrison <csharrison@chromium.org>
2018-09-13 20:37:02 +0000

    [url_pattern_index] Implement lower case ngram extraction
    This is implemented as an additional template parameter for readability.
    
    We currently store all ngrams as lowercase. By making ngram extraction
    automatically extract lower-cased ngrams, callers no longer have to
    lower-case their input (usually causing an additional allocation / copy).
    
    
    Bug: 767605
    Change-Id: I466011a5143785520d2007700b01585363736153
    Reviewed-on: https://chromium-review.googlesource.com/1223206
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591139}

--- components/url_pattern_index/ngram_extractor.h
+++ components/url_pattern_index/ngram_extractor.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/strings/string_piece.h"
+#include "base/strings/string_util.h"
 
 namespace url_pattern_index {
 
@@ -27,8 +28,13 @@ namespace url_pattern_index {
 // Template parameters:
 //  * N - the size of N-grams.
 //  * NGramType - the integer type used to encode N-grams.
+//  * CasePolicy - whether or not to lower-case the N-grams. Assumes ASCII.
 //  * IsSeparator - the type of a bool(char) functor.
-template <size_t N, typename NGramType, typename IsSeparator>
+enum class NGramCaseExtraction { kCaseSensitive, kLowerCase };
+template <size_t N,
+          typename NGramType,
+          NGramCaseExtraction CasePolicy,
+          typename IsSeparator>
 class NGramExtractor {
  public:
   // An STL compatible input iterator over N-grams contained in a string.
@@ -65,6 +71,15 @@ class NGramExtractor {
     }
 
    private:
+    char ExtractHeadByte() {
+      char head_byte = *head_;
+      switch (CasePolicy) {
+        case NGramCaseExtraction::kCaseSensitive:
+          return head_byte;
+        case NGramCaseExtraction::kLowerCase:
+          return base::ToLowerASCII(head_byte);
+      }
+    }
     // Consumes characters starting with the one pointed to by |head_|, as many
     // of them as needed to extend |ngram_| from its |current_length| to a
     // length of N. Leaves |head_| pointing to the last character consumed.
@@ -74,7 +89,7 @@ class NGramExtractor {
           current_length = 0;
           ngram_ = 0;
         } else {
-          ngram_ = ngram_ << 8 | static_cast<NGramType>(*head_);
+          ngram_ = ngram_ << 8 | static_cast<NGramType>(ExtractHeadByte());
           if (++current_length == N)
             break;
         }
@@ -117,16 +132,21 @@ class NGramExtractor {
 //
 // Typical usage:
 //   const char* str = "no*abacaba*abcd";
-//   auto extractor = CreateNGramExtractor<5, uint64_t>(
-//     str, [](char c) { return c == '*'; });
+//   auto extractor =
+//     CreateNGramExtractor<5, uint64_t, NGrameCaseExtraction::kLowercase>(
+//       str, [](char c) { return c == '*'; });
 //   for (uint64_t ngram : extractor) {
 //     ... process the |ngram| ...
 //   }
-template <size_t N, typename NGramType, typename IsSeparator>
-NGramExtractor<N, NGramType, IsSeparator> CreateNGramExtractor(
+template <size_t N,
+          typename NGramType,
+          NGramCaseExtraction CasePolicy,
+          typename IsSeparator>
+NGramExtractor<N, NGramType, CasePolicy, IsSeparator> CreateNGramExtractor(
     base::StringPiece string,
     IsSeparator is_separator) {
-  return NGramExtractor<N, NGramType, IsSeparator>(string, is_separator);
+  return NGramExtractor<N, NGramType, CasePolicy, IsSeparator>(string,
+                                                               is_separator);
 }
 
 }  // namespace url_pattern_index
