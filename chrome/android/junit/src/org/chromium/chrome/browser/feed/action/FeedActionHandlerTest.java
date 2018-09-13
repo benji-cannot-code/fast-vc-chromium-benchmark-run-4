b4037a14bbbcde23eb4c4164dd37f82d6add0ed5
Becky Zhou <huayinz@chromium.org>
2018-09-13 18:59:34 +0000

    [Feed] Set referrer url in FeedActionHandler
    Bug: 881500
    Change-Id: Ib362f0563b12aa8b5d299840a965ea5e0c8f003c
    Reviewed-on: https://chromium-review.googlesource.com/1222194
    Commit-Queue: Becky Zhou <huayinz@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591099}

--- chrome/android/junit/src/org/chromium/chrome/browser/feed/action/FeedActionHandlerTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/feed/action/FeedActionHandlerTest.java
@@ -14,9 +14,11 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import android.support.test.filters.SmallTest;
+import android.util.ArrayMap;
 
 import com.google.android.libraries.feed.api.knowncontent.ContentMetadata;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -28,6 +30,7 @@ import org.robolectric.annotation.Config;
 
 import org.chromium.base.Callback;
 import org.chromium.base.test.BaseRobolectricTestRunner;
+import org.chromium.chrome.browser.ChromeFeatureList;
 import org.chromium.chrome.browser.feed.FeedOfflineIndicator;
 import org.chromium.chrome.browser.offlinepages.OfflinePageBridge;
 import org.chromium.chrome.browser.suggestions.SuggestionsNavigationDelegate;
@@ -35,6 +38,7 @@ import org.chromium.content_public.browser.LoadUrlParams;
 import org.chromium.ui.mojom.WindowOpenDisposition;
 
 import java.util.Collections;
+import java.util.Map;
 
 /**
  * Unit tests for {@link FeedActionHandler}.
@@ -98,6 +102,15 @@ public class FeedActionHandlerTest {
                 .when(mOfflinePageBridge)
                 .getLoadUrlParamsByOfflineId(mOfflineIdCapture.capture(), anyInt(),
                         mLoadUrlParamsCallbackCapture.capture());
+
+        Map<String, Boolean> featureMap = new ArrayMap<>();
+        featureMap.put(ChromeFeatureList.INTEREST_FEED_CONTENT_SUGGESTIONS, true);
+        ChromeFeatureList.setTestFeatures(featureMap);
+    }
+
+    @After
+    public void tearDown() {
+        ChromeFeatureList.setTestFeatures(null);
     }
 
     @Test
