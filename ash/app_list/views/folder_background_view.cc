38e8de59634326155b0be531d9b0c4df829008d1
Kevin Strohbehn <ginko@google.com>
2018-09-13 23:13:44 +0000

    Revert "Folder Should no longer travel with users between pages"
    This reverts commit b08f82e94d95541b28558895f3e740a3e7d8997a.
    
    Reason for revert: Causing issues with framerate and keyboard while folder is open
    
    Original change's description:
    > Folder Should no longer travel with users between pages
    > 
    > Bug: 880975
    > Change-Id: I4a4a41833e4312c8a78ff6b74897e539bf456d1f
    > Reviewed-on: https://chromium-review.googlesource.com/1220057
    > Reviewed-by: Weidong Guo <weidongg@chromium.org>
    > Commit-Queue: Kevin Strohbehn <ginko@google.com>
    > Cr-Commit-Position: refs/heads/master@{#590751}
    
    TBR=weidongg@chromium.org,ginko@google.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 880975
    Change-Id: I107c456e6654b9808316314d5f53ded082041fc3
    Reviewed-on: https://chromium-review.googlesource.com/1226197
    Reviewed-by: Weidong Guo <weidongg@chromium.org>
    Commit-Queue: Kevin Strohbehn <ginko@google.com>
    Cr-Commit-Position: refs/heads/master@{#591199}

--- ash/app_list/views/folder_background_view.cc
+++ ash/app_list/views/folder_background_view.cc
@@ -11,9 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace app_list {
 
 FolderBackgroundView::FolderBackgroundView(AppListFolderView* folder_view)
-    : folder_view_(folder_view) {
-  SetPaintToLayer();
-}
+    : folder_view_(folder_view) {}
 
 FolderBackgroundView::~FolderBackgroundView() = default;
 
