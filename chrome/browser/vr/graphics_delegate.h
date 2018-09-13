332a7e1b0b476e6ae98fcfcb09caad1726714eea
Aldo Culquicondor <acondor@chromium.org>
2018-09-13 18:52:27 +0000

    VR: Add unit tests for BrowserRenderer
    Bug: 778374
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:linux_vr;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: Ia903ecd1f36426db67b5f229c124aacf0a953cc4
    Reviewed-on: https://chromium-review.googlesource.com/1196905
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Aldo Culquicondor <acondor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591091}

--- chrome/browser/vr/graphics_delegate.h
+++ chrome/browser/vr/graphics_delegate.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_VR_GRAPHICS_DELEGATE_H_
 #define CHROME_BROWSER_VR_GRAPHICS_DELEGATE_H_
 
-#include "base/callback.h"
+#include "base/callback_forward.h"
 #include "chrome/browser/vr/fov_rectangle.h"
 #include "chrome/browser/vr/frame_type.h"
 #include "chrome/browser/vr/gl_texture_location.h"
@@ -31,7 +31,6 @@ struct RenderInfo;
 class VR_EXPORT GraphicsDelegate {
  public:
   using Transform = float[16];
-  using SkiaContextCallback = base::OnceCallback<void()>;
   using TexturesInitializedCallback = base::OnceCallback<
       void(GlTextureLocation, unsigned int, unsigned int, unsigned int)>;
   virtual ~GraphicsDelegate() {}
@@ -63,7 +62,7 @@ class VR_EXPORT GraphicsDelegate {
 
   // These methods return true when succeeded.
   virtual bool Initialize(const scoped_refptr<gl::GLSurface>& surface) = 0;
-  virtual bool RunInSkiaContext(SkiaContextCallback callback) = 0;
+  virtual bool RunInSkiaContext(base::OnceClosure callback) = 0;
 };
 
 }  // namespace vr
