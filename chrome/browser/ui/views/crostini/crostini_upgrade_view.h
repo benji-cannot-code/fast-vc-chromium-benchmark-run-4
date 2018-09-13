2c95388ce76d6f9ff9ec8b569ff6df2d76b9a63f
Nicholas Verne <nverne@chromium.org>
2018-09-13 06:16:01 +0000

    Creates a new dialog for Crostini Upgrade.
    If the user's cros-termina component is incompatible with the current version
    of Chrome and must be upgraded, but the user is offline, we show this dialog
    asking the user to go online and try again. This happens when they attempt to
    launch any crostini app (e.g. the terminal). Testing the dialog showed a bug
    in which the spinner for Terminal was not dismissed if the CrostiniRestart
    failed before the standard spinner delay. This has been fixed in the current
    CL.
    
    Also added testing code to configure the component updater in browser tests so
    we can simulate the circumstances leading to the dialog being shown.
    
    Bug: 881616
    Change-Id: I1007a3bd2c34c8403ab80335063072c4995d5bff
    Reviewed-on: https://chromium-review.googlesource.com/1218264
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Commit-Queue: Nicholas Verne <nverne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590937}

---
+++ chrome/browser/ui/views/crostini/crostini_upgrade_view.h
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_VIEWS_CROSTINI_CROSTINI_UPGRADE_VIEW_H_
+#define CHROME_BROWSER_UI_VIEWS_CROSTINI_CROSTINI_UPGRADE_VIEW_H_
+
+#include "ui/views/window/dialog_delegate.h"
+
+namespace crostini {
+enum class ConciergeClientResult;
+}  // namespace crostini
+
+class Profile;
+
+// Provides a warning to the user that an upgrade is required and and internet
+// connection is needed.
+class CrostiniUpgradeView : public views::DialogDelegateView {
+ public:
+  static void Show(Profile* profile);
+
+  // views::DialogDelegateView:
+  int GetDialogButtons() const override;
+  base::string16 GetWindowTitle() const override;
+  bool ShouldShowCloseButton() const override;
+  gfx::Size CalculatePreferredSize() const override;
+
+  static CrostiniUpgradeView* GetActiveViewForTesting();
+
+ private:
+  CrostiniUpgradeView();
+  ~CrostiniUpgradeView() override;
+};
+
+#endif  // CHROME_BROWSER_UI_VIEWS_CROSTINI_CROSTINI_UPGRADE_VIEW_H_
