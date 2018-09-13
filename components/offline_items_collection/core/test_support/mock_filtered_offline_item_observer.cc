8e599ec12a399e7ea89830fd16f72b8cec262315
Shakti Sahu <shaktisahu@chromium.org>
2018-09-13 22:23:16 +0000

    offline_item_collections : Added single OfflineItem observer
    Added ability to add observers interested in only a single OfflineItem.
    
    Bug: 881499
    Change-Id: I0b221071a0d7e258778fd37f668c8e0c5a8c2c26
    Reviewed-on: https://chromium-review.googlesource.com/1213963
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591184}

---
+++ components/offline_items_collection/core/test_support/mock_filtered_offline_item_observer.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/offline_items_collection/core/test_support/mock_filtered_offline_item_observer.h"
+
+namespace offline_items_collection {
+
+MockFilteredOfflineItemObserver::MockObserver::MockObserver() = default;
+MockFilteredOfflineItemObserver::MockObserver::~MockObserver() = default;
+
+MockFilteredOfflineItemObserver::ScopedMockObserver::ScopedMockObserver(
+    FilteredOfflineItemObserver* observer,
+    const ContentId& id)
+    : id_(id), observer_(observer) {
+  observer_->AddObserver(id_, this);
+}
+
+MockFilteredOfflineItemObserver::ScopedMockObserver::~ScopedMockObserver() {
+  observer_->RemoveObserver(id_, this);
+}
+
+}  // namespace offline_items_collection
