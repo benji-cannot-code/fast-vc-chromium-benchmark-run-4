8f7b20bdb3c09978d4036ac734458c5c360ffac9
Tommy Steimel <steimel@chromium.org>
2018-09-13 20:39:56 +0000

    [Media Intent Handler] Add MediaLauncherActivity LaunchSourceType
    This CL adds a new LaunchSourceType enum to CustomTabIntentDataProvider
    that matches the WebappActivity.ActivityType enum but adds a new value
    for apps launched from MediaLauncherActivity. This is used to
    distinguish intents from the MLA from other MEDIA_VIEWER sources (e.g.
    the Downloads Home UI). This will allow us to add Media-Intent-Handler-
    specific metrics.
    
    Bug: 861954
    Change-Id: I01b20fec65e6207de3d24d42b9b08787a582adcb
    Reviewed-on: https://chromium-review.googlesource.com/1185633
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591140}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
@@ -102,7 +102,6 @@ import org.chromium.chrome.browser.toolbar.ToolbarControlContainer;
 import org.chromium.chrome.browser.util.ColorUtils;
 import org.chromium.chrome.browser.util.IntentUtils;
 import org.chromium.chrome.browser.util.UrlUtilities;
-import org.chromium.chrome.browser.webapps.WebappActivity;
 import org.chromium.chrome.browser.webapps.WebappCustomTabTimeSpentLogger;
 import org.chromium.components.dom_distiller.core.DomDistillerUrlUtils;
 import org.chromium.content_public.browser.LoadUrlParams;
@@ -721,8 +720,8 @@ public class CustomTabActivity extends ChromeActivity {
         Tab tab = new Tab(assignedTabId, parentTabId, mIntentDataProvider.isIncognito(),
                 getWindowAndroid(), TabLaunchType.FROM_EXTERNAL_APP, null, null);
         if (getIntent().getIntExtra(CustomTabIntentDataProvider.EXTRA_BROWSER_LAUNCH_SOURCE,
-                    WebappActivity.ActivityType.OTHER)
-                == WebappActivity.ActivityType.WEBAPK) {
+                    CustomTabIntentDataProvider.LaunchSourceType.OTHER)
+                == CustomTabIntentDataProvider.LaunchSourceType.WEBAPK) {
             String webapkPackageName = getIntent().getStringExtra(Browser.EXTRA_APPLICATION_ID);
             tab.setAppAssociatedWith(webapkPackageName);
         } else {
@@ -874,7 +873,7 @@ public class CustomTabActivity extends ChromeActivity {
         mIsInitialResume = false;
         mWebappTimeSpentLogger = WebappCustomTabTimeSpentLogger.createInstanceAndStartTimer(
                 getIntent().getIntExtra(CustomTabIntentDataProvider.EXTRA_BROWSER_LAUNCH_SOURCE,
-                        WebappActivity.ActivityType.OTHER));
+                        CustomTabIntentDataProvider.LaunchSourceType.OTHER));
 
         if (mModuleActivityDelegate != null) {
             resumeModule();
---
+++ chrome/android/javatests/src/org/chromium/chrome/browser/webapps/WebappActivityTest.java
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.webapps;
+
+import android.support.test.filters.SmallTest;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.chromium.chrome.browser.customtabs.CustomTabIntentDataProvider.LaunchSourceType;
+import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
+
+/**
+ * Tests for WebappActivity class.
+ */
+@RunWith(ChromeJUnit4ClassRunner.class)
+public class WebappActivityTest {
+    @Test
+    @SmallTest
+    public void testActivityTypeMatchesLaunchSourceType() throws Exception {
+        Assert.assertEquals(LaunchSourceType.OTHER, WebappActivity.ActivityType.OTHER);
+        Assert.assertEquals(LaunchSourceType.WEBAPP, WebappActivity.ActivityType.WEBAPP);
+        Assert.assertEquals(LaunchSourceType.WEBAPK, WebappActivity.ActivityType.WEBAPK);
+        Assert.assertEquals(LaunchSourceType.TWA, WebappActivity.ActivityType.TWA);
+    }
+}
