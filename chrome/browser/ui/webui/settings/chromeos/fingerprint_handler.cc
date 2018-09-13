d014551535359d5685a5d5b8c9316f793e1ccbc4
Sarah Hu <xiaoyinh@chromium.org>
2018-09-13 17:59:05 +0000

    cros: Fix crash in FingerprintHandler
    From the stack trace, it looks like OnEnrollScanDone is received before
    javascript is allowed. To avoid this crash, this CL moves observer
    registering after javascript is allowed.
    
    Also clean up the enroll session in the OOBE fingerprint screen, in case
    we exit the screen in the middle of a fingerprint enrollment.
    
    Bug: 881501
    Change-Id: I40a12859b06e3d2df270906fd0cebaddcae7bc52
    Reviewed-on: https://chromium-review.googlesource.com/1222894
    Commit-Queue: Xiaoyin Hu <xiaoyinh@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591068}

--- chrome/browser/ui/webui/settings/chromeos/fingerprint_handler.cc
+++ chrome/browser/ui/webui/settings/chromeos/fingerprint_handler.cc
@@ -63,9 +63,6 @@ FingerprintHandler::FingerprintHandler(Profile* profile)
   service_manager::Connector* connector =
       content::ServiceManagerConnection::GetForProcess()->GetConnector();
   connector->BindInterface(device::mojom::kServiceName, &fp_service_);
-  device::mojom::FingerprintObserverPtr observer;
-  binding_.Bind(mojo::MakeRequest(&observer));
-  fp_service_->AddFingerprintObserver(std::move(observer));
   user_id_ = ProfileHelper::Get()->GetUserIdHashFromProfile(profile);
 }
 
@@ -115,10 +112,15 @@ void FingerprintHandler::OnJavascriptAllowed() {
   // SessionManager may not exist in some tests.
   if (SessionManager::Get())
     session_observer_.Add(SessionManager::Get());
+
+  device::mojom::FingerprintObserverPtr observer;
+  binding_.Bind(mojo::MakeRequest(&observer));
+  fp_service_->AddFingerprintObserver(std::move(observer));
 }
 
 void FingerprintHandler::OnJavascriptDisallowed() {
   session_observer_.RemoveAll();
+  binding_.Close();
 }
 
 void FingerprintHandler::OnRestarted() {}
@@ -126,6 +128,10 @@ void FingerprintHandler::OnRestarted() {}
 void FingerprintHandler::OnEnrollScanDone(uint32_t scan_result,
                                           bool enroll_session_complete,
                                           int percent_complete) {
+  VLOG(1) << "Receive fingerprint enroll scan result. scan_result="
+          << scan_result
+          << ", enroll_session_complete=" << enroll_session_complete
+          << ", percent_complete=" << percent_complete;
   auto scan_attempt = std::make_unique<base::DictionaryValue>();
   scan_attempt->SetInteger("result", scan_result);
   scan_attempt->SetBoolean("isComplete", enroll_session_complete);
@@ -137,6 +143,8 @@ void FingerprintHandler::OnEnrollScanDone(uint32_t scan_result,
 void FingerprintHandler::OnAuthScanDone(
     uint32_t scan_result,
     const base::flat_map<std::string, std::vector<std::string>>& matches) {
+  VLOG(1) << "Receive fingerprint auth scan result. scan_result="
+          << scan_result;
   if (SessionManager::Get()->session_state() == SessionState::LOCKED)
     return;
 
@@ -163,7 +171,9 @@ void FingerprintHandler::OnAuthScanDone(
   FireWebUIListener("on-fingerprint-attempt-received", *fingerprint_attempt);
 }
 
-void FingerprintHandler::OnSessionFailed() {}
+void FingerprintHandler::OnSessionFailed() {
+  LOG(ERROR) << "Fingerprint session failed.";
+}
 
 void FingerprintHandler::OnSessionStateChanged() {
   SessionState state = SessionManager::Get()->session_state();
