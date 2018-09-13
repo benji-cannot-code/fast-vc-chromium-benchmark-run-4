f07479f04f08ce5018c04a8ba5a31a5f5ffc8127
Christian Biesinger <cbiesinger@chromium.org>
2018-09-13 23:08:24 +0000

    [layoutng] Create exclusion space lazily
    Many users of constraint space never use it.
    
    This is accomplished by introducing a wrapper class (thanks Ian for
    the suggestion) which internally manages the actual exclusion space
    pointer (now called NGExclusionSpaceInternal). It has the same
    interface as the old NGExclusionSpace but does not require
    memory allocation until you add an exclusion (comparatively rare).
    
    As a minor cleanup, this also changes two NGLayoutResult constructor
    arguments to use rvalue references, which they effectively already
    were.
    
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng
    Change-Id: I2c83ba64443bc45f7e328f8d5663d5a4867885a5
    Reviewed-on: https://chromium-review.googlesource.com/1222270
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591195}

--- third_party/blink/renderer/core/layout/ng/inline/ng_inline_layout_algorithm.h
+++ third_party/blink/renderer/core/layout/ng/inline/ng_inline_layout_algorithm.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class NGConstraintSpace;
+class NGExclusionSpace;
 class NGInlineBreakToken;
 class NGInlineNode;
 class NGInlineItem;
--- third_party/blink/renderer/core/layout/ng/ng_block_layout_algorithm.h
+++ third_party/blink/renderer/core/layout/ng/ng_block_layout_algorithm.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/scoped_refptr.h"
 #include "third_party/blink/renderer/core/core_export.h"
+#include "third_party/blink/renderer/core/layout/ng/exclusions/ng_exclusion_space.h"
 #include "third_party/blink/renderer/core/layout/ng/geometry/ng_margin_strut.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_block_break_token.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_block_node.h"
@@ -300,7 +301,7 @@ class CORE_EXPORT NGBlockLayoutAlgorithm
 
   bool has_processed_first_child_ = false;
 
-  std::unique_ptr<NGExclusionSpace> exclusion_space_;
+  NGExclusionSpace exclusion_space_;
   NGUnpositionedFloatVector unpositioned_floats_;
 };
 
