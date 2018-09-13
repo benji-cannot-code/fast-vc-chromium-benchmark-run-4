974dae85e95bd662e3f8708b19bc84b50ec88449
Mathias Carlen <mcarlen@chromium.org>
2018-09-13 14:32:42 +0000

    [Autofill Assistant] Introduce SelectOptionAction.
    This introduces the upper layers of the select option action. The
    interaction on the website logic will follow in a separate change.
    
    Bug: 806868
    Change-Id: I844a4d5723af18e3cedcd97c022009c5d7f996d4
    Reviewed-on: https://chromium-review.googlesource.com/1223610
    Commit-Queue: Mathias Carlen <mcarlen@chromium.org>
    Reviewed-by: Ganggui Tang <gogerald@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591002}

---
+++ components/autofill_assistant/browser/actions/select_option_action.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_AUTOFILL_ASSISTANT_BROWSER_ACTIONS_SELECT_OPTION_ACTION_H_
+#define COMPONENTS_AUTOFILL_ASSISTANT_BROWSER_ACTIONS_SELECT_OPTION_ACTION_H_
+
+#include "components/autofill_assistant/browser/actions/action.h"
+
+#include <string>
+#include <vector>
+
+#include "base/macros.h"
+
+namespace autofill_assistant {
+// An action to select an option on a given element on Web.
+class SelectOptionAction : public Action {
+ public:
+  explicit SelectOptionAction(const ActionProto& proto);
+  ~SelectOptionAction() override;
+
+  // Overrides Action:
+  void ProcessAction(ActionDelegate* delegate,
+                     ProcessActionCallback callback) override;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(SelectOptionAction);
+};
+
+}  // namespace autofill_assistant.
+#endif  // COMPONENTS_AUTOFILL_ASSISTANT_BROWSER_ACTIONS_SELECT_OPTION_ACTION_H_
--- components/autofill_assistant/browser/protocol_utils.cc
+++ components/autofill_assistant/browser/protocol_utils.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "components/autofill_assistant/browser/actions/autofill_action.h"
 #include "components/autofill_assistant/browser/actions/click_action.h"
+#include "components/autofill_assistant/browser/actions/select_option_action.h"
 #include "components/autofill_assistant/browser/actions/tell_action.h"
 #include "components/autofill_assistant/browser/actions/wait_for_dom_action.h"
 #include "components/autofill_assistant/browser/service.pb.h"
@@ -132,6 +133,10 @@ bool ProtocolUtils::ParseActions(const std::string& response,
       }
       case ActionProto::ActionInfoCase::kWaitForDom: {
         actions->emplace_back(std::make_unique<WaitForDomAction>(action));
+        break;
+      }
+      case ActionProto::ActionInfoCase::kSelectOption: {
+        actions->emplace_back(std::make_unique<SelectOptionAction>(action));
         break;
       }
       case ActionProto::ActionInfoCase::ACTION_INFO_NOT_SET: {
