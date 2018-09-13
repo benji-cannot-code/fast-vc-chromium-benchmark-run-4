ce98339e3c075dd688209cf9fe53dc83e1323a4f
Kenichi Ishibashi <bashi@chromium.org>
2018-09-13 08:20:17 +0000

    service worker: Add instrumentation for navigation crash
    Record some events in ServiceWorkerNavigationLoader. These are only
    recorded when NetworkService is enabled as the crash is happening
    with NetworkService.
    
    Bug: 881826
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Change-Id: Ie632cb3b2b4e23001aeea2e0f01076286853cf5b
    Reviewed-on: https://chromium-review.googlesource.com/1223531
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590950}

--- content/browser/service_worker/service_worker_navigation_loader.h
+++ content/browser/service_worker/service_worker_navigation_loader.h
@@ -115,6 +115,8 @@ class CONTENT_EXPORT ServiceWorkerNavigationLoader
     kCompleted,
   };
 
+  static const char* StatusString(Status status);
+
   // For FORWARD_TO_SERVICE_WORKER case.
   void StartRequest(const network::ResourceRequest& resource_request,
                     network::mojom::URLLoaderRequest request,
@@ -186,6 +188,9 @@ class CONTENT_EXPORT ServiceWorkerNavigationLoader
   mojo::Binding<network::mojom::URLLoader> binding_;
 
   Status status_ = Status::kNotStarted;
+
+  // https://crbug.com/881826
+  base::Optional<std::vector<std::string>> debug_log_;
 
   base::WeakPtrFactory<ServiceWorkerNavigationLoader> weak_factory_;
 
