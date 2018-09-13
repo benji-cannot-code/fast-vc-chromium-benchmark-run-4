d850a8c368188c438e499a35a772e77bda5e32a0
Albert J. Wong <ajwong@chromium.org>
2018-09-13 21:00:45 +0000

    Move RVH::OnRequestSetBounds into OwnerDelegate
    Currently ViewHostMsg_RequestSetBounds is sent to both the widget and
    the view. Moving this behavior into the OwnerDelegate allows routing
    to go only to the RenderWidgetHost.
    
    Bug: 545684
    Change-Id: Iac8eeb04b73eefaf99d46343714c3a63d58b9460
    Reviewed-on: https://chromium-review.googlesource.com/1220393
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Albert J. Wong <ajwong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591150}

--- content/browser/renderer_host/render_view_host_impl.h
+++ content/browser/renderer_host/render_view_host_impl.h
@@ -240,6 +240,7 @@ class CONTENT_EXPORT RenderViewHostImpl : public RenderViewHost,
   bool MayRenderWidgetForwardKeyboardEvent(
       const NativeWebKeyboardEvent& key_event) override;
   bool ShouldContributePriorityToProcess() override;
+  void RequestSetBounds(const gfx::Rect& bounds) override;
 
   // IPC message handlers.
   void OnShowView(int route_id,
@@ -250,7 +251,6 @@ class CONTENT_EXPORT RenderViewHostImpl : public RenderViewHost,
   void OnShowFullscreenWidget(int route_id);
   void OnUpdateTargetURL(const GURL& url);
   void OnClose();
-  void OnRequestSetBounds(const gfx::Rect& bounds);
   void OnDocumentAvailableInMainFrame(bool uses_temporary_zoom_level);
   void OnDidContentsPreferredSizeChange(const gfx::Size& new_size);
   void OnPasteFromSelectionClipboard();
