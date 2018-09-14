9a2708f2bcd90ee04fd0eef06a88a878895032b4
Sebastien Marchand <sebmarchand@chromium.org>
2018-09-14 16:11:46 +0000

    Switch tab_manager_features to the FeatureParams API
    This is a cleanup that replaces the really long feature parameters names
    used in tab_manager_features.h/cc by several FeatureParam objects.
    
    
    Bug: 844426
    Change-Id: Ia37d00d2de3f0ff756fa365108bbcab4a91d184c
    Reviewed-on: https://chromium-review.googlesource.com/1195852
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Commit-Queue: Sébastien Marchand <sebmarchand@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591360}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -1219,13 +1219,13 @@ const FeatureEntry::FeatureVariation
 
 #if !defined(OS_ANDROID)
 const FeatureEntry::FeatureParam kProactiveTabFreezeAndDiscard_FreezeOnly[] = {
-    {resource_coordinator::
-         kProactiveTabFreezeAndDiscard_ShouldProactivelyDiscardParam,
+    {resource_coordinator::ProactiveTabFreezeAndDiscardParams::
+         kShouldProactivelyDiscard.name,
      "false"}};
 const FeatureEntry::FeatureParam
     kProactiveTabFreezeAndDiscard_FreezeAndDiscard[] = {
-        {resource_coordinator::
-             kProactiveTabFreezeAndDiscard_ShouldProactivelyDiscardParam,
+        {resource_coordinator::ProactiveTabFreezeAndDiscardParams::
+             kShouldProactivelyDiscard.name,
          "true"}};
 const FeatureEntry::FeatureVariation kProactiveTabFreezeAndDiscardVariations[] =
     {{"Freeze only", kProactiveTabFreezeAndDiscard_FreezeOnly,
@@ -4236,7 +4236,7 @@ const FeatureEntry kFeatureEntries[] = {
      FEATURE_WITH_PARAMS_VALUE_TYPE(
          features::kProactiveTabFreezeAndDiscard,
          kProactiveTabFreezeAndDiscardVariations,
-         resource_coordinator::kProactiveTabFreezeAndDiscardFeatureName)},
+         features::kProactiveTabFreezeAndDiscard.name)},
     {"site-characteristics-database",
      flag_descriptions::kSiteCharacteristicsDatabaseName,
      flag_descriptions::kSiteCharacteristicsDatabaseDescription, kOsDesktop,
