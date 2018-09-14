efd3153bed3596c28bae0fdc8766dfd16044cdaf
Aga Wronska <agawronska@chromium.org>
2018-09-14 23:08:15 +0000

    Do not show expanded view for Public Session in Demo Mode.
    Skip showing expanded login view and start Public Session right
    away after user clicks the button on login view.
    Use default locale and input method.
    
    This change applies only to views login.
    
    Bug: 882692
    Test: Manually + run ash_unittests
    Change-Id: I85945e813cf61dd84008fab05a4a2dce728d0939
    Reviewed-on: https://chromium-review.googlesource.com/1226254
    Commit-Queue: Aga Wronska <agawronska@chromium.org>
    Reviewed-by: Jacob Dufault <jdufault@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Wenzhao (Colin) Zang <wzang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591516}

--- chrome/browser/chromeos/login/screens/user_selection_screen.cc
+++ chrome/browser/chromeos/login/screens/user_selection_screen.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/browser_process_platform_part.h"
+#include "chrome/browser/chromeos/login/demo_mode/demo_session.h"
 #include "chrome/browser/chromeos/login/easy_unlock/easy_unlock_service.h"
 #include "chrome/browser/chromeos/login/lock/screen_locker.h"
 #include "chrome/browser/chromeos/login/lock_screen_utils.h"
@@ -847,6 +848,9 @@ UserSelectionScreen::UpdateAndReturnUserListForMojo() {
               std::move(available_locales));
       user_info->public_account_info->default_locale = selected_locale;
       user_info->public_account_info->show_advanced_view = has_multiple_locales;
+      // Do not show expanded view when in demo mode.
+      user_info->public_account_info->show_expanded_view =
+          !DemoSession::IsDeviceInDemoMode();
     }
 
     user_info->can_remove = CanRemoveUser(user);
