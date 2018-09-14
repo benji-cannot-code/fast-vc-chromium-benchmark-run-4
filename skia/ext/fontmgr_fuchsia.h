7296fccde16c7242cb89a59e5182fc5d96b690c0
Kevin Marshall <kmarshall@chromium.org>
2018-09-14 00:00:05 +0000

    Roll Fuchsia SDK to bf318690d, fix FontProvider name breakage.
    The AutoRoll server is located here: https://autoroll.skia.org/r/fuchsia-sdk-chromium-autoroll
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+/master/autoroll/README.md
    
    If the roll is causing failures, please contact the current sheriff, who should
    be CC'd on the roll, and stop the roller if necessary.
    
    
    CQ_INCLUDE_TRYBOTS=luci.chromium.try:fuchsia_arm64_cast_audio;luci.chromium.try:fuchsia_x64_cast_audio
    TBR=cr-fuchsia+bot@chromium.org
    
    Change-Id: Iee9c1a2f249f0b660ed6d2f9e88f7e43eec2378d
    Reviewed-on: https://chromium-review.googlesource.com/1226271
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591216}

--- skia/ext/fontmgr_fuchsia.h
+++ skia/ext/fontmgr_fuchsia.h
@@ -20,8 +20,7 @@ namespace skia {
 
 class SK_API FuchsiaFontManager : public SkFontMgr {
  public:
-  explicit FuchsiaFontManager(
-      fuchsia::fonts::FontProviderSyncPtr font_provider);
+  explicit FuchsiaFontManager(fuchsia::fonts::ProviderSyncPtr font_provider);
 
   ~FuchsiaFontManager() override;
 
@@ -53,7 +52,7 @@ class SK_API FuchsiaFontManager : public SkFontMgr {
  private:
   class FontCache;
 
-  fuchsia::fonts::FontProviderSyncPtr font_provider_;
+  fuchsia::fonts::ProviderSyncPtr font_provider_;
 
   // Map applied to font family name before sending requests to the FontService.
   base::flat_map<std::string, std::string> font_map_;
