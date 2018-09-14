e8fe498dd43f67def13ce1ad428e381cdfb79b76
Chris Palmer <palmer@chromium.org>
2018-09-14 19:30:17 +0000

    Fix some integer overflow possibilities in Blink memory management.
    Bug: None
    Change-Id: I5f2b326a1b7b6c73156edfb43ceae6356433f4f2
    Reviewed-on: https://chromium-review.googlesource.com/1226454
    Commit-Queue: Chris Palmer <palmer@chromium.org>
    Reviewed-by: Raymond Toy <rtoy@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591434}

--- third_party/blink/renderer/platform/audio/audio_channel.cc
+++ third_party/blink/renderer/platform/audio/audio_channel.cc
@@ -27,9 +27,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include "third_party/blink/renderer/platform/audio/audio_channel.h"
+
 #include <math.h>
 #include <algorithm>
-#include "third_party/blink/renderer/platform/audio/audio_channel.h"
+#include "base/numerics/checked_math.h"
 #include "third_party/blink/renderer/platform/audio/vector_math.h"
 
 namespace blink {
@@ -59,7 +61,8 @@ void AudioChannel::CopyFrom(const AudioChannel* source_channel) {
     Zero();
     return;
   }
-  memcpy(MutableData(), source_channel->Data(), sizeof(float) * length());
+  memcpy(MutableData(), source_channel->Data(),
+         base::CheckMul(sizeof(float), length()).ValueOrDie());
 }
 
 void AudioChannel::CopyFromRange(const AudioChannel* source_channel,
@@ -85,13 +88,16 @@ void AudioChannel::CopyFromRange(const AudioChannel* source_channel,
   const float* source = source_channel->Data();
   float* destination = MutableData();
 
+  const size_t safe_length =
+      base::CheckMul(sizeof(float), range_length).ValueOrDie();
   if (source_channel->IsSilent()) {
     if (range_length == length())
       Zero();
     else
-      memset(destination, 0, sizeof(float) * range_length);
-  } else
-    memcpy(destination, source + start_frame, sizeof(float) * range_length);
+      memset(destination, 0, safe_length);
+  } else {
+    memcpy(destination, source + start_frame, safe_length);
+  }
 }
 
 void AudioChannel::SumFrom(const AudioChannel* source_channel) {
