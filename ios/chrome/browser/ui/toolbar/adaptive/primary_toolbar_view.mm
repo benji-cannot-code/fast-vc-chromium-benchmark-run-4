95c4a2bc7c329e2cf5127ada7580e808f1d1c03f
Gauthier Ambard <gambard@chromium.org>
2018-09-14 14:10:02 +0000

    [iOS] Change accessibility label of progress bar
    This CL changes the accessibility value of the progress bar, to indicate
    that it is related to the loading of the page.
    
    Bug: 228526
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: I51b55f15f420423374e7b0430945c98cfa71c964
    Reviewed-on: https://chromium-review.googlesource.com/1225876
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591340}

--- ios/chrome/browser/ui/toolbar/adaptive/primary_toolbar_view.mm
+++ ios/chrome/browser/ui/toolbar/adaptive/primary_toolbar_view.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/ios/ios_util.h"
 #include "base/logging.h"
+#import "ios/chrome/browser/ui/toolbar/adaptive/toolbar_progress_bar.h"
 #import "ios/chrome/browser/ui/toolbar/buttons/toolbar_button.h"
 #import "ios/chrome/browser/ui/toolbar/buttons/toolbar_button_factory.h"
 #import "ios/chrome/browser/ui/toolbar/buttons/toolbar_configuration.h"
@@ -16,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/toolbar/public/features.h"
 #import "ios/chrome/browser/ui/uikit_ui_util.h"
 #import "ios/chrome/common/ui_util/constraints_ui_util.h"
-#import "ios/third_party/material_components_ios/src/components/ProgressView/src/MaterialProgressView.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -53,7 +53,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @property(nonatomic, strong) NSArray<ToolbarButton*>* trailingStackViewButtons;
 
 // Progress bar displayed below the toolbar, redefined as readwrite.
-@property(nonatomic, strong, readwrite) MDCProgressView* progressBar;
+@property(nonatomic, strong, readwrite) ToolbarProgressBar* progressBar;
 
 #pragma mark** Buttons in the leading stack view. **
 // Button to navigate back, redefined as readwrite.
@@ -291,7 +291,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Sets the progress bar up.
 - (void)setUpProgressBar {
-  self.progressBar = [[MDCProgressView alloc] init];
+  self.progressBar = [[ToolbarProgressBar alloc] init];
   self.progressBar.translatesAutoresizingMaskIntoConstraints = NO;
   self.progressBar.hidden = YES;
   [self addSubview:self.progressBar];
