1edb083d8be5fc888f2316545150f6951be40678
Scott Graham <scottmg@chromium.org>
2018-09-14 19:11:16 +0000

    fuchsia: Disable ContextImplTest.DelayedNavigationEventAck, failing
    TBR=kmarshall@chromium.org
    
    Bug: 884260
    Change-Id: I47a407077b9ee38a0db8a6710f827cb111d4ab07
    Reviewed-on: https://chromium-review.googlesource.com/1227210
    Reviewed-by: Scott Graham <scottmg@chromium.org>
    Commit-Queue: Scott Graham <scottmg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591423}

--- webrunner/browser/context_impl_browsertest.cc
+++ webrunner/browser/context_impl_browsertest.cc
@@ -462,7 +462,7 @@ IN_PROC_BROWSER_TEST_F(ContextImplTest, NavigationObserverDisconnected) {
   }
 }
 
-IN_PROC_BROWSER_TEST_F(ContextImplTest, DelayedNavigationEventAck) {
+IN_PROC_BROWSER_TEST_F(ContextImplTest, DISABLED_DelayedNavigationEventAck) {
   chromium::web::FramePtr frame = CreateFrame();
 
   chromium::web::NavigationControllerPtr controller;
