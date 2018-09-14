ed2012ca231f960694f6912deade3869adc11fa6
Denis Kuznetsov <antrim@google.com>
2018-09-14 14:53:35 +0000

    Migrate OOBE Enrollment to polymer
    Get rid of enterprise-card / enterprise-header, switch existing screens
    to oobe-dialog.
    
    This cl simplify existing code before migrating enrollment flow to polymer
    completely.
    
    Bug: 811556
    Change-Id: I5035707ba57c4281a293a616225a8171a8e552dc
    Reviewed-on: https://chromium-review.googlesource.com/1225698
    Commit-Queue: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591343}

--- chrome/browser/resources/chromeos/login/custom_elements_oobe.js
+++ chrome/browser/resources/chromeos/login/custom_elements_oobe.js
@@ -46,8 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // <include src="arc_terms_of_service.js">
 // <include src="oobe_voice_interaction_value_prop.js">
 // <include src="oobe_wait_for_container_ready.js">
-// <include src="enterprise_card.js">
-// <include src="enterprise_header.js">
 // <include src="enrollment_license_card.js">
 // <include src="sync_consent.js">
 // <include src="fingerprint_setup.js">
--- chrome/browser/resources/chromeos/login/enrollment_license_card.js
+++ chrome/browser/resources/chromeos/login/enrollment_license_card.js
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 Polymer({
   is: 'enrollment-license-card',
 
-  behaviors: [I18nBehavior],
+  behaviors: [I18nBehavior, OobeDialogHostBehavior],
 
   properties: {
     /**
@@ -50,8 +50,12 @@ Polymer({
     },
   },
 
-  get submitButton() {
-    return this.$.submitButton;
+  show: function() {
+    this.behaviors.forEach((behavior) => {
+      if (behavior.onBeforeShow)
+        behavior.onBeforeShow.call(this);
+    });
+    this.$['license-selection-prompt-card'].show();
   },
 
   buttonClicked_: function() {
