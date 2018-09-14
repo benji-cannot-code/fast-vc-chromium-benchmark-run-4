b974dc7a774ba66896d9d4d0d7dc2dd6da7309d8
Dana Fried <dfried@chromium.org>
2018-09-14 20:33:24 +0000

    Respect Windows Text Zoom accessibility feature.
    This change adds an object which can access the UWP accessibility
    settings and listen on change events. It is used by the Windows
    implementation of display::Screen to listen for text zoom changes, which
    we have made the decision to support by changing our overall scaling by
    the same percentage (as anything more thorough would be prohibitively
    expensive in the current architecture).
    
    We achieve compliance by multiplying DPI and text zoom scaling together,
    the approach recommended as a stopgap by both our engineers and those
    working on a11y at Microsoft. Note that while this affects *most* UI
    within Chrome, it does not affect *all* UI - some elements (such as
    resize frame) are drawn by the system and are only scaled by DPI.
    
    A side effect is that all zoom factors are rounded to the nearest
    25%, so for instance, a net zoom of 190% will be rounded up to 200%.
    This will prevent DPI * Zoom factors that are not even multiples of 25%
    from creating blurry or smeared Chrome rendering or similar artifacts,
    which we have already seen as a result of problems with DPI scaling.
    
    Another effect is to decouple physical DPI from screen scaling, which
    has necessitated some small changes to the Windows Glass frame as well
    as the handler for the DPI changed Windows event. Without these changes
    serious visual artifacts and DCHECK failures can occur because systems
    are reporting/using different measurements for DPI. Changes to the Glass
    frame were necessary because Windows paints the resize handle around the
    window and does not use the text scale factor as part of its size
    calculation.
    
    Bug: 866513
    Change-Id: Ia99d1f3c1b2aff973d63bd7ff637b01f166cd843
    Reviewed-on: https://chromium-review.googlesource.com/1214747
    Reviewed-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591459}

--- ui/display/win/dpi.h
+++ ui/display/win/dpi.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define UI_DISPLAY_WIN_DPI_H_
 
 #include "ui/display/display_export.h"
-#include "ui/gfx/geometry/size.h"
 
 namespace display {
 namespace win {
@@ -24,15 +23,24 @@ DISPLAY_EXPORT void SetDefaultDeviceScaleFactor(float scale);
 // scale factor is 1.0. This does not handle per-monitor DPI.
 DISPLAY_EXPORT float GetDPIScale();
 
-// Equivalent to GetDPIScale() but ignores the --force-device-scale-factor flag.
-float GetUnforcedDeviceScaleFactor();
-
+// Deprecated. Use win::ScreenWin::GetDPIForHWND instead.
+//
 // Returns the equivalent DPI for |device_scaling_factor|.
 DISPLAY_EXPORT int GetDPIFromScalingFactor(float device_scaling_factor);
 
-// Returns the equivalent scaling factor for |dpi|.
-DISPLAY_EXPORT float GetScalingFactorFromDPI(int dpi);
+namespace internal {
+// Note: These methods do not take accessibility adjustments into account.
 
+// Equivalent to GetDPIScale() but ignores the --force-device-scale-factor flag.
+float GetUnforcedDeviceScaleFactor();
+
+// Returns the equivalent scaling factor for |dpi|.
+float GetScalingFactorFromDPI(int dpi);
+
+// Gets the default DPI for the system.
+int GetDefaultSystemDPI();
+
+}  // namespace internal
 }  // namespace win
 }  // namespace display
 
