baae1f3ec96da7bfe1a4f46fe55488d7b9e48a00
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2018-09-14 19:26:23 +0000

    distiller: Only fetch image data on iOS
    Only iOS makes use of image data for offline viewing. Skip image
    fetching to avoid network activity, and save memory usage in
    InMemoryContentStore.
    
    Bug: 864015
    Change-Id: Ieda9e1155bcb96d9e01442af9898df8547c7af13
    Reviewed-on: https://chromium-review.googlesource.com/1226520
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591431}

--- components/dom_distiller/core/distiller.h
+++ components/dom_distiller/core/distiller.h
@@ -85,6 +85,8 @@ class DistillerImpl : public Distiller {
 
   void SetMaxNumPagesInArticle(size_t max_num_pages);
 
+  static bool DoesFetchImages();
+
  private:
   // In case of multiple pages, the Distiller maintains state of multiple pages
   // as page numbers relative to the page number where distillation started.
@@ -117,9 +119,9 @@ class DistillerImpl : public Distiller {
       std::unique_ptr<proto::DomDistillerResult> distilled_page,
       bool distillation_successful);
 
-  virtual void FetchImage(int page_num,
-                          const std::string& image_id,
-                          const std::string& image_url);
+  virtual void MaybeFetchImage(int page_num,
+                               const std::string& image_id,
+                               const std::string& image_url);
 
   // Distills the next page.
   void DistillNextPage();
