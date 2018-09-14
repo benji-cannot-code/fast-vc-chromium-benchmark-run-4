67208b804fd96d2d3eb60bfea984a175885f0917
Navid Zolghadr <nzolghadr@chromium.org>
2018-09-14 15:13:13 +0000

    Send pointerrawmove when in pointerlock
    Unify the path for sending pointerrawmove in pointerlock
    mode with the normal event processing path.
    
    Bug: 880524
    Change-Id: I021261e0107b6172da02684cf82e0f46df304eba
    Reviewed-on: https://chromium-review.googlesource.com/1205476
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Ella Ge <eirage@chromium.org>
    Reviewed-by: Mustaq Ahmed <mustaq@chromium.org>
    Commit-Queue: Navid Zolghadr <nzolghadr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591347}

--- third_party/blink/renderer/core/exported/web_view_impl.cc
+++ third_party/blink/renderer/core/exported/web_view_impl.cc
@@ -1740,11 +1740,6 @@ WebInputEventResult WebViewImpl::HandleInputEvent(
     return WebInputEventResult::kHandledSystem;
   }
 
-  // TODO(nzolghadr): Add pointerrawmove in pointerlock path as well.
-  if (is_pointer_locked &&
-      input_event.GetType() == WebInputEvent::kPointerRawMove)
-    return WebInputEventResult::kHandledSystem;
-
   Document& main_frame_document = *MainFrameImpl()->GetFrame()->GetDocument();
 
   if (input_event.GetType() != WebInputEvent::kMouseMove) {
@@ -1761,9 +1756,13 @@ WebInputEventResult WebViewImpl::HandleInputEvent(
     }
   }
 
+  // Skip the pointerrawmove for mouse capture case.
   if (mouse_capture_node_ &&
-      (WebInputEvent::IsMouseEventType(input_event.GetType()) ||
-       input_event.GetType() == WebInputEvent::kPointerRawMove)) {
+      input_event.GetType() == WebInputEvent::kPointerRawMove)
+    return WebInputEventResult::kHandledSystem;
+
+  if (mouse_capture_node_ &&
+      WebInputEvent::IsMouseEventType(input_event.GetType())) {
     TRACE_EVENT1("input", "captured mouse event", "type",
                  input_event.GetType());
     // Save m_mouseCaptureNode since mouseCaptureLost() will clear it.
@@ -1782,10 +1781,6 @@ WebInputEventResult WebViewImpl::HandleInputEvent(
         break;
       case WebInputEvent::kMouseMove:
         event_type = EventTypeNames::mousemove;
-        break;
-      case WebInputEvent::kPointerRawMove:
-        // There will be no mouse event for raw move events.
-        event_type = EventTypeNames::pointerrawmove;
         break;
       case WebInputEvent::kMouseLeave:
         event_type = EventTypeNames::mouseout;
