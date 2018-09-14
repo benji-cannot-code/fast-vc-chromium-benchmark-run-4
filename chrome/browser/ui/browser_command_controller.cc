0cd199ae7289b19f5e0507ffe5263c584dedac67
Marc Treib <treib@chromium.org>
2018-09-14 06:21:40 +0000

    Remove unnecessary sync includes in /chrome/browser/ui
    Bug: none
    Change-Id: I5ea4c8dbc6cb3f6385fdd27ed94a5a3faf61edf2
    Reviewed-on: https://chromium-review.googlesource.com/1224034
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591295}

--- chrome/browser/ui/browser_command_controller.cc
+++ chrome/browser/ui/browser_command_controller.cc
@@ -46,7 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/pref_names.h"
 #include "chrome/common/profiling.h"
 #include "components/bookmarks/common/bookmark_pref_names.h"
-#include "components/browser_sync/profile_sync_service.h"
 #include "components/dom_distiller/core/dom_distiller_switches.h"
 #include "components/feature_engagement/buildflags.h"
 #include "components/prefs/pref_service.h"
--- chrome/browser/ui/desktop_ios_promotion/sms_service_factory.cc
+++ chrome/browser/ui/desktop_ios_promotion/sms_service_factory.cc
@@ -6,9 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/desktop_ios_promotion/sms_service_factory.h"
 
 #include "chrome/browser/signin/identity_manager_factory.h"
-#include "chrome/browser/sync/profile_sync_service_factory.h"
 #include "chrome/browser/ui/desktop_ios_promotion/sms_service.h"
-#include "components/browser_sync/profile_sync_service.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "components/signin/core/browser/profile_oauth2_token_service.h"
 #include "components/signin/core/browser/signin_manager.h"
