58ceea8eb6ce91d0dd493a5ebe3844188525b4a3
Jordy Greenblatt <jordynass@chromium.org>
2018-09-15 01:17:07 +0000

    [CrOS Multidevice] Update Strings in Setup Flow and Settings UIs
    Note that I did not update the Instant Tethering subtext but that is a
    little more involved in that it may require refactoring
    network-summary-item. Once I've fixed that I will sweep through and
    double check all strings in both UIs.
    
    Below are screenshots of the changes...
    
    SETUP FLOW
    Start setup page string changes: screen/C0fZSBURQai
    Start setup page learn more link for messages: screen/LU3KYRHr1sN
    Password prompt: screen/ZSsBEsW584o
    
    SETTINGS
    Periods deleted from learn more links in top level page:
    screen/JwibdRciTcm
    screen/NHEYcoOwWiG
    screen/4j21Ai5yodW
    
    Bug: 883474, 870108
    Change-Id: Ie6c136925add22969358c1117fa3533bffbff542
    Reviewed-on: https://chromium-review.googlesource.com/1226516
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Commit-Queue: Jordy Greenblatt <jordynass@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591548}

--- chrome/browser/resources/chromeos/multidevice_setup/ui_page_container_behavior.js
+++ chrome/browser/resources/chromeos/multidevice_setup/ui_page_container_behavior.js
@@ -110,9 +110,7 @@ const UiPageContainerBehaviorImpl = {
     if (!this.i18nExists(textId))
       return;
 
-    const validNodeFn = (node, value) => node.tagName == 'A';
-    return this.i18nAdvanced(
-        textId, {attrs: {'id': validNodeFn, 'href': validNodeFn}});
+    return loadTimeData.getString(textId);
   },
 };
 
