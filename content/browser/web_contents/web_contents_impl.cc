2a15936fd95d39b8e02313597ee579affd5873b9
Nasko Oskov <nasko@chromium.org>
2018-09-15 00:11:10 +0000

    Cleanup some usage of IsBrowserSideNavigationEnabled.
    Bug: 789577
    Change-Id: Idef29eb4330d8a4b350865d7acf1f01abc94cc8e
    Reviewed-on: https://chromium-review.googlesource.com/1227409
    Commit-Queue: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591531}

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -5566,7 +5566,6 @@ WebContentsImpl::CreateThrottlesForNavigation(
 
 std::unique_ptr<NavigationUIData> WebContentsImpl::GetNavigationUIData(
     NavigationHandle* navigation_handle) {
-  DCHECK(IsBrowserSideNavigationEnabled());
   return GetContentClient()->browser()->GetNavigationUIData(navigation_handle);
 }
 
