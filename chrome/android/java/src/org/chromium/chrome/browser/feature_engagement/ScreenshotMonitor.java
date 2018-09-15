2565f6c6d384556b842113fff8f3c3bad718d063
Pete Williamson <petewil@chromium.org>
2018-09-15 00:24:08 +0000

    Screenshot Detection P2 metrics
    This change adds metrics for how often the user takes multiple
    screenshots of a page, and when actions such as bringing
    up sharing or in product help for downloads happen after
    screenshotting a page.
    
    It also moves the logging of Tab.screenshot usercounts into
    Tab.java to make it unit testable, and adds a unit test for it.
    
    Metrics design doc: https://docs.google.com/document/d/1iyiVQODF1qh_Qiz7Is1y9jvWt1DYjgXmiJgLbFt2tSs
    
    Bug: 857505
    Change-Id: I7bdcef8602782bade2fa1b3a55ff11d1ccfc6e6f
    Reviewed-on: https://chromium-review.googlesource.com/1211022
    Commit-Queue: Peter Williamson <petewil@chromium.org>
    Reviewed-by: Jian Li <jianli@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591536}

--- chrome/android/java/src/org/chromium/chrome/browser/feature_engagement/ScreenshotMonitor.java
+++ chrome/android/java/src/org/chromium/chrome/browser/feature_engagement/ScreenshotMonitor.java
@@ -19,7 +19,6 @@ import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.VisibleForTesting;
-import org.chromium.base.metrics.RecordUserAction;
 
 /**
  * This class detects screenshots by monitoring the screenshots directory on internal and external
@@ -82,10 +81,6 @@ public class ScreenshotMonitor {
             ThreadUtils.postOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    // Unit tests do not have JNI to native side user counts.
-                    if (!mSkipOsCallsForUnitTesting) {
-                      RecordUserAction.record("Tab.Screenshot");
-                    }
                     if (mScreenshotMonitor == null) return;
                     mScreenshotMonitor.onEventOnUiThread(uriPath);
                 }
