6798c0348e90c6b3b90f294a1920f9410e9f6db1
John Chen <johnchen@chromium.org>
2018-09-15 18:33:35 +0000

    [ChromeDriver] Improve W3C mode flag handling
    While processing Init Session command, ChromeDriver now looks for W3C
    mode flag at more locations, including capabilities.firstMatch and
    desiredCapabilities. Also making it possible to change the default mode
    using a single constant kW3CDefault.
    
    Bug: chromedriver:2371
    Change-Id: I674df3571f50762a595a02034bb891513cb10f9d
    Reviewed-on: https://chromium-review.googlesource.com/1227226
    Commit-Queue: John Chen <johnchen@chromium.org>
    Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591589}

--- chrome/test/chromedriver/session.cc
+++ chrome/test/chromedriver/session.cc
@@ -37,7 +37,7 @@ const base::TimeDelta Session::kDefaultScriptTimeout =
 
 Session::Session(const std::string& id)
     : id(id),
-      w3c_compliant(false),
+      w3c_compliant(kW3CDefault),
       quit(false),
       detach(false),
       force_devtools_screenshot(false),
@@ -50,7 +50,7 @@ Session::Session(const std::string& id)
 
 Session::Session(const std::string& id, std::unique_ptr<Chrome> chrome)
     : id(id),
-      w3c_compliant(false),
+      w3c_compliant(kW3CDefault),
       quit(false),
       detach(false),
       force_devtools_screenshot(false),
