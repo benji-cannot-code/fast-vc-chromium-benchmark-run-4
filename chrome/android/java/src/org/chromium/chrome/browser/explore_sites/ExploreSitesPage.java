1e1a68565ad6042f0b45a1123e77dc426510bf65
Justin DeWitt <dewittj@chromium.org>
2018-09-17 20:02:55 +0000

    [EoS] Connect ExploreSitesBridge to the ExploreSitesService
    This actually calls ExploreSitesService::GetCatalog when the request
    comes in from the bridge.  Also adds methods for converting the native
    objects to Java.
    
    Bug: 867488
    Change-Id: Ideee8134ca7947ad95423485655441c3b55d77a7
    Reviewed-on: https://chromium-review.googlesource.com/1228312
    Reviewed-by: Cathy Li <chili@chromium.org>
    Commit-Queue: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591795}

--- chrome/android/java/src/org/chromium/chrome/browser/explore_sites/ExploreSitesPage.java
+++ chrome/android/java/src/org/chromium/chrome/browser/explore_sites/ExploreSitesPage.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.explore_sites;
 
 import android.app.Activity;
+import android.support.annotation.Nullable;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -50,7 +51,7 @@ public class ExploreSitesPage extends BasicNativePage {
         // TODO(chili): Set layout to be an observer of list model
     }
 
-    private void translateToModel(List<ExploreSitesCategory> categoryList) {
+    private void translateToModel(@Nullable List<ExploreSitesCategory> categoryList) {
         // TODO(chili): Call listmodel.addAll
     }
 
