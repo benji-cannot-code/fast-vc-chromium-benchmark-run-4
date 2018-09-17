9fcf5085a2b2c6a9368a8146c771f3a55cc44ef9
Kyle Horimoto <khorimoto@google.com>
2018-09-17 19:41:33 +0000

    [CrOS MultiDevice] Decouple localized strings from setup flow dialog.
    Previously, the setup flow only was accessible via the dialog; however,
    we are now in the process of adding it as part of OOBE. This CL moves
    these strings to a common location which will be used in a subsequent
    CL to add these strings to OOBE.
    
    Bug: 884041
    Change-Id: I539ff8e5de13a7ef59ed069361764759ece30d39
    Reviewed-on: https://chromium-review.googlesource.com/1228479
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591784}

---
+++ chrome/browser/ui/webui/chromeos/multidevice_setup/multidevice_setup_localized_strings_provider.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_WEBUI_CHROMEOS_MULTIDEVICE_SETUP_MULTIDEVICE_SETUP_LOCALIZED_STRINGS_PROVIDER_H_
+#define CHROME_BROWSER_UI_WEBUI_CHROMEOS_MULTIDEVICE_SETUP_MULTIDEVICE_SETUP_LOCALIZED_STRINGS_PROVIDER_H_
+
+namespace login {
+class LocalizedValuesBuilder;
+}
+
+namespace content {
+class WebUIDataSource;
+}
+
+namespace chromeos {
+
+namespace multidevice_setup {
+
+// Adds the strings needed for the MultiDevice setup flow to |html_source|.
+void AddLocalizedStrings(content::WebUIDataSource* html_source);
+
+// Same as AddLocalizedStrings but for a LocalizedValuesBuilder.
+void AddLocalizedValuesToBuilder(::login::LocalizedValuesBuilder* builder);
+
+}  // namespace multidevice_setup
+
+}  // namespace chromeos
+
+#endif  // CHROME_BROWSER_UI_WEBUI_CHROMEOS_MULTIDEVICE_SETUP_MULTIDEVICE_SETUP_LOCALIZED_STRINGS_PROVIDER_H_
