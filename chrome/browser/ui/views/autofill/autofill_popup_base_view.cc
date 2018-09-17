8dddb3bfcff1b27f365dfe39c1a833709311ad1f
Vasilii Sukhanov <vasilii@chromium.org>
2018-09-17 16:07:49 +0000

    Fix the generation dropdown update.
    The bug happened because we didn't call Layout().
    
    Bug: 884648
    Change-Id: Iadd1d5b66de2b60103470eccff8862ad14d185a2
    Reviewed-on: https://chromium-review.googlesource.com/1228195
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591695}

--- chrome/browser/ui/views/autofill/autofill_popup_base_view.cc
+++ chrome/browser/ui/views/autofill/autofill_popup_base_view.cc
@@ -204,6 +204,7 @@ void AutofillPopupBaseView::DoUpdateBoundsAndRedrawPopup() {
   popup_bounds.Inset(-GetWidget()->GetRootView()->border()->GetInsets());
   GetWidget()->SetBounds(popup_bounds);
 
+  Layout();
   SetClipPath();
   SchedulePaint();
 }
