9de76e9ee670867f9ca6b7477b851c6ec8d99109
Tommy Steimel <steimel@chromium.org>
2018-09-17 17:24:26 +0000

    [Media Intent Handler] Synchronize updating enabled for test
    This CL modifies MediaViewerUtil's method to force-enable the
    MediaLauncherActivity for testing to be synchronous. This prevents a
    race condition where a test tries to use the MediaLauncherActivity
    before the task to enable it has run.
    
    This CL also re-enables the tests that were disabled due to this
    issue.
    
    Bug: 882938
    Change-Id: I18252427e7391104c580d0c5360141338cc1d56d
    Reviewed-on: https://chromium-review.googlesource.com/1220673
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591721}

--- chrome/android/javatests/src/org/chromium/chrome/browser/media/MediaLauncherActivityTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/media/MediaLauncherActivityTest.java
@@ -23,7 +23,6 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.CollectionUtil;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.UrlUtils;
 import org.chromium.chrome.browser.ChromeSwitches;
 import org.chromium.chrome.browser.customtabs.CustomTabActivity;
@@ -62,7 +61,6 @@ public class MediaLauncherActivityTest {
     }
 
     @Test
-    @DisabledTest(message = "crbug.com/882938")
     @SmallTest
     public void testHandleVideoIntent() throws Exception {
         String url = TestContentProvider.createContentUrl("media/test.mp4");
@@ -70,7 +68,6 @@ public class MediaLauncherActivityTest {
     }
 
     @Test
-    @DisabledTest(message = "crbug.com/882938")
     @SmallTest
     public void testHandleAudioIntent() throws Exception {
         String url = TestContentProvider.createContentUrl("media/audio.mp3");
@@ -78,7 +75,6 @@ public class MediaLauncherActivityTest {
     }
 
     @Test
-    @DisabledTest(message = "crbug.com/882938")
     @SmallTest
     public void testHandleImageIntent() throws Exception {
         String url = TestContentProvider.createContentUrl("google.png");
@@ -86,7 +82,6 @@ public class MediaLauncherActivityTest {
     }
 
     @Test
-    @DisabledTest(message = "crbug.com/882938")
     @SmallTest
     public void testHandleFileURIIntent() throws Exception {
         String url = UrlUtils.getTestFileUrl("google.png");
