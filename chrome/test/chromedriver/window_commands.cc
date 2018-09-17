f0fed7ef28669759ea29bc17b06b8ef9681b9e81
Tatiana Buldina <buldina@chromium.org>
2018-09-17 21:06:47 +0000

    [ChromeDriver] Update returned error codes
    Make ExecuteExecuteAsyncScript(), ExecuteGet(), ExecuteExecuteScript()
    return kInvalidArgument according W3C standard
    
    chromedriver: 2000, chromedriver:2001, chromedriver:1987
    Change-Id: Id5c82fca12bc32ef88c59e84f53d04f77e6114ca
    Reviewed-on: https://chromium-review.googlesource.com/1228753
    Commit-Queue: Tatiana Buldina <buldina@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591813}

--- chrome/test/chromedriver/window_commands.cc
+++ chrome/test/chromedriver/window_commands.cc
@@ -313,7 +313,7 @@ Status ExecuteGet(Session* session,
   timeout->SetDuration(session->page_load_timeout);
   std::string url;
   if (!params.GetString("url", &url))
-    return Status(kUnknownError, "'url' must be a string");
+    return Status(kInvalidArgument, "'url' must be a string");
   Status status = web_view->Load(url, timeout);
   if (status.IsError())
     return status;
@@ -328,7 +328,7 @@ Status ExecuteExecuteScript(Session* session,
                             Timeout* timeout) {
   std::string script;
   if (!params.GetString("script", &script))
-    return Status(kUnknownError, "'script' must be a string");
+    return Status(kInvalidArgument, "'script' must be a string");
   if (script == ":takeHeapSnapshot") {
     return web_view->TakeHeapSnapshot(value);
   } else if (script == ":startProfile") {
@@ -338,7 +338,7 @@ Status ExecuteExecuteScript(Session* session,
   } else {
     const base::ListValue* args;
     if (!params.GetList("args", &args))
-      return Status(kUnknownError, "'args' must be a list");
+      return Status(kInvalidArgument, "'args' must be a list");
 
     return web_view->CallFunction(session->GetCurrentFrameId(),
                                   "function(){" + script + "}", *args, value);
@@ -352,10 +352,10 @@ Status ExecuteExecuteAsyncScript(Session* session,
                                  Timeout* timeout) {
   std::string script;
   if (!params.GetString("script", &script))
-    return Status(kUnknownError, "'script' must be a string");
+    return Status(kInvalidArgument, "'script' must be a string");
   const base::ListValue* args;
   if (!params.GetList("args", &args))
-    return Status(kUnknownError, "'args' must be a list");
+    return Status(kInvalidArgument, "'args' must be a list");
 
   return web_view->CallUserAsyncFunction(
       session->GetCurrentFrameId(), "function(){" + script + "}", *args,
