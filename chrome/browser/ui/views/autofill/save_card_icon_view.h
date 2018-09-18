4e9bd70159ade7a41cdd80e7eea301e8655bcfa2
Manas Verma <manasverma@google.com>
2018-09-18 22:22:48 +0000

    [Autofill] Browser Tests for Sign-in After Local Save.
    Adding browser tests for the sign-in after local save flow. This includes user-action sign-in metrics, the sign-in promo, and the manage cards prompt.
    
    Bug: 855186
    Change-Id: I8c1d15f79e6915230860d7f0f2529a10c01559b7
    Reviewed-on: https://chromium-review.googlesource.com/1196918
    Commit-Queue: Manas Verma <manasverma@google.com>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Mathieu Perreault <mathp@chromium.org>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592227}

--- chrome/browser/ui/views/autofill/save_card_icon_view.h
+++ chrome/browser/ui/views/autofill/save_card_icon_view.h
@@ -39,6 +39,8 @@ class SaveCardIconView : public PageActionIconView {
   const gfx::VectorIcon& GetVectorIcon() const override;
 
  private:
+  friend class SaveCardBubbleViewsBrowserTestBase;
+
   SaveCardBubbleControllerImpl* GetController() const;
 
   // gfx::AnimationDelegate:
