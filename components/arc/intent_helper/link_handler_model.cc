d840572a93218ed57e1499b279738d1296169472
Shao-Chuan Lee <shaochuan@chromium.org>
2018-09-18 04:22:59 +0000

    Record to UMA when ARC++ app is launched from link context menu
    Bug: 866326
    Change-Id: I4832e39d9bd4e82ae70e47f24dc62c5d8d3aafa6
    Reviewed-on: https://chromium-review.googlesource.com/1223553
    Commit-Queue: Shao-Chuan Lee <shaochuan@chromium.org>
    Reviewed-by: Maajid <maajid@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591934}

--- components/arc/intent_helper/link_handler_model.cc
+++ components/arc/intent_helper/link_handler_model.cc
@@ -9,10 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/bind.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/strings/utf_string_conversions.h"
 #include "components/arc/arc_bridge_service.h"
 #include "components/arc/arc_service_manager.h"
 #include "components/arc/intent_helper/arc_intent_helper_bridge.h"
+#include "components/arc/metrics/arc_metrics_constants.h"
 #include "components/google/core/common/google_util.h"
 #include "url/url_util.h"
 
@@ -76,6 +78,11 @@ void LinkHandlerModel::OpenLinkWithHandler(uint32_t handler_id) {
   if (handler_id >= handlers_.size())
     return;
   instance->HandleUrl(url_.spec(), handlers_[handler_id]->package_name);
+
+  UMA_HISTOGRAM_ENUMERATION(
+      "Arc.UserInteraction",
+      arc::UserInteractionType::APP_STARTED_FROM_LINK_CONTEXT_MENU,
+      arc::UserInteractionType::SIZE);
 }
 
 LinkHandlerModel::LinkHandlerModel() = default;
