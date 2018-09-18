3f89d9c162f2f611d2cec76d815bd2ef86e0466c
Yue Li <updowndota@chromium.org>
2018-09-18 21:16:05 +0000

    Check native Assistant flag for OptIn flow screen
    Bug: b/115781013
    Test: Manual Test
    Change-Id: I5aca7c8befd30ca65cc92bdee32bf7a0c7eb87b4
    Reviewed-on: https://chromium-review.googlesource.com/1231694
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592192}

--- chrome/browser/chromeos/login/screens/assistant_optin_flow_screen.cc
+++ chrome/browser/chromeos/login/screens/assistant_optin_flow_screen.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chromeos/assistant/buildflags.h"
+#include "chromeos/chromeos_switches.h"
 
 namespace chromeos {
 namespace {
@@ -39,9 +40,10 @@ void AssistantOptInFlowScreen::Show() {
     return;
 
 #if BUILDFLAG(ENABLE_CROS_ASSISTANT)
-  if (arc::IsAssistantAllowedForProfile(
+  if (chromeos::switches::IsAssistantEnabled() &&
+      arc::IsAssistantAllowedForProfile(
           ProfileManager::GetActiveUserProfile()) ==
-      ash::mojom::AssistantAllowedState::ALLOWED) {
+          ash::mojom::AssistantAllowedState::ALLOWED) {
     view_->Show();
     return;
   }
