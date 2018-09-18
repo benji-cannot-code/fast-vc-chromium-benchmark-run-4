e6d509d37f4f5d3e5e46f5a99cae0a97fe8d633c
Yutaka Hirano <yhirano@chromium.org>
2018-09-18 06:33:10 +0000

    Fail promise operations silently on thread termination
    On forcible thread termination (i.e., when
    v8::Isolate::IsExecutionTerminating() returns true), we cannot run
    promise related functionalities. Fail such operations silently rather
    than crashing or reporting tha failure to callers, and make the
    behavior explicit on comments.
    
    Bug: 882378
    Change-Id: I8b63746b1f25862aab2473f58dd05f0160e49ad5
    Reviewed-on: https://chromium-review.googlesource.com/1229874
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591959}

--- third_party/blink/renderer/bindings/core/v8/script_promise.cc
+++ third_party/blink/renderer/bindings/core/v8/script_promise.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/blink/renderer/platform/bindings/v8_throw_exception.h"
 #include "third_party/blink/renderer/platform/instance_counters.h"
+#include "third_party/blink/renderer/platform/wtf/compiler.h"
 #include "v8/include/v8.h"
 
 namespace blink {
@@ -171,7 +172,10 @@ class PromiseAllHandler final
 
 ScriptPromise::InternalResolver::InternalResolver(ScriptState* script_state)
     : resolver_(script_state,
-                v8::Promise::Resolver::New(script_state->GetContext())) {}
+                v8::Promise::Resolver::New(script_state->GetContext())) {
+  // |resolver| can be empty when the thread is being terminated. We ignore such
+  // errors.
+}
 
 v8::Local<v8::Promise> ScriptPromise::InternalResolver::V8Promise() const {
   if (resolver_.IsEmpty())
@@ -188,20 +192,26 @@ ScriptPromise ScriptPromise::InternalResolver::Promise() const {
 void ScriptPromise::InternalResolver::Resolve(v8::Local<v8::Value> value) {
   if (resolver_.IsEmpty())
     return;
-  resolver_.V8Value()
-      .As<v8::Promise::Resolver>()
-      ->Resolve(resolver_.GetContext(), value)
-      .ToChecked();
+  v8::Maybe<bool> result =
+      resolver_.V8Value().As<v8::Promise::Resolver>()->Resolve(
+          resolver_.GetContext(), value);
+  // |result| can be empty when the thread is being terminated. We ignore such
+  // errors.
+  ALLOW_UNUSED_LOCAL(result);
+
   Clear();
 }
 
 void ScriptPromise::InternalResolver::Reject(v8::Local<v8::Value> value) {
   if (resolver_.IsEmpty())
     return;
-  resolver_.V8Value()
-      .As<v8::Promise::Resolver>()
-      ->Reject(resolver_.GetContext(), value)
-      .ToChecked();
+  v8::Maybe<bool> result =
+      resolver_.V8Value().As<v8::Promise::Resolver>()->Reject(
+          resolver_.GetContext(), value);
+  // |result| can be empty when the thread is being terminated. We ignore such
+  // errors.
+  ALLOW_UNUSED_LOCAL(result);
+
   Clear();
 }
 
