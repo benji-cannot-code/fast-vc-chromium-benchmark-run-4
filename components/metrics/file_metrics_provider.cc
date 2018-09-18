e2acd74243a8f7a7383cdabcc5317fc1b05784f2
Brian White <bcwhite@chromium.org>
2018-09-18 18:45:51 +0000

    Add Cache ability to FilePersistentMemoryAllocator.
    This functionality is used by the FileMetricsProvider, currently based
    on a Feature to be enabled.
    
    Bug: 882892
    Change-Id: I55262453b58c5ed9473b33e8839bad423ca7ec84
    Reviewed-on: https://chromium-review.googlesource.com/1220310
    Commit-Queue: Brian White <bcwhite@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592120}

--- components/metrics/file_metrics_provider.cc
+++ components/metrics/file_metrics_provider.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/containers/flat_map.h"
+#include "base/feature_list.h"
 #include "base/files/file.h"
 #include "base/files/file_enumerator.h"
 #include "base/files/file_util.h"
@@ -32,6 +33,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace metrics {
 
 namespace {
+
+const base::Feature kCacheFileMetricData = {"CacheFileMetricData",
+                                            base::FEATURE_ENABLED_BY_DEFAULT};
 
 // These structures provide values used to define how files are opened and
 // accessed. It obviates the need for multiple code-paths within several of
@@ -491,7 +495,7 @@ FileMetricsProvider::AccessResult FileMetricsProvider::CheckAndMapMetricSource(
   }
 
   // Map the file and validate it.
-  std::unique_ptr<base::PersistentMemoryAllocator> memory_allocator =
+  std::unique_ptr<base::FilePersistentMemoryAllocator> memory_allocator =
       std::make_unique<base::FilePersistentMemoryAllocator>(
           std::move(mapped), 0, 0, base::StringPiece(), read_only);
   if (memory_allocator->GetMemoryState() ==
@@ -500,6 +504,11 @@ FileMetricsProvider::AccessResult FileMetricsProvider::CheckAndMapMetricSource(
   }
   if (memory_allocator->IsCorrupt())
     return ACCESS_RESULT_DATA_CORRUPTION;
+
+  // Cache the file data while running in a background thread so that there
+  // shouldn't be any I/O when the data is accessed from the main thread.
+  if (base::FeatureList::IsEnabled(kCacheFileMetricData))
+    memory_allocator->Cache();
 
   // Create an allocator for the mapped file. Ownership passes to the allocator.
   source->allocator = std::make_unique<base::PersistentHistogramAllocator>(
