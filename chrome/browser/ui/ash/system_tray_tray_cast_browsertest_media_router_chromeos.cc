98193c1d054605207cb7f9341ccefb669d6845bb
Eric Seckler <eseckler@chromium.org>
2018-09-18 16:59:11 +0000

    chromeos: Prepare TrayCastMediaRouter test for PostTask refactor
    This delays initialization of MockMediaRouters in the test until the
    browser's task executor is initialized.
    
    This is required for the upcoming move of
    
    BrowserThread::GetTaskRunnerForThread to
    base::Create*TaskRunnerWithTraits. In the future, TaskRunners can only
    be obtained after thread initialization. Since MockMediaRouter obtains
    a TaskRunner in its constructor, this patch moves its creation.
    
    Bug: 878356
    Change-Id: Idd599d23246d42427d35bccd3c027f0a24427cfa
    Reviewed-on: https://chromium-review.googlesource.com/1230024
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592081}

--- chrome/browser/ui/ash/system_tray_tray_cast_browsertest_media_router_chromeos.cc
+++ chrome/browser/ui/ash/system_tray_tray_cast_browsertest_media_router_chromeos.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <memory>
 #include <vector>
 
 #include "ash/public/cpp/ash_features.h"
@@ -111,6 +112,18 @@ class SystemTrayTrayCastMediaRouterChromeOSTest : public InProcessBrowserTest {
 
  private:
   // InProcessBrowserTest:
+  void PreRunTestOnMainThread() override {
+    media_router_ = std::make_unique<media_router::MockMediaRouter>();
+    ON_CALL(*media_router_, RegisterMediaSinksObserver(_))
+        .WillByDefault(Invoke(
+            this, &SystemTrayTrayCastMediaRouterChromeOSTest::CaptureSink));
+    ON_CALL(*media_router_, RegisterMediaRoutesObserver(_))
+        .WillByDefault(Invoke(
+            this, &SystemTrayTrayCastMediaRouterChromeOSTest::CaptureRoutes));
+    CastConfigClientMediaRouter::SetMediaRouterForTest(media_router_.get());
+    InProcessBrowserTest::PreRunTestOnMainThread();
+  }
+
   void SetUpOnMainThread() override {
     InProcessBrowserTest::SetUpOnMainThread();
     // Connect to the ash test interface.
@@ -124,6 +137,11 @@ class SystemTrayTrayCastMediaRouterChromeOSTest : public InProcessBrowserTest {
                         &ash_message_center_controller_);
   }
 
+  void PostRunTestOnMainThread() override {
+    InProcessBrowserTest::PostRunTestOnMainThread();
+    CastConfigClientMediaRouter::SetMediaRouterForTest(nullptr);
+  }
+
   bool CaptureSink(media_router::MediaSinksObserver* media_sinks_observer) {
     media_sinks_observer_ = media_sinks_observer;
     return true;
@@ -133,21 +151,7 @@ class SystemTrayTrayCastMediaRouterChromeOSTest : public InProcessBrowserTest {
     media_routes_observer_ = media_routes_observer;
   }
 
-  void SetUpInProcessBrowserTestFixture() override {
-    ON_CALL(media_router_, RegisterMediaSinksObserver(_))
-        .WillByDefault(Invoke(
-            this, &SystemTrayTrayCastMediaRouterChromeOSTest::CaptureSink));
-    ON_CALL(media_router_, RegisterMediaRoutesObserver(_))
-        .WillByDefault(Invoke(
-            this, &SystemTrayTrayCastMediaRouterChromeOSTest::CaptureRoutes));
-    CastConfigClientMediaRouter::SetMediaRouterForTest(&media_router_);
-  }
-
-  void TearDownInProcessBrowserTestFixture() override {
-    CastConfigClientMediaRouter::SetMediaRouterForTest(nullptr);
-  }
-
-  media_router::MockMediaRouter media_router_;
+  std::unique_ptr<media_router::MockMediaRouter> media_router_;
   media_router::MediaSinksObserver* media_sinks_observer_ = nullptr;
   media_router::MediaRoutesObserver* media_routes_observer_ = nullptr;
   ash::mojom::SystemTrayTestApiPtr tray_test_api_;
