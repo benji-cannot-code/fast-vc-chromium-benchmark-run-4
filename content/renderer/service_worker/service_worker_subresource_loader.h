ff263fa43755719fa25e6f01af92a3195f0fd8eb
Ben Kelly <wanderview@chromium.org>
2018-09-18 01:55:42 +0000

    Load blob body and side data in parallel in ServiceWorkerSubresourceLoader.
    Currently the ServiceWorkerSubresourceLoader sequentially reads the blob
    side data and then starts reading the main blob data.  This delays when
    the disk I/O can start to filling the DataPipe buffer which in turn
    increases time to first byte for the resource seen by the page.  Performing
    the reads in parallel allows the DataPipe to begin buffering data
    immediately.
    
    This CL changes the default behavior to load the main blob data and the
    side data in parallel.  The ServiceWorkerParallelSideDataReading feature
    is also added to support a future holdback study.  In addition, two new
    histograms measure the delays within the loader for starting to read the
    main blob data and for notifying the loader client that the body has
    started loading.
    
    R=kinuko@chromium.org, rkaplow@chromium.org
    
    Bug: 883052
    Change-Id: If34dbf05074bea8f3067934269099a88f2782bc0
    Reviewed-on: https://chromium-review.googlesource.com/1220563
    Commit-Queue: Ben Kelly <wanderview@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591914}

--- content/renderer/service_worker/service_worker_subresource_loader.h
+++ content/renderer/service_worker/service_worker_subresource_loader.h
@@ -98,7 +98,9 @@ class CONTENT_EXPORT ServiceWorkerSubresourceLoader
   void PauseReadingBodyFromNet() override;
   void ResumeReadingBodyFromNet() override;
 
+  int StartBlobReading(mojo::ScopedDataPipeConsumerHandle* body_pipe);
   void OnBlobSideDataReadingComplete(
+      mojo::ScopedDataPipeConsumerHandle data_pipe,
       const base::Optional<std::vector<uint8_t>>& metadata);
   void OnBlobReadingComplete(int net_error);
 
@@ -131,6 +133,8 @@ class CONTENT_EXPORT ServiceWorkerSubresourceLoader
                  ServiceWorkerSubresourceLoader>
       controller_connector_observer_;
   bool fetch_request_restarted_;
+  bool blob_reading_complete_;
+  bool side_data_reading_complete_;
 
   // These are given by the constructor (as the params for
   // URLLoaderFactory::CreateLoaderAndStart).
