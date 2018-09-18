202fda02b4505f2e5b99cf0332ff46e532422047
Robbie McElrath <rmcelrath@chromium.org>
2018-09-18 17:38:53 +0000

    Clean up ChromeSigninClientTest
    This is a follow up from a post-merge comment on crrev.com/c/1214081
    which moves the SetUp code to the constructor.
    
    Bug: 868015
    Change-Id: Ied0bd568560c49858eeb5261c5d89352f8b1d787
    Reviewed-on: https://chromium-review.googlesource.com/1229356
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592099}

--- chrome/browser/signin/chrome_signin_client_unittest.cc
+++ chrome/browser/signin/chrome_signin_client_unittest.cc
@@ -61,9 +61,7 @@ bool CallbackTester::WasCalledExactlyOnce() {
 
 class ChromeSigninClientTest : public testing::Test {
  public:
-  ChromeSigninClientTest() {}
-
-  void SetUp() override {
+  ChromeSigninClientTest() {
     // Create a signed-in profile.
     TestingProfile::Builder builder;
     profile_ = builder.Build();
