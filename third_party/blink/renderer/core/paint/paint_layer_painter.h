120fba8b4346bfe510392993c7e5c2e0bd01fe74
Xianzhu Wang <wangxianzhu@chromium.org>
2018-09-18 23:41:39 +0000

    [PE] Fix repeating fixed-position with PaintOffsetTranslation
    Previously PaintLayerPainter and PaintLayerClipper assumed that all
    fragments of an object share the same transform state. This is not true
    for repeating fixed-position objects with PaintOffsetTranslation: The
    fragments have different PaintOffsetTranslations. Previously we use
    infinite clip rect to avoid the problem of incorrect cull rects for
    repeated fragments. However, that also caused we painted too much for
    each page because of the infinite cull rects, causing slow printing
    and too much memory usage.
    
    Changed the following places to calculate correct cull rects for
    repeating fixed-position objects:
    1. PaintLayerFragment now saves root_fragment_data to indicate its
       coordinate space;
    2. PaintLayerPainter::AdjustForPaintProperties() no longer uses
       infinite cull rect when printing;
    3. In PaintLayerPainter before painting the LayoutObject,
       cull_rect is adjusted from the border box space of
       |fragment.root_fragment_data| to the containing transform's space;
    4. In PaintLayer::CollectFragments(), for each fragment, the input
       dirty rect is adjusted to the current root fragment's border box
       space;
    5. PaintLayerClipper::CalculateRectsWithGeometryMapper() is simplified
       to just map using transform states.
    
    Bug: 848839,869554
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I6d86e3716e91dbe5422cbf5e58b99fa2282d94dd
    Reviewed-on: https://chromium-review.googlesource.com/1090009
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592261}

--- third_party/blink/renderer/core/paint/paint_layer_painter.h
+++ third_party/blink/renderer/core/paint/paint_layer_painter.h
@@ -119,7 +119,9 @@ class CORE_EXPORT PaintLayerPainter {
 
   void PaintEmptyContentForFilters(GraphicsContext&);
 
-  void AdjustForPaintProperties(PaintLayerPaintingInfo&, PaintLayerFlags&);
+  void AdjustForPaintProperties(const GraphicsContext&,
+                                PaintLayerPaintingInfo&,
+                                PaintLayerFlags&);
 
   PaintLayer& paint_layer_;
 };
