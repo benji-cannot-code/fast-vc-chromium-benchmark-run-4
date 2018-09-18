0da0af0d02608bc0cfdddb969a964faddf0d7bb5
Ryan Hansberry <hansberry@chromium.org>
2018-09-18 19:37:56 +0000

    [CrOS Multidevice] Do not initialize BleConnectionManager if MultiDevice APIs are active.
    In the process, this also adapts HostConnectionMetricsLogger to use the MultiDevice APIs
    to properly log to the "Background" versions of its metrics.
    
    Bug: 824568, 752273, 854885, 857302
    Change-Id: I77fc52b3d07d903fe61ac62de2620216984d28de
    Reviewed-on: https://chromium-review.googlesource.com/1117897
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592146}

--- chromeos/components/tether/host_connection_metrics_logger.h
+++ chromeos/components/tether/host_connection_metrics_logger.h
@@ -71,6 +71,8 @@ class HostConnectionMetricsLogger
                            RecordConnectionResultSuccess);
   FRIEND_TEST_ALL_PREFIXES(HostConnectionMetricsLoggerTest,
                            RecordConnectionResultSuccess_Background);
+  FRIEND_TEST_ALL_PREFIXES(HostConnectionMetricsLoggerTest,
+                           RecordConnectionResultSuccess_MultiDeviceApiEnabled);
   FRIEND_TEST_ALL_PREFIXES(
       HostConnectionMetricsLoggerTest,
       RecordConnectionResultSuccess_Background_DifferentDevice);
@@ -78,6 +80,8 @@ class HostConnectionMetricsLogger
                            RecordConnectionResultFailure);
   FRIEND_TEST_ALL_PREFIXES(HostConnectionMetricsLoggerTest,
                            RecordConnectionResultFailure_Background);
+  FRIEND_TEST_ALL_PREFIXES(HostConnectionMetricsLoggerTest,
+                           RecordConnectionResultFailure_MultiDeviceApiEnabled);
   FRIEND_TEST_ALL_PREFIXES(
       HostConnectionMetricsLoggerTest,
       RecordConnectionResultFailure_Background_DifferentDevice);
@@ -108,6 +112,8 @@ class HostConnectionMetricsLogger
                            RecordConnectToHostDuration);
   FRIEND_TEST_ALL_PREFIXES(HostConnectionMetricsLoggerTest,
                            RecordConnectToHostDuration_Background);
+  FRIEND_TEST_ALL_PREFIXES(HostConnectionMetricsLoggerTest,
+                           RecordConnectToHostDuration_MultiDeviceApiEnabled);
   FRIEND_TEST_ALL_PREFIXES(HostConnectionMetricsLoggerTest,
                            RecordConnectionResultFailureNoResponse);
   FRIEND_TEST_ALL_PREFIXES(
