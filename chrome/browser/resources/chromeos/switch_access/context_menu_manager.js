2ee84aad2f99512bfc5e5e13053689174feec2cd
Zach Helfinstein <zhelfins@chromium.org>
2018-09-18 21:03:20 +0000

    Update SwitchAccess automation_manager.js to ES2015
    Bug: 884297
    Change-Id: I268e11dc5c71a78208fb07afd8dcc86c26964c05
    Reviewed-on: https://chromium-review.googlesource.com/1229773
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Zach Helfinstein <zhelfins@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592187}

--- chrome/browser/resources/chromeos/switch_access/context_menu_manager.js
+++ chrome/browser/resources/chromeos/switch_access/context_menu_manager.js
@@ -44,8 +44,9 @@ function ContextMenuManager(automationManager, desktop) {
 
   /**
    * Keeps track of when we're in the context menu.
+   * @private {boolean}
    */
-  this.inContextMenu = false;
+  this.inContextMenu_ = false;
 
   this.init_();
 }
@@ -81,7 +82,7 @@ ContextMenuManager.prototype = {
    * @param {!Array<!ContextMenuManager.Action>} actions
    */
   enter: function(actions) {
-    this.inContextMenu = true;
+    this.inContextMenu_ = true;
     if (actions !== this.actions_) {
       this.actions_ = actions;
       MessageHandler.sendMessage(
@@ -97,7 +98,7 @@ ContextMenuManager.prototype = {
    */
   exit: function() {
     this.clearFocusRing_();
-    this.inContextMenu = false;
+    this.inContextMenu_ = false;
     if (this.node_)
       this.node_ = null;
   },
@@ -105,10 +106,11 @@ ContextMenuManager.prototype = {
   /**
    * Move to the next available action in the menu. If this is no next action,
    * select the whole context menu to loop again.
+   * @return {boolean} Whether this function had any effect.
    */
   moveForward: function() {
-    if (!this.node_)
-      return;
+    if (!this.node_ || !this.inContextMenu_)
+      return false;
 
     this.clearFocusRing_();
     let treeWalker = new AutomationTreeWalker(
@@ -120,15 +122,17 @@ ContextMenuManager.prototype = {
     else
       this.node_ = node;
     this.updateFocusRing_();
+    return true;
   },
 
   /**
    * Move to the previous available action in the context menu. If we're at the
    * beginning of the list, start again at the end.
+   * @return {boolean} Whether this function had any effect.
    */
   moveBackward: function() {
-    if (!this.node_)
-      return;
+    if (!this.node_ || !this.inContextMenu_)
+      return false;
 
     this.clearFocusRing_();
     let treeWalker = new AutomationTreeWalker(
@@ -149,20 +153,23 @@ ContextMenuManager.prototype = {
 
     this.node_ = node;
     this.updateFocusRing_();
+    return true;
   },
 
   /**
    * Perform the action indicated by the current button (or no action if the
    * entire menu is selected). Then exit the context menu and return to
    * traditional navigation.
+   * @return {boolean} Whether this function had any effect.
    */
   selectCurrentNode: function() {
-    if (!this.node_)
-      return;
+    if (!this.node_ || !this.inContextMenu_)
+      return false;
 
     this.clearFocusRing_();
     this.node_.doDefault();
     this.exit();
+    return true;
   },
 
   /**
