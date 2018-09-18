4889b365d9f6c2f67c1efbb99b448832c79e1f53
Wei Li <weili@chromium.org>
2018-09-18 18:58:16 +0000

    Fix re-layout condition upon fullscreen style change
    When Chrome's fullscreen toolbar style changes, it needs to re-layout to
    make sure the UI is updated. This CL fixed a recent regression due to
    using |is_exiting_fullscreen| to avoid re-layout. Instead, only when
    Chrome exits fullscreen mode to non-fullscreen mode, we should avoid
    doing re-layout because exiting tab fullscreen to browser fullscreen
    still may requires a re-layout.
    
    BUG=884029
    
    Change-Id: I7392c2cb000b184c0f54b9cb6bb3de2b81a79dcf
    Reviewed-on: https://chromium-review.googlesource.com/1226014
    Commit-Queue: Wei Li <weili@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Sidney San Martín <sdy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592124}

--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -2976,7 +2976,7 @@ Profile* BrowserView::GetProfile() {
 
 void BrowserView::UpdateUIForTabFullscreen(TabFullscreenState state) {
   frame()->GetFrameView()->UpdateFullscreenTopUI(
-      state == ExclusiveAccessContext::STATE_EXIT_TAB_FULLSCREEN);
+      state == ExclusiveAccessContext::STATE_ENTER_TAB_FULLSCREEN);
 }
 
 WebContents* BrowserView::GetActiveWebContents() {
