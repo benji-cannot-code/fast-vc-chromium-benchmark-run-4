020bb5815caf82fb4f077954200aa059865ccbca
Finnur Thorarinsson <finnur@chromium.org>
2018-09-18 11:44:46 +0000

    Contacts Picker: Improved efficiency, scrolling, Select All, and search. - Minimize the use of the cursor. - Load the contact details upfront. - Keep the ContactDetails in the adapter. - Improved search: case-insensitive search within all contact details.
    Also temporarily disable photos (will be reinstated in a different way).
    
    Bug: 860467
    Change-Id: Ia6ddf396bf20d82ae301489a12795d2fad333af8
    Reviewed-on: https://chromium-review.googlesource.com/1221551
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592000}

--- chrome/android/java/src/org/chromium/chrome/browser/contacts_picker/ContactDetails.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contacts_picker/ContactDetails.java
@@ -94,17 +94,21 @@ public class ContactDetails implements Comparable<ContactDetails> {
     public String getContactDetailsAsString() {
         int count = 0;
         StringBuilder builder = new StringBuilder();
-        for (String email : mEmails) {
-            if (count++ > 0) {
-                builder.append("\n");
+        if (mEmails != null) {
+            for (String email : mEmails) {
+                if (count++ > 0) {
+                    builder.append("\n");
+                }
+                builder.append(email);
             }
-            builder.append(email);
         }
-        for (String phoneNumber : mPhoneNumbers) {
-            if (count++ > 0) {
-                builder.append("\n");
+        if (mPhoneNumbers != null) {
+            for (String phoneNumber : mPhoneNumbers) {
+                if (count++ > 0) {
+                    builder.append("\n");
+                }
+                builder.append(phoneNumber);
             }
-            builder.append(phoneNumber);
         }
 
         return builder.toString();
@@ -122,15 +126,19 @@ public class ContactDetails implements Comparable<ContactDetails> {
         writer.name("emails");
 
         writer.beginArray();
-        for (String email : mEmails) {
-            writer.value(email);
+        if (mEmails != null) {
+            for (String email : mEmails) {
+                writer.value(email);
+            }
         }
         writer.endArray();
 
         writer.name("phoneNumbers");
         writer.beginArray();
-        for (String phoneNumber : mPhoneNumbers) {
-            writer.value(phoneNumber);
+        if (mPhoneNumbers != null) {
+            for (String phoneNumber : mPhoneNumbers) {
+                writer.value(phoneNumber);
+            }
         }
         writer.endArray();
 
@@ -149,7 +157,7 @@ public class ContactDetails implements Comparable<ContactDetails> {
 
     @Override
     public int hashCode() {
-        Object[] values = {mId, mDisplayName, mEmails};
+        Object[] values = {mId, mDisplayName};
         return Arrays.hashCode(values);
     }
 
