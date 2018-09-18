3c26b64db2c7f1f4df997f7c21c467c997c841f5
Khushal <khushalsagar@chromium.org>
2018-09-18 03:03:34 +0000

    gpu: Make ServiceFontManager thread-safe.
    ServiceFontManager currently assumes that it should only be used from
    the same thread. While this is true in the case where there is a GPU
    process, and skia font cache usage is limited to the GPU main thread,
    this is not true for in process gpu.
    
    Update ServiceFontManager to allow use from multiple threads.
    
    R=ericrk@chromium.org
    
    Bug: 882549
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I58d4200f08c5ac92150129ead8ad1a09efc6d8a9
    Reviewed-on: https://chromium-review.googlesource.com/1227593
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591925}

--- gpu/command_buffer/service/service_font_manager.h
+++ gpu/command_buffer/service/service_font_manager.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/flat_map.h"
 #include "base/memory/weak_ptr.h"
-#include "base/threading/thread_checker.h"
+#include "base/synchronization/lock.h"
 #include "gpu/command_buffer/common/discardable_handle.h"
 #include "gpu/gpu_gles2_export.h"
 #include "third_party/skia/src/core/SkRemoteGlyphCache.h"
@@ -16,7 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace gpu {
 class Buffer;
 
-class GPU_GLES2_EXPORT ServiceFontManager {
+class GPU_GLES2_EXPORT ServiceFontManager
+    : public base::RefCountedThreadSafe<ServiceFontManager> {
  public:
   class GPU_GLES2_EXPORT Client {
    public:
@@ -25,7 +26,7 @@ class GPU_GLES2_EXPORT ServiceFontManager {
   };
 
   ServiceFontManager(Client* client);
-  ~ServiceFontManager();
+  void Destroy();
 
   bool Deserialize(const volatile char* memory,
                    size_t memory_size,
@@ -34,18 +35,22 @@ class GPU_GLES2_EXPORT ServiceFontManager {
   SkStrikeClient* strike_client() { return strike_client_.get(); }
 
  private:
+  friend class base::RefCountedThreadSafe<ServiceFontManager>;
   class SkiaDiscardableManager;
+
+  ~ServiceFontManager();
 
   bool AddHandle(SkDiscardableHandleId handle_id,
                  ServiceDiscardableHandle handle);
   bool DeleteHandle(SkDiscardableHandleId handle_id);
 
+  base::Lock lock_;
+
   Client* client_;
   std::unique_ptr<SkStrikeClient> strike_client_;
   base::flat_map<SkDiscardableHandleId, ServiceDiscardableHandle>
       discardable_handle_map_;
-  THREAD_CHECKER(thread_checker_);
-  base::WeakPtrFactory<ServiceFontManager> weak_factory_;
+  bool destroyed_ = false;
 };
 
 }  // namespace gpu
