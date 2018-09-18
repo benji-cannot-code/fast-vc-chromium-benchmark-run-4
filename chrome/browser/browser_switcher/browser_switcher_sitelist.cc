e7db8516d41a2e09b87b768a72baa4a0c0052b14
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2018-09-18 14:55:58 +0000

    [BrowserSwitcher] Add tests for schema-less URLs
    Bug: 884221
    Change-Id: Iaee4d979888e3bc5acf55f88c3618fd33810adf6
    Reviewed-on: https://chromium-review.googlesource.com/1226025
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592038}

--- chrome/browser/browser_switcher/browser_switcher_sitelist.cc
+++ chrome/browser/browser_switcher/browser_switcher_sitelist.cc
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service.h"
 #include "url/gurl.h"
 
+#include "third_party/re2/src/re2/re2.h"
+
 namespace browser_switcher {
 
 namespace {
@@ -27,6 +29,14 @@ bool StringContainsInsensitiveASCII(base::StringPiece input,
   return found != input.end();
 }
 
+// Checks if the omitted prefix for a non-fully specific prefix is one of the
+// expected parts that are allowed to be omitted (e.g. "https://").
+bool IsValidPrefix(base::StringPiece prefix) {
+  static re2::LazyRE2 re = {"(https?|file):(//)?"};
+  re2::StringPiece converted_prefix(prefix.data(), prefix.size());
+  return (prefix.empty() || re2::RE2::FullMatch(converted_prefix, *re));
+}
+
 // URL is passed as a (url_spec, url_host) to avoid heap-allocating a string for
 // the host every time this is called.
 bool UrlMatches(base::StringPiece url_spec,
@@ -38,8 +48,10 @@ bool UrlMatches(base::StringPiece url_spec,
   }
   if (pattern.find('/') != base::StringPiece::npos) {
     // Check prefix using the normalized URL, case sensitive.
-    return base::StartsWith(url_spec, GURL(pattern).spec(),
-                            base::CompareCase::SENSITIVE);
+    size_t pos = url_spec.find(pattern);
+    if (pos == std::string::npos)
+      return false;
+    return IsValidPrefix(base::StringPiece(url_spec.data(), pos));
   }
   // Compare hosts, case-insensitive.
   return StringContainsInsensitiveASCII(url_host, pattern);
