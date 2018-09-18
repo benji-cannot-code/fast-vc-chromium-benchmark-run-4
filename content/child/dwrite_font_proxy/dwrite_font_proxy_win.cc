20bd2768eb76867704912b64ae50b8608c50f523
Nico Weber <thakis@chromium.org>
2018-09-18 18:07:03 +0000

    win: Remove 6 exit-time destructors.
    No need to undo 5 IAT patches at process shutdown,
    and no need to call a ThreadLocalBoolean dtor either.
    
    Bug: 404525
    Change-Id: I95e4cc24d7b6ca7171457a0da8dd6b3231fe45a8
    Reviewed-on: https://chromium-review.googlesource.com/1231153
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Scott Graham <scottmg@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592111}

--- content/child/dwrite_font_proxy/dwrite_font_proxy_win.cc
+++ content/child/dwrite_font_proxy/dwrite_font_proxy_win.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/no_destructor.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/task/post_task.h"
 #include "content/child/dwrite_font_proxy/dwrite_localized_strings_win.h"
@@ -382,9 +383,10 @@ FontProxyScopeWrapper DWriteFontCollectionProxy::GetFontProxyScopeWrapper() {
     }
     SetProxy(std::move(dwrite_font_proxy));
   }
-  static base::ThreadLocalBoolean font_proxy_method_in_flight;
-
-  return FontProxyScopeWrapper(font_proxy_.get(), &font_proxy_method_in_flight);
+  static base::NoDestructor<base::ThreadLocalBoolean>
+      font_proxy_method_in_flight;
+  return FontProxyScopeWrapper(font_proxy_.get(),
+                               font_proxy_method_in_flight.get());
 }
 
 DWriteFontFamilyProxy::DWriteFontFamilyProxy() = default;
--- content/child/font_warmup_win.cc
+++ content/child/font_warmup_win.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
+#include "base/no_destructor.h"
 #include "base/numerics/safe_conversions.h"
 #include "base/numerics/safe_math.h"
 #include "base/strings/utf_string_conversions.h"
@@ -40,12 +41,6 @@ namespace {
 
 // The Skia font manager, used for the life of the process (leaked at the end).
 SkFontMgr* g_warmup_fontmgr = nullptr;
-
-base::win::IATPatchFunction g_iat_patch_open_sc_manager;
-base::win::IATPatchFunction g_iat_patch_close_service_handle;
-base::win::IATPatchFunction g_iat_patch_open_service;
-base::win::IATPatchFunction g_iat_patch_start_service;
-base::win::IATPatchFunction g_iat_patch_nt_connect_port;
 
 // These are from ntddk.h
 #if !defined(STATUS_ACCESS_DENIED)
@@ -401,27 +396,33 @@ void PatchServiceManagerCalls() {
 
   is_patched = true;
 
-  DWORD patched = g_iat_patch_open_sc_manager.Patch(
+  static base::NoDestructor<base::win::IATPatchFunction> patch_open_sc_manager;
+  DWORD patched = patch_open_sc_manager->Patch(
       L"dwrite.dll", service_provider_dll, "OpenSCManagerW",
       reinterpret_cast<void*>(OpenSCManagerWPatch));
   DCHECK(patched == 0);
 
-  patched = g_iat_patch_close_service_handle.Patch(
+  static base::NoDestructor<base::win::IATPatchFunction>
+      patch_close_service_handle;
+  patched = patch_close_service_handle->Patch(
       L"dwrite.dll", service_provider_dll, "CloseServiceHandle",
       reinterpret_cast<void*>(CloseServiceHandlePatch));
   DCHECK(patched == 0);
 
-  patched = g_iat_patch_open_service.Patch(
+  static base::NoDestructor<base::win::IATPatchFunction> patch_open_service;
+  patched = patch_open_service->Patch(
       L"dwrite.dll", service_provider_dll, "OpenServiceW",
       reinterpret_cast<void*>(OpenServiceWPatch));
   DCHECK(patched == 0);
 
-  patched = g_iat_patch_start_service.Patch(
+  static base::NoDestructor<base::win::IATPatchFunction> patch_start_service;
+  patched = patch_start_service->Patch(
       L"dwrite.dll", service_provider_dll, "StartServiceW",
       reinterpret_cast<void*>(StartServiceWPatch));
   DCHECK(patched == 0);
 
-  patched = g_iat_patch_nt_connect_port.Patch(
+  static base::NoDestructor<base::win::IATPatchFunction> patch_nt_connect_port;
+  patched = patch_nt_connect_port->Patch(
       L"dwrite.dll", "ntdll.dll", "NtAlpcConnectPort",
       reinterpret_cast<void*>(NtALpcConnectPortPatch));
   DCHECK(patched == 0);
