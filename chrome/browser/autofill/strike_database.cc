939cb321261339eca74d5768e1e4833a382a2bf6
Anne Lim <annelim@google.com>
2018-09-18 15:29:17 +0000

    Added AutofillStrikeDatabase Framework
    Added skeleton framework for AutofillStrikeDatabase.
    It will be used to store data about the number of times a user
    refuses to save their info.
    
    Bug: 884817
    
    Change-Id: Iaffac0ab45785e4c0ff261ecb9bbde31e270e00a
    Reviewed-on: https://chromium-review.googlesource.com/1228774
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Commit-Queue: Anne Lim <annelim@google.com>
    Cr-Commit-Position: refs/heads/master@{#592047}

---
+++ chrome/browser/autofill/strike_database.cc
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/autofill/strike_database.h"
+
+#include "base/task/post_task.h"
+#include "chrome/browser/autofill/strike_data.pb.h"
+#include "components/leveldb_proto/proto_database_impl.h"
+
+namespace autofill {
+
+namespace {
+
+const char kDatabaseClientName[] = "StrikeService";
+
+}  // namespace
+
+StrikeDatabase::StrikeDatabase(const base::FilePath& database_dir)
+    : db_(std::make_unique<leveldb_proto::ProtoDatabaseImpl<StrikeData>>(
+          base::CreateSequencedTaskRunnerWithTraits(
+              {base::MayBlock(), base::TaskPriority::BEST_EFFORT,
+               base::TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN}))),
+      weak_ptr_factory_(this) {
+  db_->Init(kDatabaseClientName, database_dir,
+            leveldb_proto::CreateSimpleOptions(),
+            base::BindRepeating(&StrikeDatabase::OnDatabaseInit,
+                                weak_ptr_factory_.GetWeakPtr()));
+}
+
+StrikeDatabase::~StrikeDatabase() {}
+
+void StrikeDatabase::OnDatabaseInit(bool success) {}
+
+}  // namespace autofill
