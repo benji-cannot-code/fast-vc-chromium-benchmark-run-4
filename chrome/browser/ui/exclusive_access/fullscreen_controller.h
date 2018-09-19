16a0a06b1ea6de39f833c6e9d79ddb0a96339ffa
Yuri Wiitala <miu@chromium.org>
2018-09-19 19:47:42 +0000

    Dialogs don't drop tab fullscreen when in FullscreenWithinTab mode.
    Overrides the default behavior of dropping fullscreen when a tab modal
    dialog is opened in the FullscreenWithinTab case. This is because, in
    FWT mode, the browser window is in its normal layout (not fullscreened).
    
    Bug: 883535,812769
    Change-Id: I1c262954b962d508eb86ef9a8a312bec03ab2332
    Reviewed-on: https://chromium-review.googlesource.com/1228976
    Commit-Queue: Yuri Wiitala <miu@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592522}

--- chrome/browser/ui/exclusive_access/fullscreen_controller.h
+++ chrome/browser/ui/exclusive_access/fullscreen_controller.h
@@ -104,6 +104,10 @@ class FullscreenController : public ExclusiveAccessControllerBase {
   bool IsFullscreenForTabOrPending(
       const content::WebContents* web_contents) const;
 
+  // Returns true if |web_contents| is in fullscreen mode as a screen-captured
+  // tab. See 'FullscreenWithinTab Note'.
+  bool IsFullscreenWithinTab(const content::WebContents* web_contents) const;
+
   // True if fullscreen was entered because of tab fullscreen (was not
   // previously in user-initiated fullscreen).
   bool IsFullscreenCausedByTab() const;
@@ -174,9 +178,6 @@ class FullscreenController : public ExclusiveAccessControllerBase {
   // See 'FullscreenWithinTab Note'.
   bool MaybeToggleFullscreenWithinTab(content::WebContents* web_contents,
                                       bool enter_fullscreen);
-  // Returns true if |web_contents| is in fullscreen mode as a screen-captured
-  // tab. See 'FullscreenWithinTab Note'.
-  bool IsFullscreenWithinTab(const content::WebContents* web_contents) const;
 
   // Helper methods that should be used in a TAB context.
   GURL GetRequestingOrigin() const;
