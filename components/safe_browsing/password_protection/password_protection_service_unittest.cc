c7a952c3532fc2b2dd21e6fbf65422d14a98b3e4
Daniel Rubery <drubery@chromium.org>
2018-09-19 22:35:03 +0000

    Remove expiring metrics on Referrer Chain Size
    These never produced actionable results, and can be recreated in the
    backend if we need them again.
    
    Bug: 884862
    Change-Id: Ia1ca37956fb7fe959c290c05c0f3d1650207ced5
    Reviewed-on: https://chromium-review.googlesource.com/1229501
    Reviewed-by: Jialiu Lin <jialiul@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592580}

--- components/safe_browsing/password_protection/password_protection_service_unittest.cc
+++ components/safe_browsing/password_protection/password_protection_service_unittest.cc
@@ -759,7 +759,6 @@ TEST_P(PasswordProtectionServiceTest,
       ElementsAre(base::Bucket(1 /* SUCCEEDED */, 1)));
   EXPECT_THAT(histograms_.GetAllSamples(kPasswordOnFocusVerdictHistogram),
               ElementsAre(base::Bucket(3 /* PHISHING */, 1)));
-  histograms_.ExpectTotalCount(kReferrerChainSizeOfPhishingVerdictHistogram, 1);
   LoginReputationClientResponse* actual_response =
       password_protection_service_->latest_response();
   EXPECT_EQ(expected_response.verdict_type(), actual_response->verdict_type());
@@ -807,7 +806,6 @@ TEST_P(PasswordProtectionServiceTest,
   EXPECT_THAT(
       histograms_.GetAllSamples(kProtectedPasswordEntryVerdictHistogram),
       ElementsAre(base::Bucket(3 /* PHISHING */, 1)));
-  histograms_.ExpectTotalCount(kReferrerChainSizeOfPhishingVerdictHistogram, 1);
 }
 
 TEST_P(PasswordProtectionServiceTest,
@@ -845,7 +843,6 @@ TEST_P(PasswordProtectionServiceTest,
   EXPECT_THAT(histograms_.GetAllSamples(kSyncPasswordEntryVerdictHistogram),
               ElementsAre(base::Bucket(3 /* PHISHING */, 1)));
   histograms_.ExpectTotalCount(kProtectedPasswordEntryVerdictHistogram, 0);
-  histograms_.ExpectTotalCount(kReferrerChainSizeOfPhishingVerdictHistogram, 1);
 }
 
 TEST_P(PasswordProtectionServiceTest, TestTearDownWithPendingRequests) {
