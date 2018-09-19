bb64c64b04540e5f3a3091669444600b3209206f
Zach Helfinstein <zhelfins@chromium.org>
2018-09-19 19:48:31 +0000

    Update SwitchAccess auto_scan_manager.js to ES2015
    Bug: 884297
    Change-Id: I4d733650e54b01459154c1c59da5c95246ec3d05
    Reviewed-on: https://chromium-review.googlesource.com/1232682
    Commit-Queue: Zach Helfinstein <zhelfins@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592523}

--- chrome/browser/resources/chromeos/switch_access/commands.js
+++ chrome/browser/resources/chromeos/switch_access/commands.js
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 class Commands {
   /**
-   * @constructor
    * @param {SwitchAccessInterface} switchAccess
    */
   constructor(switchAccess) {
