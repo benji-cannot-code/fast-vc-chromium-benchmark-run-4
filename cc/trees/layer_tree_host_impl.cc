efcb206b212cca0d04cbce92e74439e13f697d2c
Sahel Sharify <sahel@chromium.org>
2018-09-19 15:31:30 +0000

    Revert "Use of dump without crashing to debug smooth scroll animation bug."
    This reverts commit 5665173d7b24c8b8bba8af94d0eebfdf0ab38a13.
    
    Reason for revert: this is ranked as #1 renderer process related crash on the latest chrome dev 71.0.3554.0 on Windows. 97 crashes from 90 clients so far.
    
    Original change's description:
    > Use of dump without crashing to debug smooth scroll animation bug.
    > 
    > Bug: 797708
    > Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    > Change-Id: Icce9d0c3eeed94297936aebc81681f5db2ec0682
    > Reviewed-on: https://chromium-review.googlesource.com/1226825
    > Reviewed-by: David Bokan <bokan@chromium.org>
    > Commit-Queue: Sahel Sharify <sahel@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#591377}
    
    TBR=bokan@chromium.org,sahel@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 797708, 884592
    Change-Id: I1e2787982eb1920e28e9a38281de0c76334162f2
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Reviewed-on: https://chromium-review.googlesource.com/1233954
    Reviewed-by: Sahel Sharify <sahel@chromium.org>
    Commit-Queue: Sahel Sharify <sahel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592409}

--- cc/trees/layer_tree_host_impl.cc
+++ cc/trees/layer_tree_host_impl.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/containers/adapters.h"
 #include "base/containers/flat_map.h"
-#include "base/debug/dump_without_crashing.h"
 #include "base/json/json_writer.h"
 #include "base/memory/memory_coordinator_client_registry.h"
 #include "base/memory/ptr_util.h"
@@ -3837,14 +3836,14 @@ InputHandler::ScrollStatus LayerTreeHostImpl::ScrollAnimated(
       scroll_status.thread = SCROLL_IGNORED;
       scroll_status.main_thread_scrolling_reasons =
           MainThreadScrollingReason::kNotScrollable;
-      // Dump without crashing to debug https://crbug.com/797708, based on the
+      // Adding NOTREACHED to debug https://crbug.com/797708, based on the
       // traces on the bug scrolling gets stuck in a situation where the
       // layout_tree_host_impl assumes that there is an ongoing scroll animation
       // since scroll_node exists but the
       // ScrollOffsetAnimationsImpl::ScrollAnimationUpdateTarget returns false
       // since no keyframe_model exists. TODO(sahel):remove this once the issue
       // is fixed.
-      base::debug::DumpWithoutCrashing();
+      NOTREACHED();
     }
     return scroll_status;
   }
