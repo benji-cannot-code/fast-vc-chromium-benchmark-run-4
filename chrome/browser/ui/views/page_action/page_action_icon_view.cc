21abe98c0a7458d04f63e2a0a8696c2fa678bcf5
Elly Fong-Jones <ellyjones@chromium.org>
2018-09-19 13:22:41 +0000

    views: use platform spacebar behavior in PageActionIconView
    On Mac, the spacebar activates buttons on keydown; on other platforms it
    activates them on keyup. Not implementing this behavior here caused the
    following bug:
    
    1) User focuses the bookmark bar star
    2) User hits space to activate it, which opens the bubble
    3) User tabs to the "Remove" button
    4) User hits space to activate that button
    5) The "Remove" button activates on keydown and the bubble dismisses
    6) The star gets keyboard focus again
    7) The star immediately handles the keyup and reopens the bubble!
    
    The fix is for PageActionIconView to honor the platform spacebar affinity. There
    is an old TODO in this code to merge it with views::Button but I would not want
    to merge such a change back to beta so this small fix seems more expedient.
    
    Bug: 712341
    Change-Id: If932928bb069493e61f78f2d34e95a0aa45fb504
    Reviewed-on: https://chromium-review.googlesource.com/1230437
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592379}

--- chrome/browser/ui/views/page_action/page_action_icon_view.cc
+++ chrome/browser/ui/views/page_action/page_action_icon_view.cc
@@ -26,6 +26,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/focus_ring.h"
 #include "ui/views/style/platform_style.h"
 
+namespace {
+
+bool ActivateButtonOnSpaceDown() {
+  return views::PlatformStyle::kKeyClickActionOnSpace ==
+         views::Button::KeyClickAction::CLICK_ON_KEY_PRESS;
+}
+
+}  // namespace
+
 void PageActionIconView::Init() {
   AddChildView(image());
   image()->set_can_process_events_within_subtree(false);
@@ -128,15 +137,18 @@ bool PageActionIconView::OnKeyPressed(const ui::KeyEvent& event) {
     return false;
 
   AnimateInkDrop(views::InkDropState::ACTIVATED, nullptr /* &event */);
-  // As with Button, return activates on key down and space activates on
-  // key up.
-  if (event.key_code() == ui::VKEY_RETURN)
+  // This behavior is duplicated from Button: on some platforms buttons activate
+  // on VKEY_SPACE keydown, and on some platforms they activate on VKEY_SPACE
+  // keyup. All platforms activate buttons on VKEY_RETURN keydown though.
+  if (ActivateButtonOnSpaceDown() || event.key_code() == ui::VKEY_RETURN)
     ExecuteCommand(EXECUTE_SOURCE_KEYBOARD);
   return true;
 }
 
 bool PageActionIconView::OnKeyReleased(const ui::KeyEvent& event) {
-  if (event.key_code() != ui::VKEY_SPACE)
+  // If buttons activate on VKEY_SPACE keydown, don't re-execute the command on
+  // keyup.
+  if (event.key_code() != ui::VKEY_SPACE || ActivateButtonOnSpaceDown())
     return false;
 
   ExecuteCommand(EXECUTE_SOURCE_KEYBOARD);
