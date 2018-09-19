461aa2f415b964ba3c254aa42b56fc0f2913d980
rbpotter <rbpotter@chromium.org>
2018-09-19 23:55:35 +0000

    Print Preview refresh: Fix some issues with the link container
    Fixes some padding and focus issues in and around the link-container.
    (1) Correct padding above and below link-container
    (2) Make icon focusable rather than the entire row
    (3) Restore shortcut in system dialog string
    (4) Make icon not focusable when disabled
    (5) Correct padding in other-options-settings
    
    Bug: 884729, 885102, 884605
    Change-Id: I0c8e1ac700ef93a4c6e247329d66285386af9f9f
    Reviewed-on: https://chromium-review.googlesource.com/1231826
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592614}

--- chrome/browser/resources/print_preview/new/link_container.js
+++ chrome/browser/resources/print_preview/new/link_container.js
@@ -12,7 +12,10 @@ Polymer({
     /** @type {?print_preview.Destination} */
     destination: Object,
 
-    disabled: Boolean,
+    disabled: {
+      type: Boolean,
+      reflectToAttribute: true,
+    },
 
     /** @private {boolean} */
     shouldShowSystemDialogLink_: {
