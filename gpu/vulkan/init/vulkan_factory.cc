fb1b523f15787d6ace6cc81754f51e7450ea7cfb
Michael Spang <spang@chromium.org>
2018-09-19 22:30:31 +0000

    Revert "ozone: Enable vulkan by default"
    This reverts commit f3c9ed0efe2ade7757607bcf356712883c315450.
    
    Reason for revert: breaks Linux Chromium OS ASan LSan Tests
    
    Bug: 887048
    
    Original change's description:
    > ozone: Enable vulkan by default
    > 
    > Vulkan is enabled by default on Linux desktop (X11) builds. Enable it on
    > ozone as well to make sure we have build coverage.
    > 
    > Bug: 861853
    > Test: compile
    > Change-Id: I7c15e365b2a4cfc7160847457ce9bf46a3f12f1e
    > Reviewed-on: https://chromium-review.googlesource.com/1229233
    > Commit-Queue: Michael Spang <spang@chromium.org>
    > Reviewed-by: Antoine Labour <piman@chromium.org>
    > Reviewed-by: Chris Blume <cblume@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#592451}
    
    TBR=spang@chromium.org,piman@chromium.org,cblume@chromium.org
    
    Change-Id: I6a95fdfdb742e4a876382c72b212bacd2b601c23
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 861853
    Reviewed-on: https://chromium-review.googlesource.com/1235194
    Commit-Queue: Michael Spang <spang@chromium.org>
    Reviewed-by: Michael Spang <spang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592579}

--- gpu/vulkan/init/vulkan_factory.cc
+++ gpu/vulkan/init/vulkan_factory.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if defined(USE_X11)
-#include "gpu/vulkan/x/vulkan_implementation_x11.h"  // nogncheck
+#include "gpu/vulkan/x/vulkan_implementation_x11.h"
 #endif
 
 #if defined(USE_OZONE)
