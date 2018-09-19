9343f6a34a8a36566eb92490a9feffd08d7ebb15
Jimmy Gong <jimmyxgong@google.com>
2018-09-19 00:49:06 +0000

    Add invalid URL error message to SMB mount shares
    - Adds an error message for when an invalid SMB mount url is provided.
    
    BUG=chromium:882594
    
    Change-Id: I9952a11cdaf4baa727076737636a1346c0048bc2
    Reviewed-on: https://chromium-review.googlesource.com/1217852
    Commit-Queue: jimmy gong <jimmyxgong@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: Scott Chen <scottchen@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592287}

--- chrome/browser/chromeos/smb_client/smb_errors_unittest.cc
+++ chrome/browser/chromeos/smb_client/smb_errors_unittest.cc
@@ -128,7 +128,7 @@ TEST_F(SmbErrorsTest, SmbErrorToMountResult) {
             TranslateErrorToMountResult(smbprovider::ERROR_NOT_A_FILE));
   EXPECT_EQ(SmbMountResult::UNKNOWN_FAILURE,
             TranslateErrorToMountResult(smbprovider::ERROR_NOT_EMPTY));
-  EXPECT_EQ(SmbMountResult::NOT_FOUND,
+  EXPECT_EQ(SmbMountResult::INVALID_URL,
             TranslateErrorToMountResult(smbprovider::ERROR_INVALID_URL));
   EXPECT_EQ(SmbMountResult::UNKNOWN_FAILURE,
             TranslateErrorToMountResult(smbprovider::ERROR_IO));
@@ -170,7 +170,7 @@ TEST_F(SmbErrorsTest, FileErrorToMountResult) {
             TranslateErrorToMountResult(base::File::FILE_ERROR_NOT_A_FILE));
   EXPECT_EQ(SmbMountResult::UNKNOWN_FAILURE,
             TranslateErrorToMountResult(base::File::FILE_ERROR_NOT_EMPTY));
-  EXPECT_EQ(SmbMountResult::NOT_FOUND,
+  EXPECT_EQ(SmbMountResult::INVALID_URL,
             TranslateErrorToMountResult(base::File::FILE_ERROR_INVALID_URL));
   EXPECT_EQ(SmbMountResult::UNKNOWN_FAILURE,
             TranslateErrorToMountResult(base::File::FILE_ERROR_IO));
--- chrome/browser/resources/settings/downloads_page/smb_browser_proxy.js
+++ chrome/browser/resources/settings/downloads_page/smb_browser_proxy.js
@@ -20,6 +20,7 @@ const SmbMountResult = {
   NOT_FOUND: 3,
   UNSUPPORTED_DEVICE: 4,
   MOUNT_EXISTS: 5,
+  INVALID_URL: 6,
 };
 
 cr.define('settings', function() {
