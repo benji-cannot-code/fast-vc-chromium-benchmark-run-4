fb955c49da3964061a36e970b67443e571e8374a
Yiming Zhou <uwyiming@google.com>
2018-09-19 21:55:28 +0000

    Jslint fixes for the action recorder extension.
    Bug: 855284
    Change-Id: I10e167459213a0a4acab8acaaba3b4816798f1e0
    Reviewed-on: https://chromium-review.googlesource.com/1231380
    Reviewed-by: Jared Saul <jsaul@google.com>
    Commit-Queue: Yiming Zhou <uwyiming@google.com>
    Cr-Commit-Position: refs/heads/master@{#592561}

--- components/test/data/autofill/automated_integration/action_recorder_extension/content/panel.js
+++ components/test/data/autofill/automated_integration/action_recorder_extension/content/panel.js
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 document.addEventListener('DOMContentLoaded', () => {
-
   const startingURLField = document.getElementById('startingURL');
   const saveBtn = document.getElementById('saveBtn');
   const stopBtn = document.getElementById('stopBtn');
@@ -13,9 +12,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Sets up the 'Add Chrome Action' panel.
   const newActionTypeSelect = document.getElementById('newActionType');
-  const passwordActionParamsDiv =
-      document.getElementById('passwordActionParamsDiv');
-  const newActionParamDivs = [passwordActionParamsDiv];
   const passwordActionUserParam = document.getElementById('pmUser');
   const passwordActionPasswordParam = document.getElementById('pmPassword');
   createNewActionTypeOption('Save Password', 'savePassword');
@@ -24,9 +20,9 @@ document.addEventListener('DOMContentLoaded', () => {
   createNewActionTypeOption('Don\'t Update Password', 'rejectUpdatePassword');
   const addChromeAction = document.getElementById('addChromeAction');
   addChromeAction.addEventListener('click', (event) => {
-    let action = { type: newActionTypeSelect.value };
+    let action = {type: newActionTypeSelect.value};
 
-    switch(newActionTypeSelect.value) {
+    switch (newActionTypeSelect.value) {
       case ActionTypeEnum.SAVE_PASSWORD:
       case ActionTypeEnum.REJECT_SAVE_PASSWORD:
       case ActionTypeEnum.UPDATE_PASSWORD:
@@ -37,10 +33,8 @@ document.addEventListener('DOMContentLoaded', () => {
       default:
     }
 
-    return sendRuntimeMessageToBackgroundScript({
-      type: RecorderMsgEnum.ADD_ACTION,
-      action: action
-    });
+    return sendRuntimeMessageToBackgroundScript(
+        {type: RecorderMsgEnum.ADD_ACTION, action: action});
   });
 
   function createNewActionTypeOption(text, value) {
@@ -65,16 +59,17 @@ document.addEventListener('DOMContentLoaded', () => {
     iconBoxDiv.textContent = 'X';
     iconBoxDiv.addEventListener('click', (event) => {
       sendRuntimeMessageToBackgroundScript({
-          type: RecorderUiMsgEnum.REMOVE_ACTION,
-          action_index: action.action_index })
-      .then((response) => {
-        if (response) {
-          actionDiv.remove();
-        }
+        type: RecorderUiMsgEnum.REMOVE_ACTION,
+        action_index: action.action_index
       })
-      .catch((error) => {
-        console.error('Unable to remove the recorded action!', error);
-      });
+          .then((response) => {
+            if (response) {
+              actionDiv.remove();
+            }
+          })
+          .catch((error) => {
+            console.error('Unable to remove the recorded action!', error);
+          });
     });
     flexContainerDiv.appendChild(selectorLabel);
     flexContainerDiv.appendChild(actionLabel);
@@ -183,13 +178,13 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   saveBtn.addEventListener('click', (event) => {
-    sendRuntimeMessageToBackgroundScript({ type: RecorderMsgEnum.SAVE });
+    sendRuntimeMessageToBackgroundScript({type: RecorderMsgEnum.SAVE});
   });
   stopBtn.addEventListener('click', (event) => {
-    sendRuntimeMessageToBackgroundScript({ type: RecorderMsgEnum.STOP });
+    sendRuntimeMessageToBackgroundScript({type: RecorderMsgEnum.STOP});
   });
   cancelBtn.addEventListener('click', (event) => {
-    sendRuntimeMessageToBackgroundScript({ type: RecorderMsgEnum.CANCEL });
+    sendRuntimeMessageToBackgroundScript({type: RecorderMsgEnum.CANCEL});
   });
 
   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
@@ -208,10 +203,10 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   // Get the recipe from the background script and render it.
-  sendRuntimeMessageToBackgroundScript(
-      { type: RecorderUiMsgEnum.GET_RECIPE })
-  .then((recipe) => {
-    while(actionsListDiv.hasChildNodes()) {
+  sendRuntimeMessageToBackgroundScript({
+    type: RecorderUiMsgEnum.GET_RECIPE
+  }).then((recipe) => {
+    while (actionsListDiv.hasChildNodes()) {
       actionsListDiv.removeChild(actionsListDiv.lastChild);
     }
 
@@ -226,15 +221,15 @@ document.addEventListener('DOMContentLoaded', () => {
   // Get the saved event parameters from the background script and
   // set parameters on the UI.
   sendRuntimeMessageToBackgroundScript(
-      { type: RecorderUiMsgEnum.GET_SAVED_ACTION_PARAMS })
-  .then((params) => {
-    if (params.passwordManagerParams) {
-      passwordActionUserParam.value =
-          params.passwordManagerParams.userName;
-      passwordActionPasswordParam .value =
-          params.passwordManagerParams.password;
-    }
-  }).catch((error) => {
-    console.error('Unable to obtain saved action parameters!\r\n', error);
-  });
+      {type: RecorderUiMsgEnum.GET_SAVED_ACTION_PARAMS})
+      .then((params) => {
+        if (params.passwordManagerParams) {
+          passwordActionUserParam.value = params.passwordManagerParams.userName;
+          passwordActionPasswordParam.value =
+              params.passwordManagerParams.password;
+        }
+      })
+      .catch((error) => {
+        console.error('Unable to obtain saved action parameters!\r\n', error);
+      });
 });
