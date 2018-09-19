2d3073a17f4a2b51d3caf3c1cf0e3e62e315f0eb
Hajime Hoshi <hajimehoshi@chromium.org>
2018-09-19 06:32:58 +0000

    Use per-context task runner instead of per-thread task runner at AsyncAudioDecoder
    This is a part of efforts to forbid V8 execution on the default task
    runner of the main thread.
    
    Bug: 870606
    Change-Id: I558cbdeb55f3abdcd4f4fa95f4694c495512f91a
    Reviewed-on: https://chromium-review.googlesource.com/1229739
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592328}

--- third_party/blink/renderer/modules/webaudio/async_audio_decoder.cc
+++ third_party/blink/renderer/modules/webaudio/async_audio_decoder.cc
@@ -52,6 +52,10 @@ void AsyncAudioDecoder::DecodeAsync(
   if (!audio_data)
     return;
 
+  scoped_refptr<base::SingleThreadTaskRunner> task_runner =
+      context->GetExecutionContext()->GetTaskRunner(
+          blink::TaskType::kInternalMedia);
+
   BackgroundScheduler::PostOnBackgroundThread(
       FROM_HERE,
       CrossThreadBind(&AsyncAudioDecoder::DecodeOnBackgroundThread,
@@ -59,7 +63,8 @@ void AsyncAudioDecoder::DecodeAsync(
                       WrapCrossThreadPersistent(success_callback),
                       WrapCrossThreadPersistent(error_callback),
                       WrapCrossThreadPersistent(resolver),
-                      WrapCrossThreadPersistent(context)));
+                      WrapCrossThreadPersistent(context),
+                      std::move(task_runner)));
 }
 
 void AsyncAudioDecoder::DecodeOnBackgroundThread(
@@ -68,7 +73,8 @@ void AsyncAudioDecoder::DecodeOnBackgroundThread(
     V8PersistentCallbackFunction<V8DecodeSuccessCallback>* success_callback,
     V8PersistentCallbackFunction<V8DecodeErrorCallback>* error_callback,
     ScriptPromiseResolver* resolver,
-    BaseAudioContext* context) {
+    BaseAudioContext* context,
+    scoped_refptr<base::SingleThreadTaskRunner> task_runner) {
   DCHECK(!IsMainThread());
   scoped_refptr<AudioBus> bus = CreateBusFromInMemoryAudioFile(
       audio_data->Data(), audio_data->ByteLength(), false, sample_rate);
@@ -81,7 +87,7 @@ void AsyncAudioDecoder::DecodeOnBackgroundThread(
   // exist any more.
   if (context) {
     PostCrossThreadTask(
-        *Platform::Current()->MainThread()->GetTaskRunner(), FROM_HERE,
+        *task_runner, FROM_HERE,
         CrossThreadBind(&AsyncAudioDecoder::NotifyComplete,
                         WrapCrossThreadPersistent(audio_data),
                         WrapCrossThreadPersistent(success_callback),
--- third_party/blink/renderer/modules/webaudio/async_audio_decoder.h
+++ third_party/blink/renderer/modules/webaudio/async_audio_decoder.h
@@ -33,6 +33,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/modules/v8/v8_decode_error_callback.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_decode_success_callback.h"
 
+namespace base {
+class SingleThreadTaskRunner;
+}
+
 namespace blink {
 
 class AudioBuffer;
@@ -72,7 +76,8 @@ class AsyncAudioDecoder {
       V8PersistentCallbackFunction<V8DecodeSuccessCallback>*,
       V8PersistentCallbackFunction<V8DecodeErrorCallback>*,
       ScriptPromiseResolver*,
-      BaseAudioContext*);
+      BaseAudioContext*,
+      scoped_refptr<base::SingleThreadTaskRunner>);
   static void NotifyComplete(
       DOMArrayBuffer* audio_data,
       V8PersistentCallbackFunction<V8DecodeSuccessCallback>*,
