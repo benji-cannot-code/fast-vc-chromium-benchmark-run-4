4edd68d739404c339524e93a35b610fab9b477ba
Balazs Engedy <engedy@chromium.org>
2018-09-20 11:54:32 +0000

    Disable BLE/caBLE transports on platforms that do not support it.
    Bug: 885165
    Change-Id: I207457e481ef89ffcf0d91ee530cea2d3c33649a
    Reviewed-on: https://chromium-review.googlesource.com/1230675
    Commit-Queue: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592763}

--- content/browser/webauth/authenticator_impl.cc
+++ content/browser/webauth/authenticator_impl.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/service_manager_connection.h"
 #include "crypto/sha2.h"
 #include "device/base/features.h"
+#include "device/bluetooth/bluetooth_adapter_factory.h"
 #include "device/fido/attestation_statement.h"
 #include "device/fido/authenticator_selection_criteria.h"
 #include "device/fido/ctap_get_assertion_request.h"
@@ -421,6 +422,12 @@ base::flat_set<device::FidoTransportProtocol> GetTransportsEnabledByFlags() {
   base::flat_set<device::FidoTransportProtocol> transports;
   transports.insert(device::FidoTransportProtocol::kUsbHumanInterfaceDevice);
   transports.insert(device::FidoTransportProtocol::kInternal);
+
+  // TODO(crbug.com/885165): We should not directly access the BLE stack here.
+  // It is used by //device/fido, so its availability should be checked there.
+  if (!device::BluetoothAdapterFactory::Get().IsLowEnergySupported())
+    return transports;
+
   if (base::FeatureList::IsEnabled(features::kWebAuthBle)) {
     transports.insert(device::FidoTransportProtocol::kBluetoothLowEnergy);
   }
