8c1785b4d66b1cc8d865215d3488d5694dddb105
Gemene Narcis <gemene@google.com>
2018-09-20 13:42:25 +0000

    Support cleaning blacklisted duplicates for iOS platform
    This CL allows cleaning of blacklisted duplicates for iOS platform.
    
    Bug: 862930
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: I44e9fb90181781c261760ac0d933f8917b7d2910
    Reviewed-on: https://chromium-review.googlesource.com/1233234
    Reviewed-by: Vaclav Brozek <vabr@chromium.org>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Commit-Queue: Narcis Gemene <gemene@google.com>
    Cr-Commit-Position: refs/heads/master@{#592779}

--- ios/chrome/browser/passwords/ios_chrome_password_store_factory.cc
+++ ios/chrome/browser/passwords/ios_chrome_password_store_factory.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/android_affiliation/affiliation_service.h"
 #include "components/password_manager/core/browser/android_affiliation/affiliation_utils.h"
 #include "components/password_manager/core/browser/login_database.h"
+#include "components/password_manager/core/browser/password_manager_util.h"
 #include "components/password_manager/core/browser/password_store_default.h"
 #include "components/password_manager/core/browser/password_store_factory_util.h"
 #include "components/sync/driver/sync_service.h"
@@ -99,6 +100,9 @@ IOSChromePasswordStoreFactory::BuildServiceInstanceFor(
     LOG(WARNING) << "Could not initialize password store.";
     return nullptr;
   }
+  password_manager_util::DeleteBlacklistedDuplicates(
+      store, ios::ChromeBrowserState::FromBrowserState(context)->GetPrefs(),
+      60);
   return store;
 }
 
