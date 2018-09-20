12e89ebb292de53ed01e683814ef9ae1993661c7
Dominik Röttsches <drott@chromium.org>
2018-09-20 09:57:26 +0000

    Fallback to FreeType for COLR/CPAL on pre Windows 8.1 systems
    The availability of IDWriteFontFactory2 (which has the
    TranslateColorGlyphRun method on it) is equivalent to the ability of the
    underlying DirectWrite implementation to render COLR/CPAL fonts. We are
    using this detection to find out whether we are running on a system that
    can render COLR/CPAL natively, otherwise pass the web font to a
    COLR/CPAL SkFontMgr based on FreeType.
    
    Tests updated to reliably test rendering results for COLR/CPAL including
    our Windows 7 bots.
    
    Mac detection to do the same fallback for OS versions below Mac OS 10.13
    will follow in a subsequent CL.
    
    Bug: 882844
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng;luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I5f5aa16cbb176d22d229dee05619419cbb8bb892
    Reviewed-on: https://chromium-review.googlesource.com/1219887
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592743}

--- third_party/blink/renderer/platform/fonts/web_font_typeface_factory.h
+++ third_party/blink/renderer/platform/fonts/web_font_typeface_factory.h
@@ -28,6 +28,7 @@ class WebFontTypefaceFactory {
   static sk_sp<SkFontMgr> FontManagerForVariations();
   static sk_sp<SkFontMgr> FontManagerForSbix();
   static sk_sp<SkFontMgr> FreeTypeFontManager();
+  static sk_sp<SkFontMgr> FontManagerForColrCpal();
 
  private:
   // These values are written to logs.  New enum values can be added, but
@@ -39,7 +40,8 @@ class WebFontTypefaceFactory {
     kSuccessCbdtCblcColorFont = 3,
     kSuccessCff2Font = 4,
     kSuccessSbixFont = 5,
-    kMaxWebFontInstantiationResult = 6
+    kSuccessColrCpalFont = 6,
+    kMaxWebFontInstantiationResult = 7
   };
 
   static sk_sp<SkFontMgr> DefaultFontManager();
