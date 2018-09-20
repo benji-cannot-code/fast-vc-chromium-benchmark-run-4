8054227e7b4d3a456079ae3fe7204d1b0aaa4ac0
Joel Einbinder <einbinder@chromium.org>
2018-09-20 00:47:58 +0000

    DevTools: Don't try to forward keypresses out of extension panels
    Keypress events have a different keycode, so the wrong events were
    being captured and sent to DevTools from extension panels. But keypress
    is deprecated and we don't need these events anywhere in DevTools. So
    we can just remove the event listener.
    
    Bug: 882422
    Change-Id: I9d286f581aa77ff208463a2edbb4eea093fa98ee
    Reviewed-on: https://chromium-review.googlesource.com/1232853
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Joel Einbinder <einbinder@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592637}

--- third_party/blink/renderer/devtools/front_end/extensions/ExtensionAPI.js
+++ third_party/blink/renderer/devtools/front_end/extensions/ExtensionAPI.js
@@ -679,7 +679,6 @@ function injectedExtensionAPI(extensionInfo, inspectedTabId, themeName, keysToFo
   }
 
   document.addEventListener('keydown', forwardKeyboardEvent, false);
-  document.addEventListener('keypress', forwardKeyboardEvent, false);
 
   /**
    * @constructor
