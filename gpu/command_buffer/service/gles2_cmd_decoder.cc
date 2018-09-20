a718e15d2026272b0e5147d49eb95076583d3b35
Zhenyao Mo <zmo@chromium.org>
2018-09-20 20:15:04 +0000

    Poke GPU watchdog a few more times during context destruction.
    We still see a bit of hang at buffer_manager() destruction, which is the
    beginning of ContextGroup destruction. So we add a few pokes in context
    destruction before the ContextGroup destruction.
    
    BUG=749046
    TEST=crash reports go down
    R=piman@chromium.org
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I4726e63f2cf4fa5c1471a25ef65f8d8ac9411c09
    Reviewed-on: https://chromium-review.googlesource.com/1237114
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592923}

--- gpu/command_buffer/service/gles2_cmd_decoder.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder.cc
@@ -2373,6 +2373,11 @@ class GLES2DecoderImpl : public GLES2Decoder, public ErrorStateClient {
   // Compiles the given shader and exits command processing early.
   void CompileShaderAndExitCommandProcessingEarly(Shader* shader);
 
+  // Notify the watchdog thread of progress, preventing time-outs when a
+  // command takes a long time. May be no-op when using in-process command
+  // buffer.
+  void ReportProgress();
+
   // Generate a member function prototype for each command in an automated and
   // typesafe way.
 #define GLES2_CMD_OP(name) \
@@ -5128,6 +5133,8 @@ void GLES2DecoderImpl::Destroy(bool have_context) {
   }
   deschedule_until_finished_fences_.clear();
 
+  ReportProgress();
+
   // Unbind everything.
   state_.vertex_attrib_manager = nullptr;
   state_.default_vertex_attrib_manager = nullptr;
@@ -5161,6 +5168,8 @@ void GLES2DecoderImpl::Destroy(bool have_context) {
   srgb_converter_.reset();
   clear_framebuffer_blit_.reset();
 
+  ReportProgress();
+
   if (framebuffer_manager_.get()) {
     framebuffer_manager_->Destroy(have_context);
     if (group_->texture_manager())
@@ -5192,6 +5201,8 @@ void GLES2DecoderImpl::Destroy(bool have_context) {
     transform_feedback_manager_.reset();
   }
 
+  ReportProgress();
+
   offscreen_target_frame_buffer_.reset();
   offscreen_target_color_texture_.reset();
   offscreen_target_color_render_buffer_.reset();
@@ -5209,6 +5220,8 @@ void GLES2DecoderImpl::Destroy(bool have_context) {
   // Need to release these before releasing |group_| which may own the
   // ShaderTranslatorCache.
   DestroyShaderTranslator();
+
+  ReportProgress();
 
   // Destroy the GPU Tracer which may own some in process GPU Timings.
   if (gpu_tracer_) {
@@ -20025,6 +20038,11 @@ void GLES2DecoderImpl::CompileShaderAndExitCommandProcessingEarly(
   // command processing to allow for context preemption and GPU watchdog
   // checks.
   ExitCommandProcessingEarly();
+}
+
+void GLES2DecoderImpl::ReportProgress() {
+  if (group_)
+    group_->ReportProgress();
 }
 
 // Include the auto-generated part of this file. We split this because it means
