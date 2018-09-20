0786ec1c31c94740eccd1e11ea8b2f353fbe9b59
Manu Cornet <manucornet@chromium.org>
2018-09-20 22:41:00 +0000

    CrOS shelf: conditionally blur the shelf background
    Delegate the decision to blur the shelf to the shelf layout manager,
    and do not blur it unless there is an active session (e.g. login
    screen).
    
    Cache whether the background is currently blurred to avoid unnecessary
    updates.
    
    Bug: 883558
    Change-Id: I6bc881f8133a0363260f88efd4225fb072009fc0
    Reviewed-on: https://chromium-review.googlesource.com/1227813
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Manu Cornet <manucornet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592978}

--- ash/shelf/shelf_widget.cc
+++ ash/shelf/shelf_widget.cc
@@ -94,6 +94,7 @@ class ShelfWidget::DelegateView : public views::WidgetDelegate,
 
   bool CanActivate() const override;
   void ReorderChildLayers(ui::Layer* parent_layer) override;
+  void UpdateBackgroundBlur();
   void UpdateOpaqueBackground();
   // This will be called when the parent local bounds change.
   void OnBoundsChanged(const gfx::Rect& old_bounds) override;
@@ -117,6 +118,10 @@ class ShelfWidget::DelegateView : public views::WidgetDelegate,
   // When true, the default focus of the shelf is the last focusable child.
   bool default_last_focusable_child_ = false;
 
+  // Cache the state of the background blur so that it can be updated only
+  // when necessary.
+  bool background_is_currently_blurred_ = false;
+
   DISALLOW_COPY_AND_ASSIGN(DelegateView);
 };
 
@@ -130,8 +135,6 @@ ShelfWidget::DelegateView::DelegateView(ShelfWidget* shelf_widget)
   SetLayoutManager(std::make_unique<views::FillLayout>());
   set_allow_deactivate_on_esc(true);
 
-  if (shelf_widget_->shelf_layout_manager()->IsBackgroundBlurEnabled())
-    opaque_background_.SetBackgroundBlur(kShelfBlurRadius);
   UpdateOpaqueBackground();
 }
 
@@ -186,6 +189,18 @@ void ShelfWidget::DelegateView::ReorderChildLayers(ui::Layer* parent_layer) {
   parent_layer->StackAtBottom(&opaque_background_);
 }
 
+void ShelfWidget::DelegateView::UpdateBackgroundBlur() {
+  const bool should_blur_background =
+      shelf_widget_->shelf_layout_manager()->ShouldBlurShelfBackground();
+  if (should_blur_background == background_is_currently_blurred_)
+    return;
+
+  opaque_background_.SetBackgroundBlur(should_blur_background ? kShelfBlurRadius
+                                                              : 0);
+
+  background_is_currently_blurred_ = should_blur_background;
+}
+
 void ShelfWidget::DelegateView::UpdateOpaqueBackground() {
   const gfx::Rect local_bounds = GetLocalBounds();
   gfx::Rect opaque_background_bounds = local_bounds;
@@ -223,6 +238,7 @@ void ShelfWidget::DelegateView::UpdateOpaqueBackground() {
     }
   }
   opaque_background_.SetBounds(opaque_background_bounds);
+  UpdateBackgroundBlur();
   SchedulePaint();
 }
 
