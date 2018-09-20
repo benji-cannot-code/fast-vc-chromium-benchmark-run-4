39ab104421528d558adb0ef2b8525a4cef05912f
Mark Cogan <marq@google.com>
2018-09-20 17:34:00 +0000

    [iOS] Remove dependencies on stack view
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: I785464f847ff6344201a5ac8f7aa2a6a1aa2f3f5
    Reviewed-on: https://chromium-review.googlesource.com/1233701
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592854}

--- ios/chrome/browser/ui/stack_view/card_set.mm
+++ ios/chrome/browser/ui/stack_view/card_set.mm
@@ -15,9 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/tabs/tab_model.h"
 #include "ios/chrome/browser/ui/rtl_geometry.h"
 #import "ios/chrome/browser/ui/stack_view/card_stack_layout_manager.h"
-#import "ios/chrome/browser/ui/stack_view/page_animation_util.h"
 #import "ios/chrome/browser/ui/stack_view/stack_card.h"
 #include "ios/chrome/browser/ui/ui_util.h"
+#import "ios/chrome/browser/ui/util/page_animation_util.h"
 #import "ios/chrome/browser/web_state_list/web_state_list.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
