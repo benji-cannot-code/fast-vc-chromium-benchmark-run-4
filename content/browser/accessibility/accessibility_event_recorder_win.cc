d594ec0a42d4089b06c39cc9bf0cd1d81e3bad74
braveyao <braveyao@chromium.org>
2018-09-20 15:50:35 +0000

    Restore Create() method instead of singleton of AcceessibillityRecorder
    In a recent cl, https://chromium-review.googlesource.com/c/chromium/src/+/1171562,
    it starts to use singleton to create AcceessibillityRecorder instance.
    This causes a problem in crbug.com/883136, that because the dtor will
    never be called, windowns event will still be caught and processed during
    destuction period, which may randomly crash due to race condition.
    
    This cl is to partially revert the changes to stop listening correctly
    at destruction.
    
    Bug: 883136
    Change-Id: I1bd8d3e00b23590c222652c15632e33b4e3667e9
    Reviewed-on: https://chromium-review.googlesource.com/1234853
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592815}

--- content/browser/accessibility/accessibility_event_recorder_win.cc
+++ content/browser/accessibility/accessibility_event_recorder_win.cc
@@ -76,6 +76,10 @@ std::string AccessibilityEventToStringUTF8(int32_t event_id) {
 
 class AccessibilityEventRecorderWin : public AccessibilityEventRecorder {
  public:
+  AccessibilityEventRecorderWin(
+      BrowserAccessibilityManager* manager,
+      base::ProcessId pid,
+      const base::StringPiece& application_name_match_pattern);
   ~AccessibilityEventRecorderWin() override;
 
   // Callback registered by SetWinEventHook. Just calls OnWinEventHook.
@@ -88,11 +92,6 @@ class AccessibilityEventRecorderWin : public AccessibilityEventRecorder {
                                          DWORD event_time);
 
  private:
-  AccessibilityEventRecorderWin(
-      BrowserAccessibilityManager* manager,
-      base::ProcessId pid,
-      const base::StringPiece& application_name_match_pattern);
-
   // Called by the thunk registered by SetWinEventHook. Retrieves accessibility
   // info about the node the event was fired on and appends a string to
   // the event log.
@@ -110,25 +109,27 @@ class AccessibilityEventRecorderWin : public AccessibilityEventRecorder {
       HWND hwnd, DWORD dwId, REFIID riid, void **ppvObject);
 
   HWINEVENTHOOK win_event_hook_handle_;
+  static AccessibilityEventRecorderWin* instance_;
 
-  friend class base::NoDestructor<AccessibilityEventRecorderWin>;
   DISALLOW_COPY_AND_ASSIGN(AccessibilityEventRecorderWin);
 };
 
 // static
-AccessibilityEventRecorder& AccessibilityEventRecorder::GetInstance(
+AccessibilityEventRecorderWin* AccessibilityEventRecorderWin::instance_ =
+    nullptr;
+
+// static
+std::unique_ptr<AccessibilityEventRecorder> AccessibilityEventRecorder::Create(
     BrowserAccessibilityManager* manager,
     base::ProcessId pid,
     const base::StringPiece& application_name_match_pattern) {
   if (!application_name_match_pattern.empty()) {
-    LOG(ERROR) << "Recording accessibility events from an application name "
+    LOG(FATAL) << "Recording accessibility events from an application name "
                   "match pattern not supported on this platform yet.";
-    NOTREACHED();
   }
 
-  static base::NoDestructor<AccessibilityEventRecorderWin> instance(
+  return std::make_unique<AccessibilityEventRecorderWin>(
       manager, pid, application_name_match_pattern);
-  return *instance;
 }
 
 // static
@@ -140,9 +141,10 @@ CALLBACK void AccessibilityEventRecorderWin::WinEventHookThunk(
     LONG child_id,
     DWORD event_thread,
     DWORD event_time) {
-  static_cast<AccessibilityEventRecorderWin&>(GetInstance())
-      .OnWinEventHook(handle, event, hwnd, obj_id, child_id, event_thread,
-                      event_time);
+  if (instance_) {
+    instance_->OnWinEventHook(handle, event, hwnd, obj_id, child_id,
+                              event_thread, event_time);
+  }
 }
 
 AccessibilityEventRecorderWin::AccessibilityEventRecorderWin(
@@ -150,6 +152,8 @@ AccessibilityEventRecorderWin::AccessibilityEventRecorderWin(
     base::ProcessId pid,
     const base::StringPiece& application_name_match_pattern)
     : AccessibilityEventRecorder(manager) {
+  CHECK(!instance_) << "There can be only one instance of"
+                    << " AccessibilityEventRecorder at a time.";
   // For now, just use out of context events when running as a utility to watch
   // events (no BrowserAccessibilityManager), because otherwise Chrome events
   // are not getting reported. Being in context is better so that for
@@ -162,10 +166,12 @@ AccessibilityEventRecorderWin::AccessibilityEventRecorderWin(
                       0,  // Hook all threads
                       context);
   CHECK(win_event_hook_handle_);
+  instance_ = this;
 }
 
 AccessibilityEventRecorderWin::~AccessibilityEventRecorderWin() {
   UnhookWinEvent(win_event_hook_handle_);
+  instance_ = nullptr;
 }
 
 void AccessibilityEventRecorderWin::OnWinEventHook(
