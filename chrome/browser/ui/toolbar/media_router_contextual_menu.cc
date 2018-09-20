902f5fdbfc36df22802acfeb281a01347dd35a7f
Devlin Cronin <rdevlin.cronin@chromium.org>
2018-09-20 06:14:36 +0000

    [UI] Resolve method conflict between MenuModel, SimpleMenuModel::Delegate
    ui::MenuModel (which ui::SimpleMenuModel inherits from) declares a
    virtual method, `void MenuWillShow()`. SimpleMenuModel::Delegate
    declares a similar method, `void MenuWillShow(SimpleMenuModel*)`. This
    results in a conflict for any class that inherits from both
    SimpleMenuModel and SimpleMenuModel::Delegate (which is not uncommon).
    
    Resolve this by renaming the SimpleMenuModel::Delegate version to
    `void SimpleMenuWillShow(SimpleMenuModel*)`.
    
    Bug: 885198
    Change-Id: I0581991fa7a12368007f6c082c461acb4833750c
    Reviewed-on: https://chromium-review.googlesource.com/1230581
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592694}

--- chrome/browser/ui/toolbar/media_router_contextual_menu.cc
+++ chrome/browser/ui/toolbar/media_router_contextual_menu.cc
@@ -205,7 +205,7 @@ void MediaRouterContextualMenu::ExecuteCommand(int command_id,
   }
 }
 
-void MediaRouterContextualMenu::MenuWillShow(ui::SimpleMenuModel* source) {
+void MediaRouterContextualMenu::OnMenuWillShow(ui::SimpleMenuModel* source) {
   observer_->OnContextMenuShown();
 }
 
--- ui/base/cocoa/menu_controller_unittest.mm
+++ ui/base/cocoa/menu_controller_unittest.mm
@@ -136,7 +136,7 @@ class Delegate : public SimpleMenuModel::Delegate {
     ++execute_count_;
   }
 
-  void MenuWillShow(SimpleMenuModel* /*source*/) override {
+  void OnMenuWillShow(SimpleMenuModel* /*source*/) override {
     EXPECT_FALSE(did_show_);
     EXPECT_FALSE(did_close_);
     did_show_ = true;
@@ -160,7 +160,7 @@ class Delegate : public SimpleMenuModel::Delegate {
   int execute_count_ = 0;
   mutable int enable_count_ = 0;
   // The menu on which to call |-cancelTracking| after a short delay in
-  // MenuWillShow.
+  // OnMenuWillShow.
   NSMenu* menu_to_close_ = nil;
   bool did_show_ = false;
   bool did_close_ = false;
@@ -605,7 +605,7 @@ TEST_F(MenuControllerTest, OpenClose) {
   EXPECT_FALSE([menu isMenuOpen]);
 
   // When control returns back to here, the menu will have finished running its
-  // loop and will have closed itself (see Delegate::MenuWillShow).
+  // loop and will have closed itself (see Delegate::OnMenuWillShow).
   EXPECT_TRUE(delegate.did_show_);
 
   // When the menu tells the Model it closed, the Model posts a task to notify
