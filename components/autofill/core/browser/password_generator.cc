1f1279ce92465a0d481491154b980ecab72884f4
Vaclav Brozek <vabr@chromium.org>
2018-09-20 11:14:58 +0000

    Amend comment for autofill::kDefaultPasswordLength
    https://crrev.com/c/1230064 added a comment pointing out the
    dependency between autofill::kDefaultPasswordLength and the autofill
    server code. However, the detail was mostly given in the CL commit
    message instead of the comment itself.
    
    This CL attempts to bring more of the information into the comment to
    spare going through "git blame" in the future.
    
    Bug: 846694
    Change-Id: I0db135e31da59a5c6bacba7c2de602a7581166b5
    Reviewed-on: https://chromium-review.googlesource.com/1235574
    Reviewed-by: Christos Froussios <cfroussios@chromium.org>
    Commit-Queue: Vaclav Brozek <vabr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592754}

--- components/autofill/core/browser/password_generator.cc
+++ components/autofill/core/browser/password_generator.cc
@@ -17,8 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill {
 
-// The default length for a generated password. If you intend to change this
-// constant, make sure to update the corresponding constant on the server.
+// The default length for a generated password. Keep this the same as the
+// default length known to the classification pipeline on the autofill
+// crowd-sourcing server. (The server predicts password lengths only if the
+// prediction is smaller than the default.)
 const uint32_t kDefaultPasswordLength = 15;
 
 namespace {
