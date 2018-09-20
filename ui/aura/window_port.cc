a4a76a5fdb325b24a47ec2f2fb3231b9e7ee50b7
Scott Violet <sky@chromium.org>
2018-09-20 00:13:39 +0000

    aura: change WindowMus::Get() to return null
    If it's not a WindowMus. This is an issue now that single-process-mash allows
    both WindowPortLocal and WindowPortMus at the same time.
    
    And updates WindowTreeClient::IsWindowKnown() not to assume a WindowMus.
    
    BUG=none
    TEST=none
    
    Change-Id: Ia4d1834b2a910707bae077dd14e4a9e46481fce0
    Reviewed-on: https://chromium-review.googlesource.com/1235115
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592621}

--- ui/aura/window_port.cc
+++ ui/aura/window_port.cc
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace aura {
 
+WindowPort::WindowPort(Type type) : type_(type) {}
+
 // static
 WindowPort* WindowPort::Get(Window* window) {
   return window ? window->port_ : nullptr;
