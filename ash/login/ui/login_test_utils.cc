2dac1b8886a9278759cb70d73fc1621ed59b14a7
Wenzhao Zang <wzang@chromium.org>
2018-09-20 23:01:40 +0000

    cros: Fix accessibility issue of the disabled auth message
    Works for both tabbing and reverse tabbing. Chromevox announces
    "Time's up" and "Come back at xxx" separately.
    
    Bug: 870459
    Change-Id: Icdf76ba4a24ba3a2de48dfea7e2698ea7df4c114
    Reviewed-on: https://chromium-review.googlesource.com/1228482
    Commit-Queue: Wenzhao (Colin) Zang <wzang@chromium.org>
    Reviewed-by: Jacob Dufault <jdufault@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592987}

--- ash/login/ui/login_test_utils.cc
+++ ash/login/ui/login_test_utils.cc
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/login/ui/login_test_utils.h"
 #include "ash/login/ui/login_big_user_view.h"
 #include "base/strings/string_split.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "ui/events/test/event_generator.h"
 
 namespace ash {
 namespace {
@@ -83,6 +85,24 @@ bool HasFocusInAnyChildView(views::View* view) {
     if (HasFocusInAnyChildView(view->child_at(i)))
       return true;
   }
+  return false;
+}
+
+bool TabThroughView(ui::test::EventGenerator* event_generator,
+                    views::View* view,
+                    bool reverse) {
+  if (!HasFocusInAnyChildView(view)) {
+    ADD_FAILURE() << "View not focused initially.";
+    return false;
+  }
+
+  for (int i = 0; i < 50; ++i) {
+    event_generator->PressKey(ui::KeyboardCode::VKEY_TAB,
+                              reverse ? ui::EF_SHIFT_DOWN : 0);
+    if (!HasFocusInAnyChildView(view))
+      return true;
+  }
+
   return false;
 }
 
--- ash/login/ui/login_test_utils.h
+++ ash/login/ui/login_test_utils.h
@@ -11,6 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/login/ui/login_password_view.h"
 #include "ash/public/interfaces/login_user_info.mojom.h"
 
+namespace ui {
+namespace test {
+class EventGenerator;
+}
+}  // namespace ui
+
 namespace ash {
 
 enum class AuthTarget { kPrimary, kSecondary };
@@ -35,6 +41,13 @@ mojom::LoginUserInfoPtr CreatePublicAccountUser(const std::string& email);
 
 // Returns true if |view| or any child of it has focus.
 bool HasFocusInAnyChildView(views::View* view);
+
+// Keeps tabbing through |view| until the view loses focus.
+// The number of generated tab events will be limited - if the focus is still
+// within the view by the time the limit is hit, this will return false.
+bool TabThroughView(ui::test::EventGenerator* event_generator,
+                    views::View* view,
+                    bool reverse);
 
 }  // namespace ash
 
