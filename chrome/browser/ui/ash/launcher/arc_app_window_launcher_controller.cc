89427099db6591a83ae6472a21a5c7802a5c5afb
James Cook <jamescook@chromium.org>
2018-09-20 16:32:41 +0000

    Fix extra shelf icon for pinned ARC++ apps under SingleProcessMash
    ArcAppWindowLauncherController was observing the wrong aura::Env for
    window creation, so it wasn't calling AttachControllerToWindowIfNeeded
    to set the correct ShelfID on the window.
    
    Bug: 887129
    Test: pin Play Store icon, then tap on it
    Change-Id: I2239a032962b783a50e562b822f021ef6e0a7511
    Reviewed-on: https://chromium-review.googlesource.com/1235152
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592829}

--- chrome/browser/ui/ash/launcher/arc_app_window_launcher_controller.cc
+++ chrome/browser/ui/ash/launcher/arc_app_window_launcher_controller.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/app_types.h"
 #include "ash/public/cpp/shelf_model.h"
 #include "ash/public/cpp/window_properties.h"
+#include "ash/shell.h"
 #include "base/bind.h"
 #include "chrome/browser/chromeos/arc/arc_optin_uma.h"
 #include "chrome/browser/chromeos/arc/arc_util.h"
@@ -28,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/client/aura_constants.h"
 #include "ui/aura/env.h"
 #include "ui/base/base_window.h"
+#include "ui/base/ui_base_features.h"
 #include "ui/views/widget/widget.h"
 
 namespace {
@@ -441,8 +443,10 @@ void ArcAppWindowLauncherController::OnWindowActivated(
 }
 
 void ArcAppWindowLauncherController::StartObserving(Profile* profile) {
-  if (aura::Env::HasInstance())
-    aura::Env::GetInstance()->AddObserver(this);
+  // TODO(mash): Find another way to observe for ARC++ window creation.
+  // https://crbug.com/887156
+  if (!features::IsMultiProcessMash())
+    ash::Shell::Get()->aura_env()->AddObserver(this);
   ArcAppListPrefs* prefs = ArcAppListPrefs::Get(profile);
   DCHECK(prefs);
   prefs->AddObserver(this);
@@ -453,8 +457,8 @@ void ArcAppWindowLauncherController::StopObserving(Profile* profile) {
     window->RemoveObserver(this);
   ArcAppListPrefs* prefs = ArcAppListPrefs::Get(profile);
   prefs->RemoveObserver(this);
-  if (aura::Env::HasInstance())
-    aura::Env::GetInstance()->RemoveObserver(this);
+  if (!features::IsMultiProcessMash())
+    ash::Shell::Get()->aura_env()->RemoveObserver(this);
 }
 
 ArcAppWindowLauncherItemController*
