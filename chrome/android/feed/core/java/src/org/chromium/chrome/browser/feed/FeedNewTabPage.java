11b1736ceb28174bae0572456024a37cd6fc20f9
Becky Zhou <huayinz@chromium.org>
2018-09-20 18:01:44 +0000

    [Feed] Fix memory leak on enterprise policy changed on FeedNewTabPage
    We need to detach the MarginResizer so that it doesn't hold onto any
    Stream related object such as HeaderViewHolder.
    
    Bug: 884906
    Change-Id: Icf2dd3e367735185f5d08378651e6a16667729ac
    Reviewed-on: https://chromium-review.googlesource.com/1235016
    Commit-Queue: Becky Zhou <huayinz@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592871}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedNewTabPage.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedNewTabPage.java
@@ -73,7 +73,9 @@ public class FeedNewTabPage extends NewTabPage {
     private @Nullable FeedImageLoader mImageLoader;
     private @Nullable StreamLifecycleManager mStreamLifecycleManager;
     private @Nullable SectionHeaderView mSectionHeaderView;
+    private @Nullable MarginResizer mSectionHeaderViewMarginResizer;
     private @Nullable PersonalizedSigninPromoView mSigninPromoView;
+    private @Nullable MarginResizer mSignInPromoViewMarginResizer;
 
     // Used when Feed is disabled by policy.
     private @Nullable ScrollView mScrollViewForPolicy;
@@ -323,7 +325,8 @@ public class FeedNewTabPage extends NewTabPage {
         LayoutInflater inflater = LayoutInflater.from(activity);
         mSectionHeaderView = (SectionHeaderView) inflater.inflate(
                 R.layout.new_tab_page_snippets_expandable_header, null);
-        MarginResizer.createAndAttach(mSectionHeaderView, mUiConfig, mDefaultMargin, mWideMargin);
+        mSectionHeaderViewMarginResizer = MarginResizer.createAndAttach(
+                mSectionHeaderView, mUiConfig, mDefaultMargin, mWideMargin);
 
         View view = mStream.getView();
         view.setBackgroundColor(Color.WHITE);
@@ -360,7 +363,11 @@ public class FeedNewTabPage extends NewTabPage {
             mImageLoader.destroy();
             mImageLoader = null;
             mSectionHeaderView = null;
+            mSectionHeaderViewMarginResizer.detach();
+            mSectionHeaderViewMarginResizer = null;
             mSigninPromoView = null;
+            if (mSignInPromoViewMarginResizer != null) mSignInPromoViewMarginResizer.detach();
+            mSignInPromoViewMarginResizer = null;
         }
 
         mScrollViewForPolicy = new ScrollView(mTab.getActivity());
@@ -395,7 +402,8 @@ public class FeedNewTabPage extends NewTabPage {
                     ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
             lp.bottomMargin = mDefaultMargin;
             mSigninPromoView.setLayoutParams(lp);
-            MarginResizer.createAndAttach(mSigninPromoView, mUiConfig, mDefaultMargin, mWideMargin);
+            mSignInPromoViewMarginResizer = MarginResizer.createAndAttach(
+                    mSigninPromoView, mUiConfig, mDefaultMargin, mWideMargin);
         }
         return mSigninPromoView;
     }
