3988fd2d429aa086fb8d93f95671a9d19e2d7757
Chinglin Yu <chinglinyu@chromium.org>
2018-09-20 06:38:53 +0000

    Estimate freed memory in killing a process faster.
    Use memory_instrumentation::OSMetrics::FillOSMemoryDump(), which
    reads /proc/<pid>/statm, to get private bytes of a process, to avoid
    contention with kswapd under heavy memory pressure.
    
    BUG=chromium:872253
    TEST=manual
    R=cylee@chromium.org, sonnyrao@chromium.org, fdoray@chromium.org
    
    Change-Id: I4d43933a39c3c89d8ebb81e3ccef20277cedb258
    Reviewed-on: https://chromium-review.googlesource.com/1212246
    Commit-Queue: Chinglin Yu <chinglinyu@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Reviewed-by: Cheng-Yu Lee <cylee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592701}

--- chrome/browser/resource_coordinator/tab_manager_delegate_chromeos.cc
+++ chrome/browser/resource_coordinator/tab_manager_delegate_chromeos.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/memory/memory_kills_monitor.h"
 #include "chrome/browser/resource_coordinator/tab_lifecycle_unit_external.h"
 #include "chrome/browser/resource_coordinator/tab_manager_stats_collector.h"
+#include "chrome/browser/resource_coordinator/utils.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_window.h"
@@ -249,12 +250,8 @@ int TabManagerDelegate::MemoryStat::TargetMemoryToFreeKB() {
 }
 
 int TabManagerDelegate::MemoryStat::EstimatedMemoryFreedKB(
-    base::ProcessHandle pid) {
-  std::unique_ptr<base::ProcessMetrics> process_metrics(
-      base::ProcessMetrics::CreateProcessMetrics(pid));
-  base::ProcessMetrics::TotalsSummary summary =
-      process_metrics->GetTotalsSummary();
-  return summary.private_clean_kb + summary.private_dirty_kb + summary.swap_kb;
+    base::ProcessHandle handle) {
+  return GetPrivateMemoryKB(handle);
 }
 
 TabManagerDelegate::TabManagerDelegate(
