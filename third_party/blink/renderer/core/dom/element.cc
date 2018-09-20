899222c491fc01e41643fc682db13b1d29063df7
Navid Zolghadr <nzolghadr@chromium.org>
2018-09-20 19:36:07 +0000

    Use NotFoundError instead of InvalidPointerId
    This cl follows the new pointerevent spec which
    uses more common WebIDL specified exception
    NotFoundError instead of defining new
    InvalidPointerId exception.
    
    Bug: 885321
    Change-Id: I28e946ffcb96c9fb4f70c76a4b9c09be620f981d
    Reviewed-on: https://chromium-review.googlesource.com/1231990
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Ella Ge <eirage@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Navid Zolghadr <nzolghadr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592903}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -3782,8 +3782,9 @@ void Element::setPointerCapture(int pointer_id,
   if (GetDocument().GetFrame()) {
     if (!GetDocument().GetFrame()->GetEventHandler().IsPointerEventActive(
             pointer_id)) {
-      exception_state.ThrowDOMException(DOMExceptionCode::kInvalidPointerId,
-                                        "InvalidPointerId");
+      exception_state.ThrowDOMException(
+          DOMExceptionCode::kNotFoundError,
+          "No active pointer with the given id is found.");
     } else if (!isConnected() ||
                (GetDocument().GetPage() && GetDocument()
                                                .GetPage()
@@ -3803,8 +3804,9 @@ void Element::releasePointerCapture(int pointer_id,
   if (GetDocument().GetFrame()) {
     if (!GetDocument().GetFrame()->GetEventHandler().IsPointerEventActive(
             pointer_id)) {
-      exception_state.ThrowDOMException(DOMExceptionCode::kInvalidPointerId,
-                                        "InvalidPointerId");
+      exception_state.ThrowDOMException(
+          DOMExceptionCode::kNotFoundError,
+          "No active pointer with the given id is found.");
     } else {
       GetDocument().GetFrame()->GetEventHandler().ReleasePointerCapture(
           pointer_id, this);
