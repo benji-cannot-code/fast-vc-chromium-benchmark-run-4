3b9d74e648c2b0cd0955f664de18ae23a58682ca
Vladimir Levin <vmpstr@chromium.org>
2018-09-20 18:37:39 +0000

    Explicitly check for absolute position containment when setting a dirty bit.
    This patch does two things:
    1. Explicitly checks for absolute position object containment where before
       it was checking for the equivalence of the position attribute. My
       suspicion is that this is the intent of the check.
    2. Makes the paint layer creation or deletion only set the paint property
       update instead of a subtree update, since it seems that the initial
       intent was that the local border box properties existence may change.
    
    R=pdr@chromium.org, wangxianzhu@chromium.org
    
    Change-Id: I9d93020a6f68213b1e4b49731c7b0e0d80c65681
    Reviewed-on: https://chromium-review.googlesource.com/1235128
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592886}

--- third_party/blink/renderer/core/layout/layout_box_model_object.cc
+++ third_party/blink/renderer/core/layout/layout_box_model_object.cc
@@ -310,14 +310,17 @@ void LayoutBoxModelObject::StyleDidChange(StyleDifference diff,
   if (old_style &&
       (old_style->CanContainFixedPositionObjects(IsDocumentElement()) !=
            StyleRef().CanContainFixedPositionObjects(IsDocumentElement()) ||
-       old_style->GetPosition() != StyleRef().GetPosition() ||
-       had_layer != HasLayer())) {
-    // This may affect paint properties of the current object, and descendants
-    // even if paint properties of the current object won't change. E.g. the
-    // stacking context and/or containing block of descendants may change.
+       old_style->CanContainAbsolutePositionObjects() !=
+           StyleRef().CanContainAbsolutePositionObjects())) {
+    // If out of flow element containment changed, then we need to force a
+    // subtree paint property update, since the children elements may now be
+    // referencing a different container.
     SetSubtreeNeedsForcedPaintPropertyUpdate();
-  } else if (had_transform_related_property != HasTransformRelatedProperty()) {
-    // This affects whether to create transform node.
+  } else if (had_layer == HasLayer() &&
+             had_transform_related_property != HasTransformRelatedProperty()) {
+    // This affects whether to create transform node. Note that if the
+    // HasLayer() value changed, then all of this was already set in
+    // CreateLayerAfterStyleChange() or DestroyLayer().
     SetNeedsPaintPropertyUpdate();
     if (Layer())
       Layer()->SetNeedsCompositingInputsUpdate();
@@ -474,6 +477,8 @@ void LayoutBoxModelObject::CreateLayerAfterStyleChange() {
       std::make_unique<PaintLayer>(*this));
   SetHasLayer(true);
   Layer()->InsertOnlyThisLayerAfterStyleChange();
+  // Creating a layer may affect existence of the LocalBorderBoxProperties, so
+  // we need to ensure that we update paint properties.
   SetNeedsPaintPropertyUpdate();
 }
 
@@ -481,6 +486,8 @@ void LayoutBoxModelObject::DestroyLayer() {
   DCHECK(HasLayer() && Layer());
   SetHasLayer(false);
   GetMutableForPainting().FirstFragment().SetLayer(nullptr);
+  // Removing a layer may affect existence of the LocalBorderBoxProperties, so
+  // we need to ensure that we update paint properties.
   SetNeedsPaintPropertyUpdate();
 }
 
