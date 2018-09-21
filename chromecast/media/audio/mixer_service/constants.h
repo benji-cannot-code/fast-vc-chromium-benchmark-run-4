cfd9dfc5944579fa24d640a4508c802ef662709e
Guohui Deng <guohuideng@chromium.org>
2018-09-21 16:53:14 +0000

    [Chromecast]Add MixerServiceConnection
    MixerServiceConnection is a component that connects
    to MixerService via Socket.
    
    Bug: internal b/29571387
    Test: build and run on an audio device and verify the
    assistant works fine.
    
    
    Change-Id: Iae323943909167fe247666ffd2c319efcf34c3ac
    Reviewed-on: https://chromium-review.googlesource.com/1231813
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Reviewed-by: Sergey Volk <servolk@chromium.org>
    Reviewed-by: Misha Efimov <mef@chromium.org>
    Reviewed-by: Kenneth MacKay <kmackay@chromium.org>
    Commit-Queue: Guohui Deng <guohuideng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593229}

---
+++ chromecast/media/audio/mixer_service/constants.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMECAST_MEDIA_AUDIO_MIXER_SERVICE_CONSTANTS_H_
+#define CHROMECAST_MEDIA_AUDIO_MIXER_SERVICE_CONSTANTS_H_
+
+#include <stdint.h>
+
+#include "chromecast/media/audio/mixer_service/mixer_service_buildflags.h"
+
+namespace chromecast {
+namespace media {
+namespace mixer_service {
+
+#if BUILDFLAG(USE_UNIX_SOCKETS)
+constexpr char kDefaultUnixDomainSocketPath[] = "/tmp/mixer-service";
+#else
+constexpr int kDefaultTcpPort = 12854;
+#endif
+
+enum class MessageType : int16_t {
+  kMetadata,
+  kAudio,
+};
+
+}  // namespace mixer_service
+}  // namespace media
+}  // namespace chromecast
+
+#endif  // CHROMECAST_MEDIA_AUDIO_MIXER_SERVICE_CONSTANTS_H_
