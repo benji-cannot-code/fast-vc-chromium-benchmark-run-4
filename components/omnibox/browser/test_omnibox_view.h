92c81b97139c12d284d31cd55dde409162865be6
Tommy C. Li <tommycli@chromium.org>
2018-09-21 17:37:55 +0000

    Omnibox: Break up OmniboxViewTest.PreserveDisplayTextOnFocusSearch
    This test breaks up OmniboxViewTest.PreserveDisplayTextOnFocusSearch,
    which was a browser test, into three smaller unit tests.
    
    OmniboxViewTest.PreserveDisplayTextOnFocusSearch is flaky, and the
    whole set of OmniboxViewTest.* browser tests have been flaky for at
    least a year.
    
    Bug: 751031
    Change-Id: I6e7a8fc88f55fee706cdfc1ee0617ce4a27f5103
    Reviewed-on: https://chromium-review.googlesource.com/1232525
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593246}

--- components/omnibox/browser/test_omnibox_view.h
+++ components/omnibox/browser/test_omnibox_view.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string16.h"
 #include "components/omnibox/browser/omnibox_edit_model.h"
 #include "components/omnibox/browser/omnibox_view.h"
+#include "ui/gfx/range/range.h"
 
 struct AutocompleteMatch;
 class OmniboxEditController;
@@ -21,6 +22,8 @@ class TestOmniboxView : public OmniboxView {
  public:
   explicit TestOmniboxView(OmniboxEditController* controller)
       : OmniboxView(controller, nullptr) {}
+
+  void SetModel(std::unique_ptr<OmniboxEditModel> model);
 
   const base::string16& inline_autocomplete_text() const {
     return inline_autocomplete_text_;
@@ -35,7 +38,6 @@ class TestOmniboxView : public OmniboxView {
                  size_t selected_line,
                  base::TimeTicks match_selection_timestamp) override {}
   base::string16 GetText() const override;
-  void SetUserText(const base::string16& text, bool update_popup) override;
   void SetWindowTextAndCaretPos(const base::string16& text,
                                 size_t caret_pos,
                                 bool update_popup,
@@ -43,8 +45,8 @@ class TestOmniboxView : public OmniboxView {
   void SetCaretPos(size_t caret_pos) override {}
   void EnterKeywordModeForDefaultSearchProvider() override {}
   bool IsSelectAll() const override;
-  void GetSelectionBounds(size_t* start, size_t* end) const override {}
-  void SelectAll(bool reversed) override {}
+  void GetSelectionBounds(size_t* start, size_t* end) const override;
+  void SelectAll(bool reversed) override;
   void RevertAll() override {}
   void UpdatePopup() override {}
   void SetFocus() override {}
@@ -68,11 +70,11 @@ class TestOmniboxView : public OmniboxView {
   void EmphasizeURLComponents() override {}
   void SetEmphasis(bool emphasize, const gfx::Range& range) override {}
   void UpdateSchemeStyle(const gfx::Range& range) override {}
-  void SetModel(OmniboxEditModel* model);
 
  private:
   base::string16 text_;
   base::string16 inline_autocomplete_text_;
+  gfx::Range selection_;
 
   DISALLOW_COPY_AND_ASSIGN(TestOmniboxView);
 };
