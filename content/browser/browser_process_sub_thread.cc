9d768de6bedf243612e88c2cc6b8e1b203fe82b7
Etienne Pierre-Doray <etiennep@chromium.org>
2018-09-21 15:28:07 +0000

    [TaskScheduler]: Implement AssertLongCPUWorkAllowed.
    Use case: An expensive function wants to make sure it's not running on the UI
    thread so the expensive operations don't block that thread.
    This assertion fails on threads other than scheduler workers.
    
    Bug: 879181
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: I2a7ec2442c2020da4d68605c2476a01d25a45a33
    Reviewed-on: https://chromium-review.googlesource.com/1221787
    Reviewed-by: François Doray <fdoray@chromium.org>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593191}

--- content/browser/browser_process_sub_thread.cc
+++ content/browser/browser_process_sub_thread.cc
@@ -101,8 +101,7 @@ void BrowserProcessSubThread::Init() {
 #endif
 
   if (!is_blocking_allowed_for_testing_) {
-    base::DisallowBlocking();
-    base::DisallowBaseSyncPrimitives();
+    base::DisallowUnresponsiveTasks();
   }
 }
 
