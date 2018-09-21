992e0369c2e0f45da42c456adaf8ef69be170ebe
Noel Gordon <noel@chromium.org>
2018-09-21 04:38:36 +0000

    Remove unused file manager variable pageLoaded
    This variable was added in [1] but the patch never used the variable,
    remove it.
    
    [1] https://goo.gl/Fnvppi (good read btw, explaining why file manager
    has many separate initialization steps).
    
    Bug: 887792
    Change-Id: Ic9af3447382589bf27fc84fe7a6613c2a269d75d
    Reviewed-on: https://chromium-review.googlesource.com/1237894
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593078}

--- ui/file_manager/file_manager/foreground/js/main.js
+++ ui/file_manager/file_manager/foreground/js/main.js
@@ -9,13 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 var fileManager;
 
 /**
- * Indicates if the DOM and scripts have been already loaded.
- * TODO(noel): is this variable used anywhere?
- * @type {boolean}
- */
-var pageLoaded = false;
-
-/**
  * Initializes the File Manager UI and starts the File Manager dialog. Called
  * by main.html after the DOM and all scripts have been loaded.
  * @param event DOMContentLoaded event.
