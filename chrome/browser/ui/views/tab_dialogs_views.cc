089fcec38a19bf324aef156c594d445d9caa5ae4
Nico Weber <thakis@chromium.org>
2018-09-21 18:51:01 +0000

    Reland "mac: Remove unused TabDialogsMac/TabDialogsCocoa."
    This reverts commit 8e5ef2403719cfde60ed30e5078719813203661f.
    
    Reason for revert: Relanding with HungRendererController removed as well.
    
    Original change's description:
    > Revert "mac: Remove unused TabDialogsMac/TabDialogsCocoa."
    >
    > This reverts commit 50439b8800226bc90259e70f2ba276ed3368be82.
    >
    > Reason for revert:
    > Mac bots start failing after this CL on HungRendererControllerTest.
    > The error logs are below.
    > https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Mac10.11%20Tests/29038
    > https://logs.chromium.org/logs/chromium/buildbucket/cr-buildbucket.appspot.com/8934838111834185888/+/steps/unit_tests_on__none__GPU_on_Mac_on_Mac-10.11/0/logs/HungRendererControllerTest.TestKillButton/0
    >
    > Original change's description:
    > > mac: Remove unused TabDialogsMac/TabDialogsCocoa.
    > >
    > > Bug: 832676
    > > Change-Id: I0fb8d4b1f0197dab0b988a52741724f63a18ee9f
    > > Reviewed-on: https://chromium-review.googlesource.com/1236178
    > > Commit-Queue: Nico Weber <thakis@chromium.org>
    > > Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#592819}
    >
    > TBR=ellyjones@chromium.org,thakis@chromium.org
    >
    > Change-Id: I6554e741ef31bc99da0c90e3c4fd2bb8826a741f
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 832676
    > Reviewed-on: https://chromium-review.googlesource.com/1237797
    > Reviewed-by: Taiju Tsuiki <tzik@chromium.org>
    > Commit-Queue: Taiju Tsuiki <tzik@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#593085}
    
    TBR=ellyjones@chromium.org,thakis@chromium.org,tzik@chromium.org
    
    Change-Id: I5e076b202734c727608279952fad9d1b5657636b
    Bug: 832676
    Reviewed-on: https://chromium-review.googlesource.com/1238595
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593278}

--- chrome/browser/ui/views/tab_dialogs_views.cc
+++ chrome/browser/ui/views/tab_dialogs_views.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/sync/profile_signin_confirmation_dialog_views.h"
 #endif
 
-#if !defined(OS_MACOSX)
 // static
 void TabDialogs::CreateForWebContents(content::WebContents* contents) {
   DCHECK(contents);
@@ -28,7 +27,6 @@ void TabDialogs::CreateForWebContents(content::WebContents* contents) {
                           std::make_unique<TabDialogsViews>(contents));
   }
 }
-#endif
 
 TabDialogsViews::TabDialogsViews(content::WebContents* contents)
     : web_contents_(contents) {
