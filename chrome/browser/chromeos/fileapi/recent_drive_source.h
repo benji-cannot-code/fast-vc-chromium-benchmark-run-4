1e5f80a6d6b9d5e8ade394c7757d6a69d511d4e1
Sam McNally <sammc@chromium.org>
2018-09-21 06:15:14 +0000

    Add support for querying DriveFS over mojo to RecentDriveSource.
    Bug: 854481
    Change-Id: I3c6fd0ed9eb32410cde74b940bedf4cd1323bea7
    Reviewed-on: https://chromium-review.googlesource.com/1235492
    Commit-Queue: Sam McNally <sammc@chromium.org>
    Reviewed-by: Sergei Datsenko <dats@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593093}

--- chrome/browser/chromeos/fileapi/recent_drive_source.h
+++ chrome/browser/chromeos/fileapi/recent_drive_source.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/optional.h"
 #include "base/time/time.h"
 #include "chrome/browser/chromeos/fileapi/recent_source.h"
+#include "chromeos/components/drivefs/mojom/drivefs.mojom.h"
 #include "components/drive/chromeos/file_system_interface.h"
 #include "components/drive/file_errors.h"
 
@@ -55,6 +56,10 @@ class RecentDriveSource : public RecentSource {
                      const base::File::Info& info);
   void OnComplete();
 
+  void GotSearchResults(
+      drive::FileError error,
+      base::Optional<std::vector<drivefs::mojom::QueryItemPtr>> results);
+
   Profile* const profile_;
 
   // Set at the beginning of GetRecentFiles().
@@ -64,6 +69,8 @@ class RecentDriveSource : public RecentSource {
 
   int num_inflight_stats_ = 0;
   std::vector<RecentFile> files_;
+
+  drivefs::mojom::SearchQueryPtr search_query_;
 
   base::WeakPtrFactory<RecentDriveSource> weak_ptr_factory_;
 
