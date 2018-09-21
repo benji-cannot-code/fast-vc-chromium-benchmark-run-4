90920bce7ded89ec08ff6e2b2b16370696e2ad01
bsheedy <bsheedy@chromium.org>
2018-09-21 16:35:57 +0000

    Restore modified shared pref settings after tests
    Changes the behavior of instrumentation tests to automatically
    restore any shared preference files modified by
    --shared-prefs-file. Also adds a simple script for manually
    applying a shared preference JSON file for cases where settings
    that are normally only used during automated testing are
    needed for manual use.
    
    Change-Id: I94006c1a8e48d910ed2f7e0742f001d8d62b7b15
    Reviewed-on: https://chromium-review.googlesource.com/1220557
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Reviewed-by: Bill Orr <billorr@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593218}

--- build/android/pylib/local/device/local_device_instrumentation_test_run.py
+++ build/android/pylib/local/device/local_device_instrumentation_test_run.py
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 import contextlib
+import copy
 import hashlib
 import json
 import logging
@@ -127,6 +128,7 @@ class LocalDeviceInstrumentationTestRun(
         env, test_instance)
     self._flag_changers = {}
     self._replace_package_contextmanager = None
+    self._shared_prefs_to_restore = []
 
   #override
   def TestPackage(self):
@@ -227,6 +229,10 @@ class LocalDeviceInstrumentationTestRun(
           shared_pref = shared_prefs.SharedPrefs(
               dev, setting['package'], setting['filename'],
               use_encrypted_path=setting.get('supports_encrypted_path', False))
+          pref_to_restore = copy.copy(shared_pref)
+          pref_to_restore.Load()
+          self._shared_prefs_to_restore.append(pref_to_restore)
+
           shared_preference_utils.ApplySharedPreferenceSetting(
               shared_pref, setting)
 
@@ -311,6 +317,13 @@ class LocalDeviceInstrumentationTestRun(
       dev.RunShellCommand(['am', 'clear-debug-app'], check_return=True)
 
       valgrind_tools.SetChromeTimeoutScale(dev, None)
+
+      # Restore any shared preference files that we stored during setup.
+      # This should be run sometime before the replace package contextmanager
+      # gets exited so we don't have to special case restoring files of
+      # replaced system apps.
+      for pref_to_restore in self._shared_prefs_to_restore:
+        pref_to_restore.Commit(force_commit=True)
 
       if self._replace_package_contextmanager:
         # See pylint-related commend above with __enter__()
