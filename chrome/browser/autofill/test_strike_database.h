efca0e6838f6ffb459026b7a12b2d042f8767c0d
Anne Lim <annelim@google.com>
2018-09-21 18:57:19 +0000

    [Autofill] StrikeSystem Get/Add/ClearStrikes Architecture
    Implemented StrikeSystem Architecture, which includes functions to
    Get, Add, and Clear strike count
    
    Bug: 884817
    Change-Id: I3d936dd902213646d6bcd84e9a5fbfe8eee4fbea
    Reviewed-on: https://chromium-review.googlesource.com/1231988
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Commit-Queue: Anne Lim <annelim@google.com>
    Cr-Commit-Position: refs/heads/master@{#593280}

---
+++ chrome/browser/autofill/test_strike_database.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_AUTOFILL_TEST_STRIKE_DATABASE_H_
+#define CHROME_BROWSER_AUTOFILL_TEST_STRIKE_DATABASE_H_
+
+#include "chrome/browser/autofill/strike_database.h"
+
+namespace autofill {
+
+class TestStrikeDatabase : public StrikeDatabase {
+ public:
+  TestStrikeDatabase(const base::FilePath& database_dir);
+
+  void AddEntries(
+      std::vector<std::pair<std::string, StrikeData>> entries_to_add,
+      const SetValueCallback& callback);
+};
+
+}  // namespace autofill
+
+#endif  // CHROME_BROWSER_AUTOFILL_TEST_STRIKE_DATABASE_H_
