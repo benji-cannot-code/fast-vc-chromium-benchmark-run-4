857f6fbeabaa6cd01f5243770255fcd63c4f4351
Christopher Cameron <ccameron@chromium.org>
2018-09-21 08:41:15 +0000

    Limit prefering glClear for clearing textures to macOS
    This behavior only helps on macOS and hits lots of bugs on lots of other
    platforms.
    
    Bug: 883276
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I7b85b254c962003cf1fa5646d8a304ce04aa856a
    Reviewed-on: https://chromium-review.googlesource.com/1236930
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593121}

--- gpu/command_buffer/service/gles2_cmd_decoder_unittest_drawing.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder_unittest_drawing.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/strings/string_number_conversions.h"
+#include "build/build_config.h"
 #include "gpu/command_buffer/common/gles2_cmd_format.h"
 #include "gpu/command_buffer/common/gles2_cmd_utils.h"
 #include "gpu/command_buffer/service/context_group.h"
@@ -2336,6 +2337,7 @@ TEST_P(GLES2DecoderManualInitTest, DrawClearsDepthTexture) {
   EXPECT_EQ(GL_NO_ERROR, GetGLError());
 }
 
+#if defined(OS_MACOSX)
 TEST_P(GLES2DecoderManualInitTest, DrawClearsLargeTexture) {
   InitState init;
   init.gl_version = "OpenGL ES 3.0";
@@ -2410,6 +2412,7 @@ TEST_P(GLES2DecoderManualInitTest, DrawClearsLargeTexture) {
   EXPECT_EQ(error::kNoError, ExecuteCmd(cmd));
   EXPECT_EQ(GL_NO_ERROR, GetGLError());
 }
+#endif
 
 TEST_P(GLES3DecoderTest, DrawNoProgram) {
   SetupAllNeededVertexBuffers();
