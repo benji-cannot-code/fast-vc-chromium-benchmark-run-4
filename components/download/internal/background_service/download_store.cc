7b51d4e5a5ec0893bce1b97fdc1066599547ba05
Siddhartha <ssid@chromium.org>
2018-09-21 20:39:39 +0000

    Set the write buffer size of all proto database
    Before this CL lot of proto databases used default write buffer size
    which is 4MB. Some databases explicitly set write buffer sizes because
    of telemetry regressions observed. The rest of them were not hit by
    telemetry stories when they were added. After this CL the default buffer
    size will be set to 512KB for high end and 128KB for low end devices.
    Most databases don't store a lot of data, but do a lot of transactions.
    So, this will cause compaction more often.
    
    BUG=851034
    
    Change-Id: Ia0fb28035a8e6e3221ee7dc843924086d062a52b
    Reviewed-on: https://chromium-review.googlesource.com/1236286
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593323}

--- components/download/internal/background_service/download_store.cc
+++ components/download/internal/background_service/download_store.cc
@@ -23,6 +23,14 @@ using KeyVector = std::vector<std::string>;
 using ProtoEntryVector = std::vector<protodb::Entry>;
 using KeyProtoEntryVector = std::vector<std::pair<std::string, protodb::Entry>>;
 
+leveldb_env::Options GetDownloadDBOptions() {
+  // These options reduce memory consumption.
+  leveldb_env::Options options = leveldb_proto::CreateSimpleOptions();
+  options.reuse_logs = false;
+  options.write_buffer_size = 64 << 10;  // 64 KiB
+  return options;
+}
+
 }  // namespace
 
 DownloadStore::DownloadStore(
@@ -41,11 +49,7 @@ bool DownloadStore::IsInitialized() {
 
 void DownloadStore::Initialize(InitCallback callback) {
   DCHECK(!IsInitialized());
-  // These options reduce memory consumption.
-  leveldb_env::Options options = leveldb_proto::CreateSimpleOptions();
-  options.reuse_logs = false;
-  options.write_buffer_size = 64 << 10;  // 64 KiB
-  db_->Init(kDatabaseClientName, database_dir_, options,
+  db_->Init(kDatabaseClientName, database_dir_, GetDownloadDBOptions(),
             base::BindOnce(&DownloadStore::OnDatabaseInited,
                            weak_factory_.GetWeakPtr(), std::move(callback)));
 }
@@ -86,8 +90,7 @@ void DownloadStore::OnDatabaseDestroyed(StoreCallback callback, bool success) {
     return;
   }
 
-  db_->Init(kDatabaseClientName, database_dir_,
-            leveldb_proto::CreateSimpleOptions(),
+  db_->Init(kDatabaseClientName, database_dir_, GetDownloadDBOptions(),
             base::BindOnce(&DownloadStore::OnDatabaseInitedAfterDestroy,
                            weak_factory_.GetWeakPtr(), std::move(callback)));
 }
