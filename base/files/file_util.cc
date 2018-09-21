28fecd55e8a2e4ccbc4fd8c5b8c20ff024c54f4e
Etienne Pierre-Doray <etiennep@chromium.org>
2018-09-21 14:47:20 +0000

    [TaskScheduler]: Add ScopedBlockingCall in ReadFileToStringWithMaxSize.
    While OpenFile/CloseFile are already covered by ScopedBlockingCall,
    this function does file reading operations (fread), which may block.
    ScopedBlockingCall serves as a precise annotation of the scope that may/will
    block for the scheduler. Since it supports nesting, there is no problem calling
    CloseFile() in the same scope.
    
    Bug: 874080
    Change-Id: Id65de018cc20a2788d42d408c1c49052eed813c0
    Reviewed-on: https://chromium-review.googlesource.com/1236393
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593180}

--- base/files/file_util.cc
+++ base/files/file_util.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/threading/scoped_blocking_call.h"
 #include "build/build_config.h"
 
 namespace base {
@@ -157,6 +158,7 @@ bool ReadFileToStringWithMaxSize(const FilePath& path,
   std::string local_contents;
   local_contents.resize(chunk_size);
 
+  ScopedBlockingCall scoped_blocking_call(BlockingType::MAY_BLOCK);
   while ((bytes_read_this_pass = fread(&local_contents[bytes_read_so_far], 1,
                                        chunk_size, file)) > 0) {
     if ((max_size - bytes_read_so_far) < bytes_read_this_pass) {
