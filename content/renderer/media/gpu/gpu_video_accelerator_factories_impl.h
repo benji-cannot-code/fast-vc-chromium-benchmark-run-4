187c634a7872db4f63411ed2bf4d6b9c11580966
Dan Sanders <sandersd@chromium.org>
2018-09-21 20:53:49 +0000

    [media] Mojo replacement for VDA SupportedProfiles.
    This API allows mojom::VideoDecoder clients to get a list of rules that
    can be used to reject unsupported Initialize() calls without additional
    IPC.
    
    On the GPU side, this information is gathered from the existing VDA
    SupportedProfiles factory.
    
    On the renderer side, GpuVideoAcceleratorFactories is now responsible
    for this query, which it does upon construction. If a MojoVideoDecoder
    is initialized before the query has completed, MojoVideoDecoder will
    call the remote Intitialize() method as normal.
    
    Bug: 839951, 522298
    Change-Id: I62b1e2f6ad79f096d273d7591da1e98094b9cbfe
    Reviewed-on: https://chromium-review.googlesource.com/1066620
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593327}

--- content/renderer/media/gpu/gpu_video_accelerator_factories_impl.h
+++ content/renderer/media/gpu/gpu_video_accelerator_factories_impl.h
@@ -15,12 +15,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
+#include "base/optional.h"
 #include "base/single_thread_task_runner.h"
 #include "base/synchronization/waitable_event.h"
 #include "base/unguessable_token.h"
 #include "content/child/thread_safe_sender.h"
 #include "content/common/content_export.h"
 #include "media/mojo/interfaces/interface_factory.mojom.h"
+#include "media/mojo/interfaces/video_decoder.mojom.h"
 #include "media/mojo/interfaces/video_encode_accelerator.mojom.h"
 #include "media/video/gpu_video_accelerator_factories.h"
 #include "ui/gfx/geometry/size.h"
@@ -32,7 +34,7 @@ class GpuMemoryBufferManager;
 
 namespace ui {
 class ContextProviderCommandBuffer;
-}
+}  // namespace ui
 
 namespace content {
 
@@ -71,6 +73,8 @@ class CONTENT_EXPORT GpuVideoAcceleratorFactoriesImpl
       media::MediaLog* media_log,
       const media::RequestOverlayInfoCB& request_overlay_info_cb,
       const gfx::ColorSpace& target_color_space) override;
+  bool IsDecoderConfigSupported(
+      const media::VideoDecoderConfig& config) override;
   std::unique_ptr<media::VideoDecodeAccelerator> CreateVideoDecodeAccelerator()
       override;
   std::unique_ptr<media::VideoEncodeAccelerator> CreateVideoEncodeAccelerator()
@@ -152,6 +156,10 @@ class CONTENT_EXPORT GpuVideoAcceleratorFactoriesImpl
   void SetContextProviderLost();
   void SetContextProviderLostOnMainThread();
 
+  void OnSupportedDecoderConfigs(
+      std::vector<media::mojom::SupportedVideoDecoderConfigPtr>
+          supported_configs);
+
   const scoped_refptr<base::SingleThreadTaskRunner> main_thread_task_runner_;
   const scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
   const scoped_refptr<gpu::GpuChannelHost> gpu_channel_host_;
@@ -180,6 +188,11 @@ class CONTENT_EXPORT GpuVideoAcceleratorFactoriesImpl
 
   media::mojom::InterfaceFactoryPtr interface_factory_;
   media::mojom::VideoEncodeAcceleratorProviderPtr vea_provider_;
+
+  // SupportedDecoderConfigs state.
+  mojo::InterfacePtr<media::mojom::VideoDecoder> video_decoder_;
+  base::Optional<std::vector<media::mojom::SupportedVideoDecoderConfigPtr>>
+      supported_decoder_configs_;
 
   // For sending requests to allocate shared memory in the Browser process.
   scoped_refptr<ThreadSafeSender> thread_safe_sender_;
