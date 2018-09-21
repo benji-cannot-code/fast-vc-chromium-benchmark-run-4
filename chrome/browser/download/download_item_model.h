6b3a3a59d22401a51389e92d15602f10b43e7e4e
Min Qin <qinmin@chromium.org>
2018-09-21 23:02:31 +0000

    Implementation of OfflineItemModel
    This CL adds implementation of OfflineItemModel.
    Some of the common functionalities between OfflineItemModel and
    DownloadItemModel are moved to DownloadUIModel.
    
    Bug: 881499
    Change-Id: Ic2e4835f4eb16422f152d0ed641dba3e9646defb
    Reviewed-on: https://chromium-review.googlesource.com/1239334
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593364}

--- chrome/browser/download/download_item_model.h
+++ chrome/browser/download/download_item_model.h
@@ -15,10 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/safe_browsing/download_file_types.pb.h"
 #include "components/download/public/common/download_item.h"
 
-namespace gfx {
-class FontList;
-}
-
 // Implementation of DownloadUIModel that wrappers around a |DownloadItem*|. As
 // such, the caller is expected to ensure that the |download| passed into the
 // constructor outlives this |DownloadItemModel|. In addition, multiple
@@ -34,21 +30,13 @@ class DownloadItemModel : public DownloadUIModel,
   // DownloadUIModel implementation.
   ContentId GetContentId() const override;
   Profile* profile() const override;
-  base::string16 GetInterruptReasonText() const override;
-  base::string16 GetStatusText() const override;
   base::string16 GetTabProgressStatusText() const override;
-  base::string16 GetTooltipText(const gfx::FontList& font_list,
-                                int max_width) const override;
-  base::string16 GetWarningText(const gfx::FontList& font_list,
-                                int base_width) const override;
-  base::string16 GetWarningConfirmButtonText() const override;
   int64_t GetCompletedBytes() const override;
   int64_t GetTotalBytes() const override;
   int PercentComplete() const override;
   bool IsDangerous() const override;
   bool MightBeMalicious() const override;
   bool IsMalicious() const override;
-  bool HasSupportedImageMimeType() const override;
   bool ShouldAllowDownloadFeedback() const override;
   bool ShouldRemoveFromShelfWhenComplete() const override;
   bool ShouldShowDownloadStartedAnimation() const override;
@@ -66,8 +54,6 @@ class DownloadItemModel : public DownloadUIModel,
   bool IsBeingRevived() const override;
   void SetIsBeingRevived(bool is_being_revived) override;
   download::DownloadItem* download() override;
-  base::string16 GetProgressSizesString() const override;
-
   base::FilePath GetFileNameToReportUser() const override;
   base::FilePath GetTargetFilePath() const override;
   void OpenDownload() override;
@@ -106,8 +92,9 @@ class DownloadItemModel : public DownloadUIModel,
   void OnDownloadDestroyed(download::DownloadItem* download) override;
 
  private:
-  // Returns a string indicating the status of an in-progress download.
-  base::string16 GetInProgressStatusString() const;
+  // DownloadUIModel implementation.
+  std::string GetMimeType() const override;
+  bool IsExtensionDownload() const override;
 
   // The DownloadItem that this model represents. Note that DownloadItemModel
   // itself shouldn't maintain any state since there can be more than one
