840d540f04119bac8add8b0caa824241c152ff37
Tom Anderson <thomasanderson@chromium.org>
2018-09-21 01:15:23 +0000

    [GTK] Fix menu background color regression
    Regressed after 9c5131ad426872a1c931a6e3a27bbd4c469b7721.  This caused the menu
    background to always render white even though the text colors would still be
    styled using the system theme.  This caused illegible text when the menu
    background was supposed to be dark.
    
    R=pkasting
    TBR=sky
    
    Change-Id: I39b836e0941cbfc033515fe3891ecd42d89d73a4
    Reviewed-on: https://chromium-review.googlesource.com/1237519
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593039}

--- ui/views/controls/menu/menu_scroll_view_container.cc
+++ ui/views/controls/menu/menu_scroll_view_container.cc
@@ -189,10 +189,7 @@ MenuScrollViewContainer::MenuScrollViewContainer(SubmenuView* content_view)
   arrow_ = BubbleBorderTypeFromAnchor(
       content_view_->GetMenuItem()->GetMenuController()->GetAnchorPosition());
 
-  if (HasBubbleBorder())
-    CreateBubbleBorder();
-  else
-    CreateDefaultBorder();
+  CreateBorder();
 }
 
 bool MenuScrollViewContainer::HasBubbleBorder() {
@@ -240,8 +237,7 @@ void MenuScrollViewContainer::Layout() {
 
 void MenuScrollViewContainer::OnNativeThemeChanged(
     const ui::NativeTheme* theme) {
-  if (!HasBubbleBorder())
-    CreateDefaultBorder();
+  CreateBorder();
 }
 
 void MenuScrollViewContainer::OnPaintBackground(gfx::Canvas* canvas) {
@@ -271,6 +267,13 @@ void MenuScrollViewContainer::OnBoundsChanged(
   scroll_up_button_->SetVisible(content_pref.height() > height());
   scroll_down_button_->SetVisible(content_pref.height() > height());
   Layout();
+}
+
+void MenuScrollViewContainer::CreateBorder() {
+  if (HasBubbleBorder())
+    CreateBubbleBorder();
+  else
+    CreateDefaultBorder();
 }
 
 void MenuScrollViewContainer::CreateDefaultBorder() {
@@ -311,7 +314,9 @@ void MenuScrollViewContainer::CreateDefaultBorder() {
 
 void MenuScrollViewContainer::CreateBubbleBorder() {
   bubble_border_ =
-      new BubbleBorder(arrow_, BubbleBorder::SMALL_SHADOW, SK_ColorWHITE);
+      new BubbleBorder(arrow_, BubbleBorder::SMALL_SHADOW,
+                       GetNativeTheme()->GetSystemColor(
+                           ui::NativeTheme::kColorId_MenuBackgroundColor));
   MenuController* controller =
       content_view_->GetMenuItem()->GetMenuController();
   if (MD::IsRefreshUi() || controller->use_touchable_layout()) {
