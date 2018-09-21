72c641e47883a1fd46d0ce1d483d12ea323de9d8
Jun Mukai <mukai@chromium.org>
2018-09-21 01:25:30 +0000

    fix some TabDragController test for Mash
    This CL fixes a part of UI tests of TabDragController.
    - some cases RootWindow is wanted to set/clear/check the window
      properties when Mash is enabled, those test expectations
      should be fixed.
    - some tests expect Ash style organization of window, checking
      root window pointer to exist in a particular display.
      Unfortunately some of this isn't fully fixed due to an issue
      of crbug.com/885312
    - BrowserFrameMash's change is needed for IsWindowPositionManaged()
      which is called on some test cases. This is basically what
      BrowserFrameAsh does.
    
    BUG=885312
    TEST=interactive_ui_tests --enable-features=SingleProcessMash
    
    Change-Id: Ic2e76a7e25d5104b32a3449fb45f047deab4b0dd
    Reviewed-on: https://chromium-review.googlesource.com/1232685
    Commit-Queue: Jun Mukai <mukai@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593042}

--- chrome/browser/ui/views/frame/browser_frame_mash.h
+++ chrome/browser/ui/views/frame/browser_frame_mash.h
@@ -21,6 +21,9 @@ class BrowserFrameMash : public views::DesktopNativeWidgetAura,
   ~BrowserFrameMash() override;
 
  private:
+  // views::DesktopNativeWidgetAura:
+  void OnWindowTargetVisibilityChanged(bool visible) override;
+
   // Overridden from NativeBrowserFrame:
   views::Widget::InitParams GetWidgetParams() override;
   bool UseCustomFrame() const override;
