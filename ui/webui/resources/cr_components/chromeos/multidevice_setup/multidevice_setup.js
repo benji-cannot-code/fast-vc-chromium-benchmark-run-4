49319da5bfb9d3fcd255d7cfd3eff977c31b0135
Kyle Horimoto <khorimoto@google.com>
2018-09-21 23:36:48 +0000

    [CrOS MultiDevice] Refactor shared MultiDevice setup flow resources.
    The MultiDevice setup flow uses several HTML/JS resources to render the
    setup flow dialog. Previously, these files were added directly to the
    data source for chrome://multidevice-setup.
    
    Now that this UI is being shared between both chrome://multidevice-setup
    and chrome://oobe, the resources are moved to a shared location; namely,
    //ui/webui/resources/cr_components/chromeos/multidevice_setup. They are
    now served from
    chrome://resources/cr_components/chromeos/multidevice_setup/*.
    
    Bug: 884041
    Change-Id: Ia69c27875a9d3600006c4220f498acc5a2aef7be
    Reviewed-on: https://chromium-review.googlesource.com/1232334
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593378}

--- ui/webui/resources/cr_components/chromeos/multidevice_setup/multidevice_setup.js
+++ ui/webui/resources/cr_components/chromeos/multidevice_setup/multidevice_setup.js
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 cr.exportPath('multidevice_setup');
 
-  /** @enum {string} */
+/** @enum {string} */
 multidevice_setup.PageName = {
   FAILURE: 'setup-failed-page',
   PASSWORD: 'password-page',
