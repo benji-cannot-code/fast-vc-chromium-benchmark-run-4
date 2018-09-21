375074a34c754ae16da5ffa35317b34a49bacf19
Alan Cutter <alancutter@chromium.org>
2018-09-21 03:54:07 +0000

    Force Mac Views for HostedAppOpaqueBrowserFrameViewTest on mac-cocoa-rel
    This CL makes HostedAppOpaqueBrowserFrameViewTest force enable Mac Views
    to fix a crash on mac-cocoa-rel.
    
    Bug: 885164
    Change-Id: I8cd754954c9119a65c8aca94b7a07f2159e3f95d
    Reviewed-on: https://chromium-review.googlesource.com/1237733
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593073}

--- chrome/browser/ui/views/frame/opaque_browser_frame_view_browsertest.cc
+++ chrome/browser/ui/views/frame/opaque_browser_frame_view_browsertest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/web_application_info.h"
 #include "chrome/test/base/in_process_browser_test.h"
+#include "chrome/test/views/scoped_macviews_browser_mode.h"
 
 // Tests hosted app windows that use the OpaqueBrowserFrameView implementation
 // for their non client frames.
@@ -65,6 +66,8 @@ class HostedAppOpaqueBrowserFrameViewTest : public InProcessBrowserTest {
   HostedAppButtonContainer* hosted_app_button_container_ = nullptr;
 
  private:
+  test::ScopedMacViewsBrowserMode force_mac_views_{true};
+
   DISALLOW_COPY_AND_ASSIGN(HostedAppOpaqueBrowserFrameViewTest);
 };
 
