93630ca99e43b31d33ac8cef79fc694fd4983df6
Sam McNally <sammc@chromium.org>
2018-09-21 06:20:27 +0000

    Remove leftover support for unused Drive API endpoints.
    https://crrev.com/591294 removed the code for Drive apps, removing three
    API endpoints. The URL generator methods for those endpoints were left
    behind. Remove them as well as the option to use the internal endpoint;
    no remaining Drive API usage requires using the internal endpoint.
    
    Bug: 826090
    Change-Id: I99a0a60b3b9862d6a399b083d882b9a947cafc78
    Reviewed-on: https://chromium-review.googlesource.com/1236617
    Reviewed-by: Stuart Langley <slangley@chromium.org>
    Commit-Queue: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593094}

--- components/drive/service/drive_api_service.cc
+++ components/drive/service/drive_api_service.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "google_apis/drive/drive_api_requests.h"
 #include "google_apis/drive/files_list_request_runner.h"
 #include "google_apis/drive/request_sender.h"
-#include "google_apis/google_api_keys.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 
 using google_apis::AboutResourceCallback;
@@ -472,8 +471,7 @@ CancelCallback DriveAPIService::GetFileResource(
   DCHECK(!callback.is_null());
 
   std::unique_ptr<FilesGetRequest> request = std::make_unique<FilesGetRequest>(
-      sender_.get(), url_generator_, google_apis::IsGoogleChromeAPIKeyUsed(),
-      callback);
+      sender_.get(), url_generator_, callback);
   request->set_file_id(resource_id);
   request->set_fields(kFileResourceFields);
   return sender_->StartRequestWithAuthRetry(std::move(request));
