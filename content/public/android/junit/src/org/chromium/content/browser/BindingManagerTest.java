f7b02d8d2fb95d964b3acbb48388c8de5dbdfc8a
Bo Liu <boliu@chromium.org>
2018-09-21 22:48:00 +0000

    Minor BindingManager clean up
    Inline two methods that's has a single call site. Rename methods to
    add/removeConnection since BindingManager isn't based on LRU anymore.
    
    Change-Id: I292a9a596c89720ab17f0bade704fa5c30d11d15
    Reviewed-on: https://chromium-review.googlesource.com/1239345
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593360}

--- content/public/android/junit/src/org/chromium/content/browser/BindingManagerTest.java
+++ content/public/android/junit/src/org/chromium/content/browser/BindingManagerTest.java
@@ -49,7 +49,7 @@ public class BindingManagerTest {
                 null /* serviceBundle */);
         connection.setPid(pid);
         connection.start(false /* useStrongBinding */, null /* serviceCallback */);
-        manager.increaseRecency(connection);
+        manager.addConnection(connection);
         iterable.add(connection);
         connection.removeModerateBinding(); // Remove initial binding.
         return connection;
@@ -182,7 +182,7 @@ public class BindingManagerTest {
             // Verify that each connection has a moderate binding after binding and releasing a
             // strong binding.
             for (ChildProcessConnection connection : connections) {
-                manager.increaseRecency(connection);
+                manager.addConnection(connection);
                 mIterable.add(connection);
                 Assert.assertTrue(message, connection.isModerateBindingBound());
             }
