d28b8135c233e0f8fb1c3ad3d03f9dc36d8751be
Tommy Steimel <steimel@chromium.org>
2018-09-22 01:25:14 +0000

    [Media Controls] Keep events in node for panel in audio controls
    This CL fixes an issue where we don't keep events in node for the
    audio panel.
    
    Bug: 884108
    Change-Id: I039a606005b9be109c36f35a7bf33f4a77f951db
    Reviewed-on: https://chromium-review.googlesource.com/1239006
    Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593407}

--- third_party/blink/renderer/modules/media_controls/media_controls_impl.h
+++ third_party/blink/renderer/modules/media_controls/media_controls_impl.h
@@ -193,6 +193,9 @@ class MODULES_EXPORT MediaControlsImpl final : public HTMLDivElement,
   void OnAccessibleFocus();
   void OnAccessibleBlur();
 
+  // Returns true/false based on which set of controls to display.
+  bool ShouldShowAudioControls() const;
+
  private:
   // MediaControlsMediaEventListener is a component that is listening to events
   // and calling the appropriate callback on MediaControlsImpl. The object is
@@ -291,7 +294,6 @@ class MODULES_EXPORT MediaControlsImpl final : public HTMLDivElement,
   void UpdateActingAsAudioControls();
 
   // Returns true/false based on which set of controls to display.
-  bool ShouldShowAudioControls() const;
   bool ShouldShowVideoControls() const;
 
   // Node
