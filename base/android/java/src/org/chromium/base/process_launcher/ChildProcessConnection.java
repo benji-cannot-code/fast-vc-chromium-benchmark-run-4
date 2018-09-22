2912917e3e738eed9497e1c7d3c154ed9fe22bd0
Bo Liu <boliu@chromium.org>
2018-09-22 00:12:02 +0000

    android: Handle disconnect before setupConnection
    Android may kill a service process before our setupConnection succeeds.
    This is unlikely but not impossible. Fix a few asserts and conditions
    that do not handle this correctly.
    
    Also add an integration test for this scenario.
    
    Change-Id: Ibc5c0155c31caf9b1f73aee169147516d2a0fffe
    Reviewed-on: https://chromium-review.googlesource.com/1237493
    Commit-Queue: Bo <boliu@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593388}

--- base/android/java/src/org/chromium/base/process_launcher/ChildProcessConnection.java
+++ base/android/java/src/org/chromium/base/process_launcher/ChildProcessConnection.java
@@ -430,7 +430,8 @@ public class ChildProcessConnection {
         notifyChildProcessDied();
     }
 
-    private void onServiceConnectedOnLauncherThread(IBinder service) {
+    @VisibleForTesting
+    protected void onServiceConnectedOnLauncherThread(IBinder service) {
         assert isRunningOnLauncherThread();
         // A flag from the parent class ensures we run the post-connection logic only once
         // (instead of once per each ChildServiceConnection).
@@ -481,7 +482,8 @@ public class ChildProcessConnection {
         }
     }
 
-    private void onServiceDisconnectedOnLauncherThread() {
+    @VisibleForTesting
+    protected void onServiceDisconnectedOnLauncherThread() {
         assert isRunningOnLauncherThread();
         // Ensure that the disconnection logic runs only once (instead of once per each
         // ChildServiceConnection).
@@ -744,8 +746,12 @@ public class ChildProcessConnection {
     }
 
     @VisibleForTesting
-    public void crashServiceForTesting() throws RemoteException {
-        mService.forceKill();
+    public void crashServiceForTesting() {
+        try {
+            mService.forceKill();
+        } catch (RemoteException e) {
+            // Expected. Ignore.
+        }
     }
 
     @VisibleForTesting
