7c84601405dcb6f164914c59004583d78e04f527
Philip Rogers <pdr@chromium.org>
2018-09-22 01:06:43 +0000

    Remove ScrollAdjustmentForPaintInvalidation
    When working on another patch I noticed it is deadcode.
    
    Change-Id: I248b12e2f3a5e26af91d2cab89b5b8fd3ace8dd1
    Reviewed-on: https://chromium-review.googlesource.com/1239539
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593400}

--- third_party/blink/renderer/core/layout/layout_object.h
+++ third_party/blink/renderer/core/layout/layout_object.h
@@ -2048,8 +2048,6 @@ class CORE_EXPORT LayoutObject : public ImageResourceObserver,
 
   bool CompositedScrollsWithRespectTo(
       const LayoutBoxModelObject& paint_invalidation_container) const;
-  IntSize ScrollAdjustmentForPaintInvalidation(
-      const LayoutBoxModelObject& paint_invalidation_container) const;
 
   bool PreviousBackgroundObscured() const {
     return bitfields_.PreviousBackgroundObscured();
