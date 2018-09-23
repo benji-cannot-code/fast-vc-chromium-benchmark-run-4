fd661314ae3cddafd9b5cb66addaff333520a7bd
Robert Sesek <rsesek@chromium.org>
2018-09-23 02:01:38 +0000

    mac: Remove views-browser-windows from about:flags
    The feature still exists beacuse tests toggle it, but this removes it
    from the flags UI.
    
    Bug: 832676
    Change-Id: I6971276883e19792f962f9e17d4fa50e8265a1d0
    Reviewed-on: https://chromium-review.googlesource.com/1239766
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593452}

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -2030,11 +2030,6 @@ const char kVideoRotateToFullscreenDescription[] =
     "Enter/exit fullscreen when device is rotated to/from the orientation of "
     "the video. Only on phones.";
 
-const char kViewsBrowserWindowsName[] =
-    "Use Views browser windows instead of Cocoa.";
-const char kViewsBrowserWindowsDescription[] =
-    "Use Views browser windows instead of Cocoa, aka MacViews-Browser.";
-
 const char kWalletServiceUseSandboxName[] =
     "Use Google Payments sandbox servers";
 const char kWalletServiceUseSandboxDescription[] =
--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1225,9 +1225,6 @@ extern const char kVideoFullscreenOrientationLockDescription[];
 extern const char kVideoRotateToFullscreenName[];
 extern const char kVideoRotateToFullscreenDescription[];
 
-extern const char kViewsBrowserWindowsName[];
-extern const char kViewsBrowserWindowsDescription[];
-
 extern const char kWalletServiceUseSandboxName[];
 extern const char kWalletServiceUseSandboxDescription[];
 
