f7204e2601af44e7195dd4c99e45a4e3aa62fa25
John Abd-El-Malek <jam@chromium.org>
2018-09-24 22:00:14 +0000

    Fix ForceBrowserSignIn not working with networking service.
    The problem was that the old sign-in flow didn't work with networking service. This hasn't been converted to use DICE, which already works.
    
    There were two causes:
    -webRequest interception wasn't enabled for the few internal webui pages that are whitelisted for webRequest API
    -webRequest users don't see Set-Cookie headers for responses (since these were sent to the interceptor from the network process, and the IPC serialization removes Cookie headers)
    
    Bug: 887381
    Change-Id: I4f0b598c1610cd3b11dcf01cdd4d43425f218a4f
    Reviewed-on: https://chromium-review.googlesource.com/1238720
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593703}

--- chrome/browser/ui/webui/signin/inline_login_handler.h
+++ chrome/browser/ui/webui/signin/inline_login_handler.h
@@ -14,6 +14,10 @@ namespace base {
 class DictionaryValue;
 }
 
+namespace net {
+class CanonicalCookie;
+}
+
 namespace signin_metrics {
 enum class AccessPoint;
 }
@@ -55,6 +59,12 @@ class InlineLoginHandler : public content::WebUIMessageHandler {
   // work.
   void HandleCompleteLoginMessage(const base::ListValue* args);
 
+  // Called by HandleCompleteLoginMessage after it gets the GAIA URL's cookies
+  // from the CookieManager.
+  void HandleCompleteLoginMessageWithCookies(
+      const base::ListValue& args,
+      const std::vector<net::CanonicalCookie>& cookies);
+
   // JS callback to switch the UI from a constrainted dialog to a full tab.
   void HandleSwitchToFullTabMessage(const base::ListValue* args);
 
@@ -66,7 +76,14 @@ class InlineLoginHandler : public content::WebUIMessageHandler {
   void HandleDialogClose(const base::ListValue* args);
 
   virtual void SetExtraInitParams(base::DictionaryValue& params) {}
-  virtual void CompleteLogin(const base::ListValue* args) = 0;
+  virtual void CompleteLogin(const std::string& email,
+                             const std::string& password,
+                             const std::string& gaia_id,
+                             const std::string& auth_code,
+                             bool skip_for_now,
+                             bool trusted,
+                             bool trusted_found,
+                             bool choose_what_to_sync) = 0;
 
   base::WeakPtrFactory<InlineLoginHandler> weak_ptr_factory_;
 
