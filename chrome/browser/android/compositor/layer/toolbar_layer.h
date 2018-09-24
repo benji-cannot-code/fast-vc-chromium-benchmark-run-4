fc15ff6c155757798144c6065025b046fdacd13f
Bo Liu <boliu@chromium.org>
2018-09-24 16:19:38 +0000

    android: Avoid render pass in browser compositor
    Setting opacity on a layer with children that draws leads to using a
    render pass, which is super expensive. Use the usual trick of setting
    opacity on the leaf layers directly instead.
    
    Change-Id: If394f0bba0953b9c6b951d2f320b358fe16c8acb
    Reviewed-on: https://chromium-review.googlesource.com/1238566
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593556}

--- chrome/browser/android/compositor/layer/toolbar_layer.h
+++ chrome/browser/android/compositor/layer/toolbar_layer.h
@@ -52,6 +52,8 @@ class ToolbarLayer : public Layer {
                          int progress_bar_background_height,
                          int progress_bar_background_color);
 
+  void SetOpacity(float opacity);
+
  protected:
   explicit ToolbarLayer(ui::ResourceManager* resource_manager);
   ~ToolbarLayer() override;
