aae149f4151ebf1442b4d121b0ef7d2eef38f263
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2018-09-24 00:40:11 +0000

    Add Feature to control the usage of BluetoothSystem
    The BluetoothSystem Mojo interface will replace the current Bluetooth
    API. This introduces a Feature to use BluetoothSystem.
    
    Bug: 882346
    Change-Id: I9b0bd963642cdcd887c03124f8af1e2fa150d07e
    Reviewed-on: https://chromium-review.googlesource.com/1235364
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593463}

--- ash/public/cpp/ash_features.h
+++ ash/public/cpp/ash_features.h
@@ -77,6 +77,9 @@ ASH_PUBLIC_EXPORT extern const base::Feature kUnlockWithExternalBinary;
 // Enables views login.
 ASH_PUBLIC_EXPORT extern const base::Feature kViewsLogin;
 
+// Enables using the BluetoothSystem Mojo interface for Bluetooth operations.
+ASH_PUBLIC_EXPORT extern const base::Feature kUseBluetoothSystemInAsh;
+
 ASH_PUBLIC_EXPORT bool IsDockedMagnifierEnabled();
 
 ASH_PUBLIC_EXPORT bool IsKeyboardShortcutViewerEnabled();
