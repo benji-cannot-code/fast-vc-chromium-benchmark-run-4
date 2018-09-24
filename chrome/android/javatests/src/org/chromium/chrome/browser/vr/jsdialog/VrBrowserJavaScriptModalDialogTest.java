081ca0cd37efbd0b289a5e968d1634f0c524e691
bsheedy <bsheedy@chromium.org>
2018-09-24 21:50:54 +0000

    Automated VR Browser <select> tag test
    Automates the manual test for <select> tags/dropdown lists on webpages
    while in the VR browser.
    
    Bug: 887513
    Change-Id: I5419c6e94bc9d1cb149fb32aff9b7c5cee87732b
    Reviewed-on: https://chromium-review.googlesource.com/1241198
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593697}

--- chrome/android/javatests/src/org/chromium/chrome/browser/vr/jsdialog/VrBrowserJavaScriptModalDialogTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/vr/jsdialog/VrBrowserJavaScriptModalDialogTest.java
@@ -28,8 +28,6 @@ import org.chromium.chrome.browser.vr.util.NativeUiUtils;
 import org.chromium.chrome.browser.vr.util.VrBrowserTransitionUtils;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.util.RenderTestRule;
-import org.chromium.content_public.browser.test.util.Criteria;
-import org.chromium.content_public.browser.test.util.CriteriaHelper;
 import org.chromium.content_public.browser.test.util.JavaScriptUtils;
 
 import java.io.IOException;
@@ -106,7 +104,7 @@ public class VrBrowserJavaScriptModalDialogTest {
      */
     private void executeJavaScriptAndWaitForDialog(String script) {
         JavaScriptUtils.executeJavaScript(mActivity.getCurrentWebContents(), script);
-        checkDialogShowing("Could not spawn or locate a modal dialog.", true);
+        NativeUiUtils.waitForModalDialogStatus(true /* shouldBeShown */, mActivity);
     }
 
     /**
@@ -116,18 +114,5 @@ public class VrBrowserJavaScriptModalDialogTest {
     private JavascriptTabModalDialog getCurrentDialog() throws ExecutionException {
         return (JavascriptTabModalDialog) ThreadUtils.runOnUiThreadBlocking(
                 () -> mActivity.getModalDialogManager().getCurrentDialogForTest().getController());
-    }
-
-    /**
-     * Check whether dialog is showing as expected.
-     */
-    private void checkDialogShowing(final String errorMessage, final boolean shouldBeShown) {
-        CriteriaHelper.pollUiThread(new Criteria(errorMessage) {
-            @Override
-            public boolean isSatisfied() {
-                final boolean isShown = mActivity.getModalDialogManager().isShowing();
-                return shouldBeShown == isShown;
-            }
-        });
     }
 }
