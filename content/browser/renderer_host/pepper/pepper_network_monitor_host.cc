bb6f5ff9550897ea0ee12cf184cc267c15de0631
Robbie McElrath <rmcelrath@chromium.org>
2018-09-24 20:58:26 +0000

    Migrate content::PepperNetworkMonitorHost to NetworkConnectionTracker
    This migrates content::PepperNetworkMonitorHost from
    NetworkChangeNotifier to NetworkConnectionTracker, which works with the
    network service enabled.
    
    Bug: 887046
    Change-Id: I6fcd03fa664689cc6bcc16ff80b86fdba75d695d
    Reviewed-on: https://chromium-review.googlesource.com/1236560
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: Bill Budge <bbudge@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593672}

--- content/browser/renderer_host/pepper/pepper_network_monitor_host.cc
+++ content/browser/renderer_host/pepper/pepper_network_monitor_host.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/renderer_host/pepper/pepper_socket_utils.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/browser/network_service_instance.h"
 #include "content/public/common/socket_permission_request.h"
 #include "ppapi/proxy/ppapi_messages.h"
 #include "ppapi/shared_impl/private/net_address_private_impl.h"
@@ -48,6 +49,7 @@ PepperNetworkMonitorHost::PepperNetworkMonitorHost(BrowserPpapiHostImpl* host,
                                                    PP_Instance instance,
                                                    PP_Resource resource)
     : ResourceHost(host->GetPpapiHost(), instance, resource),
+      network_connection_tracker_(nullptr),
       weak_factory_(this) {
   int render_process_id;
   int render_frame_id;
@@ -63,12 +65,16 @@ PepperNetworkMonitorHost::PepperNetworkMonitorHost(BrowserPpapiHostImpl* host,
 }
 
 PepperNetworkMonitorHost::~PepperNetworkMonitorHost() {
-  net::NetworkChangeNotifier::RemoveNetworkChangeObserver(this);
+  if (network_connection_tracker_)
+    network_connection_tracker_->RemoveNetworkConnectionObserver(this);
 }
 
-void PepperNetworkMonitorHost::OnNetworkChanged(
-    net::NetworkChangeNotifier::ConnectionType type) {
-  if (type == net::NetworkChangeNotifier::GetConnectionType())
+void PepperNetworkMonitorHost::OnConnectionChanged(
+    network::mojom::ConnectionType type) {
+  auto current_type = network::mojom::ConnectionType::CONNECTION_UNKNOWN;
+  network_connection_tracker_->GetConnectionType(&current_type,
+                                                 base::DoNothing());
+  if (type == current_type)
     GetAndSendNetworkList();
 }
 
@@ -80,8 +86,19 @@ void PepperNetworkMonitorHost::OnPermissionCheckResult(
     return;
   }
 
-  net::NetworkChangeNotifier::AddNetworkChangeObserver(this);
+  base::PostTaskWithTraitsAndReplyWithResult(
+      FROM_HERE, {BrowserThread::UI},
+      base::BindOnce(&content::GetNetworkConnectionTracker),
+      base::BindOnce(&PepperNetworkMonitorHost::SetNetworkConnectionTracker,
+                     weak_factory_.GetWeakPtr()));
   GetAndSendNetworkList();
+}
+
+void PepperNetworkMonitorHost::SetNetworkConnectionTracker(
+    network::NetworkConnectionTracker* network_connection_tracker) {
+  DCHECK_EQ(network_connection_tracker_, nullptr);
+  network_connection_tracker_ = network_connection_tracker;
+  network_connection_tracker_->AddNetworkConnectionObserver(this);
 }
 
 void PepperNetworkMonitorHost::GetAndSendNetworkList() {
--- content/browser/renderer_host/pepper/pepper_network_monitor_host.h
+++ content/browser/renderer_host/pepper/pepper_network_monitor_host.h
@@ -10,10 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "content/common/content_export.h"
-#include "net/base/network_change_notifier.h"
 #include "net/base/network_interfaces.h"
 #include "ppapi/host/host_message_context.h"
 #include "ppapi/host/resource_host.h"
+#include "services/network/public/cpp/network_connection_tracker.h"
 
 namespace content {
 
@@ -22,7 +22,7 @@ class BrowserPpapiHostImpl;
 // The host for PPB_NetworkMonitor. This class lives on the IO thread.
 class CONTENT_EXPORT PepperNetworkMonitorHost
     : public ppapi::host::ResourceHost,
-      public net::NetworkChangeNotifier::NetworkChangeObserver {
+      public network::NetworkConnectionTracker::NetworkConnectionObserver {
  public:
   PepperNetworkMonitorHost(BrowserPpapiHostImpl* host,
                            PP_Instance instance,
@@ -30,17 +30,20 @@ class CONTENT_EXPORT PepperNetworkMonitorHost
 
   ~PepperNetworkMonitorHost() override;
 
-  // net::NetworkChangeNotifier::NetworkChangeObserver interface.
-  void OnNetworkChanged(
-      net::NetworkChangeNotifier::ConnectionType type) override;
+  // network::NetworkConnectionTracker::NetworkConnectionObserver interface.
+  void OnConnectionChanged(network::mojom::ConnectionType type) override;
 
  private:
   void OnPermissionCheckResult(bool can_use_network_monitor);
+  void SetNetworkConnectionTracker(
+      network::NetworkConnectionTracker* network_connection_tracker);
 
   void GetAndSendNetworkList();
   void SendNetworkList(std::unique_ptr<net::NetworkInterfaceList> list);
 
   ppapi::host::ReplyMessageContext reply_context_;
+
+  network::NetworkConnectionTracker* network_connection_tracker_;
 
   base::WeakPtrFactory<PepperNetworkMonitorHost> weak_factory_;
 
