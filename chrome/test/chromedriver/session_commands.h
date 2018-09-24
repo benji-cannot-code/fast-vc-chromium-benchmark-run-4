7520ca0bb72c8a7e84fe8247a386d66bdceb19be
Paul Meyer <paulmeyer@chromium.org>
2018-09-24 17:11:30 +0000

    [ChromeDriver] Implement GenerateTestReport.
    Spec: https://w3c.github.io/reporting/#generate-test-report-command
    
    Change-Id: Ia6932007db49eb11c005752b8eace6d7f1267e4f
    Reviewed-on: https://chromium-review.googlesource.com/1221694
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Paul Meyer <paulmeyer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593576}

--- chrome/test/chromedriver/session_commands.h
+++ chrome/test/chromedriver/session_commands.h
@@ -196,4 +196,8 @@ Status ExecuteDeleteScreenOrientation(Session* session,
                                       const base::DictionaryValue& params,
                                       std::unique_ptr<base::Value>* value);
 
+Status ExecuteGenerateTestReport(Session* session,
+                                 const base::DictionaryValue& params,
+                                 std::unique_ptr<base::Value>* value);
+
 #endif  // CHROME_TEST_CHROMEDRIVER_SESSION_COMMANDS_H_
