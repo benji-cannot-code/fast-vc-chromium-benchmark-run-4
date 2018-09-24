c1c7ec0268c0443f3f834d9de18ecf236eca15ae
Andrew Grieve <agrieve@chromium.org>
2018-09-24 16:22:03 +0000

    SuperSize: Rename .html files. data_url->load_url. Delete demo.ndjson
    Bug: 880671
    Change-Id: I5ed6803daf97609fe2814618f51d8199f8c44390
    Reviewed-on: https://chromium-review.googlesource.com/1240185
    Commit-Queue: agrieve <agrieve@chromium.org>
    Reviewed-by: Eric Stevenson <estevenson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593558}

--- tools/binary_size/libsupersize/static/tree-worker.js
+++ tools/binary_size/libsupersize/static/tree-worker.js
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 importScripts('./shared.js');
 
 const _PATH_SEP = '/';
-const _DEMO_DATA_URL = 'demo.ndjson';
 const _NAMES_TO_FLAGS = Object.freeze({
   hot: _FLAGS.HOT,
   generated: _FLAGS.GENERATED_SOURCE,
@@ -601,7 +600,7 @@ class DataFetcher {
 function parseOptions(options) {
   const params = new URLSearchParams(options);
 
-  const url = params.get('data_url');
+  const url = params.get('load_url');
   const groupBy = params.get('group_by') || 'source_path';
   const methodCountMode = params.has('method_count');
   const filterGeneratedFiles = params.has('generated_filter');
@@ -794,19 +793,10 @@ const actions = {
   /** @param {{input:string|null,options:string}} param0 */
   load({input, options}) {
     const {groupBy, filterTest, highlightTest, url} = parseOptions(options);
-    if (input === 'from-url://') {
-      if (url) {
-        // Display the data from the `data_url` query parameter
-        console.info('Displaying data from', url);
-        fetcher.setInput(url);
-      } else {
-        // Display starter content if nothing was specified.
-        console.info('Displaying demo data');
-        // The demo file only exists in the GCS bucket where the UI is hosted.
-        // When using `start_server`, no data is shown until the user uploads
-        // something.
-        fetcher.setInput(_DEMO_DATA_URL);
-      }
+    if (input === 'from-url://' && url) {
+      // Display the data from the `load_url` query parameter
+      console.info('Displaying data from', url);
+      fetcher.setInput(url);
     } else if (input != null) {
       console.info('Displaying uploaded data');
       fetcher.setInput(input);
--- tools/binary_size/libsupersize/templates/sw.js
+++ tools/binary_size/libsupersize/templates/sw.js
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 const cacheName = '{{cache_hash}}';
 const filesToCache = [
   'favicon.ico',
-  'index.html',
+  'viewer.html',
   'infocard-ui.js',
   'infocard.css',
   'main.css',
