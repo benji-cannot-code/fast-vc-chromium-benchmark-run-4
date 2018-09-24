53c2ac704e79cae428e1163ae843c6c81fc9b1ab
Findit <findit-for-me@appspot.gserviceaccount.com>
2018-09-24 22:05:36 +0000

    Revert "Remove SkColorSpaceXform usage from Canvas code"
    This reverts commit 267cb8af204bec0c8ab16b956ff3befec2790fca.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 593674 as the
    culprit for failures in the build cycles as shown on:
    https://findit-for-me.appspot.com/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtLzI2N2NiOGFmMjA0YmVjMGM4YWIxNmI5NTZmZjNiZWZlYzI3OTBmY2EM
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium.linux/linux-jumbo-rel/7066
    
    Sample Failed Step: compile
    
    Original change's description:
    > Remove SkColorSpaceXform usage from Canvas code
    > 
    > This is the last CL to remove SkColorSpaceXform call sites from
    > third_party/blink. This change also does some refactoring in
    > ImageBitmap unit tests, etc.
    > 
    > TBR=fserb@chromium.org
    > 
    > Bug: 774520
    > Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_slimming_paint_v2;luci.chromium.try:win_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    > Change-Id: I8279f1687ef096ae9aa2179c74d5d6cf2364ce9b
    > Reviewed-on: https://chromium-review.googlesource.com/1237140
    > Reviewed-by: Mohammad Reza Zakerinasab <zakerinasab@chromium.org>
    > Reviewed-by: Mike Klein <mtklein@chromium.org>
    > Commit-Queue: Mohammad Reza Zakerinasab <zakerinasab@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#593674}
    
    Change-Id: I8fd0d3dcd6e2e5e4eb8251485ce07451bf8f11e2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 774520
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_slimming_paint_v2;luci.chromium.try:win_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Reviewed-on: https://chromium-review.googlesource.com/1241762
    Cr-Commit-Position: refs/heads/master@{#593706}

--- third_party/blink/renderer/platform/graphics/gpu/drawing_buffer.cc
+++ third_party/blink/renderer/platform/graphics/gpu/drawing_buffer.cc
@@ -58,7 +58,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/wtf/functional.h"
 #include "third_party/blink/renderer/platform/wtf/typed_arrays/array_buffer_contents.h"
-#include "third_party/skia/include/core/SkPixmap.h"
+#include "third_party/skia/include/core/SkColorSpaceXform.h"
 #include "third_party/skia/include/core/SkSurface.h"
 #include "third_party/skia/include/gpu/GrContext.h"
 #include "third_party/skia/include/gpu/gl/GrGLTypes.h"
@@ -1368,15 +1368,15 @@ void DrawingBuffer::ReadBackFramebuffer(unsigned char* pixels,
   }
 
   if (op == WebGLImageConversion::kAlphaDoPremultiply) {
-    auto color_type = kRGBA_8888_SkColorType;
+    std::unique_ptr<SkColorSpaceXform> xform =
+        SkColorSpaceXform::New(SkColorSpace::MakeSRGBLinear().get(),
+                               SkColorSpace::MakeSRGBLinear().get());
+    SkColorSpaceXform::ColorFormat color_format =
+        SkColorSpaceXform::ColorFormat::kRGBA_8888_ColorFormat;
     if (data_type != GL_UNSIGNED_BYTE)
-      color_type = kRGBA_F16_SkColorType;
-    const auto src =
-        SkImageInfo::Make(width, height, color_type, kUnpremul_SkAlphaType);
-    const auto dst =
-        SkImageInfo::Make(width, height, color_type, kPremul_SkAlphaType);
-    SkPixmap{src, pixels, src.minRowBytes()}.readPixels(
-        SkPixmap{dst, pixels, dst.minRowBytes()});
+      color_format = SkColorSpaceXform::ColorFormat::kRGBA_F16_ColorFormat;
+    xform->apply(color_format, pixels, color_format, pixels, width * height,
+                 kPremul_SkAlphaType);
   } else if (op != WebGLImageConversion::kAlphaDoNothing) {
     NOTREACHED();
   }
