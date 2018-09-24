90f778c34e00a224b4f961c6ff6bde8fb4fea268
Jeremy Klein <jlklein@google.com>
2018-09-24 23:19:26 +0000

    Install the Messages PWA when trying to launch it.
    This allows for another forced retry point for when installation fails.
    If a user goes into multidevice settings and clicks the "set up" button
    for Android Messages, it should install the PWA if it hasn't already
    been installed before trying to launch it.
    
    Bug: 874605
    Change-Id: I319067d100712b84b4161f5ee281179bd8acbf12
    Reviewed-on: https://chromium-review.googlesource.com/1240205
    Commit-Queue: Jeremy Klein <jlklein@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593736}

--- chromeos/services/multidevice_setup/public/cpp/fake_android_sms_app_helper_delegate.h
+++ chromeos/services/multidevice_setup/public/cpp/fake_android_sms_app_helper_delegate.h
@@ -21,7 +21,7 @@ class FakeAndroidSmsAppHelperDelegate : public AndroidSmsAppHelperDelegate {
 
   // AndroidSmsAppHelperDelegate:
   void InstallAndroidSmsApp() override;
-  bool LaunchAndroidSmsApp() override;
+  void InstallAndLaunchAndroidSmsApp() override;
 
  private:
   bool has_installed_ = false;
