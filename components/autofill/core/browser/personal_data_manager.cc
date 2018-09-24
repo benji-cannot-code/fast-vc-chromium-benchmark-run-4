cd4c4bd649fef54779422d9297c515fc946571a7
Tien Mai <tienmai@chromium.org>
2018-09-24 16:50:44 +0000

    [Autofill Views] Adding 2-line dropdown display experiment
    Adds the new two line suggestion display experiment argument for
    credit card and profile autofill drop downs. This new experiment
    argument will allow autofill drop downs to show two lines worth of
    information for autofill suggestions as well as a leading icon
    for the given suggestion (if any).
    
    Bug: 870342
    Change-Id: Iaa303bafea5fe09f81538d452e2f6871c0d4282a
    
    Screenshots (Googlers-only): https://drive.google.com/drive/folders/1XDzDE9GiOt4bAG_kDXZDaiJbbBvuTwPX?usp=sharing
    
    Change-Id: Iaa303bafea5fe09f81538d452e2f6871c0d4282a
    Reviewed-on: https://chromium-review.googlesource.com/1234968
    Commit-Queue: Tien Mai <tienmai@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593569}

--- components/autofill/core/browser/personal_data_manager.cc
+++ components/autofill/core/browser/personal_data_manager.cc
@@ -1409,8 +1409,11 @@ std::vector<Suggestion> PersonalDataManager::GetProfileSuggestions(
       unique_matched_profiles, &other_field_types, type.GetStorableType(), 1,
       app_locale_, &labels);
   DCHECK_EQ(unique_suggestions.size(), labels.size());
-  for (size_t i = 0; i < labels.size(); i++)
+  for (size_t i = 0; i < labels.size(); i++) {
     unique_suggestions[i].label = labels[i];
+    // Used when two-line display is enabled.
+    unique_suggestions[i].additional_label = labels[i];
+  }
 
   return unique_suggestions;
 }
@@ -2162,6 +2165,9 @@ std::vector<Suggestion> PersonalDataManager::GetSuggestionsForCards(
         suggestion->value = credit_card->NetworkOrBankNameAndLastFourDigits();
         suggestion->label = credit_card->GetInfo(
             AutofillType(CREDIT_CARD_EXP_DATE_2_DIGIT_YEAR), app_locale_);
+        // The additional label will be used if two-line display is enabled.
+        suggestion->additional_label =
+            credit_card->DescriptiveExpiration(app_locale_);
       } else if (credit_card->number().empty()) {
         if (type.GetStorableType() != CREDIT_CARD_NAME_FULL) {
           suggestion->label = credit_card->GetInfo(
@@ -2175,6 +2181,13 @@ std::vector<Suggestion> PersonalDataManager::GetSuggestionsForCards(
         suggestion->label = credit_card->NetworkOrBankNameAndLastFourDigits();
 #else
         suggestion->label = credit_card->ObfuscatedLastFourDigits();
+        // Ad the card number with expiry information in the additional
+        // label portion so that we an show it when two-line display is
+        // enabled.
+        suggestion->additional_label =
+            credit_card
+                ->NetworkOrBankNameLastFourDigitsAndDescriptiveExpiration(
+                    app_locale_);
 #endif
       }
     }
