6c5aaa7210a7ff97f1a7f8eb16e77f5b6b0e15ed
Aaron Leventhal <aleventhal@chromium.org>
2018-09-24 14:37:14 +0000

    Only fire caret location change if it actually changes
    Also add tests for caret events.
    
    Bug: 877487
    Change-Id: Ib9ac49e60c218169e8108ef83b000fcaaf176570
    Reviewed-on: https://chromium-review.googlesource.com/1189184
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593533}

--- ui/accessibility/platform/ax_system_caret_win.h
+++ ui/accessibility/platform/ax_system_caret_win.h
@@ -32,6 +32,7 @@ class AX_EXPORT AXSystemCaretWin : private AXPlatformNodeDelegateBase {
 
   Microsoft::WRL::ComPtr<IAccessible> GetCaret() const;
   void MoveCaretTo(const gfx::Rect& bounds);
+  void Hide();
 
  private:
   // |AXPlatformNodeDelegate| members.
