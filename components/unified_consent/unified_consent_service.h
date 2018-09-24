e8151f6414b01b1245ce35429b318d55e8b65d93
Thomas Tangl <tangltom@chromium.org>
2018-09-24 11:22:36 +0000

    [unified-consent] Post task when changing sync settings
    Instead of synchronously invoking SyncService::OnUserChoseDatatypes
    in the UnifiedConsentService, a task is posted in the following cases:
     - When called through |SyncServiceObserver::OnStateChanged|
     - During Migration
     - During Rollback
    
    This fixes
     - A crash in SingleClientWalletSyncTest.DownloadAccountStorage_Card
     when UnifiedConsent is enabled.
     - Issues on the settings page for custom passphrase users (the page
     didn't refresh to visibly turn off the unified consent toggle).
    
    Bug: 886912, 887706
    Change-Id: I4b97c9b862c7495a04e329f4b9f791e21fee693a
    Reviewed-on: https://chromium-review.googlesource.com/1238499
    Commit-Queue: Thomas Tangl <tangltom@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593504}

--- components/unified_consent/unified_consent_service.h
+++ components/unified_consent/unified_consent_service.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/observer_list.h"
 #include "components/keyed_service/core/keyed_service.h"
+#include "components/sync/base/model_type.h"
 #include "components/sync/driver/sync_service_observer.h"
 #include "components/unified_consent/unified_consent_service_client.h"
 #include "services/identity/public/cpp/identity_manager.h"
@@ -132,13 +133,21 @@ class UnifiedConsentService : public KeyedService,
   // syncer::SyncServiceObserver:
   void OnStateChanged(syncer::SyncService* sync) override;
 
+  // Updates the sync settings if sync isn't disabled and the sync engine is
+  // initialized.
+  void UpdateSyncSettingsIfPossible(
+      bool sync_everything,
+      syncer::ModelTypeSet sync_data_types = syncer::UserSelectableTypes());
+
+  // Posts a task to call |UpdateSyncSettingsIfPossible|.
+  void PostTaskToUpdateSyncSettings(
+      bool sync_everything,
+      syncer::ModelTypeSet sync_data_types = syncer::UserSelectableTypes());
+
   // Called when |prefs::kUnifiedConsentGiven| pref value changes.
   // When set to true, it enables syncing of all data types and it enables all
   // non-personalized services. Otherwise it does nothing.
   void OnUnifiedConsentGivenPrefChanged();
-
-  // Enables/disables syncing everything if the sync engine is initialized.
-  void SetSyncEverythingIfPossible(bool sync_everything);
 
   // Migration helpers.
   MigrationState GetMigrationState();
@@ -158,10 +167,6 @@ class UnifiedConsentService : public KeyedService,
   // Checks if all on-by-default non-personalized services are on.
   bool AreAllOnByDefaultPrivacySettingsOn();
 
-  // Helper that checks whether it's okay to call
-  // |SyncService::OnUserChoseDatatypes|.
-  bool IsSyncConfigurable();
-
   // Records a sample for each bucket enabled by the user (except kNone).
   // kNone is recorded when none of the other buckets are recorded.
   void RecordSettingsHistogram();
@@ -178,6 +183,8 @@ class UnifiedConsentService : public KeyedService,
   syncer::SyncService* sync_service_;
 
   std::unique_ptr<PrefChangeRegistrar> pref_change_registrar_;
+
+  base::WeakPtrFactory<UnifiedConsentService> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(UnifiedConsentService);
 };
