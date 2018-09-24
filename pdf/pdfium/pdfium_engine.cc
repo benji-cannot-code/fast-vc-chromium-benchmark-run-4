bb53cc33c5424acac14efff451111aee73c9698c
Lei Zhang <thestig@chromium.org>
2018-09-24 19:05:51 +0000

    Merge PDFiumPrint's public PrintPages methods.
    Split raster PDF creation into its own function. Also add a helper
    function to consistently check for PDFs with no pages.
    
    Change-Id: I693282460b4da0e80bd1c5f2c022372bb7d58bb1
    Reviewed-on: https://chromium-review.googlesource.com/1238353
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593623}

--- pdf/pdfium/pdfium_engine.cc
+++ pdf/pdfium/pdfium_engine.cc
@@ -1152,8 +1152,8 @@ pp::Buffer_Dev PDFiumEngine::PrintPagesAsRasterPdf(
   g_last_instance_id = client_->GetPluginInstance()->pp_instance();
 #endif
 
-  return print_.PrintPagesAsRasterPdf(page_ranges, page_range_count,
-                                      print_settings, pdf_print_settings);
+  return print_.PrintPagesAsPdf(page_ranges, page_range_count, print_settings,
+                                pdf_print_settings, /*raster=*/true);
 }
 
 pp::Buffer_Dev PDFiumEngine::PrintPagesAsPdf(
@@ -1176,7 +1176,7 @@ pp::Buffer_Dev PDFiumEngine::PrintPagesAsPdf(
   }
 
   return print_.PrintPagesAsPdf(page_ranges, page_range_count, print_settings,
-                                pdf_print_settings);
+                                pdf_print_settings, /*raster=*/false);
 }
 
 void PDFiumEngine::KillFormFocus() {
