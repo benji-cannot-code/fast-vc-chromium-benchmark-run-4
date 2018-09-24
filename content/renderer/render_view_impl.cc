7ae0f0490ff5b6ba6883bb2ad9e6132166fdb3c7
Kevin McNee <mcnee@chromium.org>
2018-09-24 16:22:00 +0000

    Small cleanup for double-tap to zoom on desktop
    We update/remove comments that claim that double-tap to zoom is specific
    to Android.
    
    We also enable double-tap to zoom on Mac via WebPreferences rather than
    hardcoding it as always enabled.
    
    Bug: 802369
    Change-Id: Ib78a0a85f295166bff3363ed65dceca9e5e3468e
    Reviewed-on: https://chromium-review.googlesource.com/1235129
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Rick Byers <rbyers@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593557}

--- content/renderer/render_view_impl.cc
+++ content/renderer/render_view_impl.cc
@@ -1010,7 +1010,6 @@ void RenderView::ApplyWebPreferences(const WebPreferences& prefs,
   }
 
 #if defined(OS_MACOSX)
-  settings->SetDoubleTapToZoomEnabled(true);
   web_view->SetMaximumLegibleScale(prefs.default_maximum_page_scale_factor);
 #endif
 
