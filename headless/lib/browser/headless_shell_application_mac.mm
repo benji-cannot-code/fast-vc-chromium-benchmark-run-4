5d6038d42dac0038d65ca53b2232362e6c3893f1
erikchen <erikchen@chromium.org>
2018-09-24 17:08:02 +0000

    Add support for native event processor observer to headless mode.
    The hooks are currently required by the jank calculator. They will eventually be
    the mechanism that the browser task scheduler hooks into native events.
    
    Bug: 859155
    Change-Id: I4b633c7b350c673c7b59cdcfe3a115ae74e5bacb
    Reviewed-on: https://chromium-review.googlesource.com/1238777
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593575}

--- headless/lib/browser/headless_shell_application_mac.mm
+++ headless/lib/browser/headless_shell_application_mac.mm
@@ -6,6 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "headless/lib/browser/headless_shell_application_mac.h"
 
 #include "base/auto_reset.h"
+#include "base/observer_list.h"
+#include "content/public/browser/native_event_processor_mac.h"
+#include "content/public/browser/native_event_processor_observer_mac.h"
+
+@interface HeadlessShellCrApplication ()<NativeEventProcessor> {
+  base::ObserverList<content::NativeEventProcessorObserver>::Unchecked
+      observers_;
+}
+@end
 
 @implementation HeadlessShellCrApplication
 
@@ -16,6 +25,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // This method is required to allow handling some input events on Mac OS.
 - (void)setHandlingSendEvent:(BOOL)handlingSendEvent {
+}
+
+- (void)sendEvent:(NSEvent*)event {
+  content::ScopedNotifyNativeEventProcessorObserver scopedObserverNotifier(
+      &observers_, event);
+  [super sendEvent:event];
+}
+
+- (void)addNativeEventProcessorObserver:
+    (content::NativeEventProcessorObserver*)observer {
+  observers_.AddObserver(observer);
+}
+
+- (void)removeNativeEventProcessorObserver:
+    (content::NativeEventProcessorObserver*)observer {
+  observers_.RemoveObserver(observer);
 }
 
 @end
