28d9cc70ec4825290a34c248de4370102793100b
Mike Wasserman <msw@chromium.org>
2018-09-24 19:27:14 +0000

    ash: Remove *LimitedPreTargetHandlerForWindow helper functions
    These were added for deprecated Mash architecture plans.
    Inline calls to [Add/Remove]PreTargetHandler instead.
    
    Bug: 866529
    Test: No shelf tooltip nor double-click-to-resize-window changes.
    Change-Id: Ic4d376652e1e128c979cfdb6f5f219a2ed76f6ad
    Reviewed-on: https://chromium-review.googlesource.com/1240212
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593628}

--- ash/shelf/shelf_tooltip_manager.cc
+++ ash/shelf/shelf_tooltip_manager.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shelf/shelf_tooltip_preview_bubble.h"
 #include "ash/shelf/shelf_view.h"
 #include "ash/shell.h"
-#include "ash/wm/window_util.h"
 #include "base/bind.h"
 #include "base/strings/string16.h"
 #include "base/threading/thread_task_runner_handle.h"
@@ -42,16 +41,12 @@ ShelfTooltipManager::ShelfTooltipManager(ShelfView* shelf_view)
 ShelfTooltipManager::~ShelfTooltipManager() {
   Shell::Get()->RemovePointerWatcher(this);
   shelf_view_->shelf()->RemoveObserver(this);
-  aura::Window* window = nullptr;
-  if (shelf_view_->GetWidget())
-    window = shelf_view_->GetWidget()->GetNativeWindow();
-  if (window)
-    wm::RemoveLimitedPreTargetHandlerForWindow(this, window);
+  if (shelf_view_->GetWidget() && shelf_view_->GetWidget()->GetNativeWindow())
+    shelf_view_->GetWidget()->GetNativeWindow()->RemovePreTargetHandler(this);
 }
 
 void ShelfTooltipManager::Init() {
-  wm::AddLimitedPreTargetHandlerForWindow(
-      this, shelf_view_->GetWidget()->GetNativeWindow());
+  shelf_view_->GetWidget()->GetNativeWindow()->AddPreTargetHandler(this);
 }
 
 void ShelfTooltipManager::Close() {
