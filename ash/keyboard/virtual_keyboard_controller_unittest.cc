ae3083177bd21e4519f944fdc85218e0ce93886e
Xiaoqian Dai <xdai@chromium.org>
2018-09-24 20:56:43 +0000

    TabletModeController refactoring II.
    Refactoring of the internal input events blocker in
    TabletModeController.
    
    The input events should only be blocked if 1) we're currently in tablet
    mode or 2) we're currently in laptop mode but the lid is flipped over,
    i.e., we are in laptop mode because of an attached external mouse.
    
    Bug: 887042
    Change-Id: I8be03c684265d79ccc3499362a7154eb44c4dec0
    Reviewed-on: https://chromium-review.googlesource.com/1237407
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593671}

--- ash/keyboard/virtual_keyboard_controller_unittest.cc
+++ ash/keyboard/virtual_keyboard_controller_unittest.cc
@@ -358,7 +358,7 @@ TEST_F(VirtualKeyboardControllerAutoTest, EnabledDuringTabletMode) {
   TabletModeControllerTestApi().EnterTabletMode();
   ASSERT_TRUE(keyboard::IsKeyboardEnabled());
   // Toggle tablet mode off.
-  TabletModeControllerTestApi().LeaveTabletMode(false);
+  TabletModeControllerTestApi().LeaveTabletMode();
   ASSERT_FALSE(keyboard::IsKeyboardEnabled());
 }
 
@@ -401,7 +401,7 @@ TEST_F(VirtualKeyboardControllerAutoTest, SuppressedInTabletMode) {
   ASSERT_TRUE(notified());
   ASSERT_FALSE(IsVirtualKeyboardSuppressed());
   // Toggle tablet mode oFF.
-  TabletModeControllerTestApi().LeaveTabletMode(false);
+  TabletModeControllerTestApi().LeaveTabletMode();
   ASSERT_FALSE(keyboard::IsKeyboardEnabled());
 }
 
