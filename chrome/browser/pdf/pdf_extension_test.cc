f34bc5eea1fff5ab4948f206c335324abfe695b3
Robert Sesek <rsesek@chromium.org>
2018-09-25 17:25:59 +0000

    Remove a ScopedMacViewsBrowserMode from PDFExtensionHitTestTest.
    Bug: 845389
    Change-Id: I33de543963096de0a4473252cc4017aa52b4a981
    Reviewed-on: https://chromium-review.googlesource.com/1243623
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593984}

--- chrome/browser/pdf/pdf_extension_test.cc
+++ chrome/browser/pdf/pdf_extension_test.cc
@@ -41,7 +41,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/test/base/ui_test_utils.h"
-#include "chrome/test/views/scoped_macviews_browser_mode.h"
 #include "components/content_settings/core/browser/host_content_settings_map.h"
 #include "components/download/public/common/download_item.h"
 #include "components/viz/common/features.h"
@@ -381,10 +380,6 @@ class PDFExtensionHitTestTest : public PDFExtensionTest,
   }
 
   base::test::ScopedFeatureList feature_list_;
-
-#if defined(OS_MACOSX)
-  test::ScopedMacViewsBrowserMode cocoa_browser_mode_{false};
-#endif
 };
 
 // Disabled because it's flaky.
