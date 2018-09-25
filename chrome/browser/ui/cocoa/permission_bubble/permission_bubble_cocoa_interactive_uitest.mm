3e9b30f629923e0047c2fe6a6f55afd1f8757d41
Nico Weber <thakis@chromium.org>
2018-09-25 13:23:30 +0000

    mac: Remove unused KeepBubbleAnchored().
    Bug: 832676
    Change-Id: I97bdc876134729f448a6f911980e63cebde9db6b
    Reviewed-on: https://chromium-review.googlesource.com/1242336
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593911}

--- chrome/browser/ui/cocoa/permission_bubble/permission_bubble_cocoa_interactive_uitest.mm
+++ chrome/browser/ui/cocoa/permission_bubble/permission_bubble_cocoa_interactive_uitest.mm
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/browser_window.h"
-#include "chrome/browser/ui/cocoa/bubble_anchor_helper_views.h"
 #import "chrome/browser/ui/cocoa/location_bar/location_bar_decoration.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/test/base/in_process_browser_test.h"
