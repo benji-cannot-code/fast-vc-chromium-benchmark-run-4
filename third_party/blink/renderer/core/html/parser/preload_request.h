23eed0635fdbf9a0bcfb95fd551487f750f32537
rajendrant <rajendrant@chromium.org>
2018-09-25 22:06:03 +0000

    LazyLoad: Avoid fetching placeholder of small images with dimensions specified
    Tracking pixel images are small in general and would fit in the 2KB
    placeholder image. But it could be double fetched if it gets near the
    viewport, before the placeholder range response is received.
    
    One heuristic is to avoid lazyloading image elements with small dimensions
    (10x 10) specified in attributes. This CL implements this.
    
    Bug: 875086
    Change-Id: I70149e5d57ee7e6216238d84bbdc9d24ee9114fd
    Reviewed-on: https://chromium-review.googlesource.com/1198562
    Commit-Queue: rajendrant <rajendrant@chromium.org>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#594121}

--- third_party/blink/renderer/core/html/parser/preload_request.h
+++ third_party/blink/renderer/core/html/parser/preload_request.h
@@ -122,8 +122,8 @@ class CORE_EXPORT PreloadRequest {
     return is_image_set_ == ResourceFetcher::kImageIsImageSet;
   }
 
-  void SetIsLazyloadAttrOff(bool is_lazyload_attr_off) {
-    is_lazyload_attr_off_ = is_lazyload_attr_off;
+  void SetIsLazyloadImageDisabled(bool is_lazyload_image_disable) {
+    is_lazyload_image_disabled_ = is_lazyload_image_disable;
   }
 
  private:
@@ -154,7 +154,7 @@ class CORE_EXPORT PreloadRequest {
         referrer_source_(referrer_source),
         from_insertion_scanner_(false),
         is_image_set_(is_image_set),
-        is_lazyload_attr_off_(false) {}
+        is_lazyload_image_disabled_(false) {}
 
   KURL CompleteURL(Document*);
 
@@ -177,7 +177,7 @@ class CORE_EXPORT PreloadRequest {
   IntegrityMetadataSet integrity_metadata_;
   bool from_insertion_scanner_;
   ResourceFetcher::IsImageSet is_image_set_;
-  bool is_lazyload_attr_off_;
+  bool is_lazyload_image_disabled_;
 };
 
 typedef Vector<std::unique_ptr<PreloadRequest>> PreloadRequestStream;
