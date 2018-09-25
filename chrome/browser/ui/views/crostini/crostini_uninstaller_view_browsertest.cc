e8c65a1ced388974face2d63ee61337b44665b20
Josh Pratt <jopra@chromium.org>
2018-09-25 01:00:38 +0000

    Skip VM uninstallation steps if Crostini was not fully installed.
    This CL skips parts of the uninstall flow if the Termina VM was not
    installed. This occurs if installation and uninstallation occurs
    offline.
    
    BUG=884284
    
    Change-Id: I7e669605844b0914948bde792f67891e55ad0e7a
    Reviewed-on: https://chromium-review.googlesource.com/1233016
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Commit-Queue: Josh Pratt <jopra@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593787}

--- chrome/browser/ui/views/crostini/crostini_uninstaller_view_browsertest.cc
+++ chrome/browser/ui/views/crostini/crostini_uninstaller_view_browsertest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "chrome/browser/chromeos/crostini/crostini_manager.h"
+#include "chrome/browser/chromeos/crostini/crostini_pref_names.h"
 #include "chrome/browser/chromeos/crostini/crostini_util.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/app_list/app_list_client_impl.h"
@@ -21,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/dbus/fake_concierge_client.h"
 #include "components/crx_file/id_util.h"
+#include "components/prefs/pref_service.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/views/window/dialog_client_view.h"
 
@@ -84,8 +86,26 @@ class CrostiniUninstallerViewBrowserTest : public CrostiniDialogBrowserTest {
   WaitingFakeConciergeClient* waiting_fake_concierge_client_;
 
  private:
-
   DISALLOW_COPY_AND_ASSIGN(CrostiniUninstallerViewBrowserTest);
+};
+
+class CrostiniUninstalledUninstallerViewBrowserTest
+    : public CrostiniUninstallerViewBrowserTest {
+ public:
+  CrostiniUninstalledUninstallerViewBrowserTest() = default;
+
+  void SetUpOnMainThread() override {
+    browser()->profile()->GetPrefs()->SetBoolean(
+        crostini::prefs::kCrostiniEnabled, true);
+    // Skips installing the Cros-Termina component.
+  }
+
+  void InitCrosTermina() override {
+    // Skips setting up the Cros-Termina component.
+  }
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(CrostiniUninstalledUninstallerViewBrowserTest);
 };
 
 // Test the dialog is actually launched from the app launcher.
@@ -108,6 +128,33 @@ IN_PROC_BROWSER_TEST_F(CrostiniUninstallerViewBrowserTest, UninstallFlow) {
   EXPECT_FALSE(ActiveView()->GetWidget()->IsClosed());
   EXPECT_FALSE(HasAcceptButton());
   EXPECT_FALSE(HasCancelButton());
+
+  WaitForViewDestroyed();
+
+  histogram_tester.ExpectBucketCount(
+      "Crostini.UninstallResult",
+      static_cast<base::HistogramBase::Sample>(
+          CrostiniUninstallerView::UninstallResult::kSuccess),
+      1);
+}
+
+IN_PROC_BROWSER_TEST_F(CrostiniUninstalledUninstallerViewBrowserTest,
+                       OfflineUninstallFlowWithoutTermina) {
+  base::HistogramTester histogram_tester;
+
+  SetConnectionType(net::NetworkChangeNotifier::CONNECTION_NONE);
+  base::RunLoop().RunUntilIdle();
+
+  ShowUi("default");
+  EXPECT_NE(nullptr, ActiveView());
+  EXPECT_FALSE(ActiveView()->GetWidget()->IsClosed());
+  EXPECT_EQ(ui::DIALOG_BUTTON_OK | ui::DIALOG_BUTTON_CANCEL,
+            ActiveView()->GetDialogButtons());
+
+  EXPECT_TRUE(HasAcceptButton());
+  EXPECT_TRUE(HasCancelButton());
+
+  ActiveView()->GetDialogClientView()->AcceptWindow();
 
   WaitForViewDestroyed();
 
