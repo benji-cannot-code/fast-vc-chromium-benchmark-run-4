be587108265b04205fec6cdf2494a34d7ee5be95
Etienne Pierre-Doray <etiennep@chromium.org>
2018-09-25 16:04:19 +0000

    [TaskScheduler] Add documentation for  ScopedBlockingCall.
    This CL defines "blocking call" in scoped_blocking_call.h and mentions
    that ScopedBlockingCall serves as annotation for the TaskScheduler.
    
    Bug: 874080
    Change-Id: Id85a42eb4750620b26c587adf5a43f471ea1168c
    Reviewed-on: https://chromium-review.googlesource.com/1236459
    Reviewed-by: François Doray <fdoray@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593956}

--- base/threading/scoped_blocking_call.h
+++ base/threading/scoped_blocking_call.h
@@ -11,6 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 
+// A "blocking call" refers to any call that causes the calling thread to wait
+// off-CPU. It includes but is not limited to calls that wait on synchronous
+// file I/O operations: read or write a file from disk, interact with a pipe or
+// a socket, rename or delete a file, enumerate files in a directory, etc.
+// Acquiring a low contention lock is not considered a blocking call.
+
 // BlockingType indicates the likelihood that a blocking call will actually
 // block.
 enum class BlockingType {
@@ -47,14 +53,16 @@ class BASE_EXPORT UncheckedScopedBlockingCall {
 
 }  // namespace internal
 
-// This class must be instantiated in every scope where a blocking call is made.
-// When a ScopedBlockingCall is instantiated, it asserts that blocking calls are
-// allowed in its scope with a call to base::AssertBlockingAllowed(). CPU usage
-// should be minimal within that scope. //base APIs that block instantiate their
-// own ScopedBlockingCall; it is not necessary to instantiate another
-// ScopedBlockingCall in the scope where these APIs are used. Nested
-// ScopedBlockingCalls are supported (mostly a no-op except for WILL_BLOCK
-// nested within MAY_BLOCK which will result in immediate WILL_BLOCK semantics).
+// This class must be instantiated in every scope where a blocking call is made
+// and serves as a precise annotation of the scope that may/will block for the
+// scheduler. When a ScopedBlockingCall is instantiated, it asserts that
+// blocking calls are allowed in its scope with a call to
+// base::AssertBlockingAllowed(). CPU usage should be minimal within that scope.
+// //base APIs that block instantiate their own ScopedBlockingCall; it is not
+// necessary to instantiate another ScopedBlockingCall in the scope where these
+// APIs are used. Nested ScopedBlockingCalls are supported (mostly a no-op
+// except for WILL_BLOCK nested within MAY_BLOCK which will result in immediate
+// WILL_BLOCK semantics).
 //
 // Good:
 //   Data data;
