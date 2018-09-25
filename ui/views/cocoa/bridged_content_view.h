4050b226cdeae57b6c62f90c4b404cca8f810a2c
Trent Apted <tapted@chromium.org>
2018-09-25 00:18:13 +0000

    MacViews: Suppress post-IME key handling when there is marked IME text.
    When interacting with an IME on Mac, some key sequences cause the
    IME window to dismiss without any change to the text or ongoing
    composition state. There is no method we can intercept when
    -[NSResponder interpretKeyEvents:] does this. Currently, we assume the
    event was unhandled, and it gets processed as an accelerator. This
    may incorrectly cause a search to trigger in the Omnibox.
    
    To fix, suppress post-IME key handling when there is marked IME text.
    That is, if there was marked text at the start of keyDown: processing,
    consider the event handled by IME, regardless of what methods it
    invokes on the NSTextInputClient. Currently, only do this for tab
    and enter.
    
    Bug: 883952
    Change-Id: Ief845a82502437d8b8f5fd3593cf25f87c6447ae
    Reviewed-on: https://chromium-review.googlesource.com/1239975
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Reviewed-by: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593766}

--- ui/views/cocoa/bridged_content_view.h
+++ ui/views/cocoa/bridged_content_view.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string16.h"
 #import "ui/base/cocoa/tool_tip_base_view.h"
 #import "ui/base/cocoa/tracking_area.h"
+#include "ui/views/views_export.h"
 
 namespace ui {
 class TextInputClient;
@@ -23,6 +24,7 @@ class BridgedNativeWidgetImpl;
 // The NSView that sits as the root contentView of the NSWindow, whilst it has
 // a views::RootView present. Bridges requests from Cocoa to the hosted
 // views::View.
+VIEWS_EXPORT
 @interface BridgedContentView : ToolTipBaseView<NSTextInputClient,
                                                 NSUserInterfaceValidations,
                                                 NSDraggingSource,
