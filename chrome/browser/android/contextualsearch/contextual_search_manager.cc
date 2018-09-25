c802121347fc7110b88c9fc83eca39c14b007f66
Antonio Gomes <tonikitoo@igalia.com>
2018-09-25 18:49:18 +0000

    [android] Migrate ContextualSearchDelegate using SimpleURLLoader
    URLFetcher will stop working with advent of Network Service, and
    SimpleURLLoader is the replacement API for most clients.
    This CL migrates Android's ContextualSearchDelegate and the
    respective unittests away from URLFetcher.
    
    Bug: 773295,872875
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: Ifd2f3afd0db3ceaf75f6a8f972259c656ec7db69
    Reviewed-on: https://chromium-review.googlesource.com/1221729
    Commit-Queue: Donn Denman <donnd@chromium.org>
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594025}

--- chrome/browser/android/contextualsearch/contextual_search_manager.cc
+++ chrome/browser/android/contextualsearch/contextual_search_manager.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents_observer.h"
 #include "jni/ContextualSearchManager_jni.h"
 #include "net/url_request/url_fetcher_impl.h"
+#include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "services/service_manager/public/cpp/binder_registry.h"
 #include "services/service_manager/public/cpp/interface_provider.h"
 
@@ -85,7 +86,7 @@ ContextualSearchManager::ContextualSearchManager(JNIEnv* env,
       env, obj, reinterpret_cast<intptr_t>(this));
   Profile* profile = ProfileManager::GetActiveUserProfile();
   delegate_.reset(new ContextualSearchDelegate(
-      profile->GetRequestContext(),
+      profile->GetURLLoaderFactory(),
       TemplateURLServiceFactory::GetForProfile(profile),
       base::Bind(&ContextualSearchManager::OnSearchTermResolutionResponse,
                  base::Unretained(this)),
