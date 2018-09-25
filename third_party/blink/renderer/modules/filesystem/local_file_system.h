3b2030b0b7fa8ae43956195b4f0f954abf574539
Marijn Kruisselbrink <mek@chromium.org>
2018-09-25 22:39:24 +0000

    [FileSystem] Add support for selecting multiple files or directories.
    Bug: 878581
    Change-Id: I65fc0029da2e3951495a6cd96704085e5c1b942c
    Reviewed-on: https://chromium-review.googlesource.com/1227366
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594132}

--- third_party/blink/renderer/modules/filesystem/local_file_system.h
+++ third_party/blink/renderer/modules/filesystem/local_file_system.h
@@ -46,6 +46,7 @@ namespace blink {
 
 class AsyncFileSystemCallbacks;
 class CallbackWrapper;
+class ChooseFileSystemEntriesOptions;
 class FileSystemClient;
 class ExecutionContext;
 class KURL;
@@ -77,7 +78,8 @@ class LocalFileSystem final : public GarbageCollectedFinalized<LocalFileSystem>,
                          std::unique_ptr<AsyncFileSystemCallbacks>,
                          SynchronousType sync_type);
 
-  void ChooseEntry(ScriptPromiseResolver*);
+  void ChooseEntry(ScriptPromiseResolver*,
+                   const ChooseFileSystemEntriesOptions& options);
 
   FileSystemClient& Client() const { return *client_; }
 
