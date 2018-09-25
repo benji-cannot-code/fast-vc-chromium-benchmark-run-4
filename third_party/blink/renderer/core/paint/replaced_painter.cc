6440900e670c1d1bdae4249f1c0014e5018c7376
Luna Lu <loonybear@chromium.org>
2018-09-25 20:01:12 +0000

    Remove uneeded reference in replaced painter
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I196ed40d3d9d452373a2ad7217b1c2ab75147d2b
    Reviewed-on: https://chromium-review.googlesource.com/1244357
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594067}

--- third_party/blink/renderer/core/paint/replaced_painter.cc
+++ third_party/blink/renderer/core/paint/replaced_painter.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/paint/object_painter.h"
 #include "third_party/blink/renderer/core/paint/paint_info.h"
 #include "third_party/blink/renderer/core/paint/paint_layer.h"
-#include "third_party/blink/renderer/core/paint/rounded_inner_rect_clipper.h"
 #include "third_party/blink/renderer/core/paint/scoped_paint_state.h"
 #include "third_party/blink/renderer/core/paint/scrollable_area_painter.h"
 #include "third_party/blink/renderer/core/paint/selection_painting_utils.h"
