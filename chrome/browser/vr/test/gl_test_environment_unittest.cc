ebfb7699b7fd3de071cad36aaa644291e19df7b9
Jeremy Roman <jbroman@chromium.org>
2018-09-25 20:24:30 +0000

    sheriffing: disable vr_pixeltests:UiPixelTest.DrawVrBrowsingMode,GlTestEnvironmentTest.InitializeAndCleanup in msan builds
    Bug: 889186
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:linux_vr;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I673acc7c696802c8c5b29b9e9f4b279c12e0b858
    Tbr: acondor@chromium.org
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/1244364
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594080}

--- chrome/browser/vr/test/gl_test_environment_unittest.cc
+++ chrome/browser/vr/test/gl_test_environment_unittest.cc
@@ -10,7 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace vr {
 
-TEST(GlTestEnvironmentTest, InitializeAndCleanup) {
+#if defined(MEMORY_SANITIZER)
+#define MAYBE_InitializeAndCleanup DISABLED_InitializeAndCleanup
+#else
+#define MAYBE_InitializeAndCleanup InitializeAndCleanup
+#endif
+
+TEST(GlTestEnvironmentTest, MAYBE_InitializeAndCleanup) {
   GlTestEnvironment gl_test_environment(gfx::Size(100, 100));
   EXPECT_NE(gl_test_environment.GetFrameBufferForTesting(), 0u);
   EXPECT_EQ(glGetError(), (GLenum)GL_NO_ERROR);
