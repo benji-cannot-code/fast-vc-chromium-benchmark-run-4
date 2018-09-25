7030a00cc8103040a8feb5cfcf3f55b34697208f
Christopher Cameron <ccameron@chromium.org>
2018-09-25 01:20:08 +0000

    RemoteMacViews: Add about:flags entry for RemoteMacViews
    The feature has been around for a while, but having it in about:flags
    will help development along.
    
    Bug: 859152
    Change-Id: I3da5fff7b57961f3c41a628c69a9b65b69122860
    Reviewed-on: https://chromium-review.googlesource.com/1226298
    Commit-Queue: ccameron <ccameron@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593797}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1771,6 +1771,9 @@ extern const char kEnableWebAuthenticationTouchIdDescription[];
 extern const char kHostedAppsInWindowsName[];
 extern const char kHostedAppsInWindowsDescription[];
 
+extern const char kCreateAppWindowsInAppShimProcessName[];
+extern const char kCreateAppWindowsInAppShimProcessDescription[];
+
 extern const char kMacRTLName[];
 extern const char kMacRTLDescription[];
 
