c7acd518201f3631f49a3025f0ebef4423d2207c
Yang Guo <yangguo@chromium.org>
2018-09-25 19:17:37 +0000

    Replace deprecated uses of v8::String::NewFromUtf8
    R=jochen@chromium.org
    
    Bug: v8:7281
    Change-Id: Icacc1c60f4b0c9c3646f1ea310435ee2f90f0a05
    Reviewed-on: https://chromium-review.googlesource.com/1238534
    Commit-Queue: Yang Guo <yangguo@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594048}

--- extensions/renderer/display_source_custom_bindings.cc
+++ extensions/renderer/display_source_custom_bindings.cc
@@ -92,8 +92,10 @@ void DisplaySourceCustomBindings::StartSession(
   CHECK(sink_id_val->IsInt32());
   const int sink_id = sink_id_val->ToInt32(isolate)->Value();
   if (GetDisplaySession(sink_id)) {
-    isolate->ThrowException(v8::Exception::Error(v8::String::NewFromUtf8(
-        isolate, kSessionAlreadyStarted)));
+    isolate->ThrowException(v8::Exception::Error(
+        v8::String::NewFromUtf8(isolate, kSessionAlreadyStarted,
+                                v8::NewStringType::kNormal)
+            .ToLocalChecked()));
     return;
   }
 
@@ -105,7 +107,9 @@ void DisplaySourceCustomBindings::StartSession(
   if ((video_stream_val->IsNull() || video_stream_val->IsUndefined()) &&
       (audio_stream_val->IsNull() || audio_stream_val->IsUndefined())) {
     isolate->ThrowException(v8::Exception::Error(
-        v8::String::NewFromUtf8(isolate, kInvalidStreamArgs)));
+        v8::String::NewFromUtf8(isolate, kInvalidStreamArgs,
+                                v8::NewStringType::kNormal)
+            .ToLocalChecked()));
     return;
   }
 
@@ -117,7 +121,9 @@ void DisplaySourceCustomBindings::StartSession(
                       .Component();
     if (video_track.IsNull()) {
       isolate->ThrowException(v8::Exception::Error(
-          v8::String::NewFromUtf8(isolate, kInvalidStreamArgs)));
+          v8::String::NewFromUtf8(isolate, kInvalidStreamArgs,
+                                  v8::NewStringType::kNormal)
+              .ToLocalChecked()));
       return;
     }
   }
@@ -127,7 +133,9 @@ void DisplaySourceCustomBindings::StartSession(
                       .Component();
     if (audio_track.IsNull()) {
       isolate->ThrowException(v8::Exception::Error(
-          v8::String::NewFromUtf8(isolate, kInvalidStreamArgs)));
+          v8::String::NewFromUtf8(isolate, kInvalidStreamArgs,
+                                  v8::NewStringType::kNormal)
+              .ToLocalChecked()));
       return;
     }
   }
@@ -156,8 +164,10 @@ void DisplaySourceCustomBindings::StartSession(
   std::unique_ptr<DisplaySourceSession> session =
       DisplaySourceSessionFactory::CreateSession(session_params);
   if (!session) {
-    isolate->ThrowException(v8::Exception::Error(v8::String::NewFromUtf8(
-        isolate, kErrorNotSupported)));
+    isolate->ThrowException(v8::Exception::Error(
+        v8::String::NewFromUtf8(isolate, kErrorNotSupported,
+                                v8::NewStringType::kNormal)
+            .ToLocalChecked()));
     return;
   }
 
@@ -188,8 +198,10 @@ void DisplaySourceCustomBindings::TerminateSession(
   int sink_id = args[0]->ToInt32(args.GetIsolate())->Value();
   DisplaySourceSession* session = GetDisplaySession(sink_id);
   if (!session) {
-    isolate->ThrowException(v8::Exception::Error(v8::String::NewFromUtf8(
-        isolate, kSessionNotFound)));
+    isolate->ThrowException(
+        v8::Exception::Error(v8::String::NewFromUtf8(isolate, kSessionNotFound,
+                                                     v8::NewStringType::kNormal)
+                                 .ToLocalChecked()));
     return;
   }
 
@@ -198,14 +210,18 @@ void DisplaySourceCustomBindings::TerminateSession(
   if (state == DisplaySourceSession::Establishing) {
     // 'session started' callback has not yet been invoked.
     // This session is not existing for the user.
-    isolate->ThrowException(v8::Exception::Error(
-        v8::String::NewFromUtf8(isolate, kSessionNotFound)));
+    isolate->ThrowException(
+        v8::Exception::Error(v8::String::NewFromUtf8(isolate, kSessionNotFound,
+                                                     v8::NewStringType::kNormal)
+                                 .ToLocalChecked()));
     return;
   }
 
   if (state == DisplaySourceSession::Terminating) {
-    isolate->ThrowException(v8::Exception::Error(v8::String::NewFromUtf8(
-        isolate, kSessionAlreadyTerminating)));
+    isolate->ThrowException(v8::Exception::Error(
+        v8::String::NewFromUtf8(isolate, kSessionAlreadyTerminating,
+                                v8::NewStringType::kNormal)
+            .ToLocalChecked()));
     return;
   }
 
@@ -233,7 +249,9 @@ void DisplaySourceCustomBindings::OnCallCompleted(
   if (success)
     callback_args[1] = v8::Null(isolate);
   else
-    callback_args[1] = v8::String::NewFromUtf8(isolate, error_message.c_str());
+    callback_args[1] = v8::String::NewFromUtf8(isolate, error_message.c_str(),
+                                               v8::NewStringType::kNormal)
+                           .ToLocalChecked();
 
   module_system->CallModuleMethodSafe("displaySource", "callCompletionCallback",
                                       2, callback_args);
--- extensions/renderer/object_backed_native_handler.cc
+++ extensions/renderer/object_backed_native_handler.cc
@@ -205,10 +205,13 @@ void ObjectBackedNativeHandler::SetPrivate(v8::Local<v8::Context> context,
                                            v8::Local<v8::Object> obj,
                                            const char* key,
                                            v8::Local<v8::Value> value) {
-  obj->SetPrivate(context, v8::Private::ForApi(context->GetIsolate(),
-                                               v8::String::NewFromUtf8(
-                                                   context->GetIsolate(), key)),
-                  value)
+  obj->SetPrivate(
+         context,
+         v8::Private::ForApi(context->GetIsolate(),
+                             v8::String::NewFromUtf8(context->GetIsolate(), key,
+                                                     v8::NewStringType::kNormal)
+                                 .ToLocalChecked()),
+         value)
       .FromJust();
 }
 
@@ -223,10 +226,12 @@ bool ObjectBackedNativeHandler::GetPrivate(v8::Local<v8::Context> context,
                                            v8::Local<v8::Object> obj,
                                            const char* key,
                                            v8::Local<v8::Value>* result) {
-  return obj->GetPrivate(context,
-                         v8::Private::ForApi(context->GetIsolate(),
-                                             v8::String::NewFromUtf8(
-                                                 context->GetIsolate(), key)))
+  return obj
+      ->GetPrivate(context, v8::Private::ForApi(context->GetIsolate(),
+                                                v8::String::NewFromUtf8(
+                                                    context->GetIsolate(), key,
+                                                    v8::NewStringType::kNormal)
+                                                    .ToLocalChecked()))
       .ToLocal(result);
 }
 
@@ -239,10 +244,12 @@ void ObjectBackedNativeHandler::DeletePrivate(v8::Local<v8::Object> obj,
 void ObjectBackedNativeHandler::DeletePrivate(v8::Local<v8::Context> context,
                                               v8::Local<v8::Object> obj,
                                               const char* key) {
-  obj->DeletePrivate(context,
-                     v8::Private::ForApi(
-                         context->GetIsolate(),
-                         v8::String::NewFromUtf8(context->GetIsolate(), key)))
+  obj->DeletePrivate(
+         context,
+         v8::Private::ForApi(context->GetIsolate(),
+                             v8::String::NewFromUtf8(context->GetIsolate(), key,
+                                                     v8::NewStringType::kNormal)
+                                 .ToLocalChecked()))
       .FromJust();
 }
 
