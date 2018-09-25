4d95522c1d4a17ad5cb3f9a834ec4a675cde5b72
Chris Lu <thegreenfrog@chromium.org>
2018-09-25 18:27:09 +0000

    [ios] Ensure MDCAppBarViewController matches the width of StaticFileViewController.
    Late addition to the docs: https://github.com/material-components/material-components-ios/tree/develop/components/AppBar#migration-guide-mdcappbar-to-mdcappbarviewcontroller
    
    Bug: 874471
    Change-Id: I370379e55e962695a8eabf609972ca5dc39bd211
    Reviewed-on: https://chromium-review.googlesource.com/1239535
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594019}

--- ios/chrome/browser/ui/first_run/static_file_view_controller.mm
+++ ios/chrome/browser/ui/first_run/static_file_view_controller.mm
@@ -84,6 +84,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   // Add the app bar at the end.
   [self addChildViewController:_appBarViewController];
+  // Match the width of the parent view.
+  CGRect frame = _appBarViewController.view.frame;
+  frame.origin.x = 0;
+  frame.size.width =
+      _appBarViewController.parentViewController.view.bounds.size.width;
+  _appBarViewController.view.frame = frame;
   [self.view addSubview:_appBarViewController.view];
   [_appBarViewController didMoveToParentViewController:self];
 
