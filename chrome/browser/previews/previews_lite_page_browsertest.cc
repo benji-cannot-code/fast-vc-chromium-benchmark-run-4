5b8754c92c34efefc19a7aa8a6f86af337c0fa0a
Robert Ogden <robertogden@chromium.org>
2018-09-25 20:02:40 +0000

    Decrease Lite Page Browser test Timeout
    Anything less than 1/4 second and I was getting flakes locally.
    
    Bug: 885252
    Change-Id: Ia61a99abb4ef57686af01a34264b252096e10b36
    Reviewed-on: https://chromium-review.googlesource.com/1242683
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594070}

--- chrome/browser/previews/previews_lite_page_browsertest.cc
+++ chrome/browser/previews/previews_lite_page_browsertest.cc
@@ -128,7 +128,7 @@ class PreviewsLitePageServerBrowserTest : public InProcessBrowserTest {
           {"previews_host", previews_server().spec()},
           {"blacklisted_path_suffixes", ".mp4,.jpg"},
           {"trigger_on_localhost", "true"},
-          {"navigation_timeout_milliseconds", use_timeout ? "2000" : "0"}};
+          {"navigation_timeout_milliseconds", use_timeout ? "250" : "0"}};
       base::FieldTrialParamAssociator::GetInstance()->AssociateFieldTrialParams(
           "TrialName1", "GroupName1", feature_parameters);
 
@@ -276,7 +276,7 @@ class PreviewsLitePageServerBrowserTest : public InProcessBrowserTest {
       const net::test_server::HttpRequest& request) {
     std::unique_ptr<net::test_server::DelayedHttpResponse> response =
         std::make_unique<net::test_server::DelayedHttpResponse>(
-            base::TimeDelta::FromSeconds(3));
+            base::TimeDelta::FromMilliseconds(500));
     response->set_code(net::HttpStatusCode::HTTP_OK);
     return std::move(response);
   }
