381ad9bcdc6c1e5decb14b837d79a7a7bb0c3d44
Nico Weber <thakis@chromium.org>
2018-09-26 17:43:20 +0000

    mac: Remove some unused controls.
    Bug: 832676
    Change-Id: I9b56c47523765af4b33a997b65fe2a48a9da7f29
    Reviewed-on: https://chromium-review.googlesource.com/1246324
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594375}

--- ui/base/cocoa/controls/button_utils.mm
+++ ui/base/cocoa/controls/button_utils.mm
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ui/base/cocoa/controls/button_utils.h"
 
-#import "ui/base/cocoa/controls/hyperlink_button_cell.h"
-
 @implementation ButtonUtils
 
 + (NSButton*)buttonWithTitle:(NSString*)title
@@ -28,20 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [button setButtonType:NSSwitchButton];
   [button setFont:[NSFont systemFontOfSize:[NSFont systemFontSize]]];
   [button setBezelStyle:NSRegularSquareBezelStyle];
-  [button setTitle:title];
-  return button;
-}
-
-+ (NSButton*)linkWithTitle:(NSString*)title
-                    action:(SEL)action
-                    target:(id)target {
-  NSButton* button = [[[NSButton alloc] initWithFrame:NSZeroRect] autorelease];
-  // The cell has to be replaced before doing any of the other setup, or it will
-  // not get the new configuration.
-  [button setCell:[[[HyperlinkButtonCell alloc] init] autorelease]];
-  [button setAction:action];
-  [button setFont:[NSFont systemFontOfSize:[NSFont systemFontSize]]];
-  [button setTarget:target];
   [button setTitle:title];
   return button;
 }
