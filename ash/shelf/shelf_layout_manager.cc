ca01a4d5bb287b21c163db34d109eee81f734487
Wenzhao Zang <wzang@chromium.org>
2018-09-26 22:41:15 +0000

    cros: Fix unnecessary black shelf background issue when device boot
    This CL is more lightweight solution compared with the previous
    attempts:
    https://crrev.com/c/1229695
    (An overkill. "no wallpaper" is not a valid UI state we'll support.)
    https://crrev.com/c/1187547
    (A risky change. Large number of tests failed.)
    https://crrev.com/c/1173433
    (Doesn't quite handle the "no wallpaper" case.)
    
    Bug: 874015
    Change-Id: I7656856d1b631b7f6d2555f932116ea08e7d23ec
    Reviewed-on: https://chromium-review.googlesource.com/1237149
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Jacob Dufault <jdufault@chromium.org>
    Commit-Queue: Wenzhao (Colin) Zang <wzang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594504}

--- ash/shelf/shelf_layout_manager.cc
+++ ash/shelf/shelf_layout_manager.cc
@@ -542,8 +542,10 @@ ShelfBackgroundType ShelfLayoutManager::GetShelfBackgroundType() const {
   if (state_.session_state == session_manager::SessionState::OOBE)
     return SHELF_BACKGROUND_OOBE;
   if (state_.session_state != session_manager::SessionState::ACTIVE) {
-    if (!Shell::Get()->wallpaper_controller()->IsWallpaperBlurred())
+    if (Shell::Get()->wallpaper_controller()->HasShownAnyWallpaper() &&
+        !Shell::Get()->wallpaper_controller()->IsWallpaperBlurred()) {
       return SHELF_BACKGROUND_LOGIN_NONBLURRED_WALLPAPER;
+    }
     return SHELF_BACKGROUND_LOGIN;
   }
 
@@ -1141,6 +1143,10 @@ void ShelfLayoutManager::OnSessionStateChanged(
 }
 
 void ShelfLayoutManager::OnWallpaperBlurChanged() {
+  MaybeUpdateShelfBackground(AnimationChangeType::ANIMATE);
+}
+
+void ShelfLayoutManager::OnFirstWallpaperShown() {
   MaybeUpdateShelfBackground(AnimationChangeType::ANIMATE);
 }
 
