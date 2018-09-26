ae44c111c0cad55eea418ccb5d28c34c39cc03d2
James Cook <jamescook@chromium.org>
2018-09-26 21:52:54 +0000

    Move Chrome's ash::AccessibilityDelegate to its own file
    This makes it easier to find and consistent with ChromeShellDelegate.
    
    Also cleanup unnecessary includes in ChromeShellDelegate.
    
    I'm doing this because I may need to add a method to the delegate.
    
    Bug: none
    Test: existing browser_tests, manually test magnifier
    Change-Id: Icf0fdda4f6bea0f9fc3c281902e87fc036764950
    Reviewed-on: https://chromium-review.googlesource.com/1246661
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594487}

---
+++ chrome/browser/ui/ash/chrome_accessibility_delegate.h
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_ASH_CHROME_ACCESSIBILITY_DELEGATE_H_
+#define CHROME_BROWSER_UI_ASH_CHROME_ACCESSIBILITY_DELEGATE_H_
+
+#include "ash/accessibility/accessibility_delegate.h"
+#include "base/macros.h"
+
+// See ash::AccessibilityDelegate for details.
+class ChromeAccessibilityDelegate : public ash::AccessibilityDelegate {
+ public:
+  ChromeAccessibilityDelegate();
+  ~ChromeAccessibilityDelegate() override;
+
+  // ash::AccessibilityDelegate:
+  void SetMagnifierEnabled(bool enabled) override;
+  bool IsMagnifierEnabled() const override;
+  bool ShouldShowAccessibilityMenu() const override;
+  void SaveScreenMagnifierScale(double scale) override;
+  double GetSavedScreenMagnifierScale() override;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(ChromeAccessibilityDelegate);
+};
+
+#endif  // CHROME_BROWSER_UI_ASH_CHROME_ACCESSIBILITY_DELEGATE_H_
--- chrome/browser/ui/ash/chrome_shell_delegate.h
+++ chrome/browser/ui/ash/chrome_shell_delegate.h
@@ -6,23 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_ASH_CHROME_SHELL_DELEGATE_H_
 #define CHROME_BROWSER_UI_ASH_CHROME_SHELL_DELEGATE_H_
 
-#include <memory>
-#include <string>
-
 #include "ash/shell_delegate.h"
 #include "base/macros.h"
-#include "build/build_config.h"
-
-namespace keyboard {
-class KeyboardUI;
-}
 
 class ChromeShellDelegate : public ash::ShellDelegate {
  public:
   ChromeShellDelegate();
   ~ChromeShellDelegate() override;
 
-  // ash::ShellDelegate overrides;
+  // ash::ShellDelegate:
   bool CanShowWindowForUser(aura::Window* window) const override;
   std::unique_ptr<keyboard::KeyboardUI> CreateKeyboardUI() override;
   std::unique_ptr<ash::ScreenshotDelegate> CreateScreenshotDelegate() override;
