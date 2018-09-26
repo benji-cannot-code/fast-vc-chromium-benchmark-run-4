250c23855718bbf77fa3d51fb3fe05eb665f4e73
Joe Downing <joedow@chromium.org>
2018-09-26 03:06:46 +0000

    Fix OS Version reporting for pre-10.10 MacOS Hosts
    Bug: 889259
    Change-Id: I18314c4dc37ff7d60ea8ba989cc6155834c02fce
    Reviewed-on: https://chromium-review.googlesource.com/1244609
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594204}

--- remoting/host/host_details.cc
+++ remoting/host/host_details.cc
@@ -5,11 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "remoting/host/host_details.h"
 
-#include "base/sys_info.h"
 #include "build/build_config.h"
 
 #if defined(OS_LINUX)
 #include "base/linux_util.h"
+#include "base/sys_info.h"
+#else
+#include "remoting/base/platform_details.h"
 #endif
 
 namespace remoting {
@@ -33,12 +35,12 @@ std::string GetHostOperatingSystemName() {
 }
 
 // Get the host Operating System Version, removing the need to check for OS
-// definitions and keeps the format used consistant.
+// definitions and keeps the format used consistent.
 std::string GetHostOperatingSystemVersion() {
 #if defined(OS_LINUX)
   return base::GetLinuxDistro();
 #else
-  return base::SysInfo::OperatingSystemVersion();
+  return GetOperatingSystemVersionString();
 #endif
 }
 
