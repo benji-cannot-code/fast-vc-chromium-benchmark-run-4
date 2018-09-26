b01686afb4df10774cad0ab2fb380f2daaf52cf8
Alexei Svitkine <asvitkine@chromium.org>
2018-09-26 05:45:22 +0000

    Fix whitespace wrapping in histograms.xml pretty printing.
    Previously, the wrapping logic would often result in spans of
    whitespace between words that had to be manually fixed up, when
    a paragraph of text was re-wrapped by the pretty printer. This
    change fixes this, along with a test.
    
    See context bug for an example of the result that was produced
    before.
    
    This change applies to all XML formatters and includes the
    changes to the XML files based on this new formatting. One
    notable difference is that descriptions that used two spaces
    between sentences now use one. There was no consistency on
    this before and having only one space reduces the size of the
    file, so I think it's a positive change.
    
    Bug: 714123
    Change-Id: I619688463ba415a388bccd22d3cf19db19ba109c
    Reviewed-on: https://chromium-review.googlesource.com/1244553
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594228}

---
+++ tools/metrics/histograms/pretty_print_test.py
@@ -0,0 +1,54 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/env python
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import unittest
+
+import pretty_print
+
+
+ORIGINAL_XML = """
+<histogram-configuration>
+<histograms>
+ <histogram name="Test.Histogram" units="things">
+   <owner>person@chromium.org</owner>
+   <summary>A long line that should be formatted in a way that does not result
+     in extra whitespace between words.
+   </summary>
+ </histogram>
+</histograms>
+</histogram-configuration>
+""".strip()
+
+
+PRETTY_XML = """
+<histogram-configuration>
+
+<histograms>
+
+<histogram name="Test.Histogram" units="things">
+  <owner>person@chromium.org</owner>
+  <summary>
+    A long line that should be formatted in a way that does not result in extra
+    whitespace between words.
+  </summary>
+</histogram>
+
+</histograms>
+
+</histogram-configuration>
+""".strip()
+
+
+class PrettyPrintHistogramsXmlTest(unittest.TestCase):
+
+  def testWhitespaceWrapping(self):
+    result = pretty_print.PrettyPrintHistograms(ORIGINAL_XML)
+    self.maxDiff = None
+    self.assertMultiLineEqual(PRETTY_XML, result.strip())
+
+
+if __name__ == '__main__':
+  unittest.main()
