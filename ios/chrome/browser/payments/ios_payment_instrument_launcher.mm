acf5f9c0ab0923cfa77eda2c3c3a41697998a8e4
Daniel Bratell <bratell@opera.com>
2018-09-26 17:09:53 +0000

    [ios] Fix typo: include header instead of mm file.
    This caused a link error, duplicate symbols, in a jumbo build
    experiment. How it ever worked in normal builds we'll never know.
    
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: I4136a153aa989f2187d745f291658ac4e05fa6db
    Reviewed-on: https://chromium-review.googlesource.com/1244461
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Daniel Bratell <bratell@opera.com>
    Cr-Commit-Position: refs/heads/master@{#594357}

--- ios/chrome/browser/payments/ios_payment_instrument_launcher.mm
+++ ios/chrome/browser/payments/ios_payment_instrument_launcher.mm
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/web/public/navigation_manager.h"
 #include "ios/web/public/ssl_status.h"
 #include "ios/web/public/web_state/web_state.h"
-#include "net/base/mac/url_conversions.mm"
+#include "net/base/mac/url_conversions.h"
 #include "net/base/url_util.h"
 #include "net/cert/x509_certificate.h"
 #include "net/cert/x509_util.h"
