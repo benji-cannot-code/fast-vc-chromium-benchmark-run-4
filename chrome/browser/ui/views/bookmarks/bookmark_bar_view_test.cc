80569e5c09888f2b2574b21e1ed7c23b013b4769
Jun Mukai <mukai@chromium.org>
2018-09-26 00:04:51 +0000

    fix BookmarkBarViewTest12 for SingleProcessMash
    BookmarkBarViewTest12.CloseWithModalDialog is in timeout.
    That's because DialogWaiter is observing for a wrong aura::Env
    instance.
    
    BUG=883523
    TEST=interactive_ui_tests --enable-features=SingleProcessMash
    
    Change-Id: Ia2b057763e87458a564dbffa25c6cc92889992a6
    Reviewed-on: https://chromium-review.googlesource.com/1244125
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Jun Mukai <mukai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594149}

--- chrome/browser/ui/views/bookmarks/bookmark_bar_view_test.cc
+++ chrome/browser/ui/views/bookmarks/bookmark_bar_view_test.cc
@@ -77,13 +77,9 @@ namespace {
 class DialogWaiter : public aura::EnvObserver,
                      public views::WidgetObserver {
  public:
-  DialogWaiter()
-      : dialog_created_(false),
-        dialog_(NULL) {
-    aura::Env::GetInstance()->AddObserver(this);
-  }
+  explicit DialogWaiter(aura::Env* env) : env_(env) { env_->AddObserver(this); }
 
-  ~DialogWaiter() override { aura::Env::GetInstance()->RemoveObserver(this); }
+  ~DialogWaiter() override { env_->RemoveObserver(this); }
 
   views::Widget* WaitForDialog() {
     if (dialog_created_)
@@ -117,8 +113,9 @@ class DialogWaiter : public aura::EnvObserver,
     }
   }
 
-  bool dialog_created_;
-  views::Widget* dialog_;
+  aura::Env* env_;
+  bool dialog_created_ = false;
+  views::Widget* dialog_ = nullptr;
   base::Closure quit_closure_;
 
   DISALLOW_COPY_AND_ASSIGN(DialogWaiter);
@@ -1341,7 +1338,8 @@ class BookmarkBarViewTest12 : public BookmarkBarViewEventTestBase {
     ASSERT_TRUE(child_menu != NULL);
 
     // Click and wait until the dialog box appears.
-    std::unique_ptr<DialogWaiter> dialog_waiter(new DialogWaiter());
+    auto dialog_waiter = std::make_unique<DialogWaiter>(
+        bb_view_->GetWidget()->GetNativeWindow()->env());
     ui_test_utils::MoveMouseToCenterAndPress(
         child_menu, ui_controls::LEFT, ui_controls::DOWN | ui_controls::UP,
         base::Bind(&BookmarkBarViewTest12::Step4, base::Unretained(this),
