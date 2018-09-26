6942974560a37994c3f588c2e657f772d7640495
Morten Stenshorne <mstensho@chromium.org>
2018-09-26 23:12:41 +0000

    [LayoutNG] Fieldset min/max intrinsic size calculation.
    Extra care is needed when the legend is an orthogonal writing mode root.
    We need to do what we do in min/max calculation for such children in the
    regular block layout algorithm.
    
    Fieldset padding is also special, since it's included in the fieldset
    content anonymous child, but we have to take care of it ourselves
    anyway, in case there is no such child (empty fieldset, or fieldset with
    just a legend).
    
    Bug: 875235
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng
    Change-Id: If26d8a59ae1514085f40e5a9e1ecee007997e7ad
    Reviewed-on: https://chromium-review.googlesource.com/1245363
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594514}

--- third_party/blink/renderer/core/layout/min_max_size.cc
+++ third_party/blink/renderer/core/layout/min_max_size.cc
@@ -9,6 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+void MinMaxSize::Encompass(const MinMaxSize& other) {
+  min_size = std::max(min_size, other.min_size);
+  max_size = std::max(max_size, other.max_size);
+}
+
 void MinMaxSize::Encompass(LayoutUnit value) {
   min_size = std::max(min_size, value);
   max_size = std::max(max_size, value);
