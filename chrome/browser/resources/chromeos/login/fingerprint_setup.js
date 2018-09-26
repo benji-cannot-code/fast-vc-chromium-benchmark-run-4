376f34a37f96d89a5c39ad7f6981ea4aef83543d
Sarah Hu <xiaoyinh@chromium.org>
2018-09-26 21:32:17 +0000

    cros: Progress to fingerprint enrollment when user touches FP sensor
    Bug: 888704
    Change-Id: I3d6ccd1d4d4d19377db2d0698ac403ff79cd40e6
    Reviewed-on: https://chromium-review.googlesource.com/1244124
    Commit-Queue: Xiaoyin Hu <xiaoyinh@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594480}

--- chrome/browser/resources/chromeos/login/fingerprint_setup.js
+++ chrome/browser/resources/chromeos/login/fingerprint_setup.js
@@ -61,9 +61,13 @@ Polymer({
     },
   },
 
-  /** @override */
-  ready: function() {
+  /*
+   * Overridden from OobeDialogHostBehavior.
+   * @override
+   */
+  onBeforeShow: function() {
     this.showScreen_('setupFingerprint');
+    chrome.send('startEnroll');
   },
 
   focus: function() {
@@ -80,7 +84,8 @@ Polymer({
    */
   onEnrollScanDone: function(scanResult, isComplete, percentComplete) {
     // First tap on the sensor to start fingerprint enrollment.
-    if (this.getActiveScreen_() === this.$.placeFinger) {
+    if (this.getActiveScreen_() === this.$.placeFinger ||
+        this.getActiveScreen_() === this.$.setupFingerprint) {
       this.showScreen_('startFingerprintEnroll');
     }
 
@@ -154,7 +159,6 @@ Polymer({
    */
   onContinueToSensorLocationScreen_: function(e) {
     this.showScreen_('placeFinger');
-    chrome.send('startEnroll');
   },
 
   /**
