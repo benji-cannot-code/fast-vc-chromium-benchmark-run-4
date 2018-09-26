57ffae4fadf721f211fe0b5322f8536009e2cee0
Koji Ishii <kojii@chromium.org>
2018-09-26 01:44:49 +0000

    [LayoutNG] Implement PaintTouchActionRects for LayoutNG
    This patch implements PaintTouchActionRects in NGBoxFragmentPainter.
    
    Still fails 3 more tests, they are to be investigated.
    
    Bug: 714962
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng;luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I517b0d8b9d62304cd9259c4c31a32a5a32a1e6a7
    Reviewed-on: https://chromium-review.googlesource.com/1242256
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594178}

--- third_party/blink/renderer/core/layout/ng/ng_physical_fragment.cc
+++ third_party/blink/renderer/core/layout/ng/ng_physical_fragment.cc
@@ -396,6 +396,11 @@ const Vector<NGInlineItem>& NGPhysicalFragment::InlineItemsOfContainingBlock()
   return ToNGInlineNode(node).ItemsData(false).items;
 }
 
+TouchAction NGPhysicalFragment::EffectiveWhitelistedTouchAction() const {
+  DCHECK(GetLayoutObject());
+  return GetLayoutObject()->EffectiveWhitelistedTouchAction();
+}
+
 UBiDiLevel NGPhysicalFragment::BidiLevel() const {
   NOTREACHED();
   return 0;
--- third_party/blink/renderer/core/layout/ng/ng_physical_fragment.h
+++ third_party/blink/renderer/core/layout/ng/ng_physical_fragment.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/ng/geometry/ng_physical_size.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_break_token.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_style_variant.h"
+#include "third_party/blink/renderer/platform/graphics/touch_action.h"
 #include "third_party/blink/renderer/platform/wtf/ref_counted.h"
 
 #include <unicode/ubidi.h>
@@ -177,6 +178,10 @@ class CORE_EXPORT NGPhysicalFragment
   // Unite visual rect to propagate to parent's ContentsVisualRect.
   void PropagateContentsInkOverflow(NGPhysicalOffsetRect*,
                                     NGPhysicalOffset) const;
+
+  // The whitelisted touch action is the union of the effective touch action
+  // (from style) and blocking touch event handlers.
+  TouchAction EffectiveWhitelistedTouchAction() const;
 
   // Returns the bidi level of a text or atomic inline fragment.
   virtual UBiDiLevel BidiLevel() const;
