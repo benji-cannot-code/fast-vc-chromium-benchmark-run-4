55e56bd74fb89fdf6ca8c7fa468b0a73ecd77bb1
Zhongyi Shi <zhongyi@chromium.org>
2018-09-26 18:11:48 +0000

    Add finch config param for cronet and Chrome to retry QUIC connection on the alternate network before handshake.
    The feature will be controlled by "retry_on_alternate_network_before_handshake".
    
    Bug: 790547
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;master.tryserver.chromium.android:android_cronet_tester
    Change-Id: I5812083fe317f996313adce49f9bbd1d14b22f37
    Reviewed-on: https://chromium-review.googlesource.com/1242038
    Reviewed-by: Misha Efimov <mef@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Misha Efimov <mef@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594386}

--- components/cronet/url_request_context_config_unittest.cc
+++ components/cronet/url_request_context_config_unittest.cc
@@ -204,6 +204,7 @@ TEST(URLRequestContextConfigTest, TestExperimentalOptionParsing) {
   EXPECT_FALSE(params->quic_allow_server_migration);
   EXPECT_FALSE(params->quic_migrate_sessions_on_network_change_v2);
   EXPECT_FALSE(params->quic_migrate_sessions_early_v2);
+  EXPECT_FALSE(params->quic_retry_on_alternate_network_before_handshake);
 
   // Check race_cert_verification.
   EXPECT_TRUE(params->quic_race_cert_verification);
@@ -546,6 +547,7 @@ TEST(URLRequestContextConfigTest, SetQuicConnectionMigrationV2Options) {
       // JSON encoded experimental options.
       "{\"QUIC\":{\"migrate_sessions_on_network_change_v2\":true,"
       "\"migrate_sessions_early_v2\":true,"
+      "\"retry_on_alternate_network_before_handshake\":true,"
       "\"max_time_on_non_default_network_seconds\":10,"
       "\"max_migrations_to_non_default_network_on_write_error\":3,"
       "\"max_migrations_to_non_default_network_on_path_degrading\":4}}",
@@ -571,6 +573,7 @@ TEST(URLRequestContextConfigTest, SetQuicConnectionMigrationV2Options) {
 
   EXPECT_TRUE(params->quic_migrate_sessions_on_network_change_v2);
   EXPECT_TRUE(params->quic_migrate_sessions_early_v2);
+  EXPECT_TRUE(params->quic_retry_on_alternate_network_before_handshake);
   EXPECT_EQ(base::TimeDelta::FromSeconds(10),
             params->quic_max_time_on_non_default_network);
   EXPECT_EQ(3,
