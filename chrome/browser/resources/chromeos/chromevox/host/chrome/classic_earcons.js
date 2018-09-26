895b301a9d70d4f92556fcd3a666f941f42233c5
Eriko Kurimoto <elkurin@google.com>
2018-09-26 02:59:07 +0000

    Output classic earcon loggings
    In the cl below, earcon loggings are added but only for new_earcon.
    https://chromium-review.googlesource.com/c/chromium/src/+/1174194
    classc_earcon should be added as well.
    
    Bug: 871165
    Change-Id: I8fee148fd740a895a3e676e97b95baa18aec2934
    Reviewed-on: https://chromium-review.googlesource.com/1242645
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@google.com>
    Cr-Commit-Position: refs/heads/master@{#594201}

--- chrome/browser/resources/chromeos/chromevox/host/chrome/classic_earcons.js
+++ chrome/browser/resources/chromeos/chromevox/host/chrome/classic_earcons.js
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 goog.provide('cvox.ClassicEarcons');
 
+goog.require('LogStore');
+goog.require('TextLog');
 goog.require('cvox.AbstractEarcons');
 
 
@@ -55,7 +57,10 @@ cvox.ClassicEarcons.prototype.playEarcon = function(earcon, opt_location) {
   if (!cvox.AbstractEarcons.enabled) {
     return;
   }
-  console.log('Earcon ' + earcon);
+  if (localStorage['enableEarconLogging'] == 'true') {
+    LogStore.getInstance().writeTextLog(earcon, TextLog.LogType.EARCON);
+    console.log('Earcon ' + earcon);
+  }
 
   this.currentAudio = this.audioMap[earcon];
   if (!this.currentAudio) {
