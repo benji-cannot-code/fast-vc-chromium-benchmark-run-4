337861bfc18998e0cf4c6d1eeb153ee5d2a8d0ba
danakj <danakj@chromium.org>
2018-09-26 21:43:14 +0000

    Remove needs_repainting from the WasShown IPC to the RenderWidget.
    This bool is always true, and is not used. No need to send it.
    Also removes the dead needs_repainting_on_restore_ from the
    RenderWidgetHostImpl.
    
    R=ajwong@chromium.org, dcheng@chromium.org, piman@chromium.org
    
    Bug: 419087
    Change-Id: I673e5de215a9118b061478260d0dc012ae078d37
    Reviewed-on: https://chromium-review.googlesource.com/1246942
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Albert J. Wong <ajwong@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594482}

--- content/renderer/render_frame_impl_browsertest.cc
+++ content/renderer/render_frame_impl_browsertest.cc
@@ -212,7 +212,7 @@ TEST_F(RenderFrameImplTest, FrameResize) {
 TEST_F(RenderFrameImplTest, FrameWasShown) {
   RenderFrameTestObserver observer(frame());
 
-  ViewMsg_WasShown was_shown_message(0, true, base::TimeTicks());
+  ViewMsg_WasShown was_shown_message(0, base::TimeTicks());
   frame_widget()->OnMessageReceived(was_shown_message);
 
   EXPECT_FALSE(frame_widget()->is_hidden());
@@ -242,7 +242,7 @@ TEST_F(RenderFrameImplTest, LocalChildFrameWasShown) {
 
   RenderFrameTestObserver observer(grandchild);
 
-  ViewMsg_WasShown was_shown_message(0, true, base::TimeTicks());
+  ViewMsg_WasShown was_shown_message(0, base::TimeTicks());
   frame_widget()->OnMessageReceived(was_shown_message);
 
   EXPECT_FALSE(frame_widget()->is_hidden());
@@ -255,7 +255,7 @@ TEST_F(RenderFrameImplTest, FrameWasShownAfterWidgetClose) {
   ViewMsg_Close close_message(0);
   frame_widget()->OnMessageReceived(close_message);
 
-  ViewMsg_WasShown was_shown_message(0, true, base::TimeTicks());
+  ViewMsg_WasShown was_shown_message(0, base::TimeTicks());
   // Test passes if this does not crash.
   RenderWidget* render_widget =
       static_cast<RenderViewImpl*>(view_)->GetWidget();
