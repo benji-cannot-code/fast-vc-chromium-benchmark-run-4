813ac4299dfddaf7d992980f4296826cd9fc05ae
Doug Arnett <dougarnett@chromium.org>
2018-09-26 18:09:52 +0000

    Adds experiment_name support to PageHint based previews.
    Checks experiment_name field on Optimization defined at PageHint
    level and only whitelists if that experiment name is enabled for
    the client.
    
    Bug: 888666
    Change-Id: I42425aaf9403a023d684c94ce94860cba27513f2
    Reviewed-on: https://chromium-review.googlesource.com/1241214
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Doug Arnett <dougarnett@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594384}

--- components/previews/content/previews_hints.cc
+++ components/previews/content/previews_hints.cc
@@ -251,6 +251,11 @@ std::unique_ptr<PreviewsHints> PreviewsHints::CreateFromConfig(
 
       for (const auto& page_hint : hint.page_hints()) {
         for (const auto& optimization : page_hint.whitelisted_optimizations()) {
+          if (IsDisabledExperimentalOptimization(optimization)) {
+            // This is an experimental optimization that is not enabled so
+            // continue in case there is a non-experimental one.
+            continue;
+          }
           base::Optional<PreviewsType> previews_type =
               ConvertProtoOptimizationTypeToPreviewsOptimizationType(
                   optimization.optimization_type());
@@ -443,6 +448,11 @@ bool PreviewsHints::IsWhitelisted(const GURL& url,
        matched_page_hint->whitelisted_optimizations()) {
     if (ConvertProtoOptimizationTypeToPreviewsOptimizationType(
             optimization.optimization_type()) == type) {
+      if (IsDisabledExperimentalOptimization(optimization)) {
+        // This is an experimental optimization that is not enabled so continue
+        // in case there is a non-experimental one.
+        continue;
+      }
       *out_inflation_percent = optimization.inflation_percent();
       return true;
     }
