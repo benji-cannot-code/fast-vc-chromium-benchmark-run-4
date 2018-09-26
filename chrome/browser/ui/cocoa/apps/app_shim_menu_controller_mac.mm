5fa5859b331657c5b2b13c3d356cb7a3f6129b52
Nico Weber <thakis@chromium.org>
2018-09-26 15:14:50 +0000

    mac: Remove unused NativeAppWindowCocoa and now-always-on kEnableMacViewsNativeAppWindows flag.
    Bug: 832676
    Change-Id: Id712b18f8ccb526d938ec0b63feb580fbaf7c738
    Reviewed-on: https://chromium-review.googlesource.com/1245923
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594320}

--- chrome/browser/ui/cocoa/apps/app_shim_menu_controller_mac.mm
+++ chrome/browser/ui/cocoa/apps/app_shim_menu_controller_mac.mm
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
-#import "chrome/browser/ui/cocoa/apps/native_app_window_cocoa.h"
 #include "chrome/grit/generated_resources.h"
 #include "extensions/browser/app_window/app_window.h"
 #include "extensions/common/extension.h"
--- chrome/browser/ui/views/apps/chrome_app_window_client_views_mac.mm
+++ chrome/browser/ui/views/apps/chrome_app_window_client_views_mac.mm
@@ -6,14 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/apps/chrome_app_window_client.h"
 
 #include "chrome/browser/ui/views/apps/chrome_native_app_window_views_mac.h"
-#include "chrome/browser/ui/views_mode_controller.h"
 
 // static
 extensions::NativeAppWindow* ChromeAppWindowClient::CreateNativeAppWindowImpl(
     extensions::AppWindow* app_window,
     const extensions::AppWindow::CreateParams& params) {
-  if (views_mode_controller::IsViewsBrowserCocoa())
-    return CreateNativeAppWindowImplCocoa(app_window, params);
   ChromeNativeAppWindowViewsMac* window = new ChromeNativeAppWindowViewsMac;
   window->Init(app_window, params);
   return window;
