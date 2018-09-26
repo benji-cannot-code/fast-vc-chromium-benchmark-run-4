b15a2d4049e706738a32ce0aaae15f1aacebffd9
Vadym Doroshenko <dvadym@chromium.org>
2018-09-26 16:16:33 +0000

    Make function ShouldBeUploaded more consistent.
    For checking whether to upload votes for a form, FormStructure::ShouldBeUploaded is called
    both from Autofill and Password Manager code. Because Password Manager uploads votes rarely it's
    ok to upload Password Manager votes for any form, so call of this function might be skipped in
    Password Manager code.
    
    On other hand, ShouldBeUploaded has a check on |has_password_field_| and as result uploads for
    any password form are treated as Password Manager uploads, while they may come from Autofill.
    
    This CL implements:
    1.Removing call of ShouldBeUploaded in Password Manager code.
    2.Removing checks on has_password_field_ in ShouldBeUploaded.
    
    Bug:882941
    
    Change-Id: Iba9ab1f74451520f0a636e0748f795d73e922ce7
    Reviewed-on: https://chromium-review.googlesource.com/1219710
    Commit-Queue: Vadym Doroshenko <dvadym@chromium.org>
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594335}

--- components/password_manager/core/browser/votes_uploader.cc
+++ components/password_manager/core/browser/votes_uploader.cc
@@ -237,6 +237,11 @@ bool VotesUploader::UploadPasswordVote(
   if (!has_autofill_vote && !has_password_generation_vote)
     return false;
 
+  if (form_to_upload.form_data.fields.empty()) {
+    // List of fields may be empty in tests.
+    return false;
+  }
+
   AutofillManager* autofill_manager = client_->GetAutofillManagerForMainFrame();
   if (!autofill_manager || !autofill_manager->download_manager())
     return false;
@@ -246,10 +251,6 @@ bool VotesUploader::UploadPasswordVote(
   // credentials, the observed and pending forms are the same.
   FormStructure form_structure(form_to_upload.form_data);
   form_structure.set_submission_event(submitted_form.submission_event);
-  if (!autofill_manager->ShouldUploadForm(form_structure)) {
-    UMA_HISTOGRAM_BOOLEAN("PasswordGeneration.UploadStarted", false);
-    return false;
-  }
 
   ServerFieldTypeSet available_field_types;
   // A map from field names to field types.
@@ -336,10 +337,13 @@ void VotesUploader::UploadFirstLoginVotes(
   if (!autofill_manager || !autofill_manager->download_manager())
     return;
 
+  if (form_to_upload.form_data.fields.empty()) {
+    // List of fields may be empty in tests.
+    return;
+  }
+
   FormStructure form_structure(form_to_upload.form_data);
   form_structure.set_submission_event(form_to_upload.submission_event);
-  if (!autofill_manager->ShouldUploadForm(form_structure))
-    return;
 
   FieldTypeMap field_types = {
       {form_to_upload.username_element, autofill::USERNAME}};
@@ -378,7 +382,6 @@ void VotesUploader::SendSignInVote(
   std::unique_ptr<FormStructure> form_structure(new FormStructure(form_data));
   form_structure->set_submission_event(submission_event);
   form_structure->set_is_signin_upload(true);
-  DCHECK(form_structure->ShouldBeUploaded());
   DCHECK_EQ(2u, form_structure->field_count());
   form_structure->field(1)->set_possible_types({autofill::PASSWORD});
   autofill_manager->MaybeStartVoteUploadProcess(std::move(form_structure),
