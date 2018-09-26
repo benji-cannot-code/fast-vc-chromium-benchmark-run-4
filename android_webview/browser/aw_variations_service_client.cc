0df65abef15b6e89bd4fd2d0b889abd8184a124a
Paul Miller <paulmiller@chromium.org>
2018-09-26 22:07:19 +0000

    Revert "WebView: Ignore permanent-consistency experiments"
    This reverts commit ef70c85e24ab28f8cc8d67666e6ee42ea62d4ab0.
    
    Reason for revert: WebView now supports permanent-consistency.
    
    BUG=866722
    
    Original change's description:
    > WebView: Ignore permanent-consistency experiments
    > 
    > Android WebView currently treats permanent-consistency experiments
    > (which are intended to retain their group assignments across runs) as
    > session-consistency (which are intended to be randomly assigned on each
    > run). It's safer to not apply permanent-consistency experiments to
    > WebView at all, to avoid mistakes.
    > 
    > BUG=866722
    > 
    > Change-Id: I9d501437718d934c2a59b95335c787302ce758e7
    > Reviewed-on: https://chromium-review.googlesource.com/1148991
    > Reviewed-by: Changwan Ryu <changwan@chromium.org>
    > Reviewed-by: Steven Holte <holte@chromium.org>
    > Commit-Queue: Paul Miller <paulmiller@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#578070}
    
    TBR=changwan@chromium.org,holte@chromium.org,paulmiller@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 866722
    Change-Id: Ifd00450fbba1afc1db619109cf918ef75cbcf84f
    Reviewed-on: https://chromium-review.googlesource.com/1237244
    Reviewed-by: Paul Miller <paulmiller@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Paul Miller <paulmiller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594491}

--- android_webview/browser/aw_variations_service_client.cc
+++ android_webview/browser/aw_variations_service_client.cc
@@ -55,10 +55,6 @@ Channel AwVariationsServiceClient::GetChannel() {
   return android_webview::GetChannelOrStable();
 }
 
-bool AwVariationsServiceClient::GetSupportsPermanentConsistency() {
-  return false;
-}
-
 bool AwVariationsServiceClient::OverridesRestrictParameter(
     std::string* parameter) {
   return false;
