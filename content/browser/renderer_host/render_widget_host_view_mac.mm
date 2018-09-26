9808f060b5184f29779bdd1e5d0806ef9de3c8c9
Saman Sami <samans@chromium.org>
2018-09-26 17:55:58 +0000

    Don't update the surface range at every activation
    Updating the fallback lowerbound is not necessary anymore and will just
    waste time by causing an unnecessary commit and draw.
    
    Bug: 870456
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel
    Change-Id: Icd553c179c5a2eb98c1608524d9c1caf42a27835
    Reviewed-on: https://chromium-review.googlesource.com/1226366
    Commit-Queue: Saman Sami <samans@chromium.org>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Fady Samuel <fsamuel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594380}

--- content/browser/renderer_host/render_widget_host_view_mac.mm
+++ content/browser/renderer_host/render_widget_host_view_mac.mm
@@ -1260,8 +1260,6 @@ bool RenderWidgetHostViewMac::TransformPointToCoordSpaceForView(
     return true;
   }
 
-  if (!HasFallbackSurface())
-    return false;
   return target_view->TransformPointToLocalCoordSpace(
       point, GetCurrentSurfaceId(), transformed_point, source);
 }
