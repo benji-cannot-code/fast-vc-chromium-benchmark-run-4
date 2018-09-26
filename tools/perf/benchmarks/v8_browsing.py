ddcf51652415d3b2e874df4a11c65745ffb5f77b
Mythri Alle <mythria@chromium.org>
2018-09-26 16:13:48 +0000

    Add component in the info for v8_browsing* benchmarks
    Add component to v8_browsing benchmarks to streamline bug
    triaging process
    
    Change-Id: I407b692776dc889aaf16e4965aafcb627ed7aa37
    Reviewed-on: https://chromium-review.googlesource.com/1235996
    Commit-Queue: Mythri Alle <mythria@chromium.org>
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Cr-Commit-Position: refs/heads/master@{#594334}

--- tools/perf/benchmarks/v8_browsing.py
+++ tools/perf/benchmarks/v8_browsing.py
@@ -117,7 +117,9 @@ class _V8BrowsingBenchmark(perf_benchmark.PerfBenchmark):
     return V8BrowsingShouldAddValue(name)
 
 
-@benchmark.Info(emails=['mythria@chromium.org','ulan@chromium.org'])
+@benchmark.Info(
+   emails=['mythria@chromium.org','ulan@chromium.org'],
+   component='Blink>JavaScript')
 class V8DesktopBrowsingBenchmark(
     _V8BrowsingBenchmark):
   PLATFORM = 'desktop'
@@ -132,7 +134,9 @@ class V8DesktopBrowsingBenchmark(
     return 'v8.browsing_desktop'
 
 
-@benchmark.Info(emails=['mythria@chromium.org','ulan@chromium.org'])
+@benchmark.Info(
+   emails=['mythria@chromium.org','ulan@chromium.org'],
+   component='Blink>JavaScript')
 class V8MobileBrowsingBenchmark(
     _V8BrowsingBenchmark):
   PLATFORM = 'mobile'
@@ -147,7 +151,9 @@ class V8MobileBrowsingBenchmark(
     return 'v8.browsing_mobile'
 
 
-@benchmark.Info(emails=['mythria@chromium.org','ulan@chromium.org'])
+@benchmark.Info(
+   emails=['mythria@chromium.org','ulan@chromium.org'],
+   component='Blink>JavaScript')
 class V8FutureDesktopBrowsingBenchmark(
     _V8BrowsingBenchmark):
   PLATFORM = 'desktop'
@@ -163,7 +169,9 @@ class V8FutureDesktopBrowsingBenchmark(
     return 'v8.browsing_desktop-future'
 
 
-@benchmark.Info(emails=['mythria@chromium.org','ulan@chromium.org'])
+@benchmark.Info(
+   emails=['mythria@chromium.org','ulan@chromium.org'],
+   component='Blink>JavaScript')
 class V8FutureMobileBrowsingBenchmark(
     _V8BrowsingBenchmark):
   PLATFORM = 'mobile'
