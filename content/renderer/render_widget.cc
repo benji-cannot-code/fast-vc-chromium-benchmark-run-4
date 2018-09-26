0b237d2e26b475960b1c07e4afd8fb734d7f2fab
Keishi Hattori <keishi@chromium.org>
2018-09-26 02:28:17 +0000

    Revert "Adding UKM and UMA tracking main frame task breakdown"
    This reverts commit 18c7186d821de890abcc39dfd8cc8ad5bbb28e08.
    
    Reason for revert: Find-it says this caused MSAN bot failure
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Linux%20MSan%20Tests/11801
    
    Original change's description:
    > Adding UKM and UMA tracking main frame task breakdown
    > 
    > The paint team has no insight into what proportion of the
    > Blink complete main frame time is spent in Blink lifecycle
    > operations vs. script required updates vs. other tasks.
    > This CL adds metrics that will allow for identifying the
    > proprtion of main frame time taken by various operations.
    > 
    > Bug: 869966
    > Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    > Change-Id: Idc73dc6efd1aef5e65df647bc48de84384dba0c8
    > Reviewed-on: https://chromium-review.googlesource.com/1179995
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Reviewed-by: Robert Kaplow (sloooow) <rkaplow@chromium.org>
    > Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    > Reviewed-by: vmpstr <vmpstr@chromium.org>
    > Commit-Queue: Stephen Chenney <schenney@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#594114}
    
    TBR=sadrul@chromium.org,sky@chromium.org,vmpstr@chromium.org,rkaplow@chromium.org,chrishtr@chromium.org,fsamuel@chromium.org,holte@chromium.org,schenney@chromium.org
    
    Change-Id: I1a391af1e942bc4f4c5e993b9afcbbae67ff3905
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 869966
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Reviewed-on: https://chromium-review.googlesource.com/1243875
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594190}

--- content/renderer/render_widget.cc
+++ content/renderer/render_widget.cc
@@ -1100,13 +1100,6 @@ void RenderWidget::RecordTimeToFirstActivePaint() {
   }
 }
 
-void RenderWidget::RecordEndOfFrameMetrics(base::TimeTicks frame_begin_time) {
-  if (!GetWebWidget())
-    return;
-
-  GetWebWidget()->RecordEndOfFrameMetrics(frame_begin_time);
-}
-
 void RenderWidget::WillBeginCompositorFrame() {
   TRACE_EVENT0("gpu", "RenderWidget::willBeginCompositorFrame");
 
