0c0b378a7d91ef058ff9408aa7dbf5bcd5a5c9e7
Denis Kuznetsov <antrim@google.com>
2018-09-27 11:23:57 +0000

    Usb-Enrollment: Use actual DBus service
    Bug: 854101
    Change-Id: I8c08f755f36f6b8ddddac6018aae8c9c022ee66a
    Reviewed-on: https://chromium-review.googlesource.com/1228202
    Commit-Queue: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594668}

--- chromeos/dbus/oobe_configuration_client.h
+++ chromeos/dbus/oobe_configuration_client.h
@@ -35,6 +35,8 @@ class CHROMEOS_EXPORT OobeConfigurationClient : public DBusClient {
   virtual void CheckForOobeConfiguration(ConfigurationCallback callback) = 0;
 
  protected:
+  friend class OobeConfigurationClientTest;
+
   // Create() should be used instead.
   OobeConfigurationClient() = default;
 
