5031ef4e57b1a4251d5a189252f98a4ebafff548
Stefan Teodorescu <fane@google.com>
2018-09-27 09:32:26 +0000

    Add histogram code for management page
    Add a first metric to the chrome://management page which counts how
    many times the page is accessed
    
    Bug: 879146
    Change-Id: I42be7a314ebffb295069e44735913f49376edda3
    Reviewed-on: https://chromium-review.googlesource.com/1243484
    Commit-Queue: Stefan Teodorescu <fane@google.com>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594645}

--- chrome/browser/ui/webui/management_ui_handler.cc
+++ chrome/browser/ui/webui/management_ui_handler.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/bind_helpers.h"
 #include "base/callback.h"
+#include "base/metrics/user_metrics.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
 #include "chrome/browser/browser_process.h"
@@ -114,6 +115,7 @@ void ManagementUIHandler::RegisterMessages() {
 void ManagementUIHandler::HandleGetDeviceManagementStatus(
     const base::ListValue* args) {
   AllowJavascript();
+  base::RecordAction(base::UserMetricsAction("ManagementPageViewed"));
 
 #if defined(OS_CHROMEOS)
   policy::BrowserPolicyConnectorChromeOS* connector =
