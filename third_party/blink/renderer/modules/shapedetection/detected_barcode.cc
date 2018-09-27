290f4d18335c69d37b26cbfdacf07520cdd473c1
François Beaufort <beaufort.francois@gmail.com>
2018-09-27 08:27:21 +0000

    [Shape Detection] Make DetectedBarcode interface serializable.
    This CL makes sure DetectedBarcode interface is serializable so that
    DetectedBarcode can easily be transfered from workers.
    
    Change-Id: I539dfa2dcba7873ccad4df76b8531c797d88072c
    Reviewed-on: https://chromium-review.googlesource.com/1246452
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Commit-Queue: François Beaufort <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/master@{#594633}

--- third_party/blink/renderer/modules/shapedetection/detected_barcode.cc
+++ third_party/blink/renderer/modules/shapedetection/detected_barcode.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/modules/shapedetection/detected_barcode.h"
 
+#include "third_party/blink/renderer/bindings/core/v8/v8_object_builder.h"
 #include "third_party/blink/renderer/core/geometry/dom_rect.h"
 
 namespace blink {
@@ -39,6 +40,21 @@ DetectedBarcode::DetectedBarcode(String raw_value,
     : raw_value_(raw_value),
       bounding_box_(bounding_box),
       corner_points_(corner_points) {}
+
+ScriptValue DetectedBarcode::toJSONForBinding(ScriptState* script_state) const {
+  V8ObjectBuilder result(script_state);
+  result.AddString("rawValue", rawValue());
+  result.Add("boundingBox", boundingBox()->toJSONForBinding(script_state));
+  Vector<ScriptValue> corner_points;
+  for (const auto& corner_point : corner_points_) {
+    V8ObjectBuilder builder(script_state);
+    builder.AddNumber("x", corner_point.x());
+    builder.AddNumber("y", corner_point.y());
+    corner_points.push_back(builder.GetScriptValue());
+  }
+  result.Add("cornerPoints", corner_points);
+  return result.GetScriptValue();
+}
 
 void DetectedBarcode::Trace(blink::Visitor* visitor) {
   visitor->Trace(bounding_box_);
--- third_party/blink/renderer/modules/shapedetection/detected_barcode.h
+++ third_party/blink/renderer/modules/shapedetection/detected_barcode.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_SHAPEDETECTION_DETECTED_BARCODE_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_SHAPEDETECTION_DETECTED_BARCODE_H_
 
+#include "third_party/blink/renderer/bindings/core/v8/script_value.h"
 #include "third_party/blink/renderer/modules/imagecapture/point_2d.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
@@ -25,6 +26,8 @@ class MODULES_EXPORT DetectedBarcode final : public ScriptWrappable {
   const String& rawValue() const;
   DOMRectReadOnly* boundingBox() const;
   const HeapVector<Point2D>& cornerPoints() const;
+
+  ScriptValue toJSONForBinding(ScriptState*) const;
   void Trace(blink::Visitor*) override;
 
  private:
