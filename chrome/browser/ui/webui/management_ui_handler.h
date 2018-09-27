f09db2e8330bba6b8ea671a82342d3d74fffe20c
Stefan Teodorescu <fane@google.com>
2018-09-27 17:22:00 +0000

    Show extension information on management page
    Add a new section in the chrome://management page to show force
    installed extensions with powerful capabilities (like ability to read
    and write all data on the page).
    
    Bug: 879146
    Change-Id: Ie45368caec53952a4c18efb07f3d80c21d5540b2
    Reviewed-on: https://chromium-review.googlesource.com/1235674
    Reviewed-by: Sergey Volk <servolk@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Marton Hunyady <hunyadym@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Stefan Teodorescu <fane@google.com>
    Cr-Commit-Position: refs/heads/master@{#594764}

--- chrome/browser/ui/webui/management_ui_handler.h
+++ chrome/browser/ui/webui/management_ui_handler.h
@@ -38,6 +38,8 @@ class ManagementUIHandler : public content::WebUIMessageHandler {
 
   void HandleGetReportingInfo(const base::ListValue* args);
 
+  void HandleGetExtensions(const base::ListValue* args);
+
   DISALLOW_COPY_AND_ASSIGN(ManagementUIHandler);
 };
 
--- chrome/common/extensions/permissions/chrome_permission_message_provider.h
+++ chrome/common/extensions/permissions/chrome_permission_message_provider.h
@@ -7,9 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_COMMON_EXTENSIONS_PERMISSIONS_CHROME_PERMISSION_MESSAGE_PROVIDER_H_
 
 #include <set>
+#include <vector>
 
 #include "base/macros.h"
 #include "base/strings/string16.h"
+#include "chrome/common/extensions/permissions/chrome_permission_message_rules.h"
 #include "extensions/common/permissions/permission_message_provider.h"
 
 namespace extensions {
@@ -27,6 +29,8 @@ class ChromePermissionMessageProvider : public PermissionMessageProvider {
 
   // PermissionMessageProvider implementation.
   PermissionMessages GetPermissionMessages(
+      const PermissionIDSet& permissions) const override;
+  PermissionMessages GetPowerfulPermissionMessages(
       const PermissionIDSet& permissions) const override;
   bool IsPrivilegeIncrease(const PermissionSet& granted_permissions,
                            const PermissionSet& requested_permissions,
@@ -60,6 +64,10 @@ class ChromePermissionMessageProvider : public PermissionMessageProvider {
   bool IsHostPrivilegeIncrease(const PermissionSet& granted_permissions,
                                const PermissionSet& requested_permissions,
                                Manifest::Type extension_type) const;
+
+  PermissionMessages GetPermissionMessagesHelper(
+      const PermissionIDSet& permissions,
+      const std::vector<ChromePermissionMessageRule>& rules) const;
 
   DISALLOW_COPY_AND_ASSIGN(ChromePermissionMessageProvider);
 };
