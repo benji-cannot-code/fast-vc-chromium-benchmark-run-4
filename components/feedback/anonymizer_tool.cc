a1a95d0532ab82ea38a5e948b1641aa015899269
Allen Webb <allenwebb@google.com>
2018-09-27 15:47:22 +0000

    Revert "anonymizer_tool: Added pattern to remove usbguard serial numbers."
    This reverts commit 098b2cafe9aeab8cb0f270e6e4e2910605c0cf85.
    
    Reason for revert: This is no longer needed because of go/crosr/1199443
    
    Original change's description:
    > anonymizer_tool: Added pattern to remove usbguard serial numbers.
    > 
    > The usbguard-daemon prints serial numbers of USB devices to
    > /var/log/messages. This change adds a pattern to anonymize those
    > serial numbers to prevent leaking PII.
    > 
    > BUG=None
    > TEST=Generate a report with logs and verify usbguard-daemon log
    > lines are properly anonymized.
    > 
    > Signed-off-by: Allen Webb <allenwebb@google.com>
    > Change-Id: Ifd41c8208b4d14bb3fafbf38198b04da6a8babd6
    > Reviewed-on: https://chromium-review.googlesource.com/1160935
    > Reviewed-by: Rahul Chaturvedi <rkc@chromium.org>
    > Reviewed-by: Mattias Nissler <mnissler@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#580946}
    
    TBR=rkc@chromium.org,mnissler@chromium.org,allenwebb@google.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: None
    Change-Id: I4af62c2af7d9dc569175e5e68393c9e2a7ebb5e6
    Reviewed-on: https://chromium-review.googlesource.com/1220095
    Reviewed-by: Mattias Nissler <mnissler@chromium.org>
    Commit-Queue: Allen Webb <allenwebb@google.com>
    Cr-Commit-Position: refs/heads/master@{#594735}

--- components/feedback/anonymizer_tool.cc
+++ components/feedback/anonymizer_tool.cc
@@ -56,9 +56,6 @@ constexpr const char* kCustomPatternsWithContext[] = {
 
     // Serial numbers
     "(?i-s)(serial\\s*(?:number)?\\s*[:=]\\s*)([0-9a-zA-Z\\-\"]+)()",
-
-    // usbguard-daemon
-    "(?-s)(usbguard-daemon.* serial \")(.*)(\" name \")",
 };
 
 // Helper macro: Non capturing group
