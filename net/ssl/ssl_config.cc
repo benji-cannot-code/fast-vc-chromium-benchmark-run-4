2afe427b315b5896e0a90b933c5397c288515704
David Benjamin <davidben@chromium.org>
2018-09-27 18:54:52 +0000

    Roll src/third_party/boringssl/src 7f4f41fa8..fb86b888e
    https://boringssl.googlesource.com/boringssl/+log/7f4f41fa81c03e0f8ef1ab5b3d1d566b5968f107..fb86b888ef1c1265719208fd6c27a53ceb4920f7
    
    BUG=none
    
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Change-Id: I5f9f8df83c33c9eb3787e4385c20e0f68a0d8335
    Reviewed-on: https://chromium-review.googlesource.com/1246406
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Steven Valdez <svaldez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594800}

--- net/ssl/ssl_config.cc
+++ net/ssl/ssl_config.cc
@@ -9,11 +9,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace net {
 
+// Note these lines must be kept in sync with
+// services/network/public/mojom/ssl_config.mojom.
 const uint16_t kDefaultSSLVersionMin = SSL_PROTOCOL_VERSION_TLS1;
 
 const uint16_t kDefaultSSLVersionMax = SSL_PROTOCOL_VERSION_TLS1_2;
 
-const TLS13Variant kDefaultTLS13Variant = kTLS13VariantDraft23;
+const TLS13Variant kDefaultTLS13Variant = kTLS13VariantFinal;
 
 SSLConfig::CertAndStatus::CertAndStatus() = default;
 SSLConfig::CertAndStatus::CertAndStatus(scoped_refptr<X509Certificate> cert_arg,
