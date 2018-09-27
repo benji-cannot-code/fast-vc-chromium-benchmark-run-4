09feb8c605bd3768a21783c3b409000164d38493
Gemene Narcis <gemene@google.com>
2018-09-27 19:54:30 +0000

    Changes in HttpCredentialCleaner
    This CL introduces a few improvements to HttpCredentialCleaner:
    (1) A fix in removing the protocol from signon_realm,
    (2) refactoring the unittest to use parameters,
    (3) minor style fixes.
    
    More details about (1):
    HttpCredentialCleaner removes the protocol (HTTP or HTTPS) form
    the signon_realm in order to compare the signon_realm of HTTP
    credentials with the signon_realm of HTTPS credentials. Until now,
    a GURL was created from the signon_realm of the form and then the
    protocol was extracted from that GURL, resulting the signon_realm
    excluding protocol. This can cause problems when the auth realm contains
    characters that are forbidden in an url. This will lead in creating an
    invalid url, and the resulting signon_realm with protocol exluded will
    be an empty string.
    This CL will avoid conversion from the signon_realm string to the GURL
    and use other way to remove the protocol from the signon_realm.
    
    More details about (2):
    Unitests for this class were changed from one single test into a
    bunch of parametrised tests in order to make debug easier in case
    of failing test in the future.
    
    Bug: 871140
    Change-Id: Ic606d250f50806ae3a3fa07a480fcb01f5551c97
    Reviewed-on: https://chromium-review.googlesource.com/1249104
    Commit-Queue: Narcis Gemene <gemene@google.com>
    Reviewed-by: Vaclav Brozek <vabr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594826}

--- components/password_manager/core/browser/http_credentials_cleaner.cc
+++ components/password_manager/core/browser/http_credentials_cleaner.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/http_credentials_cleaner.h"
 
 #include "base/metrics/histogram_functions.h"
-#include "base/strings/string_piece.h"
 #include "components/password_manager/core/browser/password_manager_util.h"
 #include "url/gurl.h"
 
@@ -31,11 +30,10 @@ void HttpCredentialCleaner::OnGetPasswordStoreResults(
   });
 
   for (auto& form : results) {
-    // The next signon-realm has the protocol excluded. For example if original
-    // signon_realm is "https://google.com/". After excluding protocol it
-    // becomes "google.com/".
-    FormKey form_key({GURL(form->signon_realm).GetContent(), form->scheme,
-                      form->username_value});
+    FormKey form_key(
+        {std::string(
+             password_manager_util::GetSignonRealmWithProtocolExcluded(*form)),
+         form->scheme, form->username_value});
     if (form->origin.SchemeIs(url::kHttpScheme)) {
       PostHSTSQueryForHostAndNetworkContext(
           form->origin, network_context_getter_.Run(),
--- components/password_manager/core/browser/invalid_realm_credential_cleaner.cc
+++ components/password_manager/core/browser/invalid_realm_credential_cleaner.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/string_piece.h"
 #include "components/autofill/core/common/password_form.h"
+#include "components/password_manager/core/browser/password_manager_util.h"
 #include "components/password_manager/core/browser/password_store.h"
 #include "components/password_manager/core/common/password_manager_pref_names.h"
 #include "components/prefs/pref_service.h"
@@ -199,12 +200,9 @@ void InvalidRealmCredentialCleaner::OnGetPasswordStoreResults(
   // HTTP forms to the expected signon_realm (excluding the protocol).
   std::map<FormKeyForHttpMatch, std::string> http_credentials_map;
   for (const auto& form : http_forms) {
-    base::StringPiece signon_realm = form->signon_realm;
-    // Find the web origin in the signon_realm and remove what is before it.
-    // This will result in removing the protocol ("http://").
-    signon_realm = signon_realm.substr(
-        signon_realm.find(form->origin.GetOrigin().GetContent()));
-    http_credentials_map.emplace(GetFormKeyForHttpMatch(*form), signon_realm);
+    http_credentials_map.emplace(
+        GetFormKeyForHttpMatch(*form),
+        password_manager_util::GetSignonRealmWithProtocolExcluded(*form));
   }
 
   // Separate HTML and non-HTML HTTPS credentials.
