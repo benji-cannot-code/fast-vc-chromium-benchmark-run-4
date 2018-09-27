167b5fc5ca1786203845bcac354c51591c0973aa
erikchen <erikchen@chromium.org>
2018-09-27 20:38:38 +0000

    Use accurate X11 event timestamp computation.
    X events have a timestamp which is only well defined relative to the X11 Server
    time. The previous computation for timestamp for X11 events was making the
    assumption that Server time and Chrome time were the same. This assumption is
    not always true -- this is likely the root cause of "bad" timestamps observed in
    https://bugs.chromium.org/p/chromium/issues/detail?id=650338#c1
    
    This CL changes event timestamp computation to make a roundtrip to the X11
    Server to get an accurate base::TimeTicks. This logic was lifted out of the
    responsiveness calculator, which was already doing this computation. The latter
    will subsequently be changed to use the computation in this CL.
    
    Change-Id: I963019cd8bfb8ce14e06b3743a159c9c85f2cb82
    Bug: 859155
    Reviewed-on: https://chromium-review.googlesource.com/1249383
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594844}

--- ui/events/x/events_x.cc
+++ ui/events/x/events_x.cc
@@ -82,9 +82,7 @@ int EventFlagsFromNative(const PlatformEvent& native_event) {
 }
 
 base::TimeTicks EventTimeFromNative(const PlatformEvent& native_event) {
-  base::TimeTicks timestamp = EventTimeFromXEvent(*native_event);
-  ValidateEventTimeClock(&timestamp);
-  return timestamp;
+  return EventTimeFromXEvent(*native_event);
 }
 
 gfx::PointF EventLocationFromNative(const PlatformEvent& native_event) {
