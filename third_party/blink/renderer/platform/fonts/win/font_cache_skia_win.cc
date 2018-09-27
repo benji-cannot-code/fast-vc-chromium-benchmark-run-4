c2d4da85c2d059591524d51b4c538664512a6d40
Erik Chen <erikchen@chromium.org>
2018-09-27 19:55:13 +0000

    Side loaded fonts should be case insensitive.
    This is a source of webkit_layout_test flakiness on Windows.
    
    Bug: 889608
    Change-Id: Ib31cd068620d0c1917aaa85ce2ec7038ccd4236e
    Reviewed-on: https://chromium-review.googlesource.com/1249385
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594827}

--- third_party/blink/renderer/platform/fonts/win/font_cache_skia_win.cc
+++ third_party/blink/renderer/platform/fonts/win/font_cache_skia_win.cc
@@ -48,7 +48,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-HashMap<String, sk_sp<SkTypeface>>* FontCache::sideloaded_fonts_ = nullptr;
+HashMap<String, sk_sp<SkTypeface>, CaseFoldingHash>*
+    FontCache::sideloaded_fonts_ = nullptr;
 
 // Cached system font metrics.
 AtomicString* FontCache::menu_font_family_name_ = nullptr;
@@ -72,10 +73,11 @@ int32_t EnsureMinimumFontHeightIfNeeded(int32_t font_height) {
 // static
 void FontCache::AddSideloadedFontForTesting(sk_sp<SkTypeface> typeface) {
   if (!sideloaded_fonts_)
-    sideloaded_fonts_ = new HashMap<String, sk_sp<SkTypeface>>;
+    sideloaded_fonts_ = new HashMap<String, sk_sp<SkTypeface>, CaseFoldingHash>;
   SkString name;
   typeface->getFamilyName(&name);
-  sideloaded_fonts_->Set(name.c_str(), std::move(typeface));
+  String name_wtf(name.c_str());
+  sideloaded_fonts_->Set(name_wtf, std::move(typeface));
 }
 
 // static
