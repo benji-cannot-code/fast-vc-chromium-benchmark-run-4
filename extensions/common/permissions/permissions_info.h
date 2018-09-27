a6f7ebf9928f19118d78d6360147b12e37754fff
David Bertoni <dbertoni@chromium.org>
2018-09-27 00:31:56 +0000

    [Extensions] Changed PermissionsInfo::IDMap to use unordered_map.
    Bug: 847237
    Change-Id: I4718a8ea2d4b6d2b74d632f06f614dd042f04a07
    Reviewed-on: https://chromium-review.googlesource.com/1246941
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594536}

--- extensions/common/permissions/permissions_info.h
+++ extensions/common/permissions/permissions_info.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <set>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 #include "base/callback.h"
@@ -72,7 +73,9 @@ class PermissionsInfo {
   void RegisterPermission(std::unique_ptr<APIPermissionInfo> permission);
 
   // Maps permission ids to permissions. Owns the permissions.
-  typedef std::map<APIPermission::ID, std::unique_ptr<APIPermissionInfo>> IDMap;
+  typedef std::unordered_map<APIPermission::ID,
+                             std::unique_ptr<APIPermissionInfo>>
+      IDMap;
 
   // Maps names and aliases to permissions. Doesn't own the permissions.
   typedef std::map<std::string, APIPermissionInfo*> NameMap;
