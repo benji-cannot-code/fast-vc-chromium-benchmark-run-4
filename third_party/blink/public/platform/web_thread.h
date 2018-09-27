289dd2e4d16c00a4f9d51476abbb2e0a332eeea5
Yuta Kitamura <yutak@chromium.org>
2018-09-27 07:51:22 +0000

    Remove const qualifier from WebThread::Scheduler().
    This function returns a mutable pointer, thus it does not make sense to
    have this function const-qualified.
    
    Bug: 826203
    Change-Id: I7ea049c9a86b79db7bab5e26bf16b0499d71a08d
    Reviewed-on: https://chromium-review.googlesource.com/1247643
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Yuta Kitamura <yutak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594626}

--- third_party/blink/public/platform/web_thread.h
+++ third_party/blink/public/platform/web_thread.h
@@ -115,7 +115,7 @@ class BLINK_PLATFORM_EXPORT WebThread {
       base::sequence_manager::TaskTimeObserver*) {}
 
   // Returns the scheduler associated with the thread.
-  virtual ThreadScheduler* Scheduler() const = 0;
+  virtual ThreadScheduler* Scheduler() = 0;
 
   virtual ~WebThread() = default;
 
--- third_party/blink/renderer/platform/scheduler/renderer/webthread_impl_for_renderer_scheduler.cc
+++ third_party/blink/renderer/platform/scheduler/renderer/webthread_impl_for_renderer_scheduler.cc
@@ -25,7 +25,7 @@ blink::PlatformThreadId WebThreadImplForRendererScheduler::ThreadId() const {
   return thread_id_;
 }
 
-blink::ThreadScheduler* WebThreadImplForRendererScheduler::Scheduler() const {
+blink::ThreadScheduler* WebThreadImplForRendererScheduler::Scheduler() {
   return scheduler_;
 }
 
