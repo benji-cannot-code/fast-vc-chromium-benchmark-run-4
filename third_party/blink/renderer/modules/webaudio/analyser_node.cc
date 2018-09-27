12fdc542ccfab0f42ab79c3f0106fc778146c227
Raymond Toy <rtoy@chromium.org>
2018-09-27 21:02:02 +0000

    AnalyserNode channelCount is 2
    The spec says the channelCount should be 2 not 1 for an AnalyserNode.
    
    Remove the test expectations too since we pass the tests now.
    
    Bug: 889640, 888870
    Change-Id: I814fc31d0e15c371f9ae74dd8e3ce2fb0a906197
    Reviewed-on: https://chromium-review.googlesource.com/1247241
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Commit-Queue: Raymond Toy <rtoy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594855}

--- third_party/blink/renderer/modules/webaudio/analyser_node.cc
+++ third_party/blink/renderer/modules/webaudio/analyser_node.cc
@@ -36,7 +36,7 @@ namespace blink {
 
 AnalyserHandler::AnalyserHandler(AudioNode& node, float sample_rate)
     : AudioBasicInspectorHandler(kNodeTypeAnalyser, node, sample_rate, 1) {
-  channel_count_ = 1;
+  channel_count_ = 2;
   Initialize();
 }
 
