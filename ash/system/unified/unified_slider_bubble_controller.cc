56339f11e8b1d32f944fe9393c2a46e82a9b1853
Tetsui Ohkubo <tetsui@chromium.org>
2018-09-27 06:26:05 +0000

    Unified: Hide UnifiedSystemTray in kiosk app mode.
    On Kiosk mode, we shouldn't show volume slider bubble. The check was
    missing in UnifiedSystemTray. It caused UI regression in Kiosk devices.
    (See the bug for detail).
    
    TEST=manual(confirmed on a Kiosk device that the slider is not shown)
    BUG=888890
    
    Change-Id: I00359892b6bfc624b95c5f2a57fc7802b38a4dca
    Reviewed-on: https://chromium-review.googlesource.com/1247841
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594609}

--- ash/system/unified/unified_slider_bubble_controller.cc
+++ ash/system/unified/unified_slider_bubble_controller.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/public/cpp/app_list/app_list_features.h"
 #include "ash/root_window_controller.h"
+#include "ash/session/session_controller.h"
 #include "ash/shell.h"
 #include "ash/system/brightness/unified_brightness_slider_controller.h"
 #include "ash/system/keyboard_brightness/unified_keyboard_brightness_slider_controller.h"
@@ -114,6 +115,10 @@ void UnifiedSliderBubbleController::OnAudioSettingsButtonClicked() {
 }
 
 void UnifiedSliderBubbleController::ShowBubble(SliderType slider_type) {
+  // Never show slider bubble in kiosk app mode.
+  if (Shell::Get()->session_controller()->IsRunningInAppMode())
+    return;
+
   if (IsAnyMainBubbleShown()) {
     tray_->EnsureBubbleExpanded();
     return;
