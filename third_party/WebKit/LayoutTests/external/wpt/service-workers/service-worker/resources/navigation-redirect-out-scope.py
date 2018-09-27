f8352da6a10999a991c214c0b4493c42ed9cff1b
Matt Falkenhagen <falken@chromium.org>
2018-09-27 15:02:01 +0000

    WPT: service worker: Fix potential race in navigation-redirect.https.html.
    Before this CL, the test makes a frame which posts a message to the test
    window. But if the test fails, we might not consume the message with
    `await` and move on the next test. It's possible the next test gets an
    unexpected message from the previous test.
    
    I think a related issue caused the timeout after the Clients variant was
    added to the test, since the message handler was created after the frame
    was created and it was possible the message arrived after that.
    
    Fix things by explicitly asking the frame for the message when we're
    ready for it.
    
    Bug: 876223, 778497
    Change-Id: I82b8685df2977aeadea7475965ecccf69bd33111
    Reviewed-on: https://chromium-review.googlesource.com/1249423
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594723}

--- third_party/WebKit/LayoutTests/external/wpt/service-workers/service-worker/resources/navigation-redirect-out-scope.py
+++ third_party/WebKit/LayoutTests/external/wpt/service-workers/service-worker/resources/navigation-redirect-out-scope.py
@@ -12,10 +12,12 @@ def main(request, response):
     return status, [("content-type", "text/html")], '''
 <!DOCTYPE html>
 <script>
+onmessage = event => {
   window.parent.postMessage(
       {
-        id: 'last_url',
+        id: event.data.id,
         result: location.href
       }, '*');
+};
 </script>
 '''
