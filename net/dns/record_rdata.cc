1061c1d3b122f387f0153ed8f4c72c3d1196829e
Qingsi Wang <qingsi@google.com>
2018-09-27 21:35:55 +0000

    Enable parsing of DNS queries and writing of DNS responses.
    This CL adds utilities to DnsQuery to allow the parsing of a DNS
    query in the wire format if it is a valid query with a single question.
    A new constructor of DnsResponder is also added to generate DNS
    responses in the wire format with multiple resource record as answers.
    Name compression in multiple answers are not yet implemented in this CL.
    
    Combined with the existing utilities in //net/dns/, we now have a full
    set of capabilities to parse and generate DNS/mDNS queries and response.
    
    Bug: 878465
    Change-Id: Ifeae0d17243df037af7a3ea0bffb1079018ea714
    Reviewed-on: https://chromium-review.googlesource.com/1203320
    Commit-Queue: Qingsi Wang <qingsi@google.com>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594877}

--- net/dns/record_rdata.cc
+++ net/dns/record_rdata.cc
@@ -17,6 +17,26 @@ static const size_t kSrvRecordMinimumSize = 6;
 
 RecordRdata::RecordRdata() = default;
 
+bool RecordRdata::HasValidSize(const base::StringPiece& data, uint16_t type) {
+  switch (type) {
+    case dns_protocol::kTypeSRV:
+      return data.size() >= kSrvRecordMinimumSize;
+    case dns_protocol::kTypeA:
+      return data.size() == IPAddress::kIPv4AddressSize;
+    case dns_protocol::kTypeAAAA:
+      return data.size() == IPAddress::kIPv6AddressSize;
+    case dns_protocol::kTypeCNAME:
+    case dns_protocol::kTypePTR:
+    case dns_protocol::kTypeTXT:
+    case dns_protocol::kTypeNSEC:
+    case dns_protocol::kTypeOPT:
+      return true;
+    default:
+      VLOG(1) << "Unsupported RDATA type.";
+      return false;
+  }
+}
+
 SrvRecordRdata::SrvRecordRdata() : priority_(0), weight_(0), port_(0) {
 }
 
@@ -26,7 +46,7 @@ SrvRecordRdata::~SrvRecordRdata() = default;
 std::unique_ptr<SrvRecordRdata> SrvRecordRdata::Create(
     const base::StringPiece& data,
     const DnsRecordParser& parser) {
-  if (data.size() < kSrvRecordMinimumSize)
+  if (!HasValidSize(data, kType))
     return std::unique_ptr<SrvRecordRdata>();
 
   std::unique_ptr<SrvRecordRdata> rdata(new SrvRecordRdata);
@@ -65,7 +85,7 @@ ARecordRdata::~ARecordRdata() = default;
 std::unique_ptr<ARecordRdata> ARecordRdata::Create(
     const base::StringPiece& data,
     const DnsRecordParser& parser) {
-  if (data.size() != IPAddress::kIPv4AddressSize)
+  if (!HasValidSize(data, kType))
     return std::unique_ptr<ARecordRdata>();
 
   std::unique_ptr<ARecordRdata> rdata(new ARecordRdata);
@@ -92,7 +112,7 @@ AAAARecordRdata::~AAAARecordRdata() = default;
 std::unique_ptr<AAAARecordRdata> AAAARecordRdata::Create(
     const base::StringPiece& data,
     const DnsRecordParser& parser) {
-  if (data.size() != IPAddress::kIPv6AddressSize)
+  if (!HasValidSize(data, kType))
     return std::unique_ptr<AAAARecordRdata>();
 
   std::unique_ptr<AAAARecordRdata> rdata(new AAAARecordRdata);
