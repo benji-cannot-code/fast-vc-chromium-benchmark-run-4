9973cc6d4a45df2ff17073726e5bff5dca9586d1
Sam McNally <sammc@chromium.org>
2018-09-27 03:55:24 +0000

    Recover unsynced Drive files to Downloads when switching to DriveFS.
    When running with DriveFS enabled for the first time, recover any files
    in the local cache marked as dirty to a folder in Downloads, similar to
    the existing process for recovering from database errors, and clean up
    any old Drive client metadata. The existing Drive client code does not
    expect dirty files to be removed from its cache so clearing its metadata
    is necessary to limit confusion when switching back from DriveFS.
    
    Reset the DriveFS migration pref when running without DriveFS; each
    first DriveFS after running without DriveFS should migrate pinned files
    and recover any unsynced files. This should avoid losing data if a user
    toggles between DriveFS and not multiple times with unsynced files.
    
    Move Drive and DriveFS data to separate subdirectories in files app
    browsertests to avoid interference, in particular in DriveFS migration
    tests.
    
    Bug: 883242
    Tbr: slangley@chromium.org
    Change-Id: I20199ffd04ad7f63cb77ac9a53e49ba1bf830502
    Reviewed-on: https://chromium-review.googlesource.com/1242783
    Commit-Queue: Sam McNally <sammc@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Reviewed-by: Sergei Datsenko <dats@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594585}

--- chrome/browser/chromeos/file_manager/file_manager_browsertest_base.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest_base.cc
@@ -805,8 +805,8 @@ class DriveTestVolume : public TestVolume {
 
     EXPECT_FALSE(integration_service_);
     integration_service_ = new drive::DriveIntegrationService(
-        profile, nullptr, fake_drive_service_, std::string(), root_path(),
-        nullptr, CreateDriveFsConnectionDelegate());
+        profile, nullptr, fake_drive_service_, std::string(),
+        root_path().Append("v1"), nullptr, CreateDriveFsConnectionDelegate());
 
     return integration_service_;
   }
@@ -882,7 +882,7 @@ class DriveFsTestVolume : public DriveTestVolume {
   }
 
   void InitializeFakeDriveFs() {
-    fake_drivefs_ = std::make_unique<drivefs::FakeDriveFs>(root_path());
+    fake_drivefs_ = std::make_unique<drivefs::FakeDriveFs>(mount_path());
     fake_drivefs_->RegisterMountingForAccountId(base::BindRepeating(
         [](Profile* profile) {
           auto* user =
@@ -934,14 +934,16 @@ class DriveFsTestVolume : public DriveTestVolume {
       const AddEntriesMessage::TestEntryInfo& entry) {
     const base::FilePath target_path = GetTargetPathForTestEntry(entry);
     base::FilePath drive_path("/");
-    CHECK(root_path().AppendRelativePath(target_path, &drive_path));
+    CHECK(mount_path().AppendRelativePath(target_path, &drive_path));
     return drive_path;
   }
 
-  base::FilePath GetMyDrivePath() { return root_path().Append("root"); }
+  base::FilePath mount_path() { return root_path().Append("v2"); }
+
+  base::FilePath GetMyDrivePath() { return mount_path().Append("root"); }
 
   base::FilePath GetTeamDriveGrandRoot() {
-    return root_path().Append("team_drives");
+    return mount_path().Append("team_drives");
   }
 
   base::FilePath GetTeamDrivePath(const std::string& team_drive_name) {
@@ -1373,6 +1375,12 @@ FileManagerBrowserTestBase::CreateDriveIntegrationService(Profile* profile) {
                                    kPredefinedProfileSalt);
     drive_volumes_[profile->GetOriginalProfile()] =
         std::make_unique<DriveFsTestVolume>(profile->GetOriginalProfile());
+    if (!IsIncognitoModeTest()) {
+      base::ThreadTaskRunnerHandle::Get()->PostTask(
+          FROM_HERE,
+          base::BindOnce(base::IgnoreResult(&LocalTestVolume::Mount),
+                         base::Unretained(local_volume_.get()), profile));
+    }
   } else {
     drive_volumes_[profile->GetOriginalProfile()] =
         std::make_unique<DriveTestVolume>();
