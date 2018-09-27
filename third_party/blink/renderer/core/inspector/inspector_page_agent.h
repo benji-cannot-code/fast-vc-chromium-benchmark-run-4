5a6186550ed157346d5fdfdf0b2b850220a082f2
Pavel Feldman <pfeldman@chromium.org>
2018-09-27 00:46:24 +0000

    DevTools: allow addScriptToEvaluateOnNewDocument accept optional worldName parameter.
    Change-Id: I573cc9766e05d24eb9288c7bd5cd25a53c1d0b72
    Reviewed-on: https://chromium-review.googlesource.com/1247186
    Commit-Queue: Pavel Feldman <pfeldman@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594544}

--- third_party/blink/renderer/core/inspector/inspector_page_agent.h
+++ third_party/blink/renderer/core/inspector/inspector_page_agent.h
@@ -116,6 +116,7 @@ class CORE_EXPORT InspectorPageAgent final
       const String& identifier) override;
   protocol::Response addScriptToEvaluateOnNewDocument(
       const String& source,
+      Maybe<String> world_name,
       String* identifier) override;
   protocol::Response removeScriptToEvaluateOnNewDocument(
       const String& identifier) override;
@@ -250,6 +251,7 @@ class CORE_EXPORT InspectorPageAgent final
   InspectorAgentState::Boolean lifecycle_events_enabled_;
   InspectorAgentState::Boolean bypass_csp_enabled_;
   InspectorAgentState::StringMap scripts_to_evaluate_on_load_;
+  InspectorAgentState::StringMap worlds_to_evaluate_on_load_;
   InspectorAgentState::String standard_font_family_;
   InspectorAgentState::String fixed_font_family_;
   InspectorAgentState::String serif_font_family_;
