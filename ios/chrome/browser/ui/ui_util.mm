17865100617920d7e90fb6ad0b79f09222a5b947
Mark Cogan <marq@google.com>
2018-09-27 10:09:06 +0000

    [iOS] Remove IsRefreshInfobarEnabled()
    This CL removes the IsRefreshInfobarEnabled() flag function and refactors code around its use.
    
    Bug: 884722
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: I90dea9cf5e196f455eda1fe516140daa8ad68a8c
    Reviewed-on: https://chromium-review.googlesource.com/1245366
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Reviewed-by: Peter Lee <pkl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594654}

--- ios/chrome/browser/ui/ui_util.mm
+++ ios/chrome/browser/ui/ui_util.mm
@@ -59,10 +59,6 @@ bool IsIPhoneX() {
           (height == 2436 || height == 2688 || height == 1792));
 }
 
-bool IsRefreshInfobarEnabled() {
-  return true;
-}
-
 bool IsRefreshLocationBarEnabled() {
   return true;
 }
