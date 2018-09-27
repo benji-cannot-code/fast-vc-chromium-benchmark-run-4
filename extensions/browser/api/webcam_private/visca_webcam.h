fe94584618ac8a6726e8402f3f6bd2112f8d90b3
Daniel Fenner <dfenner@google.com>
2018-09-27 11:38:55 +0000

    Expand webcam_private api such that we can control auto focus setting ('on' or 'off') and the focus value. The focus value can be read back with the get command. Implemented for Visca and UVC cameras.
    Bug: 888475
    Change-Id: I41ac7c75e4e0893d085b7b1599ead7ffba7b5d26
    Reviewed-on: https://chromium-review.googlesource.com/1240281
    Reviewed-by: Zachary Kuznia <zork@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Daniel Fenner <dfenner@google.com>
    Cr-Commit-Position: refs/heads/master@{#594672}

--- extensions/browser/api/webcam_private/visca_webcam.h
+++ extensions/browser/api/webcam_private/visca_webcam.h
@@ -38,6 +38,7 @@ class ViscaWebcam : public Webcam {
     INQUIRY_PAN,
     INQUIRY_TILT,
     INQUIRY_ZOOM,
+    INQUIRY_FOCUS,
   };
 
   using CommandCompleteCallback =
@@ -99,6 +100,7 @@ class ViscaWebcam : public Webcam {
   void GetPan(const GetPTZCompleteCallback& callback) override;
   void GetTilt(const GetPTZCompleteCallback& callback) override;
   void GetZoom(const GetPTZCompleteCallback& callback) override;
+  void GetFocus(const GetPTZCompleteCallback& callback) override;
   void SetPan(int value,
               int pan_speed,
               const SetPTZCompleteCallback& callback) override;
@@ -116,6 +118,9 @@ class ViscaWebcam : public Webcam {
              bool tilt,
              bool zoom,
              const SetPTZCompleteCallback& callback) override;
+  void SetFocus(int value, const SetPTZCompleteCallback& callback) override;
+  void SetAutofocusState(AutofocusState state,
+                         const SetPTZCompleteCallback& callback) override;
 
   // Used only in unit tests in place of Open().
   void OpenForTesting(std::unique_ptr<SerialConnection> serial_connection);
