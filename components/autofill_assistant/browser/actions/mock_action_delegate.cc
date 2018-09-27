6c1f7eba25cda0422aa3f7267129bd47c35035d8
Jordan Demeulenaere <jdemeulenaere@google.com>
2018-09-27 14:46:23 +0000

    [Autofill Assistant] Add tests for AutofillAction.
    Bug: 806868
    Change-Id: Id9ffa19550de44af7e4f89bc8f3d4439e572398f
    Reviewed-on: https://chromium-review.googlesource.com/1248784
    Commit-Queue: Jordan Demeulenaere <jdemeulenaere@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594717}

---
+++ components/autofill_assistant/browser/actions/mock_action_delegate.cc
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/autofill_assistant/browser/actions/mock_action_delegate.h"
+
+namespace autofill_assistant {
+
+MockActionDelegate::MockActionDelegate() = default;
+MockActionDelegate::~MockActionDelegate() = default;
+
+}  // namespace autofill_assistant
