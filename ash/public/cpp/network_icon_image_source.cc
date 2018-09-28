a8ce791bb9436e7295f33c4296b267020c2b8f0b
Tetsui Ohkubo <tetsui@chromium.org>
2018-09-28 17:55:58 +0000

    Unified: Fix cellular network badge padding.
    According to spec, network's tray icon size changed from 16dip to 20dip,
    and we no longer have 2dip padding around network icons.
    
    Also, because of that, new we can remove 1dip y-axis adjustment of
    |top_left| badge e.g. "LTE" for cellular network.
    
    For detail, see the screenshot in the bug.
    
    TEST=manual
    BUG=889589
    
    Change-Id: Ib5eee3d05116e9deff936e6cb5287de9ac68a2de
    Reviewed-on: https://chromium-review.googlesource.com/1250381
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595150}

--- ash/public/cpp/network_icon_image_source.cc
+++ ash/public/cpp/network_icon_image_source.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/network_icon_image_source.h"
 
 #include "ash/public/cpp/ash_constants.h"
+#include "ash/public/cpp/ash_features.h"
 #include "third_party/skia/include/core/SkPath.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/geometry/insets.h"
@@ -76,7 +77,8 @@ void NetworkIconImageSource::Draw(gfx::Canvas* canvas) {
 
   // The other badges are flush against the edges of the canvas, except at the
   // top, where the badge is only 1dp higher than the base image.
-  const int top_badge_y = icon_y - 1;
+  const int top_badge_y =
+      features::IsSystemTrayUnifiedEnabled() ? icon_y : icon_y - 1;
   if (badges_.top_left.icon)
     paint_badge(badges_.top_left, 0, top_badge_y);
   if (badges_.bottom_left.icon) {
