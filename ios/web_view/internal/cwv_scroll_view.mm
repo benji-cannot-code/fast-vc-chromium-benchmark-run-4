45a16a2765e9eacb35fd486567a820d9f2f71083
John Z Wu <jzw@chromium.org>
2018-09-28 06:44:52 +0000

    Forward -[UIScrollViewDelegate scrollViewShouldScrollTop:].
    Bug: 886956
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: Ida9f92b1bea9093d6a46b5d9eeb4ee7da4306141
    Reviewed-on: https://chromium-review.googlesource.com/1249919
    Commit-Queue: John Wu <jzw@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Hiroshi Ichikawa <ichikawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595000}

--- ios/web_view/internal/cwv_scroll_view.mm
+++ ios/web_view/internal/cwv_scroll_view.mm
@@ -169,6 +169,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 }
 
+- (BOOL)webViewScrollViewShouldScrollToTop:
+    (CRWWebViewScrollViewProxy*)webViewScrollViewProxy {
+  if ([_delegate respondsToSelector:@selector(scrollViewShouldScrollToTop:)]) {
+    return [_delegate scrollViewShouldScrollToTop:self];
+  }
+  return YES;
+}
+
 - (void)webViewScrollViewDidResetContentSize:
     (CRWWebViewScrollViewProxy*)webViewScrollViewProxy {
   self.contentSize = _proxy.contentSize;
