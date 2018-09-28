29063be4277d4029fc70241b7c6ce2613f0ba694
Troy Hildebrandt <thildebr@chromium.org>
2018-09-28 19:20:12 +0000

    Add ability to load keys/entries at same time in LevelDB/ProtoDB.
    Keys and entries can currently be loaded separately, but never at the
    same time, meaning it requires separate calls to LoadKeys/LoadEntries
    to get keys and entries. Some users of ProtoDatabase have resorted to
    storing the keys in the entries to get around this.
    
    This CL adds LoadKeysAndEntries(WithFilter) that returns both at the
    same time. ProtoDatabase's new LoadKeysAndEntriesCallback gives back a
    vector of string/proto pairs.
    
    Bug: 883409
    Change-Id: Id624bbdeb356395c6ce679528d85c1f934a9772e
    Reviewed-on: https://chromium-review.googlesource.com/1228305
    Commit-Queue: Troy Hildebrandt <thildebr@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595175}

--- components/leveldb_proto/proto_database.h
+++ components/leveldb_proto/proto_database.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_LEVELDB_PROTO_PROTO_DATABASE_H_
 #define COMPONENTS_LEVELDB_PROTO_PROTO_DATABASE_H_
 
+#include <map>
 #include <memory>
 #include <string>
 #include <utility>
@@ -33,6 +34,9 @@ class ProtoDatabase {
   using LoadKeysCallback =
       base::OnceCallback<void(bool success,
                               std::unique_ptr<std::vector<std::string>>)>;
+  using LoadKeysAndEntriesCallback =
+      base::OnceCallback<void(bool success,
+                              std::unique_ptr<std::map<std::string, T>>)>;
   using GetCallback =
       base::OnceCallback<void(bool success, std::unique_ptr<T>)>;
   using DestroyCallback = base::OnceCallback<void(bool success)>;
@@ -79,6 +83,17 @@ class ProtoDatabase {
                                      const leveldb::ReadOptions& options,
                                      const std::string& target_prefix,
                                      LoadCallback callback) = 0;
+
+  virtual void LoadKeysAndEntries(
+      typename ProtoDatabase<T>::LoadKeysAndEntriesCallback callback) = 0;
+  virtual void LoadKeysAndEntriesWithFilter(
+      const LevelDB::KeyFilter& filter,
+      typename ProtoDatabase<T>::LoadKeysAndEntriesCallback callback) = 0;
+  virtual void LoadKeysAndEntriesWithFilter(
+      const LevelDB::KeyFilter& filter,
+      const leveldb::ReadOptions& options,
+      const std::string& target_prefix,
+      typename ProtoDatabase<T>::LoadKeysAndEntriesCallback callback) = 0;
 
   // Asynchronously loads all keys from the database and invokes |callback| with
   // those keys when complete.
