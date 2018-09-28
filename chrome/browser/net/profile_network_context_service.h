b0cfc4a3c4c6bcde8d8bc15d141e108b63251396
John Abd-El-Malek <jam@chromium.org>
2018-09-28 22:14:05 +0000

    Fix SetCTPolicy not getting called on network service restart.
    Bug: 890104
    Change-Id: Id2f726cf4a2052080c1f984cb5844f1d084bf1b0
    Reviewed-on: https://chromium-review.googlesource.com/1249732
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595230}

--- chrome/browser/net/profile_network_context_service.h
+++ chrome/browser/net/profile_network_context_service.h
@@ -10,9 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_path.h"
 #include "base/macros.h"
+#include "base/timer/timer.h"
 #include "chrome/browser/net/proxy_config_monitor.h"
 #include "components/content_settings/core/browser/content_settings_observer.h"
 #include "components/keyed_service/core/keyed_service.h"
+#include "components/prefs/pref_change_registrar.h"
 #include "components/prefs/pref_member.h"
 #include "services/network/public/mojom/network_context.mojom.h"
 
@@ -87,6 +89,15 @@ class ProfileNetworkContextService : public KeyedService,
 
   void UpdateReferrersEnabled();
 
+  // Update the CTPolicy for the given NetworkContexts.
+  void UpdateCTPolicyForContexts(
+      const std::vector<network::mojom::NetworkContext*>& contexts);
+
+  // Update the CTPolicy for the all of profiles_'s NetworkContexts.
+  void UpdateCTPolicy();
+
+  void ScheduleUpdateCTPolicy();
+
   // Creates parameters for the NetworkContext. Use |in_memory| instead of
   // |profile_->IsOffTheRecord()| because sometimes normal profiles want off the
   // record partitions (e.g. for webview tag).
@@ -124,8 +135,11 @@ class ProfileNetworkContextService : public KeyedService,
   BooleanPrefMember quic_allowed_;
   StringPrefMember pref_accept_language_;
   BooleanPrefMember block_third_party_cookies_;
-
   BooleanPrefMember enable_referrers_;
+  PrefChangeRegistrar pref_change_registrar_;
+
+  // Used to post schedule CT policy updates
+  base::OneShotTimer ct_policy_update_timer_;
 
   DISALLOW_COPY_AND_ASSIGN(ProfileNetworkContextService);
 };
--- chrome/browser/profiles/profile_impl.h
+++ chrome/browser/profiles/profile_impl.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
-#include "base/timer/timer.h"
 #include "build/build_config.h"
 #include "chrome/browser/net/reporting_permissions_checker.h"
 #include "chrome/browser/profiles/profile.h"
@@ -192,9 +191,6 @@ class ProfileImpl : public Profile {
   void UpdateNameInStorage();
   void UpdateAvatarInStorage();
   void UpdateIsEphemeralInStorage();
-  void UpdateCTPolicy();
-
-  void ScheduleUpdateCTPolicy();
 
   void GetMediaCacheParameters(base::FilePath* cache_path, int* max_size);
 
@@ -290,9 +286,6 @@ class ProfileImpl : public Profile {
   Profile::Delegate* delegate_;
 
   ReportingPermissionsCheckerFactory reporting_permissions_checker_factory_;
-
-  // Used to post schedule CT policy updates
-  base::OneShotTimer ct_policy_update_timer_;
 
   DISALLOW_COPY_AND_ASSIGN(ProfileImpl);
 };
