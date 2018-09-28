5ac7dbf7d5b3495dd8454e50e9a35e5a2cd54714
Koji Ishii <kojii@chromium.org>
2018-09-28 09:05:30 +0000

    [LayoutNG] Match AddSelfOutlineRects to legacy
    For AddSelfOutlineRects to reach to LayoutInline that has
    a continuation, this patch splits the logic in
    AddSelfOutlineRects so that it can be called recursively.
    
    In doing so, the logic flow and functions are organized to
    match to the one in LayoutObject tree.
    
    Bug: 889721
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng
    Change-Id: I79e00ea7c37c94bf06e564a69fe7fc5da4f72349
    Reviewed-on: https://chromium-review.googlesource.com/1246881
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Aleks Totic <atotic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595028}

--- third_party/blink/renderer/core/layout/ng/ng_physical_fragment.cc
+++ third_party/blink/renderer/core/layout/ng/ng_physical_fragment.cc
@@ -279,7 +279,7 @@ Node* NGPhysicalFragment::GetNode() const {
 }
 
 bool NGPhysicalFragment::HasLayer() const {
-  return layout_object_->HasLayer();
+  return layout_object_ && layout_object_->HasLayer();
 }
 
 PaintLayer* NGPhysicalFragment::Layer() const {
