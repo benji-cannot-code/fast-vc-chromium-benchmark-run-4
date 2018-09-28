615f79f46f07d30082b883d3dee47764e4c6fd1c
Jeffrey Kardatzke <jkardatzke@google.com>
2018-09-28 19:12:09 +0000

    Add autotest private API for taking a screenshot
    This is a temporary solution to taking screenshots for tast-tests that
    will work properly on ARM devices as well as deal with hardware
    overlays. Longer term the plan is to implement this via the DRM API with
    a write-back connector and then use that from the CLI screenshot
    program.
    
    BUG=chromium:849438,chromium:880597,chromium:887016
    TEST=tast run graphics.Screenshot (after updating it to use this)
    
    Change-Id: I3919822599338e4786c715d7b0419742417b83c2
    Reviewed-on: https://chromium-review.googlesource.com/1247181
    Commit-Queue: Jeffrey Kardatzke <jkardatzke@google.com>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595173}

--- chrome/browser/chromeos/extensions/autotest_private/autotest_private_api.h
+++ chrome/browser/chromeos/extensions/autotest_private/autotest_private_api.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/interfaces/ash_message_center_controller.mojom.h"
 #include "chrome/browser/chromeos/printing/cups_printers_manager.h"
 #include "chromeos/services/machine_learning/public/mojom/machine_learning_service.mojom.h"
+#include "ui/snapshot/screenshot_grabber.h"
 #endif
 
 namespace message_center {
@@ -309,6 +310,23 @@ class AutotestPrivateRunCrostiniUninstallerFunction
 #endif
 
   DISALLOW_COPY_AND_ASSIGN(AutotestPrivateRunCrostiniUninstallerFunction);
+};
+
+class AutotestPrivateTakeScreenshotFunction : public UIThreadExtensionFunction {
+ public:
+  AutotestPrivateTakeScreenshotFunction() = default;
+  DECLARE_EXTENSION_FUNCTION("autotestPrivate.takeScreenshot",
+                             AUTOTESTPRIVATE_TAKESCREENSHOT)
+
+ private:
+  ~AutotestPrivateTakeScreenshotFunction() override;
+  ResponseAction Run() override;
+#if defined(OS_CHROMEOS)
+  void ScreenshotTaken(std::unique_ptr<ui::ScreenshotGrabber> grabber,
+                       ui::ScreenshotResult screenshot_result,
+                       scoped_refptr<base::RefCountedMemory> png_data);
+#endif
+  DISALLOW_COPY_AND_ASSIGN(AutotestPrivateTakeScreenshotFunction);
 };
 
 class AutotestPrivateGetPrinterListFunction : public UIThreadExtensionFunction {
