6bd4b68472b9eef09fa05d5b0e85ad6bf7e2e3d8
Torne (Richard Coles) <torne@google.com>
2018-09-28 19:37:31 +0000

    Remove support for obsolete bionic versions.
    The supported version of the Android NDK now provides sys/ucontext.h for
    all architectures, and we no longer need to provide our own definitions.
    
    Keep the ones used by PNaCl but update the defines to no longer use them
    on Android.
    
    Bug: 437330
    Change-Id: Ia46c746b17b72261842284d4fed3772fea3a96ef
    Reviewed-on: https://chromium-review.googlesource.com/1252243
    Reviewed-by: Chris Palmer <palmer@chromium.org>
    Commit-Queue: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595181}

--- sandbox/linux/system_headers/i386_linux_ucontext.h
+++ sandbox/linux/system_headers/i386_linux_ucontext.h
@@ -9,22 +9,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdint.h>
 
-// We do something compatible with glibc. Hopefully, at some point Android will
-// provide that for us, and __BIONIC_HAVE_UCONTEXT_T should be defined.
 // This is mostly copied from breakpad (common/android/include/sys/ucontext.h),
 // except we do use sigset_t for uc_sigmask instead of a custom type.
 
-#if !defined(__BIONIC_HAVE_UCONTEXT_T)
-#if !defined(__native_client_nonsfi__)
-#include <asm/sigcontext.h>
-#else
 // In PNaCl toolchain, sigcontext is not defined. So here declare it.
 typedef struct sigaltstack {
   void* ss_sp;
   int ss_flags;
   size_t ss_size;
 } stack_t;
-#endif
 
 /* 80-bit floating-point register */
 struct _libc_fpreg {
@@ -89,9 +82,5 @@ typedef struct ucontext {
   };
   struct _libc_fpstate __fpregs_mem;
 } ucontext_t;
-
-#else
-#include <sys/ucontext.h>
-#endif  // __BIONIC_HAVE_UCONTEXT_T
 
 #endif  // SANDBOX_LINUX_SYSTEM_HEADERS_ANDROID_I386_UCONTEXT_H_
