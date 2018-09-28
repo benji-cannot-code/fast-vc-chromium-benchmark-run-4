4fbd95bbe813765118e8a296db61b453eb80bc1e
Joanmarie Diggs <jdiggs@igalia.com>
2018-09-28 22:23:23 +0000

    Include selected listbox/combobox options in accessible name calculation
    Step 2E of the Accessible Name and Description Computation spec states:
    "If the embedded control has role combobox or listbox, return the text
    alternative of the chosen option." This part of the accessible name
    calculation had not been implemented in AXNodeObject::TextAlternative.
    
    Bug: 890066
    Change-Id: I3829d8229b816fe69e939307aa6c44563a757eef
    Reviewed-on: https://chromium-review.googlesource.com/1249887
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Joanmarie Diggs <joanmarie.diggs@gmail.com>
    Cr-Commit-Position: refs/heads/master@{#595234}

--- third_party/blink/renderer/modules/accessibility/ax_object.h
+++ third_party/blink/renderer/modules/accessibility/ax_object.h
@@ -856,6 +856,7 @@ class MODULES_EXPORT AXObject : public GarbageCollectedFinalized<AXObject> {
   virtual void DetachFromParent() { parent_ = nullptr; }
   virtual AXObject* ScrollBar(AccessibilityOrientation) { return nullptr; }
   virtual void AddAccessibleNodeChildren();
+  virtual void SelectedOptions(AXObjectVector&) const {}
 
   // Properties of the object's owning document or page.
   virtual double EstimatedLoadingProgress() const { return 0; }
@@ -1047,6 +1048,7 @@ class MODULES_EXPORT AXObject : public GarbageCollectedFinalized<AXObject> {
 
   bool CanReceiveAccessibilityFocus() const;
   bool NameFromContents(bool recursive) const;
+  bool NameFromSelectedOption(bool recursive) const;
 
   ax::mojom::Role ButtonRoleType() const;
 
