c96846ba6f20c5cef483e47b239db9b2b1123203
Joel Einbinder <einbinder@chromium.org>
2018-09-28 18:16:57 +0000

    DevTools: Allow namespaced attributes of SVG elements to be edited
    Bug: 875173
    Change-Id: Ifd08a9fe6c6ba08af9f98f92570f523987959e5a
    Reviewed-on: https://chromium-review.googlesource.com/1250170
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Joel Einbinder <einbinder@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595158}

--- third_party/blink/renderer/core/inspector/inspector_dom_agent.cc
+++ third_party/blink/renderer/core/inspector/inspector_dom_agent.cc
@@ -78,6 +78,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/loader/document_loader.h"
 #include "third_party/blink/renderer/core/page/frame_tree.h"
 #include "third_party/blink/renderer/core/page/page.h"
+#include "third_party/blink/renderer/core/svg/svg_svg_element.h"
 #include "third_party/blink/renderer/core/xml/document_xpath_evaluator.h"
 #include "third_party/blink/renderer/core/xml/xpath_result.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
@@ -753,11 +754,15 @@ Response InspectorDOMAgent::setAttributesAsText(int element_id,
       element->GetDocument().IsHTMLDocument() && element->IsHTMLElement();
   // Not all elements can represent the context (i.e. IFRAME), hence using
   // document.body.
-  if (should_ignore_case && element->GetDocument().body())
+  if (should_ignore_case && element->GetDocument().body()) {
     fragment->ParseHTML(markup, element->GetDocument().body(),
                         kAllowScriptingContent);
-  else
-    fragment->ParseXML(markup, nullptr, kAllowScriptingContent);
+  } else {
+    Element* contextElement = nullptr;
+    if (element->IsSVGElement())
+      contextElement = ToSVGElement(element)->ownerSVGElement();
+    fragment->ParseXML(markup, contextElement, kAllowScriptingContent);
+  }
 
   Element* parsed_element =
       fragment->firstChild() && fragment->firstChild()->IsElementNode()
