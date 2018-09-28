6b0ecd7eca87e2a0f42594aad2ebb5f7e150c9ba
Christopher Cameron <ccameron@chromium.org>
2018-09-28 01:06:29 +0000

    RemoteMacViews: Initialize WindowResizeHelperMac
    This was accidentally dropped from crrev.com/594134
    
    Bug: 859152
    Change-Id: I159af36686ce07300969f2eaf79796be85f012e5
    Reviewed-on: https://chromium-review.googlesource.com/1244886
    Commit-Queue: ccameron <ccameron@chromium.org>
    Reviewed-by: ccameron <ccameron@chromium.org>
    Reviewed-by: Sidney San Martín <sdy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594941}

--- chrome/app_shim/chrome_main_app_mode_mac.mm
+++ chrome/app_shim/chrome_main_app_mode_mac.mm
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/binding.h"
 #include "mojo/public/cpp/platform/named_platform_channel.h"
 #include "mojo/public/cpp/system/isolated_connection.h"
+#include "ui/accelerated_widget_mac/window_resize_helper_mac.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/views/cocoa/bridge_factory_impl.h"
@@ -651,6 +652,7 @@ int ChromeAppModeStart_v4(const app_mode::ChromeAppModeInfo* info) {
   AppShimController controller;
   base::MessageLoopForUI main_message_loop;
   base::PlatformThread::SetName("CrAppShimMain");
+  ui::WindowResizeHelperMac::Get()->Init(base::ThreadTaskRunnerHandle::Get());
 
   // In tests, launching Chrome does nothing, and we won't get a ping response,
   // so just assume the socket exists.
