bd4332c76c587dae01ba3f3bfde475f5da358496
Vadym Doroshenko <dvadym@chromium.org>
2018-09-28 11:23:59 +0000

    Clear non needed data from FormData before saving.
    That's good both for privacy and storage reasons.
    
    Bug: 831123
    
    Change-Id: Ie41b6d1f25fe37f86aea9eb4625d8aa878e0a52e
    Reviewed-on: https://chromium-review.googlesource.com/1249205
    Commit-Queue: Vadym Doroshenko <dvadym@chromium.org>
    Reviewed-by: Vaclav Brozek <vabr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595055}

--- components/password_manager/core/browser/form_saver_impl.cc
+++ components/password_manager/core/browser/form_saver_impl.cc
@@ -17,9 +17,31 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/gurl.h"
 #include "url/origin.h"
 
+using autofill::FormData;
+using autofill::FormFieldData;
 using autofill::PasswordForm;
 
 namespace password_manager {
+
+namespace {
+
+// Remove all information from |form| that is not required for signature
+// calculation.
+void SanitizeFormData(FormData* form) {
+  form->main_frame_origin = url::Origin();
+  for (FormFieldData& field : form->fields) {
+    field.label.clear();
+    field.value.clear();
+    field.autocomplete_attribute.clear();
+    field.option_values.clear();
+    field.option_contents.clear();
+    field.placeholder.clear();
+    field.css_classes.clear();
+    field.id.clear();
+  }
+}
+
+}  // namespace
 
 FormSaverImpl::FormSaverImpl(PasswordStore* store) : store_(store) {
   DCHECK(store);
@@ -54,11 +76,13 @@ void FormSaverImpl::Update(
 }
 
 void FormSaverImpl::PresaveGeneratedPassword(const PasswordForm& generated) {
+  auto form = std::make_unique<PasswordForm>(generated);
+  SanitizeFormData(&form->form_data);
   if (presaved_)
-    store_->UpdateLoginWithPrimaryKey(generated, *presaved_);
+    store_->UpdateLoginWithPrimaryKey(*form, *presaved_);
   else
-    store_->AddLogin(generated);
-  presaved_.reset(new PasswordForm(generated));
+    store_->AddLogin(*form);
+  presaved_ = std::move(form);
 }
 
 void FormSaverImpl::RemovePresavedPassword() {
@@ -73,7 +97,7 @@ std::unique_ptr<FormSaver> FormSaverImpl::Clone() {
   auto result = std::make_unique<FormSaverImpl>(store_);
   if (presaved_)
     result->presaved_ = std::make_unique<PasswordForm>(*presaved_);
-  return std::move(result);
+  return result;
 }
 
 void FormSaverImpl::SaveImpl(
@@ -86,18 +110,20 @@ void FormSaverImpl::SaveImpl(
   DCHECK(!pending.blacklisted_by_user);
 
   UpdatePreferredLoginState(pending.username_value, best_matches);
+  PasswordForm sanitized_pending(pending);
+  SanitizeFormData(&sanitized_pending.form_data);
   if (presaved_) {
-    store_->UpdateLoginWithPrimaryKey(pending, *presaved_);
+    store_->UpdateLoginWithPrimaryKey(sanitized_pending, *presaved_);
     presaved_ = nullptr;
   } else if (is_new_login) {
-    store_->AddLogin(pending);
-    if (!pending.username_value.empty())
-      DeleteEmptyUsernameCredentials(pending, best_matches);
+    store_->AddLogin(sanitized_pending);
+    if (!sanitized_pending.username_value.empty())
+      DeleteEmptyUsernameCredentials(sanitized_pending, best_matches);
   } else {
     if (old_primary_key)
-      store_->UpdateLoginWithPrimaryKey(pending, *old_primary_key);
+      store_->UpdateLoginWithPrimaryKey(sanitized_pending, *old_primary_key);
     else
-      store_->UpdateLogin(pending);
+      store_->UpdateLogin(sanitized_pending);
   }
 
   if (credentials_to_update) {
@@ -116,6 +142,7 @@ void FormSaverImpl::UpdatePreferredLoginState(
         form.username_value != preferred_username) {
       // This wasn't the selected login but it used to be preferred.
       PasswordForm update(form);
+      SanitizeFormData(&update.form_data);
       update.preferred = false;
       store_->UpdateLogin(update);
     }
