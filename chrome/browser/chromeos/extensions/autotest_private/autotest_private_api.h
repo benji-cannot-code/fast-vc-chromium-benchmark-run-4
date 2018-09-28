e4109d9b41b24d461f362f7d6376809b81de3c71
Achuith Bhandarkar <achuith@chromium.org>
2018-09-28 08:35:39 +0000

    Make autotest_private chromeos only.
    BUG=None
    Test=browser tests.
    
    Change-Id: Ifa7477e130ffb8e7b526a40d903382c9dce9e116
    Reviewed-on: https://chromium-review.googlesource.com/1235715
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595022}

--- chrome/browser/chromeos/extensions/autotest_private/autotest_private_api.h
+++ chrome/browser/chromeos/extensions/autotest_private/autotest_private_api.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_EXTENSIONS_API_AUTOTEST_PRIVATE_AUTOTEST_PRIVATE_API_H_
-#define CHROME_BROWSER_EXTENSIONS_API_AUTOTEST_PRIVATE_AUTOTEST_PRIVATE_API_H_
+#ifndef CHROME_BROWSER_CHROMEOS_EXTENSIONS_AUTOTEST_PRIVATE_AUTOTEST_PRIVATE_API_H_
+#define CHROME_BROWSER_CHROMEOS_EXTENSIONS_AUTOTEST_PRIVATE_AUTOTEST_PRIVATE_API_H_
 
 #include <string>
 
@@ -383,7 +383,7 @@ void SetAutotestPrivateTest();
 class AutotestPrivateAPI : public BrowserContextKeyedAPI {
  public:
   static BrowserContextKeyedAPIFactory<AutotestPrivateAPI>*
-      GetFactoryInstance();
+  GetFactoryInstance();
 
   // TODO(achuith): Replace these with a mock object for system calls.
   bool test_mode() const { return test_mode_; }
@@ -405,9 +405,9 @@ class AutotestPrivateAPI : public BrowserContextKeyedAPI {
 
 template <>
 KeyedService*
-    BrowserContextKeyedAPIFactory<AutotestPrivateAPI>::BuildServiceInstanceFor(
-        content::BrowserContext* context) const;
+BrowserContextKeyedAPIFactory<AutotestPrivateAPI>::BuildServiceInstanceFor(
+    content::BrowserContext* context) const;
 
 }  // namespace extensions
 
-#endif  // CHROME_BROWSER_EXTENSIONS_API_AUTOTEST_PRIVATE_AUTOTEST_PRIVATE_API_H_
+#endif  // CHROME_BROWSER_CHROMEOS_EXTENSIONS_AUTOTEST_PRIVATE_AUTOTEST_PRIVATE_API_H_
