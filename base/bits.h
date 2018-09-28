ae2793898394907bf921f5f747c7759bc7fa2772
Lei Zhang <thestig@chromium.org>
2018-09-28 13:40:03 +0000

    Include <type_traits> in base/bits.h for std::enable_if and friends.
    One small missing bit from r560988.
    
    Change-Id: Ib6afc4957fdc8eae24b1f21f7929cedbe056cac2
    Reviewed-on: https://chromium-review.googlesource.com/1250205
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595067}

--- base/bits.h
+++ base/bits.h
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdint.h>
 
+#include <type_traits>
+
 #include "base/compiler_specific.h"
 #include "base/logging.h"
 #include "build/build_config.h"
