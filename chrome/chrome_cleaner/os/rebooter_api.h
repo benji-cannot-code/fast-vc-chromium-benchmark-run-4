11d179578e9286bac598c7b39346ae4f2894cb1d
Joe Mason <joenotcharles@chromium.org>
2018-09-28 15:37:13 +0000

    Add chrome_cleaner/components dir
    Fix ICWYU error in chrome_cleaner/os/rebooter_api.h
    
    R=csharp
    
    Bug: 830892
    Change-Id: I3f311bdd32e21b2901594a835aead483ae8d823b
    Reviewed-on: https://chromium-review.googlesource.com/1227683
    Commit-Queue: Joe Mason <joenotcharles@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Chris Sharp <csharp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595101}

--- chrome/chrome_cleaner/os/rebooter_api.h
+++ chrome/chrome_cleaner/os/rebooter_api.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/command_line.h"
 #include "components/chrome_cleaner/public/constants/constants.h"
 
 namespace chrome_cleaner {
---
+++ chrome/chrome_cleaner/test/test_component.cc
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/chrome_cleaner/test/test_component.h"
+
+namespace chrome_cleaner {
+
+TestComponent::Calls::Calls() = default;
+TestComponent::Calls::~Calls() = default;
+
+TestComponent::~TestComponent() {
+  calls_->destroyed = true;
+}
+
+void TestComponent::PreScan() {
+  calls_->pre_scan = true;
+}
+
+void TestComponent::PostScan(const std::vector<UwSId>& found_pups) {
+  calls_->post_scan = true;
+  calls_->post_scan_found_pups = found_pups;
+}
+
+void TestComponent::PreCleanup() {
+  calls_->pre_cleanup = true;
+}
+
+void TestComponent::PostCleanup(ResultCode result_code, RebooterAPI* rebooter) {
+  calls_->result_code = result_code;
+  calls_->post_cleanup = true;
+}
+
+void TestComponent::PostValidation(ResultCode result_code) {
+  calls_->result_code = result_code;
+  calls_->post_validation = true;
+}
+
+void TestComponent::OnClose(ResultCode result_code) {
+  calls_->result_code = result_code;
+  calls_->on_close = true;
+}
+
+}  // namespace chrome_cleaner
