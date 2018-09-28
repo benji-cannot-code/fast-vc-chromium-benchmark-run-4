5fadee77a85ed94eeff45ea002ebc3a856a5f014
Kent Tamura <tkent@chromium.org>
2018-09-28 10:48:46 +0000

    custom-elements: Invalidate :unresolved and :defined correctly.
    We changed the coverage of :unresolved and :defined in
    crrev.com/594582. But the CL didn't invalidate style correctly. This CL
    fixes it.
    
    * node.cc:
     - Invalidate :unresolved on V1 custom element state change
     - Invalidate :defined on V0 custom element state change
    
    * Tests:
      Add v1 custom element + :unresolved test to existing
      unresolved-pseudo.html.
      Add :defined invalidation test.
    
    Bug: 889336
    Change-Id: Id69ba3f9cb62d2805877b648762f4f94bd02e1f9
    Reviewed-on: https://chromium-review.googlesource.com/1250721
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595048}

--- third_party/blink/renderer/core/dom/node.cc
+++ third_party/blink/renderer/core/dom/node.cc
@@ -2621,8 +2621,10 @@ void Node::SetCustomElementState(CustomElementState new_state) {
                 static_cast<NodeFlags>(new_state);
   DCHECK(new_state == GetCustomElementState());
 
-  if (element->IsDefined() != was_defined)
+  if (element->IsDefined() != was_defined) {
     element->PseudoStateChanged(CSSSelector::kPseudoDefined);
+    element->PseudoStateChanged(CSSSelector::kPseudoUnresolved);
+  }
 }
 
 void Node::SetV0CustomElementState(V0CustomElementState new_state) {
@@ -2647,8 +2649,10 @@ void Node::SetV0CustomElementState(V0CustomElementState new_state) {
   SetFlag(kV0CustomElementFlag);
   SetFlag(new_state == kV0Upgraded, kV0CustomElementUpgradedFlag);
 
-  if (old_state == kV0NotCustomElement || new_state == kV0Upgraded)
+  if (old_state == kV0NotCustomElement || new_state == kV0Upgraded) {
     ToElement(this)->PseudoStateChanged(CSSSelector::kPseudoUnresolved);
+    ToElement(this)->PseudoStateChanged(CSSSelector::kPseudoDefined);
+  }
 }
 
 void Node::CheckSlotChange(SlotChangeType slot_change_type) {
