4cc6d3f4886fa7d7fcbae916cf74887dc05a9cf6
W. James MacLean <wjmaclean@chromium.org>
2018-09-29 19:17:03 +0000

    Fix touch-handle positions for OOPIF on Android.
    This CL removes a hack from Android's
    TouchSelectionControllerClientManager that attempts to add the current
    page scale factor in to the handles' coordinates. Somewhere along the
    way the underlying lack of page scale factor in the coordinate
    transforms went away. meaning the hack was causing the coordinates to
    be incorrect.
    
    This CL also makes a small fix to the function
    RenderWidgetHostViewAndroid::TransformPointToCoordSpaceForView that
    generalizes how it checks for the FrameSinkId necessary to do the
    transformations.
    
    Bug: 809122
    Change-Id: I74e7ddc8d448ae28061d34a42c295e5ae4f7f624
    Reviewed-on: https://chromium-review.googlesource.com/1251023
    Commit-Queue: James MacLean <wjmaclean@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595321}

--- content/browser/renderer_host/input/touch_selection_controller_client_manager_android.h
+++ content/browser/renderer_host/input/touch_selection_controller_client_manager_android.h
@@ -22,8 +22,6 @@ class TouchSelectionControllerClientManagerAndroid
   explicit TouchSelectionControllerClientManagerAndroid(
       RenderWidgetHostViewAndroid* rwhv);
   ~TouchSelectionControllerClientManagerAndroid() override;
-  void SetPageScaleFactor(float page_scale_factor);
-  float page_scale_factor() { return page_scale_factor_; }
 
   // TouchSelectionControllerClientManager implementation.
   void DidStopFlinging() override;
@@ -53,7 +51,6 @@ class TouchSelectionControllerClientManagerAndroid
  private:
   RenderWidgetHostViewAndroid* rwhv_;
   TouchSelectionControllerClient* active_client_;
-  float page_scale_factor_;
   gfx::SelectionBound manager_selection_start_;
   gfx::SelectionBound manager_selection_end_;
   base::ObserverList<TouchSelectionControllerClientManager::Observer>::Unchecked
