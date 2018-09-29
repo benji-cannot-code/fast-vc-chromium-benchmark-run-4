763bb60f90f798ca886123afae4896ee5fde490a
Peter Kvitek <kvitekp@chromium.org>
2018-09-29 00:07:16 +0000

    Introduced DevTools API Performance::SetTimeDomain().
    DevTools performance metrics now allows collection and reporting of
    duration related metrics using monotonically increasing abstract time
    and thread running time.
    
    Change-Id: Ica54cc291ba456c8dcc6a47a962e99a87cb8a475
    Reviewed-on: https://chromium-review.googlesource.com/1247528
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Alexei Filippov <alph@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595262}

--- third_party/blink/tools/audit_non_blink_usage.py
+++ third_party/blink/tools/audit_non_blink_usage.py
@@ -53,6 +53,7 @@ _CONFIG = [
             'base::Time',
             'base::TimeDelta',
             'base::TimeTicks',
+            'base::ThreadTicks',
             'base::UnguessableToken',
             'base::UnsafeSharedMemoryRegion',
             'base::WeakPtr',
