f61b3190c45c4a48e3f8d59f3f20c1fc0dfef97d
Dmitry Titov <dimich@chromium.org>
2018-10-01 22:13:43 +0000

    Add GetMontlyHostCount() stat counting method to HistoryService.
    This will be used in Explore Sites to track breadth of web usage.
    It'll be called from ExplreSitesService, on a schedule, approximately once a week.
    
    Bug: 867488
    Change-Id: If535dfead7f11dbaf501472dc42eaa882cbd94c4
    Reviewed-on: https://chromium-review.googlesource.com/1248128
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Commit-Queue: Dmitry Titov <dimich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595572}

--- components/history/core/browser/history_backend.h
+++ components/history/core/browser/history_backend.h
@@ -289,6 +289,9 @@ class HistoryBackend : public base::RefCountedThreadSafe<HistoryBackend>,
   HistoryCountResult GetHistoryCount(const base::Time& begin_time,
                                      const base::Time& end_time);
 
+  // Returns the number of hosts visited in the last month.
+  HistoryCountResult CountUniqueHostsVisitedLastMonth();
+
   // Favicon -------------------------------------------------------------------
 
   void GetFavicon(
