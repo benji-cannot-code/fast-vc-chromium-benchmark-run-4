7384bd4fc6ab0a32bafafa7f25974a25522c3903
Jeffrey Cohen <jeffreycohen@chromium.org>
2018-10-01 19:32:10 +0000

    initialize hats_base upon profile creation
    Bug: 807714
    Change-Id: Ib6a6b2fce7ddb494741364976c73569ce87756f8
    Reviewed-on: https://chromium-review.googlesource.com/1228881
    Commit-Queue: Jeffrey Cohen <jeffreycohen@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595504}

---
+++ chrome/browser/ui/hats/README.md
@@ -0,0 +1,7 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+chrome/browser/ui/hats
+=====================
+
+This directory contains HaTS (Happiness Tracking Survey) code that is used to service the display of surveys launched from any trigger point within Chrome.
+
+This code will coordinate with user's profiles to ensure that Chrome is not serving too many surveys to a single profile, only targeting profiles have UMA enabled, and not targeting enterprise users.
--- chrome/browser/ui/search/search_tab_helper.cc
+++ chrome/browser/ui/search/search_tab_helper.cc
@@ -20,11 +20,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/chrome_select_file_policy.h"
+#include "chrome/browser/ui/hats/hats_service.h"
+#include "chrome/browser/ui/hats/hats_service_factory.h"
 #include "chrome/browser/ui/location_bar/location_bar.h"
 #include "chrome/browser/ui/omnibox/clipboard_utils.h"
 #include "chrome/browser/ui/search/ntp_user_data_logger.h"
 #include "chrome/browser/ui/search/search_ipc_router_policy_impl.h"
 #include "chrome/browser/ui/tab_contents/core_tab_helper.h"
+#include "chrome/common/chrome_features.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/browser_sync/profile_sync_service.h"
@@ -221,6 +224,17 @@ void SearchTabHelper::DidFinishLoad(content::RenderFrameHost* render_frame_host,
     if (search::IsInstantNTP(web_contents_))
       RecordNewTabLoadTime(web_contents_);
   }
+
+#if !defined(OS_ANDROID)
+  if (base::FeatureList::IsEnabled(
+          features::kHappinessTrackingSurveysForDesktop)) {
+    HatsService* hats_service =
+        HatsServiceFactory::GetForProfile(profile(), true);
+    if (hats_service->ShouldShowSurvey()) {
+      // TODO launch bubble;
+    }
+  }
+#endif  // !defined(OS_ANDROID)
 }
 
 void SearchTabHelper::NavigationEntryCommitted(
