d57f1df3380c3205e691e42218ed41c639eb7881
Yuichiro Hanada <yhanada@chromium.org>
2018-10-01 09:02:14 +0000

    Extend InputMethodManager{Host, Instance} mojo interface.
    to support proxying IME APIs between Android IMEs and text fields on a
    browser window.
    
    Bug: 845079
    Change-Id: Ie0df8db88893b9d3e98c9b8494ce887325f15d8f
    Reviewed-on: https://chromium-review.googlesource.com/1177526
    Reviewed-by: Mattias Nissler <mnissler@chromium.org>
    Reviewed-by: Luis Hector Chavez <lhchavez@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Commit-Queue: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595410}

--- components/arc/ime/arc_ime_bridge_impl.h
+++ components/arc/ime/arc_ime_bridge_impl.h
@@ -39,7 +39,7 @@ class ArcImeBridgeImpl : public ArcImeBridge, public mojom::ImeHost {
                                         bool is_available) override;
 
   // mojom::ImeHost overrides:
-  void OnTextInputTypeChanged(mojom::TextInputType type,
+  void OnTextInputTypeChanged(ui::TextInputType type,
                               bool is_personalized_learning_allowed) override;
   void OnCursorRectChanged(const gfx::Rect& rect,
                            bool screen_coordinates) override;
