15a1ea821146ecd9117ebd9148a3d1a115694c66
Yao Xiao <yaoxia@chromium.org>
2018-10-01 22:03:35 +0000

    Update UI to prepare for A/B testing & enabling of ad filtering for abusive sites.
    Bug: 888556
    Change-Id: Ia330d9eade0513738ae7b9f5f81574a57489b14f
    Reviewed-on: https://chromium-review.googlesource.com/1232575
    Commit-Queue: Yao Xiao <yaoxia@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595555}

--- chrome/test/data/webui/settings/site_details_permission_tests.js
+++ chrome/test/data/webui/settings/site_details_permission_tests.js
@@ -279,7 +279,7 @@ suite('SiteDetailsPermission', function() {
       source: settings.SiteSettingSource.ADS_FILTER_BLACKLIST,
     };
     assertEquals(
-        'Site tends to show intrusive ads' +
+        'Site shows intrusive or misleading ads' +
             '\nAllow\nBlock\nAsk',
         testElement.$.permissionItem.innerText.trim());
     assertTrue(testElement.$.permissionItem.classList.contains('two-line'));
@@ -293,7 +293,7 @@ suite('SiteDetailsPermission', function() {
       source: settings.SiteSettingSource.PREFERENCE,
     };
     assertEquals(
-        'Block if site tends to show intrusive ads' +
+        'Block if site shows intrusive or misleading ads' +
             '\nAllow\nBlock\nAsk',
         testElement.$.permissionItem.innerText.trim());
     assertTrue(testElement.$.permissionItem.classList.contains('two-line'));
@@ -307,7 +307,7 @@ suite('SiteDetailsPermission', function() {
       source: settings.SiteSettingSource.DEFAULT,
     };
     assertEquals(
-        'Block if site tends to show intrusive ads' +
+        'Block if site shows intrusive or misleading ads' +
             '\nBlock (default)\nAllow\nBlock\nAsk',
         testElement.$.permissionItem.innerText.trim());
     assertTrue(testElement.$.permissionItem.classList.contains('two-line'));
