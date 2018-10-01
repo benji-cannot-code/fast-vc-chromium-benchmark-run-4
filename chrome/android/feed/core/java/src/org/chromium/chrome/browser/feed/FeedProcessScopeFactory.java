6eb4d88a049a857c7ded2bee865eb5e798efeee0
Patrick Noland <pnoland@google.com>
2018-10-01 20:04:28 +0000

    Implement the Feed App Lifecycle Host API
    The lifecycle API is the interaction point for Chrome-side application
    lifecycle events that the Feed Library cares about for the purposes
    of maintaining its internal state.
    
    Dummy files have been added so that compilation succeeds when the
    Feed is disabled via buildflag.
    This CL also adds a presubmit whitelist for all android/feed/dummy files.
    
    (Google-internal design doc:
    http://doc/1NyFLrqEAOoZe7MU2qy7bHYB6Rlp7c6lz22Zqzh-Cbo4)
    
    Bug: 866950
    Change-Id: Ie118c991aabb906e88344b5c96ab3b09e77c620f
    Reviewed-on: https://chromium-review.googlesource.com/1231992
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595529}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedProcessScopeFactory.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedProcessScopeFactory.java
@@ -30,6 +30,7 @@ public class FeedProcessScopeFactory {
     private static boolean sIsDisableForPolicy =
             !PrefServiceBridge.getInstance().getBoolean(Pref.NTP_ARTICLES_SECTION_ENABLED);
     private static PrefChangeRegistrar sPrefChangeRegistrar;
+    private static FeedAppLifecycle sFeedAppLifecycle;
     private static FeedProcessScope sFeedProcessScope;
     private static FeedScheduler sFeedScheduler;
     private static FeedOfflineIndicator sFeedOfflineIndicator;
@@ -61,6 +62,17 @@ public class FeedProcessScopeFactory {
         return sFeedOfflineIndicator;
     }
 
+    /*
+     * @return The global instance of {@link FeedAppLifecycle} for the process.
+     *         Null if the Feed is disabled.
+     */
+    public static @Nullable FeedAppLifecycle getFeedAppLifecycle() {
+        if (sFeedAppLifecycle == null) {
+            initialize();
+        }
+        return sFeedAppLifecycle;
+    }
+
     /**
      * @return Whether the dependencies provided by this class are allowed to be created. The feed
      *         process is disabled if supervised user or enterprise policy has once been added
@@ -72,7 +84,8 @@ public class FeedProcessScopeFactory {
     }
 
     private static void initialize() {
-        assert sFeedProcessScope == null && sFeedScheduler == null && sFeedOfflineIndicator == null;
+        assert sFeedProcessScope == null && sFeedScheduler == null && sFeedOfflineIndicator == null
+                && sFeedAppLifecycle == null;
         if (!isFeedProcessEnabled()) return;
 
         sPrefChangeRegistrar = new PrefChangeRegistrar();
@@ -104,6 +117,9 @@ public class FeedProcessScopeFactory {
 
         sFeedOfflineIndicator =
                 new FeedOfflineBridge(profile, sFeedProcessScope.getKnownContentApi());
+
+        sFeedAppLifecycle = new FeedAppLifecycle(sFeedProcessScope.getAppLifecycleListener(),
+                new FeedLifecycleBridge(profile), sFeedScheduler);
     }
 
     private static Configuration createConfiguration() {
@@ -125,10 +141,9 @@ public class FeedProcessScopeFactory {
      */
     @VisibleForTesting
     static void createFeedProcessScopeForTesting(FeedScheduler feedScheduler,
-            NetworkClient networkClient, FeedOfflineIndicator feedOfflineIndicator) {
+            NetworkClient networkClient, FeedOfflineIndicator feedOfflineIndicator,
+            FeedAppLifecycle feedAppLifecycle, FeedAppLifecycleListener lifecycleListener) {
         Configuration configHostApi = createConfiguration();
-        FeedAppLifecycleListener lifecycleListener =
-                new FeedAppLifecycleListener(new ThreadUtils());
         sFeedScheduler = feedScheduler;
         sFeedProcessScope = new FeedProcessScope
                                     .Builder(configHostApi, Executors.newSingleThreadExecutor(),
@@ -137,6 +152,7 @@ public class FeedProcessScopeFactory {
                                             ContextUtils.getApplicationContext())
                                     .build();
         sFeedOfflineIndicator = feedOfflineIndicator;
+        sFeedAppLifecycle = feedAppLifecycle;
     }
 
     /** Use supplied NetworkClient instead of real one, for tests. */
@@ -184,6 +200,10 @@ public class FeedProcessScopeFactory {
         if (sFeedOfflineIndicator != null) {
             sFeedOfflineIndicator.destroy();
             sFeedOfflineIndicator = null;
+        }
+        if (sFeedAppLifecycle != null) {
+            sFeedAppLifecycle.destroy();
+            sFeedAppLifecycle = null;
         }
     }
 }
--- chrome/browser/browsing_data/chrome_browsing_data_remover_delegate.cc
+++ chrome/browser/browsing_data/chrome_browsing_data_remover_delegate.cc
@@ -105,12 +105,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_ANDROID)
 #include "chrome/browser/android/customtabs/origin_verifier.h"
+#include "chrome/browser/android/feed/feed_lifecycle_bridge.h"
 #include "chrome/browser/android/oom_intervention/oom_intervention_decider.h"
 #include "chrome/browser/android/search_permissions/search_permissions_service.h"
 #include "chrome/browser/android/webapps/webapp_registry.h"
 #include "chrome/browser/media/android/cdm/media_drm_license_manager.h"
 #include "chrome/browser/offline_pages/offline_page_model_factory.h"
 #include "components/feed/buildflags.h"
+#include "components/feed/feed_feature_list.h"
 #include "components/offline_pages/core/offline_page_feature.h"
 #include "components/offline_pages/core/offline_page_model.h"
 #include "sql/database.h"
@@ -897,6 +899,13 @@ void ChromeBrowsingDataRemoverDelegate::RemoveEmbedderData(
         ContentSuggestionsServiceFactory::GetForProfileIfExists(profile_);
     if (content_suggestions_service)
       content_suggestions_service->ClearAllCachedSuggestions();
+
+#if defined(OS_ANDROID)
+#if BUILDFLAG(ENABLE_FEED_IN_CHROME)
+    if (base::FeatureList::IsEnabled(feed::kInterestFeedContentSuggestions))
+      feed::FeedLifecycleBridge::ClearCachedData();
+#endif  // BUILDFLAG(ENABLE_FEED_IN_CHROME)
+#endif  // defined(OS_ANDROID)
 
     // |ui_nqe_service| may be null if |profile_| is not a regular profile.
     UINetworkQualityEstimatorService* ui_nqe_service =
