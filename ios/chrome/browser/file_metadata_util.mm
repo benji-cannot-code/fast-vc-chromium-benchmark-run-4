04dbc30570cf41efbe4ff2845d15bf7e3231c4ea
Etienne Pierre-doray <etiennep@chromium.org>
2018-10-01 20:04:41 +0000

    [TaskScheduler]: Use ScopedBlockingCall to mark blocking tasks.
    This CL uses ScopedBlockingCall to mark blocking calls in /ios/chrome.
    
    This CL was created by replacing calls to AssertBlockingAllowed()
    with instantiations of ScopedBlockingCall(MAY_BLOCK).
    I kindly ask the reviewer to make sure of the following:
      - ScopedBlockingCall is instantiated in a scope with minimal CPU usage.
        If this is not the case, ScopedBlockingCall should be instantiated
        closer to the blocking call. See scoped_blocking_call.h for more
        info. Please let me know when/where the blocking call happens if this needs
        to be changed.
      - Parameter |blocking_type| matches expectation (MAY_BLOCK/WILL_BLOCK). See
        BlockingType for more info. While I assumed MAY_BLOCK by default, that might
        not be the best fit if we know that this callsite is guaranteed to block.
      - The ScopedBlockingCall's scope covers the entirety of the blocking operation
        previously asserted against by the AssertBlockingAllowed().
    
    This CL was uploaded by git cl split.
    
    R=rohitrao@chromium.org
    
    Bug: 874080
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: Id297ebc108dc8f8915402602ae12a63e5a3131d0
    Reviewed-on: https://chromium-review.googlesource.com/1191186
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595530}

--- ios/chrome/browser/file_metadata_util.mm
+++ ios/chrome/browser/file_metadata_util.mm
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/strings/sys_string_conversions.h"
-#include "base/threading/thread_restrictions.h"
+#include "base/threading/scoped_blocking_call.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 void SetSkipSystemBackupAttributeToItem(const base::FilePath& path,
                                         bool skip_system_backup) {
-  base::AssertBlockingAllowed();
+  base::ScopedBlockingCall scoped_blocking_call(base::BlockingType::MAY_BLOCK);
 
   NSURL* file_url =
       [NSURL fileURLWithPath:base::SysUTF8ToNSString(path.value())];
