8aa0759cadaf1025e9c489e159838ad75124e7b1
Karolina Soltys <ksolt@chromium.org>
2018-10-01 19:28:20 +0000

    Removed several levels of calls to RenderWidget::DidReceiveCompositorFrameAck.
    Bug: 725061
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I9bec1e76635379dd7a09a5160e1dff69074692f9
    Reviewed-on: https://chromium-review.googlesource.com/1183232
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Commit-Queue: Karolina Soltys <ksolt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595498}

--- content/renderer/render_widget.cc
+++ content/renderer/render_widget.cc
@@ -1014,9 +1014,6 @@ void RenderWidget::DidCompletePageScaleAnimation() {
     owner_delegate_->DidCompletePageScaleAnimationForWidget();
 }
 
-void RenderWidget::DidReceiveCompositorFrameAck() {
-}
-
 bool RenderWidget::IsClosing() const {
   // TODO(ajwong): There is oddly 2 closing states. This API is used by
   // LayerTreeView only as part of the LayerTreeViewDelegate interface and
@@ -2783,6 +2780,9 @@ cc::LayerTreeSettings RenderWidget::GenerateLayerTreeSettings(
 
   settings.always_request_presentation_time =
       cmd.HasSwitch(cc::switches::kAlwaysRequestPresentationTime);
+
+  settings.send_compositor_frame_ack = false;
+
   return settings;
 }
 
