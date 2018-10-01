5d66effd2b74b9009fb5ee2b10bd31a22f9035e4
Xing Liu <xingliu@chromium.org>
2018-10-01 19:57:33 +0000

    Android video thumbnail: Generate thumbnails for vp8, vp9 videos.
    This CL does the following:
    
    1. Use VpxVideoDecoder in utility process to do software decoding for
    vp8, vp9 video files.
    
    2. Simplify the class to render media::VideoFrame into bitmap, since
    PaintCanvasVideoRenderer works for all kind of VideoFrames.
    
    3. Do not retrieve poster image from metadata. The mime type of the
    poster image is inferred from mime sniffer, and needs to be
    processed in sandbox process.
    
    
    Bug: 826021
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I91c9cf72a75be4bc3a95203a40633647e079a40a
    Reviewed-on: https://chromium-review.googlesource.com/1234859
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595523}

--- media/filters/android/video_frame_extractor.h
+++ media/filters/android/video_frame_extractor.h
@@ -39,7 +39,7 @@ class MEDIA_EXPORT VideoFrameExtractor {
  public:
   using VideoFrameCallback =
       base::OnceCallback<void(bool success,
-                              const std::vector<uint8_t>& data,
+                              std::vector<uint8_t> data,
                               const VideoDecoderConfig& decoder_config)>;
 
   explicit VideoFrameExtractor(DataSource* data_source);
