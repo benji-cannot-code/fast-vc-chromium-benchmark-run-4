1fa4845d2150f3c9361a63adbc3f0e355a4d94a3
John Chen <johnchen@chromium.org>
2018-10-01 17:40:18 +0000

    [ChromeDriver] Fix stack overflow due to huge data
    Fix a stack overflow that can be caused by receiving a large amount of
    data from Chrome. Also increase the maximum buffer size for
    communication between app and ChromeDriver.
    
    Bug: chromium:877105, chromedriver:2587, b/112588554
    Change-Id: Ie98544237472da629c01b9422b2e01a4716e8b88
    Reviewed-on: https://chromium-review.googlesource.com/1233177
    Commit-Queue: John Chen <johnchen@chromium.org>
    Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595477}

--- chrome/test/chromedriver/server/chromedriver_server.cc
+++ chrome/test/chromedriver/server/chromedriver_server.cc
@@ -51,7 +51,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-const int kBufferSize = 100 * 1024 * 1024;  // 100 MB
+// Maximum message size between app and ChromeDriver. Data larger than 150 MB
+// or so can cause crashes in Chrome (https://crbug.com/890854), so there is no
+// need to support messages that are too large.
+const int kBufferSize = 256 * 1024 * 1024;  // 256 MB
 
 typedef base::Callback<
     void(const net::HttpServerRequestInfo&, const HttpResponseSenderFunc&)>
