e11a608d5f03f12481dad5e1267cfb4e45e29223
Darren Shen <shend@chromium.org>
2018-10-01 05:14:28 +0000

    [VK] Disable gesture editing feature in new UI.
    Gesture editing is not supported in the new UI. So we should disable
    the flag so that it doesn't appear in the keyboard settings page.
    
    Bug: 890134
    Change-Id: I7dc3c6d05bad6e19e8cbfe0a6f89de603f4ddce5
    Reviewed-on: https://chromium-review.googlesource.com/1250683
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595384}

--- ui/keyboard/keyboard_util.cc
+++ ui/keyboard/keyboard_util.cc
@@ -223,8 +223,11 @@ bool IsGestureTypingEnabled() {
 }
 
 bool IsGestureEditingEnabled() {
-  return !base::CommandLine::ForCurrentProcess()->HasSwitch(
-      switches::kDisableGestureEditing);
+  // TODO(https://crbug.com/890134): Gesture editing is not implemented in the
+  // new UI.
+  return !IsVirtualKeyboardMdUiEnabled() &&
+         !base::CommandLine::ForCurrentProcess()->HasSwitch(
+             switches::kDisableGestureEditing);
 }
 
 bool IsImeServiceEnabled() {
