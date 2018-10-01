aee5047a01d77c1193d89ebcca690326a7454ad0
Dan Rubery <drubery@chromium.org>
2018-10-01 19:24:09 +0000

    Always ping for DMG files
    There are a lot of features of DMG files that we don't support, and some
    of these can be used to deliver malware. We should always send a ping
    when we detect a DMG file, instead of relying on detecting the contained
    binary.
    
    Bug: 882927
    Change-Id: Idf7523f575a5d9778ab5d14ea1112317118fd2ed
    Reviewed-on: https://chromium-review.googlesource.com/1225050
    Reviewed-by: Jialiu Lin <jialiul@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595497}

--- chrome/browser/safe_browsing/download_protection/check_client_download_request.cc
+++ chrome/browser/safe_browsing/download_protection/check_client_download_request.cc
@@ -731,6 +731,7 @@ void CheckClientDownloadRequest::OnDmgAnalysisFinished(
   } else {
     base::UmaHistogramSparse("SBClientDownload.DmgFileFailureByType",
                              uma_file_type);
+    type_ = ClientDownloadRequest::MAC_ARCHIVE_FAILED_PARSING;
   }
 
   if (archived_executable_) {
@@ -745,16 +746,6 @@ void CheckClientDownloadRequest::OnDmgAnalysisFinished(
 
   UMA_HISTOGRAM_TIMES("SBClientDownload.ExtractDmgFeaturesTime",
                       base::TimeTicks::Now() - dmg_analysis_start_time_);
-
-  if (!archived_executable_) {
-    if (!results.success) {
-      type_ = ClientDownloadRequest::INVALID_MAC_ARCHIVE;
-    } else {
-      PostFinishTask(DownloadCheckResult::SAFE,
-                     REASON_ARCHIVE_WITHOUT_BINARIES);
-      return;
-    }
-  }
 
   OnFileFeatureExtractionDone();
 }
