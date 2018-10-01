37fcb6a4a0458923f9f3e9bd66c3eefdce346080
Bence Béky <bnc@chromium.org>
2018-10-01 23:18:59 +0000

    Don't use PrintF/AppendF in spdy.
    StrCat is 2-4x faster, so use that instead of Printf/Appendf.
    
    This CL lands server changes 211484351 by dzetkulic and
    215218657 by bnc.
    
    BUG=488484
    
    Change-Id: I5b6cab6db94c1cb5f75106832d45858a92924875
    Reviewed-on: https://chromium-review.googlesource.com/1252161
    Commit-Queue: Bence Béky <bnc@chromium.org>
    Reviewed-by: Dianna Hu <diannahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595631}

--- net/third_party/spdy/core/spdy_alt_svc_wire_format.cc
+++ net/third_party/spdy/core/spdy_alt_svc_wire_format.cc
@@ -48,7 +48,7 @@ SpdyAltSvcWireFormat::AlternativeService::AlternativeService(
       host(host),
       port(port),
       max_age(max_age),
-      version(version) {}
+      version(std::move(version)) {}
 
 SpdyAltSvcWireFormat::AlternativeService::~AlternativeService() = default;
 
@@ -265,9 +265,9 @@ SpdyString SpdyAltSvcWireFormat::SerializeHeaderFieldValue(
       }
       value.push_back(c);
     }
-    SpdyStringAppendF(&value, ":%d\"", altsvc.port);
+    value.append(SpdyStrCat(":", altsvc.port, "\""));
     if (altsvc.max_age != 86400) {
-      SpdyStringAppendF(&value, "; ma=%d", altsvc.max_age);
+      value.append(SpdyStrCat("; ma=", altsvc.max_age));
     }
     if (!altsvc.version.empty()) {
       if (is_ietf_format_quic) {
@@ -282,7 +282,7 @@ SpdyString SpdyAltSvcWireFormat::SerializeHeaderFieldValue(
           if (it != altsvc.version.begin()) {
             value.append(",");
           }
-          SpdyStringAppendF(&value, "%d", *it);
+          value.append(SpdyStrCat(*it));
         }
         value.append("\"");
       }
