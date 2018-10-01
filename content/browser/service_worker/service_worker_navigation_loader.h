8078f6a10629acae4e951c9ec24570f9929f75b9
Kenichi Ishibashi <bashi@chromium.org>
2018-10-01 06:03:30 +0000

    Remove crash instrumentation in ServiceWorkerNavigationLoader
    It seems we've fixed the crash. Remove instrumentation.
    
    Bug: 887033
    Change-Id: Ie0790f4b4a87455393f77c27e58bb956f669ba6b
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Reviewed-on: https://chromium-review.googlesource.com/1253522
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595389}

--- content/browser/service_worker/service_worker_navigation_loader.h
+++ content/browser/service_worker/service_worker_navigation_loader.h
@@ -115,8 +115,6 @@ class CONTENT_EXPORT ServiceWorkerNavigationLoader
     kCompleted,
   };
 
-  static const char* StatusString(Status status);
-
   // For FORWARD_TO_SERVICE_WORKER case.
   void StartRequest(const network::ResourceRequest& resource_request,
                     network::mojom::URLLoaderRequest request,
@@ -188,9 +186,6 @@ class CONTENT_EXPORT ServiceWorkerNavigationLoader
   mojo::Binding<network::mojom::URLLoader> binding_;
 
   Status status_ = Status::kNotStarted;
-
-  // https://crbug.com/881826
-  base::Optional<std::vector<std::string>> debug_log_;
 
   base::WeakPtrFactory<ServiceWorkerNavigationLoader> weak_factory_;
 
