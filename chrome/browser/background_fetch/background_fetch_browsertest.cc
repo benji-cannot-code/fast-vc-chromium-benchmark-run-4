c5fc32ff7107f98b868961175c3fa69b6428ae3f
Rayan Kanso <rayankans@chromium.org>
2018-10-01 14:20:03 +0000

    [Background Fetch] Fix download notification bugs.
    - Set is_resumable in the OfflineItem to true so that paused downloads
    can be resumed.
    - Fix deallocated memory access in the Abort workflow which was causing
    the browser to crash.
    
    Change-Id: I2369ffe517796c245b6d3351e4d90aa6b4a1510b
    Reviewed-on: https://chromium-review.googlesource.com/1253624
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595443}

--- chrome/browser/background_fetch/background_fetch_browsertest.cc
+++ chrome/browser/background_fetch/background_fetch_browsertest.cc
@@ -507,6 +507,7 @@ IN_PROC_BROWSER_TEST_F(BackgroundFetchBrowserTest,
   EXPECT_EQ(offline_item.title, GetExpectedTitle(kSingleFileDownloadTitle));
   EXPECT_EQ(offline_item.filter, OfflineItemFilter::FILTER_OTHER);
   EXPECT_TRUE(offline_item.is_transient);
+  EXPECT_TRUE(offline_item.is_resumable);
   EXPECT_FALSE(offline_item.is_suggested);
   EXPECT_FALSE(offline_item.is_off_the_record);
 
@@ -518,7 +519,6 @@ IN_PROC_BROWSER_TEST_F(BackgroundFetchBrowserTest,
   // Change-detector tests for values we might want to provide or change.
   EXPECT_TRUE(offline_item.description.empty());
   EXPECT_TRUE(offline_item.page_url.is_empty());
-  EXPECT_FALSE(offline_item.is_resumable);
   EXPECT_FALSE(offline_item.is_off_the_record);
 }
 
