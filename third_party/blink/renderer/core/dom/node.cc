abc26d22a1ca1ebe690c9cc6b6b5c06ae81dcf21
Yoichi Osato <yoichio@chromium.org>
2018-10-01 09:55:57 +0000

    Refactoring: DocumentMarkerVector::RemoveMarkers should receive only Text node.
    Since DocumentMarkerController::MarkerMap only contains Text nodes as key,
    the removing map entry functions should receive only Text node rather than Node.
    
    Bug: 868229
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng
    Change-Id: Id9b4daa1d116660f13d3c3961d5ed97758ceb01b
    Reviewed-on: https://chromium-review.googlesource.com/1244698
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595415}

--- third_party/blink/renderer/core/dom/node.cc
+++ third_party/blink/renderer/core/dom/node.cc
@@ -2045,8 +2045,8 @@ void Node::DidMoveToNewDocument(Document& old_document) {
         GetDocument().AddListenerTypeIfNeeded(type, *this);
     }
   }
-
-  old_document.Markers().RemoveMarkersForNode(this);
+  if (IsTextNode())
+    old_document.Markers().RemoveMarkersForNode(*ToText(this));
   if (GetDocument().GetPage() &&
       GetDocument().GetPage() != old_document.GetPage()) {
     GetDocument().GetFrame()->GetEventHandlerRegistry().DidMoveIntoPage(*this);
