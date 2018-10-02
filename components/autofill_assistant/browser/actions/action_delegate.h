805d227f3b0161240ebce9df7a94e4d498cf6a47
Jordan Demeulenaere <jdemeulenaere@google.com>
2018-10-02 14:17:55 +0000

    Reland "[Autofill Assistant] Add StopCurrentScript() in ActionDelegate."
    This is a reland of e195437f30bf33eb49b2e3838ff1abb5f52cfdda
    
    The first version of this change was broken because ActionDelegate::StopCurrentScript() was not abstract (pure virtual).
    
    This was fixed in this CL by adding "= 0"; at the function declaration in action_delegate.h.
    
    Original change's description:
    > [Autofill Assistant] Add StopCurrentScript() in ActionDelegate.
    >
    > Bug: 806868
    > Change-Id: Ic3deb3353377e950fadc0d011c75f7ed91ca5366
    > Reviewed-on: https://chromium-review.googlesource.com/1250923
    > Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    > Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#595445}
    
    Bug: 806868
    Change-Id: Ic57f8607844267c98d0ab5ece8a253ae853ec325
    Reviewed-on: https://chromium-review.googlesource.com/1256184
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Jordan Demeulenaere <jdemeulenaere@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595826}

--- components/autofill_assistant/browser/actions/action_delegate.h
+++ components/autofill_assistant/browser/actions/action_delegate.h
@@ -106,6 +106,9 @@ class ActionDelegate {
   // state.
   virtual void Restart() = 0;
 
+  // Stop the current script and show |message| if it is not empty .
+  virtual void StopCurrentScript(const std::string& message) = 0;
+
   // Return the current ClientMemory.
   virtual ClientMemory* GetClientMemory() = 0;
 
