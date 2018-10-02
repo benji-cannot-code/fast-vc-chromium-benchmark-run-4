0f0a11b6ce4d077d4971f969f8c78d86d27b372e
Hector Carmona <hcarmona@chromium.org>
2018-10-02 23:58:40 +0000

    Navi: Update Google Apps chooser to more closely match mocks.
    Followup CLs will address other visual issues to avoid over-complicating this CL
    
    Bug: 874118
    Change-Id: I709fa1165e11444899f1efa5cf23270a1fb2ae6b
    Reviewed-on: https://chromium-review.googlesource.com/c/1226464
    Commit-Queue: Hector Carmona <hcarmona@chromium.org>
    Reviewed-by: Scott Chen <scottchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596053}

--- chrome/browser/resources/welcome/onboarding_welcome/google_apps/apps_chooser.js
+++ chrome/browser/resources/welcome/onboarding_welcome/google_apps/apps_chooser.js
@@ -54,8 +54,8 @@ Polymer({
             selected: true,
           },
           {
-            name: 'Chrome Web Store',
-            icon: 'chrome_store',
+            name: 'Web Store',
+            icon: 'chrome-store',
             selected: true,
           },
         ];
@@ -85,6 +85,22 @@ Polymer({
   onAppClick_: function(e) {
     e.model.set('item.selected', !e.model.item.selected);
     this.hasAppsSelected = this.computeHasAppsSelected_();
+  },
+
+  /**
+   * @param {!Event} e
+   * @private
+   */
+  onAppPointerDown_: function(e) {
+    e.currentTarget.classList.remove('keyboard-focused');
+  },
+
+  /**
+   * @param {!Event} e
+   * @private
+   */
+  onAppKeyUp_: function(e) {
+    e.currentTarget.classList.add('keyboard-focused');
   },
 
   /**
