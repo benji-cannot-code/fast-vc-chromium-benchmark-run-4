0e833562d28e6f65d7bfecf3a657f03a696ff321
stkhapugin@chromium.org <stkhapugin@chromium.org>
2018-10-02 15:17:15 +0000

    Popup view controller
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: I674ed55abf3313a7e93b5d7404aebcf9609ca621
    Reviewed-on: https://chromium-review.googlesource.com/1235933
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595845}

--- ios/chrome/browser/ui/omnibox/popup/omnibox_popup_mediator.mm
+++ ios/chrome/browser/ui/omnibox/popup/omnibox_popup_mediator.mm
@@ -88,8 +88,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   if (self.open) {
     [self.presenter updateHeightAndAnimateAppearanceIfNecessary];
-  } else {
-    [self.presenter animateCollapse];
   }
 }
 
