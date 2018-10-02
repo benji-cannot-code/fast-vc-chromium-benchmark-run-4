a6e509ccfa63980e8681ad2a07c6601460696952
Hajime Hoshi <hajimehoshi@chromium.org>
2018-10-02 04:53:32 +0000

    Remove one LazyThreadControllerForTest usage of the two
    LazyThreadControllerForTest makes it hard to write tests related to task
    runners. We aim to remove this class, and this CL is one of the steps.
    
    In media unittests, LazyThreadControllerForTest is not necessary. Use
    WebThreadScheduler::CreateMainThreadScheduler instead.
    
    Bug: 870606
    Change-Id: I0976c4db02e466f06247bf5a82654734f6c7d7bd
    Reviewed-on: https://chromium-review.googlesource.com/1250684
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595725}

--- media/blink/run_all_unittests.cc
+++ media/blink/run_all_unittests.cc
@@ -41,7 +41,8 @@ class BlinkPlatformWithTaskEnvironment : public blink::Platform {
  public:
   BlinkPlatformWithTaskEnvironment()
       : main_thread_scheduler_(
-            blink::scheduler::CreateWebMainThreadSchedulerForTests()) {}
+            blink::scheduler::WebThreadScheduler::CreateMainThreadScheduler()) {
+  }
 
   ~BlinkPlatformWithTaskEnvironment() override {
     main_thread_scheduler_->Shutdown();
