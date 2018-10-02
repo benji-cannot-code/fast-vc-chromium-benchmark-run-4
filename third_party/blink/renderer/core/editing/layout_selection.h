6cd14ddc3ae5b49974d86d8ebbeb03fa4babf4f2
Yoichi Osato <yoichio@chromium.org>
2018-10-02 01:34:18 +0000

    Create visible Selection w/o InlineBox for ComputeLayerSelection.
    Design doc: http://bit.ly/2Njo0zR
    
    This patch introduces |SelectionInDOMTree ComputeLayoutSelection
    (const SelectionInDOMTree&)| for replacing CreateVisiblePosition
    at ComputeLayerSelection.
    
    The new adjustment algorithm is based on LayoutSelection and
    really simple: pick SelectionState::kStart/kEnd nodes.
    They are the start/end nodes of selection painting.
    
    For caret, caret positioning should be done before LayoutSelection,
    thus ComputeLayoutSelection() returns as-is.
    
    Though ComputeLayoutSelection is targeting to replace
    CreateVisibleSelection, there is difference because of simplicity:
    it doesn't consider contenteditable, block-end BRs nor any InlinBox
    matter. The difference is also tested at layout_selection_test.cc
    
    Bug: 789870
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng
    Change-Id: I9de9fef6e76ff65c1b2575c47d39e156d8e56380
    Reviewed-on: https://chromium-review.googlesource.com/1182710
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595685}

--- third_party/blink/renderer/core/editing/layout_selection.h
+++ third_party/blink/renderer/core/editing/layout_selection.h
@@ -49,6 +49,7 @@ class LayoutSelection final : public GarbageCollected<LayoutSelection> {
   void SetHasPendingSelection();
   void Commit();
 
+  SelectionInDOMTree ComputeLayoutSelection() const;
   IntRect AbsoluteSelectionBounds();
   void InvalidatePaintForSelection();
 
