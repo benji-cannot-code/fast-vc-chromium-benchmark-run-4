e75ca325d7c857ae33758d97e1f974e02c9ca9c9
Ken Rockot <rockot@chromium.org>
2018-10-02 01:06:24 +0000

    [content-service] Support alternative remote views
    == First some background:
    
    The NavigableContentsView client library by default uses
    views::RemoteViewHost to support embedding of a remote view within a
    client's own UI on platforms that support both Views and the
    Window Service.
    
    This is fine unless the client is running in the same process as the
    Window Service, which is in the case for out-of-process Ash. That is,
    when running Ash outside of the browser process on Chrome OS, we have it
    share a process with the Window Service.
    
    The Window Service implementation defines a ws::ServerRemoteViewHost
    which serves essentially the same purpose as views::RemoteViewHost but
    which actually works within the Window Service process.
    
    We obviously cannot tolerate the Content Service client library
    having a dependency on the Window Service (let alone its private
    implementation!).
    
    == What this CL does:
    
    Introduces a static NavigableContentsView::SetRemoteViewFactory API which
    can be called by a client to provide process-wide alternative View
    creation logic for remote views. If unset, the client library will
    continue to use views::RemoteViewHost for remote views.
    
    Ash uses this static method when setting up its in-process Window
    Service instance, providing a factory which emits
    ws::ServerRemoteViewHost objects.
    
    No Content Service implementation changes are required to support this,
    as it's really just a detail of how clients interface with the Window
    Service to support remote views.
    
    Bug: 854367
    Change-Id: Ic6b09502ea2f84c3a4594e69e2a531a4efc1e3da
    Reviewed-on: https://chromium-review.googlesource.com/1237376
    Commit-Queue: Ken Rockot <rockot@chromium.org>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595674}

--- services/content/public/cpp/navigable_contents_view.cc
+++ services/content/public/cpp/navigable_contents_view.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(ENABLE_REMOTE_NAVIGABLE_CONTENTS_VIEW)
 #include "services/ws/public/mojom/window_tree_constants.mojom.h"  // nogncheck
 #include "ui/base/ui_base_features.h"                   // nogncheck
+#include "ui/views/controls/native/native_view_host.h"  // nogncheck
 #include "ui/views/mus/remote_view/remote_view_host.h"  // nogncheck
 #endif  // BUILDFLAG(ENABLE_REMOTE_NAVIGABLE_CONTENTS_VIEW)
 #endif  // defined(TOOLKIT_VIEWS)
@@ -42,6 +43,15 @@ base::AtomicFlag& GetInServiceProcessFlag() {
   return *in_service_process;
 }
 
+#if BUILDFLAG(ENABLE_REMOTE_NAVIGABLE_CONTENTS_VIEW)
+
+NavigableContentsView::RemoteViewFactory& GetRemoteViewFactory() {
+  static base::NoDestructor<NavigableContentsView::RemoteViewFactory> factory;
+  return *factory;
+}
+
+#endif  // BUILDFLAG(ENABLE_REMOTE_NAVIGABLE_CONTENTS_VIEW)
+
 }  // namespace
 
 NavigableContentsView::~NavigableContentsView() = default;
@@ -61,25 +71,41 @@ NavigableContentsView::NavigableContentsView() {
   view_ = std::make_unique<views::View>();
   view_->set_owned_by_client();
   view_->SetLayoutManager(std::make_unique<views::FillLayout>());
-#if BUILDFLAG(ENABLE_REMOTE_NAVIGABLE_CONTENTS_VIEW)
-  if (!IsClientRunningInServiceProcess()) {
-    DCHECK(!remote_view_host_);
-    remote_view_host_ = new views::RemoteViewHost;
-    view_->AddChildView(remote_view_host_);
-  }
-#endif  // BUILDFLAG(ENABLE_REMOTE_NAVIGABLE_CONTENTS_VIEW)
 #endif  // defined(TOOLKIT_VIEWS)
 }
+
+#if BUILDFLAG(ENABLE_REMOTE_NAVIGABLE_CONTENTS_VIEW)
+
+// static
+void NavigableContentsView::SetRemoteViewFactory(RemoteViewFactory factory) {
+  GetRemoteViewFactory() = std::move(factory);
+}
+
+#endif  // BUILDFLAG(ENABLE_REMOTE_NAVIGABLE_CONTENTS_VIEW)
 
 void NavigableContentsView::EmbedUsingToken(
     const base::UnguessableToken& token) {
 #if defined(TOOLKIT_VIEWS)
 #if BUILDFLAG(ENABLE_REMOTE_NAVIGABLE_CONTENTS_VIEW)
-  if (remote_view_host_) {
-    const uint32_t kEmbedFlags =
-        ws::mojom::kEmbedFlagEmbedderInterceptsEvents |
-        ws::mojom::kEmbedFlagEmbedderControlsVisibility;
-    remote_view_host_->EmbedUsingToken(token, kEmbedFlags, base::DoNothing());
+  if (!IsClientRunningInServiceProcess()) {
+    const RemoteViewFactory& factory = GetRemoteViewFactory();
+    std::unique_ptr<views::NativeViewHost> remote_view_host;
+    if (factory) {
+      remote_view_host = factory.Run(token);
+    } else {
+      auto host = std::make_unique<views::RemoteViewHost>();
+      constexpr uint32_t kEmbedFlags =
+          ws::mojom::kEmbedFlagEmbedderInterceptsEvents |
+          ws::mojom::kEmbedFlagEmbedderControlsVisibility;
+      host->EmbedUsingToken(token, kEmbedFlags, base::DoNothing());
+      remote_view_host = std::move(host);
+    }
+
+    auto* raw_remote_view_host = remote_view_host.release();
+    view_->AddChildView(raw_remote_view_host);
+    native_view_ = raw_remote_view_host->native_view();
+
+    view_->Layout();
     return;
   }
 #endif  // BUILDFLAG(ENABLE_REMOTE_NAVIGABLE_CONTENTS_VIEW)
@@ -95,6 +121,9 @@ void NavigableContentsView::EmbedUsingToken(
     return;
   }
 
+  // Invoke a callback provided by the Content Service's host environment. This
+  // should parent a web content view to our own |view()|, as well as set
+  // |native_view_| to the corresponding web contents' own NativeView.
   auto callback = std::move(it->second);
   embeddings.erase(it);
   std::move(callback).Run(this);
