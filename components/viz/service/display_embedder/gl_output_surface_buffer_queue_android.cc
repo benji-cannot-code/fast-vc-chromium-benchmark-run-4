bce65bbf4bec5f81235995925db7887a07f847f6
Khushal <khushalsagar@google.com>
2018-10-02 23:57:06 +0000

    viz: Fix jumbo build failure on Android.
    The GLOutputSurfaceBufferQueueAndroid should not use ui/gl includes
    since it is expected to use client-side GL. Doing so breaks jumbo builds.
    
    R=piman@chromium.org
    
    Bug: 891529
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel
    Change-Id: Ib7b9d424d864c4a585bae561c67ce8badbd6a5aa
    Reviewed-on: https://chromium-review.googlesource.com/c/1257659
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596052}

--- components/viz/service/display_embedder/gl_output_surface_buffer_queue_android.cc
+++ components/viz/service/display_embedder/gl_output_surface_buffer_queue_android.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/service/display_embedder/gl_output_surface_buffer_queue_android.h"
 
 #include "components/viz/service/display_embedder/compositor_overlay_candidate_validator_android.h"
-#include "ui/gl/gl_bindings.h"
+#include "third_party/khronos/GLES2/gl2.h"
 
 namespace viz {
 
