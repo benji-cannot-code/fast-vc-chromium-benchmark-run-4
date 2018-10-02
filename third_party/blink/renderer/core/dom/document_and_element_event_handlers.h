1ce11f11f643d7613b2dfc161520929d1d07114d
Amos Lim <eui-sang.lim@samsung.com>
2018-10-02 00:07:51 +0000

    Implement DocumentAndElementEventHandlers
    Add DocumentAndElementEventHandlers as per the spec[1] and remove
    oncopy, oncut, and onpaste from Document and Element.
    
    [1]: https://w3c.github.io/html/single-page.html#documentandelementeventhandlers
    
    Make Document, HTMLElement, and SVGElement include DocumentAndElementEventHandlers.
    
    Bug: 835570
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ie81d7995dbe0824ad3a66ccbfabcdbe5e3864fa4
    Reviewed-on: https://chromium-review.googlesource.com/1253169
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Commit-Queue: Amos Lim <eui-sang.lim@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#595652}

---
+++ third_party/blink/renderer/core/dom/document_and_element_event_handlers.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_RENDERER_CORE_DOM_DOCUMENT_AND_ELEMENT_EVENT_HANDLERS_H_
+#define THIRD_PARTY_BLINK_RENDERER_CORE_DOM_DOCUMENT_AND_ELEMENT_EVENT_HANDLERS_H_
+
+#include "third_party/blink/renderer/core/dom/events/event_target.h"
+#include "third_party/blink/renderer/platform/wtf/allocator.h"
+
+namespace blink {
+
+class DocumentAndElementEventHandlers {
+  STATIC_ONLY(DocumentAndElementEventHandlers);
+
+ public:
+  DEFINE_STATIC_ATTRIBUTE_EVENT_LISTENER(copy);
+  DEFINE_STATIC_ATTRIBUTE_EVENT_LISTENER(cut);
+  DEFINE_STATIC_ATTRIBUTE_EVENT_LISTENER(paste);
+};
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_RENDERER_CORE_DOM_DOCUMENT_AND_ELEMENT_EVENT_HANDLERS_H_
