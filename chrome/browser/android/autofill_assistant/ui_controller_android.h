c60c7b40314ac1f869b7f1c15d024535f5f6505d
Jordan Demeulenaere <jdemeulenaere@google.com>
2018-10-02 18:54:36 +0000

    [Autofill Assistant] Implement UiControllerAndroid::ChooseAddress and ::ChooseCard.
    Bug: 806868
    Change-Id: I055e151ab2b4f1df0c1a374d946ffbf1572f4165
    Reviewed-on: https://chromium-review.googlesource.com/c/1256248
    Commit-Queue: Jordan Demeulenaere <jdemeulenaere@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595934}

--- chrome/browser/android/autofill_assistant/ui_controller_android.h
+++ chrome/browser/android/autofill_assistant/ui_controller_android.h
@@ -50,6 +50,13 @@ class UiControllerAndroid : public UiController, public Client {
       JNIEnv* env,
       const base::android::JavaParamRef<jobject>& jcaller,
       const base::android::JavaParamRef<jstring>& jscript_path);
+  void OnAddressSelected(
+      JNIEnv* env,
+      const base::android::JavaParamRef<jobject>& jcaller,
+      const base::android::JavaParamRef<jstring>& jaddress_guid);
+  void OnCardSelected(JNIEnv* env,
+                      const base::android::JavaParamRef<jobject>& jcaller,
+                      const base::android::JavaParamRef<jstring>& jcard_guid);
 
  private:
   // Java-side AutofillAssistantUiController object.
@@ -57,6 +64,8 @@ class UiControllerAndroid : public UiController, public Client {
       java_autofill_assistant_ui_controller_;
 
   UiDelegate* ui_delegate_;
+
+  base::OnceCallback<void(const std::string&)> address_or_card_callback_;
 
   DISALLOW_COPY_AND_ASSIGN(UiControllerAndroid);
 };
