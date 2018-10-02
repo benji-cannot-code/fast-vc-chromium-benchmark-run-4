845de8b55df1116512fc30023b9f46f18d875f48
Valeriya Sinevich <valeriyas@google.com>
2018-10-02 14:33:37 +0000

    [Signin] Create Multilogin::Mode variable to prepare for logging.
    Set mode to UPDATE for Mirror and log it. Needed for CL on logging which has a higher priority than the one where mode will actually be used. Use VLOG(1) to make variable and used somewhere.
    
    Change-Id: I0eb3def674b510a5a5ca433f62540a027fa1cc25
    Reviewed-on: https://chromium-review.googlesource.com/1255823
    Commit-Queue: Valeriya Sinevich <valeriyas@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595829}

--- components/signin/core/browser/account_reconcilor_delegate.cc
+++ components/signin/core/browser/account_reconcilor_delegate.cc
@@ -45,6 +45,10 @@ bool AccountReconcilorDelegate::ReorderChromeAccountsForReconcileIfNeeded(
   return false;
 }
 
+bool AccountReconcilorDelegate::ShouldUpdateAccountsOrderInCookies() const {
+  return true;
+}
+
 AccountReconcilorDelegate::RevokeTokenOption
 AccountReconcilorDelegate::ShouldRevokeSecondaryTokensBeforeReconcile(
     const std::vector<gaia::ListedAccount>& gaia_accounts) {
--- google_apis/gaia/gaia_auth_fetcher.h
+++ google_apis/gaia/gaia_auth_fetcher.h
@@ -34,6 +34,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class GaiaAuthFetcherTest;
 
+namespace signin {
+// Mode determining whether Gaia can change the order of the accounts in
+// cookies.
+enum class MultiloginMode {
+  MULTILOGIN_UPDATE_COOKIE_ACCOUNTS_ORDER = 0,
+  MULTILOGIN_PRESERVE_COOKIE_ACCOUNTS_ORDER
+};
+}  // namespace signin
+
 namespace network {
 class SimpleURLLoader;
 class SharedURLLoaderFactory;
