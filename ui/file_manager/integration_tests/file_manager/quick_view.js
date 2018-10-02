59deb7a5b5b1314f9cb7014834c9e7c9fed53715
Noel Gordon <noel@chromium.org>
2018-10-02 05:43:18 +0000

    Test FilesApp QuickView [open] attribute
    FileApp QuickView has an [open] attribute, present when it (QuickView)
    is open, and removed when it's not open. Verify the attribute appears,
    and disappears, for all quick_view.js tests.
    
    No change in behavior, no new tests.
    
    Bug: 891150
    Change-Id: If90ae81cd60cd350497dece3896173f432571ccc
    Reviewed-on: https://chromium-review.googlesource.com/1256465
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595732}

--- ui/file_manager/integration_tests/file_manager/quick_view.js
+++ ui/file_manager/integration_tests/file_manager/quick_view.js
@@ -38,7 +38,7 @@ function openQuickViewSteps(appId, name) {
     function(result) {
       chrome.test.assertTrue(!!result, 'fakeKeyDown failed');
       repeatUntil(function() {
-        const elements = ['#quick-view', '#dialog'];
+        const elements = ['#quick-view', '#dialog[open]'];
         return remoteCall
             .callRemoteTestUtil(
                 'deepQueryAllElements', appId, [elements, ['display']])
@@ -76,7 +76,7 @@ function closeQuickViewSteps(appId) {
     function(result) {
       chrome.test.assertTrue(!!result, 'fakeMouseClick failed');
       repeatUntil(function() {
-        const elements = ['#quick-view', '#dialog'];
+        const elements = ['#quick-view', '#dialog:not([open])'];
         return remoteCall
             .callRemoteTestUtil(
                 'deepQueryAllElements', appId, [elements, ['display']])
