e2c8885728724c699f43d1b2bfe83be0377f8e39
Lan Wei <lanwei@chromium.org>
2018-10-02 23:50:24 +0000

    Do not apply hover during scroll
    In order to improve the performance and eliminate the issues that the
    fake mouse move events cause to the testing and web developers, we
    should not send fake mouse move events during scroll. This is
    implemented behind a flag.
    
    We will update the hover effect after scroll finishes in a following
    patch.
    
    Bug: 877132
    Change-Id: Iee838f50e0a52b5048081d79f91442ddf0467e8f
    Reviewed-on: https://chromium-review.googlesource.com/c/1211907
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Navid Zolghadr <nzolghadr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596049}

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -500,6 +500,9 @@ void SetRuntimeFeaturesDefaultsAndUpdateFromArgs(
 
   WebRuntimeFeatures::EnableNoHoverAfterLayoutChange(
       base::FeatureList::IsEnabled(features::kNoHoverAfterLayoutChange));
+
+  WebRuntimeFeatures::EnableNoHoverDuringScroll(
+      base::FeatureList::IsEnabled(features::kNoHoverDuringScroll));
 }
 
 }  // namespace content
