019882ab8b8ab032460b482acabb115c9dbcff1b
Sylvain Defresne <sdefresne@chromium.org>
2018-10-02 15:11:33 +0000

    Convert UserCloudPolicyManagerFactory to TestingFactory
    TestingFactory is a base::Callback<> while TestingFactoryFunction
    is a function pointer. Convert UserCloudPolicyManagerFactory to
    the callback based type.
    
    Bug: 809610
    Change-Id: If041d74cf623b29672c90351460f23be9a5fb24d
    Reviewed-on: https://chromium-review.googlesource.com/1245713
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595841}

--- chrome/browser/policy/cloud/user_policy_signin_service_unittest.cc
+++ chrome/browser/policy/cloud/user_policy_signin_service_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <utility>
 
+#include "base/bind.h"
 #include "base/files/file_path.h"
 #include "base/run_loop.h"
 #include "base/test/bind_test_util.h"
@@ -170,7 +171,7 @@ class UserPolicySigninServiceTest : public testing::Test {
     // instances) so we have to inject our testing factory via a special
     // API before creating the profile.
     UserCloudPolicyManagerFactory::GetInstance()->RegisterTestingFactory(
-        BuildCloudPolicyManager);
+        base::BindRepeating(&BuildCloudPolicyManager));
     TestingProfile::Builder builder;
     builder.SetPrefService(
         std::unique_ptr<sync_preferences::PrefServiceSyncable>(
