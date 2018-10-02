7bab1d1f0ea394ea7ff3e17491e745a1b6cd753c
Ben Pastene <bpastene@chromium.org>
2018-10-02 18:06:42 +0000

    Support conditionals for tast cros vm tests.
    And add a new test "chrome_all_tast_tests" that uses a conditional. And
    add that test to the FYI bot.
    
    Bug: 876587
    Change-Id: I9518389338b99207ba045bfc62d87dda02c4e3e9
    Reviewed-on: https://chromium-review.googlesource.com/c/1256209
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Commit-Queue: Ben Pastene <bpastene@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595905}

--- build/chromeos/run_vm_test.py
+++ build/chromeos/run_vm_test.py
@@ -9,6 +9,7 @@ import argparse
 import json
 import logging
 import os
+import pipes
 import re
 import signal
 import sys
@@ -160,6 +161,7 @@ class TastTest(RemoteTest):
 
     self._suite_name = args.suite_name
     self._tests = args.tests
+    self._conditional = args.conditional
 
   @property
   def suite_name(self):
@@ -178,7 +180,10 @@ class TastTest(RemoteTest):
         '--',
         'local_test_runner',
     ]
-    self._vm_test_cmd.extend(self._tests)
+    if self._conditional:
+      self._vm_test_cmd.append(pipes.quote(self._conditional))
+    else:
+      self._vm_test_cmd.extend(self._tests)
 
 
 class GTestTest(RemoteTest):
@@ -522,7 +527,11 @@ def main():
       '--test-launcher-summary-output', type=str,
       help='Generates a simple GTest-style JSON result file for the test run.')
   tast_test_parser.add_argument(
-      '--test', '-t', action='append', dest='tests', required=True,
+      '--conditional', type=str,
+      help='A conditional whose matching tests will run '
+           '(eg: ("dep:chrome" || "dep:chrome_login")).')
+  tast_test_parser.add_argument(
+      '--test', '-t', action='append', dest='tests',
       help='A Tast test to run in the VM (eg: "ui.ChromeLogin").')
 
   add_common_args(gtest_parser)
