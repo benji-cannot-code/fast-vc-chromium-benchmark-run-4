1b7934e3a124ae9b5a23af2a8389c7ff91fb0e0a
Abhishek Bhardwaj <abhishekbh@chromium.org>
2018-10-02 18:04:16 +0000

    Clang format network related files
    This change clang formats a subset of network related source files.
    
    BUG=891143
    TEST=Compile.
    
    Change-Id: I131d132dbcd36200310a8c785eab067795e20576
    Reviewed-on: https://chromium-review.googlesource.com/c/1252334
    Commit-Queue: Abhishek Bhardwaj <abhishekbh@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595904}

--- chromeos/dbus/fake_shill_device_client.cc
+++ chromeos/dbus/fake_shill_device_client.cc
@@ -40,8 +40,8 @@ const char kFailedMessage[] = "Failed";
 void ErrorFunction(const std::string& device_path,
                    const std::string& error_name,
                    const std::string& error_message) {
-  LOG(ERROR) << "Shill Error for: " << device_path
-             << ": " << error_name << " : " << error_message;
+  LOG(ERROR) << "Shill Error for: " << device_path << ": " << error_name
+             << " : " << error_message;
 }
 
 void PostError(const std::string& error,
@@ -392,8 +392,10 @@ ShillDeviceClient::TestInterface* FakeShillDeviceClient::GetTestInterface() {
 void FakeShillDeviceClient::AddDevice(const std::string& device_path,
                                       const std::string& type,
                                       const std::string& name) {
-  DBusThreadManager::Get()->GetShillManagerClient()->GetTestInterface()->
-      AddDevice(device_path);
+  DBusThreadManager::Get()
+      ->GetShillManagerClient()
+      ->GetTestInterface()
+      ->AddDevice(device_path);
 
   base::Value* properties = GetDeviceProperties(device_path);
   properties->SetKey(shill::kTypeProperty, base::Value(type));
@@ -408,15 +410,19 @@ void FakeShillDeviceClient::AddDevice(const std::string& device_path,
 }
 
 void FakeShillDeviceClient::RemoveDevice(const std::string& device_path) {
-  DBusThreadManager::Get()->GetShillManagerClient()->GetTestInterface()->
-      RemoveDevice(device_path);
+  DBusThreadManager::Get()
+      ->GetShillManagerClient()
+      ->GetTestInterface()
+      ->RemoveDevice(device_path);
 
   stub_devices_.RemoveWithoutPathExpansion(device_path, NULL);
 }
 
 void FakeShillDeviceClient::ClearDevices() {
-  DBusThreadManager::Get()->GetShillManagerClient()->GetTestInterface()->
-      ClearDevices();
+  DBusThreadManager::Get()
+      ->GetShillManagerClient()
+      ->GetTestInterface()
+      ->ClearDevices();
 
   stub_devices_.Clear();
 }
@@ -425,8 +431,8 @@ void FakeShillDeviceClient::SetDeviceProperty(const std::string& device_path,
                                               const std::string& name,
                                               const base::Value& value,
                                               bool notify_changed) {
-  VLOG(1) << "SetDeviceProperty: " << device_path
-          << ": " << name << " = " << value;
+  VLOG(1) << "SetDeviceProperty: " << device_path << ": " << name << " = "
+          << value;
   SetPropertyInternal(dbus::ObjectPath(device_path), name, value,
                       base::DoNothing(),
                       base::Bind(&ErrorFunction, device_path), notify_changed);
@@ -434,14 +440,14 @@ void FakeShillDeviceClient::SetDeviceProperty(const std::string& device_path,
 
 std::string FakeShillDeviceClient::GetDevicePathForType(
     const std::string& type) {
-  for (base::DictionaryValue::Iterator iter(stub_devices_);
-       !iter.IsAtEnd(); iter.Advance()) {
+  for (base::DictionaryValue::Iterator iter(stub_devices_); !iter.IsAtEnd();
+       iter.Advance()) {
     const base::DictionaryValue* properties = NULL;
     if (!iter.value().GetAsDictionary(&properties))
       continue;
     std::string prop_type;
-    if (!properties->GetStringWithoutPathExpansion(
-            shill::kTypeProperty, &prop_type) ||
+    if (!properties->GetStringWithoutPathExpansion(shill::kTypeProperty,
+                                                   &prop_type) ||
         prop_type != type)
       continue;
     return iter.key();
@@ -612,8 +618,8 @@ void FakeShillDeviceClient::PassStubDeviceProperties(
     const dbus::ObjectPath& device_path,
     const DictionaryValueCallback& callback) const {
   const base::DictionaryValue* device_properties = NULL;
-  if (!stub_devices_.GetDictionaryWithoutPathExpansion(
-      device_path.value(), &device_properties)) {
+  if (!stub_devices_.GetDictionaryWithoutPathExpansion(device_path.value(),
+                                                       &device_properties)) {
     base::DictionaryValue empty_dictionary;
     callback.Run(DBUS_METHOD_CALL_FAILURE, empty_dictionary);
     return;
@@ -639,8 +645,7 @@ void FakeShillDeviceClient::NotifyObserversPropertyChanged(
   }
   base::Value* value = NULL;
   if (!dict->GetWithoutPathExpansion(property, &value)) {
-    LOG(ERROR) << "Notify for unknown property: "
-        << path << " : " << property;
+    LOG(ERROR) << "Notify for unknown property: " << path << " : " << property;
     return;
   }
   for (auto& observer : GetObserverList(device_path))
