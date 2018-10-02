31cc59382dcfbcbb133d84ba9dac9bd35595d28b
Sam Zackrisson <saza@chromium.org>
2018-10-02 12:24:30 +0000

    Remove AEC interface usage in MediaStreamAudioProcessor and AudioProcessor
    echo_control_mobile() and echo_cancellation() interfaces are replaced
    with AudioProcessing::Config.
    
    Some configuration is now hardcoded defaults in APM, and hence should
    not (can not) be configured in these processors.
    
    Statistics are now accessed through AudioProcessing::GetStatistics.
    
    A UMA metric disappears.
    
    Bug: webrtc:9535
    Change-Id: Ia23d58cf1a1315605beef3ec72e080844bb94e5d
    Reviewed-on: https://chromium-review.googlesource.com/1243123
    Reviewed-by: Oskar Sundbom <ossu@chromium.org>
    Reviewed-by: Max Morin <maxmorin@chromium.org>
    Commit-Queue: Sam Zackrisson <saza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595806}

--- media/webrtc/echo_information.h
+++ media/webrtc/echo_information.h
@@ -12,32 +12,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media {
 
-// A helper class to log echo information in general and Echo Cancellation
+// A helper class to log echo information in general and AEC2
 // quality in particular.
 class COMPONENT_EXPORT(MEDIA_WEBRTC) EchoInformation {
  public:
   EchoInformation();
   virtual ~EchoInformation();
 
-  // Updates stats, and reports delay metrics as UMA stats every 5 seconds.
+  // Updates stats, and reports metrics as UMA stats every 5 seconds.
   // Must be called every time AudioProcessing::ProcessStream() is called.
-  void UpdateAecStats(webrtc::EchoCancellation* echo_cancellation);
+  void UpdateAecStats(
+      const webrtc::AudioProcessingStats& audio_processing_stats);
 
   // Reports AEC divergent filter metrics as UMA and resets the associated data.
   void ReportAndResetAecDivergentFilterStats();
 
  private:
-  void UpdateAecDelayStats(webrtc::EchoCancellation* echo_cancellation);
-  void UpdateAecDivergentFilterStats(
-      webrtc::EchoCancellation* echo_cancellation);
-
-  // Counter to track 5 seconds of data in order to query a new metric from
-  // webrtc::EchoCancellation::GetEchoDelayMetrics().
-  int delay_stats_time_ms_;
-  bool echo_frames_received_;
-
-  // Counter to track 1 second of data in order to query a new divergent filter
-  // fraction metric from webrtc::EchoCancellation::GetMetrics().
+  // Counter to store a new value for the divergent filter fraction metric in
+  // AEC2, once every second.
   int divergent_filter_stats_time_ms_;
 
   // Total number of times we queried for the divergent filter fraction metric.
