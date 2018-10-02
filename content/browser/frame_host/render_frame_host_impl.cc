fda60635ae357f65c6cb49c75f6e58f9ff66853c
Eric Roman <eroman@chromium.org>
2018-10-02 22:13:54 +0000

    Support content:// URLs on Android using Network Service.
    Bug: 882019
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Change-Id: I5c8f62c7615708e2b1019e11e9639552b9b267ae
    Reviewed-on: https://chromium-review.googlesource.com/c/1244101
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Commit-Queue: Eric Roman <eroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596004}

--- content/browser/frame_host/render_frame_host_impl.cc
+++ content/browser/frame_host/render_frame_host_impl.cc
@@ -182,6 +182,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/url_constants.h"
 
 #if defined(OS_ANDROID)
+#include "content/browser/android/content_url_loader_factory.h"
 #include "content/browser/android/java_interfaces_impl.h"
 #include "content/browser/frame_host/render_frame_host_android.h"
 #include "content/public/browser/android/java_interfaces.h"
@@ -4074,6 +4075,18 @@ void RenderFrameHostImpl::CommitNavigation(
       non_network_url_loader_factories_.emplace(url::kFileScheme,
                                                 std::move(file_factory));
     }
+
+#if defined(OS_ANDROID)
+    if (common_params.url.SchemeIs(url::kContentScheme)) {
+      // Only content:// URLs can load content:// subresources
+      auto content_factory = std::make_unique<ContentURLLoaderFactory>(
+          base::CreateSequencedTaskRunnerWithTraits(
+              {base::MayBlock(), base::TaskPriority::BEST_EFFORT,
+               base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN}));
+      non_network_url_loader_factories_.emplace(url::kContentScheme,
+                                                std::move(content_factory));
+    }
+#endif
 
     StoragePartition* partition =
         BrowserContext::GetStoragePartition(browser_context, GetSiteInstance());
