a50a08a631fd025c236fa3bb0e99e253ba3823f6
Daniel Bratell <bratell@opera.com>
2018-10-02 13:34:11 +0000

    Resolve conflicts between Win32 headers and OpenSSL
    Both some Win32 headers and OpenSSL/BoringSSL defines some
    constant macros like X509_NAME. To avoid that clash we have
    a wrapper for <wincrypt.h>, crypto/wincrypt_shim.h Use that
    one in chrome/chrome_cleaner/os/system_util.h too since
    that header might be used close to OpenSSL code.
    
    Change-Id: I678b5aa1658e8436eed4b431e6c2950ce7acfb3b
    Reviewed-on: https://chromium-review.googlesource.com/1255825
    Reviewed-by: Joe Mason <joenotcharles@chromium.org>
    Commit-Queue: Daniel Bratell <bratell@opera.com>
    Cr-Commit-Position: refs/heads/master@{#595820}

--- chrome/chrome_cleaner/os/system_util.h
+++ chrome/chrome_cleaner/os/system_util.h
@@ -8,10 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <windows.h>
 
-#include <wincrypt.h>
-
 #include "base/strings/string16.h"
 #include "base/win/scoped_handle.h"
+#include "crypto/wincrypt_shim.h"
 
 namespace chrome_cleaner {
 
