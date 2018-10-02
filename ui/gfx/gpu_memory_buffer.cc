c2f4f3e8594fec752926f9611df2f8a6af8d0aa8
Alexandr Ilin <alexilin@chromium.org>
2018-10-02 15:35:11 +0000

    Initialize all members of GpuMemoryBufferHandle in the default ctor
    GpuMemoryBufferHandle::offset and GpuMemoryBufferHandle::stride are not
    initialized in the default constructor. These uninitialized members may be
    serialized later by mojo that makes MSAN bot unhappy.
    
    Bug: 890930
    Change-Id: I0663de62997d673b97907a8bd470e9834bdaf638
    Reviewed-on: https://chromium-review.googlesource.com/1256786
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Commit-Queue: Alexandr Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595850}

--- ui/gfx/gpu_memory_buffer.cc
+++ ui/gfx/gpu_memory_buffer.cc
@@ -9,7 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace gfx {
 
-GpuMemoryBufferHandle::GpuMemoryBufferHandle() : type(EMPTY_BUFFER), id(0) {}
+GpuMemoryBufferHandle::GpuMemoryBufferHandle()
+    : type(EMPTY_BUFFER), id(0), offset(0), stride(0) {}
 
 // TODO(crbug.com/863011): Reset |type| and possibly the handles on the
 // moved-from object.
