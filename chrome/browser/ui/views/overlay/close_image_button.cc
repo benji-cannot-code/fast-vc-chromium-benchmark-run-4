1e6c8976e692b55442a77ada5e2d69fe2275d505
Jennifer Apacible <apacible@chromium.org>
2018-10-02 19:29:42 +0000

    Remove unused include statements.
    Change-Id: I4321d6126848c955389512b25d0de55e01debcca
    Reviewed-on: https://chromium-review.googlesource.com/1249570
    Reviewed-by: CJ DiMeglio <lethalantidote@chromium.org>
    Commit-Queue: apacible <apacible@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595946}

--- chrome/browser/ui/views/overlay/close_image_button.cc
+++ chrome/browser/ui/views/overlay/close_image_button.cc
@@ -9,8 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/generated_resources.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/base/l10n/l10n_util.h"
-#include "ui/display/display.h"
-#include "ui/display/screen.h"
 #include "ui/gfx/color_palette.h"
 #include "ui/gfx/paint_vector_icon.h"
 #include "ui/views/vector_icons.h"
