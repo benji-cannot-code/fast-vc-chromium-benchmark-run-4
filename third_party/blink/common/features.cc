d446f9681219e496784273ae17bf6cb5629962bb
Carlos IL <carlosil@chromium.org>
2018-10-02 01:47:48 +0000

    Added blink mixed content auto upgrade flag
    Starting implementation of go/auto-upgrade-mixed, adds the variations
    flag and auto upgrades all mixed content (with no fallback) if it is
    set. Further CLs will filter by mixed content type (blockable vs
    optionally blockable) depending on variations parameters, and will
    add fallback.
    
    Change-Id: Ic14124fe21ba08dc34738fdedebe2a37fb73ac3d
    Reviewed-on: https://chromium-review.googlesource.com/1249094
    Commit-Queue: Carlos IL <carlosil@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595691}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -83,6 +83,9 @@ const base::Feature kStopNonTimersInBackground {
 const base::Feature kWritableFilesAPI{"WritableFilesAPI",
                                       base::FEATURE_DISABLED_BY_DEFAULT};
 
+const base::Feature kMixedContentAutoupgrade{"AutoupgradeMixedContent",
+                                             base::FEATURE_DISABLED_BY_DEFAULT};
+
 const char kAutofillPreviewStyleExperimentBgColorParameterName[] = "bg_color";
 
 const char kAutofillPreviewStyleExperimentColorParameterName[] = "color";
--- third_party/blink/renderer/core/loader/frame_loader.cc
+++ third_party/blink/renderer/core/loader/frame_loader.cc
@@ -39,8 +39,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include "base/auto_reset.h"
+#include "base/feature_list.h"
 #include "base/metrics/histogram_macros.h"
 #include "services/network/public/mojom/request_context_frame_type.mojom-blink.h"
+#include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/platform/modules/fetch/fetch_api_request.mojom-shared.h"
 #include "third_party/blink/public/platform/modules/service_worker/web_service_worker_network_provider.h"
 #include "third_party/blink/public/platform/task_type.h"
@@ -1660,8 +1662,10 @@ void FrameLoader::UpgradeInsecureRequest(ResourceRequest& resource_request,
     return;
 
   if (!(origin_context->GetSecurityContext().GetInsecureRequestPolicy() &
-        kUpgradeInsecureRequests))
+        kUpgradeInsecureRequests) &&
+      !base::FeatureList::IsEnabled(features::kMixedContentAutoupgrade)) {
     return;
+  }
 
   // Nested frames are always upgraded on the browser process.
   if (resource_request.GetFrameType() ==
