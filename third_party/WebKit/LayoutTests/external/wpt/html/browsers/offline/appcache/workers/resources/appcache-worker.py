fc7814d50b23757aaf7be88b1d32d0ea7f979692
Hiroki Nakagawa <nhiroki@chromium.org>
2018-10-03 04:11:07 +0000

    AppCache: Add WPTs for AppCache on iframes
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Change-Id: I6933b37bf1e876f5c622876dd1c84ff830436c6a
    Bug: n/a
    Reviewed-on: https://chromium-review.googlesource.com/c/1245341
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596119}

--- third_party/WebKit/LayoutTests/external/wpt/html/browsers/offline/appcache/workers/resources/appcache-worker.py
+++ third_party/WebKit/LayoutTests/external/wpt/html/browsers/offline/appcache/workers/resources/appcache-worker.py
@@ -92,6 +92,7 @@ initPromise
   .then(importFallbackScriptTest)
   .then(fetchCachedFileTest)
   .then(fetchNotInCacheFileTest)
+  .then(fetchFallbackFileTest)
   .then(_ => postMessage('Done: %s'),
         error => postMessage(error.toString()));
 '''
