d74a5df3e4b9b6d6803b05cb3b20c8685daae670
Sylvain Defresne <sdefresne@chromium.org>
2018-10-03 12:13:25 +0000

    Remove direct uses of TestingFunctionFactory
    Replace them with uses of TestingFactory which is based
    on base::Callback<> instead of function pointers.
    
    This converts uses in /chrome/browser/favicon.
    
    This CL was uploaded by git cl split.
    
    R=caitkp@chromium.org
    
    Bug: 809610
    Change-Id: I050d6189e8eb2f2f80a6670ab3649218fabcd1ae
    Reviewed-on: https://chromium-review.googlesource.com/c/1246170
    Reviewed-by: Cait Phillips <caitkp@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596185}

--- chrome/browser/favicon/favicon_service_factory.cc
+++ chrome/browser/favicon/favicon_service_factory.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/bind.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/singleton.h"
 #include "chrome/browser/favicon/chrome_favicon_client.h"
@@ -55,9 +56,9 @@ FaviconServiceFactory* FaviconServiceFactory::GetInstance() {
 }
 
 // static
-BrowserContextKeyedServiceFactory::TestingFactoryFunction
+BrowserContextKeyedServiceFactory::TestingFactory
 FaviconServiceFactory::GetDefaultFactory() {
-  return &BuildFaviconService;
+  return base::BindRepeating(&BuildFaviconService);
 }
 
 FaviconServiceFactory::FaviconServiceFactory()
