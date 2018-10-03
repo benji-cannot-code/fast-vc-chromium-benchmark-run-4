2303f0ec1acd7eb0a0b6ade6657f1ea50c49b184
Takashi Toyoshima <toyoshim@chromium.org>
2018-10-03 09:30:11 +0000

    MidiManager: Deprecate Shutdown and Finalize methods
    Now that the dynamic instantiation mode is the only mode that
    MidiService and MidiManager support, we can ensure that any
    MidiManager instance will be constructed and destructed on the
    same IO thread. This will allow us to remove complicated shutdown
    system that relies on Shutdown() and Finalize(). Now destructor
    should just work.
    
    Bug: 672793
    Change-Id: I50cc843dd997f9de68492c13d7d845810020dcb9
    Reviewed-on: https://chromium-review.googlesource.com/c/1249421
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596163}

--- media/midi/midi_manager_mac.cc
+++ media/midi/midi_manager_mac.cc
@@ -118,7 +118,16 @@ MidiManager* MidiManager::Create(MidiService* service) {
 
 MidiManagerMac::MidiManagerMac(MidiService* service) : MidiManager(service) {}
 
-MidiManagerMac::~MidiManagerMac() = default;
+MidiManagerMac::~MidiManagerMac() {
+  bool result = service()->task_service()->UnbindInstance();
+  CHECK(result);
+
+  // Do not need to dispose |coremidi_input_| and |coremidi_output_| explicitly.
+  // CoreMIDI automatically disposes them on the client disposal.
+  base::AutoLock lock(midi_client_lock_);
+  if (midi_client_)
+    MIDIClientDispose(midi_client_);
+}
 
 void MidiManagerMac::StartInitialization() {
   if (!service()->task_service()->BindInstance()) {
@@ -128,19 +137,6 @@ void MidiManagerMac::StartInitialization() {
   service()->task_service()->PostBoundTask(
       kClientTaskRunner, base::BindOnce(&MidiManagerMac::InitializeCoreMIDI,
                                         base::Unretained(this)));
-}
-
-void MidiManagerMac::Finalize() {
-  if (!service()->task_service()->UnbindInstance()) {
-    NOTREACHED();
-  }
-
-  // Do not need to dispose |coremidi_input_| and |coremidi_output_| explicitly.
-  // CoreMIDI automatically disposes them on the client disposal.
-  base::AutoLock lock(midi_client_lock_);
-  if (midi_client_)
-    MIDIClientDispose(midi_client_);
-  midi_client_ = 0u;
 }
 
 void MidiManagerMac::DispatchSendMidiData(MidiManagerClient* client,
--- media/midi/midi_manager_mac.h
+++ media/midi/midi_manager_mac.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/macros.h"
 #include "base/synchronization/lock.h"
+#include "base/thread_annotations.h"
 #include "base/threading/thread.h"
 #include "media/midi/midi_export.h"
 #include "media/midi/midi_manager.h"
@@ -30,7 +31,6 @@ class MIDI_EXPORT MidiManagerMac final : public MidiManager {
 
   // MidiManager implementation.
   void StartInitialization() override;
-  void Finalize() override;
   void DispatchSendMidiData(MidiManagerClient* client,
                             uint32_t port_index,
                             const std::vector<uint8_t>& data,
@@ -64,8 +64,8 @@ class MIDI_EXPORT MidiManagerMac final : public MidiManager {
                     const std::vector<uint8_t>& data,
                     base::TimeTicks timestamp);
 
-  // CoreMIDI client reference, should be protected by |midi_client_lock_|.
-  MIDIClientRef midi_client_ = 0;
+  // CoreMIDI client reference.
+  MIDIClientRef midi_client_ GUARDED_BY(midi_client_lock_) = 0;
   base::Lock midi_client_lock_;
 
   // Following members can be accessed without any lock on kClientTaskRunner,
