bfa798fb4facf52cbb66553a86e1fa72bd25d8df
Malay Keshav <malaykeshav@chromium.org>
2018-10-03 22:31:38 +0000

    Use paint record to back ImageSkiaRep
    Currently ImageSkiaReps are backed by SkBitmaps and cc::PaintImages.
    While cc::PaintImages are drawables, they are sourced from the SkBitmaps
    provided to the constructor. This means images that are sourced from
    drawables, such as our vector icons, are rasterized into bitmaps first
    and then stored as ImageSkiaReps. This rasterization takes place on the
    UI thread.
    
    This patch makes cc::PaintRecord as the backing store for ImageSkiaReps.
    cc::PaintRecord are drawables and can be initialized with an SkBitmap if
    needed. Having images stored this way means drawables like vector icons
    dont need to be rasterized on the UI thread. Their rasterization will be
    managed by cc or even the GPU in case of OOPR. This also removes the
    constraint that ImageSkias are a rasterized source of image. It also
    gives a way to add more drawable sources(CanvasImageSources) like
    lottie.
    
    This feature is hidden behind a feature flag UsePaintRecordForImageSkia.
    
    Bug: 882507
    Change-Id: Id4a6500ba0e12da382e95d59e5fa8bc5642082ab
    Component: ImageSkiaRep, gfx, Image, Image Skia
    Reviewed-on: https://chromium-review.googlesource.com/c/1227488
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Malay Keshav <malaykeshav@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596402}

--- ui/gfx/canvas.cc
+++ ui/gfx/canvas.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/scoped_canvas.h"
 #include "ui/gfx/skia_paint_util.h"
 #include "ui/gfx/skia_util.h"
+#include "ui/gfx/switches.h"
 #include "ui/gfx/transform.h"
 
 namespace gfx {
@@ -358,8 +359,17 @@ void Canvas::DrawImageInt(const ImageSkia& image,
   ScopedCanvas scoper(this);
   canvas_->scale(SkFloatToScalar(1.0f / bitmap_scale),
                  SkFloatToScalar(1.0f / bitmap_scale));
-  canvas_->drawImage(image_rep.paint_image(), SkFloatToScalar(x * bitmap_scale),
-                     SkFloatToScalar(y * bitmap_scale), &flags);
+  if (base::FeatureList::IsEnabled(features::kUsePaintRecordForImageSkia)) {
+    canvas_->translate(std::round(x * bitmap_scale),
+                       std::round(y * bitmap_scale));
+    canvas_->saveLayer(nullptr, &flags);
+    canvas_->drawPicture(image_rep.GetPaintRecord());
+    canvas_->restore();
+  } else {
+    canvas_->drawImage(image_rep.paint_image(),
+                       SkFloatToScalar(x * bitmap_scale),
+                       SkFloatToScalar(y * bitmap_scale), &flags);
+  }
 }
 
 void Canvas::DrawImageInt(const ImageSkia& image,
