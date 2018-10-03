b389045c19ebc5fcba0f4037777242ab9f813eec
Rouslan Solomakhin <rouslan@chromium.org>
2018-10-03 15:55:25 +0000

    [Payment Handler][CCT] Detect configuration changes.
    Before this patch, rotating a device with the Payment Handler CCT could
    result in cutting off the bottom of the CCT, making a part of web
    content inaccessible.
    
    This patch updates the height of the window when configuration changes.
    The height is never more than the height of the screen available to the
    activity.
    
    After this patch, rotating a device with the Payment Handler CCT will
    resize the CCT to always fit on the screen, so all web content is
    accessible to the user.
    
    Bug: 890184
    Change-Id: I6ddf93f61e08e6a6281192e6a57b703ac18694ce
    Reviewed-on: https://chromium-review.googlesource.com/c/1252404
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596235}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/PaymentHandlerActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/PaymentHandlerActivity.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.customtabs;
 
+import android.content.res.Configuration;
 import android.view.Gravity;
 import android.view.WindowManager;
 
@@ -29,18 +30,7 @@ public class PaymentHandlerActivity extends CustomTabActivity {
     @Override
     public void preInflationStartup() {
         super.preInflationStartup();
-
-        int heightInPhysicalPixels = (int) (getWindowAndroid().getDisplay().getDisplayHeight()
-                * BOTTOM_SHEET_HEIGHT_RATIO);
-        int minimumHeightInPhysicalPixels = getResources().getDimensionPixelSize(
-                R.dimen.payments_handler_window_minimum_height);
-        if (heightInPhysicalPixels < minimumHeightInPhysicalPixels)
-            heightInPhysicalPixels = minimumHeightInPhysicalPixels;
-
-        WindowManager.LayoutParams attributes = getWindow().getAttributes();
-        attributes.height = heightInPhysicalPixels;
-        attributes.gravity = Gravity.BOTTOM;
-        getWindow().setAttributes(attributes);
+        updateHeight();
     }
 
     @Override
@@ -53,5 +43,34 @@ public class PaymentHandlerActivity extends CustomTabActivity {
         }
 
         super.handleFinishAndClose();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        updateHeight();
+    }
+
+    private void updateHeight() {
+        int displayHeightInPixels = getWindowAndroid().getDisplay().getDisplayHeight();
+        int heightInPhysicalPixels = (int) (displayHeightInPixels * BOTTOM_SHEET_HEIGHT_RATIO);
+        int minimumHeightInPhysicalPixels = getResources().getDimensionPixelSize(
+                R.dimen.payments_handler_window_minimum_height);
+
+        if (heightInPhysicalPixels < minimumHeightInPhysicalPixels) {
+            heightInPhysicalPixels = minimumHeightInPhysicalPixels;
+        }
+
+        if (heightInPhysicalPixels > displayHeightInPixels) {
+            heightInPhysicalPixels = WindowManager.LayoutParams.MATCH_PARENT;
+        }
+
+        WindowManager.LayoutParams attributes = getWindow().getAttributes();
+
+        if (attributes.height == heightInPhysicalPixels) return;
+
+        attributes.height = heightInPhysicalPixels;
+        attributes.gravity = Gravity.BOTTOM;
+        getWindow().setAttributes(attributes);
     }
 }
