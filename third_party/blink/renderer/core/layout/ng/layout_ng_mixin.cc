d32726dd45bda13312305b2347f8d475c2c179e4
tzik <tzik@chromium.org>
2018-10-03 16:58:17 +0000

    Add CORE_EXPORT to LayoutNGMixin explicit instantiations
    This adds the explicit instantiation declarations of LayoutNGMixin, and
    adds the CORE_EXPORT to the instantiation definitions, so that it's not
    instantiated in the test binaries, and is instantiated in the libcore
    with the proper visibility.
    
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng
    Change-Id: If51cda70ab619b178ca53c1fcbad8019fafc3c0b
    Reviewed-on: https://chromium-review.googlesource.com/1258806
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596258}

--- third_party/blink/renderer/core/layout/ng/layout_ng_mixin.cc
+++ third_party/blink/renderer/core/layout/ng/layout_ng_mixin.cc
@@ -10,9 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/editing/position_with_affinity.h"
 #include "third_party/blink/renderer/core/layout/hit_test_location.h"
-#include "third_party/blink/renderer/core/layout/layout_block_flow.h"
-#include "third_party/blink/renderer/core/layout/layout_table_caption.h"
-#include "third_party/blink/renderer/core/layout/layout_table_cell.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_inline_node_data.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_physical_line_box_fragment.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_constraint_space.h"
@@ -377,8 +374,8 @@ void LayoutNGMixin<Base>::DirtyLinesFromChangedChild(
   NGPaintFragment::DirtyLinesFromChangedChild(child);
 }
 
-template class LayoutNGMixin<LayoutTableCaption>;
-template class LayoutNGMixin<LayoutTableCell>;
-template class LayoutNGMixin<LayoutBlockFlow>;
+template class CORE_TEMPLATE_EXPORT LayoutNGMixin<LayoutTableCaption>;
+template class CORE_TEMPLATE_EXPORT LayoutNGMixin<LayoutTableCell>;
+template class CORE_TEMPLATE_EXPORT LayoutNGMixin<LayoutBlockFlow>;
 
 }  // namespace blink
--- third_party/blink/renderer/core/layout/ng/layout_ng_mixin.h
+++ third_party/blink/renderer/core/layout/ng/layout_ng_mixin.h
@@ -9,7 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <type_traits>
 
 #include "third_party/blink/renderer/core/core_export.h"
+#include "third_party/blink/renderer/core/layout/layout_block_flow.h"
 #include "third_party/blink/renderer/core/layout/layout_box_model_object.h"
+#include "third_party/blink/renderer/core/layout/layout_table_caption.h"
+#include "third_party/blink/renderer/core/layout/layout_table_cell.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_constraint_space.h"
 
 namespace blink {
@@ -112,6 +115,13 @@ class LayoutNGMixin : public Base {
 
   friend class NGBaseLayoutAlgorithmTest;
 };
+
+extern template class CORE_EXTERN_TEMPLATE_EXPORT
+    LayoutNGMixin<LayoutBlockFlow>;
+extern template class CORE_EXTERN_TEMPLATE_EXPORT
+    LayoutNGMixin<LayoutTableCaption>;
+extern template class CORE_EXTERN_TEMPLATE_EXPORT
+    LayoutNGMixin<LayoutTableCell>;
 
 }  // namespace blink
 
