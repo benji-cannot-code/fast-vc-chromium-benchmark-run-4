2fee61710e793cb2ef295bc12898d7738286c74a
Jun Mukai <mukai@chromium.org>
2018-10-03 19:51:49 +0000

    Introduce ui::GestureRecognizerObserver
    This is phase-1 of tab-dragging with touch on Mash. This CL
    adds GestureRecognizerObserver class, which will be used in
    upcoming CLs.
    
    BUG=867074
    TEST=events_unittests
    
    Change-Id: I7a8c50ac16b58e1c3e31c7f0d8df097489ee49c3
    Reviewed-on: https://chromium-review.googlesource.com/c/1247101
    Commit-Queue: Jun Mukai <mukai@chromium.org>
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596333}

---
+++ ui/events/gestures/gesture_recognizer.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/events/gestures/gesture_recognizer.h"
+
+#include "ui/events/gestures/gesture_recognizer_observer.h"
+
+namespace ui {
+
+GestureRecognizer::GestureRecognizer() = default;
+GestureRecognizer::~GestureRecognizer() = default;
+
+void GestureRecognizer::AddObserver(GestureRecognizerObserver* observer) {
+  observers_.AddObserver(observer);
+}
+
+void GestureRecognizer::RemoveObserver(GestureRecognizerObserver* observer) {
+  observers_.RemoveObserver(observer);
+}
+
+}  // namespace ui
--- ui/views/controls/menu/menu_host.cc
+++ ui/views/controls/menu/menu_host.cc
@@ -87,7 +87,7 @@ void TransferGesture(Widget* source, Widget* target) {
 #else   // !defined(OS_MACOSX)
   source->GetGestureRecognizer()->TransferEventsTo(
       source->GetNativeView(), target->GetNativeView(),
-      ui::GestureRecognizer::ShouldCancelTouches::DontCancel);
+      ui::TransferTouchesBehavior::kDontCancel);
 #endif  // defined(OS_MACOSX)
 }
 
