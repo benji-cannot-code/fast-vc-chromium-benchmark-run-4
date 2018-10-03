9955285b94d5bda7f2697273e4d0163e7a8b7ec6
Hajime Hoshi <hajimehoshi@chromium.org>
2018-10-03 09:36:52 +0000

    Avoid using the default task runner in blink_platform_unittests
    The default task runner should be avoided if possible.
    
    Bug: 870606
    Change-Id: Id4a3a5cc11d7a101b9f80fc339142f43ad41e9c3
    Reviewed-on: https://chromium-review.googlesource.com/c/1256473
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596165}

--- third_party/blink/renderer/platform/testing/testing_platform_support_with_mock_scheduler.h
+++ third_party/blink/renderer/platform/testing/testing_platform_support_with_mock_scheduler.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_TESTING_TESTING_PLATFORM_SUPPORT_WITH_MOCK_SCHEDULER_H_
 
 #include <memory>
+#include "base/test/test_mock_time_task_runner.h"
 #include "third_party/blink/public/platform/web_thread.h"
 #include "third_party/blink/renderer/platform/testing/testing_platform_support.h"
 #include "third_party/blink/renderer/platform/wtf/noncopyable.h"
@@ -35,6 +36,10 @@ class TestingPlatformSupportWithMockScheduler : public TestingPlatformSupport {
  public:
   TestingPlatformSupportWithMockScheduler();
   ~TestingPlatformSupportWithMockScheduler() override;
+
+  scoped_refptr<base::TestMockTimeTaskRunner> test_task_runner() {
+    return test_task_runner_;
+  }
 
   // Runs a single task.
   void RunSingleTask();
