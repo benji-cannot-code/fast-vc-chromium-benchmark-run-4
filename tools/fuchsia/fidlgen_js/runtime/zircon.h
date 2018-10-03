8282066e15034057d387e4bc588ed325898cef60
Scott Graham <scottmg@chromium.org>
2018-10-03 19:46:26 +0000

    fuchsia: Beginnings of FIDL JS bindings
    This is the very initial stages of writing a bindings generator to allow
    interop to/from JS with FIDL.
    
    The main pieces are:
    - the build time generator which consumes the JSON IR from `fidlc` and
      outputs a fidl.js
    - integration with gn in fidl_library.gni
    - a JS runtime support library (mostly helps with message encoding)
    - new unittest binary (which also currently holds some Zircon
      integration points that will eventually be moved to a C++ runtime
      support library.)
    
    There are many, many things it does not yet handle, e.g. responses
    (either sync or async), events, passing structs, passing unions, ...
    
    For reference, test/simple.fidl generates this
    https://gist.github.com/sgraham/3935c64d5bc1b67eea1f6e3b38fef6f1
    
    
    Bug: 883496
    Change-Id: I58e5d2b81213f20a3198b68cbd2bc01660ba58f7
    Reviewed-on: https://chromium-review.googlesource.com/c/1222697
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Adam Klein <adamk@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Scott Graham <scottmg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596329}

---
+++ tools/fuchsia/fidlgen_js/runtime/zircon.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef TOOLS_FUCHSIA_FIDLGEN_JS_RUNTIME_ZIRCON_H_
+#define TOOLS_FUCHSIA_FIDLGEN_JS_RUNTIME_ZIRCON_H_
+
+#include "v8/include/v8.h"
+
+namespace fidljs {
+
+// Adds Zircon APIs bindings to |global|, for use by JavaScript callers.
+void InjectZxBindings(v8::Isolate* isolate, v8::Local<v8::Object> global);
+
+}  // namespace fidljs
+
+#endif  // TOOLS_FUCHSIA_FIDLGEN_JS_RUNTIME_ZIRCON_H_
