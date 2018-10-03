55424878ba392d1769de5a549b391809a1adfa1d
Tetsui Ohkubo <tetsui@chromium.org>
2018-10-03 09:28:28 +0000

    NewMessageListView: Add borders and round corners
    This CL adds rounded corners and separator lines to notifications in
    UnifiedMessageListView.
    
    UnifiedMessageListView will replace MessageListView. It's behind a flag:
    --enable-features=NewMessageListView
    
    Design doc: go/chrome-popup-refactoring
    
    TEST=UnifiedMessageListViewTest
    BUG=769219
    
    Change-Id: I9616b8d3e6505046448742e3f8936188e23e0129
    Reviewed-on: https://chromium-review.googlesource.com/c/1258783
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596160}

--- ash/system/message_center/unified_message_list_view.cc
+++ ash/system/message_center/unified_message_list_view.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/message_center/unified_message_list_view.h"
 
 #include "ash/system/message_center/new_unified_message_center_view.h"
+#include "ash/system/tray/tray_constants.h"
 #include "base/auto_reset.h"
 #include "ui/message_center/message_center.h"
 #include "ui/message_center/views/message_view.h"
@@ -26,7 +27,13 @@ UnifiedMessageListView::UnifiedMessageListView(
 
   SetLayoutManager(
       std::make_unique<views::BoxLayout>(views::BoxLayout::kVertical));
+}
 
+UnifiedMessageListView::~UnifiedMessageListView() {
+  MessageCenter::Get()->RemoveObserver(this);
+}
+
+void UnifiedMessageListView::Init() {
   bool is_latest = true;
   for (auto* notification : MessageCenter::Get()->GetVisibleNotifications()) {
     auto* view = CreateMessageView(*notification);
@@ -37,10 +44,7 @@ UnifiedMessageListView::UnifiedMessageListView(
     MessageCenter::Get()->DisplayedNotification(
         notification->id(), message_center::DISPLAY_SOURCE_MESSAGE_CENTER);
   }
-}
-
-UnifiedMessageListView::~UnifiedMessageListView() {
-  MessageCenter::Get()->RemoveObserver(this);
+  UpdateBorders();
 }
 
 void UnifiedMessageListView::ChildPreferredSizeChanged(views::View* child) {
@@ -67,6 +71,7 @@ void UnifiedMessageListView::OnNotificationAdded(const std::string& id) {
   // Expand the latest notification.
   view->SetExpanded(view->IsAutoExpandingAllowed());
   AddChildView(view);
+  UpdateBorders();
   PreferredSizeChanged();
 }
 
@@ -80,6 +85,7 @@ void UnifiedMessageListView::OnNotificationRemoved(const std::string& id,
     }
   }
 
+  UpdateBorders();
   PreferredSizeChanged();
 }
 
@@ -103,8 +109,6 @@ message_center::MessageView* UnifiedMessageListView::CreateMessageView(
     const Notification& notification) const {
   auto* view = message_center::MessageViewFactory::Create(notification);
   view->SetIsNested();
-  view->UpdateCornerRadius(0, 0);
-  view->SetBorder(views::NullBorder());
   if (message_center_view_)
     message_center_view_->ConfigureMessageView(view);
   return view;
@@ -116,6 +120,21 @@ void UnifiedMessageListView::CollapseAllNotifications() {
     auto* view = static_cast<message_center::MessageView*>(child_at(i));
     if (!view->IsManuallyExpandedOrCollapsed())
       view->SetExpanded(false);
+  }
+}
+
+void UnifiedMessageListView::UpdateBorders() {
+  for (int i = 0; i < child_count(); ++i) {
+    auto* view = static_cast<message_center::MessageView*>(child_at(i));
+    const bool is_top = i == 0;
+    const bool is_bottom = i == child_count() - 1;
+    view->UpdateCornerRadius(is_top ? kUnifiedTrayCornerRadius : 0,
+                             is_bottom ? kUnifiedTrayCornerRadius : 0);
+    view->SetBorder(is_bottom
+                        ? views::NullBorder()
+                        : views::CreateSolidSidedBorder(
+                              0, 0, kUnifiedNotificationSeparatorThickness, 0,
+                              kUnifiedNotificationSeparatorColor));
   }
 }
 
