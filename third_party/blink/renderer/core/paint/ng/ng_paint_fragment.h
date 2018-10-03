49a2cd9035956ac1480bd3485f8cc633a81d9ecc
Koji Ishii <kojii@chromium.org>
2018-10-03 16:17:21 +0000

    [LayoutNG] Change to FullPaintInvalidation on all objects from Subtree FullPaintInvalidation
    LayoutNG used to apply Subtree paint invalidation for new
    NGPaintFragment, because fragments are always replaced with
    new ones. With recent changes to reuse NGPaintFragment, we
    can start exploring to do less paint invalidations.
    
    This patch changes Subtree to FullPaintInvalidation on all
    objects. There might be cases where we can do even less, but
    this change:
    * paint/invalidation test results match better to legacy.
    * Fixes 4 paint/invalidation/css-grid-layout by not
      invalidating inline formatting context container.
    
    The impact on blink_perf/paint is unclear. It looks like
    large-table-background-change-with-* and some other tests are
    flaky, and this change does not seem to have much impacts on
    blink_perf/paint. Still the paint/invalidation tests indicate
    we invalidate less with this change.
    
    Ran the pinpoint 3 times on the same code. On the first try:
    https://pinpoint-dot-chromeperf.appspot.com/job/12d257d8e40000
    one test regressed by 11.5% while not much changes in others.
    
    On the second try:
    https://pinpoint-dot-chromeperf.appspot.com/job/11f8d4c2e40000
    the regressed test above improved by 8.9%, another by 9.2% and
    2.5%.
    
    On the third try:
    https://pinpoint-dot-chromeperf.appspot.com/job/1723022ce40000
    the two tests above improved by 18%, though
    fixed-and-many-layers-scroll regresed by 5.6%.
    
    Bug: 714962
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng;luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I5b0db240c3d9f84ffb7f02d0f7bd7d0aa9b82296
    Reviewed-on: https://chromium-review.googlesource.com/c/1250821
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596246}

--- third_party/blink/renderer/core/paint/ng/ng_paint_fragment.h
+++ third_party/blink/renderer/core/paint/ng/ng_paint_fragment.h
@@ -125,6 +125,9 @@ class CORE_EXPORT NGPaintFragment : public RefCounted<NGPaintFragment>,
       const LayoutSelectionStatus&) const;
   NGPhysicalOffsetRect ComputeLocalSelectionRectForReplaced() const;
 
+  // Set ShouldDoFullPaintInvalidation flag in the corresponding LayoutObject.
+  void SetShouldDoFullPaintInvalidation();
+
   // Set ShouldDoFullPaintInvalidation flag in the corresponding LayoutObject
   // recursively.
   void SetShouldDoFullPaintInvalidationRecursively();
