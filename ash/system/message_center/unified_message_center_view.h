a09825885fa8a39038d4fc8dbd8362eb160bf69a
Tetsui Ohkubo <tetsui@chromium.org>
2018-10-03 03:43:07 +0000

    Initial implementation of New MessageListView.
    This CL adds NewUnifiedMessageCenterView and UnifiedMessageListView
    that replace UnifiedMessageCenterView, and MessageListView.
    
    The new implementation is behind a flag
    (--enable-features=NewMessageListView). Eventually old implementation
    will be removed and NewUnifiedMessageCenterView will be renamed to
    UnifiedMessageCenterView.
    
    Design doc: go/chrome-popup-refactoring
    
    TEST=UnifiedMessageListViewTest
    BUG=769219
    
    Change-Id: I1db9d73acfed918bc73b828ef3b406ef5da97a0c
    Reviewed-on: https://chromium-review.googlesource.com/c/1244182
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596115}

--- ash/system/message_center/unified_message_center_view.h
+++ ash/system/message_center/unified_message_center_view.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef ASH_SYSTEM_UNIFIED_UNIFIED_MESSAGE_CENTER_VIEW_H_
-#define ASH_SYSTEM_UNIFIED_UNIFIED_MESSAGE_CENTER_VIEW_H_
+#ifndef ASH_SYSTEM_MESSAGE_CENTER_UNIFIED_MESSAGE_CENTER_VIEW_H_
+#define ASH_SYSTEM_MESSAGE_CENTER_UNIFIED_MESSAGE_CENTER_VIEW_H_
 
 #include <stddef.h>
 
@@ -138,4 +138,4 @@ class ASH_EXPORT UnifiedMessageCenterView
 
 }  // namespace ash
 
-#endif  // ASH_SYSTEM_UNIFIED_UNIFIED_MESSAGE_CENTER_VIEW_H_
+#endif  // ASH_SYSTEM_MESSAGE_CENTER_UNIFIED_MESSAGE_CENTER_VIEW_H_
