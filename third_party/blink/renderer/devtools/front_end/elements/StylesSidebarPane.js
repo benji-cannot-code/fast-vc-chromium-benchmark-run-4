25b6d2f0f24de81a3f8dcd5097a649b2451a5cee
Erik Luo <luoe@chromium.org>
2018-10-03 00:15:43 +0000

    DevTools: do not increment on Arrow when showing SuggestBox
    When SuggestBox is open, ArrowUp/Down should move vertically among
    suggestions. Before, it might increment numerical values.
    
    Bug: none
    Change-Id: Ic0ede8ddca09818cd850be5bef06a8e685018dd0
    Reviewed-on: https://chromium-review.googlesource.com/c/1258203
    Reviewed-by: Joel Einbinder <einbinder@chromium.org>
    Commit-Queue: Erik Luo <luoe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596062}

--- third_party/blink/renderer/devtools/front_end/elements/StylesSidebarPane.js
+++ third_party/blink/renderer/devtools/front_end/elements/StylesSidebarPane.js
@@ -2108,7 +2108,7 @@ Elements.StylesSidebarPane.CSSPropertyPrompt = class extends UI.TextPrompt {
       case 'ArrowDown':
       case 'PageUp':
       case 'PageDown':
-        if (this._handleNameOrValueUpDown(event)) {
+        if (!this.isSuggestBoxVisible() && this._handleNameOrValueUpDown(event)) {
           event.preventDefault();
           return;
         }
