5a7ae2058e219e2550147ba4a907c8d763c0074a
Hiroki Nakagawa <nhiroki@chromium.org>
2018-10-03 04:23:41 +0000

    PlzWorker: Support AppCache's fallback case for shared workers
    This CL makes SharedWorkerScriptFetcher ask network interceptors if they want
    to handle a response served by the default network loader. This is necessary for
    supporting AppCache's fallback.
    
    A possible confusing point of this CL is
    SharedWorkerScriptFetcher::OnReceiveResponse() can be called twice. First, it's
    called when the interceptor or the default network loader serves the shared
    worker's main script. Then, it's called again when the interceptor wants to
    serve an alternative response for the response served by the default network
    loader.
    
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Change-Id: I93b45fa1f2e42422c0e85d3a67471c891dc5992a
    Bug: 715632
    Reviewed-on: https://chromium-review.googlesource.com/c/1224611
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596122}

--- content/browser/shared_worker/shared_worker_script_loader_factory.cc
+++ content/browser/shared_worker/shared_worker_script_loader_factory.cc
@@ -90,10 +90,4 @@ void SharedWorkerScriptLoaderFactory::Clone(
   NOTREACHED();
 }
 
-base::Optional<SubresourceLoaderParams>
-SharedWorkerScriptLoaderFactory::TakeSubresourceLoaderParams() {
-  DCHECK(script_loader_);
-  return script_loader_->TakeSubresourceLoaderParams();
-}
-
 }  // namespace content
