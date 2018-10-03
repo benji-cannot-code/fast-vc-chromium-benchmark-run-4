2bbd5dcdc499d427512279d3b35f0230ccb0f7a7
Bence Béky <bnc@chromium.org>
2018-10-03 00:47:39 +0000

    SpdySession::GetPushedStream() cleanup.
    * Update hugely out-of-date comment for SpdySession::GetPushedStream().
    * Drop unused |stream_net_log| argument.
    * Do not reset |*stream| upon error.  The only production call site is
      in SpdyHttpStream::InitializeStream(), where |stream_| is already
      guaranteed to be nullptr when GetPushedStream() is called, see DCHECK
      in line 139.
    
    Change-Id: I4b7b5738dd171df9f01fc1bfd7ddec8376985e08
    Reviewed-on: https://chromium-review.googlesource.com/c/1257442
    Reviewed-by: Nick Harper <nharper@chromium.org>
    Commit-Queue: Bence Béky <bnc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596074}

--- net/spdy/spdy_session.cc
+++ net/spdy/spdy_session.cc
@@ -921,8 +921,7 @@ SpdySession::~SpdySession() {
 int SpdySession::GetPushedStream(const GURL& url,
                                  spdy::SpdyStreamId pushed_stream_id,
                                  RequestPriority priority,
-                                 SpdyStream** stream,
-                                 const NetLogWithSource& stream_net_log) {
+                                 SpdyStream** stream) {
   CHECK(!in_io_loop_);
   // |pushed_stream_id| must be valid.
   DCHECK_NE(pushed_stream_id, kNoPushedStreamFound);
@@ -931,7 +930,6 @@ int SpdySession::GetPushedStream(const GURL& url,
             pool_->push_promise_index()->FindStream(url, this));
 
   if (availability_state_ == STATE_DRAINING) {
-    *stream = nullptr;
     return ERR_CONNECTION_CLOSED;
   }
 
