b063f9b560871a57a4471939e81db7f51fdfd1fc
rbpotter <rbpotter@chromium.org>
2018-10-03 19:51:16 +0000

    Print Preview Refresh: Address UI review comments
    (1) Increase line height for destinations items
    (2) Remove "Advanced" label for printer options button
    (3) Remove margin on paper-icon-button so Print with system dialog...
        does not wrap (for English + normal font size)
    (4) Change pages per sheet label to sentence case
    (5) Fix alignment of "Destinations" label with printer names,
        particularly for printers that do not have a status/location
        displayed.
    
    Bug: 891375, 891378, 891379, 891395, 891577
    Change-Id: I13fbcd167e2e9ee513ef056ce5c9e471248dd3d8
    Reviewed-on: https://chromium-review.googlesource.com/c/1257370
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596332}

--- chrome/browser/resources/print_preview/new/destination_list.js
+++ chrome/browser/resources/print_preview/new/destination_list.js
@@ -66,7 +66,7 @@ Polymer({
 
       const entry = assert(entries[0]);
       // Don't set maxHeight below the minimum height.
-      const fullHeight = Math.max(entry.contentRect.height, 84);
+      const fullHeight = Math.max(entry.contentRect.height, 96);
       this.$.list.style.maxHeight = `${fullHeight}px`;
       this.forceIronResize();
     });
