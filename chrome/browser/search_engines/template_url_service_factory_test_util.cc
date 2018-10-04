9ea229a6ea3916d88b0b75d082aa639a2d548357
Sylvain Defresne <sdefresne@chromium.org>
2018-10-04 12:35:07 +0000

    Convert indirect uses of TestingFactoryFunction
    TestingFactoryFunction is a simple function pointer. It is
    deprecated in favor of TestingFactory which is a Callback<>.
    Convert indirect uses by using base::BindRepeating() in all
    invocation of SetTestingFactory/AddTestingFactory.
    
    This converts uses in src//chrome/browser/search_engines.
    
    This CL was uploaded by git cl split.
    
    R=pkasting@chromium.org
    
    Bug: 809610
    Change-Id: I8f556cf9c85b9103051c036d2a81720d6bdcdb87
    Reviewed-on: https://chromium-review.googlesource.com/c/1258173
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596633}

--- chrome/browser/search_engines/template_url_service_factory_test_util.cc
+++ chrome/browser/search_engines/template_url_service_factory_test_util.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/search_engines/template_url_service_factory_test_util.h"
 
+#include "base/bind.h"
 #include "base/run_loop.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
 #include "chrome/test/base/testing_profile.h"
@@ -18,7 +19,8 @@ TemplateURLServiceFactoryTestUtil::TemplateURLServiceFactoryTestUtil(
   profile_->CreateWebDataService();
 
   TemplateURLServiceFactory::GetInstance()->SetTestingFactoryAndUse(
-      profile_, TemplateURLServiceFactory::BuildInstanceFor);
+      profile_,
+      base::BindRepeating(&TemplateURLServiceFactory::BuildInstanceFor));
 }
 
 TemplateURLServiceFactoryTestUtil::~TemplateURLServiceFactoryTestUtil() {
