089cf3f0def50fe419d1fcb92226802ef37208c9
Karan Bhatia <karandeepb@chromium.org>
2018-10-04 00:43:08 +0000

    Declarative Net Request: Support runtime host permissions.
    This CL adds support for runtime host permissions to the Declarative Net Request
    API. Changes:
    
    - The behavior of REQUIRE_HOST_PERMISSION_FOR_URL_AND_INITIATOR is modified for
      the case when an extension has access to the initiator but the access to the
      request url is withheld. In this case, the extension is granted access to the
      request. This is similar to the current behavior of
      REQUIRE_HOST_PERMISSION_FOR_URL (used by web request API) and necessary for
      runtime host permissions to work. This allows extensions to intercept withheld
      cross-origin requests from a frame to which they have access.
    - RulesetManager is modified to notify the chrome layer that access to a request
      was withheld. This is necessary for us to track an extension's
      blocked/withheld actions on a tab.
    
    This CL also paves the way to transition the web request API to require host
    permissions to the initiator i.e. REQUIRE_HOST_PERMISSION_FOR_URL_AND_INITIATOR.
    
    BUG=157736, 809680
    
    Change-Id: Ic4737a55a3ad6f88625149bcb39eefeb84df7d91
    Reviewed-on: https://chromium-review.googlesource.com/c/1256219
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596450}

--- extensions/browser/api/declarative_net_request/ruleset_manager.h
+++ extensions/browser/api/declarative_net_request/ruleset_manager.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sequence_checker.h"
 #include "base/time/time.h"
 #include "extensions/common/extension_id.h"
+#include "extensions/common/permissions/permissions_data.h"
 #include "extensions/common/url_pattern_set.h"
 
 class GURL;
@@ -109,9 +110,10 @@ class RulesetManager {
   bool ShouldEvaluateRequest(const WebRequestInfo& request) const;
 
   // Returns whether |ruleset| should be evaluated for the given |request|.
-  bool ShouldEvaluateRulesetForRequest(const ExtensionRulesetData& ruleset,
-                                       const WebRequestInfo& request,
-                                       bool is_incognito_context) const;
+  PermissionsData::PageAccess ShouldEvaluateRulesetForRequest(
+      const ExtensionRulesetData& ruleset,
+      const WebRequestInfo& request,
+      bool is_incognito_context) const;
 
   // Sorted in decreasing order of |extension_install_time|.
   // Use a flat_set instead of std::set/map. This makes [Add/Remove]Ruleset
