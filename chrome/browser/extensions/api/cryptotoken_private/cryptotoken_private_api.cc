8e47ed479729786c001245fd555fd33c27daccad
Adam Langley <agl@chromium.org>
2018-10-04 23:09:01 +0000

    Also collect attestation metrics in U2F.
    54e0bebf0e added a histogram to collect metrics on the results of
    attestation prompts in webauthn. While there is still a sizable amount
    of legacy U2F usage, it's useful to collect similar metrics there too.
    
    This change causes U2F to also emit events for the existing histogram.
    Since we don't want to do significant work on the U2F code at this
    point, the granularity of the metrics in the U2F case is limited:
    there's no direct link to the frame nor U2F logic in the C++ code so
    timeouts aren't detected and abandonments are classified as negative
    responses—same as explicit user interactions.
    
    Change-Id: Ibb58dc084a0bb6599744d7112cf9975cd278c4e0
    Reviewed-on: https://chromium-review.googlesource.com/c/1255154
    Commit-Queue: Adam Langley <agl@chromium.org>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596907}

--- chrome/browser/extensions/api/cryptotoken_private/cryptotoken_private_api.cc
+++ chrome/browser/extensions/api/cryptotoken_private/cryptotoken_private_api.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 
 #include "base/callback.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
 #include "chrome/browser/extensions/extension_tab_util.h"
@@ -24,6 +25,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/origin.h"
 
 namespace {
+
+// U2FAttestationPromptResult enumerates events related to attestation prompts.
+// These values are recorded in an UMA histogram and so should not be
+// reassigned.
+enum class U2FAttestationPromptResult {
+  // kQueried indicates that the embedder was queried in order to determine
+  // whether attestation information should be returned to the origin.
+  kQueried = 0,
+  // kAllowed indicates that the query to the embedder was resolved positively.
+  // (E.g. the user clicked to allow, or the embedder allowed immediately by
+  // policy.) Note that this may still be recorded if the user clicks to allow
+  // attestation after the request has timed out.
+  kAllowed = 1,
+  // kBlocked indicates that the query to the embedder was resolved negatively.
+  // (E.g. the user clicked to block, or closed the dialog.) Navigating away or
+  // closing the tab also fall into this bucket.
+  kBlocked = 2,
+  kMaxValue = kBlocked,
+};
 
 const char kGoogleDotCom[] = "google.com";
 constexpr const char* kGoogleGstaticAppIds[] = {
@@ -54,6 +74,11 @@ bool ContainsAppIdByHash(const base::ListValue& list,
   }
 
   return false;
+}
+
+void RecordAttestationEvent(U2FAttestationPromptResult event) {
+  UMA_HISTOGRAM_ENUMERATION("WebAuthentication.U2FAttestationPromptResult",
+                            event);
 }
 
 }  // namespace
@@ -202,6 +227,7 @@ CryptotokenPrivateCanAppIdGetAttestationFunction::Run() {
     return RespondNow(Error("no PermissionRequestManager"));
   }
 
+  RecordAttestationEvent(U2FAttestationPromptResult::kQueried);
   // The created AttestationPermissionRequest deletes itself once complete.
   permission_request_manager->AddRequest(NewAttestationPermissionRequest(
       origin,
@@ -211,6 +237,8 @@ CryptotokenPrivateCanAppIdGetAttestationFunction::Run() {
 }
 
 void CryptotokenPrivateCanAppIdGetAttestationFunction::Complete(bool result) {
+  RecordAttestationEvent(result ? U2FAttestationPromptResult::kAllowed
+                                : U2FAttestationPromptResult::kBlocked);
   Respond(OneArgument(std::make_unique<base::Value>(result)));
 }
 
