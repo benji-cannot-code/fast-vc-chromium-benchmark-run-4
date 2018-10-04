21cf00951583f03b44d6b8a594dcc62fb5284450
Sylvain Defresne <sdefresne@chromium.org>
2018-10-04 11:30:40 +0000

    Convert indirect uses of TestingFactoryFunction
    TestingFactoryFunction is a simple function pointer. It is
    deprecated in favor of TestingFactory which is a Callback<>.
    Convert indirect uses by using base::BindRepeating() in all
    invocation of SetTestingFactory/AddTestingFactory.
    
    This converts uses in src//chrome/browser/ssl.
    
    This CL was uploaded by git cl split.
    
    R=rsleevi@chromium.org
    
    Bug: 809610
    Change-Id: I27af4c0d03bd13ab7d7d9ed3941b7afea0de2302
    Reviewed-on: https://chromium-review.googlesource.com/c/1258984
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596594}

--- chrome/browser/ssl/ssl_blocking_page_unittest.cc
+++ chrome/browser/ssl/ssl_blocking_page_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ssl/ssl_blocking_page.h"
 
+#include "base/bind.h"
 #include "base/strings/string_number_conversions.h"
 #include "chrome/browser/extensions/api/safe_browsing_private/safe_browsing_private_event_router_factory.h"
 #include "chrome/browser/safe_browsing/test_extension_event_observer.h"
@@ -35,8 +36,10 @@ class SSLBlockingPageTest : public ChromeRenderViewHostTestHarness {
     test_event_router_ =
         extensions::CreateAndUseTestEventRouter(browser_context());
     extensions::SafeBrowsingPrivateEventRouterFactory::GetInstance()
-        ->SetTestingFactory(browser_context(),
-                            safe_browsing::BuildSafeBrowsingPrivateEventRouter);
+        ->SetTestingFactory(
+            browser_context(),
+            base::BindRepeating(
+                &safe_browsing::BuildSafeBrowsingPrivateEventRouter));
   }
 
   extensions::TestEventRouter* test_event_router() {
