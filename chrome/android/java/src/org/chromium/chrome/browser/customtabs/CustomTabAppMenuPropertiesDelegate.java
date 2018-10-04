e22f4bb67b749d12f8f147d22e5acef87eb2b7c5
Boris Sazonov <bsazonov@chromium.org>
2018-10-04 22:31:33 +0000

    [Android] Fix refresh button tinting
    This CL changes how the drawable for the refresh button in the app menu
    is tinted. Now Drawable object is tinted instead of relying on
    ImageButton tinting, because ImageView can't tint LevelListDrawable on
    some Android versions.
    
    Bug: 891593
    Change-Id: Ic5d45b3f281f3a1dc44cada814e5c0d83b14b2b4
    Reviewed-on: https://chromium-review.googlesource.com/c/1262636
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596889}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabAppMenuPropertiesDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabAppMenuPropertiesDelegate.java
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.customtabs;
 
+import android.graphics.drawable.Drawable;
+import android.support.v4.graphics.drawable.DrawableCompat;
+import android.support.v7.content.res.AppCompatResources;
 import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -66,7 +69,10 @@ public class CustomTabAppMenuPropertiesDelegate extends AppMenuPropertiesDelegat
             forwardMenuItem.setEnabled(currentTab.canGoForward());
 
             mReloadMenuItem = menu.findItem(R.id.reload_menu_id);
-            mReloadMenuItem.setIcon(R.drawable.btn_reload_stop);
+            Drawable icon = AppCompatResources.getDrawable(mActivity, R.drawable.btn_reload_stop);
+            DrawableCompat.setTintList(
+                    icon, AppCompatResources.getColorStateList(mActivity, R.color.dark_mode_tint));
+            mReloadMenuItem.setIcon(icon);
             loadingStateChanged(currentTab.isLoading());
 
             MenuItem shareItem = menu.findItem(R.id.share_row_menu_id);
