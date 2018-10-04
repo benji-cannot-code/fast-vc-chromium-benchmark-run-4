c7eb14048fbdf4d0f139fe827699235d9504bae1
Daniel Cheng <dcheng@chromium.org>
2018-10-04 20:52:59 +0000

    Remove dependency on non-Blink variants from SecurityOrigin StructTraits
    Bug: 891686
    Change-Id: I5770942e18ee8a9221f322101bcc2793d74d3695
    Reviewed-on: https://chromium-review.googlesource.com/c/1262617
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596844}

--- third_party/blink/renderer/platform/mojo/security_origin_struct_traits.h
+++ third_party/blink/renderer/platform/mojo/security_origin_struct_traits.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/weborigin/security_origin.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 #include "url/mojom/origin.mojom-blink.h"
-#include "url/mojom/origin_mojom_traits.h"
 
 namespace mojo {
 
