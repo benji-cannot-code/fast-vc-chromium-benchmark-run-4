dbae156dabd3d7992ffdb6e4782939586403f699
Sylvain Defresne <sdefresne@chromium.org>
2018-10-04 09:52:44 +0000

    Convert indirect uses of TestingFactoryFunction
    TestingFactoryFunction is a simple function pointer. It is
    deprecated in favor of TestingFactory which is a Callback<>.
    Convert indirect uses by using base::BindRepeating() in all
    invocation of SetTestingFactory/AddTestingFactory.
    
    This converts uses in src//chrome/browser/translate.
    
    This CL was uploaded by git cl split.
    
    R=droger@chromium.org
    
    Bug: 809610
    Change-Id: Iad4ab19844ec672459312067de78681b70684765
    Reviewed-on: https://chromium-review.googlesource.com/c/1258356
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596563}

--- chrome/browser/translate/chrome_translate_client_unittest.cc
+++ chrome/browser/translate/chrome_translate_client_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/bind.h"
 #include "base/command_line.h"
 #include "base/memory/ref_counted.h"
 #include "base/run_loop.h"
@@ -46,8 +47,9 @@ class ChromeTranslateClientTest : public ChromeRenderViewHostTestHarness {
     ChromeRenderViewHostTestHarness::SetUp();
     fake_user_event_service_ = static_cast<syncer::FakeUserEventService*>(
         browser_sync::UserEventServiceFactory::GetInstance()
-            ->SetTestingFactoryAndUse(browser_context(),
-                                      &BuildFakeUserEventService));
+            ->SetTestingFactoryAndUse(
+                browser_context(),
+                base::BindRepeating(&BuildFakeUserEventService)));
     scoped_feature_list_ = std::make_unique<base::test::ScopedFeatureList>();
     scoped_feature_list_->InitWithFeatures(
         {switches::kSyncUserLanguageDetectionEvents,
