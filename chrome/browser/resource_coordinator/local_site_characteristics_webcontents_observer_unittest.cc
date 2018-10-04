8cf8627d0e16f8ff9c9aafd40e7d433466c24c87
Sylvain Defresne <sdefresne@chromium.org>
2018-10-04 12:18:41 +0000

    Convert indirect uses of TestingFactoryFunction
    TestingFactoryFunction is a simple function pointer. It is
    deprecated in favor of TestingFactory which is a Callback<>.
    Convert indirect uses by using base::BindRepeating() in all
    invocation of SetTestingFactory/AddTestingFactory.
    
    This converts uses in src//chrome/browser/resource_coordinator.
    
    This CL was uploaded by git cl split.
    
    R=haraken@chromium.org
    
    Bug: 809610
    Change-Id: I057b0bd65c07e68f71692e9f02259cd1e02448c9
    Reviewed-on: https://chromium-review.googlesource.com/c/1259030
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596623}

--- chrome/browser/resource_coordinator/local_site_characteristics_webcontents_observer_unittest.cc
+++ chrome/browser/resource_coordinator/local_site_characteristics_webcontents_observer_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/resource_coordinator/local_site_characteristics_webcontents_observer.h"
 
+#include "base/bind.h"
 #include "base/macros.h"
 #include "base/test/simple_test_tick_clock.h"
 #include "chrome/browser/resource_coordinator/local_site_characteristics_data_store_factory.h"
@@ -92,7 +93,7 @@ class LocalSiteCharacteristicsWebContentsObserverTest
     test_clock().Advance(base::TimeDelta::FromSeconds(1));
     // Set the testing factory for the test browser context.
     LocalSiteCharacteristicsDataStoreFactory::GetInstance()->SetTestingFactory(
-        browser_context(), &BuildMockDataStoreForContext);
+        browser_context(), base::BindRepeating(&BuildMockDataStoreForContext));
 
     TabLoadTracker::Get()->StartTracking(web_contents());
     LocalSiteCharacteristicsWebContentsObserver::
