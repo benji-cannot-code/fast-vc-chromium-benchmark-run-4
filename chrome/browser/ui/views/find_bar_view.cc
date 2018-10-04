c3c3f314fb89b6a6063be6e078f3b00c898e4115
lanwei <lanwei@chromium.org>
2018-10-04 21:26:59 +0000

    Do not show virtual keyboard when pressing Ctrl+F
    When we use Ctrl+F to open the find bar to seach text in the page, we
    should not pop up the virtual keybaord.
    
    Bug: 871756
    Change-Id: I64fa8337668f823aeb6f5d5af2aae0e9fc50e684
    Reviewed-on: https://chromium-review.googlesource.com/c/1257582
    Reviewed-by: Navid Zolghadr <nzolghadr@chromium.org>
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596857}

--- chrome/browser/ui/views/find_bar_view.cc
+++ chrome/browser/ui/views/find_bar_view.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/i18n/number_formatting.h"
 #include "base/macros.h"
 #include "base/strings/string_util.h"
+#include "build/build_config.h"
 #include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/themes/theme_properties.h"
@@ -322,7 +323,9 @@ void FindBarView::AddedToWidget() {
 
 void FindBarView::SetFocusAndSelection(bool select_all) {
   find_text_->RequestFocus();
+#if !defined(OS_WIN)
   GetWidget()->GetInputMethod()->ShowVirtualKeyboardIfEnabled();
+#endif
   if (select_all && !find_text_->text().empty())
     find_text_->SelectAll(true);
 }
