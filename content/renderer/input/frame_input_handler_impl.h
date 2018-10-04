f6853c8e5cc88892154f1122b53e96e93a6da2d3
Shimi Zhang <ctzsm@chromium.org>
2018-10-04 23:19:17 +0000

    Refactor SelectWordAroundCaret{Ack}()
    This CL:
    1) Moves SelectWordAroundCaret{Ack}() from RenderViewHost to
    RenderFrameHost.
    2) Moves SelectWordAroundCaret() from RenderView to FrameInputHandler,
    the new place is more suitable for this function.
    3) Migrates IPC to mojo, and does other IPC cleanup.
    
    Bug: 716618, 545684
    Change-Id: Ied823345dc7ff68ea6dd505eeb14ab5c6aca17d1
    Reviewed-on: https://chromium-review.googlesource.com/c/821561
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: Shimi Zhang <ctzsm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596914}

--- content/renderer/input/frame_input_handler_impl.h
+++ content/renderer/input/frame_input_handler_impl.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_RENDERER_INPUT_FRAME_INPUT_HANDLER_IMPL_H_
 
 #include "base/memory/ref_counted.h"
+#include "build/build_config.h"
 #include "content/common/content_export.h"
 #include "content/common/input/input_handler.mojom.h"
 #include "content/renderer/render_frame_impl.h"
@@ -67,6 +68,9 @@ class CONTENT_EXPORT FrameInputHandlerImpl : public mojom::FrameInputHandler {
   void SelectAll() override;
   void CollapseSelection() override;
   void SelectRange(const gfx::Point& base, const gfx::Point& extent) override;
+#if defined(OS_ANDROID)
+  void SelectWordAroundCaret(SelectWordAroundCaretCallback callback) override;
+#endif  // defined(OS_ANDROID)
   void AdjustSelectionByCharacterOffset(
       int32_t start,
       int32_t end,
@@ -97,7 +101,7 @@ class CONTENT_EXPORT FrameInputHandlerImpl : public mojom::FrameInputHandler {
   FrameInputHandlerImpl(base::WeakPtr<RenderFrameImpl> render_frame,
                         mojom::FrameInputHandlerRequest request);
 
-  void RunOnMainThread(const base::Closure& closure);
+  void RunOnMainThread(base::OnceClosure closure);
   void BindNow(mojom::FrameInputHandlerRequest request);
   void ExecuteCommandOnMainThread(const std::string& command,
                                   UpdateState state);
