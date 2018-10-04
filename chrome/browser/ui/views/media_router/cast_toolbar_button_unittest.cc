ca5b798570277252844f8e41da124e0b5aeb52c8
Sylvain Defresne <sdefresne@chromium.org>
2018-10-04 11:48:16 +0000

    Convert indirect uses of TestingFactoryFunction
    TestingFactoryFunction is a simple function pointer. It is
    deprecated in favor of TestingFactory which is a Callback<>.
    Convert indirect uses by using base::BindRepeating() in all
    invocation of SetTestingFactory/AddTestingFactory.
    
    This converts uses in src//chrome/browser/ui/views/media_router.
    
    This CL was uploaded by git cl split.
    
    R=takumif@chromium.org
    
    Bug: 809610
    Change-Id: I1fe6c352e5560dae357b5ab6aec08c493d47513a
    Reviewed-on: https://chromium-review.googlesource.com/c/1258008
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596603}

--- chrome/browser/ui/views/media_router/cast_toolbar_button_unittest.cc
+++ chrome/browser/ui/views/media_router/cast_toolbar_button_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/media_router/cast_toolbar_button.h"
 
+#include "base/bind.h"
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/media/router/media_router_factory.h"
 #include "chrome/browser/media/router/test/mock_media_router.h"
@@ -70,9 +71,9 @@ class CastToolbarButtonTest : public ChromeViewsTestBase {
   void SetUp() override {
     ChromeViewsTestBase::SetUp();
     MediaRouterFactory::GetInstance()->SetTestingFactory(
-        &profile_, &MockMediaRouter::Create);
+        &profile_, base::BindRepeating(&MockMediaRouter::Create));
     MediaRouterUIServiceFactory::GetInstance()->SetTestingFactory(
-        &profile_, &BuildUIService);
+        &profile_, base::BindRepeating(&BuildUIService));
 
     window_ = std::make_unique<TestBrowserWindow>();
     Browser::CreateParams browser_params(&profile_, true);
