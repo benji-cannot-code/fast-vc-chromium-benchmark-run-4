bac2fed405bb4ffb77b175aa8d6ea7f0ea2d01b7
mrefaat <mrefaat@chromium.org>
2018-10-04 23:28:11 +0000

    Fix "Open in" feature to treat downloading corrupted files as failure.
    Also fixed "Open in" unit test as the existing test was already passing a
    corrupted PDF data to the UrlLoader. the reason was that the generated
    pdf data had null characters, So when casting it to char* then converting
    that to string it produced 8 letters Data stream.
    
    Added unittest for the corrupted pdf case.
    
    Bug: 892211
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: I4fff1e22f617e6a0b2c08d9c6412e14265b8aab4
    Reviewed-on: https://chromium-review.googlesource.com/c/1261926
    Commit-Queue: Mohammad Refaat <mrefaat@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596918}

--- ios/chrome/browser/ui/open_in_controller.mm
+++ ios/chrome/browser/ui/open_in_controller.mm
@@ -9,10 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/location.h"
 #include "base/logging.h"
-#include "base/task/post_task.h"
-
+#include "base/mac/scoped_cftyperef.h"
 #include "base/sequenced_task_runner.h"
 #include "base/strings/sys_string_conversions.h"
+#include "base/task/post_task.h"
 #include "base/threading/scoped_blocking_call.h"
 #include "components/strings/grit/components_strings.h"
 #import "ios/chrome/browser/ui/alert_coordinator/alert_coordinator.h"
@@ -62,6 +62,15 @@ const CGFloat kOverlayedViewLabelWidthPercentage = 0.7;
 
 // Bottom margin for the label displayed on the |overlayedView_|.
 const CGFloat kOverlayedViewLabelBottomMargin = 60;
+
+// Returns true if the file located at |url| is a valid PDF file.
+bool HasValidPdfAtUrl(NSURL* _Nullable url) {
+  if (!url)
+    return false;
+  base::ScopedCFTypeRef<CGPDFDocumentRef> document(
+      CGPDFDocumentCreateWithURL((__bridge CFURLRef)url));
+  return document;
+}
 
 }  // anonymous namespace
 
@@ -523,7 +532,9 @@ class OpenInControllerBridge
 #pragma mark -
 #pragma mark File management
 
-- (void)removeDocumentAtPath:(NSString*)path {
+- (void)removeDocumentAtPath:(nullable NSString*)path {
+  if (!path)
+    return;
   base::ScopedBlockingCall scoped_blocking_call(base::BlockingType::WILL_BLOCK);
   NSFileManager* fileManager = [NSFileManager defaultManager];
   NSError* error = nil;
@@ -585,18 +596,17 @@ class OpenInControllerBridge
 }
 
 - (void)urlLoadDidComplete:(const base::FilePath&)filePath {
-  if (!filePath.empty()) {
-    NSURL* fileURL =
-        [NSURL fileURLWithPath:base::SysUTF8ToNSString(filePath.value())];
-    if (downloadCanceled_) {
-      sequencedTaskRunner_->PostTask(FROM_HERE, base::BindOnce(^{
-                                       [self
-                                           removeDocumentAtPath:[fileURL path]];
-                                     }));
-    } else {
-      [self presentOpenInMenuForFileAtURL:fileURL];
-    }
-  } else if (!downloadCanceled_) {
+  NSURL* fileURL = nil;
+  if (!filePath.empty())
+    fileURL = [NSURL fileURLWithPath:base::SysUTF8ToNSString(filePath.value())];
+  if (!downloadCanceled_ && HasValidPdfAtUrl(fileURL)) {
+    [self presentOpenInMenuForFileAtURL:fileURL];
+    return;
+  }
+  sequencedTaskRunner_->PostTask(FROM_HERE, base::BindOnce(^{
+                                   [self removeDocumentAtPath:fileURL.path];
+                                 }));
+  if (!downloadCanceled_) {
     if (IsIPadIdiom())
       [self hideOpenInToolbar];
     [self removeOverlayedView];
