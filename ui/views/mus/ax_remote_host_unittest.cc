4affe2d87bfc2892d3190580735dfb9e7794db80
James Cook <jamescook@chromium.org>
2018-10-04 23:08:15 +0000

    chromeos: Support multiple AXRemoteHosts from remote processes
    This is a step toward supporting chrome browser windows under
    single-process and multi-process mash.
    
    Break AXHostService into two parts. The core AXHostService manages
    a map of AXRemoteHostDelegates. Each AXRemoteHostDelegate is-a
    ui::AXHostDelegate and therefore can handle PerformAction requests
    from accessibility extensions. AXHostService receives incoming
    AXEvents from remote apps and routes them to the correct
    AXRemoteHostDelegate, which forwards them to the extension.
    
    Integration tests already exist in SpokenFeedbackTest.
    
    Added unit tests.
    
    Bug: 888147
    Test: unit_tests, views_mus_unittests
    Change-Id: If2ee438b310acba9437a251bd77e06bfb7d9a099
    Reviewed-on: https://chromium-review.googlesource.com/c/1260205
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596906}

--- ui/views/mus/ax_remote_host_unittest.cc
+++ ui/views/mus/ax_remote_host_unittest.cc
@@ -45,7 +45,7 @@ class TestAXHostService : public ax::mojom::AXHost {
   }
 
   void ResetCounts() {
-    add_client_count_ = 0;
+    remote_host_count_ = 0;
     event_count_ = 0;
     last_tree_id_ = ui::AXTreeIDUnknown();
     last_updates_.clear();
@@ -53,9 +53,9 @@ class TestAXHostService : public ax::mojom::AXHost {
   }
 
   // ax::mojom::AXHost:
-  void SetRemoteHost(ax::mojom::AXRemoteHostPtr client,
-                     SetRemoteHostCallback cb) override {
-    ++add_client_count_;
+  void RegisterRemoteHost(ax::mojom::AXRemoteHostPtr client,
+                          RegisterRemoteHostCallback cb) override {
+    ++remote_host_count_;
     std::move(cb).Run(TestAXTreeID(), automation_enabled_);
     client.FlushForTesting();
   }
@@ -70,7 +70,7 @@ class TestAXHostService : public ax::mojom::AXHost {
 
   mojo::Binding<ax::mojom::AXHost> binding_{this};
   bool automation_enabled_ = false;
-  int add_client_count_ = 0;
+  int remote_host_count_ = 0;
   int event_count_ = 0;
   ui::AXTreeID last_tree_id_ = ui::AXTreeIDUnknown();
   std::vector<ui::AXTreeUpdate> last_updates_;
@@ -138,7 +138,7 @@ TEST_F(AXRemoteHostTest, CreateRemote) {
   CreateRemote(&service);
 
   // Client registered itself with service.
-  EXPECT_EQ(1, service.add_client_count_);
+  EXPECT_EQ(1, service.remote_host_count_);
 }
 
 TEST_F(AXRemoteHostTest, AutomationEnabled) {
