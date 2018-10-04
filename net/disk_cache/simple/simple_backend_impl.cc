e44f772fdf9e2d7a6bfb0127634a4d6862e67615
Maks Orlovich <morlovich@chromium.org>
2018-10-04 22:07:34 +0000

    SimpleCache: bump the individual resource limit on very small caches
    Android WebView limits cache to 20MiB as of now, which resulted, before
    this change, in a 2.5MiB size limit for individual resources... resulting in
    parallel fetches to those resource failing due to lack of a cache entry to
    back multiple readers.
    
    This bumps the limit to at least 5MiB to mitigate somewhat.
    
    Bug: 890973
    Change-Id: Ic6b587414c85d47dbc62411d508446e33cb5cd17
    Reviewed-on: https://chromium-review.googlesource.com/c/1262575
    Reviewed-by: Asanka Herath <asanka@chromium.org>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596878}

--- net/disk_cache/simple/simple_backend_impl.cc
+++ net/disk_cache/simple/simple_backend_impl.cc
@@ -62,6 +62,9 @@ namespace {
 // Maximum fraction of the cache that one entry can consume.
 const int kMaxFileRatio = 8;
 
+// Overrides the above.
+const int kMinFileSizeLimit = 5 * 1024 * 1024;
+
 bool g_fd_limit_histogram_has_been_populated = false;
 
 void MaybeHistogramFdLimit() {
@@ -300,7 +303,8 @@ bool SimpleBackendImpl::SetMaxSize(int64_t max_bytes) {
 }
 
 int SimpleBackendImpl::GetMaxFileSize() const {
-  return base::saturated_cast<int>(index_->max_size() / kMaxFileRatio);
+  return std::max(base::saturated_cast<int>(index_->max_size() / kMaxFileRatio),
+                  kMinFileSizeLimit);
 }
 
 void SimpleBackendImpl::OnDoomStart(uint64_t entry_hash) {
