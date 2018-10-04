79c3cad2bcf460ab303e8808349f0f692b2e8051
Anthony Vallee-Dubois <anthonyvd@chromium.org>
2018-10-04 15:05:28 +0000

    Log in UMA the languages added/removed in the Explicit Language Ask
    This CL adds 2 histograms that record languages added and removed from
    Accept Languages by the user from the Explicit Language Ask prompt during
    Android Chrome 2nd run.
    
    Bug: 891307
    Change-Id: I1b6e8feb7e3d56f77c0381ec5870eb9694085b74
    Reviewed-on: https://chromium-review.googlesource.com/c/1255575
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Michael Martis <martis@chromium.org>
    Commit-Queue: anthonyvd <anthonyvd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596669}

--- chrome/android/java/src/org/chromium/chrome/browser/language/LanguageAskPrompt.java
+++ chrome/android/java/src/org/chromium/chrome/browser/language/LanguageAskPrompt.java
@@ -24,6 +24,7 @@ import org.chromium.chrome.browser.modaldialog.ModalDialogManager;
 import org.chromium.chrome.browser.modaldialog.ModalDialogView;
 import org.chromium.chrome.browser.preferences.PrefServiceBridge;
 import org.chromium.chrome.browser.preferences.languages.LanguageItem;
+import org.chromium.components.language.AndroidLanguageMetricsBridge;
 import org.chromium.components.language.GeoLanguageProviderBridge;
 
 import java.util.ArrayList;
@@ -220,6 +221,7 @@ public class LanguageAskPrompt implements ModalDialogView.Controller {
 
         for (String language : languagesToAdd) {
             PrefServiceBridge.getInstance().updateUserAcceptLanguages(language, true);
+            AndroidLanguageMetricsBridge.reportExplicitLanguageAskStateChanged(language, true);
         }
 
         HashSet<String> languagesToRemove = new HashSet<String>(mInitialLanguages);
@@ -227,6 +229,7 @@ public class LanguageAskPrompt implements ModalDialogView.Controller {
 
         for (String language : languagesToRemove) {
             PrefServiceBridge.getInstance().updateUserAcceptLanguages(language, false);
+            AndroidLanguageMetricsBridge.reportExplicitLanguageAskStateChanged(language, false);
         }
     }
 
