7264bcf5faf33538fdcc32a4ff9702465b3862ab
John Chen <johnchen@chromium.org>
2018-10-04 15:56:35 +0000

    [ChromeDriver] W3C-compliant process capabilities
    Re-implement processing of capabilities based on W3C spec.
    
    spec: https://www.w3.org/TR/webdriver/#processing-capabilities
    
    Bug: chromedriver:1997
    Change-Id: I3304c54ba886b2319b178dad38a09713e349da25
    Reviewed-on: https://chromium-review.googlesource.com/c/1260506
    Commit-Queue: John Chen <johnchen@chromium.org>
    Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596689}

--- chrome/test/chromedriver/test/run_py_tests.py
+++ chrome/test/chromedriver/test/run_py_tests.py
@@ -2478,7 +2478,7 @@ class MobileEmulationCapabilityTest(ChromeDriverBaseTest):
     self.assertTrue('hello' in driver.GetPageSource())
 
   def testUnsupportedPageLoadStrategyRaisesException(self):
-    self.assertRaises(chromedriver.UnknownError,
+    self.assertRaises(chromedriver.InvalidArgument,
                       self.CreateDriver, page_load_strategy="unsupported")
 
   def testNetworkConnectionDisabledByDefault(self):
