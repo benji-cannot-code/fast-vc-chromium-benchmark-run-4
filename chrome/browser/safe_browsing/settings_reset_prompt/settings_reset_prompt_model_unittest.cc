5a7cb81e8ef597d7f836efa7ed7f08ad293c07be
Sylvain Defresne <sdefresne@chromium.org>
2018-10-04 15:20:39 +0000

    Convert indirect uses of TestingFactoryFunction
    TestingFactoryFunction is a simple function pointer. It is
    deprecated in favor of TestingFactory which is a Callback<>.
    Convert indirect uses by using base::BindRepeating() in all
    invocation of SetTestingFactory/AddTestingFactory.
    
    This converts uses in src//chrome/browser/safe_browsing/settings_reset_prompt.
    
    This CL was uploaded by git cl split.
    
    R=robertshield@chromium.org
    
    Bug: 809610
    Change-Id: Ia4da1523758d8091bad781d01026453d4085560f
    Reviewed-on: https://chromium-review.googlesource.com/c/1259008
    Reviewed-by: Robert Shield <robertshield@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596674}

--- chrome/browser/safe_browsing/settings_reset_prompt/settings_reset_prompt_model_unittest.cc
+++ chrome/browser/safe_browsing/settings_reset_prompt/settings_reset_prompt_model_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <unordered_set>
 #include <utility>
 
+#include "base/bind.h"
 #include "base/callback.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
@@ -94,7 +95,7 @@ class SettingsResetPromptModelTest
 
     profile_->CreateWebDataService();
     TemplateURLServiceFactory::GetInstance()->SetTestingFactory(
-        profile(), CreateTemplateURLServiceForTesting);
+        profile(), base::BindRepeating(&CreateTemplateURLServiceForTesting));
 
     SessionStartupPref::SetStartupPref(profile(), startup_pref_);
 
