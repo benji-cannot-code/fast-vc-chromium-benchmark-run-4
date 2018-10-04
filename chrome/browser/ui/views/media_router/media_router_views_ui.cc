f9f90ef581601f2e186cc8d9da73d94951975e7c
Takumi Fujimoto <takumif@chromium.org>
2018-10-04 22:59:11 +0000

    [Harmony Cast Dialog] Show presentation origin in dialog header
    When the presentation request is set, use its origin in the dialog
    header. E.g. "Cast youtube.com" instead of "Cast".
    
    If the user selects a non-tab source (desktop or local file), switch to
    using just "Cast".
    
    Bug: 851610
    Change-Id: I7c672e458fa82af0182e1d9ed20a7f922e3ad814
    Reviewed-on: https://chromium-review.googlesource.com/c/1259618
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596902}

--- chrome/browser/ui/views/media_router/media_router_views_ui.cc
+++ chrome/browser/ui/views/media_router/media_router_views_ui.cc
@@ -46,7 +46,8 @@ MediaRouterViewsUI::~MediaRouterViewsUI() {
 
 void MediaRouterViewsUI::AddObserver(CastDialogController::Observer* observer) {
   observers_.AddObserver(observer);
-  observer->OnModelUpdated(model_);
+  // TODO(takumif): Update the header when this object is initialized instead.
+  UpdateModelHeader();
 }
 
 void MediaRouterViewsUI::RemoveObserver(
@@ -109,8 +110,6 @@ void MediaRouterViewsUI::OnRoutesUpdated(
 }
 
 void MediaRouterViewsUI::UpdateSinks() {
-  model_.set_dialog_header(
-      l10n_util::GetStringUTF16(IDS_MEDIA_ROUTER_CAST_DIALOG_TITLE));
   std::vector<UIMediaSink> media_sinks;
   for (const MediaSinkWithCastModes& sink : GetEnabledSinks()) {
     auto route_it = std::find_if(
@@ -165,6 +164,31 @@ void MediaRouterViewsUI::OnIssue(const Issue& issue) {
 void MediaRouterViewsUI::OnIssueCleared() {
   issue_ = base::nullopt;
   UpdateSinks();
+}
+
+void MediaRouterViewsUI::OnDefaultPresentationChanged(
+    const content::PresentationRequest& presentation_request) {
+  // This sets the default cast mode to presentation when the dialog is opened.
+  // So we need to update the header to reflect that.
+  MediaRouterUIBase::OnDefaultPresentationChanged(presentation_request);
+  UpdateModelHeader();
+}
+
+void MediaRouterViewsUI::OnDefaultPresentationRemoved() {
+  MediaRouterUIBase::OnDefaultPresentationRemoved();
+  UpdateModelHeader();
+}
+
+void MediaRouterViewsUI::UpdateModelHeader() {
+  const std::string source_name = GetTruncatedPresentationRequestSourceName();
+  const base::string16 header_text =
+      source_name.empty()
+          ? l10n_util::GetStringUTF16(IDS_MEDIA_ROUTER_CAST_DIALOG_TITLE)
+          : l10n_util::GetStringFUTF16(IDS_MEDIA_ROUTER_PRESENTATION_CAST_MODE,
+                                       base::UTF8ToUTF16(source_name));
+  model_.set_dialog_header(header_text);
+  for (CastDialogController::Observer& observer : observers_)
+    observer.OnModelUpdated(model_);
 }
 
 void MediaRouterViewsUI::FileDialogFileSelected(
