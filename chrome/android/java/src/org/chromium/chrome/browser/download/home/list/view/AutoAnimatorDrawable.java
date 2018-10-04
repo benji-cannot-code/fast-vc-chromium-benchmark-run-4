1928c6960f6dab3aa32aad555e00bc49feeef12b
David Trainor <dtrainor@chromium.org>
2018-10-04 04:32:01 +0000

    Clean up the generic item holder for downloads home v2
    Address some issues with how the generic item holder was building
    
    - Fix AsyncImageView not properly showing the unavailable drawable
    (rebase issue).
    - Fix issue with progress.getPercentage() being called on an
    indeterminate progress state.
    - Make sure we properly show the grey circle around the generic icon.
    - Add a way to not wrap a drawable with an animator if there is nothing
    to animate.
    
    thumbnails: 
    Change-Id: I3bd15451af234e8291d886d4c3df64d696a41ac6
    Reviewed-on: https://chromium-review.googlesource.com/c/1258349
    Commit-Queue: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596501}

--- chrome/android/java/src/org/chromium/chrome/browser/download/home/list/view/AutoAnimatorDrawable.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/home/list/view/AutoAnimatorDrawable.java
@@ -19,6 +19,8 @@ import android.support.annotation.Nullable;
 import android.support.graphics.drawable.Animatable2Compat;
 import android.support.v7.graphics.drawable.DrawableWrapper;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 /**
  * A helper {@link Drawable} that wraps another {@link Drawable} and starts/stops any
  * {@link Animatable} {@link Drawable}s in the {@link Drawable} hierarchy when this {@link Drawable}
@@ -38,7 +40,7 @@ public class AutoAnimatorDrawable extends DrawableWrapper {
      *                 {@code drawable} is {@code null}.
      */
     public static Drawable wrap(@Nullable Drawable drawable) {
-        if (drawable == null) return null;
+        if (drawable == null || !shouldWrapDrawable(drawable)) return drawable;
         return new AutoAnimatorDrawable(drawable);
     }
 
@@ -69,6 +71,12 @@ public class AutoAnimatorDrawable extends DrawableWrapper {
 
     private static void stopAnimatedDrawables(@Nullable Drawable drawable) {
         AutoAnimatorDrawable.animatedDrawableHelper(drawable, animatable -> animatable.stop());
+    }
+
+    private static boolean shouldWrapDrawable(@Nullable Drawable drawable) {
+        AtomicBoolean found = new AtomicBoolean();
+        AutoAnimatorDrawable.animatedDrawableHelper(drawable, animatable -> found.set(true));
+        return found.get();
     }
 
     @TargetApi(Build.VERSION_CODES.M)
