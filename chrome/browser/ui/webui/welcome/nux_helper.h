dab0bc4f76f8fc26bd887c23831403adfb18cf4a
Greg Thompson <grt@chromium.org>
2018-10-04 07:36:23 +0000

    Revert "NUX Onboarding: clean up build process."
    This reverts commit 4ad50af377742356d0a99a3005553f87620661e8.
    
    Reason for revert: Breaks is_chrome_branded builds -- startup_tab_provider_unittest.cc is still referencing members of StandardOnboardingTabsParams that were removed.
    
    Original change's description:
    > NUX Onboarding: clean up build process.
    > 
    > This CL cleans up the remnants of the NUX Experiments bulid configs. Including:
    > - removing os_win and google_chrome_branded build flag.
    > - removing prefs that tracks if the one-off NUX experiments were seen.
    > - fixing closure compile errors now that it needs to build on non-official bots.
    > 
    > Bug: 874153
    > Change-Id: I03c1ed09c54f5ba91944bcb31837e80a0ee1fc74
    > Reviewed-on: https://chromium-review.googlesource.com/c/1252906
    > Commit-Queue: Scott Chen <scottchen@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#596432}
    
    TBR=sky@chromium.org,scottchen@chromium.org
    
    Change-Id: I643cfa84bb9da07b4e1ebf8a9481d0ac32aeea90
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 874153
    Reviewed-on: https://chromium-review.googlesource.com/c/1260128
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596535}

--- chrome/browser/ui/webui/welcome/nux_helper.h
+++ chrome/browser/ui/webui/welcome/nux_helper.h
@@ -7,12 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_WEBUI_WELCOME_NUX_HELPER_H_
 
 #include "base/macros.h"
-#include "chrome/browser/profiles/profile.h"
-
-class Profile;
 
 namespace nux {
-bool IsNuxOnboardingEnabled(Profile* profile);
-}  // namespace nux
+bool IsNuxOnboardingEnabled();
+};  // namespace nux
 
 #endif  // CHROME_BROWSER_UI_WEBUI_WELCOME_NUX_HELPER_H_
