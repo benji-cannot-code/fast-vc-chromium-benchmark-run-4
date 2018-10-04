e7f764a3c54e0109af0275a88c5a6003cf7a9556
Aaron Leventhal <aleventhal@chromium.org>
2018-10-04 05:10:22 +0000

    Useful result for IAText get_characterExtents() in empty textfield
    Products like ZoomText use get_characterExtents with the special IA2_TEXT_OFFSET_CARET to get the
    bounds of the character with the caret. Sometimes this results in getting the bounds for the
    null character at the end of an editable textfield's text. In this case, get_characterExtents()
    should return what the caret bounds would be if placed there.
    
    In order to make this work, we can use the bounds of the anonymous inner editor element within
    a textfield, and collapse it to the width of the caret. Normally this is exposed, but in the
    case of an empty textfield, we were ignoring it.
    
    This CL also adds a lot more tests for different kinds of empty fields. The plan is to move
    the character extents implementation into the ui/accessibility code so that the automation
    API support can leverage it.
    
    Bug: 869985
    
    Change-Id: I907a9df834f88aa9bf536e4dbbc9432e33d31202
    Reviewed-on: https://chromium-review.googlesource.com/c/1255203
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596513}

--- third_party/blink/renderer/modules/accessibility/ax_layout_object.h
+++ third_party/blink/renderer/modules/accessibility/ax_layout_object.h
@@ -237,6 +237,7 @@ class MODULES_EXPORT AXLayoutObject : public AXNodeObject {
   bool CanIgnoreTextAsEmpty() const;
   bool CanIgnoreSpaceNextTo(LayoutObject*, bool is_after) const;
   bool HasAriaCellRole(Element*) const;
+  bool IsPlaceholder() const;
 
   bool is_autofill_available_;
 
