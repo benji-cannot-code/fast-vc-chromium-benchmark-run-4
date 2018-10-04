774e7e3d59ca76b2b4d92930f595a298d622ba5b
W. James MacLean <wjmaclean@chromium.org>
2018-10-04 20:20:50 +0000

    Convert coords per touch point with full transforms.
    It has long been known that the translation-only model for transforming
    point coordinates of Touch and TouchscreenGesture events in
    RenderWidgetHostInputEventRouter is insufficient in the case of non-
    trivial CSS transformations and OOPIFs; specifically when scale and/or
    rotation is involved.
    
    This CL fixes that by using full transforms when they are available via
    Viz hit testing.
    
    Bug: 883508
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I53c163b0c942f0eb62d5bee76245003c0f032cfc
    Reviewed-on: https://chromium-review.googlesource.com/c/1224453
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: James MacLean <wjmaclean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596826}

--- content/browser/renderer_host/render_widget_host_input_event_router.h
+++ content/browser/renderer_host/render_widget_host_input_event_router.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/content_export.h"
 #include "content/public/common/input_event_ack_state.h"
 #include "ui/gfx/geometry/vector2d_conversions.h"
+#include "ui/gfx/transform.h"
 
 struct FrameHostMsg_HittestData_Params;
 
@@ -165,6 +166,7 @@ class CONTENT_EXPORT RenderWidgetHostInputEventRouter
   struct TargetData {
     RenderWidgetHostViewBase* target;
     gfx::Vector2dF delta;
+    gfx::Transform transform;
 
     TargetData() : target(nullptr) {}
   };
