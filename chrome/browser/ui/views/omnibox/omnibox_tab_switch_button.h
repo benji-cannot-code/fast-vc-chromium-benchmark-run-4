dfe75aa512f8241607e4c298ec7d3dc241e2d7f0
Orin Jaworski <orinj@chromium.org>
2018-10-04 22:54:44 +0000

    Add new omnibox_pedal* code and related changes for a single Pedal
    This CL adds a few new .h and .cc files for upcoming omnibox Pedals
    prototype.  None of this code is used yet, but changes necessary
    to ensure that one Pedal can be used are included so it compiles.
    
    Change-Id: If3d03e1fd532fdace55bde941ab1d5597748a168
    Reviewed-on: https://chromium-review.googlesource.com/c/1247223
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596898}

--- chrome/browser/ui/views/omnibox/omnibox_tab_switch_button.h
+++ chrome/browser/ui/views/omnibox/omnibox_tab_switch_button.h
@@ -19,8 +19,8 @@ class OmniboxTabSwitchButton : public views::MdTextButton {
  public:
   OmniboxTabSwitchButton(OmniboxPopupContentsView* model,
                          OmniboxResultView* result_view,
-                         int ids_hint,
-                         int ids_hint_short,
+                         const base::string16& hint,
+                         const base::string16& hint_short,
                          const gfx::VectorIcon& icon);
 
   ~OmniboxTabSwitchButton() override;
@@ -85,9 +85,9 @@ class OmniboxTabSwitchButton : public views::MdTextButton {
   base::string16 goal_text_;
   std::unique_ptr<gfx::SlideAnimation> animation_;
 
-  // String identifiers for hint text and its short version (may be same).
-  int ids_hint_;
-  int ids_hint_short_;
+  // Label strings for hint text and its short version (may be same).
+  base::string16 hint_;
+  base::string16 hint_short_;
 
   DISALLOW_COPY_AND_ASSIGN(OmniboxTabSwitchButton);
 };
---
+++ components/omnibox/browser/omnibox_pedal_provider_unittest.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/omnibox/browser/omnibox_pedal_provider.h"
+
+#include "base/strings/utf_string_conversions.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+class OmniboxPedalProviderTest : public testing::Test {
+ protected:
+  OmniboxPedalProviderTest() {}
+};
+
+TEST_F(OmniboxPedalProviderTest, QueriesTriggerPedals) {
+  OmniboxPedalProvider provider;
+  EXPECT_EQ(provider.FindPedalMatch(base::ASCIIToUTF16("")), nullptr);
+  EXPECT_EQ(provider.FindPedalMatch(base::ASCIIToUTF16("clear histor")),
+            nullptr);
+  EXPECT_NE(provider.FindPedalMatch(base::ASCIIToUTF16("clear history")),
+            nullptr);
+}
