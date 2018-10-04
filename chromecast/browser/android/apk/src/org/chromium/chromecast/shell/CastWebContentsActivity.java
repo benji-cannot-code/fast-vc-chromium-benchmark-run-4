a0a9f328fcb6c9dcb775d44ba0344e018d7e4e23
Thoren Paulson <thoren@chromium.org>
2018-10-04 19:06:30 +0000

    [Chromecast] Destroy SurfaceHelper when finishing.
    This change puts SurfaceHelper in a Controller to make sure it gets
    destroyed at the right times.
    
    Bug: internal b/117112379
    Test: junit
    Change-Id: I04399128e657862952daa9bcc7b7d9bef293e83f
    Reviewed-on: https://chromium-review.googlesource.com/c/1256215
    Reviewed-by: Simeon Anfinrud <sanfin@chromium.org>
    Commit-Queue: Thoren Paulson <thoren@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596789}

--- chromecast/browser/android/apk/src/org/chromium/chromecast/shell/CastWebContentsActivity.java
+++ chromecast/browser/android/apk/src/org/chromium/chromecast/shell/CastWebContentsActivity.java
@@ -58,6 +58,8 @@ public class CastWebContentsActivity extends Activity {
     private final Controller<CastAudioManager> mAudioManagerState = new Controller<>();
     // Set in unittests to skip some behavior.
     private final Controller<Unit> mIsTestingState = new Controller<>();
+    // Set at creation. Handles destroying SurfaceHelper.
+    private final Controller<CastWebContentsSurfaceHelper> mSurfaceHelperState = new Controller<>();
 
     private CastWebContentsSurfaceHelper mSurfaceHelper;
 
@@ -85,13 +87,23 @@ public class CastWebContentsActivity extends Activity {
 
             setContentView(R.layout.cast_web_contents_activity);
 
-            mSurfaceHelper = new CastWebContentsSurfaceHelper(this, /* hostActivity */
+            mSurfaceHelperState.set(new CastWebContentsSurfaceHelper(this /* hostActivity */,
                     CastWebContentsView.onLayoutActivity(this,
                             (FrameLayout) findViewById(R.id.web_contents_container),
                             CastSwitches.getSwitchValueColor(
                                     CastSwitches.CAST_APP_BACKGROUND_COLOR, Color.BLACK)),
-                    (Uri uri) -> mIsFinishingState.set("Delayed teardown for URI: " + uri));
+                    (Uri uri) -> mIsFinishingState.set("Delayed teardown for URI: " + uri)));
         }));
+
+        mSurfaceHelperState.subscribe((CastWebContentsSurfaceHelper surfaceHelper) -> {
+            mSurfaceHelper = surfaceHelper;
+            return () -> {
+                mSurfaceHelper.onDestroy();
+                mSurfaceHelper = null;
+            };
+        });
+
+        mCreatedState.subscribe(Observers.onExit(x -> mSurfaceHelperState.reset()));
 
         mCreatedState.map(x -> getWindow())
                 .and(mGotIntentState)
@@ -128,6 +140,7 @@ public class CastWebContentsActivity extends Activity {
 
         mIsFinishingState.subscribe(Observers.onEnter((String reason) -> {
             if (DEBUG) Log.d(TAG, "Finishing activity: " + reason);
+            mSurfaceHelperState.reset();
             finish();
         }));
 
@@ -195,9 +208,6 @@ public class CastWebContentsActivity extends Activity {
     @Override
     protected void onDestroy() {
         if (DEBUG) Log.d(TAG, "onDestroy");
-        if (mSurfaceHelper != null) {
-            mSurfaceHelper.onDestroy();
-        }
         mCreatedState.reset();
         super.onDestroy();
     }
@@ -294,6 +304,6 @@ public class CastWebContentsActivity extends Activity {
 
     @RemovableInRelease
     public void setSurfaceHelperForTesting(CastWebContentsSurfaceHelper surfaceHelper) {
-        mSurfaceHelper = surfaceHelper;
+        mSurfaceHelperState.set(surfaceHelper);
     }
 }
