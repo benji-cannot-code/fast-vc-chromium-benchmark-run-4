39bf90f23266db1b10c265c040d4c772ca1cbb6f
A Olsen <olsen@chromium.org>
2018-10-04 09:45:13 +0000

    Move system_settings_provider into chromeos/system
    Moves system_settings_provider from chrome/browser/chromeos/system to
    chromeos/system - part of an effort to move cros_settings and related
    settings code out of chrome/browser and into chromeos/system, so that
    they can be used in processes other than the chrome process.
    
    Duplicates the timezone_util functions that system_settings_provider
    depends on - ideally, all of timezone_util would move too but since
    it depends on cros_settings, it might have to be moved last.
    
    Bug: 446937
    Change-Id: I3863cd3dd274b0f94dc4d87938b7ccda2fe89269
    Reviewed-on: https://chromium-review.googlesource.com/c/1128861
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Lutz Justen <ljusten@chromium.org>
    Commit-Queue: A Olsen <olsen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596559}

--- chromeos/settings/system_settings_provider.h
+++ chromeos/settings/system_settings_provider.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_CHROMEOS_SETTINGS_SYSTEM_SETTINGS_PROVIDER_H_
-#define CHROME_BROWSER_CHROMEOS_SETTINGS_SYSTEM_SETTINGS_PROVIDER_H_
+#ifndef CHROMEOS_SETTINGS_SYSTEM_SETTINGS_PROVIDER_H_
+#define CHROMEOS_SETTINGS_SYSTEM_SETTINGS_PROVIDER_H_
 
 #include <memory>
 #include <string>
@@ -21,8 +21,9 @@ class Value;
 
 namespace chromeos {
 
-class SystemSettingsProvider : public CrosSettingsProvider,
-                               public system::TimezoneSettings::Observer {
+class CHROMEOS_EXPORT SystemSettingsProvider
+    : public CrosSettingsProvider,
+      public system::TimezoneSettings::Observer {
  public:
   explicit SystemSettingsProvider(const NotifyObserversCallback& notify_cb);
   ~SystemSettingsProvider() override;
@@ -48,4 +49,4 @@ class SystemSettingsProvider : public CrosSettingsProvider,
 
 }  // namespace chromeos
 
-#endif  // CHROME_BROWSER_CHROMEOS_SETTINGS_SYSTEM_SETTINGS_PROVIDER_H_
+#endif  // CHROMEOS_SETTINGS_SYSTEM_SETTINGS_PROVIDER_H_
