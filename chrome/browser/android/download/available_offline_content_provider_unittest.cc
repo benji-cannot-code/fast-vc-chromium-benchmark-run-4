d3f50ba84200d7880d441b70517bbb9bba191fce
Sylvain Defresne <sdefresne@chromium.org>
2018-10-04 12:02:11 +0000

    Convert indirect uses of TestingFactoryFunction
    TestingFactoryFunction is a simple function pointer. It is
    deprecated in favor of TestingFactory which is a Callback<>.
    Convert indirect uses by using base::BindRepeating() in all
    invocation of SetTestingFactory/AddTestingFactory.
    
    This converts uses in src//chrome/browser/android/download.
    
    This CL was uploaded by git cl split.
    
    R=xingliu@chromium.org
    
    Bug: 809610
    Change-Id: I196b778462b96eebc23acfe59de4a293aab0496e
    Reviewed-on: https://chromium-review.googlesource.com/c/1257837
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596609}

--- chrome/browser/android/download/available_offline_content_provider_unittest.cc
+++ chrome/browser/android/download/available_offline_content_provider_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/android/download/available_offline_content_provider.h"
 
+#include "base/bind.h"
 #include "base/strings/string_util.h"
 #include "base/test/bind_test_util.h"
 #include "base/test/scoped_feature_list.h"
@@ -98,7 +99,7 @@ class AvailableOfflineContentTest : public testing::Test {
     // single MockOfflineContentProvider.
     aggregator_ = static_cast<OfflineContentAggregator*>(
         OfflineContentAggregatorFactory::GetInstance()->SetTestingFactoryAndUse(
-            &profile_, &BuildOfflineContentAggregator));
+            &profile_, base::BindRepeating(&BuildOfflineContentAggregator)));
     aggregator_->RegisterProvider(kProviderNamespace, &content_provider_);
     content_provider_.SetVisuals({});
   }
