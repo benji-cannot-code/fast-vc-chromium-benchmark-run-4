c493ca229f302c23a5a6ed91873655f055b78726
Becky Zhou <huayinz@chromium.org>
2018-10-04 23:55:41 +0000

    [Feed] Fix show/hide articles thumbnail flickers
    Bug: 889233
    Change-Id: I15614c6eef636f4e7b4dd0f204ee4155d3246cfa
    Reviewed-on: https://chromium-review.googlesource.com/c/1262642
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Becky Zhou <huayinz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596930}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedNewTabPageMediator.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedNewTabPageMediator.java
@@ -179,6 +179,7 @@ class FeedNewTabPageMediator
         if (mSignInPromo != null) {
             mSignInPromo.setCanShowPersonalizedSuggestions(suggestionsVisible);
         }
+        mStreamContentChanged = true;
     }
 
     /**
