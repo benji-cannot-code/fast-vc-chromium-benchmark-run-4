22b288391348ad5061015fd80e405e2e8b49d0f6
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-10-04 10:58:15 +0000

    Import wpt@685c5052e8b1fdc0170eb56770c85594508c685d
    Using wpt-import in Chromium 72758d92461e238aa314c1ffb6219eab17daf5ae.
    With Chromium commits locally applied on WPT:
    92605c16c6 "[Unified Plan] Remote MediaStreamTracks should be muted by default."
    65d72632bf "Make DTMF tone change more deterministic"
    
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/25792
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    TBR=markdittmer
    
    No-Export: true
    Change-Id: Id7eca3852ec010a1c60b0f2df1b56468664b8d09
    Reviewed-on: https://chromium-review.googlesource.com/c/1260925
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596581}

---
+++ third_party/WebKit/LayoutTests/external/wpt/html/editing/dnd/resources/32mb.py
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+thirty_two_megabytes = 32 * 1024 * 1024
+chunk = 'ab' * 512 * 512
+chunk_length = len(chunk)
+
+def main(request, response):
+    def content():
+        bytes_sent = 0
+        while bytes_sent < thirty_two_megabytes:
+            yield chunk
+            bytes_sent += chunk_length
+
+    return [("Content-Type", "text/plain")], content()
