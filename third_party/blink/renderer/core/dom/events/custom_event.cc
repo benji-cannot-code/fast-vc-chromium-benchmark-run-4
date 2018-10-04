8075c69e39d5da791b30a0fe1f4ab932c59b9c6c
Haeun Kim <ggrace.kim93@gmail.com>
2018-10-04 19:13:13 +0000

    Events: CustomEvent.detail should use ScriptValue::V8ValueFor
    Storing details as TraceWrapperV8Reference is not good.
    We should use ScriptValue and return it using V8ValueFor().
    
    Bug: 891598
    Change-Id: I7ff310ed6fda629ed316e99e8ac13fd10b5aed53
    Reviewed-on: https://chromium-review.googlesource.com/c/1257472
    Reviewed-by: Jinho Bang <jinho.bang@samsung.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Jinho Bang <jinho.bang@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#596795}

--- third_party/blink/renderer/core/dom/events/custom_event.cc
+++ third_party/blink/renderer/core/dom/events/custom_event.cc
@@ -26,8 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/dom/events/custom_event.h"
 
-#include "third_party/blink/renderer/bindings/core/v8/serialization/serialized_script_value.h"
-#include "third_party/blink/renderer/bindings/core/v8/serialization/serialized_script_value_factory.h"
 #include "third_party/blink/renderer/core/event_names.h"
 
 namespace blink {
@@ -37,12 +35,9 @@ CustomEvent::CustomEvent() = default;
 CustomEvent::CustomEvent(ScriptState* script_state,
                          const AtomicString& type,
                          const CustomEventInit& initializer)
-    : Event(type, initializer) {
-  world_ = WrapRefCounted(&script_state->World());
-  if (initializer.hasDetail()) {
-    detail_.Set(initializer.detail().GetIsolate(),
-                initializer.detail().V8Value());
-  }
+    : Event(type, initializer),
+      detail_(initializer.hasDetail() ? initializer.detail()
+                                      : ScriptValue::CreateNull(script_state)) {
 }
 
 CustomEvent::~CustomEvent() = default;
@@ -53,23 +48,14 @@ void CustomEvent::initCustomEvent(ScriptState* script_state,
                                   bool cancelable,
                                   const ScriptValue& script_value) {
   initEvent(type, bubbles, cancelable);
-  world_ = WrapRefCounted(&script_state->World());
   if (!IsBeingDispatched() && !script_value.IsEmpty())
-    detail_.Set(script_value.GetIsolate(), script_value.V8Value());
+    detail_ = script_value;
+  else
+    detail_ = ScriptValue::CreateNull(script_state);
 }
 
 ScriptValue CustomEvent::detail(ScriptState* script_state) const {
-  v8::Isolate* isolate = script_state->GetIsolate();
-  if (detail_.IsEmpty())
-    return ScriptValue(script_state, v8::Null(isolate));
-  // Returns a clone of |detail_| if the world is different.
-  if (!world_ || world_->GetWorldId() != script_state->World().GetWorldId()) {
-    v8::Local<v8::Value> value = detail_.NewLocal(isolate);
-    scoped_refptr<SerializedScriptValue> serialized =
-        SerializedScriptValue::SerializeAndSwallowExceptions(isolate, value);
-    return ScriptValue(script_state, serialized->Deserialize(isolate));
-  }
-  return ScriptValue(script_state, detail_.NewLocal(isolate));
+  return ScriptValue(script_state, detail_.V8ValueFor(script_state));
 }
 
 const AtomicString& CustomEvent::InterfaceName() const {
@@ -77,7 +63,6 @@ const AtomicString& CustomEvent::InterfaceName() const {
 }
 
 void CustomEvent::Trace(blink::Visitor* visitor) {
-  visitor->Trace(detail_);
   Event::Trace(visitor);
 }
 
