dbf8a5a455fe1e0044997686b90bdae079a41e4f
Steven Bennetts <stevenjb@chromium.org>
2018-10-04 21:03:20 +0000

    keyboard_util: Remove flag/switch wrappers (Take 2)
    Original CL: https://chromium-review.googlesource.com/1226191
    
    This CL moves some common flag/switch wrappers into the callers to
    remove unnecessary dependencies on keyboard_util.h. With the upcoming
    Chrome/Ash separation, the wrappers cause more confusion than they
    improve readability.
    
    This CL also moves some keyboard_util.cc code only used in
    KeyboardController to that class.
    
    For minor changes to input_ime_api_chromeos.cc, approved in Take 1:
    TBR=shuchen@chromium.org
    
    Bug: 843332
    Change-Id: I4e6ee790190dbf10cd64a8d3a18fbeca982fce07
    Reviewed-on: https://chromium-review.googlesource.com/c/1259478
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596849}

--- chrome/browser/extensions/api/virtual_keyboard_private/chrome_virtual_keyboard_delegate.cc
+++ chrome/browser/extensions/api/virtual_keyboard_private/chrome_virtual_keyboard_delegate.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "base/bind.h"
 #include "base/command_line.h"
+#include "base/feature_list.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/metrics/user_metrics.h"
 #include "base/metrics/user_metrics_action.h"
@@ -22,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/ash/chrome_keyboard_controller_client.h"
 #include "chrome/browser/ui/chrome_pages.h"
 #include "chrome/common/url_constants.h"
+#include "chromeos/chromeos_features.h"
 #include "components/user_manager/user_manager.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/common/service_manager_connection.h"
@@ -32,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/audio/public/cpp/audio_system_factory.h"
 #include "services/service_manager/public/cpp/connector.h"
 #include "ui/aura/window_tree_host.h"
+#include "ui/base/ui_base_features.h"
 #include "ui/keyboard/keyboard_controller.h"
 #include "ui/keyboard/keyboard_switches.h"
 #include "ui/keyboard/keyboard_util.h"
@@ -271,18 +274,29 @@ void ChromeVirtualKeyboardDelegate::OnHasInputDevices(
   // TODO(blakeo): once the old flag's usages have been removed from the
   // extension and all pushes have settled, remove this overly verbose comment.
   features->AppendString(GenerateFeatureFlag(
-      "floatingkeyboard", keyboard::IsFloatingVirtualKeyboardEnabled()));
-  features->AppendString(
-      GenerateFeatureFlag("gesturetyping", keyboard::IsGestureTypingEnabled()));
+      "floatingkeyboard",
+      base::FeatureList::IsEnabled(features::kEnableFloatingVirtualKeyboard)));
   features->AppendString(GenerateFeatureFlag(
-      "gestureediting", keyboard::IsGestureEditingEnabled()));
+      "gesturetyping", !base::CommandLine::ForCurrentProcess()->HasSwitch(
+                           keyboard::switches::kDisableGestureTyping)));
+  // TODO(shend): Gesture editing is not implemented in the MD UI.
+  // https://crbug.com/890134.
+  bool enable_gesture_editing =
+      !base::FeatureList::IsEnabled(features::kEnableVirtualKeyboardMdUi) &&
+      !base::CommandLine::ForCurrentProcess()->HasSwitch(
+          keyboard::switches::kDisableGestureEditing);
+  features->AppendString(
+      GenerateFeatureFlag("gestureediting", enable_gesture_editing));
   features->AppendString(GenerateFeatureFlag(
       "fullscreenhandwriting",
-      keyboard::IsFullscreenHandwritingVirtualKeyboardEnabled()));
+      base::FeatureList::IsEnabled(
+          features::kEnableFullscreenHandwritingVirtualKeyboard)));
   features->AppendString(GenerateFeatureFlag(
-      "virtualkeyboardmdui", keyboard::IsVirtualKeyboardMdUiEnabled()));
-  features->AppendString(
-      GenerateFeatureFlag("imeservice", keyboard::IsImeServiceEnabled()));
+      "virtualkeyboardmdui",
+      base::FeatureList::IsEnabled(features::kEnableVirtualKeyboardMdUi)));
+  features->AppendString(GenerateFeatureFlag(
+      "imeservice", base::FeatureList::IsEnabled(
+                        chromeos::features::kImeServiceConnectable)));
 
   auto config = ChromeKeyboardControllerClient::Get()->GetKeyboardConfig();
   // TODO(oka): Change this to use config.voice_input.
--- chrome/browser/ui/ash/chrome_keyboard_ui.cc
+++ chrome/browser/ui/ash/chrome_keyboard_ui.cc
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/root_window_controller.h"
 #include "ash/shell.h"
+#include "base/command_line.h"
+#include "base/feature_list.h"
 #include "base/macros.h"
 #include "base/no_destructor.h"
 #include "chrome/browser/ui/ash/chrome_keyboard_controller_client.h"
@@ -29,12 +31,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/ime/ime_bridge.h"
 #include "ui/base/ime/input_method.h"
 #include "ui/base/ime/text_input_client.h"
+#include "ui/base/ui_base_features.h"
 #include "ui/compositor/layer.h"
 #include "ui/compositor_extra/shadow.h"
 #include "ui/gfx/geometry/insets.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/keyboard/keyboard_controller.h"
 #include "ui/keyboard/keyboard_resource_util.h"
+#include "ui/keyboard/keyboard_switches.h"
 #include "ui/keyboard/keyboard_util.h"
 #include "ui/wm/core/shadow_types.h"
 
@@ -146,8 +150,9 @@ aura::Window* ChromeKeyboardUI::GetKeyboardWindow() {
   // keyboard to be see-through.
   // TODO(https://crbug.com/840731): Find a permanent fix for this on the
   // keyboard extension side.
-  if (keyboard::IsFullscreenHandwritingVirtualKeyboardEnabled() ||
-      keyboard::IsVirtualKeyboardMdUiEnabled()) {
+  if (base::FeatureList::IsEnabled(
+          features::kEnableFullscreenHandwritingVirtualKeyboard) ||
+      base::FeatureList::IsEnabled(features::kEnableVirtualKeyboardMdUi)) {
     view->SetBackgroundColor(SK_ColorTRANSPARENT);
     view->GetNativeView()->SetTransparent(true);
   }
@@ -251,8 +256,10 @@ GURL ChromeKeyboardUI::GetVirtualKeyboardUrl() {
   if (!override_url.is_empty())
     return override_url;
 
-  if (!keyboard::IsInputViewEnabled())
+  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
+          keyboard::switches::kDisableInputView)) {
     return GURL(keyboard::kKeyboardURL);
+  }
 
   chromeos::input_method::InputMethodManager* ime_manager =
       chromeos::input_method::InputMethodManager::Get();
