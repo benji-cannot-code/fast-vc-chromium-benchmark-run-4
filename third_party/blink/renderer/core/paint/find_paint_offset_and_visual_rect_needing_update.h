3f038441b660b7bc5c00657969bebd418307a93e
Xianzhu Wang <wangxianzhu@chromium.org>
2018-10-04 23:10:08 +0000

    [CI] Improve FindPaintOffsetNeedingUpdateScope and FindPropertyNeedingUpdateScope
    - Remove the last usage from FindPaintOffsetNeedingUpdate of
      TransformPaintPropertyNode::Clone() and operator==(), so we
      can remove them. This will make it easy to add move-only data
      in TransformPaintPropertyNode::State.
    
    - Now FindPaintOffsetNeedingUpdateScope is also detecting unexpected
      change of PaintOffsetTranslation during UpdateForSelf(), and unexpected
      change of paint offset and PaintOffsetTranslation during
      UpdateForChildren(). Previously it detected unexpected change of
      paint offset during UpdateForObjectLocationAndSize() only.
    
    - Now FindPropertyNeedingUpdateScope is also detecting unexpected
      change of FragmentClip. It no longer detects unexpected change of
      paint offset which is now detected by FindPaintOffsetNeedingUpdateScope.
    
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Id78fa30f51445698d3f93e26c8b7852e3811bedb
    Reviewed-on: https://chromium-review.googlesource.com/c/1260124
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596909}

--- third_party/blink/renderer/core/paint/find_paint_offset_and_visual_rect_needing_update.h
+++ third_party/blink/renderer/core/paint/find_paint_offset_and_visual_rect_needing_update.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if DCHECK_IS_ON()
 
 #include "third_party/blink/renderer/core/layout/layout_object.h"
-#include "third_party/blink/renderer/core/paint/find_properties_needing_update.h"
 #include "third_party/blink/renderer/core/paint/object_paint_invalidator.h"
 #include "third_party/blink/renderer/core/paint/paint_invalidator.h"
 #include "third_party/blink/renderer/core/paint/paint_layer.h"
@@ -33,10 +32,11 @@ class FindPaintOffsetNeedingUpdateScope {
         fragment_data_(fragment_data),
         is_actually_needed_(is_actually_needed),
         old_paint_offset_(fragment_data.PaintOffset()) {
-    auto* properties = fragment_data.PaintProperties();
-    if (properties && properties->PaintOffsetTranslation()) {
-      old_paint_offset_translation_ =
-          properties->PaintOffsetTranslation()->Clone();
+    if (const auto* properties = fragment_data.PaintProperties()) {
+      if (const auto* translation = properties->PaintOffsetTranslation()) {
+        old_parent_ = translation->Parent();
+        old_translation_ = translation->Matrix();
+      }
     }
   }
 
@@ -44,12 +44,20 @@ class FindPaintOffsetNeedingUpdateScope {
     if (is_actually_needed_)
       return;
     LayoutPoint paint_offset = fragment_data_.PaintOffset();
-    DCHECK_OBJECT_PROPERTY_EQ(object_, &old_paint_offset_, &paint_offset);
-    auto* paint_properties = fragment_data_.PaintProperties();
-    const auto* paint_offset_translation =
-        paint_properties ? paint_properties->PaintOffsetTranslation() : nullptr;
-    DCHECK_OBJECT_PROPERTY_EQ(object_, old_paint_offset_translation_.get(),
-                              paint_offset_translation);
+    DCHECK_EQ(old_paint_offset_, paint_offset) << object_.DebugName();
+
+    const TransformPaintPropertyNode* new_parent = nullptr;
+    base::Optional<TransformationMatrix> new_translation;
+    if (const auto* properties = fragment_data_.PaintProperties()) {
+      if (const auto* translation = properties->PaintOffsetTranslation()) {
+        new_parent = translation->Parent();
+        new_translation = translation->Matrix();
+      }
+    }
+    DCHECK_EQ(!!old_translation_, !!new_translation) << object_.DebugName();
+    DCHECK_EQ(old_parent_, new_parent) << object_.DebugName();
+    if (old_translation_ && new_translation)
+      DCHECK_EQ(*old_translation_, *new_translation) << object_.DebugName();
   }
 
  private:
@@ -57,7 +65,8 @@ class FindPaintOffsetNeedingUpdateScope {
   const FragmentData& fragment_data_;
   const bool& is_actually_needed_;
   LayoutPoint old_paint_offset_;
-  scoped_refptr<TransformPaintPropertyNode> old_paint_offset_translation_;
+  const TransformPaintPropertyNode* old_parent_ = nullptr;
+  base::Optional<TransformationMatrix> old_translation_;
 };
 
 class FindVisualRectNeedingUpdateScopeBase {
