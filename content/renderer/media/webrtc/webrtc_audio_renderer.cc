f6ee6e172473837add6afc6bb364ab105a5cd349
henrika <henrika@chromium.org>
2018-10-05 09:59:46 +0000

    Removes old Windows dependency from WebRtcAudioRenderer
    Bug: NONE
    Change-Id: I596b793a81a4e4d29e05d7459b273028a96c4a99
    Reviewed-on: https://chromium-review.googlesource.com/c/1264380
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597059}

--- content/renderer/media/webrtc/webrtc_audio_renderer.cc
+++ content/renderer/media/webrtc/webrtc_audio_renderer.cc
@@ -27,11 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/web_media_stream_track.h"
 #include "third_party/webrtc/api/mediastreaminterface.h"
 
-#if defined(OS_WIN)
-#include "base/win/windows_version.h"
-#include "media/audio/win/core_audio_util_win.h"
-#endif
-
 namespace content {
 
 namespace {
