fed6b7c5e8d9ccb786c092126dc03f887e8b0f11
Noel Gordon <noel@chromium.org>
2018-10-05 08:05:35 +0000

    Add Quick View video load test
    Now we have the QuickView <webview> infra to detect the 'loaded' state
    of thew <webview> content, add a <file-safe-media> video test and also
    check the <webview> renders its content on a transparent black body.
    
    Bug: 891150
    Change-Id: I634a60ba733bfff79ed72b0e75d7024da4c8182c
    Reviewed-on: https://chromium-review.googlesource.com/c/1264097
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597028}

--- chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
@@ -388,6 +388,7 @@ WRAPPED_INSTANTIATE_TEST_CASE_P(
                       TestCase("openQuickView").InGuestMode(),
                       TestCase("openQuickView").TabletMode(),
                       TestCase("openQuickViewImage"),
+                      TestCase("openQuickViewVideo"),
                       TestCase("openQuickViewScrollText"),
                       TestCase("openQuickViewScrollHtml"),
                       TestCase("openQuickViewBackgroundColorText"),
