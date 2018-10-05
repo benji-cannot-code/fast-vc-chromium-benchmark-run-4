ca183eb91b5e596d7c64d5cc4cd0ad738aa0cdd6
Ken Rockot <rockot@chromium.org>
2018-10-05 21:54:25 +0000

    [service-manager] Introduce ServiceBinding
    Adds a new ServiceBinding helper to the Service Manager's client
    library. This is intended as a replacement for ServiceContext, because
    ServiceContext is cumbersome in its insistence to own the
    corresponding Service object.
    
    ServiceBinding, like ServiceContext, deals with the IPC interface
    between a local Service instance and the Service Manager, exposing
    interesting events to the Service implementation while also providing
    it access to its Connector and assigned Identity.
    
    Some particularly gnarly tests have been updated to use ServiceBinding
    in lieu of ServiceContext. Furthermore, without ServiceContext and with
    a lot of its legacy motivations gone, ServiceRunner isn't terribly
    useful either. The tests have also been trivially updated to manage
    their own MessageLoop instead of using ServiceRunner.
    
    This is the first in a series of patches which aim to completely
    eliminate ServiceContext, ForwardingService (in service.h), and
    ServiceRunner, requiring Service implementations to write a little more
    boilerplate in exchange for flexibility with ownership semantics.
    
    Bug: 891780
    Change-Id: Ib0c1d3263f34a5e33a167a8187edad1d52413f73
    Reviewed-on: https://chromium-review.googlesource.com/c/1259485
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Ken Rockot <rockot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597324}

--- services/service_manager/tests/lifecycle/app_client.h
+++ services/service_manager/tests/lifecycle/app_client.h
@@ -9,11 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/bind.h"
+#include "base/callback.h"
 #include "base/macros.h"
 #include "mojo/public/cpp/bindings/binding_set.h"
 #include "services/service_manager/public/cpp/binder_registry.h"
 #include "services/service_manager/public/cpp/service.h"
-#include "services/service_manager/public/cpp/service_runner.h"
+#include "services/service_manager/public/cpp/service_binding.h"
 #include "services/service_manager/public/mojom/service.mojom.h"
 #include "services/service_manager/tests/lifecycle/lifecycle_unittest.mojom.h"
 
@@ -23,16 +24,15 @@ namespace test {
 class AppClient : public Service,
                   public mojom::LifecycleControl {
  public:
-  AppClient();
+  explicit AppClient(service_manager::mojom::ServiceRequest request,
+                     base::OnceClosure quit_closure);
   ~AppClient() override;
-
-  void set_runner(ServiceRunner* runner) { runner_ = runner; }
 
   // Service:
   void OnBindInterface(const BindSourceInfo& source_info,
                        const std::string& interface_name,
                        mojo::ScopedMessagePipeHandle interface_pipe) override;
-  bool OnServiceManagerConnectionLost() override;
+  void OnDisconnected() override;
 
   void Create(mojom::LifecycleControlRequest request);
 
@@ -45,9 +45,11 @@ class AppClient : public Service,
  private:
   class ServiceImpl;
 
-  void BindingLost();
+  void LifecycleControlBindingLost();
 
-  ServiceRunner* runner_ = nullptr;
+  ServiceBinding service_binding_;
+  base::OnceClosure quit_closure_;
+
   BinderRegistry registry_;
   mojo::BindingSet<mojom::LifecycleControl> bindings_;
 
