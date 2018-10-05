d213066e3e190ced8c22186a345027c8f630af67
Christopher Cameron <ccameron@chromium.org>
2018-10-05 19:26:55 +0000

    RemoteMacViews: Track parent and children in BridgedNativeWidgetHostImpl
    Track parent and children in BridgedNativeWidgetHostImpl. This tracking
    is already done in BridgedNativeWidgetImpl, which is not necessarily
    accessible in the browser process.
    
    Update the parent in NativeWidgetPrivate::ReparentNativeView, but
    only when the BridgedNativeWidgetImpl state would be updated by
    BridgedNativeWidgetImpl::ReparentNativeView.
    
    Remove BridgedNativeWidgetHostImpl from its parent in OnWindowWillClose.
    Move removal of children from being in BridgedNativeWidgetImpl's
    destructor to between calling OnWindowWillClose and OnWindowHasClosed,
    to ensure that all children have detached themselves before their
    parent is destroyed.
    
    Update some functions in NativeWidgetMac and NativeWidgetPrivate to
    use the state in BridgedNativeWidgetHostImpl, instead of using the
    state in BridgedNativeWidgetImpl.
    
    Bug: 859152
    Change-Id: Id98b9239db88154d9304f84d369d40d47af61d0f
    Reviewed-on: https://chromium-review.googlesource.com/c/1262624
    Commit-Queue: ccameron <ccameron@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597238}

--- ui/views/cocoa/bridged_native_widget_host_impl.h
+++ ui/views/cocoa/bridged_native_widget_host_impl.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define UI_VIEWS_COCOA_BRIDGED_NATIVE_WIDGET_HOST_IMPL_H_
 
 #include <memory>
+#include <vector>
 
 #include "base/mac/scoped_nsobject.h"
 #include "base/macros.h"
@@ -63,13 +64,17 @@ class VIEWS_EXPORT BridgedNativeWidgetHostImpl
       uint64_t bridged_native_widget_id);
   uint64_t bridged_native_widget_id() const { return widget_id_; }
 
-  // Creates one side of the bridge. |parent| must not be NULL.
-  explicit BridgedNativeWidgetHostImpl(NativeWidgetMac* parent);
+  // Creates one side of the bridge. |owner| must not be NULL.
+  explicit BridgedNativeWidgetHostImpl(NativeWidgetMac* owner);
   ~BridgedNativeWidgetHostImpl() override;
 
   // The NativeWidgetMac that owns |this|.
   views::NativeWidgetMac* native_widget_mac() const {
     return native_widget_mac_;
+  }
+  BridgedNativeWidgetHostImpl* parent() const { return parent_; }
+  std::vector<BridgedNativeWidgetHostImpl*> children() const {
+    return children_;
   }
 
   // The bridge factory that was used to create the true NSWindow for this
@@ -164,6 +169,10 @@ class VIEWS_EXPORT BridgedNativeWidgetHostImpl
   // The restored bounds will be derived from the current NSWindow frame unless
   // fullscreen or transitioning between fullscreen states.
   gfx::Rect GetRestoredBounds() const;
+
+  // Set |parent_| and update the old and new parents' |children_|. It is valid
+  // to set |new_parent| to nullptr.
+  void SetParent(BridgedNativeWidgetHostImpl* new_parent);
 
   // Properties set and queried by views. Not actually native.
   void SetNativeWindowProperty(const char* name, void* value);
@@ -301,7 +310,12 @@ class VIEWS_EXPORT BridgedNativeWidgetHostImpl
   const uint64_t widget_id_;
   views::NativeWidgetMac* const native_widget_mac_;  // Weak. Owns |this_|.
 
-  // The factory that was used to create |bridge_ptr_|.
+  // Parent and child widgets.
+  BridgedNativeWidgetHostImpl* parent_ = nullptr;
+  std::vector<BridgedNativeWidgetHostImpl*> children_;
+
+  // The factory that was used to create |bridge_ptr_|. This must be the same
+  // as |parent_->bridge_factory_host_|.
   BridgeFactoryHost* bridge_factory_host_ = nullptr;
 
   Widget::InitParams::Type widget_type_ = Widget::InitParams::TYPE_WINDOW;
