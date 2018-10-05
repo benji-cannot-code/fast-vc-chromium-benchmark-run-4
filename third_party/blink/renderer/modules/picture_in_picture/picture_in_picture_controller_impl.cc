67fe0d6a06d4a4e6eb5c91715af9f8fc721a6579
Ian Clelland <iclelland@chromium.org>
2018-10-05 04:17:18 +0000

    Enable FP reporting for PIP policy violations.
    This queues a report through the Reporting API whenever
    requestPictureInPicture is called in a frame in which the
    'picture-in-picture' feature is not allowed according to feature policy.
    
    Bug: 867471
    Change-Id: I1cede10385f6a5110fd0ddd5bf124ff5d3737dd9
    Reviewed-on: https://chromium-review.googlesource.com/c/1262432
    Commit-Queue: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: François Beaufort <beaufort.francois@gmail.com>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596988}

--- third_party/blink/renderer/modules/picture_in_picture/picture_in_picture_controller_impl.cc
+++ third_party/blink/renderer/modules/picture_in_picture/picture_in_picture_controller_impl.cc
@@ -56,7 +56,8 @@ PictureInPictureControllerImpl::IsDocumentAllowed() const {
   // "picture-in-picture", return kDisabledByFeaturePolicy status.
   if (RuntimeEnabledFeatures::PictureInPictureAPIEnabled() &&
       !GetSupplementable()->IsFeatureEnabled(
-          blink::mojom::FeaturePolicyFeature::kPictureInPicture)) {
+          blink::mojom::FeaturePolicyFeature::kPictureInPicture,
+          ReportOptions::kReportOnFailure)) {
     return Status::kDisabledByFeaturePolicy;
   }
 
