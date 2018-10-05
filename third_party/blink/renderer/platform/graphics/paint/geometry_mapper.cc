818c908af2864c3306a2ee51b2ee3e812bddbe64
Vladimir Levin <vmpstr@chromium.org>
2018-10-05 23:39:50 +0000

    Unalias effect nodes when determining whether it has opacity/distorsions
    This patch unaliases effect nodes in two places in layout object:
    - HasDistortingVisualEffects
    - HasNonZeroEffectiveOpacity,
    
    both of which navigate up the effect node chain and check properties.
    
    R=chrishtr@chromium.org, pdr@chromium.org, wangxianzhu@chromium.org
    
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ib035679dff6822a7cc6127d6b7589e10a89e0cc3
    Reviewed-on: https://chromium-review.googlesource.com/c/1265088
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597367}

--- third_party/blink/renderer/platform/graphics/paint/geometry_mapper.cc
+++ third_party/blink/renderer/platform/graphics/paint/geometry_mapper.cc
@@ -10,11 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-template <typename NodeType>
-const NodeType* SafeUnalias(const NodeType* node) {
-  return node ? node->Unalias() : nullptr;
-}
-
 const TransformationMatrix& GeometryMapper::SourceToDestinationProjection(
     const TransformPaintPropertyNode* source,
     const TransformPaintPropertyNode* destination) {
