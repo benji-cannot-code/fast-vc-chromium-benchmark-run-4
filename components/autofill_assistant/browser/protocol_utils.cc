3c1021ae3fff8fe293f6800a01f5518acaac3b2f
Mathias Carlen <mcarlen@chromium.org>
2018-10-05 08:17:00 +0000

    [Autofill Assistant] Implement the highlight element action.
    This change adds a highlight element action which updates the css shadow of a
    given element.
    
    R=gogerald@chromium.org
    
    Bug: 806868
    Change-Id: Ife28259e4020721fd87487770b154c452425a48c
    Reviewed-on: https://chromium-review.googlesource.com/c/1262555
    Commit-Queue: Mathias Carlen <mcarlen@chromium.org>
    Reviewed-by: Ganggui Tang <gogerald@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597032}

--- components/autofill_assistant/browser/protocol_utils.cc
+++ components/autofill_assistant/browser/protocol_utils.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill_assistant/browser/actions/autofill_action.h"
 #include "components/autofill_assistant/browser/actions/click_action.h"
 #include "components/autofill_assistant/browser/actions/focus_element_action.h"
+#include "components/autofill_assistant/browser/actions/highlight_element_action.h"
 #include "components/autofill_assistant/browser/actions/navigate_action.h"
 #include "components/autofill_assistant/browser/actions/reset_action.h"
 #include "components/autofill_assistant/browser/actions/select_option_action.h"
@@ -189,6 +190,14 @@ bool ProtocolUtils::ParseActions(
       }
       case ActionProto::ActionInfoCase::kReset: {
         actions->emplace_back(std::make_unique<ResetAction>(action));
+        break;
+      }
+      case ActionProto::ActionInfoCase::kHighlightElement: {
+        actions->emplace_back(std::make_unique<HighlightElementAction>(action));
+        break;
+      }
+      case ActionProto::ActionInfoCase::kUploadDom: {
+        actions->emplace_back(std::make_unique<UploadDomAction>(action));
         break;
       }
       default:
