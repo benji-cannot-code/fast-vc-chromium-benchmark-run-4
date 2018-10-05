dd0f1cabaf0fa791c73761030a3b47b0fee38f7f
Sebastien Marchand <sebmarchand@chromium.org>
2018-10-05 16:56:30 +0000

    Add a feature flag to disable the proactive discarding protections
    Bug: 876348
    Change-Id: Ib932652af7814f421009be79a7e9a1e5413fffd7
    Reviewed-on: https://chromium-review.googlesource.com/c/1228505
    Commit-Queue: Sébastien Marchand <sebmarchand@chromium.org>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597171}

--- chrome/browser/resource_coordinator/tab_manager_features.cc
+++ chrome/browser/resource_coordinator/tab_manager_features.cc
@@ -84,6 +84,8 @@ const char kProactiveTabFreezeAndDiscardFeatureName[] =
     "ProactiveTabFreezeAndDiscard";
 const char kProactiveTabFreezeAndDiscard_ShouldProactivelyDiscardParam[] =
     "ShouldProactivelyDiscard";
+const char kProactiveTabFreezeAndDiscard_DisableHeuristicsParam[] =
+    "DisableHeuristicsProtections";
 
 // Instantiate the feature parameters for proactive tab discarding.
 constexpr base::FeatureParam<bool>
@@ -110,6 +112,8 @@ constexpr base::FeatureParam<int>
     ProactiveTabFreezeAndDiscardParams::kUnfreezeTimeout;
 constexpr base::FeatureParam<int>
     ProactiveTabFreezeAndDiscardParams::kRefreezeTimeout;
+constexpr base::FeatureParam<bool>
+    ProactiveTabFreezeAndDiscardParams::kDisableHeuristicsProtections;
 
 // Instantiate the feature parameters for the site characteristics database.
 constexpr base::FeatureParam<int>
@@ -199,6 +203,9 @@ ProactiveTabFreezeAndDiscardParams GetProactiveTabFreezeAndDiscardParams(
 
   params.refreeze_timeout = base::TimeDelta::FromSeconds(
       ProactiveTabFreezeAndDiscardParams::kRefreezeTimeout.Get());
+
+  params.disable_heuristics_protections =
+      ProactiveTabFreezeAndDiscardParams::kDisableHeuristicsProtections.Get();
 
   return params;
 }
