bb16457f0ce5f2e26868eb0c66a979a2860553cc
Paul Jensen <pauljensen@chromium.org>
2018-10-05 14:54:38 +0000

    [Cronet] Native performance test
    Bug: 879211
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;master.tryserver.chromium.android:android_cronet_tester
    Change-Id: Id3f234c20fb0bf28fab51a4697d8fab7b473ba73
    Reviewed-on: https://chromium-review.googlesource.com/c/1207196
    Commit-Queue: Paul Jensen <pauljensen@chromium.org>
    Reviewed-by: Misha Efimov <mef@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597119}

---
+++ components/cronet/native/perftest/main.cc
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/cronet/native/perftest/perf_test.h"
+
+#include "base/logging.h"
+
+// When invoked, passes first and only argument to native performance test.
+int main(int argc, char* argv[]) {
+  CHECK_EQ(argc, 2) << "Must include experimental options in JSON as only arg.";
+  PerfTest(argv[1]);
+  return 0;
+}
---
+++ components/cronet/native/perftest/perf_test.h
@@ -0,0 +1,8 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Run Cronet native performance test. |json_args| is the string containing
+// the JSON formatted arguments from components/cronet/native/perftest/run.py.
+void PerfTest(const char* json_args);
