1a708e038c4b7a51c5caad78c3e93f4a69f42958
Stephane Zermatten <szermatt@chromium.org>
2018-10-05 16:25:34 +0000

    [Autofill Assistant] Fix WaitForDomAction.
    Before this patch, WaitForDomAction could get into situation where
    rounds was < 0 and crash in debug mode. This could happen if timeout_ms
    was very small or at the end, if the element was not found.
    
    This patch fixes the round counting and dchecks.
    
    With this patch, WaitForDomAction won't crash in debug mode when the
    element cannot be found.
    
    Bug: 806868
    Change-Id: I308883441876df66225550b9ddc43d1894a48de6
    Reviewed-on: https://chromium-review.googlesource.com/c/1264576
    Reviewed-by: Ganggui Tang <gogerald@chromium.org>
    Commit-Queue: Ganggui Tang <gogerald@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597160}

--- components/autofill_assistant/browser/actions/wait_for_dom_action.cc
+++ components/autofill_assistant/browser/actions/wait_for_dom_action.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/autofill_assistant/browser/actions/wait_for_dom_action.h"
 
+#include <algorithm>
 #include <cmath>
 
 #include "base/bind.h"
@@ -45,7 +46,9 @@ void WaitForDomAction::ProcessAction(ActionDelegate* delegate,
 
   int timeout_ms = proto_.wait_for_dom().timeout_ms();
   if (timeout_ms > 0)
-    check_rounds = std::ceil(timeout_ms / kCheckPeriodInMilliseconds);
+    check_rounds = std::min(
+        1,
+        static_cast<int>(std::ceil(timeout_ms / kCheckPeriodInMilliseconds)));
 
   CheckElementExists(delegate, check_rounds, std::move(callback));
 }
@@ -53,7 +56,7 @@ void WaitForDomAction::ProcessAction(ActionDelegate* delegate,
 void WaitForDomAction::CheckElementExists(ActionDelegate* delegate,
                                           int rounds,
                                           ProcessActionCallback callback) {
-  DCHECK(rounds > 0);
+  DCHECK_GT(rounds, 0);
   std::vector<std::string> selectors;
   for (const auto& selector : proto_.wait_for_dom().selectors()) {
     selectors.emplace_back(selector);
@@ -74,13 +77,14 @@ void WaitForDomAction::OnCheckElementExists(ActionDelegate* delegate,
     return;
   }
 
-  if (rounds == 0) {
+  --rounds;
+  if (rounds <= 0) {
+    DCHECK_EQ(rounds, 0);
     UpdateProcessedAction(ELEMENT_RESOLUTION_FAILED);
     std::move(callback).Run(std::move(processed_action_proto_));
     return;
   }
 
-  --rounds;
   base::PostDelayedTaskWithTraits(
       FROM_HERE, {content::BrowserThread::UI},
       base::BindOnce(&WaitForDomAction::CheckElementExists,
