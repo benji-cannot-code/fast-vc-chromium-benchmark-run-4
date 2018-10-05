1dc11ff1798c38b5494607741809612fbd3eb2ec
Alex Leung <alexleung@google.com>
2018-10-05 00:55:12 +0000

    Use android mojo media client in cast_mojo_media_client
    Bug: b:116044941
    Change-Id: I9c9ed2bf82164e8fa390ffcc98405085caa5b3ae
    Reviewed-on: https://chromium-review.googlesource.com/c/1260496
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Commit-Queue: Alex Leung <alexleung@google.com>
    Cr-Commit-Position: refs/heads/master@{#596949}

--- chromecast/browser/cast_content_browser_client.cc
+++ chromecast/browser/cast_content_browser_client.cc
@@ -91,6 +91,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_ANDROID)
 #include "components/cdm/browser/cdm_message_filter_android.h"
 #include "components/crash/content/browser/child_exit_observer_android.h"
+#include "media/mojo/services/android_mojo_media_client.h"
 #if !BUILDFLAG(USE_CHROMECAST_CDMS)
 #include "components/cdm/browser/media_drm_storage_impl.h"
 #include "url/origin.h"
@@ -125,6 +126,10 @@ namespace {
 #if BUILDFLAG(ENABLE_MOJO_MEDIA_IN_BROWSER_PROCESS)
 static std::unique_ptr<service_manager::Service> CreateMediaService(
     CastContentBrowserClient* browser_client) {
+#if defined(OS_ANDROID)
+  return std::make_unique<::media::MediaService>(
+      std::make_unique<::media::AndroidMojoMediaClient>());
+#else
   auto mojo_media_client = std::make_unique<media::CastMojoMediaClient>(
       browser_client->GetCmaBackendFactory(),
       base::Bind(&CastContentBrowserClient::CreateCdmFactory,
@@ -133,6 +138,7 @@ static std::unique_ptr<service_manager::Service> CreateMediaService(
       browser_client->GetVideoResolutionPolicy(),
       browser_client->media_resource_tracker());
   return std::make_unique<::media::MediaService>(std::move(mojo_media_client));
+#endif  // defined(OS_ANDROID)
 }
 #endif  // BUILDFLAG(ENABLE_MOJO_MEDIA_IN_BROWSER_PROCESS)
 
@@ -661,7 +667,7 @@ void CastContentBrowserClient::ExposeInterfacesToMediaService(
 #if defined(OS_ANDROID) && !BUILDFLAG(USE_CHROMECAST_CDMS)
   registry->AddInterface(
       base::BindRepeating(&CreateMediaDrmStorage, render_frame_host));
-#endif
+#endif  // defined(OS_ANDROID) && !BUILDFLAG(USE_CHROMECAST_CDMS)
 
   std::string application_session_id =
       CastNavigationUIData::GetSessionIdForWebContents(
