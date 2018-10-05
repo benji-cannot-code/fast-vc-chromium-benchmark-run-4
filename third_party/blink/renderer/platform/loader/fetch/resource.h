b3925242a51047139b17fb5bb7b03a411e9037e3
Scott Little <sclittle@chromium.org>
2018-10-05 18:54:19 +0000

    Bypass the cache when reloading LoFi images.
    This is a partial revert of
    https://chromium-review.googlesource.com/c/chromium/src/+/1166600, as a
    temporary workaround to the bug listed below, which is caused by a bug
    in the network stack that affects partial range cache revalidations.
    
    This CL only bypasses the cache when reloading placeholders for LoFi,
    and not when reloading placeholders for lazily loaded images, because
    bypassing the cache when lazily loading images can potentially waste a
    lot of network data, plus lazy image loading isn't launched yet, so
    keeping the behavior as-is will make it easier to debug and fix the
    underlying problem.
    
    Bug: 892461
    Change-Id: I96ed023431fc4e7e346497820da1de14940aca64
    Reviewed-on: https://chromium-review.googlesource.com/c/1263565
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Scott Little <sclittle@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597219}

--- third_party/blink/renderer/platform/loader/fetch/resource.h
+++ third_party/blink/renderer/platform/loader/fetch/resource.h
@@ -477,6 +477,7 @@ class PLATFORM_EXPORT Resource : public GarbageCollectedFinalized<Resource>,
     return clients_;
   }
 
+  void SetCachePolicyBypassingCache();
   void SetPreviewsState(WebURLRequest::PreviewsState);
   void ClearRangeRequestHeader();
 
