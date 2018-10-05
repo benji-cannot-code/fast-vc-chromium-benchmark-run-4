2e028d7ea73f6dedbd63ac33351db81e9976234b
Finnur Thorarinsson <finnur@chromium.org>
2018-10-05 15:14:09 +0000

    Contacts Picker: Add test for Select All function.
    TBR: twellington
    Bug: 860467
    Change-Id: I7e8b0f0391befdab1e68fd52eaab6a4480f9882a
    Reviewed-on: https://chromium-review.googlesource.com/c/1264795
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597128}

--- ui/android/java/src/org/chromium/ui/base/SelectFileDialog.java
+++ ui/android/java/src/org/chromium/ui/base/SelectFileDialog.java
@@ -441,6 +441,10 @@ public class SelectFileDialog
             case CONTACTS_SELECTED:
                 onFileNotSelected();
                 break;
+
+            case SELECT_ALL:
+            case UNDO_SELECT_ALL:
+                break;
         }
     }
 
