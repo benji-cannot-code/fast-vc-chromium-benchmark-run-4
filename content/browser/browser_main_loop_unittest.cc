987b246c9eb6467eba64bfcf87ee972f90912780
Xi Han <hanxi@google.com>
2018-10-05 19:52:30 +0000

    Start TaskScheduler before creating BrowserMainLoop.
    This is a follow up CL for: https://crrev.com/c/1115783.
    
    In this CL, we use ScopedExecutionFence to prevents tasks running between the
    peorid from BrowserMainRunner is instantiated to
    BrowserMainLoop::CreateThreads() is called.
    
    Bug: 846846
    Change-Id: I9dec8415e45f901c5883c196ab867c0ea4928594
    Reviewed-on: https://chromium-review.googlesource.com/c/1174955
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597255}

--- content/browser/browser_main_loop_unittest.cc
+++ content/browser/browser_main_loop_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/scoped_command_line.h"
 #include "content/browser/browser_thread_impl.h"
 #include "content/browser/scheduler/browser_task_executor.h"
+#include "content/browser/startup_helper.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/common/main_function_params.h"
@@ -23,14 +24,17 @@ namespace content {
 // the number of cores in its foreground pool.
 TEST(BrowserMainLoopTest, CreateThreadsInSingleProcess) {
   {
-    base::TaskScheduler::Create("Browser");
-    BrowserTaskExecutor::Create();
     base::test::ScopedCommandLine scoped_command_line;
     scoped_command_line.GetProcessCommandLine()->AppendSwitch(
         switches::kSingleProcess);
+    base::TaskScheduler::Create("Browser");
+    StartBrowserTaskScheduler();
+    BrowserTaskExecutor::Create();
     MainFunctionParams main_function_params(
         *scoped_command_line.GetProcessCommandLine());
-    BrowserMainLoop browser_main_loop(main_function_params);
+    BrowserMainLoop browser_main_loop(
+        main_function_params,
+        std::make_unique<base::TaskScheduler::ScopedExecutionFence>());
     browser_main_loop.MainMessageLoopStart();
     browser_main_loop.Init();
     browser_main_loop.CreateThreads();
--- content/browser/browser_main_runner_impl.cc
+++ content/browser/browser_main_runner_impl.cc
@@ -55,7 +55,10 @@ BrowserMainRunnerImpl* BrowserMainRunnerImpl::Create() {
 }
 
 BrowserMainRunnerImpl::BrowserMainRunnerImpl()
-    : initialization_started_(false), is_shutdown_(false) {}
+    : initialization_started_(false),
+      is_shutdown_(false),
+      scoped_execution_fence_(
+          std::make_unique<base::TaskScheduler::ScopedExecutionFence>()) {}
 
 BrowserMainRunnerImpl::~BrowserMainRunnerImpl() {
   if (initialization_started_ && !is_shutdown_)
@@ -108,7 +111,8 @@ int BrowserMainRunnerImpl::Initialize(const MainFunctionParams& parameters) {
     gfx::win::MaybeInitializeDirectWrite();
 #endif  // OS_WIN
 
-    main_loop_.reset(new BrowserMainLoop(parameters));
+    main_loop_.reset(
+        new BrowserMainLoop(parameters, std::move(scoped_execution_fence_)));
 
     main_loop_->Init();
 
