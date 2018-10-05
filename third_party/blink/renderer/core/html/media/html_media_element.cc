737ec049f63d7968fc7c0463a43a8d320484cf34
Mounir Lamouri <mlamouri@chromium.org>
2018-10-05 19:30:33 +0000

    HTMLMediaElement: asynchronously pause a video when removed from document.
    This changes follow the spec with regards to pausing a video when it
    leaves a document: when a video leaves a document, the removal process
    should continue and, asynchronously, Blink checks that the video is
    still in an active document. It allows a video to move from one part of
    the document to another without pausing. Moving into a document or
    into two different outside of the document trees will also not pause.
    
    This is following the specification and Edge/Safari/Firefox
    implementations.
    
    Bug: 382879, 490511
    Change-Id: I48abdaa5f81790e2cae7edc7b0bfb444f6ee7787
    Reviewed-on: https://chromium-review.googlesource.com/c/1074808
    Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597242}

--- third_party/blink/renderer/core/html/media/html_media_element.cc
+++ third_party/blink/renderer/core/html/media/html_media_element.cc
@@ -479,6 +479,10 @@ HTMLMediaElement::HTMLMediaElement(const QualifiedName& tag_name,
           document.GetTaskRunner(TaskType::kInternalMedia),
           this,
           &HTMLMediaElement::CheckViewportIntersectionTimerFired),
+      removed_from_document_timer_(
+          document.GetTaskRunner(TaskType::kInternalMedia),
+          this,
+          &HTMLMediaElement::OnRemovedFromDocumentTimerFired),
       played_time_ranges_(),
       async_event_queue_(EventQueue::Create(GetExecutionContext(),
                                             TaskType::kMediaElementEvent)),
@@ -578,6 +582,8 @@ void HTMLMediaElement::DidMoveToNewDocument(Document& old_document) {
   check_viewport_intersection_timer_.MoveToNewTaskRunner(
       GetDocument().GetTaskRunner(TaskType::kInternalMedia));
   deferred_load_timer_.MoveToNewTaskRunner(
+      GetDocument().GetTaskRunner(TaskType::kInternalMedia));
+  removed_from_document_timer_.MoveToNewTaskRunner(
       GetDocument().GetTaskRunner(TaskType::kInternalMedia));
 
   autoplay_policy_->DidMoveToNewDocument(old_document);
@@ -730,11 +736,9 @@ void HTMLMediaElement::RemovedFrom(ContainerNode& insertion_point) {
   BLINK_MEDIA_LOG << "removedFrom(" << (void*)this << ", " << insertion_point
                   << ")";
 
+  removed_from_document_timer_.StartOneShot(TimeDelta(), FROM_HERE);
+
   HTMLElement::RemovedFrom(insertion_point);
-  if (insertion_point.InActiveDocument()) {
-    if (network_state_ > kNetworkEmpty)
-      PauseInternal();
-  }
 }
 
 void HTMLMediaElement::AttachLayoutTree(AttachContext& context) {
@@ -1004,7 +1008,7 @@ void HTMLMediaElement::InvokeResourceSelectionAlgorithm() {
   // 3 - Set the media element's delaying-the-load-event flag to true (this
   // delays the load event)
   SetShouldDelayLoadEvent(true);
-  if (GetMediaControls())
+  if (GetMediaControls() && isConnected())
     GetMediaControls()->Reset();
 
   // 4 - Await a stable state, allowing the task that invoked this algorithm to
@@ -3615,6 +3619,7 @@ void HTMLMediaElement::ContextDestroyed(ExecutionContext*) {
     GetLayoutObject()->UpdateFromElement();
 
   StopPeriodicTimers();
+  removed_from_document_timer_.Stop();
 }
 
 bool HTMLMediaElement::HasPendingActivity() const {
@@ -4148,6 +4153,13 @@ EnumerationHistogram& HTMLMediaElement::ShowControlsHistogram() const {
   DEFINE_STATIC_LOCAL(EnumerationHistogram, histogram,
                       ("Media.Controls.Show.Audio", kMediaControlsShowMax));
   return histogram;
+}
+
+void HTMLMediaElement::OnRemovedFromDocumentTimerFired(TimerBase*) {
+  if (InActiveDocument())
+    return;
+
+  PauseInternal();
 }
 
 void HTMLMediaElement::ClearWeakMembers(Visitor* visitor) {
