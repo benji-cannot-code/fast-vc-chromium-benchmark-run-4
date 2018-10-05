6e808d5c46486e89edf62b94ff5c4ebaadcfb109
Sam McNally <sammc@chromium.org>
2018-10-05 02:41:03 +0000

    Fix a crash in TtsControllerImpl::VoicesChanged due to a use after free.
    TtsHandler doesn't remove itself as a VoicesChangedDelegate on
    destruction, causing use after frees.
    
    Bug: 885311
    Change-Id: Id6ed9cd5e9a1caa16e7f095eeeeb6baae1baa327
    Reviewed-on: https://chromium-review.googlesource.com/c/1260604
    Commit-Queue: Sam McNally <sammc@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596966}

--- chrome/browser/ui/webui/settings/tts_handler.cc
+++ chrome/browser/ui/webui/settings/tts_handler.cc
@@ -28,7 +28,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace settings {
 TtsHandler::TtsHandler() : weak_factory_(this) {}
 
-TtsHandler::~TtsHandler() {}
+TtsHandler::~TtsHandler() {
+  TtsController::GetInstance()->RemoveVoicesChangedDelegate(this);
+}
 
 void TtsHandler::HandleGetAllTtsVoiceData(const base::ListValue* args) {
   OnVoicesChanged();
