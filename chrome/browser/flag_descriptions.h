218849e71f3ec695bd303d33dfeaf757965ffb0c
Marijn Kruisselbrink <mek@chromium.org>
2018-10-05 16:59:49 +0000

    Revert "Prevent renderer process flooding the browser with IPC."
    This reverts commit 7d7cc8da0527068406db5308edfe1cb97a54f715.
    
    Reason for revert: The new browser tests are very flaky and fail regularly, for example in https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Mac10.11%20Tests/29408:
    
    [9182:775:1005/081300.634221:INFO:CONSOLE(4)] "Throttling navigation to prevent the browser from hanging. See https://crbug.com/882238 and chrome://flags/#disable-ipc-flooding-protection", source:  (4)
      ../../content/browser/navigation_browsertest.cc:1281: Failure
      Expected equality of these values:
        100
        count_replace_state
          Which is: 202
    
    Original change's description:
    > Prevent renderer process flooding the browser with IPC.
    > 
    > Some javascript function called in an infinite loop can cause the
    > browser process to be flooded by IPC and become unresponsive.
    > 
    > Limit the number of IPC sent to 10 per second per frame.
    > 
    > This was already done for:
    >  - history.pushState()
    >  - history.replaceState()
    > 
    > This is extended to:
    >  - history.back()
    >  - history.forward()
    >  - history.go()
    >  - navigations...
    > 
    > 3 tests added.
    > 
    > Bug: 882238
    > Change-Id: I821ff742307ab471ca52f27c6003d003ee978a46
    > Reviewed-on: https://chromium-review.googlesource.com/c/1230067
    > Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    > Reviewed-by: Camille Lamy <clamy@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Chris Palmer <palmer@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#596715}
    
    TBR=palmer@chromium.org,dcheng@chromium.org,clamy@chromium.org,arthursonzogni@chromium.org
    
    Change-Id: I27843365fff5da639091524af85791af0af9ce6d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 882238
    Reviewed-on: https://chromium-review.googlesource.com/c/1265060
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597172}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -220,9 +220,6 @@ extern const char kDevtoolsExperimentsDescription[];
 extern const char kDisableAudioForDesktopShareName[];
 extern const char kDisableAudioForDesktopShareDescription[];
 
-extern const char kDisableIpcFloodingProtectionName[];
-extern const char kDisableIpcFloodingProtectionDescription[];
-
 extern const char kDisablePushStateThrottleName[];
 extern const char kDisablePushStateThrottleDescription[];
 
