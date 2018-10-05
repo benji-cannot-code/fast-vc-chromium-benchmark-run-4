a41353a8432e320c341c99517c8d07270f0395aa
Xiaoqian Dai <xdai@chromium.org>
2018-10-05 23:07:15 +0000

    Revert TabletModeController refactoring cls.
    The refacotring CLs exposed an underlying issue that caused b/117075672.
    I need to revert all the refactoring for now, and fix the underlying issue
    first and then reland these refactoring cls.
    
    Basically the assumption of "--enable-touchview" USE flag is not longer true,
    thus Chrome can't reply on this flag to decide whether a device is capable of
    entering tablet mode. We need to find other ways to do so.
    
    Since it's a pure revert, TBR xiyuan@.
    
    Revert "TabletModeController refactoring II."
    
    This reverts commit 43b3f48dfdf7c77f1b75285bff576341286fedf0.
    
    Revert "TabletModeController refactoring III."
    
    This reverts commit 95f93d44d3caf29694533328e2162c360ed795c2.
    
    Revert "Fix the regression caused by the refactoring."
    
    This reverts commit 5b0a138d4ce2b8890dd858ca67cdb61878752c42.
    
    Bug:887042, b/117075672
    TBR=xiyuan@chromium.org
    
    Change-Id: I494a8d05be1fb07bea404cd714b0b1d9689e46d4
    Reviewed-on: https://chromium-review.googlesource.com/c/1265996
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597356}

--- ash/wm/tablet_mode/tablet_mode_controller_test_api.cc
+++ ash/wm/tablet_mode/tablet_mode_controller_test_api.cc
@@ -22,8 +22,9 @@ void TabletModeControllerTestApi::EnterTabletMode() {
   tablet_mode_controller_->AttemptEnterTabletMode();
 }
 
-void TabletModeControllerTestApi::LeaveTabletMode() {
-  tablet_mode_controller_->AttemptLeaveTabletMode();
+void TabletModeControllerTestApi::LeaveTabletMode(
+    bool called_by_device_update) {
+  tablet_mode_controller_->AttemptLeaveTabletMode(called_by_device_update);
 }
 
 void TabletModeControllerTestApi::AttachExternalMouse() {
