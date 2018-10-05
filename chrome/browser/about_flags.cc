066f3c1d848dac9f63e46789bfab2e4bcfb4dff9
Jared Saul <jsaul@google.com>
2018-10-05 22:27:45 +0000

    [Autofill] Always enable updated prompt explanation for upload
    Please see the bug for the detailed cleanup plan.
    
    Bug: 891912
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: Ifdb7cb4718917cdbf4f3e5137fe47a3c98c83997
    Reviewed-on: https://chromium-review.googlesource.com/c/1260011
    Commit-Queue: Jared Saul <jsaul@google.com>
    Reviewed-by: Robert Kaplow (sloooow) <rkaplow@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597346}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -3107,14 +3107,6 @@ const FeatureEntry kFeatureEntries[] = {
      kOsAndroid,
      FEATURE_VALUE_TYPE(
          autofill::features::kAutofillUpstreamUseGooglePayBrandingOnMobile)},
-    {"enable-autofill-credit-card-upload-update-prompt-explanation",
-     flag_descriptions::
-         kEnableAutofillCreditCardUploadUpdatePromptExplanationName,
-     flag_descriptions::
-         kEnableAutofillCreditCardUploadUpdatePromptExplanationDescription,
-     kOsAll,
-     FEATURE_VALUE_TYPE(
-         autofill::features::kAutofillUpstreamUpdatePromptExplanation)},
     {"enable-autofill-local-card-migration-show-feedback",
      flag_descriptions::kEnableAutofillLocalCardMigrationShowFeedbackName,
      flag_descriptions::
